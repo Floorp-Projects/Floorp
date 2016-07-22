e49ad837f4dd11d90ad28b7c068b91d5539fbcdc
Julian Descottes <jdescottes@mozilla.com>
2016-07-22 17:35:03 +0200

    Bug 1285591 - fix accessibility in devtools autocomplete using suggestion list clone;r=bgrins
    Devtools autocomplete popups are hosted in a different document from the input
    being autocompleted. To allow accessibility tools such as screen readers to still
    make sense of this widget, a clone of the suggestion list is now inserted in the same
    document as the input, and the aria-activedescendant attribute is updated on the input
    accordingly.
    
    MozReview-Commit-ID: 8rFjF6nvEyU

--- devtools/client/webconsole/test/browser_webconsole_bug_585991_autocomplete_popup.js
+++ devtools/client/webconsole/test/browser_webconsole_bug_585991_autocomplete_popup.js
@@ -25,13 +25,16 @@ function consoleOpened(HUD) {
   ];
 
   let popup = HUD.jsterm.autocompletePopup;
+  let input = HUD.jsterm.inputNode;
 
   ok(!popup.isOpen, "popup is not open");
+  ok(!input.hasAttribute("aria-activedescendant"), "no aria-activedescendant");
 
   popup.once("popup-opened", () => {
     ok(popup.isOpen, "popup is open");
 
     is(popup.itemCount, 0, "no items");
+    ok(!input.hasAttribute("aria-activedescendant"), "no aria-activedescendant");
 
     popup.setItems(items);
 
@@ -44,31 +47,37 @@ function consoleOpened(HUD) {
 
     is(popup.selectedIndex, 2, "Index of the first item from bottom is selected.");
     is(popup.selectedItem, items[2], "First item from bottom is selected");
+    checkActiveDescendant(popup, input);
 
     popup.selectedIndex = 1;
 
     is(popup.selectedIndex, 1, "index 1 is selected");
     is(popup.selectedItem, items[1], "item1 is selected");
+    checkActiveDescendant(popup, input);
 
     popup.selectedItem = items[2];
 
     is(popup.selectedIndex, 2, "index 2 is selected");
     is(popup.selectedItem, items[2], "item2 is selected");
+    checkActiveDescendant(popup, input);
 
     is(popup.selectPreviousItem(), items[1], "selectPreviousItem() works");
 
     is(popup.selectedIndex, 1, "index 1 is selected");
     is(popup.selectedItem, items[1], "item1 is selected");
+    checkActiveDescendant(popup, input);
 
     is(popup.selectNextItem(), items[2], "selectNextItem() works");
 
     is(popup.selectedIndex, 2, "index 2 is selected");
     is(popup.selectedItem, items[2], "item2 is selected");
+    checkActiveDescendant(popup, input);
 
     ok(popup.selectNextItem(), "selectNextItem() works");
 
     is(popup.selectedIndex, 0, "index 0 is selected");
     is(popup.selectedItem, items[0], "item0 is selected");
+    checkActiveDescendant(popup, input);
 
     items.push({label: "label3", value: "value3"});
     popup.appendItem(items[3]);
@@ -77,15 +86,18 @@ function consoleOpened(HUD) {
 
     popup.selectedIndex = 3;
     is(popup.selectedItem, items[3], "item3 is selected");
+    checkActiveDescendant(popup, input);
 
     popup.removeItem(items[2]);
 
     is(popup.selectedIndex, 2, "index2 is selected");
     is(popup.selectedItem, items[3], "item3 is still selected");
+    checkActiveDescendant(popup, input);
     is(popup.itemCount, items.length - 1, "item2 removed");
 
     popup.clearItems();
     is(popup.itemCount, 0, "items cleared");
+    ok(!input.hasAttribute("aria-activedescendant"), "no aria-activedescendant");
 
     popup.once("popup-closed", () => {
       deferred.resolve();
@@ -93,7 +105,19 @@ function consoleOpened(HUD) {
     popup.hidePopup();
   });
 
-  popup.openPopup(HUD.jsterm.inputNode);
+  popup.openPopup(input);
 
   return deferred.promise;
+}
+
+function checkActiveDescendant(popup, input) {
+  let activeElement = input.ownerDocument.activeElement;
+  let descendantId = activeElement.getAttribute("aria-activedescendant");
+  let popupItem = popup._tooltip.panel.querySelector("#" + descendantId);
+  let cloneItem = input.ownerDocument.querySelector("#" + descendantId);
+
+  ok(popupItem, "Active descendant is found in the popup list");
+  ok(cloneItem, "Active descendant is found in the list clone");
+  is(popupItem.innerHTML, cloneItem.innerHTML,
+    "Cloned item has the same HTML as the original element");
 }
