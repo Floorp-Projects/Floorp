1de39f8b4ef7c9467304c002f16c5f5e5265cdef
Jeff Gilbert <jgilbert@mozilla.com>
2016-07-21 19:32:59 -0700

    Bug 1280499 - Unlock UNPACK_BUFFER. - r=ethlin
    MozReview-Commit-ID: 5RbFNeKsvqF

--- dom/canvas/WebGL2ContextBuffers.cpp
+++ dom/canvas/WebGL2ContextBuffers.cpp
@@ -20,15 +20,10 @@ WebGL2Context::ValidateBufferTarget(GLenum target, const char* funcName)
     case LOCAL_GL_COPY_WRITE_BUFFER:
     case LOCAL_GL_ELEMENT_ARRAY_BUFFER:
     case LOCAL_GL_PIXEL_PACK_BUFFER:
+    case LOCAL_GL_PIXEL_UNPACK_BUFFER:
     case LOCAL_GL_TRANSFORM_FEEDBACK_BUFFER:
     case LOCAL_GL_UNIFORM_BUFFER:
         return true;
-
-    case LOCAL_GL_PIXEL_UNPACK_BUFFER:
-        ErrorInvalidOperation("%s: PBOs are still under development, and are currently"
-                              " disabled.",
-                              funcName);
-        return false;
 
     default:
         ErrorInvalidEnumInfo(funcName, target);
