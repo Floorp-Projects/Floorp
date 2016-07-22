fdc4986c85f698d60a39a2ac7e0f917a860da7f5
Honza Bambas <honzab.moz@firemni.cz>
2016-07-22 09:58:00 +0200

    Bug 1287219 - Fix leak of nsFtpControlConnection in nsFtpProtocolHandler::RemoveConnection, r=mcmanus

--- netwerk/protocol/ftp/nsFtpConnectionThread.cpp
+++ netwerk/protocol/ftp/nsFtpConnectionThread.cpp
@@ -262,10 +262,10 @@ nsFtpState::EstablishControlConnection()
     LOG(("FTP:(%x) trying cached control\n", this));
         
     // Look to see if we can use a cached control connection:
-    nsFtpControlConnection *connection = nullptr;
+    RefPtr<nsFtpControlConnection> connection;
     // Don't use cached control if anonymous (bug #473371)
     if (!mChannel->HasLoadFlag(nsIRequest::LOAD_ANONYMOUS))
-        gFtpHandler->RemoveConnection(mChannel->URI(), &connection);
+        gFtpHandler->RemoveConnection(mChannel->URI(), getter_AddRefs(connection));
 
     if (connection) {
         mControlConnection.swap(connection);
