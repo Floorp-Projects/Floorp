ea27d6dd7955f26b9762b7538e483b60d7609a2c
Jordan Lund <jlund@mozilla.com>
2016-07-22 18:24:50 -0700

    Bug 1288882 - use_vcs_unique_share defined in mozharness migration configs is ignored, r=Callek a=release DONTBUILD
    note: this requires a clean work dir unfortunately. so you have to blow away
    the fake build/hg-share and any repos in build/
    
    MozReview-Commit-ID: 3TfNLdga9Dt
    
    --HG--
    extra : rebase_source : 25972c5b53eb1bddd490c7aea6a085b713ff7d03
    extra : amend_source : 5841fb61e94ab9c4c0f43b344f1a68d589a5c356

--- testing/mozharness/configs/merge_day/release_to_esr.py
+++ testing/mozharness/configs/merge_day/release_to_esr.py
@@ -21,7 +21,6 @@ config = {
          "MAR_CHANNEL_ID=firefox-mozilla-release",
          "MAR_CHANNEL_ID=firefox-mozilla-esr"),
     ],
-    "use_vcs_unique_share": True,
     "vcs_share_base": os.path.join(ABS_WORK_DIR, 'hg-shared'),
     # "hg_share_base": None,
     "tools_repo_url": "https://hg.mozilla.org/build/tools",
