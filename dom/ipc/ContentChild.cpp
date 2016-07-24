e1b288ac54645424b227d5e9c415745a87d27f69
Chris Pearce <cpearce@mozilla.com>
2016-07-25 10:18:34 +1200

    Bug 1288976 - Use gmp-changed rather than gmp-path-added to retry MediaKeys requests. r=gerald,a=gchang
    We're already routing the "gmp-changed" observer service notification over from
    the chrome process to the content process, and it fires at pretty much the same
    time as the "gmp-path-added" notification (and a few more) so we can just switch
    to have the MediaKeySystemAccessManager listen on that notification instead, and
    we'll be e10s compatible.
    
    
    
    
    MozReview-Commit-ID: EowFDfdWgAJ

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1742,6 +1742,9 @@ bool
 ContentChild::RecvNotifyGMPsChanged()
 {
   GMPDecoderModule::UpdateUsableCodecs();
+  MOZ_ASSERT(NS_IsMainThread());
+  nsCOMPtr<nsIObserverService> obs = mozilla::services::GetObserverService();
+  obs->NotifyObservers(nullptr, "gmp-changed", nullptr);
   return true;
 }
 
