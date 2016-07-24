f964b84f2eaa14537ca31d2f840a46a6c05def1a
Kris Maglione <maglione.k@gmail.com>
2016-07-24 16:09:26 -0700

    Bug 1288885: Support testing WebExtensions from xpcshell tests. r=aswan
    Most of the test helper code is derived from the SpecialPowers/ExtensionTestUtils
    code that does the same. Eventually, the two implementations should probably
    be unified, but I don't think it's worth the trouble for now.
    
    MozReview-Commit-ID: 7Yy9jWkGsMM

--- browser/components/extensions/test/xpcshell/test_ext_manifest_commands.js
+++ browser/components/extensions/test/xpcshell/test_ext_manifest_commands.js
@@ -4,7 +4,7 @@
 
 
 add_task(function* test_manifest_commands() {
-  let normalized = yield normalizeManifest({
+  let normalized = yield ExtensionTestUtils.normalizeManifest({
     "commands": {
       "toggle-feature": {
         "suggested_key": {"default": "Shifty+Y"},
