cbe6e2d5c248c30ab6fcb879d435374576244e26
Jordan Lund <jlund@mozilla.com>
2016-07-23 22:44:47 -0700

    Bug 1278261 - drop api-9 mozilla-release release support, remove all api-9 and api-11 references, r=Callek
    this can be uplifted through 48 mozilla-release
    
    MozReview-Commit-ID: KncTJ8hAgfO

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_9_debug.py
+++
@@ -1,9 +0,0 @@
-config = {
-    'base_name': 'Android armv7 API 9 %(branch)s debug',
-    'stage_platform': 'android-api-9-debug',
-    'build_type': 'api-9-debug',
-    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-9-10-constrained/debug',
-    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android/releng.manifest',
-    'multi_locale_config_platform': 'android',
-    'debug_build': True,
-}
