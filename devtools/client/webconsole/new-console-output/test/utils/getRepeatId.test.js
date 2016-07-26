d9b24b57ac928877d312099f547bbda2b297680c
Lin Clark <lclark@mozilla.com>
2016-07-26 15:56:46 -0400

    Bug 1289258 - Part 4: Convert utils tests to mocha. r=bgrins
    --HG--
    rename : devtools/client/webconsole/new-console-output/test/utils/test_render_perf.html => devtools/client/webconsole/new-console-output/test/mochitest/test_render_perf.html
    extra : rebase_source : 15bce09cc8cbfa788ffffd3f4bd1d6617c7d6cc7

---
+++ devtools/client/webconsole/new-console-output/test/utils/getRepeatId.test.js
@@ -0,0 +1,41 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+const { getRepeatId } = require("devtools/client/webconsole/new-console-output/utils/messages");
+const { stubConsoleMessages } = require("devtools/client/webconsole/new-console-output/test/fixtures/stubs");
+
+const expect = require("expect");
+
+describe("getRepeatId:", () => {
+  it("returns same repeatId for duplicate values", () => {
+    const message1 = stubConsoleMessages.get("console.log('foobar', 'test')");
+    const message2 = message1.set("repeat", 3);
+    expect(getRepeatId(message1)).toEqual(getRepeatId(message2));
+  });
+
+  it("returns different repeatIds for different values", () => {
+    const message1 = stubConsoleMessages.get("console.log('foobar', 'test')");
+    const message2 = message1.set("parameters", ["funny", "monkey"]);
+    expect(getRepeatId(message1)).toNotEqual(getRepeatId(message2));
+  });
+
+  it("returns different repeatIds for different severities", () => {
+    const message1 = stubConsoleMessages.get("console.log('foobar', 'test')");
+    const message2 = message1.set("level", "error");
+    expect(getRepeatId(message1)).toNotEqual(getRepeatId(message2));
+  });
+
+  it("handles falsy values distinctly", () => {
+    const messageNaN = stubConsoleMessages.get("console.log(NaN)");
+    const messageUnd = stubConsoleMessages.get("console.log(undefined)");
+    const messageNul = stubConsoleMessages.get("console.log(null)");
+
+    const repeatIds = new Set([
+      getRepeatId(messageNaN),
+      getRepeatId(messageUnd),
+      getRepeatId(messageNul)]
+    );
+    expect(repeatIds.size).toEqual(3);
+  });
+});
