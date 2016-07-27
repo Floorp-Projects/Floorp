35a50161ac968cd5eed2d1dfcf90cbe7830232f0
Tom Schuster <evilpies@gmail.com>
2016-07-27 20:12:04 +0200

    Bug 1279303 - Implement change to O.getOwnPropertyDescriptors and update tests. r=efaust

--- js/src/tests/test262/built-ins/Object/getOwnPropertyDescriptors/shell.js
+++ js/src/tests/test262/built-ins/Object/getOwnPropertyDescriptors/shell.js
@@ -1,5 +1,14 @@
 var assert = {
     sameValue: assertEq,
+    notSameValue(a, b, msg) {
+      try {
+        assertEq(a, b);
+        throw "equal"
+      } catch (e) {
+        if (e === "equal")
+          throw new Error("Assertion failed: expected different values, got " + a);
+      }
+    },
     throws(ctor, f) {
       var fullmsg;
       try {
