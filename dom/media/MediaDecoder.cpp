348c35b5ddeaebad654726e3658deaa83e59ee12
JW Wang <jwwang@mozilla.com>
2016-07-27 10:38:25 +0800

    Bug 1289976. Part 4 - Remove the IsShutdown() check from MediaDecoder::NotifyOwnerActivityChanged() which happens before Shutdown(). r=kaku
    MozReview-Commit-ID: 9tWLW4JE0Uv
    
    --HG--
    extra : rebase_source : 9f33359f5d8833a8f90319303a96cf029460f3ac

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -292,10 +292,7 @@ void
 MediaDecoder::NotifyOwnerActivityChanged(bool aIsVisible)
 {
   MOZ_ASSERT(NS_IsMainThread());
-
-  if (IsShutdown()) {
-    return;
-  }
+  MOZ_ASSERT(!IsShutdown());
 
   SetElementVisibility(aIsVisible);
 
