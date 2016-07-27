fb2eb08b4deba3c00e3e4adc2da81be81f1b4167
Boris Zbarsky <bzbarsky@mit.edu>
2016-07-27 11:05:36 -0400

    Bug 1289179.  IDBRequest::SetResultCallback should expose its script owner to active JS before entering its compartment.  r=baku

--- dom/indexedDB/IDBRequest.cpp
+++ dom/indexedDB/IDBRequest.cpp
@@ -367,6 +367,7 @@ IDBRequest::SetResultCallback(ResultCallback* aCallback)
     // If we have a script owner we want the SafeJSContext and then to enter the
     // script owner's compartment.
     autoJS.Init();
+    JS::ExposeObjectToActiveJS(GetScriptOwner());
     ac.emplace(autoJS.cx(), GetScriptOwner());
   } else {
     // Otherwise our owner is a window and we use that to initialize.
