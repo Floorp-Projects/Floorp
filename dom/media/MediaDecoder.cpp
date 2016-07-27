3953db3fce05fc7aeb3e196b4c1358d65761ef1e
JW Wang <jwwang@mozilla.com>
2016-07-27 10:31:14 +0800

    Bug 1289976. Part 1 - Remove the IsShutdown() check from MediaDecoder::ConstructMediaTracks(). r=kaku 1. ConstructMediaTracks() is called from ChangeState() when |mPlayState == PLAY_STATE_PLAYING|. 2. ConstructMediaTracks() is called from MetadataLoaded() which asserts |!IsShutdown()|.
    MozReview-Commit-ID: 6OaPYcCOCii

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -1864,8 +1864,9 @@ void
 MediaDecoder::ConstructMediaTracks()
 {
   MOZ_ASSERT(NS_IsMainThread());
+  MOZ_ASSERT(!IsShutdown());
 
-  if (IsShutdown() || mMediaTracksConstructed || !mInfo) {
+  if (mMediaTracksConstructed || !mInfo) {
     return;
   }
 
