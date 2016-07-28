15482ac4c525d3f29b08176afeb176b2b6a3f033
Jon Coppeard <jcoppeard@mozilla.com>
2016-07-28 11:49:06 +0100

    Bug 1288780 - Don't destroy GCPtrs while there may be store buffer entries pointing into them r=terrence

--- js/src/jsweakmap.h
+++ js/src/jsweakmap.h
@@ -412,4 +412,12 @@ class ObjectWeakMap
 
 } /* namespace js */
 
+namespace JS {
+
+template <>
+struct DeletePolicy<js::ObjectValueMap> : public js::GCManagedDeletePolicy<js::ObjectValueMap>
+{};
+
+} /* namespace JS */
+
 #endif /* jsweakmap_h */
