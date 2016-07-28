bb72afa14ef68704efde3dfcd32b51aded4e8e30
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-07-28 10:06:35 +0200

    Backed out changeset 8da1ed0adf15 (bug 1288885) for failure and crashes in test_ext_background_window_properties.js on Android in xpcshell and sets tests. r=backout

--- browser/components/extensions/test/xpcshell/head.js
+++ browser/components/extensions/test/xpcshell/head.js
@@ -2,24 +2,12 @@
 
 const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 
-/* exported createHttpServer */
-
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "AppConstants",
-                                  "resource://gre/modules/AppConstants.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "Extension",
                                   "resource://gre/modules/Extension.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "ExtensionData",
                                   "resource://gre/modules/Extension.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "ExtensionManagement",
-                                  "resource://gre/modules/ExtensionManagement.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "ExtensionTestUtils",
-                                  "resource://testing-common/ExtensionXPCShellUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "FileUtils",
-                                  "resource://gre/modules/FileUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "HttpServer",
-                                  "resource://testing-common/httpd.js");
 XPCOMUtils.defineLazyModuleGetter(this, "NetUtil",
                                   "resource://gre/modules/NetUtil.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "Schemas",
@@ -27,29 +15,36 @@ XPCOMUtils.defineLazyModuleGetter(this, "Schemas",
 XPCOMUtils.defineLazyModuleGetter(this, "Services",
                                   "resource://gre/modules/Services.jsm");
 
-ExtensionTestUtils.init(this);
+/* exported normalizeManifest */
 
+let BASE_MANIFEST = {
+  "applications": {"gecko": {"id": "test@web.ext"}},
 
-/**
- * Creates a new HttpServer for testing, and begins listening on the
- * specified port. Automatically shuts down the server when the test
- * unit ends.
- *
- * @param {integer} [port]
- *        The port to listen on. If omitted, listen on a random
- *        port. The latter is the preferred behavior.
- *
- * @returns {HttpServer}
- */
-function createHttpServer(port = -1) {
-  let server = new HttpServer();
-  server.start(port);
+  "manifest_version": 2,
 
-  do_register_cleanup(() => {
-    return new Promise(resolve => {
-      server.stop(resolve);
-    });
-  });
+  "name": "name",
+  "version": "0",
+};
 
-  return server;
+function* normalizeManifest(manifest, baseManifest = BASE_MANIFEST) {
+  const {Management} = Cu.import("resource://gre/modules/Extension.jsm", {});
+  yield Management.lazyInit();
+
+  let errors = [];
+  let context = {
+    url: null,
+
+    logError: error => {
+      errors.push(error);
+    },
+
+    preprocessors: {},
+  };
+
+  manifest = Object.assign({}, baseManifest, manifest);
+
+  let normalized = Schemas.normalize(manifest, "manifest.WebExtensionManifest", context);
+  normalized.errors = errors;
+
+  return normalized;
 }
