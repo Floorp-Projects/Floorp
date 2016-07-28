64e8b494aeb75774118f771eb32e5d2ef643d705
Jarda Snajdr <jsnajdr@gmail.com>
2016-07-28 02:49:00 -0400

    Bug 1287172 - Console netlogging: sanitize POST body data when it's a longString. r=bgrins

--- devtools/client/webconsole/net/components/post-tab.js
+++ devtools/client/webconsole/net/components/post-tab.js
@@ -180,7 +180,7 @@ var PostTab = React.createClass({
         key: "raw-longstring",
         name: Locale.$STR("netRequest.rawData"),
         content: DOM.div({className: "netInfoResponseContent"},
-          text.initial,
+          sanitize(text.initial),
           SizeLimit({
             actions: this.props.actions,
             data: file.request.postData,
