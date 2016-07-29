e1928319c4d514d0245f3266b7fa653ba0f06409
Wes Kocher <wkocher@mozilla.com>
2016-07-29 14:48:10 -0700

    Backed out changeset 7638ea3c616c (bug 1275289) for being the likely cause of browser_documentnavigation.js bustage CLOSED TREE

--- toolkit/components/jsdownloads/src/DownloadLegacy.js
+++ toolkit/components/jsdownloads/src/DownloadLegacy.js
@@ -90,20 +90,10 @@ DownloadLegacyTransfer.prototype = {
     if ((aStateFlags & Ci.nsIWebProgressListener.STATE_START) &&
         (aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK)) {
 
-      let blockedByParentalControls = false;
-      // If it is a failed download, aRequest.responseStatus doesn't exist.
-      // (missing file on the server, network failure to download)
-      try {
-        // If the request's response has been blocked by Windows Parental Controls
-        // with an HTTP 450 error code, we must cancel the request synchronously.
-        blockedByParentalControls = aRequest instanceof Ci.nsIHttpChannel &&
+      // If the request's response has been blocked by Windows Parental Controls
+      // with an HTTP 450 error code, we must cancel the request synchronously.
+      let blockedByParentalControls = aRequest instanceof Ci.nsIHttpChannel &&
                                       aRequest.responseStatus == 450;
-      } catch (e) {
-        if (e.result == Cr.NS_ERROR_NOT_AVAILABLE) {
-          aRequest.cancel(Cr.NS_BINDING_ABORTED);
-        }
-      }
-
       if (blockedByParentalControls) {
         aRequest.cancel(Cr.NS_BINDING_ABORTED);
       }
