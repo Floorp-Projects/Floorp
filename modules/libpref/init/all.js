3345af4b54ebc057fdc476dd5669f834e44ed8f2
Giorgio Maone <g.maone@informaction.com>
2016-07-29 23:38:43 +0200

    Bug 1201979 - Support requestBody in onBeforeRequest. r=kmag
    MozReview-Commit-ID: LAHKN5uZO0
    
    --HG--
    extra : rebase_source : 4abe13bea2d21eece768a600f0d76aea42617747

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5451,6 +5451,9 @@ pref("toolkit.pageThumbs.minHeight", 0);
 
 pref("webextensions.tests", false);
 
+// 16MB default non-parseable upload limit for requestBody.raw.bytes
+pref("webextensions.webRequest.requestBodyMaxRawBytes", 16777216);
+
 // Allow customization of the fallback directory for file uploads
 pref("dom.input.fallbackUploadDir", "");
 
