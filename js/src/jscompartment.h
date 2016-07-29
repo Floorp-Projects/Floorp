ce3be76a23df7c7b9c468a52b3e1cf2f84838b62
Terrence Cole <terrence@mozilla.com>
2016-07-29 09:55:56 -0700

    Bug 1290513 - Remove unused |existing| parameter from the Value variant of wrap; r=jonco

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -563,12 +563,11 @@ struct JSCompartment
 
     MOZ_MUST_USE bool init(JSContext* maybecx);
 
-    MOZ_MUST_USE inline bool wrap(JSContext* cx, JS::MutableHandleValue vp,
-                                            JS::HandleObject existing = nullptr);
+    MOZ_MUST_USE inline bool wrap(JSContext* cx, JS::MutableHandleValue vp);
 
     MOZ_MUST_USE bool wrap(JSContext* cx, js::MutableHandleString strp);
     MOZ_MUST_USE bool wrap(JSContext* cx, JS::MutableHandleObject obj,
-                                     JS::HandleObject existingArg = nullptr);
+                           JS::HandleObject existingArg = nullptr);
     MOZ_MUST_USE bool wrap(JSContext* cx, JS::MutableHandle<js::PropertyDescriptor> desc);
     MOZ_MUST_USE bool wrap(JSContext* cx, JS::MutableHandle<JS::GCVector<JS::Value>> vec);
 
