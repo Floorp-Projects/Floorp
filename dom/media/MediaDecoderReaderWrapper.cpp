fe4d39e47c5de4ae23dab9ef7959fa80327152d0
Kaku Kuo <tkuo@mozilla.com>
2016-07-29 14:51:18 +0800

    Bug 1274626 part 5 - provide APIs to switch to blank decoders dynamically; r=jya
    MozReview-Commit-ID: PyoIsrEf1r

--- dom/media/MediaDecoderReaderWrapper.cpp
+++ dom/media/MediaDecoderReaderWrapper.cpp
@@ -410,4 +410,14 @@ MediaDecoderReaderWrapper::OnMetadataRead(MetadataHolder* aMetadata)
   }
 }
 
+void
+MediaDecoderReaderWrapper::SetVideoBlankDecode(bool aIsBlankDecode)
+{
+  MOZ_ASSERT(mOwnerThread->IsCurrentThreadIn());
+  nsCOMPtr<nsIRunnable> r =
+    NewRunnableMethod<bool>(mReader, &MediaDecoderReader::SetVideoBlankDecode,
+                            aIsBlankDecode);
+  mReader->OwnerThread()->Dispatch(r.forget());
+}
+
 } // namespace mozilla
