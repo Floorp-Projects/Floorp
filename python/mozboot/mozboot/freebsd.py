cb9989726e8ae00be99bbfbe54e483759794faed
Jan Beich <jbeich@FreeBSD.org>
2016-07-30 06:09:54 +0000

    Bug 1290647 - FreeBSD bootstrap: GNU tar is required for ./mach package. r=glandium, a=sylvestre
    MozReview-Commit-ID: 36Yn6n5bOeQ

--- python/mozboot/mozboot/freebsd.py
+++ python/mozboot/mozboot/freebsd.py
@@ -14,6 +14,7 @@ class FreeBSDBootstrapper(BaseBootstrapper):
         self.packages = [
             'autoconf213',
             'gmake',
+            'gtar',
             'mercurial',
             'pkgconf',
             'zip',
