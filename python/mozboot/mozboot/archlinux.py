df99bba152a994b804625d18be75d6ba6911c6ea
Jan Beich <jbeich@FreeBSD.org>
2016-07-30 06:09:54 +0000

    Bug 1290647 - Add gconf2 to more platforms with bootstrap after bug 1167201. r=glandium, a=sylvestre
    MozReview-Commit-ID: 36Yn6n5bOeQ
    
    --HG--
    extra : source : fc5553a33bd1fc7386420e4b3a97dd11503e263b
    extra : intermediate-source : 69b597846cf684fb7ca2841e71602dfdb88fe77b

--- python/mozboot/mozboot/archlinux.py
+++ python/mozboot/mozboot/archlinux.py
@@ -29,6 +29,7 @@ class ArchlinuxBootstrapper(BaseBootstrapper):
         'alsa-lib',
         'dbus-glib',
         'desktop-file-utils',
+        'gconf',
         'gtk2',
         'gtk3',
         'hicolor-icon-theme',
