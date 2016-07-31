249cd65c7163b8a825be59d070e56f8c139fff1f
Nicholas Nethercote <nnethercote@mozilla.com>
2016-08-01 09:13:29 +1000

    Bug 1156255 - Make fallible a potentially large allocation in nsSimpleURI::SetPath. r=dholbert.
    --HG--
    extra : rebase_source : 9dfe68e8179e293c096a93276ff1100a052527b2

--- netwerk/base/nsSimpleURI.cpp
+++ netwerk/base/nsSimpleURI.cpp
@@ -359,7 +359,9 @@ nsSimpleURI::SetPath(const nsACString &path)
     if (hashPos < 0) {
         mIsRefValid = false;
         mRef.Truncate(); // invariant: mRef should be empty when it's not valid
-        mPath = path;
+        if (!mPath.Assign(path, fallible)) {
+            return NS_ERROR_OUT_OF_MEMORY;
+        }
         return NS_OK;
     }
 
