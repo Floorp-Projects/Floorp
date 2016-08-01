92517ddb46864f516dbd748d074bc2d1475911fa
dimi <dlee@mozilla.com>
2016-08-01 09:08:30 +0800

    Bug 1254766 - Stop caching Safe Browsing completions to disk. r=gcp
    MozReview-Commit-ID: IUmgq7T8Ugw

--- toolkit/components/url-classifier/tests/mochitest/classifierHelper.js
+++ toolkit/components/url-classifier/tests/mochitest/classifierHelper.js
@@ -115,6 +115,17 @@ classifierHelper.resetDB = function() {
   });
 };
 
+classifierHelper.reloadDatabase = function() {
+  return new Promise(function(resolve, reject) {
+    gScript.addMessageListener("reloadSuccess", function handler() {
+      gScript.removeMessageListener('reloadSuccess', handler);
+      resolve();
+    });
+
+    gScript.sendAsyncMessage("doReload");
+  });
+}
+
 classifierHelper._update = function(testUpdate, onsuccess, onerror) {
   // Queue the task if there is still an on-going update
   classifierHelper._updates.push({"data": testUpdate,
