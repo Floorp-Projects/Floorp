b7b02bc2d0de72cb625e80c362a56e39422ec62e
Andrew Swan <aswan@mozilla.com>
2016-08-01 16:30:18 -0700

    Bug 1213990 Clear storage when webextension is uninstalled r=kmag
    MozReview-Commit-ID: BeMOxOCSeru

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4630,6 +4630,10 @@ pref("extensions.alwaysUnpack", false);
 pref("extensions.minCompatiblePlatformVersion", "2.0");
 pref("extensions.webExtensionsMinPlatformVersion", "42.0a1");
 
+// Other webextensions prefs
+pref("extensions.webextensions.keepStorageOnUninstall", false);
+pref("extensions.webextensions.keepUuidOnUninstall", false);
+
 pref("network.buffer.cache.count", 24);
 pref("network.buffer.cache.size",  32768);
 
