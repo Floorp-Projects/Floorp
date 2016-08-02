b7af421b38a14977ae68760f300f3a6062e69e80
Ethan Lin <ethlin@mozilla.com>
2016-08-02 23:00:00 +0200

    Bug 1289975 - Reuse canvas buffer when setting the same dimension. r=nical

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -1685,7 +1685,11 @@ CanvasRenderingContext2D::GetHeight() const
 NS_IMETHODIMP
 CanvasRenderingContext2D::SetDimensions(int32_t aWidth, int32_t aHeight)
 {
-  ClearTarget();
+  bool retainBuffer = false;
+  if (aWidth == mWidth && aHeight == mHeight) {
+    retainBuffer = true;
+  }
+  ClearTarget(retainBuffer);
 
   // Zero sized surfaces can cause problems.
   mZero = false;
@@ -1704,9 +1708,22 @@ CanvasRenderingContext2D::SetDimensions(int32_t aWidth, int32_t aHeight)
 }
 
 void
-CanvasRenderingContext2D::ClearTarget()
+CanvasRenderingContext2D::ClearTarget(bool aRetainBuffer)
 {
+  RefPtr<PersistentBufferProvider> provider = mBufferProvider;
+  if (aRetainBuffer && provider) {
+    // We should reset the buffer data before reusing the buffer.
+    if (mTarget) {
+      mTarget->SetTransform(Matrix());
+    }
+    ClearRect(0, 0, mWidth, mHeight);
+  }
+
   Reset();
+
+  if (aRetainBuffer) {
+    mBufferProvider = provider;
+  }
 
   mResetLayer = true;
 
