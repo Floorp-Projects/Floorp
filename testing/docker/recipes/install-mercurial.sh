869ad0eda7328860974d67750f278b32d8f559ad
Gregory Szorc <gps@mozilla.com>
2016-08-02 12:10:25 -0700

    Bug 1291365 - Support installing Mercurial on Ubuntu 12.04; r=dustin
    I produced Mercurial .deb packages for Ubuntu 12.04 and uploaded them to
    tooltool. I have updated the Mercurial installation script to detect
    Ubuntu 12.04 and install the appropriate Mercurial packages.
    
    MozReview-Commit-ID: GsNFOhhms7b
    
    --HG--
    extra : rebase_source : d64016382035ae99a3be9abc5bf974ab710c058b

--- testing/docker/recipes/install-mercurial.sh
+++ testing/docker/recipes/install-mercurial.sh
@@ -20,6 +20,15 @@ if [ -f /etc/lsb-release ]; then
         HG_COMMON_DIGEST=b476e2612e7495a1c7c5adfd84511aa7479e26cc9070289513ec705fbfc4c61806ce2dbcceca0e63f2e80669be416f3467a3cebb522dcb8a6aeb62cdd3df82f2
         HG_COMMON_SIZE=1818422
         HG_COMMON_FILENAME=mercurial-common_3.8.4_all.deb
+    elif [ "${DISTRIB_ID}" = "Ubuntu" -a "${DISTRIB_RELEASE}" = "12.04" ]; then
+        HG_DEB=1
+        HG_DIGEST=96366b6baac26017a2ef9cd0bbbba4f18756e02921b0ff3541f677484d53d2041f01202b743ea8cdb96db58e88317da18befefc9711a085b054f9abc3dad1679
+        HG_SIZE=54992
+        HG_FILENAME=mercurial_3.8.4_amd64.deb
+
+        HG_COMMON_SIZE=2946616
+        HG_COMMON_DIGEST=96c6bc305ae85f16885d0b6ac6800361d680811346a01338a56a2174a3c0ae5d86bbd827d93fe8c59d71acd2e1cc7d6e79e39a179836d5695cbfa3b370982ee5
+        HG_COMMON_FILENAME=mercurial-common_3.8.4_all.deb
     fi
 fi
 
