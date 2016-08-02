e427482452bd720e1aa2653b271a73e59f5e12e7
Boris Zbarsky <bzbarsky@mit.edu>
2016-08-02 11:05:38 -0700

    Bug 824592 part 2.  Give nsStyledElement an IID.  r=peterv

--- dom/base/nsStyledElement.h
+++ dom/base/nsStyledElement.h
@@ -23,6 +23,11 @@ class Declaration;
 } // namespace css
 } // namespace mozilla
 
+// IID for nsStyledElement interface
+#define NS_STYLED_ELEMENT_IID \
+{ 0xacbd9ea6, 0x15aa, 0x4f37, \
+ { 0x8c, 0xe0, 0x35, 0x1e, 0xd7, 0x21, 0xca, 0xe9 } }
+
 typedef mozilla::dom::Element nsStyledElementBase;
 
 class nsStyledElement : public nsStyledElementBase
@@ -35,6 +40,11 @@ protected:
   {}
 
 public:
+  // We don't want to implement AddRef/Release because that would add an extra
+  // function call for those on pretty much all elements.  But we do need QI, so
+  // we can QI to nsStyledElement.
+  NS_IMETHOD QueryInterface(REFNSIID aIID, void** aInstancePtr) override;
+
   // Element interface methods
   virtual mozilla::css::Declaration* GetInlineStyleDeclaration() override;
   virtual nsresult SetInlineStyleDeclaration(mozilla::css::Declaration* aDeclaration,
@@ -42,6 +52,8 @@ public:
                                              bool aNotify) override;
 
   nsICSSDeclaration* Style();
+
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_STYLED_ELEMENT_IID)
 
 protected:
 
@@ -70,4 +82,5 @@ protected:
   nsresult  ReparseStyleAttribute(bool aForceInDataDoc);
 };
 
+NS_DEFINE_STATIC_IID_ACCESSOR(nsStyledElement, NS_STYLED_ELEMENT_IID)
 #endif // __NS_STYLEDELEMENT_H_
