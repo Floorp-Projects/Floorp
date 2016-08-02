7f9305180e71f24b2be539f18b3ff6523ad61db0
Rob Wu <rob@robwu.nl>
2016-08-01 19:01:12 -0700

    Bug 1295323 - s/CalledProcessError/subprocess.CalledProcessError/ r=dminor
    MozReview-Commit-ID: IxtsQCQbAE2
    
    --HG--
    extra : rebase_source : ce51dc92b539948b54ac1c9c3e83c4a664917efd

--- testing/xpcshell/remotexpcshelltests.py
+++ testing/xpcshell/remotexpcshelltests.py
@@ -456,7 +456,7 @@ class XPCShellRemote(xpcshell.XPCShellTests, object):
                         if szip:
                             try:
                                 out = subprocess.check_output([szip, '-d', localFile], stderr=subprocess.STDOUT)
-                            except CalledProcessError:
+                            except subprocess.CalledProcessError:
                                 print >> sys.stderr, "Error calling %s on %s.." % (szip, localFile)
                                 if out:
                                     print >> sys.stderr, out
@@ -476,7 +476,7 @@ class XPCShellRemote(xpcshell.XPCShellTests, object):
                 if szip:
                     try:
                         out = subprocess.check_output([szip, '-d', localFile], stderr=subprocess.STDOUT)
-                    except CalledProcessError:
+                    except subprocess.CalledProcessError:
                         print >> sys.stderr, "Error calling %s on %s.." % (szip, localFile)
                         if out:
                             print >> sys.stderr, out
@@ -495,7 +495,7 @@ class XPCShellRemote(xpcshell.XPCShellTests, object):
                         if szip:
                             try:
                                 out = subprocess.check_output([szip, '-d', localFile], stderr=subprocess.STDOUT)
-                            except CalledProcessError:
+                            except subprocess.CalledProcessError:
                                 print >> sys.stderr, "Error calling %s on %s.." % (szip, localFile)
                                 if out:
                                     print >> sys.stderr, out
