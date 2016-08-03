7c83cd27cef321e280323f7e7031173f9136a183
Drew Willcoxon <adw@mozilla.com>
2016-08-02 18:00:26 -0700

    Bug 1180944 - Implement one-off searches from Awesomebar. r=mak,florian
    MozReview-Commit-ID: A9YXB32L7MN

--- browser/base/content/test/urlbar/head.js
+++ browser/base/content/test/urlbar/head.js
@@ -327,10 +327,18 @@ function promiseSearchComplete(win = window) {
   });
 }
 
-function promiseAutocompleteResultPopup(inputText, win = window) {
+function promiseAutocompleteResultPopup(inputText,
+                                        win = window,
+                                        fireInputEvent = false) {
   waitForFocus(() => {
     win.gURLBar.focus();
     win.gURLBar.value = inputText;
+    if (fireInputEvent) {
+      // This is necessary to get the urlbar to set gBrowser.userTypedValue.
+      let event = document.createEvent("Events");
+      event.initEvent("input", true, true);
+      win.gURLBar.dispatchEvent(event);
+    }
     win.gURLBar.controller.startSearch(inputText);
   }, win);
 
