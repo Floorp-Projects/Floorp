4b68f17715bb2f5c22ac77557cf5f8528b9e7f8b
Nick Alexander <nalexander@mozilla.com>
2016-08-03 16:02:00 -0700

    Bug 1291370 - Add Fennec-and-GeckoView Task Cluster job building using --with-gradle. r=dustin,gbrown
    We run only a small subset of the test suite to save resources.  Over
    time, we'll run more of the platform test suite.
    
    --HG--
    extra : rebase_source : 264fbe55be5b56857278ae8d731ef51b1b41cf3c
    extra : histedit_source : a3550c7e2ea8a41cd6285e511b7268a9e7af11cd%2Cec6024dadcb7c071170aa452064776666283bb27

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle.py
@@ -0,0 +1,8 @@
+config = {
+    'base_name': 'Android armv7 API 15+ %(branch)s Gradle',
+    'stage_platform': 'android-api-15-gradle',
+    'build_type': 'api-15-gradle',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-15-gradle/nightly',
+    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android/releng.manifest',
+    'multi_locale_config_platform': 'android',
+}
