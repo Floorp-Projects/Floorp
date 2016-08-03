68cdc941e8b3a9a308f23ea5f7e7b09042a1f5f4
Patrick McManus <mcmanus@ducksong.com>
2016-08-03 14:36:50 -0400

    Bug 1291814 - enforce h2 increasing push ids. r=hurley

--- netwerk/protocol/http/Http2Session.h
+++ netwerk/protocol/http/Http2Session.h
@@ -317,6 +317,7 @@ private:
 
   uint32_t          mSendingChunkSize;        /* the transmission chunk size */
   uint32_t          mNextStreamID;            /* 24 bits */
+  uint32_t          mLastPushedID;
   uint32_t          mConcurrentHighWater;     /* max parallelism on session */
   uint32_t          mPushAllowance;           /* rwin for unmatched pushes */
 
