66155ae633a8f1fe797caef879c6ee14809fc797
Masayuki Nakano <masayuki@d-toybox.com>
2016-08-03 17:23:56 +0900

    Bug 1259665 part.2 Rename WidgetPointerEvent::height to mHeight r=smaug
    MozReview-Commit-ID: Ji4X53dByiH

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -7201,7 +7201,7 @@ DispatchPointerFromMouseOrTouch(PresShell* aShell,
       event.mRefPoint = touch->mRefPoint;
       event.mModifiers = touchEvent->mModifiers;
       event.mWidth = touch->RadiusX();
-      event.height = touch->RadiusY();
+      event.mHeight = touch->RadiusY();
       event.tiltX = touch->tiltX;
       event.tiltY = touch->tiltY;
       event.mTime = touchEvent->mTime;
