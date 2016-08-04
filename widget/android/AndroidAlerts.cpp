680bdb769d707a1b46d1496fc4da508f3b44d8b8
Jim Chen <nchen@mozilla.com>
2016-08-04 09:17:54 -0400

    Bug 1288821 - Add AndroidAlerts as a nsAlertsService backend; r=snorp
    Add a proper backend for nsAlertsService on Android, so we don't need
    any special-case code for Android in nsAlertsService itself.

---
+++ widget/android/AndroidAlerts.cpp
@@ -0,0 +1,102 @@
+/* -*- Mode: c++; tab-width: 40; indent-tabs-mode: nil; c-basic-offset: 4; -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "AndroidAlerts.h"
+#include "GeneratedJNIWrappers.h"
+#include "nsAlertsUtils.h"
+#include "nsAppShell.h"
+
+namespace mozilla {
+namespace widget {
+
+NS_IMPL_ISUPPORTS(AndroidAlerts, nsIAlertsService)
+
+NS_IMETHODIMP
+AndroidAlerts::ShowAlertNotification(const nsAString & aImageUrl,
+                                     const nsAString & aAlertTitle,
+                                     const nsAString & aAlertText,
+                                     bool aAlertTextClickable,
+                                     const nsAString & aAlertCookie,
+                                     nsIObserver * aAlertListener,
+                                     const nsAString & aAlertName,
+                                     const nsAString & aBidi,
+                                     const nsAString & aLang,
+                                     const nsAString & aData,
+                                     nsIPrincipal * aPrincipal,
+                                     bool aInPrivateBrowsing)
+{
+    MOZ_ASSERT_UNREACHABLE("Should be implemented by nsAlertsService.");
+    return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+NS_IMETHODIMP
+AndroidAlerts::ShowAlert(nsIAlertNotification* aAlert,
+                         nsIObserver* aAlertListener)
+{
+    return ShowPersistentNotification(EmptyString(), aAlert, aAlertListener);
+}
+
+NS_IMETHODIMP
+AndroidAlerts::ShowPersistentNotification(const nsAString& aPersistentData,
+                                          nsIAlertNotification* aAlert,
+                                          nsIObserver* aAlertListener)
+{
+    // nsAlertsService disables our alerts backend if we ever return failure
+    // here. To keep the backend enabled, we always return NS_OK even if we
+    // encounter an error here.
+    nsresult rv;
+
+    nsAutoString imageUrl;
+    rv = aAlert->GetImageURL(imageUrl);
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsAutoString title;
+    rv = aAlert->GetTitle(title);
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsAutoString text;
+    rv = aAlert->GetText(text);
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsAutoString cookie;
+    rv = aAlert->GetCookie(cookie);
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsAutoString name;
+    rv = aAlert->GetName(name);
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsCOMPtr<nsIPrincipal> principal;
+    rv = aAlert->GetPrincipal(getter_AddRefs(principal));
+    NS_ENSURE_SUCCESS(rv, NS_OK);
+
+    nsAutoString host;
+    nsAlertsUtils::GetSourceHostPort(principal, host);
+
+    if (!aPersistentData.IsEmpty()) {
+        java::GeckoAppShell::ShowPersistentAlertNotificationWrapper(
+                aPersistentData, imageUrl, title, text, cookie, name, host);
+    } else {
+        if (aAlertListener) {
+            // This will remove any observers already registered for this id
+            nsAppShell::PostEvent(AndroidGeckoEvent::MakeAddObserver(name, aAlertListener));
+        }
+
+        java::GeckoAppShell::ShowAlertNotificationWrapper(
+               imageUrl, title, text, cookie, name, host);
+    }
+    return NS_OK;
+}
+
+NS_IMETHODIMP
+AndroidAlerts::CloseAlert(const nsAString& aAlertName,
+                          nsIPrincipal* aPrincipal)
+{
+    java::GeckoAppShell::CloseNotification(aAlertName);
+    return NS_OK;
+}
+
+} // namespace widget
+} // namespace mozilla
