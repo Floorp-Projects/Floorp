b32d7f22ee50f88aaab41710f4c047b866f6d5d0
Jim Chen <nchen@mozilla.com>
2016-08-04 09:16:05 -0400

    Bug 1289208 - Add native methods to AndroidGamepadManager; r=snorp
    Add native method calls to AndroidGamepadManager to replace the gamepad
    events in GeckoEvent. Implement those calls in AndroidGamepad.cpp. The
    jni/Refs.h change is necessary to fix a compile error when using
    jni::BooleanArray.

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/AndroidGamepadManager.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/AndroidGamepadManager.java
@@ -11,6 +11,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import org.mozilla.gecko.AppConstants.Versions;
+import org.mozilla.gecko.annotation.WrapForJNI;
 import org.mozilla.gecko.util.GamepadUtils;
 import org.mozilla.gecko.util.ThreadUtils;
 
@@ -128,6 +129,13 @@ public class AndroidGamepadManager {
         }
     }
 
+    @WrapForJNI
+    private static native void onGamepadChange(int id, boolean added);
+    @WrapForJNI
+    private static native void onButtonChange(int id, int button, boolean pressed, float value);
+    @WrapForJNI
+    private static native void onAxisChange(int id, boolean[] valid, float[] values);
+
     private static boolean sStarted;
     private static final SparseArray<Gamepad> sGamepads = new SparseArray<>();
     private static final SparseArray<List<KeyEvent>> sPendingGamepads = new SparseArray<>();
@@ -219,7 +227,7 @@ public class AndroidGamepadManager {
                                     int which) {
         if (pressed != gamepad.dpad[which]) {
             gamepad.dpad[which] = pressed;
-            GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadButtonEvent(gamepad.id, FIRST_DPAD_BUTTON + which, pressed, Math.abs(value)));
+            onButtonChange(gamepad.id, FIRST_DPAD_BUTTON + which, pressed, Math.abs(value));
         }
     }
 
@@ -251,7 +259,7 @@ public class AndroidGamepadManager {
         }
         if (anyValidAxes) {
             // Send an axismove event.
-            GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadAxisEvent(gamepad.id, valid, axes));
+            onAxisChange(gamepad.id, valid, axes);
         }
 
         // Map triggers to buttons.
@@ -263,7 +271,7 @@ public class AndroidGamepadManager {
                 if (value != gamepad.triggers[i]) {
                     gamepad.triggers[i] = value;
                     boolean pressed = value > TRIGGER_PRESSED_THRESHOLD;
-                    GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadButtonEvent(gamepad.id, trigger.button, pressed, value));
+                    onButtonChange(gamepad.id, trigger.button, pressed, value);
                 }
             }
         }
@@ -322,7 +330,7 @@ public class AndroidGamepadManager {
 
         Gamepad gamepad = sGamepads.get(deviceId);
         boolean pressed = ev.getAction() == KeyEvent.ACTION_DOWN;
-        GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadButtonEvent(gamepad.id, key, pressed, pressed ? 1.0f : 0.0f));
+        onButtonChange(gamepad.id, key, pressed, pressed ? 1.0f : 0.0f);
         return true;
     }
 
@@ -344,18 +352,13 @@ public class AndroidGamepadManager {
     }
 
     private static void addGamepad(InputDevice device) {
-        //TODO: when we're using a newer SDK version, use these.
-        //if (Build.VERSION.SDK_INT >= 12) {
-        //int vid = device.getVendorId();
-        //int pid = device.getProductId();
-        //}
         sPendingGamepads.put(device.getId(), new ArrayList<KeyEvent>());
-        GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadAddRemoveEvent(device.getId(), true));
+        onGamepadChange(device.getId(), true);
     }
 
     private static void removeGamepad(int deviceId) {
         Gamepad gamepad = sGamepads.get(deviceId);
-        GeckoAppShell.sendEventToGecko(GeckoEvent.createGamepadAddRemoveEvent(gamepad.id, false));
+        onGamepadChange(gamepad.id, false);
         sGamepads.remove(deviceId);
     }
 
