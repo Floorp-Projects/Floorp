07cea0a040311a4980ef362ced190b9556fc498b
Benjamin Bouvier <benj@benj.me>
2016-08-04 13:22:23 +0200

    Bug 1292126: Re-enable wasm test cases disabled; r=lth
    MozReview-Commit-ID: 6QnI9M9QSJs

--- js/src/jit-test/tests/wasm/basic-float.js
+++ js/src/jit-test/tests/wasm/basic-float.js
@@ -30,8 +30,8 @@ testUnary('f32', 'abs', -40, 40);
 testUnary('f32', 'neg', 40, -40);
 testUnary('f32', 'floor', 40.9, 40);
 testUnary('f32', 'ceil', 40.1, 41);
-//testUnary('f32', 'nearest', -41.5, -42); // TODO: NYI
-//testUnary('f32', 'trunc', -41.5, -41); // TODO: NYI
+testUnary('f32', 'nearest', -41.5, -42);
+testUnary('f32', 'trunc', -41.5, -41);
 testUnary('f32', 'sqrt', 40, 6.324555397033691);
 
 testBinary('f32', 'add', 40, 2, 42);
@@ -40,7 +40,7 @@ testBinary('f32', 'mul', 40, 2, 80);
 testBinary('f32', 'div', 40, 3, 13.333333015441895);
 testBinary('f32', 'min', 40, 2, 2);
 testBinary('f32', 'max', 40, 2, 40);
-//testBinary('f32', 'copysign', 40, -2, -40); // TODO: NYI
+testBinary('f32', 'copysign', 40, -2, -40);
 
 testComparison('f32', 'eq', 40, 40, 1);
 testComparison('f32', 'ne', 40, 40, 0);
@@ -53,8 +53,8 @@ testUnary('f64', 'abs', -40, 40);
 testUnary('f64', 'neg', 40, -40);
 testUnary('f64', 'floor', 40.9, 40);
 testUnary('f64', 'ceil', 40.1, 41);
-//testUnary('f64', 'nearest', -41.5, -42); // TODO: NYI
-//testUnary('f64', 'trunc', -41.5, -41); // TODO: NYI
+testUnary('f64', 'nearest', -41.5, -42);
+testUnary('f64', 'trunc', -41.5, -41);
 testUnary('f64', 'sqrt', 40, 6.324555320336759);
 
 testBinary('f64', 'add', 40, 2, 42);
@@ -63,7 +63,7 @@ testBinary('f64', 'mul', 40, 2, 80);
 testBinary('f64', 'div', 40, 3, 13.333333333333334);
 testBinary('f64', 'min', 40, 2, 2);
 testBinary('f64', 'max', 40, 2, 40);
-//testBinary('f64', 'copysign', 40, -2, -40); // TODO: NYI
+testBinary('f64', 'copysign', 40, -2, -40);
 
 testComparison('f64', 'eq', 40, 40, 1);
 testComparison('f64', 'ne', 40, 40, 0);
