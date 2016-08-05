07440559c6e9563f81da65b09784f04fac5edd4d
Markus Stange <mstange@themasta.com>
2016-08-04 23:51:58 -0400

    Bug 1012752 - Snap scrolled area to layer pixels. r=tnikkel
    We want the maximum scroll position to be aligned with layer pixels. That way
    we don't have to re-rasterize the scrolled contents once scrolling hits the
    edge of the scrollable area.
    
    Here's how we determine the maximum scroll position: We get the scroll port
    rect, snapped to layer pixels. Then we get the scrolled rect and also snap
    that to layer pixels. The maximum scroll position is set to the difference
    between right/bottom edges of these rectangles.
    Now the scrollable area is computed by adding this maximum scroll position
    to the unsnapped scroll port size.
    The underlying idea here is: Pretend we have overflow:visible so that the
    scrolled contents start at (0, 0) relative to the scroll port and spill over
    the scroll port edges. When these contents are rendered, their rendering is
    snapped to layer pixels. We want those exact pixels to be accessible by
    scrolling.
    
    This way of computing the snapped scrollable area ensures that, if you scroll
    to the maximum scroll position, the right/bottom edges of the rendered
    scrolled contents line up exactly with the right/bottom edges of the scroll
    port. The scrolled contents are neither cut off nor are they moved too far.
    (This is something that no other browser engine gets completely right, see the
    testcase in bug 1012752.)
    
    There are also a few disadvantages to this solution. We snap to layer pixels,
    and the size of a layer pixel can depend on the zoom level, the document
    resolution, the current screen's scale factor, and CSS transforms. The snap
    origin is the position of the reference frame. So a change to any of these
    things can influence the scrollable area and the maximum scroll position.
    This patch does not make us adjust the current scroll position in the event
    that the maximum scroll position changes such that the current scroll position
    would be out of range, unless there's a reflow of the scrolled contents. This
    means that we can sometimes render a slightly inconsistent state where the
    current scroll position exceeds the maximum scroll position. We can fix this
    once it turns out to be a problem; I doubt that it will be a problem because
    none of the other browsers seems to prevent this problem either.
    
    The size of the scrollable area is exposed through the DOM properties
    scrollWidth and scrollHeight. At the moment, these are integer properties, so
    their value is rounded to the nearest CSS pixel. Before this patch, the
    returned value would always be within 0.5 CSS pixels of the value that layout
    computed for the content's scrollable overflow based on the CSS styles of the
    contents.
    Now that scrollWidth and scrollHeight also depend on pixel snapping, their
    values can deviate by up to one layer pixel from what the page might expect
    based on the styles of the contents. This change requires a few changes to
    existing tests.
    The fact that scrollWidth and scrollHeight can change based on the position of
    the scrollable element and the zoom level / resolution may surprise some web
    pages. However, this also seems to happen in Edge. Edge seems to always round
    scrollWidth and scrollHeight upwards, possibly to their equivalent of layout
    device pixels.
    
    MozReview-Commit-ID: 3LFV7Lio4tG

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -3701,7 +3701,7 @@ ContainerState::ComputeOpaqueRect(nsDisplayItem* aItem,
     }
     nsIFrame* scrollFrame = do_QueryFrame(sf);
     displayport += scrollFrame->GetOffsetToCrossDoc(mContainerReferenceFrame);
-    if (opaque.Contains(displayport)) {
+    if (opaquePixels.Contains(ScaleRegionToNearestPixels(displayport))) {
       *aOpaqueForAnimatedGeometryRootParent = true;
     }
   }
