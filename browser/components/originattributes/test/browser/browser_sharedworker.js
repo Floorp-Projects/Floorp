b6d2d47447e2137fc21f0fb5805fc16959f0f625
Tim Huang <tihuang@mozilla.com>
2016-08-07 01:20:59 +0800

    Bug 1264593 - Add a test case for the isolation on the shared worker by first party domain. r=arthuredelstein,baku
    Adapted from Tor Browser patch 15564

---
+++ browser/components/originattributes/test/browser/browser_sharedworker.js
@@ -0,0 +1,26 @@
+/**
+ * Bug 1264593 - A test case for the shared worker by first party isolation.
+ */
+
+const TEST_DOMAIN = "http://example.net/";
+const TEST_PATH = TEST_DOMAIN + "browser/browser/components/originattributes/test/browser/";
+const TEST_PAGE = TEST_PATH + "file_sharedworker.html";
+
+function* getResultFromSharedworker(aBrowser) {
+  let response = yield ContentTask.spawn(aBrowser, null, function* () {
+    let worker = new content.SharedWorker("file_sharedworker.js", "isolationSharedWorkerTest");
+
+    let result = yield new Promise(resolve => {
+      worker.port.onmessage = function (e) {
+        content.document.getElementById("display").innerHTML = e.data;
+        resolve(e.data);
+      };
+    });
+
+    return result;
+  });
+
+  return response;
+}
+
+IsolationTestTools.runTests(TEST_PAGE, getResultFromSharedworker);
