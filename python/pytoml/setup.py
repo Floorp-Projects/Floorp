ef91835e00c02ab11e2c3f1442af4cc3456a88d3
Nathan Froyd <froydnj@gmail.com>
2016-08-06 00:49:26 -0400

    Bug 1231764 - part 4 - add pytoml to the virtualenv; r=chmanchester
    We need to parse Cargo.toml files from moz.build to determine crate
    package names.

---
+++ python/pytoml/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pytoml',
+    version='0.1.10',
+
+    description='A parser for TOML-0.4.0',
+    author='Martin Vejnár',
+    author_email='avakar@ratatanek.cz',
+    url='https://github.com/avakar/pytoml',
+    license='MIT',
+
+    packages=['pytoml'],
+    )
