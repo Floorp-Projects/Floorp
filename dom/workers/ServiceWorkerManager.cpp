c4f467ea5296240c2473ea8dbee5380318ecf1fa
Phil Ringnalda <philringnalda@gmail.com>
2016-08-06 18:58:23 -0700

    Back out 6 changesets (bug 1290116) for a 70% chance of WinXP debug timing out in test_subscription_change.html
    Backed out changeset aa715b19902b (bug 1290116)
    Backed out changeset c9922c014310 (bug 1290116)
    Backed out changeset 44b50089c079 (bug 1290116)
    Backed out changeset ca998a8cbe48 (bug 1290116)
    Backed out changeset 74ceee4dc2f4 (bug 1290116)
    Backed out changeset 90006dffbe5b (bug 1290116)

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -696,12 +696,6 @@ public:
     }
 
     for (uint32_t i = 0; i < data->mOrderedScopes.Length(); ++i) {
-      RefPtr<ServiceWorkerRegistrationInfo> info =
-        data->mInfos.GetWeak(data->mOrderedScopes[i]);
-      if (info->mPendingUninstall) {
-        continue;
-      }
-
       NS_ConvertUTF8toUTF16 scope(data->mOrderedScopes[i]);
 
       nsCOMPtr<nsIURI> scopeURI;
@@ -1310,16 +1304,9 @@ ServiceWorkerManager::WorkerIsIdle(ServiceWorkerInfo* aWorker)
     return;
   }
 
-  if (reg->GetActive() != aWorker) {
-    return;
+  if (reg->GetActive() == aWorker) {
+    reg->TryToActivateAsync();
   }
-
-  if (!reg->IsControllingDocuments() && reg->mPendingUninstall) {
-    RemoveRegistration(reg);
-    return;
-  }
-
-  reg->TryToActivateAsync();
 }
 
 already_AddRefed<ServiceWorkerJobQueue>
@@ -2014,20 +2001,17 @@ void
 ServiceWorkerManager::StopControllingADocument(ServiceWorkerRegistrationInfo* aRegistration)
 {
   aRegistration->StopControllingADocument();
-  if (aRegistration->IsControllingDocuments() || !aRegistration->IsIdle()) {
-    return;
+  if (!aRegistration->IsControllingDocuments()) {
+    if (aRegistration->mPendingUninstall) {
+      RemoveRegistration(aRegistration);
+    } else {
+      // We use to aggressively terminate the worker at this point, but it
+      // caused problems.  There are more uses for a service worker than actively
+      // controlled documents.  We need to let the worker naturally terminate
+      // in case its handling push events, message events, etc.
+      aRegistration->TryToActivateAsync();
+    }
   }
-
-  if (aRegistration->mPendingUninstall) {
-    RemoveRegistration(aRegistration);
-    return;
-  }
-
-  // We use to aggressively terminate the worker at this point, but it
-  // caused problems.  There are more uses for a service worker than actively
-  // controlled documents.  We need to let the worker naturally terminate
-  // in case its handling push events, message events, etc.
-  aRegistration->TryToActivateAsync();
 }
 
 NS_IMETHODIMP
