757c5c2302ad2a7579c43d32797840fa8b6a7cb4
JW Wang <jwwang@mozilla.com>
2016-08-08 11:04:55 +0800

    Bug 1290318 - listen to "cacheservice:empty-cache" to clear media cache. r=mayhemer
    MozReview-Commit-ID: 7t4FPCxOUYl

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -589,6 +589,7 @@ static const char* sObserverTopics[] = {
   "profiler-subprocess",
 #endif
   "gmp-changed",
+  "cacheservice:empty-cache",
 };
 
 // PreallocateAppProcess is called by the PreallocatedProcessManager.
@@ -2906,6 +2907,9 @@ ContentParent::Observe(nsISupports* aSubject,
 #endif
   else if (!strcmp(aTopic, "gmp-changed")) {
     Unused << SendNotifyGMPsChanged();
+  }
+  else if (!strcmp(aTopic, "cacheservice:empty-cache")) {
+    Unused << SendNotifyEmptyHTTPCache();
   }
   return NS_OK;
 }
