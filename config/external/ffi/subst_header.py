481ad0d18abf96d000fdb1694ec45e27ebfb726a
Chris Manchester <cmanchester@mozilla.com>
2016-08-08 16:58:54 -0700

    Bug 1262155 - Use moz.build instead of libffi's build system. r=ted,glandium
    MozReview-Commit-ID: 3wobNk0HDU5

---
+++ config/external/ffi/subst_header.py
@@ -0,0 +1,25 @@
+# -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
+# vim: set filetype=python:
+# This Souce Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distibuted with this
+# file, You can obtain one at http://mozilla.og/MPL/2.0/.
+
+import sys
+import buildconfig
+from mozbuild.preprocessor import Preprocessor
+
+def main(output, input_file):
+    pp = Preprocessor()
+    pp.context.update({
+        'FFI_EXEC_TRAMPOLINE_TABLE': '0',
+        'HAVE_LONG_DOUBLE': '0',
+        'TARGET': buildconfig.substs['FFI_TARGET'],
+        'VERSION': '',
+    })
+    pp.do_filter('substitution')
+    pp.setMarker(None)
+    pp.out = output
+    pp.do_include(input_file)
+
+if __name__ == '__main__':
+    main(*sys.agv[1:])
