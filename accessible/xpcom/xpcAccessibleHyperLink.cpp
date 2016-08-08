413edf2a4ed56272199348b78d32913f53a321b2
Aaron Klotz <aklotz@mozilla.com>
2016-08-08 15:40:54 -0600

    Bug 1268544: Disable proxy code paths in accessible/xpcom pending resolution of bug 1288839; r=tbsaunde
    MozReview-Commit-ID: 3AAnFir6rnx

--- accessible/xpcom/xpcAccessibleHyperLink.cpp
+++ accessible/xpcom/xpcAccessibleHyperLink.cpp
@@ -21,13 +21,17 @@ xpcAccessibleHyperLink::GetStartIndex(int32_t* aStartIndex)
 
   if (Intl().IsAccessible()) {
     *aStartIndex = Intl().AsAccessible()->StartOffset();
-  } else { 
+  } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     bool isIndexValid = false;
     uint32_t startOffset = Intl().AsProxy()->StartOffset(&isIndexValid);
     if (!isIndexValid)
       return NS_ERROR_FAILURE;
 
     *aStartIndex = startOffset;
+#endif
   }
 
   return NS_OK;
@@ -44,13 +48,17 @@ xpcAccessibleHyperLink::GetEndIndex(int32_t* aEndIndex)
 
   if (Intl().IsAccessible()) {
     *aEndIndex = Intl().AsAccessible()->EndOffset();
-  } else { 
+  } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     bool isIndexValid = false;
     uint32_t endOffset = Intl().AsProxy()->EndOffset(&isIndexValid);
     if (!isIndexValid)
       return NS_ERROR_FAILURE;
 
     *aEndIndex = endOffset;
+#endif
   }
 
   return NS_OK;
@@ -67,13 +75,17 @@ xpcAccessibleHyperLink::GetAnchorCount(int32_t* aAnchorCount)
 
   if (Intl().IsAccessible()) {
     *aAnchorCount = Intl().AsAccessible()->AnchorCount();
-  } else { 
+  } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     bool isCountValid = false;
     uint32_t anchorCount = Intl().AsProxy()->AnchorCount(&isCountValid);
     if (!isCountValid)
       return NS_ERROR_FAILURE;
 
     *aAnchorCount = anchorCount;
+#endif
   }
 
   return NS_OK;
@@ -96,6 +108,9 @@ xpcAccessibleHyperLink::GetURI(int32_t aIndex, nsIURI** aURI)
 
     RefPtr<nsIURI>(Intl().AsAccessible()->AnchorURIAt(aIndex)).forget(aURI);
   } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     nsCString spec;
     bool isURIValid = false;
     Intl().AsProxy()->AnchorURIAt(aIndex, spec, &isURIValid);
@@ -107,6 +122,7 @@ xpcAccessibleHyperLink::GetURI(int32_t aIndex, nsIURI** aURI)
     NS_ENSURE_SUCCESS(rv, rv);
 
     uri.forget(aURI);
+#endif
   }
 
   return NS_OK;
@@ -131,7 +147,11 @@ xpcAccessibleHyperLink::GetAnchor(int32_t aIndex, nsIAccessible** aAccessible)
 
     NS_IF_ADDREF(*aAccessible = ToXPC(Intl().AsAccessible()->AnchorAt(aIndex)));
   } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     NS_IF_ADDREF(*aAccessible = ToXPC(Intl().AsProxy()->AnchorAt(aIndex)));
+#endif
   }
 
   return NS_OK;
@@ -149,7 +169,11 @@ xpcAccessibleHyperLink::GetValid(bool* aValid)
   if (Intl().IsAccessible()) {
     *aValid = Intl().AsAccessible()->IsLinkValid();
   } else {
+#if defined(XP_WIN)
+    return NS_ERROR_NOT_IMPLEMENTED;
+#else
     *aValid = Intl().AsProxy()->IsLinkValid();
+#endif
   }
 
   return NS_OK;
