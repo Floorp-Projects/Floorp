3faa701d51a4c7a707d7cb36a3011807edd9719a
Bob Owen <bobowencode@gmail.com>
2016-08-08 17:11:38 +0100

    Bug 1288194 - Part 1: Implement PathBuilderRecording::Arc correctly. r=bas, a=ritu
    MozReview-Commit-ID: Iz7Io8IxHvR

--- gfx/2d/PathRecording.h
+++ gfx/2d/PathRecording.h
@@ -10,6 +10,8 @@
 #include <vector>
 #include <ostream>
 
+#include "PathHelpers.h"
+
 namespace mozilla {
 namespace gfx {
 
@@ -63,7 +65,11 @@ public:
   virtual void Close();
 
   /* Add an arc to the current figure */
-  virtual void Arc(const Point &, float, float, float, bool) { }
+  virtual void Arc(const Point &aOrigin, float aRadius, float aStartAngle,
+                   float aEndAngle, bool aAntiClockwise) {
+    ArcToBezier(this, aOrigin, Size(aRadius, aRadius), aStartAngle, aEndAngle,
+                aAntiClockwise);
+  }
 
   /* Point the current subpath is at - or where the next subpath will start
    * if there is no active subpath.
