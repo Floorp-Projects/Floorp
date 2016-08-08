6913fc005da55521c17850cc261ed9b75a32e8e0
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-08-08 13:56:38 +1000

    Bug 1128069 - [MSE] P6. Call NotifyDataArrived from MediaDecoder. r=gerald, a=lizzard
    Calling NotifyDataArrived from each sourcebuffer will cause multiple unnecessary NotifyDataArrived to the MediaFormatReader when it could only be done once. Additionally, it ensures that the media duration is updated prior to the reader actioning on the notification.
    
    Extra: mEnded is only ever accessed on the main thread, there's no need to make it atomic.
    
    MozReview-Commit-ID: IKq7IRBbWic
    
    --HG--
    extra : source : b406ebdeebd2d448b15f01ea8d1aa1040f41c0bd

--- dom/media/mediasource/MediaSourceDecoder.cpp
+++ dom/media/mediasource/MediaSourceDecoder.cpp
@@ -196,7 +196,12 @@ MediaSourceDecoder::Ended(bool aEnded)
 {
   MOZ_ASSERT(NS_IsMainThread());
   static_cast<MediaSourceResource*>(GetResource())->SetEnded(aEnded);
-  mEnded = true;
+  if (aEnded) {
+    // We want the MediaSourceReader to refresh its buffered range as it may
+    // have been modified (end lined up).
+    NotifyDataArrived();
+  }
+  mEnded = aEnded;
 }
 
 void
@@ -313,6 +318,8 @@ MediaSourceDecoder::CanPlayThrough()
 TimeInterval
 MediaSourceDecoder::ClampIntervalToEnd(const TimeInterval& aInterval)
 {
+  MOZ_ASSERT(NS_IsMainThread());
+
   if (!mEnded) {
     return aInterval;
   }
