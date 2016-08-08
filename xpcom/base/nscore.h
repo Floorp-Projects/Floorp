33221112506f2cc4d6ba55b559cd71ad0e0fd9f1
Nicholas Nethercote <nnethercote@mozilla.com>
2016-08-08 10:40:42 +1000

    Bug 1293117 (part 1) - Add comment explaining how NS_IMETHOD, NS_IMETHODIMP and NS_METHOD should be used. r=froydnj.

--- xpcom/base/nscore.h
+++ xpcom/base/nscore.h
@@ -171,6 +171,11 @@
 
 /**
  * Generic API modifiers which return the standard XPCOM nsresult type
+ *
+ * - NS_IMETHOD: use for in-class declarations and definitions.
+ * - NS_IMETHODIMP: use for out-of-class definitions.
+ * - NS_METHOD: usually used in conjunction with NS_CALLBACK.
+ * - NS_CALLBACK: used in some legacy situations. Best avoided.
  */
 #define NS_IMETHOD          NS_IMETHOD_(nsresult)
 #define NS_IMETHODIMP       NS_IMETHODIMP_(nsresult)
