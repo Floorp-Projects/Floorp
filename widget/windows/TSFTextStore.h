1f206a7c93769506a638ab7da3a19f40124d0b4d
Masayuki Nakano <masayuki@d-toybox.com>
2016-08-09 16:53:00 +0900

    Bug 1273510 part.1 TSFTextStore should use native caret hack only when ATOK 2011 - 2016 is active r=m_kato
    ATOK 2011 - 2016 check focused window class name if it's a Mozilla's window.  Then, they refer native caret position for deciding its popup window.  However, future ATOK release will stop referring native caret position on Mozilla's windows.  Therefore, we can stop creating native caret for new ATOK.
    
    MozReview-Commit-ID: HPh3DVqTkvc
    
    --HG--
    extra : rebase_source : 00b75b75df2ba1bace328cfb75172c78c6bea29d

--- widget/windows/TSFTextStore.h
+++ widget/windows/TSFTextStore.h
@@ -985,7 +985,7 @@ protected:
   static DWORD sClientId;
 
   // Enables/Disables hack for specific TIP.
-  static bool sCreateNativeCaretForATOK;
+  static bool sCreateNativeCaretForLegacyATOK;
   static bool sDoNotReturnNoLayoutErrorToMSSimplifiedTIP;
   static bool sDoNotReturnNoLayoutErrorToMSTraditionalTIP;
   static bool sDoNotReturnNoLayoutErrorToFreeChangJie;
