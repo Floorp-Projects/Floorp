986a1df1a6211941b4bef8097b18b4e9905ce294
Anthony Jones <ajones@mozilla.com>
2016-08-09 13:34:58 +1200

    Bug 1287397 - Fix wave chunk size overflow. r=cpearce, a=lizzard
    MozReview-Commit-ID: Dg1OT13urxe

--- dom/media/wave/WaveDemuxer.cpp
+++ dom/media/wave/WaveDemuxer.cpp
@@ -112,8 +112,6 @@ WAVTrackDemuxer::Init()
     uint32_t aChunkName = mHeaderParser.GiveHeader().ChunkName();
     uint32_t aChunkSize = mHeaderParser.GiveHeader().ChunkSize();
 
-    aChunkSize += aChunkSize % 2;
-
     if (aChunkName == FRMT_CODE) {
       if (!FmtChunkParserInit()) {
         return false;
@@ -134,7 +132,8 @@ WAVTrackDemuxer::Init()
       }
       break;
     } else {
-      mOffset += aChunkSize; // Skip other irrelevant chunks.
+      // Wave files are 2-byte aligned so we need to round up
+      mOffset += (aChunkSize + 1) & ~1; // Skip other irrelevant chunks.
     }
     mHeaderParser.Reset();
   }
