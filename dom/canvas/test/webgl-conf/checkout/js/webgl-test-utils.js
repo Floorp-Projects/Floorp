9a9a50c44988f086e3e2cb1af5e80a073bce36b6
Jeff Gilbert <jgilbert@mozilla.com>
2016-08-10 15:16:18 -0700

    Bug 1293845 - Update tests to handle new webidl. a=test-only
    MozReview-Commit-ID: 7hcluLAw7gx

--- dom/canvas/test/webgl-conf/checkout/js/webgl-test-utils.js
+++ dom/canvas/test/webgl-conf/checkout/js/webgl-test-utils.js
@@ -1239,10 +1239,9 @@ var checkFloatBuffer = function(gl, target, expected, opt_msg, opt_errorRange) {
   if (opt_errorRange === undefined)
     opt_errorRange = 0.001;
 
-  var outData = new ArrayBuffer(Float32Array.BYTES_PER_ELEMENT * expected.length);
-  gl.getBufferSubData(target, 0, outData);
+  var floatArray = new Float32Array(expected.length);
+  gl.getBufferSubData(target, 0, floatArray);
 
-  var floatArray = new Float32Array(outData);
   for (var i = 0; i < expected.length; i++) {
     if (Math.abs(floatArray[i] - expected[i]) > opt_errorRange) {
       testFailed(opt_msg);
