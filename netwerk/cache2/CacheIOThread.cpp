8649bc3c7256a1d9553155434c7b32614de927b9
Honza Bambas <honzab.moz@firemni.cz>
2016-08-10 10:50:00 -0400

    Bug 1288204 - Use CancelSynchronousIo Win32 function to cancel blocking I/O on the HTTP cache thread. r=michal

--- netwerk/cache2/CacheIOThread.cpp
+++ netwerk/cache2/CacheIOThread.cpp
@@ -11,6 +11,10 @@
 #include "nsThreadUtils.h"
 #include "mozilla/IOInterposer.h"
 
+#ifdef XP_WIN
+#include <Windows.h>
+#endif
+
 namespace mozilla {
 namespace net {
 
@@ -63,6 +67,152 @@ void CacheIOTelemetry::Report(uint32_t aLevel, CacheIOTelemetry::size_type aLeng
 
 } // anon
 
+namespace detail {
+
+/**
+ * Helper class encapsulating platform-specific code to cancel
+ * any pending IO operation taking too long.  Solely used during
+ * shutdown to prevent any IO shutdown hangs.
+ * Mainly designed for using Win32 CancelSynchronousIo function.
+ */
+class BlockingIOWatcher
+{
+#ifdef XP_WIN
+  typedef BOOL(WINAPI* TCancelSynchronousIo)(HANDLE hThread);
+  TCancelSynchronousIo mCancelSynchronousIo;
+  // The native handle to the thread
+  HANDLE mThread;
+  // Event signaling back to the main thread, see NotifyOperationDone.
+  HANDLE mEvent;
+#endif
+
+public:
+  // Created and destroyed on the main thread only
+  BlockingIOWatcher();
+  ~BlockingIOWatcher();
+
+  // Called on the IO thread to grab the platform specific
+  // reference to it.
+  void InitThread();
+  // If there is a blocking operation being handled on the IO
+  // thread, this is called on the main thread during shutdown.
+  // Waits for notification from the IO thread for up to two seconds.
+  // If that times out, it attempts to cancel the IO operation.
+  void WatchAndCancel(Monitor& aMonitor);
+  // Called by the IO thread after each operation has been
+  // finished (after each Run() call).  This wakes the main
+  // thread up and makes WatchAndCancel() early exit and become
+  // a no-op.
+  void NotifyOperationDone();
+};
+
+#ifdef XP_WIN
+
+BlockingIOWatcher::BlockingIOWatcher()
+  : mCancelSynchronousIo(NULL)
+  , mThread(NULL)
+  , mEvent(NULL)
+{
+  HMODULE kernel32_dll = GetModuleHandle("kernel32.dll");
+  if (!kernel32_dll) {
+    return;
+  }
+
+  FARPROC ptr = GetProcAddress(kernel32_dll, "CancelSynchronousIo");
+  if (!ptr) {
+    return;
+  }
+
+  mCancelSynchronousIo = reinterpret_cast<TCancelSynchronousIo>(ptr);
+
+  mEvent = ::CreateEvent(NULL, TRUE, FALSE, NULL);
+}
+
+BlockingIOWatcher::~BlockingIOWatcher()
+{
+  if (mEvent) {
+    CloseHandle(mEvent);
+  }
+  if (mThread) {
+    CloseHandle(mThread);
+  }
+}
+
+void BlockingIOWatcher::InitThread()
+{
+  // GetCurrentThread() only returns a pseudo handle, hence DuplicateHandle
+  BOOL result = ::DuplicateHandle(
+    GetCurrentProcess(),
+    GetCurrentThread(),
+    GetCurrentProcess(),
+    &mThread,
+    0,
+    FALSE,
+    DUPLICATE_SAME_ACCESS);
+}
+
+void BlockingIOWatcher::WatchAndCancel(Monitor& aMonitor)
+{
+  if (!mEvent) {
+    return;
+  }
+
+  // Reset before we enter the monitor to raise the chance we catch
+  // the currently pending IO op completion.
+  ::ResetEvent(mEvent);
+
+  HANDLE thread;
+  {
+    MonitorAutoLock lock(aMonitor);
+    thread = mThread;
+
+    if (!thread) {
+      return;
+    }
+  }
+
+  LOG(("Blocking IO operation pending on IO thread, waiting..."));
+
+  // It seems wise to use the I/O lag time as a maximum time to wait
+  // for an operation to finish.  When that times out and cancelation
+  // succeeds, there will be no other IO operation permitted.  By default
+  // this is two seconds.
+  uint32_t maxLag = std::min<uint32_t>(5, CacheObserver::MaxShutdownIOLag()) * 1000;
+
+  DWORD result = ::WaitForSingleObject(mEvent, maxLag);
+  if (result == WAIT_TIMEOUT) {
+    LOG(("CacheIOThread: Attempting to cancel a long blocking IO operation"));
+    BOOL result = mCancelSynchronousIo(thread);
+    if (result) {
+      LOG(("  cancelation signal succeeded"));
+    } else {
+      DWORD error = GetLastError();
+      LOG(("  cancelation signal failed with GetLastError=%u", error));
+    }
+  }
+}
+
+void BlockingIOWatcher::NotifyOperationDone()
+{
+  if (mEvent) {
+    ::SetEvent(mEvent);
+  }
+}
+
+#else // WIN
+
+// Stub code only (we don't implement IO cancelation for this platform)
+
+BlockingIOWatcher::BlockingIOWatcher() { }
+BlockingIOWatcher::~BlockingIOWatcher() { }
+void BlockingIOWatcher::InitThread() { }
+void BlockingIOWatcher::WatchAndCancel(Monitor&) { }
+void BlockingIOWatcher::NotifyOperationDone() { }
+
+#endif
+
+} // detail
+
 CacheIOThread* CacheIOThread::sSelf = nullptr;
 
 NS_IMPL_ISUPPORTS(CacheIOThread, nsIThreadObserver)
@@ -76,6 +226,7 @@ CacheIOThread::CacheIOThread()
 , mHasXPCOMEvents(false)
 , mRerunCurrentEvent(false)
 , mShutdown(false)
+, mIOCancelableEvents(0)
 #ifdef DEBUG
 , mInsideLoop(true)
 #endif
@@ -100,11 +251,19 @@ CacheIOThread::~CacheIOThread()
 
 nsresult CacheIOThread::Init()
 {
+  {
+    MonitorAutoLock lock(mMonitor);
+    // Yeah, there is not a thread yet, but we want to make sure
+    // the sequencing is correct.
+    mBlockingIOWatcher = MakeUnique<detail::BlockingIOWatcher>();
+  }
+
   mThread = PR_CreateThread(PR_USER_THREAD, ThreadFunc, this,
                             PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD,
                             PR_JOINABLE_THREAD, 128 * 1024);
-  if (!mThread)
+  if (!mThread) {
     return NS_ERROR_FAILURE;
+  }
 
   return NS_OK;
 }
@@ -195,8 +354,12 @@ bool CacheIOThread::YieldInternal()
   return true;
 }
 
-nsresult CacheIOThread::Shutdown()
+void CacheIOThread::Shutdown()
 {
+  if (!mThread) {
+    return;
+  }
+
   {
     MonitorAutoLock lock(mMonitor);
     mShutdown = true;
@@ -205,8 +368,24 @@ nsresult CacheIOThread::Shutdown()
 
   PR_JoinThread(mThread);
   mThread = nullptr;
+}
 
-  return NS_OK;
+void CacheIOThread::CancelBlockingIO()
+{
+  // This is an attempt to cancel any blocking I/O operation taking
+  // too long time.
+  if (!mBlockingIOWatcher) {
+    return;
+  }
+
+  if (!mIOCancelableEvents) {
+    LOG(("CacheIOThread::CancelBlockingIO, no blocking operation to cancel"));
+    return;
+  }
+
+  // OK, when we are here, we are processing an IO on the thread that
+  // can be cancelled.
+  mBlockingIOWatcher->WatchAndCancel(mMonitor);
 }
 
 already_AddRefed<nsIEventTarget> CacheIOThread::Target()
@@ -244,6 +423,9 @@ void CacheIOThread::ThreadFunc()
   {
     MonitorAutoLock lock(mMonitor);
 
+    MOZ_ASSERT(mBlockingIOWatcher);
+    mBlockingIOWatcher->InitThread();
+
     // This creates nsThread for this PRThread
     nsCOMPtr<nsIThread> xpcomThread = NS_GetCurrentThread();
 
@@ -274,6 +456,9 @@ loopStart:
         do {
           nsIThread *thread = mXPCOMThread;
           rv = thread->ProcessNextEvent(false, &processedEvent);
+
+          MOZ_ASSERT(mBlockingIOWatcher);
+          mBlockingIOWatcher->NotifyOperationDone();
         } while (NS_SUCCEEDED(rv) && processedEvent);
       }
 
@@ -290,16 +475,15 @@ loopStart:
         goto loopStart;
       }
 
-      if (EventsPending())
+      if (EventsPending()) {
         continue;
+      }
 
-      if (mShutdown)
+      if (mShutdown) {
         break;
+      }
 
       lock.Wait(PR_INTERVAL_NO_TIMEOUT);
-
-      if (EventsPending())
-        continue;
 
     } while (true);
 
@@ -348,6 +532,9 @@ void CacheIOThread::LoopOneLevel(uint32_t aLevel)
       mRerunCurrentEvent = false;
 
       events[index]->Run();
+
+      MOZ_ASSERT(mBlockingIOWatcher);
+      mBlockingIOWatcher->NotifyOperationDone();
 
       if (mRerunCurrentEvent) {
         // The event handler yields to higher priority events and wants to rerun.
@@ -410,6 +597,28 @@ size_t CacheIOThread::SizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf) co
 size_t CacheIOThread::SizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf) const
 {
   return mallocSizeOf(this) + SizeOfExcludingThis(mallocSizeOf);
+}
+
+CacheIOThread::Cancelable::Cancelable(bool aCancelable)
+  : mCancelable(aCancelable)
+{
+  // This will only ever be used on the I/O thread,
+  // which is expected to be alive longer than this class.
+  MOZ_ASSERT(CacheIOThread::sSelf);
+  MOZ_ASSERT(CacheIOThread::sSelf->IsCurrentThread());
+
+  if (mCancelable) {
+    ++CacheIOThread::sSelf->mIOCancelableEvents;
+  }
+}
+
+CacheIOThread::Cancelable::~Cancelable()
+{
+  MOZ_ASSERT(CacheIOThread::sSelf);
+
+  if (mCancelable) {
+    --CacheIOThread::sSelf->mIOCancelableEvents;
+  }
 }
 
 } // namespace net
