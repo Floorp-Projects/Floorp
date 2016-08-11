4d8756b9707a7c2e753f67b4b333895eac37502b
Alexandre Poirot <poirot.alex@gmail.com>
2016-08-11 04:14:36 -0700

    Bug 1292081 - Prevent reloading web pages twice when devtools are opened. r=jryans, a=ritu
    --HG--
    extra : source : 39fdb9c2017e96940075926234809cef0f8b5f1b

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -583,7 +583,12 @@ Toolbox.prototype = {
       ["forceReload2", true]
     ].forEach(([id, force]) => {
       let key = toolboxStrings("toolbox." + id + ".key");
-      shortcuts.on(key, this.reloadTarget.bind(this, force));
+      shortcuts.on(key, (name, event) => {
+        this.reloadTarget(force);
+
+        // Prevent Firefox shortcuts from reloading the page
+        event.preventDefault();
+      });
     });
   },
 
