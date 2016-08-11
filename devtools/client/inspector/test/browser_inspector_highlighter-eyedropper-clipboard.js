28939c8aa180b427f8f398d351aae7d3bdd9f7b8
Alexandre Poirot <poirot.alex@gmail.com>
2016-08-11 04:14:36 -0700

    Bug 1289433 - Fix Eye dropper focus to support key shortcuts on it when opening from the inspector. r=jdescottes

--- devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-clipboard.js
+++ devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-clipboard.js
@@ -14,7 +14,7 @@ add_task(function* () {
                .then(getHighlighterHelperFor(HIGHLIGHTER_TYPE));
   helper.prefix = ID;
 
-  let {show, synthesizeKey, finalize} = helper;
+  let {show, finalize} = helper;
 
   info("Show the eyedropper with the copyOnSelect option");
   yield show("html", {copyOnSelect: true});
@@ -24,8 +24,7 @@ add_task(function* () {
 
   yield waitForClipboard(() => {
     info("Activate the eyedropper so the background color is copied");
-    let generateKey = synthesizeKey({key: "VK_RETURN", options: {}});
-    generateKey.next();
+    EventUtils.synthesizeKey("VK_RETURN", {});
   }, "#FF0000");
 
   ok(true, "The clipboard contains the right value");
