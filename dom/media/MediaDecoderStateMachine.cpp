6cc2957dd50a5e30fb198f14c9c7047e90dc44b2
JW Wang <jwwang@mozilla.com>
2016-08-11 16:07:45 +0800

    Bug 1294615. Part 1 - Assert mQueuedSeek.Exists() is false in InitiateSeek(). r=kaku InitiateSeek() is called from several places where we can prove mQueuedSeek.Exists() is false: 1. MaybeFinishDecodeFirstFrame(): mQueuedSeek is moved when calling InitiateSeek(). 2. Seek() rejects mQueuedSeek before calling InitiateSeek(). 3. StartDecoding(): mQueuedSeek is moved when calling InitiateSeek(). 4. VisibilityChanged(): it doesn't call InitiateSeek() when mQueuedSeek.Exists() is true.
    MozReview-Commit-ID: BriPw0VID5O

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1614,6 +1614,7 @@ MediaDecoderStateMachine::InitiateSeek(SeekJob aSeekJob)
            &MediaDecoderStateMachine::OnSeekTaskResolved,
            &MediaDecoderStateMachine::OnSeekTaskRejected));
 
+  MOZ_ASSERT(!mQueuedSeek.Exists());
   MOZ_ASSERT(!mCurrentSeek.Exists());
   mCurrentSeek = Move(aSeekJob);
   return mCurrentSeek.mPromise.Ensure(__func__);
