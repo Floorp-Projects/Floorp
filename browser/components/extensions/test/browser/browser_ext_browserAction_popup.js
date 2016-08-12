51f5bc3b4eb2ebbdcc2646e287a8abf02c853d2d
Kris Maglione <maglione.k@gmail.com>
2016-08-11 18:30:10 -0700

    Bug 1280128: [webext] Use transparent backgrounds and correct border radii for popup browsers. r=bwinton ui-r=maritz a=lizzard
    MozReview-Commit-ID: 9jmuxdSLmBM
    
    --HG--
    extra : source : 9999b6b8022bfe815d79a4f309986d2859285e16
    extra : intermediate-source : d7750d91b9179fab6fe27f912552101d2515c5b9
    extra : histedit_source : ca5df8f135ab14ad49d3b591138d579d3d8f3bb6%2C9c973c7798028a3b7499c98ad64bfe97ab8d9d42

--- browser/components/extensions/test/browser/browser_ext_browserAction_popup.js
+++ browser/components/extensions/test/browser/browser_ext_browserAction_popup.js
@@ -20,8 +20,8 @@ function* testInArea(area) {
       "popup-a.html": scriptPage("popup-a.js"),
       "popup-a.js": function() {
         window.onload = () => {
-          let background = window.getComputedStyle(document.body).backgroundColor;
-          browser.test.assertEq("rgb(252, 252, 252)", background);
+          let color = window.getComputedStyle(document.body).color;
+          browser.test.assertEq("rgb(34, 36, 38)", color);
           browser.runtime.sendMessage("from-popup-a");
         };
         browser.runtime.onMessage.addListener(msg => {
