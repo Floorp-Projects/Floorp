449f8d410e7d0f92b7dda469f5b1f915e82f1787
Tom Tromey <tom@tromey.com>
2016-08-12 12:52:40 -0600

    Bug 1290230 - move clipboard helper to new devtools platform module; r=gregtatum
    MozReview-Commit-ID: 53tOehW9kxo

---
+++ devtools/shared/platform/content/clipboard.js
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Helpers for clipboard handling.
+
+/* globals document */
+
+"use strict";
+
+function copyString(string) {
+  let doCopy = function (e) {
+    e.clipboardData.setData("text/plain", string);
+    e.preventDefault();
+  };
+
+  document.addEventListener("copy", doCopy);
+  document.execCommand("copy", false, null);
+  document.removeEventListener("copy", doCopy);
+}
+
+function getCurrentFlavors() {
+  // See bug 1295692.
+  return [];
+}
+
+function getData() {
+  // See bug 1295692.
+  return null;
+}
+
+exports.copyString = copyString;
+exports.getCurrentFlavors = getCurrentFlavors;
+exports.getData = getData;
