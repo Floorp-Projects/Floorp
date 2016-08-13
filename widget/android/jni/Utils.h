6d28618a736c57a018ed45b0b0eaf544dcbd7a17
Jim Chen <nchen@mozilla.com>
2016-08-12 23:15:52 -0400

    Bug 1292323 - Implement JNI thread checking and dispatching; r=snorp
    Implement checking the calling thread of a JNI call based on the
    calledFrom attribute set in WrapForJNI. Also implement automatic call
    dispatching based on the dispatchTo attribute set in WrapForJNI. This
    eliminates the use of UsesNativeCallProxy and UsesGeckoThreadProxy.

--- widget/android/jni/Utils.h
+++ widget/android/jni/Utils.h
@@ -3,13 +3,58 @@
 
 #include <jni.h>
 
+#include "mozilla/UniquePtr.h"
+
 #if defined(DEBUG) || !defined(RELEASE_BUILD)
+#define MOZ_CHECK_JNI
+#endif
+
+#ifdef MOZ_CHECK_JNI
+#include <pthread.h>
 #include "mozilla/Assertions.h"
+#include "APKOpen.h"
 #include "MainThreadUtils.h"
 #endif
 
 namespace mozilla {
 namespace jni {
+
+// How exception during a JNI call should be treated.
+enum class ExceptionMode
+{
+    // Abort on unhandled excepion (default).
+    ABORT,
+    // Ignore the exception and return to caller.
+    IGNORE,
+    // Catch any exception and return a nsresult.
+    NSRESULT,
+};
+
+// Thread that a particular JNI call is allowed on.
+enum class CallingThread
+{
+    // Can be called from any thread (default).
+    ANY,
+    // Can be called from the Gecko thread.
+    GECKO,
+    // Can be called from the Java UI thread.
+    UI,
+};
+
+// If and where a JNI call will be dispatched.
+enum class DispatchTarget
+{
+    // Call happens synchronously on the calling thread (default).
+    CURRENT,
+    // Call happens synchronously on the calling thread, but the call is
+    // wrapped in a function object and is passed thru UsesNativeCallProxy.
+    // Method must return void.
+    PROXY,
+    // Call is dispatched asynchronously on the Gecko thread. Method must
+    // return void.
+    GECKO,
+};
+
 
 extern JNIEnv* sGeckoThreadEnv;
 
@@ -20,13 +65,9 @@ inline bool IsAvailable()
 
 inline JNIEnv* GetGeckoThreadEnv()
 {
-#if defined(DEBUG) || !defined(RELEASE_BUILD)
-    if (!NS_IsMainThread()) {
-        MOZ_CRASH("Not on main thread");
-    }
-    if (!sGeckoThreadEnv) {
-        MOZ_CRASH("Don't have a JNIEnv");
-    }
+#ifdef MOZ_CHECK_JNI
+    MOZ_RELEASE_ASSERT(NS_IsMainThread(), "Must be on Gecko thread");
+    MOZ_RELEASE_ASSERT(sGeckoThreadEnv, "Must have a JNIEnv");
 #endif
     return sGeckoThreadEnv;
 }
@@ -34,6 +75,21 @@ inline JNIEnv* GetGeckoThreadEnv()
 void SetGeckoThreadEnv(JNIEnv* aEnv);
 
 JNIEnv* GetEnvForThread();
+
+#ifdef MOZ_CHECK_JNI
+#define MOZ_ASSERT_JNI_THREAD(thread) \
+    do { \
+        if ((thread) == mozilla::jni::CallingThread::GECKO) { \
+            MOZ_RELEASE_ASSERT(::NS_IsMainThread()); \
+        } else if ((thread) == mozilla::jni::CallingThread::UI) { \
+            const bool isOnUiThread = ::pthread_equal(::pthread_self(), \
+                                                      ::getJavaUiThread()); \
+            MOZ_RELEASE_ASSERT(isOnUiThread); \
+        } \
+    } while (0)
+#else
+#define MOZ_ASSERT_JNI_THREAD(thread) do {} while (0)
+#endif
 
 bool ThrowException(JNIEnv *aEnv, const char *aClass,
                     const char *aMessage);
@@ -62,11 +118,21 @@ bool HandleUncaughtException(JNIEnv* aEnv);
         } \
     } while (0)
 
+
 uintptr_t GetNativeHandle(JNIEnv* env, jobject instance);
 
 void SetNativeHandle(JNIEnv* env, jobject instance, uintptr_t handle);
 
 jclass GetClassGlobalRef(JNIEnv* aEnv, const char* aClassName);
+
+
+struct AbstractCall
+{
+    virtual ~AbstractCall() {}
+    virtual void operator()() = 0;
+};
+
+void DispatchToGeckoThread(UniquePtr<AbstractCall>&& aCall);
 
 } // jni
 } // mozilla
