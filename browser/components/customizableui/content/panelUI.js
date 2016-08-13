04dce2d36661458e74caff0e0553a7da6e92adef
Wes Kocher <wkocher@mozilla.com>
2016-08-12 19:11:21 -0700

    Backed out changeset 33fcf83eefe4 (bug 1171746) for browser_visibleFindSelection.js failures a=backout

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -335,9 +335,6 @@ const PanelUI = {
       tempPanel.setAttribute("id", "customizationui-widget-panel");
       tempPanel.setAttribute("class", "cui-widget-panel");
       tempPanel.setAttribute("viewId", aViewId);
-      if (aAnchor.getAttribute("tabspecific")) {
-        tempPanel.setAttribute("tabspecific", true);
-      }
       if (this._disableAnimations) {
         tempPanel.setAttribute("animate", "false");
       }
