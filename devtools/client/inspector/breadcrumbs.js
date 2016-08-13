5a7de135de25e731c8c43b52082949153818501a
Sami Jaktholm <sjakthol@outlook.com>
2016-08-13 10:14:01 +0300

    Bug 1284125 - Fix intermittent browser_inspector_delete-selected-node-02.js. r=jdescottes
    The intermittency is caused by a race condition. When delete is clicked in the
    context menu, markupview changes the selection and sends the removeNode request
    to the server. The selection change triggers the usual inspector update process
    in the client. At the same time, the server removes the node and queues the
    mutation triggered by the removal. And here lies the issue.
    
    If the inspector components finish updating BEFORE the removal mutation is
    received from the server, the test continues before the breadcrumbs learn
    about the deletion and the test fails. If the update is still pending when
    the mutation is received, the breadcrumbs have time to update before the
    test continues to make assertions about the breadcrumb contents.
    
    The fix here is to make the test to ensure that the breadcrumbs have seen the
    deletion before it continues. To enable that, the breadcrumbs need to tell
    the world that it has been updated even if the breadcrumbs were just trimmed
    and a non-element node was selected that does not trigger the full update
    process (early return in the code).
    
    As the inspector update process has been collecting cruft for years and tests
    make a lot of assumptions about the emitted events, it's not safe to trigger a
    new inspector update in this special case. Therefore, only the
    breadcrumbs-updated event is emitted in this special case and the test waits
    for that if the deleted node is still present in the breadcrumbs.
    
    MozReview-Commit-ID: AjC6k6SzLCu

--- devtools/client/inspector/breadcrumbs.js
+++ devtools/client/inspector/breadcrumbs.js
@@ -815,6 +815,7 @@ HTMLBreadcrumbs.prototype = {
     }
 
     // If this was an interesting deletion; then trim the breadcrumb trail
+    let trimmed = false;
     if (reason === "markupmutation") {
       for (let {type, removed} of mutations) {
         if (type !== "childList") {
@@ -825,6 +826,7 @@ HTMLBreadcrumbs.prototype = {
           let removedIndex = this.indexOf(node);
           if (removedIndex > -1) {
             this.cutAfter(removedIndex - 1);
+            trimmed = true;
           }
         }
       }
@@ -833,6 +835,10 @@ HTMLBreadcrumbs.prototype = {
     if (!this.selection.isElementNode()) {
       // no selection
       this.setCursor(-1);
+      if (trimmed) {
+        // Since something changed, notify the interested parties.
+        this.inspector.emit("breadcrumbs-updated", this.selection.nodeFront);
+      }
       return;
     }
 
