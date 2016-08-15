02e95dd4a4f724e379feed87e00e6294e9e53d8c
Aaron Klotz <aklotz@mozilla.com>
2016-08-15 15:35:46 -0600

    Bug 1268544: Followup: fix missing include in DocAccessibleChildBase; r=me
    MozReview-Commit-ID: CzeTjJa9gg6

--- accessible/ipc/DocAccessibleChildBase.cpp
+++ accessible/ipc/DocAccessibleChildBase.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/a11y/DocAccessibleChildBase.h"
+#include "mozilla/a11y/ProxyAccessibleBase.h"
 
 #include "Accessible-inl.h"
 
