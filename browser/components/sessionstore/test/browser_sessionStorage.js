63364d00f1879fbbef65c693a82ea7a4e5ee4a8a
Rob Wood <rwood@mozilla.com>
2016-08-15 15:05:24 -0400

    Bug 1259329 - Split test up to fix intermittent timeout. r=jmaher, a=test-only
    MozReview-Commit-ID: 5uDU6ScsDea
    
    --HG--
    extra : source : e5d9fd75cdd99f02a4f20773e7a74450bfa76959

--- browser/components/sessionstore/test/browser_sessionStorage.js
+++ browser/components/sessionstore/test/browser_sessionStorage.js
@@ -183,46 +183,6 @@ add_task(function respect_privacy_level() {
     "https sessionStorage data has been saved");
 });
 
-// Test that we record the size of messages.
-add_task(function* test_telemetry() {
-  Services.telemetry.canRecordExtended = true;
-  let histogram = Services.telemetry.getHistogramById("FX_SESSION_RESTORE_DOM_STORAGE_SIZE_ESTIMATE_CHARS");
-  let snap1 = histogram.snapshot();
-
-  let tab = gBrowser.addTab(URL);
-  let browser = tab.linkedBrowser;
-  yield promiseBrowserLoaded(browser);
-
-  // Flush to make sure chrome received all data.
-  yield TabStateFlusher.flush(browser);
-  let snap2 = histogram.snapshot();
-
-  Assert.ok(snap2.counts[5] > snap1.counts[5]);
-  yield promiseRemoveTab(tab);
-  Services.telemetry.canRecordExtended = false;
-});
-
-// Lower the size limit for DOM Storage content. Check that DOM Storage
-// is not updated, but that other things remain updated.
-add_task(function* test_large_content() {
-  Services.prefs.setIntPref("browser.sessionstore.dom_storage_limit", 5);
-
-  let tab = gBrowser.addTab(URL);
-  let browser = tab.linkedBrowser;
-  yield promiseBrowserLoaded(browser);
-
-  // Flush to make sure chrome received all data.
-  yield TabStateFlusher.flush(browser);
-
-  let state = JSON.parse(ss.getTabState(tab));
-  info(JSON.stringify(state, null, "\t"));
-  Assert.equal(state.storage, null, "We have no storage for the tab");
-  Assert.equal(state.entries[0].title, OUTER_VALUE);
-  yield promiseRemoveTab(tab);
-
-  Services.prefs.clearUserPref("browser.sessionstore.dom_storage_limit");
-});
-
 function purgeDomainData(browser, domain) {
   return sendMessage(browser, "ss-test:purgeDomainData", domain);
 }
