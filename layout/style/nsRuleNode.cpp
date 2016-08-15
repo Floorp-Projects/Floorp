70f5971663e713e47d7c28a1339955e17063d10c
cku <cku@mozilla.com>
2016-08-15 15:33:03 +0800

    Bug 1295062 - Fix mask-position initial value in nsRuleNode::ComputeSVGResetData. r=astley,heycam
    MozReview-Commit-ID: EvbSdG0xLtV

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -7252,7 +7252,9 @@ nsRuleNode::ComputeBackgroundData(void* aStartStruct,
 
   // background-position-x/y: enum, length, percent (flags), inherit [list]
   nsStyleImageLayers::Position::PositionCoord initialPositionCoord;
-  initialPositionCoord.mPercent = 0.0f;
+  initialPositionCoord.mPercent =
+    nsStyleImageLayers::Position::GetInitialValue(
+      nsStyleImageLayers::LayerType::Background);
   initialPositionCoord.mLength = 0;
   initialPositionCoord.mHasPercent = true;
 
@@ -10028,7 +10030,9 @@ nsRuleNode::ComputeSVGResetData(void* aStartStruct,
 
   // mask-position-x/y: enum, length, percent (flags), inherit [list]
   nsStyleImageLayers::Position::PositionCoord initialPositionCoord;
-  initialPositionCoord.mPercent = 0.0f;
+  initialPositionCoord.mPercent =
+    nsStyleImageLayers::Position::GetInitialValue(
+      nsStyleImageLayers::LayerType::Mask);
   initialPositionCoord.mLength = 0;
   initialPositionCoord.mHasPercent = true;
 
