7e3fe5aecc09d4fdfaaaf46d5e29fbbd0b5bc6b8
Bob Owen <bobowencode@gmail.com>
2016-08-16 07:40:43 +0100

    Bug 1259601: Add content process sandbox level to about:support sandboxing information. r=jld, r=mossop
    MozReview-Commit-ID: HFRiEbkEztp

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -560,7 +560,7 @@ var snapshotFormatters = {
   },
 
   sandbox: function sandbox(data) {
-    if (AppConstants.platform != "linux" || !AppConstants.MOZ_SANDBOX)
+    if (!AppConstants.MOZ_SANDBOX)
       return;
 
     let strings = stringBundle();
