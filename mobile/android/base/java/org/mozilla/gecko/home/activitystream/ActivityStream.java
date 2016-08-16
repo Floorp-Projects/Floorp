d98458ba5390e41406b9072324e39a3b2d264b47
Andrzej Hunt <ahunt@mozilla.com>
2016-08-16 13:38:19 -0700

    Bug 1288106 - Move history (highlights) into main recyclerview, and eliminated horizontal highlights r=sebastian
    Moving all vertically listed items into one RecylerView avoids the layouting issues encountered
    with nested RVs. This results in a slightly more complex adapter, however overall this still
    seems simpler than having to hack around the RecyclerView height measurements. (This also
    makes the layout itself simpler, which hopefully means better performance too.)
    
    MozReview-Commit-ID: HFS9q5JNYpY
    
    --HG--
    rename : mobile/android/base/java/org/mozilla/gecko/home/activitystream/MainRecyclerAdapter.java => mobile/android/base/java/org/mozilla/gecko/home/activitystream/StreamRecyclerAdapter.java
    extra : rebase_source : a8914cade238e031f171718914dbd67e8f81fba1

--- mobile/android/base/java/org/mozilla/gecko/home/activitystream/ActivityStream.java
+++ mobile/android/base/java/org/mozilla/gecko/home/activitystream/ActivityStream.java
@@ -5,21 +5,26 @@
  package org.mozilla.gecko.home.activitystream;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import org.mozilla.gecko.GeckoProfile;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.animation.PropertyAnimator;
 import org.mozilla.gecko.home.HomeBanner;
 import org.mozilla.gecko.home.HomeFragment;
 import org.mozilla.gecko.home.HomeScreen;
+import org.mozilla.gecko.home.SimpleCursorLoader;
 
 public class ActivityStream extends FrameLayout implements HomeScreen {
+    private StreamRecyclerAdapter adapter;
 
     public ActivityStream(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -65,13 +70,52 @@ public class ActivityStream extends FrameLayout implements HomeScreen {
                      PropertyAnimator animator) {
         // Signal to load data from storage as needed, compare with HomePager
         RecyclerView rv = (RecyclerView) findViewById(R.id.activity_stream_main_recyclerview);
-        rv.setAdapter(new MainRecyclerAdapter(getContext()));
+
+        adapter = new StreamRecyclerAdapter();
+        rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
         rv.setHasFixedSize(true);
+
+        lm.initLoader(0, null, new CursorLoaderCallbacks());
     }
 
     @Override
     public void unload() {
         // Signal to clear data that has been loaded, compare with HomePager
+    }
+
+    /**
+     * This is a temporary cursor loader. We'll probably need a completely new query for AS,
+     * at that time we can switch to the new CursorLoader, as opposed to using our outdated
+     * SimpleCursorLoader.
+     */
+    private static class HistoryLoader extends SimpleCursorLoader {
+        public HistoryLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected Cursor loadCursor() {
+            final Context context = getContext();
+            return GeckoProfile.get(context).getDB()
+                    .getRecentHistory(context.getContentResolver(), 10);
+        }
+    }
+
+    private class CursorLoaderCallbacks implements LoaderManager.LoaderCallbacks<Cursor> {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            return new HistoryLoader(getContext());
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            adapter.swapCursor(data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            adapter.swapCursor(null);
+        }
     }
 }
