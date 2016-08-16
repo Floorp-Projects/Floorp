faa27f3b859f440af3b0720c3ce364e2135e11eb
Sander Mathijs van Veen <smvv@kompiler.org>
2016-08-16 09:25:01 -0400

    Bug 1295031 - Pass direct = true only if we have enough space for the forwarding pointer. r=jandem
    --HG--
    extra : rebase_source : 6940838a5f490c6b128e3cfe333c0b4eb05480e6

---
+++ js/src/jit-test/tests/basic/bug1295031.js
@@ -0,0 +1,20 @@
+try {
+    while (true) {
+        a = inIon() ? 0 : 300;
+        try {
+            buf = new Uint8ClampedArray(a);
+            assertEq(buf.length, 300);
+        } catch (e) {
+            assertEqa;
+        }
+    }
+} catch(exc1) {}
+loadFile()
+function loadFile() {
+    try {
+        switch (lfRunTypeId) {
+          case 1:
+            eval();
+        }
+    } catch (lfVare) {}
+}
