1d9aabb4aa27f2e3ad6ba2899ad06b707da5fd9a
Mason Chang <mchang@mozilla.com>
2016-08-16 08:46:13 -0700

    Bug 1294121 - Develop a continuous paint mode for Firefox. r=mattwoodrow

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1765,6 +1765,11 @@ already_AddRefed<LayerManager> nsDisplayList::PaintRoot(nsDisplayListBuilder* aB
       layerManager->BeginTransaction();
     }
   }
+
+  if (XRE_IsContentProcess() && gfxPrefs::AlwaysPaint()) {
+    FrameLayerBuilder::InvalidateAllLayers(layerManager);
+  }
+
   if (widgetTransaction) {
     layerBuilder->DidBeginRetainedLayerTransaction(layerManager);
   }
