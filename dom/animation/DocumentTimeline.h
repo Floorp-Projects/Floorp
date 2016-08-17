cc78c4e0cda6a4c2e7d60b20f07a235b6c60341c
Brian Birtles <birtles@gmail.com>
2016-08-17 09:53:22 +0900

    Bug 1291665 - Unregister from refresh driver in DocumentTimeline::Unlink. r=hiro, a=abillings
    --HG--
    extra : source : 3f9686fc43a6e58d383ec1bdc10eae3072943c71

--- dom/animation/DocumentTimeline.h
+++ dom/animation/DocumentTimeline.h
@@ -83,6 +83,7 @@ public:
 protected:
   TimeStamp GetCurrentTimeStamp() const;
   nsRefreshDriver* GetRefreshDriver() const;
+  void UnregisterFromRefreshDriver();
 
   nsCOMPtr<nsIDocument> mDocument;
 
