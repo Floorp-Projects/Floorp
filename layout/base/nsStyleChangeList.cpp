0543fee3c75a66ac7bd241b513e41d528d79d47f
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-08-16 17:15:29 -0700

    Bug 1295818: Port nsStyleChangeList to modern C++. r=heycam
    MozReview-Commit-ID: 2j1h1AYkFql

--- layout/base/nsStyleChangeList.cpp
+++ layout/base/nsStyleChangeList.cpp
@@ -13,124 +13,40 @@
 #include "nsIFrame.h"
 #include "nsFrameManager.h"
 
-static const uint32_t kGrowArrayBy = 10;
-
-nsStyleChangeList::nsStyleChangeList()
-  : mArray(mBuffer),
-    mArraySize(kStyleChangeBufferSize),
-    mCount(0)
-{
-  MOZ_COUNT_CTOR(nsStyleChangeList);
-}
-
-nsStyleChangeList::~nsStyleChangeList()
-{
-  MOZ_COUNT_DTOR(nsStyleChangeList);
-  Clear();
-}
-
-nsresult 
-nsStyleChangeList::ChangeAt(int32_t aIndex, nsIFrame*& aFrame, nsIContent*& aContent, 
-                            nsChangeHint& aHint) const
-{
-  if ((0 <= aIndex) && (aIndex < mCount)) {
-    aFrame = mArray[aIndex].mFrame;
-    aContent = mArray[aIndex].mContent;
-    aHint = mArray[aIndex].mHint;
-    return NS_OK;
-  }
-  return NS_ERROR_ILLEGAL_VALUE;
-}
-
-nsresult 
-nsStyleChangeList::ChangeAt(int32_t aIndex, const nsStyleChangeData** aChangeData) const
-{
-  if ((0 <= aIndex) && (aIndex < mCount)) {
-    *aChangeData = &mArray[aIndex];
-    return NS_OK;
-  }
-  return NS_ERROR_ILLEGAL_VALUE;
-}
-
-nsresult 
+void
 nsStyleChangeList::AppendChange(nsIFrame* aFrame, nsIContent* aContent, nsChangeHint aHint)
 {
-  NS_ASSERTION(aFrame || (aHint & nsChangeHint_ReconstructFrame),
-               "must have frame");
-  NS_ASSERTION(aContent || !(aHint & nsChangeHint_ReconstructFrame),
-               "must have content");
+  MOZ_ASSERT(aFrame || (aHint & nsChangeHint_ReconstructFrame),
+             "must have frame");
+  MOZ_ASSERT(aContent || !(aHint & nsChangeHint_ReconstructFrame),
+             "must have content");
   // XXXbz we should make this take Element instead of nsIContent
-  NS_ASSERTION(!aContent || aContent->IsElement() ||
-               // display:contents elements posts the changes for their children:
-               (aFrame && aContent->GetParent() &&
-                aFrame->PresContext()->FrameManager()->
-                  GetDisplayContentsStyleFor(aContent->GetParent())),
-               "Shouldn't be trying to restyle non-elements directly, "
-               "except if it's a display:contents child");
-  NS_ASSERTION(!(aHint & nsChangeHint_AllReflowHints) ||
-               (aHint & nsChangeHint_NeedReflow),
-               "Reflow hint bits set without actually asking for a reflow");
+  MOZ_ASSERT(!aContent || aContent->IsElement() ||
+             // display:contents elements posts the changes for their children:
+             (aFrame && aContent->GetParent() &&
+             aFrame->PresContext()->FrameManager()->
+               GetDisplayContentsStyleFor(aContent->GetParent())),
+             "Shouldn't be trying to restyle non-elements directly, "
+             "except if it's a display:contents child");
+  MOZ_ASSERT(!(aHint & nsChangeHint_AllReflowHints) ||
+             (aHint & nsChangeHint_NeedReflow),
+             "Reflow hint bits set without actually asking for a reflow");
 
-  if ((0 < mCount) && (aHint & nsChangeHint_ReconstructFrame)) { // filter out all other changes for same content
+  // Filter out all other changes for same content
+  if (!IsEmpty() && (aHint & nsChangeHint_ReconstructFrame)) {
     if (aContent) {
-      for (int32_t index = mCount - 1; index >= 0; --index) {
-        if (aContent == mArray[index].mContent) { // remove this change
-          aContent->Release();
-          mCount--;
-          if (index < mCount) { // move later changes down
-            ::memmove(&mArray[index], &mArray[index + 1], 
-                      (mCount - index) * sizeof(nsStyleChangeData));
-          }
-        }
-      }
+      // NOTE: This is captured by reference to please static analysis.
+      // Capturing it by value as a pointer should be fine in this case.
+      RemoveElementsBy([&](const nsStyleChangeData& aData) {
+        return aData.mContent == aContent;
+      });
     }
   }
 
-  int32_t last = mCount - 1;
-  if ((0 < mCount) && aFrame && (aFrame == mArray[last].mFrame)) { // same as last frame
-    mArray[last].mHint |= aHint;
+  if (!IsEmpty() && aFrame && aFrame == LastElement().mFrame) {
+    LastElement().mHint |= aHint;
+    return;
   }
-  else {
-    if (mCount == mArraySize) {
-      int32_t newSize = mArraySize + kGrowArrayBy;
-      nsStyleChangeData* newArray = new nsStyleChangeData[newSize];
-      if (newArray) {
-        memcpy(newArray, mArray, mCount * sizeof(nsStyleChangeData));
-        if (mArray != mBuffer) {
-          delete [] mArray;
-        }
-        mArray = newArray;
-        mArraySize = newSize;
-      }
-      else {
-        return NS_ERROR_OUT_OF_MEMORY;
-      }
-    }
-    mArray[mCount].mFrame = aFrame;
-    mArray[mCount].mContent = aContent;
-    if (aContent) {
-      aContent->AddRef();
-    }
-    mArray[mCount].mHint = aHint;
-    mCount++;
-  }
-  return NS_OK;
+
+  AppendElement(nsStyleChangeData { aFrame, aContent, aHint });
 }
-
-void
-nsStyleChangeList::Clear()
-{
-  for (int32_t index = mCount - 1; index >= 0; --index) {
-    nsIContent* content = mArray[index].mContent;
-    if (content) {
-      content->Release();
-    }
-  }
-  if (mArray != mBuffer) {
-    delete [] mArray;
-    mArray = mBuffer;
-    mArraySize = kStyleChangeBufferSize;
-  }
-  mCount = 0;
-}
-
