062865160225e570c4b042138e370debaeb60db6
Edwin Flores <edwin@mozilla.com>
2016-08-17 09:26:11 +0100

    Bug 1293560 - Fix debug output in ScaledFontWin::GetFontFileData - r=jfkthame

--- gfx/2d/ScaledFontWin.cpp
+++ gfx/2d/ScaledFontWin.cpp
@@ -7,6 +7,7 @@
 
 #include "AutoHelpersWin.h"
 #include "Logging.h"
+#include "nsString.h"
 #include "SFNTData.h"
 
 #ifdef USE_SKIA
@@ -68,7 +69,8 @@ ScaledFontWin::GetFontFileData(FontFileDataOutput aDataCallback, void *aBaton)
           reinterpret_cast<char16_t*>(mLogFont.lfFaceName), &index, LF_FACESIZE - 1)) {
       gfxWarning() << "Failed to get index for face name.";
       gfxDevCrash(LogReason::GetFontFileDataFailed) <<
-        "Failed to get index for face name |" << mLogFont.lfFaceName << "|.";
+        "Failed to get index for face name |" <<
+        NS_ConvertUTF16toUTF8(mLogFont.lfFaceName).get() << "|.";
       return false;
     }
   }
