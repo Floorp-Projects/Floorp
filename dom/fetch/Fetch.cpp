5c0ab8720f0ff426a5114ac463fd892c4f77a2e9
Ben Kelly <ben@wanderview.com>
2016-08-17 20:01:35 -0700

    Bug 1293690 P2 Set explicit status levels to fail at when calling WorkerHolder::HoldWorker. r=baku

--- dom/fetch/Fetch.cpp
+++ dom/fetch/Fetch.cpp
@@ -918,7 +918,7 @@ FetchBody<Derived>::RegisterWorkerHolder()
   MOZ_ASSERT(!mWorkerHolder);
   mWorkerHolder = new FetchBodyWorkerHolder<Derived>(this);
 
-  if (!mWorkerHolder->HoldWorker(mWorkerPrivate)) {
+  if (!mWorkerHolder->HoldWorker(mWorkerPrivate, Closing)) {
     NS_WARNING("Failed to add workerHolder");
     mWorkerHolder = nullptr;
     return false;
