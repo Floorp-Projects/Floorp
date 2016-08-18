233632047db022b312095b939167dedea601654f
Aaron Klotz <aklotz@mozilla.com>
2016-08-18 00:50:03 -0600

    Bug 1272146: Add thunk for IAccessible property accesses that pass non-self child IDs; r=tbsaunde
    MozReview-Commit-ID: Kx8UVGP2q7h

--- accessible/windows/msaa/RootAccessibleWrap.cpp
+++ accessible/windows/msaa/RootAccessibleWrap.cpp
@@ -5,14 +5,18 @@
 
 #include "RootAccessibleWrap.h"
 
+#include "ChildIDThunk.h"
 #include "Compatibility.h"
+#include "mozilla/mscom/interceptor.h"
 #include "nsCoreUtils.h"
+#include "nsIXULRuntime.h"
 #include "nsWinUtils.h"
 
 using namespace mozilla::a11y;
+using namespace mozilla::mscom;
 
 ////////////////////////////////////////////////////////////////////////////////
-// Constructor/desctructor
+// Constructor/destructor
 
 RootAccessibleWrap::
   RootAccessibleWrap(nsIDocument* aDocument, nsIPresShell* aPresShell) :
@@ -22,6 +26,59 @@ RootAccessibleWrap::
 
 RootAccessibleWrap::~RootAccessibleWrap()
 {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Accessible
+
+void
+RootAccessibleWrap::GetNativeInterface(void** aOutAccessible)
+{
+  MOZ_ASSERT(aOutAccessible);
+  if (!aOutAccessible) {
+    return;
+  }
+
+  if (mInterceptor &&
+      SUCCEEDED(mInterceptor->Resolve(IID_IAccessible, aOutAccessible))) {
+    return;
+  }
+
+  *aOutAccessible = nullptr;
+
+  RefPtr<IAccessible> rootAccessible;
+  RootAccessible::GetNativeInterface((void**)getter_AddRefs(rootAccessible));
+
+  if (!mozilla::BrowserTabsRemoteAutostart() || XRE_IsContentProcess()) {
+    // We only need to wrap this interface if our process is non-content e10s
+    rootAccessible.forget(aOutAccessible);
+    return;
+  }
+
+  // Otherwise, we need to wrap that IAccessible with an interceptor
+  RefPtr<IInterceptorSink> eventSink(MakeAndAddRef<ChildIDThunk>());
+
+  RefPtr<IAccessible> interceptor;
+  HRESULT hr = CreateInterceptor<IAccessible>(
+      STAUniquePtr<IAccessible>(rootAccessible.forget().take()), eventSink,
+      getter_AddRefs(interceptor));
+  if (FAILED(hr)) {
+    return;
+  }
+
+  RefPtr<IWeakReferenceSource> weakRefSrc;
+  hr = interceptor->QueryInterface(IID_IWeakReferenceSource,
+                                   (void**)getter_AddRefs(weakRefSrc));
+  if (FAILED(hr)) {
+    return;
+  }
+
+  hr = weakRefSrc->GetWeakReference(getter_AddRefs(mInterceptor));
+  if (FAILED(hr)) {
+    return;
+  }
+
+  interceptor.forget(aOutAccessible);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
