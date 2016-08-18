d8d0dd3e36e17ce275676fbd7b5e611f1efc638b
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-08-18 16:04:35 -0700

    Bug 1294499 - New console frontend: Add collapsible stacktrace for console.error/trace messages. r=linclark
    MozReview-Commit-ID: LctpJdFtxX0

---
+++ devtools/client/webconsole/new-console-output/components/collapse-button.js
@@ -0,0 +1,42 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// React & Redux
+const {
+  createClass,
+  DOM: dom,
+  PropTypes,
+} = require("devtools/client/shared/vendor/react");
+
+const CollapseButton = createClass({
+
+  displayName: "CollapseButton",
+
+  propTypes: {
+    open: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+  },
+
+  render: function () {
+    const { title, open, onClick } = this.props;
+
+    let classes = ["theme-twisty"];
+
+    if (open) {
+      classes.push("open");
+    }
+
+    return dom.a({
+      className: classes.join(" "),
+      onClick: onClick,
+      title
+    });
+  }
+});
+
+module.exports.CollapseButton = CollapseButton;
