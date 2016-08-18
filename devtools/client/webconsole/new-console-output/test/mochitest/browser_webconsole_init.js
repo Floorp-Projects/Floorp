14d469149dd12c116c64cb6aebe70579a5825e71
Brian Grinstead <briangrinstead@gmail.com>
2016-08-18 16:04:33 -0700

    Bug 1292325 - New console frontend: Add a waitForMessages function for end-to-end testing. r=linclark
    MozReview-Commit-ID: H908h06zfNJ

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_init.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_init.js
@@ -9,15 +9,60 @@ const TEST_URI = "http://example.com/browser/devtools/client/webconsole/new-cons
 
 add_task(function* () {
   let toolbox = yield openNewTabAndToolbox(TEST_URI, "webconsole");
-  let ui = toolbox.getCurrentPanel().hud.ui;
+  let hud = toolbox.getCurrentPanel().hud;
+  let {ui} = hud;
 
   ok(ui.jsterm, "jsterm exists");
   ok(ui.newConsoleOutput, "newConsoleOutput exists");
 
-  // @TODO: fix proptype errors and actually emit 'new-messages' event
-  // let receivedLog = ui.once("new-messages");
-  // yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function() {
-  //   content.wrappedJSObject.doLogs();
-  // });
-  // yield receivedLog;
+  // @TODO: fix proptype errors
+  let receievedMessages = waitForMessages({
+    hud,
+    messages: [{
+      text: '0',
+    }, {
+      text: '1',
+    }, {
+      text: '2',
+    }],
+  });
+
+  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function() {
+    content.wrappedJSObject.doLogs(3);
+  });
+
+  yield receievedMessages;
 });
+
+/**
+ * Wait for messages in the web console output, resolving once they are receieved.
+ *
+ * @param object options
+ *        - hud: the webconsole
+ *        - messages: Array[Object]. An array of messages to match. Current supported options:
+ *            - text: Exact text match in .message-body
+ */
+function waitForMessages({ hud, messages }) {
+  return new Promise(resolve => {
+    let numMatched = 0;
+    let receivedLog = hud.ui.on("new-messages", function messagesReceieved(e, newMessage) {
+      for (let message of messages) {
+        if (message.matched) {
+          continue;
+        }
+
+        if (newMessage.node.querySelector(".message-body").textContent == message.text) {
+          numMatched++;
+          message.matched = true;
+          info("Matched a message with text: " + message.text + ", still waiting for " + (messages.length - numMatched) + " messages");
+        }
+
+        if (numMatched === messages.length) {
+          hud.ui.off("new-messages", messagesReceieved);
+          resolve();
+          return;
+        }
+      }
+    });
+  });
+}
