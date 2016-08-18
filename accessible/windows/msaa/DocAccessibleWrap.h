37b7d7a6b0cd741ca7a1eefcd98396ade49b171f
Aaron Klotz <aklotz@mozilla.com>
2016-08-18 09:49:13 -0600

    Bug 1268544: Integrate remote COM objects into a11y code; r=tbsaunde
    MozReview-Commit-ID: ctPgegQ83a

--- accessible/windows/msaa/DocAccessibleWrap.h
+++ accessible/windows/msaa/DocAccessibleWrap.h
@@ -22,10 +22,14 @@ public:
 
   // IAccessible
 
-    // Override get_accValue to provide URL when no other value is available
-    virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_accValue( 
-        /* [optional][in] */ VARIANT varChild,
-        /* [retval][out] */ BSTR __RPC_FAR *pszValue);
+  // Override get_accParent for e10s
+  virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_accParent(
+      /* [retval][out] */ IDispatch __RPC_FAR *__RPC_FAR *ppdispParent) override;
+
+  // Override get_accValue to provide URL when no other value is available
+  virtual /* [id][propget] */ HRESULT STDMETHODCALLTYPE get_accValue(
+      /* [optional][in] */ VARIANT varChild,
+      /* [retval][out] */ BSTR __RPC_FAR *pszValue) override;
 
   // Accessible
   virtual void Shutdown();
