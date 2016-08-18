5164dfdac226a5b050fea441ac59a75d84785f4a
Jarda Snajdr <jsnajdr@gmail.com>
2016-08-18 17:05:15 +0200

    Bug 1297082 - Part 1: Convert Curl.jsm to a CommonJS module r=Honza
    MozReview-Commit-ID: LI7Ra1w3RBj

--- devtools/client/netmonitor/test/browser_net_curl-utils.js
+++ devtools/client/netmonitor/test/browser_net_curl-utils.js
@@ -1,9 +1,13 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+"use strict";
+
 /**
  * Tests Curl Utils functionality.
  */
+
+const { CurlUtils } = require("devtools/client/shared/curl");
 
 function test() {
   initNetMonitor(CURL_UTILS_URL).then(([aTab, aDebuggee, aMonitor]) => {
