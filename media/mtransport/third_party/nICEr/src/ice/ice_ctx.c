930f6fca1434bc27d2720e69f728f44aca0382b7
Michael Froman <mfroman@mozilla.com>
2016-08-18 17:29:49 -0500

    Bug 1295552 - restart ice ctx needs stun/turn/dns setup to match original ctx. r=drno a=lizzard
    MozReview-Commit-ID: H8o3gEnFfzQ

--- media/mtransport/third_party/nICEr/src/ice/ice_ctx.c
+++ media/mtransport/third_party/nICEr/src/ice/ice_ctx.c
@@ -168,6 +168,31 @@ int nr_ice_ctx_set_turn_servers(nr_ice_ctx *ctx,nr_ice_turn_server *servers,int 
     return(_status);
   }
 
+int nr_ice_ctx_copy_turn_servers(nr_ice_ctx *ctx, nr_ice_turn_server *servers, int ct)
+  {
+    int _status, i, r;
+
+    if (r = nr_ice_ctx_set_turn_servers(ctx, servers, ct)) {
+      ABORT(r);
+    }
+
+    // make copies of the username and password so they aren't freed twice
+    for (i = 0; i < ct; ++i) {
+      if (!(ctx->turn_servers[i].username = r_strdup(servers[i].username))) {
+        ABORT(R_NO_MEMORY);
+      }
+      if (r = r_data_create(&ctx->turn_servers[i].password,
+                            servers[i].password->data,
+                            servers[i].password->len)) {
+        ABORT(r);
+      }
+    }
+
+    _status=0;
+   abort:
+    return(_status);
+  }
+
 static int nr_ice_ctx_set_local_addrs(nr_ice_ctx *ctx,nr_local_addr *addrs,int ct)
   {
     int _status,i,r;
