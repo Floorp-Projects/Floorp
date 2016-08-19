13a7ef4db2a41531be25a5796a3281e42e3b9c83
Xidorn Quan <me@upsuper.org>
2016-08-20 00:43:01 +1000

    Bug 1296625 - Replace query to ui.use_activity_cursor pref with a cached bool value. r=smaug
    MozReview-Commit-ID: GeTr29JG4qy

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -282,6 +282,7 @@ bool nsContentUtils::sGettersDecodeURLHash = false;
 bool nsContentUtils::sPrivacyResistFingerprinting = false;
 bool nsContentUtils::sSendPerformanceTimingNotifications = false;
 bool nsContentUtils::sAppendLFInSerialization = false;
+bool nsContentUtils::sUseActivityCursor = false;
 
 uint32_t nsContentUtils::sHandlingInputTimeout = 1000;
 
@@ -605,6 +606,9 @@ nsContentUtils::Init()
 
   Preferences::AddBoolVarCache(&sDoNotTrackEnabled,
                                "privacy.donottrackheader.enabled", false);
+
+  Preferences::AddBoolVarCache(&sUseActivityCursor,
+                               "ui.use_activity_cursor", false);
 
   Element::InitCCCallbacks();
 
