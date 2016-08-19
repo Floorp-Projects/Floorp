6c0642b98ad253b0fb45703a0ba048d8ff81df69
Mike Hommey <mh+mozilla@glandium.org>
2016-08-19 11:11:57 +0900

    Bug 1257049 - Stop spawning a separate process for config.status from configure.py. r=gps

--- python/mozbuild/mozbuild/config_status.py
+++ python/mozbuild/mozbuild/config_status.py
@@ -63,7 +63,7 @@ files by running:
 
 def config_status(topobjdir='.', topsrcdir='.', defines=None,
                   non_global_defines=None, substs=None, source=None,
-                  mozconfig=None):
+                  mozconfig=None, args=sys.argv[1:]):
     '''Main function, providing config.status functionality.
 
     Contrary to config.status, it doesn't use CONFIG_FILES or CONFIG_HEADERS
@@ -107,7 +107,7 @@ def config_status(topobjdir='.', topsrcdir='.', defines=None,
                         ' '.join(default_backends))
     parser.add_argument('--dry-run', action='store_true',
                         help='do everything except writing files out.')
-    options = parser.parse_args()
+    options = parser.parse_args(args)
 
     # Without -n, the current directory is meant to be the top object directory
     if not options.not_topobjdir:
