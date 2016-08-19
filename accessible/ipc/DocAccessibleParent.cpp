c650a459be89f21d192a89fc8c37d05ffe134d4f
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-08-19 10:02:19 +0200

    Backed out changeset ff58e1a5f483 (bug 1268544)

--- accessible/ipc/DocAccessibleParent.cpp
+++ accessible/ipc/DocAccessibleParent.cpp
@@ -482,6 +482,7 @@ DocAccessibleParent::RecvCOMProxy(const IAccessibleHolder& aCOMProxy,
   SetCOMInterface(ptr);
 
   Accessible* outerDoc = OuterDocOfRemoteBrowser();
+  MOZ_ASSERT(outerDoc);
   IAccessible* rawNative = nullptr;
   if (outerDoc) {
     outerDoc->GetNativeInterface((void**) &rawNative);
