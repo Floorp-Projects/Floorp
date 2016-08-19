30d2f1e7a82a815487aff6b32144dd5bdfdfc5db
Cykesiopka <cykesiopka.bmo@gmail.com>
2016-08-19 09:36:53 +0800

    Bug 1275841 - Rename nsSDR.(cpp|h) to SecretDecoderRing.(cpp|h). r=keeler
    There are currently two ways of naming files containing the implementation of
    interface nsIFoo:
    1. nsFoo.(cpp|h) (previous convention)
    2. Foo.(cpp|h) (new convention)
    
    nsSDR.(cpp|h) matches neither of these. Renaming the files to follow convention
    makes it easier to discover what the files implement, and increases codebase
    consistency.
    
    MozReview-Commit-ID: 3ThPxPouiie

--- security/manager/ssl/SecretDecoderRing.h
+++ security/manager/ssl/SecretDecoderRing.h
@@ -4,49 +4,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef _NSSDR_H_
-#define _NSSDR_H_
+#ifndef SecretDecoderRing_h
+#define SecretDecoderRing_h
 
 #include "nsISecretDecoderRing.h"
 #include "nsNSSShutDown.h"
 
 /**
- * NS_SDR_CONTRACTID - contract id for SDR services.
  *   Implements nsISecretDecoderRing.
  *   Should eventually implement an interface to set window
  *   context and other information. (nsISecretDecoderRingConfig)
- *
- * NOTE: This definition should move to base code.  It
- *   is conditionally defined here until it is moved.
- *   Delete this after defining in the new location.
  */
-#ifndef NS_SDR_CONTRACTID
-#define NS_SDR_CONTRACTID "@mozilla.org/security/sdr;1"
-#endif
+#define NS_SECRETDECODERRING_CONTRACTID "@mozilla.org/security/sdr;1"
 
-// ===============================================
-// nsSecretDecoderRing - implementation of nsISecretDecoderRing
-// ===============================================
-
-#define NS_SDR_CID \
+#define NS_SECRETDECODERRING_CID \
   { 0x0c4f1ddc, 0x1dd2, 0x11b2, { 0x9d, 0x95, 0xf2, 0xfd, 0xf1, 0x13, 0x04, 0x4b } }
 
-class nsSecretDecoderRing : public nsISecretDecoderRing
-                          , public nsISecretDecoderRingConfig
-                          , public nsNSSShutDownObject
+class SecretDecoderRing : public nsISecretDecoderRing
+                        , public nsISecretDecoderRingConfig
+                        , public nsNSSShutDownObject
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSISECRETDECODERRING
   NS_DECL_NSISECRETDECODERRINGCONFIG
 
-  nsSecretDecoderRing();
+  SecretDecoderRing();
 
   // Nothing to release.
   virtual void virtualDestroyNSSReference() override {}
 
 protected:
-  virtual ~nsSecretDecoderRing();
+  virtual ~SecretDecoderRing();
 };
 
-#endif /* _NSSDR_H_ */
+#endif // SecretDecoderRing_h
