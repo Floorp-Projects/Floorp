eb5db91302f36a35c2d599ad3af50a5bf95f6133
Jon Coppeard <jcoppeard@mozilla.com>
2016-08-19 18:04:47 +0100

    Bug 1293127 - Fix testcode to handle zeal builds r=me

--- js/src/jit-test/tests/gc/bug-1293127.js
+++ js/src/jit-test/tests/gc/bug-1293127.js
@@ -1,5 +1,6 @@
 // Test that we can create 1000 cross compartment wrappers to nursery objects
 // without trigger a minor GC.
+gczeal(0);
 let g = newGlobal();
 evalcx("function f(x) { return {x: x}; }", g);
 gc();
@@ -7,4 +8,4 @@ let initial = gcparam("gcNumber");
 for (let i = 0; i < 1000; i++)
     g.f(i);
 let final = gcparam("gcNumber");
-assertEq(initial, final);
+assertEq(final, initial);
