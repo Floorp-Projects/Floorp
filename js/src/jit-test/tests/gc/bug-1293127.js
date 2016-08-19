45e8f3aed18d7ddd670012297c4b910ddd0e5cbc
Jon Coppeard <jcoppeard@mozilla.com>
2016-08-19 16:56:25 +0100

    Bug 1293127 - Mark CCW keys that have nursery pointers explicitly rather than using the generic buffer r=terrence

---
+++ js/src/jit-test/tests/gc/bug-1293127.js
@@ -0,0 +1,10 @@
+// Test that we can create 1000 cross compartment wrappers to nursery objects
+// without trigger a minor GC.
+let g = newGlobal();
+evalcx("function f(x) { return {x: x}; }", g);
+gc();
+let initial = gcparam("gcNumber");
+for (let i = 0; i < 1000; i++)
+    g.f(i);
+let final = gcparam("gcNumber");
+assertEq(initial, final);
