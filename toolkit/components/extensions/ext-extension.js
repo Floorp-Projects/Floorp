f600b2cf9921b70e01182b147feb4cc511ed96c5
Rob Wu <rob@robwu.nl>
2016-08-18 19:19:40 -0700

    Bug 1287010 - Use schema-generated extension, split ext-extension.js r=billm
    - This was the last non-schema-generated API in content scripts.
    
    MozReview-Commit-ID: FaIOCHoircf

--- toolkit/components/extensions/ext-extension.js
+++ toolkit/components/extensions/ext-extension.js
@@ -4,10 +4,6 @@ extensions.registerSchemaAPI("extension", "addon_parent", context => {
   let {extension} = context;
   return {
     extension: {
-      getURL: function(url) {
-        return extension.baseURI.resolve(url);
-      },
-
       getViews: function(fetchProperties) {
         let result = Cu.cloneInto([], context.cloneScope);
 
@@ -32,11 +28,8 @@ extensions.registerSchemaAPI("extension", "addon_parent", context => {
       },
 
       get lastError() {
+        // TODO(robwu): See comment about lastError in ext-runtime.js
         return context.lastError;
-      },
-
-      get inIncognitoContext() {
-        return context.incognito;
       },
 
       isAllowedIncognitoAccess() {
