1808ef840f75d9d527e89cf1346bdf2a46dc6b0f
Chris Pearce <cpearce@mozilla.com>
2016-08-19 21:34:42 +1200

    Bug 1295630 - Provide a way for A/V sync to be ruined, so talos can test compositor throughput by painting expired frames. r=mattwoodrow, a=ritu
    In bug 1258870 I changed the media code so that we dropped all late video
    frames. Without this, our A/V sync was broken when the decode was struggling
    to keep up, and we weren't reporting dropped frames when the decode couldn't
    keep up, and so players couldn't adapt to a bitrate which the decode could keep
    up on.
    
    However, dropping all late frames broke talos tests which relied on the old
    behaviour of us rendering video frames that were late. So this patch adds a
    pref to cause the frame dropping code to not drop the last frame in the
    queue, so there will always be something for the compositor to composit. This
    means talos will once again be able to test how fast it can composit frames
    that aren't supposed to be drawn.
    
    The pref is media.ruin-av-sync.enabled.
    
    It defaults to false.
    
    MozReview-Commit-ID: J3VvpzoDRmI

--- dom/media/mediasink/VideoSink.h
+++ dom/media/mediasink/VideoSink.h
@@ -139,6 +139,15 @@ private:
   // Max frame number sent to compositor at a time.
   // Based on the pref value obtained in MDSM.
   const uint32_t mVideoQueueSendToCompositorSize;
+
+  // Talos tests for the compositor require at least one frame in the
+  // video queue so that the compositor has something to composit during
+  // the talos test when the decode is stressed. We have a minimum size
+  // on the video queue in order to facilitate this talos test.
+  // Note: Normal playback should not have a queue size of more than 0,
+  // otherwise A/V sync will be ruined! *Only* make this non-zero for
+  // testing purposes.
+  const uint32_t mMinVideoQueueSize;
 };
 
 } // namespace media
