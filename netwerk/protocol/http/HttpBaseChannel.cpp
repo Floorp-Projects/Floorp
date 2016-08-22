9c9509fcaa4a79bdfa4aa337041c6f1b71d3c789
Wes Kocher <wkocher@mozilla.com>
2016-08-22 11:29:04 -0700

    Backed out changeset e1f3ed984607 (bug 1261585) for causing bug 1292586 a=backout

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -69,7 +69,6 @@ HttpBaseChannel::HttpBaseChannel()
   , mPriority(PRIORITY_NORMAL)
   , mRedirectionLimit(gHttpHandler->RedirectionLimit())
   , mApplyConversion(true)
-  , mHaveListenerForTraceableChannel(false)
   , mCanceled(false)
   , mIsPending(false)
   , mWasOpened(false)
@@ -2649,7 +2648,6 @@ HttpBaseChannel::SetNewListener(nsIStreamListener *aListener, nsIStreamListener 
 
   wrapper.forget(_retval);
   mListener = aListener;
-  mHaveListenerForTraceableChannel = true;
   return NS_OK;
 }
 
