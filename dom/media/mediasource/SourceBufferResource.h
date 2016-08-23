8934c64db0df23bca5510576214ef69625e9593e
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-08-23 17:38:46 +1200

    Bug 1296453 - [MSE] P2. Clean up SourceBufferResource. r=gerald, a=lizzard
    The code was designed around the need for a MediaResource::Read which is
    no longer possible since bug 1190238
    
    MozReview-Commit-ID: 7s76YWg93jS

--- dom/media/mediasource/SourceBufferResource.h
+++ dom/media/mediasource/SourceBufferResource.h
@@ -136,8 +136,6 @@ public:
 
 private:
   virtual ~SourceBufferResource();
-  nsresult SeekInternal(int64_t aOffset);
-  nsresult ReadInternal(char* aBuffer, uint32_t aCount, uint32_t* aBytes, bool aMayBlock);
   nsresult ReadAtInternal(int64_t aOffset, char* aBuffer, uint32_t aCount, uint32_t* aBytes, bool aMayBlock);
 
   const nsCString mType;
