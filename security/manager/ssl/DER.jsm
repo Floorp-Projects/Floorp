d5c67d9540f00ce5995dffc70af48ea4f78f8d52
David Keeler <dkeeler@mozilla.com>
2016-08-23 16:30:38 -0700

    bug 1299676 - introduce der.jsm as a minimal js ASN.1 DER decoder r=Cykesiopka,jcj
    MozReview-Commit-ID: 29oNa3S3s6a

---
+++ security/manager/ssl/DER.jsm
@@ -0,0 +1,304 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// A minimal ASN.1 DER decoder. Supports input lengths up to 65539 (one byte for
+// the outer tag, one byte for the 0x82 length-length indicator, two bytes
+// indicating a contents length of 65535, and then the 65535 bytes of contents).
+// Intended to be used like so:
+//
+// let bytes = <an array of bytes describing a SEQUENCE OF INTEGER>;
+// let der = new DER.DER(bytes);
+// let contents = new DER.DER(der.readTagAndGetContents(DER.SEQUENCE));
+// while (!contents.atEnd()) {
+//   let integerBytes = contents.readTagAndGetContents(DER.INTEGER);
+//   <... do something with integerBytes ...>
+// }
+// der.assertAtEnd();
+//
+// For CHOICE, use readTLVChoice and pass an array of acceptable tags.
+// The convenience function readBIT_STRING is provided to handle the unused bits
+// aspect of BIT STRING. It returns an object that has the properties contents
+// (an array of bytes consisting of the bytes making up the BIT STRING) and
+// unusedBits (indicating the number of unused bits at the end).
+// All other functions generally return an array of bytes or a single byte as
+// appropriate.
+// peekTag can be used to see if the next tag is an expected given tag.
+// readTLV reads and returns an entire (tag, length, value) tuple (again
+// returned as an array of bytes).
+
+const UNIVERSAL = 0 << 6;
+const CONSTRUCTED = 1 << 5;
+const CONTEXT_SPECIFIC = 2 << 6;
+
+const INTEGER = UNIVERSAL | 0x02; // 0x02
+const BIT_STRING = UNIVERSAL | 0x03; // 0x03
+const NULL = UNIVERSAL | 0x05; // 0x05
+const OBJECT_IDENTIFIER = UNIVERSAL | 0x06; // 0x06
+const PrintableString = UNIVERSAL | 0x13; // 0x13
+const TeletexString = UNIVERSAL | 0x14; // 0x14
+const IA5String = UNIVERSAL | 0x16; // 0x16
+const UTCTime = UNIVERSAL | 0x17; // 0x17
+const GeneralizedTime = UNIVERSAL | 0x18; // 0x18
+const UTF8String = UNIVERSAL | 0x0c; // 0x0c
+const SEQUENCE = UNIVERSAL | CONSTRUCTED | 0x10; // 0x30
+const SET = UNIVERSAL | CONSTRUCTED | 0x11; // 0x31
+
+const ERROR_INVALID_INPUT = "invalid input";
+const ERROR_DATA_TRUNCATED = "data truncated";
+const ERROR_EXTRA_DATA = "extra data";
+const ERROR_INVALID_LENGTH = "invalid length";
+const ERROR_UNSUPPORTED_ASN1 = "unsupported asn.1";
+const ERROR_UNSUPPORTED_LENGTH = "unsupported length";
+const ERROR_INVALID_BIT_STRING = "invalid BIT STRING encoding";
+
+/** Class representing a decoded BIT STRING. */
+class BitString {
+  /**
+   * @param {Number} unusedBits the number of unused bits
+   * @param {Number[]} contents an array of bytes comprising the BIT STRING
+   */
+  constructor(unusedBits, contents) {
+    this._unusedBits = unusedBits;
+    this._contents = contents;
+  }
+
+  /**
+   * Get the number of unused bits in the BIT STRING
+   * @return {Number} the number of unused bits
+   */
+  get unusedBits() {
+    return this._unusedBits;
+  }
+
+  /**
+   * Get the contents of the BIT STRING
+   * @return {Number[]} an array of bytes representing the contents
+   */
+  get contents() {
+    return this._contents;
+  }
+}
+
+/** Class representing DER-encoded data. Provides methods for decoding it. */
+class DER {
+  /**
+   * @param {Number[]} bytes an array of bytes representing the encoded data
+   */
+  constructor(bytes) {
+    // Reject non-array inputs.
+    if (!Array.isArray(bytes)) {
+      throw new Error(ERROR_INVALID_INPUT);
+    }
+    if (bytes.length > 65539) {
+      throw new Error(ERROR_UNSUPPORTED_LENGTH);
+    }
+    // Reject inputs containing non-integer values or values too small or large.
+    if (bytes.some(b => !Number.isInteger(b) || b < 0 || b > 255)) {
+      throw new Error(ERROR_INVALID_INPUT);
+    }
+    this._bytes = bytes;
+    this._cursor = 0;
+  }
+
+  /**
+   * Asserts that the decoder is at the end of the given data. Throws an error
+   * if this is not the case.
+   */
+  assertAtEnd() {
+    if (!this.atEnd()) {
+      throw new Error(ERROR_EXTRA_DATA);
+    }
+  }
+
+  /**
+   * Determines whether or not the decoder is at the end of the given data.
+   * @return {Boolean} true if the decoder is at the end and false otherwise
+   */
+  atEnd() {
+    return this._cursor == this._bytes.length;
+  }
+
+  /**
+   * Reads the next byte of data. Throws if no more data is available.
+   * @return {Number} the next byte of data
+   */
+  readByte() {
+    if (this._cursor >= this._bytes.length) {
+      throw new Error(ERROR_DATA_TRUNCATED);
+    }
+    let val = this._bytes[this._cursor];
+    this._cursor++;
+    return val;
+  }
+
+  /**
+   * Given the next expected tag, reads and asserts that the next tag is in fact
+   * the given tag.
+   * @param {Number} expectedTag the expected next tag
+   */
+  _readExpectedTag(expectedTag) {
+    let tag = this.readByte();
+    if (tag != expectedTag) {
+      throw new Error(`unexpected tag: found ${tag} instead of ${expectedTag}`);
+    }
+  }
+
+  /**
+   * Decodes and returns the length portion of an ASN.1 TLV tuple. Throws if the
+   * length is incorrectly encoded or if it describes a length greater than
+   * 65535 bytes. Indefinite-length encoding is not supported.
+   * @return {Number} the length of the value of the TLV tuple
+   */
+  _readLength() {
+    let nextByte = this.readByte();
+    if (nextByte < 0x80) {
+      return nextByte;
+    }
+    if (nextByte == 0x80) {
+      throw new Error(ERROR_UNSUPPORTED_ASN1);
+    }
+    if (nextByte == 0x81) {
+      let length = this.readByte();
+      if (length < 0x80) {
+        throw new Error(ERROR_INVALID_LENGTH);
+      }
+      return length;
+    }
+    if (nextByte == 0x82) {
+      let length1 = this.readByte();
+      let length2 = this.readByte();
+      let length = (length1 << 8) | length2;
+      if (length < 256) {
+        throw new Error(ERROR_INVALID_LENGTH);
+      }
+      return length;
+    }
+    throw new Error(ERROR_UNSUPPORTED_LENGTH);
+  }
+
+  /**
+   * Reads <length> bytes of data if available. Throws if less than <length>
+   * bytes are available.
+   * @param {Number} length the number of bytes to read. Must be non-negative.
+   * @return {Number[]} the next <length> bytes of data
+   */
+  readBytes(length) {
+    if (length < 0) {
+      throw new Error(ERROR_INVALID_LENGTH);
+    }
+    let bytes = [];
+    for (let i = 0; i < length; i++) {
+      bytes.push(this.readByte());
+    }
+    return bytes;
+  }
+
+  /**
+   * Given an expected next ASN.1 tag, ensures that that tag is next and returns
+   * the contents of that tag. Throws if a different tag is encountered or if
+   * the data is otherwise incorrectly encoded.
+   * @param {Number} tag the next expected ASN.1 tag
+   * @return {Number[]} the contents of the tag
+   */
+  readTagAndGetContents(tag) {
+    this._readExpectedTag(tag);
+    let length = this._readLength();
+    return this.readBytes(length);
+  }
+
+  /**
+   * Returns the next byte without advancing the decoder. Throws if no more data
+   * is available.
+   * @return {Number} the next available byte
+   */
+  _peekByte() {
+    if (this._cursor >= this._bytes.length) {
+      throw new Error(ERROR_DATA_TRUNCATED);
+    }
+    return this._bytes[this._cursor];
+  }
+
+  /**
+   * Given an expected tag, reads the next entire ASN.1 TLV tuple, asserting
+   * that the tag matches.
+   * @param {Number} tag the expected tag
+   * @return {Number[]} an array of bytes representing the TLV tuple
+   */
+  _readExpectedTLV(tag) {
+    let mark = this._cursor;
+    this._readExpectedTag(tag);
+    let length = this._readLength();
+    // read the bytes so we know they're there (also to advance the cursor)
+    this.readBytes(length);
+    return this._bytes.slice(mark, this._cursor);
+  }
+
+  /**
+   * Reads the next ASN.1 tag, length, and value and returns them as an array of
+   * bytes.
+   * @return {Number[]} an array of bytes representing the next ASN.1 TLV
+   */
+  readTLV() {
+    let nextTag = this._peekByte();
+    return this._readExpectedTLV(nextTag);
+  }
+
+  /**
+   * Convenience function for decoding a BIT STRING. Reads and returns the
+   * contents of the expected next BIT STRING. Throws if the next TLV isn't a
+   * BIT STRING or if the BIT STRING is incorrectly encoded.
+   * @return {BitString} the next BIT STRING
+   */
+  readBIT_STRING() {
+    let contents = this.readTagAndGetContents(BIT_STRING);
+    if (contents.length < 1) {
+      throw new Error(ERROR_INVALID_BIT_STRING);
+    }
+    let unusedBits = contents[0];
+    if (unusedBits > 7) {
+      throw new Error(ERROR_INVALID_BIT_STRING);
+    }
+    // Zero bytes of content but some amount of padding is invalid.
+    if (contents.length == 1 && unusedBits != 0) {
+      throw new Error(ERROR_INVALID_BIT_STRING);
+    }
+    return new BitString(unusedBits, contents.slice(1, contents.length));
+  }
+
+  /**
+   * Looks to see if the next ASN.1 tag is the expected given tag.
+   * @param {Number} tag the expected next ASN.1 tag
+   * @return {Boolean} true if the next tag is the given one and false otherwise
+   */
+  peekTag(tag) {
+    if (this._cursor >= this._bytes.length) {
+      return false;
+    }
+    return this._bytes[this._cursor] == tag;
+  }
+
+  /**
+   * Given a list of possible next ASN.1 tags, returns the next TLV if the next
+   * tag is in the list. Throws if the next tag is not in the list or if the
+   * data is incorrectly encoded.
+   * @param {Number[]} tagList the list of potential next tags
+   * @return {Number[]} the contents of the next TLV if the next tag is in
+   *                    <tagList>
+   */
+  readTLVChoice(tagList) {
+    let tag = this._peekByte();
+    if (!tagList.includes(tag)) {
+      throw new Error(
+        `unexpected tag: found ${tag} instead of one of ${tagList}`);
+    }
+    return this._readExpectedTLV(tag);
+  }
+}
+
+this.DER = { UNIVERSAL, CONSTRUCTED, CONTEXT_SPECIFIC, INTEGER, BIT_STRING,
+             NULL, OBJECT_IDENTIFIER, PrintableString, TeletexString, IA5String,
+             UTCTime, GeneralizedTime, UTF8String, SEQUENCE, SET, DER };
+this.EXPORTED_SYMBOLS = ["DER"];
---
+++ security/manager/ssl/tests/unit/test_der.js
@@ -0,0 +1,219 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Tests DER.jsm functionality.
+
+var { DER } = Cu.import("resource://gre/modules/psm/DER.jsm", {});
+
+function run_simple_tests() {
+  throws(() => new DER.DER("this is not an array"), /invalid input/,
+         "should throw given non-array input");
+  throws(() => new DER.DER([0, "invalid input", 1]), /invalid input/,
+         "should throw given non-byte data (string case)");
+  throws(() => new DER.DER([31, 1, {}]), /invalid input/,
+         "should throw given non-byte data (object case)");
+  throws(() => new DER.DER([0.1, 3, 1]), /invalid input/,
+         "should throw given non-byte data (non-integer case)");
+  throws(() => new DER.DER([1, 3, -1]), /invalid input/,
+         "should throw given non-byte data (negative integer case)");
+  throws(() => new DER.DER([1, 300, 79]), /invalid input/,
+         "should throw given non-byte data (large integer case)");
+
+  let testReadByte = new DER.DER([0x0a, 0x0b]);
+  equal(testReadByte.readByte(), 0x0a, "should read 0x0a");
+  equal(testReadByte.readByte(), 0x0b, "should read 0x0b");
+  throws(() => testReadByte.readByte(), /data truncated/,
+         "reading more data than is available should fail");
+
+  let testReadBytes = new DER.DER([0x0c, 0x0d, 0x0e]);
+  deepEqual(testReadBytes.readBytes(3), [0x0c, 0x0d, 0x0e],
+            "should read correct sequence of bytes");
+
+  let testReadNegativeBytes = new DER.DER([0xff, 0xaf]);
+  throws(() => testReadNegativeBytes.readBytes(-4), /invalid length/,
+         "reading a negative number of bytes should fail");
+
+  let testReadZeroBytes = new DER.DER([]);
+  equal(testReadZeroBytes.readBytes(0).length, 0,
+        "reading zero bytes should result in a zero-length array");
+
+  let testReadTooManyBytes = new DER.DER([0xab, 0xcd, 0xef]);
+  throws(() => testReadTooManyBytes.readBytes(4), /data truncated/,
+         "reading too many bytes should fail");
+
+  let testSEQUENCE = new DER.DER([0x30, 0x01, 0x01]);
+  let content = testSEQUENCE.readTagAndGetContents(DER.SEQUENCE);
+  equal(content.length, 1, "content should have length 1");
+  equal(content[0], 1, "value of content should be [1]");
+  ok(testSEQUENCE.atEnd(), "testSEQUENCE should be at the end of its input");
+  testSEQUENCE.assertAtEnd();
+
+  // The length purports to be 4 bytes, but there are only 2 available.
+  let truncatedSEQUENCE = new DER.DER([0x30, 0x04, 0x00, 0x00]);
+  throws(() => truncatedSEQUENCE.readTagAndGetContents(DER.SEQUENCE),
+         /data truncated/, "should get 'data truncated' error");
+
+  // With 2 bytes of content, there is 1 remaining after reading the content.
+  let extraDataSEQUENCE = new DER.DER([0x30, 0x02, 0xab, 0xcd, 0xef]);
+  content = extraDataSEQUENCE.readTagAndGetContents(DER.SEQUENCE);
+  equal(content.length, 2, "content should have length 2");
+  deepEqual(content, [0xab, 0xcd], "value of content should be [0xab, 0xcd]");
+  ok(!extraDataSEQUENCE.atEnd(),
+     "extraDataSEQUENCE should not be at the end of its input");
+  throws(() => extraDataSEQUENCE.assertAtEnd(), /extra data/,
+         "should get 'extra data' error");
+
+  // The length of 0x81 0x01 is invalid because it could be encoded as just
+  // 0x01, which is shorter.
+  let invalidLengthSEQUENCE1 = new DER.DER([0x30, 0x81, 0x01, 0x00]);
+  throws(() => invalidLengthSEQUENCE1.readTagAndGetContents(DER.SEQUENCE),
+         /invalid length/, "should get 'invalid length' error");
+
+  // Similarly, 0x82 0x00 0x01 could be encoded as just 0x01, which is shorter.
+  let invalidLengthSEQUENCE2 = new DER.DER([0x30, 0x82, 0x00, 0x01, 0x00]);
+  throws(() => invalidLengthSEQUENCE2.readTagAndGetContents(DER.SEQUENCE),
+         /invalid length/, "should get 'invalid length' error");
+
+  // Lengths requiring 4 bytes to encode are not supported.
+  let unsupportedLengthSEQUENCE = new DER.DER([0x30, 0x83, 0x01, 0x01, 0x01]);
+  throws(() => unsupportedLengthSEQUENCE.readTagAndGetContents(DER.SEQUENCE),
+         /unsupported length/, "should get 'unsupported length' error");
+
+  // Indefinite lengths are not supported (and aren't DER anyway).
+  let unsupportedASN1SEQUENCE = new DER.DER([0x30, 0x80, 0x01, 0x00, 0x00]);
+  throws(() => unsupportedASN1SEQUENCE.readTagAndGetContents(DER.SEQUENCE),
+         /unsupported asn.1/, "should get 'unsupported asn.1' error");
+
+  let unexpectedTag = new DER.DER([0x31, 0x01, 0x00]);
+  throws(() => unexpectedTag.readTagAndGetContents(DER.SEQUENCE),
+         /unexpected tag/, "should get 'unexpected tag' error");
+
+  let readTLVTestcase = new DER.DER([0x02, 0x03, 0x45, 0x67, 0x89]);
+  let bytes = readTLVTestcase.readTLV();
+  deepEqual(bytes, [0x02, 0x03, 0x45, 0x67, 0x89],
+            "bytes read with readTLV should be equal to expected value");
+
+  let peekTagTestcase = new DER.DER([0x30, 0x01, 0x00]);
+  ok(peekTagTestcase.peekTag(DER.SEQUENCE),
+     "peekTag should return true for peeking with a SEQUENCE at a SEQUENCE");
+  ok(!peekTagTestcase.peekTag(DER.SET),
+     "peekTag should return false for peeking with a SET at a SEQUENCE");
+  peekTagTestcase.readTLV();
+  ok(!peekTagTestcase.peekTag(DER.SEQUENCE),
+     "peekTag should return false for peeking at a DER with no more data");
+
+  let tlvChoiceTestcase = new DER.DER([0x31, 0x02, 0xaa, 0xbb]);
+  let tlvChoiceContents = tlvChoiceTestcase.readTLVChoice([DER.NULL, DER.SET]);
+  deepEqual(tlvChoiceContents, [0x31, 0x02, 0xaa, 0xbb],
+            "readTLVChoice should return expected bytes");
+
+  let tlvChoiceNoMatchTestcase = new DER.DER([0x30, 0x01, 0xff]);
+  throws(() => tlvChoiceNoMatchTestcase.readTLVChoice([DER.NULL, DER.SET]),
+         /unexpected tag/,
+         "readTLVChoice should throw if no matching tag is found");
+}
+
+function run_bit_string_tests() {
+  let bitstringDER = new DER.DER([0x03, 0x04, 0x03, 0x01, 0x02, 0xf8]);
+  let bitstring = bitstringDER.readBIT_STRING();
+  equal(bitstring.unusedBits, 3, "BIT STRING should have 3 unused bits");
+  deepEqual(bitstring.contents, [0x01, 0x02, 0xf8],
+            "BIT STRING should have expected contents");
+
+  let bitstringTooManyUnusedBits = new DER.DER([0x03, 0x02, 0x08, 0x00]);
+  throws(() => bitstringTooManyUnusedBits.readBIT_STRING(),
+         /invalid BIT STRING encoding/,
+         "BIT STRING with too many unused bits should throw");
+
+  // A BIT STRING must have the unused bits byte, and so its length must be at
+  // least one.
+  let bitstringMissingUnusedBits = new DER.DER([0x03, 0x00]);
+  throws(() => bitstringMissingUnusedBits.readBIT_STRING(),
+         /invalid BIT STRING encoding/,
+         "BIT STRING with missing unused bits (and no contents) should throw");
+
+  // The minimal BIT STRING is 03 01 00 (zero bits of padding and zero bytes of
+  // content).
+  let minimalBitstringDER = new DER.DER([0x03, 0x01, 0x00]);
+  let minimalBitstring = minimalBitstringDER.readBIT_STRING();
+  equal(minimalBitstring.unusedBits, 0,
+       "minimal BIT STRING should have 0 unused bits");
+  equal(minimalBitstring.contents.length, 0,
+        "minimal BIT STRING should have empty contents");
+
+  // However, a BIT STRING with zero bytes of content can't have any padding,
+  // because that makes no sense.
+  let noContentsPaddedBitstringDER = new DER.DER([0x03, 0x01, 0x03]);
+  throws(() => noContentsPaddedBitstringDER.readBIT_STRING(),
+          /invalid BIT STRING encoding/,
+         "BIT STRING with no contents with non-zero padding should throw");
+}
+
+function run_compound_tests() {
+  let derBytes =
+    [ 0x30, 0x1a,                       // SEQUENCE
+        0x02, 0x02, 0x77, 0xff,         //   INTEGER
+        0x06, 0x03, 0x2b, 0x01, 0x01,   //   OBJECT IDENTIFIER
+        0x30, 0x07,                     //   SEQUENCE
+          0x05, 0x00,                   //     NULL
+          0x02, 0x03, 0x45, 0x46, 0x47, //     INTEGER
+        0x30, 0x06,                     //   SEQUENCE
+          0x02, 0x02, 0x00, 0xff,       //     INTEGER
+          0x05, 0x00 ];                 //     NULL
+  let der = new DER.DER(derBytes);
+  let contents = new DER.DER(der.readTagAndGetContents(DER.SEQUENCE));
+  let firstINTEGER = contents.readTagAndGetContents(DER.INTEGER);
+  deepEqual(firstINTEGER, [0x77, 0xff],
+            "first INTEGER should have expected value");
+  let oid = contents.readTagAndGetContents(DER.OBJECT_IDENTIFIER);
+  deepEqual(oid, [0x2b, 0x01, 0x01],
+            "OBJECT IDENTIFIER should have expected value");
+
+  let firstNested = new DER.DER(contents.readTagAndGetContents(DER.SEQUENCE));
+  let firstNestedNULL = firstNested.readTagAndGetContents(DER.NULL);
+  equal(firstNestedNULL.length, 0,
+        "first nested NULL should have expected value (empty array)");
+  let firstNestedINTEGER = firstNested.readTagAndGetContents(DER.INTEGER);
+  deepEqual(firstNestedINTEGER, [0x45, 0x46, 0x47],
+            "first nested INTEGER should have expected value");
+  firstNested.assertAtEnd();
+
+  let secondNested = new DER.DER(contents.readTagAndGetContents(DER.SEQUENCE));
+  let secondNestedINTEGER = secondNested.readTagAndGetContents(DER.INTEGER);
+  deepEqual(secondNestedINTEGER, [0x00, 0xff],
+            "second nested INTEGER should have expected value");
+  let secondNestedNULL = secondNested.readTagAndGetContents(DER.NULL);
+  equal(secondNestedNULL.length, 0,
+        "second nested NULL should have expected value (empty array)");
+  secondNested.assertAtEnd();
+
+  contents.assertAtEnd();
+  der.assertAtEnd();
+
+  let invalidDERBytes =
+    [ 0x30, 0x06,     // SEQUENCE
+        0x30, 0x02,   //   SEQUENCE
+          0x02, 0x01, //     INTEGER (missing data)
+        0x05, 0x00,   //   NULL
+      0x00, 0x00 ];   // (extra data)
+  let invalidDER = new DER.DER(invalidDERBytes);
+  let invalidContents = new DER.DER(
+    invalidDER.readTagAndGetContents(DER.SEQUENCE));
+  let invalidContentsContents = new DER.DER(
+    invalidContents.readTagAndGetContents(DER.SEQUENCE));
+  throws(() => invalidContentsContents.readTagAndGetContents(DER.INTEGER),
+         /data truncated/, "should throw due to missing data");
+  let nestedNULL = invalidContents.readTagAndGetContents(DER.NULL);
+  equal(nestedNULL.length, 0, "nested NULL should have expected value");
+  invalidContents.assertAtEnd();
+  throws(() => invalidDER.assertAtEnd(), /extra data/,
+         "should throw due to extra data");
+}
+
+function run_test() {
+  run_simple_tests();
+  run_bit_string_tests();
+  run_compound_tests();
+}
