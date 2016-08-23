b37ee69413879c2315ab43ef67742d00e276b91b
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-08-23 18:54:42 +0200

    Backed out changeset 03d5eb3c540b (bug 1284874)

--- testing/marionette/harness/marionette/tests/unit/test_pagesource.py
+++ testing/marionette/harness/marionette/tests/unit/test_pagesource.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import MarionetteTestCase, skip_if_mobile
+from marionette import MarionetteTestCase
 
 
 class TestPageSource(MarionetteTestCase):
@@ -27,7 +27,6 @@ class TestPageSource(MarionetteTestCase):
         self.assertEqual(re.sub("\s", "", source), "<xml><foo><bar>baz</bar></foo></xml>")
 
 
-@skip_if_mobile
 class TestPageSourceChrome(MarionetteTestCase):
     def setUp(self):
         MarionetteTestCase.setUp(self)
