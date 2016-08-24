25f7de89e34afef58e4871027300f01fdb0cebeb
Dão Gottwald <dao@mozilla.com>
2016-08-24 16:40:11 +0200

    Bug 1297157 - Abstract away the visuallyselected attribute on anonymous tab nodes. r=mconley

--- browser/base/content/test/webrtc/browser_devices_get_user_media_anim.js
+++ browser/base/content/test/webrtc/browser_devices_get_user_media_anim.js
@@ -42,7 +42,7 @@ var gTests = [
 
       // After selecting a new tab, check the attribute is still there,
       // and the icon is now visible.
-      gBrowser.selectedTab = gBrowser.addTab();
+      yield BrowserTestUtils.switchTab(gBrowser, gBrowser.addTab());
       is(gBrowser.selectedTab.getAttribute("sharing"), "",
          "the new tab doesn't have the 'sharing' attribute");
       is(tab.getAttribute("sharing"), aSharing,
@@ -51,7 +51,7 @@ var gTests = [
             "the animated sharing icon of the tab is now visible");
 
       // Ensure the icon disappears when selecting the tab.
-      gBrowser.removeCurrentTab();
+      yield BrowserTestUtils.removeTab(gBrowser.selectedTab);
       ok(tab.selected, "the tab with ongoing sharing is selected again");
       is(window.getComputedStyle(icon).display, "none",
          "the animated sharing icon is gone after selecting the tab again");
