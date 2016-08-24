d3bb235f83b37aaf6203c246e668f821fbd125b0
Benjamin Smedberg <benjamin@smedbergs.us>
2016-08-24 12:40:17 -0400

    Bug 1293062 - Plugin permissions are confused between Flash and the VLC plugin. Fix this by checking that the plugin name for the special Flash plugin is "Shockwave Flash", which is true of Adobe Flash but not replacement plugins such as VLC or even gnash. This causes other plugins to have their own pref tag so that users can enable/disable them separately. r=jimm
    MozReview-Commit-ID: 3sGKO4S3U8U

--- dom/plugins/base/nsPluginTags.cpp
+++ dom/plugins/base/nsPluginTags.cpp
@@ -360,8 +360,12 @@ void nsPluginTag::InitMime(const char* const* aMimeTypes,
         mSupportsAsyncInit = true;
         break;
       case nsPluginHost::eSpecialType_Flash:
-        mIsFlashPlugin = true;
-        mSupportsAsyncInit = true;
+        // VLC sometimes claims to implement the Flash MIME type, and we want
+        // to allow users to control that separately from Adobe Flash.
+        if (Name().EqualsLiteral("Shockwave Flash")) {
+          mIsFlashPlugin = true;
+          mSupportsAsyncInit = true;
+        }
         break;
       case nsPluginHost::eSpecialType_Silverlight:
       case nsPluginHost::eSpecialType_Unity:
