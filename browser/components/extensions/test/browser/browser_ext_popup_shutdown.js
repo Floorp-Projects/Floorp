c0d3aa8e1c5af9ed8c73fc39197181e8e0cffd40
Kris Maglione <maglione.k@gmail.com>
2016-08-24 16:26:35 -0700

    Bug 1293287: Close extension popups on extension shutdown. r=aswan
    MozReview-Commit-ID: 5lpIk6XrdOM

---
+++ browser/components/extensions/test/browser/browser_ext_popup_shutdown.js
@@ -0,0 +1,74 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+let getExtension = () => {
+  return ExtensionTestUtils.loadExtension({
+    background() {
+      browser.tabs.query({active: true, currentWindow: true}, tabs => {
+        browser.pageAction.show(tabs[0].id);
+      });
+    },
+
+    manifest: {
+      "browser_action": {
+        "default_popup": "popup.html",
+        "browser_style": false,
+      },
+
+      "page_action": {
+        "default_popup": "popup.html",
+        "browser_style": false,
+      },
+    },
+
+    files: {
+      "popup.html": `<!DOCTYPE html>
+        <html><head><meta charset="utf-8"></head></html>`,
+    },
+  });
+};
+
+add_task(function* testStandaloneBrowserAction() {
+  info("Test stand-alone browserAction popup");
+
+  let extension = getExtension();
+  yield extension.startup();
+
+  clickBrowserAction(extension);
+  let browser = yield awaitExtensionPanel(extension);
+  let panel = getPanelForNode(browser);
+
+  yield extension.unload();
+
+  is(panel.parentNode, null, "Panel should be removed from the document");
+});
+
+add_task(function* testMenuPanelBrowserAction() {
+  let extension = getExtension();
+  yield extension.startup();
+
+  let widget = getBrowserActionWidget(extension);
+  CustomizableUI.addWidgetToArea(widget.id, CustomizableUI.AREA_PANEL);
+
+  clickBrowserAction(extension);
+  let browser = yield awaitExtensionPanel(extension);
+  let panel = getPanelForNode(browser);
+
+  yield extension.unload();
+
+  is(panel.state, "closed", "Panel should be closed");
+});
+
+add_task(function* testPageAction() {
+  let extension = getExtension();
+  yield extension.startup();
+
+  clickPageAction(extension);
+  let browser = yield awaitExtensionPanel(extension);
+  let panel = getPanelForNode(browser);
+
+  yield extension.unload();
+
+  is(panel.parentNode, null, "Panel should be removed from the document");
+});
