99338a81458b3c49d78565be201d61fbf91ee4c2
Andi-Bogdan Postelnicu <bpostelnicu@mozilla.com>
2016-08-24 17:10:07 +0300

    Bug 1282408 - add ignore initialization check flag for mPseudoStackHack in ImageBridgeThread. r=ttromey
    MozReview-Commit-ID: 2B0bPOi1Tde

--- gfx/layers/ipc/ImageBridgeChild.cpp
+++ gfx/layers/ipc/ImageBridgeChild.cpp
@@ -82,7 +82,7 @@ private:
 #ifdef MOZ_ENABLE_PROFILER_SPS
   // This is needed to avoid a spurious leak report.  There's no other
   // use for it.  See bug 1239504 and bug 1215265.
-  PseudoStack* mPseudoStackHack;
+  MOZ_INIT_OUTSIDE_CTOR PseudoStack* mPseudoStackHack;
 #endif
 };
 }
