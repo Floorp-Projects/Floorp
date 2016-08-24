b8e6d1d0627444ab7b69658d3dd218ce4e4e4c08
Botond Ballo <botond@mozilla.com>
2016-08-24 13:42:57 -0400

    Bug 1321885 - Add CSSPixel::FromAppUnits(nscoord) and CSSPixel::ToAppUnits(CSSCoord). r=kats
    MozReview-Commit-ID: 8V9gjVf35bC

--- layout/base/Units.h
+++ layout/base/Units.h
@@ -194,6 +194,10 @@ struct CSSPixel {
 
   // Conversions from app units
 
+  static CSSCoord FromAppUnits(nscoord aCoord) {
+    return NSAppUnitsToFloatPixels(aCoord, float(AppUnitsPerCSSPixel()));
+  }
+
   static CSSPoint FromAppUnits(const nsPoint& aPoint) {
     return CSSPoint(NSAppUnitsToFloatPixels(aPoint.x, float(AppUnitsPerCSSPixel())),
                     NSAppUnitsToFloatPixels(aPoint.y, float(AppUnitsPerCSSPixel())));
@@ -237,6 +241,10 @@ struct CSSPixel {
   }
 
   // Conversions to app units
+
+  static nscoord ToAppUnits(CSSCoord aCoord) {
+    return NSToCoordRoundWithClamp(aCoord * float(AppUnitsPerCSSPixel()));
+  }
 
   static nsPoint ToAppUnits(const CSSPoint& aPoint) {
     return nsPoint(NSToCoordRoundWithClamp(aPoint.x * float(AppUnitsPerCSSPixel())),
