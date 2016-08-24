0c9bd28c12b5e8d2e80d47d7a59b5ea74f14ed81
Ryan Hunt <rhunt@mozilla.com>
2016-08-24 14:02:00 -0400

    Bug 1297831 - Add an OffscreenFormat gfxVar for use in CompositorOGL. r=jgilbert
    MozReview-Commit-ID: JLthlwaZg1Q

--- gfx/config/gfxVars.h
+++ gfx/config/gfxVars.h
@@ -27,6 +27,7 @@ class gfxVarReceiver;
   _(ContentBackend,             BackendType,      BackendType::NONE)    \
   _(TileSize,                   IntSize,          IntSize(-1, -1))      \
   _(UseXRender,                 bool,             false)                \
+  _(OffscreenFormat,            gfxImageFormat,   mozilla::gfx::SurfaceFormat::X8R8G8B8_UINT32) \
   /* Add new entries above this line. */
 
 // Some graphics settings are computed on the UI process and must be
