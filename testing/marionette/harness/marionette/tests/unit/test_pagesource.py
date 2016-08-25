579e8d9c0b7fe0289c827cab4a4189568a7bdaa4
Maja Frydrychowicz <mjzffr@gmail.com>
2016-08-25 11:11:21 -0400

    Bug 1284874 - Skip irrelevant Marionette tests on Fennec; r=automatedtester,dustin,gbrown
    MozReview-Commit-ID: K2Wy9ij6dz3
    
    --HG--
    extra : rebase_source : 2123d9368c95ec19b51b94afe4e88cd68f1e416e

--- testing/marionette/harness/marionette/tests/unit/test_pagesource.py
+++ testing/marionette/harness/marionette/tests/unit/test_pagesource.py
@@ -25,27 +25,3 @@ class TestPageSource(MarionetteTestCase):
         source = self.marionette.page_source
         import re
         self.assertEqual(re.sub("\s", "", source), "<xml><foo><bar>baz</bar></foo></xml>")
-
-
-class TestPageSourceChrome(MarionetteTestCase):
-    def setUp(self):
-        MarionetteTestCase.setUp(self)
-        self.marionette.set_context("chrome")
-        self.win = self.marionette.current_window_handle
-        self.marionette.execute_script("window.open('chrome://marionette/content/test.xul', 'foo', 'chrome,centerscreen');")
-        self.marionette.switch_to_window('foo')
-        self.assertNotEqual(self.win, self.marionette.current_window_handle)
-
-    def tearDown(self):
-        self.assertNotEqual(self.win, self.marionette.current_window_handle)
-        self.marionette.execute_script("window.close();")
-        self.marionette.switch_to_window(self.win)
-        MarionetteTestCase.tearDown(self)
-
-    def testShouldReturnXULDetails(self):
-        wins = self.marionette.window_handles
-        wins.remove(self.win)
-        newWin = wins.pop()
-        self.marionette.switch_to_window(newWin)
-        source  = self.marionette.page_source
-        self.assertTrue('<textbox id="textInput"' in source)
