19dd82492c525107347268d07b12cef80a87501b
Julian Descottes <jdescottes@mozilla.com>
2016-08-25 17:18:30 +0200

    Bug 1295608 - Migrate devtools/client/inspector to use l10n.js instead of Services.string;r=pbro
    MozReview-Commit-ID: KD0AmgDTpbX

--- devtools/client/inspector/rules/rules.js
+++ devtools/client/inspector/rules/rules.js
@@ -10,9 +10,6 @@
 const promise = require("promise");
 const defer = require("devtools/shared/defer");
 const Services = require("Services");
-/* eslint-disable mozilla/reject-some-requires */
-const {XPCOMUtils} = require("resource://gre/modules/XPCOMUtils.jsm");
-/* eslint-enable mozilla/reject-some-requires */
 const {Task} = require("devtools/shared/task");
 const {Tools} = require("devtools/client/definitions");
 const {l10n} = require("devtools/shared/inspector/css-logic");
@@ -31,11 +28,6 @@ const EventEmitter = require("devtools/shared/event-emitter");
 const StyleInspectorMenu = require("devtools/client/inspector/shared/style-inspector-menu");
 const {KeyShortcuts} = require("devtools/client/shared/key-shortcuts");
 const clipboardHelper = require("devtools/shared/platform/clipboard");
-
-XPCOMUtils.defineLazyGetter(this, "_strings", function () {
-  return Services.strings.createBundle(
-    "chrome://devtools-shared/locale/styleinspector.properties");
-});
 
 const {AutocompletePopup} = require("devtools/client/shared/autocomplete-popup");
 
