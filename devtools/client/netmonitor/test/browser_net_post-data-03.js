6e73c927cdfa31c782471a1e061a21ab558a746a
Jarda Snajdr <jsnajdr@gmail.com>
2016-08-25 13:01:42 +0200

    Bug 1297362 - Part 4: Eliminate CPOWs from Netmonitor mochitests L-P r=ochameau
    MozReview-Commit-ID: 6MXmmKK6d5r

--- devtools/client/netmonitor/test/browser_net_post-data-03.js
+++ devtools/client/netmonitor/test/browser_net_post-data-03.js
@@ -1,87 +1,96 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+"use strict";
+
 /**
  * Tests if the POST requests display the correct information in the UI,
  * for raw payloads with content-type headers attached to the upload stream.
  */
 
-function test() {
-  initNetMonitor(POST_RAW_WITH_HEADERS_URL).then(([aTab, aDebuggee, aMonitor]) => {
-    info("Starting test... ");
+add_task(function* () {
+  let [tab, , monitor] = yield initNetMonitor(POST_RAW_WITH_HEADERS_URL);
+  info("Starting test... ");
 
-    let { document, L10N, NetMonitorView } = aMonitor.panelWin;
-    let { RequestsMenu, NetworkDetails } = NetMonitorView;
-    let TAB_UPDATED = aMonitor.panelWin.EVENTS.TAB_UPDATED;
-    RequestsMenu.lazyUpdate = false;
+  let { document, EVENTS, L10N, NetMonitorView } = monitor.panelWin;
+  let { RequestsMenu } = NetMonitorView;
 
-    Task.spawn(function* () {
-      yield waitForNetworkEvents(aMonitor, 0, 1);
+  RequestsMenu.lazyUpdate = false;
 
-      NetMonitorView.toggleDetailsPane({ visible: true });
-      RequestsMenu.selectedIndex = 0;
-
-      yield waitFor(aMonitor.panelWin, TAB_UPDATED);
-
-      let tab = document.querySelectorAll("#details-pane tab")[0];
-      let tabpanel = document.querySelectorAll("#details-pane tabpanel")[0];
-      let requestFromUploadScope = tabpanel.querySelectorAll(".variables-view-scope")[2];
-
-      is(tab.getAttribute("selected"), "true",
-        "The headers tab in the network details pane should be selected.");
-      is(tabpanel.querySelectorAll(".variables-view-scope").length, 3,
-        "There should be 3 header scopes displayed in this tabpanel.");
-
-      is(requestFromUploadScope.querySelector(".name").getAttribute("value"),
-        L10N.getStr("requestHeadersFromUpload") + " (" +
-        L10N.getFormatStr("networkMenu.sizeKB", L10N.numberWithDecimals(74 / 1024, 3)) + ")",
-        "The request headers from upload scope doesn't have the correct title.");
-
-      is(requestFromUploadScope.querySelectorAll(".variables-view-variable").length, 2,
-        "There should be 2 header values displayed in the request headers from upload scope.");
-
-      is(requestFromUploadScope.querySelectorAll(".variables-view-variable .name")[0].getAttribute("value"),
-        "content-type", "The first request header name was incorrect.");
-      is(requestFromUploadScope.querySelectorAll(".variables-view-variable .value")[0].getAttribute("value"),
-        "\"application/x-www-form-urlencoded\"", "The first request header value was incorrect.");
-      is(requestFromUploadScope.querySelectorAll(".variables-view-variable .name")[1].getAttribute("value"),
-        "custom-header", "The second request header name was incorrect.");
-      is(requestFromUploadScope.querySelectorAll(".variables-view-variable .value")[1].getAttribute("value"),
-        "\"hello world!\"", "The second request header value was incorrect.");
-
-      EventUtils.sendMouseEvent({ type: "mousedown" },
-        document.querySelectorAll("#details-pane tab")[2]);
-
-      yield waitFor(aMonitor.panelWin, TAB_UPDATED);
-
-      tab = document.querySelectorAll("#details-pane tab")[2];
-      tabpanel = document.querySelectorAll("#details-pane tabpanel")[2];
-      let formDataScope = tabpanel.querySelectorAll(".variables-view-scope")[0];
-
-      is(tab.getAttribute("selected"), "true",
-        "The response tab in the network details pane should be selected.");
-      is(tabpanel.querySelectorAll(".variables-view-scope").length, 1,
-        "There should be 1 header scope displayed in this tabpanel.");
-
-      is(formDataScope.querySelector(".name").getAttribute("value"),
-        L10N.getStr("paramsFormData"),
-        "The form data scope doesn't have the correct title.");
-
-      is(formDataScope.querySelectorAll(".variables-view-variable").length, 2,
-        "There should be 2 payload values displayed in the form data scope.");
-
-      is(formDataScope.querySelectorAll(".variables-view-variable .name")[0].getAttribute("value"),
-        "foo", "The first payload param name was incorrect.");
-      is(formDataScope.querySelectorAll(".variables-view-variable .value")[0].getAttribute("value"),
-        "\"bar\"", "The first payload param value was incorrect.");
-      is(formDataScope.querySelectorAll(".variables-view-variable .name")[1].getAttribute("value"),
-        "baz", "The second payload param name was incorrect.");
-      is(formDataScope.querySelectorAll(".variables-view-variable .value")[1].getAttribute("value"),
-        "\"123\"", "The second payload param value was incorrect.");
-
-      teardown(aMonitor).then(finish);
-    });
-
-    aDebuggee.performRequests();
+  let wait = waitForNetworkEvents(monitor, 0, 1);
+  yield ContentTask.spawn(tab.linkedBrowser, {}, function* () {
+    content.wrappedJSObject.performRequests();
   });
-}
+  yield wait;
+
+  let onEvent = monitor.panelWin.once(EVENTS.TAB_UPDATED);
+  NetMonitorView.toggleDetailsPane({ visible: true });
+  RequestsMenu.selectedIndex = 0;
+  yield onEvent;
+
+  let tabEl = document.querySelectorAll("#details-pane tab")[0];
+  let tabpanel = document.querySelectorAll("#details-pane tabpanel")[0];
+  let requestFromUploadScope = tabpanel.querySelectorAll(".variables-view-scope")[2];
+
+  is(tabEl.getAttribute("selected"), "true",
+    "The headers tab in the network details pane should be selected.");
+  is(tabpanel.querySelectorAll(".variables-view-scope").length, 3,
+    "There should be 3 header scopes displayed in this tabpanel.");
+
+  is(requestFromUploadScope.querySelector(".name").getAttribute("value"),
+    L10N.getStr("requestHeadersFromUpload") + " (" +
+    L10N.getFormatStr("networkMenu.sizeKB", L10N.numberWithDecimals(74 / 1024, 3)) + ")",
+    "The request headers from upload scope doesn't have the correct title.");
+
+  is(requestFromUploadScope.querySelectorAll(".variables-view-variable").length, 2,
+    "There should be 2 headers displayed in the request headers from upload scope.");
+
+  is(requestFromUploadScope.querySelectorAll(".variables-view-variable .name")[0]
+    .getAttribute("value"),
+    "content-type", "The first request header name was incorrect.");
+  is(requestFromUploadScope.querySelectorAll(".variables-view-variable .value")[0]
+    .getAttribute("value"), "\"application/x-www-form-urlencoded\"",
+    "The first request header value was incorrect.");
+  is(requestFromUploadScope.querySelectorAll(".variables-view-variable .name")[1]
+    .getAttribute("value"),
+    "custom-header", "The second request header name was incorrect.");
+  is(requestFromUploadScope.querySelectorAll(".variables-view-variable .value")[1]
+    .getAttribute("value"),
+    "\"hello world!\"", "The second request header value was incorrect.");
+
+  onEvent = monitor.panelWin.once(EVENTS.TAB_UPDATED);
+  EventUtils.sendMouseEvent({ type: "mousedown" },
+    document.querySelectorAll("#details-pane tab")[2]);
+  yield onEvent;
+
+  tabEl = document.querySelectorAll("#details-pane tab")[2];
+  tabpanel = document.querySelectorAll("#details-pane tabpanel")[2];
+  let formDataScope = tabpanel.querySelectorAll(".variables-view-scope")[0];
+
+  is(tab.getAttribute("selected"), "true",
+    "The response tab in the network details pane should be selected.");
+  is(tabpanel.querySelectorAll(".variables-view-scope").length, 1,
+    "There should be 1 header scope displayed in this tabpanel.");
+
+  is(formDataScope.querySelector(".name").getAttribute("value"),
+    L10N.getStr("paramsFormData"),
+    "The form data scope doesn't have the correct title.");
+
+  is(formDataScope.querySelectorAll(".variables-view-variable").length, 2,
+    "There should be 2 payload values displayed in the form data scope.");
+
+  is(formDataScope.querySelectorAll(".variables-view-variable .name")[0]
+    .getAttribute("value"),
+    "foo", "The first payload param name was incorrect.");
+  is(formDataScope.querySelectorAll(".variables-view-variable .value")[0]
+    .getAttribute("value"),
+    "\"bar\"", "The first payload param value was incorrect.");
+  is(formDataScope.querySelectorAll(".variables-view-variable .name")[1]
+    .getAttribute("value"),
+    "baz", "The second payload param name was incorrect.");
+  is(formDataScope.querySelectorAll(".variables-view-variable .value")[1]
+    .getAttribute("value"),
+    "\"123\"", "The second payload param value was incorrect.");
+
+  return teardown(monitor);
+});
--- devtools/client/netmonitor/test/browser_net_prefs-reload.js
+++ devtools/client/netmonitor/test/browser_net_prefs-reload.js
@@ -1,233 +1,212 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+"use strict";
+
 /**
  * Tests if the prefs that should survive across tool reloads work.
  */
 
-function test() {
-  initNetMonitor(SIMPLE_URL).then(([aTab, aDebuggee, aMonitor]) => {
-    info("Starting test... ");
+add_task(function* () {
+  let [,, monitor] = yield initNetMonitor(SIMPLE_URL);
+  info("Starting test... ");
 
-    // This test reopens the network monitor a bunch of times, for different
-    // hosts (bottom, side, window). This seems to be slow on debug builds.
-    requestLongerTimeout(3);
+  // This test reopens the network monitor a bunch of times, for different
+  // hosts (bottom, side, window). This seems to be slow on debug builds.
+  requestLongerTimeout(3);
 
-    // Use these getters instead of caching instances inside the panel win,
-    // since the tool is reopened a bunch of times during this test
-    // and the instances will differ.
-    let getView = () => aMonitor.panelWin.NetMonitorView;
-    let getController = () => aMonitor.panelWin.NetMonitorController;
+  // Use these getters instead of caching instances inside the panel win,
+  // since the tool is reopened a bunch of times during this test
+  // and the instances will differ.
+  let getView = () => monitor.panelWin.NetMonitorView;
 
-    let prefsToCheck = {
-      filters: {
-        // A custom new value to be used for the verified preference.
-        newValue: ["html", "css"],
-        // Getter used to retrieve the current value from the frontend, in order
-        // to verify that the pref was applied properly.
-        validateValue: ($) => getView().RequestsMenu._activeFilters,
-        // Predicate used to modify the frontend when setting the new pref value,
-        // before trying to validate the changes.
-        modifyFrontend: ($, aValue) => aValue.forEach(e => getView().RequestsMenu.filterOn(e))
-      },
-      networkDetailsWidth: {
-        newValue: ~~(Math.random() * 200 + 100),
-        validateValue: ($) => ~~$("#details-pane").getAttribute("width"),
-        modifyFrontend: ($, aValue) => $("#details-pane").setAttribute("width", aValue)
-      },
-      networkDetailsHeight: {
-        newValue: ~~(Math.random() * 300 + 100),
-        validateValue: ($) => ~~$("#details-pane").getAttribute("height"),
-        modifyFrontend: ($, aValue) => $("#details-pane").setAttribute("height", aValue)
-      }
-      /* add more prefs here... */
-    };
-
-    function storeFirstPrefValues() {
-      info("Caching initial pref values.");
-
-      for (let name in prefsToCheck) {
-        let currentValue = aMonitor.panelWin.Prefs[name];
-        prefsToCheck[name].firstValue = currentValue;
-      }
+  let prefsToCheck = {
+    filters: {
+      // A custom new value to be used for the verified preference.
+      newValue: ["html", "css"],
+      // Getter used to retrieve the current value from the frontend, in order
+      // to verify that the pref was applied properly.
+      validateValue: ($) => getView().RequestsMenu._activeFilters,
+      // Predicate used to modify the frontend when setting the new pref value,
+      // before trying to validate the changes.
+      modifyFrontend: ($, value) => value.forEach(e => getView().RequestsMenu.filterOn(e))
+    },
+    networkDetailsWidth: {
+      newValue: ~~(Math.random() * 200 + 100),
+      validateValue: ($) => ~~$("#details-pane").getAttribute("width"),
+      modifyFrontend: ($, value) => $("#details-pane").setAttribute("width", value)
+    },
+    networkDetailsHeight: {
+      newValue: ~~(Math.random() * 300 + 100),
+      validateValue: ($) => ~~$("#details-pane").getAttribute("height"),
+      modifyFrontend: ($, value) => $("#details-pane").setAttribute("height", value)
     }
+    /* add more prefs here... */
+  };
 
-    function validateFirstPrefValues() {
-      info("Validating current pref values to the UI elements.");
+  yield testBottom();
+  yield testSide();
+  yield testWindow();
 
-      for (let name in prefsToCheck) {
-        let currentValue = aMonitor.panelWin.Prefs[name];
-        let firstValue = prefsToCheck[name].firstValue;
-        let validateValue = prefsToCheck[name].validateValue;
+  info("Moving toolbox back to the bottom...");
+  yield monitor._toolbox.switchHost(Toolbox.HostType.BOTTOM);
+  return teardown(monitor);
 
-        is(currentValue.toSource(), firstValue.toSource(),
-          "Pref " + name + " should be equal to first value: " + firstValue);
-        is(currentValue.toSource(), validateValue(aMonitor.panelWin.$).toSource(),
-          "Pref " + name + " should validate: " + currentValue);
-      }
+  function storeFirstPrefValues() {
+    info("Caching initial pref values.");
+
+    for (let name in prefsToCheck) {
+      let currentValue = monitor.panelWin.Prefs[name];
+      prefsToCheck[name].firstValue = currentValue;
     }
+  }
 
-    function modifyFrontend() {
-      info("Modifying UI elements to the specified new values.");
+  function validateFirstPrefValues() {
+    info("Validating current pref values to the UI elements.");
 
-      for (let name in prefsToCheck) {
-        let currentValue = aMonitor.panelWin.Prefs[name];
-        let firstValue = prefsToCheck[name].firstValue;
-        let newValue = prefsToCheck[name].newValue;
-        let validateValue = prefsToCheck[name].validateValue;
-        let modifyFrontend = prefsToCheck[name].modifyFrontend;
+    for (let name in prefsToCheck) {
+      let currentValue = monitor.panelWin.Prefs[name];
+      let firstValue = prefsToCheck[name].firstValue;
+      let validateValue = prefsToCheck[name].validateValue;
 
-        modifyFrontend(aMonitor.panelWin.$, newValue);
-        info("Modified UI element affecting " + name + " to: " + newValue);
-
-        is(currentValue.toSource(), firstValue.toSource(),
-          "Pref " + name + " should still be equal to first value: " + firstValue);
-        isnot(currentValue.toSource(), newValue.toSource(),
-          "Pref " + name + " should't yet be equal to second value: " + newValue);
-        is(newValue.toSource(), validateValue(aMonitor.panelWin.$).toSource(),
-          "The UI element affecting " + name + " should validate: " + newValue);
-      }
+      is(currentValue.toSource(), firstValue.toSource(),
+        "Pref " + name + " should be equal to first value: " + firstValue);
+      is(currentValue.toSource(), validateValue(monitor.panelWin.$).toSource(),
+        "Pref " + name + " should validate: " + currentValue);
     }
+  }
 
-    function validateNewPrefValues() {
-      info("Invalidating old pref values to the modified UI elements.");
+  function modifyFrontend() {
+    info("Modifying UI elements to the specified new values.");
 
-      for (let name in prefsToCheck) {
-        let currentValue = aMonitor.panelWin.Prefs[name];
-        let firstValue = prefsToCheck[name].firstValue;
-        let newValue = prefsToCheck[name].newValue;
-        let validateValue = prefsToCheck[name].validateValue;
+    for (let name in prefsToCheck) {
+      let currentValue = monitor.panelWin.Prefs[name];
+      let firstValue = prefsToCheck[name].firstValue;
+      let newValue = prefsToCheck[name].newValue;
+      let validateValue = prefsToCheck[name].validateValue;
+      let modFrontend = prefsToCheck[name].modifyFrontend;
 
-        isnot(currentValue.toSource(), firstValue.toSource(),
-          "Pref " + name + " should't be equal to first value: " + firstValue);
-        is(currentValue.toSource(), newValue.toSource(),
-          "Pref " + name + " should now be equal to second value: " + newValue);
-        is(newValue.toSource(), validateValue(aMonitor.panelWin.$).toSource(),
-          "The UI element affecting " + name + " should validate: " + newValue);
-      }
+      modFrontend(monitor.panelWin.$, newValue);
+      info("Modified UI element affecting " + name + " to: " + newValue);
+
+      is(currentValue.toSource(), firstValue.toSource(),
+        "Pref " + name + " should still be equal to first value: " + firstValue);
+      isnot(currentValue.toSource(), newValue.toSource(),
+        "Pref " + name + " should't yet be equal to second value: " + newValue);
+      is(newValue.toSource(), validateValue(monitor.panelWin.$).toSource(),
+        "The UI element affecting " + name + " should validate: " + newValue);
     }
+  }
 
-    function resetFrontend() {
-      info("Resetting UI elements to the cached initial pref values.");
+  function validateNewPrefValues() {
+    info("Invalidating old pref values to the modified UI elements.");
 
-      for (let name in prefsToCheck) {
-        let currentValue = aMonitor.panelWin.Prefs[name];
-        let firstValue = prefsToCheck[name].firstValue;
-        let newValue = prefsToCheck[name].newValue;
-        let validateValue = prefsToCheck[name].validateValue;
-        let modifyFrontend = prefsToCheck[name].modifyFrontend;
+    for (let name in prefsToCheck) {
+      let currentValue = monitor.panelWin.Prefs[name];
+      let firstValue = prefsToCheck[name].firstValue;
+      let newValue = prefsToCheck[name].newValue;
+      let validateValue = prefsToCheck[name].validateValue;
 
-        modifyFrontend(aMonitor.panelWin.$, firstValue);
-        info("Modified UI element affecting " + name + " to: " + firstValue);
-
-        isnot(currentValue.toSource(), firstValue.toSource(),
-          "Pref " + name + " should't yet be equal to first value: " + firstValue);
-        is(currentValue.toSource(), newValue.toSource(),
-          "Pref " + name + " should still be equal to second value: " + newValue);
-        is(firstValue.toSource(), validateValue(aMonitor.panelWin.$).toSource(),
-          "The UI element affecting " + name + " should validate: " + firstValue);
-      }
+      isnot(currentValue.toSource(), firstValue.toSource(),
+        "Pref " + name + " should't be equal to first value: " + firstValue);
+      is(currentValue.toSource(), newValue.toSource(),
+        "Pref " + name + " should now be equal to second value: " + newValue);
+      is(newValue.toSource(), validateValue(monitor.panelWin.$).toSource(),
+        "The UI element affecting " + name + " should validate: " + newValue);
     }
+  }
 
-    function testBottom() {
-      info("Testing prefs reload for a bottom host.");
-      storeFirstPrefValues();
+  function resetFrontend() {
+    info("Resetting UI elements to the cached initial pref values.");
 
-      // Validate and modify while toolbox is on the bottom.
-      validateFirstPrefValues();
-      modifyFrontend();
+    for (let name in prefsToCheck) {
+      let currentValue = monitor.panelWin.Prefs[name];
+      let firstValue = prefsToCheck[name].firstValue;
+      let newValue = prefsToCheck[name].newValue;
+      let validateValue = prefsToCheck[name].validateValue;
+      let modFrontend = prefsToCheck[name].modifyFrontend;
 
-      return restartNetMonitor(aMonitor)
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
+      modFrontend(monitor.panelWin.$, firstValue);
+      info("Modified UI element affecting " + name + " to: " + firstValue);
 
-          // Revalidate and reset frontend while toolbox is on the bottom.
-          validateNewPrefValues();
-          resetFrontend();
-
-          return restartNetMonitor(aMonitor);
-        })
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
-
-          // Revalidate.
-          validateFirstPrefValues();
-        });
+      isnot(currentValue.toSource(), firstValue.toSource(),
+        "Pref " + name + " should't yet be equal to first value: " + firstValue);
+      is(currentValue.toSource(), newValue.toSource(),
+        "Pref " + name + " should still be equal to second value: " + newValue);
+      is(firstValue.toSource(), validateValue(monitor.panelWin.$).toSource(),
+        "The UI element affecting " + name + " should validate: " + firstValue);
     }
+  }
 
-    function testSide() {
-      info("Moving toolbox to the side...");
+  function* testBottom() {
+    info("Testing prefs reload for a bottom host.");
+    storeFirstPrefValues();
 
-      return aMonitor._toolbox.switchHost(Toolbox.HostType.SIDE)
-        .then(() => {
-          info("Testing prefs reload for a side host.");
-          storeFirstPrefValues();
+    // Validate and modify while toolbox is on the bottom.
+    validateFirstPrefValues();
+    modifyFrontend();
 
-          // Validate and modify frontend while toolbox is on the side.
-          validateFirstPrefValues();
-          modifyFrontend();
+    let [,, newMonitor] = yield restartNetMonitor(monitor);
+    monitor = newMonitor;
 
-          return restartNetMonitor(aMonitor);
-        })
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
+    // Revalidate and reset frontend while toolbox is on the bottom.
+    validateNewPrefValues();
+    resetFrontend();
 
-          // Revalidate and reset frontend while toolbox is on the side.
-          validateNewPrefValues();
-          resetFrontend();
+    let [,, newMonitor2] = yield restartNetMonitor(monitor);
+    monitor = newMonitor2;
 
-          return restartNetMonitor(aMonitor);
-        })
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
+    // Revalidate.
+    validateFirstPrefValues();
+  }
 
-          // Revalidate.
-          validateFirstPrefValues();
-        });
-    }
+  function* testSide() {
+    info("Moving toolbox to the side...");
 
-    function testWindow() {
-      info("Moving toolbox into a window...");
+    yield monitor._toolbox.switchHost(Toolbox.HostType.SIDE);
+    info("Testing prefs reload for a side host.");
+    storeFirstPrefValues();
 
-      return aMonitor._toolbox.switchHost(Toolbox.HostType.WINDOW)
-        .then(() => {
-          info("Testing prefs reload for a window host.");
-          storeFirstPrefValues();
+    // Validate and modify frontend while toolbox is on the side.
+    validateFirstPrefValues();
+    modifyFrontend();
 
-          // Validate and modify frontend while toolbox is in a window.
-          validateFirstPrefValues();
-          modifyFrontend();
+    let [,, newMonitor] = yield restartNetMonitor(monitor);
+    monitor = newMonitor;
 
-          return restartNetMonitor(aMonitor);
-        })
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
+    // Revalidate and reset frontend while toolbox is on the side.
+    validateNewPrefValues();
+    resetFrontend();
 
-          // Revalidate and reset frontend while toolbox is in a window.
-          validateNewPrefValues();
-          resetFrontend();
+    let [,, newMonitor2] = yield restartNetMonitor(monitor);
+    monitor = newMonitor2;
 
-          return restartNetMonitor(aMonitor);
-        })
-        .then(([,, aNewMonitor]) => {
-          aMonitor = aNewMonitor;
+    // Revalidate.
+    validateFirstPrefValues();
+  }
 
-          // Revalidate.
-          validateFirstPrefValues();
-        });
-    }
+  function* testWindow() {
+    info("Moving toolbox into a window...");
 
-    function cleanupAndFinish() {
-      info("Moving toolbox back to the bottom...");
+    yield monitor._toolbox.switchHost(Toolbox.HostType.WINDOW);
+    info("Testing prefs reload for a window host.");
+    storeFirstPrefValues();
 
-      aMonitor._toolbox.switchHost(Toolbox.HostType.BOTTOM)
-        .then(() => teardown(aMonitor))
-        .then(finish);
-    }
+    // Validate and modify frontend while toolbox is in a window.
+    validateFirstPrefValues();
+    modifyFrontend();
 
-    testBottom()
-      .then(testSide)
-      .then(testWindow)
-      .then(cleanupAndFinish);
-  });
-}
+    let [,, newMonitor] = yield restartNetMonitor(monitor);
+    monitor = newMonitor;
+
+    // Revalidate and reset frontend while toolbox is in a window.
+    validateNewPrefValues();
+    resetFrontend();
+
+    let [,, newMonitor2] = yield restartNetMonitor(monitor);
+    monitor = newMonitor2;
+
+    // Revalidate.
+    validateFirstPrefValues();
+  }
+});
