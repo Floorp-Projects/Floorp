27031a4fb746c59d1599903089414fec3e11fbcb
Matthew Gregan <kinetik@flim.org>
2016-08-25 13:49:22 +1200

    Bug 1296988 - Update libnestegg.  r=rillian a=lizzard
    --HG--
    extra : rebase_source : 6861f277e827a5bb0bfac090921cccec9c069e8d
    extra : intermediate-source : 95e4e1f78ae623c5884b4f2401ad5447e4639467
    extra : source : 84b4b7e346b8101ae63c3f7497f99bef8be66e23

--- media/libnestegg/include/nestegg.h
+++ media/libnestegg/include/nestegg.h
@@ -460,13 +460,6 @@ int nestegg_has_cues(nestegg * context);
     @retval 1 The file is a WebM file. */
 int nestegg_sniff(unsigned char const * buffer, size_t length);
 
-/** Set the underlying allocation function for library allocations.
-    @param realloc_func The desired function.
-    @retval 1 Success.  realloc_func(p, 0) acts as free()
-    @retval 0 Failure. realloc_func(p, 0) does not act as free()
-    @retval -1 Failure. realloc_func(NULL, 1) failed. */
-int nestegg_set_halloc_func(void * (* realloc_func)(void *, size_t));
-
 #if defined(__cplusplus)
 }
 #endif
