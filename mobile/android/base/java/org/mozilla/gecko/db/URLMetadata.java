1b7ab73a1e7367e22a90e6b27252ff17ca43a1a9
Randall Barker <rbarker@mozilla.com>
2016-08-25 12:50:00 -0700

    Bug 1297850 - part 3, Fix Robocop breakage caused by proguard stripping out URLMetadata r=jchen

--- mobile/android/base/java/org/mozilla/gecko/db/URLMetadata.java
+++ mobile/android/base/java/org/mozilla/gecko/db/URLMetadata.java
@@ -11,8 +11,11 @@ import java.util.Map;
 
 import org.json.JSONObject;
 
+import org.mozilla.gecko.annotation.RobocopTarget;
+
 import android.content.ContentResolver;
 
+@RobocopTarget
 public interface URLMetadata {
     public Map<String, Object> fromJSON(JSONObject obj);
     public Map<String, Map<String, Object>> getForURLs(final ContentResolver cr,
