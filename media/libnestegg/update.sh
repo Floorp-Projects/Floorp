3cb3e7021c50481f2ee61a661717b1cc2f96535b
Matthew Gregan <kinetik@flim.org>
2016-08-25 13:49:22 +1200

    Bug 1296988 - Update libnestegg.  r=rillian

--- media/libnestegg/update.sh
+++ media/libnestegg/update.sh
@@ -1,11 +1,6 @@
 # Usage: sh update.sh <upstream_src_directory>
 cp $1/include/nestegg/nestegg.h include
 cp $1/src/nestegg.c src
-cp $1/halloc/halloc.h src
-cp $1/halloc/src/align.h src
-cp $1/halloc/src/halloc.c src
-cp $1/halloc/src/hlist.h src
-cp $1/halloc/src/macros.h src
 cp $1/LICENSE .
 cp $1/README.md .
 cp $1/AUTHORS .
