ea97eb194df71debbb1032765bfd74d1baeb3a25
Julian Descottes <jdescottes@mozilla.com>
2016-08-25 17:21:00 +0200

    Bug 1295608 - Migrate devtools/client/shared to use l10n.js instead of Services.string;r=pbro
    MozReview-Commit-ID: 2OdCYtae3ad

--- devtools/client/shared/widgets/MdnDocsWidget.js
+++ devtools/client/shared/widgets/MdnDocsWidget.js
@@ -30,6 +30,9 @@ const {getCSSLexer} = require("devtools/shared/css-lexer");
 const EventEmitter = require("devtools/shared/event-emitter");
 const {gDevTools} = require("devtools/client/framework/devtools");
 
+const {LocalizationHelper} = require("devtools/shared/l10n");
+const L10N = new LocalizationHelper("devtools/locale/inspector.properties");
+
 const XHTML_NS = "http://www.w3.org/1999/xhtml";
 
 // Parameters for the XHR request
@@ -263,8 +266,7 @@ function MdnDocsWidget(tooltipContainer) {
   };
 
   // get the localized string for the link text
-  this.elements.linkToMdn.textContent =
-    l10n.strings.GetStringFromName("docsTooltip.visitMDN");
+  this.elements.linkToMdn.textContent = L10N.getStr("docsTooltip.visitMDN");
 
   // listen for clicks and open in the browser window instead
   let mainWindow = Services.wm.getMostRecentWindow(gDevTools.chromeWindowType);
@@ -348,8 +350,7 @@ MdnDocsWidget.prototype = {
      */
     function gotError(error) {
       // show error message
-      elements.summary.textContent =
-        l10n.strings.GetStringFromName("docsTooltip.loadDocsError");
+      elements.summary.textContent = L10N.getStr("docsTooltip.loadDocsError");
 
       // hide the throbber
       elements.info.classList.remove("devtools-throbber");
@@ -372,19 +373,6 @@ MdnDocsWidget.prototype = {
     this.elements = null;
   }
 };
-
-/**
- * L10N utility class
- */
-function L10N() {}
-L10N.prototype = {};
-
-var l10n = new L10N();
-
-loader.lazyGetter(L10N.prototype, "strings", () => {
-  return Services.strings.createBundle(
-    "chrome://devtools/locale/inspector.properties");
-});
 
 /**
  * Test whether a node is all whitespace.
