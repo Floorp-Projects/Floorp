dd4c98173443d18e33624fe9f472cecc2a9dbfd6
Leo Gaspard <leo@gaspard.io>
2016-08-25 16:28:31 -0700

    Bug 1283334 - Part 3: Do not sparsify dense arrays when freezing - Ion. r=nbp

--- js/src/jit/shared/LOpcodes-shared.h
+++ js/src/jit/shared/LOpcodes-shared.h
@@ -294,6 +294,8 @@
     _(ArrayJoin)                    \
     _(StoreElementHoleV)            \
     _(StoreElementHoleT)            \
+    _(FallibleStoreElementV)        \
+    _(FallibleStoreElementT)        \
     _(LoadTypedArrayElementHole)    \
     _(LoadTypedArrayElementStatic)  \
     _(StoreTypedArrayElementHole)   \
