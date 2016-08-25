f4601d5ce9227bd9e00a709b0bdfd166bf830497
JW Wang <jwwang@mozilla.com>
2016-08-26 06:08:54 +1200

    Bug 1298217. Part 4 - Remove the call to DispatchDecodeTasksIfNeeded() from CheckIfDecodeComplete() because we've decoded all samples. r=kaku
    MozReview-Commit-ID: 22qa8K8j5zb

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -833,7 +833,6 @@ MediaDecoderStateMachine::CheckIfDecodeComplete()
     // We've finished decoding all active streams,
     // so move to COMPLETED state.
     SetState(DECODER_STATE_COMPLETED);
-    DispatchDecodeTasksIfNeeded();
     ScheduleStateMachine();
   }
   DECODER_LOG("CheckIfDecodeComplete %scompleted",
