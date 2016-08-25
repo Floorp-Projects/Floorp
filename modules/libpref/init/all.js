43e77092c94b0292b2ee8f7bad7ba92e19feff21
Mason Chang <mchang@mozilla.com>
2016-08-25 12:35:41 -0700

    Bug 1007702. Enable skia on unaccelerated windows. r=lsalzman

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -753,7 +753,7 @@ pref("gfx.font_rendering.opentype_svg.enabled", true);
 // comma separated list of backends to use in order of preference
 // e.g., pref("gfx.canvas.azure.backends", "direct2d,skia,cairo");
 pref("gfx.canvas.azure.backends", "direct2d1.1,skia,cairo");
-pref("gfx.content.azure.backends", "direct2d1.1,cairo");
+pref("gfx.content.azure.backends", "direct2d1.1,skia,cairo");
 #else
 #ifdef XP_MACOSX
 pref("gfx.content.azure.backends", "skia");
