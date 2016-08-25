d7ec5428dac39ea9f3862761e7bfb054003375b8
Haik Aftandilian <haftandilian@mozilla.com>
2016-08-25 15:19:52 -0700

    Bug 1290619 - Content sandbox rules should use actual profile directory, not Profiles/*/ regexes. r=jimm
    Passes the profile dir to the content process as a -profile CLI option so
    that the correct profile dir can be used in the OS X content sandbox rules.
    Only enabled on OS X for now.
    
    On Nightly, profile directories will now be read/write protected from the
    content process (apart from a few profile subdirectories) even when they
    don't reside in ~/Library.
    
    MozReview-Commit-ID: rrTcQwTNdT

--- dom/ipc/ContentChild.h
+++ dom/ipc/ContentChild.h
@@ -21,6 +21,9 @@
 #include "nsWeakPtr.h"
 #include "nsIWindowProvider.h"
 
+#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
+#include "nsIFile.h"
+#endif
 
 struct ChromePackage;
 class nsIObserver;
@@ -113,6 +116,19 @@ public:
   void GetProcessName(nsAString& aName) const;
 
   void GetProcessName(nsACString& aName) const;
+
+#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
+  void GetProfileDir(nsIFile** aProfileDir) const
+  {
+    *aProfileDir = mProfileDir;
+    NS_IF_ADDREF(*aProfileDir);
+  }
+
+  void SetProfileDir(nsIFile* aProfileDir)
+  {
+    mProfileDir = aProfileDir;
+  }
+#endif
 
   bool IsAlive() const;
 
@@ -680,6 +696,10 @@ private:
 
   nsCOMPtr<nsIDomainPolicy> mPolicy;
   nsCOMPtr<nsITimer> mForceKillTimer;
+
+#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
+  nsCOMPtr<nsIFile> mProfileDir;
+#endif
 
   // Hashtable to keep track of the pending GetFilesHelper objects.
   // This GetFilesHelperChild objects are removed when RecvGetFilesResponse is
