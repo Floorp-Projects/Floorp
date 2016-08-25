879567fac551673cc60dac92aacf316edf37aff9
Rob Wu <rob@robwu.nl>
2016-08-24 23:29:29 -0700

    Bug 1298979 - Decouple ProxyContext from ExtensionContext r=billm
    To allow ExtensionContext to be refactored, we first need to remove the
    dependency of ProxyContext on ExtensionContext.
    
    With the decoupling, we can make setContentWindow unconditional and
    remove externallyVisible. Let's clean up later.
    
    MozReview-Commit-ID: 1KmSQpxFTVK

--- toolkit/components/extensions/test/mochitest/file_teardown_test.js
+++ toolkit/components/extensions/test/mochitest/file_teardown_test.js
@@ -4,7 +4,7 @@
 let {Management} = Components.utils.import("resource://gre/modules/Extension.jsm", {});
 let events = [];
 function record(type, extensionContext) {
-  let eventType = type == "page-load" ? "load" : "unload";
+  let eventType = type == "page-load" || type == "proxy-context-load" ? "load" : "unload";
   let url = extensionContext.uri.spec;
   let extensionId = extensionContext.extension.id;
   events.push({eventType, url, extensionId});
@@ -12,9 +12,13 @@ function record(type, extensionContext) {
 
 Management.on("page-load", record);
 Management.on("page-unload", record);
+Management.on("proxy-context-load", record);
+Management.on("proxy-context-unload", record);
 addMessageListener("cleanup", () => {
   Management.off("page-load", record);
   Management.off("page-unload", record);
+  Management.off("proxy-context-load", record);
+  Management.off("proxy-context-unload", record);
 });
 
 addMessageListener("get-context-events", extensionId => {
