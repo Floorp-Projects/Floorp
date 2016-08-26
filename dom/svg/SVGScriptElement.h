25288acf7354f8aeaeaa7cbcd0026787d080692b
Boris Chiou <boris.chiou@gmail.com>
2016-08-26 18:06:04 +0800

    Bug 1245751 - Part 9: Allow href without xlink on SVG <script> elements. r=jwatt
    MozReview-Commit-ID: KS41J1RAPIk

--- dom/svg/SVGScriptElement.h
+++ dom/svg/SVGScriptElement.h
@@ -75,9 +75,9 @@ protected:
 
   virtual StringAttributesInfo GetStringInfo() override;
 
-  enum { HREF };
-  nsSVGString mStringAttributes[1];
-  static StringInfo sStringInfo[1];
+  enum { HREF, XLINK_HREF };
+  nsSVGString mStringAttributes[2];
+  static StringInfo sStringInfo[2];
 };
 
 } // namespace dom
