6aa106320d5f89f9ddcc60a225b166aa37421de1
Anjana Vakil <anjanavakil@gmail.com>
2016-08-26 15:52:29 +0200

    Bug 1291796 - Minor fixes to harness unit tests; r=maja_zf
    In test_marionette_runner.py, fix pytest warning raised when
    importing TestManifest class directly in global namespace.
    
    In test_marionette_arguments.py, improve readability by
    shortening/changing some names and removing unnecessary
    comments (not needed as code is self-explanatory).
    
    MozReview-Commit-ID: GDzxlEqb7MB
    
    --HG--
    extra : rebase_source : 78927cae7f8ec011d2b398e3a1ce71174d7b028c

--- testing/marionette/harness/marionette/tests/harness_unit/test_marionette_arguments.py
+++ testing/marionette/harness/marionette/tests/harness_unit/test_marionette_arguments.py
@@ -6,9 +6,9 @@ import pytest
 from marionette.runtests import MarionetteArguments
 
 
-@pytest.mark.parametrize("sock_timeout_value", ['A', '10', '1B-', '1C2', '44.35'])
-def test_parse_arg_socket_timeout_with_multiple_values(sock_timeout_value):
-    argv = ['marionette', '--socket-timeout', sock_timeout_value]
+@pytest.mark.parametrize("socket_timeout", ['A', '10', '1B-', '1C2', '44.35'])
+def test_parse_arg_socket_timeout(socket_timeout):
+    argv = ['marionette', '--socket-timeout', socket_timeout]
     parser = MarionetteArguments()
 
     def _is_float_convertible(value):
@@ -18,15 +18,13 @@ def test_parse_arg_socket_timeout_with_multiple_values(sock_timeout_value):
         except:
             return False
 
-    if not _is_float_convertible(sock_timeout_value):
-        # should raising exception, since sock_timeout must be convertible to float.
+    if not _is_float_convertible(socket_timeout):
         with pytest.raises(SystemExit) as ex:
             parser.parse_args(args=argv)
         assert ex.value.code == 2
     else:
-        # should pass without raising exception.
         args = parser.parse_args(args=argv)
-        assert hasattr(args, 'socket_timeout') and args.socket_timeout == float(sock_timeout_value)
+        assert hasattr(args, 'socket_timeout') and args.socket_timeout == float(socket_timeout)
 
 
 if __name__ == '__main__':
