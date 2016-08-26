d14c6b15aad622c282646824a70effd9a477d668
Drew Willcoxon <adw@mozilla.com>
2016-08-26 16:36:56 -0700

    Bug 1292345 - Downloads panel didn't shrink to the fit height after all items are downloaded. r=me
    MozReview-Commit-ID: AV0S8GM0ztj
    
    --HG--
    extra : rebase_source : e8d01256b994368b72c73344232f64cb26d6a357

--- browser/components/downloads/content/downloads.js
+++ browser/components/downloads/content/downloads.js
@@ -1525,6 +1525,10 @@ const DownloadsFooter = {
       } else {
         this._footerNode.removeAttribute("showingsummary");
       }
+      if (!aValue && this._showingSummary) {
+        // Make sure the panel's height shrinks when the summary is hidden.
+        DownloadsBlockedSubview.view.setHeightToFit();
+      }
       this._showingSummary = aValue;
     }
     return aValue;
