4dc4ffb3cf005accd3c565ec85d28b5d5a3175bc
Jarda Snajdr <jsnajdr@gmail.com>
2016-08-26 15:47:30 +0200

    Bug 1297362 - Part 6: Eliminate CPOWs from Netmonitor mochitests HAR r=ochameau
    MozReview-Commit-ID: LoomRfTAvOU

--- devtools/client/netmonitor/har/test/browser_net_har_copy_all_as_har.js
+++ devtools/client/netmonitor/har/test/browser_net_har_copy_all_as_har.js
@@ -7,9 +7,7 @@
  * Basic tests for exporting Network panel content into HAR format.
  */
 add_task(function* () {
-  // The first 'tab' isn't necessary so, don't create a var for it
-  // to avoid eslint warning.
-  let [ , debuggee, monitor ] = yield initNetMonitor(SIMPLE_URL);
+  let [tab, , monitor ] = yield initNetMonitor(SIMPLE_URL);
 
   info("Starting test... ");
 
@@ -18,9 +16,10 @@ add_task(function* () {
 
   RequestsMenu.lazyUpdate = false;
 
-  debuggee.location.reload();
+  let wait = waitForNetworkEvents(monitor, 1);
+  tab.linkedBrowser.reload();
+  yield wait;
 
-  yield waitForNetworkEvents(monitor, 1);
   yield RequestsMenu.copyAllAsHar();
 
   let jsonString = SpecialPowers.getClipboardData("text/unicode");
@@ -46,5 +45,5 @@ add_task(function* () {
     "Check response body");
   isnot(entry.timings, undefined, "Check timings");
 
-  teardown(monitor).then(finish);
+  return teardown(monitor);
 });
