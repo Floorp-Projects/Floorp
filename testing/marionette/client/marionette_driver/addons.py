944046ef2107addb6cb8e8d34acc0d097b362ea5
Andreas Tolfsen <ato@mozilla.com>
2016-08-26 13:38:03 +0100

    Bug 1298025 - Move addon installation internally to Marionette; r=ahal,automatedtester
    Addons can be installed and uninstalled using the Marionette client
    utility found in testing/marionette/client/marionette_driver/addons.py.
    It injects system-privileged chrome JavaScript to manipulate the addon
    manager service.
    
    To make this feature more widely adopted, i.e. by other clients such as
    WebDriver, this patch moves the addon installation code to internally
    in Marionette and exposes them as so called WebDriver extension commands.
    
    This patch also _explicitly breaks_ backwards compatibility with
    older Geckos that do not support the new Marionette:installAddon and
    Marionette:uninstallAddon commands.
    
    MozReview-Commit-ID: 18IceiGIg5H

--- testing/marionette/client/marionette_driver/addons.py
+++ testing/marionette/client/marionette_driver/addons.py
@@ -2,44 +2,34 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from .errors import MarionetteException
+from . import errors
 
-__all__ = ['Addons', 'AddonInstallException']
-
-
-# From https://developer.mozilla.org/en-US/Add-ons/Add-on_Manager/AddonManager#AddonInstall_errors
-ADDON_INSTALL_ERRORS = {
-    -1: "ERROR_NETWORK_FAILURE: A network error occured.",
-    -2: "ERROR_INCORECT_HASH: The downloaded file did not match the expected hash.",
-    -3: "ERROR_CORRUPT_FILE: The file appears to be corrupt.",
-    -4: "ERROR_FILE_ACCESS: There was an error accessing the filesystem.",
-    -5: "ERROR_SIGNEDSTATE_REQUIRED: The addon must be signed and isn't.",
-}
+__all__ = ["Addons", "AddonInstallException"]
 
 
-class AddonInstallException(MarionetteException):
+class AddonInstallException(errors.MarionetteException):
     pass
 
 
 class Addons(object):
-    """
-    An API for installing and inspecting addons during Gecko runtime. This
-    is a partially implemented wrapper around Gecko's `AddonManager API`_.
+    """An API for installing and inspecting addons during Gecko
+    runtime. This is a partially implemented wrapper around Gecko's
+    `AddonManager API`_.
 
     For example::
 
         from marionette_driver.addons import Addons
         addons = Addons(marionette)
-        addons.install('path/to/extension.xpi')
+        addons.install("/path/to/extension.xpi")
 
     .. _AddonManager API: https://developer.mozilla.org/en-US/Add-ons/Add-on_Manager
-    """
 
+    """
     def __init__(self, marionette):
         self._mn = marionette
 
     def install(self, path, temp=False):
-        """Install an addon.
+        """Install a Firefox addon.
 
         If the addon is restartless, it can be used right away. Otherwise
         a restart using :func:`~marionette_driver.marionette.Marionette.restart`
@@ -49,55 +39,20 @@ class Addons(object):
         :param temp: Install a temporary addon. Temporary addons will
                      automatically be uninstalled on shutdown and do not need
                      to be signed, though they must be restartless.
+
         :returns: The addon ID string of the newly installed addon.
+
         :raises: :exc:`AddonInstallException`
+
         """
-        with self._mn.using_context('chrome'):
-            addon_id, status = self._mn.execute_async_script("""
-              let fileUtils = Components.utils.import("resource://gre/modules/FileUtils.jsm");
-              let FileUtils = fileUtils.FileUtils;
-              Components.utils.import("resource://gre/modules/AddonManager.jsm");
-              let listener = {
-                onInstallEnded: function(install, addon) {
-                  marionetteScriptFinished([addon.id, 0]);
-                },
-
-                onInstallFailed: function(install) {
-                  marionetteScriptFinished([null, install.error]);
-                },
-
-                onInstalled: function(addon) {
-                  AddonManager.removeAddonListener(listener);
-                  marionetteScriptFinished([addon.id, 0]);
-                }
-              }
-
-              let file = new FileUtils.File(arguments[0]);
-              let temp = arguments[1];
-
-              if (!temp) {
-                AddonManager.getInstallForFile(file, function(aInstall) {
-                  if (aInstall.error != 0) {
-                    marionetteScriptFinished([null, aInstall.error]);
-                  }
-                  aInstall.addListener(listener);
-                  aInstall.install();
-                });
-              } else {
-                AddonManager.addAddonListener(listener);
-                AddonManager.installTemporaryAddon(file);
-              }
-            """, script_args=[path, temp], debug_script=True)
-
-        if status:
-            if status in ADDON_INSTALL_ERRORS:
-                raise AddonInstallException(ADDON_INSTALL_ERRORS[status])
-            raise AddonInstallException(
-                "Addon failed to install with return code: {}".format(status))
-        return addon_id
+        body = {"path": path, "temporary": temp}
+        try:
+            return self._mn._send_message("addon:install", body, key="value")
+        except errors.UnknownException as e:
+            raise AddonInstallException(e)
 
     def uninstall(self, addon_id):
-        """Uninstall an addon.
+        """Uninstall a Firefox addon.
 
         If the addon is restartless, it will be uninstalled right away.
         Otherwise a restart using :func:`~marionette_driver.marionette.Marionette.restart`
@@ -109,12 +64,7 @@ class Addons(object):
         Python.
 
         :param addon_id: The addon ID string to uninstall.
+
         """
-        with self._mn.using_context('chrome'):
-            return self._mn.execute_async_script("""
-              Components.utils.import("resource://gre/modules/AddonManager.jsm");
-              AddonManager.getAddonByID(arguments[0], function(addon) {
-                addon.uninstall();
-                marionetteScriptFinished(0);
-              });
-            """, script_args=[addon_id])
+        body = {"id": addon_id}
+        self._mn._send_message("addon:uninstall", body)
