185d3d769327dbac62ae334c7dcdfc5d8a53be28
Jed Davis <jld@mozilla.com>
2016-08-26 21:29:00 -0400

    Bug 1295622 - Don't crash the content process if a memory report fails. r=njn, a=lizzard

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1185,7 +1185,9 @@ ContentChild::RecvPMemoryReportRequestConstructor(
     rv = actor->Run();
   }
 
-  return !NS_WARN_IF(NS_FAILED(rv));
+  // Bug 1295622: don't kill the process just because this failed.
+  NS_WARN_IF(NS_FAILED(rv));
+  return true;
 }
 
 NS_IMETHODIMP MemoryReportRequestChild::Run()
@@ -1205,9 +1207,12 @@ NS_IMETHODIMP MemoryReportRequestChild::Run()
   RefPtr<MemoryReportFinishedCallback> finished =
     new MemoryReportFinishedCallback(this);
 
-  return mgr->GetReportsForThisProcessExtended(cb, nullptr, mAnonymize,
-                                               FileDescriptorToFILE(mDMDFile, "wb"),
-                                               finished, nullptr);
+  nsresult rv =
+    mgr->GetReportsForThisProcessExtended(cb, nullptr, mAnonymize,
+                                          FileDescriptorToFILE(mDMDFile, "wb"),
+                                          finished, nullptr);
+  NS_WARN_IF(NS_FAILED(rv));
+  return rv;
 }
 
 bool
