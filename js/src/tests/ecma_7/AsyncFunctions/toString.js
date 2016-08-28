8f486dc57ca0e15dd185325153feef9570b614e6
Tooru Fujisawa <arai_a@mac.com>
2016-08-29 02:06:19 +0900

    Bug 1185106 - Part 13: Support async function in Function.prototype.toString. r=till
    MozReview-Commit-ID: 1CwKhHKOLhk

---
+++ js/src/tests/ecma_7/AsyncFunctions/toString.js
@@ -0,0 +1,24 @@
+var BUGNUMBER = 1185106;
+var summary = "async function toString";
+
+print(BUGNUMBER + ": " + summary);
+
+async function f1(a, b, c) { await a; }
+
+assertEq(f1.toString(),
+         "async function f1(a, b, c) { await a; }");
+
+assertEq(async function (a, b, c) { await a; }.toString(),
+         "async function (a, b, c) { await a; }");
+
+assertEq((async (a, b, c) => await a).toString(),
+         "async (a, b, c) => await a");
+
+assertEq((async (a, b, c) => { await a; }).toString(),
+         "async (a, b, c) => { await a; }");
+
+assertEq({ async foo(a, b, c) { await a; } }.foo.toString(),
+         "async function foo(a, b, c) { await a; }");
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
