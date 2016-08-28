b81095ad3801db0e2b4fc75535051d07794f19ee
Tooru Fujisawa <arai_a@mac.com>
2016-08-28 23:58:29 +0900

    Bug 1185106 - Part 9.3: Add parser test for yield in async function statement in export default. r=till
    MozReview-Commit-ID: 3OsdBSSyz64

--- js/src/tests/ecma_7/AsyncFunctions/syntax-modules.js
+++ js/src/tests/ecma_7/AsyncFunctions/syntax-modules.js
@@ -9,6 +9,11 @@ if (typeof parseModule === "function") {
     assertThrows(() => parseModule("var await = 5;"), SyntaxError);
     assertThrows(() => parseModule("export var await;"), SyntaxError);
     assertThrows(() => parseModule("async function f() { function g() { await 3; } }"), SyntaxError);
+
+    if (typeof Reflect !== "undefined" && Reflect.parse) {
+        assertThrows(() => Reflect.parse("export default async function() { yield; }", { target: "module" }), SyntaxError);
+        assertThrows(() => Reflect.parse("export default async function() { yield = 1; }", { target: "module" }), SyntaxError);
+    }
 }
 
 if (typeof reportCompare === "function")
