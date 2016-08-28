655b98b5614ab5a09787f94435629a1a1abb5b01
Mariusz Kierski <mkierski@mozilla.com>
2016-08-28 23:58:29 +0900

    Bug 1185106 - Part 7.2: Add parser test for async method. r=efaust,till
    MozReview-Commit-ID: Lv2sjpSILhz

--- js/src/tests/js1_8_5/reflect-parse/async.js
+++ js/src/tests/js1_8_5/reflect-parse/async.js
@@ -7,6 +7,12 @@ assertDecl("async function foo() {}", asyncFunDecl(ident("foo"), [], blockStmt([
 assertExpr("(async function() {})", asyncFunExpr(null, [], blockStmt([])));
 assertExpr("(async function foo() {})", asyncFunExpr(ident("foo"), [], blockStmt([])));
 
+// async method.
+assertExpr("({ async foo() {} })", objExpr([{ key: ident("foo"), value: asyncFunExpr(ident("foo"), [], blockStmt([]))}]));
+
+assertStmt("class C { async foo() {} }", classStmt(ident("C"), null, [classMethod(ident("foo"), asyncFunExpr(ident("foo"), [], blockStmt([])), "method", false)]));
+assertStmt("class C { static async foo() {} }", classStmt(ident("C"), null, [classMethod(ident("foo"), asyncFunExpr(ident("foo"), [], blockStmt([])), "method", true)]));
+
 // await expression.
 assertDecl("async function foo() { await bar }", asyncFunDecl(ident("foo"), [], blockStmt([exprStmt(unExpr("await", ident("bar")))])));
 
