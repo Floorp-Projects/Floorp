3a838dfd988ecd05965f078615a6f94b2c2a06ff
Dan Glastonbury <dglastonbury@mozilla.com>
2016-08-29 10:41:02 +0800

    Bug 1262053 - part1 : unblock window's media when the page was first visited. r=baku
    MozReview-Commit-ID: 5g5cYdgC8sg

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5532,6 +5532,7 @@ pref("dom.webkitBlink.dirPicker.enabled", true);
 pref("dom.webkitBlink.filesystem.enabled", true);
 #endif
 
+pref("media.block-autoplay-until-in-foreground", true);
 #ifdef MOZ_STYLO
 // Is the Servo-backed style system enabled?
 pref("layout.css.servo.enabled", true);
