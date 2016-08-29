a2aef59386e7a5a89744651a17293017ededc490
Jan Odvarko <odvarko@gmail.com>
2016-08-29 17:07:24 +0200

    Bug 1284839 - Reps: Handle spaces between title and rep. r=linclark

--- devtools/client/shared/components/reps/object-with-text.js
+++ devtools/client/shared/components/reps/object-with-text.js
@@ -31,7 +31,7 @@ define(function (require, exports, module) {
         return span({className: "objectBox"},
           this.props.objectLink({
             object: grip
-          }, this.getType(grip))
+          }, this.getType(grip) + " ")
         );
       }
       return "";
