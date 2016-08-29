018c1a1d88baf9fb575bb02ab60d52fdf24e4910
Alfredo.Yang <ayang@mozilla.com>
2016-08-29 14:37:22 +0800

    Bug 1240412 - add VP9-in-MP4 support to Rust demuxer. r=rillian
    MozReview-Commit-ID: 1Yq8OOV3kvM

--- dom/media/platforms/agnostic/VPXDecoder.cpp
+++ dom/media/platforms/agnostic/VPXDecoder.cpp
@@ -28,6 +28,8 @@ static int MimeTypeToCodec(const nsACString& aMimeType)
     return VPXDecoder::Codec::VP8;
   } else if (aMimeType.EqualsLiteral("video/webm; codecs=vp9")) {
     return VPXDecoder::Codec::VP9;
+  } else if (aMimeType.EqualsLiteral("video/vp9")) {
+    return VPXDecoder::Codec::VP9;
   }
   return -1;
 }
@@ -230,7 +232,9 @@ VPXDecoder::IsVPX(const nsACString& aMimeType, uint8_t aCodecMask)
   return ((aCodecMask & VPXDecoder::VP8) &&
           aMimeType.EqualsLiteral("video/webm; codecs=vp8")) ||
          ((aCodecMask & VPXDecoder::VP9) &&
-          aMimeType.EqualsLiteral("video/webm; codecs=vp9"));
+          aMimeType.EqualsLiteral("video/webm; codecs=vp9")) ||
+         ((aCodecMask & VPXDecoder::VP9) &&
+          aMimeType.EqualsLiteral("video/vp9"));
 }
 
 /* static */
