9de1898bc2dcc3cb01dfafcaa42a2b5be7826082
Haik Aftandilian <haftandilian@mozilla.com>
2016-08-30 13:32:21 -0700

    Bug 1290619 - Content sandbox rules should use actual profile directory, not Profiles/*/ regex's; r=jimm
    Passes the profile dir to the content process as a -profile CLI
    option so that the correct profile dir can be used in the OS X content
    sandbox rules.  Only enabled on OS X for now.
    
    On Nightly, profile directories will now be read/write protected
    from the content process (apart from a few profile subdirectories) even
    when they don't reside in ~/Library.
    
    xpcshell tests invoke the content process without providing a
    profile directory. In that case, we don't need to add filesystem
    profile dir. read/write exclusion rules to the sandbox.
    
    This patch adds two new macros to the content sandbox rule set:
    |profileDir| holds the path to the profile or the emptry string;
    |hasProfileDir| is a boolean (1 or 0) that indicates whether or
    not the profile directory rules should be added. If |hasProfileDir|
    is 0, profile directory exclusion rules don't need to be added
    and |profileDir| is not used.
    
    MozReview-Commit-ID: rrTcQwTNdT
    
    --HG--
    extra : rebase_source : 3d5b612c8eb3a1d0da028eba277cd9d6f0c9ac00

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -23,6 +23,10 @@
 #include "prenv.h"
 #include "nsXPCOMPrivate.h"
 
+#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
+#include "nsAppDirectoryServiceDefs.h"
+#endif
+
 #include "nsExceptionHandler.h"
 
 #include "nsDirectoryServiceDefs.h"
@@ -608,6 +612,20 @@ AddAppDirToCommandLine(std::vector<std::string>& aCmdLine)
         aCmdLine.push_back(path.get());
 #endif
       }
+
+#if defined(XP_MACOSX) && defined(MOZ_CONTENT_SANDBOX)
+      // Full path to the profile dir
+      nsCOMPtr<nsIFile> profileDir;
+      rv = directoryService->Get(NS_APP_USER_PROFILE_50_DIR,
+                                 NS_GET_IID(nsIFile),
+                                 getter_AddRefs(profileDir));
+      if (NS_SUCCEEDED(rv)) {
+        nsAutoCString path;
+        MOZ_ALWAYS_SUCCEEDS(profileDir->GetNativePath(path));
+        aCmdLine.push_back("-profile");
+        aCmdLine.push_back(path.get());
+      }
+#endif
     }
   }
 }
