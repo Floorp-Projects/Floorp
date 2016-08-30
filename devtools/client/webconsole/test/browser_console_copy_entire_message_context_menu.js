60288f3e08dcef22a7cecb2425d9d4b2bc1e9948
Jarda Snajdr <jsnajdr@gmail.com>
2016-08-30 12:34:22 +0200

    Bug 1298225 - Format clipboard text of console stack traces into multiple lines. r=bgrins, a=ritu
    MozReview-Commit-ID: HkCFEwjhCwx

--- devtools/client/webconsole/test/browser_console_copy_entire_message_context_menu.js
+++ devtools/client/webconsole/test/browser_console_copy_entire_message_context_menu.js
@@ -3,67 +3,95 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/* globals goDoCommand */
+
 "use strict";
 
 // Test copying of the entire console message when right-clicked
 // with no other text selected. See Bug 1100562.
 
-function test() {
+add_task(function* () {
   let hud;
   let outputNode;
   let contextMenu;
 
-  const TEST_URI = "http://example.com/browser/devtools/client/webconsole/" +
-                   "test/test-console.html";
+  const TEST_URI = "http://example.com/browser/devtools/client/webconsole/test/test-console.html";
 
-  Task.spawn(runner).then(finishTest);
+  const { tab, browser } = yield loadTab(TEST_URI);
+  hud = yield openConsole(tab);
+  outputNode = hud.outputNode;
+  contextMenu = hud.iframeWindow.document.getElementById("output-contextmenu");
 
-  function* runner() {
-    const {tab} = yield loadTab(TEST_URI);
-    hud = yield openConsole(tab);
-    outputNode = hud.outputNode;
-    contextMenu = hud.iframeWindow.document.getElementById("output-contextmenu");
+  registerCleanupFunction(() => {
+    hud = outputNode = contextMenu = null;
+  });
 
-    registerCleanupFunction(() => {
-      hud = outputNode = contextMenu = null;
-    });
+  hud.jsterm.clearOutput();
 
-    hud.jsterm.clearOutput();
-    content.console.log("bug 1100562");
+  yield ContentTask.spawn(browser, {}, function* () {
+    let button = content.document.getElementById("testTrace");
+    button.click();
+  });
 
-    let [results] = yield waitForMessages({
-      webconsole: hud,
-      messages: [{
+  let results = yield waitForMessages({
+    webconsole: hud,
+    messages: [
+      {
         text: "bug 1100562",
         category: CATEGORY_WEBDEV,
         severity: SEVERITY_LOG,
-      }]
-    });
+        lines: 1,
+      },
+      {
+        name: "console.trace output",
+        consoleTrace: true,
+        lines: 3,
+      },
+    ]
+  });
 
-    outputNode.focus();
-    let message = [...results.matched][0];
+  outputNode.focus();
 
-    yield waitForContextMenu(contextMenu, message, copyFromPopup,
-                             testContextMenuCopy);
+  for (let result of results) {
+    let message = [...result.matched][0];
 
-    function copyFromPopup() {
+    yield waitForContextMenu(contextMenu, message, () => {
       let copyItem = contextMenu.querySelector("#cMenu_copy");
       copyItem.doCommand();
 
       let controller = top.document.commandDispatcher
                                    .getControllerForCommand("cmd_copy");
       is(controller.isCommandEnabled("cmd_copy"), true, "cmd_copy is enabled");
-    }
+    });
 
-    function testContextMenuCopy() {
-      waitForClipboard((str) => {
-        return message.textContent.trim() == str.trim();
-      }, () => {
-        goDoCommand("cmd_copy");
-      }, () => {}, () => {}
-      );
-    }
+    let clipboardText;
 
-    yield closeConsole(tab);
+    yield waitForClipboardPromise(
+      () => goDoCommand("cmd_copy"),
+      (str) => {
+        clipboardText = str;
+        return message.textContent == clipboardText;
+      }
+    );
+
+    ok(clipboardText, "Clipboard text was found and saved");
+
+    let lines = clipboardText.split("\n");
+    ok(lines.length > 0, "There is at least one newline in the message");
+    is(lines.pop(), "", "There is a newline at the end");
+    is(lines.length, result.lines, `There are ${result.lines} lines in the message`);
+
+    // Test the first line for "timestamp message repeat file:line"
+    let firstLine = lines.shift();
+    ok(/^[\d:.]+ .+ \d+ .+:\d+$/.test(firstLine),
+      "The message's first line has the right format");
+
+    // Test the remaining lines (stack trace) for "TABfunctionName sourceURL:line:col"
+    for (let line of lines) {
+      ok(/^\t.+ .+:\d+:\d+$/.test(line), "The stack trace line has the right format");
+    }
   }
-}
+
+  yield closeConsole(tab);
+  yield finishTest();
+});
