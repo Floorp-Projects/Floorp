3c93a880bbe9e2590049fc91398b7144a951d67f
Ethan Glasser-Camp <eglassercamp@mozilla.com>
2016-08-30 12:19:19 -0400

    Bug 1299208 - Update kinto.js to v4.0.3. r=MattN
    MozReview-Commit-ID: HlLbIpRri19

--- services/common/kinto-offline-client.js
+++ services/common/kinto-offline-client.js
@@ -20,7 +20,7 @@
 this.EXPORTED_SYMBOLS = ["loadKinto"];
 
 /*
- * Version 4.0.2 - ef8a96f
+ * Version 4.0.3 - 8100433
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.loadKinto = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -2644,7 +2644,7 @@ function deepEqual(a, b) {
   if (typeof a !== typeof b) {
     return false;
   }
-  if (!(a instanceof Object) || !(b instanceof Object)) {
+  if (!(a && typeof a == "object") || !(b && typeof b == "object")) {
     return false;
   }
   if (Object.keys(a).length !== Object.keys(b).length) {
