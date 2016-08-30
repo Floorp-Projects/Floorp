6ab6a5de8871ebb78f3329b915aa4b49678a0554
Andreas Tolfsen <ato@mozilla.com>
2016-08-30 12:46:01 +0100

    Bug 1244425 - Avoid CPOW when setting file array on <input type=file>; r=automatedtester a=testonly
    This removes the need to use a CPOW when sending keys to <input type=file>
    elements.  It was previously not possible to constructo File objects in
    privileged content space, but this now appears fixed.
    
    MozReview-Commit-ID: 8XOVsDdypwC

--- testing/marionette/frame.js
+++ testing/marionette/frame.js
@@ -223,7 +223,6 @@ frame.Manager = class {
     mm.addWeakMessageListener("Marionette:getImportedScripts", this.driver.importedScripts);
     mm.addWeakMessageListener("Marionette:register", this.driver);
     mm.addWeakMessageListener("Marionette:listenersAttached", this.driver);
-    mm.addWeakMessageListener("Marionette:getFiles", this.driver);
     mm.addWeakMessageListener("MarionetteFrame:handleModal", this);
     mm.addWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
     mm.addWeakMessageListener("MarionetteFrame:getInterruptedState", this);
@@ -252,7 +251,6 @@ frame.Manager = class {
     mm.removeWeakMessageListener("Marionette:getImportedScripts", this.driver.importedScripts);
     mm.removeWeakMessageListener("Marionette:listenersAttached", this.driver);
     mm.removeWeakMessageListener("Marionette:register", this.driver);
-    mm.removeWeakMessageListener("Marionette:getFiles", this.driver);
     mm.removeWeakMessageListener("MarionetteFrame:handleModal", this);
     mm.removeWeakMessageListener("MarionetteFrame:getCurrentFrameId", this);
   }
