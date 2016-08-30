fa9d8fdd22093103a3c554e475eefca970f0e3bb
Nathan Froyd <froydnj@gmail.com>
2016-08-29 20:41:22 -0400

    Bug 1297801 - part 2 - make IPDL Transition functions take an inout parameter for State; r=billm
    The calls to IPDL Transition() functions consistently look like:
    
      Transition(VAR, ..., &VAR);
    
    We can save space by only passing &VAR and deriving the state we're
    coming from by loading VAR in Transition itself.  It's not great using
    inout parameters here, but we call Transition enough times that this
    change saves a reasonable amount of space: about 10K on x86-64 Linux.
    The unsightliness of inout parameters here is lessened by the only
    callers of Transition being generated code.

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -1797,7 +1797,8 @@ class _GenerateProtocolCode(ipdl.ast.Visitor):
             else:
                 return ExprVar(s.decl.cxxname)
 
-        # bool Transition(State from, Trigger trigger, State* next)
+        # bool Transition(Trigger trigger, State* next)
+        # The state we are transitioning from is stored in *next.
         fromvar = ExprVar('from')
         triggervar = ExprVar('trigger')
         nextvar = ExprVar('next')
@@ -1806,8 +1807,7 @@ class _GenerateProtocolCode(ipdl.ast.Visitor):
 
         transitionfunc = FunctionDefn(FunctionDecl(
             'Transition',
-            params=[ Decl(Type('State'), fromvar.name),
-                     Decl(Type('mozilla::ipc::Trigger'), triggervar.name),
+            params=[ Decl(Type('mozilla::ipc::Trigger'), triggervar.name),
                      Decl(Type('State', ptr=1), nextvar.name) ],
             ret=Type.BOOL))
 
@@ -1902,6 +1902,8 @@ class _GenerateProtocolCode(ipdl.ast.Visitor):
         if usesend or userecv:
             transitionfunc.addstmt(Whitespace.NL)
 
+        transitionfunc.addstmt(StmtDecl(Decl(Type('State'), fromvar.name),
+                                        init=ExprDeref(nextvar)))
         transitionfunc.addstmt(fromswitch)
         # all --> Error transitions break to here.  But only insert this
         # block if there is any possibility of such transitions.
@@ -5429,8 +5431,7 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
         ifbad = StmtIf(ExprNot(
             ExprCall(
                 ExprVar(self.protocol.name +'::Transition'),
-                args=[ stateexpr,
-                       ExprCall(ExprVar('Trigger'),
+                args=[ ExprCall(ExprVar('Trigger'),
                                 args=[ action, ExprVar(msgid) ]),
                        ExprAddrOf(stateexpr) ])))
         ifbad.addifstmts(_badTransition())
