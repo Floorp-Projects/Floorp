fcffa74c5b03b522477d2af50d8f3f2768272acb
Nicolas Silva <nsilva@mozilla.com>
2016-08-30 13:48:20 +0200

    Bug 1088300 - Remove mCompositableClient from TileClient, pass the compositable and layer by reference to remove the possibility of unexpectedly storing null pointers. r=milan

--- gfx/layers/client/ClientTiledPaintedLayer.cpp
+++ gfx/layers/client/ClientTiledPaintedLayer.cpp
@@ -445,10 +445,10 @@ ClientTiledPaintedLayer::RenderLayer()
 
   if (!mContentClient) {
     if (wantSingleTiledContentClient) {
-      mContentClient = new SingleTiledContentClient(this, ClientManager());
+      mContentClient = new SingleTiledContentClient(*this, ClientManager());
       mHaveSingleTiledContentClient = true;
     } else {
-      mContentClient = new MultiTiledContentClient(this, ClientManager());
+      mContentClient = new MultiTiledContentClient(*this, ClientManager());
       mHaveSingleTiledContentClient = false;
     }
 
