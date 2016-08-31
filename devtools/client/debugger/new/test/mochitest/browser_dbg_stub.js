2b4c2e5994d0b131819707ab41d3b57f2dd9630c
James Long <longster@gmail.com>
2016-08-31 17:21:24 -0400

    Bug 1294686 - stub out mochitest directory for new debugger r=bgrins

---
+++ devtools/client/debugger/new/test/mochitest/browser_dbg_stub.js
@@ -0,0 +1,7 @@
+
+add_task(function*() {
+  ok(true,
+     "This is a stub so that we can run the new debugger tests " +
+     "by copying them in here. This will go away once we land " +
+     "the initial suite of new tests");
+});
