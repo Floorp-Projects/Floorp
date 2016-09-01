5f8dfa9ba68d8e8a33e7898d3ea352eb4f77547c
Andrea Marchesini <amarchesini@mozilla.com>
2016-08-31 21:33:05 -0700

    Bug 790919 - Don't dispatch close event in Workers, r=bkelly

--- dom/workers/test/terminate_worker.js
+++ dom/workers/test/terminate_worker.js
@@ -2,10 +2,6 @@
  * Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
-onclose = function() {
-  postMessage("Closed!");
-}
-
 onmessage = function(event) {
   throw "No messages should reach me!";
 }
