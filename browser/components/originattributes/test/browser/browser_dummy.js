34b99b6ead331ad1124dd960edb8b2c27fd5eb24
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-09-02 15:33:51 +0200

    Backed out changeset 57a1ca19b38c (bug 1260931)

---
+++ browser/components/originattributes/test/browser/browser_dummy.js
@@ -0,0 +1,10 @@
+/*
+ * This is a dummy test case which makes this could be built.
+ * Should be removed after actual tests landed.
+ */
+
+"use strict";
+
+add_task(function* () {
+  ok(true, "Make this test pass anyway.");
+});
