720ace84cd605eea825e1fbddd85317d1553c5dc
Alessio Placitelli <alessio.placitelli@gmail.com>
2016-09-02 03:44:00 -0400

    Bug 1288709 - Split the browser_UsageTelemetry.js test. r=gijs a=testonly
    MozReview-Commit-ID: IykhSB7RtSH

--- browser/modules/test/browser_UsageTelemetry.js
+++ browser/modules/test/browser_UsageTelemetry.js
@@ -44,15 +44,6 @@ function browserLocationChanged(browser) {
   });
 }
 
-function promiseBrowserStateRestored() {
-  return new Promise(resolve => {
-     Services.obs.addObserver(function observer(aSubject, aTopic) {
-       Services.obs.removeObserver(observer, "sessionstore-browser-state-restored");
-       resolve();
-     }, "sessionstore-browser-state-restored", false);
-  });
-}
-
 /**
  * An helper that checks the value of a scalar if it's expected to be > 0,
  * otherwise makes sure that the scalar it's not reported.
@@ -250,75 +241,4 @@ add_task(function* test_URIAndDomainCounts() {
   // Clean up.
   yield BrowserTestUtils.removeTab(firstTab);
   yield BrowserTestUtils.closeWindow(newWin);
-});
-
-add_task(function* test_privateMode() {
-  // Let's reset the counts.
-  Services.telemetry.clearScalars();
-
-  // Open a private window and load a website in it.
-  let privateWin = yield BrowserTestUtils.openNewBrowserWindow({private: true});
-  yield BrowserTestUtils.loadURI(privateWin.gBrowser.selectedBrowser, "http://example.com/");
-  yield BrowserTestUtils.browserLoaded(privateWin.gBrowser.selectedBrowser);
-
-  // Check that tab and window count is recorded.
-  const scalars =
-    Services.telemetry.snapshotScalars(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN);
-
-  ok(!(TOTAL_URI_COUNT in scalars), "We should not track URIs in private mode.");
-  ok(!(UNIQUE_DOMAINS_COUNT in scalars), "We should not track unique domains in private mode.");
-  is(scalars[TAB_EVENT_COUNT], 1, "The number of open tab event count must match the expected value.");
-  is(scalars[MAX_CONCURRENT_TABS], 2, "The maximum tab count must match the expected value.");
-  is(scalars[WINDOW_OPEN_COUNT], 1, "The number of window open event count must match the expected value.");
-  is(scalars[MAX_CONCURRENT_WINDOWS], 2, "The maximum window count must match the expected value.");
-
-  // Clean up.
-  yield BrowserTestUtils.closeWindow(privateWin);
-});
-
-add_task(function* test_sessionRestore() {
-  const PREF_RESTORE_ON_DEMAND = "browser.sessionstore.restore_on_demand";
-  Services.prefs.setBoolPref(PREF_RESTORE_ON_DEMAND, false);
-  registerCleanupFunction(() => {
-    Services.prefs.clearUserPref(PREF_RESTORE_ON_DEMAND);
-  });
-
-  // Let's reset the counts.
-  Services.telemetry.clearScalars();
-
-  // The first window will be put into the already open window and the second
-  // window will be opened with _openWindowWithState, which is the source of the problem.
-  const state = {
-    windows: [
-      {
-        tabs: [
-          { entries: [{ url: "http://example.org" }], extData: { "uniq": 3785 } }
-        ],
-        selected: 1
-      }
-    ]
-  };
-
-  // Save the current session.
-  let SessionStore =
-    Cu.import("resource:///modules/sessionstore/SessionStore.jsm", {}).SessionStore;
-  let backupState = SessionStore.getBrowserState();
-
-  // Load the custom state and wait for SSTabRestored, as we want to make sure
-  // that the URI counting code was hit.
-  let tabRestored = BrowserTestUtils.waitForEvent(gBrowser.tabContainer, "SSTabRestored");
-  SessionStore.setBrowserState(JSON.stringify(state));
-  yield tabRestored;
-
-  // Check that the URI is not recorded.
-  const scalars =
-    Services.telemetry.snapshotScalars(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN);
-
-  ok(!(TOTAL_URI_COUNT in scalars), "We should not track URIs from restored sessions.");
-  ok(!(UNIQUE_DOMAINS_COUNT in scalars), "We should not track unique domains from restored sessions.");
-
-  // Restore the original session and cleanup.
-  let sessionRestored = promiseBrowserStateRestored();
-  SessionStore.setBrowserState(JSON.stringify(state));
-  yield sessionRestored;
 });
