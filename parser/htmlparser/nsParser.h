363a5143aa4d809de77455c95ecb5a065b48afca
Boris Zbarsky <bzbarsky@mit.edu>
2016-09-02 17:55:38 -0400

    Bug 1297125.  Make sure the parser insertion point is defined when firing the load event for external <scripts> or firing the error event on a failed external script load (but not other cases, like bogus script URL).  r=hsivonen
    If we have a creator parser, then we were a parser-inserted script and should
    presumably be able to set a valid insertion point when we run or fire our
    load/error events.  For the error event case, we do this in
    nsScriptElement::ScriptAvailable, so that async error events due to things like
    bogus script URLs do not end up with a valid insertion point.  For the load
    event case, we just do this in ScriptEvaluated directly.
    
    ScriptEvaluated is called while the scriptloader has our script set as the
    current parser-inserted script.  But for the error event case we need to
    maintain that state around the ScriptAvailable call that will fire the event.

--- parser/htmlparser/nsParser.h
+++ parser/htmlparser/nsParser.h
@@ -258,12 +258,12 @@ class nsParser final : public nsIParser,
     /**
      * No-op.
      */
-    virtual void BeginEvaluatingParserInsertedScript() override;
+    virtual void PushDefinedInsertionPoint() override;
 
     /**
      * No-op.
      */
-    virtual void EndEvaluatingParserInsertedScript() override;
+    virtual void PopDefinedInsertionPoint() override;
 
     /**
      * No-op.
