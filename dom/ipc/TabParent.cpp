2426d43cfae540521932d14c53ad190955346c1a
Kan-Ru Chen <kanru@kanru.info>
2016-09-02 16:03:19 +0800

    Bug 1269036 - Also destroy TabParent properly in case of mozbrowser. r=smaug
    MozReview-Commit-ID: Co0HL40b4q7

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -2787,6 +2787,9 @@ TabParent::RecvBrowserFrameOpenWindow(PBrowserParent* aOpener,
                                         this, aRenderFrame, aURL, aName, aFeatures,
                                         aTextureFactoryIdentifier, aLayersId);
   *aOutWindowOpened = (opened == BrowserElementParent::OPEN_WINDOW_ADDED);
+  if (!*aOutWindowOpened) {
+    Destroy();
+  }
   return true;
 }
 
