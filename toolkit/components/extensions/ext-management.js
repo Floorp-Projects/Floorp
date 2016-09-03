2b1224c670d1b17900e851b8d93e43dfc33a96ac
Phil Ringnalda <philringnalda@gmail.com>
2016-09-03 11:03:29 -0700

    Back out 73e11489018d (bug 1220136) for Windows failures in test_ext_management_uninstall_self.js

--- toolkit/components/extensions/ext-management.js
+++ toolkit/components/extensions/ext-management.js
@@ -2,24 +2,8 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-
-XPCOMUtils.defineLazyGetter(this, "strBundle", function() {
-  const stringSvc = Cc["@mozilla.org/intl/stringbundle;1"].getService(Ci.nsIStringBundleService);
-  return stringSvc.createBundle("chrome://global/locale/extensions.properties");
-});
 XPCOMUtils.defineLazyModuleGetter(this, "AddonManager",
                                   "resource://gre/modules/AddonManager.jsm");
-XPCOMUtils.defineLazyServiceGetter(this, "promptService",
-                                   "@mozilla.org/embedcomp/prompt-service;1",
-                                   "nsIPromptService");
-
-function _(key, ...args) {
-  if (args.length) {
-    return strBundle.formatStringFromName(key, args, args.length);
-  }
-  return strBundle.GetStringFromName(key);
-}
 
 function installType(addon) {
   if (addon.temporarilyInstalled) {
@@ -68,41 +52,10 @@ extensions.registerSchemaAPI("management", "addon_parent", context => {
             }
 
             resolve(extInfo);
-          } catch (err) {
-            reject(err);
+          } catch (e) {
+            reject(e);
           }
         }));
-      },
-
-      uninstallSelf: function(options) {
-        return new Promise((resolve, reject) => {
-          if (options && options.showConfirmDialog) {
-            let message = _("uninstall.confirmation.message", extension.name);
-            if (options.dialogMessage) {
-              message = `${options.dialogMessage}\n${message}`;
-            }
-            let title = _("uninstall.confirmation.title", extension.name);
-            let buttonFlags = promptService.BUTTON_POS_0 * promptService.BUTTON_TITLE_IS_STRING +
-                              promptService.BUTTON_POS_1 * promptService.BUTTON_TITLE_IS_STRING;
-            let button0Title = _("uninstall.confirmation.button-0.label");
-            let button1Title = _("uninstall.confirmation.button-1.label");
-            let response = promptService.confirmEx(null, title, message, buttonFlags, button0Title, button1Title, null, null, {value: 0});
-            if (response == 1) {
-              return reject({message: "User cancelled uninstall of extension"});
-            }
-          }
-          AddonManager.getAddonByID(extension.id, addon => {
-            let canUninstall = Boolean(addon.permissions & AddonManager.PERM_CAN_UNINSTALL);
-            if (!canUninstall) {
-              return reject({message: "The add-on cannot be uninstalled"});
-            }
-            try {
-              addon.uninstall();
-            } catch (err) {
-              return reject(err);
-            }
-          });
-        });
       },
     },
   };
