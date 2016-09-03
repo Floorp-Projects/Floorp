f4d62b47170fcf4f741e7b7e7c0c0803349c873e
Phil Ringnalda <philringnalda@gmail.com>
2016-09-03 09:09:44 -0700

    Bug 1271074 - Increase the timeout of browser_vimemacs.js (and remove the previous 'increase' that tried to decrease it) a=testonly

--- devtools/client/sourceeditor/test/browser_vimemacs.js
+++ devtools/client/sourceeditor/test/browser_vimemacs.js
@@ -3,14 +3,12 @@
 
 "use strict";
 
-requestLongerTimeout(2);
-
 const URI = "chrome://mochitests/content/browser/devtools/client" +
             "/sourceeditor/test/codemirror/vimemacs.html";
 loadHelperScript("helper_codemirror_runner.js");
 
 function test() {
-  requestLongerTimeout(3);
+  requestLongerTimeout(4);
   waitForExplicitFinish();
 
   let tab = gBrowser.addTab();
