b202c6a20f99034cb11a8f0c239f0fe1548347a5
Bryce Van Dyk <bvandyk@mozilla.com>
2016-09-05 11:02:07 +1200

    Bug 1300442 - Housekeeping and QoL changes: code style, extra logging on VP. r=maja_zf
    - Use format() instead of old style formatting (%s, etc).
    - Remove unneeded positional args on format strings.
    - Break some long lines for pep8 conformance.
    - Use brackets instead of \ to continue long lines.
    - Log interval on video puppeteer.
    - Remove an unneeded media source check. We have explicit media source checks
    in tests, and the media source prefix has changed, rendering the check broken.
    
    MozReview-Commit-ID: 4FPVoOD0P5B

--- dom/media/test/external/external_media_tests/playback/test_eme_playback.py
+++ dom/media/test/external/external_media_tests/playback/test_eme_playback.py
@@ -2,7 +2,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from external_media_harness.testcase import MediaTestCase, VideoPlaybackTestsMixin, EMESetupMixin
+from external_media_harness.testcase import (
+    MediaTestCase,
+    VideoPlaybackTestsMixin,
+    EMESetupMixin
+)
 
 
 class TestEMEPlayback(MediaTestCase, VideoPlaybackTestsMixin, EMESetupMixin):
