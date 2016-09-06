0065d73bb4f636ed9221f99de8434b2d5775d233
Andrea Marchesini <amarchesini@mozilla.com>
2016-09-06 13:19:01 +0200

    Bug 1299480 - Fix regression in the context menu when containers are in used, r=gijs

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -151,7 +151,7 @@ nsContextMenu.prototype = {
       inContainer = true;
       var item = document.getElementById("context-openlinkincontainertab");
 
-      item.setAttribute("usercontextid", userContextId);
+      item.setAttribute("data-usercontextid", userContextId);
 
       var label = ContextualIdentityService.getUserContextLabel(userContextId);
       item.setAttribute("label",
@@ -1008,7 +1008,7 @@ nsContextMenu.prototype = {
 
     let params = {
       allowMixedContent: persistAllowMixedContentInChildTab,
-      userContextId: parseInt(event.target.getAttribute('usercontextid'))
+      userContextId: parseInt(event.target.getAttribute('data-usercontextid')),
     };
 
     openLinkIn(this.linkURL, "tab", this._openLinkInParameters(params));
