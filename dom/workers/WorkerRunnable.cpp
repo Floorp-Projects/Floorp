f4227c1a5cbadf91927242006e1c13af3b63f6a9
Andrea Marchesini <amarchesini@mozilla.com>
2016-09-06 04:35:49 +0200

    Bug 1300655 - WorkerProxyToMainThreadRunnable::PostDispatchOnMainThread::ReleaseRunnable should implement Cancel() correctly, r=bkelly

--- dom/workers/WorkerRunnable.cpp
+++ dom/workers/WorkerRunnable.cpp
@@ -716,12 +716,12 @@ WorkerProxyToMainThreadRunnable::PostDispatchOnMainThread()
       MOZ_ASSERT(aRunnable);
     }
 
-    // We must call RunBackOnWorkerThread() also if the runnable is cancelled.
+    // We must call RunBackOnWorkerThread() also if the runnable is canceled.
     nsresult
     Cancel() override
     {
       WorkerRun(nullptr, mWorkerPrivate);
-      return NS_OK;
+      return MainThreadWorkerControlRunnable::Cancel();
     }
 
     virtual bool
