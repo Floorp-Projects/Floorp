95dbb8627b6489855abe9c806718254cf87a654e
Marco Bonardo <mbonardo@mozilla.com>
2016-09-07 17:44:46 +0200

    Bug 1301093 - Part 1: don't wait for a result when we won't get one. r=adw
    MozReview-Commit-ID: E6l9mW3ZoKp
    
    --HG--
    extra : rebase_source : 39144a190f27257b190178308b26fa3f7bea15a2

--- browser/base/content/test/urlbar/browser_autocomplete_enter_race.js
+++ browser/base/content/test/urlbar/browser_autocomplete_enter_race.js
@@ -1,23 +1,43 @@
-add_task(function*() {
-  registerCleanupFunction(function* () {
-    yield PlacesUtils.bookmarks.remove(bm);
-  });
+// The order of these tests matters!
 
+add_task(function* setup () {
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser);
   let bm = yield PlacesUtils.bookmarks.insert({ parentGuid: PlacesUtils.bookmarks.unfiledGuid,
                                                 url: "http://example.com/?q=%s",
                                                 title: "test" });
+  registerCleanupFunction(function* () {
+    yield PlacesUtils.bookmarks.remove(bm);
+    yield BrowserTestUtils.removeTab(tab);
+  });
   yield PlacesUtils.keywords.insert({ keyword: "keyword",
                                       url: "http://example.com/?q=%s" });
+  // Needs at least one success.
+  ok(true, "Setup complete");
+});
 
-  yield new Promise(resolve => waitForFocus(resolve, window));
-
+add_task(function* test_keyword() {
   yield promiseAutocompleteResultPopup("keyword bear");
   gURLBar.focus();
   EventUtils.synthesizeKey("d", {});
   EventUtils.synthesizeKey("VK_RETURN", {});
+  info("wait for the page to load");
+  yield BrowserTestUtils.browserLoaded(gBrowser.selectedTab.linkedBrowser,
+                                      false, "http://example.com/?q=beard");
+});
 
-  yield promiseTabLoadEvent(gBrowser.selectedTab);
-  is(gBrowser.selectedBrowser.currentURI.spec,
-     "http://example.com/?q=beard",
-     "Latest typed characters should have been used");
+add_task(function* test_sametext() {
+  yield promiseAutocompleteResultPopup("example.com", window, true);
+
+  // Simulate re-entering the same text searched the last time. This may happen
+  // through a copy paste, but clipboard handling is not much reliable, so just
+  // fire an input event.
+  info("synthesize input event");
+  let event = document.createEvent("Events");
+  event.initEvent("input", true, true);
+  gURLBar.dispatchEvent(event);
+  EventUtils.synthesizeKey("VK_RETURN", {});
+
+  info("wait for the page to load");
+  yield BrowserTestUtils.browserLoaded(gBrowser.selectedTab.linkedBrowser,
+                                       false, "http://example.com/");
 });
