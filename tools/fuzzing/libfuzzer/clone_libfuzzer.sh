db23b1d0e3173cba9093a104718453be20ceb8ce
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-09-07 18:45:40 +0200

    Backed out changeset 95e68b473e91 (bug 1289194) for failure to process moz.build file. r=backout a=backout
    MozReview-Commit-ID: GhfzNoiE808

--- tools/fuzzing/libfuzzer/clone_libfuzzer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir tmp/
-git clone --no-checkout --depth 1 https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer tmp/
-mv tmp/.git .
-rm -Rf tmp
-git reset --hard HEAD
