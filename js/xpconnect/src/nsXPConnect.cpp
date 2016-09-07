ac4b6dabc5db7afc74a7df440b06afcdb95c95a6
Andrew McCreight <continuation@gmail.com>
2016-09-07 11:49:38 -0700

    Bug 1300830 - Remove XPCJSRuntime::mDetachedWrappedNativeProtoMap. r=mrbkap
    Nothing is ever added to this hash table.
    
    MozReview-Commit-ID: 7gmlBZKNPOc
    
    --HG--
    extra : rebase_source : 9a14e405facedc8148c5dd8e363f98c691e23231

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -83,7 +83,6 @@ nsXPConnect::~nsXPConnect()
 
     mShuttingDown = true;
     XPCWrappedNativeScope::SystemIsBeingShutDown();
-    mContext->SystemIsBeingShutDown();
 
     // The above causes us to clean up a bunch of XPConnect data structures,
     // after which point we need to GC to clean everything up. We need to do
