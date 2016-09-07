45b4a6cadd8570c4b6f50faf721cb0f7bd2b95e3
Gabriel Luong <gabriel.luong@gmail.com>
2016-09-07 15:41:51 -0400

    Bug 1249558 - Part 2: Display the grid info bar for a grid area highlight r=pbro

--- devtools/client/inspector/test/browser_inspector_infobar_01.js
+++ devtools/client/inspector/test/browser_inspector_infobar_01.js
@@ -64,26 +64,26 @@ function* testPosition(test, inspector, testActor) {
   yield selectAndHighlightNode(test.selector, inspector);
 
   let position = yield testActor.getHighlighterNodeAttribute(
-    "box-model-nodeinfobar-container", "position");
+    "box-model-infobar-container", "position");
   is(position, test.position, "Node " + test.selector + ": position matches");
 
   let tag = yield testActor.getHighlighterNodeTextContent(
-    "box-model-nodeinfobar-tagname");
+    "box-model-infobar-tagname");
   is(tag, test.tag, "node " + test.selector + ": tagName matches.");
 
   if (test.id) {
     let id = yield testActor.getHighlighterNodeTextContent(
-      "box-model-nodeinfobar-id");
+      "box-model-infobar-id");
     is(id, "#" + test.id, "node " + test.selector + ": id matches.");
   }
 
   let classes = yield testActor.getHighlighterNodeTextContent(
-    "box-model-nodeinfobar-classes");
+    "box-model-infobar-classes");
   is(classes, test.classes, "node " + test.selector + ": classes match.");
 
   if (test.dims) {
     let dims = yield testActor.getHighlighterNodeTextContent(
-      "box-model-nodeinfobar-dimensions");
+      "box-model-infobar-dimensions");
     is(dims, test.dims, "node " + test.selector + ": dims match.");
   }
 }
