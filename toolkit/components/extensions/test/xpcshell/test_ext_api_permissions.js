046c0ab1c6f51e88a7fd7aae6f956d6184eaa28d
Rob Wu <rob@robwu.nl>
2016-09-07 17:10:33 -0700

    Bug 1287007 - Fix test_ext_api_permissions.js r=billm
    "extension.views" will only be available in content, so
    use the "proxy-context-load" event to detect new views.
    
    MozReview-Commit-ID: K86Be5IDk42

--- toolkit/components/extensions/test/xpcshell/test_ext_api_permissions.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_api_permissions.js
@@ -2,6 +2,16 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
+let {Management} = Cu.import("resource://gre/modules/Extension.jsm", {});
+function getNextContext() {
+  return new Promise(resolve => {
+    Management.on("proxy-context-load", function listener(type, context) {
+      Management.off("proxy-context-load", listener);
+      resolve(context);
+    });
+  });
+}
+
 add_task(function* test_storage_api_without_permissions() {
   let extension = ExtensionTestUtils.loadExtension({
     background() {},
@@ -11,9 +21,10 @@ add_task(function* test_storage_api_without_permissions() {
     },
   });
 
+  let contextPromise = getNextContext();
   yield extension.startup();
 
-  let context = Array.from(extension.extension.views)[0];
+  let context = yield contextPromise;
 
   ok(!("storage" in context._unwrappedAPIs),
      "The storage API should not be initialized");
@@ -30,9 +41,10 @@ add_task(function* test_storage_api_with_permissions() {
     },
   });
 
+  let contextPromise = getNextContext();
   yield extension.startup();
 
-  let context = Array.from(extension.extension.views)[0];
+  let context = yield contextPromise;
 
   equal(typeof context._unwrappedAPIs.storage, "object",
         "The storage API should be initialized");
