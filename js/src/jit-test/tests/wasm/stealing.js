3199d33034b26555568cb0e9451ff86875f15fa9
Luke Wagner <luke@mozilla.com>
2016-09-08 13:51:47 -0500

    Bug 1284156 - Throw when attempting to steal/transfer an ArrayBuffer aliasing WebAssembly.Memory (r=sfink)
    MozReview-Commit-ID: ChqSBp0xGWx

---
+++ js/src/jit-test/tests/wasm/stealing.js
@@ -0,0 +1,19 @@
+load(libdir + "wasm.js");
+
+var exp = evalText(`(module
+    (memory 1)
+    (export "mem" memory)
+    (func $f (result i32) (i32.load (i32.const 0)))
+    (export "f" $f)
+)`).exports;
+
+var ab = exp.mem.buffer;
+new Int32Array(ab)[0] = 42;
+
+assertEq(exp.f(), 42);
+
+assertThrowsInstanceOf(() => detachArrayBuffer(ab), Error);
+assertEq(exp.f(), 42);
+
+assertThrowsInstanceOf(() => serialize(ab, [ab]), Error);
+assertEq(exp.f(), 42);
