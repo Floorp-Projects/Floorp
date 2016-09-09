3511b1af7417044da6c1f8056479c56c5700acb7
Iris Hsiao <ihsiao@mozilla.com>
2016-09-09 15:33:19 +0800

    Backed out changeset bb033983fe8d (bug 1301344), this shouldn't be landed, it's for test

--- netwerk/test/gtest/TestStandardURL.cpp
+++ netwerk/test/gtest/TestStandardURL.cpp
@@ -67,30 +67,3 @@ MOZ_GTEST_BENCH(TestStandardURL, Perf, [] {
         url->GetRef(out);
     }
 });
-
-TEST(TestStandardURL, IPv4) {
-    nsCOMPtr<nsIURL> url( do_CreateInstance(NS_STANDARDURL_CONTRACTID) );
-    ASSERT_TRUE(url);
-    ASSERT_EQ(url->SetSpec(NS_LITERAL_CSTRING("http://other.com/")), NS_OK);
-
-    nsAutoCString out;
-
-    ASSERT_EQ(url->GetSpec(out), NS_OK);
-    ASSERT_TRUE(out == NS_LITERAL_CSTRING("http://other.com/"));
-
-    ASSERT_EQ(url->SetSpec(NS_LITERAL_CSTRING("http://%30%78%63%30%2e%30%32%35%30.01%2e")), NS_OK);
-    ASSERT_EQ(url->GetSpec(out), NS_OK);
-    ASSERT_TRUE(out == NS_LITERAL_CSTRING("http://192.168.0.1/"));
-
-    ASSERT_EQ(url->SetSpec(NS_LITERAL_CSTRING("http://0x7f000001")), NS_OK);
-    ASSERT_EQ(url->GetSpec(out), NS_OK);
-    ASSERT_TRUE(out == NS_LITERAL_CSTRING("http://127.0.0.1/"));
-
-    ASSERT_EQ(url->SetSpec(NS_LITERAL_CSTRING("http://00000000000000000000000000177.0000000.0000000.0001")), NS_OK);
-    ASSERT_EQ(url->GetSpec(out), NS_OK);
-    ASSERT_TRUE(out == NS_LITERAL_CSTRING("http://127.0.0.1/"));
-
-    ASSERT_EQ(url->SetSpec(NS_LITERAL_CSTRING("http://0x7f.0x00.0x00.0x01")), NS_OK);
-    ASSERT_EQ(url->GetSpec(out), NS_OK);
-    ASSERT_TRUE(out == NS_LITERAL_CSTRING("http://127.0.0.1/"));
-}
