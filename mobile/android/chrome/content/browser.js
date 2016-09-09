8b77ea4d539e24e10e6c217449dae450e7329a89
Alastor Wu <alwu@mozilla.com>
2016-09-09 09:50:25 +0800

    Bug 1290467 - part3 : notify MediaControlService with event 'MEDIA_PLAYING_CHANGE'. r=sebastian
    The 'MEDIA_PLAYING_CHANGE' is used for controling media control interface and
    the 'AUDIO_PLAYING_CHANGE' is used for showing the tab sound indicator.
    
    MozReview-Commit-ID: 8hZjC77Ju71
    
    --HG--
    extra : rebase_source : 3699ea482e89a5c2535defce8ca2689a180d5c49

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -3567,6 +3567,7 @@ Tab.prototype = {
     this.browser.addEventListener("VideoBindingCast", this, true, true);
 
     Services.obs.addObserver(this, "before-first-paint", false);
+    Services.obs.addObserver(this, "media-playback", false);
 
     // Always intialise new tabs with basic session store data to avoid
     // problems with functions that always expect it to be present
@@ -3676,6 +3677,7 @@ Tab.prototype = {
     this.browser.removeEventListener("VideoBindingCast", this, true, true);
 
     Services.obs.removeObserver(this, "before-first-paint");
+    Services.obs.removeObserver(this, "media-playback", false);
 
     // Make sure the previously selected panel remains selected. The selected panel of a deck is
     // not stable when panels are removed.
@@ -4451,6 +4453,21 @@ Tab.prototype = {
           if (contentDocument instanceof Ci.nsIImageDocument) {
             contentDocument.shrinkToFit();
           }
+        }
+        break;
+
+      case "media-playback":
+        if (!aSubject) {
+          return;
+        }
+
+        let winId = aSubject.QueryInterface(Ci.nsISupportsPRUint64).data;
+        if (this.browser.outerWindowID == winId) {
+          Messaging.sendRequest({
+            type: "Tab:MediaPlaybackChange",
+            tabID: this.id,
+            active: aData === "active"
+          });
         }
         break;
     }
