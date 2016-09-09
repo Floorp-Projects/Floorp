98a0fd63360a093c80eb01d627387f47499d0e20
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-09-09 16:20:09 -0400

    Bug 1302172 - [mozlint] Convert unittest tests to use pytest instead, r=maja_zf
    MozReview-Commit-ID: D4bN62QbkKm
    
    --HG--
    extra : rebase_source : 22ffe05ca78095fc469520b9f85d93585251cd1c

--- python/mozlint/test/test_types.py
+++ python/mozlint/test/test_types.py
@@ -3,76 +3,48 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
-from unittest import TestCase
+import sys
 
-from mozunit import main
+import pytest
 
-from mozlint import LintRoller
 from mozlint.result import ResultContainer
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+@pytest.fixture
+def path(filedir):
+    def _path(name):
+        return os.path.join(filedir, name)
+    return _path
 
 
-class TestLinterTypes(TestCase):
+@pytest.fixture(params=['string.lint', 'regex.lint', 'external.lint'])
+def linter(lintdir, request):
+    return os.path.join(lintdir, request.param)
 
-    def __init__(self, *args, **kwargs):
-        TestCase.__init__(self, *args, **kwargs)
 
-        self.lintdir = os.path.join(here, 'linters')
-        self.filedir = os.path.join(here, 'files')
-        self.files = [os.path.join(self.filedir, f) for f in os.listdir(self.filedir)]
+def test_linter_types(lint, linter, files, path):
+    lint.read(linter)
+    result = lint.roll(files)
+    assert isinstance(result, dict)
+    assert path('foobar.js') in result
+    assert path('no_foobar.js') not in result
 
-    def setUp(self):
-        TestCase.setUp(self)
-        self.lint = LintRoller(root=here)
+    result = result[path('foobar.js')][0]
+    assert isinstance(result, ResultContainer)
 
-    def path(self, name):
-        return os.path.join(self.filedir, name)
+    name = os.path.basename(linter).split('.')[0]
+    assert result.linter.lower().startswith(name)
 
-    def test_string_linter(self):
-        self.lint.read(os.path.join(self.lintdir, 'string.lint'))
-        result = self.lint.roll(self.files)
-        self.assertIsInstance(result, dict)
 
-        self.assertIn(self.path('foobar.js'), result.keys())
-        self.assertNotIn(self.path('no_foobar.js'), result.keys())
+def test_no_filter(lint, lintdir, files):
+    lint.read(os.path.join(lintdir, 'explicit_path.lint'))
+    result = lint.roll(files)
+    assert len(result) == 0
 
-        result = result[self.path('foobar.js')][0]
-        self.assertIsInstance(result, ResultContainer)
-        self.assertEqual(result.linter, 'StringLinter')
-
-    def test_regex_linter(self):
-        self.lint.read(os.path.join(self.lintdir, 'regex.lint'))
-        result = self.lint.roll(self.files)
-        self.assertIsInstance(result, dict)
-        self.assertIn(self.path('foobar.js'), result.keys())
-        self.assertNotIn(self.path('no_foobar.js'), result.keys())
-
-        result = result[self.path('foobar.js')][0]
-        self.assertIsInstance(result, ResultContainer)
-        self.assertEqual(result.linter, 'RegexLinter')
-
-    def test_external_linter(self):
-        self.lint.read(os.path.join(self.lintdir, 'external.lint'))
-        result = self.lint.roll(self.files)
-        self.assertIsInstance(result, dict)
-        self.assertIn(self.path('foobar.js'), result.keys())
-        self.assertNotIn(self.path('no_foobar.js'), result.keys())
-
-        result = result[self.path('foobar.js')][0]
-        self.assertIsInstance(result, ResultContainer)
-        self.assertEqual(result.linter, 'ExternalLinter')
-
-    def test_no_filter(self):
-        self.lint.read(os.path.join(self.lintdir, 'explicit_path.lint'))
-        result = self.lint.roll(self.files)
-        self.assertEqual(len(result), 0)
-
-        self.lint.lintargs['use_filters'] = False
-        result = self.lint.roll(self.files)
-        self.assertEqual(len(result), 2)
+    lint.lintargs['use_filters'] = False
+    result = lint.roll(files)
+    assert len(result) == 2
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(pytest.main(['--verbose', __file__]))
