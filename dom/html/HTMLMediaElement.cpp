ee72ed8ff229daae38eca3681af0a7f1581cfbda
Ryan VanderMeulen <ryanvm@gmail.com>
2016-09-09 14:01:06 -0400

    Backed out 3 changesets (bug 1298594) because they weren't approved for uplift.
    Backed out changeset 0ee7678d527b (bug 1298594)
    Backed out changeset 824368e15957 (bug 1298594)
    Backed out changeset 5a2f2368796f (bug 1298594)

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -2506,6 +2506,7 @@ HTMLMediaElement::HTMLMediaElement(already_AddRefed<mozilla::dom::NodeInfo>& aNo
     mPlayingThroughTheAudioChannelBeforeSeek(false),
     mPausedForInactiveDocumentOrChannel(false),
     mEventDeliveryPaused(false),
+    mWaitingFired(false),
     mIsRunningLoadMethod(false),
     mIsDoingExplicitLoad(false),
     mIsLoadingFromSourceChildren(false),
@@ -4355,6 +4356,11 @@ HTMLMediaElement::UpdateReadyStateInternal()
     if (mFirstFrameLoaded) {
       ChangeReadyState(nsIDOMHTMLMediaElement::HAVE_CURRENT_DATA);
     }
+    if (!mWaitingFired && nextFrameStatus == MediaDecoderOwner::NEXT_FRAME_UNAVAILABLE_BUFFERING) {
+      FireTimeUpdate(false);
+      DispatchAsyncEvent(NS_LITERAL_STRING("waiting"));
+      mWaitingFired = true;
+    }
     return;
   }
 
@@ -4413,23 +4419,8 @@ void HTMLMediaElement::ChangeReadyState(nsMediaReadyState aState)
   UpdateAudioChannelPlayingState();
 
   // Handle raising of "waiting" event during seek (see 4.8.10.9)
-  // or
-  // 4.8.12.7 Ready states:
-  // "If the previous ready state was HAVE_FUTURE_DATA or more, and the new
-  // ready state is HAVE_CURRENT_DATA or less
-  // If the media element was potentially playing before its readyState
-  // attribute changed to a value lower than HAVE_FUTURE_DATA, and the element
-  // has not ended playback, and playback has not stopped due to errors,
-  // paused for user interaction, or paused for in-band content, the user agent
-  // must queue a task to fire a simple event named timeupdate at the element,
-  // and queue a task to fire a simple event named waiting at the element."
   if (mPlayingBeforeSeek &&
       mReadyState < nsIDOMHTMLMediaElement::HAVE_FUTURE_DATA) {
-    DispatchAsyncEvent(NS_LITERAL_STRING("waiting"));
-  } else if (oldState >= nsIDOMHTMLMediaElement::HAVE_FUTURE_DATA &&
-             mReadyState < nsIDOMHTMLMediaElement::HAVE_FUTURE_DATA &&
-             !Paused() && !Ended() && !mError) {
-    FireTimeUpdate(false);
     DispatchAsyncEvent(NS_LITERAL_STRING("waiting"));
   }
 
@@ -4438,6 +4429,10 @@ void HTMLMediaElement::ChangeReadyState(nsMediaReadyState aState)
       !mLoadedDataFired) {
     DispatchAsyncEvent(NS_LITERAL_STRING("loadeddata"));
     mLoadedDataFired = true;
+  }
+
+  if (mReadyState == nsIDOMHTMLMediaElement::HAVE_CURRENT_DATA) {
+    mWaitingFired = false;
   }
 
   if (oldState < nsIDOMHTMLMediaElement::HAVE_FUTURE_DATA &&
