e6af94a77f895e8ce5c71e2cd75ae6b55b0ba2cd
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2016-09-09 23:04:45 -0700

    Bug 1286718 - Revert bug 1149975 to remove the additional login fill doorhanger code. r=johannh
    MozReview-Commit-ID: 7dAZKshpwdC
    
    --HG--
    extra : rebase_source : 526809227b66a7ae79513f7f36b7d4ac137a00ac

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4326,7 +4326,6 @@ pref("signon.autofillForms",                true);
 pref("signon.autologin.proxy",              false);
 pref("signon.formlessCapture.enabled",      true);
 pref("signon.storeWhenAutocompleteOff",     true);
-pref("signon.ui.experimental",              false);
 pref("signon.debug",                        false);
 pref("signon.recipes.path",                 "chrome://passwordmgr/content/recipes.json");
 pref("signon.schemeUpgrades",               false);
