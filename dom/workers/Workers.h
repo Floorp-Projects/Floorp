26cadb42f0c74896fa267b5a146c88e8659ea20c
Tooru Fujisawa <arai_a@mac.com>
2016-09-11 18:15:24 +0900

    Bug 1290337 - Part 16: Replace Value parameter to const Value& in dom. r=smaug

--- dom/workers/Workers.h
+++ dom/workers/Workers.h
@@ -348,7 +348,7 @@ public:
 };
 
 WorkerCrossThreadDispatcher*
-GetWorkerCrossThreadDispatcher(JSContext* aCx, JS::Value aWorker);
+GetWorkerCrossThreadDispatcher(JSContext* aCx, const JS::Value& aWorker);
 
 // Random unique constant to facilitate JSPrincipal debugging
 const uint32_t kJSPrincipalsDebugToken = 0x7e2df9d2;
