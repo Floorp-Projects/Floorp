2f2fe0746ea45e0a0c3d070c1282861cb35d2ca8
Terrence Cole <terrence@mozilla.com>
2016-09-12 11:20:09 -0700

    Bug 1159541 - Use normal paths for TypeSet::Type tracing; r=sfink

--- js/src/jit/JitcodeMap.cpp
+++ js/src/jit/JitcodeMap.cpp
@@ -24,6 +24,8 @@
 
 #include "jsscriptinlines.h"
 
+#include "vm/TypeInference-inl.h"
+
 using mozilla::Maybe;
 
 namespace js {
@@ -898,7 +900,7 @@ JitcodeGlobalEntry::IonEntry::mark(JSTracer* trc)
          iter != optsAllTypes_->end(); iter++)
     {
         if (ShouldMarkProvider::ShouldMark(&iter->type)) {
-            TypeSet::MarkTypeUnbarriered(trc, &iter->type, "jitcodeglobaltable-ionentry-type");
+            iter->type.trace(trc);
             markedAny = true;
         }
         if (iter->hasAllocationSite() && ShouldMarkProvider::ShouldMark(&iter->script)) {
