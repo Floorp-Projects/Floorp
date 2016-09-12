21998f99be4990fda20ac0c841498354e0eaff35
Andrea Marchesini <amarchesini@mozilla.com>
2016-09-12 12:19:01 +0200

    Bug 1301094 - Part 3: Test for Blob+XHR+FormData+Workers. r=smaug, a=ritu

---
+++ dom/workers/test/worker_bug1301094.js
@@ -0,0 +1,11 @@
+onmessage = function(e) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", 'worker_bug1301094.js', false);
+  xhr.onload = function() {
+    self.postMessage("OK");
+  };
+
+  var fd = new FormData();
+  fd.append('file', e.data);
+  xhr.send(fd);
+}
