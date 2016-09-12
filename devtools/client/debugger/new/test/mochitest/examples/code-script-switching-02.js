2057d58d711e9fe931686779e4343120e8f9b441
James Long <longster@gmail.com>
2016-09-12 14:20:22 -0400

    Bug 1301487 - Update the debugger bundle and implement a proper shutdown method r=me UPDATE_BUNDLE

---
+++ devtools/client/debugger/new/test/mochitest/examples/code-script-switching-02.js
@@ -0,0 +1,13 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function secondCall() {
+  // This comment is useful: ☺
+  debugger;
+  function foo() {}
+  if (x) {
+    foo();
+  }
+}
+
+var x = true;
