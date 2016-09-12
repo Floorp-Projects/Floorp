12f9c4e67f37347fa8a163069a3569dfdbc61532
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-09-12 20:03:26 +0200

    Backed out changeset 5b64b2e8e518 (bug 1300473) for failing devtools test browser_perf-recording-selected-03.js. r=backout

--- devtools/client/performance/test/browser_perf-button-states.js
+++ devtools/client/performance/test/browser_perf-button-states.js
@@ -16,11 +16,13 @@ add_task(function* () {
     win: window
   });
 
-  let { $, $$, EVENTS, PerformanceController, PerformanceView } = panel.panelWin;
-
+  let { $, EVENTS, PerformanceController, PerformanceView } = panel.panelWin;
   let recordButton = $("#main-record-button");
 
-  checkRecordButtonsStates(false, false);
+  ok(!recordButton.hasAttribute("checked"),
+    "The record button should not be checked yet.");
+  ok(!recordButton.hasAttribute("locked"),
+    "The record button should not be locked yet.");
 
   let uiStartClick = once(PerformanceView, EVENTS.UI_START_RECORDING);
   let recordingStarted = once(PerformanceController, EVENTS.RECORDING_STATE_CHANGE, {
@@ -35,11 +37,17 @@ add_task(function* () {
   click(recordButton);
   yield uiStartClick;
 
-  checkRecordButtonsStates(true, true);
+  ok(recordButton.hasAttribute("checked"),
+    "The record button should now be checked.");
+  ok(recordButton.hasAttribute("locked"),
+    "The record button should be locked.");
 
   yield recordingStarted;
 
-  checkRecordButtonsStates(true, false);
+  ok(recordButton.hasAttribute("checked"),
+    "The record button should still be checked.");
+  ok(!recordButton.hasAttribute("locked"),
+    "The record button should not be locked.");
 
   yield backendStartReady;
   yield uiStateRecording;
@@ -58,19 +66,13 @@ add_task(function* () {
   yield uiStopClick;
   yield recordingStopped;
 
-  checkRecordButtonsStates(false, false);
+  ok(!recordButton.hasAttribute("checked"),
+    "The record button should not be checked.");
+  ok(!recordButton.hasAttribute("locked"),
+    "The record button should not be locked.");
 
   yield backendStopReady;
   yield uiStateRecorded;
 
   yield teardownToolboxAndRemoveTab(panel);
-
-  function checkRecordButtonsStates(checked, locked) {
-    for (let button of $$(".record-button")) {
-      is(button.classList.contains("checked"), checked,
-         "The record button checked state should be " + checked);
-      is(button.disabled, locked,
-         "The record button locked state should be " + locked);
-    }
-  }
 });
