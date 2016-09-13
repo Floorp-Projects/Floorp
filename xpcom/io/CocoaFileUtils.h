40ad411272cc567e662505415246d5c55020492a
Josh Aas <jaas@kflag.net>
2016-09-13 02:25:36 -0500

    Bug 337051: Add origin and referrer URL metadata to downloaded files on OS X. r=mstange,pamadini

--- xpcom/io/CocoaFileUtils.h
+++ xpcom/io/CocoaFileUtils.h
@@ -21,6 +21,12 @@ nsresult GetFileCreatorCode(CFURLRef aUrl, OSType* aCreatorCode);
 nsresult SetFileCreatorCode(CFURLRef aUrl, OSType aCreatorCode);
 nsresult GetFileTypeCode(CFURLRef aUrl, OSType* aTypeCode);
 nsresult SetFileTypeCode(CFURLRef aUrl, OSType aTypeCode);
+void     AddOriginMetadataToFile(const CFStringRef filePath,
+                                 const CFURLRef sourceURL,
+                                 const CFURLRef referrerURL);
+void     AddQuarantineMetadataToFile(const CFStringRef filePath,
+                                     const CFURLRef sourceURL,
+                                     const CFURLRef referrerURL);
 
 } // namespace CocoaFileUtils
 
