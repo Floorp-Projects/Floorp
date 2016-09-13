8113d9b8b0185ca52d215255f9ecae65bd7bd190
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-09-13 12:24:12 +0100

    Bug 1289172 - enable automigration on beta 50 for a few cycles, r=dolske,a=ritu
    MozReview-Commit-ID: 5AnCZ5xu8sN

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -3,8 +3,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 pref("startup.homepage_override_url", "");
+#if MOZ_UPDATE_CHANNEL == beta
+pref("startup.homepage_welcome_url", "");
+pref("startup.homepage_welcome_url.additional", "");
+#else
 pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
 pref("startup.homepage_welcome_url.additional", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/learnmore/");
+#endif
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
 // The time interval between the downloading of mar file chunks in the
