593b2fa13ef4cf7e10aee27f88ba594924ae51ad
Tooru Fujisawa <arai_a@mac.com>
2016-09-14 04:07:04 +0900

    Bug 1302582 - Add JS_ReportError*ASCII variants. r=jwalden

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -317,6 +317,17 @@ checkReportFlags(JSContext* cx, unsigned* flags)
     return false;
 }
 
+#ifdef DEBUG
+static void
+AssertIsASCII(const char* s)
+{
+    while (*s) {
+        MOZ_ASSERT((*s & 0x80) == 0);
+        s++;
+    }
+}
+#endif
+
 bool
 js::ReportErrorVA(JSContext* cx, unsigned flags, const char* format,
                   ErrorArgumentsType argumentsType, va_list ap)
@@ -336,6 +347,11 @@ js::ReportErrorVA(JSContext* cx, unsigned flags, const char* format,
         return false;
     }
     messagelen = strlen(message);
+
+#ifdef DEBUG
+    if (argumentsType == ArgumentsAreASCII)
+        AssertIsASCII(message);
+#endif
 
     report.flags = flags;
     report.errorNumber = JSMSG_USER_DEFINED_ERROR;
@@ -553,6 +569,12 @@ class MOZ_RAII AutoMessageArgs
             } else if (typeArg == ArgumentsAreASCII || typeArg == ArgumentsAreLatin1) {
                 const char* charArg = va_arg(ap, char*);
                 size_t charArgLength = strlen(charArg);
+
+#ifdef DEBUG
+                if (typeArg == ArgumentsAreASCII)
+                    AssertIsASCII(charArg);
+#endif
+
                 args_[i] = InflateString(cx, charArg, &charArgLength);
                 if (!args_[i])
                     return false;
@@ -608,6 +630,11 @@ js::ExpandErrorArgumentsVA(ExclusiveContext* cx, JSErrorCallback callback,
 
     if (efs) {
         reportp->exnType = efs->exnType;
+
+#ifdef DEBUG
+        if (argumentsType == ArgumentsAreASCII)
+            AssertIsASCII(efs->format);
+#endif
 
         uint16_t argCount = efs->argCount;
         MOZ_RELEASE_ASSERT(argCount <= JS::MaxNumErrorArguments);
