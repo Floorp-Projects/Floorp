44c72cf57190ae03941d6cb20a05747ed98c8785
Jim Chen <nchen@mozilla.com>
2016-09-14 12:43:35 -0400

    Bug 1258470 - 1. Remove RTL and ZoomConstraints variables from gfx; r=rbarker We no longer send viewport metadata, so we don't actually make use of the RTL and ZoomConstraints variables we keep in Java. There is a single usage of an RTL flag in ImmutableViewportMetrics.offsetViewportByAndClamp, but I think the two branches are equivalent, so the RTL flag is not needed there either.

--- mobile/android/base/java/org/mozilla/gecko/Tabs.java
+++ mobile/android/base/java/org/mozilla/gecko/Tabs.java
@@ -108,7 +108,6 @@ public class Tabs implements GeckoEventListener {
             "Tab:Added",
             "Tab:Close",
             "Tab:Select",
-            "Tab:LoadedFromCache",
             "Content:LocationChange",
             "Content:SecurityChange",
             "Content:StateChange",
@@ -120,7 +119,6 @@ public class Tabs implements GeckoEventListener {
             "Link:Feed",
             "Link:OpenSearch",
             "DesktopMode:Changed",
-            "Tab:ViewportMetadata",
             "Tab:StreamStart",
             "Tab:StreamStop",
             "Tab:AudioPlayingChange",
@@ -561,10 +559,6 @@ public class Tabs implements GeckoEventListener {
             } else if (event.equals("DesktopMode:Changed")) {
                 tab.setDesktopMode(message.getBoolean("desktopMode"));
                 notifyListeners(tab, TabEvents.DESKTOP_MODE_CHANGE);
-            } else if (event.equals("Tab:ViewportMetadata")) {
-                tab.setZoomConstraints(new ZoomConstraints(message));
-                tab.setIsRTL(message.getBoolean("isRTL"));
-                notifyListeners(tab, TabEvents.VIEWPORT_CHANGE);
             } else if (event.equals("Tab:StreamStart")) {
                 tab.setRecording(true);
                 notifyListeners(tab, TabEvents.RECORDING_CHANGE);
@@ -631,7 +625,6 @@ public class Tabs implements GeckoEventListener {
         LINK_FEED,
         SECURITY_CHANGE,
         DESKTOP_MODE_CHANGE,
-        VIEWPORT_CHANGE,
         RECORDING_CHANGE,
         BOOKMARK_ADDED,
         BOOKMARK_REMOVED,
