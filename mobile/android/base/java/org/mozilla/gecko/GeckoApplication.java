fef2725577ff2e2c2e1aee53356af9c282b7c715
Jim Chen <nchen@mozilla.com>
2016-09-14 12:46:50 -0400

    Bug 1291384 - 1. Take BrowserDB out of GeckoProfile; r=nalexander
    Remove references to BrowserDB and its factory from GeckoProfile.
    Instead of keeping the DB in mDB, GeckoProfile now keeps an arbitrary
    object in mData. Using a data object lets us avoid needing another map
    to map profiles to DBs. This feature could be very useful for GeckoView
    consumers as well.
    
    The new way to get a BrowserDB from a profile/context is through
    BrowserDB.from(Context) or BrowserDB.from(GeckoProfile), which takes
    care of creating a local DB if necessary and associating the DB with the
    profile.

--- mobile/android/base/java/org/mozilla/gecko/GeckoApplication.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApplication.java
@@ -169,24 +169,6 @@ public class GeckoApplication extends Application
 
         MulticastDNSManager.getInstance(context).init();
 
-        // Make sure that all browser-ish applications default to the real LocalBrowserDB.
-        // GeckoView consumers use their own Application class, so this doesn't affect them.
-        //
-        // We need to do this before any access to the profile; it controls
-        // which database class is used.
-        //
-        // As such, this needs to occur before the GeckoView in GeckoApp is inflated -- i.e., in the
-        // GeckoApp constructor or earlier -- because GeckoView implicitly accesses the profile. This is earlier!
-        GeckoProfile.setBrowserDBFactory(new BrowserDB.Factory() {
-            @Override
-            public BrowserDB get(String profileName, File profileDir) {
-                // Note that we don't use the profile directory -- we
-                // send operations to the ContentProvider, which does
-                // its own thing.
-                return new LocalBrowserDB(profileName);
-            }
-        });
-
         GeckoService.register();
 
         EventDispatcher.getInstance().registerBackgroundThreadListener(new EventListener(),
