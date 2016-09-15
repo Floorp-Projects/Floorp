be2e0c5858b164f90bd74099e89b0725366ec599
Nicolas Silva <nsilva@mozilla.com>
2016-09-15 13:43:22 +0200

    Bug 1088300 - Don't attempt to paint a ClientLayerManager if IPC is down. r=milan

--- gfx/layers/client/ClientLayerManager.cpp
+++ gfx/layers/client/ClientLayerManager.cpp
@@ -272,6 +272,16 @@ ClientLayerManager::EndTransactionInternal(DrawPaintedLayerCallback aCallback,
   PROFILER_LABEL("ClientLayerManager", "EndTransactionInternal",
     js::ProfileEntry::Category::GRAPHICS);
 
+  if (!mForwarder || !mForwarder->IPCOpen()) {
+    gfxCriticalError() << "LayerManager::EndTransaction while IPC is dead.";
+    // Pointless to try to render since the content cannot be sent to the
+    // compositor. We should not get here in the first place but I suspect
+    // This is happening during shutdown, tab-switch or some other scenario
+    // where we already started tearing the resources down but something
+    // triggered painting anyway.
+    return false;
+  }
+
 #ifdef MOZ_LAYERS_HAVE_LOG
   MOZ_LAYERS_LOG(("  ----- (beginning paint)"));
   Log();
