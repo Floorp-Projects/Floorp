f042334d9a7de69b2036db7c64c92205e7bc1761
Benjamin Bouvier <benj@benj.me>
2016-09-15 21:34:36 +0200

    Bug 1303122: Handle stack in codegen of LCompareAndBranchI on x64; r=luke
    MozReview-Commit-ID: EV5JUmcNYeh

--- js/src/jit/x64/CodeGenerator-x64.h
+++ js/src/jit/x64/CodeGenerator-x64.h
@@ -19,6 +19,7 @@ class CodeGeneratorX64 : public CodeGeneratorX86Shared
     }
 
   protected:
+    Operand ToOperand64(const LInt64Allocation& a);
     ValueOperand ToValue(LInstruction* ins, size_t pos);
     ValueOperand ToOutValue(LInstruction* ins);
     ValueOperand ToTempValue(LInstruction* ins, size_t pos);
