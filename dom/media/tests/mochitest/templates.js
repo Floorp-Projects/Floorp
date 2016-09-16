f6ee0a33369d61e05461cc83cf27e53e93e3e470
Phil Ringnalda <philringnalda@gmail.com>
2016-09-15 19:37:25 -0700

    Backed out changeset 27f8a2467b31 (bug 1302935) for jsep_session_unittest failures CLOSED TREE

--- dom/media/tests/mochitest/templates.js
+++ dom/media/tests/mochitest/templates.js
@@ -507,10 +507,9 @@ var commandsPeerConnectionOfferAnswer = [
   }
 ];
 
-function PC_LOCAL_REMOVE_ALL_BUT_H264_FROM_OFFER(test) {
+function PC_LOCAL_REMOVE_VP8_FROM_OFFER(test) {
   isnot(test.originalOffer.sdp.search("H264/90000"), -1, "H.264 should be present in the SDP offer");
-    test.originalOffer.sdp = sdputils.removeCodec(sdputils.removeCodec(sdputils.removeCodec(
-	test.originalOffer.sdp, 120), 121, 97));
+  test.originalOffer.sdp = sdputils.removeVP8(test.originalOffer.sdp);
   info("Updated H264 only offer: " + JSON.stringify(test.originalOffer));
 };
 
