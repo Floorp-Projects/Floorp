7b35049a6a1c431897200beaf9b6accf3c85a0fa
Phil Ringnalda <philringnalda@gmail.com>
2016-09-18 11:18:17 -0700

    Backed out changeset d1cfae63f790 (bug 1297549) for talos bustage

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -548,9 +548,6 @@ TabChild::TabChild(nsIContentChild* aManager,
   , mDidSetRealShowInfo(false)
   , mDidLoadURLInit(false)
   , mAPZChild(nullptr)
-#if defined(XP_WIN) && defined(ACCESSIBILITY)
-  , mNativeWindowHandle(0)
-#endif
   , mLayerObserverEpoch(0)
 {
   // In the general case having the TabParent tell us if APZ is enabled or not
@@ -2535,17 +2532,6 @@ TabChild::RecvPrint(const uint64_t& aOuterWindowID, const PrintData& aPrintData)
 
 #endif
   return true;
-}
-
-bool
-TabChild::RecvUpdateNativeWindowHandle(const uintptr_t& aNewHandle)
-{
-#if defined(XP_WIN) && defined(ACCESSIBILITY)
-  mNativeWindowHandle = aNewHandle;
-  return true;
-#else
-  return false;
-#endif
 }
 
 bool
