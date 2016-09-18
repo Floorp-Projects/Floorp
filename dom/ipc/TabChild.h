99be2eef50477903ce0db386299fb7947cdc7eb1
Phil Ringnalda <philringnalda@gmail.com>
2016-09-18 11:18:17 -0700

    Backed out changeset d1cfae63f790 (bug 1297549) for talos bustage

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -590,8 +590,6 @@ public:
   virtual bool RecvPrint(const uint64_t& aOuterWindowID,
                          const PrintData& aPrintData) override;
 
-  virtual bool RecvUpdateNativeWindowHandle(const uintptr_t& aNewHandle) override;
-
   /**
    * Native widget remoting protocol for use with windowed plugins with e10s.
    */
@@ -649,10 +647,6 @@ public:
   {
       mAPZChild = aAPZChild;
   }
-
-#if defined(XP_WIN) && defined(ACCESSIBILITY)
-  uintptr_t GetNativeWindowHandle() const { return mNativeWindowHandle; }
-#endif
 
   // Request that the docshell be marked as active.
   void ForcePaint(uint64_t aLayerObserverEpoch);
@@ -791,11 +785,6 @@ private:
   // APZChild clears this pointer from its destructor, so it shouldn't be a
   // dangling pointer.
   layers::APZChild* mAPZChild;
-
-#if defined(XP_WIN) && defined(ACCESSIBILITY)
-  // The handle associated with the native window that contains this tab
-  uintptr_t mNativeWindowHandle;
-#endif // defined(XP_WIN)
 
   // The most recently seen layer observer epoch in RecvSetDocShellIsActive.
   uint64_t mLayerObserverEpoch;
