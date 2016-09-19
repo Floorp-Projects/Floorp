20aa05cd0f5d896f94eb67c9e964f83afae24431
Jarda Snajdr <jsnajdr@gmail.com>
2016-09-19 23:33:00 +0200

    Bug 1303800 - Turn on javascript.options.asyncstack pref for Netmonitor tests that need it (beta) on a CLOSED TREE.  r=ochameau, a=test-only a=blocker

--- devtools/client/netmonitor/test/browser_net_cause.js
+++ devtools/client/netmonitor/test/browser_net_cause.js
@@ -78,6 +78,9 @@ const EXPECTED_REQUESTS = [
 ];
 
 var test = Task.async(function* () {
+  // Async stacks aren't on by default in all builds
+  yield SpecialPowers.pushPrefEnv({ set: [["javascript.options.asyncstack", true]] });
+
   // the initNetMonitor function clears the network request list after the
   // page is loaded. That's why we first load a bogus page from SIMPLE_URL,
   // and only then load the real thing from CAUSE_URL - we want to catch
