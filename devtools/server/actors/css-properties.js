0a89c5906262cae6a2e4fad1d956ad9f07a5ac9a
Tom Tromey <tom@tromey.com>
2016-09-19 12:36:56 -0600

    Bug 1265796 - add CSS subproperty info to CssProperties; r=gregtatum
    MozReview-Commit-ID: JIDTwgnxAJE

--- devtools/server/actors/css-properties.js
+++ devtools/server/actors/css-properties.js
@@ -75,6 +75,8 @@ function generateCssProperties() {
       values.unshift("COLOR");
     }
 
+    let subproperties = DOMUtils.getSubpropertiesForCSSProperty(name);
+
     // In order to maintain any backwards compatible changes when debugging older
     // clients, take the definition from the static CSS properties database, and fill it
     // in with the most recent property definition from the server.
@@ -82,7 +84,8 @@ function generateCssProperties() {
     const serverDefinition = {
       isInherited: DOMUtils.isInheritedProperty(name),
       values,
-      supports
+      supports,
+      subproperties,
     };
     properties[name] = Object.assign(clientDefinition, serverDefinition);
   });
