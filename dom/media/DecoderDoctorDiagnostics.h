ff33fd0b4dbe02ab25d9739cdbcf6886b2e7893f
Gerald Squelart <gsquelart@mozilla.com>
2016-09-18 19:50:01 -0700

    Bug 1247056 - DecDoc::StoreEvent for singular playback issues/solutions - r=jwwang
    MozReview-Commit-ID: GYoitdB8Sj0

--- dom/media/DecoderDoctorDiagnostics.h
+++ dom/media/DecoderDoctorDiagnostics.h
@@ -13,6 +13,13 @@ class nsIDocument;
 
 namespace mozilla {
 
+struct DecoderDoctorEvent {
+  enum Domain {
+    // TODO
+  } mDomain;
+  nsresult mResult;
+};
+
 // DecoderDoctorDiagnostics class, used to gather data from PDMs/DecoderTraits,
 // and then notify the user about issues preventing (or worsening) playback.
 //
@@ -46,10 +53,15 @@ public:
                                  bool aIsSupported,
                                  const char* aCallSite);
 
+  void StoreEvent(nsIDocument* aDocument,
+                  const DecoderDoctorEvent& aEvent,
+                  const char* aCallSite);
+
   enum DiagnosticsType {
     eUnsaved,
     eFormatSupportCheck,
-    eMediaKeySystemAccessRequest
+    eMediaKeySystemAccessRequest,
+    eEvent
   };
   DiagnosticsType Type() const { return mDiagnosticsType; }
 
@@ -91,6 +103,11 @@ public:
     return mKeySystemIssue;
   }
 
+  DecoderDoctorEvent event() const
+  {
+    return mEvent;
+  }
+
 private:
   // Currently-known type of diagnostics. Set from one of the 'Store...' methods.
   // This helps ensure diagnostics are only stored once,
@@ -111,6 +128,8 @@ private:
   nsString mKeySystem;
   bool mIsKeySystemSupported = false;
   KeySystemIssue mKeySystemIssue = eUnset;
+
+  DecoderDoctorEvent mEvent;
 };
 
 } // namespace mozilla
