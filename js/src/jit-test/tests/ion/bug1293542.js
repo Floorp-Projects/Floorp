e996c0df4baf75188f6a86b1c7b51d20e9d3bf5f
Hannes Verschore [:h4writer] <hv1989@gmail.com>
2016-09-20 02:08:00 +0200

    Bug 1303809 - Fix ion/bug1293542.js to not fail when merging to aurora. r=bbouvier

--- js/src/jit-test/tests/ion/bug1293542.js
+++ js/src/jit-test/tests/ion/bug1293542.js
@@ -1,7 +1,7 @@
 
-function f(x) {
-    return (x ** (1 / ~4294967297)) && x;
-};
+try { eval("3 ** 4") } catch (e if e instanceof SyntaxError) { quit(); };
+
+var f = new Function("x", "return (x ** (1 / ~4294967297)) && x");
 for (var i = 0; i < 2; ++i) {
     assertEq(f(-Infinity), 0);
 }
