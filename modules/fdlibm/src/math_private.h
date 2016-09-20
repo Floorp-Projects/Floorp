a96c9fd437dbd009cdf4b830040c17bf67e7700c
Tooru Fujisawa <arai_a@mac.com>
2016-09-20 16:57:08 +0900

    Bug 1303463 - Use safer STRICT_ASSIGN on VisualStudio. r=jwalden

--- modules/fdlibm/src/math_private.h
+++ modules/fdlibm/src/math_private.h
@@ -276,7 +276,7 @@ do {								\
 /*
  * Attempt to get strict C99 semantics for assignment with non-C99 compilers.
  */
-#if FLT_EVAL_METHOD == 0 || __GNUC__ == 0
+#if !defined(_MSC_VER) && (FLT_EVAL_METHOD == 0 || __GNUC__ == 0)
 #define	STRICT_ASSIGN(type, lval, rval)	((lval) = (rval))
 #else
 #define	STRICT_ASSIGN(type, lval, rval) do {	\
