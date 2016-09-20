32de764081c87d501e9f18c22b9361e5e22c2332
Lin Clark <lclark@mozilla.com>
2016-09-20 11:37:00 -0700

    Bug 1294517 - Part 2: New console frontend: Add tests for console.trace(). r=bgrins
    MozReview-Commit-ID: 9sldZfh4PLY

--- devtools/client/webconsole/new-console-output/test/store/messages.test.js
+++ devtools/client/webconsole/new-console-output/test/store/messages.test.js
@@ -2,8 +2,10 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 "use strict";
 
-const { getAllMessages } = require("devtools/client/webconsole/new-console-output/selectors/messages");
-const { getRepeatId } = require("devtools/client/webconsole/new-console-output/utils/messages");
+const {
+  getAllMessages,
+  getAllMessagesUiById,
+} = require("devtools/client/webconsole/new-console-output/selectors/messages");
 const {
   setupActions,
   setupStore
@@ -19,90 +21,106 @@ describe("Message reducer:", () => {
     actions = setupActions();
   });
 
-  it("adds a message to an empty store", () => {
-    const { dispatch, getState } = setupStore([]);
+  describe("messagesById", () => {
+    it("adds a message to an empty store", () => {
+      const { dispatch, getState } = setupStore([]);
 
-    const packet = stubPackets.get("console.log('foobar', 'test')");
-    const message = stubPreparedMessages.get("console.log('foobar', 'test')");
-    dispatch(actions.messageAdd(packet));
-
-    const messages = getAllMessages(getState());
-
-    expect(messages.first()).toEqual(message);
-  });
-
-  it("increments repeat on a repeating message", () => {
-    const { dispatch, getState } = setupStore([
-      "console.log('foobar', 'test')",
-      "console.log('foobar', 'test')"
-    ]);
-
-    const packet = stubPackets.get("console.log('foobar', 'test')");
-    dispatch(actions.messageAdd(packet));
-    dispatch(actions.messageAdd(packet));
-
-    const messages = getAllMessages(getState());
-
-    expect(messages.size).toBe(1);
-    expect(messages.first().repeat).toBe(4);
-  });
-
-  it("does not clobber a unique message", () => {
-    const { dispatch, getState } = setupStore([
-      "console.log('foobar', 'test')",
-      "console.log('foobar', 'test')"
-    ]);
-
-    const packet = stubPackets.get("console.log('foobar', 'test')");
-    dispatch(actions.messageAdd(packet));
-
-    const packet2 = stubPackets.get("console.log(undefined)");
-    dispatch(actions.messageAdd(packet2));
-
-    const messages = getAllMessages(getState());
-
-    expect(messages.size).toBe(2);
-    expect(messages.first().repeat).toBe(3);
-    expect(messages.last().repeat).toBe(1);
-  });
-
-  it("clears the store in response to console.clear()", () => {
-    const { dispatch, getState } = setupStore([
-      "console.log('foobar', 'test')",
-      "console.log(undefined)"
-    ]);
-
-    dispatch(actions.messageAdd(stubPackets.get("console.clear()")));
-
-    const messages = getAllMessages(getState());
-
-    expect(messages.size).toBe(1);
-    expect(messages.first().parameters[0]).toBe("Console was cleared.");
-  });
-
-  it("limits the number of messages displayed", () => {
-    const { dispatch, getState } = setupStore([]);
-
-    const logLimit = 1000;
-    const packet = stubPackets.get("console.log(undefined)");
-    for (let i = 1; i <= logLimit + 1; i++) {
-      packet.message.arguments = [`message num ${i}`];
+      const packet = stubPackets.get("console.log('foobar', 'test')");
+      const message = stubPreparedMessages.get("console.log('foobar', 'test')");
       dispatch(actions.messageAdd(packet));
-    }
 
-    const messages = getAllMessages(getState());
-    expect(messages.count()).toBe(logLimit);
-    expect(messages.first().parameters[0]).toBe(`message num 2`);
-    expect(messages.last().parameters[0]).toBe(`message num ${logLimit + 1}`);
+      const messages = getAllMessages(getState());
+
+      expect(messages.first()).toEqual(message);
+    });
+
+    it("increments repeat on a repeating message", () => {
+      const { dispatch, getState } = setupStore([
+        "console.log('foobar', 'test')",
+        "console.log('foobar', 'test')"
+      ]);
+
+      const packet = stubPackets.get("console.log('foobar', 'test')");
+      dispatch(actions.messageAdd(packet));
+      dispatch(actions.messageAdd(packet));
+
+      const messages = getAllMessages(getState());
+
+      expect(messages.size).toBe(1);
+      expect(messages.first().repeat).toBe(4);
+    });
+
+    it("does not clobber a unique message", () => {
+      const { dispatch, getState } = setupStore([
+        "console.log('foobar', 'test')",
+        "console.log('foobar', 'test')"
+      ]);
+
+      const packet = stubPackets.get("console.log('foobar', 'test')");
+      dispatch(actions.messageAdd(packet));
+
+      const packet2 = stubPackets.get("console.log(undefined)");
+      dispatch(actions.messageAdd(packet2));
+
+      const messages = getAllMessages(getState());
+
+      expect(messages.size).toBe(2);
+      expect(messages.first().repeat).toBe(3);
+      expect(messages.last().repeat).toBe(1);
+    });
+
+    it("clears the store in response to console.clear()", () => {
+      const { dispatch, getState } = setupStore([
+        "console.log('foobar', 'test')",
+        "console.log(undefined)"
+      ]);
+
+      dispatch(actions.messageAdd(stubPackets.get("console.clear()")));
+
+      const messages = getAllMessages(getState());
+
+      expect(messages.size).toBe(1);
+      expect(messages.first().parameters[0]).toBe("Console was cleared.");
+    });
+
+    it("limits the number of messages displayed", () => {
+      const { dispatch, getState } = setupStore([]);
+
+      const logLimit = 1000;
+      const packet = stubPackets.get("console.log(undefined)");
+      for (let i = 1; i <= logLimit + 1; i++) {
+        packet.message.arguments = [`message num ${i}`];
+        dispatch(actions.messageAdd(packet));
+      }
+
+      const messages = getAllMessages(getState());
+      expect(messages.count()).toBe(logLimit);
+      expect(messages.first().parameters[0]).toBe(`message num 2`);
+      expect(messages.last().parameters[0]).toBe(`message num ${logLimit + 1}`);
+    });
+
+    it("does not add null messages to the store", () => {
+      const { dispatch, getState } = setupStore([]);
+
+      const message = stubPackets.get("console.time('bar')");
+      dispatch(actions.messageAdd(message));
+
+      const messages = getAllMessages(getState());
+      expect(messages.size).toBe(0);
+    });
   });
 
-  it("does not add null messages to the store", () => {
-    const { dispatch, getState } = setupStore([]);
+  describe("messagesUiById", () => {
+    it("opens console.trace messages when they are added", () => {
+      const { dispatch, getState } = setupStore([]);
 
-    const message = stubPackets.get("console.time('bar')");
-    dispatch(actions.messageAdd(message));
+      const message = stubPackets.get("console.trace()");
+      dispatch(actions.messageAdd(message));
 
-    const messages = getAllMessages(getState());
-    expect(messages.size).toBe(0);
+      const messages = getAllMessages(getState());
+      const messagesUi = getAllMessagesUiById(getState());
+      expect(messagesUi.size).toBe(1);
+      expect(messagesUi.first()).toBe(messages.first().id);
+    });
   });
 });
