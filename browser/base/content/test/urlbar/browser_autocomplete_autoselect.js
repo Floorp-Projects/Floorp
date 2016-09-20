1abc7ccb35dd257fdb135d8182f77f325d16d7a7
Panos Astithas <past@mozilla.com>
2016-09-20 11:07:10 +0300

    Bug 1303811 - Ensure one-off search buttons are enabled in the URL bar while running browser_autocomplete_autoselect.js (bug 1303811) on a CLOSED TREE. r=adw a=test-only
    MozReview-Commit-ID: CK97lT8SxV1

--- browser/base/content/test/urlbar/browser_autocomplete_autoselect.js
+++ browser/base/content/test/urlbar/browser_autocomplete_autoselect.js
@@ -1,3 +1,5 @@
+const ONEOFF_URLBAR_PREF = "browser.urlbar.oneOffSearches";
+
 function repeat(limit, func) {
   for (let i = 0; i < limit; i++) {
     func(i);
@@ -26,8 +28,10 @@ function is_selected_one_off(index) {
 add_task(function*() {
   let maxResults = Services.prefs.getIntPref("browser.urlbar.maxRichResults");
 
+  Services.prefs.setBoolPref(ONEOFF_URLBAR_PREF, true);
   registerCleanupFunction(function* () {
     yield PlacesTestUtils.clearHistory();
+    Services.prefs.clearUserPref(ONEOFF_URLBAR_PREF);
   });
 
   let visits = [];
