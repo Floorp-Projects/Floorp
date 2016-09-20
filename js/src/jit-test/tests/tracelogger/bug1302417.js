766f04970cae070609a73a73cf7c959da6d40164
h4writer <hv1989@gmail.com>
2016-09-20 14:48:50 +0200

    Bug 1302417: TraceLogging - Don't run the debug check when the item is an error, r=bbouvier

---
+++ js/src/jit-test/tests/tracelogger/bug1302417.js
@@ -0,0 +1,18 @@
+
+
+if (typeof new Debugger().setupTraceLoggerScriptCalls == "function") {
+    lfLogBuffer = `
+        var du = new Debugger;
+        du.setupTraceLoggerScriptCalls();
+        startTraceLogger();
+    `;
+    loadFile(lfLogBuffer);
+    function loadFile(lfVarx) {
+        oomTest(function() {
+                m = parseModule(lfVarx);
+                m.declarationInstantiation();
+                m.evaluation();
+        })
+    }
+}
+
