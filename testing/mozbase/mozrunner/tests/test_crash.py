e519d55a2532496e5b8b188755c4e21a4b0f4c61
Henrik Skupin <mail@hskupin.info>
2016-09-21 12:57:50 +0200

    Bug 1299216 - [mozrunner] check_for_crashes() has to return the crash count since its last invocation. r=ahal Adapt check_for_crashes() for latest mozcrash changes, which returns the number of crashes for both log_crashes() and check_for_crashes() now. Also the runner should accumulate the number of crashes so it will be known at any time how many times the process has been crashed.
    MozReview-Commit-ID: Dl9FlH1TalH

---
+++ testing/mozbase/mozrunner/tests/test_crash.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import mock
+
+import mozrunnertest
+
+
+class MozrunnerCrashTestCase(mozrunnertest.MozrunnerTestCase):
+
+    @mock.patch('mozcrash.log_crashes', return_value=2)
+    def test_crash_count_with_logger(self, log_crashes):
+        self.assertEqual(self.runner.crashed, 0)
+        self.assertEqual(self.runner.check_for_crashes(), 2)
+        self.assertEqual(self.runner.crashed, 2)
+        self.assertEqual(self.runner.check_for_crashes(), 2)
+        self.assertEqual(self.runner.crashed, 4)
+
+        log_crashes.return_value = 0
+        self.assertEqual(self.runner.check_for_crashes(), 0)
+        self.assertEqual(self.runner.crashed, 4)
+
+    @mock.patch('mozcrash.check_for_crashes', return_value=2)
+    def test_crash_count_without_logger(self, check_for_crashes):
+        self.runner.logger = None
+
+        self.assertEqual(self.runner.crashed, 0)
+        self.assertEqual(self.runner.check_for_crashes(), 2)
+        self.assertEqual(self.runner.crashed, 2)
+        self.assertEqual(self.runner.check_for_crashes(), 2)
+        self.assertEqual(self.runner.crashed, 4)
+
+        check_for_crashes.return_value = 0
+        self.assertEqual(self.runner.check_for_crashes(), 0)
+        self.assertEqual(self.runner.crashed, 4)
