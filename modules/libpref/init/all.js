d21669eb5b187ab5374df72347fb4927853b793f
Wes Kocher <wkocher@mozilla.com>
2016-09-21 12:44:31 -0700

    Backed out changeset 7e898928c51b (bug 838954) for lint failures a=backout CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5558,3 +5558,9 @@ pref("media.block-autoplay-until-in-foreground", true);
 // Is the Servo-backed style system enabled?
 pref("layout.css.servo.enabled", true);
 #endif
+
+#ifdef NIGHTLY_BUILD
+pref("dom.html_fragment_serialisation.appendLF", true);
+#else
+pref("dom.html_fragment_serialisation.appendLF", false);
+#endif
