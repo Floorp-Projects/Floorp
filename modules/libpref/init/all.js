81ab040edf71676ef1dddaaffd572b100068be2c
Wes Kocher <wkocher@mozilla.com>
2016-09-21 11:42:16 -0700

    Backout revisions b80d560e0102 and fc593dd9a280 (bug 838954) a=backout

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5564,9 +5564,3 @@ pref("media.block-autoplay-until-in-foreground", true);
 // Is the Servo-backed style system enabled?
 pref("layout.css.servo.enabled", true);
 #endif
-
-#ifdef NIGHTLY_BUILD
-pref("dom.html_fragment_serialisation.appendLF", true);
-#else
-pref("dom.html_fragment_serialisation.appendLF", false);
-#endif
