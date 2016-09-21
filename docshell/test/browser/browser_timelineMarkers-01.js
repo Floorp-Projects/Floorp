d152dd7eaf9d686727c986ebf3c3e4083a535a09
Blake Kaplan <mrbkap@gmail.com>
2016-09-20 17:07:39 -0700

    Bug 1304531 - Remove CPOW usage from browser_timelineMarkers-01.js. r=Felipe, a=test-only
    MozReview-Commit-ID: Ec3py9hntta

--- docshell/test/browser/browser_timelineMarkers-01.js
+++ docshell/test/browser/browser_timelineMarkers-01.js
@@ -5,46 +5,28 @@
 
 // Test that the docShell has the right profile timeline API
 
-var test = Task.async(function*() {
-  waitForExplicitFinish();
+const URL = "data:text/html;charset=utf-8,Test page";
 
-  yield openUrl("data:text/html;charset=utf-8,Test page");
+add_task(function* () {
+  yield BrowserTestUtils.withNewTab({ gBrowser, url: URL },
+    function* (browser) {
+      yield ContentTask.spawn(browser, null, function() {
+        ok("recordProfileTimelineMarkers" in docShell,
+           "The recordProfileTimelineMarkers attribute exists");
+        ok("popProfileTimelineMarkers" in docShell,
+           "The popProfileTimelineMarkers function exists");
+        ok(docShell.recordProfileTimelineMarkers === false,
+           "recordProfileTimelineMarkers is false by default");
+        ok(docShell.popProfileTimelineMarkers().length === 0,
+           "There are no markers by default");
 
-  let docShell = content.QueryInterface(Ci.nsIInterfaceRequestor)
-                        .getInterface(Ci.nsIWebNavigation)
-                        .QueryInterface(Ci.nsIDocShell);
+        docShell.recordProfileTimelineMarkers = true;
+        ok(docShell.recordProfileTimelineMarkers === true,
+           "recordProfileTimelineMarkers can be set to true");
 
-  ok("recordProfileTimelineMarkers" in docShell,
-    "The recordProfileTimelineMarkers attribute exists");
-  ok("popProfileTimelineMarkers" in docShell,
-    "The popProfileTimelineMarkers function exists");
-  ok(docShell.recordProfileTimelineMarkers === false,
-    "recordProfileTimelineMarkers is false by default");
-  ok(docShell.popProfileTimelineMarkers().length === 0,
-    "There are no markers by default");
-
-  docShell.recordProfileTimelineMarkers = true;
-  ok(docShell.recordProfileTimelineMarkers === true,
-    "recordProfileTimelineMarkers can be set to true");
-
-  docShell.recordProfileTimelineMarkers = false;
-  ok(docShell.recordProfileTimelineMarkers === false,
-    "recordProfileTimelineMarkers can be set to false");
-
-  gBrowser.removeCurrentTab();
-  finish();
+        docShell.recordProfileTimelineMarkers = false;
+        ok(docShell.recordProfileTimelineMarkers === false,
+           "recordProfileTimelineMarkers can be set to false");
+      });
+    });
 });
-
-function openUrl(url) {
-  return new Promise(function(resolve, reject) {
-    window.focus();
-
-    let tab = window.gBrowser.selectedTab = window.gBrowser.addTab(url);
-    let linkedBrowser = tab.linkedBrowser;
-
-    linkedBrowser.addEventListener("load", function onload() {
-      linkedBrowser.removeEventListener("load", onload, true);
-      resolve(tab);
-    }, true);
-  });
-}
