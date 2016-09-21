f5eb31583b7bc086b8fb3db535b20a1e08f3548d
Iris Hsiao <ihsiao@mozilla.com>
2016-09-21 12:25:36 +0800

    Backed out changeset 85cacbef3243 (bug 1303247) for GTest Assertion failures CLOSED TREE

--- media/libstagefright/binding/include/mp4_demuxer/MP4Metadata.h
+++ media/libstagefright/binding/include/mp4_demuxer/MP4Metadata.h
@@ -42,9 +42,6 @@ private:
   mutable bool mPreferRust;
   mutable bool mReportedAudioTrackTelemetry;
   mutable bool mReportedVideoTrackTelemetry;
-#ifndef RELEASE_BUILD
-  mutable bool mRustTestMode;
-#endif
   bool ShouldPreferRust() const;
 #endif
 };
