ace738008847facec2cf0d1fde1a6e9634681841
Bobby Holley <bobbyholley@gmail.com>
2016-09-21 15:42:45 -0700

    Bug 1304913 - Have Servo manage node data directly without FFI calls. r=Manishearth
    MozReview-Commit-ID: H8f8VP18TbM

--- dom/base/nsGenericDOMDataNode.cpp
+++ dom/base/nsGenericDOMDataNode.cpp
@@ -567,7 +567,7 @@ nsGenericDOMDataNode::BindToTree(nsIDocument* aDocument, nsIContent* aParent,
   // So for now, we just mark nodes as dirty when they're inserted into a
   // document or shadow tree.
   if (IsStyledByServo() && IsInComposedDoc()) {
-    MOZ_ASSERT(!ServoData().get());
+    MOZ_ASSERT(!HasServoData());
     SetIsDirtyForServo();
   }
 
@@ -605,10 +605,10 @@ nsGenericDOMDataNode::UnbindFromTree(bool aDeep, bool aNullParent)
   // Computed styled data isn't useful for detached nodes, and we'll need to
   // recomputed it anyway if we ever insert the nodes back into a document.
   if (IsStyledByServo()) {
-    ServoData().reset();
+    ClearServoData();
   } else {
 #ifdef MOZ_STYLO
-    MOZ_ASSERT(!ServoData());
+    MOZ_ASSERT(!HasServoData());
 #endif
   }
 
