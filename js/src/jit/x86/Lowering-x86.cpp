741d9cb87072b39201acb4a7e687db59dcf88cce
Wes Kocher <wkocher@mozilla.com>
2016-09-21 16:37:32 -0700

    Backed out changeset 5a71ed072a2b (bug 1300546) for android failures in test_bug772796.html a=backout CLOSED TREE

--- js/src/jit/x86/Lowering-x86.cpp
+++ js/src/jit/x86/Lowering-x86.cpp
@@ -209,7 +209,8 @@ LIRGeneratorX86::lowerForALUInt64(LInstructionHelper<INT64_PIECES, 2 * INT64_PIE
                                   MDefinition* mir, MDefinition* lhs, MDefinition* rhs)
 {
     ins->setInt64Operand(0, useInt64RegisterAtStart(lhs));
-    ins->setInt64Operand(INT64_PIECES, useInt64OrConstant(rhs));
+    ins->setInt64Operand(INT64_PIECES,
+                         lhs != rhs ? useInt64OrConstant(rhs) : useInt64OrConstantAtStart(rhs));
     defineInt64ReuseInput(ins, mir, 0);
 }
 
