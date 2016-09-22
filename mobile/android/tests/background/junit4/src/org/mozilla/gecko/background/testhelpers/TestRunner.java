2f25baf4f9c43803d1162b3800d74d81ab606ce8
Grigory Kruglov <gkruglov@mozilla.com>
2016-09-22 13:42:42 -0700

    Bug 1300094 - Update robolectric to 3.1.2 r=sebastian
    MozReview-Commit-ID: KuvzMbZ2n1q

--- mobile/android/tests/background/junit4/src/org/mozilla/gecko/background/testhelpers/TestRunner.java
+++ mobile/android/tests/background/junit4/src/org/mozilla/gecko/background/testhelpers/TestRunner.java
@@ -25,7 +25,7 @@
 package org.mozilla.gecko.background.testhelpers;
 
 import org.junit.runners.model.InitializationError;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.manifest.AndroidManifest;
 import org.robolectric.res.FileFsFile;
@@ -46,7 +46,7 @@ import org.robolectric.util.ReflectionHelpers;
  * that uses a Gradle `buildConfigField` to find build outputs.
  * See https://github.com/robolectric/robolectric/issues/1648#issuecomment-113731011.
  */
-public class TestRunner extends RobolectricGradleTestRunner {
+public class TestRunner extends RobolectricTestRunner {
     private FsFile buildFolder;
 
     public TestRunner(Class<?> klass) throws InitializationError {
