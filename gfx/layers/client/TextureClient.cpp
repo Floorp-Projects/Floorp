2255e55899f03f30c8164efe6a0eeb72b11965fd
David Anderson <danderson@mozilla.com>
2016-09-21 22:40:20 -0700

    Remove the ChildActor helper class. (bug 1303426 part 3, r=nical)
    --HG--
    extra : rebase_source : 1b0219df4a61caedd210dd82def132accf3d3ad9

--- gfx/layers/client/TextureClient.cpp
+++ gfx/layers/client/TextureClient.cpp
@@ -94,12 +94,13 @@ void DeallocateTextureClient(TextureDeallocParams params);
  * TextureClient's data until the compositor side confirmed that it is safe to
  * deallocte or recycle the it.
  */
-class TextureChild final : public ChildActor<PTextureChild>
+class TextureChild final : PTextureChild
 {
   ~TextureChild()
   {
     // We should have deallocated mTextureData in ActorDestroy
     MOZ_ASSERT(!mTextureData);
+    MOZ_ASSERT(mOwnerCalledDestroy);
   }
 public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(TextureChild)
@@ -113,6 +114,7 @@ public:
   , mMainThreadOnly(false)
   , mIPCOpen(false)
   , mOwnsTextureData(false)
+  , mOwnerCalledDestroy(false)
   {}
 
   bool Recv__delete__() override { return true; }
@@ -143,6 +145,19 @@ private:
     mIPCOpen = false;
     Release();
   }
+
+  /// The normal way to destroy the actor.
+  ///
+  /// This will asynchronously send a Destroy message to the parent actor, whom
+  /// will send the delete message.
+  void Destroy(CompositableForwarder* aFwd);
+
+  /// The ugly and slow way to destroy the actor.
+  ///
+  /// This will block until the Parent actor has handled the Destroy message,
+  /// and then start the asynchronous handshake (and destruction will already
+  /// be done on the parent side, when the async part happens).
+  void DestroySynchronously(CompositableForwarder* aFwd);
 
   // This lock is used order to prevent several threads to access the
   // TextureClient's data concurrently. In particular, it prevents shutdown
@@ -218,6 +233,7 @@ private:
   bool mMainThreadOnly;
   bool mIPCOpen;
   bool mOwnsTextureData;
+  bool mOwnerCalledDestroy;
 
   friend class TextureClient;
   friend void DeallocateTextureClient(TextureDeallocParams params);
@@ -255,6 +271,32 @@ TextureChild::ActorDestroy(ActorDestroyReason why)
   if (mTextureData) {
     DestroyTextureData(mTextureData, GetAllocator(), mOwnsTextureData, mMainThreadOnly);
     mTextureData = nullptr;
+  }
+}
+
+void
+TextureChild::Destroy(CompositableForwarder* aFwd)
+{
+  MOZ_ASSERT(!mOwnerCalledDestroy);
+  if (!mOwnerCalledDestroy) {
+    mOwnerCalledDestroy = true;
+    if (!aFwd || !aFwd->DestroyInTransaction(this, false)) {
+      this->SendDestroy();
+    }
+  }
+}
+
+void
+TextureChild::DestroySynchronously(CompositableForwarder* aFwd)
+{
+  MOZ_PERFORMANCE_WARNING("gfx", "IPDL actor requires synchronous deallocation");
+  MOZ_ASSERT(!mOwnerCalledDestroy);
+  if (!mOwnerCalledDestroy) {
+    mOwnerCalledDestroy = true;
+    if (!aFwd || !aFwd->DestroyInTransaction(this, true)) {
+      this->SendDestroySync();
+      this->SendDestroy();
+    }
   }
 }
 
@@ -408,14 +450,6 @@ void TextureClient::Destroy(bool aForceSync)
 
     DeallocateTextureClient(params);
   }
-}
-
-bool
-TextureClient::DestroyFallback(PTextureChild* aActor)
-{
-  // should not end up here so crash debug builds.
-  MOZ_ASSERT(false);
-  return aActor->SendDestroySync();
 }
 
 void
