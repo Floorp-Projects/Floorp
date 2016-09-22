c206fb55ed3ec06da418e6c85c6ff5ff0166d6ea
Ryan VanderMeulen <ryanvm@gmail.com>
2016-09-22 18:28:42 -0400

    Backed out changeset 66951f9c2c53 (bug 1299593) for fetch/api/headers/headers-basic.html wpt failures.

--- js/src/tests/ecma_6/Array/values.js
+++ js/src/tests/ecma_6/Array/values.js
@@ -1,20 +1,18 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/licenses/publicdomain/ */
 
-if (Array.prototype.values) {
-    assertEq(Array.prototype.values, Array.prototype[Symbol.iterator]);
-    assertEq(Array.prototype.values.name, "values");
-    assertEq(Array.prototype.values.length, 0);
+assertEq(Array.prototype.values, Array.prototype[Symbol.iterator]);
+assertEq(Array.prototype.values.name, "values");
+assertEq(Array.prototype.values.length, 0);
 
-    function valuesUnscopeable() {
-        var values = "foo";
-        with ([1, 2, 3]) {
-            assertEq(indexOf, Array.prototype.indexOf);
-            assertEq(values, "foo");
-        }
+function valuesUnscopeable() {
+    var values = "foo";
+    with ([1, 2, 3]) {
+        assertEq(indexOf, Array.prototype.indexOf);
+        assertEq(values, "foo");
     }
-    valuesUnscopeable();
 }
+valuesUnscopeable();
 
 if (typeof reportCompare === 'function')
     reportCompare(0, 0);
