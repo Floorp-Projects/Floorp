e807af7eb7c60a4be8ceb03022fedf10a2bbae43
Masayuki Nakano <masayuki@d-toybox.com>
2016-09-22 10:54:51 +0900

    Bug 972110 - Rewrite editor/libeditor/test_bug674770-1.html without setTimeout() for avoiding intermittent failure. r=smaug, a=test-only
    We can assume that if middle button's click event on a link isn't consumed by any event handlers including system event group's, it will cause open new tab.  With this assumption, we can avoid using setTimeout which causes random orange.
    
    However, unfortunately, in e10s mode, the default is NOT consumed at window in bubbling phase but consumed at that time. So, when not working the link is expected, we cannot check Event.defaultPrevented.  But fortunately, we can check if the page is loaded after that.
    
    Note that for testing this, the test needs to check if an event handler which is either in default group or system group consumed a click event.  However, this runs as mochitest-plain.  Therefore, Event.defaultPrevented returns false if the event is consumed only in the system group's event listener.  For avoiding this issue, this patch adds defaultPreventedInAnyGroups() into SpecialPowers.  In SpecialPowers, Event.defaultPrevented is accessed from chrome context.  Therefore, we can get the result what this test needs.
    
    MozReview-Commit-ID: Cfn4lFR1dfI

--- testing/specialpowers/content/specialpowersAPI.js
+++ testing/specialpowers/content/specialpowersAPI.js
@@ -1578,6 +1578,14 @@ SpecialPowersAPI.prototype = {
       removeSystemEventListener(target, type, listener, useCapture);
   },
 
+  // helper method to check if the event is consumed by either default group's
+  // event listener or system group's event listener.
+  defaultPreventedInAnyGroup: function(event) {
+    // FYI: Event.defaultPrevented returns false in content context if the
+    //      event is consumed only by system group's event listeners.
+    return event.defaultPrevented;
+  },
+
   getDOMRequestService: function() {
     var serv = Services.DOMRequest;
     var res = {};
