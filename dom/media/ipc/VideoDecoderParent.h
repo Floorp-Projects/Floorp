26aa4a9e69cb3fb7f4375849c23be46a7b2e9b14
Matt Woodrow <mwoodrow@mozilla.com>
2016-09-23 09:44:26 +1200

    Bug 1302918 - Add PVideoBridge to share textures with the compositor. r=dvander,nical

--- dom/media/ipc/VideoDecoderParent.h
+++ dom/media/ipc/VideoDecoderParent.h
@@ -48,8 +48,6 @@ public:
 private:
   ~VideoDecoderParent();
 
-  VideoDecoderManagerParent* GetManager() { return static_cast<VideoDecoderManagerParent*>(Manager()); }
-
   RefPtr<VideoDecoderManagerParent> mParent;
   RefPtr<VideoDecoderParent> mIPDLSelfRef;
   RefPtr<TaskQueue> mManagerTaskQueue;
