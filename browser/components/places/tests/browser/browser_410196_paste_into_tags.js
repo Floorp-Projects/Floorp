d624ba76c01e1239f2a471d602d959c030428ed5
Pascal Chevrel <pascalc>
2016-09-23 07:06:00 +0200

    Bug 1283076 - "Change default bookmarks for Nightly". r=dolske

--- browser/components/places/tests/browser/browser_410196_paste_into_tags.js
+++ browser/components/places/tests/browser/browser_410196_paste_into_tags.js
@@ -93,7 +93,8 @@ function focusTag(PlacesOrganizer) {
   let fooTag = tags.getChild(0);
   let tagNode = fooTag;
   PlacesOrganizer._places.selectNode(fooTag);
-  is(tagNode.title, 'foo', "tagNode title is foo");
+  // Bug 1283076: Nightly has a default 'bug' tag already set
+  ok(tagNode.title in {'bug':true, 'foo':true} , "tagNode title is foo/bug");
   let ip = PlacesOrganizer._places.insertionPoint;
   ok(ip.isTag, "IP is a tag");
 }
