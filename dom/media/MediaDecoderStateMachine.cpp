70d30f62af82c1a5d4320543713cc43ba95b750d
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-09-23 16:02:54 +0200

    Backed out 12 changesets (bug 1247056) for windows bc7 test failures
    Backed out changeset 13434fa439d1 (bug 1247056)
    Backed out changeset e1e5be2fc586 (bug 1247056)
    Backed out changeset f48fd90bf839 (bug 1247056)
    Backed out changeset 8e164f6d6c5c (bug 1247056)
    Backed out changeset 024d7295b6f4 (bug 1247056)
    Backed out changeset 32fc8121ca20 (bug 1247056)
    Backed out changeset 49558f78782a (bug 1247056)
    Backed out changeset 2b3dce1c98f6 (bug 1247056)
    Backed out changeset be036a725d3a (bug 1247056)
    Backed out changeset 344a41f87a23 (bug 1247056)
    Backed out changeset 0ce3c5cb6e6a (bug 1247056)
    Backed out changeset 01aa7f5bde71 (bug 1247056)

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2875,13 +2875,9 @@ void MediaDecoderStateMachine::OnMediaSinkAudioComplete()
   mAudioCompleted = true;
   // To notify PlaybackEnded as soon as possible.
   ScheduleStateMachine();
-
-  // Report OK to Decoder Doctor (to know if issue may have been resolved).
-  mOnDecoderDoctorEvent.Notify(
-    DecoderDoctorEvent{DecoderDoctorEvent::eAudioSinkStartup, NS_OK});
 }
 
-void MediaDecoderStateMachine::OnMediaSinkAudioError(nsresult aResult)
+void MediaDecoderStateMachine::OnMediaSinkAudioError()
 {
   MOZ_ASSERT(OnTaskQueue());
   MOZ_ASSERT(mInfo.HasAudio());
@@ -2889,11 +2885,6 @@ void MediaDecoderStateMachine::OnMediaSinkAudioError(nsresult aResult)
 
   mMediaSinkAudioPromise.Complete();
   mAudioCompleted = true;
-
-  // Result should never be NS_OK in this *error* handler. Report to Dec-Doc.
-  MOZ_ASSERT(NS_FAILED(aResult));
-  mOnDecoderDoctorEvent.Notify(
-    DecoderDoctorEvent{DecoderDoctorEvent::eAudioSinkStartup, aResult});
 
   // Make the best effort to continue playback when there is video.
   if (HasVideo()) {
