1eca1c563cbf87e375479e9fff08d8bbb2369452
Xidorn Quan <me@upsuper.org>
2016-09-26 22:03:25 +1000

    Bug 1304302 part 4 - Add all methods StyleSheetHandle needs to StyleSheet. r=heycam
    The methods are written in the same order as in StyleSheetHandle::Ptr.
    
    MozReview-Commit-ID: 6b4311ailNj

--- layout/style/StyleSheetInlines.h
+++ layout/style/StyleSheetInlines.h
@@ -7,6 +7,7 @@
 #ifndef mozilla_StyleSheetInlines_h
 #define mozilla_StyleSheetInlines_h
 
+#include "mozilla/TypeTraits.h"
 #include "mozilla/ServoStyleSheet.h"
 #include "mozilla/CSSStyleSheet.h"
 
@@ -48,6 +49,153 @@ StyleSheet::AsServo() const
   MOZ_ASSERT(IsServo());
   return *static_cast<const ServoStyleSheet*>(this);
 }
+
+#define FORWARD_CONCRETE(method_, geckoargs_, servoargs_) \
+  static_assert(!IsSame<decltype(&StyleSheet::method_), \
+                        decltype(&CSSStyleSheet::method_)>::value, \
+                "CSSStyleSheet should define its own " #method_); \
+  static_assert(!IsSame<decltype(&StyleSheet::method_), \
+                        decltype(&ServoStyleSheet::method_)>::value, \
+                "ServoStyleSheet should define its own " #method_); \
+  if (IsServo()) { \
+    return AsServo().method_ servoargs_; \
+  } \
+  return AsGecko().method_ geckoargs_;
+
+#define FORWARD(method_, args_) FORWARD_CONCRETE(method_, args_, args_)
+
+MozExternalRefCountType
+StyleSheet::AddRef()
+{
+  FORWARD(AddRef, ())
+}
+
+MozExternalRefCountType
+StyleSheet::Release()
+{
+  FORWARD(Release, ())
+}
+
+bool
+StyleSheet::IsInline() const
+{
+  return !SheetInfo().mOriginalSheetURI;
+}
+
+nsIURI*
+StyleSheet::GetSheetURI() const
+{
+  return SheetInfo().mSheetURI;
+}
+
+nsIURI*
+StyleSheet::GetOriginalURI() const
+{
+  return SheetInfo().mOriginalSheetURI;
+}
+
+nsIURI*
+StyleSheet::GetBaseURI() const
+{
+  return SheetInfo().mBaseURI;
+}
+
+void
+StyleSheet::SetURIs(nsIURI* aSheetURI, nsIURI* aOriginalSheetURI,
+                    nsIURI* aBaseURI)
+{
+  NS_PRECONDITION(aSheetURI && aBaseURI, "null ptr");
+  StyleSheetInfo& info = SheetInfo();
+  MOZ_ASSERT(!HasRules() && !info.mComplete,
+             "Can't call SetURIs on sheets that are complete or have rules");
+  info.mSheetURI = aSheetURI;
+  info.mOriginalSheetURI = aOriginalSheetURI;
+  info.mBaseURI = aBaseURI;
+}
+
+bool
+StyleSheet::IsApplicable() const
+{
+  return !mDisabled && SheetInfo().mComplete;
+}
+
+bool
+StyleSheet::HasRules() const
+{
+  FORWARD(HasRules, ())
+}
+
+void
+StyleSheet::SetOwningDocument(nsIDocument* aDocument)
+{
+  FORWARD(SetOwningDocument, (aDocument))
+}
+
+StyleSheet*
+StyleSheet::GetParentSheet() const
+{
+  FORWARD(GetParentSheet, ())
+}
+
+void
+StyleSheet::AppendStyleSheet(StyleSheet* aSheet)
+{
+  FORWARD_CONCRETE(AppendStyleSheet, (&aSheet->AsGecko()), (&aSheet->AsServo()))
+}
+
+nsIPrincipal*
+StyleSheet::Principal() const
+{
+  return SheetInfo().mPrincipal;
+}
+
+void
+StyleSheet::SetPrincipal(nsIPrincipal* aPrincipal)
+{
+  StyleSheetInfo& info = SheetInfo();
+  NS_PRECONDITION(!info.mPrincipalSet, "Should only set principal once");
+  if (aPrincipal) {
+    info.mPrincipal = aPrincipal;
+#ifdef DEBUG
+    info.mPrincipalSet = true;
+#endif
+  }
+}
+
+CORSMode
+StyleSheet::GetCORSMode() const
+{
+  return SheetInfo().mCORSMode;
+}
+
+net::ReferrerPolicy
+StyleSheet::GetReferrerPolicy() const
+{
+  return SheetInfo().mReferrerPolicy;
+}
+
+void
+StyleSheet::GetIntegrity(dom::SRIMetadata& aResult) const
+{
+  aResult = SheetInfo().mIntegrity;
+}
+
+size_t
+StyleSheet::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
+{
+  FORWARD(SizeOfIncludingThis, (aMallocSizeOf))
+}
+
+#ifdef DEBUG
+void
+StyleSheet::List(FILE* aOut, int32_t aIndex) const
+{
+  FORWARD(List, (aOut, aIndex))
+}
+#endif
+
+#undef FORWARD
+#undef FORWARD_CONCRETE
 
 }
 
