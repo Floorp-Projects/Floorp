d61a9893cd2e44876fe9ecbb088e0cbd72a1fa66
Olli Pettay <Olli.Pettay@helsinki.fi>
2016-09-27 01:03:24 +0300

    Bug 1305309, backout Bug 1214805, r=backout

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2768,16 +2768,6 @@ public:
     return mHasScrollLinkedEffect;
   }
 
-  bool MayHavePluginFramesForPrinting()
-  {
-    return mMayHavePluginFramesForPrinting;
-  }
-
-  void SetMayHavePluginFramesForPrinting()
-  {
-    mMayHavePluginFramesForPrinting = true;
-  }
-
 protected:
   bool GetUseCounter(mozilla::UseCounter aUseCounter)
   {
@@ -3073,10 +3063,6 @@ protected:
 
   // True is document has ever been in a foreground window.
   bool mEverInForeground : 1;
-
-  // True if this document is a static clone for printing and may
-  // have elements referring to plugins in the original document.
-  bool mMayHavePluginFramesForPrinting : 1;
 
   enum Type {
     eUnknown, // should never be used
