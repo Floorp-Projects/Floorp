407c355a7455e074a6ecff66dc46fb42afdfc9ae
Alexandre Poirot <poirot.alex@gmail.com>
2016-09-26 07:06:59 -0700

    Bug 1297758 - Fetch target and fronts from inspector getter rather than toolbox. r=bgrins
    MozReview-Commit-ID: HchlqORtfH5

--- devtools/client/inspector/rules/views/rule-editor.js
+++ devtools/client/inspector/rules/views/rule-editor.js
@@ -87,7 +87,7 @@ RuleEditor.prototype = {
 
   get isSelectorEditable() {
     let trait = this.isEditable &&
-      this.toolbox.target.client.traits.selectorEditable &&
+      this.ruleView.inspector.target.client.traits.selectorEditable &&
       this.rule.domRule.type !== ELEMENT_STYLE &&
       this.rule.domRule.type !== CSSRule.KEYFRAME_RULE;
 
