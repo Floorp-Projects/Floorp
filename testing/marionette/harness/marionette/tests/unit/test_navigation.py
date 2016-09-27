b2605976f07a1de338da79e44adf6c6db4a77c5e
Andreas Tolfsen <ato@mozilla.com>
2016-09-27 13:07:28 +0100

    Bug 1302707 - Fix type check to allow page loading timeout of 0; r=automatedtester
    The `get` function in testing/marionette/listener.js used an evaluated
    if-condition test to determine if a page timeout was given.  This would
    fail if passed 0 because 0 evaluates to false in JavaScript.
    
    This patch fixes the incorrect type check by looking at whether the
    variable has been defined or not.
    
    MozReview-Commit-ID: 39vDZRjKAFb
    
    --HG--
    extra : rebase_source : f8100e05f9b1165e20b5aaab6e89b09fd110b3d2

--- testing/marionette/harness/marionette/tests/unit/test_navigation.py
+++ testing/marionette/harness/marionette/tests/unit/test_navigation.py
@@ -122,18 +122,11 @@ class TestNavigate(MarionetteTestCase):
         self.assertEqual("complete", state)
         self.assertTrue(self.marionette.find_element(By.ID, "mozLink"))
 
-    def test_should_throw_a_timeoutexception_when_loading_page(self):
-        try:
+    def test_error_when_exceeding_page_load_timeout(self):
+        with self.assertRaises(TimeoutException):
             self.marionette.set_page_load_timeout(0)
-            self.marionette.navigate(self.test_doc)
-            self.assertTrue(self.marionette.find_element(By.ID, "mozLink"))
-            self.fail("Should have thrown a MarionetteException")
-        except TimeoutException as e:
-            self.assertTrue("Error loading page, timed out" in str(e))
-        except Exception as e:
-            import traceback
-            print traceback.format_exc()
-            self.fail("Should have thrown a TimeoutException instead of %s" % type(e))
+            self.marionette.navigate(self.marionette.absolute_url("slow"))
+            self.marionette.find_element(By.TAG_NAME, "p")
 
     def test_navigate_iframe(self):
         self.marionette.navigate(self.iframe_doc)
