0aa18121c9a9844c1cacb50e258dc186ac64c657
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2016-09-27 15:38:00 -0400

    Bug 1305360 - Part 1: Add a mechanism to allow users to opt out of protection for small buffers. r=jandem

--- js/src/ds/PageProtectingVector.h
+++ js/src/ds/PageProtectingVector.h
@@ -58,6 +58,13 @@ class PageProtectingVector final
      */
     intptr_t unprotectedBytes;
 
+    /*
+     * The size in bytes that a buffer needs to be before its pages will be
+     * protected. This is intended to reduce churn for small vectors while
+     * still offering protection when they grow large enough.
+     */
+    size_t protectionLowerBound;
+
     bool protectionEnabled;
     bool regionUnprotected;
 
@@ -65,6 +72,8 @@ class PageProtectingVector final
         unprotectedBytes += offsetToPage;
         offsetToPage = (pageSize - (uintptr_t(vector.begin()) & pageMask)) & pageMask;
         unprotectedBytes -= offsetToPage;
+        protectionEnabled = vector.capacity() >= protectionLowerBound &&
+                            vector.capacity() >= pageSize + offsetToPage;
     }
 
     void protect() {
@@ -92,6 +101,10 @@ class PageProtectingVector final
     void protectNewBuffer() {
         updateOffsetToPage();
         protect();
+    }
+
+    void unprotectOldBuffer() {
+        unprotect();
     }
 
     bool anyProtected(size_t first, size_t last) {
@@ -125,7 +138,7 @@ class PageProtectingVector final
 
         void emplace(PageProtectingVector* holder) {
             vector = holder;
-            vector->unprotect();
+            vector->unprotectOldBuffer();
         }
 
         explicit AutoUnprotect(PageProtectingVector* holder) {
@@ -146,23 +159,22 @@ class PageProtectingVector final
         offsetToPage(0),
         protectedBytes(0),
         unprotectedBytes(0),
+        protectionLowerBound(0),
         protectionEnabled(false),
-        regionUnprotected(false) { updateOffsetToPage(); }
+        regionUnprotected(false) { protectNewBuffer(); }
 
-    ~PageProtectingVector() { unprotect(); }
+    ~PageProtectingVector() { unprotectOldBuffer(); }
 
-    /* Enable protection for the entire buffer. */
-    void enableProtection() {
-        MOZ_ASSERT(!protectionEnabled);
-        protectionEnabled = true;
-        protectNewBuffer();
-    }
-
-    /* Disable protection for the entire buffer. */
-    void disableProtection() {
-        MOZ_ASSERT(protectionEnabled);
-        unprotect();
-        protectionEnabled = false;
+    /*
+     * Sets the lower bound on the size, in bytes, that this vector's underlying
+     * capacity has to be before its used pages will be protected.
+     */
+    void setLowerBoundForProtection(size_t bytes) {
+        if (protectionLowerBound != bytes) {
+            unprotectOldBuffer();
+            protectionLowerBound = bytes;
+            protectNewBuffer();
+        }
     }
 
     /*
