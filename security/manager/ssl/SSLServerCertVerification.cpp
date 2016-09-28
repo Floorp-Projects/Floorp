48fe9b31662f3813f65d463d50aff8adb4178b27
Sergei Chernov <sergei.cv@ndivi.com>
2016-09-28 20:19:56 +0300

    Bug 1305289 - Certificate Transparency - basic UI indicator; r=Dolske,keeler
    MozReview-Commit-ID: b0SUW2WNJT

--- security/manager/ssl/SSLServerCertVerification.cpp
+++ security/manager/ssl/SSLServerCertVerification.cpp
@@ -1424,6 +1424,8 @@ AuthCertificate(CertVerifier& certVerifier,
       MOZ_LOG(gPIPNSSLog, LogLevel::Debug,
              ("AuthCertificate setting NEW cert %p\n", nsc.get()));
     }
+
+    status->SetCertificateTransparencyInfo(certificateTransparencyInfo);
   }
 
   if (rv != SECSuccess) {
