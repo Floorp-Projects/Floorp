43d5a7bbd116c686ee9db7cb32e46373ba9dd570
Alastair Pharo <asppsa@gmail.com>
2016-09-28 07:47:35 +0200

    Bug 1304019 - Add support for setting task's index rank r=dustin
    MozReview-Commit-ID: I5pPbPiW7pY

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -104,6 +104,8 @@ task_description_schema = Schema({
                 Required('gecko-v2'): basestring,
             }
         ),
+
+        'rank': Any('by-tier', int, 'pushdate')
     },
 
     # The `run_on_projects` attribute, defaulting to "all".  This dictates the
@@ -433,12 +435,20 @@ def add_index_routes(config, tasks):
             for tpl in V2_ROUTE_TEMPLATES:
                 routes.append(tpl.format(**subs))
 
-        # rank is zero for non-tier-1 tasks and based on pushid for others;
-        # this sorts tier-{2,3} builds below tier-1 in the index
-        tier = task.get('treeherder', {}).get('tier', 3)
-        task.setdefault('extra', {})['index'] = {
-            'rank': 0 if tier > 1 else int(config.params['pushdate'])
-        }
+        # The default behavior is to rank tasks according to their tier
+        extra_index = task.setdefault('extra', {}).setdefault('index', {})
+        rank = index.get('rank', 'by-tier')
+
+        if rank == 'by-tier':
+            # rank is zero for non-tier-1 tasks and based on pushid for others;
+            # this sorts tier-{2,3} builds below tier-1 in the index
+            tier = task.get('treeherder', {}).get('tier', 3)
+            extra_index['rank'] = 0 if tier > 1 else int(config.params['pushdate'])
+        elif rank == 'pushdate':
+            extra_index['rank'] = int(config.params['pushdate'])
+        else:
+            extra_index['rank'] = rank
+
         del task['index']
         yield task
 
