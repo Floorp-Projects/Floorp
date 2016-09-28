483e37e831c54a8cd7153550f3a26c922c56abc4
Jeff Gilbert <jdashg@gmail.com>
2016-09-28 16:32:34 -0700

    Bug 1306174 - Remove ETC support from core WebGL2. r=ethlin, a=ritu

--- dom/canvas/WebGLContextExtensions.cpp
+++ dom/canvas/WebGLContextExtensions.cpp
@@ -48,7 +48,7 @@ WebGLContext::GetExtensionString(WebGLExtensionID ext)
         WEBGL_EXTENSION_IDENTIFIER(OES_vertex_array_object)
         WEBGL_EXTENSION_IDENTIFIER(WEBGL_color_buffer_float)
         WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_atc)
-        WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_es3)
+        WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_es3_0)
         WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_etc1)
         WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_pvrtc)
         WEBGL_EXTENSION_IDENTIFIER(WEBGL_compressed_texture_s3tc)
@@ -122,6 +122,8 @@ WebGLContext::IsExtensionSupported(WebGLExtensionID ext) const
     // WEBGL_
     case WebGLExtensionID::WEBGL_compressed_texture_atc:
         return gl->IsExtensionSupported(gl::GLContext::AMD_compressed_ATC_texture);
+    case WebGLExtensionID::WEBGL_compressed_texture_es3_0:
+        return gl->IsSupported(gl::GLFeature::ES3_compatibility);
     case WebGLExtensionID::WEBGL_compressed_texture_etc1:
         return gl->IsExtensionSupported(gl::GLContext::OES_compressed_ETC1_RGB8_texture);
     case WebGLExtensionID::WEBGL_compressed_texture_pvrtc:
@@ -212,8 +214,6 @@ WebGLContext::IsExtensionSupported(WebGLExtensionID ext) const
             switch (ext) {
             case WebGLExtensionID::EXT_disjoint_timer_query:
                 return WebGLExtensionDisjointTimerQuery::IsSupported(this);
-            case WebGLExtensionID::WEBGL_compressed_texture_es3:
-                return gl->IsExtensionSupported(gl::GLContext::ARB_ES3_compatibility);
 
             default:
                 // For warnings-as-errors.
@@ -395,7 +395,7 @@ WebGLContext::EnableExtension(WebGLExtensionID ext)
     case WebGLExtensionID::WEBGL_compressed_texture_atc:
         obj = new WebGLExtensionCompressedTextureATC(this);
         break;
-    case WebGLExtensionID::WEBGL_compressed_texture_es3:
+    case WebGLExtensionID::WEBGL_compressed_texture_es3_0:
         obj = new WebGLExtensionCompressedTextureES3(this);
         break;
     case WebGLExtensionID::WEBGL_compressed_texture_etc1:
