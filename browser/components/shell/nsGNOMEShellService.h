4311cc732c91d75e3db9985ab3d320b8fbb74572
Enes Goktas <egoktas@mozilla.com>
2016-09-27 19:54:37 -0700

    Bug 1302855 - Fold browsercomps into xul; r=bsmedberg
    MozReview-Commit-ID: 8oaVngXFfh3

--- browser/components/shell/nsGNOMEShellService.h
+++ browser/components/shell/nsGNOMEShellService.h
@@ -7,7 +7,7 @@
 #define nsgnomeshellservice_h____
 
 #include "nsIGNOMEShellService.h"
-#include "nsStringAPI.h"
+#include "nsString.h"
 #include "mozilla/Attributes.h"
 
 class nsGNOMEShellService final : public nsIGNOMEShellService
