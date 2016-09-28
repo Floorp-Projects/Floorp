752f4ef74d540343e132e3cec8ca25dc3662b9ba
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-09-28 16:54:12 +0100

    Bug 1274633 - Remove official branding 'learn more' page. r=MattN, a=ritu
    MozReview-Commit-ID: 6OtpPeaw0zj

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -4,7 +4,7 @@
 
 pref("startup.homepage_override_url", "");
 pref("startup.homepage_welcome_url", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/");
-pref("startup.homepage_welcome_url.additional", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/firstrun/learnmore/");
+pref("startup.homepage_welcome_url.additional", "");
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
 // The time interval between the downloading of mar file chunks in the
