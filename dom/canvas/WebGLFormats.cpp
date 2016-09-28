e2360a09c61fbde7146f8f9f8f7f0a8f68aae2c4
Jeff Gilbert <jdashg@gmail.com>
2016-09-28 16:32:34 -0700

    Bug 1306174 - Remove ETC support from core WebGL2. - r=ethlin a=ritu

--- dom/canvas/WebGLFormats.cpp
+++ dom/canvas/WebGLFormats.cpp
@@ -959,28 +959,6 @@ FormatUsageAuthority::CreateForWebGL2(gl::GLContext* gl)
     fnAllowES3TexFormat(FOO(DEPTH24_STENCIL8  ), true, false);
     fnAllowES3TexFormat(FOO(DEPTH32F_STENCIL8 ), true, false);
 
-    // GLES 3.0.4, p147, table 3.19
-    // GLES 3.0.4, p286+, $C.1 "ETC Compressed Texture Image Formats"
-    // Note that all compressed texture formats are filterable:
-    // GLES 3.0.4 p161:
-    // "[A] texture is complete unless any of the following conditions hold true:
-    //  [...]
-    //  * The effective internal format specified for the texture arrays is a sized
-    //    internal color format that is not texture-filterable (see table 3.13) and [the
-    //    mag filter requires filtering]."
-    // Compressed formats are not sized internal color formats, and indeed they are not
-    // listed in table 3.13.
-    fnAllowES3TexFormat(FOO(COMPRESSED_RGB8_ETC2                     ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_SRGB8_ETC2                    ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_RGBA8_ETC2_EAC                ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_SRGB8_ALPHA8_ETC2_EAC         ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_R11_EAC                       ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_RG11_EAC                      ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_SIGNED_R11_EAC                ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_SIGNED_RG11_EAC               ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_RGB8_PUNCHTHROUGH_ALPHA1_ETC2 ), false, true);
-    fnAllowES3TexFormat(FOO(COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2), false, true);
-
 #undef FOO
 
     // GLES 3.0.4, p206, "Required Renderbuffer Formats":
