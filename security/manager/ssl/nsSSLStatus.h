5c32cb31655ecbcaec9a0094e5f42f90bcbe8460
Sergei Chernov <sergei.cv@ndivi.com>
2016-09-28 20:19:56 +0300

    Bug 1305289 - Certificate Transparency - basic UI indicator; r=Dolske,keeler
    MozReview-Commit-ID: b0SUW2WNJT

--- security/manager/ssl/nsSSLStatus.h
+++ security/manager/ssl/nsSSLStatus.h
@@ -7,6 +7,7 @@
 #ifndef _NSSSLSTATUS_H
 #define _NSSSLSTATUS_H
 
+#include "CertVerifier.h" // For CertificateTransparencyInfo
 #include "nsISSLStatus.h"
 #include "nsCOMPtr.h"
 #include "nsXPIDLString.h"
@@ -37,9 +38,13 @@ public:
     return mServerCert != nullptr;
   }
 
+  void SetCertificateTransparencyInfo(
+    const mozilla::psm::CertificateTransparencyInfo& info);
+
   /* public for initilization in this file */
   uint16_t mCipherSuite;
   uint16_t mProtocolVersion;
+  uint16_t mCertificateTransparencyStatus;
 
   bool mIsDomainMismatch;
   bool mIsNotValidAtThisTime;
