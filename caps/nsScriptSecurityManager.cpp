9f0454f829ba104631fa188f020a149291ba4402
Olli Pettay <Olli.Pettay@helsinki.fi>
2016-09-29 20:31:50 +0300

    Bug 1306300, null check nsILoadContext in GetLoadContextCodebasePrincipal, r=baku

--- caps/nsScriptSecurityManager.cpp
+++ caps/nsScriptSecurityManager.cpp
@@ -1169,6 +1169,7 @@ nsScriptSecurityManager::
                                   nsILoadContext* aLoadContext,
                                   nsIPrincipal** aPrincipal)
 {
+  NS_ENSURE_STATE(aLoadContext);
   DocShellOriginAttributes docShellAttrs;
   bool result = aLoadContext->GetOriginAttributes(docShellAttrs);;
   NS_ENSURE_TRUE(result, NS_ERROR_FAILURE);
