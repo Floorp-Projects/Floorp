d520a4e996f9250bac3af2a82d8d1bb28e3decac
Mats Palmgren <mats@mozilla.com>
2016-09-29 21:49:54 +0200

    Bug 1303643 part 1 - Add a flag to control whether percentages should be applied also to min-content contributions.  r=dholbert

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -1381,6 +1381,7 @@ public:
     IGNORE_PADDING = 0x01,
     BAIL_IF_REFLOW_NEEDED = 0x02, // returns NS_INTRINSIC_WIDTH_UNKNOWN if so
     MIN_INTRINSIC_ISIZE = 0x04, // use min-width/height instead of width/height
+    ADD_PERCENTS = 0x08, // apply AddPercents also for MIN_ISIZE
   };
   static nscoord IntrinsicForAxis(mozilla::PhysicalAxis aAxis,
                                   nsRenderingContext*   aRenderingContext,
@@ -1423,14 +1424,10 @@ public:
    * 'aPercent' percent of the returned value.  If 'aPercent' is less than
    * or equal to zero the original 'aCurrent' value is returned. If 'aPercent'
    * is greater than or equal to 1.0 the value nscoord_MAX is returned.
-   * (We don't increase the size if MIN_ISIZE is passed in, though.)
    */
-  static nscoord AddPercents(IntrinsicISizeType aType, nscoord aCurrent,
-                             float aPercent)
+  static nscoord AddPercents(nscoord aCurrent, float aPercent)
   {
-    if (aPercent > 0.0f && aType == nsLayoutUtils::PREF_ISIZE) {
-      // XXX Should we also consider percentages for min widths, up to a
-      // limit?
+    if (aPercent > 0.0f) {
       return MOZ_UNLIKELY(aPercent >= 1.0f) ? nscoord_MAX
         : NSToCoordRound(float(aCurrent) / (1.0f - aPercent));
     }
