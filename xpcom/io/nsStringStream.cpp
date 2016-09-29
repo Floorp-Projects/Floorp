d553ddf41eac1bb730da82b3259b2f3dd8649bae
Josh Matthews <josh@joshmatthews.net>
2016-09-29 06:20:00 +0200

    Bug 1294450 - Make AutoIPCStream favour PSendStream for large input streams. r=nfroydj

--- xpcom/io/nsStringStream.cpp
+++ xpcom/io/nsStringStream.cpp
@@ -25,6 +25,8 @@
 #include "nsIIPCSerializableInputStream.h"
 
 using namespace mozilla::ipc;
+using mozilla::Maybe;
+using mozilla::Some;
 
 //-----------------------------------------------------------------------------
 // nsIStringInputStream implementation
@@ -352,6 +354,12 @@ nsStringInputStream::Deserialize(const InputStreamParams& aParams,
   }
 
   return true;
+}
+
+Maybe<uint64_t>
+nsStringInputStream::ExpectedSerializedLength()
+{
+  return Some(static_cast<uint64_t>(Length()));
 }
 
 /////////
