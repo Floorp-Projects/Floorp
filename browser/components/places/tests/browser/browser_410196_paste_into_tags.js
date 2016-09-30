57eef99a44e3b3fa0f318ec359bb0150778ef0a7
Pascal Chevrel <pascal.chevrel@free.fr>
2016-09-30 06:58:32 +0200

    Bug 1283076 - Change default bookmarks for Nightly. r=dolske
    - no change on default bookmarks for non-nightly builds
    - replace firefox bookmarks by this list for Nightly:
      - Link to the Firefox Nightly blog (https://blog.nightly.mozilla.org/)
      - Link to Bugzilla https://bugzilla.mozilla.org/
      - Link to MDN https://developer.mozilla.org/
      - Link to Nightly Tester Tools addon https://addons.mozilla.org/en-US/firefox/addon/nightly-tester-tools/
      - Link to about:crashes
      - Link to Nightly IRC channel on irc.mozilla.org via mibbit
      - Link to Planet Mozilla https://planet.mozilla.org/
    
    The default bookmark in the bookmark toolbar which was a tour of Firefox feature is now a link to mozilla.org/contribute
    
    This patch also:
    - removes all rdf id references in links which are unused
    - moves all the data-uri icons to replacement variables to avoid multiple copy-pastes and make the code easier to read
    - removes the invalid mention that the file was automatically generated, it is not the case
    - updates the meta charset line for a shorter one using HTML5 syntax
    - uses lowercase html
    - some of the links have keywords or tags defined
    - update tests with specific values to test for the Nightly channel vs other channels

--- browser/components/places/tests/browser/browser_410196_paste_into_tags.js
+++ browser/components/places/tests/browser/browser_410196_paste_into_tags.js
@@ -93,7 +93,9 @@ function focusTag(PlacesOrganizer) {
   let fooTag = tags.getChild(0);
   let tagNode = fooTag;
   PlacesOrganizer._places.selectNode(fooTag);
-  is(tagNode.title, 'foo', "tagNode title is foo");
+  // Bug 1283076: Nightly has a default 'bug' tag already set
+  let tagValue = AppConstants.NIGHTLY_BUILD ? 'bug' : 'foo';
+  is(tagNode.title, tagValue, "tagNode title is " + tagValue);
   let ip = PlacesOrganizer._places.insertionPoint;
   ok(ip.isTag, "IP is a tag");
 }
