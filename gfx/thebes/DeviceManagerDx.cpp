9b8774ca603e79ea68bf798ce8c1977f18840005
Wes Kocher <wkocher@mozilla.com>
2016-09-29 18:10:17 -0700

    Backed out changeset e1300d4c0a52 (bug 1292923) for windows reftest failures a=backout

--- gfx/thebes/DeviceManagerDx.cpp
+++ gfx/thebes/DeviceManagerDx.cpp
@@ -671,18 +671,6 @@ DeviceManagerDx::TextureSharingWorks()
 }
 
 bool
-DeviceManagerDx::CanInitializeKeyedMutexTextures()
-{
-  MutexAutoLock lock(mDeviceLock);
-  if (!mDeviceStatus) {
-    return false;
-  }
-  // Disable this on all Intel devices because of crashes.
-  // See bug 1292923.
-  return mDeviceStatus->adapter().VendorId != 0x8086;
-}
-
-bool
 DeviceManagerDx::IsWARP()
 {
   MutexAutoLock lock(mDeviceLock);
