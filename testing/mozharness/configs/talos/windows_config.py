240e00321ec4d73eb1d8623eeb5cfb38e02cc5dd
Phil Ringnalda <philringnalda@gmail.com>
2016-09-30 18:39:22 -0700

    Backed out 3 changesets (bug 1306691) for Windows mda bustage
    Backed out changeset 3aa78ab9fce9 (bug 1306691)
    Backed out changeset 2a95fbb01ffe (bug 1306691)
    Backed out changeset c7d8bed34d87 (bug 1306691)

--- testing/mozharness/configs/talos/windows_config.py
+++ testing/mozharness/configs/talos/windows_config.py
@@ -16,10 +16,12 @@ config = {
         "http://pypi.pvt.build.mozilla.org/pub",
         "http://pypi.pub.build.mozilla.org/pub",
     ],
-    "virtualenv_modules": ['talos', 'mozinstall'],
+    "virtualenv_modules": ['pywin32', 'talos', 'mozinstall'],
     "exes": {
         'python': PYTHON,
         'virtualenv': [PYTHON, 'c:/mozilla-build/buildbotve/virtualenv.py'],
+        'easy_install': ['%s/scripts/python' % VENV_PATH,
+                         '%s/scripts/easy_install-2.7-script.py' % VENV_PATH],
         'mozinstall': ['%s/scripts/python' % VENV_PATH,
                        '%s/scripts/mozinstall-script.py' % VENV_PATH],
         'hg': 'c:/mozilla-build/hg/hg',
