b1b922dcdb3e5e73d55be3f43dd34147b594d1a1
Lin Clark <lclark@mozilla.com>
2016-10-02 15:16:49 -0700

    Bug 1306775 - New console frontend: Fix classes on page errors and console commands. r=me
    MozReview-Commit-ID: 97gjsvbKt1d

--- devtools/client/webconsole/new-console-output/components/message-types/page-error.js
+++ devtools/client/webconsole/new-console-output/components/message-types/page-error.js
@@ -33,7 +33,8 @@ function PageError(props) {
   } = props;
   const {
     id: messageId,
-    source, type,
+    source,
+    type,
     level,
     messageText: messageBody,
     repeat,
@@ -47,6 +48,7 @@ function PageError(props) {
     source,
     type,
     level,
+    topLevelClasses: [],
     messageBody,
     repeat,
     frame,
