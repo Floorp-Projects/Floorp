c4d1326d68b4c4cf5063e13fa30e473b9f5ccdd1
Kartikaya Gupta <kgupta@mozilla.com>
2016-10-04 14:41:15 -0400

    Bug 1307522 - Remove a bunch of unnecessary methods from the PanZoomTarget interface now that JPZ is gone. r=snorp
    MozReview-Commit-ID: CPIDkGX3jhr

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoLayerClient.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoLayerClient.java
@@ -267,7 +267,7 @@ class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         // Page size is owned by the layer client, so no need to notify it of
         // this change.
 
-        post(new Runnable() {
+        mView.post(new Runnable() {
             @Override
             public void run() {
                 mView.requestRender();
@@ -318,7 +318,7 @@ class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
             // viewport-related state we have lying around (i.e. mViewportMetrics).
             // Usually this information is updated via handleViewportMessage
             // while we remain on the same document.
-            setViewportMetrics(newMetrics);
+            setViewportMetrics(newMetrics, true);
 
             // Indicate that the document is about to be composited so the
             // LayerView background can be removed.
@@ -641,35 +641,8 @@ class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         setViewportSize(viewportSize.width, viewportSize.height, null);
     }
 
-    /** Implementation of PanZoomTarget */
-    @Override
-    public ImmutableViewportMetrics getViewportMetrics() {
+    ImmutableViewportMetrics getViewportMetrics() {
         return mViewportMetrics;
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public FullScreenState getFullScreenState() {
-        return mView.getFullScreenState();
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public PointF getVisibleEndOfLayerView() {
-        return mToolbarAnimator.getVisibleEndOfLayerView();
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public void setAnimationTarget(ImmutableViewportMetrics metrics) {
-    }
-
-    /** Implementation of PanZoomTarget
-     * You must hold the monitor while calling this.
-     */
-    @Override
-    public void setViewportMetrics(ImmutableViewportMetrics metrics) {
-        setViewportMetrics(metrics, true);
     }
 
     /*
@@ -712,61 +685,24 @@ class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         viewportMetricsChanged(notifyGecko);
     }
 
-    /** Implementation of PanZoomTarget
-     * Scroll the viewport by a certain amount. This will take viewport margins
-     * and margin animation into account. If margins are currently animating,
-     * this will just go ahead and modify the viewport origin, otherwise the
-     * delta will be applied to the margins and the remainder will be applied to
-     * the viewport origin.
-     *
-     * You must hold the monitor while calling this.
-     */
-    @Override
-    public void scrollBy(float dx, float dy) {
-        // Set mViewportMetrics manually so the margin changes take.
-        mViewportMetrics = mViewportMetrics.offsetViewportBy(dx, dy);
-        viewportMetricsChanged(true);
-    }
-
     /** Implementation of PanZoomTarget */
     @Override
     public void panZoomStopped() {
         mToolbarAnimator.onPanZoomStopped();
     }
 
-    /** Implementation of PanZoomTarget */
-    @Override
-    public boolean post(Runnable action) {
-        return mView.post(action);
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public void postRenderTask(RenderTask task) {
-        mView.postRenderTask(task);
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public void removeRenderTask(RenderTask task) {
-        mView.removeRenderTask(task);
-    }
-
-    /** Implementation of PanZoomTarget */
-    @Override
-    public Object getLock() {
+    Object getLock() {
         return this;
     }
 
-    /** Implementation of PanZoomTarget
+    /**
      * Converts a point from layer view coordinates to layer coordinates. In other words, given a
      * point measured in pixels from the top left corner of the layer view, returns the point in
      * pixels measured from the last scroll position we sent to Gecko, in CSS pixels. Assuming the
      * events being sent to Gecko are processed in FIFO order, this calculation should always be
      * correct.
      */
-    @Override
-    public PointF convertViewPointToLayerPoint(PointF viewPoint) {
+    PointF convertViewPointToLayerPoint(PointF viewPoint) {
         if (!mGeckoIsReady) {
             return null;
         }
@@ -790,8 +726,7 @@ class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         return layerPoint;
     }
 
-    @Override
-    public Matrix getMatrixForLayerRectToViewRect() {
+    Matrix getMatrixForLayerRectToViewRect() {
         if (!mGeckoIsReady) {
             return null;
         }
