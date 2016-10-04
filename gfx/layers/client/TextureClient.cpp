a416002aa786731e25303ede081eb1adf9d3574f
Matt Woodrow <mwoodrow@mozilla.com>
2016-10-04 15:14:56 +1300

    Bug 1297568 - Remove some invalid assertions that can happen when the GPU process crashes. r=dvander

--- gfx/layers/client/TextureClient.cpp
+++ gfx/layers/client/TextureClient.cpp
@@ -1071,7 +1071,6 @@ TextureClient::CreateForDrawing(TextureForwarder* aAllocator,
   gfx::BackendType moz2DBackend = BackendTypeForBackendSelector(aLayersBackend, aSelector);
 
   // also test the validity of aAllocator
-  MOZ_ASSERT(aAllocator && aAllocator->IPCOpen());
   if (!aAllocator || !aAllocator->IPCOpen()) {
     return nullptr;
   }
@@ -1168,7 +1167,6 @@ TextureClient::CreateFromSurface(KnowsCompositor* aAllocator,
                                  TextureAllocationFlags aAllocFlags)
 {
   // also test the validity of aAllocator
-  MOZ_ASSERT(aAllocator && aAllocator->GetTextureForwarder()->IPCOpen());
   if (!aAllocator || !aAllocator->GetTextureForwarder()->IPCOpen()) {
     return nullptr;
   }
