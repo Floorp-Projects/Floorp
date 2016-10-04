240d11e76ddd5cb121c7f92e40cae693d1453435
Greg Tatum <tatum.creative@gmail.com>
2016-10-04 15:15:15 -0500

    Bug 1307009 - Remove caching optimization; r=tromey
    This caching feature is no longer valid as the client-side database
    will not necessarily be the same as the server since the DB is
    only built on nightly.
    
    MozReview-Commit-ID: 550EQmgO9O7

--- devtools/shared/specs/css-properties.js
+++ devtools/shared/specs/css-properties.js
@@ -3,16 +3,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { Arg, RetVal, generateActorSpec } = require("devtools/shared/protocol");
+const { RetVal, generateActorSpec } = require("devtools/shared/protocol");
 
 const cssPropertiesSpec = generateActorSpec({
   typeName: "cssProperties",
 
   methods: {
     getCSSDatabase: {
-      request: {
-        clientBrowserVersion: Arg(0, "string"),
-      },
+      request: {},
 
       response: RetVal("json"),
     }
