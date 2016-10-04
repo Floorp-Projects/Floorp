c430bf36ec3492fef16c6a87124c5977ad06b518
Bas Schouten <bschouten@mozilla.com>
2016-10-04 20:33:47 +0200

    Bug 1291084: Unconditionally create all devices as threadsafe. r=mattwoodrow
    MozReview-Commit-ID: GWJWd7oXoOb

--- gfx/thebes/DeviceManagerDx.cpp
+++ gfx/thebes/DeviceManagerDx.cpp
@@ -17,6 +17,7 @@
 #include "mozilla/layers/CompositorThread.h"
 #include "nsIGfxInfo.h"
 #include <d3d11.h>
+#include <d3d11_4.h>
 #include <ddraw.h>
 
 namespace mozilla {
@@ -374,9 +375,19 @@ DeviceManagerDx::CreateDevice(IDXGIAdapter* aAdapter,
       aFlags,
       mFeatureLevels.Elements(), mFeatureLevels.Length(),
       D3D11_SDK_VERSION, getter_AddRefs(aOutDevice), nullptr, nullptr);
+
   } MOZ_SEH_EXCEPT (EXCEPTION_EXECUTE_HANDLER) {
     return false;
   }
+
+  RefPtr<ID3D10Multithread> mt;
+  HRESULT hr = aOutDevice->QueryInterface((ID3D10Multithread**)getter_AddRefs(mt));
+  if (SUCCEEDED(hr) && mt) {
+    mt->SetMultithreadProtected(TRUE);
+  } else {
+    gfxCriticalError() << "Failed to set device safe for multithreading.";
+  }
+
   return true;
 }
 
@@ -483,11 +494,6 @@ DeviceManagerDx::CreateContentDevice()
   }
   mContentDevice->SetExceptionMode(0);
 
-  RefPtr<ID3D10Multithread> multi;
-  hr = mContentDevice->QueryInterface(__uuidof(ID3D10Multithread), getter_AddRefs(multi));
-  if (SUCCEEDED(hr) && multi) {
-    multi->SetMultithreadProtected(TRUE);
-  }
   return FeatureStatus::Available;
 }
 
@@ -496,12 +502,6 @@ DeviceManagerDx::CreateDecoderDevice()
 {
   if (mCompositorDevice && mCompositorDeviceSupportsVideo && !mDecoderDevice) {
     mDecoderDevice = mCompositorDevice;
-
-    RefPtr<ID3D10Multithread> multi;
-    mDecoderDevice->QueryInterface(__uuidof(ID3D10Multithread), getter_AddRefs(multi));
-    if (multi) {
-      multi->SetMultithreadProtected(TRUE);
-    }
   }
 
   if (mDecoderDevice) {
@@ -530,11 +530,6 @@ DeviceManagerDx::CreateDecoderDevice()
   if (FAILED(hr) || !device || !D3D11Checks::DoesDeviceWork()) {
     return nullptr;
   }
-
-  RefPtr<ID3D10Multithread> multi;
-  device->QueryInterface(__uuidof(ID3D10Multithread), getter_AddRefs(multi));
-
-  multi->SetMultithreadProtected(TRUE);
 
   mDecoderDevice = device;
   return device;
