937fbae9a48954a3183a346673c20fd9931e71a0
Phil Ringnalda <philringnalda@gmail.com>
2016-10-03 20:17:28 -0700

    Backed out changeset b26e668306fb (bug 1278687) for making test_ext_webrequest.html between near-permaorange and actual-permaorange

--- testing/specialpowers/content/specialpowersAPI.js
+++ testing/specialpowers/content/specialpowersAPI.js
@@ -1945,23 +1945,7 @@ SpecialPowersAPI.prototype = {
   },
 
   _nextExtensionID: 0,
-  _extensionListeners: null,
-
   loadExtension: function(ext, handler) {
-    if (this._extensionListeners == null) {
-      this._extensionListeners = new Set();
-
-      this._addMessageListener("SPExtensionMessage", msg => {
-        for (let listener of this._extensionListeners) {
-          try {
-            listener(msg);
-          } catch (e) {
-            Cu.reportError(e);
-          }
-        }
-      });
-    }
-
     // Note, this is not the addon is as used by the AddonManager etc,
     // this is just an identifier used for specialpowers messaging
     // between this content process and the chrome process.
@@ -1975,7 +1959,7 @@ SpecialPowersAPI.prototype = {
     let unloadPromise = new Promise(resolve => { resolveUnload = resolve; });
 
     startupPromise.catch(() => {
-      this._extensionListeners.delete(listener);
+      this._removeMessageListener("SPExtensionMessage", listener);
     });
 
     handler = Cu.waiveXrays(handler);
@@ -2016,7 +2000,7 @@ SpecialPowersAPI.prototype = {
           state = "failed";
           rejectStartup("startup failed");
         } else if (msg.data.type == "extensionUnloaded") {
-          this._extensionListeners.delete(listener);
+          this._removeMessageListener("SPExtensionMessage", listener);
           state = "unloaded";
           resolveUnload();
         } else if (msg.data.type in handler) {
@@ -2027,7 +2011,7 @@ SpecialPowersAPI.prototype = {
       }
     };
 
-    this._extensionListeners.add(listener);
+    this._addMessageListener("SPExtensionMessage", listener);
     return extension;
   },
 
