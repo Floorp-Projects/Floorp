b76c5e8295edb0e8d59ecc99f2627330319bc2ef
Sean Stangl <sstangl@mozilla.com>
2016-10-03 21:40:23 -0400

    Bug 1303178 - Make ARM ScratchRegister usage explicit. r=nbp

--- js/src/jit/arm/SharedICHelpers-arm.h
+++ js/src/jit/arm/SharedICHelpers-arm.h
@@ -87,11 +87,14 @@ EmitBaselineTailCallVM(JitCode* target, MacroAssembler& masm, uint32_t argSize)
 
     // Compute frame size.
     masm.movePtr(BaselineFrameReg, r0);
-    masm.ma_add(Imm32(BaselineFrame::FramePointerOffset), r0);
+    masm.as_add(r0, r0, Imm8(BaselineFrame::FramePointerOffset));
     masm.ma_sub(BaselineStackReg, r0);
 
     // Store frame size without VMFunction arguments for GC marking.
-    masm.ma_sub(r0, Imm32(argSize), r1);
+    {
+        ScratchRegisterScope scratch(masm);
+        masm.ma_sub(r0, Imm32(argSize), r1, scratch);
+    }
     masm.store32(r1, Address(BaselineFrameReg, BaselineFrame::reverseOffsetOfFrameSize()));
 
     // Push frame descriptor and perform the tail call.
@@ -133,7 +136,7 @@ EmitBaselineCreateStubFrameDescriptor(MacroAssembler& masm, Register reg, uint32
     // Compute stub frame size. We have to add two pointers: the stub reg and
     // previous frame pointer pushed by EmitEnterStubFrame.
     masm.mov(BaselineFrameReg, reg);
-    masm.ma_add(Imm32(sizeof(void*) * 2), reg);
+    masm.as_add(reg, reg, Imm8(sizeof(void*) * 2));
     masm.ma_sub(BaselineStackReg, reg);
 
     masm.makeFrameDescriptor(reg, JitFrame_BaselineStub, headerSize);
@@ -174,7 +177,7 @@ EmitBaselineEnterStubFrame(MacroAssembler& masm, Register scratch)
 
     // Compute frame size.
     masm.mov(BaselineFrameReg, scratch);
-    masm.ma_add(Imm32(BaselineFrame::FramePointerOffset), scratch);
+    masm.as_add(scratch, scratch, Imm8(BaselineFrame::FramePointerOffset));
     masm.ma_sub(BaselineStackReg, scratch);
 
     masm.store32(scratch, Address(BaselineFrameReg, BaselineFrame::reverseOffsetOfFrameSize()));
