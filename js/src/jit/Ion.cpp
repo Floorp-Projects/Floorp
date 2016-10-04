d0b07a8db63d96d90319063d0acbf55f35b3d949
Jan de Mooij <jdemooij@mozilla.com>
2016-10-04 12:07:30 +0200

    Bug 1303710 - Don't Ion-compile scripts with too many typesets. r=bhackett, a=sylvestre

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -2381,6 +2381,13 @@ CheckScript(JSContext* cx, JSScript* script, bool osr)
         return false;
     }
 
+    if (script->nTypeSets() >= UINT16_MAX) {
+        // In this case multiple bytecode ops can share a single observed
+        // TypeSet (see bug 1303710).
+        TrackAndSpewIonAbort(cx, script, "too many typesets");
+        return false;
+    }
+
     return true;
 }
 
