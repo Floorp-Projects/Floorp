c43e36721568e48857e96d82e64bae6965627f82
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-10-05 17:02:57 +0200

    Backed out changeset 5505acbab659 (bug 1291715)

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.h
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.h
@@ -76,7 +76,6 @@ class DOMMediaStream;
 namespace dom {
 class RTCCertificate;
 struct RTCConfiguration;
-class RTCDTMFSender;
 struct RTCIceServer;
 struct RTCOfferOptions;
 struct RTCRtpParameters;
@@ -439,14 +438,14 @@ public:
   AddTrack(mozilla::dom::MediaStreamTrack& aTrack, DOMMediaStream& aStream);
 
   NS_IMETHODIMP_TO_ERRORRESULT(InsertDTMF, ErrorResult &rv,
-                               dom::RTCRtpSender& sender,
-                               const nsAString& tones,
-                               uint32_t duration, uint32_t interToneGap) {
+                               mozilla::dom::RTCRtpSender& sender,
+                               const nsAString& tones, uint32_t duration,
+                               uint32_t interToneGap) {
     rv = InsertDTMF(sender, tones, duration, interToneGap);
   }
 
   NS_IMETHODIMP_TO_ERRORRESULT(GetDTMFToneBuffer, ErrorResult &rv,
-                               dom::RTCRtpSender& sender,
+                               mozilla::dom::RTCRtpSender& sender,
                                nsAString& outToneBuffer) {
     rv = GetDTMFToneBuffer(sender, outToneBuffer);
   }
@@ -851,22 +850,6 @@ private:
   // storage for Telemetry data
   uint16_t mMaxReceiving[SdpMediaSection::kMediaTypes];
   uint16_t mMaxSending[SdpMediaSection::kMediaTypes];
-
-  // DTMF
-  struct DTMFState {
-    PeerConnectionImpl* mPeerConnectionImpl;
-    nsCOMPtr<nsITimer> mSendTimer;
-    std::string mTrackId;
-    nsString mTones;
-    size_t mLevel;
-    uint32_t mDuration;
-    uint32_t mInterToneGap;
-  };
-
-  static void
-  DTMFSendTimerCallback_m(nsITimer* timer, void*);
-
-  nsTArray<DTMFState> mDTMFStates;
 
 public:
   //these are temporary until the DataChannel Listen/Connect API is removed
