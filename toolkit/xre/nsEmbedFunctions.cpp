453a6a0da07b3c8fbcec48c37f71f975fefab296
Randall Barker <rbarker@mozilla.com>
2016-10-05 16:46:53 -0700

    Bug 1314466 - part 5, Add service process manager r=snorp

--- toolkit/xre/nsEmbedFunctions.cpp
+++ toolkit/xre/nsEmbedFunctions.cpp
@@ -50,6 +50,9 @@
 #include "base/message_loop.h"
 #include "base/process_util.h"
 #include "chrome/common/child_process.h"
+#if defined(MOZ_WIDGET_ANDROID)
+#include "chrome/common/ipc_channel.h"
+#endif //  defined(MOZ_WIDGET_ANDROID)
 
 #include "mozilla/ipc/BrowserProcessSubThread.h"
 #include "mozilla/ipc/GeckoChildProcessHost.h"
@@ -221,6 +224,17 @@ namespace startup {
 GeckoProcessType sChildProcessType = GeckoProcessType_Default;
 } // namespace startup
 } // namespace mozilla
+
+#if defined(MOZ_WIDGET_ANDROID)
+void
+XRE_SetAndroidChildFds (int crashFd, int ipcFd)
+{
+#if defined(MOZ_CRASHREPORTER)
+  CrashReporter::SetNotificationPipeForChild(crashFd);
+#endif // defined(MOZ_CRASHREPORTER)
+  IPC::Channel::SetClientChannelFd(ipcFd);
+}
+#endif // defined(MOZ_WIDGET_ANDROID)
 
 void
 XRE_SetProcessType(const char* aProcessTypeString)
