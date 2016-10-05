49c984d90d35ce4b66c29df77ab1f0c3920cfc0b
Gerald Squelart <gsquelart@mozilla.com>
2016-10-05 15:04:04 -0700

    Bug 1263665 - Block libav < 54.35.1 - r=jya,gijs,smaug a=al
    And display user notification that libav may be vulnerable and should be
    updated.
    
    Note: New strings are hard-coded, as translation cannot be done in time.
    
    MozReview-Commit-ID: HgTXlDnj2Gw

--- dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.h
+++ dom/media/platforms/ffmpeg/FFmpegRuntimeLinker.h
@@ -17,13 +17,28 @@ class FFmpegRuntimeLinker
 public:
   static bool Init();
   static already_AddRefed<PlatformDecoderModule> CreateDecoderModule();
+  enum LinkStatus
+  {
+    LinkStatus_INIT = 0,  // Never been linked.
+    LinkStatus_SUCCEEDED, // Found a usable library.
+    // The following error statuses are sorted from most to least preferred
+    // (i.e., if more than one happens, the top one is chosen.)
+    LinkStatus_INVALID_FFMPEG_CANDIDATE, // Found ffmpeg with unexpected contents.
+    LinkStatus_UNUSABLE_LIBAV57, // Found LibAV 57, which we cannot use.
+    LinkStatus_INVALID_LIBAV_CANDIDATE, // Found libav with unexpected contents.
+    LinkStatus_OBSOLETE_FFMPEG,
+    LinkStatus_OBSOLETE_LIBAV,
+    LinkStatus_INVALID_CANDIDATE, // Found some lib with unexpected contents.
+    LinkStatus_NOT_FOUND, // Haven't found any library with an expected name.
+  };
+  static LinkStatus LinkStatusCode() { return sLinkStatus; }
+  static const char* LinkStatusString();
+  // Library name to which the sLinkStatus applies, or "" if not applicable.
+  static const char* LinkStatusLibraryName() { return sLinkStatusLibraryName; }
 
 private:
-  static enum LinkStatus {
-    LinkStatus_INIT = 0,
-    LinkStatus_FAILED,
-    LinkStatus_SUCCEEDED
-  } sLinkStatus;
+  static LinkStatus sLinkStatus;
+  static const char* sLinkStatusLibraryName;
 };
 
 }
