d319d31da9b40356a3b86b4a9b63973e68ae8198
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-10-05 17:11:01 +0200

    Backed out changeset 4fa21c0bcb63 (bug 1307009)

--- devtools/server/actors/css-properties.js
+++ devtools/server/actors/css-properties.js
@@ -10,6 +10,10 @@ loader.lazyGetter(this, "DOMUtils", () => {
   return Cc["@mozilla.org/inspector/dom-utils;1"].getService(Ci.inIDOMUtils);
 });
 
+loader.lazyGetter(this, "appInfo", () => {
+  return Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+});
+
 const protocol = require("devtools/shared/protocol");
 const { ActorClassWithSpec, Actor } = protocol;
 const { cssPropertiesSpec } = require("devtools/shared/specs/css-properties");
@@ -28,7 +32,15 @@ exports.CssPropertiesActor = ActorClassWithSpec(cssPropertiesSpec, {
     Actor.prototype.destroy.call(this);
   },
 
-  getCSSDatabase() {
+  getCSSDatabase(clientBrowserVersion) {
+    // If the client and server are both the same version of Firefox, do not return a
+    // database, use the client-side css-properties-db.js.
+    const serverBrowserVersion = appInfo.platformVersion.match(/^\d+/)[0];
+
+    if (clientBrowserVersion !== 0 && clientBrowserVersion === serverBrowserVersion) {
+      return {};
+    }
+
     const properties = generateCssProperties();
     const pseudoElements = DOMUtils.getCSSPseudoElementNames();
 
