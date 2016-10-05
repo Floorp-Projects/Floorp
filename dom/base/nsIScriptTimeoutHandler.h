4022ea5d6fafe45a817ef6f2909668c201012e2e
Andreas Farre <farre@mozilla.com>
2016-10-05 14:26:08 +0200

    Bug 1198381 - Extract nsITimeoutHandler from nsIScriptTimeoutHandler. r=smaug
    MozReview-Commit-ID: HJHrbodWYVf

--- dom/base/nsIScriptTimeoutHandler.h
+++ dom/base/nsIScriptTimeoutHandler.h
@@ -6,6 +6,7 @@
 #ifndef nsIScriptTimeoutHandler_h___
 #define nsIScriptTimeoutHandler_h___
 
+#include "nsITimeoutHandler.h"
 #include "nsTArray.h"
 #include "js/TypeDecls.h"
 #include "mozilla/Function.h"
@@ -26,7 +27,7 @@ class Function;
  * language agnostic way.
  */
 
-class nsIScriptTimeoutHandler : public nsISupports
+class nsIScriptTimeoutHandler : public nsITimeoutHandler
 {
 public:
   NS_DECLARE_STATIC_IID_ACCESSOR(NS_ISCRIPTTIMEOUTHANDLER_IID)
@@ -41,8 +42,6 @@ public:
   // Get the location of the script.
   // Note: The memory pointed to by aFileName is owned by the
   // nsIScriptTimeoutHandler and should not be freed by the caller.
-  virtual void GetLocation(const char **aFileName, uint32_t *aLineNo,
-                           uint32_t *aColumn) = 0;
 
   // If we have a Function, get the arguments for passing to it.
   virtual const nsTArray<JS::Value>& GetArgs() = 0;
