e099839460117fc5ff8b2a72768bc51e0c96ab2d
Gerald Squelart <gsquelart@mozilla.com>
2016-10-05 15:04:04 -0700

    Bug 1263665 - media.libavcodec.allow-obsolete=true bypasses blocking - r=jya
    If "media.libavcodec.allow-obsolete" is set to true, the checks for older
    libavcodec library versions are ignored.
    
    MozReview-Commit-ID: HBhHfFomsrr

--- dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
+++ dom/media/platforms/ffmpeg/FFmpegLibWrapper.cpp
@@ -4,6 +4,7 @@
 
 #include "FFmpegLibWrapper.h"
 #include "FFmpegLog.h"
+#include "MediaPrefs.h"
 #include "mozilla/PodOperations.h"
 #include "mozilla/Types.h"
 #include "prlink.h"
@@ -49,11 +50,16 @@ FFmpegLibWrapper::Link()
       // support FFmpeg 57 at this stage.
       Unlink();
       return LinkResult::CannotUseLibAV57;
-    } else if (version < (54u << 16 | 35u << 8 | 1u)) {
-      // Refuse any LibAV version prior to 54.35.1.
+    }
+#ifdef MOZ_FFMPEG
+    if (version < (54u << 16 | 35u << 8 | 1u)
+        && !MediaPrefs::LibavcodecAllowObsolete()) {
+      // Refuse any libavcodec version prior to 54.35.1.
+      // (Unless media.libavcodec.allow-obsolete==true)
       Unlink();
       return LinkResult::BlockedOldLibAVVersion;
     }
+#endif
   }
 
   enum {
