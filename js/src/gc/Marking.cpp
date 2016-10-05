e71c0fa9cede5dac51eabac01ff648516e3f3c59
Jon Coppeard <jcoppeard@mozilla.com>
2016-10-05 10:09:24 +0200

    Bug 1302682 - Add assertion to help catch incorrect alloc kind when tenuring r=sfink

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -2482,6 +2482,14 @@ js::TenuringTracer::traceSlots(Value* vp, Value* end)
         traverse(vp);
 }
 
+#ifdef DEBUG
+static inline ptrdiff_t
+OffsetToChunkEnd(void* p)
+{
+    return ChunkLocationOffset - (uintptr_t(p) & gc::ChunkMask);
+}
+#endif
+
 size_t
 js::TenuringTracer::moveObjectToTenured(JSObject* dst, JSObject* src, AllocKind dstKind)
 {
@@ -2501,6 +2509,7 @@ js::TenuringTracer::moveObjectToTenured(JSObject* dst, JSObject* src, AllocKind 
         tenuredSize = srcSize = sizeof(NativeObject);
 
     // Copy the Cell contents.
+    MOZ_ASSERT(OffsetToChunkEnd(src) >= ptrdiff_t(srcSize));
     js_memcpy(dst, src, srcSize);
 
     // Move any hash code attached to the object.
