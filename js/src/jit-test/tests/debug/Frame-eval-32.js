56d940dd14504ca2e4b2f730c474c5bfb795efdf
Ryan VanderMeulen <ryanvm@gmail.com>
2016-10-06 13:54:35 -0400

    Backed out changeset 33573c52d0e8 (bug 1285213) for bustage.

--- js/src/jit-test/tests/debug/Frame-eval-32.js
+++
@@ -1,8 +0,0 @@
-// |jit-test| error: ReferenceError
-
-// Test the TDZ works for glbao lexicals through Debugger environments in
-// compound assignments.
-load(libdir + "evalInFrame.js");
-
-evalInFrame(0, "x |= 0");
-let x;
