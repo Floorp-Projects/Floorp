f3a870685c52dbd6966940347e58cdcdc82b1f2c
Matt Woodrow <mwoodrow@mozilla.com>
2016-10-06 14:32:55 +0200

    Bug 1292923 - Don't upload to textures during creation on Intel cards as it frequently crashes. r=dvander

--- gfx/thebes/DeviceManagerDx.cpp
+++ gfx/thebes/DeviceManagerDx.cpp
@@ -671,6 +671,18 @@ DeviceManagerDx::TextureSharingWorks()
 }
 
 bool
+DeviceManagerDx::CanInitializeKeyedMutexTextures()
+{
+  MutexAutoLock lock(mDeviceLock);
+  if (!mDeviceStatus) {
+    return false;
+  }
+  // Disable this on all Intel devices because of crashes.
+  // See bug 1292923.
+  return mDeviceStatus->adapter().VendorId != 0x8086;
+}
+
+bool
 DeviceManagerDx::IsWARP()
 {
   MutexAutoLock lock(mDeviceLock);
