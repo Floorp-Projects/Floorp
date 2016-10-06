f1b48981a86f450a325dd66f01d888a8426f740a
Felipe Gomes <felipc@gmail.com>
2016-10-06 14:39:40 -0300

    Bug 1304164 - Temporarily disable e10s for users on the Russian locale. r=mconley a=lizzard
    MozReview-Commit-ID: Hrt6yEh1BBT

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -165,5 +165,15 @@ function optedOut() {
  * string must be returned.
  */
 function getTemporaryDisqualification() {
+  let applicationLanguage =
+    Cc["@mozilla.org/chrome/chrome-registry;1"]
+      .getService(Ci.nsIXULChromeRegistry)
+      .getSelectedLocale("global")
+      .split("-")[0];
+
+  if (applicationLanguage == "ru") {
+    return "ru";
+  }
+
   return "";
 }
