81cff1917b1b242bbce22608dcaf5abcf46381d8
Matt Woodrow <mwoodrow@mozilla.com>
2016-10-06 14:32:55 +0200

    Bug 1292923 - Don't upload to textures during creation on Intel cards as it frequently crashes. r=dvander

--- gfx/layers/IMFYCbCrImage.cpp
+++ gfx/layers/IMFYCbCrImage.cpp
@@ -237,7 +237,8 @@ IMFYCbCrImage::GetTextureClient(KnowsCompositor* aForwarder)
     return nullptr;
   }
 
-  if (!gfx::DeviceManagerDx::Get()->AlphaTextureSharingWorks()) {
+  if (!gfx::DeviceManagerDx::Get()->CanInitializeKeyedMutexTextures() ||
+      !gfx::DeviceManagerDx::Get()->AlphaTextureSharingWorks()) {
     return nullptr;
   }
 
