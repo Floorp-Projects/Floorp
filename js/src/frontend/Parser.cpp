74cf4232c22df31275c9fdcd2f962a1ab5c5a717
André Bargull <andre.bargull@gmail.com>
2016-10-06 21:54:24 -0700

    Bug 1305566 - Part 1: Allow 'static' with escape sequences as method name. r=arai

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -6347,13 +6347,6 @@ Parser<ParseHandler>::classDefinition(YieldHandling yieldHandling,
 
         bool isStatic = false;
         if (tt == TOK_NAME && tokenStream.currentName() == context->names().static_) {
-            MOZ_ASSERT(pc->sc()->strict(), "classes are always strict");
-
-            // Strict mode forbids "class" as Identifier, so it can only be the
-            // unescaped keyword.
-            if (!checkUnescapedName())
-                return null();
-
             if (!tokenStream.peekToken(&tt, TokenStream::KeywordIsName))
                 return null();
             if (tt == TOK_RC) {
