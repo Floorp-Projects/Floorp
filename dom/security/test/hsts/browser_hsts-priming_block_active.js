dfdf12eda35c69cdcb26e0e1a1e0ce36515fdab0
Kate McKinley <kmckinley@mozilla.com>
2016-10-07 17:19:38 +0900

    Bug 1305993 - Break tests up to avoid timeouts r=ckerschb, a=test-only
    MozReview-Commit-ID: 8y2gwNjnEnT

---
+++ dom/security/test/hsts/browser_hsts-priming_block_active.js
@@ -0,0 +1,24 @@
+/*
+ * Description of the test:
+ *   Check that HSTS priming occurs correctly with mixed content when active
+ *   content is blocked.
+ */
+'use strict';
+
+//jscs:disable
+add_task(function*() {
+  //jscs:enable
+  Services.obs.addObserver(Observer, "console-api-log-event", false);
+  Services.obs.addObserver(Observer, "http-on-examine-response", false);
+  registerCleanupFunction(do_cleanup);
+
+  let which = "block_active";
+
+  SetupPrefTestEnvironment(which);
+
+  for (let server of Object.keys(test_servers)) {
+    yield execute_test(server, test_settings[which].mimetype);
+  }
+
+  SpecialPowers.popPrefEnv();
+});
