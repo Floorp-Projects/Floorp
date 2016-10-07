3aa11e06edaf2bc564646972e07d8b1d82a9a0cc
Gabriel Luong <gabriel.luong@gmail.com>
2016-10-07 13:33:52 -0400

    Bug 1307481 - Part 3: HTMLTooltip should receive the document that it should be attached to instead of the toolbox r=jdescottes

--- devtools/client/webconsole/jsterm.js
+++ devtools/client/webconsole/jsterm.js
@@ -252,13 +252,11 @@ JSTerm.prototype = {
     };
 
     let doc = this.hud.document;
-
     let toolbox = gDevTools.getToolbox(this.hud.owner.target);
-    if (!toolbox) {
-      // In some cases (e.g. Browser Console), there is no toolbox.
-      toolbox = { doc };
-    }
-    this.autocompletePopup = new AutocompletePopup(toolbox, autocompleteOptions);
+    let tooltipDoc = toolbox ? toolbox.doc : doc;
+    // The popup will be attached to the toolbox document or HUD document in the case
+    // such as the browser console which doesn't have a toolbox.
+    this.autocompletePopup = new AutocompletePopup(tooltipDoc, autocompleteOptions);
 
     let inputContainer = doc.querySelector(".jsterm-input-container");
     this.completeNode = doc.querySelector(".jsterm-complete-node");
