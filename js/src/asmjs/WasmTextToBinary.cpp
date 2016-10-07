a8b7bf25bd73c02513c3b46f626c5b56d3401eab
Benjamin Bouvier <benj@benj.me>
2016-10-07 17:03:14 +0200

    Bug 1308056: wasm: add syntax for inline import/export in memories; r=luke
    MozReview-Commit-ID: Ijh7d0eqvuM

--- js/src/asmjs/WasmTextToBinary.cpp
+++ js/src/asmjs/WasmTextToBinary.cpp
@@ -2749,6 +2749,36 @@ ParseLimits(WasmParseContext& c, Limits* resizable)
 static bool
 ParseMemory(WasmParseContext& c, WasmToken token, AstModule* module)
 {
+    AstName name = c.ts.getIfName();
+
+    WasmToken openParen;
+    if (c.ts.getIf(WasmToken::OpenParen)) {
+        if (c.ts.getIf(WasmToken::Import)) {
+            InlineImport names;
+            if (!ParseInlineImport(c, &names))
+                return false;
+            if (!c.ts.match(WasmToken::CloseParen, c.error))
+                return false;
+
+            Limits memory;
+            if (!ParseLimits(c, &memory))
+                return false;
+
+            auto* imp = new(c.lifo) AstImport(name, names.module.text(), names.field.text(),
+                                              DefinitionKind::Memory, memory);
+            return imp && module->append(imp);
+        }
+
+        if (c.ts.getIf(WasmToken::Export)) {
+            if (!ParseInlineExport(c, DefinitionKind::Memory, module))
+                return false;
+            if (!c.ts.match(WasmToken::CloseParen, c.error))
+                return false;
+        } else {
+            c.ts.unget(openParen);
+        }
+    }
+
     if (c.ts.getIf(WasmToken::OpenParen)) {
         if (!c.ts.match(WasmToken::Data, c.error))
             return false;
@@ -2854,6 +2884,9 @@ ParseImport(WasmParseContext& c, AstModule* module)
     WasmToken openParen;
     if (c.ts.getIf(WasmToken::OpenParen, &openParen)) {
         if (c.ts.getIf(WasmToken::Memory)) {
+            if (name.empty())
+                name = c.ts.getIfName();
+
             Limits memory;
             if (!ParseLimits(c, &memory))
                 return nullptr;
--- js/src/jit-test/tests/wasm/text.js
+++ js/src/jit-test/tests/wasm/text.js
@@ -112,5 +112,16 @@ assertErrorMessage(() => wasmEvalText(`
     )
 `), SyntaxError, /import after global definition/);
 
+// Memory.
+assertErrorMessage(() => wasmEvalText('(module (memory (export)))'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(module (memory (export "g")))'), SyntaxError, parsingError);
+wasmEvalText('(module (memory $t (export "g") 0))');
+
+const mem = new WebAssembly.Memory({ initial: 1 });
+assertErrorMessage(() => wasmEvalText('(module (memory $t (import) 1))'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(module (memory $t (import "mod") 1))'), SyntaxError, parsingError);
+assertErrorMessage(() => wasmEvalText('(module (memory $t (import "mod" "field")))'), SyntaxError, parsingError);
+wasmEvalText('(module (memory $t (import "mod" "field") 1))', { mod: {field: mem} });
+
 // Note: the s-expression text format is temporary, this file is mostly just to
 // hold basic error smoke tests.
