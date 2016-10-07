66cd41fb3c7c70e7b56138881391816bf6496846
Botond Ballo <botond@mozilla.com>
2016-10-07 15:12:00 -0400

    Bug 1307557 - Fix an out-of-bounds access in Element::DescribeAttribute(). r=dveditz
    MozReview-Commit-ID: 15ItbWAQAPv

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -2813,9 +2813,9 @@ Element::DescribeAttribute(uint32_t index, nsAString& aOutDescription) const
   aOutDescription.AppendLiteral("=\"");
   nsAutoString value;
   mAttrsAndChildren.AttrAt(index)->ToString(value);
-  for (int i = value.Length(); i >= 0; --i) {
-    if (value[i] == char16_t('"'))
-      value.Insert(char16_t('\\'), uint32_t(i));
+  for (uint32_t i = value.Length(); i > 0; --i) {
+    if (value[i - 1] == char16_t('"'))
+      value.Insert(char16_t('\\'), i - 1);
   }
   aOutDescription.Append(value);
   aOutDescription.Append('"');
