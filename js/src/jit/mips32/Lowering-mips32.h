0e5a7816a920cd74644fed1ecf09437326fcfe92
Shi Dan <shid@lemote.com>
2016-10-10 17:08:15 +0800

    Bug 1290812 - Part 21: Preparations in IonMonkey to support i64 on mips32. r=jandem
    ---
     js/src/jit/mips32/Assembler-mips32.cpp | 60 ++++++++++++++++++++++++++++++++++
     js/src/jit/mips32/Assembler-mips32.h   |  3 ++
     js/src/jit/mips32/Lowering-mips32.cpp  | 30 +++++++++++++++++
     js/src/jit/mips32/Lowering-mips32.h    |  3 ++
     4 files changed, 96 insertions(+)

--- js/src/jit/mips32/Lowering-mips32.h
+++ js/src/jit/mips32/Lowering-mips32.h
@@ -31,6 +31,9 @@ class LIRGeneratorMIPS : public LIRGeneratorMIPSShared
     void lowerUntypedPhiInput(MPhi* phi, uint32_t inputPosition, LBlock* block, size_t lirIndex);
     void defineUntypedPhi(MPhi* phi, size_t lirIndex);
 
+    void lowerInt64PhiInput(MPhi* phi, uint32_t inputPosition, LBlock* block, size_t lirIndex);
+    void defineInt64Phi(MPhi* phi, size_t lirIndex);
+
     void lowerTruncateDToInt32(MTruncateToInt32* ins);
     void lowerTruncateFToInt32(MTruncateToInt32* ins);
 
