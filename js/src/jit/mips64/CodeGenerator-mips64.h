64de66ec4b12bc4d0c32c9220117f04563cf566f
Heiher <r@hev.cc>
2016-10-10 17:08:14 +0800

    Bug 1290812 - Part 20: Implement the 64bit variant of WasmLoadGlobalVar and WasmStoreGlobalVar on mips64. r=bbouvier
    ---
     js/src/jit/mips64/CodeGenerator-mips64.cpp | 18 ++++++++++++++++++
     js/src/jit/mips64/CodeGenerator-mips64.h   |  2 ++
     2 files changed, 20 insertions(+)

--- js/src/jit/mips64/CodeGenerator-mips64.h
+++ js/src/jit/mips64/CodeGenerator-mips64.h
@@ -50,6 +50,8 @@ class CodeGeneratorMIPS64 : public CodeGeneratorMIPSShared
     void visitUDivOrModI64(LUDivOrModI64* lir);
     void visitWasmLoadI64(LWasmLoadI64* lir);
     void visitWasmStoreI64(LWasmStoreI64* ins);
+    void visitWasmLoadGlobalVarI64(LWasmLoadGlobalVarI64* ins);
+    void visitWasmStoreGlobalVarI64(LWasmStoreGlobalVarI64* ins);
     void visitAsmSelectI64(LAsmSelectI64* ins);
     void visitAsmReinterpretFromI64(LAsmReinterpretFromI64* lir);
     void visitAsmReinterpretToI64(LAsmReinterpretToI64* lir);
