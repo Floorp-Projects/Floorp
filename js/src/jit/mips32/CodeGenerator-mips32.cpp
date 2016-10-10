59c2b779289127ead0f699bcba147becfad89f40
Shi Dan <shid@lemote.com>
2016-10-10 17:08:32 +0800

    Bug 1290812 - Part 34: Implement the 64bit variant of Clz and Ctz on mips32. r=bbouvier
    ---
     js/src/jit/MacroAssembler.h                   |  4 ++--
     js/src/jit/mips32/CodeGenerator-mips32.cpp    | 18 +++++++++++++++
     js/src/jit/mips32/CodeGenerator-mips32.h      |  2 ++
     js/src/jit/mips32/MacroAssembler-mips32-inl.h | 33 +++++++++++++++++++++++++++
     4 files changed, 55 insertions(+), 2 deletions(-)

--- js/src/jit/mips32/CodeGenerator-mips32.cpp
+++ js/src/jit/mips32/CodeGenerator-mips32.cpp
@@ -480,6 +480,24 @@ CodeGeneratorMIPS::visitWrapInt64ToInt32(LWrapInt64ToInt32* lir)
 }
 
 void
+CodeGeneratorMIPS::visitClzI64(LClzI64* lir)
+{
+    Register64 input = ToRegister64(lir->getInt64Operand(0));
+    Register64 output = ToOutRegister64(lir);
+    masm.clz64(input, output.low);
+    masm.move32(Imm32(0), output.high);
+}
+
+void
+CodeGeneratorMIPS::visitCtzI64(LCtzI64* lir)
+{
+    Register64 input = ToRegister64(lir->getInt64Operand(0));
+    Register64 output = ToOutRegister64(lir);
+    masm.ctz64(input, output.low);
+    masm.move32(Imm32(0), output.high);
+}
+
+void
 CodeGeneratorMIPS::setReturnDoubleRegs(LiveRegisterSet* regs)
 {
     MOZ_ASSERT(ReturnFloat32Reg.code_ == ReturnDoubleReg.code_);
