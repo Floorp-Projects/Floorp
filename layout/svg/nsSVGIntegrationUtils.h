f984f3a88885d75984757788f95896b1fdcdd9d6
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-10-12 18:02:18 +0200

    Backed out changeset b91db6687d19 (bug 1299715)

--- layout/svg/nsSVGIntegrationUtils.h
+++ layout/svg/nsSVGIntegrationUtils.h
@@ -9,7 +9,6 @@
 #include "gfxMatrix.h"
 #include "gfxRect.h"
 #include "nsRegionFwd.h"
-#include "mozilla/gfx/Rect.h"
 
 class gfxContext;
 class gfxDrawable;
@@ -38,7 +37,6 @@ struct nsSize;
 class nsSVGIntegrationUtils final
 {
   typedef mozilla::gfx::DrawTarget DrawTarget;
-  typedef mozilla::gfx::IntRect IntRect;
   typedef mozilla::image::DrawResult DrawResult;
 
 public:
@@ -140,8 +138,6 @@ public:
     mozilla::layers::LayerManager* layerManager;
     bool handleOpacity; // If true, PaintMaskAndClipPath/ PaintFilter should
                         // apply css opacity.
-    IntRect maskRect;
-
     explicit PaintFramesParams(gfxContext& aCtx, nsIFrame* aFrame,
                                const nsRect& aDirtyRect,
                                const nsRect& aBorderArea,
@@ -206,13 +202,6 @@ public:
                           const DrawTarget* aDrawTarget,
                           const gfxMatrix& aContextMatrix,
                           uint32_t aFlags);
-
-  /**
-   * For non-SVG frames, this gives the offset to the frame's "user space".
-   * For SVG frames, this returns a zero offset.
-   */
-  static nsPoint
-  GetOffsetToBoundingBox(nsIFrame* aFrame);
 };
 
 #endif /*NSSVGINTEGRATIONUTILS_H_*/
