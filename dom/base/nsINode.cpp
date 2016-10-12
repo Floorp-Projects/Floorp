580172553e39d8208b93b2b50d78fffc0b1c996a
Olli Pettay <Olli.Pettay@helsinki.fi>
2016-10-13 00:34:25 +0300

    Bug 1308922, ensure expected DOM tree operations when calling prepend, r=ehsan, a=ritu

--- dom/base/nsINode.cpp
+++ dom/base/nsINode.cpp
@@ -1866,7 +1866,8 @@ nsINode::Prepend(const Sequence<OwningNodeOrString>& aNodes,
     return;
   }
 
-  InsertBefore(*node, mFirstChild, aRv);
+  nsCOMPtr<nsINode> refNode = mFirstChild;
+  InsertBefore(*node, refNode, aRv);
 }
 
 void
