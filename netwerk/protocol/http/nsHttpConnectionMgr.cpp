f1c3f1aef0266865fc96d626491681df3af4acf0
Jonathan Hao <jhao@mozilla.com>
2016-10-12 15:32:22 +0800

    Bug 1264562 - Part 1: Add firstPartyDomain to socket transport (adapted from Tor Browser patch 13670) r=mayhemer

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -3081,6 +3081,12 @@ nsHalfOpenSocket::SetupStreams(nsISocketTransport **transport,
 
     socketTransport->SetConnectionFlags(tmpFlags);
 
+    nsAutoCString firstPartyDomain =
+      NS_ConvertUTF16toUTF8(mEnt->mConnInfo->GetOriginAttributes().mFirstPartyDomain);
+    if (!firstPartyDomain.IsEmpty()) {
+        socketTransport->SetFirstPartyDomain(firstPartyDomain);
+    }
+
     socketTransport->SetQoSBits(gHttpHandler->GetQoSBits());
 
     if (!ci->GetNetworkInterfaceId().IsEmpty()) {
