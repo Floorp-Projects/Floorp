9b3b55bfaec920a86761ca4c9d722b2e41d411bf
Wes Kocher <wkocher@mozilla.com>
2016-10-13 12:04:59 -0700

    Bug 1309699 - Spell the preference name correctly so it works a=me

--- toolkit/components/thumbnails/test/browser_thumbnails_bg_no_cookies_stored.js
+++ toolkit/components/thumbnails/test/browser_thumbnails_bg_no_cookies_stored.js
@@ -5,7 +5,7 @@
 // that cookie is not saved for subsequent requests.
 function* runTests() {
   yield SpecialPowers.pushPrefEnv({
-    set: [["privacy.about_newtab_segragation.enabled", true]]
+    set: [["privacy.about_newtab_segregation.enabled", true]]
   });
 
   let url = bgTestPageURL({
