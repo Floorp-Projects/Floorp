ddfa975699b4ac0319162e054a569ec4c5769da6
André Bargull <andre.bargull@gmail.com>
2016-10-14 09:03:46 -0700

    Bug 1122396 - Validate newly created instances in typed array builtins. r=evilpie
    --HG--
    extra : rebase_source : efcbff4b5e1b6f9e142bf2bc2cfa09d7afe12771

--- js/src/tests/ecma_6/TypedArray/from_constructor.js
+++ js/src/tests/ecma_6/TypedArray/from_constructor.js
@@ -1,41 +1,9 @@
 for (var constructor of anyTypedArrayConstructors) {
-    // %TypedArray%.from can be applied to any constructor.
-    // For example, the Date builtin constructor.
-    // Unlike Array.from, %TypedArray%.from doesn't set the 'length' property at the end.
-    var d = constructor.from.call(Date, ["A", "B"]);
-    assertEq(d instanceof constructor, false);
-    assertEq(Object.prototype.toString.call(d), "[object Date]");
-    assertEq(Object.getPrototypeOf(d), Date.prototype);
-    assertEq(d.length, undefined);
-    assertEq(d[0], "A");
-    assertEq(d[1], "B");
-
-    // Or RegExp.
-    var obj = constructor.from.call(RegExp, [1]);
-    assertEq(obj instanceof constructor, false);
-    assertEq(Object.getPrototypeOf(obj), RegExp.prototype);
-    assertEq(Object.getOwnPropertyNames(obj).join(","),
-             "0,lastIndex");
-    assertEq(obj.length, undefined);
-
-    // Or any JS function.
-    function C(arg) {
-        this.args = arguments;
-    }
-    var c = constructor.from.call(C, {length: 1, 0: "zero"});
-    assertEq(c instanceof C, true);
-    assertEq(c.args.length, 1);
-    assertEq(c.args[0], 1);
-    assertEq(c.length, undefined);
-    assertEq(c[0], "zero");
-
     // Note %TypedArray%.from(iterable) calls 'this' with an argument whose value is
     // `[...iterable].length`, while Array.from(iterable) doesn't pass any argument.
-    assertEq(constructor.from.call(Object, []) instanceof Number, true);
-    assertDeepEq(constructor.from.call(Object, []), new Number(0));
-    assertEq(constructor.from.call(Number,[1, , "a"]) + 1, 4);
     constructor.from.call(function(len){
         assertEq(len, 3);
+        return new constructor(len);
     }, Array(3));
 
     // If the 'this' value passed to %TypedArray.from is not a constructor,
@@ -53,13 +21,14 @@ for (var constructor of anyTypedArrayConstructors) {
 
     // %TypedArray%.from does not get confused if global constructors for typed arrays
     // are replaced with another constructor.
-    function NotArray() {
+    function NotArray(...rest) {
+        return new constructor(...rest);
     }
     var RealArray = constructor;
     NotArray.from = constructor.from;
     this[constructor.name] = NotArray;
     assertEq(RealArray.from([1]) instanceof RealArray, true);
-    assertEq(NotArray.from([1]) instanceof NotArray, true);
+    assertEq(NotArray.from([1]) instanceof RealArray, true);
     this[constructor.name] = RealArray;
 }
 
