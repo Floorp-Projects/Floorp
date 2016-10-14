0e842d3e6d8e1b6c65394f7371b045fafb00155c
Kartikaya Gupta <kgupta@mozilla.com>
2016-10-14 15:38:01 -0400

    Bug 1275314 - Allow flushing in-progress checkerboard reports in the GPU process as well. r=dvander
    MozReview-Commit-ID: 3EPv8hh7Zxh

--- gfx/layers/apz/util/CheckerboardReportService.cpp
+++ gfx/layers/apz/util/CheckerboardReportService.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/Unused.h"
 #include "mozilla/dom/CheckerboardReportServiceBinding.h" // for dom::CheckerboardReports
 #include "mozilla/gfx/GPUParent.h"
+#include "mozilla/gfx/GPUProcessManager.h"
 #include "nsContentUtils.h" // for nsContentUtils
 #include "nsXULAppAPI.h"
 
@@ -211,6 +212,10 @@ void
 CheckerboardReportService::FlushActiveReports()
 {
   MOZ_ASSERT(XRE_IsParentProcess());
+  gfx::GPUProcessManager* gpu = gfx::GPUProcessManager::Get();
+  if (gpu && gpu->NotifyGpuObservers("APZ:FlushActiveCheckerboard")) {
+    return;
+  }
 
   nsCOMPtr<nsIObserverService> obsSvc = mozilla::services::GetObserverService();
   MOZ_ASSERT(obsSvc);
