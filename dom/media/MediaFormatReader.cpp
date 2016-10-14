784908b6e8f4880ab407d50b7cf8657088f9b09f
Kaku Kuo <kaku@mozilla.com>
2016-10-14 17:09:04 +0800

    Bug 1309516 part 2 - replace MediaFormatReader::DemuxStartTime() with MediaInfo::mStartTime; r=jya
    MozReview-Commit-ID: JzO0Yv33ljL
    
    --HG--
    extra : rebase_source : e59bc8aa168dfda9ec5e41fd81bd98a2b9391663
    extra : intermediate-source : cca0e5eaf29e7cfdc32f8b66e217e0d8fb0fd0ea
    extra : source : 6af01ba877814a43b68bc004b0e28160216e7367

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -1965,25 +1965,11 @@ MediaFormatReader::SetSeekTarget(const SeekTarget& aTarget)
   // Transform the seek target time to the demuxer timeline.
   if (!ForceZeroStartTime()) {
     target.SetTime(aTarget.GetTime() - TimeUnit::FromMicroseconds(StartTime())
-                   + DemuxStartTime());
+                   + mInfo.mStartTime);
   }
 
   mOriginalSeekTarget = target;
   mFallbackSeekTime = mPendingSeekTime = Some(target.GetTime());
-}
-
-TimeUnit
-MediaFormatReader::DemuxStartTime()
-{
-  MOZ_ASSERT(OnTaskQueue());
-  MOZ_ASSERT(!ForceZeroStartTime());
-  MOZ_ASSERT(HasAudio() || HasVideo());
-
-  const TimeUnit startTime =
-    std::min(mAudio.mFirstDemuxedSampleTime.refOr(TimeUnit::FromInfinity()),
-             mVideo.mFirstDemuxedSampleTime.refOr(TimeUnit::FromInfinity()));
-
-  return startTime.IsInfinite() ? TimeUnit::FromMicroseconds(0) : startTime;
 }
 
 void
