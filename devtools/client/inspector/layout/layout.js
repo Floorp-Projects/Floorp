2089ea1f64d2cbc201896ddb4b8b4ea773846043
Gabriel Luong <gabriel.luong@gmail.com>
2016-10-14 20:31:04 -0400

    Bug 1308227 - Add a CSS Grid accordion container to the layout panel r=jdescottes

--- devtools/client/inspector/layout/layout.js
+++ devtools/client/inspector/layout/layout.js
@@ -23,7 +23,7 @@ LayoutViewTool.prototype = {
 
     let store = this.store = Store();
     let provider = React.createElement(ReactRedux.Provider, { store }, App());
-    ReactDOM.render(provider, this.document.querySelector("#layout-root"));
+    ReactDOM.render(provider, this.document.querySelector("#layoutview-container"));
   },
 
   destroy() {
