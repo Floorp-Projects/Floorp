5dd2332a4bc4653dd5dbb47dc469f53ea1d693fa
Kaku Kuo <kaku@mozilla.com>
2016-10-16 21:49:07 +0800

    Bug 1309516 part 4 - always notify LoadedMetadataEvent before decoding first frame;r=jwwang
    MozReview-Commit-ID: 4VcmELgvAjC

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -675,14 +675,6 @@ private:
   // True if all video frames are already rendered.
   bool mVideoCompleted = false;
 
-  // Flag whether we notify metadata before decoding the first frame or after.
-  //
-  // Note that the odd semantics here are designed to replicate the current
-  // behavior where we notify the decoder each time we come out of dormant, but
-  // send suppressed event visibility for those cases. This code can probably be
-  // simplified.
-  bool mNotifyMetadataBeforeFirstFrame;
-
   // True if we should not decode/preroll unnecessary samples, unless we're
   // played. "Prerolling" in this context refers to when we decode and
   // buffer decoded samples in advance of when they're needed for playback.
