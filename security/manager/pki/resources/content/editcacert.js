2ec528ef46e3b35a3d31939791dfc2d1b27dc358
Cykesiopka <cykesiopka.bmo@gmail.com>
2016-10-18 00:02:42 +0800

    Bug 332442 - Stop abusing window name to pass cert ref to editcerts.js. r=mgoodwin
    editcacert.xul/editcerts.js currently requires the |dbKey| attribute of an
    nsIX509Cert to be passed to it via the window name so it can get a handle to the
    nsIX509Cert.
    
    This has two problems:
    1. This used to trigger warnings, and is unwise to do in any case.
    2. It's unnecessary complexity - the nsIX509Cert can be passed directly.
    
    This patch:
    1. Addresses the two problems.
    2. Adds a test to ensure the functionality of editcerts.js actually works.
    3. Rewrites editcerts.js to better fit modern PSM style.
    4. Updates the name of editcerts.js so it's more consistent with the general
       convention under security/pki/resources/content
    
    MozReview-Commit-ID: ECxziXq5TmL

---
+++ security/manager/pki/resources/content/editcacert.js
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* import-globals-from pippki.js */
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+var gCertDB = Cc["@mozilla.org/security/x509certdb;1"]
+                .getService(Ci.nsIX509CertDB);
+/**
+ * Cert to edit the trust of.
+ * @type nsIX509Cert
+ */
+var gCert;
+
+/**
+ * onload() handler.
+ */
+function onLoad() {
+  gCert = window.arguments[0];
+
+  let bundle = document.getElementById("pippki_bundle");
+  setText("certmsg",
+          bundle.getFormattedString("editTrustCA", [gCert.commonName]));
+
+  let sslCheckbox = document.getElementById("trustSSL");
+  sslCheckbox.checked = gCertDB.isCertTrusted(gCert, Ci.nsIX509Cert.CA_CERT,
+                                              Ci.nsIX509CertDB.TRUSTED_SSL);
+
+  let emailCheckbox = document.getElementById("trustEmail");
+  emailCheckbox.checked = gCertDB.isCertTrusted(gCert, Ci.nsIX509Cert.CA_CERT,
+                                                Ci.nsIX509CertDB.TRUSTED_EMAIL);
+
+  let objSignCheckbox = document.getElementById("trustObjSign");
+  objSignCheckbox.checked =
+    gCertDB.isCertTrusted(gCert, Ci.nsIX509Cert.CA_CERT,
+                          Ci.nsIX509CertDB.TRUSTED_OBJSIGN);
+}
+
+/**
+ * ondialogaccept() handler.
+ *
+ * @returns {Boolean} true to make the dialog close, false otherwise.
+ */
+function onDialogAccept() {
+  let sslCheckbox = document.getElementById("trustSSL");
+  let emailCheckbox = document.getElementById("trustEmail");
+  let objSignCheckbox = document.getElementById("trustObjSign");
+  let trustSSL = sslCheckbox.checked ? Ci.nsIX509CertDB.TRUSTED_SSL : 0;
+  let trustEmail = emailCheckbox.checked ? Ci.nsIX509CertDB.TRUSTED_EMAIL : 0;
+  let trustObjSign = objSignCheckbox.checked ? Ci.nsIX509CertDB.TRUSTED_OBJSIGN
+                                             : 0;
+
+  gCertDB.setCertTrust(gCert, Ci.nsIX509Cert.CA_CERT,
+                       trustSSL | trustEmail | trustObjSign);
+  return true;
+}
