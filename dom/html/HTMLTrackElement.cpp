62057cb4d07df451162a7a5a0313fa8920a89397
bechen <bechen@mozilla.com>
2016-10-18 11:59:56 +0800

    Bug 1308862 - Null check for innerWindow. r=jwwang, a=gchang

--- dom/html/HTMLTrackElement.cpp
+++ dom/html/HTMLTrackElement.cpp
@@ -126,11 +126,14 @@ NS_IMPL_ISUPPORTS(WindowDestroyObserver, nsIObserver);
 HTMLTrackElement::HTMLTrackElement(already_AddRefed<mozilla::dom::NodeInfo>& aNodeInfo)
   : nsGenericHTMLElement(aNodeInfo)
   , mLoadResourceDispatched(false)
+  , mWindowDestroyObserver(nullptr)
 {
   nsISupports* parentObject = OwnerDoc()->GetParentObject();
   NS_ENSURE_TRUE_VOID(parentObject);
   nsCOMPtr<nsPIDOMWindowInner> window = do_QueryInterface(parentObject);
-  mWindowDestroyObserver = new WindowDestroyObserver(this, window->WindowID());
+  if (window) {
+    mWindowDestroyObserver = new WindowDestroyObserver(this, window->WindowID());
+  }
 }
 
 HTMLTrackElement::~HTMLTrackElement()
