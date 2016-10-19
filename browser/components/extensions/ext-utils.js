d2dfc382dcc63560989ae76fb79e4d7eadce622a
Kris Maglione <maglione.k@gmail.com>
2016-10-19 15:13:25 -0700

    Bug 1287209: Make popup tests compatible with remote browsers. r=aswan
    MozReview-Commit-ID: 79Xa3j5Kzov

--- browser/components/extensions/ext-utils.js
+++ browser/components/extensions/ext-utils.js
@@ -345,6 +345,12 @@ class PanelPopup extends BasePopup {
 
     this.contentReady.then(() => {
       panel.openPopup(imageNode, "bottomcenter topright", 0, 0, false, false);
+
+      let event = new this.window.CustomEvent("WebExtPopupLoaded", {
+        bubbles: true,
+        detail: {extension},
+      });
+      this.browser.dispatchEvent(event);
     });
   }
 
@@ -457,6 +463,12 @@ class ViewPopup extends BasePopup {
 
       this.tempPanel.remove();
       this.tempPanel = null;
+
+      let event = new this.window.CustomEvent("WebExtPopupLoaded", {
+        bubbles: true,
+        detail: {extension: this.extension},
+      });
+      this.browser.dispatchEvent(event);
 
       return true;
     }.bind(this));
