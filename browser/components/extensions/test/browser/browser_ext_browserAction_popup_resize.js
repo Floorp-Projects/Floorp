1b0cc16a74def28a6c2c2827215e5283ebad31d8
Kris Maglione <maglione.k@gmail.com>
2016-10-20 15:27:51 -0700

    Bug 1287209: Handle popup and options browser resizing using a frame script. r=aswan
    MozReview-Commit-ID: DIfynklqrus
    
    --HG--
    extra : source : 68806639c03110bfb67dd6c705a5441a917afcd6
    extra : intermediate-source : 7ea8213113d73fcbdd5160397db9ed4fbe295ee0
    extra : histedit_source : 62c8339301d4e1939b5873554d2c751d76a15c22%2C291e31423225845deb0a7785cd06e9f4cec81221

--- browser/components/extensions/test/browser/browser_ext_browserAction_popup_resize.js
+++ browser/components/extensions/test/browser/browser_ext_browserAction_popup_resize.js
@@ -14,8 +14,7 @@ function* awaitResize(browser) {
   // looking for, but don't wait longer than a few seconds.
 
   return Promise.race([
-    BrowserTestUtils.waitForEvent(browser, "WebExtPopupResized")
-      .then(() => BrowserTestUtils.waitForEvent(browser, "WebExtPopupResized")),
+    BrowserTestUtils.waitForEvent(browser, "WebExtPopupResized", event => event.detail === "delayed"),
     new Promise(resolve => setTimeout(resolve, 5000)),
   ]);
 }
@@ -35,8 +34,6 @@ add_task(function* testBrowserActionPopupResize() {
   });
 
   yield extension.startup();
-
-  clickBrowserAction(extension, window);
 
   let browser = yield openPanel(extension);
   let panelWindow = browser.contentWindow;
