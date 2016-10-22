cdc58cdaed1d4715f5a0775474e03063424e4946
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-10-22 14:40:48 +0100

    Bug 1279501 - add telemetry for the number of bookmarks, history visits and logins are imported from another browser, r=bsmedberg,mak,a=gchang
    MozReview-Commit-ID: EiZfkj6AsVL
    
    --HG--
    extra : source : b99f3a05db461e9a4384d95a95c7ecbfde863bc4
    extra : amend_source : b3f32f289eb984ede37621186ba5c679c98b276f

--- browser/components/migration/360seProfileMigrator.js
+++ browser/components/migration/360seProfileMigrator.js
@@ -155,7 +155,7 @@ Bookmarks.prototype = {
 
           try {
             if (is_folder == 1) {
-              let newFolderGuid = (yield PlacesUtils.bookmarks.insert({
+              let newFolderGuid = (yield MigrationUtils.insertBookmarkWrapper({
                 parentGuid,
                 type: PlacesUtils.bookmarks.TYPE_FOLDER,
                 title
@@ -163,7 +163,7 @@ Bookmarks.prototype = {
 
               idToGuid.set(id, newFolderGuid);
             } else {
-              yield PlacesUtils.bookmarks.insert({
+              yield MigrationUtils.insertBookmarkWrapper({
                 parentGuid,
                 url,
                 title
