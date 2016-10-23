8425f11c269d567255600a4022f6efbfa11a7047
Adam Velebil <adamtomasv@gmail.com>
2016-10-23 22:41:49 +0200

    Bug 1313565 - Convert NS_STYLE_USER_MODIFY_* to enum class ; r=manishearth,xidorn
    MozReview-Commit-ID: IA4bGLgRxd6

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -3398,7 +3398,7 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleUserInterface
   }
 
   mozilla::StyleUserInput   mUserInput;       // [inherited]
-  uint8_t                   mUserModify;      // [inherited] (modify-content)
+  mozilla::StyleUserModify  mUserModify;      // [inherited] (modify-content)
   mozilla::StyleUserFocus   mUserFocus;       // [inherited] (auto-select)
   uint8_t                   mPointerEvents;   // [inherited] see nsStyleConsts.h
 
