122229122097606fcf2a79a9c5b4163322bb554a
Byron Campen [:bwc] <docfaraday@gmail.com>
2016-10-24 13:36:56 -0500

    Bug 1312304: Remove ICE policy "none". r=drno,smaug
    MozReview-Commit-ID: 2adsRJ6pFnD

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
@@ -479,9 +479,6 @@ PeerConnectionConfiguration::Init(const RTCConfiguration& aSrc)
   }
 
   switch (aSrc.mIceTransportPolicy) {
-    case dom::RTCIceTransportPolicy::None:
-      setIceTransportPolicy(NrIceCtx::ICE_POLICY_NONE);
-      break;
     case dom::RTCIceTransportPolicy::Relay:
       setIceTransportPolicy(NrIceCtx::ICE_POLICY_RELAY);
       break;
