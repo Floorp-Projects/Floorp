ae9c018212445137881fc0acf97886ed79de898a
Arthur Edelstein <arthuredelstein@gmail.com>
2016-10-24 19:19:00 -0400

    Bug 1312541 - Test isolation of cookies. r=baku, r=timhuang
    Based on the patch for 1264567.

---
+++ browser/components/originattributes/test/browser/browser_cookieIsolation.js
@@ -0,0 +1,28 @@
+/**
+ * Bug 1312541 - A test case for document.cookie isolation.
+ */
+
+const TEST_PAGE = "http://mochi.test:8888/browser/browser/components/" +
+                  "originattributes/test/browser/file_firstPartyBasic.html";
+
+// Use a random key so we don't access it in later tests.
+const key = "key" + Math.random().toString();
+const re = new RegExp(key + "=([0-9\.]+)");
+
+// Define the testing function
+function* doTest(aBrowser) {
+    return yield ContentTask.spawn(aBrowser, {key, re},
+                                   function ({key, re}) {
+    let result = re.exec(content.document.cookie);
+    if (result) {
+      return result[1];
+    } else {
+      // No value is found, so we create one.
+      let value = Math.random().toString();
+      content.document.cookie = key + "=" + value;
+      return value;
+    }
+  });
+}
+
+IsolationTestTools.runTests(TEST_PAGE, doTest);
