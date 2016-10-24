88a946d38025cffda419fd070bf036ee896870f1
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-10-24 23:04:09 +1100

    Bug 1309163: P3. Add PPS decoder. r=rillian
    Currently, only decode the first PPS found.
    
    MozReview-Commit-ID: APzyvUdeSXR

--- media/libstagefright/binding/H264.cpp
+++ media/libstagefright/binding/H264.cpp
@@ -11,11 +11,98 @@
 #include "mp4_demuxer/H264.h"
 #include <media/stagefright/foundation/ABitReader.h>
 #include <limits>
+#include <cmath>
 
 using namespace mozilla;
 
-namespace mp4_demuxer
+namespace mp4_demuxer {
+
+// Default scaling lists (per spec).
+// ITU H264:
+// Table 7-2 – Assignment of mnemonic names to scaling list indices and
+// specification of fall-back rule
+static const uint8_t Default_4x4_Intra[16] = {
+    6, 13, 13, 20,
+   20, 20, 28, 28,
+   28, 28, 32, 32,
+   32, 37, 37, 42
+};
+
+static const uint8_t Default_4x4_Inter[16] = {
+   10, 14, 14, 20,
+   20, 20, 24, 24,
+   24, 24, 27, 27,
+   27, 30, 30, 34
+};
+
+static const uint8_t Default_8x8_Intra[64] = {
+    6, 10, 10, 13, 11, 13, 16, 16,
+   16, 16, 18, 18, 18, 18, 18, 23,
+   23, 23, 23, 23, 23, 25, 25, 25,
+   25, 25, 25, 25, 27, 27, 27, 27,
+   27, 27, 27, 27, 29, 29, 29, 29,
+   29, 29, 29, 31, 31, 31, 31, 31,
+   31, 33, 33, 33, 33, 33, 36, 36,
+   36, 36, 38, 38, 38, 40, 40, 42
+};
+
+static const uint8_t Default_8x8_Inter[64] = {
+    9, 13, 13, 15, 13, 15, 17, 17,
+   17, 17, 19, 19, 19, 19, 19, 21,
+   21, 21, 21, 21, 21, 22, 22, 22,
+   22, 22, 22, 22, 24, 24, 24, 24,
+   24, 24, 24, 24, 25, 25, 25, 25,
+   25, 25, 25, 27, 27, 27, 27, 27,
+   27, 28, 28, 28, 28, 28, 30, 30,
+   30, 30, 32, 32, 32, 33, 33, 35
+};
+
+namespace detail {
+static void
+scaling_list(BitReader& aBr, uint8_t* aScalingList, int aSizeOfScalingList,
+             const uint8_t* aDefaultList, const uint8_t* aFallbackList)
 {
+  int32_t lastScale = 8;
+  int32_t nextScale = 8;
+  int32_t deltaScale;
+
+  // (pic|seq)_scaling_list_present_flag[i]
+  if (!aBr.ReadBit()) {
+    if (aFallbackList) {
+      memcpy(aScalingList, aFallbackList, aSizeOfScalingList);
+    }
+    return;
+  }
+
+  for (int i = 0; i < aSizeOfScalingList; i++) {
+    if (nextScale != 0) {
+      deltaScale = aBr.ReadSE();
+      nextScale = (lastScale + deltaScale + 256) % 256;
+      if (!i && !nextScale) {
+        memcpy(aScalingList, aDefaultList, aSizeOfScalingList);
+        return;
+      }
+    }
+    aScalingList[i] = (nextScale == 0) ? lastScale : nextScale;
+    lastScale = aScalingList[i];
+  }
+}
+} // namespace detail.
+
+template <size_t N>
+static void
+scaling_list(BitReader& aBr, uint8_t (&aScalingList)[N],
+             const uint8_t (&aDefaultList)[N], const uint8_t (&aFallbackList)[N])
+{
+  detail::scaling_list(aBr, aScalingList, N, aDefaultList, aFallbackList);
+}
+
+template <size_t N>
+static void
+scaling_list(BitReader& aBr, uint8_t (&aScalingList)[N], const uint8_t (&aDefaultList)[N])
+{
+  detail::scaling_list(aBr, aScalingList, N, aDefaultList, nullptr);
+}
 
 SPSData::SPSData()
 {
@@ -26,7 +113,30 @@ SPSData::SPSData()
   colour_primaries = 2;
   transfer_characteristics = 2;
   sample_ratio = 1.0;
+  memset(scaling_matrix4x4, 16, sizeof(scaling_matrix4x4));
+  memset(scaling_matrix8x8, 16, sizeof(scaling_matrix8x8));
 }
+
+PPSData::PPSData()
+{
+  PodZero(this);
+  memset(scaling_matrix4x4, 16, sizeof(scaling_matrix4x4));
+  memset(scaling_matrix8x8, 16, sizeof(scaling_matrix8x8));
+}
+
+const uint8_t H264::ZZ_SCAN[16] = { 0,  1,  4,  8,
+                                    5,  2,  3,  6,
+                                    9, 12, 13, 10,
+                                    7, 11, 14, 15 };
+
+const uint8_t H264::ZZ_SCAN8[64] = {  0,  1,  8, 16,  9,  2,  3, 10,
+                                     17, 24, 32, 25, 18, 11,  4,  5,
+                                     12, 19, 26, 33, 40, 48, 41, 34,
+                                     27, 20, 13,  6,  7, 14, 21, 28,
+                                     35, 42, 49, 56, 57, 50, 43, 36,
+                                     29, 22, 15, 23, 30, 37, 44, 51,
+                                     58, 59, 52, 45, 38, 31, 39, 46,
+                                     53, 60, 61, 54, 47, 55, 62, 63 };
 
 /* static */ already_AddRefed<mozilla::MediaByteBuffer>
 H264::DecodeNALUnit(const mozilla::MediaByteBuffer* aNAL)
@@ -91,10 +201,6 @@ H264::DecodeSPS(const mozilla::MediaByteBuffer* aSPS, SPSData& aDest)
   }
   BitReader br(aSPS);
 
-  int32_t lastScale;
-  int32_t nextScale;
-  int32_t deltaScale;
-
   aDest.profile_idc = br.ReadBits(8);
   aDest.constraint_set0_flag = br.ReadBit();
   aDest.constraint_set1_flag = br.ReadBit();
@@ -108,32 +214,47 @@ H264::DecodeSPS(const mozilla::MediaByteBuffer* aSPS, SPSData& aDest)
   if (aDest.profile_idc == 100 || aDest.profile_idc == 110 ||
       aDest.profile_idc == 122 || aDest.profile_idc == 244 ||
       aDest.profile_idc == 44 || aDest.profile_idc == 83 ||
-     aDest.profile_idc == 86 || aDest.profile_idc == 118 ||
+      aDest.profile_idc == 86 || aDest.profile_idc == 118 ||
       aDest.profile_idc == 128 || aDest.profile_idc == 138 ||
       aDest.profile_idc == 139 || aDest.profile_idc == 134) {
     if ((aDest.chroma_format_idc = br.ReadUE()) == 3) {
       aDest.separate_colour_plane_flag = br.ReadBit();
     }
-    br.ReadUE();  // bit_depth_luma_minus8
-    br.ReadUE();  // bit_depth_chroma_minus8
-    br.ReadBit(); // qpprime_y_zero_transform_bypass_flag
-    if (br.ReadBit()) { // seq_scaling_matrix_present_flag
-      for (int idx = 0; idx < ((aDest.chroma_format_idc != 3) ? 8 : 12); ++idx) {
-        if (br.ReadBit()) { // Scaling list present
-          lastScale = nextScale = 8;
-          int sl_n = (idx < 6) ? 16 : 64;
-          for (int sl_i = 0; sl_i < sl_n; sl_i++) {
-            if (nextScale) {
-              deltaScale = br.ReadSE();
-              nextScale = (lastScale + deltaScale + 256) % 256;
-            }
-            lastScale = (nextScale == 0) ? lastScale : nextScale;
-          }
-        }
+    br.ReadUE();        // bit_depth_luma_minus8
+    br.ReadUE();        // bit_depth_chroma_minus8
+    br.ReadBit();       // qpprime_y_zero_transform_bypass_flag
+    aDest.seq_scaling_matrix_present_flag = br.ReadBit();
+    if (aDest.seq_scaling_matrix_present_flag) {
+      scaling_list(br, aDest.scaling_matrix4x4[0], Default_4x4_Intra,
+                   Default_4x4_Intra);
+      scaling_list(br, aDest.scaling_matrix4x4[1], Default_4x4_Intra,
+                   aDest.scaling_matrix4x4[0]);
+      scaling_list(br, aDest.scaling_matrix4x4[2], Default_4x4_Intra,
+                   aDest.scaling_matrix4x4[1]);
+      scaling_list(br, aDest.scaling_matrix4x4[3], Default_4x4_Inter,
+                   Default_4x4_Inter);
+      scaling_list(br, aDest.scaling_matrix4x4[4], Default_4x4_Inter,
+                   aDest.scaling_matrix4x4[3]);
+      scaling_list(br, aDest.scaling_matrix4x4[5], Default_4x4_Inter,
+                   aDest.scaling_matrix4x4[4]);
+
+      scaling_list(br, aDest.scaling_matrix8x8[0], Default_8x8_Intra,
+                   Default_8x8_Intra);
+      scaling_list(br, aDest.scaling_matrix8x8[1], Default_8x8_Inter,
+                   Default_8x8_Inter);
+      if (aDest.chroma_format_idc == 3) {
+        scaling_list(br, aDest.scaling_matrix8x8[2], Default_8x8_Intra,
+                     aDest.scaling_matrix8x8[0]);
+        scaling_list(br, aDest.scaling_matrix8x8[3], Default_8x8_Inter,
+                     aDest.scaling_matrix8x8[1]);
+        scaling_list(br, aDest.scaling_matrix8x8[4], Default_8x8_Intra,
+                     aDest.scaling_matrix8x8[2]);
+        scaling_list(br, aDest.scaling_matrix8x8[5], Default_8x8_Inter,
+                     aDest.scaling_matrix8x8[3]);
       }
     }
   } else if (aDest.profile_idc == 183) {
-      aDest.chroma_format_idc = 0;
+    aDest.chroma_format_idc = 0;
   } else {
     // default value if chroma_format_idc isn't set.
     aDest.chroma_format_idc = 1;
@@ -240,7 +361,7 @@ H264::vui_parameters(BitReader& aBr, SPSData& aDest)
     aDest.sar_width = aDest.sar_height = 0;
 
     // From E.2.1 VUI parameters semantics (ITU-T H.264 02/2014)
-    switch (aDest.aspect_ratio_idc)  {
+    switch (aDest.aspect_ratio_idc) {
       case 0:
         // Unspecified
         break;
@@ -368,7 +489,7 @@ H264::vui_parameters(BitReader& aBr, SPSData& aDest)
         break;
       case 255:
         /* Extended_SAR */
-        aDest.sar_width  = aBr.ReadBits(16);
+        aDest.sar_width = aBr.ReadBits(16);
         aDest.sar_height = aBr.ReadBits(16);
         if (aDest.sar_width && aDest.sar_height) {
           aDest.sample_ratio = float(aDest.sar_width) / float(aDest.sar_height);
@@ -401,7 +522,7 @@ H264::vui_parameters(BitReader& aBr, SPSData& aDest)
   }
 
   aDest.timing_info_present_flag = aBr.ReadBit();
-  if (aDest.timing_info_present_flag ) {
+  if (aDest.timing_info_present_flag) {
     aDest.num_units_in_tick = aBr.ReadBits(32);
     aDest.time_scale = aBr.ReadBits(32);
     aDest.fixed_frame_rate_flag = aBr.ReadBit();
@@ -409,7 +530,8 @@ H264::vui_parameters(BitReader& aBr, SPSData& aDest)
 }
 
 /* static */ bool
-H264::DecodeSPSFromExtraData(const mozilla::MediaByteBuffer* aExtraData, SPSData& aDest)
+H264::DecodeSPSFromExtraData(const mozilla::MediaByteBuffer* aExtraData,
+                             SPSData& aDest)
 {
   if (!AnnexB::HasSPS(aExtraData)) {
     return false;
@@ -420,17 +542,19 @@ H264::DecodeSPSFromExtraData(const mozilla::MediaByteBuffer* aExtraData, SPSData
     return false;
   }
 
-  if (!(reader.ReadU8() & 0x1f)) {
+  uint8_t numSps = reader.ReadU8() & 0x1f;
+  if (!numSps) {
     // No SPS.
     return false;
   }
+  NS_ASSERTION(numSps == 1, "More than one SPS in extradata");
+
   uint16_t length = reader.ReadU16();
 
   if ((reader.PeekU8() & 0x1f) != 7) {
     // Not a SPS NAL type.
     return false;
   }
-
   const uint8_t* ptr = reader.Read(length);
   if (!ptr) {
     return false;
@@ -455,8 +579,7 @@ H264::EnsureSPSIsSane(SPSData& aSPS)
   static const float default_aspect = 4.0f / 3.0f;
   if (aSPS.sample_ratio <= 0.0f || aSPS.sample_ratio > 6.0f) {
     if (aSPS.pic_width && aSPS.pic_height) {
-      aSPS.sample_ratio =
-        (float) aSPS.pic_width / (float) aSPS.pic_height;
+      aSPS.sample_ratio = (float)aSPS.pic_width / (float)aSPS.pic_height;
     } else {
       aSPS.sample_ratio = default_aspect;
     }
@@ -469,6 +592,189 @@ H264::EnsureSPSIsSane(SPSData& aSPS)
     valid = false;
   }
   return valid;
+}
+
+/* static */ bool
+H264::DecodePPSFromExtraData(const mozilla::MediaByteBuffer* aExtraData,
+                             const SPSData& aSPS, PPSData& aDest)
+{
+  if (!AnnexB::HasPPS(aExtraData)) {
+    return false;
+  }
+  ByteReader reader(aExtraData);
+
+  if (!reader.Read(5)) {
+    return false;
+  }
+
+  uint8_t numSps = reader.ReadU8() & 0x1f;
+  if (!numSps) {
+    // No SPS.
+    return false;
+  }
+  NS_ASSERTION(numSps == 1, "More than one SPS in extradata");
+  for (uint8_t i = 0; i < numSps; i++) {
+    uint16_t length = reader.ReadU16();
+
+    if ((reader.PeekU8() & 0x1f) != 7) {
+      // Not a SPS NAL type.
+      return false;
+    }
+    const uint8_t* ptr = reader.Read(length);
+    if (!ptr) {
+      return false;
+    }
+  }
+  uint8_t numPps = reader.ReadU8();
+  if (!numPps) {
+    // No PPs.
+    return false;
+  }
+  NS_ASSERTION(numPps == 1, "More than one PPS in extradata");
+  uint16_t length = reader.ReadU16();
+
+  if ((reader.PeekU8() & 0x1f) != 8) {
+    // Not a PPS NAL type.
+    return false;
+  }
+  const uint8_t* ptr = reader.Read(length);
+  if (!ptr) {
+    return false;
+  }
+
+  RefPtr<mozilla::MediaByteBuffer> rawNAL = new mozilla::MediaByteBuffer;
+  rawNAL->AppendElements(ptr, length);
+
+  RefPtr<mozilla::MediaByteBuffer> pps = DecodeNALUnit(rawNAL);
+
+  if (!pps) {
+    return false;
+  }
+
+  return DecodePPS(pps, aSPS, aDest);
+}
+
+/* static */ bool
+H264::DecodePPS(const mozilla::MediaByteBuffer* aPPS,const SPSData& aSPS,
+                PPSData& aDest)
+{
+  if (!aPPS) {
+    return false;
+  }
+
+  memcpy(aDest.scaling_matrix4x4, aSPS.scaling_matrix4x4,
+         sizeof(aDest.scaling_matrix4x4));
+  memcpy(aDest.scaling_matrix8x8, aSPS.scaling_matrix8x8,
+         sizeof(aDest.scaling_matrix8x8));
+
+  BitReader br(aPPS);
+
+  aDest.pic_parameter_set_id = br.ReadUE();
+  aDest.seq_parameter_set_id = br.ReadUE();
+  aDest.entropy_coding_mode_flag = br.ReadBit();
+  aDest.bottom_field_pic_order_in_frame_present_flag = br.ReadBit();
+  aDest.num_slice_groups_minus1 = br.ReadUE();
+  if (aDest.num_slice_groups_minus1 > 0) {
+    aDest.slice_group_map_type = br.ReadUE();
+    switch (aDest.slice_group_map_type) {
+      case 0:
+        for (uint8_t iGroup = 0; iGroup <= aDest.num_slice_groups_minus1;
+             iGroup++) {
+          aDest.run_length_minus1[iGroup] = br.ReadUE();
+        }
+        break;
+      case 2:
+        for (uint8_t iGroup = 0; iGroup < aDest.num_slice_groups_minus1;
+             iGroup++) {
+          aDest.top_left[iGroup] = br.ReadUE();
+          aDest.bottom_right[iGroup] = br.ReadUE();
+        }
+        break;
+      case 3:
+      case 4:
+      case 5:
+        aDest.slice_group_change_direction_flag = br.ReadBit();
+        aDest.slice_group_change_rate_minus1 = br.ReadUE();
+        break;
+      case 6:
+        aDest.pic_size_in_map_units_minus1 = br.ReadUE();
+        for (uint32_t i = 0; i <= aDest.pic_size_in_map_units_minus1; i++) {
+          /* slice_group_id[ i ] identifies a slice group of the i-th slice group map
+            unit in raster scan order. The length of the slice_group_id[i] syntax
+            element is Ceil(Log2(num_slice_groups_minus1+1)) bits. The value of
+            slice_group_id[i] shall be in the range of 0 to num_slice_groups_minus1,
+            inclusive. */
+          br.ReadBits(std::ceil(std::log2(aDest.num_slice_groups_minus1 + 1)));
+        }
+        break;
+      default:
+        return false;
+    }
+  }
+  aDest.num_ref_idx_l0_default_active_minus1 = br.ReadUE();
+  aDest.num_ref_idx_l1_default_active_minus1 = br.ReadUE();
+  if (aDest.num_ref_idx_l0_default_active_minus1 > 32 ||
+      aDest.num_ref_idx_l1_default_active_minus1 > 32) {
+    // reference overflow.
+    return false;
+  }
+  aDest.weighted_pred_flag = br.ReadBit();
+  aDest.weighted_bipred_idc = br.ReadBits(2);
+  aDest.pic_init_qp_minus26 = br.ReadSE();
+  aDest.pic_init_qs_minus26 = br.ReadSE();
+  aDest.chroma_qp_index_offset = br.ReadSE();
+  aDest.deblocking_filter_control_present_flag = br.ReadBit();
+  aDest.constrained_intra_pred_flag = br.ReadBit();
+  aDest.redundant_pic_cnt_present_flag = br.ReadBit();
+  if (br.BitsLeft()) {
+    aDest.transform_8x8_mode_flag = br.ReadBit();
+    if (br.ReadBit()) { // pic_scaling_matrix_present_flag
+      if (aSPS.seq_scaling_matrix_present_flag) {
+        scaling_list(br, aDest.scaling_matrix4x4[0], Default_4x4_Intra);
+        scaling_list(br, aDest.scaling_matrix4x4[1], Default_4x4_Intra,
+                     aDest.scaling_matrix4x4[0]);
+        scaling_list(br, aDest.scaling_matrix4x4[2], Default_4x4_Intra,
+                     aDest.scaling_matrix4x4[1]);
+        scaling_list(br, aDest.scaling_matrix4x4[3], Default_4x4_Inter);
+      } else {
+        scaling_list(br, aDest.scaling_matrix4x4[0], Default_4x4_Intra,
+                     Default_4x4_Intra);
+        scaling_list(br, aDest.scaling_matrix4x4[1], Default_4x4_Intra,
+                     aDest.scaling_matrix4x4[0]);
+        scaling_list(br, aDest.scaling_matrix4x4[2], Default_4x4_Intra,
+                     aDest.scaling_matrix4x4[1]);
+        scaling_list(br, aDest.scaling_matrix4x4[3], Default_4x4_Inter,
+                     Default_4x4_Inter);
+      }
+      scaling_list(br, aDest.scaling_matrix4x4[4], Default_4x4_Inter,
+                   aDest.scaling_matrix4x4[3]);
+      scaling_list(br, aDest.scaling_matrix4x4[5], Default_4x4_Inter,
+                   aDest.scaling_matrix4x4[4]);
+      if (aDest.transform_8x8_mode_flag) {
+        if (aSPS.seq_scaling_matrix_present_flag) {
+          scaling_list(br, aDest.scaling_matrix8x8[0], Default_8x8_Intra);
+          scaling_list(br, aDest.scaling_matrix8x8[1], Default_8x8_Inter);
+        } else {
+          scaling_list(br, aDest.scaling_matrix8x8[0], Default_8x8_Intra,
+                       Default_8x8_Intra);
+          scaling_list(br, aDest.scaling_matrix8x8[1], Default_8x8_Inter,
+                       Default_8x8_Inter);
+        }
+        if (aSPS.chroma_format_idc == 3) {
+          scaling_list(br, aDest.scaling_matrix8x8[2], Default_8x8_Intra,
+                       aDest.scaling_matrix8x8[0]);
+          scaling_list(br, aDest.scaling_matrix8x8[3], Default_8x8_Inter,
+                       aDest.scaling_matrix8x8[1]);
+          scaling_list(br, aDest.scaling_matrix8x8[4], Default_8x8_Intra,
+                       aDest.scaling_matrix8x8[2]);
+          scaling_list(br, aDest.scaling_matrix8x8[5], Default_8x8_Inter,
+                       aDest.scaling_matrix8x8[3]);
+        }
+      }
+    }
+    aDest.second_chroma_qp_index_offset = br.ReadSE();
+  }
+  return true;
 }
 
 /* static */ uint32_t
