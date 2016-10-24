d3928d343e5f69a40f6b98d24974c2106367afd8
Shih-Chiang Chien <schien@mozilla.com>
2016-10-24 19:11:26 +0800

    Bug 1311944 - notify device disconnect only when session termination. r=smaug
    MozReview-Commit-ID: JNVDv3L2r6t

--- dom/presentation/tests/mochitest/test_presentation_1ua_sender_and_receiver.js
+++ dom/presentation/tests/mochitest/test_presentation_1ua_sender_and_receiver.js
@@ -231,6 +231,24 @@ function testCloseConnection() {
       };
     }),
     new Promise(function(aResolve, aReject) {
+      let timeout = setTimeout(function() {
+        gScript.removeMessageListener('device-disconnected',
+                                      deviceDisconnectedHandler);
+        ok(true, "terminate after close should not trigger device.disconnect");
+        aResolve();
+      }, 3000);
+
+      function deviceDisconnectedHandler() {
+        gScript.removeMessageListener('device-disconnected',
+                                      deviceDisconnectedHandler);
+        ok(false, "terminate after close should not trigger device.disconnect");
+        clearTimeout(timeout);
+        aResolve();
+      }
+
+      gScript.addMessageListener('device-disconnected', deviceDisconnectedHandler);
+    }),
+    new Promise(function(aResolve, aReject) {
       gScript.addMessageListener('receiver-closed', function onReceiverClosed() {
         gScript.removeMessageListener('receiver-closed', onReceiverClosed);
         gScript.removeMessageListener('control-channel-established',
@@ -294,6 +312,8 @@ function testReconnect() {
         aReject();
       });
     });
+
+    postMessageToIframe('prepare-for-reconnect');
   });
 }
 
