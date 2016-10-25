06858c2f4da1e9a5019882b8ab832efa0b6735ca
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-10-25 12:32:58 +0200

    Backed out changeset ffaf3d371130 (bug 1198381)

--- dom/base/Timeout.h
+++ dom/base/Timeout.h
@@ -36,8 +36,6 @@ public:
 
   nsresult InitTimer(uint32_t aDelay);
 
-  enum class Reason { eTimeoutOrInterval, eIdleCallbackTimeout };
-
   static void TimerNameCallback(nsITimer* aTimer, void* aClosure, char* aBuf,
                                 size_t aLen);
 
@@ -60,10 +58,8 @@ public:
   // True if this is a repeating/interval timer
   bool mIsInterval;
 
-  Reason mReason;
-
   // Returned as value of setTimeout()
-  uint32_t mTimeoutId;
+  uint32_t mPublicId;
 
   // Interval in milliseconds
   uint32_t mInterval;
