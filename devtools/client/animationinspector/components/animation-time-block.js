d8e52e81992af9cfb6127d3c680cfb1da7b103fb
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-10-25 10:09:53 +0200

    Backed out changeset 9441ce3d4832 (bug 1210795)

--- devtools/client/animationinspector/components/animation-time-block.js
+++ devtools/client/animationinspector/components/animation-time-block.js
@@ -68,8 +68,11 @@ AnimationTimeBlock.prototype = {
     // Create a container element to hold the delay and iterations.
     // It is positioned according to its delay (divided by the playbackrate),
     // and its width is according to its duration (divided by the playbackrate).
-    const {x, delayX, delayW, endDelayX, endDelayW} =
+    let {x, iterationW, delayX, delayW, negativeDelayW, endDelayX, endDelayW} =
       TimeScale.getAnimationDimensions(animation);
+
+    // background properties for .iterations element
+    let backgroundIterations = TimeScale.getIterationsBackgroundData(animation);
 
     // Animation summary graph element.
     const summaryEl = createNode({
