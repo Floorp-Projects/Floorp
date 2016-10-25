55f7c0ac0ba36d15927f65a32bbd6cf3de10c146
Andrzej Hunt <ahunt@mozilla.com>
2016-10-25 11:29:23 -0700

    Bug 1312467 - Pass url-open listeners into TopSitesCard and handle clicks there r=sebastian
    This is in preparation for adding context-menu support to TopSitesCard, which requires
    the two url-open listeners.
    
    MozReview-Commit-ID: 7ubTCqCqcW9

--- mobile/android/base/java/org/mozilla/gecko/home/activitystream/topsites/TopSitesCard.java
+++ mobile/android/base/java/org/mozilla/gecko/home/activitystream/topsites/TopSitesCard.java
@@ -6,39 +6,56 @@ package org.mozilla.gecko.home.activitystream.topsites;
 
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.activitystream.ActivityStream;
+import org.mozilla.gecko.home.HomePager;
+import org.mozilla.gecko.home.activitystream.menu.ActivityStreamContextMenu;
 import org.mozilla.gecko.icons.IconCallback;
 import org.mozilla.gecko.icons.IconResponse;
 import org.mozilla.gecko.icons.Icons;
 import org.mozilla.gecko.util.DrawableUtil;
 import org.mozilla.gecko.widget.FaviconView;
 
+import java.util.EnumSet;
 import java.util.concurrent.Future;
 
-class TopSitesCard extends RecyclerView.ViewHolder implements IconCallback {
+class TopSitesCard extends RecyclerView.ViewHolder
+        implements IconCallback, View.OnClickListener {
     private final FaviconView faviconView;
 
     private final TextView title;
     private final ImageView menuButton;
     private Future<IconResponse> ongoingIconLoad;
 
-    public TopSitesCard(FrameLayout card) {
+    private String url;
+
+    private final HomePager.OnUrlOpenListener onUrlOpenListener;
+    private final HomePager.OnUrlOpenInBackgroundListener onUrlOpenInBackgroundListener;
+
+    public TopSitesCard(FrameLayout card, final HomePager.OnUrlOpenListener onUrlOpenListener, final HomePager.OnUrlOpenInBackgroundListener onUrlOpenInBackgroundListener) {
         super(card);
 
         faviconView = (FaviconView) card.findViewById(R.id.favicon);
 
         title = (TextView) card.findViewById(R.id.title);
         menuButton = (ImageView) card.findViewById(R.id.menu);
+
+        this.onUrlOpenListener = onUrlOpenListener;
+        this.onUrlOpenInBackgroundListener = onUrlOpenInBackgroundListener;
+
+        card.setOnClickListener(this);
     }
 
-    void bind(TopSitesPageAdapter.TopSite topSite) {
+    void bind(final TopSitesPageAdapter.TopSite topSite) {
         final String label = ActivityStream.extractLabel(topSite.url, true);
         title.setText(label);
+
+        this.url = topSite.url;
 
         if (ongoingIconLoad != null) {
             ongoingIconLoad.cancel(true);
@@ -59,5 +76,12 @@ class TopSitesCard extends RecyclerView.ViewHolder implements IconCallback {
 
         menuButton.setImageDrawable(
                 DrawableUtil.tintDrawable(menuButton.getContext(), R.drawable.menu, tintColor));
+    }
+
+    @Override
+    public void onClick(View clickedView) {
+        if (clickedView == itemView) {
+            onUrlOpenListener.onUrlOpen(url, EnumSet.noneOf(HomePager.OnUrlOpenListener.Flags.class));
+        }
     }
 }
