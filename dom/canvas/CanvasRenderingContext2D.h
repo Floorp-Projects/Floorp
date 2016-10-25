29fc2fb4172ad1297302b1caf079d4342625cdf0
Markus Stange <mstange@themasta.com>
2016-10-25 14:00:21 -0400

    Bug 1308859 - Back out the instrumentation because I think I know what the problem is now.
    MozReview-Commit-ID: 7XdGDR950df

--- dom/canvas/CanvasRenderingContext2D.h
+++ dom/canvas/CanvasRenderingContext2D.h
@@ -863,13 +863,24 @@ protected:
     * Returns true if the result of a drawing operation should be
     * drawn with a filter.
     */
-  bool NeedToApplyFilter();
+  bool NeedToApplyFilter()
+  {
+    return EnsureUpdatedFilter().mPrimitives.Length() > 0;
+  }
 
   /**
    * Calls UpdateFilter if the canvas's WriteOnly state has changed between the
    * last call to UpdateFilter and now.
    */
-  const gfx::FilterDescription& EnsureUpdatedFilter();
+  const gfx::FilterDescription& EnsureUpdatedFilter() {
+    const ContextState& state = CurrentState();
+    bool isWriteOnly = mCanvasElement && mCanvasElement->IsWriteOnly();
+    if (state.filterSourceGraphicTainted != isWriteOnly) {
+      UpdateFilter();
+    }
+    MOZ_ASSERT(state.filterSourceGraphicTainted == isWriteOnly);
+    return state.filter;
+  }
 
   bool NeedToCalculateBounds()
   {
@@ -1090,11 +1101,6 @@ protected:
   };
 
   AutoTArray<ContextState, 3> mStyleStack;
-
-  // Temporary instrumentation to help debug bug 1308859
-  static CanvasRenderingContext2D* sThisContext;
-  static bool sThisContextWasDestroyed;
-  static bool sThisContextHadItsFilterUpdated;
 
   inline ContextState& CurrentState() {
     return mStyleStack[mStyleStack.Length() - 1];
