56d597a09f5cdd818b335dac0753a87177b9e441
Jim Chen <nchen@mozilla.com>
2016-10-25 12:28:54 -0400

    Bug 1307816 - 11. Use GeckoThread for waiting on Gecko; r=esawin
    Right now we send a "process-gecko-events" message to
    GeckoInputConnection in order to wait on Gecko during testing. However,
    now we have GeckoThread.waitOnGecko() to do that, so we can just use
    that directly.

--- mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testInputConnection.java
+++ mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testInputConnection.java
@@ -191,7 +191,7 @@ public class testInputConnection extends JavascriptBridgeTest {
             assertTextAndSelectionAt("Can set the composing text", ic, "bad", 3);
             getJS().asyncCall("test_reflush_changes");
             // Wait for text change notifications to come in.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             assertTextAndSelectionAt("Can re-flush text changes", ic, "good", 4);
             ic.setComposingText("done", 1);
             assertTextAndSelectionAt("Can update composition after re-flushing", ic, "done", 4);
@@ -206,7 +206,7 @@ public class testInputConnection extends JavascriptBridgeTest {
             assertTextAndSelectionAt("Can set the composing text", ic, "foobar", 6);
             getJS().asyncCall("test_set_selection");
             // Wait for text change notifications to come in.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             assertTextAndSelectionAt("Can select after committing", ic, "foobar", 3);
             ic.setComposingText("barfoo", 1);
             assertTextAndSelectionAt("Can compose after selecting", ic, "barfoo", 6);
@@ -244,7 +244,7 @@ public class testInputConnection extends JavascriptBridgeTest {
             assertTextAndSelectionAt("Can clear text", ic, "", 0);
 
             // Make sure we don't leave behind stale events for the following test.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
         }
     }
@@ -275,21 +275,21 @@ public class testInputConnection extends JavascriptBridgeTest {
             // and the input connection thread. Therefore, to ensure these events are
             // issued and to ensure the bug appears, we have to process all Gecko events,
             // then all input connection events, and finally all Gecko events again.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
-            processGeckoEvents(ic);
+            processGeckoEvents();
             assertTextAndSelectionAt("Can set composing region (resetting)", ic, "foo", 3);
 
             ic.setComposingText("foobar", 1);
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
-            processGeckoEvents(ic);
+            processGeckoEvents();
             assertTextAndSelectionAt("Can change composing text (resetting)", ic, "foobar", 6);
 
             ic.setComposingText("baz", 1);
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
-            processGeckoEvents(ic);
+            processGeckoEvents();
             assertTextAndSelectionAt("Can reset composing text (resetting)", ic, "baz", 3);
 
             ic.finishComposingText();
@@ -299,7 +299,7 @@ public class testInputConnection extends JavascriptBridgeTest {
             assertTextAndSelectionAt("Can clear text", ic, "", 0);
 
             // Make sure we don't leave behind stale events for the following test.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
         }
     }
@@ -329,7 +329,7 @@ public class testInputConnection extends JavascriptBridgeTest {
             assertTextAndSelectionAt("Can handle hiding input", ic, "foo", 3);
 
             // Make sure we don't leave behind stale events for the following test.
-            processGeckoEvents(ic);
+            processGeckoEvents();
             processInputConnectionEvents();
         }
     }
