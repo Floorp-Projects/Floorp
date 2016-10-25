f2270cab9362411fca3bd15fc7d3e67d2c5014c7
Kan-Ru Chen <kanru@kanru.info>
2016-10-25 22:35:02 +0800

    Bug 1081858 - Part 4. Implement segment break transformation rules. r=jfkthame
    MozReview-Commit-ID: BcOm4LVWGzW

--- intl/unicharutil/util/nsUnicharUtils.cpp
+++ intl/unicharutil/util/nsUnicharUtils.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
@@ -423,6 +423,13 @@ HashUTF8AsUTF16(const char* aUTF8, uint32_t aLength, bool* aErr)
   }
 
   return hash;
+}
+
+bool
+IsSegmentBreakSkipChar(uint32_t u)
+{
+  return unicode::IsEastAsianWidthFWH(u) &&
+         unicode::GetScriptCode(u) != unicode::Script::HANGUL;
 }
 
 } // namespace mozilla
