96829e967df2b0258356b9680b54400065104a57
Nicholas Hurley <hurley@todesschaf.org>
2016-10-26 14:05:27 -0700

    Bug 1313099 - Put nodejs in tooltool (mozharness changes) r=jlund
    MozReview-Commit-ID: 7ijnlF5D9B2

--- testing/mozharness/configs/remove_executables.py
+++ testing/mozharness/configs/remove_executables.py
@@ -2,5 +2,7 @@ config = {
     # We bake this directly into the tester image now...
     "download_minidump_stackwalk": False,
     "minidump_stackwalk_path": "/usr/local/bin/linux64-minidump_stackwalk",
+    "download_nodejs": False,
+    "nodejs_path": "/usr/local/bin/node",
     "exes": {}
 }
