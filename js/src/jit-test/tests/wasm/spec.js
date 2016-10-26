17414360abc04f3562af46abff34bb084fff9b0d
Benjamin Bouvier <benj@benj.me>
2016-10-26 18:42:25 +0200

    Bug 1313024: Implement assert_soft_invalid in the wast interpreter; r=luke
    MozReview-Commit-ID: 6P8lfjzxUSd
    
    --HG--
    extra : rebase_source : d32588c65f69ada094be112b93e6fa7fe3ca8337
    extra : histedit_source : 8eb589e2c91d4ce7e594810ebc04f720edeab791

--- js/src/jit-test/tests/wasm/spec.js
+++ js/src/jit-test/tests/wasm/spec.js
@@ -416,6 +416,23 @@ function exec(e) {
         return;
     }
 
+    if (exprName === "assert_soft_invalid") {
+        let moduleText = e.list[1].toString();
+        let errMsg = e.list[2];
+        if (errMsg) {
+            assert(errMsg.quoted, "assert_soft_invalid second argument must be a string");
+            errMsg.quoted = false;
+        }
+
+        try {
+            new WebAssembly.Module(wasmTextToBinary(moduleText));
+        } catch(e) {
+            debug('assert_soft_invalid caught:\nExpected:', errMsg, '\nActual:', e.toString());
+        }
+
+        return;
+    }
+
     if (exprName === 'assert_trap') {
         let caught = false;
         let errMsg = e.list[2];
--- js/src/jit-test/tests/wasm/spec/soft-fail.wast.js
+++ js/src/jit-test/tests/wasm/spec/soft-fail.wast.js
@@ -1,4 +1,2 @@
 // |jit-test| test-also-wasm-baseline
-// TODO operator assert_soft_invalid
-quit();
 var importedArgs = ['soft-fail.wast']; load(scriptdir + '../spec.js');
