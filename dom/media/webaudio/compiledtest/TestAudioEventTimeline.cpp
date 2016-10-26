98715f6438146f5998749e13027c9f70ff303fd2
Dan Minor <dminor@mozilla.com>
2016-10-26 10:33:20 -0400

    Bug 1313058 - Fix SetValueCurveAtTime interpolation; r=padenot
    This interpolates over aCurveLength - 1 steps rather than over
    aCurveLength steps as was done before.
    
    Previously we would reach the final value on the curve before
    the end of the specified duration. For example, on the curve
    [1.0, 0.0] with duration 1000, we would interpolate from 1.0 to
    0.0 by time 500 rather than time 1000. With these changes, we
    don't reach 0.0 until time 1000, as expected.
    
    This also updates TestSpecExample in TestAudioEventTimeline.cpp
    to match the curve in the latest spec.
    
    MozReview-Commit-ID: Cgs8csbRUMh
    
    --HG--
    extra : rebase_source : 1960128558ae9174933cd5be3c1fbfcb79f5ba1d

--- dom/media/webaudio/compiledtest/TestAudioEventTimeline.cpp
+++ dom/media/webaudio/compiledtest/TestAudioEventTimeline.cpp
@@ -102,7 +102,11 @@ void TestSpecExample()
 
   ErrorResultMock rv;
 
-  float curve[] = { -1.0f, 0.0f, 1.0f };
+  uint32_t curveLength = 44100;
+  float* curve = new float[curveLength];
+  for (uint32_t i = 0; i < curveLength; ++i) {
+    curve[i] = sin(M_PI * i / float(curveLength));
+  }
 
   // This test is copied from the example in the Web Audio spec
   const double t0 = 0.0,
@@ -127,7 +131,7 @@ void TestSpecExample()
   is(rv, NS_OK, "ExponentialRampToValueAtTime succeeded");
   timeline.ExponentialRampToValueAtTime(0.05f, t6, rv);
   is(rv, NS_OK, "ExponentialRampToValueAtTime succeeded");
-  timeline.SetValueCurveAtTime(curve, ArrayLength(curve), t6, t7 - t6, rv);
+  timeline.SetValueCurveAtTime(curve, curveLength, t6, t7 - t6, rv);
   is(rv, NS_OK, "SetValueCurveAtTime succeeded");
 
   is(timeline.GetValueAtTime(0.0), 0.2f, "Correct value");
@@ -144,10 +148,11 @@ void TestSpecExample()
   is(timeline.GetValueAtTime(0.55), (0.15f * powf(0.75f / 0.15f, 0.15f / 0.2f)), "Correct value");
   is(timeline.GetValueAtTime(0.6), 0.75f, "Correct value");
   is(timeline.GetValueAtTime(0.65), (0.75f * powf(0.05f / 0.75f, 0.5f)), "Correct value");
-  is(timeline.GetValueAtTime(0.7), -1.0f, "Correct value");
-  is(timeline.GetValueAtTime(0.8), 0.0f, "Correct value");
-  is(timeline.GetValueAtTime(0.9), 1.0f, "Correct value");
-  is(timeline.GetValueAtTime(1.0), 1.0f, "Correct value");
+  is(timeline.GetValueAtTime(0.7), 0.0f, "Correct value");
+  is(timeline.GetValueAtTime(0.85), 1.0f, "Correct value");
+  is(timeline.GetValueAtTime(1.0), curve[curveLength - 1], "Correct value");
+
+  delete[] curve;
 }
 
 void TestInvalidEvents()
