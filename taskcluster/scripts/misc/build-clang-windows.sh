8e876a257657af5c60d32df47aab19c73a54eb9c
Nathan Froyd <froydnj@mozilla.com>
2016-10-26 16:18:24 -0400

    Bug 1306650 - part 4 - modify build-clang-windows.sh for running under taskcluster; r=ehsan
    There are a number of changes here:
    
    - Taskcluster machines don't have MSVC installed, so we have to setup all
      the MSVC infrastructure ourselves;
    - Gecko contains useful Python packages to use, so we need to setup a
      python virtualenv to have access to those.  Fortunately, mach handles all
      of that for us;
    - We need to add build tools to our PATH, as they are not pre-installed;
    - We need to define UPLOAD_PATH ourself.

--- taskcluster/scripts/misc/build-clang-windows.sh
+++ taskcluster/scripts/misc/build-clang-windows.sh
@@ -19,14 +19,39 @@ fi
 TOOLTOOL_MANIFEST=build/src/browser/config/tooltool-manifests/win32/releng.manifest
 ./tooltool.py --authentication-file="${TOOLTOOL_AUTH_FILE}" -m "${TOOLTOOL_MANIFEST}" fetch
 
+# Set up all the Visual Studio paths.
+MSVC_DIR=vs2015u3
+VSWINPATH="$(cd ${MSVC_DIR} && pwd)"
+
+echo vswinpath ${VSWINPATH}
+
+export WINDOWSSDKDIR="${VSWINPATH}/SDK"
+export WIN32_REDIST_DIR="${VSWINPATH}/VC/redist/x86/Microsoft.VC140.CRT"
+export WIN_UCRT_REDIST_DIR="${VSWINPATH}/SDK/Redist/ucrt/DLLs/x86"
+
+export PATH="${VSWINPATH}/VC/bin/amd64_x86:${VSWINPATH}/VC/bin/amd64:${VSWINPATH}/VC/bin:${VSWINPATH}/SDK/bin/x86:${VSWINPATH}/SDK/bin/x64:${VSWINPATH}/DIA SDK/bin:${PATH}"
+export PATH="${VSWINPATH}/VC/redist/x86/Microsoft.VC140.CRT:${VSWINPATH}/VC/redist/x64/Microsoft.VC140.CRT:${VSWINPATH}/SDK/Redist/ucrt/DLLs/x86:${VSWINPATH}/SDK/Redist/ucrt/DLLs/x64:${PATH}"
+
+export INCLUDE="${VSWINPATH}/VC/include:${VSWINPATH}/VC/atlmfc/include:${VSWINPATH}/SDK/Include/10.0.14393.0/ucrt:${VSWINPATH}/SDK/Include/10.0.14393.0/shared:${VSWINPATH}/SDK/Include/10.0.14393.0/um:${VSWINPATH}/SDK/Include/10.0.14393.0/winrt:${VSWINPATH}/DIA SDK/include"
+export LIB="${VSWINPATH}/VC/lib:${VSWINPATH}/VC/atlmfc/lib:${VSWINPATH}/SDK/lib/10.0.14393.0/ucrt/x86:${VSWINPATH}/SDK/lib/10.0.14393.0/um/x86:${VSWINPATH}/DIA SDK/lib"
+
+# We use |mach python| to set up a virtualenv automatically for us.  We create
+# a dummy mozconfig, because the default machinery for config.guess-choosing
+# of the objdir doesn't work very well.
+MOZCONFIG="$(pwd)/mozconfig"
+cat > ${MOZCONFIG} <<EOF
+mk_add_options MOZ_OBJDIR=$(pwd)/objdir
+EOF
+
 # gets a bit too verbose here
 set +x
 
-cd build/src/build/build-clang
-/c/mozilla-build/python/python.exe ./build-clang.py -c clang-static-analysis-win32.json
+BUILD_CLANG_DIR=build/src/build/build-clang
+MOZCONFIG=${MOZCONFIG} build/src/mach python ${BUILD_CLANG_DIR}/build-clang.py -c ${BUILD_CLANG_DIR}/clang-static-analysis-win32.json
 
 set -x
 
 # Put a tarball in the artifacts dir
+UPLOAD_PATH=public/build
 mkdir -p ${UPLOAD_PATH}
 cp clang.tar.* ${UPLOAD_PATH}
