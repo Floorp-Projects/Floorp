8d53811f32ced849f993020f039c1a7d4b825b96
Lee Salzman <lsalzman@mozilla.com>
2016-10-26 13:41:07 -0400

    Bug 1309272, part 3 - Allow DrawTargetSkia to work without an SkSurface. r=mchang
    MozReview-Commit-ID: JczfiM0eIct
    
    --HG--
    extra : rebase_source : 8f14d4a8c2cbbc349cf81b6c31859090a9511dcf

--- gfx/2d/DrawTargetSkia.cpp
+++ gfx/2d/DrawTargetSkia.cpp
@@ -273,7 +273,7 @@ already_AddRefed<SourceSurface>
 DrawTargetSkia::Snapshot()
 {
   RefPtr<SourceSurfaceSkia> snapshot = mSnapshot;
-  if (!snapshot) {
+  if (mSurface && !snapshot) {
     snapshot = new SourceSurfaceSkia();
     sk_sp<SkImage> image;
     // If the surface is raster, making a snapshot may trigger a pixel copy.
@@ -1863,7 +1863,7 @@ void*
 DrawTargetSkia::GetNativeSurface(NativeSurfaceType aType)
 {
 #ifdef USE_SKIA_GPU
-  if (aType == NativeSurfaceType::OPENGL_TEXTURE) {
+  if (aType == NativeSurfaceType::OPENGL_TEXTURE && mSurface) {
     GrBackendObject handle = mSurface->getTextureHandle(SkSurface::kFlushRead_BackendHandleAccess);
     if (handle) {
       return (void*)(uintptr_t)reinterpret_cast<GrGLTextureInfo *>(handle)->fID;
@@ -2114,7 +2114,9 @@ DrawTargetSkia::MarkChanged()
     mSnapshot = nullptr;
 
     // Handle copying of any image snapshots bound to the surface.
-    mSurface->notifyContentWillChange(SkSurface::kRetain_ContentChangeMode);
+    if (mSurface) {
+      mSurface->notifyContentWillChange(SkSurface::kRetain_ContentChangeMode);
+    }
   }
 }
 
