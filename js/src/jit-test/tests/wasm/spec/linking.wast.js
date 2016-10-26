2501d1e5c66b0e01a82d28e5be4a84ef54602751
Benjamin Bouvier <benj@benj.me>
2016-10-26 18:42:25 +0200

    Bug 1313024: Implement assert_soft_invalid in the wast interpreter; r=luke
    MozReview-Commit-ID: 6P8lfjzxUSd

--- js/src/jit-test/tests/wasm/spec/linking.wast.js
+++ js/src/jit-test/tests/wasm/spec/linking.wast.js
@@ -1,4 +1,4 @@
 // |jit-test| test-also-wasm-baseline
-// TODO type mismatch on reimports
+// TODO type checks against exotic exported function objects
 quit();
 var importedArgs = ['linking.wast']; load(scriptdir + '../spec.js');
