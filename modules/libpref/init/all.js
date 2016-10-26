60132b603e523397919d1bfa86be91802ef7cdda
Shawn Huang <shuang@mozilla.com>
2016-10-26 21:38:08 +0800

    Bug 1304966 - Enable Storage API only for nightly bulid, r=baku

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5513,3 +5513,10 @@ pref("security.mixed_content.use_hsts", false);
 // mixed-content blocking
 pref("security.mixed_content.use_hsts", true);
 #endif
+
+// Disable Storage api in release builds.
+#ifdef NIGHTLY_BUILD
+pref("dom.storageManager.enabled", true);
+#else
+pref("dom.storageManager.enabled", false);
+#endif
