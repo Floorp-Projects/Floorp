70feaf53f3db269e5ec24687a303666d8d902259
J. Ryan Stinnett <jryans@gmail.com>
2016-10-26 20:01:53 -0500

    Bug 1278762 - Support view source in RDM. r=ochameau
    MozReview-Commit-ID: 6RVynUTJ4qY

--- devtools/client/responsive.html/browser/tunnel.js
+++ devtools/client/responsive.html/browser/tunnel.js
@@ -109,6 +109,17 @@ function tunnelToInnerBrowser(outer, inner) {
         enumerable: true,
       });
 
+      // The `outerWindowID` of the content is used by browser actions like view source
+      // and print.  They send the ID down to the client to find the right content frame
+      // to act on.
+      Object.defineProperty(outer, "outerWindowID", {
+        get() {
+          return inner.outerWindowID;
+        },
+        configurable: true,
+        enumerable: true,
+      });
+
       // The `permanentKey` property on a <xul:browser> is used to index into various maps
       // held by the session store.  When you swap content around with
       // `_swapBrowserDocShells`, these keys are also swapped so they follow the content.
@@ -274,6 +285,7 @@ function tunnelToInnerBrowser(outer, inner) {
       // are on the prototype.
       delete outer.frameLoader;
       delete outer[FRAME_LOADER];
+      delete outer.outerWindowID;
 
       // Invalidate outer's permanentKey so that SessionStore stops associating
       // things that happen to the outer browser with the content inside in the
