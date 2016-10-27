d0b8c4ae757dcea136d693304e4366779dcf2730
Honza Bambas <honzab.moz@firemni.cz>
2016-10-27 06:13:00 -0400

    Bug 1312646 - Carry ChooseApplicationCache flag from child after redirect callback is done. r=jduell

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -1684,10 +1684,17 @@ HttpChannelChild::OnRedirectVerifyCallback(nsresult result)
     }
   }
 
+  bool chooseAppcache = false;
+  nsCOMPtr<nsIApplicationCacheChannel> appCacheChannel =
+    do_QueryInterface(newHttpChannel);
+  if (appCacheChannel) {
+    appCacheChannel->GetChooseApplicationCache(&chooseAppcache);
+  }
+
   if (mIPCOpen)
     SendRedirect2Verify(result, *headerTuples, loadFlags, redirectURI,
                         corsPreflightArgs, forceHSTSPriming,
-                        mixedContentWouldBlock);
+                        mixedContentWouldBlock, chooseAppcache);
 
   return NS_OK;
 }
