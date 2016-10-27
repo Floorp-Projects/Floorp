1b2dbe1daa7b173f3e5d6fbf6983f0309c730099
Phil Ringnalda <philringnalda@gmail.com>
2016-10-26 20:48:39 -0700

    Backed out changeset baf3f0f96116 (bug 1311960) for Android mochitest, mochitest-media, and crashtest bustage

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -619,11 +619,7 @@ pref("media.video-queue.default-size", 3);
 pref("media.android-media-codec.enabled", true);
 pref("media.android-media-codec.preferred", true);
 // Run decoder in seperate process.
-#ifdef NIGHTLY_BUILD
-pref("media.android-remote-codec.enabled", true);
-#else
 pref("media.android-remote-codec.enabled", false);
-#endif
 
 // Enable MSE
 pref("media.mediasource.enabled", true);
