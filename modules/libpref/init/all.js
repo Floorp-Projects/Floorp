85c5a7368381e058af4301e86544e0daf98aad9c
George Wright <george@mozilla.com>
2016-10-27 11:25:58 -0400

    Bug 1306465 - Disable d3d9 fallback by default r=milan

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4617,7 +4617,8 @@ pref("gfx.direct2d.force-enabled", false);
 
 pref("layers.prefer-opengl", false);
 pref("layers.prefer-d3d9", false);
-pref("layers.allow-d3d9-fallback", true);
+// Disable for now due to bug 1304360
+pref("layers.allow-d3d9-fallback", false);
 #endif
 
 // Copy-on-write canvas
