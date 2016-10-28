6bdfc05c485ab759482c7e205dd12e08e985dc40
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-10-28 17:03:56 +1100

    Bug 1292374: P3. Enable VP9 hardware acceleration on windows. r=gerald
    MozReview-Commit-ID: BETY7nFDu5o

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -120,7 +120,7 @@ private:
 #ifdef XP_WIN
   DECL_MEDIA_PREF("media.wmf.enabled",                        PDMWMFEnabled, bool, true);
   DECL_MEDIA_PREF("media.decoder-doctor.wmf-disabled-is-failure", DecoderDoctorWMFDisabledIsFailure, bool, false);
-  DECL_MEDIA_PREF("media.wmf.vp9.enabled",                    PDMWMFVP9DecoderEnabled, bool, false);
+  DECL_MEDIA_PREF("media.wmf.vp9.enabled",                    PDMWMFVP9DecoderEnabled, bool, true);
   DECL_MEDIA_PREF("media.wmf.low-latency.enabled",            PDMWMFLowLatencyEnabled, bool, false);
   DECL_MEDIA_PREF("media.wmf.decoder.thread-count",           PDMWMFThreadCount, int32_t, -1);
   DECL_MEDIA_PREF("media.wmf.skip-blacklist",                 PDMWMFSkipBlacklist, bool, false);
