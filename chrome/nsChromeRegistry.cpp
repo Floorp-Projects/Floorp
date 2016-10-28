e6931aeaa20ac6821ad51249986f070acdc37cdf
Jonathan Kew <jkew@mozilla.com>
2016-10-28 12:04:07 +0100

    Bug 1312049 - Switch nsChromeRegistry::GetDirectionForLocale to use uloc_isRightToLeft. r=gandalf

--- chrome/nsChromeRegistry.cpp
+++ chrome/nsChromeRegistry.cpp
@@ -650,6 +650,11 @@ nsChromeRegistry::MustLoadURLRemotely(nsIURI *aURI, bool *aResult)
 bool
 nsChromeRegistry::GetDirectionForLocale(const nsACString& aLocale)
 {
+#ifdef ENABLE_INTL_API
+  nsAutoCString locale(aLocale);
+  SanitizeForBCP47(locale);
+  return uloc_isRightToLeft(locale.get());
+#else
   // first check the intl.uidirection.<locale> preference, and if that is not
   // set, check the same preference but with just the first two characters of
   // the locale. If that isn't set, default to left-to-right.
@@ -670,6 +675,7 @@ nsChromeRegistry::GetDirectionForLocale(const nsACString& aLocale)
   }
 
   return dir.EqualsLiteral("rtl");
+#endif
 }
 
 NS_IMETHODIMP_(bool)
