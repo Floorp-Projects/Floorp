196f6ec151847109063e91ad73cf0295b5950a2f
Andrew Swan <aswan@mozilla.com>
2016-10-28 14:39:18 -0700

    Bug 1308688 - Expose mozAddonManager allowed hosts to chrome. r=bkelly, a=gchang
    MozReview-Commit-ID: JI3qMmCtYFT

--- toolkit/mozapps/extensions/AddonManagerWebAPI.h
+++ toolkit/mozapps/extensions/AddonManagerWebAPI.h
@@ -19,6 +19,15 @@ private:
   static bool IsValidSite(nsIURI* uri);
 };
 
+namespace dom {
+
+class AddonManagerPermissions {
+public:
+  static bool IsHostPermitted(const GlobalObject&, const nsAString& host);
+};
+
+} // namespace mozilla::dom
+
 } // namespace mozilla
 
 #endif // addonmanagerwebapi_h_
