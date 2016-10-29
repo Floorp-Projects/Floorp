8379986e33f73e0c7ed7777d178ce2738b0e4815
catdesk <catdesk@tuta.io>
2016-10-29 11:43:03 +0200

    Bug 1313834 - Clean up Promise usage of SpecialPowers.pushPrefEnv in browser_aboutCertError.js. r=johannh
    MozReview-Commit-ID: 8rsjCQjbXQk
    
    --HG--
    extra : rebase_source : c4693d9b6e89cc4d91e3159ec3896ba478e178b4

--- browser/base/content/test/general/browser_aboutCertError.js
+++ browser/base/content/test/general/browser_aboutCertError.js
@@ -145,8 +145,7 @@ add_task(function* checkWrongSystemTimeWarning() {
   let localDateFmt = formatter.format(new Date());
 
   let skew = Math.floor((Date.now() - serverDate.getTime()) / 1000);
-  yield new Promise(r => SpecialPowers.pushPrefEnv({set:
-    [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]}, r));
+  yield SpecialPowers.pushPrefEnv({set: [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]});
 
   info("Loading a bad cert page with a skewed clock");
   let message = yield Task.spawn(setUpPage);
@@ -167,8 +166,7 @@ add_task(function* checkWrongSystemTimeWarning() {
   serverDateFmt = formatter.format(serverDate);
 
   skew = Math.floor((Date.now() - serverDate.getTime()) / 1000);
-  yield new Promise(r => SpecialPowers.pushPrefEnv({set:
-    [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]}, r));
+  yield SpecialPowers.pushPrefEnv({set: [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]});
 
   info("Loading a bad cert page with a skewed clock");
   message = yield Task.spawn(setUpPage);
@@ -184,8 +182,7 @@ add_task(function* checkWrongSystemTimeWarning() {
 
   // pretend we only have a slightly skewed system time, four hours
   skew = 60 * 60 * 4;
-  yield new Promise(r => SpecialPowers.pushPrefEnv({set:
-    [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]}, r));
+  yield SpecialPowers.pushPrefEnv({set: [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]});
 
   info("Loading a bad cert page with an only slightly skewed clock");
   message = yield Task.spawn(setUpPage);
@@ -196,8 +193,7 @@ add_task(function* checkWrongSystemTimeWarning() {
 
   // now pretend we have no skewed system time
   skew = 0;
-  yield new Promise(r => SpecialPowers.pushPrefEnv({set:
-    [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]}, r));
+  yield SpecialPowers.pushPrefEnv({set: [[PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, skew]]});
 
   info("Loading a bad cert page with no skewed clock");
   message = yield Task.spawn(setUpPage);
