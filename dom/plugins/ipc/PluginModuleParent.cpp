81c169c2a9f20477281ee535cef80b9ef0efc37e
Bill McCloskey <billm@mozilla.com>
2016-10-30 11:26:40 -0700

    Bug 792652 - Remove methods from MessageListener (r=dvander)
    With this change, MessageChannel stores mListener as an IToplevelProtocol
    rather than a MessageListener (which isn't really a useful concept on
    its own). The MessageListener methods are split out to IProtocol and
    IToplevelProtocol. MessageListener gets deleted. Some of the inline
    functions in MessageChannel had to be moved to MessageChannel.cpp since
    IToplevelProtocol isn't defined in MessageChannel.h.

--- dom/plugins/ipc/PluginModuleParent.cpp
+++ dom/plugins/ipc/PluginModuleParent.cpp
@@ -1036,8 +1036,7 @@ PluginInstanceParent*
 PluginModuleChromeParent::GetManagingInstance(mozilla::ipc::IProtocol* aProtocol)
 {
     MOZ_ASSERT(aProtocol);
-    mozilla::ipc::MessageListener* listener =
-        static_cast<mozilla::ipc::MessageListener*>(aProtocol);
+    mozilla::ipc::IProtocol* listener = aProtocol;
     switch (listener->GetProtocolTypeId()) {
         case PPluginInstanceMsgStart:
             // In this case, aProtocol is the instance itself. Just cast it.
