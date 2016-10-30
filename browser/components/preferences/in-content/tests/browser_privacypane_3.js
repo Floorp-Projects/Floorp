72dbe3512dfbd331c5a3d6773c3d0dc3cb0744a6
David Malaschonok <davidmalaschonok@gmail.com>
2016-10-30 11:59:29 +0100

    Bug 552434 - Allow the history mode to stay in Custom even if the settings match the preference values for keeping history. r=jaws
    MozReview-Commit-ID: 4xLKlNRvHkz

--- browser/components/preferences/in-content/tests/browser_privacypane_3.js
+++ browser/components/preferences/in-content/tests/browser_privacypane_3.js
@@ -11,7 +11,7 @@ loader.loadSubScript(rootDir + "privacypane_tests_perwindow.js", this);
 run_test_subset([
   test_custom_retention("rememberHistory", "remember"),
   test_custom_retention("rememberHistory", "custom"),
-  test_custom_retention("rememberForms", "remember"),
   test_custom_retention("rememberForms", "custom"),
-  test_historymode_retention("remember", "remember"),
+  test_custom_retention("rememberForms", "custom"),
+  test_historymode_retention("remember", "custom"),
 ]);
