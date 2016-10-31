1817f9012b0327b5ccfd43f059304d5ebb9cba22
Kit Cambridge <kit@yakshaving.ninja>
2016-10-31 11:13:14 -0700

    Bug 1314084 - Implement `nsIAnnotationService::getObservers`. r=mak
    MozReview-Commit-ID: 42y8d1fbfJO

--- toolkit/components/places/tests/unit/test_null_interfaces.js
+++ toolkit/components/places/tests/unit/test_null_interfaces.js
@@ -19,7 +19,7 @@ var testServices = [
      "onPageAnnotationSet", "onPageAnnotationRemoved"]
   ],
   ["browser/livemark-service;2", ["mozIAsyncLivemarks"], ["reloadLivemarks"]],
-  ["browser/annotation-service;1", ["nsIAnnotationService"], []],
+  ["browser/annotation-service;1", ["nsIAnnotationService"], ["getObservers"]],
   ["browser/favicon-service;1", ["nsIFaviconService"], []],
   ["browser/tagging-service;1", ["nsITaggingService"], []],
 ];
