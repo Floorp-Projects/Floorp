34ffdef630cae362ea6c65f532a81b154f9dd243
Valentin Gosu <valentin.gosu@gmail.com>
2016-10-31 15:21:14 +0100

    Bug 1307456 - Remove PackagedAppService/Verifier r=bagder
    MozReview-Commit-ID: LgT7RQAbEf8

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1594,16 +1594,6 @@ pref("network.http.tcp_keepalive.long_lived_idle_time", 600);
 pref("network.http.enforce-framing.http1", false); // should be named "strict"
 pref("network.http.enforce-framing.soft", true);
 
-// Whether nsHttpChannel should use the PackagedAppService to load
-// resources from a package when directed to a URL
-// such as http://domain.com/package.pak!//resource.html
-// See http://www.w3.org/TR/web-packaging/#streamable-package-format
-pref("network.http.enable-packaged-apps", false);
-
-// Enable this to bring in the signature verification if the signature exists.
-// Set to false if you don't need the signed packaged web app support (i.e. NSec).
-pref("network.http.signed-packages.enabled", false);
-
 // If it is set to false, headers with empty value will not appear in the header
 // array - behavior as it used to be. If it is true: empty headers coming from
 // the network will exist in header array as empty string. Call SetHeader with
