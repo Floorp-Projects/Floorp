2b0c24a4d24fd8efbbaefc6c738175a1b5932cf9
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-10-31 15:07:31 +0100

    Backed out changeset 6892ec7e77f3 (bug 1232966)

--- js/src/jit-test/tests/sharedbuf/byteLength.js
+++
@@ -1,9 +0,0 @@
-// SharedArrayBuffer.prototype.byteLength
-
-if (!this.SharedArrayBuffer)
-    quit(0);
-
-load(libdir + "asserts.js");
-
-let buffer = new SharedArrayBuffer(137);
-assertEq(buffer.byteLength, 137);
