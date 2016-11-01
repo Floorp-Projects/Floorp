2dee0550941876f1639130fc161478c2cc8d6f55
Jeff Gilbert (:jgilbert) <jgilbert@mozilla.com>
2016-11-01 14:58:07 -0700

    Bug 1318507 - Update implementation. - r=ethlin a=gchang
    MozReview-Commit-ID: CxFIfXyVm0O

--- dom/canvas/WebGLSampler.cpp
+++ dom/canvas/WebGLSampler.cpp
@@ -53,44 +53,134 @@ WebGLSampler::WrapObject(JSContext* cx, JS::Handle<JSObject*> givenProto)
     return dom::WebGLSamplerBinding::Wrap(cx, this, givenProto);
 }
 
-void
-WebGLSampler::SamplerParameter1i(GLenum pname, GLint param)
+static bool
+ValidateSamplerParameterParams(WebGLContext* webgl, const char* funcName, GLenum pname,
+                               GLint paramInt)
 {
     switch (pname) {
     case LOCAL_GL_TEXTURE_MIN_FILTER:
-        mMinFilter = param;
+        switch (paramInt) {
+        case LOCAL_GL_NEAREST:
+        case LOCAL_GL_LINEAR:
+        case LOCAL_GL_NEAREST_MIPMAP_NEAREST:
+        case LOCAL_GL_NEAREST_MIPMAP_LINEAR:
+        case LOCAL_GL_LINEAR_MIPMAP_NEAREST:
+        case LOCAL_GL_LINEAR_MIPMAP_LINEAR:
+            return true;
+
+        default:
+            break;
+        }
         break;
 
     case LOCAL_GL_TEXTURE_MAG_FILTER:
-        mMagFilter = param;
+        switch (paramInt) {
+        case LOCAL_GL_NEAREST:
+        case LOCAL_GL_LINEAR:
+            return true;
+
+        default:
+            break;
+        }
         break;
 
     case LOCAL_GL_TEXTURE_WRAP_S:
-        mWrapS = param;
-        break;
-
     case LOCAL_GL_TEXTURE_WRAP_T:
-        mWrapT = param;
-        break;
-
     case LOCAL_GL_TEXTURE_WRAP_R:
-        mWrapR = param;
-        break;
+        switch (paramInt) {
+        case LOCAL_GL_CLAMP_TO_EDGE:
+        case LOCAL_GL_REPEAT:
+        case LOCAL_GL_MIRRORED_REPEAT:
+            return true;
 
-    case LOCAL_GL_TEXTURE_COMPARE_MODE:
-        mCompareMode = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_COMPARE_FUNC:
-        mCompareFunc = param;
+        default:
+            break;
+        }
         break;
 
     case LOCAL_GL_TEXTURE_MIN_LOD:
-        mMinLod = param;
+    case LOCAL_GL_TEXTURE_MAX_LOD:
+        return true;
+
+    case LOCAL_GL_TEXTURE_COMPARE_MODE:
+        switch (paramInt) {
+        case LOCAL_GL_NONE:
+        case LOCAL_GL_COMPARE_REF_TO_TEXTURE:
+            return true;
+
+        default:
+            break;
+        }
+        break;
+
+    case LOCAL_GL_TEXTURE_COMPARE_FUNC:
+        switch (paramInt) {
+        case LOCAL_GL_LEQUAL:
+        case LOCAL_GL_GEQUAL:
+        case LOCAL_GL_LESS:
+        case LOCAL_GL_GREATER:
+        case LOCAL_GL_EQUAL:
+        case LOCAL_GL_NOTEQUAL:
+        case LOCAL_GL_ALWAYS:
+        case LOCAL_GL_NEVER:
+            return true;
+
+        default:
+            break;
+        }
+        break;
+
+    default:
+        webgl->ErrorInvalidEnum("%s: invalid pname: %s", funcName,
+                                webgl->EnumName(pname));
+        return false;
+    }
+
+    webgl->ErrorInvalidEnum("%s: invalid param: %s", funcName, webgl->EnumName(paramInt));
+    return false;
+}
+
+void
+WebGLSampler::SamplerParameter(const char* funcName, GLenum pname, GLint paramInt)
+{
+    if (!ValidateSamplerParameterParams(mContext, funcName, pname, paramInt))
+        return;
+
+    switch (pname) {
+    case LOCAL_GL_TEXTURE_MIN_FILTER:
+        mMinFilter = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_MAG_FILTER:
+        mMagFilter = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_WRAP_S:
+        mWrapS = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_WRAP_T:
+        mWrapT = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_WRAP_R:
+        mWrapR = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_COMPARE_MODE:
+        mCompareMode = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_COMPARE_FUNC:
+        mCompareFunc = paramInt;
+        break;
+
+    case LOCAL_GL_TEXTURE_MIN_LOD:
+        mMinLod = paramInt;
         break;
 
     case LOCAL_GL_TEXTURE_MAX_LOD:
-        mMaxLod = param;
+        mMaxLod = paramInt;
         break;
 
     default:
@@ -102,59 +192,14 @@ WebGLSampler::SamplerParameter1i(GLenum pname, GLint param)
         if (this == mContext->mBoundSamplers[i])
             mContext->InvalidateResolveCacheForTextureWithTexUnit(i);
     }
+
+    ////
+
+    mContext->gl->MakeCurrent();
+    mContext->gl->fSamplerParameteri(mGLName, pname, paramInt);
 }
 
-void
-WebGLSampler::SamplerParameter1f(GLenum pname, GLfloat param)
-{
-    switch (pname) {
-    case LOCAL_GL_TEXTURE_MIN_LOD:
-        mMinLod = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_MAX_LOD:
-        mMaxLod = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_WRAP_S:
-        mWrapS = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_WRAP_T:
-        mWrapT = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_WRAP_R:
-        mWrapR = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_MAG_FILTER:
-        mMagFilter = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_MIN_FILTER:
-        mMinFilter = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_COMPARE_MODE:
-        mCompareMode = param;
-        break;
-
-    case LOCAL_GL_TEXTURE_COMPARE_FUNC:
-        mCompareFunc = param;
-        break;
-
-    default:
-        MOZ_CRASH("GFX: Unhandled pname");
-        break;
-    }
-
-    for (uint32_t i = 0; i < mContext->mBoundSamplers.Length(); ++i) {
-        if (this == mContext->mBoundSamplers[i])
-            mContext->InvalidateResolveCacheForTextureWithTexUnit(i);
-    }
-}
-
+////
 
 NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_0(WebGLSampler)
 NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(WebGLSampler, AddRef)
