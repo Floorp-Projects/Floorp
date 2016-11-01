998d263f00de0aa716b1f52a00e365d36cba154a
Kilik Kuo <kikuo@mozilla.com>
2016-11-01 19:27:18 +0800

    Bug 1306196-[P1] Provide AIDL definiion for EME functionality on Fennec MediaManager Service. r=cpearce,jchen
    MozReview-Commit-ID: IPr9WQQbGNB
    
    --HG--
    extra : rebase_source : 455b28413e9aa1dcf7ccca7c8934ba7d467982f9

--- mobile/android/base/java/org/mozilla/gecko/media/SessionKeyInfo.java
+++ mobile/android/base/java/org/mozilla/gecko/media/SessionKeyInfo.java
@@ -4,15 +4,48 @@
 
 package org.mozilla.gecko.media;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import org.mozilla.gecko.annotation.WrapForJNI;
 
-@WrapForJNI
-public final class SessionKeyInfo {
+public final class SessionKeyInfo implements Parcelable {
+    @WrapForJNI
     public byte[] keyId;
+
+    @WrapForJNI
     public int status;
 
+    @WrapForJNI
     public SessionKeyInfo(byte[] keyId, int status) {
         this.keyId = keyId;
         this.status = status;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int parcelableFlags) {
+        dest.writeByteArray(keyId);
+        dest.writeInt(status);
+    }
+
+    public static final Creator<SessionKeyInfo> CREATOR = new Creator<SessionKeyInfo>() {
+        @Override
+        public SessionKeyInfo createFromParcel(Parcel in) {
+            return new SessionKeyInfo(in);
+        }
+
+        @Override
+        public SessionKeyInfo[] newArray(int size) {
+            return new SessionKeyInfo[size];
+        }
+    };
+
+    private SessionKeyInfo(Parcel src) {
+        keyId = src.createByteArray();
+        status = src.readInt();
     }
 }
