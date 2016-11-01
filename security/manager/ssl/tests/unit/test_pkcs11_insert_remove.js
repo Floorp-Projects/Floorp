02380e20e2430d3f9979f8248b46f2d26f006e4c
Cykesiopka <cykesiopka.bmo@gmail.com>
2016-11-02 00:08:25 +0800

    Bug 1260626 - Add helper function in head_psm.js to load and unload the test PKCS11 module. r=keeler
    This helps cleanup some tests.
    
    MozReview-Commit-ID: 5xBBsIgKYR9

--- security/manager/ssl/tests/unit/test_pkcs11_insert_remove.js
+++ security/manager/ssl/tests/unit/test_pkcs11_insert_remove.js
@@ -72,14 +72,5 @@ function run_test() {
   Services.obs.addObserver(new SmartcardObserver("smartcard-remove"),
                            "smartcard-remove", false);
 
-  let pkcs11 = Cc["@mozilla.org/security/pkcs11;1"].getService(Ci.nsIPKCS11);
-  do_register_cleanup(function() {
-    pkcs11.deleteModule("PKCS11 Test Module");
-  });
-  let libraryName = ctypes.libraryName("pkcs11testmodule");
-  let libraryFile = Services.dirsvc.get("CurWorkD", Ci.nsILocalFile);
-  libraryFile.append("pkcs11testmodule");
-  libraryFile.append(libraryName);
-  ok(libraryFile.exists(), "The pkcs11testmodule file should exist");
-  pkcs11.addModule("PKCS11 Test Module", libraryFile.path, 0, 0);
+  loadPKCS11TestModule(false);
 }
