92f6fb26c43cbbf492c648469406bafe2a223aa8
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-11-02 15:34:44 +0100

    Backed out changeset e81bf7852eb2 (bug 1288308)

--- netwerk/base/ShutdownLayer.cpp
+++ netwerk/base/ShutdownLayer.cpp
@@ -15,15 +15,6 @@ static PRDescIdentity sWinSockShutdownLayerIdentity;
 static PRIOMethods sWinSockShutdownLayerMethods;
 static PRIOMethods *sWinSockShutdownLayerMethodsPtr = nullptr;
 
-namespace mozilla {
-namespace net {
-
-extern PRDescIdentity nsNamedPipeLayerIdentity;
-
-} // namespace net
-} // namespace mozilla
-
-
 PRStatus
 WinSockClose(PRFileDesc *aFd)
 {
@@ -52,12 +43,6 @@ nsresult mozilla::net::AttachShutdownLayer(PRFileDesc *aFd)
     sWinSockShutdownLayerMethods = *PR_GetDefaultIOMethods();
     sWinSockShutdownLayerMethods.close = WinSockClose;
     sWinSockShutdownLayerMethodsPtr = &sWinSockShutdownLayerMethods;
-  }
-
-  if (PR_GetIdentitiesLayer(aFd, mozilla::net::nsNamedPipeLayerIdentity)) {
-    // Do not attach shutdown layer on named pipe layer,
-    // it is for PR_NSPR_IO_LAYER only.
-    return NS_OK;
   }
 
   PRFileDesc * layer;
