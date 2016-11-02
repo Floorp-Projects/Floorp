8e1dba7d2e3b235a4ad6c28afbebec79a4e11480
Blake Kaplan <mrbkap@gmail.com>
2016-11-02 16:03:11 -0700

    Bug 1314812 - Remove code that manually times out loads in favor of relying on the test harness. r=Gijs
    MozReview-Commit-ID: FZB9ys9lvIN
    
    --HG--
    extra : rebase_source : 11a63b4b08fd8c64bbdbd4de2c4d968503aa807d

--- browser/base/content/test/urlbar/head.js
+++ browser/base/content/test/urlbar/head.js
@@ -207,7 +207,6 @@ function waitForDocLoadAndStopIt(aExpectedURL, aBrowser=gBrowser.selectedBrowser
  */
 function promiseTabLoadEvent(tab, url)
 {
-  let deferred = Promise.defer();
   info("Wait tab event: load");
 
   function handle(loadedUrl) {
@@ -224,22 +223,10 @@ function promiseTabLoadEvent(tab, url)
   // loads and one that is rejected if we take too long to load the url.
   let loaded = BrowserTestUtils.browserLoaded(tab.linkedBrowser, false, handle);
 
-  let timeout = setTimeout(() => {
-    deferred.reject(new Error("Timed out while waiting for a 'load' event"));
-  }, 30000);
-
-  loaded.then(() => {
-    clearTimeout(timeout);
-    deferred.resolve()
-  });
-
   if (url)
     BrowserTestUtils.loadURI(tab.linkedBrowser, url);
 
-  // Promise.all rejects if either promise rejects (i.e. if we time out) and
-  // if our loaded promise resolves before the timeout, then we resolve the
-  // timeout promise as well, causing the all promise to resolve.
-  return Promise.all([deferred.promise, loaded]);
+  return loaded;
 }
 
 function makeActionURI(action, params) {
