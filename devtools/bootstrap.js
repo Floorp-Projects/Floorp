a0b65af4c47607debd49484905dbdbaf5fafb974
Alexandre Poirot <poirot.alex@gmail.com>
2016-11-02 07:55:00 -0700

    Bug 1349509 - Reload devtools when installing the addon. r=jryans
    MozReview-Commit-ID: 4FXwRHvKWJo
    
    --HG--
    extra : rebase_source : d1849d81a4abb14f93b387218e8cc9a86bd3f151

--- devtools/bootstrap.js
+++ devtools/bootstrap.js
@@ -142,20 +142,22 @@ let getTopLevelWindow = function (window) {
 function reload(event) {
   // We automatically reload the toolbox if we are on a browser tab
   // with a toolbox already opened
-  let top = getTopLevelWindow(event.view);
-  let isBrowser = top.location.href.includes("/browser.xul");
   let reloadToolbox = false;
-  if (isBrowser && top.gBrowser) {
-    // We do not use any devtools code before the call to Loader.jsm reload as
-    // any attempt to use Loader.jsm to load a module will instanciate a new
-    // Loader.
-    let nbox = top.gBrowser.getNotificationBox();
-    reloadToolbox =
-      top.document.getAnonymousElementByAttribute(nbox, "class",
-        "devtools-toolbox-bottom-iframe") ||
-      top.document.getAnonymousElementByAttribute(nbox, "class",
-        "devtools-toolbox-side-iframe") ||
-      Services.wm.getMostRecentWindow("devtools:toolbox");
+  if (event) {
+    let top = getTopLevelWindow(event.view);
+    let isBrowser = top.location.href.includes("/browser.xul");
+    if (isBrowser && top.gBrowser) {
+      // We do not use any devtools code before the call to Loader.jsm reload as
+      // any attempt to use Loader.jsm to load a module will instanciate a new
+      // Loader.
+      let nbox = top.gBrowser.getNotificationBox();
+      reloadToolbox =
+        top.document.getAnonymousElementByAttribute(nbox, "class",
+          "devtools-toolbox-bottom-iframe") ||
+        top.document.getAnonymousElementByAttribute(nbox, "class",
+          "devtools-toolbox-side-iframe") ||
+        Services.wm.getMostRecentWindow("devtools:toolbox");
+    }
   }
   let browserConsole = Services.wm.getMostRecentWindow("devtools:webconsole");
   let reopenBrowserConsole = false;
@@ -248,6 +250,7 @@ function reload(event) {
     setTimeout(() => {
       let { TargetFactory } = devtools.require("devtools/client/framework/target");
       let { gDevTools } = devtools.require("devtools/client/framework/devtools");
+      let top = getTopLevelWindow(event.view);
       let target = TargetFactory.forTab(top.gBrowser.selectedTab);
       gDevTools.showToolbox(target);
     }, 1000);
@@ -286,6 +289,8 @@ function startup(data) {
       originalPrefValues[name] = userValue;
     }
   }
+
+  reload();
 }
 function shutdown() {
   listener.stop();
@@ -301,6 +306,11 @@ function shutdown() {
   }
 }
 function install() {
-  actionOccurred("reloadAddonInstalled");
+  try {
+    actionOccurred("reloadAddonInstalled");
+  } catch (e) {
+    // When installing on Firefox builds without devtools, telemetry doesn't
+    // work yet and throws.
+  }
 }
 function uninstall() {}
