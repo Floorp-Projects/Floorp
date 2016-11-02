f60b6d3d97f9a3f84019911935d53e9dee9d3523
Andrzej Hunt <ahunt@mozilla.com>
2016-11-02 21:13:19 +0100

    Bug 1312477 - Post: remove arbitrary peek and max heights from BottomSheet menu r=sebastian
    Setting the max height is completely arbitrary, and largely unnecessary - I think it's better
    to let the bottom automatically handle this until we know more about how the bottom sheet
    is perceived by real users.
    
    The peek height is similarly arbitrary - we'll probably want to discuss a good default height
    algorithm with UX, but setting an arbitrary hardcoded value seems wrong, especially since
    it doesn't behave well in landscape mode. BottomSheetDialog already does an acceptable
    job of finding a default peek height.
    
    MozReview-Commit-ID: LyAYzcytR6H
    
    --HG--
    extra : rebase_source : a40a6be42e57e14bb4ba1078323f666d32fe21ed

--- mobile/android/base/java/org/mozilla/gecko/home/activitystream/menu/ActivityStreamContextMenu.java
+++ mobile/android/base/java/org/mozilla/gecko/home/activitystream/menu/ActivityStreamContextMenu.java
@@ -68,7 +68,7 @@ public abstract class ActivityStreamContextMenu
         this.onUrlOpenListener = onUrlOpenListener;
         this.onUrlOpenInBackgroundListener = onUrlOpenInBackgroundListener;
     }
-    
+
     /**
      * Must be called before the menu is shown.
      * <p/>
