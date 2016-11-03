04cf55defa72bc0294293fd3cd4db74d5e50156c
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-11-03 22:41:18 +0100

    Bug 1314590 - Add Mozilla license header in devtools/client/shared/components/test/mochitest/ files. r=Honza;
    MozReview-Commit-ID: FayTCRXuAWN

--- devtools/client/shared/components/test/mochitest/head.js
+++ devtools/client/shared/components/test/mochitest/head.js
@@ -1,5 +1,6 @@
-/* Any copyright is dedicated to the Public Domain.
-   http://creativecommons.org/publicdomain/zero/1.0/ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /* eslint no-unused-vars: [2, {"vars": "local"}] */
 
 "use strict";
