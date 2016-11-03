b888d766cbb0bdcfd0336c7a7a0090f71ad894e8
JW Wang <jwwang@mozilla.com>
2016-11-03 15:15:13 +0800

    Bug 1314884. Part 3 - remove the call to |mPendingSeek.RejectIfExists(__func__)| from DormantState::HandleSeek() since it will be rejected in Exit() before transitioning to SEEKING. r=kaku
    MozReview-Commit-ID: A5HE03q87Ey
    
    --HG--
    extra : rebase_source : 6ef593f21d40f3ba6bef5e69d925d9d141548afe
    extra : source : eadfd6cc3f0b2370bd85bff0d7019d41f6006f2b

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -1281,7 +1281,6 @@ MediaDecoderStateMachine::
 DormantState::HandleSeek(SeekTarget aTarget)
 {
   // Exit dormant when the user wants to seek.
-  mPendingSeek.RejectIfExists(__func__);
   SeekJob seekJob;
   seekJob.mTarget = aTarget;
   return SetState<SeekingState>(Move(seekJob));
