69f4374119ab35cd11e56014b642dbe8dbffb4ba
Sébastien Blin <amarok@enconn.fr>
2016-11-03 05:50:00 +0000

    Bug 1189491 - Take anchors into account in the reader view, r=gijs
    MozReview-Commit-ID: 1udixhxnVao

--- toolkit/components/reader/test/browser_readerMode.js
+++ toolkit/components/reader/test/browser_readerMode.js
@@ -10,7 +10,7 @@ const TEST_PREFS = [
   ["reader.parse-on-load.enabled", true],
 ];
 
-const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
+const TEST_PATH = getRootDirectory(gTestPath).replace("chrome://mochitests/content", "http://example.com");
 
 var readerButton = document.getElementById("reader-mode-button");
 
