0e24a8fd224e75ced9f5da87dfce2bf7011ca2bd
Lin Clark <lclark@mozilla.com>
2016-11-03 14:56:00 -0400

    Bug 1312481 - Part 2: Make it possible to load React Virtualized in devtools (including XUL-based tools). r=Honza

--- devtools/client/shared/vendor/react-addons-shallow-compare.js
+++ devtools/client/shared/vendor/react-addons-shallow-compare.js
@@ -1,1 +1,9 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
 module.exports = require("devtools/client/shared/vendor/react").addons.shallowCompare;
