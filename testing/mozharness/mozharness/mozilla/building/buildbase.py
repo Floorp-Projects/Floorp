876aaefedb02980ba2bcf81de703e4be401cc6d0
Gregory Szorc <gps@mozilla.com>
2016-11-03 14:41:58 -0700

    Bug 1315041 - Record environment state in perfherder options; r=wlach
    Currently, perfherder data containing resource usage goes into the same
    bucket regardless of the execution environment. This means results for
    buildbot, taskcluster, and for different AWS instance types are all
    mixed together, making the data noisy.
    
    This commit adds extraOptions values to the perfherder data to
    identify:
    
    * buildbot vs taskcluster
    * the TC instance type
    
    Data will go to separate buckets and should be much more consistent.
    
    MozReview-Commit-ID: A0zSkdVI6ZM

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -50,7 +50,10 @@ from mozharness.mozilla.testing.errors import TinderBoxPrintRe
 from mozharness.mozilla.testing.unittest import tbox_print_summary
 from mozharness.mozilla.updates.balrog import BalrogMixin
 from mozharness.mozilla.taskcluster_helper import Taskcluster
-from mozharness.base.python import VirtualenvMixin
+from mozharness.base.python import (
+    PerfherderResourceOptionsMixin,
+    VirtualenvMixin,
+)
 
 AUTOMATION_EXIT_CODES = EXIT_STATUS_DICT.values()
 AUTOMATION_EXIT_CODES.sort()
@@ -587,7 +590,7 @@ def generate_build_UID():
 
 class BuildScript(BuildbotMixin, PurgeMixin, MockMixin, BalrogMixin,
                   SigningMixin, VirtualenvMixin, MercurialScript,
-                  SecretsMixin):
+                  SecretsMixin, PerfherderResourceOptionsMixin):
     def __init__(self, **kwargs):
         # objdir is referenced in _query_abs_dirs() so let's make sure we
         # have that attribute before calling BaseScript.__init__
@@ -1863,6 +1866,7 @@ or run without that action (ie: --no-{action})"
         data = {
             'name': 'build times',
             'value': resources['duration'],
+            'extraOptions': self.perfherder_resource_options(),
             'subtests': [],
         }
 
