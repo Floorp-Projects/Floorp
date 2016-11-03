ad7e6226423b96dc58c173ac16684709e049f5ea
Paul Yang <pyang@mozilla.com>
2016-11-03 10:55:31 -0400

    Bug 1261197 - Use StructuredOutputParser for parsing xpcshell test results, r=ahal
    MozReview-Commit-ID: EKFrxBtGYKp
    
    --HG--
    extra : rebase_source : d2605022fac3d3dd9e691f1729420158d6c06ee5

--- testing/mozharness/configs/unittests/mac_unittest.py
+++ testing/mozharness/configs/unittests/mac_unittest.py
@@ -255,7 +255,6 @@ config = {
         "https://blobupload.elasticbeanstalk.com",
     ],
     "unstructured_flavors": {"mochitest": ['jetpack'],
-                            "xpcshell": [],
                             "gtest": [],
                             "mozmill": [],
                             "cppunittest": [],
