308f139df4aa33bd23e668afd86f536b84fe2272
Kris Maglione <maglione.k@gmail.com>
2016-11-04 14:32:26 -0700

    Bug 1314361 - Part 5: Remove origin attribute comparison helpers for ignoring addonId. r=bholley

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -190,21 +190,10 @@ ChromeUtils::IsOriginAttributesEqual(const dom::OriginAttributesDictionary& aA,
 }
 
 /* static */ bool
-ChromeUtils::IsOriginAttributesEqualIgnoringAddonId(const dom::OriginAttributesDictionary& aA,
-                                                    const dom::OriginAttributesDictionary& aB)
-{
-  return aA.mAppId == aB.mAppId &&
-         aA.mInIsolatedMozBrowser == aB.mInIsolatedMozBrowser &&
-         aA.mUserContextId == aB.mUserContextId &&
-         aA.mPrivateBrowsingId == aB.mPrivateBrowsingId;
-}
-
-/* static */ bool
 ChromeUtils::IsOriginAttributesEqualIgnoringFPD(const dom::OriginAttributesDictionary& aA,
                                                 const dom::OriginAttributesDictionary& aB)
 {
-  return aA.mAddonId == aB.mAddonId &&
-         aA.mAppId == aB.mAppId &&
+  return aA.mAppId == aB.mAppId &&
          aA.mInIsolatedMozBrowser == aB.mInIsolatedMozBrowser &&
          aA.mUserContextId == aB.mUserContextId &&
          aA.mPrivateBrowsingId == aB.mPrivateBrowsingId;
