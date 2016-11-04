e9c537b742c7a1dda0e75707347db69d5ac3e563
Gregory Szorc <gps@mozilla.com>
2016-11-04 10:03:36 -0700

    Bug 1314918 - Revert browser eslint rule changes to unbust eslint task; r=Standard8
    This is a partial revert of 9601d282b06b.

--- browser/.eslintrc.js
+++ browser/.eslintrc.js
@@ -4,12 +4,4 @@ module.exports = {
   "extends": [
     "../toolkit/.eslintrc.js"
   ],
-
-  "rules": {
-    "no-unused-vars": ["error", {
-      "vars": "local",
-      "varsIgnorePattern": "^Cc|Ci|Cu|Cr|EXPORTED_SYMBOLS",
-      "args": "none",
-    }]
-  }
 };
