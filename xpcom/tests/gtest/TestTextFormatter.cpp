f858baa31dea27634b13371840301a52ea32044e
Eric Rahm <erahm@mozilla.com>
2016-11-04 16:24:42 -0700

    Bug 1313482 - Convert XPCOM test TestTextFormatter to a gtest. r=froydnj
    MozReview-Commit-ID: FQ5K3jvqb7c
    
    --HG--
    rename : xpcom/tests/TestTextFormatter.cpp => xpcom/tests/gtest/TestTextFormatter.cpp

--- xpcom/tests/gtest/TestTextFormatter.cpp
+++ xpcom/tests/gtest/TestTextFormatter.cpp
@@ -3,38 +3,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <nsTextFormatter.h>
-#include <nsStringGlue.h>
-#include <stdio.h>
+#include "nsTextFormatter.h"
+#include "nsString.h"
+#include "gtest/gtest.h"
 
-int main() 
-{  
-  int test_ok = true;
-
+TEST(TextFormatter, Tests)
+{
   nsAutoString fmt(NS_LITERAL_STRING("%3$s %4$S %1$d %2$d %2$d %3$s"));
-  char utf8[] = "Hello"; 
-  char16_t ucs2[]={'W', 'o', 'r', 'l', 'd', 0x4e00, 0xAc00, 0xFF45, 0x0103, 0x00}; 
-  int d=3; 
+  char utf8[] = "Hello";
+  char16_t ucs2[]={'W', 'o', 'r', 'l', 'd', 0x4e00, 0xAc00, 0xFF45, 0x0103, 0x00};
+  int d=3;
 
-  char16_t buf[256]; 
-  nsTextFormatter::snprintf(buf, 256, fmt.get(), d, 333, utf8, ucs2); 
-  nsAutoString out(buf); 
-  if(strcmp("Hello World", NS_LossyConvertUTF16toASCII(out).get()))
-    test_ok = false;
+  char16_t buf[256];
+  nsTextFormatter::snprintf(buf, 256, fmt.get(), d, 333, utf8, ucs2);
+  nsAutoString out(buf);
+  ASSERT_STREQ("Hello World", NS_LossyConvertUTF16toASCII(out).get());
 
-  const char16_t *uout = out.get(); 
+  const char16_t *uout = out.get();
   const char16_t expected[] = {0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20,
                                 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x4E00,
                                 0xAC00, 0xFF45, 0x0103, 0x20, 0x33,
                                 0x20, 0x33, 0x33, 0x33, 0x20, 0x33,
                                 0x33, 0x33, 0x20, 0x48, 0x65, 0x6C,
                                 0x6C, 0x6F};
-  for(uint32_t i=0;i<out.Length();i++) {
-    if(uout[i] != expected[i]) {
-      test_ok = false;
-      break;
-    }
+
+  for (uint32_t i=0; i<out.Length(); i++) {
+    ASSERT_EQ(uout[i], expected[i]);
   }
-  printf(test_ok? "nsTextFormatter: OK\n": "nsTextFormatter: FAIL\n");
 }
 
