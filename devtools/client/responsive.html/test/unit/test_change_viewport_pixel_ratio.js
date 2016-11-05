7d1ff48d66f7f537ed52a31d2ee92a038bf41476
Matteo Ferretti <mferretti@mozilla.com>
2016-11-05 13:45:10 +0100

    Bug 1276971 - Adding UI to display and change the current DPI / DPR setting; r=gl,jryans
    MozReview-Commit-ID: Dnb6OLhOvOK

---
+++ devtools/client/responsive.html/test/unit/test_change_viewport_pixel_ratio.js
@@ -0,0 +1,22 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test changing the viewport pixel ratio.
+
+const { addViewport, changeViewportPixelRatio } =
+  require("devtools/client/responsive.html/actions/viewports");
+const NEW_PIXEL_RATIO = 5.5;
+
+add_task(function* () {
+  let store = Store();
+  const { getState, dispatch } = store;
+
+  dispatch(addViewport());
+  dispatch(changeViewportPixelRatio(0, NEW_PIXEL_RATIO));
+
+  let viewport = getState().viewports[0];
+  equal(viewport.pixelRatio, NEW_PIXEL_RATIO,
+    `Viewport's pixel ratio changed to ${NEW_PIXEL_RATIO}`);
+});
