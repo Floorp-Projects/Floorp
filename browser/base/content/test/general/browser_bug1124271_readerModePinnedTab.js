3b219cba1e564f94b85bad1d1e85a6807ddb20b3
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-11-05 13:54:35 +0100

    Backed out changeset 1cb8dac3eb40 (bug 1189491) for failing browser_readerMode.js. r=backout

--- browser/base/content/test/general/browser_bug1124271_readerModePinnedTab.js
+++ browser/base/content/test/general/browser_bug1124271_readerModePinnedTab.js
@@ -6,7 +6,7 @@
 
 const PREF = "reader.parse-on-load.enabled";
 
-const TEST_PATH = getRootDirectory(gTestPath).replace("chrome://mochitests/content", "http://example.com");
+const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
 
 var readerButton = document.getElementById("reader-mode-button");
 
@@ -37,9 +37,9 @@ add_task(function* () {
   // Ensure no new tabs are opened when exiting reader mode in a pinned tab
   is(gBrowser.tabs.length, initialTabsCount, "No additional tabs were opened.");
 
-  let pageShownPromise = BrowserTestUtils.waitForContentEvent(tab.linkedBrowser, "pageshow");
   readerButton.click();
-  yield pageShownPromise;
+  yield BrowserTestUtils.waitForContentEvent(tab.linkedBrowser, "pageshow");
+
   // Ensure no new tabs are opened when exiting reader mode in a pinned tab
   is(gBrowser.tabs.length, initialTabsCount, "No additional tabs were opened.");
 
--- browser/base/content/test/general/browser_readerMode_hidden_nodes.js
+++ browser/base/content/test/general/browser_readerMode_hidden_nodes.js
@@ -11,7 +11,7 @@ const TEST_PREFS = [
   ["browser.reader.detectedFirstArticle", false],
 ];
 
-const TEST_PATH = getRootDirectory(gTestPath).replace("chrome://mochitests/content", "http://example.com");
+const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
 
 var readerButton = document.getElementById("reader-mode-button");
 
