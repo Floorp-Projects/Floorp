2e136118e4347b14e4cd71ce5a39ecdcf2a31f06
Mats Palmgren <mats@mozilla.com>
2016-11-05 02:57:06 +0100

    Bug 1300369 part 5 - Make nsFrame::ComputeSize and nsFrame::ShrinkWidthToFit handle margin-box min-size clamping.  r=dholbert

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -4743,6 +4743,20 @@ nsFrame::ComputeSize(nsRenderingContext* aRenderingContext,
     minISize = std::min(maxISize, GetMinISize(aRenderingContext));
     if (inlineStyleCoord->IsCoordPercentCalcUnit()) {
       minISize = std::min(minISize, result.ISize(aWM));
+    } else if (aFlags & eIClampMarginBoxMinSize) {
+      auto cbSize = aCBSize.ISize(aWM);
+      if (cbSize != NS_UNCONSTRAINEDSIZE) {
+        // "if the grid item spans only grid tracks that have a fixed max track
+        // sizing function, its automatic minimum size in that dimension is
+        // further clamped to less than or equal to the size necessary to fit
+        // its margin box within the resulting grid area (flooring at zero)"
+        // https://drafts.csswg.org/css-grid/#min-size-auto
+        auto maxMinISize = std::max(nscoord(0), cbSize -
+                                                aPadding.ISize(aWM) -
+                                                aBorder.ISize(aWM) -
+                                                aMargin.ISize(aWM));
+        minISize = std::min(minISize, maxMinISize);
+      }
     }
   } else {
     // Treat "min-width: auto" as 0.
@@ -4914,7 +4928,8 @@ nsFrame::ShrinkWidthToFit(nsRenderingContext* aRenderingContext,
   nscoord result;
   nscoord minISize = GetMinISize(aRenderingContext);
   if (minISize > aISizeInCB) {
-    result = minISize;
+    const bool clamp = aFlags & ComputeSizeFlags::eIClampMarginBoxMinSize;
+    result = MOZ_UNLIKELY(clamp) ? aISizeInCB : minISize;
   } else {
     nscoord prefISize = GetPrefISize(aRenderingContext);
     if (prefISize > aISizeInCB) {
