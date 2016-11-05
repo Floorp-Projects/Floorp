84598ab5f9e091051739a3014c74225559f03b5d
Phil Ringnalda <philringnalda@gmail.com>
2016-11-04 20:32:15 -0700

    bug 1313099 followup, add missing comma

--- testing/mozharness/configs/unittests/win_taskcluster_unittest.py
+++ testing/mozharness/configs/unittests/win_taskcluster_unittest.py
@@ -278,7 +278,7 @@ config = {
     'blob_uploader_auth_file': 'C:/builds/oauth.txt',
     "download_minidump_stackwalk": True,
     "minidump_stackwalk_path": "win32-minidump_stackwalk.exe",
-    "minidump_tooltool_manifest_path": "config/tooltool-manifests/win32/releng.manifest"
+    "minidump_tooltool_manifest_path": "config/tooltool-manifests/win32/releng.manifest",
     "download_nodejs": True,
     "nodejs_path": "node-win32.exe",
     "nodejs_tooltool_manifest_path": "config/tooltool-manifests/win32/nodejs.manifest",
