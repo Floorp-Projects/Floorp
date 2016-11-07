63c79ffa973f1c8c7d8b6a198b17f733507d4384
Phil Ringnalda <philringnalda@gmail.com>
2016-11-06 22:34:23 -0800

    Backed out changeset 63be72bf8bbd (bug 1315470) for failures in datachannel-emptystring.html

--- dom/base/nsDOMDataChannel.cpp
+++ dom/base/nsDOMDataChannel.cpp
@@ -357,7 +357,7 @@ nsDOMDataChannel::Send(nsIInputStream* aMsgStream,
   MOZ_ASSERT(state == mozilla::DataChannel::OPEN,
              "Unknown state in nsDOMDataChannel::Send");
 
-  bool sent;
+  int32_t sent;
   if (aMsgStream) {
     sent = mDataChannel->SendBinaryStream(aMsgStream, aMsgLength);
   } else {
@@ -367,7 +367,7 @@ nsDOMDataChannel::Send(nsIInputStream* aMsgStream,
       sent = mDataChannel->SendMsg(aMsgString);
     }
   }
-  if (!sent) {
+  if (sent < 0) {
     aRv.Throw(NS_ERROR_FAILURE);
   }
 }
