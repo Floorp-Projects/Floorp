3b14786056e9f2d55968221d3899254b16828e95
Jeff Walden <jwalden@mit.edu>
2016-11-07 15:23:17 -0800

    Bug 1296814 - Split Parser::report into Parser::zeport (a temporary name) that uses the current offset, and Parser::reportWithNode that derives it from a Node.  r=arai
    --HG--
    extra : rebase_source : 4943a81cf142960826ca07cadbac7797bbce3612

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -907,7 +907,8 @@ class Parser final : private JS::AutoGCRooter, public StrictModeGetter
     bool reportHelper(ParseReportKind kind, bool strict, uint32_t offset,
                       unsigned errorNumber, va_list args);
   public:
-    bool report(ParseReportKind kind, bool strict, Node pn, unsigned errorNumber, ...);
+    bool zeport(ParseReportKind kind, bool strict, unsigned errorNumber, ...);
+    bool reportWithNode(ParseReportKind kind, bool strict, Node pn, unsigned errorNumber, ...);
     bool reportNoOffset(ParseReportKind kind, bool strict, unsigned errorNumber, ...);
     bool reportWithOffset(ParseReportKind kind, bool strict, uint32_t offset, unsigned errorNumber,
                           ...);
