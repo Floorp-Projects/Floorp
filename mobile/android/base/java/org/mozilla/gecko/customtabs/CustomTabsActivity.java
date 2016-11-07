5c4fccfa289ebff81381a08a40062ed7e9a32a7c
Dylan Roeh <droeh@mozilla.com>
2016-11-07 09:21:23 -0600

    Bug 1279278 - Use domain name as a Toolbar title and behave correctly when Toolbar navigation is used. r=sebastian

--- mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
+++ mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
@@ -5,15 +5,20 @@
 
 package org.mozilla.gecko.customtabs;
 
-import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.R;
@@ -24,18 +29,54 @@ import org.mozilla.gecko.util.GeckoRequest;
 import org.mozilla.gecko.util.NativeJSObject;
 import org.mozilla.gecko.util.ThreadUtils;
 
+import java.lang.reflect.Field;
+
 import static android.support.customtabs.CustomTabsIntent.EXTRA_TOOLBAR_COLOR;
 
-public class CustomTabsActivity extends GeckoApp {
+public class CustomTabsActivity extends GeckoApp implements Tabs.OnTabsChangedListener {
+    private static final String LOGTAG = "CustomTabsActivity";
     private static final int NO_COLOR = -1;
     private Toolbar toolbar;
+
+    private ActionBar actionBar;
+    private int tabId = -1;
+    private boolean useDomainTitle = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         updateActionBarWithToolbar(toolbar);
-        updateToolbarColor();
+        try {
+            // Since we don't create the Toolbar's TextView ourselves, this seems
+            // to be the only way of changing the ellipsize setting.
+            Field f = toolbar.getClass().getDeclaredField("mTitleTextView");
+            f.setAccessible(true);
+            TextView textView = (TextView) f.get(toolbar);
+            textView.setEllipsize(TextUtils.TruncateAt.START);
+        } catch (Exception e) {
+            // If we can't ellipsize at the start of the title, we shouldn't display the host
+            // so as to avoid displaying a misleadingly truncated host.
+            Log.w(LOGTAG, "Failed to get Toolbar TextView, using default title.");
+            useDomainTitle = false;
+        }
+        actionBar = getSupportActionBar();
+        updateToolbarColor(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Tabs.registerOnTabsChangedListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Tabs.unregisterOnTabsChangedListener(this);
     }
 
     @Override
@@ -46,6 +87,31 @@ public class CustomTabsActivity extends GeckoApp {
     @Override
     protected void onDone() {
         finish();
+    }
+
+    @Override
+    public void onTabChanged(Tab tab, Tabs.TabEvents msg, String data) {
+        if (tab == null) {
+            return;
+        }
+
+        if (tabId >= 0 && tab.getId() != tabId) {
+            return;
+        }
+
+        if (msg == Tabs.TabEvents.LOCATION_CHANGE) {
+            tabId = tab.getId();
+            final Uri uri = Uri.parse(tab.getURL());
+            String title = null;
+            if (uri != null) {
+                title = uri.getHost();
+            }
+            if (!useDomainTitle || title == null || title.isEmpty()) {
+                actionBar.setTitle(AppConstants.MOZ_APP_BASENAME);
+            } else {
+                actionBar.setTitle(title);
+            }
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -65,7 +131,7 @@ public class CustomTabsActivity extends GeckoApp {
         }
     }
 
-    private void updateToolbarColor() {
+    private void updateToolbarColor(final Toolbar toolbar) {
         final int color = getIntent().getIntExtra(EXTRA_TOOLBAR_COLOR, NO_COLOR);
         if (color == NO_COLOR) {
             return;
