b1e6ed7c5def9115fad51a068b899cbde877b03b
Christian Holler (:decoder) <choller@mozilla.com>
2016-11-07 15:58:20 +0100

    Bug 1302451 - Import LibFuzzer code. r=froydnj
    MozReview-Commit-ID: 3NRSquBHyr2

--- tools/fuzzing/libfuzzer/clone_libfuzzer.sh
+++ tools/fuzzing/libfuzzer/clone_libfuzzer.sh
@@ -2,6 +2,10 @@
 
 mkdir tmp/
 git clone --no-checkout --depth 1 https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer tmp/
-mv tmp/.git .
-rm -Rf tmp
-git reset --hard HEAD
+(cd tmp && git reset --hard 61f251f8f4eef0ec6fae92bd0d7b63e76ed75b7f)
+
+# Copy only source code and includes
+cp tmp/*.cpp tmp/*.h tmp/*.def .
+
+# Remove the temporary directory
+rm -Rf tmp/
