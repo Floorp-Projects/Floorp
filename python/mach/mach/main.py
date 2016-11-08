cc764ee794b7a9624ef40da3be6bdb25c1ecefbe
Gregory Szorc <gps@mozilla.com>
2016-11-08 12:15:13 -0800

    Bug 1315785 - Set an environment variable when mach is attached to a TTY; r=glandium
    The way it works now, `mach` commands often invoke subprocesses where
    the subprocesses' stdio file descriptors are pipes so the mach command
    can e.g. parse output.
    
    Processes like clang, gcc, and cargo determine if they can send color
    codes to {stderr, stdout} by seeing if those file descriptors are TTYs.
    When e.g. `make` is executed via `mach`, this test fails because those
    descriptors are pipes (even though they eventually end up on a TTY).
    
    We can't wire the file descriptors to the TTY because `mach` needs
    to analyze output. We don't want users defining process flags to force
    color in their mozconfigs because color codes would still be sent
    if stdout was not a TTY.
    
    This patch sets the MACH_STDOUT_ISATTY environment variable in all mach
    commands when stdout is a TTY. Subsequent processes can then look for
    this variable to determine whether to override color settings, print
    terminal control codes, etc.
    
    MozReview-Commit-ID: GxXP2mQssjC
    
    --HG--
    extra : rebase_source : 4b99547b453cb7dd5cb590a71ed554ce2bc4759d

--- python/mach/mach/main.py
+++ python/mach/mach/main.py
@@ -342,6 +342,13 @@ To see more help for a specific command, run:
             if stderr.encoding is None:
                 sys.stderr = codecs.getwriter('utf-8')(stderr)
 
+            # Allow invoked processes (which may not have a handle on the
+            # original stdout file descriptor) to know if the original stdout
+            # is a TTY. This provides a mechanism to allow said processes to
+            # enable emitting code codes, for example.
+            if os.isatty(orig_stdout.fileno()):
+                os.environ[b'MACH_STDOUT_ISATTY'] = b'1'
+
             return self._run(argv)
         except KeyboardInterrupt:
             print('mach interrupted by signal or user action. Stopping.')
