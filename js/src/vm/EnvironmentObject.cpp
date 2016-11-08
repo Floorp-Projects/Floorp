2c0854ccc214d734395432c4c4c8409663479cdc
Shu-yu Guo <shu@rfrn.org>
2016-11-08 15:46:05 -0800

    Bug 1308578 - Consider .this bindings of derived class constructors to be always observable. (r=jandem)

--- js/src/vm/EnvironmentObject.cpp
+++ js/src/vm/EnvironmentObject.cpp
@@ -3133,7 +3133,7 @@ js::GetThisValueForDebuggerMaybeOptimizedOut(JSContext* cx, AbstractFramePtr fra
             }
 
             if (loc.kind() == BindingLocation::Kind::Frame && ei.withinInitialFrame())
-                res.set(frame.unaliasedLocal(bi.location().slot()));
+                res.set(frame.unaliasedLocal(loc.slot()));
             else
                 res.setMagic(JS_OPTIMIZED_OUT);
 
