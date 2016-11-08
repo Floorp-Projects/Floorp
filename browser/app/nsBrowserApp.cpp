c8328263a0e28a84f61d3c01ca36f80d934179ee
Jed Davis <jld@mozilla.com>
2016-11-08 15:06:36 -0700

    Bug 1277968 - Use `firefox -contentproc` on sandbox-enabled Linux. r=ted
    MozReview-Commit-ID: 7izy7UvJpwS

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -89,8 +89,7 @@ SSE2Check()
 }
 #endif
 
-#if !defined(MOZ_WIDGET_COCOA) && !defined(MOZ_WIDGET_ANDROID) \
-  && !(defined(XP_LINUX) && defined(MOZ_SANDBOX))
+#if !defined(MOZ_WIDGET_COCOA) && !defined(MOZ_WIDGET_ANDROID)
 #define MOZ_BROWSER_CAN_BE_CONTENTPROC
 #include "../../ipc/contentproc/plugin-container.cpp"
 #endif
