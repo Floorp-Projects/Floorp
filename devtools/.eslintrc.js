30a940a9902b37739bce5135f0bd06a29fb49ff8
Julian Descottes <jdescottes@mozilla.com>
2016-11-08 11:33:48 +0100

    Bug 1315922 - enable react/prop-types eslint rule for devtools;r=jryans
    MozReview-Commit-ID: KT0v8CQ6DON

--- devtools/.eslintrc.js
+++ devtools/.eslintrc.js
@@ -56,8 +56,7 @@ module.exports = {
     "react/no-direct-mutation-state": "error",
     "react/no-unknown-property": "error",
     "react/prefer-es6-class": ["warn", "never"],
-    // Disabled temporarily until errors are fixed.
-    "react/prop-types": "off",
+    "react/prop-types": "error",
     "react/sort-comp": ["error", {
       order: [
         "lifecycle",
