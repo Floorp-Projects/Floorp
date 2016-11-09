835a5f43580bceed91546798be5deaacafdd5fd9
Benjamin Bouvier <benj@benj.me>
2016-11-09 17:09:16 +0100

    Bug 1316156: Use a constant-or-register for int64 pass-stack-arg; r=luke
    MozReview-Commit-ID: 2ce0srABGpt

---
+++ js/src/jit-test/tests/wasm/regress/pass-stack-int64.js
@@ -0,0 +1,17 @@
+load(libdir + "wasm.js");
+
+var params = '';
+var locals = '';
+for (let i = 0; i < 20; i++) {
+    params += '(param i64) ';
+    locals += `(get_local ${i}) `;
+}
+
+wasmEvalText(`
+(module
+    (func
+        ${params}
+        (call 0 ${locals})
+    )
+)
+`);
