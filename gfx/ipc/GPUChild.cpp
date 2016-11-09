c94aec102b593aa16858dc71235cb3bdeb51277d
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-11-09 10:26:27 +0100

    Backed out changeset 364305744e4c (bug 1297790) for test bustage

--- gfx/ipc/GPUChild.cpp
+++ gfx/ipc/GPUChild.cpp
@@ -171,12 +171,6 @@ GPUChild::ActorDestroy(ActorDestroyReason aWhy)
 #endif
     Telemetry::Accumulate(Telemetry::SUBPROCESS_ABNORMAL_ABORT,
         nsDependentCString(XRE_ChildProcessTypeToString(GeckoProcessType_GPU), 1));
-
-    // Notify the Telemetry environment so that we can refresh and do a subsession split
-    if (nsCOMPtr<nsIObserverService> obsvc = services::GetObserverService()) {
-      obsvc->NotifyObservers(nullptr, "compositor:process-aborted", nullptr);
-    }
-
   }
 
   gfxVars::RemoveReceiver(this);
