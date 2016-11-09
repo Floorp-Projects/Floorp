866edf0c83ba882c1a97933e3bc4eb44a9faa05b
Benjamin Bouvier <benj@benj.me>
2016-11-09 17:31:54 +0100

    Bug 1304672: Undo the renaming of debuggerObservesAsmJS => Wasm; r=luke
    MozReview-Commit-ID: 1vxk30n72Zf

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -2782,10 +2782,10 @@ Debugger::updateObservesAsmJSOnDebuggees(IsObserving observing)
         GlobalObject* global = r.front();
         JSCompartment* comp = global->compartment();
 
-        if (comp->debuggerObservesWasm() == observing)
+        if (comp->debuggerObservesAsmJS() == observing)
             continue;
 
-        comp->updateDebuggerObservesWasm();
+        comp->updateDebuggerObservesAsmJS();
     }
 }
 
@@ -3478,7 +3478,7 @@ Debugger::setAllowUnobservedAsmJS(JSContext* cx, unsigned argc, Value* vp)
     for (WeakGlobalObjectSet::Range r = dbg->debuggees.all(); !r.empty(); r.popFront()) {
         GlobalObject* global = r.front();
         JSCompartment* comp = global->compartment();
-        comp->updateDebuggerObservesWasm();
+        comp->updateDebuggerObservesAsmJS();
     }
 
     args.rval().setUndefined();
@@ -3930,7 +3930,7 @@ Debugger::addDebuggeeGlobal(JSContext* cx, Handle<GlobalObject*> global)
     // (6)
     AutoRestoreCompartmentDebugMode debugModeGuard(debuggeeCompartment);
     debuggeeCompartment->setIsDebuggee();
-    debuggeeCompartment->updateDebuggerObservesWasm();
+    debuggeeCompartment->updateDebuggerObservesAsmJS();
     debuggeeCompartment->updateDebuggerObservesCoverage();
     if (observesAllExecution() && !ensureExecutionObservabilityOfCompartment(cx, debuggeeCompartment))
         return false;
@@ -4046,7 +4046,7 @@ Debugger::removeDebuggeeGlobal(FreeOp* fop, GlobalObject* global,
         global->compartment()->unsetIsDebuggee();
     } else {
         global->compartment()->updateDebuggerObservesAllExecution();
-        global->compartment()->updateDebuggerObservesWasm();
+        global->compartment()->updateDebuggerObservesAsmJS();
         global->compartment()->updateDebuggerObservesCoverage();
     }
 }
