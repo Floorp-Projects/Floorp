5149ff18e0db7b820df449688d57bda0a17b1a18
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-11-09 12:19:06 +1100

    Bug 1313632: P2. Cleaner Ogg related headers. r=kaku
    MozReview-Commit-ID: DpnezpZw27H

--- dom/media/platforms/agnostic/OpusDecoder.cpp
+++ dom/media/platforms/agnostic/OpusDecoder.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "OpusDecoder.h"
+#include "OpusParser.h"
 #include "TimeUnits.h"
 #include "VorbisUtils.h"
 #include "VorbisDecoder.h" // For VorbisLayout
@@ -12,8 +13,12 @@
 #include "mozilla/PodOperations.h"
 #include "mozilla/SyncRunnable.h"
 
-#include <stdint.h>
 #include <inttypes.h>  // For PRId64
+
+#include "opus/opus.h"
+extern "C" {
+#include "opus/opus_multistream.h"
+}
 
 #define OPUS_DEBUG(arg, ...) MOZ_LOG(sPDMLog, mozilla::LogLevel::Debug, \
     ("OpusDataDecoder(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
