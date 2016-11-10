6b1bb74d9500444571656b9b2712e98a52ae0f2f
peter chang <pchang@mozilla.com>
2016-11-10 11:41:09 +0800

    Bug 1316486 - BindRenderbuffer should return invalid operation with a deleted render buffer input, r=mtseng
    MozReview-Commit-ID: Gu976wWy352

--- dom/canvas/WebGLContextGL.cpp
+++ dom/canvas/WebGLContextGL.cpp
@@ -170,9 +170,9 @@ WebGLContext::BindRenderbuffer(GLenum target, WebGLRenderbuffer* wrb)
     if (!ValidateObjectAllowDeletedOrNull("bindRenderbuffer", wrb))
         return;
 
-    // silently ignore a deleted buffer
-    if (wrb && wrb->IsDeleted())
-        return;
+    if (wrb && wrb->IsDeleted()) {
+        return ErrorInvalidOperation("BindRenderbuffer: bind a deleted buffer");
+    }
 
     // Usually, we would now call into glBindRenderbuffer. However, since we have to
     // potentially emulate packed-depth-stencil, there's not a specific renderbuffer that
