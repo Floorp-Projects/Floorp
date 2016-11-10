340889922bedea2a0dc0734bdd793a58c2c33e51
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-11-10 13:44:31 +0100

    Bug 1315634 - Discard fallback resume points created on JSOP_CALLPROP if we are not inlining any function. r=h4writer

---
+++ js/src/jit-test/tests/backup-point-bug1315634.js
@@ -0,0 +1,29 @@
+setJitCompilerOption('ion.forceinlineCaches', 1);
+function g(f, x) {
+    for (var j = 0; j < 3; ++j)
+        for (var k = 0; k < 21; ++k)
+            try {
+                f(x[k]);
+            } catch (e) {}
+}
+a0 = y = [];
+function f2() {
+    f1()
+};
+function f1() {
+    switch (abs(abs(3187503207)(134217728) | 0) | 0) {
+        case -2:
+            this.y.splice(NaN, 2, x({}) = 4277)
+    }
+    return
+    4006901336 | 0
+}
+g(f1, []);
+g(f2, []);
+f1 = (function() {
+    function f() {
+        a0.splice(NaN, 0);
+    }
+    return f;
+})();
+g(f2, []);
