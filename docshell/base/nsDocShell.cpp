6b7f32e003196ada6ec6f04935cc0a3703e5b2c2
Markus Stange <mstange@themasta.com>
2016-11-10 14:26:54 -0500

    Bug 1307242 - Add a non-blank paint telemetry probe for foreground root content documents. r=bkelly, f=bsmedberg
    This adds a probe called TIME_TO_NON_BLANK_PAINT_MS which reports time to
    non-blank paint for root content documents whose docshell was active for the
    entire time between navigation start and first non-blank paint.
    
    MozReview-Commit-ID: 7JQdk5vHr1O

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -1728,7 +1728,9 @@ nsDocShell::MaybeInitTiming()
     mTiming = new nsDOMNavigationTiming();
   }
 
-  mTiming->NotifyNavigationStart();
+  mTiming->NotifyNavigationStart(
+    mIsActive ? nsDOMNavigationTiming::DocShellState::eActive
+              : nsDOMNavigationTiming::DocShellState::eInactive);
 }
 
 //
@@ -6217,6 +6219,20 @@ nsDocShell::SetIsActive(bool aIsActive)
 
       doc->PostVisibilityUpdateEvent();
     }
+  }
+
+  // Tell the nsDOMNavigationTiming about it
+  RefPtr<nsDOMNavigationTiming> timing = mTiming;
+  if (!timing && mContentViewer) {
+    nsIDocument* doc = mContentViewer->GetDocument();
+    if (doc) {
+      timing = doc->GetNavigationTiming();
+    }
+  }
+  if (timing) {
+    timing->NotifyDocShellStateChanged(
+      aIsActive ? nsDOMNavigationTiming::DocShellState::eActive
+                : nsDOMNavigationTiming::DocShellState::eInactive);
   }
 
   // Recursively tell all of our children, but don't tell <iframe mozbrowser>
