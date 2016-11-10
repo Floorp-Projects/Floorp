914875a1aa5f1223f26ce17c8539d9ec479803b2
Petr Sumbera <petr.sumbera@oracle.com>
2016-11-09 18:08:05 -0800

    Bug 1315686 - Rename DS to avoid macro conflict on Solaris. - r=jgilbert
    MozReview-Commit-ID: 7rLiD8Ux9VN

--- dom/canvas/WebGLFramebuffer.cpp
+++ dom/canvas/WebGLFramebuffer.cpp
@@ -537,7 +537,7 @@ WebGLFBAttachPoint::GetParameter(const char* funcName, WebGLContext* webgl, JSCo
 
         if (format->componentType == webgl::ComponentType::Special) {
             // Special format is used for DS mixed format(e.g. D24S8 and D32FS8).
-            MOZ_ASSERT(format->unsizedFormat == webgl::UnsizedFormat::DS);
+            MOZ_ASSERT(format->unsizedFormat == webgl::UnsizedFormat::DEPTH_STENCIL);
             MOZ_ASSERT(attachment == LOCAL_GL_DEPTH_ATTACHMENT ||
                        attachment == LOCAL_GL_STENCIL_ATTACHMENT);
 
