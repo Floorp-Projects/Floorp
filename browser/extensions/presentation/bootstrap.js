7bc597d3c0ead4ae2a7568339e7a0ec0c901216e
Chun-Min Chang <chun.m.chang@gmail.com>
2016-11-10 15:33:09 +0800

    Bug 1289974 part 1: Device selection for presentation API on Firefox; r=mconley
    MozReview-Commit-ID: 8z8xM4hr2F3
    
    --HG--
    extra : rebase_source : 35c2b090b3d8711df3ee9a1edbf3fe3a7535199a

---
+++ browser/extensions/presentation/bootstrap.js
@@ -0,0 +1,84 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const {classes: Cc, interfaces: Ci, utils: Cu, manager: Cm} = Components;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+const PRESENTATION_DEVICE_PROMPT_PATH =
+  "chrome://presentation/content/PresentationDevicePrompt.jsm";
+
+function log(aMsg) {
+  // dump("@ Presentation: " + aMsg + "\n");
+}
+
+function install(aData, aReason) {
+}
+
+function uninstall(aData, aReason) {
+}
+
+function startup(aData, aReason) {
+  log("startup");
+  Presentation.init();
+}
+
+function shutdown(aData, aReason) {
+  log("shutdown");
+  Presentation.uninit();
+}
+
+// Register/unregister a constructor as a factory.
+function Factory() {}
+Factory.prototype = {
+  register: function(targetConstructor) {
+    let proto = targetConstructor.prototype;
+    this._classID = proto.classID;
+
+    let factory = XPCOMUtils._getFactory(targetConstructor);
+    this._factory = factory;
+
+    let registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.registerFactory(proto.classID, proto.classDescription,
+                              proto.contractID, factory);
+  },
+
+  unregister: function() {
+    let registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.unregisterFactory(this._classID, this._factory);
+    this._factory = null;
+    this._classID = null;
+  },
+};
+
+var Presentation = {
+  // PUBLIC APIs
+  init: function() {
+    log("init");
+    // Register PresentationDevicePrompt into a XPCOM component.
+    Cu.import(PRESENTATION_DEVICE_PROMPT_PATH);
+    this._register();
+  },
+
+  uninit: function() {
+    log("uninit");
+    // Unregister PresentationDevicePrompt XPCOM component.
+    this._unregister();
+    Cu.unload(PRESENTATION_DEVICE_PROMPT_PATH);
+  },
+
+  // PRIVATE APIs
+  _register: function() {
+    log("_register");
+    this._devicePromptFactory = new Factory();
+    this._devicePromptFactory.register(PresentationDevicePrompt);
+  },
+
+  _unregister: function() {
+    log("_unregister");
+    this._devicePromptFactory.unregister();
+    delete this._devicePromptFactory;
+  },
+};
