401eb29d329c1448b7c0f6b07006abb1dac1a2b2
Markus Stange <mstange@themasta.com>
2016-11-09 22:46:26 -0500

    Bug 1307242 - Call nsPresContext::NotifyNonBlankPaint when building the display list for a presshell that contains more than just canvas backgrounds. r=mattwoodrow, a=lizzard
    MozReview-Commit-ID: LLmt3ooMxL3

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -4899,7 +4899,7 @@ PresShell::CreateRangePaintInfo(nsIDOMRange* aRange,
 
   nsRect rangeRect = ClipListToRange(&info->mBuilder, &info->mList, range);
 
-  info->mBuilder.LeavePresShell(ancestorFrame);
+  info->mBuilder.LeavePresShell(ancestorFrame, &info->mList);
 
 #ifdef DEBUG
   if (gDumpRangePaintList) {
@@ -6012,10 +6012,10 @@ PresShell::DoUpdateApproximateFrameVisibility(bool aRemoveOnly)
     }
   }
   builder.IgnorePaintSuppression();
-  builder.EnterPresShell(rootFrame, updateRect);
+  builder.EnterPresShell(rootFrame);
   nsDisplayList list;
   rootFrame->BuildDisplayListForStackingContext(&builder, updateRect, &list);
-  builder.LeavePresShell(rootFrame, updateRect);
+  builder.LeavePresShell(rootFrame, &list);
 
   RebuildApproximateFrameVisibilityDisplayList(list);
 
