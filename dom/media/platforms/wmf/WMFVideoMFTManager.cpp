d506aa6af979bfb6ff5f8cc3878c99e5f1939075
Matt Woodrow <mwoodrow@mozilla.com>
2016-11-11 15:04:06 +1300

    Bug 1316222 - Use SyncRunnable instead of NS_DISPATCH_SYNC in WMFVideoMFTManager since the latter can spin the event loop. r=jya

--- dom/media/platforms/wmf/WMFVideoMFTManager.cpp
+++ dom/media/platforms/wmf/WMFVideoMFTManager.cpp
@@ -31,6 +31,7 @@
 #include "GMPUtils.h" // For SplitAt. TODO: Move SplitAt to a central place.
 #include "MP4Decoder.h"
 #include "VPXDecoder.h"
+#include "mozilla/SyncRunnable.h"
 
 #define LOG(...) MOZ_LOG(sPDMLog, mozilla::LogLevel::Debug, (__VA_ARGS__))
 
@@ -372,7 +373,9 @@ WMFVideoMFTManager::InitializeDXVA(bool aForceD3D9)
   if (NS_IsMainThread()) {
     event->Run();
   } else {
-    NS_DispatchToMainThread(event, NS_DISPATCH_SYNC);
+    // This logic needs to run on the main thread
+    nsCOMPtr<nsIThread> mainThread = do_GetMainThread();
+    mozilla::SyncRunnable::DispatchToThread(mainThread, event);
   }
   mDXVA2Manager = event->mDXVA2Manager;
 
@@ -608,7 +611,9 @@ WMFVideoMFTManager::CanUseDXVA(IMFMediaType* aType)
   if (NS_IsMainThread()) {
     event->Run();
   } else {
-    NS_DispatchToMainThread(event, NS_DISPATCH_SYNC);
+    // This logic needs to run on the main thread
+    nsCOMPtr<nsIThread> mainThread = do_GetMainThread();
+    mozilla::SyncRunnable::DispatchToThread(mainThread, event);
   }
 
   return event->mSupportsConfig;
