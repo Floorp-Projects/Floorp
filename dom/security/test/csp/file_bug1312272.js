61009932ee25ded9b0a4f3d1f42544ef463023c8
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-11-11 12:57:22 +0100

    Backed out changeset cab5e6485c77 (bug 1312272) for bustage

--- dom/security/test/csp/file_bug1312272.js
+++
@@ -1,8 +0,0 @@
-var m = document.getElementById("m");
-m.addEventListener("click", function() {
-  // this will trigger after onstart, obviously.
-  parent.postMessage('finish', '*');
-});
-console.log("finish-handler setup");
-m.click();
-console.log("clicked");
