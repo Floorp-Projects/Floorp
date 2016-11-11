2ec30cb11d88477ffa48400d774f5788d0ea10e1
Henrik Skupin <mail@hskupin.info>
2016-11-11 09:54:55 +0100

    Bug 1316707 - Remove B2G mach command for Marionette. r=ato
    MozReview-Commit-ID: DXBObRkyiUA

--- testing/marionette/mach_commands.py
+++ testing/marionette/mach_commands.py
@@ -63,37 +63,6 @@ def run_marionette(tests, binary=None, topsrcdir=None, **kwargs):
         return 0
 
 @CommandProvider
-class B2GCommands(MachCommandBase):
-    def __init__(self, context):
-        MachCommandBase.__init__(self, context)
-
-        for attr in ('b2g_home', 'device_name'):
-            setattr(self, attr, getattr(context, attr, None))
-    @Command('marionette-webapi', category='testing',
-        description='Run a Marionette webapi test (test WebAPIs using marionette).',
-        conditions=[conditions.is_b2g])
-    @CommandArgument('--tag', action='append', dest='test_tags',
-        help='Filter out tests that don\'t have the given tag. Can be used '
-             'multiple times in which case the test must contain at least one '
-             'of the given tags.')
-    @CommandArgument('tests', nargs='*', metavar='TESTS',
-        help='Path to test(s) to run.')
-    def run_marionette_webapi(self, tests, **kwargs):
-        emulator = None
-        if self.device_name:
-            if self.device_name.startswith('emulator'):
-                emulator = 'arm'
-                if 'x86' in self.device_name:
-                    emulator = 'x86'
-
-        if self.substs.get('ENABLE_MARIONETTE') != '1':
-            print(MARIONETTE_DISABLED_B2G % 'marionette-webapi')
-            return 1
-
-        return run_marionette(tests, b2g_path=self.b2g_home, emulator=emulator,
-            topsrcdir=self.topsrcdir, **kwargs)
-
-@CommandProvider
 class MachCommands(MachCommandBase):
     @Command('marionette-test', category='testing',
         description='Run a Marionette test (Check UI or the internal JavaScript using marionette).',
