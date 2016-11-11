12a171309a9a3dac3634dff8c1f617fbd1e91951
Bobby Holley <bobbyholley@gmail.com>
2016-11-11 00:00:50 -0800

    Bug 1315394 - stylo: stop unconditionally marking the root node as dirty in PresShell::Init. r=heycam
    MozReview-Commit-ID: 66gJWIZl0wo

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -1698,13 +1698,6 @@ PresShell::Initialize(nscoord aWidth, nscoord aHeight)
   mPresContext->SetVisibleArea(nsRect(0, 0, aWidth, aHeight));
 
   if (mStyleSet->IsServo() && mDocument->GetRootElement()) {
-    // If we have the root element already, go ahead style it along with any
-    // descendants.
-    //
-    // Some things, like nsDocumentViewer::GetPageMode, recreate the PresShell
-    // while keeping the content tree alive (see bug 1292280) - so we
-    // unconditionally mark the root as dirty.
-    mDocument->GetRootElement()->SetIsDirtyForServo();
     mStyleSet->AsServo()->StyleDocument(/* aLeaveDirtyBits = */ false);
   }
 
