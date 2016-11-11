19f26bda9487b3ee7df83919fbad2936f62d7287
Chris Pearce <cpearce@mozilla.com>
2016-11-11 14:55:56 +1300

    Bug 1306314 - Pipe decryptor ID through to WidevineAdapter. r=gerald,a=gchang
    MozReview-Commit-ID: HqRoImDhuFl

--- dom/media/gmp/widevine-adapter/WidevineAdapter.h
+++ dom/media/gmp/widevine-adapter/WidevineAdapter.h
@@ -21,7 +21,10 @@ public:
 
   // These are called in place of the corresponding GMP API functions.
   GMPErr GMPInit(const GMPPlatformAPI* aPlatformAPI) override;
-  GMPErr GMPGetAPI(const char* aAPIName, void* aHostAPI, void** aPluginAPI) override;
+  GMPErr GMPGetAPI(const char* aAPIName,
+                   void* aHostAPI,
+                   void** aPluginAPI,
+                   uint32_t aDecryptorId) override;
   void GMPShutdown() override;
   void GMPSetNodeId(const char* aNodeId, uint32_t aLength) override;
 
