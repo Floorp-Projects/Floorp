a4724864d61bbf2da70919031b0a0b0801891454
Luke Wagner <luke@mozilla.com>
2016-11-13 13:21:49 -0600

    Bug 1317033 - Baldr: hoist mprotect out of MacroAssembler::patchCall/FarJump (r=sunfish)
    MozReview-Commit-ID: Nejpro1fxc

--- js/src/jit/arm/MacroAssembler-arm.h
+++ js/src/jit/arm/MacroAssembler-arm.h
@@ -1536,6 +1536,10 @@ class MacroAssemblerARMCompat : public MacroAssemblerARM
     // Instrumentation for entering and leaving the profiler.
     void profilerEnterFrame(Register framePtr, Register scratch);
     void profilerExitFrame();
+
+    struct AutoPrepareForPatching {
+        explicit AutoPrepareForPatching(MacroAssemblerARMCompat&) {}
+    };
 };
 
 typedef MacroAssemblerARMCompat MacroAssemblerSpecific;
