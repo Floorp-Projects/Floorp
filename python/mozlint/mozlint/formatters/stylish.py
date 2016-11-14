ab127bc249509f4ee0811fc23b778c437b3c6c7d
Andrew Halberstadt <ahalberstadt@mozilla.com>
2016-11-14 11:56:46 -0500

    Bug 1316925 - Keep track of failed linters in stylish formatter summary, r=jgraham
    This replaces the "return_code" property on the LintRoller object with a list of "failed"
    linters. This is a bit more useful as it lets us know exactly which linters had a problem
    (whereas previously we just knew *something* went wrong). This patch pushes determining
    the return code back into cli.py, which I think is fine.
    
    In addition, we now pass the list of failed linters into the formatter. This allows us to
    clarify exactly how many linters hit a failure which is a lot better than a seemingly
    "successful" summary message.
    
    Finally I also removed the "no files to lint" message because I've seen several people
    confuse it for an error. I'll probably add it back as a debug log message when we switch
    to using mozlog for output.
    
    MozReview-Commit-ID: 4wyCeOZdOf8

--- python/mozlint/mozlint/formatters/stylish.py
+++ python/mozlint/mozlint/formatters/stylish.py
@@ -43,7 +43,7 @@ class StylishFormatter(object):
         'brightyellow': [11, 3],
     }
     fmt = "  {c1}{lineno}{column}  {c2}{level}{normal}  {message}  {c1}{rule}({linter}){normal}"
-    fmt_summary = "{t.bold}{c}\u2716 {problem} ({error}, {warning}){t.normal}"
+    fmt_summary = "{t.bold}{c}\u2716 {problem} ({error}, {warning}{failure}){t.normal}"
 
     def __init__(self, disable_colors=None):
         if disable_colors or not blessings:
@@ -77,8 +77,9 @@ class StylishFormatter(object):
             s += 's'
         return str(num) + ' ' + s
 
-    def __call__(self, result):
+    def __call__(self, result, failed=None, **kwargs):
         message = []
+        failed = failed or []
 
         num_errors = 0
         num_warnings = 0
@@ -110,13 +111,21 @@ class StylishFormatter(object):
 
             message.append('')  # newline
 
+        # If there were failures, make it clear which linters failed
+        for fail in failed:
+            message.append("{c}A failure occured in the {name} linter.".format(
+                c=self.color('brightred'),
+                name=fail,
+            ))
+
         # Print a summary
         message.append(self.fmt_summary.format(
             t=self.term,
-            c=self.color('brightred') if num_errors else self.color('brightyellow'),
-            problem=self._pluralize('problem', num_errors + num_warnings),
+            c=self.color('brightred') if num_errors or failed else self.color('brightyellow'),
+            problem=self._pluralize('problem', num_errors + num_warnings + len(failed)),
             error=self._pluralize('error', num_errors),
             warning=self._pluralize('warning', num_warnings),
+            failure=', {}'.format(self._pluralize('failure', len(failed))) if failed else '',
         ))
 
         return '\n'.join(message)
