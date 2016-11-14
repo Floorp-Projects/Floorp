891306a89ab604b8dc10a1de098079432fced3ce
Iris Hsiao <ihsiao@mozilla.com>
2016-11-14 11:07:28 +0800

    Backed out changeset 9343cb73c218 (bug 1306314)

--- dom/media/gmp/GMPContentParent.cpp
+++ dom/media/gmp/GMPContentParent.cpp
@@ -194,11 +194,10 @@ GMPContentParent::GetGMPAudioDecoder(GMPAudioDecoderParent** aGMPAD)
 }
 
 nsresult
-GMPContentParent::GetGMPVideoDecoder(GMPVideoDecoderParent** aGMPVD,
-                                     uint32_t aDecryptorId)
+GMPContentParent::GetGMPVideoDecoder(GMPVideoDecoderParent** aGMPVD)
 {
   // returned with one anonymous AddRef that locks it until Destroy
-  PGMPVideoDecoderParent* pvdp = SendPGMPVideoDecoderConstructor(aDecryptorId);
+  PGMPVideoDecoderParent* pvdp = SendPGMPVideoDecoderConstructor();
   if (!pvdp) {
     return NS_ERROR_FAILURE;
   }
@@ -231,7 +230,7 @@ GMPContentParent::GetGMPVideoEncoder(GMPVideoEncoderParent** aGMPVE)
 }
 
 PGMPVideoDecoderParent*
-GMPContentParent::AllocPGMPVideoDecoderParent(const uint32_t& aDecryptorId)
+GMPContentParent::AllocPGMPVideoDecoderParent()
 {
   GMPVideoDecoderParent* vdp = new GMPVideoDecoderParent(this);
   NS_ADDREF(vdp);
