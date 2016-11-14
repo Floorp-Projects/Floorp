9af44ca17195954a9693797987e55630d81f5abc
André Bargull <andre.bargull@gmail.com>
2016-11-14 12:28:13 -0800

    Bug 1317308: Don't match Mongolian vowel separator as white space in regular expressions. r=arai

--- js/src/tests/ecma_5/RegExp/regexp-space-character-class.js
+++ js/src/tests/ecma_5/RegExp/regexp-space-character-class.js
@@ -7,8 +7,6 @@ var gTestfile = 'regexp-space-character-class.js';
 //-----------------------------------------------------------------------------
 var BUGNUMBER = 514808;
 var summary = 'Correct space character class in regexes';
-var actual = '';
-var expect = '';
  
  
 //-----------------------------------------------------------------------------
@@ -20,26 +18,13 @@ function test()
 enterFunc ('test');
 printBugNumber(BUGNUMBER);
 printStatus (summary);
- 
-var spaces = [ "\u0009", "\u000b", "\u000c", "\u0020", "\u00a0", "\u1680",
-"\u180e", "\u2000", "\u2001", "\u2002", "\u2003", "\u2004",
-"\u2005", "\u2006", "\u2007", "\u2008", "\u2009", "\u200a",
-"\u202f", "\u205f", "\u3000", "\ufeff" ];
-var line_terminators = [ "\u2028", "\u2029", "\u000a", "\u000d" ];
-var space_chars = [].concat(spaces, line_terminators);
- 
+
+// NOTE: White space and line terminators are now tested in
+// ecma_6/RegExp/character-class-escape-s.js.
+
 var non_space_chars = [ "\u200b", "\u200c", "\u200d" ];
- 
-var chars = [].concat(space_chars, non_space_chars);
-var is_space = [].concat(space_chars.map(function(ch) { return true; }),
-non_space_chars.map(function(ch) { return false; }));
-var expect = is_space.join(',');
- 
-var actual = chars.map(function(ch) { return /\s/.test(ch); }).join(',');
-reportCompare(expect, actual, summary);
- 
-var actual = chars.map(function(ch) { return /\s/.test(ch); }).join(',');
-reportCompare(expect, actual, summary);
- 
+
+reportCompare(non_space_chars.some(function(ch){ return /\s/.test(ch); }), false, summary);
+
 exitFunc ('test');
 }
