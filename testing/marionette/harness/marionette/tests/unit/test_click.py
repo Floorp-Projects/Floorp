72fbc079be93fbb0a928a39f4ad9f9b92adeb429
Andreas Tolfsen <ato@mozilla.com>
2016-11-14 21:12:00 +0000

    Bug 1317386 - Add test for overlay element after scroll. r=automatedtester, a=test-only
    Add a test that checks that `scrollIntoView({block: "end", inline:
    "nearest"})` works by placing the element at the bottom of the viewport,
    where the overlay does not cover it.
    
    MozReview-Commit-ID: Gzv9n17240G
    
    --HG--
    extra : source : d8ad355ff155d942ce6658a630a7f6418e520490
    extra : intermediate-source : 54e23725434ea1aefd120b7d663a9ee16a113140

--- testing/marionette/harness/marionette/tests/unit/test_click.py
+++ testing/marionette/harness/marionette/tests/unit/test_click.py
@@ -2,12 +2,50 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import urllib
 from unittest import skip
 
 from marionette_driver.by import By
 from marionette_driver.errors import NoSuchElementException, ElementNotVisibleException
 from marionette_driver.wait import Wait
 from marionette import MarionetteTestCase
+
+
+def inline(doc):
+    return "data:text/html;charset=utf-8,{}".format(urllib.quote(doc))
+
+
+# The <a> element in the following HTML is not interactable because it
+# is hidden by an overlay when scrolled into the top of the viewport.
+# It should be interactable when scrolled in at the bottom of the
+# viewport.
+fixed_overlay = inline("""
+<style>
+* { margin: 0; padding: 0; }
+body { height: 300vh }
+div, a { display: block }
+div {
+  background-color: pink;
+  position: fixed;
+  width: 100%;
+  height: 40px;
+  top: 0;
+}
+a {
+  margin-top: 1000px;
+}
+</style>
+
+<div>overlay</div>
+<a href=#>link</a>
+
+<script>
+window.clicked = false;
+
+let link = document.querySelector("a");
+link.addEventListener("click", () => window.clicked = true);
+</script>
+""")
 
 
 class TestLegacyClick(MarionetteTestCase):
@@ -45,6 +83,12 @@ class TestLegacyClick(MarionetteTestCase):
         self.marionette.navigate(test_html)
         self.marionette.find_element(By.ID, "overflowLink").click()
         self.wait_for_condition(lambda mn: self.marionette.title == "XHTML Test Page")
+
+    def test_scroll_into_view_near_end(self):
+        self.marionette.navigate(fixed_overlay)
+        link = self.marionette.find_element(By.TAG_NAME, "a")
+        link.click()
+        self.assertTrue(self.marionette.execute_script("return window.clicked", sandbox=None))
 
 
 class TestClick(TestLegacyClick):
