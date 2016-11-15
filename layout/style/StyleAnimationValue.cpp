23b961ef60fe3c31e57342ab056470da4a9fa9b2
Boris Chiou <boris.chiou@gmail.com>
2016-11-15 11:58:16 +0800

    Bug 1286151 - Part 4: Factor out ComputeSingleShadowSquareDistance. r=dholbert
    Both eUnit_Shadow and the drop-shadow of eUnit_Filter needs to compute the
    distance of two shadows, so we can factor out the implementation.
    
    MozReview-Commit-ID: IGdxQWMXdE1
    
    --HG--
    extra : rebase_source : 9889c556c0cc007102a152859575cfe56830ca61

--- layout/style/StyleAnimationValue.cpp
+++ layout/style/StyleAnimationValue.cpp
@@ -760,6 +760,59 @@ AddWeightedFilterFunction(double aCoeff1, const nsCSSValueList* aList1,
 static inline float
 GetNumberOrPercent(const nsCSSValue &aValue);
 
+static bool
+ComputeSingleShadowSquareDistance(const nsCSSValueList* aShadow1,
+                                  const nsCSSValueList* aShadow2,
+                                  double& aSquareDistance)
+{
+  MOZ_ASSERT(aShadow1->mValue.GetUnit() == eCSSUnit_Array, "wrong unit");
+  MOZ_ASSERT(aShadow2->mValue.GetUnit() == eCSSUnit_Array, "wrong unit");
+  const nsCSSValue::Array* array1 = aShadow1->mValue.GetArrayValue();
+  const nsCSSValue::Array* array2 = aShadow2->mValue.GetArrayValue();
+
+  double squareDistance = 0.0;
+  // X, Y, Radius, Spread
+  for (size_t i = 0; i < 4; ++i) {
+    MOZ_ASSERT(array1->Item(i).GetUnit() == eCSSUnit_Pixel,
+               "unexpected unit");
+    MOZ_ASSERT(array2->Item(i).GetUnit() == eCSSUnit_Pixel,
+               "unexpected unit");
+    double diff = array1->Item(i).GetFloatValue() -
+                  array2->Item(i).GetFloatValue();
+    squareDistance += diff * diff;
+  }
+
+  // Color, Inset
+  const nsCSSValue& color1 = array1->Item(4);
+  const nsCSSValue& color2 = array2->Item(4);
+  const nsCSSValue& inset1 = array1->Item(5);
+  const nsCSSValue& inset2 = array2->Item(5);
+  if ((color1.GetUnit() != color2.GetUnit() &&
+        (!color1.IsNumericColorUnit() ||
+         !color2.IsNumericColorUnit())) ||
+      inset1 != inset2) {
+    // According to AddWeightedShadowItems, we don't know how to animate
+    // between color and no-color, or between inset and not-inset,
+    // so we cannot compute the distance either.
+    // Note: There are only two possible states of the inset value:
+    //  (1) GetUnit() == eCSSUnit_Null
+    //  (2) GetUnit() == eCSSUnit_Enumerated &&
+    //      GetIntValue() == NS_STYLE_BOX_SHADOW_INSET
+    return false;
+  }
+
+  // We compute the distance of colors only if both are numeric color units.
+  if (color1.GetUnit() != eCSSUnit_Null) {
+    double colorDistance =
+      StyleAnimationValue::ComputeColorDistance(ExtractColor(color1),
+                                                ExtractColor(color2));
+    squareDistance += colorDistance * colorDistance;
+  }
+
+  aSquareDistance = squareDistance;
+  return true;
+}
+
 // Return false if we cannot compute the distance between these filter
 // functions.
 static bool
@@ -824,49 +877,11 @@ ComputeFilterSquareDistance(const nsCSSValueList* aList1,
     case eCSSKeyword_drop_shadow: {
       MOZ_ASSERT(!func1.GetListValue()->mNext && !func2.GetListValue()->mNext,
                  "drop-shadow filter func doesn't support lists");
-      const nsCSSValue& shadow1 = func1.GetListValue()->mValue;
-      const nsCSSValue& shadow2 = func2.GetListValue()->mValue;
-
-      MOZ_ASSERT(shadow1.GetUnit() == eCSSUnit_Array, "wrong unit");
-      MOZ_ASSERT(shadow2.GetUnit() == eCSSUnit_Array, "wrong unit");
-      const nsCSSValue::Array* array1 = shadow1.GetArrayValue();
-      const nsCSSValue::Array* array2 = shadow2.GetArrayValue();
-      double squareDistance = 0.0;
-
-      // X, Y, Radius, Spread
-      for (size_t i = 0; i < 4; ++i) {
-        MOZ_ASSERT(array1->Item(i).GetUnit() == eCSSUnit_Pixel,
-                   "unexpected unit");
-        MOZ_ASSERT(array2->Item(i).GetUnit() == eCSSUnit_Pixel,
-                   "unexpected unit");
-        double diff = array1->Item(i).GetFloatValue() -
-                      array2->Item(i).GetFloatValue();
-        squareDistance += diff * diff;
-      }
-
-      // Color, Inset
-      const nsCSSValue& colorValue1 = array1->Item(4);
-      const nsCSSValue& colorValue2 = array2->Item(4);
-      const nsCSSValue& inset1 = array1->Item(5);
-      const nsCSSValue& inset2 = array2->Item(5);
-      if ((colorValue1.GetUnit() != colorValue2.GetUnit() &&
-            (!colorValue1.IsNumericColorUnit() ||
-             !colorValue2.IsNumericColorUnit())) ||
-          inset1.GetUnit() != inset2.GetUnit()) {
-        // We don't know how to animate between color and no-color, or
-        // between inset and not-inset.
-        // NOTE: In case when both colors' units are eCSSUnit_Null, that means
-        // neither color value was specified, so we can interpolate.
+      if (!ComputeSingleShadowSquareDistance(func1.GetListValue(),
+                                             func2.GetListValue(),
+                                             aSquareDistance)) {
         return false;
       }
-
-      if (colorValue1.GetUnit() != eCSSUnit_Null) {
-        double diff =
-          StyleAnimationValue::ComputeColorDistance(ExtractColor(colorValue1),
-                                                    ExtractColor(colorValue2));
-        squareDistance += diff * diff;
-      }
-      aSquareDistance = squareDistance;
       break;
     }
     default:
@@ -1648,53 +1663,25 @@ StyleAnimationValue::ComputeDistance(nsCSSPropertyID aProperty,
         return false;
       }
 
-      const nsCSSValueList *shadow1 = normValue1.GetCSSValueListValue();
-      const nsCSSValueList *shadow2 = normValue2.GetCSSValueListValue();
+      const nsCSSValueList* shadow1 = normValue1.GetCSSValueListValue();
+      const nsCSSValueList* shadow2 = normValue2.GetCSSValueListValue();
 
-      double squareDistance = 0.0;
+      aDistance = 0.0;
       MOZ_ASSERT(!shadow1 == !shadow2, "lists should be same length");
       while (shadow1) {
-        nsCSSValue::Array *array1 = shadow1->mValue.GetArrayValue();
-        nsCSSValue::Array *array2 = shadow2->mValue.GetArrayValue();
-        for (size_t i = 0; i < 4; ++i) {
-          MOZ_ASSERT(array1->Item(i).GetUnit() == eCSSUnit_Pixel,
-                     "unexpected unit");
-          MOZ_ASSERT(array2->Item(i).GetUnit() == eCSSUnit_Pixel,
-                     "unexpected unit");
-          double diff = array1->Item(i).GetFloatValue() -
-                        array2->Item(i).GetFloatValue();
-          squareDistance += diff * diff;
+        double squareDistance = 0.0;
+        if (!ComputeSingleShadowSquareDistance(shadow1, shadow2,
+                                               squareDistance)) {
+          NS_ERROR("Unexpected ComputeSingleShadowSquareDistance failure; "
+                   "why didn't we fail earlier, in AddWeighted calls above?");
         }
-
-        const nsCSSValue &color1 = array1->Item(4);
-        const nsCSSValue &color2 = array2->Item(4);
-#ifdef DEBUG
-        {
-          const nsCSSValue &inset1 = array1->Item(5);
-          const nsCSSValue &inset2 = array2->Item(5);
-          // There are only two possible states of the inset value:
-          //  (1) GetUnit() == eCSSUnit_Null
-          //  (2) GetUnit() == eCSSUnit_Enumerated &&
-          //      GetIntValue() == NS_STYLE_BOX_SHADOW_INSET
-          MOZ_ASSERT(((color1.IsNumericColorUnit() &&
-                       color2.IsNumericColorUnit()) ||
-                      (color1.GetUnit() == color2.GetUnit())) &&
-                     inset1 == inset2,
-                     "AddWeighted should have failed");
-        }
-#endif
-
-        if (color1.GetUnit() != eCSSUnit_Null) {
-          double colorDistance = ComputeColorDistance(color1.GetColorValue(),
-                                                      color2.GetColorValue());
-          squareDistance += colorDistance * colorDistance;
-        }
+        aDistance += squareDistance; // cumulative distance^2; sqrt()'d below.
 
         shadow1 = shadow1->mNext;
         shadow2 = shadow2->mNext;
         MOZ_ASSERT(!shadow1 == !shadow2, "lists should be same length");
       }
-      aDistance = sqrt(squareDistance);
+      aDistance = sqrt(aDistance);
       return true;
     }
     case eUnit_Shape:
