af46587aa37d218f1ed6d23e7c8b29c05a0bca5a
Dan Minor <dminor@mozilla.com>
2016-11-15 11:53:47 -0500

    Bug 1317726 - sdp_file_parser should not depend on xpcom glue; r=drno
    MozReview-Commit-ID: KDv3RzGurQx
    
    --HG--
    extra : rebase_source : a72246ebe3bc520855a37a001752230b25316153
    extra : amend_source : 0e3a7509e013cc55d50227d07203a29ee55d6a6f

--- media/webrtc/signaling/src/sdp/sipcc/cpr_string.c
+++ media/webrtc/signaling/src/sdp/sipcc/cpr_string.c
@@ -10,13 +10,19 @@
 #include "cpr_strings.h"
 
 /* From cpr_stdlib.h */
+#ifdef CPR_STRING_USE_FALLIBLE_MALLOC
+#define cpr_malloc(a) malloc(a)
+#define cpr_calloc(a, b) calloc(a, b)
+#define cpr_realloc(a, b) realloc(a, b)
+#define cpr_free(a) free(a)
+#else
 #include "mozilla/mozalloc.h"
 
 #define cpr_malloc(a) moz_xmalloc(a)
 #define cpr_calloc(a, b) moz_xcalloc(a, b)
 #define cpr_realloc(a, b) moz_xrealloc(a, b)
 #define cpr_free(a) free(a)
-
+#endif
 
 /**
  * sstrncpy
