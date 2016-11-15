12eaf5ca37ff26b7779de44b0593ffd0d2010266
Andrew McCreight <amccreight@mozilla.com>
2016-11-15 07:59:24 -0800

    Backed out changeset 63cafb0c2f0f (bug 1315837) for causing bug 1317415.

--- dom/base/DOMIntersectionObserver.h
+++ dom/base/DOMIntersectionObserver.h
@@ -142,9 +142,7 @@ public:
   void Observe(Element& aTarget);
   void Unobserve(Element& aTarget);
 
-  bool UnlinkTarget(Element& aTarget);
   void Disconnect();
-
   void TakeRecords(nsTArray<RefPtr<DOMIntersectionObserverEntry>>& aRetVal);
 
   mozilla::dom::IntersectionCallback* IntersectionCallback() { return mCallback; }
