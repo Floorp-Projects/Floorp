940f2fba2949c5456bfad7e1a912ab83c9f0dc78
Bob Owen <bobowencode@gmail.com>
2016-11-15 11:10:02 +0000

    Bug 1315212 - Allow for failed call to wrapped DrawTarget in DrawTargetRecording::CreateSimilarDrawTarget. r=bas, a=ritu
    MozReview-Commit-ID: 4SMixY4Nook

--- gfx/2d/DrawTargetRecording.h
+++ gfx/2d/DrawTargetRecording.h
@@ -19,16 +19,6 @@ public:
   MOZ_DECLARE_REFCOUNTED_VIRTUAL_TYPENAME(DrawTargetRecording, override)
   DrawTargetRecording(DrawEventRecorder *aRecorder, DrawTarget *aDT, bool aHasData = false);
 
-  /**
-   * Used for creating a DrawTargetRecording for a CreateSimilarDrawTarget call.
-   *
-   * @param aDT DrawTargetRecording on which CreateSimilarDrawTarget  was called
-   * @param aSize size for the similar DrawTarget
-   * @param aFormat format for the similar DrawTarget
-   */
-  DrawTargetRecording(const DrawTargetRecording *aDT, const IntSize &aSize,
-                      SurfaceFormat aFormat);
-
   ~DrawTargetRecording();
 
   virtual DrawTargetType GetType() const override { return mFinalDT->GetType(); }
@@ -322,6 +312,17 @@ public:
   }
 
 private:
+  /**
+   * Used for creating a DrawTargetRecording for a CreateSimilarDrawTarget call.
+   * We have to call CreateSimilarDrawTarget on mFinalDT up front and pass it in
+   * as it can fail.
+   *
+   * @param aDT DrawTargetRecording on which CreateSimilarDrawTarget was called
+   * @param aSimilarDT Similar DrawTarget created from aDT.mFinalDT.
+   */
+  DrawTargetRecording(const DrawTargetRecording *aDT,
+                      DrawTarget *aSimilarDT);
+
   Path *GetPathForPathRecording(const Path *aPath) const;
   already_AddRefed<PathRecording> EnsurePathStored(const Path *aPath);
   void EnsurePatternDependenciesStored(const Pattern &aPattern);
