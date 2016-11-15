bf24b8862bc072a9c325a2a0e7192c28036f9df9
Andreas Tolfsen <ato@mozilla.com>
2016-11-15 00:32:22 +0000

    Bug 1317462 - Remove screenshot capabilities from Marionette; r=automatedtester
    MozReview-Commit-ID: EVdiixG6dsD
    
    --HG--
    extra : rebase_source : 7cf09ebd0f0129387dcfd04eb4246a38e5a8c293

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -143,8 +143,6 @@ this.GeckoDriver = function(appName, server) {
     "raisesAccessibilityExceptions": false,
     "rotatable": this.appName == "B2G",
     "acceptSslCerts": false,
-    "takesElementScreenshot": true,
-    "takesScreenshot": true,
     "proxy": {},
 
     // proprietary extensions
