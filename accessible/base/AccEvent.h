bd104a765a5953379181f64c200c5a9c8de5a3ae
Iris Hsiao <ihsiao@mozilla.com>
2016-11-16 14:41:57 +0800

    Backed out changeset b2694ad14de7 (bug 1270916) CLOSED TREE

--- accessible/base/AccEvent.h
+++ accessible/base/AccEvent.h
@@ -211,7 +211,7 @@ class AccTreeMutationEvent : public AccEvent
 {
 public:
   AccTreeMutationEvent(uint32_t aEventType, Accessible* aTarget) :
-    AccEvent(aEventType, aTarget, eAutoDetect, eCoalesceReorder), mGeneration(0) {}
+    AccEvent(aEventType, aTarget, eAutoDetect, eCoalesceReorder) {}
 
   // Event
   static const EventGroup kEventGroup = eTreeMutationEvent;
@@ -225,16 +225,9 @@ public:
   AccTreeMutationEvent* NextEvent() const { return mNextEvent; }
   AccTreeMutationEvent* PrevEvent() const { return mPrevEvent; }
 
-  /**
-   * A sequence number to know when this event was fired.
-   */
-  uint32_t EventGeneration() const { return mGeneration; }
-  void SetEventGeneration(uint32_t aGeneration) { mGeneration = aGeneration; }
-
 private:
   RefPtr<AccTreeMutationEvent> mNextEvent;
   RefPtr<AccTreeMutationEvent> mPrevEvent;
-  uint32_t mGeneration;
 };
 
 /**
