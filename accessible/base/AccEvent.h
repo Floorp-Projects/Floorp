c737c290f9f6092e4715d800e9a234898946dc6a
Iris Hsiao <ihsiao@mozilla.com>
2016-11-16 14:42:28 +0800

    Backed out changeset baca5dd6bab6 (bug 1270916) CLOSED TREE

--- accessible/base/AccEvent.h
+++ accessible/base/AccEvent.h
@@ -202,25 +202,15 @@ private:
   friend class EventTree;
 };
 
-/**
- * A base class for events related to tree mutation, either an AccMutation
- * event, or an AccReorderEvent.
- */
-class AccTreeMutationEvent : public AccEvent
-{
-public:
-  AccTreeMutationEvent(uint32_t aEventType, Accessible* aTarget) :
-    AccEvent(aEventType, aTarget, eAutoDetect, eCoalesceReorder) {}
-};
 
 /**
  * Base class for show and hide accessible events.
  */
-class AccMutationEvent: public AccTreeMutationEvent
+class AccMutationEvent: public AccEvent
 {
 public:
   AccMutationEvent(uint32_t aEventType, Accessible* aTarget) :
-    AccTreeMutationEvent(aEventType, aTarget)
+    AccEvent(aEventType, aTarget, eAutoDetect, eCoalesceReorder)
   {
     // Don't coalesce these since they are coalesced by reorder event. Coalesce
     // contained text change events.
@@ -308,11 +298,12 @@ private:
 /**
  * Class for reorder accessible event. Takes care about
  */
-class AccReorderEvent : public AccTreeMutationEvent
+class AccReorderEvent : public AccEvent
 {
 public:
   explicit AccReorderEvent(Accessible* aTarget) :
-    AccTreeMutationEvent(::nsIAccessibleEvent::EVENT_REORDER, aTarget) { }
+    AccEvent(::nsIAccessibleEvent::EVENT_REORDER, aTarget,
+             eAutoDetect, eCoalesceReorder) { }
   virtual ~AccReorderEvent() { }
 
   // Event
