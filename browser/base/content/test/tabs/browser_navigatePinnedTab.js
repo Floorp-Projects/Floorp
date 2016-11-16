1ebea9fa2904c6607c6c219c87d9e00efc167683
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-11-16 16:31:42 +0000

    Bug 1314249 - if the target changes, assume we should not pass an opener ref, r=smaug
    MozReview-Commit-ID: 7DBrRW1WiMP

--- browser/base/content/test/tabs/browser_navigatePinnedTab.js
+++ browser/base/content/test/tabs/browser_navigatePinnedTab.js
@@ -29,7 +29,30 @@ add_task(function* () {
   is(appTab.linkedBrowser.currentURI.spec, TEST_LINK_CHANGED,
      "New page loaded in the app tab");
   is(gBrowser.tabs.length, initialTabsNo, "No additional tabs were opened");
+
+  // Now check that opening a link that does create a new tab works,
+  // and also that it nulls out the opener.
+  let pageLoadPromise = BrowserTestUtils.browserLoaded(appTab.linkedBrowser, "http://example.com/");
+  yield BrowserTestUtils.loadURI(appTab.linkedBrowser, "http://example.com/");
+  info("Started loading example.com");
+  yield pageLoadPromise;
+  info("Loaded example.com");
+  let newTabPromise = BrowserTestUtils.waitForNewTab(gBrowser, "http://example.org/");
+  yield ContentTask.spawn(browser, null, function* () {
+    let link = content.document.createElement("a");
+    link.href = "http://example.org/";
+    content.document.body.appendChild(link);
+    link.click();
+  });
+  info("Created & clicked link");
+  let extraTab = yield newTabPromise;
+  info("Got a new tab");
+  yield ContentTask.spawn(extraTab.linkedBrowser, null, function* () {
+    is(content.opener, null, "No opener should be available");
+  });
+  yield BrowserTestUtils.removeTab(extraTab);
 });
+
 
 registerCleanupFunction(function() {
   gBrowser.removeTab(gBrowser.selectedTab);
