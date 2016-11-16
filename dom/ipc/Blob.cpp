24085dac282dd6bd787dc545280f8320b38d5376
Andrea Marchesini <amarchesini@mozilla.com>
2016-11-16 15:04:23 +0100

    Bug 1317725 -Correct creation of Sliced Remote Blob actorss when used in FormData in workers, r=smaug
    --HG--
    rename : dom/workers/test/script_bug1301094.js => dom/workers/test/script_createFile.js

--- dom/ipc/Blob.cpp
+++ dom/ipc/Blob.cpp
@@ -2244,7 +2244,6 @@ RemoteBlobImpl::CommonInit(BlobChild* aActor)
 {
   MOZ_ASSERT(aActor);
   aActor->AssertIsOnOwningThread();
-  MOZ_ASSERT(!mIsSlice);
 
   mActor = aActor;
   mActorTarget = aActor->EventTarget();
@@ -2744,12 +2743,16 @@ RemoteBlobSliceImpl::EnsureActorWasCreatedInternal()
                                 mStart + mLength /* end */,
                                 mContentType /* contentType */));
 
+  BlobChild* actor;
+
   if (nsIContentChild* contentManager = baseActor->GetContentManager()) {
-    mActor = SendSliceConstructor(contentManager, this, params);
+    actor = SendSliceConstructor(contentManager, this, params);
   } else {
-    mActor =
+    actor =
       SendSliceConstructor(baseActor->GetBackgroundManager(), this, params);
   }
+
+  CommonInit(actor);
 }
 
 NS_IMPL_ISUPPORTS_INHERITED0(BlobChild::RemoteBlobSliceImpl,
