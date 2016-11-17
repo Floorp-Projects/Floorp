5a4f7344b959b79b81b57922f24dd73014d9e8a0
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-11-17 14:58:13 -0800

    Bug 1318466 - Fix image_builder to fail if docker socket returns 404. r=dustin
    MozReview-Commit-ID: sgjfYiAqtT

--- testing/docker/image_builder/build-image.sh
+++ testing/docker/image_builder/build-image.sh
@@ -37,23 +37,27 @@ test -f "$CONTEXT_FILE" || raise_error "Context file wasn't created"
 # Post context tar-ball to docker daemon
 # This interacts directly with the docker remote API, see:
 # https://docs.docker.com/engine/reference/api/docker_remote_api_v1.18/
-curl -s \
+curl -s --fail \
   -X POST \
   --header 'Content-Type: application/tar' \
   --data-binary "@$CONTEXT_FILE" \
   --unix-socket /var/run/docker.sock "http:/build?t=$IMAGE_NAME:$HASH" \
   | tee /tmp/docker-build.log \
-  | jq -r '.status + .progress, .stream[:-1], .error | select(. != null)'
+  | jq -jr '(.status + .progress, .error | select(. != null) + "\n"), .stream | select(. != null)'
 
 # Exit non-zero if there is error entries in the log
 if cat /tmp/docker-build.log | jq -se 'add | .error' > /dev/null; then
   raise_error "Image build failed: `cat /tmp/docker-build.log | jq -rse 'add | .error'`";
 fi
 
-# Get image from docker daemon
+# Get image from docker daemon (try up to 10 times)
 # This interacts directly with the docker remote API, see:
 # https://docs.docker.com/engine/reference/api/docker_remote_api_v1.18/
-curl -s \
-  -X GET \
-  --unix-socket /var/run/docker.sock "http:/images/$IMAGE_NAME:$HASH/get" \
-  | zstd -3 -c -o /home/worker/workspace/artifacts/image.tar.zst
+count=0
+while ! curl -s --fail -X GET  \
+             --unix-socket /var/run/docker.sock "http:/images/$IMAGE_NAME:$HASH/get" \
+             | zstd -3 -c -o /home/worker/workspace/artifacts/image.tar.zst; do
+  ((c++)) && ((c==10)) && echo 'Failed to get image from docker' && exit 1;
+  echo 'Waiting for image to be ready';
+  sleep 5;
+done
