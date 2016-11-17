2e362c67dbf361baa3d4216dd2451078aa59739d
Tooru Fujisawa <arai_a@mac.com>
2016-11-17 14:19:51 +0900

    Bug 1317824 - Check debugger resumption value of generator and async function. r=jimb

--- js/src/vm/AsyncFunction.h
+++ js/src/vm/AsyncFunction.h
@@ -32,6 +32,9 @@ MOZ_MUST_USE bool
 AsyncFunctionAwaitedRejected(JSContext* cx, Handle<PromiseObject*> resultPromise,
                              HandleValue generatorVal, HandleValue reason);
 
+MOZ_MUST_USE bool
+CheckAsyncResumptionValue(JSContext* cx, HandleValue v);
+
 } // namespace js
 
 #endif /* vm_AsyncFunction_h */
