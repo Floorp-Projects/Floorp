3bbbff67f407868dac3ab2412759aaf6bbfe0586
Andrea Marchesini <amarchesini@mozilla.com>
2016-11-17 14:52:16 +0100

    Bug 1315905 - Cleanup Necko http security check. r=valentin, a=gchang

--- caps/BasePrincipal.cpp
+++ caps/BasePrincipal.cpp
@@ -192,6 +192,18 @@ OriginAttributes::CreateSuffix(nsACString& aStr) const
 #endif
 }
 
+void
+OriginAttributes::CreateAnonymizedSuffix(nsACString& aStr) const
+{
+  OriginAttributes attrs = *this;
+
+  if (!attrs.mFirstPartyDomain.IsEmpty()) {
+    attrs.mFirstPartyDomain.AssignLiteral("_anonymizedFirstPartyDomain_");
+  }
+
+  attrs.CreateSuffix(aStr);
+}
+
 namespace {
 
 class MOZ_STACK_CLASS PopulateFromSuffixIterator final
