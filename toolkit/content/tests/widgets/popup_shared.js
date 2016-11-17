9da3647006f9213d526eba8f1c4bb26137f4f73d
Neil Deakin <neil@mozilla.com>
2016-11-17 09:28:27 -0500

    Bug 1313131, don't wrap when using cursor navigation in menus onr dropdowns on Mac, or dropdowns on Windows, r=ksteuber

--- toolkit/content/tests/widgets/popup_shared.js
+++ toolkit/content/tests/widgets/popup_shared.js
@@ -247,8 +247,11 @@ function goNextStepSync()
     test.test(test.testname, step);
 
     // no events to check for so just check the result
-    if (!("events" in test))
+    if (!("events" in test)) {
       checkResult();
+    } else if (typeof test.events == "function" && !test.events().length) {
+      checkResult();
+    }
   }
   else {
     finish();
