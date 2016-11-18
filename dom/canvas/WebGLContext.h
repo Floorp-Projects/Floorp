8f23a719642c4735de9c8ba48ae1e198173eb333
Jeff Gilbert (:jgilbert) <jgilbert@mozilla.com>
2016-11-17 17:50:38 -0800

    Bug 1310247 - Check if buffers are bound for transform feedback. - r=ethlin a=gchang
    MozReview-Commit-ID: 4EsOdz7vXNH

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -1577,7 +1577,9 @@ protected:
     }
 
     WebGLRefPtr<WebGLBuffer>* ValidateBufferSlot(const char* funcName, GLenum target);
+public:
     WebGLBuffer* ValidateBufferSelection(const char* funcName, GLenum target);
+protected:
     IndexedBufferBinding* ValidateIndexedBufferSlot(const char* funcName, GLenum target,
                                                     GLuint index);
 
@@ -1592,6 +1594,8 @@ protected:
         }
         return true;
     }
+
+    bool ValidateForNonTransformFeedback(const char* funcName, WebGLBuffer* buffer);
 
 public:
     template<typename T>
@@ -1751,6 +1755,17 @@ protected:
 
     ////////////////////////////////////
 
+private:
+    mutable bool mBuffersForUB_Dirty;
+    mutable std::set<const WebGLBuffer*> mBuffersForUB;
+
+public:
+    void OnUBIndexedBindingsChanged() const { mBuffersForUB_Dirty = true; }
+    const decltype(mBuffersForUB)& BuffersForUB() const;
+
+    ////////////////////////////////////
+
+protected:
     // Generic Vertex Attributes
     UniquePtr<GLenum[]> mVertexAttribType;
     GLfloat mVertexAttrib0Vector[4];
