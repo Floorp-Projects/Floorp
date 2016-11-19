793fec58179f7fdf6478ccc7af32b95d48012e54
Michael Layzell <michael@thelayzells.com>
2016-11-19 14:22:14 -0500

    Bug 1318428 - Use the nsstring bindings in rust-url-capi, r=valentin
    This patch also drops the pretense that rust-url-capi will be used from
    outside of c++, or that it will be used outside of mozilla-central,
    removing the ifdef __cplusplus code, and including the C++ header
    "nsString.h".
    
    MozReview-Commit-ID: BULhHf3DObe

--- netwerk/base/rust-url-capi/src/rust-url-capi.h
+++ netwerk/base/rust-url-capi/src/rust-url-capi.h
@@ -5,48 +5,50 @@
 #ifndef __RUST_URL_CAPI
 #define __RUST_URL_CAPI
 #include <stdlib.h>
+#include "nsString.h"
 
-#ifdef __cplusplus
 extern "C" {
-#endif
+
+// NOTE: Preconditions
+// * All nsACString* pointers are unchecked, and must be non-null
+// * All rusturl_ptr pointers must refer to pointers which are returned
+//   by rusturl_new, and must be freed with rusturl_free.
 
 struct rusturl;
 typedef struct rusturl* rusturl_ptr;
 
-rusturl_ptr rusturl_new(const char *spec, size_t src_len);
+rusturl_ptr rusturl_new(const nsACString* spec);
 void rusturl_free(rusturl_ptr url);
 
-int32_t rusturl_get_spec(rusturl_ptr url, void*);
-int32_t rusturl_get_scheme(rusturl_ptr url, void*);
-int32_t rusturl_get_username(rusturl_ptr url, void*);
-int32_t rusturl_get_password(rusturl_ptr url, void*);
-int32_t rusturl_get_host(rusturl_ptr url, void*);
-int32_t rusturl_get_port(rusturl_ptr url); // returns port or -1
-int32_t rusturl_get_path(rusturl_ptr url, void*);
-int32_t rusturl_get_query(rusturl_ptr url, void*);
-int32_t rusturl_get_fragment(rusturl_ptr url, void*);
-int32_t rusturl_has_fragment(rusturl_ptr url); // 1 true, 0 false, < 0 error
+int32_t rusturl_get_spec(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_scheme(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_username(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_password(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_host(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_port(const rusturl_ptr url); // returns port or -1
+int32_t rusturl_get_path(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_query(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_get_fragment(const rusturl_ptr url, nsACString* cont);
+int32_t rusturl_has_fragment(const rusturl_ptr url); // 1 true, 0 false, < 0 error
 
-int32_t rusturl_set_scheme(rusturl_ptr url, const char *scheme, size_t len);
-int32_t rusturl_set_username(rusturl_ptr url, const char *user, size_t len);
-int32_t rusturl_set_password(rusturl_ptr url, const char *pass, size_t len);
-int32_t rusturl_set_host_port(rusturl_ptr url, const char *hostport, size_t len);
-int32_t rusturl_set_host_and_port(rusturl_ptr url, const char *hostport, size_t len);
-int32_t rusturl_set_host(rusturl_ptr url, const char *host, size_t len);
-int32_t rusturl_set_port(rusturl_ptr url, const char *port, size_t len);
+int32_t rusturl_set_scheme(rusturl_ptr url, const nsACString* scheme);
+int32_t rusturl_set_username(rusturl_ptr url, const nsACString* user);
+int32_t rusturl_set_password(rusturl_ptr url, const nsACString* password);
+int32_t rusturl_set_host_port(rusturl_ptr url, const nsACString* hostport);
+int32_t rusturl_set_host_and_port(rusturl_ptr url, const nsACString* hostport);
+int32_t rusturl_set_host(rusturl_ptr url, const nsACString* host);
+int32_t rusturl_set_port(rusturl_ptr url, const nsACString* port);
 int32_t rusturl_set_port_no(rusturl_ptr url, const int32_t port);
-int32_t rusturl_set_path(rusturl_ptr url, const char *path, size_t len);
-int32_t rusturl_set_query(rusturl_ptr url, const char *path, size_t len);
-int32_t rusturl_set_fragment(rusturl_ptr url, const char *path, size_t len);
+int32_t rusturl_set_path(rusturl_ptr url, const nsACString* path);
+int32_t rusturl_set_query(rusturl_ptr url, const nsACString* query);
+int32_t rusturl_set_fragment(rusturl_ptr url, const nsACString* fragment);
 
-int32_t rusturl_resolve(rusturl_ptr url, const char *relative, size_t len, void*);
-int32_t rusturl_common_base_spec(rusturl_ptr url1, rusturl_ptr url2, void*);
-int32_t rusturl_relative_spec(rusturl_ptr url1, rusturl_ptr url2, void*);
+int32_t rusturl_resolve(const rusturl_ptr url, const nsACString* relative, nsACString* cont);
+int32_t rusturl_common_base_spec(const rusturl_ptr url1, const rusturl_ptr url2, nsACString* cont);
+int32_t rusturl_relative_spec(const rusturl_ptr url1, const rusturl_ptr url2, nsACString* cont);
 
 size_t sizeof_rusturl();
 
-#ifdef __cplusplus
 }
-#endif
 
 #endif // __RUST_URL_CAPI
