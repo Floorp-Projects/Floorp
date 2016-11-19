c1394b8292259c44b659c6f2aa62f9f71003445a
Yura Zenevich <yzenevich@mozilla.com>
2016-11-18 23:29:10 -0500

    Bug 1297474 - shutdown accessibility service only if there are no references to accessibles in JS. r=surkov
    MozReview-Commit-ID: BTOuBQXA1Ly

--- accessible/tests/browser/browser_shutdown_acc_reference.js
+++ accessible/tests/browser/browser_shutdown_acc_reference.js
@@ -5,44 +5,51 @@
 'use strict';
 
 add_task(function* () {
-  info('Creating a service');
   // Create a11y service.
   let a11yInit = initPromise();
-  let accService1 = Cc['@mozilla.org/accessibilityService;1'].getService(
+  let accService = Cc['@mozilla.org/accessibilityService;1'].getService(
     Ci.nsIAccessibilityService);
+
   yield a11yInit;
-  ok(accService1, 'Service initialized');
+  ok(accService, 'Service initialized');
 
-  // Add another reference to a11y service. This will not trigger
-  // 'a11y-init-or-shutdown' event
-  let accService2 = Cc['@mozilla.org/accessibilityService;1'].getService(
-    Ci.nsIAccessibilityService);
-  ok(accService2, 'Service initialized');
+  // Accessible object reference will live longer than the scope of this
+  // function.
+  let acc = yield new Promise(resolve => {
+    let intervalId = setInterval(() => {
+      let tabAcc = accService.getAccessibleFor(gBrowser.mCurrentTab);
+      if (tabAcc) {
+        clearInterval(intervalId);
+        resolve(tabAcc);
+      }
+    }, 10);
+  });
+  ok(acc, 'Accessible object is created');
 
-  info('Removing all service references');
   let canShutdown = false;
   // This promise will resolve only if canShutdown flag is set to true. If
   // 'a11y-init-or-shutdown' event with '0' flag comes before it can be shut
   // down, the promise will reject.
   let a11yShutdown = new Promise((resolve, reject) =>
-    shutdownPromise().then(flag => canShutdown ?
-      resolve() : reject('Accessible service was shut down incorrectly')));
-  // Remove first a11y service reference.
-  accService1 = null;
-  ok(!accService1, 'Service is removed');
-  // Force garbage collection that should not trigger shutdown because there is
-  // another reference.
-  forceGC();
+    shutdownPromise().then(flag => canShutdown ? resolve() :
+      reject('Accessible service was shut down incorrectly')));
 
+  accService = null;
+  ok(!accService, 'Service is removed');
+
+  // Force garbage collection that should not trigger shutdown because there is
+  // a reference to an accessible object.
+  forceGC();
   // Have some breathing room when removing a11y service references.
   yield new Promise(resolve => executeSoon(resolve));
 
   // Now allow a11y service to shutdown.
   canShutdown = true;
-  // Remove last a11y service reference.
-  accService2 = null;
-  ok(!accService2, 'Service is removed');
-  // Force garbage collection that should trigger shutdown.
+  // Remove a reference to an accessible object.
+  acc = null;
+  ok(!acc, 'Accessible object is removed');
+
+  // Force garbage collection that should now trigger shutdown.
   forceGC();
   yield a11yShutdown;
 });
