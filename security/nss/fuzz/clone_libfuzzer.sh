b84e4455492fb115b0df40873f03924e5a8b574e
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-11-21 06:01:05 +0100

    Bug 1317947 - land NSS 0x86638bd12ab0, r=me

--- security/nss/fuzz/clone_libfuzzer.sh
+++ security/nss/fuzz/clone_libfuzzer.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd $(dirname $0)
+
 mkdir tmp/
-git clone -q https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer tmp/
-mv tmp/.git libFuzzer/
-rm -fr tmp
-cd libFuzzer
-git reset --hard 1b543d6e5073b56be214394890c9193979a3d7e1
+git clone --no-checkout --depth 1 https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer tmp/
+(cd tmp && git reset --hard 1b543d6e5073b56be214394890c9193979a3d7e1)
+
+mkdir -p libFuzzer
+cp tmp/*.cpp tmp/*.h tmp/*.def libFuzzer
+rm -Rf tmp/
