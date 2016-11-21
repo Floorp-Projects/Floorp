e3b8cdc4b9302d2668210524435d2c1ae4ac96c0
Julian Seward <jseward@acm.org>
2016-11-21 08:49:36 +0100

    Bug 1318030 - Possible uninitialised value uses relating to security/pkix/test/gtest/pkixcert_extension_tests.cpp.  r=dkeeler@mozilla.com.

--- security/pkix/test/gtest/pkixcheck_CheckSignatureAlgorithm_tests.cpp
+++ security/pkix/test/gtest/pkixcheck_CheckSignatureAlgorithm_tests.cpp
@@ -46,6 +46,12 @@ struct CheckSignatureAlgorithmTestParams
   Result expectedResult;
 };
 
+::std::ostream& operator<<(::std::ostream& os,
+                           const CheckSignatureAlgorithmTestParams&)
+{
+  return os << "TODO (bug 1318770)";
+}
+
 #define BS(s) ByteString(s, MOZILLA_PKIX_ARRAY_LENGTH(s))
 
 // python DottedOIDToCode.py --tlv sha256WithRSAEncryption 1.2.840.113549.1.1.11
