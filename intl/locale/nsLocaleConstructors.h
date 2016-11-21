75596d3c32837123e7987d6e4a857ac4da4341b5
Gregory Moore <olucafont6@yahoo.com>
2016-11-21 15:58:37 -0800

    Bug 1301640 - Replaced per-platform variants of nsIDateTimeFormat with single, ICU-backed version that has static c++ callers. r=emk
    Also removed unused format selectors and removed option to pass in a locale. Changed all callers to use new version.
    
    MozReview-Commit-ID: Gq0b1c3C1eO

--- intl/locale/nsLocaleConstructors.h
+++ intl/locale/nsLocaleConstructors.h
@@ -7,7 +7,6 @@
 #define nsLocaleConstructors_h__
 
 #include "nsCollationCID.h"
-#include "nsDateTimeFormatCID.h"
 #include "mozilla/ModuleUtils.h"
 #include "nsILocaleService.h"
 #include "nsIScriptableDateFormat.h"
@@ -25,17 +24,14 @@
 
 #ifdef XP_WIN
 #include "windows/nsCollationWin.h"
-#include "windows/nsDateTimeFormatWin.h"
 #endif
 
 #ifdef USE_MAC_LOCALE
 #include "mac/nsCollationMacUC.h"
-#include "mac/nsDateTimeFormatMac.h"
 #endif
 
 #ifdef USE_UNIX_LOCALE
 #include "unix/nsCollationUnix.h"
-#include "unix/nsDateTimeFormatUnix.h"
 #endif
 
 #define NSLOCALE_MAKE_CTOR(ctor_, iface_, func_)          \
@@ -63,17 +59,14 @@ NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsPlatformCharset, Init)
 
 #ifdef XP_WIN
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsCollationWin)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsDateTimeFormatWin)
 #endif
 
 #ifdef USE_UNIX_LOCALE
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsCollationUnix)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsDateTimeFormatUnix)
 #endif  
 
 #ifdef USE_MAC_LOCALE
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsCollationMacUC)
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsDateTimeFormatMac)
 #endif  
 
 #endif
