8833589595ac2135cb23703f86578325152d6d6a
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-11-23 15:45:15 +0100

    Backed out changeset fe5975fad5d1 (bug 1319242) for bustage

--- js/src/jit-test/tests/ion/fold-linear-arith-bug1319242.js
+++
@@ -1,7 +0,0 @@
-function f(x) {
-    // Check that we do not fold +1 and -2 across truncated/non-truncated operations.
-    return (((x | 0) + 1) | 0) + -2;
-}
-const int32_min = -Math.pow(2,31);
-f(Infinity);
-assertEq(f(int32_min - 1), int32_min - 2);
