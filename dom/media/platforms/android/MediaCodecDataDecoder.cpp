d26fd597ff357a6ca6171a5946992bca7e649765
James Cheng <jacheng@mozilla.com>
2016-11-23 14:12:05 +0800

    Bug 1317239 - Part2 - Config the video decoder with adaptive playback feature if it is supported. r=jolin
    MozReview-Commit-ID: FORF2NNxgUH

--- dom/media/platforms/android/MediaCodecDataDecoder.cpp
+++ dom/media/platforms/android/MediaCodecDataDecoder.cpp
@@ -373,10 +373,19 @@ nsresult
 MediaCodecDataDecoder::InitDecoder(Surface::Param aSurface)
 {
   mDecoder = CreateDecoder(mMimeType);
+
   if (!mDecoder) {
     INVOKE_CALLBACK(Error,
                     MediaResult(NS_ERROR_DOM_MEDIA_FATAL_ERR, __func__));
     return NS_ERROR_FAILURE;
+  }
+
+  // Check if the video codec supports adaptive playback or not.
+  if (aSurface && java::HardwareCodecCapabilityUtils::CheckSupportsAdaptivePlayback(
+                    mDecoder, nsCString(TranslateMimeType(mMimeType)))) {
+      // TODO: may need to find a way to not use hard code to decide the max w/h.
+      mFormat->SetInteger(MediaFormat::KEY_MAX_WIDTH, 1920);
+      mFormat->SetInteger(MediaFormat::KEY_MAX_HEIGHT, 1080);
   }
 
   MediaCrypto::LocalRef crypto = MediaDrmProxy::GetMediaCrypto(mDrmStubId);
