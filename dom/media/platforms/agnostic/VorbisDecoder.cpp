7dacd679bdd396e78376124a14e1ed11491ff9fc
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-11-23 11:21:42 +1100

    Bug 1319543: Prevent overflow if sample's time is negative. r=gerald a=gchang
    MozReview-Commit-ID: I94vvJccOqE

--- dom/media/platforms/agnostic/VorbisDecoder.cpp
+++ dom/media/platforms/agnostic/VorbisDecoder.cpp
@@ -154,7 +154,7 @@ VorbisDataDecoder::DoDecode(MediaRawData* aSample)
   const unsigned char* aData = aSample->Data();
   size_t aLength = aSample->Size();
   int64_t aOffset = aSample->mOffset;
-  uint64_t aTstampUsecs = aSample->mTime;
+  int64_t aTstampUsecs = aSample->mTime;
   int64_t aTotalFrames = 0;
 
   MOZ_ASSERT(mPacketCount >= 3);
