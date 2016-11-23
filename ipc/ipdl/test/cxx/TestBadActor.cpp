ced5e33c0a851d9fac51d9c92bd3ae5996f4688c
Andrew McCreight <continuation@gmail.com>
2016-11-23 13:40:04 -0800

    Bug 1319910 - Crash child, not parent, on FatalError in TestActorPunning and TestBadActor. r=billm
    The parent process crashes if it gets a bad message from the child,
    but that makes it hard to test. This patch overrides the fatal error
    handling method and uses the old behavior, that kills the child.
    
    I copied the code to kill the child from TestHangs.
    
    MozReview-Commit-ID: 3YgqaCgHGI0
    
    --HG--
    extra : rebase_source : cbecee2742014e969c641b89833cff5f46b99a33

--- ipc/ipdl/test/cxx/TestBadActor.cpp
+++ ipc/ipdl/test/cxx/TestBadActor.cpp
@@ -19,6 +19,30 @@ TestBadActorParent::Main()
   Unused << child->Call__delete__(child);
 }
 
+// By default, fatal errors kill the parent process, but this makes it
+// hard to test, so instead we use the previous behavior and kill the
+// child process.
+void
+TestBadActorParent::HandleFatalError(const char* aProtocolName, const char* aErrorMsg) const
+{
+  if (!!strcmp(aProtocolName, "PTestBadActorSubParent")) {
+    fail("wrong protocol hit a fatal error");
+  }
+
+  if (!!strcmp(aErrorMsg, "incoming message racing with actor deletion")) {
+    fail("wrong fatal error");
+  }
+
+  ipc::ScopedProcessHandle otherProcessHandle;
+  if (!base::OpenProcessHandle(OtherPid(), &otherProcessHandle.rwget())) {
+    fail("couldn't open child process");
+  } else {
+    if (!base::KillProcess(otherProcessHandle, 0, false)) {
+      fail("terminating child process");
+    }
+  }
+}
+
 PTestBadActorSubParent*
 TestBadActorParent::AllocPTestBadActorSubParent()
 {
