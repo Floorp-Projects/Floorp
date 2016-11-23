a07194e1b348c03404860081839c272e7ba85232
Boris Zbarsky <bzbarsky@mit.edu>
2016-11-22 22:41:51 -0500

    Bug 1319255 part 1.  Stop doing casts to HTMLShadowElement* simply based on tag.  r=wchen a=jcristau

--- dom/base/nsINode.h
+++ dom/base/nsINode.h
@@ -1325,6 +1325,10 @@ public:
    */
   virtual bool IsNodeApzAwareInternal() const;
 
+  // HTML elements named <shadow> may or may not be HTMLShadowElement.  This is
+  // a way to ask an element whether it's an HTMLShadowElement.
+  virtual bool IsHTMLShadowElement() const { return false; }
+
 protected:
   nsIURI* GetExplicitBaseURI() const {
     if (HasExplicitBaseURI()) {
