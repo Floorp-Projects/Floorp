52b4c327ccfd9ae54a6d96b25e6425b3d29b2193
Julian Descottes <jdescottes@mozilla.com>
2016-11-24 16:12:16 +0100

    Bug 1319049 - fix Date previewer for worker debugging;r=ochameau
    MozReview-Commit-ID: Fy1Rp4dNv09
    
    --HG--
    extra : rebase_source : ab0aacc01abaf06b2985b2b73ee8562322f495b1

---
+++ devtools/client/debugger/test/mochitest/browser_dbg_worker-console-04.js
@@ -0,0 +1,23 @@
+// Check that the date previewer works in the console of a worker debugger.
+
+"use strict";
+
+const TAB_URL = EXAMPLE_URL + "doc_WorkerActor.attachThread-tab.html";
+const WORKER_URL = "code_WorkerActor.attachThread-worker.js";
+
+add_task(function* testPausedByConsole() {
+  let {client, tab, workerClient, toolbox} =
+    yield initWorkerDebugger(TAB_URL, WORKER_URL);
+
+  info("Check Date objects can be used in the console");
+  let jsterm = yield getSplitConsole(toolbox);
+  let executed = yield jsterm.execute("new Date(0)");
+  ok(executed.textContent.includes("1970-01-01T00:00:00.000Z"),
+      "Text for message appeared correct");
+
+  terminateWorkerInTab(tab, WORKER_URL);
+  yield waitForWorkerClose(workerClient);
+  yield gDevTools.closeToolbox(TargetFactory.forWorker(workerClient));
+  yield close(client);
+  yield removeTab(tab);
+});
