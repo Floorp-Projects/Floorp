af988a969a230ad3e2de998e60c587acfc6902fa
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-11-24 17:02:32 +0100

    Backed out changeset 4909935f8081 (bug 1319626)

--- gfx/layers/ipc/ShadowLayerParent.cpp
+++ gfx/layers/ipc/ShadowLayerParent.cpp
@@ -112,14 +112,6 @@ ShadowLayerParent::AsTextLayer() const
          : nullptr;
 }
 
-BorderLayer*
-ShadowLayerParent::AsBorderLayer() const
-{
-  return mLayer && mLayer->GetType() == Layer::TYPE_BORDER
-         ? static_cast<BorderLayer*>(mLayer.get())
-         : nullptr;
-}
-
 void
 ShadowLayerParent::ActorDestroy(ActorDestroyReason why)
 {
--- gfx/layers/ipc/ShadowLayers.h
+++ gfx/layers/ipc/ShadowLayers.h
@@ -210,7 +210,6 @@ public:
   void CreatedCanvasLayer(ShadowableLayer* aCanvas);
   void CreatedRefLayer(ShadowableLayer* aRef);
   void CreatedTextLayer(ShadowableLayer* aRef);
-  void CreatedBorderLayer(ShadowableLayer* aRef);
 
   /**
    * At least one attribute of |aMutant| has changed, and |aMutant|
