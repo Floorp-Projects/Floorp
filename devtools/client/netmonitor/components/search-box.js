c2d53f2ac5c35e7dcadb89691c80b7df2fc41f86
Fred Lin <gasolin@mozilla.com>
2016-11-24 17:05:31 +0800

    Bug 1317649 - Implement Cookies Panel;r=Honza,jsnajdr,rickychien
    use properties-view
    show other Cookie properties
    
    MozReview-Commit-ID: 7526Hm2ORbL

--- devtools/client/netmonitor/components/search-box.js
+++ devtools/client/netmonitor/components/search-box.js
@@ -8,18 +8,16 @@ const { connect } = require("devtools/client/shared/vendor/react-redux");
 const SearchBox = require("devtools/client/shared/components/search-box");
 const { L10N } = require("../l10n");
 const Actions = require("../actions/index");
-const { FREETEXT_FILTER_SEARCH_DELAY } = require("../constants");
+const { FILTER_SEARCH_DELAY } = require("../constants");
 
 module.exports = connect(
   (state) => ({
-    delay: FREETEXT_FILTER_SEARCH_DELAY,
+    delay: FILTER_SEARCH_DELAY,
     keyShortcut: L10N.getStr("netmonitor.toolbar.filterFreetext.key"),
     placeholder: L10N.getStr("netmonitor.toolbar.filterFreetext.label"),
     type: "filter",
   }),
   (dispatch) => ({
-    onChange: (url) => {
-      dispatch(Actions.setRequestFilterText(url));
-    },
+    onChange: (text) => dispatch(Actions.setRequestFilterText(text)),
   })
 )(SearchBox);
