cea1c78d52ec88fb16dc558b3f4f8359d8782743
Thomas Wisniewski <wisniewskit@gmail.com>
2016-11-24 23:23:14 -0500

    Bug 943918 - Part 1: style system support for tab-size:<length> and tab-size:<number>. r=heycam

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -3946,7 +3946,7 @@ already_AddRefed<CSSValue>
 nsComputedDOMStyle::DoGetTabSize()
 {
   RefPtr<nsROCSSPrimitiveValue> val = new nsROCSSPrimitiveValue;
-  val->SetNumber(StyleText()->mTabSize);
+  SetValueToCoord(val, StyleText()->mTabSize, true);
   return val.forget();
 }
 
