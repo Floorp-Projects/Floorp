eea152f73a83602cd6d44f6abf3ae2ad3b44fc02
Timothy Nikkel <tnikkel@gmail.com>
2016-11-26 01:56:27 -0600

    Bug 1317562. Make nsTreeBodyFrame use async image notifications during painting. r=aosmond

--- layout/xul/tree/nsTreeBodyFrame.cpp
+++ layout/xul/tree/nsTreeBodyFrame.cpp
@@ -2205,7 +2205,7 @@ nsTreeBodyFrame::GetImage(int32_t aRowIndex, nsTreeColumn* aCol, bool aUseContex
       return NS_ERROR_FAILURE;
 
     // We don't want discarding/decode-on-draw for xul images
-    imageRequest->StartDecoding(imgIContainer::FLAG_NONE);
+    imageRequest->StartDecoding(imgIContainer::FLAG_ASYNC_NOTIFY);
     imageRequest->LockImage();
 
     // In a case it was already cached.
