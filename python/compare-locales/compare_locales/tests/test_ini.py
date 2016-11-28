0643a8fe290dae30d9172f6e0dc8211458d31845
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-11-28 14:19:47 +0100

    Backed out changeset 626c5d74b74c (bug 1315976) for breaking Mac L10n nightlies. r=backout a=backout
    MozReview-Commit-ID: 8mie8tWYrcz

--- python/compare-locales/compare_locales/tests/test_ini.py
+++ python/compare-locales/compare_locales/tests/test_ini.py
@@ -23,30 +23,23 @@ class TestIniParser(ParserTestMixin, unittest.TestCase):
         self._test('''; This file is in the UTF-8 encoding
 [Strings]
 TitleText=Some Title
-''', (
-            ('Comment', 'UTF-8 encoding'),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('UTF-8' in self.parser.header)
 
     def testMPL2_Space_UTF(self):
         self._test(mpl2 + '''
 ; This file is in the UTF-8 encoding
 [Strings]
 TitleText=Some Title
-''', (
-            ('Comment', mpl2),
-            ('Comment', 'UTF-8'),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('MPL' in self.parser.header)
 
     def testMPL2_Space(self):
         self._test(mpl2 + '''
 [Strings]
 TitleText=Some Title
-''', (
-            ('Comment', mpl2),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('MPL' in self.parser.header)
 
     def testMPL2_MultiSpace(self):
         self._test(mpl2 + '''\
@@ -55,33 +48,26 @@ TitleText=Some Title
 
 [Strings]
 TitleText=Some Title
-''', (
-            ('Comment', mpl2),
-            ('Comment', 'more comments'),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('MPL' in self.parser.header)
 
     def testMPL2_JunkBeforeCategory(self):
         self._test(mpl2 + '''\
 Junk
 [Strings]
 TitleText=Some Title
-''', (
-            ('Comment', mpl2),
-            ('Junk', 'Junk'),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title')))
+''', (('_junk_\\d+_0-213$', mpl2 + '''\
+Junk
+[Strings]'''), ('TitleText', 'Some Title')))
+        self.assert_('MPL' not in self.parser.header)
 
     def test_TrailingComment(self):
         self._test(mpl2 + '''
 [Strings]
 TitleText=Some Title
 ;Stray trailing comment
-''', (
-            ('Comment', mpl2),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),
-            ('Comment', 'Stray trailing')))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('MPL' in self.parser.header)
 
     def test_SpacedTrailingComments(self):
         self._test(mpl2 + '''
@@ -91,11 +77,8 @@ TitleText=Some Title
 ;Stray trailing comment
 ;Second stray comment
 
-''', (
-            ('Comment', mpl2),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),
-            ('Comment', 'Second stray comment')))
+''', (('TitleText', 'Some Title'),))
+        self.assert_('MPL' in self.parser.header)
 
     def test_TrailingCommentsAndJunk(self):
         self._test(mpl2 + '''
@@ -106,13 +89,14 @@ TitleText=Some Title
 Junk
 ;Second stray comment
 
-''', (
-            ('Comment', mpl2),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),
-            ('Comment', 'Stray trailing'),
-            ('Junk', 'Junk'),
-            ('Comment', 'Second stray comment')))
+''', (('TitleText', 'Some Title'), ('_junk_\\d+_231-284$', '''\
+
+;Stray trailing comment
+Junk
+;Second stray comment
+
+''')))
+        self.assert_('MPL' in self.parser.header)
 
     def test_JunkInbetweenEntries(self):
         self._test(mpl2 + '''
@@ -122,18 +106,10 @@ TitleText=Some Title
 Junk
 
 Good=other string
-''', (
-            ('Comment', mpl2),
-            ('IniSection', 'Strings'),
-            ('TitleText', 'Some Title'),
-            ('Junk', 'Junk'),
-            ('Good', 'other string')))
+''', (('TitleText', 'Some Title'), ('_junk_\\d+_231-236$', '''\
 
-    def test_empty_file(self):
-        self._test('', tuple())
-        self._test('\n', (('Whitespace', '\n'),))
-        self._test('\n\n', (('Whitespace', '\n\n'),))
-        self._test(' \n\n', (('Whitespace', ' \n\n'),))
+Junk'''), ('Good', 'other string')))
+        self.assert_('MPL' in self.parser.header)
 
 if __name__ == '__main__':
     unittest.main()
