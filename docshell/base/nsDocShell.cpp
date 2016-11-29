6986dfa2d4b653ece08667bbc8dc54f30f181e66
Timothy Nikkel <tnikkel@gmail.com>
2016-11-29 15:15:35 -0600

    Bug 1312609. r=smaug a=abillings
    --HG--
    extra : source : 65fa05989b391912d3c69d0b693dbd03a87cb1b0
    extra : intermediate-source : be3e241d379879d78657bd09257d4ec87d0c64e9

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -8926,6 +8926,8 @@ nsDocShell::RestoreFromHistory()
     nsSubDocumentFrame* subDocFrame =
       do_QueryFrame(container->GetPrimaryFrame());
     rootViewParent = subDocFrame ? subDocFrame->EnsureInnerView() : nullptr;
+  } else {
+    rootViewParent = nullptr;
   }
   if (sibling &&
       sibling->GetShell() &&
