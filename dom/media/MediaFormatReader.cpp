2d2f757ab6d1e992170fc41db04dde634bc64489
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-11-30 01:42:50 +1100

    Bug 1319992: P3. Remove no longer used seeking argument. r=jwwang
    MozReview-Commit-ID: 9Oc0SHX0Rw2

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -2277,7 +2277,7 @@ MediaFormatReader::OnVideoSkipFailed(MediaTrackDemuxer::SkipFailureHolder aFailu
 }
 
 RefPtr<MediaDecoderReader::SeekPromise>
-MediaFormatReader::Seek(const SeekTarget& aTarget, int64_t aUnused)
+MediaFormatReader::Seek(const SeekTarget& aTarget)
 {
   MOZ_ASSERT(OnTaskQueue());
 
