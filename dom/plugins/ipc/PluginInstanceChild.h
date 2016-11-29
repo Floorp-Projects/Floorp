6d2b3930d481b8714c40d4ee7391f62f4732b5af
Chris Peterson <cpeterson@mozilla.com>
2016-11-28 22:52:18 -0800

    Bug 1320020 - Remove Unity plugin quirk compatibility. r=aklotz

--- dom/plugins/ipc/PluginInstanceChild.h
+++ dom/plugins/ipc/PluginInstanceChild.h
@@ -318,8 +318,6 @@ private:
     void SetupFlashMsgThrottle();
     void UnhookWinlessFlashThrottle();
     void HookSetWindowLongPtr();
-    void SetUnityHooks();
-    void ClearUnityHooks();
     void InitImm32Hook();
     static inline bool SetWindowLongHookCheck(HWND hWnd,
                                                 int nIndex,
@@ -340,9 +338,6 @@ private:
                                           int nReserved,
                                           HWND hWnd,
                                           CONST RECT *prcRect);
-    static HWND WINAPI SetCaptureHook(HWND aHwnd);
-    static LRESULT CALLBACK UnityGetMessageHookProc(int aCode, WPARAM aWparam, LPARAM aLParam);
-    static LRESULT CALLBACK UnitySendMessageHookProc(int aCode, WPARAM aWparam, LPARAM aLParam);
     static BOOL CALLBACK EnumThreadWindowsCallback(HWND hWnd,
                                                    LPARAM aParam);
     static LRESULT CALLBACK WinlessHiddenFlashWndProc(HWND hWnd,
@@ -475,8 +470,6 @@ private:
     nsIntPoint mPluginSize;
     WNDPROC mWinlessThrottleOldWndProc;
     HWND mWinlessHiddenMsgHWND;
-    HHOOK mUnityGetMessageHook;
-    HHOOK mUnitySendMessageHook;
 #endif
 
     friend class ChildAsyncCall;
