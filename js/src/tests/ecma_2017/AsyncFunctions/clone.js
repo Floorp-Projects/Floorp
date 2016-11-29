a4999f2bd1f1da736e1fe465372233c79b3749cf
Tooru Fujisawa <arai_a@mac.com>
2016-11-29 09:08:44 +0900

    Bug 1320697 - Remove wrong assertion in jsscript.cpp. r=till

--- js/src/tests/ecma_2017/AsyncFunctions/clone.js
+++ js/src/tests/ecma_2017/AsyncFunctions/clone.js
@@ -1,7 +1,22 @@
-// |reftest| skip-if(!xulRuntime.shell) -- needs clone
+// |reftest| skip-if(!xulRuntime.shell) -- needs clone, cloneAndExecuteScript, drainJobQueue
 
 // Async function cannot be cloned.
 assertThrowsInstanceOf(() => clone(async function f() {}), TypeError);
+
+// unwrapped async function can be cloned.
+let g = newGlobal();
+cloneAndExecuteScript(`
+async function f() {
+  var a = await 1;
+  var b = await 2;
+  var c = await 3;
+  return a + b + c;
+}
+var V;
+f().then(v => V = v);
+drainJobQueue();
+`, g);
+assertEq(g.V, 6);
 
 if (typeof reportCompare === "function")
     reportCompare(true, true);
