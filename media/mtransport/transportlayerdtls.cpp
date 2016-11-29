64ab60e9d95d23730b2a400e85ee5fa2a967419d
Martin Thomson <martin.thomson@gmail.com>
2016-11-29 14:00:29 +1100

    Bug 1316261 - Configure trust anchors list to be empty for WebRTC, r=drno, a=gchang
    MozReview-Commit-ID: CEdShfGj7tE

--- media/mtransport/transportlayerdtls.cpp
+++ media/mtransport/transportlayerdtls.cpp
@@ -514,6 +514,13 @@ bool TransportLayerDtls::Setup() {
       return false;
     }
 
+    UniqueCERTCertList zero_certs(CERT_NewCertList());
+    rv = SSL_SetTrustAnchors(ssl_fd.get(), zero_certs.get());
+    if (rv != SECSuccess) {
+        MOZ_MTLOG(ML_ERROR, "Couldn't set trust anchors");
+        return false;
+    }
+
     // Insist on a certificate from the client
     rv = SSL_OptionSet(ssl_fd, SSL_REQUEST_CERTIFICATE, PR_TRUE);
     if (rv != SECSuccess) {
