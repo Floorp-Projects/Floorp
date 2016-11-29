d3d81a00f7d15c6624f274f5f2ae28c4dbbe6d9b
Jan de Mooij <jdemooij@mozilla.com>
2016-11-29 11:59:25 +0100

    Bug 1319524 - Add JSAutoCompartment to GetNPObjectWrapper. r=bholley, a=ritu

--- dom/plugins/base/nsJSNPRuntime.cpp
+++ dom/plugins/base/nsJSNPRuntime.cpp
@@ -1185,6 +1185,8 @@ GetNPObjectWrapper(JSContext *cx, JSObject *aObj, bool wrapResult = true)
       }
       return obj;
     }
+
+    JSAutoCompartment ac(cx, obj);
     if (!::JS_GetPrototype(cx, obj, &obj)) {
       return nullptr;
     }
