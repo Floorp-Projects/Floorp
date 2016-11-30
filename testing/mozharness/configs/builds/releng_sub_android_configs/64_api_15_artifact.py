28e3b417b341abbc912b863191331213f430a9f9
Nick Alexander <nalexander@mozilla.com>
2016-11-29 20:10:31 -0800

    Bug 1285608 - Part 1: Add Android moz.build and Gradle artifact build configs. r=maja_zf
    MozReview-Commit-ID: AXh5ueeUs38

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_artifact.py
@@ -0,0 +1,8 @@
+config = {
+    'base_name': 'Android armv7 API 15+ %(branch)s Artifact build',
+    'stage_platform': 'android-api-15',
+    'build_type': 'api-15-opt-artifact',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-15/nightly-artifact',
+    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android/releng.manifest',
+    'multi_locale_config_platform': 'android',
+}
--- testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_artifact.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_artifact.py
@@ -1,9 +1,8 @@
 config = {
-    'base_name': 'Android 4.2 x86 %(branch)s build',
+    'base_name': 'Android 4.2 x86 %(branch)s Artifact build',
     'stage_platform': 'android-x86',
     'publish_nightly_en_US_routes': False,
     'build_type': 'x86-opt',
-    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86/nightly',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86/nightly-artifact',
     'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android-x86/releng.manifest',
-    'artifact_flag_build_variant_in_try': 'x86-artifact',
 }
