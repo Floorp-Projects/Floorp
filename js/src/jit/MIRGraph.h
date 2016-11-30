e1cb5f1e5a3e0631b0ff38baf2488b0509c078d8
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2016-11-30 15:08:30 +0000

    Bug 1319888 - MBasicBlock::NewSplitEdge: use the successor info instead of the predecessor info. r=h4writer

--- js/src/jit/MIRGraph.h
+++ js/src/jit/MIRGraph.h
@@ -117,9 +117,8 @@ class MBasicBlock : public TempObject, public InlineListNode<MBasicBlock>
     static MBasicBlock* NewPendingLoopHeader(MIRGraph& graph, const CompileInfo& info,
                                              MBasicBlock* pred, BytecodeSite* site,
                                              unsigned loopStateSlots);
-    static MBasicBlock* NewSplitEdge(MIRGraph& graph, const CompileInfo& info,
-                                     MBasicBlock* pred, size_t predEdgeIdx,
-                                     MBasicBlock* succ);
+    static MBasicBlock* NewSplitEdge(MIRGraph& graph, MBasicBlock* pred,
+                                     size_t predEdgeIdx, MBasicBlock* succ);
 
     bool dominates(const MBasicBlock* other) const {
         return other->domIndex() - domIndex() < numDominated();
