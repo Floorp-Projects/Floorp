2e920ec7291683ff0b7111267a3bd7bb59b23229
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-11-30 17:24:45 +1100

    Bug 1320705: P2. Add mDiscardPadding information. r=kinetik a=jcristau
    The WebMDemuxer added this information in the ExtraData field. However, this is incompatible with the MSE demuxer which will override the ExtraData field under some circumstances.
    It's cleaner to have a dedicated field anyway.
    
    MozReview-Commit-ID: GadD11zzZ8f

--- dom/media/MediaData.h
+++ dom/media/MediaData.h
@@ -639,6 +639,10 @@ public:
   // Indicates that this is the last packet of the stream.
   bool mEOS = false;
 
+  // Indicate to the audio decoder that mDiscardPadding frames should be
+  // trimmed.
+  uint32_t mDiscardPadding = 0;
+
   RefPtr<SharedTrackInfo> mTrackInfo;
 
   // Return a deep copy or nullptr if out of memory.
