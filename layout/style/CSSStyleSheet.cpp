2fe9cf45bfe33837071b1bc08118450f2612009c
Boris Zbarsky <bzbarsky@mit.edu>
2016-11-30 18:18:14 -0500

    Bug 851892 part 1.  Make all CSS rules cycle-collected.  r=heycam,smaug

--- layout/style/CSSStyleSheet.cpp
+++ layout/style/CSSStyleSheet.cpp
@@ -509,8 +509,10 @@ CSSStyleSheet::TraverseInner(nsCycleCollectionTraversalCallback &cb)
 
   const nsCOMArray<css::Rule>& rules = mInner->mOrderedRules;
   for (int32_t i = 0, count = rules.Count(); i < count; ++i) {
-    NS_CYCLE_COLLECTION_NOTE_EDGE_NAME(cb, "mOrderedRules[i]");
-    cb.NoteXPCOMChild(rules[i]->GetExistingDOMRule());
+    if (!rules[i]->IsCCLeaf()) {
+      NS_CYCLE_COLLECTION_NOTE_EDGE_NAME(cb, "mOrderedRules[i]");
+      cb.NoteXPCOMChild(rules[i]);
+    }
   }
 }
 
