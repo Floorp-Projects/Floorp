84330a08acd0db8b5cae38bae912741dc5308954
Chris Pearce <cpearce@mozilla.com>
2016-12-01 11:23:27 +1300

    Bug 1339755 - Move DecryptJob id calculation into its constructor. r=jwwang
    The job id is just a counter, so rather than have other users of DecryptJob
    reimplement their own counter, we can push the id/counter code into DecryptJob
    itself.
    
    MozReview-Commit-ID: 3RB8ctplWkK

--- dom/media/gmp/DecryptJob.cpp
+++ dom/media/gmp/DecryptJob.cpp
@@ -4,8 +4,23 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "DecryptJob.h"
+#include <atomic>
 
 namespace mozilla {
+
+std::atomic<uint32_t> sDecryptJobInstanceCount = 0;
+
+DecryptJob::DecryptJob(MediaRawData* aSample)
+  : mId(++sDecryptJobInstanceCount )
+  , mSample(aSample)
+{
+}
+
+RefPtr<DecryptPromise>
+DecryptJob::Ensure()
+{
+  return mPromise.Ensure(__func__);
+}
 
 void
 DecryptJob::PostResult(DecryptStatus aResult)
