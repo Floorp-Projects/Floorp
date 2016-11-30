8bf71660ffa32c29f19d60d860edb4f45bf6005d
Kearwood Gilbert <kgilbert@mozilla.com>
2016-11-30 14:24:29 -0800

    Bug 1318468 - Update VR controller inputs on every VR Vsync rather than every 2D vsync r=daoshengmu
    MozReview-Commit-ID: ICuIw4e56VC

--- gfx/vr/VRManager.cpp
+++ gfx/vr/VRManager.cpp
@@ -181,9 +181,6 @@ VRManager::NotifyVsync(const TimeStamp& aVsyncTimestamp)
   }
 
   if (bHaveEventListener) {
-    for (uint32_t i = 0; i < mControllerManagers.Length(); ++i) {
-      mControllerManagers[i]->HandleInput();
-    }
     // If content has set an EventHandler to be notified of VR display events
     // we must continually refresh the VR display enumeration to check
     // for events that we must fire such as Window.onvrdisplayconnect
@@ -211,6 +208,9 @@ VRManager::NotifyVsync(const TimeStamp& aVsyncTimestamp)
 void
 VRManager::NotifyVRVsync(const uint32_t& aDisplayID)
 {
+  for (uint32_t i = 0; i < mControllerManagers.Length(); ++i) {
+    mControllerManagers[i]->HandleInput();
+  }
   for (auto iter = mVRManagerParents.Iter(); !iter.Done(); iter.Next()) {
     Unused << iter.Get()->GetKey()->SendNotifyVRVSync(aDisplayID);
   }
