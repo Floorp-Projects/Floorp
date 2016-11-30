d7270bee6614858e82bd3271fac4df481ba03989
Jason Orendorff <jorendorff@mozilla.com>
2016-11-30 15:31:56 -0600

    Bug 1312001 - Scramble hash codes securely, to avoid leaking bits of object and symbol addresses. r=jandem, a=lizzard
    MozReview-Commit-ID: yR1cIjrlPP

--- js/src/jscompartment.cpp
+++ js/src/jscompartment.cpp
@@ -76,6 +76,7 @@ JSCompartment::JSCompartment(Zone* zone, const JS::CompartmentOptions& options =
     nonSyntacticLexicalEnvironments_(nullptr),
     gcIncomingGrayPointers(nullptr),
     debugModeBits(0),
+    randomKeyGenerator_(runtime_->forkRandomKeyGenerator()),
     watchpointMap(nullptr),
     scriptCountsMap(nullptr),
     debugScriptMap(nullptr),
@@ -1283,6 +1284,13 @@ JSCompartment::addTelemetry(const char* filename, DeprecatedLanguageExtension e)
         return;
 
     sawDeprecatedLanguageExtension[e] = true;
+}
+
+mozilla::HashCodeScrambler
+JSCompartment::randomHashCodeScrambler()
+{
+    return mozilla::HashCodeScrambler(randomKeyGenerator_.next(),
+                                      randomKeyGenerator_.next());
 }
 
 AutoSetNewObjectMetadata::AutoSetNewObjectMetadata(ExclusiveContext* ecx
