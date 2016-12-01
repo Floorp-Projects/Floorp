cb910388ab9083ffcb8483b7c2e7e59f72ab358c
Blake Kaplan <mrbkap@gmail.com>
2016-11-30 18:00:06 -0800

    Bug 1330822 - Remove CPOWs from browser_loadURI.js. r=Felipe, a=test-only

--- docshell/test/browser/browser_loadURI.js
+++ docshell/test/browser/browser_loadURI.js
@@ -25,21 +25,10 @@ function test() {
   postStream.setData(dataStream);
 
   tab.linkedBrowser.loadURIWithFlags("http://mochi.test:8888/browser/docshell/test/browser/print_postdata.sjs", 0, null, null, postStream);
-  onTabLoad(tab, function (doc) {
-    var bodyText = doc.body.textContent;
-    is(bodyText, gPostData, "post data was submitted correctly");
-    finish();
+  BrowserTestUtils.browserLoaded(tab.linkedBrowser).then(() => {
+    ContentTask.spawn(tab.linkedBrowser, gPostData, function(postData) {
+      var bodyText = content.document.body.textContent;
+      is(bodyText, postData, "post data was submitted correctly");
+    }).then(() => { finish(); });
   });
-}
-
-function onTabLoad(tab, cb) {
-  tab.linkedBrowser.addEventListener("load", function listener(event) {
-    if (event.originalTarget != tab.linkedBrowser.contentDocument ||
-        event.target.location.href == "about:blank") {
-      info("skipping spurious load event");
-      return;
-    }
-    tab.linkedBrowser.removeEventListener("load", listener, true);
-    cb(tab.linkedBrowser.contentDocument);
-  }, true);
 }
