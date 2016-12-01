4cdcae01af6d26b157363d03dcc78fd43f4a5594
Ethan Lin <ethlin@mozilla.com>
2016-12-01 11:22:41 +0800

    Bug 1321231 - Fix the overflow rect of WebRenderLayer. r=nical

--- gfx/layers/wr/WebRenderPaintedLayer.cpp
+++ gfx/layers/wr/WebRenderPaintedLayer.cpp
@@ -71,10 +71,11 @@ WebRenderPaintedLayer::RenderLayer()
   Manager()->AddImageKeyForDiscard(key);
 
   Rect relBounds = TransformedVisibleBoundsRelativeToParent();
+  Rect overflow(0, 0, relBounds.width, relBounds.height);
   Matrix4x4 transform;// = GetTransform();
   if (gfxPrefs::LayersDump()) printf_stderr("PaintedLayer %p using %s as bounds/overflow, %s for transform\n", this, Stringify(relBounds).c_str(), Stringify(transform).c_str());
   WRBridge()->AddWebRenderCommand(
-    OpPopDLBuilder(toWrRect(relBounds), toWrRect(relBounds), transform, FrameMetrics::NULL_SCROLL_ID));
+      OpPopDLBuilder(toWrRect(relBounds), toWrRect(overflow), transform, FrameMetrics::NULL_SCROLL_ID));
 }
 
 } // namespace layers
