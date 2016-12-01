b4ff4ad7eac6d254ec0e6c06c744e4eb644d9d66
Ms2ger <Ms2ger@gmail.com>
2016-12-01 09:23:21 +0100

    Bug 1319831 - Clarify some code with explicit casts; r=Waldo

--- js/public/Id.h
+++ js/public/Id.h
@@ -78,7 +78,8 @@ static MOZ_ALWAYS_INLINE int32_t
 JSID_TO_INT(jsid id)
 {
     MOZ_ASSERT(JSID_IS_INT(id));
-    return ((uint32_t)JSID_BITS(id)) >> 1;
+    uint32_t bits = static_cast<uint32_t>(JSID_BITS(id)) >> 1;
+    return static_cast<int32_t>(bits);
 }
 
 #define JSID_INT_MIN  0
@@ -95,7 +96,8 @@ INT_TO_JSID(int32_t i)
 {
     jsid id;
     MOZ_ASSERT(INT_FITS_IN_JSID(i));
-    JSID_BITS(id) = ((i << 1) | JSID_TYPE_INT);
+    uint32_t bits = (static_cast<uint32_t>(i) << 1) | JSID_TYPE_INT;
+    JSID_BITS(id) = static_cast<size_t>(bits);
     return id;
 }
 
