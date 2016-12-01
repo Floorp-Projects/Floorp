ac1f0ecad6caf04c137bc2a332a6865e977e94ba
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-12-01 17:19:12 +0100

    Backed out changeset 8c4dcbaa44b9 (bug 1321525) for failing test_fileapi.html and test_fileReader.html. r=backout on a CLOSED TREE

--- dom/file/FileReader.cpp
+++ dom/file/FileReader.cpp
@@ -349,18 +349,21 @@ FileReader::ReadFileContent(Blob& aBlob,
                             eDataFormat aDataFormat,
                             ErrorResult& aRv)
 {
+  //Implicit abort to clear any other activity going on
+  ErrorResult error;
+  Abort(error);
+  error.SuppressException();
+
   if (mReadyState == LOADING) {
-    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
+    // A nested ReadAsSomething() as been called during one of the events
+    // dispatched by Abort(). We have to terminate this operation in order to
+    // continue the nested one.
+    aRv.Throw(NS_ERROR_ABORT);
     return;
   }
 
   mError = nullptr;
-
   SetDOMStringToNull(mResult);
-  mResultArrayBuffer = nullptr;
-
-  mAsyncStream = nullptr;
-
   mTransferred = 0;
   mTotal = 0;
   mReadyState = EMPTY;
