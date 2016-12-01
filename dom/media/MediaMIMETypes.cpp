14cc7d114da5aa5b7face2672a74a03088a85afa
Gerald Squelart <gsquelart@mozilla.com>
2016-12-01 13:05:45 +1100

    Bug 1329568 - MediaExtendedMIMEType - r=jya
    This patch factors out all data handling of MIME strings from MediaContentType
    to MediaExtendedMIMEType.
    
    MediaExtendedMIMEType is pretty much a copy of the old MediaContentType, as the
    functionality was fine (but will be modified in upcoming patches).
    MediaContentType then just delegates the work to its embedded
    MediaExtendedMIMEType field.
    The main difference is that the default constructor and Populate() method have
    been replaced with a single constructor that takes all the arguments at once.
    
    MozReview-Commit-ID: GBAgPDT2DUW
    
    --HG--
    rename : dom/media/MediaContentType.cpp => dom/media/MediaMIMETypes.cpp
    rename : dom/media/MediaContentType.h => dom/media/MediaMIMETypes.h
    extra : rebase_source : 1c925d8e049d9d349ec4c3dd1a079f570b809970

---
+++ dom/media/MediaMIMETypes.cpp
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "MediaMIMETypes.h"
+
+#include "nsContentTypeParser.h"
+
+namespace mozilla {
+
+static int32_t
+GetParameterAsNumber(const nsContentTypeParser& aParser,
+                     const char* aParameter,
+                     const int32_t aErrorReturn)
+{
+  nsAutoString parameterString;
+  nsresult rv = aParser.GetParameter(aParameter, parameterString);
+  if (NS_FAILED_impl(rv)) {
+    return aErrorReturn;
+  }
+  int32_t number = parameterString.ToInteger(&rv);
+  if (MOZ_UNLIKELY(NS_FAILED_impl(rv))) {
+    return aErrorReturn;
+  }
+  return number;
+}
+
+MediaExtendedMIMEType::MediaExtendedMIMEType(const nsACString& aMIMEType,
+                                             bool aHaveCodecs,
+                                             const nsAString& aCodecs,
+                                             int32_t aWidth, int32_t aHeight,
+                                             int32_t aFramerate, int32_t aBitrate)
+  : mMIMEType(Move(aMIMEType))
+  , mHaveCodecs(aHaveCodecs)
+  , mCodecs(Move(aCodecs))
+  , mWidth(aWidth)
+  , mHeight(aHeight)
+  , mFramerate(aFramerate)
+  , mBitrate(aBitrate)
+{
+}
+
+Maybe<MediaExtendedMIMEType>
+MakeMediaExtendedMIMEType(const nsAString& aType)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  nsContentTypeParser parser(aType);
+  nsAutoString mime;
+  nsresult rv = parser.GetType(mime);
+  if (!NS_SUCCEEDED(rv) || mime.IsEmpty()) {
+    return Nothing();
+  }
+
+  NS_ConvertUTF16toUTF8 mime8{mime};
+
+  nsAutoString codecs;
+  rv = parser.GetParameter("codecs", codecs);
+  bool haveCodecs = NS_SUCCEEDED(rv);
+
+  int32_t width = GetParameterAsNumber(parser, "width", -1);
+  int32_t height = GetParameterAsNumber(parser, "height", -1);
+  int32_t framerate = GetParameterAsNumber(parser, "framerate", -1);
+  int32_t bitrate = GetParameterAsNumber(parser, "bitrate", -1);
+
+  return Some(MediaExtendedMIMEType(mime8,
+                                    haveCodecs, codecs,
+                                    width, height,
+                                    framerate, bitrate));
+}
+
+Maybe<MediaExtendedMIMEType>
+MakeMediaExtendedMIMEType(const nsACString& aType)
+{
+  return MakeMediaExtendedMIMEType(NS_ConvertUTF8toUTF16(aType));
+}
+
+Maybe<MediaExtendedMIMEType>
+MakeMediaExtendedMIMEType(const char* aType)
+{
+  if (!aType) {
+    return Nothing();
+  }
+  return MakeMediaExtendedMIMEType(nsDependentCString(aType));
+}
+
+} // namespace mozilla
