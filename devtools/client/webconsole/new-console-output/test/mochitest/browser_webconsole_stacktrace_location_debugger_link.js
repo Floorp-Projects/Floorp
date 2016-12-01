6c17e99d3621b051cdd4b68c4b9346e20fefde4a
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2016-12-01 08:29:43 +0100

    Bug 1318796 - Open the appropriate tool when clicking on a location in the new console; r=jdescottes
    We were always opening the debugger when clicking on the location link,
    which could be wrong since we can have CSS warnings, or logs coming from Scratchpad.
    So we add the function to open in StyleEditor and Scratchpad, and copy some mochitests
    that where testing the interaction with those links.
    
    
    MozReview-Commit-ID: 73mQNfy199m
    
    --HG--
    rename : devtools/client/webconsole/test/browser_webconsole_bug_766001_JS_Console_in_Debugger.js => devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_location_debugger_link.js
    rename : devtools/client/webconsole/test/browser_webconsole_scratchpad_panel_link.js => devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_location_scratchpad_link.js
    rename : devtools/client/webconsole/test/browser_webconsole_bug_782653_CSS_links_in_Style_Editor.js => devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_location_styleeditor_link.js
    rename : devtools/client/webconsole/test/test-bug-766001-console-log.js => devtools/client/webconsole/new-console-output/test/mochitest/test-location-debugger-link-console-log.js
    rename : devtools/client/webconsole/test/test-bug-766001-js-errors.js => devtools/client/webconsole/new-console-output/test/mochitest/test-location-debugger-link-errors.js
    rename : devtools/client/webconsole/test/test-bug-766001-js-console-links.html => devtools/client/webconsole/new-console-output/test/mochitest/test-location-debugger-link.html
    rename : devtools/client/webconsole/test/test-bug-782653-css-errors-1.css => devtools/client/webconsole/new-console-output/test/mochitest/test-location-styleeditor-link-1.css
    rename : devtools/client/webconsole/test/test-bug-782653-css-errors-2.css => devtools/client/webconsole/new-console-output/test/mochitest/test-location-styleeditor-link-2.css
    rename : devtools/client/webconsole/test/test-bug-782653-css-errors.html => devtools/client/webconsole/new-console-output/test/mochitest/test-location-styleeditor-link.html
    extra : rebase_source : a8494acb5e450f86fa9cddd34515265fc3494d17

---
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_stacktrace_location_debugger_link.js
@@ -0,0 +1,63 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that message source links for js errors and console API calls open in
+// the jsdebugger when clicked.
+
+"use strict";
+
+const TEST_URI = "http://example.com/browser/devtools/client/webconsole/" +
+  "new-console-output/test/mochitest/test-stacktrace-location-debugger-link.html";
+
+add_task(function* () {
+  // Force the new debugger UI, in case this gets uplifted with the old
+  // debugger still turned on
+  Services.prefs.setBoolPref("devtools.debugger.new-debugger-frontend", true);
+  Services.prefs.setBoolPref("devtools.webconsole.filter.log", true);
+  registerCleanupFunction(function* () {
+    Services.prefs.clearUserPref("devtools.debugger.new-debugger-frontend");
+    Services.prefs.clearUserPref("devtools.webconsole.filter.log");
+  });
+
+  let hud = yield openNewTabAndConsole(TEST_URI);
+  let target = TargetFactory.forTab(gBrowser.selectedTab);
+  let toolbox = gDevTools.getToolbox(target);
+
+  yield testOpenInDebugger(hud, toolbox, "console.trace()");
+});
+
+function* testOpenInDebugger(hud, toolbox, text) {
+  info(`Testing message with text "${text}"`);
+  let messageNode = yield waitFor(() => findMessage(hud, text));
+  let frameLinksNode = messageNode.querySelectorAll(".stack-trace .frame-link");
+  is(frameLinksNode.length, 3,
+    "The message does have the expected number of frames in the stacktrace");
+
+  for (let frameLinkNode of frameLinksNode) {
+    yield checkClickOnNode(hud, toolbox, frameLinkNode);
+
+    info("Selecting the console again");
+    yield toolbox.selectTool("webconsole");
+  }
+}
+
+function* checkClickOnNode(hud, toolbox, frameLinkNode) {
+  info("checking click on node location");
+
+  let onSourceInDebuggerOpened = once(hud.ui, "source-in-debugger-opened");
+
+  EventUtils.sendMouseEvent({ type: "click" },
+    frameLinkNode.querySelector(".frame-link-source"));
+
+  yield onSourceInDebuggerOpened;
+
+  let url = frameLinkNode.getAttribute("data-url");
+  let dbg = toolbox.getPanel("jsdebugger");
+  is(
+    dbg._selectors().getSelectedSource(dbg._getState()).get("url"),
+    url,
+    `Debugger is opened at expected source url (${url})`
+  );
+}
