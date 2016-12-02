06a372d5b852ec408b36ed75111aed15397a385b
Jessica Jong <jjong@mozilla.com>
2016-12-02 00:41:00 -0500

    Bug 1321245 - Request native key bindings for delayed keypress events. r=smaug

--- widget/BasicEvents.h
+++ widget/BasicEvents.h
@@ -132,9 +132,12 @@ public:
   // Similar to mComposed. Set it to true to allow events cross the boundary
   // between native non-anonymous content and native anonymouse content
   bool mComposedInNativeAnonymousContent : 1;
-  // True if the event is suppressed or delayed. This is used when parent side
-  // process the key event after content side, parent side may drop the key
-  // event if it was suppressed or delayed in content side.
+  // Set to true for events which are suppressed or delayed so that later a
+  // DelayedEvent of it is dispatched. This is used when parent side process
+  // the key event after content side, and may drop the event if the event
+  // was suppressed or delayed in contents side.
+  // It is also set to true for the events (in a DelayedInputEvent), which will
+  // be dispatched afterwards.
   bool mIsSuppressedOrDelayed : 1;
 
   // If the event is being handled in target phase, returns true.
