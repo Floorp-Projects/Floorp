6bb880eb1950279bb61aa81d9af5f67b67ea457c
Eric Rahm <erahm@mozilla.com>
2016-12-02 10:33:48 -0800

    Bug 1311687 - Pop eval context on early returns. r=peterv a=lizzard
    Make sure the eval context stack is cleaned up on failure.
    
    MozReview-Commit-ID: AUNen1xt9He

--- dom/xslt/xslt/txExecutionState.h
+++ dom/xslt/xslt/txExecutionState.h
@@ -94,6 +94,14 @@ public:
     // Stack functions
     nsresult pushEvalContext(txIEvalContext* aContext);
     txIEvalContext* popEvalContext();
+
+    /**
+     * Helper that deletes all entries before |aContext| and then
+     * pops it off the stack. The caller must delete |aContext| if
+     * desired.
+     */
+    void popAndDeleteEvalContextUntil(txIEvalContext* aContext);
+
     nsresult pushBool(bool aBool);
     bool popBool();
     nsresult pushResultHandler(txAXMLEventHandler* aHandler);
