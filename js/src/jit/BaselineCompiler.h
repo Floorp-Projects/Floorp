9f3f54d9e4496b48f69e5f0e6d63d1730096a7d3
Tooru Fujisawa <arai_a@mac.com>
2016-12-03 07:44:21 +0900

    Bug 883377 - Part 3: Support JSOP_SETFUNNAME in Baseline and Ion. r=jandem

--- js/src/jit/BaselineCompiler.h
+++ js/src/jit/BaselineCompiler.h
@@ -71,6 +71,7 @@ namespace jit {
     _(JSOP_REGEXP)             \
     _(JSOP_LAMBDA)             \
     _(JSOP_LAMBDA_ARROW)       \
+    _(JSOP_SETFUNNAME)         \
     _(JSOP_BITOR)              \
     _(JSOP_BITXOR)             \
     _(JSOP_BITAND)             \
