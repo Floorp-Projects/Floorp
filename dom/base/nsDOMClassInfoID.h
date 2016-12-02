e4df55ba05acbbdc423fc6649ba23925b3bb3f68
Nicholas Nethercote <nnethercote@mozilla.com>
2016-12-02 16:06:20 +1100

    Bug 1321746 - Inline nsDOMClassInfoClasses.h. r=bkelly.
    nsDOMClassInfoClasses.h is only #included in one place, so we might as well
    just put its contents in that place direclty. This also avoids some macro
    usage, which is nice.

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -14,18 +14,62 @@
 
 #include "nsIXPCScriptable.h"
 
-#define DOMCI_CLASS(_dom_class)                                               \
-  eDOMClassInfo_##_dom_class##_id,
+enum nsDOMClassInfoID
+{
+  eDOMClassInfo_DOMPrototype_id,
+  eDOMClassInfo_DOMConstructor_id,
 
-enum nsDOMClassInfoID {
+  // CSS classes
+  eDOMClassInfo_CSSStyleRule_id,
+  eDOMClassInfo_CSSImportRule_id,
+  eDOMClassInfo_CSSMediaRule_id,
+  eDOMClassInfo_CSSNameSpaceRule_id,
 
-#include "nsDOMClassInfoClasses.h"
+  // XUL classes
+#ifdef MOZ_XUL
+  eDOMClassInfo_XULCommandDispatcher_id,
+#endif
+  eDOMClassInfo_XULControllers_id,
+#ifdef MOZ_XUL
+  eDOMClassInfo_TreeSelection_id,
+  eDOMClassInfo_TreeContentView_id,
+#endif
+
+#ifdef MOZ_XUL
+  eDOMClassInfo_XULTemplateBuilder_id,
+  eDOMClassInfo_XULTreeBuilder_id,
+#endif
+
+  eDOMClassInfo_CSSMozDocumentRule_id,
+  eDOMClassInfo_CSSSupportsRule_id,
+
+  // @font-face in CSS
+  eDOMClassInfo_CSSFontFaceRule_id,
+
+  eDOMClassInfo_ContentFrameMessageManager_id,
+  eDOMClassInfo_ContentProcessMessageManager_id,
+  eDOMClassInfo_ChromeMessageBroadcaster_id,
+  eDOMClassInfo_ChromeMessageSender_id,
+
+  eDOMClassInfo_CSSKeyframeRule_id,
+  eDOMClassInfo_CSSKeyframesRule_id,
+
+  // @counter-style in CSS
+  eDOMClassInfo_CSSCounterStyleRule_id,
+
+  eDOMClassInfo_CSSPageRule_id,
+
+  eDOMClassInfo_CSSFontFeatureValuesRule_id,
+
+  eDOMClassInfo_XULControlElement_id,
+  eDOMClassInfo_XULLabeledControlElement_id,
+  eDOMClassInfo_XULButtonElement_id,
+  eDOMClassInfo_XULCheckboxElement_id,
+  eDOMClassInfo_XULPopupElement_id,
 
   // This one better be the last one in this list
   eDOMClassInfoIDCount
 };
-
-#undef DOMCI_CLASS
 
 /**
  * nsIClassInfo helper macros
