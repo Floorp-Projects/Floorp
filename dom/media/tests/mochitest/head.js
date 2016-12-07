5f34bd7433972e3bf0eecc8373d3aaec5016d72d
Jan-Ivar Bruaroey <jib@mozilla.com>
2016-12-07 18:37:51 -0500

    Bug 1322274: Test sender.replaceTrack and other methods on close in parallel. r=mt
    MozReview-Commit-ID: 1aDeoLVDHkL

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -526,6 +526,20 @@ function mustThrowWith(msg, reason, f) {
   }
 };
 
+/* Get a dummy audio track */
+function getSilentTrack() {
+  let ctx = new AudioContext(), oscillator = ctx.createOscillator();
+  let dst = oscillator.connect(ctx.createMediaStreamDestination());
+  oscillator.start();
+  return Object.assign(dst.stream.getAudioTracks()[0], {enabled: false});
+}
+
+function getBlackTrack({width = 640, height = 480} = {}) {
+  let canvas = Object.assign(document.createElement("canvas"), {width, height});
+  canvas.getContext('2d').fillRect(0, 0, width, height);
+  let stream = canvas.captureStream();
+  return Object.assign(stream.getVideoTracks()[0], {enabled: false});
+}
 
 /*** Test control flow methods */
 
