e442d257525741c54701bff97a58ba126abd92f4
Edgar Chen <echen@mozilla.com>
2016-12-08 09:22:46 -1000

    Bug 1318630 - Part 1: Fix missing control pref checks for custom element feature; r=wchen a=jcristau
    MozReview-Commit-ID: F9EJMk0kVEA

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -12793,7 +12793,7 @@ nsDocument::CheckCustomElementName(const ElementCreationOptions& aOptions,
   // only check aOptions if 'is' is passed and the webcomponents preference
   // is enabled
   if (!aOptions.mIs.WasPassed() ||
-      !Preferences::GetBool("dom.webcomponents.enabled")) {
+      !CustomElementRegistry::IsCustomElementEnabled()) {
       return nullptr;
   }
 
