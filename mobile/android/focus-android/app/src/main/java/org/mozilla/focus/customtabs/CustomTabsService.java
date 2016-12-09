dcedb2cfa721d96b71e74624774f089a3ef89840
Sebastian Kaspari <s.kaspari@gmail.com>
2016-12-09 10:55:48 -1000

    [focus] Add minimal custom tabs implementation. Fixes https://github.com/mozilla-mobile/focus-android/issues/1.

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/customtabs/CustomTabsService.java
@@ -0,0 +1,49 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.customtabs;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.customtabs.ICustomTabsCallback;
+import android.support.customtabs.ICustomTabsService;
+
+import java.util.List;
+
+public class CustomTabsService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new ICustomTabsService.Stub() {
+            @Override
+            public boolean warmup(long flags) throws RemoteException {
+                return true;
+            }
+
+            @Override
+            public boolean newSession(ICustomTabsCallback callback) throws RemoteException {
+                return true;
+            }
+
+            @Override
+            public boolean mayLaunchUrl(ICustomTabsCallback callback, Uri url, Bundle extras, List<Bundle> otherLikelyBundles) throws RemoteException {
+                return true;
+            }
+
+            @Override
+            public Bundle extraCommand(String s, Bundle bundle) throws RemoteException {
+                return null;
+            }
+
+            @Override
+            public boolean updateVisuals(ICustomTabsCallback callback, Bundle bundle) throws RemoteException {
+                return false;
+            }
+        };
+    }
+}
