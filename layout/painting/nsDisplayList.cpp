d5b1e77f3223b3046d44aa260d87fd4627dab90e
Botond Ballo <botond@mozilla.com>
2016-12-09 15:05:52 -0500

    Bug 1321412 - Follow-up to remove an unused variable. r=bustage on a CLOSED TREE
    MozReview-Commit-ID: 3fCQyMFrajX

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -6292,9 +6292,6 @@ nsDisplayTransform::ShouldPrerenderTransformedContent(nsDisplayListBuilder* aBui
     return PartialPrerender;
   }
 
-  nsRect visual = aFrame->GetVisualOverflowRect();
-
-
   EffectCompositor::SetPerformanceWarning(
     aFrame, eCSSProperty_transform,
     AnimationPerformanceWarning(
