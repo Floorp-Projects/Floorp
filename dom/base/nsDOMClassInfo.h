485a9080b1e266a3d6bdac894d0bdc73d30e2ca6
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-12-12 12:18:03 +0100

    Backed out changeset 6d2cb22d85b0 (bug 1322080) for xpcshell failures on 10.10 debug

--- dom/base/nsDOMClassInfo.h
+++ dom/base/nsDOMClassInfo.h
@@ -180,6 +180,11 @@ public:
                          bool *_retval) override;
 
   virtual void PreserveWrapper(nsISupports *aNative) override;
+
+  static nsIClassInfo *doCreate(nsDOMClassInfoData* aData)
+  {
+    return new nsEventTargetSH(aData);
+  }
 };
 
 // A place to hang some static methods that we should really consider
