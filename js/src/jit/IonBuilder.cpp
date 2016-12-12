da9fde680321783c1b2bd73d2652479ba2dcee3c
Hannes Verschore <hv1989@gmail.com>
2016-12-12 10:30:02 -1000

    Bug 1310155: IonMonkey - Part 1.2 - Remove accidental pushed spewing, r=jandem

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -1677,14 +1677,9 @@ IonBuilder::visitBackEdge(CFGBackEdge* ins, bool* restarted)
         // phis may have generated incorrect nodes. The new types have been
         // incorporated into the header phis, so remove all blocks for the
         // loop body and restart with the new types.
-        GraphSpewer& gs = graphSpewer();
-        gs.spewPass("beforeloop");
-
         *restarted = true;
         if (!restartLoop(ins->getSuccessor(0)))
             return false;
-
-        gs.spewPass("afterloop");
         return true;
     }
 
