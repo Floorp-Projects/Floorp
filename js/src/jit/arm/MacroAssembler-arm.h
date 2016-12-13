afcaaec3cded3dee0a75d149d5fc3333104fc32f
Emanuel Hoogeveen <emanuel.hoogeveen@gmail.com>
2016-12-13 04:10:00 -0500

    Bug 1322445 - Extend PageProtectingVector to protect unused pages as well. r=jandem

--- js/src/jit/arm/MacroAssembler-arm.h
+++ js/src/jit/arm/MacroAssembler-arm.h
@@ -1536,10 +1536,6 @@ class MacroAssemblerARMCompat : public MacroAssemblerARM
     // Instrumentation for entering and leaving the profiler.
     void profilerEnterFrame(Register framePtr, Register scratch);
     void profilerExitFrame();
-
-    struct AutoPrepareForPatching {
-        explicit AutoPrepareForPatching(MacroAssemblerARMCompat&) {}
-    };
 };
 
 typedef MacroAssemblerARMCompat MacroAssemblerSpecific;
