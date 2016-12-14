6871a9416e8ff7ddf7505c51071d6c8d321721de
Bobby Holley <bobbyholley@gmail.com>
2016-12-13 19:11:28 -0800

    Bug 1294572 - Drop Servo data in SetXBLInsertionParent, and call StyleNewSubtree after all bindings have been removed and applied. r=heycam
    MozReview-Commit-ID: Iv7uyq4uQye

--- dom/base/FragmentOrElement.cpp
+++ dom/base/FragmentOrElement.cpp
@@ -23,6 +23,7 @@
 #include "mozilla/EventDispatcher.h"
 #include "mozilla/EventListenerManager.h"
 #include "mozilla/EventStates.h"
+#include "mozilla/ServoRestyleManager.h"
 #include "mozilla/dom/Attr.h"
 #include "nsDOMAttributeMap.h"
 #include "nsIAtom.h"
@@ -1111,6 +1112,12 @@ FragmentOrElement::SetXBLInsertionParent(nsIContent* aContent)
     if (slots) {
       slots->mXBLInsertionParent = nullptr;
     }
+  }
+
+  // We just changed the flattened tree, so any Servo style data is now invalid.
+  // We rely on nsXBLService::LoadBindings to re-traverse the subtree afterwards.
+  if (IsStyledByServo() && IsElement() && AsElement()->HasServoData()) {
+    ServoRestyleManager::ClearServoDataFromSubtree(AsElement());
   }
 }
 
