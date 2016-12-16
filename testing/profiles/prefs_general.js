62e30e86e7c7f61383e780a88f95cf34c3431502
Geoff Brown <gbrown@mozilla.com>
2016-12-16 10:19:40 -0700

    Bug 1323988 - Set gfx.logging.level=1 during tests; r=milan,a=test-only

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -32,6 +32,7 @@ user_pref("devtools.debugger.remote-port", 6023);
 user_pref("devtools.devedition.promo.enabled", false);
 user_pref("browser.EULA.override", true);
 user_pref("gfx.color_management.force_srgb", true);
+user_pref("gfx.logging.level", 1);
 user_pref("network.manage-offline-status", false);
 // Disable speculative connections so they aren't reported as leaking when they're hanging around.
 user_pref("network.http.speculative-parallel-limit", 0);
