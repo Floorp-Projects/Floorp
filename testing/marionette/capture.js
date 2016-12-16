52af49fdb060a1aa4f24797f7162c7b294f27823
Carsten "Tomcat" Book <cbook@mozilla.com>
2016-12-16 12:32:23 +0100

    Backed out changeset 1a07b3fcb30b (bug 1243415)

--- testing/marionette/capture.js
+++ testing/marionette/capture.js
@@ -91,20 +91,12 @@ capture.viewport = function (win, highlights=[]) {
  *     has been painted on.
  */
 capture.canvas = function (win, left, top, width, height, highlights=[]) {
-  let scale = win.devicePixelRatio;
-
   let canvas = win.document.createElementNS(XHTML_NS, "canvas");
-  canvas.width = width * scale;
-  canvas.height = height * scale;
+  canvas.width = width;
+  canvas.height = height;
 
   let ctx = canvas.getContext(CONTEXT_2D);
-  let flags = ctx.DRAWWINDOW_DRAW_CARET |
-      ctx.DRAWWINDOW_DRAW_VIEW;
-      // Bug 1009762 - Crash in [@ mozilla::gl::ReadPixelsIntoDataSurface]
-      // ctx.DRAWWINDOW_USE_WIDGET_LAYERS;
-
-  ctx.scale(scale, scale);
-  ctx.drawWindow(win, left, top, width, height, BG_COLOUR, flags);
+  ctx.drawWindow(win, left, top, width, height, BG_COLOUR);
   ctx = capture.highlight_(ctx, highlights, top, left);
 
   return canvas;
