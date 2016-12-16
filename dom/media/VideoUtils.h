679e2c70fce9d084b31175ed98b0b56913614df2
Gerald Squelart <gsquelart@mozilla.com>
2016-12-16 19:03:20 +1100

    Bug 1329564 - StringListRange::begin/end() can be const - r=jya
    The StringListRange iterator does not modify the list, and cannot be used to
    modify the list, so we can make the begin&end functions const.
    
    MozReview-Commit-ID: 4uNf6CWQ767

--- dom/media/VideoUtils.h
+++ dom/media/VideoUtils.h
@@ -396,6 +396,8 @@ public:
       SearchItemAt(mComma + 1);
       return *this;
     }
+    // DereferencedType should be 'const nsDependent[C]String' pointing into
+    // mList (which is 'const ns[C]String&').
     typedef decltype(Substring(Pointer(), Pointer())) DereferencedType;
     DereferencedType operator*()
     {
@@ -467,14 +469,14 @@ public:
   };
 
   explicit StringListRange(const String& aList) : mList(aList) {}
-  Iterator begin()
+  Iterator begin() const
   {
     return Iterator(mList.Data()
                     + ((empties == StringListRangeEmptyItems::ProcessEmptyItems
                         && mList.Length() == 0) ? 1 : 0),
                     mList.Length());
   }
-  Iterator end()
+  Iterator end() const
   {
     return Iterator(mList.Data() + mList.Length()
                     + (empties != StringListRangeEmptyItems::Skip ? 1 : 0),
