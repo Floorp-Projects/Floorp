e3a6b8d415ce50af2a070c5abbe4b9cc024c7f31
Iris Hsiao <ihsiao@mozilla.com>
2016-12-19 16:17:22 +0800

    Backed out changeset b3eaebda3d9d (bug 1318266)

--- layout/svg/nsSVGIntegrationUtils.cpp
+++ layout/svg/nsSVGIntegrationUtils.cpp
@@ -756,7 +756,6 @@ nsSVGIntegrationUtils::PaintMask(const PaintFramesParams& aParams)
   nsSVGUtils::DetermineMaskUsage(aParams.frame, aParams.handleOpacity,
                                  maskUsage);
   MOZ_ASSERT(maskUsage.shouldGenerateMaskLayer ||
-             maskUsage.shouldApplyClipPath ||
              maskUsage.shouldGenerateClipMaskLayer);
 
   nsIFrame* frame = aParams.frame;
@@ -810,7 +809,7 @@ nsSVGIntegrationUtils::PaintMask(const PaintFramesParams& aParams)
   }
 
   // Paint clip-path onto ctx.
-  if (maskUsage.shouldGenerateClipMaskLayer || maskUsage.shouldApplyClipPath) {
+  if (maskUsage.shouldGenerateClipMaskLayer) {
     matSR.Restore();
     matSR.SetContext(&ctx);
 
