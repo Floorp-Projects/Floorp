ac7e9328945acdbfd113af5511189aecfb68f603
Jeff Walden <jwalden@mit.edu>
2016-12-19 15:28:25 -0500

    Bug 1324536 - Only add/remove files in intl/{icu,tzdata}/{source,SVN-INFO} so that random other detritus in intl/{icu,tzdata} doesn't get included.  r=anba
    --HG--
    extra : rebase_source : 04731d57ce73dce2a10c7e6a45d8dc4c72f6ddd1

--- intl/update-tzdata.sh
+++ intl/update-tzdata.sh
@@ -162,7 +162,7 @@ if [ $dry = false ]; then
     update_icu_data "be" "${icudata_file_be}"
   fi
 
-  hg addremove "${tzdata_dir}" "${icudata_file_le}"
+  hg addremove "${tzdata_dir}/source" "${tzdata_dir}/SVN-INFO" "${icudata_file_le}"
   if [ -n "${icudata_file_be}" ]; then
     hg addremove "${icudata_file_be}"
   fi
