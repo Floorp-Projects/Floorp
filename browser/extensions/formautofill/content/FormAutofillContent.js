408607f43c6cc97127d486ff01f4dd5257c58449
Steve Chung <schung@mozilla.com>
2016-12-19 15:46:11 +0800

    Bug 1304634 - Part 2: Introduce form autofill search factory and registration in FormAutofillcontent, r=MattN
    MozReview-Commit-ID: 6UA0E332iJt

--- browser/extensions/formautofill/content/FormAutofillContent.js
+++ browser/extensions/formautofill/content/FormAutofillContent.js
@@ -8,8 +8,10 @@
 
 "use strict";
 
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr, manager: Cm} = Components;
 
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+let {FormAutoCompleteResult} = Cu.import("resource://gre/modules/nsFormAutoCompleteResult.jsm", {});
 
 /**
  * Handles profile autofill for a DOM Form element.
@@ -129,5 +131,112 @@ FormAutofillHandler.prototype = {
 
       fieldDetail.element.setUserInput(field.value);
     }
+  },
+};
+
+// Register/unregister a constructor as a factory.
+function AutocompleteFactory() {}
+AutocompleteFactory.prototype = {
+  register(targetConstructor) {
+    let proto = targetConstructor.prototype;
+    this._classID = proto.classID;
+
+    let factory = XPCOMUtils._getFactory(targetConstructor);
+    this._factory = factory;
+
+    let registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.registerFactory(proto.classID, proto.classDescription,
+                              proto.contractID, factory);
+
+    if (proto.classID2) {
+      this._classID2 = proto.classID2;
+      registrar.registerFactory(proto.classID2, proto.classDescription,
+                                proto.contractID2, factory);
+    }
+  },
+
+  unregister() {
+    let registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.unregisterFactory(this._classID, this._factory);
+    if (this._classID2) {
+      registrar.unregisterFactory(this._classID2, this._factory);
+    }
+    this._factory = null;
+  },
+};
+
+
+/**
+ * @constructor
+ *
+ * @implements {nsIAutoCompleteSearch}
+ */
+function AutofillProfileAutoCompleteSearch() {
+
+}
+AutofillProfileAutoCompleteSearch.prototype = {
+  classID: Components.ID("4f9f1e4c-7f2c-439e-9c9e-566b68bc187d"),
+  contractID: "@mozilla.org/autocomplete/search;1?name=autofill-profiles",
+  classDescription: "AutofillProfileAutoCompleteSearch",
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch]),
+
+  // Begin nsIAutoCompleteSearch implementation
+
+  /**
+   * Searches for a given string and notifies a listener (either synchronously
+   * or asynchronously) of the result
+   *
+   * @param {string} searchString the string to search for
+   * @param {string} searchParam
+   * @param {Object} previousResult a previous result to use for faster searchinig
+   * @param {Object} listener the listener to notify when the search is complete
+   */
+  startSearch(searchString, searchParam, previousResult, listener) {
+    // TODO: These mock data should be replaced by form autofill API
+    let labels = ["Mary", "John"];
+    let values = ["Mary S.", "John S."];
+    let comments = ["123 Sesame Street.", "331 E. Evelyn Avenue"];
+    let result = new FormAutoCompleteResult(searchString,
+                                            Ci.nsIAutoCompleteResult.RESULT_SUCCESS,
+                                            0, "", values, labels,
+                                            comments);
+
+    listener.onSearchResult(this, result);
+  },
+
+  /**
+   * Stops an asynchronous search that is in progress
+   */
+  stopSearch() {
+  },
+};
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([AutofillProfileAutoCompleteSearch]);
+
+// TODO: Remove this lint option once we apply ProfileAutocomplete while
+//       content script initialization.
+/* eslint no-unused-vars: [2, {"vars": "local"}] */
+let ProfileAutocomplete = {
+  _registered: false,
+  _factory: null,
+
+  ensureRegistered() {
+    if (this._registered) {
+      return;
+    }
+
+    this._factory = new AutocompleteFactory();
+    this._factory.register(AutofillProfileAutoCompleteSearch);
+    this._registered = true;
+  },
+
+  ensureUnregistered() {
+    if (!this._registered) {
+      return;
+    }
+
+    this._factory.unregister();
+    this._factory = null;
+    this._registered = false;
   },
 };
