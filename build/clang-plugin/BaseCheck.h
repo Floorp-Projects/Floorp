cd93cff857cb130e448735a3f8c0fa6b021155e6
Ehsan Akhgari <ehsan@mozilla.com>
2016-12-18 19:42:04 -0500

    Bug 1324320 - Port the must-override check to the clang-tidy compatible framework; r=mystor

--- build/clang-plugin/BaseCheck.h
+++ build/clang-plugin/BaseCheck.h
@@ -2,16 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BaseCheck_h__
-#define BaseCheck_h__
+#ifndef MozCheck_h__
+#define MozCheck_h__
 
 class MozContext {};
 typedef MozContext ContextType;
 
-class BaseCheck : public MatchFinder::MatchCallback {
+class MozCheck : public MatchFinder::MatchCallback {
 public:
-  BaseCheck(StringRef CheckName, ContextType* Context) {}
+  MozCheck(StringRef CheckName, ContextType* Context) {}
   virtual void registerMatchers(MatchFinder *Finder) {}
+  virtual void registerPPCallbacks(CompilerInstance& CI) {}
   virtual void check(const MatchFinder::MatchResult &Result) {}
   DiagnosticBuilder diag(SourceLocation Loc, StringRef Description,
                          DiagnosticIDs::Level Level = DiagnosticIDs::Warning) {
