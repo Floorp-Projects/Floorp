b81f01930bde556ed6001e87af7d59db9a74752a
Alastor Wu <alwu@mozilla.com>
2016-12-19 15:04:16 +0800

    Bug 1324311 - window-less agent can't own audio focus. r=baku a=gchang
    MozReview-Commit-ID: HBieQMoIGOv

--- dom/audiochannel/AudioChannelService.cpp
+++ dom/audiochannel/AudioChannelService.cpp
@@ -1071,6 +1071,11 @@ AudioChannelService::AudioChannelWindow::RequestAudioFocus(AudioChannelAgent* aA
 {
   MOZ_ASSERT(aAgent);
 
+  // Don't need to check audio focus for window-less agent.
+  if (!aAgent->Window()) {
+    return;
+  }
+
   // We already have the audio focus. No operation is needed.
   if (mOwningAudioFocus) {
     return;
