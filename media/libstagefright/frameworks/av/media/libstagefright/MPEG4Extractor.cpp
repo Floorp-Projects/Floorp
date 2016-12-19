f27ae2254035a8167fd47181be4d14f8c9562eb7
Jean-Yves Avenard <jyavenard@mozilla.com>
2016-12-19 21:04:46 +1100

    Bug 1324381: P3. Ignore duration sets to -1. r=gerald a=gchang
    MozReview-Commit-ID: 7RH5deLo0Pm
    
    --HG--
    extra : rebase_source : 1b603914ed3dbcdccc498f340fef2a1150658b35
    extra : source : 64551ddca98393b52002ac9a34b9c16c2e33a888

--- media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/frameworks/av/media/libstagefright/MPEG4Extractor.cpp
@@ -1105,7 +1105,12 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
                         < (ssize_t)sizeof(duration)) {
                     return ERROR_IO;
                 }
-                duration = ntoh64(duration);
+                // Avoid duration sets to -1, which is incorrect.
+                if (duration != -1) {
+                    duration = ntoh64(duration);
+                } else {
+                    duration = 0;
+                }
             } else {
                 uint32_t duration32;
                 if (mDataSource->readAt(
@@ -1116,6 +1121,8 @@ status_t MPEG4Extractor::parseChunk(off64_t *offset, int depth) {
                 // ffmpeg sets duration to -1, which is incorrect.
                 if (duration32 != 0xffffffff) {
                     duration = ntohl(duration32);
+                } else {
+                  duration = 0;
                 }
             }
             if (duration < 0) {
