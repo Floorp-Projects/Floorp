1af303239b5a5f13a38e95f73d829907aa8932ad
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-12-19 11:31:56 +0100

    Bug 1324414 - Reference prebuilt docker images by HASH. r=dustin
    This adds a HASH file next to the VERSION file in the image
    context folders for prebuilt docker images. And uses the
    HASH for referencing the image in the tasks created by
    the decision task.
    
    This way docker will validate the image hash when pulling it
    in production. Thus, attackers won't be able to inject code
    by compromising the remote docker registries we use to store
    prebuilt images. Further more, this makes validation of the
    Chain-Of-Trust artifacts easier as this eliminates the need
    for whitelists and hash validation.
    
    MozReview-Commit-ID: FD3B9MyeU9Q
    
    --HG--
    extra : rebase_source : e01cdbd0db06b36ba95dec3da936ee307a23aae7

--- taskcluster/taskgraph/test/test_util_docker.py
+++ taskcluster/taskgraph/test/test_util_docker.py
@@ -46,17 +46,35 @@ class TestDocker(unittest.TestCase):
 
     def test_docker_image_explicit_registry(self):
         files = {}
-        files["{}/myimage/REGISTRY".format(docker.DOCKER_ROOT)] = "cool-images"
-        files["{}/myimage/VERSION".format(docker.DOCKER_ROOT)] = "1.2.3"
+        files["{}/myimage/REGISTRY".format(docker.IMAGE_DIR)] = "cool-images"
+        files["{}/myimage/VERSION".format(docker.IMAGE_DIR)] = "1.2.3"
+        files["{}/myimage/HASH".format(docker.IMAGE_DIR)] = "sha256:434..."
         with MockedOpen(files):
-            self.assertEqual(docker.docker_image('myimage'), "cool-images/myimage:1.2.3")
+            self.assertEqual(docker.docker_image('myimage'), "cool-images/myimage@sha256:434...")
+
+    def test_docker_image_explicit_registry_by_tag(self):
+        files = {}
+        files["{}/myimage/REGISTRY".format(docker.IMAGE_DIR)] = "myreg"
+        files["{}/myimage/VERSION".format(docker.IMAGE_DIR)] = "1.2.3"
+        files["{}/myimage/HASH".format(docker.IMAGE_DIR)] = "sha256:434..."
+        with MockedOpen(files):
+            self.assertEqual(docker.docker_image('myimage', by_tag=True), "myreg/myimage:1.2.3")
 
     def test_docker_image_default_registry(self):
         files = {}
-        files["{}/REGISTRY".format(docker.DOCKER_ROOT)] = "mozilla"
-        files["{}/myimage/VERSION".format(docker.DOCKER_ROOT)] = "1.2.3"
+        files["{}/REGISTRY".format(docker.IMAGE_DIR)] = "mozilla"
+        files["{}/myimage/VERSION".format(docker.IMAGE_DIR)] = "1.2.3"
+        files["{}/myimage/HASH".format(docker.IMAGE_DIR)] = "sha256:434..."
         with MockedOpen(files):
-            self.assertEqual(docker.docker_image('myimage'), "mozilla/myimage:1.2.3")
+            self.assertEqual(docker.docker_image('myimage'), "mozilla/myimage@sha256:434...")
+
+    def test_docker_image_default_registry_by_tag(self):
+        files = {}
+        files["{}/REGISTRY".format(docker.IMAGE_DIR)] = "mozilla"
+        files["{}/myimage/VERSION".format(docker.IMAGE_DIR)] = "1.2.3"
+        files["{}/myimage/HASH".format(docker.IMAGE_DIR)] = "sha256:434..."
+        with MockedOpen(files):
+            self.assertEqual(docker.docker_image('myimage', by_tag=True), "mozilla/myimage:1.2.3")
 
     def test_create_context_tar_basic(self):
         tmp = tempfile.mkdtemp()
