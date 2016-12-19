5c12d2debcec83d569b4fb897050738935485ae5
Xidorn Quan <me@upsuper.org>
2016-12-19 15:28:31 +1100

    Bug 1323983 part 1 - Ensure to release the pointer even if the document has been detached. r=smaug
    MozReview-Commit-ID: FY5lDQBYl6U
    
    --HG--
    extra : rebase_source : a4d8540f9d22de86549660e173b5a0dc52835b75

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -11703,6 +11703,12 @@ nsDocument::SetPointerLock(Element* aElement, int aCursorStyle)
   nsIPresShell* shell = GetShell();
   if (!shell) {
     NS_WARNING("SetPointerLock(): No PresShell");
+    if (!aElement) {
+      // If we are unlocking pointer lock, but for some reason the doc
+      // has already detached from the presshell, just ask the event
+      // state manager to release the pointer.
+      EventStateManager::SetPointerLock(nullptr, nullptr);
+    }
     return false;
   }
   nsPresContext* presContext = shell->GetPresContext();
@@ -11728,7 +11734,7 @@ nsDocument::SetPointerLock(Element* aElement, int aCursorStyle)
   RefPtr<EventStateManager> esm = presContext->EventStateManager();
   esm->SetCursor(aCursorStyle, nullptr, false,
                  0.0f, 0.0f, widget, true);
-  esm->SetPointerLock(widget, aElement);
+  EventStateManager::SetPointerLock(widget, aElement);
 
   return true;
 }
