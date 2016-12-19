1df3298b6b54b27556c9c14fc5702af29590feb5
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2016-12-19 17:34:00 +0000

    Bug 1324410 - fix dnd to the location bar for plaintext, r=mak
    MozReview-Commit-ID: 7md3hnxisDP
    
    --HG--
    extra : rebase_source : b37fc117474315a50963f682636f9ea13f53e8e2

--- browser/base/content/test/urlbar/browser_dragdropURL.js
+++ browser/base/content/test/urlbar/browser_dragdropURL.js
@@ -2,8 +2,11 @@
 
 const TEST_URL = "data:text/html,a test page";
 const DRAG_URL = "http://www.example.com/";
+const DRAG_FORBIDDEN_URL = "chrome://browser/content/aboutDialog.xul";
+const DRAG_TEXT = "Firefox is awesome";
+const DRAG_WORD = "Firefox";
 
-add_task(function* checkURLBarUpdateForDrag() {
+add_task(function* checkDragURL() {
   yield BrowserTestUtils.withNewTab(TEST_URL, function* (browser) {
     // Have to use something other than the URL bar as a source, so picking the
     // downloads button somewhat arbitrarily:
@@ -11,5 +14,25 @@ add_task(function* checkURLBarUpdateForDrag() {
                               [[{type: "text/plain", data: DRAG_URL}]], "copy", window);
     is(gURLBar.value, TEST_URL, "URL bar value should not have changed");
     is(gBrowser.selectedBrowser.userTypedValue, null, "Stored URL bar value should not have changed");
+  });
+});
+
+add_task(function* checkDragForbiddenURL() {
+  yield BrowserTestUtils.withNewTab(TEST_URL, function* (browser) {
+    EventUtils.synthesizeDrop(document.getElementById("downloads-button"), gURLBar,
+                              [[{type: "text/plain", data: DRAG_FORBIDDEN_URL}]], "copy", window);
+    isnot(gURLBar.value, DRAG_FORBIDDEN_URL, "Shouldn't be allowed to drop forbidden URL on URL bar");
+  });
+});
+
+add_task(function* checkDragText() {
+  yield BrowserTestUtils.withNewTab(TEST_URL, function* (browser) {
+    EventUtils.synthesizeDrop(document.getElementById("downloads-button"), gURLBar,
+                              [[{type: "text/plain", data: DRAG_TEXT}]], "copy", window);
+    is(gURLBar.value, DRAG_TEXT, "Dragging normal text should replace the URL bar value");
+
+    EventUtils.synthesizeDrop(document.getElementById("downloads-button"), gURLBar,
+                              [[{type: "text/plain", data: DRAG_WORD}]], "copy", window);
+    is(gURLBar.value, DRAG_WORD, "Dragging a single word should replace the URL bar value");
   });
 });
