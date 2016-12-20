8ff995b7f291bdef118675da2de63732e1653054
Honza Bambas <honzab.moz@firemni.cz>
2016-12-20 08:27:00 +0100

    Bug 1312243 - Block authentication dialog pop-ups by cancling 3 of them in a row, r=Dolske+mcmanus a=jcristau

--- netwerk/protocol/viewsource/nsViewSourceChannel.cpp
+++ netwerk/protocol/viewsource/nsViewSourceChannel.cpp
@@ -726,6 +726,20 @@ nsViewSourceChannel::SetChannelId(const nsACString& aChannelId)
 }
 
 NS_IMETHODIMP
+nsViewSourceChannel::GetTopLevelContentWindowId(uint64_t *aWindowId)
+{
+  return !mHttpChannel ? NS_ERROR_NULL_POINTER :
+      mHttpChannel->GetTopLevelContentWindowId(aWindowId);
+}
+
+NS_IMETHODIMP
+nsViewSourceChannel::SetTopLevelContentWindowId(uint64_t aWindowId)
+{
+  return !mHttpChannel ? NS_ERROR_NULL_POINTER :
+      mHttpChannel->SetTopLevelContentWindowId(aWindowId);
+}
+
+NS_IMETHODIMP
 nsViewSourceChannel::GetRequestMethod(nsACString & aRequestMethod)
 {
     return !mHttpChannel ? NS_ERROR_NULL_POINTER :
