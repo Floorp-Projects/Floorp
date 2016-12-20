f0c5bf8b96b066cfb25cee29a6800a843172ec6a
Alexander Surkov <surkov.alexander@gmail.com>
2016-12-20 16:45:57 -0500

    Bug 1324460 - implement AOM get() method, r=smaug

--- accessible/aom/AccessibleNode.h
+++ accessible/aom/AccessibleNode.h
@@ -8,6 +8,7 @@
 #define A11Y_AOM_ACCESSIBLENODE_H
 
 #include "nsWrapperCache.h"
+#include "mozilla/ErrorResult.h"
 #include "mozilla/dom/BindingDeclarations.h"
 
 class nsINode;
@@ -40,6 +41,9 @@ public:
   nsINode* GetDOMNode();
 
   bool Is(const Sequence<nsString>& aFlavors);
+  void Get(JSContext* cx, const nsAString& aAttribute,
+           JS::MutableHandle<JS::Value> aValue,
+           ErrorResult& aRv);
 
   a11y::Accessible* Internal() const { return mIntl; }
 
