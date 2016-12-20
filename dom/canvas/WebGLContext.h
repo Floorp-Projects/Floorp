fc7040343660d38565c4c4692f3b57b8df54a448
Jeff Gilbert <jgilbert@mozilla.com>
2016-12-20 14:59:51 -0800

    Bug 1324924 - Support ImageBitmap for Tex*Image*. (flattened) - r=bz,daoshengmu,mtseng. a=gchang
    MozReview-Commit-ID: JzcbzeFHyHn

--- dom/canvas/WebGLContext.h
+++ dom/canvas/WebGLContext.h
@@ -204,6 +204,7 @@ struct TexImageSource
 
     const WebGLsizeiptr* mPboOffset;
 
+    const dom::ImageBitmap* mImageBitmap;
     const dom::ImageData* mImageData;
 
     const dom::Element* mDomElem;
@@ -245,6 +246,10 @@ struct TexImageSourceAdapter final : public TexImageSource
 
     TexImageSourceAdapter(const WebGLsizeiptr* pboOffset, ErrorResult* ignored) {
         mPboOffset = pboOffset;
+    }
+
+    TexImageSourceAdapter(const dom::ImageBitmap* imageBitmap, ErrorResult*) {
+        mImageBitmap = imageBitmap;
     }
 
     TexImageSourceAdapter(const dom::ImageData* imageData, ErrorResult*) {
