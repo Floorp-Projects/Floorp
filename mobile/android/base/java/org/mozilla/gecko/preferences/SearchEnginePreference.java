53f757d9a7cb1a0ec239266697975c17308613a7
Jim Chen <nchen@mozilla.com>
2016-12-20 17:01:04 -0500

    Bug 1324817 - Fix NPE in setSearchEngineFromBundle; r=me
    Fix a NPE in setSearchEngineFromBundle when mIdentifier is null. r=me
    for trivial patch.

--- mobile/android/base/java/org/mozilla/gecko/preferences/SearchEnginePreference.java
+++ mobile/android/base/java/org/mozilla/gecko/preferences/SearchEnginePreference.java
@@ -145,7 +145,7 @@ public class SearchEnginePreference extends CustomListPreference {
         mIdentifier = geckoEngine.getString("identifier");
 
         // A null JS value gets converted into a string.
-        if (mIdentifier.equals("null")) {
+        if (mIdentifier == null || mIdentifier.equals("null")) {
             mIdentifier = "other";
         }
 
