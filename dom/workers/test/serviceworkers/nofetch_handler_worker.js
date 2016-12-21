dac5502015dee5fa896ee96bc74950f079aeaaf6
Catalin Badea <catalin.badea392@gmail.com>
2016-12-21 10:38:00 +0800

    Bug 1325101 - Update nofetch handler mochitest. r=bkelly

--- dom/workers/test/serviceworkers/nofetch_handler_worker.js
+++ dom/workers/test/serviceworkers/nofetch_handler_worker.js
@@ -6,3 +6,9 @@ self.oninstall = function(event) {
       addEventListener('fetch', handleFetch);
       self.onfetch = handleFetch;
 }
+
+// Bug 1325101. Make sure adding event listeners for other events
+// doesn't set the fetch flag.
+addEventListener('push', function() {})
+addEventListener('message', function() {})
+addEventListener('non-sw-event', function() {})
