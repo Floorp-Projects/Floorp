13636a4be114eb4cb14c34ed91fdb9811a97df0a
Luca Greco <lgreco@mozilla.com>
2016-12-20 23:22:57 -0500

    Bug 1322782 - Fixed coding style in MatchPattern tests. r=kmag a=test-only
    MozReview-Commit-ID: JsF5J5vyeLQ
    
    --HG--
    extra : source : 012ca1fe054898184ef11cc36f35f23bd89e03fb

--- toolkit/modules/tests/xpcshell/test_MatchPattern.js
+++ toolkit/modules/tests/xpcshell/test_MatchPattern.js
@@ -26,69 +26,69 @@ function fail({url, pattern})
 function run_test()
 {
   // Invalid pattern.
-  fail({url:"http://mozilla.org", pattern:""});
+  fail({url: "http://mozilla.org", pattern: ""});
 
   // Pattern must include trailing slash.
-  fail({url:"http://mozilla.org", pattern:"http://mozilla.org"});
+  fail({url: "http://mozilla.org", pattern: "http://mozilla.org"});
 
   // Protocol not allowed.
-  fail({url:"http://mozilla.org", pattern:"gopher://wuarchive.wustl.edu/"});
+  fail({url: "http://mozilla.org", pattern: "gopher://wuarchive.wustl.edu/"});
 
-  pass({url:"http://mozilla.org", pattern:"http://mozilla.org/"});
-  pass({url:"http://mozilla.org/", pattern:"http://mozilla.org/"});
+  pass({url: "http://mozilla.org", pattern: "http://mozilla.org/"});
+  pass({url: "http://mozilla.org/", pattern: "http://mozilla.org/"});
 
-  pass({url:"http://mozilla.org/", pattern:"*://mozilla.org/"});
-  pass({url:"https://mozilla.org/", pattern:"*://mozilla.org/"});
-  fail({url:"file://mozilla.org/", pattern:"*://mozilla.org/"});
-  fail({url:"ftp://mozilla.org/", pattern:"*://mozilla.org/"});
+  pass({url: "http://mozilla.org/", pattern: "*://mozilla.org/"});
+  pass({url: "https://mozilla.org/", pattern: "*://mozilla.org/"});
+  fail({url: "file://mozilla.org/", pattern: "*://mozilla.org/"});
+  fail({url: "ftp://mozilla.org/", pattern: "*://mozilla.org/"});
 
-  fail({url:"http://mozilla.com", pattern:"http://*mozilla.com*/"});
-  fail({url:"http://mozilla.com", pattern:"http://mozilla.*/"});
-  fail({url:"http://mozilla.com", pattern:"http:/mozilla.com/"});
+  fail({url: "http://mozilla.com", pattern: "http://*mozilla.com*/"});
+  fail({url: "http://mozilla.com", pattern: "http://mozilla.*/"});
+  fail({url: "http://mozilla.com", pattern: "http:/mozilla.com/"});
 
-  pass({url:"http://google.com", pattern:"http://*.google.com/"});
-  pass({url:"http://docs.google.com", pattern:"http://*.google.com/"});
+  pass({url: "http://google.com", pattern: "http://*.google.com/"});
+  pass({url: "http://docs.google.com", pattern: "http://*.google.com/"});
 
-  pass({url:"http://mozilla.org:8080", pattern:"http://mozilla.org/"});
-  pass({url:"http://mozilla.org:8080", pattern:"*://mozilla.org/"});
-  fail({url:"http://mozilla.org:8080", pattern:"http://mozilla.org:8080/"});
+  pass({url: "http://mozilla.org:8080", pattern: "http://mozilla.org/"});
+  pass({url: "http://mozilla.org:8080", pattern: "*://mozilla.org/"});
+  fail({url: "http://mozilla.org:8080", pattern: "http://mozilla.org:8080/"});
 
   // Now try with * in the path.
-  pass({url:"http://mozilla.org", pattern:"http://mozilla.org/*"});
-  pass({url:"http://mozilla.org/", pattern:"http://mozilla.org/*"});
+  pass({url: "http://mozilla.org", pattern: "http://mozilla.org/*"});
+  pass({url: "http://mozilla.org/", pattern: "http://mozilla.org/*"});
 
-  pass({url:"http://mozilla.org/", pattern:"*://mozilla.org/*"});
-  pass({url:"https://mozilla.org/", pattern:"*://mozilla.org/*"});
-  fail({url:"file://mozilla.org/", pattern:"*://mozilla.org/*"});
-  fail({url:"http://mozilla.com", pattern:"http://mozilla.*/*"});
+  pass({url: "http://mozilla.org/", pattern: "*://mozilla.org/*"});
+  pass({url: "https://mozilla.org/", pattern: "*://mozilla.org/*"});
+  fail({url: "file://mozilla.org/", pattern: "*://mozilla.org/*"});
+  fail({url: "http://mozilla.com", pattern: "http://mozilla.*/*"});
 
-  pass({url:"http://google.com", pattern:"http://*.google.com/*"});
-  pass({url:"http://docs.google.com", pattern:"http://*.google.com/*"});
+  pass({url: "http://google.com", pattern: "http://*.google.com/*"});
+  pass({url: "http://docs.google.com", pattern: "http://*.google.com/*"});
 
   // Check path stuff.
-  fail({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/"});
-  pass({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/*"});
-  pass({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/a*f"});
-  pass({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/a*"});
-  pass({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/*f"});
-  fail({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/*e"});
-  fail({url:"http://mozilla.com/abc/def", pattern:"http://mozilla.com/*c"});
+  fail({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/"});
+  pass({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/*"});
+  pass({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/a*f"});
+  pass({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/a*"});
+  pass({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/*f"});
+  fail({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/*e"});
+  fail({url: "http://mozilla.com/abc/def", pattern: "http://mozilla.com/*c"});
 
-  fail({url:"http:///a.html", pattern:"http:///a.html"});
-  pass({url:"file:///foo", pattern:"file:///foo*"});
-  pass({url:"file:///foo/bar.html", pattern:"file:///foo*"});
+  fail({url: "http:///a.html", pattern: "http:///a.html"});
+  pass({url: "file:///foo", pattern: "file:///foo*"});
+  pass({url: "file:///foo/bar.html", pattern: "file:///foo*"});
 
-  pass({url:"http://mozilla.org/a", pattern:"<all_urls>"});
-  pass({url:"https://mozilla.org/a", pattern:"<all_urls>"});
-  pass({url:"ftp://mozilla.org/a", pattern:"<all_urls>"});
-  pass({url:"file:///a", pattern:"<all_urls>"});
-  fail({url:"gopher://wuarchive.wustl.edu/a", pattern:"<all_urls>"});
+  pass({url: "http://mozilla.org/a", pattern: "<all_urls>"});
+  pass({url: "https://mozilla.org/a", pattern: "<all_urls>"});
+  pass({url: "ftp://mozilla.org/a", pattern: "<all_urls>"});
+  pass({url: "file:///a", pattern: "<all_urls>"});
+  fail({url: "gopher://wuarchive.wustl.edu/a", pattern: "<all_urls>"});
 
   // Multiple patterns.
-  pass({url:"http://mozilla.org", pattern:["http://mozilla.org/"]});
-  pass({url:"http://mozilla.org", pattern:["http://mozilla.org/", "http://mozilla.com/"]});
-  pass({url:"http://mozilla.com", pattern:["http://mozilla.org/", "http://mozilla.com/"]});
-  fail({url:"http://mozilla.biz", pattern:["http://mozilla.org/", "http://mozilla.com/"]});
+  pass({url: "http://mozilla.org", pattern: ["http://mozilla.org/"]});
+  pass({url: "http://mozilla.org", pattern: ["http://mozilla.org/", "http://mozilla.com/"]});
+  pass({url: "http://mozilla.com", pattern: ["http://mozilla.org/", "http://mozilla.com/"]});
+  fail({url: "http://mozilla.biz", pattern: ["http://mozilla.org/", "http://mozilla.com/"]});
 
   // Match url with fragments.
   pass({url: "http://mozilla.org/base#some-fragment", pattern: "http://mozilla.org/base"});
