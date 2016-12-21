be76c082e010689ad929b715de3fd093e94fc6e1
Xidorn Quan <me@upsuper.org>
2016-12-21 12:02:24 +1100

    Bug 1323983 part 1 - Ensure to release the pointer even if the document has been detached. r=smaug
    MozReview-Commit-ID: FY5lDQBYl6U
    
    --HG--
    extra : rebase_source : 47159f623c46549b48c786b28b5c00c246288076

--- dom/events/EventStateManager.h
+++ dom/events/EventStateManager.h
@@ -921,7 +921,7 @@ private:
   // Last mouse event mRefPoint (the offset from the widget's origin in
   // device pixels) when mouse was locked, used to restore mouse position
   // after unlocking.
-  LayoutDeviceIntPoint mPreLockPoint;
+  static LayoutDeviceIntPoint sPreLockPoint;
 
   // Stores the mRefPoint of the last synthetic mouse move we dispatched
   // to re-center the mouse when we were pointer locked. If this is (-1,-1) it
@@ -1015,7 +1015,7 @@ public:
   void KillClickHoldTimer();
   void FireContextClick();
 
-  void SetPointerLock(nsIWidget* aWidget, nsIContent* aElement) ;
+  static void SetPointerLock(nsIWidget* aWidget, nsIContent* aElement) ;
   static void sClickHoldCallback ( nsITimer* aTimer, void* aESM ) ;
 };
 
