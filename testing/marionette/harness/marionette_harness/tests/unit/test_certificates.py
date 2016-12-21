118acfdd15c008e26b5db540e0cdd642ad883627
Henrik Skupin <mail@hskupin.info>
2016-12-21 17:34:59 +0100

    Bug 1323770 - Fix inappropriatelly skipped/disabled tests. r=maja_zf a=test-only
    Commenting out test methods is not the way how we should mark tests
    as being skipped. The correct skip methods have to be used instead so
    that the final results also show the correct skip count.
    
    MozReview-Commit-ID: LKL4YQCyFko

--- testing/marionette/harness/marionette_harness/tests/unit/test_certificates.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_certificates.py
@@ -4,10 +4,12 @@
 
 from marionette_driver.errors import UnknownException
 
-from marionette_harness import MarionetteTestCase
+from marionette_harness import MarionetteTestCase, skip
 
 
 class TestCertificates(MarionetteTestCase):
+
+    @skip("Bug 1325079")
     def test_block_insecure_sites(self):
         self.marionette.delete_session()
         self.marionette.start_session()
@@ -17,16 +19,16 @@ class TestCertificates(MarionetteTestCase):
         with self.assertRaises(UnknownException):
             self.marionette.navigate(self.fixtures.where_is("test.html", on="https"))
 
+    @skip("Bug 1325079")
     def test_accept_all_insecure(self):
         self.marionette.delete_session()
         self.marionette.start_session({"desiredCapability": {"acceptSslCerts": ["*"]}})
         self.marionette.navigate(self.fixtures.where_is("test.html", on="https"))
         self.assertIn("https://", self.marionette.url)
 
-    """
+    @skip("Bug 1325079")
     def test_accept_some_insecure(self):
         self.marionette.delete_session()
         self.marionette.start_session({"requiredCapabilities": {"acceptSslCerts": ["127.0.0.1"]}})
         self.marionette.navigate(self.fixtures.where_is("test.html", on="https"))
         self.assertIn("https://", self.marionette.url)
-    """
