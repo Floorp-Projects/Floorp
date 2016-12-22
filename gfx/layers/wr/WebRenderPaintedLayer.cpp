8ae7aaf486c0b6220a509f5c25e235b03f9e8816
JerryShih <hshih@mozilla.com>
2016-12-22 17:22:37 +0800

    Bug 1321144 - use ImageClient in WebRenderPaintedLayer. r=sotaro?
    MozReview-Commit-ID: GLtXYBuYXWo

--- gfx/layers/wr/WebRenderPaintedLayer.cpp
+++ gfx/layers/wr/WebRenderPaintedLayer.cpp
@@ -15,45 +15,95 @@ namespace layers {
 
 using namespace mozilla::gfx;
 
+WebRenderPaintedLayer::~WebRenderPaintedLayer()
+{
+  MOZ_COUNT_DTOR(WebRenderPaintedLayer);
+
+  if (mExternalImageId) {
+    Manager()->AddExternalImageIdForDiscard(mExternalImageId);
+  }
+}
+
 void
 WebRenderPaintedLayer::RenderLayer()
 {
   LayerIntRegion visibleRegion = GetVisibleRegion();
   LayerIntRect bounds = visibleRegion.GetBounds();
-  LayerIntSize size = bounds.Size();
+  gfx::IntSize size = bounds.Size().ToUnknownSize();
+
   if (size.IsEmpty()) {
       return;
   }
 
-  WRScrollFrameStackingContextGenerator scrollFrames(this);
-
-  RefPtr<DrawTarget> target = gfx::Factory::CreateDrawTarget(gfx::BackendType::SKIA, size.ToUnknownSize(), SurfaceFormat::B8G8R8A8);
-  target->SetTransform(Matrix().PreTranslate(-bounds.x, -bounds.y));
-  RefPtr<gfxContext> ctx = gfxContext::CreatePreservingTransformOrNull(target);
-  MOZ_ASSERT(ctx); // already checked the target above
-
-  Manager()->GetPaintedLayerCallback()(this,
-                                       ctx,
-                                       visibleRegion.ToUnknownRegion(), visibleRegion.ToUnknownRegion(),
-                                       DrawRegionClip::DRAW, nsIntRegion(), Manager()->GetPaintedLayerCallbackData());
-#if 0
-  static int count;
-  char buf[400];
-  sprintf(buf, "wrout%d.png", count++);
-  gfxUtils::WriteAsPNG(target, buf);
-#endif
-
-  WRImageKey key;
-  {
-      unsigned char* data;
-      IntSize size;
-      int32_t stride;
-      SurfaceFormat format;
-      target->LockBits(&data, &size, &stride, &format);
-      gfx::ByteBuffer buf(size.height * stride, data);
-      WRBridge()->SendAddImage(size.width, size.height, stride, RGBA8, buf, &key);
-      target->ReleaseBits(data);
+  if (!mImageContainer) {
+    mImageContainer = LayerManager::CreateImageContainer();
   }
+
+  if (!mImageClient) {
+    mImageClient = ImageClient::CreateImageClient(CompositableType::IMAGE,
+                                                  WRBridge(),
+                                                  TextureFlags::DEFAULT);
+    if (!mImageClient) {
+      return;
+    }
+    mImageClient->Connect();
+  }
+
+  // If we already have previous frame's mExternalImageId, remove that id here.
+  if (mExternalImageId) {
+    Manager()->AddExternalImageIdForDiscard(mExternalImageId);
+  }
+  // Try to use different image id for each content updates.
+  mExternalImageId = WRBridge()->AllocExternalImageIdForCompositable(mImageClient,
+                                                                     size,
+                                                                     SurfaceFormat::B8G8R8A8);
+  MOZ_ASSERT(mExternalImageId);
+
+  RefPtr<TextureClient> texture = mImageClient->GetTextureClientRecycler()
+    ->CreateOrRecycle(SurfaceFormat::B8G8R8A8,
+                      size,
+                      BackendSelector::Content,
+                      TextureFlags::DEFAULT);
+  if (!texture) {
+    return;
+  }
+  MOZ_ASSERT(texture->CanExposeDrawTarget());
+
+  {
+    TextureClientAutoLock autoLock(texture, OpenMode::OPEN_WRITE_ONLY);
+    if (!autoLock.Succeeded()) {
+      return;
+    }
+    RefPtr<DrawTarget> target = texture->BorrowDrawTarget();
+    if (!target || !target->IsValid()) {
+      return;
+    }
+
+    target->SetTransform(Matrix().PreTranslate(-bounds.x, -bounds.y));
+    RefPtr<gfxContext> ctx = gfxContext::CreatePreservingTransformOrNull(target);
+    MOZ_ASSERT(ctx); // already checked the target above
+
+    Manager()->GetPaintedLayerCallback()(this,
+                                         ctx,
+                                         visibleRegion.ToUnknownRegion(), visibleRegion.ToUnknownRegion(),
+                                         DrawRegionClip::DRAW, nsIntRegion(), Manager()->GetPaintedLayerCallbackData());
+
+#if 0
+    static int count;
+    char buf[400];
+    sprintf(buf, "wrout%d.png", count++);
+    gfxUtils::WriteAsPNG(target, buf);
+#endif
+  }
+  RefPtr<TextureWrapperImage> image =
+    new TextureWrapperImage(texture, IntRect(IntPoint(0, 0), size));
+  mImageContainer->SetCurrentImageInTransaction(image);
+
+  if (!mImageClient->UpdateImage(mImageContainer, /* unused */0)) {
+    return;
+  }
+
+  WRScrollFrameStackingContextGenerator scrollFrames(this);
 
   // Since we are creating a stacking context below using the visible region of
   // this layer, we need to make sure the image display item has coordinates
@@ -73,8 +123,7 @@ WebRenderPaintedLayer::RenderLayer()
 
   WRBridge()->AddWebRenderCommand(
       OpPushDLBuilder(toWrRect(relBounds), toWrRect(overflow), transform, FrameMetrics::NULL_SCROLL_ID));
-  WRBridge()->AddWebRenderCommand(OpDPPushImage(toWrRect(rect), toWrRect(clip), Nothing(), key));
-  Manager()->AddImageKeyForDiscard(key);
+  WRBridge()->AddWebRenderCommand(OpDPPushExternalImageId(toWrRect(rect), toWrRect(clip), Nothing(), mExternalImageId));
 
   if (gfxPrefs::LayersDump()) printf_stderr("PaintedLayer %p using %s as bounds/overflow, %s for transform\n", this, Stringify(relBounds).c_str(), Stringify(transform).c_str());
   WRBridge()->AddWebRenderCommand(OpPopDLBuilder());
