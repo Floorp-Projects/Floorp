e4955123771ee5b1402e897f0efd57372ed207a4
Drew Willcoxon <adw@mozilla.com>
2016-12-21 19:14:30 -0800

    Bug 1257550 - Awesomebar result providers should be able to add document fragments or iframes. r=mak
    MozReview-Commit-ID: J4aJtETNLoO
    
    --HG--
    extra : rebase_source : 1c625cb45eba2df71e2eac2ac9d291dedc7e15d8

---
+++ browser/base/content/test/urlbar/urlbarAddonIframe.js
@@ -0,0 +1,52 @@
+// Listen for messages from the test.
+addEventListener("TestEvent", event => {
+  let type = event.detail.type;
+  dump("urlbarAddonIframe.js got TestEvent, type=" + type +
+       " messageID=" + event.detail.messageID + "\n");
+  switch (type) {
+  case "function":
+    callUrlbarFunction(event.detail);
+    break;
+  case "event":
+    expectEvent(event.detail);
+    break;
+  }
+});
+
+// Calls a urlbar API function.
+function callUrlbarFunction(detail) {
+  let args = detail.data;
+  let methodName = args.shift();
+  dump("urlbarAddonIframe.js calling urlbar." + methodName + "\n");
+  let rv = urlbar[methodName](...args);
+  ack(detail, rv);
+}
+
+// Waits for an event of a specified type to happen.
+function expectEvent(detail) {
+  let type = detail.data;
+  dump("urlbarAddonIframe.js expecting event of type " + type + "\n");
+  // Ack that the message was received and an event listener was added.
+  ack(detail, null, 0);
+  addEventListener(type, function onEvent(event) {
+    dump("urlbarAddonIframe.js got event of type " + type + "\n");
+    if (event.type != type) {
+      return;
+    }
+    dump("urlbarAddonIframe.js got expected event\n");
+    removeEventListener(type, onEvent);
+    // Ack that the event was received.
+    ack(detail, event.detail, 1);
+  });
+}
+
+// Sends an ack to the test.
+function ack(originalEventDetail, ackData=null, ackIndex=0) {
+  dispatchEvent(new CustomEvent("TestEventAck", {
+    detail: {
+      messageID: originalEventDetail.messageID,
+      ackIndex: ackIndex,
+      data: ackData,
+    },
+  }));
+}
