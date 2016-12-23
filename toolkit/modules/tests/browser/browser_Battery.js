8df5c4b1fa8ebaabd2b538d74ca1106e24e48b1e
malayaleecoder <malayaleecoder@gmail.com>
2016-12-24 02:44:56 +0530

    Bug 1325099 - reset imported.Debugging.fake value and catch TypeError. r=jmaher
    MozReview-Commit-ID: ElEGCLoSAeC

--- toolkit/modules/tests/browser/browser_Battery.js
+++ toolkit/modules/tests/browser/browser_Battery.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+"use strict";
 var imported = Components.utils.import("resource://gre/modules/Battery.jsm", this);
 Cu.import("resource://gre/modules/Services.jsm", this);
 
@@ -13,8 +13,15 @@ function test() {
   GetBattery().then(function(battery) {
     for (let k of ["charging", "chargingTime", "dischargingTime", "level"]) {
       let backup = battery[k];
-      battery[k] = "__magic__";
-      is(battery[k], backup, "Setting battery " + k + "preference without spoofing enabled should fail");
+      try {
+        battery[k] = "__magic__";
+      } catch (e) {
+        // We are testing that we cannot set battery to new values
+        // when "use strict" is enabled, this throws a TypeError
+        if (e.name != "TypeError")
+          throw e;
+      }
+      is(battery[k], backup, "Setting battery " + k + " preference without spoofing enabled should fail");
     }
 
     imported.Debugging.fake = true;
@@ -35,6 +42,9 @@ function test() {
       is(battery.dischargingTime, 50, "Test for dischargingTime setter");
       is(battery.level, 0.7, "Test for level setter");
 
+      // Resetting the value to make the test run successful
+      // for multiple runs in same browser session.
+      imported.Debugging.fake = false;
       finish();
     });
   });
