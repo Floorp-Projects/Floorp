30961dadd7c29f37322d7d6f5bd35e93be51aaa2
Xidorn Quan <me@upsuper.org>
2016-12-23 11:32:33 +1100

    Bug 1325351 part 2 - Simplify 'Some' function. r=froydnj
    MozReview-Commit-ID: AkLVQw9WYwy
    
    --HG--
    extra : rebase_source : 97449d0b2376d53ee7761da2a1ab425492c70ba6

--- mfbt/Maybe.h
+++ mfbt/Maybe.h
@@ -438,11 +438,12 @@ public:
  * if you need to construct a Maybe value that holds a const, volatile, or
  * reference value, you need to use emplace() instead.
  */
-template<typename T>
-Maybe<typename RemoveCV<typename RemoveReference<T>::Type>::Type>
+template<typename T,
+         typename U = typename std::remove_cv<
+           typename std::remove_reference<T>::type>::type>
+Maybe<U>
 Some(T&& aValue)
 {
-  typedef typename RemoveCV<typename RemoveReference<T>::Type>::Type U;
   Maybe<U> value;
   value.emplace(Forward<T>(aValue));
   return value;
