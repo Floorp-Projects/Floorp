2f2428ee7378c54c2340af704385cdb6b68b5ed0
James Cheng <jacheng@mozilla.com>
2016-12-23 15:50:13 +0800

    Bug 1325332 - [EME][Fennec] Pref on EME API and only support it for M+ android version. r=cpearce
    MozReview-Commit-ID: Bi5pRhIFsxB

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -637,6 +637,12 @@ pref("media.mediasource.enabled", true);
 
 pref("media.mediadrm-widevinecdm.visible", true);
 
+#ifdef NIGHTLY_BUILD
+// Enable EME(Encrypted media extensions)
+pref("media.eme.enabled", true);
+pref("media.eme.apiVisible", true);
+#endif
+
 // optimize images memory usage
 pref("image.downscale-during-decode.enabled", true);
 
