5d8f325849dfe5a831c4c7d92f1763ef4fba2aba
Tanvi Vyas <tanvi@mozilla.com>
2016-12-23 17:37:31 -0800

    Bug 1319119 - Show degraded security UI for HTTP pages with password fields in release. r=MattN, a=lizzard

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1215,12 +1215,7 @@ pref("social.shareDirectory", "https://activations.cdn.mozilla.net/sharePanel.ht
 pref("security.mixed_content.block_active_content", true);
 
 // Show degraded UI for http pages with password fields.
-// Only for Nightly, Dev Edition and early beta, not for late beta or release.
-#ifdef EARLY_BETA_OR_EARLIER
 pref("security.insecure_password.ui.enabled", true);
-#else
-pref("security.insecure_password.ui.enabled", false);
-#endif
 
 pref("security.insecure_field_warning.contextual.enabled", false);
 
