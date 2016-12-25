1f3ea9a1ac52a58fe99d440b23e25aadf8413417
Dão Gottwald <dao@mozilla.com>
2016-12-25 07:36:25 +0100

    Bug 1324049 - Implement pref for opening the privacy policy in a background tab on first run instead of showing the data choices infobar. r=gijs

--- toolkit/components/telemetry/datareporting-prefs.js
+++ toolkit/components/telemetry/datareporting-prefs.js
@@ -9,3 +9,4 @@ pref("datareporting.policy.dataSubmissionPolicyBypassNotification", false);
 pref("datareporting.policy.currentPolicyVersion", 2);
 pref("datareporting.policy.minimumPolicyVersion", 1);
 pref("datareporting.policy.minimumPolicyVersion.channel-beta", 2);
+pref("datareporting.policy.firstRunURL", "");
