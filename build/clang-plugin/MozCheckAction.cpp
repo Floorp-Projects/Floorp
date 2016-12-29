9bf0b6bb08427fbccbc76a30cf2ed8e577e9f553
Ehsan Akhgari <ehsan@mozilla.com>
2016-12-29 11:32:31 -0500

    Bug 1326229 - Move the include for FrontendPluginRegistry.h to MozCheckAction.cpp; r=mystor

--- build/clang-plugin/MozCheckAction.cpp
+++ build/clang-plugin/MozCheckAction.cpp
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "plugin.h"
 #include "DiagnosticsMatcher.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
 
 class MozCheckAction : public PluginASTAction {
 public:
