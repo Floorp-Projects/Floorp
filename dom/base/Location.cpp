163bcc1489f1461a919ecfcbbfdabe542e6d74ee
Sebastian Hengst <archaeopteryx@coole-files.de>
2016-12-29 10:39:12 +0100

    Backed out changeset e3023975fdec (bug 1321719) for bustage. r=backout a=backout

--- dom/base/Location.cpp
+++ dom/base/Location.cpp
@@ -581,7 +581,7 @@ Location::GetPathname(nsAString& aPathname)
 
   result = GetURI(getter_AddRefs(uri));
 
-  nsCOMPtr<nsIURIWithQuery> url(do_QueryInterface(uri));
+  nsCOMPtr<nsIURL> url(do_QueryInterface(uri));
   if (url) {
     nsAutoCString file;
 
@@ -604,12 +604,12 @@ Location::SetPathname(const nsAString& aPathname)
     return rv;
   }
 
-  nsCOMPtr<nsIURIWithQuery> url(do_QueryInterface(uri));
-  if (url && NS_SUCCEEDED(url->SetFilePath(NS_ConvertUTF16toUTF8(aPathname)))) {
-    return SetURI(uri);
+  rv = uri->SetPath(NS_ConvertUTF16toUTF8(aPathname));
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
   }
 
-  return NS_OK;
+  return SetURI(uri);
 }
 
 NS_IMETHODIMP
