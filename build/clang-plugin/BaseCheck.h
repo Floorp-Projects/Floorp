65c49d82e48021f4258952f74bba5ed4e4b2507c
Ehsan Akhgari <ehsan@mozilla.com>
2016-12-28 21:19:30 -0500

    Bug 1324320 follow-up: Fix a mistake that breaks the build

--- build/clang-plugin/BaseCheck.h
+++ build/clang-plugin/BaseCheck.h
@@ -2,15 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MozCheck_h__
-#define MozCheck_h__
+#ifndef BaseCheck_h__
+#define BaseCheck_h__
 
 class MozContext {};
 typedef MozContext ContextType;
 
-class MozCheck : public MatchFinder::MatchCallback {
+class BaseCheck : public MatchFinder::MatchCallback {
 public:
-  MozCheck(StringRef CheckName, ContextType* Context) {}
+  BaseCheck(StringRef CheckName, ContextType* Context) {}
   virtual void registerMatchers(MatchFinder *Finder) {}
   virtual void registerPPCallbacks(CompilerInstance& CI) {}
   virtual void check(const MatchFinder::MatchResult &Result) {}
@@ -18,7 +18,6 @@ public:
                          DiagnosticIDs::Level Level = DiagnosticIDs::Warning) {
     DiagnosticsEngine &Diag = Context->getDiagnostics();
     unsigned ID = Diag.getDiagnosticIDs()->getCustomDiagID(Level, Description);
-    // We treat all diagnostics as errors here.
     return Diag.Report(Loc, ID);
   }
 
