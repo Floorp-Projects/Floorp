52ae4e2677c207e629a4a08865b1e0111076b819
Tim Nguyen <ntim.bugs@gmail.com>
2016-12-30 03:11:52 +0100

    Bug 1325213 - Fix lots of eslint errors in devtools/shared/. r=jryans
    MozReview-Commit-ID: 2XxhfV8ih0S

--- devtools/shared/performance/test/head.js
+++ devtools/shared/performance/test/head.js
@@ -1,7 +1,10 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* exported require */
+
 "use strict";
 
-var { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+var { utils: Cu } = Components;
 
 var { require } = Cu.import("resource://devtools/shared/Loader.jsm", {});
