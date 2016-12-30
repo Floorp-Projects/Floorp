eb7be14ba426c0576299efd681a7bdb80b150eca
Tim Nguyen <ntim.bugs@gmail.com>
2016-12-30 03:11:52 +0100

    Bug 1325213 - Fix lots of eslint errors in devtools/shared/. r=jryans
    MozReview-Commit-ID: 2XxhfV8ih0S

--- devtools/shared/tests/unit/test_assert.js
+++ devtools/shared/tests/unit/test_assert.js
@@ -2,6 +2,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+"use strict";
+
 // Test DevToolsUtils.assert
 
 ALLOW_CONSOLE_ERRORS = true;
@@ -32,5 +34,6 @@ function run_test() {
   }
 
   ok(assertionFailed,
-     "The assertion should have failed, which should throw an error when assertions are enabled.");
+     "The assertion should have failed, which should throw an error when assertions " +
+     "are enabled.");
 }
