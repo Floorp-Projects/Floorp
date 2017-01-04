74e1c150349efc6cb60bcc6681b9356737302676
Jeff Walden <jwalden@mit.edu>
2017-01-04 14:45:37 -0800

    Bug 1326454 - Make TokenStream::updateLineInfoForEOL fallible, and immediately handle errors when it fails instead of temporarily deferring such handling.  r=arai

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -489,10 +489,9 @@ class MOZ_STACK_CLASS TokenStream
         bool sawOctalEscape:1;  // Saw an octal character escape.
         bool hadError:1;        // Hit a syntax error, at start or during a
                                 // token.
-        bool hitOOM:1;          // Hit OOM.
 
         Flags()
-          : isEOF(), isDirtyLine(), sawOctalEscape(), hadError(), hitOOM()
+          : isEOF(), isDirtyLine(), sawOctalEscape(), hadError()
         {}
     };
 
@@ -1019,7 +1018,7 @@ class MOZ_STACK_CLASS TokenStream
         }
     }
 
-    void updateLineInfoForEOL();
+    MOZ_MUST_USE MOZ_ALWAYS_INLINE bool updateLineInfoForEOL();
     void updateFlagsForEOL();
 
     const Token& nextToken() const {
