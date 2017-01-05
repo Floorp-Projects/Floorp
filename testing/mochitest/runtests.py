0395a07b4448857bb85098c4476f22ed899e4dd8
Kris Maglione <maglione.k@gmail.com>
2017-01-05 14:53:55 -0800

    Bug 1326456: Re-enable leak checks on OS-X. r=jmaher
    MozReview-Commit-ID: 18a18QFaiEC

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -2232,7 +2232,6 @@ toolbar#nav-bar {
 
             info = mozinfo.info
             skip_leak_conditions = [
-                (info['debug'] and options.flavor == 'plain' and d.startswith('toolkit/components/extensions/test/mochitest') and info['os'] == 'mac', 'bug 1326456'),  # noqa
                 (info['debug'] and options.flavor == 'plain' and d == 'toolkit/components/prompts/test' and info['os'] == 'mac', 'bug 1325275'),  # noqa
             ]
 
