5bf4bde134c8b3b8ffe535a2e2e00ab972b1a6a6
Andrea Marchesini <amarchesini@mozilla.com>
2017-01-05 17:07:54 +0100

    Bug 1294533 - FileReader should start reading data only after allocating the buffer, r=bkelly

--- dom/file/FileReader.cpp
+++ dom/file/FileReader.cpp
@@ -412,15 +412,6 @@ FileReader::ReadFileContent(Blob& aBlob,
     return;
   }
 
-  aRv = DoAsyncWait();
-  if (NS_WARN_IF(aRv.Failed())) {
-    return;
-  }
-
-  //FileReader should be in loading state here
-  mReadyState = LOADING;
-  DispatchProgressEvent(NS_LITERAL_STRING(LOADSTART_STR));
-
   if (mDataFormat == FILE_AS_ARRAYBUFFER) {
     mFileData = js_pod_malloc<char>(mTotal);
     if (!mFileData) {
@@ -428,6 +419,16 @@ FileReader::ReadFileContent(Blob& aBlob,
       aRv.Throw(NS_ERROR_OUT_OF_MEMORY);
     }
   }
+
+  aRv = DoAsyncWait();
+  if (NS_WARN_IF(aRv.Failed())) {
+    FreeFileData();
+    return;
+  }
+
+  //FileReader should be in loading state here
+  mReadyState = LOADING;
+  DispatchProgressEvent(NS_LITERAL_STRING(LOADSTART_STR));
 }
 
 nsresult
