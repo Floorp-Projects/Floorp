af06080b87e5ac570edb8a512e9b32eea793bc6a
Patrick McManus <mcmanus@ducksong.com>
2017-01-05 10:40:17 -0500

    Bug 1321783 - Make updater be networking conservative r=dkeeler r=rstrong r=dragana a=lizzard

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -2058,6 +2058,7 @@ HttpChannelChild::ContinueAsyncOpen()
   openArgs.appCacheClientID() = appCacheClientId;
   openArgs.allowSpdy() = mAllowSpdy;
   openArgs.allowAltSvc() = mAllowAltSvc;
+  openArgs.beConservative() = mBeConservative;
   openArgs.initialRwin() = mInitialRwin;
 
   uint32_t cacheKey = 0;
