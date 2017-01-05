31bce7285abc7b2abac8e6646d154322766e40a3
Matheus Longaray <mlongaray@hp.com>
2017-01-05 17:38:13 +0100

    Bug 1323987 - Update browser printing code accordingly. r=mconley
    This patch updates browser printing code to make use of about:printpreview
    when loading new tab for print preview. The same URI is used when the user
    makes use of simplify page feature while print previewing.
    
    MozReview-Commit-ID: DXHT71hpnWo

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3353,7 +3353,7 @@ var PrintPreviewListener = {
       let browser = gBrowser.selectedBrowser;
       let preferredRemoteType = browser.remoteType;
       this._tabBeforePrintPreview = gBrowser.selectedTab;
-      this._printPreviewTab = gBrowser.loadOneTab("about:blank",
+      this._printPreviewTab = gBrowser.loadOneTab("about:printpreview",
                                                   { inBackground: false,
                                                     preferredRemoteType,
                                                     relatedBrowser: browser });
@@ -3362,7 +3362,7 @@ var PrintPreviewListener = {
     return gBrowser.getBrowserForTab(this._printPreviewTab);
   },
   createSimplifiedBrowser() {
-    this._simplifyPageTab = gBrowser.loadOneTab("about:blank",
+    this._simplifyPageTab = gBrowser.loadOneTab("about:printpreview",
                                                 { inBackground: true });
     return this.getSimplifiedSourceBrowser();
   },
