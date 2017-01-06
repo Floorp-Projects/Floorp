3a05577b43c63f945d01b210f9fb0682e0ee554f
Ting-Yu Lin <tlin@mozilla.com>
2017-01-06 16:35:29 +0800

    Bug 1311244 Part 2 - Create ShapeUtils, and move EnumerationToLength into it. r=dbaron
    More functions in nsCSSClipPathInstance will be refactored and moved into
    ShapeUtils in subsequent patches.
    
    MozReview-Commit-ID: LmJUevY8YGr

---
+++ layout/base/ShapeUtils.cpp
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/ShapeUtils.h"
+
+#include <cstdlib>
+
+namespace mozilla {
+
+nscoord
+ShapeUtils::ComputeShapeRadius(const StyleShapeRadius aType,
+                               const nscoord aCenter,
+                               const nscoord aPosMin,
+                               const nscoord aPosMax)
+{
+  nscoord dist1 = std::abs(aPosMin - aCenter);
+  nscoord dist2 = std::abs(aPosMax - aCenter);
+  nscoord length = 0;
+  switch (aType) {
+    case StyleShapeRadius::FarthestSide:
+      length = dist1 > dist2 ? dist1 : dist2;
+      break;
+    case StyleShapeRadius::ClosestSide:
+      length = dist1 > dist2 ? dist2 : dist1;
+      break;
+  }
+  return length;
+}
+
+} // namespace mozilla
