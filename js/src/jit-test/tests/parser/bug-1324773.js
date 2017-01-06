ec9ddb69347ac11d7523c44d6f2091a49973a118
Jon Coppeard <jcoppeard@mozilla.com>
2017-01-06 11:23:22 +0000

    Bug 1324773 - Sweep JSCompartment::varNames_ r=sfink a=abillings

---
+++ js/src/jit-test/tests/parser/bug-1324773.js
@@ -0,0 +1,13 @@
+if (!('gczeal' in this))
+    quit();
+var lfGlobal = newGlobal();
+lfGlobal.evaluate(`
+    for (var i = 0; i < 600; i++)
+        eval('function f' + i + '() {}');
+`);
+var lfGlobal = newGlobal();
+lfGlobal.evaluate(`
+    var dbg = new Debugger();
+    gczeal(9, 1);
+    dbg.findScripts({});
+`);
