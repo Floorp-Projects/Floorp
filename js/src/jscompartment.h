ff46bde7d075ffda6a3f908ef22dfafa184a9847
Jon Coppeard <jcoppeard@mozilla.com>
2017-01-06 11:23:22 +0000

    Bug 1324773 - Sweep JSCompartment::varNames_ r=sfink a=abillings a=jcristau

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -653,6 +653,7 @@ struct JSCompartment
     void sweepDebugEnvironments();
     void sweepNativeIterators();
     void sweepTemplateObjects();
+    void sweepVarNames();
 
     void purge();
     void clearTables();
