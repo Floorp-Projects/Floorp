52b0ebbba99c0d3d99bb945515b71ec45d874039
Nicolas Silva <nsilva@mozilla.com>
2017-01-06 19:10:20 +0100

    Bug 1328602 - Add a few comments explaining where some of the pieces are meant to fit. r=gfx?

--- gfx/layers/wr/RendererOGL.h
+++ gfx/layers/wr/RendererOGL.h
@@ -8,6 +8,7 @@
 #define MOZILLA_LAYERS_RENDEREROGL_H
 
 #include "mozilla/layers/RenderThread.h"
+#include "mozilla/layers/WebRenderTypes.h"
 #include "mozilla/gfx/webrender.h"
 
 namespace mozilla {
@@ -28,29 +29,39 @@ namespace layers {
 
 class WebRenderBridgeParent;
 
+/// Owns the WebRender renderer and GL context.
+///
+/// There is one renderer per window, all owned by the render thread.
+/// This class is a similar abstraction to CompositorOGL except that it is used
+/// on the render thread instead of the compositor thread.
 class RendererOGL
 {
 public:
 
+  /// Render thread only.
   static RendererOGL* Create(already_AddRefed<RenderThread> aThread,
                              already_AddRefed<widget::CompositorWidget> aWidget,
                              WrRenderer* aWrRenderer,
-                             uint64_t aPipelineId,
+                             gfx::WindowId aWindowId,
                              WebRenderBridgeParent* aBridge);
 
+  /// Render thread only.
   void Update();
 
+  /// Render thread only.
   bool Render(uint64_t aTransactionId);
 
+  /// Render thread only.
   bool RenderToTarget(gfx::DrawTarget& aTarget);
 
+  /// Render thread only.
   ~RendererOGL();
 
 protected:
   RendererOGL(already_AddRefed<RenderThread> aThread,
               already_AddRefed<gl::GLContext> aGL,
               already_AddRefed<widget::CompositorWidget>,
-              uint64_t aPipelineId,
+              gfx::WindowId aWindowId,
               WrRenderer* aWrRenderer,
               WebRenderBridgeParent* aBridge);
 
@@ -59,7 +70,7 @@ protected:
   RefPtr<widget::CompositorWidget> mWidget;
   WrRenderer* mWrRenderer;
   WebRenderBridgeParent* mBridge;
-  uint64_t mPipelineId;
+  gfx::WindowId mWindowId;
 };
 
 }
