a019bdcabbc4a7794e9f70b289272280baf0f071
Jory A. Pratt <anarchy@gentoo.org>
2017-01-08 11:34:00 -0500

    Bug 1320560 - Use C99 math isfinite, finite has been depreciated and all modern libc implementations use isfinite. r=froydnj

--- xpcom/ds/nsMathUtils.h
+++ xpcom/ds/nsMathUtils.h
@@ -103,12 +103,8 @@ NS_finite(double aNum)
 #ifdef WIN32
   // NOTE: '!!' casts an int to bool without spamming MSVC warning C4800.
   return !!_finite(aNum);
-#elif defined(XP_DARWIN)
-  // Darwin has deprecated |finite| and recommends |isfinite|. The former is
-  // not present in the iOS SDK.
-  return std::isfinite(aNum);
 #else
-  return finite(aNum);
+  return std::isfinite(aNum);
 #endif
 }
 
