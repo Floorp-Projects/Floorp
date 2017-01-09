42e1871519364fc789c8c0fa24e75ca9028e6211
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2017-01-09 22:04:05 +0000

    Bug 1329220 - fix print preview source browser confusion, r=mconley
    Restore overwriting of this._sourceBrowser as it was before bug 1308621
    to ensure we continue to use the same DOM to do print previewing (and
    eventually printing).
    
    Bug 1308621 already changed the enterPrintPreview code to rely on the
    member _sourceBrowser variable, which will be updated to point to the
    print preview browser if/when print preview is being reinitialized for
    the same page (because one of the print settings changes). We need to
    do this to avoid re-initializing off the original browser, which may
    now have navigated or be displaying something else entirely.
    
    This also updates the 'simplified' mode code to rely on the extant
    _originalURL member to avoid displaying the page URL as about:blank
    after a settings change.
    
    MozReview-Commit-ID: DZ1kT7Mb0mS
    
    --HG--
    extra : rebase_source : cb79c835d6f8bcd67a7118de3a19b1cab85b6593

---
+++ toolkit/components/printing/tests/browser_page_change_print_original.js
@@ -0,0 +1,57 @@
+/**
+ * Verify that if the page contents change after print preview is initialized,
+ * and we re-initialize print preview (e.g. by changing page orientation),
+ * we still show (and will therefore print) the original contents.
+ */
+add_task(function* pp_after_orientation_change() {
+  const DATA_URI = `data:text/html,<script>window.onafterprint = function() { setTimeout("window.location = 'data:text/plain,REPLACED PAGE!'", 0); }</script><pre>INITIAL PAGE</pre>`;
+  // Can only do something if we have a print preview UI:
+  if (AppConstants.platform != "win" && AppConstants.platform != "linux") {
+    ok(true, "Can't test if there's no print preview.");
+    return;
+  }
+
+  // Ensure we get a browserStopped for this browser
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, DATA_URI, false, true);
+  let browserToPrint = tab.linkedBrowser;
+  let ppBrowser = PrintPreviewListener.getPrintPreviewBrowser();
+
+  // Get a promise now that resolves when the original tab's location changes.
+  let originalTabNavigated = BrowserTestUtils.browserStopped(browserToPrint);
+
+  // Enter print preview:
+  let printPreviewEntered = BrowserTestUtils.waitForMessage(ppBrowser.messageManager, "Printing:Preview:Entered");
+  document.getElementById("cmd_printPreview").doCommand();
+  yield printPreviewEntered;
+
+  // Assert that we are showing the original page
+  yield ContentTask.spawn(ppBrowser, null, function* () {
+    is(content.document.body.textContent, "INITIAL PAGE", "Should have initial page print previewed.");
+  });
+
+  yield originalTabNavigated;
+
+  // Change orientation and wait for print preview to re-enter:
+  let orient = PrintUtils.getPrintSettings().orientation;
+  let orientToSwitchTo = orient != Ci.nsIPrintSettings.kPortraitOrientation ?
+    "portrait" : "landscape";
+  let printPreviewToolbar = document.querySelector("toolbar[printpreview=true]");
+
+  printPreviewEntered = BrowserTestUtils.waitForMessage(ppBrowser.messageManager, "Printing:Preview:Entered");
+  printPreviewToolbar.orient(orientToSwitchTo);
+  yield printPreviewEntered;
+
+  // Check that we're still showing the original page.
+  yield ContentTask.spawn(ppBrowser, null, function* () {
+    is(content.document.body.textContent, "INITIAL PAGE", "Should still have initial page print previewed.");
+  });
+
+  // Check that the other tab is definitely showing the new page:
+  yield ContentTask.spawn(browserToPrint, null, function* () {
+    is(content.document.body.textContent, "REPLACED PAGE!", "Original page should have changed.");
+  });
+
+  PrintUtils.exitPrintPreview();
+
+  yield BrowserTestUtils.removeTab(tab);
+});
