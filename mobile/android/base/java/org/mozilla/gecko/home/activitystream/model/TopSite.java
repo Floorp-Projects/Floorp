ee2e32c874334f7da3aea9b6775249ffcd6edd93
Sebastian Kaspari <s.kaspari@gmail.com>
2017-01-09 14:39:34 +0100

    Bug 1328937 - Introduce shared interface for activity stream models. r=Grisha
    MozReview-Commit-ID: EkfmQfCQNMM

--- mobile/android/base/java/org/mozilla/gecko/home/activitystream/model/TopSite.java
+++ mobile/android/base/java/org/mozilla/gecko/home/activitystream/model/TopSite.java
@@ -10,11 +10,12 @@ import android.support.annotation.Nullable;
 
 import org.mozilla.gecko.db.BrowserContract;
 
-public class TopSite {
+public class TopSite implements Item {
     private final long id;
     private final String url;
     private final String title;
-    private @Nullable final Boolean isBookmarked;
+    private @Nullable Boolean isBookmarked;
+    private final @Nullable boolean isPinned;
     private @BrowserContract.TopSites.TopSiteType final int type;
 
     public static TopSite fromCursor(Cursor cursor) {
@@ -39,6 +40,7 @@ public class TopSite {
         this.url = url;
         this.title = title;
         this.isBookmarked = isBookmarked;
+        this.isPinned = type == BrowserContract.TopSites.TYPE_PINNED;
         this.type = type;
     }
 
@@ -64,7 +66,18 @@ public class TopSite {
         return type;
     }
 
-    public boolean isPinned() {
-        return type == BrowserContract.TopSites.TYPE_PINNED;
+    public Boolean isPinned() {
+        return isPinned;
+    }
+
+    @Override
+    public void updateBookmarked(boolean bookmarked) {
+        this.isBookmarked = bookmarked;
+    }
+
+    @Override
+    public void updatePinned(boolean pinned) {
+        throw new UnsupportedOperationException(
+                "Pinned state of a top site should be known at the time of querying the database already");
     }
 }
