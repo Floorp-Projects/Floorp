c2b41f9948309074a519a7042f689f6498f7535d
Nicolas Silva <nsilva@mozilla.com>
2017-01-10 10:17:30 +0100

    Bug 1328602 - Begin implementing a C++ wrapper around WebRender's RenderApi. r=gfx?

---
+++ gfx/layers/wr/WebRenderAPI.cpp
@@ -0,0 +1,114 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "WebRenderAPI.h"
+#include "mozilla/layers/RendererOGL.h"
+#include "mozilla/layers/WebRenderBridgeParent.h"
+#include "mozilla/layers/CompositorThread.h"
+#include "mozilla/widget/CompositorWidget.h"
+
+namespace mozilla {
+namespace layers {
+
+class NewRenderer : public RendererEvent
+{
+public:
+  NewRenderer(WrRenderer* aWrRenderer, WebRenderBridgeParent* aBridge,
+              RefPtr<widget::CompositorWidget>&& aWidget)
+  : mWrRenderer(aWrRenderer)
+  , mBridge(aBridge)
+  , mCompositorWidget(Move(aWidget))
+  {
+    MOZ_COUNT_CTOR(NewRenderer);
+  }
+
+  ~NewRenderer()
+  {
+    MOZ_COUNT_DTOR(NewRenderer);
+  }
+
+  virtual void Run(RenderThread& aRenderThread, gfx::WindowId aWindowId) override
+  {
+    RefPtr<RenderThread> thread = &aRenderThread;
+    auto renderer = RendererOGL::Create(thread.forget(),
+                                        mCompositorWidget.forget(),
+                                        mWrRenderer,
+                                        aWindowId,
+                                        mBridge);
+    if (renderer) {
+      aRenderThread.AddRenderer(aWindowId, Move(renderer));
+    } else {
+      // TODO: should notify the compositor thread rather than crash.
+      MOZ_CRASH("Failed to initialize the Renderer.");
+    }
+  }
+
+  WrRenderer* mWrRenderer;
+  WebRenderBridgeParent* mBridge;
+  RefPtr<widget::CompositorWidget> mCompositorWidget;
+};
+
+class RemoveRenderer : public RendererEvent
+{
+public:
+  RemoveRenderer() { MOZ_COUNT_CTOR(RemoveRenderer); }
+
+  ~RemoveRenderer() { MOZ_COUNT_DTOR(RemoveRenderer); }
+
+  virtual void Run(RenderThread& aRenderThread, gfx::WindowId aWindowId) override
+  {
+    aRenderThread.RemoveRenderer(aWindowId);
+  }
+};
+
+
+//static
+already_AddRefed<WebRenderAPI>
+WebRenderAPI::Create(bool aEnableProfiler,
+                     WebRenderBridgeParent* aBridge,
+                     RefPtr<widget::CompositorWidget>&& aWidget)
+{
+  MOZ_ASSERT(aBridge);
+  MOZ_ASSERT(aWidget);
+
+  static uint64_t sNextId = 1;
+  gfx::WindowId id(sNextId++);
+
+  WrAPI* api = nullptr;
+  WrRenderer* renderer = nullptr;
+
+  wr_window_new(id.mHandle, aEnableProfiler, &api, &renderer);
+  MOZ_ASSERT(renderer);
+  MOZ_ASSERT(api);
+
+  auto event = MakeUnique<NewRenderer>(renderer, aBridge, Move(aWidget));
+
+  // TODO use the WebRender API instead of scheduling on this message loop directly.
+  // this needs PR #688
+  RenderThread::Get()->RunEvent(id, Move(event));
+
+  RefPtr<WebRenderAPI> ptr(new WebRenderAPI(api, id));
+  return ptr.forget();
+}
+
+WebRenderAPI::~WebRenderAPI()
+{
+  auto event = MakeUnique<RemoveRenderer>();
+
+  // TODO use the WebRender API instead of scheduling on this message loop directly.
+  // this needs PR #688
+  RenderThread::Get()->RunEvent(mId, Move(event));
+
+#ifdef MOZ_ENABLE_WEBRENDER
+  // Need to wrap this in an ifdef otherwise VC++ emits a warning (treated as error)
+  // in the non-webrender targets.
+  // We should be able to remove this #ifdef if/when we remove the
+  // MOZ_MAKE_COMPILER_ASSUME_IS_UNREACHABLE annotations in webrender.h
+  wr_api_delete(mApi);
+#endif
+}
+
+} // namespace
+} // namespace
---
+++ gfx/layers/wr/WebRenderAPI.h
@@ -0,0 +1,51 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 sts=2 ts=8 et tw=99 : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_LAYERS_WEBRENDERAPI_H
+#define MOZILLA_LAYERS_WEBRENDERAPI_H
+
+#include "mozilla/AlreadyAddRefed.h"
+#include "mozilla/gfx/webrender.h"
+#include "mozilla/layers/WebRenderTypes.h"
+
+namespace mozilla {
+
+namespace widget {
+class CompositorWidget;
+}
+
+namespace layers {
+
+class WebRenderBridgeParent;
+class RendererOGL;
+
+class WebRenderAPI
+{
+NS_INLINE_DECL_REFCOUNTING(WebRenderAPI);
+public:
+  /// Compositor thread only.
+  static already_AddRefed<WebRenderAPI> Create(bool aEnableProfiler,
+                                               WebRenderBridgeParent* aBridge,
+                                               RefPtr<widget::CompositorWidget>&& aWidget);
+
+  gfx::WindowId GetId() const { return mId; }
+
+protected:
+  WebRenderAPI(WrAPI* aRawApi, gfx::WindowId aId)
+  : mApi(aRawApi)
+  , mId(aId)
+  {}
+
+  ~WebRenderAPI();
+
+  WrAPI* mApi;
+  gfx::WindowId mId;
+};
+
+} // namespace
+} // namespace
+
+#endif
