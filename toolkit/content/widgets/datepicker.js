4d95ad362b4b05de89d715d32333f9c4cffa0ef0
Scott Wu <scottcwwu@gmail.com>
2017-01-10 16:41:36 +0800

    Bug 1329589 - Set calendar variables for date picker using Intl.getCalendarInfo r=gandalf,mconley
    MozReview-Commit-ID: KmKVSAyNgeW

--- toolkit/content/widgets/datepicker.js
+++ toolkit/content/widgets/datepicker.js
@@ -38,16 +38,15 @@ function DatePicker(context) {
       const { year = now.getFullYear(),
               month = now.getMonth(),
               day = now.getDate(),
+              firstDayOfWeek,
+              weekends,
               locale } = this.props;
-
-      // TODO: Use calendar info API to get first day of week & weekends
-      //       (Bug 1287503)
       const dateKeeper = new DateKeeper({
         year, month, day
       }, {
-        calViewSize: CAL_VIEW_SIZE,
-        firstDayOfWeek: 0,
-        weekends: [0]
+        firstDayOfWeek,
+        weekends,
+        calViewSize: CAL_VIEW_SIZE
       });
 
       this.state = {
