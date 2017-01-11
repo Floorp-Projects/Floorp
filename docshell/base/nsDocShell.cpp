5a16281e8cc9c4173b10fe49094813a9dfba60af
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-01-11 15:42:10 +0100

    Backed out changeset 3b3884fa2026 (bug 1329288) for m10 test failures

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -68,7 +68,6 @@
 #include "nsIScriptSecurityManager.h"
 #include "nsIScriptObjectPrincipal.h"
 #include "nsIScrollableFrame.h"
-#include "nsContentPolicyUtils.h" // NS_CheckContentLoadPolicy(...)
 #include "nsISeekableStream.h"
 #include "nsAutoPtr.h"
 #include "nsQueryObject.h"
@@ -9864,31 +9863,6 @@ nsDocShell::InternalLoad(nsIURI* aURI,
       MOZ_ASSERT(mItemType == elementDocShell->ItemType(),
                 "subframes should have the same docshell type as their parent");
 #endif
-    }
-
-    // XXXbz would be nice to know the loading principal here... but we don't    
-    nsCOMPtr<nsIPrincipal> requestingPrincipal = aTriggeringPrincipal;
-    if (!requestingPrincipal && aReferrer) {
-      rv =
-        CreatePrincipalFromReferrer(aReferrer, getter_AddRefs(requestingPrincipal));
-      NS_ENSURE_SUCCESS(rv, rv);
-    }
-
-    int16_t shouldLoad = nsIContentPolicy::ACCEPT;
-    rv = NS_CheckContentLoadPolicy(contentType,
-                                   aURI,
-                                   requestingPrincipal,
-                                   requestingContext,
-                                   EmptyCString(),  // mime guess
-                                   nullptr,  // extra
-                                   &shouldLoad);
-  
-    if (NS_FAILED(rv) || NS_CP_REJECTED(shouldLoad)) {
-      if (NS_SUCCEEDED(rv) && shouldLoad == nsIContentPolicy::REJECT_TYPE) {
-        return NS_ERROR_CONTENT_BLOCKED_SHOW_ALT;
-      }
-
-      return NS_ERROR_CONTENT_BLOCKED;
     }
 
     // If HSTS priming was set by nsMixedContentBlocker::ShouldLoad, and we
