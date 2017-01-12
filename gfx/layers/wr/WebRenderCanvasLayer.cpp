68d615c0bd3d49b8c74bec2dfb68ea489df31e69
Ethan Lin <ethlin@mozilla.com>
2017-01-12 16:09:07 +0800

    Bug 1322504 - Part2. Fix bound/format problem after using ContentClient. r=sotaro

--- gfx/layers/wr/WebRenderCanvasLayer.cpp
+++ gfx/layers/wr/WebRenderCanvasLayer.cpp
@@ -70,11 +70,12 @@ WebRenderCanvasLayer::RenderLayer()
       clip = rect;
   }
   if (gfxPrefs::LayersDump()) printf_stderr("CanvasLayer %p using rect:%s clip:%s\n", this, Stringify(rect).c_str(), Stringify(clip).c_str());
+
   gfx::Rect relBounds = TransformedVisibleBoundsRelativeToParent();
   gfx::Rect overflow(0, 0, relBounds.width, relBounds.height);
   WRBridge()->AddWebRenderCommand(
       OpDPPushStackingContext(ToWRRect(relBounds), ToWRRect(overflow), Nothing(), transform, FrameMetrics::NULL_SCROLL_ID));
-  WRBridge()->AddWebRenderCommand(OpDPPushExternalImageId(ToWRRect(rect), ToWRRect(clip), Nothing(), mExternalImageId));
+  WRBridge()->AddWebRenderCommand(OpDPPushExternalImageId(LayerIntRegion(), ToWRRect(rect), ToWRRect(clip), Nothing(), mExternalImageId));
 
   if (gfxPrefs::LayersDump()) printf_stderr("CanvasLayer %p using %s as bounds/overflow, %s for transform\n", this, Stringify(relBounds).c_str(), Stringify(transform).c_str());
   WRBridge()->AddWebRenderCommand(OpDPPopStackingContext());
