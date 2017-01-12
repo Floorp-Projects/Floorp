87cc4b90c33d2050beec9530c20b41fceda565a2
Blake Kaplan <mrbkap@gmail.com>
2017-01-12 15:33:05 -0800

    Bug 1330822 - Remove CPOWs from browser_usercontext.js. r=Gijs
    MozReview-Commit-ID: 6gFJKF76ZDE

--- browser/components/contextualidentity/test/browser/browser_usercontext.js
+++ browser/components/contextualidentity/test/browser/browser_usercontext.js
@@ -69,7 +69,7 @@ add_task(function* test() {
     yield BrowserTestUtils.browserLoaded(browser);
 
     // get the title
-    let title = browser.contentDocument.title.trim().split("|");
+    let title = browser.contentTitle.trim().split("|");
 
     // check each item in the title and validate it meets expectatations
     for (let part of title) {
