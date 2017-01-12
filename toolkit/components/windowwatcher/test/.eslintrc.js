db213f97f7214e584a78d1ae7c649556eb28522c
Mark Banner <standard8@mozilla.com>
2017-01-12 10:24:10 +0000

    Bug 1331006 - [eslint] Define more globals, and add a rule to import test globals from SimpleTest/EventUtils. r=mossop
    MozReview-Commit-ID: 5ifWLAa8wG2

---
+++ toolkit/components/windowwatcher/test/.eslintrc.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = { // eslint-disable-line no-undef
+  "extends": [
+    "../../../../testing/mochitest/browser.eslintrc.js",
+    "../../../../testing/mochitest/chrome.eslintrc.js",
+    "../../../../testing/mochitest/mochitest.eslintrc.js",
+  ]
+};
