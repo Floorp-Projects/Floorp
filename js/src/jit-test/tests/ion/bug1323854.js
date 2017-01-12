0acdf3bfdfc56e9f86c79016afad88e84a6b5732
Hannes Verschore <hv1989@gmail.com>
2017-01-12 17:18:30 +0100

    Bug 1323854 - IonMonkey: Part2: Keep the same semantics as bytecode on CFGCompare, r=nbp

---
+++ js/src/jit-test/tests/ion/bug1323854.js
@@ -0,0 +1,16 @@
+
+g = (function () {
+    "use asm";
+    function f(i0, d1) {
+        d1 = 4258326539 >>> 0;
+        switch (-8 && i0) {
+            case -1:
+                d1 = 0;
+            case 0:
+        }
+    }
+    return f;
+})();
+dis(g);
+g();
+g();
