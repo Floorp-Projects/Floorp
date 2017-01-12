213d81f2f41d0c07e181a2ddcc31f85314355d9b
Jared Wein <jwein@mozilla.com>
2017-01-12 12:10:07 -0500

    Bug 1323618 - Allow locking off of psuedo-classes through inIDOMUtils. r=heycam
    This patch doesn't currently work. The test fails in two test cases. Right now the styles for a 'locked-off' psuedo class are still being applied.
    
    MozReview-Commit-ID: DppYTmILpwy
    * * *
    [mq]: temp
    
    MozReview-Commit-ID: 74iIOQumfrw
    
    --HG--
    extra : rebase_source : 276e694b61aaeb0cb55a963c793d58876a92546f

--- devtools/shared/specs/inspector.js
+++ devtools/shared/specs/inspector.js
@@ -234,7 +234,8 @@ const walkerSpec = generateActorSpec({
       request: {
         node: Arg(0, "domnode"),
         pseudoClass: Arg(1),
-        parents: Option(2)
+        parents: Option(2),
+        enabled: Option(2, "boolean"),
       },
       response: {}
     },
