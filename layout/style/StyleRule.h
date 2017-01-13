f99593acd91bd7f2e3e857148e9338b59d9283dd
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-13 10:41:02 -0500

    Bug 851892 part 4.  Make css::Rule wrappercached.  r=heycam,peterv
    Note that this increases the size of css::Rule by three words, unfortunately.

--- layout/style/StyleRule.h
+++ layout/style/StyleRule.h
@@ -354,6 +354,9 @@ public:
 
   virtual size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const override;
 
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aGivenProto) override;
+
 private:
   ~StyleRule();
 
