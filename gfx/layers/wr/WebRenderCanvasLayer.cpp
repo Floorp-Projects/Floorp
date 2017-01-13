f6243f02ff7542b6c626505b86c5bef6af4c9aca
Ryan Hunt <rhunt@eqrion.net>
2017-01-13 11:59:07 -0600

    Bug 1328893 - Specify an ImageRendering for some WebRenderLayers r=kats

--- gfx/layers/wr/WebRenderCanvasLayer.cpp
+++ gfx/layers/wr/WebRenderCanvasLayer.cpp
@@ -69,15 +69,19 @@ WebRenderCanvasLayer::RenderLayer()
   } else {
       clip = rect;
   }
+
   if (gfxPrefs::LayersDump()) printf_stderr("CanvasLayer %p using rect:%s clip:%s\n", this, Stringify(rect).c_str(), Stringify(clip).c_str());
+
   gfx::Rect relBounds = TransformedVisibleBoundsRelativeToParent();
   gfx::Rect overflow(0, 0, relBounds.width, relBounds.height);
+  WRTextureFilter filter = (mFlags | TextureFlags::USE_NEAREST_FILTER) ? WRTextureFilter::Point : WRTextureFilter::Linear;
+
   WRBridge()->AddWebRenderCommand(
       OpDPPushStackingContext(ToWRRect(relBounds), ToWRRect(overflow), Nothing(), transform, FrameMetrics::NULL_SCROLL_ID));
-  WRBridge()->AddWebRenderCommand(OpDPPushExternalImageId(ToWRRect(rect), ToWRRect(clip), Nothing(), mExternalImageId));
+  WRBridge()->AddWebRenderCommand(OpDPPushExternalImageId(ToWRRect(rect), ToWRRect(clip), Nothing(), filter, mExternalImageId));
+  WRBridge()->AddWebRenderCommand(OpDPPopStackingContext());
 
   if (gfxPrefs::LayersDump()) printf_stderr("CanvasLayer %p using %s as bounds/overflow, %s for transform\n", this, Stringify(relBounds).c_str(), Stringify(transform).c_str());
-  WRBridge()->AddWebRenderCommand(OpDPPopStackingContext());
 }
 
 void
