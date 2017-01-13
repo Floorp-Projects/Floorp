aa25e42e9f39da73a46ea473e69182e0aa1c6791
Francois Marier <francois@mozilla.com>
2017-01-13 14:11:19 -0800

    Bug 1330253 - Enable test_safe_browsing_initial_download on try. r=dustin,whimboo
    MozReview-Commit-ID: 7SruSkfIhzI

--- testing/mozharness/configs/builds/releng_base_linux_64_builds.py
+++ testing/mozharness/configs/builds/releng_base_linux_64_builds.py
@@ -46,7 +46,7 @@ config = {
     'secret_files': [
         {'filename': '/builds/gapi.data',
          'secret_name': 'project/releng/gecko/build/level-%(scm-level)s/gapi.data',
-         'min_scm_level': 2, 'default': 'try-build-has-no-secrets'},
+         'min_scm_level': 1},
         {'filename': '/builds/mozilla-desktop-geoloc-api.key',
          'secret_name': 'project/releng/gecko/build/level-%(scm-level)s/mozilla-desktop-geoloc-api.key',
          'min_scm_level': 2, 'default': 'try-build-has-no-secrets'},
