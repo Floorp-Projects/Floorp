590fb01eaf81912ff490c4b27cef27d9cd09000a
Jeff Muizelaar <jmuizelaar@mozilla.com>
2017-01-13 15:21:52 -0500

    Bug 1330921. Actually backout 6699d31aaa45 because I forgot the bug number

--- gfx/layers/wr/WebRenderColorLayer.cpp
+++ gfx/layers/wr/WebRenderColorLayer.cpp
@@ -17,29 +17,16 @@ WebRenderColorLayer::RenderLayer()
 {
   WRScrollFrameStackingContextGenerator scrollFrames(this);
 
-  LayerIntRegion visibleRegion = GetVisibleRegion();
-  LayerIntRect bounds = visibleRegion.GetBounds();
-  Rect rect = RelativeToVisible(IntRectToRect(bounds.ToUnknownRect()));
-  Rect clip;
+  gfx::Rect rect = RelativeToParent(GetTransform().TransformBounds(IntRectToRect(mBounds)));
+  gfx::Rect clip;
   if (GetClipRect().isSome()) {
-      clip = RelativeToTransformedVisible(IntRectToRect(GetClipRect().ref().ToUnknownRect()));
+      clip = RelativeToParent(IntRectToRect(GetClipRect().ref().ToUnknownRect()));
   } else {
       clip = rect;
   }
   if (gfxPrefs::LayersDump()) printf_stderr("ColorLayer %p using rect:%s clip:%s\n", this, Stringify(rect).c_str(), Stringify(clip).c_str());
-
-
-  gfx::Matrix4x4 transform;// = GetTransform();
-  gfx::Rect relBounds = TransformedVisibleBoundsRelativeToParent();
-  gfx::Rect overflow(0, 0, relBounds.width, relBounds.height);
-
-  WRBridge()->AddWebRenderCommand(
-      OpDPPushStackingContext(ToWRRect(relBounds), ToWRRect(overflow), Nothing(), transform, FrameMetrics::NULL_SCROLL_ID));
   WRBridge()->AddWebRenderCommand(
     OpDPPushRect(ToWRRect(rect), ToWRRect(clip), mColor.r, mColor.g, mColor.b, mColor.a));
-
-  if (gfxPrefs::LayersDump()) printf_stderr("ColorLayer %p using %s as bounds, %s as overflow, %s for transform\n", this, Stringify(relBounds).c_str(), Stringify(overflow).c_str(), Stringify(transform).c_str());
-  WRBridge()->AddWebRenderCommand(OpDPPopStackingContext());
 }
 
 } // namespace layers
