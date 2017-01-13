3f2ab6c4271c3e2bfc850349d3f65875db7683e8
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-13 10:41:02 -0500

    Bug 851892 part 2.  Remove the now-unused GetExistingDOMRule method.  r=heycam

--- layout/style/StyleRule.cpp
+++ layout/style/StyleRule.cpp
@@ -1464,12 +1464,6 @@ StyleRule::GetDOMRule()
   return mDOMRule;
 }
 
-/* virtual */ nsIDOMCSSRule*
-StyleRule::GetExistingDOMRule()
-{
-  return mDOMRule;
-}
-
 void
 StyleRule::SetDeclaration(Declaration* aDecl)
 {
