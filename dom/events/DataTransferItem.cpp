0b7f9700e0a2e3fe1e416967ffc90cde38921504
Michael Layzell <michael@thelayzells.com>
2017-01-13 14:35:51 -0500

    Bug 1330979 - Don't raise NS_ERROR_DOM_SECURITY_ERROR on DataTransfer access violations. r=baku, a=lizzard
    In our previous code, we would accidentially not perform the correct
    security checks when retreiving data from DataTransferItems in some
    situations. The regressing patch fixed this behavior and ensured that we
    always perform the correct security checks, making any "insecure"
    accesses (such as accesses to file data from the OS in DragEnter) would
    raise an NS_ERROR_DOM_SECURTY_ERROR. This behavior is not consistent
    with Chrome, and thus OneDrive would accidentally trigger an exception
    and break its d&d handling logic.
    
    With this patch our behavior for "insecure" accesses to file data from
    the OS is more in line with Chrome's, in that we now don't raise an
    exception, but instead just produce the value "null" when the data
    should not be avaliable yet. From my quick test this fixes the problem
    with OneDrive, and should be a fairly trivial patch to uplift to Beta,
    with very little risk.
    
    This patch doesn't include a test, as Drag and Drop is a very difficult
    component to test in automation, however I am currently working on
    defining a set of manual tests for the Drag and Drop component, and I'll
    make sure that our behavior for "insecure" accesses is tested in those
    tests in the future.
    
    MozReview-Commit-ID: 4pnPyL1tgcV

--- dom/events/DataTransferItem.cpp
+++ dom/events/DataTransferItem.cpp
@@ -481,9 +481,13 @@ DataTransferItem::Data(nsIPrincipal* aPrincipal, ErrorResult& aRv)
   // source of the drag is in a child frame of the caller. In that case,
   // we only allow access to data of the same principal. During other events,
   // only allow access to the data with the same principal.
+  //
+  // We don't want to fail with an exception in this siutation, rather we want
+  // to just pretend as though the stored data is "nullptr". This is consistent
+  // with Chrome's behavior and is less surprising for web applications which
+  // don't expect execptions to be raised when performing certain operations.
   if (Principal() && checkItemPrincipal &&
       !aPrincipal->Subsumes(Principal())) {
-    aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
     return nullptr;
   }
 
@@ -498,13 +502,11 @@ DataTransferItem::Data(nsIPrincipal* aPrincipal, ErrorResult& aRv)
     if (pt) {
       nsIScriptContext* c = pt->GetContextForEventHandlers(&rv);
       if (NS_WARN_IF(NS_FAILED(rv) || !c)) {
-        aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
         return nullptr;
       }
 
       nsIGlobalObject* go = c->GetGlobalObject();
       if (NS_WARN_IF(!go)) {
-        aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
         return nullptr;
       }
 
@@ -513,7 +515,6 @@ DataTransferItem::Data(nsIPrincipal* aPrincipal, ErrorResult& aRv)
 
       nsIPrincipal* dataPrincipal = sp->GetPrincipal();
       if (NS_WARN_IF(!dataPrincipal || !aPrincipal->Equals(dataPrincipal))) {
-        aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
         return nullptr;
       }
     }
