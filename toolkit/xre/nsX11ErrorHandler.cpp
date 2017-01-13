9eb887fe2a5cb4e4a336fd7062668b876751ee3d
Karl Tomlinson <karlt+@karlt.net>
2017-01-13 20:50:58 +1300

    Bug 1331320 - Install X11 error handler through Xlib in plugin process as GTK2 does not use g_error. r=glandium, a=jcristau
    MozReview-Commit-ID: 9Io2fABc2hI

--- toolkit/xre/nsX11ErrorHandler.cpp
+++ toolkit/xre/nsX11ErrorHandler.cpp
@@ -149,7 +149,6 @@ X11Error(Display *display, XErrorEvent *event) {
 }
 }
 
-#if (MOZ_WIDGET_GTK != 3)
 void
 InstallX11ErrorHandler()
 {
@@ -161,4 +160,3 @@ InstallX11ErrorHandler()
     XSynchronize(display, True);
   }
 }
-#endif
