fded75d0b5e78973f170ac419de4f06e8383e0ce
Tom Tromey <tom@tromey.com>
2017-01-13 10:16:17 -0700

    Bug 1060419 - introduce mozilla::Smprintf and friends; r=froydnj
    MozReview-Commit-ID: BsmaFtJEvQH
    
    --HG--
    extra : rebase_source : 4b552141b26d35f63e63a75191b90c1f257a2152

--- js/src/jsprf.h
+++ js/src/jsprf.h
@@ -29,37 +29,56 @@
 
 #include "mozilla/IntegerPrintfMacros.h"
 #include "mozilla/SizePrintfMacros.h"
+#include "mozilla/Types.h"
 
 #include <stdarg.h>
 
 #include "jstypes.h"
 
+namespace mozilla {
+
 /*
 ** sprintf into a malloc'd buffer. Return a pointer to the malloc'd
-** buffer on success, nullptr on failure. Call "JS_smprintf_free" to release
+** buffer on success, nullptr on failure. Call "SmprintfFree" to release
 ** the memory returned.
 */
-extern JS_PUBLIC_API(char*) JS_smprintf(const char* fmt, ...)
+extern MFBT_API char* Smprintf(const char* fmt, ...)
     MOZ_FORMAT_PRINTF(1, 2);
 
 /*
-** Free the memory allocated, for the caller, by JS_smprintf
+** Free the memory allocated, for the caller, by Smprintf
 */
-extern JS_PUBLIC_API(void) JS_smprintf_free(char* mem);
+extern MFBT_API void SmprintfFree(char* mem);
 
 /*
 ** "append" sprintf into a malloc'd buffer. "last" is the last value of
 ** the malloc'd buffer. sprintf will append data to the end of last,
-** growing it as necessary using realloc. If last is nullptr, JS_sprintf_append
+** growing it as necessary using realloc. If last is nullptr, SmprintfAppend
 ** will allocate the initial string. The return value is the new value of
 ** last for subsequent calls, or nullptr if there is a malloc failure.
 */
-extern JS_PUBLIC_API(char*) JS_sprintf_append(char* last, const char* fmt, ...)
+extern MFBT_API char* SmprintfAppend(char* last, const char* fmt, ...)
     MOZ_FORMAT_PRINTF(2, 3);
 
 /*
 ** va_list forms of the above.
 */
+extern MFBT_API char* Vsmprintf(const char* fmt, va_list ap);
+extern MFBT_API char* VsmprintfAppend(char* last, const char* fmt, va_list ap);
+
+} // namespace mozilla
+
+/* Wrappers for mozilla::Smprintf and friends that are used throughout
+   JS.  */
+
+extern JS_PUBLIC_API(char*) JS_smprintf(const char* fmt, ...)
+    MOZ_FORMAT_PRINTF(1, 2);
+
+extern JS_PUBLIC_API(void) JS_smprintf_free(char* mem);
+
+extern JS_PUBLIC_API(char*) JS_sprintf_append(char* last, const char* fmt, ...)
+     MOZ_FORMAT_PRINTF(2, 3);
+
 extern JS_PUBLIC_API(char*) JS_vsmprintf(const char* fmt, va_list ap);
 extern JS_PUBLIC_API(char*) JS_vsprintf_append(char* last, const char* fmt, va_list ap);
 
