927284d4ff44d0e124441bcd793d25b67e9cb48d
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-01-13 10:50:33 +0100

    Backed out changeset 314efb239b64 (bug 1147371)

--- js/src/builtin/Map.js
+++ js/src/builtin/Map.js
@@ -14,14 +14,44 @@ function MapConstructorInit(iterable) {
     if (!IsCallable(adder))
         ThrowTypeError(JSMSG_NOT_FUNCTION, typeof adder);
 
-    // Steps 6.c-8.
-    for (var nextItem of allowContentIter(iterable)) {
+    // Step 6.c.
+    var iterFn = iterable[std_iterator];
+    if (!IsCallable(iterFn))
+        ThrowTypeError(JSMSG_NOT_ITERABLE, DecompileArg(0, iterable));
+
+    var iter = callContentFunction(iterFn, iterable);
+    if (!IsObject(iter))
+        ThrowTypeError(JSMSG_NOT_NONNULL_OBJECT, typeof iter);
+
+    // Step 7 (not applicable).
+
+    // Step 8.
+    while (true) {
+        // Step 8.a.
+        var next = callContentFunction(iter.next, iter);
+        if (!IsObject(next))
+            ThrowTypeError(JSMSG_NOT_NONNULL_OBJECT, typeof next);
+
+        // Step 8.b.
+        if (next.done)
+            return;
+
+        // Step 8.c.
+        var nextItem = next.value;
+
         // Step 8.d.
-        if (!IsObject(nextItem))
+        if (!IsObject(nextItem)) {
+            IteratorCloseThrow(iter);
             ThrowTypeError(JSMSG_INVALID_MAP_ITERABLE, "Map");
+        }
 
         // Steps 8.e-j.
-        callContentFunction(adder, map, nextItem[0], nextItem[1]);
+        try {
+            callContentFunction(adder, map, nextItem[0], nextItem[1]);
+        } catch (e) {
+            IteratorCloseThrow(iter);
+            throw e;
+        }
     }
 }
 
