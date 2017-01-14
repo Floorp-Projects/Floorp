64affe9854eec72c9d7ec98416437fdd5c5a5cf7
Wes Kocher <wkocher@mozilla.com>
2017-01-13 17:02:57 -0800

    Backed out changeset 4dc794806128 (bug 1317374) for spidermonkey bustage a=backout
    MozReview-Commit-ID: EJDwWWtrHTb

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -1177,12 +1177,6 @@ Parser<ParseHandler>::tryDeclareVar(HandlePropertyName name, DeclarationKind kin
                     *redeclaredKind = Some(declaredKind);
                     return true;
                 }
-            } else if (kind == DeclarationKind::VarForAnnexBLexicalFunction) {
-                MOZ_ASSERT(DeclarationKindIsParameter(declaredKind));
-
-                // Annex B.3.3.1 disallows redeclaring parameter names.
-                *redeclaredKind = Some(declaredKind);
-                return true;
             }
         } else {
             if (!scope->addDeclaredName(pc, p, name, kind))
