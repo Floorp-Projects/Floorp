686006613eebff9f55506274879e47a690df63ba
Phil Ringnalda <philringnalda@gmail.com>
2017-01-15 11:50:10 -0800

    Backed out 20 changesets (bug 851892) for load failures in Android crashtest-4
    Backed out changeset b9c4115cdeac (bug 851892)
    Backed out changeset 5f491bf49b85 (bug 851892)
    Backed out changeset 9bf5bcb3e8c5 (bug 851892)
    Backed out changeset a9cab46e8b45 (bug 851892)
    Backed out changeset 73858e15c8c0 (bug 851892)
    Backed out changeset a25638588b6b (bug 851892)
    Backed out changeset d58e0e5069ef (bug 851892)
    Backed out changeset d8da2a3d8f10 (bug 851892)
    Backed out changeset a8da3c34983f (bug 851892)
    Backed out changeset 29cf8acbd21e (bug 851892)
    Backed out changeset c53cd7bdf8b3 (bug 851892)
    Backed out changeset e841a2796375 (bug 851892)
    Backed out changeset 2eab85b00159 (bug 851892)
    Backed out changeset ca4b1fb9cae4 (bug 851892)
    Backed out changeset 74c0ba66f108 (bug 851892)
    Backed out changeset 719bb9f41e5b (bug 851892)
    Backed out changeset d6aa4c6192df (bug 851892)
    Backed out changeset dc81a167a75d (bug 851892)
    Backed out changeset 65422477b3a5 (bug 851892)
    Backed out changeset 3089dd379077 (bug 851892)

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -19,6 +19,12 @@ enum nsDOMClassInfoID
   eDOMClassInfo_DOMPrototype_id,
   eDOMClassInfo_DOMConstructor_id,
 
+  // CSS classes
+  eDOMClassInfo_CSSStyleRule_id,
+  eDOMClassInfo_CSSImportRule_id,
+  eDOMClassInfo_CSSMediaRule_id,
+  eDOMClassInfo_CSSNameSpaceRule_id,
+
   // XUL classes
 #ifdef MOZ_XUL
   eDOMClassInfo_XULCommandDispatcher_id,
@@ -34,10 +40,26 @@ enum nsDOMClassInfoID
   eDOMClassInfo_XULTreeBuilder_id,
 #endif
 
+  eDOMClassInfo_CSSMozDocumentRule_id,
+  eDOMClassInfo_CSSSupportsRule_id,
+
+  // @font-face in CSS
+  eDOMClassInfo_CSSFontFaceRule_id,
+
   eDOMClassInfo_ContentFrameMessageManager_id,
   eDOMClassInfo_ContentProcessMessageManager_id,
   eDOMClassInfo_ChromeMessageBroadcaster_id,
   eDOMClassInfo_ChromeMessageSender_id,
+
+  eDOMClassInfo_CSSKeyframeRule_id,
+  eDOMClassInfo_CSSKeyframesRule_id,
+
+  // @counter-style in CSS
+  eDOMClassInfo_CSSCounterStyleRule_id,
+
+  eDOMClassInfo_CSSPageRule_id,
+
+  eDOMClassInfo_CSSFontFeatureValuesRule_id,
 
   eDOMClassInfo_XULControlElement_id,
   eDOMClassInfo_XULLabeledControlElement_id,
