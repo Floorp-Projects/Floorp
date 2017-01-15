fe127a441750bffa8da6015c0e086e3353da3266
Steffen Wilberg <steffen.wilberg@web.de>
2017-01-15 22:00:58 +0100

    Bug 1326821 - about:preferences - Ctrl+K shortcut doesn't focus searchbar. r=Gijs, a=jcristau
    MozReview-Commit-ID: 3zNwW2b4K17

--- browser/components/preferences/in-content/tests/browser_bug731866.js
+++ browser/components/preferences/in-content/tests/browser_bug731866.js
@@ -13,9 +13,11 @@ var gElements;
 
 function checkElements(expectedPane) {
   for (let element of gElements) {
-    // preferences elements fail is_element_visible checks because they are never visible.
+    // keyset and preferences elements fail is_element_visible checks because they are never visible.
     // special-case the drmGroup item because its visibility depends on pref + OS version
-    if (element.nodeName == "preferences" || element.id === "drmGroup") {
+    if (element.nodeName == "keyset" ||
+        element.nodeName == "preferences" ||
+        element.id === "drmGroup") {
       continue;
     }
     let attributeValue = element.getAttribute("data-category");
