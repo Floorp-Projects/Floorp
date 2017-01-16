276f7423bb3c0ab3b6260226e78894e2c9b05083
Gabriele Svelto <gsvelto@mozilla.com>
2017-01-16 17:39:22 +0100

    Bug 1310703 - Introduce the pingsender executable; r=ted
    MozReview-Commit-ID: 2SEwwGywahQ

---
+++ toolkit/components/telemetry/pingsender/pingsender_win.cpp
@@ -0,0 +1,116 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <string>
+
+#include <windows.h>
+#include <wininet.h>
+
+namespace PingSender {
+
+using std::string;
+
+/**
+ * A helper to automatically close internet handles when they go out of scope
+ */
+class ScopedHInternet {
+public:
+  explicit ScopedHInternet(HINTERNET handle) : mHandle(handle) {}
+
+  ~ScopedHInternet() {
+    if (mHandle) {
+      InternetCloseHandle(mHandle);
+    }
+  }
+
+  bool empty() { return (mHandle == nullptr); }
+  HINTERNET get() { return mHandle; }
+
+private:
+  HINTERNET mHandle;
+};
+
+const size_t kUrlComponentsSchemeLength = 256;
+const size_t kUrlComponentsHostLength = 256;
+const size_t kUrlComponentsPathLength = 256;
+
+/**
+ * Post the specified payload to a telemetry server
+ *
+ * @param url The URL of the telemetry server
+ * @param payload The ping payload
+ */
+bool
+Post(const string& url, const string& payload)
+{
+  char scheme[kUrlComponentsSchemeLength];
+  char host[kUrlComponentsHostLength];
+  char path[kUrlComponentsPathLength];
+
+  URL_COMPONENTS components = {};
+  components.dwStructSize = sizeof(components);
+  components.lpszScheme = scheme;
+  components.dwSchemeLength = kUrlComponentsSchemeLength;
+  components.lpszHostName = host;
+  components.dwHostNameLength = kUrlComponentsHostLength;
+  components.lpszUrlPath = path;
+  components.dwUrlPathLength = kUrlComponentsPathLength;
+
+  if (!InternetCrackUrl(url.c_str(), url.size(), 0, &components)) {
+    PINGSENDER_LOG("ERROR: Could not separate the URL components\n");
+    return false;
+  }
+
+  ScopedHInternet internet(InternetOpen(kUserAgent,
+                                        INTERNET_OPEN_TYPE_PRECONFIG,
+                                        /* lpszProxyName */ NULL,
+                                        /* lpszProxyBypass */ NULL,
+                                        /* dwFlags */ 0));
+
+  if (internet.empty()) {
+    PINGSENDER_LOG("ERROR: Could not open wininet internet handle\n");
+    return false;
+  }
+
+  ScopedHInternet connection(InternetConnect(internet.get(),
+                                             host, components.nPort,
+                                             /* lpszUsername */ NULL,
+                                             /* lpszPassword */ NULL,
+                                             INTERNET_SERVICE_HTTP,
+                                             /* dwFlags */ 0,
+                                             /* dwContext */ NULL));
+
+  if (connection.empty()) {
+    PINGSENDER_LOG("ERROR: Could not connect\n");
+    return false;
+  }
+
+  DWORD flags = ((strcmp(scheme, "https") == 0) ? INTERNET_FLAG_SECURE : 0) |
+                INTERNET_FLAG_NO_COOKIES;
+  ScopedHInternet request(HttpOpenRequest(connection.get(), "POST", path,
+                                          /* lpszVersion */ NULL,
+                                          /* lpszReferer */ NULL,
+                                          /* lplpszAcceptTypes */ NULL,
+                                          flags,
+                                          /* dwContext */ NULL));
+
+  if (request.empty()) {
+    PINGSENDER_LOG("ERROR: Could not open HTTP POST request\n");
+    return false;
+  }
+
+  bool rv = HttpSendRequest(request.get(),
+                            /* lpszHeaders */ nullptr,
+                            /* dwHeadersLength */ 0,
+                            (LPVOID)payload.c_str(),
+                            payload.size());
+  if (!rv) {
+    PINGSENDER_LOG("ERROR: Could not execute HTTP POST request\n");
+  }
+
+  return rv;
+}
+
+} // namespace PingSender
