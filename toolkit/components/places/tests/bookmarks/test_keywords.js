fc3193aa2b323ef65737012ba6b7dad38d93967a
Mayank Srivastav <mayanksri18@yahoo.in>
2017-01-16 19:59:19 +0530

    Bug 1329926 - Remove deprecated API getURIForKeyword and updated test cases for PlacesUtils.keywords.fetch(). r=mak

--- toolkit/components/places/tests/bookmarks/test_keywords.js
+++ toolkit/components/places/tests/bookmarks/test_keywords.js
@@ -2,7 +2,7 @@ const URI1 = NetUtil.newURI("http://test1.mozilla.org/");
 const URI2 = NetUtil.newURI("http://test2.mozilla.org/");
 const URI3 = NetUtil.newURI("http://test3.mozilla.org/");
 
-function check_keyword(aURI, aKeyword) {
+function* check_keyword(aURI, aKeyword) {
   if (aKeyword)
     aKeyword = aKeyword.toLowerCase();
 
@@ -16,11 +16,11 @@ function check_keyword(aURI, aKeyword) {
   }
 
   if (aKeyword) {
-    let uri = PlacesUtils.bookmarks.getURIForKeyword(aKeyword);
-    Assert.equal(uri.spec, aURI.spec);
+    let uri = yield PlacesUtils.keywords.fetch(aKeyword);
+    Assert.equal(uri.url, aURI.spec);
     // Check case insensitivity.
-    uri = PlacesUtils.bookmarks.getURIForKeyword(aKeyword.toUpperCase());
-    Assert.equal(uri.spec, aURI.spec);
+    uri = yield PlacesUtils.keywords.fetch(aKeyword.toUpperCase());
+    Assert.equal(uri.url, aURI.spec);
   }
 }
 
@@ -66,10 +66,6 @@ function expectNotifications() {
 }
 
 add_task(function test_invalid_input() {
-  Assert.throws(() => PlacesUtils.bookmarks.getURIForKeyword(null),
-                /NS_ERROR_ILLEGAL_VALUE/);
-  Assert.throws(() => PlacesUtils.bookmarks.getURIForKeyword(""),
-                /NS_ERROR_ILLEGAL_VALUE/);
   Assert.throws(() => PlacesUtils.bookmarks.getKeywordForBookmark(null),
                 /NS_ERROR_ILLEGAL_VALUE/);
   Assert.throws(() => PlacesUtils.bookmarks.getKeywordForBookmark(0),
