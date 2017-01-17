7c7185f15c75b2be2af770b1dfaf4c8d364e1f1f
David Anderson <danderson@mozilla.com>
2017-01-16 23:51:24 -0800

    Rename the GPU process prefs. (bug 1330554, r=milan)

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4569,7 +4569,7 @@ pref("layers.acceleration.disabled", false);
 pref("layers.bench.enabled", false);
 
 #if defined(XP_WIN) && defined(NIGHTLY_BUILD)
-pref("layers.gpu-process.dev.enabled", true);
+pref("layers.gpu-process.enabled", true);
 pref("media.gpu-process-decoder", true);
 #endif
 
