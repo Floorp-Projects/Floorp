d4ced30f67d780b07db8571deb442ce33a66e13e
Wes Kocher <wkocher@mozilla.com>
2017-01-17 17:55:24 -0800

    Backed out 4 changesets (bug 1306329) for 3000+ hazards a=backout
    Backed out changeset 1c2f51ce3faf (bug 1306329)
    Backed out changeset 6bb17b9a62d8 (bug 1306329)
    Backed out changeset 377ca1419f1a (bug 1306329)
    Backed out changeset ad4e531c7070 (bug 1306329)
    
    MozReview-Commit-ID: KJAxdyJeJ6J

--- xpcom/build/xrecore.h
+++ xpcom/build/xrecore.h
@@ -12,6 +12,14 @@
 /**
  * Import/export macros for libXUL APIs.
  */
-#define XRE_API(type, name, params) type name params;
+#ifdef XPCOM_GLUE
+#define XRE_API(type, name, params) \
+  typedef type (NS_FROZENCALL * name##Type) params; \
+  extern name##Type name NS_HIDDEN;
+#elif defined(IMPL_LIBXUL)
+#define XRE_API(type, name, params) EXPORT_XPCOM_API(type) name params;
+#else
+#define XRE_API(type, name, params) IMPORT_XPCOM_API(type) name params;
+#endif
 
 #endif // xrecore_h__
