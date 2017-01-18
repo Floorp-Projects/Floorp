9846e87c77d1edaa81f4dcd9277e1639981a83d4
Kartikaya Gupta <kgupta@mozilla.com>
2017-01-18 08:06:13 -0500

    Back out bug 1329609 (2 csets) for non-QR reftest failures. r=backout

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4661,8 +4661,6 @@ pref("layers.bufferrotation.enabled", true);
 pref("layers.componentalpha.enabled", true);
 pref("layers.draw-mask-debug", false);
 
-pref("layers.advanced.border-layers", false);
-
 pref("gfx.content.always-paint", false);
 
 #ifdef ANDROID
