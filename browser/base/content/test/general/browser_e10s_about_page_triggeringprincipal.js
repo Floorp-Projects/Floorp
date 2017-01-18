17b31799111a47017d56ffc3f46e2734f5e9e740
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2017-01-18 08:44:42 +0100

    Bug 1329032 - Test privileged about page to use SystemPrincipal as TriggeringPrincipal when loading about page in child. r=gijs

---
+++ browser/base/content/test/general/browser_e10s_about_page_triggeringprincipal.js
@@ -0,0 +1,44 @@
+"use strict";
+Cu.import("resource://gre/modules/Services.jsm");
+
+registerCleanupFunction(function() {
+  Services.ppmm.broadcastAsyncMessage("AboutPrincipalTest:Unregister");
+  BrowserTestUtils.waitForMessage(Services.ppmm, "AboutPrincipalTest:Unregistered").then(
+    Services.ppmm.removeDelayedProcessScript(
+      "chrome://mochitests/content/browser/browser/base/content/test/general/file_register_about_page.js"
+    )
+  );
+});
+
+add_task(function* test_principal() {
+  Services.ppmm.loadProcessScript(
+    "chrome://mochitests/content/browser/browser/base/content/test/general/file_register_about_page.js",
+    true
+  );
+
+  yield BrowserTestUtils.withNewTab("about:test-about-principal-parent", function*(browser) {
+    let loadPromise = BrowserTestUtils.browserLoaded(browser, false, "about:test-about-principal-child");
+    let myLink = browser.contentDocument.getElementById("aboutchildprincipal");
+    myLink.click();
+    yield loadPromise;
+
+    yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function*() {
+      let channel = content.document.docShell.currentDocumentChannel;
+      is(channel.originalURI.asciiSpec,
+         "about:test-about-principal-child",
+         "sanity check - make sure we test the principal for the correct URI");
+
+      let triggeringPrincipal = channel.loadInfo.triggeringPrincipal;
+      ok(Services.scriptSecurityManager.isSystemPrincipal(triggeringPrincipal),
+         "loading about: from privileged page must have a triggering of System");
+
+      let contentPolicyType = channel.loadInfo.externalContentPolicyType;
+      is(contentPolicyType, Ci.nsIContentPolicy.TYPE_DOCUMENT,
+        "sanity check - loading a top level document");
+
+      let loadingPrincipal = channel.loadInfo.loadingPrincipal;
+      is(loadingPrincipal, null,
+         "sanity check - load of TYPE_DOCUMENT must have a null loadingPrincipal");
+    });
+  });
+});
