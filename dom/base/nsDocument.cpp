187dedfae8758f5c2111726975de8b5260152438
Bill McCloskey <billm@mozilla.com>
2017-01-18 15:07:54 -0800

    Bug 1331804 - New naming scheme for runnables (r=ehsan)
    MozReview-Commit-ID: FOfrUXwGYws

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -5284,7 +5284,7 @@ nsDocument::UnblockDOMContentLoaded()
     MOZ_RELEASE_ASSERT(NS_IsMainThread());
     nsCOMPtr<nsIRunnable> ev =
       NewRunnableMethod(this, &nsDocument::DispatchContentLoadedEvents);
-    Dispatch("DispatchContentLoadedEvents", TaskCategory::Other, ev.forget());
+    Dispatch("nsDocument::DispatchContentLoadedEvents", TaskCategory::Other, ev.forget());
   } else {
     DispatchContentLoadedEvents();
   }
@@ -12059,7 +12059,7 @@ nsDocument::PostVisibilityUpdateEvent()
 {
   nsCOMPtr<nsIRunnable> event =
     NewRunnableMethod(this, &nsDocument::UpdateVisibilityState);
-  Dispatch("UpdateVisibility", TaskCategory::Other, event.forget());
+  Dispatch("nsDocument::UpdateVisibilityState", TaskCategory::Other, event.forget());
 }
 
 void
