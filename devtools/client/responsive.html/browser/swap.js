1cc668c24661d6cbe749eb06a1c8d37a26e37809
J. Ryan Stinnett <jryans@gmail.com>
2017-01-18 17:06:55 -0600

    Bug 1331601 - Restore remoteType on RDM close. r=ochameau
    In bug 1318767, `updateBrowserRemoteness` was changed to take the `remoteType`
    as part of an options argument, but the RDM call site wasn't updated.
    
    MozReview-Commit-ID: 8GSSwicaHvz

--- devtools/client/responsive.html/browser/swap.js
+++ devtools/client/responsive.html/browser/swap.js
@@ -150,8 +150,9 @@ function swapToInnerBrowser({ tab, containerURL, getInnerBrowser }) {
       // 5. Force the original browser tab to be remote since web content is
       //    loaded in the child process, and we're about to swap the content
       //    into this tab.
-      gBrowser.updateBrowserRemoteness(tab.linkedBrowser, true,
-                                       contentBrowser.remoteType);
+      gBrowser.updateBrowserRemoteness(tab.linkedBrowser, true, {
+        remoteType: contentBrowser.remoteType,
+      });
 
       // 6. Swap the content into the original browser tab and close the
       //    temporary tab used to hold the content via
