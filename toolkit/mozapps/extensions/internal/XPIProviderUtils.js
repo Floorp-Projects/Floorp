c1b9b705b79eac4c81c9ace65f67461bc7ff7500
Andrew Swan <aswan@mozilla.com>
2017-01-18 18:16:19 -0800

    Bug 1317363 Implement the new sideloading flow r=florian,rhelmer
    MozReview-Commit-ID: JgloWKYAhlK

--- toolkit/mozapps/extensions/internal/XPIProviderUtils.js
+++ toolkit/mozapps/extensions/internal/XPIProviderUtils.js
@@ -89,7 +89,8 @@ const PROP_JSON_FIELDS = ["id", "syncGUID", "location", "version", "type",
                           "softDisabled", "foreignInstall", "hasBinaryComponents",
                           "strictCompatibility", "locales", "targetApplications",
                           "targetPlatforms", "multiprocessCompatible", "signedState",
-                          "seen", "dependencies", "hasEmbeddedWebExtension", "mpcOptedOut"];
+                          "seen", "dependencies", "hasEmbeddedWebExtension", "mpcOptedOut",
+                          "userPermissions"];
 
 // Properties that should be migrated where possible from an old database. These
 // shouldn't include properties that can be read directly from install.rdf files
