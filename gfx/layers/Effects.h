5f98743717a05be6a898f5ef517771106114cb68
Miko Mynttinen <mikokm@gmail.com>
2017-01-19 21:16:30 +0100

    Bug 1332268 - Update EffectComponentAlpha texture coordinates when rendering split layers r=mattwoodrow
    MozReview-Commit-ID: HFgikDMPB7F

--- gfx/layers/Effects.h
+++ gfx/layers/Effects.h
@@ -38,6 +38,8 @@ namespace layers {
  * to the compositor by the compositable host as a parameter to DrawQuad.
  */
 
+struct TexturedEffect;
+
 struct Effect
 {
   NS_INLINE_DECL_REFCOUNTING(Effect)
@@ -46,6 +48,7 @@ struct Effect
 
   EffectTypes mType;
 
+  virtual TexturedEffect* AsTexturedEffect() { return nullptr; }
   virtual void PrintInfo(std::stringstream& aStream, const char* aPrefix) = 0;
 
 protected:
@@ -66,6 +69,7 @@ struct TexturedEffect : public Effect
      , mSamplingFilter(aSamplingFilter)
   {}
 
+  virtual TexturedEffect* AsTexturedEffect() { return this; }
   virtual const char* Name() = 0;
   virtual void PrintInfo(std::stringstream& aStream, const char* aPrefix);
 
