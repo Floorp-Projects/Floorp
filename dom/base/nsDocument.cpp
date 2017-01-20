2e6c0592692ab05033bfd8cdac734aa6f40e278f
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-01-20 19:30:32 +0100

    Backed out changeset 9ef39356f62c (bug 851892)

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -5198,7 +5198,7 @@ nsDocument::StyleRuleChanged(StyleSheet* aSheet,
     DO_STYLESHEET_NOTIFICATION(StyleRuleChangeEvent,
                                "StyleRuleChanged",
                                mRule,
-                               aStyleRule);
+                               aStyleRule ? aStyleRule->GetDOMRule() : nullptr);
   }
 }
 
@@ -5212,7 +5212,8 @@ nsDocument::StyleRuleAdded(StyleSheet* aSheet,
     DO_STYLESHEET_NOTIFICATION(StyleRuleChangeEvent,
                                "StyleRuleAdded",
                                mRule,
-                               aStyleRule);
+                               aStyleRule ? aStyleRule->GetDOMRule()
+                                          : nullptr);
   }
 }
 
@@ -5226,7 +5227,8 @@ nsDocument::StyleRuleRemoved(StyleSheet* aSheet,
     DO_STYLESHEET_NOTIFICATION(StyleRuleChangeEvent,
                                "StyleRuleRemoved",
                                mRule,
-                               aStyleRule);
+                               aStyleRule ? aStyleRule->GetDOMRule()
+                                          : nullptr);
   }
 }
 
