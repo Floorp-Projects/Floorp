c2144deeeaa93c2b5cbf8dd9cc589fedeff75fb7
Chris Manchester <cmanchester@mozilla.com>
2017-01-19 17:05:02 -0800

    Bug 1332517 - Add an 'artifact' key to mozinfo. r=mshal
    MozReview-Commit-ID: GPq52LMg23Q
    
    --HG--
    extra : rebase_source : 7b61110ab5d8d901c351a1824b3825b5a57f6601

--- python/mozbuild/mozbuild/mozinfo.py
+++ python/mozbuild/mozbuild/mozinfo.py
@@ -96,6 +96,7 @@ def build_dict(config, env=os.environ):
     d['require_signing'] = substs.get('MOZ_REQUIRE_SIGNING') == '1'
     d['official'] = bool(substs.get('MOZILLA_OFFICIAL'))
     d['updater'] = substs.get('MOZ_UPDATER') == '1'
+    d['artifact'] = substs.get('MOZ_ARTIFACT_BUILDS') == '1'
 
     def guess_platform():
         if d['buildapp'] in ('browser', 'mulet'):
