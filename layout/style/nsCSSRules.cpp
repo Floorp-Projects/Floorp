1af6fa219123627d6621499a6e45f2f158facaff
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-20 12:39:53 -0500

    Bug 1332550 - Use our existing function for removing a rule at a given index from a group rule. r=heycam, a=abillings

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -2398,7 +2398,7 @@ nsCSSKeyframesRule::DeleteRule(const nsAString& aKey)
     nsIDocument* doc = GetDocument();
     MOZ_AUTO_DOC_UPDATE(doc, UPDATE_STYLE, true);
 
-    mRules.RemoveObjectAt(index);
+    DeleteStyleRuleAt(index);
 
     CSSStyleSheet* sheet = GetStyleSheet();
     if (sheet) {
