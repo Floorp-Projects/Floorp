f40fa57e5ca134bfa84ff7acad98d2b0b4f06bee
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-20 13:57:42 -0500

    Bug 851892 followup.  Disable most of Rule::IsKnownLive for now to reopen CLOSED TREE

--- layout/style/nsCSSRules.cpp
+++ layout/style/nsCSSRules.cpp
@@ -83,6 +83,10 @@ Rule::IsKnownLive() const
     return true;
   }
 
+#if 1
+  return false;
+#else
+  // Disabled pending bug 1332704 getting resolved for now.
   StyleSheet* sheet = GetStyleSheet();
   if (!sheet) {
     return false;
@@ -94,6 +98,7 @@ Rule::IsKnownLive() const
 
   return nsCCUncollectableMarker::InGeneration(
     sheet->GetAssociatedDocument()->GetMarkedCCGeneration());
+#endif
 }
 
 NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_BEGIN(Rule)
