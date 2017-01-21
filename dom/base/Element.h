3f43a691db612baa080c33fca173ac4de602b218
Bobby Holley <bobbyholley@gmail.com>
2017-01-20 18:24:41 -0800

    Bug 1331322 - Move MAY_HAVE_CLASS to mBoolFlags. r=bholley
    This fits a bit better with the other stuff, and allows us to add our new NAC
    bit with the other NAC related bits, which also happens to be a field that
    Servo already has easy access to.

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -623,7 +623,7 @@ public:
    * guaranteed (e.g. we could have class="").
    */
   const nsAttrValue* GetClasses() const {
-    if (HasFlag(NODE_MAY_HAVE_CLASS)) {
+    if (MayHaveClass()) {
       return DoGetClasses();
     }
     return nullptr;
