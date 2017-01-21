df92190b83095528aa3f13b2770129c46111f4b9
Jan Henning <jh+bugzilla@buttercookie.de>
2017-01-21 19:58:57 +0100

    Bug 1332863 - Turn on strict mode for mobile session store. r=sebastian
    MozReview-Commit-ID: 20KRW4yxPm4

--- mobile/android/components/SessionStore.js
+++ mobile/android/components/SessionStore.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
@@ -200,10 +201,11 @@ SessionStore.prototype = {
         break;
       case "domwindowopened": {
         let window = aSubject;
-        window.addEventListener("load", () => {
+        let onLoad = () => {
+          window.removeEventListener("load", onLoad);
           this.onWindowOpen(window);
-          window.removeEventListener("load", arguments.callee);
-        });
+        };
+        window.addEventListener("load", onLoad);
         break;
       }
       case "domwindowclosed": // catch closed windows
