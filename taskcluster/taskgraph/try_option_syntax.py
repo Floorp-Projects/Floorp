17e0a9c01736642347009cd35ceab5a7602cef53
Dustin J. Mitchell <dustin@mozilla.com>
2017-01-21 18:53:19 +0000

    Bug 1332844: alias "Ubuntu" to test platform linux32, rather than linux; r=gbrown
    MozReview-Commit-ID: GjY9tF5TxjL

--- taskcluster/taskgraph/try_option_syntax.py
+++ taskcluster/taskgraph/try_option_syntax.py
@@ -124,7 +124,7 @@ UNITTEST_ALIASES = {
 # substrings.  This is intended only for backward-compatibility.  New test
 # platforms should have their `test_platform` spelled out fully in try syntax.
 UNITTEST_PLATFORM_PRETTY_NAMES = {
-    'Ubuntu': ['linux', 'linux64', 'linux64-asan'],
+    'Ubuntu': ['linux32', 'linux64', 'linux64-asan'],
     'x64': ['linux64', 'linux64-asan'],
     'Android 4.3': ['android-4.3-arm7-api-15'],
     # other commonly-used substrings for platforms not yet supported with
