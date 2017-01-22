1657537d0e692056463b093565a9a298c7fde2e9
karo <kkoorts@mozilla.com>
2017-01-23 11:06:17 +1300

    Bug 1332952 - Implement GetAsSourceSurface() for SharedRGBImage. r=mattwoodrow a=lizzard
    MozReview-Commit-ID: E9zl5FchvZG
    
    --HG--
    extra : source : 7927148d6726a6cb3751a5b3f8664fa9c6f5496d

--- gfx/layers/ipc/SharedRGBImage.cpp
+++ gfx/layers/ipc/SharedRGBImage.cpp
@@ -105,7 +105,32 @@ SharedRGBImage::GetTextureClient(KnowsCompositor* aForwarder)
 already_AddRefed<gfx::SourceSurface>
 SharedRGBImage::GetAsSourceSurface()
 {
-  return nullptr;
+  NS_ASSERTION(NS_IsMainThread(), "Must be main thread");
+
+  if (mSourceSurface) {
+    RefPtr<gfx::SourceSurface> surface(mSourceSurface);
+    return surface.forget();
+  }
+
+  RefPtr<gfx::SourceSurface> surface;
+  {
+    // We are 'borrowing' the DrawTarget and retaining a permanent reference to
+    // the underlying data (via the surface). It is in this instance since we
+    // know that the TextureClient is always wrapping a BufferTextureData and
+    // therefore it won't go away underneath us.
+    BufferTextureData* decoded_buffer =
+      mTextureClient->GetInternalData()->AsBufferTextureData();
+    RefPtr<gfx::DrawTarget> drawTarget = decoded_buffer->BorrowDrawTarget();
+
+    if (!drawTarget) {
+      return nullptr;
+    }
+
+    surface = drawTarget->Snapshot();
+  }
+
+  mSourceSurface = surface;
+  return surface.forget();
 }
 
 } // namespace layers
