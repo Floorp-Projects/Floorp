72c9bcd9f2ac47bc12ee6635b333ad60f7c4b370
Xidorn Quan <me@upsuper.org>
2017-01-23 16:08:41 +1100

    Bug 1332699 - Don't check handler of legacy version if the event is not trusted. r=smaug
    MozReview-Commit-ID: A7G4bkyNsOe

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -1314,9 +1314,10 @@ EventListenerManager::HandleEventInternal(nsPresContext* aPresContext,
     // If we didn't find any matching listeners, and our event has a legacy
     // version, we'll now switch to looking for that legacy version and we'll
     // recheck our listeners.
-    if (hasListenerForCurrentGroup || usingLegacyMessage) {
-      // (No need to recheck listeners, because we already found a match, or we
-      // already rechecked them.)
+    if (hasListenerForCurrentGroup ||
+        usingLegacyMessage || !aEvent->IsTrusted()) {
+      // No need to recheck listeners, because we already found a match, we
+      // already rechecked them, or it is not a trusted event.
       break;
     }
     EventMessage legacyEventMessage = GetLegacyEventMessage(eventMessage);
