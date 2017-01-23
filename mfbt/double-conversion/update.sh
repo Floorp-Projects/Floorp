f0374b3e833d78d01986d5f6152cecc23a169fc0
Jeff Walden <jwalden@mit.edu>
2017-01-23 11:27:01 -0800

    Bug 1332797 - Make the double-conversion update script clone the double-conversion repository into a temporary directory, then copy out of it, taking an optional git revision to use.  After this patch, |./update.sh 04cae7a8d5ef3d62ceffb03cdc3d38f258457a52| is a no-op.  r=froydnj

--- mfbt/double-conversion/update.sh
+++ mfbt/double-conversion/update.sh
@@ -1,23 +1,40 @@
-# Usage: ./update.sh <double-conversion-src-directory>
+# Usage: ./update.sh [<git-rev-to-use>]
 #
 # Copies the needed files from a directory containing the original
-# double-conversion source that we need.
+# double-conversion source that we need.  If no revision is specified, the tip
+# revision is used.
 
 # This was last updated with git rev 04cae7a8d5ef3d62ceffb03cdc3d38f258457a52.
 
 set -e
 
-cp $1/LICENSE ./
-cp $1/README ./
+TMPDIR=`mktemp --directory`
+LOCAL_CLONE="$TMPDIR/double-conversion"
+
+git clone https://github.com/google/double-conversion.git "$LOCAL_CLONE"
+
+REV=""
+
+if [ "$1" !=  "" ]; then
+  git -C "$LOCAL_CLONE" checkout "$1"
+fi
+
+cp "$LOCAL_CLONE/LICENSE" ./
+cp "$LOCAL_CLONE/README" ./
 
 # Includes
-cp $1/src/*.h ./
+for header in "$LOCAL_CLONE/src/"*.h; do
+  cp "$header" ./
+done
 
 # Source
-cp $1/src/*.cc ./
+for ccfile in "$LOCAL_CLONE/src/"*.cc; do
+  cp "$ccfile" ./
+done
 
 patch -p3 < add-mfbt-api-markers.patch
 patch -p3 < use-StandardInteger.patch
 patch -p3 < use-mozilla-assertions.patch
 patch -p3 < use-static_assert.patch
 patch -p3 < ToPrecision-exponential.patch
+patch -p3 < fix-Wshadow-issues.patch
