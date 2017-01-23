f082dc5051d2f1c7fa393804720dfd404cd1996e
Boris Zbarsky <bzbarsky@mit.edu>
2017-01-23 11:15:58 -0500

    Bug 1332704 - Have ~nsDocument actually clear the self-pointer from _all_ its sheets, and reenable Rule::IsKnownLive bits that depends on that pointer being managed correctly. r=heycam, a=jcristau

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -1494,6 +1494,11 @@ nsDocument::~nsDocument()
   for (StyleSheet* sheet : mStyleSheets) {
     sheet->SetOwningDocument(nullptr);
   }
+  for (auto& sheets : mAdditionalSheets) {
+    for (StyleSheet* sheet : sheets) {
+      sheet->SetOwningDocument(nullptr);
+    }
+  }
   if (mAttrStyleSheet) {
     mAttrStyleSheet->SetOwningDocument(nullptr);
   }
