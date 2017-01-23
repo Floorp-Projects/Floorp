e1754317a77a0013ff963000ac97439a72f3d7fd
Jeff Walden <jwalden@mit.edu>
2017-01-23 15:13:21 -0800

    Bug 1332797 - Adjust update.sh to work against tip double-conversion.  r=froydnj

--- mfbt/double-conversion/update.sh
+++ mfbt/double-conversion/update.sh
@@ -4,7 +4,7 @@
 # double-conversion source that we need.  If no revision is specified, the tip
 # revision is used.
 
-# This was last updated with git rev 04cae7a8d5ef3d62ceffb03cdc3d38f258457a52.
+# This was last updated with git rev d8d4e668ee1e6e10b728f0671a89b07d7c4d45be.
 
 set -e
 
@@ -13,9 +13,7 @@ LOCAL_PATCHES=""
 LOCAL_PATCHES="$LOCAL_PATCHES add-mfbt-api-markers.patch"
 LOCAL_PATCHES="$LOCAL_PATCHES use-StandardInteger.patch"
 LOCAL_PATCHES="$LOCAL_PATCHES use-mozilla-assertions.patch"
-LOCAL_PATCHES="$LOCAL_PATCHES use-static_assert.patch"
 LOCAL_PATCHES="$LOCAL_PATCHES ToPrecision-exponential.patch"
-LOCAL_PATCHES="$LOCAL_PATCHES fix-Wshadow-issues.patch"
 
 TMPDIR=`mktemp --directory`
 LOCAL_CLONE="$TMPDIR/double-conversion"
@@ -35,15 +33,15 @@ mkdir "$DEST"
 
 # Copy over critical files.
 cp "$LOCAL_CLONE/LICENSE" "$DEST/"
-cp "$LOCAL_CLONE/README" "$DEST/"
+cp "$LOCAL_CLONE/README.md" "$DEST/"
 
 # Includes
-for header in "$LOCAL_CLONE/src/"*.h; do
+for header in "$LOCAL_CLONE/double-conversion/"*.h; do
   cp "$header" "$DEST/"
 done
 
 # Source
-for ccfile in "$LOCAL_CLONE/src/"*.cc; do
+for ccfile in "$LOCAL_CLONE/double-conversion/"*.cc; do
   cp "$ccfile" "$DEST/"
 done
 
