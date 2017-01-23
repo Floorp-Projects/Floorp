6172266f2fc9051256ec84e16527dd17b6b5251c
Scott Wu <scottcwwu@gmail.com>
2017-01-23 10:45:08 +0800

    Bug 1304306 - (Part 2) Add mochitest-browser-chrome test for Form Autofill preferences r=MattN
    MozReview-Commit-ID: 5aGNp18rL1g

---
+++ browser/extensions/formautofill/test/browser/browser_privacyPreferences.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const PAGE_PREFS = "about:preferences";
+const PAGE_PRIVACY = PAGE_PREFS + "#privacy";
+const GROUP_AUTOFILL = "#formAutofillGroup";
+const CHECKBOX_AUTOFILL = GROUP_AUTOFILL + " checkbox";
+const PREF_AUTOFILL_ENABLED = "browser.formautofill.enabled";
+
+let args = {
+  group: GROUP_AUTOFILL,
+  checkbox: CHECKBOX_AUTOFILL,
+};
+
+// Visibility of form autofill group should be hidden when opening
+// preferences page.
+add_task(function* test_aboutPreferences() {
+  yield BrowserTestUtils.withNewTab({gBrowser, url: PAGE_PREFS}, function* (browser) {
+    yield ContentTask.spawn(browser, args, (args) => {
+      is(content.document.querySelector(args.group).hidden, true,
+        "Form Autofill group should be hidden");
+    });
+  });
+});
+
+// Visibility of form autofill group should be visible when opening
+// directly to privacy page. Checkbox is not checked by default.
+add_task(function* test_aboutPreferencesPrivacy() {
+  yield BrowserTestUtils.withNewTab({gBrowser, url: PAGE_PRIVACY}, function* (browser) {
+    yield ContentTask.spawn(browser, args, (args) => {
+      is(content.document.querySelector(args.group).hidden, false,
+        "Form Autofill group should be visible");
+      is(content.document.querySelector(args.checkbox).checked, false,
+        "Checkbox should be unchecked");
+    });
+  });
+});
+
+// Checkbox should be checked when form autofill is enabled.
+add_task(function* test_autofillEnabledCheckbox() {
+  SpecialPowers.pushPrefEnv({set: [[PREF_AUTOFILL_ENABLED, true]]});
+
+  yield BrowserTestUtils.withNewTab({gBrowser, url: PAGE_PRIVACY}, function* (browser) {
+    yield ContentTask.spawn(browser, args, (args) => {
+      is(content.document.querySelector(args.group).hidden, false,
+        "Form Autofill group should be visible");
+      is(content.document.querySelector(args.checkbox).checked, true,
+        "Checkbox should be checked when Form Autofill is enabled");
+    });
+  });
+});
