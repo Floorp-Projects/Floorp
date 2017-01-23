1258664c6b60be60bf435feaa7c2bd3d62869492
Nihanth Subramanya <nhnt11@gmail.com>
2017-01-24 00:59:27 +0530

    Bug 1323420 - Add a pref to control the close button on web permission prompts. r=past, a=lizzard
    MozReview-Commit-ID: JHEY478q3C
    
    --HG--
    extra : source : 0c698bfb1310af765951946cfb317e37471e1881
    extra : histedit_source : 6200e33a23e611c7ec12d899e43ab620789a501b%2C0258190eeb4b72cba6280064f7911af9cdfbfde6

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1220,6 +1220,10 @@ pref("privacy.popups.disable_from_plugins", 2);
 
 // send "do not track" HTTP header, disabled by default
 pref("privacy.donottrackheader.enabled",    false);
+// If true, close buton will be shown on permission prompts
+// and for all PopupNotifications, the secondary action of
+// the popup will be called when the popup is dismissed.
+pref("privacy.permissionPrompts.showCloseButton", false);
 // Enforce tracking protection in all modes
 pref("privacy.trackingprotection.enabled",  false);
 // Enforce tracking protection in Private Browsing mode
