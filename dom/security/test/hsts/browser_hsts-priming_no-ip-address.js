389a460d5d5171c0a3df7090e28bb0adaa988bbc
Kate McKinley <kmckinley@mozilla.com>
2017-01-23 14:01:43 -0800

    Bug 1328460 - Don't send priming to IP or non-standard ports. r=ckerschb, a=lizzard
    MozReview-Commit-ID: GLyLfp8gqYt

---
+++ dom/security/test/hsts/browser_hsts-priming_no-ip-address.js
@@ -0,0 +1,30 @@
+/*
+ * Description of the test:
+ *   If the top-level domain sends the STS header but does not have
+ *   includeSubdomains, HSTS priming requests should still be sent to
+ *   subdomains.
+ */
+'use strict';
+
+//jscs:disable
+add_task(function*() {
+  //jscs:enable
+  Observer.add_observers(Services);
+  registerCleanupFunction(do_cleanup);
+
+  // add the top-level server
+  test_servers['localhost-ip'] = {
+    host: '127.0.0.1',
+    response: true,
+    id: 'localhost-ip',
+  };
+  test_settings.block_active.result['localhost-ip'] = 'blocked';
+
+  let which = "block_active";
+
+  SetupPrefTestEnvironment(which);
+
+  yield execute_test("localhost-ip", test_settings[which].mimetype);
+
+  SpecialPowers.popPrefEnv();
+});
