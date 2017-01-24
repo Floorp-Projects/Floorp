41c09c9bdb1c21e4305f4a2e0e3213e237e36dda
Kartikaya Gupta <kgupta@mozilla.com>
2017-01-24 13:13:43 -0500

    Back out cset 6ec880339dd4 for missing bug number in commit message. r=backout

--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -801,17 +801,7 @@ WebRenderBridgeParent::SetWebRenderProfilerEnabled(bool aEnabled)
     if (MOZ_USE_RENDER_THREAD) {
       mApi->SetProfilerEnabled(aEnabled);
     } else {
-      if (CompositorThreadHolder::IsInCompositorThread()) {
-        wr_profiler_set_enabled(mWRWindowState, aEnabled);
-      } else {
-        bool enabled = aEnabled;
-        WrWindowState* state = mWRWindowState;
-        RefPtr<Runnable> runnable =
-          NS_NewRunnableFunction([state, enabled]() {
-          wr_profiler_set_enabled(state, enabled);
-        });
-        CompositorThreadHolder::Loop()->PostTask(runnable.forget());
-      }
+      wr_profiler_set_enabled(mWRWindowState, aEnabled);
     }
   }
 }
