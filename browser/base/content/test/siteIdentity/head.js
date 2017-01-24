bb6549a755064dc72dc53e90a7d0f1471c9f7b59
Johann Hofmann <jhofmann@mozilla.com>
2017-01-24 11:42:09 +0100

    Bug 1327946 - Remove conditional moz-user-focus on identity block from urlbar. r=Gijs, a=lizzard
    MozReview-Commit-ID: 9F39TeDIGUf
    
    --HG--
    extra : source : ce01ff2ef384ad24a4e529f3cdba3e81e9bb3501

---
+++ browser/base/content/test/siteIdentity/head.js
@@ -0,0 +1,6 @@
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(this, "Promise",
+  "resource://gre/modules/Promise.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Task",
+  "resource://gre/modules/Task.jsm");
