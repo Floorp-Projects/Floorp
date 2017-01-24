607569820bf393d61b307c8a1af71dcddf2efdea
Jared Wein <jwein@mozilla.com>
2017-01-24 11:44:44 -0500

    Bug 1330340 - Implement support for lightweight theme properties (headerURL, accentcolor, textcolor) for Theming API. r=mikedeboer
    MozReview-Commit-ID: GS1Gh4HVcOt

--- browser/components/extensions/ext-theme.js
+++ browser/components/extensions/ext-theme.js
@@ -1,19 +1,62 @@
 "use strict";
 
+Cu.import("resource://gre/modules/Services.jsm");
+
 XPCOMUtils.defineLazyModuleGetter(this, "Preferences",
                                   "resource://gre/modules/Preferences.jsm");
 
 /* eslint-disable mozilla/balanced-listeners */
 extensions.on("manifest_theme", (type, directive, extension, manifest) => {
   let enabled = Preferences.get("extensions.webextensions.themes.enabled");
-  extension.emit("test-message", "themes-enabled", enabled);
 
-  if (enabled) {
-    // Apply theme only if themes are enabled.
+  if (!enabled || !manifest || !manifest.theme) {
+    return;
+  }
+  // Apply theme only if themes are enabled.
+  let lwtStyles = {footerURL: ""};
+  if (manifest.theme.colors) {
+    let colors = manifest.theme.colors;
+    for (let color of Object.getOwnPropertyNames(colors)) {
+      let val = colors[color];
+      // Since values are optional, they may be `null`.
+      if (val === null) {
+        continue;
+      }
+
+      if (color == "accentcolor") {
+        lwtStyles.accentcolor = val;
+        continue;
+      }
+      if (color == "textcolor") {
+        lwtStyles.textcolor = val;
+      }
+    }
+  }
+
+  if (manifest.theme.images) {
+    let images = manifest.theme.images;
+    for (let image of Object.getOwnPropertyNames(images)) {
+      let val = images[image];
+      if (val === null) {
+        continue;
+      }
+
+      if (image == "headerURL") {
+        lwtStyles.headerURL = val;
+      }
+    }
+  }
+
+  if (lwtStyles.headerURL &&
+      lwtStyles.accentcolor &&
+      lwtStyles.textcolor) {
+    Services.obs.notifyObservers(null,
+      "lightweight-theme-styling-update",
+      JSON.stringify(lwtStyles));
   }
 });
 
 extensions.on("shutdown", (type, extension) => {
-  // Remove theme only if it has been applied.
+  Services.obs.notifyObservers(null, "lightweight-theme-styling-update", null);
 });
 /* eslint-enable mozilla/balanced-listeners */
