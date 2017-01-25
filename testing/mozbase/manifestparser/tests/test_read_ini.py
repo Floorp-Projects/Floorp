3f9cbf87a10b161c138ff17ad5fc96abd54dce46
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-01-25 14:38:37 -0500

    Bug 1333564 - [manifestparser] Stop supporting ';' as a valid comment character, r=jmaher
    It turns out there are shockingly few cases of manifestparser manifests that actually use the ';'
    character as a comment. Because we will soon allow inline comments, deprecating the use of ';' will
    ensure that values are allowed to have semicolons in them.
    
    Even without inline comments, might as well enforce consistency across manifests.
    
    MozReview-Commit-ID: AEPPQFdNXG0

--- testing/mozbase/manifestparser/tests/test_read_ini.py
+++ testing/mozbase/manifestparser/tests/test_read_ini.py
@@ -46,27 +46,6 @@ kittens = true # This test requires kittens
         control = parser.get('test_felinicity.py', 'kittens')
         self.assertEqual(result, control)
 
-        # test ';' inline comments (really, the lack thereof)
-        string = string.replace('#', ';')
-        buffer = StringIO()
-        buffer.write(string)
-        buffer.seek(0)
-        result = read_ini(buffer)[0][1]['kittens']
-        self.assertEqual(result, "true ; This test requires kittens")
-
-        # compare this to ConfigParser
-        # python 2.7 ConfigParser *does* support ';' as an
-        # inline comment delimeter (ibid).
-        # Python 3.x configparser, OTOH, does not support
-        # inline-comments by default.  It does support their specification,
-        # though they are weakly discouraged:
-        # http://docs.python.org/dev/library/configparser.html
-        buffer.seek(0)
-        parser = ConfigParser()
-        parser.readfp(buffer)
-        control = parser.get('test_felinicity.py', 'kittens')
-        self.assertNotEqual(result, control)
-
 
 if __name__ == '__main__':
     mozunit.main()
