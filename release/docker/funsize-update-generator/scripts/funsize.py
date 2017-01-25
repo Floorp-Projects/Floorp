da9699766af909fad24b9896f48cb92537a7dc85
Rail Aliiev <rail@mozilla.com>
2017-01-24 23:24:47 -0500

    Bug 1333663 - Temporarily disable AV in funsize due to false positive in ClamAV. r=aki, a=release
    MozReview-Commit-ID: 8srzLTiCMHX

--- release/docker/funsize-update-generator/scripts/funsize.py
+++ release/docker/funsize-update-generator/scripts/funsize.py
@@ -211,8 +211,8 @@ def main():
             complete_mars["%s_size" % mar_type] = os.path.getsize(dest)
             complete_mars["%s_hash" % mar_type] = get_hash(dest)
             unpack(work_env, dest, unpack_dir)
-            log.info("AV-scanning %s ...", unpack_dir)
-            sh.clamscan("-r", unpack_dir, _timeout=600, _err_to_out=True)
+            log.info("Skipping AV-scanning %s ...", unpack_dir)
+            # sh.clamscan("-r", unpack_dir, _timeout=600, _err_to_out=True)
             log.info("Done.")
 
         path = os.path.join(work_env.workdir, "to")
