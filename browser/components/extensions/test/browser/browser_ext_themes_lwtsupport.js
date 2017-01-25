32af5b34f93fab2c7b90278ea9a090db9f037252
Matthew Wein <mwein@mozilla.com>
2017-01-25 15:11:20 -0800

    Bug 1330341 - Add support for dynamic updates r=mikedeboer
    MozReview-Commit-ID: 8wA7J1oX2t

--- browser/components/extensions/test/browser/browser_ext_themes_lwtsupport.js
+++ browser/components/extensions/test/browser/browser_ext_themes_lwtsupport.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const kBackground = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
-const kAccentColor = "#a14040";
-const kTextColor = "#fac96e";
+const BACKGROUND = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==";
+const ACCENT_COLOR = "#a14040";
+const TEXT_COLOR = "#fac96e";
 
 function hexToRGB(hex) {
   hex = parseInt((hex.indexOf("#") > -1 ? hex.substring(1) : hex), 16);
@@ -15,16 +15,16 @@ add_task(function* setup() {
   });
 });
 
-add_task(function* testSupportLWTProperties() {
+add_task(function* test_support_LWT_properties() {
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       "theme": {
         "images": {
-          "headerURL": kBackground,
+          "headerURL": BACKGROUND,
         },
         "colors": {
-          "accentcolor": kAccentColor,
-          "textcolor": kTextColor,
+          "accentcolor": ACCENT_COLOR,
+          "textcolor": TEXT_COLOR,
         },
       },
     },
@@ -39,11 +39,11 @@ add_task(function* testSupportLWTProperties() {
   Assert.equal(docEl.getAttribute("lwthemetextcolor"), "bright",
     "LWT text color attribute should be set");
 
-  Assert.equal(style.backgroundImage, 'url("' + kBackground.replace(/"/g, '\\"') + '")',
+  Assert.equal(style.backgroundImage, 'url("' + BACKGROUND.replace(/"/g, '\\"') + '")',
     "Expected background image");
-  Assert.equal(style.backgroundColor, "rgb(" + hexToRGB(kAccentColor).join(", ") + ")",
+  Assert.equal(style.backgroundColor, "rgb(" + hexToRGB(ACCENT_COLOR).join(", ") + ")",
     "Expected correct background color");
-  Assert.equal(style.color, "rgb(" + hexToRGB(kTextColor).join(", ") + ")",
+  Assert.equal(style.color, "rgb(" + hexToRGB(TEXT_COLOR).join(", ") + ")",
     "Expected correct text color");
 
   yield extension.unload();
@@ -51,12 +51,12 @@ add_task(function* testSupportLWTProperties() {
   Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
 });
 
-add_task(function* testLWTRequiresAllPropertiesDefinedImageOnly() {
+add_task(function* test_LWT_requires_all_properties_defined_image_only() {
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       "theme": {
         "images": {
-          "headerURL": kBackground,
+          "headerURL": BACKGROUND,
         },
       },
     },
@@ -70,13 +70,13 @@ add_task(function* testLWTRequiresAllPropertiesDefinedImageOnly() {
   Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
 });
 
-add_task(function* testLWTRequiresAllPropertiesDefinedColorsOnly() {
+add_task(function* test_LWT_requires_all_properties_defined_colors_only() {
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       "theme": {
         "colors": {
-          "accentcolor": kAccentColor,
-          "textcolor": kTextColor,
+          "accentcolor": ACCENT_COLOR,
+          "textcolor": TEXT_COLOR,
         },
       },
     },
