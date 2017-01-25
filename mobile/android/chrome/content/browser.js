eddd48539f78ed03395db84dc4437f51bcd1e3e9
Jan Henning <jh+bugzilla@buttercookie.de>
2017-01-25 20:31:30 +0100

    Bug 1333567 - Send the notification expected by the session store when restarting, too. r=sebastian
    Since bug 1228593, the mobile session store
    - once again flushes its data when we are quitting, to make sure the latest state (including any potential cleaning of history/tabs) is flushed to disk
    - ignores windows/tabs closing as a byproduct of shutdown
    
    The latter point is dependent on a new shutdown notification introduced in that bug. Because we forgot to add that notification to the restart code used for add-on updates, in that case the session store currently doesn't enter shutdown mode and therefore records the window being closed during shutdown before flushing its data to disk, which means that all open tabs are lost.
    
    MozReview-Commit-ID: LgtdQoYwacM

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1388,7 +1388,7 @@ var BrowserApp = {
   quit: function quit(aClear = { sanitize: {}, dontSaveSession: false }) {
     // Notify all windows that an application quit has been requested.
     let cancelQuit = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
-    Services.obs.notifyObservers(cancelQuit, "quit-application-requested", "restart");
+    Services.obs.notifyObservers(cancelQuit, "quit-application-requested", null);
 
     // Quit aborted.
     if (cancelQuit.data) {
@@ -5536,6 +5536,7 @@ var XPInstallObserver = {
 
         // If nothing aborted, quit the app
         if (cancelQuit.data == false) {
+          Services.obs.notifyObservers(null, "quit-application-proceeding", null);
           let appStartup = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
           appStartup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
         }
