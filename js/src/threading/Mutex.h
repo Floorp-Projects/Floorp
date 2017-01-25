7ccf4bdaf1490c6a752eeea2c4afb6085af3f5d0
Nathan Froyd <froydnj@mozilla.com>
2017-01-25 07:36:41 -0500

    Bug 1333429 - part 2 - don't dynamically allocate platform data in js::Mutex; r=fitzgen
    Dynamically allocating platform data was necessary when movability was
    supported, as mutexes on certain platforms (e.g. pthread_mutex_t) cannot
    be copied.  But now that movability is no longer supported, we can go
    back to having the platform data be a normal member of the Mutex object.

--- js/src/threading/Mutex.h
+++ js/src/threading/Mutex.h
@@ -8,13 +8,9 @@
 #define threading_Mutex_h
 
 #include "mozilla/Assertions.h"
-#include "mozilla/Attributes.h"
 #include "mozilla/Move.h"
 #include "mozilla/ThreadLocal.h"
 #include "mozilla/Vector.h"
-
-#include <new>
-#include <string.h>
 
 namespace js {
 
@@ -41,14 +37,27 @@ protected:
 private:
   MutexImpl(const MutexImpl&) = delete;
   void operator=(const MutexImpl&) = delete;
+  MutexImpl(MutexImpl&&) = delete;
+  void operator=(MutexImpl&&) = delete;
+
+  PlatformData* platformData();
+
+// Linux and maybe other platforms define the storage size of pthread_mutex_t in
+// bytes. However, we must define it as an array of void pointers to ensure
+// proper alignment.
+#if defined(__APPLE__) && defined(__MACH__) && defined(__i386__)
+  void* platformData_[11];
+#elif defined(__APPLE__) && defined(__MACH__) && defined(__amd64__)
+  void* platformData_[8];
+#elif defined(__linux__)
+  void* platformData_[40 / sizeof(void*)];
+#elif defined(_WIN32)
+  void* platformData_[6];
+#else
+  void* platformData_[64 / sizeof(void*)];
+#endif
 
   friend class js::ConditionVariable;
-  PlatformData* platformData() {
-    MOZ_ASSERT(platformData_);
-    return platformData_;
-  };
-
-  PlatformData* platformData_;
 };
 
 } // namespace detail
