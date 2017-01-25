fb11d2db3dae20ec61fe1639479dc278fa6e7740
Mark Hammond <mhammond@skippinet.com.au>
2017-01-25 18:11:05 +1100

    Bug 1310065 - use more reliable preferences for calculating the client count in the sync scheduler. r=eoger
    This patch switches to using preferences written by the clients engine to
    determine the number of clients so it's correct before the first sync for
    a browser session.
    
    It also fixes another bug I discovered - if a mobile device dropped from the
    client list, the preference for the number of mobile devices would remain at
    1 rather than resetting to zero.
    
    MozReview-Commit-ID: IPythSQcorx

--- services/sync/modules/engines/clients.js
+++ services/sync/modules/engines/clients.js
@@ -128,12 +128,15 @@ ClientEngine.prototype = {
   /**
    * Obtain information about device types.
    *
-   * Returns a Map of device types to integer counts.
+   * Returns a Map of device types to integer counts. Guaranteed to include
+   * "desktop" (which will have at least 1 - this device) and "mobile" (which
+   * may have zero) counts. It almost certainly will include only these 2.
    */
   get deviceTypes() {
     let counts = new Map();
 
-    counts.set(this.localType, 1);
+    counts.set(this.localType, 1); // currently this must be DEVICE_TYPE_DESKTOP
+    counts.set(DEVICE_TYPE_MOBILE, 0);
 
     for (let id in this._store._remoteClients) {
       let record = this._store._remoteClients[id];
@@ -385,16 +388,18 @@ ClientEngine.prototype = {
 
   _syncFinish() {
     // Record histograms for our device types, and also write them to a pref
-    // so non-histogram telemetry (eg, UITelemetry) has easy access to them.
+    // so non-histogram telemetry (eg, UITelemetry) and the sync scheduler
+    // has easy access to them, and so they are accurate even before we've
+    // successfully synced the first time after startup.
     for (let [deviceType, count] of this.deviceTypes) {
       let hid;
       let prefName = this.name + ".devices.";
       switch (deviceType) {
-        case "desktop":
+        case DEVICE_TYPE_DESKTOP:
           hid = "WEAVE_DEVICE_COUNT_DESKTOP";
           prefName += "desktop";
           break;
-        case "mobile":
+        case DEVICE_TYPE_MOBILE:
           hid = "WEAVE_DEVICE_COUNT_MOBILE";
           prefName += "mobile";
           break;
