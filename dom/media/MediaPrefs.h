452f0cc1b3439a5cf77912cd0e18d7178d0dd501
Chun-Min Chang <chun.m.chang@gmail.com>
2017-01-26 17:35:31 +0800

    Bug 1321502 - part 1: Enable multi-channel support in Gecko on Windows; r=jya
    MozReview-Commit-ID: KURUk3EffOu

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -90,8 +90,12 @@ private:
   DECL_MEDIA_PREF("accessibility.monoaudio.enable",           MonoAudio, bool, false);
   DECL_MEDIA_PREF("media.resampling.enabled",                 AudioSinkResampling, bool, false);
   DECL_MEDIA_PREF("media.resampling.rate",                    AudioSinkResampleRate, uint32_t, 48000);
+#if defined(XP_WIN)
+  // Enable multiple channel support on Windows.
+  DECL_MEDIA_PREF("media.forcestereo.enabled",                AudioSinkForceStereo, bool, false);
+#else
   DECL_MEDIA_PREF("media.forcestereo.enabled",                AudioSinkForceStereo, bool, true);
-
+#endif
   // VideoSink
   DECL_MEDIA_PREF("media.ruin-av-sync.enabled",               RuinAvSync, bool, false);
 
