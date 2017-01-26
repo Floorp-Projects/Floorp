f3093e499cd6ad7e8b5a2589775b960fb1527e30
Jon Coppeard <jcoppeard@mozilla.com>
2017-01-26 11:44:17 +0000

    Bug 1332773 - Don't trace things in other runtimes in the pre barrier verifier r=sfink a=jcristau
    --HG--
    extra : source : f74f0827be8b4b7f08ecf5cda763377c5cc40151
    extra : intermediate-source : 10606a7696400f990722e323c1d9bfc35e6d612d

---
+++ js/src/jit-test/tests/gc/bug-1332773.js
@@ -0,0 +1,10 @@
+if (helperThreadCount() == 0)
+    quit();
+
+evalInWorker(`
+var gTestcases = new Array();
+typeof document != "object" || !document.location.href.match(/jsreftest.html/);
+gczeal(4, 10);
+f = ([a = class target extends b {}, b] = [void 0]) => {};
+f()
+`)
