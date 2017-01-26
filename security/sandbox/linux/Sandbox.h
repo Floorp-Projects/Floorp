38513dad59091286be378a2a697793c9ad82dfa3
Gian-Carlo Pascutto <gcp@mozilla.com>
2017-01-26 19:59:50 +0100

    Bug 1330326 - Make sandboxing policy more configurable via preferences. r=jld
    MozReview-Commit-ID: 9P0bSLLKRWp

--- security/sandbox/linux/Sandbox.h
+++ security/sandbox/linux/Sandbox.h
@@ -24,7 +24,8 @@ MOZ_EXPORT void SandboxEarlyInit(GeckoProcessType aType);
 // (No-op if MOZ_DISABLE_CONTENT_SANDBOX is set.)
 // aBrokerFd is the filesystem broker client file descriptor,
 // or -1 to allow direct filesystem access.
-MOZ_EXPORT bool SetContentProcessSandbox(int aBrokerFd);
+MOZ_EXPORT bool SetContentProcessSandbox(int aBrokerFd,
+                                         std::vector<int>& aSyscallWhitelist);
 #endif
 
 #ifdef MOZ_GMP_SANDBOX
