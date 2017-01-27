e7ac97534fd8729630f473d62e8a9b2beee5fe34
Mythmon <mcooper@mozilla.com>
2017-01-27 10:22:44 -0800

    No bug - Update shield-recipe-client from GitHub r=Gijs
    --HG--
    extra : rebase_source : 4c9ffe3a55b5682215aa4e94dc35637c07798725

--- browser/extensions/shield-recipe-client/test/browser/browser_NormandyApi.js
+++ browser/extensions/shield-recipe-client/test/browser/browser_NormandyApi.js
@@ -10,12 +10,12 @@ add_task(function* () {
       [
         "extensions.shield-recipe-client.api_url",
         "http://mochi.test:8888/browser/browser/extensions/shield-recipe-client/test",
-      ]
-    ]
-  })
+      ],
+    ],
+  });
 
   // Test that NormandyApi can fetch from the test server.
-  const response = yield NormandyApi.get("test_server.sjs");
+  const response = yield NormandyApi.get("browser/test_server.sjs");
   const data = yield response.json();
   Assert.deepEqual(data, {test: "data"}, "NormandyApi returned incorrect server data.");
 });
