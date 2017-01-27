bfb1eb8adbc5c2e03e4237d6b4c2ae798a9c1e85
Michael Layzell <michael@thelayzells.com>
2017-01-27 14:14:31 -0500

    Bug 1334586 - Propagate reload flags when reloading pages with Large-Allocation headers. r=smaug, a=lizzard
    MozReview-Commit-ID: ESfCqSc0BJK

--- browser/base/content/tab-content.js
+++ browser/base/content/tab-content.js
@@ -718,8 +718,8 @@ var WebBrowserChrome = {
   },
 
   // Try to reload the currently active or currently loading page in a new process.
-  reloadInFreshProcess(aDocShell, aURI, aReferrer, aTriggeringPrincipal) {
-    E10SUtils.redirectLoad(aDocShell, aURI, aReferrer, aTriggeringPrincipal, true);
+  reloadInFreshProcess(aDocShell, aURI, aReferrer, aTriggeringPrincipal, aLoadFlags) {
+    E10SUtils.redirectLoad(aDocShell, aURI, aReferrer, aTriggeringPrincipal, true, aLoadFlags);
     return true;
   },
 
