3b64035ca72150d2bdc4b4f20c7c5e74e03c48c5
Michael Layzell <michael@thelayzells.com>
2017-01-27 15:35:26 -0500

    Bug 1331087 - Part 1: Disable the largeAllocation header by default outside of Win32 builds, r=ehsan
    MozReview-Commit-ID: 4jUFLv4fMZa

---
+++ dom/tests/browser/browser_largeAllocation_win32.js
@@ -0,0 +1,18 @@
+let testDir = gTestPath.substr(0, gTestPath.lastIndexOf("/"));
+Services.scriptloader.loadSubScript(testDir + "/helper_largeAllocation.js", this);
+
+add_task(function*() {
+  info("Test 1 - On win32 - no forceEnable pref");
+  yield SpecialPowers.pushPrefEnv({
+    set: [
+      // Enable the header if it is disabled
+      ["dom.largeAllocationHeader.enabled", true],
+      // Increase processCount.webLargeAllocation to avoid any races where
+      // processes aren't being cleaned up quickly enough.
+      ["dom.ipc.processCount.webLargeAllocation", 20]
+    ]
+  });
+
+  yield* largeAllocSuccessTests();
+});
+
