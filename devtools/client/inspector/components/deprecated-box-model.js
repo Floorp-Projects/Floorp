b0eccb57d91813958015f154d0d4d5678fb24ad6
Gabriel Luong <gabriel.luong@gmail.com>
2017-01-28 17:11:24 -0500

    Bug 1333561 - Part 4: Rename box-model.js to deprecated-box-model.js. r=jdescottes
    --HG--
    rename : devtools/client/inspector/components/box-model.js => devtools/client/inspector/components/deprecated-box-model.js

--- devtools/client/inspector/components/deprecated-box-model.js
+++ devtools/client/inspector/components/deprecated-box-model.js
@@ -4,6 +4,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * THIS MODULE IS DEPRECATED.
+ *
+ * To continue any work related to the box model view, see the new react/redux
+ * implementation in devtools/client/inspector/layout/.
+ */
+
 "use strict";
 
 const {Task} = require("devtools/shared/task");
