d4e78d75712c1616387b8b86829ce516c727b519
Jared Wein <jwein@mozilla.com>
2017-01-31 17:32:54 -0500

    Bug 1335171 - Implement Chrome-parity support for a few theme properties. r=mattw,mikedeboer
    MozReview-Commit-ID: C244c3Oh4bC
    
    --HG--
    extra : rebase_source : cc4b95d2c85bae68d3e527273112a89e12038a7e

--- browser/components/extensions/ext-theme.js
+++ browser/components/extensions/ext-theme.js
@@ -12,10 +12,14 @@ let themeMap = new WeakMap();
 class Theme {
   /**
    * Creates a theme instance.
+   *
+   * @param {string} baseURI The base URI of the extension, used to
+   *   resolve relative filepaths.
    */
-  constructor() {
+  constructor(baseURI) {
     // A dictionary of light weight theme styles.
     this.lwtStyles = {};
+    this.baseURI = baseURI;
   }
 
   /**
@@ -50,14 +54,29 @@ class Theme {
    * @param {Object} colors Dictionary mapping color properties to values.
    */
   loadColors(colors) {
-    let {accentcolor, textcolor} = colors;
+    for (let color of Object.keys(colors)) {
+      Services.console.logStringMessage(`parsing color=${color}`);
+      let val = colors[color];
 
-    if (accentcolor) {
-      this.lwtStyles.accentcolor = accentcolor;
-    }
+      if (!val) {
+        continue;
+      }
 
-    if (textcolor) {
-      this.lwtStyles.textcolor = textcolor;
+      let cssColor = val;
+      if (Array.isArray(val)) {
+        cssColor = "rgb" + (val.length > 3 ? "a" : "") + "(" + val.join(",") + ")";
+      }
+
+      switch (color) {
+        case "accentcolor":
+        case "frame":
+          this.lwtStyles.accentcolor = cssColor;
+          break;
+        case "textcolor":
+        case "tab_text":
+          this.lwtStyles.textcolor = cssColor;
+          break;
+      }
     }
   }
 
@@ -67,10 +86,21 @@ class Theme {
    * @param {Object} images Dictionary mapping image properties to values.
    */
   loadImages(images) {
-    let {headerURL} = images;
+    for (let image of Object.keys(images)) {
+      let val = images[image];
 
-    if (headerURL) {
-      this.lwtStyles.headerURL = headerURL;
+      if (!val) {
+        continue;
+      }
+
+      switch (image) {
+        case "headerURL":
+        case "theme_frame": {
+          let resolvedURL = this.baseURI.resolve(val);
+          this.lwtStyles.headerURL = resolvedURL;
+          break;
+        }
+      }
     }
   }
 
@@ -91,7 +121,7 @@ extensions.on("manifest_theme", (type, directive, extension, manifest) => {
     return;
   }
 
-  let theme = new Theme();
+  let theme = new Theme(extension.baseURI);
   theme.load(manifest.theme);
   themeMap.set(extension, theme);
 });
