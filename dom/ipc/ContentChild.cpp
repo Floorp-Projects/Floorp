6a20c7a40211f0fef7a4e7e830b6da6ea3a848d1
Phil Ringnalda <philringnalda@gmail.com>
2017-01-30 21:49:42 -0800

    Backed out 4 changesets (bug 1285898) for browser_localStorage_e10s.js failure
    Backed out changeset 9c6057cde326 (bug 1285898)
    Backed out changeset bd68ebab64fa (bug 1285898)
    Backed out changeset 6fdb24e1256d (bug 1285898)
    Backed out changeset 6681b50c1f6d (bug 1285898)

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -35,7 +35,6 @@
 #include "mozilla/dom/PCrashReporterChild.h"
 #include "mozilla/dom/ProcessGlobal.h"
 #include "mozilla/dom/PushNotifier.h"
-#include "mozilla/dom/Storage.h"
 #include "mozilla/dom/StorageIPC.h"
 #include "mozilla/dom/TabGroup.h"
 #include "mozilla/dom/workers/ServiceWorkerManager.h"
@@ -3031,20 +3030,6 @@ mozilla::ipc::IPCResult
 ContentChild::RecvBlobURLUnregistration(const nsCString& aURI)
 {
   nsHostObjectProtocolHandler::RemoveDataEntry(aURI);
-  return IPC_OK();
-}
-
-mozilla::ipc::IPCResult
-ContentChild::RecvDispatchLocalStorageChange(const nsString& aDocumentURI,
-                                             const nsString& aKey,
-                                             const nsString& aOldValue,
-                                             const nsString& aNewValue,
-                                             const IPC::Principal& aPrincipal,
-                                             const bool& aIsPrivate)
-{
-  Storage::DispatchStorageEvent(Storage::LocalStorage,
-                                aDocumentURI, aKey, aOldValue, aNewValue,
-                                aPrincipal, aIsPrivate, nullptr);
   return IPC_OK();
 }
 
