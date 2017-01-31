b2bab02fc7a0027443107402a2fdf7468018ffd2
Ricky Chien <rchien@mozilla.com>
2017-01-31 19:29:00 +0800

    Bug 1336379 - Implement StatisticsPanel r=Honza
    MozReview-Commit-ID: FNCSetNPzz6
    
    --HG--
    extra : rebase_source : e73958d192ad38f4e1ac2f4e63bb4de075a7de08

--- devtools/client/netmonitor/test/browser_net_statistics-01.js
+++ devtools/client/netmonitor/test/browser_net_statistics-01.js
@@ -12,53 +12,44 @@ add_task(function* () {
   info("Starting test... ");
 
   let panel = monitor.panelWin;
-  let { $, $all, EVENTS, NetMonitorView, gStore, windowRequire } = panel;
+  let { document, gStore, windowRequire } = panel;
   let Actions = windowRequire("devtools/client/netmonitor/actions/index");
 
-  is(NetMonitorView.currentFrontendMode, "network-inspector-view",
-   "The initial frontend mode is correct.");
+  let body = document.querySelector("#body");
 
-  is($("#primed-cache-chart").childNodes.length, 0,
-    "There should be no primed cache chart created yet.");
-  is($("#empty-cache-chart").childNodes.length, 0,
-    "There should be no empty cache chart created yet.");
-
-  let onChartDisplayed = Promise.all([
-    panel.once(EVENTS.PRIMED_CACHE_CHART_DISPLAYED),
-    panel.once(EVENTS.EMPTY_CACHE_CHART_DISPLAYED)
-  ]);
-  let onPlaceholderDisplayed = panel.once(EVENTS.PLACEHOLDER_CHARTS_DISPLAYED);
+  is(body.selectedPanel.id, "inspector-panel",
+    "The current main panel is correct.");
 
   info("Displaying statistics view");
   gStore.dispatch(Actions.openStatistics(true));
-  is(NetMonitorView.currentFrontendMode, "network-statistics-view",
-   "The current frontend mode is correct.");
+  is(body.selectedPanel.id, "statistics-panel",
+    "The current main panel is correct.");
 
   info("Waiting for placeholder to display");
-  yield onPlaceholderDisplayed;
-  is($("#primed-cache-chart").childNodes.length, 1,
+
+  is(document.querySelector(".primed-cache-chart").childNodes.length, 1,
     "There should be a placeholder primed cache chart created now.");
-  is($("#empty-cache-chart").childNodes.length, 1,
+  is(document.querySelector(".empty-cache-chart").childNodes.length, 1,
     "There should be a placeholder empty cache chart created now.");
 
-  is($all(".pie-chart-container[placeholder=true]").length, 2,
+  is(document.querySelectorAll(".pie-chart-container[placeholder=true]").length, 2,
     "Two placeholder pie chart appear to be rendered correctly.");
-  is($all(".table-chart-container[placeholder=true]").length, 2,
+  is(document.querySelectorAll(".table-chart-container[placeholder=true]").length, 2,
     "Two placeholder table chart appear to be rendered correctly.");
 
   info("Waiting for chart to display");
-  yield onChartDisplayed;
-  is($("#primed-cache-chart").childNodes.length, 1,
+
+  is(document.querySelector(".primed-cache-chart").childNodes.length, 1,
     "There should be a real primed cache chart created now.");
-  is($("#empty-cache-chart").childNodes.length, 1,
+  is(document.querySelector(".empty-cache-chart").childNodes.length, 1,
     "There should be a real empty cache chart created now.");
 
   yield waitUntil(
-    () => $all(".pie-chart-container:not([placeholder=true])").length == 2);
+    () => document.querySelectorAll(".pie-chart-container:not([placeholder=true])").length == 2);
   ok(true, "Two real pie charts appear to be rendered correctly.");
 
   yield waitUntil(
-    () => $all(".table-chart-container:not([placeholder=true])").length == 2);
+    () => document.querySelectorAll(".table-chart-container:not([placeholder=true])").length == 2);
   ok(true, "Two real table charts appear to be rendered correctly.");
 
   yield teardown(monitor);
