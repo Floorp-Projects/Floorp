1761c7357a506e16a334c4a6e34d99ced4e10bd2
Ricky Chien <rchien@mozilla.com>
2017-01-31 19:29:00 +0800

    Bug 1336379 - Implement StatisticsPanel r=Honza
    MozReview-Commit-ID: FNCSetNPzz6

--- devtools/client/netmonitor/netmonitor-view.js
+++ devtools/client/netmonitor/netmonitor-view.js
@@ -2,18 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-disable mozilla/reject-some-requires */
 /* globals $, gStore, NetMonitorController, dumpn */
 
 "use strict";
 
-const { testing: isTesting } = require("devtools/shared/flags");
 const { Task } = require("devtools/shared/task");
 const { ViewHelpers } = require("devtools/client/shared/widgets/view-helpers");
 const { RequestsMenuView } = require("./requests-menu-view");
 const { CustomRequestView } = require("./custom-request-view");
 const { SidebarView } = require("./sidebar-view");
-const { StatisticsView } = require("./statistics-view");
 const { ACTIVITY_TYPE } = require("./constants");
 const { Prefs } = require("./prefs");
 const { createFactory } = require("devtools/client/shared/vendor/react");
@@ -23,17 +20,8 @@ const Provider = createFactory(require("devtools/client/shared/vendor/react-redu
 
 // Components
 const DetailsPanel = createFactory(require("./shared/components/details-panel"));
+const StatisticsPanel = createFactory(require("./components/statistics-panel"));
 const Toolbar = createFactory(require("./components/toolbar"));
-
-// ms
-const WDA_DEFAULT_VERIFY_INTERVAL = 50;
-
-// Use longer timeout during testing as the tests need this process to succeed
-// and two seconds is quite short on slow debug builds. The timeout here should
-// be at least equal to the general mochitest timeout of 45 seconds so that this
-// never gets hit during testing.
-// ms
-const WDA_DEFAULT_GIVE_UP_TIMEOUT = isTesting ? 45000 : 2000;
 
 /**
  * Object defining the network monitor view components.
@@ -52,6 +40,13 @@ var NetMonitorView = {
       DetailsPanel({ toolbox: NetMonitorController._toolbox }),
     ), this.detailsPanel);
 
+    this.statisticsPanel = $("#statistics-panel");
+
+    ReactDOM.render(Provider(
+      { store: gStore },
+      StatisticsPanel(),
+    ), this.statisticsPanel);
+
     this.toolbar = $("#react-toolbar-hook");
 
     ReactDOM.render(Provider(
@@ -61,7 +56,6 @@ var NetMonitorView = {
 
     this.RequestsMenu.initialize(gStore);
     this.CustomRequest.initialize();
-    this.Statistics.initialize(gStore);
 
     // Store watcher here is for observing the statisticsOpen state change.
     // It should be removed once we migrate to react and apply react/redex binding.
@@ -79,8 +73,8 @@ var NetMonitorView = {
     this._isDestroyed = true;
     this.RequestsMenu.destroy();
     this.CustomRequest.destroy();
-    this.Statistics.destroy();
     ReactDOM.unmountComponentAtNode(this.detailsPanel);
+    ReactDOM.unmountComponentAtNode(this.statisticsPanel);
     ReactDOM.unmountComponentAtNode(this.toolbar);
     this.unsubscribeStore();
 
@@ -149,10 +143,6 @@ var NetMonitorView = {
     }
   },
 
-  /**
-   * Gets the current mode for this tool.
-   * @return string (e.g, "network-inspector-view" or "network-statistics-view")
-   */
   get currentFrontendMode() {
     // The getter may be called from a timeout after the panel is destroyed.
     if (!this._body.selectedPanel) {
@@ -161,9 +151,6 @@ var NetMonitorView = {
     return this._body.selectedPanel.id;
   },
 
-  /**
-   * Toggles between the frontend view modes ("Inspector" vs. "Statistics").
-   */
   toggleFrontendMode: function () {
     if (gStore.getState().ui.statisticsOpen) {
       this.showNetworkStatisticsView();
@@ -172,45 +159,13 @@ var NetMonitorView = {
     }
   },
 
-  /**
-   * Switches to the "Inspector" frontend view mode.
-   */
   showNetworkInspectorView: function () {
-    this._body.selectedPanel = $("#network-inspector-view");
+    this._body.selectedPanel = $("#inspector-panel");
   },
 
-  /**
-   * Switches to the "Statistics" frontend view mode.
-   */
   showNetworkStatisticsView: function () {
-    this._body.selectedPanel = $("#network-statistics-view");
-
-    let controller = NetMonitorController;
-    let requestsView = this.RequestsMenu;
-    let statisticsView = this.Statistics;
-
-    Task.spawn(function* () {
-      statisticsView.displayPlaceholderCharts();
-      yield controller.triggerActivity(ACTIVITY_TYPE.RELOAD.WITH_CACHE_ENABLED);
-
-      try {
-        // • The response headers and status code are required for determining
-        // whether a response is "fresh" (cacheable).
-        // • The response content size and request total time are necessary for
-        // populating the statistics view.
-        // • The response mime type is used for categorization.
-        yield whenDataAvailable(requestsView.store, [
-          "responseHeaders", "status", "contentSize", "mimeType", "totalTime"
-        ]);
-      } catch (ex) {
-        // Timed out while waiting for data. Continue with what we have.
-        console.error(ex);
-      }
-
-      const requests = requestsView.store.getState().requests.requests.valueSeq();
-      statisticsView.createPrimedCacheChart(requests);
-      statisticsView.createEmptyCacheChart(requests);
-    });
+    this._body.selectedPanel = $("#statistics-panel");
+    NetMonitorController.triggerActivity(ACTIVITY_TYPE.RELOAD.WITH_CACHE_ENABLED);
   },
 
   reloadPage: function () {
@@ -221,41 +176,6 @@ var NetMonitorView = {
   _body: null,
   _detailsPane: null,
 };
-
-/**
- * Makes sure certain properties are available on all objects in a data store.
- *
- * @param Store dataStore
- *        A Redux store for which to check the availability of properties.
- * @param array mandatoryFields
- *        A list of strings representing properties of objects in dataStore.
- * @return object
- *         A promise resolved when all objects in dataStore contain the
- *         properties defined in mandatoryFields.
- */
-function whenDataAvailable(dataStore, mandatoryFields) {
-  return new Promise((resolve, reject) => {
-    let interval = setInterval(() => {
-      const { requests } = dataStore.getState().requests;
-      const allFieldsPresent = !requests.isEmpty() && requests.every(
-        item => mandatoryFields.every(
-          field => item.get(field) !== undefined
-        )
-      );
-
-      if (allFieldsPresent) {
-        clearInterval(interval);
-        clearTimeout(timer);
-        resolve();
-      }
-    }, WDA_DEFAULT_VERIFY_INTERVAL);
-
-    let timer = setTimeout(() => {
-      clearInterval(interval);
-      reject(new Error("Timed out while waiting for data"));
-    }, WDA_DEFAULT_GIVE_UP_TIMEOUT);
-  });
-}
 
 // A smart store watcher to notify store changes as necessary
 function storeWatcher(initialValue, reduceValue, onChange) {
@@ -276,6 +196,5 @@ function storeWatcher(initialValue, reduceValue, onChange) {
 NetMonitorView.Sidebar = new SidebarView();
 NetMonitorView.RequestsMenu = new RequestsMenuView();
 NetMonitorView.CustomRequest = new CustomRequestView();
-NetMonitorView.Statistics = new StatisticsView();
 
 exports.NetMonitorView = NetMonitorView;
