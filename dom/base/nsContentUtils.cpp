7bf741ca7150ae7eb7faa4302faac19eb55498ab
Boris Zbarsky <bzbarsky@mit.edu>
2017-02-01 15:43:36 -0500

    Bug 1335368 part 4.  Stop using IsCallerChrome in nsContentUtils::IsRequestFullScreenAllowed.  r=bholley

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -6831,11 +6831,11 @@ nsContentUtils::IsFullScreenApiEnabled()
 
 /* static */
 bool
-nsContentUtils::IsRequestFullScreenAllowed()
+nsContentUtils::IsRequestFullScreenAllowed(CallerType aCallerType)
 {
   return !sTrustedFullScreenOnly ||
          EventStateManager::IsHandlingUserInput() ||
-         IsCallerChrome();
+         aCallerType == CallerType::System;
 }
 
 /* static */
