38b0eb4c9419a8e554e307e4a2eda8ddd49a2a39
Gerald Squelart <gsquelart@mozilla.com>
2017-02-01 19:17:29 +1100

    Bug 1334445 - Abort AnnexB parsing on ByteWriter failure. r=jya, a=jcristau
    All writes through a ByteWriter are now checked for success.
    Possible failures could be in three classes:
    A. Attempt to write a single unreasonably-large block of data. In this case we
    just don't allocate more memory, so Firefox should keep functioning but simply
    refuse to play this media.
    B. Accumulation of a lot of data into a single vector. In this case we return
    from the function, which should immediately free the large vector, and
    hopefully allow Firefox to continue unscathed; but there is the possibility
    that other memory allocations elsewhere will fail in the meantime.
    C. OOM across all of Firefox, and this write is just the final drop. Like
    previously we will still try to return and deallocate some memory, but this
    may not be enough to recover.
    
    So even though this patch cannot fix all possible OOM causes, I think it is
    worth adding, as it is simple enough, and will help in some cases where
    corrupted data could sensibly be handled without making Firefox crash.
    
    MozReview-Commit-ID: JPVC9hao4Uq
    
    --HG--
    extra : source : feb30777c832e60eb05a15d8c01ce01ba35da426
    extra : intermediate-source : 4803d9443a778cfc4ba0c77b64feea46f5329c42

--- media/libstagefright/binding/include/mp4_demuxer/ByteWriter.h
+++ media/libstagefright/binding/include/mp4_demuxer/ByteWriter.h
@@ -22,49 +22,49 @@ public:
   {
   }
 
-  void WriteU8(uint8_t aByte)
+  MOZ_MUST_USE bool WriteU8(uint8_t aByte)
   {
-    Write(&aByte, 1);
+    return Write(&aByte, 1);
   }
 
-  void WriteU16(uint16_t aShort)
+  MOZ_MUST_USE bool WriteU16(uint16_t aShort)
   {
     uint8_t c[2];
     mozilla::BigEndian::writeUint16(&c[0], aShort);
-    Write(&c[0], 2);
+    return Write(&c[0], 2);
   }
 
-  void WriteU32(uint32_t aLong)
+  MOZ_MUST_USE bool WriteU32(uint32_t aLong)
   {
     uint8_t c[4];
     mozilla::BigEndian::writeUint32(&c[0], aLong);
-    Write(&c[0], 4);
+    return Write(&c[0], 4);
   }
 
-  void Write32(int32_t aLong)
+  MOZ_MUST_USE bool Write32(int32_t aLong)
   {
     uint8_t c[4];
     mozilla::BigEndian::writeInt32(&c[0], aLong);
-    Write(&c[0], 4);
+    return Write(&c[0], 4);
   }
 
-  void WriteU64(uint64_t aLongLong)
+  MOZ_MUST_USE bool WriteU64(uint64_t aLongLong)
   {
     uint8_t c[8];
     mozilla::BigEndian::writeUint64(&c[0], aLongLong);
-    Write(&c[0], 8);
+    return Write(&c[0], 8);
   }
 
-  void Write64(int64_t aLongLong)
+  MOZ_MUST_USE bool Write64(int64_t aLongLong)
   {
     uint8_t c[8];
     mozilla::BigEndian::writeInt64(&c[0], aLongLong);
-    Write(&c[0], 8);
+    return Write(&c[0], 8);
   }
 
-  void Write(const uint8_t* aSrc, size_t aCount)
+  MOZ_MUST_USE bool Write(const uint8_t* aSrc, size_t aCount)
   {
-    MOZ_RELEASE_ASSERT(mPtr.append(aSrc, aCount));
+    return mPtr.append(aSrc, aCount);
   }
 
 private:
