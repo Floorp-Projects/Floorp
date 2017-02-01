a51561a1c43ff94040ae292b9a358dd6d6f450a1
J.C. Jones <jjones@mozilla.com>
2017-02-01 15:21:04 -0700

    Bug 1260318 - Scope U2F Soft Tokens to a single AppID r=qdot,rbarnes
    This change includes the FIDO "App ID" as part of the function used to generate
    the wrapping key used in the NSS-based U2F soft token, cryptographically binding
    the "Key Handle" to the site that Key Handle is intended for.
    
    This is a breaking change with existing registered U2F keys, but since our soft
    token is hidden behind a pref, it does not attempt to be backward-compatible.
    
    - Updated for rbarnes' and qdot's reviews comments. Thanks!
    - Made more strict in size restrictions, and added a version field
      to help us be this strict.
    - Bugfix for an early unprotected buffer use (Thanks again rbarnes!)
    - Fix a sneaky memory leak re: CryptoBuffer.ToSECItem
    
    MozReview-Commit-ID: Jf6gNPauT4Y
    
    --HG--
    extra : rebase_source : 4ff5898e93e4a0a75576e5e54035a1cb6dd952d7

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3442,6 +3442,7 @@ ContentParent::RecvNSSU2FTokenIsCompatibleVersion(const nsString& aVersion,
 
 mozilla::ipc::IPCResult
 ContentParent::RecvNSSU2FTokenIsRegistered(nsTArray<uint8_t>&& aKeyHandle,
+                                           nsTArray<uint8_t>&& aApplication,
                                            bool* aIsValidKeyHandle)
 {
   MOZ_ASSERT(aIsValidKeyHandle);
@@ -3452,6 +3453,7 @@ ContentParent::RecvNSSU2FTokenIsRegistered(nsTArray<uint8_t>&& aKeyHandle,
   }
 
   nsresult rv = nssToken->IsRegistered(aKeyHandle.Elements(), aKeyHandle.Length(),
+                                       aApplication.Elements(), aApplication.Length(),
                                        aIsValidKeyHandle);
   if (NS_FAILED(rv)) {
     return IPC_FAIL_NO_REASON(this);
