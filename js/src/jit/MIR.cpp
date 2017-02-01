6f540c093a179e94ce4dd8854615f263f4e2e982
Hannes Verschore <hv1989@gmail.com>
2017-02-01 01:36:26 +0100

    Bug 1328133: IonMonkey - Ensure ballast in PrototypeHasIndexedProperty, r=nbp a=gchang
    --HG--
    extra : source : 2286081f1ce3fa6d38d8d14cc2ecee918b6ab252

--- js/src/jit/MIR.cpp
+++ js/src/jit/MIR.cpp
@@ -6017,7 +6017,7 @@ jit::ElementAccessMightBeFrozen(CompilerConstraintList* constraints, MDefinition
     return !types || types->hasObjectFlags(constraints, OBJECT_FLAG_FROZEN);
 }
 
-bool
+AbortReasonOr<bool>
 jit::ElementAccessHasExtraIndexedProperty(IonBuilder* builder, MDefinition* obj)
 {
     TemporaryTypeSet* types = obj->resultTypeSet();
@@ -6354,7 +6354,7 @@ jit::AddObjectsForPropertyRead(MDefinition* obj, PropertyName* name,
     }
 }
 
-static bool
+AbortReasonOr<bool>
 PrototypeHasIndexedProperty(IonBuilder* builder, JSObject* obj)
 {
     do {
@@ -6367,13 +6367,15 @@ PrototypeHasIndexedProperty(IonBuilder* builder, JSObject* obj)
         if (index.nonData(builder->constraints()) || index.isOwnProperty(builder->constraints()))
             return true;
         obj = obj->staticPrototype();
+        if (!builder->alloc().ensureBallast())
+            return builder->abort(AbortReason::Alloc);
     } while (obj);
 
     return false;
 }
 
 // Whether Array.prototype, or an object on its proto chain, has an indexed property.
-bool
+AbortReasonOr<bool>
 jit::ArrayPrototypeHasIndexedProperty(IonBuilder* builder, JSScript* script)
 {
     if (JSObject* proto = script->global().maybeGetArrayPrototype())
@@ -6382,7 +6384,7 @@ jit::ArrayPrototypeHasIndexedProperty(IonBuilder* builder, JSScript* script)
 }
 
 // Whether obj or any of its prototypes have an indexed property.
-bool
+AbortReasonOr<bool>
 jit::TypeCanHaveExtraIndexedProperties(IonBuilder* builder, TemporaryTypeSet* types)
 {
     const Class* clasp = types->getKnownClass(builder->constraints());
