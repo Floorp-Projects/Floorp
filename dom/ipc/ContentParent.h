e854a721215c35ee69998d1f390a58ca17905fb3
J.C. Jones <jjones@mozilla.com>
2017-02-01 15:21:04 -0700

    Bug 1260318 - Scope U2F Soft Tokens to a single AppID r=qdot,rbarnes
    This change includes the FIDO "App ID" as part of the function used to generate
    the wrapping key used in the NSS-based U2F soft token, cryptographically binding
    the "Key Handle" to the site that Key Handle is intended for.
    
    This is a breaking change with existing registered U2F keys, but since our soft
    token is hidden behind a pref, it does not attempt to be backward-compatible.
    
    - Updated for rbarnes' and qdot's reviews comments. Thanks!
    - Made more strict in size restrictions, and added a version field
      to help us be this strict.
    - Bugfix for an early unprotected buffer use (Thanks again rbarnes!)
    - Fix a sneaky memory leak re: CryptoBuffer.ToSECItem
    
    MozReview-Commit-ID: Jf6gNPauT4Y

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -795,6 +795,7 @@ private:
                                                                      bool* aIsCompatible) override;
 
   virtual mozilla::ipc::IPCResult RecvNSSU2FTokenIsRegistered(nsTArray<uint8_t>&& aKeyHandle,
+                                                              nsTArray<uint8_t>&& aApplication,
                                                               bool* aIsValidKeyHandle) override;
 
   virtual mozilla::ipc::IPCResult RecvNSSU2FTokenRegister(nsTArray<uint8_t>&& aApplication,
