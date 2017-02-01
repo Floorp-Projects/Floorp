b0bba0c768bb481d9d6cbc8cf1f7d44aee69a4b6
Bill McCloskey <billm@mozilla.com>
2017-02-01 14:08:46 -0800

    Bug 1337537 - Make a SystemGroup singleton (r=ehsan)
    MozReview-Commit-ID: Jnwrr49daeM

--- xpcom/threads/SystemGroup.cpp
+++ xpcom/threads/SystemGroup.cpp
@@ -7,21 +7,91 @@
 #include "SystemGroup.h"
 
 #include "mozilla/Move.h"
+#include "mozilla/UniquePtr.h"
 #include "nsINamed.h"
 
 using namespace mozilla;
+
+class SystemGroupImpl final : public ValidatingDispatcher
+{
+public:
+  SystemGroupImpl();
+  ~SystemGroupImpl() {}
+
+  static void InitStatic();
+  static void ShutdownStatic();
+  static SystemGroupImpl* Get();
+
+  NS_METHOD_(MozExternalRefCountType) AddRef(void)
+  {
+    return 2;
+  }
+  NS_METHOD_(MozExternalRefCountType) Release(void)
+  {
+    return 1;
+  }
+
+private:
+  static UniquePtr<SystemGroupImpl> sSingleton;
+};
+
+UniquePtr<SystemGroupImpl> SystemGroupImpl::sSingleton;
+
+SystemGroupImpl::SystemGroupImpl()
+{
+  CreateEventTargets(/* aNeedValidation = */ true);
+}
+
+/* static */ void
+SystemGroupImpl::InitStatic()
+{
+  MOZ_ASSERT(!sSingleton);
+  MOZ_ASSERT(NS_IsMainThread());
+  sSingleton = MakeUnique<SystemGroupImpl>();
+}
+
+/* static */ void
+SystemGroupImpl::ShutdownStatic()
+{
+  sSingleton->Shutdown();
+  sSingleton = nullptr;
+}
+
+/* static */ SystemGroupImpl*
+SystemGroupImpl::Get()
+{
+  MOZ_ASSERT(sSingleton);
+  return sSingleton.get();
+}
+
+void
+SystemGroup::InitStatic()
+{
+  SystemGroupImpl::InitStatic();
+}
+
+void
+SystemGroup::Shutdown()
+{
+  SystemGroupImpl::ShutdownStatic();
+}
 
 /* static */ nsresult
 SystemGroup::Dispatch(const char* aName,
                       TaskCategory aCategory,
                       already_AddRefed<nsIRunnable>&& aRunnable)
 {
-  return Dispatcher::UnlabeledDispatch(aName, aCategory, Move(aRunnable));
+  return SystemGroupImpl::Get()->Dispatch(aName, aCategory, Move(aRunnable));
 }
 
 /* static */ nsIEventTarget*
 SystemGroup::EventTargetFor(TaskCategory aCategory)
 {
-  nsCOMPtr<nsIEventTarget> main = do_GetMainThread();
-  return main;
+  return SystemGroupImpl::Get()->EventTargetFor(aCategory);
+}
+
+/* static */ AbstractThread*
+SystemGroup::AbstractMainThreadFor(TaskCategory aCategory)
+{
+  return SystemGroupImpl::Get()->AbstractMainThreadFor(aCategory);
 }
