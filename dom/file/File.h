6dfd275c58eee3a322f52e0491fed0480c857b4b
Andrea Marchesini <amarchesini@mozilla.com>
2017-02-01 17:13:08 +0100

    Bug 1335536 - File.createFromNsIFile and File.createFromFileName should be async - part 1 - tests, r=smaug

--- dom/file/File.h
+++ dom/file/File.h
@@ -43,6 +43,7 @@ struct FilePropertyBag;
 class BlobImpl;
 class File;
 class OwningArrayBufferOrArrayBufferViewOrBlobOrUSVString;
+class Promise;
 
 class Blob : public nsIDOMBlob
            , public nsIXHRSendable
@@ -204,14 +205,14 @@ public:
               ErrorResult& aRv);
 
   // ChromeOnly
-  static already_AddRefed<File>
+  static already_AddRefed<Promise>
   CreateFromFileName(const GlobalObject& aGlobal,
                      const nsAString& aData,
                      const ChromeFilePropertyBag& aBag,
                      ErrorResult& aRv);
 
   // ChromeOnly
-  static already_AddRefed<File>
+  static already_AddRefed<Promise>
   CreateFromNsIFile(const GlobalObject& aGlobal,
                     nsIFile* aData,
                     const ChromeFilePropertyBag& aBag,
