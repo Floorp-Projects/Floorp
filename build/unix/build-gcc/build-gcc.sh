a29b4e60841b68a9454d5870c1dbbcee7ad7bcc7
Mike Hommey <mh+mozilla@glandium.org>
2017-02-01 16:35:29 +0900

    Bug 1335667 - Validate all downloaded sources when building GCC. r=froydnj
    We can just check the GPG signature for the upstream tarballs that are
    GPG signed. We keep a copy of the relevant GPG keys in tree so that
    we only use a controlled set of keys.
    
    I validated the GPG keys by:
    - Creating a fresh keyring.
    - Importing the keys with gpg --receive-key.
    - Importing my own GPG public key in that keyring.
    - Importing the gpg keys that the PGP pathfinder told me were on the path
      to those keys (which weren't directly in their keyring, so I had to
      manually find some steps first).
    - Using `gpg --check-sigs` to validate that the all those keys I got are
      the right ones.
    
    Then the relevant GPG keys were exported with `gpg --export --armor` and
    stripped with https://github.com/glandium/pgpstrip/.
    
    For MPC, the first GPG-signed version upstream was 0.8.2, while the GCC
    script to download prerequisites downloads 0.8.1. So instead of using
    0.8.1, we use 0.8.2, which we can verify.
    
    For GMP, the GCC script downloads 4.3.2. The only web-of-trust path is
    through a revoked key, which signs a revoked uid of the GMP key.
    Releases newer than 5.1.0 are signed with a new key that can be
    validated with the steps above. So instead of using 4.3.2, we use 5.1.3
    (last of the 5.1.x line).
    
    But MPFR 2.4.2, which the GCC script downloads, doesn't build against
    GMP 5.1.3, so instead of that, we use MPFR 3.1.5.
    
    Sadly, the remaining GCC prerequisites are not signed, so I had to:
    - Download the files from ftp.gnu.org.
    - Download the corresponding files from snapshot.debian.org.
    - Compare the raw files when possible, or the uncompressed (not extracted)
      files (when, thankfully, they matched).
    - Validate those snapshot.debian.org files checksums against the
      checksums in the corresponding Sources.bz2/xz files.
    - Validate the Sources.bz2/xz checksums against the corresponding InRelease
      files.
    - Validate the InRelease files GPG signatures against the Debian
      archives keyring.
    
    With all those things we actually don't get through the GCC script, we
    also change how we get those prerequisites, by diverting the commands
    the script runs and making it output the urls instead of downloading and
    extracting the files.
    
    All downloaded files, GPG-validated or otherwise, have their SHA-256
    digest checked against a list in build/unix/build-gcc/checksums.

--- build/unix/build-gcc/build-gcc.sh
+++ build/unix/build-gcc/build-gcc.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+set -x
+
 gcc_version=4.9.4
 binutils_version=2.25.1
 this_path=$(readlink -f $(dirname $0))
@@ -17,7 +19,33 @@ if test -z $TMPDIR; then
   TMPDIR=/tmp/
 fi
 
-wget -c -P $TMPDIR ftp://ftp.gnu.org/gnu/binutils/binutils-$binutils_version.tar.bz2
+mkdir gpg
+GPG="gpg --homedir $root_dir/gpg"
+# GPG key used to sign GCC
+$GPG --import $this_path/13975A70E63C361C73AE69EF6EEB81F8981C74C7.key
+# GPG key used to sign binutils
+$GPG --import $this_path/EAF1C276A747E9ED86210CBAC3126D3B4AE55E93.key
+# GPG key used to sign GMP
+$GPG --import $this_path/343C2FF0FBEE5EC2EDBEF399F3599FF828C67298.key
+# GPG key used to sign MPFR
+$GPG --import $this_path/07F3DBBECC1A39605078094D980C197698C3739D.key
+# GPG key used to sign MPC
+$GPG --import $this_path/AD17A21EF8AED8F1CC02DBD9F7D5C9BF765C61E3.key
+
+> $root_dir/downloads
+
+download() {
+  wget -c -P $TMPDIR $1/$2
+  (cd $TMPDIR; sha256sum $2) >> $root_dir/downloads
+}
+
+download_and_check() {
+  download $1 ${2%.*}
+  wget -c -P $TMPDIR $1/$2
+  $GPG --verify $TMPDIR/$2 $TMPDIR/${2%.*}
+}
+
+download_and_check ftp://ftp.gnu.org/gnu/binutils binutils-$binutils_version.tar.bz2.sig
 tar xjf $TMPDIR/binutils-$binutils_version.tar.bz2
 mkdir binutils-objdir
 cd binutils-objdir
@@ -30,16 +58,54 @@ cd ..
 
 case "$gcc_version" in
 *-*)
-  wget -c -P $TMPDIR ftp://gcc.gnu.org/pub/gcc/snapshots/$gcc_version/gcc-$gcc_version.tar.bz2
+  download ftp://gcc.gnu.org/pub/gcc/snapshots/$gcc_version/gcc-$gcc_version.tar.bz2
   ;;
 *)
-  wget -c -P $TMPDIR ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version/gcc-$gcc_version.tar.bz2
+  download_and_check ftp://ftp.gnu.org/gnu/gcc/gcc-$gcc_version gcc-$gcc_version.tar.bz2.sig
   ;;
 esac
 tar xjf $TMPDIR/gcc-$gcc_version.tar.bz2
 cd gcc-$gcc_version
 
-./contrib/download_prerequisites
+(
+  # Divert commands that download_prerequisites use
+  ln() { :; }
+  tar() { :; }
+  sed() { :; }
+  wget() {
+    echo $1
+  }
+
+  . ./contrib/download_prerequisites
+) | while read url; do
+  file=$(basename $url)
+  case "$file" in
+  gmp-*.tar.*)
+    # If download_prerequisites wants 4.3.2, use 5.1.3 instead.
+    file=${file/4.3.2/5.1.3}
+    download_and_check https://gmplib.org/download/gmp $file.sig
+    ;;
+  mpfr-*.tar.*)
+    # If download_prerequisites wants 2.4.2, use 3.1.5 instead.
+    file=${file/2.4.2/3.1.5}
+    download_and_check http://www.mpfr.org/${file%.tar.*} $file.asc
+    ;;
+  mpc-*.tar.*)
+    # If download_prerequisites wants 0.8.1, use 0.8.2 instead.
+    file=${file/0.8.1/0.8.2}
+    download_and_check http://www.multiprecision.org/mpc/download $file.asc
+    ;;
+  *)
+    download $(dirname $url) $file
+    ;;
+  esac
+  tar xaf $TMPDIR/$file
+  ln -sf ${file%.tar.*} ${file%-*}
+done
+
+# Check all the downloads we did are in the checksums list, and that the
+# checksums match.
+diff -u <(sort -k 2 $root_dir/downloads) $this_path/checksums
 
 patch -p1 < "${this_path}/PR64905.patch"
 
