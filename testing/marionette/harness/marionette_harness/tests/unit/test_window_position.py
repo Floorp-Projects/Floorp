2e9f0aa4b3906165f1bf1a4d0d275a2f1ab9c421
Andreas Tolfsen <ato@mozilla.com>
2017-02-02 14:57:19 +0000

    Bug 1319237 - Improve window position test assertions. r=maja_zf, a=test-only
    MozReview-Commit-ID: qmFS0JEqx3
    
    --HG--
    extra : source : eb9c22a9b7076f39af3f3ef95bf65beaefe1daef
    extra : histedit_source : 6c5331a0d16dae960c2c160eed24d58ed1ec575e

--- testing/marionette/harness/marionette_harness/tests/unit/test_window_position.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_window_position.py
@@ -10,23 +10,24 @@ from marionette_harness import MarionetteTestCase
 class TestWindowPosition(MarionetteTestCase):
     def test_get_types(self):
         position = self.marionette.get_window_position()
-        self.assertTrue(isinstance(position["x"], int))
-        self.assertTrue(isinstance(position["y"], int))
+        self.assertIsInstance(position["x"], int)
+        self.assertIsInstance(position["y"], int)
 
     def test_set_types(self):
         for x, y in (["a", "b"], [1.2, 3.4], [True, False], [[], []], [{}, {}]):
+            print("testing invalid type position ({},{})".format(x, y))
             with self.assertRaises(InvalidArgumentException):
                 self.marionette.set_window_position(x, y)
 
     def test_out_of_bounds_arguments(self):
-        with self.assertRaises(InvalidArgumentException):
-            self.marionette.set_window_position(-1, 0)
-        with self.assertRaises(InvalidArgumentException):
-            self.marionette.set_window_position(0, -1)
+        for x, y in [(-1,0), (0,-1), (-1,-1)]:
+            print("testing out of bounds position ({},{})".format(x, y))
+            with self.assertRaises(InvalidArgumentException):
+                self.marionette.set_window_position(x, y)
 
-    def test_move(self):
+    def test_move_to_new_position(self):
         old_position = self.marionette.get_window_position()
         new_position = {"x": old_position["x"] + 10, "y": old_position["y"] + 10}
         self.marionette.set_window_position(new_position["x"], new_position["y"])
-        self.assertNotEqual(old_position['x'], new_position["x"])
-        self.assertNotEqual(old_position['y'], new_position["y"])
+        self.assertNotEqual(old_position["x"], new_position["x"])
+        self.assertNotEqual(old_position["y"], new_position["y"])
