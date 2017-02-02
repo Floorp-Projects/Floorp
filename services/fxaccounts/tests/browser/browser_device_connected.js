809b4550844fd53009369d0c562ff381d1dcee0b
Edouard Oger <eoger@fastmail.com>
2017-02-02 15:49:03 -0500

    Bug 1332858 - Alternate wording for Sync new device connected notification when device name missing. r=markh
    MozReview-Commit-ID: DdwrRGjwI3
    
    --HG--
    extra : rebase_source : 9740cd1fb21fd411c73d8a29e5c3ae31bf11bcb0

--- services/fxaccounts/tests/browser/browser_device_connected.js
+++ services/fxaccounts/tests/browser/browser_device_connected.js
@@ -6,33 +6,55 @@ Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 const { MockRegistrar } =
   Cu.import("resource://testing-common/MockRegistrar.jsm", {});
 
-const StubAlertsService = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAlertsService, Ci.nsISupports]),
+let accountsBundle = Services.strings.createBundle(
+  "chrome://browser/locale/accounts.properties"
+);
 
-  showAlertNotification(image, title, text, clickable, cookie, clickCallback) {
-    // We can't simulate a click on the alert popup,
-    // so instead we call the click listener ourselves directly
-    clickCallback.observe(null, "alertclickcallback", null);
-  }
-}
+let expectedBody;
 
-add_task(function*() {
+add_task(async function setup() {
+  const alertsService = {
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIAlertsService, Ci.nsISupports]),
+    showAlertNotification: (image, title, text, clickable, cookie, clickCallback) => {
+      // We can't simulate a click on the alert popup,
+      // so instead we call the click listener ourselves directly
+      clickCallback.observe(null, "alertclickcallback", null);
+      Assert.equal(text, expectedBody);
+    }
+  };
+  let alertsServiceCID = MockRegistrar.register("@mozilla.org/alerts-service;1", alertsService);
+  registerCleanupFunction(() => {
+    MockRegistrar.unregister(alertsServiceCID);
+  });
+});
+
+async function testDeviceConnected(deviceName) {
+  info("testDeviceConnected with deviceName=" + deviceName);
   gBrowser.selectedBrowser.loadURI("about:robots");
-  yield waitForDocLoadComplete();
+  await waitForDocLoadComplete();
 
-  MockRegistrar.register("@mozilla.org/alerts-service;1", StubAlertsService);
   Preferences.set("identity.fxaccounts.settings.devices.uri", "http://localhost/devices");
 
   let waitForTabPromise = BrowserTestUtils.waitForNewTab(gBrowser);
 
-  Services.obs.notifyObservers(null, "fxaccounts:device_connected", "My phone");
+  Services.obs.notifyObservers(null, "fxaccounts:device_connected", deviceName);
 
-  let tab = yield waitForTabPromise;
+  let tab = await waitForTabPromise;
   Assert.ok("Tab successfully opened");
 
   let expectedURI = Preferences.get("identity.fxaccounts.settings.devices.uri",
                                     "prefundefined");
   Assert.equal(tab.linkedBrowser.currentURI.spec, expectedURI);
 
-  yield BrowserTestUtils.removeTab(tab);
+  await BrowserTestUtils.removeTab(tab);
+}
+
+add_task(async function() {
+  expectedBody = accountsBundle.formatStringFromName("deviceConnectedBody", ["My phone"], 1);
+  await testDeviceConnected("My phone");
+});
+
+add_task(async function() {
+  expectedBody = accountsBundle.GetStringFromName("deviceConnectedBody.noDeviceName");
+  await testDeviceConnected(null);
 });
