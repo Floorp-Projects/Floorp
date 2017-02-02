39893e0a0c80f4b96ed06207fc9201d1c602c998
Gregory Szorc <gps@mozilla.com>
2017-02-02 15:30:16 -0800

    Bug 1322769 - Add a "servo" feature; r=manishearth
    MozReview-Commit-ID: A9dNTxNkl39

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -2,6 +2,9 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#[cfg(feature="servo")]
+extern crate geckoservo;
+
 extern crate mp4parse_capi;
 extern crate nsstring;
 extern crate rust_url_capi;
