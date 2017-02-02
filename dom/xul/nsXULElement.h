266f7a9f5fe1a0ca96bf27f9d3e13520c4e139f0
Boris Zbarsky <bzbarsky@mit.edu>
2017-02-02 10:32:57 -0500

    Bug 835981 part 3.  Switch nsIDOMXULElement::DoCommand consumers to nsXULElement instead.  r=peterv

--- dom/xul/nsXULElement.h
+++ dom/xul/nsXULElement.h
@@ -564,7 +564,7 @@ public:
     nsIControllers* GetControllers(mozilla::ErrorResult& rv);
     already_AddRefed<mozilla::dom::BoxObject> GetBoxObject(mozilla::ErrorResult& rv);
     void Click(mozilla::ErrorResult& rv);
-    // The XPCOM DoCommand never fails, so it's OK for us.
+    void DoCommand();
     already_AddRefed<nsINodeList>
       GetElementsByAttribute(const nsAString& aAttribute,
                              const nsAString& aValue);
