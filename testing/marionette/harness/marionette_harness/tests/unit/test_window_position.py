04f8a708be1e361a37e5d95a48791131060391e2
Andreas Tolfsen <ato@mozilla.com>
2017-02-02 14:58:36 +0000

    Bug 1319237 - Avoid reposition waiting if position is unchanged. r=maja_zf, a=test-only
    When the requested window position is the same as the current position,
    we should avoid the wait condition.
    
    MozReview-Commit-ID: 3koG5BeOkFC

--- testing/marionette/harness/marionette_harness/tests/unit/test_window_position.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_window_position.py
@@ -31,3 +31,10 @@ class TestWindowPosition(MarionetteTestCase):
         self.marionette.set_window_position(new_position["x"], new_position["y"])
         self.assertNotEqual(old_position["x"], new_position["x"])
         self.assertNotEqual(old_position["y"], new_position["y"])
+
+    def test_move_to_existing_position(self):
+        old_position = self.marionette.get_window_position()
+        self.marionette.set_window_position(old_position["x"], old_position["y"])
+        new_position = self.marionette.get_window_position()
+        self.assertEqual(old_position["x"], new_position["x"])
+        self.assertEqual(old_position["y"], new_position["y"])
