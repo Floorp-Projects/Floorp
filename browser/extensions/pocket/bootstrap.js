9af96704df05bd431691be2e5c19e8b9b828b7a9
Mark Banner <standard8@mozilla.com>
2017-02-02 12:08:56 +0000

    Bug 1336070 - Fix no-undef issues in the pocket code. r=mixedpuppy
    MozReview-Commit-ID: F5brmleBkca

--- browser/extensions/pocket/bootstrap.js
+++ browser/extensions/pocket/bootstrap.js
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* global ADDON_ENABLE:false, ADDON_DISABLE:false, APP_SHUTDOWN: false */
+
 const {classes: Cc, interfaces: Ci, utils: Cu, manager: Cm} = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
