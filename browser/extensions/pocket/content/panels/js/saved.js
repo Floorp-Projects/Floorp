bc706a59a20578d23c02819cae3986aca9d721d4
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-03 13:27:41 +0100

    Backed out changeset c34aa5230729 (bug 1336070) for failing browser_button_state.js and browser_report_site_issue.js. r=backout on a CLOSED TREE

--- browser/extensions/pocket/content/panels/js/saved.js
+++ browser/extensions/pocket/content/panels/js/saved.js
@@ -1,6 +1,3 @@
-/* global $:false, Handlebars:false */
-/* import-globals-from messages.js */
-
 /*
 PKT_SAVED_OVERLAY is the view itself and contains all of the methods to manipute the overlay and messaging.
 It does not contain any logic for saving or communication with the extension or server.
@@ -174,7 +171,7 @@ var PKT_SAVED_OVERLAY = function(options) {
                 if ($.trim(text).length > 25 || !$.trim(text).length) {
                     if (text.length > 25) {
                         myself.showTagsError(myself.dictJSON.maxtaglength);
-                        this.changestamp = Date.now();
+                        changestamp = Date.now();
                         setTimeout(function() {
                             $(".token-input-input-token input").val(text).focus();
                         }, 10);
@@ -206,7 +203,7 @@ var PKT_SAVED_OVERLAY = function(options) {
                     }
                 }).on("keypress", "input", function(e) {
                     if (e.which == 13) {
-                        if (typeof this.changestamp == "undefined" || (Date.now() - this.changestamp > 250)) {
+                        if (typeof changestamp == "undefined" || (Date.now() - changestamp > 250)) {
                             e.preventDefault();
                             myself.wrapper.find(".pkt_ext_btn").trigger("click");
                         }
@@ -218,13 +215,13 @@ var PKT_SAVED_OVERLAY = function(options) {
             },
             onAdd() {
                 myself.checkValidTagSubmit();
-                this.changestamp = Date.now();
+                changestamp = Date.now();
                 myself.hideInactiveTags();
                 myself.checkPlaceholderStatus();
             },
             onDelete() {
                 myself.checkValidTagSubmit();
-                this.changestamp = Date.now();
+                changestamp = Date.now();
                 myself.showActiveTags();
                 myself.checkPlaceholderStatus();
             },
@@ -536,7 +533,6 @@ PKT_SAVED.prototype = {
 $(function() {
     if (!window.thePKT_SAVED) {
         var thePKT_SAVED = new PKT_SAVED();
-        /* global thePKT_SAVED */
         window.thePKT_SAVED = thePKT_SAVED;
         thePKT_SAVED.init();
     }
