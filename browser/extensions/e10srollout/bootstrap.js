b364159843a9040dd78514ba919ee405dc5ff5d8
Felipe Gomes <felipc@gmail.com>
2017-02-03 16:25:34 -0200

    Bug 1329752 - Enable e10s for 100% of eligible users on ESR. Addons eligibility is mpc=true or webextension. r=mconley a=jcristau
    MozReview-Commit-ID: Bb0gfkPKkQg

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -14,11 +14,13 @@ Cu.import("resource://gre/modules/UpdateUtils.jsm");
 const TEST_THRESHOLD = {
   "beta"    : 0.5,  // 50%
   "release" : 1.0,  // 100%
+  "esr"     : 1.0,  // 100%
 };
 
 const ADDON_ROLLOUT_POLICY = {
   "beta"    : "51alladdons", // Any WebExtension or addon except with mpc = false
   "release" : "51set1",
+  "esr"     : "esrA", // WebExtensions and Addons with mpc=true
 };
 
 const PREF_COHORT_SAMPLE       = "e10s.rollout.cohortSample";
