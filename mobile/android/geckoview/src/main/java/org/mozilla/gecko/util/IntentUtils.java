d9f19756bcf83874d008e653683faf0c7c30499f
Jan Henning <jh+bugzilla@buttercookie.de>
2017-02-05 15:35:00 +0100

    Bug 1336734 - Part 1 - Have GeckoPreferences properly support GeckoActivityStatus. r=sebastian
    Currently, GeckoPreferences always returns "false" for isGeckoActivityOpened(), which means that when we're e.g. opening a new settings screen, GeckoApplication's onActivityPause() code assumes that Firefox is being backgrounded for real, calling GeckoThread.onPause(). This is then immediately followed by a call to onActivityResume() which unpauses Gecko again.
    
    To avoid this, GeckoPreferences needs to properly implement support for GeckoActivityStatus and check the target of outgoing intents along the lines of the implementation in GeckoActivity.
    
    Since checkIfGeckoActivity() is now used outside GeckoActivity as well, we refactor it into our IntentUtils.
    
    MozReview-Commit-ID: UfPNAic5os

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/IntentUtils.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/IntentUtils.java
@@ -6,12 +6,14 @@
 
 package org.mozilla.gecko.util;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.mozglue.SafeIntent;
 
 import java.util.HashMap;
@@ -105,5 +107,25 @@ public class IntentUtils {
     public static boolean getIsInAutomationFromEnvironment(final SafeIntent intent) {
         final HashMap<String, String> envVars = IntentUtils.getEnvVarMap(intent);
         return !TextUtils.isEmpty(envVars.get(IntentUtils.ENV_VAR_IN_AUTOMATION));
+    }
+
+    /**
+     * Checks whether the target of the passed intent will result in us opening one
+     * of our own activities or not.
+     *
+     * @param intent The intent to be checked.
+     * @return True if the intent target is within our app.
+     */
+    public static boolean checkIfGeckoActivity(Intent intent) {
+        // Whenever we call our own activity, either the component and its package name is set,
+        // or else the intent is for an action where we know that we'll stay within our app.
+        // If we call an activity from another package, or an open intent (leaving android to resolve)
+        // component has a different package name or it is null.
+        ComponentName component = intent.getComponent();
+        String action = intent.getAction();
+        return (component != null &&
+                AppConstants.ANDROID_PACKAGE_NAME.equals(component.getPackageName()) ||
+                action != null &&
+                action.startsWith(AppConstants.ANDROID_PACKAGE_NAME + ".ACTION_FXA_"));
     }
 }
