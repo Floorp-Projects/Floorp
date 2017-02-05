6b7b1cf6c4bcc5618efe6526819b04f564e3e377
Zibi Braniecki <gandalf@mozilla.com>
2017-02-05 11:41:43 -0800

    Bug 1333184 - Introduce OSPreferences API. r=jfkthame
    MozReview-Commit-ID: ALvLGtBmRgn

---
+++ intl/locale/mac/OSPreferences_mac.cpp
@@ -0,0 +1,39 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "OSPreferences.h"
+#include <Carbon/Carbon.h>
+
+using namespace mozilla::intl;
+
+bool
+OSPreferences::ReadSystemLocales(nsTArray<nsCString>& aLocaleList)
+{
+  MOZ_ASSERT(aLocaleList.IsEmpty());
+
+  // Get string representation of user's current locale
+  CFLocaleRef userLocaleRef = ::CFLocaleCopyCurrent();
+  CFStringRef userLocaleStr = ::CFLocaleGetIdentifier(userLocaleRef);
+
+  AutoTArray<UniChar, 32> buffer;
+  int size = ::CFStringGetLength(userLocaleStr);
+
+  CFRange range = ::CFRangeMake(0, size);
+  ::CFStringGetCharacters(userLocaleStr, range, buffer.Elements());
+
+  // Convert the locale string to the format that Mozilla expects
+  NS_LossyConvertUTF16toASCII locale(
+      reinterpret_cast<const char16_t*>(buffer.Elements()), buffer.Length());
+
+  CFRelease(userLocaleRef);
+
+  if (CanonicalizeLanguageTag(locale)) {
+    aLocaleList.AppendElement(locale);
+    return true;
+  }
+
+  return false;
+}
