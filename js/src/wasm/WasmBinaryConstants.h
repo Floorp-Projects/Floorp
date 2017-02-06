3056c763c102c3fdf1004e50004d667d2e2794be
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-06 18:36:06 +0100

    Backed out changeset f78e406f5ef5 and b42c3703393d (bug 1331939) for landing on the wrong branch. r=backout a=backout

--- js/src/wasm/WasmBinaryConstants.h
+++ js/src/wasm/WasmBinaryConstants.h
@@ -102,7 +102,7 @@ enum class DefinitionKind
     Global                               = 0x03
 };
 
-enum class GlobalTypeImmediate
+enum class GlobalFlags
 {
     IsMutable                            = 0x1,
     AllowedMask                          = 0x1
