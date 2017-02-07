6774aad97524c5633b675ec11a7d34867acd47d8
Wes Kocher <wkocher@mozilla.com>
2017-02-07 14:46:18 -0800

    Backed out changeset 7038e10280b2 (bug 1252960) for test_safe_browsing_warning_pages.py failures a=backout
    MozReview-Commit-ID: 9Dfm6gbDFc8

--- mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
+++ mobile/android/base/java/org/mozilla/gecko/db/BrowserProvider.java
@@ -164,8 +164,8 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
 
     static {
         sTables = new Table[] {
-            // See awful shortcut assumption hack in getURLImageDataTable.
-            new URLImageDataTable()
+            // See awful shortcut assumption hack in getURLMetadataTable.
+            new URLMetadataTable()
         };
         // We will reuse this.
         HashMap<String, String> map;
@@ -370,8 +370,8 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
 
     // Convenience accessor.
     // Assumes structure of sTables!
-    private URLImageDataTable getURLImageDataTable() {
-        return (URLImageDataTable) sTables[0];
+    private URLMetadataTable getURLMetadataTable() {
+        return (URLMetadataTable) sTables[0];
     }
 
     private static boolean hasFaviconsInProjection(String[] projection) {
@@ -2204,7 +2204,7 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
 
         return deleteFavicons(uri, faviconSelection, null) +
                deleteThumbnails(uri, thumbnailSelection, null) +
-               getURLImageDataTable().deleteUnused(getWritableDatabase(uri));
+               getURLMetadataTable().deleteUnused(getWritableDatabase(uri));
     }
 
     @Override
