3cca3542288b0129ca74fa3a272520d435b303fe
sotaro <sotaro.ikeda.g@gmail.com>
2017-02-07 17:07:00 +0900

    Bug 1336024 - Split Image selection logic from ImageHost r=nical

--- gfx/layers/composite/ImageHost.cpp
+++ gfx/layers/composite/ImageHost.cpp
@@ -16,8 +16,6 @@
 #include "nsPrintfCString.h"            // for nsPrintfCString
 #include "nsString.h"                   // for nsAutoCString
 
-#define BIAS_TIME_MS 1.0
-
 namespace mozilla {
 
 using namespace gfx;
@@ -28,9 +26,7 @@ class ISurfaceAllocator;
 
 ImageHost::ImageHost(const TextureInfo& aTextureInfo)
   : CompositableHost(aTextureInfo)
-  , mLastFrameID(-1)
-  , mLastProducerID(-1)
-  , mBias(BIAS_NONE)
+  , ImageComposite()
   , mLocked(false)
 {}
 
@@ -152,97 +148,14 @@ ImageHost::RemoveTextureHost(TextureHost* aTexture)
   }
 }
 
-static TimeStamp
-GetBiasedTime(const TimeStamp& aInput, ImageHost::Bias aBias)
+TimeStamp
+ImageHost::GetCompositionTime() const
 {
-  switch (aBias) {
-  case ImageHost::BIAS_NEGATIVE:
-    return aInput - TimeDuration::FromMilliseconds(BIAS_TIME_MS);
-  case ImageHost::BIAS_POSITIVE:
-    return aInput + TimeDuration::FromMilliseconds(BIAS_TIME_MS);
-  default:
-    return aInput;
+  TimeStamp time;
+  if (GetCompositor()) {
+    time = GetCompositor()->GetCompositionTime();
   }
-}
-
-static ImageHost::Bias
-UpdateBias(const TimeStamp& aCompositionTime,
-           const TimeStamp& aCompositedImageTime,
-           const TimeStamp& aNextImageTime, // may be null
-           ImageHost::Bias aBias)
-{
-  if (aCompositedImageTime.IsNull()) {
-    return ImageHost::BIAS_NONE;
-  }
-  TimeDuration threshold = TimeDuration::FromMilliseconds(1.0);
-  if (aCompositionTime - aCompositedImageTime < threshold &&
-      aCompositionTime - aCompositedImageTime > -threshold) {
-    // The chosen frame's time is very close to the composition time (probably
-    // just before the current composition time, but due to previously set
-    // negative bias, it could be just after the current composition time too).
-    // If the inter-frame time is almost exactly equal to (a multiple of)
-    // the inter-composition time, then we're in a dangerous situation because
-    // jitter might cause frames to fall one side or the other of the
-    // composition times, causing many frames to be skipped or duplicated.
-    // Try to prevent that by adding a negative bias to the frame times during
-    // the next composite; that should ensure the next frame's time is treated
-    // as falling just before a composite time.
-    return ImageHost::BIAS_NEGATIVE;
-  }
-  if (!aNextImageTime.IsNull() &&
-      aNextImageTime - aCompositionTime < threshold &&
-      aNextImageTime - aCompositionTime > -threshold) {
-    // The next frame's time is very close to our composition time (probably
-    // just after the current composition time, but due to previously set
-    // positive bias, it could be just before the current composition time too).
-    // We're in a dangerous situation because jitter might cause frames to
-    // fall one side or the other of the composition times, causing many frames
-    // to be skipped or duplicated.
-    // Try to prevent that by adding a negative bias to the frame times during
-    // the next composite; that should ensure the next frame's time is treated
-    // as falling just before a composite time.
-    return ImageHost::BIAS_POSITIVE;
-  }
-  return ImageHost::BIAS_NONE;
-}
-
-int ImageHost::ChooseImageIndex() const
-{
-  if (!GetCompositor() || mImages.IsEmpty()) {
-    return -1;
-  }
-  TimeStamp now = GetCompositor()->GetCompositionTime();
-
-  if (now.IsNull()) {
-    // Not in a composition, so just return the last image we composited
-    // (if it's one of the current images).
-    for (uint32_t i = 0; i < mImages.Length(); ++i) {
-      if (mImages[i].mFrameID == mLastFrameID &&
-          mImages[i].mProducerID == mLastProducerID) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  uint32_t result = 0;
-  while (result + 1 < mImages.Length() &&
-      GetBiasedTime(mImages[result + 1].mTimeStamp, mBias) <= now) {
-    ++result;
-  }
-  return result;
-}
-
-const ImageHost::TimedImage* ImageHost::ChooseImage() const
-{
-  int index = ChooseImageIndex();
-  return index >= 0 ? &mImages[index] : nullptr;
-}
-
-ImageHost::TimedImage* ImageHost::ChooseImage()
-{
-  int index = ChooseImageIndex();
-  return index >= 0 ? &mImages[index] : nullptr;
+  return time;
 }
 
 TextureHost*
@@ -421,31 +334,6 @@ ImageHost::Composite(LayerComposite* aLayer,
   // since callers of ChooseImage(Index) assume the same image will be chosen
   // during a given composition. This must happen after autoLock's
   // destructor!
-  mBias = UpdateBias(
-      GetCompositor()->GetCompositionTime(), mImages[imageIndex].mTimeStamp,
-      uint32_t(imageIndex + 1) < mImages.Length() ?
-          mImages[imageIndex + 1].mTimeStamp : TimeStamp(),
-      mBias);
-}
-
-void
-ImageHost::BindTextureSource()
-{
-  int imageIndex = ChooseImageIndex();
-  if (imageIndex < 0) {
-    return;
-  }
-
-  if (uint32_t(imageIndex) + 1 < mImages.Length()) {
-    GetCompositor()->CompositeUntil(mImages[imageIndex + 1].mTimeStamp + TimeDuration::FromMilliseconds(BIAS_TIME_MS));
-  }
-
-  TimedImage* img = &mImages[imageIndex];
-  img->mTextureHost->SetCompositor(GetCompositor());
-  SetCurrentTextureHost(img->mTextureHost);
-
-  // XXX Add TextureSource binding
-
   mBias = UpdateBias(
       GetCompositor()->GetCompositionTime(), mImages[imageIndex].mTimeStamp,
       uint32_t(imageIndex + 1) < mImages.Length() ?
