ae2f47edba48ed818e3ec779faea4706bf5893d2
Masayuki Nakano <masayuki@d-toybox.com>
2017-02-07 22:36:16 +0900

    Bug 1336011 part.2 Create an alias of the type of mEditorObservers r=smaug
    MozReview-Commit-ID: 7LWl5F4C0Ge

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -1876,7 +1876,7 @@ void
 EditorBase::NotifyEditorObservers(NotificationForEditorObservers aNotification)
 {
   // Copy the observers since EditAction()s can modify mEditorObservers.
-  nsTArray<mozilla::OwningNonNull<nsIEditorObserver>> observers(mEditorObservers);
+  AutoEditorObserverArray observers(mEditorObservers);
   switch (aNotification) {
     case eNotifyEditorObserversOfEnd:
       mIsInEditAction = false;
