40b2b1f1dba8040c7857de0d88a84404a76ada0b
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-02-07 08:55:28 +0100

    Bug 1325707: P1. Check returned value. r=gerald
    This is really just for cleanliness perspective, as it can never happen, we always check that demuxing a packet succeeded first.
    
    MozReview-Commit-ID: FQ1yz16m9Ix

--- dom/media/ogg/OggCodecState.cpp
+++ dom/media/ogg/OggCodecState.cpp
@@ -1305,6 +1305,9 @@ OpusState::PacketOutAsMediaRawData()
   }
 
   RefPtr<MediaRawData> data = OggCodecState::PacketOutAsMediaRawData();
+  if (!data) {
+    return nullptr;
+  }
 
   if (data->mEOS && mPrevPacketGranulepos != -1) {
     // If this is the last packet, perform end trimming.
