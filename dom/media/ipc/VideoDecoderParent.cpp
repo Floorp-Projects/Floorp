ec5935390254d0b5fddde8567bd6adc3822270a0
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-02-07 09:03:36 +0100

    Bug 1325707: P2. Handle OOM conditions when creating MediaRawData object. r=gerald
    MozReview-Commit-ID: HtkhrT36Kf4
    
    --HG--
    extra : rebase_source : febe0c4114d8d888b152db3f7b5331c0fb32f442

--- dom/media/ipc/VideoDecoderParent.cpp
+++ dom/media/ipc/VideoDecoderParent.cpp
@@ -124,7 +124,13 @@ VideoDecoderParent::RecvInput(const MediaRawDataIPDL& aData)
   MOZ_ASSERT(OnManagerThread());
   // XXX: This copies the data into a buffer owned by the MediaRawData. Ideally we'd just take ownership
   // of the shmem.
-  RefPtr<MediaRawData> data = new MediaRawData(aData.buffer().get<uint8_t>(), aData.buffer().Size<uint8_t>());
+  RefPtr<MediaRawData> data = new MediaRawData(aData.buffer().get<uint8_t>(),
+                                               aData.buffer().Size<uint8_t>());
+  if (!data->Data()) {
+    // OOM
+    Error(NS_ERROR_OUT_OF_MEMORY);
+    return IPC_OK();
+  }
   data->mOffset = aData.base().offset();
   data->mTime = aData.base().time();
   data->mTimecode = aData.base().timecode();
