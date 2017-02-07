a76c2d28fe7e2de824e7b6b5ff01526def25313c
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-07 14:16:26 +1100

    Bug 1337189 (part 6) - Move sRegisteredThreads{,Mutex} out of Sampler. r=mstange.
    They don't particularly belong there, and Sampler will be going away eventually
    anyway.
    
    --HG--
    extra : rebase_source : 4faab111925848215f7e8db8244b33ee46c5396c

--- tools/profiler/core/platform-win32.cc
+++ tools/profiler/core/platform-win32.cc
@@ -165,11 +165,11 @@ class SamplerThread
       gSampler->DeleteExpiredMarkers();
 
       if (!gSampler->IsPaused()) {
-        mozilla::StaticMutexAutoLock lock(Sampler::sRegisteredThreadsMutex);
+        mozilla::StaticMutexAutoLock lock(sRegisteredThreadsMutex);
 
         bool isFirstProfiledThread = true;
-        for (uint32_t i = 0; i < Sampler::sRegisteredThreads->size(); i++) {
-          ThreadInfo* info = (*Sampler::sRegisteredThreads)[i];
+        for (uint32_t i = 0; i < sRegisteredThreads->size(); i++) {
+          ThreadInfo* info = (*sRegisteredThreads)[i];
 
           // This will be null if we're not interested in profiling this thread.
           if (!info->hasProfile() || info->IsPendingDelete()) {
