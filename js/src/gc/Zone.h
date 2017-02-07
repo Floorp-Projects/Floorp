adaab624943f40f5e1a886efd665cd32872be68d
Brian Hackett <bhackett1024@gmail.com>
2017-02-07 10:15:24 -0700

    Bug 1337324 - Fix GCManagedDeletePolicy so that it again tolerates being called off thread, r=jonco.

--- js/src/gc/Zone.h
+++ js/src/gc/Zone.h
@@ -810,11 +810,11 @@ struct GCManagedDeletePolicy
 {
     void operator()(const T* ptr) {
         if (ptr) {
-            JSContext* cx = TlsContext.get();
-            if (cx->runtime()->zoneGroupFromMainThread()->nursery().isEnabled()) {
+            JSRuntime* rt = TlsContext.get()->runtime();
+            if (CurrentThreadCanAccessRuntime(rt) && rt->zoneGroupFromMainThread()->nursery().isEnabled()) {
                 // The object may contain nursery pointers and must only be
                 // destroyed after a minor GC.
-                cx->runtime()->zoneGroupFromMainThread()->callAfterMinorGC(deletePtr, const_cast<T*>(ptr));
+                rt->zoneGroupFromMainThread()->callAfterMinorGC(deletePtr, const_cast<T*>(ptr));
             } else {
                 // The object cannot contain nursery pointers so can be
                 // destroyed immediately.
