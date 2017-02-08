8c4dc86be298bdd0bf1407920886c6ba641fd8b7
Benjamin Smedberg <benjamin@smedbergs.us>
2017-02-08 14:27:49 -0500

    Bug 1338172 part B - remove the ancient carbon detection on Mac which is no longer used, r=jimm
    MozReview-Commit-ID: 9UEAfPHg7fP

--- dom/plugins/base/nsNPAPIPluginInstance.h
+++ dom/plugins/base/nsNPAPIPluginInstance.h
@@ -302,10 +302,6 @@ public:
   NPError FinalizeAsyncSurface(NPAsyncSurface *surface);
   void SetCurrentAsyncSurface(NPAsyncSurface *surface, NPRect *changed);
 
-  // Called when the instance fails to instantiate beceause the Carbon
-  // event model is not supported.
-  void CarbonNPAPIFailure();
-
   // Returns the contents scale factor of the screen the plugin is drawn on.
   double GetContentsScaleFactor();
 
