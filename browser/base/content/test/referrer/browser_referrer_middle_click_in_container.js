a1d575c2b88e94b6839dba8459dfc2c21e8e0b2e
Gabor Krizsanits <gkrizsanits@mozilla.com>
2017-02-08 11:22:33 +0100

    Bug 1337354 - Fix races in referrer tests. r=mrbkap, a=test-only

--- browser/base/content/test/referrer/browser_referrer_middle_click_in_container.js
+++ browser/base/content/test/referrer/browser_referrer_middle_click_in_container.js
@@ -5,10 +5,11 @@ function startMiddleClickTestCase(aTestNumber) {
   info("browser_referrer_middle_click: " +
        getReferrerTestDescription(aTestNumber));
   someTabLoaded(gTestWindow).then(function(aNewTab) {
-    gTestWindow.gBrowser.selectedTab = aNewTab;
-    checkReferrerAndStartNextTest(aTestNumber, null, aNewTab,
-                                  startMiddleClickTestCase,
-                                  { userContextId: 3 });
+    BrowserTestUtils.switchTab(gTestWindow.gBrowser, aNewTab).then(() => {
+      checkReferrerAndStartNextTest(aTestNumber, null, aNewTab,
+                                    startMiddleClickTestCase,
+                                    { userContextId: 3 });
+    });
   });
 
   clickTheLink(gTestWindow, "testlink", {button: 1});
