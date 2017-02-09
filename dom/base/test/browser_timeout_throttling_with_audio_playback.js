ee6ec67f89a5a86b94de3b619e45688700d339d2
Ehsan Akhgari <ehsan@mozilla.com>
2017-02-08 20:03:22 -0500

    Bug 1336484 - Don't throttle timeouts in background tabs that are playing audio; r=baku a=gchang
    In websites such as Facebook Live, timeout chains are used to drive the
    playback of a video or something similar in JavaScript.  Throttling the
    minimum timeout values a tab playing a video from such websites in the
    background could make the timeout based scheduling of video playback to
    not work correctly, and cause audio buffer under-runs that are audible.
    
    In order to address this, other major browsers don't throttle timeouts
    in tabs that are playing audio.  This brings us to parity to other
    browsers (even though we already do this for websites that use Web Audio
    since we've had similar bug reports using the Web Audio API.)
    
    The current audio agent setup that drives the tab audio notification
    icons is currently tracking whether a Window is playing audio.  We use
    this setup to decide whether to throttle timeouts when a window goes
    into background.

---
+++ dom/base/test/browser_timeout_throttling_with_audio_playback.js
@@ -0,0 +1,61 @@
+const kBaseURI = "http://mochi.test:8888/browser/dom/base/test/empty.html";
+const kPluginJS = "chrome://mochitests/content/browser/dom/base/test/plugin.js";
+var testURLs = [
+  "http://mochi.test:8888/browser/dom/base/test/file_audioLoop.html",
+  "http://mochi.test:8888/browser/dom/base/test/file_audioLoopInIframe.html",
+  "http://mochi.test:8888/browser/dom/base/test/file_pluginAudio.html",
+  "http://mochi.test:8888/browser/dom/base/test/file_webaudioLoop.html",
+];
+
+// We want to ensure that while audio is being played back, a background tab is
+// treated the same as a foreground tab as far as timeout throttling is concerned.
+// So we use a 10ms minimum timeout value for foreground tabs and a 100,000 second
+// minimum timeout value for background tabs.  This means that in case the test
+// fails, it will time out in practice, but just for sanity the test condition
+// ensures that the observed timeout delay falls in this range.
+const kMinTimeoutForeground = 10;
+const kMinTimeoutBackground = 100 * 1000 * 1000;
+
+Services.scriptloader.loadSubScript(kPluginJS, this);
+
+function* runTest(url) {
+  let currentTab = gBrowser.selectedTab;
+  let newTab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, kBaseURI);
+  let newBrowser = gBrowser.getBrowserForTab(newTab);
+
+  // Wait for the UI to indicate that audio is being played back.
+  let promise = BrowserTestUtils.waitForAttribute("soundplaying", newTab, "true");
+  newBrowser.loadURI(url);
+  yield promise;
+
+  // Put the tab in the background.
+  yield BrowserTestUtils.switchTab(gBrowser, currentTab);
+
+  let timeout = yield ContentTask.spawn(newBrowser, {}, function() {
+    return new Promise(resolve => {
+      let before = new Date();
+      content.window.setTimeout(function() {
+        let after = new Date();
+        resolve(after - before);
+      }, 0);
+    });
+  });
+  ok(timeout >= kMinTimeoutForeground &&
+     timeout <= kMinTimeoutBackground, `Got the correct timeout (${timeout})`);
+
+  // All done.
+  yield BrowserTestUtils.removeTab(newTab);
+}
+
+add_task(function* setup() {
+  yield SpecialPowers.pushPrefEnv({"set": [
+    ["dom.min_timeout_value", kMinTimeoutForeground],
+    ["dom.min_background_timeout_value", kMinTimeoutBackground],
+  ]});
+});
+
+add_task(function* test() {
+  for (var url of testURLs) {
+    yield runTest(url);
+  }
+});
