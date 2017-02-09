606e52ba0f6ad04097a936df7a1236fdbf07c9b1
Boris Chiou <boris.chiou@gmail.com>
2017-02-09 11:33:17 +0800

    Bug 1337313 - Part 2: Add AnimationValue::SerializeSpecifiedValue. r=hiro
    Move the implementation of uncomputing to string into AnimationValue,
    and add one more FFI for RawServoAnimationValue.
    
    MozReview-Commit-ID: JFnXnGO3mlp
    
    --HG--
    extra : rebase_source : 83c0e084ef4202a5c98c668d68bfe9066196d735

--- layout/style/StyleAnimationValue.h
+++ layout/style/StyleAnimationValue.h
@@ -589,27 +589,44 @@ struct AnimationValue
   StyleAnimationValue mGecko;
   RefPtr<RawServoAnimationValue> mServo;
 
-  bool operator==(const AnimationValue& aOther) const {
+  bool operator==(const AnimationValue& aOther) const
+  {
     // FIXME: Bug 1337229: add a deep == impl for RawServoAnimationValue.
     return mGecko == aOther.mGecko && mServo == aOther.mServo;
   }
 
   bool IsNull() const { return mGecko.IsNull() && !mServo; }
 
-  float GetOpacity() const {
+  float GetOpacity() const
+  {
     return mServo ? Servo_AnimationValues_GetOpacity(mServo)
                   : mGecko.GetFloatValue();
   }
 
   // Returns the scale for mGecko or mServo, which are calculated with
   // reference to aFrame.
-  gfxSize GetScaleValue(const nsIFrame* aFrame) const {
+  gfxSize GetScaleValue(const nsIFrame* aFrame) const
+  {
     if (mServo) {
       RefPtr<nsCSSValueSharedList> list;
       Servo_AnimationValues_GetTransform(mServo, &list);
       return nsStyleTransformMatrix::GetScaleValue(list, aFrame);
     }
     return mGecko.GetScaleValue(aFrame);
+  }
+
+  // Uncompute this AnimationValue and then serialize it.
+  void SerializeSpecifiedValue(nsCSSPropertyID aProperty,
+                               nsAString& aString) const
+  {
+    if (mServo) {
+      Servo_AnimationValue_Serialize(mServo, aProperty, &aString);
+      return;
+    }
+
+    DebugOnly<bool> uncomputeResult =
+      StyleAnimationValue::UncomputeValue(aProperty, mGecko, aString);
+    MOZ_ASSERT(uncomputeResult, "failed to uncompute StyleAnimationValue");
   }
 };
 
