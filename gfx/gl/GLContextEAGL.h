2effb9a24d11c3c1fbb04d1f79f41d7572bb285a
Jeff Gilbert <jgilbert@mozilla.com>
2017-02-09 20:31:36 -0800

    Bug 1339256 - Detect robust_buffer_access_behavior. - r=daoshengmu
    MozReview-Commit-ID: 4w5D9bOQbY8

--- gfx/gl/GLContextEAGL.h
+++ gfx/gl/GLContextEAGL.h
@@ -51,8 +51,6 @@ public:
 
     virtual bool IsDoubleBuffered() const override;
 
-    virtual bool SupportsRobustness() const override { return false; }
-
     virtual bool SwapBuffers() override;
 
     virtual void GetWSIInfo(nsCString* const out) const override;
