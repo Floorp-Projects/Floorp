e3eb9722f7d4b392634fd4cb920d75b02999915d
Mantaroh Yoshinaga <mantaroh@gmail.com>
2017-02-10 12:32:44 +0900

    Bug 1302648 part 5 - Queue animationcancel when animation status is idle. r=birtles
    MozReview-Commit-ID: DRjWboQwR0A

--- layout/style/nsTransitionManager.h
+++ layout/style/nsTransitionManager.h
@@ -233,9 +233,6 @@ protected:
 
 
   enum class TransitionPhase;
-  // Return the TransitionPhase to use when the transition doesn't have a target
-  // effect.
-  TransitionPhase GetTransitionPhaseWithoutEffect() const;
 
   // The (pseudo-)element whose computed transition-property refers to this
   // transition (if any).
