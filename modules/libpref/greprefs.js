bb1bf30ace7b677d55f1d5db382636a1ea3db595
Iris Hsiao <ihsiao@mozilla.com>
2017-02-10 14:00:02 +0800

    Backed out changeset e887fb5d5d06 (bug 1337748) for eslint failure

--- modules/libpref/greprefs.js
+++ modules/libpref/greprefs.js
@@ -1,4 +1,4 @@
-#include ../../security/manager/ssl/security-prefs.js
+#include ../../netwerk/base/security-prefs.js
 #include init/all.js
 #ifdef MOZ_DATA_REPORTING
 #include ../../toolkit/components/telemetry/datareporting-prefs.js
