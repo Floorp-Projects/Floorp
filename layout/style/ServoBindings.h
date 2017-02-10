a1953373cd4295c8ea1e63592f245f2e86fc6ba2
Manish Goregaokar <manishearth@gmail.com>
2017-02-09 17:43:52 -0800

    Bug 1338388 - Part 5: stylo: Add mako template for URLOrNone, use for marker-* properties; r=heycam
    MozReview-Commit-ID: 4QKKzJ1DVYP
    
    --HG--
    extra : rebase_source : 68947510d2a4fb91d70c9e0d9d261d980cb2d313

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -233,10 +233,6 @@ void Gecko_SetCursorImage(nsCursorImage* cursor,
 void Gecko_CopyCursorArrayFrom(nsStyleUserInterface* dest,
                                const nsStyleUserInterface* src);
 
-// Display style.
-void Gecko_SetMozBinding(nsStyleDisplay* style_struct, ServoBundledURI bundled_uri);
-void Gecko_CopyMozBindingFrom(nsStyleDisplay* des, const nsStyleDisplay* src);
-
 // Dirtiness tracking.
 uint32_t Gecko_GetNodeFlags(RawGeckoNodeBorrowed node);
 void Gecko_SetNodeFlags(RawGeckoNodeBorrowed node, uint32_t flags);
@@ -302,6 +298,9 @@ void Gecko_nsStyleFilter_SetURLValue(nsStyleFilter* effects, ServoBundledURI uri
 void Gecko_nsStyleSVGPaint_CopyFrom(nsStyleSVGPaint* dest, const nsStyleSVGPaint* src);
 void Gecko_nsStyleSVGPaint_SetURLValue(nsStyleSVGPaint* paint, ServoBundledURI uri);
 void Gecko_nsStyleSVGPaint_Reset(nsStyleSVGPaint* paint);
+
+mozilla::css::URLValue* Gecko_NewURLValue(ServoBundledURI uri);
+NS_DECL_THREADSAFE_FFI_REFCOUNTING(mozilla::css::URLValue, CSSURLValue);
 
 void Gecko_FillAllBackgroundLists(nsStyleImageLayers* layers, uint32_t max_len);
 void Gecko_FillAllMaskLists(nsStyleImageLayers* layers, uint32_t max_len);
