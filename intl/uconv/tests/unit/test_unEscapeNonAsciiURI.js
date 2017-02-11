515a5dcc78a1025ce536b42c530682469b8b9136
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2017-02-11 20:40:58 +0900

    Bug 1338758 - Handle success codes from nsIUnicodeDecoder in nsTextToSubURI::UnEscapeNonAsciiURI. r=m_kato a=jcristau
    MozReview-Commit-ID: 43jAOFPYMeT
    
    --HG--
    rename : intl/uconv/tests/unit/test_bug699673.js => intl/uconv/tests/unit/test_unEscapeNonAsciiURI.js
    extra : source : 9aeb203f8d6f4dede99451a5228547eddc47185a

---
+++ intl/uconv/tests/unit/test_unEscapeNonAsciiURI.js
@@ -0,0 +1,46 @@
+// Tests for nsITextToSubURI.unEscapeNonAsciiURI
+function run_test() {
+  const textToSubURI = Components.classes["@mozilla.org/intl/texttosuburi;1"].getService(Components.interfaces.nsITextToSubURI);
+
+  // Tests whether nsTextToSubURI does UTF-16 unescaping (it shouldn't)
+  const testURI = "data:text/html,%FE%FF";
+  do_check_eq(textToSubURI.unEscapeNonAsciiURI("UTF-16", testURI), testURI);
+
+  // Tests whether incomplete multibyte sequences throw.
+  const tests = [{
+    input: "http://example.com/?p=%E9",
+    throws: Components.results.NS_ERROR_ILLEGAL_INPUT,
+  }, {
+    input: "http://example.com/?p=%E9%80",
+    throws: Components.results.NS_ERROR_ILLEGAL_INPUT,
+  }, {
+    input: "http://example.com/?p=%E9%80%80",
+    expected: "http://example.com/?p=\u9000",
+  }, {
+    input: "http://example.com/?p=%E9e",
+    throws: Components.results.NS_ERROR_ILLEGAL_INPUT,
+  }, {
+    input: "http://example.com/?p=%E9%E9",
+    throws: Components.results.NS_ERROR_ILLEGAL_INPUT,
+  }, {
+    input: "http://example.com/?name=M%FCller/",
+    throws: Components.results.NS_ERROR_ILLEGAL_INPUT,
+  }, {
+    input: "http://example.com/?name=M%C3%BCller/",
+    expected: "http://example.com/?name=Müller/",
+  }];
+
+  for (const t of tests) {
+    if (t.throws !== undefined) {
+      let thrown = undefined;
+      try {
+        textToSubURI.unEscapeNonAsciiURI("UTF-8", t.input);
+      } catch (e) {
+        thrown = e.result;
+      }
+      do_check_eq(thrown, t.throws);
+    } else {
+      do_check_eq(textToSubURI.unEscapeNonAsciiURI("UTF-8", t.input), t.expected);
+    }
+  }
+}
