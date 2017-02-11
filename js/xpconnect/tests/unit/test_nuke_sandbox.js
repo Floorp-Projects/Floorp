842f58444671265c27117f1e6e1dc595bc4dcd10
Kris Maglione <maglione.k@gmail.com>
2017-02-11 14:26:05 -0800

    Bug 1322273: Return DeadObjectProxy when wrapping for nuked compartment. r=bholley
    MozReview-Commit-ID: V07P0eZvKO

--- js/xpconnect/tests/unit/test_nuke_sandbox.js
+++ js/xpconnect/tests/unit/test_nuke_sandbox.js
@@ -4,20 +4,46 @@
 
 /* See https://bugzilla.mozilla.org/show_bug.cgi?id=769273 */
 
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+
+const global = this;
+
 function run_test()
 {
-  var sb = Components.utils.Sandbox("http://www.blah.com");
+  var ifacePointer = Cc["@mozilla.org/supports-interface-pointer;1"]
+      .createInstance(Ci.nsISupportsInterfacePointer);
+
+  var sb = Cu.Sandbox(global);
   sb.prop = "prop"
-  var refToObjFromSb = Components.utils.evalInSandbox("var a = {prop2:'prop2'}; a", sb); 
-  Components.utils.nukeSandbox(sb);
-  do_check_true(Components.utils.isDeadWrapper(sb), "sb should be dead");
+  sb.ifacePointer = ifacePointer
+
+  var refToObjFromSb = Cu.evalInSandbox(`
+    Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+    ifacePointer.data = {
+      QueryInterface: XPCOMUtils.generateQI([]),
+      wrappedJSObject: {foo: "bar"},
+    };
+
+    var a = {prop2:'prop2'};
+    a
+  `, sb);
+
+  equal(ifacePointer.data.wrappedJSObject.foo, "bar",
+        "Got expected wrapper into sandbox")
+
+  Cu.nukeSandbox(sb);
+  ok(Cu.isDeadWrapper(sb), "sb should be dead");
+  ok(Cu.isDeadWrapper(ifacePointer.data.wrappedJSObject),
+     "Wrapper retrieved via XPConnect should be dead");
+
   try{
     sb.prop;
     do_check_true(false);
   } catch (e) {
     do_check_true(e.toString().indexOf("can't access dead object") > -1);
   }
-  
+
   Components.utils.isDeadWrapper(refToObjFromSb, "ref to object from sb should be dead");
   try{
     refToObjFromSb.prop2;
@@ -25,5 +51,4 @@ function run_test()
   } catch (e) {
     do_check_true(e.toString().indexOf("can't access dead object") > -1);
   }
-  
 }
