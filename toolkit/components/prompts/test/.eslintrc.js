788b2cbf99d14a0b293db381b6d446da33424e90
Florian Quèze <florian@queze.net>
2017-02-13 13:01:32 +0100

    Bug 1338585 - Add an eslint rule to require using .ownerGlobal instead of .ownerDocument.defaultView, r=jaws.

--- toolkit/components/prompts/test/.eslintrc.js
+++ toolkit/components/prompts/test/.eslintrc.js
@@ -3,5 +3,9 @@
 module.exports = {
   "extends": [
     "../../../../testing/mochitest/mochitest.eslintrc.js"
-  ]
+  ],
+  "rules": {
+    // ownerGlobal doesn't exist in content privileged windows.
+    "mozilla/use-ownerGlobal": "off",
+  }
 };
