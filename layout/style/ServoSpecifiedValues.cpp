8ce0fb7f1cda87d41e82eabb72b87536ddc44a8b
Manish Goregaokar <manishearth@gmail.com>
2017-02-12 16:02:29 -0800

    Bug 1338936 - Part 1: stylo: Add stubbed-out ServoSpecifiedValues interface, use it for pres attr mapping; r=bz,emilio
    MozReview-Commit-ID: 6wg32flypt7

---
+++ layout/style/ServoSpecifiedValues.cpp
@@ -0,0 +1,26 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/ServoSpecifiedValues.h"
+
+namespace {
+
+#define STYLE_STRUCT(name, checkdata_cb) | NS_STYLE_INHERIT_BIT(name)
+const uint64_t ALL_SIDS = 0
+#include "nsStyleStructList.h"
+;
+#undef STYLE_STRUCT
+
+} // anonymous namespace
+
+
+ServoSpecifiedValues::ServoSpecifiedValues(nsPresContext* aContext,
+                                           RawServoDeclarationBlock* aDecl)
+
+  : GenericSpecifiedValues(StyleBackendType::Servo, aContext, ALL_SIDS)
+  ,  mDecl(aDecl)
+{
+
+}
