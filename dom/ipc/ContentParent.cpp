64618b99497af4cd146b39f4319c9c5169991f8b
Dave Townsend <dtownsend@oxymoronical.com>
2017-02-13 15:06:00 -0500

    Bug 1326245 - Tell the child processes when we're in safe mode. r=smaug, a=jcristau
    --HG--
    extra : source : a9c518127961735cd8ea777537b07c6b79335152

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2079,6 +2079,11 @@ ContentParent::LaunchSubprocess(ProcessPriority aInitialPriority /* = PROCESS_PR
   PROFILER_LABEL_FUNC(js::ProfileEntry::Category::OTHER);
 
   std::vector<std::string> extraArgs;
+
+  if (gSafeMode) {
+    extraArgs.push_back("-safeMode");
+  }
+
   if (!mSubprocess->LaunchAndWaitForProcessHandle(extraArgs)) {
     MarkAsDead();
     return false;
