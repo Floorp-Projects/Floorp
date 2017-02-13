54da76be6ba2f8c29cbecba4768c643845358929
Bill McCloskey <billm@mozilla.com>
2017-02-13 15:45:23 -0800

    Bug 1339289 - Give names to a lot of common timers (r=ehsan)
    MozReview-Commit-ID: IMsv5bkyjBL

--- startupcache/StartupCache.cpp
+++ startupcache/StartupCache.cpp
@@ -589,8 +589,9 @@ StartupCache::ResetStartupWriteTimer()
     rv = mTimer->Cancel();
   NS_ENSURE_SUCCESS(rv, rv);
   // Wait for 10 seconds, then write out the cache.
-  mTimer->InitWithFuncCallback(StartupCache::WriteTimeout, this, 60000,
-                               nsITimer::TYPE_ONE_SHOT);
+  mTimer->InitWithNamedFuncCallback(StartupCache::WriteTimeout, this, 60000,
+                                    nsITimer::TYPE_ONE_SHOT,
+                                    "StartupCache::WriteTimeout");
   return NS_OK;
 }
 
