9b5f982476d5e9a9994fa4e5beac50fa6080c08a
Chris Pearce <cpearce@mozilla.com>
2017-02-14 11:29:32 +1300

    Bug 1338924 - Pass duration of sample being decrypted to CDM. r=gerald
    MozReview-Commit-ID: 3UujuYPpfJi

--- dom/media/gmp/GMPDecryptorProxy.h
+++ dom/media/gmp/GMPDecryptorProxy.h
@@ -55,7 +55,8 @@ public:
 
   virtual void Decrypt(uint32_t aId,
                        const mozilla::CryptoSample& aCrypto,
-                       const nsTArray<uint8_t>& aBuffer) = 0;
+                       const nsTArray<uint8_t>& aBuffer,
+                       uint64_t aDurationUsecs) = 0;
 
   virtual void Close() = 0;
 };
