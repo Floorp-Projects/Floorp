a19e007056b83aaa9a9ad14998585d1ad81a5fd1
Florian Quèze <florian@queze.net>
2017-02-13 13:01:32 +0100

    Bug 1338585 - Add an eslint rule to require using .ownerGlobal instead of .ownerDocument.defaultView, r=jaws.

---
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/rules/use-ownerGlobal.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview Require .ownerGlobal instead of .ownerDocument.defaultView.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+
+// -----------------------------------------------------------------------------
+// Rule Definition
+// -----------------------------------------------------------------------------
+
+var helpers = require("../helpers");
+
+module.exports = function(context) {
+
+  // ---------------------------------------------------------------------------
+  // Public
+  //  --------------------------------------------------------------------------
+
+  return {
+    "MemberExpression": function(node) {
+      if (node.property.type != "Identifier" ||
+          node.property.name != "defaultView" ||
+          node.object.type != "MemberExpression" ||
+          node.object.property.type != "Identifier" ||
+          node.object.property.name != "ownerDocument") {
+        return;
+      }
+
+      context.report(node, "use .ownerGlobal of .ownerDocument.defaultView");
+    }
+  };
+};
