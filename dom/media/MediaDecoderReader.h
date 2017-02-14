c7cfa0cd9d255077e2d650aa8a9e4c04bc5568d4
Gerald Squelart <gsquelart@squelart.com>
2017-02-15 09:23:15 +1100

    Bug 1326372 - P2. Backed out changeset 19c468c32d03 bug 1319995-P1 - r=jya a=abillings
    Original commit description:
    Run demuxing operations on its own task queue.
    
    We runs all demuxing operations on a dedicated task queue.
    MediaDataDemuxer's members using a synchronous API are handled via thread-safe copy that are updated along the operations.
    
    The buffered range calculation is now handled separately and the entire operation is made asynchronous.
    
    --HG--
    extra : rebase_source : 58501acce96c612ff57157a2d60b892b003ab38c

--- dom/media/MediaDecoderReader.h
+++ dom/media/MediaDecoderReader.h
@@ -195,10 +195,11 @@ public:
 
   // Called once new data has been cached by the MediaResource.
   // mBuffered should be recalculated and updated accordingly.
-  virtual void NotifyDataArrived()
+  void NotifyDataArrived()
   {
     MOZ_ASSERT(OnTaskQueue());
     NS_ENSURE_TRUE_VOID(!mShutdown);
+    NotifyDataArrivedInternal();
     UpdateBuffered();
   }
 
@@ -253,6 +254,24 @@ protected:
 
   // Recomputes mBuffered.
   virtual void UpdateBuffered();
+
+  // Populates aBuffered with the time ranges which are buffered. This may only
+  // be called on the decode task queue, and should only be used internally by
+  // UpdateBuffered - mBuffered (or mirrors of it) should be used for everything
+  // else.
+  //
+  // This base implementation in MediaDecoderReader estimates the time ranges
+  // buffered by interpolating the cached byte ranges with the duration
+  // of the media. Reader subclasses should override this method if they
+  // can quickly calculate the buffered ranges more accurately.
+  //
+  // The primary advantage of this implementation in the reader base class
+  // is that it's a fast approximation, which does not perform any I/O.
+  //
+  // The OggReader relies on this base implementation not performing I/O,
+  // since in FirefoxOS we can't do I/O on the main thread, where this is
+  // called.
+  virtual media::TimeIntervals GetBuffered();
 
   RefPtr<MediaDataPromise> DecodeToFirstVideoData();
 
@@ -326,6 +345,8 @@ private:
 
   virtual void VisibilityChanged();
 
+  virtual void NotifyDataArrivedInternal() {}
+
   // Overrides of this function should decodes an unspecified amount of
   // audio data, enqueuing the audio data in mAudioQueue. Returns true
   // when there's more audio to decode, false if the audio is finished,
@@ -345,15 +366,6 @@ private:
   {
     return false;
   }
-
-  // GetBuffered estimates the time ranges buffered by interpolating the cached
-  // byte ranges with the duration of the media. Reader subclasses should
-  // override this method if they can quickly calculate the buffered ranges more
-  // accurately.
-  //
-  // The primary advantage of this implementation in the reader base class is
-  // that it's a fast approximation, which does not perform any I/O.
-  media::TimeIntervals GetBuffered();
 
   // Promises used only for the base-class (sync->async adapter) implementation
   // of Request{Audio,Video}Data.
