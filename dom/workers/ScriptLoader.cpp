7abdae314a9ba1c3642b89494d9d5efc41a95a51
Ben Kelly <ben@wanderview.com>
2017-02-14 10:06:39 -0500

    Bug 1337543 P6 Persist response headers for offlined service worker scripts. r=baku

--- dom/workers/ScriptLoader.cpp
+++ dom/workers/ScriptLoader.cpp
@@ -671,6 +671,7 @@ private:
     RefPtr<mozilla::dom::InternalResponse> ir =
       new mozilla::dom::InternalResponse(200, NS_LITERAL_CSTRING("OK"));
     ir->SetBody(loadInfo.mCacheReadStream, InternalResponse::UNKNOWN_BODY_SIZE);
+
     // Drop our reference to the stream now that we've passed it along, so it
     // doesn't hang around once the cache is done with it and keep data alive.
     loadInfo.mCacheReadStream = nullptr;
@@ -699,6 +700,7 @@ private:
     }
 
     ir->SetPrincipalInfo(Move(principalInfo));
+    ir->Headers()->FillResponseHeaders(loadInfo.mChannel);
 
     RefPtr<mozilla::dom::Response> response =
       new mozilla::dom::Response(mCacheCreator->Global(), ir);
