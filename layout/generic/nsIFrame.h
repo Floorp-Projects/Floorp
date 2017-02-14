27a05abaea7f9a15d72957ce0924c22dc93ee96f
Ting-Yu Lin <tlin@mozilla.com>
2017-02-14 18:41:41 +0800

    Bug 775624 Part 24 - Implement operator<< for nsReflowStatus to use mozilla::ToString. r=dholbert
    To print log in nsBlockFrame, run "GECKO_BLOCK_DEBUG_FLAGS=reflow ./mach run".
    
    Here is a sample nsReflowStatus string:
    [Complete=Y,NIF=N,Truncated=N,Break=N,FirstLetter=Y].
    
    MozReview-Commit-ID: 4voGcXfJlN7

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -343,6 +343,12 @@ private:
 #define NS_FRAME_SET_TRUNCATION(aStatus, aReflowInput, aMetrics) \
   aStatus.UpdateTruncated(aReflowInput, aMetrics);
 
+#ifdef DEBUG
+// Convert nsReflowStatus to a human-readable string.
+std::ostream&
+operator<<(std::ostream& aStream, const nsReflowStatus& aStatus);
+#endif
+
 //----------------------------------------------------------------------
 
 /**
