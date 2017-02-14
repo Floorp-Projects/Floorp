5a66ff3eb618dc5bb56360c5dc38db91c2d098df
Benjamin Smedberg <benjamin@smedbergs.us>
2017-02-14 09:10:31 -0500

    Bug 1333887 - Hold nsNPAPIPlugin alive for active nsNPAPIPluginInstance objects. This should happen through the plugin tag, but apparently that isn't always working. r=qdot, a=abillings
    --HG--
    extra : source : 0b83dd97c786bfc2641d1733e278faa35475d09c
    extra : intermediate-source : 04578bce528a0390e8f9291b5a50ad5663803bf7

--- dom/plugins/base/nsNPAPIPlugin.h
+++ dom/plugins/base/nsNPAPIPlugin.h
@@ -12,6 +12,7 @@
 
 #include "mozilla/dom/ScriptSettings.h"
 #include "mozilla/PluginLibrary.h"
+#include "mozilla/RefCounted.h"
 
 #if defined(XP_WIN)
 #define NS_NPAPIPLUGIN_CALLBACK(_type, _name) _type (__stdcall * _name)
@@ -24,7 +25,9 @@ typedef NS_NPAPIPLUGIN_CALLBACK(NPError, NP_PLUGININIT) (const NPNetscapeFuncs* 
 typedef NS_NPAPIPLUGIN_CALLBACK(NPError, NP_PLUGINUNIXINIT) (const NPNetscapeFuncs* pCallbacks, NPPluginFuncs* fCallbacks);
 typedef NS_NPAPIPLUGIN_CALLBACK(NPError, NP_PLUGINSHUTDOWN) ();
 
-class nsNPAPIPlugin : public nsISupports
+// nsNPAPIPlugin is held alive both by active nsPluginTag instances and
+// by active nsNPAPIPluginInstance.
+class nsNPAPIPlugin final
 {
 private:
   typedef mozilla::PluginLibrary PluginLibrary;
@@ -32,7 +35,7 @@ private:
 public:
   nsNPAPIPlugin();
 
-  NS_DECL_ISUPPORTS
+  NS_INLINE_DECL_REFCOUNTING(nsNPAPIPlugin)
 
   // Constructs and initializes an nsNPAPIPlugin object. A nullptr file path
   // will prevent this from calling NP_Initialize.
@@ -59,8 +62,8 @@ public:
 
   static nsresult RetainStream(NPStream *pstream, nsISupports **aRetainedPeer);
 
-protected:
-  virtual ~nsNPAPIPlugin();
+private:
+  ~nsNPAPIPlugin();
 
   NPPluginFuncs mPluginFuncs;
   PluginLibrary* mLibrary;
