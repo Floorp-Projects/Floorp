e2442d9cea8c66d51a39af542db01eb618ad6b41
Dão Gottwald <dao@mozilla.com>
2017-02-15 18:42:15 +0100

    Bug 1339413 - Implement prefs for capping the amount of history we import from Chrome. r=Gijs
    MozReview-Commit-ID: BdlNxS76Ko9

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1535,6 +1535,9 @@ pref("browser.migrate.automigrate.enabled", false);
 pref("browser.migrate.automigrate.daysToOfferUndo", 4);
 pref("browser.migrate.automigrate.ui.enabled", true);
 
+pref("browser.migrate.chrome.history.limit", 0);
+pref("browser.migrate.chrome.history.maxAgeInDays", 0);
+
 // Enable browser frames for use on desktop.  Only exposed to chrome callers.
 pref("dom.mozBrowserFramesEnabled", true);
 
