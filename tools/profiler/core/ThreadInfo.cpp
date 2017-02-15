1a4bd77645f80e7fc04c9e5e015de49fbf5c45a6
Nicholas Nethercote <nnethercote@mozilla.com>
2017-02-15 14:44:09 +1100

    Bug 1340928 (part 2) - Don't use ThreadInfo to pass Linux memory measurements. r=mstange.
    This change means that all the relevant code is now within
    platform-linux-android.cpp, which is nice.

--- tools/profiler/core/ThreadInfo.cpp
+++ tools/profiler/core/ThreadInfo.cpp
@@ -23,10 +23,6 @@ ThreadInfo::ThreadInfo(const char* aName, int aThreadId,
   , mStackTop(aStackTop)
   , mPendingDelete(false)
   , mMutex(MakeUnique<mozilla::Mutex>("ThreadInfo::mMutex"))
-#if defined(GP_OS_linux) || defined(GP_OS_android)
-  , mRssMemory(0)
-  , mUssMemory(0)
-#endif
 {
   MOZ_COUNT_CTOR(ThreadInfo);
   mThread = NS_GetCurrentThread();
