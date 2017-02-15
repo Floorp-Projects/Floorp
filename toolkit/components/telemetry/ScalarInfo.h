40ce7f8ee484e1e293e4d74a3e91d16990533267
Georg Fritzsche <georg.fritzsche@googlemail.com>
2017-02-15 16:59:42 +0100

    Bug 1313326 - Part 2: Restrict event recording to specified processes. r=dexter

--- toolkit/components/telemetry/ScalarInfo.h
+++ toolkit/components/telemetry/ScalarInfo.h
@@ -6,7 +6,7 @@
 #ifndef TelemetryScalarInfo_h__
 #define TelemetryScalarInfo_h__
 
-#include "nsXULAppAPI.h"
+#include "TelemetryCommon.h"
 
 // This module is internal to Telemetry. It defines a structure that holds the
 // scalar info. It should only be used by TelemetryScalarData.h automatically
@@ -15,26 +15,18 @@
 
 namespace {
 
-enum class RecordedProcessType : uint32_t {
-  Main       = (1 << GeckoProcessType_Default),  // Also known as "parent process"
-  Content    = (1 << GeckoProcessType_Content),
-  Gpu        = (1 << GeckoProcessType_GPU),
-  AllChilds  = 0xFFFFFFFF - 1,  // All the children processes (i.e. content, gpu, ...)
-  All        = 0xFFFFFFFF       // All the processes
-};
-MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(RecordedProcessType)
-
 struct ScalarInfo {
   uint32_t kind;
   uint32_t name_offset;
   uint32_t expiration_offset;
   uint32_t dataset;
-  RecordedProcessType record_in_processes;
+  mozilla::Telemetry::Common::RecordedProcessType record_in_processes;
   bool keyed;
 
   const char *name() const;
   const char *expiration() const;
 };
+
 } // namespace
 
 #endif // TelemetryScalarInfo_h__
