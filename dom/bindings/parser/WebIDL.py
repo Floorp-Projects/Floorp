873f5d96569d5280fb2a02aa278ea3e3b76486ea
Boris Zbarsky <bzbarsky@mit.edu>
2017-02-15 00:00:04 -0500

    Bug 1330699 part 5.  Disallow mozmap-typed constants.  r=qdot

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -3884,6 +3884,9 @@ class IDLConst(IDLInterfaceMember):
         if type.isDictionary():
             raise WebIDLError("A constant cannot be of a dictionary type",
                               [self.location])
+        if type.isMozMap():
+            raise WebIDLError("A constant cannot be of a MozMap type",
+                              [self.location])
         self.type = type
         self.value = value
 
