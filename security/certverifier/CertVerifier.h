d152366807c28ae07b891485e827660fc5d6f24f
David Keeler <dkeeler@mozilla.com>
2017-02-15 11:02:43 -0800

    Bug 1339010 - Ensure pinning and CT telemetry info has been initialized. r=jcj, a=jcristau
    --HG--
    extra : source : 4c83176013d37417bf9724bc970295b80ead7776

--- security/certverifier/CertVerifier.h
+++ security/certverifier/CertVerifier.h
@@ -69,6 +69,8 @@ enum class NetscapeStepUpPolicy : uint32_t;
 class PinningTelemetryInfo
 {
 public:
+  PinningTelemetryInfo() { Reset(); }
+
   // Should we accumulate pinning telemetry for the result?
   bool accumulateResult;
   Telemetry::ID certPinningResultHistogram;
@@ -83,6 +85,8 @@ public:
 class CertificateTransparencyInfo
 {
 public:
+  CertificateTransparencyInfo() { Reset(); }
+
   // Was CT enabled?
   bool enabled;
   // Did we receive and process any binary SCT data from the supported sources?
