7e738b68e5db8c10934019d90699085733b43f01
Shu-yu Guo <shu@rfrn.org>
2017-02-14 16:22:00 -0800

    Bug 1338796 - Do not call iterator.return if iterator.throw is present in yield*. r=arai, a=lizzard

--- js/src/tests/ecma_6/shell.js
+++ js/src/tests/ecma_6/shell.js
@@ -21,10 +21,11 @@
 
     /** Make an iterator with a return method. */
     global.makeIterator = function makeIterator(overrides) {
+        var throwMethod;
+        if (overrides && overrides.throw)
+            throwMethod = overrides.throw;
         var iterator = {
-            throw: function(e) {
-                throw e;
-            },
+            throw: throwMethod,
             next: function(x) {
                 if (overrides && overrides.next)
                     return overrides.next(x);
