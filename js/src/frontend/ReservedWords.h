c60a35c3eda2eda3137f4ecc1d8e472c53d79ec8
Tooru Fujisawa <arai_a@mac.com>
2017-02-15 23:53:05 +0900

    Bug 1336783 - Part 1: Rework on reserved word and remove TokenStream::KeywordIsName. r=jwalden

--- js/src/frontend/ReservedWords.h
+++ js/src/frontend/ReservedWords.h
@@ -4,15 +4,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* A higher-order macro for enumerating keyword tokens. */
+/* A higher-order macro for enumerating reserved word tokens. */
 
-#ifndef vm_Keywords_h
-#define vm_Keywords_h
+#ifndef vm_ReservedWords_h
+#define vm_ReservedWords_h
 
-#define FOR_EACH_JAVASCRIPT_KEYWORD(macro) \
+#define FOR_EACH_JAVASCRIPT_RESERVED_WORD(macro) \
     macro(false, false_, TOK_FALSE) \
     macro(true, true_, TOK_TRUE) \
     macro(null, null, TOK_NULL) \
+    \
     /* Keywords. */ \
     macro(break, break_, TOK_BREAK) \
     macro(case, case_, TOK_CASE) \
@@ -36,31 +37,45 @@
     macro(this, this_, TOK_THIS) \
     macro(throw, throw_, TOK_THROW) \
     macro(try, try_, TOK_TRY) \
-    macro(typeof, typeof, TOK_TYPEOF) \
+    macro(typeof, typeof_, TOK_TYPEOF) \
     macro(var, var, TOK_VAR) \
     macro(void, void_, TOK_VOID) \
     macro(while, while_, TOK_WHILE) \
     macro(with, with, TOK_WITH) \
     macro(import, import, TOK_IMPORT) \
-    macro(export, export, TOK_EXPORT) \
+    macro(export, export_, TOK_EXPORT) \
     macro(class, class_, TOK_CLASS) \
     macro(extends, extends, TOK_EXTENDS) \
     macro(super, super, TOK_SUPER) \
-    /* Reserved keywords. */ \
-    macro(enum, enum_, TOK_RESERVED) \
-    /* Future reserved keywords, but only in strict mode. */ \
-    macro(implements, implements, TOK_STRICT_RESERVED) \
-    macro(interface, interface, TOK_STRICT_RESERVED) \
-    macro(package, package, TOK_STRICT_RESERVED) \
-    macro(private, private_, TOK_STRICT_RESERVED) \
-    macro(protected, protected_, TOK_STRICT_RESERVED) \
-    macro(public, public_, TOK_STRICT_RESERVED) \
+    \
+    /* Future reserved words. */ \
+    macro(enum, enum_, TOK_ENUM) \
+    \
+    /* Future reserved words, but only in strict mode. */ \
+    macro(implements, implements, TOK_IMPLEMENTS) \
+    macro(interface, interface, TOK_INTERFACE) \
+    macro(package, package, TOK_PACKAGE) \
+    macro(private, private_, TOK_PRIVATE) \
+    macro(protected, protected_, TOK_PROTECTED) \
+    macro(public, public_, TOK_PUBLIC) \
+    \
+    /* Contextual keywords. */ \
+    macro(as, as, TOK_AS) \
+    macro(async, async, TOK_ASYNC) \
     macro(await, await, TOK_AWAIT) \
+    macro(each, each, TOK_EACH) \
+    macro(from, from, TOK_FROM) \
+    macro(get, get, TOK_GET) \
+    macro(let, let, TOK_LET) \
+    macro(of, of, TOK_OF) \
+    macro(set, set, TOK_SET) \
+    macro(static, static_, TOK_STATIC) \
+    macro(target, target, TOK_TARGET) \
     /* \
      * Yield is a token inside function*.  Outside of a function*, it is a \
-     * future reserved keyword in strict mode, but a keyword in JS1.7 even \
+     * future reserved word in strict mode, but a keyword in JS1.7 even \
      * when strict.  Punt logic to parser. \
      */ \
     macro(yield, yield, TOK_YIELD)
 
-#endif /* vm_Keywords_h */
+#endif /* vm_ReservedWords_h */
