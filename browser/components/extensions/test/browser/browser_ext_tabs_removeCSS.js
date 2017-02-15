70b64eef0041cea292f62b8497f78d86cbd4bdd5
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-02-15 14:43:57 +0100

    Backed out changeset 4a964c3f1759 (bug 1339394) for suspicion of causing failures on OS X browser_selectpopup.js tests

--- browser/components/extensions/test/browser/browser_ext_tabs_removeCSS.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_removeCSS.js
@@ -9,7 +9,7 @@ add_task(function* testExecuteScript() {
     let tasks = [
       // Insert CSS file.
       {
-        background: "rgba(0, 0, 0, 0)",
+        background: "transparent",
         foreground: "rgb(0, 113, 4)",
         promise: () => {
           return browser.tabs.insertCSS({
@@ -29,7 +29,7 @@ add_task(function* testExecuteScript() {
       },
       // Remove CSS code again.
       {
-        background: "rgba(0, 0, 0, 0)",
+        background: "transparent",
         foreground: "rgb(0, 113, 4)",
         promise: () => {
           return browser.tabs.removeCSS({
@@ -39,7 +39,7 @@ add_task(function* testExecuteScript() {
       },
       // Remove CSS file again.
       {
-        background: "rgba(0, 0, 0, 0)",
+        background: "transparent",
         foreground: "rgb(0, 0, 0)",
         promise: () => {
           return browser.tabs.removeCSS({
@@ -60,7 +60,7 @@ add_task(function* testExecuteScript() {
       },
       // Remove CSS code again.
       {
-        background: "rgba(0, 0, 0, 0)",
+        background: "transparent",
         foreground: "rgb(0, 0, 0)",
         promise: () => {
           return browser.tabs.removeCSS({
