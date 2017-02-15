9d6898e4f6e11ffa5ba38595860af9cb1d1f14f5
Kate McKinley <kmckinley@mozilla.com>
2017-02-15 12:40:41 +0900

    Bug 1331730 - Log CORS messages from the content process r=bz,mayhemer
    In e10s, a channel created by parent does not have a reliable reference
    to the inner window ID that initiated the request. Without that, the
    channel must request that the content process log and blocked messages
    to the web console. This patch creates a new ipdl interface to pass the
    message from the parent to the child process. The nsCORSListenerProxy
    also needs to keep a reference to the nsIHttpChannel that created it so
    it can find its way back to the child. Additionally, the
    HttpChannelParent needs to be propagated when creating a new channel for
    CORS.
    
    MozReview-Commit-ID: 8CUhlVCTWxt

---
+++ dom/security/test/cors/browser_CORS-console-warnings.js
@@ -0,0 +1,93 @@
+/*
+ * Description of the test:
+ *   Ensure that CORS warnings are printed to the web console.
+ *
+ *   This test uses the same tests as the plain mochitest, but needs access to
+ *   the console.
+ */
+'use strict';
+
+function console_observer(subject, topic, data) {
+  var message = subject.wrappedJSObject.arguments[0];
+  ok(false, message);
+};
+
+var webconsole = null;
+var messages_seen = 0;
+var expected_messages = 50;
+
+function on_new_message(event, new_messages) {
+  for (let message of new_messages) {
+    let elem = message.node;
+    let text = elem.textContent;
+    if (text.match('Cross-Origin Request Blocked:')) {
+      ok(true, "message is: " + text);
+      messages_seen++;
+    }
+  }
+}
+
+function do_cleanup() {
+  if (webconsole) {
+    webconsole.ui.off("new-messages", on_new_message);
+  }
+  yield unsetCookiePref();
+}
+
+/**
+ * Set e10s related preferences in the test environment.
+ * @return {Promise} promise that resolves when preferences are set.
+ */
+function setCookiePref() {
+  return new Promise(resolve =>
+    // accept all cookies so that the CORS requests will send the right cookies
+    SpecialPowers.pushPrefEnv({
+      set: [
+        ["network.cookie.cookieBehavior", 0],
+      ]
+    }, resolve));
+}
+
+/**
+ * Unset e10s related preferences in the test environment.
+ * @return {Promise} promise that resolves when preferences are unset.
+ */
+function unsetCookiePref() {
+  return new Promise(resolve => {
+    SpecialPowers.popPrefEnv(resolve);
+  });
+}
+
+//jscs:disable
+add_task(function*() {
+  //jscs:enable
+  // A longer timeout is necessary for this test than the plain mochitests
+  // due to opening a new tab with the web console.
+  requestLongerTimeout(4);
+  registerCleanupFunction(do_cleanup);
+  yield setCookiePref();
+
+  let test_uri = "http://mochi.test:8888/browser/dom/security/test/cors/file_cors_logging_test.html";
+
+  let tab = yield BrowserTestUtils.openNewForegroundTab(gBrowser, "about:blank");
+
+  let toolbox = yield openToolboxForTab(tab, "webconsole");
+  ok(toolbox, "Got toolbox");
+  let hud = toolbox.getCurrentPanel().hud;
+  ok(hud, "Got hud");
+
+  if (!webconsole) {
+    registerCleanupFunction(do_cleanup);
+    hud.ui.on("new-messages", on_new_message);
+    webconsole = hud;
+  }
+
+  BrowserTestUtils.loadURI(gBrowser, test_uri);
+
+  yield BrowserTestUtils.waitForLocationChange(gBrowser, test_uri+"#finished");
+
+  // Different OS combinations
+  ok(messages_seen > 0, "Saw " + messages_seen + " messages.");
+
+  yield BrowserTestUtils.removeTab(tab);
+});
