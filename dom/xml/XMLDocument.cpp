a32e48710dbc0f762c35214f8de8225aff50627e
John Dai <jdai@mozilla.com>
2017-02-15 14:49:46 +0800

    Bug 1338889 - Part 1: Remove redundant trailing spaces. r=bz a=jcristau From 0e82adec9e2f34040dacfff8eb2dfc134196161e Mon Sep 17 00:00:00 2001

--- dom/xml/XMLDocument.cpp
+++ dom/xml/XMLDocument.cpp
@@ -9,7 +9,7 @@
 #include "nsParserCIID.h"
 #include "nsCharsetSource.h"
 #include "nsIXMLContentSink.h"
-#include "nsPresContext.h" 
+#include "nsPresContext.h"
 #include "nsIContent.h"
 #include "nsIContentViewerContainer.h"
 #include "nsIContentViewer.h"
@@ -61,8 +61,8 @@ using namespace mozilla::dom;
 
 nsresult
 NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
-                  const nsAString& aNamespaceURI, 
-                  const nsAString& aQualifiedName, 
+                  const nsAString& aNamespaceURI,
+                  const nsAString& aQualifiedName,
                   nsIDOMDocumentType* aDoctype,
                   nsIURI* aDocumentURI,
                   nsIURI* aBaseURI,
@@ -74,7 +74,7 @@ NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
   // Note: can't require that aDocumentURI/aBaseURI/aPrincipal be non-null,
   // since at least one caller (XMLHttpRequest) doesn't have decent args to
   // pass in.
-  
+
   nsresult rv;
 
   *aInstancePtrResult = nullptr;
@@ -152,7 +152,7 @@ NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
   // XMLDocuments and documents "created in memory" get to be UTF-8 by default,
   // unlike the legacy HTML mess
   doc->SetDocumentCharacterSet(NS_LITERAL_CSTRING("UTF-8"));
-  
+
   if (aDoctype) {
     nsCOMPtr<nsINode> doctypeAsNode = do_QueryInterface(aDoctype);
     ErrorResult result;
@@ -161,7 +161,7 @@ NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
       return result.StealNSResult();
     }
   }
-  
+
   if (!aQualifiedName.IsEmpty()) {
     ErrorResult result;
     nsCOMPtr<Element> root =
@@ -310,7 +310,7 @@ XMLDocument::Load(const nsAString& aUrl, ErrorResult& aRv)
     docForWarning->WarnOnceAbout(nsIDocument::eChromeUseOfDOM3LoadMethod);
   } else {
     docForWarning->WarnOnceAbout(nsIDocument::eUseOfDOM3LoadMethod);
-  } 
+  }
 
   nsIURI *baseURI = mDocumentURI;
   nsAutoCString charset;
@@ -401,7 +401,7 @@ XMLDocument::Load(const nsAString& aUrl, ErrorResult& aRv)
 
   nsCOMPtr<nsIChannel> channel;
   // nsIRequest::LOAD_BACKGROUND prevents throbber from becoming active,
-  // which in turn keeps STOP button from becoming active  
+  // which in turn keeps STOP button from becoming active
   rv = NS_NewChannel(getter_AddRefs(channel),
                      uri,
                      callingDoc ? callingDoc.get() :
@@ -435,8 +435,8 @@ XMLDocument::Load(const nsAString& aUrl, ErrorResult& aRv)
 
   // Prepare for loading the XML document "into oneself"
   nsCOMPtr<nsIStreamListener> listener;
-  if (NS_FAILED(rv = StartDocumentLoad(kLoadAsData, channel, 
-                                       loadGroup, nullptr, 
+  if (NS_FAILED(rv = StartDocumentLoad(kLoadAsData, channel,
+                                       loadGroup, nullptr,
                                        getter_AddRefs(listener),
                                        false))) {
     NS_ERROR("XMLDocument::Load: Failed to start the document load.");
@@ -518,7 +518,7 @@ XMLDocument::StartDocumentLoad(const char* aCommand,
 {
   nsresult rv = nsDocument::StartDocumentLoad(aCommand,
                                               aChannel, aLoadGroup,
-                                              aContainer, 
+                                              aContainer,
                                               aDocListener, aReset, aSink);
   if (NS_FAILED(rv)) return rv;
 
@@ -542,7 +542,7 @@ XMLDocument::StartDocumentLoad(const char* aCommand,
   NS_ENSURE_SUCCESS(rv, rv);
 
   nsCOMPtr<nsIXMLContentSink> sink;
-    
+
   if (aSink) {
     sink = do_QueryInterface(aSink);
   }
@@ -563,7 +563,7 @@ XMLDocument::StartDocumentLoad(const char* aCommand,
 
   NS_ASSERTION(mChannel, "How can we not have a channel here?");
   mChannelIsPending = true;
-  
+
   SetDocumentCharacterSet(charset);
   mParser->SetDocumentCharset(charset, charsetSource);
   mParser->SetCommand(aCommand);
