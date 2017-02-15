b6bc0669910b9b3ff5574939abca401e4c9f0add
Zibi Braniecki <gandalf@mozilla.com>
2017-02-14 16:05:47 -0800

    Bug 1329904 - Introduce mozIntl.DateTimeFormat. r=Waldo
    MozReview-Commit-ID: LKUcrg0bht4
    
    --HG--
    extra : rebase_source : abe40dec1d13d3f7d372453ae6bf1a69c1252233

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -2622,6 +2622,7 @@ static const JSFunctionSpec intrinsic_functions[] = {
     JS_FN("intl_NumberFormat_availableLocales", intl_NumberFormat_availableLocales, 0,0),
     JS_FN("intl_numberingSystem", intl_numberingSystem, 1,0),
     JS_FN("intl_patternForSkeleton", intl_patternForSkeleton, 2,0),
+    JS_FN("intl_patternForStyle", intl_patternForStyle, 3,0),
     JS_FN("intl_PluralRules_availableLocales", intl_PluralRules_availableLocales, 0,0),
     JS_FN("intl_GetPluralCategories", intl_GetPluralCategories, 2, 0),
     JS_FN("intl_SelectPluralRule", intl_SelectPluralRule, 2,0),
