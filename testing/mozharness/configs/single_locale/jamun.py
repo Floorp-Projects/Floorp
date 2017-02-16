fb7f22630defca3ae966c02599e3d4f5826e29cf
Johan Lorenzo <jlorenzo@mozilla.com>
2017-02-16 18:51:25 +0100

    Bug 1337825 - Jamun: Change android-api-15-{l10n,nightly} to l10n bumped file r=aki a=release
    Also adds missing files
    
    MozReview-Commit-ID: C0G6bE6wmK4

---
+++ testing/mozharness/configs/single_locale/jamun.py
@@ -0,0 +1,35 @@
+import os
+
+config = {
+    "nightly_build": True,
+    "branch": "jamun",
+    "en_us_binary_url": "http://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central",
+    "update_channel": "nightly-jamun",
+    "latest_mar_dir": '/pub/firefox/nightly/latest-jamun-l10n',
+
+    # l10n
+    "hg_l10n_base": "https://hg.mozilla.org/releases/l10n/mozilla-aurora",
+
+    # mar
+    "mar_tools_url": os.environ.get(
+        "MAR_TOOLS_URL",
+        # Buildbot l10n fetches from ftp rather than setting an environ var
+        "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/mar-tools/%(platform)s"
+    ),
+
+    # repositories
+    "mozilla_dir": "jamun",
+    "repos": [{
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/build/tools",
+        "branch": "default",
+        "dest": "tools",
+    }, {
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/projects/jamun",
+        "revision": "%(revision)s",
+        "dest": "jamun",
+    }],
+    # purge options
+    'is_automation': True,
+}
