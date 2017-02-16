c69d1507109af503b7ffae1b69651db6cd796295
Kate McKinley <kmckinley@mozilla.com>
2017-02-16 10:48:59 +0900

    Bug 1339669 - Update security.mixed_content.hsts_priming_cache_timeout default r=mayhemer
    MozReview-Commit-ID: CNFrPUyrdO8
    
    --HG--
    extra : rebase_source : 858da20cf65369ede5908b07921dfba501956b84

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5593,7 +5593,7 @@ pref("security.mixed_content.send_hsts_priming", true);
 pref("security.mixed_content.use_hsts", true);
 #endif
 // Approximately 1 week default cache for HSTS priming failures, in seconds
-pref ("security.mixed_content.hsts_priming_cache_timeout", 10080);
+pref ("security.mixed_content.hsts_priming_cache_timeout", 604800);
 // Force the channel to timeout in 3 seconds if we have not received
 // expects a time in milliseconds
 pref ("security.mixed_content.hsts_priming_request_timeout", 3000);
