d20e35d3b8d0be1a8cbaf2f92e0740168967d3f0
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-02-17 21:35:40 +0100

    Bug 1313688 - Move new console stub generation to head.js; r=jdescottes
    Move the stub generation to functions in head.js so we can use them in other places.
    Fix the getCleanedPacket function for properties that can differ from one OS to another.
    Enhance test speed by removing the use of temp files to get the packets, and also by
    evaluating the command in a single tab, instead of opening a tab for each command.
    
    MozReview-Commit-ID: 2HTA4SwyXDi

--- devtools/client/webconsole/new-console-output/test/fixtures/stub-generators/browser_webconsole_update_stubs_page_error.js
+++ devtools/client/webconsole/new-console-output/test/fixtures/stub-generators/browser_webconsole_update_stubs_page_error.js
@@ -6,43 +6,10 @@
 "use strict";
 
 Cu.import("resource://gre/modules/osfile.jsm", {});
-const TEST_URI = "http://example.com/browser/devtools/client/webconsole/new-console-output/test/fixtures/stub-generators/test-console-api.html";
-
-const { pageError: snippets} = require("devtools/client/webconsole/new-console-output/test/fixtures/stub-generators/stub-snippets.js");
-
-let stubs = {
-  preparedMessages: [],
-  packets: [],
-};
 
 add_task(function* () {
-  let toolbox = yield openNewTabAndToolbox(TEST_URI, "webconsole");
-  ok(true, "make the test not fail");
-
-  for (let [key, code] of snippets) {
-    OS.File.writeAtomic(TEMP_FILE_PATH, `${code}`);
-    let received = new Promise(resolve => {
-      toolbox.target.client.addListener("pageError", function onPacket(e, packet) {
-        toolbox.target.client.removeListener("pageError", onPacket);
-        info("Received page error:" + e + " " + JSON.stringify(packet, null, "\t"));
-
-        let message = prepareMessage(packet, {getNextId: () => 1});
-        stubs.packets.push(formatPacket(message.messageText, packet));
-        stubs.preparedMessages.push(formatStub(message.messageText, packet));
-        resolve();
-      });
-    });
-
-    yield ContentTask.spawn(gBrowser.selectedBrowser, key, function (subKey) {
-      let script = content.document.createElement("script");
-      script.src = "test-tempfile.js?key=" + encodeURIComponent(subKey);
-      content.document.body.appendChild(script);
-    });
-
-    yield received;
-  }
-
+  let fileContent = yield generatePageErrorStubs();
   let filePath = OS.Path.join(`${BASE_PATH}/stubs`, "pageError.js");
-  OS.File.writeAtomic(filePath, formatFile(stubs, "ConsoleMessage"));
-  OS.File.writeAtomic(TEMP_FILE_PATH, "");
+  yield OS.File.writeAtomic(filePath, fileContent);
+  ok(true, "Make the test not fail");
 });
