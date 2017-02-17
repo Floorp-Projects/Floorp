59b0428986596be8e582093748cabd17dc709e86
Mike Hommey <mh+mozilla@glandium.org>
2017-02-17 11:56:12 +0900

    Bug 1341214 - Define GECKO in a single location. r=dustin
    Instead of every file trying to get the top source directory having an
    ad-hoc definition that gets wrong if the files gets moved around for
    some reason, define it in a more central location.

--- taskcluster/taskgraph/task/docker_image.py
+++ taskcluster/taskgraph/task/docker_image.py
@@ -9,6 +9,7 @@ import os
 import urllib2
 
 from . import base
+from .. import GECKO
 from taskgraph.util.docker import (
     docker_image,
     generate_context_hash,
@@ -17,7 +18,6 @@ from taskgraph.util.docker import (
 from taskgraph.util.templates import Templates
 
 logger = logging.getLogger(__name__)
-GECKO = os.path.realpath(os.path.join(__file__, '..', '..', '..', '..'))
 
 # if running in a task, prefer to use the taskcluster proxy (http://taskcluster/),
 # otherwise hit the services directly
