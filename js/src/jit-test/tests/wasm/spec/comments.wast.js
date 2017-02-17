4789f531347ad5f642c94cfee63b7038e7648ff1
Benjamin Bouvier <benj@benj.me>
2017-02-17 18:06:33 +0100

    Bug 1332691: Generate wasm JavaScript tests from the shared test suite; r=luke
    MozReview-Commit-ID: 5HQAuUmS10O

---
+++ js/src/jit-test/tests/wasm/spec/comments.wast.js
@@ -0,0 +1,12 @@
+
+// comments.wast:9
+let $1 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:47
+let $2 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:58
+let $3 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
+
+// comments.wast:67
+let $4 = instance("\x00\x61\x73\x6d\x01\x00\x00\x00");
