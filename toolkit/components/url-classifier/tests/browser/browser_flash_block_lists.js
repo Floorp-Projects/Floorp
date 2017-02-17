5c031f8062ed266f753f4130fb2049aa8efbeef9
Kirk Steuber <ksteuber@mozilla.com>
2017-02-17 09:34:34 -0800

    Bug 1340448 - Fix intermittent timeout of browser_flash_block_lists.js r=Felipe a=test-only
    Uplifting this test-only patch in order to uplift bug 1350381 without conflicts
    
    MozReview-Commit-ID: InSc42mWEJN

--- toolkit/components/url-classifier/tests/browser/browser_flash_block_lists.js
+++ toolkit/components/url-classifier/tests/browser/browser_flash_block_lists.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
-requestLongerTimeout(2);
+requestLongerTimeout(3);
 
 var {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 
