97fb61d8ed970770854aa02cdaee3a673a516f87
Sebastian Kaspari <s.kaspari@gmail.com>
2017-02-17 11:42:14 +0100

    [focus] HomeFragment: Transform settings button into overflow menu. (https://github.com/mozilla-mobile/focus-android/issues/37).

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/HomeFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/HomeFragment.java
@@ -10,17 +10,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.PopupMenu;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.mozilla.focus.R;
+import org.mozilla.focus.activity.AboutActivity;
+import org.mozilla.focus.activity.HelpActivity;
+import org.mozilla.focus.activity.RightsActivity;
 import org.mozilla.focus.activity.SettingsActivity;
 
 /**
  * Fragment displaying the "home" screen when launching the app.
  */
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
     public static final String FRAGMENT_TAG = "home";
 
     public static HomeFragment create() {
@@ -32,7 +38,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         final View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         view.findViewById(R.id.url).setOnClickListener(this);
-        view.findViewById(R.id.settings).setOnClickListener(this);
+        view.findViewById(R.id.menu).setOnClickListener(this);
 
         return view;
     }
@@ -48,10 +54,42 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                         .commit();
                 break;
 
-            case R.id.settings:
-                Intent intent = new Intent(getActivity(), SettingsActivity.class);
-                startActivity(intent);
+            case R.id.menu:
+                final PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_home, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(this);
+                popupMenu.setGravity(Gravity.TOP);
+                popupMenu.show();
                 break;
         }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        final int id = item.getItemId();
+
+        switch (id) {
+            case R.id.settings:
+                Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
+
+            case R.id.about:
+                Intent aboutIntent = new Intent(getActivity(), AboutActivity.class);
+                startActivity(aboutIntent);
+                break;
+
+            case R.id.rights:
+                Intent rightsIntent = new Intent(getActivity(), RightsActivity.class);
+                startActivity(rightsIntent);
+                break;
+
+            case R.id.help:
+                Intent helpIntent = new Intent(getActivity(), HelpActivity.class);
+                startActivity(helpIntent);
+                break;
+        }
+
+        return false;
     }
 }
