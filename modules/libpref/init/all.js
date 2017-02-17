1316c622ff0d4ba8bcaf877fa8992f3f6b3bf882
Kris Maglione <maglione.k@gmail.com>
2017-02-16 17:26:15 -0800

    Bug 1339144 - Use windowless browser for remote background pages. r=aswan
    MozReview-Commit-ID: LfmPbkTsgwu

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2923,6 +2923,9 @@ pref("dom.ipc.processCount", 2);
 pref("dom.ipc.processCount", 1);
 #endif
 
+// WebExtensions only support a single extension process.
+pref("dom.ipc.processCount.extension", 1);
+
 // Disable support for SVG
 pref("svg.disabled", false);
 
