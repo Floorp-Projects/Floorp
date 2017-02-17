9353cc052d3db315da88539fed241f4922068b81
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-02-17 14:45:50 +0100

    Backed out changeset 5c217cd4e9dc (bug 1340434)

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -398,6 +398,11 @@ SimpleTest.requestCompleteLog = function() {
         SimpleTest._forceLogMessageOutput = false;
     });
 };
+// Request complete log when using failure patterns so that failure info
+// from infra can be useful.
+if (usesFailurePatterns()) {
+  SimpleTest.requestCompleteLog();
+}
 
 SimpleTest._logResult = function (test, passInfo, failInfo, stack) {
     var url = SimpleTest._getCurrentTestURL();
@@ -1705,10 +1710,4 @@ function getAndroidSdk() {
         document.documentElement.removeChild(iframe);
     }
     return gAndroidSdk;
-}
-
-// Request complete log when using failure patterns so that failure info
-// from infra can be useful.
-if (usesFailurePatterns()) {
-  SimpleTest.requestCompleteLog();
 }
