0db17aa09fb6a72b89d2e0c6bb63eeb46cd71ff3
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-02-17 08:08:06 +0100

    Bug 1313688 - Fail a test if stubs don't match stubs created by stub generators; r=jdescottes
    MozReview-Commit-ID: 2HTA4SwyXDi

---
+++ devtools/client/webconsole/new-console-output/test/fixtures/stub-generators/browser_webconsole_check_stubs_console_api.js
@@ -0,0 +1,23 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+Cu.import("resource://gre/modules/osfile.jsm", {});
+
+add_task(function* () {
+  let generatedStubs = yield generateConsoleApiStubs();
+
+  let repoStubFilePath = getTestFilePath("../stubs/consoleApi.js");
+  let repoStubFileContent = yield OS.File.read(repoStubFilePath, { encoding: "utf-8" });
+
+  if (generatedStubs != repoStubFileContent) {
+    ok(false, "The consoleApi stubs file needs to be updated by running " +
+      "`mach test devtools/client/webconsole/new-console-output/test/fixtures/" +
+      "stub-generators/browser_webconsole_update_stubs_console_api.js`");
+  } else {
+    ok(true, "The consoleApi stubs file is up to date");
+  }
+});
