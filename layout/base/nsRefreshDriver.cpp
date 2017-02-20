6cd355d7b2219b4789760b7613ec5ae11e9cdfb5
Samael Wang <freesamael@gmail.com>
2017-02-20 16:25:09 +0800

    Bug 1283947 - Clear pending transactions when ClientLayerManager is assigning to a different refresh driver. r=mattwoodrow

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -2185,6 +2185,13 @@ nsRefreshDriver::RevokeTransactionId(uint64_t aTransactionId)
   mPendingTransaction--;
 }
 
+void
+nsRefreshDriver::ClearPendingTransactions()
+{
+  mCompletedTransaction = mPendingTransaction;
+  mWaitingForTransaction = false;
+}
+
 mozilla::TimeStamp
 nsRefreshDriver::GetTransactionStart()
 {
