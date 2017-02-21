b8dbefd80784363428bb33ae0697ec9821a88da4
Tom Schuster <evilpies@gmail.com>
2017-02-21 14:24:10 +0100

    Bug 1341087 - Implement bitwise symbol equality comparison in Ion. r=h4writer

--- js/src/jit-test/tests/symbol-equality.js
+++ js/src/jit-test/tests/symbol-equality.js
@@ -19,5 +19,23 @@ function equalOperands() {
     }
 }
 
+function bitwiseCompare() {
+    var ar = [true, false, Symbol(), null, undefined];
+    var s = Symbol();
+    ar.push(s);
+
+    for (var i = 0; i < 150; i++) {
+        for (var j = 0; j < ar.length; j++) {
+            var equal = (j == ar.indexOf(s));
+
+            assertEq(ar[j] === s, equal);
+            assertEq(ar[j] !== s, !equal);
+            assertEq(ar[j] == s, equal);
+            assertEq(ar[j] != s, !equal);
+        }
+    }
+}
+
 equalOperands();
 simpleEquality();
+bitwiseCompare();
