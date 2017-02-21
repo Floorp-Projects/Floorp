5e50069015e390ff26b4d723f1ae312ff6577a8c
Brian Birtles <birtles@gmail.com>
2017-02-21 13:50:43 +0900

    Bug 1026804 - Turn on dom.event.highrestimestamp.enabled by default; r=smaug
    MozReview-Commit-ID: HDvzwTbERci
    
    --HG--
    extra : rebase_source : 3cdb22268a905de36900d64e77440dc80875ffce

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1260,11 +1260,7 @@ pref("privacy.trackingprotection.lower_network_priority",  false);
 
 pref("dom.event.contextmenu.enabled",       true);
 pref("dom.event.clipboardevents.enabled",   true);
-#if defined(XP_WIN) && !defined(RELEASE_OR_BETA) || defined(MOZ_WIDGET_GTK) && !defined(RELEASE_OR_BETA) || defined(XP_MACOSX) && !defined(RELEASE_OR_BETA) || defined(MOZ_WIDGET_ANDROID) && !defined(RELEASE_OR_BETA)
 pref("dom.event.highrestimestamp.enabled",  true);
-#else
-pref("dom.event.highrestimestamp.enabled",  false);
-#endif
 
 pref("dom.webcomponents.enabled",           false);
 pref("dom.webcomponents.customelements.enabled", false);
