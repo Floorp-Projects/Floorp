93f999e4af274726017ee896b818bca4511aa68b
Patrick McManus <mcmanus@ducksong.com>
2017-02-21 15:19:46 -0500

    Bug 1322373 - TLS 1.3 early-data for HTTP/2. r=dragana,mcmanus

--- netwerk/protocol/http/ASpdySession.h
+++ netwerk/protocol/http/ASpdySession.h
@@ -28,8 +28,9 @@ public:
   virtual PRIntervalTime IdleTime() = 0;
   virtual uint32_t ReadTimeoutTick(PRIntervalTime now) = 0;
   virtual void DontReuse() = 0;
+  virtual uint32_t SpdyVersion() = 0;
 
-  static ASpdySession *NewSpdySession(uint32_t version, nsISocketTransport *);
+  static ASpdySession *NewSpdySession(uint32_t version, nsISocketTransport *, bool);
 
   // MaybeReTunnel() is called by the connection manager when it cannot
   // dispatch a tunneled transaction. That might be because the tunnels it
