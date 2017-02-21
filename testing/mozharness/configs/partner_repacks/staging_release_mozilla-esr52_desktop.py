b1969d5c73730615c281d217792a986374416375
Rail Aliiev <rail@mozilla.com>
2017-02-21 12:00:44 -0500

    Bug 1337366 - SHA1 repacks configs for ESR52 r=jlund a=release DONTBUILD
    MozReview-Commit-ID: LM1wuXGLtn3
    
    --HG--
    rename : testing/mozharness/configs/partner_repacks/staging_release_mozilla-release_desktop.py => testing/mozharness/configs/partner_repacks/staging_release_mozilla-esr52_desktop.py
    extra : source : 70fb9299aee568b0d41e75a772a281922a969b07

---
+++ testing/mozharness/configs/partner_repacks/staging_release_mozilla-esr52_desktop.py
@@ -0,0 +1,6 @@
+config = {
+    "appName": "Firefox",
+    "log_name": "partner_repack",
+    "repack_manifests_url": "https://github.com/mozilla-partners/mozilla-sha1-manifest",
+    "repo_file": "https://raw.githubusercontent.com/mozilla/git-repo/master/repo",
+}
