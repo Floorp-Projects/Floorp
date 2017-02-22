db82cb981b21786d75d67a93c75d31cda3e6730e
Phil Ringnalda <philringnalda@gmail.com>
2017-02-21 19:58:54 -0800

    Backed out changeset 14dbbe36e676 (bug 1313459) for browser_ext_browserAction_area.js timeouts

--- browser/components/extensions/ext-browserAction.js
+++ browser/components/extensions/ext-browserAction.js
@@ -38,14 +38,7 @@ function isAncestorOrSelf(target, node) {
 }
 
 // WeakMap[Extension -> BrowserAction]
-const browserActionMap = new WeakMap();
-
-const browserAreas = {
-  "navbar": CustomizableUI.AREA_NAVBAR,
-  "menupanel": CustomizableUI.AREA_PANEL,
-  "tabstrip": CustomizableUI.AREA_TABSTRIP,
-  "personaltoolbar": CustomizableUI.AREA_BOOKMARKS,
-};
+var browserActionMap = new WeakMap();
 
 // Responsible for the browser_action section of the manifest as well
 // as the associated popup.
@@ -69,7 +62,6 @@ function BrowserAction(options, extension) {
     badgeBackgroundColor: null,
     icon: IconDetails.normalize({path: options.default_icon}, extension),
     popup: options.default_popup || "",
-    area: browserAreas[options.default_area || "navbar"],
   };
 
   this.browserStyle = options.browser_style || false;
@@ -93,7 +85,7 @@ BrowserAction.prototype = {
       removable: true,
       label: this.defaults.title || this.extension.name,
       tooltiptext: this.defaults.title || "",
-      defaultArea: this.defaults.area,
+      defaultArea: CustomizableUI.AREA_NAVBAR,
 
       onBeforeCreated: document => {
         let view = document.createElementNS(XUL_NS, "panelview");
