4ab576e32a966d556cd30baf70cdc692f30f6fdf
Chris Pearce <cpearce@mozilla.com>
2017-02-23 11:00:28 +1300

    Bug 1341894 - Correct encryption subsamples in AVCC -> AnnexB conversion. r=jya
    This menas we can have GMPVideoDecoder's AVCC -> AnnexB conversion done by the H264Converter, and
    simplify the code in WidevineVideoDecoder.
    
    MozReview-Commit-ID: 3HT5VXth6LL

--- dom/media/platforms/agnostic/gmp/GMPVideoDecoder.h
+++ dom/media/platforms/agnostic/gmp/GMPVideoDecoder.h
@@ -44,7 +44,8 @@ public:
   }
   ConversionRequired NeedsConversion() const override
   {
-    return ConversionRequired::kNeedAVCC;
+    return mConvertToAnnexB ? ConversionRequired::kNeedAnnexB
+                            : ConversionRequired::kNeedAVCC;
   }
 
   // GMPVideoDecoderCallbackProxy
@@ -100,6 +101,7 @@ private:
   MozPromiseHolder<DecodePromise> mDrainPromise;
   MozPromiseHolder<FlushPromise> mFlushPromise;
   DecodedData mDecodedData;
+  bool mConvertToAnnexB = false;
 };
 
 } // namespace mozilla
