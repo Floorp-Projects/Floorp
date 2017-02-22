04420c38c3a4c42381d139fb07279d43e9b943ba
Julian Descottes <jdescottes@mozilla.com>
2017-02-22 20:48:23 +0100

    Bug 1336198 - part10: fix race condition in boxmodel tests;r=gl
    MozReview-Commit-ID: IDBgbnlMX2c
    
    --HG--
    extra : rebase_source : 60363b4b4c4971212e333e50af01572d9c36e9d8
    extra : histedit_source : 8169b297ed90c299b0330718c6eab831f7b2a927

--- devtools/client/inspector/boxmodel/test/head.js
+++ devtools/client/inspector/boxmodel/test/head.js
@@ -66,10 +66,37 @@ function openBoxModelView() {
 
 /**
  * Wait for the boxmodel-view-updated event.
- * @return a promise
+ *
+ * @param {InspectorPanel} inspector
+ *        The instance of InspectorPanel currently loaded in the toolbox.
+ * @param {Boolean} waitForSelectionUpdate
+ *        Should the boxmodel-view-updated event come from a new selection.
+ * @return {Promise} a promise
  */
-function waitForUpdate(inspector) {
-  return inspector.once("boxmodel-view-updated");
+function waitForUpdate(inspector, waitForSelectionUpdate) {
+  return new Promise(resolve => {
+    inspector.on("boxmodel-view-updated", function onUpdate(e, reasons) {
+      // Wait for another update event if we are waiting for a selection related event.
+      if (waitForSelectionUpdate && !reasons.includes("new-selection")) {
+        return;
+      }
+
+      inspector.off("boxmodel-view-updated", onUpdate);
+      resolve();
+    });
+  });
+}
+
+/**
+ * Wait for both boxmode-view-updated and markuploaded events.
+ *
+ * @return {Promise} a promise that resolves when both events have been received.
+ */
+function waitForMarkupLoaded(inspector) {
+  return Promise.all([
+    waitForUpdate(inspector),
+    inspector.once("markuploaded"),
+  ]);
 }
 
 function getStyle(testActor, selector, propertyName) {
@@ -93,6 +120,7 @@ function setStyle(testActor, selector, propertyName, value) {
  */
 var _selectNode = selectNode;
 selectNode = function* (node, inspector, reason) {
+  let onUpdate = waitForUpdate(inspector, true);
   yield _selectNode(node, inspector, reason);
-  yield waitForUpdate(inspector);
+  yield onUpdate;
 };
