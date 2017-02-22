0cbbf3cb83a812559c44180829e9514949be244a
Maja Frydrychowicz <mjzffr@gmail.com>
2017-02-22 17:03:59 -0500

    Bug 1337133 - Dispatch pointerDown and pointerUp actions for mouse. r=ato, a=test-only
    MozReview-Commit-ID: FC0lF0S2Mzz

--- testing/web-platform/tests/webdriver/actions/mouse.py
+++ testing/web-platform/tests/webdriver/actions/mouse.py
@@ -1,8 +1,8 @@
 from support.refine import get_events
 
 
-#def test_nothing(session, test_actions_page, mouse_chain):
-#    mouse_chain \
-#        .pointer_down(1) \
-#        .perform()
-#    assert True
+def test_nothing(session, test_actions_page, mouse_chain):
+    mouse_chain \
+        .pointer_down(0) \
+        .perform()
+    assert True
