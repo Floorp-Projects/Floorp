bea1853007ce3d48e38ffdb3650c82538cae84ce
Iris Hsiao <ihsiao@mozilla.com>
2017-02-22 10:33:56 +0800

    Backed out changeset e85a00771f86 (bug 1322273) for assertion failures

--- js/xpconnect/tests/unit/test_nuke_sandbox.js
+++ js/xpconnect/tests/unit/test_nuke_sandbox.js
@@ -4,46 +4,20 @@
 
 /* See https://bugzilla.mozilla.org/show_bug.cgi?id=769273 */
 
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-
-const global = this;
-
 function run_test()
 {
-  var ifacePointer = Cc["@mozilla.org/supports-interface-pointer;1"]
-      .createInstance(Ci.nsISupportsInterfacePointer);
-
-  var sb = Cu.Sandbox(global);
+  var sb = Components.utils.Sandbox("http://www.blah.com");
   sb.prop = "prop"
-  sb.ifacePointer = ifacePointer
-
-  var refToObjFromSb = Cu.evalInSandbox(`
-    Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-
-    ifacePointer.data = {
-      QueryInterface: XPCOMUtils.generateQI([]),
-      wrappedJSObject: {foo: "bar"},
-    };
-
-    var a = {prop2:'prop2'};
-    a
-  `, sb);
-
-  equal(ifacePointer.data.wrappedJSObject.foo, "bar",
-        "Got expected wrapper into sandbox")
-
-  Cu.nukeSandbox(sb);
-  ok(Cu.isDeadWrapper(sb), "sb should be dead");
-  ok(Cu.isDeadWrapper(ifacePointer.data.wrappedJSObject),
-     "Wrapper retrieved via XPConnect should be dead");
-
+  var refToObjFromSb = Components.utils.evalInSandbox("var a = {prop2:'prop2'}; a", sb); 
+  Components.utils.nukeSandbox(sb);
+  do_check_true(Components.utils.isDeadWrapper(sb), "sb should be dead");
   try{
     sb.prop;
     do_check_true(false);
   } catch (e) {
     do_check_true(e.toString().indexOf("can't access dead object") > -1);
   }
-
+  
   Components.utils.isDeadWrapper(refToObjFromSb, "ref to object from sb should be dead");
   try{
     refToObjFromSb.prop2;
@@ -51,4 +25,5 @@ function run_test()
   } catch (e) {
     do_check_true(e.toString().indexOf("can't access dead object") > -1);
   }
+  
 }
