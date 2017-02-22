f6826c758202e6a543dede29f027ecfa7b312920
Mike Hommey <mh+mozilla@glandium.org>
2017-02-22 17:20:41 +0900

    Bug 1355731 - Move tooltool to mozbuild.action. r=gps

--- taskcluster/scripts/misc/tooltool-download.sh
+++ taskcluster/scripts/misc/tooltool-download.sh
@@ -2,10 +2,10 @@
 
 cd $HOME/workspace/build/src
 
-chmod +x taskcluster/docker/recipes/tooltool.py
+chmod +x python/mozbuild/mozbuild/action/tooltool.py
 : TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/home/worker/tooltool-cache}
 export TOOLTOOL_CACHE
 
-./taskcluster/docker/recipes/tooltool.py --url=http://relengapi/tooltool/ -m "${TOOLTOOL_MANIFEST}" fetch
+./python/mozbuild/mozbuild/action/tooltool.py --url=http://relengapi/tooltool/ -m "${TOOLTOOL_MANIFEST}" fetch
 
 cd $OLDPWD
