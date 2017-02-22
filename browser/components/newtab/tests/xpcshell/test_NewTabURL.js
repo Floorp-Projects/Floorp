36dac5da7bc61b77d0ab70ee1254a6f24e730c46
Ursula Sarracini <>
2017-02-22 13:18:09 -0500

    Bug 1340181 - Hide Activity Stream URL in URLbar r=fkiefer,mconley
    MozReview-Commit-ID: F0P5tn2wyG

--- browser/components/newtab/tests/xpcshell/test_NewTabURL.js
+++ browser/components/newtab/tests/xpcshell/test_NewTabURL.js
@@ -17,7 +17,7 @@ XPCOMUtils.defineLazyServiceGetter(this, "aboutNewTabService",
 
 add_task(function*() {
   let defaultURL = aboutNewTabService.newTabURL;
-  Services.prefs.setBoolPref("browser.newtabpage.remote", false);
+  Services.prefs.setBoolPref("browser.newtabpage.activity-stream.enabled", false);
 
   Assert.equal(NewTabURL.get(), defaultURL, `Default newtab URL should be ${defaultURL}`);
   let url = "http://example.com/";
@@ -33,9 +33,9 @@ add_task(function*() {
   Assert.ok(!NewTabURL.overridden, "Newtab URL should not be overridden");
   Assert.equal(NewTabURL.get(), defaultURL, "Newtab URL should be the default");
 
-  // change newtab page to remote
+  // change newtab page to activity stream
   NewTabPrefsProvider.prefs.init();
-  Services.prefs.setBoolPref("browser.newtabpage.remote", true);
+  Services.prefs.setBoolPref("browser.newtabpage.activity-stream.enabled", true);
   Assert.equal(NewTabURL.get(), "about:newtab", `Newtab URL should be about:newtab`);
   Assert.ok(!NewTabURL.overridden, "Newtab URL should not be overridden");
   NewTabPrefsProvider.prefs.uninit();
