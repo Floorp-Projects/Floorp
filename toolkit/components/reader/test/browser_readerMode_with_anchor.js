36c10374b22b413288f341d98c8240527575d7ea
Evan Tseng <evan@tseng.io>
2017-02-23 16:01:41 +0800

    Bug 1340799 - Add a test for clicking hash links in reader mode to scroll to the anchor, r=Gijs
    MozReview-Commit-ID: EzoVcuW2qm4

--- toolkit/components/reader/test/browser_readerMode_with_anchor.js
+++ toolkit/components/reader/test/browser_readerMode_with_anchor.js
@@ -13,9 +13,31 @@ add_task(function* () {
     readerButton.click();
     yield pageShownPromise;
     yield ContentTask.spawn(browser, null, function* () {
-      // Check if offset != 0
-      ok(content.document.getElementById("foo") !== null, "foo element should be in document");
-      ok(content.pageYOffset != 0, "pageYOffset should be > 0");
+      let foo = content.document.getElementById("foo");
+      ok(foo, "foo element should be in document");
+      let {scrollTop} = content.document.documentElement;
+      let {offsetTop} = foo;
+      Assert.lessOrEqual(Math.abs(scrollTop - offsetTop), 1,
+        `scrollTop (${scrollTop}) should be within 1 CSS pixel of offsetTop (${offsetTop})`);
+    });
+  });
+});
+
+add_task(function* () {
+  yield BrowserTestUtils.withNewTab(TEST_PATH + "readerModeArticle.html", function* (browser) {
+    let pageShownPromise = BrowserTestUtils.waitForContentEvent(browser, "AboutReaderContentReady");
+    let readerButton = document.getElementById("reader-mode-button");
+    readerButton.click();
+    yield pageShownPromise;
+    is(content.document.documentElement.scrollTop, 0, "scrollTop should be 0");
+    yield BrowserTestUtils.synthesizeMouseAtCenter("#foo-anchor", {}, browser);
+    yield ContentTask.spawn(browser, null, function* () {
+      let foo = content.document.getElementById("foo");
+      ok(foo, "foo element should be in document");
+      let {scrollTop} = content.document.documentElement;
+      let {offsetTop} = foo;
+      Assert.lessOrEqual(Math.abs(scrollTop - offsetTop), 1,
+        `scrollTop (${scrollTop}) should be within 1 CSS pixel of offsetTop (${offsetTop})`);
     });
   });
 });
