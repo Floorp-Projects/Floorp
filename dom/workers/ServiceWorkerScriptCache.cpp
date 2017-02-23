ee97fbca0002814436122d41ad2e6ecb5a20b468
Ho-Pang Hsu <hopang.hsu@gmail.com>
2017-02-23 18:34:36 +0800

    Bug 1290951 - Part 2.2: Make CompareManager::ComparisonFinished() public. r=bkelly

--- dom/workers/ServiceWorkerScriptCache.cpp
+++ dom/workers/ServiceWorkerScriptCache.cpp
@@ -351,6 +351,29 @@ public:
     return NS_OK;
   }
 
+  void
+  ComparisonFinished(nsresult aStatus, bool aIsEqual)
+  {
+    AssertIsOnMainThread();
+    MOZ_ASSERT(mCallback);
+    MOZ_ASSERT(mState == WaitingForScriptOrComparisonResult);
+
+    if (NS_WARN_IF(NS_FAILED(aStatus))) {
+      Fail(aStatus);
+      return;
+    }
+
+    if (aIsEqual) {
+      mCallback->ComparisonResult(aStatus, aIsEqual, EmptyString(), mMaxScope);
+      Cleanup();
+      return;
+    }
+
+    // Write to Cache so ScriptLoader reads succeed.
+    mState = WaitingForOpen;
+    WriteNetworkBufferToNewCache();
+  }
+
 private:
   ~CompareManager()
   {
@@ -493,29 +516,6 @@ private:
     mState = WaitingForPut;
     WriteToCache(cache);
     return;
-  }
-
-  void
-  ComparisonFinished(nsresult aStatus, bool aIsEqual)
-  {
-    AssertIsOnMainThread();
-    MOZ_ASSERT(mCallback);
-    MOZ_ASSERT(mState == WaitingForScriptOrComparisonResult);
-
-    if (NS_WARN_IF(NS_FAILED(aStatus))) {
-      Fail(aStatus);
-      return;
-    }
-
-    if (aIsEqual) {
-      mCallback->ComparisonResult(aStatus, aIsEqual, EmptyString(), mMaxScope);
-      Cleanup();
-      return;
-    }
-
-    // Write to Cache so ScriptLoader reads succeed.
-    mState = WaitingForOpen;
-    WriteNetworkBufferToNewCache();
   }
 
   void
