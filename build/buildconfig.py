97b1f7187081c8314a5e3f95a7436d5497950eaa
Jan Beich <jbeich@FreeBSD.org>
2017-02-23 22:01:18 +0000

    Bug 1316879 - Don't use CPP/CXXCPP from environment after bug 1299919. r=glandium, a=lizzard
    MozReview-Commit-ID: 4Of9nsVWK3q
    
    --HG--
    extra : source : 0041f17d932f68322f8693a05574f9870299eb12

--- build/buildconfig.py
+++ build/buildconfig.py
@@ -16,5 +16,5 @@ for var in ('topsrcdir', 'topobjdir', 'defines', 'non_global_defines',
 substs = dict(substs)
 
 for var in os.environ:
-    if var != 'SHELL' and var in substs:
+    if var not in ('CPP', 'CXXCPP', 'SHELL') and var in substs:
         substs[var] = os.environ[var]
