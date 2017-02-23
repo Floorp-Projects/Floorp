1cea6cca52a4cc03893731e79080a06da65e16ba
Edgar Chen <echen@mozilla.com>
2017-02-23 18:24:05 +0800

    Bug 1341693 - Don't need to check GetDocShell() when creating CustomElementRegistry; r=wchen
    MozReview-Commit-ID: 7k3MQBEMpnV

--- dom/base/CustomElementRegistry.cpp
+++ dom/base/CustomElementRegistry.cpp
@@ -175,30 +175,6 @@ CustomElementRegistry::IsCustomElementEnabled(JSContext* aCx, JSObject* aObject)
          nsContentUtils::IsWebComponentsEnabled();
 }
 
-/* static */ already_AddRefed<CustomElementRegistry>
-CustomElementRegistry::Create(nsPIDOMWindowInner* aWindow)
-{
-  MOZ_ASSERT(aWindow);
-  MOZ_ASSERT(aWindow->IsInnerWindow());
-
-  if (!aWindow->GetDocShell()) {
-    return nullptr;
-  }
-
-  if (!IsCustomElementEnabled()) {
-    return nullptr;
-  }
-
-  RefPtr<CustomElementRegistry> customElementRegistry =
-    new CustomElementRegistry(aWindow);
-
-  if (!customElementRegistry->Init()) {
-    return nullptr;
-  }
-
-  return customElementRegistry.forget();
-}
-
 /* static */ void
 CustomElementRegistry::ProcessTopElementQueue()
 {
@@ -241,6 +217,10 @@ CustomElementRegistry::CustomElementRegistry(nsPIDOMWindowInner* aWindow)
  , mIsCustomDefinitionRunning(false)
  , mIsBackupQueueProcessing(false)
 {
+  MOZ_ASSERT(aWindow);
+  MOZ_ASSERT(aWindow->IsInnerWindow());
+  MOZ_ALWAYS_TRUE(mConstructors.init());
+
   mozilla::HoldJSObjects(this);
 
   if (!sProcessingStack) {
@@ -253,12 +233,6 @@ CustomElementRegistry::CustomElementRegistry(nsPIDOMWindowInner* aWindow)
 CustomElementRegistry::~CustomElementRegistry()
 {
   mozilla::DropJSObjects(this);
-}
-
-bool
-CustomElementRegistry::Init()
-{
-  return mConstructors.init();
 }
 
 CustomElementDefinition*
