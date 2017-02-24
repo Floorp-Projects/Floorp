3efe52d1a8c64874d78f3c9139a4229f76e5a536
Alex Chronopoulos <achronop@gmail.com>
2017-02-24 11:37:51 +0200

    Bug 1339816 - Part 2: Uplift cubeb audiounit drift fix. r=padenot a=gchang

--- media/libcubeb/gtest/test_resampler.cpp
+++ media/libcubeb/gtest/test_resampler.cpp
@@ -717,7 +717,8 @@ TEST(cubeb, resampler_passthrough_duplex_callback_reordering)
 
   output_seq_idx += BUF_BASE_SIZE;
 
-  ASSERT_EQ(prebuffer_frames, static_cast<long>(ARRAY_LENGTH(input_buffer_prebuffer) / input_params.channels));
+  // prebuffer_frames will hold the frames used by the resampler.
+  ASSERT_EQ(prebuffer_frames, BUF_BASE_SIZE);
   ASSERT_EQ(got, BUF_BASE_SIZE);
 
   for (uint32_t i = 0; i < 300; i++) {
