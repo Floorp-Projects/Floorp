3e288fa2da60c39c9dc15027e1c9f35d5f9237bd
Alastor Wu <alwu@mozilla.com>
2017-02-24 17:15:20 +0800

    Bug 1339230 - part1 : only need to do audio competing for active agent. r=baku
    In Fennec, the audio competiting is to stop the playing tab if there is new
    incoming tab. We only implement the way to pause the tab, not including to resume
    the tab, so we just need to run the audio competing related stuffs when the agent
    is active.
    
    MozReview-Commit-ID: 3F0M2jLw9VY

--- dom/audiochannel/AudioChannelService.h
+++ dom/audiochannel/AudioChannelService.h
@@ -233,8 +233,7 @@ private:
   void SetDefaultVolumeControlChannelInternal(int32_t aChannel,
                                               bool aVisible, uint64_t aChildID);
 
-  void RefreshAgentsAudioFocusChanged(AudioChannelAgent* aAgent,
-                                      bool aActive);
+  void RefreshAgentsAudioFocusChanged(AudioChannelAgent* aAgent);
 
   class AudioChannelConfig final : public AudioPlaybackConfig
   {
@@ -261,7 +260,7 @@ private:
       mChannels[(int16_t)AudioChannel::System].mMuted = false;
     }
 
-    void AudioFocusChanged(AudioChannelAgent* aNewPlayingAgent, bool aActive);
+    void AudioFocusChanged(AudioChannelAgent* aNewPlayingAgent);
     void AudioAudibleChanged(AudioChannelAgent* aAgent,
                              AudibleState aAudible,
                              AudibleChangedReasons aReason);
@@ -310,11 +309,12 @@ private:
     void MaybeNotifyMediaBlockStart(AudioChannelAgent* aAgent);
 
     void RequestAudioFocus(AudioChannelAgent* aAgent);
-    void NotifyAudioCompetingChanged(AudioChannelAgent* aAgent, bool aActive);
+
+    // We need to do audio competing only when the new incoming agent started.
+    void NotifyAudioCompetingChanged(AudioChannelAgent* aAgent);
 
     uint32_t GetCompetingBehavior(AudioChannelAgent* aAgent,
-                                  int32_t aIncomingChannelType,
-                                  bool aIncomingChannelActive) const;
+                                  int32_t aIncomingChannelType) const;
     bool IsAgentInvolvingInAudioCompeting(AudioChannelAgent* aAgent) const;
     bool IsAudioCompetingInSameTab() const;
     bool IsContainingPlayingAgent(AudioChannelAgent* aAgent) const;
