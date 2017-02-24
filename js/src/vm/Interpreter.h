af1fbf4e9a4e93ec3d75be112f96e577a589c729
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-24 23:10:56 +0100

    Backed out changeset 82cdbd24d06f (bug 1341061) for failing crashtest 366271-1.html. r=backout

--- js/src/vm/Interpreter.h
+++ js/src/vm/Interpreter.h
@@ -420,6 +420,14 @@ ThrowingOperation(JSContext* cx, HandleValue v);
 bool
 GetProperty(JSContext* cx, HandleValue value, HandlePropertyName name, MutableHandleValue vp);
 
+bool
+GetEnvironmentName(JSContext* cx, HandleObject obj, HandlePropertyName name,
+                   MutableHandleValue vp);
+
+bool
+GetEnvironmentNameForTypeOf(JSContext* cx, HandleObject obj, HandlePropertyName name,
+                            MutableHandleValue vp);
+
 JSObject*
 Lambda(JSContext* cx, HandleFunction fun, HandleObject parent);
 
