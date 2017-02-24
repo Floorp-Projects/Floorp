c8bdd61518ee6e9e20c4e37016740fc1984b2dce
cku <cku@mozilla.com>
2017-02-24 11:40:44 +0800

    Bug 1341149 - Implement ImageLayerClipState::ctor. r=mstange, a=lizzard
    MozReview-Commit-ID: 7MPrjtgz6c7

--- layout/painting/nsCSSRendering.h
+++ layout/painting/nsCSSRendering.h
@@ -577,7 +577,7 @@ struct nsCSSRendering {
                     bool* aOutIsTransformedFixed = nullptr);
 
   struct ImageLayerClipState {
-    nsRect mBGClipArea;  // Affected by mClippedRadii
+    nsRect mBGClipArea;            // Affected by mClippedRadii
     nsRect mAdditionalBGClipArea;  // Not affected by mClippedRadii
     nsRect mDirtyRect;
     gfxRect mDirtyRectGfx;
@@ -590,6 +590,14 @@ struct nsCSSRendering {
     // Whether we are being asked to draw with a caller provided background
     // clipping area. If this is true we also disable rounded corners.
     bool mCustomClip;
+
+    ImageLayerClipState()
+     : mHasRoundedCorners(false),
+       mHasAdditionalBGClipArea(false),
+       mCustomClip(false)
+    {
+      memset(mRadii, 0, sizeof(nscoord) * 8);
+    }
   };
 
   static void
