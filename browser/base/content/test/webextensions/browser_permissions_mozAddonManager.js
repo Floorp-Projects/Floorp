160990e2cde6c62602f94cd5786a834e26cabc8b
Andrew Swan <aswan@mozilla.com>
2017-02-23 16:30:33 -0800

    Bug 1340586 Part 3 Split up browser_extension_permissions.js r=mossop
    MozReview-Commit-ID: A7NLR6f1VVO
    
    --HG--
    rename : browser/base/content/test/webextensions/browser_extension_permissions.js => browser/base/content/test/webextensions/browser_permissions_addons_search.js
    rename : browser/base/content/test/webextensions/browser_extension_permissions.js => browser/base/content/test/webextensions/browser_permissions_installTrigger.js
    rename : browser/base/content/test/webextensions/browser_extension_permissions.js => browser/base/content/test/webextensions/browser_permissions_local_file.js
    rename : browser/base/content/test/webextensions/browser_extension_permissions.js => browser/base/content/test/webextensions/browser_permissions_mozAddonManager.js
    extra : rebase_source : 9ace0c68ff4317a4753ac4c3c50a060fbbd8f6d9

---
+++ browser/base/content/test/webextensions/browser_permissions_mozAddonManager.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const INSTALL_PAGE = `${BASE}/file_install_extensions.html`;
+
+async function installMozAM(filename) {
+  gBrowser.selectedBrowser.loadURI(INSTALL_PAGE);
+  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+
+  await ContentTask.spawn(gBrowser.selectedBrowser, `${BASE}/${filename}`, function*(url) {
+    yield content.wrappedJSObject.installMozAM(url);
+  });
+}
+
+add_task(() => testInstallMethod(installMozAM));
