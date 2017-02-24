fb1d1f92e3eda01eee0c061cdb8e7b1448e2bb94
Xidorn Quan <me@upsuper.org>
2017-02-24 21:32:20 +1100

    Bug 1342303 part 3 - Remove nsCOMArray::EnumerateForwards uses in docshell. r=smaug
    MozReview-Commit-ID: FJen0p0c54u

--- docshell/shistory/nsSHEntry.cpp
+++ docshell/shistory/nsSHEntry.cpp
@@ -59,19 +59,14 @@ nsSHEntry::nsSHEntry(const nsSHEntry& aOther)
 {
 }
 
-static bool
-ClearParentPtr(nsISHEntry* aEntry, void* /* aData */)
-{
-  if (aEntry) {
-    aEntry->SetParent(nullptr);
-  }
-  return true;
-}
-
 nsSHEntry::~nsSHEntry()
 {
   // Null out the mParent pointers on all our kids.
-  mChildren.EnumerateForwards(ClearParentPtr, nullptr);
+  for (nsISHEntry* entry : mChildren) {
+    if (entry) {
+      entry->SetParent(nullptr);
+    }
+  }
 }
 
 NS_IMPL_ISUPPORTS(nsSHEntry, nsISHContainer, nsISHEntry, nsISHEntryInternal)
