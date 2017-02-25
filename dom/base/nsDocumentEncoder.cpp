a5701e725a4bbaa4a5447840a5da780be5561bf7
Mats Palmgren <mats@mozilla.com>
2017-02-25 11:33:34 +0100

    Bug 1341137 part 2 - Provide a way to add ranges to Selection objects that aren't associated with a shell/nsFrameSelection.  r=smaug a=jcristau

--- dom/base/nsDocumentEncoder.cpp
+++ dom/base/nsDocumentEncoder.cpp
@@ -1403,6 +1403,9 @@ nsHTMLCopyEncoder::SetSelection(nsISelection* aSelection)
     return NS_OK;
   }
 
+  // XXX We should try to get rid of the Selection object here.
+  // XXX bug 1245883
+
   // also consider ourselves in a text widget if we can't find an html document
   nsCOMPtr<nsIHTMLDocument> htmlDoc = do_QueryInterface(mDocument);
   if (!(htmlDoc && mDocument->IsHTMLDocument())) {
@@ -1430,7 +1433,10 @@ nsHTMLCopyEncoder::SetSelection(nsISelection* aSelection)
     rv = PromoteRange(myRange);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    rv = mSelection->AddRange(myRange);
+    ErrorResult result;
+    nsRange* r = static_cast<nsRange*>(myRange.get());
+    mSelection->AsSelection()->AddRangeInternal(*r, mDocument, result);
+    rv = result.StealNSResult();
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
