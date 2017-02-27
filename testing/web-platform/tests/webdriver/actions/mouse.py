3861ad64012e47d3dd6d8e2943d7d02e28bfd6ea
Maja Frydrychowicz <mjzffr@gmail.com>
2017-02-27 08:26:32 -0500

    Bug 1337133 - Set up fixtures for mouse actions in wdspec tests. r=ato, a=test-only
    MozReview-Commit-ID: zzIQgSgEPb

--- testing/web-platform/tests/webdriver/actions/mouse.py
+++ testing/web-platform/tests/webdriver/actions/mouse.py
@@ -1,1 +1,8 @@
-pass
+from support.refine import get_events
+
+
+#def test_nothing(session, test_actions_page, mouse_chain):
+#    mouse_chain \
+#        .pointer_down(1) \
+#        .perform()
+#    assert True
