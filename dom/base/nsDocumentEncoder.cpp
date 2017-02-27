de6dbed247a2c3e9080d4e19d4c480cb7e108615
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-02-27 16:44:10 +0100

    Backed out changeset 020e7124b44a (bug 1341137)

--- dom/base/nsDocumentEncoder.cpp
+++ dom/base/nsDocumentEncoder.cpp
@@ -1403,9 +1403,6 @@ nsHTMLCopyEncoder::SetSelection(nsISelection* aSelection)
     return NS_OK;
   }
 
-  // XXX We should try to get rid of the Selection object here.
-  // XXX bug 1245883
-
   // also consider ourselves in a text widget if we can't find an html document
   nsCOMPtr<nsIHTMLDocument> htmlDoc = do_QueryInterface(mDocument);
   if (!(htmlDoc && mDocument->IsHTMLDocument())) {
@@ -1433,10 +1430,7 @@ nsHTMLCopyEncoder::SetSelection(nsISelection* aSelection)
     rv = PromoteRange(myRange);
     NS_ENSURE_SUCCESS(rv, rv);
 
-    ErrorResult result;
-    nsRange* r = static_cast<nsRange*>(myRange.get());
-    mSelection->AsSelection()->AddRangeInternal(*r, mDocument, result);
-    rv = result.StealNSResult();
+    rv = mSelection->AddRange(myRange);
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
