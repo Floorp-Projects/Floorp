9b85fa7b5a0c5b31fae6cdb46a6264b1526da91f
Nick Thomas <nthomas@mozilla.com>
2017-02-27 13:01:46 +1300

    Bug 1342814 - add win64 location for stub installer, r=rail a=release DONTBUILD
    MozReview-Commit-ID: GRvoHrUhGlV

--- testing/mozharness/configs/releases/dev_bouncer_firefox_beta.py
+++ testing/mozharness/configs/releases/dev_bouncer_firefox_beta.py
@@ -69,6 +69,10 @@ config = {
                     "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Setup%%20Stub%%20%(version)s.exe",
                     "bouncer-platform": "win",
                 },
+                "win64": {
+                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Setup%%20Stub%%20%(version)s.exe",
+                    "bouncer-platform": "win64",
+                },
             },
         },
         "complete-mar": {
