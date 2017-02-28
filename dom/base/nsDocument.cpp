a0902d98551e0502a067ab50da01dc276ae9190d
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-28 18:56:14 +0100

    Backed out changeset d19ad1c1c214 (bug 1266747) for failing dom/workers/test/serviceworkers/test_claim.html. r=backout

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -3254,21 +3254,6 @@ nsIDocument::HasFocus(ErrorResult& rv) const
   return false;
 }
 
-TimeStamp
-nsIDocument::LastFocusTime() const
-{
-  return mLastFocusTime;
-}
-
-void
-nsIDocument::SetLastFocusTime(const TimeStamp& aFocusTime)
-{
-  MOZ_DIAGNOSTIC_ASSERT(!aFocusTime.IsNull());
-  MOZ_DIAGNOSTIC_ASSERT(mLastFocusTime.IsNull() ||
-                        aFocusTime >= mLastFocusTime);
-  mLastFocusTime = aFocusTime;
-}
-
 NS_IMETHODIMP
 nsDocument::GetReferrer(nsAString& aReferrer)
 {
@@ -4768,14 +4753,6 @@ nsDocument::SetScriptGlobalObject(nsIScriptGlobalObject *aScriptGlobalObject)
     }
 
     MaybeRescheduleAnimationFrameNotifications();
-
-    // If we are set in a window that is already focused we should remember this
-    // as the time the document gained focus.
-    bool focused = false;
-    Unused << HasFocus(&focused);
-    if (focused) {
-      SetLastFocusTime(TimeStamp::Now());
-    }
   }
 
   // Remember the pointer to our window (or lack there of), to avoid
