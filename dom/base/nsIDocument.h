394630f688fc113143aba1e7743962276eaf959a
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-02-28 18:56:14 +0100

    Backed out changeset d19ad1c1c214 (bug 1266747) for failing dom/workers/test/serviceworkers/test_claim.html. r=backout

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2656,8 +2656,6 @@ public:
   }
   Element* GetActiveElement();
   bool HasFocus(mozilla::ErrorResult& rv) const;
-  mozilla::TimeStamp LastFocusTime() const;
-  void SetLastFocusTime(const mozilla::TimeStamp& aFocusTime);
   // Event handlers are all on nsINode already
   bool MozSyntheticDocument() const
   {
@@ -3026,10 +3024,6 @@ protected:
 
   // container for per-context fonts (downloadable, SVG, etc.)
   RefPtr<mozilla::dom::FontFaceSet> mFontFaceSet;
-
-  // Last time this document or a one of its sub-documents was focused.  If
-  // focus has never occurred then mLastFocusTime.IsNull() will be true.
-  mozilla::TimeStamp mLastFocusTime;
 
   // True if BIDI is enabled.
   bool mBidiEnabled : 1;
