550affaf1214186333f4078616b921c65467c86f
Tom Tromey <tom@tromey.com>
2017-02-28 14:18:40 -0700

    Bug 1343292 - change return types in GenericPrinter; r=nbp
    MozReview-Commit-ID: 1GJYujhrWj7
    
    --HG--
    extra : rebase_source : 90aef429db4285707f23105f950d9d930f98c21b

--- js/src/vm/Printer.h
+++ js/src/vm/Printer.h
@@ -35,17 +35,17 @@ class GenericPrinter
     GenericPrinter();
 
   public:
-    // Puts |len| characters from |s| at the current position and return an offset to
-    // the beginning of this new data.
-    virtual int put(const char* s, size_t len) = 0;
+    // Puts |len| characters from |s| at the current position and
+    // return true on success, false on failure.
+    virtual bool put(const char* s, size_t len) = 0;
 
-    inline int put(const char* s) {
+    inline bool put(const char* s) {
         return put(s, strlen(s));
     }
 
     // Prints a formatted string into the buffer.
-    virtual int printf(const char* fmt, ...) MOZ_FORMAT_PRINTF(2, 3);
-    virtual int vprintf(const char* fmt, va_list ap);
+    virtual bool printf(const char* fmt, ...) MOZ_FORMAT_PRINTF(2, 3);
+    virtual bool vprintf(const char* fmt, va_list ap);
 
     // Report that a string operation failed to get the memory it requested. The
     // first call to this function calls JS_ReportOutOfMemory, and sets this
@@ -108,10 +108,10 @@ class Sprinter final : public GenericPrinter
     // internal content. The caller *must* completely fill this space on success.
     char* reserve(size_t len);
 
-    // Puts |len| characters from |s| at the current position and return an offset to
-    // the beginning of this new data.
-    virtual int put(const char* s, size_t len) override;
-    using GenericPrinter::put; // pick up |inline int put(const char* s);|
+    // Puts |len| characters from |s| at the current position and
+    // return true on success, false on failure.
+    virtual bool put(const char* s, size_t len) override;
+    using GenericPrinter::put; // pick up |inline bool put(const char* s);|
 
     // Format the given format/arguments as if by JS_vsmprintf, then put it.
     // Return true on success, else return false and report an error (typically
@@ -119,9 +119,9 @@ class Sprinter final : public GenericPrinter
     MOZ_MUST_USE bool jsprintf(const char* fmt, ...) MOZ_FORMAT_PRINTF(2, 3);
 
     // Prints a formatted string into the buffer.
-    virtual int vprintf(const char* fmt, va_list ap) override;
+    virtual bool vprintf(const char* fmt, va_list ap) override;
 
-    int putString(JSString* str);
+    bool putString(JSString* str);
 
     ptrdiff_t getOffset() const;
 
@@ -152,14 +152,14 @@ class Fprinter final : public GenericPrinter
     void flush();
     void finish();
 
-    // Puts |len| characters from |s| at the current position and return an
-    // offset to the beginning of this new data.
-    virtual int put(const char* s, size_t len) override;
-    using GenericPrinter::put; // pick up |inline int put(const char* s);|
+    // Puts |len| characters from |s| at the current position and
+    // return true on success, false on failure.
+    virtual bool put(const char* s, size_t len) override;
+    using GenericPrinter::put; // pick up |inline bool put(const char* s);|
 
     // Prints a formatted string into the buffer.
-    virtual int printf(const char* fmt, ...) override MOZ_FORMAT_PRINTF(2, 3);
-    virtual int vprintf(const char* fmt, va_list ap) override;
+    virtual bool printf(const char* fmt, ...) override MOZ_FORMAT_PRINTF(2, 3);
+    virtual bool vprintf(const char* fmt, va_list ap) override;
 };
 
 // LSprinter, is similar to Sprinter except that instead of using an
@@ -198,14 +198,14 @@ class LSprinter final : public GenericPrinter
     // Drop the current string, and let them be free with the LifoAlloc.
     void clear();
 
-    // Puts |len| characters from |s| at the current position and return an
-    // offset to the beginning of this new data.
-    virtual int put(const char* s, size_t len) override;
-    using GenericPrinter::put; // pick up |inline int put(const char* s);|
+    // Puts |len| characters from |s| at the current position and
+    // return true on success, false on failure.
+    virtual bool put(const char* s, size_t len) override;
+    using GenericPrinter::put; // pick up |inline bool put(const char* s);|
 
     // Prints a formatted string into the buffer.
-    virtual int printf(const char* fmt, ...) override MOZ_FORMAT_PRINTF(2, 3);
-    virtual int vprintf(const char* fmt, va_list ap) override;
+    virtual bool printf(const char* fmt, ...) override MOZ_FORMAT_PRINTF(2, 3);
+    virtual bool vprintf(const char* fmt, va_list ap) override;
 
     // Report that a string operation failed to get the memory it requested. The
     // first call to this function calls JS_ReportOutOfMemory, and sets this
