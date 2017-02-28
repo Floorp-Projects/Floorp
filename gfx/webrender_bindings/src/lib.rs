a511778b56171294fb86e805b22b86ec12c3b2d1
Kartikaya Gupta <kgupta@mozilla.com>
2017-02-28 09:08:58 -0500

    Bug 1343089 - Remove unnecessary crate dependencies from webrender_bindings. r=rhunt

--- gfx/webrender_bindings/src/lib.rs
+++ gfx/webrender_bindings/src/lib.rs
@@ -7,15 +7,6 @@ extern crate webrender_traits;
 extern crate euclid;
 extern crate app_units;
 extern crate gleam;
-extern crate fnv;
-
-#[cfg(target_os="macos")]
-extern crate core_foundation;
-
-#[cfg(target_os="windows")]
-extern crate kernel32;
-#[cfg(target_os="windows")]
-extern crate winapi;
 
 #[allow(non_snake_case)]
 pub mod bindings;
