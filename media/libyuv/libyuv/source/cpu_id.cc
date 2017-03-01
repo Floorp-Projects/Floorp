b4b1d2146e3fb859be642cb45c95fe7c34809269
Randell Jesup <rjesup@jesup.org>
2017-02-28 23:20:50 -0500

    Bug 1342730: clean up MipsCpuCaps r=sotaro
    MozReview-Commit-ID: 2EwQHcN8gfF

--- media/libyuv/libyuv/source/cpu_id.cc
+++ media/libyuv/libyuv/source/cpu_id.cc
@@ -177,6 +177,7 @@ LIBYUV_API SAFEBUFFERS int MipsCpuCaps(const char* cpuinfo_name,
     if (strcmp(ase, " dspr2") == 0) {
       return kCpuHasDSPR2;
     }
+    return 0;
   }
   while (fgets(cpuinfo_line, sizeof(cpuinfo_line) - 1, f)) {
     if (memcmp(cpuinfo_line, "ASEs implemented", 16) == 0) {
@@ -189,6 +190,7 @@ LIBYUV_API SAFEBUFFERS int MipsCpuCaps(const char* cpuinfo_name,
         if (strcmp(ase, " dspr2") == 0) {
           return kCpuHasDSPR2;
         }
+        return 0;
       }
     }
   }
