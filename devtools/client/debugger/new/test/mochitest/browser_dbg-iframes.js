686e0997b932feed9e6a7f94b17e13a6bca56339
Wes Kocher <wkocher@mozilla.com>
2017-03-01 10:38:33 -0800

    Backed out 2 changesets (bug 1338567) for near permafailing debugger test failures a=backout CLOSED TREE
    Backed out changeset c594a1353f5d (bug 1338567)
    Backed out changeset e9dcba1a87af (bug 1338567)
    
    MozReview-Commit-ID: 4vB5haf8zyB

--- devtools/client/debugger/new/test/mochitest/browser_dbg-iframes.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-iframes.js
@@ -1,12 +1,26 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-const {
-  setupTestRunner,
-  iframes
-} = require("devtools/client/debugger/new/integration-tests");
+/**
+ * Test debugging a page with iframes
+ *  1. pause in the main thread
+ *  2. pause in the iframe
+ */
+add_task(function* () {
+  const dbg = yield initDebugger("doc-iframes.html");
 
-add_task(function*() {
-  setupTestRunner(this);
-  yield iframes(this);
+  // test pausing in the main thread
+  yield reload(dbg);
+  yield waitForPaused(dbg);
+  assertPausedLocation(dbg, "iframes.html", 8);
+
+  // test pausing in the iframe
+  yield resume(dbg);
+  yield waitForPaused(dbg);
+  assertPausedLocation(dbg, "debugger-statements.html", 8);
+
+  // test pausing in the iframe
+  yield resume(dbg);
+  yield waitForPaused(dbg);
+  assertPausedLocation(dbg, "debugger-statements.html", 12);
 });
