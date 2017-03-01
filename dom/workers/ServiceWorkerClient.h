e9c410ff0227b40bd3db2a2deb298bd76457024b
Ben Kelly <ben@wanderview.com>
2017-02-28 22:12:27 -0500

    Bug 1266747 P2 Track last focus time on ServiceWorkerClient. r=smaug

--- dom/workers/ServiceWorkerClient.h
+++ dom/workers/ServiceWorkerClient.h
@@ -46,8 +46,9 @@ private:
 
   // Window Clients
   VisibilityState mVisibilityState;
-  bool mFocused;
   FrameType mFrameType;
+  TimeStamp mLastFocusTime;
+  bool mFocused;
 };
 
 class ServiceWorkerClient : public nsISupports,
