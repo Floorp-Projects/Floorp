d7b6a3ff79d550b848c91318f6b808b3f28e78bc
Daosheng Mu <daoshengmu@gmail.com>
2017-03-02 00:00:23 +0800

    Bug 1323328 - Part 3: Add VR display request present mochitest; r=kip
    MozReview-Commit-ID: 13WuFhUFcTo
    
    --HG--
    extra : rebase_source : 00d5c0f36f3550c19367c4a9b6882c10175a0862

---
+++ dom/vr/test/requestPresent.js
@@ -0,0 +1,34 @@
+// requestPresent.js
+//
+// This file provides helpers for testing VRDisplay requestPresent.
+
+function setupVRDisplay(test) {
+  assert_equals(typeof (navigator.getVRDisplays), "function", "'navigator.getVRDisplays()' must be defined.");
+  return VRSimulationDriver.AttachWebVRDisplay().then(() => {
+    return navigator.getVRDisplays();
+  }).then((displays) => {
+    assert_equals(displays.length, 1, "displays.length must be one after attach.");
+    vrDisplay = displays[0];
+    return validateNewVRDisplay(test, vrDisplay);
+  });
+}
+
+// Validate the settings off a freshly created VRDisplay (prior to calling
+// requestPresent).
+function validateNewVRDisplay(test, display) {
+  assert_true(display.capabilities.canPresent, "display.capabilities.canPresent must always be true for HMDs.");
+  assert_equals(display.capabilities.maxLayers, 1, "display.capabilities.maxLayers must always be 1 when display.capabilities.canPresent is true for current spec revision.");
+  assert_false(display.isPresenting, "display.isPresenting must be false before calling requestPresent.");
+  assert_equals(display.getLayers().length, 0, "display.getLayers() should have no layers if not presenting.");
+  var promise = display.exitPresent();
+  return promise_rejects(test, null, promise);
+}
+
+// Validate the settings off a VRDisplay after requestPresent promise is
+// rejected or after exitPresent is fulfilled.
+function validateDisplayNotPresenting(test, display) {
+  assert_false(display.isPresenting, "display.isPresenting must be false if requestPresent is rejected or after exitPresent is fulfilled.");
+  assert_equals(display.getLayers().length, 0, "display.getLayers() should have no layers if requestPresent is rejected  or after exitPresent is fulfilled.");
+  var promise = display.exitPresent();
+  return promise_rejects(test, null, promise);
+}
