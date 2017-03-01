4d87f0665b0007144261ef3f0393de0957b7b9be
Christoph Diehl <cdiehl@mozilla.com>
2017-02-28 16:24:43 -0800

    Bug 777600 - Add MessageManager Fuzzer. r=billm
    --HG--
    extra : rebase_source : 2e37fe0109e45216820543512dfbe58f1fba15fe

---
+++ tools/fuzzing/common/FuzzingTraits.cpp
@@ -0,0 +1,28 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "FuzzingTraits.h"
+
+namespace mozilla {
+namespace fuzzing {
+
+/* static */
+unsigned int
+FuzzingTraits::Random(unsigned int aMax)
+{
+  MOZ_ASSERT(aMax > 0, "aMax needs to be bigger than 0");
+  return static_cast<unsigned int>(random() % aMax);
+}
+
+/* static */
+bool
+FuzzingTraits::Sometimes(unsigned int aProbability)
+{
+  return FuzzingTraits::Random(aProbability) == 0;
+}
+
+} // namespace fuzzing
+} // namespace mozilla
