4a18d31d122b2a239341989116c6179fedd9e0d7
Jonathan Watt <jwatt@jwatt.org>
2017-03-01 10:41:14 +0000

    Bug 1058040, part 10b - Account for the SVGContextPaint member in SVGImageContext::operator==. r=dholbert
    MozReview-Commit-ID: KqP0al5v5mB

--- layout/svg/SVGImageContext.h
+++ layout/svg/SVGImageContext.h
@@ -79,7 +79,7 @@ public:
     return mGlobalOpacity;
   }
 
-  const SVGContextPaint* GetContextPaint() const {
+  const SVGEmbeddingContextPaint* GetContextPaint() const {
     return mContextPaint.get();
   }
 
@@ -88,7 +88,15 @@ public:
   }
 
   bool operator==(const SVGImageContext& aOther) const {
-    return mViewportSize == aOther.mViewportSize &&
+    bool contextPaintIsEqual =
+      // neither have context paint, or they have the same object:
+      (mContextPaint == aOther.mContextPaint) ||
+      // or both have context paint that are different but equivalent objects:
+      (mContextPaint && aOther.mContextPaint &&
+       *mContextPaint == *aOther.mContextPaint);
+
+    return contextPaintIsEqual &&
+           mViewportSize == aOther.mViewportSize &&
            mPreserveAspectRatio == aOther.mPreserveAspectRatio &&
            mGlobalOpacity == aOther.mGlobalOpacity &&
            mIsPaintingSVGImageElement == aOther.mIsPaintingSVGImageElement;
@@ -119,7 +127,7 @@ private:
   }
 
   // NOTE: When adding new member-vars, remember to update Hash() & operator==.
-  RefPtr<SVGContextPaint>       mContextPaint;
+  RefPtr<SVGEmbeddingContextPaint> mContextPaint;
   Maybe<CSSIntSize>             mViewportSize;
   Maybe<SVGPreserveAspectRatio> mPreserveAspectRatio;
   gfxFloat                      mGlobalOpacity;
