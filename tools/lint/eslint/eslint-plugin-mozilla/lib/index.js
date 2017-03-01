fb13a772a18c870ac6795ec5d96894ed561ff9d6
Mark Banner <standard8@mozilla.com>
2017-03-01 21:34:24 +0000

    Bug 1343519 - Change the ESLint rule 'import-test-globals' to be an environment (mozilla/simpletest) to better describe the purpose of it. r=mossop
    MozReview-Commit-ID: DbrsnUVrAas
    
    --HG--
    rename : tools/lint/eslint/eslint-plugin-mozilla/lib/rules/import-test-globals.js => tools/lint/eslint/eslint-plugin-mozilla/lib/environments/simpletest.js
    extra : rebase_source : 97d0537c347999159c82d4757b265f580ec981eb

--- tools/lint/eslint/eslint-plugin-mozilla/lib/index.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/index.js
@@ -16,6 +16,7 @@ module.exports = {
     "browser-window": require("../lib/environments/browser-window.js"),
     "chrome-worker": require("../lib/environments/chrome-worker.js"),
     "frame-script": require("../lib/environments/frame-script.js"),
+    "simpletest": require("../lib/environments/simpletest.js"),
   },
   processors: {
     ".xml": require("../lib/processors/xbl-bindings"),
@@ -26,7 +27,6 @@ module.exports = {
     "balanced-listeners": require("../lib/rules/balanced-listeners"),
     "import-globals": require("../lib/rules/import-globals"),
     "import-headjs-globals": require("../lib/rules/import-headjs-globals"),
-    "import-test-globals": require("../lib/rules/import-test-globals"),
     "mark-test-function-used": require("../lib/rules/mark-test-function-used"),
     "no-aArgs": require("../lib/rules/no-aArgs"),
     "no-cpows-in-tests": require("../lib/rules/no-cpows-in-tests"),
@@ -44,7 +44,6 @@ module.exports = {
     "balanced-listeners": 0,
     "import-globals": 0,
     "import-headjs-globals": 0,
-    "import-test-globals": 0,
     "mark-test-function-used": 0,
     "no-aArgs": 0,
     "no-cpows-in-tests": 0,
