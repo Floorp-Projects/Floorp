f47d230857869b8c38ee798ec844eb1b1e9f1643
Randell Jesup <rjesup@jesup.org>
2017-02-28 23:31:47 -0500

    Bug 1342732: Adjust ANY11P16 r=sotaro
    MozReview-Commit-ID: ARnj1K5o2LM

--- media/libyuv/update.py
+++ media/libyuv/update.py
@@ -39,6 +39,10 @@ def apply_patches():
     os.system("patch -p3 < add_H444ToARGB.patch")
     # Patch for bug 1342730
     os.system("patch -p3 < cpu_id.patch")
+    # Patch for bug 1342730
+    os.system("patch -p3 < cpu_id.patch")
+    # Patch for bug 1342732
+    os.system("patch -p3 < row_any.patch")
 
 def update_readme(commit):
     with open('README_MOZILLA') as f:
