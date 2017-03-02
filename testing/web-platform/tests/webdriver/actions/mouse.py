9bbfaf9654809cc59e9f96f686642d44d9dd0b81
Maja Frydrychowicz <mjzffr@gmail.com>
2017-03-01 23:36:37 -0500

    Bug 1337133 - Add a wdspec test for mouse actions. r=ato, a=test-only
    Proof of concept to be replaced with more tests in a
    follow-up.
    
    MozReview-Commit-ID: 8FHJOgpO1wr

--- testing/web-platform/tests/webdriver/actions/mouse.py
+++ testing/web-platform/tests/webdriver/actions/mouse.py
@@ -1,8 +1,31 @@
-from support.refine import get_events
+from support.refine import get_events, filter_dict
 
 
-def test_nothing(session, test_actions_page, mouse_chain):
+def test_click_at_coordinates(session, test_actions_page, mouse_chain):
+    div_point = {
+        "x": 82,
+        "y": 187,
+    }
+    button = 0
     mouse_chain \
-        .pointer_down(0) \
+        .pointer_move(div_point["x"], div_point["y"], duration=1000) \
+        .pointer_down(button) \
+        .pointer_up(button) \
         .perform()
-    assert True
+    events = get_events(session)
+    assert len(events) == 4
+    for e in events:
+        if e["type"] != "mousemove":
+            assert e["pageX"] == div_point["x"]
+            assert e["pageY"] == div_point["y"]
+            assert e["target"] == "outer"
+        if e["type"] != "mousedown":
+            assert e["buttons"] == 0
+        assert e["button"] == button
+    expected = [
+        {"type": "mousedown", "buttons": 1},
+        {"type": "mouseup",  "buttons": 0},
+        {"type": "click", "buttons": 0},
+    ]
+    filtered_events = [filter_dict(e, expected[0]) for e in events]
+    assert expected == filtered_events[1:]
