a233140e6075c731a01b84e1b6819aa45259704b
Kartikaya Gupta <kgupta@mozilla.com>
2017-03-02 14:17:19 -0500

    Bug 1264494 - Turn on Linux64 debug static analysis builds on Taskcluster. r=dustin,kmoir
    MozReview-Commit-ID: KXimjewdpz4

--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_stat_and_debug.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_stat_and_debug.py
@@ -1,36 +1,46 @@
 import os
 
-MOZ_OBJDIR = 'obj-firefox'
-
 config = {
+    # note: overridden by MOZHARNESS_ACTIONS in TaskCluster tasks
     'default_actions': [
         'clobber',
         'clone-tools',
         'checkout-sources',
         'setup-mock',
         'build',
-        'upload-files',
-        'sendchange',
         # 'generate-build-stats',
-        'update',  # decided by query_is_nightly()
     ],
-    'debug_build': True,
-    'stage_platform': 'linux64-st-an-debug',
-    'build_type': 'st-an-debug',
-    'tooltool_manifest_src': "browser/config/tooltool-manifests/linux64/\
-clang.manifest",
-    'platform_supports_post_upload_to_latest': False,
+    "buildbot_json_path": "buildprops.json",
+    'exes': {
+        "buildbot": "/tools/buildbot/bin/buildbot",
+    },
+    'app_ini_path': '%(obj_dir)s/dist/bin/application.ini',
+    # decides whether we want to use moz_sign_cmd in env
     'enable_signing': False,
+    'enable_ccache': True,
+    'vcs_share_base': '/builds/hg-shared',
+    'objdir': 'obj-firefox',
+    'tooltool_script': ["/builds/tooltool.py"],
+    'tooltool_bootstrap': "setup.sh",
+    'enable_count_ctors': True,
     'enable_talos_sendchange': False,
     'enable_unittest_sendchange': False,
     'perfherder_extra_options': ['static-analysis'],
-    #### 64 bit build specific #####
+    #########################################################################
+
+
+    #########################################################################
+    ###### 64 bit specific ######
+    'base_name': 'Linux_x86-64_%(branch)s_Static_Analysis',
+    'platform': 'linux64',
+    'stage_platform': 'linux64-st-an-opt',
+    'publish_nightly_en_US_routes': False,
     'env': {
         'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
         'MOZ_AUTOMATION': '1',
         'DISPLAY': ':2',
         'HG_SHARE_BASE_DIR': '/builds/hg-shared',
-        'MOZ_OBJDIR': MOZ_OBJDIR,
+        'MOZ_OBJDIR': 'obj-firefox',
         'TINDERBOX_OUTPUT': '1',
         'TOOLTOOL_CACHE': '/builds/tooltool_cache',
         'TOOLTOOL_HOME': '/builds',
@@ -39,13 +49,41 @@ clang.manifest",
         'CCACHE_COMPRESS': '1',
         'CCACHE_UMASK': '002',
         'LC_ALL': 'C',
-        'XPCOM_DEBUG_BREAK': 'stack-and-abort',
-        # 64 bit specific
+        ## 64 bit specific
         'PATH': '/tools/buildbot/bin:/usr/local/bin:/usr/lib64/ccache:/bin:\
 /usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:/tools/python27/bin:\
 /tools/python27-mercurial/bin:/home/cltbld/bin',
+        'LD_LIBRARY_PATH': "/tools/gcc-4.3.3/installed/lib64",
+        ##
     },
-    'src_mozconfig': 'browser/config/mozconfigs/linux64/\
-debug-static-analysis-clang',
+    'mock_packages': [
+        'autoconf213', 'python', 'mozilla-python27', 'zip', 'mozilla-python27-mercurial',
+        'git', 'ccache', 'perl-Test-Simple', 'perl-Config-General',
+        'yasm', 'wget',
+        'mpfr',  # required for system compiler
+        'xorg-x11-font*',  # fonts required for PGO
+        'imake',  # required for makedepend!?!
+        ### <-- from releng repo
+        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'gcc473_0moz1',
+        'yasm', 'ccache',
+        ###
+        'valgrind', 'dbus-x11',
+        ######## 64 bit specific ###########
+        'glibc-static', 'libstdc++-static',
+        'gtk2-devel', 'libnotify-devel',
+        'alsa-lib-devel', 'libcurl-devel', 'wireless-tools-devel',
+        'libX11-devel', 'libXt-devel', 'mesa-libGL-devel', 'gnome-vfs2-devel',
+        'GConf2-devel',
+        ### from releng repo
+        'gcc45_0moz3', 'gcc454_0moz1', 'gcc472_0moz1', 'gcc473_0moz1',
+        'yasm', 'ccache',
+        ###
+        'pulseaudio-libs-devel', 'gstreamer-devel',
+        'gstreamer-plugins-base-devel', 'freetype-2.3.11-6.el6_1.8.x86_64',
+        'freetype-devel-2.3.11-6.el6_1.8.x86_64'
+    ],
+    'src_mozconfig': 'browser/config/mozconfigs/linux64/debug-static-analysis-clang',
+    'tooltool_manifest_src': 'browser/config/tooltool-manifests/linux64/\
+clang.manifest.centos6',
     #######################
 }
