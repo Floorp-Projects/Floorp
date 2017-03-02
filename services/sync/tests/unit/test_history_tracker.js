85e2d02ed0de6f78a948abb1c804c69389374875
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-03-02 13:08:42 +0100

    Backed out changeset e91a6d043fc6 (bug 1317223)

--- services/sync/tests/unit/test_history_tracker.js
+++ services/sync/tests/unit/test_history_tracker.js
@@ -74,21 +74,22 @@ function run_test() {
 
 async function verifyTrackerEmpty() {
   let changes = engine.pullNewChanges();
-  do_check_empty(changes);
+  equal(changes.count(), 0);
   equal(tracker.score, 0);
 }
 
 async function verifyTrackedCount(expected) {
   let changes = engine.pullNewChanges();
-  do_check_attribute_count(changes, expected);
+  equal(changes.count(), expected);
 }
 
 async function verifyTrackedItems(tracked) {
   let changes = engine.pullNewChanges();
-  let trackedIDs = new Set(Object.keys(changes));
+  let trackedIDs = new Set(changes.ids());
   for (let guid of tracked) {
-    ok(guid in changes, `${guid} should be tracked`);
-    ok(changes[guid] > 0, `${guid} should have a modified time`);
+    ok(changes.has(guid), `${guid} should be tracked`);
+    ok(changes.getModifiedTimestamp(guid) > 0,
+      `${guid} should have a modified time`);
     trackedIDs.delete(guid);
   }
   equal(trackedIDs.size, 0, `Unhandled tracked IDs: ${
