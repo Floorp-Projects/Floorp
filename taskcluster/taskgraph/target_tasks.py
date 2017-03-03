6b150a9801a1e5847780bcd3cf9a2f3b5affec1e
Aki Sasaki <asasaki@mozilla.com>
2017-03-02 18:13:18 -0800

    bug 1337824 - stop beta linux l10n on push. r=rail a=release
    MozReview-Commit-ID: DeWTzloCviv

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -212,7 +212,7 @@ def target_tasks_mozilla_beta(full_task_graph, parameters):
         if platform in ('linux64-nightly', 'linux-nightly'):
             if task.kind not in [
                 'balrog', 'beetmover', 'beetmover-checksums', 'beetmover-l10n',
-                'checksums-signing',
+                'checksums-signing', 'nightly-l10n', 'nightly-l10n-signing'
             ]:
                 return task.attributes.get('nightly', False)
     return [l for l, t in full_task_graph.tasks.iteritems() if filter(t)]
