e9e59c798ec665ec2088d6158f02a4e0b8dabbaf
Mike Conley <mconley@mozilla.com>
2017-03-03 18:36:12 -0500

    Bug 1340661 - Manually draw checkbox and radio frames on Android. r=snorp,tnikkel
    MozReview-Commit-ID: 8IiaRZNJs16

---
+++ widget/android/AndroidColors.h
@@ -0,0 +1,15 @@
+#ifndef mozilla_widget_AndroidColors_h
+#define mozilla_widget_AndroidColors_h
+
+#include "mozilla/gfx/2D.h"
+
+namespace mozilla {
+namespace widget {
+
+static const Color sAndroidBorderColor(Color(0.73f, 0.73f, 0.73f));
+static const Color sAndroidCheckColor(Color(0.19f, 0.21f, 0.23f));
+
+} // namespace widget
+} // namespace mozilla
+
+#endif
