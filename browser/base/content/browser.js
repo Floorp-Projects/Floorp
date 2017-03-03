9dcbda1e525d19e62b570d2e0ef359f12d94300f
Kevin Jones <kevinhowjones@gmail.com>
2017-03-03 18:58:27 +0100

    Bug 1344212 - Create lazy browser only if the addTab caller opts in. r=dao

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5079,7 +5079,6 @@ nsBrowserAccess.prototype = {
                                       fromExternal: aIsExternal,
                                       inBackground: loadInBackground,
                                       forceNotRemote: aForceNotRemote,
-                                      forceBrowserInsertion: true,
                                       opener: aOpener,
                                       });
     let browser = win.gBrowser.getBrowserForTab(tab);
