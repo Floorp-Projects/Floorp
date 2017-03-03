284045b9c2d7c54dcea7984d55b6dd1524196985
Chris Pearce <cpearce@mozilla.com>
2017-03-03 14:10:28 +1300

    Bug 1343140 - Output a black frame if Widevine CDM refuses to decode due to losing a key. r=gerald
    MozReview-Commit-ID: JQon5f87FSF

--- dom/media/gmp/widevine-adapter/WidevineVideoFrame.h
+++ dom/media/gmp/widevine-adapter/WidevineVideoFrame.h
@@ -36,6 +36,8 @@ public:
   void SetTimestamp(int64_t aTimestamp) override;
   int64_t Timestamp() const override;
 
+  void InitToBlack(uint32_t aWidth, uint32_t aHeight, int64_t aTimeStamp);
+
 protected:
   cdm::VideoFormat mFormat;
   cdm::Size mSize;
