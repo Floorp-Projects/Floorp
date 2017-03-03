20162012f3fea3dfe9551d58a72e336906b1c329
Mike Conley <mconley@mozilla.com>
2017-03-03 18:36:12 -0500

    Bug 1340661 - Manually draw checkbox and radio frames on Android. r=snorp,tnikkel
    MozReview-Commit-ID: 8IiaRZNJs16

--- layout/forms/nsGfxRadioControlFrame.cpp
+++ layout/forms/nsGfxRadioControlFrame.cpp
@@ -40,3 +40,88 @@ nsGfxRadioControlFrame::AccessibleType()
   return a11y::eHTMLRadioButtonType;
 }
 #endif
+
+#ifdef ANDROID
+
+#include "mozilla/widget/AndroidColors.h"
+
+static void
+PaintRadioBorder(nsIFrame* aFrame,
+                 DrawTarget* aDrawTarget,
+                 const nsRect& aDirtyRect,
+                 nsPoint aPt)
+{
+  nsRect rect(aPt, aFrame->GetSize());
+  rect.Deflate(aFrame->GetUsedBorderAndPadding());
+
+  Rect devPxRect =
+    ToRect(nsLayoutUtils::RectToGfxRect(rect,
+                                        aFrame->PresContext()->AppUnitsPerDevPixel()));
+  // Radio controls aren't something that we can render on Android
+  // natively. We fake native drawing of appearance: radio items
+  // out here, and use hardcoded colours to simulate native
+  // theming.
+  RefPtr<PathBuilder> builder = aDrawTarget->CreatePathBuilder();
+  AppendEllipseToPath(builder, devPxRect.Center(), devPxRect.Size());
+  RefPtr<Path> ellipse = builder->Finish();
+  aDrawTarget->Stroke(ellipse, mozilla::widget::sAndroidBorderColor);
+}
+
+//--------------------------------------------------------------
+// Draw the dot for a non-native radio button in the checked state.
+static void
+PaintCheckedRadioButton(nsIFrame* aFrame,
+                        DrawTarget* aDrawTarget,
+                        const nsRect& aDirtyRect,
+                        nsPoint aPt)
+{
+  // The dot is an ellipse 2px on all sides smaller than the content-box,
+  // drawn in the foreground color.
+  nsRect rect(aPt, aFrame->GetSize());
+  rect.Deflate(aFrame->GetUsedBorderAndPadding());
+  rect.Deflate(nsPresContext::CSSPixelsToAppUnits(2),
+               nsPresContext::CSSPixelsToAppUnits(2));
+
+  Rect devPxRect =
+    ToRect(nsLayoutUtils::RectToGfxRect(rect,
+                                        aFrame->PresContext()->AppUnitsPerDevPixel()));
+
+  RefPtr<PathBuilder> builder = aDrawTarget->CreatePathBuilder();
+  AppendEllipseToPath(builder, devPxRect.Center(), devPxRect.Size());
+  RefPtr<Path> ellipse = builder->Finish();
+  aDrawTarget->Fill(ellipse, mozilla::widget::sAndroidCheckColor);
+}
+
+void
+nsGfxRadioControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
+                                         const nsRect&           aDirtyRect,
+                                         const nsDisplayListSet& aLists)
+{
+  nsFormControlFrame::BuildDisplayList(aBuilder, aDirtyRect, aLists);
+
+  if (!IsVisibleForPainting(aBuilder)) {
+    return;
+  }
+
+  if (IsThemed()) {
+    return; // The theme will paint the check, if any.
+  }
+
+  if (StyleDisplay()->mAppearance != NS_THEME_RADIO) {
+    return;
+  }
+
+  aLists.Content()->AppendNewToTop(new (aBuilder)
+    nsDisplayGeneric(aBuilder, this, PaintRadioBorder,
+                     "RadioBorder", nsDisplayItem::TYPE_RADIOBUTTON_BORDER));
+
+  bool checked = true;
+  GetCurrentCheckState(&checked); // Get check state from the content model
+  if (checked) {
+    aLists.Content()->AppendNewToTop(new (aBuilder)
+      nsDisplayGeneric(aBuilder, this, PaintCheckedRadioButton,
+                       "CheckedRadioButton",
+                       nsDisplayItem::TYPE_CHECKED_RADIOBUTTON));
+  }
+}
+#endif
