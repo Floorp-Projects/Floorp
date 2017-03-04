c2dbcbf9b9e1e0024eaa8670737fec4316dfbdde
Jon Coppeard <jcoppeard@mozilla.com>
2017-03-04 08:59:42 +0000

    Bug 1259730 - Make UnmarkGray use an explicit mark stack instead of recursion r=sfink

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -1046,6 +1046,8 @@ class GCRuntime
 
     GCMarker marker;
 
+    Vector<JS::GCCellPtr, 0, SystemAllocPolicy> unmarkGrayStack;
+
     /* Track heap usage for this runtime. */
     HeapUsage usage;
 
