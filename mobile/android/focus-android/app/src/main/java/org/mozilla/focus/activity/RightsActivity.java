5600dc7c0630d746e64ea6879c487d226260b2d2
Luigi Tedone <luigitedone@gmail.com>
2017-03-05 16:12:49 +0100

    [focus] Add 'Your Rights' screen (https://github.com/mozilla-mobile/focus-android/issues/23)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/RightsActivity.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/RightsActivity.java
@@ -1,6 +1,34 @@
 package org.mozilla.focus.activity;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
 
-public class RightsActivity extends Activity {
+import org.mozilla.focus.R;
+
+public class RightsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_rights);
+        WebView mWebView = null;
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(false);
+        mWebView.loadUrl("file:///android_asset/rights-focus.html");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
 }
