74ef9a167cc6df1991dcea18e069ae4657ca1057
Ya-Chieh Wu <ywu@mozilla.com>
2017-03-05 22:07:00 +0100

    Bug 1340472 - Add a way to unregister the plugin in mortar-pdf's bootstrap. r=ehung.

--- browser/extensions/mortar/host/pdf/bootstrap.js
+++ browser/extensions/mortar/host/pdf/bootstrap.js
@@ -12,58 +12,67 @@ function sandboxScript(sandbox)
   Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader).loadSubScript("resource://ppapipdf.js/ppapi-content-sandbox.js", sandbox);
 }
 
-let plugins;
-function startup(data) {
-  dump(">>>STARTED!!!\n");
+const handlerURI = "chrome://ppapipdf.js/content/viewer.html";
+let pdfium = {
+  init(bootstrapData) {
+    let root = bootstrapData.installPath.parent.parent;
+    let rpclib = root.clone();
+    let pluginlib = root.clone();
+    let os = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime).OS;
+    if (os == "Darwin") {
+      rpclib.appendRelativePath("ppapi/out/rpc.dylib");
+      pluginlib.appendRelativePath("plugin/libpepperpdfium.dylib");
+    } else if (os == "Linux") {
+      rpclib.appendRelativePath("ppapi/out/rpc.so");
+      pluginlib.appendRelativePath("plugin/libpepperpdfium.so");
+    } else if (os == "WINNT") {
+      rpclib.appendRelativePath("ppapi\\out\\rpc.dll");
+      pluginlib.appendRelativePath("plugin\\pepperpdfium.dll");
+    } else {
+      throw("Don't know the path to the libraries for this OS!");
+    }
+    rpclib = rpclib.path;
+    pluginlib = pluginlib.path;
 
-  let root = data.installPath.parent.parent;
-  let rpclib = root.clone();
-  let pluginlib = root.clone();
-  let os = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime).OS;
-  if (os == "Darwin") {
-    rpclib.appendRelativePath("ppapi/out/rpc.dylib");
-    pluginlib.appendRelativePath("plugin/libpepperpdfium.dylib");
-  } else if (os == "Linux") {
-    rpclib.appendRelativePath("ppapi/out/rpc.so");
-    pluginlib.appendRelativePath("plugin/libpepperpdfium.so");
-  } else if (os == "WINNT") {
-    rpclib.appendRelativePath("ppapi\\out\\rpc.dll");
-    pluginlib.appendRelativePath("plugin\\pepperpdfium.dll");
-  } else {
-    throw("Don't know the path to the libraries for this OS!");
-  }
-  rpclib = rpclib.path;
-  pluginlib = pluginlib.path;
-
-  let pluginHost = Cc["@mozilla.org/plugin/host;1"].getService(Ci.nsIPluginHost);
-  let plugin = pluginHost.registerFakePlugin({
-    handlerURI: "chrome://ppapipdf.js/content/viewer.html",
-    mimeEntries: [
-      { type: "application/pdf", extension: "pdf" },
-      { type: "application/vnd.adobe.pdf", extension: "pdf" },
-      { type: "application/vnd.adobe.pdfxml", extension: "pdfxml" },
-      { type: "application/vnd.adobe.x-mars", extension: "mars" },
-      { type: "application/vnd.adobe.xdp+xml", extension: "xdp" },
-      { type: "application/vnd.adobe.xfdf", extension: "xfdf" },
-      { type: "application/vnd.adobe.xfd+xml", extension: "xfd" },
-      { type: "application/vnd.fdf", extension: "fdf" },
-    ],
-    name: "PPAPI PDF plugin",
-    niceName: "PPAPI PDF plugin",
-    version: "1.0",
-    sandboxScript : `(${sandboxScript.toSource()})(this);`,
-    ppapiProcessArgs: [ rpclib, pluginlib ],
-  });
-  plugin.enabledState = Ci.nsIPluginTag.STATE_ENABLED;
-
-  let rng = Cc["@mozilla.org/security/random-generator;1"].createInstance(Ci.nsIRandomGenerator);
-  Services.ppmm.addMessageListener("ppapi.js:generateRandomBytes", ({ data }) => {
+    let pluginHost = Cc["@mozilla.org/plugin/host;1"].getService(Ci.nsIPluginHost);
+    let plugin = pluginHost.registerFakePlugin({
+      handlerURI: handlerURI,
+      mimeEntries: [
+        { type: "application/pdf", extension: "pdf" },
+        { type: "application/vnd.adobe.pdf", extension: "pdf" },
+        { type: "application/vnd.adobe.pdfxml", extension: "pdfxml" },
+        { type: "application/vnd.adobe.x-mars", extension: "mars" },
+        { type: "application/vnd.adobe.xdp+xml", extension: "xdp" },
+        { type: "application/vnd.adobe.xfdf", extension: "xfdf" },
+        { type: "application/vnd.adobe.xfd+xml", extension: "xfd" },
+        { type: "application/vnd.fdf", extension: "fdf" },
+      ],
+      name: "PPAPI PDF plugin",
+      niceName: "PPAPI PDF plugin",
+      version: "1.0",
+      sandboxScript : `(${sandboxScript.toSource()})(this);`,
+      ppapiProcessArgs: [ rpclib, pluginlib ],
+    });
+    plugin.enabledState = Ci.nsIPluginTag.STATE_ENABLED;
+    Services.ppmm.addMessageListener("ppapi.js:generateRandomBytes", this.generateRandomBytesListener);
+  },
+  uninit() {
+    let pluginHost = Cc["@mozilla.org/plugin/host;1"].getService(Ci.nsIPluginHost);
+    pluginHost.unregisterFakePlugin(handlerURI);
+    Services.ppmm.removeMessageListener("ppapi.js:generateRandomBytes", this.generateRandomBytesListener);
+  },
+  generateRandomBytesListener(data) {
+    let rng = Cc["@mozilla.org/security/random-generator;1"].createInstance(Ci.nsIRandomGenerator);
     return rng.generateRandomBytes(data);
-  });
-
-  dump("<<<STARTED!!!\n");
+  },
 }
 
+function startup(aData) {
+  dump(">>>STARTED!!!\n");
+  pdfium.init(aData);
+  dump("<<<STARTED!!!\n");
+}
 function shutdown() {
   dump("SHUTDOWN!!!\n");
+  pdfium.uninit();
 }
