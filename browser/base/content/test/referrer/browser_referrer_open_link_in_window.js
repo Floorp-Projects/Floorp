512146e2ff3fbf97a92f1d5de9932c204847e30f
Wes Kocher <wkocher@mozilla.com>
2017-03-06 15:30:03 -0800

    Backed out changeset ba253783b432 (bug 1335801) for browser_referrer_simple_click.js failures a=backout CLOSED TREE
    MozReview-Commit-ID: 5jn0Okch3jt

--- browser/base/content/test/referrer/browser_referrer_open_link_in_window.js
+++ browser/base/content/test/referrer/browser_referrer_open_link_in_window.js
@@ -6,7 +6,7 @@ function startNewWindowTestCase(aTestNumber) {
        getReferrerTestDescription(aTestNumber));
   contextMenuOpened(gTestWindow, "testlink").then(function(aContextMenu) {
     newWindowOpened().then(function(aNewWindow) {
-      BrowserTestUtils.firstBrowserLoaded(aNewWindow, false).then(function() {
+      someTabLoaded(aNewWindow).then(function() {
         checkReferrerAndStartNextTest(aTestNumber, aNewWindow, null,
                                       startNewWindowTestCase);
       });
