7abe2149d3888a2412b5967663d7ab32bc5603fd
JW Wang <jwwang@mozilla.com>
2017-03-06 10:36:12 +0800

    Bug 1343787 - Allow MediaShutdownManager::Register() to fail. r=gerald, a=lizzard
    Also move the Register() calls to Load().

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -461,7 +461,6 @@ MediaDecoder::MediaDecoder(MediaDecoderOwner* aOwner)
   mWatchManager.Watch(mIsAudioDataAudible, &MediaDecoder::NotifyAudibleStateChanged);
 
   MediaShutdownManager::InitStatics();
-  MediaShutdownManager::Instance().Register(this);
 }
 
 #undef INIT_MIRROR
@@ -629,7 +628,12 @@ MediaDecoder::Load(nsIStreamListener** aStreamListener)
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(mResource, "Can't load without a MediaResource");
 
-  nsresult rv = OpenResource(aStreamListener);
+  nsresult rv = MediaShutdownManager::Instance().Register(this);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
+  }
+
+  rv = OpenResource(aStreamListener);
   NS_ENSURE_SUCCESS(rv, rv);
 
   SetStateMachine(CreateStateMachine());
