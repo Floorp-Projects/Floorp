0127cd03e2521ccff3811497a21bed44c7f04907
Timothy Guan-tin Chien <timdream@gmail.com>
2017-03-06 16:31:47 +0800

    Bug 1344431 - Tell parent the non-reader-able reader page is not readable, r=Gijs
    Although regression window testing pin this to bug 1260276, I believe
    this is a regression from bug 1126967. Bug 1260276 just make it more
    visible because we stop automatically redirect users to the original
    page.
    
    This patch fix the bug by checking if the current page is in readerable
    state (i.e. not error state), and send the message accordingly.
    
    MozReview-Commit-ID: B5UJcPvVlAc
    
    --HG--
    extra : rebase_source : 630347e1f4256550857d84bc6e8a30036b114362

--- mobile/android/chrome/content/content.js
+++ mobile/android/chrome/content/content.js
@@ -22,7 +22,7 @@ var AboutReaderListener = {
 
   _articlePromise: null,
 
-  _isLeavingReaderMode: false,
+  _isLeavingReaderableReaderMode: false,
 
   init: function() {
     addEventListener("AboutReaderContentLoaded", this, false, true);
@@ -41,7 +41,7 @@ var AboutReaderListener = {
           this._articlePromise = ReaderMode.parseDocument(content.document).catch(Cu.reportError);
           ReaderMode.enterReaderMode(docShell, content);
         } else {
-          this._isLeavingReaderMode = true;
+          this._isLeavingReaderableReaderMode = this.isReaderableAboutReader;
           ReaderMode.leaveReaderMode(docShell, content);
         }
         break;
@@ -54,6 +54,11 @@ var AboutReaderListener = {
 
   get isAboutReader() {
     return content.document.documentURI.startsWith("about:reader");
+  },
+
+  get isReaderableAboutReader() {
+    return this.isAboutReader &&
+      !content.document.documentElement.dataset.isError;
   },
 
   get isErrorPage() {
@@ -84,12 +89,12 @@ var AboutReaderListener = {
         break;
 
       case "pagehide":
-        // this._isLeavingReaderMode is used here to keep the Reader Mode icon
+        // this._isLeavingReaderableReaderMode is used here to keep the Reader Mode icon
         // visible in the location bar when transitioning from reader-mode page
         // back to the source page.
-        sendAsyncMessage("Reader:UpdateReaderButton", { isArticle: this._isLeavingReaderMode });
-        if (this._isLeavingReaderMode) {
-          this._isLeavingReaderMode = false;
+        sendAsyncMessage("Reader:UpdateReaderButton", { isArticle: this._isLeavingReaderableReaderMode });
+        if (this._isLeavingReaderableReaderMode) {
+          this._isLeavingReaderableReaderMode = false;
         }
         break;
 
