ac2d7da8fbb37d1fc78eac479720e0bdbc24d56d
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-03-06 11:03:46 +0900

    Bug 1344596 - Allow OSPreferences API without ENABLE_INTL_API. r=gandalf a=gchang
    Since we have to keep --with-intl-api=no setting, we allow OSPrefrences API wiout ENABLE_INTL_API.
    
    MozReview-Commit-ID: 8sQHW6iq9Ss

--- intl/locale/unix/OSPreferences_unix.cpp
+++ intl/locale/unix/OSPreferences_unix.cpp
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsPosixLocale.h"
 #include "OSPreferences.h"
 
 using namespace mozilla::intl;
@@ -11,6 +12,7 @@ using namespace mozilla::intl;
 bool
 OSPreferences::ReadSystemLocales(nsTArray<nsCString>& aLocaleList)
 {
+#ifdef ENABLE_INTL_API
   MOZ_ASSERT(aLocaleList.IsEmpty());
 
   nsAutoCString defaultLang(uloc_getDefault());
@@ -20,6 +22,15 @@ OSPreferences::ReadSystemLocales(nsTArray<nsCString>& aLocaleList)
     return true;
   }
   return false;
+#else
+  nsAutoString locale;
+  nsPosixLocale::GetXPLocale(getenv("LANG"), locale);
+  if (locale.IsEmpty()) {
+    locale.AssignLiteral("en-US");
+  }
+  aLocaleList.AppendElement(NS_LossyConvertUTF16toASCII(locale));
+  return true;
+#endif
 }
 
 bool
