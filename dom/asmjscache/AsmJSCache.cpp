dcc35b89e65a59f480fa712a1c19216b6ad0b77a
Jan Varga <jan.varga@gmail.com>
2017-03-06 18:38:42 +0100

    Bug 1311057 - Part 2: Remove isApp from quota manager and its clients; r=asuth

--- dom/asmjscache/AsmJSCache.cpp
+++ dom/asmjscache/AsmJSCache.cpp
@@ -351,7 +351,6 @@ public:
     mWriteParams(aWriteParams),
     mState(eInitial),
     mResult(JS::AsmJSCache_InternalError),
-    mIsApp(false),
     mEnforcingQuota(true),
     mDeleteReceived(false),
     mActorDestroyed(false),
@@ -581,7 +580,6 @@ private:
   State mState;
   JS::AsmJSCacheResult mResult;
 
-  bool mIsApp;
   bool mEnforcingQuota;
   bool mDeleteReceived;
   bool mActorDestroyed;
@@ -603,12 +601,11 @@ ParentRunnable::InitOnMainThread()
   }
 
   rv = QuotaManager::GetInfoFromPrincipal(principal, &mSuffix, &mGroup,
-                                          &mOrigin, &mIsApp);
+                                          &mOrigin);
   NS_ENSURE_SUCCESS(rv, rv);
 
   mEnforcingQuota =
-    QuotaManager::IsQuotaEnforced(quota::PERSISTENCE_TYPE_TEMPORARY, mOrigin,
-                                  mIsApp);
+    QuotaManager::IsQuotaEnforced(quota::PERSISTENCE_TYPE_TEMPORARY);
 
   return NS_OK;
 }
@@ -627,7 +624,6 @@ ParentRunnable::OpenDirectory()
   QuotaManager::Get()->OpenDirectory(quota::PERSISTENCE_TYPE_TEMPORARY,
                                      mGroup,
                                      mOrigin,
-                                     mIsApp,
                                      quota::Client::ASMJS,
                                      /* aExclusive */ true,
                                      this);
@@ -644,8 +640,7 @@ ParentRunnable::ReadMetadata()
 
   nsresult rv =
     qm->EnsureOriginIsInitialized(quota::PERSISTENCE_TYPE_TEMPORARY, mSuffix,
-                                  mGroup, mOrigin, mIsApp,
-                                  getter_AddRefs(mDirectory));
+                                  mGroup, mOrigin, getter_AddRefs(mDirectory));
   if (NS_WARN_IF(NS_FAILED(rv))) {
     mResult = JS::AsmJSCache_StorageInitFailure;
     return rv;
