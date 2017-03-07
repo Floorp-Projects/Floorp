5560100540547e5db03d150d133217a242edb62b
Benjamin Smedberg <benjamin@smedbergs.us>
2017-03-07 09:34:03 -0500

    Bug 1335475 - Deny plugins from non-HTTP/HTTPS origins. r=bytesized,qdot
    MozReview-Commit-ID: 3kPeycfMWVw
    
    --HG--
    extra : rebase_source : dc062997ac3a51cd27589c9b0cb29e850676cefc

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5290,6 +5290,7 @@ pref("urlclassifier.flashExceptTable", "testexcept-flash-simple,except-flash-dig
 pref("urlclassifier.flashSubDocTable", "test-flashsubdoc-simple,block-flashsubdoc-digest256");
 pref("urlclassifier.flashSubDocExceptTable", "testexcept-flashsubdoc-simple,except-flashsubdoc-digest256");
 
+pref("plugins.http_https_only", true);
 pref("plugins.flashBlock.enabled", false);
 
 // Allow users to ignore Safe Browsing warnings.
