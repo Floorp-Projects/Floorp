5167a2867def70275e273385da9f2278a07a80ea
Tooru Fujisawa <arai_a@mac.com>
2017-03-07 19:54:23 +0900

    Bug 774744 - Clarify the parameter name or role of the value in some NOT_NONNULL_OBJECT error. r=jwalden

--- js/src/builtin/WeakSetObject.cpp
+++ js/src/builtin/WeakSetObject.cpp
@@ -118,12 +118,7 @@ WeakSetObject::construct(JSContext* cx, unsigned argc, Value* vp)
                 MOZ_ASSERT(!keyVal.isMagic(JS_ELEMENTS_HOLE));
 
                 if (keyVal.isPrimitive()) {
-                    UniqueChars bytes =
-                        DecompileValueGenerator(cx, JSDVG_SEARCH_STACK, keyVal, nullptr);
-                    if (!bytes)
-                        return false;
-                    JS_ReportErrorNumberLatin1(cx, GetErrorMessage, nullptr,
-                                               JSMSG_NOT_NONNULL_OBJECT, bytes.get());
+                    ReportNotObjectWithName(cx, "WeakSet value", keyVal);
                     return false;
                 }
 
