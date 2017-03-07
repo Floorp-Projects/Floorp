ecb328f9defd79eb78e89e38a8d097d1ed97f965
André Bargull <andre.bargull@gmail.com>
2017-03-07 09:28:33 -0800

    Bug 1340583 - Part 3: Extend jstests reftest line to allow to define error types. r=shu
    --HG--
    extra : rebase_source : 19f9cfae17d4c5e16f6919ba40b4b88da659eb58

--- js/src/tests/lib/tests.py
+++ js/src/tests/lib/tests.py
@@ -168,6 +168,7 @@ class RefTestCase(RefTest):
     def __init__(self, path):
         RefTest.__init__(self, path)
         self.enable = True   # bool: True => run test, False => don't run
+        self.error = None    # str?: Optional error type
         self.expect = True   # bool: expected result, True => pass
         self.random = False  # bool: True => ignore output as 'random'
         self.slow = False    # bool: True => test may run slowly
@@ -185,6 +186,8 @@ class RefTestCase(RefTest):
         ans = self.path
         if not self.enable:
             ans += ', skip'
+        if self.error is not None:
+            ans += ', error=' + self.error
         if not self.expect:
             ans += ', fails'
         if self.random:
