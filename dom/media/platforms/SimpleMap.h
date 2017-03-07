39866f9dc8b6adf31fb11aa482e9608cea1cf1b7
John Lin <jolin@mozilla.com>
2017-03-07 19:49:08 +0800

    Bug 1344649 - part 3: rename DurationMap and turn it into a generic class. r=jya a=gchang
    MozReview-Commit-ID: AMMxMPkuYXk

--- dom/media/platforms/SimpleMap.h
+++ dom/media/platforms/SimpleMap.h
@@ -2,37 +2,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_DurationMap_h
-#define mozilla_DurationMap_h
+#ifndef mozilla_SimpleMap_h
+#define mozilla_SimpleMap_h
 
 #include "mozilla/Pair.h"
 #include "nsTArray.h"
 
 namespace mozilla {
 
-class DurationMap
+template<typename T>
+class SimpleMap
 {
 public:
-  typedef Pair<int64_t, int64_t> DurationElement;
+  typedef Pair<int64_t, T> Element;
 
-  DurationMap() : mMutex("DurationMap") { }
+  SimpleMap() : mMutex("SimpleMap") { }
 
-  // Insert Key and Duration pair at the end of our map.
-  void Insert(int64_t aKey, int64_t aDuration)
+  // Insert Key and Value pair at the end of our map.
+  void Insert(int64_t aKey, const T& aValue)
   {
     MutexAutoLock lock(mMutex);
-    mMap.AppendElement(MakePair(aKey, aDuration));
+    mMap.AppendElement(MakePair(aKey, aValue));
   }
-  // Sets aDuration matching aKey and remove it from the map if found.
+  // Sets aValue matching aKey and remove it from the map if found.
   // The element returned is the first one found.
   // Returns true if found, false otherwise.
-  bool Find(int64_t aKey, int64_t& aDuration)
+  bool Find(int64_t aKey, T& aValue)
   {
     MutexAutoLock lock(mMutex);
     for (uint32_t i = 0; i < mMap.Length(); i++) {
-      DurationElement& element = mMap[i];
+      Element& element = mMap[i];
       if (element.first() == aKey) {
-        aDuration = element.second();
+        aValue = element.second();
         mMap.RemoveElementAt(i);
         return true;
       }
@@ -48,9 +49,9 @@ public:
 
 private:
   Mutex mMutex; // To protect mMap.
-  AutoTArray<DurationElement, 16> mMap;
+  AutoTArray<Element, 16> mMap;
 };
 
 } // namespace mozilla
 
-#endif // mozilla_DurationMap_h
+#endif // mozilla_SimpleMap_h
