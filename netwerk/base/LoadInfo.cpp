8c2ad72d08a0f5d7d226daad97e06e23391f6e97
Andrea Marchesini <amarchesini@mozilla.com>
2017-03-08 07:41:20 +0100

    Bug 1345168 - Fix the indentation in LoadInfo.cpp, r=tjr

--- netwerk/base/LoadInfo.cpp
+++ netwerk/base/LoadInfo.cpp
@@ -149,33 +149,33 @@ LoadInfo::LoadInfo(nsIPrincipal* aLoadingPrincipal,
     }
   }
 
-    // If CSP requires SRI (require-sri-for), then store that information
-    // in the loadInfo so we can enforce SRI before loading the subresource.
-    if (!mEnforceSRI) {
-      // do not look into the CSP if already true:
-      // a CSP saying that SRI isn't needed should not
-      // overrule GetVerifySignedContent
-      if (aLoadingPrincipal) {
-        nsCOMPtr<nsIContentSecurityPolicy> csp;
-        aLoadingPrincipal->GetCsp(getter_AddRefs(csp));
-        uint32_t externalType =
-          nsContentUtils::InternalContentPolicyTypeToExternal(aContentPolicyType);
-        // csp could be null if loading principal is system principal
-        if (csp) {
-          csp->RequireSRIForType(externalType, &mEnforceSRI);
-        }
-        // if CSP is delivered via a meta tag, it's speculatively available
-        // as 'preloadCSP'. If we are preloading a script or style, we have
-        // to apply that speculative 'preloadCSP' for such loads.
-        if (!mEnforceSRI && nsContentUtils::IsPreloadType(aContentPolicyType)) {
-          nsCOMPtr<nsIContentSecurityPolicy> preloadCSP;
-          aLoadingPrincipal->GetPreloadCsp(getter_AddRefs(preloadCSP));
-          if (preloadCSP) {
-            preloadCSP->RequireSRIForType(externalType, &mEnforceSRI);
-          }
+  // If CSP requires SRI (require-sri-for), then store that information
+  // in the loadInfo so we can enforce SRI before loading the subresource.
+  if (!mEnforceSRI) {
+    // do not look into the CSP if already true:
+    // a CSP saying that SRI isn't needed should not
+    // overrule GetVerifySignedContent
+    if (aLoadingPrincipal) {
+      nsCOMPtr<nsIContentSecurityPolicy> csp;
+      aLoadingPrincipal->GetCsp(getter_AddRefs(csp));
+      uint32_t externalType =
+        nsContentUtils::InternalContentPolicyTypeToExternal(aContentPolicyType);
+      // csp could be null if loading principal is system principal
+      if (csp) {
+        csp->RequireSRIForType(externalType, &mEnforceSRI);
+      }
+      // if CSP is delivered via a meta tag, it's speculatively available
+      // as 'preloadCSP'. If we are preloading a script or style, we have
+      // to apply that speculative 'preloadCSP' for such loads.
+      if (!mEnforceSRI && nsContentUtils::IsPreloadType(aContentPolicyType)) {
+        nsCOMPtr<nsIContentSecurityPolicy> preloadCSP;
+        aLoadingPrincipal->GetPreloadCsp(getter_AddRefs(preloadCSP));
+        if (preloadCSP) {
+          preloadCSP->RequireSRIForType(externalType, &mEnforceSRI);
         }
       }
     }
+  }
 
   InheritOriginAttributes(mLoadingPrincipal, mOriginAttributes);
 
