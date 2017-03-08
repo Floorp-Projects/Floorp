7f2a0016f43e930ce628702d8701ba053afc0a7d
Andrzej Hunt <andrzej@ahunt.org>
2017-03-08 13:54:12 +0100

    [focus] Implement SupportUtils for SUMO URL preparation (https://github.com/mozilla-mobile/focus-android/issues/89)

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/SupportUtils.java
@@ -0,0 +1,39 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.utils;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Locale;
+
+public class SupportUtils {
+
+    public static String getSumoURLForTopic(final Context context, final String topic) {
+        String escapedTopic;
+        try {
+            escapedTopic = URLEncoder.encode(topic, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("utf-8 should always be available", e);
+        }
+
+        final String appVersion;
+        try {
+            appVersion = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // This should be impossible - we should always be able to get information about ourselves:
+            throw new IllegalStateException("Unable find package details for Focus", e);
+        }
+
+        final String osTarget = "Android";
+        final String langTag = Locales.getLanguageTag(Locale.getDefault());
+
+        return "https://support.mozilla.org/1/mobile/" + appVersion + "/" + osTarget + "/" + langTag + "/" + escapedTopic;
+    }
+}
