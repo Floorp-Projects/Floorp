98a57de9a3bfd66957332dbf11915ece1da4fe80
André Bargull <andre.bargull@gmail.com>
2017-03-09 09:58:58 -0800

    Bug 1345769 - Part 4: Update tests for native function source code. r=arai

--- js/src/tests/js1_7/extensions/regress-381301.js
+++ js/src/tests/js1_7/extensions/regress-381301.js
@@ -23,8 +23,16 @@ function test()
 
   var o =
     Object.defineProperty({}, "x", { get: decodeURI, enumerable: true, configurable: true });
-  expect = '( { get x ( ) { [ native code ] } } )';
+  expect = '({get x() {[native code]}})';
   actual =  uneval(o);
+
+  // Native function syntax:
+  // `function IdentifierName_opt ( FormalParameters ) { [ native code ] }`
+
+  // The placement of whitespace characters in the native function's body is
+  // implementation-dependent, so we need to replace those for this test.
+  var re = new RegExp(["\\{", "\\[", "native", "code", "\\]", "\\}"].join("\\s*"));
+  actual = actual.replace(re, "{[native code]}");
 
   compareSource(expect, actual, summary);
 
