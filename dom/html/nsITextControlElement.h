fd1e270b7089147b5748a1bdfe3915a420b91aa9
Boris Zbarsky <bzbarsky@mit.edu>
2017-03-09 14:44:06 -0500

    Bug 1343037 part 18.  Implement nsTextEditorState::SetRangeText.  r=ehsan
    MozReview-Commit-ID: FEo9yv5iu6U

--- dom/html/nsITextControlElement.h
+++ dom/html/nsITextControlElement.h
@@ -174,6 +174,12 @@ public:
    */
   NS_IMETHOD_(void) OnValueChanged(bool aNotify, bool aWasInteractiveUserChange) = 0;
 
+  /**
+   * Helpers for value manipulation from SetRangeText.
+   */
+  virtual void GetValueFromSetRangeText(nsAString& aValue) = 0;
+  virtual nsresult SetValueFromSetRangeText(const nsAString& aValue) = 0;
+
   static const int32_t DEFAULT_COLS = 20;
   static const int32_t DEFAULT_ROWS = 1;
   static const int32_t DEFAULT_ROWS_TEXTAREA = 2;
