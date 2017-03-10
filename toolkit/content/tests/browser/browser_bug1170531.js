24e2137d449d7c8ff4599d054e78a97254ab169e
Michael Layzell <michael@thelayzells.com>
2017-03-10 12:20:26 -0500

    Bug 1179542 - Wait for the page to load before checking menu status. r=ehsan, a=test-only
    MozReview-Commit-ID: 7wHbkAC8eCI

--- toolkit/content/tests/browser/browser_bug1170531.js
+++ toolkit/content/tests/browser/browser_bug1170531.js
@@ -46,6 +46,7 @@ add_task(function* () {
     let menu_cut_disabled, menu_copy_disabled;
 
     yield BrowserTestUtils.loadURI(browser, "data:text/html,<div>hello!</div>");
+    yield BrowserTestUtils.browserLoaded(browser);
     browser.focus();
     yield new Promise(resolve => waitForFocus(resolve, window));
     yield new Promise(openMenu);
@@ -56,6 +57,7 @@ add_task(function* () {
     yield new Promise(closeMenu);
 
     yield BrowserTestUtils.loadURI(browser, "data:text/html,<div contentEditable='true'>hello!</div>");
+    yield BrowserTestUtils.browserLoaded(browser);
     browser.focus();
     yield new Promise(resolve => waitForFocus(resolve, window));
     yield new Promise(openMenu);
@@ -66,6 +68,7 @@ add_task(function* () {
     yield new Promise(closeMenu);
 
     yield BrowserTestUtils.loadURI(browser, "about:preferences");
+    yield BrowserTestUtils.browserLoaded(browser);
     browser.focus();
     yield new Promise(resolve => waitForFocus(resolve, window));
     yield new Promise(openMenu);
