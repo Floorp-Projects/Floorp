73ad6edf571b17b89b2573d607eed466ee51c21a
Dustin J. Mitchell <dustin@mozilla.com>
2017-03-10 18:17:43 +0000

    Bug 1333255: implement optimizations as named functions; r=jonasfj
    MozReview-Commit-ID: 9xkHny7IYfA

--- taskcluster/taskgraph/task/transform.py
+++ taskcluster/taskgraph/task/transform.py
@@ -8,11 +8,9 @@ import logging
 import itertools
 
 from . import base
-from .. import files_changed
 from ..util.python_path import find_object
 from ..util.templates import merge
 from ..util.yaml import load_yaml
-from ..util.seta import is_low_value_task
 
 from ..transforms.base import TransformSequence, TransformConfig
 
@@ -84,51 +82,11 @@ class TransformTask(base.Task):
     """
 
     def __init__(self, kind, task):
-        self.when = task.get('when', {})
         super(TransformTask, self).__init__(kind, task['label'],
                                             task['attributes'], task['task'],
-                                            index_paths=task.get('index-paths'),
+                                            optimizations=task.get('optimizations'),
                                             dependencies=task.get('dependencies'))
-
-    def optimize(self, params):
-        bbb_task = False
-
-        if self.index_paths:
-            optimized, taskId = super(TransformTask, self).optimize(params)
-            if optimized:
-                return optimized, taskId
-
-        elif 'files-changed' in self.when:
-            changed = files_changed.check(
-                params, self.when['files-changed'])
-            if not changed:
-                logger.debug('no files found matching a pattern in `when.files-changed` for ' +
-                             self.label)
-                return True, None
-
-        # no need to call SETA for build jobs
-        if self.task.get('extra', {}).get('treeherder', {}).get('jobKind', '') == 'build':
-            return False, None
-
-        # for bbb tasks we need to send in the buildbot buildername
-        if self.task.get('provisionerId', '') == 'buildbot-bridge':
-            self.label = self.task.get('payload').get('buildername')
-            bbb_task = True
-
-        # we would like to return 'False, None' while it's high_value_task
-        # and we wouldn't optimize it. Otherwise, it will return 'True, None'
-        if is_low_value_task(self.label,
-                             params.get('project'),
-                             params.get('pushlog_id'),
-                             params.get('pushdate'),
-                             bbb_task):
-            # Always optimize away low-value tasks
-            return True, None
-        else:
-            return False, None
 
     @classmethod
     def from_json(cls, task_dict):
-        # when reading back from JSON, we lose the "when" information
-        task_dict['when'] = {}
         return cls(task_dict['attributes']['kind'], task_dict)
