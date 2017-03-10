83cdc02ec0a26996ccda0a6dd56a1d954c90466f
Franziskus Kiefer <franziskuskiefer@gmail.com>
2017-03-10 06:01:18 +0100

    Bug 1345368 - land NSS 848abc2061a4, r=me
    --HG--
    rename : security/nss/fuzz/git-copy.sh => security/nss/fuzz/config/git-copy.sh
    rename : security/nss/fuzz/certDN.options => security/nss/fuzz/options/certDN.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-add.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-addmod.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-div.options
    rename : security/nss/fuzz/mpi-expmod.options => security/nss/fuzz/options/mpi-expmod.options
    rename : security/nss/fuzz/mpi-invmod.options => security/nss/fuzz/options/mpi-invmod.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-mod.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-mulmod.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-sqr.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-sqrmod.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-sub.options
    rename : security/nss/fuzz/mpi-mod.options => security/nss/fuzz/options/mpi-submod.options
    rename : security/nss/fuzz/quickder.options => security/nss/fuzz/options/quickder.options
    rename : security/nss/fuzz/tls-client.options => security/nss/fuzz/options/tls-client-no_fuzzer_mode.options
    rename : security/nss/fuzz/tls-client.options => security/nss/fuzz/options/tls-client.options

--- security/nss/fuzz/config/clone_libfuzzer.sh
+++ security/nss/fuzz/config/clone_libfuzzer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 d=$(dirname $0)
-$d/git-copy.sh https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer b96a41ac6bbc3824fc7c7977662bebacac8f0983 $d/libFuzzer
+$d/git-copy.sh https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer b96a41ac6bbc3824fc7c7977662bebacac8f0983 $d/../libFuzzer
 
 # [https://llvm.org/bugs/show_bug.cgi?id=31318]
 # This prevents a known buffer overrun that won't be fixed as the affected code
 # will go away in the near future. Until that is we have to patch it as we seem
 # to constantly run into it.
-cat <<EOF | patch -p0 -d $d
+cat <<EOF | patch -p0 -d $d/..
 diff --git libFuzzer/FuzzerLoop.cpp libFuzzer/FuzzerLoop.cpp
 --- libFuzzer/FuzzerLoop.cpp
 +++ libFuzzer/FuzzerLoop.cpp
@@ -26,7 +26,7 @@ EOF
 # Latest Libfuzzer uses __sanitizer_dump_coverage(), a symbol to be introduced
 # with LLVM 4.0. To keep our code working with LLVM 3.x to simplify development
 # of fuzzers we'll just provide it ourselves.
-cat <<EOF | patch -p0 -d $d
+cat <<EOF | patch -p0 -d $d/..
 diff --git libFuzzer/FuzzerTracePC.cpp libFuzzer/FuzzerTracePC.cpp
 --- libFuzzer/FuzzerTracePC.cpp
 +++ libFuzzer/FuzzerTracePC.cpp
