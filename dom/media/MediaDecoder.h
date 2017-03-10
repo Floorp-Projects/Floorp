2e1c7a3659ecd420f1e73d43a7401bdad2eb526b
Kaku Kuo <kaku@mozilla.com>
2017-03-10 16:29:59 +0800

    Bug 1346498 part 1 - extract the MediaDecoder::NotifyCompositor() method; r=jwwang
    MozReview-Commit-ID: Hfw3FJUSVVe

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -590,6 +590,9 @@ private:
   MediaEventSource<void>*
   DataArrivedEvent() override { return &mDataArrivedEvent; }
 
+  // Called when the owner's activity changed.
+  void NotifyCompositor();
+
   MediaEventSource<RefPtr<layers::KnowsCompositor>>*
   CompositorUpdatedEvent() override { return &mCompositorUpdatedEvent; }
 
