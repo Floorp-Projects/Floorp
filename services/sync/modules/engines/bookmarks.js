ba5b7f517212b025ac732496c85657ab617c8556
Edouard Oger <eoger@fastmail.com>
2017-03-10 15:03:07 -0500

    Bug 745410 - Remove microsummary support from Sync. r=kitcambridge
    MozReview-Commit-ID: BdBgxHeHndj

--- services/sync/modules/engines/bookmarks.js
+++ services/sync/modules/engines/bookmarks.js
@@ -74,7 +74,6 @@ function isSyncedRootNode(node) {
 function getTypeObject(type) {
   switch (type) {
     case "bookmark":
-    case "microsummary":
       return Bookmark;
     case "query":
       return BookmarkQuery;
@@ -384,7 +383,6 @@ BookmarksEngine.prototype = {
         }
         // No queryID? Fall through to the regular bookmark case.
       case "bookmark":
-      case "microsummary":
         key = "b" + item.bmkUri + ":" + (item.title || "");
         break;
       case "folder":
