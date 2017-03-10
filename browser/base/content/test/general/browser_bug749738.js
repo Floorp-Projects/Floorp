1f59c5272e19de295b273a336ac9255c32aa3fb5
Blake Kaplan <mrbkap@gmail.com>
2017-03-09 17:29:08 -0800

    Bug 1346286 - Remove CPOWs from more tests. r=Felipe, r=Gijs, a=test-only
    --HG--
    extra : source : b7b163f179c4e4c93dda9f3b82bf5b87d89fe3d0
    extra : intermediate-source : 8e62e7262959dd317b29df48b0c9fcdb13967155

--- browser/base/content/test/general/browser_bug749738.js
+++ browser/base/content/test/general/browser_bug749738.js
@@ -12,7 +12,8 @@ function test() {
   let tab = gBrowser.addTab();
   gBrowser.selectedTab = tab;
 
-  load(tab, DUMMY_PAGE, function() {
+  BrowserTestUtils.loadURI(tab.linkedBrowser, DUMMY_PAGE);
+  BrowserTestUtils.browserLoaded(tab.linkedBrowser).then(() => {
     gFindBar.onFindCommand();
     EventUtils.sendString("Dummy");
     gBrowser.removeTab(tab);
@@ -25,12 +26,4 @@ function test() {
     }
     finish();
   });
-}
-
-function load(aTab, aUrl, aCallback) {
-  aTab.linkedBrowser.addEventListener("load", function onload(aEvent) {
-    aEvent.currentTarget.removeEventListener("load", onload, true);
-    waitForFocus(aCallback, content);
-  }, true);
-  aTab.linkedBrowser.loadURI(aUrl);
 }
