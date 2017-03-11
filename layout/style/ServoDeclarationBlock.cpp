3ec39e0d42f68fda0c72c426df4043f49e073781
Manish Goregaokar <manishearth@gmail.com>
2017-03-10 18:28:02 -0800

    Bug 1341690 - stylo: Make URLs work in inline style; r=emilio
    MozReview-Commit-ID: 6Tc0kBw4V8c

--- layout/style/ServoDeclarationBlock.cpp
+++ layout/style/ServoDeclarationBlock.cpp
@@ -12,11 +12,15 @@
 namespace mozilla {
 
 /* static */ already_AddRefed<ServoDeclarationBlock>
-ServoDeclarationBlock::FromCssText(const nsAString& aCssText)
+ServoDeclarationBlock::FromCssText(const nsAString& aCssText,
+                                   const GeckoParserExtraData& aExtraData)
 {
   NS_ConvertUTF16toUTF8 value(aCssText);
+  nsCString baseString;
+  // FIXME (bug 1343964): Figure out a better solution for sending the base uri to servo
+  aExtraData.mBaseURI->get()->GetSpec(baseString);
   RefPtr<RawServoDeclarationBlock>
-    raw = Servo_ParseStyleAttribute(&value).Consume();
+    raw = Servo_ParseStyleAttribute(&value, &baseString, &aExtraData).Consume();
   RefPtr<ServoDeclarationBlock> decl = new ServoDeclarationBlock(raw.forget());
   return decl.forget();
 }
