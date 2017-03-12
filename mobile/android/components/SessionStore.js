79dc2ce74ea60e049911d60922f410d0aa7013c3
Jan Henning <jh+bugzilla@buttercookie.de>
2017-03-12 11:13:33 +0100

    Bug 1346171 - Part 2 - Test synchronous session file deletion on clearing history. r=sebastian a=lizzard
    MozReview-Commit-ID: JUrbSD2t41t
    
    --HG--
    extra : source : 0be73c1886dd3bbbced4d6d056ad797d8e390424
    extra : intermediate-source : b364c6b8169d20e86287510bc6d3fbdb6dded75e

--- mobile/android/components/SessionStore.js
+++ mobile/android/components/SessionStore.js
@@ -1869,6 +1869,11 @@ SessionStore.prototype = {
     } else if (this._loadState <= STATE_QUITTING) {
       this.saveStateDelayed();
     }
+  },
+
+  setLoadState: function ss_setLoadState(aState) {
+    this.flushPendingState();
+    this._loadState = aState;
   }
 
 };
