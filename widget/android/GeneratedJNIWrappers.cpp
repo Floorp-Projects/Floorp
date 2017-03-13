07bad5683e5843919d9a001f39090ac82728d16f
Jim Chen <nchen@mozilla.com>
2017-03-13 19:55:00 +0100

    Bug 1346634 - Only notify push observers from xpcom queue; r=snorp a=lizzard
    Until bug 1344892 is fixed, only notify push observers (and not all
    observers) from the XPCOM nsThread event queue, to avoid regressions
    such as session restore breaking.

--- widget/android/GeneratedJNIWrappers.cpp
+++ widget/android/GeneratedJNIWrappers.cpp
@@ -562,6 +562,9 @@ auto GeckoAppShell::MoveTaskToBack() -> void
 constexpr char GeckoAppShell::NotifyObservers_t::name[];
 constexpr char GeckoAppShell::NotifyObservers_t::signature[];
 
+constexpr char GeckoAppShell::NotifyPushObservers_t::name[];
+constexpr char GeckoAppShell::NotifyPushObservers_t::signature[];
+
 constexpr char GeckoAppShell::NotifyAlertListener_t::name[];
 constexpr char GeckoAppShell::NotifyAlertListener_t::signature[];
 
