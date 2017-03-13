ef3c464d7c69720195fcf2d1b96d40e7afab6384
Andrzej Hunt <andrzej@ahunt.org>
2017-03-13 13:47:26 +0100

    [focus] android2po: Retain CDATA sections without additional escaping
    This is hacky and tricky, but python and lxml like to pretend
    CDATA doesn't exist. This solution retains CDATA in the po files,
    which should make translations simpler.
    
    (Note: string import does the same thing: at that point we no longer
     care, Android correctly handles the escaped html at that stage and
     displays it as expected even when retrieved via Resources.getString().)

--- mobile/android/focus-android/tools/l10n/android2po/convert.py
+++ mobile/android/focus-android/tools/l10n/android2po/convert.py
@@ -285,6 +285,7 @@ def get_element_text(tag, name, warnfunc=dummy_warn):
                 value += "<%s%s>" % (tag_name, params_str)
             if elem.text is not None:
                 t = elem.text
+                raw = etree.tostring(elem)
                 # Leading/Trailing whitespace is removed completely
                 # ONLY if there are no nested tags. Handle this before
                 # calling ``convert_text``, so that whitespace
@@ -307,10 +308,15 @@ def get_element_text(tag, name, warnfunc=dummy_warn):
                     raise UnsupportedResourceError(
                         'resource references (%s) are not supported' % t)
 
-                converted_value, elem_formatted = convert_text(t)
-                if elem_formatted:
-                    formatted = True
-                value += converted_value
+                if "<![CDATA[" in raw:
+                    # Don't escape CDATA sections - they're already hand-crafted and can break
+                    # if we do more escaping:
+                    value += '<![CDATA[' + t.replace('\n', '') + ']]>'
+                else:
+                    converted_value, elem_formatted = convert_text(t)
+                    if elem_formatted:
+                        formatted = True
+                    value += converted_value
         elif ev == 'end':
             # The closing root tag has no info for us at all.
             if not is_root:
@@ -339,8 +345,10 @@ def read_xml(xml_file, language=None, warnfunc=dummy_warn):
     result = ResourceTree(language)
     comment = []
 
+    parser = etree.XMLParser(strip_cdata=False)
+
     try:
-        doc = etree.parse(xml_file)
+        doc = etree.parse(xml_file, parser=parser)
     except etree.XMLSyntaxError as e:
         raise InvalidResourceError(e)
 
