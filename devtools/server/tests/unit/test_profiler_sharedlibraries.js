a450aaafe1d48f266a7419015410bf86beb5aab2
Markus Stange <mstange@themasta.com>
2017-03-14 18:54:51 -0400

    Bug 1329111 - Update devtools users of nsIProfiler.getSharedLibraryInformation. r=gregtatum
    This actor message is not used by the devtools themselves, it's only exposed so that
    the add-on can use it. See bug 1131397.
    
    MozReview-Commit-ID: F1dm0U4rkEd

--- devtools/server/tests/unit/test_profiler_sharedlibraries.js
+++ devtools/server/tests/unit/test_profiler_sharedlibraries.js
@@ -4,16 +4,14 @@
 "use strict";
 
 /**
- * Tests whether the profiler responds to "getSharedLibraryInformation" adequately.
+ * Tests whether the profiler responds to "sharedLibraries" adequately.
  */
-
-const Profiler = Cc["@mozilla.org/tools/profiler;1"].getService(Ci.nsIProfiler);
 
 function run_test()
 {
   get_chrome_actors((client, form) => {
     let actor = form.profilerActor;
-    test_getsharedlibraryinformation(client, actor, () => {
+    test_sharedlibraries(client, actor, () => {
       client.close().then(() => {
         do_test_finished();
       });
@@ -23,20 +21,19 @@ function run_test()
   do_test_pending();
 }
 
-function test_getsharedlibraryinformation(client, actor, callback)
+function test_sharedlibraries(client, actor, callback)
 {
-  client.request({ to: actor, type: "getSharedLibraryInformation" }, response => {
-    do_check_eq(typeof response.sharedLibraryInformation, "string");
-    let libs = [];
-    try {
-      libs = JSON.parse(response.sharedLibraryInformation);
-    } catch (e) {
-      do_check_true(false);
-    }
+  client.request({ to: actor, type: "sharedLibraries" }, libs => {
+    do_check_eq(typeof libs, "object");
+    do_check_true(Array.isArray(libs));
     do_check_eq(typeof libs, "object");
     do_check_true(libs.length >= 1);
     do_check_eq(typeof libs[0], "object");
     do_check_eq(typeof libs[0].name, "string");
+    do_check_eq(typeof libs[0].path, "string");
+    do_check_eq(typeof libs[0].debugName, "string");
+    do_check_eq(typeof libs[0].debugPath, "string");
+    do_check_eq(typeof libs[0].arch, "string");
     do_check_eq(typeof libs[0].start, "number");
     do_check_eq(typeof libs[0].end, "number");
     do_check_true(libs[0].start <= libs[0].end);
