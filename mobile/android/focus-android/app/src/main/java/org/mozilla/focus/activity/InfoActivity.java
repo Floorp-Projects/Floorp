1c0fa1d957628902e5bc60b33d956dd72b4e83c0
Andrzej Hunt <andrzej@ahunt.org>
2017-03-14 12:54:51 +0100

    [focus] Add About: support to InfoActivity (https://github.com/mozilla-mobile/focus-android/issues/21)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/InfoActivity.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/InfoActivity.java
@@ -7,8 +7,10 @@ package org.mozilla.focus.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,6 +18,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.mozilla.focus.R;
+import org.mozilla.focus.utils.HtmlLoader;
+import org.mozilla.focus.utils.SupportUtils;
+
+import java.util.Map;
 
 /**
  * A generic activity that supports showing additional information in a WebView. This is useful
@@ -34,6 +40,10 @@ public class InfoActivity extends AppCompatActivity {
         return intent;
     }
 
+    public static final Intent getAboutIntent(final Context context) {
+        return getIntentFor(context, "about:", context.getResources().getString(R.string.menu_about));
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +59,7 @@ public class InfoActivity extends AppCompatActivity {
         // WebViewClient() is enough to disable that behaviour.
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl(url);
+        loadURL(url, webView);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(title);
@@ -65,5 +75,26 @@ public class InfoActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void loadURL(final String url, final WebView webView) {
+        if (url.equals("about:")) {
+            final Resources resources = webView.getContext().getResources();
+
+            final Map<String, String> substitutionMap = new ArrayMap<>();
+            final String appName = webView.getContext().getResources().getString(R.string.app_name);
+            final String learnMoreURL = SupportUtils.getManifestoURL();
+
+            final String aboutContent = resources.getString(R.string.about_content, appName, learnMoreURL);
+            substitutionMap.put("%about-content%", aboutContent);
+
+            final String data = HtmlLoader.loadResourceFile(webView.getContext(), R.raw.about, substitutionMap);
+            // We use a file:/// base URL so that we have the right origin to load file:/// css and
+            // image resources.
+            webView.loadDataWithBaseURL("file:///android_res/raw/about.html", data, "text/html", "UTF-8", null);
+        } else {
+            webView.loadUrl(url);
+        }
+
     }
 }
