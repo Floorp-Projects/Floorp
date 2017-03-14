925e02b6ecb2b23582da4a391aea3c28d95cbf81
Jared Wein <jwein@mozilla.com>
2017-03-14 19:27:33 -0400

    Bug 1347329 - Move the select tests to a forms subdirectory and split apart the color tests to their own file since the test was getting very long. r=mossop
    MozReview-Commit-ID: IGjS2aiL4a2
    
    --HG--
    rename : browser/base/content/test/general/browser_selectpopup.js => browser/base/content/test/forms/browser_selectpopup.js
    extra : rebase_source : cf2a9bee2cbc46808c0f559baddd50cbdae90aab

---
+++ browser/base/content/test/forms/head.js
@@ -0,0 +1,19 @@
+/* eslint-env mozilla/frame-script */
+
+function hideSelectPopup(selectPopup, mode = "enter", win = window) {
+  let browser = win.gBrowser.selectedBrowser;
+  let selectClosedPromise = ContentTask.spawn(browser, null, function*() {
+    Cu.import("resource://gre/modules/SelectContentHelper.jsm");
+    return ContentTaskUtils.waitForCondition(() => !SelectContentHelper.open);
+  });
+
+  if (mode == "escape") {
+    EventUtils.synthesizeKey("KEY_Escape", { code: "Escape" }, win);
+  } else if (mode == "enter") {
+    EventUtils.synthesizeKey("KEY_Enter", { code: "Enter" }, win);
+  } else if (mode == "click") {
+    EventUtils.synthesizeMouseAtCenter(selectPopup.lastChild, { }, win);
+  }
+
+  return selectClosedPromise;
+}
