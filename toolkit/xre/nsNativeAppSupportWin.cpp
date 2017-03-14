7c0497b430ee2eff937c066c31973ef4c7edcafd
David Major <dmajor@mozilla.com>
2017-03-14 15:26:27 +1300

    Bug 1344629 - Part 6: Rewrite unnecessary uses of nsLiteralString. r=dbaron
    There's an antipattern where nsLiteralString is used as an unnecessary intermediary in converting from CharT* to CharT*,
    e.g. CallAFunctionThatTakesACharPointer(NS_LITERAL_CSTRING("foo").get());
    or
    NS_NAMED_LITERAL_STRING(foo, "abc");
    CallAFunctionThatTakesACharPointer(foo.get());
    
    This patch rewrites the callsites that can be trivially changed to use char*/char16_t*.
    
    I'd somewhat like to remove nsTLiteralString::get() altogether, but in code that's less straightforward than these examples, get() is useful enough to keep.
    
    MozReview-Commit-ID: Kh1rUziVllo

--- toolkit/xre/nsNativeAppSupportWin.cpp
+++ toolkit/xre/nsNativeAppSupportWin.cpp
@@ -953,7 +953,7 @@ nsNativeAppSupportWin::HandleDDENotification( UINT uType,       // transaction t
                     do {
                         // Get most recently used Nav window.
                         nsCOMPtr<mozIDOMWindowProxy> navWin;
-                        GetMostRecentWindow( NS_LITERAL_STRING( "navigator:browser" ).get(),
+                        GetMostRecentWindow( u"navigator:browser",
                                              getter_AddRefs( navWin ) );
                         nsCOMPtr<nsPIDOMWindowOuter> piNavWin = do_QueryInterface(navWin);
                         if ( !piNavWin ) {
@@ -1430,7 +1430,7 @@ nsNativeAppSupportWin::OpenBrowserWindow()
     // browser window.
 
     nsCOMPtr<mozIDOMWindowProxy> navWin;
-    GetMostRecentWindow( NS_LITERAL_STRING( "navigator:browser" ).get(), getter_AddRefs( navWin ) );
+    GetMostRecentWindow( u"navigator:browser", getter_AddRefs( navWin ) );
 
     // This isn't really a loop.  We just use "break" statements to fall
     // out to the OpenWindow call when things go awry.
