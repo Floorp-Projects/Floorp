bdb389dbd4b048db8a02230e4440ef165eb51334
Sebastian Kaspari <s.kaspari@gmail.com>
2017-03-14 17:46:24 +0100

    [focus] Always look for known browsers (https://github.com/mozilla-mobile/focus-android/issues/177)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Browsers.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Browsers.java
@@ -66,13 +66,12 @@ public class Browsers {
         final PackageManager packageManager = context.getPackageManager();
 
         final Map<String, ActivityInfo> browsers = resolveBrowsers(packageManager, url);
-        if (browsers.size() <= 1) {
-            // If there's a default browser set then modern Android systems won't return other options
-            // anymore when using queryIntentActivities(). That's annoying and our only option is
-            // to go through a list of known browsers and see if anyone of them is installed and
-            // wants to handle our URL.
-            findKnownBrowsers(packageManager, browsers, url);
-        }
+
+        // If there's a default browser set then modern Android systems won't return other browsers
+        // anymore when using queryIntentActivities(). That's annoying and our only option is
+        // to go through a list of known browsers and see if anyone of them is installed and
+        // wants to handle our URL.
+        findKnownBrowsers(packageManager, browsers, url);
 
         this.browsers = browsers;
         this.defaultBrowser = findDefault(packageManager, url);
