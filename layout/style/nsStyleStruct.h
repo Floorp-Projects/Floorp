85448394ac4bfa6f67853a4d175b1e357008324f
cku <cku@mozilla.com>
2017-03-15 12:21:07 +0800

    Bug 1340044 - Part 1. Declare mTransformBox as StyleGeometryBox. r=heycam
    MozReview-Commit-ID: 9fcHkDtNHhL
    
    --HG--
    extra : rebase_source : 61cdec71eea4f80e2e1e702bf6a03132287545a3

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -2747,6 +2747,8 @@ private:
 
 struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay
 {
+  typedef mozilla::StyleGeometryBox StyleGeometryBox;
+
   explicit nsStyleDisplay(const nsPresContext* aContext);
   nsStyleDisplay(const nsStyleDisplay& aOther);
   ~nsStyleDisplay();
@@ -2835,7 +2837,7 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay
   // null, as appropriate.)
   uint8_t mBackfaceVisibility;
   uint8_t mTransformStyle;
-  uint8_t mTransformBox;        // [reset] see nsStyleConsts.h
+  StyleGeometryBox mTransformBox; // [reset] see nsStyleConsts.h
   RefPtr<nsCSSValueSharedList> mSpecifiedTransform; // [reset]
   nsStyleCoord mTransformOrigin[3]; // [reset] percent, coord, calc, 3rd param is coord, calc only
   nsStyleCoord mChildPerspective; // [reset] none, coord
