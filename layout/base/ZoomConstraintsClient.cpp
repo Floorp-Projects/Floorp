74cb471e5d72b7823a342eb19d404189ef10b3e2
KuoE0 <kuoe0.tw@gmail.com>
2017-03-15 12:08:13 +0800

    Bug 1342863 - (Part 3) DocGroup labeling for runnables dispatched by NS_DispatchTo(Main|Current)Thread in ZoomConstraintsClient. r=dholbert
    MozReview-Commit-ID: GcExvSnUB8o

--- layout/base/ZoomConstraintsClient.cpp
+++ layout/base/ZoomConstraintsClient.cpp
@@ -158,8 +158,12 @@ ZoomConstraintsClient::Observe(nsISupports* aSubject, const char* aTopic, const 
     // We need to run this later because all the pref change listeners need
     // to execute before we can be guaranteed that gfxPrefs::ForceUserScalable()
     // returns the updated value.
-    NS_DispatchToMainThread(NewRunnableMethod(
-      this, &ZoomConstraintsClient::RefreshZoomConstraints));
+
+    RefPtr<nsRunnableMethod<ZoomConstraintsClient>> event =
+      NewRunnableMethod(this, &ZoomConstraintsClient::RefreshZoomConstraints);
+    mDocument->Dispatch("ZoomConstraintsClient::RefreshZoomConstraints",
+                        TaskCategory::Other,
+                        event.forget());
   }
   return NS_OK;
 }
