dd44ef236095da5bb3e8d61a8d50ba51f1c96784
Phil Ringnalda <philringnalda@gmail.com>
2017-03-14 20:03:31 -0700

    Backed out 3 changesets (bug 1346987) for media, blob, and temporaryfile failures CLOSED TREE
    Backed out changeset d24fa1b4553b (bug 1346987)
    Backed out changeset 34701b9ed4ba (bug 1346987)
    Backed out changeset f24f4fdc5cc8 (bug 1346987)

--- dom/file/MutableBlobStorage.cpp
+++ dom/file/MutableBlobStorage.cpp
@@ -111,7 +111,6 @@ public:
     : mBlobStorage(aBlobStorage)
   {
     MOZ_ASSERT(NS_IsMainThread());
-    MOZ_ASSERT(XRE_IsParentProcess());
     MOZ_ASSERT(aBlobStorage);
   }
 
@@ -119,11 +118,14 @@ public:
   Run() override
   {
     MOZ_ASSERT(!NS_IsMainThread());
-    MOZ_ASSERT(XRE_IsParentProcess());
 
     PRFileDesc* tempFD = nullptr;
     nsresult rv = NS_OpenAnonymousTemporaryFile(&tempFD);
     if (NS_WARN_IF(NS_FAILED(rv))) {
+      // In sandboxed context we are not allowed to create temporary files, but
+      // this doesn't mean that BlobStorage should fail. We can continue to
+      // store data in memory.  We don't change the storageType so that we don't
+      // try to create a temporary file again.
       return NS_OK;
     }
 
@@ -532,20 +534,9 @@ MutableBlobStorage::ShouldBeTemporaryStorage(uint64_t aSize) const
 nsresult
 MutableBlobStorage::MaybeCreateTemporaryFile()
 {
-  if (XRE_IsParentProcess()) {
-    nsresult rv = DispatchToIOThread(new CreateTemporaryFileRunnable(this));
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      return rv;
-    }
-  } else {
-    RefPtr<MutableBlobStorage> self(this);
-    ContentChild::GetSingleton()->
-      AsyncOpenAnonymousTemporaryFile([self](PRFileDesc* prfile) {
-        if (prfile) {
-          // The ownership of the prfile is moved to the FileCreatedRunnable.
-          NS_DispatchToMainThread(new FileCreatedRunnable(self, prfile));
-        }
-      });
+  nsresult rv = DispatchToIOThread(new CreateTemporaryFileRunnable(this));
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return rv;
   }
 
   mStorageState = eWaitingForTemporaryFile;
