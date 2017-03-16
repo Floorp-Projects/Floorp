652713e3783497db579b802dcb047796e7f1a620
Andrzej Hunt <andrzej@ahunt.org>
2017-03-16 17:45:05 +0100

    [focus] Implement loadPngAsDataURI
    Loading android resources in HTML in a webview can fail for various
    stupid reasons. We can hack around that by loading image data
    as a data: URI. We don't need to use this often, so this seems
    like a satisfactory solution.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/HtmlLoader.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/HtmlLoader.java
@@ -1,9 +1,11 @@
 package org.mozilla.focus.utils;
 
 import android.content.Context;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RawRes;
+import android.util.Base64;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -57,5 +59,52 @@ public class HtmlLoader {
                 // just because we couldn't close a file?
             }
         }
+    }
+
+    private final static byte[] pngHeader = new byte[] { -119, 80, 78, 71, 13, 10, 26, 10 };
+
+    public static String loadPngAsDataURI(@NonNull final Context context,
+                                          @NonNull final @DrawableRes int resourceID) {
+
+        // We are copying the approach BitmapFactory.decodeResource(Resources, int, Options)
+        // uses - you are explicitly allowed to open Drawables, but the method has a @RawRes
+        // annotation (despite officially supporting Drawables).
+        //noinspection ResourceType
+        final InputStream pngInputStream = context.getResources().openRawResource(resourceID);
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(pngInputStream));
+
+        final StringBuilder builder = new StringBuilder();
+        builder.append("data:image/png;base64,");
+
+        try {
+            // Base64 encodes 3 bytes at a time, make sure we have a multiple of 3 here
+            // I don't know what a sensible chunk size is, let's just go with 300b.
+            final byte[] data = new byte[3*100];
+            int bytesRead;
+            boolean headerVerified = false;
+
+            while ((bytesRead = pngInputStream.read(data)) > 0) {
+                // Sanity check: lets make sure this is still a png (i.e. make sure the build system
+                // or Android haven't broken / change the image format).
+                if (!headerVerified) {
+                    if (bytesRead < 8) {
+                        throw new IllegalStateException("Loaded drawable is improbably small");
+                    }
+
+                    for (int i = 0; i < pngHeader.length; i++) {
+                        if (data[i] != pngHeader[i]) {
+                            throw new IllegalStateException("Invalid png detected");
+                        }
+                    }
+                    headerVerified = true;
+                }
+
+                builder.append(Base64.encodeToString(data, 0, bytesRead, 0));
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to load png data");
+        }
+
+        return  builder.toString();
     }
 }
