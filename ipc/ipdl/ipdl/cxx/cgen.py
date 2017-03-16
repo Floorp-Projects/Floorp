625b54086e97c8bd6914300436a0f1310b25e4d3
Kan-Ru Chen <kanru@kanru.info>
2017-03-16 17:36:15 +0800

    Bug 1313200 - Allow IPC messages to async return MozPromises. r=billm,jwwang
    This patch implements async returns for IPDL using MozPromises. There
    are following changes:
    
    * Initialize AbstractThreads for MessageLoops
    * Record promises and their reject functions
      * When async message returns, call their resolve functions
      * When send error or channel close, call their reject functions
    * Implement "unresolved-ipc-promises" count for about:memory
    * Test cases
    
    See bug attachment for generated code from test cases
    
    MozReview-Commit-ID: 7xmg8gwDGaW
    
    --HG--
    rename : ipc/ipdl/test/ipdl/error/AsyncReturn.ipdl => ipc/ipdl/test/ipdl/ok/AsyncReturn.ipdl
    extra : rebase_source : 55c4f68a3f8b7d0df5ca9f9c45b9a205b337282d

--- ipc/ipdl/ipdl/cxx/cgen.py
+++ ipc/ipdl/ipdl/cxx/cgen.py
@@ -38,8 +38,18 @@ class CxxCodeGen(CodePrinter, Visitor):
 
         if t.T is not None:
             self.write('<')
-            t.T.accept(self)
+            if type(t.T) is list:
+                t.T[0].accept(self)
+                for tt in t.T[1:]:
+                    self.write(', ')
+                    tt.accept(self)
+            else:
+                t.T.accept(self)
             self.write('>')
+
+        if t.inner is not None:
+            self.write('::')
+            t.inner.accept(self)
 
         ts = ''
         if t.ptr:            ts += '*'
@@ -345,7 +355,8 @@ class CxxCodeGen(CodePrinter, Visitor):
         self.write('(')
         es.obj.accept(self)
         self.write(')')
-        self.write(es.op + es.field)
+        self.write(es.op)
+        es.field.accept(self)
 
     def visitExprAssn(self, ea):
         ea.lhs.accept(self)
@@ -377,6 +388,24 @@ class CxxCodeGen(CodePrinter, Visitor):
         self.write('delete ')
         ed.obj.accept(self)
 
+    def visitExprLambda(self, l):
+        self.write('[')
+        ncaptures = len(l.captures)
+        for i, c in enumerate(l.captures):
+            c.accept(self)
+            if i != (ncaptures-1):
+                self.write(', ')
+        self.write('](')
+        self.writeDeclList(l.params)
+        self.write(')')
+        if l.ret:
+            self.write(' -> ')
+            l.ret.accept(self)
+        self.println(' {')
+        self.indent()
+        self.visitBlock(l)
+        self.dedent()
+        self.printdent('}')
 
     def visitStmtBlock(self, b):
         self.printdentln('{')
