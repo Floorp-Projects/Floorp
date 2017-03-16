80e2e16b3ebff3dd033c0f7bdd128285f38494ad
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-03-16 19:59:39 +0100

    Backed out changeset eaac943ff2f6 (bug 1329111)

--- tools/profiler/tests/test_shared_library.js
+++ tools/profiler/tests/test_shared_library.js
@@ -13,19 +13,11 @@ function run_test() {
   if (!profiler)
     return;
 
-  var libs = profiler.sharedLibraries;
+  var sharedStr = profiler.getSharedLibraryInformation();
+  sharedStr = sharedStr.toLowerCase();
 
-  do_check_eq(typeof libs, "object");
-  do_check_true(Array.isArray(libs));
-  do_check_eq(typeof libs, "object");
-  do_check_true(libs.length >= 1);
-  do_check_eq(typeof libs[0], "object");
-  do_check_eq(typeof libs[0].name, "string");
-  do_check_eq(typeof libs[0].path, "string");
-  do_check_eq(typeof libs[0].debugName, "string");
-  do_check_eq(typeof libs[0].debugPath, "string");
-  do_check_eq(typeof libs[0].arch, "string");
-  do_check_eq(typeof libs[0].start, "number");
-  do_check_eq(typeof libs[0].end, "number");
-  do_check_true(libs[0].start <= libs[0].end);
+  // Let's not hardcode anything too specific
+  // just some sanity checks.
+  do_check_neq(sharedStr, null);
+  do_check_neq(sharedStr, "");
 }
