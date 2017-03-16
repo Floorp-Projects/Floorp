044f4bbc8a3fcc52697811b72113b3adac66bb34
Andrzej Hunt <andrzej@ahunt.org>
2017-03-16 13:09:51 +0100

    [focus] Use pref-keys for about/help/rights in Settings
    We're doing string substitutions for about, so a pref key is more
    sane/reliable. We might as well use keys for everything just to be extra
    safe.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/SettingsFragment.java
@@ -9,14 +9,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 
 import org.mozilla.focus.R;
 import org.mozilla.focus.activity.HelpActivity;
 import org.mozilla.focus.activity.InfoActivity;
-import org.mozilla.focus.widget.DefaultBrowserPreference;
 
 public class SettingsFragment extends PreferenceFragment {
     @Override
@@ -28,16 +26,13 @@ public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        // We could use preference keys (instead of titles) here, but the only places those would
-        // be used are in the menu XML, and here - so we might as well use the already
-        // existing preference titles:
-        if (preference.getTitle().equals(getResources().getString(R.string.menu_about))) {
+        if (preference.getKey().equals(getResources().getString(R.string.pref_key_about))) {
             final Intent intent = InfoActivity.getAboutIntent(getActivity());
             startActivity(intent);
-        } else if (preference.getTitle().equals(getResources().getString(R.string.menu_help))) {
+        } else if (preference.getKey().equals(getResources().getString(R.string.pref_key_help))) {
             final Intent intent = new Intent(getActivity(), HelpActivity.class);
             startActivity(intent);
-        } else if (preference.getTitle().equals(getResources().getString(R.string.menu_rights))) {
+        } else if (preference.getKey().equals(getResources().getString(R.string.pref_key_rights))) {
             final Intent intent = InfoActivity.getRightsIntent(getActivity());
             startActivity(intent);
         }
