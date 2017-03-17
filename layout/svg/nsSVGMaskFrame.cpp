e35416592f8b9151b0b333d935f90071a39cda9d
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-03-17 21:53:24 +0100

    Backed out changeset a46f3da8aba6 (bug 1345853)

--- layout/svg/nsSVGMaskFrame.cpp
+++ layout/svg/nsSVGMaskFrame.cpp
@@ -320,7 +320,7 @@ nsSVGMaskFrame::GetMaskForMaskedFrame(MaskParams& aParams)
 
   // Moz2D transforms in the opposite direction to Thebes
   if (!maskSurfaceMatrix.Invert()) {
-    return MakePair(DrawResult::SUCCESS, RefPtr<SourceSurface>());
+    return MakePair(DrawResult::TEMPORARY_ERROR, RefPtr<SourceSurface>());
   }
 
   *aParams.maskTransform = ToMatrix(maskSurfaceMatrix);
