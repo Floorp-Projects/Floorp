8a66aa54a4dea83fe5fab12ba81a4fc2b835acdb
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-03-17 11:04:33 +0900

    Bug 1348177 - Part 2. Remove GetProcAddress for shell32 to use Vista+/7+ API (tookit part). r=jimm
    It is unnecessary to use Vista/7+ API via GetProcAddress.
    
    MozReview-Commit-ID: AT6OJhhf4r2

--- toolkit/components/alerts/nsAlertsService.h
+++ toolkit/components/alerts/nsAlertsService.h
@@ -10,23 +10,6 @@
 #include "nsCOMPtr.h"
 #include "nsXULAlerts.h"
 
-#ifdef XP_WIN
-typedef enum tagMOZ_QUERY_USER_NOTIFICATION_STATE {
-    QUNS_NOT_PRESENT = 1,
-    QUNS_BUSY = 2,
-    QUNS_RUNNING_D3D_FULL_SCREEN = 3,
-    QUNS_PRESENTATION_MODE = 4,
-    QUNS_ACCEPTS_NOTIFICATIONS = 5,
-    QUNS_QUIET_TIME = 6,
-    QUNS_IMMERSIVE = 7
-} MOZ_QUERY_USER_NOTIFICATION_STATE;
-
-extern "C" {
-// This function is Windows Vista or later
-typedef HRESULT (__stdcall *SHQueryUserNotificationStatePtr)(MOZ_QUERY_USER_NOTIFICATION_STATE *pquns);
-}
-#endif // defined(XP_WIN)
-
 class nsAlertsService : public nsIAlertsService,
                         public nsIAlertsDoNotDisturb
 {
