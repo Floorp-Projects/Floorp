28498b4eebb1e7f7f03e45dc86bcd88d51b65886
Andrzej Hunt <andrzej@ahunt.org>
2017-03-17 19:57:13 +0100

    [focus] Set status bar transparency in styles instead of code
    This improves startup behaviour. With status bar colour setting
    in code, the status bar turns purpley later than the main screen does,
    which looks bad (this is only noticeable on an older phone, and only
    when it's being extra slow).

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/MainActivity.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         setContentView(R.layout.activity_main);
 
