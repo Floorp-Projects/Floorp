cc7bc8e08db8f73ea3e9dcfcecbaed08c1b708be
Nazım Can Altınova <canaltinova@gmail.com>
2017-03-19 15:36:16 +0300

    Bug 1342139 - Implement gecko bindings for will-change r=xidorn
    MozReview-Commit-ID: KNhVz9vjXUl
    
    --HG--
    extra : rebase_source : 20f5d2ec6c151e2d3024bcd11b5ca920158f5a93

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -297,6 +297,10 @@ void Gecko_EnsureImageLayersLength(nsStyleImageLayers* layers, size_t len,
 void Gecko_EnsureStyleAnimationArrayLength(void* array, size_t len);
 void Gecko_EnsureStyleTransitionArrayLength(void* array, size_t len);
 
+void Gecko_ClearWillChange(nsStyleDisplay* display, size_t length);
+void Gecko_AppendWillChange(nsStyleDisplay* display, nsIAtom* atom);
+void Gecko_CopyWillChangeFrom(nsStyleDisplay* dest, nsStyleDisplay* src);
+
 mozilla::Keyframe* Gecko_AnimationAppendKeyframe(RawGeckoKeyframeListBorrowedMut keyframes,
                                                  float offset,
                                                  const nsTimingFunction* timingFunction);
