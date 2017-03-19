e6c5b5f79f41807e2871467974b8b9288f8c3c17
Mark Banner <standard8@mozilla.com>
2017-03-19 18:35:17 +0000

    Bug 1344932 - Separate out the ESLint spidermonkey preprocessor to avoid breaking './mach eslint --fix' on the whole tree.' r=jryans
    This doesn't allow --fix to run on the whole tree in one go, but does allow it to be run on individual directories. This
    is due to an ESLint issue, where once the processor is loaded, it applies to the rest of the files.
    
    MozReview-Commit-ID: Fqg5yK22Dhu

---
+++ tools/lint/eslint/eslint-plugin-spidermonkey-js/lib/index.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview A processor to help parse the spidermonkey js code.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+module.exports = {
+  processors: {
+    ".js": require("../lib/processors/self-hosted")
+  }
+};
