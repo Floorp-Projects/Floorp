fea71ca3d5cd4f6a2ca5538a9a511d6a3f0a6ade
Mark Banner <standard8@mozilla.com>
2017-03-20 23:40:44 +0100

    Bug 1345991 - SVG Image Elements aren't displayed in the Page Info view media list. r=florian, a=gchang
    MozReview-Commit-ID: LlHwl2Kn1Vk

---
+++ browser/base/content/test/general/browser_pageinfo_svg_image.js
@@ -0,0 +1,37 @@
+function test() {
+  waitForExplicitFinish();
+
+  gBrowser.selectedTab = gBrowser.addTab();
+
+  gBrowser.selectedBrowser.addEventListener("load", function() {
+    var pageInfo = BrowserPageInfo(gBrowser.selectedBrowser.currentURI.spec,
+                                   "mediaTab");
+
+    pageInfo.addEventListener("load", function() {
+      pageInfo.onFinished.push(function() {
+        executeSoon(function() {
+          var imageTree = pageInfo.document.getElementById("imagetree");
+          var imageRowsNum = imageTree.view.rowCount;
+
+          ok(imageTree, "Image tree is null (media tab is broken)");
+
+          is(imageRowsNum, 1, "should have one image");
+
+          // Only bother running this if we've got the right number of rows.
+          if (imageRowsNum == 1) {
+            is(imageTree.view.getCellText(0, imageTree.columns[0]),
+               "https://example.com/browser/browser/base/content/test/pageinfo/title_test.svg",
+               "The URL should be the svg image.");
+          }
+
+          pageInfo.close();
+          gBrowser.removeCurrentTab();
+          finish();
+        });
+      });
+    }, {capture: true, once: true});
+  }, {capture: true, once: true});
+
+  content.location =
+    "https://example.com/browser/browser/base/content/test/pageinfo/svg_image.html";
+}
