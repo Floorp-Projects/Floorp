f4ba137a1e6d99563bcb206134d9d89cf674ce83
J. Ryan Stinnett <jryans@gmail.com>
2017-03-21 16:44:09 -0500

    Bug 1349360 - Mark experimental SourceMapService as deprecated. r=ochameau
    The existing `SourceMapService` uses server-side source maps, which we are
    replacing with client-side source maps.  It will be removed later in bug
    1349354, but for now mark it as deprecated so we don't use it with any other
    tools.
    
    MozReview-Commit-ID: ABmWGC5asZd
    
    --HG--
    extra : rebase_source : 52d4238f0d4316c77fb8f4b769f84babe4d7c446

--- devtools/client/preferences/devtools.js
+++ devtools/client/preferences/devtools.js
@@ -301,7 +301,11 @@ pref("devtools.webconsole.new-frontend-enabled", true);
 pref("devtools.webconsole.new-frontend-enabled", false);
 #endif
 
-// Enable the experimental support for source maps in console (work in progress)
+// Enable the server-side mapping service for source maps in console (deprecated)
+// NOTE: This approach to source maps uses server-side source maps, which we are
+// replacing with client-side source maps.  Do not use this in new code paths.
+// To be removed in bug 1349354.  Read more about ongoing work with source maps:
+// https://docs.google.com/document/d/19TKnMJD3CMBzwByNE4aBBVWnl-AEan8Sf4hxi6J-eps/edit
 pref("devtools.source-map.locations.enabled", false);
 
 // The number of lines that are displayed in the web console.
