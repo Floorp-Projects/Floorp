f7b1029d7bf6d1e79211dd0c18414cbbba5524bf
Tom Schuster <evilpies@gmail.com>
2017-03-21 18:48:50 +0100

    Bug 1348821 - For MegamorphicNativeSlot differentiate between HandleMissing. r=jandem

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -547,7 +547,7 @@ GetPropIRGenerator::attachMegamorphicNativeSlot(ObjOperandId objId, jsid id, boo
     }
     writer.typeMonitorResult();
 
-    trackAttached("MegamorphicNativeSlot");
+    trackAttached(handleMissing ? "MegamorphicMissingNativeSlot" : "MegamorphicNativeSlot");
 }
 
 bool
