d08e1cb76d4eea00efe27f3834a9ee24759b1f6e
Kevin Chen <kechen@mozilla.com>
2017-03-21 03:57:56 +0000

    Bug 1343754 - Add a function for replacing event target for actor and label PTexture; r=bevistseng,billm,nical
    MozReview-Commit-ID: KGVBWsexkqC

--- gfx/layers/ipc/VideoBridgeChild.cpp
+++ gfx/layers/ipc/VideoBridgeChild.cpp
@@ -108,7 +108,8 @@ VideoBridgeChild::CreateTexture(const SurfaceDescriptor& aSharedData,
                                 LayersBackend aLayersBackend,
                                 TextureFlags aFlags,
                                 uint64_t aSerial,
-                                wr::MaybeExternalImageId& aExternalImageId)
+                                wr::MaybeExternalImageId& aExternalImageId,
+                                nsIEventTarget* aTarget)
 {
   MOZ_ASSERT(CanSend());
   return SendPTextureConstructor(aSharedData, aLayersBackend, aFlags, aSerial);
