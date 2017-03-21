5fd1d453a987fc4852f6cae9b48303b11c996937
Nathan Froyd <froydnj@mozilla.com>
2017-03-21 10:20:36 -0500

    Bug 1312087 - part 1 - move mozilla::{Mutex,CondVar} to use mozglue locking primitives; r=erahm
    This change moves us away from NSPR primitives for our primary
    synchronization primitives.  We're still using PRMonitor for
    ReentrantMonitor, however.
    
    The benefits of this change:
    
    * Slightly faster, as we don't have to deal with some of NSPR's overhead;
    
    * Smaller datatypes.  On POSIX platforms in particular, PRLock is
      enormous. PRCondVar also has some unnecessary overhead.
    
    * Less dynamic memory allocation.  Out of necessity, Mutex and CondVar
      allocated the NSPR data structures they needed, which lead to
      unnecessary checks for failure.
    
      While sizeof(Mutex) and sizeof(CondVar) may get bigger, since they're
      embedding structures now, the total memory usage should be less.
    
    * Less NSPR usage.  This shouldn't need any explanation.

--- xpcom/threads/BlockingResourceBase.cpp
+++ xpcom/threads/BlockingResourceBase.cpp
@@ -379,18 +379,24 @@ void
 OffTheBooksMutex::Lock()
 {
   CheckAcquire();
-  PR_Lock(mLock);
-  Acquire();       // protected by mLock
+  this->lock();
+  mOwningThread = PR_GetCurrentThread();
+  Acquire();
 }
 
 void
 OffTheBooksMutex::Unlock()
 {
-  Release();                  // protected by mLock
-  PRStatus status = PR_Unlock(mLock);
-  NS_ASSERTION(PR_SUCCESS == status, "bad Mutex::Unlock()");
+  Release();
+  mOwningThread = nullptr;
+  this->unlock();
 }
 
+void
+OffTheBooksMutex::AssertCurrentThreadOwns() const
+{
+  MOZ_ASSERT(IsAcquired() && mOwningThread == PR_GetCurrentThread());
+}
 
 //
 // Debug implementation of ReentrantMonitor
@@ -491,18 +497,24 @@ CondVar::Wait(PRIntervalTime aInterval)
   // save mutex state and reset to empty
   AcquisitionState savedAcquisitionState = mLock->GetAcquisitionState();
   BlockingResourceBase* savedChainPrev = mLock->mChainPrev;
+  PRThread* savedOwningThread = mLock->mOwningThread;
   mLock->ClearAcquisitionState();
   mLock->mChainPrev = 0;
+  mLock->mOwningThread = nullptr;
 
   // give up mutex until we're back from Wait()
-  nsresult rv =
-    PR_WaitCondVar(mCvar, aInterval) == PR_SUCCESS ? NS_OK : NS_ERROR_FAILURE;
+  if (aInterval == PR_INTERVAL_NO_TIMEOUT) {
+    mImpl.wait(*mLock);
+  } else {
+    mImpl.wait_for(*mLock, TimeDuration::FromMilliseconds(double(aInterval)));
+  }
 
   // restore saved state
   mLock->SetAcquisitionState(savedAcquisitionState);
   mLock->mChainPrev = savedChainPrev;
+  mLock->mOwningThread = savedOwningThread;
 
-  return rv;
+  return NS_OK;
 }
 
 #endif // ifdef DEBUG
