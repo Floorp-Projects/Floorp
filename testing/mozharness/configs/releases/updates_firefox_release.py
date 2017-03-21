5357d41d3d246e25c99004847bd45bd88a449e39
Ben Hearsum <bhearsum@mozilla.com>
2017-03-21 11:01:46 -0400

    bug 1348922: make release automation update websense bypass rule. r=rail, a=release

--- testing/mozharness/configs/releases/updates_firefox_release.py
+++ testing/mozharness/configs/releases/updates_firefox_release.py
@@ -40,7 +40,7 @@ config = {
             "mar_channel_ids": [],
             "channel_names": ["release", "release-localtest", "release-cdntest"],
             "rules_to_update": ["firefox-release-cdntest", "firefox-release-localtest"],
-            "publish_rules": ["firefox-release"],
+            "publish_rules": ["firefox-release", "firefox-release-nowebsense-bypass"],
         },
     },
     "balrog_use_dummy_suffix": False,
