8cf97ab6b939e7281f13e2900cdda818c0aa654f
Gabor Krizsanits <gkrizsanits@mozilla.com>
2017-03-21 12:58:41 +0100

    Bug 1341008 - Stop rebroadcasting BlobURLUnregistration. r=baku, a=jcristau

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -3016,7 +3016,8 @@ ContentChild::RecvBlobURLRegistration(const nsCString& aURI, PBlobChild* aBlobCh
 mozilla::ipc::IPCResult
 ContentChild::RecvBlobURLUnregistration(const nsCString& aURI)
 {
-  nsHostObjectProtocolHandler::RemoveDataEntry(aURI);
+  nsHostObjectProtocolHandler::RemoveDataEntry(aURI,
+                                               /* aBroadcastToOtherProcesses = */ false);
   return IPC_OK();
 }
 
