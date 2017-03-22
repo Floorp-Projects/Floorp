b8d744d09c82226930c9bcf81507884e14ff77f9
Maja Frydrychowicz <mjzffr@gmail.com>
2017-03-22 14:26:18 -0400

    Bug 1347483 - blessings package should not be downloaded during mach virtualenv setup. r=mshal, a=test-only
    This fixes a regression introduced in Bug 1335873, which changes the mozbase
    packages.txt to call mozlog's setup.py. Calling setup.py registers the
    pytest_mozlog plugin for marionette-harness tests.
    
    Instead, we can register the pytest-mozlog plugin via command-line arguments
    to pytest, which are set in pytest.ini for the marionette-harness tests.
    As a result, we can revert the mozbase packages.txt to not refer to mozlog's
    setup.py
    
    I'm leaving the pytest entry-point in mozlog's setup.py so that external
    consumers don't have to register the pytest_mozlog plugin manually.
    
    MozReview-Commit-ID: I5wNq5H1x3X
    
    --HG--
    extra : source : 0cf4ce9588caa74a82f63374d8cc5e198434d351

--- testing/marionette/harness/marionette_harness/tests/harness_unit/test_httpd.py
+++ testing/marionette/harness/marionette_harness/tests/harness_unit/test_httpd.py
@@ -88,4 +88,4 @@ def test_handler(server):
 if __name__ == "__main__":
     import sys
     sys.exit(pytest.main(
-        ['-p', 'no:terminalreporter', '--log-tbpl=-', __file__]))
+        ['--log-tbpl=-', __file__]))
