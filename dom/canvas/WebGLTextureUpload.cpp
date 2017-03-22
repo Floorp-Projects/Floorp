e1398d7ecc0be125ddeb0b3b9aebba6f0425f58e
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-03-22 22:50:54 +0100

    Backed out changeset 5f62af954609 (bug 1333858) for bustage. r=backout on a CLOSED TREE

--- dom/canvas/WebGLTextureUpload.cpp
+++ dom/canvas/WebGLTextureUpload.cpp
@@ -2011,15 +2011,11 @@ DoCopyTexOrSubImage(WebGLContext* webgl, const char* funcName, bool isSubImage,
 
     ////
 
-    int32_t readX, readY;
-    int32_t writeX, writeY;
-    int32_t rwWidth, rwHeight;
-    if (!Intersect(srcTotalWidth, xWithinSrc, dstWidth, &readX, &writeX, &rwWidth) ||
-        !Intersect(srcTotalHeight, yWithinSrc, dstHeight, &readY, &writeY, &rwHeight))
-    {
-        webgl->ErrorOutOfMemory("%s: Bad subrect selection.", funcName);
-        return false;
-    }
+    uint32_t readX, readY;
+    uint32_t writeX, writeY;
+    uint32_t rwWidth, rwHeight;
+    Intersect(srcTotalWidth, xWithinSrc, dstWidth, &readX, &writeX, &rwWidth);
+    Intersect(srcTotalHeight, yWithinSrc, dstHeight, &readY, &writeY, &rwHeight);
 
     writeX += xOffset;
     writeY += yOffset;
