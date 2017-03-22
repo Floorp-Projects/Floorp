6d17601050c458da97f295fcd3dcc963b036cc55
Jan de Mooij <jdemooij@mozilla.com>
2017-03-22 15:47:21 +0100

    Bug 1346140 - Flatten external strings when creating dependent strings. r=jwalden, r=h4writer, a=lizzard

--- js/src/vm/String.cpp
+++ js/src/vm/String.cpp
@@ -928,6 +928,9 @@ AutoStableStringChars::init(JSContext* cx, JSString* s)
 
     MOZ_ASSERT(state_ == Uninitialized);
 
+    if (linearString->isExternal() && !linearString->ensureFlat(cx))
+        return false;
+
     // If the chars are inline then we need to copy them since they may be moved
     // by a compacting GC.
     if (baseIsInline(linearString)) {
@@ -958,6 +961,9 @@ AutoStableStringChars::initTwoByte(JSContext* cx, JSString* s)
 
     if (linearString->hasLatin1Chars())
         return copyAndInflateLatin1Chars(cx, linearString);
+
+    if (linearString->isExternal() && !linearString->ensureFlat(cx))
+        return false;
 
     // If the chars are inline then we need to copy them since they may be moved
     // by a compacting GC.
