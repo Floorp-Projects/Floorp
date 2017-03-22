f0c929ff2d0c437cefe6a10da4ef14767fe79a35
Michael Froman <mfroman@mozilla.com>
2017-03-21 19:59:05 -0500

    Bug 1345511 - pt 2 - add IPC mechanism for getting stun addrs on main process. r=bwc
    PStunAddrsRequest.ipdl defines the new IPC protocol to get stun
      addrs on the main process.
    StunAddrsRequestChild requests the stun addrs from the parent.
    StunAddrsRequestParent uses a static method on NrIceCtx to get the
      stun addrs from the STS thead and sends the addrs back to the
      child process.
    NrIceStunAddr (nricestunaddr.{cpp|h}) wraps nr_local_addr and makes
      it easier to serialize/deserialize over IPC.
    NrIceStunAddrMessageUtils follows the pattern used by other Necko
      IPC classes to define top-level serialization/deserialization
      calls used by the IPC framework.
    
    Modifications under netwerk/ipc are to connect the new IPC
    protocol to get stun addrs to PNecko since it is a network
    related IPC protocol.
    
    MozReview-Commit-ID: GyEapBe5krl
    
    --HG--
    extra : rebase_source : c650d6aa4f7928bcae6032424303869074a755d4

---
+++ media/mtransport/ipc/StunAddrsRequestChild.cpp
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "StunAddrsRequestChild.h"
+#include "mozilla/net/NeckoChild.h"
+
+using namespace mozilla::ipc;
+
+namespace mozilla {
+namespace net {
+
+StunAddrsRequestChild::StunAddrsRequestChild(StunAddrsListener* listener) :
+  mListener(listener)
+{
+  gNeckoChild->SendPStunAddrsRequestConstructor(this);
+  // IPDL holds a reference until IPDL channel gets destroyed
+  AddIPDLReference();
+}
+
+mozilla::ipc::IPCResult
+StunAddrsRequestChild::RecvOnStunAddrsAvailable(const NrIceStunAddrArray& addrs)
+{
+  if (mListener) {
+    mListener->OnStunAddrsAvailable(addrs);
+  }
+  return IPC_OK();
+}
+
+void
+StunAddrsRequestChild::Cancel()
+{
+  mListener = nullptr;
+}
+
+NS_IMPL_ADDREF(StunAddrsRequestChild)
+NS_IMPL_RELEASE(StunAddrsRequestChild)
+
+NS_IMPL_ADDREF(StunAddrsListener)
+NS_IMPL_RELEASE(StunAddrsListener)
+
+} // namespace net
+} // namespace mozilla
---
+++ media/mtransport/ipc/StunAddrsRequestChild.h
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_net_StunAddrsRequestChild_h
+#define mozilla_net_StunAddrsRequestChild_h
+
+#include "mozilla/net/PStunAddrsRequestChild.h"
+
+namespace mozilla {
+namespace net {
+
+class StunAddrsListener {
+public:
+  virtual void OnStunAddrsAvailable(const NrIceStunAddrArray& addrs) = 0;
+
+  NS_IMETHOD_(MozExternalRefCountType) AddRef();
+  NS_IMETHOD_(MozExternalRefCountType) Release();
+
+protected:
+  virtual ~StunAddrsListener() {}
+
+  ThreadSafeAutoRefCnt mRefCnt;
+  NS_DECL_OWNINGTHREAD
+};
+
+class StunAddrsRequestChild final : public PStunAddrsRequestChild
+{
+public:
+  explicit StunAddrsRequestChild(StunAddrsListener* listener);
+
+  NS_IMETHOD_(MozExternalRefCountType) AddRef();
+  NS_IMETHOD_(MozExternalRefCountType) Release();
+
+  // Not sure why AddIPDLReference & ReleaseIPDLReference don't come
+  // from PStunAddrsRequestChild since the IPC plumbing seem to
+  // expect this.
+  void AddIPDLReference() { AddRef(); }
+  void ReleaseIPDLReference() { Release(); }
+
+  void Cancel();
+
+protected:
+  virtual ~StunAddrsRequestChild() {}
+
+  virtual mozilla::ipc::IPCResult RecvOnStunAddrsAvailable(
+      const NrIceStunAddrArray& addrs) override;
+
+  RefPtr<StunAddrsListener> mListener;
+
+  ThreadSafeAutoRefCnt mRefCnt;
+  NS_DECL_OWNINGTHREAD
+};
+
+} // namespace net
+} // namespace mozilla
+
+#endif // mozilla_net_StunAddrsRequestChild_h
