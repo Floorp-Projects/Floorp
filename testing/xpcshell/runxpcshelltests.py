3f66a2627bf6fbf77d57c4c8aedadeacc8d4db5c
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-03-23 10:38:04 +0100

    Backed out changeset 18fd8676751a (bug 1333003)

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -942,9 +942,7 @@ class XPCShellTests(object):
         usingTSan = "tsan" in self.mozInfo and self.mozInfo["tsan"]
         if usingASan or usingTSan:
             # symbolizer support
-            llvmsym = os.path.join(
-                self.xrePath,
-                "llvm-symbolizer" + self.mozInfo["bin_suffix"].encode('ascii'))
+            llvmsym = os.path.join(self.xrePath, "llvm-symbolizer")
             if os.path.isfile(llvmsym):
                 if usingASan:
                     self.env["ASAN_SYMBOLIZER_PATH"] = llvmsym
