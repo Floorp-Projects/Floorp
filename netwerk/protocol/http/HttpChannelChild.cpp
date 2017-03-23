acf1c25201d122edc5101a507d378f1a18d05e06
Kershaw Chang <kechang@mozilla.com>
2017-03-23 23:31:00 +0100

    Bug 1343750 - Part1: Add an API to get Dispatcher from nsILoadInfo, r=mayhemer

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -2070,46 +2070,27 @@ HttpChannelChild::SetEventTarget()
 {
   nsCOMPtr<nsILoadInfo> loadInfo;
   GetLoadInfo(getter_AddRefs(loadInfo));
-  if (!loadInfo) {
+
+  RefPtr<Dispatcher> dispatcher =
+    nsContentUtils::GetDispatcherByLoadInfo(loadInfo);
+
+  if (!dispatcher) {
     return;
   }
 
-  nsCOMPtr<nsIDOMDocument> domDoc;
-  loadInfo->GetLoadingDocument(getter_AddRefs(domDoc));
-  nsCOMPtr<nsIDocument> doc = do_QueryInterface(domDoc);
-  RefPtr<Dispatcher> dispatcher;
-  if (doc) {
-    dispatcher = doc->GetDocGroup();
-  } else {
-    // There's no document yet, but this might be a top-level load where we can
-    // find a TabGroup.
-    uint64_t outerWindowId;
-    if (NS_FAILED(loadInfo->GetOuterWindowID(&outerWindowId))) {
-      // No window. This might be an add-on XHR, a service worker request, or
-      // something else.
-      return;
-    }
-    RefPtr<nsGlobalWindow> window = nsGlobalWindow::GetOuterWindowWithId(outerWindowId);
-    if (!window) {
-      return;
-    }
-
 #ifdef DEBUG
+  if (dispatcher->AsTabGroup()) {
     // We have a TabGroup. This must be a top-level load.
     bool isMainDocumentChannel;
     GetIsMainDocumentChannel(&isMainDocumentChannel);
     MOZ_ASSERT(isMainDocumentChannel);
+  }
 #endif
 
-    dispatcher = window->TabGroup();
-  }
-
-  if (dispatcher) {
-    nsCOMPtr<nsIEventTarget> target =
-      dispatcher->EventTargetFor(TaskCategory::Network);
-    gNeckoChild->SetEventTargetForActor(this, target);
-    mEventQ->RetargetDeliveryTo(target);
-  }
+  nsCOMPtr<nsIEventTarget> target =
+    dispatcher->EventTargetFor(TaskCategory::Network);
+  gNeckoChild->SetEventTargetForActor(this, target);
+  mEventQ->RetargetDeliveryTo(target);
 }
 
 nsresult
