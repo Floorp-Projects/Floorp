93d11e3441f899e7cddcffe7cc33873d9135b7eb
Wes Kocher <wkocher@mozilla.com>
2017-03-23 11:01:44 -0700

    Backed out 7 changesets (bug 1333003) for windows asan failures a=backout
    Backed out changeset 3d2b2eeda8d3 (bug 1333003)
    Backed out changeset 400d409ba4ca (bug 1333003)
    Backed out changeset 1ba027abdfc9 (bug 1333003)
    Backed out changeset 70114135bd8c (bug 1333003)
    Backed out changeset 5715b15e33c0 (bug 1333003)
    Backed out changeset 375e952bd738 (bug 1333003)
    Backed out changeset d5d4112599f2 (bug 1333003)
    
    MozReview-Commit-ID: DZUHJTdjX7V

--- testing/gtest/rungtests.py
+++ testing/gtest/rungtests.py
@@ -128,9 +128,7 @@ class GTests(object):
         # ASan specific environment stuff
         if mozinfo.info["asan"]:
             # Symbolizer support
-            llvmsym = os.path.join(
-                self.xre_path,
-                "llvm-symbolizer" + mozinfo.info["bin_suffix"].encode('ascii'))
+            llvmsym = os.path.join(self.xre_path, "llvm-symbolizer")
             if os.path.isfile(llvmsym):
                 env["ASAN_SYMBOLIZER_PATH"] = llvmsym
                 log.info("gtest | ASan using symbolizer at %s", llvmsym)
