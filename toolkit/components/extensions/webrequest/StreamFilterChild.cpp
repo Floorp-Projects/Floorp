1844b31782c3fa2186990d19af953b07f43a97ae
Kris Maglione <maglione.k@gmail.com>
2017-03-22 20:33:05 -0700

    Bug 1255894: Part 3 - Create skeleton IPDL framework for OOP stream filters. r=baku
    This interface will allow extensions running into a content process to attach
    a filtering stream listener to an HTTP request in the parent process. The
    content process attaches a listener by sending a message from the content
    process containing the ID of the request to filter, and the ID of the add-on
    making the request. The permissions and request mappings for this are handled
    by the web request service added in part 2.
    
    MozReview-Commit-ID: B7Dd3ywwCBX

---
+++ toolkit/components/extensions/webrequest/StreamFilterChild.cpp
@@ -0,0 +1,18 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "StreamFilterChild.h"
+
+namespace mozilla {
+namespace extensions {
+
+void
+StreamFilterChild::ActorDestroy(ActorDestroyReason aWhy)
+{
+}
+
+} // namespace extensions
+} // namespace mozilla
