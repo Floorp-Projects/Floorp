6aa94b64562795a04acf12f3884cb8d3290d46a6
Nicholas Nethercote <nnethercote@mozilla.com>
2017-03-23 17:15:54 +1100

    Bug 1349856 - Reinstate proper usage of mReflowCause. r=froydnj.
    Bug 1322553 introduced GeckoProfilerTracingRAII. Part 2 converted a bunch of
    places to use it, but messed one of them up:
    
    > -          profiler_tracing("Paint", "Reflow", Move(mReflowCause), TRACING_INTERVAL_START);
    > +          tracingLayoutFlush.emplace("Paint", "Reflow");
    
    The Move(mReflowCause) was erroneously deleted. (There was a similar case with
    Move(mStyleCause) that was not deleted.)
    
    This patch reinstates the Move(mReflowCause).
    
    --HG--
    extra : rebase_source : 72aa0be43f5e381ddd7999cb2f56a44051f18779

--- layout/base/nsRefreshDriver.cpp
+++ layout/base/nsRefreshDriver.cpp
@@ -1891,7 +1891,7 @@ nsRefreshDriver::Tick(int64_t aNowEpoch, TimeStamp aNowTime)
           continue;
 
         if (!tracingLayoutFlush) {
-          tracingLayoutFlush.emplace("Paint", "Reflow");
+          tracingLayoutFlush.emplace("Paint", "Reflow", Move(mReflowCause));
           mReflowCause = nullptr;
         }
 
