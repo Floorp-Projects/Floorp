8b4c678479f066cc29af9da5f615df2568079456
Alastor Wu <alwu@mozilla.com>
2017-03-24 12:01:43 +0800

    Bug 1347758 - part1 : window should know whehter there is any alive media component and decide to resume the tab or not. r=Ehsan
    We should let window decide whether resume the media component, not document.
    Because we might have media component which is not in DOM tree, but we still
    want to play it.
    
    Window should be resumed when all following conditions are true,
    (1) the tab is in the foreground
    (2) there is any alive media component (MediaElement/WebAudio/PlugIn...)
    (3) the window is blocked (nsISuspendedTypes::SUSPENDED_BLOCK)
    
    MozReview-Commit-ID: JXw5MA4FCxF
    
    --HG--
    extra : rebase_source : 416113dd2282a979ea26c4694878c2f2db578274

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -1327,7 +1327,6 @@ nsIDocument::nsIDocument()
     mFontFaceSetDirty(true),
     mGetUserFontSetCalled(false),
     mPostedFlushUserFontSet(false),
-    mEverInForeground(false),
     mCompatMode(eCompatibility_FullStandards),
     mReadyState(ReadyState::READYSTATE_UNINITIALIZED),
     mStyleBackendType(StyleBackendType::None),
@@ -1426,8 +1425,6 @@ nsDocument::nsDocument(const char* aContentType)
 
   // void state used to differentiate an empty source from an unselected source
   mPreloadPictureFoundSource.SetIsVoid(true);
-
-  mEverInForeground = false;
 }
 
 void
@@ -12212,19 +12209,10 @@ nsDocument::PostVisibilityUpdateEvent()
 void
 nsDocument::MaybeActiveMediaComponents()
 {
-  if (mEverInForeground) {
-    return;
-  }
-
   if (!mWindow) {
     return;
   }
 
-  if (mMediaContent.IsEmpty()) {
-    return;
-  }
-
-  mEverInForeground = true;
   GetWindow()->MaybeActiveMediaComponents();
 }
 
