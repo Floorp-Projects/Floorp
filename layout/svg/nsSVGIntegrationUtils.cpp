e492f484624ce95026c4ed6e13b150fd6fed6c5f
Ryan VanderMeulen <ryanvm@gmail.com>
2017-03-24 15:58:42 -0400

    Backed out changeset 12a4f938b5aa (bug 1345853) for bustage.

--- layout/svg/nsSVGIntegrationUtils.cpp
+++ layout/svg/nsSVGIntegrationUtils.cpp
@@ -921,6 +921,7 @@ nsSVGIntegrationUtils::PaintMaskAndClipPath(const PaintFramesParams& aParams)
       result &= paintResult.result;
       maskSurface = paintResult.maskSurface;
       if (maskSurface) {
+        MOZ_ASSERT(paintResult.result == DrawResult::SUCCESS);
         shouldPushMask = true;
         maskTransform = paintResult.maskTransform;
         opacityApplied = paintResult.opacityApplied;
