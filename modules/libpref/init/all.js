1e8f85e2f445758112c3e6c568869040e500b2aa
Gerald Squelart <gsquelart@mozilla.com>
2017-03-24 16:37:45 +1100

    Bug 1343442 - Allow MediaDecodeError/Warning user notifications in Nightly - r=jya
    We want the new MediaDecodeError messages to be forwarded to the front-end in
    browser-media.js (even though they won't be handled just yet; an upcoming patch
    will add the handling code).
    
    This is limited to Nightly for now, like the "Report Site Issues" button,
    because we forward URLs to webcompat.com (at the user's request).
    
    And MediaDecodeWarning was not added, as it may cause too much annoyance, but
    it is available for testers to get more diagnostics information if needed.
    
    MozReview-Commit-ID: HPDpA1mg4HX

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -385,7 +385,11 @@ pref("media.gmp.storage.version.expected", 1);
 
 // Filter what triggers user notifications.
 // See DecoderDoctorDocumentWatcher::ReportAnalysis for details.
+#ifdef NIGHTLY_BUILD
+pref("media.decoder-doctor.notifications-allowed", "MediaWMFNeeded,MediaWidevineNoWMF,MediaCannotInitializePulseAudio,MediaCannotPlayNoDecoders,MediaUnsupportedLibavcodec,MediaDecodeError");
+#else
 pref("media.decoder-doctor.notifications-allowed", "MediaWMFNeeded,MediaWidevineNoWMF,MediaCannotInitializePulseAudio,MediaCannotPlayNoDecoders,MediaUnsupportedLibavcodec");
+#endif
 // Whether we report partial failures.
 pref("media.decoder-doctor.verbose", false);
 // Whether DD should consider WMF-disabled a WMF failure, useful for testing.
