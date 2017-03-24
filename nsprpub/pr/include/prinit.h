1e3b452a93d454e0fdd934ca075e182d029b8b8a
Kai Engert <kaie@kuix.de>
2017-03-24 14:58:00 +0100

    Bug 1350291, uplift NSPR_4_15_BETA, r=franziskus

--- nsprpub/pr/include/prinit.h
+++ nsprpub/pr/include/prinit.h
@@ -31,11 +31,11 @@ PR_BEGIN_EXTERN_C
 ** The format of the version string is
 **     "<major version>.<minor version>[.<patch level>] [<Beta>]"
 */
-#define PR_VERSION  "4.14"
+#define PR_VERSION  "4.15 Beta"
 #define PR_VMAJOR   4
-#define PR_VMINOR   14
+#define PR_VMINOR   15
 #define PR_VPATCH   0
-#define PR_BETA     PR_FALSE
+#define PR_BETA     PR_TRUE
 
 /*
 ** PRVersionCheck
