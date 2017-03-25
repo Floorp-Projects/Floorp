6e571eaff542ce8ac5801ad4d29855cd945af7e0
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-03-25 03:01:19 +0200

    Bug 1349946, ensure expected focus handling when redirecting focus, r=enndeakin a=al

--- layout/generic/nsPluginFrame.cpp
+++ layout/generic/nsPluginFrame.cpp
@@ -1650,8 +1650,10 @@ nsPluginFrame::HandleEvent(nsPresContext* aPresContext,
   }
   else if (anEvent->mMessage == ePluginFocus) {
     nsIFocusManager* fm = nsFocusManager::GetFocusManager();
-    if (fm)
-      return fm->FocusPlugin(GetContent());
+    if (fm) {
+      nsCOMPtr<nsIContent> content = GetContent();
+      return fm->FocusPlugin(content);
+    }
   }
 
   if (mInstanceOwner->SendNativeEvents() &&
