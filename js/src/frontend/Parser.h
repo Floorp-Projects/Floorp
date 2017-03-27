0d1b41f17ab47c1715f01f9c7b7ec0cfda5bbda4
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-03-27 17:25:03 +0200

    Backed out changeset 3701b28662ac (bug 1331092)

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -13,7 +13,6 @@
 #include "mozilla/Maybe.h"
 #include "mozilla/TypeTraits.h"
 
-#include "jsiter.h"
 #include "jspubtd.h"
 
 #include "frontend/BytecodeCompiler.h"
@@ -1176,7 +1175,6 @@ class Parser final : public ParserBase, private JS::AutoGCRooter
 
     Node forStatement(YieldHandling yieldHandling);
     bool forHeadStart(YieldHandling yieldHandling,
-                      IteratorKind iterKind,
                       ParseNodeKind* forHeadKind,
                       Node* forInitialPart,
                       mozilla::Maybe<ParseContext::Scope>& forLetImpliedScope,
