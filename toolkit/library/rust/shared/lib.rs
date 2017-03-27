e86ce3e5cc8014961195a7326192d963ecfe833b
Ted Mielczarek <ted@mielczarek.org>
2017-03-27 14:40:22 -0400

    bug 1275780 - capture Rust panic message in crash reports. r=froydnj
    MozReview-Commit-ID: IUlYqPEtkgg

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -11,11 +11,58 @@ extern crate rust_url_capi;
 #[cfg(feature = "quantum_render")]
 extern crate webrender_bindings;
 
+use std::boxed::Box;
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::panic;
 
 /// Used to implement `nsIDebug2::RustPanic` for testing purposes.
 #[no_mangle]
 pub extern "C" fn intentional_panic(message: *const c_char) {
     panic!("{}", unsafe { CStr::from_ptr(message) }.to_string_lossy());
+}
+
+/// Contains the panic message, if set.
+static mut PANIC_REASON: Option<(*const str, usize)> = None;
+
+/// Configure a panic hook to capture panic messages for crash reports.
+///
+/// We don't store this in `gMozCrashReason` because:
+/// a) Rust strings aren't null-terminated, so we'd have to allocate
+///    memory to get a null-terminated string
+/// b) The panic=abort handler is going to call `abort()` on non-Windows,
+///    which is `mozalloc_abort` for us, which will use `MOZ_CRASH` and
+///    overwrite `gMozCrashReason` with an unhelpful string.
+#[no_mangle]
+pub extern "C" fn install_rust_panic_hook() {
+    panic::set_hook(Box::new(|info| {
+        // Try to handle &str/String payloads, which should handle 99% of cases.
+        let payload = info.payload();
+        // We'll hold a raw *const str here, but it will be OK because
+        // Rust is going to abort the process before the payload could be
+        // deallocated.
+        if let Some(s) = payload.downcast_ref::<&str>() {
+            unsafe { PANIC_REASON = Some((*s as *const str, s.len())) }
+        } else if let Some(s) = payload.downcast_ref::<String>() {
+            unsafe { PANIC_REASON = Some((s.as_str() as *const str, s.len())) }
+        } else {
+            // Not the most helpful thing, but seems unlikely to happen
+            // in practice.
+            println!("Unhandled panic payload!");
+        }
+    }));
+}
+
+#[no_mangle]
+pub extern "C" fn get_rust_panic_reason(reason: *mut *const c_char, length: *mut usize) -> bool {
+    unsafe {
+        match PANIC_REASON {
+            Some((s, len)) => {
+                *reason = s as *const c_char;
+                *length = len;
+                true
+            }
+            None => false,
+        }
+    }
 }
