a595fd9b6c6e0d664826578db220332efecd172f
Cykesiopka <cykesiopka.bmo@gmail.com>
2017-03-27 22:56:38 +0800

    Bug 1350599 - Use guaranteed preloaded test domains instead of real domains in HSTS tests. r=keeler, a=test-only
    Some of our tests currently assume that certain real domains are HSTS preloaded.
    While most of the time these domains are in fact preloaded, this may change
    during periods of maintenance or other events.
    
    To avoid this, the changes here perform the following renames:
    bugzilla.mozilla.org -> includesubdomains.preloaded.test
    login.persona.org -> includesubdomains2.preloaded.test
    www.torproject.org -> noincludesubdomains.preloaded.test
    
    In addition, some tests that refer to mozilla.com (but don't depend on it being
    preloaded) are made to refer to example.com instead to avoid referring to real
    domains in tests.
    
    MozReview-Commit-ID: 3987moJnKGk
    
    --HG--
    extra : source : 9cd5529dcc38c97971c37b3fa483d16187917792

--- security/manager/ssl/tests/unit/sss_readstate_child_worker.js
+++ security/manager/ssl/tests/unit/sss_readstate_child_worker.js
@@ -12,11 +12,11 @@ function run_test() {
                            Services.io.newURI("https://notexpired.example.com"),
                            0));
   ok(SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS,
-                           Services.io.newURI("https://bugzilla.mozilla.org"),
+                           Services.io.newURI("https://includesubdomains.preloaded.test"),
                            0));
   ok(!SSService.isSecureURI(
        Ci.nsISiteSecurityService.HEADER_HSTS,
-       Services.io.newURI("https://sub.bugzilla.mozilla.org"), 0));
+       Services.io.newURI("https://sub.includesubdomains.preloaded.test"), 0));
   ok(SSService.isSecureURI(
        Ci.nsISiteSecurityService.HEADER_HSTS,
        Services.io.newURI("https://incsubdomain.example.com"), 0));
@@ -24,10 +24,10 @@ function run_test() {
        Ci.nsISiteSecurityService.HEADER_HSTS,
        Services.io.newURI("https://sub.incsubdomain.example.com"), 0));
   ok(!SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS,
-                            Services.io.newURI("https://login.persona.org"),
+                            Services.io.newURI("https://includesubdomains2.preloaded.test"),
                             0));
   ok(!SSService.isSecureURI(Ci.nsISiteSecurityService.HEADER_HSTS,
-                            Services.io.newURI("https://sub.login.persona.org"),
+                            Services.io.newURI("https://sub.includesubdomains2.preloaded.test"),
                             0));
   do_test_finished();
 }
