d99d9dea197b9a0dda9126d11e596ff627532ece
David Anderson <dvander@alliedmods.net>
2017-03-27 14:02:50 -0700

    Fix DXGITextureHostD3D11 not being able to lock without a compositor. (bug 1350247, r=mattwoodrow)

--- gfx/layers/d3d11/TextureD3D11.cpp
+++ gfx/layers/d3d11/TextureD3D11.cpp
@@ -737,20 +737,21 @@ DXGITextureHostD3D11::GetDevice()
   if (mFlags & TextureFlags::INVALID_COMPOSITOR) {
     return nullptr;
   }
-
-  return mProvider->GetD3D11Device();
+  return mDevice;
 }
 
 void
 DXGITextureHostD3D11::SetTextureSourceProvider(TextureSourceProvider* aProvider)
 {
   if (!aProvider || !aProvider->GetD3D11Device()) {
+    mDevice = nullptr;
     mProvider = nullptr;
     mTextureSource = nullptr;
     return;
   }
 
   mProvider = aProvider;
+  mDevice = aProvider->GetD3D11Device();
 
   if (mTextureSource) {
     mTextureSource->SetTextureSourceProvider(aProvider);
@@ -776,6 +777,9 @@ DXGITextureHostD3D11::LockWithoutCompositor()
   // Unlike the normal Lock() function, this function may be called when
   // mCompositor is nullptr such as during WebVR frame submission. So, there is
   // no 'mCompositor' checking here.
+  if (!mDevice) {
+    mDevice = DeviceManagerDx::Get()->GetCompositorDevice();
+  }
   return LockInternal();
 }
 
