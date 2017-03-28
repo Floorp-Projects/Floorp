c98698f1f1869d036e9831cdcedb3827404b6e60
Matteo Ferretti <mferretti@mozilla.com>
2017-03-28 12:40:22 +0200

    Bug 1349275 - Refactor `moveInfobar` function. r=pbro, a=lizzard
    - Added `getViewportDimensions`
    - Added `getComputedStylePropertyValue` to `CanvasFrameAnonymousContentHelper`
    - Refactored totally `moveInfobar` to works with both APZ enabled and new
      positioned absolutely highlighters
    - Updated `AutoRefreshHighlighter` for having a `scrollUpdate` method.
    - Updated tests
    
    MozReview-Commit-ID: 5m31ZzRzLXr

--- devtools/server/actors/highlighters/box-model.js
+++ devtools/server/actors/highlighters/box-model.js
@@ -342,6 +342,10 @@ BoxModelHighlighter.prototype = extend(AutoRefreshHighlighter.prototype, {
     return shown;
   },
 
+  _scrollUpdate: function () {
+    this._moveInfobar();
+  },
+
   /**
    * Hide the highlighter, the outline and the infobar.
    */
@@ -498,7 +502,7 @@ BoxModelHighlighter.prototype = extend(AutoRefreshHighlighter.prototype, {
     }
 
     // Un-zoom the root wrapper if the page was zoomed.
-    let rootId = this.ID_CLASS_PREFIX + "root";
+    let rootId = this.ID_CLASS_PREFIX + "elements";
     this.markup.scaleRootElement(this.currentNode, rootId);
 
     return true;
