278d51c7bd9a3815608895b029afba6d0ce0928a
Aryeh Gregor <ayg@aryeh.name>
2017-03-28 15:55:05 +0300

    Bug 1314388 - Remove SVGZoomEvent; r=longsonr,smaug
    Some code in SVGSVGElement.cpp might be unnecessary now, but Robert said
    to leave it for a followup.
    
    MozReview-Commit-ID: 8PpRGeGrREJ

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -1119,8 +1119,6 @@ var interfaceNamesInGlobalScope =
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "SVGZoomAndPan",
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    "SVGZoomEvent",
-// IMPORTANT: Do not change this list without review from a DOM peer!
     "Text",
 // IMPORTANT: Do not change this list without review from a DOM peer!
     "TextDecoder",
