5bed2bfda58a9a335e86607ce9f7e2dcc05da136
Iris Hsiao <ihsiao@mozilla.com>
2017-03-28 15:23:59 +0800

    Backed out changeset 6e4ccddb5c4b (bug 1350671) for build bustage

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -46,29 +46,6 @@ class ServoStyleSet
 {
   friend class ServoRestyleManager;
 public:
-  class AutoAllowStaleStyles
-  {
-  public:
-    AutoAllowStaleStyles(ServoStyleSet* aStyleSet)
-      : mStyleSet(aStyleSet)
-    {
-      if (mStyleSet) {
-        MOZ_ASSERT(!mStyleSet->mAllowResolveStaleStyles);
-        mStyleSet->mAllowResolveStaleStyles = true;
-      }
-    }
-
-    ~AutoAllowStaleStyles()
-    {
-      if (mStyleSet) {
-        mStyleSet->mAllowResolveStaleStyles = false;
-      }
-    }
-
-  private:
-    ServoStyleSet* mStyleSet;
-  };
-
   static bool IsInServoTraversal()
   {
     // The callers of this function are generally main-thread-only _except_
@@ -319,7 +296,6 @@ private:
   EnumeratedArray<SheetType, SheetType::Count,
                   nsTArray<RefPtr<ServoStyleSheet>>> mSheets;
   int32_t mBatching;
-  bool mAllowResolveStaleStyles;
 
   // Stores pointers to our cached style contexts for non-inheriting anonymous
   // boxes.
