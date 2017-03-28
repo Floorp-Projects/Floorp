d18618bd5e935d045372d350125690c357a08ab8
Ricky Chien <ricky060709@gmail.com>
2017-03-28 18:51:26 +0800

    Bug 1350215 - Rename components from camelcase to hyphens r=Honza
    MozReview-Commit-ID: Em8AYdGzTcb
    
    --HG--
    rename : devtools/client/netmonitor/src/components/CustomRequestPanel.js => devtools/client/netmonitor/src/components/custom-request-panel.js
    rename : devtools/client/netmonitor/src/components/Editor.js => devtools/client/netmonitor/src/components/editor.js
    rename : devtools/client/netmonitor/src/components/MDNLink.js => devtools/client/netmonitor/src/components/mdn-link.js
    rename : devtools/client/netmonitor/src/components/PreviewPanel.js => devtools/client/netmonitor/src/components/preview-panel.js
    rename : devtools/client/netmonitor/src/components/RequestListEmptyNotice.js => devtools/client/netmonitor/src/components/request-list-empty-notice.js
    rename : devtools/client/netmonitor/src/components/RequestListHeader.js => devtools/client/netmonitor/src/components/request-list-header.js
    rename : devtools/client/netmonitor/src/components/RequestListItem.js => devtools/client/netmonitor/src/components/request-list-item.js
    rename : devtools/client/netmonitor/src/components/StatisticsPanel.js => devtools/client/netmonitor/src/components/statistics-panel.js
    rename : devtools/client/netmonitor/src/components/TimingsPanel.js => devtools/client/netmonitor/src/components/timings-panel.js
    rename : devtools/client/netmonitor/src/components/Toolbar.js => devtools/client/netmonitor/src/components/toolbar.js
    extra : rebase_source : e889b5a89f9915c5a03dab400de01d573e07b6fb

--- devtools/client/netmonitor/src/components/request-list.js
+++ devtools/client/netmonitor/src/components/request-list.js
@@ -11,9 +11,9 @@ const {
 } = require("devtools/client/shared/vendor/react");
 
 // Components
-const RequestListContent = createFactory(require("./RequestListContent"));
-const RequestListEmptyNotice = createFactory(require("./RequestListEmptyNotice"));
-const RequestListHeader = createFactory(require("./RequestListHeader"));
+const RequestListContent = createFactory(require("./request-list-content"));
+const RequestListEmptyNotice = createFactory(require("./request-list-empty-notice"));
+const RequestListHeader = createFactory(require("./request-list-header"));
 
 const { div } = DOM;
 
