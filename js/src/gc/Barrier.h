fc6d82690887fe645a52a7bb2ad9df4fe5e01197
Jon Coppeard <jcoppeard@mozilla.com>
2017-03-28 10:34:26 +0100

    Backed out changeset 2c8aea7a6fa9 (bug 1349637) for intermittent assertion failures

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -251,12 +251,6 @@ CheckEdgeIsNotBlackToGray(JSObject* src, const Value& dst)
     MOZ_ASSERT_IF(IsMarkedBlack(src), JS::ValueIsNotGray(dst));
 }
 
-MOZ_ALWAYS_INLINE void
-CheckEdgeIsNotBlackToGray(JSObject* src, gc::Cell* dst)
-{
-    MOZ_ASSERT_IF(IsMarkedBlack(src), JS::CellIsNotGray(dst));
-}
-
 template <typename T>
 struct InternalBarrierMethods {};
 
