b89d0dc9d826c2baa9ff4a4def550da02eb8b6cf
Kartikaya Gupta <kgupta@mozilla.com>
2017-03-28 16:40:24 -0400

    Bug 1351384 - Stop using the CompositorOptions to check if WebRender is enabled. r=dvander
    I suspect that the PuppetWidget is trying to create the layer manager after
    it has been connected to a TabChild but before the TabChild has populated the
    CompositorOptions. This results in the PuppetWidget effectively getting an
    uninitialized value for the CompositorOptions, and so it sometimes randomly
    creates a WebRenderLayerManager, later resulting in a crash.
    
    It seems like exposing the potentially-uninitialized CompositorOptions from
    TabChild like this is a bad idea, so I'm removing that API and using the more
    reliable gfxVars in PuppetWidget. This is fine for WebRender purposes because
    we no longer care to allow having WR compositors co-exist with non-WR
    compositors.
    
    We may eventually want to remove the CompositorOptions entirely, but for now
    the rest of the usage of it seems fine.
    
    MozReview-Commit-ID: 6ekG8j1PskK

--- dom/ipc/TabChild.h
+++ dom/ipc/TabChild.h
@@ -620,7 +620,6 @@ public:
     return mParentIsActive;
   }
 
-  const mozilla::layers::CompositorOptions& GetCompositorOptions() const;
   bool AsyncPanZoomEnabled() const;
 
   virtual ScreenIntSize GetInnerSize() override;
