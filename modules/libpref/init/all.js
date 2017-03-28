0eb89125df3f3242962b1149e44f6d86aeab2754
bechen <bechen@mozilla.com>
2017-03-28 15:01:13 +0800

    Bug 1318542 - Add preference "media.webvtt.pseudo.enabled". r=rillian
    MozReview-Commit-ID: BrdCPgapAYi

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -545,6 +545,9 @@ pref("media.getusermedia.audiocapture.enabled", false);
 // TextTrack WebVTT Region extension support.
 pref("media.webvtt.regions.enabled", false);
 
+// WebVTT pseudo element and class support.
+pref("media.webvtt.pseudo.enabled", true);
+
 // AudioTrack and VideoTrack support
 pref("media.track.enabled", false);
 
