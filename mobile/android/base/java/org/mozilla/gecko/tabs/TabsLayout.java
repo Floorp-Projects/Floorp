4e47c2dc88618ed5e9540a5490d3f30d9cc5d85b
Tom Klein <twointofive@gmail.com>
2017-03-28 11:29:52 -0500

    Bug 1350718 - 1. Scroll to a tab added to the tabs tray by an external app. r=maliu
    If another app opens a link in Fennec, and Fennec restores itself in a state
    where the tabs tray is already open, we need to scroll to the newly added tab
    since it gets added offscreen (not to mention the scroll position restored when
    we open is unconstrained (it's whatever the user left it at before they switched
    apps)).
    
    This introduces one small change in behavior:
    1) Use a gridded tabs tray;
    2) Fill more tabs than will fit in the tray;
    3) Put more than one tab on the last row;
    4) Scroll so that the last row is partially, but not fully, hidden;
    5) Close the last tab and then undo the close.
    In that case we now scroll the last row fully into view, whereas previously we
    maintained the old (partially hidden) scroll position.  (If you undo close any
    tab other than the last on the final row then you still get the old behavior.)
    
    Note that this fixes the case where the other app adds a *new* tab in Fennec
    with the tabs tray open; it's (currently) also possible to open a link in an
    already existing tab with the tabs tray open - that's bug 1353226.
    
    MozReview-Commit-ID: BazXFwT0B8v

--- mobile/android/base/java/org/mozilla/gecko/tabs/TabsLayout.java
+++ mobile/android/base/java/org/mozilla/gecko/tabs/TabsLayout.java
@@ -105,7 +105,9 @@ public abstract class TabsLayout extends RecyclerView
 
         switch (msg) {
             case ADDED:
-                final int tabIndex = Integer.parseInt(data);
+                int tabIndex = Integer.parseInt(data);
+                // A tabIndex of -1 means "add a new last tab".
+                tabIndex = tabIndex == -1 ? tabsAdapter.getItemCount() : tabIndex;
                 tabsAdapter.notifyTabInserted(tab, tabIndex);
                 if (addAtIndexRequiresScroll(tabIndex)) {
                     // (The SELECTED tab is updated *after* this call to ADDED, so don't just call
@@ -132,8 +134,9 @@ public abstract class TabsLayout extends RecyclerView
     }
 
     /**
-     * Addition of a tab at selected positions (dependent on LayoutManager) will result in a tab
-     * being added out of view - return true if {@code index} is such a position.
+     * Addition of a tab at selected positions (dependent on LayoutManager) can result in a tab
+     * being added out of view - return true if {@code index} is such a position.  This should be
+     * called only after the add has occurred.
      */
     abstract protected boolean addAtIndexRequiresScroll(int index);
 
