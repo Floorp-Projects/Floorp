c38b4b4056d6a78f0e3d0bb53ae365e6495c2b3e
Ted Campbell <tcampbell@mozilla.com>
2017-03-27 20:40:31 -0400

    Bug 1351951 - Add ICStubCompiler::assumeStubFrame
    MozReview-Commit-ID: FKwYMjDqCLf

--- js/src/jit/SharedIC.h
+++ js/src/jit/SharedIC.h
@@ -1073,6 +1073,7 @@ class ICStubCompiler
     // performing a tail call. This is required for the return address
     // to pc mapping to work.
     void enterStubFrame(MacroAssembler& masm, Register scratch);
+    void assumeStubFrame(MacroAssembler& masm);
     void leaveStubFrame(MacroAssembler& masm, bool calledIntoIon = false);
 
     // Some stubs need to emit Gecko Profiler updates.  This emits the guarding
