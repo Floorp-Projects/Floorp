c4bb96cf7462e204870e97b4a18faf54be4370f3
Henrik Skupin <mail@hskupin.info>
2017-03-28 21:41:38 +0200

    Bug 1291320 - Refactor page load algorithm for listener framescript. r=ato,automatedtester
    This refactoring allows us to re-use the same load algorithm for
    each command which could trigger a page load. It also takes remoteness
    changes into account, and waits until the load has been done.
    
    With this change we no longer check for readyState only, but observe
    the necessary DOM events as fired for page unloads and loads. This will
    help us to implement the page loading strategy later.
    
    By observing the DOM events, I also expect a small increase of performance
    for any kind of page load, given that we now return immediately and do not
    have a delay of 100ms at maximum.
    
    MozReview-Commit-ID: IVtO6KgJFES

--- testing/marionette/navigate.js
+++ testing/marionette/navigate.js
@@ -14,106 +14,30 @@ this.navigate = {};
 
 /**
  * Determines if we expect to get a DOM load event (DOMContentLoaded)
- * on navigating to the |future| URL.
+ * on navigating to the |url|.
  *
- * @param {string} current
- *     URL the browser is currently visiting.
- * @param {string=} future
- *     Destination URL, if known.
+ * @param {string} url
+ *     Destination URL
  *
  * @return {boolean}
- *     Full page load would be expected if future is followed.
+ *     Full page load would be expected if url gets loaded.
  *
  * @throws TypeError
- *     If |current| is not defined, or any of |current| or |future|
- *     are invalid URLs.
+ *     If |url| is an invalid URL.
  */
-navigate.isLoadEventExpected = function (current, future = undefined) {
-  if (typeof current == "undefined") {
-    throw TypeError("Expected at least one URL");
-  }
-
+navigate.isLoadEventExpected = function (url) {
   // assume we will go somewhere exciting
-  if (typeof future == "undefined") {
-    return true;
+  if (typeof url == "undefined") {
+    throw TypeError("Expected destination URL");
   }
 
-  let cur = new navigate.IdempotentURL(current);
-  let fut = new navigate.IdempotentURL(future);
-
-  // assume javascript:<whatever> will modify current document
-  // but this is not an entirely safe assumption to make,
-  // considering it could be used to set window.location
-  if (fut.protocol == "javascript:") {
-    return false;
-  }
-
-  // navigating to same url, but with any hash
-  if (cur.origin == fut.origin &&
-      cur.pathname == fut.pathname &&
-      fut.hash != "") {
-    return false;
+  switch (new URL(url).protocol) {
+    // assume javascript:<whatever> will modify current document
+    // but this is not an entirely safe assumption to make,
+    // considering it could be used to set window.location
+    case "javascript:":
+      return false;
   }
 
   return true;
-};
-
-/**
- * Sane URL implementation that normalises URL fragments (hashes) and
- * path names for "data:" URLs, and makes them idempotent.
- *
- * At the time of writing this, the web is approximately 10 000 days (or
- * ~27.39 years) old.  One should think that by this point we would have
- * solved URLs.  The following code is prudent example that we have not.
- *
- * When a URL with a fragment identifier but no explicit name for the
- * fragment is given, i.e. "#", the {@code hash} property a {@code URL}
- * object computes is an empty string.  This is incidentally the same as
- * the default value of URLs without fragments, causing a lot of confusion.
- *
- * This means that the URL "http://a/#b" produces a hash of "#b", but that
- * "http://a/#" produces "".  This implementation rectifies this behaviour
- * by returning the actual full fragment, which is "#".
- *
- * "data:" URLs that contain fragments, which if they have the same origin
- * and path name are not meant to cause a page reload on navigation,
- * confusingly adds the fragment to the {@code pathname} property.
- * This implementation remedies this behaviour by trimming it off.
- *
- * The practical result of this is that while {@code URL} objects are
- * not idempotent, the returned URL elements from this implementation
- * guarantees that |url.hash == url.hash|.
- *
- * @param {string|URL} o
- *     Object to make an URL of.
- *
- * @return {navigate.IdempotentURL}
- *     Considered by some to be a somewhat saner URL.
- *
- * @throws TypeError
- *     If |o| is not a valid type or if is a string that cannot be parsed
- *     as a URL.
- */
-navigate.IdempotentURL = function (o) {
-  let url = new URL(o);
-
-  let hash = url.hash;
-  if (hash == "" && url.href[url.href.length - 1] == "#") {
-    hash = "#";
-  }
-
-  return {
-    hash: hash,
-    host: url.host,
-    hostname: url.hostname,
-    href: url.href,
-    origin: url.origin,
-    password: url.password,
-    pathname: url.pathname,
-    port: url.port,
-    protocol: url.protocol,
-    search: url.search,
-    searchParams: url.searchParams,
-    username: url.username,
-  };
 };
