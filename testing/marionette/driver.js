b094d42c731fd471bdb39d7dc6b060b9d45b48a2
Henrik Skupin <mail@hskupin.info>
2017-03-28 21:41:38 +0200

    Bug 1291320 - Refactor page load algorithm for listener framescript. r=ato, r=automatedtester, a=test-only
    This refactoring allows us to re-use the same load algorithm for
    each command which could trigger a page load. It also takes remoteness
    changes into account, and waits until the load has been done.
    
    With this change we no longer check for readyState only, but observe
    the necessary DOM events as fired for page unloads and loads. This will
    help us to implement the page loading strategy later.
    
    By observing the DOM events, I also expect a small increase of performance
    for any kind of page load, given that we now return immediately and do not
    have a delay of 100ms at maximum.
    
    MozReview-Commit-ID: IVtO6KgJFES
    
    --HG--
    extra : source : c7d68974f5a8fe73c25002a6f9ee725eb064996e
    extra : intermediate-source : 512980f54f8cec06e824d424f7b1c84bca1546a2

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -972,7 +972,7 @@ GeckoDriver.prototype.executeJSScript = function* (cmd, resp) {
  * @param {string} url
  *     URL to navigate to.
  */
-GeckoDriver.prototype.get = function*(cmd, resp) {
+GeckoDriver.prototype.get = function* (cmd, resp) {
   assert.content(this.context);
   assert.window(this.getCurrentWindow());
 
@@ -991,7 +991,7 @@ GeckoDriver.prototype.get = function*(cmd, resp) {
       startTime: new Date().getTime(),
     };
     this.mm.broadcastAsyncMessage(
-        "Marionette:pollForReadyState" + this.curBrowser.curFrameId,
+        "Marionette:waitForPageLoaded" + this.curBrowser.curFrameId,
         parameters);
   });
 
@@ -1092,9 +1092,7 @@ GeckoDriver.prototype.goBack = function* (cmd, resp) {
       startTime: new Date().getTime(),
     };
     this.mm.broadcastAsyncMessage(
-        // TODO: combine with
-        // "Marionette:pollForReadyState" + this.curBrowser.curFrameId,
-        "Marionette:pollForReadyState" + this.curBrowser.curFrameId,
+        "Marionette:waitForPageLoaded" + this.curBrowser.curFrameId,
         parameters);
   });
 
@@ -1133,9 +1131,7 @@ GeckoDriver.prototype.goForward = function* (cmd, resp) {
       startTime: new Date().getTime(),
     };
     this.mm.broadcastAsyncMessage(
-        // TODO: combine with
-        // "Marionette:pollForReadyState" + this.curBrowser.curFrameId,
-        "Marionette:pollForReadyState" + this.curBrowser.curFrameId,
+        "Marionette:waitForPageLoaded" + this.curBrowser.curFrameId,
         parameters);
   });
 
