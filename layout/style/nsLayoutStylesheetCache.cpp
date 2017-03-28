4fcf4c5505a184b4342975eed9ea2f12ab3de1cf
Ting-Yu Lin <tlin@mozilla.com>
2017-03-28 18:06:26 +0800

    Bug 1321754 - Add an enum value to SheetParsingMode for agent sheets that use no unsafe rules. r=heycam
    scrollbars.css is the only sheet which is parsed as author level, but later
    added as agent level in [1]. Add a new enum value so that it can be parsed
    as author level in gecko (nsCSSParser::AgentRulesEnabled() will exclude it),
    but servo can recognize it as agent level sheet when the sheet is created.
    
    Delete UserRulesEnabled() because no one uses it.
    
    [1] http://searchfox.org/mozilla-central/rev/7419b368156a6efa24777b21b0e5706be89a9c2f/layout/base/nsDocumentViewer.cpp#2326
    
    MozReview-Commit-ID: 2lrV4ogfnHM

--- layout/style/nsLayoutStylesheetCache.cpp
+++ layout/style/nsLayoutStylesheetCache.cpp
@@ -79,7 +79,7 @@ nsLayoutStylesheetCache::ScrollbarsSheet()
   if (!mScrollbarsSheet) {
     // Scrollbars don't need access to unsafe rules
     LoadSheetURL("chrome://global/skin/scrollbars.css",
-                 &mScrollbarsSheet, eAuthorSheetFeatures, eCrash);
+                 &mScrollbarsSheet, eSafeAgentSheetFeatures, eCrash);
   }
 
   return mScrollbarsSheet;
