c8b5f4a5504529e3a712db21233adee5e8786401
Andreas Tolfsen <ato@mozilla.com>
2017-03-29 18:25:55 +0100

    Bug 1350887 - Include Marionette prefs amongst defaults; r=ted, r=whimboo, a=test-only
    The Marionette component ships in Firefox, but is not enabled by default.
    We want to facilitate activating Marionette at runtime by flipping
    the marionette.enabled preference, and showing the Marionette related
    preferences in about:config helps discoverability.
    
    It is also useful to rely on the preferences' default values so that
    they do not have to be hardcoded in the component.
    
    When Marionette is enabled by setting marionette.enabled to true, a set of
    recommended automation preferences found in testing/marionette/server.js
    are set if the user has not overriden/user-defined one of them and
    marionette.prefs.recommended is true (default).  When Marionette is
    stopped, the altered preferences are reset.
    
    MozReview-Commit-ID: 3HLnEI0TEBB
    
    --HG--
    extra : source : 6f536b2275ae93552dec3c10c5625b04373532ed

--- testing/marionette/prefs/marionette.js
+++ testing/marionette/prefs/marionette.js
@@ -2,6 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Marionette is the remote protocol that lets OOP programs communicate
+// with, instrument, and control Gecko.
+//
+// It is included in Firefox, but not enabled by default unless the
+// --marionette flag is passed or the marionette.enabled preference is
+// set to true.
+
 // Whether or not Marionette is enabled.
 pref("marionette.enabled", false);
 
