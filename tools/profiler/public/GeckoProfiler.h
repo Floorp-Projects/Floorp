bd636bdfbb2c5e781a620ac0cd34ed60b917e6c1
Nicholas Nethercote <nnethercote@mozilla.com>
2017-03-29 13:52:47 +1100

    Bug 1351528 (part 1) - Streamline TracingMetadata. r=mstange.
    This patch:
    
    - Removes TRACING_EVENT_BACKTRACE, which is unused.
    
    - Removes TRACING_DEFAULT and replaces all its uses with TRACING_EVENT, because
      there is no difference in how those two are used.
    
    - Removes TRACING_TIMESTAMP, which is unused and also doesn't do anything
      different to TRACING_EVENT.

--- tools/profiler/public/GeckoProfiler.h
+++ tools/profiler/public/GeckoProfiler.h
@@ -41,12 +41,9 @@ class Promise;
 class nsIProfilerStartParams;
 
 enum TracingMetadata {
-  TRACING_DEFAULT,
+  TRACING_EVENT,
   TRACING_INTERVAL_START,
   TRACING_INTERVAL_END,
-  TRACING_EVENT,
-  TRACING_EVENT_BACKTRACE,
-  TRACING_TIMESTAMP
 };
 
 class ProfilerBacktrace;
@@ -127,10 +124,10 @@ using UniqueProfilerBacktrace =
 // Adds a tracing marker to the PseudoStack. A no-op if the profiler is
 // inactive or in privacy mode.
 PROFILER_FUNC_VOID(profiler_tracing(const char* aCategory, const char* aInfo,
-                                    TracingMetadata metaData = TRACING_DEFAULT))
+                                    TracingMetadata metaData = TRACING_EVENT))
 PROFILER_FUNC_VOID(profiler_tracing(const char* aCategory, const char* aInfo,
                                     UniqueProfilerBacktrace aCause,
-                                    TracingMetadata metaData = TRACING_DEFAULT))
+                                    TracingMetadata metaData = TRACING_EVENT))
 
 // Initialize the profiler. If MOZ_PROFILER_STARTUP is set the profiler will be
 // started. This call must happen before any other profiler calls (except
