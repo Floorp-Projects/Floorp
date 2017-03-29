f10ac73fd47001da584a8c9f834486aa780b8000
Nicholas Hurley <hurley@mozilla.com>
2017-03-28 17:15:23 -0700

    Bug 1346392 - Force non-spdy on sticky auth connections. r=dragana, a=lizzard
    MozReview-Commit-ID: IkYTVgetKsn

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -116,6 +116,7 @@ public:
     NS_IMETHOD OnAuthAvailable() override;
     NS_IMETHOD OnAuthCancelled(bool userCancel) override;
     NS_IMETHOD CloseStickyConnection() override;
+    NS_IMETHOD ForceNoSpdy() override;
     // Functions we implement from nsIHttpAuthenticableChannel but are
     // declared in HttpBaseChannel must be implemented in this class. We
     // just call the HttpBaseChannel:: impls.
