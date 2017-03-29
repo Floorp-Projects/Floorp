b26415ec8795baf5082611a91ac570eec5900f4c
Iris Hsiao <ihsiao@mozilla.com>
2017-03-29 10:29:13 +0800

    Backed out changeset 448adfbcfda1 (bug 1349275) for eslint failure
    --HG--
    extra : rebase_source : 0728e29de869800694671a1133fa7c9beb66df06

--- devtools/server/actors/highlighters/box-model.js
+++ devtools/server/actors/highlighters/box-model.js
@@ -352,10 +352,6 @@ BoxModelHighlighter.prototype = extend(AutoRefreshHighlighter.prototype, {
     return shown;
   },
 
-  _scrollUpdate: function () {
-    this._moveInfobar();
-  },
-
   /**
    * Hide the highlighter, the outline and the infobar.
    */
@@ -512,7 +508,7 @@ BoxModelHighlighter.prototype = extend(AutoRefreshHighlighter.prototype, {
     }
 
     // Un-zoom the root wrapper if the page was zoomed.
-    let rootId = this.ID_CLASS_PREFIX + "elements";
+    let rootId = this.ID_CLASS_PREFIX + "root";
     this.markup.scaleRootElement(this.currentNode, rootId);
 
     return true;
