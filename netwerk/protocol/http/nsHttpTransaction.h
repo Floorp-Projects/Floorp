06d3cac937ece994a2287f2a92e1e0a7401aaeaa
Nicholas Hurley <hurley@mozilla.com>
2017-03-28 17:15:23 -0700

    Bug 1346392 - Force non-spdy on sticky auth connections. r=dragana, a=lizzard
    MozReview-Commit-ID: IkYTVgetKsn
    
    --HG--
    extra : source : 88f793b6a03c67cc6c1c986e0d46f8310f88d31a
    extra : amend_source : 69fdc21f8cc348aaba17f9d290ab31014f9057c7

--- netwerk/protocol/http/nsHttpTransaction.h
+++ netwerk/protocol/http/nsHttpTransaction.h
@@ -131,6 +131,8 @@ public:
     void DispatchedAsBlocking();
     void RemoveDispatchedAsBlocking();
 
+    void DisableSpdy() override;
+
     nsHttpTransaction *QueryHttpTransaction() override { return this; }
 
     Http2PushedStream *GetPushedStream() { return mPushedStream; }
@@ -191,7 +193,6 @@ private:
 
     bool ResponseTimeoutEnabled() const final;
 
-    void DisableSpdy() override;
     void ReuseConnectionOnRestartOK(bool reuseOk) override { mReuseOnRestart = reuseOk; }
 
     // Called right after we parsed the response head.  Checks for connection based
