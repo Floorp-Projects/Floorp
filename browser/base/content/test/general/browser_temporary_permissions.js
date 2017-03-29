2f68c75cb960561c1e9800e38fcbf834f7e9e15d
Johann Hofmann <jhofmann@mozilla.com>
2017-03-29 16:15:56 +0200

    Bug 1334842 - Fix intermittent browser_temporary_permissions.js. r=Nihanth, a=test-only
    This intermittent was likely occurring because we set the expiry timeout
    for temporary permissions to a really low value in the previous test.
    The failing test was only failing on slow machines, leading me to believe
    that the time between setting and checking was larger than the 500ms timeout
    defined in the previous test. Thus, the permission was reset on checking it.
    The expiry pref was set using pushPrefEnv, which restores prefs only after
    the entire test was run. To just eradicate this category of problems in
    the future I moved the test that manipulates the expiry into its own file.
    
    MozReview-Commit-ID: 3mc5xHY4XLn
    
    --HG--
    extra : rebase_source : 5adf105474931f73f678be895e6a45566c31dee5

--- browser/base/content/test/general/browser_temporary_permissions.js
+++ browser/base/content/test/general/browser_temporary_permissions.js
@@ -3,15 +3,11 @@
 
 "use strict";
 
-Cu.import("resource:///modules/SitePermissions.jsm", this);
 Cu.import("resource:///modules/E10SUtils.jsm");
 
 const ORIGIN = "https://example.com";
 const PERMISSIONS_PAGE = getRootDirectory(gTestPath).replace("chrome://mochitests/content", ORIGIN) + "permissions.html"
 const SUBFRAME_PAGE = getRootDirectory(gTestPath).replace("chrome://mochitests/content", ORIGIN) + "temporary_permissions_subframe.html"
-
-const EXPIRE_TIME_MS = 100;
-const TIMEOUT_MS = 500;
 
 // Test that setting temp permissions triggers a change in the identity block.
 add_task(function* testTempPermissionChangeEvents() {
@@ -36,58 +32,6 @@ add_task(function* testTempPermissionChangeEvents() {
   });
 });
 
-// Test that temporary permissions can be re-requested after they expired
-// and that the identity block is updated accordingly.
-// TODO (blocked by bug 1334421): Write a check for webrtc permissions,
-// because they use a different code path.
-add_task(function* testTempPermissionRequestAfterExpiry() {
-  yield SpecialPowers.pushPrefEnv({set: [
-        ["privacy.temporary_permission_expire_time_ms", EXPIRE_TIME_MS],
-  ]});
-
-  let uri = NetUtil.newURI(ORIGIN);
-  let id = "geo";
-
-  yield BrowserTestUtils.withNewTab(PERMISSIONS_PAGE, function*(browser) {
-    let blockedIcon = gIdentityHandler._identityBox
-      .querySelector(`.blocked-permission-icon[data-permission-id='${id}']`);
-
-    SitePermissions.set(uri, id, SitePermissions.BLOCK, SitePermissions.SCOPE_TEMPORARY, browser);
-
-    Assert.deepEqual(SitePermissions.get(uri, id, browser), {
-      state: SitePermissions.BLOCK,
-      scope: SitePermissions.SCOPE_TEMPORARY,
-    });
-
-    ok(blockedIcon.hasAttribute("showing"), "blocked permission icon is shown");
-
-    yield new Promise((c) => setTimeout(c, TIMEOUT_MS));
-
-    Assert.deepEqual(SitePermissions.get(uri, id, browser), {
-      state: SitePermissions.UNKNOWN,
-      scope: SitePermissions.SCOPE_PERSISTENT,
-    });
-
-    let popupshown = BrowserTestUtils.waitForEvent(PopupNotifications.panel, "popupshown");
-
-    // Request a permission;
-    yield BrowserTestUtils.synthesizeMouseAtCenter(`#${id}`, {}, browser);
-
-    yield popupshown;
-
-    ok(!blockedIcon.hasAttribute("showing"), "blocked permission icon is not shown");
-
-    let popuphidden = BrowserTestUtils.waitForEvent(PopupNotifications.panel, "popuphidden");
-
-    let notification = PopupNotifications.panel.firstChild;
-    EventUtils.synthesizeMouseAtCenter(notification.secondaryButton, {});
-
-    yield popuphidden;
-
-    SitePermissions.remove(uri, id, browser);
-  });
-});
-
 // Test that temp blocked permissions requested by subframes (with a different URI) affect the whole page.
 add_task(function* testTempPermissionSubframes() {
   let uri = NetUtil.newURI(ORIGIN);
@@ -96,7 +40,7 @@ add_task(function* testTempPermissionSubframes() {
   yield BrowserTestUtils.withNewTab(SUBFRAME_PAGE, function*(browser) {
     let popupshown = BrowserTestUtils.waitForEvent(PopupNotifications.panel, "popupshown");
 
-    // Request a permission;
+    // Request a permission.
     yield ContentTask.spawn(browser, uri.host, function(host) {
       E10SUtils.wrapHandlingUserInput(content, true, function() {
         let frame = content.document.getElementById("frame");
