bd72ba232f06456b84f2c423a688af5d2b86e165
Andrzej Hunt <andrzej@ahunt.org>
2017-03-29 13:13:45 -0700

    [focus] Update Trie tests to reflect partial domain fixes (https://github.com/mozilla-mobile/focus-android/issues/360)

--- mobile/android/focus-android/app/src/testWebkit/java/org/mozilla/focus/webkit/matcher/TrieTest.java
+++ mobile/android/focus-android/app/src/testWebkit/java/org/mozilla/focus/webkit/matcher/TrieTest.java
@@ -24,17 +24,29 @@ public class TrieTest {
         assertNotNull(foundNode);
         assertEquals(putNode, foundNode);
 
-        // Substring matching: only works in one direction
+        // Substring matching: doesn't happen (except for subdomains, we test those later)
         assertNull(trie.findNode(FocusString.create("hell")));
-        assertNotNull(trie.findNode(FocusString.create("hellohello")));
+        assertNull(trie.findNode(FocusString.create("hellop")));
 
         trie.put(FocusString.create("hellohello"));
 
         // Ensure both old and new overlapping strings can still be found
         assertNotNull(trie.findNode(FocusString.create("hello")));
         assertNotNull(trie.findNode(FocusString.create("hellohello")));
+
+        // These still don't match:
         assertNull(trie.findNode(FocusString.create("hell")));
-        assertNull(trie.findNode(FocusString.create("hella")));
+        assertNull(trie.findNode(FocusString.create("hellop")));
+
+        // Domain specific / partial domain tests:
+        trie.put(FocusString.create("foo.com").reverse());
+
+        // Domain and subdomain can be found
+        assertNotNull(trie.findNode(FocusString.create("foo.com").reverse()));
+        assertNotNull(trie.findNode(FocusString.create("bar.foo.com").reverse()));
+        // But other domains with some overlap don't match
+        assertNull(trie.findNode(FocusString.create("bar-foo.com").reverse()));
+        assertNull(trie.findNode(FocusString.create("oo.com").reverse()));
     }
 
     @Test
