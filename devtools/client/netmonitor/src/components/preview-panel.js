a97f9263716684cd97a1262bbdb7dd1f8e20fbc2
Iris Hsiao <ihsiao@mozilla.com>
2017-03-30 18:07:37 +0800

    Backed out changeset 3d0faaeb8496 (bug 1350229) for eslint failure

---
+++ devtools/client/netmonitor/src/components/preview-panel.js
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { DOM, PropTypes } = require("devtools/client/shared/vendor/react");
+
+const { div, iframe } = DOM;
+
+/*
+ * Preview panel component
+ * Display HTML content within a sandbox enabled iframe
+ */
+function PreviewPanel({ request }) {
+  const htmlBody = request.responseContent ?
+    request.responseContent.content.text : "";
+
+  return (
+    div({ className: "panel-container" },
+      iframe({
+        sandbox: "",
+        srcDoc: typeof htmlBody === "string" ? htmlBody : "",
+      })
+    )
+  );
+}
+
+PreviewPanel.displayName = "PreviewPanel";
+
+PreviewPanel.propTypes = {
+  request: PropTypes.object.isRequired,
+};
+
+module.exports = PreviewPanel;
