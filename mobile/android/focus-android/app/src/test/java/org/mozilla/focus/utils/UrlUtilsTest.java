1e7c23118d2d9d7f46ebe9488fb32db0def11757
Andrzej Hunt <andrzej@ahunt.org>
2017-03-30 13:29:57 -0700

    [focus] Add more UrlUtil tests (https://github.com/mozilla-mobile/focus-android/issues/387)

--- mobile/android/focus-android/app/src/test/java/org/mozilla/focus/utils/UrlUtilsTest.java
+++ mobile/android/focus-android/app/src/test/java/org/mozilla/focus/utils/UrlUtilsTest.java
@@ -28,4 +28,27 @@ public class UrlUtilsTest {
         Assert.assertTrue(UrlUtils.urlsMatchExceptForTrailingSlash("http://www.MOZILLA.org", "http://www.mozilla.org/"));
     }
 
+    @Test
+    public void isPermittedResourceProtocol() {
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("http:"));
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("https:"));
+
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("http://foobar.com"));
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("https://foobar.com"));
+
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("data:???????????????"));
+        Assert.assertTrue(UrlUtils.isPermittedResourceProtocol("file:///android_asset/something/index.html"));
+
+        Assert.assertFalse(UrlUtils.isPermittedResourceProtocol("nielsenwebid://nuid/999"));
+    }
+
+    @Test
+    public void isPermittedProtocol() {
+        Assert.assertTrue(UrlUtils.isSupportedProtocol("http:"));
+        Assert.assertTrue(UrlUtils.isSupportedProtocol("http://mozilla.org"));
+        Assert.assertTrue(UrlUtils.isSupportedProtocol("error:-1"));
+        Assert.assertTrue(UrlUtils.isSupportedProtocol("data:?????"));
+
+        Assert.assertFalse(UrlUtils.isSupportedProtocol("market:details?id=org.mozilla.firefox"));
+    }
 }
