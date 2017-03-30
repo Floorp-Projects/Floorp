c66f0c70aac67411e77997cd733332bdb1e912d1
Paolo Amadini <paolo.mozmail@amadzone.org>
2017-03-30 15:51:18 +0100

    Bug 1346716 - Remove the entry for "mimeTypes.rdf" in the directory service. r=mak
    This improves performance because we won't try to fix the permissions of the file every time the nsIHandlerService implementation wants to open it, and makes it easier to access the file path from xpcshell tests.
    
    MozReview-Commit-ID: DZmLa92qUnZ

--- xpcom/io/nsAppDirectoryServiceDefs.h
+++ xpcom/io/nsAppDirectoryServiceDefs.h
@@ -71,7 +71,6 @@
 
 #define NS_APP_LOCALSTORE_50_FILE               "LclSt"
 #define NS_APP_USER_PANELS_50_FILE              "UPnls"
-#define NS_APP_USER_MIMETYPES_50_FILE           "UMimTyp"
 #define NS_APP_CACHE_PARENT_DIR                 "cachePDir"
 
 #define NS_APP_DOWNLOADS_50_FILE                "DLoads"
