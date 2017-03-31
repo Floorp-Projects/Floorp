b5610163c0576def0ffebf3e4b8ba2723c5bb6d1
Johann Hofmann <jhofmann@mozilla.com>
2017-03-31 22:39:45 +0200

    Bug 1337246 - Part 1 - Add a hostnameIsLocalIPAddress function to nsIIOService. r=valentin
    MozReview-Commit-ID: 81A37b1juGD

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -585,6 +585,35 @@ nsIOService::ExtractScheme(const nsACString &inURI, nsACString &scheme)
     return net_ExtractURLScheme(inURI, scheme);
 }
 
+NS_IMETHODIMP
+nsIOService::HostnameIsLocalIPAddress(nsIURI *aURI, bool *aResult)
+{
+  NS_ENSURE_ARG_POINTER(aURI);
+
+  nsCOMPtr<nsIURI> innerURI = NS_GetInnermostURI(aURI);
+  NS_ENSURE_ARG_POINTER(innerURI);
+
+  nsAutoCString host;
+  nsresult rv = innerURI->GetAsciiHost(host);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+
+  *aResult = false;
+
+  PRNetAddr addr;
+  PRStatus result = PR_StringToNetAddr(host.get(), &addr);
+  if (result == PR_SUCCESS) {
+    NetAddr netAddr;
+    PRNetAddrToNetAddr(&addr, &netAddr);
+    if (IsIPAddrLocal(&netAddr)) {
+      *aResult = true;
+    }
+  }
+
+  return NS_OK;
+}
+
 NS_IMETHODIMP 
 nsIOService::GetProtocolFlags(const char* scheme, uint32_t *flags)
 {
