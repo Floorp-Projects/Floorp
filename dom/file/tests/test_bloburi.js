287f9dab4e34c3670ddb2a819c55d29a57b31391
Kate McKinley <kmckinley@mozilla.com>
2017-04-01 15:11:53 -0400

    Bug 1237868 - Don't set URI_IS_LOCAL_RESOURCE on media streams. r=baku, a=gchang
    This patch only sets URI_IS_LOCAL_RESOURCE if the resource is a blob or
    font table entry, to avoid having media streams treated as local
    objects.
    
    MozReview-Commit-ID: GOVr8FPByQy

---
+++ dom/file/tests/test_bloburi.js
@@ -0,0 +1,33 @@
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cr = Components.results;
+
+var ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+
+var uris = [
+  {
+    uri: "blob:https://example.com/230d5d50-35f9-9745-a64a-15e47b731a81",
+    local: true,
+  },
+  {
+    uri: "rstp://1.2.3.4/some_path?param=a",
+    local: false,
+  },
+  {
+    uri: "moz-fonttable://something",
+    local: true,
+  }
+];
+
+function run_test()
+{
+  for (let i = 0; i < uris.length; i++) {
+    let uri = ios.newURI(uris[i].uri);
+    let handler = ios.getProtocolHandler(uri.scheme).QueryInterface(Ci.nsIProtocolHandler);
+    let flags = handler.protocolFlags;
+    
+    do_check_eq(Ci.nsIProtocolHandler.URI_IS_LOCAL_RESOURCE & flags,
+                (uris[i].local) ? Ci.nsIProtocolHandler.URI_IS_LOCAL_RESOURCE : 0);
+  }
+}
+
