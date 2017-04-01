26723021db36e01fd95b8d1b3ee00532aaaa3b60
Ricky Chien <ricky060709@gmail.com>
2017-04-01 23:01:06 +0800

    Bug 1352699 - Make netmonitor run on devtools-launchpad r=Honza
    MozReview-Commit-ID: 4khCXm2lfzG

--- devtools/client/netmonitor/src/components/editor.js
+++ devtools/client/netmonitor/src/components/editor.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-disable react/prop-types */
-
 "use strict";
 
 const {
@@ -103,5 +101,3 @@ const Editor = createClass({
 });
 
 module.exports = Editor;
-
-/* eslint-enable react/prop-types */
