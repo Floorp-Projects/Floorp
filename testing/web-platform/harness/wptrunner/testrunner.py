9115045faf08ff7bf1a98c182273fad3149edae2
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-04-01 19:13:16 +0200

    Backed out changeset 024d5d30566d (bug 1352351) for frequently failing resume-timer-on-history-back.html on Windows. r=backout a=backout
    MozReview-Commit-ID: JSU0OCtbAOe

--- testing/web-platform/harness/wptrunner/testrunner.py
+++ testing/web-platform/harness/wptrunner/testrunner.py
@@ -499,10 +499,9 @@ class TestRunnerManager(threading.Thread):
                     return None, None
             try:
                 # Need to block here just to allow for contention with other processes
-                test = test_queue.get(block=True, timeout=2)
+                test = test_queue.get(block=True, timeout=1)
             except Empty:
-                if test_queue.empty():
-                    test_queue = None
+                pass
         return test, test_queue
 
     def run_test(self):
