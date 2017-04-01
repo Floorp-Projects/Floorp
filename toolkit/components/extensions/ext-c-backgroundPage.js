bb351ebfca7df444e0b9b37e6afebd7c72581e94
Kris Maglione <maglione.k@gmail.com>
2017-03-31 19:36:00 -0700

    Bug 1350522: Part 2 - Convert toolkit APIs to lazy loading. r=aswan
    MozReview-Commit-ID: 8TbTIM4WX2d
    
    --HG--
    extra : source : a3ed5ad1bc338e7fd8055c2efcf73695c25e09e5

--- toolkit/components/extensions/ext-c-backgroundPage.js
+++ toolkit/components/extensions/ext-c-backgroundPage.js
@@ -1,45 +1,25 @@
 "use strict";
 
-global.initializeBackgroundPage = (contentWindow) => {
-  // Override the `alert()` method inside background windows;
-  // we alias it to console.log().
-  // See: https://bugzilla.mozilla.org/show_bug.cgi?id=1203394
-  let alertDisplayedWarning = false;
-  let alertOverwrite = text => {
-    if (!alertDisplayedWarning) {
-      require("devtools/client/framework/devtools-browser");
-
-      let hudservice = require("devtools/client/webconsole/hudservice");
-      hudservice.openBrowserConsoleOrFocus();
-
-      contentWindow.console.warn("alert() is not supported in background windows; please use console.log instead.");
-
-      alertDisplayedWarning = true;
-    }
-
-    contentWindow.console.log(text);
-  };
-  Cu.exportFunction(alertOverwrite, contentWindow, {defineAs: "alert"});
-};
-
-extensions.registerSchemaAPI("extension", "addon_child", context => {
-  function getBackgroundPage() {
-    for (let view of context.extension.views) {
-      if (view.viewType == "background" && context.principal.subsumes(view.principal)) {
-        return view.contentWindow;
+this.backgroundPage = class extends ExtensionAPI {
+  getAPI(context) {
+    function getBackgroundPage() {
+      for (let view of context.extension.views) {
+        if (view.viewType == "background" && context.principal.subsumes(view.principal)) {
+          return view.contentWindow;
+        }
       }
+      return null;
     }
-    return null;
-  }
-  return {
-    extension: {
-      getBackgroundPage,
-    },
-
-    runtime: {
-      getBackgroundPage() {
-        return context.cloneScope.Promise.resolve(getBackgroundPage());
+    return {
+      extension: {
+        getBackgroundPage,
       },
-    },
-  };
-});
+
+      runtime: {
+        getBackgroundPage() {
+          return context.cloneScope.Promise.resolve(getBackgroundPage());
+        },
+      },
+    };
+  }
+};
--- toolkit/components/extensions/ext-c-runtime.js
+++ toolkit/components/extensions/ext-c-runtime.js
@@ -1,106 +1,103 @@
 "use strict";
 
-function runtimeApiFactory(context) {
-  let {extension} = context;
+this.runtime = class extends ExtensionAPI {
+  getAPI(context) {
+    let {extension} = context;
 
-  return {
-    runtime: {
-      onConnect: context.messenger.onConnect("runtime.onConnect"),
+    return {
+      runtime: {
+        onConnect: context.messenger.onConnect("runtime.onConnect"),
 
-      onMessage: context.messenger.onMessage("runtime.onMessage"),
+        onMessage: context.messenger.onMessage("runtime.onMessage"),
 
-      onConnectExternal: context.messenger.onConnectExternal("runtime.onConnectExternal"),
+        onConnectExternal: context.messenger.onConnectExternal("runtime.onConnectExternal"),
 
-      onMessageExternal: context.messenger.onMessageExternal("runtime.onMessageExternal"),
+        onMessageExternal: context.messenger.onMessageExternal("runtime.onMessageExternal"),
 
-      connect: function(extensionId, connectInfo) {
-        let name = (connectInfo !== null && connectInfo.name) || "";
-        extensionId = extensionId || extension.id;
-        let recipient = {extensionId};
+        connect: function(extensionId, connectInfo) {
+          let name = (connectInfo !== null && connectInfo.name) || "";
+          extensionId = extensionId || extension.id;
+          let recipient = {extensionId};
 
-        return context.messenger.connect(context.messageManager, name, recipient);
-      },
+          return context.messenger.connect(context.messageManager, name, recipient);
+        },
 
-      sendMessage: function(...args) {
-        let options; // eslint-disable-line no-unused-vars
-        let extensionId, message, responseCallback;
-        if (typeof args[args.length - 1] === "function") {
-          responseCallback = args.pop();
-        }
-        if (!args.length) {
-          return Promise.reject({message: "runtime.sendMessage's message argument is missing"});
-        } else if (args.length === 1) {
-          message = args[0];
-        } else if (args.length === 2) {
-          if (typeof args[0] === "string" && args[0]) {
-            [extensionId, message] = args;
+        sendMessage: function(...args) {
+          let options; // eslint-disable-line no-unused-vars
+          let extensionId, message, responseCallback;
+          if (typeof args[args.length - 1] === "function") {
+            responseCallback = args.pop();
+          }
+          if (!args.length) {
+            return Promise.reject({message: "runtime.sendMessage's message argument is missing"});
+          } else if (args.length === 1) {
+            message = args[0];
+          } else if (args.length === 2) {
+            if (typeof args[0] === "string" && args[0]) {
+              [extensionId, message] = args;
+            } else {
+              [message, options] = args;
+            }
+          } else if (args.length === 3) {
+            [extensionId, message, options] = args;
+          } else if (args.length === 4 && !responseCallback) {
+            return Promise.reject({message: "runtime.sendMessage's last argument is not a function"});
           } else {
-            [message, options] = args;
+            return Promise.reject({message: "runtime.sendMessage received too many arguments"});
           }
-        } else if (args.length === 3) {
-          [extensionId, message, options] = args;
-        } else if (args.length === 4 && !responseCallback) {
-          return Promise.reject({message: "runtime.sendMessage's last argument is not a function"});
-        } else {
-          return Promise.reject({message: "runtime.sendMessage received too many arguments"});
-        }
 
-        if (extensionId != null && typeof extensionId !== "string") {
-          return Promise.reject({message: "runtime.sendMessage's extensionId argument is invalid"});
-        }
-
-        extensionId = extensionId || extension.id;
-        let recipient = {extensionId};
-
-        if (options != null) {
-          if (typeof options !== "object") {
-            return Promise.reject({message: "runtime.sendMessage's options argument is invalid"});
+          if (extensionId != null && typeof extensionId !== "string") {
+            return Promise.reject({message: "runtime.sendMessage's extensionId argument is invalid"});
           }
-          if (typeof options.toProxyScript === "boolean") {
-            recipient.toProxyScript = options.toProxyScript;
-          } else {
-            return Promise.reject({message: "runtime.sendMessage's options.toProxyScript argument is invalid"});
+
+          extensionId = extensionId || extension.id;
+          let recipient = {extensionId};
+
+          if (options != null) {
+            if (typeof options !== "object") {
+              return Promise.reject({message: "runtime.sendMessage's options argument is invalid"});
+            }
+            if (typeof options.toProxyScript === "boolean") {
+              recipient.toProxyScript = options.toProxyScript;
+            } else {
+              return Promise.reject({message: "runtime.sendMessage's options.toProxyScript argument is invalid"});
+            }
           }
-        }
 
-        return context.messenger.sendMessage(context.messageManager, message, recipient, responseCallback);
+          return context.messenger.sendMessage(context.messageManager, message, recipient, responseCallback);
+        },
+
+        connectNative(application) {
+          let recipient = {
+            childId: context.childManager.id,
+            toNativeApp: application,
+          };
+
+          return context.messenger.connectNative(context.messageManager, "", recipient);
+        },
+
+        sendNativeMessage(application, message) {
+          let recipient = {
+            childId: context.childManager.id,
+            toNativeApp: application,
+          };
+          return context.messenger.sendNativeMessage(context.messageManager, message, recipient);
+        },
+
+        get lastError() {
+          return context.lastError;
+        },
+
+        getManifest() {
+          return Cu.cloneInto(extension.manifest, context.cloneScope);
+        },
+
+        id: extension.id,
+
+        getURL: function(url) {
+          return extension.baseURI.resolve(url);
+        },
       },
-
-      connectNative(application) {
-        let recipient = {
-          childId: context.childManager.id,
-          toNativeApp: application,
-        };
-
-        return context.messenger.connectNative(context.messageManager, "", recipient);
-      },
-
-      sendNativeMessage(application, message) {
-        let recipient = {
-          childId: context.childManager.id,
-          toNativeApp: application,
-        };
-        return context.messenger.sendNativeMessage(context.messageManager, message, recipient);
-      },
-
-      get lastError() {
-        return context.lastError;
-      },
-
-      getManifest() {
-        return Cu.cloneInto(extension.manifest, context.cloneScope);
-      },
-
-      id: extension.id,
-
-      getURL: function(url) {
-        return extension.baseURI.resolve(url);
-      },
-    },
-  };
-}
-
-extensions.registerSchemaAPI("runtime", "addon_child", runtimeApiFactory);
-extensions.registerSchemaAPI("runtime", "content_child", runtimeApiFactory);
-extensions.registerSchemaAPI("runtime", "devtools_child", runtimeApiFactory);
-extensions.registerSchemaAPI("runtime", "proxy_script", runtimeApiFactory);
+    };
+  }
+};
