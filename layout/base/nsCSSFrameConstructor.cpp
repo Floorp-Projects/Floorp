1dac83f9703639b35e86c0ddc500c7deb32750c2
Cameron McCormack <cam@mcc.id.au>
2017-04-02 15:15:44 +0800

    Bug 1352785 - stylo: Ensure children of elements with invalid -moz-binding URLs get styled. r=bholley
    MozReview-Commit-ID: 2zot13HgjWU

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -5767,44 +5767,55 @@ nsCSSFrameConstructor::AddFrameConstructionItemsInternal(nsFrameConstructorState
   const nsStyleDisplay* display = aStyleContext->StyleDisplay();
   RefPtr<nsStyleContext> styleContext(aStyleContext);
   PendingBinding* pendingBinding = nullptr;
-  if ((aFlags & ITEM_ALLOW_XBL_BASE) && display->mBinding)
-  {
-    // Ensure that our XBL bindings are installed.
+  if (aFlags & ITEM_ALLOW_XBL_BASE) {
+    if (display->mBinding) {
+      // Ensure that our XBL bindings are installed.
 
-    nsXBLService* xblService = nsXBLService::GetInstance();
-    if (!xblService)
-      return;
+      nsXBLService* xblService = nsXBLService::GetInstance();
+      if (!xblService)
+        return;
 
-    bool resolveStyle;
+      bool resolveStyle;
 
-    nsAutoPtr<PendingBinding> newPendingBinding(new PendingBinding());
+      nsAutoPtr<PendingBinding> newPendingBinding(new PendingBinding());
 
-    nsresult rv = xblService->LoadBindings(aContent, display->mBinding->GetURI(),
-                                           display->mBinding->mOriginPrincipal,
-                                           getter_AddRefs(newPendingBinding->mBinding),
-                                           &resolveStyle);
-    if (NS_FAILED(rv) && rv != NS_ERROR_XBL_BLOCKED)
-      return;
+      nsresult rv = xblService->LoadBindings(aContent, display->mBinding->GetURI(),
+                                             display->mBinding->mOriginPrincipal,
+                                             getter_AddRefs(newPendingBinding->mBinding),
+                                             &resolveStyle);
+      if (NS_FAILED(rv) && rv != NS_ERROR_XBL_BLOCKED)
+        return;
 
-    if (newPendingBinding->mBinding) {
-      pendingBinding = newPendingBinding;
-      // aState takes over owning newPendingBinding
-      aState.AddPendingBinding(newPendingBinding.forget());
+      if (newPendingBinding->mBinding) {
+        pendingBinding = newPendingBinding;
+        // aState takes over owning newPendingBinding
+        aState.AddPendingBinding(newPendingBinding.forget());
+      }
+
+      if (aContent->IsStyledByServo()) {
+        NS_WARNING("stylo: Skipping Unsupported binding re-resolve. This needs fixing.");
+        resolveStyle = false;
+      }
+
+      if (resolveStyle) {
+        styleContext =
+          ResolveStyleContext(styleContext->GetParent(), aContent, &aState);
+        display = styleContext->StyleDisplay();
+        aStyleContext = styleContext;
+      }
+
+      aTag = mDocument->BindingManager()->ResolveTag(aContent, &aNameSpaceID);
+    } else if (display->mBinding.ForceGet()) {
+      if (aContent->IsStyledByServo()) {
+        // Servo's should_traverse_children skips styling descendants of
+        // elements with a -moz-binding value.  For -moz-binding URLs that can
+        // be resolved, we will load the binding above, which will style the
+        // children after they have been rearranged in the flattened tree.
+        // If the URL couldn't be resolved, we still need to style the children,
+        // so we do that here.
+        mPresShell->StyleSet()->AsServo()->StyleNewChildren(aContent->AsElement());
+      }
     }
-
-    if (aContent->IsStyledByServo()) {
-      NS_WARNING("stylo: Skipping Unsupported binding re-resolve. This needs fixing.");
-      resolveStyle = false;
-    }
-
-    if (resolveStyle) {
-      styleContext =
-        ResolveStyleContext(styleContext->GetParent(), aContent, &aState);
-      display = styleContext->StyleDisplay();
-      aStyleContext = styleContext;
-    }
-
-    aTag = mDocument->BindingManager()->ResolveTag(aContent, &aNameSpaceID);
   }
 
   bool isGeneratedContent = ((aFlags & ITEM_IS_GENERATED_CONTENT) != 0);
