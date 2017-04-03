bf22378ea2e4a36305654393c25cb080ac0a62cc
Xidorn Quan <me@upsuper.org>
2017-04-03 19:55:06 +1000

    Bug 1352763 part 3 - Have ServoStyleSheet also implement nsICSSLoaderObserver. r=heycam
    MozReview-Commit-ID: 1Z6jAmQ9CY6

--- layout/style/ServoStyleSheet.h
+++ layout/style/ServoStyleSheet.h
@@ -90,6 +90,10 @@ public:
     nsIDocument* aCloneDocument,
     nsINode* aCloneOwningNode) const final;
 
+  // nsICSSLoaderObserver interface
+  NS_IMETHOD StyleSheetLoaded(StyleSheet* aSheet, bool aWasAlternate,
+                              nsresult aStatus) final;
+
 protected:
   virtual ~ServoStyleSheet();
 
