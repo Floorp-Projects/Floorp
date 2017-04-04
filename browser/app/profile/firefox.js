b9c316d161981169250f70b57623bd792bb2873e
Panos Astithas <past@mozilla.com>
2017-04-04 16:03:43 +0300

    Enable one-off search buttons in the URL bar on all channels (bug 1344929). r=adw
    MozReview-Commit-ID: 8pVvMrneOq7

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -337,11 +337,7 @@ pref("browser.urlbar.suggest.history.onlyTyped",    false);
 pref("browser.urlbar.formatting.enabled", true);
 pref("browser.urlbar.trimURLs", true);
 
-#if defined(NIGHTLY_BUILD)
 pref("browser.urlbar.oneOffSearches", true);
-#else
-pref("browser.urlbar.oneOffSearches", false);
-#endif
 
 // If changed to true, copying the entire URL from the location bar will put the
 // human readable (percent-decoded) URL on the clipboard.
