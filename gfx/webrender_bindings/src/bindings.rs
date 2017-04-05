ebd34c8deb871617d95816b70950fba9f030e8ba
Kartikaya Gupta <kgupta@mozilla.com>
2017-04-05 11:18:43 -0400

    Bug 1353735 - Fix missing ABI declaration. r=jrmuizel
    MozReview-Commit-ID: IZkitQliVma

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -616,8 +616,8 @@ impl webrender_traits::RenderNotifier for CppNotifier {
 }
 
 #[no_mangle]
-pub extern fn wr_renderer_set_external_image_handler(renderer: &mut Renderer,
-                                                     external_image_handler: *mut WrExternalImageHandler) {
+pub extern "C" fn wr_renderer_set_external_image_handler(renderer: &mut Renderer,
+                                                         external_image_handler: *mut WrExternalImageHandler) {
     if !external_image_handler.is_null() {
         renderer.set_external_image_handler(Box::new(
             unsafe {
