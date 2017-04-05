def036d24efcfc9da728fdff4833c26d516878dd
L. David Baron <dbaron@dbaron.org>
2017-04-04 20:59:21 -0700

    Bug 1353187 - Guard access to the frame property table with a frame state bit.  r=dholbert
    This protects all accesses to the frame property table with a bit stored
    on the frame.  This means we avoid hashtable operations when asking
    about frame properties on frames that have no properties.
    
    The changes to RestyleManager, and the new HasSkippingBitCheck API, are
    needed because RestyleManager depended on being able to ask for
    properties on a deleted frame (knowing that the property in question
    could not have been set on any new frames since the deleted frame was
    destroyed), in order to use the destruction of the properties that
    happens at frame destruction as a mechanism for learning that the frame
    was destroyed.  The changes there preserve the use of that mechanism,
    although it becomes a bit uglier.  The ugliness is well-deserved.
    
    MozReview-Commit-ID: BScmDUlWq65

--- layout/generic/nsFrameStateBits.h
+++ layout/generic/nsFrameStateBits.h
@@ -270,6 +270,9 @@ FRAME_STATE_BIT(Generic, 54, NS_FRAME_HAS_LAYER_ACTIVITY_PROPERTY)
 // a stylo tree traversal.
 FRAME_STATE_BIT(Generic, 55, NS_FRAME_OWNS_ANON_BOXES)
 
+// Frame has properties in the nsIFrame::Properties() hash.
+FRAME_STATE_BIT(Generic, 56, NS_FRAME_HAS_PROPERTIES)
+
 // Set for all descendants of MathML sub/supscript elements (other than the
 // base frame) to indicate that the SSTY font feature should be used.
 FRAME_STATE_BIT(Generic, 58, NS_FRAME_MATHML_SCRIPT_DESCENDANT)
