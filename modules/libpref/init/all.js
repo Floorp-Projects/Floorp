27dcd8fff0447679a1e0c2ae78b19a5b0eb66cd9
Tom Ritter <tom@mozilla.com>
2017-04-05 11:18:34 -0500

    Bug 1329336 Block remote jar files by default r=valentin
    MozReview-Commit-ID: CqmTk9ICHVF

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1700,12 +1700,7 @@ pref("dom.server-events.default-reconnection-time", 5000); // in milliseconds
 // by the jar channel.
 pref("network.jar.open-unsafe-types", false);
 // If true, loading remote JAR files using the jar: protocol will be prevented.
-#ifdef RELEASE_OR_BETA
-// Keep allowing remote JAR files for IBM iNotes (see bug 1255139) for now.
-pref("network.jar.block-remote-files", false);
-#else
 pref("network.jar.block-remote-files", true);
-#endif
 
 // This preference, if true, causes all UTF-8 domain names to be normalized to
 // punycode.  The intention is to allow UTF-8 domain names as input, but never
