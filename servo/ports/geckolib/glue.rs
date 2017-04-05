e816fa300392cd332ca2f1f796602405dcee9694
Iris Hsiao <ihsiao@mozilla.com>
2017-04-05 16:56:36 +0800

    Backed out changeset aa46d54ab74b (bug 1321754) for stylo test failures

--- servo/ports/geckolib/glue.rs
+++ servo/ports/geckolib/glue.rs
@@ -327,7 +327,6 @@ pub extern "C" fn Servo_StyleSheet_Empty(mode: SheetParsingMode) -> RawServoStyl
         SheetParsingMode::eAuthorSheetFeatures => Origin::Author,
         SheetParsingMode::eUserSheetFeatures => Origin::User,
         SheetParsingMode::eAgentSheetFeatures => Origin::UserAgent,
-        SheetParsingMode::eSafeAgentSheetFeatures => Origin::UserAgent,
     };
     let shared_lock = global_style_data.shared_lock.clone();
     Arc::new(Stylesheet::from_str(
@@ -350,7 +349,6 @@ pub extern "C" fn Servo_StyleSheet_FromUTF8Bytes(loader: *mut Loader,
         SheetParsingMode::eAuthorSheetFeatures => Origin::Author,
         SheetParsingMode::eUserSheetFeatures => Origin::User,
         SheetParsingMode::eAgentSheetFeatures => Origin::UserAgent,
-        SheetParsingMode::eSafeAgentSheetFeatures => Origin::UserAgent,
     };
 
     let url_data = unsafe { RefPtr::from_ptr_ref(&extra_data) };
