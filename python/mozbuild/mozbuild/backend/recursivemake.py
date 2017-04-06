3edcd866473c289eebdcb6dc01253d4bda993335
Wes Kocher <wkocher@mozilla.com>
2017-04-06 15:29:24 -0700

    Backed out 4 changesets (bug 1320179) for causing merge conflicts a=backout
    Backed out changeset b8aa677f7452 (bug 1320179)
    Backed out changeset 9e2f77570128 (bug 1320179)
    Backed out changeset 470d8143b350 (bug 1320179)
    Backed out changeset ea4f1b3cc7b1 (bug 1320179)

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -524,7 +524,6 @@ class RecursiveMakeBackend(CommonBackend):
                 '.h',
                 '.inc',
                 '.py',
-                '.rs',
             )
             tier = 'export' if any(f.endswith(export_suffixes) for f in obj.outputs) else 'misc'
             self._no_skip[tier].add(backend_file.relobjdir)
