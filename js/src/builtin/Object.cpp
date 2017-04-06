3ec81308e82608e22d89abd1540193152bfa9a5a
Jan de Mooij <jdemooij@mozilla.com>
2017-04-06 10:15:05 +0200

    Bug 1353679 part 1 - Optimize the Object case in Object.prototype.toString because it's very common. r=evilpie

--- js/src/builtin/Object.cpp
+++ js/src/builtin/Object.cpp
@@ -516,17 +516,22 @@ js::obj_toString(JSContext* cx, unsigned argc, Value* vp)
         // Non-standard (bug 1277801): Use ClassName as a fallback in the interim
         if (!builtinTag) {
             const char* className = GetObjectClassName(cx, obj);
+            // "[object Object]" is by far the most common case at this point,
+            // so we optimize it here.
+            if (strcmp(className, "Object") == 0) {
+                builtinTag = cx->names().objectObject;
+            } else {
+                StringBuffer sb(cx);
+                if (!sb.append("[object ") || !sb.append(className, strlen(className)) ||
+                    !sb.append("]"))
+                {
+                    return false;
+                }
 
-            StringBuffer sb(cx);
-            if (!sb.append("[object ") || !sb.append(className, strlen(className)) ||
-                !sb.append("]"))
-            {
-                return false;
+                builtinTag = sb.finishString();
+                if (!builtinTag)
+                    return false;
             }
-
-            builtinTag = sb.finishString();
-            if (!builtinTag)
-                return false;
         }
 
         args.rval().setString(builtinTag);
