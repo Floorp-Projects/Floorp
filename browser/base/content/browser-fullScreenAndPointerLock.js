94b383c4506d4e9e3c426561d92df1366abd8b7a
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-04-07 01:04:54 +0200

    Backed out 2 changesets (bug 893505) for failing eslint. r=backout
    Backed out changeset 5880d471d75c (bug 893505)
    Backed out changeset 3246d7cb2729 (bug 893505)

--- browser/base/content/browser-fullScreenAndPointerLock.js
+++ browser/base/content/browser-fullScreenAndPointerLock.js
@@ -537,8 +537,7 @@ var FullScreen = {
     // e.g. we wouldn't want the autoscroll icon firing this event, so when the user
     // toggles chrome when moving mouse to the top, it doesn't go away again.
     if (aEvent.type == "popupshown" && !FullScreen._isChromeCollapsed &&
-        aEvent.target.localName != "tooltip" && aEvent.target.localName != "window" &&
-        aEvent.target.getAttribute("nopreventnavboxhide") != "true")
+        aEvent.target.localName != "tooltip" && aEvent.target.localName != "window")
       FullScreen._isPopupOpen = true;
     else if (aEvent.type == "popuphidden" && aEvent.target.localName != "tooltip" &&
              aEvent.target.localName != "window") {
@@ -546,10 +545,6 @@ var FullScreen = {
       // Try again to hide toolbar when we close the popup.
       FullScreen.hideNavToolbox(true);
     }
-  },
-
-  get navToolboxHidden() {
-    return this._isChromeCollapsed;
   },
 
   // Autohide helpers for the context menu item
@@ -584,7 +579,6 @@ var FullScreen = {
     }
 
     this._isChromeCollapsed = false;
-    Services.obs.notifyObservers(null, "fullscreen-nav-toolbox", "shown");
   },
 
   hideNavToolbox(aAnimate = false) {
@@ -608,8 +602,6 @@ var FullScreen = {
     gNavToolbox.style.marginTop =
       -gNavToolbox.getBoundingClientRect().height + "px";
     this._isChromeCollapsed = true;
-    Services.obs.notifyObservers(null, "fullscreen-nav-toolbox", "hidden");
-
     MousePosTracker.removeListener(this);
   },
 
