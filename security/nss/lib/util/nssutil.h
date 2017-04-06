798db12aa51654410a070b10b8d3697c5dddb2ed
Ryan VanderMeulen <ryanvm@gmail.com>
2017-04-06 15:23:59 -0400

    Bug 1353740 - Upgrade Firefox 53 to NSS 3.29.5. a=lizzard

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -19,10 +19,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <Beta>]"
  */
-#define NSSUTIL_VERSION "3.29.3"
+#define NSSUTIL_VERSION "3.29.5"
 #define NSSUTIL_VMAJOR 3
 #define NSSUTIL_VMINOR 29
-#define NSSUTIL_VPATCH 3
+#define NSSUTIL_VPATCH 5
 #define NSSUTIL_VBUILD 0
 #define NSSUTIL_BETA PR_FALSE
 
