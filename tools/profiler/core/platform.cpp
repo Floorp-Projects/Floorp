a951919765d6223f46b51a1cf0fb2b9952d40253
Markus Stange <mstange@themasta.com>
2017-04-08 21:07:52 -0400

    Bug 1321617 - Move parts of StreamJSON out into the callers and rename it to locked_profiler_stream_json_for_this_process. r=njn
    MozReview-Commit-ID: 239BJOa2bix
    
    --HG--
    extra : rebase_source : 36f248d60bf4c82deb2a62c5a45d9c1c3c44af2e
    extra : histedit_source : e5b2b4cb5f19e6efb69ff8e2d391199d3dbaf3ea

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1370,38 +1370,36 @@ BuildJavaThreadJSObject(SpliceableJSONWriter& aWriter)
 #endif
 
 static void
-StreamJSON(PS::LockRef aLock, SpliceableJSONWriter& aWriter, double aSinceTime)
+locked_profiler_stream_json_for_this_process(PS::LockRef aLock, SpliceableJSONWriter& aWriter, double aSinceTime)
 {
-  LOG("StreamJSON");
+  LOG("locked_profiler_stream_json_for_this_process");
 
   MOZ_RELEASE_ASSERT(NS_IsMainThread());
   MOZ_RELEASE_ASSERT(gPS && gPS->IsActive(aLock));
 
-  aWriter.Start(SpliceableJSONWriter::SingleLineStyle);
+  // Put shared library info
+  aWriter.StartArrayProperty("libs");
+  AppendSharedLibraries(aWriter);
+  aWriter.EndArray();
+
+  // Put meta data
+  aWriter.StartObjectProperty("meta");
   {
-    // Put shared library info
-    aWriter.StartArrayProperty("libs");
-    AppendSharedLibraries(aWriter);
-    aWriter.EndArray();
+    StreamMetaJSCustomObject(aLock, aWriter);
+  }
+  aWriter.EndObject();
 
-    // Put meta data
-    aWriter.StartObjectProperty("meta");
-    {
-      StreamMetaJSCustomObject(aLock, aWriter);
-    }
+  // Data of TaskTracer doesn't belong in the circular buffer.
+  if (gPS->FeatureTaskTracer(aLock)) {
+    aWriter.StartObjectProperty("tasktracer");
+    StreamTaskTracer(aLock, aWriter);
     aWriter.EndObject();
+  }
 
-    // Data of TaskTracer doesn't belong in the circular buffer.
-    if (gPS->FeatureTaskTracer(aLock)) {
-      aWriter.StartObjectProperty("tasktracer");
-      StreamTaskTracer(aLock, aWriter);
-      aWriter.EndObject();
-    }
-
-    // Lists the samples for each thread profile
-    aWriter.StartArrayProperty("threads");
-    {
-      gPS->SetIsPaused(aLock, true);
+  // Lists the samples for each thread profile
+  aWriter.StartArrayProperty("threads");
+  {
+    gPS->SetIsPaused(aLock, true);
 
       const PS::ThreadVector& liveThreads = gPS->LiveThreads(aLock);
       for (size_t i = 0; i < liveThreads.size(); i++) {
@@ -1422,43 +1420,22 @@ StreamJSON(PS::LockRef aLock, SpliceableJSONWriter& aWriter, double aSinceTime)
       }
 
 #if defined(PROFILE_JAVA)
-      if (gPS->FeatureJava(aLock)) {
-        java::GeckoJavaSampler::Pause();
+    if (gPS->FeatureJava(aLock)) {
+      java::GeckoJavaSampler::Pause();
 
-        aWriter.Start();
-        {
-          BuildJavaThreadJSObject(aWriter);
-        }
-        aWriter.End();
-
-        java::GeckoJavaSampler::Unpause();
+      aWriter.Start();
+      {
+        BuildJavaThreadJSObject(aWriter);
       }
+      aWriter.End();
+
+      java::GeckoJavaSampler::Unpause();
+    }
 #endif
 
-      gPS->SetIsPaused(aLock, false);
-    }
-    aWriter.EndArray();
-
-    aWriter.StartArrayProperty("processes");
-    // When notifying observers in other places in this file we are careful
-    // to do it when gPSMutex is unlocked, to avoid deadlocks. But that's not
-    // necessary here, because "profiler-subprocess" observers just call back
-    // into SubprocessCallback, which is simple and doesn't lock gPSMutex.
-    if (CanNotifyObservers()) {
-      // Send a event asking any subprocesses (plugins) to
-      // give us their information
-      SubprocessClosure closure(&aWriter);
-      nsCOMPtr<nsIObserverService> os =
-        mozilla::services::GetObserverService();
-      if (os) {
-        RefPtr<ProfileSaveEvent> pse =
-          new ProfileSaveEvent(SubProcessCallback, &closure);
-        os->NotifyObservers(pse, "profiler-subprocess", nullptr);
-      }
-    }
-    aWriter.EndArray();
+    gPS->SetIsPaused(aLock, false);
   }
-  aWriter.End();
+  aWriter.EndArray();
 }
 
 // END saving/streaming code
@@ -2160,7 +2137,31 @@ profiler_get_profile(double aSinceTime)
   }
 
   SpliceableChunkedJSONWriter b;
-  StreamJSON(lock, b, aSinceTime);
+  b.Start(SpliceableJSONWriter::SingleLineStyle);
+  {
+    locked_profiler_stream_json_for_this_process(lock, b, aSinceTime);
+
+    b.StartArrayProperty("processes");
+    // When notifying observers in other places in this file we are careful
+    // to do it when gPSMutex is unlocked, to avoid deadlocks. But that's not
+    // necessary here, because "profiler-subprocess" observers just call back
+    // into SubprocessCallback, which is simple and doesn't lock gPSMutex.
+    if (CanNotifyObservers()) {
+      // Send a event asking any subprocesses (plugins) to
+      // give us their information
+      SubprocessClosure closure(&b);
+      nsCOMPtr<nsIObserverService> os =
+        mozilla::services::GetObserverService();
+      if (os) {
+        RefPtr<ProfileSaveEvent> pse =
+          new ProfileSaveEvent(SubProcessCallback, &closure);
+        os->NotifyObservers(pse, "profiler-subprocess", nullptr);
+      }
+    }
+    b.EndArray();
+  }
+  b.End();
+
   return b.WriteFunc()->CopyData();
 }
 
@@ -2207,7 +2208,17 @@ locked_profiler_save_profile_to_file(PS::LockRef aLock, const char* aFilename)
   stream.open(aFilename);
   if (stream.is_open()) {
     SpliceableJSONWriter w(mozilla::MakeUnique<OStreamJSONWriteFunc>(stream));
-    StreamJSON(aLock, w, /* sinceTime */ 0);
+    w.Start(SpliceableJSONWriter::SingleLineStyle);
+    {
+      locked_profiler_stream_json_for_this_process(aLock, w, /* sinceTime */ 0);
+
+      // Don't include profiles from other processes because this is a
+      // synchronous function.
+      w.StartArrayProperty("processes");
+      w.EndArray();
+    }
+    w.End();
+
     stream.close();
   }
 }
