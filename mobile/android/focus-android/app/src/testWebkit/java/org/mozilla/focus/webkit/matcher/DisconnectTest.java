dc95bbf3534e9a1f946cd22c38ec0cc9135b2a22
Andrzej Hunt <andrzej@ahunt.org>
2017-04-10 16:27:27 -0700

    [focus] Add UrlMatcher disconnect list integration test (https://github.com/mozilla-mobile/focus-android/issues/431)
    This verifies that we've actually loaded disconnect lists correctly.
    
    Note: this test will fail in focusWebkitRelease when running tests locally
    (e.g. via |gradlew test|), due to the lack of the Adjust API token. That is
    expected, BuddyBuild seems to always supply the API token hence that failure
    doesn't occur on Buddybuild. It seems best to just ignore the local failure
    in order to avoid making our adjust initialisation code have context-specific
    workarounds (which could then fail, etc.).

---
+++ mobile/android/focus-android/app/src/testWebkit/java/org/mozilla/focus/webkit/matcher/DisconnectTest.java
@@ -0,0 +1,112 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+package org.mozilla.focus.webkit.matcher;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mozilla.focus.BuildConfig;
+import org.mozilla.focus.R;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Integration test for tracking protection, which tests that some real websites are/aren't
+ * blocked under given circumstances.
+ *
+ * UrlMatcherTest tests the general correctness of our URL matching code, this test is intended
+ * to verify that our disconnect lists are loaded correctly (and therefore are dependent on the
+ * contents of our disconnect lists).
+ *
+ * This test also verifies that the entity lists (whitelists for specific domains) actually work
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, packageName = "org.mozilla.focus")
+public class DisconnectTest {
+
+    // IMPORTANT NOTE - IF RUNNING TESTS USING ANDROID STUDIO:
+    // Read the following for a guide on how to get AS to correctly load resources, if you don't
+    // do this you will get Resource Loading exceptions:
+    // http://robolectric.org/getting-started/#note-for-linux-and-mac-users
+    @Test
+    public void matches() throws Exception {
+        final UrlMatcher matcher = UrlMatcher.loadMatcher(RuntimeEnvironment.application, R.raw.blocklist, new int[] { R.raw.google_mapping }, R.raw.entitylist);
+
+        // Enable everything
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(RuntimeEnvironment.application);
+        prefs.edit()
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_ads), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_analytics), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_other), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_social), true)
+                .commit();
+
+        // We check that our google_mapping was loaded correctly. We do these checks per-category, so we have:
+        // ads:
+        assertTrue(matcher.matches("http://admeld.com/foobar", "http://mozilla.org"));
+        // And we check that the entitylist unblocks this on google properties:
+        assertFalse(matcher.matches("http://admeld.com/foobar", "http://google.com"));
+        // analytics:
+        assertTrue(matcher.matches("http://google-analytics.com/foobar", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://google-analytics.com/foobar", "http://google.com"));
+        // social:
+        assertTrue(matcher.matches("http://plus.google.com/something", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://plus.google.com/something", "http://google.com"));
+
+        // Facebook is special in that we move it from "Disconnect" into social:
+        assertTrue(matcher.matches("http://facebook.fr", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://facebook.fr", "http://facebook.com"));
+
+        // Now disable social, and check that only social sites have changed:
+        prefs.edit()
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_ads), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_analytics), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_other), true)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_social), false)
+                .commit();
+
+        // ads:
+        assertTrue(matcher.matches("http://admeld.com/foobar", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://admeld.com/foobar", "http://google.com"));
+        // analytics:
+        assertTrue(matcher.matches("http://google-analytics.com/foobar", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://google-analytics.com/foobar", "http://google.com"));
+        // social:
+        assertFalse(matcher.matches("http://plus.google.com/something", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://plus.google.com/something", "http://google.com"));
+
+        // And facebook which has been moved from Disconnect into social
+        assertFalse(matcher.matches("http://facebook.fr", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://facebook.fr", "http://facebook.com"));
+
+        // Now disable everything - all sites should work:
+        prefs.edit()
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_ads), false)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_analytics), false)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_other), false)
+                .putBoolean(RuntimeEnvironment.application.getString(R.string.pref_key_privacy_block_social), false)
+                .commit();
+
+        // ads:
+        assertFalse(matcher.matches("http://admeld.com/foobar", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://admeld.com/foobar", "http://google.com"));
+        // analytics:
+        assertFalse(matcher.matches("http://google-analytics.com/foobar", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://google-analytics.com/foobar", "http://google.com"));
+        // social:
+        assertFalse(matcher.matches("http://plus.google.com/something", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://plus.google.com/something", "http://google.com"));
+
+        // Facebook is special in that we move it from "Disconnect" into social:
+        assertFalse(matcher.matches("http://facebook.fr", "http://mozilla.org"));
+        assertFalse(matcher.matches("http://facebook.fr", "http://facebook.com"));
+    }
+}
