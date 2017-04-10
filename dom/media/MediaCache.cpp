64b0fbf69a6afad1c1158a7ae68a53a3d48b9d61
JW Wang <jwwang@mozilla.com>
2017-04-10 11:56:35 +0800

    Bug 1354389. P2 - remove the unused function. r=cpearce
    MozReview-Commit-ID: 4Mx89MxJmVO
    
    --HG--
    extra : rebase_source : 0427e7597dcf7087485945fd283e91e81de9b912

--- dom/media/MediaCache.cpp
+++ dom/media/MediaCache.cpp
@@ -152,8 +152,6 @@ public:
   // to handle changes happening when the monitor is not held.
   nsresult ReadCacheFile(int64_t aOffset, void* aData, int32_t aLength,
                          int32_t* aBytes);
-  // This will fail if all aLength bytes are not read
-  nsresult ReadCacheFileAllBytes(int64_t aOffset, void* aData, int32_t aLength);
 
   int64_t AllocateResourceID()
   {
@@ -687,29 +685,6 @@ MediaCache::ReadCacheFile(
     return fileCache->Read(aOffset,
       reinterpret_cast<uint8_t*>(aData), aLength, aBytes);
   }
-}
-
-nsresult
-MediaCache::ReadCacheFileAllBytes(int64_t aOffset, void* aData, int32_t aLength)
-{
-  mReentrantMonitor.AssertCurrentThreadIn();
-
-  int64_t offset = aOffset;
-  int32_t count = aLength;
-  // Cast to char* so we can do byte-wise pointer arithmetic
-  char* data = static_cast<char*>(aData);
-  while (count > 0) {
-    int32_t bytes;
-    nsresult rv = ReadCacheFile(offset, data, count, &bytes);
-    if (NS_FAILED(rv))
-      return rv;
-    if (bytes == 0)
-      return NS_ERROR_FAILURE;
-    count -= bytes;
-    data += bytes;
-    offset += bytes;
-  }
-  return NS_OK;
 }
 
 static int32_t GetMaxBlocks()
