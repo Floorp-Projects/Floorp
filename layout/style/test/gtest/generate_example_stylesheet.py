63558c115c237e6a20a563438907b3a85850435e
Simon Sapin <simon.sapin@exyr.org>
2017-04-10 11:26:15 +0200

    Bug 1355028 - stylo: Add gtest microbenchmarks for CSS parsing performance. r=bholley,froydnj
    These can be run with:
    
        ./mach gtest Stylo.*
    
    Note that running `./mach build` is required after modifying example.css
    
    MozReview-Commit-ID: 8KvhYHgGLfD

---
+++ layout/style/test/gtest/generate_example_stylesheet.py
@@ -0,0 +1,11 @@
+def main(output, stylesheet):
+    css = open(stylesheet, 'rb').read()
+    css = css.replace('\\', '\\\\').replace('\n', '\\n').replace('"', '\\"')
+
+    # Work around "error C2026: string too big"
+    # https://msdn.microsoft.com/en-us/library/dddywwsc.aspx
+    chunk_size = 10000
+    chunks = ('"%s"' % css[i:i + chunk_size] for i in range(0, len(css), chunk_size))
+
+    header = '#define EXAMPLE_STYLESHEET ' + ' '.join(chunks)
+    output.write(header)
