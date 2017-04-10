659a78629f99bf6e2e01b9362ecf162c7a834d44
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-04-10 18:29:16 +0900

    Bug 1330912 - Part 1. Add async version of SetCurrentDictionary using list. r=Ehsan
    Now, mozInlineSpellChecker::UpdateCurrentDictionary uses several sync IPC message to update spellcheck dictionary.  So I want async IPC to set dictionary.
    
    Also, since nsEditorSpellCheck::DictionaryFetched calls SetCurrentDictionary several times (max: 6 times), so to reduce calls, we should use list of dictionary.
    
    MozReview-Commit-ID: EVMAJxpMT2X

--- extensions/spellcheck/hunspell/glue/RemoteSpellCheckEngineChild.cpp
+++ extensions/spellcheck/hunspell/glue/RemoteSpellCheckEngineChild.cpp
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/UniquePtr.h"
 #include "RemoteSpellCheckEngineChild.h"
 
 namespace mozilla {
@@ -16,6 +17,39 @@ RemoteSpellcheckEngineChild::~RemoteSpellcheckEngineChild()
   // null out the owner's SpellcheckEngineChild to prevent state corruption
   // during shutdown
   mOwner->DeleteRemoteEngine();
+}
+
+RefPtr<GenericPromise>
+RemoteSpellcheckEngineChild::SetCurrentDictionaryFromList(
+  const nsTArray<nsString>& aList)
+{
+  MozPromiseHolder<GenericPromise>* promiseHolder =
+    new MozPromiseHolder<GenericPromise>();
+  if (!SendSetDictionaryFromList(
+         aList,
+         reinterpret_cast<intptr_t>(promiseHolder))) {
+    delete promiseHolder;
+    return GenericPromise::CreateAndReject(NS_ERROR_FAILURE, __func__);
+  }
+  // promiseHolder will removed by receive message
+  return promiseHolder->Ensure(__func__);
+}
+
+mozilla::ipc::IPCResult
+RemoteSpellcheckEngineChild::RecvNotifyOfCurrentDictionary(
+                               const nsString& aDictionary,
+                               const intptr_t& aId)
+{
+  MozPromiseHolder<GenericPromise>* promiseHolder =
+    reinterpret_cast<MozPromiseHolder<GenericPromise>*>(aId);
+  mOwner->mCurrentDictionary = aDictionary;
+  if (aDictionary.IsEmpty()) {
+    promiseHolder->RejectIfExists(NS_ERROR_NOT_AVAILABLE, __func__);
+  } else {
+    promiseHolder->ResolveIfExists(true, __func__);
+  }
+  delete promiseHolder;
+  return IPC_OK();
 }
 
 } //namespace mozilla
