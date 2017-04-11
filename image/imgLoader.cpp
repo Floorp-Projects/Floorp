8d24e03eb98cbf168068c75f4b06924b98db39a2
Timothy Nikkel <tnikkel@gmail.com>
2017-04-11 03:14:11 -0500

    Bug 1342567. r=aosmond a=dveditz

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -2192,6 +2192,18 @@ imgLoader::LoadImage(nsIURI* aURI,
         request->SetCacheEntry(entry);
 
         if (mCacheTracker) {
+          if (MOZ_UNLIKELY(!entry->GetExpirationState()->IsTracked())) {
+            bool inCache = false;
+            RefPtr<imgCacheEntry> e;
+            if (cache.Get(key, getter_AddRefs(e)) && e) {
+              inCache = (e == entry);
+            }
+            gfxCriticalNoteOnce << "entry with no proxies is no in tracker "
+                                << "request->HasConsumers() "
+                                << (request->HasConsumers() ? "true" : "false")
+                                << " inCache " << (inCache ? "true" : "false");
+          }
+
           mCacheTracker->MarkUsed(entry);
         }
       }
