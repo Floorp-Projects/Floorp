d07e8fbcc2d7768db85e51c36064936ced6f9a5b
Kartikaya Gupta <kgupta@mozilla.com>
2017-04-11 16:13:44 -0400

    Bug 1355577 - Make the pointer in WrByteSlice const on the C++ side to match the Rust side. r=jrmuizel
    MozReview-Commit-ID: FWCzZt8RAMj

--- gfx/webrender_bindings/webrender_ffi.h
+++ gfx/webrender_bindings/webrender_ffi.h
@@ -66,7 +66,7 @@ WR_DECL_FFI_2(WrFontKey, uint32_t, uint32_t)
 // view of a buffer of bytes.
 // The canonical gecko equivalent is mozilla::Range<uint8_t>.
 struct WrByteSlice {
-  uint8_t* buffer;
+  const uint8_t* buffer;
   size_t len;
 };
 
