2f7b2ff3903530dbd94ff2764612b92a029bb3fc
Fischer.json <fischer.json@gmail.com>
2017-04-11 12:07:29 +0800

    Bug 1309123 - Show persistent-storage permission request notification, r=florian
    MozReview-Commit-ID: H3YAalEyWME
    
    --HG--
    extra : rebase_source : 486c5ed353d048c832baf3d6ec81e74c617b5761

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -2315,6 +2315,11 @@ const ContentPermissionIntegration = {
       case "desktop-notification": {
         return new PermissionUI.DesktopNotificationPermissionPrompt(request);
       }
+      case "persistent-storage": {
+        if (Services.prefs.getBoolPref("browser.storageManager.enabled")) {
+          return new PermissionUI.PersistentStoragePermissionPrompt(request);
+        }
+      }
     }
     return undefined;
   },
