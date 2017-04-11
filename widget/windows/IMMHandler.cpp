61ce5526c6ccacba88dc78048373a05a5d598b77
Masayuki Nakano <masayuki@d-toybox.com>
2017-04-11 21:24:55 +0900

    Bug 1296220 Rename nsIMEUpdatePreference to mozilla::widget::IMEUpdatePreference r=m_kato
    MozReview-Commit-ID: 2rIXTlwA6my
    
    --HG--
    extra : rebase_source : a51be3edd717092738c2b5e8ccc4f60540712bfd

--- widget/windows/IMMHandler.cpp
+++ widget/windows/IMMHandler.cpp
@@ -390,12 +390,12 @@ IMMHandler::GetKeyboardCodePage()
 }
 
 // static
-nsIMEUpdatePreference
-IMMHandler::GetIMEUpdatePreference()
+IMENotificationRequests
+IMMHandler::GetIMENotificationRequests()
 {
-  return nsIMEUpdatePreference(
-    nsIMEUpdatePreference::NOTIFY_POSITION_CHANGE |
-    nsIMEUpdatePreference::NOTIFY_MOUSE_BUTTON_EVENT_ON_CHAR);
+  return IMENotificationRequests(
+    IMENotificationRequests::NOTIFY_POSITION_CHANGE |
+    IMENotificationRequests::NOTIFY_MOUSE_BUTTON_EVENT_ON_CHAR);
 }
 
 // used for checking the lParam of WM_IME_COMPOSITION
