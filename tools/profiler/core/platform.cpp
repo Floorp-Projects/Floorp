ab8ce3b9b4e74e32e81e7bfa55855146a9796f0f
Markus Stange <mstange@themasta.com>
2017-04-12 16:45:28 -0400

    Bug 1321617 - Reduce the scope of the lock in profiler_get_profile. r=njn
    MozReview-Commit-ID: 5OmVMwy1yMl

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1438,6 +1438,24 @@ locked_profiler_stream_json_for_this_process(PS::LockRef aLock, SpliceableJSONWr
   aWriter.EndArray();
 }
 
+static bool
+profiler_stream_json_for_this_process(SpliceableJSONWriter& aWriter, double aSinceTime)
+{
+  LOG("profiler_stream_json_for_this_process");
+
+  MOZ_RELEASE_ASSERT(NS_IsMainThread());
+  MOZ_RELEASE_ASSERT(gPS);
+
+  PS::AutoLock lock(gPSMutex);
+
+  if (!gPS->IsActive(lock)) {
+    return false;
+  }
+
+  locked_profiler_stream_json_for_this_process(lock, aWriter, aSinceTime);
+  return true;
+}
+
 // END saving/streaming code
 ////////////////////////////////////////////////////////////////////////
 
@@ -2130,22 +2148,14 @@ profiler_get_profile(double aSinceTime)
   MOZ_RELEASE_ASSERT(NS_IsMainThread());
   MOZ_RELEASE_ASSERT(gPS);
 
-  PS::AutoLock lock(gPSMutex);
-
-  if (!gPS->IsActive(lock)) {
-    return nullptr;
-  }
-
   SpliceableChunkedJSONWriter b;
   b.Start(SpliceableJSONWriter::SingleLineStyle);
   {
-    locked_profiler_stream_json_for_this_process(lock, b, aSinceTime);
+    if (!profiler_stream_json_for_this_process(b, aSinceTime)) {
+      return nullptr;
+    }
 
     b.StartArrayProperty("processes");
-    // When notifying observers in other places in this file we are careful
-    // to do it when gPSMutex is unlocked, to avoid deadlocks. But that's not
-    // necessary here, because "profiler-subprocess" observers just call back
-    // into SubprocessCallback, which is simple and doesn't lock gPSMutex.
     if (CanNotifyObservers()) {
       // Send a event asking any subprocesses (plugins) to
       // give us their information
