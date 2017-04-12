3ab253476f5743208ece0425c5b7e5cfd25475ea
Jeff Gilbert <jgilbert@mozilla.com>
2017-04-12 09:22:28 -0700

    Bug 1355763 - Handle UNPACK_ state for ConvertPlanarYCbCr, and reject blits with _SKIP_* for now. - r=daoshengmu
    MozReview-Commit-ID: JsDF66DwTfZ

--- gfx/gl/ScopedGLHelpers.h
+++ gfx/gl/ScopedGLHelpers.h
@@ -362,6 +362,28 @@ protected:
     void UnwrapImpl();
 };
 
+struct ResetUnpackState
+    : public ScopedGLWrapper<ResetUnpackState>
+{
+    friend struct ScopedGLWrapper<ResetUnpackState>;
+
+protected:
+    GLuint mAlignment;
+
+    GLuint mPBO;
+    GLuint mRowLength;
+    GLuint mImageHeight;
+    GLuint mSkipPixels;
+    GLuint mSkipRows;
+    GLuint mSkipImages;
+
+public:
+    explicit ResetUnpackState(GLContext* gl);
+
+protected:
+    void UnwrapImpl();
+};
+
 struct ScopedBindPBO final
     : public ScopedGLWrapper<ScopedBindPBO>
 {
