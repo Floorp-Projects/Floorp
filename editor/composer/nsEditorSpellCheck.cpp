4c3994411238bc58c305436979f25c2e99eb04fa
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-04-12 17:43:51 +0900

    Bug 1056322 - Don't run DictionaryFetcher when spllchecker isn't initialized. r=masayuki a=gchang
    When CanSpellCheck isn't called yet, mSpellChecker won't be initialized.  So we should check it before running DictionaryFetcher.
    
    I think that UpdateCurrentDictionary is called by OnFocus as long as nsEditorSpellCheck::InitSpellChecker isn't called.  But I cannot find test case to reproduce this...
    
    MozReview-Commit-ID: 5H1pJ0AnX5X

--- editor/composer/nsEditorSpellCheck.cpp
+++ editor/composer/nsEditorSpellCheck.cpp
@@ -665,6 +665,10 @@ nsEditorSpellCheck::DeleteSuggestedWordList()
 NS_IMETHODIMP
 nsEditorSpellCheck::UpdateCurrentDictionary(nsIEditorSpellCheckCallback* aCallback)
 {
+  if (NS_WARN_IF(!mSpellChecker)) {
+    return NS_ERROR_NOT_INITIALIZED;
+  }
+
   nsresult rv;
 
   RefPtr<nsEditorSpellCheck> kungFuDeathGrip = this;
