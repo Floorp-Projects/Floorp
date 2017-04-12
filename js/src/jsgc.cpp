10e30f9447d2d8ea5efdda37783d97ef5be5d37b
Shu-yu Guo <shu@rfrn.org>
2017-04-12 14:13:21 -0700

    Bug 1348134 - Handle compression tasks with major GCs instead of eagerly. (r=sfink,jonco)

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -3941,6 +3941,15 @@ GCRuntime::beginMarkPhase(JS::gcreason::Reason reason, AutoLockForExclusiveAcces
         }
     }
 
+    /*
+     * Process any queued source compressions during the start of a major
+     * GC.
+     */
+    {
+        AutoLockHelperThreadState helperLock;
+        HelperThreadState().startHandlingCompressionTasks(helperLock);
+    }
+
     startNumber = number;
 
     /*
@@ -4981,6 +4990,7 @@ MAKE_GC_SWEEP_TASK(SweepInitialShapesTask);
 MAKE_GC_SWEEP_TASK(SweepObjectGroupsTask);
 MAKE_GC_SWEEP_TASK(SweepRegExpsTask);
 MAKE_GC_SWEEP_TASK(SweepMiscTask);
+MAKE_GC_SWEEP_TASK(AttachCompressedSourcesTask);
 #undef MAKE_GC_SWEEP_TASK
 
 /* virtual */ void
@@ -5032,6 +5042,22 @@ SweepMiscTask::run()
         c->sweepTemplateLiteralMap();
         c->sweepSelfHostingScriptSource();
         c->sweepNativeIterators();
+    }
+}
+
+/* virtual */ void
+AttachCompressedSourcesTask::run()
+{
+    AutoLockHelperThreadState lock;
+    GlobalHelperThreadState::SourceCompressionTaskVector& finished =
+        HelperThreadState().compressionFinishedList(lock);
+    for (size_t i = 0; i < finished.length(); i++) {
+        SourceCompressionTask* task = finished[i];
+        if (task->runtimeMatches(runtime())) {
+            task->complete();
+            js_delete(task);
+            HelperThreadState().remove(finished, &i);
+        }
     }
 }
 
@@ -5113,6 +5139,7 @@ GCRuntime::beginSweepingSweepGroup(AutoLockForExclusiveAccess& lock)
     SweepObjectGroupsTask sweepObjectGroupsTask(rt);
     SweepRegExpsTask sweepRegExpsTask(rt);
     SweepMiscTask sweepMiscTask(rt);
+    AttachCompressedSourcesTask attachCompressedSourcesTask(rt);
     WeakCacheTaskVector sweepCacheTasks = PrepareWeakCacheTasks(rt);
 
     for (GCSweepGroupIter zone(rt); !zone.done(); zone.next()) {
@@ -5161,12 +5188,13 @@ GCRuntime::beginSweepingSweepGroup(AutoLockForExclusiveAccess& lock)
             startTask(sweepObjectGroupsTask, gcstats::PHASE_SWEEP_TYPE_OBJECT, helperLock);
             startTask(sweepRegExpsTask, gcstats::PHASE_SWEEP_REGEXP, helperLock);
             startTask(sweepMiscTask, gcstats::PHASE_SWEEP_MISC, helperLock);
+            startTask(attachCompressedSourcesTask, gcstats::PHASE_SWEEP_MISC, helperLock);
             for (auto& task : sweepCacheTasks)
                 startTask(task, gcstats::PHASE_SWEEP_MISC, helperLock);
         }
 
-        // The remainder of the of the tasks run in parallel on the active
-        // thread until we join, below.
+        // The remainder of the tasks run in parallel on the active thread
+        // until we join, below.
         {
             gcstats::AutoPhase ap(stats(), gcstats::PHASE_SWEEP_MISC);
 
@@ -5242,6 +5270,7 @@ GCRuntime::beginSweepingSweepGroup(AutoLockForExclusiveAccess& lock)
         joinTask(sweepObjectGroupsTask, gcstats::PHASE_SWEEP_TYPE_OBJECT, helperLock);
         joinTask(sweepRegExpsTask, gcstats::PHASE_SWEEP_REGEXP, helperLock);
         joinTask(sweepMiscTask, gcstats::PHASE_SWEEP_MISC, helperLock);
+        joinTask(attachCompressedSourcesTask, gcstats::PHASE_SWEEP_MISC, helperLock);
         for (auto& task : sweepCacheTasks)
             joinTask(task, gcstats::PHASE_SWEEP_MISC, helperLock);
     }
