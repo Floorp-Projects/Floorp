d9a7bab6ded8558fcb0b8e1eb5c4f764fb8b97f3
Iris Hsiao <ihsiao@mozilla.com>
2017-04-13 14:46:38 +0800

    Backed out changeset f71e84602f68 (bug 1355763) for assertion failures at ImageBitmap.cpp

--- dom/canvas/TexUnpackBlob.cpp
+++ dom/canvas/TexUnpackBlob.cpp
@@ -637,14 +637,6 @@ TexUnpackImage::TexOrSubImage(bool isSubImage, bool needsRespec, const char* fun
             break;
         }
 
-        if (webgl->mPixelStore_UnpackSkipPixels ||
-            webgl->mPixelStore_UnpackSkipRows ||
-            webgl->mPixelStore_UnpackSkipImages)
-        {
-            fallbackReason = "non-zero UNPACK_SKIP_* not yet supported";
-            break;
-        }
-
         const auto fnHasPremultMismatch = [&]() {
             if (mSrcAlphaType == gfxAlphaType::Opaque)
                 return false;
