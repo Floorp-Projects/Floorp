0ee4a22385e9886c2cfb15f4ccd24295ca2e0c43
Kris Maglione <maglione.k@gmail.com>
2017-04-14 16:13:41 -0700

    Bug 1314861: Minor optimization: Define globals for shared sandbox modules on the sandbox rather than each module. r=ochameau
    MozReview-Commit-ID: Lre6L2u4Y2r
    
    --HG--
    extra : rebase_source : 118422fee9361152037f7746fa4beaffa6d5157d

--- devtools/server/actors/script.js
+++ devtools/server/actors/script.js
@@ -2305,7 +2305,7 @@ exports.AddonThreadActor = AddonThreadActor;
  *        An optional prefix for the reported error message.
  */
 var oldReportError = reportError;
-reportError = function (aError, aPrefix = "") {
+this.reportError = function (aError, aPrefix = "") {
   assert(aError instanceof Error, "Must pass Error objects to reportError");
   let msg = aPrefix + aError.message + ":\n" + aError.stack;
   oldReportError(msg);
