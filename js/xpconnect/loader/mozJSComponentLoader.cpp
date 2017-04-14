e96634ee681e21348cd875d0b53bb84edf2016fa
Andrew McCreight <continuation@gmail.com>
2017-04-14 14:13:19 -0700

    Bug 1356666, part 2 - Fix some style issues in mozJSSubScriptLoader. r=mrbkap
    mSystemPrincipal is an nsCOMPtr so it does not need to be explicitly
    initialized.
    
    Fields don't have to be lined up.
    
    Empty function bodies don't need to be commented as such.
    
    The blank line at the start of mozJSComponentLoader.cpp prevents Emacs
    from using the mode line.
    
    MozReview-Commit-ID: 7Az1x8jmxTI

--- js/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/xpconnect/loader/mozJSComponentLoader.cpp
@@ -1,4 +1,3 @@
-
 /* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* vim: set ts=8 sts=4 et sw=4 tw=99: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
