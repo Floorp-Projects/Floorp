eaf04b0fea84793d22cd20da9ccd5305bdfeb12f
Jan Henning <jh+bugzilla@buttercookie.de>
2017-04-14 19:58:09 +0200

    Bug 1352997 - Part 7 - Don't switch activities if we're closing a tab while exiting the activity. r=sebastian,walkingice
    Closing the currently selected tab will select another tab, which can trigger an activity switch if the tab types differ. We don't want that if we're about to close the activity, as that'll bring that activity into the foreground instead of simply walking back along the activity history stack.
    
    To give an example: Without this patch, closing the last custom tab will select a normal browsing tab, which will trigger an activity switch, so instead of returning to the user's previous activity, pressing back will send us to BrowserApp.
    
    There's one additional catch:
    If we change our onDone() behaviour and no longer finish() the SingleTabActivity when exiting (in order to avoid a costly restart of Gecko should the user return to us soon), this means that the activity that has just been exited could be brought back into the foreground via the onResume() codepath.
    
    In that case the mLastActiveGeckoApp-check won't be triggered if no other GeckoApp-based activity was active in the meantime, so we wouldn't reselect/reopen the activity's desired tab, but instead display the tab that was selected when closing the previous tab. Therefore, we track this via an additional flag that is set for this case.
    
    MozReview-Commit-ID: 3jOvBXQUrfo
    
    --HG--
    extra : rebase_source : a535fff92c02ef9d711faf6083e9146ed992fc03

--- mobile/android/base/java/org/mozilla/gecko/Tabs.java
+++ mobile/android/base/java/org/mozilla/gecko/Tabs.java
@@ -313,6 +313,10 @@ public class Tabs implements BundleEventListener {
     }
 
     public synchronized Tab selectTab(int id) {
+        return selectTab(id, true);
+    }
+
+    public synchronized Tab selectTab(int id, boolean switchActivities) {
         if (!mTabs.containsKey(id))
             return null;
 
@@ -325,7 +329,7 @@ public class Tabs implements BundleEventListener {
             return tab;
         }
 
-        if (oldTab != null && oldTab.getType() != tab.getType() &&
+        if (switchActivities && oldTab != null && oldTab.getType() != tab.getType() &&
                 !currentActivityMatchesTab(tab)) {
             // We're in the wrong activity for this kind of tab, so launch the correct one
             // and then try again.
@@ -536,16 +540,22 @@ public class Tabs implements BundleEventListener {
         closeTab(tab, getNextTab(tab));
     }
 
+    /** Don't switch activities even if the default next tab is of a different tab type */
+    public synchronized void closeTabNoActivitySwitch(Tab tab) {
+        closeTab(tab, getNextTab(tab), false, false);
+    }
+
     public synchronized void closeTab(Tab tab, Tab nextTab) {
-        closeTab(tab, nextTab, false);
+        closeTab(tab, nextTab, false, true);
     }
 
     public synchronized void closeTab(Tab tab, boolean showUndoToast) {
-        closeTab(tab, getNextTab(tab), showUndoToast);
+        closeTab(tab, getNextTab(tab), showUndoToast, true);
     }
 
     /** Close tab and then select nextTab */
-    public synchronized void closeTab(final Tab tab, Tab nextTab, boolean showUndoToast) {
+    public synchronized void closeTab(final Tab tab, Tab nextTab,
+                                      boolean showUndoToast, boolean switchActivities) {
         if (tab == null)
             return;
 
@@ -556,7 +566,7 @@ public class Tabs implements BundleEventListener {
             nextTab = loadUrl(getHomepageForNewTab(mAppContext), LOADURL_NEW_TAB);
         }
 
-        selectTab(nextTab.getId());
+        selectTab(nextTab.getId(), switchActivities);
 
         tab.onDestroy();
 
