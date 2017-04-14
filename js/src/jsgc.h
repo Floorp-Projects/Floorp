ce543bcc73cf10a2aa24b05a6c2d656c126bfb5f
Jan de Mooij <jdemooij@mozilla.com>
2017-04-14 12:29:15 +0200

    Bug 1353359 part 4 - Use BindName IC in Ion and remove the old IonCache infrastructure. r=evilpie

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -1405,9 +1405,6 @@ struct MOZ_RAII AutoDisableCompactingGC
     JSContext* cx;
 };
 
-void
-PurgeJITCaches(JS::Zone* zone);
-
 // This is the same as IsInsideNursery, but not inlined.
 bool
 UninlinedIsInsideNursery(const gc::Cell* cell);
