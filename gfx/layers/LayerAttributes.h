e0066c431976b00dcc14e8a054780ebb66734f4f
David Anderson <dvander@alliedmods.net>
2017-04-13 21:25:16 -0700

    Make finer-grained decisions about whether to update hit testing trees. (bug 1352918, r=kats)
    Updating hit testing trees is very expensive for many layers, and often
    layer properties that change don't necessitate hit testing tree updates.
    While layer tree structure changes do, we can be smarter about
    individual layer attributes.

--- gfx/layers/LayerAttributes.h
+++ gfx/layers/LayerAttributes.h
@@ -170,6 +170,27 @@ public:
     return true;
   }
 
+  // This returns true if scrolling info is equivalent for the purposes of
+  // APZ hit testing.
+  bool HitTestingInfoIsEqual(const SimpleLayerAttributes& aOther) const {
+    if (mIsScrollbarContainer != aOther.mIsScrollbarContainer) {
+      return false;
+    }
+    if (mScrollbarTargetContainerId != aOther.mScrollbarTargetContainerId) {
+      return false;
+    }
+    if (mScrollbarDirection != aOther.mScrollbarDirection) {
+      return false;
+    }
+    if (FixedPositionScrollContainerId() != aOther.FixedPositionScrollContainerId()) {
+      return false;
+    }
+    if (mTransform != aOther.mTransform) {
+      return false;
+    }
+    return true;
+  }
+
   //
   // Getters.
   //
