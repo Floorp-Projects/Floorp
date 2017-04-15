e63348d011a49db5c240ecd32aab813ed3cf83f3
Kris Maglione <maglione.k@gmail.com>
2017-04-15 14:09:41 -0700

    Bug 1356826: Part 6 - Wait for delayed startup before checking for side-loads. r=aswan
    MozReview-Commit-ID: IpuRNbdUXmE

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1143,6 +1143,11 @@ addEventListener("DOMContentLoaded", function onDCL() {
   gBrowser.updateBrowserRemoteness(initBrowser, gMultiProcessBrowser);
 });
 
+let _resolveDelayedStartup;
+var delayedStartupPromise = new Promise(resolve => {
+  _resolveDelayedStartup = resolve;
+});
+
 var gBrowserInit = {
   delayedStartupFinished: false,
 
@@ -1605,6 +1610,7 @@ var gBrowserInit = {
 
     this.delayedStartupFinished = true;
 
+    _resolveDelayedStartup();
     Services.obs.notifyObservers(window, "browser-delayed-startup-finished");
     TelemetryTimestamps.add("delayedStartupFinished");
   },
