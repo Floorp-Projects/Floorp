faa7c87b80e5edb05d9868837844b4f5801ff0bf
Shane Caraveo <scaraveo@mozilla.com>
2017-04-14 17:01:23 -0700

    Bug 1350597 fix reload/upgrade of sidebar addon, r=kmag
    MozReview-Commit-ID: FcC7zJn1ka5

--- browser/components/extensions/ext-sidebarAction.js
+++ browser/components/extensions/ext-sidebarAction.js
@@ -101,10 +101,11 @@ this.sidebarAction = class extends ExtensionAPI {
                        (evt, tab) => { this.updateWindow(tab.ownerGlobal); });
 
     let install = this.extension.startupReason === "ADDON_INSTALL";
+    let upgrade = ["ADDON_UPGRADE", "ADDON_DOWNGRADE"].includes(this.extension.startupReason);
     for (let window of windowTracker.browserWindows()) {
       this.updateWindow(window);
-      if (install) {
-        let {SidebarUI} = window;
+      let {SidebarUI} = window;
+      if (install || (upgrade && SidebarUI.lastOpenedId == this.id)) {
         SidebarUI.show(this.id);
       }
     }
