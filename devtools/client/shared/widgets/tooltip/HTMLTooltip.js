d752c863a758961f90020d3d3160ddfc1d6f454d
Ricky Chien <ricky060709@gmail.com>
2017-04-15 15:28:29 +0800

    Bug 1356766 - Support HTMLTooltop when running launchpad on Chrome r=ntim
    MozReview-Commit-ID: KNeN8hrJjdl

--- devtools/client/shared/widgets/tooltip/HTMLTooltip.js
+++ devtools/client/shared/widgets/tooltip/HTMLTooltip.js
@@ -174,6 +174,15 @@ function (anchorRect, viewportRect, width, type, offset, isRtl) {
  * is always the element's ownerDocument).
  */
 const getRelativeRect = function (node, relativeTo) {
+  // getBoxQuads is a non-standard WebAPI which will not work on non-firefox
+  // browser when running launchpad on Chrome.
+  if (!node.getBoxQuads) {
+    let {top, left, width, height} = node.getBoundingClientRect();
+    let right = left + width;
+    let bottom = top + height;
+    return {top, right, bottom, left, width, height};
+  }
+
   // Width and Height can be taken from the rect.
   let {width, height} = node.getBoundingClientRect();
 
