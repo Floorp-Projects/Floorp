ef0f911e919270141999cb8f602d337779f1cdc1
Markus Stange <mstange@themasta.com>
2017-04-16 13:41:32 -0400

    Bug 1346592 - When stopping the profiler, mark all ThreadInfos as not being profiled. r=njn
    This also renames HasProfile() to IsBeingProfiled().
    
    MozReview-Commit-ID: 70RGHNbyZG3

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -269,8 +269,8 @@ private:
 
   // Info on all the registered threads, both live and dead. ThreadIds in
   // mLiveThreads are unique. ThreadIds in mDeadThreads may not be, because
-  // ThreadIds can be reused. HasProfile() is true for all ThreadInfos in
-  // mDeadThreads because we don't hold onto ThreadInfos for non-profiled dead
+  // ThreadIds can be reused. IsBeingProfiled() is true for all ThreadInfos in
+  // mDeadThreads because we don't hold on to ThreadInfos for non-profiled dead
   // threads.
   ThreadVector mLiveThreads;
   ThreadVector mDeadThreads;
@@ -1358,7 +1358,7 @@ locked_profiler_stream_json_for_this_process(PS::LockRef aLock, SpliceableJSONWr
       const PS::ThreadVector& liveThreads = gPS->LiveThreads(aLock);
       for (size_t i = 0; i < liveThreads.size(); i++) {
         ThreadInfo* info = liveThreads.at(i);
-        if (!info->HasProfile()) {
+        if (!info->IsBeingProfiled()) {
           continue;
         }
         info->StreamJSON(gPS->Buffer(aLock), aWriter, gPS->StartTime(aLock),
@@ -1368,7 +1368,7 @@ locked_profiler_stream_json_for_this_process(PS::LockRef aLock, SpliceableJSONWr
       const PS::ThreadVector& deadThreads = gPS->DeadThreads(aLock);
       for (size_t i = 0; i < deadThreads.size(); i++) {
         ThreadInfo* info = deadThreads.at(i);
-        MOZ_ASSERT(info->HasProfile());
+        MOZ_ASSERT(info->IsBeingProfiled());
         info->StreamJSON(gPS->Buffer(aLock), aWriter, gPS->StartTime(aLock),
                          aSinceTime);
       }
@@ -1615,7 +1615,7 @@ SamplerThread::Run()
         for (uint32_t i = 0; i < liveThreads.size(); i++) {
           ThreadInfo* info = liveThreads[i];
 
-          if (!info->HasProfile()) {
+          if (!info->IsBeingProfiled()) {
             // We are not interested in profiling this thread.
             continue;
           }
@@ -1861,10 +1861,9 @@ locked_register_thread(PS::LockRef aLock, const char* aName, void* stackTop)
 
   tlsPseudoStack.set(pseudoStack.get());
 
-  if (ShouldProfileThread(aLock, info)) {
-    info->SetHasProfile();
-
-    if (gPS->IsActive(aLock) && gPS->FeatureJS(aLock)) {
+  if (gPS->IsActive(aLock) && ShouldProfileThread(aLock, info)) {
+    info->StartProfiling();
+    if (gPS->FeatureJS(aLock)) {
       // This startJSSampling() call is on-thread, so we can poll manually to
       // start JS sampling immediately.
       pseudoStack->startJSSampling();
@@ -2352,8 +2351,7 @@ locked_profiler_start(PS::LockRef aLock, int aEntries, double aInterval,
     ThreadInfo* info = liveThreads.at(i);
 
     if (ShouldProfileThread(aLock, info)) {
-      info->SetHasProfile();
-      info->Stack()->reinitializeOnResume();
+      info->StartProfiling();
       if (featureJS) {
         info->Stack()->startJSSampling();
       }
@@ -2479,15 +2477,15 @@ locked_profiler_stop(PS::LockRef aLock)
   }
 #endif
 
-  // Stop JS sampling live threads.
-  if (gPS->FeatureJS(aLock)) {
-    PS::ThreadVector& liveThreads = gPS->LiveThreads(aLock);
-    for (uint32_t i = 0; i < liveThreads.size(); i++) {
-      ThreadInfo* info = liveThreads.at(i);
-      if (ShouldProfileThread(aLock, info)) {
-        MOZ_RELEASE_ASSERT(info->HasProfile());
+  // Stop sampling live threads.
+  PS::ThreadVector& liveThreads = gPS->LiveThreads(aLock);
+  for (uint32_t i = 0; i < liveThreads.size(); i++) {
+    ThreadInfo* info = liveThreads.at(i);
+    if (info->IsBeingProfiled()) {
+      if (gPS->FeatureJS(aLock)) {
         info->Stack()->stopJSSampling();
       }
+      info->StopProfiling();
     }
   }
 
@@ -2499,8 +2497,9 @@ locked_profiler_stop(PS::LockRef aLock)
   }
 
   if (gPS->FeatureJS(aLock)) {
-    // We just called stopJSSampling() on all relevant threads. We can also
-    // manually poll the current thread so it stops profiling immediately.
+    // We just called stopJSSampling() (through ThreadInfo::StopProfiling) on
+    // all relevant threads. We can also manually poll the current thread so
+    // it stops profiling immediately.
     if (PseudoStack* stack = tlsPseudoStack.get()) {
       stack->pollJSSampling();
     }
@@ -2712,7 +2711,7 @@ profiler_unregister_thread()
   ThreadInfo* info = FindLiveThreadInfo(lock, &i);
   if (info) {
     DEBUG_LOG("profiler_unregister_thread: %s", info->Name());
-    if (gPS->IsActive(lock) && info->HasProfile()) {
+    if (gPS->IsActive(lock) && info->IsBeingProfiled()) {
       gPS->DeadThreads(lock).push_back(info);
     } else {
       delete info;
@@ -3043,7 +3042,7 @@ profiler_clear_js_context()
     // Flush this thread's ThreadInfo, if it is being profiled.
     ThreadInfo* info = FindLiveThreadInfo(lock);
     MOZ_RELEASE_ASSERT(info);
-    if (info->HasProfile()) {
+    if (info->IsBeingProfiled()) {
       info->FlushSamplesAndMarkers(gPS->Buffer(lock), gPS->StartTime(lock));
     }
 
