ac9e05d8720925ba8a48b9bf66eca34d80903df2
Shawn Huang <shuang@mozilla.com>
2017-04-17 15:06:39 +0800

    Bug 1286717 - Part 2: Add mochitests for persist/persisted functions, r=janv

---
+++ dom/quota/test/unit/test_storage_manager_persist_deny.js
@@ -0,0 +1,24 @@
+var disableWorkerTest = "Persist doesn't work in workers";
+var testGenerator = testSteps();
+
+function* testSteps()
+{
+  SpecialPowers.pushPrefEnv({
+    "set": [["dom.storageManager.enabled", true],
+            ["dom.storageManager.prompt.testing", true],
+            ["dom.storageManager.prompt.testing.allow", false]]
+  }, continueToNextStep);
+  yield undefined;
+
+  navigator.storage.persist().then(grabArgAndContinueHandler);
+  let persistResult = yield undefined;
+
+  is(persistResult, false, "Cancel the persist prompt and resolve a promise with false");
+
+  navigator.storage.persisted().then(grabArgAndContinueHandler);
+  let persistedResult = yield undefined;
+
+  is(persistResult, persistedResult, "Persist/persisted results are consistent");
+
+  finishTest();
+}
