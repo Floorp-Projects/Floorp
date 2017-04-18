392d4c4a1a9a8c0388fd59c6f6dbcfe28bb9f46a
Sylvestre Ledru <sledru@mozilla.com>
2017-04-18 11:59:55 +0200

    Bug 1354821 - Generate fennec nightly apk with org.mozilla.fennec_aurora as id r=rail
    * nightly-aurora-id is a copy of nightly
    * mobile/android/branding/aurora/ is a copy of nightly with the id and package name changed
    
    MozReview-Commit-ID: 2VT0dHDXEMg
    
    --HG--
    extra : rebase_source : 7396a1a7eabb4037fb6936bfc1af10666a677e14

--- mobile/android/branding/aurora/configure.sh
+++ mobile/android/branding/aurora/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME="Firefox Aurora"
+MOZ_APP_DISPLAYNAME="Firefox Nightly"
 ANDROID_PACKAGE_NAME=org.mozilla.fennec_aurora
 MOZ_UPDATER=1
 MOZ_ANDROID_ANR_REPORTER=1
