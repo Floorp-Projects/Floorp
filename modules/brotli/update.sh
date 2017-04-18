8c750da529c1497819b0c7c786a8e0673472766e
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-04-19 00:03:35 +0200

    Backed out changeset 820a15b7a5dd (bug 1355671) for Windows static failures. r=backout

--- modules/brotli/update.sh
+++ modules/brotli/update.sh
@@ -6,19 +6,14 @@
 MY_TEMP_DIR=`mktemp -d -t brotli_update.XXXXXX` || exit 1
 
 git clone https://github.com/google/brotli ${MY_TEMP_DIR}/brotli
-git -C ${MY_TEMP_DIR}/brotli checkout v0.4.0
 
-COMMIT=$(git -C ${MY_TEMP_DIR}/brotli rev-parse HEAD)
-perl -p -i -e "s/\[commit [0-9a-f]{40}\]/[commit ${COMMIT}]/" README.mozilla;
+COMMIT=`(cd ${MY_TEMP_DIR}/brotli && git log | head -n 1)`
+perl -p -i -e "s/\[commit [0-9a-f]{40}\]/[${COMMIT}]/" README.mozilla;
 
-DIRS="dec enc tools"
-
-for d in $DIRS; do
-	rm -rf $d
-	mv ${MY_TEMP_DIR}/brotli/$d $d
-done
+rm -rf dec
+mv ${MY_TEMP_DIR}/brotli/dec dec
 rm -rf ${MY_TEMP_DIR}
-hg addremove $DIRS
+hg add dec
 
 echo "###"
 echo "### Updated brotli/dec to $COMMIT."
