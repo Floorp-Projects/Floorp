2cf17a6f913c8a45bf906453bb9f1d59829b1a42
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-04-18 12:15:56 +0900

    Bug 1210796 - Part 14: Add close button. r=pbro
    MozReview-Commit-ID: 59NdVAtgeLw
    
    --HG--
    extra : rebase_source : f8c4ecf6ff9d1fed99f8c07aca6365590aa5e90e

--- devtools/client/animationinspector/test/browser_animation_detail_displayed.js
+++ devtools/client/animationinspector/test/browser_animation_detail_displayed.js
@@ -10,6 +10,7 @@
 // 2. Hidden at first if multiple animations were displayed.
 // 3. Display after click on an animation.
 // 4. Display from first time if displayed animation is only one.
+// 5. Close the animation-detail element by clicking on close button.
 
 requestLongerTimeout(5);
 
@@ -19,23 +20,41 @@ add_task(function* () {
   const timelineComponent = panel.animationsTimelineComponent;
   const animationDetailEl =
     timelineComponent.rootWrapperEl.querySelector(".animation-detail");
+  const splitboxControlledEl =
+    timelineComponent.rootWrapperEl.querySelector(".controlled");
 
   // 1. Existance of animation-detail element.
   ok(animationDetailEl, "The animation-detail element should exist");
 
   // 2. Hidden at first if multiple animations were displayed.
   const win = animationDetailEl.ownerDocument.defaultView;
-  is(win.getComputedStyle(animationDetailEl).display, "none",
+  is(win.getComputedStyle(splitboxControlledEl).display, "none",
      "The animation-detail element should be hidden at first "
      + "if multiple animations were displayed");
 
   // 3. Display after click on an animation.
   yield clickOnAnimation(panel, 0);
-  isnot(win.getComputedStyle(animationDetailEl).display, "none",
+  isnot(win.getComputedStyle(splitboxControlledEl).display, "none",
         "The animation-detail element should be displayed after clicked on an animation");
 
   // 4. Display from first time if displayed animation is only one.
   yield selectNodeAndWaitForAnimations("#target1", inspector);
   ok(animationDetailEl.querySelector(".property"),
      "The property in animation-detail element should be displayed");
+
+  // 5. Close the animation-detail element by clicking on close button.
+  const previousHeight = animationDetailEl.offsetHeight;
+  const button = animationDetailEl.querySelector(".animation-detail-header button");
+  const onclosed = timelineComponent.once("animation-detail-closed");
+  EventUtils.sendMouseEvent({type: "click"}, button, win);
+  yield onclosed;
+  is(win.getComputedStyle(splitboxControlledEl).display, "none",
+     "animation-detail element should not display");
+
+  // Select another animation.
+  yield selectNodeAndWaitForAnimations("#target2", inspector);
+  isnot(win.getComputedStyle(splitboxControlledEl).display, "none",
+        "animation-detail element should display");
+  is(animationDetailEl.offsetHeight, previousHeight,
+     "The height of animation-detail should keep the height");
 });
