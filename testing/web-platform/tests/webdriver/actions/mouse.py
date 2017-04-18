4beddafd8d3b40f13200845441bf6c88bcbda040
Maja Frydrychowicz <mjzffr@gmail.com>
2017-04-18 19:07:04 -0400

    Bug 1357590 - Test drag and drop with mouse actions; r=ato
    MozReview-Commit-ID: 6vRHj7pRGdh
    
    --HG--
    extra : rebase_source : e10220c78136861645bbf78aa3d00dbb33d2f564

--- testing/web-platform/tests/webdriver/actions/mouse.py
+++ testing/web-platform/tests/webdriver/actions/mouse.py
@@ -1,3 +1,4 @@
+import pytest
 import urllib
 
 from support.refine import get_events, filter_dict
@@ -11,6 +12,18 @@ def inline(doc):
 def link_doc(dest):
     content = "<a href=\"{}\" id=\"link\">destination</a>".format(dest)
     return inline(content)
+
+
+def get_center(rect):
+    return {
+        "x": rect["width"] / 2 + rect["x"],
+        "y": rect["height"] / 2 + rect["y"],
+    }
+
+
+# TODO use pytest.approx once we upgrade to pytest > 3.0
+def approx(n, m, tolerance=1):
+    return abs(n - m) < tolerance
 
 
 def test_click_at_coordinates(session, test_actions_page, mouse_chain):
@@ -43,9 +56,7 @@ def test_click_at_coordinates(session, test_actions_page, mouse_chain):
 
 def test_click_element_center(session, test_actions_page, mouse_chain):
     outer = session.find.css("#outer", all=False)
-    outer_rect = outer.rect
-    center_x = outer_rect["width"] / 2 + outer_rect["x"]
-    center_y = outer_rect["height"] / 2 + outer_rect["y"]
+    center = get_center(outer.rect)
     mouse_chain.click(element=outer).perform()
     events = get_events(session)
     assert len(events) == 4
@@ -53,9 +64,8 @@ def test_click_element_center(session, test_actions_page, mouse_chain):
     assert ["mousemove", "mousedown", "mouseup", "click"] == event_types
     for e in events:
         if e["type"] != "mousemove":
-            # TODO use pytest.approx once we upgrade to pytest > 3.0
-            assert abs(e["pageX"] - center_x) < 1
-            assert abs(e["pageY"] - center_y) < 1
+            assert approx(e["pageX"], center["x"])
+            assert approx(e["pageY"], center["y"])
             assert e["target"] == "outer"
 
 
@@ -75,3 +85,29 @@ def test_click_navigation(session, url):
     session.url = start
     click(session.find.css("#link", all=False))
     assert session.url == destination
+
+
+@pytest.mark.parametrize("drag_duration", [0, 300, 800])
+@pytest.mark.parametrize("dx, dy", [(20, 0), (0, 15), (10, 15)])
+def test_drag_and_drop(session, test_actions_page, mouse_chain, dx, dy, drag_duration):
+    drag_target = session.find.css("#dragTarget", all=False)
+    initial_rect = drag_target.rect
+    initial_center = get_center(initial_rect)
+    # Conclude chain with extra move to allow time for last queued
+    # coordinate-update of drag_target and to test that drag_target is "dropped".
+    mouse_chain \
+        .pointer_move(0, 0, origin=drag_target) \
+        .pointer_down() \
+        .pointer_move(dx, dy, duration=drag_duration, origin="pointer") \
+        .pointer_up() \
+        .pointer_move(80, 50, duration=100, origin="pointer") \
+        .perform()
+    # mouseup that ends the drag is at the expected destination
+    e = get_events(session)[1]
+    assert e["type"] == "mouseup"
+    assert approx(e["pageX"], initial_center["x"] + dx)
+    assert approx(e["pageY"], initial_center["y"] + dy)
+    # check resulting location of the dragged element
+    final_rect = drag_target.rect
+    assert initial_rect["x"] + dx == final_rect["x"]
+    assert initial_rect["y"] + dy == final_rect["y"]
