e1121dae1a987263d8ad976b839514cdbaf6a32f
Kartikaya Gupta <kgupta@mozilla.com>
2017-04-20 10:38:04 -0400

    Bug 1357754 - Extract a template function from UpdateHitTestingTree. r=botond
    Currently the public UpdateHitTestingTree function takes a Layer* which
    assumes there is a layer tree to traverse. However, with WebRender, that
    is not the case. This patch has two conceptual changes.
    
    The first change is to extract the innards of UpdateHitTestingTree to take
    a LayerMetricsWrapper instead of a Layer* and traverse the layer tree
    using that. This was already mostly happening but this makes the
    Layer*/LayerMetricsWrapper boundary a little more explicit.
    
    The second change is to make the extracted helper function templated, so
    it accepts anything that is template-compatible with
    LayerMetricsWrapper. I chose to use a template rather inheritance to
    avoid the performance hit of virtual functions, since this code runs
    relatively often.
    
    This paves the way for adding a different kind of tree-traversal object
    instead of the LayerMetricsWrapper while reusing the same logic to build
    the hit-testing tree and APZC nodes.
    
    MozReview-Commit-ID: 6SmnX6Bn2QV

--- gfx/layers/apz/src/APZCTreeManager.h
+++ gfx/layers/apz/src/APZCTreeManager.h
@@ -436,6 +436,13 @@ private:
   typedef bool (*GuidComparator)(const ScrollableLayerGuid&, const ScrollableLayerGuid&);
 
   /* Helpers */
+  template<class ScrollNode>
+  void UpdateHitTestingTreeImpl(uint64_t aRootLayerTreeId,
+                                const ScrollNode& aRoot,
+                                bool aIsFirstPaint,
+                                uint64_t aOriginatingLayersId,
+                                uint32_t aPaintSequenceNumber);
+
   void AttachNodeToTree(HitTestingTreeNode* aNode,
                         HitTestingTreeNode* aParent,
                         HitTestingTreeNode* aNextSibling);
@@ -466,7 +473,8 @@ private:
   already_AddRefed<HitTestingTreeNode> RecycleOrCreateNode(TreeBuildingState& aState,
                                                            AsyncPanZoomController* aApzc,
                                                            uint64_t aLayersId);
-  HitTestingTreeNode* PrepareNodeForLayer(const LayerMetricsWrapper& aLayer,
+  template<class ScrollNode>
+  HitTestingTreeNode* PrepareNodeForLayer(const ScrollNode& aLayer,
                                           const FrameMetrics& aMetrics,
                                           uint64_t aLayersId,
                                           const gfx::Matrix4x4& aAncestorTransform,
@@ -474,7 +482,8 @@ private:
                                           HitTestingTreeNode* aNextSibling,
                                           TreeBuildingState& aState);
 
-  void PrintAPZCInfo(const LayerMetricsWrapper& aLayer,
+  template<class ScrollNode>
+  void PrintAPZCInfo(const ScrollNode& aLayer,
                      const AsyncPanZoomController* apzc);
 
   void NotifyScrollbarDragRejected(const ScrollableLayerGuid& aGuid) const;
