1a070f322efb4eed467d5281dad5001f763261ff
Daosheng Mu <daoshengmu@gmail.com>
2017-04-20 21:11:17 +0800

    Bug 1358010 - Skip vr mochitests when running on release and non-Windows platforms; r=kip
    MozReview-Commit-ID: 7TeTXyIfrh

--- dom/vr/test/runVRTest.js
+++ dom/vr/test/runVRTest.js
@@ -1,6 +1,5 @@
 function runVRTest(callback) {
-  SpecialPowers.pushPrefEnv({"set" : [["dom.vr.enabled", true],
-                                      ["dom.vr.puppet.enabled", true],
+  SpecialPowers.pushPrefEnv({"set" : [["dom.vr.puppet.enabled", true],
                                       ["dom.vr.require-gesture", false],
                                       ["dom.vr.test.enabled", true]]},
   () => {
