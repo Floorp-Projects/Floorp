2d731ded3df866bcaf167c56f1dbf9208f9b8686
Shu-yu Guo <shu@rfrn.org>
2017-04-20 17:48:57 -0700

    Bug 1357483 - Stop changing the end position of parenthesized expression ParseNodes. (r=Waldo)

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -9925,7 +9925,6 @@ Parser<ParseHandler>::primaryExpr(YieldHandling yieldHandling, TripledotHandling
         if (!expr)
             return null();
         MUST_MATCH_TOKEN(TOK_RP, JSMSG_PAREN_IN_PAREN);
-        handler.setEndPosition(expr, pos().end);
         return handler.parenthesize(expr);
       }
 
