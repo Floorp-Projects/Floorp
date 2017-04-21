2d124478915b3527397885e751930bbbe9c22974
Georg Fritzsche <georg.fritzsche@googlemail.com>
2017-04-21 15:48:44 +0200

    Bug 1357001 - Part 5 - Fix linting errors. r=chutten

--- toolkit/components/telemetry/parse_scalars.py
+++ toolkit/components/telemetry/parse_scalars.py
@@ -55,8 +55,8 @@ class ScalarType:
         for n in [group_name, probe_name]:
             if len(n) > MAX_NAME_LENGTH:
                 raise ParserError(("Name '{}' exceeds maximum name length of {} characters.\n"
-                                  "See: {}#the-yaml-definition-file")
-                                 .format(n, MAX_NAME_LENGTH, BASE_DOC_URL))
+                                   "See: {}#the-yaml-definition-file")
+                                  .format(n, MAX_NAME_LENGTH, BASE_DOC_URL))
 
         def check_name(name, error_msg_prefix, allowed_char_regexp):
             # Check if we only have the allowed characters.
@@ -68,8 +68,8 @@ class ScalarType:
             # Don't allow leading/trailing digits, '.' or '_'.
             if re.search(r'(^[\d\._])|([\d\._])$', name):
                 raise ParserError((error_msg_prefix + " name must not have a leading/trailing "
-                                  "digit, a dot or underscore. Got: '{}'.\n"
-                                  " See: {}#the-yaml-definition-file").format(name, BASE_DOC_URL))
+                                   "digit, a dot or underscore. Got: '{}'.\n"
+                                   " See: {}#the-yaml-definition-file").format(name, BASE_DOC_URL))
 
         check_name(group_name, 'Group', r'\.')
         check_name(probe_name, 'Probe', r'_')
@@ -115,20 +115,20 @@ class ScalarType:
         missing_fields = [f for f in REQUIRED_FIELDS.keys() if f not in definition]
         if len(missing_fields) > 0:
             raise ParserError(self._name + ' - missing required fields: ' + ', '.join(missing_fields) +
-                           '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
+                              '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
 
         # Do we have any unknown field?
         unknown_fields = [f for f in definition.keys() if f not in ALL_FIELDS]
         if len(unknown_fields) > 0:
             raise ParserError(self._name + ' - unknown fields: ' + ', '.join(unknown_fields) +
-                           '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
+                              '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
 
         # Checks the type for all the fields.
         wrong_type_names = ['{} must be {}'.format(f, ALL_FIELDS[f].__name__)
                             for f in definition.keys() if not isinstance(definition[f], ALL_FIELDS[f])]
         if len(wrong_type_names) > 0:
             raise ParserError(self._name + ' - ' + ', '.join(wrong_type_names) +
-                            '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
+                              '.\nSee: {}#required-fields'.format(BASE_DOC_URL))
 
         # Check that the lists are not empty and that data in the lists
         # have the correct types.
@@ -137,16 +137,16 @@ class ScalarType:
             # Check for empty lists.
             if len(definition[field]) == 0:
                 raise ParserError(("Field '{}' for probe '{}' must not be empty" +
-                                 ".\nSee: {}#required-fields)")
-                                .format(field, self._name, BASE_DOC_URL))
+                                   ".\nSee: {}#required-fields)")
+                                  .format(field, self._name, BASE_DOC_URL))
             # Check the type of the list content.
             broken_types =\
                 [not isinstance(v, LIST_FIELDS_CONTENT[field]) for v in definition[field]]
             if any(broken_types):
                 raise ParserError(("Field '{}' for probe '{}' must only contain values of type {}"
-                                 ".\nSee: {}#the-yaml-definition-file)")
-                                .format(field, self._name, LIST_FIELDS_CONTENT[field].__name__,
-                                        BASE_DOC_URL))
+                                   ".\nSee: {}#the-yaml-definition-file)")
+                                  .format(field, self._name, LIST_FIELDS_CONTENT[field].__name__,
+                                          BASE_DOC_URL))
 
     def validate_values(self, definition):
         """This function checks that the fields have the correct values.
@@ -159,26 +159,26 @@ class ScalarType:
         scalar_kind = definition.get('kind')
         if scalar_kind not in SCALAR_TYPES_MAP.keys():
             raise ParserError(self._name + ' - unknown scalar kind: ' + scalar_kind +
-                             '.\nSee: {}'.format(BASE_DOC_URL))
+                              '.\nSee: {}'.format(BASE_DOC_URL))
 
         # Validate the collection policy.
         collection_policy = definition.get('release_channel_collection', None)
         if collection_policy and collection_policy not in ['opt-in', 'opt-out']:
             raise ParserError(self._name + ' - unknown collection policy: ' + collection_policy +
-                             '.\nSee: {}#optional-fields'.format(BASE_DOC_URL))
+                              '.\nSee: {}#optional-fields'.format(BASE_DOC_URL))
 
         # Validate the cpp_guard.
         cpp_guard = definition.get('cpp_guard')
         if cpp_guard and re.match(r'\W', cpp_guard):
             raise ParserError(self._name + ' - invalid cpp_guard: ' + cpp_guard +
-                             '.\nSee: {}#optional-fields'.format(BASE_DOC_URL))
+                              '.\nSee: {}#optional-fields'.format(BASE_DOC_URL))
 
         # Validate record_in_processes.
         record_in_processes = definition.get('record_in_processes', [])
         for proc in record_in_processes:
             if not utils.is_valid_process_name(proc):
                 raise ParserError(self._name + ' - unknown value in record_in_processes: ' + proc +
-                                 '.\nSee: {}'.format(BASE_DOC_URL))
+                                  '.\nSee: {}'.format(BASE_DOC_URL))
 
     @property
     def name(self):
