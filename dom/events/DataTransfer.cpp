9960823f40e9fe529e36879b0e43d199f98d2f84
Michael Layzell <michael@thelayzells.com>
2017-04-21 16:38:12 -0400

    Bug 1352852 - Add dummy data to the DataTransfer when dragging a draggable object, r=enndeakin
    When dragging a `draggable=true` HTML DOM node, if no data is added to the
    DataTransfer during the DragStart event, we currently cancel the drag. This is
    inconsistent with Chrome's behaviour.
    
    This patch adds a chrome-only (hidden from content) item to the DataTransfer:
    application/x-moz-dummy-data. This data is added only when no other data has
    been added to the DataTransfer, and the target of the dragstart event was a
    draggable=true HTML DOM node.
    
    This hidden node allows for the drag event to complete successfully, while
    appearing the same as Chrome's behavior to content scripts.
    
    MozReview-Commit-ID: HVqEr7aR6DD

--- dom/events/DataTransfer.cpp
+++ dom/events/DataTransfer.cpp
@@ -38,6 +38,7 @@
 #include "mozilla/dom/BindingUtils.h"
 #include "mozilla/dom/OSFileSystem.h"
 #include "mozilla/dom/Promise.h"
+#include "mozilla/storage/Variant.h"
 #include "nsNetUtil.h"
 
 namespace mozilla {
@@ -1571,6 +1572,29 @@ DataTransfer::FillInExternalCustomTypes(nsIVariant* aData, uint32_t aIndex,
       SetDataWithPrincipal(format, variant, aIndex, aPrincipal);
     }
   } while (type != eCustomClipboardTypeId_None);
+}
+
+void
+DataTransfer::AddDummyHiddenData()
+{
+  RefPtr<nsVariantCC> variant = new nsVariantCC();
+  variant->SetAsAString(EmptyString());
+
+  ErrorResult rv;
+  RefPtr<DataTransferItem> item =
+    mItems->SetDataWithPrincipal(NS_LITERAL_STRING(kDummyTypeMime),
+                                 variant,
+                                 /* aIndex = */ 0,
+                                 nsContentUtils::GetSystemPrincipal(),
+                                 /* aInsertOnly = */ true,
+                                 /* aHidden = */ true,
+                                 rv);
+  MOZ_ASSERT(item->ChromeOnly());
+
+  // We might have gotten a NS_ERROR_DOM_NOT_SUPPORTED_ERROR because we've
+  // already added an application/x-moz-dummy-data. We want to ignore that
+  // error.
+  rv.SuppressException();
 }
 
 } // namespace dom
