3031c953ada6b133d17fc95f045f3a7b497ad712
Sean Lee <selee@mozilla.com>
2017-04-21 15:20:26 +0800

    Bug 1349489 - Part 2: Add test fixtures for autofill field name heuristics.; r=MattN
    MozReview-Commit-ID: IuvSEpSHVtK

---
+++ browser/extensions/formautofill/test/unit/heuristics/test_basic.js
@@ -0,0 +1,22 @@
+/* global runHeuristicsTest */
+
+"use strict";
+
+runHeuristicsTest([
+  {
+    fixturePath: "autocomplete_basic.html",
+    expectedResult: [
+      [
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "organization"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "street-address"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "address-level2"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "address-level1"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "postal-code"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "country"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "tel"},
+        {"section": "", "addressType": "", "contactType": "", "fieldName": "email"},
+      ],
+    ],
+  },
+], "../../fixtures/");
+
