de1f4c78e1278981c8e8aad6af1ce896568acbfc
Alessio Placitelli <alessio.placitelli@gmail.com>
2017-04-24 16:32:14 +0200

    Bug 1346223 - Remove SessionRecorder.jsm. r=gfritzsche
    This patch also moves the activeTicks logic to TelemetrySession.jsm
    along with the related test coverage.
    
    MozReview-Commit-ID: 8vXffqo2V85
    
    --HG--
    extra : rebase_source : f681b06b48a56e2890af98fd3a1b2dc21a44a77c

---
+++ toolkit/components/telemetry/tests/unit/test_TelemetrySession_activeTicks.js
@@ -0,0 +1,49 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/
+*/
+
+Cu.import("resource://gre/modules/TelemetryController.jsm", this);
+Cu.import("resource://gre/modules/TelemetrySession.jsm", this);
+
+
+add_task(function* test_setup() {
+  // Addon manager needs a profile directory
+  do_get_profile();
+  loadAddonManager("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+  // Make sure we don't generate unexpected pings due to pref changes.
+  yield setEmptyPrefWatchlist();
+
+  Services.prefs.setBoolPref(PREF_TELEMETRY_ENABLED, true);
+});
+
+add_task(function* test_record_activeTicks() {
+  yield TelemetryController.testSetup();
+
+  let checkActiveTicks = (expected) => {
+    let payload = TelemetrySession.getPayload();
+    Assert.equal(payload.simpleMeasurements.activeTicks, expected,
+                 "TelemetrySession must record the expected number of active ticks.");
+  };
+
+  for (let i = 0; i < 3; i++) {
+    Services.obs.notifyObservers(null, "user-interaction-active");
+  }
+  checkActiveTicks(3);
+
+  // Now send inactive. This must not increment the active ticks.
+  Services.obs.notifyObservers(null, "user-interaction-inactive");
+  checkActiveTicks(3);
+
+  // If we send active again, this should be counted as inactive.
+  Services.obs.notifyObservers(null, "user-interaction-active");
+  checkActiveTicks(3);
+
+  // If we send active again, this should be counted as active.
+  Services.obs.notifyObservers(null, "user-interaction-active");
+  checkActiveTicks(4);
+
+  Services.obs.notifyObservers(null, "user-interaction-active");
+  checkActiveTicks(5);
+
+  yield TelemetryController.testShutdown();
+});
