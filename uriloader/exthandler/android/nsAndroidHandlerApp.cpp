f570b48f7bd4a005933021c93908f2de1abb983d
Paolo Amadini <paolo.mozmail@amadzone.org>
2017-04-24 10:28:12 +0100

    Bug 986975 - do_QueryInterface abuse in nsAndroidHandlerApp.cpp. r=mak
    This change is tested in detail as part of bug 1355585.
    
    MozReview-Commit-ID: 5z4evaUQDFI

--- uriloader/exthandler/android/nsAndroidHandlerApp.cpp
+++ uriloader/exthandler/android/nsAndroidHandlerApp.cpp
@@ -55,19 +55,20 @@ nsAndroidHandlerApp::SetDetailedDescription(const nsAString & aDescription)
   return NS_OK;
 }
 
-// XXX Workaround for bug 986975 to maintain the existing broken semantics
-template<>
-struct nsISharingHandlerApp::COMTypeInfo<nsAndroidHandlerApp, void> {
-  static const nsIID kIID;
-};
-const nsIID nsISharingHandlerApp::COMTypeInfo<nsAndroidHandlerApp, void>::kIID = NS_IHANDLERAPP_IID;
-
 NS_IMETHODIMP
 nsAndroidHandlerApp::Equals(nsIHandlerApp *aHandlerApp, bool *aRetval)
 {
-  nsCOMPtr<nsAndroidHandlerApp> aApp = do_QueryInterface(aHandlerApp);
-  *aRetval = aApp && aApp->mName.Equals(mName) &&
-    aApp->mDescription.Equals(mDescription);
+  *aRetval = false;
+  if (!aHandlerApp) {
+    return NS_OK;
+  }
+
+  nsAutoString name;
+  nsAutoString detailedDescription;
+  aHandlerApp->GetName(name);
+  aHandlerApp->GetDetailedDescription(detailedDescription);
+
+  *aRetval = name.Equals(mName) && detailedDescription.Equals(mDescription);
   return NS_OK;
 }
 
