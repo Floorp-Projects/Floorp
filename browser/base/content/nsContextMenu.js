01e762cd0a4d0c22f6df76d58cc9c944c30ad919
Luca Greco <lgreco@mozilla.com>
2017-04-24 16:22:31 +0200

    Bug 1358314 - Fix 'Open Link in New Tab' context menu item after tab navigated in non-e10s mode. r=kmag
    MozReview-Commit-ID: KIG3QTx4J6b

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1050,8 +1050,11 @@ nsContextMenu.prototype = {
     }
 
     if (!this.isRemote) {
-      params.frameOuterWindowID = WebNavigationFrames.getFrameId(this.target.ownerGlobal);
+      // Propagate the frameOuterWindowID value saved when
+      // the context menu has been opened.
+      params.frameOuterWindowID = this.frameOuterWindowID;
     }
+
     // If we want to change userContextId, we must be sure that we don't
     // propagate the referrer.
     if ("userContextId" in params &&
