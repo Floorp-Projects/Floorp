7e85440bb7c8ba66496a4b3162a80edfcb94a2c5
Sebastian Kaspari <s.kaspari@gmail.com>
2017-04-24 15:11:10 +0200

    [focus] Add text selection action for launching private search (from third-party apps). (https://github.com/mozilla-mobile/focus-android/issues/506)

---
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/TextActionActivity.java
@@ -0,0 +1,38 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+
+import org.mozilla.focus.utils.UrlUtils;
+
+/**
+ * Activity for receiving and processing an ACTION_PROCESS_TEXT intent.
+ */
+public class TextActionActivity extends Activity {
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final String searchText = getIntent().getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString();
+        final String searchUrl = UrlUtils.createSearchUrl(this, searchText);
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(searchUrl));
+
+        startActivity(intent);
+
+        finish();
+    }
+}
