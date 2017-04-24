e0d317e3763f3fd521dde1ec002837461622a85d
Sean Lee <selee@mozilla.com>
2017-04-24 22:30:37 +0800

    Bug 1349490 - Part 1: Implement FormAutofillUtil.extractLabelStrings function.; r=MattN
    MozReview-Commit-ID: 9kCZkgnqOfF
    
    --HG--
    extra : rebase_source : 03290f57596568fc0b720c6a6dd45142cb91431c

--- browser/extensions/formautofill/FormAutofillUtils.jsm
+++ browser/extensions/formautofill/FormAutofillUtils.jsm
@@ -62,6 +62,48 @@ this.FormAutofillUtils = {
     });
   },
 
+  // The tag name list is from Chromium except for "STYLE":
+  // eslint-disable-next-line max-len
+  // https://cs.chromium.org/chromium/src/components/autofill/content/renderer/form_autofill_util.cc?l=216&rcl=d33a171b7c308a64dc3372fac3da2179c63b419e
+  EXCLUDED_TAGS: ["SCRIPT", "NOSCRIPT", "OPTION", "STYLE"],
+  /**
+   * Extract all strings of an element's children to an array.
+   * "element.textContent" is a string which is merged of all children nodes,
+   * and this function provides an array of the strings contains in an element.
+   *
+   * @param  {Object} element
+   *         A DOM element to be extracted.
+   * @returns {Array}
+   *          All strings in an element.
+   */
+  extractLabelStrings(element) {
+    let strings = [];
+    let _extractLabelStrings = (el) => {
+      if (this.EXCLUDED_TAGS.includes(el.tagName)) {
+        return;
+      }
+
+      if (el.nodeType == Ci.nsIDOMNode.TEXT_NODE ||
+          el.childNodes.length == 0) {
+        let trimmedText = el.textContent.trim();
+        if (trimmedText) {
+          strings.push(trimmedText);
+        }
+        return;
+      }
+
+      for (let node of el.childNodes) {
+        if (node.nodeType != Ci.nsIDOMNode.ELEMENT_NODE &&
+            node.nodeType != Ci.nsIDOMNode.TEXT_NODE) {
+          continue;
+        }
+        _extractLabelStrings(node);
+      }
+    };
+    _extractLabelStrings(element);
+    return strings;
+  },
+
   findLabelElements(element) {
     let document = element.ownerDocument;
     let labels = [];
---
+++ browser/extensions/formautofill/test/unit/test_extractLabelStrings.js
@@ -0,0 +1,66 @@
+"use strict";
+
+Cu.import("resource://formautofill/FormAutofillUtils.jsm");
+
+const TESTCASES = [
+  {
+    description: "A label element contains one input element.",
+    document: `<label id="typeA"> label type A
+                 <!-- This comment should not be extracted. -->
+                 <input type="text">
+                 <script>FOO</script>
+                 <noscript>FOO</noscript>
+                 <option>FOO</option>
+                 <style>FOO</style>
+               </label>`,
+    inputId: "typeA",
+    expectedStrings: ["label type A"],
+  },
+  {
+    description: "A label element with inner div contains one input element.",
+    document: `<label id="typeB"> label type B
+                 <!-- This comment should not be extracted. -->
+                 <script>FOO</script>
+                 <noscript>FOO</noscript>
+                 <option>FOO</option>
+                 <style>FOO</style>
+                 <div> inner div
+                   <input type="text">
+                 </div>
+               </label>`,
+    inputId: "typeB",
+    expectedStrings: ["label type B", "inner div"],
+  },
+  {
+    description: "A label element with inner prefix/postfix strings contains span elements.",
+    document: `<label id="typeC"> label type C
+                 <!-- This comment should not be extracted. -->
+                 <script>FOO</script>
+                 <noscript>FOO</noscript>
+                 <option>FOO</option>
+                 <style>FOO</style>
+                 <div> inner div prefix
+                   <span>test C-1 </span>
+                   <span> test C-2</span>
+                  inner div postfix
+                 </div>
+               </label>`,
+    inputId: "typeC",
+    expectedStrings: ["label type C", "inner div prefix", "test C-1",
+      "test C-2", "inner div postfix"],
+  },
+];
+
+TESTCASES.forEach(testcase => {
+  add_task(function* () {
+    do_print("Starting testcase: " + testcase.description);
+
+    let doc = MockDocument.createTestDocument(
+      "http://localhost:8080/test/", testcase.document);
+
+    let element = doc.getElementById(testcase.inputId);
+    let strings = FormAutofillUtils.extractLabelStrings(element);
+
+    Assert.deepEqual(strings, testcase.expectedStrings);
+  });
+});
