17f9aa454f06cab695ae5fcec89407a7ad30b6a5
Ralph Giles <giles@mozilla.com>
2017-04-24 15:05:01 -0700

    Bug 1358662 - Implement keyframe and framesize VPXDecoder helpers. r=jya
    Encapsulate code from WebMDemuxer to query keyframe and frame
    resolution inside VPXDecoder, so we have a clean wrapper for
    all the libvpx functions we use.
    
    MozReview-Commit-ID: ASRRhNl0A41

--- dom/media/platforms/agnostic/VPXDecoder.h
+++ dom/media/platforms/agnostic/VPXDecoder.h
@@ -7,6 +7,7 @@
 #define VPXDecoder_h_
 
 #include "PlatformDecoderModule.h"
+#include "mozilla/Span.h"
 
 #include <stdint.h>
 #define VPX_DONT_DEFINE_STDINT_TYPES
@@ -44,6 +45,12 @@ public:
   static bool IsVPX(const nsACString& aMimeType, uint8_t aCodecMask=VP8|VP9);
   static bool IsVP8(const nsACString& aMimeType);
   static bool IsVP9(const nsACString& aMimeType);
+
+  // Return true if a sample is a keyframe for the specified codec.
+  static bool IsKeyframe(Span<const uint8_t> aBuffer, Codec aCodec);
+
+  // Return the frame dimensions for a sample for the specified codec.
+  static nsIntSize GetFrameSize(Span<const uint8_t> aBuffer, Codec aCodec);
 
 private:
   ~VPXDecoder();
