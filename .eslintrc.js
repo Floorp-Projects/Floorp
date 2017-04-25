4b160a3723502ef3bd55ebe36d54d4c34db485a2
Mark Banner <standard8@mozilla.com>
2017-04-25 20:12:21 +0100

    Bug 1359011 - Make the mozilla/recommended eslint configuration the default for the whole tree. r=mossop
    MozReview-Commit-ID: HtUW43tCli1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,19 @@
 "use strict";
 
 module.exports = {
-  // When adding items to this file please check for effects on sub-directories.
+  // New rules and configurations should generally be added in
+  // tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js to
+  // allow external repositories that use the plugin to pick them up as well.
+  "extends": [
+    "plugin:mozilla/recommended"
+  ],
   "plugins": [
     "mozilla"
   ],
-  "rules": {
-    "mozilla/avoid-removeChild": "error",
-    "mozilla/avoid-nsISupportsString-preferences": "error",
-    "mozilla/import-browser-window-globals": "error",
-    "mozilla/import-globals": "warn",
-    "mozilla/no-import-into-var-and-global": "error",
-    "mozilla/no-useless-parameters": "error",
-    "mozilla/no-useless-removeEventListener": "error",
-    "mozilla/use-default-preference-values": "error",
-    "mozilla/use-ownerGlobal": "error",
-    // No (!foo in bar) or (!object instanceof Class)
-    "no-unsafe-negation": "error",
-    // No eval() and no strings in the first param of setTimeout or setInterval
-    "no-implied-eval": "error",
-    "no-eval": "error",
-  },
   // The html plugin is enabled via a command line option on eslint. To avoid
   // bad interactions with the xml preprocessor in eslint-plugin-mozilla, we
   // turn off processing of the html plugin for .xml files.
   "settings": {
     "html/xml-extensions": [ ".xhtml" ]
-  },
-  "env": {
-    "es6": true
-  },
-  "parserOptions": {
-    "ecmaVersion": 8,
   },
 };
