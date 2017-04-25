0118bd8fd8129ca74d46b98a0e5e6907c28c4727
Aaron Klotz <aklotz@mozilla.com>
2017-04-25 12:29:20 -0600

    Bug 1359536 - Fix incorrect ABI for js-ctypes declaration of GetVersionExW. r=mhowell, a=gchang
    MozReview-Commit-ID: CyAnHFLwfrb

--- toolkit/mozapps/update/nsUpdateService.js
+++ toolkit/mozapps/update/nsUpdateService.js
@@ -958,7 +958,7 @@ function shouldUseService() {
     try {
       try {
         let GetVersionEx = kernel32.declare("GetVersionExW",
-                                            ctypes.default_abi,
+                                            ctypes.winapi_abi,
                                             BOOL,
                                             OSVERSIONINFOEXW.ptr);
         let winVer = OSVERSIONINFOEXW();
