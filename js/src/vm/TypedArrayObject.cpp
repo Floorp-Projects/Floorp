18a90016196d8c49623ddf4920fa4ca6e3577a86
André Bargull <andre.bargull@gmail.com>
2017-04-25 09:36:00 -0700

    Bug 1317383 - Part 3: Use ToIndex when TypedArray constructor is called with ArrayBuffer (ES2017). r=evilpie

--- js/src/vm/TypedArrayObject.cpp
+++ js/src/vm/TypedArrayObject.cpp
@@ -746,17 +746,11 @@ class TypedArrayObjectTemplate : public TypedArrayObject
         if (!GetPrototypeFromConstructor(cx, newTarget, &proto))
             return nullptr;
 
-        int32_t byteOffset = 0;
+        uint64_t byteOffset = 0;
         if (args.hasDefined(1)) {
             // Step 6.
-            if (!ToInt32(cx, args[1], &byteOffset))
+            if (!ToIndex(cx, args[1], &byteOffset))
                 return nullptr;
-            if (byteOffset < 0) {
-                JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                                          JSMSG_TYPED_ARRAY_NEGATIVE_ARG,
-                                          "1");
-                return nullptr;
-            }
 
             // Step 7.
             if (byteOffset % sizeof(NativeType) != 0) {
@@ -766,17 +760,11 @@ class TypedArrayObjectTemplate : public TypedArrayObject
             }
         }
 
-        int32_t length = -1;
+        uint64_t length = UINT64_MAX;
         if (args.hasDefined(2)) {
             // Step 8.a.
-            if (!ToInt32(cx, args[2], &length))
+            if (!ToIndex(cx, args[2], &length))
                 return nullptr;
-            if (length < 0) {
-                JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                                          JSMSG_TYPED_ARRAY_NEGATIVE_ARG,
-                                          "2");
-                return nullptr;
-            }
         }
 
         // Steps 9-17.
@@ -792,9 +780,12 @@ class TypedArrayObjectTemplate : public TypedArrayObject
     // Steps 9-12.
     static bool
     computeAndCheckLength(JSContext* cx, HandleArrayBufferObjectMaybeShared bufferMaybeUnwrapped,
-                          uint32_t byteOffset, int32_t lengthInt, uint32_t* length)
+                          uint64_t byteOffset, uint64_t lengthIndex, uint32_t* length)
     {
         MOZ_ASSERT(byteOffset % sizeof(NativeType) == 0);
+        MOZ_ASSERT(byteOffset < uint64_t(DOUBLE_INTEGRAL_PRECISION_LIMIT));
+        MOZ_ASSERT_IF(lengthIndex != UINT64_MAX,
+                      lengthIndex < uint64_t(DOUBLE_INTEGRAL_PRECISION_LIMIT));
 
         // Step 9.
         if (bufferMaybeUnwrapped->isDetached()) {
@@ -805,55 +796,46 @@ class TypedArrayObjectTemplate : public TypedArrayObject
         // Step 10.
         uint32_t bufferByteLength = bufferMaybeUnwrapped->byteLength();
 
-        // 11.c, 12.b.
-        if (byteOffset > bufferByteLength) {
-            JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                                      JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
-            return false; // invalid byteOffset
-        }
-
         uint32_t len;
-        if (lengthInt < 0) {
-            // Step 11.b.
-            uint32_t newByteLength = bufferByteLength - byteOffset;
-            len = newByteLength / sizeof(NativeType);
-
-            // Step 11.a.
-            if (len * sizeof(NativeType) != newByteLength) {
-                JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                                          JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
-                return false; // given byte array doesn't map exactly to sizeof(NativeType) * N
-            }
-
-            // ArrayBuffer is too large for TypedArrays:
-            // Standalone ArrayBuffers can hold up to INT32_MAX bytes, whereas
-            // buffers in TypedArrays must have less than or equal to
-            // |INT32_MAX - sizeof(NativeType) - INT32_MAX % sizeof(NativeType)|
-            // bytes.
-            if (len >= INT32_MAX / sizeof(NativeType)) {
+        if (lengthIndex == UINT64_MAX) {
+            // Steps 11.a, 11.c.
+            if (bufferByteLength % sizeof(NativeType) != 0 || byteOffset > bufferByteLength) {
+                // The given byte array doesn't map exactly to
+                // |sizeof(NativeType) * N| or |byteOffset| is invalid.
                 JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                                           JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
                 return false;
             }
+
+            // Step 11.b.
+            uint32_t newByteLength = bufferByteLength - uint32_t(byteOffset);
+            len = newByteLength / sizeof(NativeType);
         } else {
-            // Step 12.a (implicit).
-            len = uint32_t(lengthInt);
+            // Step 12.a.
+            uint64_t newByteLength = lengthIndex * sizeof(NativeType);
 
             // Step 12.b.
-            if (len >= INT32_MAX / sizeof(NativeType)) {
+            if (byteOffset + newByteLength > bufferByteLength) {
+                // |byteOffset + newByteLength| is too big for the arraybuffer
                 JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                                           JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
                 return false;
             }
-            uint32_t newByteLength = len * sizeof(NativeType);
 
-            // Step 12.b (|byteOffset| moved to the RHS to avoid overflow).
-            if (newByteLength > bufferByteLength - byteOffset) {
-                JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
-                                          JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
-                return false; // |byteOffset + newByteLength| is too big for the arraybuffer
-            }
+            len = uint32_t(lengthIndex);
         }
+
+        // ArrayBuffer is too large for TypedArrays:
+        // Standalone ArrayBuffers can hold up to INT32_MAX bytes, whereas
+        // buffers in TypedArrays must have less than or equal to
+        // |INT32_MAX - sizeof(NativeType) - INT32_MAX % sizeof(NativeType)|
+        // bytes.
+        if (len >= INT32_MAX / sizeof(NativeType)) {
+            JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
+                                      JSMSG_TYPED_ARRAY_CONSTRUCT_BOUNDS);
+            return false;
+        }
+        MOZ_ASSERT(byteOffset <= UINT32_MAX);
 
         *length = len;
         return true;
@@ -864,15 +846,15 @@ class TypedArrayObjectTemplate : public TypedArrayObject
     // Steps 9-17.
     static JSObject*
     fromBufferSameCompartment(JSContext* cx, HandleArrayBufferObjectMaybeShared buffer,
-                              uint32_t byteOffset, int32_t lengthInt, HandleObject proto)
+                              uint64_t byteOffset, uint64_t lengthIndex, HandleObject proto)
     {
         // Steps 9-12.
         uint32_t length;
-        if (!computeAndCheckLength(cx, buffer, byteOffset, lengthInt, &length))
+        if (!computeAndCheckLength(cx, buffer, byteOffset, lengthIndex, &length))
             return nullptr;
 
         // Steps 13-17.
-        return makeInstance(cx, buffer, byteOffset, length, proto);
+        return makeInstance(cx, buffer, uint32_t(byteOffset), length, proto);
     }
 
     // Create a TypedArray object in another compartment.
@@ -890,8 +872,8 @@ class TypedArrayObjectTemplate : public TypedArrayObject
     // in B, its [[Prototype]] must be (a cross-compartment wrapper for) the
     // TypedArray.prototype in A.
     static JSObject*
-    fromBufferWrapped(JSContext* cx, HandleObject bufobj, uint32_t byteOffset, int32_t lengthInt,
-                      HandleObject proto)
+    fromBufferWrapped(JSContext* cx, HandleObject bufobj, uint64_t byteOffset,
+                      uint64_t lengthIndex, HandleObject proto)
     {
         JSObject* unwrapped = CheckedUnwrap(bufobj);
         if (!unwrapped) {
@@ -908,7 +890,7 @@ class TypedArrayObjectTemplate : public TypedArrayObject
         unwrappedBuffer = &unwrapped->as<ArrayBufferObjectMaybeShared>();
 
         uint32_t length;
-        if (!computeAndCheckLength(cx, unwrappedBuffer, byteOffset, lengthInt, &length))
+        if (!computeAndCheckLength(cx, unwrappedBuffer, byteOffset, lengthIndex, &length))
             return nullptr;
 
         // Make sure to get the [[Prototype]] for the created typed array from
@@ -927,7 +909,8 @@ class TypedArrayObjectTemplate : public TypedArrayObject
             if (!cx->compartment()->wrap(cx, &wrappedProto))
                 return nullptr;
 
-            typedArray = makeInstance(cx, unwrappedBuffer, byteOffset, length, wrappedProto);
+            typedArray =
+                makeInstance(cx, unwrappedBuffer, uint32_t(byteOffset), length, wrappedProto);
             if (!typedArray)
                 return nullptr;
         }
@@ -948,11 +931,12 @@ class TypedArrayObjectTemplate : public TypedArrayObject
             return nullptr; // invalid byteOffset
         }
 
+        uint64_t lengthIndex = lengthInt >= 0 ? uint64_t(lengthInt) : UINT64_MAX;
         if (bufobj->is<ArrayBufferObjectMaybeShared>()) {
             HandleArrayBufferObjectMaybeShared buffer = bufobj.as<ArrayBufferObjectMaybeShared>();
-            return fromBufferSameCompartment(cx, buffer, byteOffset, lengthInt, nullptr);
+            return fromBufferSameCompartment(cx, buffer, byteOffset, lengthIndex, nullptr);
         }
-        return fromBufferWrapped(cx, bufobj, byteOffset, lengthInt, nullptr);
+        return fromBufferWrapped(cx, bufobj, byteOffset, lengthIndex, nullptr);
     }
 
     static bool
