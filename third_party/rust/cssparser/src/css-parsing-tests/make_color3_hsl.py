50ba3ee7a79b38e51858b8cccc90c3773e49e4ee
Servo VCS Sync <servo-vcs-sync@mozilla.com>
2017-04-25 00:10:45 +0000

    No bug - Revendor rust dependencies

--- third_party/rust/cssparser/src/css-parsing-tests/make_color3_hsl.py
+++ third_party/rust/cssparser/src/css-parsing-tests/make_color3_hsl.py
@@ -2,18 +2,32 @@ import colorsys  # It turns out Python already does HSL -> RGB!
 trim = lambda s: s if not s.endswith('.0') else s[:-2]
 print('[')
 print(',\n'.join(
-    f[0] % (
-        (n, h, trim(str(s/10.)), trim(str(l/10.)),
-         f[1] % round(a / 255., 2) if a is not None else '')
-        + tuple(trim(str(min(255, v * 256)))
-                for v in colorsys.hls_to_rgb(h/360., l/1000., s/1000.))
-        + (a if a is not None else 255,)
+    function_format % tuple(
+        [
+            function_name,
+            hue,
+            trim(str(saturation / 10.)),
+            trim(str(lightness / 10.)),
+            alpha_format % round(alpha / 255., 2) if alpha is not None else ''
+        ] + [
+            trim(str(min(255, component * 256)))
+            for component in colorsys.hls_to_rgb(
+                hue / 360.,
+                lightness / 1000.,
+                saturation / 1000.
+            )
+        ] + [
+            alpha if alpha is not None else 255
+        ]
     )
-    for f in [('"%s(%s, %s%%, %s%%%s)", [%s, %s, %s, %s]', ', %s'), ('"%s(%s %s%% %s%%%s)", [%s, %s, %s, %s]', ' / %s')]
-    for n in ["hsl", "hsla"]
-    for a in [None, 255, 64, 0]
-    for l in range(0, 1001, 125)
-    for s in range(0, 1001, 125)
-    for h in range(0, 360, 30)
+    for function_format, alpha_format in [
+        ('"%s(%s, %s%%, %s%%%s)", [%s, %s, %s, %s]', ', %s'),
+        ('"%s(%s %s%% %s%%%s)", [%s, %s, %s, %s]', ' / %s')
+    ]
+    for function_name in ["hsl", "hsla"]
+    for alpha in [None, 255, 64, 0]
+    for lightness in range(0, 1001, 125)
+    for saturation in range(0, 1001, 125)
+    for hue in range(0, 360, 30)
 ))
 print(']')
