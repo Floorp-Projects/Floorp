520937839b7e4376a89f7235c2c57e9266a957db
André Bargull <andre.bargull@gmail.com>
2017-04-25 09:35:46 -0700

    Bug 1317383 - Part 2: Use ToIndex when constructing TypedArray with length argument (ES2017). r=evilpie
    --HG--
    extra : rebase_source : d60ffa2f7677deec6269a0df245d6a574966f0a7

---
+++ js/src/tests/ecma_6/TypedArray/constructor-length-too-large.js
@@ -0,0 +1,28 @@
+// Test that all TypedArray constructor variants throw a RangeError when
+// attempting to create a too large array.
+
+// The maximum typed array length is (currently) limited to
+// `(INT32_MAX / BYTES_PER_ELEMENT) - 1`.
+
+const INT32_MAX = 2**31 - 1;
+
+// 22.2.4.2 TypedArray ( length )
+for (let TA of typedArrayConstructors) {
+    assertThrows(() => new TA(INT32_MAX), RangeError);
+    assertThrows(() => new TA(INT32_MAX >> Math.log2(TA.BYTES_PER_ELEMENT)), RangeError);
+}
+
+// 22.2.4.3 TypedArray ( typedArray )
+const largeInt8Array = new Int8Array(2**30);
+for (let TA of typedArrayConstructors.filter(c => c.BYTES_PER_ELEMENT > 1)) {
+    assertThrows(() => new TA(largeInt8Array), RangeError);
+}
+
+// 22.2.4.4 TypedArray ( object )
+for (let TA of typedArrayConstructors) {
+    assertThrows(() => new TA({length: INT32_MAX}), RangeError);
+    assertThrows(() => new TA({length: INT32_MAX >> Math.log2(TA.BYTES_PER_ELEMENT)}), RangeError);
+}
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
