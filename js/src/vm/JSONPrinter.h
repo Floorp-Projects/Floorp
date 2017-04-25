ef7e1b10b2192ba5c2c85db614a9f8c5a4c36a63
Steve Fink <sfink@mozilla.com>
2017-04-25 13:38:43 -0700

    Bug 1322560 - Implement TimeDuration property in JSONPrinter

--- js/src/vm/JSONPrinter.h
+++ js/src/vm/JSONPrinter.h
@@ -9,6 +9,8 @@
 
 #include <stdio.h>
 
+#include "mozilla/TimeStamp.h"
+
 #include "js/TypeDecls.h"
 #include "vm/Printer.h"
 
@@ -42,6 +44,12 @@ class JSONPrinter
     void property(const char* name, uint32_t value);
     void property(const char* name, uint64_t value);
     void property(const char* name, double value);
+
+    // JSON requires decimals to be separated by periods, but the LC_NUMERIC
+    // setting may cause printf to use commas in some locales. Enforce using a
+    // period.
+    enum TimePrecision { SECONDS, MILLISECONDS };
+    void property(const char* name, const mozilla::TimeDuration& dur, TimePrecision precision);
 
     void beginStringProperty(const char* name);
     void endStringProperty();
