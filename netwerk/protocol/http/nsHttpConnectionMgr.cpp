deb3127d30f30976026e71154270148c41e3280e
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-04-25 16:31:58 +0200

    Backed out changeset ee570e804c7f (bug 1188435)

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -3512,7 +3512,9 @@ nsHalfOpenSocket::FastOpenConnected(nsresult aError)
         // This is called from nsSocketTransport::RecoverFromError. The
         // socket will try connect and we need to rewind nsHttpTransaction
         MOZ_ASSERT(mConnectionNegotiatingFastOpen);
-        Unused << mConnectionNegotiatingFastOpen->Transaction()->RestartOnFastOpenError();
+        DebugOnly<nsresult> rv =
+            mConnectionNegotiatingFastOpen->Transaction()->RestartOnFastOpenError();
+        MOZ_ASSERT(NS_SUCCEEDED(rv));
     }
     if (mConnectionNegotiatingFastOpen) {
         mSocketTransport = nullptr;
