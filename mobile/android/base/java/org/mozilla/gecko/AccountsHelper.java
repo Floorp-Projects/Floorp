c9b3f40856bdb19d54636f1b0428396931a63729
Wes Kocher <wkocher@mozilla.com>
2017-04-25 12:47:59 -0700

    Backed out 3 changesets (bug 1351805) for android Test failures a=backout
    Backed out changeset da34da5f310a (bug 1351805)
    Backed out changeset 430b56176e04 (bug 1351805)
    Backed out changeset a3dfb4b9dc66 (bug 1351805)
    
    MozReview-Commit-ID: AE4RcKV3akz

--- mobile/android/base/java/org/mozilla/gecko/AccountsHelper.java
+++ mobile/android/base/java/org/mozilla/gecko/AccountsHelper.java
@@ -19,6 +19,7 @@ import org.json.JSONException;
 import org.mozilla.gecko.background.fxa.FxAccountUtils;
 import org.mozilla.gecko.fxa.FirefoxAccounts;
 import org.mozilla.gecko.fxa.FxAccountConstants;
+import org.mozilla.gecko.fxa.FxAccountDeviceRegistrator;
 import org.mozilla.gecko.fxa.authenticator.AndroidFxAccount;
 import org.mozilla.gecko.fxa.login.Engaged;
 import org.mozilla.gecko.fxa.login.State;
