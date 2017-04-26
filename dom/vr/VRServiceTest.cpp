edefcd2284f3993016507d582e56f870eb5cf48e
Daosheng Mu <daoshengmu@gmail.com>
2017-04-26 13:17:25 +0800

    Bug 1355648 - Part 3: Set position and orientation IsValid properties for GamepadPose tests; r=kip
    MozReview-Commit-ID: 29sjo3yhg98

--- dom/vr/VRServiceTest.cpp
+++ dom/vr/VRServiceTest.cpp
@@ -212,6 +212,7 @@ VRMockController::NewPoseMove(const Nullable<Float32Array>& aPosition,
     poseState.orientation[1] = value.Data()[1];
     poseState.orientation[2] = value.Data()[2];
     poseState.orientation[3] = value.Data()[3];
+    poseState.isOrientationValid = true;
   }
   if (!aPosition.IsNull()) {
     const Float32Array& value = aPosition.Value();
@@ -220,6 +221,7 @@ VRMockController::NewPoseMove(const Nullable<Float32Array>& aPosition,
     poseState.position[0] = value.Data()[0];
     poseState.position[1] = value.Data()[1];
     poseState.position[2] = value.Data()[2];
+    poseState.isPositionValid = true;
   }
   if (!aAngularVelocity.IsNull()) {
     const Float32Array& value = aAngularVelocity.Value();
