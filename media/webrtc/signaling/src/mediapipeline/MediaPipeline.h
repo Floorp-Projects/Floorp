3f2743fa3595c1ef1d26a350b9b2bc62f8b648b0
Michael Froman <mfroman@mozilla.com>
2017-04-26 10:51:00 -0500

    Bug 1358224 - pt 2 - change to RTP stream id filtering on simulcast mochitests. r=drno
    The simulcast mochitests setup the receiving PeerConnection to receive
    simulcast video streams which Firefox doesn't really support.  Without
    a test media server, this is about the best we can do and still test
    simulcast.
    
    Unfortunately the two simulcast streams arriving with different ssrcs
    (as expect) exercises code we have to deal with some services switching
    ssrcs midstream.  In the tests, this causes intermittent failures
    because the test is waiting to receive a certain ssrc, and the receiving
    VideoConduit has switched to the other ssrc.
    
    This change adds the ability to filter on RID at the MediaPipeline level,
    which we can setup prior to media flowing.  This avoids the ssrc switching
    issue since the VideoConduit only receives one ssrc until we change the
    RID filter to the second RID.  At that point, the VideoConduit sees a new
    ssrc and the switching code works as intended.
    
    The modified mochitests setup the RTP stream id header extension, and then
    filter on each of the RTP stream ids in turn.
    
    MozReview-Commit-ID: KApfaxMX8rl

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
@@ -118,6 +118,14 @@ class MediaPipeline : public sigslot::has_slots<> {
   // everything but the nth ssrc
   void SelectSsrc_m(size_t ssrc_index);
   void SelectSsrc_s(uint32_t ssrc);
+  // Used only for testing; adds RTP header extension for RTP Stream Id with
+  // the given id.
+  void AddRIDExtension_m(size_t extension_id);
+  void AddRIDExtension_s(size_t extension_id);
+  // Used only for testing; installs a MediaPipelineFilter that filters
+  // everything but the given RID
+  void AddRIDFilter_m(const std::string& rid);
+  void AddRIDFilter_s(const std::string& rid);
 
   virtual Direction direction() const { return direction_; }
   virtual const std::string& trackid() const { return track_id_; }
