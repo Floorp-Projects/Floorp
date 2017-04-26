4fe0c1c85664567f4c546af99b399d3d12b95916
Brian Birtles <birtles@gmail.com>
2017-04-26 13:00:12 +0900

    Bug 1355348 - Convert stylo assert in nsComputedDOMStyle::DoGetStyleContextNoFlush to a warning; r=hiro
    This allows us to run SMIL tests that include a combination of additive and
    non-additive animation.
    
    MozReview-Commit-ID: 2uVN184h7Ge

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -558,13 +558,13 @@ nsComputedDOMStyle::DoGetStyleContextNoFlush(Element* aElement,
         if (aAnimationFlag == eWithoutAnimation) {
           nsPresContext* presContext = presShell->GetPresContext();
           MOZ_ASSERT(presContext, "Should have a prescontext if we have a frame");
-          MOZ_ASSERT(presContext->StyleSet()->IsGecko(),
-                     "stylo: Need ResolveStyleByRemovingAnimation for stylo");
           if (presContext && presContext->StyleSet()->IsGecko()) {
             nsStyleSet* styleSet = presContext->StyleSet()->AsGecko();
             return styleSet->ResolveStyleByRemovingAnimation(
                      aElement, result, eRestyle_AllHintsWithAnimations);
           } else {
+            NS_WARNING("stylo: Getting the unanimated style context is not yet"
+                       " supported for Servo");
             return nullptr;
           }
         }
