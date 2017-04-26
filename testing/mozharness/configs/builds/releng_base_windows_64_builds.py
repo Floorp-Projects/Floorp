f2b5a3d7966b96e4dd93077add943abe78539241
Gregory Szorc <gps@mozilla.com>
2017-04-26 13:31:40 -0700

    Bug 1304508 - Remove references to pymake in mozharness configs; r=ted
    Now that mozharness is calling `mach build` to invoke the "check" target,
    there are no more consumers of the "enable_pymake" config option. So we
    remove it from the configs.
    
    We also remove definitions of the "make" executable referring to pymake.
    The only call sites I could find for "query_exe('make')" are in
    testing/mozharness/scripts/mobile_l10n.py. So most of these definitions of
    the "make" executable appear to be a cargo cult or left over from a
    previous rewrite (the code we just changed to stop calling "make" wasn't
    using query_exe). Since mobile_l10n.py is still using query_exe() to find
    the make executable, there's a chance switching away from pymake (if this
    patch even does that - I don't think we touched a config file related to
    that script) could break something. I'm fine with teasing out that bug.
    
    MozReview-Commit-ID: 7HR6ShAKcoV

--- testing/mozharness/configs/builds/releng_base_windows_64_builds.py
+++ testing/mozharness/configs/builds/releng_base_windows_64_builds.py
@@ -28,12 +28,6 @@ config = {
             sys.executable,
             'c:\\mozilla-build\\buildbotve\\scripts\\buildbot'
         ],
-        "make": [
-            sys.executable,
-            os.path.join(
-                os.getcwd(), 'build', 'src', 'build', 'pymake', 'make.py'
-            )
-        ],
         'virtualenv': [
             sys.executable,
             'c:/mozilla-build/buildbotve/virtualenv.py'
@@ -86,7 +80,6 @@ config = {
         'MINIDUMP_STACKWALK': '%(abs_tools_dir)s/breakpad/win64/minidump_stackwalk.exe',
         'MINIDUMP_SAVE_PATH': '%(base_work_dir)s/minidumps',
     },
-    'enable_pymake': True,
     'src_mozconfig': 'browser/config/mozconfigs/win64/nightly',
     'tooltool_manifest_src': "browser/config/tooltool-manifests/win64/releng.manifest",
     #########################################################################
