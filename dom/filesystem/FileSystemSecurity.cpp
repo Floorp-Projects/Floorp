ee651d844f665c6182afd5054c510916c6e8f9a4
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-04-26 17:56:55 +0200

    Backed out changeset b06d30abb358 (bug 1354308) for failing test_basic.html, test_formSubmission.html and test_no_dnd.html. r=backout

--- dom/filesystem/FileSystemSecurity.cpp
+++ dom/filesystem/FileSystemSecurity.cpp
@@ -89,17 +89,9 @@ FileSystemSecurity::ContentProcessHasAccessTo(ContentParentId aId,
   MOZ_ASSERT(NS_IsMainThread());
   AssertIsInMainProcess();
 
-#if defined(XP_WIN)
-  if (StringBeginsWith(aPath, NS_LITERAL_STRING("..\\")) ||
-      FindInReadable(NS_LITERAL_STRING("\\..\\"), aPath)) {
+  if (FindInReadable(NS_LITERAL_STRING(".."), aPath)) {
     return false;
   }
-#elif defined(XP_UNIX)
-  if (StringBeginsWith(aPath, NS_LITERAL_STRING("../")) ||
-      FindInReadable(NS_LITERAL_STRING("/../"), aPath)) {
-    return false;
-  }
-#endif
 
   nsTArray<nsString>* paths;
   if (!mPaths.Get(aId, &paths)) {
