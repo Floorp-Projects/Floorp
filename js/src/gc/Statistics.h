29642b93be5e94ca2071a91d5fe2bcd2c7128cd5
Steve Fink <sfink@mozilla.com>
2017-04-26 13:48:09 -0700

    Bug 1322560 - Add APIs for retrieving major GC timing info
    --HG--
    extra : rebase_source : ca6176bc7af4a71521f416aa4a70230aeee90ec0

--- js/src/gc/Statistics.h
+++ js/src/gc/Statistics.h
@@ -245,7 +245,7 @@ struct Statistics
     void reset(gc::AbortReason reason) {
         MOZ_ASSERT(reason != gc::AbortReason::None);
         if (!aborted)
-            slices.back().resetReason = reason;
+            slices_.back().resetReason = reason;
     }
 
     void nonincremental(gc::AbortReason reason) {
@@ -274,7 +274,7 @@ struct Statistics
 
     UniqueChars formatCompactSliceMessage() const;
     UniqueChars formatCompactSummaryMessage() const;
-    UniqueChars formatJsonMessage(uint64_t timestamp) const;
+    UniqueChars formatJsonMessage(uint64_t timestamp, bool includeSlices = true) const;
     UniqueChars formatDetailedMessage() const;
 
     JS::GCSliceCallback setSliceCallback(JS::GCSliceCallback callback);
@@ -319,10 +319,18 @@ struct Statistics
     };
 
     typedef Vector<SliceData, 8, SystemAllocPolicy> SliceDataVector;
-    typedef SliceDataVector::ConstRange SliceRange;
 
-    SliceRange sliceRange() const { return slices.all(); }
-    size_t slicesLength() const { return slices.length(); }
+    const SliceDataVector& slices() const { return slices_; }
+
+    TimeStamp start() const {
+        MOZ_ASSERT(phaseStartTimes[PHASE_GC_BEGIN]);
+        return slices_[0].start;
+    }
+
+    TimeStamp end() const {
+        MOZ_ASSERT(phaseStartTimes[PHASE_GC_BEGIN]);
+        return slices_.back().end;
+    }
 
     // Occasionally print header lines for profiling information.
     void maybePrintProfileHeaders();
@@ -332,6 +340,9 @@ struct Statistics
 
     // Print total profile times on shutdown.
     void printTotalProfileTimes();
+
+    // Return JSON for the timings of just the given slice.
+    UniqueChars formatJsonSlice(size_t sliceNum) const;
 
   private:
     JSRuntime* runtime;
@@ -345,7 +356,7 @@ struct Statistics
 
     gc::AbortReason nonincrementalReason_;
 
-    SliceDataVector slices;
+    SliceDataVector slices_;
 
     /* Most recent time when the given phase started. */
     EnumeratedArray<Phase, PHASE_LIMIT, TimeStamp> phaseStartTimes;
