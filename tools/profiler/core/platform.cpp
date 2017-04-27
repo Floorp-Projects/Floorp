58a28f63c7fcfb3fdd653dcfbe3a432b9e2dfd44
Nicholas Nethercote <nnethercote@mozilla.com>
2017-04-27 15:57:35 +1000

    Bug 1359717 - Remove unnecessary SetIsPaused() calls. r=mstange.
    We pause/unpause the profiler before/after some streaming operations. But these
    pause/unpause pairs occur with gPSMutex locked, and ActivePS::IsPaused() also
    requires that gPSMutex be locked. Therefore these pause/unpause pairs cannot
    be observed, and so this patch removes them.

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -1578,8 +1578,6 @@ locked_profiler_stream_json_for_this_process(PSLockRef aLock,
   // Lists the samples for each thread profile
   aWriter.StartArrayProperty("threads");
   {
-    ActivePS::SetIsPaused(aLock, true);
-
     const CorePS::ThreadVector& liveThreads = CorePS::LiveThreads(aLock);
     for (size_t i = 0; i < liveThreads.size(); i++) {
       ThreadInfo* info = liveThreads.at(i);
@@ -1611,8 +1609,6 @@ locked_profiler_stream_json_for_this_process(PSLockRef aLock,
       java::GeckoJavaSampler::Unpause();
     }
 #endif
-
-    ActivePS::SetIsPaused(aLock, false);
   }
   aWriter.EndArray();
 }
@@ -3021,15 +3017,11 @@ profiler_clear_js_context()
   // requires a live JSContext.
 
   if (ActivePS::Exists(lock)) {
-    ActivePS::SetIsPaused(lock, true);
-
     // Flush this thread's ThreadInfo, if it is being profiled.
     if (info->IsBeingProfiled()) {
       info->FlushSamplesAndMarkers(ActivePS::Buffer(lock),
                                    CorePS::ProcessStartTime(lock));
     }
-
-    ActivePS::SetIsPaused(lock, false);
   }
 
   // We don't call info->StopJSSampling() here; there's no point doing that for
