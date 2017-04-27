40c7ff6fc371e80ff81cd9642c0bee2186060042
Ben Hearsum <bhearsum@mozilla.com>
2017-04-27 10:35:36 -0400

    bug 1358601: set up CI builds and tests for DevEdition on mozilla-beta - add linux devedition builds r=aki,kmoir
    MozReview-Commit-ID: 9mghIaHomfs

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -381,6 +381,7 @@ class BuildOptionParser(object):
         'debug-artifact': 'builds/releng_sub_%s_configs/%s_debug_artifact.py',
         'qr-debug': 'builds/releng_sub_%s_configs/%s_qr_debug.py',
         'qr-opt': 'builds/releng_sub_%s_configs/%s_qr_opt.py',
+        'devedition': 'builds/releng_sub_%s_configs/%s_devedition.py',
     }
     build_pool_cfg_file = 'builds/build_pool_specifics.py'
     branch_cfg_file = 'builds/branch_specifics.py'
