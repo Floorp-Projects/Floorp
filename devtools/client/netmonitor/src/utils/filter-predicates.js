6c5dbdf83536aecca10ac1386d0a92d0682a969a
Dragana Damjanovic <dd.mozilla@gmail.com>
2017-04-27 16:48:43 +0200

    Bug 1334776 - ResponseHeader Visitor will get original header casing. We need to fix it at some places in devtools.

--- devtools/client/netmonitor/src/utils/filter-predicates.js
+++ devtools/client/netmonitor/src/utils/filter-predicates.js
@@ -78,7 +78,7 @@ function isWS({ requestHeaders, responseHeaders }) {
 
   // Find the 'upgrade' header.
   let upgradeHeader = requestHeaders.headers.find(header => {
-    return (header.name == "Upgrade");
+    return (header.name.toLowerCase() == "upgrade");
   });
 
   // If no header found on request, check response - mainly to get
@@ -87,7 +87,7 @@ function isWS({ requestHeaders, responseHeaders }) {
   if (!upgradeHeader && responseHeaders &&
       Array.isArray(responseHeaders.headers)) {
     upgradeHeader = responseHeaders.headers.find(header => {
-      return (header.name == "Upgrade");
+      return (header.name.toLowerCase() == "upgrade");
     });
   }
 
