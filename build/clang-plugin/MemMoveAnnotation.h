2372ce0fc1b113e8c3ae74cf7028f324c461acd7
Michael Layzell <michael@thelayzells.com>
2017-04-27 12:44:55 -0400

    Bug 1339537 - Part 5: Produce better annotation reason diagnostics for implicit annotations, r=ehsan
    This allows for the alignas(_) case to be distinguished from the
    MOZ_NON_PARAM case through notes.
    
    MozReview-Commit-ID: 4KIbzEKnmNU

--- build/clang-plugin/MemMoveAnnotation.h
+++ build/clang-plugin/MemMoveAnnotation.h
@@ -17,7 +17,7 @@ public:
   virtual ~MemMoveAnnotation() {}
 
 protected:
-  bool hasFakeAnnotation(const TagDecl *D) const override {
+  std::string getImplicitReason(const TagDecl *D) const override {
     // Annotate everything in ::std, with a few exceptions; see bug
     // 1201314 for discussion.
     if (getDeclarationNamespace(D) == "std") {
@@ -48,11 +48,11 @@ protected:
           Name == "_Atomic_llong" ||
           Name == "_Atomic_ullong" ||
           Name == "_Atomic_address") {
-        return false;
+        return "";
       }
-      return true;
+      return "it is an stl-provided type not guaranteed to be memmove-able";
     }
-    return false;
+    return "";
   }
 };
 
