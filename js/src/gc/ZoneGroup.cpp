5dadf4bf020d4c29878c0b90c7d4e88f2dfcc25c
Bill McCloskey <billm@mozilla.com>
2017-04-27 15:25:21 -0700

    Bug 1360372 - Acquire cooperative lock when entering system zone group (r=bhackett)
    MozReview-Commit-ID: 92SgTKMD6xt

--- js/src/gc/ZoneGroup.cpp
+++ js/src/gc/ZoneGroup.cpp
@@ -57,12 +57,17 @@ ZoneGroup::~ZoneGroup()
 }
 
 void
-ZoneGroup::enter()
+ZoneGroup::enter(JSContext* cx)
 {
-    JSContext* cx = TlsContext.get();
     if (ownerContext().context() == cx) {
         MOZ_ASSERT(enterCount);
     } else {
+        if (useExclusiveLocking) {
+            MOZ_ASSERT(!usedByHelperThread);
+            while (ownerContext().context() != nullptr) {
+                cx->yieldToEmbedding();
+            }
+        }
         MOZ_RELEASE_ASSERT(ownerContext().context() == nullptr);
         MOZ_ASSERT(enterCount == 0);
         ownerContext_ = CooperatingContext(cx);
