4a108d0ac398e260867954d4be1f29c33a96ea12
Jeremy Chen <jeremychen@mozilla.com>
2017-04-27 17:26:31 +0800

    Bug 1354437 - enable border-spacing interpolation test. r=hiro
    So far, we don't have a type to test anamations of a pair of length.
    Since border-spacing consists two absolute lengths, we shall add this
    new type.
    
    MozReview-Commit-ID: Bo8VMWPLDHc

--- testing/web-platform/tests/web-animations/animation-model/animation-types/property-list.js
+++ testing/web-platform/tests/web-animations/animation-model/animation-types/property-list.js
@@ -259,8 +259,7 @@ var gCSSProperties = {
   },
   'border-spacing': {
     // https://drafts.csswg.org/css-tables/#propdef-border-spacing
-    types: [
-    ]
+    types: [ 'lengthPair' ]
   },
   'border-top-color': {
     // https://drafts.csswg.org/css-backgrounds-3/#border-top-color
