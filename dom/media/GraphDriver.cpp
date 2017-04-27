e79da7518015ba1c425be662bae3aa14be56ced6
Alex Chronopoulos <achronop@gmail.com>
2017-04-27 10:24:25 +0300

    Bug 971528 - Allow stereo capture in AudioCallbackDriver. r=padenot * * * [mq]: graph-driver-comments
    MozReview-Commit-ID: JnU3opk8X5K
    * * *
    [mq]: tmp
    
    MozReview-Commit-ID: 73CtZw4CRG
    
    --HG--
    extra : rebase_source : bca0932ccffc6348260039fc7ec829245a9b2236

--- dom/media/GraphDriver.cpp
+++ dom/media/GraphDriver.cpp
@@ -662,10 +662,19 @@ AudioCallbackDriver::Init()
     latency_frames = std::max((uint32_t) 512, latency_frames);
   }
 
-
   input = output;
-  input.channels = mInputChannels; // change to support optional stereo capture
-  input.layout = CUBEB_LAYOUT_MONO;
+  input.channels = mInputChannels;
+  input.layout = CUBEB_LAYOUT_UNDEFINED;
+
+#ifdef MOZ_WEBRTC
+  if (mGraphImpl->mInputWanted) {
+    StaticMutexAutoLock lock(AudioInputCubeb::Mutex());
+    uint32_t maxInputChannels = 0;
+    if (AudioInputCubeb::GetDeviceMaxChannels(mGraphImpl->mInputDeviceID, maxInputChannels) == 0) {
+      input.channels = mInputChannels = maxInputChannels;
+    }
+  }
+#endif
 
   cubeb_stream* stream = nullptr;
   CubebUtils::AudioDeviceID input_id = nullptr, output_id = nullptr;
