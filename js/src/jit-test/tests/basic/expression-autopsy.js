a277af4a0def7fe57a24378bc2909f7ad192d940
André Bargull <andre.bargull@gmail.com>
2017-04-27 09:24:08 -0700

    Bug 1360220 - Replace emitRequireObjectCoercible with JSOP_CHECKOBJCOERCIBLE. r=shu

--- js/src/jit-test/tests/basic/expression-autopsy.js
+++ js/src/jit-test/tests/basic/expression-autopsy.js
@@ -183,22 +183,10 @@ check_one("[...].foo",
           function() { [undefined, ...[]].foo(); },
           " is not a function");
 
-// Manual testing for this case: the only way to trigger an error is *not* on
-// an attempted property access during destructuring, and the error message
-// invoking ToObject(null) is different: "can't convert {0} to object".
-try
-{
-  (function() {
-    var [{x}] = [null, {}];
-   })();
-  throw new Error("didn't throw");
-}
-catch (e)
-{
-  assertEq(e instanceof TypeError, true,
-           "expected TypeError, got " + e);
-  assertEq(e.message, "can't convert null to object");
-}
+check_one("[...][Symbol.iterator](...).next(...).value",
+          function () { var [{x}] = [null, {}]; }, " is null");
+check_one("[...][Symbol.iterator](...).next(...).value",
+          function () { var [{x}] = [void 0, {}]; }, " is undefined");
 
 try {
   (function() {
