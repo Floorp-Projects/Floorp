bd012d95462d4cc1c3a0c5dd972b41278928e995
Tom Tromey <tom@tromey.com>
2017-04-28 10:13:26 -0600

    Bug 1334318 - remove last uses of PR_smprintf; r=froydnj
    This removes the last uses of PR_smprintf from the tree (excluding the
    security and nsprpub directories).  It also fixes a related latent bug
    in nsAppRunner.cpp (which was incorrectly freeing the pointer passed to
    PR_SetEnv).
    
    MozReview-Commit-ID: GynP2PhuWWO
    
    --HG--
    extra : rebase_source : c3b83c7bd08b1c222e137a00323caf5481352845

--- toolkit/xre/nsUpdateDriver.cpp
+++ toolkit/xre/nsUpdateDriver.cpp
@@ -24,6 +24,7 @@
 #include "mozilla/Preferences.h"
 #include "nsPrintfCString.h"
 #include "mozilla/DebugOnly.h"
+#include "mozilla/Printf.h"
 
 #ifdef XP_MACOSX
 #include "nsILocalFileMac.h"
@@ -406,18 +407,15 @@ AppendToLibPath(const char *pathToAppend)
 {
   char *pathValue = getenv(LD_LIBRARY_PATH_ENVVAR_NAME);
   if (nullptr == pathValue || '\0' == *pathValue) {
-    char *s = PR_smprintf("%s=%s", LD_LIBRARY_PATH_ENVVAR_NAME, pathToAppend);
+    // Leak the string because that is required by PR_SetEnv.
+    char *s = Smprintf("%s=%s", LD_LIBRARY_PATH_ENVVAR_NAME, pathToAppend).release();
     PR_SetEnv(s);
   } else if (!strstr(pathValue, pathToAppend)) {
-    char *s = PR_smprintf("%s=%s" PATH_SEPARATOR "%s",
-                    LD_LIBRARY_PATH_ENVVAR_NAME, pathToAppend, pathValue);
+    // Leak the string because that is required by PR_SetEnv.
+    char *s = Smprintf("%s=%s" PATH_SEPARATOR "%s",
+                       LD_LIBRARY_PATH_ENVVAR_NAME, pathToAppend, pathValue).release();
     PR_SetEnv(s);
   }
-
-  // The memory used by PR_SetEnv is not copied to the environment on all
-  // platform, it can be used by reference directly. So we purposely do not
-  // call PR_smprintf_free on s.  Subsequent calls to PR_SetEnv will free
-  // the old memory first.
 }
 #endif
 
