b54c52315ca829a7a1ed0b798899eda7038f4441
Andrzej Hunt <andrzej@ahunt.org>
2017-04-28 11:55:34 +0800

    [focus] Run findbugs via buddybuild_postbuild.sh

---
+++ mobile/android/focus-android/buddybuild_postbuild.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e # Exit (and fail) immediately if any command in this scriptfails
+
+# buddybuild doesn't seem to offer any direct way of running findbugs.
+# findbugs is run as part of |gradle build| and |gradle| check, but those
+# aren't run directly in buddybuild.
+./gradlew findbugs
