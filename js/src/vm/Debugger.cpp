84742375b07e9e5a81a2a374e1142c76ee6ed56f
Steve Fink <sfink@mozilla.com>
2017-04-28 11:13:14 -0700

    Backed out changeset 4dee851a0d45 (bug 1322560)
    --HG--
    extra : rebase_source : ef09906aaa98d4cb8a005d831ac887b6b24f11c1

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -11739,21 +11739,21 @@ GarbageCollectionEvent::Create(JSRuntime* rt, ::js::gcstats::Statistics& stats, 
 
     data->nonincrementalReason = stats.nonincrementalReason();
 
-    for (auto& slice : stats.slices()) {
+    for (auto range = stats.sliceRange(); !range.empty(); range.popFront()) {
         if (!data->reason) {
             // There is only one GC reason for the whole cycle, but for legacy
             // reasons this data is stored and replicated on each slice. Each
             // slice used to have its own GCReason, but now they are all the
             // same.
-            data->reason = gcreason::ExplainReason(slice.reason);
+            data->reason = gcreason::ExplainReason(range.front().reason);
             MOZ_ASSERT(data->reason);
         }
 
         if (!data->collections.growBy(1))
             return nullptr;
 
-        data->collections.back().startTimestamp = slice.start;
-        data->collections.back().endTimestamp = slice.end;
+        data->collections.back().startTimestamp = range.front().start;
+        data->collections.back().endTimestamp = range.front().end;
     }
 
     return data;
