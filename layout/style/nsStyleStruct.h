5e40c4609b2a910a843b90db9e877152810a6153
cku <cku@mozilla.com>
2017-04-28 19:47:04 +0800

    Bug 1310885 - Part 11. (gecko) Clean up unused things. r=heycam
    MozReview-Commit-ID: CxHbNyVNSz8
    
    --HG--
    extra : rebase_source : c9b3600e31023d79736f4df871ed741c37b1647f
    extra : intermediate-source : d650a2abb555fb2bed3d832bc8f007ec7909c99e
    extra : source : 33e2a248ed4da28bba3b0e4b745e3d894c68ed4d

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -344,13 +344,6 @@ public:
   // on the main thread before get() can be used.
   nsStyleImageRequest(
       Mode aModeFlags,
-      const nsAString& aURL,
-      already_AddRefed<mozilla::URLExtraData> aExtraData);
-
-  // Can be called from any thread, but Resolve() must be called later
-  // on the main thread before get() can be used.
-  nsStyleImageRequest(
-      Mode aModeFlags,
       mozilla::css::ImageValue* aImageValue);
 
   bool Resolve(nsPresContext* aPresContext);
