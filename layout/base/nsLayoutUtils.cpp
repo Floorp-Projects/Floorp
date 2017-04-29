fd257b22bebd922405417ba09cba07dcb99342d6
Phil Ringnalda <philringnalda@gmail.com>
2017-04-28 20:12:06 -0700

    Backed out 7 changesets (bug 1302470) for mochitest-chrome failures CLOSED TREE
    Backed out changeset de9f42b512c8 (bug 1302470)
    Backed out changeset 2394e63f50ff (bug 1302470)
    Backed out changeset 3542d7bacbdc (bug 1302470)
    Backed out changeset 22793139a866 (bug 1302470)
    Backed out changeset cc6ff95187d0 (bug 1302470)
    Backed out changeset 033fce5ecd39 (bug 1302470)
    Backed out changeset 0bb3e70e5f46 (bug 1302470)
    
    MozReview-Commit-ID: 5DmpbQp335X

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -3232,8 +3232,6 @@ nsLayoutUtils::GetFramesForArea(nsIFrame* aFrame, const nsRect& aRect,
     builder.SetDescendIntoSubdocuments(false);
   }
 
-  builder.SetHitTestShouldStopAtFirstOpaque(aFlags & ONLY_VISIBLE);
-
   builder.EnterPresShell(aFrame);
   aFrame->BuildDisplayListForStackingContext(&builder, aRect, &list);
   builder.LeavePresShell(aFrame, nullptr);
@@ -3249,6 +3247,7 @@ nsLayoutUtils::GetFramesForArea(nsIFrame* aFrame, const nsRect& aRect,
 #endif
 
   nsDisplayItem::HitTestState hitTestState;
+  builder.SetHitTestShouldStopAtFirstOpaque(aFlags & ONLY_VISIBLE);
   list.HitTest(&builder, aRect, &hitTestState, &aOutFrames);
   list.DeleteAll();
   return NS_OK;
