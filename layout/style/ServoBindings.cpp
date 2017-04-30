f41cc887a0ee671771280c9363116053b7403424
Cameron McCormack <cam@mcc.id.au>
2017-04-30 14:51:29 +0800

    Bug 1356103 - Part 7: Use PostTraversalTasks to deal with FontFace's Promise during Servo traversal. r=bholley
    The PostTraversalTask does not take a strong reference to the FontFace
    object, since as a DOM object, we can't call AddRef/Release on it
    from the Servo style worker threads.  The FontFace objects that we
    encounter are all held on to by the FontFaceSet, and none of the
    work that we do during font loads should drop FontFace objects
    from the FontFaceSet.  (That only happens under
    nsIDocument::FlushUserFontSet, which is only called on the
    main thread.)
    
    MozReview-Commit-ID: 5CdtGQYC9aL

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -1846,8 +1846,18 @@ Gecko_GetFontMetrics(RawGeckoPresContextBorrowed aPresContext,
   MOZ_ASSERT(NS_IsMainThread());
   MutexAutoLock lock(*sServoFontMetricsLock);
   GeckoFontMetrics ret;
-  // Safe because we are locked, and this function is only
-  // ever called from Servo parallel traversal or the main thread
+
+  // Getting font metrics can require some main thread only work to be
+  // done, such as work that needs to touch non-threadsafe refcounted
+  // objects (like the DOM FontFace/FontFaceSet objects), network loads, etc.
+  //
+  // To handle this work, font code checks whether we are in a Servo traversal
+  // and if so, appends PostTraversalTasks to the current ServoStyleSet
+  // to be performed immediately after the traversal is finished.  This
+  // works well for starting downloadable font loads, since we don't have
+  // those fonts available to get metrics for anyway.  Platform fonts and
+  // ArrayBuffer-backed FontFace objects are handled synchronously.
+
   nsPresContext* presContext = const_cast<nsPresContext*>(aPresContext);
   presContext->SetUsesExChUnits(true);
   RefPtr<nsFontMetrics> fm = nsRuleNode::GetMetricsFor(presContext, aIsVertical,
