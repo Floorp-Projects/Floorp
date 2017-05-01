7f4fedb60444df43ad45024180c392af2516f764
Ben Hearsum <bhearsum@mozilla.com>
2017-05-01 16:30:44 -0400

    bug 1358601: set up CI builds and tests for DevEdition on mozilla-beta - enable pgo, fix Linux treeherder symbols, use correct mozconfigs for Mac and Windows. r=rail, a=ritu
    MozReview-Commit-ID: BFE1LtMd9Lm

--- testing/mozharness/configs/builds/releng_sub_windows_configs/64_devedition.py
+++ testing/mozharness/configs/builds/releng_sub_windows_configs/64_devedition.py
@@ -1,4 +1,5 @@
 config = {
+    'src_mozconfig': 'browser/config/mozconfigs/win64/devedition',
     'force_clobber': True,
     'stage_platform': 'win64-devedition',
     'stage_product': 'devedition',
