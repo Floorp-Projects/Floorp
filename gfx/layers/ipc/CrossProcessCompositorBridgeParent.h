068dcddeb0cc1b7a7b9f967904f997e9c90e5b64
David Anderson <dvander@alliedmods.net>
2017-04-30 17:22:56 -0700

    Make PLayerTransaction's constructor async. (bug 1350634, ipc_r=billm, r=mattwoodrow, r=kats)
    PLayerTransaction's constructor was previously synchronous so we could
    return a TextureFactoryIdentifier. This is quite reliably available
    already in the case of opening a tab, due to RenderFrameParent knowing
    which compositor it is attached to, so we can make the constructor
    asynchronous.
    
    In the top-level widget case, we add a new synchronous message to find
    the TextureFactoryIdentifier.

--- gfx/layers/ipc/CrossProcessCompositorBridgeParent.h
+++ gfx/layers/ipc/CrossProcessCompositorBridgeParent.h
@@ -91,9 +91,7 @@ public:
 
   virtual PLayerTransactionParent*
     AllocPLayerTransactionParent(const nsTArray<LayersBackend>& aBackendHints,
-                                 const uint64_t& aId,
-                                 TextureFactoryIdentifier* aTextureFactoryIdentifier,
-                                 bool *aSuccess) override;
+                                 const uint64_t& aId) override;
 
   virtual bool DeallocPLayerTransactionParent(PLayerTransactionParent* aLayers) override;
 
