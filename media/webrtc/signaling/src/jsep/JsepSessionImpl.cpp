95d79a14e0aa41aeca7a6d7ff03c4361b931e482
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2017-05-01 21:02:53 -0700

    Bug 1361206: warn about non-matching RTP header extension IDs. r=bwc
    MozReview-Commit-ID: DLG5ICBydAK

--- media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
+++ media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
@@ -2085,10 +2085,14 @@ JsepSessionImpl::ValidateAnswer(const Sdp& offer, const Sdp& answer)
             }
 
             if (offExt.entry < 4096 && (offExt.entry != ansExt.entry)) {
-              JSEP_SET_ERROR("Answer changed id for extmap attribute at level "
-                             << i << " (" << offExt.extensionname << ") from "
-                             << offExt.entry << " to " << ansExt.entry << ".");
-              return NS_ERROR_INVALID_ARG;
+              // FIXME we do not return an error here, because Cisco Spark
+              // actually does respond with different extension ID's then we
+              // offer. See bug 1361206 for details.
+              MOZ_MTLOG(ML_WARNING, "Answer changed id for extmap attribute at"
+                        " level " << i << " (" << offExt.extensionname << ") "
+                        "from " << offExt.entry << " to "
+                        << ansExt.entry << ".");
+              // return NS_ERROR_INVALID_ARG;
             }
 
             if (ansExt.entry >= 4096) {
