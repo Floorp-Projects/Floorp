fe1001ba5fcac83af9c3e5b47daed3c3c13b8cde
Nick Alexander <nalexander@mozilla.com>
2017-05-02 15:27:37 -0700

    Bug 1361545 - Add "skin" flavorDimension for Photon mobile work. r=sebastian
    This is pretty straight-forward.
    
    Sadly, this will require local developers to add a "skin" product
    flavor to their invocations, like:
    
    ./mach gradle app:assembleLocalAustralisDebug
    
    In addition, this shows how many different variants of the Gradle
    product flavor are embedded into our automation configurations.  I
    can't solve that at this time.
    
    Since I was here, I took the time to rename "automation" to
    "official", which makes "localAustralis" the default in Android
    Studio, avoiding a common issue with new builders producing an APK
    that doesn't include omni.ja in the IDE.
    
    MozReview-Commit-ID: CtU7zFpNCob

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_findbugs.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_findbugs.py
@@ -6,7 +6,10 @@ config = {
     'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android-frontend/releng.manifest',
     'multi_locale_config_platform': 'android',
     'postflight_build_mach_commands': [
-        ['gradle', 'app:findbugsAutomationDebug'],
+        ['gradle',
+         'app:findbugsOfficialAustralisDebug',
+         'app:findbugsOfficialPhotonDebug',
+        ],
     ],
     'artifact_flag_build_variant_in_try': None, # There's no artifact equivalent.
 }
