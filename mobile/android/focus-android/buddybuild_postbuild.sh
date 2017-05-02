1f20d79527bf20eb188e082b280ea0efb8cd3d70
Andrzej Hunt <andrzej@ahunt.org>
2017-05-01 20:24:33 -0700

    [focus] git reset before buddybuild_postbuild.sh to remove SDK injection

--- mobile/android/focus-android/buddybuild_postbuild.sh
+++ mobile/android/focus-android/buddybuild_postbuild.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e # Exit (and fail) immediately if any command in this scriptfails
 
+# buddybuild modifies our buildscripts and sources (this is partly to enable
+# their SDK, and partly to allow selecting flavours in the BuddyBuild UI).
+# We don't know where the Buddybuild SDK lives, which causes gradle builds
+# to be broken (due to the SDK dependency injected into FocusApplication),
+# it's easiest just to revert to a clean source state here:
+git reset --hard
+
 # buddybuild doesn't seem to offer any direct way of running findbugs.
 # findbugs is run as part of |gradle build| and |gradle| check, but those
 # aren't run directly in buddybuild.
