f1bd49a3a342d3a2e8cec617a40fa7c4b8a46990
cku <cku@mozilla.com>
2017-05-02 16:20:01 +0800

    Bug 1310885 - Part 3. (gecko) Create a new API to receive cached ImageValue from stylo.
    This patch implement several things:
    1. Implement a new constructor for nsStyleImageRequest to receive an existing
       ImageValue from the caller.
    2. Implement Gecko_ImageValue_Create to allow stylo to create a gecko::ImageValue
       object.
    3. Implement Gecko_SetXXXXImageValue to allow stylo to pass a created ImageValue
       back to gecko.
    
    MozReview-Commit-ID: 1SbW5w8FSV2

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -276,6 +276,11 @@ void Gecko_CopyListStyleTypeFrom(nsStyleList* dst, const nsStyleList* src);
 void Gecko_SetNullImageValue(nsStyleImage* image);
 void Gecko_SetGradientImageValue(nsStyleImage* image, nsStyleGradient* gradient);
 NS_DECL_THREADSAFE_FFI_REFCOUNTING(mozilla::css::ImageValue, ImageValue);
+mozilla::css::ImageValue* Gecko_ImageValue_Create(ServoBundledURI aURI);
+void Gecko_SetLayerImageImageValue(nsStyleImage* image,
+                                   mozilla::css::ImageValue* aImageValue);
+
+// XXX cku: remove this function after gecko and stylo side are both ready
 void Gecko_SetUrlImageValue(nsStyleImage* image,
                             ServoBundledURI uri);
 void Gecko_SetImageElement(nsStyleImage* image, nsIAtom* atom);
@@ -290,17 +295,26 @@ nsStyleGradient* Gecko_CreateGradient(uint8_t shape,
 
 // list-style-image style.
 void Gecko_SetListStyleImageNone(nsStyleList* style_struct);
+void Gecko_SetListStyleImageImageValue(nsStyleList* style_struct,
+                                  mozilla::css::ImageValue* aImageValue);
+// XXX cku: remove this function after gecko and stylo side are both ready
 void Gecko_SetListStyleImage(nsStyleList* style_struct,
                              ServoBundledURI uri);
 void Gecko_CopyListStyleImageFrom(nsStyleList* dest, const nsStyleList* src);
 
 // cursor style.
 void Gecko_SetCursorArrayLength(nsStyleUserInterface* ui, size_t len);
+void Gecko_SetCursorImageValue(nsCursorImage* aCursor,
+                               mozilla::css::ImageValue* aImageValue);
+// XXX cku: remove this function after gecko and stylo side are both ready
 void Gecko_SetCursorImage(nsCursorImage* cursor,
                           ServoBundledURI uri);
 void Gecko_CopyCursorArrayFrom(nsStyleUserInterface* dest,
                                const nsStyleUserInterface* src);
 
+void Gecko_SetContentDataImageValue(nsStyleContentData* aList,
+                                    mozilla::css::ImageValue* aImageValue);
+// XXX cku: remove this function after gecko and stylo side are both ready
 void Gecko_SetContentDataImage(nsStyleContentData* content_data, ServoBundledURI uri);
 void Gecko_SetContentDataArray(nsStyleContentData* content_data, nsStyleContentType type, uint32_t len);
 
