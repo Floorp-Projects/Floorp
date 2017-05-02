e464dadcebf0d3ada2eda0bb092800f2af2e3ab3
Emilio Cobos Álvarez <emilio@crisal.io>
2017-05-02 22:42:26 +0200

    Bug 1361766: Move MathML content state changes outside of reflow. r=xidorn
    This also adds assertions to ensure attributes and state don't change during
    layout or frame construction.
    
    MozReview-Commit-ID: BANcpxnRsYS
    
    --HG--
    extra : rebase_source : 5d1bc424d3ed90fda3047d3c92c09251346b7bec

--- layout/mathml/nsMathMLmunderoverFrame.h
+++ layout/mathml/nsMathMLmunderoverFrame.h
@@ -7,50 +7,82 @@
 #define nsMathMLmunderoverFrame_h___
 
 #include "mozilla/Attributes.h"
+#include "nsIReflowCallback.h"
 #include "nsMathMLContainerFrame.h"
 
 //
 // <munderover> -- attach an underscript-overscript pair to a base
 //
 
-class nsMathMLmunderoverFrame : public nsMathMLContainerFrame {
+class nsMathMLmunderoverFrame final
+  : public nsMathMLContainerFrame
+  , public nsIReflowCallback
+{
+
 public:
   NS_DECL_FRAMEARENA_HELPERS
 
   friend nsIFrame* NS_NewMathMLmunderoverFrame(nsIPresShell* aPresShell, nsStyleContext* aContext);
 
-  virtual nsresult
-  Place(DrawTarget*          aDrawTarget,
-        bool                 aPlaceOrigin,
-        ReflowOutput& aDesiredSize) override;
+  nsresult Place(DrawTarget* aDrawTarget,
+                 bool aPlaceOrigin,
+                 ReflowOutput& aDesiredSize) override;
 
-  NS_IMETHOD
-  InheritAutomaticData(nsIFrame* aParent) override;
+  NS_IMETHOD InheritAutomaticData(nsIFrame* aParent) override;
 
-  NS_IMETHOD
-  TransmitAutomaticData() override;
+  NS_IMETHOD TransmitAutomaticData() override;
 
-  NS_IMETHOD
-  UpdatePresentationData(uint32_t        aFlagsValues,
-                         uint32_t        aFlagsToUpdate) override;
+  NS_IMETHOD UpdatePresentationData(uint32_t aFlagsValues,
+                                    uint32_t aFlagsToUpdate) override;
 
-  virtual nsresult
-  AttributeChanged(int32_t         aNameSpaceID,
-                   nsIAtom*        aAttribute,
-                   int32_t         aModType) override;
+  void DestroyFrom(nsIFrame* aRoot) override;
 
-  uint8_t
-  ScriptIncrement(nsIFrame* aFrame) override;
+  nsresult AttributeChanged(int32_t aNameSpaceID,
+                            nsIAtom* aAttribute,
+                            int32_t aModType) override;
+
+  uint8_t ScriptIncrement(nsIFrame* aFrame) override;
+
+  // nsIReflowCallback.
+  bool ReflowFinished() override;
+  void ReflowCallbackCanceled() override;
 
 protected:
-  explicit nsMathMLmunderoverFrame(nsStyleContext* aContext) : nsMathMLContainerFrame(aContext),
-                                                               mIncrementUnder(false),
-                                                               mIncrementOver(false) {}
+  explicit nsMathMLmunderoverFrame(nsStyleContext* aContext)
+    : nsMathMLContainerFrame(aContext)
+    , mIncrementUnder(false)
+    , mIncrementOver(false)
+  {}
+
   virtual ~nsMathMLmunderoverFrame();
 
 private:
+  // Helper to set the "increment script level" flag on the element belonging
+  // to a child frame given by aChildIndex.
+  //
+  // When this flag is set, the style system will increment the scriptlevel for
+  // the child element. This is needed for situations where the style system
+  // cannot itself determine the scriptlevel (mfrac, munder, mover, munderover).
+  //
+  // This should be called during reflow.
+  //
+  // We set the flag and if it changed, we request appropriate restyling and
+  // also queue a post-reflow callback to ensure that restyle and reflow happens
+  // immediately after the current reflow.
+  void SetIncrementScriptLevel(uint32_t aChildIndex, bool aIncrement);
+  void SetPendingPostReflowIncrementScriptLevel();
+
   bool mIncrementUnder;
   bool mIncrementOver;
+
+  struct SetIncrementScriptLevelCommand
+  {
+    uint32_t mChildIndex;
+    bool mDoIncrement;
+  };
+
+  nsTArray<SetIncrementScriptLevelCommand>
+    mPostReflowIncrementScriptLevelCommands;
 };
 
 
