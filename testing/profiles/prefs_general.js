273d177fca479d5ebb06932739036cb098491627
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-05-02 20:19:17 +0200

    Backed out changeset 46dd4a4338a1 (bug 1352204) for timing out Talos suites g2, o and s. r=backout

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -88,7 +88,6 @@ user_pref("extensions.installDistroAddons", false);
 // XPI extensions are required for test harnesses to load
 user_pref("extensions.defaultProviders.enabled", true);
 user_pref("xpinstall.signatures.required", false);
-user_pref("extensions.allow-non-mpc-extensions", true);
 
 user_pref("geo.wifi.uri", "http://%(server)s/tests/dom/tests/mochitest/geolocation/network_geolocation.sjs");
 user_pref("geo.wifi.timeToWaitBeforeSending", 2000);
