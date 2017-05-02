924f6f2cbdb51344e38c464730f50e4c7800d2f6
Andrzej Hunt <andrzej@ahunt.org>
2017-05-02 10:08:45 -0700

    [focus] Revert "Disable findbugs in buddybuild for now"
    This reverts commit https://github.com/mozilla-mobile/firefox-android/commit/13fb639e5007e555844ff3f8b1151ef6824262d4.

--- mobile/android/focus-android/buddybuild_postbuild.sh
+++ mobile/android/focus-android/buddybuild_postbuild.sh
@@ -4,6 +4,7 @@ set -e # Exit (and fail) immediately if any command in this scriptfails
 # buddybuild doesn't seem to offer any direct way of running findbugs.
 # findbugs is run as part of |gradle build| and |gradle| check, but those
 # aren't run directly in buddybuild.
-#./gradlew findbugs
+./gradlew findbugs
+
 ./gradlew jacocoTestReport
- bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
