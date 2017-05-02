f6c374b187606370d1c24f9117480c13baff3e0d
Alastor Wu <alwu@mozilla.com>
2017-05-02 18:53:21 +0800

    Bug 1357639 - part5 : create new type test class for media playback. r=sebastian
    Create new test class for reducing the redundant code and can provide more
    flexibility for adding new related test in the future.
    
    MozReview-Commit-ID: 2f3O8vfHo12

--- mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testMediaControl.java
+++ mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testMediaControl.java
@@ -4,41 +4,17 @@
 
 package org.mozilla.gecko.tests;
 
-import org.mozilla.gecko.Actions;
-import org.mozilla.gecko.R;
-import org.mozilla.gecko.Tab;
-import org.mozilla.gecko.Tabs;
-import org.mozilla.gecko.media.AudioFocusAgent;
-import org.mozilla.gecko.media.AudioFocusAgent.State;
 import org.mozilla.gecko.media.MediaControlService;
 
-import android.content.Intent;
-import android.content.Context;
 import android.media.AudioManager;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.os.Build;
-import android.service.notification.StatusBarNotification;
-
-import com.robotium.solo.Condition;
-
-public class testMediaControl extends BaseTest {
-    private Context mContext;
-    private int mPrevIcon = 0;
-    private boolean mPrevTabAudioPlaying = false;
-
+public class testMediaControl extends MediaPlaybackTest {
     public void testMediaControl() {
-        // The API to check system notification is available after version 23.
-        if (Build.VERSION.SDK_INT < 23) {
-            return;
-        }
+        info("- ensure the test is running on correct Android version -");
+        checkAndroidVersionForMediaControlTest();
 
         info("- wait for gecko ready -");
         blockForGeckoReady();
-
-        info("- setup testing memeber variable -");
-        setupForTesting();
 
         info("- load URL -");
         final String MEDIA_URL = getAbsoluteUrl(mStringHelper.ROBOCOP_MEDIA_PLAYBACK_LOOP_URL);
@@ -125,149 +101,33 @@ public class testMediaControl extends BaseTest {
         final String BLANK_URL = getAbsoluteUrl(mStringHelper.ROBOCOP_BLANK_PAGE_01_URL);
         addTab(BLANK_URL);
 
-        info("- the media control should still be displayed in status bar -");
-        checkDisplayedNotificationStates(true /* playing */);
+        info("- the media control shouldn't be changed -");
+        checkMediaNotificationStates(true /* playing */);
 
         info("- close tab -");
         closeAllTabs();
+
+        info("- run next test : testCloseTab -");
+        testCloseTab();
     }
 
-    /**
-     * Testing tool functions
-     */
-    private void info(String msg) {
-        mAsserter.dumpLog(msg);
-    }
+    private void testCloseTab() {
+        info("- load URL -");
+        final String MEDIA_URL = getAbsoluteUrl(mStringHelper.ROBOCOP_MEDIA_PLAYBACK_LOOP_URL);
+        loadUrlAndWait(MEDIA_URL);
 
-    private void setupForTesting() {
-        mContext = getInstrumentation().getTargetContext();
-    }
+        info("- check whether audio starts playing -");
+        checkIfMediaPlayingSuccess(true /* playing */);
 
-    private void waitUntilNotificationUIChanged() {
-        waitForCondition(new Condition() {
-            @Override
-            public boolean isSatisfied() {
-                NotificationManager notificationManager = (NotificationManager)
-                    mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-                StatusBarNotification[] sbns = notificationManager.getActiveNotifications();
-                if (sbns.length == 1 &&
-                    sbns[0].getNotification().actions.length == 1) {
-                    // Ensure the UI has been changed.
-                    if (sbns[0].getNotification().actions[0].icon != mPrevIcon) {
-                        mPrevIcon = sbns[0].getNotification().actions[0].icon ;
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }, MAX_WAIT_MS);
-    }
+        info("- close audible tab -");
+        final Tab tab = Tabs.getInstance().getFirstTabForUrl(MEDIA_URL);
+        Tabs.getInstance().closeTab(tab);
 
-    private void waitUntilTabAudioPlayingStateChanged() {
-        final Tab tab = Tabs.getInstance().getSelectedTab();
-        waitForCondition(new Condition() {
-            @Override
-            public boolean isSatisfied() {
-                if (tab.isAudioPlaying() != mPrevTabAudioPlaying) {
-                    mPrevTabAudioPlaying = tab.isAudioPlaying();
-                    return true;
-                }
-                return false;
-            }
-        }, MAX_WAIT_MS);
-    }
-
-    private void notifyMediaControlService(String action) {
-        Intent intent = new Intent(mContext, MediaControlService.class);
-        intent.setAction(action);
-        mContext.startService(intent);
-    }
-
-    private void checkIfMediaPlayingSuccess(boolean isTabPlaying) {
-        checkIfMediaPlayingSuccess(isTabPlaying, false);
-    }
-
-    private void checkIfMediaPlayingSuccess(boolean isTabPlaying,
-                                            boolean clearNotification) {
-        checkAudioFocusStateChanged(isTabPlaying);
-        checkMediaNotificationStatesChanged(isTabPlaying, clearNotification);
-    }
-
-    /**
-     * This method is used to check whether notification states are correct or
-     * not after notification UI changed.
-     */
-    private void checkMediaNotificationStatesChanged(boolean isTabPlaying,
-                                                     boolean clearNotification) {
+        info("- media control should disappear -");
         waitUntilNotificationUIChanged();
+        checkIfMediaNotificationBeCleared();
 
-        final Tab tab = Tabs.getInstance().getSelectedTab();
-        mAsserter.ok(isTabPlaying == tab.isMediaPlaying(),
-                     "Checking the media playing state of tab, isTabPlaying = " + isTabPlaying,
-                     "Tab's media playing state is correct.");
-
-        if (clearNotification) {
-            checkIfNotificationBeCleared();
-        } else {
-            checkDisplayedNotificationStates(isTabPlaying);
-        }
-    }
-
-    private void checkDisplayedNotificationStates(boolean isTabPlaying) {
-        NotificationManager notificationManager = (NotificationManager)
-            mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        StatusBarNotification[] sbns = notificationManager.getActiveNotifications();
-        mAsserter.is(sbns.length, 1,
-                     "Should only have one notification in system's status bar.");
-
-        Notification notification = sbns[0].getNotification();
-        mAsserter.is(notification.actions.length, 1,
-                     "Only has one action in notification.");
-
-        mAsserter.is(notification.actions[0].title,
-                     mContext.getString(isTabPlaying ? R.string.media_pause : R.string.media_play),
-                     "Action has correct title.");
-        mAsserter.is(notification.actions[0].icon,
-                     isTabPlaying ? R.drawable.ic_media_pause : R.drawable.ic_media_play,
-                     "Action has correct icon.");
-    }
-
-    private void checkIfNotificationBeCleared() {
-        NotificationManager notificationManager = (NotificationManager)
-            mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        StatusBarNotification[] sbns = notificationManager.getActiveNotifications();
-        mAsserter.is(sbns.length, 0,
-                     "Should not have notification in system's status bar.");
-    }
-
-    /**
-     * This method is used to check whether audio focus state are correct or
-     * not after tab's audio playing state changed.
-     */
-    private void checkAudioFocusStateChanged(boolean isTabPlaying) {
-        waitUntilTabAudioPlayingStateChanged();
-
-        final Tab tab = Tabs.getInstance().getSelectedTab();
-        mAsserter.ok(isTabPlaying == tab.isAudioPlaying(),
-                     "Checking the audio playing state of tab, isTabPlaying = " + isTabPlaying,
-                     "Tab's audio playing state is correct.");
-
-        if (isTabPlaying) {
-            mAsserter.is(AudioFocusAgent.getInstance().getAudioFocusState(),
-                         State.OWN_FOCUS,
-                         "Audio focus state is correct.");
-        } else {
-            boolean isLostFocus =
-                AudioFocusAgent.getInstance().getAudioFocusState().equals(State.LOST_FOCUS) ||
-                AudioFocusAgent.getInstance().getAudioFocusState().equals(State.LOST_FOCUS_TRANSIENT);
-            mAsserter.ok(isLostFocus,
-                         "Checking the audio focus when the tab is not playing",
-                         "Audio focus state is correct.");
-        }
-    }
-
-    private AudioFocusAgent getAudioFocusAgent() {
-        return AudioFocusAgent.getInstance();
+        info("- close tab -");
+        closeAllTabs();
     }
 }
