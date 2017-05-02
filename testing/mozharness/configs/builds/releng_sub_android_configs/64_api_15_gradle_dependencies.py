571de094b35d85e65b95582b07bd34a43198233d
Nick Alexander <nalexander@mozilla.com>
2017-05-02 15:27:37 -0700

    Bug 1361545 - Add "skin" flavorDimension for Photon mobile work. r=sebastian
    This is pretty straight-forward.
    
    Sadly, this will require local developers to add a "skin" product
    flavor to their invocations, like:
    
    ./mach gradle app:assembleLocalAustralisDebug
    
    In addition, this shows how many different variants of the Gradle
    product flavor are embedded into our automation configurations.  I
    can't solve that at this time.
    
    Since I was here, I took the time to rename "automation" to
    "official", which makes "localAustralis" the default in Android
    Studio, avoiding a common issue with new builders producing an APK
    that doesn't include omni.ja in the IDE.
    
    MozReview-Commit-ID: CtU7zFpNCob
    
    --HG--
    extra : rebase_source : 477ef683f850ff11cfa128e17855666bb7758a7a

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle_dependencies.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_15_gradle_dependencies.py
@@ -7,16 +7,20 @@ config = {
     'multi_locale_config_platform': 'android',
     'postflight_build_mach_commands': [
         ['gradle',
-         'assembleAutomationRelease',
-         'assembleAutomationDebug',
-         'assembleAutomationDebugAndroidTest',
-         'checkstyle',
-         'findbugsAutomationDebug',
+         'app:assembleOfficialAustralisRelease',
+         'app:assembleOfficialAustralisDebug',
+         'app:assembleOfficialAustralisDebugAndroidTest',
+         'app:findbugsOfficialAustralisDebug',
+         'app:assembleOfficialPhotonRelease',
+         'app:assembleOfficialPhotonDebug',
+         'app:assembleOfficialPhotonDebugAndroidTest',
+         'app:findbugsOfficialPhotonDebug',
          # Does not include Gecko binaries -- see mobile/android/gradle/with_gecko_binaries.gradle.
          'geckoview:assembleWithoutGeckoBinaries',
          # So that we pick up the test dependencies for the builders.
          'geckoview_example:assembleWithoutGeckoBinaries',
          'geckoview_example:assembleWithoutGeckoBinariesAndroidTest',
+         'checkstyle',
         ],
     ],
     'artifact_flag_build_variant_in_try': None, # There's no artifact equivalent.
