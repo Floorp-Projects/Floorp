c2cf02e97d61a688639d56ca5646e5372df588ce
Avikalpa Kundu <avikalpakundu>
2017-05-03 06:38:00 +0200

    Bug 1344834 - Enable flake8 rule E501: 'line too long (113 > 99 characters)'. r=Dexter

--- toolkit/components/telemetry/gen-histogram-data.py
+++ toolkit/components/telemetry/gen-histogram-data.py
@@ -99,8 +99,8 @@ def shared_static_asserts(output, histogram):
     static_assert(output, "%s < %s" % (low, high), "low >= high for %s" % name)
     static_assert(output, "%s > 2" % n_buckets, "Not enough values for %s" % name)
     static_assert(output, "%s >= 1" % low, "Incorrect low value for %s" % name)
-    static_assert(output, "%s > %s" % (high, n_buckets),
-                  "high must be > number of buckets for %s; you may want an enumerated histogram" % name)
+    static_assert(output, "%s > %s" % (high, n_buckets), "high must be > number of buckets for %s;"
+                  " you may want an enumerated histogram" % name)
 
 
 def static_asserts_for_linear(output, histogram):
