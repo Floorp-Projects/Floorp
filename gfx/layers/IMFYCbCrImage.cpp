fd5bf8f94040c1605577b8ea150c71c2e70ed68c
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-05-03 23:23:45 +0200

    Bug 1223270: P2. Add D3D11YCbCrImage type. r=nical
    And related methods.
    
    Will allow to directly upload a YUV420 buffer into a D3D11 texture.
    
    MozReview-Commit-ID: FugfJB3818Y

--- gfx/layers/IMFYCbCrImage.cpp
+++ gfx/layers/IMFYCbCrImage.cpp
@@ -190,9 +190,8 @@ IMFYCbCrImage::GetD3D11TextureData(Data aData, gfx::IntSize aSize)
     mt->Leave();
   }
 
-  return DXGIYCbCrTextureData::Create(TextureFlags::DEFAULT, textureY,
-                                      textureCb, textureCr, aSize, aData.mYSize,
-                                      aData.mCbCrSize);
+  return DXGIYCbCrTextureData::Create(textureY, textureCb, textureCr,
+                                      aSize, aData.mYSize, aData.mCbCrSize);
 }
 
 TextureClient*
