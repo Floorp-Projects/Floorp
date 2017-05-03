3cc190916f09b25156f62821d89b7d14bf812ffd
Ho-Pang Hsu <hopang.hsu@gmail.com>
2017-05-04 01:38:52 +0800

    Bug 1290951 - Part 3.2: Tolerate missing imported scripts. r=bkelly.

--- dom/workers/ServiceWorkerScriptCache.cpp
+++ dom/workers/ServiceWorkerScriptCache.cpp
@@ -154,6 +154,12 @@ public:
     return Move(mPrincipalInfo);
   }
 
+  bool
+  Succeeded() const
+  {
+    return NS_SUCCEEDED(mNetworkResult);
+  }
+
 private:
   ~CompareNetwork()
   {
@@ -579,6 +585,10 @@ private:
     MOZ_ASSERT(aCN);
     MOZ_ASSERT(mState == WaitingForPut);
 
+    if (!aCN->Succeeded()) {
+      return;
+    }
+
     ErrorResult result;
     nsCOMPtr<nsIInputStream> body;
     result = NS_NewCStringInputStream(getter_AddRefs(body),
@@ -763,7 +773,11 @@ CompareNetwork::Finished()
   // mNetworkResult is prior to mCacheResult, since it's needed for reporting
   // various error to the web contenet.
   if (NS_FAILED(mNetworkResult)) {
-    rv = mNetworkResult;
+    // An imported script could become offline, since it might no longer be
+    // needed by the new importing script. In that case, the importing script
+    // must be different, and thus, it's okay to report same script found here.
+    rv = mIsMainScript ? mNetworkResult : NS_OK;
+    same = true;
   } else if (mCC && NS_FAILED(mCacheResult)) {
     rv = mCacheResult;
   } else { // Both passed.
