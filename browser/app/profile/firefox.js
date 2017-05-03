eb829f971bd9284a7e610e780f6cae34fc6c9252
Gabor Krizsanits <gkrizsanits@mozilla.com>
2017-05-03 12:00:20 +0200

    Bug 1341008 - Use the preallocated process manager by default. r=billm, r=kmag To mitigate the delay that a new content process startup might cause, when the browser reaches a non-busy state we attempt to prelaunch an empty content process in the background that can be grabbed and used the next time we need one. This patch enables the preallocated process manager by default and attempts to fix all the issues that prevented us doing this sooner.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1573,6 +1573,8 @@ pref("browser.esedbreader.loglevel", "Error");
 
 pref("browser.laterrun.enabled", false);
 
+pref("dom.ipc.processPrelaunch.enabled", true);
+
 #ifdef EARLY_BETA_OR_EARLIER
 pref("browser.migrate.automigrate.enabled", true);
 #else
