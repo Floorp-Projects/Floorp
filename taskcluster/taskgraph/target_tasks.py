2df6d74f9d2afc3f8494f4871e72cdc884ccadd1
Ryan VanderMeulen <ryanvm@gmail.com>
2017-05-03 16:03:24 -0400

    Bug 1361449 - Run Windows TC builds on Beta. r=jonasfj, a=test-only
    --HG--
    extra : source : bdd7191b7571f1d3790a0427a1401f8a898e3d83

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -217,9 +217,8 @@ def target_tasks_mozilla_beta(full_task_graph, parameters):
         if not filter_for_project(task, parameters):
             return False
         platform = task.attributes.get('build_platform')
-        if platform in ('linux64-pgo', 'linux-pgo', 'win32-pgo', 'win64-pgo',
-                        'android-api-15-nightly', 'android-x86-nightly',
-                        'win32', 'win64'):
+        if platform in ('linux64-pgo', 'linux-pgo', 'android-api-15-nightly',
+                        'android-x86-nightly'):
             return False
         if platform in ('linux64', 'linux', 'macosx64'):
             if task.attributes['build_type'] == 'opt':
