606150d6f4126a4b52fd846e248de804834d6751
Jessica Jong <jjong@mozilla.com>
2017-05-04 11:43:46 +0800

    Bug 1345767 - Part 1: Create classes for each of the input types. r=smaug
    MozReview-Commit-ID: 4O6YHHGsLBP
    
    --HG--
    extra : rebase_source : 8cf558b33ae865c532f65a9c786ad8b78e272b99

---
+++ dom/html/input/SingleLineTextInputTypes.h
@@ -0,0 +1,120 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef SingleLineTextInputTypes_h__
+#define SingleLineTextInputTypes_h__
+
+#include "InputType.h"
+
+class SingleLineTextInputTypeBase : public ::InputType
+{
+public:
+  ~SingleLineTextInputTypeBase() override {}
+
+protected:
+  explicit SingleLineTextInputTypeBase(
+    mozilla::dom::HTMLInputElement* aInputElement)
+      : InputType(aInputElement)
+  {}
+};
+
+// input type=text
+class TextInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) TextInputType(aInputElement);
+  }
+
+private:
+  explicit TextInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+// input type=search
+class SearchInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) SearchInputType(aInputElement);
+  }
+
+private:
+  explicit SearchInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+// input type=tel
+class TelInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) TelInputType(aInputElement);
+  }
+
+private:
+  explicit TelInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+// input type=url
+class URLInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) URLInputType(aInputElement);
+  }
+
+private:
+  explicit URLInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+// input type=email
+class EmailInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) EmailInputType(aInputElement);
+  }
+
+private:
+  explicit EmailInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+// input type=password
+class PasswordInputType : public SingleLineTextInputTypeBase
+{
+public:
+  static InputType*
+  Create(mozilla::dom::HTMLInputElement* aInputElement, void* aMemory)
+  {
+    return new (aMemory) PasswordInputType(aInputElement);
+  }
+
+private:
+  explicit PasswordInputType(mozilla::dom::HTMLInputElement* aInputElement)
+    : SingleLineTextInputTypeBase(aInputElement)
+  {}
+};
+
+#endif /* SingleLineTextInputTypes_h__ */
