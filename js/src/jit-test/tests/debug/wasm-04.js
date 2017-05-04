290e7911e9319d5e923a25d4740b8f5d72fd9d16
Yury Delendik <ydelendik@mozilla.com>
2017-05-04 10:39:09 -0500

    Bug 1362084 - Add binary source and JS source maps support. r=luke
    MozReview-Commit-ID: 7WVcaH9ngHL

--- js/src/jit-test/tests/debug/wasm-04.js
+++ js/src/jit-test/tests/debug/wasm-04.js
@@ -23,6 +23,5 @@ assertThrowsInstanceOf(() => s.sourceLength, Error);
 assertThrowsInstanceOf(() => s.global, Error);
 assertThrowsInstanceOf(() => s.getChildScripts(), Error);
 assertThrowsInstanceOf(() => s.getAllOffsets(), Error);
-assertThrowsInstanceOf(() => s.getAllColumnOffsets(), Error);
 assertThrowsInstanceOf(() => s.getBreakpoint(0), Error);
 assertThrowsInstanceOf(() => s.getOffsetsCoverage(), Error);
