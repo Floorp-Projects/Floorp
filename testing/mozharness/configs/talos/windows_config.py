0214f00fbe0f6b4d647eb8df9f9fb937dd76372f
Armen Zambrano G. <armenzg@mozilla.com>
2017-05-04 16:47:45 -0400

    Bug 1361462 - Python3 support for Mozharness r=aki,rwood
    This adds support for fetching Python3 zip files
    for Talos for Windows 32-bit and 64-bit.
    
    MozReview-Commit-ID: KpYXQrfwRBY

--- testing/mozharness/configs/talos/windows_config.py
+++ testing/mozharness/configs/talos/windows_config.py
@@ -46,4 +46,8 @@ config = {
     "tooltool_cache": os.path.join('c:\\', 'build', 'tooltool_cache'),
     "minidump_stackwalk_path": "win32-minidump_stackwalk.exe",
     "minidump_tooltool_manifest_path": "config/tooltool-manifests/win32/releng.manifest",
+    "python3_manifest": {
+        "win32": "python3.manifest",
+        "win64": "python3_x64.manifest",
+    }
 }
