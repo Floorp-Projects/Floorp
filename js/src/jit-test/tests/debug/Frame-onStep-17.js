2e878c27aaaaccf593e0524291ce9269e86bd77f
Tom Tromey <tom@tromey.com>
2017-05-05 11:50:56 -0600

    Bug 1362403 - update source coordinates after emitting loop bodies; r=jimb
    MozReview-Commit-ID: CEJV2nSvg24
    
    --HG--
    extra : rebase_source : d607b725e4841233e176c639a8ec5c42ca7e86b4

---
+++ js/src/jit-test/tests/debug/Frame-onStep-17.js
@@ -0,0 +1,31 @@
+var g = newGlobal();
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+var log;
+var previous;
+
+dbg.onDebuggerStatement = function (frame) {
+  let debugLine = frame.script.getOffsetLocation(frame.offset).lineNumber;
+  log = '';
+  previous = '';
+  frame.onStep = function() {
+    let foundLine = this.script.getOffsetLocation(this.offset).lineNumber;
+    if (this.script.getLineOffsets(foundLine).indexOf(this.offset) >= 0) {
+      let thisline = (foundLine - debugLine).toString(16);
+      if (thisline !== previous) {
+        log += thisline;
+        previous = thisline;
+      }
+    }
+  };
+};
+
+function testOne(loopKind) {
+  let body = "var array = [2, 4, 6];\ndebugger;\nfor (let iter " +
+      loopKind + " array) {\n  print(iter);\n}\n";
+  g.eval(body);
+  assertEq(log, "12121212");
+}
+
+testOne("in");
+testOne("of");
