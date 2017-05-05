410eb8b030d4a7a39ccd4214786b268f8175bbf9
Wes Kocher <wkocher@mozilla.com>
2017-05-05 15:58:49 -0700

    Backed out changeset eee394a4575b (bug 1361392) for leaksanitizer failures in pretty much all ASAN mochitests a=backout
    MozReview-Commit-ID: 7ap4ag3i0IE

--- gfx/thebes/gfxHarfBuzzShaper.h
+++ gfx/thebes/gfxHarfBuzzShaper.h
@@ -100,12 +100,14 @@ protected:
                               uint32_t        aOffset,
                               uint32_t        aLength,
                               const char16_t *aText,
+                              hb_buffer_t    *aBuffer,
                               bool            aVertical,
                               RoundingFlags   aRounding);
 
     // retrieve glyph positions, applying advance adjustments and attachments
     // returns results in appUnits
     nscoord GetGlyphPositions(gfxContext *aContext,
+                              hb_buffer_t *aBuffer,
                               nsTArray<nsPoint>& aPositions,
                               uint32_t aAppUnitsPerDevUnit);
 
@@ -129,9 +131,6 @@ protected:
 
     // size-specific font object, owned by the gfxHarfBuzzShaper
     hb_font_t         *mHBFont;
-
-    // harfbuzz buffer for the shaping process
-    hb_buffer_t       *mBuffer;
 
     FontCallbackData   mCallbackData;
 
