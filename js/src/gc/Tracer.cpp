85d81dae68b61d1bca9b8f60c13b0bf24d28ecc9
Andrew McCreight <continuation@gmail.com>
2017-05-05 09:26:29 -0700

    Bug 1362205 - Annotate ScopeKinds in JS_GetTraceThingInfo(). r=sfink
    MozReview-Commit-ID: 8Hti5sQAXGA

--- js/src/gc/Tracer.cpp
+++ js/src/gc/Tracer.cpp
@@ -464,6 +464,12 @@ JS_GetTraceThingInfo(char* buf, size_t bufsize, JSTracer* trc, void* thing,
             break;
           }
 
+          case JS::TraceKind::Scope:
+          {
+            js::Scope* scope = static_cast<js::Scope*>(thing);
+            snprintf(buf, bufsize, " %s", js::ScopeKindString(scope->kind()));
+          }
+
           default:
             break;
         }
