cb0d27041b97113d51f0a670e36ca20cca896304
Nazım Can Altınova <canaltinova@gmail.com>
2017-05-06 01:12:03 +0300

    Bug 1360488 - Part 2: stylo: Propagate quirks mode information from Gecko to Servo r=bholley
    MozReview-Commit-ID: 6AyuC2jd3d4
    
    --HG--
    extra : rebase_source : d73057d48589e22cdddcf465b4cad7c28dd8f2de

--- layout/style/ServoDeclarationBlock.cpp
+++ layout/style/ServoDeclarationBlock.cpp
@@ -13,12 +13,13 @@ namespace mozilla {
 
 /* static */ already_AddRefed<ServoDeclarationBlock>
 ServoDeclarationBlock::FromCssText(const nsAString& aCssText,
-                                   URLExtraData* aExtraData)
+                                   URLExtraData* aExtraData,
+                                   nsCompatibility aMode)
 {
   NS_ConvertUTF16toUTF8 value(aCssText);
   // FIXME (bug 1343964): Figure out a better solution for sending the base uri to servo
   RefPtr<RawServoDeclarationBlock>
-    raw = Servo_ParseStyleAttribute(&value, aExtraData).Consume();
+    raw = Servo_ParseStyleAttribute(&value, aExtraData, aMode).Consume();
   RefPtr<ServoDeclarationBlock> decl = new ServoDeclarationBlock(raw.forget());
   return decl.forget();
 }
