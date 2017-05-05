3b2a3259d7116619bc988b4ed7b1abff2886d9e5
Ben Hearsum <bhearsum@mozilla.com>
2017-05-05 14:47:09 -0400

    bug 1361361: allow MAR_CHANNEL_ID and ACCEPTED_MAR_CHANNEL_IDS to be set different for DevEdition. r=nalexander,rstrong, a=ritu
    MozReview-Commit-ID: FC2Sqq2bMxH

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -48,12 +48,17 @@ MOZ_EXTENSIONS_DEFAULT=" gio"
 MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
-# This should usually be the same as the value MAR_CHANNEL_ID.
+# ACCEPTED_MAR_CHANNEL_IDS should usually be the same as the value MAR_CHANNEL_ID.
 # If more than one ID is needed, then you should use a comma separated list
 # of values.
-ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-beta,firefox-mozilla-release
 # The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
-MAR_CHANNEL_ID=firefox-mozilla-beta
+if test "$MOZ_UPDATE_CHANNEL" = "aurora"; then
+  ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-aurora
+  MAR_CHANNEL_ID=firefox-mozilla-aurora
+else
+  ACCEPTED_MAR_CHANNEL_IDS=firefox-mozilla-beta,firefox-mozilla-release
+  MAR_CHANNEL_ID=firefox-mozilla-beta
+fi
 MOZ_PROFILE_MIGRATOR=1
 MOZ_JSDOWNLOADS=1
 
