40ff5f8646a71a7852e77b835dde51ed1eda5285
Ehsan Akhgari <ehsan@mozilla.com>
2017-05-05 22:40:04 -0400

    Back out bug 1357107 since it broke a feature that we have no automated tests for...

--- image/build/nsImageModule.cpp
+++ image/build/nsImageModule.cpp
@@ -10,7 +10,6 @@
 #include "nsMimeTypes.h"
 
 #include "DecodePool.h"
-#include "ImageBlocker.h"
 #include "ImageFactory.h"
 #include "ShutdownTracker.h"
 #include "SurfaceCache.h"
@@ -39,7 +38,6 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsICOEncoder)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsJPEGEncoder)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsPNGEncoder)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsBMPEncoder)
-NS_GENERIC_FACTORY_CONSTRUCTOR(ImageBlocker)
 NS_DEFINE_NAMED_CID(NS_IMGLOADER_CID);
 NS_DEFINE_NAMED_CID(NS_IMGREQUESTPROXY_CID);
 NS_DEFINE_NAMED_CID(NS_IMGTOOLS_CID);
@@ -47,7 +45,6 @@ NS_DEFINE_NAMED_CID(NS_ICOENCODER_CID);
 NS_DEFINE_NAMED_CID(NS_JPEGENCODER_CID);
 NS_DEFINE_NAMED_CID(NS_PNGENCODER_CID);
 NS_DEFINE_NAMED_CID(NS_BMPENCODER_CID);
-NS_DEFINE_NAMED_CID(IMAGEBLOCKER_CID);
 
 static const mozilla::Module::CIDEntry kImageCIDs[] = {
   { &kNS_IMGLOADER_CID, false, nullptr, imgLoaderConstructor, },
@@ -57,7 +54,6 @@ static const mozilla::Module::CIDEntry kImageCIDs[] = {
   { &kNS_JPEGENCODER_CID, false, nullptr, nsJPEGEncoderConstructor, },
   { &kNS_PNGENCODER_CID, false, nullptr, nsPNGEncoderConstructor, },
   { &kNS_BMPENCODER_CID, false, nullptr, nsBMPEncoderConstructor, },
-  { &kIMAGEBLOCKER_CID, false, nullptr, ImageBlockerConstructor, },
   { nullptr }
 };
 
@@ -70,7 +66,6 @@ static const mozilla::Module::ContractIDEntry kImageContracts[] = {
   { "@mozilla.org/image/encoder;2?type=" IMAGE_JPEG, &kNS_JPEGENCODER_CID },
   { "@mozilla.org/image/encoder;2?type=" IMAGE_PNG, &kNS_PNGENCODER_CID },
   { "@mozilla.org/image/encoder;2?type=" IMAGE_BMP, &kNS_BMPENCODER_CID },
-  { IMAGEBLOCKER_CONTRACTID, &kIMAGEBLOCKER_CID },
   { nullptr }
 };
 
@@ -88,7 +83,6 @@ static const mozilla::Module::CategoryEntry kImageCategories[] = {
   { "Gecko-Content-Viewers", IMAGE_APNG, "@mozilla.org/content/document-loader-factory;1" },
   { "Gecko-Content-Viewers", IMAGE_X_PNG, "@mozilla.org/content/document-loader-factory;1" },
   { "content-sniffing-services", "@mozilla.org/image/loader;1", "@mozilla.org/image/loader;1" },
-  { "content-policy", IMAGEBLOCKER_CONTRACTID, IMAGEBLOCKER_CONTRACTID },
   { nullptr }
 };
 
