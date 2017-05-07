eb054662cc4a214cc66489a1616663f61b7d27cb
Dan Banner <dbugs@thebanners.uk>
2017-05-07 15:27:05 +0100

    Bug 1320156 - Remove leftover Privacy-16.png files. r=florian
    MozReview-Commit-ID: GXCfNNwiqFf
    
    --HG--
    extra : rebase_source : 8c296d23d6fb4fb9883cd05a4378f032dcfb0d8d

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -120,8 +120,6 @@ var whitelist = new Set([
   // Bug 1339424 (wontfix?)
   {file: "chrome://browser/locale/taskbar.properties",
    platforms: ["linux", "macosx"]},
-  // Bug 1320156
-  {file: "chrome://browser/skin/Privacy-16.png", platforms: ["linux"]},
   // Bug 1343584
   {file: "chrome://browser/skin/click-to-play-warning-stripes.png"},
   // Bug 1343824
