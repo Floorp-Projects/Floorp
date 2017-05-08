5b4f00a59efef0b18e2d36531f1f7ab63baedb4a
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-05-08 16:24:32 +0200

    Bug 1361975 Fixing tests in order to support window.location not nullable: fix eslint complaint. r=eslint-fix

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -2417,7 +2417,7 @@ Toolbox.prototype = {
           // defined.
           try {
             win.location.replace("about:blank");
-          } catch(e) {
+          } catch (e) {
             // Do nothing;
           }
 
