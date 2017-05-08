21816ce4f3f2e349a7816844583221c28e960e8e
Tom Tromey <tom@tromey.com>
2017-05-08 13:49:48 -0600

    Bug 1334276 - add Printf unit tests; r=froydnj
    Add unit tests for Printf.  Code coverage now at 90%.  Doing better is
    difficult due to the large number of assertions and failure cases, and
    because some remaining untested code implements behavior undefined by
    the spec, for example "%0s".
    
    MozReview-Commit-ID: FenOur7bOt2
    
    --HG--
    extra : rebase_source : 4a399743e87912d8a72a8607dd520e676785f52d

--- mozglue/misc/Printf.cpp
+++ mozglue/misc/Printf.cpp
@@ -388,6 +388,11 @@ BuildArgArray(const char* fmt, va_list ap, NumArgStateVector& nas)
 
         c = *p++;
 
+        // flags
+        while ((c == '-') || (c == '+') || (c == ' ') || (c == '0')) {
+            c = *p++;
+        }
+
         // width
         if (c == '*') {
             // not supported feature, for the argument is not numbered
@@ -440,10 +445,14 @@ BuildArgArray(const char* fmt, va_list ap, NumArgStateVector& nas)
         case 'd':
         case 'c':
         case 'i':
+            break;
+
         case 'o':
         case 'u':
         case 'x':
         case 'X':
+            // Mark as unsigned type.
+            nas[cn].type |= 1;
             break;
 
         case 'e':
@@ -472,11 +481,9 @@ BuildArgArray(const char* fmt, va_list ap, NumArgStateVector& nas)
                 break;
             }
 #endif
-            if (nas[cn].type == TYPE_INTN) {
-                nas[cn].type = TYPE_STRING;
-            } else {
-                nas[cn].type = TYPE_UNKNOWN;
-            }
+            // Other type sizes are not supported here.
+            MOZ_ASSERT (nas[cn].type == TYPE_INTN);
+            nas[cn].type = TYPE_STRING;
             break;
 
         case 'n':
@@ -500,10 +507,10 @@ BuildArgArray(const char* fmt, va_list ap, NumArgStateVector& nas)
 
     cn = 0;
     while (cn < number) {
-        if (nas[cn].type == TYPE_UNKNOWN) {
-            cn++;
-            continue;
-        }
+        // A TYPE_UNKNOWN here means that the format asked for a
+        // positional argument without specifying the meaning of some
+        // earlier argument.
+        MOZ_ASSERT (nas[cn].type != TYPE_UNKNOWN);
 
         VARARGS_ASSIGN(nas[cn].ap, ap);
 
@@ -629,6 +636,11 @@ mozilla::PrintfTarget::vprint(const char* fmt, va_list ap)
         if (c == '*') {
             c = *fmt++;
             width = va_arg(ap, int);
+            if (width < 0) {
+                width = -width;
+                flags |= FLAG_LEFT;
+                flags &= ~FLAG_ZEROS;
+            }
         } else {
             width = 0;
             while ((c >= '0') && (c <= '9')) {
@@ -817,10 +829,6 @@ mozilla::PrintfTarget::vprint(const char* fmt, va_list ap)
                 if (!cvt_s(u.s, width, prec, flags))
                     return false;
                 break;
-            } else if (type == TYPE_LONGLONG) {
-                // This should have asserted during BuildArgArray anyway.
-                MOZ_ASSERT(0);
-                break;
             }
             MOZ_ASSERT(type == TYPE_LONG);
             MOZ_FALLTHROUGH;
@@ -848,7 +856,7 @@ mozilla::PrintfTarget::vprint(const char* fmt, va_list ap)
                 }
             }
 #else
-            // This should have asserted during BuildArgArray anyway.
+            // Not supported here.
             MOZ_ASSERT(0);
 #endif
             break;
