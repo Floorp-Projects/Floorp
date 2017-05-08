bbff1a16b2049a3a2342953c6cdf6fe38cc68fe9
Jonathan Kew <jkew@mozilla.com>
2017-05-08 12:08:24 +0100

    Bug 1349310 - Update graphite2 to release 1.3.10. r=milan

--- gfx/graphite2/src/FeatureMap.cpp
+++ gfx/graphite2/src/FeatureMap.cpp
@@ -275,7 +275,8 @@ bool FeatureRef::applyValToFeature(uint32 val, Features & pDest) const
     else
       if (pDest.m_pMap!=&m_pFace->theSill().theFeatureMap())
         return false;       //incompatible
-    pDest.reserve(m_index + 1);
+    if (m_index >= pDest.size())
+        pDest.resize(m_index+1);
     pDest[m_index] &= ~m_mask;
     pDest[m_index] |= (uint32(val) << m_bits);
     return true;
