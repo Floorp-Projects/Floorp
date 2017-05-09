8c4cab8374a6dca665619141f80048517188fcc6
Kit Cambridge <kit@yakshaving.ninja>
2017-05-09 13:04:50 -0700

    Bug 1335891 - Ensure and test that Sync changes don't bump the score. r=markh
    MozReview-Commit-ID: 4dP5oZS3pm

--- services/sync/tests/unit/test_places_guid_downgrade.js
+++ services/sync/tests/unit/test_places_guid_downgrade.js
@@ -1,7 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-Cu.import("resource://gre/modules/PlacesUtils.jsm");
 Cu.import("resource://services-common/async.js");
 Cu.import("resource://services-sync/util.js");
 Cu.import("resource://services-sync/engines.js");
