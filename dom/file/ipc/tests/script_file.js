970770ef7208adf458c15fca5eb83a80d01861b0
Andrea Marchesini <amarchesini@mozilla.com>
2017-05-09 11:07:33 +0200

    Bug 1363309 - Fix for an intermittent failure in dom/file/ipc/test/test_ipcBlob_fileReaderSync.html, r=me

--- dom/file/ipc/tests/script_file.js
+++ dom/file/ipc/tests/script_file.js
@@ -6,7 +6,17 @@ addMessageListener("file.open", function (e) {
                    .getService(Ci.nsIDirectoryService)
                    .QueryInterface(Ci.nsIProperties)
                    .get("ProfD", Ci.nsIFile);
-  testFile.append("prefs.js");
+  testFile.append("ipc_fileReader_testing");
+  testFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0o600);
+
+  var outStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+                      .createInstance(Components.interfaces.nsIFileOutputStream);
+  outStream.init(testFile, 0x02 | 0x08 | 0x20, // write, create, truncate
+                 0666, 0);
+
+  var fileData = "Hello World!";
+  outStream.write(fileData, fileData.length);
+  outStream.close();
 
   File.createFromNsIFile(testFile).then(function(file) {
     sendAsyncMessage("file.opened", { file });
