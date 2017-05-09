24e81f8901ce714417fc63285ea26a543972421f
Dão Gottwald <dao@mozilla.com>
2017-05-09 15:56:20 +0200

    Bug 1363118 - Rename browser.shell.skipDefaultBrowserCheck pref for clarity. r=Gijs
    MozReview-Commit-ID: EIfXl15GQqJ
    
    --HG--
    extra : rebase_source : df140a6973fd5ac076d23eb41d54a4a476e3c597

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -1070,7 +1070,7 @@ BrowserGlue.prototype = {
 
       const skipDefaultBrowserCheck =
         Services.prefs.getBoolPref("browser.shell.skipDefaultBrowserCheckOnFirstRun") &&
-        Services.prefs.getBoolPref("browser.shell.skipDefaultBrowserCheck");
+        !Services.prefs.getBoolPref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun");
 
       const usePromptLimit = !AppConstants.RELEASE_OR_BETA;
       let promptCount =
@@ -1104,7 +1104,7 @@ BrowserGlue.prototype = {
       // browser has been run a few times.
       if (willPrompt) {
         if (skipDefaultBrowserCheck) {
-          Services.prefs.setBoolPref("browser.shell.skipDefaultBrowserCheck", false);
+          Services.prefs.setBoolPref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun", true);
           willPrompt = false;
         } else {
           promptCount++;
@@ -1667,7 +1667,7 @@ BrowserGlue.prototype = {
   },
 
   _migrateUI: function BG__migrateUI() {
-    const UI_VERSION = 44;
+    const UI_VERSION = 45;
     const BROWSER_DOCURL = "chrome://browser/content/browser.xul";
 
     let currentUIVersion;
@@ -1959,6 +1959,13 @@ BrowserGlue.prototype = {
       Services.prefs.clearUserPref("browser.tabs.animate");
       Services.prefs.clearUserPref("browser.fullscreen.animate");
       Services.prefs.clearUserPref("alerts.disableSlidingEffect");
+    }
+
+    if (currentUIVersion < 45) {
+      const LEGACY_PREF = "browser.shell.skipDefaultBrowserCheck";
+      Services.prefs.setBoolPref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun",
+                                 !Services.prefs.getBoolPref(LEGACY_PREF));
+      Services.prefs.clearUserPref(LEGACY_PREF);
     }
 
     // Update the migration version.
