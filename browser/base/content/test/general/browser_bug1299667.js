bb5a155daa196e1880e1e6ae6236e066e2aa6bc8
Dão Gottwald <dao@mozilla.com>
2017-05-11 00:24:37 +0200

    Bug 1363902 - Simplify some browser chrome tests by utilizing TestUtils.topicObserved. r=mikedeboer
    MozReview-Commit-ID: Flc8iE19Gkt
    
    --HG--
    extra : rebase_source : ece0308fbf7b8a24abd44ca9b8e3745abdf1cd17

--- browser/base/content/test/general/browser_bug1299667.js
+++ browser/base/content/test/general/browser_bug1299667.js
@@ -1,26 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-const { addObserver, removeObserver } = Cc["@mozilla.org/observer-service;1"].
-                                          getService(Ci.nsIObserverService);
-
-function receive(topic) {
-  return new Promise((resolve, reject) => {
-    let timeout = setTimeout(() => {
-      reject(new Error("Timeout"));
-    }, 90000);
-
-    const observer = {
-      observe: subject => {
-        removeObserver(observer, topic);
-        clearTimeout(timeout);
-        resolve(subject);
-      }
-    };
-    addObserver(observer, topic, false);
-  });
-}
-
 add_task(function* () {
   yield BrowserTestUtils.openNewForegroundTab(gBrowser, "http://example.com");
 
@@ -28,7 +8,7 @@ add_task(function* () {
     content.history.pushState({}, "2", "2.html");
   });
 
-  yield receive("sessionstore-state-write-complete");
+  yield TestUtils.topicObserved("sessionstore-state-write-complete");
 
   // Wait for the session data to be flushed before continuing the test
   yield new Promise(resolve => SessionStore.getSessionHistory(gBrowser.selectedTab, resolve));
