409c95124c7f39cb8450f6a86e3fad4d4d4986e4
Birunthan Mohanathas <birunthan@mohanathas.com>
2017-05-10 20:50:00 +0300

    Bug 903966 - Stop blocking 'http://127.0.0.1/' as mixed content. r=ckerschb,kmckinley
    According to the spec, content from loopback addresses should no longer
    be treated as mixed content even in secure origins. See:
    - https://github.com/w3c/webappsec-mixed-content/commit/349501cdaa4b4dc1e2a8aacb216ced58fd316165
    - https://w3c.github.io/webappsec-secure-contexts/#is-origin-trustworthy
    
    Note that we only whitelist '127.0.0.1' and '::1' to match Chrome 53 and
    later. See:
    - https://chromium.googlesource.com/chromium/src.git/+/130ee686fa00b617bfc001ceb3bb49782da2cb4e
    
    It is unclear if HTTPS origins should be able to use workers and WebSocket
    connections through a loopback HTTP address. They are not supported in Chrome
    (whether this is intentional or not is uncertain) so lets just ignore them for
    now.
    
    See also: https://github.com/w3c/web-platform-tests/pull/5304

--- dom/security/test/hsts/browser_hsts-priming_no-ip-address.js
+++ dom/security/test/hsts/browser_hsts-priming_no-ip-address.js
@@ -14,7 +14,7 @@ add_task(function*() {
 
   // add the top-level server
   test_servers['localhost-ip'] = {
-    host: '127.0.0.1',
+    host: '127.0.0.2',
     response: true,
     id: 'localhost-ip',
   };
