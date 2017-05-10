85e7d31eb40a898af9f8a9954a284c486ca7cab8
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-05-10 12:11:11 +0200

    Backed out changeset 9483bb6d8ce6 (bug 1330570) for test failures in test_selection_move_commands.html | cmd_scrollBottom - -300 should equal -300

--- layout/style/ImageLoader.cpp
+++ layout/style/ImageLoader.cpp
@@ -315,7 +315,7 @@ ImageLoader::GetPresContext()
 }
 
 void InvalidateImagesCallback(nsIFrame* aFrame, 
-                              DisplayItemData* aItem)
+                              FrameLayerBuilder::DisplayItemData* aItem)
 {
   nsDisplayItem::Type type = nsDisplayItem::GetDisplayItemTypeFromKey(aItem->GetDisplayItemKey());
   uint8_t flags = nsDisplayItem::GetDisplayItemFlagsForType(type);
