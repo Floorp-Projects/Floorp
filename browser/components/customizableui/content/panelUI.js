9f8e4284c621871b88fdaed5f1b2f55f2f10220b
Gijs <gijskruitbosch@gmail.com>
2017-05-10 12:36:05 -0400

    Bug 1354094 - show the overflow button when there are items in the permanent overflow menu, r=mikedeboer
    This also disables the menu button and overflow button correctly in
    customize mode, and fixes an issues where the overflow button state
    would stay 'open' when closing the panel by clicking the button
    a second time.
    
    MozReview-Commit-ID: BNTkE6zvv9Q

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -40,6 +40,7 @@ const PanelUI = {
       addonNotificationContainer: gPhotonStructure ? "appMenu-addon-banners" : "PanelUI-footer-addons",
 
       overflowFixedList: gPhotonStructure ? "widget-overflow-fixed-list" : "",
+      navbar: "nav-bar",
     };
   },
 
@@ -85,6 +86,10 @@ const PanelUI = {
       this.overflowFixedList.hidden = false;
       this.overflowFixedList.nextSibling.hidden = false;
       CustomizableUI.registerMenuPanel(this.overflowFixedList, CustomizableUI.AREA_FIXED_OVERFLOW_PANEL);
+      this.navbar.setAttribute("photon-structure", "true");
+      this.updateOverflowStatus();
+    } else {
+      this.navbar.removeAttribute("photon-structure");
     }
   },
 
@@ -596,7 +601,20 @@ const PanelUI = {
     this.reinit();
   },
 
+  updateOverflowStatus() {
+    let hasKids = this.overflowFixedList.hasChildNodes();
+    if (hasKids && !this.navbar.hasAttribute("nonemptyoverflow")) {
+      this.navbar.setAttribute("nonemptyoverflow", "true");
+    } else if (!hasKids && this.navbar.hasAttribute("nonemptyoverflow")) {
+      this.navbar.removeAttribute("nonemptyoverflow");
+    }
+  },
+
   onWidgetAfterDOMChange(aNode, aNextNode, aContainer, aWasRemoval) {
+    if (gPhotonStructure && aContainer == this.overflowFixedList) {
+      this.updateOverflowStatus();
+      return;
+    }
     if (aContainer != this.contents) {
       return;
     }
