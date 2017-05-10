bcfc1df746b10bca0b86c0c4c9aa55b0a673de3b
Valentin Gosu <valentin.gosu@gmail.com>
2017-05-10 19:23:54 +0200

    Bug 1363700 - Add RCWN stats to about:networking r=bz,michal
    MozReview-Commit-ID: GVt1omCfL6t

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -7249,6 +7249,13 @@ nsHttpChannel::OnStopRequest(nsIRequest *request, nsISupports *ctxt, nsresult st
         }
     }
 
+    gIOService->IncrementRequestNumber();
+    if (rcwnStatus == kRaceUsedCache) {
+        gIOService->IncrementCacheWonRequestNumber();
+    } else if (rcwnStatus == kRaceUsedNetwork) {
+        gIOService->IncrementNetWonRequestNumber();
+    }
+
     // Register entry to the Performance resource timing
     mozilla::dom::Performance* documentPerformance = GetPerformance();
     if (documentPerformance) {
