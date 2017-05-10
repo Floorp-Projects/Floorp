20ac00c192dd674728118c158321a408952d6d6a
Lee Salzman <lsalzman@mozilla.com>
2017-05-09 22:15:18 -0400

    Bug 1350262 - implement prime rehash policy compat for unordered_map and unordered_set in libstdc++. r=glandium
    MozReview-Commit-ID: 1zlGjRMKcBM

--- build/unix/stdc++compat/stdc++compat.cpp
+++ build/unix/stdc++compat/stdc++compat.cpp
@@ -32,6 +32,36 @@ libstdc++ 4.6.1.
 
 #define GLIBCXX_VERSION(a, b, c) (((a) << 16) | ((b) << 8) | (c))
 
+#if MOZ_LIBSTDCXX_VERSION >= GLIBCXX_VERSION(3, 4, 18)
+// Implementation of utility functions for the prime rehash policy used in
+// unordered_map and unordered_set.
+#include <unordered_map>
+#include <tr1/unordered_map>
+namespace std
+{
+  size_t
+  __detail::_Prime_rehash_policy::_M_next_bkt(size_t __n) const
+  {
+    tr1::__detail::_Prime_rehash_policy policy(_M_max_load_factor);
+    size_t ret = policy._M_next_bkt(__n);
+    _M_next_resize = policy._M_next_resize;
+    return ret;
+  }
+
+  pair<bool, size_t>
+  __detail::_Prime_rehash_policy::_M_need_rehash(size_t __n_bkt,
+                                                 size_t __n_elt,
+                                                 size_t __n_ins) const
+  {
+    tr1::__detail::_Prime_rehash_policy policy(_M_max_load_factor);
+    policy._M_next_resize = _M_next_resize;
+    pair<bool, size_t> ret = policy._M_need_rehash(__n_bkt, __n_elt, __n_ins);
+    _M_next_resize = policy._M_next_resize;
+    return ret;
+  }
+}
+#endif
+
 #if MOZ_LIBSTDCXX_VERSION >= GLIBCXX_VERSION(3, 4, 20)
 namespace std {
 
