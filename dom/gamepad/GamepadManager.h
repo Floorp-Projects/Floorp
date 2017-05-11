dd60d0ea4ba585c8876b70ee3630ecbeb7cd6d22
Daosheng Mu <daoshengmu@gmail.com>
2017-05-10 20:30:00 -0400

    Bug 1358885 - Check VRManagerChild is created in GampadManager for avoiding crash on Android. r=kip, a=gchang
    MozReview-Commit-ID: I0fimVQKAHy

--- dom/gamepad/GamepadManager.h
+++ dom/gamepad/GamepadManager.h
@@ -124,7 +124,6 @@ class GamepadManager final : public nsIObserver,
   // will be destroyed during the IPDL shutdown chain, so we
   // don't need to refcount it here.
   nsTArray<GamepadEventChannelChild *> mChannelChildren;
-  gfx::VRManagerChild* mVRChannelChild;
 
  private:
 
