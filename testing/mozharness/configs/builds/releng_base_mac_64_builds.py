eb29c5796540cc9553f98d20f425c70c48879157
Gregory Szorc <gps@mozilla.com>
2017-05-11 12:49:09 -0700

    Bug 1362164 - Remove mozharness code to print ccache stats; r=ted
    I was going to inline "enable_ccache" into buildbase.py because AFAICT
    its value is effectively "if not windows." However, I realized that
    all this is doing is dumping ccache stats (something the build system
    itself is in a better position to do because it actually knows if
    ccache is enabled). Then I realized we don't use ccache directly
    any more in automation because we use sccache (or at least that's the
    way it should be).
    
    Since there's no need for this ccache code in mozharness, this commit
    deletes it. If we want it back, we can add the functionality to
    `mach build`.
    
    MozReview-Commit-ID: BrRi1QKe5l3

--- testing/mozharness/configs/builds/releng_base_mac_64_builds.py
+++ testing/mozharness/configs/builds/releng_base_mac_64_builds.py
@@ -24,7 +24,6 @@ config = {
     'app_ini_path': '%(obj_dir)s/dist/bin/application.ini',
     # decides whether we want to use moz_sign_cmd in env
     'enable_signing': True,
-    'enable_ccache': True,
     'vcs_share_base': '/builds/hg-shared',
     'objdir': 'obj-firefox',
     'tooltool_script': ["/builds/tooltool.py"],
