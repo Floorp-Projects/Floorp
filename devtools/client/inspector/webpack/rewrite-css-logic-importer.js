789ccba9a234aa8f685df124f918029db210730e
Julian Descottes <jdescottes@mozilla.com>
2017-05-11 23:43:01 +0200

    Bug 1365014 - inspector.html add shims for sdk, uuid, lazy requires;r=gl
    MozReview-Commit-ID: CtYnDV4V0HU

---
+++ devtools/client/inspector/webpack/rewrite-css-logic-importer.js
@@ -0,0 +1,14 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+module.exports = function (content) {
+  this.cacheable && this.cacheable();
+
+  return content.replace(
+    "loader.lazyImporter(this, \"findCssSelector\", \"resource://gre/modules/css-selector.js\");",
+    "let findCssSelector = function () {};"
+  );
+};
