430820b15b1e11f900e9994747566e07ecbb4232
Tom Tromey <tom@tromey.com>
2017-05-12 14:34:26 -0600

    Bug 1333352 - use client-side source map service in markup view event bubble; r=jdescottes
    MozReview-Commit-ID: D8bF5kkHp2p

---
+++ devtools/client/inspector/markup/test/browser_markup_events_source_map.js
@@ -0,0 +1,58 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check that source maps work in the event popup.
+
+const INITIAL_URL = URL_ROOT + "doc_markup_void_elements.html";
+const TEST_URL = URL_ROOT + "doc_markup_events-source_map.html";
+
+/* import-globals-from helper_events_test_runner.js */
+loadHelperScript("helper_events_test_runner.js");
+
+const TEST_DATA = [
+  {
+    selector: "#clicky",
+    isSourceMapped: true,
+    expected: [
+      {
+        type: "click",
+        filename: "webpack:///events_original.js:7",
+        attributes: [
+          "Bubbling",
+          "DOM2"
+        ],
+        handler: `function clickme() {
+  console.log("clickme");
+}`,
+      },
+    ],
+  },
+];
+
+add_task(function* () {
+  // Load some other URL before opening the toolbox, then navigate to
+  // the test URL.  This ensures that source map service will see the
+  // sources as they are loaded, avoiding any races.
+  let {toolbox, inspector, testActor} = yield openInspectorForURL(INITIAL_URL);
+
+  // Ensure the source map service is operating.  This looks a bit
+  // funny, but sourceMapURLService is a getter, and we don't need the
+  // result.
+  toolbox.sourceMapURLService;
+
+  yield navigateTo(inspector, TEST_URL);
+
+  yield inspector.markup.expandAll();
+
+  for (let test of TEST_DATA) {
+    yield checkEventsForNode(test, inspector, testActor);
+  }
+
+  // Wait for promises to avoid leaks when running this as a single test.
+  // We need to do this because we have opened a bunch of popups and don't them
+  // to affect other test runs when they are GCd.
+  yield promiseNextTick();
+});
