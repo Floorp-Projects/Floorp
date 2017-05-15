491ab295f08ca82a027cbbb8d06ab7b12ae4b25a
John Lin <jolin@mozilla.com>
2017-05-15 16:43:49 +0800

    bug 1364341 - part 1: release all resources used by existing codec when re-configure. r=jya
    MozReview-Commit-ID: JLZ0vSBTg6X

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/Codec.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/Codec.java
@@ -360,7 +360,7 @@ import org.mozilla.gecko.gfx.GeckoSurface;
 
         if (mCodec != null) {
             if (DEBUG) { Log.d(LOGTAG, "release existing codec: " + mCodec); }
-            releaseCodec();
+            mCodec.release();
         }
 
         if (DEBUG) { Log.d(LOGTAG, "configure " + this); }
@@ -405,19 +405,6 @@ import org.mozilla.gecko.gfx.GeckoSurface;
     @Override
     public synchronized boolean isAdaptivePlaybackSupported() {
         return mIsAdaptivePlaybackSupported;
-    }
-
-    private void releaseCodec() {
-        try {
-            // In case Codec.stop() is not called yet.
-            mInputProcessor.stop();
-            mOutputProcessor.stop();
-
-            mCodec.release();
-        } catch (Exception e) {
-            reportError(Error.FATAL, e);
-        }
-        mCodec = null;
     }
 
     private AsyncCodec createCodecForFormat(MediaFormat format, boolean isEncoder) throws IOException {
@@ -543,7 +530,15 @@ import org.mozilla.gecko.gfx.GeckoSurface;
     @Override
     public synchronized void release() throws RemoteException {
         if (DEBUG) { Log.d(LOGTAG, "release " + this); }
-        releaseCodec();
+        try {
+            // In case Codec.stop() is not called yet.
+            mInputProcessor.stop();
+            mOutputProcessor.stop();
+            mCodec.release();
+        } catch (Exception e) {
+            reportError(Error.FATAL, e);
+        }
+        mCodec = null;
         mSamplePool.reset();
         mSamplePool = null;
         mCallbacks.asBinder().unlinkToDeath(this, 0);
