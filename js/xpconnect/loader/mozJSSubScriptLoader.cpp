204e251abe9e88695c0e33f3013063da51042584
Andrew McCreight <continuation@gmail.com>
2017-05-15 15:04:45 -0700

    Bug 989373, part 4 - Remove reuse global flag from subscript loader. r=kmag
    FindTargetObject in DoLoadSubScriptWithOptions will always return a
    global object, so the boolean values we pass around to determine if a
    global was being reused will always be false. The next patch will
    eliminate the various |function| arguments that are now unused.
    
    MozReview-Commit-ID: GvPNFGluRub

--- js/xpconnect/loader/mozJSSubScriptLoader.cpp
+++ js/xpconnect/loader/mozJSSubScriptLoader.cpp
@@ -132,15 +132,12 @@ PrepareScript(nsIURI* uri,
               const nsAString& charset,
               const char* buf,
               int64_t len,
-              bool reuseGlobal,
               bool wantReturnValue,
               MutableHandleScript script,
               MutableHandleFunction function)
 {
     JS::CompileOptions options(cx);
-    // Use line 0 to make the function body starts from line 1 when
-    // |reuseGlobal == true|.
-    options.setFileAndLine(uriStr, reuseGlobal ? 0 : 1)
+    options.setFileAndLine(uriStr, 1)
            .setVersion(JSVERSION_LATEST)
            .setNoScriptRval(!wantReturnValue);
     if (!charset.IsVoid()) {
@@ -159,34 +156,18 @@ PrepareScript(nsIURI* uri,
             return false;
         }
 
-        if (!reuseGlobal) {
-            if (JS_IsGlobalObject(targetObj)) {
-                return JS::Compile(cx, options, srcBuf, script);
-            }
-            return JS::CompileForNonSyntacticScope(cx, options, srcBuf, script);
+        if (JS_IsGlobalObject(targetObj)) {
+            return JS::Compile(cx, options, srcBuf, script);
         }
-        AutoObjectVector envChain(cx);
-        if (!JS_IsGlobalObject(targetObj) && !envChain.append(targetObj)) {
-            return false;
-        }
-        return JS::CompileFunction(cx, envChain, options, nullptr, 0, nullptr,
-                                   srcBuf, function);
+        return JS::CompileForNonSyntacticScope(cx, options, srcBuf, script);
     }
     // We only use lazy source when no special encoding is specified because
     // the lazy source loader doesn't know the encoding.
-    if (!reuseGlobal) {
-        options.setSourceIsLazy(true);
-        if (JS_IsGlobalObject(targetObj)) {
-            return JS::Compile(cx, options, buf, len, script);
-        }
-        return JS::CompileForNonSyntacticScope(cx, options, buf, len, script);
+    options.setSourceIsLazy(true);
+    if (JS_IsGlobalObject(targetObj)) {
+        return JS::Compile(cx, options, buf, len, script);
     }
-    AutoObjectVector envChain(cx);
-    if (!JS_IsGlobalObject(targetObj) && !envChain.append(targetObj)) {
-        return false;
-    }
-    return JS::CompileFunction(cx, envChain, options, nullptr, 0, nullptr,
-                               buf, len, function);
+    return JS::CompileForNonSyntacticScope(cx, options, buf, len, script);
 }
 
 static bool
@@ -270,14 +251,13 @@ public:
 
     NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(AsyncScriptLoader)
 
-    AsyncScriptLoader(nsIChannel* aChannel, bool aReuseGlobal, bool aWantReturnValue,
+    AsyncScriptLoader(nsIChannel* aChannel, bool aWantReturnValue,
                       JSObject* aTargetObj, const nsAString& aCharset,
                       bool aCache, Promise* aPromise)
         : mChannel(aChannel)
         , mTargetObj(aTargetObj)
         , mPromise(aPromise)
         , mCharset(aCharset)
-        , mReuseGlobal(aReuseGlobal)
         , mWantReturnValue(aWantReturnValue)
         , mCache(aCache)
     {
@@ -294,7 +274,6 @@ private:
     Heap<JSObject*> mTargetObj;
     RefPtr<Promise> mPromise;
     nsString mCharset;
-    bool mReuseGlobal;
     bool mWantReturnValue;
     bool mCache;
 };
@@ -404,7 +383,7 @@ AsyncScriptLoader::OnStreamComplete(nsIIncrementalStreamLoader* aLoader,
 
     if (!PrepareScript(uri, cx, targetObj, spec.get(), mCharset,
                        reinterpret_cast<const char*>(aBuf), aLength,
-                       mReuseGlobal, mWantReturnValue, &script, &function))
+                       mWantReturnValue, &script, &function))
     {
         return NS_OK;
     }
@@ -424,7 +403,6 @@ mozJSSubScriptLoader::ReadScriptAsync(nsIURI* uri,
                                       HandleObject targetObj,
                                       const nsAString& charset,
                                       nsIIOService* serv,
-                                      bool reuseGlobal,
                                       bool wantReturnValue,
                                       bool cache,
                                       MutableHandleValue retval)
@@ -467,7 +445,6 @@ mozJSSubScriptLoader::ReadScriptAsync(nsIURI* uri,
 
     RefPtr<AsyncScriptLoader> loadObserver =
         new AsyncScriptLoader(channel,
-                              reuseGlobal,
                               wantReturnValue,
                               targetObj,
                               charset,
@@ -489,7 +466,6 @@ mozJSSubScriptLoader::ReadScript(nsIURI* uri,
                                  const nsAString& charset,
                                  const char* uriStr,
                                  nsIIOService* serv,
-                                 bool reuseGlobal,
                                  bool wantReturnValue,
                                  MutableHandleScript script,
                                  MutableHandleFunction function)
@@ -540,8 +516,7 @@ mozJSSubScriptLoader::ReadScript(nsIURI* uri,
     NS_ENSURE_SUCCESS(rv, false);
 
     return PrepareScript(uri, cx, targetObj, uriStr, charset,
-                         buf.get(), len,
-                         reuseGlobal, wantReturnValue,
+                         buf.get(), len, wantReturnValue,
                          script, function);
 }
 
@@ -605,15 +580,13 @@ mozJSSubScriptLoader::DoLoadSubScriptWithOptions(const nsAString& url,
     }
 
     RootedObject targetObj(cx);
-    mozJSComponentLoader* loader = mozJSComponentLoader::Get();
-    loader->FindTargetObject(cx, &targetObj);
-
-    // We base reusingGlobal off of what the loader told us, but we may not
-    // actually be using that object.
-    bool reusingGlobal = !JS_IsGlobalObject(targetObj);
-
-    if (options.target)
+    if (options.target) {
         targetObj = options.target;
+    } else {
+        mozJSComponentLoader* loader = mozJSComponentLoader::Get();
+        loader->FindTargetObject(cx, &targetObj);
+        MOZ_ASSERT(JS_IsGlobalObject(targetObj));
+    }
 
     // Remember an object out of the calling compartment so that we
     // can properly wrap the result later.
@@ -710,20 +683,18 @@ mozJSSubScriptLoader::DoLoadSubScriptWithOptions(const nsAString& url,
     // If we are doing an async load, trigger it and bail out.
     if (!script && options.async) {
         return ReadScriptAsync(uri, targetObj, options.charset, serv,
-                               reusingGlobal, options.wantReturnValue,
-                               !!cache, retval);
+                               options.wantReturnValue, !!cache, retval);
     }
 
-    if (!script) {
-        if (!ReadScript(uri, cx, targetObj, options.charset,
-                        static_cast<const char*>(uriStr.get()), serv,
-                        reusingGlobal, options.wantReturnValue, &script,
-                        &function))
-        {
-            return NS_OK;
-        }
-    } else {
+    if (script) {
+        // |script| came from the cache, so don't bother writing it
+        // |back there.
         cache = nullptr;
+    } else if (!ReadScript(uri, cx, targetObj, options.charset,
+                           static_cast<const char*>(uriStr.get()), serv,
+                           options.wantReturnValue, &script,
+                           &function)) {
+        return NS_OK;
     }
 
     Unused << EvalScript(cx, targetObj, retval, uri, !!cache,
