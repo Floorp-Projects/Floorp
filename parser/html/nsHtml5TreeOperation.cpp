118fc52e41ca3d86b37b2fb7d32509b392b93269
Henri Sivonen <hsivonen@hsivonen.fi>
2017-05-15 15:54:54 +0300

    Bug 1333333 - Label runnables in the HTML parser. r=billm
    MozReview-Commit-ID: 6ItIgFSSR2s
    
    --HG--
    extra : rebase_source : b7451779a0e2ab8fd427a7a9ab1fb4cb8c0801e4

--- parser/html/nsHtml5TreeOperation.cpp
+++ parser/html/nsHtml5TreeOperation.cpp
@@ -615,9 +615,8 @@ void
 nsHtml5TreeOperation::SvgLoad(nsIContent* aNode)
 {
   nsCOMPtr<nsIRunnable> event = new nsHtml5SVGLoadDispatcher(aNode);
-  if (NS_FAILED(aNode->OwnerDoc()->Dispatch("nsHtml5SVGLoadDispatcher",
-                                            TaskCategory::Other,
-                                            event.forget()))) {
+  if (NS_FAILED(aNode->OwnerDoc()->Dispatch(
+        "nsHtml5SVGLoadDispatcher", TaskCategory::Network, event.forget()))) {
     NS_WARNING("failed to dispatch svg load dispatcher");
   }
 }
