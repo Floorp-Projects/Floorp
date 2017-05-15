178c00696040b770d978d10f0a254f5b15cfd161
Dustin J. Mitchell <dustin@mozilla.com>
2017-05-15 13:32:15 +0000

    Bug 1364887: don't run SETA on try pushes; r=jmaher
    MozReview-Commit-ID: 7L2I3WWziSE

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -771,7 +771,6 @@ def make_job_description(config, tests):
         jobdesc['run-on-projects'] = test['run-on-projects']
         jobdesc['scopes'] = []
         jobdesc['tags'] = test.get('tags', {})
-        jobdesc['optimizations'] = [['seta']]  # always run SETA for tests
         jobdesc['extra'] = {
             'chunks': {
                 'current': test['this-chunk'],
@@ -788,6 +787,12 @@ def make_job_description(config, tests):
             'tier': test['tier'],
             'platform': test.get('treeherder-machine-platform', test['build-platform']),
         }
+
+        # run SETA unless this is a try push
+        jobdesc['optimizations'] = optimizations = []
+        if config['project'] != 'try':
+            optimizations.append(['seta'])
+
         run = jobdesc['run'] = {}
         run['using'] = 'mozharness-test'
         run['test'] = test
