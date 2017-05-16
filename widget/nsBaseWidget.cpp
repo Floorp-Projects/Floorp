21787fe5e6735d9478d27c89189bb4a4c507d4bf
Kartikaya Gupta <kgupta@mozilla.com>
2017-05-16 09:43:36 -0400

    Bug 1364987 - Remove the pref to allow APZ with webrender. r=rhunt
    People seem to be turning on this pref in the wild and generating crash reports
    which are wasting time and energy of people who have better things to do.
    
    MozReview-Commit-ID: 5VfjKaDluYJ

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -1255,9 +1255,8 @@ void nsBaseWidget::CreateCompositor(int aWidth, int aHeight)
 
   bool enableWR = gfx::gfxVars::UseWebRender();
   bool enableAPZ = UseAPZ();
-  if (enableWR && !gfxPrefs::APZAllowWithWebRender()) {
-    // Disable APZ on widgets using WebRender, since it doesn't work yet. Allow
-    // it on non-WR widgets or if the pref forces it on.
+  if (enableWR) {
+    // Disable APZ on widgets using WebRender, since it doesn't work yet.
     enableAPZ = false;
   }
   CompositorOptions options(enableAPZ, enableWR);
