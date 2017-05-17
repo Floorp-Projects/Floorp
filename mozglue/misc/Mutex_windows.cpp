2f24c4f229039dbbe2d8a93a2b760a3a06cc1d0e
Eric Rahm <erahm@mozilla.com>
2017-05-17 15:44:20 -0700

    Bug 1364624 - Switch from CRITICALSECTION to SRWLOCK. r=froydnj
    MozReview-Commit-ID: 6JpGEQyUFz

--- mozglue/misc/Mutex_windows.cpp
+++ mozglue/misc/Mutex_windows.cpp
@@ -14,38 +14,23 @@
 
 mozilla::detail::MutexImpl::MutexImpl()
 {
-  // This number was adopted from NSPR.
-  const static DWORD LockSpinCount = 1500;
-
-#if defined(RELEASE_OR_BETA)
-  // Vista and later automatically allocate and subsequently leak a debug info
-  // object for each critical section that we allocate unless we tell the
-  // system not to do that.
-  DWORD flags = CRITICAL_SECTION_NO_DEBUG_INFO;
-#else
-  DWORD flags = 0;
-#endif // defined(RELEASE_OR_BETA)
-
-  BOOL r = InitializeCriticalSectionEx(&platformData()->criticalSection,
-                                       LockSpinCount, flags);
-  MOZ_RELEASE_ASSERT(r);
+  InitializeSRWLock(&platformData()->lock);
 }
 
 mozilla::detail::MutexImpl::~MutexImpl()
 {
-  DeleteCriticalSection(&platformData()->criticalSection);
 }
 
 void
 mozilla::detail::MutexImpl::lock()
 {
-  EnterCriticalSection(&platformData()->criticalSection);
+  AcquireSRWLockExclusive(&platformData()->lock);
 }
 
 void
 mozilla::detail::MutexImpl::unlock()
 {
-  LeaveCriticalSection(&platformData()->criticalSection);
+  ReleaseSRWLockExclusive(&platformData()->lock);
 }
 
 mozilla::detail::MutexImpl::PlatformData*
