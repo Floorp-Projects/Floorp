ef78119ea606cacaec238824e8f3ca62c33c3985
Chris Pearce <cpearce@mozilla.com>
2017-05-17 17:10:14 +1200

    Bug 1355252 - Reject navigator.requestMediaKeySystemAccess if no MediaCapabilities supplied. r=jwwang
    As required by step 15 of EME's Get Supported Configuration and Consent algorithm,
    https://www.w3.org/TR/encrypted-media/#get-supported-configuration-and-consent
    
    MozReview-Commit-ID: 8c24VrmygBx

--- dom/media/eme/MediaKeySystemAccess.cpp
+++ dom/media/eme/MediaKeySystemAccess.cpp
@@ -926,8 +926,13 @@ GetSupportedConfig(const KeySystemConfig& aKeySystem,
 
   // If the videoCapabilities and audioCapabilities members in candidate
   // configuration are both empty, return NotSupported.
-  // TODO: Most sites using EME still don't pass capabilities, so we
-  // can't reject on it yet without breaking them. So add this later.
+  if (aCandidate.mAudioCapabilities.IsEmpty() &&
+      aCandidate.mVideoCapabilities.IsEmpty()) {
+    EME_LOG("MediaKeySystemConfiguration (label='%s') rejected; "
+            "no supported audio or video capabilities specified",
+            NS_ConvertUTF16toUTF8(aCandidate.mLabel).get());
+    return false;
+  }
 
   // If the videoCapabilities member in candidate configuration is non-empty:
   if (!aCandidate.mVideoCapabilities.IsEmpty()) {
