e66382f6014dd057fd0e16b13abe9ae23d05cade
Jim Chen <nchen@mozilla.com>
2017-05-17 13:06:21 -0400

    Bug 1360321 - 3. Don't use mmap2 on Android AArch64 in mozjemalloc; r=glandium
    Android NDK defines SYS_mmap2 but it expands to a nonexistent symbol.
    mmap2 may not be supported in any case in some AArch64 kernels, so we
    should just use mmap.
    
    MozReview-Commit-ID: 5Vjuja5fLIL

--- memory/mozjemalloc/jemalloc.c
+++ memory/mozjemalloc/jemalloc.c
@@ -341,6 +341,10 @@ void *_mmap(void *addr, size_t length, int prot, int flags,
 	} args = { addr, length, prot, flags, fd, offset };
 	return (void *) syscall(SYS_mmap, &args);
 #else
+#if defined(MOZ_MEMORY_ANDROID) && defined(__aarch64__) && defined(SYS_mmap2)
+/* Android NDK defines SYS_mmap2 for AArch64 despite it not supporting mmap2 */
+#undef SYS_mmap2
+#endif
 #ifdef SYS_mmap2
 	return (void *) syscall(SYS_mmap2, addr, length, prot, flags,
 	                       fd, offset >> 12);
