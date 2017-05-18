0f5cee1615ec5b867a0b4c9ca6c2030614e39426
Jordan Lund <jlund@mozilla.com>
2017-05-17 17:24:01 -0700

    Bug 1365588 - fix l10n repacks for DevEdition on Beta, gecko, r=rail a=release
    MozReview-Commit-ID: 1arksrFhsv9

---
+++ testing/mozharness/configs/single_locale/mozilla-beta_devedition.py
@@ -0,0 +1,37 @@
+config = {
+    "nightly_build": True,
+    "branch": "mozilla-beta",
+    "en_us_binary_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-beta/",
+    "update_channel": "aurora",  # devedition uses aurora based branding
+
+    # l10n
+    "hg_l10n_base": "https://hg.mozilla.org/releases/l10n/mozilla-beta",
+
+    # repositories
+    "mozilla_dir": "mozilla-beta",
+    "repos": [{
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/build/tools",
+        "branch": "default",
+        "dest": "tools",
+    }, {
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/releases/mozilla-beta",
+        "revision": "%(revision)s",
+        "dest": "mozilla-beta",
+        "clone_upstream_url": "https://hg.mozilla.org/mozilla-unified",
+    }],
+    # purge options
+    'purge_minsize': 12,
+    'is_automation': True,
+    'default_actions': [
+        "clobber",
+        "pull",
+        "clone-locales",
+        "list-locales",
+        "setup",
+        "repack",
+        "taskcluster-upload",
+        "summary",
+    ],
+}
