763a6bb5b1decaba3f507f57a4692dc63725931f
Andrew Swan <aswan@mozilla.com>
2017-05-18 13:08:58 -0700

    Bug 1358907 Part 3 Avoid reading XPI database at startup r=Dexter,kmag
    Switch telemetry and experiments from AddonManager.getAddonsByTypes()
    to AddonManager.getActiveAddons() which gives us less detailed information
    in the environment during startup but also means we don't need to load the
    extensions database until startup is complete.
    
    MozReview-Commit-ID: 4SxdPHSPovB
    
    --HG--
    extra : rebase_source : da99f2692ee57e66d65c6b1c1a84412088cc7b28
    extra : source : c391410c07ed5a9462ca7dec8c01037c9ab14466

--- toolkit/components/telemetry/tests/unit/test_ChildEvents.js
+++ toolkit/components/telemetry/tests/unit/test_ChildEvents.js
@@ -66,6 +66,7 @@ add_task(async function() {
   // Setup.
   do_get_profile(true);
   loadAddonManager(APP_ID, APP_NAME, APP_VERSION, PLATFORM_VERSION);
+  finishAddonManagerStartup();
   Services.prefs.setBoolPref(TelemetryUtils.Preferences.TelemetryEnabled, true);
   await TelemetryController.testSetup();
   // Make sure we don't generate unexpected pings due to pref changes.
