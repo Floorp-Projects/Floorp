525e8fb57a236d816276e62faa79d229c8ae888c
btian <btian@mozilla.com>
2017-05-18 09:31:00 +0800

    Bug 1299390 - part 3: Remove BrowserElement methods required by BrowserElementAudioChannel only. r=alwu,baku
    MozReview-Commit-ID: LAyGMvmmZpz

--- dom/browser-element/BrowserElementParent.js
+++ dom/browser-element/BrowserElementParent.js
@@ -168,8 +168,6 @@ BrowserElementParent.prototype = {
       "got-contentdimensions": this._gotDOMRequestResult,
       "got-can-go-back": this._gotDOMRequestResult,
       "got-can-go-forward": this._gotDOMRequestResult,
-      "got-muted": this._gotDOMRequestResult,
-      "got-volume": this._gotDOMRequestResult,
       "requested-dom-fullscreen": this._requestedDOMFullscreen,
       "fullscreen-origin-change": this._fullscreenOriginChange,
       "exit-dom-fullscreen": this._exitDomFullscreen,
@@ -622,22 +620,6 @@ BrowserElementParent.prototype = {
 
   clearMatch: defineNoReturnMethod(function() {
     return this._sendAsyncMsg('clear-match');
-  }),
-
-  mute: defineNoReturnMethod(function() {
-    this._sendAsyncMsg('mute');
-  }),
-
-  unmute: defineNoReturnMethod(function() {
-    this._sendAsyncMsg('unmute');
-  }),
-
-  getMuted: defineDOMRequestMethod('get-muted'),
-
-  getVolume: defineDOMRequestMethod('get-volume'),
-
-  setVolume: defineNoReturnMethod(function(volume) {
-    this._sendAsyncMsg('set-volume', {volume});
   }),
 
   goBack: defineNoReturnMethod(function() {
