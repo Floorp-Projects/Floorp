3d59495f93d4d060c4dd56c5bab38e31183517f4
Jon Coppeard <jcoppeard@mozilla.com>
2017-05-18 16:47:53 +0100

    Bug 1365654 - Fix style bustage r=me

--- js/src/jsapi-tests/testHashTable.cpp
+++ js/src/jsapi-tests/testHashTable.cpp
@@ -2,11 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/Move.h"
+
 #include "js/HashTable.h"
 #include "js/Utility.h"
-#include "jsapi-tests/tests.h"
 
-#include "mozilla/Move.h"
+#include "jsapi-tests/tests.h"
 
 //#define FUZZ
 
