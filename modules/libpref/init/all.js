c86a4c9b1bcb7409b63e0a8756b3a2193355155b
Ethan Lin <ethlin@mozilla.com>
2017-05-18 17:40:18 +0800

    Bug 1363998 - Part2. Add pref "layers.advanced.background-color" to all.js and turn it on for certain reftests. r=kats

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5709,6 +5709,7 @@ pref("fuzzing.enabled", false);
 // it to a boolean as appropriate. In particular, do NOT add ifdefs here to
 // turn these on and off, instead use the conditional-pref code in gfxPrefs.h
 // to do that.
+pref("layers.advanced.background-color", 2);
 pref("layers.advanced.border-layers", 2);
 pref("layers.advanced.boxshadow-inset-layers", 2);
 pref("layers.advanced.boxshadow-outer-layers", 2);
