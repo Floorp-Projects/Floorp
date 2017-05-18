090d6d31d37ebd01b3098a93495284cf125c01f2
Cykesiopka <cykesiopka.bmo@gmail.com>
2017-05-19 00:57:46 +0800

    Bug 1362735 - Clean up nsIPKCS11 implementation. r=keeler
    The changes here:
    1. Make it easier to discover where nsIPKCS11 is implemented / make it easier to
       discover what the file implements.
    2. Reduce global scope pollution.
    3. Make nsCrypto.h no longer unnecessarily exported.
    4. Remove NS_CRYPTO_CONTRACTID from nsDOMCID.h, since the define isn't used
       anywhere.
    5. Move the definition of NS_PKCS11_CONTRACTID from nsDOMCID.h into PSM code,
       since this contract ID is firmly in PSM territory now.
    
    MozReview-Commit-ID: 2PdFM0mlL4R

--- security/manager/ssl/PKCS11.h
+++ security/manager/ssl/PKCS11.h
@@ -3,30 +3,34 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#ifndef _nsCrypto_h_
-#define _nsCrypto_h_
+#ifndef PKCS11_h
+#define PKCS11_h
 
 #include "nsIPKCS11.h"
 
 #include "nsNSSShutDown.h"
 
+namespace mozilla { namespace psm {
+
 #define NS_PKCS11_CID \
   {0x74b7a390, 0x3b41, 0x11d4, { 0x8a, 0x80, 0x00, 0x60, 0x08, 0xc8, 0x44, 0xc3} }
 
-class nsPkcs11 : public nsIPKCS11
-               , public nsNSSShutDownObject
+class PKCS11 : public nsIPKCS11
+             , public nsNSSShutDownObject
 {
 public:
-  nsPkcs11();
+  PKCS11();
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSIPKCS11
 
 protected:
-  virtual ~nsPkcs11();
+  virtual ~PKCS11();
 
 private:
   virtual void virtualDestroyNSSReference() override {}
 };
 
-#endif //_nsCrypto_h_
+} } // namespace mozilla::psm
+
+#endif // PKCS11_h
