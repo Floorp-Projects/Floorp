2a8d0501180e16ddab6beba7cce061458292451e
Peter Van der Beken <peterv@propagandism.org>
2017-05-18 09:07:25 +0200

    Bug 1252211 - Remove DOMCI for TreeSelection, XULCommandDispatcher and XULControllers. r=bz.

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -21,11 +21,6 @@ enum nsDOMClassInfoID
 
   // XUL classes
 #ifdef MOZ_XUL
-  eDOMClassInfo_XULCommandDispatcher_id,
-#endif
-  eDOMClassInfo_XULControllers_id,
-#ifdef MOZ_XUL
-  eDOMClassInfo_TreeSelection_id,
   eDOMClassInfo_TreeContentView_id,
 #endif
 
