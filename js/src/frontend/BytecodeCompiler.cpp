0df0e66a59bfdf96163c2ba94a997011c13d5792
Shu-yu Guo <shu@rfrn.org>
2017-05-18 18:17:23 -0700

    Bug 1364648 - Fix OOB column handling for default class constructors' toString offsets. (r=jimb)
    CompileOptions's column field has been conflating two meanings: the starting
    column of the ScriptSource, and the starting column of the current thing
    being compiled.
    
    In the shell's evaluate() function, when the "column" option is passed,
    it's fine to conflate the two meanings above.
    
    When delazifying functions, it is incorrect to conflate the two
    meanings. This is observable when generating SRC_CLASS_SPAN, which is a
    srcnote used to save the toString offsets for a default class
    constructor. (Since default class constructors aren't syntactically
    present, there's no JSFunction made ahead of time. And since class
    constructors must be toString'd as the class source instead of the
    function source, we save the offsets in a srcnote to use when we
    actually create the constructor at runtime.) When we save these offsets,
    these are offsets into the ScriptSource buffer, and must be de-offset.
    But it's incorrect to subtract the starting column of the lazy function,
    which is itself offset from the starting column of the underlying
    ScriptSource.

--- js/src/frontend/BytecodeCompiler.cpp
+++ js/src/frontend/BytecodeCompiler.cpp
@@ -645,10 +645,11 @@ frontend::CompileLazyFunction(JSContext* cx, Handle<LazyScript*> lazy, const cha
 {
     MOZ_ASSERT(cx->compartment() == lazy->functionNonDelazifying()->compartment());
 
+    uint32_t sourceStartColumn = lazy->scriptSource()->startColumn();
     CompileOptions options(cx, lazy->version());
     options.setMutedErrors(lazy->mutedErrors())
            .setFileAndLine(lazy->filename(), lazy->lineno())
-           .setColumn(lazy->column())
+           .setColumn(lazy->column(), sourceStartColumn)
            .setNoScriptRval(false)
            .setSelfHostingMode(false);
 
@@ -681,7 +682,7 @@ frontend::CompileLazyFunction(JSContext* cx, Handle<LazyScript*> lazy, const cha
 
     Rooted<JSFunction*> fun(cx, lazy->functionNonDelazifying());
     MOZ_ASSERT(!lazy->isLegacyGenerator());
-    ParseNode* pn = parser.standaloneLazyFunction(fun, lazy->toStringStart() + lazy->column(),
+    ParseNode* pn = parser.standaloneLazyFunction(fun, lazy->toStringStart() + sourceStartColumn,
                                                   lazy->strict(), lazy->generatorKind(), lazy->asyncKind());
     if (!pn)
         return false;
