4c8055d3828972ce194e6053639ce11a884bd6eb
Ting-Yu Lin <tlin@mozilla.com>
2017-05-19 15:18:00 -0700

    Bug 1363640 Part 5 - Add a table in nsXULPrototypeCache to cache XBL documents with servo backend. r=bz,heycam
    We assume those XBL documents without a bound document use gecko style
    backend. Their backend type will be explicitly set when loading in Part 6.
    
    MozReview-Commit-ID: 5c5xtYFvgAD

--- dom/xbl/nsXBLService.cpp
+++ dom/xbl/nsXBLService.cpp
@@ -951,9 +951,16 @@ nsXBLService::LoadBindingDocumentInfo(nsIContent* aBoundElement,
   bool useXULCache = cache && cache->IsEnabled();
 
   if (!info && useXULCache) {
+    // Assume Gecko style backend for the XBL document without a bound
+    // document. The only case is loading platformHTMLBindings.xml which
+    // doesn't have any style sheets or style attributes.
+    StyleBackendType styleBackend
+      = aBoundDocument ? aBoundDocument->GetStyleBackendType()
+                       : StyleBackendType::Gecko;
+
     // This cache crosses the entire product, so that any XBL bindings that are
     // part of chrome will be reused across all XUL documents.
-    info = cache->GetXBLDocumentInfo(documentURI);
+    info = cache->GetXBLDocumentInfo(documentURI, styleBackend);
   }
 
   bool useStartupCache = useXULCache && IsChromeOrResourceURI(documentURI);
@@ -1015,6 +1022,12 @@ nsXBLService::LoadBindingDocumentInfo(nsIContent* aBoundElement,
     // document that has loaded some bindings.
     bindingManager->PutXBLDocumentInfo(info);
   }
+
+  MOZ_ASSERT(!aBoundDocument || !info ||
+             aBoundDocument->GetStyleBackendType() ==
+               info->GetDocument()->GetStyleBackendType(),
+             "Style backend type mismatched between the bound document and "
+             "the XBL document loaded.");
 
   info.forget(aResult);
 
