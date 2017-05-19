f8f207eb758379c18112cad70e69cbd9d2f5dd85
Matt Howell <mhowell@mozilla.com>
2017-05-19 08:51:19 -0700

    Bug 1355818 - Wait for staging to finish in the update wizard if downloading is done before the download page appears. r=rstrong, a=gchang

--- toolkit/mozapps/update/nsUpdateService.js
+++ toolkit/mozapps/update/nsUpdateService.js
@@ -3117,6 +3117,9 @@ UpdateManager.prototype = {
       if (!handleUpdateFailure(update, parts[1])) {
         handleFallbackToCompleteUpdate(update, true);
       }
+
+      update.QueryInterface(Ci.nsIWritablePropertyBag);
+      update.setProperty("stagingFailed", "true");
     }
     if (update.state == STATE_APPLIED && shouldUseService()) {
       writeStatusFile(getUpdatesDir(), update.state = STATE_APPLIED_SERVICE);
