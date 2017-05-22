a3a0032cc5d566cd744e5a4da49e420b23b49412
Valentin Gosu <valentin.gosu@gmail.com>
2017-05-22 19:11:15 +0200

    Bug 1366224 - Change RCWN thresholds to race more often r=mcmanus The previous limits were chosen a bit arbitrarily, and didn't trigger racing too often. These limits are also quite arbitrary, but should cause us to race more. These limits can be removed once we implement bug 1325331 and we can make a fair approximation regarding the cache latency.
    MozReview-Commit-ID: CUssxUrs1Wu

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1684,8 +1684,8 @@ pref("network.http.max_response_header_size", 393216);
 
 // If we should attempt to race the cache and network
 pref("network.http.rcwn.enabled", true);
-pref("network.http.rcwn.cache_queue_normal_threshold", 50);
-pref("network.http.rcwn.cache_queue_priority_threshold", 10);
+pref("network.http.rcwn.cache_queue_normal_threshold", 8);
+pref("network.http.rcwn.cache_queue_priority_threshold", 2);
 // We might attempt to race the cache with the network only if a resource
 // is smaller than this size.
 pref("network.http.rcwn.small_resource_size_kb", 256);
