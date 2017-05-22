822131e89cb720d1100749a30160fb0bdf26436a
Manish Goregaokar <manishearth@gmail.com>
2017-05-22 13:32:13 -0700

    Bug 1365937 - Mark Servo_DeclarationBlock_GetCssText as threadsafe; r=froydnj
    MozReview-Commit-ID: 899nIoyc8OD
    
    --HG--
    extra : rebase_source : ddfb15158549fc7679a1a9669f26a7217a858fa6

--- js/src/devtools/rootAnalysis/analyzeHeapWrites.js
+++ js/src/devtools/rootAnalysis/analyzeHeapWrites.js
@@ -26,6 +26,7 @@ function checkExternalFunction(entry)
         "ceil",
         /memchr/,
         "strlen",
+        /Servo_DeclarationBlock_GetCssText/,
         // Assume that atomic accesses are threadsafe.
         /^__atomic_fetch_/,
         /^__atomic_load_/,
