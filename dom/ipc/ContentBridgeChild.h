28479706eac3ab27b2e0ab76666e2596cf32eb08
Andrea Marchesini <amarchesini@mozilla.com>
2017-05-23 18:00:30 +0200

    Bug 1359718 - Get rid of PBlob - part 0 - remove PMemoryStream, r=smaug

--- dom/ipc/ContentBridgeChild.h
+++ dom/ipc/ContentBridgeChild.h
@@ -36,9 +36,6 @@ public:
   SendPBlobConstructor(PBlobChild* actor,
                        const BlobConstructorParams& aParams) override;
 
-  virtual PMemoryStreamChild*
-  SendPMemoryStreamConstructor(const uint64_t& aSize) override;
-
   jsipc::CPOWManager* GetCPOWManager() override;
 
   virtual bool SendPBrowserConstructor(PBrowserChild* aActor,
@@ -87,10 +84,6 @@ protected:
 
   virtual PBlobChild* AllocPBlobChild(const BlobConstructorParams& aParams) override;
   virtual bool DeallocPBlobChild(PBlobChild*) override;
-
-  virtual PMemoryStreamChild*
-  AllocPMemoryStreamChild(const uint64_t& aSize) override;
-  virtual bool DeallocPMemoryStreamChild(PMemoryStreamChild*) override;
 
   virtual PIPCBlobInputStreamChild*
   AllocPIPCBlobInputStreamChild(const nsID& aID,
