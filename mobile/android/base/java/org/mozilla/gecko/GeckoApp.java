d7062cd0a5bd64504e23e7c82cde6514193eb475
Jan Henning <jh+bugzilla@buttercookie.de>
2017-05-23 21:15:01 +0200

    Bug 1346413 - Part 0 - Remove unneeded imports. r=jchen
    MozReview-Commit-ID: 4JwHRdI4JGy
    
    --HG--
    extra : rebase_source : f93ceeaf7e93183a8134555c3de9026e41b64a70

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -88,7 +88,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
-import android.widget.AbsoluteLayout;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.FrameLayout;
