58a242fc9b14558ffce9825ac9fc9122cacc1786
Nathan Froyd <froydnj@mozilla.com>
2017-05-22 21:26:26 -0400

    Bug 1363172 - add mozilla::RWLock; r=erahm
    This sort of lock is mostly useful in the context of Stylo right now,
    but perhaps there are other applications waiting to be written.

---
+++ xpcom/tests/gtest/TestRWLock.cpp
@@ -0,0 +1,82 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "nsThreadUtils.h"
+#include "mozilla/Atomics.h"
+#include "mozilla/RWLock.h"
+#include "gtest/gtest.h"
+
+using mozilla::RWLock;
+
+static const size_t sNumThreads = 4;
+static const size_t sOuterIterations = 100;
+static const size_t sInnerIterations = 100;
+static const size_t sWriteLockIteration = 10;
+
+// Based on example code from _Programming with POSIX Threads_.  Not an actual
+// test of correctness, but more of a "does this work at all" sort of test.
+
+class RWLockRunnable : public mozilla::Runnable {
+public:
+  RWLockRunnable(RWLock* aRWLock, mozilla::Atomic<size_t>* aSharedData)
+    : mRWLock(aRWLock)
+    , mSharedData(aSharedData)
+  {}
+
+  NS_DECL_NSIRUNNABLE
+
+private:
+  ~RWLockRunnable() = default;
+
+  RWLock* mRWLock;
+  mozilla::Atomic<size_t>* mSharedData;
+};
+
+NS_IMETHODIMP
+RWLockRunnable::Run()
+{
+  for (size_t i = 0; i < sOuterIterations; ++i) {
+    if (i % sWriteLockIteration == 0) {
+      mozilla::AutoWriteLock lock(*mRWLock);
+
+      ++(*mSharedData);
+    } else {
+      mozilla::AutoReadLock lock(*mRWLock);
+
+      // Loop and try to force other threads to run, but check that our
+      // shared data isn't being modified by them.
+      size_t initialValue = *mSharedData;
+      for (size_t j = 0; j < sInnerIterations; ++j) {
+        EXPECT_EQ(initialValue, *mSharedData);
+
+        // This is a magic yield call.
+        PR_Sleep(PR_INTERVAL_NO_WAIT);
+      }
+    }
+  }
+
+  return NS_OK;
+}
+
+TEST(RWLock, SmokeTest)
+{
+  nsCOMPtr<nsIThread> threads[sNumThreads];
+  RWLock rwlock("test lock");
+  mozilla::Atomic<size_t> data(0);
+
+  for (size_t i = 0; i < sNumThreads; ++i) {
+    nsCOMPtr<nsIRunnable> event = new RWLockRunnable(&rwlock, &data);
+    NS_NewNamedThread("RWLockTester", getter_AddRefs(threads[i]), event);
+  }
+
+  // Wait for all the threads to finish.
+  for (size_t i = 0; i < sNumThreads; ++i) {
+    nsresult rv = threads[i]->Shutdown();
+    EXPECT_TRUE(NS_SUCCEEDED(rv));
+  }
+
+  EXPECT_EQ(data, (sOuterIterations / sWriteLockIteration) * sNumThreads);
+}
---
+++ xpcom/threads/RWLock.cpp
@@ -0,0 +1,89 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/RWLock.h"
+
+#ifdef XP_WIN
+#include <windows.h>
+
+static_assert(sizeof(SRWLOCK) <= sizeof(void*), "SRWLOCK is too big!");
+
+#define NativeHandle(m) (reinterpret_cast<SRWLOCK*>(&m))
+#else
+#define NativeHandle(m) (&m)
+#endif
+
+namespace mozilla {
+
+RWLock::RWLock(const char* aName)
+  : BlockingResourceBase(aName, eMutex)
+#ifdef DEBUG
+  , mOwningThread(nullptr)
+#endif
+{
+#ifdef XP_WIN
+  InitializeSRWLock(NativeHandle(mRWLock));
+#else
+  MOZ_RELEASE_ASSERT(pthread_rwlock_init(NativeHandle(mRWLock), nullptr) == 0,
+                     "pthread_rwlock_init failed");
+#endif
+}
+
+#ifndef XP_WIN
+RWLock::~RWLock()
+{
+  MOZ_RELEASE_ASSERT(pthread_rwlock_destroy(NativeHandle(mRWLock)) == 0,
+                     "pthread_rwlock_destroy failed");
+}
+#endif
+
+void
+RWLock::ReadLockInternal()
+{
+#ifdef XP_WIN
+  AcquireSRWLockShared(NativeHandle(mRWLock));
+#else
+  MOZ_RELEASE_ASSERT(pthread_rwlock_rdlock(NativeHandle(mRWLock)) == 0,
+                     "pthread_rwlock_rdlock failed");
+#endif
+}
+
+void
+RWLock::ReadUnlockInternal()
+{
+#ifdef XP_WIN
+  ReleaseSRWLockShared(NativeHandle(mRWLock));
+#else
+  MOZ_RELEASE_ASSERT(pthread_rwlock_unlock(NativeHandle(mRWLock)) == 0,
+                     "pthread_rwlock_unlock failed");
+#endif
+}
+
+void
+RWLock::WriteLockInternal()
+{
+#ifdef XP_WIN
+  AcquireSRWLockExclusive(NativeHandle(mRWLock));
+#else
+  MOZ_RELEASE_ASSERT(pthread_rwlock_wrlock(NativeHandle(mRWLock)) == 0,
+                     "pthread_rwlock_wrlock failed");
+#endif
+}
+
+void
+RWLock::WriteUnlockInternal()
+{
+#ifdef XP_WIN
+  ReleaseSRWLockExclusive(NativeHandle(mRWLock));
+#else
+  MOZ_RELEASE_ASSERT(pthread_rwlock_unlock(NativeHandle(mRWLock)) == 0,
+                     "pthread_rwlock_unlock failed");
+#endif
+}
+
+}
+
+#undef NativeHandle
