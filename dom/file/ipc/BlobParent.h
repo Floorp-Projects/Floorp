dcdba0d302bfdd05bd9dec0a4e31a37d3102c5cf
Andrea Marchesini <amarchesini@mozilla.com>
2017-05-23 18:04:27 +0200

    Bug 1359718 - Get rid of PBlob - part 1 - Remove PBlobStream, r=smaug, r=kanru

--- dom/file/ipc/BlobParent.h
+++ dom/file/ipc/BlobParent.h
@@ -35,7 +35,6 @@ namespace dom {
 class ContentParent;
 class BlobImpl;
 class nsIContentParent;
-class PBlobStreamParent;
 
 class BlobParent final
   : public PBlobParent
@@ -44,9 +43,6 @@ class BlobParent final
 
   class IDTableEntry;
   typedef nsDataHashtable<nsIDHashKey, IDTableEntry*> IDTable;
-
-  class OpenStreamRunnable;
-  friend class OpenStreamRunnable;
 
   class RemoteBlobImpl;
 
@@ -63,15 +59,6 @@ class BlobParent final
   nsCOMPtr<nsIContentParent> mContentManager;
 
   nsCOMPtr<nsIEventTarget> mEventTarget;
-
-  // nsIInputStreams backed by files must ensure that the files are actually
-  // opened and closed on a background thread before we can send their file
-  // handles across to the child. The child process could crash during this
-  // process so we need to make sure we cancel the intended response in such a
-  // case. We do that by holding an array of nsRevocableEventPtr. If the child
-  // crashes then this actor will be destroyed and the nsRevocableEventPtr
-  // destructor will cancel any stream events that are currently in flight.
-  nsTArray<nsRevocableEventPtr<OpenStreamRunnable>> mOpenStreamRunnables;
 
   RefPtr<IDTableEntry> mIDTableEntry;
 
@@ -189,9 +176,6 @@ private:
   void
   NoteDyingRemoteBlobImpl();
 
-  void
-  NoteRunnableCompleted(OpenStreamRunnable* aRunnable);
-
   nsIEventTarget*
   EventTarget() const
   {
@@ -205,26 +189,8 @@ private:
   virtual void
   ActorDestroy(ActorDestroyReason aWhy) override;
 
-  virtual PBlobStreamParent*
-  AllocPBlobStreamParent(const uint64_t& aStart,
-                         const uint64_t& aLength) override;
-
-  virtual mozilla::ipc::IPCResult
-  RecvPBlobStreamConstructor(PBlobStreamParent* aActor,
-                             const uint64_t& aStart,
-                             const uint64_t& aLength) override;
-
-  virtual bool
-  DeallocPBlobStreamParent(PBlobStreamParent* aActor) override;
-
   virtual mozilla::ipc::IPCResult
   RecvResolveMystery(const ResolveMysteryParams& aParams) override;
-
-  virtual mozilla::ipc::IPCResult
-  RecvBlobStreamSync(const uint64_t& aStart,
-                     const uint64_t& aLength,
-                     InputStreamParams* aParams,
-                     OptionalFileDescriptorSet* aFDs) override;
 
   virtual mozilla::ipc::IPCResult
   RecvWaitForSliceCreation() override;
