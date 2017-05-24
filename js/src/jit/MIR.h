88c9460edc1e8aaca87c67043753e2437a4a09ad
Ryan VanderMeulen <ryanvm@gmail.com>
2017-05-23 21:38:13 -0400

    Backed out changeset 9108c50863b3 (bug 1343723) for SM test failures.

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -11640,9 +11640,6 @@ class MNewLexicalEnvironmentObject
     bool appendRoots(MRootList& roots) const override {
         return roots.append(scope_);
     }
-    AliasSet getAliasSet() const override {
-        return AliasSet::None();
-    }
 };
 
 // Allocate a new LexicalEnvironmentObject from existing one
@@ -11669,11 +11666,6 @@ class MCopyLexicalEnvironmentObject
     }
     bool possiblyCalls() const override {
         return true;
-    }
-    AliasSet getAliasSet() const override {
-        return AliasSet::Load(AliasSet::ObjectFields |
-                              AliasSet::FixedSlot |
-                              AliasSet::DynamicSlot);
     }
 };
 
