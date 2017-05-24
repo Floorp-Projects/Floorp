e4db983e91c2e657c5dc1acaba0a7dee4ec702d4
Mihai Tabara <mtabara@mozilla.com>
2017-05-24 14:35:14 +0300

    Bug 1358614 - new balrog config for devedition. r=rail a=release DONTBUILD
    MozReview-Commit-ID: CHPk5GCI5nE
    
    --HG--
    extra : source : ef90d25c7a1a51fef80293d7c7a1b6f98081a015

---
+++ testing/mozharness/configs/releases/updates_firefox_devedition.py
@@ -0,0 +1,35 @@
+
+config = {
+    "log_name": "updates_devedition",
+    "repo": {
+        "repo": "https://hg.mozilla.org/build/tools",
+        "branch": "default",
+        "dest": "tools",
+        "vcs": "hg",
+    },
+    "vcs_share_base": "/builds/hg-shared",
+    "push_dest": "ssh://hg.mozilla.org/build/tools",
+    "shipped-locales-url": "https://hg.mozilla.org/releases/mozilla-beta/raw-file/{revision}/browser/locales/shipped-locales",
+    "ignore_no_changes": True,
+    "ssh_user": "ffxbld",
+    "ssh_key": "~/.ssh/ffxbld_rsa",
+    "archive_domain": "archive.mozilla.org",
+    "archive_prefix": "https://archive.mozilla.org/pub",
+    "previous_archive_prefix": "https://archive.mozilla.org/pub",
+    "download_domain": "download.mozilla.org",
+    "balrog_url": "https://aus5.mozilla.org",
+    "balrog_username": "ffxbld",
+    "update_channels": {
+        "aurora": {
+            "version_regex": r"^.*$",
+            "requires_mirrors": True,
+            "patcher_config": "mozDevedition-branch-patcher2.cfg",
+            "update_verify_channel": "aurora-localtest",
+            "mar_channel_ids": [],
+            "channel_names": ["aurora", "aurora-localtest", "aurora-cdntest"],
+            "rules_to_update": ["devedition-cdntest", "devedition-localtest"],
+            "publish_rules": ["devedition"],
+        },
+    },
+    "balrog_use_dummy_suffix": False,
+}
