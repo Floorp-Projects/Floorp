be5fcdd4fca1b99af3174c24527f72022aecfa7b
Ben Hearsum <bhearsum@mozilla.com>
2017-05-24 11:24:26 -0400

    bug 1357808: fix flake8 error. a=dawn DONTBUILD

--- taskcluster/taskgraph/loader/push_apk.py
+++ taskcluster/taskgraph/loader/push_apk.py
@@ -29,8 +29,8 @@ def get_dependent_loaded_tasks(config, loaded_tasks):
         task for task in tasks_with_matching_kind
         # old-id builds are not shipped through the Play store, so we don't
         # want them as dependencies.
-        if task.attributes.get('build_platform', '').startswith('android') and \
-          'old-id' not in task.attributes.get('build_platform', '')
+        if task.attributes.get('build_platform', '').startswith('android') \
+        and 'old-id' not in task.attributes.get('build_platform', '')
     ]
 
     return android_tasks
