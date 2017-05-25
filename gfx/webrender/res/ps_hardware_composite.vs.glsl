5c92b786c450c30e19aebfdf7cb72d632bb71ce7
Kartikaya Gupta <kgupta@mozilla.com>
2017-05-25 13:27:55 -0400

    Bug 1365934 - Update webrender to 76a3213080ca5c2e2a612c3023c50c81a111fd55. r=jrmuizel,kvark
    This includes a change to bindings.rs for an API change to the readback function
    in webrender cset 052b0a7.
    
    MozReview-Commit-ID: K9eMrF3O6OX

--- gfx/webrender/res/ps_hardware_composite.vs.glsl
+++ gfx/webrender/res/ps_hardware_composite.vs.glsl
@@ -4,9 +4,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 void main(void) {
-    PrimitiveInstance pi = fetch_prim_instance();
-    AlphaBatchTask dest_task = fetch_alpha_batch_task(pi.render_task_index);
-    AlphaBatchTask src_task = fetch_alpha_batch_task(pi.user_data.x);
+    CompositeInstance ci = fetch_composite_instance();
+    AlphaBatchTask dest_task = fetch_alpha_batch_task(ci.render_task_index);
+    AlphaBatchTask src_task = fetch_alpha_batch_task(ci.src_task_index);
 
     vec2 dest_origin = dest_task.render_target_origin -
                        dest_task.screen_space_origin +
@@ -21,5 +21,5 @@ void main(void) {
     vec2 st1 = (src_task.render_target_origin + src_task.size) / texture_size;
     vUv = vec3(mix(st0, st1, aPosition.xy), src_task.render_target_layer_index);
 
-    gl_Position = uTransform * vec4(local_pos, pi.z, 1.0);
+    gl_Position = uTransform * vec4(local_pos, ci.z, 1.0);
 }
