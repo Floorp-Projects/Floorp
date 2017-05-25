60155008c52e22e7c3e907203458c03b56a125e6
Geoff Brown <gbrown@mozilla.com>
2017-05-25 07:32:42 -0600

    Bug 1367235 - Enable eslint on testing/xpcshell - mechanical updates; r=Standard8

--- testing/xpcshell/example/unit/test_check_nsIException_failing.js
+++ testing/xpcshell/example/unit/test_check_nsIException_failing.js
@@ -2,7 +2,7 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 function run_test() {
-  do_check_throws_nsIException(function () {
+  do_check_throws_nsIException(function() {
     throw Error("I find your relaxed dishabille unpalatable");
   }, "NS_NOINTERFACE");
 }
