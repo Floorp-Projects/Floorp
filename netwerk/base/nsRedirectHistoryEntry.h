4fcb94e96833c509a309499b2e6610100ae8c9f5
Thomas Nguyen <tnguyen@mozilla.com>
2017-05-25 19:42:00 +0200

    Bug 1351146 - P1 - Add more information to redirect chains. r=dragana
    In order to provide more details context of how client arrived at the unsafe
    page, particularly in redirect case, we may have to add more information to
    redirect chains including:
    - referrer (if any)
    - remote address.
    - URL
    
    We may want to use an idl interface instead of nsIPrincipal to store these
    information
    
    MozReview-Commit-ID: 3Uh4r06w60C

---
+++ netwerk/base/nsRedirectHistoryEntry.h
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsRedirectHistoryEntry_h__
+#define nsRedirectHistoryEntry_h__
+
+#include "nsIRedirectHistoryEntry.h"
+
+class nsIURI;
+class nsIPrincipal;
+
+namespace mozilla {
+namespace net {
+
+class nsRedirectHistoryEntry final : public nsIRedirectHistoryEntry
+{
+public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSIREDIRECTHISTORYENTRY
+
+  nsRedirectHistoryEntry(nsIPrincipal* aPrincipal, nsIURI* aReferrer,
+                         const nsACString& aRemoteAddress);
+
+private:
+  ~nsRedirectHistoryEntry();
+
+  nsCOMPtr<nsIPrincipal> mPrincipal;
+  nsCOMPtr<nsIURI> mReferrer;
+  nsCString mRemoteAddress;
+
+};
+
+} // namespace net
+} // namespace mozilla
+
+#endif // nsRedirectHistoryEntry_h__
