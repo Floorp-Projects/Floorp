30ea634bb08e4e4cf5b99c1e4047feb49993fd32
Paolo Amadini <paolo.mozmail@amadzone.org>
2017-05-25 15:00:49 +0100

    Bug 1242505 - Part 5 - Remove obsolete whitelisting of unhandled rejections. r=Mossop
    The whitelisting function thisTestLeaksUncaughtRejectionsAndShouldBeFixed was replaced by expectUncaughtRejection, and existing calls did not take effect anymore.
    
    MozReview-Commit-ID: 3uOxkgWYWEz
    
    --HG--
    extra : rebase_source : 5a10a3ebbfe0ce2a801330041f95447c313a9a70
    extra : source : 6f0394b523a66dab444b8551deb8f3c6c81d8f31

--- devtools/client/webconsole/test/browser_webconsole_output_dom_elements_01.js
+++ devtools/client/webconsole/test/browser_webconsole_output_dom_elements_01.js
@@ -8,10 +8,6 @@
 
 "use strict";
 
-thisTestLeaksUncaughtRejectionsAndShouldBeFixed(null);
-thisTestLeaksUncaughtRejectionsAndShouldBeFixed(
-  "TypeError: this.toolbox is null");
-
 // Test the webconsole output for various types of DOM Nodes.
 
 const TEST_URI = "http://example.com/browser/devtools/client/webconsole/" +
