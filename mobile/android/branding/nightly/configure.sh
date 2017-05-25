a18f3392011640e046cef74d46b5a4277e8d78dc
Sylvestre Ledru <sledru@mozilla.com>
2017-05-26 00:05:06 +0200

    Bug 1367912 - Rename of the nightly version in the app launcher s?sebastian r=sebastian
    MozReview-Commit-ID: GmIOMXHXKzT

--- mobile/android/branding/nightly/configure.sh
+++ mobile/android/branding/nightly/configure.sh
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=Nightly
+MOZ_APP_DISPLAYNAME="Firefox Nightly"
 MOZ_UPDATER=1
 MOZ_ANDROID_ANR_REPORTER=1
 MOZ_ANDROID_SHARED_ID=org.mozilla.fennec.sharedID
