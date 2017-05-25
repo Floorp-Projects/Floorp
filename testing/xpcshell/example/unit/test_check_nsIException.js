0da003433254165d808109ede1f7f1a33d75a0e0
Geoff Brown <gbrown@mozilla.com>
2017-05-25 07:32:44 -0600

    Bug 1367235 - Additional changes for eslint on testing/xpcshell; r=Standard8

--- testing/xpcshell/example/unit/test_check_nsIException.js
+++ testing/xpcshell/example/unit/test_check_nsIException.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/* import-globals-from ../../head.js */
+
 function run_test() {
   let env = Components.classes["@mozilla.org/process/environment;1"]
                       .getService(Components.interfaces.nsIEnvironment);
--- testing/xpcshell/example/unit/test_check_nsIException_failing.js
+++ testing/xpcshell/example/unit/test_check_nsIException_failing.js
@@ -1,6 +1,8 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/* import-globals-from ../../head.js */
+
 function run_test() {
   do_check_throws_nsIException(function() {
     throw Error("I find your relaxed dishabille unpalatable");
