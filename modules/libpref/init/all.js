e84601e7a77a168b85afe4bb376b845aead58a9b
Valentin Gosu <valentin.gosu@gmail.com>
2017-05-26 10:59:16 +0300

    Bug 1367365 - Disable RCWN r=me a=tomcat
    MozReview-Commit-ID: GuIC5Gv09Z3
    
    --HG--
    extra : amend_source : 38de21ab56fc9b05109bfc96511f81c718c136d7

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1683,7 +1683,7 @@ pref("network.http.keep_empty_response_headers_as_empty_string", true);
 pref("network.http.max_response_header_size", 393216);
 
 // If we should attempt to race the cache and network
-pref("network.http.rcwn.enabled", true);
+pref("network.http.rcwn.enabled", false);
 pref("network.http.rcwn.cache_queue_normal_threshold", 8);
 pref("network.http.rcwn.cache_queue_priority_threshold", 2);
 // We might attempt to race the cache with the network only if a resource
