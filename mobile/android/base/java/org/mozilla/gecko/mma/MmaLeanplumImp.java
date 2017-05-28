f66d05e3d78968fc42b60951f807d5a969a1a1fd
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-05-28 18:44:50 +0200

    Backed out changeset 49714b590d48 (bug 1361664)

--- mobile/android/base/java/org/mozilla/gecko/mma/MmaLeanplumImp.java
+++ mobile/android/base/java/org/mozilla/gecko/mma/MmaLeanplumImp.java
@@ -7,14 +7,17 @@
 package org.mozilla.gecko.mma;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageManager;
 
 import com.leanplum.Leanplum;
 import com.leanplum.LeanplumActivityHelper;
+import com.leanplum.annotations.Parser;
 
+import org.mozilla.gecko.ActivityHandlerHelper;
 import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.MmaConstants;
+import org.mozilla.gecko.util.ContextUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,10 +25,7 @@ import java.util.Map;
 
 public class MmaLeanplumImp implements MmaInterface {
     @Override
-    public void init(final Activity activity) {
-        if (activity == null) {
-            return;
-        }
+    public void init(Activity activity) {
         Leanplum.setApplicationContext(activity.getApplicationContext());
 
         LeanplumActivityHelper.enableLifecycleCallbacks(activity.getApplication());
@@ -36,29 +36,18 @@ public class MmaLeanplumImp implements MmaInterface {
             Leanplum.setAppIdForDevelopmentMode(MmaConstants.MOZ_LEANPLUM_SDK_CLIENTID, MmaConstants.MOZ_LEANPLUM_SDK_KEY);
         }
 
+
         Map<String, Object> attributes = new HashMap<>();
-        boolean installedFocus = isPackageInstalled(activity, "org.mozilla.focus");
-        boolean installedKlar = isPackageInstalled(activity, "org.mozilla.klar");
+        boolean installedFocus = ContextUtils.isPackageInstalled(activity, "org.mozilla.focus");
+        boolean installedKlar = ContextUtils.isPackageInstalled(activity, "org.mozilla.klar");
         if (installedFocus || installedKlar) {
             attributes.put("focus", "installed");
         }
         Leanplum.start(activity, attributes);
-
+        Leanplum.track("Launch");
         // this is special to Leanplum. Since we defer LeanplumActivityHelper's onResume call till
-        // switchboard completes loading. We miss the call to LeanplumActivityHelper.onResume.
-        // So I manually call it here.
-        //
-        // There's a risk that if this is called after activity's onPause(Although I've
-        // tested it's seems okay). We  should require their SDK to separate activity call back with
-        // SDK initialization and Activity lifecycle in the future.
-        //
-        // I put it under runOnUiThread because in current Leanplum's SDK design, this should be run in main thread.
-        activity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                LeanplumActivityHelper.onResume(activity);
-            }
-        });
+        // switchboard completes loading, we manually call it here.
+        LeanplumActivityHelper.onResume(activity);
     }
 
     @Override
@@ -81,15 +70,5 @@ public class MmaLeanplumImp implements MmaInterface {
     @Override
     public void stop() {
         Leanplum.stop();
-    }
-
-    private static boolean isPackageInstalled(final Context context, String packageName) {
-        try {
-            PackageManager pm = context.getPackageManager();
-            pm.getPackageInfo(packageName, 0);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
     }
 }
