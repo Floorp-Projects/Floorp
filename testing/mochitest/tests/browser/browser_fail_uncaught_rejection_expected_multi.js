2aea17aeb64442dc1299c0344ad5073e1185536c
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-05-28 11:31:06 +0200

    Backed out changeset c9c01c06908a (bug 1242505)

--- testing/mochitest/tests/browser/browser_fail_uncaught_rejection_expected_multi.js
+++
@@ -1,9 +0,0 @@
-setExpectedFailuresForSelfTest(1);
-
-// The test will fail because an expected uncaught rejection is actually caught.
-Cu.import("resource://testing-common/PromiseTestUtils.jsm", this);
-PromiseTestUtils.expectUncaughtRejection(/Promise rejection./);
-
-function test() {
-  Promise.reject(new Error("Promise rejection.")).catch(() => {});
-}
