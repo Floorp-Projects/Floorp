2b20be2612accaa61778be56711bf71c39da9465
Johan Lorenzo <jlorenzo@mozilla.com>
2017-05-29 16:27:23 +0200

    Bug 1368484 - Don't upload AArch64 APK to Google Play r=bhearsum
    MozReview-Commit-ID: KB2csWd7bzu

--- taskcluster/taskgraph/util/push_apk.py
+++ taskcluster/taskgraph/util/push_apk.py
@@ -11,7 +11,6 @@ from taskgraph.util.schema import validate_schema
 
 REQUIRED_ARCHITECTURES = {
     'android-x86-nightly',
-    'android-aarch64-nightly',
     'android-api-15-nightly',
 }
 PLATFORM_REGEX = re.compile(r'signing-android-(\S+)-nightly')
