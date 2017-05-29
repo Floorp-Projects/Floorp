37d8c6f7e0f074eafe093f08e1403afa0fd3cee3
meetshah <meet1995@gmail.com>
2017-05-29 23:41:56 +0530

    Bug 1366231 - Implement History.hasVisits as a wrapper of asyncHistory.isURIVisited; r=mak
    Tests for the wrapper implementation added in test_hasVisits.js in toolkit/components/places/tests/history.
    
    MozReview-Commit-ID: KEW2bDTSWVY
    
    --HG--
    extra : rebase_source : 453aa3dc1503c2595ed0161ad7dc350bec09111e

---
+++ toolkit/components/places/tests/history/test_hasVisits.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests for `History.hasVisits` as implemented in History.jsm
+
+"use strict";
+
+add_task(async function test_has_visits_error_cases() {
+  Assert.throws(
+    () => PlacesUtils.history.hasVisits(),
+    /TypeError: Invalid url or guid: undefined/,
+    "passing a null into History.hasVisits should throw a TypeError"
+  );
+  Assert.throws(
+    () => PlacesUtils.history.hasVisits(1),
+    /TypeError: Invalid url or guid: 1/,
+    "passing an invalid url into History.hasVisits should throw a TypeError"
+  );
+  Assert.throws(
+    () => PlacesUtils.history.hasVisits({}),
+    /TypeError: Invalid url or guid: [object Object]/,
+    `passing an invalid (not of type URI or nsIURI) object to History.hasVisits
+     should throw a TypeError`
+  );
+});
+
+add_task(async function test_history_has_visits() {
+  const TEST_URL = "http://mozilla.com/";
+  await PlacesTestUtils.clearHistory();
+  Assert.equal(await PlacesUtils.history.hasVisits(TEST_URL), false,
+               "Test Url should not be in history.");
+  await PlacesTestUtils.addVisits(TEST_URL);
+  Assert.equal(await PlacesUtils.history.hasVisits(TEST_URL), true,
+               "Test Url should be in history.");
+  await PlacesTestUtils.clearHistory();
+});
