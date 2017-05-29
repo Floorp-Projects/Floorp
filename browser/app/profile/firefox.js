bc7bc18e80b28a7a549e725ed1872f0153ef8695
Sean Lee <selee@mozilla.com>
2017-05-29 10:51:57 +0800

    Bug 1349490 - Part 4: Add "extensions.formautofill.heuristics.enabled" for toggling form autofill heuristics.; r=MattN
    MozReview-Commit-ID: Le3o7f7AXKn

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1651,6 +1651,7 @@ pref("extensions.formautofill.experimental", true);
 pref("extensions.formautofill.experimental", false);
 #endif
 pref("extensions.formautofill.addresses.enabled", true);
+pref("extensions.formautofill.heuristics.enabled", false);
 pref("extensions.formautofill.loglevel", "Warn");
 
 // Whether or not to restore a session with lazy-browser tabs.
