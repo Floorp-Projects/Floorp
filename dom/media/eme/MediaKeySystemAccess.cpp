948d3a918b251f0dcf1a791f96eda9f9ecb45c88
Chris Pearce <cpearce@mozilla.com>
2017-05-30 10:33:30 +1200

    Bug 1355252 - Backed out changeset ddb3969de046. r=backout
    MozReview-Commit-ID: cdH0WSFOaz

--- dom/media/eme/MediaKeySystemAccess.cpp
+++ dom/media/eme/MediaKeySystemAccess.cpp
@@ -926,13 +926,8 @@ GetSupportedConfig(const KeySystemConfig& aKeySystem,
 
   // If the videoCapabilities and audioCapabilities members in candidate
   // configuration are both empty, return NotSupported.
-  if (aCandidate.mAudioCapabilities.IsEmpty() &&
-      aCandidate.mVideoCapabilities.IsEmpty()) {
-    EME_LOG("MediaKeySystemConfiguration (label='%s') rejected; "
-            "no supported audio or video capabilities specified",
-            NS_ConvertUTF16toUTF8(aCandidate.mLabel).get());
-    return false;
-  }
+  // TODO: Most sites using EME still don't pass capabilities, so we
+  // can't reject on it yet without breaking them. So add this later.
 
   // If the videoCapabilities member in candidate configuration is non-empty:
   if (!aCandidate.mVideoCapabilities.IsEmpty()) {
