f8e620ec5e6b53ad5c6bd4729022a94c5600f8eb
Samuel Vargas <svargas@mozilla.com>
2017-05-30 15:21:33 +0000

    Bug 1325113 - Implement support for WEBGL_compressed_texture_s3tc_srgb - r=jgilbert,qdot
    MozReview-Commit-ID: mI4dlB2lK

--- dom/canvas/WebGLExtensionCompressedTextureS3TC_SRGB.cpp
+++ dom/canvas/WebGLExtensionCompressedTextureS3TC_SRGB.cpp
@@ -14,7 +14,7 @@
 
 namespace mozilla {
 
-WebGLExtensionCompressedTextureS3TC::WebGLExtensionCompressedTextureS3TC(WebGLContext* webgl)
+WebGLExtensionCompressedTextureS3TC_SRGB::WebGLExtensionCompressedTextureS3TC_SRGB(WebGLContext* webgl)
     : WebGLExtensionBase(webgl)
 {
     RefPtr<WebGLContext> webgl_ = webgl; // Bug 1201275
@@ -30,30 +30,18 @@ WebGLExtensionCompressedTextureS3TC::WebGLExtensionCompressedTextureS3TC(WebGLCo
 
 #define FOO(x) LOCAL_GL_ ## x, webgl::EffectiveFormat::x
 
-    fnAdd(FOO(COMPRESSED_RGB_S3TC_DXT1_EXT));
-    fnAdd(FOO(COMPRESSED_RGBA_S3TC_DXT1_EXT));
-    fnAdd(FOO(COMPRESSED_RGBA_S3TC_DXT3_EXT));
-    fnAdd(FOO(COMPRESSED_RGBA_S3TC_DXT5_EXT));
+    fnAdd(FOO(COMPRESSED_SRGB_S3TC_DXT1_EXT));
+    fnAdd(FOO(COMPRESSED_SRGB_ALPHA_S3TC_DXT1_EXT));
+    fnAdd(FOO(COMPRESSED_SRGB_ALPHA_S3TC_DXT3_EXT));
+    fnAdd(FOO(COMPRESSED_SRGB_ALPHA_S3TC_DXT5_EXT));
 
 #undef FOO
 }
 
-WebGLExtensionCompressedTextureS3TC::~WebGLExtensionCompressedTextureS3TC()
+WebGLExtensionCompressedTextureS3TC_SRGB::~WebGLExtensionCompressedTextureS3TC_SRGB()
 {
 }
 
-bool
-WebGLExtensionCompressedTextureS3TC::IsSupported(const WebGLContext* webgl)
-{
-   gl::GLContext* gl = webgl->GL();
-   if (gl->IsExtensionSupported(gl::GLContext::EXT_texture_compression_s3tc))
-      return true;
-
-   return gl->IsExtensionSupported(gl::GLContext::EXT_texture_compression_dxt1) &&
-          gl->IsExtensionSupported(gl::GLContext::ANGLE_texture_compression_dxt3) &&
-          gl->IsExtensionSupported(gl::GLContext::ANGLE_texture_compression_dxt5);
-}
-
-IMPL_WEBGL_EXTENSION_GOOP(WebGLExtensionCompressedTextureS3TC, WEBGL_compressed_texture_s3tc)
+IMPL_WEBGL_EXTENSION_GOOP(WebGLExtensionCompressedTextureS3TC_SRGB, WEBGL_compressed_texture_s3tc_srgb)
 
 } // namespace mozilla
