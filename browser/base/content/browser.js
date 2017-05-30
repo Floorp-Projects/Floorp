4ec48b17be5c3f5fe86d1e42068d7c52c65e8573
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-05-30 08:51:22 +0200

    Backed out changeset 2b92af4575ae (bug 1357386)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1426,11 +1426,7 @@ var gBrowserInit = {
         // This function throws for certain malformed URIs, so use exception handling
         // so that we don't disrupt startup
         try {
-          gBrowser.loadTabs(specs, {
-            inBackground: false,
-            replace: true,
-            // Bug 1365232, provide correct triggeringPrincipal
-          });
+          gBrowser.loadTabs(specs, false, true);
         } catch (e) {}
       } else if (uriToLoad instanceof XULElement) {
         // swap the given tab with the default about:blank tab and then close
@@ -2168,10 +2164,7 @@ function BrowserGoHome(aEvent) {
   case "tab":
     urls = homePage.split("|");
     var loadInBackground = getBoolPref("browser.tabs.loadBookmarksInBackground", false);
-    gBrowser.loadTabs(urls, {
-      inBackground: loadInBackground,
-      triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
-    });
+    gBrowser.loadTabs(urls, loadInBackground);
     break;
   case "window":
     OpenBrowserWindow();
@@ -2189,11 +2182,7 @@ function loadOneOrMoreURIs(aURIString) {
   // This function throws for certain malformed URIs, so use exception handling
   // so that we don't disrupt startup
   try {
-    gBrowser.loadTabs(aURIString.split("|"), {
-      inBackground: false,
-      replace: true,
-      // Bug 1365232, provide correct triggeringPrincipal
-    });
+    gBrowser.loadTabs(aURIString.split("|"), false, true);
   } catch (e) {
   }
 }
@@ -5840,15 +5829,14 @@ function stripUnsafeProtocolOnPaste(pasteData) {
 }
 
 // handleDroppedLink has the following 2 overloads:
-//   handleDroppedLink(event, url, name, triggeringPrincipal)
-//   handleDroppedLink(event, links, triggeringPrincipal)
-function handleDroppedLink(event, urlOrLinks, nameOrTriggeringPrincipal, triggeringPrincipal) {
+//   handleDroppedLink(event, url, name)
+//   handleDroppedLink(event, links)
+function handleDroppedLink(event, urlOrLinks, name) {
   let links;
   if (Array.isArray(urlOrLinks)) {
     links = urlOrLinks;
-    triggeringPrincipal = nameOrTriggeringPrincipal;
   } else {
-    links = [{ url: urlOrLinks, nameOrTriggeringPrincipal, type: "" }];
+    links = [{ url: urlOrLinks, name, type: "" }];
   }
 
   let lastLocationChange = gBrowser.selectedBrowser.lastLocationChange;
@@ -5879,7 +5867,6 @@ function handleDroppedLink(event, urlOrLinks, nameOrTriggeringPrincipal, trigger
         allowThirdPartyFixup: false,
         postDatas,
         userContextId,
-        triggeringPrincipal,
       });
     }
   })();
