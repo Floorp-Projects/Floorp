f81a2233723928474a34acfbd4e81be42d57aaa3
Grigory Kruglov <gkruglov@mozilla.com>
2017-05-29 21:48:05 -0400

    Bug 1308337 - Part 4: Instrument FetchMetaGlobal stage r=nalexander
    MozReview-Commit-ID: 6zXqgsAIajN

--- mobile/android/services/src/main/java/org/mozilla/gecko/sync/stage/FetchMetaGlobalStage.java
+++ mobile/android/services/src/main/java/org/mozilla/gecko/sync/stage/FetchMetaGlobalStage.java
@@ -4,17 +4,25 @@
 
 package org.mozilla.gecko.sync.stage;
 
+import android.os.SystemClock;
+
 import org.mozilla.gecko.background.common.log.Logger;
 import org.mozilla.gecko.sync.GlobalSession;
+import org.mozilla.gecko.sync.HTTPFailureException;
 import org.mozilla.gecko.sync.InfoCollections;
 import org.mozilla.gecko.sync.MetaGlobal;
 import org.mozilla.gecko.sync.PersistedMetaGlobal;
 import org.mozilla.gecko.sync.delegates.MetaGlobalDelegate;
 import org.mozilla.gecko.sync.net.SyncStorageResponse;
+import org.mozilla.gecko.sync.telemetry.TelemetryCollector;
 
 public class FetchMetaGlobalStage extends AbstractNonRepositorySyncStage {
   private static final String LOG_TAG = "FetchMetaGlobalStage";
   private static final String META_COLLECTION = "meta";
+
+  private static final String TELEMETRY_ERROR_NAME = "metaglobal";
+  private static final String TELEMETRY_ERROR_MISSING = "missing";
+  private static final String TELEMETRY_ERROR_NO_INFO_COLLECTIONS = "noic";
 
   public class StageMetaGlobalDelegate implements MetaGlobalDelegate {
 
@@ -31,21 +39,34 @@ public class FetchMetaGlobalStage extends AbstractNonRepositorySyncStage {
       // Take the timestamp from the response since it is later than the timestamp from info/collections.
       pmg.persistLastModified(response.normalizedWeaveTimestamp());
 
-      session.processMetaGlobal(global);
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+      session.processMetaGlobal(global, telemetryStageCollector);
     }
 
     @Override
     public void handleFailure(SyncStorageResponse response) {
+      telemetryStageCollector.error = new TelemetryCollector.StageErrorBuilder()
+              .setLastException(new HTTPFailureException(response))
+              .build();
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
       session.handleHTTPError(response, "Failure fetching meta/global.");
     }
 
     @Override
     public void handleError(Exception e) {
+      telemetryStageCollector.error = new TelemetryCollector.StageErrorBuilder()
+              .setLastException(e)
+              .build();
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
       session.abort(e, "Failure fetching meta/global.");
     }
 
     @Override
     public void handleMissing(MetaGlobal global, SyncStorageResponse response) {
+      // While not strictly an error, it's good to keep track of this.
+      telemetryStageCollector.error = new TelemetryCollector
+              .StageErrorBuilder(TELEMETRY_ERROR_NAME, TELEMETRY_ERROR_MISSING)
+              .build();
       session.processMissingMetaGlobal(global);
     }
   }
@@ -54,6 +75,10 @@ public class FetchMetaGlobalStage extends AbstractNonRepositorySyncStage {
   public void execute() throws NoSuchStageException {
     InfoCollections infoCollections = session.config.infoCollections;
     if (infoCollections == null) {
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+      telemetryStageCollector.error = new TelemetryCollector
+              .StageErrorBuilder(TELEMETRY_ERROR_NAME, TELEMETRY_ERROR_NO_INFO_COLLECTIONS)
+              .build();
       session.abort(null, "No info/collections set in FetchMetaGlobalStage.");
       return;
     }
@@ -65,7 +90,8 @@ public class FetchMetaGlobalStage extends AbstractNonRepositorySyncStage {
       MetaGlobal global = session.config.persistedMetaGlobal().metaGlobal(session.config.metaURL(), session.getAuthHeaderProvider());
       if (global != null) {
         Logger.info(LOG_TAG, "Using persisted meta/global for this session.");
-        session.processMetaGlobal(global); // Calls session.advance().
+        telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+        session.processMetaGlobal(global, telemetryStageCollector); // Calls session.advance().
         return;
       }
       Logger.info(LOG_TAG, "Failed to use persisted meta/global for this session.");
