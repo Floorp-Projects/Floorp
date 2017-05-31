76f9a761a7dab7cca8e46883509a7d0f6fd54357
Markus Stange <mstange@themasta.com>
2017-05-31 00:35:15 -0400

    Bug 1330184 - Use threadsafe refcounting for nsProfilerStartParams. r=njn
    MozReview-Commit-ID: HVihlkBr4r3
    
    --HG--
    extra : rebase_source : 7ecd602f8fd3fb1c7b290e0ff88940eb9f774b42

--- tools/profiler/gecko/nsProfilerStartParams.h
+++ tools/profiler/gecko/nsProfilerStartParams.h
@@ -13,7 +13,10 @@
 class nsProfilerStartParams : public nsIProfilerStartParams
 {
 public:
-  NS_DECL_ISUPPORTS
+  // This class can be used on multiple threads. For example, it's used for the
+  // observer notification from profiler_start, which can run on any thread but
+  // posts the notification to the main thread.
+  NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIPROFILERSTARTPARAMS
 
   nsProfilerStartParams(uint32_t aEntries,
