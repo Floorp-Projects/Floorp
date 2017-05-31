febe32387f87fbe64f730f4686159482bdd8d1f4
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-05-31 22:53:16 +0200

    Backed out changeset bf392a1d0c5a (bug 1368560) for failing xpcshell's test_ext_storage_sync.js and test_ext_storage_sync_crypto.js on Linux. r=backout on a CLOSED TREE

--- services/sync/tests/unit/test_utils_passphrase.js
+++ services/sync/tests/unit/test_utils_passphrase.js
@@ -1,9 +1,51 @@
 Cu.import("resource://services-sync/util.js");
 
 function run_test() {
+  _("Generated passphrase has length 26.");
+  let pp = Utils.generatePassphrase();
+  do_check_eq(pp.length, 26);
+
+  const key = "abcdefghijkmnpqrstuvwxyz23456789";
+  _("Passphrase only contains [" + key + "].");
+  do_check_true(pp.split("").every(chr => key.indexOf(chr) != -1));
+
+  _("Hyphenated passphrase has 5 hyphens.");
+  let hyphenated = Utils.hyphenatePassphrase(pp);
+  _("H: " + hyphenated);
+  do_check_eq(hyphenated.length, 31);
+  do_check_eq(hyphenated[1], "-");
+  do_check_eq(hyphenated[7], "-");
+  do_check_eq(hyphenated[13], "-");
+  do_check_eq(hyphenated[19], "-");
+  do_check_eq(hyphenated[25], "-");
+  do_check_eq(pp,
+      hyphenated.slice(0, 1) + hyphenated.slice(2, 7)
+      + hyphenated.slice(8, 13) + hyphenated.slice(14, 19)
+      + hyphenated.slice(20, 25) + hyphenated.slice(26, 31));
+
+  _("Arbitrary hyphenation.");
+  // We don't allow invalid characters for our base32 character set.
+  do_check_eq(Utils.hyphenatePassphrase("1234567"), "2-34567");  // Not partial, so no trailing dash.
+  do_check_eq(Utils.hyphenatePassphrase("1234567890"), "2-34567-89");
+  do_check_eq(Utils.hyphenatePassphrase("abcdeabcdeabcdeabcdeabcde"), "a-bcdea-bcdea-bcdea-bcdea-bcde");
+  do_check_eq(Utils.hyphenatePartialPassphrase("1234567"), "2-34567-");
+  do_check_eq(Utils.hyphenatePartialPassphrase("1234567890"), "2-34567-89");
+  do_check_eq(Utils.hyphenatePartialPassphrase("abcdeabcdeabcdeabcdeabcde"), "a-bcdea-bcdea-bcdea-bcdea-bcde");
+
+  do_check_eq(Utils.hyphenatePartialPassphrase("a"), "a-");
+  do_check_eq(Utils.hyphenatePartialPassphrase("1234567"), "2-34567-");
+  do_check_eq(Utils.hyphenatePartialPassphrase("a-bcdef-g"),
+              "a-bcdef-g");
+  do_check_eq(Utils.hyphenatePartialPassphrase("abcdefghijklmnop"),
+              "a-bcdef-ghijk-mnp");
+  do_check_eq(Utils.hyphenatePartialPassphrase("abcdefghijklmnopabcde"),
+              "a-bcdef-ghijk-mnpab-cde");
+  do_check_eq(Utils.hyphenatePartialPassphrase("a-bcdef-ghijk-LMNOP-ABCDE-Fg"),
+              "a-bcdef-ghijk-mnpab-cdefg-");
+  // Cuts off.
+  do_check_eq(Utils.hyphenatePartialPassphrase("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").length, 31);
+
   _("Normalize passphrase recognizes hyphens.");
-  const pp = "26ect2thczm599m2ffqarbicjq";
-  const hyphenated = "2-6ect2-thczm-599m2-ffqar-bicjq";
   do_check_eq(Utils.normalizePassphrase(hyphenated), pp);
 
   _("Skip whitespace.");
