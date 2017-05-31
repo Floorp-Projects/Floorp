02e12853abdfa06980d457ac13032cd3ffeec0f0
Emilio Cobos Álvarez <emilio@crisal.io>
2017-05-31 13:12:33 +0200

    Bug 1368986: Cleanup stylesheet copy-constructor. r=heycam
    MozReview-Commit-ID: I8vUuJfET24

--- layout/style/CSSStyleSheet.cpp
+++ layout/style/CSSStyleSheet.cpp
@@ -355,15 +355,18 @@ CSSStyleSheet::CSSStyleSheet(const CSSStyleSheet& aCopy,
                              dom::CSSImportRule* aOwnerRuleToUse,
                              nsIDocument* aDocumentToUse,
                              nsINode* aOwningNodeToUse)
-  : StyleSheet(aCopy, aOwnerRuleToUse, aDocumentToUse, aOwningNodeToUse),
-    mInRuleProcessorCache(false),
-    mScopeElement(nullptr),
-    mRuleProcessors(nullptr)
+  : StyleSheet(aCopy,
+               aParentToUse,
+               aOwnerRuleToUse,
+               aDocumentToUse,
+               aOwningNodeToUse)
+  , mInRuleProcessorCache(false)
+  , mScopeElement(nullptr)
+  , mRuleProcessors(nullptr)
 {
-  mParent = aParentToUse;
-
   if (mDirty) { // CSSOM's been there, force full copy now
-    NS_ASSERTION(mInner->mComplete, "Why have rules been accessed on an incomplete sheet?");
+    NS_ASSERTION(mInner->mComplete,
+                 "Why have rules been accessed on an incomplete sheet?");
     // FIXME: handle failure?
     //
     // NOTE: It's important to call this from the subclass, since it could
