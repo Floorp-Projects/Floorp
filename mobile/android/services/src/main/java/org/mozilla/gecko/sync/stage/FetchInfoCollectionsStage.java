31f9e82f6f8e7f7839fdb7928dbb3a4b3dc2a730
Grigory Kruglov <gkruglov@mozilla.com>
2017-05-31 17:38:30 -0400

    Bug 1308337 - Part 5: Instrument FetchInfoCollectionsStage r=nalexander
    MozReview-Commit-ID: 64jTO81tDpK
    
    --HG--
    extra : rebase_source : 801c9c27730c217c6cf2d6e2bf086ef78f36f3a4

--- mobile/android/services/src/main/java/org/mozilla/gecko/sync/stage/FetchInfoCollectionsStage.java
+++ mobile/android/services/src/main/java/org/mozilla/gecko/sync/stage/FetchInfoCollectionsStage.java
@@ -4,12 +4,16 @@
 
 package org.mozilla.gecko.sync.stage;
 
+import android.os.SystemClock;
+
 import java.net.URISyntaxException;
 
 import org.mozilla.gecko.sync.ExtendedJSONObject;
+import org.mozilla.gecko.sync.HTTPFailureException;
 import org.mozilla.gecko.sync.InfoCollections;
 import org.mozilla.gecko.sync.delegates.JSONRecordFetchDelegate;
 import org.mozilla.gecko.sync.net.SyncStorageResponse;
+import org.mozilla.gecko.sync.telemetry.TelemetryCollector;
 
 public class FetchInfoCollectionsStage extends AbstractNonRepositorySyncStage {
   public class StageInfoCollectionsDelegate implements JSONRecordFetchDelegate {
@@ -17,16 +21,25 @@ public class FetchInfoCollectionsStage extends AbstractNonRepositorySyncStage {
     @Override
     public void handleSuccess(ExtendedJSONObject global) {
       session.config.infoCollections = new InfoCollections(global);
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
       session.advance();
     }
 
     @Override
     public void handleFailure(SyncStorageResponse response) {
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+      telemetryStageCollector.error = new TelemetryCollector.StageErrorBuilder()
+              .setLastException(new HTTPFailureException(response))
+              .build();
       session.handleHTTPError(response, "Failure fetching info/collections.");
     }
 
     @Override
     public void handleError(Exception e) {
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+      telemetryStageCollector.error = new TelemetryCollector.StageErrorBuilder()
+              .setLastException(e)
+              .build();
       session.abort(e, "Failure fetching info/collections.");
     }
 
@@ -37,6 +50,10 @@ public class FetchInfoCollectionsStage extends AbstractNonRepositorySyncStage {
     try {
       session.fetchInfoCollections(new StageInfoCollectionsDelegate());
     } catch (URISyntaxException e) {
+      telemetryStageCollector.finished = SystemClock.elapsedRealtime();
+      telemetryStageCollector.error = new TelemetryCollector.StageErrorBuilder()
+              .setLastException(e)
+              .build();
       session.abort(e, "Invalid URI.");
     }
   }
