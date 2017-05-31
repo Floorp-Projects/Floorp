39885c641198a90ade8efdcbba718445ebca4dd0
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-05-31 08:34:22 +0200

    Backed out changeset bab63755b35d (bug 1357005) for eslint failure

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1671,6 +1671,3 @@ pref("urlclassifier.phishTable", "goog-phish-shavar,goog-phish-proto,test-phish-
 #endif
 
 pref("browser.suppress_first_window_animation", true);
-
-// Preferences for Photon onboarding system extension
-pref("browser.onboarding.disabled", false);
