a683839564b8511d715722f2e0b7d5937afb2bb6
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-05-31 14:21:57 +0200

    Backed out changeset 8966fcaed76d (bug 1367040)

--- testing/web-platform/tests/tools/browserutils/utils.py
+++ testing/web-platform/tests/tools/browserutils/utils.py
@@ -6,6 +6,8 @@ import tarfile
 import zipfile
 from io import BytesIO
 
+import requests
+
 logger = logging.getLogger(__name__)
 
 
@@ -105,8 +107,6 @@ class pwd(object):
 
 def get(url):
     """Issue GET request to a given URL and return the response."""
-    import requests
-
     logger.debug("GET %s" % url)
     resp = requests.get(url, stream=True)
     resp.raise_for_status()
