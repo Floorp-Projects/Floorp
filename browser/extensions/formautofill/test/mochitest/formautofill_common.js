65c157f987352f45a8c3d5c8b1c4dd8f8bf9f58b
Ray Lin <ralin@mozilla.com>
2017-06-01 21:53:37 +0800

    Bug 1300995 - Part 1. Add a footer on formautofill popup to let users open a preferences privacy tab when click on it. r=MattN
    MozReview-Commit-ID: Izr6IbHlkLY

--- browser/extensions/formautofill/test/mochitest/formautofill_common.js
+++ browser/extensions/formautofill/test/mochitest/formautofill_common.js
@@ -43,10 +43,12 @@ async function onAddressChanged(type) {
   });
 }
 
-function checkMenuEntries(expectedValues) {
+function checkMenuEntries(expectedValues, isFormAutofillResult = true) {
   let actualValues = getMenuEntries();
+  // Expect one more item would appear at the bottom as the footer if the result is from form autofill.
+  let expectedLength = isFormAutofillResult ? expectedValues.length + 1 : expectedValues.length;
 
-  is(actualValues.length, expectedValues.length, " Checking length of expected menu");
+  is(actualValues.length, expectedLength, " Checking length of expected menu");
   for (let i = 0; i < expectedValues.length; i++) {
     is(actualValues[i], expectedValues[i], " Checking menu entry #" + i);
   }
