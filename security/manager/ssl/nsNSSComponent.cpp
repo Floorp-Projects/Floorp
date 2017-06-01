835cb956cc703cc1ba4559a62118a0b94ce4e4fb
EKR <ekr@rtfm.com>
2017-06-01 10:17:00 +0200

    Bug 1368599 - Disable TLS 1.3 by default for Release 54 r=dkeeler a=ritu

--- security/manager/ssl/nsNSSComponent.cpp
+++ security/manager/ssl/nsNSSComponent.cpp
@@ -1623,7 +1623,7 @@ nsNSSComponent::setEnabledTLSVersions()
   // keep these values in sync with security-prefs.js
   // 1 means TLS 1.0, 2 means TLS 1.1, etc.
   static const uint32_t PSM_DEFAULT_MIN_TLS_VERSION = 1;
-  static const uint32_t PSM_DEFAULT_MAX_TLS_VERSION = 4;
+  static const uint32_t PSM_DEFAULT_MAX_TLS_VERSION = 3;
 
   uint32_t minFromPrefs = Preferences::GetUint("security.tls.version.min",
                                                PSM_DEFAULT_MIN_TLS_VERSION);
