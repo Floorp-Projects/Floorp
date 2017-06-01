e51b07a10f47b5282350ff13118bd4e825c8a707
Ehsan Akhgari <ehsan@mozilla.com>
2017-06-01 10:20:29 -0400

    Bug 1369140 - Part 4: Test that window opening no longer incurs a reflow in focusAndSelectUrlBar

--- browser/base/content/test/performance/browser_windowopen_reflows.js
+++ browser/base/content/test/performance/browser_windowopen_reflows.js
@@ -16,14 +16,12 @@ const EXPECTED_REFLOWS = [
   // Selecting the address bar causes two reflows, unfortunately.
   [
     "select@chrome://global/content/bindings/textbox.xml",
-    "focusAndSelectUrlBar@chrome://browser/content/browser.js",
     "_delayedStartup@chrome://browser/content/browser.js",
   ],
 
   // Selecting the address bar causes two reflows, unfortunately.
   [
     "select@chrome://global/content/bindings/textbox.xml",
-    "focusAndSelectUrlBar@chrome://browser/content/browser.js",
     "_delayedStartup@chrome://browser/content/browser.js",
   ],
 ];
