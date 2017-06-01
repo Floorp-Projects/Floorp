b3f93cf1409c86f9399d619a85556dced65149d3
Ryan VanderMeulen <ryanvm@gmail.com>
2017-06-01 11:12:04 -0400

    Backed out 4 changesets (bug 1363505) to avoid conflicts with bug 1369140 on inbound.
    Backed out changeset c6b6bc96e99c (bug 1363505)
    Backed out changeset 494590d4307a (bug 1363505)
    Backed out changeset 6f6b73f4f305 (bug 1363505)
    Backed out changeset 0c50bc93a3dc (bug 1363505)

--- browser/base/content/test/performance/browser_tabclose_reflows.js
+++ browser/base/content/test/performance/browser_tabclose_reflows.js
@@ -28,7 +28,27 @@ if (gMultiProcessBrowser) {
  * uninterruptible reflows when closing new tabs.
  */
 add_task(async function() {
-  await ensureNoPreloadedBrowser();
+  // If we've got a preloaded browser, get rid of it so that it
+  // doesn't interfere with the test if it's loading. We have to
+  // do this before we disable preloading or changing the new tab
+  // URL, otherwise _getPreloadedBrowser will return null, despite
+  // the preloaded browser existing.
+  let preloaded = gBrowser._getPreloadedBrowser();
+  if (preloaded) {
+    preloaded.remove();
+  }
+
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.newtab.preload", false]],
+  });
+
+  let aboutNewTabService = Cc["@mozilla.org/browser/aboutnewtab-service;1"]
+                             .getService(Ci.nsIAboutNewTabService);
+  aboutNewTabService.newTabURL = "about:blank";
+
+  registerCleanupFunction(() => {
+    aboutNewTabService.resetNewTabURL();
+  });
 
   // Because the tab strip is a scrollable frame, we can't use the
   // default dirtying function from withReflowObserver and reliably
