5c4ea689ce95b79fa79ee7be44e4aa89cd9bbdb0
Nicholas Nethercote <nnethercote@mozilla.com>
2017-06-02 08:16:00 +1000

    Bug 1369280 (part 1) - Reindent nsProfiler.h. r=mstange.

--- tools/profiler/gecko/nsProfiler.h
+++ tools/profiler/gecko/nsProfiler.h
@@ -1,10 +1,11 @@
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef _NSPROFILER_H_
-#define _NSPROFILER_H_
+#ifndef nsProfiler_h
+#define nsProfiler_h
 
 #include "nsIProfiler.h"
 #include "nsIObserver.h"
@@ -18,29 +19,29 @@ class ProfileGatherer;
 class nsProfiler final : public nsIProfiler, public nsIObserver
 {
 public:
-    nsProfiler();
+  nsProfiler();
 
-    NS_DECL_ISUPPORTS
-    NS_DECL_NSIOBSERVER
-    NS_DECL_NSIPROFILER
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+  NS_DECL_NSIPROFILER
 
-    nsresult Init();
+  nsresult Init();
 
-    static nsProfiler* GetOrCreate()
-    {
-	nsCOMPtr<nsIProfiler> iprofiler =
-	    do_GetService("@mozilla.org/tools/profiler;1");
-	return static_cast<nsProfiler*>(iprofiler.get());
-    }
+  static nsProfiler* GetOrCreate()
+  {
+    nsCOMPtr<nsIProfiler> iprofiler =
+      do_GetService("@mozilla.org/tools/profiler;1");
+    return static_cast<nsProfiler*>(iprofiler.get());
+  }
 
-    void GatheredOOPProfile(const nsACString& aProfile);
+  void GatheredOOPProfile(const nsACString& aProfile);
 
 private:
-    ~nsProfiler();
+  ~nsProfiler();
 
-    RefPtr<mozilla::ProfileGatherer> mGatherer;
-    bool mLockedForPrivateBrowsing;
+  RefPtr<mozilla::ProfileGatherer> mGatherer;
+  bool mLockedForPrivateBrowsing;
 };
 
-#endif /* _NSPROFILER_H_ */
+#endif // nsProfiler_h
 
