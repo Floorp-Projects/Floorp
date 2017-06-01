d0db46dda1896d68185d873531bd9ddd6b3b3914
Mike Conley <mconley@mozilla.com>
2017-06-01 11:37:31 -0400

    Bug 1369140 - Part 6: Adjust the last few reflow tests to reflect reality. r=bustage-fix on a CLOSED TREE
    MozReview-Commit-ID: 4mVMX4GzZ7X
    
    --HG--
    extra : amend_source : 9990085a0c924d17a34cdf8035bf00b5e156bae3

--- browser/base/content/test/performance/browser_tabopen_reflows.js
+++ browser/base/content/test/performance/browser_tabopen_reflows.js
@@ -18,16 +18,6 @@ const EXPECTED_REFLOWS = [
   [
     "select@chrome://global/content/bindings/textbox.xml",
   ],
-
-  // selection change notification may cause querying the focused editor content
-  // by IME and that will cause reflow.
-  [
-    "select@chrome://global/content/bindings/textbox.xml",
-  ],
-
-  [
-    "select@chrome://global/content/bindings/textbox.xml",
-  ],
 ];
 
 /*
