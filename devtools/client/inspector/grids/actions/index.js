988f3b98971e9e2f6e5e2f24272cc22aae086ef7
Micah Tigley <tigleym@gmail.com>
2017-05-31 18:37:43 -0600

    Bug 1367627 - Show overlay of grid areas in Grid Highlighter. r=gl
    MozReview-Commit-ID: 9KWPltfpWlT

--- devtools/client/inspector/grids/actions/index.js
+++ devtools/client/inspector/grids/actions/index.js
@@ -17,6 +17,9 @@ createEnum([
   // Update the entire grids state with the new list of grids.
   "UPDATE_GRIDS",
 
+  // Update the grid highlighter's show grid areas state.
+  "UPDATE_SHOW_GRID_AREAS",
+
   // Update the grid highlighter's show grid line numbers state.
   "UPDATE_SHOW_GRID_LINE_NUMBERS",
 
