1350aee458b124e46469de48383032633cbc9bd2
Doug Thayer <dothayer@mozilla.com>
2017-06-02 15:13:23 -0700

    Bug 1369899 - Listen to MozDOMFullscreen events r=Gijs
    While we are now correctly evaluating the fullscreen situation
    in OSX so that our update doorhangers behave as if we're in
    windowed mode, we aren't correctly listening for state changes.
    This addresses that, and tries to limit the chattiness by only
    listening to the fullscreen events that we care about.
    
    MozReview-Commit-ID: 9J009l4w21E

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -60,7 +60,33 @@ const PanelUI = {
     Services.obs.addObserver(this, "fullscreen-nav-toolbox");
     Services.obs.addObserver(this, "appMenu-notifications");
 
-    window.addEventListener("fullscreen", this);
+    XPCOMUtils.defineLazyPreferenceGetter(this, "autoHideToolbarInFullScreen",
+      "browser.fullscreen.autohide", false, (pref, previousValue, newValue) => {
+        // On OSX, or with autohide preffed off, MozDOMFullscreen is the only
+        // event we care about, since fullscreen should behave just like non
+        // fullscreen. Otherwise, we don't want to listen to these because
+        // we'd just be spamming ourselves with both of them whenever a user
+        // opened a video.
+        if (newValue) {
+          window.removeEventListener("MozDOMFullscreen:Entered", this);
+          window.removeEventListener("MozDOMFullscreen:Exited", this);
+          window.addEventListener("fullscreen", this);
+        } else {
+          window.addEventListener("MozDOMFullscreen:Entered", this);
+          window.addEventListener("MozDOMFullscreen:Exited", this);
+          window.removeEventListener("fullscreen", this);
+        }
+
+        this._updateNotifications(false);
+      }, autoHidePref => autoHidePref && Services.appinfo.OS !== "Darwin");
+
+    if (this.autoHideToolbarInFullScreen) {
+      window.addEventListener("fullscreen", this);
+    } else {
+      window.addEventListener("MozDOMFullscreen:Entered", this);
+      window.addEventListener("MozDOMFullscreen:Exited", this);
+    }
+
     window.addEventListener("activate", this);
     window.matchMedia("(-moz-overlay-scrollbars)").addListener(this._overlayScrollListenerBoundFn);
     CustomizableUI.addListener(this);
@@ -175,6 +201,8 @@ const PanelUI = {
     Services.obs.removeObserver(this, "fullscreen-nav-toolbox");
     Services.obs.removeObserver(this, "appMenu-notifications");
 
+    window.removeEventListener("MozDOMFullscreen:Entered", this);
+    window.removeEventListener("MozDOMFullscreen:Exited", this);
     window.removeEventListener("fullscreen", this);
     window.removeEventListener("activate", this);
     this.menuButton.removeEventListener("mousedown", this);
@@ -326,6 +354,8 @@ const PanelUI = {
       case "keypress":
         this.toggle(aEvent);
         break;
+      case "MozDOMFullscreen:Entered":
+      case "MozDOMFullscreen:Exited":
       case "fullscreen":
       case "activate":
         this._updateNotifications();
@@ -744,10 +774,8 @@ const PanelUI = {
         this._showBannerItem(notifications[0]);
       }
     } else if (doorhangers.length > 0) {
-      let autoHideFullScreen = Services.prefs.getBoolPref("browser.fullscreen.autohide", false) &&
-                               Services.appinfo.OS !== "Darwin";
       // Only show the doorhanger if the window is focused and not fullscreen
-      if ((window.fullScreen && autoHideFullScreen) || Services.focus.activeWindow !== window) {
+      if ((window.fullScreen && this.autoHideToolbarInFullScreen) || Services.focus.activeWindow !== window) {
         this._hidePopup();
         this._showBadge(doorhangers[0]);
         this._showBannerItem(doorhangers[0]);
