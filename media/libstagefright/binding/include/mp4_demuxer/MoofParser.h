1c8aac2556af9c051441a39c49852fed2f19d187
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-06-02 21:15:10 +0200

    Bug 1367128: P2. Add methods to trim index. r=gerald
    The MoofParser's used by the MP4Demuxer, can grow very large as it keeps adding new tables and is never reset.
    
    Once we have demuxed all data present in the MP4 media segment, we no longer require the samples table for that media segment and we can drop it from the index.
    
    Unfortunately. some websites (in particular some using live video) use media segments containing a single sample in order to (incorrectly) reduce latency. While this is a very bad approach from a performance perspective it does happen in the wild.
    
    MozReview-Commit-ID: I66jxcScmKM

--- media/libstagefright/binding/include/mp4_demuxer/MoofParser.h
+++ media/libstagefright/binding/include/mp4_demuxer/MoofParser.h
@@ -262,6 +262,11 @@ public:
   }
   bool RebuildFragmentedIndex(
     const mozilla::MediaByteRangeSet& aByteRanges);
+  // If *aCanEvict is set to true. then will remove all moofs already parsed
+  // from index then rebuild the index. *aCanEvict is set to true upon return if
+  // some moofs were removed.
+  bool RebuildFragmentedIndex(
+    const mozilla::MediaByteRangeSet& aByteRanges, bool* aCanEvict);
   bool RebuildFragmentedIndex(BoxContext& aContext);
   Interval<Microseconds> GetCompositionRange(
     const mozilla::MediaByteRangeSet& aByteRanges);
