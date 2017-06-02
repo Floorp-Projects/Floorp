988dbf1864012cf4a8e5d43e30e748ea280d314c
Mike Hommey <mh+mozilla@glandium.org>
2017-06-02 14:01:55 +0900

    Bug 1369615 - Avoid unnecessary struct field shorthand in webrender. r=kats
    This applies the patch from https://github.com/servo/webrender/pull/1330
    without pulling all the recent changes to webrender.
    
    Bug 1321847 will ensure further updates don't break building with older
    versions of rustc without meaning to.

--- gfx/webrender/src/renderer.rs
+++ gfx/webrender/src/renderer.rs
@@ -1058,7 +1058,7 @@ impl Renderer {
 
         let config = FrameBuilderConfig {
             enable_scrollbars: options.enable_scrollbars,
-            default_font_render_mode,
+            default_font_render_mode: default_font_render_mode,
             debug: options.debug,
             cache_expiry_frames: options.cache_expiry_frames,
         };
