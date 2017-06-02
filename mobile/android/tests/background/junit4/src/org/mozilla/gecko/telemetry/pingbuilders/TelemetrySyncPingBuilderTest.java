fa406e74f2dd330ac52b6645db17040342339584
Grigory Kruglov <gkruglov@mozilla.com>
2017-06-02 19:10:59 -0400

    Bug 1369891 - Improve sync ping tests and stop them from failing in automation r=nalexander
    Switch to explicit checks of contents of json objects, vs checking against toString output.
    
    MozReview-Commit-ID: A9YqG3A2My5
    
    --HG--
    extra : rebase_source : 9ea8dc9769c7e950c2e08e7acbe2384502c0621a

--- mobile/android/tests/background/junit4/src/org/mozilla/gecko/telemetry/pingbuilders/TelemetrySyncPingBuilderTest.java
+++ mobile/android/tests/background/junit4/src/org/mozilla/gecko/telemetry/pingbuilders/TelemetrySyncPingBuilderTest.java
@@ -6,6 +6,9 @@ package org.mozilla.gecko.telemetry.pingbuilders;
 import android.os.Bundle;
 import android.os.Parcelable;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.simple.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,10 +40,24 @@ public class TelemetrySyncPingBuilderTest {
                 .setRestarted(false)
                 .build();
         ExtendedJSONObject payload = localPing.getPayload();
-        assertEquals(
-                "{\"uid\":\"uid\",\"took\":123,\"deviceID\":\"device-id\",\"version\":1}",
-                payload.toString()
-        );
+        assertEquals("uid", payload.getString("uid"));
+        assertEquals(Long.valueOf(123L), payload.getLong("took"));
+        assertEquals("device-id", payload.getString("deviceID"));
+        assertEquals(Integer.valueOf(1), payload.getIntegerSafely("version"));
+        assertFalse(payload.containsKey("restarted"));
+
+        localPing = builder
+                .setDeviceID("device-id")
+                .setUID("uid")
+                .setTook(123L)
+                .setRestarted(true)
+                .build();
+        payload = localPing.getPayload();
+        assertEquals("uid", payload.getString("uid"));
+        assertEquals(Long.valueOf(123L), payload.getLong("took"));
+        assertEquals("device-id", payload.getString("deviceID"));
+        assertEquals(Integer.valueOf(1), payload.getIntegerSafely("version"));
+        assertEquals(true, payload.getBoolean("restarted"));
     }
 
     @Test
@@ -69,10 +86,9 @@ public class TelemetrySyncPingBuilderTest {
                 .setDevices(devices)
                 .build()
                 .getPayload();
-        assertEquals(
-                "{\"devices\":[{\"os\":\"Android\",\"id\":\"80daf12dsadsa4236914cff2cc6e9d0f80a965380e2cf8e976e4004ead887521b5d9\",\"version\":\"53.0a1\"}],\"version\":1}",
-                payload.toString()
-        );
+        JSONArray devicesJSON = payload.getArray("devices");
+        assertEquals(1, devicesJSON.size());
+        assertDevice((ExtendedJSONObject) devicesJSON.get(0), "Android", "53.0a1", "80daf12dsadsa4236914cff2cc6e9d0f80a965380e2cf8e976e4004ead887521b5d9");
 
         device = new Bundle();
         device.putString("os", "iOS");
@@ -85,9 +101,15 @@ public class TelemetrySyncPingBuilderTest {
                 .setDevices(devices)
                 .build()
                 .getPayload();
-        assertEquals(
-                "{\"devices\":[{\"os\":\"Android\",\"id\":\"80daf12dsadsa4236914cff2cc6e9d0f80a965380e2cf8e976e4004ead887521b5d9\",\"version\":\"53.0a1\"},{\"os\":\"iOS\",\"id\":\"fa813452774b3cdc8f5f73290b5346df800f644b7b92a1ab94b6e2af748d261362\",\"version\":\"8.0\"}],\"version\":1}",
-                payload.toString()
-        );
+        devicesJSON = payload.getArray("devices");
+        assertEquals(2, devicesJSON.size());
+        assertDevice((ExtendedJSONObject) devicesJSON.get(0), "Android", "53.0a1", "80daf12dsadsa4236914cff2cc6e9d0f80a965380e2cf8e976e4004ead887521b5d9");
+        assertDevice((ExtendedJSONObject) devicesJSON.get(1), "iOS", "8.0", "fa813452774b3cdc8f5f73290b5346df800f644b7b92a1ab94b6e2af748d261362");
+    }
+
+    private void assertDevice(ExtendedJSONObject device, String os, String version, String id) throws JSONException {
+        assertEquals(os, device.getString("os"));
+        assertEquals(version, device.getString("version"));
+        assertEquals(id, device.getString("id"));
     }
 }
