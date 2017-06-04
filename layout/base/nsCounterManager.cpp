8a1bde2ba0c34181cd9ec736463b30d6282995d6
Mats Palmgren <mats@mozilla.com>
2017-06-04 16:45:15 +0200

    Bug 1367209 part 4 - Use LookupForAdd to avoid an additional hashtable lookup when inserting a new entry.  r=jfkthame
    MozReview-Commit-ID: u24jLUvoCJ

--- layout/base/nsCounterManager.cpp
+++ layout/base/nsCounterManager.cpp
@@ -238,14 +238,9 @@ nsCounterManager::AddResetOrIncrement(nsIFrame* aFrame, int32_t aIndex,
 nsCounterList*
 nsCounterManager::CounterListFor(const nsSubstring& aCounterName)
 {
-  // XXX Why doesn't nsTHashtable provide an API that allows us to use
-  // get/put in one hashtable lookup?
-  nsCounterList* counterList;
-  if (!mNames.Get(aCounterName, &counterList)) {
-    counterList = new nsCounterList();
-    mNames.Put(aCounterName, counterList);
-  }
-  return counterList;
+  return mNames.LookupForAdd(aCounterName).OrInsert([]() {
+    return new nsCounterList();
+  });
 }
 
 void
