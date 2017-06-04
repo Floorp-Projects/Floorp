668f0116532f2f72ac152b3f1bed66395d6787cb
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-06-04 12:03:25 +0200

    Bug 1345342: Cancel pending requests. r=jya
    If the pending first frame decoding was cancelled, the next call to decode could lead to a crash.
    
    MozReview-Commit-ID: 6Q4eKUzqOly

--- dom/media/platforms/wrappers/H264Converter.cpp
+++ dom/media/platforms/wrappers/H264Converter.cpp
@@ -131,6 +131,10 @@ H264Converter::Decode(MediaRawData* aSample)
 RefPtr<MediaDataDecoder::FlushPromise>
 H264Converter::Flush()
 {
+  mDecodePromiseRequest.DisconnectIfExists();
+  mFlushRequest.DisconnectIfExists();
+  mShutdownRequest.DisconnectIfExists();
+  mDecodePromise.RejectIfExists(NS_ERROR_DOM_MEDIA_CANCELED, __func__);
   mNeedKeyframe = true;
   if (mDecoder) {
     return mDecoder->Flush();
