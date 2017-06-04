25a5c60c5a2ce7447f85e7cfed0cecee37357461
Mats Palmgren <mats@mozilla.com>
2017-06-04 16:45:15 +0200

    Bug 1367209 part 1 - Fix some indentation and code style issues.  r=jfkthame
    MozReview-Commit-ID: GrmWUb0XvBL

--- layout/base/nsCounterManager.cpp
+++ layout/base/nsCounterManager.cpp
@@ -18,11 +18,12 @@ using namespace mozilla;
 
 bool
 nsCounterUseNode::InitTextFrame(nsGenConList* aList,
-        nsIFrame* aPseudoFrame, nsIFrame* aTextFrame)
+                                nsIFrame* aPseudoFrame,
+                                nsIFrame* aTextFrame)
 {
   nsCounterNode::InitTextFrame(aList, aPseudoFrame, aTextFrame);
 
-  nsCounterList *counterList = static_cast<nsCounterList*>(aList);
+  nsCounterList* counterList = static_cast<nsCounterList*>(aList);
   counterList->Insert(this);
   bool dirty = counterList->IsDirty();
   if (!dirty) {
@@ -45,113 +46,117 @@ nsCounterUseNode::InitTextFrame(nsGenConList* aList,
 
 // assign the correct |mValueAfter| value to a node that has been inserted
 // Should be called immediately after calling |Insert|.
-void nsCounterUseNode::Calc(nsCounterList *aList)
+void
+nsCounterUseNode::Calc(nsCounterList* aList)
 {
-    NS_ASSERTION(!aList->IsDirty(),
-                 "Why are we calculating with a dirty list?");
-    mValueAfter = aList->ValueBefore(this);
+  NS_ASSERTION(!aList->IsDirty(),
+               "Why are we calculating with a dirty list?");
+  mValueAfter = aList->ValueBefore(this);
 }
 
 // assign the correct |mValueAfter| value to a node that has been inserted
 // Should be called immediately after calling |Insert|.
-void nsCounterChangeNode::Calc(nsCounterList *aList)
+void
+nsCounterChangeNode::Calc(nsCounterList* aList)
 {
-    NS_ASSERTION(!aList->IsDirty(),
-                 "Why are we calculating with a dirty list?");
-    if (mType == RESET) {
-        mValueAfter = mChangeValue;
-    } else {
-        NS_ASSERTION(mType == INCREMENT, "invalid type");
-        mValueAfter = nsCounterManager::IncrementCounter(aList->ValueBefore(this),
-                                                         mChangeValue);
-    }
+  NS_ASSERTION(!aList->IsDirty(), "Why are we calculating with a dirty list?");
+  if (mType == RESET) {
+    mValueAfter = mChangeValue;
+  } else {
+    NS_ASSERTION(mType == INCREMENT, "invalid type");
+    mValueAfter = nsCounterManager::IncrementCounter(aList->ValueBefore(this),
+                                                     mChangeValue);
+  }
 }
 
 // The text that should be displayed for this counter.
 void
 nsCounterUseNode::GetText(nsString& aResult)
 {
-    aResult.Truncate();
+  aResult.Truncate();
 
-    AutoTArray<nsCounterNode*, 8> stack;
-    stack.AppendElement(static_cast<nsCounterNode*>(this));
+  AutoTArray<nsCounterNode*, 8> stack;
+  stack.AppendElement(static_cast<nsCounterNode*>(this));
 
-    if (mAllCounters && mScopeStart)
-        for (nsCounterNode *n = mScopeStart; n->mScopePrev; n = n->mScopeStart)
-            stack.AppendElement(n->mScopePrev);
-
-    WritingMode wm = mPseudoFrame ?
-        mPseudoFrame->GetWritingMode() : WritingMode();
-    for (uint32_t i = stack.Length() - 1;; --i) {
-        nsCounterNode *n = stack[i];
-        nsAutoString text;
-        bool isTextRTL;
-        mCounterStyle->GetCounterText(n->mValueAfter, wm, text, isTextRTL);
-        aResult.Append(text);
-        if (i == 0)
-            break;
-        aResult.Append(mSeparator);
+  if (mAllCounters && mScopeStart) {
+    for (nsCounterNode* n = mScopeStart; n->mScopePrev; n = n->mScopeStart) {
+      stack.AppendElement(n->mScopePrev);
     }
+  }
+
+  WritingMode wm = mPseudoFrame ?
+    mPseudoFrame->GetWritingMode() : WritingMode();
+  for (uint32_t i = stack.Length() - 1;; --i) {
+    nsCounterNode* n = stack[i];
+    nsAutoString text;
+    bool isTextRTL;
+    mCounterStyle->GetCounterText(n->mValueAfter, wm, text, isTextRTL);
+    aResult.Append(text);
+    if (i == 0) {
+      break;
+    }
+    aResult.Append(mSeparator);
+  }
 }
 
 void
-nsCounterList::SetScope(nsCounterNode *aNode)
+nsCounterList::SetScope(nsCounterNode* aNode)
 {
-    // This function is responsible for setting |mScopeStart| and
-    // |mScopePrev| (whose purpose is described in nsCounterManager.h).
-    // We do this by starting from the node immediately preceding
-    // |aNode| in content tree order, which is reasonably likely to be
-    // the previous element in our scope (or, for a reset, the previous
-    // element in the containing scope, which is what we want).  If
-    // we're not in the same scope that it is, then it's too deep in the
-    // frame tree, so we walk up parent scopes until we find something
-    // appropriate.
+  // This function is responsible for setting |mScopeStart| and
+  // |mScopePrev| (whose purpose is described in nsCounterManager.h).
+  // We do this by starting from the node immediately preceding
+  // |aNode| in content tree order, which is reasonably likely to be
+  // the previous element in our scope (or, for a reset, the previous
+  // element in the containing scope, which is what we want).  If
+  // we're not in the same scope that it is, then it's too deep in the
+  // frame tree, so we walk up parent scopes until we find something
+  // appropriate.
 
-    if (aNode == First()) {
-        aNode->mScopeStart = nullptr;
-        aNode->mScopePrev = nullptr;
-        return;
-    }
-
-    // Get the content node for aNode's rendering object's *parent*,
-    // since scope includes siblings, so we want a descendant check on
-    // parents.
-    nsIContent *nodeContent = aNode->mPseudoFrame->GetContent()->GetParent();
-
-    for (nsCounterNode *prev = Prev(aNode), *start;
-         prev; prev = start->mScopePrev) {
-        // If |prev| starts a scope (because it's a real or implied
-        // reset), we want it as the scope start rather than the start
-        // of its enclosing scope.  Otherwise, there's no enclosing
-        // scope, so the next thing in prev's scope shares its scope
-        // start.
-        start = (prev->mType == nsCounterNode::RESET || !prev->mScopeStart)
-                  ? prev : prev->mScopeStart;
-
-        // |startContent| is analogous to |nodeContent| (see above).
-        nsIContent *startContent = start->mPseudoFrame->GetContent()->GetParent();
-        NS_ASSERTION(nodeContent || !startContent,
-                     "null check on startContent should be sufficient to "
-                     "null check nodeContent as well, since if nodeContent "
-                     "is for the root, startContent (which is before it) "
-                     "must be too");
-
-             // A reset's outer scope can't be a scope created by a sibling.
-        if (!(aNode->mType == nsCounterNode::RESET &&
-              nodeContent == startContent) &&
-              // everything is inside the root (except the case above,
-              // a second reset on the root)
-            (!startContent ||
-             nsContentUtils::ContentIsDescendantOf(nodeContent,
-                                                   startContent))) {
-            aNode->mScopeStart = start;
-            aNode->mScopePrev  = prev;
-            return;
-        }
-    }
-
+  if (aNode == First()) {
     aNode->mScopeStart = nullptr;
-    aNode->mScopePrev  = nullptr;
+    aNode->mScopePrev = nullptr;
+    return;
+  }
+
+  // Get the content node for aNode's rendering object's *parent*,
+  // since scope includes siblings, so we want a descendant check on
+  // parents.
+  nsIContent* nodeContent = aNode->mPseudoFrame->GetContent()->GetParent();
+
+  for (nsCounterNode* prev = Prev(aNode), *start;
+       prev; prev = start->mScopePrev) {
+    // If |prev| starts a scope (because it's a real or implied
+    // reset), we want it as the scope start rather than the start
+    // of its enclosing scope.  Otherwise, there's no enclosing
+    // scope, so the next thing in prev's scope shares its scope
+    // start.
+    start = (prev->mType == nsCounterNode::RESET || !prev->mScopeStart)
+      ? prev : prev->mScopeStart;
+
+    // |startContent| is analogous to |nodeContent| (see above).
+    nsIContent* startContent = start->mPseudoFrame->GetContent()->GetParent();
+    NS_ASSERTION(nodeContent || !startContent,
+                 "null check on startContent should be sufficient to "
+                 "null check nodeContent as well, since if nodeContent "
+                 "is for the root, startContent (which is before it) "
+                 "must be too");
+
+    // A reset's outer scope can't be a scope created by a sibling.
+    if (!(aNode->mType == nsCounterNode::RESET &&
+          nodeContent == startContent) &&
+        // everything is inside the root (except the case above,
+        // a second reset on the root)
+        (!startContent ||
+         nsContentUtils::ContentIsDescendantOf(nodeContent,
+                                               startContent))) {
+      aNode->mScopeStart = start;
+      aNode->mScopePrev  = prev;
+      return;
+    }
+  }
+
+  aNode->mScopeStart = nullptr;
+  aNode->mScopePrev  = nullptr;
 }
 
 void
@@ -164,7 +169,7 @@ nsCounterList::RecalcAll()
     node->Calc(this);
 
     if (node->mType == nsCounterNode::USE) {
-      nsCounterUseNode *useNode = node->UseNode();
+      nsCounterUseNode* useNode = node->UseNode();
       // Null-check mText, since if the frame constructor isn't
       // batching, we could end up here while the node is being
       // constructed.
@@ -183,25 +188,27 @@ nsCounterManager::nsCounterManager()
 }
 
 bool
-nsCounterManager::AddCounterResetsAndIncrements(nsIFrame *aFrame)
+nsCounterManager::AddCounterResetsAndIncrements(nsIFrame* aFrame)
 {
-    const nsStyleContent *styleContent = aFrame->StyleContent();
-    if (!styleContent->CounterIncrementCount() &&
-        !styleContent->CounterResetCount())
-        return false;
+  const nsStyleContent* styleContent = aFrame->StyleContent();
+  if (!styleContent->CounterIncrementCount() &&
+      !styleContent->CounterResetCount()) {
+    return false;
+  }
 
-    // Add in order, resets first, so all the comparisons will be optimized
-    // for addition at the end of the list.
-    int32_t i, i_end;
-    bool dirty = false;
-    for (i = 0, i_end = styleContent->CounterResetCount(); i != i_end; ++i)
-      dirty |= AddResetOrIncrement(aFrame, i, styleContent->CounterResetAt(i),
-                                   nsCounterChangeNode::RESET);
-    for (i = 0, i_end = styleContent->CounterIncrementCount(); i != i_end; ++i)
-      dirty |=
-        AddResetOrIncrement(aFrame, i, styleContent->CounterIncrementAt(i),
-                            nsCounterChangeNode::INCREMENT);
-    return dirty;
+  // Add in order, resets first, so all the comparisons will be optimized
+  // for addition at the end of the list.
+  int32_t i, i_end;
+  bool dirty = false;
+  for (i = 0, i_end = styleContent->CounterResetCount(); i != i_end; ++i) {
+    dirty |= AddResetOrIncrement(aFrame, i, styleContent->CounterResetAt(i),
+                                 nsCounterChangeNode::RESET);
+  }
+  for (i = 0, i_end = styleContent->CounterIncrementCount(); i != i_end; ++i) {
+    dirty |= AddResetOrIncrement(aFrame, i, styleContent->CounterIncrementAt(i),
+                                 nsCounterChangeNode::INCREMENT);
+  }
+  return dirty;
 }
 
 bool
@@ -213,7 +220,6 @@ nsCounterManager::AddResetOrIncrement(nsIFrame* aFrame, int32_t aIndex,
     new nsCounterChangeNode(aFrame, aType, aCounterData.mValue, aIndex);
 
   nsCounterList* counterList = CounterListFor(aCounterData.mCounter);
-
   counterList->Insert(node);
   if (!counterList->IsLast(node)) {
     // Tell the caller it's responsible for recalculating the entire
@@ -233,25 +239,25 @@ nsCounterManager::AddResetOrIncrement(nsIFrame* aFrame, int32_t aIndex,
 nsCounterList*
 nsCounterManager::CounterListFor(const nsSubstring& aCounterName)
 {
-    // XXX Why doesn't nsTHashtable provide an API that allows us to use
-    // get/put in one hashtable lookup?
-    nsCounterList *counterList;
-    if (!mNames.Get(aCounterName, &counterList)) {
-        counterList = new nsCounterList();
-        mNames.Put(aCounterName, counterList);
-    }
-    return counterList;
+  // XXX Why doesn't nsTHashtable provide an API that allows us to use
+  // get/put in one hashtable lookup?
+  nsCounterList* counterList;
+  if (!mNames.Get(aCounterName, &counterList)) {
+    counterList = new nsCounterList();
+    mNames.Put(aCounterName, counterList);
+  }
+  return counterList;
 }
 
 void
 nsCounterManager::RecalcAll()
 {
-    for (auto iter = mNames.Iter(); !iter.Done(); iter.Next()) {
-        nsCounterList* list = iter.UserData();
-        if (list->IsDirty()) {
-            list->RecalcAll();
-        }
+  for (auto iter = mNames.Iter(); !iter.Done(); iter.Next()) {
+    nsCounterList* list = iter.UserData();
+    if (list->IsDirty()) {
+      list->RecalcAll();
     }
+  }
 }
 
 void
@@ -263,7 +269,7 @@ nsCounterManager::SetAllDirty()
 }
 
 bool
-nsCounterManager::DestroyNodesFor(nsIFrame *aFrame)
+nsCounterManager::DestroyNodesFor(nsIFrame* aFrame)
 {
   bool destroyedAny = false;
   for (auto iter = mNames.Iter(); !iter.Done(); iter.Next()) {
