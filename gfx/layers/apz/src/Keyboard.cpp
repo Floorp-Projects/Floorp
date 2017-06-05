63af54a51a52b5b4323c88c24490a2eac7e5c271
Ryan Hunt <rhunt@eqrion.net>
2017-06-05 18:24:35 -0500

    Bug 1351783 part 4 - Add a KeyboardShortcut type. r=kats,masayuki
    Keyboard scrolling works by first dispatching a key event to the focused element
    of the page. It is then caught by a XBL binding put on the chrome event handler of
    every window. The XBL binding searches through all of its handlers to find one
    that can handle the keyboard event. The matching binding has a command string
    which is dispatched to the nsGlobalWindowCommands which dispatches to PresShell
    which does the actual scrolling.
    
    To do this asynchronously, we need a representation of the XBL handlers that can
    be applied to a KeyboardInput to get a KeyboardAction.
    
    This commit adds KeyboardShortcut for this purpose. KeyboardShortcut is designed
    to be compatible with nsXBLPrototypeHandler and to only handle the specific cases
    we care about for keyboard scrolling. If a XBL handler runs javascript or does
    anything else we cannot handle in an OMT situation, then we create a
    dispatch-to-content KeyboardShortcut.
    
    MozReview-Commit-ID: 1qzywS3QHVp

--- gfx/layers/apz/src/Keyboard.cpp
+++ gfx/layers/apz/src/Keyboard.cpp
@@ -5,6 +5,8 @@
 
 #include "mozilla/layers/Keyboard.h"
 
+#include "mozilla/TextEvents.h" // for IgnoreModifierState
+
 namespace mozilla {
 namespace layers {
 
@@ -36,6 +38,96 @@ KeyboardScrollAction::KeyboardScrollAction(KeyboardScrollActionType aType, bool 
   : mType(aType)
   , mForward(aForward)
 {
+}
+
+KeyboardShortcut::KeyboardShortcut()
+{
+}
+
+KeyboardShortcut::KeyboardShortcut(KeyboardInput::KeyboardEventType aEventType,
+                                   uint32_t aKeyCode,
+                                   uint32_t aCharCode,
+                                   Modifiers aModifiers,
+                                   Modifiers aModifiersMask,
+                                   const KeyboardScrollAction& aAction)
+  : mAction(aAction)
+  , mKeyCode(aKeyCode)
+  , mCharCode(aCharCode)
+  , mModifiers(aModifiers)
+  , mModifiersMask(aModifiersMask)
+  , mEventType(aEventType)
+  , mDispatchToContent(false)
+{
+}
+
+KeyboardShortcut::KeyboardShortcut(KeyboardInput::KeyboardEventType aEventType,
+                                   uint32_t aKeyCode,
+                                   uint32_t aCharCode,
+                                   Modifiers aModifiers,
+                                   Modifiers aModifiersMask)
+  : mKeyCode(aKeyCode)
+  , mCharCode(aCharCode)
+  , mModifiers(aModifiers)
+  , mModifiersMask(aModifiersMask)
+  , mEventType(aEventType)
+  , mDispatchToContent(true)
+{
+}
+
+bool
+KeyboardShortcut::Matches(const KeyboardInput& aInput,
+                          const IgnoreModifierState& aIgnore,
+                          uint32_t aOverrideCharCode) const
+{
+  return mEventType == aInput.mType &&
+         MatchesKey(aInput, aOverrideCharCode) &&
+         MatchesModifiers(aInput, aIgnore);
+}
+
+bool
+KeyboardShortcut::MatchesKey(const KeyboardInput& aInput,
+                             uint32_t aOverrideCharCode) const
+{
+  // Compare by the key code if we have one
+  if (!mCharCode) {
+    return mKeyCode == aInput.mKeyCode;
+  }
+
+  // We are comparing by char code
+  uint32_t charCode;
+
+  // If we are comparing against a shortcut candidate then we might
+  // have an override char code
+  if (aOverrideCharCode) {
+    charCode = aOverrideCharCode;
+  } else {
+    charCode = aInput.mCharCode;
+  }
+
+  // Both char codes must be in lowercase to compare correctly
+  if (IS_IN_BMP(charCode)) {
+    charCode = ToLowerCase(static_cast<char16_t>(charCode));
+  }
+
+  return mCharCode == charCode;
+}
+
+bool
+KeyboardShortcut::MatchesModifiers(const KeyboardInput& aInput,
+                                   const IgnoreModifierState& aIgnore) const
+{
+  Modifiers modifiersMask = mModifiersMask;
+
+  // If we are ignoring Shift or OS, then unset that part of the mask
+  if (aIgnore.mOS) {
+    modifiersMask &= ~MODIFIER_OS;
+  }
+  if (aIgnore.mShift) {
+    modifiersMask &= ~MODIFIER_SHIFT;
+  }
+
+  // Mask off the modifiers we are ignoring from the keyboard input
+  return (aInput.modifiers & modifiersMask) == mModifiers;
 }
 
 } // namespace layers
--- gfx/layers/apz/src/Keyboard.h
+++ gfx/layers/apz/src/Keyboard.h
@@ -6,9 +6,15 @@
 #ifndef mozilla_layers_Keyboard_h
 #define mozilla_layers_Keyboard_h
 
+#include <stdint.h> // for uint32_t
+
+#include "InputData.h"          // for KeyboardInput
 #include "nsIScrollableFrame.h" // for nsIScrollableFrame::ScrollUnit
 
 namespace mozilla {
+
+struct IgnoreModifierState;
+
 namespace layers {
 
 /**
@@ -38,6 +44,67 @@ public:
   KeyboardScrollActionType mType;
   // Whether to scroll forward or backward along the axis of this action type
   bool mForward;
+};
+
+/**
+ * This class is an off main-thread <xul:handler> for scrolling commands.
+ */
+class KeyboardShortcut final
+{
+public:
+  KeyboardShortcut();
+
+  /**
+   * Create a keyboard shortcut that when matched can be handled by executing
+   * the specified keyboard action.
+   */
+  KeyboardShortcut(KeyboardInput::KeyboardEventType aEventType,
+                   uint32_t aKeyCode,
+                   uint32_t aCharCode,
+                   Modifiers aModifiers,
+                   Modifiers aModifiersMask,
+                   const KeyboardScrollAction& aAction);
+
+  /**
+   * Create a keyboard shortcut that when matched should be handled by ignoring
+   * the keyboard event and dispatching it to content.
+   */
+  KeyboardShortcut(KeyboardInput::KeyboardEventType aEventType,
+                   uint32_t aKeyCode,
+                   uint32_t aCharCode,
+                   Modifiers aModifiers,
+                   Modifiers aModifiersMask);
+
+  bool Matches(const KeyboardInput& aInput,
+               const IgnoreModifierState& aIgnore,
+               uint32_t aOverrideCharCode = 0) const;
+
+private:
+  bool MatchesKey(const KeyboardInput& aInput,
+                  uint32_t aOverrideCharCode) const;
+  bool MatchesModifiers(const KeyboardInput& aInput,
+                        const IgnoreModifierState& aIgnore) const;
+
+public:
+  // The action to perform when this shortcut is matched,
+  // and not flagged to be dispatched to content
+  KeyboardScrollAction mAction;
+
+  // Only one of mKeyCode or mCharCode may be non-zero
+  // whichever one is non-zero is the one to compare when matching
+  uint32_t mKeyCode;
+  uint32_t mCharCode;
+
+  // The modifiers that must be active for this shortcut
+  Modifiers mModifiers;
+  // The modifiers to compare when matching this shortcut
+  Modifiers mModifiersMask;
+
+  // The type of keyboard event to match against
+  KeyboardInput::KeyboardEventType mEventType;
+
+  // Whether events matched by this must be dispatched to content
+  bool mDispatchToContent;
 };
 
 } // namespace layers
