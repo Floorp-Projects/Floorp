50d7a195485476cd9ce2b864bf2ec4b3306d1a11
Jim Chen <nchen@mozilla.com>
2017-06-05 13:55:59 -0400

    Bug 1367081 - 4. Load SelectHelper and InputWidgetHelper in PromptService.js; r=droeh
    Right now SelectHelper and InputWidgetHelper are loaded in browser.js,
    which means they only work for GeckoApp. This patch loads them in
    PromptService.js instead, which means they will work in all windows. The
    patch also changes some code in SelectHelper and InputWidgetHelper that
    used to assume they are running under the browser.xul chrome window.
    
    MozReview-Commit-ID: HveDzIzK1b4

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -129,8 +129,6 @@ var GlobalEventDispatcher = EventDispatcher.instance;
 var WindowEventDispatcher = EventDispatcher.for(window);
 
 var lazilyLoadedBrowserScripts = [
-  ["SelectHelper", "chrome://browser/content/SelectHelper.js"],
-  ["InputWidgetHelper", "chrome://browser/content/InputWidgetHelper.js"],
   ["MasterPassword", "chrome://browser/content/MasterPassword.js"],
   ["PluginHelper", "chrome://browser/content/PluginHelper.js"],
   ["OfflineApps", "chrome://browser/content/OfflineApps.js"],
@@ -4740,9 +4738,6 @@ var BrowserEventHandler = {
     BrowserApp.deck.addEventListener("DOMUpdatePageReport", PopupBlockerObserver.onUpdatePageReport);
     BrowserApp.deck.addEventListener("MozMouseHittest", this, true);
     BrowserApp.deck.addEventListener("OpenMediaWithExternalApp", this, true);
-
-    InitLater(() => BrowserApp.deck.addEventListener("click", InputWidgetHelper, true));
-    InitLater(() => BrowserApp.deck.addEventListener("click", SelectHelper, true));
 
     // ReaderViews support backPress listeners.
     WindowEventDispatcher.registerListener((event, data, callback) => {
