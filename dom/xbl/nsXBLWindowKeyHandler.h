18867b218f7058fa08ba50236f59cb035ed43fef
Ryan Hunt <rhunt@eqrion.net>
2017-06-05 18:29:04 -0500

    Bug 1351783 part 5 - Add a KeyboardMap type. r=kats,masayuki
    The XBL bindings used for scrolling are managed by a nsXBLWindowKeyHandler. This
    class loads the handlers and has logic for searching through them to match a
    keyboard event. This commit adds a KeyboardMap class for storing KeyboardShortcuts
    and for mirroring the search logic of nsXBLWindowKeyHandler.
    
    MozReview-Commit-ID: 5BmFBilKTJy
    
    --HG--
    extra : rebase_source : 96cd0f5808ba6c4926e9da368ab3780b1d9d1449

--- dom/xbl/nsXBLWindowKeyHandler.h
+++ dom/xbl/nsXBLWindowKeyHandler.h
@@ -8,13 +8,13 @@
 #define nsXBLWindowKeyHandler_h__
 
 #include "mozilla/EventForwards.h"
+#include "mozilla/layers/Keyboard.h"
 #include "nsWeakPtr.h"
 #include "nsIDOMEventListener.h"
 
 class nsIAtom;
 class nsIDOMElement;
 class nsIDOMKeyEvent;
-class nsXBLSpecialDocInfo;
 class nsXBLPrototypeHandler;
 
 namespace mozilla {
@@ -30,6 +30,7 @@ class nsXBLWindowKeyHandler : public nsIDOMEventListener
 {
   typedef mozilla::EventListenerManager EventListenerManager;
   typedef mozilla::IgnoreModifierState IgnoreModifierState;
+  typedef mozilla::layers::KeyboardMap KeyboardMap;
 
 public:
   nsXBLWindowKeyHandler(nsIDOMElement* aElement, mozilla::dom::EventTarget* aTarget);
@@ -38,6 +39,8 @@ public:
          EventListenerManager* aEventListenerManager);
   void RemoveKeyboardEventListenersFrom(
          EventListenerManager* aEventListenerManager);
+
+  static KeyboardMap CollectKeyboardShortcuts();
 
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMEVENTLISTENER
@@ -80,6 +83,9 @@ protected:
   nsIAtom* ConvertEventToDOMEventType(
              const mozilla::WidgetKeyboardEvent& aWidgetKeyboardEvent) const;
 
+  // lazily load the special doc info for loading handlers
+  static void EnsureSpecialDocInfo();
+
   // lazily load the handlers. Overridden to handle being attached
   // to a particular element rather than the document
   nsresult EnsureHandlers();
@@ -120,8 +126,7 @@ protected:
   nsXBLPrototypeHandler* mHandler;     // platform bindings
   nsXBLPrototypeHandler* mUserHandler; // user-specific bindings
 
-  // holds document info about bindings
-  static nsXBLSpecialDocInfo* sXBLSpecialDocInfo;
+  // holds reference count to document info about bindings
   static uint32_t sRefCnt;
 };
 
