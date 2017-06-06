21aa0b6ac5253b9d42854bf3f6fff37c0c6be6ca
Jon Coppeard <jcoppeard@mozilla.com>
2017-06-06 11:25:57 +0100

    Bug 1370252 - Fix spurious jit-tests failures with incremental zeal mode r=sfink

--- js/src/jit-test/tests/gc/bug-1323868.js
+++ js/src/jit-test/tests/gc/bug-1323868.js
@@ -1,5 +1,6 @@
 if (helperThreadCount() == 0)
     quit();
+gczeal(0);
 startgc(8301);
 offThreadCompileScript("(({a,b,c}))");
 gcparam("maxBytes", gcparam("gcBytes"));
--- js/src/jit-test/tests/gc/bug1283169.js
+++ js/src/jit-test/tests/gc/bug1283169.js
@@ -1,4 +1,5 @@
 if (helperThreadCount() === 0)
     quit(0);
+gczeal(0);
 startgc(45);
 offThreadCompileScript("print(1)");
