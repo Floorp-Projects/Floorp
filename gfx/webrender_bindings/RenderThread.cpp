eb68bd9d78a3fc5d1cf4890f5d91901006f178a0
JerryShih <hshih@mozilla.com>
2017-06-06 19:18:40 +0800

    Bug 1366502 - Update the thread model for RegisterExternalImage(), UnregisterExternalImage() and GetRenderTexture() call. v2. r=sotaro
    If we call UnregisterExternalImage() at non-render-thread and decrease the RenderTextureHost's ref-count to zero, the RenderTextureHost will be released in non-render-thread.
    That will cause some problems if we use some thread-specific functions in ~RenderTextureHost().
    This patch uses a message loop in UnregisterExternalImage() to resolve this problem.
    
    MozReview-Commit-ID: CDazxGkE1cK

--- gfx/webrender_bindings/RenderThread.cpp
+++ gfx/webrender_bindings/RenderThread.cpp
@@ -275,11 +275,13 @@ RenderThread::DecPendingFrameCount(wr::WindowId aWindowId)
 }
 
 void
-RenderThread::RegisterExternalImage(uint64_t aExternalImageId, RenderTextureHost* aTexture)
+RenderThread::RegisterExternalImage(uint64_t aExternalImageId, already_AddRefed<RenderTextureHost> aTexture)
 {
   MutexAutoLock lock(mRenderTextureMapLock);
-  MOZ_ASSERT(!mRenderTextures.Get(aExternalImageId));
-  mRenderTextures.Put(aExternalImageId, aTexture);
+
+  MOZ_ASSERT(!mRenderTextures.Get(aExternalImageId).get());
+  RefPtr<RenderTextureHost> texture(aTexture);
+  mRenderTextures.Put(aExternalImageId, Move(texture));
 }
 
 void
@@ -287,12 +289,35 @@ RenderThread::UnregisterExternalImage(uint64_t aExternalImageId)
 {
   MutexAutoLock lock(mRenderTextureMapLock);
   MOZ_ASSERT(mRenderTextures.Get(aExternalImageId).get());
-  mRenderTextures.Remove(aExternalImageId);
+  if (!IsInRenderThread()) {
+    // The RenderTextureHost should be released in render thread. So, post the
+    // deletion task here.
+    // The shmem and raw buffer are owned by compositor ipc channel. It's
+    // possible that RenderTextureHost is still exist after the shmem/raw buffer
+    // deletion. Then the buffer in RenderTextureHost becomes invalid. It's fine
+    // for this situation. Gecko will only release the buffer if WR doesn't need
+    // it. So, no one will access the invalid buffer in RenderTextureHost.
+    RefPtr<RenderTextureHost> texture = mRenderTextures.Get(aExternalImageId);
+    mRenderTextures.Remove(aExternalImageId);
+    Loop()->PostTask(NewRunnableMethod<RefPtr<RenderTextureHost>>(
+      this, &RenderThread::DeferredRenderTextureHostDestroy, Move(texture)
+    ));
+  } else {
+    mRenderTextures.Remove(aExternalImageId);
+  }
+}
+
+void
+RenderThread::DeferredRenderTextureHostDestroy(RefPtr<RenderTextureHost>)
+{
+  // Do nothing. Just decrease the ref-count of RenderTextureHost.
 }
 
 RenderTextureHost*
 RenderThread::GetRenderTexture(WrExternalImageId aExternalImageId)
 {
+  MOZ_ASSERT(IsInRenderThread());
+
   MutexAutoLock lock(mRenderTextureMapLock);
   MOZ_ASSERT(mRenderTextures.Get(aExternalImageId.mHandle).get());
   return mRenderTextures.Get(aExternalImageId.mHandle).get();
