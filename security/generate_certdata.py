bcc2da0b6a8c440d76ad55d82a8f2e3b6ba8697b
Franziskus Kiefer <franziskuskiefer@gmail.com>
2017-06-06 09:59:42 +0200

    Bug 1345368 - adapt to new NSS certdata.py, r=ted UPGRADE_NSS_RELEASE
    --HG--
    extra : rebase_source : e995f611f59a8f10af70781a1ee50e6c89f4a8d9

--- security/generate_certdata.py
+++ security/generate_certdata.py
@@ -4,8 +4,12 @@
 # and moz.build `GENERATED_FILES` semantics.
 
 import buildconfig
+import os
 import subprocess
 
 def main(output, *inputs):
-    output.write(subprocess.check_output([buildconfig.substs['PERL']] + list(inputs)))
+    env=dict(os.environ)
+    env['PERL'] = buildconfig.substs['PERL']
+    output.write(subprocess.check_output([buildconfig.substs['PYTHON'],
+                 inputs[0], inputs[2]], env=env))
     return None
