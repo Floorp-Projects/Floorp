e04a5a47c685c5de7d429a930ea3a27234d88e00
L. David Baron <dbaron@dbaron.org>
2017-06-06 22:27:18 -0700

    Bug 1367576 - Shrink existing caches to the size that's actually needed.  r=karlt
    Now that, thanks to bug 1367577, we have the theme constants in an enum,
    we can make these arrays smaller rather than assuming that the constants
    might use any valid uint8_t value.
    
    MozReview-Commit-ID: A6GjTarVurc

--- widget/gtk/nsNativeThemeGTK.h
+++ widget/gtk/nsNativeThemeGTK.h
@@ -11,6 +11,7 @@
 #include "nsIAtom.h"
 #include "nsIObserver.h"
 #include "nsNativeTheme.h"
+#include "nsThemeConstants.h"
 
 #include <gtk/gtk.h>
 #include "gtkdrawing.h"
@@ -85,8 +86,8 @@ private:
   void RefreshWidgetWindow(nsIFrame* aFrame);
   WidgetNodeType NativeThemeToGtkTheme(uint8_t aWidgetType, nsIFrame* aFrame);
 
-  uint8_t mDisabledWidgetTypes[32];
-  uint8_t mSafeWidgetStates[1024];    // 256 widgets * 32 bits per widget
+  uint8_t mDisabledWidgetTypes[(ThemeWidgetType_COUNT + 7) / 8];
+  uint8_t mSafeWidgetStates[ThemeWidgetType_COUNT * 4]; // 32 bits per widget
   static const char* sDisabledEngines[];
 
   // Because moz_gtk_get_widget_border can be slow, we cache its results
