8422d2bbb54702c5cde47d181ba6eb45de074566
Ralph Giles <giles@mozilla.com>
2017-06-07 14:57:55 -0700

    Bug 1371366 - mozboot: Install rust 1.18.0. r=froydnj
    This version offers a 15-20% improvement to compilation
    speed, so we will likely require it for developers soon.
    
    Begin installing it now to reduce the frequence of double
    migrations.
    
    MozReview-Commit-ID: JZRssgOjkF7

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -162,7 +162,7 @@ MODERN_MERCURIAL_VERSION = LooseVersion('3.7.3')
 MODERN_PYTHON_VERSION = LooseVersion('2.7.3')
 
 # Upgrade rust older than this.
-MODERN_RUST_VERSION = LooseVersion('1.17.0')
+MODERN_RUST_VERSION = LooseVersion('1.18.0')
 
 class BaseBootstrapper(object):
     """Base class for system bootstrappers."""
