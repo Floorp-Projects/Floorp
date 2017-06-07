4144e1d1779b4f1c522a95d293c70d0281f77bae
Petr Sumbera <petr.sumbera@oracle.com>
2017-06-07 08:35:14 -0700

    Bug 1158445 - posix_madvise() should be used instead of madvise in Poison code; r=froydnj

--- mfbt/tests/TestPoisonArea.cpp
+++ mfbt/tests/TestPoisonArea.cpp
@@ -269,7 +269,7 @@ ReleaseRegion(void* aPage)
 static bool
 ProbeRegion(uintptr_t aPage)
 {
-  return !!madvise(reinterpret_cast<void*>(aPage), PageSize(), MADV_NORMAL);
+  return !!posix_madvise(reinterpret_cast<void*>(aPage), PageSize(), POSIX_MADV_NORMAL);
 }
 
 static int
