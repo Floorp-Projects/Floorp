ef8c27aebce4011c49630b4c79d4e2525bacc50c
Lars T Hansen <lhansen@mozilla.com>
2017-06-08 13:44:56 +0200

    Bug 1277562 - Part 13: Guard against re-adding an import dependency when reoptimizing for tier2. r=luke
    --HG--
    extra : rebase_source : d807c59d949fddfbf4e3e212900e8d774d135774
    extra : intermediate-source : d409538ef75d1da17bc931d220415ca9df8b2d09
    extra : source : ce10fbb37e52741d276e6a779857a36e3d8b05cb

--- js/src/wasm/WasmInstance.cpp
+++ js/src/wasm/WasmInstance.cpp
@@ -191,9 +191,13 @@ Instance::callImport(JSContext* cx, uint32_t funcImportIndex, unsigned argc, con
         return true;
 
     // The import may already have become optimized.
+    for (auto t : code().tiers()) {
+        void* jitExitCode = codeBase(t) + fi.jitExitCodeOffset();
+        if (import.code == jitExitCode)
+            return true;
+    }
+
     void* jitExitCode = codeBase(tier) + fi.jitExitCodeOffset();
-    if (import.code == jitExitCode)
-        return true;
 
     // Test if the function is JIT compiled.
     if (!importFun->hasScript())
