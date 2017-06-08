73da6dadb9b73291095df4e28a26b074a35eb968
Honza Bambas <honzab.moz@firemni.cz>
2017-06-08 19:42:42 +0200

    Bug 1369496 - linux build fix, r=me

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -837,7 +837,7 @@ bool nsHttpTransaction::ShouldStopReading()
 
     if (mContentRead < 16000) {
         // Let the first bytes go, it may also well be all the content we get
-        LOG(("nsHttpTransaction::ShouldStopReading too few content (%llu) this=%p", mContentRead, this));
+        LOG(("nsHttpTransaction::ShouldStopReading too few content (%" PRIi64 ") this=%p", mContentRead, this));
         return false;
     }
 
