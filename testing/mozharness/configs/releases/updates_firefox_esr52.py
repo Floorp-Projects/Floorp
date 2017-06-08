7d93ffb486c584be104e3b7d16f3035de7b6a965
Ben Hearsum <bhearsum@mozilla.com>
2017-06-08 13:57:20 -0400

    bug 1359084: update balrog usernames in release configs. r=asasaki a=release

--- testing/mozharness/configs/releases/updates_firefox_esr52.py
+++ testing/mozharness/configs/releases/updates_firefox_esr52.py
@@ -18,7 +18,7 @@ config = {
     "previous_archive_prefix": "https://archive.mozilla.org/pub",
     "download_domain": "download.mozilla.org",
     "balrog_url": "https://aus5.mozilla.org",
-    "balrog_username": "ffxbld",
+    "balrog_username": "balrog-ffxbld",
     "update_channels": {
         "esr": {
             "version_regex": r".*",
