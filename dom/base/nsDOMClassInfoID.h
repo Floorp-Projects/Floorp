dd23eb4bcd60a183305d36ab5254d289824498ff
Peter Van der Beken <peterv@propagandism.org>
2017-06-08 21:04:02 +0200

    Bug 1252211 - Convert TreeContentView to WebIDL. r=bz.

--- dom/base/nsDOMClassInfoID.h
+++ dom/base/nsDOMClassInfoID.h
@@ -19,11 +19,6 @@ enum nsDOMClassInfoID
   eDOMClassInfo_DOMPrototype_id,
   eDOMClassInfo_DOMConstructor_id,
 
-  // XUL classes
-#ifdef MOZ_XUL
-  eDOMClassInfo_TreeContentView_id,
-#endif
-
   eDOMClassInfo_ContentFrameMessageManager_id,
   eDOMClassInfo_ContentProcessMessageManager_id,
   eDOMClassInfo_ChromeMessageBroadcaster_id,
