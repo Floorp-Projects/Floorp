defaa764a6fb01ce6ba4f4d46eed16dc08d60b98
Bob Silverberg <bsilverberg@mozilla.com>
2017-06-09 11:04:46 -0400

    Bug 1282981 - Implement management.get and getAll, r=mixedpuppy
    This updates the existing implementation of browser.management.getAll to allow both themes and extensions
    to be returned. It also adds an API for browser.management.get.
    
    MozReview-Commit-ID: C1v2FqQqNux
    
    --HG--
    extra : rebase_source : a31c4e90dc13e01e3596ccc1a6437d60ff8931e8

--- toolkit/components/extensions/test/xpcshell/test_ext_management.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_management.js
@@ -9,22 +9,58 @@ add_task(async function setup() {
   await ExtensionTestUtils.startAddonManager();
 });
 
-add_task(async function test_management_schema() {
-  async function background() {
-    browser.test.assertTrue(browser.management, "browser.management API exists");
-    let self = await browser.management.getSelf();
-    browser.test.assertEq(browser.runtime.id, self.id, "got self");
-    browser.test.notifyPass("management-schema");
+add_task(async function test_management_getAll() {
+  const id1 = "get_all_test1@tests.mozilla.com";
+  const id2 = "get_all_test2@tests.mozilla.com";
+
+  function getManifest(id) {
+    return {
+      applications: {
+        gecko: {
+          id,
+        },
+      },
+      name: id,
+      version: "1.0",
+      permissions: ["management"],
+    };
   }
 
-  let extension = ExtensionTestUtils.loadExtension({
-    manifest: {
-      permissions: ["management"],
-    },
-    background: `(${background})()`,
+  async function background() {
+    browser.test.onMessage.addListener(async (msg, id) => {
+      let addon = await browser.management.get(id);
+      browser.test.sendMessage("addon", addon);
+    });
+
+    let addons = await browser.management.getAll();
+    browser.test.assertEq(addons.length, 2, "management.getAll returned two extensions.");
+    browser.test.sendMessage("addons", addons);
+  }
+
+  let extension1 = ExtensionTestUtils.loadExtension({
+    manifest: getManifest(id1),
     useAddonManager: "temporary",
   });
-  await extension.startup();
-  await extension.awaitFinish("management-schema");
-  await extension.unload();
+
+  let extension2 = ExtensionTestUtils.loadExtension({
+    manifest: getManifest(id2),
+    background,
+    useAddonManager: "temporary",
+  });
+
+  await extension1.startup();
+  await extension2.startup();
+
+  let addons = await extension2.awaitMessage("addons");
+  for (let id of [id1, id2]) {
+    let addon = addons.find(a => { return a.id === id; });
+    equal(addon.name, id, `The extension with id ${id} was returned by getAll.`);
+  }
+
+  extension2.sendMessage("getAddon", id1);
+  let addon = await extension2.awaitMessage("addon");
+  equal(addon.name, id1, `The extension with id ${id1} was returned by get.`);
+
+  await extension2.unload();
+  await extension1.unload();
 });
