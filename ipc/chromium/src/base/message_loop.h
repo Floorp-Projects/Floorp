1dd741346913312e5d7472484ded4362ee7f0927
Bill McCloskey <billm@mozilla.com>
2017-06-09 13:40:34 -0700

    Bug 1366072 - Use nsIRunnable instead of Runnable in MessageLoop (r=kanru)
    MozReview-Commit-ID: 9LmBghd0D46

--- ipc/chromium/src/base/message_loop.h
+++ ipc/chromium/src/base/message_loop.h
@@ -26,6 +26,8 @@
 #endif
 
 #include "nsAutoPtr.h"
+#include "nsCOMPtr.h"
+#include "nsIRunnable.h"
 #include "nsThreadUtils.h"
 
 class nsIThread;
@@ -112,12 +114,12 @@ public:
   // NOTE: These methods may be called on any thread.  The Task will be invoked
   // on the thread that executes MessageLoop::Run().
 
-  void PostTask(already_AddRefed<mozilla::Runnable> task);
+  void PostTask(already_AddRefed<nsIRunnable> task);
 
-  void PostDelayedTask(already_AddRefed<mozilla::Runnable> task, int delay_ms);
+  void PostDelayedTask(already_AddRefed<nsIRunnable> task, int delay_ms);
 
   // PostIdleTask is not thread safe and should be called on this thread
-  void PostIdleTask(already_AddRefed<mozilla::Runnable> task);
+  void PostIdleTask(already_AddRefed<nsIRunnable> task);
 
   // Run the message loop.
   void Run();
@@ -282,12 +284,12 @@ public:
 
   // This structure is copied around by value.
   struct PendingTask {
-    RefPtr<mozilla::Runnable> task;    // The task to run.
+    nsCOMPtr<nsIRunnable> task;        // The task to run.
     base::TimeTicks delayed_run_time;  // The time when the task should be run.
     int sequence_num;                  // Secondary sort key for run time.
     bool nestable;                     // OK to dispatch from a nested loop.
 
-    PendingTask(already_AddRefed<mozilla::Runnable> aTask, bool aNestable)
+    PendingTask(already_AddRefed<nsIRunnable> aTask, bool aNestable)
         : task(aTask), sequence_num(0), nestable(aNestable) {
     }
 
@@ -355,10 +357,10 @@ public:
   // appended to the list work_queue_.  Such re-entrancy generally happens when
   // an unrequested message pump (typical of a native dialog) is executing in
   // the context of a task.
-  bool QueueOrRunTask(already_AddRefed<mozilla::Runnable> new_task);
+  bool QueueOrRunTask(already_AddRefed<nsIRunnable> new_task);
 
   // Runs the specified task and deletes it.
-  void RunTask(already_AddRefed<mozilla::Runnable> task);
+  void RunTask(already_AddRefed<nsIRunnable> task);
 
   // Calls RunTask or queues the pending_task on the deferred task list if it
   // cannot be run right now.  Returns true if the task was run.
@@ -378,7 +380,7 @@ public:
   bool DeletePendingTasks();
 
   // Post a task to our incomming queue.
-  void PostTask_Helper(already_AddRefed<mozilla::Runnable> task, int delay_ms);
+  void PostTask_Helper(already_AddRefed<nsIRunnable> task, int delay_ms);
 
   // base::MessagePump::Delegate methods:
   virtual bool DoWork() override;
