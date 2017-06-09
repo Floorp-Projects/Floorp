a93a0fef11bae9c6a82cc56889d0ad3def5cfeae
Micah Tigley <tigleym@gmail.com>
2017-06-08 19:12:25 -0600

    Bug 1356474 - Add units tests for the Grid Outline component. r=gl
    MozReview-Commit-ID: 85ZjxjkpIgU

--- devtools/client/inspector/grids/test/browser_grids_grid-outline-selected-grid.js
+++ devtools/client/inspector/grids/test/browser_grids_grid-outline-selected-grid.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-// Tests toggling ON/OFF the grid highlighter from the grid ispector panel.
+// Tests that the grid outline is shown when a grid container is selected.
 
 const TEST_URI = `
   <style type='text/css'>
@@ -12,41 +12,39 @@ const TEST_URI = `
     }
   </style>
   <div id="grid">
-    <div id="cell1">cell1</div>
-    <div id="cell2">cell2</div>
+    <div id="cella">Cell A</div>
+    <div id="cellb">Cell B</div>
+    <div id="cellc">Cell C</div>
   </div>
 `;
 
 add_task(function* () {
   yield addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
-  let { gridInspector, inspector } = yield openLayoutView();
+
+  let { inspector, gridInspector } = yield openLayoutView();
   let { document: doc } = gridInspector;
   let { highlighters, store } = inspector;
 
-  yield selectNode("#grid", inspector);
   let gridList = doc.getElementById("grid-list");
   let checkbox = gridList.children[0].querySelector("input");
 
   info("Checking the initial state of the Grid Inspector.");
-  is(gridList.childNodes.length, 1, "One grid container is listed.");
-  ok(!checkbox.checked, `Grid item ${checkbox.value} is unchecked in the grid list.`);
-  ok(!highlighters.highlighters[HIGHLIGHTER_TYPE],
-    "No CSS grid highlighter exists in the highlighters overlay.");
-  ok(!highlighters.gridHighlighterShown, "No CSS grid highlighter is shown.");
+  ok(!doc.getElementById("grid-outline-container"),
+    "There should be no grid outline shown.");
 
   info("Toggling ON the CSS grid highlighter from the layout panel.");
   let onHighlighterShown = highlighters.once("grid-highlighter-shown");
   let onCheckboxChange = waitUntilState(store, state =>
     state.grids.length == 1 &&
     state.grids[0].highlighted);
+  let onGridOutlineRendered = waitForDOM(doc, "#grid-cell-group rect", 4);
   checkbox.click();
   yield onHighlighterShown;
   yield onCheckboxChange;
+  let elements = yield onGridOutlineRendered;
 
-  info("Checking the CSS grid highlighter is created.");
-  ok(highlighters.highlighters[HIGHLIGHTER_TYPE],
-    "CSS grid highlighter is created in the highlighters overlay.");
-  ok(highlighters.gridHighlighterShown, "CSS grid highlighter is shown.");
+  info("Checking the grid outline is shown.");
+  is(elements.length, 4, "Grid outline is shown.");
 
   info("Toggling OFF the CSS grid highlighter from the layout panel.");
   let onHighlighterHidden = highlighters.once("grid-highlighter-hidden");
@@ -57,6 +55,5 @@ add_task(function* () {
   yield onHighlighterHidden;
   yield onCheckboxChange;
 
-  info("Checking the CSS grid highlighter is not shown.");
-  ok(!highlighters.gridHighlighterShown, "No CSS grid highlighter is shown.");
+  ok(!doc.getElementById("grid-outline-container"), "No grid outline is hidden.");
 });
