f7e4d4b0334153967237284d536a76336082b4d5
Eric Rahm <erahm@mozilla.com>
2017-06-09 16:09:17 -0700

    Bug 1370644 - Part 1: Only use NS_ERROR for an imminent failure. r=froydnj, a=debug-only
    This modifies the logic in |CheckAcquisition| to only call |NS_ERROR| if we're
    really going to deadlock. Instead, if we detect a suspicious cycle, we just use
    an |NS_WARNING|. This means that we'll still output warning text in debug
    builds, but we won't cause the process to abort.
    
    MozReview-Commit-ID: 71mFInWwbDY

--- xpcom/tests/gtest/TestDeadlockDetector.cpp
+++ xpcom/tests/gtest/TestDeadlockDetector.cpp
@@ -136,6 +136,8 @@ TEST_F(TESTNAME(DeadlockDetectorTest), TESTNAME(Sanity2DeathTest))
     ASSERT_DEATH_IF_SUPPORTED(Sanity2_Child(), regex);
 }
 
+#if 0
+// Temporarily disabled, see bug 1370644.
 int
 Sanity3_Child()
 {
@@ -173,6 +175,7 @@ TEST_F(TESTNAME(DeadlockDetectorTest), TESTNAME(Sanity3DeathTest))
 
     ASSERT_DEATH_IF_SUPPORTED(Sanity3_Child(), regex);
 }
+#endif
 
 int
 Sanity4_Child()
@@ -219,6 +222,8 @@ struct ThreadState
   int id;
 };
 
+#if 0
+// Temporarily disabled, see bug 1370644.
 static void
 TwoThreads_thread(void* arg)
 {
@@ -277,6 +282,7 @@ TEST_F(TESTNAME(DeadlockDetectorTest), TESTNAME(TwoThreadsDeathTest))
 
     ASSERT_DEATH_IF_SUPPORTED(TwoThreads_Child(), regex);
 }
+#endif
 
 static void
 ContentionNoDeadlock_thread(void* arg)
