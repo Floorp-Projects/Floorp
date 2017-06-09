ad503dd5d3a392bcea2d0b3f6a96978ae9940523
Greg Mierzwinski <gmierz2@outlook.com>
2017-06-09 14:51:20 -0400

    Bug 1372324 - Enable talos tests on linux64-ccov. r=jmaher
    This patch enables talos test suites to run on VM (taskcluster) and also enables these test suites to run with GCOV code coverage instrumentation on the linux64-ccov build.
    
    MozReview-Commit-ID: 7p59zvra1ge
    
    --HG--
    extra : rebase_source : 990ebecb9daaee7c5030e08b0d763493103f0fe8

--- testing/talos/talos/config.py
+++ testing/talos/talos/config.py
@@ -431,6 +431,7 @@ def get_browser_config(config):
                 'test_timeout': 1200,
                 'xperf_path': None,
                 'error_filename': None,
+                'no_upload_results': False,
                 }
     browser_config = dict(title=config['title'])
     browser_config.update(dict([(i, config[i]) for i in required]))
