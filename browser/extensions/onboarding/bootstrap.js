ab96a7fe45adbdba296c35c5e0a4d9ffe8362dbe
Fischer.json <fischer.json@gmail.com>
2017-06-10 16:14:08 +0800

    Bug 1357020 - Should hide the onboarding tour if user explicitly checked the hide-the-tour checkbox, r=gasolin,mossop,rexboy
    This patch
    - adds one hide-onboarding-tour checkbox
    - after toggling the overlay, hides the onboarding tour if user checked hide-the-tour checkbox
    - creates the message channel between the chrome process and the content process to set prefs.
    - listens to the pref-updated event and then hide the onboarding tour across pages.
    - Add one browser_onboarding_hide_tours.js test
    
    MozReview-Commit-ID: 7ZjbrhfO9dB

--- browser/extensions/onboarding/bootstrap.js
+++ browser/extensions/onboarding/bootstrap.js
@@ -6,6 +6,41 @@
 
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Preferences.jsm");
+
+const PREF_WHITELIST = [
+  "browser.onboarding.enabled",
+  "browser.onboarding.hidden",
+  "browser.onboarding.notification.finished"
+];
+
+/**
+ * Set pref. Why no `getPrefs` function is due to the priviledge level.
+ * We cannot set prefs inside a framescript but can read.
+ * For simplicity and effeciency, we still read prefs inside the framescript.
+ *
+ * @param {Array} prefs the array of prefs to set.
+ *   The array element carrys info to set pref, should contain
+ *   - {String} name the pref name, such as `browser.onboarding.hidden`
+ *   - {*} value the value to set
+ **/
+function setPrefs(prefs) {
+  prefs.forEach(pref => {
+    if (PREF_WHITELIST.includes(pref.name)) {
+      Preferences.set(pref.name, pref.value);
+    }
+  });
+}
+
+function initContentMessageListener() {
+  Services.mm.addMessageListener("Onboarding:OnContentMessage", msg => {
+    switch (msg.data.action) {
+      case "set-prefs":
+        setPrefs(msg.data.params);
+        break;
+    }
+  });
+}
 
 function install(aData, aReason) {}
 
@@ -13,6 +48,7 @@ function uninstall(aData, aReason) {}
 
 function startup(aData, reason) {
   Services.mm.loadFrameScript("resource://onboarding/onboarding.js", true);
+  initContentMessageListener();
 }
 
 function shutdown(aData, reason) {}
