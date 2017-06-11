dfd1e13cc5d4eb9ec298c566e5bde2bc5021f048
Chris Peterson <cpeterson@mozilla.com>
2017-06-11 14:45:24 -0700

    Bug 1373525 - intl/unicharutil: Move ScopedUNumberFormatTraits class out of unnamed namespace. r=emk
    Move the class definition out of an unnamed namespace because it is in a header files included in multiple source files. clang treats the header file's class declaration in an unnamed namespace as distinct in each compilation unit and emits -Wunused-member-function warnings because the member function release() is used in some compilation units but not others.
    
    intl/unicharutil/util/ICUUtils.h:25:17 [-Wunused-member-function] unused member function 'empty'
    intl/unicharutil/util/ICUUtils.h:26:17 [-Wunused-member-function] unused member function 'release'
    
    MozReview-Commit-ID: 7AgzzzRgni4
    
    --HG--
    extra : source : aeed73b6225ba4834a25de0ec9e23592de25c871
    extra : intermediate-source : d0788a9eba9621e04f6be30771acac5707d4a7e1

--- intl/unicharutil/util/ICUUtils.h
+++ intl/unicharutil/util/ICUUtils.h
@@ -19,12 +19,10 @@
 
 class nsIContent;
 
-namespace {
-  struct ScopedUNumberFormatTraits {
-    typedef UNumberFormat* type;
-    static type empty() { return nullptr; }
-    static void release(type handle) { if (handle) unum_close(handle); }
-  };
+struct ScopedUNumberFormatTraits {
+  typedef UNumberFormat* type;
+  static type empty() { return nullptr; }
+  static void release(type handle) { if (handle) unum_close(handle); }
 };
 typedef mozilla::Scoped<ScopedUNumberFormatTraits> AutoCloseUNumberFormat;
 
@@ -111,4 +109,3 @@ public:
 #endif /* MOZILLA_INTERNAL_API */
 
 #endif /* mozilla_ICUUtils_h__ */
-
