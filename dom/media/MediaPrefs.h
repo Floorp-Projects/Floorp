f3ae45c84193591b4e35e5ce6d7ae24f05a5395e
Kaku Kuo <kaku@mozilla.com>
2017-06-12 16:36:42 +0800

    Bug 1371188 P4 - add media.decoder.skip-to-next-key-frame.enabled pref; r=jya
    MozReview-Commit-ID: HUQX1IeWkEE
    
    --HG--
    extra : rebase_source : c2c5a5c0fc1e815273ceb552951978bc0f6f91fd
    extra : source : 53e44c5538a6318588d6973ca833067e8df06042

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -145,6 +145,7 @@ private:
   DECL_MEDIA_PREF("media.decoder.fuzzing.video-output-minimum-interval-ms", PDMFuzzingInterval, uint32_t, 0);
   DECL_MEDIA_PREF("media.decoder.fuzzing.dont-delay-inputexhausted", PDMFuzzingDelayInputExhausted, bool, true);
   DECL_MEDIA_PREF("media.decoder.recycle.enabled",            MediaDecoderCheckRecycling, bool, false);
+  DECL_MEDIA_PREF("media.decoder.skip-to-next-key-frame.enabled", MFRSkipToNextKeyFrameEnabled, bool, true);
   DECL_MEDIA_PREF("media.gmp.decoder.enabled",                PDMGMPEnabled, bool, true);
   DECL_MEDIA_PREF("media.gmp.decoder.aac",                    GMPAACPreferred, uint32_t, 0);
   DECL_MEDIA_PREF("media.gmp.decoder.h264",                   GMPH264Preferred, uint32_t, 0);
