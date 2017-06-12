fa3c0a94e8803ac91e2828547dcff3b9aa26bdb1
Kris Maglione <maglione.k@gmail.com>
2017-06-12 14:42:49 -0700

    Bug 1371246: Handle serializing Blobs in StructuredCloneHolder instances. r=billm
    MozReview-Commit-ID: 2n15NCnLC48

--- dom/base/StructuredCloneBlob.cpp
+++ dom/base/StructuredCloneBlob.cpp
@@ -99,13 +99,14 @@ StructuredCloneBlob::Deserialize(JSContext* aCx, JS::HandleObject aTargetScope,
 
 
 /* static */ JSObject*
-StructuredCloneBlob::ReadStructuredClone(JSContext* aCx, JSStructuredCloneReader* aReader)
+StructuredCloneBlob::ReadStructuredClone(JSContext* aCx, JSStructuredCloneReader* aReader,
+                                         StructuredCloneHolder* aHolder)
 {
   JS::RootedObject obj(aCx);
   {
     RefPtr<StructuredCloneBlob> holder = new StructuredCloneBlob();
 
-    if (!holder->ReadStructuredCloneInternal(aCx, aReader) ||
+    if (!holder->ReadStructuredCloneInternal(aCx, aReader, aHolder) ||
         !holder->WrapObject(aCx, nullptr, &obj)) {
       return nullptr;
     }
@@ -114,12 +115,22 @@ StructuredCloneBlob::ReadStructuredClone(JSContext* aCx, JSStructuredCloneReader
 }
 
 bool
-StructuredCloneBlob::ReadStructuredCloneInternal(JSContext* aCx, JSStructuredCloneReader* aReader)
+StructuredCloneBlob::ReadStructuredCloneInternal(JSContext* aCx, JSStructuredCloneReader* aReader,
+                                                 StructuredCloneHolder* aHolder)
 {
   uint32_t length;
   uint32_t version;
   if (!JS_ReadUint32Pair(aReader, &length, &version)) {
     return false;
+  }
+
+  uint32_t blobOffset;
+  uint32_t blobCount;
+  if (!JS_ReadUint32Pair(aReader, &blobOffset, &blobCount)) {
+    return false;
+  }
+  if (blobCount) {
+    BlobImpls().AppendElements(&aHolder->BlobImpls()[blobOffset], blobCount);
   }
 
   JSStructuredCloneData data(length, length, 4096);
@@ -136,13 +147,17 @@ StructuredCloneBlob::ReadStructuredCloneInternal(JSContext* aCx, JSStructuredClo
 }
 
 bool
-StructuredCloneBlob::WriteStructuredClone(JSContext* aCx, JSStructuredCloneWriter* aWriter)
+StructuredCloneBlob::WriteStructuredClone(JSContext* aCx, JSStructuredCloneWriter* aWriter,
+                                          StructuredCloneHolder* aHolder)
 {
   auto& data = mBuffer->data();
   if (!JS_WriteUint32Pair(aWriter, SCTAG_DOM_STRUCTURED_CLONE_HOLDER, 0) ||
-      !JS_WriteUint32Pair(aWriter, data.Size(), JS_STRUCTURED_CLONE_VERSION)) {
+      !JS_WriteUint32Pair(aWriter, data.Size(), JS_STRUCTURED_CLONE_VERSION) ||
+      !JS_WriteUint32Pair(aWriter, aHolder->BlobImpls().Length(), BlobImpls().Length())) {
     return false;
   }
+
+  aHolder->BlobImpls().AppendElements(BlobImpls());
 
   auto iter = data.Iter();
   while (!iter.Done()) {
