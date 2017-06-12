54b155873d9d9eed2397be1003052ed580c1d636
Henrik Skupin <mail@hskupin.info>
2017-06-12 17:22:21 +0200

    Bug 1372094 - Add mozharness config for firefox-ui test jobs on Windows. r=jmaher
    MozReview-Commit-ID: 4lEUCXuSOkw
    
    --HG--
    extra : rebase_source : a19692364e8047943b0d8bd5967392986cadf35b

---
+++ testing/mozharness/configs/firefox_ui_tests/taskcluster_windows.py
@@ -0,0 +1,29 @@
+# Config file for firefox ui tests run via TaskCluster.
+
+import os
+import sys
+
+
+config = {
+    "virtualenv_python_dll": os.path.join(os.path.dirname(sys.executable), 'python27.dll'),
+    "virtualenv_path": 'venv',
+    "exes": {
+        'python': sys.executable,
+        'virtualenv': [
+            sys.executable,
+            os.path.join(os.path.dirname(sys.executable), 'Lib', 'site-packages', 'virtualenv.py')
+        ],
+        'mozinstall': ['build/venv/scripts/python', 'build/venv/scripts/mozinstall-script.py'],
+        'tooltool.py': [sys.executable, os.path.join(os.environ['MOZILLABUILD'], 'tooltool.py')],
+        'hg': os.path.join(os.environ['PROGRAMFILES'], 'Mercurial', 'hg')
+    },
+
+    "proxxy": {},
+    "find_links": [
+        "http://pypi.pub.build.mozilla.org/pub",
+    ],
+    "pip_index": False,
+
+    "download_minidump_stackwalk": True,
+    "download_symbols": "ondemand",
+}
