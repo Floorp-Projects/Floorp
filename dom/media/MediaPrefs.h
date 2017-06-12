9ab31d5d4cbd5a36ebfc83da0b19638167fd227e
Gerald Squelart <gsquelart@mozilla.com>
2017-06-12 15:42:26 +1200

    Bug 1371882 - Avoid MemoryBlockCache when combined sizes > 'media.memory_caches_combined_limit_...' - r=cpearce
    Don't go over the lowest of 'media.memory_caches_combined_limit_kb'
    (kilobytes) or 'media.memory_caches_combined_limit_pc_sysmem' (percents of
    system memory).
    
    Added more logging around creation/destruction of MediaCaches.
    
    MozReview-Commit-ID: Cdz4ycyn1RR

--- dom/media/MediaPrefs.h
+++ dom/media/MediaPrefs.h
@@ -88,6 +88,9 @@ private:
 
   // Cache sizes.
   DECL_MEDIA_PREF("media.memory_cache_max_size",              MediaMemoryCacheMaxSize, uint32_t, 8192);
+  DECL_MEDIA_PREF("media.memory_caches_combined_limit_kb",    MediaMemoryCachesCombinedLimitKb, uint32_t, 524288);
+  DECL_MEDIA_PREF("media.memory_caches_combined_limit_pc_sysmem",
+                                                              MediaMemoryCachesCombinedLimitPcSysmem, uint32_t, 5);
   DECL_MEDIA_PREF("media.cache.resource-index",               MediaResourceIndexCache, uint32_t, 8192);
 
   // AudioSink
