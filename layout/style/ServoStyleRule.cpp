6f02fdbf01c9ebacf4a099959b14c3ca99cb9cf0
Fernando Jimenez Moreno <ferjmoreno@gmail.com>
2017-06-12 14:54:44 +0200

    Bug 1370501 - stylo: Implement ServoStyleRule::GetSpecificity. r=emilio
    MozReview-Commit-ID: Kxa7Q3Yw4It

--- layout/style/ServoStyleRule.cpp
+++ layout/style/ServoStyleRule.cpp
@@ -269,7 +269,7 @@ ServoStyleRule::GetSelectorText(uint32_t aSelectorIndex, nsAString& aText)
 nsresult
 ServoStyleRule::GetSpecificity(uint32_t aSelectorIndex, uint64_t* aSpecificity)
 {
-  // TODO Bug 1370501
+  Servo_StyleRule_GetSpecificityAtIndex(mRawRule, aSelectorIndex, aSpecificity);
   return NS_OK;
 }
 
