61ceffa8affcec57115abf4dbffd48e2b135a68e
Edouard Oger <eoger@fastmail.com>
2017-06-12 14:51:17 -0400

    Bug 1363412 - Notify other clients when uploading the local clients record for the first time. r=markh
    MozReview-Commit-ID: Ldc3Jrj8RhV

--- services/sync/tests/unit/test_uistate.js
+++ services/sync/tests/unit/test_uistate.js
@@ -3,26 +3,6 @@
 
 "use strict";
 
-// ================================================
-// Load mocking/stubbing library, sinon
-// docs: http://sinonjs.org/releases/v2.3.2/
-Cu.import("resource://gre/modules/Timer.jsm");
-const {Loader} = Cu.import("resource://gre/modules/commonjs/toolkit/loader.js", {});
-const loader = new Loader.Loader({
-  paths: {
-    "": "resource://testing-common/",
-  },
-  globals: {
-    setTimeout,
-    setInterval,
-    clearTimeout,
-    clearInterval,
-  },
-});
-const require = Loader.Require(loader, {id: ""});
-const sinon = require("sinon-2.3.2");
-// ================================================
-
 Cu.import("resource://services-sync/UIState.jsm");
 
 const UIStateInternal = UIState._internal;
