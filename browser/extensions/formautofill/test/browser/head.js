eaa3ec75f75f4de9e8a3c05ecfebc6759274e560
Ray Lin <ralin@mozilla.com>
2017-06-13 10:29:56 +0800

    Bug 1300995 - Part 2. Add a browser chrome test for form autofill popup footer. r=MattN
    MozReview-Commit-ID: 7IAp0SzPzmm
    
    --HG--
    extra : rebase_source : 66ed7625a69524757da4769b068e0b221a0dfaa3

--- browser/extensions/formautofill/test/browser/head.js
+++ browser/extensions/formautofill/test/browser/head.js
@@ -1,11 +1,12 @@
-/* exported MANAGE_PROFILES_DIALOG_URL, EDIT_PROFILE_DIALOG_URL,
+/* exported MANAGE_PROFILES_DIALOG_URL, EDIT_PROFILE_DIALOG_URL, BASE_URL,
             TEST_ADDRESS_1, TEST_ADDRESS_2, TEST_ADDRESS_3,
-            getAddresses, saveAddress, removeAddresses */
+            sleep, getAddresses, saveAddress, removeAddresses */
 
 "use strict";
 
 const MANAGE_PROFILES_DIALOG_URL = "chrome://formautofill/content/manageProfiles.xhtml";
 const EDIT_PROFILE_DIALOG_URL = "chrome://formautofill/content/editProfile.xhtml";
+const BASE_URL = "http://mochi.test:8888/browser/browser/extensions/formautofill/test/browser/";
 
 const TEST_ADDRESS_1 = {
   "given-name": "John",
@@ -31,6 +32,10 @@ const TEST_ADDRESS_3 = {
   "postal-code": "12345",
 };
 
+async function sleep(ms = 500) {
+  await new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function getAddresses() {
   return new Promise(resolve => {
     Services.cpmm.addMessageListener("FormAutofill:Addresses", function getResult(result) {
@@ -50,3 +55,10 @@ function removeAddresses(guids) {
   Services.cpmm.sendAsyncMessage("FormAutofill:RemoveAddresses", {guids});
   return TestUtils.topicObserved("formautofill-storage-changed");
 }
+
+registerCleanupFunction(async function() {
+  let addresses = await getAddresses();
+  if (addresses.length) {
+    await removeAddresses(addresses.map(address => address.guid));
+  }
+});
