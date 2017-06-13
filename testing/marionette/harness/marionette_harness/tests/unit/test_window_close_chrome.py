fdf2a903c46ae063283545ecee62874f36e81585
Henrik Skupin <mail@hskupin.info>
2017-06-13 18:08:44 +0200

    Bug 1368965 - Don't inject chrome window handles into window handles. r=ato, a=test-only
    Chrome window handles and window handles are identifiers for different types
    of windows. As such those should not be mixed up. Especially for chrome
    windows without a tabbrowser we erroneously inject such a chrome window
    handle. It can break those tests which assume it's a tab.
    
    MozReview-Commit-ID: 1n2vOyfaSUh

--- testing/marionette/harness/marionette_harness/tests/unit/test_window_close_chrome.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_window_close_chrome.py
@@ -39,11 +39,12 @@ class TestCloseWindow(WindowManagerMixin, MarionetteTestCase):
         win = self.open_window(trigger=open_window_with_js)
         self.marionette.switch_to_window(win)
 
-        self.assertIn(win, self.marionette.window_handles)
+        self.assertIn(win, self.marionette.chrome_window_handles)
+        self.assertNotIn(win, self.marionette.window_handles)
         chrome_window_handles = self.marionette.close_chrome_window()
         self.assertNotIn(win, chrome_window_handles)
         self.assertListEqual(self.start_windows, chrome_window_handles)
-        self.assertNotIn(win, self.marionette.window_handles)
+        self.assertNotIn(win, self.marionette.chrome_window_handles)
 
     def test_close_chrome_window_for_last_open_window(self):
         self.close_all_windows()
