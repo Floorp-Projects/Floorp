fd88e9a71ee631ea1b573d7f4b5645a2454e4684
Michael Layzell <michael@thelayzells.com>
2017-06-13 13:37:31 -0400

    Bug 1350633 - Remove the Msg_GetWidgetNativeData sync IPC, r=billm, r=jimm
    MozReview-Commit-ID: Bql29wgVDZ5

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -411,6 +411,7 @@ TabChild::TabChild(nsIContentChild* aManager,
   , mPendingDocShellPreserveLayers(false)
   , mPendingDocShellReceivedMessage(false)
   , mPendingDocShellBlockers(0)
+  , mWidgetNativeData(0)
 {
   nsWeakPtr weakPtrThis(do_GetWeakReference(static_cast<nsITabChild*>(this)));  // for capture by the lambda
   mSetAllowedTouchBehaviorCallback = [weakPtrThis](uint64_t aInputBlockId,
@@ -3209,6 +3210,13 @@ TabChild::RecvSetOriginAttributes(const OriginAttributes& aOriginAttributes)
   nsCOMPtr<nsIDocShell> docShell = do_GetInterface(WebNavigation());
   nsDocShell::Cast(docShell)->SetOriginAttributes(aOriginAttributes);
 
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult
+TabChild::RecvSetWidgetNativeData(const WindowsHandle& aWidgetNativeData)
+{
+  mWidgetNativeData = aWidgetNativeData;
   return IPC_OK();
 }
 
