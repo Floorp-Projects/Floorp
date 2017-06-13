1eb768cc0d28ddb183eb486f58c00f32c4b67910
Tim Huang <tihuang@mozilla.com>
2017-06-13 14:10:18 +0800

    Bug 1372072 - Part 2: Add a test case for check whether network information API has been spoofed correctly when 'privacy.resistFingerprinting' is true. r=arthuredelstein,baku
    This adds a test case to test that network information is correctly spoofed when
    'privacy.resistFingerprinting' is true. This tests not only windows, but also
    workers.
    
    MozReview-Commit-ID: Lt6HZlFrcja

---
+++ browser/components/resistfingerprinting/test/browser/file_workerNetInfo.js
@@ -0,0 +1,26 @@
+function ok(a, msg) {
+  postMessage({type: "status", status: !!a, msg});
+}
+
+function is(a, b, msg) {
+  ok(a === b, msg);
+}
+
+function finish() {
+  postMessage({type: "finish"});
+}
+
+function runTests() {
+  ok("connection" in navigator, "navigator.connection should exist");
+  is(navigator.connection.type, "unknown", "The connection type is spoofed correctly");
+
+  finish();
+}
+
+self.onmessage = function(e) {
+  if (e.data.type === "runTests") {
+    runTests();
+  } else {
+    ok(false, "Unknown message type");
+  }
+}
