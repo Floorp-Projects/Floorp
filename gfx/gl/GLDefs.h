8195d166cd24992a110dc95f38b2be9c7d4a50b5
JerryShih <hshih@mozilla.com>
2017-06-12 23:11:00 -0400

    Bug 1372489 - Update GLConsts.h and GLDefs.h. r=jgilbert
    Get the latest gl.xml, egl.xml, wgl.xml and glx.xml.
    Then, execute the |GLParseRegistryXML.py|. Please check the details in |GLParseRegistryXML.py|.
    
    The ANGLE_stream_producer_d3d_texture_nv12 and ANGLE_keyed_mutex extension are not exist in egl.xml,
    so add the "LOCAL_EGL_DXGI_KEYED_MUTEX_ANGLE" and "LOCAL_EGL_D3D_TEXTURE_SUBRESOURCE_ID_ANGLE" manually in GLDefs.h.
    
    egl:
    https://github.com/KhronosGroup/EGL-Registry/commit/2e6e7ff7bf91d0ffcd7ca297aa2b18977b5e9492
    gl, glx and wgl:
    https://github.com/KhronosGroup/OpenGL-Registry/commit/23a3a95673921d4bf99001a0c50ef2ac2850dcf1
    
    MozReview-Commit-ID: nUVJix5HZp
    
    --HG--
    extra : rebase_source : 9eff9ab1f9867cf6dbe8ac3e09f7c07b8898371d

--- gfx/gl/GLDefs.h
+++ gfx/gl/GLDefs.h
@@ -9,6 +9,7 @@
 #include "GLConsts.h"
 
 // TODO: use official constant names instead of followed ones.
+
 // IMG_texture_compression_pvrtc
 #define LOCAL_GL_COMPRESSED_RGB_PVRTC_4BPPV1            0x8C00
 #define LOCAL_GL_COMPRESSED_RGB_PVRTC_2BPPV1            0x8C01
@@ -18,26 +19,6 @@
 // OES_EGL_image_external
 #define LOCAL_GL_TEXTURE_EXTERNAL                       0x8D65
 #define LOCAL_GL_TEXTURE_BINDING_EXTERNAL               0x8D67
-
-// EGL_KHR_fence_sync
-#define LOCAL_EGL_SYNC_FENCE                            0x30F9
-#define LOCAL_EGL_SYNC_TYPE                             0x30F7
-#define LOCAL_EGL_SYNC_STATUS                           0x30F1
-#define LOCAL_EGL_SYNC_CONDITION                        0x30F8
-#define LOCAL_EGL_SIGNALED                              0x30F2
-#define LOCAL_EGL_UNSIGNALED                            0x30F3
-#define LOCAL_EGL_SYNC_PRIOR_COMMANDS_COMPLETE          0x30F0
-#define LOCAL_EGL_SYNC_FLUSH_COMMANDS_BIT               0x0001
-#define LOCAL_EGL_FOREVER                               0xFFFFFFFFFFFFFFFFull
-#define LOCAL_EGL_TIMEOUT_EXPIRED                       0x30F5
-#define LOCAL_EGL_CONDITION_SATISFIED                   0x30F6
-#define LOCAL_EGL_SUCCESS                               0x3000
-
-// EGL_KHR_gl_texture_2D_image
-#define LOCAL_EGL_GL_TEXTURE_2D                         0x30B1
-
-// EGL_KHR_image_base (not supplied by EGL_KHR_image!)
-#define LOCAL_EGL_IMAGE_PRESERVED                       0x30D2
 
 // AMD_compressed_ATC_texture
 #define LOCAL_GL_ATC_RGB                                0x8C92
@@ -57,6 +38,12 @@
 #define LOCAL_EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE    0x3205
 #define LOCAL_EGL_PLATFORM_ANGLE_TYPE_DEFAULT_ANGLE         0x3206
 
+// EGL_ANGLE_keyed_mutex
+#define LOCAL_EGL_DXGI_KEYED_MUTEX_ANGLE                     0x3209
+
+// EGL_ANGLE_stream_producer_d3d_texture_nv12
+#define LOCAL_EGL_D3D_TEXTURE_SUBRESOURCE_ID_ANGLE           0x33AB
+
 // EGL_ANGLE_platform_angle_d3d
 #define LOCAL_EGL_PLATFORM_ANGLE_TYPE_D3D9_ANGLE              0x3207
 #define LOCAL_EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE             0x3208
@@ -75,16 +62,9 @@
 #define LOCAL_WGL_ACCESS_READ_WRITE                     0x0001
 #define LOCAL_WGL_ACCESS_WRITE_DISCARD                  0x0002
 
-// GL_KHR_robustness
-#define LOCAL_GL_CONTEXT_ROBUST_ACCESS                  0x90F3
-
 // Others
 #define LOCAL_EGL_PRESERVED_RESOURCES                   0x3030
 #define LOCAL_EGL_CONTEXT_RESET_NOTIFICATION_STRATEGY_EXT 0x3138
-#define LOCAL_GL_RESET_NOTIFICATION_STRATEGY            0x8256
-#define LOCAL_GL_LOSE_CONTEXT_ON_RESET                  0x8252
-#define LOCAL_GL_NO_RESET_NOTIFICATION                  0x8261
-#define LOCAL_GL_CONTEXT_LOST                           0x9242
 #define LOCAL_GL_CONTEXT_FLAGS_ARB                      0x2094
 #define LOCAL_GL_CONTEXT_CORE_PROFILE_BIT_ARB           0x00000001
 #define LOCAL_GL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB  0x00000002
