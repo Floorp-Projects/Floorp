8aeb2009b35bf5c9c9dcd3a571b2a86b666053ce
JerryShih <hshih@mozilla.com>
2017-06-15 14:43:53 +0800

    Bug 1372490 - GetError() returns GL_CONTEXT_LOST not GL_CONTEXT_LOST_WEBGL. r=jgilbert
    MozReview-Commit-ID: KYr0cKoeWJo

--- dom/canvas/WebGLContextGL.cpp
+++ dom/canvas/WebGLContextGL.cpp
@@ -863,7 +863,7 @@ WebGLContext::GetError()
     if (IsContextLost()) {
         if (mEmitContextLostErrorOnce) {
             mEmitContextLostErrorOnce = false;
-            return LOCAL_GL_CONTEXT_LOST;
+            return LOCAL_GL_CONTEXT_LOST_WEBGL;
         }
         // Don't return yet, since WEBGL_lose_contexts contradicts the
         // original spec, and allows error generation while lost.
