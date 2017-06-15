783d39bffbac028ec17287fa5084e4d4adb8d2dd
Sean Lee <selee@mozilla.com>
2017-06-15 09:45:24 +0800

    Bug 1372843 - Identify the focused form only when applying heuristics. r=MattN
    MozReview-Commit-ID: 9y6GgYxlQt3

--- browser/extensions/formautofill/test/unit/test_onFormSubmitted.js
+++ browser/extensions/formautofill/test/unit/test_onFormSubmitted.js
@@ -11,6 +11,7 @@ const MOCK_DOC = MockDocument.createTestDocument("http://localhost:8080/test/",
                       <input id="tel" autocomplete="tel">
                       <input id="submit" type="submit">
                     </form>`);
+const TARGET_ELEMENT_ID = "street-addr";
 
 const TESTCASES = [
   {
@@ -110,7 +111,8 @@ TESTCASES.forEach(testcase => {
     }
     sinon.stub(FormAutofillContent, "_onFormSubmit");
 
-    FormAutofillContent.identifyAutofillFields(MOCK_DOC);
+    let element = MOCK_DOC.getElementById(TARGET_ELEMENT_ID);
+    FormAutofillContent.identifyAutofillFields(element);
     FormAutofillContent.notify(form);
 
     do_check_eq(FormAutofillContent._onFormSubmit.called,
