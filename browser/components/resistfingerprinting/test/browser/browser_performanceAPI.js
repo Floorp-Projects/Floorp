fc7f9d8b230d48c7c8f5b4d858fa16768c32e6df
Tim Huang <tihuang@mozilla.com>
2017-06-15 16:48:28 +0800

    Bug 1369303 - Part 3: Add a test case for making sure that performance APIs have been correctly spoofed. r=arthuredelstein,baku
    This test case will open a content tab and access performance API in that
    tab to check whether or not performance APIs are correctly spoofed.
    
    MozReview-Commit-ID: KdG6xzQFmv6

---
+++ browser/components/resistfingerprinting/test/browser/browser_performanceAPI.js
@@ -0,0 +1,59 @@
+/**
+ * Bug 1369303 - A test for making sure that performance APIs have been correctly
+ *   spoofed or disabled.
+ */
+
+const TEST_PATH = "http://example.net/browser/browser/" +
+                  "components/resistfingerprinting/test/browser/"
+
+const PERFORMANCE_TIMINGS = [
+  "navigationStart",
+  "unloadEventStart",
+  "unloadEventEnd",
+  "redirectStart",
+  "redirectEnd",
+  "fetchStart",
+  "domainLookupStart",
+  "domainLookupEnd",
+  "connectStart",
+  "connectEnd",
+  "requestStart",
+  "responseStart",
+  "responseEnd",
+  "domLoading",
+  "domInteractive",
+  "domContentLoadedEventStart",
+  "domContentLoadedEventEnd",
+  "domComplete",
+  "loadEventStart",
+  "loadEventEnd",
+];
+
+add_task(async function runTests() {
+  await SpecialPowers.pushPrefEnv({"set":
+    [["privacy.resistFingerprinting", true]]
+  });
+
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser, TEST_PATH + "file_dummy.html");
+
+  await ContentTask.spawn(tab.linkedBrowser, PERFORMANCE_TIMINGS, async function(list) {
+    // Check that whether the performance timing API is correctly spoofed.
+    for (let time of list) {
+      is(content.performance.timing[time], 0, `The timing(${time}) is correctly spoofed.`);
+    }
+
+    // Try to add some entries.
+    content.performance.mark("Test");
+    content.performance.mark("Test-End");
+    content.performance.measure("Test-Measure", "Test", "Test-End");
+
+    // Check that no entries for performance.getEntries/getEntriesByType/getEntriesByName.
+    is(content.performance.getEntries().length, 0, "No entries for performance.getEntries()");
+    is(content.performance.getEntriesByType("resource").length, 0, "No entries for performance.getEntriesByType()");
+    is(content.performance.getEntriesByName("Test", "mark").length, 0, "No entries for performance.getEntriesByName()");
+
+  });
+
+  await BrowserTestUtils.removeTab(tab);
+});
