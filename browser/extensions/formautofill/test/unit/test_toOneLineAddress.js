2038145a170cd8c6c31457743443b19f321243d0
Sean Lee <selee@mozilla.com>
2017-06-15 17:19:34 +0800

    Bug 1370475 - Part 2: Convert the address related fields for display purpose. r=lchang
    MozReview-Commit-ID: 1Jk0pRc1snu

---
+++ browser/extensions/formautofill/test/unit/test_toOneLineAddress.js
@@ -0,0 +1,52 @@
+
+"use strict";
+
+Cu.import("resource://formautofill/FormAutofillUtils.jsm");
+
+add_task(async function test_getCategoriesFromFieldNames() {
+  const TEST_CASES = [
+    {
+      strings: ["A", "B", "C", "D"],
+      expectedValue: "A B C D",
+    },
+    {
+      strings: ["A", "B", "", "D"],
+      expectedValue: "A B D",
+    },
+    {
+      strings: ["", "B", "", "D"],
+      expectedValue: "B D",
+    },
+    {
+      strings: "A B C",
+      expectedValue: "A B C",
+    },
+    {
+      strings: "A\nB\n\n\nC",
+      expectedValue: "A B C",
+    },
+    {
+      strings: "A B \nC",
+      expectedValue: "A B C",
+    },
+    {
+      strings: "A-B-C",
+      expectedValue: "A B C",
+      delimiter: "-",
+    },
+    {
+      strings: "A B\n \nC",
+      expectedValue: "A B C",
+    },
+  ];
+
+  for (let tc of TEST_CASES) {
+    let result;
+    if (tc.delimiter) {
+      result = FormAutofillUtils.toOneLineAddress(tc.strings, tc.delimiter);
+    } else {
+      result = FormAutofillUtils.toOneLineAddress(tc.strings);
+    }
+    Assert.deepEqual(result, tc.expectedValue);
+  }
+});
