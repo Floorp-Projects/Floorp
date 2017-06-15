33d1176016f1e5ae17e1a4f4532a0d28b8cd0dd8
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-15 23:07:08 +0200

    Backed out changeset cc5161b809a0 (bug 1300995)

--- browser/extensions/formautofill/test/browser/head.js
+++ browser/extensions/formautofill/test/browser/head.js
@@ -1,12 +1,11 @@
-/* exported MANAGE_PROFILES_DIALOG_URL, EDIT_PROFILE_DIALOG_URL, BASE_URL,
+/* exported MANAGE_PROFILES_DIALOG_URL, EDIT_PROFILE_DIALOG_URL,
             TEST_ADDRESS_1, TEST_ADDRESS_2, TEST_ADDRESS_3,
-            sleep, getAddresses, saveAddress, removeAddresses */
+            getAddresses, saveAddress, removeAddresses */
 
 "use strict";
 
 const MANAGE_PROFILES_DIALOG_URL = "chrome://formautofill/content/manageProfiles.xhtml";
 const EDIT_PROFILE_DIALOG_URL = "chrome://formautofill/content/editProfile.xhtml";
-const BASE_URL = "http://mochi.test:8888/browser/browser/extensions/formautofill/test/browser/";
 
 const TEST_ADDRESS_1 = {
   "given-name": "John",
@@ -32,10 +31,6 @@ const TEST_ADDRESS_3 = {
   "postal-code": "12345",
 };
 
-async function sleep(ms = 500) {
-  await new Promise(resolve => setTimeout(resolve, ms));
-}
-
 function getAddresses() {
   return new Promise(resolve => {
     Services.cpmm.addMessageListener("FormAutofill:Addresses", function getResult(result) {
@@ -55,10 +50,3 @@ function removeAddresses(guids) {
   Services.cpmm.sendAsyncMessage("FormAutofill:RemoveAddresses", {guids});
   return TestUtils.topicObserved("formautofill-storage-changed");
 }
-
-registerCleanupFunction(async function() {
-  let addresses = await getAddresses();
-  if (addresses.length) {
-    await removeAddresses(addresses.map(address => address.guid));
-  }
-});
