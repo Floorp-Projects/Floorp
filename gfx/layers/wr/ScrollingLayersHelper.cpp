9337827d1328fa225ad630df92caaa69dd454dae
Kartikaya Gupta <kgupta@mozilla.com>
2017-06-15 15:35:04 -0400

    Bug 1373349 - Correct the origin of the content rect when creating scroll layers in WR. r=botond,jrmuizel
    Also tweaks the documentation in FrameMetrics.h to be more detailed and
    less misleading.
    
    MozReview-Commit-ID: 183V2Q9kY6C

--- gfx/layers/wr/ScrollingLayersHelper.cpp
+++ gfx/layers/wr/ScrollingLayersHelper.cpp
@@ -63,11 +63,13 @@ ScrollingLayersHelper::ScrollingLayersHelper(WebRenderLayer* aLayer,
     // The content rect that we hand to PushScrollLayer should be relative to
     // the same origin as the clipBounds that we hand to PushScrollLayer - that
     // is, both of them should be relative to the stacking context `aStackingContext`.
-    // However, when we get the scrollable rect from the FrameMetrics, it has
-    // a nominal top-left of 0,0 (maybe different for RTL pages?) and so to
-    // get it in the same coordinate space we're going to shift it by the
-    // composition bounds top-left.
-    contentRect.MoveBy(clipBounds.TopLeft());
+    // However, when we get the scrollable rect from the FrameMetrics, the origin
+    // has nothing to do with the position of the frame but instead represents
+    // the minimum allowed scroll offset of the scrollable content. While APZ
+    // uses this to clamp the scroll position, we don't need to send this to
+    // WebRender at all. Instead, we take the position from the composition
+    // bounds.
+    contentRect.MoveTo(clipBounds.TopLeft());
     mBuilder->PushScrollLayer(fm.GetScrollId(),
         aStackingContext.ToRelativeWrRect(contentRect),
         aStackingContext.ToRelativeWrRect(clipBounds));
