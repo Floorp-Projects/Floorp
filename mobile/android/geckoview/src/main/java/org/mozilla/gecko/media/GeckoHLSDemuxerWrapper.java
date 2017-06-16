31f11c7ae335f8491a61b45f5c5c5afd9e6d676c
Phil Ringnalda <philringnalda@gmail.com>
2017-06-15 22:54:37 -0700

    Backed out changeset d598bbacfa4b (bug 1373500) for Fennec build bustage
    MozReview-Commit-ID: 16xfBr8sfWw

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/GeckoHLSDemuxerWrapper.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/GeckoHLSDemuxerWrapper.java
@@ -14,7 +14,7 @@ import org.mozilla.gecko.mozglue.JNIObject;
 
 public final class GeckoHLSDemuxerWrapper {
     private static final String LOGTAG = "GeckoHLSDemuxerWrapper";
-    private static final boolean DEBUG = AppConstants.NIGHTLY_BUILD || AppConstants.DEBUG_BUILD;;
+    private static final boolean DEBUG = false;
 
     // NOTE : These TRACK definitions should be synced with Gecko.
     public enum TrackType {
