69f69b6f43f321ef1371d02941c13b83b855c403
Oriol <oriol-bugzilla@hotmail.com>
2017-06-16 11:31:00 -0400

    Bug 1373765 - Move jsonViewInitialized into JSONView.initialized. r=Honza

--- devtools/client/jsonview/json-viewer.js
+++ devtools/client/jsonview/json-viewer.js
@@ -100,6 +100,6 @@ define(function (require, exports, module) {
   // Send notification event to the window. Can be useful for
   // tests as well as extensions.
   let event = new CustomEvent("JSONViewInitialized", {});
-  window.jsonViewInitialized = true;
+  JSONView.initialized = true;
   window.dispatchEvent(event);
 });
