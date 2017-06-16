1687a10f2f1357eead3c9a0cf79c757e63059f51
Justin Wood <Callek@gmail.com>
2017-06-16 15:48:00 -0400

    Bug 1373326 - Reformat repackage mozharness code to support new mach repackage stuff. r=aki (For Landing more OSX Nightly Support from date to central)
    MozReview-Commit-ID: FSbQZ1Fbdcs

--- testing/mozharness/configs/repackage/osx_signed.py
+++ testing/mozharness/configs/repackage/osx_signed.py
@@ -1,15 +1,22 @@
 import os
 
 config = {
-    "input_filename": "target.tar.gz",
-    "output_filename": "target.dmg",
-    "input_home": "/home/worker/workspace/inputs",
+    "input_home": "{abs_work_dir}/inputs",
     "src_mozconfig": "browser/config/mozconfigs/macosx64/repack",
+
+    "download_config": {
+        "target.tar.gz": os.environ.get("SIGNED_INPUT"),
+    },
+
+    "repackage_config": [[
+        "dmg",
+        "-i", "{abs_work_dir}/inputs/target.tar.gz",
+        "-o", "{output_home}/target.dmg"
+    ]],
 
     # ToolTool
     "tooltool_manifest_src": 'browser/config/tooltool-manifests/macosx64/cross-releng.manifest',
     "tooltool_url": 'http://relengapi/tooltool/',
-    "tooltool_bootstrap": "setup.sh",
     'tooltool_script': ["/builds/tooltool.py"],
     'tooltool_cache': os.environ.get('TOOLTOOL_CACHE'),
 }
