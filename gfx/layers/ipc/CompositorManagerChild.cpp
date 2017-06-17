e2142193ab72e28216f7d9166dcc876b2e0b830e
Andrew Osmond <aosmond@mozilla.com>
2017-06-17 17:52:34 -0400

    Bug 1373540 - Fix switchover from the GPU process compositor to the same process compositor. r=dvander

--- gfx/layers/ipc/CompositorManagerChild.cpp
+++ gfx/layers/ipc/CompositorManagerChild.cpp
@@ -54,6 +54,20 @@ CompositorManagerChild::Init(Endpoint<PCompositorManagerChild>&& aEndpoint,
 }
 
 /* static */ void
+CompositorManagerChild::OnGPUProcessShutdown()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  if (!sInstance) {
+    return;
+  }
+
+  // GPUChild was torn down first when the GPU process was lost. We can't wait
+  // for CompositorManagerChild::ActorDestroy because we may recreate the
+  // CompositorManager pair before that is executed.
+  sInstance = nullptr;
+}
+
+/* static */ void
 CompositorManagerChild::Shutdown()
 {
   MOZ_ASSERT(NS_IsMainThread());
@@ -187,6 +201,9 @@ void
 CompositorManagerChild::ActorDestroy(ActorDestroyReason aReason)
 {
   mCanSend = false;
+  if (sInstance == this) {
+    sInstance = nullptr;
+  }
 }
 
 PCompositorBridgeChild*
