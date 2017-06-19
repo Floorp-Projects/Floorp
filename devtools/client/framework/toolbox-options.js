e37e37487a2c76ba866247b0ca450c7f0644882c
Gabriel Luong <gabriel.luong@gmail.com>
2017-06-19 14:45:43 -0400

    Bug 1373492 - Enable the layout view by default. r=pbro

--- devtools/client/framework/toolbox-options.js
+++ devtools/client/framework/toolbox-options.js
@@ -322,11 +322,6 @@ OptionsPanel.prototype = {
       label: "Enable new debugger frontend",
       id: "devtools-new-debugger",
       parentId: "debugger-options"
-    }, {
-      pref: "devtools.layoutview.enabled",
-      label: "Enable layout panel",
-      id: "devtools-layout-panel",
-      parentId: "inspector-options"
     }];
 
     let createPreferenceOption = ({pref, label, id}) => {
