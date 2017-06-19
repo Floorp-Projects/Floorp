0abd58582307218ac4a44b787131f4f44b0d86ef
Boris Zbarsky <bzbarsky@mit.edu>
2017-06-19 14:42:01 -0400

    Bug 1373798 part 3.  Rewrite our existing checks for the state of the "dir" attr on top of the new event state flags.  r=mystor
    MozReview-Commit-ID: LpCYABK5ZRN
    
    --HG--
    extra : rebase_source : 2771f8fb377b051b8dc8d93d08a2360817331f21

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -305,6 +305,23 @@ public:
   void ClearStyleStateLocks();
 
   /**
+   * Accessors for the state of our dir attribute.
+   */
+  bool HasDirAuto() const
+  {
+    return State().HasState(NS_EVENT_STATE_DIR_ATTR_LIKE_AUTO);
+  }
+
+  /**
+   * Elements with dir="rtl" or dir="ltr".
+   */
+  bool HasFixedDir() const
+  {
+    return State().HasAtLeastOneOfStates(NS_EVENT_STATE_DIR_ATTR_LTR |
+                                         NS_EVENT_STATE_DIR_ATTR_RTL);
+  }
+
+  /**
    * Get the inline style declaration, if any, for this element.
    */
   DeclarationBlock* GetInlineStyleDeclaration() const;
@@ -446,15 +463,6 @@ public:
   }
 
   bool GetBindingURL(nsIDocument* aDocument, css::URLValue **aResult);
-
-  // The bdi element defaults to dir=auto if it has no dir attribute set.
-  // Other elements will only have dir=auto if they have an explicit dir=auto,
-  // which will mean that HasValidDir() returns true but HasFixedDir() returns
-  // false
-  inline bool HasDirAuto() const {
-    return (!HasFixedDir() &&
-            (HasValidDir() || IsHTMLElement(nsGkAtoms::bdi)));
-  }
 
   Directionality GetComputedDirectionality() const;
 
