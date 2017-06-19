af4e81298bc7f2b983caac2e60bc86a060dd9131
domfarolino@gmail.com <domfarolino@gmail.com>
2017-06-19 13:12:00 -0400

    Bug 1373088 - Don't use a timeout for compositor process startup when debugging child process. r=milan
    --HG--
    extra : rebase_source : 39e5c564fc7b0c2f7e94535a9c7b17bfd03914e3

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -551,7 +551,7 @@ private:
   DECL_GFX_PREF(Once, "layers.force-shmem-tiles",              ForceShmemTiles, bool, false);
   DECL_GFX_PREF(Once, "layers.gpu-process.enabled",            GPUProcessEnabled, bool, false);
   DECL_GFX_PREF(Once, "layers.gpu-process.force-enabled",      GPUProcessForceEnabled, bool, false);
-  DECL_GFX_PREF(Once, "layers.gpu-process.timeout_ms",         GPUProcessTimeoutMs, int32_t, 5000);
+  DECL_GFX_PREF(Once, "layers.gpu-process.startup_timeout_ms", GPUProcessTimeoutMs, int32_t, 5000);
   DECL_GFX_PREF(Live, "layers.gpu-process.max_restarts",       GPUProcessMaxRestarts, int32_t, 1);
   DECL_GFX_PREF(Once, "layers.gpu-process.allow-software",     GPUProcessAllowSoftware, bool, false);
   // Note: This pref will only be used if it is less than layers.gpu-process.max_restarts.
