378b5b97c262eac6ca54e90629828c6070c04251
JW Wang <jwwang@mozilla.com>
2017-06-20 18:10:56 +0800

    Bug 1374930. P4 - move ResourceCallback to ChannelMediaDecoder. r=cpearce
    We also move some methods to protected so they are callable from ChannelMediaDecoder.
    
    MozReview-Commit-ID: 6s9LKNkbJhX

--- dom/media/ChannelMediaDecoder.h
+++ dom/media/ChannelMediaDecoder.h
@@ -8,13 +8,60 @@
 #define ChannelMediaDecoder_h_
 
 #include "MediaDecoder.h"
+#include "MediaResourceCallback.h"
 
 namespace mozilla {
 
 class ChannelMediaDecoder : public MediaDecoder
 {
+  // Used to register with MediaResource to receive notifications which will
+  // be forwarded to MediaDecoder.
+  class ResourceCallback : public MediaResourceCallback
+  {
+    // Throttle calls to MediaDecoder::NotifyDataArrived()
+    // to be at most once per 500ms.
+    static const uint32_t sDelay = 500;
+
+  public:
+    explicit ResourceCallback(AbstractThread* aMainThread);
+    // Start to receive notifications from ResourceCallback.
+    void Connect(ChannelMediaDecoder* aDecoder);
+    // Called upon shutdown to stop receiving notifications.
+    void Disconnect();
+
+  private:
+    /* MediaResourceCallback functions */
+    MediaDecoderOwner* GetMediaOwner() const override;
+    void SetInfinite(bool aInfinite) override;
+    void NotifyNetworkError() override;
+    void NotifyDataArrived() override;
+    void NotifyDataEnded(nsresult aStatus) override;
+    void NotifyPrincipalChanged() override;
+    void NotifySuspendedStatusChanged() override;
+    void NotifyBytesConsumed(int64_t aBytes, int64_t aOffset) override;
+
+    static void TimerCallback(nsITimer* aTimer, void* aClosure);
+
+    // The decoder to send notifications. Main-thread only.
+    ChannelMediaDecoder* mDecoder = nullptr;
+    nsCOMPtr<nsITimer> mTimer;
+    bool mTimerArmed = false;
+    const RefPtr<AbstractThread> mAbstractMainThread;
+  };
+
+  RefPtr<ResourceCallback> mResourceCallback;
+
 public:
   explicit ChannelMediaDecoder(MediaDecoderInit& aInit);
+
+  // Return a callback object used to register with MediaResource to receive
+  // notifications.
+  MediaResourceCallback* GetResourceCallback() const
+  {
+    return mResourceCallback;
+  }
+
+  void Shutdown() override;
 
   // Create a new decoder of the same type as this one.
   // Subclasses must implement this.
