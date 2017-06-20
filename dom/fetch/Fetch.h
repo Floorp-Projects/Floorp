92d89f8544e76f75fd5344769e0d99fa548344b8
Andrea Marchesini <amarchesini@mozilla.com>
2017-06-20 17:53:21 +0200

    Bug 1373555 - Move the Fetch consume body login in a separate class - part 3 - Move the consuming body logic from FetchBody to FetchBodyConsumer, r=bkelly

--- dom/fetch/Fetch.h
+++ dom/fetch/Fetch.h
@@ -8,7 +8,6 @@
 #define mozilla_dom_Fetch_h
 
 #include "nsAutoPtr.h"
-#include "nsIInputStreamPump.h"
 #include "nsIStreamLoader.h"
 
 #include "nsCOMPtr.h"
@@ -73,6 +72,15 @@ ExtractByteStreamFromBody(const fetch::BodyInit& aBodyInit,
 
 template <class Derived> class FetchBodyConsumer;
 
+enum FetchConsumeType
+{
+  CONSUME_ARRAYBUFFER,
+  CONSUME_BLOB,
+  CONSUME_FORMDATA,
+  CONSUME_JSON,
+  CONSUME_TEXT,
+};
+
 /*
  * FetchBody's body consumption uses nsIInputStreamPump to read from the
  * underlying stream to a block of memory, which is then adopted by
@@ -110,6 +118,8 @@ template <class Derived>
 class FetchBody
 {
 public:
+  friend class FetchBodyConsumer<Derived>;
+
   NS_INLINE_DECL_PURE_VIRTUAL_REFCOUNTING
 
   bool
@@ -146,19 +156,6 @@ public:
   }
 
   // Utility public methods accessed by various runnables.
-  void
-  BeginConsumeBodyMainThread(FetchBodyConsumer<Derived>* aConsumer);
-
-  void
-  ContinueConsumeBody(FetchBodyConsumer<Derived>* aConsumer, nsresult aStatus,
-                      uint32_t aLength, uint8_t* aResult);
-
-  void
-  ContinueConsumeBlobBody(FetchBodyConsumer<Derived>* aConsumer,
-                          BlobImpl* aBlobImpl);
-
-  void
-  CancelPump();
 
   void
   SetBodyUsed()
@@ -166,11 +163,17 @@ public:
     mBodyUsed = true;
   }
 
-  // Always set whenever the FetchBody is created on the worker thread.
-  workers::WorkerPrivate* mWorkerPrivate;
+  const nsCString&
+  MimeType() const
+  {
+    return mMimeType;
+  }
 
 protected:
   nsCOMPtr<nsIGlobalObject> mOwner;
+
+  // Always set whenever the FetchBody is created on the worker thread.
+  workers::WorkerPrivate* mWorkerPrivate;
 
   explicit FetchBody(nsIGlobalObject* aOwner);
 
@@ -178,27 +181,16 @@ protected:
 
   void
   SetMimeType();
-private:
-  enum ConsumeType
-  {
-    CONSUME_ARRAYBUFFER,
-    CONSUME_BLOB,
-    CONSUME_FORMDATA,
-    CONSUME_JSON,
-    CONSUME_TEXT,
-  };
 
+private:
   Derived*
   DerivedClass() const
   {
     return static_cast<Derived*>(const_cast<FetchBody*>(this));
   }
 
-  nsresult
-  BeginConsumeBody();
-
   already_AddRefed<Promise>
-  ConsumeBody(ConsumeType aType, ErrorResult& aRv);
+  ConsumeBody(FetchConsumeType aType, ErrorResult& aRv);
 
   bool
   IsOnTargetThread()
@@ -215,15 +207,6 @@ private:
   // Only ever set once, always on target thread.
   bool mBodyUsed;
   nsCString mMimeType;
-
-  // Only touched on target thread.
-  ConsumeType mConsumeType;
-  RefPtr<Promise> mConsumePromise;
-#ifdef DEBUG
-  bool mReadDone;
-#endif
-
-  nsMainThreadPtrHandle<nsIInputStreamPump> mConsumeBodyPump;
 
   // The main-thread event target for runnable dispatching.
   nsCOMPtr<nsIEventTarget> mMainThreadEventTarget;
