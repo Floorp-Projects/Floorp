8bf9340e551da8eecbef972e3f92d9fb4fe750a6
Kartikaya Gupta <kgupta@mozilla.com>
2017-06-20 16:04:07 -0400

    Bug 1369156 - Bump the minimum allowed version of cargo-vendor. r=froydnj
    MozReview-Commit-ID: 2W7og1U8LBv

--- python/mozbuild/mozbuild/vendor_rust.py
+++ python/mozbuild/mozbuild/vendor_rust.py
@@ -39,15 +39,18 @@ class VendorRust(MozbuildObject):
 
     def check_cargo_vendor_version(self, cargo):
         '''
-        Ensure that cargo-vendor is new enough. cargo-vendor 0.1.3 and newer
-        strips out .gitattributes files which we want.
+        Ensure that cargo-vendor is new enough. cargo-vendor 0.1.11 and newer
+        strips out .orig and .rej files which we want.
         '''
         for l in subprocess.check_output([cargo, 'install', '--list']).splitlines():
-            # The line looks like `cargo-vendor v0.1.3:`
-            m = re.match('cargo-vendor v((\d\.)*\d):', l)
+            # The line looks like one of the following:
+            #  cargo-vendor v0.1.11:
+            #  cargo-vendor v0.1.11 (file:///path/to/local/build/cargo-vendor):
+            # and we want to extract the version part of it
+            m = re.match('cargo-vendor v((\d+\.)*\d+)', l)
             if m:
                 version = m.group(1)
-                return LooseVersion(version) >= b'0.1.3'
+                return LooseVersion(version) >= b'0.1.11'
         return False
 
     def check_modified_files(self):
@@ -114,7 +117,7 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
             self.run_process(args=[cargo, 'install', 'cargo-vendor'],
                              append_env=env)
         elif not self.check_cargo_vendor_version(cargo):
-            self.log(logging.INFO, 'cargo_vendor', {}, 'cargo-vendor >= 0.1.3 required; force-reinstalling (this may take a few minutes)...')
+            self.log(logging.INFO, 'cargo_vendor', {}, 'cargo-vendor >= 0.1.11 required; force-reinstalling (this may take a few minutes)...')
             env = self.check_openssl()
             self.run_process(args=[cargo, 'install', '--force', 'cargo-vendor'],
                              append_env=env)
@@ -274,7 +277,7 @@ license file's hash.
             path = mozpath.join(self.topsrcdir, crate_root)
             # We do an |update -p| here to regenerate the Cargo.lock file with minimal changes. See bug 1324462
             self._run_command_in_srcdir(args=[cargo, 'update', '--manifest-path', mozpath.join(path, 'Cargo.toml'), '-p', lib])
-            self._run_command_in_srcdir(args=[cargo, 'vendor', '--sync', mozpath.join(path, 'Cargo.lock'), vendor_dir])
+            self._run_command_in_srcdir(args=[cargo, 'vendor', '--no-delete', '--sync', mozpath.join(path, 'Cargo.lock'), vendor_dir])
 
         if not self._check_licenses(vendor_dir):
             self.log(logging.ERROR, 'license_check_failed', {},
