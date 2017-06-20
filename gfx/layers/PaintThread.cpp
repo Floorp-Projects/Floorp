dafcabde790088c39babbf74cbff66b8de04501f
Mason Chang <mchang@mozilla.com>
2017-06-20 13:35:39 -0700

    Bug 1372441 - Enable replay of ClientPaintedLayers off the main thread. r=dvander

--- gfx/layers/PaintThread.cpp
+++ gfx/layers/PaintThread.cpp
@@ -6,21 +6,48 @@
 
 #include "PaintThread.h"
 
+#include "mozilla/gfx/DrawEventRecorder.h"
+#include "mozilla/gfx/InlineTranslator.h"
+#include "mozilla/SyncRunnable.h"
+
 namespace mozilla {
 namespace layers {
 
+using namespace gfx;
+
 StaticAutoPtr<PaintThread> PaintThread::sSingleton;
+
+void
+PaintThread::Release()
+{
+}
+
+void
+PaintThread::AddRef()
+{
+}
+
+void
+PaintThread::InitOnPaintThread()
+{
+  MOZ_ASSERT(!NS_IsMainThread());
+  mThreadId = PlatformThread::CurrentId();
+}
 
 bool
 PaintThread::Init()
 {
   MOZ_ASSERT(NS_IsMainThread());
-  nsresult rv = NS_NewNamedThread("PaintThread", getter_AddRefs(PaintThread::sSingleton->mThread));
+  nsresult rv = NS_NewNamedThread("PaintThread", getter_AddRefs(mThread));
 
   if (NS_FAILED(rv)) {
     return false;
   }
 
+  nsCOMPtr<nsIRunnable> paintInitTask =
+    NewRunnableMethod(this, &PaintThread::InitOnPaintThread);
+
+  SyncRunnable::DispatchToThread(PaintThread::sSingleton->mThread, paintInitTask);
   return true;
 }
 
@@ -58,6 +85,37 @@ PaintThread::ShutdownImpl()
 {
   MOZ_ASSERT(NS_IsMainThread());
   PaintThread::sSingleton->mThread->AsyncShutdown();
+}
+
+bool
+PaintThread::IsOnPaintThread()
+{
+  MOZ_ASSERT(mThread);
+  return PlatformThread::CurrentId() == mThreadId;
+}
+
+void
+PaintThread::PaintContents(DrawEventRecorderMemory* aRecording,
+                           DrawTarget* aTarget)
+{
+  if (!IsOnPaintThread()) {
+    MOZ_ASSERT(NS_IsMainThread());
+    nsCOMPtr<nsIRunnable> paintTask =
+      NewRunnableMethod<DrawEventRecorderMemory*, DrawTarget*>(this,
+                                                  &PaintThread::PaintContents,
+                                                  aRecording, aTarget);
+
+    SyncRunnable::DispatchToThread(mThread, paintTask);
+    return;
+  }
+
+  // Draw all the things into the actual dest target.
+  // This shouldn't exist in the future. For now, its just testing
+  // to make sure we properly record and can replay all the draw
+  // commands
+  std::istream& stream = aRecording->GetInputStream();
+  InlineTranslator translator(aTarget, nullptr);
+  translator.TranslateRecording(stream);
 }
 
 } // namespace layers
