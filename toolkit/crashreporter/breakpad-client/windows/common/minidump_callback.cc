8b323a48e253b681e6710567a3d05c5fb8c5d11e
Cervantes Yu <cyu@mozilla.com>
2017-06-20 15:00:16 +0800

    Bug 1286802 - Part 2: Refactor google_breakpad::ExceptionHandler for including heap regions. r=ted
    This moves type definitions and minidump callback to minidump_callback.{h|cpp},
    which will be used by google_breakpad::ExceptionHandler for in-process crash
    generation, and by googld_breakpad::CrashGenerationServer for out-of-process
    crash generation.
    
    MozReview-Commit-ID: AMsQHSUTYNx

--- toolkit/crashreporter/breakpad-client/windows/common/minidump_callback.cc
+++ toolkit/crashreporter/breakpad-client/windows/common/minidump_callback.cc
@@ -6,3 +6,49 @@
 
 #include "minidump_callback.h"
 
+#include <algorithm>
+#include <cassert>
+
+namespace google_breakpad {
+
+BOOL CALLBACK MinidumpWriteDumpCallback(
+    PVOID context,
+    const PMINIDUMP_CALLBACK_INPUT callback_input,
+    PMINIDUMP_CALLBACK_OUTPUT callback_output) {
+  switch (callback_input->CallbackType) {
+  case MemoryCallback: {
+    MinidumpCallbackContext* callback_context =
+        reinterpret_cast<MinidumpCallbackContext*>(context);
+
+    if (callback_context->iter == callback_context->end)
+      return FALSE;
+
+    // Include the specified memory region.
+    callback_output->MemoryBase = callback_context->iter->ptr;
+    callback_output->MemorySize = callback_context->iter->length;
+    callback_context->iter++;
+    return TRUE;
+  }
+
+    // Include all modules.
+  case IncludeModuleCallback:
+  case ModuleCallback:
+    return TRUE;
+
+    // Include all threads.
+  case IncludeThreadCallback:
+  case ThreadCallback:
+    return TRUE;
+
+    // Stop receiving cancel callbacks.
+  case CancelCallback:
+    callback_output->CheckCancel = FALSE;
+    callback_output->Cancel = FALSE;
+    return TRUE;
+  }
+  // Ignore other callback types.
+  return FALSE;
+}
+
+}  // namespace google_breakpad
+
--- toolkit/crashreporter/breakpad-client/windows/handler/exception_handler.h
+++ toolkit/crashreporter/breakpad-client/windows/handler/exception_handler.h
@@ -69,6 +69,7 @@
 #include <string>
 #include <vector>
 
+#include "windows/common/minidump_callback.h"
 #include "windows/common/ipc_protocol.h"
 #include "windows/crash_generation/crash_generation_client.h"
 #include "common/scoped_ptr.h"
@@ -78,22 +79,6 @@ namespace google_breakpad {
 
 using std::vector;
 using std::wstring;
-
-// These entries store a list of memory regions that the client wants included
-// in the minidump.
-struct AppMemory {
-  ULONG64 ptr;
-  ULONG length;
-
-  bool operator==(const struct AppMemory& other) const {
-    return ptr == other.ptr;
-  }
-
-  bool operator==(const void* other) const {
-    return ptr == reinterpret_cast<ULONG64>(other);
-  }
-};
-typedef std::list<AppMemory> AppMemoryList;
 
 class ExceptionHandler {
  public:
@@ -356,13 +341,6 @@ class ExceptionHandler {
   bool WriteMinidumpWithException(DWORD requesting_thread_id,
                                   EXCEPTION_POINTERS* exinfo,
                                   MDRawAssertionInfo* assertion);
-
-  // This function is used as a callback when calling MinidumpWriteDump,
-  // in order to add additional memory regions to the dump.
-  static BOOL CALLBACK MinidumpWriteDumpCallback(
-      PVOID context,
-      const PMINIDUMP_CALLBACK_INPUT callback_input,
-      PMINIDUMP_CALLBACK_OUTPUT callback_output);
 
   // This function does the actual writing of a minidump.  It is
   // called on the handler thread.  requesting_thread_id is the ID of
