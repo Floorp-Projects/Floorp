6892764cecb8389753ba07edf657894c523f5d6b
Aaron Klotz <aklotz@mozilla.com>
2017-06-20 12:32:21 -0600

    Bug 1374643: Use IID instead of REFIID for a11y::HandlerProvider::mTargetUnkIid; r=jimm
    MozReview-Commit-ID: IMFMxhHsbp6

--- accessible/ipc/win/HandlerProvider.h
+++ accessible/ipc/win/HandlerProvider.h
@@ -64,7 +64,7 @@ private:
 
   Atomic<uint32_t>                  mRefCnt;
   Mutex                             mMutex; // Protects mSerializer
-  REFIID                            mTargetUnkIid;
+  const IID                         mTargetUnkIid;
   mscom::InterceptorTargetPtr<IUnknown> mTargetUnk; // Constant, main thread only
   UniquePtr<mscom::StructToStream>  mSerializer;
 };
