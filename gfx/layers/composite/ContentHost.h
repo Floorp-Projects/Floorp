f56c8518c46bc68b314b6f77e37d294be1d228b4
David Anderson <danderson@mozilla.com>
2017-06-20 01:17:16 -0700

    Expose ContentTextureHost buffer properties. (bug 1365879 part 4, r=mattwoodrow)

--- gfx/layers/composite/ContentHost.h
+++ gfx/layers/composite/ContentHost.h
@@ -63,6 +63,11 @@ public:
   // tiles are fading in.
   virtual void AddAnimationInvalidation(nsIntRegion& aRegion) { }
 
+  virtual gfx::IntRect GetBufferRect() {
+    MOZ_ASSERT_UNREACHABLE("Must be implemented in derived class");
+    return gfx::IntRect();
+  }
+
 protected:
   explicit ContentHost(const TextureInfo& aTextureInfo)
     : CompositableHost(aTextureInfo)
@@ -92,13 +97,19 @@ public:
   explicit ContentHostBase(const TextureInfo& aTextureInfo);
   virtual ~ContentHostBase();
 
-protected:
+  virtual gfx::IntRect GetBufferRect() override { return mBufferRect; }
+
   virtual nsIntPoint GetOriginOffset()
   {
     return mBufferRect.TopLeft() - mBufferRotation;
   }
 
+  gfx::IntPoint GetBufferRotation()
+  {
+    return mBufferRotation.ToUnknownPoint();
+  }
 
+protected:
   gfx::IntRect mBufferRect;
   nsIntPoint mBufferRotation;
   bool mInitialised;
