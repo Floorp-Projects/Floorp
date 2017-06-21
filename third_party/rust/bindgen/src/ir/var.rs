3167c350362f427f06c5b0cc94781e7eacddf833
Servo VCS Sync <servo-vcs-sync@mozilla.com>
2017-06-21 02:15:52 +0000

    No bug - Revendor rust dependencies

--- third_party/rust/bindgen/src/ir/var.rs
+++ third_party/rust/bindgen/src/ir/var.rs
@@ -284,17 +284,35 @@ fn parse_macro(ctx: &BindgenContext,
                -> Option<(Vec<u8>, cexpr::expr::EvalResult)> {
     use cexpr::{expr, nom};
 
-    let cexpr_tokens = match unit.cexpr_tokens(cursor) {
+    let mut cexpr_tokens = match unit.cexpr_tokens(cursor) {
         None => return None,
         Some(tokens) => tokens,
     };
 
     let parser = expr::IdentifierParser::new(ctx.parsed_macros());
-    let result = parser.macro_definition(&cexpr_tokens);
 
-    match result {
-        nom::IResult::Done(_, (id, val)) => Some((id.into(), val)),
-        _ => None,
+    match parser.macro_definition(&cexpr_tokens) {
+        nom::IResult::Done(_, (id, val)) => {
+            return Some((id.into(), val));
+        }
+        _ => {}
+    }
+
+    // Try without the last token, to workaround a libclang bug in versions
+    // previous to 4.0.
+    //
+    // See:
+    //   https://bugs.llvm.org//show_bug.cgi?id=9069
+    //   https://reviews.llvm.org/D26446
+    if cexpr_tokens.pop().is_none() {
+        return None;
+    }
+
+    match parser.macro_definition(&cexpr_tokens) {
+        nom::IResult::Done(_, (id, val)) => {
+            Some((id.into(), val))
+        }
+        _ => None
     }
 }
 
