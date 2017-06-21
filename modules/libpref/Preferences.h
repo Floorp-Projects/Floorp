5cf8c34a9455c3cfbdd6fee12cf4e4a5ca13b80c
Benjamin Smedberg <benjamin@smedbergs.us>
2017-06-21 15:26:10 -0400

    Bug 1367813 - 1) Add telemetry for prefs.js not existing or being corrupted, and the presence of a user.js file. 2) Rename and change the nsIPrefService.readUserPrefs API. The new API reads user prefs from a file but doesn't remember that location or save changed preferences to that location. r=milan data-r=rweiss
    MozReview-Commit-ID: FD5npJlB24W
    
    --HG--
    extra : rebase_source : 049275836880f0aa4d19a9af2de9ec4101432b44

--- modules/libpref/Preferences.h
+++ modules/libpref/Preferences.h
@@ -442,9 +442,7 @@ protected:
    *         or the error code related to the read attempt.
    */
   nsresult UseDefaultPrefFile();
-  nsresult UseUserPrefFile();
-  nsresult ReadAndOwnUserPrefFile(nsIFile *aFile);
-  nsresult ReadAndOwnSharedUserPrefFile(nsIFile *aFile);
+  void UseUserPrefFile();
   nsresult MakeBackupPrefFile(nsIFile *aFile);
 
   // Default pref file save can be blocking or not.
