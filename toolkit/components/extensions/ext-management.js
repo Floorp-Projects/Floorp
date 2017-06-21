29414a264f2c6cd1f5fbe1373e8c7d577e250ddd
Bob Silverberg <bsilverberg@mozilla.com>
2017-06-21 14:29:37 -0400

    Bug 1374237 - Part 2: Declare all top-level functions in toolkit API files files via const., r=mixedpuppy
    Also changed some function names to attempt to guarantee uniqueness across API files.
    
    MozReview-Commit-ID: GnwWwTADF3a
    
    --HG--
    extra : rebase_source : 2d51f3de2958089de97ce98ed1f01c68ec0ed74a

--- toolkit/components/extensions/ext-management.js
+++ toolkit/components/extensions/ext-management.js
@@ -26,14 +26,14 @@ var {
   ExtensionError,
 } = ExtensionUtils;
 
-function _(key, ...args) {
+const _ = (key, ...args) => {
   if (args.length) {
     return strBundle.formatStringFromName(key, args, args.length);
   }
   return strBundle.GetStringFromName(key);
-}
+};
 
-function installType(addon) {
+const installType = addon => {
   if (addon.temporarilyInstalled) {
     return "development";
   } else if (addon.foreignInstall) {
@@ -42,9 +42,9 @@ function installType(addon) {
     return "other";
   }
   return "normal";
-}
+};
 
-function getExtensionInfoForAddon(extension, addon) {
+const getExtensionInfoForAddon = (extension, addon) => {
   let extInfo = {
     id: addon.id,
     name: addon.name,
@@ -85,7 +85,7 @@ function getExtensionInfoForAddon(extension, addon) {
     extInfo.updateUrl = addon.updateURL;
   }
   return extInfo;
-}
+};
 
 const listenerMap = new WeakMap();
 // Some management APIs are intentionally limited.
@@ -141,7 +141,7 @@ class AddonListener {
 
 let addonListener;
 
-function getListener(extension, context) {
+const getManagementListener = (extension, context) => {
   if (!listenerMap.has(extension)) {
     if (!addonListener) {
       addonListener = new AddonListener();
@@ -158,7 +158,7 @@ function getListener(extension, context) {
     });
   }
   return addonListener;
-}
+};
 
 this.management = class extends ExtensionAPI {
   getAPI(context) {
@@ -229,9 +229,9 @@ this.management = class extends ExtensionAPI {
             fire.async(data);
           };
 
-          getListener(extension, context).on("onDisabled", listener);
+          getManagementListener(extension, context).on("onDisabled", listener);
           return () => {
-            getListener(extension, context).off("onDisabled", listener);
+            getManagementListener(extension, context).off("onDisabled", listener);
           };
         }).api(),
 
@@ -240,9 +240,9 @@ this.management = class extends ExtensionAPI {
             fire.async(data);
           };
 
-          getListener(extension, context).on("onEnabled", listener);
+          getManagementListener(extension, context).on("onEnabled", listener);
           return () => {
-            getListener(extension, context).off("onEnabled", listener);
+            getManagementListener(extension, context).off("onEnabled", listener);
           };
         }).api(),
 
@@ -251,9 +251,9 @@ this.management = class extends ExtensionAPI {
             fire.async(data);
           };
 
-          getListener(extension, context).on("onInstalled", listener);
+          getManagementListener(extension, context).on("onInstalled", listener);
           return () => {
-            getListener(extension, context).off("onInstalled", listener);
+            getManagementListener(extension, context).off("onInstalled", listener);
           };
         }).api(),
 
@@ -262,9 +262,9 @@ this.management = class extends ExtensionAPI {
             fire.async(data);
           };
 
-          getListener(extension, context).on("onUninstalled", listener);
+          getManagementListener(extension, context).on("onUninstalled", listener);
           return () => {
-            getListener(extension, context).off("onUninstalled", listener);
+            getManagementListener(extension, context).off("onUninstalled", listener);
           };
         }).api(),
 
