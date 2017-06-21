e1b7db0651c36424a58ea833de40edca148e1024
Bill McCloskey <billm@mozilla.com>
2017-06-20 19:42:13 -0700

    Bug 1382922 - Refactor event queue to allow multiple implementations (r=erahm)
    This patch refactors the nsThread event queue to clean it up and to make it easier to restructure. The fundamental concepts are as follows:
    
    Each nsThread will have a pointer to a refcounted SynchronizedEventQueue. A SynchronizedEQ takes care of doing the locking and condition variable work when posting and popping events. For the actual storage of events, it delegates to an AbstractEventQueue data structure. It keeps a UniquePtr to the AbstractEventQueue that it uses for storage.
    
    Both SynchronizedEQ and AbstractEventQueue are abstract classes. There is only one concrete implementation of SynchronizedEQ in this patch, which is called ThreadEventQueue. ThreadEventQueue uses locks and condition variables to post and pop events the same way nsThread does. It also encapsulates the functionality that DOM workers need to implement their special event loops (PushEventQueue and PopEventQueue). In later Quantum DOM work, I plan to have another SynchronizedEQ implementation for the main thread, called SchedulerEventQueue. It will have special code for the cooperatively scheduling threads in Quantum DOM.
    
    There are two concrete implementations of AbstractEventQueue in this patch: EventQueue and PrioritizedEventQueue. EventQueue replaces the old nsEventQueue. The other AbstractEventQueue implementation is PrioritizedEventQueue, which uses multiple queues for different event priorities.
    
    The final major piece here is ThreadEventTarget, which splits some of the code for posting events out of nsThread. Eventually, my plan is for multiple cooperatively scheduled nsThreads to be able to share a ThreadEventTarget. In this patch, though, each nsThread has its own ThreadEventTarget. The class's purpose is just to collect some related code together.
    
    One final note: I tried to avoid virtual dispatch overhead as much as possible. Calls to SynchronizedEQ methods do use virtual dispatch, since I plan to use different implementations for different threads with Quantum DOM. But all the calls to EventQueue methods should be non-virtual. Although the methods are declared virtual, all the classes used are final and the concrete classes involved should all be known through templatization.
    
    MozReview-Commit-ID: 9Evtr9oIJvx

---
+++ xpcom/threads/PrioritizedEventQueue.cpp
@@ -0,0 +1,310 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "PrioritizedEventQueue.h"
+#include "mozilla/EventQueue.h"
+#include "nsThreadManager.h"
+#include "nsXPCOMPrivate.h" // for gXPCOMThreadsShutDown
+
+using namespace mozilla;
+
+template<class InnerQueueT>
+PrioritizedEventQueue<InnerQueueT>::PrioritizedEventQueue(UniquePtr<InnerQueueT> aHighQueue,
+                                                          UniquePtr<InnerQueueT> aInputQueue,
+                                                          UniquePtr<InnerQueueT> aNormalQueue,
+                                                          UniquePtr<InnerQueueT> aIdleQueue,
+                                                          already_AddRefed<nsIIdlePeriod> aIdlePeriod)
+  : mHighQueue(Move(aHighQueue))
+  , mInputQueue(Move(aInputQueue))
+  , mNormalQueue(Move(aNormalQueue))
+  , mIdleQueue(Move(aIdleQueue))
+  , mIdlePeriod(aIdlePeriod)
+{
+  static_assert(IsBaseOf<AbstractEventQueue, InnerQueueT>::value,
+                "InnerQueueT must be an AbstractEventQueue subclass");
+}
+
+template<class InnerQueueT>
+void
+PrioritizedEventQueue<InnerQueueT>::PutEvent(already_AddRefed<nsIRunnable>&& aEvent,
+                                             EventPriority aPriority,
+                                             const MutexAutoLock& aProofOfLock)
+{
+  // Double check the priority with a QI.
+  RefPtr<nsIRunnable> event(aEvent);
+  EventPriority priority = aPriority;
+  if (nsCOMPtr<nsIRunnablePriority> runnablePrio = do_QueryInterface(event)) {
+    uint32_t prio = nsIRunnablePriority::PRIORITY_NORMAL;
+    runnablePrio->GetPriority(&prio);
+    if (prio == nsIRunnablePriority::PRIORITY_HIGH) {
+      priority = EventPriority::High;
+    } else if (prio == nsIRunnablePriority::PRIORITY_INPUT) {
+      priority = EventPriority::Input;
+    }
+  }
+
+  if (priority == EventPriority::Input && !mWriteToInputQueue) {
+    priority = EventPriority::Normal;
+  }
+
+  switch (priority) {
+  case EventPriority::High:
+    mHighQueue->PutEvent(event.forget(), priority, aProofOfLock);
+    break;
+  case EventPriority::Input:
+    mInputQueue->PutEvent(event.forget(), priority, aProofOfLock);
+    break;
+  case EventPriority::Normal:
+    mNormalQueue->PutEvent(event.forget(), priority, aProofOfLock);
+    break;
+  case EventPriority::Idle:
+    mIdleQueue->PutEvent(event.forget(), priority, aProofOfLock);
+    break;
+  }
+}
+
+template<class InnerQueueT>
+TimeStamp
+PrioritizedEventQueue<InnerQueueT>::GetIdleDeadline()
+{
+  // If we are shutting down, we won't honor the idle period, and we will
+  // always process idle runnables.  This will ensure that the idle queue
+  // gets exhausted at shutdown time to prevent intermittently leaking
+  // some runnables inside that queue and even worse potentially leaving
+  // some important cleanup work unfinished.
+  if (gXPCOMThreadsShutDown || nsThreadManager::get().GetCurrentThread()->ShuttingDown()) {
+    return TimeStamp::Now();
+  }
+
+  TimeStamp idleDeadline;
+  {
+    // Releasing the lock temporarily since getting the idle period
+    // might need to lock the timer thread. Unlocking here might make
+    // us receive an event on the main queue, but we've committed to
+    // run an idle event anyhow.
+    MutexAutoUnlock unlock(*mMutex);
+    mIdlePeriod->GetIdlePeriodHint(&idleDeadline);
+  }
+
+  // If HasPendingEvents() has been called and it has returned true because of
+  // pending idle events, there is a risk that we may decide here that we aren't
+  // idle and return null, in which case HasPendingEvents() has effectively
+  // lied.  Since we can't go back and fix the past, we have to adjust what we
+  // do here and forcefully pick the idle queue task here.  Note that this means
+  // that we are choosing to run a task from the idle queue when we would
+  // normally decide that we aren't in an idle period, but this can only happen
+  // if we fall out of the idle period in between the call to HasPendingEvents()
+  // and here, which should hopefully be quite rare.  We are effectively
+  // choosing to prioritize the sanity of our API semantics over the optimal
+  // scheduling.
+  if (!mHasPendingEventsPromisedIdleEvent &&
+      (!idleDeadline || idleDeadline < TimeStamp::Now())) {
+    return TimeStamp();
+  }
+  if (mHasPendingEventsPromisedIdleEvent && !idleDeadline) {
+    // If HasPendingEvents() has been called and it has returned true, but we're no
+    // longer in the idle period, we must return a valid timestamp to pretend that
+    // we are still in the idle period.
+    return TimeStamp::Now();
+  }
+  return idleDeadline;
+}
+
+template<class InnerQueueT>
+already_AddRefed<nsIRunnable>
+PrioritizedEventQueue<InnerQueueT>::GetEvent(EventPriority* aPriority,
+                                             const MutexAutoLock& aProofOfLock)
+{
+  MakeScopeExit([&] {
+    mHasPendingEventsPromisedIdleEvent = false;
+  });
+
+#ifndef RELEASE_OR_BETA
+  // Clear mNextIdleDeadline so that it is possible to determine that
+  // we're running an idle runnable in ProcessNextEvent.
+  *mNextIdleDeadline = TimeStamp();
+#endif
+
+  bool highPending = mHighQueue->HasPendingEvent(aProofOfLock);
+  bool normalPending = mNormalQueue->HasPendingEvent(aProofOfLock);
+  size_t inputCount = mInputQueue->Count(aProofOfLock);
+
+  if (mReadFromInputQueue && mInputHandlingStartTime.IsNull() && inputCount > 0) {
+    mInputHandlingStartTime =
+      InputEventStatistics::Get()
+      .GetInputHandlingStartTime(inputCount);
+  }
+
+  // We check the different queues in the following order. The conditions we use
+  // are meant to avoid starvation and to ensure that we don't process an event
+  // at the wrong time.
+  //
+  // HIGH: if mProcessHighPriorityQueue
+  // INPUT: if inputCount > 0 && TimeStamp::Now() > mInputHandlingStartTime
+  // NORMAL: if normalPending
+  //
+  // If we still don't have an event, then we take events from the queues
+  // in the following order:
+  //
+  // HIGH
+  // INPUT
+  // IDLE: if GetIdleDeadline()
+  //
+  // If we don't get an event in this pass, then we return null since no events
+  // are ready.
+
+  // This variable determines which queue we will take an event from.
+  EventPriority queue;
+
+  if (mProcessHighPriorityQueue) {
+    queue = EventPriority::High;
+  } else if (inputCount > 0 && TimeStamp::Now() > mInputHandlingStartTime
+             && mReadFromInputQueue) {
+    queue = EventPriority::Input;
+  } else if (normalPending) {
+    queue = EventPriority::Normal;
+  } else if (highPending) {
+    queue = EventPriority::High;
+  } else if (inputCount > 0 && mReadFromInputQueue) {
+    queue = EventPriority::Input;
+  } else {
+    // We may not actually return an idle event in this case.
+    queue = EventPriority::Idle;
+  }
+
+  MOZ_ASSERT_IF(queue == EventPriority::Input, mReadFromInputQueue);
+
+  mProcessHighPriorityQueue = highPending;
+
+  if (aPriority) {
+    *aPriority = queue;
+  }
+
+  if (queue == EventPriority::High) {
+    nsCOMPtr<nsIRunnable> event = mHighQueue->GetEvent(aPriority, aProofOfLock);
+    MOZ_ASSERT(event);
+    mInputHandlingStartTime = TimeStamp();
+    mProcessHighPriorityQueue = false;
+    return event.forget();
+  }
+
+  if (queue == EventPriority::Input) {
+    nsCOMPtr<nsIRunnable> event = mInputQueue->GetEvent(aPriority, aProofOfLock);
+    MOZ_ASSERT(event);
+    return event.forget();
+  }
+
+  if (queue == EventPriority::Normal) {
+    nsCOMPtr<nsIRunnable> event = mNormalQueue->GetEvent(aPriority, aProofOfLock);
+    return event.forget();
+  }
+
+  // If we get here, then all queues except idle are empty.
+  MOZ_ASSERT(queue == EventPriority::Idle);
+
+  if (!mIdleQueue->HasPendingEvent(aProofOfLock)) {
+    MOZ_ASSERT(!mHasPendingEventsPromisedIdleEvent);
+    return nullptr;
+  }
+
+  TimeStamp idleDeadline = GetIdleDeadline();
+  if (!idleDeadline) {
+    return nullptr;
+  }
+
+  nsCOMPtr<nsIRunnable> event = mIdleQueue->GetEvent(aPriority, aProofOfLock);
+  if (event) {
+    nsCOMPtr<nsIIdleRunnable> idleEvent = do_QueryInterface(event);
+    if (idleEvent) {
+      idleEvent->SetDeadline(idleDeadline);
+    }
+
+#ifndef RELEASE_OR_BETA
+    // Store the next idle deadline to be able to determine budget use
+    // in ProcessNextEvent.
+    *mNextIdleDeadline = idleDeadline;
+#endif
+  }
+
+  return event.forget();
+}
+
+template<class InnerQueueT>
+bool
+PrioritizedEventQueue<InnerQueueT>::HasPendingEvent(const MutexAutoLock& aProofOfLock)
+{
+  mHasPendingEventsPromisedIdleEvent = false;
+
+  if (mHighQueue->HasPendingEvent(aProofOfLock) ||
+      mInputQueue->HasPendingEvent(aProofOfLock) ||
+      mNormalQueue->HasPendingEvent(aProofOfLock)) {
+    return true;
+  }
+
+  bool hasPendingIdleEvent = false;
+
+  // Note that GetIdleDeadline() checks mHasPendingEventsPromisedIdleEvent,
+  // but that's OK since we set it to false in the beginning of this method!
+  TimeStamp idleDeadline = GetIdleDeadline();
+
+  // Only examine the idle queue if we are in an idle period.
+  if (idleDeadline) {
+    hasPendingIdleEvent = mIdleQueue->HasPendingEvent(aProofOfLock);
+    mHasPendingEventsPromisedIdleEvent = hasPendingIdleEvent;
+  }
+
+  return hasPendingIdleEvent;
+}
+
+template<class InnerQueueT>
+size_t
+PrioritizedEventQueue<InnerQueueT>::Count(const MutexAutoLock& aProofOfLock) const
+{
+  MOZ_CRASH("unimplemented");
+}
+
+// This is used to flush pending events in nsChainedEventQueue::mNormalQueue
+// before starting event prioritization.
+template<class InnerQueueT>
+class PrioritizedEventQueue<InnerQueueT>::EnablePrioritizationRunnable final
+  : public mozilla::Runnable
+{
+public:
+  explicit EnablePrioritizationRunnable(PrioritizedEventQueue<InnerQueueT>* aQueue)
+    : Runnable("EnablePrioritizationRunnable")
+  {}
+
+  NS_IMETHOD Run() override
+  {
+    // Do don't need to do this with the lock held because mReadFromInputQueue
+    // is only read from the main thread.
+    MOZ_ASSERT(NS_IsMainThread());
+    MOZ_ASSERT(mQueue->mWriteToInputQueue);
+    MOZ_ASSERT(!mQueue->mReadFromInputQueue);
+    mQueue->mReadFromInputQueue = true;
+    return NS_OK;
+  }
+
+private:
+  PrioritizedEventQueue<InnerQueueT>* mQueue;
+};
+
+template<class InnerQueueT>
+void
+PrioritizedEventQueue<InnerQueueT>::EnableInputEventPrioritization(const MutexAutoLock& aProofOfLock)
+{
+  MOZ_ASSERT(!mWriteToInputQueue);
+  MOZ_ASSERT(!mReadFromInputQueue);
+  mWriteToInputQueue = true;
+  mInputHandlingStartTime = TimeStamp();
+
+  RefPtr<EnablePrioritizationRunnable> runnable = new EnablePrioritizationRunnable(this);
+  PutEvent(runnable.forget(), EventPriority::Normal, aProofOfLock);
+}
+
+namespace mozilla {
+template class PrioritizedEventQueue<EventQueue>;
+}
