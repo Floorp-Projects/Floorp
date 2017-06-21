95c57a8a15077d6be89f1896c847d0add1a9de3e
Nathan Froyd <froydnj@mozilla.com>
2017-06-21 16:36:03 -0400

    Bug 1310852 - part 4 - always download packages for stylo during `mach bootstrap`; r=rillian
    With configure now being able to auto-detect the presence of a `mach
    bootstrap`-installed clang and libclang and the upcoming
    build-by-defaultness of Stylo, we can start downloading these packages
    all the time.

--- python/mozboot/mozboot/windows.py
+++ python/mozboot/mozboot/windows.py
@@ -8,18 +8,6 @@ import subprocess
 
 from mozboot.base import BaseBootstrapper
 
-STYLO_MOZCONFIG = '''
-To enable Stylo in your builds, paste the lines between the chevrons
-(>>> and <<<) into your mozconfig file:
-
-<<<
-ac_add_options --enable-stylo
-
-ac_add_options --with-libclang-path="{state_dir}/clang/bin"
-ac_add_options --with-clang-path="{state_dir}/clang/bin/clang.exe"
->>>
-'''
-
 
 class WindowsBootstrapper(BaseBootstrapper):
     '''Bootstrapper for msys2 based environments for building in Windows.'''
@@ -79,10 +67,6 @@ class WindowsBootstrapper(BaseBootstrapper):
 
     def install_mobile_android_artifact_mode_packages(self):
         raise NotImplementedError('We do not support building Android on Windows. Sorry!')
-
-    def suggest_browser_mozconfig(self):
-        if self.stylo:
-            print(STYLO_MOZCONFIG.format(state_dir=self.state_dir))
 
     def ensure_stylo_packages(self, state_dir, checkout_root):
         import stylo
