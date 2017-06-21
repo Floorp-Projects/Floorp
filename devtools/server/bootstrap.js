8a25ab04768f61f08807cd9f75355bcde07d5744
Julian Descottes <jdescottes@mozilla.com>
2017-06-21 19:23:11 +0200

    Bug 1369801 - dt-addon-fennec: package specific version of devtools addon for fennec;r=jryans
    MozReview-Commit-ID: 50jh5spnHw6
    
    --HG--
    extra : rebase_source : 8b0b8f2f936a6f83166690cd15df642c61114a7e

---
+++ devtools/server/bootstrap.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* exported startup, shutdown, install, uninstall */
+
+// Fake bootstrap used when shipping only server part of devtools.
+
+"use strict";
+
+function startup(data) {
+}
+
+function shutdown(data, reason) {
+}
+
+function install() {}
+function uninstall() {}
