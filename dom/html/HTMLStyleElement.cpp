deae2413d62c84c484494d2f6e6625e960b1906d
Cameron McCormack <cam@mcc.id.au>
2017-06-21 09:25:43 +0800

    Bug 1291515 - Part 1: Introduce a pref for <style scoped>. r=bholley,dbaron
    MozReview-Commit-ID: 1J9IvPrC0xh
    
    --HG--
    extra : rebase_source : 2c3a139cc5e4e223e5b9b7892020c96df2cf264c

--- dom/html/HTMLStyleElement.cpp
+++ dom/html/HTMLStyleElement.cpp
@@ -179,7 +179,8 @@ HTMLStyleElement::AfterSetAttr(int32_t aNameSpaceID, nsIAtom* aName,
         aName == nsGkAtoms::media ||
         aName == nsGkAtoms::type) {
       UpdateStyleSheetInternal(nullptr, nullptr, true);
-    } else if (aName == nsGkAtoms::scoped) {
+    } else if (aName == nsGkAtoms::scoped &&
+               nsContentUtils::IsScopedStyleEnabled(OwnerDoc())) {
       bool isScoped = aValue;
       UpdateStyleSheetScopedness(isScoped);
     }
@@ -242,7 +243,8 @@ HTMLStyleElement::GetStyleSheetInfo(nsAString& aTitle,
 
   GetAttr(kNameSpaceID_None, nsGkAtoms::type, aType);
 
-  *aIsScoped = HasAttr(kNameSpaceID_None, nsGkAtoms::scoped);
+  *aIsScoped = HasAttr(kNameSpaceID_None, nsGkAtoms::scoped) &&
+               nsContentUtils::IsScopedStyleEnabled(OwnerDoc());
 
   nsAutoString mimeType;
   nsAutoString notUsed;
