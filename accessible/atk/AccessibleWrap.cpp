a31096518e4974aa854f03d8dca9a27d33c0f33b
Joanmarie Diggs <jdiggs@igalia.com>
2017-06-22 07:25:00 -0400

    Bug 1375460 - "haspopup" AtkObject attribute missing if multiprocess enabled. r=surkov
    Move the object attribute exposure of states::HASPOPUP from the ATK wrapper
    code to the shared accessibility code. Call UniversalStatesFor() rather than
    State() for performance.

--- accessible/atk/AccessibleWrap.cpp
+++ accessible/atk/AccessibleWrap.cpp
@@ -754,17 +754,8 @@ AtkAttributeSet*
 GetAttributeSet(Accessible* aAccessible)
 {
   nsCOMPtr<nsIPersistentProperties> attributes = aAccessible->Attributes();
-  if (attributes) {
-    // There is no ATK state for haspopup, must use object attribute to expose
-    // the same info.
-    if (aAccessible->State() & states::HASPOPUP) {
-      nsAutoString unused;
-      attributes->SetStringProperty(NS_LITERAL_CSTRING("haspopup"),
-                                    NS_LITERAL_STRING("true"), unused);
-    }
-
+  if (attributes)
     return ConvertToAtkAttributeSet(attributes);
-  }
 
   return nullptr;
 }
