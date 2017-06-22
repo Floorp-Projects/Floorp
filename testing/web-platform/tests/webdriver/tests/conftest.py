07f670d5b821fcedde70cd4331c4ee94c2675d0f
Maja Frydrychowicz <mjzffr@gmail.com>
2017-06-22 16:45:57 -0400

    Bug 1353895 - Move all wdspec tests into a package; r=jgraham
    We need a top-level package for tests and support files so that
    support modules are importable from subpackages. For example,
    previously webdriver/support/inline.py could not be imported from
    webdriver/actions/mouse.py.
    
    I'm creating the package in a subdirectory under webdriver in
    order to avoid a name-clash with webdriver, the WebDriver client
    library. Part of the motivation for adding the subdirectory rather
    than renaming the client library is that it's a relatively
    isolated change.
    
    MozReview-Commit-ID: BihAKcNLbXK
    
    --HG--
    rename : testing/web-platform/meta/webdriver/contexts.py.ini => testing/web-platform/meta/webdriver/tests/contexts.py.ini
    rename : testing/web-platform/meta/webdriver/navigation.py.ini => testing/web-platform/meta/webdriver/tests/navigation.py.ini
    rename : testing/web-platform/tests/webdriver/actions/__init__.py => testing/web-platform/tests/webdriver/tests/actions/__init__.py
    rename : testing/web-platform/tests/webdriver/actions/conftest.py => testing/web-platform/tests/webdriver/tests/actions/conftest.py
    rename : testing/web-platform/tests/webdriver/actions/key.py => testing/web-platform/tests/webdriver/tests/actions/key.py
    rename : testing/web-platform/tests/webdriver/actions/mouse.py => testing/web-platform/tests/webdriver/tests/actions/mouse.py
    rename : testing/web-platform/tests/webdriver/actions/sequence.py => testing/web-platform/tests/webdriver/tests/actions/sequence.py
    rename : testing/web-platform/tests/webdriver/actions/special_keys.py => testing/web-platform/tests/webdriver/tests/actions/special_keys.py
    rename : testing/web-platform/tests/webdriver/actions/support/__init__.py => testing/web-platform/tests/webdriver/tests/actions/support/__init__.py
    rename : testing/web-platform/tests/webdriver/actions/support/keys.py => testing/web-platform/tests/webdriver/tests/actions/support/keys.py
    rename : testing/web-platform/tests/webdriver/actions/support/refine.py => testing/web-platform/tests/webdriver/tests/actions/support/refine.py
    rename : testing/web-platform/tests/webdriver/actions/support/test_actions_wdspec.html => testing/web-platform/tests/webdriver/tests/actions/support/test_actions_wdspec.html
    rename : testing/web-platform/tests/webdriver/conftest.py => testing/web-platform/tests/webdriver/tests/conftest.py
    rename : testing/web-platform/tests/webdriver/contexts.py => testing/web-platform/tests/webdriver/tests/contexts.py
    rename : testing/web-platform/tests/webdriver/cookies.py => testing/web-platform/tests/webdriver/tests/cookies.py
    rename : testing/web-platform/tests/webdriver/navigation.py => testing/web-platform/tests/webdriver/tests/navigation.py
    rename : testing/web-platform/tests/webdriver/support/__init__.py => testing/web-platform/tests/webdriver/tests/support/__init__.py
    rename : testing/web-platform/tests/webdriver/support/asserts.py => testing/web-platform/tests/webdriver/tests/support/asserts.py
    rename : testing/web-platform/tests/webdriver/support/fixtures.py => testing/web-platform/tests/webdriver/tests/support/fixtures.py
    rename : testing/web-platform/tests/webdriver/support/http_request.py => testing/web-platform/tests/webdriver/tests/support/http_request.py
    rename : testing/web-platform/tests/webdriver/support/inline.py => testing/web-platform/tests/webdriver/tests/support/inline.py
    rename : testing/web-platform/tests/webdriver/support/merge_dictionaries.py => testing/web-platform/tests/webdriver/tests/support/merge_dictionaries.py
    rename : testing/web-platform/tests/webdriver/window_maximizing.py => testing/web-platform/tests/webdriver/tests/window_maximizing.py
    extra : rebase_source : cb2aed22a39fc5b720c70e4b9864ebe20ee8ce40

---
+++ testing/web-platform/tests/webdriver/tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from tests.support.fixtures import (
+    configuration, create_frame, create_window, http, new_session, server_config, session,
+    url)
+
+pytest.fixture(scope="session")(configuration)
+pytest.fixture()(create_frame)
+pytest.fixture()(create_window)
+pytest.fixture()(http)
+pytest.fixture(scope="function")(new_session)
+pytest.fixture()(server_config)
+pytest.fixture(scope="function")(session)
+pytest.fixture()(url)
