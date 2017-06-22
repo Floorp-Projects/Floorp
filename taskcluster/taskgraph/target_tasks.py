08523d0221355bcc57cc0457897e7f0b9a5682f9
Chris AtLee <catlee@mozilla.com>
2017-06-22 12:48:17 -0400

    Bug 1371038: Remove unused stylo tasks target r=Callek
    MozReview-Commit-ID: BXxHTeOyksO

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -291,18 +291,6 @@ def target_tasks_pine(full_task_graph, parameters):
     return [l for l, t in full_task_graph.tasks.iteritems() if filter(t)]
 
 
-@_target_task('stylo_tasks')
-def target_tasks_stylo(full_task_graph, parameters):
-    """Target stylotasks that only run on the m-c branch."""
-    def filter(task):
-        platform = task.attributes.get('build_platform')
-        # only select platforms
-        if platform not in ('linux64-stylo'):
-            return False
-        return True
-    return [l for l, t in full_task_graph.tasks.iteritems() if filter(t)]
-
-
 # nightly_linux should be refactored to be nightly_all once
 # https://bugzilla.mozilla.org/show_bug.cgi?id=1267425 dependent bugs are
 # implemented
