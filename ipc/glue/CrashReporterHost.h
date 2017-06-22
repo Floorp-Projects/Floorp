b23dbffaf23a3b47f09901498261461a4b17c9f2
Cervantes Yu <cyu@mozilla.com>
2017-06-22 18:52:58 +0800

    Bug 1360308 - Part 2: Use callback in CrashReporterHost::GenerateMinidumpAndPair() and up the caller chain. r=gsvelto
    This changes CrashReporterHost::GenerateMinidumpAndPair() and up the caller chain to use callbacks so we
    may call it synchronously or asynchronously.
    
    MozReview-Commit-ID: 4PQH6cVdOk0

--- ipc/glue/CrashReporterHost.h
+++ ipc/glue/CrashReporterHost.h
@@ -7,6 +7,8 @@
 #ifndef mozilla_ipc_CrashReporterHost_h
 #define mozilla_ipc_CrashReporterHost_h
 
+#include <functional>
+
 #include "mozilla/UniquePtr.h"
 #include "mozilla/ipc/Shmem.h"
 #include "base/process.h"
@@ -36,6 +38,61 @@ class CrashReporterHost
 #endif
 
 public:
+
+  template <typename T>
+  class CallbackWrapper {
+  public:
+    void Init(std::function<void(T)>&& aCallback, bool aAsync)
+    {
+      mCallback = Move(aCallback);
+      mAsync = aAsync;
+      if (IsAsync()) {
+        // Don't call do_GetCurrentThread() is this is called synchronously
+        // because 1. it's unnecessary, and 2. more importantly, it might create
+        // one if called from a native thread, and the thread will be leaked.
+        mTargetThread = do_GetCurrentThread();
+      }
+    }
+
+    bool IsEmpty()
+    {
+      return !mCallback;
+    }
+
+    bool IsAsync()
+    {
+      return mAsync;
+    }
+
+    void Invoke(T aResult)
+    {
+      if (IsAsync()) {
+        decltype(mCallback) callback = Move(mCallback);
+        mTargetThread->
+          Dispatch(NS_NewRunnableFunction([callback, aResult](){
+                     callback(aResult);
+                   }), NS_DISPATCH_NORMAL);
+      } else {
+        MOZ_ASSERT(!mTargetThread);
+        mCallback(aResult);
+      }
+
+      Clear();
+    }
+
+  private:
+    void Clear()
+    {
+      mCallback = nullptr;
+      mTargetThread = nullptr;
+      mAsync = false;
+    }
+
+    bool mAsync;
+    std::function<void(T)> mCallback;
+    nsCOMPtr<nsIThread> mTargetThread;
+  };
+
   CrashReporterHost(GeckoProcessType aProcessType,
                     const Shmem& aShmem,
                     ThreadId aThreadId);
@@ -61,12 +118,15 @@ public:
 
   // Generate a paired minidump. This does not take the crash report, as
   // GenerateCrashReport does. After this, FinalizeCrashReport may be called.
-  //
-  // This calls TakeCrashedChildMinidump and FinalizeCrashReport.
-  bool
+  // Minidump(s) can be generated synchronously or asynchronously, specified in
+  // argument aAsync. When the operation completes, aCallback is invoked, where
+  // the callback argument denotes whether the operation succeeded.
+  void
   GenerateMinidumpAndPair(GeckoChildProcessHost* aChildProcess,
                           nsIFile* aMinidumpToPair,
-                          const nsACString& aPairName);
+                          const nsACString& aPairName,
+                          std::function<void(bool)>&& aCallback,
+                          bool aAsync);
 
   // This is a static helper function to notify the crash service that a
   // crash has occurred. When PCrashReporter is removed, we can make this
@@ -94,6 +154,7 @@ private:
                             const nsString& aChildDumpID);
 
 private:
+  CallbackWrapper<bool> mCreateMinidumpCallback;
   GeckoProcessType mProcessType;
   Shmem mShmem;
   ThreadId mThreadId;
