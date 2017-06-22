f85d81d30e621b22e1518b6819b9f4aea3156212
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-06-22 16:37:11 -0400

    Bug 1372263 - Add additional mochitest selftests for crashes, assertions and leaks, r=jmaher
    This also starts running the selftests on linux debug builds, since that's the only place that we
    can test assertions and leaks.
    
    MozReview-Commit-ID: JTdTLOLWn5r
    
    --HG--
    extra : rebase_source : 340aca0c4e5f9697b1d652fd192332e47a1acab9
    extra : histedit_source : 2d4b542d2122b4c6d2d48fc9c49848d5453e4533

--- testing/mochitest/tests/python/test_basic_mochitest_plain.py
+++ testing/mochitest/tests/python/test_basic_mochitest_plain.py
@@ -11,11 +11,13 @@ import pytest
 from conftest import build, filter_action
 
 sys.path.insert(0, os.path.join(build.topsrcdir, 'testing', 'mozharness'))
-from mozharness.base.log import INFO, WARNING
+from mozharness.base.log import INFO, WARNING, ERROR
 from mozharness.base.errors import BaseErrorList
-from mozharness.mozilla.buildbot import TBPL_SUCCESS, TBPL_WARNING
+from mozharness.mozilla.buildbot import TBPL_SUCCESS, TBPL_WARNING, TBPL_FAILURE
 from mozharness.mozilla.structuredlog import StructuredOutputParser
 from mozharness.mozilla.testing.errors import HarnessErrorList
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_mozharness_status(lines, status):
@@ -26,8 +28,15 @@ def get_mozharness_status(lines, status):
         suite_category='mochitest',
     )
 
-    for line in lines:
-        parser.parse_single_line(json.dumps(line))
+    # Processing the log with mozharness will re-print all the output to stdout
+    # Since this exact same output has already been printed by the actual test
+    # run, temporarily redirect stdout to devnull.
+    with open(os.devnull, 'w') as fh:
+        orig = sys.stdout
+        sys.stdout = fh
+        for line in lines:
+            parser.parse_single_line(json.dumps(line))
+        sys.stdout = orig
     return parser.evaluate_parser(status)
 
 
@@ -37,7 +46,7 @@ def test_output_pass(runtests):
 
     tbpl_status, log_level = get_mozharness_status(lines, status)
     assert tbpl_status == TBPL_SUCCESS
-    assert log_level == WARNING
+    assert log_level in (INFO, WARNING)
 
     lines = filter_action('test_status', lines)
     assert len(lines) == 1
@@ -67,6 +76,71 @@ def test_output_fail(runtests):
     if build_obj:
         assert set(lines[0].keys()) == set(lines[1].keys())
         assert set(lines[0].values()) == set(lines[1].values())
+
+
+def test_output_crash(runtests):
+    status, lines = runtests('test_crash.html', environment=["MOZ_CRASHREPORTER_SHUTDOWN=1"])
+    assert status == 1
+
+    tbpl_status, log_level = get_mozharness_status(lines, status)
+    assert tbpl_status == TBPL_FAILURE
+    assert log_level == ERROR
+
+    crash = filter_action('crash', lines)
+    assert len(crash) == 1
+    assert crash[0]['action'] == 'crash'
+    assert crash[0]['signature']
+    assert crash[0]['minidump_path']
+
+    lines = filter_action('test_end', lines)
+    assert len(lines) == 0
+
+
+@pytest.mark.skip_mozinfo("!debug")
+def test_output_assertion(runtests):
+    status, lines = runtests('test_assertion.html')
+    # TODO: mochitest should return non-zero here
+    assert status == 0
+
+    tbpl_status, log_level = get_mozharness_status(lines, status)
+    assert tbpl_status == TBPL_WARNING
+    assert log_level == WARNING
+
+    test_end = filter_action('test_end', lines)
+    assert len(test_end) == 1
+    # TODO: this should be ASSERT, but moving the assertion check before
+    # the test_end action caused a bunch of failures.
+    assert test_end[0]['status'] == 'OK'
+
+    assertions = filter_action('assertion_count', lines)
+    assert len(assertions) == 1
+    assert assertions[0]['count'] == 1
+
+
+@pytest.mark.skip_mozinfo("!debug")
+def test_output_leak(monkeypatch, runtests):
+    # Monkeypatch mozleak so we always process a failing leak log
+    # instead of the actual one.
+    import mozleak
+    old_process_leak_log = mozleak.process_leak_log
+
+    def process_leak_log(*args, **kwargs):
+        return old_process_leak_log(os.path.join(here, 'files', 'leaks.log'), *args[1:], **kwargs)
+
+    monkeypatch.setattr('mozleak.process_leak_log', process_leak_log)
+
+    status, lines = runtests('test_pass.html')
+    # TODO: mochitest should return non-zero here
+    assert status == 0
+
+    tbpl_status, log_level = get_mozharness_status(lines, status)
+    assert tbpl_status == TBPL_FAILURE
+    assert log_level == ERROR
+
+    errors = filter_action('log', lines)
+    errors = [e for e in errors if e['level'] == 'ERROR']
+    assert len(errors) == 1
+    assert 'leakcheck' in errors[0]['message']
 
 
 if __name__ == '__main__':
