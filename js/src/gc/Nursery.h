6f38c23f69d00c79034001f6f60ef4c7a17c7f21
Nicholas Nethercote <nnethercote@mozilla.com>
2017-06-23 16:24:25 +1000

    Bug 1375776 (part 6) - Remove Nursery::trackTimings_. r=sfink.
    If these timings are on for all MOZ_GECKO_PROFILER platforms (i.e. all tier 1
    platforms) then we might as well simplify things by having them on for all
    platforms.

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -276,7 +276,6 @@ class Nursery
     void clearMinorGCRequest() { minorGCTriggerReason_ = JS::gcreason::NO_REASON; }
 
     bool enableProfiling() const { return enableProfiling_; }
-    bool trackTimings() const { return trackTimings_; }
 
   private:
     /* The amount of space in the mapped nursery available to allocations. */
@@ -322,12 +321,6 @@ class Nursery
     /* Report minor collections taking at least this long, if enabled. */
     mozilla::TimeDuration profileThreshold_;
     bool enableProfiling_;
-
-    /*
-     * Track timings if either enableProfiling_ or the Gecko profiler is
-     * compiled in.
-     */
-    bool trackTimings_;
 
     /* Report ObjectGroups with at lest this many instances tenured. */
     int64_t reportTenurings_;
@@ -476,8 +469,6 @@ class Nursery
     void maybeClearProfileDurations();
     void startProfile(ProfileKey key);
     void endProfile(ProfileKey key);
-    void maybeStartProfile(ProfileKey key);
-    void maybeEndProfile(ProfileKey key);
     static void printProfileDurations(const ProfileDurations& times);
 
     friend class TenuringTracer;
