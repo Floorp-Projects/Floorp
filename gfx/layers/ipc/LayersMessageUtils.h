52d5447f23b3ddaa765d42806f26cb5843cd8e8d
David Anderson <danderson@mozilla.com>
2017-06-22 21:12:20 -0700

    Add Advanced Layers to the compositor line in about:support. (bug 1365879 part 22, r=milan)
    --HG--
    extra : rebase_source : 123d4190c0343b60491f34757b26532b9def5075

--- gfx/layers/ipc/LayersMessageUtils.h
+++ gfx/layers/ipc/LayersMessageUtils.h
@@ -304,6 +304,7 @@ struct ParamTraits<mozilla::layers::TextureFactoryIdentifier>
     WriteParam(aMsg, aParam.mSupportsPartialUploads);
     WriteParam(aMsg, aParam.mSupportsComponentAlpha);
     WriteParam(aMsg, aParam.mSupportsBackdropCopyForComponentAlpha);
+    WriteParam(aMsg, aParam.mUsingAdvancedLayers);
     WriteParam(aMsg, aParam.mSyncHandle);
   }
 
@@ -317,6 +318,7 @@ struct ParamTraits<mozilla::layers::TextureFactoryIdentifier>
                   ReadParam(aMsg, aIter, &aResult->mSupportsPartialUploads) &&
                   ReadParam(aMsg, aIter, &aResult->mSupportsComponentAlpha) &&
                   ReadParam(aMsg, aIter, &aResult->mSupportsBackdropCopyForComponentAlpha) &&
+                  ReadParam(aMsg, aIter, &aResult->mUsingAdvancedLayers) &&
                   ReadParam(aMsg, aIter, &aResult->mSyncHandle);
     return result;
   }
