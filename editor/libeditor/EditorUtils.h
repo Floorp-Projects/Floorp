d58979382584ed2a66dce7126a6de41ecf0bc55f
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-06-23 12:28:59 +0200

    Backed out changeset 094e8f3937e8 (bug 1375131) for bustage

--- editor/libeditor/EditorUtils.h
+++ editor/libeditor/EditorUtils.h
@@ -7,7 +7,6 @@
 #ifndef mozilla_EditorUtils_h
 #define mozilla_EditorUtils_h
 
-#include "mozilla/dom/Selection.h"
 #include "mozilla/EditorBase.h"
 #include "mozilla/GuardObjects.h"
 #include "nsCOMPtr.h"
@@ -26,6 +25,10 @@ class nsRange;
 
 namespace mozilla {
 template <class T> class OwningNonNull;
+
+namespace dom {
+class Selection;
+} // namespace dom
 
 /***************************************************************************
  * EditActionResult is useful to return multiple results of an editor
@@ -317,23 +320,6 @@ public:
 protected:
   EditorBase* mEditorBase;
   MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
-};
-
-class MOZ_STACK_CLASS AutoRangeArray final
-{
-public:
-  AutoRangeArray(dom::Selection* aSelection)
-  {
-    if (!aSelection) {
-      return;
-    }
-    uint32_t rangeCount = aSelection->RangeCount();
-    for (uint32_t i = 0; i < rangeCount; i++) {
-      mRanges.AppendElement(*aSelection->GetRangeAt(i));
-    }
-  }
-
-  AutoTArray<mozilla::OwningNonNull<nsRange>, 8> mRanges;
 };
 
 /******************************************************************************
