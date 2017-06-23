381991e7524cd70f33fbbf8b1fdacca836b0feeb
Michael Layzell <michael@thelayzells.com>
2017-06-23 15:41:04 -0400

    Bug 1375940 - Part 2: Fix an assertion in PresShell::HandleEventWithTarget, r=smaug
    MozReview-Commit-ID: 4hRjlgbLDA8

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -7949,7 +7949,8 @@ PresShell::HandleEventWithTarget(WidgetEvent* aEvent, nsIFrame* aFrame,
   if (aContent) {
     nsIDocument* doc = aContent->GetComposedDoc();
     NS_ASSERTION(doc, "event for content that isn't in a document");
-    NS_ASSERTION(!doc || doc->GetShell() == this, "wrong shell");
+    // NOTE: We don't require that the document still have a PresShell.
+    // See bug 1375940.
   }
 #endif
   NS_ENSURE_STATE(!aContent || aContent->GetComposedDoc() == mDocument);
