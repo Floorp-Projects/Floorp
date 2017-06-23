ca73b5de993653531c4d9be6482553402ec7bd6f
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-23 18:55:38 +0200

    Backed out changeset 909ccc166a06 (bug 1374587) on suspicion of causing timeouts in browser_grids_number-of-css-grids-telemetry.js. r=backout

--- devtools/client/inspector/shared/utils.js
+++ devtools/client/inspector/shared/utils.js
@@ -100,18 +100,17 @@ function advanceValidate(keyCode, value, insertionPoint) {
 exports.advanceValidate = advanceValidate;
 
 /**
- * Create a debouncing function wrapper to only call the target function after a certain
- * amount of time has passed without it being called.
+ * Create a throttling function wrapper to regulate its frequency.
  *
  * @param {Function} func
- *         The function to debounce
+ *         The function to throttle
  * @param {number} wait
- *         The wait period
+ *         The throttling period
  * @param {Object} scope
  *         The scope to use for func
- * @return {Function} The debounced function
+ * @return {Function} The throttled function
  */
-function debounce(func, wait, scope) {
+function throttle(func, wait, scope) {
   let timer = null;
 
   return function () {
@@ -124,55 +123,6 @@ function debounce(func, wait, scope) {
       timer = null;
       func.apply(scope, args);
     }, wait);
-  };
-}
-
-exports.debounce = debounce;
-
-/**
- * From underscore's `_.throttle`
- * http://underscorejs.org
- * (c) 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
- * Underscore may be freely distributed under the MIT license.
- *
- * Returns a function, that, when invoked, will only be triggered at most once during a
- * given window of time. The throttled function will run as much as it can, without ever
- * going more than once per wait duration.
- *
- * @param  {Function} func
- *         The function to throttle
- * @param  {number} wait
- *         The wait period
- * @param  {Object} scope
- *         The scope to use for func
- * @return {Function} The throttled function
- */
-function throttle(func, wait, scope) {
-  let args, result;
-  let timeout = null;
-  let previous = 0;
-
-  let later = function () {
-    previous = Date.now();
-    timeout = null;
-    result = func.apply(scope, args);
-    args = null;
-  };
-
-  return function () {
-    let now = Date.now();
-    let remaining = wait - (now - previous);
-    args = arguments;
-    if (remaining <= 0) {
-      clearTimeout(timeout);
-      timeout = null;
-      previous = now;
-      result = func.apply(scope, args);
-      args = null;
-    } else if (!timeout) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
   };
 }
 
