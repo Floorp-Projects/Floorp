75f8c3dcd1a1593b16dae3ffbbbeaad195c7213d
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-06-23 17:05:37 +0900

    Bug 1375767 - Don't use nsCSSPseudoElements::GetPseudoType() r=birtles
    It iterates over all pseudo types if a given element is not pseudo.
    We should use IsGeneratedContentContainerForBefore() or
    IsGeneratedContentContainerForAfter instead.
    
    MozReview-Commit-ID: 51Jnl4e8M2V
    
    --HG--
    extra : rebase_source : aca066a84871cc18003d0325b94354c66cc1da91

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -525,6 +525,21 @@ PseudoTagAndCorrectElementForAnimation(const Element*& aElementOrPseudo) {
   return nullptr;
 }
 
+static CSSPseudoElementType
+GetPseudoTypeFromElementForAnimation(const Element*& aElementOrPseudo) {
+  if (aElementOrPseudo->IsGeneratedContentContainerForBefore()) {
+    aElementOrPseudo = aElementOrPseudo->GetParent()->AsElement();
+    return CSSPseudoElementType::before;
+  }
+
+  if (aElementOrPseudo->IsGeneratedContentContainerForAfter()) {
+    aElementOrPseudo = aElementOrPseudo->GetParent()->AsElement();
+    return CSSPseudoElementType::after;
+  }
+
+  return CSSPseudoElementType::NotPseudo;
+}
+
 bool
 Gecko_GetAnimationRule(RawGeckoElementBorrowed aElement,
                        EffectCompositor::CascadeLevel aCascadeLevel,
@@ -542,12 +557,8 @@ Gecko_GetAnimationRule(RawGeckoElementBorrowed aElement,
     return false;
   }
 
-  nsIAtom* pseudoTag = PseudoTagAndCorrectElementForAnimation(aElement);
-
   CSSPseudoElementType pseudoType =
-    nsCSSPseudoElements::GetPseudoType(
-      pseudoTag,
-      nsCSSProps::EnabledState::eIgnoreEnabledState);
+    GetPseudoTypeFromElementForAnimation(aElement);
 
   return presContext->EffectCompositor()
     ->GetServoAnimationRule(aElement,
@@ -581,10 +592,8 @@ Gecko_UpdateAnimations(RawGeckoElementBorrowed aElement,
     return;
   }
 
-  nsIAtom* pseudoTag = PseudoTagAndCorrectElementForAnimation(aElement);
   CSSPseudoElementType pseudoType =
-    nsCSSPseudoElements::GetPseudoType(pseudoTag,
-                                       CSSEnabledState::eForAllContent);
+    GetPseudoTypeFromElementForAnimation(aElement);
 
   if (aTasks & UpdateAnimationsTasks::CSSAnimations) {
     presContext->AnimationManager()->
@@ -629,10 +638,8 @@ Gecko_UpdateAnimations(RawGeckoElementBorrowed aElement,
 bool
 Gecko_ElementHasAnimations(RawGeckoElementBorrowed aElement)
 {
-  nsIAtom* pseudoTag = PseudoTagAndCorrectElementForAnimation(aElement);
   CSSPseudoElementType pseudoType =
-    nsCSSPseudoElements::GetPseudoType(pseudoTag,
-                                       CSSEnabledState::eForAllContent);
+    GetPseudoTypeFromElementForAnimation(aElement);
 
   return !!EffectSet::GetEffectSet(aElement, pseudoType);
 }
