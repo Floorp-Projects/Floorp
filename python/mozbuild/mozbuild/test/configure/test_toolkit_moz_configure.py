c1aaffdfff066bebffe4302208f72967245b1491
Mike Hommey <mh+mozilla@glandium.org>
2017-06-22 22:05:31 -0700

    Bug 1354581 - Add unit tests for DEVELOPER_OPTIONS. r=chmanchester
    MozReview-Commit-ID: JXNICUdzWzD

--- python/mozbuild/mozbuild/test/configure/test_toolkit_moz_configure.py
+++ python/mozbuild/mozbuild/test/configure/test_toolkit_moz_configure.py
@@ -9,6 +9,7 @@ import os
 from buildconfig import topsrcdir
 from common import BaseConfigureTest
 from mozunit import main
+from mozbuild.configure.options import InvalidOptionError
 
 
 class TestToolkitMozConfigure(BaseConfigureTest):
@@ -61,6 +62,25 @@ class TestToolkitMozConfigure(BaseConfigureTest):
         self.assertEqual(
             get_value('--enable-necko-protocols=default,-http,-about'),
             tuple(p for p in default_protocols if p not in ('http', 'about')))
+
+    def test_developer_options(self):
+        def get_value(args=[], environ={}):
+            sandbox = self.get_sandbox({}, {}, args, environ)
+            return sandbox._value_for(sandbox['developer_options'])
+
+        self.assertEqual(get_value(), True)
+
+        self.assertEqual(get_value(['--enable-release']), None)
+
+        self.assertEqual(get_value(environ={'MOZILLA_OFFICIAL': 1}), None)
+
+        self.assertEqual(get_value(['--enable-release'],
+                         environ={'MOZILLA_OFFICIAL': 1}), None)
+
+        with self.assertRaises(InvalidOptionError):
+            get_value(['--disable-release'], environ={'MOZILLA_OFFICIAL': 1})
+
+        self.assertEqual(get_value(environ={'MOZ_AUTOMATION': 1}), None)
 
 
 if __name__ == '__main__':
