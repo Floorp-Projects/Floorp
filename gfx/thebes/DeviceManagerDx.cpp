000ab020c20125de0e81928e37187e0e42115d40
David Anderson <danderson@mozilla.com>
2017-06-23 14:23:13 -0700

    Add Telemetry environment and failure tracking for Advanced Layers. (bug 1365879 part 23, r=milan, data_r=bsmedberg)

--- gfx/thebes/DeviceManagerDx.cpp
+++ gfx/thebes/DeviceManagerDx.cpp
@@ -19,6 +19,7 @@
 #include "mozilla/layers/DeviceAttachmentsD3D11.h"
 #include "mozilla/layers/MLGDeviceD3D11.h"
 #include "nsIGfxInfo.h"
+#include "nsString.h"
 #include <d3d11.h>
 #include <ddraw.h>
 
@@ -651,6 +652,16 @@ DisableAdvancedLayers(FeatureStatus aStatus, const nsCString aMessage, const nsC
   FeatureFailure info(aStatus, aMessage, aFailureId);
   if (GPUParent* gpu = GPUParent::GetSingleton()) {
     gpu->SendUpdateFeature(Feature::ADVANCED_LAYERS, info);
+  }
+
+  if (aFailureId.Length()) {
+    nsString failureId = NS_ConvertUTF8toUTF16(aFailureId.get());
+    Telemetry::ScalarAdd(Telemetry::ScalarID::GFX_ADVANCED_LAYERS_FAILURE_ID, failureId, 1);
+  }
+
+  // Notify TelemetryEnvironment.jsm.
+  if (RefPtr<nsIObserverService> obs = mozilla::services::GetObserverService()) {
+    obs->NotifyObservers(nullptr, "gfx-features-ready", nullptr);
   }
 }
 
