85f218d2dc9c0b8933ef3a2ac05f9654c7d273ae
Haik Aftandilian <haftandilian@mozilla.com>
2017-06-23 17:10:54 -0700

    Bug 1334550 - Part 1 - Proxy moz-extension protocol requests to the parent process; r=jimm,mayhemer
    Changes ExtensionProtocolHandler to use remote streams for
    moz-extension loads of file and JAR URI's to allow for
    filesystem read-access sandboxing.
    
    Adds messaging to PNecko to allow child processes to
    request an input stream or file descriptor for moz-extension
    URI's.
    
    Add ExtensionProtocolHandler singleton so that NeckoParent can
    call methods directly and ExtensionProtocolHandler::NewFD
    can use a new member variable |mFileOpenerThread| to open files.
    
    Adds FileDescriptorFile, a limited implementation of nsIFile
    that wraps a file descriptor, to be sideloaded into
    nsJARChannels so that extension JAR files can be read
    using a file descriptor without accessing the filesystem
    directly.
    
    MozReview-Commit-ID: 1pcnIpjz2yR

--- modules/libjar/nsJARChannel.h
+++ modules/libjar/nsJARChannel.h
@@ -51,6 +51,8 @@ public:
 
     nsresult Init(nsIURI *uri);
 
+    void SetFile(nsIFile *file);
+
 private:
     virtual ~nsJARChannel();
 
@@ -98,6 +100,7 @@ private:
     // to the request if we get called back via RetargetDeliveryTo.
     nsCOMPtr<nsIRequest>            mRequest;
     nsCOMPtr<nsIFile>               mJarFile;
+    nsCOMPtr<nsIFile>               mJarFileOverride;
     nsCOMPtr<nsIURI>                mJarBaseURI;
     nsCString                       mJarEntry;
     nsCString                       mInnerJarEntry;
