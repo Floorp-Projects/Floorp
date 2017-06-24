3c6347dab6c35f089bfbbda75359f4f392c607b7
Andreas Tolfsen <ato@sny.no>
2017-06-24 19:01:04 +0100

    Bug 1376128 - Add eslint rules for testing/marionette; r=automatedtester,standard8
    The eslint rules are based on the default, non-opinionated eslint rules
    used throughout mozilla-central.  The testing/marionette eslint rules
    are a specialisation of these based on the current coding style used.
    These should not be contradictory to the overall JS code style that can
    be inferred from the existing code base.
    
    MozReview-Commit-ID: 2rKabgXXnu4

--- testing/marionette/marionette.eslintrc.js
+++
@@ -1,8 +0,0 @@
-// Parent config file for all marionette files.
-module.exports = {
-  // All globals made available in the test environment.
-  "globals": {
-    "ok": false,
-    "is": false,
-  }
-};
