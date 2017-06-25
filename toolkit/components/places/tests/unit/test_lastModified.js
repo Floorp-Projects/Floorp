62aed0ebbc7824d66ebce1578af1a3acf4a1fcd5
Mark Banner <standard8@mozilla.com>
2017-06-25 20:17:12 +0100

    Bug 1376232 - When updating date added the async Bookmarks.update API should also change the last modified date. r=mak
    MozReview-Commit-ID: BGiT1w8BVlq

--- toolkit/components/places/tests/unit/test_lastModified.js
+++ toolkit/components/places/tests/unit/test_lastModified.js
@@ -4,31 +4,73 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+function assert_date_eq(a, b) {
+  if (typeof(a) != "number") {
+    a = PlacesUtils.toPRTime(a);
+  }
+  if (typeof(b) != "number") {
+    b = PlacesUtils.toPRTime(b);
+  }
+  Assert.equal(a, b, "The dates should match");
+}
+
  /**
   * Test that inserting a new bookmark will set lastModified to the same
   * values as dateAdded.
   */
-// main
-function run_test() {
-  var bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
-           getService(Ci.nsINavBookmarksService);
-  var itemId = bs.insertBookmark(bs.bookmarksMenuFolder,
-                                 uri("http://www.mozilla.org/"),
-                                 bs.DEFAULT_INDEX,
-                                 "itemTitle");
-  var dateAdded = bs.getItemDateAdded(itemId);
-  do_check_eq(dateAdded, bs.getItemLastModified(itemId));
+add_task(async function test_bookmarkLastModified() {
+  let bookmark = await PlacesUtils.bookmarks.insert({
+    parentGuid: PlacesUtils.bookmarks.menuGuid,
+    url: "http://www.mozilla.org/",
+    title: "itemTitle"
+  });
+
+  let guid = bookmark.guid;
+
+  // Check the bookmark from the database.
+  bookmark = await PlacesUtils.bookmarks.fetch(guid);
+
+  let dateAdded = PlacesUtils.toPRTime(bookmark.dateAdded);
+  assert_date_eq(dateAdded, bookmark.lastModified);
 
   // Change lastModified, then change dateAdded.  LastModified should be set
   // to the new dateAdded.
   // This could randomly fail on virtual machines due to timing issues, so
   // we manually increase the time value.  See bug 500640 for details.
-  bs.setItemLastModified(itemId, dateAdded + 1000);
-  do_check_true(bs.getItemLastModified(itemId) === dateAdded + 1000);
-  do_check_true(bs.getItemDateAdded(itemId) < bs.getItemLastModified(itemId));
-  bs.setItemDateAdded(itemId, dateAdded + 2000);
-  do_check_true(bs.getItemDateAdded(itemId) === dateAdded + 2000);
-  do_check_eq(bs.getItemDateAdded(itemId), bs.getItemLastModified(itemId));
+  await PlacesUtils.bookmarks.update({
+    guid,
+    lastModified: PlacesUtils.toDate(dateAdded + 1000)
+  });
 
-  bs.removeItem(itemId);
-}
+  bookmark = await PlacesUtils.bookmarks.fetch(guid);
+
+  assert_date_eq(bookmark.lastModified, dateAdded + 1000);
+  Assert.ok(bookmark.dateAdded < bookmark.lastModified,
+    "Date added should be earlier than last modified.");
+
+  await PlacesUtils.bookmarks.update({
+    guid,
+    dateAdded: PlacesUtils.toDate(dateAdded + 2000)
+  });
+
+  bookmark = await PlacesUtils.bookmarks.fetch(guid);
+
+  assert_date_eq(bookmark.dateAdded, dateAdded + 2000);
+  assert_date_eq(bookmark.dateAdded, bookmark.lastModified);
+
+  // If dateAdded is set to older than lastModified, then we shouldn't
+  // update lastModified to keep sync happy.
+  let origLastModified = bookmark.lastModified;
+
+  await PlacesUtils.bookmarks.update({
+    guid,
+    dateAdded: PlacesUtils.toDate(dateAdded - 10000)
+  });
+
+  bookmark = await PlacesUtils.bookmarks.fetch(guid);
+
+  assert_date_eq(bookmark.dateAdded, dateAdded - 10000);
+  assert_date_eq(bookmark.lastModified, origLastModified);
+
+  await PlacesUtils.bookmarks.remove(guid);
+});
