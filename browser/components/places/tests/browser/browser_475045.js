ba4d4411e06a7af62a9765b6257f07f42c943920
Mark Banner <standard8@mozilla.com>
2017-06-27 15:08:31 -0700

    Bug 1119282 - Update most browser/components/places browser mochitests to pass with async Places transations and fix some async transaction issues. r=mak
    MozReview-Commit-ID: Lc5oWCx1onJ

--- browser/components/places/tests/browser/browser_475045.js
+++ browser/components/places/tests/browser/browser_475045.js
@@ -34,24 +34,27 @@ add_task(async function test() {
    * @param aMimeType
    *        The mime type to use for the drop operation.
    */
-  let simulateDragDrop = function(aEffect, aMimeType) {
-    const uriSpec = "http://www.mozilla.org/D1995729-A152-4e30-8329-469B01F30AA7";
-    let uri = makeURI(uriSpec);
+  let simulateDragDrop = async function(aEffect, aMimeType) {
+    const url = "http://www.mozilla.org/D1995729-A152-4e30-8329-469B01F30AA7";
+    let promiseItemAddedNotification = promiseBookmarksNotification(
+      "onItemAdded", (itemId, parentId, index, type, uri, guid) => uri.spec == url);
+
     EventUtils.synthesizeDrop(placesItems.childNodes[0],
                               placesItems,
                               [[{type: aMimeType,
-                                data: uriSpec}]],
+                                data: url}]],
                               aEffect, window);
 
-    // Verify that the drop produces exactly one bookmark.
-    let bookmarkIds = PlacesUtils.bookmarks
-                      .getBookmarkIdsForURI(uri);
-    ok(bookmarkIds.length == 1, "There should be exactly one bookmark");
+    await promiseItemAddedNotification;
 
-    PlacesUtils.bookmarks.removeItem(bookmarkIds[0]);
+    // Verify that the drop produces exactly one bookmark.
+    let bookmark = await PlacesUtils.bookmarks.fetch({url});
+    Assert.ok(bookmark, "There should be exactly one bookmark");
+
+    await PlacesUtils.bookmarks.remove(bookmark.guid);
 
     // Verify that we removed the bookmark successfully.
-    ok(!PlacesUtils.bookmarks.isBookmarked(uri), "URI should be removed");
+    Assert.equal(await PlacesUtils.bookmarks.fetch({url}), null, "URI should be removed");
   };
 
   /**
@@ -62,44 +65,48 @@ add_task(async function test() {
    * @param aMimeType
    *        The mime type to use for the drop operation.
    */
-  let simulateDragDropMultiple = function(aEffect, aMimeType) {
-    const uriSpecs = [
+  let simulateDragDropMultiple = async function(aEffect, aMimeType) {
+    const urls = [
       "http://www.mozilla.org/C54263C6-A484-46CF-8E2B-FE131586348A",
       "http://www.mozilla.org/71381257-61E6-4376-AF7C-BF3C5FD8870D",
       "http://www.mozilla.org/091A88BD-5743-4C16-A005-3D2EA3A3B71E"
     ];
-    let uris = uriSpecs.map(spec => makeURI(spec));
     let data;
     if (aMimeType == "text/x-moz-url")
-      data = uriSpecs.map(spec => spec + "\n" + spec).join("\n");
+      data = urls.map(spec => spec + "\n" + spec).join("\n");
     else
-      data = uriSpecs.join("\n");
+      data = urls.join("\n");
+
+    let promiseItemAddedNotification = promiseBookmarksNotification(
+      "onItemAdded", (itemId, parentId, index, type, uri, guid) => uri.spec == urls[2]);
+
     EventUtils.synthesizeDrop(placesItems.childNodes[0],
                               placesItems,
                               [[{type: aMimeType,
                                  data}]],
                               aEffect, window);
 
-    // Verify that the drop produces exactly one bookmark per each URL.
-    for (let uri of uris) {
-      let bookmarkIds = PlacesUtils.bookmarks
-        .getBookmarkIdsForURI(uri);
-      ok(bookmarkIds.length == 1, "There should be exactly one bookmark");
+    await promiseItemAddedNotification;
 
-      PlacesUtils.bookmarks.removeItem(bookmarkIds[0]);
+    // Verify that the drop produces exactly one bookmark per each URL.
+    for (let url of urls) {
+      let bookmark = await PlacesUtils.bookmarks.fetch({url});
+      Assert.equal(typeof(bookmark), "object", "There should be exactly one bookmark");
+
+      await PlacesUtils.bookmarks.remove(bookmark.guid);
 
       // Verify that we removed the bookmark successfully.
-      ok(!PlacesUtils.bookmarks.isBookmarked(uri), "URI should be removed");
+      Assert.equal(await PlacesUtils.bookmarks.fetch({url}), null, "URI should be removed");
     }
   };
 
   // Simulate a bookmark drop for all of the mime types and effects.
   let mimeTypes = ["text/plain", "text/unicode", "text/x-moz-url"];
   let effects = ["move", "copy", "link"];
-  effects.forEach(function(effect) {
-    mimeTypes.forEach(function(mimeType) {
-      simulateDragDrop(effect, mimeType);
-      simulateDragDropMultiple(effect, mimeType);
-    });
-  });
+  for (let effect of effects) {
+    for (let mimeType of mimeTypes) {
+      await simulateDragDrop(effect, mimeType);
+      await simulateDragDropMultiple(effect, mimeType);
+    }
+  }
 });
