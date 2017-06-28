6db1a43ab34fe4b406bccc24cde95483edd90f22
Paolo Amadini <paolo.mozmail@amadzone.org>
2017-06-29 00:13:06 +0100

    Bug 1374242 - Remove obsolete whitelisting of unhandled rejections. r=Mossop
    The whitelisting function thisTestLeaksUncaughtRejectionsAndShouldBeFixed was replaced by expectUncaughtRejection, and existing calls did not take effect anymore.
    
    MozReview-Commit-ID: 3uOxkgWYWEz

--- devtools/client/webaudioeditor/test/browser_wa_first-run.js
+++ devtools/client/webaudioeditor/test/browser_wa_first-run.js
@@ -1,12 +1,6 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-//
-// Whitelisting this test.
-// As part of bug 1077403, the leaking uncaught rejection should be fixed.
-//
-thisTestLeaksUncaughtRejectionsAndShouldBeFixed("Error: Connection closed");
-
 /**
  * Tests that the reloading/onContentLoaded hooks work.
  */
--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -1046,9 +1046,6 @@ function testScope(aTester, aTest, expected) {
     self.SimpleTest.ignoreAllUncaughtExceptions(aIgnoring);
   };
 
-  this.thisTestLeaksUncaughtRejectionsAndShouldBeFixed = function(...rejections) {
-  };
-
   this.expectAssertions = function test_expectAssertions(aMin, aMax) {
     let min = aMin;
     let max = aMax;
