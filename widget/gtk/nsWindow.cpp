4da7fd48e31b23b7d1d5c4d7e8ec59d64198ced0
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-28 02:24:15 +0200

    Backed out changeset e744a3029de2 (bug 1375940) for failing dom/events/test/test_dblclick_kill_window.html. r=backout

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -6625,10 +6625,7 @@ nsWindow::SynthesizeNativeMouseEvent(LayoutDeviceIntPoint aPoint,
   // explicitly *before* requesting a button-press/release. You will also need to wait
   // for the motion event to be dispatched before requesting a button-press/release
   // event to maintain the desired event order.
-  if (aNativeMessage == GDK_BUTTON_PRESS ||
-      aNativeMessage == GDK_BUTTON_RELEASE ||
-      aNativeMessage == GDK_2BUTTON_PRESS ||
-      aNativeMessage == GDK_3BUTTON_PRESS) {
+  if (aNativeMessage == GDK_BUTTON_PRESS || aNativeMessage == GDK_BUTTON_RELEASE) {
     GdkEvent event;
     memset(&event, 0, sizeof(GdkEvent));
     event.type = (GdkEventType)aNativeMessage;
