c0ccbb5f83369d4cb7a16e9232a676f1ec22a318
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-28 02:58:17 +0200

    Backed out changeset 0b20a0fd86ff (bug 1308876)

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -4716,7 +4716,6 @@ nsBlockFrame::PlaceLine(BlockReflowInput& aState,
     aState.mReflowStatus.SetInlineLineBreakBeforeAndReset();
     // Reflow the line again when we reflow at our new position.
     aLine->MarkDirty();
-    *aKeepReflowGoing = false;
     return true;
   }
 
@@ -4728,7 +4727,6 @@ nsBlockFrame::PlaceLine(BlockReflowInput& aState,
     if (ShouldAvoidBreakInside(aState.mReflowInput)) {
       // All our content doesn't fit, start on the next page.
       aState.mReflowStatus.SetInlineLineBreakBeforeAndReset();
-      *aKeepReflowGoing = false;
     } else {
       // Push aLine and all of its children and anything else that
       // follows to our next-in-flow.
@@ -4737,10 +4735,6 @@ nsBlockFrame::PlaceLine(BlockReflowInput& aState,
     return true;
   }
 
-  // Note that any early return before this update of aState.mBCoord
-  // must either (a) return false or (b) set aKeepReflowGoing to false.
-  // Otherwise we'll keep reflowing later lines at an incorrect
-  // position, and we might not come back and clean up the damage later.
   aState.mBCoord = newBCoord;
 
   // Add the already placed current-line floats to the line
