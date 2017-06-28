584008b54231f7a3eb18b1c0f76912f283104a0c
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-28 03:29:13 +0200

    Backed out changeset f031600d142d (bug 1367696)

--- browser/extensions/formautofill/test/unit/head.js
+++ browser/extensions/formautofill/test/unit/head.js
@@ -53,7 +53,7 @@ extensionDir.append(EXTENSION_ID);
 // If the unpacked extension doesn't exist, use the packed version.
 if (!extensionDir.exists()) {
   extensionDir = extensionDir.parent;
-  extensionDir.leafName += ".xpi";
+  extensionDir.append(EXTENSION_ID + ".xpi");
 }
 Components.manager.addBootstrappedManifestLocation(extensionDir);
 
