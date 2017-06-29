8642712c2ae9ed4d4f813f8e646e7766e70dcaab
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-06-30 01:01:13 +0200

    Backed out changeset 26fd6d0a1254 (bug 1369577) for failing xpcshell's test_ext_simple.js and test_ext_startup_cache.js on Android. r=backout

--- toolkit/components/extensions/ext-webRequest.js
+++ toolkit/components/extensions/ext-webRequest.js
@@ -11,7 +11,7 @@ XPCOMUtils.defineLazyModuleGetter(this, "WebRequest",
                                   "resource://gre/modules/WebRequest.jsm");
 
 // EventManager-like class specifically for WebRequest. Inherits from
-// EventManager. Takes care of converting |details| parameter
+// SingletonEventManager. Takes care of converting |details| parameter
 // when invoking listeners.
 function WebRequestEventManager(context, eventName) {
   let name = `webRequest.${eventName}`;
@@ -118,10 +118,10 @@ function WebRequestEventManager(context, eventName) {
     };
   };
 
-  return EventManager.call(this, context, name, register);
+  return SingletonEventManager.call(this, context, name, register);
 }
 
-WebRequestEventManager.prototype = Object.create(EventManager.prototype);
+WebRequestEventManager.prototype = Object.create(SingletonEventManager.prototype);
 
 this.webRequest = class extends ExtensionAPI {
   getAPI(context) {
