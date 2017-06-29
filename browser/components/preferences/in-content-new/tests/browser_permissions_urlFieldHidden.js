ba99d3ff1f8b297d8dbe6c4223959f3ee31ee72d
Prathiksha <prathikshaprasadsuman@gmail.com>
2017-06-29 23:49:35 +0530

    Bug 1373206 - Create a new dialog for notification settings under Firefox Preferences to match the new spec. r=johannh
    MozReview-Commit-ID: 5gk4mr3D0jT
    
    --HG--
    extra : rebase_source : 5caf97ce85ad22a92dc8405757731275375165b4

--- browser/components/preferences/in-content-new/tests/browser_permissions_urlFieldHidden.js
+++ browser/components/preferences/in-content-new/tests/browser_permissions_urlFieldHidden.js
@@ -24,22 +24,3 @@ add_task(async function urlFieldVisibleForPopupPermissions(finish) {
   popupPolicyCheckbox.click();
   gBrowser.removeCurrentTab();
 });
-
-add_task(async function urlFieldHiddenForNotificationPermissions() {
-  await openPreferencesViaOpenPreferencesAPI("panePrivacy", {leaveOpen: true});
-  let win = gBrowser.selectedBrowser.contentWindow;
-  let doc = win.document;
-  let notificationsPolicyButton = doc.getElementById("notificationsPolicyButton");
-  ok(notificationsPolicyButton, "notificationsPolicyButton found");
-  let dialogPromise = promiseLoadSubDialog(PERMISSIONS_URL);
-  notificationsPolicyButton.click();
-  let dialog = await dialogPromise;
-  ok(dialog, "dialog loaded");
-
-  let urlLabel = dialog.document.getElementById("urlLabel");
-  ok(urlLabel.hidden, "urlLabel should be hidden as requested");
-  let url = dialog.document.getElementById("url");
-  ok(url.hidden, "url should be hidden as requested");
-
-  gBrowser.removeCurrentTab();
-});
