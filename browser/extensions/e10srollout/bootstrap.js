852a1870b516e141093fe2a7d7b5def7eda2e142
Blake Kaplan <mrbkap@gmail.com>
2017-06-28 17:10:47 -0700

    Bug 1377001 - Tweak e10srollout for Beta 55 (see bug for details). r=Felipe
    MozReview-Commit-ID: 7vyaBNkFdc5

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -22,8 +22,10 @@ const TEST_THRESHOLD = {
 // If a user qualifies for the e10s-multi experiement, this is how many
 // content processes to use and whether to allow addons for the experiment.
 const MULTI_EXPERIMENT = {
-  "beta": { buckets: { 1: .5, 4: 1, }, // 1 process: 50%, 4 processes: 50%
-            addonsDisableExperiment: false },
+  "beta": { buckets: { 4: 1, }, // 4 processes: 100%
+
+            // See below for an explanation, this only allows webextensions.
+            get addonsDisableExperiment() { return getAddonsDisqualifyForMulti(); } },
 
   "release": { buckets: { 1: .2, 4: 1 }, // 1 process: 20%, 4 processes: 80%
 
