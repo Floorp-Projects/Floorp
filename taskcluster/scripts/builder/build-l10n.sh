6ce904f19dca734894c51a2fe4a3a5a8993e7925
Wes Kocher <wkocher@mozilla.com>
2017-06-30 14:44:28 -0700

    Backed out changeset 485d1af7805a (bug 1338651) for valgrind issues a=backout

--- taskcluster/scripts/builder/build-l10n.sh
+++ taskcluster/scripts/builder/build-l10n.sh
@@ -4,7 +4,7 @@ set -x -e
 
 echo "running as" $(id)
 
-. /builds/worker/scripts/xvfb.sh
+. /home/worker/scripts/xvfb.sh
 
 ####
 # Taskcluster friendly wrapper for performing fx desktop l10n repacks via mozharness.
@@ -18,11 +18,11 @@ echo "running as" $(id)
 : MOZHARNESS_ACTIONS            ${MOZHARNESS_ACTIONS}
 : MOZHARNESS_OPTIONS            ${MOZHARNESS_OPTIONS}
 
-: TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/builds/worker/tooltool-cache}
+: TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/home/worker/tooltool-cache}
 
 : NEED_XVFB                     ${NEED_XVFB:=false}
 
-: WORKSPACE                     ${WORKSPACE:=/builds/worker/workspace}
+: WORKSPACE                     ${WORKSPACE:=/home/worker/workspace}
 
 set -v
 
@@ -86,7 +86,7 @@ if [ -n "$MOZHARNESS_OPTIONS" ]; then
     done
 fi
 
-cd /builds/worker
+cd /home/worker
 
 python2.7 $WORKSPACE/build/src/testing/${MOZHARNESS_SCRIPT} \
   --disable-mock \
