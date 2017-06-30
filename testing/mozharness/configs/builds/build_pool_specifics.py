62d8e4f4fa32a79d60dd369eed428c302bd492e9
Wes Kocher <wkocher@mozilla.com>
2017-06-30 14:45:26 -0700

    Backed out changeset f8a62747c51c (bug 1338651) for valgrind issues a=backout

--- testing/mozharness/configs/builds/build_pool_specifics.py
+++ testing/mozharness/configs/builds/build_pool_specifics.py
@@ -38,7 +38,7 @@ config = {
         "tooltool_url": 'http://relengapi/tooltool/',
         'upload_env': {
             'UPLOAD_HOST': 'localhost',
-            'UPLOAD_PATH': '/builds/worker/artifacts',
+            'UPLOAD_PATH': '/home/worker/artifacts',
         },
     },
 }
