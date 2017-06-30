bfa0ef5f9afd220d6260ec6e7e3b9e36495f8e2f
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-06-30 09:41:23 +0200

    Backed out changeset feedcde68c2a (bug 1338651) for bustage

--- taskcluster/scripts/misc/tooltool-download.sh
+++ taskcluster/scripts/misc/tooltool-download.sh
@@ -2,7 +2,7 @@
 
 cd $HOME/workspace/build/src
 
-: TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/builds/worker/tooltool-cache}
+: TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/home/worker/tooltool-cache}
 export TOOLTOOL_CACHE
 
 ./mach artifact toolchain -v --tooltool-url=http://relengapi/tooltool/ --tooltool-manifest "${TOOLTOOL_MANIFEST}"${TOOLTOOL_CACHE:+ --cache-dir ${TOOLTOOL_CACHE}}
