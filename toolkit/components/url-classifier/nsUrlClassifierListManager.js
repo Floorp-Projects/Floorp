62625fef2803a18cbc87af2135a11036fa894546
Thomas Nguyen <tnguyen@mozilla.com>
2017-06-30 13:31:03 -0700

    Bug 1297614 - Refactor and remove dead code in toolkit/components/url-classifier/content r=francois
    MozReview-Commit-ID: GzVGS1ZTRGL
    
    --HG--
    extra : rebase_source : ee868b5fb590f4965b17535383327df98504e963

--- toolkit/components/url-classifier/nsUrlClassifierListManager.js
+++ toolkit/components/url-classifier/nsUrlClassifierListManager.js
@@ -14,15 +14,7 @@ function Init() {
   // Pull the library in.
   var jslib = Cc["@mozilla.org/url-classifier/jslib;1"]
               .getService().wrappedJSObject;
-  modScope.G_Preferences = jslib.G_Preferences;
-  modScope.G_PreferenceObserver = jslib.G_PreferenceObserver;
-  modScope.G_ObserverServiceObserver = jslib.G_ObserverServiceObserver;
-  modScope.G_Debug = jslib.G_Debug;
-  modScope.G_Assert = jslib.G_Assert;
-  modScope.G_debugService = jslib.G_debugService;
-  modScope.G_Alarm = jslib.G_Alarm;
   modScope.BindToObject = jslib.BindToObject;
-  modScope.PROT_XMLFetcher = jslib.PROT_XMLFetcher;
   modScope.RequestBackoffV4 = jslib.RequestBackoffV4;
 
   // We only need to call Init once.
