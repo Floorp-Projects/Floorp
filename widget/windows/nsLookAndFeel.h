949cb19d0bd3327a98e4792de42fd1ab90e60da8
Mats Palmgren <mats@mozilla.com>
2017-06-30 03:34:15 +0200

    Bug 1377348 - Make nsLookAndFeel final to allow the compiler to devirtualize some calls.  r=tn
    MozReview-Commit-ID: 7AoZKsTyb3O

--- widget/windows/nsLookAndFeel.h
+++ widget/windows/nsLookAndFeel.h
@@ -32,7 +32,8 @@
 #define SM_SYSTEMDOCKED         0x00002004
 #endif
 
-class nsLookAndFeel: public nsXPLookAndFeel {
+class nsLookAndFeel final : public nsXPLookAndFeel
+{
   static OperatingSystemVersion GetOperatingSystemVersion();
 public:
   nsLookAndFeel();
