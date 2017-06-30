cecd8dd2889686bd1915c2b24909c1b8e2850c46
Paul Ellenbogen <pe5@cs.princeton.edu>
2017-06-30 12:54:12 -0700

    Bug 1379265 - Add C++ bindings for rsdparsa; r=bwc
    MozReview-Commit-ID: FdhpTT5wzwI
    
    --HG--
    extra : rebase_source : e2c0c58dfe41e25df101ec52bca0b092bc330246

---
+++ media/webrtc/signaling/src/sdp/RsdparsaSdpGlue.h
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef _RUSTSDPGLUE_H_
+#define _RUSTSDPGLUE_H_
+
+#include <string>
+#include <vector>
+
+#include "signaling/src/sdp/Sdp.h"
+#include "signaling/src/sdp/RsdparsaSdpInc.h"
+
+namespace mozilla
+{
+
+struct FreeRustSdpSession {
+  void operator()(RustSdpSession* aSess) { sdp_free_session(aSess); }
+};
+
+typedef UniquePtr<RustSdpSession, FreeRustSdpSession> RsdparsaSessionHandle;
+
+std::string convertStringView(StringView str);
+std::vector<std::string> convertStringVec(StringVec* vec);
+sdp::AddrType convertAddressType(RustSdpAddrType addr);
+
+}
+
+
+#endif
