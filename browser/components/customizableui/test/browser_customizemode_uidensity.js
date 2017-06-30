d63352edc40a2f0273cff0c25cf573da3ec897ee
Johann Hofmann <jhofmann@mozilla.com>
2017-06-29 22:41:03 -0700

    Bug 1350210 - Part 3 - Add uidensity icons and only show uidensity settings in Photon. r=nhnt11
    MozReview-Commit-ID: 6uI2oeQk8hw
    
    --HG--
    extra : rebase_source : e8aa4141f294433ab6f070f3d0e4fe4c42e073ea

--- browser/components/customizableui/test/browser_customizemode_uidensity.js
+++ browser/components/customizableui/test/browser_customizemode_uidensity.js
@@ -116,10 +116,20 @@ async function testModeButton(mode, modePref) {
 }
 
 add_task(async function test_compact_mode_button() {
+  if (!AppConstants.MOZ_PHOTON_THEME) {
+    ok(true, "Skipping test because Photon is not enabled.");
+    return;
+  }
+
   await testModeButton("compact", window.gUIDensity.MODE_COMPACT);
 });
 
 add_task(async function test_touch_mode_button() {
+  if (!AppConstants.MOZ_PHOTON_THEME) {
+    ok(true, "Skipping test because Photon is not enabled.");
+    return;
+  }
+
   // OSX doesn't get touch mode for now.
   if (AppConstants.platform == "macosx") {
     is(document.getElementById("customization-uidensity-menu-button-touch"), null,
