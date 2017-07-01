c086ae32f6365441dee868915e38ef27eaa17ffc
Mats Palmgren <mats@mozilla.com>
2017-07-01 19:59:21 +0200

    Bug 1377486 - Make nsWindow 'final' to possibly devirtualize some calls.  r=tn
    MozReview-Commit-ID: GrcIukJYmt6

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -68,7 +68,7 @@ class TimeStamp;
 class CurrentX11TimeGetter;
 }
 
-class nsWindow : public nsBaseWidget
+class nsWindow final : public nsBaseWidget
 {
 public:
     typedef mozilla::gfx::DrawTarget DrawTarget;
@@ -565,12 +565,6 @@ private:
     RefPtr<mozilla::widget::IMContextWrapper> mIMContext;
 
     mozilla::UniquePtr<mozilla::CurrentX11TimeGetter> mCurrentTimeGetter;
-};
-
-class nsChildWindow : public nsWindow {
-public:
-    nsChildWindow();
-    ~nsChildWindow();
 };
 
 #endif /* __nsWindow_h__ */
