31eb2b6b5acaef7ffb315dea76673f982f7b295d
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-07-03 11:50:33 +0200

    Bug 1229277 - Introduce a map to match JSScript to script filenames instead of relying on ScriptSourceObject. r=nbp,jonco

--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -2966,7 +2966,6 @@ GenerateLcovInfo(JSContext* cx, JSCompartment* comp, GenericPrinter& out)
     coverage::LCovCompartment compCover;
     for (JSScript* topLevel: topScripts) {
         RootedScript topScript(cx, topLevel);
-        compCover.collectSourceFile(comp, &topScript->scriptSourceUnwrap());
 
         // We found the top-level script, visit all the functions reachable
         // from the top-level function, and delazify them.
@@ -2978,7 +2977,10 @@ GenerateLcovInfo(JSContext* cx, JSCompartment* comp, GenericPrinter& out)
         RootedFunction fun(cx);
         do {
             script = queue.popCopy();
-            compCover.collectCodeCoverageInfo(comp, script->sourceObject(), script);
+            if (!script->initScriptName(cx))
+                return false;
+            if (script->hasScriptName())
+                compCover.collectCodeCoverageInfo(comp, script);
 
             // Iterate from the last to the first object in order to have
             // the functions them visited in the opposite order when popping
