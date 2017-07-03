9322c6508c913da8b43277f46b99d37df53861ac
Nicholas Nethercote <nnethercote@mozilla.com>
2017-07-04 09:58:38 +1000

    Bug 1377803 - Remove an unnecessary plarena.h #include in WebRTC. r=drno,glandium.
    We can avoid the symbol visibility problem by putting
    sanitizer/asan_interface.h in the config/system-headers.

--- media/webrtc/trunk/webrtc/base/sanitizer.h
+++ media/webrtc/trunk/webrtc/base/sanitizer.h
@@ -11,11 +11,6 @@
 #ifndef WEBRTC_BASE_SANITIZER_H_
 #define WEBRTC_BASE_SANITIZER_H_
 
-#ifdef WEBRTC_MOZILLA_BUILD
-// We need to define the __asan_* functions with the right visibility
-#include "nspr/plarena.h"
-#endif
-
 #if defined(__has_feature)
 #if __has_feature(address_sanitizer)
 #define RTC_HAS_ASAN 1
