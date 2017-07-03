a016e6e84ac755a3706c5efaffabf0ba574b3181
Wes Kocher <wkocher@mozilla.com>
2017-07-03 11:05:59 -0700

    Backed out changeset 25b411a687da (bug 1377004) for quantum_pageload_google failures on win10 a=backout
    MozReview-Commit-ID: IYlC0BWMxC7
    
    --HG--
    extra : source : 44502217ccf7cd6e6de02a47a084cbc2e33a2b01

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -4164,9 +4164,7 @@ nsHalfOpenSocket::StartFastOpen()
             SetupBackupTimer();
         }
     }
-    if (mEnt) {
-        mEnt->mDoNotDestroy = false;
-    }
+    mEnt->mDoNotDestroy = false;
     return rv;
 }
 
@@ -4261,14 +4259,17 @@ nsHalfOpenSocket::SetFastOpenConnected(nsresult aError, bool aWillRetry)
         mStreamOut = nullptr;
         mStreamIn = nullptr;
 
-        Abandon();
+        // If backup transport ha already started put this HalfOpen back to
+        // mEnt list.
+        if (mBackupTransport) {
+            mEnt->mHalfOpens.AppendElement(this);
+            gHttpHandler->ConnMgr()->mNumHalfOpenConns++;
+        }
     }
 
     mFastOpenInProgress = false;
     mConnectionNegotiatingFastOpen = nullptr;
-    if (mEnt) {
-        mEnt->mDoNotDestroy = false;
-    }
+    mEnt->mDoNotDestroy = false;
 }
 
 void
