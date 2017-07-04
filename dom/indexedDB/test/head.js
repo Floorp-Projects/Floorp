269529ee510e83711e0682b6928460224bd160b0
Peter Van der Beken <peterv@propagandism.org>
2017-07-04 18:03:18 +0200

    Bug 888600 - Move ContentFrameMessageManager to WebIDL. Part 2: Various test fixes to prepare for using WebIDL bindings for MessageManager classes. r=bz.

--- dom/indexedDB/test/head.js
+++ dom/indexedDB/test/head.js
@@ -80,11 +80,14 @@ function waitForMessage(aMessage, browser)
 {
   return new Promise((resolve, reject) => {
     /* eslint-disable no-undef */
+    // When contentScript runs, "this" is a ContentFrameMessageManager (so that's where
+    // addEventListener will add the listener), but the non-bubbling "message" event is
+    // sent to the Window involved, so we need a capturing listener.
     function contentScript() {
       addEventListener("message", function(event) {
         sendAsyncMessage("testLocal:message",
           {message: event.data});
-      }, {once: true}, true);
+      }, {once: true, capture: true}, true);
     }
     /* eslint-enable no-undef */
 
