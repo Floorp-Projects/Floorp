1a62cd9afa423dd74ea566310b52c84249b6104e
Kartikaya Gupta <kgupta@mozilla.com>
2017-07-04 11:16:40 -0400

    Bug 1336528 - Run cargo-vendor on all the lockfiles together. r=froydnj
    MozReview-Commit-ID: BBuizrn3z5k

--- python/mozbuild/mozbuild/vendor_rust.py
+++ python/mozbuild/mozbuild/vendor_rust.py
@@ -273,12 +273,17 @@ license file's hash.
             ('mozjs_sys', 'js/src'),
             ('geckodriver', 'testing/geckodriver'),
         )
+
+        lockfiles = []
         for (lib, crate_root) in crates_and_roots:
             path = mozpath.join(self.topsrcdir, crate_root)
             # We use check_call instead of mozprocess to ensure errors are displayed.
             # We do an |update -p| here to regenerate the Cargo.lock file with minimal changes. See bug 1324462
             subprocess.check_call([cargo, 'update', '--manifest-path', mozpath.join(path, 'Cargo.toml'), '-p', lib], cwd=self.topsrcdir)
-            subprocess.check_call([cargo, 'vendor', '--quiet', '--no-delete', '--sync', mozpath.join(path, 'Cargo.lock'), vendor_dir], cwd=self.topsrcdir)
+            lockfiles.append('--sync')
+            lockfiles.append(mozpath.join(path, 'Cargo.lock'))
+
+        subprocess.check_call([cargo, 'vendor', '--quiet', '--no-delete'] + lockfiles + [vendor_dir], cwd=self.topsrcdir)
 
         if not self._check_licenses(vendor_dir):
             self.log(logging.ERROR, 'license_check_failed', {},
