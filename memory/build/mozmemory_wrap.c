1948b2eb99688d4de7b9f97623cbd02a9b98a42c
Mike Hommey <mh+mozilla@glandium.org>
2017-07-04 15:01:50 +0900

    Bug 1356701 - Export unprefixed malloc and duplication functions on OSX. r=njn
    Going through the system zone allocator for every call to realloc/free
    on OSX is costly, because the zone allocator needs to first verify that
    the allocations do belong to the allocator it invokes (which ends up
    calling jemalloc's malloc_usable_size), which is unnecessary when we
    expect the allocations to belong to jemalloc.
    
    So, we export the malloc/realloc/free/etc. symbols from
    libmozglue.dylib, such that libraries and programs linked against it
    calls directly into jemalloc instead of going through the system zone
    allocator, effectively shortcutting the allocator verification.
    
    The risk is that some things in Gecko try to realloc/free pointers it
    got from system libraries, if those were allocated with a system zone
    that is not jemalloc.

--- memory/build/mozmemory_wrap.c
+++ memory/build/mozmemory_wrap.c
@@ -68,7 +68,6 @@ mozmem_malloc_impl(_ZdaPvRKSt9nothrow_t)(void *ptr)
 #undef strndup
 #undef strdup
 
-#ifndef XP_DARWIN
 MOZ_MEMORY_API char *
 strndup_impl(const char *src, size_t len)
 {
@@ -86,7 +85,6 @@ strdup_impl(const char *src)
   size_t len = strlen(src);
   return strndup_impl(src, len);
 }
-#endif /* XP_DARWIN */
 
 #ifdef ANDROID
 #include <stdarg.h>
