e4d7874f9d98bd88e5d79a748c558d61995062be
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2017-07-04 22:51:36 +0100

    Bug 1378194 - ordering of buttons in browser_ext_themes_icons.js matters; don't append bookmarks menu button at the end, r=jaws
    MozReview-Commit-ID: 8WOsV4u9DJY

--- browser/components/extensions/test/browser/browser_ext_themes_icons.js
+++ browser/components/extensions/test/browser/browser_ext_themes_icons.js
@@ -140,12 +140,14 @@ async function runTestWithIcons(icons) {
     ["forget", "#panic-button", "panic-button"],
     ["pocket", "#pocket-button", "pocket-button"],
   ];
+  // We add these at the beginning because adding them at the end can end up
+  // putting them in the overflow panel, where they aren't displayed the same way.
   if (AppConstants.MOZ_PHOTON_THEME) {
-    ICON_INFO.push(["bookmark_star", "#star-button"]);
-    ICON_INFO.push(["bookmark_menu", "#bookmarks-menu-button", "bookmarks-menu-button"]);
+    ICON_INFO.unshift(["bookmark_star", "#star-button"]);
+    ICON_INFO.unshift(["bookmark_menu", "#bookmarks-menu-button", "bookmarks-menu-button"]);
   } else {
-    ICON_INFO.push(["bookmark_star", "#bookmarks-menu-button", "bookmarks-menu-button"]);
-    ICON_INFO.push(["bookmark_menu", "#bookmarks-menu-button > .toolbarbutton-menubutton-dropmarker > .dropmarker-icon"]);
+    ICON_INFO.unshift(["bookmark_star", "#bookmarks-menu-button", "bookmarks-menu-button"]);
+    ICON_INFO.unshift(["bookmark_menu", "#bookmarks-menu-button > .toolbarbutton-menubutton-dropmarker > .dropmarker-icon"]);
   }
 
   window.maximize();
