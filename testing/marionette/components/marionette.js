598b7a0835255facd30daecfc3cc852ac5189fa1
Henrik Skupin <mail@hskupin.info>
2017-07-05 10:35:28 +0200

    Bug 1378209 - Add DEBUG log line for received observer notifications. r=automatedtester, a=test-only
    For debugging purposes it would be useful to see which observer
    notifications have been received and handled by the Marionette
    component.
    
    MozReview-Commit-ID: 1EucCEjOXhu

--- testing/marionette/components/marionette.js
+++ testing/marionette/components/marionette.js
@@ -168,6 +168,8 @@ MarionetteComponent.prototype.handle = function (cmdLine) {
 };
 
 MarionetteComponent.prototype.observe = function (subject, topic, data) {
+  this.logger.debug(`Received observer notification "${topic}"`);
+
   switch (topic) {
     case "command-line-startup":
       Services.obs.removeObserver(this, topic);
