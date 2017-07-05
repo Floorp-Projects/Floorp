d3614565be8effb6bd8b4485016e52b0bf6b0b86
Wes Kocher <wkocher@mozilla.com>
2017-07-04 22:32:54 -0700

    Backed out 3 changesets (bug 1372433) for stylo assertions in ServoRestyleManager.cpp a=backout
    Backed out changeset 3abe2b5f8b24 (bug 1372433)
    Backed out changeset cbb550def80d (bug 1372433)
    Backed out changeset 78b5ca196c54 (bug 1372433)
    
    MozReview-Commit-ID: H6t8LpY3FHX

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -2385,8 +2385,6 @@ ContentChild::RecvNotifyAlertsObserver(const nsCString& aType, const nsString& a
   return IPC_OK();
 }
 
-// NOTE: This method is being run in the SystemGroup, and thus cannot directly
-// touch pages. See GetSpecificMessageEventTarget.
 mozilla::ipc::IPCResult
 ContentChild::RecvNotifyVisited(const URIParams& aURI)
 {
@@ -3545,10 +3543,8 @@ ContentChild::RecvShareCodeCoverageMutex(const CrossProcessMutexHandle& aHandle)
 already_AddRefed<nsIEventTarget>
 ContentChild::GetSpecificMessageEventTarget(const Message& aMsg)
 {
-  if ((aMsg.type() == PJavaScript::Msg_DropTemporaryStrongReferences__ID
-       || aMsg.type() == PJavaScript::Msg_DropObject__ID
-       || aMsg.type() == PContent::Msg_NotifyVisited__ID)
-      && SystemGroup::Initialized()) {
+  if (aMsg.type() == PJavaScript::Msg_DropTemporaryStrongReferences__ID
+      || aMsg.type() == PJavaScript::Msg_DropObject__ID) {
     return do_AddRef(SystemGroup::EventTargetFor(TaskCategory::Other));
   }
 
