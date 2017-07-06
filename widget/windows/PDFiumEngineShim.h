ee4e319c470626f3555c8155186b0a8c8058833d
Farmer Tseng <fatseng@mozilla.com>
2017-07-06 10:25:46 +0800

    Bug 1372113 - Call PDFium function directly in PDFiumEngineShim r=jwatt
    1.PDfium would be built into the xul (bug 1368948) so PDFiumEngineShim could
    call functions directly. However, I still keep loading dynamic library for
    debugging purpose. Create a new preference for storing the library file path.
    2.Remove redundant define and typedef which are written in PDFium head file.
    3.Include an appropriate head file from PDFium.
    4.Remove non-used PDFium APIs.
    5.Use the singleton pattern to retrieve the instance of PDFiumEngineShim.
    6.Don't destroy the library inside PDFiumEngineShim::CloseDocument because the
    library has to deal with multi-document.
    
    MozReview-Commit-ID: 97ZBFp1qU8V
    
    --HG--
    extra : rebase_source : a60ee37906a94788e0baabbb95bd8848eca096af

--- widget/windows/PDFiumEngineShim.h
+++ widget/windows/PDFiumEngineShim.h
@@ -7,92 +7,25 @@
 #define PDFIUMENGINESHIM_H
 
 #include "prlink.h"
+#include "fpdfview.h"
+
+//#define USE_EXTERNAL_PDFIUM
 
 /* include windows.h for the HDC definitions that we need. */
 #include <windows.h>
 
 namespace mozilla {
 namespace widget {
-/**
- * Create Bug 1349139 to replace following defines with an appropriate include file.
- */
-// Page rendering flags. They can be combined with bit-wise OR.
-//
-// Set if annotations are to be rendered.
-#define FPDF_ANNOT 0x01
-// Set if using text rendering optimized for LCD display.
-#define FPDF_LCD_TEXT 0x02
-// Don't use the native text output available on some platforms
-#define FPDF_NO_NATIVETEXT 0x04
-// Grayscale output.
-#define FPDF_GRAYSCALE 0x08
-// Set if you want to get some debug info.
-#define FPDF_DEBUG_INFO 0x80
-// Set if you don't want to catch exceptions.
-#define FPDF_NO_CATCH 0x100
-// Limit image cache size.
-#define FPDF_RENDER_LIMITEDIMAGECACHE 0x200
-// Always use halftone for image stretching.
-#define FPDF_RENDER_FORCEHALFTONE 0x400
-// Render for printing.
-#define FPDF_PRINTING 0x800
-// Set to disable anti-aliasing on text.
-#define FPDF_RENDER_NO_SMOOTHTEXT 0x1000
-// Set to disable anti-aliasing on images.
-#define FPDF_RENDER_NO_SMOOTHIMAGE 0x2000
-// Set to disable anti-aliasing on paths.
-#define FPDF_RENDER_NO_SMOOTHPATH 0x4000
-// Set whether to render in a reverse Byte order, this flag is only used when
-// rendering to a bitmap.
-#define FPDF_REVERSE_BYTE_ORDER 0x10
-
-// PDF types
-typedef void* FPDF_ACTION;
-typedef void* FPDF_BITMAP;
-typedef void* FPDF_BOOKMARK;
-typedef void* FPDF_CLIPPATH;
-typedef void* FPDF_DEST;
-typedef void* FPDF_DOCSCHHANDLE;
-typedef void* FPDF_DOCUMENT;
-typedef void* FPDF_FONT;
-typedef void* FPDF_HMODULE;
-typedef void* FPDF_LINK;
-typedef void* FPDF_MODULEMGR;
-typedef void* FPDF_PAGE;
-typedef void* FPDF_PAGELINK;
-typedef void* FPDF_PAGEOBJECT;  // Page object(text, path, etc)
-typedef void* FPDF_PAGERANGE;
-typedef void* FPDF_PATH;
-typedef void* FPDF_RECORDER;
-typedef void* FPDF_SCHHANDLE;
-typedef void* FPDF_STRUCTELEMENT;
-typedef void* FPDF_STRUCTTREE;
-typedef void* FPDF_TEXTPAGE;
-typedef const char* FPDF_STRING;
-
-// Basic data types
-typedef int FPDF_BOOL;
-typedef int FPDF_ERROR;
-typedef unsigned long FPDF_DWORD;
-typedef float FS_FLOAT;
-
-typedef const char* FPDF_BYTESTRING;
 
 typedef void (*FPDF_InitLibrary_Pfn)();
 typedef void (*FPDF_DestroyLibrary_Pfn)();
-typedef FPDF_DOCUMENT (*FPDF_LoadMemDocument_Pfn)(const void* aDataBuf,
-                                                  int aSize,
-                                                  FPDF_BYTESTRING aPassword);
+
 typedef FPDF_DOCUMENT (*FPDF_LoadDocument_Pfn)(FPDF_STRING file_path,
                                                FPDF_BYTESTRING password);
 
 typedef void(*FPDF_CloseDocument_Pfn)(FPDF_DOCUMENT aDocument);
 
 typedef int (*FPDF_GetPageCount_Pfn)(FPDF_DOCUMENT aDocument);
-typedef int (*FPDF_GetPageSizeByIndex_Pfn)(FPDF_DOCUMENT aDocument,
-                                           int aPageIndex,
-                                           double* aWidth,
-                                           double* aWeight);
 
 typedef FPDF_PAGE (*FPDF_LoadPage_Pfn)(FPDF_DOCUMENT aDocument, int aPageIndex);
 typedef void (*FPDF_ClosePage_Pfn)(FPDF_PAGE aPage);
@@ -104,9 +37,6 @@ typedef void (*FPDF_RenderPage_Pfn)(HDC aDC,
                                     int aSizeY,
                                     int aRotate,
                                     int aFlags);
-typedef void (*FPDF_RenderPage_Close_Pfn) (FPDF_PAGE aPage);
-
-
 
 /**
  * This class exposes an interface to the PDFium library and
@@ -116,12 +46,12 @@ class PDFiumEngineShim
 {
 public:
 
-  explicit PDFiumEngineShim(PRLibrary* aLibrary);
-  ~PDFiumEngineShim();
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(PDFiumEngineShim)
 
-  FPDF_DOCUMENT LoadMemDocument(const void* aDataBuf,
-                                int aSize,
-                                FPDF_BYTESTRING aPassword);
+  static already_AddRefed<PDFiumEngineShim> GetInstanceOrNull();
+
+  bool Init();
+
   FPDF_DOCUMENT LoadDocument(FPDF_STRING file_path,
                              FPDF_BYTESTRING aPassword);
   void CloseDocument(FPDF_DOCUMENT aDocument);
@@ -136,29 +66,25 @@ public:
                   int aSizeX, int aSizeY,
                   int aRotate, int aFlags);
 
-  void RenderPage_Close(FPDF_PAGE aPage);
-
 private:
 
-  bool InitSymbolsAndLibrary();
-  void InitLibrary();
-  void DestroyLibrary();
+  PDFiumEngineShim();
+  ~PDFiumEngineShim();
 
-  PRLibrary*  mPRLibrary;
   bool        mInitialized ;
 
   FPDF_InitLibrary_Pfn        mFPDF_InitLibrary;
   FPDF_DestroyLibrary_Pfn     mFPDF_DestroyLibrary;
-  FPDF_LoadMemDocument_Pfn    mFPDF_LoadMemDocument;
   FPDF_LoadDocument_Pfn       mFPDF_LoadDocument;
   FPDF_CloseDocument_Pfn      mFPDF_CloseDocument;
   FPDF_GetPageCount_Pfn       mFPDF_GetPageCount;
-  FPDF_GetPageSizeByIndex_Pfn mFPDF_GetPageSizeByIndex;
   FPDF_LoadPage_Pfn           mFPDF_LoadPage;
   FPDF_ClosePage_Pfn          mFPDF_ClosePage;
   FPDF_RenderPage_Pfn         mFPDF_RenderPage;
-  FPDF_RenderPage_Close_Pfn   mFPDF_RenderPage_Close;
 
+#ifdef USE_EXTERNAL_PDFIUM
+  PRLibrary*  mPRLibrary;
+#endif
 };
 
 } // namespace widget
