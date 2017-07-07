c2b455eb85363b24f30b030cc6155c231192d3db
Sebastian Kaspari <s.kaspari@gmail.com>
2017-07-07 15:48:30 +0200

    [focus] Correctly leave immersive mode when exiting full screen mode again. (https://github.com/mozilla-mobile/focus-android/issues/872)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -396,6 +396,8 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                 // Show browser UI and web content again
                 browserContainer.setVisibility(View.VISIBLE);
 
+                exitImmersiveMode();
+
                 // Notify renderer that we left fullscreen mode.
                 if (fullscreenCallback != null) {
                     fullscreenCallback.fullScreenExited();
@@ -442,6 +444,20 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    /**
+     * Show the system bars again.
+     */
+    private void exitImmersiveMode() {
+        final Activity activity = getActivity();
+        if (activity == null) {
+            return;
+        }
+
+        activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     @Override
