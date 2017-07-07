4044510f8c75a3a4e55748d4417185628325cec6
gasolin <gasolin@gmail.com>
2017-07-07 11:52:33 +0800

    Bug 1375775 - test cases for reusable tourset;r=mossop
    MozReview-Commit-ID: IlxgYRuFpCc

--- browser/extensions/onboarding/test/browser/browser_onboarding_tours.js
+++ browser/extensions/onboarding/test/browser/browser_onboarding_tours.js
@@ -29,11 +29,10 @@ function assertTourCompletedStyle(tourId, expectComplete, browser) {
 
 add_task(async function test_hide_onboarding_tours() {
   resetOnboardingDefaultState();
-  await SpecialPowers.pushPrefEnv({set: [["browser.onboarding.enabled", true]]});
 
   let tourIds = TOUR_IDs;
   let tabs = [];
-  for (let url of [ABOUT_NEWTAB_URL, ABOUT_HOME_URL]) {
+  for (let url of URLs) {
     let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
     await BrowserTestUtils.loadURI(tab.linkedBrowser, url);
     await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
@@ -61,11 +60,10 @@ add_task(async function test_hide_onboarding_tours() {
 
 add_task(async function test_click_action_button_to_set_tour_completed() {
   resetOnboardingDefaultState();
-  await SpecialPowers.pushPrefEnv({set: [["browser.onboarding.enabled", true]]});
 
   let tourIds = TOUR_IDs;
   let tabs = [];
-  for (let url of [ABOUT_NEWTAB_URL, ABOUT_HOME_URL]) {
+  for (let url of URLs) {
     let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
     await BrowserTestUtils.loadURI(tab.linkedBrowser, url);
     await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
@@ -91,7 +89,6 @@ add_task(async function test_click_action_button_to_set_tour_completed() {
 
 add_task(async function test_set_right_tour_completed_style_on_overlay() {
   resetOnboardingDefaultState();
-  await SpecialPowers.pushPrefEnv({set: [["browser.onboarding.enabled", true]]});
 
   let tourIds = TOUR_IDs;
   // Make the tours of even number as completed
@@ -100,7 +97,7 @@ add_task(async function test_set_right_tour_completed_style_on_overlay() {
   }
 
   let tabs = [];
-  for (let url of [ABOUT_NEWTAB_URL, ABOUT_HOME_URL]) {
+  for (let url of URLs) {
     let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
     await BrowserTestUtils.loadURI(tab.linkedBrowser, url);
     await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
