c57409fca6dd330f0e7770b41ab69a23cec6e751
Kris Maglione <maglione.k@gmail.com>
2017-07-06 21:13:40 -0700

    Bug 1357486: Enable OOP extensions by default on Windows. r=aswan
    MozReview-Commit-ID: 3Tet7tNtmGx
    
    --HG--
    extra : source : 7124641a168ffaa20461764f2f554992dfd856e6
    extra : absorb_source : a8b47dcf788b841a832ea5a8669f8940b2412f6d
    extra : histedit_source : dd3cab4db72f4d24b3103834a644173632064d25

--- toolkit/components/extensions/test/mochitest/head.js
+++ toolkit/components/extensions/test/mochitest/head.js
@@ -8,11 +8,11 @@ var {AppConstants} = SpecialPowers.Cu.import("resource://gre/modules/AppConstant
 // mochitest-remote.ini. When running from mochitest-remote.ini, the tests are
 // copied to the sub-directory "test-oop-extensions", which we detect here, and
 // use to select our configuration.
-if (location.pathname.includes("test-oop-extensions")) {
-  SpecialPowers.pushPrefEnv({set: [
-    ["extensions.webextensions.remote", true],
-    ["layers.popups.compositing.enabled", true],
-  ]});
+let remote = location.pathname.includes("test-oop-extensions");
+SpecialPowers.pushPrefEnv({set: [
+  ["extensions.webextensions.remote", remote],
+]});
+if (remote) {
   // We don't want to reset this at the end of the test, so that we don't have
   // to spawn a new extension child process for each test unit.
   SpecialPowers.setIntPref("dom.ipc.keepProcessesAlive.extension", 1);
