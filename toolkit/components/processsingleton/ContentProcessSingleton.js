d0a6dc0361c7cdb7ed8ca678be1204d8d9177cde
Kris Maglione <maglione.k@gmail.com>
2017-07-07 14:34:02 -0700

    Bug 1357486: Part 0c - Propagate addonId to parent process with console messages. r=aswan
    MozReview-Commit-ID: I1LzpMcfgEh

--- toolkit/components/processsingleton/ContentProcessSingleton.js
+++ toolkit/components/processsingleton/ContentProcessSingleton.js
@@ -62,6 +62,7 @@ ContentProcessSingleton.prototype = {
         functionName: consoleMsg.functionName.substring(0,
           MSG_MGR_CONSOLE_INFO_MAX),
         timeStamp: consoleMsg.timeStamp,
+        addonId: consoleMsg.addonId,
         arguments: [],
       };
 
