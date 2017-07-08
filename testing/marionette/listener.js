966e4e8fbf2529f01c7a390d6637709b14d22b3f
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-07-08 10:49:13 +0200

    Backed out changeset 7571a1477753 (bug 1374672)

--- testing/marionette/listener.js
+++ testing/marionette/listener.js
@@ -1137,9 +1137,9 @@ function get(msg) {
   try {
     if (typeof url == "string") {
       try {
+        let requestedURL = new URL(url).toString();
         if (loadEventExpected === null) {
-          loadEventExpected = navigate.isLoadEventExpected(
-              curContainer.frame.location, url);
+          loadEventExpected = navigate.isLoadEventExpected(requestedURL);
         }
       } catch (e) {
         let err = new InvalidArgumentError("Malformed URL: " + e.message);
