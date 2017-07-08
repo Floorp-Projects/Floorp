32f63d21ec939c9400b8e49cf749690f7af819ca
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-07-08 10:49:13 +0200

    Backed out changeset 7571a1477753 (bug 1374672)

--- testing/marionette/navigate.js
+++ testing/marionette/navigate.js
@@ -14,45 +14,29 @@ this.navigate = {};
 
 /**
  * Determines if we expect to get a DOM load event (DOMContentLoaded)
- * on navigating to the |future| URL.
+ * on navigating to the |url|.
  *
- * @param {string} current
- *     URL the browser is currently visiting.
- * @param {string=} future
- *     Destination URL, if known.
+ * @param {string} url
+ *     Destination URL
  *
  * @return {boolean}
- *     Full page load would be expected if future is followed.
+ *     Full page load would be expected if url gets loaded.
  *
  * @throws TypeError
- *     If |current| is not defined, or any of |current| or |future|
- *     are invalid URLs.
+ *     If |url| is an invalid URL.
  */
-navigate.isLoadEventExpected = function(current, future = undefined) {
+navigate.isLoadEventExpected = function(url) {
   // assume we will go somewhere exciting
-  if (typeof current == "undefined") {
-    throw TypeError("Expected at least one URL");
+  if (typeof url == "undefined") {
+    throw TypeError("Expected destination URL");
   }
 
-  // Assume we will go somewhere exciting
-  if (typeof future == "undefined") {
-    return true;
-  }
-
-  let cur = new URL(current);
-  let fut = new URL(future);
-
-  // Assume javascript:<whatever> will modify the current document
-  // but this is not an entirely safe assumption to make,
-  // considering it could be used to set window.location
-  if (fut.protocol == "javascript:") {
-    return false;
-  }
-
-  // If hashes are present and identical
-  if (cur.href.includes("#") && fut.href.includes("#") &&
-      cur.hash === fut.hash) {
-    return false;
+  switch (new URL(url).protocol) {
+    // assume javascript:<whatever> will modify current document
+    // but this is not an entirely safe assumption to make,
+    // considering it could be used to set window.location
+    case "javascript:":
+      return false;
   }
 
   return true;
