1921f26e23e968803e8ec81b2a80209314f915f4
Kartikaya Gupta <kgupta@mozilla.com>
2017-07-10 07:22:31 -0400

    Bug 1374730 - Update Cargo.lock files and revendor rust dependencies. r=jrmuizel
    MozReview-Commit-ID: AoLr2W2NGhP

--- third_party/rust/bitflags/tests/external.rs
+++ third_party/rust/bitflags/tests/external.rs
@@ -5,13 +5,13 @@ extern crate bitflags;
 
 bitflags! {
     /// baz
-    flags Flags: u32 {
-        const A       = 0b00000001,
+    struct Flags: u32 {
+        const A       = 0b00000001;
         #[doc = "bar"]
-        const B       = 0b00000010,
-        const C       = 0b00000100,
+        const B       = 0b00000010;
+        const C       = 0b00000100;
         #[doc = "foo"]
-        const ABC     = A.bits | B.bits | C.bits,
+        const ABC     = A.bits | B.bits | C.bits;
     }
 }
 
