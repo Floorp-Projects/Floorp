038afacb9ff87326ff34a61ae13eaf6cf9c824da
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-07-11 13:14:55 +0200

    Backed out changeset 2ae22a66e02d (bug 1366694) for memory leaks

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -307,14 +307,8 @@ SandboxBroker::SetSecurityLevelForContentProcess(int32_t aSandboxLevel,
     }
   }
 
-#if defined(DEBUG)
-  // This is required for a MOZ_ASSERT check in WindowsMessageLoop.cpp
-  // WinEventHook, see bug 1366694 for details.
-  DWORD uiExceptions = JOB_OBJECT_UILIMIT_HANDLES;
-#else
-  DWORD uiExceptions = 0;
-#endif
-  sandbox::ResultCode result = mPolicy->SetJobLevel(jobLevel, uiExceptions);
+  sandbox::ResultCode result = mPolicy->SetJobLevel(jobLevel,
+                                                    0 /* ui_exceptions */);
   MOZ_RELEASE_ASSERT(sandbox::SBOX_ALL_OK == result,
                      "Setting job level failed, have you set memory limit when jobLevel == JOB_NONE?");
 
