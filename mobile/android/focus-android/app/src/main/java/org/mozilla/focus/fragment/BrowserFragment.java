323403922b4b74f3371f08772c62dcd20bca9b1e
Sebastian Kaspari <s.kaspari@gmail.com>
2017-07-12 19:12:52 +0200

    [focus] Keep screen on while playing full screen videos. (https://github.com/mozilla-mobile/focus-android/issues/910)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -26,6 +26,8 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.URLUtil;
 import android.widget.FrameLayout;
@@ -437,7 +439,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             return;
         }
 
-        activity.getWindow().getDecorView().setSystemUiVisibility(
+        final Window window = activity.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        window.getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -455,7 +459,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             return;
         }
 
-        activity.getWindow().getDecorView().setSystemUiVisibility(
+        final Window window = activity.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        window.getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
