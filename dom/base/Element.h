94f4192698f1f3faea137d33f05ae27a2ed10f53
Kartikaya Gupta <kgupta@mozilla.com>
2017-07-12 15:28:50 -0400

    Bug 1380375 - Remove the DOM bindings for the unused scrollgrab feature. r=smaug
    MozReview-Commit-ID: 87aJOsc6zbO

--- dom/base/Element.h
+++ dom/base/Element.h
@@ -149,8 +149,7 @@ enum {
                               ELEMENT_POTENTIAL_RESTYLE_ROOT_FLAGS |
                               ELEMENT_IS_CONDITIONAL_RESTYLE_ANCESTOR,
 
-  // Set if this element is marked as 'scrollgrab' (see bug 912666)
-  ELEMENT_HAS_SCROLLGRAB = ELEMENT_FLAG_BIT(5),
+  // ELEMENT_FLAG_BIT(5) is currently unused
 
   // Remaining bits are for subclasses
   ELEMENT_TYPE_SPECIFIC_BITS_OFFSET = NODE_TYPE_SPECIFIC_BITS_OFFSET + 6
