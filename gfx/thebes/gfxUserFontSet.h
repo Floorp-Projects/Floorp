2d734daeffd353e22d08c13eecb5fc6667e14c22
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-07-12 12:41:37 +0200

    Backed out changeset 0cd32b60dabe (bug 1376964)

--- gfx/thebes/gfxUserFontSet.h
+++ gfx/thebes/gfxUserFontSet.h
@@ -301,10 +301,6 @@ public:
                                      gfxUserFontEntry* aUserFontEntry,
                                      bool              aPrivate);
 
-        // Generation number that is incremented whenever an entry is added to
-        // the cache.  (Removals don't increment it.)
-        static uint32_t Generation() { return sGeneration; }
-
         // Clear everything so that we don't leak URIs and Principals.
         static void Shutdown();
 
@@ -432,8 +428,6 @@ public:
         };
 
         static nsTHashtable<Entry>* sUserFonts;
-
-        static uint32_t sGeneration;
     };
 
     void SetLocalRulesUsed() {
