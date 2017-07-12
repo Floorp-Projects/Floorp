6db69bbd2ea346fd4bc4be710cfeb179b5c11c36
Rail Aliiev <rail@mozilla.com>
2017-07-12 09:30:41 -0400

    Bug 1380297 - Mac nightly repackage fails trying to read CodeName r=mshal
    MozReview-Commit-ID: LauJRihWxJ0

--- python/mozbuild/mozbuild/repackaging/application_ini.py
+++ python/mozbuild/mozbuild/repackaging/application_ini.py
@@ -6,13 +6,21 @@ import os
 import ConfigParser
 import mozpack.path as mozpath
 
-def get_application_ini_value(application_directory, section, value):
+
+def get_application_ini_value(application_directory, section, value,
+                              fallback=None):
     rc = None
     for root, dirs, files in os.walk(application_directory):
         if 'application.ini' in files:
             parser = ConfigParser.ConfigParser()
             parser.read(mozpath.join(root, 'application.ini'))
-            rc = parser.get(section, value)
+            try:
+                rc = parser.get(section, value)
+            except ConfigParser.NoOptionError:
+                if not fallback:
+                    raise
+                else:
+                    rc = parser.get(section, fallback)
             break
 
     if rc is None:
