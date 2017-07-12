4734586594ae2959e9762cb22303de17d5520d48
Fischer.json <fischer.json@gmail.com>
2017-07-12 12:44:34 +0800

    Bug 1372067 - Part 2: Add the test cases, r=mossop
    This commit
    - splits up the browser_onboarding_notification.js test
    - adds the test_mute_notification_on_1st_session test case
    - adds the test_move_on_to_next_notification_when_reaching_max_prompt_count test case
    - adds the test_move_on_to_next_notification_when_reaching_max_life_time test case
    - adds the test_move_on_to_next_notification_after_interacting_with_notification test case
    - updates the existing test cases for the new notification timing policy
    - updates the browser_newtab_focus.js test
    
    MozReview-Commit-ID: Ej2bHJSr35z

---
+++ browser/extensions/onboarding/test/browser/browser_onboarding_notification_4.js
@@ -0,0 +1,84 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function test_remove_all_tour_notifications_through_close_button() {
+  resetOnboardingDefaultState();
+  skipMuteNotificationOnFirstSession();
+
+  let tourIds = TOUR_IDs;
+  let tab = null;
+  let targetTourId = null;
+  let reloadPromise = null;
+  await closeTourNotificationsOneByOne();
+
+  let expectedPrefUpdate = promisePrefUpdated("browser.onboarding.notification.finished", true);
+  reloadPromise = BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  tab.linkedBrowser.reload();
+  await reloadPromise;
+  await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
+  await expectedPrefUpdate;
+  let tourId = await getCurrentNotificationTargetTourId(tab.linkedBrowser);
+  ok(!tourId, "Should not prompt tour notifications any more after closing all notifcations.");
+  await BrowserTestUtils.removeTab(tab);
+
+  async function closeTourNotificationsOneByOne() {
+    for (let i = 0; i < tourIds.length; ++i) {
+      if (tab) {
+        reloadPromise = BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+        tab.linkedBrowser.reload();
+        await reloadPromise;
+      } else {
+        tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
+        await BrowserTestUtils.loadURI(tab.linkedBrowser, ABOUT_NEWTAB_URL);
+      }
+      await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
+      await promiseTourNotificationOpened(tab.linkedBrowser);
+      targetTourId = await getCurrentNotificationTargetTourId(tab.linkedBrowser);
+      is(targetTourId, tourIds[i], `Should show tour notifications of ${targetTourId}`);
+      await BrowserTestUtils.synthesizeMouseAtCenter("#onboarding-notification-close-btn", {}, tab.linkedBrowser);
+      await promiseTourNotificationClosed(tab.linkedBrowser);
+    }
+  }
+});
+
+add_task(async function test_remove_all_tour_notifications_through_action_button() {
+  resetOnboardingDefaultState();
+  skipMuteNotificationOnFirstSession();
+
+  let tourIds = TOUR_IDs;
+  let tab = null;
+  let targetTourId = null;
+  let reloadPromise = null;
+  await clickTourNotificationActionButtonsOneByOne();
+
+  let expectedPrefUpdate = promisePrefUpdated("browser.onboarding.notification.finished", true);
+  reloadPromise = BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  tab.linkedBrowser.reload();
+  await reloadPromise;
+  await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
+  await expectedPrefUpdate;
+  let tourId = await getCurrentNotificationTargetTourId(tab.linkedBrowser);
+  ok(!tourId, "Should not prompt tour notifcations any more after taking actions on all notifcations.");
+  await BrowserTestUtils.removeTab(tab);
+
+  async function clickTourNotificationActionButtonsOneByOne() {
+    for (let i = 0; i < tourIds.length; ++i) {
+      if (tab) {
+        reloadPromise = BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+        tab.linkedBrowser.reload();
+        await reloadPromise;
+      } else {
+        tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
+        await BrowserTestUtils.loadURI(tab.linkedBrowser, ABOUT_NEWTAB_URL);
+      }
+      await promiseOnboardingOverlayLoaded(tab.linkedBrowser);
+      await promiseTourNotificationOpened(tab.linkedBrowser);
+      targetTourId = await getCurrentNotificationTargetTourId(tab.linkedBrowser);
+      is(targetTourId, tourIds[i], `Should show tour notifications of ${targetTourId}`);
+      await BrowserTestUtils.synthesizeMouseAtCenter("#onboarding-notification-action-btn", {}, tab.linkedBrowser);
+      await promiseTourNotificationClosed(tab.linkedBrowser);
+    }
+  }
+});
