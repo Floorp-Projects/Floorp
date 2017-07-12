8c234e0627dcb0d70291e9ca19f99ac6a7c9ec02
André Bargull <andre.bargull@gmail.com>
2017-07-12 07:37:53 -0700

    Bug 1380087 - Add an intrinsic to test for Map and Set objects. r=till
    --HG--
    extra : rebase_source : c30f89c15ddce434a86ca043b7ae6a9c5d00c905

--- js/src/builtin/Set.js
+++ js/src/builtin/Set.js
@@ -19,26 +19,21 @@ function SetConstructorInit(iterable) {
         callContentFunction(adder, set, nextValue);
 }
 
-/* ES6 20121122 draft 15.16.4.6. */
+// ES2018 draft rev f83aa38282c2a60c6916ebc410bfdf105a0f6a54
+// 23.2.3.6 Set.prototype.forEach ( callbackfn [ , thisArg ] )
 function SetForEach(callbackfn, thisArg = undefined) {
-    /* Step 1-2. */
+    // Step 1.
     var S = this;
-    if (!IsObject(S))
-        ThrowTypeError(JSMSG_INCOMPATIBLE_PROTO, "Set", "forEach", typeof S);
 
-    /* Step 3-4. */
-    try {
-        callFunction(std_Set_has, S);
-    } catch (e) {
-        // has will throw on non-Set objects, throw our own error in that case.
-        ThrowTypeError(JSMSG_INCOMPATIBLE_PROTO, "Set", "forEach", typeof S);
-    }
+    // Steps 2-3.
+    if (!IsObject(S) || !IsSetObject(S))
+        return callFunction(CallSetMethodIfWrapped, S, callbackfn, thisArg, "SetForEach");
 
-    /* Step 5-6. */
+    // Step 4.
     if (!IsCallable(callbackfn))
         ThrowTypeError(JSMSG_NOT_FUNCTION, DecompileArg(0, callbackfn));
 
-    /* Step 7-8. */
+    // Steps 5-8.
     var values = callFunction(std_Set_iterator, S);
     while (true) {
         var result = callFunction(SetIteratorNext, values);
