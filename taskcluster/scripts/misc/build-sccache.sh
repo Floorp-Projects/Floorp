5937b178ee2f5dbfa15daf5467a6812a7eb23445
Mike Hommey <mh+mozilla@glandium.org>
2017-07-14 11:07:40 +0900

    Bug 1381772 - Add taskcluster jobs to build sccache for automation. r=ted
    The MinGit tooltool package used for Windows builds comes straight from
    https://github.com/git-for-windows/git/releases/
    
    This builds the version currently used on automation.

---
+++ taskcluster/scripts/misc/build-sccache.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -x -e -v
+
+SCCACHE_REVISION=69334a26ba65fc88e3934271a2ce6781c51b445e
+
+# This script is for building sccache
+
+case "$(uname -s)" in
+Linux)
+    WORKSPACE=$HOME/workspace
+    UPLOAD_DIR=$HOME/artifacts
+    export CC=clang
+    PATH="$WORKSPACE/build/src/clang/bin:$PATH"
+    COMPRESS_EXT=xz
+    ;;
+MINGW*)
+    WORKSPACE=$PWD
+    UPLOAD_DIR=$WORKSPACE/public/build
+    WIN_WORKSPACE="$(pwd -W)"
+    COMPRESS_EXT=bz2
+
+    export INCLUDE="$WIN_WORKSPACE/build/src/vs2015u3/VC/include;$WIN_WORKSPACE/build/src/vs2015u3/VC/atlmfc/include;$WIN_WORKSPACE/build/src/vs2015u3/SDK/Include/10.0.14393.0/ucrt;$WIN_WORKSPACE/build/src/vs2015u3/SDK/Include/10.0.14393.0/shared;$WIN_WORKSPACE/build/src/vs2015u3/SDK/Include/10.0.14393.0/um;$WIN_WORKSPACE/build/src/vs2015u3/SDK/Include/10.0.14393.0/winrt;$WIN_WORKSPACE/build/src/vs2015u3/DIA SDK/include"
+
+    export LIB="$WIN_WORKSPACE/build/src/vs2015u3/VC/lib/amd64;$WIN_WORKSPACE/build/src/vs2015u3/VC/atlmfc/lib/amd64;$WIN_WORKSPACE/build/src/vs2015u3/SDK/lib/10.0.14393.0/um/x64;$WIN_WORKSPACE/build/src/vs2015u3/SDK/lib/10.0.14393.0/ucrt/x64;$WIN_WORKSPACE/build/src/vs2015u3/DIA SDK/lib/amd64"
+
+    PATH="$WORKSPACE/build/src/vs2015u3/VC/bin/amd64:$WORKSPACE/build/src/vs2015u3/VC/bin:$WORKSPACE/build/src/vs2015u3/SDK/bin/x64:$WORKSPACE/build/src/vs2015u3/redist/x64/Microsoft.VC140.CRT:$WORKSPACE/build/src/vs2015u3/SDK/Redist/ucrt/DLLs/x64:$WORKSPACE/build/src/vs2015u3/DIA SDK/bin/amd64:$WORKSPACE/build/src/mingw64/bin:$PATH"
+    ;;
+esac
+
+cd $WORKSPACE/build/src
+
+. taskcluster/scripts/misc/tooltool-download.sh
+
+PATH="$PWD/rustc/bin:$PATH"
+
+git clone https://github.com/mozilla/sccache sccache
+
+cd sccache
+
+git checkout $SCCACHE_REVISION
+
+cargo build --verbose --release
+
+mkdir sccache2
+cp target/release/sccache* sccache2/
+tar -acf sccache2.tar.$COMPRESS_EXT sccache2
+mkdir -p $UPLOAD_DIR
+cp sccache2.tar.$COMPRESS_EXT $UPLOAD_DIR
