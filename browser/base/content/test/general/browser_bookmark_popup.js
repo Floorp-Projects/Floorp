bcdb06bc1b9a1c5f2897a344c13f648f4e197b1d
Mark Banner <standard8@mozilla.com>
2017-07-17 09:59:41 +0100

    Bug 1336806 - In browser_bookmark_popup.js' hover tests, place the mouse in the middle of the panel to try and avoid intermittents. r=mak
    MozReview-Commit-ID: 3X89DkfWxQv
    
    --HG--
    extra : rebase_source : ad82ba9b6a0fdcab680dec54548055fe5b359c7d

--- browser/base/content/test/general/browser_bookmark_popup.js
+++ browser/base/content/test/general/browser_bookmark_popup.js
@@ -11,7 +11,7 @@
 let bookmarkPanel = document.getElementById("editBookmarkPanel");
 let bookmarkStar = AppConstants.MOZ_PHOTON_THEME ? BookmarkingUI.star : BookmarkingUI.button;
 let bookmarkPanelTitle = document.getElementById("editBookmarkPanelTitle");
-let editBookmarkPanelRemoveButtonRect;
+let editBookmarkPanelRect;
 
 StarUI._closePanelQuickForTesting = true;
 
@@ -40,8 +40,8 @@ async function test_bookmarks_popup({isNewBookmark, popupShowFn, popupEditFn,
       await shownPromise;
       Assert.equal(bookmarkPanel.state, "open", "Panel should be 'open' after shownPromise is resolved");
 
-    editBookmarkPanelRemoveButtonRect =
-      document.getElementById("editBookmarkPanelRemoveButton").getBoundingClientRect();
+    editBookmarkPanelRect =
+      document.getElementById("editBookmarkPanel").getBoundingClientRect();
 
       if (popupEditFn) {
         await popupEditFn();
@@ -386,8 +386,9 @@ add_task(async function mouse_hovering_panel_should_prevent_autoclose() {
       await new Promise(resolve => {
         EventUtils.synthesizeNativeMouseMove(
           document.documentElement,
-          editBookmarkPanelRemoveButtonRect.left,
-          editBookmarkPanelRemoveButtonRect.top,
+          // Locate the mouse in the middle of the panel.
+          (editBookmarkPanelRect.left + editBookmarkPanelRect.right) / 2,
+          (editBookmarkPanelRect.top + editBookmarkPanelRect.bottom) / 2,
           resolve);
       });
       EventUtils.synthesizeKey("D", {accelKey: true}, window);
