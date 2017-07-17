759140610ec61241235a49bc43b7915849ba7a91
Jonathan Guillotte-Blouin <jguillotteblouin@mozilla.com>
2017-07-17 13:29:21 -0700

    Bug 1381186 - open/close stub dialog on (show/abort)Payment. r=MattN
    MozReview-Commit-ID: K3YyFlIttjD
    
    --HG--
    extra : rebase_source : 97639e91e6d35ade1fa11cea5ae923fa522bc44a

--- toolkit/components/payments/paymentUIService.js
+++ toolkit/components/payments/paymentUIService.js
@@ -9,25 +9,57 @@ const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components;
 const DIALOG_URL = "chrome://payments/content/paymentRequest.xhtml";
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 XPCOMUtils.defineLazyServiceGetter(this,
                                    "paymentSrv",
                                    "@mozilla.org/dom/payments/payment-request-service;1",
                                    "nsIPaymentRequestService");
 
-function PaymentUIService() {}
+function defineLazyLogGetter(scope, logPrefix) {
+  XPCOMUtils.defineLazyGetter(scope, "log", () => {
+    let {ConsoleAPI} = Cu.import("resource://gre/modules/Console.jsm", {});
+    return new ConsoleAPI({
+      maxLogLevelPref: "dom.payments.loglevel",
+      prefix: logPrefix,
+    });
+  });
+}
+
+function PaymentUIService() {
+  defineLazyLogGetter(this, "Payment UI Service");
+  paymentSrv.setTestingUIService(this);
+  this.log.debug("constructor");
+}
 
 PaymentUIService.prototype = {
   classID: Components.ID("{01f8bd55-9017-438b-85ec-7c15d2b35cdc}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIPaymentUIService]),
+  REQUEST_ID_PREFIX: "paymentRequest-",
 
   showPayment(requestId) {
+    this.log.debug("showPayment");
+    let chromeWindow = Services.wm.getMostRecentWindow("navigator:browser");
+    chromeWindow.openDialog(DIALOG_URL,
+                            `${this.REQUEST_ID_PREFIX}${requestId}`,
+                            "modal,dialog,centerscreen");
   },
 
   abortPayment(requestId) {
+    this.log.debug(`abortPayment: ${requestId}`);
     let abortResponse = Cc["@mozilla.org/dom/payments/payment-abort-action-response;1"]
                           .createInstance(Ci.nsIPaymentAbortActionResponse);
     abortResponse.init(requestId, Ci.nsIPaymentActionResponse.ABORT_SUCCEEDED);
+
+    let enu = Services.wm.getEnumerator(null);
+    let win;
+    while ((win = enu.getNext())) {
+      if (win.name == `${this.REQUEST_ID_PREFIX}${requestId}`) {
+        this.log.debug(`closing: ${win.name}`);
+        win.close();
+        break;
+      }
+    }
     paymentSrv.respondPayment(abortResponse.QueryInterface(Ci.nsIPaymentActionResponse));
   },
 
