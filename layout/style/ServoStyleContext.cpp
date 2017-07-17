31277519ca1ce24519eca9c4f477b6fce9b62954
Manish Goregaokar <manishearth@gmail.com>
2017-07-17 11:42:24 -0700

    Bug 1367904 - Part 17: stylo: Clean up GetContext, call it UpdateWithElementState; r=bholley
    MozReview-Commit-ID: I23w5hFatk8

--- layout/style/ServoStyleContext.cpp
+++ layout/style/ServoStyleContext.cpp
@@ -8,6 +8,8 @@
 #include "nsStyleConsts.h"
 #include "nsStyleStruct.h"
 #include "nsPresContext.h"
+#include "nsCSSRuleProcessor.h"
+#include "mozilla/dom/HTMLBodyElement.h"
 
 #include "mozilla/ServoBindings.h"
 
@@ -28,4 +30,42 @@ ServoStyleContext::ServoStyleContext(nsStyleContext* aParent,
 
   // No need to call ApplyStyleFixups here, since fixups are handled by Servo when
   // producing the ServoComputedValues.
+}
+
+void
+ServoStyleContext::UpdateWithElementState(Element* aElementForAnimation)
+{
+  bool isLink = false;
+  bool isVisitedLink = false;
+  // If we need visited styles for callers where `aElementForAnimation` is null,
+  // we can precompute these and pass them as flags, similar to nsStyleSet.cpp.
+  if (aElementForAnimation) {
+    isLink = nsCSSRuleProcessor::IsLink(aElementForAnimation);
+    isVisitedLink = nsCSSRuleProcessor::GetContentState(aElementForAnimation)
+                                       .HasState(NS_EVENT_STATE_VISITED);
+  }
+
+
+  auto parent = GetParentAllowServo();
+
+  // The true visited state of the relevant link is used to decided whether
+  // visited styles should be consulted for all visited dependent properties.
+  bool relevantLinkVisited = isLink ? isVisitedLink :
+    (parent && parent->RelevantLinkVisited());
+
+  if (relevantLinkVisited && GetStyleIfVisited()) {
+    AddStyleBit(NS_STYLE_RELEVANT_LINK_VISITED);
+  }
+
+  // Set the body color on the pres context. See nsStyleSet::GetContext
+  if (aElementForAnimation &&
+      aElementForAnimation->IsHTMLElement(nsGkAtoms::body) &&
+      GetPseudoType() == CSSPseudoElementType::NotPseudo &&
+      mPresContext->CompatibilityMode() == eCompatibility_NavQuirks) {
+    nsIDocument* doc = aElementForAnimation->GetUncomposedDoc();
+    if (doc && doc->GetBodyElement() == aElementForAnimation) {
+      // Update the prescontext's body color
+      mPresContext->SetBodyTextColor(StyleColor()->mColor);
+    }
+  }
 }
