6c837fa0105b94c4c1ebe72c91d47b2e8aaccd9c
Ed Lee <edilee@mozilla.com>
2017-07-17 10:53:20 -0700

    Bug 1381569 - Turn on Activity Stream by default for Nightly. r=mconley
    MozReview-Commit-ID: Bsv1hIigZ0K

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1276,7 +1276,11 @@ pref("browser.newtabpage.columns", 5);
 pref("browser.newtabpage.directory.source", "https://tiles.services.mozilla.com/v3/links/fetch/%LOCALE%/%CHANNEL%");
 
 // activates Activity Stream
+#ifdef NIGHTLY_BUILD
+pref("browser.newtabpage.activity-stream.enabled", true);
+#else
 pref("browser.newtabpage.activity-stream.enabled", false);
+#endif
 
 // Enable the DOM fullscreen API.
 pref("full-screen-api.enabled", true);
