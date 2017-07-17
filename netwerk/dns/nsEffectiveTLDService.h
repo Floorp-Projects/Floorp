457b6daae879be2ae78eff2748b16ad2b70d868e
Eric Rahm <erahm@mozilla.com>
2017-07-17 16:10:18 -0700

    Bug 1380154 - Part 2: Generate a DAFSA and use it for eTLDs. r=jduell
    This replaces our giant sorted array of eTLD names with a more compact DAFSA.
    
    MozReview-Commit-ID: 3zMBzUM9QUg

--- netwerk/dns/nsEffectiveTLDService.h
+++ netwerk/dns/nsEffectiveTLDService.h
@@ -12,66 +12,10 @@
 #include "nsString.h"
 #include "nsCOMPtr.h"
 #include "mozilla/Attributes.h"
-#include "mozilla/BinarySearch.h"
+#include "mozilla/Dafsa.h"
 #include "mozilla/MemoryReporting.h"
 
 class nsIIDNService;
-
-// struct for static data generated from effective_tld_names.dat
-struct ETLDEntry {
-  friend class nsEffectiveTLDService;
-
-public:
-  bool IsNormal()    const { return wild || !exception; }
-  bool IsException() const { return exception; }
-  bool IsWild()      const { return wild; }
-
-  const char* GetEffectiveTLDName() const
-  {
-    return strings.strtab + strtab_index;
-  }
-
-  static const ETLDEntry* GetEntry(const char* aDomain);
-
-  static const size_t ETLD_ENTRY_N_INDEX_BITS = 30;
-
-  // These fields must be public to allow static construction.
-  uint32_t strtab_index : ETLD_ENTRY_N_INDEX_BITS;
-  uint32_t exception : 1;
-  uint32_t wild : 1;
-
-private:
-  struct Cmp {
-    int operator()(const ETLDEntry aEntry) const
-    {
-      return strcmp(mName, aEntry.GetEffectiveTLDName());
-    }
-    explicit Cmp(const char* aName) : mName(aName) {}
-    const char* mName;
-  };
-
-#define ETLD_STR_NUM_1(line) str##line
-#define ETLD_STR_NUM(line) ETLD_STR_NUM_1(line)
-  struct etld_string_list {
-#define ETLD_ENTRY(name, ex, wild) char ETLD_STR_NUM(__LINE__)[sizeof(name)];
-#include "etld_data.inc"
-#undef ETLD_ENTRY
-  };
-
-  // This static string table is all the eTLD domain names packed together.
-  static const union etld_strings {
-    struct etld_string_list list;
-    char strtab[1];
-  } strings;
-
-  // This is the static entries table. Each entry has an index into the string
-  // table. The entries are in sorted order so that binary search can be used.
-  static const ETLDEntry entries[];
-
-  void FuncForStaticAsserts(void);
-#undef ETLD_STR_NUM
-#undef ETLD_STR_NUM1
-};
 
 class nsEffectiveTLDService final
   : public nsIEffectiveTLDService
@@ -93,6 +37,7 @@ private:
   ~nsEffectiveTLDService();
 
   nsCOMPtr<nsIIDNService>     mIDNService;
+  mozilla::Dafsa mGraph;
 };
 
 #endif // EffectiveTLDService_h
