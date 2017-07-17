517a24a97054118f1f083385bf4098433a03c9bd
Sebastian Kaspari <s.kaspari@gmail.com>
2017-07-17 19:36:17 +0200

    [focus] Execute codecov on travis instead of buddybuild.

--- mobile/android/focus-android/buddybuild_postbuild.sh
+++ mobile/android/focus-android/buddybuild_postbuild.sh
@@ -4,12 +4,3 @@ set -e # Exit (and fail) immediately if any command in this scriptfails
 # Check that our APK files are not bigger than they should be
 python tools/metrics/apk_size.py
 
-# buddybuild modifies our buildscripts and sources (this is partly to enable
-# their SDK, and partly to allow selecting flavours in the BuddyBuild UI).
-# We don't know where the Buddybuild SDK lives, which causes gradle builds
-# to be broken (due to the SDK dependency injected into FocusApplication),
-# it's easiest just to revert to a clean source state here:
-git reset --hard
-
-./gradlew jacocoTestReport
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
