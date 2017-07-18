4e598f9195c22b63af79c9daf3713dbb689896c9
Simon Sapin <simon.sapin@exyr.org>
2017-07-18 08:58:11 +0200

    Bug 1344131 - Add a gtest bench for Servo_DeclarationBlock_SetPropertyById. r=emilio
    MozReview-Commit-ID: 83Ryi6CKTLV

--- layout/style/test/gtest/StyloParsingBench.cpp
+++ layout/style/test/gtest/StyloParsingBench.cpp
@@ -16,6 +16,7 @@ using namespace mozilla::css;
 using namespace mozilla::net;
 
 #define PARSING_REPETITIONS 20
+#define SETPROPERTY_REPETITIONS (1000 * 1000)
 
 #ifdef MOZ_STYLO
 
@@ -57,3 +58,33 @@ static void GeckoParsingBench() {
 }
 
 MOZ_GTEST_BENCH(Stylo, Gecko_nsCSSParser_ParseSheet_Bench, GeckoParsingBench);
+
+
+#ifdef MOZ_STYLO
+
+static void ServoSetPropertyByIdBench() {
+  RefPtr<RawServoDeclarationBlock> block = Servo_DeclarationBlock_CreateEmpty().Consume();
+  RefPtr<URLExtraData> data = new URLExtraData(
+    NullPrincipalURI::Create(), nullptr, NullPrincipal::Create());
+
+  NS_NAMED_LITERAL_CSTRING(css_, " 10px");
+  const nsACString& css = css_;
+  ASSERT_TRUE(IsUTF8(css));
+
+  for (int i = 0; i < SETPROPERTY_REPETITIONS; i++) {
+    Servo_DeclarationBlock_SetPropertyById(
+      block,
+      eCSSProperty_width,
+      &css,
+      /* is_important = */ false,
+      data,
+      ParsingMode::Default,
+      eCompatibility_FullStandards,
+      nullptr
+    );
+  }
+}
+
+MOZ_GTEST_BENCH(Stylo, Servo_DeclarationBlock_SetPropertyById_Bench, ServoSetPropertyByIdBench);
+
+#endif
