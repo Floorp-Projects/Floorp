e964060488e456abfd65762b52ada808f6c470f0
Tom Tung <shes050117@gmail.com>
2017-07-18 18:57:54 +0800

    Bug 1290481 - P8: Implement few utility functions to access direcotry padding file. r=bkelly
    MozReview-Commit-ID: KlVsaGhpABk

--- dom/cache/FileUtils.h
+++ dom/cache/FileUtils.h
@@ -19,6 +19,12 @@ namespace mozilla {
 namespace dom {
 namespace cache {
 
+enum DirPaddingFile
+{
+  FILE,
+  TMP_FILE
+};
+
 nsresult
 BodyCreateDir(nsIFile* aBaseDir);
 
@@ -77,6 +83,49 @@ RemoveNsIFile(const QuotaInfo& aQuotaInfo, nsIFile* aFile);
 void
 DecreaseUsageForQuotaInfo(const QuotaInfo& aQuotaInfo,
                           const int64_t& aUpdatingSize);
+
+/**
+ * This function is used to check if the directory padding file is existed.
+ */
+
+bool
+DirectoryPaddingFileExists(nsIFile* aBaseDir, DirPaddingFile aPaddingFileType);
+
+/**
+ *
+ * The functions below are used to read/write/delete the directory padding file
+ * after acquiring the mutex lock. The mutex lock is held by
+ * CacheQuotaClient to prevent multi-thread accessing issue. To avoid deadlock,
+ * these functions should only access by static functions in
+ * dom/cache/QuotaClient.cpp.
+ *
+ */
+
+nsresult
+LockedDirectoryPaddingGet(nsIFile* aBaseDir, int64_t* aPaddingSizeOut);
+
+nsresult
+LockedDirectoryPaddingInit(nsIFile* aBaseDir);
+
+nsresult
+LockedMaybeUpdateDirectoryPaddingFile(nsIFile* aBaseDir,
+                                      mozIStorageConnection* aConn,
+                                      const int64_t aIncreaseSize,
+                                      const int64_t aDecreaseSize,
+                                      bool* aUpdatedOut);
+
+nsresult
+LockedDirectoryPaddingTemporaryWrite(nsIFile* aBaseDir, int64_t aPaddingSize);
+
+nsresult
+LockedDirectoryPaddingFinalizeWrite(nsIFile* aBaseDir);
+
+nsresult
+LockedDirectoryPaddingRestore(nsIFile* aBaseDir, mozIStorageConnection* aConn);
+
+nsresult
+LockedDirectoryPaddingDeleteFile(nsIFile* aBaseDir,
+                                 DirPaddingFile aPaddingFileType);
 } // namespace cache
 } // namespace dom
 } // namespace mozilla
