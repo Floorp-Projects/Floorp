e83e83cb0786814fb1febeaa85217991b28d9e6d
Justin Wood <Callek@gmail.com>
2017-07-18 11:57:26 -0400

    Bug 1381577 - Part N; Make repackage task output its artifact to a locale included subdirectory, rather than rely on worker to publish there. r=kmoir Land date changes to support windows nightlies onto central
    This will be needed for windows, because if the artifact is output to a folder that is, itself, an artifact folder, we'll end up publishing the named artifact twice. Which is unhelpful.
    
    MozReview-Commit-ID: 5S6SNul6Fm9
    
    --HG--
    extra : rebase_source : 5501d8d757c7408c9dd014bc04a8d36429bdd9b5

--- testing/mozharness/configs/repackage/osx_signed.py
+++ testing/mozharness/configs/repackage/osx_signed.py
@@ -2,8 +2,10 @@ import os
 
 config = {
     "input_home": "{abs_work_dir}/inputs",
-    "output_home": "{abs_work_dir}/artifacts",
+    "output_home": "{abs_work_dir}/artifacts{locale}",
     "src_mozconfig": "browser/config/mozconfigs/macosx64/repack",
+
+    "locale": os.environ.get("LOCALE"),
 
     "download_config": {
         "target.tar.gz": os.environ.get("SIGNED_INPUT"),
