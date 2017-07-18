12d20c064be499662f42b1448fbce113eac45145
Zibi Braniecki <zbraniecki@mozilla.com>
2017-07-17 22:21:47 -0700

    Bug 1344625: Revert "Bug 1349879 - Intl object is hidden on Android beta and release channel". r=m_kato
    MozReview-Commit-ID: 3hMis1hhqag

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -39,7 +39,7 @@ var ecmaGlobals =
     "Int32Array",
     "Int8Array",
     "InternalError",
-    {name: "Intl", nonReleaseAndroid: true, android: false},
+    "Intl",
     "Iterator",
     "JSON",
     "Map",
@@ -262,9 +262,8 @@ function createInterfaceMap(version, userAgent) {
         ok(!("pref" in entry), "Bogus pref annotation for " + entry.name);
         if ((entry.nightly === !isNightly) ||
             (entry.nightlyAndroid === !(isAndroid && isNightly) && isAndroid) ||
-            (entry.nonReleaseAndroid === !(isAndroid && !isRelease) && isAndroid) ||
             (entry.desktop === !isDesktop) ||
-            (entry.android === !isAndroid && !entry.nonReleaseAndroid && !entry.nightlyAndroid) ||
+            (entry.android === !isAndroid && !entry.nightlyAndroid) ||
             (entry.release === !isRelease) ||
             (entry.isSecureContext === !isSecureContext) ||
             entry.disabled) {
