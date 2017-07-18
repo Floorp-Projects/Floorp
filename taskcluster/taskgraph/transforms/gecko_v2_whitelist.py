b4420a91fe9af2ebcd34b750d51306dc5875a25a
Justin Wood <Callek@gmail.com>
2017-07-18 13:39:59 -0400

    Bug 1381577 - Part P; Sign repackaged win artifacts. r=kmoir Land date changes to support windows nightlies onto central
    MozReview-Commit-ID: 9FbrSvyU9ny

--- taskcluster/taskgraph/transforms/gecko_v2_whitelist.py
+++ taskcluster/taskgraph/transforms/gecko_v2_whitelist.py
@@ -79,6 +79,7 @@ JOB_NAME_WHITELIST = set([
     'win32-clang-tidy',
     'win32-debug',
     'win32-nightly-repackage',
+    'win32-nightly-repackage-signing',
     'win32-noopt-debug',
     'win32-opt',
     'win32-pgo',
