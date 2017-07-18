c4978e80f539266438378f8f7c4af2d11d46ef7d
Tom Tung <shes050117@gmail.com>
2017-07-18 19:08:59 +0800

    Bug 1290481 - P14: Add a test to verify QuotaManager upgrade from version 2 to version 3. r=bkelly, janv
    MozReview-Commit-ID: KlVsaGhpABk
    
    --HG--
    extra : rebase_source : 465c2f8c45d3c7e5f2e3ac885b016ed15510bb9f

---
+++ dom/quota/test/unit/test_version3_0upgrade.js
@@ -0,0 +1,76 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+var testGenerator = testSteps();
+
+function* testSteps()
+{
+  const origins = [
+    "storage/default/chrome/",
+    "storage/default/http+++www.mozilla.org/"
+  ];
+  const paddingFilePath = "cache/.padding";
+
+  info("Clearing");
+
+  clear(continueToNextStepSync);
+  yield undefined;
+
+  // The profile contains two cache storages:
+  // - storage/default/chrome/cache,
+  // - storage/default/http+++www.mozilla.org/cache
+  // The file create_cache.js in the package was run locally, specifically it
+  // was temporarily added to xpcshell.ini and then executed:
+  //   mach xpcshell-test --interactive dom/quota/test/unit/create_cache.js
+  // Note: it only creates the directory "storage/default/chrome/cache".
+  // To make it become the profile in the test, two more manual steps are
+  // needed.
+  // 1. Remove the folder "storage/temporary".
+  // 2. Copy the content under the "storage/default/chrome" to
+  //    "storage/default/http+++www.mozilla.org".
+  installPackage("version3_0upgrade_profile");
+
+  info("Checking padding file before upgrade (QM version 2.0)");
+
+  for (let origin of origins) {
+    let paddingFile = getRelativeFile(origin + paddingFilePath);
+
+    let exists = paddingFile.exists();
+    ok(!exists, "Padding file doesn't exist");
+  }
+
+  info("Initializing");
+
+  // Initialize QuotaManager to trigger upgrade the QM to version 3.0
+  let request = init(continueToNextStepSync);
+  yield undefined;
+
+  ok(request.resultCode == NS_OK, "Initialization succeeded");
+
+  info("Checking padding files after upgrade (QM version 3.0)");
+
+  for (let origin of origins) {
+    let paddingFile = getRelativeFile(origin + paddingFilePath);
+
+    let exists = paddingFile.exists();
+    ok(exists, "Padding file does exist");
+
+    info("Reading out contents of padding file");
+
+    File.createFromNsIFile(paddingFile).then(grabArgAndContinueHandler);
+    let domFile = yield undefined;
+
+    let fileReader = new FileReader();
+    fileReader.onload = continueToNextStepSync;
+    fileReader.readAsArrayBuffer(domFile);
+    yield undefined;
+
+    let paddingFileInfo = new Float64Array(fileReader.result);
+    ok(paddingFileInfo.length == 1, "Padding file does take 64 bytes.");
+    ok(paddingFileInfo[0] == 0, "Padding size does default to zero.");
+  }
+
+  finishTest();
+}
