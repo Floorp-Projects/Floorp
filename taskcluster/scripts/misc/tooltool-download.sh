ecf1b81be826cdbccef432e2954e3e28e638e5e8
Mike Hommey <mh+mozilla@glandium.org>
2017-07-18 14:51:04 +0900

    Bug 1381770 - Enable multiple download retries in tooltool-download.sh. r=mshal
    The scripts that use tooltool-download.sh don't run regularly, but when
    they do, they might hit some download problems (the relengapi proxy
    tends to be rather unreliable for some reason), and in that case, it
    would be better to retry a few times, like other job types, rather than
    fail directly.

--- taskcluster/scripts/misc/tooltool-download.sh
+++ taskcluster/scripts/misc/tooltool-download.sh
@@ -5,6 +5,6 @@ cd $WORKSPACE/build/src
 : TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/home/worker/tooltool-cache}
 export TOOLTOOL_CACHE
 
-./mach artifact toolchain -v --tooltool-url=http://relengapi/tooltool/ --tooltool-manifest "${TOOLTOOL_MANIFEST}"${TOOLTOOL_CACHE:+ --cache-dir ${TOOLTOOL_CACHE}}
+./mach artifact toolchain -v --tooltool-url=http://relengapi/tooltool/ --tooltool-manifest "${TOOLTOOL_MANIFEST}"${TOOLTOOL_CACHE:+ --cache-dir ${TOOLTOOL_CACHE}} --retry 5
 
 cd $OLDPWD
