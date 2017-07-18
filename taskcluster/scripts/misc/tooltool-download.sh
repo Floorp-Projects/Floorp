d66c310fb739c6aaa0b79fca3fe9863c04e6ac0e
Mike Hommey <mh+mozilla@glandium.org>
2017-07-18 16:12:58 +0900

    Bug 1381770 - In tooltool-download.sh, only use the relengapi proxy when it's configured. r=mshal
    --HG--
    extra : rebase_source : 971e4eab1a5a04dc68ba566b2e963e3f61f53b82

--- taskcluster/scripts/misc/tooltool-download.sh
+++ taskcluster/scripts/misc/tooltool-download.sh
@@ -2,9 +2,16 @@
 
 cd $WORKSPACE/build/src
 
+TOOLTOOL_DL_FLAGS=
+
+if [ -n "$RELENGAPI_PORT" ]; then
+    # When the worker has the relengapi proxy setup, use it.
+    TOOLTOOL_DL_FLAGS="${TOOLTOOL_DL_FLAGS=} --tooltool-url=http://relengapi/tooltool/"
+fi
+
 : TOOLTOOL_CACHE                ${TOOLTOOL_CACHE:=/home/worker/tooltool-cache}
 export TOOLTOOL_CACHE
 
-./mach artifact toolchain -v --tooltool-url=http://relengapi/tooltool/ --tooltool-manifest "${TOOLTOOL_MANIFEST}"${TOOLTOOL_CACHE:+ --cache-dir ${TOOLTOOL_CACHE}} --retry 5
+./mach artifact toolchain -v${TOOLTOOL_DL_FLAGS} --tooltool-manifest "${TOOLTOOL_MANIFEST}"${TOOLTOOL_CACHE:+ --cache-dir ${TOOLTOOL_CACHE}} --retry 5
 
 cd $OLDPWD
