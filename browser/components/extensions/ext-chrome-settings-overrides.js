518592e0489907191b007bf46357d1bacfaba32d
Wes Kocher <wkocher@mozilla.com>
2017-07-18 22:44:06 -0700

    Backed out changeset c49af70f1a94 (bug 1378882) for eslint failures in ext-chrome-settings-overrides.js a=backout CLOSED TREE
    MozReview-Commit-ID: DAuBI2jFzG3

--- browser/components/extensions/ext-chrome-settings-overrides.js
+++ browser/components/extensions/ext-chrome-settings-overrides.js
@@ -4,30 +4,8 @@
 
 "use strict";
 
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-const {Management} = Cu.import("resource://gre/modules/Extension.jsm", {});
-
 XPCOMUtils.defineLazyModuleGetter(this, "ExtensionPreferencesManager",
                                   "resource://gre/modules/ExtensionPreferencesManager.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "ExtensionSettingsStore",
-                                  "resource://gre/modules/ExtensionSettingsStore.jsm");
-
-const LOCAL_STORE_TYPE = "dynamic_values";
-const DEFAULT_SEARCH_SETTING_NAME = "defaultSearch";
-
-const processDefaultSearchSetting = (extension, action) => {
-  let item = ExtensionSettingsStore[action](extension, LOCAL_STORE_TYPE, DEFAULT_SEARCH_SETTING_NAME);
-  if (["disable", "removeSetting"].includes(action) && item) {
-    try {
-      let engine = Services.search.getEngineByName(item.value || item.initialValue);
-      if (engine && Services.search.getDefaultEngines().includes(engine)) {
-        Services.search.currentEngine = engine;
-      }
-    } catch (e) {
-      Components.utils.reportError(e);
-    }
-  }
-};
 
 const searchInitialized = () => {
   return new Promise(resolve => {
@@ -46,31 +24,6 @@ const searchInitialized = () => {
   });
 };
 
-/* eslint-disable mozilla/balanced-listeners */
-Management.on("shutdown", async (type, extension) => {
-  switch (extension.shutdownReason) {
-    case "ADDON_DISABLE":
-    case "ADDON_DOWNGRADE":
-    case "ADDON_UPGRADE":
-      await searchInitialized();
-      processDefaultSearchSetting(extension, "disable");
-      break;
-
-    case "ADDON_UNINSTALL":
-      await searchInitialized();
-      processDefaultSearchSetting(extension, "removeSetting");
-      break;
-  }
-});
-
-Management.on("startup", async (type, extension) => {
-  if (["ADDON_ENABLE", "ADDON_UPGRADE", "ADDON_DOWNGRADE"].includes(extension.startupReason)) {
-    await searchInitialized();
-    processDefaultSearchSetting(extension, "enable");
-  }
-});
-/* eslint-enable mozilla/balanced-listeners */
-
 this.chrome_settings_overrides = class extends ExtensionAPI {
   async onManifestEntry(entryName) {
     let {extension} = this;
@@ -81,67 +34,38 @@ this.chrome_settings_overrides = class extends ExtensionAPI {
                                              manifest.chrome_settings_overrides.homepage);
     }
     if (manifest.chrome_settings_overrides.search_provider) {
-      let setDefault = false;
       await searchInitialized();
       let searchProvider = manifest.chrome_settings_overrides.search_provider;
-      if (searchProvider.is_default) {
-        let engineName = searchProvider.name.trim();
-        let engine = Services.search.getEngineByName(engineName);
-        if (engine && Services.search.getDefaultEngines().includes(engine)) {
-          await ExtensionSettingsStore.addSetting(
-            extension, LOCAL_STORE_TYPE, DEFAULT_SEARCH_SETTING_NAME, engineName, () => {
-              return Services.search.currentEngine.name;
-            });
-          setDefault = true;
-        } else {
-          Components.utils.reportError("is_default can only be used for built-in engines.")
+      let isCurrent = false;
+      let index = -1;
+      if (extension.startupReason === "ADDON_UPGRADE") {
+        let engines = Services.search.getEnginesByExtensionID(extension.id);
+        if (engines.length > 0) {
+          // There can be only one engine right now
+          isCurrent = Services.search.currentEngine == engines[0];
+          // Get position of engine and store it
+          index = Services.search.getEngines().indexOf(engines[0]);
+          Services.search.removeEngine(engines[0]);
         }
-      // If the setting exists for the extension, but is missing from the manifest,
-      // remove it
-      } else if (ExtensionSettingsStore.hasSetting(
-                 extension, LOCAL_STORE_TYPE, DEFAULT_SEARCH_SETTING_NAME)) {
-        await searchInitialized();
-        processDefaultSearchSetting(extension, "removeSetting");
       }
-      // We only process the rest of search_provider if we did not set
-      // the default engine
-      if (!setDefault) {
-        let isCurrent = false;
-        let index = -1;
+      try {
+        Services.search.addEngineWithDetails(searchProvider.name.trim(),
+                                             searchProvider.favicon_url,
+                                             searchProvider.keyword, null,
+                                             "GET", searchProvider.search_url,
+                                             extension.id);
         if (extension.startupReason === "ADDON_UPGRADE") {
-          let engines = Services.search.getEnginesByExtensionID(extension.id);
-          if (engines.length > 0) {
-            // There can be only one engine right now
-            isCurrent = Services.search.currentEngine == engines[0];
-            // Get position of engine and store it
-            index = Services.search.getEngines().indexOf(engines[0]);
-            Services.search.removeEngine(engines[0]);
+          let engine = Services.search.getEngineByName(searchProvider.name.trim());
+          if (isCurrent) {
+            Services.search.currentEngine = engine;
+          }
+          if (index != -1) {
+            Services.search.moveEngine(engine, index);
           }
         }
-        try {
-          Services.search.addEngineWithDetails(searchProvider.name.trim(),
-                                               searchProvider.favicon_url,
-                                               searchProvider.keyword, null,
-                                               "GET", searchProvider.search_url,
-                                               extension.id);
-          if (extension.startupReason === "ADDON_UPGRADE") {
-            let engine = Services.search.getEngineByName(searchProvider.name.trim());
-            if (isCurrent) {
-              Services.search.currentEngine = engine;
-            }
-            if (index != -1) {
-              Services.search.moveEngine(engine, index);
-            }
-          }
-        } catch (e) {
-          Components.utils.reportError(e);
-        }
+      } catch (e) {
+        Components.utils.reportError(e);
       }
-    }
-    let item = ExtensionSettingsStore.getSetting(LOCAL_STORE_TYPE, DEFAULT_SEARCH_SETTING_NAME);
-    if (item) {
-      await searchInitialized();
-      Services.search.currentEngine = Services.search.getEngineByName(item.value || item.initialValue);
     }
   }
   async onShutdown(reason) {
