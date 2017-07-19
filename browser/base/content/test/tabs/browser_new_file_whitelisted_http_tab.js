9f346cf6f0b811a9b89da6459ec80431564af84f
Bob Owen <bobowencode@gmail.com>
2017-07-19 09:37:25 +0100

    Bug 1378377 Part 3: Make sure domains whitelisted for file:// URI use run in file contest process. r=Gijs

---
+++ browser/base/content/test/tabs/browser_new_file_whitelisted_http_tab.js
@@ -0,0 +1,27 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+
+const TEST_HTTP = "http://example.org/";
+
+// Test for bug 1378377.
+add_task(async function() {
+  // Set prefs to ensure file content process.
+  await SpecialPowers.pushPrefEnv(
+    {set: [["browser.tabs.remote.separateFileUriProcess", true]]});
+
+  await BrowserTestUtils.withNewTab(TEST_HTTP, async function(fileBrowser) {
+    is(fileBrowser.remoteType, E10SUtils.WEB_REMOTE_TYPE,
+      "Check that tab normally has web remote type.");
+  });
+
+  // Set prefs to whitelist TEST_HTTP for file:// URI use.
+  await SpecialPowers.pushPrefEnv(
+    {set: [["capability.policy.policynames", "allowFileURI"],
+           ["capability.policy.allowFileURI.sites", TEST_HTTP],
+           ["capability.policy.allowFileURI.checkloaduri.enabled", "allAccess"]]});
+
+  await BrowserTestUtils.withNewTab(TEST_HTTP, async function(fileBrowser) {
+    is(fileBrowser.remoteType, E10SUtils.FILE_REMOTE_TYPE,
+      "Check that tab now has file remote type.");
+  });
+});
