8827f0633cc2ec99251f5eb5ee21eb5874b24ef9
Gabriel Luong <gabriel.luong@gmail.com>
2017-07-19 16:37:58 -0400

    Bug 1381689 - Upgrade to CodeMirror 5.27.4. r=bgrins

--- devtools/client/sourceeditor/codemirror/addon/fold/xml-fold.js
+++ devtools/client/sourceeditor/codemirror/addon/fold/xml-fold.js
@@ -21,8 +21,8 @@
   function Iter(cm, line, ch, range) {
     this.line = line; this.ch = ch;
     this.cm = cm; this.text = cm.getLine(line);
-    this.min = range ? range.from : cm.firstLine();
-    this.max = range ? range.to - 1 : cm.lastLine();
+    this.min = range ? Math.max(range.from, cm.firstLine()) : cm.firstLine();
+    this.max = range ? Math.min(range.to - 1, cm.lastLine()) : cm.lastLine();
   }
 
   function tagAt(iter, ch) {
@@ -163,10 +163,10 @@
     }
   };
 
-  CodeMirror.findEnclosingTag = function(cm, pos, range) {
+  CodeMirror.findEnclosingTag = function(cm, pos, range, tag) {
     var iter = new Iter(cm, pos.line, pos.ch, range);
     for (;;) {
-      var open = findMatchingOpen(iter);
+      var open = findMatchingOpen(iter, tag);
       if (!open) break;
       var forward = new Iter(cm, pos.line, pos.ch, range);
       var close = findMatchingClose(forward, open.tag);
