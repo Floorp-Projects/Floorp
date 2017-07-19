44b191ee4ae6d6b7b477e32a4e3c843c6ec89a3f
Kevin Chen <kechen@mozilla.com>
2017-07-18 23:09:00 -0400

    Bug 1364563 - Add a path for content process only device reset. r=dvander

--- gfx/layers/ipc/CrossProcessCompositorBridgeParent.h
+++ gfx/layers/ipc/CrossProcessCompositorBridgeParent.h
@@ -58,6 +58,8 @@ public:
   virtual mozilla::ipc::IPCResult RecvStartFrameTimeRecording(const int32_t& aBufferSize, uint32_t* aOutStartIndex) override { return IPC_OK(); }
   virtual mozilla::ipc::IPCResult RecvStopFrameTimeRecording(const uint32_t& aStartIndex, InfallibleTArray<float>* intervals) override  { return IPC_OK(); }
 
+  virtual mozilla::ipc::IPCResult RecvCheckContentOnlyTDR(const uint32_t& sequenceNum, bool* isContentOnlyTDR) override;
+
   virtual mozilla::ipc::IPCResult RecvClearApproximatelyVisibleRegions(const uint64_t& aLayersId,
                                                     const uint32_t& aPresShellId) override;
 
