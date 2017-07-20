205be9e0fd22488b32bb9d763ef531dc3b8c33ed
Shawn Huang <shuang@mozilla.com>
2017-07-20 15:10:30 +0800

    Bug 1222284 - Part 2: Replace setFinishedCallback with waitForMessage for browser tests, r=btseng
    For all tests depend on testFinishedCallback() function, the following had been changed:
    * waitForExplicitFinish() will be replaced with add_task().
    * gBrowser.selectedBrowser.addEventListener() will be replaced with waitForMessage().
    * testFinishedCallback, testResult, and testException will be unified to testResult.

--- dom/indexedDB/test/browser_permissionsPromptAllow.js
+++ dom/indexedDB/test/browser_permissionsPromptAllow.js
@@ -7,80 +7,56 @@ const testPageURL = "http://mochi.test:8888/browser/" +
   "dom/indexedDB/test/browser_permissionsPrompt.html";
 const notificationID = "indexedDB-permissions-prompt";
 
-function test()
-{
-  waitForExplicitFinish();
-
+add_task(async function test1() {
   // We want a prompt.
   removePermission(testPageURL, "indexedDB");
-  executeSoon(test1);
-}
 
-function test1()
-{
   info("creating tab");
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
 
-  gBrowser.selectedBrowser.addEventListener("load", function() {
-    setFinishedCallback(function(isIDBDatabase, exception) {
-      ok(isIDBDatabase,
-         "First database creation was successful");
-      ok(!exception, "No exception");
-      is(getPermission(testPageURL, "indexedDB"),
-         Components.interfaces.nsIPermissionManager.ALLOW_ACTION,
-         "Correct permission set");
-      gBrowser.removeCurrentTab();
-      executeSoon(test2);
-    });
-
-    registerPopupEventHandler("popupshowing", function() {
-      ok(true, "prompt showing");
-    });
-    registerPopupEventHandler("popupshown", function() {
-      ok(true, "prompt shown");
-      triggerMainCommand(this);
-    });
-    registerPopupEventHandler("popuphidden", function() {
-      ok(true, "prompt hidden");
-    });
-
-  }, {capture: true, once: true});
+  registerPopupEventHandler("popupshowing", function() {
+    ok(true, "prompt showing");
+  });
+  registerPopupEventHandler("popupshown", function() {
+    ok(true, "prompt shown");
+    triggerMainCommand(this);
+  });
+  registerPopupEventHandler("popuphidden", function() {
+    ok(true, "prompt hidden");
+  });
 
   info("loading test page: " + testPageURL);
-  content.location = testPageURL;
-}
+  gBrowser.selectedBrowser.loadURI(testPageURL);
 
-function test2()
-{
+  await waitForMessage(true, gBrowser);
+  is(getPermission(testPageURL, "indexedDB"),
+     Components.interfaces.nsIPermissionManager.ALLOW_ACTION,
+     "Correct permission set");
+  gBrowser.removeCurrentTab();
+});
+
+add_task(async function test2() {
   info("creating tab");
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
 
-  gBrowser.selectedBrowser.addEventListener("load", function() {
-    setFinishedCallback(function(isIDBDatabase, exception) {
-      ok(isIDBDatabase,
-         "First database creation was successful");
-      ok(!exception, "No exception");
-      is(getPermission(testPageURL, "indexedDB"),
-         Components.interfaces.nsIPermissionManager.ALLOW_ACTION,
-         "Correct permission set");
-      gBrowser.removeCurrentTab();
-      unregisterAllPopupEventHandlers();
-      removePermission(testPageURL, "indexedDB");
-      executeSoon(finish);
-    });
-
-    registerPopupEventHandler("popupshowing", function() {
-      ok(false, "Shouldn't show a popup this time");
-    });
-    registerPopupEventHandler("popupshown", function() {
-      ok(false, "Shouldn't show a popup this time");
-    });
-    registerPopupEventHandler("popuphidden", function() {
-      ok(false, "Shouldn't show a popup this time");
-    });
-
-  }, {capture: true, once: true});
+  registerPopupEventHandler("popupshowing", function() {
+    ok(false, "Shouldn't show a popup this time");
+  });
+  registerPopupEventHandler("popupshown", function() {
+    ok(false, "Shouldn't show a popup this time");
+  });
+  registerPopupEventHandler("popuphidden", function() {
+    ok(false, "Shouldn't show a popup this time");
+  });
 
   info("loading test page: " + testPageURL);
-  content.location = testPageURL;
-}
+  gBrowser.selectedBrowser.loadURI(testPageURL);
+
+  await waitForMessage(true, gBrowser);
+  is(getPermission(testPageURL, "indexedDB"),
+     Components.interfaces.nsIPermissionManager.ALLOW_ACTION,
+     "Correct permission set");
+  gBrowser.removeCurrentTab();
+  unregisterAllPopupEventHandlers();
+  removePermission(testPageURL, "indexedDB");
+});
