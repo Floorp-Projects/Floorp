3939600e86c366aeb7747eeb60c11aed2128467c
Justin Wood <Callek@gmail.com>
2017-07-20 10:29:26 -0400

    Bug 1381577 - Part AJ; Allow to override worker type in test definitions, supports explicitly buildbot-bridge tests. r=dustin Land date changes to support windows nightlies onto central
    MozReview-Commit-ID: 8AfCK50vRJc
    
    --HG--
    extra : rebase_source : 0183e58585b62d120d7ba1c9d0d48f8b500b88c5

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -330,6 +330,11 @@ test_description_schema = Schema({
         Optional('files-changed'): [basestring],
     }),
 
+    Optional('worker-type'): optionally_keyed_by(
+        'test-platform',
+        Any(basestring, None),
+    ),
+
 }, required=True)
 
 
@@ -540,6 +545,7 @@ def handle_keyed_by(config, tests):
         'mozharness.config',
         'mozharness.extra-options',
         'mozharness.requires-signed-builds',
+        'worker-type',
     ]
     for test in tests:
         for field in fields:
@@ -773,7 +779,10 @@ def set_worker_type(config, tests):
         # during the taskcluster migration, this is a bit tortured, but it
         # will get simpler eventually!
         test_platform = test['test-platform']
-        if test_platform.startswith('macosx'):
+        if test.get('worker-type'):
+            # This test already has its worker type defined, so just use that (yields below)
+            pass
+        elif test_platform.startswith('macosx'):
             # note that some portion of these will be allocated to BBB below
             test['worker-type'] = MACOSX_WORKER_TYPES['macosx64']
         elif test_platform.startswith('win'):
