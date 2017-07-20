7c157892222bcb4952b0cbf16204195ca25ef54a
L. David Baron <dbaron@dbaron.org>
2017-07-20 15:46:59 -0700

    Bug 1381080 patch 2 - Encapsulate setting to empty buffer in a new method.  r=erahm
    This is needed for patch 4.
    
    MozReview-Commit-ID: 7xKQv37cy5k
    
    --HG--
    extra : transplant_source : %ED%16%85q%7F%E7%117%29%D2c%C2%21%20%D7%C9%BA%CCC%CC

--- xpcom/string/nsTSubstring.cpp
+++ xpcom/string/nsTSubstring.cpp
@@ -688,9 +688,7 @@ nsTSubstring_CharT::SetCapacity(size_type aCapacity, const fallible_t&)
   // if our capacity is reduced to zero, then free our buffer.
   if (aCapacity == 0) {
     ::ReleaseData(mData, mDataFlags);
-    mData = char_traits::sEmptyBuffer;
-    mLength = 0;
-    mDataFlags = DataFlags::TERMINATED;
+    SetToEmptyBuffer();
     return true;
   }
 
