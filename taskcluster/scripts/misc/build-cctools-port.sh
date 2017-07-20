daaefe8df99e04582fc103904de2155bd5c0d3b4
Mike Hommey <mh+mozilla@glandium.org>
2017-07-21 07:30:14 +0900

    Bug 1382860 - Use docker_worker_add_public_artifacts for toolchain docker-worker jobs. r=dustin
    This affects the location of the artifacts directory, so adjust the
    scripts and artifact definitions as a consequence.

--- taskcluster/scripts/misc/build-cctools-port.sh
+++ taskcluster/scripts/misc/build-cctools-port.sh
@@ -4,7 +4,7 @@ set -x -e -v
 # This script is for building cctools (Apple's binutils) for Linux using
 # cctools-port (https://github.com/tpoechtrager/cctools-port).
 WORKSPACE=$HOME/workspace
-UPLOAD_DIR=$WORKSPACE/artifacts
+UPLOAD_DIR=$HOME/artifacts
 
 # Repository info
 : CROSSTOOL_PORT_REPOSITORY    ${CROSSTOOL_PORT_REPOSITORY:=https://github.com/tpoechtrager/cctools-port}
