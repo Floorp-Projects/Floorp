3b7d119417a733c568c174cf79e2306997ac9018
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-07-20 18:28:20 +0200

    Backed out changeset cf72ce758a61 (bug 1381554) for failing browser_ext_popup_corners.js, at least on OS X opt. r=backout

--- addon-sdk/source/lib/sdk/panel/utils.js
+++ addon-sdk/source/lib/sdk/panel/utils.js
@@ -254,6 +254,10 @@ function setupPanelFrame(frame) {
   frame.setAttribute("transparent", "transparent");
   frame.setAttribute("autocompleteenabled", true);
   frame.setAttribute("tooltip", "aHTMLTooltip");
+  if (platform === "darwin") {
+    frame.style.borderRadius = "var(--arrowpanel-border-radius, 3.5px)";
+    frame.style.padding = "1px";
+  }
 }
 
 function make(document, options) {
