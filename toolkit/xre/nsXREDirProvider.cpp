2a757d9ae7db28170a7d0016f18c9076e82aeada
Benjamin Smedberg <benjamin@smedbergs.us>
2017-07-20 16:19:39 -0400

    Bug 1382817 - Prefs shouldn't start out dirty or be written almost immediately after being read, r=milan
    MozReview-Commit-ID: BFLMsMQjn9w
    
    --HG--
    extra : rebase_source : 9e9ba20270b055e8b1e3400c8926b7c4c932d752

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -1026,6 +1026,8 @@ nsXREDirProvider::GetDirectory(nsIFile* *aResult)
 NS_IMETHODIMP
 nsXREDirProvider::DoStartup()
 {
+  nsresult rv;
+
   if (!mProfileNotified) {
     nsCOMPtr<nsIObserverService> obsSvc =
       mozilla::services::GetObserverService();
@@ -1038,8 +1040,7 @@ nsXREDirProvider::DoStartup()
        crashes and because we want to begin crash tracking before other code run
        from this notification since they may cause crashes.
     */
-    nsresult rv = mozilla::Preferences::ResetAndReadUserPrefs();
-    if (NS_FAILED(rv)) NS_WARNING("Failed to setup pref service.");
+    mozilla::Preferences::InitializeUserPrefs();
 
     bool safeModeNecessary = false;
     nsCOMPtr<nsIAppStartup> appStartup (do_GetService(NS_APPSTARTUP_CONTRACTID));
