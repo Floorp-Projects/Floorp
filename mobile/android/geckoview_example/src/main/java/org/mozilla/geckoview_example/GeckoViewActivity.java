37e37382dc6037516f6dc16dc202359ed5c097e7
Jim Chen <nchen@mozilla.com>
2017-07-20 17:52:14 -0400

    Bug 1322586 - 5. Implement PermissionDelegate for geckoview_example; r=droeh
    Add a sample implementation of PermissionDelegate for geckoview_example;
    Because the prompt code has some existing boilerplate, the actual
    prompts are implemented in BasicGeckoViewPrompt.
    
    MozReview-Commit-ID: EDfmRPn4cjR
    
    --HG--
    extra : rebase_source : 3b4b770c606a9be3326bca3acd87770c5f8bb4fa

--- mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
+++ mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
@@ -7,12 +7,16 @@ package org.mozilla.geckoview_example;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.WindowManager;
+
+import java.util.Locale;
 
 import org.mozilla.gecko.GeckoView;
 import org.mozilla.gecko.GeckoViewSettings;
@@ -24,6 +28,7 @@ public class GeckoViewActivity extends Activity {
     private static final String USE_MULTIPROCESS_EXTRA = "use_multiprocess";
 
     /* package */ static final int REQUEST_FILE_PICKER = 1;
+    private static final int REQUEST_PERMISSIONS = 2;
 
     private GeckoView mGeckoView;
 
@@ -61,6 +66,10 @@ public class GeckoViewActivity extends Activity {
         prompt.filePickerRequestCode = REQUEST_FILE_PICKER;
         mGeckoView.setPromptDelegate(prompt);
 
+        final MyGeckoViewPermission permission = new MyGeckoViewPermission();
+        permission.androidPermissionRequestCode = REQUEST_PERMISSIONS;
+        mGeckoView.setPermissionDelegate(permission);
+
         loadFromIntent(getIntent());
     }
 
@@ -92,6 +101,19 @@ public class GeckoViewActivity extends Activity {
             prompt.onFileCallbackResult(resultCode, data);
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode,
+                                           final String[] permissions,
+                                           final int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSIONS) {
+            final MyGeckoViewPermission permission = (MyGeckoViewPermission)
+                    mGeckoView.getPermissionDelegate();
+            permission.onRequestPermissionsResult(permissions, grantResults);
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
@@ -141,6 +163,110 @@ public class GeckoViewActivity extends Activity {
                 statusString = "unknown";
             }
             Log.i(LOGTAG, "Security status changed to " + statusString);
+        }
+    }
+
+    private class MyGeckoViewPermission implements GeckoView.PermissionDelegate {
+
+        public int androidPermissionRequestCode = 1;
+        private Callback mCallback;
+
+        public void onRequestPermissionsResult(final String[] permissions,
+                                               final int[] grantResults) {
+            if (mCallback == null) {
+                return;
+            }
+
+            final Callback cb = mCallback;
+            mCallback = null;
+            for (final int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    // At least one permission was not granted.
+                    cb.reject();
+                    return;
+                }
+            }
+            cb.grant();
+        }
+
+        @Override
+        public void requestAndroidPermissions(final GeckoView view, final String[] permissions,
+                                              final Callback callback) {
+            if (Build.VERSION.SDK_INT < 23) {
+                // requestPermissions was introduced in API 23.
+                callback.grant();
+                return;
+            }
+            mCallback = callback;
+            requestPermissions(permissions, androidPermissionRequestCode);
+        }
+
+        @Override
+        public void requestContentPermission(final GeckoView view, final String uri,
+                                             final String type, final String access,
+                                             final Callback callback) {
+            final int resId;
+            if ("geolocation".equals(type)) {
+                resId = R.string.request_geolocation;
+            } else if ("desktop-notification".equals(type)) {
+                resId = R.string.request_notification;
+            } else {
+                Log.w(LOGTAG, "Unknown permission: " + type);
+                callback.reject();
+                return;
+            }
+
+            final String title = getString(resId, Uri.parse(uri).getAuthority());
+            final BasicGeckoViewPrompt prompt = (BasicGeckoViewPrompt)
+                    mGeckoView.getPromptDelegate();
+            prompt.promptForPermission(view, title, callback);
+        }
+
+        private void normalizeMediaName(final GeckoBundle[] sources) {
+            if (sources == null) {
+                return;
+            }
+            for (final GeckoBundle source : sources) {
+                final String mediaSource = source.getString("mediaSource");
+                String name = source.getString("name");
+                if ("camera".equals(mediaSource)) {
+                    if (name.toLowerCase(Locale.ENGLISH).contains("front")) {
+                        name = getString(R.string.media_front_camera);
+                    } else {
+                        name = getString(R.string.media_back_camera);
+                    }
+                } else if (!name.isEmpty()) {
+                    continue;
+                } else if ("microphone".equals(mediaSource)) {
+                    name = getString(R.string.media_microphone);
+                } else {
+                    name = getString(R.string.media_other);
+                }
+                source.putString("name", name);
+            }
+        }
+
+        @Override
+        public void requestMediaPermission(final GeckoView view, final String uri,
+                                           final GeckoBundle[] video,
+                                           final GeckoBundle[] audio,
+                                           final MediaCallback callback) {
+            final String host = Uri.parse(uri).getAuthority();
+            final String title;
+            if (audio == null) {
+                title = getString(R.string.request_video, host);
+            } else if (video == null) {
+                title = getString(R.string.request_audio, host);
+            } else {
+                title = getString(R.string.request_media, host);
+            }
+
+            normalizeMediaName(video);
+            normalizeMediaName(audio);
+
+            final BasicGeckoViewPrompt prompt = (BasicGeckoViewPrompt)
+                    mGeckoView.getPromptDelegate();
+            prompt.promptForMedia(view, title, video, audio, callback);
         }
     }
 }
