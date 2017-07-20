942fbeb215b516800c5991232dc9f6da75d4884a
Mike Hommey <mh+mozilla@glandium.org>
2017-07-21 07:30:14 +0900

    Bug 1382860 - Use docker_worker_add_public_artifacts for toolchain docker-worker jobs. r=dustin
    This affects the location of the artifacts directory, so adjust the
    scripts and artifact definitions as a consequence.

--- taskcluster/scripts/misc/build-hfsplus-linux.sh
+++ taskcluster/scripts/misc/build-hfsplus-linux.sh
@@ -4,7 +4,7 @@ set -x -e -v
 # This script is for building hfsplus for Linux.
 WORKSPACE=$HOME/workspace
 HOME_DIR=$WORKSPACE/build
-UPLOAD_DIR=$WORKSPACE/artifacts
+UPLOAD_DIR=$HOME/artifacts
 
 cd $HOME_DIR/src
 
