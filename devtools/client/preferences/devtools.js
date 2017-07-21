8d526d873ef6c27625882d732f68aa5cae435aa9
Julian Descottes <jdescottes@mozilla.com>
2017-07-21 12:27:35 +0200

    Bug 1369801 - dt-addon-prefs: move jsonview enabled pref outside of devtools addon;r=ochameau
    DevTools preferences are loaded dynamically by calling DevtoolsPreferences.loadPrefs().
    We can not preload them when the addon starts, otherwise this will slow down the startup
    of Firefox.
    
    But jsonview's converter-observer needs to check preferences to check if jsonview is
    enabled very early. Moving devtools.jsonview.enabled to a separate preferences file
    that is still processed by firefox fixes the issue.
    
    The downside is that this pref will keep following m-c's release cycle and not the addon's.
    But it is so generic it should not be a big issue.
    
    
    MozReview-Commit-ID: HrD5IVe54Ks

--- devtools/client/preferences/devtools.js
+++ devtools/client/preferences/devtools.js
@@ -352,8 +352,5 @@ pref("devtools.editor.autocomplete", true);
 // version for each user.
 pref("devtools.telemetry.tools.opened.version", "{}");
 
-// Enable the JSON View tool (an inspector for application/json documents).
-pref("devtools.jsonview.enabled", true);
-
 // Enable the HTML responsive design mode for all channels.
 pref("devtools.responsive.html.enabled", true);
