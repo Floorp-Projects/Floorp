fe6e6017b79b3fa43e44e6929d43902ec9423977
Julian Descottes <jdescottes@mozilla.com>
2017-07-21 16:05:19 +0200

    Bug 1369801 - dt-addon-prefs: load DevTools prefs when starting Loader.jsm;r=ochameau
    DevTools should not execute any code on the browser startup. Loading preferences takes
    a non negligeable time and should be deferred to the devtools initialization.
    
    For all devtools entry points, Loader.jsm is always loaded first, so it is safe to
    load the preferences here.
    
    MozReview-Commit-ID: Hg4VBj2LqPo

--- devtools/bootstrap.js
+++ devtools/bootstrap.js
@@ -10,10 +10,6 @@
 const Cu = Components.utils;
 const {Services} = Cu.import("resource://gre/modules/Services.jsm", {});
 
-XPCOMUtils.defineLazyGetter(this, "DevToolsPreferences", function () {
-  return Cu.import("chrome://devtools/content/preferences/DevToolsPreferences.jsm", {}).DevToolsPreferences;
-});
-
 function unload(reason) {
   // This frame script is going to be executed in all processes:
   // parent and child
@@ -50,10 +46,7 @@ function unload(reason) {
   Cu.unload("resource://devtools/shared/deprecated-sync-thenables.js");
 }
 
-function startup(data) {
-  // Load DevToolsPreferences as early as possible.
-  DevToolsPreferences.loadPrefs();
-}
+function startup(data) {}
 
 function shutdown(data, reason) {
   // On browser shutdown, do not try to cleanup anything
