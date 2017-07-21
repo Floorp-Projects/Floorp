9654eabe7f3588826e3447fcddf5868d6cb0087a
Cameron McCormack <cam@mcc.id.au>
2017-07-21 11:42:42 +0800

    Bug 1380133 - Part 1: Minor reformatting and encapsulation. r=emilio
    MozReview-Commit-ID: 3hmptLbxxok
    ---
     dom/animation/KeyframeUtils.h      |  2 +-
     layout/style/ServoBindings.cpp     | 24 +++++++++++++++---------
     layout/style/ServoStyleContext.cpp | 21 ++++++++++++---------
     layout/style/ServoStyleContext.h   | 33 ++++++++++++++-------------------
     layout/style/ServoTypes.h          | 24 ++++++++++++++----------
     layout/style/nsAnimationManager.h  |  7 +++----
     6 files changed, 59 insertions(+), 52 deletions(-)
    
    --HG--
    extra : rebase_source : 150278b5f7429986e79976ec9faf290db6de86c0

--- layout/style/ServoBindings.cpp
+++ layout/style/ServoBindings.cpp
@@ -209,21 +209,27 @@ Gecko_DestroyAnonymousContentList(nsTArray<nsIContent*>* aAnonContent)
 }
 
 void
-Gecko_ServoStyleContext_Init(ServoStyleContext* aContext,
-                             const ServoStyleContext* aParentContext,
-                             RawGeckoPresContextBorrowed aPresContext, const ServoComputedValues* aValues,
-                             mozilla::CSSPseudoElementType aPseudoType, nsIAtom* aPseudoTag)
+Gecko_ServoStyleContext_Init(
+    ServoStyleContext* aContext,
+    const ServoStyleContext* aParentContext,
+    RawGeckoPresContextBorrowed aPresContext,
+    const ServoComputedValues* aValues,
+    mozilla::CSSPseudoElementType aPseudoType,
+    nsIAtom* aPseudoTag)
 {
   // because it is within an Arc it is unsafe for the Rust side to ever
   // carry around a mutable non opaque reference to the context, so we
   // cast it here.
-  ServoStyleContext* parent = const_cast<ServoStyleContext*>(aParentContext);
-  nsPresContext* pres = const_cast<nsPresContext*>(aPresContext);
-  new (KnownNotNull, aContext) ServoStyleContext(parent, pres, aPseudoTag,
-                                                 aPseudoType, ServoComputedValuesForgotten(aValues));
+  auto parent = const_cast<ServoStyleContext*>(aParentContext);
+  auto presContext = const_cast<nsPresContext*>(aPresContext);
+  new (KnownNotNull, aContext) ServoStyleContext(
+      parent, presContext, aPseudoTag, aPseudoType,
+      ServoComputedValuesForgotten(aValues));
 }
 
-ServoComputedValues::ServoComputedValues(const ServoComputedValuesForgotten aValue) {
+ServoComputedValues::ServoComputedValues(
+    const ServoComputedValuesForgotten aValue)
+{
   PodAssign(this, aValue.mPtr);
 }
 
