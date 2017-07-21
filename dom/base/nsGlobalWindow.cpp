59a3c8b33b88af887b00d6585ab2e896247bfc2d
Wes Kocher <wkocher@mozilla.com>
2017-07-21 15:21:32 -0700

    Backed out changeset cd615bff6069 (bug 1375119) for android mochitest failures in test_webaudioNotification.html a=backout
    MozReview-Commit-ID: 4XQnvJ9Qr7R

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -4425,11 +4425,6 @@ nsPIDOMWindowInner::SyncStateFromParentWindow()
 bool
 nsPIDOMWindowInner::IsPlayingAudio()
 {
-  for (uint32_t i = 0; i < mAudioContexts.Length(); i++) {
-    if (mAudioContexts[i]->IsRunning()) {
-      return true;
-    }
-  }
   RefPtr<AudioChannelService> acs = AudioChannelService::Get();
   if (!acs) {
     return false;
