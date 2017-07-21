72e31a0fc227fd5b1d33ba450771575583690d16
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-07-21 15:19:31 +0200

    Backed out changeset 140948f5b955 (bug 1366294)

--- ipc/chromium/src/base/histogram.h
+++ ipc/chromium/src/base/histogram.h
@@ -455,7 +455,7 @@ class CustomHistogram : public Histogram {
   virtual ClassType histogram_type() const;
 
  protected:
-  explicit CustomHistogram(const std::vector<Sample>& custom_ranges);
+  CustomHistogram(const std::vector<Sample>& custom_ranges);
 
   // Initialize ranges_ mapping.
   void InitializedCustomBucketRange(const std::vector<Sample>& custom_ranges);
