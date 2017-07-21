1a9ba697c8552d5beb405ff0c32c5c12bfd375d2
Andrea Marchesini <amarchesini@mozilla.com>
2017-07-21 16:51:53 +0200

    Bug 1382768 - Performance API must be a memory reporter, r=njn

--- dom/performance/Performance.h
+++ dom/performance/Performance.h
@@ -11,6 +11,7 @@
 #include "mozilla/DOMEventTargetHelper.h"
 #include "nsCOMPtr.h"
 #include "nsDOMNavigationTiming.h"
+#include "nsIMemoryReporter.h"
 
 class nsITimedChannel;
 class nsIHttpChannel;
@@ -33,9 +34,13 @@ class WorkerPrivate;
 
 // Base class for main-thread and worker Performance API
 class Performance : public DOMEventTargetHelper
+                  , public nsIMemoryReporter
 {
+  MOZ_DEFINE_MALLOC_SIZE_OF(MallocSizeOf)
+
 public:
   NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIMEMORYREPORTER
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(Performance,
                                            DOMEventTargetHelper)
 
