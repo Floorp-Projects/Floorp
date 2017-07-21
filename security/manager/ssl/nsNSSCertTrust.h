e3018d572d5f7f945762907c94e8115f490f1076
David Keeler <dkeeler@mozilla.com>
2017-07-21 16:58:42 -0700

    bug 1382866 - prompt for authentication when changing certificate trust fails r=Cykesiopka,jcj
    MozReview-Commit-ID: 3ryUyAfbNCs
    
    --HG--
    rename : security/manager/ssl/tests/unit/test_certDB_import.js => security/manager/ssl/tests/unit/test_certDB_import_with_master_password.js
    extra : rebase_source : ad39258078c1084166f0c59250352cd899b10507

--- security/manager/ssl/nsNSSCertTrust.h
+++ security/manager/ssl/nsNSSCertTrust.h
@@ -57,8 +57,7 @@ public:
   /* set p <--> P */
   void AddPeerTrust(bool ssl, bool email, bool objSign);
 
-  /* get it (const?) (shallow?) */
-  CERTCertTrust * GetTrust() { return &mTrust; }
+  CERTCertTrust& GetTrust() { return mTrust; }
 
 private:
   void addTrust(unsigned int *t, unsigned int v);
