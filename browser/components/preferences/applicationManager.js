dd190f936028fb2da6c6e5e18006f98204238138
Florian Quèze <florian@queze.net>
2017-07-23 00:18:00 +0200

    Bug 1381853 - Make browser mochitests ignore global properties added by XPCOMUtils.defineLazyScriptGetter, r=Mossop.

--- browser/components/preferences/applicationManager.js
+++ browser/components/preferences/applicationManager.js
@@ -17,10 +17,12 @@ var gAppManagerDialog = {
     // Remove this once we use the in-content-new preferences page.
     var pane;
     if (Services.prefs.getBoolPref("browser.preferences.useOldOrganization")) {
-      Services.scriptloader.loadSubScript("chrome://browser/content/preferences/in-content/applications.js");
+      Services.scriptloader.loadSubScript("chrome://browser/content/preferences/in-content/applications.js",
+                                          window);
       pane = gApplicationsPane;
     } else {
-      Services.scriptloader.loadSubScript("chrome://browser/content/preferences/in-content-new/main.js");
+      Services.scriptloader.loadSubScript("chrome://browser/content/preferences/in-content-new/main.js",
+                                          window);
       pane = gMainPane;
     }
     var bundle = document.getElementById("appManagerBundle");
