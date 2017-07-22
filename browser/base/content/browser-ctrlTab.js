867641202000b68c9cbadc8f0046e0dcfe3cda8a
Florian Quèze <florian@queze.net>
2017-07-23 00:17:57 +0200

    Bug 1381853 - lazy load browser-ctrlTab.js into the browser window, r=mconley.

--- browser/base/content/browser-ctrlTab.js
+++ browser/base/content/browser-ctrlTab.js
@@ -1,7 +1,9 @@
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file is loaded into the browser window scope.
+/* eslint-env mozilla/browser-window */
 
 /**
  * Tab previews utility, produces thumbnails
