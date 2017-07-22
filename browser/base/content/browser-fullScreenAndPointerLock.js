50fea258c71165fd0a4e3bde49ed7d444ccc377d
Florian Quèze <florian@queze.net>
2017-07-23 00:17:57 +0200

    Bug 1381853 - lazy load browser-fullScreenAndPointerLock.js into the browser window, r=mconley.

--- browser/base/content/browser-fullScreenAndPointerLock.js
+++ browser/base/content/browser-fullScreenAndPointerLock.js
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file is loaded into the browser window scope.
+/* eslint-env mozilla/browser-window */
+
 var PointerlockFsWarning = {
 
   _element: null,
