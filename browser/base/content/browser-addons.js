f0035bea93a4236b31448b000316add8e16bfdcc
Florian Quèze <florian@queze.net>
2017-07-23 00:17:57 +0200

    Bug 1381853 - lazy load browser-addons.js into the browser window, r=mconley.

--- browser/base/content/browser-addons.js
+++ browser/base/content/browser-addons.js
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file is loaded into the browser window scope.
+/* eslint-env mozilla/browser-window */
+
 // Removes a doorhanger notification if all of the installs it was notifying
 // about have ended in some way.
 function removeNotificationOnEnd(notification, installs) {
@@ -29,7 +32,7 @@ function removeNotificationOnEnd(notification, installs) {
   }
 }
 
-const gXPInstallObserver = {
+var gXPInstallObserver = {
   _findChildShell(aDocShell, aSoughtShell) {
     if (aDocShell == aSoughtShell)
       return aDocShell;
@@ -482,7 +485,7 @@ const gXPInstallObserver = {
   }
 };
 
-const gExtensionsNotifications = {
+var gExtensionsNotifications = {
   initialized: false,
   init() {
     this.updateAlerts();
