8f87702a2a44b9936a9180e74067c652a15abe22
Kaku Kuo <kaku@mozilla.com>
2017-07-24 13:29:04 +0800

    Bug 1383610 - turn off preference on desktop version release channel but keep the Fennec on; r=bwu
    MozReview-Commit-ID: 38RLaR3Vxeh

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -635,6 +635,9 @@ pref("media.eme.enabled", true);
 pref("media.hls.enabled", true);
 #endif
 
+// Whether to suspend decoding of videos in background tabs.
+pref("media.suspend-bkgnd-video.enabled", true);
+
 // optimize images memory usage
 pref("image.downscale-during-decode.enabled", true);
 
