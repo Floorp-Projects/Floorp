b9212a110d77d6d3f1c7475437e11d0fea12a6bc
Doug Thayer <dothayer@mozilla.com>
2017-07-24 16:29:20 -0700

    Bug 1383904 - Show old OS X elevation dialog on elevation fail r=rstrong, a=rkothari The patch which originally disabled this seems to have been in error, thinking it was the cause of failing tests on try. Can't provide much more context than that.

--- toolkit/mozapps/update/nsUpdateService.js
+++ toolkit/mozapps/update/nsUpdateService.js
@@ -3879,7 +3879,6 @@ UpdatePrompt.prototype = {
    */
   showUpdateElevationRequired: function UP_showUpdateElevationRequired() {
     if (getPref("getBoolPref", PREF_APP_UPDATE_SILENT, false) ||
-        getPref("getBoolPref", PREF_APP_UPDATE_DOORHANGER, false) ||
         this._getAltUpdateWindow()) {
       return;
     }
