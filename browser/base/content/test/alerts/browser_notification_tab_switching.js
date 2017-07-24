823f61b3a629c9f402636c3d6ea408c6b4d282e5
Wes Kocher <wkocher@mozilla.com>
2017-07-24 16:13:28 -0700

    Backed out changeset 7e0990dca8d1 (bug 1380065) at developer's request a=backout
    MozReview-Commit-ID: HTBdXaEmagI

--- browser/base/content/test/alerts/browser_notification_tab_switching.js
+++ browser/base/content/test/alerts/browser_notification_tab_switching.js
@@ -9,13 +9,6 @@ var notification;
 var notificationURL = "http://example.org/browser/browser/base/content/test/alerts/file_dom_notifications.html";
 var newWindowOpenedFromTab;
 
-// run these tests with notification animations enabled
-add_task(async function setup() {
-  await SpecialPowers.pushPrefEnv({
-    "set": [["toolkit.cosmeticAnimations.enabled", true]]
-  });
-});
-
 add_task(async function test_notificationPreventDefaultAndSwitchTabs() {
   let pm = Services.perms;
   pm.add(makeURI(notificationURL), "desktop-notification", pm.ALLOW_ACTION);
