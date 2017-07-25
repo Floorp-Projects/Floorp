573123f3092967430f5275217ac75edb6d9053de
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-07-25 14:33:53 +0200

    Backed out changeset 61637891db5f (bug 1377158)

--- image/VectorImage.h
+++ image/VectorImage.h
@@ -72,8 +72,7 @@ public:
   virtual void ReportUseCounters() override;
 
 protected:
-  explicit VectorImage(ImageURL* aURI = nullptr,
-                       StyleBackendType aStyleBackendType = StyleBackendType::None);
+  explicit VectorImage(ImageURL* aURI = nullptr);
   virtual ~VectorImage();
 
   virtual nsresult StartAnimation() override;
@@ -124,7 +123,7 @@ private:
                                           // (Only set after mIsFullyLoaded.)
   bool           mHasPendingInvalidation; // Invalidate observers next refresh
                                           // driver tick.
-  StyleBackendType mStyleBackendType;     // The style backend used for this image document.
+
   friend class ImageFactory;
 };
 
