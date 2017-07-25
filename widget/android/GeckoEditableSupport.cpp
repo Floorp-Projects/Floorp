c3a8629e4f22bc66a081495fb5fee741596d6bb2
Jim Chen <nchen@mozilla.com>
2017-07-25 15:32:53 -0400

    Bug 1351170 - Don't crash on selection exception in Beta; r=me
    Don't crash when we encounter a selection exception when in Beta. Still
    crash when in Nightly so we can investigate the source of the crash.
    r=me for trivial patch.
    
    MozReview-Commit-ID: HgmudX4VfWO

--- widget/android/GeckoEditableSupport.cpp
+++ widget/android/GeckoEditableSupport.cpp
@@ -664,7 +664,11 @@ GeckoEditableSupport::FlushIMEChanges(FlushChangesFlag aFlags)
             FlushIMEText(FLUSH_FLAG_RECOVER);
         } else {
             // Give up because we've already tried.
+#ifdef RELEASE_OR_BETA
+            env->ExceptionClear();
+#else
             MOZ_CATCH_JNI_EXCEPTION(env);
+#endif
         }
         return true;
     };
