5015483142fd64ab66574ff7f23df02b0308774e
Julian Descottes <jdescottes@mozilla.com>
2017-07-25 16:45:08 +0200

    Bug 1386620 - dt-addon-prefs: move devtools.theme preference to devtools-startup-prefs.js;r=bgrins
    MozReview-Commit-ID: GOWh7COmCSx

--- devtools/shim/devtools-startup-prefs.js
+++ devtools/shim/devtools-startup-prefs.js
@@ -10,3 +10,10 @@
 
 // Enable the JSON View tool (an inspector for application/json documents).
 pref("devtools.jsonview.enabled", true);
+
+// Default theme ("dark" or "light")
+#ifdef MOZ_DEV_EDITION
+sticky_pref("devtools.theme", "dark");
+#else
+sticky_pref("devtools.theme", "light");
+#endif
