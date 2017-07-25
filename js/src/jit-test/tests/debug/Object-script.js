7625ad9ec81f389970b56bd5ee55d76df7cccb77
Jim Blandy <jimb@mozilla.com>
2017-07-25 10:48:28 -0700

    Bug 1382258 - Handle async functions properly in Debugger API. r=tromey
    An async function or generator is represented by a native function wrapped
    around a scripted function. In order to avoid treating async functions as if
    they were opaque native functions, Debugger must explicitly look through these
    wrappers to find the scripted function inside.

--- js/src/jit-test/tests/debug/Object-script.js
+++ js/src/jit-test/tests/debug/Object-script.js
@@ -4,10 +4,22 @@ var hits = 0;
 dbg.onDebuggerStatement = function (frame) {
     var arr = frame.arguments;
     assertEq(arr[0].script instanceof Debugger.Script, true);
-    assertEq(arr[1].script, undefined);
-    assertEq(arr[2].script, undefined);
+    assertEq(arr[1].script instanceof Debugger.Script, true);
+    assertEq(arr[2].script instanceof Debugger.Script, true);
+    assertEq(arr[3].script instanceof Debugger.Script, true);
+    assertEq(arr[4].script, undefined);
+    assertEq(arr[5].script, undefined);
+    assertEq(arr.length, 6);
     hits++;
 };
 
-g.eval("(function () { debugger; })(function g(){}, {}, Math.atan2);");
+g.eval(`
+    function f() { debugger; }
+    f(function g(){},
+      function* h() {},
+      async function j() {},
+      async function* k() {},
+      {},
+      Math.atan2);
+`);
 assertEq(hits, 1);
