256f1ec05a17859f40c134644011771743a66309
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-07-25 10:52:58 +0200

    Backed out changeset 29131feeed60 (bug 1380065) for windows 7 perma failure in test_bug884693.xul | Got expected message count - got 1, expected +0

--- browser/base/content/test/performance/browser_tabopen_squeeze_reflows.js
+++ browser/base/content/test/performance/browser_tabopen_squeeze_reflows.js
@@ -17,13 +17,6 @@ const EXPECTED_REFLOWS = [
   ],
 ];
 
-// run these tests with notification animations enabled
-add_task(async function setup() {
-  await SpecialPowers.pushPrefEnv({
-    "set": [["toolkit.cosmeticAnimations.enabled", true]]
-  });
-});
-
 /*
  * This test ensures that there are no unexpected
  * uninterruptible reflows when opening a new tab that will
