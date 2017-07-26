3bacd995dc7314b57582dfc525c0a04a124ffef0
David Anderson <danderson@mozilla.com>
2017-07-25 22:38:33 -0700

    Don't assert about borrowed DT refcounts when async painting. (bug 1381393, r=mattwoodrow)

--- gfx/layers/CompositorTypes.h
+++ gfx/layers/CompositorTypes.h
@@ -252,7 +252,12 @@ enum class OpenMode : uint8_t {
   OPEN_WRITE       = 0x2,
   OPEN_READ_WRITE  = OPEN_READ|OPEN_WRITE,
   OPEN_READ_ONLY   = OPEN_READ,
-  OPEN_WRITE_ONLY  = OPEN_WRITE
+  OPEN_WRITE_ONLY  = OPEN_WRITE,
+
+  // This is only used in conjunction with OMTP to indicate that the DrawTarget
+  // that is being borrowed will be painted asynchronously, and so will outlive
+  // the write lock.
+  OPEN_ASYNC_WRITE = 0x04
 };
 MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(OpenMode)
 
