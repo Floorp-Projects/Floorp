ff63e4ac21531cc96288b6db199dba9973988ff0
Andrea Marchesini <amarchesini@mozilla.com>
2017-07-26 15:55:51 +0200

    Bug 1379568 - Filtering for StorageEvent broadcasting, r=asuth

--- dom/storage/StorageNotifierService.h
+++ dom/storage/StorageNotifierService.h
@@ -24,6 +24,12 @@ public:
                              const char16_t* aStorageType,
                              bool aPrivateBrowsing) = 0;
 
+  virtual bool
+  IsPrivateBrowsing() const = 0;
+
+  virtual nsIPrincipal*
+  GetPrincipal() const = 0;
+
   virtual nsIEventTarget*
   GetEventTarget() const = 0;
 };
