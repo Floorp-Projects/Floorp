81324a6669e2192e714b22ca053480e968919fd4
Kate Ustiuzhanina <kustiuzhanina@mozilla.com>
2017-07-26 11:48:21 +0100

    Bug 1374270 - Support sending health ping on shutdown via PingSender. r=Dexter

--- toolkit/components/telemetry/tests/unit/test_TelemetryHealthPing.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryHealthPing.js
@@ -41,6 +41,7 @@ add_task(async function setup() {
   await TelemetryController.testSetup();
   PingServer.start();
   TelemetrySend.setServer("http://localhost:" + PingServer.port);
+  Preferences.set(TelemetryUtils.Preferences.Server, "http://localhost:" + PingServer.port);
 });
 
 add_task(async function test_sendImmediately() {
@@ -177,6 +178,38 @@ add_task(async function test_sendOnlyTopTenDiscardedPings() {
       [PING_TYPE + 2]: 1
     }
   });
+});
+
+add_task(async function test_usePingSenderOnShutdown() {
+  TelemetryHealthPing.testReset();
+  PingServer.clearRequests();
+
+  // This first failure should immediately trigger a ping.
+  // After this, subsequent failures should be throttled.
+  await TelemetryHealthPing.recordSendFailure("testFailure");
+  await PingServer.promiseNextPing();
+
+  TelemetryHealthPing.recordSendFailure("testFailure");
+  let nextRequest = PingServer.promiseNextRequest();
+
+  await TelemetryController.testReset();
+  await TelemetryController.testShutdown();
+  let request = await nextRequest;
+  let ping = decodeRequestPayload(request);
+
+  checkHealthPingStructure(ping, {
+    [TelemetryHealthPing.FailureType.SEND_FAILURE]: {
+      "testFailure": 1
+    },
+    "os": TelemetryHealthPing.OsInfo,
+    "reason": TelemetryHealthPing.Reason.SHUT_DOWN
+  });
+
+  // Check that the health ping is sent at shutdown using the pingsender.
+  Assert.equal(request.getHeader("User-Agent"), "pingsender/1.0",
+    "Should have received the correct user agent string.");
+  Assert.equal(request.getHeader("X-PingSender-Version"), "1.0",
+    "Should have received the correct PingSender version string.");
 });
 
 add_task(async function cleanup() {
