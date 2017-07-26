0c78f092add052329e035eaa820f01c46b14257e
Botond Ballo <botond@mozilla.com>
2017-07-26 19:32:57 -0400

    Bug 1105109 - Notify content when APZ is handling an autoscroll. r=kats
    MozReview-Commit-ID: BeuZt30fMpn

--- gfx/layers/apz/util/APZCCallbackHelper.cpp
+++ gfx/layers/apz/util/APZCCallbackHelper.cpp
@@ -959,6 +959,18 @@ APZCCallbackHelper::NotifyAsyncScrollbarDragRejected(const FrameMetrics::ViewID&
 }
 
 /* static */ void
+APZCCallbackHelper::NotifyAutoscrollHandledByAPZ(const FrameMetrics::ViewID& aScrollId)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  nsCOMPtr<nsIObserverService> observerService = mozilla::services::GetObserverService();
+  MOZ_ASSERT(observerService);
+
+  nsAutoString data;
+  data.AppendInt(aScrollId);
+  observerService->NotifyObservers(nullptr, "autoscroll-handled-by-apz", data.get());
+}
+
+/* static */ void
 APZCCallbackHelper::NotifyPinchGesture(PinchGestureInput::PinchGestureType aType,
                                        LayoutDeviceCoord aSpanChange,
                                        Modifiers aModifiers,
