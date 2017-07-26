69950dcd35e55ff9d8116ae8b5bdbc5bdc7b27b0
Michael Comella <michael.l.comella@gmail.com>
2017-07-26 15:32:14 -0700

    Bug 1377287 - review: Only update views if they're the same as when asyncTask began. r=mcomella
    MozReview-Commit-ID: 8XA32VFNSH7

--- mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/topsites/TopSitesCard.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/topsites/TopSitesCard.java
@@ -7,6 +7,7 @@ package org.mozilla.gecko.activitystream.homepanel.topsites;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.UiThread;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -30,6 +31,7 @@ import org.mozilla.gecko.util.ViewUtil;
 import org.mozilla.gecko.widget.FaviconView;
 
 import java.lang.ref.WeakReference;
+import java.util.UUID;
 import java.util.concurrent.Future;
 
 /* package-local */ class TopSitesCard extends RecyclerView.ViewHolder
@@ -128,18 +130,25 @@ import java.util.concurrent.Future;
 
     /** Updates the text of the given view to the page domain. */
     private static class UpdateCardTitleAsyncTask extends URIUtils.GetHostSecondLevelDomainAsyncTask {
+        private static final int VIEW_TAG_ID = R.id.title; // same as the view.
+
         private final WeakReference<TextView> titleViewWeakReference;
+        private final UUID viewTagAtStart;
 
         UpdateCardTitleAsyncTask(final Context contextReference, final String uriString, final TextView titleView) {
             super(contextReference, uriString);
             this.titleViewWeakReference = new WeakReference<>(titleView);
+
+            // See isTagSameAsStartTag for details.
+            viewTagAtStart = UUID.randomUUID();
+            titleView.setTag(VIEW_TAG_ID, viewTagAtStart);
         }
 
         @Override
         protected void onPostExecute(final String hostSLD) {
             super.onPostExecute(hostSLD);
             final TextView titleView = titleViewWeakReference.get();
-            if (titleView == null) {
+            if (titleView == null || !isTagSameAsStartTag(titleView)) {
                 return;
             }
 
@@ -148,6 +157,15 @@ import java.util.concurrent.Future;
             // We use consistent padding all around the title, and the top padding is never modified,
             // so we can pass that in as the default padding:
             ViewUtil.setCenteredText(titleView, updateText, titleView.getPaddingTop());
+        }
+
+        /**
+         * Returns true if the tag on the given view matches the tag from the constructor. We do this to ensure
+         * the View we're making this request for hasn't been re-used by the time this request completes.
+         */
+        @UiThread
+        private boolean isTagSameAsStartTag(final View viewToCheck) {
+            return viewTagAtStart.equals(viewToCheck.getTag(VIEW_TAG_ID));
         }
     }
 }
