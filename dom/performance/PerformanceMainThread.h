3e5e18e8e00ef416939a6b5061e7dfcefe3a1f84
Ryan VanderMeulen <ryanvm@gmail.com>
2017-07-25 21:50:06 -0400

    Backed out changesets f4378a5e6c7f and b7d81fea0b33 (bug 1159003) for causing bug 1383553.

--- dom/performance/PerformanceMainThread.h
+++ dom/performance/PerformanceMainThread.h
@@ -13,17 +13,15 @@ namespace mozilla {
 namespace dom {
 
 class PerformanceMainThread final : public Performance
-                                  , public nsIObserver
 {
 public:
+  PerformanceMainThread(nsPIDOMWindowInner* aWindow,
+                        nsDOMNavigationTiming* aDOMTiming,
+                        nsITimedChannel* aChannel);
+
   NS_DECL_ISUPPORTS_INHERITED
-  NS_DECL_NSIOBSERVER
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(PerformanceMainThread,
                                                          Performance)
-
-  static already_AddRefed<PerformanceMainThread>
-  Create(nsPIDOMWindowInner* aWindow, nsDOMNavigationTiming* aDOMTiming,
-         nsITimedChannel* aChannel);
 
   virtual PerformanceTiming* Timing() override;
 
@@ -49,14 +47,13 @@ public:
     return mChannel;
   }
 
-  void Shutdown() override;
-
 protected:
-  PerformanceMainThread(nsPIDOMWindowInner* aWindow,
-                        nsDOMNavigationTiming* aDOMTiming,
-                        nsITimedChannel* aChannel);
-
   ~PerformanceMainThread();
+
+  nsISupports* GetAsISupports() override
+  {
+    return this;
+  }
 
   void InsertUserEntry(PerformanceEntry* aEntry) override;
 
