197103663aea9b4ee59e52978a79fb3014883eaa
Mihai Tabara <mtabara@mozilla.com>
2017-07-26 11:59:10 +0100

    Bug 1383121 - fix username in shipit builder in automation configs. r=aselagea a=release DONTBUILD
    MozReview-Commit-ID: 2wnRalWOXCm

--- testing/mozharness/configs/releases/dev_postrelease_firefox_release.py
+++ testing/mozharness/configs/releases/dev_postrelease_firefox_release.py
@@ -18,5 +18,5 @@ config = {
     "ssh_user": "ffxbld",
     "ssh_key": "~/.ssh/ffxbld_rsa",
     "ship_it_root": "https://ship-it-dev.allizom.org",
-    "ship_it_username":  "stage-ffxbld",
+    "ship_it_username":  "ship_it-stage-ffxbld",
 }
