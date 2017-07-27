b8f4b23d0c9bd8229c8b5760135be45358bb59bf
Shawn Huang <shuang@mozilla.com>
2017-07-27 17:50:13 +0800

    Bug 1362354 - Send abort event if window.stop() is called, r=baku
    Per XHR 4.5.6, end-user abort, it is required to send abort event.
    This fixed test XMLHttpRequest/abort-after-stop.htm.

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -2237,6 +2237,14 @@ XMLHttpRequestMainThread::OnStopRequest(nsIRequest *request, nsISupports *ctxt, 
     return NS_OK;
   }
 
+  if (status == NS_BINDING_ABORTED &&
+      ((mState == State::opened && mFlagSend) ||
+        mState == State::headers_received ||
+        mState == State::loading)) {
+    // Make sure we send event abort if termination reason is end-user abort.
+    Abort();
+  }
+
   mWaitingForOnStopRequest = false;
 
   if (mRequestObserver) {
