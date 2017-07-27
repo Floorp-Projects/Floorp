5d5a9cecb185683f13c1f29399461485ff458b6f
Brian Stack <bstack@mozilla.com>
2017-07-27 10:47:37 -0700

    Bug 1384729 - Move actions into taskgraph dir r=dustin
    MozReview-Commit-ID: HK5MR8Xw3On

--- taskcluster/taskgraph/actions/retrigger.py
+++ taskcluster/taskgraph/actions/retrigger.py
@@ -1,4 +1,12 @@
-from registry import register_task_action
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from .registry import register_task_action
 
 
 @register_task_action(
