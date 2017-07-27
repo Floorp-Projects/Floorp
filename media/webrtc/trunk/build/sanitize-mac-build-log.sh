e023dd622360e8f002b67489cb92f75f152e4e24
Dan Minor <dminor@mozilla.com>
2017-07-27 12:42:30 -0400

    Bug 1393119 - Add webrtc.org trunk/build/ files to support gn build; r=jesup
    MozReview-Commit-ID: KaupPBeJucK

--- media/webrtc/trunk/build/sanitize-mac-build-log.sh
+++ media/webrtc/trunk/build/sanitize-mac-build-log.sh
@@ -2,5 +2,4 @@
 # Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-sed -f `dirname "${0}"`/`basename "${0}" sh`sed
-
+sed -r -f `dirname "${0}"`/`basename "${0}" sh`sed
