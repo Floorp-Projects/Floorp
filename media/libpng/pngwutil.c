8e5226d344bd849c6a71f508aa413352db70a024
Glenn Randers-Pehrson <glennrp+bmo@gmail.com>
2017-07-27 11:42:00 -0400

    Bug 1377118 - Update in-tree libpng to version 1.6.31. r=jrmuizel
    --HG--
    extra : rebase_source : 72613717bc7380d217f64ea6ae1191563bdedf4a

--- media/libpng/pngwutil.c
+++ media/libpng/pngwutil.c
@@ -1008,13 +1008,16 @@ png_compress_IDAT(png_structrp png_ptr, png_const_bytep input,
                optimize_cmf(data, png_image_size(png_ptr));
 #endif
 
+         if (size > 0)
 #ifdef PNG_WRITE_APNG_SUPPORTED
-         if (png_ptr->num_frames_written == 0)
+         {
+            if (png_ptr->num_frames_written == 0)
 #endif
-         png_write_complete_chunk(png_ptr, png_IDAT, data, size);
+            png_write_complete_chunk(png_ptr, png_IDAT, data, size);
 #ifdef PNG_WRITE_APNG_SUPPORTED
-         else
-            png_write_fdAT(png_ptr, data, size);
+            else
+               png_write_fdAT(png_ptr, data, size);
+         }
 #endif /* WRITE_APNG */
 
          png_ptr->mode |= PNG_HAVE_IDAT;
@@ -1062,13 +1065,16 @@ png_compress_IDAT(png_structrp png_ptr, png_const_bytep input,
             optimize_cmf(data, png_image_size(png_ptr));
 #endif
 
+         if (size > 0)
 #ifdef PNG_WRITE_APNG_SUPPORTED
-         if (png_ptr->num_frames_written == 0)
+         {
+            if (png_ptr->num_frames_written == 0)
 #endif
-         png_write_complete_chunk(png_ptr, png_IDAT, data, size);
+            png_write_complete_chunk(png_ptr, png_IDAT, data, size);
 #ifdef PNG_WRITE_APNG_SUPPORTED
-         else
-            png_write_fdAT(png_ptr, data, size);
+            else
+               png_write_fdAT(png_ptr, data, size);
+         }
 #endif /* WRITE_APNG */
 
          png_ptr->zstream.avail_out = 0;
@@ -1489,6 +1495,37 @@ png_write_bKGD(png_structrp png_ptr, png_const_color_16p back, int color_type)
       png_save_uint_16(buf, back->gray);
       png_write_complete_chunk(png_ptr, png_bKGD, buf, (png_size_t)2);
    }
+}
+#endif
+
+#ifdef PNG_WRITE_eXIf_SUPPORTED
+/* Write the Exif data */
+void /* PRIVATE */
+png_write_eXIf(png_structrp png_ptr, png_bytep exif, int num_exif)
+{
+   int i;
+   png_byte buf[3];
+
+   png_debug(1, "in png_write_eXIf");
+
+   if (num_exif > (int)png_ptr->num_exif)
+   {
+      png_debug2(3, "num_exif = %d, png_ptr->num_exif = %d", num_exif,
+          png_ptr->num_exif);
+
+      png_warning(png_ptr, "Invalid number of exif bytes specified");
+      return;
+   }
+
+   png_write_chunk_header(png_ptr, png_eXIf, (png_uint_32)(num_exif));
+
+   for (i = 0; i < num_exif; i++)
+   {
+      buf[i] = exif[i];
+      png_write_chunk_data(png_ptr, buf, (png_size_t)1);
+   }
+
+   png_write_chunk_end(png_ptr);
 }
 #endif
 
