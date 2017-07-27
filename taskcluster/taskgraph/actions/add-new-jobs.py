b3691dbdc8d453fc4a5c5f9f387ad6a62eb380be
Brian Stack <bstack@mozilla.com>
2017-07-27 10:47:37 -0700

    Bug 1384729 - Move actions into taskgraph dir r=dustin
    MozReview-Commit-ID: HK5MR8Xw3On
    
    --HG--
    rename : taskcluster/actions/__init__.py => taskcluster/taskgraph/actions/__init__.py
    rename : taskcluster/actions/add-new-jobs.py => taskcluster/taskgraph/actions/add-new-jobs.py
    rename : taskcluster/actions/hello-action.py => taskcluster/taskgraph/actions/hello-action.py
    rename : taskcluster/actions/registry.py => taskcluster/taskgraph/actions/registry.py
    rename : taskcluster/actions/retrigger.py => taskcluster/taskgraph/actions/retrigger.py
    rename : taskcluster/actions/run_missing_tests.py => taskcluster/taskgraph/actions/run_missing_tests.py
    rename : taskcluster/actions/test-retrigger-action.py => taskcluster/taskgraph/actions/test-retrigger-action.py
    rename : taskcluster/actions/util.py => taskcluster/taskgraph/actions/util.py
    extra : rebase_source : 679650244290855bb05b3151bca1d704afa9e552

--- taskcluster/taskgraph/actions/add-new-jobs.py
+++ taskcluster/taskgraph/actions/add-new-jobs.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
 from .registry import register_callback_action
 from slugid import nice as slugid
 
-from actions.util import create_task
+from .util import create_task
 from taskgraph.util.taskcluster import get_artifact
 from taskgraph.util.parameterization import resolve_task_references
 from taskgraph.taskgraph import TaskGraph
