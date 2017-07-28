dc0a14020eecd87cdc7de1e2e74fc7f4cc7b82ed
Ray Lin <ralin@mozilla.com>
2017-07-28 17:23:30 +0800

    Bug 1371149 - Part 2. Add a chrome browser test for form autofill insecure field. r=MattN,seanlee
    MozReview-Commit-ID: 7KjI9siHlqt
    
    --HG--
    extra : rebase_source : fefa91b72a89a600f3337b3ec0beadcd77ed7bd2

--- browser/extensions/formautofill/test/browser/head.js
+++ browser/extensions/formautofill/test/browser/head.js
@@ -1,7 +1,9 @@
 /* exported MANAGE_ADDRESSES_DIALOG_URL, EDIT_ADDRESS_DIALOG_URL, BASE_URL,
             TEST_ADDRESS_1, TEST_ADDRESS_2, TEST_ADDRESS_3,
-            sleep, expectPopupOpen, openPopupOn, clickDoorhangerButton,
-            getAddresses, saveAddress, removeAddresses */
+            TEST_CREDIT_CARD_1, TEST_CREDIT_CARD_2, TEST_CREDIT_CARD_3,
+            sleep, expectPopupOpen, openPopupOn, expectPopupClose, closePopup, clickDoorhangerButton,
+            getAddresses, saveAddress, removeAddresses, saveCreditCard,
+            getDisplayedPopupItems */
 
 "use strict";
 
@@ -33,22 +35,51 @@ const TEST_ADDRESS_3 = {
   "postal-code": "12345",
 };
 
+const TEST_CREDIT_CARD_1 = {
+  "cc-name": "John Doe",
+  "cc-number": "1234567812345678",
+  // "cc-number-encrypted": "",
+  "cc-exp-month": 4,
+  "cc-exp-year": 2017,
+};
+
+const TEST_CREDIT_CARD_2 = {
+  "cc-name": "Timothy Berners-Lee",
+  "cc-number": "1111222233334444",
+  "cc-exp-month": 12,
+  "cc-exp-year": 2022,
+};
+
+const TEST_CREDIT_CARD_3 = {
+  "cc-number": "9999888877776666",
+  "cc-exp-month": 1,
+  "cc-exp-year": 2000,
+};
+
 const MAIN_BUTTON_INDEX = 0;
 const SECONDARY_BUTTON_INDEX = 1;
+
+function getDisplayedPopupItems(browser, selector = ".autocomplete-richlistitem") {
+  const {autoCompletePopup: {richlistbox: itemsBox}} = browser;
+  const listItemElems = itemsBox.querySelectorAll(selector);
+
+  return [...listItemElems].filter(item => item.getAttribute("collapsed") != "true");
+}
 
 async function sleep(ms = 500) {
   await new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function expectPopupOpen(browser) {
-  const {autoCompletePopup, autoCompletePopup: {richlistbox: itemsBox}} = browser;
-  const listItemElems = itemsBox.querySelectorAll(".autocomplete-richlistitem");
+  const {autoCompletePopup} = browser;
+  const listItemElems = getDisplayedPopupItems(browser);
 
   await BrowserTestUtils.waitForCondition(() => autoCompletePopup.popupOpen,
                                          "popup should be open");
   await BrowserTestUtils.waitForCondition(() => {
     return [...listItemElems].every(item => {
       return (item.getAttribute("originaltype") == "autofill-profile" ||
+             item.getAttribute("originaltype") == "insecureWarning" ||
              item.getAttribute("originaltype") == "autofill-footer") &&
              item.hasAttribute("formautofillattached");
     });
@@ -58,12 +89,37 @@ async function expectPopupOpen(browser) {
 async function openPopupOn(browser, selector) {
   await SimpleTest.promiseFocus(browser);
   /* eslint no-shadow: ["error", { "allow": ["selector"] }] */
-  await ContentTask.spawn(browser, {selector}, async function({selector}) {
-    content.document.querySelector(selector).focus();
+  const identified = await ContentTask.spawn(browser, {selector}, async function({selector}) {
+    const input = content.document.querySelector(selector);
+    const forms = content.document.getElementsByTagName("form");
+    const rootElement = [...forms].find(form => form.contains(input)) || content.document.body;
+
+    input.focus();
+    if (rootElement.hasAttribute("test-formautofill-identified")) {
+      return true;
+    }
+    rootElement.setAttribute("test-formautofill-identified", "true");
+    return false;
   });
-  await sleep(2000);
+  // Wait 2 seconds for identifyAutofillFields if the form hasn't been identified yet.
+  if (!identified) {
+    await sleep(2000);
+  }
   await BrowserTestUtils.synthesizeKey("VK_DOWN", {}, browser);
   await expectPopupOpen(browser);
+}
+
+async function expectPopupClose(browser) {
+  await BrowserTestUtils.waitForCondition(() => !browser.autoCompletePopup.popupOpen,
+    "popup should have closed");
+}
+
+async function closePopup(browser) {
+  await ContentTask.spawn(browser, {}, async function() {
+    content.document.activeElement.blur();
+  });
+
+  await expectPopupClose(browser);
 }
 
 function getRecords(data) {
@@ -85,6 +141,10 @@ function saveAddress(address) {
   return TestUtils.topicObserved("formautofill-storage-changed");
 }
 
+function saveCreditCard(creditcard) {
+  Services.cpmm.sendAsyncMessage("FormAutofill:SaveCreditCard", {creditcard});
+  return TestUtils.topicObserved("formautofill-storage-changed");
+}
 function removeAddresses(guids) {
   Services.cpmm.sendAsyncMessage("FormAutofill:RemoveAddresses", {guids});
   return TestUtils.topicObserved("formautofill-storage-changed");
