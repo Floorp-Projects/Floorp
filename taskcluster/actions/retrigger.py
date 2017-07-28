63821a23f8108551b722cfcfc4921aca3bfc484e
Wes Kocher <wkocher@mozilla.com>
2017-07-28 11:45:38 -0700

    Backed out changeset 9368bd804513 (bug 1384729) for build failures in test_mozbuild_reading.py a=backout CLOSED TREE
    MozReview-Commit-ID: 2etJcpEm0ye

--- taskcluster/actions/retrigger.py
+++ taskcluster/actions/retrigger.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from __future__ import absolute_import, print_function, unicode_literals
-
-from .registry import register_task_action
+from registry import register_task_action
 
 
 @register_task_action(
