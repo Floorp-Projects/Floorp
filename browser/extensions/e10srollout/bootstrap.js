208b2221248aec32f7e520bc0e3727edcd1683d8
Blake Kaplan <mrbkap@gmail.com>
2017-07-28 16:28:39 -0700

    Bug 1385491 - Call setCohort after setting e10s prefs. r=Felipe
    Due to bug 1385396, setCohort initializes graphics and indirectly checks for
    whether e10s is enabled. By setting the pref before calling setCohort, we
    ensure that this call receives (and caches) the right value.
    
    MozReview-Commit-ID: 1lF1Up4Xvct

--- browser/extensions/e10srollout/bootstrap.js
+++ browser/extensions/e10srollout/bootstrap.js
@@ -142,8 +142,8 @@ function defineCohort() {
     // the proper prefs must already be set.
     setCohort("optedOut");
   } else if (userOptedIn.e10s) {
-    setCohort("optedIn");
     eligibleForMulti = true;
+    setCohort("optedIn");
   } else if (temporaryDisqualification != "") {
     // Users who are disqualified by the backend (from multiprocessBlockPolicy)
     // can be put into either the test or control groups, because e10s will
@@ -153,25 +153,25 @@ function defineCohort() {
     // For these volatile disqualification reasons, however, we must not try
     // to activate e10s because the backend doesn't know about it. E10S_STATUS
     // here will be accumulated as "2 - Disabled", which is fine too.
-    setCohort(`temp-disqualified-${temporaryDisqualification}`);
     Preferences.reset(PREF_TOGGLE_E10S);
     Preferences.reset(PREF_E10S_PROCESSCOUNT + ".web");
+    setCohort(`temp-disqualified-${temporaryDisqualification}`);
   } else if (!disqualified && testThreshold < 1.0 &&
              temporaryQualification != "") {
     // Users who are qualified for e10s and on channels where some population
     // would not receive e10s can be pushed into e10s anyway via a temporary
     // qualification which overrides the user sample value when non-empty.
+    Preferences.set(PREF_TOGGLE_E10S, true);
+    eligibleForMulti = true;
     setCohort(`temp-qualified-${temporaryQualification}`);
-    Preferences.set(PREF_TOGGLE_E10S, true);
-    eligibleForMulti = true;
   } else if (testGroup) {
-    setCohort(`${cohortPrefix}test`);
     Preferences.set(PREF_TOGGLE_E10S, true);
     eligibleForMulti = true;
+    setCohort(`${cohortPrefix}test`);
   } else {
-    setCohort(`${cohortPrefix}control`);
     Preferences.reset(PREF_TOGGLE_E10S);
     Preferences.reset(PREF_E10S_PROCESSCOUNT + ".web");
+    setCohort(`${cohortPrefix}control`);
   }
 
   // Now determine if this user should be in the e10s-multi experiment.
@@ -208,10 +208,9 @@ function defineCohort() {
   let multiUserSample = getUserSample(true);
   for (let sampleName of Object.getOwnPropertyNames(buckets)) {
     if (multiUserSample < buckets[sampleName]) {
-      setCohort(`${cohortPrefix}multiBucket${sampleName}`);
-
       // NB: Coerce sampleName to an integer because this is an integer pref.
       Preferences.set(PREF_E10S_PROCESSCOUNT + ".web", +sampleName);
+      setCohort(`${cohortPrefix}multiBucket${sampleName}`);
       break;
     }
   }
