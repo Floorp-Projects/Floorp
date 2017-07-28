8155e483d53c2d0e5e88137d7cb9b8efb4dd490d
Eric Rahm <erahm@mozilla.com>
2017-07-27 18:35:00 -0700

    Bug 1380154 - Part 3: Cache most recently used eTLD entries. r=njn, r=jduell
    This adds a most recently used (MRU) cache for the most common base domain
    requests (aAddtionalParts == 1). With a table size of 31 I saw 8777 hits and
    22 misses when loading twitter, youtube, and techcrunch. In stress testing
    this provided a 75% reduction in run time.
    
    
    MozReview-Commit-ID: 3JgCwIZagMs

--- netwerk/dns/nsEffectiveTLDService.h
+++ netwerk/dns/nsEffectiveTLDService.h
@@ -37,7 +37,40 @@ private:
   ~nsEffectiveTLDService();
 
   nsCOMPtr<nsIIDNService>     mIDNService;
+
+  // The DAFSA provides a compact encoding of the rather large eTLD list.
   mozilla::Dafsa mGraph;
+
+  struct TLDCacheEntry
+  {
+    nsCString mHost;
+    nsCString mBaseDomain;
+  };
+
+  // We use a small most recently used cache to compensate for DAFSA lookups
+  // being slightly slower than a binary search on a larger table of strings.
+  //
+  // We first check the cache for a matching result and avoid a DAFSA lookup
+  // if a match is found. Otherwise we lookup the domain in the DAFSA and then
+  // cache the result. During standard browsing the same domains are repeatedly
+  // fed into |GetBaseDomainInternal| so this ends up being an effective
+  // mitigation getting about a 99% hit rate with four tabs open.
+  //
+  // A size of 31 is used rather than a more logical power-of-two such as 32
+  // since it is a prime number and provides fewer collisions when when used
+  // with our hash algorithms.
+  static const uint32_t kTableSize = 31;
+  TLDCacheEntry mMruTable[kTableSize];
+
+  /**
+   * Performs a lookup on the MRU table and provides a pointer to the hash
+   * entry that matched or should be used for adding this host.
+   *
+   * @param aHost The host to lookup.
+   * @param aEntry Out param, the entry in the MRU table to use.
+   * @return True if a match was found, false if there was a miss.
+   */
+  inline bool LookupForAdd(const nsACString& aHost, TLDCacheEntry** aEntry);
 };
 
 #endif // EffectiveTLDService_h
