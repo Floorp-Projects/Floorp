126cf32e822b017ada17f9a5721d7fcf6928f6b6
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-07-28 09:35:17 -0400

    Bug 1385311 - Make sure AddressSanitizer errors turn the task orange, r=jmaher
    MozReview-Commit-ID: EvLGeaLVotV
    
    --HG--
    extra : rebase_source : 5a1b9b70f0832507f54ff601b16a8e654084c10d

--- testing/mozharness/mozharness/mozilla/testing/errors.py
+++ testing/mozharness/mozharness/mozilla/testing/errors.py
@@ -115,6 +115,7 @@ TestPassed = [
 HarnessErrorList = [
     {'substr': 'TEST-UNEXPECTED', 'level': ERROR, },
     {'substr': 'PROCESS-CRASH', 'level': ERROR, },
+    {'regex': re.compile('''ERROR: (Address|Leak)Sanitizer'''), 'level': ERROR, },
     {'regex': re.compile('''thread '([^']+)' panicked'''), 'level': ERROR, },
 ]
 
