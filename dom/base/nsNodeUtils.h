50fd0b7a62b505124c61643702aca30f6dee7630
Ehsan Akhgari <ehsan@mozilla.com>
2017-07-29 01:07:05 -0400

    Bug 1385533 - Reserve space for one pointer in the mMutationObservers array on node slots; r=smaug
    This helps avoid allocations for the first mutation observer (for example, Range)
    created by a caller.

--- dom/base/nsNodeUtils.h
+++ dom/base/nsNodeUtils.h
@@ -137,10 +137,10 @@ public:
   {
     nsINode::nsSlots* slots = aContent->GetExistingSlots();
     if (slots && !slots->mMutationObservers.IsEmpty()) {
-      NS_OBSERVER_ARRAY_NOTIFY_OBSERVERS(slots->mMutationObservers,
-                                         nsIMutationObserver,
-                                         ParentChainChanged,
-                                         (aContent));
+      NS_OBSERVER_AUTO_ARRAY_NOTIFY_OBSERVERS(slots->mMutationObservers,
+                                              nsIMutationObserver, 1,
+                                              ParentChainChanged,
+                                              (aContent));
     }
   }
 
