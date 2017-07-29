4ca54d2261000422d898e14e4c62843a7ef86b95
Wes Kocher <wkocher@mozilla.com>
2017-07-28 19:31:38 -0700

    Backed out 6 changesets (bug 1369604) for android bustage in activitystream/Utils.java a=backout CLOSED TREE
    Backed out changeset 07ae1124989b (bug 1369604)
    Backed out changeset 10a40b857266 (bug 1369604)
    Backed out changeset e49ad83bc31b (bug 1369604)
    Backed out changeset b33806ccdd9f (bug 1369604)
    Backed out changeset e226cd5d64b2 (bug 1369604)
    Backed out changeset 9e2a5ef546dd (bug 1369604)
    
    MozReview-Commit-ID: 2IwX8r9b56D

--- mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/model/Highlight.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/model/Highlight.java
@@ -7,31 +7,12 @@ package org.mozilla.gecko.activitystream.homepanel.model;
 
 import android.database.Cursor;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
-import android.text.TextUtils;
 import android.text.format.DateUtils;
-import org.mozilla.gecko.activitystream.Utils;
-import org.mozilla.gecko.activitystream.ranking.HighlightCandidateCursorIndices;
-import org.mozilla.gecko.activitystream.ranking.HighlightsRanking;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.mozilla.gecko.activitystream.Utils;
+import org.mozilla.gecko.db.BrowserContract;
 
 public class Highlight implements Item {
-
-    /**
-     * A pattern matching a json object containing the key "image_url" and extracting the value. afaik, these urls
-     * are not encoded so it's entirely possible that the url will contain a quote and we will not extract the whole
-     * url. However, given these are coming from websites providing favicon-like images, it's not likely a quote will
-     * appear and since these urls are only being used to compare against one another (as imageURLs in Highlight items),
-     * a partial URL may actually have the same behavior: good enough for me!
-     */
-    private static final Pattern FAST_IMAGE_URL_PATTERN = Pattern.compile("\"image_url\":\"([^\"]+)\"");
-
-    // A pattern matching a json object containing the key "description_length" and extracting the value: this
-    // regex should perfectly match values in json without whitespace.
-    private static final Pattern FAST_DESCRIPTION_LENGTH_PATTERN = Pattern.compile("\"description_length\":([0-9]+)");
-
     private final String title;
     private final String url;
     private final Utils.HighlightSource source;
@@ -39,54 +20,26 @@ public class Highlight implements Item {
 
     private long historyId;
 
-    private @Nullable Metadata metadata; // lazily-loaded.
-    private @Nullable final String metadataJSON;
-    private @Nullable String fastImageURL;
-    private int fastDescriptionLength;
+    private Metadata metadata;
 
     private @Nullable Boolean isPinned;
     private @Nullable Boolean isBookmarked;
 
-    public static Highlight fromCursor(final Cursor cursor, final HighlightCandidateCursorIndices cursorIndices) {
-        return new Highlight(cursor, cursorIndices);
+    public static Highlight fromCursor(Cursor cursor) {
+        return new Highlight(cursor);
     }
 
-    private Highlight(final Cursor cursor, final HighlightCandidateCursorIndices cursorIndices) {
-        title = cursor.getString(cursorIndices.titleColumnIndex);
-        url = cursor.getString(cursorIndices.urlColumnIndex);
-        source = Utils.highlightSource(cursor, cursorIndices);
-        time = cursor.getLong(cursorIndices.highlightsDateColumnIndex);
+    private Highlight(Cursor cursor) {
+        title = cursor.getString(cursor.getColumnIndexOrThrow(BrowserContract.History.TITLE));
+        url = cursor.getString(cursor.getColumnIndexOrThrow(BrowserContract.Combined.URL));
+        source = Utils.highlightSource(cursor);
+        time = cursor.getLong(cursor.getColumnIndexOrThrow(BrowserContract.Highlights.DATE));
 
-        historyId = cursor.getLong(cursorIndices.historyIDColumnIndex);
+        historyId = cursor.getLong(cursor.getColumnIndexOrThrow(BrowserContract.Highlights.HISTORY_ID));
 
-        metadataJSON = cursor.getString(cursorIndices.metadataColumnIndex);
-        fastImageURL = initFastImageURL(metadataJSON);
-        fastDescriptionLength = initFastDescriptionLength(metadataJSON);
+        metadata = Metadata.fromCursor(cursor);
 
         updateState();
-    }
-
-    /** Gets a fast image URL. Full docs for this method at {@link #getFastImageURLForComparison()} & {@link #FAST_IMAGE_URL_PATTERN}. */
-    @VisibleForTesting static @Nullable String initFastImageURL(final String metadataJSON) {
-        return extractFirstGroupFromMetadataJSON(metadataJSON, FAST_IMAGE_URL_PATTERN);
-    }
-
-    /** Gets a fast description length. Full docs for this method at {@link #getFastDescriptionLength()} & {@link #FAST_DESCRIPTION_LENGTH_PATTERN}. */
-    @VisibleForTesting static int initFastDescriptionLength(final String metadataJSON) {
-        final String extractedStr = extractFirstGroupFromMetadataJSON(metadataJSON, FAST_DESCRIPTION_LENGTH_PATTERN);
-        try {
-            return !TextUtils.isEmpty(extractedStr) ? Integer.parseInt(extractedStr) : 0;
-        } catch (final NumberFormatException e) { /* intentionally blank */ }
-        return 0;
-    }
-
-    private static @Nullable String extractFirstGroupFromMetadataJSON(final String metadataJSON, final Pattern pattern) {
-        if (metadataJSON == null) {
-            return null;
-        }
-
-        final Matcher matcher = pattern.matcher(metadataJSON);
-        return matcher.find() ? matcher.group(1) : null;
     }
 
     private void updateState() {
@@ -115,65 +68,8 @@ public class Highlight implements Item {
         return url;
     }
 
-    /**
-     * Retrieves the metadata associated with this highlight, lazily loaded.
-     *
-     * AVOID USING THIS FOR A LARGE NUMBER OF ITEMS, particularly in {@link HighlightsRanking#extractFeatures(Cursor)},
-     * where we added lazy loading to improve performance.
-     *
-     * The JSONObject constructor inside Metadata takes a non-trivial amount of time to run so
-     * we lazily load it. At the time of writing, in {@link HighlightsRanking#extractFeatures(Cursor)}, we get
-     * 500 highlights before curating down to the ~5 shown items. For the non-displayed items, we use
-     * the getFast* methods and, for the shown items, lazy-load the metadata since only then is it necessary.
-     * These methods include:
-     * - {@link #getFastDescriptionLength()}
-     * - {@link #getFastImageURLForComparison()}
-     * - {@link #hasFastImageURL()}
-     */
-    public Metadata getMetadataSlow() {
-        if (metadata == null) {
-            metadata = new Metadata(metadataJSON);
-        }
+    public Metadata getMetadata() {
         return metadata;
-    }
-
-    /**
-     * Returns the image url in the highlight's metadata. This value does not provide valid image url but is
-     * consistent across invocations and can be used to compare against other Highlight's fast image urls.
-     * See {@link #getMetadataSlow()} for a description of why we use this method.
-     *
-     * To get a valid image url (at a performance penalty), use {@link #getMetadataSlow()}
-     * {@link #getMetadataSlow()} & {@link Metadata#getImageUrl()}.
-     *
-     * Note that this explanation is dependent on the implementation of {@link #initFastImageURL(String)}.
-     *
-     * @return the image url, or null if one could not be found.
-     */
-    public @Nullable String getFastImageURLForComparison() {
-        return fastImageURL;
-    }
-
-    /**
-     * Returns true if {@link #getFastImageURLForComparison()} has found an image url, false otherwise.
-     * See that method for caveats.
-     */
-    public boolean hasFastImageURL() {
-        return fastImageURL != null;
-    }
-
-    /**
-     * Returns the description length in the highlight's metadata. This value is expected to correct in all cases.
-     * See {@link #getMetadataSlow()} for why we use this method.
-     *
-     * This is a faster version of {@link #getMetadataSlow()} & {@link Metadata#getDescriptionLength()} because
-     * retrieving the metadata in this way does a full json parse, which is slower.
-     *
-     * Note: this explanation is dependent on the implementation of {@link #initFastDescriptionLength(String)}.
-     *
-     * @return the given description length, or 0 if no description length was given
-     */
-    public int getFastDescriptionLength() {
-        return fastDescriptionLength;
     }
 
     public Boolean isBookmarked() {
--- mobile/android/base/java/org/mozilla/gecko/activitystream/ranking/HighlightCandidate.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/ranking/HighlightCandidate.java
@@ -7,78 +7,48 @@ package org.mozilla.gecko.activitystream.ranking;
 
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.annotation.VisibleForTesting;
+
+import org.mozilla.gecko.activitystream.ranking.RankingUtils.Func1;
+import org.mozilla.gecko.db.BrowserContract;
 import org.mozilla.gecko.activitystream.homepanel.model.Highlight;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A highlight candidate (Highlight object + features). Ranking will determine whether this is an
  * actual highlight.
  */
 /* package-private */ class HighlightCandidate {
+    /* package-private */ static final String FEATURE_AGE_IN_DAYS = "ageInDays";
+    /* package-private */ static final String FEATURE_IMAGE_COUNT = "imageCount";
+    /* package-private */ static final String FEATURE_DOMAIN_FREQUENCY = "domainFrequency";
+    /* package-private */ static final String FEATURE_VISITS_COUNT = "visitsCount";
+    /* package-private */ static final String FEATURE_BOOKMARK_AGE_IN_MILLISECONDS = "bookmarkageInDays";
+    /* package-private */ static final String FEATURE_DESCRIPTION_LENGTH = "descriptionLength";
+    /* package-private */ static final String FEATURE_PATH_LENGTH = "pathLength";
+    /* package-private */ static final String FEATURE_QUERY_LENGTH = "queryLength";
+    /* package-private */ static final String FEATURE_IMAGE_SIZE = "imageSize";
 
-    // Features we score over for Highlight results - see Features class for more details & usage.
-    @Retention(RetentionPolicy.SOURCE)
-    @IntDef({FEATURE_AGE_IN_DAYS, FEATURE_BOOKMARK_AGE_IN_MILLISECONDS, FEATURE_DESCRIPTION_LENGTH,
-            FEATURE_DOMAIN_FREQUENCY, FEATURE_IMAGE_COUNT, FEATURE_IMAGE_SIZE, FEATURE_PATH_LENGTH,
-            FEATURE_QUERY_LENGTH, FEATURE_VISITS_COUNT})
-    /* package-private */ @interface FeatureName {}
+    @StringDef({FEATURE_AGE_IN_DAYS, FEATURE_IMAGE_COUNT, FEATURE_DOMAIN_FREQUENCY, FEATURE_VISITS_COUNT,
+            FEATURE_BOOKMARK_AGE_IN_MILLISECONDS, FEATURE_DESCRIPTION_LENGTH, FEATURE_PATH_LENGTH,
+            FEATURE_QUERY_LENGTH, FEATURE_IMAGE_SIZE})
+    public @interface Feature {}
 
-    // IF YOU ADD A FIELD, INCREMENT `FEATURE_COUNT`! For a perf boost, we use these ints to index into an array and
-    // FEATURE_COUNT tracks the number of features we have and thus how big the array needs to be.
-    private static final int FEATURE_COUNT = 9; // = the-greatest-feature-index + 1.
-    /* package-private */ static final int FEATURE_AGE_IN_DAYS = 0;
-    /* package-private */ static final int FEATURE_BOOKMARK_AGE_IN_MILLISECONDS = 1;
-    /* package-private */ static final int FEATURE_DESCRIPTION_LENGTH = 2;
-    /* package-private */ static final int FEATURE_DOMAIN_FREQUENCY = 3;
-    /* package-private */ static final int FEATURE_IMAGE_COUNT = 4;
-    /* package-private */ static final int FEATURE_IMAGE_SIZE = 5;
-    /* package-private */ static final int FEATURE_PATH_LENGTH = 6;
-    /* package-private */ static final int FEATURE_QUERY_LENGTH = 7;
-    /* package-private */ static final int FEATURE_VISITS_COUNT = 8;
-
-    /**
-     * A data class for accessing Features values. It acts as a map from FeatureName -> value:
-     * <pre>
-     *   Features features = new Features();
-     *   features.put(FEATURE_AGE_IN_DAYS, 30);
-     *   double value = features.get(FEATURE_AGE_IN_DAYS);
-     * </pre>
-     *
-     * This data is accessed frequently and needs to be performant. As such, the implementation is a little fragile
-     * (e.g. we could increase type safety with enums and index into the backing array with Enum.ordinal(), but it
-     * gets called enough that it's not worth the performance trade-off).
-     */
-    /* package-private */ static class Features {
-        private final double[] values = new double[FEATURE_COUNT];
-
-        Features() {}
-
-        /* package-private */ double get(final @FeatureName int featureName) {
-            return values[featureName];
-        }
-
-        /* package-private */ void put(final @FeatureName int featureName, final double value) {
-            values[featureName] = value;
-        }
-    }
-
-    @VisibleForTesting final Features features = new Features();
+    @VisibleForTesting final Map<String, Double> features;
     private Highlight highlight;
     private @Nullable String imageUrl;
     private String host;
     private double score;
 
-    public static HighlightCandidate fromCursor(final Cursor cursor, final HighlightCandidateCursorIndices cursorIndices)
-            throws InvalidHighlightCandidateException {
+    public static HighlightCandidate fromCursor(Cursor cursor) throws InvalidHighlightCandidateException {
         final HighlightCandidate candidate = new HighlightCandidate();
 
-        extractHighlight(candidate, cursor, cursorIndices);
-        extractFeatures(candidate, cursor, cursorIndices);
+        extractHighlight(candidate, cursor);
+        extractFeatures(candidate, cursor);
 
         return candidate;
     }
@@ -86,24 +56,22 @@ import java.lang.annotation.RetentionPolicy;
     /**
      * Extract highlight object from cursor.
      */
-    private static void extractHighlight(final HighlightCandidate candidate, final Cursor cursor,
-            final HighlightCandidateCursorIndices cursorIndices) {
-        candidate.highlight = Highlight.fromCursor(cursor, cursorIndices);
+    private static void extractHighlight(HighlightCandidate candidate, Cursor cursor) {
+        candidate.highlight = Highlight.fromCursor(cursor);
     }
 
     /**
      * Extract and assign features that will be used for ranking.
      */
-    private static void extractFeatures(final HighlightCandidate candidate, final Cursor cursor,
-            final HighlightCandidateCursorIndices cursorIndices) throws InvalidHighlightCandidateException {
+    private static void extractFeatures(HighlightCandidate candidate, Cursor cursor) throws InvalidHighlightCandidateException {
         candidate.features.put(
                 FEATURE_AGE_IN_DAYS,
-                (System.currentTimeMillis() - cursor.getDouble(cursorIndices.historyDateLastVisitedColumnIndex))
+                (System.currentTimeMillis() - cursor.getDouble(cursor.getColumnIndexOrThrow(BrowserContract.History.DATE_LAST_VISITED)))
                         / (1000 * 3600 * 24));
 
         candidate.features.put(
                 FEATURE_VISITS_COUNT,
-                cursor.getDouble(cursorIndices.visitsColumnIndex));
+                cursor.getDouble(cursor.getColumnIndexOrThrow(BrowserContract.History.VISITS)));
 
         // Until we can determine those numbers we assume this domain has only been visited once
         // and the cursor returned all database entries.
@@ -115,7 +83,7 @@ import java.lang.annotation.RetentionPolicy;
                 FEATURE_DOMAIN_FREQUENCY,
                 Math.log(1 + domainCountSize / occurrences));
 
-        candidate.imageUrl = candidate.highlight.getFastImageURLForComparison();
+        candidate.imageUrl = candidate.highlight.getMetadata().getImageUrl();
 
         // The desktop add-on used the number of images returned form Embed.ly here. This is not the
         // same as total images on the page (think of small icons or the famous spacer.gif). So for
@@ -124,13 +92,13 @@ import java.lang.annotation.RetentionPolicy;
         // At this point we can try to find a fathom rule for determining a good value here.
         candidate.features.put(
                 FEATURE_IMAGE_COUNT,
-                candidate.highlight.hasFastImageURL() ? 1d : 0d);
+                candidate.highlight.getMetadata().hasImageUrl() ? 1d : 0d);
 
         // TODO: We do not store the size of the main image (Bug 1335819).
         // The desktop add-on calculates: Math.min(image.width * image.height, 1e5)
         candidate.features.put(
                 FEATURE_IMAGE_SIZE,
-                candidate.highlight.hasFastImageURL() ? 1d : 0d
+                candidate.highlight.getMetadata().hasImageUrl() ? 1d : 0d
         );
 
         // Historical note: before Bug 1335198, this value was not really the time at which the
@@ -140,19 +108,20 @@ import java.lang.annotation.RetentionPolicy;
         // to the real creation date, or the earliest one mentioned in the clients constellation.
         // We are sourcing highlights from the recent visited history - so in order to
         // show up this bookmark need to have been visited recently too.
-        if (cursor.isNull(cursorIndices.bookmarkDateCreatedColumnIndex)) {
+        final int bookmarkDateColumnIndex = cursor.getColumnIndexOrThrow(BrowserContract.Bookmarks.DATE_CREATED);
+        if (cursor.isNull(bookmarkDateColumnIndex)) {
             candidate.features.put(
                     FEATURE_BOOKMARK_AGE_IN_MILLISECONDS,
                     0d);
         } else {
             candidate.features.put(
                     FEATURE_BOOKMARK_AGE_IN_MILLISECONDS,
-                    Math.max(1, System.currentTimeMillis() - cursor.getDouble(cursorIndices.bookmarkDateCreatedColumnIndex)));
+                    Math.max(1, System.currentTimeMillis() - cursor.getDouble(bookmarkDateColumnIndex)));
         }
 
         candidate.features.put(
                 FEATURE_DESCRIPTION_LENGTH,
-                (double) candidate.highlight.getFastDescriptionLength());
+                (double) candidate.highlight.getMetadata().getDescriptionLength());
 
         final Uri uri = Uri.parse(candidate.highlight.getUrl());
 
@@ -176,6 +145,7 @@ import java.lang.annotation.RetentionPolicy;
     }
 
     @VisibleForTesting HighlightCandidate() {
+        features = new HashMap<>();
     }
 
     /* package-private */ double getScore() {
@@ -194,17 +164,37 @@ import java.lang.annotation.RetentionPolicy;
         return host;
     }
 
-    /**
-     * Gets an estimate of the actual image url that should only be used to compare against other return
-     * values of this method. See {@link Highlight#getFastImageURLForComparison()} for more details.
-     */
     @Nullable
-    /* package-private */ String getFastImageUrlForComparison() {
+    /* package-private */ String getImageUrl() {
         return imageUrl;
     }
 
     /* package-private */ Highlight getHighlight() {
         return highlight;
+    }
+
+    /* package-private */ double getFeatureValue(@Feature String feature) {
+        if (!features.containsKey(feature)) {
+            throw new IllegalStateException("No value for feature " + feature);
+        }
+
+        return features.get(feature);
+    }
+
+    /* package-private */ void setFeatureValue(@Feature String feature, double value) {
+        features.put(feature, value);
+    }
+
+    /* package-private */ Map<String, Double> getFilteredFeatures(Func1<String, Boolean> filter) {
+        Map<String, Double> filteredFeatures = new HashMap<>();
+
+        for (Map.Entry<String, Double> entry : features.entrySet()) {
+            if (filter.call(entry.getKey())) {
+                filteredFeatures.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return filteredFeatures;
     }
 
     /* package-private */ static class InvalidHighlightCandidateException extends Exception {
