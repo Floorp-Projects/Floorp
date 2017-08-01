ee9c921ae44e4ba959ffdecf5359cc7c9b3a2d42
Mitchell Kager <mitchell.kager@nextcentury.com>
2017-08-01 13:27:08 -0400

    [focus] Filter out Focus in the BottomSheet when "Open with..." is clicked https://github.com/mozilla-mobile/focus-android/issues/894

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Browsers.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Browsers.java
@@ -76,7 +76,7 @@ public class Browsers {
 
         final Uri uri = Uri.parse(url);
 
-        final Map<String, ActivityInfo> browsers = resolveBrowsers(packageManager, uri);
+        final Map<String, ActivityInfo> browsers = resolveBrowsers(context, packageManager, uri);
 
         // If there's a default browser set then modern Android systems won't return other browsers
         // anymore when using queryIntentActivities(). That's annoying and our only option is
@@ -102,7 +102,7 @@ public class Browsers {
         return null;
     }
 
-    private Map<String, ActivityInfo> resolveBrowsers(PackageManager packageManager, @NonNull Uri uri) {
+    private Map<String, ActivityInfo> resolveBrowsers(Context context, PackageManager packageManager, @NonNull Uri uri) {
         final Map<String, ActivityInfo> browsers = new HashMap<>();
 
         final Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -111,7 +111,9 @@ public class Browsers {
         final List<ResolveInfo> infos = packageManager.queryIntentActivities(intent, 0);
 
         for (ResolveInfo info : infos) {
-            browsers.put(info.activityInfo.packageName, info.activityInfo);
+            if (!context.getPackageName().equals(info.activityInfo.packageName)) {
+                browsers.put(info.activityInfo.packageName, info.activityInfo);
+            }
         }
 
         return browsers;
@@ -190,7 +192,7 @@ public class Browsers {
      * Does this user have browsers installed that are not Focus, Firefox or the default browser?
      */
     public boolean hasMultipleThirdPartyBrowsers(Context context) {
-        if (browsers.size() > 2) {
+        if (browsers.size() > 1) {
             // There are more than us and Firefox.
             return true;
         }
