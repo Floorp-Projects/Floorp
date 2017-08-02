195f9f0987bb09ccb90f8ddf72633dc082a741d6
Valentin Gosu <valentin.gosu@gmail.com>
2017-08-02 13:44:48 +0300

    Bug 1383831 - Upgrade rust-url to version 1.5.1 r=froydnj
    MozReview-Commit-ID: L3j6K5dDWHi
    
    --HG--
    extra : rebase_source : 65b34e84139927f1c28dc38cf2a00a0c2902b975

--- third_party/rust/unicode-normalization/src/test.rs
+++ third_party/rust/unicode-normalization/src/test.rs
@@ -8,14 +8,19 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use UnicodeNormalization;
+
+use std::char;
+use super::UnicodeNormalization;
+use super::char::is_combining_mark;
+
 
 #[test]
 fn test_nfd() {
     macro_rules! t {
         ($input: expr, $expected: expr) => {
-            assert_eq!($input.nfd().collect::<String>(), $expected);
-            // A dummy iterator that is not std::str::Chars directly:
+            assert_eq!($input.nfd().to_string(), $expected);
+            // A dummy iterator that is not std::str::Chars directly;
+            // note that `id_func` is used to ensure `Clone` implementation
             assert_eq!($input.chars().map(|c| c).nfd().collect::<String>(), $expected);
         }
     }
@@ -35,7 +40,7 @@ fn test_nfd() {
 fn test_nfkd() {
     macro_rules! t {
         ($input: expr, $expected: expr) => {
-            assert_eq!($input.nfkd().collect::<String>(), $expected);
+            assert_eq!($input.nfkd().to_string(), $expected);
         }
     }
     t!("abc", "abc");
@@ -54,7 +59,7 @@ fn test_nfkd() {
 fn test_nfc() {
     macro_rules! t {
         ($input: expr, $expected: expr) => {
-            assert_eq!($input.nfc().collect::<String>(), $expected);
+            assert_eq!($input.nfc().to_string(), $expected);
         }
     }
     t!("abc", "abc");
@@ -74,7 +79,7 @@ fn test_nfc() {
 fn test_nfkc() {
     macro_rules! t {
         ($input: expr, $expected: expr) => {
-            assert_eq!($input.nfkc().collect::<String>(), $expected);
+            assert_eq!($input.nfkc().to_string(), $expected);
         }
     }
     t!("abc", "abc");
@@ -152,4 +157,25 @@ fn test_official() {
             assert_eq!(s5, &r5[..]);
         }
     }
+}
+
+
+
+#[test]
+fn test_is_combining_mark_ascii() {
+    for cp in 0..0x7f {
+        assert!(!is_combining_mark(char::from_u32(cp).unwrap()));
+    }
+}
+
+#[test]
+fn test_is_combining_mark_misc() {
+    // https://github.com/unicode-rs/unicode-normalization/issues/16
+    // U+11C3A BHAIKSUKI VOWEL SIGN O
+    // Category: Mark, Nonspacing [Mn]
+    assert!(is_combining_mark('\u{11C3A}'));
+
+    // U+11C3F BHAIKSUKI SIGN VIRAMA
+    // Category: Mark, Nonspacing [Mn]
+    assert!(is_combining_mark('\u{11C3F}'));
 }
