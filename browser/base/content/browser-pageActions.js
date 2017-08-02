7867cfd097620470b689efc354c229e5c6cff287
Drew Willcoxon <adw@mozilla.com>
2017-08-01 20:37:22 -0700

    Bug 1386337 - Disable Photon page actions when Photon is not enabled. r=Gijs
    MozReview-Commit-ID: 3GrsscIxeyW

--- browser/base/content/browser-pageActions.js
+++ browser/base/content/browser-pageActions.js
@@ -47,6 +47,9 @@ var BrowserPageActions = {
    * Inits.  Call to init.
    */
   init() {
+    if (!AppConstants.MOZ_PHOTON_THEME) {
+      return;
+    }
     for (let action of PageActions.actions) {
       this.placeAction(action, PageActions.insertBeforeActionIDInUrlbar(action));
     }
