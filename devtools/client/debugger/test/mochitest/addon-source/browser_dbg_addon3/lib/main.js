94c69246a62350c28fdc71b7adc612f31d9ff26b
Julian Descottes <jdescottes@mozilla.com>
2017-08-02 12:01:24 +0200

    Bug 1386550 - remove usage of sdk/system/events in DevTools test;r=sole
    MozReview-Commit-ID: FM1AjShYmkj

--- devtools/client/debugger/test/mochitest/addon-source/browser_dbg_addon3/lib/main.js
+++ devtools/client/debugger/test/mochitest/addon-source/browser_dbg_addon3/lib/main.js
@@ -1,13 +1,20 @@
 var { Cc, Ci } = require("chrome");
-var { once } = require("sdk/system/events");
-
 var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+
 var observer = {
   observe: function () {
     debugger;
   }
 };
 
-once("sdk:loader:destroy", () => observerService.removeObserver(observer, "debuggerAttached"));
-
 observerService.addObserver(observer, "debuggerAttached");
+
+var sdkLoaderDestroyObserver = {
+  observe: function () {
+    // Remove all observers on sdk:loader:destroy
+    observerService.removeObserver(observer, "debuggerAttached");
+    observerService.removeObserver(sdkLoaderDestroyObserver, "sdk:loader:destroy");
+  }
+};
+
+observerService.addObserver(sdkLoaderDestroyObserver, "sdk:loader:destroy");
