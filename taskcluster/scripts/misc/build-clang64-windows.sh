e14ea5614036b993b916c695bacbb218b9712f64
Mike Hommey <mh+mozilla@glandium.org>
2017-08-02 17:25:53 +0900

    Bug 1386588 - Rename the clang build manifests. r=gps
    The clang toolchains have not been static-analysis-specific for a while.

--- taskcluster/scripts/misc/build-clang64-windows.sh
+++ taskcluster/scripts/misc/build-clang64-windows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source build/src/taskcluster/scripts/misc/build-clang-windows-helper64.sh clang-static-analysis-win64.json
+source build/src/taskcluster/scripts/misc/build-clang-windows-helper64.sh clang-win64.json
