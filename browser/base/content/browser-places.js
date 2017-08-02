ed34823b6f3a674932c75eb6cc94f4dbb1a21242
Erica Wright <ewright@mozilla.com>
2017-08-02 16:20:52 -0400

    Bug 1382570 - Ignore non-left clicks on the bookmark icon. r=Gijs
    MozReview-Commit-ID: KrFYUijzawU

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -1962,8 +1962,8 @@ var BookmarkingUI = {
   },
 
   onStarCommand(aEvent) {
-    // Ignore clicks on the star if we are updating its state.
-    if (!this._pendingUpdate) {
+    // Ignore non-left clicks on the star, or if we are updating its state.
+    if (!this._pendingUpdate && aEvent.button == 0) {
       let isBookmarked = this._itemGuids.size > 0;
       // Disable the old animation in photon
       if (!isBookmarked && !AppConstants.MOZ_PHOTON_THEME)
