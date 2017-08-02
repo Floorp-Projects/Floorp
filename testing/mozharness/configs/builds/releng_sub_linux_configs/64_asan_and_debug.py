ba80d5f551e1e5d078394617c10a4cc505351c3b
Mike Hommey <mh+mozilla@glandium.org>
2017-08-02 20:06:10 +0900

    Bug 1386589 - Remove the linux64 tooltool manifests that look the same as releng.manifest. r=gps

--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_asan_and_debug.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_asan_and_debug.py
@@ -19,7 +19,7 @@ config = {
     'build_type': 'asan-debug',
     'debug_build': True,
     'tooltool_manifest_src': "browser/config/tooltool-manifests/linux64/\
-asan.manifest",
+releng.manifest",
     'platform_supports_post_upload_to_latest': False,
     'enable_signing': False,
     'enable_talos_sendchange': False,
