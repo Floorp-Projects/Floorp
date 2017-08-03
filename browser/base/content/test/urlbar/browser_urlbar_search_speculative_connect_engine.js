333b2147aaf694f71e332ff23e238dd9d1b8a281
Evelyn Hung <jj.evelyn@gmail.com>
2017-08-03 21:41:58 +0800

    Bug 1355451 - Speculative connect to websites on mousedown on awesomebar item. r=mak
    We preconnect to an explicit URL or the url in moz-action:remotetab on the popup.
    
    MozReview-Commit-ID: 2U6nhwM6RUk

--- browser/base/content/test/urlbar/browser_urlbar_search_speculative_connect_engine.js
+++ browser/base/content/test/urlbar/browser_urlbar_search_speculative_connect_engine.js
@@ -7,7 +7,6 @@
 // This test ensures that we setup a speculative network connection to
 // current search engine if the first result is 'searchengine'.
 
-let {HttpServer} = Cu.import("resource://testing-common/httpd.js", {});
 let gHttpServer = null;
 let gScheme = "http";
 let gHost = "localhost"; // 'localhost' by default.
@@ -15,21 +14,11 @@ let gPort = 20709; // the port number must be identical to what we said in searc
 const TEST_ENGINE_BASENAME = "searchSuggestionEngine2.xml";
 
 add_task(async function setup() {
-  if (!gHttpServer) {
-    gHttpServer = new HttpServer();
-    try {
-      gHttpServer.start(gPort);
-      gPort = gHttpServer.identity.primaryPort;
-      gHttpServer.identity.setPrimary(gScheme, gHost, gPort);
-    } catch (ex) {
-      info("We can't launch our http server successfully.")
-    }
-  }
-  is(gHttpServer.identity.has(gScheme, gHost, gPort), true, "make sure we have this domain listed");
+  gHttpServer = runHttpServer(gScheme, gHost, gPort);
 
   await SpecialPowers.pushPrefEnv({
     set: [["browser.urlbar.autoFill", true],
-          // Turn off speculative connect to the search engine.
+          // Make sure search suggestion for location bar is enabled
           ["browser.search.suggest.enabled", true],
           ["browser.urlbar.suggest.searches", true],
           ["browser.urlbar.speculativeConnect.enabled", true],
