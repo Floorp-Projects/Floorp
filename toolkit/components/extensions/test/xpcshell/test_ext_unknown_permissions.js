930d5055aefc9ab5703a968b60625ab91c1f46e3
Tomislav Jovanovic <tomica@gmail.com>
2017-08-04 00:26:50 +0200

    Bug 1385864  - Drop invalid optional_permissions from manifests  r=kmag
    MozReview-Commit-ID: Df2wjNOTVkF
    
    --HG--
    extra : rebase_source : c185d2908a8d39d77d145b872e7e977d6d3973e7

--- toolkit/components/extensions/test/xpcshell/test_ext_unknown_permissions.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_unknown_permissions.js
@@ -9,6 +9,10 @@ add_task(async function test_unknown_permissions() {
         "http://*/",
         "chrome://favicon/",
       ],
+      optional_permissions: [
+        "chrome://favicon/",
+        "https://example.com/",
+      ],
     },
   });
 
@@ -20,11 +24,16 @@ add_task(async function test_unknown_permissions() {
   let policy = WebExtensionPolicy.getByID(extension.id);
   Assert.deepEqual(Array.from(policy.permissions).sort(), ["activeTab", "http://*/*"]);
 
+  Assert.deepEqual(extension.extension.manifest.optional_permissions, ["https://example.com/"]);
+
   ok(messages.some(message => /Error processing permissions\.1: Value "fooUnknownPermission" must/.test(message)),
      'Got expected error for "fooUnknownPermission"');
 
   ok(messages.some(message => /Error processing permissions\.3: Value "chrome:\/\/favicon\/" must/.test(message)),
      'Got expected error for "chrome://favicon/"');
+
+  ok(messages.some(message => /Error processing optional_permissions\.0: Value "chrome:\/\/favicon\/" must/.test(message)),
+     "Got expected error from optional_permissions");
 
   await extension.unload();
 });
