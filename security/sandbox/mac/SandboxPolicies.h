3732006288f0f29259bd86698708349c4949bcf1
Alex Gaynor <agaynor@mozilla.com>
2017-08-03 10:14:33 -0400

    Bug 1385096 - remove access to the com.apple.bird mach service from the content process; r=haik
    MozReview-Commit-ID: FqKZVL16zz9

--- security/sandbox/mac/SandboxPolicies.h
+++ security/sandbox/mac/SandboxPolicies.h
@@ -192,8 +192,7 @@ static const char contentSandboxRules[] = R"(
       (global-name "com.apple.SystemConfiguration.configd")
       (global-name "com.apple.iconservices")
       (global-name "com.apple.cache_delete")
-      (global-name "com.apple.pluginkit.pkd")
-      (global-name "com.apple.bird"))
+      (global-name "com.apple.pluginkit.pkd"))
 
 ; bug 1376163
   (if (>= macosMinorVersion 13)
