46eb226e63b0a80c61ecf7645f071b66427f55a5
Wes Kocher <wkocher@mozilla.com>
2017-08-02 18:14:56 -0700

    Backed out changeset bcd223309de8 (bug 1360666) for asserting in stylo reftests a=backout
    MozReview-Commit-ID: HH3rwDwvYyz

--- dom/base/nsFocusManager.h
+++ dom/base/nsFocusManager.h
@@ -69,11 +69,6 @@ public:
   nsIContent* GetFocusedContent() { return mFocusedContent; }
 
   /**
-   * Returns true if aContent currently has focus.
-   */
-  bool IsFocused(nsIContent* aContent);
-
-  /**
    * Return a focused window. Version of nsIFocusManager::GetFocusedWindow.
    */
   nsPIDOMWindowOuter* GetFocusedWindow() const { return mFocusedWindow; }
