bb9bd5010d1ffcec1daf959c088abf4f45642853
Nicolas Ouellet-Payeur <nicolaso@google.com>
2017-08-03 00:28:05 -0700

    Bug 1368202 - convert uses of "defer" to "new Promise" in client/inspector r=tromey
    MozReview-Commit-ID: lXAVyomDlV
    
    --HG--
    extra : rebase_source : 351d82404ec42169e24c7fc1b8a9aa6b2af02dc5

--- devtools/client/inspector/rules/test/browser_rules_original-source-link.js
+++ devtools/client/inspector/rules/test/browser_rules_original-source-link.js
@@ -50,20 +50,18 @@ function* testClickingLink(toolbox, view) {
 }
 
 function checkDisplayedStylesheet(toolbox) {
-  let def = defer();
-
   let panel = toolbox.getCurrentPanel();
-  panel.UI.on("editor-selected", (event, editor) => {
-    // The style editor selects the first sheet at first load before
-    // selecting the desired sheet.
-    if (editor.styleSheet.href.endsWith("scss")) {
-      info("Original source editor selected");
-      editor.getSourceEditor().then(editorSelected)
-        .then(def.resolve, def.reject);
-    }
+  return new Promise((resolve, reject) => {
+    panel.UI.on("editor-selected", (event, editor) => {
+      // The style editor selects the first sheet at first load before
+      // selecting the desired sheet.
+      if (editor.styleSheet.href.endsWith("scss")) {
+        info("Original source editor selected");
+        editor.getSourceEditor().then(editorSelected)
+          .then(resolve, reject);
+      }
+    });
   });
-
-  return def.promise;
 }
 
 function editorSelected(editor) {
