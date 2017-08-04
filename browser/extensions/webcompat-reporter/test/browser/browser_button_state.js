6318f83aed1e04dd3f1186c6801b5bbfe90543b1
Mike Taylor <miket@mozilla.com>
2017-08-03 23:19:23 -0500

    Bug 1373650. Update webcompat-reporter tests. r=Gijs
    MozReview-Commit-ID: Bekl3MAjByC

--- browser/extensions/webcompat-reporter/test/browser/browser_button_state.js
+++ browser/extensions/webcompat-reporter/test/browser/browser_button_state.js
@@ -5,24 +5,18 @@ const NONREPORTABLE_PAGE = "about:blank";
 /* Test that the Report Site Issue button is enabled for http and https tabs,
    on page load, or TabSelect, and disabled for everything else. */
 add_task(async function test_button_state_disabled() {
-  CustomizableUI.addWidgetToArea("webcompat-reporter-button", "nav-bar");
   let tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, REPORTABLE_PAGE);
+  openPageActions();
   is(isButtonDisabled(), false, "Check that button is enabled for reportable schemes on tab load");
 
   let tab2 = await BrowserTestUtils.openNewForegroundTab(gBrowser, NONREPORTABLE_PAGE);
+  openPageActions();
   is(isButtonDisabled(), true, "Check that button is disabled for non-reportable schemes on tab load");
 
   let tab3 = await BrowserTestUtils.openNewForegroundTab(gBrowser, REPORTABLE_PAGE2);
+  openPageActions();
   is(isButtonDisabled(), false, "Check that button is enabled for reportable schemes on tab load");
 
-
-  await BrowserTestUtils.switchTab(gBrowser, tab2);
-  is(isButtonDisabled(), true, "Check that button is disabled for non-reportable schemes on TabSelect");
-
-  await BrowserTestUtils.switchTab(gBrowser, tab1);
-  is(isButtonDisabled(), false, "Check that button is enabled for reportable schemes on TabSelect");
-
-  CustomizableUI.reset();
   await BrowserTestUtils.removeTab(tab1);
   await BrowserTestUtils.removeTab(tab2);
   await BrowserTestUtils.removeTab(tab3);
