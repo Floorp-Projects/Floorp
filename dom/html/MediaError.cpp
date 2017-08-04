54b4e6b05dd8e0a2ab44d219f76e9f6be6d522d9
Chung-Sheng Fu <cfu@mozilla.com>
2017-08-04 18:46:26 +0800

    Bug 1354633 - When privacy.resistFingerprinting = true, MediaError.message can only get whitelisted messages r=arthuredelstein,jwwang
    MozReview-Commit-ID: mYBftzcZT5

--- dom/html/MediaError.cpp
+++ dom/html/MediaError.cpp
@@ -5,8 +5,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/MediaError.h"
+
+#include <string>
+#include <unordered_set>
+
 #include "nsDOMClassInfoID.h"
 #include "mozilla/dom/MediaErrorBinding.h"
+#include "nsContentUtils.h"
+#include "nsIScriptError.h"
+#include "jsapi.h"
 
 namespace mozilla {
 namespace dom {
@@ -31,6 +38,50 @@ MediaError::MediaError(HTMLMediaElement* aParent, uint16_t aCode,
 void
 MediaError::GetMessage(nsAString& aResult) const
 {
+  // When fingerprinting resistance is enabled, only messages in this list
+  // can be returned to content script.
+  static const std::unordered_set<std::string> whitelist = {
+    "404: Not Found"
+    // TODO
+  };
+
+  bool shouldBlank = (whitelist.find(mMessage.get()) == whitelist.end());
+
+  if (shouldBlank) {
+    // Print a warning message to JavaScript console to alert developers of
+    // a non-whitelisted error message.
+    nsAutoCString message =
+      NS_LITERAL_CSTRING(
+        "This error message will be blank when privacy.resistFingerprinting = true."
+        "  If it is really necessary, please add it to the whitelist in"
+        " MediaError::GetMessage: ") +
+      mMessage;
+    nsIDocument* ownerDoc = mParent->OwnerDoc();
+    AutoJSAPI api;
+    if (api.Init(ownerDoc->GetScopeObject())) {
+      // We prefer this API because it can also print to our debug log and
+      // try server's log viewer.
+      JS_ReportWarningASCII(api.cx(), "%s", message.get());
+    } else {
+      // If failed to use JS_ReportWarningASCII, fall back to
+      // nsContentUtils::ReportToConsoleNonLocalized, which can only print to
+      // JavaScript console.
+      nsContentUtils::ReportToConsoleNonLocalized(
+        NS_ConvertASCIItoUTF16(message),
+        nsIScriptError::warningFlag,
+        NS_LITERAL_CSTRING("MediaError"),
+        ownerDoc
+      );
+    }
+  }
+
+  if (!nsContentUtils::IsCallerChrome() &&
+      nsContentUtils::ShouldResistFingerprinting() &&
+      shouldBlank) {
+    aResult.Truncate();
+    return;
+  }
+
   CopyUTF8toUTF16(mMessage, aResult);
 }
 
