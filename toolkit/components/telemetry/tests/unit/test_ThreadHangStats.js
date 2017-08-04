27017afe3616bd1bfc4efb0f6a277a5eb2489596
Michael Layzell <michael@thelayzells.com>
2017-08-04 07:23:00 -0400

    Bug 1353264 - Disable testing native stack collection in Beta. r=froydnj, a=test-only
    MozReview-Commit-ID: 4Ssxa3iUcHK
    
    --HG--
    extra : amend_source : f966b1aa9ecf137aa1e928ceb1fcde22b9c1cd6e

--- toolkit/components/telemetry/tests/unit/test_ThreadHangStats.js
+++ toolkit/components/telemetry/tests/unit/test_ThreadHangStats.js
@@ -84,17 +84,9 @@ function run_test() {
       notEqual(endHangs.hangs[0].stack.length, 0);
       equal(typeof endHangs.hangs[0].stack[0], "string");
 
-      // Make sure there is a nativeStacks field with native stacks in it.
+      // Make sure there is a nativeStacks field
       ok(Array.isArray(endHangs.nativeStacks.memoryMap));
       ok(Array.isArray(endHangs.nativeStacks.stacks));
-      ok(endHangs.nativeStacks.stacks.length > 0);
-
-      // Make sure that at least one of the hangs contains a native stack.
-      ok(endHangs.hangs.some((hang) => {
-        return typeof hang.nativeStack == "number" &&
-          hang.nativeStack < endHangs.nativeStacks.stacks.length &&
-          Array.isArray(endHangs.nativeStacks.stacks[hang.nativeStack]);
-      }));
 
       check_histogram(endHangs.hangs[0].histogram);
 
