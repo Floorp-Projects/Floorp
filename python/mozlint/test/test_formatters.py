cd2ffadf5dae5cd5620495927c040f2ee8559ce6
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-08-04 10:53:43 -0400

    Bug 1306122 - [mozlint] Create a compact formatter that mimics the eslint 'compact' format, r=armenzg
    MozReview-Commit-ID: 5JJJhMIrMIB

--- python/mozlint/test/test_formatters.py
+++ python/mozlint/test/test_formatters.py
@@ -14,6 +14,43 @@ from mozlint import ResultContainer
 from mozlint import formatters
 
 
+EXPECTED = {
+    'compact': {
+        'kwargs': {},
+        'format': """
+a/b/c.txt: line 1, Error - oh no foo (foo)
+a/b/c.txt: line 4, Error - oh no baz (baz)
+d/e/f.txt: line 4, col 2, Warning - oh no bar (bar-not-allowed)
+
+3 problems
+""".strip(),
+    },
+    'stylish': {
+        'kwargs': {
+            'disable_colors': True,
+        },
+        'format': """
+a/b/c.txt
+  1  error  oh no foo  (foo)
+  4  error  oh no baz  (baz)
+
+d/e/f.txt
+  4:2  warning  oh no bar  bar-not-allowed (bar)
+
+\u2716 3 problems (2 errors, 1 warning)
+""".strip(),
+    },
+    'treeherder': {
+        'kwargs': {},
+        'format': """
+TEST-UNEXPECTED-ERROR | a/b/c.txt:1 | oh no foo (foo)
+TEST-UNEXPECTED-ERROR | a/b/c.txt:4 | oh no baz (baz)
+TEST-UNEXPECTED-WARNING | d/e/f.txt:4:2 | oh no bar (bar-not-allowed)
+""".strip(),
+    },
+}
+
+
 @pytest.fixture
 def results(scope='module'):
     containers = (
@@ -47,31 +84,11 @@ def results(scope='module'):
     return results
 
 
-def test_stylish_formatter(results):
-    expected = """
-a/b/c.txt
-  1  error  oh no foo  (foo)
-  4  error  oh no baz  (baz)
-
-d/e/f.txt
-  4:2  warning  oh no bar  bar-not-allowed (bar)
-
-\u2716 3 problems (2 errors, 1 warning)
-""".strip()
-
-    fmt = formatters.get('stylish', disable_colors=True)
-    assert expected == fmt(results)
-
-
-def test_treeherder_formatter(results):
-    expected = """
-TEST-UNEXPECTED-ERROR | a/b/c.txt:1 | oh no foo (foo)
-TEST-UNEXPECTED-ERROR | a/b/c.txt:4 | oh no baz (baz)
-TEST-UNEXPECTED-WARNING | d/e/f.txt:4:2 | oh no bar (bar-not-allowed)
-""".strip()
-
-    fmt = formatters.get('treeherder')
-    assert expected == fmt(results)
+@pytest.mark.parametrize("name", EXPECTED.keys())
+def test_formatters(results, name):
+    opts = EXPECTED[name]
+    fmt = formatters.get(name, **opts['kwargs'])
+    assert fmt(results) == opts['format']
 
 
 def test_json_formatter(results):
