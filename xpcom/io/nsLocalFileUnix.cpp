06311d2e3abfcbda4aecac2c07c936757b5f900a
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2017-08-05 09:43:49 +0900

    Bug 1375125 - Remove nsILocalFile. r=froydnj
    MozReview-Commit-ID: 6oU6Ya5UXtC
    
    --HG--
    extra : rebase_source : 55b6e30aa348c1b939326698b0da7d72c26b479c

--- xpcom/io/nsLocalFileUnix.cpp
+++ xpcom/io/nsLocalFileUnix.cpp
@@ -239,12 +239,10 @@ nsLocalFile::nsLocalFile(const nsLocalFile& aOther)
 #ifdef MOZ_WIDGET_COCOA
 NS_IMPL_ISUPPORTS(nsLocalFile,
                   nsILocalFileMac,
-                  nsILocalFile,
                   nsIFile,
                   nsIHashable)
 #else
 NS_IMPL_ISUPPORTS(nsLocalFile,
-                  nsILocalFile,
                   nsIFile,
                   nsIHashable)
 #endif
