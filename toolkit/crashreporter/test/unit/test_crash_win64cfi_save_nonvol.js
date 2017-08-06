8903dd01b20507cc3f86ef8ae7c0baadc9da6ade
Carl Corcoran <carlco@gmail.com>
2017-08-06 08:46:50 +0200

    Bug 1333126 - Use win64 PE unwind metadata to improve client-side stack walking; r=gsvelto a=gchang
    This includes tests that cover both regular CFI stack walking as well as
    pathological corner cases.
    
    
    MozReview-Commit-ID: GDARnPSemyu

---
+++ toolkit/crashreporter/test/unit/test_crash_win64cfi_save_nonvol.js
@@ -0,0 +1,7 @@
+
+function run_test() {
+  do_x64CFITest("CRASH_X64CFI_SAVE_NONVOL", [
+      { symbol: "CRASH_X64CFI_SAVE_NONVOL", trust: "context" },
+      { symbol: "CRASH_X64CFI_LAUNCHER", trust: "cfi" }
+    ]);
+}
