49dbc92941bbc53c98edbd11496e954c6004cce3
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-07 14:02:59 +0200

    Backed out changeset 4d92e459f9ab (bug 1333126)

--- toolkit/crashreporter/test/unit/test_crash_win64cfi_save_nonvol.js
+++
@@ -1,7 +0,0 @@
-
-function run_test() {
-  do_x64CFITest("CRASH_X64CFI_SAVE_NONVOL", [
-      { symbol: "CRASH_X64CFI_SAVE_NONVOL", trust: "context" },
-      { symbol: "CRASH_X64CFI_LAUNCHER", trust: "cfi" }
-    ]);
-}
