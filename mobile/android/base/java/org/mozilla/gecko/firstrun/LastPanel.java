a0233f6a52025d0301999ce58acb7980c4bee2d8
Nevin Chen <cnevinchen@gmail.com>
2017-08-07 14:50:04 +0800

    Bug 1366664 - If the user has alread signed-in, we show only three first-run panel. r=walkingice
    MozReview-Commit-ID: DcsbR7TEplk
    
    --HG--
    extra : rebase_source : 8d20568f7afc628ed036f98706a241aa747941d7

--- mobile/android/base/java/org/mozilla/gecko/firstrun/LastPanel.java
+++ mobile/android/base/java/org/mozilla/gecko/firstrun/LastPanel.java
@@ -6,27 +6,17 @@
 package org.mozilla.gecko.firstrun;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Telemetry;
 import org.mozilla.gecko.TelemetryContract;
 
-/**
- * Base class for our first run pages. We call these FirstrunPanel for consistency
- * with HomePager/HomePanel.
- *
- * @see FirstrunPager for the containing pager.
- */
-public class FirstrunPanel extends Fragment {
-
-    public static final int TITLE_RES = -1;
-    protected boolean showBrowserHint = true;
-
+public class LastPanel extends FirstrunPanel {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
         final ViewGroup root = (ViewGroup) inflater.inflate(R.layout.firstrun_basepanel_checkable_fragment, container, false);
@@ -39,42 +29,19 @@ public class FirstrunPanel extends Fragment {
             ((ImageView) root.findViewById(R.id.firstrun_image)).setImageResource(imageRes);
             ((TextView) root.findViewById(R.id.firstrun_text)).setText(textRes);
             ((TextView) root.findViewById(R.id.firstrun_subtext)).setText(subtextRes);
+            ((TextView) root.findViewById(R.id.firstrun_link)).setText(R.string.firstrun_welcome_button_browser);
+
         }
 
         root.findViewById(R.id.firstrun_link).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Telemetry.sendUIEvent(TelemetryContract.Event.ACTION, TelemetryContract.Method.BUTTON, "firstrun-next");
-                pagerNavigation.next();
+                close();
             }
         });
 
+
         return root;
-    }
-
-    public interface PagerNavigation {
-        void next();
-        void finish();
-    }
-    protected PagerNavigation pagerNavigation;
-
-    public void setPagerNavigation(PagerNavigation listener) {
-        this.pagerNavigation = listener;
-    }
-
-    protected void next() {
-        if (pagerNavigation != null) {
-            pagerNavigation.next();
-        }
-    }
-
-    protected void close() {
-        if (pagerNavigation != null) {
-            pagerNavigation.finish();
-        }
-    }
-
-    protected boolean shouldShowBrowserHint() {
-        return showBrowserHint;
     }
 }
