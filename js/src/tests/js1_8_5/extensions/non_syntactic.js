7920369d26b761d408cfed6285579804ba65e77d
Andrew McCreight <continuation@gmail.com>
2017-08-07 16:35:43 -0700

    Bug 1388191 - Add way to test evaluation with envChain in the shell. r=jorendorff
    This is used by Gecko, but it isn't directly testable in the shell.
    
    The new test has to be disabled for jstestbrowser because the special
    option is shell-only.
    
    MozReview-Commit-ID: 6gckRSkD4fQ
    
    --HG--
    extra : rebase_source : beb064d0103c30a737bd4192ac1d3db219dcba24

---
+++ js/src/tests/js1_8_5/extensions/non_syntactic.js
@@ -0,0 +1,36 @@
+// |reftest| skip-if(!xulRuntime.shell) -- needs evaluate()
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/licenses/publicdomain/
+
+// Check references to someVar, both as a variable and on |this|, in
+// various evaluation contexts.
+var someVar = 1;
+
+// Top level.
+assertEq(someVar, 1);
+assertEq(this.someVar, 1);
+
+// Inside evaluate.
+evaluate("assertEq(someVar, 1);");
+evaluate("assertEq(this.someVar, 1);");
+
+// With an object on the scope, no shadowing.
+var someObject = { someOtherField: 2 };
+var evalOpt = { envChainObject: someObject };
+evaluate("assertEq(someVar, 1);", evalOpt);
+evaluate("assertEq(this.someVar, undefined);", evalOpt);
+
+// With an object on the scope, shadowing global.
+someObject = { someVar: 2 };
+evalOpt = { envChainObject: someObject };
+var alsoSomeObject = someObject;
+evaluate("assertEq(someVar, 2);", evalOpt);
+evaluate("assertEq(this.someVar, 2);", evalOpt);
+evaluate("assertEq(this, alsoSomeObject);", evalOpt);
+
+// With an object on the scope, inside a function.
+evaluate("(function() { assertEq(someVar, 2);})()", evalOpt);
+evaluate("(function() { assertEq(this !== alsoSomeObject, true);})()", evalOpt);
+evaluate("(function() { assertEq(this.someVar, 1);})()", evalOpt);
+
+reportCompare(true, true);
