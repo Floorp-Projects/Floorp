77afc4ab392645ccb83fd1920612312b16b04623
Francois Marier <francois@mozilla.com>
2017-08-08 15:52:29 -0700

    Bug 1385484 - Cleanup Safe Browsing prefs and sync the download protection setting. r=hchang
    MozReview-Commit-ID: B42p2Gw8y9i

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1169,6 +1169,7 @@ pref("services.sync.prefs.sync.browser.newtabpage.pinned", true);
 pref("services.sync.prefs.sync.browser.offline-apps.notify", true);
 pref("services.sync.prefs.sync.browser.safebrowsing.phishing.enabled", true);
 pref("services.sync.prefs.sync.browser.safebrowsing.malware.enabled", true);
+pref("services.sync.prefs.sync.browser.safebrowsing.downloads.enabled", true);
 pref("services.sync.prefs.sync.browser.search.update", true);
 pref("services.sync.prefs.sync.browser.sessionstore.restore_on_demand", true);
 pref("services.sync.prefs.sync.browser.startup.homepage", true);
