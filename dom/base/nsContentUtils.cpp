ce152ea930dee5699cd875868f135661d49f8443
Alexandre Poirot <poirot.alex@gmail.com>
2017-08-08 16:29:34 +0200

    Bug 1382377 - Make Console API be a no-op until DevTools are opened at least once. r=baku,jdescottes
    MozReview-Commit-ID: 4xuzRQKH3Vg
    
    --HG--
    extra : rebase_source : 43d8fe53c387d742c5b1c872cebdfbfc870910b0

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -294,6 +294,7 @@ bool nsContentUtils::sIsUserTimingLoggingEnabled = false;
 bool nsContentUtils::sIsFormAutofillAutocompleteEnabled = false;
 bool nsContentUtils::sIsWebComponentsEnabled = false;
 bool nsContentUtils::sIsCustomElementsEnabled = false;
+bool nsContentUtils::sDevToolsEnabled = false;
 bool nsContentUtils::sSendPerformanceTimingNotifications = false;
 bool nsContentUtils::sUseActivityCursor = false;
 bool nsContentUtils::sAnimationsAPICoreEnabled = false;
@@ -702,6 +703,9 @@ nsContentUtils::Init()
 
   Preferences::AddBoolVarCache(&sIsCustomElementsEnabled,
                                "dom.webcomponents.customelements.enabled", false);
+
+  Preferences::AddBoolVarCache(&sDevToolsEnabled,
+                               "devtools.enabled");
 
   Preferences::AddIntVarCache(&sPrivacyMaxInnerWidth,
                               "privacy.window.maxInnerWidth",
@@ -10802,3 +10806,17 @@ nsContentUtils::ExtractErrorValues(JSContext* aCx,
   }
 }
 
+/* static */ bool
+nsContentUtils::DevToolsEnabled(JSContext* aCx)
+{
+  if (NS_IsMainThread()) {
+    return sDevToolsEnabled;
+  }
+
+  workers::WorkerPrivate* workerPrivate = workers::GetWorkerPrivateFromContext(aCx);
+  if (!workerPrivate) {
+    return false;
+  }
+
+  return workerPrivate->DevToolsEnabled();
+}
