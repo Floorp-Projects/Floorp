3fd5dfdd58d499a43b00f8ce90fe83967f9aba0d
Aryeh Gregor <ayg@aryeh.name>
2017-08-08 17:01:27 +0300

    Bug 1175033 - Support custom use counters that can be incremented from C++; r=froydnj
    MozReview-Commit-ID: 7uFDgmM2zqs
    
    --HG--
    extra : rebase_source : 0c456c35162d33da1c72f82ee982d0eef23f5e3d

--- dom/base/UseCounter.h
+++ dom/base/UseCounter.h
@@ -18,10 +18,13 @@ enum UseCounter : int16_t {
     eUseCounter_##interface_##_##name_##_setter,
 #define USE_COUNTER_CSS_PROPERTY(name_, id_) \
     eUseCounter_property_##id_,
+#define USE_COUNTER_CUSTOM(name_, desc_) \
+    eUseCounter_custom_##name_,
 #include "mozilla/dom/UseCounterList.h"
 #undef USE_COUNTER_DOM_METHOD
 #undef USE_COUNTER_DOM_ATTRIBUTE
 #undef USE_COUNTER_CSS_PROPERTY
+#undef USE_COUNTER_CUSTOM
 
 #define DEPRECATED_OPERATION(op_) \
   eUseCounter_##op_,
