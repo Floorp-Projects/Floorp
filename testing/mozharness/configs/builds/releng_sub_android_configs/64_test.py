9d82d58d3423469c0898352443edc06794c908a3
Johan Lorenzo <jlorenzo@mozilla.com>
2017-08-09 16:18:15 +0200

    Bug 1384482 - Rename android-api-15 into android-api-16 r=mtabara
    MozReview-Commit-ID: 4j66pTZEJzq

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_test.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_test.py
@@ -1,8 +1,8 @@
 config = {
     'base_name': 'Android armv7 unit tests %(branch)s',
     'stage_platform': 'android-test',
-    'build_type': 'api-15-opt',
-    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-15-frontend/nightly',
+    'build_type': 'api-16-opt',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16-frontend/nightly',
     'multi_locale_config_platform': 'android',
     # unit tests don't produce a package. So don't collect package metrics.
     'disable_package_metrics': True,
