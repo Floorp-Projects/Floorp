7f8c1a285ab16f81f00800009bb28cb3c5045116
Nicholas Nethercote <nnethercote@mozilla.com>
2017-08-09 20:41:38 +1000

    Bug 1386103 (part 1, attempt 3) - Specify nsAuto[C]String storage size via template parameter. r=dbaron.

--- xpcom/string/string-template-def-char.h
+++ xpcom/string/string-template-def-char.h
@@ -11,6 +11,7 @@
 #define nsTString_CharT                     nsCString
 #define nsTStringRepr_CharT                 nsCStringRepr
 #define nsTFixedString_CharT                nsFixedCString
+#define nsTAutoStringN_CharT                nsAutoCStringN
 #define nsTAutoString_CharT                 nsAutoCString
 #define nsTSubstring_CharT                  nsACString
 #define PrintfAppend_CharT                  PrintfAppend_nsACString
