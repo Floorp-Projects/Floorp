357b81bcfdc4e30963cbc56206ea6233a09f6238
Thomas Wisniewski <wisniewskit@gmail.com>
2017-08-08 22:55:43 -0400

    Bug 1388591 - Implement OfflineAudioCanvas dict constructor; r=padenot,smaug
    MozReview-Commit-ID: F9h9JO5tYeU
    
    --HG--
    extra : rebase_source : 111cb0d4a574cce4d5c8869e5953dfbb68d01e72

--- dom/media/webaudio/AudioContext.h
+++ dom/media/webaudio/AudioContext.h
@@ -8,6 +8,7 @@
 #define AudioContext_h_
 
 #include "mozilla/dom/AudioChannelBinding.h"
+#include "mozilla/dom/OfflineAudioContextBinding.h"
 #include "MediaBufferDecoder.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/DOMEventTargetHelper.h"
@@ -153,6 +154,12 @@ public:
   // Constructor for regular AudioContext
   static already_AddRefed<AudioContext>
   Constructor(const GlobalObject& aGlobal, ErrorResult& aRv);
+
+  // Constructor for offline AudioContext with options object
+  static already_AddRefed<AudioContext>
+  Constructor(const GlobalObject& aGlobal,
+              const OfflineAudioContextOptions& aOptions,
+              ErrorResult& aRv);
 
   // Constructor for offline AudioContext
   static already_AddRefed<AudioContext>
