3de240864fc0f18df07ef43ac74c9c5a5328d1a6
Johan Lorenzo <jlorenzo@mozilla.com>
2017-08-10 19:25:36 +0200

    Bug 1383771 - Add widevine support to Nightly builds r=aki
    Other related changes:
    * Only target.{zip,tar.gz,tar.bz} get the widevine signature (not the various installers).
    * Linux{32,64}-nightly are now repackaged. Their mar files are not signed during signed step anymore. It now happens after the repackage.
    * As a consequence, funsize routes for linux are now set to repackage-signing (instead of signing)
    * Signed upstream artifacts are now defined in a dedicated module (to avoid duplication)
    * Platforms defined in beetmover_repackage now allow regex (to reduce duplication too)
    * Mozharness configs: Delete unused (and misleading) `src_mozconfig` for windows. This value is actually not used when `run_configure` (in the same dict) is set to False.
    
    MozReview-Commit-ID: COKqevW9Mzn
    
    --HG--
    extra : rebase_source : b9d18b73e288768c3e8f68c6a0b7059ebd73b166

---
+++ testing/mozharness/configs/repackage/linux32_signed.py
@@ -0,0 +1,30 @@
+import os
+
+platform = "linux32"
+
+config = {
+    "input_home": "{abs_work_dir}/inputs",
+    "output_home": "{abs_work_dir}/artifacts{locale}",
+
+    "locale": os.environ.get("LOCALE"),
+
+    "download_config": {
+        "target.tar.gz": os.environ.get("SIGNED_INPUT"),
+        "mar": os.environ.get("UNSIGNED_MAR"),
+    },
+
+    "repackage_config": [[
+        "mar",
+        "-i", "{abs_work_dir}/inputs/target.tar.gz",
+        "--mar", "{abs_work_dir}/inputs/mar",
+        "-o", "{output_home}/target.complete.mar"
+    ]],
+
+    # ToolTool
+    "tooltool_manifest_src": 'browser/config/tooltool-manifests/{}/releng.manifest'.format(platform),
+    "tooltool_url": 'http://relengapi/tooltool/',
+    'tooltool_script': ["/builds/tooltool.py"],
+    'tooltool_cache': os.environ.get('TOOLTOOL_CACHE'),
+
+    'run_configure': False,
+}
