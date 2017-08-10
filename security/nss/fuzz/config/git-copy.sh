11d57944d767fc31cd1f87cead06c78a80249388
Franziskus Kiefer <franziskuskiefer@gmail.com>
2017-08-10 09:22:53 +0200

    Bug 1386955 - land NSS 3e81bdac8449 UPGRADE_NSS_RELEASE, r=me

--- security/nss/fuzz/config/git-copy.sh
+++ security/nss/fuzz/config/git-copy.sh
@@ -7,18 +7,18 @@ if [ $# -lt 3 ]; then
   exit 2
 fi
 
-REPO=$1
-COMMIT=$2
-DIR=$3
+REPO="$1"
+COMMIT="$2"
+DIR="$3"
 
 echo "Copy '$COMMIT' from '$REPO' to '$DIR'"
-if [ -f $DIR/.git-copy ]; then
-  CURRENT=$(cat $DIR/.git-copy)
-  if [ $(echo -n $COMMIT | wc -c) != "40" ]; then
+if [ -f "$DIR"/.git-copy ]; then
+  CURRENT=$(cat "$DIR"/.git-copy)
+  if [ $(echo -n "$COMMIT" | wc -c) != "40" ]; then
     # On the off chance that $COMMIT is a remote head.
-    ACTUAL=$(git ls-remote $REPO $COMMIT | cut -c 1-40 -)
+    ACTUAL=$(git ls-remote "$REPO" "$COMMIT" | cut -c 1-40 -)
   else
-    ACTUAL=$COMMIT
+    ACTUAL="$COMMIT"
   fi
   if [ "$CURRENT" = "$ACTUAL" ]; then
     echo "Up to date."
@@ -26,8 +26,9 @@ if [ -f $DIR/.git-copy ]; then
   fi
 fi
 
-git init -q $DIR
-git -C $DIR fetch -q --depth=1 $REPO $COMMIT:git-copy-tmp
-git -C $DIR reset --hard git-copy-tmp
-git -C $DIR rev-parse --verify HEAD > $DIR/.git-copy
-rm -rf $DIR/.git
+rm -rf "$DIR"
+git init -q "$DIR"
+git -C "$DIR" fetch -q --depth=1 "$REPO" "$COMMIT":git-copy-tmp
+git -C "$DIR" reset --hard git-copy-tmp
+git -C "$DIR" rev-parse --verify HEAD > "$DIR"/.git-copy
+rm -rf "$DIR"/.git
