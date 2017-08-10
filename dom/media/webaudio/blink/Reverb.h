ae8259ec47408102a67331480c9706f78c127819
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-10 11:18:36 +0200

    Backed out changeset be8e60de5c0c (bug 1388656) for heap buffer overflow at AudioNodeEngine.cpp:375:12 in mozilla::AudioBufferSumOfSquares. r=backout

--- dom/media/webaudio/blink/Reverb.h
+++ dom/media/webaudio/blink/Reverb.h
@@ -50,7 +50,8 @@ public:
     // renderSliceSize is a rendering hint, so the FFTs can be optimized to not all occur at the same time (very bad when rendering on a real-time thread).
     Reverb(mozilla::ThreadSharedFloatArrayBufferList* impulseResponseBuffer,
            size_t impulseResponseBufferLength, size_t maxFFTSize,
-           bool useBackgroundThreads, bool normalize, float sampleRate);
+           size_t numberOfChannels, bool useBackgroundThreads, bool normalize,
+           float sampleRate);
 
     void process(const mozilla::AudioBlock* sourceBus,
                  mozilla::AudioBlock* destinationBus);
@@ -62,7 +63,7 @@ public:
 private:
     void initialize(const nsTArray<const float*>& impulseResponseBuffer,
                     size_t impulseResponseBufferLength, size_t maxFFTSize,
-                    bool useBackgroundThreads);
+                    size_t numberOfChannels, bool useBackgroundThreads);
 
     size_t m_impulseResponseLength;
 
