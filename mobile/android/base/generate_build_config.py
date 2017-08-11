edab55cffc7d850225bbeff81356361faf5a40b5
Wes Kocher <wkocher@mozilla.com>
2017-08-11 13:37:22 -0700

    Backed out changeset db3e18c3e3fa (bug 1389257) for conflicting with the incoming m-c merge a=backout
    MozReview-Commit-ID: BoVZfQtAAwZ

--- mobile/android/base/generate_build_config.py
+++ mobile/android/base/generate_build_config.py
@@ -50,6 +50,7 @@ def _defines():
                 'MOZ_ANDROID_PWA',
                 'MOZ_LEANPLUM_SDK_KEY',
                 'MOZ_LEANPLUM_SDK_CLIENTID',
+                'MOZ_ANDROID_POCKET',
                 'MOZ_ANDROID_SEARCH_ACTIVITY',
                 'MOZ_CRASHREPORTER',
                 'MOZ_DEBUG',
