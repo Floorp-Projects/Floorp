6cad66651c01db0d5538b216b542f1b72363e743
Stone Shih <sshih@mozilla.com>
2017-08-11 15:18:33 +0800

    Backed out changeset 0059106aaa20 (bug 1351148)

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3122,7 +3122,7 @@ pref("idle_queue.long_period", 50);
 
 // Control the event prioritization on content main thread
 #ifdef NIGHTLY_BUILD
-pref("prioritized_input_events.enabled", false);
+pref("prioritized_input_events.enabled", true);
 #else
 pref("prioritized_input_events.enabled", false);
 #endif
