8e97249c630169dca15009feca9c6c1667591c99
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-11 09:28:11 +0200

    Backed out changeset d34e59a86079 (bug 1380081)

--- toolkit/components/backgroundhangmonitor/BackgroundHangMonitor.cpp
+++ toolkit/components/backgroundhangmonitor/BackgroundHangMonitor.cpp
@@ -30,10 +30,9 @@
 #include <algorithm>
 
 // Activate BHR only for one every BHR_BETA_MOD users.
-// We're doing experimentation with collecting a lot more data from BHR, and
-// don't want to enable it for beta users at the moment. We can scale this up in
-// the future.
-#define BHR_BETA_MOD INT32_MAX;
+// This is now 100% of Beta population for the Beta 45/46 e10s A/B trials
+// It can be scaled back again in the future
+#define BHR_BETA_MOD 1;
 
 // Maximum depth of the call stack in the reported thread hangs. This value represents
 // the 99.9th percentile of the thread hangs stack depths reported by Telemetry.
