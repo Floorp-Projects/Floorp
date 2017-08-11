4ef5979801031a6f1d7a3fd3ab541617d77d9f84
loongson <loongson@localhost.localdomain>
2017-08-11 00:42:00 -0400

    Bug 1389401 - MIPS: bug 1335068 Break wasm globals out of the code segment. r=luke

--- js/src/jit/mips32/MacroAssembler-mips32.h
+++ js/src/jit/mips32/MacroAssembler-mips32.h
@@ -996,12 +996,10 @@ class MacroAssemblerMIPSCompat : public MacroAssemblerMIPS
         as_movs(dest, src);
     }
     void loadWasmGlobalPtr(uint32_t globalDataOffset, Register dest) {
-        loadPtr(Address(GlobalReg, globalDataOffset - WasmGlobalRegBias), dest);
+        loadPtr(Address(WasmTlsReg, offsetof(wasm::TlsData, globalArea) + globalDataOffset), dest);
     }
     void loadWasmPinnedRegsFromTls() {
         loadPtr(Address(WasmTlsReg, offsetof(wasm::TlsData, memoryBase)), HeapReg);
-        loadPtr(Address(WasmTlsReg, offsetof(wasm::TlsData, globalData)), GlobalReg);
-        ma_addu(GlobalReg, Imm32(WasmGlobalRegBias));
     }
 
     // Instrumentation for entering and leaving the profiler.
