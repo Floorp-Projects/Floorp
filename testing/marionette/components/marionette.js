409f485355351990151d60f23cf3f44355502912
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-11 09:06:41 +0200

    Backed out changeset 69f808119fa3 (bug 1254136) for failing marionette-headless' test_cli_arguments.py TestCommandLineArguments.test_start_in_safe_mode on OS X and in marionette on Linux opt. r=backout

--- testing/marionette/components/marionette.js
+++ testing/marionette/components/marionette.js
@@ -198,7 +198,6 @@ MarionetteComponent.prototype.observe = function(subject, topic, data) {
     case "command-line-startup":
       Services.obs.removeObserver(this, topic);
       this.handle(subject);
-      break;
 
     case "profile-after-change":
       // Using sessionstore-windows-restored as the xpcom category doesn't
