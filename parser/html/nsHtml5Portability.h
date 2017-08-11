c8858d701f2850fffbe5797331a7de8f7481afe9
Henri Sivonen <hsivonen@hsivonen.fi>
2017-08-11 09:22:57 +0300

    Bug 1375701 - Atomize class attribute value in the parser in the innerHTML case. r=Ehsan
    MozReview-Commit-ID: CKyGlzYS15e

--- parser/html/nsHtml5Portability.h
+++ parser/html/nsHtml5Portability.h
@@ -61,7 +61,8 @@ class nsHtml5Portability
     static nsHtml5String newStringFromBuffer(char16_t* buf,
                                              int32_t offset,
                                              int32_t length,
-                                             nsHtml5TreeBuilder* treeBuilder);
+                                             nsHtml5TreeBuilder* treeBuilder,
+                                             bool maybeAtomize);
     static nsHtml5String newEmptyString();
     static nsHtml5String newStringFromLiteral(const char* literal);
     static nsHtml5String newStringFromString(nsHtml5String string);
