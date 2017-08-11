7e57abcf5804ce2dd2984f5cc7c83236911cc35b
Andrea Marchesini <amarchesini@mozilla.com>
2017-08-10 18:04:55 -0700

    Bug 1128959 - Implement the WHATWG Streams spec - part 5 - Disable StreamS API by default, r=bkelly

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -9033,6 +9033,25 @@ nsContentUtils::PushEnabled(JSContext* aCx, JSObject* aObj)
 
 // static
 bool
+nsContentUtils::StreamsEnabled(JSContext* aCx, JSObject* aObj)
+{
+  if (NS_IsMainThread()) {
+    return Preferences::GetBool("dom.streams.enabled", false);
+  }
+
+  using namespace workers;
+
+  // Otherwise, check the pref via the WorkerPrivate
+  WorkerPrivate* workerPrivate = GetWorkerPrivateFromContext(aCx);
+  if (!workerPrivate) {
+    return false;
+  }
+
+  return workerPrivate->StreamsEnabled();
+}
+
+// static
+bool
 nsContentUtils::IsNonSubresourceRequest(nsIChannel* aChannel)
 {
   nsLoadFlags loadFlags = 0;
