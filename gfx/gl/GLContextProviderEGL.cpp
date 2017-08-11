2c267f6dc65aa8c1ecc47fafd96dee22d5897600
Jeff Gilbert <jgilbert@mozilla.com>
2017-08-10 18:00:02 -0700

    Bug 1390386 - Make MakeCurrent const. - r=daoshengmu
    MozReview-Commit-ID: 3m3T6iMgZOc

--- gfx/gl/GLContextProviderEGL.cpp
+++ gfx/gl/GLContextProviderEGL.cpp
@@ -339,7 +339,8 @@ GLContextEGL::SetEGLSurfaceOverride(EGLSurface surf) {
 }
 
 bool
-GLContextEGL::MakeCurrentImpl(bool aForce) {
+GLContextEGL::MakeCurrentImpl(bool aForce) const
+{
     bool succeeded = true;
 
     // Assume that EGL has the same problem as WGL does,
@@ -374,7 +375,8 @@ GLContextEGL::MakeCurrentImpl(bool aForce) {
 }
 
 bool
-GLContextEGL::IsCurrent() {
+GLContextEGL::IsCurrent() const
+{
     return sEGLLibrary.fGetCurrentContext() == mContext;
 }
 
