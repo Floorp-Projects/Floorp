7a1209abe14633ce68fc78ced4bf694a4ef3460f
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-12 13:48:06 +0200

    Backed out changeset 35420ca15900 (bug 1379256)

--- toolkit/components/osfile/modules/osfile_async_worker.js
+++ toolkit/components/osfile/modules/osfile_async_worker.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-env worker */
 
 if (this.Components) {
   throw new Error("This worker can only be loaded from a worker thread");
