27452223f38510d15d2d16978936255bb6ac8e8b
Eric Rahm <erahm@mozilla.com>
2017-08-11 17:46:15 -0700

    Bug 1389598 - Part 2: Remove gonk references from media/ r=jesup

--- media/webrtc/trunk/webrtc/modules/audio_device/linux/pulseaudiosymboltable_linux.cc
+++ media/webrtc/trunk/webrtc/modules/audio_device/linux/pulseaudiosymboltable_linux.cc
@@ -29,7 +29,7 @@
 
 namespace webrtc_adm_linux_pulse {
 
-#if defined(__OpenBSD__) || defined(WEBRTC_GONK)
+#if defined(__OpenBSD__)
 LATE_BINDING_SYMBOL_TABLE_DEFINE_BEGIN(PulseAudioSymbolTable, "libpulse.so")
 #else
 LATE_BINDING_SYMBOL_TABLE_DEFINE_BEGIN(PulseAudioSymbolTable, "libpulse.so.0")
