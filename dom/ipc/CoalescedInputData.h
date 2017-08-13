3cadc1130d43f195851d4956c102241a176aa3bf
Stone Shih <sshih@mozilla.com>
2017-08-13 14:08:48 +0800

    Bug 1361067 Part1: Refactor coalescing mouse wheel events. r=smaug.
    MozReview-Commit-ID: 3NJJ77zAxNJ

---
+++ dom/ipc/CoalescedInputData.h
@@ -0,0 +1,65 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_CoalescedInputData_h
+#define mozilla_dom_CoalescedInputData_h
+
+#include "mozilla/UniquePtr.h"
+#include "FrameMetrics.h"
+
+namespace mozilla {
+namespace dom {
+
+template<class InputEventType>
+class CoalescedInputData
+{
+protected:
+  typedef mozilla::layers::ScrollableLayerGuid ScrollableLayerGuid;
+
+  UniquePtr<InputEventType> mCoalescedInputEvent;
+  ScrollableLayerGuid mGuid;
+  uint64_t mInputBlockId;
+
+public:
+  CoalescedInputData()
+    : mInputBlockId(0)
+  {
+  }
+
+  void Reset()
+  {
+    mCoalescedInputEvent = nullptr;
+  }
+
+  bool IsEmpty()
+  {
+    return !mCoalescedInputEvent;
+  }
+
+  bool CanCoalesce(const InputEventType& aEvent,
+                   const ScrollableLayerGuid& aGuid,
+                   const uint64_t& aInputBlockId);
+
+  const InputEventType* GetCoalescedEvent()
+  {
+    return mCoalescedInputEvent.get();
+  }
+
+  ScrollableLayerGuid GetScrollableLayerGuid()
+  {
+    return mGuid;
+  }
+
+  uint64_t GetInputBlockId()
+  {
+    return mInputBlockId;
+  }
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_CoalescedInputData_h
