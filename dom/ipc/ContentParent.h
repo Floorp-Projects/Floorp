a6c9ae790c91fdfeb4a62feff7f86ed227fe9def
Kris Maglione <maglione.k@gmail.com>
2017-08-13 19:00:49 -0700

    Bug 1390010: Part 1 - Allow JS callers to retrieve parent message manager from ContentParent. r=krizsa
    JS code is notified when a new ContentParent is created via normal
    "ipc:content-created" notifications, but can't do anything with it, since
    nsIContentParent is not scriptable. This allows JS callers to retrieve the
    parent process message manager, which is the normal way they interact with
    content children.
    
    MozReview-Commit-ID: 7lcZ4XkJ6uR

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -26,6 +26,7 @@
 #include "nsPluginTags.h"
 #include "nsFrameMessageManager.h"
 #include "nsHashKeys.h"
+#include "nsIInterfaceRequestor.h"
 #include "nsIObserver.h"
 #include "nsIThreadInternal.h"
 #include "nsIDOMGeoPositionCallback.h"
@@ -109,6 +110,7 @@ class ContentParent final : public PContentParent
                           , public nsIObserver
                           , public nsIDOMGeoPositionCallback
                           , public nsIDOMGeoPositionErrorCallback
+                          , public nsIInterfaceRequestor
                           , public gfx::gfxVarReceiver
                           , public mozilla::LinkedListElement<ContentParent>
                           , public gfx::GPUProcessListener
@@ -310,6 +312,7 @@ public:
   NS_DECL_NSIOBSERVER
   NS_DECL_NSIDOMGEOPOSITIONCALLBACK
   NS_DECL_NSIDOMGEOPOSITIONERRORCALLBACK
+  NS_DECL_NSIINTERFACEREQUESTOR
 
   /**
    * MessageManagerCallback methods that we override.
