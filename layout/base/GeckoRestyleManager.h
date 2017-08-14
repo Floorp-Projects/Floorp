362d6ee69e5434fc137f7edcdfe0e6f9c77f5cf4
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2017-08-14 20:00:04 +0900

    Bug 1388692 - Make GeckoRestyleManager::UpdateAnimationStyles() private. r=birtles
    It's only used inside the class.
    
    MozReview-Commit-ID: IrIUwrDkUCB

--- layout/base/GeckoRestyleManager.h
+++ layout/base/GeckoRestyleManager.h
@@ -216,7 +216,6 @@ private:
   void BeginProcessingRestyles(RestyleTracker& aRestyleTracker);
   void EndProcessingRestyles();
 
-public:
   // Perform any pending animation restyles.
   //
   // This is a superset of the work performed by
@@ -240,6 +239,8 @@ public:
   // compare against.  When we do this, we don't bother touching frames
   // other than primary frames.
   void UpdateAnimationStyles();
+
+public:
   void UpdateAnimationStylesForHitTesting();
 
   // Rebuilds all style data by throwing out the old rule tree and
