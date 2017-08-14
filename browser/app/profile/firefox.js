2209d4ad7b5af38b10e03a8698b43c06fe543cf6
gasolin <gasolin@gmail.com>
2017-08-14 14:30:46 +0800

    Bug 1366056 - showing v57 tourset for new user tour and update user tour;r=Fischer,mossop,rexboy
    MozReview-Commit-ID: JUmPyJiNKFx

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1708,7 +1708,7 @@ pref("browser.suppress_first_window_animation", true);
 // Preferences for Photon onboarding system extension
 pref("browser.onboarding.enabled", true);
 // Mark this as an upgraded profile so we don't offer the initial new user onboarding tour.
-pref("browser.onboarding.tourset-version", 1);
+pref("browser.onboarding.tourset-version", 2);
 pref("browser.onboarding.hidden", false);
 // On the Activity-Stream page, the snippet's position overlaps with our notification.
 // So use `browser.onboarding.notification.finished` to let the AS page know
@@ -1717,8 +1717,8 @@ pref("browser.onboarding.notification.finished", false);
 pref("browser.onboarding.notification.mute-duration-on-first-session-ms", 300000); // 5 mins
 pref("browser.onboarding.notification.max-life-time-per-tour-ms", 432000000); // 5 days
 pref("browser.onboarding.notification.max-prompt-count-per-tour", 8);
-pref("browser.onboarding.newtour", "private,addons,customize,search,default,sync");
-pref("browser.onboarding.updatetour", "");
+pref("browser.onboarding.newtour", "performance,private,screenshots,addons,customize,default");
+pref("browser.onboarding.updatetour", "performance,library,screenshots,singlesearch,customize,sync");
 
 // Preference that allows individual users to disable Screenshots.
 pref("extensions.screenshots.disabled", false);
