f1de84df7741789073b38ff9a0307acb29f97abc
Michael Layzell <michael@thelayzells.com>
2017-08-14 11:11:57 +1000

    Bug 1384766 - Reduce expected frame count for GeckoProfiler.SuspendAndSample. r=njn, a=test-only

--- tools/profiler/tests/gtest/GeckoProfiler.cpp
+++ tools/profiler/tests/gtest/GeckoProfiler.cpp
@@ -770,7 +770,7 @@ void DoSuspendAndSample(int aTid, nsIThread* aThread)
                                            /* sampleNative = */ true);
 
         ASSERT_TRUE(collector.mSetIsMainThread == 1);
-        ASSERT_TRUE(collector.mFrames > 5); // approximate; must be > 0
+        ASSERT_TRUE(collector.mFrames > 0);
       }),
     NS_DISPATCH_SYNC);
 }
