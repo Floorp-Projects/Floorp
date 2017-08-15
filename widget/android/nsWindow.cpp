2b39fb10aa8c84e555a9a997f0cba38744e2f1fe
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-15 18:30:03 +0200

    Backed out changeset 6798fd30a785 (bug 1126479)

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -1781,12 +1781,6 @@ nsWindow::SetZIndex(int32_t aZIndex)
 void
 nsWindow::SetSizeMode(nsSizeMode aMode)
 {
-    if (aMode == mSizeMode) {
-        return;
-    }
-
-    nsBaseWidget::SetSizeMode(aMode);
-
     switch (aMode) {
         case nsSizeMode_Minimized:
             GeckoAppShell::MoveTaskToBack();
@@ -1936,8 +1930,6 @@ nsWindow::MakeFullScreen(bool aFullScreen, nsIScreen*)
 
     nsIWidgetListener* listener = GetWidgetListener();
     if (listener) {
-        mSizeMode = mIsFullScreen ? nsSizeMode_Fullscreen : nsSizeMode_Normal;
-        listener->SizeModeChanged(mSizeMode);
         listener->FullscreenChanged(mIsFullScreen);
     }
     return NS_OK;
