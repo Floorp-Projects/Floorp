af1bc71c5dcf0bb51a392f5c6eaf77466e7c74a4
Emilio Cobos Álvarez <emilio@crisal.io>
2017-08-15 23:21:30 +0200

    Bug 1390650: stylo: Remove some unused FFI functions. r=bholley,xidorn
    MozReview-Commit-ID: K9fXGRmgkr4
    
    --HG--
    extra : rebase_source : 2428cd915b9d7f4b75b23df99e9d532034ff63a7

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -136,8 +136,6 @@ struct FontSizePrefs
 };
 
 // DOM Traversal.
-uint32_t Gecko_ChildrenCount(RawGeckoNodeBorrowed node);
-bool Gecko_NodeIsElement(RawGeckoNodeBorrowed node);
 bool Gecko_IsInDocument(RawGeckoNodeBorrowed node);
 bool Gecko_FlattenedTreeParentIsParent(RawGeckoNodeBorrowed node);
 bool Gecko_IsSignificantChild(RawGeckoNodeBorrowed node,
@@ -176,12 +174,9 @@ Gecko_LoadStyleSheet(mozilla::css::Loader* loader,
 // Selector Matching.
 uint64_t Gecko_ElementState(RawGeckoElementBorrowed element);
 uint64_t Gecko_DocumentState(const nsIDocument* aDocument);
-bool Gecko_IsTextNode(RawGeckoNodeBorrowed node);
 bool Gecko_IsRootElement(RawGeckoElementBorrowed element);
 bool Gecko_MatchesElement(mozilla::CSSPseudoClassType type, RawGeckoElementBorrowed element);
-nsIAtom* Gecko_LocalName(RawGeckoElementBorrowed element);
 nsIAtom* Gecko_Namespace(RawGeckoElementBorrowed element);
-nsIAtom* Gecko_GetElementId(RawGeckoElementBorrowed element);
 bool Gecko_MatchLang(RawGeckoElementBorrowed element,
                      nsIAtom* override_lang, bool has_override_lang,
                      const char16_t* value);
@@ -383,7 +378,6 @@ nsStyleContentData::CounterFunction* Gecko_SetCounterFunction(
     nsStyleContentData* content_data, nsStyleContentType type);
 
 // Dirtiness tracking.
-uint32_t Gecko_GetNodeFlags(RawGeckoNodeBorrowed node);
 void Gecko_SetNodeFlags(RawGeckoNodeBorrowed node, uint32_t flags);
 void Gecko_UnsetNodeFlags(RawGeckoNodeBorrowed node, uint32_t flags);
 void Gecko_NoteDirtyElement(RawGeckoElementBorrowed element);
@@ -402,7 +396,6 @@ uint32_t Gecko_CalcStyleDifference(ServoStyleContextBorrowed old_style,
                                    ServoStyleContextBorrowed new_style,
                                    uint64_t old_style_bits,
                                    bool* any_style_changed);
-nsChangeHint Gecko_HintsHandledForDescendants(nsChangeHint aHint);
 
 // Get an element snapshot for a given element from the table.
 const ServoElementSnapshot*
