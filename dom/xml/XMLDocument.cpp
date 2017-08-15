4dfa602312cf92ec0adae75fe7f36754c7bfbcd0
Bobby Holley <bobbyholley@gmail.com>
2017-08-14 19:50:28 -0700

    Bug 1389300 - Inherit style backend into NS_NewDOMDocument. r=smaug,r=heycam
    Our current machinery for enabling stylo requires a docshell - if there isn't
    one, we default to the Gecko style system.
    
    When getComputedStyle operates on an element without a presshell, it uses the
    caller's presshell instead. If the element has previously been styled with
    one style system (but no longer has a presshell), and the caller uses a
    different style backend, using the caller's style system can cause crashes when
    we pull bits of cached data off the DOM (like cached style attributes).
    
    So we want to throw when window.getComputedStyle(element) is called for a
    (window, element) pair with different style backends (which is what the next
    patch in this bug does).
    
    However, that causes a few failures where stylo-backed documents try to do
    getComputedStyle on an XHR document (which, without a docshell, will use the
    gecko style system).
    
    So this patch does some work to propagate the creator's style backend into
    various docshell-less documents. This should allow both chrome (which uses gecko)
    and content (which uses stylo) to use getComputedStyle on the response document
    for XHRs they create.
    
    Note that the second patch in this bug will make
    chromeWin.getComputedStyle(contentObj) throw. If we discover code that does
    that, we can just make it invoke the content's getComputedStyle method over Xrays.
    
    MozReview-Commit-ID: 5OsmHJKq5Ui

--- dom/xml/XMLDocument.cpp
+++ dom/xml/XMLDocument.cpp
@@ -69,7 +69,8 @@ NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
                   nsIPrincipal* aPrincipal,
                   bool aLoadedAsData,
                   nsIGlobalObject* aEventObject,
-                  DocumentFlavor aFlavor)
+                  DocumentFlavor aFlavor,
+                  StyleBackendType aStyleBackend)
 {
   // Note: can't require that aDocumentURI/aBaseURI/aPrincipal be non-null,
   // since at least one caller (XMLHttpRequest) doesn't have decent args to
@@ -126,6 +127,12 @@ NS_NewDOMDocument(nsIDOMDocument** aInstancePtrResult,
 
   if (NS_FAILED(rv)) {
     return rv;
+  }
+
+  // If we were passed an explicit style backend for this document set it
+  // immediately after creation, before any content is inserted.
+  if (aStyleBackend != StyleBackendType::None) {
+    d->SetStyleBackendType(aStyleBackend);
   }
 
   if (isHTML) {
@@ -209,13 +216,15 @@ nsresult
 NS_NewXBLDocument(nsIDOMDocument** aInstancePtrResult,
                   nsIURI* aDocumentURI,
                   nsIURI* aBaseURI,
-                  nsIPrincipal* aPrincipal)
+                  nsIPrincipal* aPrincipal,
+                  StyleBackendType aStyleBackend)
 {
   nsresult rv = NS_NewDOMDocument(aInstancePtrResult,
                                   NS_LITERAL_STRING("http://www.mozilla.org/xbl"),
                                   NS_LITERAL_STRING("bindings"), nullptr,
                                   aDocumentURI, aBaseURI, aPrincipal, false,
-                                  nullptr, DocumentFlavorLegacyGuess);
+                                  nullptr, DocumentFlavorLegacyGuess,
+                                  aStyleBackend);
   NS_ENSURE_SUCCESS(rv, rv);
 
   nsCOMPtr<nsIDocument> idoc = do_QueryInterface(*aInstancePtrResult);
