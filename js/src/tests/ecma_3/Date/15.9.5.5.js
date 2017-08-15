7c3c688857a5d5da3c6fc7818f668a79f2939df2
Ted Campbell <tcampbell@mozilla.com>
2017-08-15 15:39:46 -0400

    Bug 1330149 - Disable tests sensitive to host TZ on Windows. r=anba
    These tests pass when our test infrastruction is in PDT, but there are
    underlying issues. Disable tests on Windows for now to allow migrating
    tests to TaskCluster.
    
    MozReview-Commit-ID: 5HigFMyyaP5

--- js/src/tests/ecma_3/Date/15.9.5.5.js
+++ js/src/tests/ecma_3/Date/15.9.5.5.js
@@ -1,4 +1,4 @@
-// |reftest| random-if(xulRuntime.OS=="Linux")
+// |reftest| random-if(xulRuntime.OS=="Linux") skip-if(xulRuntime.OS=="WINNT")
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- js/src/tests/js1_5/extensions/toLocaleFormat-02.js
+++ js/src/tests/js1_5/extensions/toLocaleFormat-02.js
@@ -1,4 +1,4 @@
-// |reftest| fails-if(xulRuntime.OS=="WINNT") skip-if(xulRuntime.OS=="WINNT"&&isDebugBuild)
+// |reftest| skip-if(xulRuntime.OS=="WINNT")
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
