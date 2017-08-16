f98c906515d3a2a68aaafd9558135172af7e880c
Wes Kocher <wkocher@mozilla.com>
2017-08-16 16:25:30 -0700

    Backed out 2 changesets (bug 1383622) for eslint failures a=backout
    Backed out changeset 60005e2d9ea5 (bug 1383622)
    Backed out changeset d229587489da (bug 1383622)
    
    MozReview-Commit-ID: J7N7iCBLsGZ

--- services/sync/tps/extensions/tps/components/tps-cmdline.js
+++ services/sync/tps/extensions/tps/components/tps-cmdline.js
@@ -21,7 +21,6 @@ const nsIWindowWatcher               = Components.interfaces.nsIWindowWatcher;
 
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://gre/modules/Services.jsm");
-Components.utils.import("resource://gre/modules/osfile.jsm");
 
 function TPSCmdLineHandler() {}
 
@@ -58,7 +57,7 @@ TPSCmdLineHandler.prototype = {
 
     options.ignoreUnusedEngines = cmdLine.handleFlag("ignore-unused-engines",
                                                      false);
-    let uri = cmdLine.resolveURI(OS.Path.normalize(uristr)).spec;
+    let uri = cmdLine.resolveURI(uristr).spec;
 
     const onStartupFinished = () => {
       Services.obs.removeObserver(onStartupFinished, "browser-delayed-startup-finished");
@@ -69,7 +68,7 @@ TPSCmdLineHandler.prototype = {
       ios.offline = false;
       Components.utils.import("resource://tps/tps.jsm");
       Components.utils.import("resource://tps/quit.js", TPS);
-      TPS.RunTestPhase(uri, phase, logfile, options).catch(err => TPS.DumpError("TestPhase failed", err));
+      TPS.RunTestPhase(uri, phase, logfile, options);
     };
     Services.obs.addObserver(onStartupFinished, "browser-delayed-startup-finished");
   },
