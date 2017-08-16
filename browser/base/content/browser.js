a478b0ef54bf6136836a9f8dbdf848e6a8794fdc
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-08-16 11:15:50 +0200

    Backed out changeset adf5ed713e0d (bug 1376895)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1088,14 +1088,6 @@ function _loadURIWithFlags(browser, uri, params) {
   }
 
   let mustChangeProcess = requiredRemoteType != currentRemoteType;
-  let newFrameloader = false;
-  if (browser.getAttribute("isPreloadBrowser") == "true" && uri != "about:newtab") {
-    // Leaving about:newtab from a used to be preloaded browser should run the process
-    // selecting algorithm again.
-    mustChangeProcess = true;
-    newFrameloader = true;
-    browser.removeAttribute("isPreloadBrowser");
-  }
 
   // !requiredRemoteType means we're loading in the parent/this process.
   if (!requiredRemoteType) {
@@ -1130,8 +1122,7 @@ function _loadURIWithFlags(browser, uri, params) {
         referrer: referrer ? referrer.spec : null,
         referrerPolicy,
         remoteType: requiredRemoteType,
-        postData,
-        newFrameloader,
+        postData
       }
 
       if (params.userContextId) {
@@ -1176,11 +1167,6 @@ function LoadInOtherProcess(browser, loadOptions, historyIndex = -1) {
 // Called when a docshell has attempted to load a page in an incorrect process.
 // This function is responsible for loading the page in the correct process.
 function RedirectLoad({ target: browser, data }) {
-  if (browser.getAttribute("isPreloadBrowser") == "true") {
-    browser.removeAttribute("isPreloadBrowser");
-    data.loadOptions.newFrameloader = true;
-  }
-
   if (data.loadOptions.reloadInFreshProcess) {
     // Convert the fresh process load option into a large allocation remote type
     // to use common processing from this point.
