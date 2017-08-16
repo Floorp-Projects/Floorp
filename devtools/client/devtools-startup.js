0d599a6f42df94e603d7342eb60fabb4c9a8d547
Julian Descottes <jdescottes@mozilla.com>
2017-08-16 10:18:36 +0200

    Bug 1386821 - Init devtools if developer toolbar is enabled. r=ochameau, a=lizzard
    MozReview-Commit-ID: 57vThSXG4zq

--- devtools/client/devtools-startup.js
+++ devtools/client/devtools-startup.js
@@ -26,6 +26,11 @@ const kDebuggerPrefs = [
   "devtools.debugger.remote-enabled",
   "devtools.chrome.enabled"
 ];
+
+// If devtools.toolbar.visible is set to true, the developer toolbar should appear on
+// startup.
+const TOOLBAR_VISIBLE_PREF = "devtools.toolbar.visible";
+
 const { XPCOMUtils } = Cu.import("resource://gre/modules/XPCOMUtils.jsm", {});
 XPCOMUtils.defineLazyModuleGetter(this, "Services",
                                   "resource://gre/modules/Services.jsm");
@@ -161,6 +166,19 @@ function DevToolsStartup() {}
 
 DevToolsStartup.prototype = {
   /**
+   * Boolean flag to check if DevTools have been already initialized or not.
+   * By initialized, we mean that its main modules are loaded.
+   */
+  initialized: false,
+
+  /**
+   * Boolean flag to check if the devtools initialization was already sent to telemetry.
+   * We only want to record one devtools entry point per Firefox run, but we are not
+   * interested in all the entry points (e.g. devtools.toolbar.visible).
+   */
+  recorded: false,
+
+  /**
    * Flag that indicates if the developer toggle was already added to customizableUI.
    */
   developerToggleCreated: false,
@@ -192,6 +210,12 @@ DevToolsStartup.prototype = {
     // Only top level Firefox Windows fire a browser-delayed-startup-finished event
     let onWindowReady = window => {
       this.hookWindow(window);
+
+      if (Services.prefs.getBoolPref(TOOLBAR_VISIBLE_PREF, false)) {
+        // Loading devtools-browser will open the developer toolbar by also checking this
+        // pref.
+        this.initDevTools();
+      }
 
       if (devtoolsFlag) {
         this.handleDevToolsFlag(window);
@@ -359,14 +383,8 @@ DevToolsStartup.prototype = {
     return k;
   },
 
-  /**
-   * Boolean flag to check if DevTools have been already initialized or not.
-   * By initialized, we mean that its main modules are loaded.
-   */
-  initialized: false,
-
   initDevTools: function (reason) {
-    if (!this.initialized) {
+    if (reason && !this.recorded) {
       // Only save the first call for each firefox run as next call
       // won't necessarely start the tool. For example key shortcuts may
       // only change the currently selected tool.
@@ -376,6 +394,7 @@ DevToolsStartup.prototype = {
       } catch (e) {
         dump("DevTools telemetry entry point failed: " + e + "\n");
       }
+      this.recorded = true;
     }
     this.initialized = true;
     let { require } = Cu.import("resource://devtools/shared/Loader.jsm", {});
