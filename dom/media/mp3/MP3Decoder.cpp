991dc3fef65898fb1dc628501d6007dee5feae78
Chris Pearce <cpearce@mozilla.com>
2017-08-16 17:19:39 +1200

    Bug 1390748 - Remove unnecessary includes in *Decoders. r=jwwang
    MozReview-Commit-ID: 3bfAdpopOfp

--- dom/media/mp3/MP3Decoder.cpp
+++ dom/media/mp3/MP3Decoder.cpp
@@ -1,4 +1,3 @@
-
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim:set ts=2 sw=2 sts=2 et cindent: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
@@ -6,9 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MP3Decoder.h"
-
 #include "MediaContainerType.h"
-#include "MP3Demuxer.h"
 #include "PDMFactory.h"
 
 namespace mozilla {
