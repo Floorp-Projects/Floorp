79818d4848f8d3853f5cd2cb7bfdf828cc3a2fa7
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2017-08-16 15:54:36 +0100

    Bug 1371765 - make downloads button non-customizable, r=jaws,Paolo
    MozReview-Commit-ID: E9izQpa4fFZ

--- browser/components/customizableui/test/browser_1161838_inserted_new_default_buttons.js
+++ browser/components/customizableui/test/browser_1161838_inserted_new_default_buttons.js
@@ -17,7 +17,7 @@ function test() {
   CustomizableUIInternal.saveState();
   CustomizableUIInternal.loadSavedState();
 
-  CustomizableUIInternal._introduceNewBuiltinWidgets();
+  CustomizableUIInternal._updateForNewVersion();
   is(gFuturePlacements.size, 0,
      "No change to future placements initially.");
 
@@ -44,7 +44,7 @@ function test() {
 
   let savedPlacements = CustomizableUIBSPass.gSavedState.placements[CustomizableUI.AREA_NAVBAR];
   // Then call the re-init routine so we re-add the builtin widgets
-  CustomizableUIInternal._introduceNewBuiltinWidgets();
+  CustomizableUIInternal._updateForNewVersion();
   is(gFuturePlacements.size, 1,
      "Should have 1 more future placement");
   let futureNavbarPlacements = gFuturePlacements.get(CustomizableUI.AREA_NAVBAR);
