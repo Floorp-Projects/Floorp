d6f95f047704a13dca6cee0d9e0e7fd0db71bd65
Johan Lorenzo <jlorenzo@mozilla.com>
2017-08-16 13:52:36 +0200

    Bug 1390123 - part 2: Fix macaroon location r=mtabara a=release
    MozReview-Commit-ID: 2BRepPICsYE
    
    --HG--
    extra : amend_source : 6690895a6d265a285e86112f49aa398d0499b2f5

--- taskcluster/docker/firefox-snap/fetch_macaroons.sh
+++ taskcluster/docker/firefox-snap/fetch_macaroons.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-url=$1
-config=~/.snapcraft/snapcraft.cfg
+set -ex
 
-mkdir -p ~/.snapcraft
-curl -s $url | \
+url="$1"
+
+CONFIG="$HOME/.config/snapcraft/snapcraft.cfg"
+
+mkdir -p "$( dirname "$CONFIG" )"
+curl -s "$url" | \
     python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["content"]' | \
     base64 -d > \
-    $config
-chmod 600 $config
+    "$CONFIG"
+chmod 600 "$CONFIG"
