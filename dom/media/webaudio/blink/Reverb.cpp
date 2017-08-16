6b393f6589be9d64ab71cc1949e3b00ccbe3ab24
Karl Tomlinson <karlt+@karlt.net>
2017-08-16 12:32:32 +1200

    bug 1389641 add 'f' to literal initializer for float const r=trivial
    Address MSVC build failures.

--- dom/media/webaudio/blink/Reverb.cpp
+++ dom/media/webaudio/blink/Reverb.cpp
@@ -38,7 +38,7 @@ using namespace mozilla;
 namespace WebCore {
 
 // Empirical gain calibration tested across many impulse responses to ensure perceived volume is same as dry (unprocessed) signal
-const float GainCalibration = 0.00125;
+const float GainCalibration = 0.00125f;
 const float GainCalibrationSampleRate = 44100;
 
 // A minimum power value to when normalizing a silent (or very quiet) impulse response
