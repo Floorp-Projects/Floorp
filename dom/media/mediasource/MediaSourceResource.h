bb7b361ad41cd2e369ca881e7f9d81ac6911feb1
JW Wang <jwwang@mozilla.com>
2017-08-16 15:37:29 +0800

    Bug 1390443. P2 - remove unused IsExpectingMoreData(). r=cpearce
    MozReview-Commit-ID: KTxmjIrD1WB

--- dom/media/mediasource/MediaSourceResource.h
+++ dom/media/mediasource/MediaSourceResource.h
@@ -70,12 +70,6 @@ public:
     mEnded = aEnded;
   }
 
-  bool IsExpectingMoreData() override
-  {
-    MonitorAutoLock mon(mMonitor);
-    return !mEnded;
-  }
-
 private:
   size_t SizeOfExcludingThis(MallocSizeOf aMallocSizeOf) const override
   {
