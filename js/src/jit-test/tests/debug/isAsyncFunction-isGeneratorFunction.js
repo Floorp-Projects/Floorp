a60559c3d831908998821ece2ad4e71eab4037e0
Tooru Fujisawa <arai_a@mac.com>
2017-08-17 01:31:19 +0900

    Bug 1390730 - Remove nightly-only guard from tests for async iteration. r=jimb

--- js/src/jit-test/tests/debug/isAsyncFunction-isGeneratorFunction.js
+++ js/src/jit-test/tests/debug/isAsyncFunction-isGeneratorFunction.js
@@ -1,8 +1,6 @@
 // Debugger.Script.prototype.isAsyncFunction, Debugger.Object.prototype.isAsyncFunction,
 // Debugger.Script.prototype.isGeneratorFunction, Debugger.Object.prototype.isGeneratorFunction
 
-load(libdir + 'nightly-only.js');
-
 var g = newGlobal();
 var dbg = new Debugger();
 var gDO = dbg.addDebuggee(g);
@@ -31,19 +29,15 @@ checkExpr('non_debuggee', { isAsync: undefined, isGenerator: undefined });
 checkExpr('(function(){})', { isAsync: false, isGenerator: false });
 checkExpr('(function*(){})', { isAsync: false, isGenerator: true });
 checkExpr('(async function snerf(){})', { isAsync: true, isGenerator: false });
-nightlyOnly(g.SyntaxError, () => {
-  checkExpr('(async function* omlu(){})', { isAsync: true, isGenerator: true });
-});
+checkExpr('(async function* omlu(){})', { isAsync: true, isGenerator: true });
 
 checkExpr('new Function("1+2")', { isAsync: false, isGenerator: false });
 checkExpr('Object.getPrototypeOf(function*(){}).constructor("1+2")',
           { isAsync: false, isGenerator: true });
 checkExpr('Object.getPrototypeOf(async function(){}).constructor("1+2")',
           { isAsync: true, isGenerator: false });
-nightlyOnly(g.SyntaxError, () => {
-  checkExpr('Object.getPrototypeOf(async function*(){}).constructor("1+2")',
-            { isAsync: true, isGenerator: true });
-});
+checkExpr('Object.getPrototypeOf(async function*(){}).constructor("1+2")',
+          { isAsync: true, isGenerator: true });
 
 // Check eval scripts.
 function checkFrame(expr, type)
