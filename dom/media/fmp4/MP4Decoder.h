5d80e797029c86550bd187d2c37222fee4d7eec8
Chris Pearce <cpearce@mozilla.com>
2017-08-16 17:19:39 +1200

    Bug 1390748 - Remove unnecessary includes in *Decoders. r=jwwang
    MozReview-Commit-ID: 3bfAdpopOfp

--- dom/media/fmp4/MP4Decoder.h
+++ dom/media/fmp4/MP4Decoder.h
@@ -6,13 +6,14 @@
 #if !defined(MP4Decoder_h_)
 #define MP4Decoder_h_
 
-#include "ChannelMediaDecoder.h"
+class nsACString;
 #include "mozilla/dom/Promise.h"
 #include "mozilla/layers/KnowsCompositor.h"
 
 namespace mozilla {
 
 class MediaContainerType;
+class DecoderDoctorDiagnostics;
 
 // Decoder that uses a bundled MP4 demuxer and platform decoders to play MP4.
 class MP4Decoder
