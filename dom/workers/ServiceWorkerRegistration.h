502a2f107a26dc56e9ad9d24bfb51d091422b483
Ho-Pang Hsu <hopang.hsu@gmail.com>
2017-08-16 14:21:29 +0800

    Bug 1353636 - Part 2: Expose ServieworkerRegistration.updateViaCache. r=bkelly
    --HG--
    extra : rebase_source : 51eb4aebc78f0e48487e88ce5f9f9b70bf8efadd

--- dom/workers/ServiceWorkerRegistration.h
+++ dom/workers/ServiceWorkerRegistration.h
@@ -10,6 +10,7 @@
 #include "mozilla/DOMEventTargetHelper.h"
 #include "mozilla/dom/ServiceWorkerBinding.h"
 #include "mozilla/dom/ServiceWorkerCommon.h"
+#include "mozilla/dom/ServiceWorkerRegistrationBinding.h"
 #include "mozilla/dom/workers/bindings/WorkerHolder.h"
 #include "nsContentUtils.h" // Required for nsContentUtils::PushEnabled
 
@@ -89,6 +90,9 @@ public:
 
   virtual void
   GetScope(nsAString& aScope) const = 0;
+
+  virtual ServiceWorkerUpdateViaCache
+  UpdateViaCache() const = 0;
 
   virtual already_AddRefed<Promise>
   Update(ErrorResult& aRv) = 0;
