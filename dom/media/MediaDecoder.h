9619955a2f8adfc4bbe91ac9e58934fae2cc55ac
Carsten "Tomcat" Book <cbook@mozilla.com>
2017-08-17 08:35:36 +0200

    Backed out changeset ad5c7eb1bfe1 (bug 1390748)
    --HG--
    extra : rebase_source : 8aaefbb511c4e003a68fe2cf57541d8c3d2d29f2

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -203,6 +203,7 @@ public:
   // Must be called before Shutdown().
   bool OwnerHasError() const;
 
+public:
   // Returns true if this media supports random seeking. False for example with
   // chained ogg files.
   bool IsMediaSeekable();
