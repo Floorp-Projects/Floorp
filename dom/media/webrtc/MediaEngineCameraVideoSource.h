ddaa2a4fa51ef962dabde83dd631ff384ba8110c
Randell Jesup <rjesup@jesup.org>
2017-08-17 13:54:06 -0400

    Bug 1374164: Clear mImageContainer as well as mImage when stopping a gUM stream r=jib a=lizzard Also removes hiding of mImage/mImageContainer/mMonitor by MediaEngineDefault, general cleanup of MediaEngineDefault shutdown, and fixing locking around access to protected members.
    MozReview-Commit-ID: 2oVttyizTw9

--- dom/media/webrtc/MediaEngineCameraVideoSource.h
+++ dom/media/webrtc/MediaEngineCameraVideoSource.h
@@ -64,15 +64,10 @@ public:
   void Shutdown() override
   {
     MonitorAutoLock lock(mMonitor);
-    // Release mImage and it's resources just in case -- also we can be
-    // held by something in a CC chain, and not be deleted until final-cc,
-    // which is too late for releasing images.  (This should be null'd on
-    // Stop(), but apparently Stop() may not get called in this case
-    // somehow.) (Bug 1374164)
-
+    // really Stop() *should* be called before it gets here
     Unused << NS_WARN_IF(mImage);
-
     mImage = nullptr;
+    mImageContainer = nullptr;
   }
 
 protected:
