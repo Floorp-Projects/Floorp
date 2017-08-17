fee7fb93ca5fc060c686a0f7a06b6c5305e641c5
Alastor Wu <alwu@mozilla.com>
2017-08-17 11:10:31 +0800

    Bug 1388660 - part2 : add test. r=Ehsan
    Add a new file "almostSilentAudioTrack.webm" and the test to prevent showing
    the sound indicator when playing that kinds of video.
    
    MozReview-Commit-ID: CeUhAePBuqs
    
    --HG--
    extra : rebase_source : c1deb2ec749fcebeccb554b64ffe77b46a18aa3d

---
+++ toolkit/content/tests/browser/browser_sound_indicator_silent_video.js
@@ -0,0 +1,67 @@
+const SILENT_PAGE = "https://example.com/browser/toolkit/content/tests/browser/file_silentAudioTrack.html";
+const ALMOST_SILENT_PAGE = "https://example.com/browser/toolkit/content/tests/browser/file_almostSilentAudioTrack.html";
+
+function check_audio_playing_state(isPlaying) {
+  let autoPlay = content.document.getElementById("autoplay");
+  if (!autoPlay) {
+    ok(false, "Can't get the audio element!");
+  }
+
+  is(autoPlay.paused, !isPlaying,
+     "The playing state of autoplay audio is correct.");
+
+  // wait for a while to make sure the video is playing and related event has
+  // been dispatched (if any).
+  let PLAYING_TIME_SEC = 0.5;
+  ok(PLAYING_TIME_SEC < autoPlay.duration, "The playing time is valid.")
+
+  return new Promise(resolve => {
+    autoPlay.ontimeupdate = function() {
+      if (autoPlay.currentTime > PLAYING_TIME_SEC) {
+        resolve();
+      }
+    };
+  });
+}
+
+add_task(async function should_not_show_sound_indicator_for_silent_video() {
+  info("- open new foreground tab -");
+  let tab = await BrowserTestUtils.openNewForegroundTab(window.gBrowser,
+                                                        "about:blank");
+
+  info("- tab should not have sound indicator before playing silent video -");
+  await waitForTabPlayingEvent(tab, false);
+
+  info("- loading autoplay silent video -");
+  tab.linkedBrowser.loadURI(SILENT_PAGE);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  await ContentTask.spawn(tab.linkedBrowser, true /* playing */,
+                          check_audio_playing_state);
+
+  info("- tab should not have sound indicator after playing silent video -");
+  await waitForTabPlayingEvent(tab, false);
+
+  info("- remove tab -");
+  await BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function should_not_show_sound_indicator_for_almost_silent_video() {
+  info("- open new foreground tab -");
+  let tab = await BrowserTestUtils.openNewForegroundTab(window.gBrowser,
+                                                        "about:blank");
+
+  info("- tab should not have sound indicator before playing almost silent video -");
+  await waitForTabPlayingEvent(tab, false);
+
+  info("- loading autoplay almost silent video -");
+  tab.linkedBrowser.loadURI(ALMOST_SILENT_PAGE);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  await ContentTask.spawn(tab.linkedBrowser, true /* playing */,
+                          check_audio_playing_state);
+
+  info("- tab should not have sound indicator after playing almost silent video -");
+  await waitForTabPlayingEvent(tab, false);
+
+  info("- remove tab -");
+  await BrowserTestUtils.removeTab(tab);
+});
