12b351281d49b8702709468cf218bd6cb7a8f102
Wes Kocher <wkocher@mozilla.com>
2017-08-17 13:51:05 -0700

    Backed out changeset 15956a990499 (bug 1369672) for build bustage a=backout CLOSED TREE
    MozReview-Commit-ID: 40Rckwto87s

--- gfx/ots/src/name.h
+++ gfx/ots/src/name.h
@@ -9,7 +9,6 @@
 #include <string>
 #include <utility>
 #include <vector>
-#include <unordered_set>
 
 #include "ots.h"
 
@@ -51,12 +50,10 @@ class OpenTypeNAME : public Table {
 
   bool Parse(const uint8_t *data, size_t length);
   bool Serialize(OTSStream *out);
-  bool IsValidNameId(uint16_t nameID, bool addIfMissing = false);
 
  private:
   std::vector<NameRecord> names;
   std::vector<std::string> lang_tags;
-  std::unordered_set<uint16_t> name_ids;
 };
 
 }  // namespace ots
