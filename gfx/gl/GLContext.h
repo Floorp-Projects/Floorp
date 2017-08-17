431ac276e109f34c72f98b2a2df38bd6f578049f
Jeff Gilbert <jgilbert@mozilla.com>
2017-08-17 15:01:27 -0700

    Bug 1390386 - fGetError should use BEFORE_GL_CALL. - r=jrmuizel
    MozReview-Commit-ID: B8sR38Bzjb5

--- gfx/gl/GLContext.h
+++ gfx/gl/GLContext.h
@@ -568,7 +568,6 @@ private:
         return err;
     }
 
-public:
     GLenum FlushErrors() const {
         GLenum err = RawGetErrorAndClear();
         if (!mTopError)
@@ -576,18 +575,10 @@ public:
         return err;
     }
 
-    // We smash all errors together, so you never have to loop on this. We
-    // guarantee that immediately after this call, there are no errors left.
-    GLenum fGetError() {
-        FlushErrors();
-
-        GLenum err = mTopError;
-        mTopError = LOCAL_GL_NO_ERROR;
-        return err;
-    }
-
     ////////////////////////////////////
     // Use this safer option.
+
+public:
     class LocalErrorScope;
 
 private:
@@ -784,6 +775,19 @@ public:
 // -----------------------------------------------------------------------------
 // GL official entry points
 public:
+    // We smash all errors together, so you never have to loop on this. We
+    // guarantee that immediately after this call, there are no errors left.
+    GLenum fGetError() {
+        GLenum err = LOCAL_GL_CONTEXT_LOST;
+        BEFORE_GL_CALL;
+
+        FlushErrors();
+        err = mTopError;
+        mTopError = LOCAL_GL_NO_ERROR;
+
+        AFTER_GL_CALL;
+        return err;
+    }
 
     void fActiveTexture(GLenum texture) {
         BEFORE_GL_CALL;
