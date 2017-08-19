454794c9ebbbe13c943a0cd0d83b0626315fa526
Honza Bambas <honzab.moz@firemni.cz>
2017-08-19 05:35:00 -0400

    Bug 1053321 - Correctly pass info about 'defer' and 'async' attributes to HTML5 parser invoked script preload. r=bkelly

--- parser/html/nsHtml5SpeculativeLoad.h
+++ parser/html/nsHtml5SpeculativeLoad.h
@@ -128,7 +128,9 @@ class nsHtml5SpeculativeLoad {
                            nsHtml5String aType,
                            nsHtml5String aCrossOrigin,
                            nsHtml5String aIntegrity,
-                           bool aParserInHead)
+                           bool aParserInHead,
+                           bool aAsync,
+                           bool aDefer)
     {
       NS_PRECONDITION(mOpCode == eSpeculativeLoadUninitialized,
                       "Trying to reinitialize a speculative load!");
@@ -139,6 +141,8 @@ class nsHtml5SpeculativeLoad {
       aType.ToString(mTypeOrCharsetSourceOrDocumentModeOrMetaCSPOrSizesOrIntegrity);
       aCrossOrigin.ToString(mCrossOriginOrMedia);
       aIntegrity.ToString(mReferrerPolicyOrIntegrity);
+      mIsAsync = aAsync;
+      mIsDefer = aDefer;
     }
 
     inline void InitStyle(nsHtml5String aUrl,
@@ -223,6 +227,13 @@ class nsHtml5SpeculativeLoad {
 
   private:
     eHtml5SpeculativeLoad mOpCode;
+
+
+    /**
+     * Whether the refering element has async and/or defer attributes.
+     */
+    bool mIsAsync;
+    bool mIsDefer;
 
     /* If mOpCode is eSpeculativeLoadPictureSource, this is the value of the
      * "sizes" attribute. If the attribute is not set, this will be a void
