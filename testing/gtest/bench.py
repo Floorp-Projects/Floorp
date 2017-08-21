157c942bc4d2dd77ffc772d4b55220bab4530622
Simon Sapin <simon.sapin@exyr.org>
2017-08-21 18:34:54 +0200

    Bug 1392329 - Add a command-line front-end script for gtest microbenchmarks. r=froydnj
    Example usage:
    
    $ ./testing/gtest/bench.py Stylo*
    
      29.779 ±  0.254 ms    Stylo.Servo_StyleSheet_FromUTF8Bytes_Bench
      28.841 ±  0.031 ms    Stylo.Gecko_nsCSSParser_ParseSheet_Bench
     296.240 ±  4.744 ms    Stylo.Servo_DeclarationBlock_SetPropertyById_Bench
     293.855 ±  4.304 ms    Stylo.Servo_DeclarationBlock_SetPropertyById_WithInitialSpace_Bench
    
    MozReview-Commit-ID: L6vx8A8Iz9q
    
    --HG--
    extra : rebase_source : 6fab50ba3b34f84e4e07315581034a862ed00a3b

---
+++ testing/gtest/bench.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys, subprocess, json, statistics
+
+proc = subprocess.Popen(["./mach", "gtest", sys.argv[1]], stdout=subprocess.PIPE)
+for line in proc.stdout:
+    if line.startswith(b"PERFHERDER_DATA:"):
+        data = json.loads(line[len("PERFHERDER_DATA:"):].decode("utf8"))
+        for suite in data["suites"]:
+            for subtest in suite["subtests"]:
+                print("%4d.%03d ± %6s ms    %s.%s" % (
+                    subtest["value"] / 1000.,
+                    subtest["value"] % 1000,
+                    "%.3f" % (statistics.stdev(subtest["replicates"]) / 1000),
+                    suite["name"],
+                    subtest["name"],
+                ))
