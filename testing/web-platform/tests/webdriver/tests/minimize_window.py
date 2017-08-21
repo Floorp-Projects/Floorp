14df70e770075eeba53a0a0d60811a676fa6e5d8
Wes Kocher <wkocher@mozilla.com>
2017-08-21 09:19:10 -0700

    Backed out 10 changesets (bug 1391691) for wpt failures a=backout
    Backed out changeset 666ac679317e (bug 1391691)
    Backed out changeset 46f82e1e2cde (bug 1391691)
    Backed out changeset 1fd98ace1473 (bug 1391691)
    Backed out changeset 5bddbd90ec7c (bug 1391691)
    Backed out changeset 4653134d01ef (bug 1391691)
    Backed out changeset fd88b612ac2e (bug 1391691)
    Backed out changeset 6306abc0b5e9 (bug 1391691)
    Backed out changeset 649b0e761c87 (bug 1391691)
    Backed out changeset 6c48daaad075 (bug 1391691)
    Backed out changeset 17aeed1f6454 (bug 1391691)
    
    MozReview-Commit-ID: 6Cw1QibNQKM

--- testing/web-platform/tests/webdriver/tests/minimize_window.py
+++ testing/web-platform/tests/webdriver/tests/minimize_window.py
@@ -20,6 +20,7 @@ def test_handle_user_prompt(session):
 
 
 def test_minimize(session):
+    before_size = session.window.size
     assert session.window.state == "normal"
 
     # step 4
@@ -30,6 +31,7 @@ def test_minimize(session):
 
 
 def test_payload(session):
+    before_size = session.window.size
     assert session.window.state == "normal"
 
     result = session.transport.send("POST", "session/%s/window/minimize" % session.session_id)
