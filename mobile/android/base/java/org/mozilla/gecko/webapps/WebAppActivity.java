d6bb1b8736fa9238f6ed6849c41f2624d9df2020
Eugen Sawin <esawin@mozilla.com>
2017-08-21 16:31:14 +0200

    Bug 1389236 - [5.0] Fix rebase. r=me

--- mobile/android/base/java/org/mozilla/gecko/webapps/WebAppActivity.java
+++ mobile/android/base/java/org/mozilla/gecko/webapps/WebAppActivity.java
@@ -31,11 +31,8 @@ import org.json.JSONException;
 
 import org.mozilla.gecko.ActivityHandlerHelper;
 import org.mozilla.gecko.AppConstants;
-<<<<<<< 99693e4feced612bcc473dd332e2dc15928b71df
-import org.mozilla.gecko.DoorHangerPopup;
-=======
 import org.mozilla.gecko.customtabs.CustomTabsActivity;
->>>>>>> Bug 1389236 - [3.1] Launch a custom tabs intent for out-of-scope URIs in the PWA's onLoadUri handler. r=snorp
+import org.mozilla.gecko.DoorHangerPopup;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.GeckoView;
 import org.mozilla.gecko.GeckoViewSettings;
