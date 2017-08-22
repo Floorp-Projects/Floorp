a59ffe698fd4480a0564122a1a70cf6e69f032d2
Chenxia Liu <liuche@mozilla.com>
2017-08-22 14:43:08 -0700

    Bug 1380808 - TopPanel -> TopPanelRow. r=mcomella
    MozReview-Commit-ID: 69p6FfdJh4P

--- mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/StreamItemAnimator.java
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/StreamItemAnimator.java
@@ -8,8 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 
-import org.mozilla.gecko.activitystream.homepanel.stream.TopPanel;
-
 /**
  * We need our own item animator override to avoid default RV-style animations for certain panels.
  */
@@ -21,7 +19,7 @@ public class StreamItemAnimator extends DefaultItemAnimator {
             // The top panel doesn't ever change in size. We really don't want to reload it
             // because it has its own state (i.e. the ViewPager state gets lost, and we
             // also flicker when creating a new ViewHolder). Therefore we should try to
-            // keep the existing TopPanel whenever possible.
+            // keep the existing TopPanelRow whenever possible.
             return true;
         }
 
