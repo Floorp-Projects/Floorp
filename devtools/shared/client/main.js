77e8ca2d953acdcb715407524a033bd4bc82bbcc
Nicolas Chevobbe <nchevobbe@mozilla.com>
2017-08-22 12:07:48 +0200

    Bug 1392614 - Add an onEnumSymbols function on the object actor. r=ochameau
    This will enable lazy loading symbols for a grip if needed.
    This patch introduce a new SymbolIteratorActor, which is similar
    to PropertyIteratorActor but with a data structure that better
    fits symbols, and how we already handle them, i.e. an `ownSymbols`
    array property (and not an `ownProperties` object like PropertyIteratorActor).
    
    We take this as an opportunity to add a test for enumSymbols, but also
    for enumProperties that did not have server unit tests (it is only tested
    in the frontend with the variable view, which might be deprecated at some
    point).
    
    MozReview-Commit-ID: IEIKA8zwH90

--- devtools/shared/client/main.js
+++ devtools/shared/client/main.js
@@ -2669,6 +2669,30 @@ ObjectClient.prototype = {
   }),
 
   /**
+   * Request a SymbolIteratorClient instance to enumerate symbols in an object.
+   *
+   * @param onResponse function Called with the request's response.
+   */
+  enumSymbols: DebuggerClient.requester({
+    type: "enumSymbols"
+  }, {
+    before: function (packet) {
+      if (this._grip.type !== "object") {
+        throw new Error("enumSymbols is only valid for objects grips.");
+      }
+      return packet;
+    },
+    after: function (response) {
+      if (response.iterator) {
+        return {
+          iterator: new SymbolIteratorClient(this._client, response.iterator)
+        };
+      }
+      return response;
+    }
+  }),
+
+  /**
    * Request the property descriptor of the object's specified property.
    *
    * @param name string The name of the requested property.
@@ -2839,6 +2863,61 @@ PropertyIteratorClient.prototype = {
    *
    * @param callback Function
    *        The function called when we receive the property values.
+   */
+  all: DebuggerClient.requester({
+    type: "all"
+  }, {}),
+};
+
+/**
+ * A SymbolIteratorClient provides a way to access to symbols
+ * of an object efficiently, slice by slice.
+ *
+ * @param client DebuggerClient
+ *        The debugger client parent.
+ * @param grip Object
+ *        A SymbolIteratorActor grip returned by the protocol via
+ *        TabActor.enumSymbols request.
+ */
+function SymbolIteratorClient(client, grip) {
+  this._grip = grip;
+  this._client = client;
+  this.request = this._client.request;
+}
+
+SymbolIteratorClient.prototype = {
+  get actor() {
+    return this._grip.actor;
+  },
+
+  /**
+   * Get the total number of symbols available in the iterator.
+   */
+  get count() {
+    return this._grip.count;
+  },
+
+  /**
+   * Get a set of following symbols.
+   *
+   * @param start Number
+   *        The index of the first symbol to fetch.
+   * @param count Number
+   *        The number of symbols to fetch.
+   * @param callback Function
+   *        The function called when we receive the symbols.
+   */
+  slice: DebuggerClient.requester({
+    type: "slice",
+    start: arg(0),
+    count: arg(1)
+  }, {}),
+
+  /**
+   * Get all the symbols.
+   *
+   * @param callback Function
+   *        The function called when we receive the symbols.
    */
   all: DebuggerClient.requester({
     type: "all"
