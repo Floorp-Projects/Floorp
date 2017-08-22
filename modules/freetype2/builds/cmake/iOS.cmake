bf22003a9a9fc7c9ba55ea37adac63d01a9eace1
Jonathan Kew <jkew@mozilla.com>
2017-08-22 09:20:05 +0100

    Bug 1364714 - (re-landing) Update Freetype to version 2.8, and fix/annotate tests affected by changes to font metrics & rasterization. r=dholbert,jwatt,ryanvm,jfkthame

--- modules/freetype2/builds/cmake/iOS.cmake
+++ modules/freetype2/builds/cmake/iOS.cmake
@@ -1,6 +1,6 @@
 # iOS.cmake
 #
-# Copyright 2014-2016 by
+# Copyright 2014-2017 by
 # David Turner, Robert Wilhelm, and Werner Lemberg.
 #
 # Written by David Wimsey <david@wimsey.us>
--- modules/freetype2/src/gzip/rules.mk
+++ modules/freetype2/src/gzip/rules.mk
@@ -3,7 +3,7 @@
 #
 
 
-# Copyright 2002-2016 by
+# Copyright 2002-2017 by
 # David Turner, Robert Wilhelm, and Werner Lemberg.
 #
 # This file is part of the FreeType project, and may only be used, modified,
@@ -34,8 +34,8 @@ endif
 
 # gzip support sources
 #
-# All source and header files get loaded by `ftgzip.c' only if SYTEM_ZLIB is
-# not defined (regardless whether we have a `single' or a `multi' build).
+# All source and header files get loaded by `ftgzip.c' only if SYSTEM_ZLIB
+# is not defined (regardless whether we have a `single' or a `multi' build).
 # However, it doesn't harm if we add everything as a dependency
 # unconditionally.
 #
