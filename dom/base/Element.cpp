c4f39df3cf3508ef13b0f94f0c0b05f3a24676a6
Emilio Cobos Álvarez <emilio@crisal.io>
2017-08-23 09:58:57 +0200

    Bug 1389743: Only reconstruct frames synchronously from ContentRemoved when called from frame construction. r=mats
    There's only one case of sync frame construction from ContentRemoved now, and
    it's not on the element being removed, but on the whitespace siblings if needed,
    and _only_ when they don't support lazy frame construction.
    
    Basically, this switches all the RecreateFramesForContent calls to use
    `aAsyncInsert` (which I changed to an enum class for readability), except when
    we're already reframing.
    
    Also, it switches ReframeTextIfNeeded to opt-in into lazy frame construction,
    since it's used only when aFlags == CONTENT_REMOVED.
    
    This allows to simplify the DestroyFramesFor API (which I'm happy to rename to
    something more meaningful, since now it's something like
    DestroyFramesForAndRecreateThemAsync), and do some other consistency cleanups.
    
    A bunch of the ContentRemoved callsites were pretty random at passing
    aAsyncInsert, and that was some kind of a mess. This patch ensures consistency,
    and makes it impossible to do O(n^2) work when removing DOM nodes, which is
    nice.
    
    The underlying reason for this is explained in the description of bug 1377848,
    and basically allows us to remove a bunch of Servo hacks on the longer term (a
    few of them are going away already, yay!).
    
    MozReview-Commit-ID: 2DrUTxGV8RX

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -1123,12 +1123,9 @@ Element::CreateShadowRoot(ErrorResult& aError)
     return nullptr;
   }
 
-  nsIDocument* doc = GetComposedDoc();
-  nsIContent* destroyedFramesFor = nullptr;
-  if (doc) {
-    nsIPresShell* shell = doc->GetShell();
-    if (shell) {
-      shell->DestroyFramesFor(this, &destroyedFramesFor);
+  if (nsIDocument* doc = GetComposedDoc()) {
+    if (nsIPresShell* shell = doc->GetShell()) {
+      shell->DestroyFramesFor(this);
       MOZ_ASSERT(!shell->FrameManager()->GetDisplayContentsStyleFor(this));
     }
   }
@@ -1170,17 +1167,6 @@ Element::CreateShadowRoot(ErrorResult& aError)
   xblBinding->SetBoundElement(this);
 
   SetXBLBinding(xblBinding);
-
-  // Recreate the frame for the bound content because binding a ShadowRoot
-  // changes how things are rendered.
-  if (doc) {
-    MOZ_ASSERT(doc == GetComposedDoc());
-    nsIPresShell* shell = doc->GetShell();
-    if (shell) {
-      shell->CreateFramesFor(destroyedFramesFor);
-    }
-  }
-
   return shadowRoot.forget();
 }
 
