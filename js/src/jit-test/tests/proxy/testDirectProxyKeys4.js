e3c83c72110bdcd5f0f836617c6f667f113764f8
André Bargull <andre.bargull@gmail.com>
2017-08-23 23:56:40 +0200

    Bug 1389752 - Throw TypeError if [[OwnPropertyKeys]] of scripted proxies contains duplicates. r=till, r=qdot

--- js/src/jit-test/tests/proxy/testDirectProxyKeys4.js
+++ js/src/jit-test/tests/proxy/testDirectProxyKeys4.js
@@ -1,5 +1,7 @@
 load(libdir + "asserts.js");
 
+// Throw TypeError if trap returns a property key multiple times.
+
 var handler = { ownKeys: () => [ 'foo', 'foo' ] };
 for (let p of [new Proxy({}, handler), Proxy.revocable({}, handler).proxy])
-    assertDeepEq(Object.keys(p), []); // Properties are not enumerable.
+    assertThrowsInstanceOf(() => Object.keys(p), TypeError);
