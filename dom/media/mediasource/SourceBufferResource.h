3b2cfc6314df8ebce9d03cbbcc0e1a0a28c8b321
JW Wang <jwwang@mozilla.com>
2017-08-23 15:52:33 +0800

    Bug 1393345 - remove MediaResource::IsSuspended() which has no callers. r=gerald
    MozReview-Commit-ID: Jn6TIXpFflR

--- dom/media/mediasource/SourceBufferResource.h
+++ dom/media/mediasource/SourceBufferResource.h
@@ -69,11 +69,6 @@ public:
     return GetLength();
   }
   bool IsDataCachedToEndOfResource(int64_t aOffset) override { return false; }
-  bool IsSuspended() override
-  {
-    UNIMPLEMENTED();
-    return false;
-  }
   nsresult ReadFromCache(char* aBuffer,
                          int64_t aOffset,
                          uint32_t aCount) override;
