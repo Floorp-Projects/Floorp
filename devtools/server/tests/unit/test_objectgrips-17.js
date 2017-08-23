35554dd61cfa91a8159fafd6111ec466a403e739
Oriol Brufau <oriol-bugzilla@hotmail.com>
2017-08-23 15:50:00 -0400

    Bug 1389787 - Let the console unwrap proxy objects to avoid running traps. r=jimb

--- devtools/server/tests/unit/test_objectgrips-17.js
+++ devtools/server/tests/unit/test_objectgrips-17.js
@@ -4,10 +4,12 @@
 
 "use strict";
 
+var gServer;
 var gDebuggee;
 var gClient;
 var gThreadClient;
-var gCallback;
+var gGlobal;
+var gHasXrays;
 
 function run_test() {
   run_test_with_server(DebuggerServer, function () {
@@ -16,80 +18,178 @@ function run_test() {
   do_test_pending();
 }
 
-function addTestNullPrincipalGlobal(name, server = DebuggerServer) {
-  // System principal objects are considered safe even when not wrapped in Xray,
-  // and therefore proxy traps may run. So the test needs to use a null principal.
-  let global = Cu.Sandbox(null);
-  global.__name = name;
-  server.addTestGlobal(global);
-  return global;
+async function run_test_with_server(server, callback) {
+  gServer = server;
+  initTestDebuggerServer(server);
+
+  // Run tests with a system principal debuggee.
+  await run_tests_in_principal(systemPrincipal, "test-grips-system-principal");
+
+  // Run tests with a null principal debuggee.
+  await run_tests_in_principal(null, "test-grips-null-principal");
+
+  callback();
 }
 
-async function run_test_with_server(server, callback) {
-  gCallback = callback;
-  initTestDebuggerServer(server);
-  gDebuggee = addTestNullPrincipalGlobal("test-grips", server);
+async function run_tests_in_principal(principal, title) {
+  // Prepare the debuggee.
+  gDebuggee = Cu.Sandbox(principal);
+  gDebuggee.__name = title;
+  gServer.addTestGlobal(gDebuggee);
   gDebuggee.eval(function stopMe(arg1, arg2) {
     debugger;
   }.toString());
-
-  gClient = new DebuggerClient(server.connectPipe());
+  gClient = new DebuggerClient(gServer.connectPipe());
   await gClient.connect();
-  const [,, threadClient] = await attachTestTabAndResume(gClient, "test-grips");
+  const [,, threadClient] = await attachTestTabAndResume(gClient, title);
   gThreadClient = threadClient;
-  test_proxy_grip();
+
+  // Bug 1390701 should remove the following condition.
+  if (principal === null) {
+    // Test objects created in the debuggee.
+    await testPrincipal(undefined, false);
+  }
+
+  // Bug 1390701 should remove the following condition.
+  if (false) {
+    // Test objects created in a new system principal with Xrays.
+    await testPrincipal(systemPrincipal, true);
+
+    // Test objects created in a new system principal without Xrays.
+    await testPrincipal(systemPrincipal, false);
+  }
+
+  // Test objects created in a new null principal with Xrays.
+  await testPrincipal(null, true);
+
+  // Test objects created in a new null principal without Xrays.
+  await testPrincipal(null, false);
+
+  // Finish.
+  await gClient.close();
 }
 
-async function test_proxy_grip() {
-  gThreadClient.addOneTimeListener("paused", async function (event, packet) {
-    let [proxyGrip, inheritsProxyGrip] = packet.frame.arguments;
+function testPrincipal(principal, wantXrays = true) {
+  // Create a global object with the specified security principal.
+  // If none is specified, use the debuggee.
+  if (principal !== undefined) {
+    gGlobal = Cu.Sandbox(principal, {wantXrays});
+    gHasXrays = wantXrays;
+  } else {
+    gGlobal = gDebuggee;
+    gHasXrays = false;
+  }
 
-    // Check the grip of the proxy object.
-    check_proxy_grip(proxyGrip);
+  return new Promise(function (resolve) {
+    gThreadClient.addOneTimeListener("paused", async function (event, packet) {
+      // Get the grips.
+      let [proxyGrip, inheritsProxyGrip] = packet.frame.arguments;
 
-    // Retrieve the properties of the object which inherits from a proxy,
-    // and check the grip of its prototype.
-    let objClient = gThreadClient.pauseGrip(inheritsProxyGrip);
-    let response = await objClient.getPrototypeAndProperties();
-    check_prototype_and_properties(response);
+      // Check the grip of the proxy object.
+      check_proxy_grip(proxyGrip);
 
-    // Check that none of the above ran proxy traps.
-    let trapDidRun = gDebuggee.eval("trapDidRun");
-    strictEqual(trapDidRun, false, "No proxy trap did run.");
+      // Retrieve the properties of the object which inherits from a proxy,
+      // and check the grip of its prototype.
+      let objClient = gThreadClient.pauseGrip(inheritsProxyGrip);
+      let response = await objClient.getPrototypeAndProperties();
+      check_properties(response.ownProperties);
+      check_prototype(response.prototype);
 
-    await gThreadClient.resume();
-    await gClient.close();
-    gCallback();
+      // Check that none of the above ran proxy traps.
+      strictEqual(gGlobal.trapDidRun, false, "No proxy trap did run.");
+
+      // Resume the debugger and finish the current test.
+      await gThreadClient.resume();
+      resolve();
+    });
+
+    // Create objects in `gGlobal`, and debug them in `gDebuggee`. They may get various
+    // kinds of security wrappers, or no wrapper at all.
+    // To detect that no proxy trap runs, the proxy handler should define all possible
+    // traps, but the list is long and may change. Therefore a second proxy is used as
+    // the handler, so that a single `get` trap suffices.
+    gGlobal.eval(`
+      var trapDidRun = false;
+      var proxy = new Proxy({}, new Proxy({}, {get: (_, trap) => {
+        return function(_, arg) {
+          if (trap === "has" && arg === "__exposedProps__") {
+            // Tolerate this case until bug 1392026 is fixed.
+            return false;
+          }
+          trapDidRun = true;
+          throw new Error("proxy trap '" + trap + "' was called.");
+        }
+      }}));
+      var inheritsProxy = Object.create(proxy, {x:{value:1}});
+    `);
+    let data = Cu.createObjectIn(gDebuggee, {defineAs: "data"});
+    data.proxy = gGlobal.proxy;
+    data.inheritsProxy = gGlobal.inheritsProxy;
+    gDebuggee.eval("stopMe(data.proxy, data.inheritsProxy);");
   });
+}
 
-  gDebuggee.eval(`{
-    var trapDidRun = false;
-    var proxy = new Proxy({}, new Proxy({}, {get: (_, trap) => {
-      trapDidRun = true;
-      throw new Error("proxy " + trap + " trap was called.");
-    }}));
-    var inheritsProxy = Object.create(proxy, {x:{value:1}});
-    stopMe(proxy, inheritsProxy);
-  }`);
+function isSystemPrincipal(obj) {
+  return Cu.getObjectPrincipal(obj) === systemPrincipal;
 }
 
 function check_proxy_grip(grip) {
-  strictEqual(grip.class, "Proxy", "The grip has a Proxy class.");
-  ok(grip.proxyTarget, "There is a [[ProxyTarget]] grip.");
-  ok(grip.proxyHandler, "There is a [[ProxyHandler]] grip.");
-
   const {preview} = grip;
-  strictEqual(preview.ownPropertiesLength, 2, "The preview has 2 properties.");
-  let target = preview.ownProperties["<target>"].value;
-  strictEqual(target, grip.proxyTarget, "<target> contains the [[ProxyTarget]].");
-  let handler = preview.ownProperties["<handler>"].value;
-  strictEqual(handler, grip.proxyHandler, "<handler> contains the [[ProxyHandler]].");
+
+  if (gGlobal === gDebuggee) {
+    // The proxy has no security wrappers.
+    strictEqual(grip.class, "Proxy", "The grip has a Proxy class.");
+    ok(grip.proxyTarget, "There is a [[ProxyTarget]] grip.");
+    ok(grip.proxyHandler, "There is a [[ProxyHandler]] grip.");
+    strictEqual(preview.ownPropertiesLength, 2, "The preview has 2 properties.");
+    let target = preview.ownProperties["<target>"].value;
+    strictEqual(target, grip.proxyTarget, "<target> contains the [[ProxyTarget]].");
+    let handler = preview.ownProperties["<handler>"].value;
+    strictEqual(handler, grip.proxyHandler, "<handler> contains the [[ProxyHandler]].");
+  } else if (!isSystemPrincipal(gDebuggee)) {
+    // The debuggee is not allowed to remove the security wrappers.
+    strictEqual(grip.class, "Object", "The grip has an Object class.");
+    ok(!("ownPropertyLength" in grip), "The grip doesn't know the number of properties.");
+  } else if (!gHasXrays || isSystemPrincipal(gGlobal)) {
+    // The proxy has non-opaque security wrappers.
+    strictEqual(grip.class, "Proxy", "The grip has a Proxy class.");
+    ok(!("proxyTarget" in grip), "There is no [[ProxyTarget]] grip.");
+    ok(!("proxyHandler" in grip), "There is no [[ProxyHandler]] grip.");
+    strictEqual(preview.ownPropertiesLength, 0, "The preview has no properties.");
+    ok(!("<target>" in preview), "The preview has no <target> property.");
+    ok(!("<handler>" in preview), "The preview has no <handler> property.");
+  } else {
+    // The proxy has opaque security wrappers.
+    strictEqual(grip.class, "Opaque", "The grip has an Opaque class.");
+    strictEqual(grip.ownPropertyLength, 0, "The grip has no properties.");
+  }
 }
 
-function check_prototype_and_properties(response) {
-  let ownPropertiesLength = Reflect.ownKeys(response.ownProperties).length;
-  strictEqual(ownPropertiesLength, 1, "1 own property was retrieved.");
-  strictEqual(response.ownProperties.x.value, 1, "The property has the right value.");
-  check_proxy_grip(response.prototype);
+function check_properties(props) {
+  let ownPropertiesLength = Reflect.ownKeys(props).length;
+
+  if (!isSystemPrincipal(gDebuggee) && gDebuggee !== gGlobal) {
+    // The debuggee is not allowed to access the object.
+    strictEqual(ownPropertiesLength, 0, "No own property could be retrieved.");
+  } else {
+    // The debuggee can access the properties.
+    strictEqual(ownPropertiesLength, 1, "1 own property was retrieved.");
+    strictEqual(props.x.value, 1, "The property has the right value.");
+  }
+}
+
+function check_prototype(proto) {
+  if (!isSystemPrincipal(gDebuggee) && gDebuggee !== gGlobal) {
+    // The debuggee is not allowed to access the object. It sees a null prototype.
+    strictEqual(proto.type, "null", "The prototype is null.");
+  } else if (!gHasXrays || isSystemPrincipal(gGlobal)) {
+    // The object has no security wrappers or non-opaque ones.
+    // The debuggee sees the proxy when retrieving the prototype.
+    check_proxy_grip(proto);
+  } else {
+    // The object has opaque security wrappers.
+    // The debuggee sees `Object.prototype` when retrieving the prototype.
+    strictEqual(proto.class, "Object", "The prototype has a Object class.");
+  }
 }
 
