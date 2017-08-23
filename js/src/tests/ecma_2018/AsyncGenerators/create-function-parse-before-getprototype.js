48ac36b18704a6c62cc4d6a70122b039fce3fca0
André Bargull <andre.bargull@gmail.com>
2017-08-23 15:17:57 +0200

    Bug 1391780 - Call GetPrototypeFromConstructor after parsing in CreateDynamicFunction. r=till

---
+++ js/src/tests/ecma_2018/AsyncGenerators/create-function-parse-before-getprototype.js
@@ -0,0 +1,19 @@
+var getProtoCalled = false;
+
+var newTarget = Object.defineProperty(function(){}.bind(), "prototype", {
+    get() {
+        getProtoCalled = true;
+        return null;
+    }
+});
+
+var AsyncGenerator = async function*(){}.constructor;
+
+assertThrowsInstanceOf(() => {
+    Reflect.construct(AsyncGenerator, ["#error"], newTarget);
+}, SyntaxError);
+
+assertEq(getProtoCalled, false);
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
