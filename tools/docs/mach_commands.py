aa4cabec87dfa57f675cc125d4ebe06c73b2a830
Gregory Szorc <gps@mozilla.com>
2017-08-24 10:49:28 -0700

    Bug 1390693 - Move S3 upload logic to standalone module; r=dustin
    We try to keep mach_commands.py files as minimal as possible.
    
    MozReview-Commit-ID: I4kvZtDjqGd

--- tools/docs/mach_commands.py
+++ tools/docs/mach_commands.py
@@ -135,63 +135,9 @@ class Documentation(MachCommandBase):
         outdir = os.path.join(self.topobjdir, 'docs')
         self.build_docs(what=what, outdir=outdir, format='html')
 
-        self.s3_upload(os.path.join(outdir, 'html', 'Mozilla_Source_Tree_Docs'))
+        from moztreedocs.upload import s3_upload
+        s3_upload(os.path.join(outdir, 'html', 'Mozilla_Source_Tree_Docs'))
 
-    def s3_upload(self, root):
-        """Upload the contents of outdir recursively to S3"""
-        import boto3
-        import mimetypes
-        import requests
-
-        region = 'us-west-2'
-        level = os.environ.get('MOZ_SCM_LEVEL', '1')
-        bucket = {
-            '1': 'gecko-docs.mozilla.org-l1',
-            '2': 'gecko-docs.mozilla.org-l2',
-            '3': 'gecko-docs.mozilla.org',
-        }[level]
-        secrets_url = 'http://taskcluster/secrets/v1/secret/'
-        secrets_url += 'project/releng/gecko/build/level-{}/gecko-docs-upload'.format(level)
-
-        # Get the credentials from the TC secrets service.  Note that these
-        # differ per SCM level
-        if 'TASK_ID' in os.environ:
-            print("Using AWS credentials from the secrets service")
-            session = requests.Session()
-            res = session.get(secrets_url)
-            res.raise_for_status()
-            secret = res.json()['secret']
-            session = boto3.session.Session(
-                aws_access_key_id=secret['AWS_ACCESS_KEY_ID'],
-                aws_secret_access_key=secret['AWS_SECRET_ACCESS_KEY'],
-                region_name=region)
-        else:
-            print("Trying to use your AWS credentials..")
-            session = boto3.session.Session(region_name=region)
-        s3 = session.client('s3')
-
-        try:
-            old_cwd = os.getcwd()
-            os.chdir(root)
-
-            for dir, dirs, filenames in os.walk('.'):
-                if dir == '.':
-                    # ignore a few things things in the root directory
-                    bad = [d for d in dirs if d.startswith('.') or d in ('_venv', '_staging')]
-                    for b in bad:
-                        dirs.remove(b)
-                for filename in filenames:
-                    pathname = os.path.join(dir, filename)[2:]  # strip '.''
-                    content_type, content_encoding = mimetypes.guess_type(pathname)
-                    extra_args = {}
-                    if content_type:
-                        extra_args['ContentType'] = content_type
-                    if content_encoding:
-                        extra_args['ContentEncoding'] = content_encoding
-                    print('uploading', pathname)
-                    s3.upload_file(pathname, bucket, pathname, ExtraArgs=extra_args)
-        finally:
-            os.chdir(old_cwd)
 
 def die(msg, exit_code=1):
     msg = '%s: %s' % (sys.argv[0], msg)
