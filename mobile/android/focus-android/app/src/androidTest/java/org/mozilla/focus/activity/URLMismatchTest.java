fb7c863420ffb96aca0a3b0c643766753c54b8e1
Sebastian Kaspari <s.kaspari@gmail.com>
2017-08-24 17:02:16 +0200

    [focus] URLMisMatchTest: Click into text field before typing and use replaceText(). (https://github.com/mozilla-mobile/focus-android/issues/1200)

--- mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/URLMismatchTest.java
+++ mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/URLMismatchTest.java
@@ -20,10 +20,9 @@ import org.junit.runner.RunWith;
 import org.mozilla.focus.R;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
@@ -71,7 +70,7 @@ public class URLMismatchTest {
         onView(withId(R.id.url_edit))
                 .check(matches(isDisplayed()))
                 .check(matches(hasFocus()))
-                .perform(typeText("mozilla"));
+                .perform(click(), replaceText("mozilla"));
 
         // Verify that the search hint is displayed and click it.
         onView(withId(R.id.search_hint))
@@ -92,7 +91,7 @@ public class URLMismatchTest {
 
         // Type "moz" - Verify that it auto-completes to "mozilla.org" and then load the website
         onView(withId(R.id.url_edit))
-                .perform(clearText(), typeText("moz"))
+                .perform(click(), replaceText("mozilla"))
                 .check(matches(withText("mozilla.org")))
                 .perform(pressImeActionButton());
 
