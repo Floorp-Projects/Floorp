7819a7a50c22410d7812fd53ac69df58b378f726
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-08-24 16:17:40 -0400

    Bug 1393590 - [mach] Use description field for settings instead of gettext locales, r=gps
    This preserves ./mach settings' --list option. If --list is passed in, we call splitlines()
    on the description and print only the first line. The full multi-line description will be
    printed otherwise.
    
    This also displays the type and/or choices of the option as appropriate.
    
    MozReview-Commit-ID: 7UMsN9qslWt

--- python/mach/mach/commands/settings.py
+++ python/mach/mach/commands/settings.py
@@ -4,22 +4,14 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-import os
 from textwrap import TextWrapper
 
+from mach.config import TYPE_CLASSES
 from mach.decorators import (
     CommandArgument,
     CommandProvider,
     Command,
-    SubCommand,
 )
-
-POLIB_NOT_FOUND = """
-Could not detect the 'polib' package on the local system.
-Please run:
-
-    pip install polib
-""".lstrip()
 
 
 @CommandProvider
@@ -39,94 +31,27 @@ class Settings(object):
                      help='Show settings in a concise list')
     def settings(self, short=None):
         """List available settings."""
-        if short:
-            for section in sorted(self._settings):
-                for option in sorted(self._settings[section]._settings):
-                    short, full = self._settings.option_help(section, option)
-                    print('%s.%s -- %s' % (section, option, short))
-            return
-
+        types = {v: k for k, v in TYPE_CLASSES.items()}
         wrapper = TextWrapper(initial_indent='# ', subsequent_indent='# ')
-        for section in sorted(self._settings):
-            print('[%s]' % section)
+        for i, section in enumerate(sorted(self._settings)):
+            if not short:
+                print('%s[%s]' % ('' if i == 0 else '\n', section))
 
             for option in sorted(self._settings[section]._settings):
-                short, full = self._settings.option_help(section, option)
-                print(wrapper.fill(full))
+                meta = self._settings[section].get_meta(option)
+                desc = meta['description']
 
-                if option != '*':
-                    print(';%s =' % option)
-                    print('')
+                if short:
+                    print('%s.%s -- %s' % (section, option, desc.splitlines()[0]))
+                    continue
 
-    @SubCommand('settings', 'locale-gen',
-                description='Generate or update gettext .po and .mo locale files.')
-    @CommandArgument('sections', nargs='*',
-                     help='A list of strings in the form of either <section> or '
-                          '<section>.<option> to translate. By default, prompt to '
-                          'translate all applicable options.')
-    @CommandArgument('--locale', default='en_US',
-                     help='Locale to generate, defaults to en_US.')
-    @CommandArgument('--overwrite', action='store_true', default=False,
-                     help='Overwrite pre-existing entries in .po files.')
-    def locale_gen(self, sections, **kwargs):
-        try:
-            import polib
-        except ImportError:
-            print(POLIB_NOT_FOUND)
-            return 1
+                if option == '*':
+                    option = '<option>'
 
-        self.was_prompted = False
+                if 'choices' in meta:
+                    value = "{%s}" % ', '.join(meta['choices'])
+                else:
+                    value = '<%s>' % types[meta['type_cls']]
 
-        sections = sections or self._settings
-        for section in sections:
-            self._gen_section(section, **kwargs)
-
-        if not self.was_prompted:
-            print("All specified options already have an {} translation. "
-                  "To overwrite existing translations, pass --overwrite."
-                  .format(kwargs['locale']))
-
-    def _gen_section(self, section, **kwargs):
-        if '.' in section:
-            section, option = section.split('.')
-            return self._gen_option(section, option, **kwargs)
-
-        for option in sorted(self._settings[section]._settings):
-            self._gen_option(section, option, **kwargs)
-
-    def _gen_option(self, section, option, locale, overwrite):
-        import polib
-
-        meta = self._settings[section]._settings[option]
-
-        localedir = os.path.join(meta['localedir'], locale, 'LC_MESSAGES')
-        if not os.path.isdir(localedir):
-            os.makedirs(localedir)
-
-        path = os.path.join(localedir, '{}.po'.format(section))
-        if os.path.isfile(path):
-            po = polib.pofile(path)
-        else:
-            po = polib.POFile()
-
-        optionid = '{}.{}'.format(section, option)
-        for name in ('short', 'full'):
-            msgid = '{}.{}'.format(optionid, name)
-            entry = po.find(msgid)
-            if not entry:
-                entry = polib.POEntry(msgid=msgid)
-                po.append(entry)
-
-            if entry in po.translated_entries() and not overwrite:
-                continue
-
-            self.was_prompted = True
-
-            msgstr = raw_input("Translate {} to {}:\n"
-                               .format(msgid, locale))
-            entry.msgstr = msgstr
-
-        if self.was_prompted:
-            mopath = os.path.join(localedir, '{}.mo'.format(section))
-            po.save(path)
-            po.save_as_mofile(mopath)
+                print(wrapper.fill(desc))
+                print(';%s=%s' % (option, value))
