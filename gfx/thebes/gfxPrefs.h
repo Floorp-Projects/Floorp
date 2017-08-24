da586051cac6d1f7f8e97b1715dcccbd16d5cfee
Kaku Kuo <kaku@mozilla.com>
2017-08-24 19:02:06 +0800

    Bug 1393392 P1 - a preference to ignore hw-decoding blacklist in gpu process; r=gerald,mattwoodrow
    Gecko still finds out the current driver is blacklisted or not if we set "media.wmf.deblacklisting-for-telemetry-in-gpu-process" = true.
    But this is only for telemetry usage.
    
    MozReview-Commit-ID: 2Ydg527uQhe
    
    --HG--
    extra : rebase_source : d516f12674aa5532416635d6b95950786b74f6a2
    extra : intermediate-source : ed0c0f7e47aca0ab962922f5e9ac58d6486ebc87
    extra : source : b24c06b2b9854f68b60ba4a73755cf65c5266ae9

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -667,6 +667,7 @@ private:
   DECL_GFX_PREF(Once, "media.windows-media-foundation.use-sync-texture", PDMWMFUseSyncTexture, bool, true);
   DECL_GFX_PREF(Live, "media.wmf.low-latency.enabled", PDMWMFLowLatencyEnabled, bool, false);
   DECL_GFX_PREF(Live, "media.wmf.skip-blacklist", PDMWMFSkipBlacklist, bool, false);
+  DECL_GFX_PREF(Live, "media.wmf.deblacklisting-for-telemetry-in-gpu-process", PDMWMFDeblacklistingForTelemetryInGPUProcess, bool, false);
 #endif
 
   // These affect how line scrolls from wheel events will be accelerated.
