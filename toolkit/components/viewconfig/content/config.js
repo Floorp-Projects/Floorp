dcbeae3372705e96c7fb7979a1e7a45cb40b393a
Nicholas Nethercote <nnethercote@mozilla.com>
2017-08-24 11:46:02 +1000

    Bug 1392989 (part 2) - Remove unused atom-service references. r=jaws.
    Plus a couple of other unused service references nearby.

--- toolkit/components/viewconfig/content/config.js
+++ toolkit/components/viewconfig/content/config.js
@@ -11,11 +11,7 @@ const nsISupportsString = Components.interfaces.nsISupportsString;
 const nsIPrefBranch = Components.interfaces.nsIPrefBranch;
 const nsIClipboardHelper = Components.interfaces.nsIClipboardHelper;
 
-const nsSupportsString_CONTRACTID = "@mozilla.org/supports-string;1";
-const nsPrompt_CONTRACTID = "@mozilla.org/embedcomp/prompt-service;1";
-const nsPrefService_CONTRACTID = "@mozilla.org/preferences-service;1";
 const nsClipboardHelper_CONTRACTID = "@mozilla.org/widget/clipboardhelper;1";
-const nsAtomService_CONTRACTID = "@mozilla.org/atom-service;1";
 
 const gPrefBranch = Services.prefs;
 const gClipboardHelper = Components.classes[nsClipboardHelper_CONTRACTID].getService(nsIClipboardHelper);
