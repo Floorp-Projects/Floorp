bde606d75c6de870837e28c9332e9c84b54baa56
André Bargull <andre.bargull@gmail.com>
2017-08-25 12:09:13 +0200

    Bug 1315641 - Align error messages for (Typed)Array.from with standard iteration error messages. r=evilpie

--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -171,7 +171,7 @@ function GetIterator(obj, method) {
 
     // Step 5.
     if (!IsObject(iterator))
-        ThrowTypeError(JSMSG_NOT_ITERATOR, ToString(iterator));
+        ThrowTypeError(JSMSG_GET_ITER_RETURNED_PRIMITIVE);
 
     // Step 6.
     return iterator;
