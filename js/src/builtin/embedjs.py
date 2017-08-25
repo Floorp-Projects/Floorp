9e6798ac008442fdd00c6f592cff63c0d328c2d4
Mike Shal <mshal@mozilla.com>
2017-08-24 22:52:01 -0400

    Bug 1402012 - Update buildconfig.py to use PartialConfigEnvironment; r=glandium
    By using the PartialConfigEnvironment, the clients of buildconfig will
    depend on config.statusd/ files instead of config.status directly.
    Clients can access substs and defines using buildconfig.substs['FOO'] or
    buildconfig.defines['BAR'], and then collect file-level dependencies for
    make using buildconfig.get_dependencies(). All GENERATED_FILES rules
    already make use of this because file_generate.py automatically includes
    these dependencies (along with all python modules loaded).
    
    As a result of this commit, re-running configure will no longer cause
    the world to be rebuilt. Although config.status is updated, no build
    steps use config.status directly and instead depend on values in
    config.statusd/, which are written with FileAvoidWrite. Since those
    files are not official targets according to the make backend, make won't
    try to continually rebuild the backend when those files are out of date.
    And since they are FileAvoidWrite, make will only re-run dependent steps
    if the actual configure value has changed.
    
    As a result of using JSON to load data from the config.statusd
    directory, substs can be unicode (instead of a bare string type).
    generate_certdata.py converts the subst manually to a string so the
    value can be exported to the environment without issue on Windows.
    
    Additionally, patching the buildconfig.substs dict no longer works, so
    the unit-symbolstore.py test was modified to patch the underlying
    buildconfig.substs._dict instead.
    
    The other files that needed to be modified make use of all the defines
    for the preprocessor. Those that are used during 'mach build' now use
    buildconfig.defines['ALLDEFINES'], which maps to a special
    FileAvoidWrite file generated for the PartialConfigEnvironment.
    
    MozReview-Commit-ID: 2pJ4s3TVeS8
    
    --HG--
    extra : rebase_source : d6bb0208483f9f043e7be1b36907ca13243985f8

--- js/src/builtin/embedjs.py
+++ js/src/builtin/embedjs.py
@@ -132,8 +132,7 @@ def messages(jsmsg):
 
 def get_config_defines(buildconfig):
   # Collect defines equivalent to ACDEFINES and add MOZ_DEBUG_DEFINES.
-  env = {key: value for key, value in buildconfig.defines.iteritems()
-         if key not in buildconfig.non_global_defines}
+  env = buildconfig.defines['ALLDEFINES']
   for define in buildconfig.substs['MOZ_DEBUG_DEFINES']:
     env[define] = 1
   return env
