75b113a96d4730add384d90651c943997ddfe8e9
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-25 17:06:10 +0200

    Backed out changeset 57ec52606739 (bug 1389752) for failing web-platform-tests' /fetch/api/headers/headers-record.html. r=backout

--- js/src/jit-test/tests/proxy/testDirectProxyGetOwnPropertyNames4.js
+++ js/src/jit-test/tests/proxy/testDirectProxyGetOwnPropertyNames4.js
@@ -1,7 +1,5 @@
 load(libdir + "asserts.js");
 
-// Throw TypeError if trap returns a property key multiple times.
-
 var handler = { ownKeys : () => [ 'foo', 'foo' ] };
 for (let p of [new Proxy({}, handler), Proxy.revocable({}, handler).proxy])
-    assertThrowsInstanceOf(() => Object.getOwnPropertyNames(p), TypeError);
+    assertDeepEq(Object.getOwnPropertyNames(p), ['foo', 'foo']);
