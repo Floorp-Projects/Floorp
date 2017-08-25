9a112d04e74692e801807756b3e4263ba1f39e57
cku <cku@mozilla.com>
2017-08-25 16:33:33 +0800

    Bug 1385239 - Part 1: Pass gfxContext, instead of DrawTarget, into nsFilterInstance::PaintFilteredFrame. r=mstange, a=lizzard
    This change makes the next patch free of doing Matrix to gfxMatrix conversion.
    
    MozReview-Commit-ID: 8K9RNgjZPTw
    
    --HG--
    extra : source : 45278d597320c6f107d21fcff591a5bac7c42a46

--- layout/svg/nsSVGIntegrationUtils.cpp
+++ layout/svg/nsSVGIntegrationUtils.cpp
@@ -1106,7 +1106,7 @@ nsSVGIntegrationUtils::PaintFilter(const PaintFramesParams& aParams)
 
   gfxMatrix tm =
     scaleMatrix * nsSVGUtils::GetCSSPxToDevPxMatrix(frame);
-  nsFilterInstance::PaintFilteredFrame(frame, context.GetDrawTarget(),
+  nsFilterInstance::PaintFilteredFrame(frame, &context,
                                        tm, &callback, &dirtyRegion,
                                        aParams.imgParams);
 
