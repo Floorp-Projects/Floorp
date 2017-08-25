34e218781dad2d84f316b8f66d52977ccf074191
Marco Zehe <mzehe@mozilla.com>
2017-08-25 14:28:39 +0200

    Bug 1392926 - Only expose the Region landmark role if a section element has been explicitly named by the author, r=surkov
    MozReview-Commit-ID: 2HIVAuQBfQx
    
    --HG--
    extra : rebase_source : e309a9d82a2ce61b399cc0da5d547740d8bdb5a9

--- accessible/generic/HyperTextAccessible.cpp
+++ accessible/generic/HyperTextAccessible.cpp
@@ -1175,6 +1175,13 @@ HyperTextAccessible::LandmarkRole() const
     return nsGkAtoms::main;
   }
 
+  // Only return xml-roles "region" if the section has an accessible name.
+  if (mContent->IsHTMLElement(nsGkAtoms::section)) {
+    nsAutoString name;
+    const_cast<HyperTextAccessible*>(this)->Name(name);
+    return name.IsEmpty() ? nullptr : nsGkAtoms::region;
+  }
+
   return nullptr;
 }
 
