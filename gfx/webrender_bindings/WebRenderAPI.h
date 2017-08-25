03c08e9cebbb51b4d6ea4b078f6cee93b1d24898
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-25 12:40:37 +0200

    Backed out changeset 663105088e1e (bug 1393031)

--- gfx/webrender_bindings/WebRenderAPI.h
+++ gfx/webrender_bindings/WebRenderAPI.h
@@ -53,13 +53,8 @@ struct Line {
 class ResourceUpdateQueue {
 public:
   ResourceUpdateQueue();
+
   ~ResourceUpdateQueue();
-  ResourceUpdateQueue(ResourceUpdateQueue&&);
-  ResourceUpdateQueue(ResourceUpdateQueue&) = delete;
-  ResourceUpdateQueue(const ResourceUpdateQueue&) = delete;
-  ResourceUpdateQueue& operator=(ResourceUpdateQueue&&);
-  ResourceUpdateQueue& operator=(ResourceUpdateQueue&) = delete;
-  ResourceUpdateQueue& operator=(const ResourceUpdateQueue&) = delete;
 
   void AddImage(wr::ImageKey aKey,
                 const ImageDescriptor& aDescriptor,
