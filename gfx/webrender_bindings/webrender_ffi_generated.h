caf800b93ae8a316a6e5a0c202a6fbc59b7342c6
Nicolas Silva <nsilva@mozilla.com>
2017-08-25 11:58:00 +0200

    Bug 1393031 - Expose WebRender's ResourceUpdates to C++. r=sotaro

--- gfx/webrender_bindings/webrender_ffi_generated.h
+++ gfx/webrender_bindings/webrender_ffi_generated.h
@@ -167,6 +167,9 @@ struct DocumentHandle;
 // RenderBackend.
 struct Renderer;
 
+// The resource updates for a given transaction (they must be applied in the same frame).
+struct ResourceUpdates;
+
 struct Vec_u8;
 
 struct WrRenderedEpochs;
@@ -178,83 +181,6 @@ struct WrThreadPool;
 typedef Vec_u8 VecU8;
 
 typedef Arc_VecU8 ArcVecU8;
-
-struct IdNamespace {
-  uint32_t mHandle;
-
-  bool operator==(const IdNamespace& aOther) const {
-    return mHandle == aOther.mHandle;
-  }
-  bool operator!=(const IdNamespace& aOther) const {
-    return mHandle != aOther.mHandle;
-  }
-  bool operator<(const IdNamespace& aOther) const {
-    return mHandle < aOther.mHandle;
-  }
-  bool operator<=(const IdNamespace& aOther) const {
-    return mHandle <= aOther.mHandle;
-  }
-};
-
-struct ImageKey {
-  IdNamespace mNamespace;
-  uint32_t mHandle;
-
-  bool operator==(const ImageKey& aOther) const {
-    return mNamespace == aOther.mNamespace &&
-           mHandle == aOther.mHandle;
-  }
-};
-
-typedef ImageKey WrImageKey;
-
-struct WrImageDescriptor {
-  ImageFormat format;
-  uint32_t width;
-  uint32_t height;
-  uint32_t stride;
-  bool is_opaque;
-
-  bool operator==(const WrImageDescriptor& aOther) const {
-    return format == aOther.format &&
-           width == aOther.width &&
-           height == aOther.height &&
-           stride == aOther.stride &&
-           is_opaque == aOther.is_opaque;
-  }
-};
-
-struct ByteSlice {
-  const uint8_t *buffer;
-  size_t len;
-
-  bool operator==(const ByteSlice& aOther) const {
-    return buffer == aOther.buffer &&
-           len == aOther.len;
-  }
-};
-
-struct WrExternalImageId {
-  uint64_t mHandle;
-
-  bool operator==(const WrExternalImageId& aOther) const {
-    return mHandle == aOther.mHandle;
-  }
-};
-
-typedef ExternalImageType WrExternalImageBufferType;
-
-struct FontKey {
-  IdNamespace mNamespace;
-  uint32_t mHandle;
-
-  bool operator==(const FontKey& aOther) const {
-    return mNamespace == aOther.mNamespace &&
-           mHandle == aOther.mHandle;
-  }
-};
-
-typedef FontKey WrFontKey;
 
 struct Epoch {
   uint32_t mHandle;
@@ -395,6 +321,23 @@ struct WrTransformProperty {
   LayoutTransform transform;
 };
 
+struct IdNamespace {
+  uint32_t mHandle;
+
+  bool operator==(const IdNamespace& aOther) const {
+    return mHandle == aOther.mHandle;
+  }
+  bool operator!=(const IdNamespace& aOther) const {
+    return mHandle != aOther.mHandle;
+  }
+  bool operator<(const IdNamespace& aOther) const {
+    return mHandle < aOther.mHandle;
+  }
+  bool operator<=(const IdNamespace& aOther) const {
+    return mHandle <= aOther.mHandle;
+  }
+};
+
 typedef IdNamespace WrIdNamespace;
 
 // Represents RGBA screen colors with floating point numbers.
@@ -463,6 +406,18 @@ struct WrComplexClipRegion {
            radii == aOther.radii;
   }
 };
+
+struct ImageKey {
+  IdNamespace mNamespace;
+  uint32_t mHandle;
+
+  bool operator==(const ImageKey& aOther) const {
+    return mNamespace == aOther.mNamespace &&
+           mHandle == aOther.mHandle;
+  }
+};
+
+typedef ImageKey WrImageKey;
 
 struct WrImageMask {
   WrImageKey image;
@@ -580,6 +535,18 @@ struct WrFilterOp {
   }
 };
 
+struct FontKey {
+  IdNamespace mNamespace;
+  uint32_t mHandle;
+
+  bool operator==(const FontKey& aOther) const {
+    return mNamespace == aOther.mNamespace &&
+           mHandle == aOther.mHandle;
+  }
+};
+
+typedef FontKey WrFontKey;
+
 typedef uint32_t GlyphIndex;
 
 struct GlyphInstance {
@@ -605,6 +572,16 @@ struct TextShadow {
 };
 
 typedef YuvColorSpace WrYuvColorSpace;
+
+struct ByteSlice {
+  const uint8_t *buffer;
+  size_t len;
+
+  bool operator==(const ByteSlice& aOther) const {
+    return buffer == aOther.buffer &&
+           len == aOther.len;
+  }
+};
 
 struct MutByteSlice {
   uint8_t *buffer;
@@ -660,6 +637,14 @@ struct WrExternalImage {
   }
 };
 
+struct WrExternalImageId {
+  uint64_t mHandle;
+
+  bool operator==(const WrExternalImageId& aOther) const {
+    return mHandle == aOther.mHandle;
+  }
+};
+
 typedef WrExternalImage (*LockExternalImageCallback)(void*, WrExternalImageId, uint8_t);
 
 typedef void (*UnlockExternalImageCallback)(void*, WrExternalImageId, uint8_t);
@@ -675,6 +660,24 @@ struct WrExternalImageHandler {
            unlock_func == aOther.unlock_func;
   }
 };
+
+struct WrImageDescriptor {
+  ImageFormat format;
+  uint32_t width;
+  uint32_t height;
+  uint32_t stride;
+  bool is_opaque;
+
+  bool operator==(const WrImageDescriptor& aOther) const {
+    return format == aOther.format &&
+           width == aOther.width &&
+           height == aOther.height &&
+           stride == aOther.stride &&
+           is_opaque == aOther.is_opaque;
+  }
+};
+
+typedef ExternalImageType WrExternalImageBufferType;
 
 /* DO NOT MODIFY THIS MANUALLY! This file was generated using cbindgen.
  * To generate this file:
@@ -700,40 +703,10 @@ const VecU8 *wr_add_ref_arc(const ArcVecU8 *aArc)
 WR_FUNC;
 
 WR_INLINE
-void wr_api_add_blob_image(DocumentHandle *aDh,
-                           WrImageKey aImageKey,
-                           const WrImageDescriptor *aDescriptor,
-                           ByteSlice aBytes)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_add_external_image(DocumentHandle *aDh,
-                               WrImageKey aImageKey,
-                               const WrImageDescriptor *aDescriptor,
-                               WrExternalImageId aExternalImageId,
-                               WrExternalImageBufferType aBufferType,
-                               uint8_t aChannelIndex)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_add_image(DocumentHandle *aDh,
-                      WrImageKey aImageKey,
-                      const WrImageDescriptor *aDescriptor,
-                      ByteSlice aBytes)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_add_raw_font(DocumentHandle *aDh,
-                         WrFontKey aKey,
-                         uint8_t *aFontBuffer,
-                         size_t aBufferSize,
-                         uint32_t aIndex)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_clear_root_display_list(DocumentHandle *aDh,
-                                    WrEpoch aEpoch,
-                                    WrPipelineId aPipelineId)
+void wr_api_clear_display_list(DocumentHandle *aDh,
+                               WrEpoch aEpoch,
+                               WrPipelineId aPipelineId,
+                               ResourceUpdates *aResources)
 WR_FUNC;
 
 WR_INLINE
@@ -744,16 +717,6 @@ WR_FUNC;
 WR_INLINE
 void wr_api_delete(DocumentHandle *aDh)
 WR_DESTRUCTOR_SAFE_FUNC;
-
-WR_INLINE
-void wr_api_delete_font(DocumentHandle *aDh,
-                        WrFontKey aKey)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_delete_image(DocumentHandle *aDh,
-                         WrImageKey aKey)
-WR_FUNC;
 
 WR_INLINE
 void wr_api_finalize_builder(WrState *aState,
@@ -784,16 +747,17 @@ void wr_api_send_external_event(DocumentHandle *aDh,
 WR_DESTRUCTOR_SAFE_FUNC;
 
 WR_INLINE
-void wr_api_set_root_display_list(DocumentHandle *aDh,
-                                  ColorF aColor,
-                                  WrEpoch aEpoch,
-                                  float aViewportWidth,
-                                  float aViewportHeight,
-                                  WrPipelineId aPipelineId,
-                                  LayoutSize aContentSize,
-                                  BuiltDisplayListDescriptor aDlDescriptor,
-                                  uint8_t *aDlData,
-                                  size_t aDlSize)
+void wr_api_set_display_list(DocumentHandle *aDh,
+                             ColorF aColor,
+                             WrEpoch aEpoch,
+                             float aViewportWidth,
+                             float aViewportHeight,
+                             WrPipelineId aPipelineId,
+                             LayoutSize aContentSize,
+                             BuiltDisplayListDescriptor aDlDescriptor,
+                             uint8_t *aDlData,
+                             size_t aDlSize,
+                             ResourceUpdates *aResources)
 WR_FUNC;
 
 WR_INLINE
@@ -808,26 +772,8 @@ void wr_api_set_window_parameters(DocumentHandle *aDh,
 WR_FUNC;
 
 WR_INLINE
-void wr_api_update_blob_image(DocumentHandle *aDh,
-                              WrImageKey aImageKey,
-                              const WrImageDescriptor *aDescriptor,
-                              ByteSlice aBytes)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_update_external_image(DocumentHandle *aDh,
-                                  WrImageKey aKey,
-                                  const WrImageDescriptor *aDescriptor,
-                                  WrExternalImageId aExternalImageId,
-                                  WrExternalImageBufferType aImageType,
-                                  uint8_t aChannelIndex)
-WR_FUNC;
-
-WR_INLINE
-void wr_api_update_image(DocumentHandle *aDh,
-                         WrImageKey aKey,
-                         const WrImageDescriptor *aDescriptor,
-                         ByteSlice aBytes)
+void wr_api_update_resources(DocumentHandle *aDh,
+                             ResourceUpdates *aResources)
 WR_FUNC;
 
 WR_INLINE
@@ -1152,6 +1098,78 @@ WR_FUNC;
 
 WR_INLINE
 void wr_renderer_update(Renderer *aRenderer)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_add_blob_image(ResourceUpdates *aResources,
+                                        WrImageKey aImageKey,
+                                        const WrImageDescriptor *aDescriptor,
+                                        ByteSlice aBytes)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_add_external_image(ResourceUpdates *aResources,
+                                            WrImageKey aImageKey,
+                                            const WrImageDescriptor *aDescriptor,
+                                            WrExternalImageId aExternalImageId,
+                                            WrExternalImageBufferType aBufferType,
+                                            uint8_t aChannelIndex)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_add_image(ResourceUpdates *aResources,
+                                   WrImageKey aImageKey,
+                                   const WrImageDescriptor *aDescriptor,
+                                   ByteSlice aBytes)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_add_raw_font(ResourceUpdates *aResources,
+                                      WrFontKey aKey,
+                                      uint8_t *aFontBuffer,
+                                      size_t aBufferSize,
+                                      uint32_t aIndex)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_delete(ResourceUpdates *aUpdates)
+WR_DESTRUCTOR_SAFE_FUNC;
+
+WR_INLINE
+void wr_resource_updates_delete_font(ResourceUpdates *aResources,
+                                     WrFontKey aKey)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_delete_image(ResourceUpdates *aResources,
+                                      WrImageKey aKey)
+WR_FUNC;
+
+WR_INLINE
+ResourceUpdates *wr_resource_updates_new()
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_update_blob_image(ResourceUpdates *aResources,
+                                           WrImageKey aImageKey,
+                                           const WrImageDescriptor *aDescriptor,
+                                           ByteSlice aBytes)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_update_external_image(ResourceUpdates *aResources,
+                                               WrImageKey aKey,
+                                               const WrImageDescriptor *aDescriptor,
+                                               WrExternalImageId aExternalImageId,
+                                               WrExternalImageBufferType aImageType,
+                                               uint8_t aChannelIndex)
+WR_FUNC;
+
+WR_INLINE
+void wr_resource_updates_update_image(ResourceUpdates *aResources,
+                                      WrImageKey aKey,
+                                      const WrImageDescriptor *aDescriptor,
+                                      ByteSlice aBytes)
 WR_FUNC;
 
 WR_INLINE
