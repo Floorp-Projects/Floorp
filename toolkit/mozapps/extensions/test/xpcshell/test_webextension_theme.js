862cb6e695a852fb64db6726110cf1096e622593
Andrew Swan <aswan@mozilla.com>
2017-08-26 08:57:56 -0700

    Bug 1393156 Remove addons manager tests specific to complete themes r=kmag
    MozReview-Commit-ID: 7D7k2q3x8v9

--- toolkit/mozapps/extensions/test/xpcshell/test_webextension_theme.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_webextension_theme.js
@@ -9,10 +9,12 @@
  */
 
 const {LightweightThemeManager} = AM_Cu.import("resource://gre/modules/LightweightThemeManager.jsm", {});
-const THEME_IDS = ["theme1@tests.mozilla.org", "theme3@tests.mozilla.org",
-  "theme2@personas.mozilla.org", "default@tests.mozilla.org"];
-const REQUIRE_RESTART = { [THEME_IDS[0]]: 1 };
-const DEFAULT_THEME = THEME_IDS[3];
+const THEME_IDS = [
+  "theme3@tests.mozilla.org",
+  "theme2@personas.mozilla.org",
+  "default@tests.mozilla.org"
+];
+const DEFAULT_THEME = THEME_IDS[2];
 
 const profileDir = gProfD.clone();
 profileDir.append("extensions");
@@ -23,20 +25,6 @@ var gActiveTheme = null;
 add_task(async function setup_to_default_browserish_state() {
   createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
 
-  writeInstallRDFForExtension({
-    id: THEME_IDS[0],
-    version: "1.0",
-    name: "Test 1",
-    type: 4,
-    skinnable: true,
-    internalName: "theme1/1.0",
-    targetApplications: [{
-      id: "xpcshell@tests.mozilla.org",
-      minVersion: "1",
-      maxVersion: "2"
-    }]
-  }, profileDir);
-
   await promiseWriteWebManifestForExtension({
     author: "Some author",
     manifest_version: 2,
@@ -45,11 +33,10 @@ add_task(async function setup_to_default_browserish_state() {
     theme: { images: { headerURL: "example.png" } },
     applications: {
       gecko: {
-        id: THEME_IDS[1]
+        id: THEME_IDS[0]
       }
     }
   }, profileDir);
-
   // We need a default theme for some of these things to work but we have hidden
   // the one in the application directory.
   writeInstallRDFForExtension({
@@ -68,7 +55,7 @@ add_task(async function setup_to_default_browserish_state() {
 
   // We can add an LWT only after the Addon Manager was started.
   LightweightThemeManager.currentTheme = {
-    id: THEME_IDS[2].substr(0, THEME_IDS[2].indexOf("@")),
+    id: THEME_IDS[1].substr(0, THEME_IDS[1].indexOf("@")),
     version: "1",
     name: "Bling",
     description: "SO MUCH BLING!",
@@ -81,24 +68,21 @@ add_task(async function setup_to_default_browserish_state() {
     accentcolor: Math.random().toString()
   };
 
-  let [ t1, t2, t3, d ] = await promiseAddonsByIDs(THEME_IDS);
+  let [ t1, t2, d ] = await promiseAddonsByIDs(THEME_IDS);
   Assert.ok(t1, "Theme addon should exist");
   Assert.ok(t2, "Theme addon should exist");
-  Assert.ok(t3, "Theme addon should exist");
   Assert.ok(d, "Theme addon should exist");
 
-  t1.userDisabled = t2.userDisabled = t3.userDisabled = true;
+  t1.userDisabled = t2.userDisabled = true;
   Assert.ok(!t1.isActive, "Theme should be disabled");
   Assert.ok(!t2.isActive, "Theme should be disabled");
-  Assert.ok(!t3.isActive, "Theme should be disabled");
   Assert.ok(d.isActive, "Default theme should be active");
 
   await promiseRestartManager();
 
-  [ t1, t2, t3, d ] = await promiseAddonsByIDs(THEME_IDS);
+  [ t1, t2, d ] = await promiseAddonsByIDs(THEME_IDS);
   Assert.ok(!t1.isActive, "Theme should still be disabled");
   Assert.ok(!t2.isActive, "Theme should still be disabled");
-  Assert.ok(!t3.isActive, "Theme should still be disabled");
   Assert.ok(d.isActive, "Default theme should still be active");
 
   gActiveTheme = d.id;
@@ -117,20 +101,21 @@ async function setDisabledStateAndCheck(which, disabled = false) {
 
   let themeToDisable = disabled ? which : gActiveTheme;
   let themeToEnable = disabled ? DEFAULT_THEME : which;
-  let expectRestart = !!(REQUIRE_RESTART[themeToDisable] || REQUIRE_RESTART[themeToEnable]);
 
   let expectedStates = {
     [themeToDisable]: true,
     [themeToEnable]: false
   };
   let expectedEvents = {
-    [themeToDisable]: [ [ "onDisabling", expectRestart ] ],
-    [themeToEnable]: [ [ "onEnabling", expectRestart ] ]
+    [themeToDisable]: [
+      [ "onDisabling", false ],
+      [ "onDisabled", false ],
+    ],
+    [themeToEnable]: [
+      [ "onEnabling", false ],
+      [ "onEnabled", false ],
+    ]
   };
-  if (!expectRestart) {
-    expectedEvents[themeToDisable].push([ "onDisabled", false ]);
-    expectedEvents[themeToEnable].push([ "onEnabled", false ]);
-  }
 
   // Set the state of the theme to change.
   let theme = await promiseAddonByID(which);
@@ -142,17 +127,10 @@ async function setDisabledStateAndCheck(which, disabled = false) {
     isDisabled = (theme.id in expectedStates) ? expectedStates[theme.id] : true;
     Assert.equal(theme.userDisabled, isDisabled,
       `Theme '${theme.id}' should be ${isDisabled ? "dis" : "en"}abled`);
-    // Some themes need a restart to get their act together.
-    if (expectRestart && (theme.id == themeToEnable || theme.id == themeToDisable)) {
-      let expectedFlag = theme.id == themeToEnable ? AddonManager.PENDING_ENABLE : AddonManager.PENDING_DISABLE;
-      Assert.ok(hasFlag(theme.pendingOperations, expectedFlag),
-        "When expecting a restart, the pending operation flags should match");
-    } else {
-      Assert.equal(theme.pendingOperations, AddonManager.PENDING_NONE,
-        "There should be no pending operations when no restart is expected");
-      Assert.equal(theme.isActive, !isDisabled,
-        `Theme '${theme.id} should be ${isDisabled ? "in" : ""}active`);
-    }
+    Assert.equal(theme.pendingOperations, AddonManager.PENDING_NONE,
+      "There should be no pending operations when no restart is expected");
+    Assert.equal(theme.isActive, !isDisabled,
+      `Theme '${theme.id} should be ${isDisabled ? "in" : ""}active`);
   }
 
   await promiseRestartManager();
@@ -173,55 +151,38 @@ async function setDisabledStateAndCheck(which, disabled = false) {
   ensure_test_completed();
 }
 
-add_task(async function test_complete_themes() {
-  // Enable the complete theme.
+add_task(async function test_WebExtension_themes() {
+  // Enable the WebExtension theme.
   await setDisabledStateAndCheck(THEME_IDS[0]);
 
-  // Disabling the complete theme should revert to the default theme.
+  // Disabling WebExtension should revert to the default theme.
   await setDisabledStateAndCheck(THEME_IDS[0], true);
 
   // Enable it again.
   await setDisabledStateAndCheck(THEME_IDS[0]);
 
-  // Enabling a WebExtension theme should disable the active theme.
+  // Enabling an LWT should disable the active theme.
   await setDisabledStateAndCheck(THEME_IDS[1]);
 
-  // Switching back should disable the WebExtension theme.
+  // Switching back should disable the LWT.
   await setDisabledStateAndCheck(THEME_IDS[0]);
 });
 
-add_task(async function test_WebExtension_themes() {
-  // Enable the WebExtension theme.
+add_task(async function test_LWTs() {
+  // Start with enabling an LWT.
   await setDisabledStateAndCheck(THEME_IDS[1]);
 
-  // Disabling WebExtension should revert to the default theme.
+  // Disabling LWT should revert to the default theme.
   await setDisabledStateAndCheck(THEME_IDS[1], true);
 
   // Enable it again.
   await setDisabledStateAndCheck(THEME_IDS[1]);
 
-  // Enabling an LWT should disable the active theme.
-  await setDisabledStateAndCheck(THEME_IDS[2]);
-
-  // Switching back should disable the LWT.
-  await setDisabledStateAndCheck(THEME_IDS[1]);
-});
-
-add_task(async function test_LWTs() {
-  // Start with enabling an LWT.
-  await setDisabledStateAndCheck(THEME_IDS[2]);
-
-  // Disabling LWT should revert to the default theme.
-  await setDisabledStateAndCheck(THEME_IDS[2], true);
-
-  // Enable it again.
-  await setDisabledStateAndCheck(THEME_IDS[2]);
-
   // Enabling a WebExtension theme should disable the active theme.
-  await setDisabledStateAndCheck(THEME_IDS[1]);
+  await setDisabledStateAndCheck(THEME_IDS[0]);
 
   // Switching back should disable the LWT.
-  await setDisabledStateAndCheck(THEME_IDS[2]);
+  await setDisabledStateAndCheck(THEME_IDS[1]);
 });
 
 add_task(async function test_default_theme() {
@@ -229,8 +190,49 @@ add_task(async function test_default_theme() {
   await setDisabledStateAndCheck(DEFAULT_THEME);
 
   // Swith to the WebExtension theme.
-  await setDisabledStateAndCheck(THEME_IDS[1]);
+  await setDisabledStateAndCheck(THEME_IDS[0]);
 
   // Enable it again.
   await setDisabledStateAndCheck(DEFAULT_THEME);
+});
+
+add_task(async function uninstall_offers_undo() {
+  const ID = THEME_IDS[0];
+  let theme = await promiseAddonByID(ID);
+
+  Assert.ok(theme, "Webextension theme is present");
+  Assert.ok(!theme.isActive, "Webextension theme is not active");
+
+  function promiseAddonEvent(event, id) {
+    return new Promise(resolve => {
+      let listener = {
+        // eslint-disable-next-line object-shorthand
+        [event]: function(addon) {
+          if (id) {
+            Assert.equal(addon.id, id, "Got event for expected addon");
+          }
+          AddonManager.removeAddonListener(listener);
+          resolve();
+        }
+      };
+      AddonManager.addAddonListener(listener);
+    });
+  }
+
+  let uninstallingPromise = promiseAddonEvent("onUninstalling", ID);
+  theme.uninstall(true);
+  await uninstallingPromise;
+
+  Assert.ok(hasFlag(theme.pendingOperations, AddonManager.PENDING_UNINSTALL),
+            "Theme being uninstalled has PENDING_UNINSTALL flag");
+
+  let cancelPromise = promiseAddonEvent("onOperationCancelled", ID);
+  theme.cancelUninstall();
+  await cancelPromise;
+
+  Assert.equal(theme.pendingOperations, AddonManager.PENDING_NONE,
+               "PENDING_UNINSTALL flag is cleared when uninstall is canceled");
+
+  theme.uninstall();
+  await promiseRestartManager();
 });
