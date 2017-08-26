96e6582cc604074005638702d7371f4597b38028
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2017-08-26 17:54:14 +0900

    Bug 1394084 - Remove "code" from jsprototype.h macros. r=luke
    MozReview-Commit-ID: HbH0Iip2f3n

--- js/src/jspubtd.h
+++ js/src/jspubtd.h
@@ -77,7 +77,7 @@ enum JSType {
 
 /* Dense index into cached prototypes and class atoms for standard objects. */
 enum JSProtoKey {
-#define PROTOKEY_AND_INITIALIZER(name,code,init,clasp) JSProto_##name = code,
+#define PROTOKEY_AND_INITIALIZER(name,init,clasp) JSProto_##name,
     JS_FOR_EACH_PROTOTYPE(PROTOKEY_AND_INITIALIZER)
 #undef PROTOKEY_AND_INITIALIZER
     JSProto_LIMIT
