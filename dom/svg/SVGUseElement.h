a36bb66978e949b9a019e46294302350548f7e7c
Bobby Holley <bobbyholley@gmail.com>
2017-08-27 11:15:03 -0700

    Bug 1393791 - Move SVG Use anonymous content to the frame. r=emilio
    This brings it into alignment with what everything else does.
    
    MozReview-Commit-ID: 2A9p8umHnKi

--- dom/svg/SVGUseElement.h
+++ dom/svg/SVGUseElement.h
@@ -57,9 +57,7 @@ public:
   NS_DECL_NSIMUTATIONOBSERVER_NODEWILLBEDESTROYED
 
   // for nsSVGUseFrame's nsIAnonymousContentCreator implementation.
-  nsIContent* CreateAnonymousContent();
-  nsIContent* GetAnonymousContent() const { return mClone; }
-  void DestroyAnonymousContent();
+  already_AddRefed<nsIContent> CreateAnonymousContent();
 
   // nsSVGElement specializations:
   virtual gfxMatrix PrependLocalTransformsTo(
@@ -97,6 +95,8 @@ protected:
   private:
     SVGUseElement* mContainer;
   };
+
+  nsSVGUseFrame* GetFrame() const;
 
   virtual LengthAttributesInfo GetLengthInfo() override;
   virtual StringAttributesInfo GetStringInfo() override;
