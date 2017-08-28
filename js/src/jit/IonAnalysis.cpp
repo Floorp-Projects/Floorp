139b412ab25c442fa5f08eae2119f2b1f7482959
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2017-08-28 13:04:53 +0000

    Bug 1388045 - Branch Pruning: Check the compile info associated with the resume point. r=sstangl, a=gchang

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -211,6 +211,7 @@ FlagAllOperandsAsHavingRemovedUses(MIRGenerator* mir, MBasicBlock* block)
         if (MResumePoint* rp = ins->resumePoint()) {
             // Note: no need to iterate over the caller's of the resume point as
             // this is the same as the entry resume point.
+            MOZ_ASSERT(&rp->block()->info() == &info);
             for (size_t i = 0, e = rp->numOperands(); i < e; i++) {
                 if (info.isObservableSlot(i))
                     rp->getOperand(i)->setUseRemovedUnchecked();
@@ -224,6 +225,7 @@ FlagAllOperandsAsHavingRemovedUses(MIRGenerator* mir, MBasicBlock* block)
         if (mir->shouldCancel("FlagAllOperandsAsHavingRemovedUses loop 2"))
             return false;
 
+        const CompileInfo& info = rp->block()->info();
         for (size_t i = 0, e = rp->numOperands(); i < e; i++) {
             if (info.isObservableSlot(i))
                 rp->getOperand(i)->setUseRemovedUnchecked();
