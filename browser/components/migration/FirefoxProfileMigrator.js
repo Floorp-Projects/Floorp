28ed6a7c5c0e16e33b1f0a8410a1e81b1be97cd3
David Keeler <dkeeler@mozilla.com>
2017-08-28 12:08:09 -0700

    bug 1394532 - fix up modeline in FirefoxProfileMigrator.js r=mossop
    MozReview-Commit-ID: ChPuFDYrbDt

--- browser/components/migration/FirefoxProfileMigrator.js
+++ browser/components/migration/FirefoxProfileMigrator.js
@@ -1,6 +1,6 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
- * vim: sw=2 ts=2 sts=2 et */
- /* This Source Code Form is subject to the terms of the Mozilla Public
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sw=2 ts=2 sts=2 et */
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
