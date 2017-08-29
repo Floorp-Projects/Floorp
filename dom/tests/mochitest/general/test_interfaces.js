01aa90b3cc70964cd63b049ceb9c4f3b3c66904a
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-29 09:17:04 +0200

    Backed out changeset 6cbeeec89846 (bug 1378342)

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -103,10 +103,6 @@ var legacyMozPrefixedInterfaces =
 var interfaceNamesInGlobalScope =
   [
 // IMPORTANT: Do not change this list without review from a DOM peer!
-    "AbortController",
-// IMPORTANT: Do not change this list without review from a DOM peer!
-    "AbortSignal",
-// IMPORTANT: Do not change this list without review from a DOM peer!
     "AnalyserNode",
 // IMPORTANT: Do not change this list without review from a DOM peer!
     {name: "Animation"},
