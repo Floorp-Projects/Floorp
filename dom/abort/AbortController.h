1c556f2d3f066b2254ba82e97d268136a1ec068c
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-08-29 09:17:15 +0200

    Backed out changeset 203ad2fab9aa (bug 1378342)

--- dom/abort/AbortController.h
+++ dom/abort/AbortController.h
@@ -8,19 +8,16 @@
 #define mozilla_dom_AbortController_h
 
 #include "mozilla/dom/BindingDeclarations.h"
-#include "mozilla/ErrorResult.h"
+#include "mozilla/dom/AbortSignal.h"
 #include "nsCycleCollectionParticipant.h"
 #include "nsWrapperCache.h"
-
-class nsIGlobalObject;
 
 namespace mozilla {
 namespace dom {
 
-class AbortSignal;
-
 class AbortController final : public nsISupports
                             , public nsWrapperCache
+                            , public AbortSignal::Follower
 {
 public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
@@ -45,6 +42,19 @@ public:
 
   void
   Abort();
+
+  void
+  Follow(AbortSignal& aSignal);
+
+  void
+  Unfollow(AbortSignal& aSignal);
+
+  AbortSignal*
+  Following() const;
+
+  // AbortSignal::Follower
+
+  void Aborted() override;
 
 private:
   ~AbortController() = default;
