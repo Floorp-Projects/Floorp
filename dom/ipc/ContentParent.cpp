d54cc4e71ea3ce81d9ffd3ad82fb2ca221f2b3a5
Michael Layzell <michael@thelayzells.com>
2017-08-29 11:19:48 -0400

    Bug 1394596 - Support window.open(undefined, undefined, "noopener") in the child process, r=smaug

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -4829,7 +4829,7 @@ ContentParent::RecvCreateWindowInDifferentProcess(
   const bool& aCalledFromJS,
   const bool& aPositionSpecified,
   const bool& aSizeSpecified,
-  const URIParams& aURIToLoad,
+  const OptionalURIParams& aURIToLoad,
   const nsCString& aFeatures,
   const nsCString& aBaseURI,
   const float& aFullZoom,
