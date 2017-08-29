7298681f5c77848c9f805c5a344098d6d4479eb2
Marco Bonardo <mbonardo@mozilla.com>
2017-08-29 23:26:24 +0200

    Bug 1392081 - Reset the Places toolbar view when the toolbar is collapsed. r=Gijs
    MozReview-Commit-ID: IFOxCnslZRX
    
    --HG--
    extra : rebase_source : 446fb2849146df0560336bd8cf751d93e18648b3

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5530,7 +5530,6 @@ function setToolbarVisibility(toolbar, isVisible, persist = true) {
   let event = new CustomEvent("toolbarvisibilitychange", eventParams);
   toolbar.dispatchEvent(event);
 
-  PlacesToolbarHelper.init();
   BookmarkingUI.onToolbarVisibilityChange();
 }
 
