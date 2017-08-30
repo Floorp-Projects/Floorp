fd75d336161c8a901b1f0e2e1cac1867aaa40f49
Wes Kocher <wkocher@mozilla.com>
2017-08-30 14:27:11 -0700

    Backed out changeset 1575ef52aa22 (bug 1394053) for failures in browser_security-1.js,browser_security-2.js,browser_security.js a=bustage CLOSED TREE
    MozReview-Commit-ID: G8PrqcM5UYf

--- browser/components/preferences/in-content/security.js
+++ browser/components/preferences/in-content/security.js
@@ -174,32 +174,24 @@ var gSecurityPane = {
       safeBrowsingMalwarePref.value = enableSafeBrowsing.checked;
 
       if (enableSafeBrowsing.checked) {
-        if (blockDownloads) {
-          blockDownloads.removeAttribute("disabled");
-          if (blockDownloads.checked) {
-            blockUncommonUnwanted.removeAttribute("disabled");
-          }
-        } else {
+        blockDownloads.removeAttribute("disabled");
+        if (blockDownloads.checked) {
           blockUncommonUnwanted.removeAttribute("disabled");
         }
       } else {
-        if (blockDownloads) {
-          blockDownloads.setAttribute("disabled", "true");
-        }
+        blockDownloads.setAttribute("disabled", "true");
         blockUncommonUnwanted.setAttribute("disabled", "true");
       }
     });
 
-    if (blockDownloads) {
-      blockDownloads.addEventListener("command", function() {
-        blockDownloadsPref.value = blockDownloads.checked;
-        if (blockDownloads.checked) {
-          blockUncommonUnwanted.removeAttribute("disabled");
-        } else {
-          blockUncommonUnwanted.setAttribute("disabled", "true");
-        }
-      });
-    }
+    blockDownloads.addEventListener("command", function() {
+      blockDownloadsPref.value = blockDownloads.checked;
+      if (blockDownloads.checked) {
+        blockUncommonUnwanted.removeAttribute("disabled");
+      } else {
+        blockUncommonUnwanted.setAttribute("disabled", "true");
+      }
+    });
 
     blockUncommonUnwanted.addEventListener("command", function() {
       blockUnwantedPref.value = blockUncommonUnwanted.checked;
@@ -231,18 +223,13 @@ var gSecurityPane = {
 
     enableSafeBrowsing.checked = safeBrowsingPhishingPref.value && safeBrowsingMalwarePref.value;
     if (!enableSafeBrowsing.checked) {
-      if (blockDownloads) {
-        blockDownloads.setAttribute("disabled", "true");
-      }
-
+      blockDownloads.setAttribute("disabled", "true");
       blockUncommonUnwanted.setAttribute("disabled", "true");
     }
 
-    if (blockDownloads) {
-      blockDownloads.checked = blockDownloadsPref.value;
-      if (!blockDownloadsPref.value) {
-        blockUncommonUnwanted.setAttribute("disabled", "true");
-      }
+    blockDownloads.checked = blockDownloadsPref.value;
+    if (!blockDownloadsPref.value) {
+      blockUncommonUnwanted.setAttribute("disabled", "true");
     }
 
     blockUncommonUnwanted.checked = blockUnwantedPref.value && blockUncommonPref.value;
