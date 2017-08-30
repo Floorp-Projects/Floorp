178c85d02bf3a4a5d24ac19aa58c6a257da57133
Luke Chang <lchang@mozilla.com>
2017-08-30 11:29:59 +0800

    Bug 1395019 - [Form Autofill] Part 1: The raw data of credit card fields shouldn't include "cc-exp". r=seanlee
    MozReview-Commit-ID: 4OSOgeEgWh9

--- browser/extensions/formautofill/test/unit/test_creditCardRecords.js
+++ browser/extensions/formautofill/test/unit/test_creditCardRecords.js
@@ -128,11 +128,13 @@ add_task(async function test_getAll() {
   // Check computed fields.
   do_check_eq(creditCards[0]["cc-given-name"], "John");
   do_check_eq(creditCards[0]["cc-family-name"], "Doe");
+  do_check_eq(creditCards[0]["cc-exp"], "2017-04");
 
   // Test with rawData set.
   creditCards = profileStorage.creditCards.getAll({rawData: true});
   do_check_eq(creditCards[0]["cc-given-name"], undefined);
   do_check_eq(creditCards[0]["cc-family-name"], undefined);
+  do_check_eq(creditCards[0]["cc-exp"], undefined);
 
   // Modifying output shouldn't affect the storage.
   creditCards[0]["cc-name"] = "test";
