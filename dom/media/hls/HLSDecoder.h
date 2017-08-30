6dd36073da386c7b2704ef8b1d22190628c0edaf
James Cheng <jacheng@mozilla.com>
2017-08-30 14:14:32 +0800

    Bug 1395015 - Remove HLSResource. r=jwwang
    Move the dependent method into HLSDecoder.
    Remove the class entirely.
    
    MozReview-Commit-ID: F9eOFQvgeLQ
    
    --HG--
    extra : rebase_source : a45407d88513883245f429c5f4bd09c78bef6f10

--- dom/media/hls/HLSDecoder.h
+++ dom/media/hls/HLSDecoder.h
@@ -7,21 +7,17 @@
 #ifndef HLSDecoder_h_
 #define HLSDecoder_h_
 
-#include "HLSResource.h"
 #include "MediaDecoder.h"
 
 namespace mozilla {
+
+class HLSResourceCallbacksSupport;
 
 class HLSDecoder final : public MediaDecoder
 {
 public:
   // MediaDecoder interface.
-  explicit HLSDecoder(MediaDecoderInit& aInit)
-    : MediaDecoder(aInit)
-  {
-  }
-
-  void Shutdown() override;
+  explicit HLSDecoder(MediaDecoderInit& aInit);
 
   // Returns true if the HLS backend is pref'ed on.
   static bool IsEnabled();
@@ -42,8 +38,11 @@ public:
   bool IsTransportSeekable() override { return true; }
   void Suspend() override;
   void Resume() override;
+  void Shutdown() override;
 
 private:
+  friend class HLSResourceCallbacksSupport;
+
   void PinForSeek() override {}
   void UnpinForSeek() override {}
 
@@ -58,7 +57,11 @@ private:
 
   bool IsLiveStream() override final { return false; }
 
-  UniquePtr<HLSResource> mResource;
+  nsCOMPtr<nsIChannel> mChannel;
+  nsCOMPtr<nsIURI> mURI;
+  java::GeckoHLSResourceWrapper::GlobalRef mHLSResourceWrapper;
+  java::GeckoHLSResourceWrapper::Callbacks::GlobalRef mJavaCallbacks;
+  RefPtr<HLSResourceCallbacksSupport> mCallbackSupport;
 };
 
 } // namespace mozilla
