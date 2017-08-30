c4a2fd3bfb4e37802fd2fd70714fd3a56b9ec85f
Kris Maglione <maglione.k@gmail.com>
2017-08-30 15:47:17 -0700

    Bug 1363482: Part 2 - Allow pre-loading file and JAR entry contents off-thread during startup. r=erahm
    MozReview-Commit-ID: 8bKzYpXBQvT
    
    --HG--
    extra : rebase_source : 6750d471a0a39338b5145e2dab9d953b4c30a63f

--- xpcom/build/FileLocation.h
+++ xpcom/build/FileLocation.h
@@ -35,6 +35,11 @@ public:
   FileLocation();
   ~FileLocation();
 
+  FileLocation(const FileLocation& aOther);
+  FileLocation(FileLocation&& aOther);
+
+  FileLocation& operator=(const FileLocation&) = default;
+
   /**
    * Constructor for plain files
    */
@@ -51,7 +56,7 @@ public:
   /**
    * Creates a new file location relative to another one.
    */
-  FileLocation(const FileLocation& aFile, const char* aPath = nullptr);
+  FileLocation(const FileLocation& aFile, const char* aPath);
 
   /**
    * Initialization functions corresponding to constructors
