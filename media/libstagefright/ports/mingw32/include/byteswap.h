568bd0f7fcbe7ee9bc1ae47f6025ec79bd0e2a12
Tom Ritter <tom@mozilla.com>
2017-08-30 20:39:51 -0500

    Bug 1349912 Do not use win32 local includes for libstagefright on MinGW build r=froydnj
    This duplicates all the win32 includes except pthread.h into a new directory
    for MinGW. MinGW needs all the same dummy includes _except_ it needs its
    own pthread.h for winpthreads.
    
    MozReview-Commit-ID: AlIvHhFoSIS
    
    --HG--
    extra : rebase_source : aba50e79b0be3bc7306bd32b4010076a8ca1184b

---
+++ media/libstagefright/ports/mingw32/include/byteswap.h
@@ -0,0 +1,10 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+#ifndef BYTESWAP_H_
+#define BYTESWAP_H_
+
+#include <stdlib.h>
+#define bswap_16 _byteswap_ushort
+
+#endif
