1a90ef84f7cbc3d3177aaa056c0fd0b0ad13ab79
Tom Tromey <tom@tromey.com>
2017-08-31 12:03:24 -0600

    Bug 1395545 - test that an invalid source map is reported to the console; r=nchevobbe
    MozReview-Commit-ID: 4a1auGMOKGT
    
    --HG--
    extra : rebase_source : 928871607eef5f77c1a0e1de7d72b82e2fb589fa

---
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_sourcemap_invalid.js
@@ -0,0 +1,34 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that an invalid source map is reported.
+
+const JS_URL = URL_ROOT + "code_bundle_invalidmap.js";
+
+const PAGE_URL = `data:text/html,
+<!doctype html>
+
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Empty test page to test source map with invalid source map</title>
+  </head>
+
+  <body>
+    <script src="${JS_URL}"></script>
+  </body>
+
+</html>`;
+
+add_task(function* () {
+  yield pushPref("devtools.source-map.client-service.enabled", true);
+
+  const hud = yield openNewTabAndConsole(PAGE_URL);
+
+  const node = yield waitFor(() => findMessage(hud, "Source map error"));
+  ok(node, "source map error is displayed in web console");
+});
