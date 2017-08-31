89443bb39f1a23ed8854fb95374aa3bf47860125
Mike Hommey <mh+mozilla@glandium.org>
2017-08-31 10:29:11 +0900

    Bug 1395776 - Add a level of indirection on top of mozjemalloc. r=njn
    Practically speaking, with code inlining, this doesn't change anything,
    but will allow, in a subsequent change, to easily divert the exported
    allocation functions (malloc, etc.) in order to inject replace-malloc
    in the pipeline.
    
    The added macro magic is copied from replace-malloc.c.
    
    At the same time, reformat the functions we're touching.
    
    --HG--
    extra : rebase_source : f615909101f832f3cc471e23a3cc44a60886843f

---
+++ memory/mozjemalloc/mozjemalloc.h
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozjemalloc_types.h"
+
+/* Generic interface exposing the whole public allocator API
+ * This facilitates the implementation of things like replace-malloc.
+ * Note: compilers are expected to be able to optimize out `this`.
+ */
+template <typename T>
+struct Allocator: public T {
+#define MALLOC_FUNCS (MALLOC_FUNCS_MALLOC | MALLOC_FUNCS_JEMALLOC)
+#define MALLOC_DECL(name, return_type, ...) \
+  static return_type name(__VA_ARGS__);
+#include "malloc_decls.h"
+};
+
+/* The MozJemalloc allocator */
+struct MozJemallocBase {};
+typedef Allocator<MozJemallocBase> MozJemalloc;
