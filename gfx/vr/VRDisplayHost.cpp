094ea9d4e5d11fb8dd57f721d2720fdcf2c7486d
Kearwood "Kip" Gilbert <kgilbert@mozilla.com>
2017-08-31 16:29:14 -0700

    Bug 1394561 - Ensure WebVR content can catch up when IPC messages are delayed r=daoshengmu,kanru
    MozReview-Commit-ID: F4NKtyaNwEo

--- gfx/vr/VRDisplayHost.cpp
+++ gfx/vr/VRDisplayHost.cpp
@@ -162,6 +162,7 @@ VRDisplayHost::NotifyVSync()
 
 void
 VRDisplayHost::SubmitFrame(VRLayerParent* aLayer, PTextureParent* aTexture,
+                           uint64_t aFrameId,
                            const gfx::Rect& aLeftEyeRect,
                            const gfx::Rect& aRightEyeRect)
 {
@@ -173,7 +174,7 @@ VRDisplayHost::SubmitFrame(VRLayerParent* aLayer, PTextureParent* aTexture,
   }
 
   // Ensure that we only accept the first SubmitFrame call per RAF cycle.
-  if (!mFrameStarted) {
+  if (!mFrameStarted || aFrameId != mDisplayInfo.mFrameId) {
     return;
   }
   mFrameStarted = false;
