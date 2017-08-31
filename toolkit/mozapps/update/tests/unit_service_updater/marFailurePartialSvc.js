a2f10ba8bcf5435bb30831ad576b950bca6c4b2f
Robert Strong <robert.bugzilla@gmail.com>
2017-08-31 12:20:22 -0700

    test code - Bug 1367910 - Delete active-updates.xml instead of writing to it when there isn't an active update. r=dothayer

--- toolkit/mozapps/update/tests/unit_service_updater/marFailurePartialSvc.js
+++ toolkit/mozapps/update/tests/unit_service_updater/marFailurePartialSvc.js
@@ -32,14 +32,8 @@ function setupUpdaterTestFinished() {
 function runUpdateFinished() {
   checkAppBundleModTime();
   standardInit();
-  Assert.equal(readStatusFile(), STATE_NONE,
-               "the status file failure code" + MSG_SHOULD_EQUAL);
-  Assert.equal(gUpdateManager.updateCount, 1,
-               "the update manager updateCount attribute" + MSG_SHOULD_EQUAL);
-  Assert.equal(gUpdateManager.getUpdateAt(0).state, STATE_FAILED,
-               "the update state" + MSG_SHOULD_EQUAL);
-  Assert.equal(gUpdateManager.getUpdateAt(0).errorCode, LOADSOURCE_ERROR_WRONG_SIZE,
-               "the update errorCode" + MSG_SHOULD_EQUAL);
+  checkUpdateManager(STATE_NONE, false, STATE_FAILED,
+                     LOADSOURCE_ERROR_WRONG_SIZE, 1);
   checkPostUpdateRunningFile(false);
   checkFilesAfterUpdateFailure(getApplyDirFile);
   checkUpdateLogContents(LOG_PARTIAL_FAILURE);
