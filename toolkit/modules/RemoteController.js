8959fa76a07a9b14ca14c372c99fbe98703fa08d
Ehsan Akhgari <ehsan@mozilla.com>
2017-08-31 23:22:34 -0400

    Bug 1395795 - Convert RemoteController.jsm into a normal JS script in order to avoid paying the cost of wrapping the arrays passed in the arguments; r=Mossop

--- toolkit/modules/RemoteController.js
+++ toolkit/modules/RemoteController.js
@@ -3,8 +3,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-this.EXPORTED_SYMBOLS = ["RemoteController"];
-
 const Ci = Components.interfaces;
 const Cc = Components.classes;
 const Cu = Components.utils;
