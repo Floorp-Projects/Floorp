599064ea412e26456ed741ddcda131e5654f3f7a
jason laster <jlaster@mozilla.com>
2017-09-01 13:14:41 -0400

    Bug 1395396 - Update Debugger frontend (8/30/2017). r=jdescottes
    MozReview-Commit-ID: 9etCxtC3nLz

--- devtools/client/debugger/new/test/mochitest/browser_dbg-editor-highlight.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-editor-highlight.js
@@ -44,7 +44,7 @@ add_task(function*() {
   // Make sure the source is in the loading state, wait for it to be
   // fully loaded, and check the highlighted line.
   const simple1 = findSource(dbg, "simple1.js");
-  ok(getSource(getState(), simple1.id).get("loading"));
+  ok(getSource(getState(), simple1.id).get("loadedState"));
   yield waitForDispatch(dbg, "LOAD_SOURCE_TEXT");
   ok(getSource(getState(), simple1.id).get("text"));
   assertHighlightLocation(dbg, "simple1.js", 6);
