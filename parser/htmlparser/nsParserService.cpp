1e3e28cf8e2aa45a43c0884b31a6d9184cb4458c
Nicholas Nethercote <nnethercote@mozilla.com>
2017-09-01 19:42:42 +1000

    Bug 1395828 (part 2) - Rename nsHTMLTags methods. r=mrbkap.
    The patch uses the nsParserService method names (minus the "HTML" prefix)
    because they are more descriptive. This will make it easier to replace
    nsParserService method calls with nsHTMLTags method calls.
    
    The patch also adds nsHTMLTags::AtomTagToId(), to match up with
    nsParserService::HTMLAtomTagToId().
    
    --HG--
    extra : rebase_source : 278c53a0787a928de9477dd30a5fd037791d7990

--- parser/htmlparser/nsParserService.cpp
+++ parser/htmlparser/nsParserService.cpp
@@ -24,19 +24,19 @@ NS_IMPL_ISUPPORTS(nsParserService, nsIParserService)
 int32_t
 nsParserService::HTMLAtomTagToId(nsIAtom* aAtom) const
 {
-  return nsHTMLTags::LookupTag(nsDependentAtomString(aAtom));
+  return nsHTMLTags::StringTagToId(nsDependentAtomString(aAtom));
 }
 
 int32_t
 nsParserService::HTMLCaseSensitiveAtomTagToId(nsIAtom* aAtom) const
 {
-  return nsHTMLTags::CaseSensitiveLookupTag(aAtom);
+  return nsHTMLTags::CaseSensitiveAtomTagToId(aAtom);
 }
 
 int32_t
 nsParserService::HTMLStringTagToId(const nsAString& aTag) const
 {
-  return nsHTMLTags::LookupTag(aTag);
+  return nsHTMLTags::StringTagToId(aTag);
 }
 
 NS_IMETHODIMP
