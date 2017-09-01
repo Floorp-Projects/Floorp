b124cf4bf504b7b1486770c31ac2c2665e87f3b3
Kevin Hsieh <kevin.hsieh@ucla.edu>
2017-08-31 17:41:13 -0700

    Bug 1395777 (part 2) - Make orthogonal ruby annotations inter-character. r=xidorn
    MozReview-Commit-ID: JjpEnQI9Dmv
    
    --HG--
    extra : rebase_source : df628c84bd9c5d4b1eedca874c8e924786d6964d

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3091,6 +3091,9 @@ pref("layout.css.column-span.enabled", false);
 // Is effect of xml:base disabled for style attribute?
 pref("layout.css.style-attr-with-xml-base.disabled", true);
 
+// Are inter-character ruby annotations enabled?
+pref("layout.css.ruby.intercharacter.enabled", false);
+
 // pref for which side vertical scrollbars should be on
 // 0 = end-side in UI direction
 // 1 = end-side in document/content direction
