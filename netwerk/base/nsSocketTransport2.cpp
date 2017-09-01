2b8812b8c81eb72ee923bf20f0f4fda0aed80aa9
Honza Bambas <honzab.moz@firemni.cz>
2017-09-01 10:42:00 +0200

    Bug 910207 - Prevent client certificate pop-up coming from a speculative connection, r=dkeeler
    MozReview-Commit-ID: IHKzHwsJUiQ

--- netwerk/base/nsSocketTransport2.cpp
+++ netwerk/base/nsSocketTransport2.cpp
@@ -1263,8 +1263,12 @@ nsSocketTransport::BuildSocket(PRFileDesc *&fd, bool &proxyTransparent, bool &us
                 }
                 // don't call into PSM while holding mLock!!
                 nsCOMPtr<nsISSLSocketControl> secCtrl(do_QueryInterface(secinfo));
-                if (secCtrl)
+                if (secCtrl) {
+                    if (mConnectionFlags & nsISocketTransport::SPECULATIVE) {
+                        secCtrl->SetSpeculative(true);
+                    }
                     secCtrl->SetNotificationCallbacks(callbacks);
+                }
                 // remember if socket type is SSL so we can ProxyStartSSL if need be.
                 usingSSL = isSSL;
             }
