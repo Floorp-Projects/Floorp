298dd9aea9c50a953ba12fb7f1c7c585c441ba9e
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-01 14:07:47 +0200

    Backed out changeset 35afffcb4182 (bug 1374235) for build bustage because it landed before its servo commit. r=backout

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -185,6 +185,13 @@ private:
    */
   void StyleNewChildRange(nsIContent* aStartChild, nsIContent* aEndChild);
 
+  /**
+   * Calls StyleSubtreeForReconstruct on each child in the aStartChild/aEndChild
+   * range. Only used when we are styled by Servo.
+   */
+  void StyleChildRangeForReconstruct(nsIContent* aStartChild,
+                                     nsIContent* aEndChild);
+
 public:
   /**
    * Lazy frame construction is controlled by the aAllowLazyConstruction bool
