5abee4f830d01a9ce8550ffa5e4ae772f9657736
Jan Henning <jh+bugzilla@buttercookie.de>
2017-09-02 21:55:40 +0200

    Bug 1396292 - Part 2 - Explicitly run permissions callback on background thread where applicable. r=sebastian
    Currently, this only concerns setImageAs, where not running on the UI thread after a permissions prompt triggers a network-related strict mode violation and interestingly enough results in a crash (as opposed to a logcat complaint) on release builds.
    
    There's also a permissions check in the UpdateService which runs on a (but not *the*) background thread, but since we don't prompt in that case there's no action necessary.
    
    MozReview-Commit-ID: KKxW96AyDWH

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -909,6 +909,7 @@ public abstract class GeckoApp extends GeckoActivity
     private void setImageAs(final String aSrc) {
         Permissions
                 .from(this)
+                .onBackgroundThread()
                 .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 .andFallback(new Runnable() {
                     @Override
@@ -932,6 +933,9 @@ public abstract class GeckoApp extends GeckoActivity
      * @param aSrc The URI to download the image from.
      */
     private void downloadImageForSetImage(final String aSrc) {
+        // Network access from the main thread can cause a StrictMode crash on release builds.
+        ThreadUtils.assertOnBackgroundThread();
+
         boolean isDataURI = aSrc.startsWith("data:");
         Bitmap image = null;
         InputStream is = null;
