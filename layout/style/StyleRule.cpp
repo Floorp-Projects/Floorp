3b00935fe1f4b230b86ab3abe7588f4329c7394f
Xidorn Quan <me@upsuper.org>
2017-09-04 12:07:24 +1000

    Bug 1387905 part 2 - Use 10bit for specificity in Gecko as well. r=heycam
    MozReview-Commit-ID: G5qq0FY0HQ8

--- layout/style/StyleRule.cpp
+++ layout/style/StyleRule.cpp
@@ -504,14 +504,14 @@ int32_t nsCSSSelector::CalcWeightWithoutNegations() const
              "after them, specificity calculation must be updated");
 
   if (IsPseudoElement()) {
-    weight += 0x000001;
+    weight += 1;
   }
   if (nullptr != mCasedTag) {
-    weight += 0x000001;
+    weight += 1;
   }
   nsAtomList* list = mIDList;
   while (nullptr != list) {
-    weight += 0x010000;
+    weight += 1 << 20;
     list = list->mNext;
   }
   list = mClassList;
@@ -523,7 +523,7 @@ int32_t nsCSSSelector::CalcWeightWithoutNegations() const
   }
 #endif
   while (nullptr != list) {
-    weight += 0x000100;
+    weight += 1 << 10;
     list = list->mNext;
   }
   // FIXME (bug 561154):  This is incorrect for :-moz-any(), which isn't
@@ -533,12 +533,12 @@ int32_t nsCSSSelector::CalcWeightWithoutNegations() const
   // highest-specificity options first).
   nsPseudoClassList *plist = mPseudoClassList;
   while (nullptr != plist) {
-    weight += 0x000100;
+    weight += 1 << 10;
     plist = plist->mNext;
   }
   nsAttrSelector* attr = mAttrList;
   while (nullptr != attr) {
-    weight += 0x000100;
+    weight += 1 << 10;
     attr = attr->mNext;
   }
   return weight;
