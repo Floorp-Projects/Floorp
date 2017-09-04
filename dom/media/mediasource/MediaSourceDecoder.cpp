d126636ae8b885d8d6accafdc2259d5166946df8
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-09-04 22:47:36 +0200

    Bug 1396629 - P5. Show container type in debug info. r=cpearce
    MozReview-Commit-ID: 3olXuGeqqoS

--- dom/media/mediasource/MediaSourceDecoder.cpp
+++ dom/media/mediasource/MediaSourceDecoder.cpp
@@ -255,6 +255,7 @@ MediaSourceDecoder::SetMediaSourceDuration(double aDuration)
 void
 MediaSourceDecoder::GetMozDebugReaderData(nsACString& aString)
 {
+  aString += NS_LITERAL_CSTRING("Container Type: MediaSource\n");
   if (mReader && mDemuxer) {
     mReader->GetMozDebugReaderData(aString);
     mDemuxer->GetMozDebugReaderData(aString);
