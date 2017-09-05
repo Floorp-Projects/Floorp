650476791071e46a472d4c5181bcd266c77fe333
Tobias Schneider <schneider@jancona.com>
2017-09-05 09:32:35 -0700

    Bug 1394522 - Null check ref pointers when iterating over intersection observers. r=smaug, a=gchang

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -12850,9 +12850,15 @@ nsDocument::UpdateIntersectionObservations()
       time = perf->Now();
     }
   }
+  nsTArray<RefPtr<DOMIntersectionObserver>> observers(mIntersectionObservers.Count());
   for (auto iter = mIntersectionObservers.Iter(); !iter.Done(); iter.Next()) {
     DOMIntersectionObserver* observer = iter.Get()->GetKey();
-    observer->Update(this, time);
+      observers.AppendElement(observer);
+  }
+  for (const auto& observer : observers) {
+    if (observer) {
+      observer->Update(this, time);
+    }
   }
 }
 
@@ -12879,7 +12885,9 @@ nsDocument::NotifyIntersectionObservers()
     observers.AppendElement(observer);
   }
   for (const auto& observer : observers) {
-    observer->Notify();
+    if (observer) {
+      observer->Notify();
+    }
   }
 }
 
