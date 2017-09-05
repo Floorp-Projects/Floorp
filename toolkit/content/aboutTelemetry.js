37ab9d8a49bd35f6fd7a54ebda5d288f1eba21bc
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-05 22:09:17 +0200

    Backed out changeset 78cce071b69e (bug 1396211) for eslint failure in toolkit/content/aboutTelemetry.js: Strings must use doublequote. r=backout

--- toolkit/content/aboutTelemetry.js
+++ toolkit/content/aboutTelemetry.js
@@ -166,6 +166,49 @@ function removeAllChildNodes(node) {
   }
 }
 
+/**
+ * Pad a number to two digits with leading "0".
+ */
+function padToTwoDigits(n) {
+  return String(n).padStart(2, "0");
+}
+
+/**
+ * Return yesterdays date with the same time.
+ */
+function yesterday(date) {
+  let d = new Date(date);
+  d.setDate(d.getDate() - 1);
+  return d;
+}
+
+/**
+ * Return tomorrow's date with the same time.
+ */
+function tomorrow(date) {
+  let d = new Date(date);
+  d.setDate(d.getDate() + 1);
+  return d;
+}
+
+/**
+ * This returns a short date string of the form YYYY/MM/DD.
+ */
+function shortDateString(date) {
+  return date.getFullYear()
+         + "/" + padToTwoDigits(date.getMonth() + 1)
+         + "/" + padToTwoDigits(date.getDate());
+}
+
+/**
+ * This returns a short time string of the form hh:mm:ss.
+ */
+function shortTimeString(date) {
+  return padToTwoDigits(date.getHours())
+         + ":" + padToTwoDigits(date.getMinutes())
+         + ":" + padToTwoDigits(date.getSeconds());
+}
+
 var Settings = {
   SETTINGS: [
     // data upload
@@ -412,32 +455,24 @@ var PingPicker = {
 
     let pingTypes = new Set();
     pingTypes.add(this.TYPE_ALL);
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    const yesterday = new Date(today);
-    yesterday.setDate(today.getDate() - 1);
-
+    let todayString =  (new Date()).toDateString();
+    let yesterdayString = yesterday(new Date()).toDateString();
     for (let p of this._archivedPings) {
       pingTypes.add(p.type);
-      const pingDate = new Date(p.timestampCreated);
-      const datetimeText = Services.intl.createDateTimeFormat(undefined, {
-          dateStyle: 'short',
-          timeStyle: 'medium'
-        }).format(pingDate);
-      const pingName = `${datetimeText}, ${p.type}`;
+      let date = new Date(p.timestampCreated);
+      let datetext = date.toLocaleDateString() + " " + shortTimeString(date);
+      let text = datetext + ", " + p.type;
 
       let option = document.createElement("option");
-      let content = document.createTextNode(pingName);
+      let content = document.createTextNode(text);
       option.appendChild(content);
       option.setAttribute("value", p.id);
       option.dataset.type = p.type;
-      option.dataset.date = datetimeText;
+      option.dataset.date = datetext;
 
-      pingDate.setHours(0, 0, 0, 0);
-      if (pingDate.getTime() === today.getTime()) {
+      if (date.toDateString() == todayString) {
         pingSelector.children[0].appendChild(option);
-      } else if (pingDate.getTime() === yesterday.getTime()) {
+      } else if (date.toDateString() == yesterdayString) {
         pingSelector.children[1].appendChild(option);
       } else {
         pingSelector.children[2].appendChild(option);
