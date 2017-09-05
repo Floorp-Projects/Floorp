705387ed4f5c32d6ff06c11b847352ae6ce1493d
Kris Maglione <maglione.k@gmail.com>
2017-09-04 17:21:06 -0700

    Bug 1369533: Return dead wrappers rather than null for dead CallbackObject values. r=bz
    We don't have access to an appropriate context to create the dead wrapper in
    when the callback is nuked, so instead, this patch creates a new dead wrapper
    in the caller compartment each time the property is accessed. This is the same
    behavior we'd get when trying to re-wrap a cross-compartment dead wrapper, so
    it's consistent with the way we handle these situations elsewhere.
    
    MozReview-Commit-ID: 3cMeR4z8EOe

--- dom/base/CustomElementRegistry.cpp
+++ dom/base/CustomElementRegistry.cpp
@@ -593,6 +593,9 @@ CustomElementRegistry::Define(const nsAString& aName,
   }
 
   JSContext *cx = jsapi.cx();
+  // Note: No calls that might run JS or trigger CC before this point, or
+  // there's a (vanishingly small) chance of our constructor being nulled
+  // before we access it.
   JS::Rooted<JSObject*> constructor(cx, aFunctionConstructor.CallableOrNull());
 
   /**
@@ -878,7 +881,7 @@ CustomElementRegistry::Get(JSContext* aCx, const nsAString& aName,
     return;
   }
 
-  aRetVal.setObjectOrNull(data->mConstructor->CallableOrNull());
+  aRetVal.setObject(*data->mConstructor->Callback(aCx));
 }
 
 already_AddRefed<Promise>
--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -6912,7 +6912,7 @@ def getWrapTemplateForType(type, descriptorProvider, result, successCode,
         # Callbacks can store null if we nuked the compartments their
         # objects lived in.
         wrapCode = setObjectOrNull(
-            "GetCallbackFromCallbackObject(%(result)s)",
+            "GetCallbackFromCallbackObject(cx, %(result)s)",
             wrapAsType=type)
         if type.nullable():
             wrapCode = (
