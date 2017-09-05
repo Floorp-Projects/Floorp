068a8234ceeab93c3825ccee3ea122369f8920ae
Andrew Osmond <aosmond@mozilla.com>
2017-09-05 07:58:44 -0400

    Bug 1370412 - Part 1. Add preference to control "factor of 2" mode for ImageSurfaceCache. r=tnikkel

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4641,6 +4641,13 @@ pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,1,1.1,1.2,1.33,1.5,1.7,2
 // Image-related prefs
 //
 
+// Maximum number of surfaces for an image before entering "factor of 2" mode.
+// This in addition to the number of "native" sizes of an image. A native size
+// is a size for which we can decode a frame without up or downscaling. Most
+// images only have 1, but some (i.e. ICOs) may have multiple frames for the
+// same data at different sizes.
+pref("image.cache.factor2.threshold-surfaces", 4);
+
 // The maximum size, in bytes, of the decoded images we cache
 pref("image.cache.size", 5242880);
 
