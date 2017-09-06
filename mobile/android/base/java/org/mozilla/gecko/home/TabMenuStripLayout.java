e76993632e2f5f9ba5e89d13f768b284cc923a5c
Jing-wei Wu <topwu.tw@gmail.com>
2017-09-06 10:15:14 +0800

    Bug 1394378 - Wrap drawable to support tinting across different API levels. r=nechen
    According to the comment of `DrawableCompat.wrap(Drawable)`[1], a drawable should be wrapped
    to support tinting across the different API levels.
    
    [1] https://android.googlesource.com/platform/frameworks/support/+/android-6.0.1_r67/v4/java/android/support/v4/graphics/drawable/DrawableCompat.java#329
    
    MozReview-Commit-ID: JJ4alkFF1og
    
    --HG--
    extra : rebase_source : 075164b921ee9b7a2ab0261251688dc4e3950b29

--- mobile/android/base/java/org/mozilla/gecko/home/TabMenuStripLayout.java
+++ mobile/android/base/java/org/mozilla/gecko/home/TabMenuStripLayout.java
@@ -70,7 +70,7 @@ class TabMenuStripLayout extends ThemedLinearLayout
         a.recycle();
 
         if (stripResId != -1) {
-            strip = getResources().getDrawable(stripResId);
+            strip = ResourcesCompat.getDrawable(getResources(), stripResId, null);
 
             if (stripColor != null) {
                 final int backgroundTintColor = stripColor.getColorForState(getDrawableState(), Color.TRANSPARENT);
@@ -297,6 +297,7 @@ class TabMenuStripLayout extends ThemedLinearLayout
         super.setPrivateMode(isPrivate);
 
         if (modeChanged && stripColor != null) {
+            strip = DrawableCompat.wrap(strip);
             final int backgroundTintColor = stripColor.getColorForState(getDrawableState(), Color.TRANSPARENT);
             DrawableCompat.setTint(strip, backgroundTintColor);
         }
