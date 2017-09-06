aaa621f85804d0ab083f00a89df2f774d7086985
Eden Chuang <echuang@mozilla.com>
2017-09-06 14:36:24 +0800

    Bug 1388661 - Support currency validation in PaymentRequet API. r=baku
        This patch implements currency validation algorithm according to the spec
        https://w3c.github.io/payment-request/#validity-checkers.
    
        1. amount.currencySystem must be "urn:iso:std:iso:4217".
        2. amount.currency is valid with following criteria
           1. The currency length must be 3.
           2. The currency contains any character that must be in the range "A" to
              "Z"(U+0041 to U+005A) or the range "a" to "z"(U+0061 to U+007A).
    
        According to the spec, converting the currency to upper case and save it in
        nsIPaymentRequest.

--- dom/payments/PaymentRequestManager.h
+++ dom/payments/PaymentRequestManager.h
@@ -41,7 +41,8 @@ public:
    *  process can ask specific task by sending requestId only.
    */
   nsresult
-  CreatePayment(nsPIDOMWindowInner* aWindow,
+  CreatePayment(JSContext* aCx,
+                nsPIDOMWindowInner* aWindow,
                 nsIPrincipal* aTopLevelPrincipal,
                 const Sequence<PaymentMethodData>& aMethodData,
                 const PaymentDetailsInit& aDetails,
@@ -53,7 +54,8 @@ public:
   nsresult AbortPayment(const nsAString& aRequestId);
   nsresult CompletePayment(const nsAString& aRequestId,
                            const PaymentComplete& aComplete);
-  nsresult UpdatePayment(const nsAString& aRequestId,
+  nsresult UpdatePayment(JSContext* aCx,
+                         const nsAString& aRequestId,
                          const PaymentDetailsUpdate& aDetails);
 
   nsresult RespondPayment(const IPCPaymentActionResponse& aResponse);
