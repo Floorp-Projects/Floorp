8bc151b3cf64e8226df1b2ba52a0eba870837be7
Nathan Froyd <froydnj@mozilla.com>
2017-09-06 08:57:09 -0400

    Bug 1396864 - eliminate -Wunused-private-field warning in WebrcMediaCodecVideoCodec; r=jesup
    The mTimestamp field is unused.

--- media/webrtc/signaling/src/media-conduit/WebrtcMediaCodecVP8VideoCodec.cpp
+++ media/webrtc/signaling/src/media-conduit/WebrtcMediaCodecVP8VideoCodec.cpp
@@ -696,8 +696,7 @@ static bool I420toNV12(uint8_t* dstY, uint16_t* dstUV, const webrtc::VideoFrame&
 
 // Encoder.
 WebrtcMediaCodecVP8VideoEncoder::WebrtcMediaCodecVP8VideoEncoder()
-  : mTimestamp(0)
-  , mCallback(nullptr)
+  : mCallback(nullptr)
   , mMediaCodecEncoder(nullptr) {
   CSFLogDebug(logTag,  "%s ", __FUNCTION__);
 
