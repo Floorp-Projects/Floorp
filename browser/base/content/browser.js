92f229c5be6474a2dff0bb5bfbb43cd8ddc32f54
Michael Kaply <mozilla@kaply.com>
2017-09-06 15:11:25 -0500

    Bug 1378461 - Report other search engine names. r=mak, data-r=rweiss

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3964,8 +3964,7 @@ const BrowserSearch = {
       return engine.identifier;
     }
 
-    if (!engine || (engine.name === undefined) ||
-        !Services.prefs.getBoolPref("toolkit.telemetry.enabled"))
+    if (!engine || (engine.name === undefined))
       return "other";
 
     return "other-" + engine.name;
