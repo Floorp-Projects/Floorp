635b1152237fdedc3f801101ba17377744e44fe4
Emilio Cobos Álvarez <emilio@crisal.io>
2017-09-07 17:02:20 +0200

    Bug 1397091: Merge InsertionKind and LazyConstructionAllowed. r=bz
    They now map 1-to-1, so there's no reason to keep them separate.
    
    MozReview-Commit-ID: A2Wqa3QNdw8

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -1815,7 +1815,7 @@ PresShell::Initialize(nscoord aWidth, nscoord aHeight)
       // Have the style sheet processor construct frame for the root
       // content object down
       mFrameConstructor->ContentInserted(
-          nullptr, root, nullptr, nsCSSFrameConstructor::LazyConstructionAllowed::No);
+          nullptr, root, nullptr, nsCSSFrameConstructor::InsertionKind::Sync);
       VERIFY_STYLE_TREE;
 
       // Something in mFrameConstructor->ContentInserted may have caused
@@ -4429,7 +4429,7 @@ PresShell::ContentAppended(nsIDocument *aDocument,
   mFrameConstructor->ContentAppended(
       aContainer,
       aFirstNewContent,
-      nsCSSFrameConstructor::LazyConstructionAllowed::Yes);
+      nsCSSFrameConstructor::InsertionKind::Async);
 
   VERIFY_STYLE_TREE;
 }
@@ -4459,7 +4459,7 @@ PresShell::ContentInserted(nsIDocument* aDocument,
       aMaybeContainer,
       aChild,
       nullptr,
-      nsCSSFrameConstructor::LazyConstructionAllowed::Yes);
+      nsCSSFrameConstructor::InsertionKind::Async);
 
   if (aChild->NodeType() == nsIDOMNode::DOCUMENT_TYPE_NODE) {
     MOZ_ASSERT(container == aDocument);
