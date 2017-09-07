a48c5f0818d613317831acdf1774d1c3c458aeed
Kevin Chen <kechen@mozilla.com>
2017-09-07 15:04:30 +0800

    Bug 1393376 - (Part 2) Retain BasicLayerManager in WebRenderFallbackData for layer invalidation computation; r=jrmuizel
    MozReview-Commit-ID: 6rmJkXzLubX

--- gfx/layers/wr/WebRenderUserData.h
+++ gfx/layers/wr/WebRenderUserData.h
@@ -6,6 +6,7 @@
 #ifndef GFX_WEBRENDERUSERDATA_H
 #define GFX_WEBRENDERUSERDATA_H
 
+#include "BasicLayers.h"                // for BasicLayerManager
 #include "mozilla/layers/StackingContextHelper.h"
 #include "mozilla/webrender/WebRenderAPI.h"
 #include "mozilla/layers/AnimationInfo.h"
@@ -127,6 +128,7 @@ public:
   void SetInvalid(bool aInvalid) { mInvalid = aInvalid; }
   bool IsInvalid() { return mInvalid; }
 
+  RefPtr<BasicLayerManager> mBasicLayerManager;
 protected:
   nsAutoPtr<nsDisplayItemGeometry> mGeometry;
   nsRect mBounds;
