5f9addc6ce92010468b03c33985879c59d8f82f0
Sebastian Kaspari <s.kaspari@gmail.com>
2017-09-07 12:26:34 +0200

    [focus] Rename default browser setting. (https://github.com/mozilla-mobile/focus-android/issues/1053)

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/widget/DefaultBrowserPreference.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/widget/DefaultBrowserPreference.java
@@ -1,5 +1,4 @@
-/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
@@ -21,27 +20,36 @@ import org.mozilla.focus.activity.InfoActivity;
 import org.mozilla.focus.utils.Browsers;
 import org.mozilla.focus.utils.SupportUtils;
 
-@TargetApi(Build.VERSION_CODES.N)
 public class DefaultBrowserPreference extends Preference {
     private Switch switchView;
 
     @SuppressWarnings("unused") // Instantiated from XML
     public DefaultBrowserPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setWidgetLayoutResource(R.layout.preference_default_browser);
+        init();
     }
 
     @SuppressWarnings("unused") // Instantiated from XML
     public DefaultBrowserPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
+    }
+
+    private void init() {
         setWidgetLayoutResource(R.layout.preference_default_browser);
+
+        final String appName = getContext().getResources().getString(R.string.app_name);
+        final String title = getContext().getResources().getString(R.string.preference_default_browser2, appName);
+
+        setTitle(title);
     }
 
     @Override
     protected void onBindView(View view) {
         super.onBindView(view);
 
-        switchView = (Switch) view.findViewById(R.id.switch_widget);
+        switchView = view.findViewById(R.id.switch_widget);
+
         update();
     }
 
@@ -63,6 +71,7 @@ public class DefaultBrowserPreference extends Preference {
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.N)
     private void openDefaultAppsSettings(Context context) {
         try {
             Intent intent = new Intent(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS);
