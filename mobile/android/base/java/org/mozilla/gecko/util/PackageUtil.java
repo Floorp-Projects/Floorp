d8532152a3fb32135672498b1e5c2c5e1036cdf6
Julian_Chu <walkingice0204@gmail.com>
2017-09-09 00:47:44 +0800

    Bug 1355735 - Add util to pick name for default browser r=jwu
    The util PackageUtil helps to get ResolveInfo of default browser, then
    we can use it in CustomTabs menu. If user hasn't set any default
    browser, instead we display "..." for browser name.
    
    MozReview-Commit-ID: 6DkFkZ8Ovzq

---
+++ mobile/android/base/java/org/mozilla/gecko/util/PackageUtil.java
@@ -0,0 +1,67 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+public class PackageUtil {
+
+    private static final Uri TEST_URI = Uri.parse("https://www.mozilla.org");
+
+    private PackageUtil() {
+    }
+
+    /**
+     * Get information of user's default browser
+     *
+     * @param context
+     * @return information of default browser, null if user hasn't set any default value.
+     */
+    public static ResolveInfo getDefaultBrowser(@NonNull Context context) {
+        final Intent browserIntent = new Intent(Intent.ACTION_VIEW, TEST_URI);
+        final int additionalFlags = 0; // no additional flags
+
+        // this info might be activity picker
+        final ResolveInfo resolveInfo = context.getPackageManager()
+                .resolveActivity(browserIntent, additionalFlags);
+
+        if (resolveInfo == null) {
+            return null;
+        }
+
+        if (!resolveInfo.activityInfo.exported) {
+            return null;
+        }
+
+        final List<ResolveInfo> browsers = resolveBrowsers(context);
+        for (ResolveInfo it : browsers) {
+            if (resolveInfo.activityInfo.packageName.equals(it.activityInfo.packageName)) {
+                return resolveInfo;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Information of activities which could handle a web-page-browsing intent.
+     *
+     * @param context
+     * @return a List of activity's information. If default browser set, the list might only has one item.
+     */
+    private static List<ResolveInfo> resolveBrowsers(@NonNull Context context) {
+        final Intent browserIntent = new Intent(Intent.ACTION_VIEW, TEST_URI);
+        final int additionalFlags = 0; // no additional flags
+
+        return context.getPackageManager().queryIntentActivities(browserIntent, additionalFlags);
+    }
+}
