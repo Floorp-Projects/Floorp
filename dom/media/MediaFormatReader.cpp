c59188889b2b5178a4e8175c7545cd2e323cfe2a
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-09-08 15:45:38 +0200

    Bug 1397307 - P2. Wrap boolean in structure to prevent unwanted conversion. r=gerald
    Automatic conversion (say from int to bool) makes DecoderParam difficult to extend.
    
    MozReview-Commit-ID: G0T7jPogskN
    
    --HG--
    extra : rebase_source : 59437fd2b430ccd6be50b18c98b5a5c4ed2c8240

--- dom/media/MediaFormatReader.cpp
+++ dom/media/MediaFormatReader.cpp
@@ -736,7 +736,7 @@ MediaFormatReader::DecoderFactory::DoCreateDecoder(Data& aData)
         : *ownerData.mOriginalInfo->GetAsAudioInfo(),
         ownerData.mTaskQueue,
         mOwner->mCrashHelper,
-        ownerData.mIsNullDecode,
+        CreateDecoderParams::UseNullDecoder(ownerData.mIsNullDecode),
         &result,
         TrackInfo::kAudioTrack,
         &mOwner->OnTrackWaitingForKeyProducer()
@@ -755,7 +755,7 @@ MediaFormatReader::DecoderFactory::DoCreateDecoder(Data& aData)
         mOwner->mKnowsCompositor,
         mOwner->GetImageContainer(),
         mOwner->mCrashHelper,
-        ownerData.mIsNullDecode,
+        CreateDecoderParams::UseNullDecoder(ownerData.mIsNullDecode),
         &result,
         TrackType::kVideoTrack,
         &mOwner->OnTrackWaitingForKeyProducer()
