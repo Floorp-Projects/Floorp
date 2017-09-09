b6e67380d1c9d9b4d58b0cdd19ec64fcfdb09104
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-09 22:36:55 +0200

    Backed out changeset fdb8456c34de (bug 864845) for T-e10s(s) for tscrollx failure. r=backout

--- devtools/client/inspector/rules/test/head.js
+++ devtools/client/inspector/rules/test/head.js
@@ -57,8 +57,8 @@ addTab = function (url) {
  */
 function getStyle(testActor, selector, propName) {
   return testActor.eval(`
-    document.querySelector("${selector}")
-            .style.getPropertyValue("${propName}");
+    content.document.querySelector("${selector}")
+                    .style.getPropertyValue("${propName}");
   `);
 }
 
