7191827641a646775c8c3c4bcaeabf835feb3012
Kris Maglione <maglione.k@gmail.com>
2017-09-08 18:17:09 -0700

    Bug 1371065: Part 2 - rm -rf addon-sdk r=Mossop
    MozReview-Commit-ID: CZYhLIUsQEO
    
    --HG--
    extra : rebase_source : fd6af199323b3cf502de0ef157cd3f3dcf08a232

--- addon-sdk/source/python-lib/cuddlefish/tests/linker-files/one/lib/two.js
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-
-// this ought to find our sibling, not packages/development-mode/lib/main.js
-var main = require("main");
-exports.foo = 1;
