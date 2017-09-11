226d639299dc425eccc88997574d7cb2f428838d
Ryan VanderMeulen <ryanvm@gmail.com>
2017-09-11 12:31:02 -0400

    Bug 1377618 - Update Firefox 56 to NSS_3_32_1_RTM. r=kaie, a=gchang UPGRADE_NSS_RELEASE
    --HG--
    extra : histedit_source : f184a3bc50ca4322df61debce439024ff338b0df

--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -21,10 +21,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION "3.32" SOFTOKEN_ECC_STRING
+#define SOFTOKEN_VERSION "3.32.1" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR 3
 #define SOFTOKEN_VMINOR 32
-#define SOFTOKEN_VPATCH 0
+#define SOFTOKEN_VPATCH 1
 #define SOFTOKEN_VBUILD 0
 #define SOFTOKEN_BETA PR_FALSE
 
