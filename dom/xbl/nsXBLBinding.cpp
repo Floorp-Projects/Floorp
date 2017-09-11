82a163baff418a4ab28f619d3d068225912bbaca
Emilio Cobos Álvarez <emilio@crisal.io>
2017-09-11 13:11:01 +0200

    Bug 1397983: Rename nsXBLBinding::{Install,Uninstall}AnonymousContent to {Bind,Unbind}AnonymousContent. r=smaug
    MozReview-Commit-ID: LZCjwiNtMYZ
    
    --HG--
    extra : rebase_source : 2e5f2d65f876d34c8ad97af6530b79e82cf94a30

--- dom/xbl/nsXBLBinding.cpp
+++ dom/xbl/nsXBLBinding.cpp
@@ -135,7 +135,7 @@ nsXBLBinding::~nsXBLBinding(void)
     // It is unnecessary to uninstall anonymous content in a shadow tree
     // because the ShadowRoot itself is a DocumentFragment and does not
     // need any additional cleanup.
-    nsXBLBinding::UninstallAnonymousContent(mContent->OwnerDoc(), mContent);
+    nsXBLBinding::UnbindAnonymousContent(mContent->OwnerDoc(), mContent);
   }
   nsXBLDocumentInfo* info = mPrototypeBinding->XBLDocumentInfo();
   NS_RELEASE(info);
@@ -147,8 +147,8 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsXBLBinding)
   // XXX Probably can't unlink mPrototypeBinding->XBLDocumentInfo(), because
   //     mPrototypeBinding is weak.
   if (tmp->mContent && !tmp->mIsShadowRootBinding) {
-    nsXBLBinding::UninstallAnonymousContent(tmp->mContent->OwnerDoc(),
-                                            tmp->mContent);
+    nsXBLBinding::UnbindAnonymousContent(tmp->mContent->OwnerDoc(),
+                                         tmp->mContent);
   }
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mContent)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mNextBinding)
@@ -191,8 +191,9 @@ nsXBLBinding::GetBindingWithContent()
 }
 
 void
-nsXBLBinding::InstallAnonymousContent(nsIContent* aAnonParent, nsIContent* aElement,
-                                      bool aChromeOnlyContent)
+nsXBLBinding::BindAnonymousContent(nsIContent* aAnonParent,
+                                   nsIContent* aElement,
+                                   bool aChromeOnlyContent)
 {
   // We need to ensure two things.
   // (1) The anonymous content should be fooled into thinking it's in the bound
@@ -237,8 +238,8 @@ nsXBLBinding::InstallAnonymousContent(nsIContent* aAnonParent, nsIContent* aElem
 }
 
 void
-nsXBLBinding::UninstallAnonymousContent(nsIDocument* aDocument,
-                                        nsIContent* aAnonParent)
+nsXBLBinding::UnbindAnonymousContent(nsIDocument* aDocument,
+                                     nsIContent* aAnonParent)
 {
   nsAutoScriptBlocker scriptBlocker;
   // Hold a strong ref while doing this, just in case.
@@ -340,8 +341,8 @@ nsXBLBinding::GenerateAnonymousContent()
 
     // Do this after looking for <children> as this messes up the parent
     // pointer which would make the GetNextNode call above fail
-    InstallAnonymousContent(mContent, mBoundElement,
-                            mPrototypeBinding->ChromeOnlyContent());
+    BindAnonymousContent(mContent, mBoundElement,
+                         mPrototypeBinding->ChromeOnlyContent());
 
     // Insert explicit children into insertion points
     if (mDefaultInsertionPoint && mInsertionPoints.IsEmpty()) {
@@ -368,8 +369,8 @@ nsXBLBinding::GenerateAnonymousContent()
             // the bound element didn't match any of the <children> in the
             // binding. This became a pseudo-API that we have to maintain.
 
-            // Undo InstallAnonymousContent
-            UninstallAnonymousContent(doc, mContent);
+            // Undo BindAnonymousContent
+            UnbindAnonymousContent(doc, mContent);
 
             // Clear out our children elements to avoid dangling references.
             ClearInsertionPoints();
@@ -822,7 +823,7 @@ nsXBLBinding::ChangeDocument(nsIDocument* aOldDocument, nsIDocument* aNewDocumen
     // Update the anonymous content.
     // XXXbz why not only for style bindings?
     if (mContent && !mIsShadowRootBinding) {
-      nsXBLBinding::UninstallAnonymousContent(aOldDocument, mContent);
+      nsXBLBinding::UnbindAnonymousContent(aOldDocument, mContent);
     }
 
     // Now that we've unbound our anonymous content from the tree and updated
--- dom/xbl/nsXBLBinding.h
+++ dom/xbl/nsXBLBinding.h
@@ -105,10 +105,10 @@ public:
   bool ImplementsInterface(REFNSIID aIID) const;
 
   void GenerateAnonymousContent();
-  void InstallAnonymousContent(nsIContent* aAnonParent, nsIContent* aElement,
-                               bool aNativeAnon);
-  static void UninstallAnonymousContent(nsIDocument* aDocument,
-                                        nsIContent* aAnonParent);
+  void BindAnonymousContent(nsIContent* aAnonParent, nsIContent* aElement,
+                            bool aNativeAnon);
+  static void UnbindAnonymousContent(nsIDocument* aDocument,
+                                     nsIContent* aAnonParent);
   void InstallEventHandlers();
   nsresult InstallImplementation();
 
