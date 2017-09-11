7ddca337a72262aba380437c3c62ffe3a8b90b0c
J.C. Jones <jjones@mozilla.com>
2017-09-11 12:56:59 -0700

    Bug 1245527 - Rewrite U2F.cpp to use U2FTokenManager. r=keeler, r=ttaubert
    - This patch reworks the U2F module to asynchronously call U2FManager,
      which in turn handles constructing and managing the U2FTokenManager
      via IPC.
    - Add U2FTransaction{Parent,Child} implementations to mirror similar ones for
      WebAuthn
    - Rewrite all tests to compensate for U2F executing asynchronously now.
      - Used async tasks, used the manifest parameters for scheme, and generally
        made these cleaner.
    - The mochitest "pref =" functionality from Bug 1328830 doesn't support Android
      yet, causing breakage on Android. Rework the tests to go back to the old way
      of using iframes to test U2F.
    
    
    NOTE TO REVIEWERS:
     Since this is huge, I recommend the following:
    
     keeler - please review U2F.cpp/h, the tests, and the security-prefs.js. Most
              of the U2F logic is still in U2F.cpp like before, but there's been
              some reworking of how it is called.
    
     ttaubert - please review U2FManager, the Transaction classes, build changes,
                and the changes to nsGlobalWindow. All of these should be very
                similar to the WebAuthn code it's patterned off.
    
    
    MozReview-Commit-ID: C1ZN2ch66Rm

--- dom/u2f/U2F.h
+++ dom/u2f/U2F.h
@@ -10,20 +10,13 @@
 #include "js/TypeDecls.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/dom/BindingDeclarations.h"
-#include "mozilla/dom/CryptoBuffer.h"
 #include "mozilla/dom/Nullable.h"
 #include "mozilla/dom/U2FBinding.h"
 #include "mozilla/ErrorResult.h"
 #include "mozilla/MozPromise.h"
-#include "mozilla/ReentrantMonitor.h"
-#include "mozilla/SharedThreadPool.h"
-#include "nsCycleCollectionParticipant.h"
-#include "nsIU2FToken.h"
-#include "nsNSSShutDown.h"
 #include "nsPIDOMWindow.h"
 #include "nsProxyRelease.h"
 #include "nsWrapperCache.h"
-
 #include "U2FAuthenticator.h"
 
 class nsISerialEventTarget;
@@ -38,251 +31,15 @@ class U2FSignCallback;
 struct RegisterRequest;
 struct RegisteredKey;
 
-// These structs are analogs to the WebIDL versions, but can be used on worker
-// threads which lack a JSContext.
-struct LocalRegisterRequest
-{
-  nsString mChallenge;
-  nsString mVersion;
-  CryptoBuffer mClientData;
-};
-
-struct LocalRegisteredKey
-{
-  nsString mKeyHandle;
-  nsString mVersion;
-  Nullable<nsString> mAppId;
-  // TODO: Support transport preferences
-  // Nullable<nsTArray<Transport>> mTransports;
-};
-
-typedef MozPromise<nsString, ErrorCode, false> U2FPromise;
-typedef MozPromise<Authenticator, ErrorCode, false> U2FPrepPromise;
-
-// U2FPrepTasks return lists of Authenticators that are OK to
-// proceed; they're useful for culling incompatible Authenticators.
-// Currently, only IsRegistered is supported.
-class U2FPrepTask : public Runnable
-{
-public:
-  explicit U2FPrepTask(const Authenticator& aAuthenticator,
-                       nsISerialEventTarget* aEventTarget);
-
-  RefPtr<U2FPrepPromise> Execute();
-
-protected:
-  virtual ~U2FPrepTask();
-
-  Authenticator mAuthenticator;
-  MozPromiseHolder<U2FPrepPromise> mPromise;
-  const nsCOMPtr<nsISerialEventTarget> mEventTarget;
-};
-
-// Determine whether the provided Authenticator already knows
-// of the provided Registered Key.
-class U2FIsRegisteredTask final : public U2FPrepTask
-{
-public:
-  U2FIsRegisteredTask(const Authenticator& aAuthenticator,
-                      const LocalRegisteredKey& aRegisteredKey,
-                      const CryptoBuffer& aAppParam,
-                      nsISerialEventTarget* aEventTarget);
-
-  NS_DECL_NSIRUNNABLE
-private:
-  ~U2FIsRegisteredTask();
-
-  LocalRegisteredKey mRegisteredKey;
-  CryptoBuffer mAppParam;
-};
-
-class U2FTask : public Runnable
-{
-public:
-  U2FTask(const nsAString& aOrigin,
-          const nsAString& aAppId,
-          const Authenticator& aAuthenticator,
-          nsISerialEventTarget* aEventTarget);
-
-  RefPtr<U2FPromise> Execute();
-
-  nsString mOrigin;
-  nsString mAppId;
-  Authenticator mAuthenticator;
-  const nsCOMPtr<nsISerialEventTarget> mEventTarget;
-
-protected:
-  virtual ~U2FTask();
-
-  MozPromiseHolder<U2FPromise> mPromise;
-};
-
-// Use the provided Authenticator to Register a new scoped credential
-// for the provided application.
-class U2FRegisterTask final : public U2FTask
-{
-public:
-  U2FRegisterTask(const nsAString& aOrigin,
-                  const nsAString& aAppId,
-                  const Authenticator& aAuthenticator,
-                  const CryptoBuffer& aAppParam,
-                  const CryptoBuffer& aChallengeParam,
-                  const LocalRegisterRequest& aRegisterEntry,
-                  nsISerialEventTarget* aEventTarget);
-
-  NS_DECL_NSIRUNNABLE
-private:
-  ~U2FRegisterTask();
-
-  CryptoBuffer mAppParam;
-  CryptoBuffer mChallengeParam;
-  LocalRegisterRequest mRegisterEntry;
-};
-
-// Generate an assertion using the provided Authenticator for the given origin
-// and provided application to attest to ownership of a valid scoped credential.
-class U2FSignTask final : public U2FTask
-{
-public:
-  U2FSignTask(const nsAString& aOrigin,
-              const nsAString& aAppId,
-              const nsAString& aVersion,
-              const Authenticator& aAuthenticator,
-              const CryptoBuffer& aAppParam,
-              const CryptoBuffer& aChallengeParam,
-              const CryptoBuffer& aClientData,
-              const CryptoBuffer& aKeyHandle,
-              nsISerialEventTarget* aEventTarget);
-
-  NS_DECL_NSIRUNNABLE
-private:
-  ~U2FSignTask();
-
-  nsString mVersion;
-  CryptoBuffer mAppParam;
-  CryptoBuffer mChallengeParam;
-  CryptoBuffer mClientData;
-  CryptoBuffer mKeyHandle;
-};
-
-// Mediate inter-thread communication for multiple authenticators being queried
-// in concert. Operates as a cyclic buffer with a stop-work method.
-class U2FStatus
-{
-public:
-  U2FStatus();
-  U2FStatus(const U2FStatus&) = delete;
-
-  void WaitGroupAdd();
-  void WaitGroupDone();
-  void WaitGroupWait();
-
-  void Stop(const ErrorCode aErrorCode);
-  void Stop(const ErrorCode aErrorCode, const nsAString& aResponse);
-  bool IsStopped();
-  ErrorCode GetErrorCode();
-  nsString GetResponse();
-
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(U2FStatus)
-
-private:
-  ~U2FStatus();
-
-  uint16_t mCount;
-  bool mIsStopped;
-  nsString mResponse;
-  MOZ_INIT_OUTSIDE_CTOR ErrorCode mErrorCode;
-  ReentrantMonitor mReentrantMonitor;
-};
-
-// U2FRunnables run to completion, performing a single U2F operation such as
-// registering, or signing.
-class U2FRunnable : public Runnable
-                  , public nsNSSShutDownObject
-{
-public:
-  U2FRunnable(const nsAString& aOrigin, const nsAString& aAppId,
-              nsISerialEventTarget* aEventTarget);
-
-  // No NSS resources to release.
-  virtual
-  void virtualDestroyNSSReference() override {};
-
-protected:
-  virtual ~U2FRunnable();
-  ErrorCode EvaluateAppID();
-
-  nsString mOrigin;
-  nsString mAppId;
-  const nsCOMPtr<nsISerialEventTarget> mEventTarget;
-};
-
-// This U2FRunnable completes a single application-requested U2F Register
-// operation.
-class U2FRegisterRunnable : public U2FRunnable
-{
-public:
-  U2FRegisterRunnable(const nsAString& aOrigin,
-                      const nsAString& aAppId,
-                      const Sequence<RegisterRequest>& aRegisterRequests,
-                      const Sequence<RegisteredKey>& aRegisteredKeys,
-                      const Sequence<Authenticator>& aAuthenticators,
-                      U2FRegisterCallback* aCallback,
-                      nsISerialEventTarget* aEventTarget);
-
-  void SendResponse(const RegisterResponse& aResponse);
-  void SetTimeout(const int32_t aTimeoutMillis);
-
-  NS_DECL_NSIRUNNABLE
-
-private:
-  ~U2FRegisterRunnable();
-
-  nsTArray<LocalRegisterRequest> mRegisterRequests;
-  nsTArray<LocalRegisteredKey> mRegisteredKeys;
-  nsTArray<Authenticator> mAuthenticators;
-  nsMainThreadPtrHandle<U2FRegisterCallback> mCallback;
-  Nullable<int32_t> opt_mTimeoutSeconds;
-};
-
-// This U2FRunnable completes a single application-requested U2F Sign operation.
-class U2FSignRunnable : public U2FRunnable
-{
-public:
-  U2FSignRunnable(const nsAString& aOrigin,
-                  const nsAString& aAppId,
-                  const nsAString& aChallenge,
-                  const Sequence<RegisteredKey>& aRegisteredKeys,
-                  const Sequence<Authenticator>& aAuthenticators,
-                  U2FSignCallback* aCallback,
-                  nsISerialEventTarget* aEventTarget);
-
-  void SendResponse(const SignResponse& aResponse);
-  void SetTimeout(const int32_t aTimeoutMillis);
-
-  NS_DECL_NSIRUNNABLE
-
-private:
-  ~U2FSignRunnable();
-
-  nsString mChallenge;
-  CryptoBuffer mClientData;
-  nsTArray<LocalRegisteredKey> mRegisteredKeys;
-  nsTArray<Authenticator> mAuthenticators;
-  nsMainThreadPtrHandle<U2FSignCallback> mCallback;
-  Nullable<int32_t> opt_mTimeoutSeconds;
-};
-
 // The U2F Class is used by the JS engine to initiate U2F operations.
 class U2F final : public nsISupports
                 , public nsWrapperCache
-                , public nsNSSShutDownObject
 {
 public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(U2F)
 
-  U2F();
+  explicit U2F(nsPIDOMWindowInner* aParent);
 
   nsPIDOMWindowInner*
   GetParentObject() const
@@ -291,7 +48,7 @@ public:
   }
 
   void
-  Init(nsPIDOMWindowInner* aParent, ErrorResult& aRv);
+  Init(ErrorResult& aRv);
 
   virtual JSObject*
   WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto) override;
@@ -312,16 +69,13 @@ public:
        const Optional<Nullable<int32_t>>& opt_aTimeoutSeconds,
        ErrorResult& aRv);
 
-  // No NSS resources to release.
-  virtual
-  void virtualDestroyNSSReference() override {};
-
 private:
-  nsCOMPtr<nsPIDOMWindowInner> mParent;
   nsString mOrigin;
-  Sequence<Authenticator> mAuthenticators;
-  bool mInitialized;
+  nsCOMPtr<nsPIDOMWindowInner> mParent;
   nsCOMPtr<nsISerialEventTarget> mEventTarget;
+  Maybe<nsMainThreadPtrHandle<U2FRegisterCallback>> mRegisterCallback;
+  Maybe<nsMainThreadPtrHandle<U2FSignCallback>> mSignCallback;
+  MozPromiseRequestHolder<U2FPromise> mPromiseHolder;
 
   ~U2F();
 };
