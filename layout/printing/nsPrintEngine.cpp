0587d75d083f0d784b2370e4d076e5df448694e2
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-09-11 10:42:22 +0900

    Bug 1263503 - memory leak of filename into layout/printing/nsPrintEngine.cpp. r=mantaroh
    "fileNameStr = fileName;" will be fileNameStr.Assign(fileName), so this causes small memory leak.  We should use Adopt, getter_copies, or AString parameter instead.
    
    MozReview-Commit-ID: 5VTgttj2LdK

--- layout/printing/nsPrintEngine.cpp
+++ layout/printing/nsPrintEngine.cpp
@@ -1847,9 +1847,7 @@ nsPrintEngine::SetupToPrintContent()
   printData->mPrintSettings->GetPrintToFile(&isPrintToFile);
   if (isPrintToFile) {
     // On some platforms The BeginDocument needs to know the name of the file.
-    char16_t* fileName = nullptr;
-    printData->mPrintSettings->GetToFileName(&fileName);
-    fileNameStr = fileName;
+    printData->mPrintSettings->GetToFileName(getter_Copies(fileNameStr));
   }
 
   nsAutoString docTitleStr;
