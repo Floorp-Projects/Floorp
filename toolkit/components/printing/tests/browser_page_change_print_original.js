4e4c8863738e8c814289a52581ab3e67b5e509b5
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2017-09-12 07:06:03 +0200

    Bug 1397657 - Update tests within toolkit/ to comply with new toplevel data: URI navigation policy. r=francois,dtownsend

--- toolkit/components/printing/tests/browser_page_change_print_original.js
+++ toolkit/components/printing/tests/browser_page_change_print_original.js
@@ -1,13 +1,16 @@
 // This file spawns content tasks.
 /* global content */
 
+const TEST_PATH = getRootDirectory(gTestPath)
+                    .replace("chrome://mochitests/content", "http://example.com");
+
 /**
  * Verify that if the page contents change after print preview is initialized,
  * and we re-initialize print preview (e.g. by changing page orientation),
  * we still show (and will therefore print) the original contents.
  */
 add_task(async function pp_after_orientation_change() {
-  const DATA_URI = `data:text/html,<script>window.onafterprint = function() { setTimeout("window.location = 'data:text/plain,REPLACED PAGE!'", 0); }</script><pre>INITIAL PAGE</pre>`;
+  const URI = TEST_PATH + "file_page_change_print_original_1.html";
   // Can only do something if we have a print preview UI:
   if (AppConstants.platform != "win" && AppConstants.platform != "linux") {
     ok(true, "Can't test if there's no print preview.");
@@ -15,7 +18,7 @@ add_task(async function pp_after_orientation_change() {
   }
 
   // Ensure we get a browserStopped for this browser
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, DATA_URI, false, true);
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, URI, false, true);
   let browserToPrint = tab.linkedBrowser;
   let ppBrowser = PrintPreviewListener.getPrintPreviewBrowser();
 
@@ -29,7 +32,7 @@ add_task(async function pp_after_orientation_change() {
 
   // Assert that we are showing the original page
   await ContentTask.spawn(ppBrowser, null, async function() {
-    is(content.document.body.textContent, "INITIAL PAGE", "Should have initial page print previewed.");
+    is(content.document.body.textContent.trim(), "INITIAL PAGE", "Should have initial page print previewed.");
   });
 
   await originalTabNavigated;
@@ -46,12 +49,12 @@ add_task(async function pp_after_orientation_change() {
 
   // Check that we're still showing the original page.
   await ContentTask.spawn(ppBrowser, null, async function() {
-    is(content.document.body.textContent, "INITIAL PAGE", "Should still have initial page print previewed.");
+    is(content.document.body.textContent.trim(), "INITIAL PAGE", "Should still have initial page print previewed.");
   });
 
   // Check that the other tab is definitely showing the new page:
   await ContentTask.spawn(browserToPrint, null, async function() {
-    is(content.document.body.textContent, "REPLACED PAGE!", "Original page should have changed.");
+    is(content.document.body.textContent.trim(), "REPLACED PAGE!", "Original page should have changed.");
   });
 
   PrintUtils.exitPrintPreview();
