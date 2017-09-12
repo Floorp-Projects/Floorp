7e7212e4ce0c4dbe850c2a598cd8eb3931b48e91
Leo Balter <leonardo.balter@gmail.com>
2017-09-12 13:45:00 -0700

    Bug 1374290 - Test the local import script. r=sfink
    --HG--
    extra : rebase_source : ae03ea7cd4c28c2614cf66825c215804d0954dcb

--- js/src/tests/test/run.py
+++ js/src/tests/test/run.py
@@ -1,23 +1,75 @@
 #!/usr/bin/env python
 # Adapted from https://github.com/tc39/test262/blob/master/tools/generation/test/run.py
 
-import shutil, subprocess, sys, os, unittest
+import shutil, subprocess, contextlib, tempfile, sys, os, unittest
 
 testDir = os.path.dirname(os.path.relpath(__file__))
 OUT_DIR = os.path.join(testDir, 'out')
 EXPECTED_DIR = os.path.join(testDir, 'expected')
 ex = os.path.join(testDir, '..', 'test262-export.py')
+importExec = os.path.join(testDir, '..', 'test262-update.py')
+test262Url = 'git://github.com/tc39/test262.git'
+
+@contextlib.contextmanager
+def TemporaryDirectory():
+    tmpDir = tempfile.mkdtemp()
+    try:
+        yield tmpDir
+    finally:
+        shutil.rmtree(tmpDir)
 
 class TestExport(unittest.TestCase):
     maxDiff = None
 
-    def fixture(self, name):
-        relpath = os.path.relpath(os.path.join(testDir, 'fixtures', name))
+    def exportScript(self):
+        relpath = os.path.relpath(os.path.join(testDir, 'fixtures', 'export'))
         sp = subprocess.Popen(
             [ex, relpath, '--out', OUT_DIR],
             stdout=subprocess.PIPE)
         stdout, stderr = sp.communicate()
         return dict(stdout=stdout, stderr=stderr, returncode=sp.returncode)
+
+    def importLocal(self):
+        with TemporaryDirectory() as cloneDir:
+            branch = 'smTempBranch'
+            # Clone Test262 to a local branch
+            subprocess.check_call(
+                ['git', 'clone', '--depth=1', test262Url, cloneDir]
+            )
+            # Checkout to a new branch
+            subprocess.check_call(
+                ['git', '-C', cloneDir, 'checkout', '-b', branch]
+            )
+            # Make changes on the new branch
+            # Remove test/language/export/escaped-from.js
+            subprocess.check_call(
+                ['git', '-C', cloneDir, 'rm',
+                    'test/language/export/escaped-from.js']
+            )
+            # Rename test/language/export/escaped-default.js
+            subprocess.check_call(
+                ['git', '-C', cloneDir, 'mv',
+                    'test/language/export/escaped-default.js',
+                    'test/language/export/escaped-foobarbaz.js',
+                ]
+            )
+            # Copy fixtures files
+            fixturesDir = os.path.join(testDir, 'fixtures', 'import', 'files')
+            shutil.copytree(fixturesDir, os.path.join(cloneDir, 'test', 'temp42'))
+            # Stage and Commit changes
+            subprocess.check_call(['git', '-C', cloneDir, 'add', '.'])
+            subprocess.check_call(['git', '-C', cloneDir, 'commit', '-m', '"local foo"'])
+
+            # Run import script
+            print("%s --local %s --out %s" % (importExec, cloneDir, OUT_DIR))
+            sp = subprocess.Popen(
+                [importExec, '--local', cloneDir, '--out', OUT_DIR],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            stdoutdata, _ = sp.communicate()
+
+            return stdoutdata, sp.returncode, cloneDir
 
     def isTestFile(self, filename):
         return not (
@@ -52,13 +104,30 @@ class TestExport(unittest.TestCase):
                         expectedHandle.read(),
                         actualHandle.read())
 
+    def compareContents(self, output, filePath, folder):
+        with open(filePath, "rb") as file:
+            expected = file.read()
+
+        expected = expected.replace('{{folder}}', folder)
+        self.assertMultiLineEqual(output, expected)
+
     def tearDown(self):
         shutil.rmtree(OUT_DIR, ignore_errors=True)
 
     def test_export(self):
-        result = self.fixture('export')
+        result = self.exportScript()
         self.assertEqual(result['returncode'], 0)
         self.compareTrees('export')
+
+    def test_import_local(self):
+        output, returncode, folder = self.importLocal()
+        self.assertEqual(returncode, 0)
+        self.compareTrees(os.path.join('import', 'files'))
+        self.compareContents(
+            output,
+            os.path.join(testDir, 'expected', 'import', 'output.txt'),
+            folder
+        )
 
 if __name__ == '__main__':
     unittest.main()
