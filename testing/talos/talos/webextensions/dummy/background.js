97810bfa02560eff7be14a79d5092e11c135efd4
Kris Maglione <maglione.k@gmail.com>
2017-09-11 18:03:07 -0700

    Bug 1398974: Part 1 - Add webRequest listeners to webext talos add-on. r=mixedpuppy,jmaher
    MozReview-Commit-ID: EUakliO1WnL

--- testing/talos/talos/webextensions/dummy/background.js
+++ testing/talos/talos/webextensions/dummy/background.js
@@ -1,3 +1,37 @@
 "use strict";
 
-// Empty background script
+/* eslint-env webextensions */
+
+browser.webRequest.onBeforeRequest.addListener(
+  details => {
+    let filter = browser.webRequest.filterResponseData(details.requestId);
+
+    filter.onstop = event => {
+      filter.close();
+    };
+    filter.ondata = event => {
+      filter.write(event.data);
+    };
+  }, {
+    urls: ["<all_urls>"],
+  },
+  ["blocking"]);
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+  details => {
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ["https://*/*", "http://*/*"]},
+  ["blocking", "requestHeaders"]);
+
+browser.webRequest.onHeadersReceived.addListener(
+  details => {
+    return {responseHeaders: details.responseHeaders};
+  },
+  {urls: ["https://*/*", "http://*/*"]},
+  ["blocking", "responseHeaders"]);
+
+browser.webRequest.onErrorOccurred.addListener(
+  details => {
+  },
+  {urls: ["https://*/*", "http://*/*"]});
