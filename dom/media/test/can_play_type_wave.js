e12a21d67b233df85dc23d1de1fa2ce1dde93e0c
James Cheng <jacheng@mozilla.com>
2017-09-12 17:28:48 +0800

    Bug 1398102 - [Part2] Fix test fail due to wrong assumption. r=gerald
    MozReview-Commit-ID: 1wRNQ2oIY1w

--- dom/media/test/can_play_type_wave.js
+++ dom/media/test/can_play_type_wave.js
@@ -14,8 +14,8 @@ function check_wave(v, enabled) {
   check("audio/wave; codecs=6", "probably");
   check("audio/wave; codecs=7", "probably");
   // "no codecs" should be supported, I guess
-  check("audio/wave; codecs=", "probably");
-  check("audio/wave; codecs=\"\"", "probably");
+  check("audio/wave; codecs=", "maybe");
+  check("audio/wave; codecs=\"\"", "maybe");
 
   // Unsupported Wave codecs
   check("audio/wave; codecs=0", "");
