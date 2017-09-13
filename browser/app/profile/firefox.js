d089151bf7e7b52ced21aefa93bb24b0c08e84ce
Ursula Sarracini <none@none>
2017-09-13 18:06:33 -0400

    Bug 1396274 - Disable Activity Stream about:home in browser_aboutHome.js r=Mardak
    MozReview-Commit-ID: LXzgaAhNjnV
    
    --HG--
    extra : rebase_source : dca565e5bcd16b5f3f13aa355567a903d2c63b82

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1275,7 +1275,7 @@ pref("browser.newtabpage.directory.source", "https://tiles.services.mozilla.com/
 // activates Activity Stream
 pref("browser.newtabpage.activity-stream.enabled", true);
 pref("browser.newtabpage.activity-stream.prerender", true);
-pref("browser.newtabpage.activity-stream.aboutHome.enabled", false);
+pref("browser.newtabpage.activity-stream.aboutHome.enabled", true);
 
 // Enable the DOM fullscreen API.
 pref("full-screen-api.enabled", true);
