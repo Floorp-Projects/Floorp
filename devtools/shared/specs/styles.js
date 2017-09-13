4722ef2d5a9fe76c20f47ec83e6c0d427d909f6c
Alexandre Poirot <poirot.alex@gmail.com>
2017-09-13 22:48:43 +0200

    Bug 1397330 - Define all specs and fronts relying on implicit require. r=jdescottes
    MozReview-Commit-ID: HzcEWauFKjz

--- devtools/shared/specs/styles.js
+++ devtools/shared/specs/styles.js
@@ -10,8 +10,6 @@ const {
   generateActorSpec,
   types
 } = require("devtools/shared/protocol");
-require("devtools/shared/specs/node");
-require("devtools/shared/specs/stylesheets");
 
 // Predeclare the domstylerule actor type
 types.addActorType("domstylerule");
