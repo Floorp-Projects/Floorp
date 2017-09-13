c1da39e098ee8d3eada707d09259a3ca9d983510
Morris Tseng <mtseng@mozilla.com>
2017-09-13 17:05:52 +0800

    Bug 1394249 - SetNeedToCalcHasBCBorders to true when initialize nsTableFrame. r=mats
    In the printing preview, we create continuous table frame if table is too
    long to containing in a page. But the default value of
    NeedToCalcHasBCBorders is false which means we don't calculate
    HasBCBorders for continuous table frame. Thus, the border collapse is
    not shown when printing preview.
    
    MozReview-Commit-ID: IqhLSYuWj30

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -173,6 +173,9 @@ nsTableFrame::Init(nsIContent*       aContent,
   const nsStyleTableBorder* tableStyle = StyleTableBorder();
   bool borderCollapse = (NS_STYLE_BORDER_COLLAPSE == tableStyle->mBorderCollapse);
   SetBorderCollapse(borderCollapse);
+  if (borderCollapse) {
+    SetNeedToCalcHasBCBorders(true);
+  }
 
   if (!aPrevInFlow) {
     // If we're the first-in-flow, we manage the cell map & layout strategy that
