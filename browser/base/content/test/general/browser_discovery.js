7c804055582e9d6d8b726d22830baaa651b338f2
Wes Kocher <wkocher@mozilla.com>
2017-09-12 18:06:16 -0700

    Backed out changeset 5ed9b657a1ce (bug 1352459) for xperf failures a=backout CLOSED TREE
    MozReview-Commit-ID: 9XmmBsTf39a

--- browser/base/content/test/general/browser_discovery.js
+++ browser/base/content/test/general/browser_discovery.js
@@ -34,26 +34,17 @@ var iconDiscoveryTests = [
 ];
 
 function runIconDiscoveryTest() {
-  let testCase = iconDiscoveryTests[0];
-  let head = doc().getElementById("linkparent");
+  var testCase = iconDiscoveryTests[0];
+  var head = doc().getElementById("linkparent");
+  var hasSrc = gBrowser.getIcon() != null;
+  if (testCase.pass)
+    ok(hasSrc, testCase.text);
+  else
+    ok(!hasSrc, testCase.text);
 
-  // Because there is debounce logic in ContentLinkHandler.jsm to reduce the
-  // favicon loads, we have to wait some time before checking that icon was
-  // stored properly.
-  BrowserTestUtils.waitForCondition(() => {
-    return gBrowser.getIcon() != null;
-  }, "wait for icon load to finish", 100, 5)
-  .then(() => {
-    ok(testCase.pass, testCase.text);
-  })
-  .catch(() => {
-    ok(!testCase.pass, testCase.text);
-  })
-  .then(() => {
-    head.removeChild(head.getElementsByTagName("link")[0]);
-    iconDiscoveryTests.shift();
-    iconDiscovery(); // Run the next test.
-  });
+  head.removeChild(head.getElementsByTagName("link")[0]);
+  iconDiscoveryTests.shift();
+  iconDiscovery(); // Run the next test.
 }
 
 function iconDiscovery() {
@@ -71,63 +62,6 @@ function iconDiscovery() {
     var href = testCase.href || rootDir + "moz.png";
     var type = testCase.type || "image/png";
     if (testCase.pass == undefined)
-      testCase.pass = true;
-
-    link.rel = rel;
-    link.href = href;
-    link.type = type;
-    head.appendChild(link);
-  } else {
-    richIconDiscovery();
-  }
-}
-
-let richIconDiscoveryTests = [
-  { rel: "apple-touch-icon", text: "apple-touch-icon discovered" },
-  { rel: "apple-touch-icon-precomposed", text: "apple-touch-icon-precomposed discovered" },
-  { rel: "fluid-icon", text: "fluid-icon discovered" },
-  { rel: "unknown-icon", pass: false, text: "unknown icon not discovered" }
-];
-
-function runRichIconDiscoveryTest() {
-  let testCase = richIconDiscoveryTests[0];
-  let head = doc().getElementById("linkparent");
-
-  // Because there is debounce logic in ContentLinkHandler.jsm to reduce the
-  // favicon loads, we have to wait some time before checking that icon was
-  // stored properly.
-  BrowserTestUtils.waitForCondition(() => {
-    return gBrowser.getIcon() != null;
-  }, "wait for icon load to finish", 100, 5)
-  .then(() => {
-    ok(testCase.pass, testCase.text);
-  })
-  .catch(() => {
-    ok(!testCase.pass, testCase.text);
-  })
-  .then(() => {
-    head.removeChild(head.getElementsByTagName("link")[0]);
-    richIconDiscoveryTests.shift();
-    richIconDiscovery(); // Run the next test.
-  });
-}
-
-function richIconDiscovery() {
-  if (richIconDiscoveryTests.length) {
-    setHandlerFunc(runRichIconDiscoveryTest);
-    gBrowser.setIcon(gBrowser.selectedTab, null,
-                     Services.scriptSecurityManager.getSystemPrincipal()
-    );
-
-    let testCase = richIconDiscoveryTests[0];
-    let head = doc().getElementById("linkparent");
-    let link = doc().createElement("link");
-
-    let rel = testCase.rel;
-    let rootDir = getRootDirectory(gTestPath);
-    let href = testCase.href || rootDir + "moz.png";
-    let type = testCase.type || "image/png";
-    if (testCase.pass === undefined)
       testCase.pass = true;
 
     link.rel = rel;
--- browser/base/content/test/general/browser_subframe_favicons_not_used.js
+++ browser/base/content/test/general/browser_subframe_favicons_not_used.js
@@ -9,24 +9,10 @@ function test() {
 
   tab.linkedBrowser.addEventListener("load", function() {
     let expectedIcon = testPath + "file_bug970276_favicon1.ico";
-    let icon;
+    is(gBrowser.getIcon(tab), expectedIcon, "Correct icon.");
 
-    // Because there is debounce logic in ContentLinkHandler.jsm to reduce the
-    // favicon loads, we have to wait some time before checking that icon was
-    // stored properly.
-    BrowserTestUtils.waitForCondition(() => {
-      icon = gBrowser.getIcon(tab);
-      return icon != null;
-    }, "wait for favicon load to finish", 100, 5)
-    .then(() => {
-      is(icon, expectedIcon, "Correct icon.");
-    })
-    .catch(() => {
-      ok(false, "Can't get the correct icon.");
-    })
-    .then(() => {
-      gBrowser.removeTab(tab);
-      finish();
-    });
+    gBrowser.removeTab(tab);
+
+    finish();
   }, {capture: true, once: true});
 }
