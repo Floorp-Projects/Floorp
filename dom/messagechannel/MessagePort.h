fcb5aa08e3567fbbc8f20b82cc853aaf44a48f9c
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-13 08:56:53 +0200

    Backed out changeset a8d6f6fd7bda (bug 1359017) for failing wpt's /html/dom/reflection-forms.html. r=backout

--- dom/messagechannel/MessagePort.h
+++ dom/messagechannel/MessagePort.h
@@ -74,8 +74,6 @@ public:
 
   void SetOnmessage(EventHandlerNonNull* aCallback);
 
-  IMPL_EVENT_HANDLER(messageerror)
-
   // Non WebIDL methods
 
   void UnshippedEntangle(MessagePort* aEntangledPort);
@@ -148,8 +146,6 @@ private:
 
   // Dispatch events from the Message Queue using a nsRunnable.
   void Dispatch();
-
-  void DispatchError();
 
   void StartDisentangling();
   void Disentangle();
