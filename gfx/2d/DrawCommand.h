865fcae12b5560d5c1ab6474cf464cdc0db7e0ba
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-13 23:06:13 +0200

    Backed out changeset 8cd211d1292e (bug 1399178) for unexpected passes on quantumrenderer, e.g. in reftest layout/reftests/counters/counter-suffix-ref.html. r=backout

--- gfx/2d/DrawCommand.h
+++ gfx/2d/DrawCommand.h
@@ -37,7 +37,6 @@ enum class CommandType : int8_t {
   POPCLIP,
   POPLAYER,
   SETTRANSFORM,
-  SETPERMITSUBPIXELAA,
   FLUSH
 };
 
@@ -674,25 +673,6 @@ public:
 
 private:
   Matrix mTransform;
-};
-
-class SetPermitSubpixelAACommand : public DrawingCommand
-{
-  friend class DrawTargetCaptureImpl;
-public:
-  explicit SetPermitSubpixelAACommand(bool aPermitSubpixelAA)
-    : DrawingCommand(CommandType::SETPERMITSUBPIXELAA)
-    , mPermitSubpixelAA(aPermitSubpixelAA)
-  {
-  }
-
-  virtual void ExecuteOnDT(DrawTarget* aDT, const Matrix* aMatrix) const
-  {
-    aDT->SetPermitSubpixelAA(mPermitSubpixelAA);
-  }
-
-private:
-  bool mPermitSubpixelAA;
 };
 
 class FlushCommand : public DrawingCommand
