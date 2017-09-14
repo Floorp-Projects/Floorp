a95bbdf3f66138bee9a91182a20b22ea888ab766
Ted Mielczarek <ted@mielczarek.org>
2017-09-14 06:42:53 -0400

    bug 1299959 - use GENERATED_FILES for libffi's preprocessed assembly files. r=chmanchester
    libffi has assembly files on Windows that need to be preprocessed. Previously
    this was handled by using a wrapper shell script as the assembler. This
    patch handles them in GENERATED_FILES with a Python script that performs
    an equivalent transformation.
    
    MozReview-Commit-ID: 3w0HNpUb8TA
    
    --HG--
    extra : rebase_source : 284a3825cdaf2957507d60276a772f88f8c96488
    extra : source : 3f422cbd4f222bc0b19f0a9e76878787652b3788

---
+++ config/external/ffi/preprocess_libffi_asm.py
@@ -0,0 +1,26 @@
+# -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
+# vim: set filetype=python:
+# This Souce Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distibuted with this
+# file, You can obtain one at http://mozilla.og/MPL/2.0/.
+
+import buildconfig
+import mozpack.path as mozpath
+import os
+import re
+import shlex
+import subprocess
+
+def main(output, input_asm, defines, includes):
+    defines = shlex.split(defines)
+    includes = shlex.split(includes)
+    # CPP uses -E which generates #line directives. -EP suppresses them.
+    cpp = buildconfig.substs['CPP'] + ['-EP']
+    input_asm = mozpath.relpath(input_asm, os.getcwd())
+    args = cpp + defines + includes + [input_asm]
+    print(' '.join(args))
+    preprocessed = subprocess.check_output(args)
+    r = re.compile('F[dpa][^ ]*')
+    for line in preprocessed.splitlines():
+        output.write(r.sub('', line))
+        output.write('\n')
