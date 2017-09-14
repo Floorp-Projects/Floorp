d38d1a12e5ebb0d33e2d1d446707626bcd793f97
Ethan Lin <ethlin@mozilla.com>
2017-09-14 14:40:31 +0800

    Bug 1397375 - Part3. Remove the related webrender user data when destroying a frame. r=jrmuizel
    MozReview-Commit-ID: IYjfqWiiPZ

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -10483,6 +10483,15 @@ nsIFrame::DestroyContentArray(ContentArray* aArray)
   delete aArray;
 }
 
+/*static*/ void
+nsIFrame::DestroyWebRenderUserDataTable(WebRenderUserDataTable* aTable)
+{
+  for (auto iter = aTable->Iter(); !iter.Done(); iter.Next()) {
+    iter.UserData()->RemoveFromTable();
+  }
+  delete aTable;
+}
+
 bool
 nsIFrame::IsPseudoStackingContextFromStyle() {
   // If you change this, also change the computation of pseudoStackingContext
