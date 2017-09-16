e0914799fca9bd593253d840217c33a4890cf3ac
Ryan VanderMeulen <ryanvm@gmail.com>
2017-09-16 12:18:26 -0400

    Backed out changeset ce09da537d44 (bug 1400391) for Android xpcshell failures.

--- toolkit/components/extensions/test/xpcshell/head_remote.js
+++ toolkit/components/extensions/test/xpcshell/head_remote.js
@@ -2,6 +2,3 @@
 
 Services.prefs.setBoolPref("extensions.webextensions.remote", true);
 Services.prefs.setIntPref("dom.ipc.keepProcessesAlive.extension", 1);
-
-/* globals testEnv */
-testEnv.expectRemote = true; // tested in head_test.js
--- toolkit/components/extensions/test/xpcshell/head_test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-/* globals testEnv */
-Assert.equal(WebExtensionPolicy.useRemoteWebExtensions, testEnv.expectRemote, "useRemoteWebExtensions matches");
-Assert.equal(WebExtensionPolicy.isExtensionProcess, !testEnv.expectRemote, "testing from extension process");
