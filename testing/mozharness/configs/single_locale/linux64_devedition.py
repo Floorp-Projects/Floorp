89231b3d3aaa0243227c0dfbae2d4816c81fd1a0
Axel Hecht <axel@pike.org>
2017-09-18 15:00:37 +0200

    bug 1399523, adjust configs to not merge l10n in mozharness, r=Callek
    MozReview-Commit-ID: 5SmOCzTZiqz

--- testing/mozharness/configs/single_locale/linux64_devedition.py
+++ testing/mozharness/configs/single_locale/linux64_devedition.py
@@ -8,10 +8,8 @@ config = {
     "bootstrap_env": {
         "MOZ_OBJDIR": "obj-firefox",
         "EN_US_BINARY_URL": "%(en_us_binary_url)s",
-        "LOCALE_MERGEDIR": "%(abs_merge_dir)s/",
         "MOZ_UPDATE_CHANNEL": "%(update_channel)s",
         "DIST": "%(abs_objdir)s",
-        "LOCALE_MERGEDIR": "%(abs_merge_dir)s/",
         "L10NBASEDIR": "../../l10n",
         "MOZ_MAKE_COMPLETE_MAR": "1",
         'EN_US_PACKAGE_NAME': 'target.tar.bz2',
@@ -31,7 +29,6 @@ config = {
     "locales_file": "%(branch)s/browser/locales/all-locales",
     "locales_dir": "browser/locales",
     "hg_l10n_tag": "default",
-    "merge_locales": True,
 
     # MAR
     "previous_mar_dir": "dist/previous",
