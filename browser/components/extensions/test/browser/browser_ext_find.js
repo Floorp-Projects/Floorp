f7d41414af30908d30add012b4d9ae926b2d1291
Dan Banner <dbugs@thebanners.uk>
2017-09-18 19:07:17 +0100

    Bug 1395890 - Change all ESLint rules that are warnings to errors. r=standard8
    MozReview-Commit-ID: LJS6m1GppiS

--- browser/components/extensions/test/browser/browser_ext_find.js
+++ browser/components/extensions/test/browser/browser_ext_find.js
@@ -3,6 +3,7 @@
 
 function frameScript() {
   function getSelectedText() {
+    // eslint-disable-next-line mozilla/no-cpows-in-tests
     let frame = this.content.frames[0].frames[1];
     let Ci = Components.interfaces;
     let docShell = frame.QueryInterface(Ci.nsIInterfaceRequestor)
@@ -135,4 +136,3 @@ add_task(async function testDuplicatePinnedTab() {
   is(message.data.rect.top, top, `rect.top: - Expected: ${message.data.rect.top}, Actual: ${top}`);
   is(message.data.rect.left, left, `rect.left: - Expected: ${message.data.rect.left}, Actual: ${left}`);
 });
-
