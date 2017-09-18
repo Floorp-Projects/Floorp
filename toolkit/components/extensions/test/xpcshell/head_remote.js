37552268a286a7927952c0ff887ef053b9977e52
Shane Caraveo <scaraveo@mozilla.com>
2017-09-17 19:34:13 -0700

    Bug 1400391 - Fix e10s xpcshell-test for remote webextensions. r=kmag, a=lizzard
    MozReview-Commit-ID: AgdmUNWLGaq
    
    --HG--
    extra : source : 4f2552380412a7c656a1523bdd6c796827fa6266

--- toolkit/components/extensions/test/xpcshell/head_remote.js
+++ toolkit/components/extensions/test/xpcshell/head_remote.js
@@ -2,3 +2,6 @@
 
 Services.prefs.setBoolPref("extensions.webextensions.remote", true);
 Services.prefs.setIntPref("dom.ipc.keepProcessesAlive.extension", 1);
+
+/* globals testEnv */
+testEnv.expectRemote = true; // tested in head_test.js
