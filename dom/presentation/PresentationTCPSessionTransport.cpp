69e8f0e795192e58ef44d0586d117d19376d23c9
Boris Zbarsky <bzbarsky@mit.edu>
2017-09-19 16:26:21 +0200

    Bug 1401171 - Make nsIMultiplexInputStream not inherit from nsIInputStream.  r=bkelly
    This is a preexisting issue that makes nsMultiplexInputStream multiple-inherit
    from nsIInputStream: once via nsIMultipartInputStream and once via
    nsIAsyncInputStream.  This causes problems once we end up with more multiplex
    streams that are async streams, because then some assingments to
    nsCOMPtr<nsIInputStream> start asserting.  This patch just removes the footgun
    by getting rid of the multiple inheritance.

--- dom/presentation/PresentationTCPSessionTransport.cpp
+++ dom/presentation/PresentationTCPSessionTransport.cpp
@@ -240,6 +240,7 @@ PresentationTCPSessionTransport::CreateStream()
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
+  nsCOMPtr<nsIInputStream> stream = do_QueryInterface(mMultiplexStream);
 
   mMultiplexStreamCopier = do_CreateInstance("@mozilla.org/network/async-stream-copier;1", &rv);
   if (NS_WARN_IF(NS_FAILED(rv))) {
@@ -253,7 +254,7 @@ PresentationTCPSessionTransport::CreateStream()
   }
 
   nsCOMPtr<nsIEventTarget> target = do_QueryInterface(sts);
-  rv = mMultiplexStreamCopier->Init(mMultiplexStream,
+  rv = mMultiplexStreamCopier->Init(stream,
                                     mSocketOutputStream,
                                     target,
                                     true, /* source buffered */
