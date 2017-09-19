245111caad187afb8d2ce776889d90fd0864af5b
Ethan Lin <ethlin@mozilla.com>
2017-09-19 17:05:22 +0800

    Bug 1398706 - Always create new clip ids in ScrollingLayersHelper if it's inside a mask. r=kats
    MozReview-Commit-ID: Hhsbgpqddo1

--- gfx/webrender_bindings/WebRenderAPI.h
+++ gfx/webrender_bindings/WebRenderAPI.h
@@ -226,8 +226,8 @@ public:
   wr::WrClipId DefineClip(const wr::LayoutRect& aClipRect,
                           const nsTArray<wr::WrComplexClipRegion>* aComplex = nullptr,
                           const wr::WrImageMask* aMask = nullptr);
-  void PushClip(const wr::WrClipId& aClipId, bool aRecordInStack = true);
-  void PopClip(bool aRecordInStack = true);
+  void PushClip(const wr::WrClipId& aClipId, bool aMask = false);
+  void PopClip(bool aMask = false);
 
   wr::WrStickyId DefineStickyFrame(const wr::LayoutRect& aContentRect,
                                    const wr::StickySideConstraint* aTop,
@@ -401,6 +401,10 @@ public:
 
   // Try to avoid using this when possible.
   wr::WrState* Raw() { return mWrState; }
+
+  // Return true if the current clip stack has any mask type clip.
+  bool HasMaskClip() { return mMaskClipCount > 0; }
+
 protected:
   wr::WrState* mWrState;
 
@@ -415,6 +419,9 @@ protected:
   // Nothing() value indicates a root scroll id. We also use this structure to
   // ensure that we don't define a particular scroll layer multiple times.
   std::unordered_map<layers::FrameMetrics::ViewID, Maybe<layers::FrameMetrics::ViewID>> mScrollParents;
+
+  // The number of mask clips that are in the stack.
+  uint32_t mMaskClipCount;
 
   friend class WebRenderAPI;
 };
