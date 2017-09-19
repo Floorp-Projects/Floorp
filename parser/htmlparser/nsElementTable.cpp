f42ee63cbaf2a14e4eef379077f88ed1553f8d95
Nicholas Nethercote <nnethercote@mozilla.com>
2017-09-19 11:08:39 +1000

    Bug 1400777 (part 2) - De-expose HTML group constants[]. r=mrbkap.
    The patch also removes some low-value comments.

--- parser/htmlparser/nsElementTable.cpp
+++ parser/htmlparser/nsElementTable.cpp
@@ -8,11 +8,38 @@
 #include "nsIAtom.h"
 #include "nsElementTable.h"
 
-/***************************************************************************** 
-  Now it's time to list all the html elements all with their capabilities...
-******************************************************************************/
+static const int kNone= 0x0;
 
-// The Element Table (sung to the tune of Modern Major General)
+static const int kHTMLContent   = 0x0001; //  HEAD, (FRAMESET | BODY)
+static const int kHeadContent   = 0x0002; //  Elements that *must* be in the head.
+static const int kHeadMisc      = 0x0004; //  Elements that *can* be in the head.
+
+static const int kSpecial       = 0x0008; //  A,    IMG,  APPLET, OBJECT, FONT, BASEFONT, BR, SCRIPT, 
+                                          //  MAP,  Q,    SUB,    SUP,    SPAN, BDO,      IFRAME
+
+static const int kFormControl   = 0x0010; //  INPUT SELECT  TEXTAREA  LABEL BUTTON
+static const int kPreformatted  = 0x0020; //  PRE
+static const int kPreExclusion  = 0x0040; //  IMG,  OBJECT, APPLET, BIG,  SMALL,  SUB,  SUP,  FONT, BASEFONT
+static const int kFontStyle     = 0x0080; //  TT, I, B, U, S, STRIKE, BIG, SMALL
+static const int kPhrase        = 0x0100; //  EM, STRONG, DFN, CODE, SAMP, KBD, VAR, CITE, ABBR, ACRONYM
+static const int kHeading       = 0x0200; //  H1..H6
+static const int kBlockMisc     = 0x0400; //  OBJECT, SCRIPT
+static const int kBlock         = 0x0800; //  ADDRESS, BLOCKQUOTE, CENTER, DIV, DL, FIELDSET, FORM, 
+                                          //  ISINDEX, HR, NOSCRIPT, NOFRAMES, P, TABLE
+static const int kList          = 0x1000; //  UL, OL, DIR, MENU
+static const int kPCDATA        = 0x2000; //  plain text and entities...
+static const int kSelf          = 0x4000; //  whatever THIS tag is...
+static const int kExtensions    = 0x8000; //  BGSOUND, WBR, NOBR
+static const int kTable         = 0x10000;//  TR,TD,THEAD,TBODY,TFOOT,CAPTION,TH
+static const int kDLChild       = 0x20000;//  DL, DT
+static const int kCDATA         = 0x40000;//  just plain text...
+
+static const int kInlineEntity  = (kPCDATA|kFontStyle|kPhrase|kSpecial|kFormControl|kExtensions);  //  #PCDATA, %fontstyle, %phrase, %special, %formctrl
+static const int kBlockEntity   = (kHeading|kList|kPreformatted|kBlock); //  %heading, %list, %preformatted, %block
+static const int kFlowEntity    = (kBlockEntity|kInlineEntity); //  %blockentity, %inlineentity
+static const int kAllTags       = 0xffffff;
+
+
 
 #ifdef DEBUG
 #define ELEM(tag, parent, leaf) { eHTMLTag_##tag, parent, leaf },
