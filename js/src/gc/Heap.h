c9e470d740ab90aa75326fa35f4e48c03771161d
Jon Coppeard <jcoppeard@mozilla.com>
2017-09-19 12:31:31 +0100

    Bug 1399944 - Check for valid GC cell pointers in various places r=sfink

--- js/src/gc/Heap.h
+++ js/src/gc/Heap.h
@@ -1107,6 +1107,9 @@ static_assert(js::gc::ChunkRuntimeOffset == offsetof(Chunk, trailer) +
 static_assert(js::gc::ChunkLocationOffset == offsetof(Chunk, trailer) +
                                              offsetof(ChunkTrailer, location),
               "The hardcoded API location offset must match the actual offset.");
+static_assert(js::gc::ChunkStoreBufferOffset == offsetof(Chunk, trailer) +
+                                                offsetof(ChunkTrailer, storeBuffer),
+              "The hardcoded API storeBuffer offset must match the actual offset.");
 
 /*
  * Tracks the used sizes for owned heap data and automatically maintains the
