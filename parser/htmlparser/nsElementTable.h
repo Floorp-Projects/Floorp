7b0fccab6ba259086914b8764d16e1eb10af7fa5
Nicholas Nethercote <nnethercote@mozilla.com>
2017-09-20 08:32:51 +1000

    Bug 1400777 (part 5) - Clean up nsElementTable.{cpp,h}. r=mrbkap.
    This patch makes some style fixes and other minor improvements.

--- parser/htmlparser/nsElementTable.h
+++ parser/htmlparser/nsElementTable.h
@@ -1,31 +1,22 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-/**
- * MODULE NOTES:
- * @update  gess 4/1/98
- * 
- */
-
-
-
-#ifndef _NSELEMENTABLE
-#define _NSELEMENTABLE
+#ifndef nsElementTable_h
+#define nsElementTable_h
 
 #include "nsHTMLTags.h"
-#include "nsIDTD.h"
 
 #ifdef DEBUG
-extern void CheckElementTable();
+void CheckElementTable();
 #endif
 
 struct nsHTMLElement
 {
-  static  bool    IsContainer(nsHTMLTag aTag);
-  static  bool    IsBlock(nsHTMLTag aTag);
+  static bool IsContainer(nsHTMLTag aTag);
+  static bool IsBlock(nsHTMLTag aTag);
 };
 
-#endif
+#endif // nsElementTable_h
