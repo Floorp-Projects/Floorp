1bd72c0834a9bedf595cd583a55237169e465e15
grenewode <grenewodemiller@gmail.com>
2017-09-19 19:08:01 -0400

    Bug 1385233 - Fixed the moz.build so that preprocessor instructions are no longer required in bootstrap.js. r=jaws moz.build had bootstrap.js in FINAL_TARGET_PP_FILES which invokes preprocessing. Solved by moving bootstrap.js to FINAL_TARGET_FILES.
    MozReview-Commit-ID: 8Sk8Cu5eFn9
    
    --HG--
    extra : rebase_source : e423032746fb9da79a1b05ae3318979511f2021d

--- browser/tools/mozscreenshots/mozscreenshots/extension/bootstrap.js
+++ browser/tools/mozscreenshots/mozscreenshots/extension/bootstrap.js
@@ -1,11 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-/*
-#if 0
-Workaround a build system bug where this file doesn't get packaged if not pre-processed.
-#endif
-*/
 
 /* exported install, uninstall, startup, shutdown */
 
