6c306c6abb6b4b364d7269ffbbcd2525d2be89be
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-21 00:07:06 +0200

    Backed out changeset 53e599ac1ed7 (bug 1359279) for android linting failure. r=backout

--- mobile/android/services/src/main/java/org/mozilla/gecko/fxa/sync/FxAccountSyncAdapter.java
+++ mobile/android/services/src/main/java/org/mozilla/gecko/fxa/sync/FxAccountSyncAdapter.java
@@ -484,7 +484,7 @@ public class FxAccountSyncAdapter extends AbstractThreadedSyncAdapter {
 
     FxAccountDeviceListUpdater deviceListUpdater = new FxAccountDeviceListUpdater(fxAccount, context.getContentResolver());
     // Since the clients stage requires a fresh list of remote devices, we update the device list synchronously.
-    deviceListUpdater.updateAndMaybeRenewRegistration(context);
+    deviceListUpdater.update();
   }
 
   /**
