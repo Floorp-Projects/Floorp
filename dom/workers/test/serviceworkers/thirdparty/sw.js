aedd9dfac3fa3f2816e0d2001dc2ae8adcd63b14
Ben Kelly <ben@wanderview.com>
2017-09-20 09:24:07 -0700

    Bug 1336364 P8 Expand test_third_party_iframes.html to verify worker scripts are intercepted. r=asuth

--- dom/workers/test/serviceworkers/thirdparty/sw.js
+++ dom/workers/test/serviceworkers/thirdparty/sw.js
@@ -1,14 +1,29 @@
 self.addEventListener("fetch", function(event) {
   dump("fetch " + event.request.url + "\n");
-  if (event.request.url.indexOf("iframe2.html") >= 0) {
+  if (event.request.url.includes("iframe2.html")) {
     var body =
       "<script>" +
         "window.parent.postMessage({" +
           "source: 'iframe', status: 'swresponse'" +
         "}, '*');" +
+        "var w = new Worker('worker.js');" +
+        "w.onmessage = function(evt) {" +
+          "window.parent.postMessage({" +
+            "source: 'worker'," +
+            "status: evt.data," +
+          "}, '*');" +
+      "};" +
       "</script>";
     event.respondWith(new Response(body, {
       headers: {'Content-Type': 'text/html'}
     }));
+    return;
+  }
+  if (event.request.url.includes("worker.js")) {
+    var body = "self.postMessage('worker-swresponse');";
+    event.respondWith(new Response(body, {
+      headers: {'Content-Type': 'application/javascript'}
+    }));
+    return;
   }
 });
