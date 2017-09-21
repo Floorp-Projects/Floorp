6bc8986bc160971f2c0b42442e3e12c83de6b177
Wes Kocher <wkocher@mozilla.com>
2017-09-21 13:48:10 -0700

    Backed out changeset e89d2565799b (bug 1047098) for xpcshell bustage a=backout
    MozReview-Commit-ID: FRFALkRxDxl

--- toolkit/components/places/tests/head_common.js
+++ toolkit/components/places/tests/head_common.js
@@ -75,7 +75,7 @@ XPCOMUtils.defineLazyGetter(this, "SMALLSVG_DATA_URI", function() {
 var gTestDir = do_get_cwd();
 
 // Initialize profile.
-var gProfD = do_get_profile(true);
+var gProfD = do_get_profile();
 
 Services.prefs.setBoolPref("browser.urlbar.usepreloadedtopurls.enabled", false);
 do_register_cleanup(() =>
