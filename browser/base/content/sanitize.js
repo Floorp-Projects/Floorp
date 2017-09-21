f63a60f24ee0d4b64bf6eaa0487929a010d6a021
Andrea Marchesini <amarchesini@mozilla.com>
2017-09-21 21:32:00 +0200

    Bug 1047098 - "Clear Recent History" must clean up all the ServiceWorkers. r=bkelly, a=lizzard
    This includes minor shutdown fixes by :asuth as discussed on
    https://bugzilla.mozilla.org/show_bug.cgi?id=1047098#c56 and c57.
    
    --HG--
    extra : rebase_source : fa5919053c5aa2cd877f4f24ea288058d24700fa
    extra : intermediate-source : a98d099fc07906b36c63af613d8928a07abfee5e
    extra : source : e89d2565799b4b02d5ee2c56da8d44dc0067f26a

--- browser/base/content/sanitize.js
+++ browser/base/content/sanitize.js
@@ -25,6 +25,9 @@ XPCOMUtils.defineLazyModuleGetter(this, "setTimeout",
                                   "resource://gre/modules/Timer.jsm");
 
 
+XPCOMUtils.defineLazyServiceGetter(this, "serviceWorkerManager",
+                                   "@mozilla.org/serviceworkers/manager;1",
+                                   "nsIServiceWorkerManager");
 XPCOMUtils.defineLazyServiceGetter(this, "quotaManagerService",
                                    "@mozilla.org/dom/quota-manager-service;1",
                                    "nsIQuotaManagerService");
@@ -297,6 +300,14 @@ Sanitizer.prototype = {
 
         // LocalStorage
         Services.obs.notifyObservers(null, "extension:purge-localStorage");
+
+        // ServiceWorkers
+        let serviceWorkers = serviceWorkerManager.getAllRegistrations();
+        for (let i = 0; i < serviceWorkers.length; i++) {
+          let sw = serviceWorkers.queryElementAt(i, Ci.nsIServiceWorkerRegistrationInfo);
+          let host = sw.principal.URI.host;
+          serviceWorkerManager.removeAndPropagate(host);
+        }
 
         // QuotaManager
         let promises = [];
