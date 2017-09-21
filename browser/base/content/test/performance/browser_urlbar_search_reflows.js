3d16048e6847ef3be603ce294850d4f6d4021131
Florian Quèze <florian@queze.net>
2017-09-21 12:49:52 +0200

    Bug 1401432 - Remove the 'setTimeout handler' stack frames in browser_urlbar_search_reflows.js to fix failures on beta, r=mak over IRC.

--- browser/base/content/test/performance/browser_urlbar_search_reflows.js
+++ browser/base/content/test/performance/browser_urlbar_search_reflows.js
@@ -23,9 +23,6 @@ const EXPECTED_REFLOWS_FIRST_OPEN = [
       "handleOverUnderflow@chrome://global/content/bindings/autocomplete.xml",
       "_onChanged@chrome://global/content/bindings/autocomplete.xml",
       "_appendCurrentResult/<@chrome://global/content/bindings/autocomplete.xml",
-      "setTimeout handler*_appendCurrentResult@chrome://global/content/bindings/autocomplete.xml",
-      "_invalidate@chrome://global/content/bindings/autocomplete.xml",
-      "invalidate@chrome://global/content/bindings/autocomplete.xml"
     ],
     times: 6, // This number should only ever go down - never up.
     minTimes: 0, // Sometimes this is not hit.
