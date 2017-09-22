da099ec7d1c344b2743b5e758d0f741807ad7797
Bill McCloskey <billm@mozilla.com>
2017-09-22 10:56:08 -0700

    Bug 1412190 - Allow active context to be null during profiling (r=jandem)
    MozReview-Commit-ID: 3bLcXBPpxtY

--- js/src/vm/GeckoProfiler.cpp
+++ js/src/vm/GeckoProfiler.cpp
@@ -441,7 +441,7 @@ ProfileEntry::script() const
     // AutoSuppressProfilerSampling prohibits the runtime's active context from
     // being changed while it exists.
     JSContext* cx = script->runtimeFromAnyThread()->activeContext();
-    if (!cx->isProfilerSamplingEnabled())
+    if (!cx || !cx->isProfilerSamplingEnabled())
         return nullptr;
 
     MOZ_ASSERT(!IsForwarded(script));
