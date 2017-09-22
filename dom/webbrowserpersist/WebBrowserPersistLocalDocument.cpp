1abda608e91722d89e23551f4dc17f4ef2aef2a0
Kyle Machulis <kyle@nonpolynomial.com>
2017-09-22 14:56:51 -0700

    Bug 1389650 - Remove nsIDOMHTMLAnchorElement; r=bz
    Removes the XPCOM interface for nsIDOMHTMLAnchorElement, replacing it
    with binding class usage.
    
    MozReview-Commit-ID: 7v0bKlY7Fax

--- dom/webbrowserpersist/WebBrowserPersistLocalDocument.cpp
+++ dom/webbrowserpersist/WebBrowserPersistLocalDocument.cpp
@@ -6,6 +6,7 @@
 #include "WebBrowserPersistLocalDocument.h"
 #include "WebBrowserPersistDocumentParent.h"
 
+#include "mozilla/dom/HTMLAnchorElement.h"
 #include "mozilla/dom/HTMLInputElement.h"
 #include "mozilla/dom/HTMLObjectElement.h"
 #include "mozilla/dom/HTMLSharedElement.h"
@@ -20,7 +21,6 @@
 #include "nsIDOMAttr.h"
 #include "nsIDOMComment.h"
 #include "nsIDOMDocument.h"
-#include "nsIDOMHTMLAnchorElement.h"
 #include "nsIDOMHTMLAreaElement.h"
 #include "nsIDOMHTMLBaseElement.h"
 #include "nsIDOMHTMLCollection.h"
@@ -941,7 +941,7 @@ PersistNodeFixup::FixupNode(nsIDOMNode *aNodeIn,
     }
 
     // Fix up href and file links in the elements
-    nsCOMPtr<nsIDOMHTMLAnchorElement> nodeAsAnchor = do_QueryInterface(aNodeIn);
+    RefPtr<dom::HTMLAnchorElement> nodeAsAnchor = dom::HTMLAnchorElement::FromContent(content);
     if (nodeAsAnchor) {
         rv = GetNodeToFixup(aNodeIn, aNodeOut);
         if (NS_SUCCEEDED(rv) && *aNodeOut) {
