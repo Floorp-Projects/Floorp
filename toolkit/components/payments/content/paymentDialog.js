e522b6fb84175a4c9e6639e24750b03274ed13e3
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2017-09-21 21:35:24 -0700

    Bug 1402210 - Add payment UI documentation and do minor code cleanup. r=marcosc
    MozReview-Commit-ID: Cpn5mKcEZGo
    
    --HG--
    extra : rebase_source : d65efac187f631e0097c300bf0e8fa6f9a78c540

--- toolkit/components/payments/content/paymentDialog.js
+++ toolkit/components/payments/content/paymentDialog.js
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * Runs in the privileged outer dialog. Each dialog loads this script in its
+ * own scope.
+ */
+
 "use strict";
 
 const { classes: Cc, interfaces: Ci, results: Cr, utils: Cu } = Components;
