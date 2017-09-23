d1ac60c027504758a12cf06280daaa4fae2b0f58
Michael Comella <michael.l.comella@gmail.com>
2017-09-22 17:04:53 -0700

    Bug 1402005: Add a learn more link to Top Sites. r=liuche
    Explanation of our motivations are added in code comments.
    
    In testing locally, the link I was sent to was:
    - https://support.mozilla.org/1/mobile/58.0a1/Android/en-US/activity-stream
    
    Existing code does not use a referrer so I didn't add one:
    http://searchfox.org/mozilla-central/rev/56ad02e34d0d36ca4d5ccaa885d26aff270b8ff7/mobile/android/base/java/org/mozilla/gecko/preferences/LinkPreference.java#44
    
    MozReview-Commit-ID: GTY1ku07uWy
    
    --HG--
    extra : rebase_source : fea160fdf5cff0ee5bfb99604b0e5ebe59d86ffb

---
+++ mobile/android/base/java/org/mozilla/gecko/activitystream/homepanel/stream/LearnMoreRow.java
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.activitystream.homepanel.stream;
+
+import android.support.annotation.LayoutRes;
+import android.view.View;
+import org.mozilla.gecko.AppConstants;
+import org.mozilla.gecko.Locales;
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.Tabs;
+
+import java.util.Locale;
+
+/**
+ * A row in Activity Stream with a link to SUMO so that new users to Activity Stream can read about how it works.
+ *
+ * We added this in FF57, when we switched to the new top sites layout, because we were concerned users would be
+ * confused by the new layout or not be able to find new features (e.g. how to disable Pocket). We may remove it
+ * in a future release once users get used to it.
+ *
+ * Note: it's more correct to add this outside of the RecyclerView but we're trying to uplift this and it's a simpler
+ * change to add it directly to the RecyclerView because we can follow the patterns already established for other row
+ * items, rather than fighting with putting a RecyclerView (a scrollable container) inside a ScrollView (ouch) in
+ * order to make sure Learn More is shown.
+ */
+public class LearnMoreRow extends StreamViewHolder {
+
+    public static final @LayoutRes int LAYOUT_ID = R.layout.activity_stream_learn_more_row;
+
+    /**
+     * Template for the learn more link; the String params, in order, are:
+     * - Firefox version
+     * - OS
+     * - locale
+     */
+    private static final String LEARN_MORE_URL_TEMPLATE = "https://support.mozilla.org/1/mobile/%s/%s/%s/activity-stream";
+
+    public LearnMoreRow(final View itemView) {
+        super(itemView);
+
+        final View learnMoreLink = itemView.findViewById(R.id.learn_more_link);
+        learnMoreLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                // todo: add telemetry.
+                Tabs.getInstance().loadUrl(getLearnMoreURL());
+            }
+        });
+    }
+
+    /**
+     * Gets the URL to be opened when "Learn more" is clicked.
+     *
+     * The results of this method should not be cached because it retrieves a locale, which can change.
+     */
+    private static String getLearnMoreURL() {
+        // I figured out which values to use here from GeckoPreferences.
+        final String VERSION = AppConstants.MOZ_APP_VERSION;
+        final String OS = AppConstants.OS_TARGET;
+        final String LOCALE = Locales.getLanguageTag(Locale.getDefault());
+
+        return String.format(LEARN_MORE_URL_TEMPLATE, VERSION, OS, LOCALE);
+    }
+}
