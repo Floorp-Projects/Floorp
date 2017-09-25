611e9eee3ab2e0c9648b9d11b331cf2dad03bf42
Alexandre Poirot <poirot.alex@gmail.com>
2017-09-25 10:15:50 +0200

    Bug 1250832 - Remove DevTools globals from browser.xul scope. r=jryans
    MozReview-Commit-ID: 1kKJrz9iEx0

--- devtools/client/scratchpad/test/head.js
+++ devtools/client/scratchpad/test/head.js
@@ -9,6 +9,7 @@ const {FileUtils} = Cu.import("resource://gre/modules/FileUtils.jsm", {});
 const {console} = Cu.import("resource://gre/modules/Console.jsm", {});
 const {ScratchpadManager} = Cu.import("resource://devtools/client/scratchpad/scratchpad-manager.jsm", {});
 const {require} = Cu.import("resource://devtools/shared/Loader.jsm", {});
+const {gDevTools} = require("devtools/client/framework/devtools");
 const Services = require("Services");
 const DevToolsUtils = require("devtools/shared/DevToolsUtils");
 const flags = require("devtools/shared/flags");
