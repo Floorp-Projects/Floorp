55338abf7a9fd027ccf91bfac6deea0600a77f97
Yura Zenevich <yura.zenevich@gmail.com>
2017-09-25 13:02:57 -0400

    Bug 1401980 - shutdown accessibility service on accessibliity.force_disable pref change. r=surkov
    MozReview-Commit-ID: 8YfCaoB5Stt

--- widget/cocoa/nsChildView.mm
+++ widget/cocoa/nsChildView.mm
@@ -3103,7 +3103,9 @@ nsChildView::GetDocumentAccessible()
   if (!mozilla::a11y::ShouldA11yBeEnabled())
     return nullptr;
 
-  if (mAccessible) {
+  // mAccessible might be dead if accessibility was previously disabled and is
+  // now being enabled again.
+  if (mAccessible && mAccessible->IsAlive()) {
     RefPtr<a11y::Accessible> ret;
     CallQueryReferent(mAccessible.get(),
                       static_cast<a11y::Accessible**>(getter_AddRefs(ret)));
