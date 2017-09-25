f9a099ffec5a684c3b48f4b93af2615e5d643736
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2017-09-25 17:24:26 +0100

    Bug 1402896 - Make the url bar strip javascript even when preceded by control characters. r=mak, a=sledru
    MozReview-Commit-ID: 5ZO8n5lfvnl

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6128,7 +6128,7 @@ function stripUnsafeProtocolOnPaste(pasteData) {
   // LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL for those.
   let changed = false;
   let pasteDataNoJS = pasteData.replace(/\r?\n/g, "")
-                               .replace(/^(?:\s*javascript:)+/i,
+                               .replace(/^(?:\W*javascript:)+/i,
                                         () => {
                                                 changed = true;
                                                 return "";
