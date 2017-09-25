ff75287fb536b2c0cd0cbf933891584fc5b5395f
Shane Caraveo <scaraveo@mozilla.com>
2017-09-25 15:14:09 -0700

    Bug 1401350 - Fix proxy auth for system requests. r=kmag, a=sledru
    MozReview-Commit-ID: CAh89djQobI

--- toolkit/components/extensions/test/xpcshell/test_ext_proxy_auth.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_proxy_auth.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1", "nsIXMLHttpRequest");
+
 const proxy = createHttpServer();
 
 // accept proxy connections for mozilla.org
@@ -8,14 +10,35 @@ proxy.identity.add("http", "mozilla.org", 80);
 proxy.registerPathHandler("/", (request, response) => {
   if (request.hasHeader("Proxy-Authorization")) {
     response.setStatusLine(request.httpVersion, 200, "OK");
-    response.write("ok");
+    response.setHeader("Content-Type", "text/plain", false);
+    response.write("ok, got proxy auth");
   } else {
     response.setStatusLine(request.httpVersion, 407, "Proxy authentication required");
+    response.setHeader("Content-Type", "text/plain", false);
     response.setHeader("Proxy-Authenticate", 'Basic realm="foobar"', false);
-    response.write("ok");
+    response.write("auth required");
   }
 });
 
+function getExtension(background) {
+  return ExtensionTestUtils.loadExtension({
+    manifest: {
+      permissions: [
+        "proxy",
+        "webRequest",
+        "webRequestBlocking",
+        "<all_urls>",
+      ],
+    },
+    background: `(${background})(${proxy.identity.primaryPort})`,
+    files: {
+      "proxy.js": `
+        function FindProxyForURL(url, host) {
+          return "PROXY localhost:${proxy.identity.primaryPort}; DIRECT";
+        }`,
+    },
+  });
+}
 add_task(async function test_webRequest_auth_proxy() {
   async function background(port) {
     browser.webRequest.onBeforeRequest.addListener(details => {
@@ -48,23 +71,7 @@ add_task(async function test_webRequest_auth_proxy() {
     browser.test.sendMessage("pac-ready");
   }
 
-  let handlingExt = ExtensionTestUtils.loadExtension({
-    manifest: {
-      permissions: [
-        "proxy",
-        "webRequest",
-        "webRequestBlocking",
-        "<all_urls>",
-      ],
-    },
-    background: `(${background})(${proxy.identity.primaryPort})`,
-    files: {
-      "proxy.js": `
-        function FindProxyForURL(url, host) {
-          return "PROXY localhost:${proxy.identity.primaryPort}; DIRECT";
-        }`,
-    },
-  });
+  let handlingExt = getExtension(background);
 
   await handlingExt.startup();
   await handlingExt.awaitMessage("pac-ready");
@@ -73,5 +80,49 @@ add_task(async function test_webRequest_auth_proxy() {
 
   await handlingExt.awaitMessage("done");
   await contentPage.close();
+  await handlingExt.unload();
+});
+
+add_task(async function test_webRequest_auth_proxy_system() {
+  async function background(port) {
+    browser.webRequest.onBeforeRequest.addListener(details => {
+      browser.test.fail("onBeforeRequest");
+    }, {urls: ["<all_urls>"]});
+    browser.webRequest.onAuthRequired.addListener(details => {
+      browser.test.sendMessage("onAuthRequired");
+      // cancel is silently ignored, if it were not (e.g someone messes up in
+      // WebRequest.jsm and allows cancel) this test would fail.
+      return {
+        cancel: true,
+        authCredentials: {username: "puser", password: "ppass"},
+      };
+    }, {urls: ["<all_urls>"]}, ["blocking"]);
+
+    await browser.proxy.register("proxy.js");
+    browser.test.sendMessage("pac-ready");
+  }
+
+  let handlingExt = getExtension(background);
+
+  await handlingExt.startup();
+  await handlingExt.awaitMessage("pac-ready");
+
+  function fetch(url) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.mozBackgroundRequest = true;
+      xhr.open("GET", url);
+      xhr.onload = () => { resolve(xhr.responseText); };
+      xhr.onerror = () => { reject(xhr.status); };
+      // use a different contextId to avoid auth cache.
+      xhr.setOriginAttributes({userContextId: 1});
+      xhr.send();
+    });
+  }
+
+  await Promise.all([
+    handlingExt.awaitMessage("onAuthRequired"),
+    fetch("http://mozilla.org"),
+  ]);
   await handlingExt.unload();
 });
