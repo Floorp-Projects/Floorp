0e3f381e83706cf49206aa9bff7a8d25d710e760
Morris Tseng <mtseng@mozilla.com>
2017-09-25 11:19:12 +0800

    Bug 1403459 - Passing transform-style from display item directly. r=kats
    MozReview-Commit-ID: JVhjSFmvnAq

--- gfx/layers/wr/StackingContextHelper.h
+++ gfx/layers/wr/StackingContextHelper.h
@@ -37,7 +37,8 @@ public:
                         gfx::Matrix4x4* aTransformPtr = nullptr,
                         gfx::Matrix4x4* aPerspectivePtr = nullptr,
                         const gfx::CompositionOp& aMixBlendMode = gfx::CompositionOp::OP_OVER,
-                        bool aBackfaceVisible = true);
+                        bool aBackfaceVisible = true,
+                        bool aIsPreserve3D = false);
   // This version of the constructor should only be used at the root level
   // of the tree, so that we have a StackingContextHelper to pass down into
   // the RenderLayer traversal, but don't actually want it to push a stacking
