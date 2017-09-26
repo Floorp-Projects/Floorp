5d0c1f64ce5a0bca5ca425aeaa981c09bd7774df
Brian Hackett <bhackett1024@gmail.com>
2017-09-26 14:12:25 -0400

    Backed out changeset 072f8d4a9964 (bug 1355109) for causing crashes with various extensions. a=lizzard

--- js/src/jsfriendapi.cpp
+++ js/src/jsfriendapi.cpp
@@ -1370,20 +1370,6 @@ js::GetDOMProxyShadowsCheck()
     return gDOMProxyShadowsCheck;
 }
 
-static XrayJitInfo* gXrayJitInfo = nullptr;
-
-JS_FRIEND_API(void)
-js::SetXrayJitInfo(XrayJitInfo* info)
-{
-    gXrayJitInfo = info;
-}
-
-XrayJitInfo*
-js::GetXrayJitInfo()
-{
-    return gXrayJitInfo;
-}
-
 bool
 js::detail::IdMatchesAtom(jsid id, JSAtom* atom)
 {
