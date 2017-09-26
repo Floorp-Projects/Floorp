937c786bbf1ee0853f6d3bafb49f1f379adf1687
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-26 10:38:43 +0200

    Backed out changeset db56e6da54d3 (bug 1402824)

--- xpcom/threads/CooperativeThreadPool.h
+++ xpcom/threads/CooperativeThreadPool.h
@@ -10,11 +10,8 @@
 #include "mozilla/Array.h"
 #include "mozilla/CondVar.h"
 #include "mozilla/Mutex.h"
-#include "mozilla/ThreadLocal.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Variant.h"
-#include "nsCOMPtr.h"
-#include "nsThreadUtils.h"
 #include "prthread.h"
 
 // Windows silliness. winbase.h defines an empty no-argument Yield macro.
