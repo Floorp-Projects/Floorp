190c3906d3598561e171df1b63bbc43c30a03335
Jessica Jong <jjong@mozilla.com>
2017-09-26 13:55:58 +0800

    Bug 1334043 - Part 1: Replace attached callback (v0) with connected callback (v1). r=smaug
    MozReview-Commit-ID: 3qQh4viyvBX

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -1621,13 +1621,12 @@ Element::BindToTree(nsIDocument* aDocument, nsIContent* aParent,
   }
 
   nsIDocument* composedDoc = GetComposedDoc();
-  if (composedDoc) {
-    // Attached callback must be enqueued whenever custom element is inserted into a
-    // document and this document has a browsing context.
-    if (GetCustomElementData() && composedDoc->GetDocShell()) {
-      // Enqueue an attached callback for the custom element.
+  if (CustomElementRegistry::IsCustomElementEnabled() && composedDoc) {
+    // Connected callback must be enqueued whenever a custom element becomes
+    // connected.
+    if (GetCustomElementData()) {
       nsContentUtils::EnqueueLifecycleCallback(
-        composedDoc, nsIDocument::eAttached, this);
+        composedDoc, nsIDocument::eConnected, this);
     }
   }
 
@@ -2625,7 +2624,7 @@ Element::SetAttrAndNotify(int32_t aNamespaceID,
     }
   }
 
-  if (nsContentUtils::IsWebComponentsEnabled()) {
+  if (CustomElementRegistry::IsCustomElementEnabled()) {
     if (CustomElementData* data = GetCustomElementData()) {
       if (CustomElementDefinition* definition =
             nsContentUtils::GetElementDefinitionIfObservingAttr(this,
@@ -2929,7 +2928,7 @@ Element::UnsetAttr(int32_t aNameSpaceID, nsIAtom* aName,
     }
   }
 
-  if (nsContentUtils::IsWebComponentsEnabled()) {
+  if (CustomElementRegistry::IsCustomElementEnabled()) {
     if (CustomElementData* data = GetCustomElementData()) {
       if (CustomElementDefinition* definition =
             nsContentUtils::GetElementDefinitionIfObservingAttr(this,
