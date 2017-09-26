99aa8f99759d07005bb86c03262434a14baca535
Steve Armand <stevea1@mac.com>
2017-09-25 22:57:18 -0400

    Bug 1397849 - Enable py2 and py3 linter on testing/mozbase. r=ahal
    MozReview-Commit-ID: GnaVLhtO4un

--- testing/mozbase/mozdevice/adb_tests/test_device_running_adb_as_root.py
+++ testing/mozbase/mozdevice/adb_tests/test_device_running_adb_as_root.py
@@ -5,6 +5,9 @@
  Running this test case requires various reboots which makes it a
  very slow test case to run.
 """
+
+from __future__ import absolute_import, print_function
+
 import unittest
 import sys
 
@@ -37,13 +40,13 @@ class TestFileOperations(unittest.TestCase):
 if __name__ == "__main__":
     dm = DeviceManagerADB()
     if not dm.devices():
-        print "There are no connected adb devices"
+        print("There are no connected adb devices")
         sys.exit(1)
     else:
         if not (int(dm._runCmd(["shell", "getprop", "ro.secure"]).output[0]) and
                 int(dm._runCmd(["shell", "getprop", "ro.debuggable"]).output[0])):
-            print "This test case is meant for devices with devices that start " \
-                "adbd as non-root and allows for adbd to be restarted as root."
+            print("This test case is meant for devices with devices that start "
+                  "adbd as non-root and allows for adbd to be restarted as root.")
             sys.exit(1)
 
     unittest.main()
