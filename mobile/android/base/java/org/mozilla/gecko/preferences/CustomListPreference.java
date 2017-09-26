520ec8f10c24e060874f9e152fb7890723c70242
Michael Comella <michael.l.comella@gmail.com>
2017-09-26 16:23:59 -0700

    Bug 1403347 - Don't cache CustomListPreference dialog items. r=liuche, a=sledru
    We're returning a list of only a few items that, at worst, reads from
    resources and is infrequently accessed: there is no reason to cache these
    values and the bugs, like this one, that caches entail.
    
    At the end of this patch, there's no crash, but the scrolling behavior isn't
    great: that's bug 1403139.
    
    MozReview-Commit-ID: 3zoXWk78cM4

--- mobile/android/base/java/org/mozilla/gecko/preferences/CustomListPreference.java
+++ mobile/android/base/java/org/mozilla/gecko/preferences/CustomListPreference.java
@@ -97,13 +97,6 @@ public abstract class CustomListPreference extends Preference implements View.On
         }
     }
 
-    private String[] getCachedDialogItems() {
-        if (mDialogItems == null) {
-            mDialogItems = createDialogItems();
-        }
-        return mDialogItems;
-    }
-
     /**
      * Returns the strings to be displayed in the dialog.
      */
@@ -115,7 +108,7 @@ public abstract class CustomListPreference extends Preference implements View.On
     public void showDialog() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setTitle(getTitle().toString());
-        builder.setItems(getCachedDialogItems(), new DialogInterface.OnClickListener() {
+        builder.setItems(createDialogItems(), new DialogInterface.OnClickListener() {
             // Forward relevant events to the container class for handling.
             @Override
             public void onClick(DialogInterface dialog, int indexClicked) {
