14713d1d4b308f4e2f5cbdd85dcdfe727db96394
Brian Hackett <bhackett1024@gmail.com>
2017-09-26 11:48:55 -0400

    Backed out changeset 072f8d4a9964 (bug 1355109) for causing crashes with various extensions. a=lizzard

--- js/xpconnect/src/XPCJSRuntime.cpp
+++ js/xpconnect/src/XPCJSRuntime.cpp
@@ -2844,7 +2844,6 @@ XPCJSRuntime::Initialize(JSContext* cx)
     js::SetPreserveWrapperCallback(cx, PreserveWrapper);
     JS_SetAccumulateTelemetryCallback(cx, AccumulateTelemetryCallback);
     js::SetWindowProxyClass(cx, &OuterWindowProxyClass);
-    js::SetXrayJitInfo(&gXrayJitInfo);
     JS::SetProcessLargeAllocationFailureCallback(OnLargeAllocationFailureCallback);
 
     // The JS engine needs to keep the source code around in order to implement
