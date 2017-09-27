d47f93acf2b4019145a013ab7ec482d18aff2b34
Marina Samuel <msamuel@mozilla.com>
2017-09-27 16:29:06 -0400

    Bug 1403695: Send a generic health telemetry ping through Ping Centre for Activity Stream. r=k88hudson
    MozReview-Commit-ID: CJGX1aihFuV

--- browser/components/tests/unit/head.js
+++ browser/components/tests/unit/head.js
@@ -6,4 +6,13 @@ const {interfaces: Ci, classes: Cc, results: Cr, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
 
+// ================================================
+// Load mocking/stubbing library, sinon
+// docs: http://sinonjs.org/releases/v2.3.2/
+/* exported sinon */
+Cu.import("resource://gre/modules/Timer.jsm");
+Services.scriptloader.loadSubScript("resource://testing-common/sinon-2.3.2.js", this);
+/* globals sinon */
+// ================================================
+
 var gProfD = do_get_profile().QueryInterface(Ci.nsIFile);
