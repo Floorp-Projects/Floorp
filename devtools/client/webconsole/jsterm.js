dad876af8a463e57139a9a3f425663742bf86ca7
abhinav <abhinav.koppula@gmail.com>
2017-09-28 00:12:00 +0530

    Bug 1385995 - Adding `Copy object` to the context menu which allows to copy the object/variable logged to the console. r=nchevobbe
    MozReview-Commit-ID: EbOE9qrpikn

--- devtools/client/webconsole/jsterm.js
+++ devtools/client/webconsole/jsterm.js
@@ -549,6 +549,21 @@ JSTerm.prototype = {
   },
 
   /**
+   * Copy the object/variable by invoking the server
+   * which invokes the `copy(variable)` command and makes it
+   * available in the clipboard
+   * @param evalString - string which has the evaluation string to be copied
+   * @param options - object - Options for evaluation
+   * @return object
+   *         A promise object that is resolved when the server response is
+   *         received.
+   */
+  copyObject: function (evalString, evalOptions) {
+    return this.webConsoleClient.evaluateJSAsync(`copy(${evalString})`,
+      null, evalOptions);
+  },
+
+  /**
    * Retrieve the FrameActor ID given a frame depth.
    *
    * @param number frame
