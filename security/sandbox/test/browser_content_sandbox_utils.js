9a88df4221a0011f938b6c40f997d377439800fc
Haik Aftandilian <haftandilian@mozilla.com>
2017-09-27 16:01:57 -0700

    Bug 1393805 - Part 5 - Test that the system extensions dev dir is readable from content. r=bobowen
    MozReview-Commit-ID: 7YN7S7R39CU
    
    --HG--
    extra : rebase_source : 01e3fe0acb051723219d9d5de5b1fd19d9751c34

--- security/sandbox/test/browser_content_sandbox_utils.js
+++ security/sandbox/test/browser_content_sandbox_utils.js
@@ -71,6 +71,10 @@ function GetHomeDir() {
   return (homeDir);
 }
 
+function GetSystemExtensionsDevDir() {
+  return Services.dirsvc.get("XRESysExtDev", Ci.nsIFile);
+}
+
 function GetPerUserExtensionDir() {
   return Services.dirsvc.get("XREUSysExt", Ci.nsIFile);
 }
