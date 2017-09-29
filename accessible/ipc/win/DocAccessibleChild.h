18fc56c85b2e730bd0eba4b590e7fa75ba45f959
Alexander Surkov <surkov.alexander@gmail.com>
2017-09-29 15:28:23 -0400

    Bug 1402951 - no show events for content on document load complete, r=aklotz

--- accessible/ipc/win/DocAccessibleChild.h
+++ accessible/ipc/win/DocAccessibleChild.h
@@ -111,7 +111,8 @@ private:
     SerializedShow(DocAccessibleChild* aTarget,
                    ShowEventData& aEventData, bool aFromUser)
       : DeferredEvent(aTarget)
-      , mEventData(aEventData.ID(), aEventData.Idx(), nsTArray<AccessibleData>())
+      , mEventData(aEventData.ID(), aEventData.Idx(),
+                   nsTArray<AccessibleData>(), aEventData.EventSuppressed())
       , mFromUser(aFromUser)
     {
       // Since IPDL doesn't generate a move constructor for ShowEventData,
