5726ada8a0a74a73ed61c62ea61ee6389d870e4d
Masayuki Nakano <masayuki@d-toybox.com>
2017-09-29 15:15:14 +0900

    Bug 1404206 - Part 2: Make TSFTextStore and IMEHandler handle "mozAwesomebar" inputmode value. r=m_kato, r=gijs
    When "mozAwesomebar" is set to inputmode value, that means that the Smart
    Location Bar gets focus.  In that case, we should notify IME of input scopes
    as "URL" because on-screen keyboard for URL has some useful additional keys
    but they are not hindrances even when users want to type non-URL text.
    
    On the other hand, MS-IME for Japanese and Google Japanese Input changes their
    open state to "closed" if we notify them of URL input scope.  A lot of users
    complain about this behavior.  Therefore, we should notify only them of
    "Default" input scope even when "mozAwesomebar" has focus.
    
    MozReview-Commit-ID: DIgqpR7TXQx

--- widget/windows/TSFTextStore.h
+++ widget/windows/TSFTextStore.h
@@ -251,6 +251,14 @@ public:
   static bool IsMSJapaneseIMEActive();
 
   /**
+   * Returns true if active TIP is Google Japanese Input.
+   * Note that if Google Japanese Input is installed as an IMM-IME,
+   * this return false even if Google Japanese Input is active.
+   * So, you may need to check IMMHandler::IsGoogleJapaneseInputActive() too.
+   */
+  static bool IsGoogleJapaneseInputActive();
+
+  /**
    * Returns true if TSF may crash if GetSelection() returns E_FAIL.
    */
   static bool DoNotReturnErrorFromGetSelection();
