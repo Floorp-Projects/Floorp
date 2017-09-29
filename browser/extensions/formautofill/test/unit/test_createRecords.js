957f57bd8fd2838d91e20e0e82543671481ccc38
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-29 09:43:51 +0200

    Backed out changeset 130a44290c28 (bug 1401411) for eslint failure at browser/extensions/formautofill/test/unit/test_createRecords.js:207:8 | Missing trailing comma. r=backout

--- browser/extensions/formautofill/test/unit/test_createRecords.js
+++ browser/extensions/formautofill/test/unit/test_createRecords.js
@@ -26,19 +26,19 @@ const TESTCASES = [
     description: "\"country\" using @autocomplete shouldn't be identified aggressively",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
-                <input id="organization" autocomplete="organization">
+                <input id="family-name" autocomplete="family-name">
                 <input id="country" autocomplete="country">
                </form>`,
     formValue: {
       "given-name": "John",
-      "organization": "Mozilla",
-      "country": "United States",
+      "family-name": "Doe",
+      country: "United States",
     },
     expectedRecord: {
       address: {
         "given-name": "John",
-        "organization": "Mozilla",
-        "country": "United States",
+        "family-name": "Doe",
+        country: "United States",
       },
     },
   },
@@ -46,19 +46,19 @@ const TESTCASES = [
     description: "\"country\" using heuristics should be identified aggressively",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
-                <input id="organization" autocomplete="organization">
+                <input id="family-name" autocomplete="family-name">
                 <input id="country" name="country">
                </form>`,
     formValue: {
       "given-name": "John",
-      "organization": "Mozilla",
-      "country": "United States",
+      "family-name": "Doe",
+      country: "United States",
     },
     expectedRecord: {
       address: {
         "given-name": "John",
-        "organization": "Mozilla",
-        "country": "US",
+        "family-name": "Doe",
+        country: "US",
       },
     },
   },
@@ -66,20 +66,20 @@ const TESTCASES = [
     description: "\"tel\" related fields should be concatenated",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
-                <input id="organization" autocomplete="organization">
+                <input id="family-name" autocomplete="family-name">
                 <input id="tel-country-code" autocomplete="tel-country-code">
                 <input id="tel-national" autocomplete="tel-national">
                </form>`,
     formValue: {
       "given-name": "John",
-      "organization": "Mozilla",
+      "family-name": "Doe",
       "tel-country-code": "+1",
       "tel-national": "1234567890",
     },
     expectedRecord: {
       address: {
         "given-name": "John",
-        "organization": "Mozilla",
+        "family-name": "Doe",
         "tel": "+11234567890",
       },
     },
@@ -88,21 +88,21 @@ const TESTCASES = [
     description: "\"tel\" should be removed if it's too short",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
+                <input id="family-name" autocomplete="family-name">
                 <input id="organization" autocomplete="organization">
-                <input id="country" autocomplete="country">
                 <input id="tel" autocomplete="tel-national">
                </form>`,
     formValue: {
       "given-name": "John",
+      "family-name": "Doe",
       "organization": "Mozilla",
-      "country": "United States",
       "tel": "1234",
     },
     expectedRecord: {
       address: {
         "given-name": "John",
+        "family-name": "Doe",
         "organization": "Mozilla",
-        "country": "United States",
         "tel": "",
       },
     },
@@ -111,21 +111,21 @@ const TESTCASES = [
     description: "\"tel\" should be removed if it's too long",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
+                <input id="family-name" autocomplete="family-name">
                 <input id="organization" autocomplete="organization">
-                <input id="country" autocomplete="country">
                 <input id="tel" autocomplete="tel-national">
                </form>`,
     formValue: {
       "given-name": "John",
+      "family-name": "Doe",
       "organization": "Mozilla",
-      "country": "United States",
       "tel": "1234567890123456",
     },
     expectedRecord: {
       address: {
         "given-name": "John",
+        "family-name": "Doe",
         "organization": "Mozilla",
-        "country": "United States",
         "tel": "",
       },
     },
@@ -134,121 +134,23 @@ const TESTCASES = [
     description: "\"tel\" should be removed if it contains invalid characters",
     document: `<form>
                 <input id="given-name" autocomplete="given-name">
-                <input id="organization" autocomplete="organization">
-                <input id="country" autocomplete="country">
-                <input id="tel" autocomplete="tel-national">
-               </form>`,
-    formValue: {
-      "given-name": "John",
-      "organization": "Mozilla",
-      "country": "United States",
-      "tel": "12345###!!!",
-    },
-    expectedRecord: {
-      address: {
-        "given-name": "John",
-        "organization": "Mozilla",
-        "country": "United States",
-        "tel": "",
-      },
-    },
-  },
-  {
-    description: "All name related fields should be counted as 1 field only.",
-    document: `<form>
-                <input id="given-name" autocomplete="given-name">
                 <input id="family-name" autocomplete="family-name">
                 <input id="organization" autocomplete="organization">
+                <input id="tel" autocomplete="tel-national">
                </form>`,
     formValue: {
       "given-name": "John",
       "family-name": "Doe",
       "organization": "Mozilla",
+      "tel": "12345###!!!",
     },
     expectedRecord: {
-      address: undefined,
-    },
-  },
-  {
-    description: "All telephone related fields should be counted as 1 field only.",
-    document: `<form>
-                <input id="tel-country-code" autocomplete="tel-country-code">
-                <input id="tel-area-code" autocomplete="tel-area-code">
-                <input id="tel-local" autocomplete="tel-local">
-                <input id="organization" autocomplete="organization">
-               </form>`,
-    formValue: {
-      "tel-country-code": "+1",
-      "tel-area-code": "123",
-      "tel-local": "4567890",
-      "organization": "Mozilla",
-    },
-    expectedRecord: {
-      address: undefined,
-    },
-  },
-  {
-    description: "A credit card form with the value of cc-number, cc-exp, and cc-name.",
-    document: `<form>
-                <input id="cc-number" autocomplete="cc-number">
-                <input id="cc-name" autocomplete="cc-name">
-                <input id="cc-exp" autocomplete="cc-exp">
-               </form>`,
-    formValue: {
-      "cc-number": "4444000022220000",
-      "cc-name": "Foo Bar",
-      "cc-exp": "2022-06",
-    },
-    expectedRecord: {
-      creditCard: {
-        "cc-number": "4444000022220000",
-        "cc-name": "Foo Bar",
-        "cc-exp": "2022-06",
-      }
-    },
-  },
-  {
-    description: "A credit card form with cc-number value only.",
-    document: `<form>
-                <input id="cc-number" autocomplete="cc-number">
-               </form>`,
-    formValue: {
-      "cc-number": "4444000022220000",
-    },
-    expectedRecord: {
-      creditCard: {
-        "cc-number": "4444000022220000",
+      address: {
+        "given-name": "John",
+        "family-name": "Doe",
+        "organization": "Mozilla",
+        "tel": "",
       },
-    },
-  },
-  {
-    description: "A credit card form must have cc-number value.",
-    document: `<form>
-                <input id="cc-number" autocomplete="cc-number">
-                <input id="cc-name" autocomplete="cc-name">
-                <input id="cc-exp" autocomplete="cc-exp">
-               </form>`,
-    formValue: {
-      "cc-number": "",
-      "cc-name": "Foo Bar",
-      "cc-exp": "2022-06",
-    },
-    expectedRecord: {
-      creditCard: undefined,
-    },
-  },
-  {
-    description: "A credit card form must have cc-number field.",
-    document: `<form>
-                <input id="cc-name" autocomplete="cc-name">
-                <input id="cc-exp" autocomplete="cc-exp">
-               </form>`,
-    formValue: {
-      "cc-name": "Foo Bar",
-      "cc-exp": "2022-06",
-    },
-    expectedRecord: {
-      creditCard: undefined,
     },
   },
 ];
