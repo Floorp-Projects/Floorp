fa3775306488fd0be9fa45b3a1fb25a901e3f172
Haik Aftandilian <haftandilian@mozilla.com>
2017-09-29 12:44:22 -0700

    Bug 1403744 - Part 2 - Test that the per-user extensions dir is readable from content on Windows. r=bobowen
    MozReview-Commit-ID: 7YN7S7R39CU
    
    --HG--
    extra : rebase_source : c86998b1738ee1f4d24562105acf63c20811b8a1

--- security/sandbox/test/browser_content_sandbox_fs.js
+++ security/sandbox/test/browser_content_sandbox_fs.js
@@ -405,6 +405,17 @@ async function testFileAccess() {
     });
   }
 
+  if (isWin()) {
+    let extDir = GetPerUserExtensionDir();
+    tests.push({
+      desc:       "per-user extensions dir",
+      ok:         true,
+      browser:    webBrowser,
+      file:       extDir,
+      minLevel:   minHomeReadSandboxLevel(),
+    });
+  }
+
   if (isMac()) {
     // If ~/Library/Caches/TemporaryItems exists, when level <= 2 we
     // make sure it's readable. For level 3, we make sure it isn't.
