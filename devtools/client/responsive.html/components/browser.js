5d940bbf9ded5920c00f336c83b0df0f3deb94cf
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-09-29 10:45:04 +0200

    Backed out changeset 5a0227d048e8 (bug 1305777) for failing devtools' devtools/client/styleeditor/test/browser_styleeditor_media_sidebar_links.js on Windows 7 debug without e10s. r=backout
    --HG--
    rename : devtools/client/responsive.html/browser/content.js => devtools/client/responsivedesign/responsivedesign-child.js

--- devtools/client/responsive.html/components/browser.js
+++ devtools/client/responsive.html/components/browser.js
@@ -15,8 +15,6 @@ const { DOM: dom, createClass, addons, PropTypes } =
 const e10s = require("../utils/e10s");
 const message = require("../utils/message");
 
-const FRAME_SCRIPT = "resource://devtools/client/responsive.html/browser/content.js";
-
 module.exports = createClass({
 
   /**
@@ -85,7 +83,8 @@ module.exports = createClass({
     e10s.on(mm, "OnContentResize", onContentResize);
 
     let ready = e10s.once(mm, "ChildScriptReady");
-    mm.loadFrameScript(FRAME_SCRIPT, true);
+    mm.loadFrameScript("resource://devtools/client/responsivedesign/" +
+                       "responsivedesign-child.js", true);
     yield ready;
 
     let browserWindow = getToplevelWindow(window);
