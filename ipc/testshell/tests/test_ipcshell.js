256f12363f59b510a5455f2d06cc5e6106583eb6
Andrew McCreight <continuation@gmail.com>
2017-09-29 09:46:32 -0700

    Bug 1403961, part 2 - Fix and enable eslint for ipc/. r=billm
    After the previous patch, all that is needed to pass eslint is some
    whitespace fixes I generated using:
      ./mach eslint --fix ipc
    
    The .eslintrc.js file makes eslint expect XPCShell global variables.
    
    With those two changes, eslint can be enabled for the ipc/ directory.
    
    MozReview-Commit-ID: BqSICp2iV6O
    
    --HG--
    extra : rebase_source : ccab8be9fc1ae1116ae55631d3978c39d35cad05

--- ipc/testshell/tests/test_ipcshell.js
+++ ipc/testshell/tests/test_ipcshell.js
@@ -18,14 +18,14 @@ function run_test() {
   sendCommand("runtime.processType;", callback);
 
   [ [ "C", "D" ], [ "D", "C" ], [ "\u010C", "D" ], [ "D", "\u010C" ] ].forEach(
-    function (pair) {
+    function(pair) {
       do_test_pending();
       var cmp = pair[0].localeCompare(pair[1]);
       sendCommand(
-          "'"+ pair[0] +"'.localeCompare('"+ pair[1] +"');",
-          function (result) {
+          "'" + pair[0] + "'.localeCompare('" + pair[1] + "');",
+          function(result) {
               do_check_eq(cmp, result);
-              do_test_finished(); 
+              do_test_finished();
           });
     })
 }
