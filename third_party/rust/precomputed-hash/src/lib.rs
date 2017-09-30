60e5ee69b6c008e58d47c039403e8f1d9b7bcb96
Simon Sapin <simon.sapin@exyr.org>
2017-09-30 17:26:05 +0200

    Bug 1404625 - Revendor Rust dependencies r=SimonSapin
    MozReview-Commit-ID: 5NZ5UFJThSx

--- third_party/rust/precomputed-hash/src/lib.rs
+++ third_party/rust/precomputed-hash/src/lib.rs
@@ -7,3 +7,16 @@ pub trait PrecomputedHash {
     /// Return the precomputed hash for this item.
     fn precomputed_hash(&self) -> u32;
 }
+
+// These are equivalent to the `std::Hash` impls.
+impl<'a, T: PrecomputedHash> PrecomputedHash for &'a T {
+    fn precomputed_hash(&self) -> u32 {
+        (**self).precomputed_hash()
+    }
+}
+
+impl<'a, T: PrecomputedHash> PrecomputedHash for &'a mut T {
+    fn precomputed_hash(&self) -> u32 {
+        (**self).precomputed_hash()
+    }
+}
