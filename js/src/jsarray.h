0e2fd8b57abd8878c40d0e8f026f73365ae923d6
Jan de Mooij <jdemooij@mozilla.com>
2017-10-01 11:37:00 +0200

    Bug 1398768 part 10 - Remove and clean up more code. r=evilpie

--- js/src/jsarray.h
+++ js/src/jsarray.h
@@ -79,8 +79,6 @@ NewDenseFullyAllocatedArrayWithTemplate(JSContext* cx, uint32_t length, JSObject
 extern ArrayObject*
 NewDenseCopyOnWriteArray(JSContext* cx, HandleArrayObject templateObject, gc::InitialHeap heap);
 
-// The methods below can create either boxed or unboxed arrays.
-
 extern ArrayObject*
 NewFullyAllocatedArrayTryUseGroup(JSContext* cx, HandleObjectGroup group, size_t length,
                                   NewObjectKind newKind = GenericObject);
