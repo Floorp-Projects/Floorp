45b2547e1136d308f3ff6f372d8f73f19292e980
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-02 19:57:06 +0200

    Backed out changeset 436ecd081ffa (bug 1401379)

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -3601,6 +3601,8 @@ nsGlobalWindow::DetachFromDocShell()
     mContext = nullptr;
   }
 
+  mDocShell = nullptr; // Weak Reference
+
   NS_ASSERTION(!mNavigator, "Non-null mNavigator in outer window!");
 
   if (mFrames) {
