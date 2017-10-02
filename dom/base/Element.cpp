4322c150abc524eed728590e5ea6c70cfce02c50
Emilio Cobos Álvarez <emilio@crisal.io>
2017-10-02 08:50:20 +0200

    Bug 1404134: Drop assertion that can be hit mid-unbind. r=heycam
    On that test-case we have something like:
    
      <body restyle-root>
        <element dirty-descendants>
          <element-2>
        </element>
      </body>
    
    If we change the state of element-2 after unbinding the <body>, which clears the
    body's flags, but not the children's flags, there'll be no restyle root, even
    though our parent has the relevant flags anyway.
    
    This doesn't compromise correctness in any way, so I'd rather drop the assertion
    that shuffle a bunch of stuff so it holds.
    
    MozReview-Commit-ID: KFVTkXclKST

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -4425,7 +4425,6 @@ NoteDirtyElement(Element* aElement, uint32_t aBits)
     // Similarly, if our parent already has the bit we're propagating, we can
     // assume everything is already set up.
     if (parent->HasAllFlags(aBits)) {
-      MOZ_ASSERT(aElement->GetComposedDoc()->GetServoRestyleRoot());
       return;
     }
 
