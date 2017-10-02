4b193ecf70ea06ba7f8540dc7c4ff38930f1a141
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-02 19:57:00 +0200

    Backed out changeset 318b85f85682 (bug 1401379)

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -3861,7 +3861,7 @@ nsGlobalWindow::ConfirmDialogIfNeeded()
 {
   MOZ_ASSERT(IsOuterWindow());
 
-  NS_ENSURE_TRUE(!IsCleanedUp(), false);
+  NS_ENSURE_TRUE(mDocShell, false);
   nsCOMPtr<nsIPromptService> promptSvc =
     do_GetService("@mozilla.org/embedcomp/prompt-service;1");
 
@@ -4766,12 +4766,6 @@ nsGlobalWindow::GetParentOuter()
 {
   MOZ_RELEASE_ASSERT(IsOuterWindow());
 
-  // If we've been cleaned up, we shouldn't report that we have a parent
-  // anymore.
-  if (mCleanedUp) {
-    return nullptr;
-  }
-
   if (!mDocShell) {
     return nullptr;
   }
@@ -5115,7 +5109,7 @@ nsGlobalWindow::GetClosedOuter()
   MOZ_RELEASE_ASSERT(IsOuterWindow());
 
   // If someone called close(), or if we don't have a docshell, we're closed.
-  return mIsClosed || mCleanedUp;
+  return mIsClosed || !mDocShell;
 }
 
 bool
@@ -5454,12 +5448,6 @@ nsGlobalWindow::GetSanitizedOpener(nsPIDOMWindowOuter* aOpener)
 
   // First, ensure that we're not handing back a chrome window to content:
   if (win->IsChromeWindow()) {
-    return nullptr;
-  }
-
-  // If we or the opener have been cleaned up we should report a null as our
-  // opener.
-  if (mCleanedUp || win->mCleanedUp) {
     return nullptr;
   }
 
@@ -9392,7 +9380,7 @@ nsGlobalWindow::CloseOuter(bool aTrustedCaller)
 {
   MOZ_RELEASE_ASSERT(IsOuterWindow());
 
-  if (mCleanedUp || !mDocShell || IsInModalState() ||
+  if (!mDocShell || IsInModalState() ||
       (IsFrame() && !mDocShell->GetIsMozBrowser())) {
     // window.close() is called on a frame in a frameset, on a window
     // that's already closed, or on a window for which there's
@@ -9482,7 +9470,7 @@ nsGlobalWindow::ForceClose()
   MOZ_ASSERT(IsOuterWindow());
   MOZ_ASSERT(XRE_GetProcessType() == GeckoProcessType_Default);
 
-  if (IsFrame() || mCleanedUp) {
+  if (IsFrame() || !mDocShell) {
     // This may be a frame in a frameset, or a window that's already closed.
     // Ignore such calls.
     return;
