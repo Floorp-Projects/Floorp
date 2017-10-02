ea2566c47ab8e7b20e0d1526c9396a6c301cc5b6
Nick Alexander <nalexander@mozilla.com>
2017-10-02 13:27:51 -0700

    Bug 1352599 - Part 1: Add a Proguard toolchain task for Android builds. r=froydnj
    MozReview-Commit-ID: 5lyHGLkzyg

---
+++ taskcluster/scripts/misc/repack-proguard-jar.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x -e -v
+
+# This script is for repacking Proguard to get the `proguard.jar`
+# needed to shrink Android packages.
+
+WORKSPACE=$HOME/workspace
+STAGE=$WORKSPACE/proguard
+UPLOAD_DIR=$HOME/artifacts
+
+VERSION=5.3.3
+URL=https://newcontinuum.dl.sourceforge.net/project/proguard/proguard/5.3/proguard$VERSION.tar.gz
+ARCHIVE=proguard$VERSION.tar.gz
+DIR=proguard$VERSION
+SHA256SUM=95bf9580107f00d0e26f01026dcfe9e7a772e5449488b03ba832836c3760b3af
+
+mkdir -p $UPLOAD_DIR $STAGE
+
+cd $WORKSPACE
+wget $URL
+echo "$SHA256SUM  $ARCHIVE" | sha256sum -c -
+
+# Just the file we need.
+tar zxvf $ARCHIVE '*/proguard.jar'
+
+# The archive is to satisfy source distribution requirements.
+mv $ARCHIVE $UPLOAD_DIR
+
+# This leaves us with $STAGE/lib/proguard.jar.
+mv $DIR/lib $STAGE
+
+cat >$STAGE/README<<EOF
+proguard.jar extracted from ${URL}.
+That archive, which includes source, is available as a taskcluster artifact:
+https://queue.taskcluster.net/v1/task/$TASK_ID/artifacts/public/$ARCHIVE
+EOF
+tar cf - -C $WORKSPACE `basename $STAGE` | xz > $UPLOAD_DIR/proguard-jar.tar.xz
