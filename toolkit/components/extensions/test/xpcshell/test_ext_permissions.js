c9501dcdfa01edc05d4c33f98736878a496d6ebd
Andrew Swan <aswan@mozilla.com>
2017-10-02 14:49:48 -0700

    Bug 1399550 - Explicitly close content page in test_ext_permissions.js. r=kmag, a=test-only
    MozReview-Commit-ID: HnTbRVHXkWR

--- toolkit/components/extensions/test/xpcshell/test_ext_permissions.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_permissions.js
@@ -337,7 +337,7 @@ add_task(async function test_alreadyGranted() {
 
   await withHandlingUserInput(extension, async () => {
     let url = await extension.awaitMessage("ready");
-    await ExtensionTestUtils.loadContentPage(url, {extension});
+    let page = await ExtensionTestUtils.loadContentPage(url, {extension});
     await extension.awaitMessage("page-ready");
 
     async function checkRequest(arg, expectPrompt, msg) {
@@ -372,6 +372,7 @@ add_task(async function test_alreadyGranted() {
                        "already granted optional wildcard origin");
     await checkRequest({origins: ["http://host.optional-domain.com/"]}, false,
                        "host matching optional wildcard origin");
+    await page.close();
   });
 
   await extension.unload();
