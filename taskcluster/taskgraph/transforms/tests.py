95c011a9f085b928d3f85f25991557f0be80a5de
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-02 16:03:12 +0200

    Backed out changeset ddfc127526b2 (bug 1403222) for failing python/mozbuild/mozbuild/test/frontend/test_reader.py::TestBuildReader::test_schedules, at least for Linux asan opt builds. r=backout on a CLOSED TREE

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -939,7 +939,7 @@ def make_job_description(config, tests):
         if test.get('when'):
             jobdesc['when'] = test['when']
         else:
-            schedules = [suite, platform_family(test['build-platform'])]
+            schedules = [platform_family(test['build-platform'])]
             if config.params['project'] != 'try':
                 # for non-try branches, include SETA
                 jobdesc['optimization'] = {'skip-unless-schedules-or-seta': schedules}
