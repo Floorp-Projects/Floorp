2dbbc650a44e370ebba7320b6df6686d472d88d9
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-04 11:24:36 +0200

    Backed out changeset 9389dd982590 (bug 1404897)

--- xpcom/ds/nsBaseHashtable.h
+++ xpcom/ds/nsBaseHashtable.h
@@ -128,9 +128,10 @@ public:
   }
 
   /**
-   * Put a new value for the associated key
+   * put a new value for the associated key
    * @param aKey the key to put
    * @param aData the new data
+   * @return always true, unless memory allocation failed
    */
   void Put(KeyType aKey, const UserDataType& aData)
   {
@@ -148,30 +149,6 @@ public:
     }
 
     ent->mData = aData;
-
-    return true;
-  }
-
-  /**
-   * Put a new value for the associated key
-   * @param aKey the key to put
-   * @param aData the new data
-   */
-  void Put(KeyType aKey, UserDataType&& aData)
-  {
-    if (!Put(aKey, mozilla::Move(aData), mozilla::fallible)) {
-      NS_ABORT_OOM(this->mTable.EntrySize() * this->mTable.EntryCount());
-    }
-  }
-
-  MOZ_MUST_USE bool Put(KeyType aKey, UserDataType&& aData, const fallible_t&)
-  {
-    EntryType* ent = this->PutEntry(aKey, mozilla::fallible);
-    if (!ent) {
-      return false;
-    }
-
-    ent->mData = mozilla::Move(aData);
 
     return true;
   }
