59e766b0471d4bbbc9aa5d85a18dc5c5dd6e088e
Julian Descottes <jdescottes@mozilla.com>
2017-10-04 15:16:37 +0200

    Bug 1361080 - add aboutdevtools component;r=ochameau
    MozReview-Commit-ID: GuyQfXRKFpb
    
    --HG--
    extra : rebase_source : 808377154712b98c1bab76ee83ecbd72cb0ddc47

---
+++ devtools/shim/aboutdevtools/aboutdevtools-registration.js
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Register the about:devtools URL, that is opened whenever a user attempts to open
+// DevTools for the first time.
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+const { XPCOMUtils } = Cu.import("resource://gre/modules/XPCOMUtils.jsm", {});
+const { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
+
+const { nsIAboutModule } = Ci;
+
+function AboutDevtools() {}
+
+AboutDevtools.prototype = {
+  uri: Services.io.newURI("chrome://devtools-shim/content/aboutdevtools/aboutdevtools.xhtml"),
+  classDescription: "about:devtools",
+  classID: Components.ID("3a16d383-92bd-4c24-ac10-0e2bd66883ab"),
+  contractID: "@mozilla.org/network/protocol/about;1?what=devtools",
+
+  QueryInterface: XPCOMUtils.generateQI([nsIAboutModule]),
+
+  newChannel: function (uri, loadInfo) {
+    let chan = Services.io.newChannelFromURIWithLoadInfo(
+      this.uri,
+      loadInfo
+    );
+    chan.owner = Services.scriptSecurityManager.getSystemPrincipal();
+    return chan;
+  },
+
+  getURIFlags: function (uri) {
+    return nsIAboutModule.ALLOW_SCRIPT;
+  }
+};
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([
+  AboutDevtools
+]);
