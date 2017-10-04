1ca4342864979f86d8a6243220785a7a715df631
Chih-Yi Leu <subsevenx2001@gmail.com>
2017-10-04 17:27:53 +0800

    Bug 1371190 - Part 7: Re-apply various patches to the in-tree copy of ANGLE. r=jgilbert
    Bug 1363216 - Turn off std::future for MinGW.
    Bug 1331335 - Add SSE2 Flags to all of libANGLE.
    Bug 1364169 - on ANGLE context creation asks for robustness but does not get it.
    Bug 1370865 - Suppress more MSVC warnings in gfx/angle.
    Bug 1373525 - gfx/angle: Suppress -Wimplicit-fallthrough warnings in third-party code.
    Bug 1347866 - Part3: ANGLE patch uplift for bug1325733.
    Bug 1347866 - Part4: ANGLE patch uplift for bug1325741.
    Bug 1322746 - Add general ID3D11Texture2D to EGLStream support to ANGLE.
    Bug 1366425 - Avoid losing context on out of memory error for ANGLE.
    
    --HG--
    extra : rebase_source : 2d422e188797b07194b8df0f0f1f3fd435c710e4

--- gfx/angle/src/libANGLE/validationEGL.cpp
+++ gfx/angle/src/libANGLE/validationEGL.cpp
@@ -1972,10 +1972,23 @@ Error ValidateCreateStreamProducerD3DTextureNV12ANGLE(const Display *display,
         return EglBadState() << "Stream not in connecting state";
     }
 
-    if (stream->getConsumerType() != Stream::ConsumerType::GLTextureYUV ||
-        stream->getPlaneCount() != 2)
-    {
-        return EglBadMatch() << "Incompatible stream consumer type";
+    switch (stream->getConsumerType()) {
+    case Stream::ConsumerType::GLTextureYUV:
+        if (stream->getPlaneCount() != 2)
+        {
+            return Error(EGL_BAD_MATCH, "Incompatible stream consumer type");
+        }
+        break;
+
+    case Stream::ConsumerType::GLTextureRGB:
+        if (stream->getPlaneCount() != 1)
+        {
+            return Error(EGL_BAD_MATCH, "Incompatible stream consumer type");
+        }
+        break;
+
+    default:
+        return Error(EGL_BAD_MATCH, "Incompatible stream consumer type");
     }
 
     return NoError();
@@ -2009,6 +2022,12 @@ Error ValidateStreamPostD3DTextureNV12ANGLE(const Display *display,
                     return EglBadParameter() << "Invalid subresource index";
                 }
                 break;
+            case EGL_NATIVE_BUFFER_PLANE_OFFSET_IMG:
+                if (value < 0)
+                {
+                    return Error(EGL_BAD_PARAMETER, "Invalid plane offset");
+                }
+                break;
             default:
                 return EglBadAttribute() << "Invalid attribute";
         }
@@ -2031,7 +2050,7 @@ Error ValidateStreamPostD3DTextureNV12ANGLE(const Display *display,
         return EglBadParameter() << "Texture is null";
     }
 
-    return stream->validateD3D11NV12Texture(texture);
+    return stream->validateD3D11NV12Texture(texture, attribs);
 }
 
 Error ValidateGetSyncValuesCHROMIUM(const Display *display,
