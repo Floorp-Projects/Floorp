81d1af42aa2b4fb07d66429585cb0ec18c7c9501
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-05 00:21:03 +0200

    Backed out changeset 98092d164d57 (bug 1393805) for mass test failures, at least on OS X. r=backout on a CLOSED TREE

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -104,7 +104,6 @@ protected:
   nsresult GetFilesInternal(const char* aProperty, nsISimpleEnumerator** aResult);
   static nsresult GetUserDataDirectoryHome(nsIFile* *aFile, bool aLocal);
   static nsresult GetSysUserExtensionsDirectory(nsIFile* *aFile);
-  static nsresult GetSysUserExtensionsDevDirectory(nsIFile* *aFile);
 #if defined(XP_UNIX) || defined(XP_MACOSX)
   static nsresult GetSystemExtensionsDirectory(nsIFile** aFile);
 #endif
@@ -119,7 +118,6 @@ protected:
                                     bool aLocal);
 
   static nsresult AppendSysUserExtensionPath(nsIFile* aFile);
-  static nsresult AppendSysUserExtensionsDevPath(nsIFile* aFile);
 
   // Internal helper that splits a path into components using the '/' and '\\'
   // delimiters.
