0a9e68bcd951b4178a55f88697da1ac68249bf99
Yura Zenevich <yura.zenevich@gmail.com>
2017-10-05 16:20:48 -0400

    Bug 1388256 - improving stability of name caching test. r=eeejay
    MozReview-Commit-ID: A6yoo3CZEyP

--- accessible/tests/browser/tree/head.js
+++ accessible/tests/browser/tree/head.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-/* exported contentSpawnMutation, testChildrenIds */
+/* exported testChildrenIds */
 
 // Load the shared-head file first.
 /* import-globals-from ../shared-head.js */
@@ -24,42 +24,4 @@ function testChildrenIds(acc, expectedIds) {
   let ids = arrayFromChildren(acc).map(child => getAccessibleDOMNodeID(child));
   Assert.deepEqual(ids, expectedIds,
     `Children for ${getAccessibleDOMNodeID(acc)} are wrong.`);
-}
-
-/*
- * This function spawns a content task and awaits expected mutation events from
- * aria-owns changes. It's good at catching events we did *not* expect. We do
- * this advancing the layout refresh to flush the relocations/insertions queue.
- */
-async function contentSpawnMutation(browser, waitFor, func) {
-  let onReorders = waitForEvents({ expected: waitFor.expected || [] });
-  let unexpectedListener = new UnexpectedEvents(waitFor.unexpected || []);
-
-  function tick() {
-    // 100ms is an arbitrary positive number to advance the clock.
-    // We don't need to advance the clock for a11y mutations, but other
-    // tick listeners may depend on an advancing clock with each refresh.
-    content.QueryInterface(Ci.nsIInterfaceRequestor)
-      .getInterface(Ci.nsIDOMWindowUtils).advanceTimeAndRefresh(100);
-  }
-
-  // This stops the refreh driver from doing its regular ticks, and leaves
-  // us in control.
-  await ContentTask.spawn(browser, null, tick);
-
-  // Perform the tree mutation.
-  await ContentTask.spawn(browser, null, func);
-
-  // Do one tick to flush our queue (insertions, relocations, etc.)
-  await ContentTask.spawn(browser, null, tick);
-
-  await onReorders;
-
-  unexpectedListener.stop();
-
-  // Go back to normal refresh driver ticks.
-  await ContentTask.spawn(browser, null, function() {
-    content.QueryInterface(Ci.nsIInterfaceRequestor)
-      .getInterface(Ci.nsIDOMWindowUtils).restoreNormalRefresh();
-  });
 }
