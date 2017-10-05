191574f9d85c42b5345952c1fd6e3182a8184b54
Ethan Lin <ethlin@mozilla.com>
2017-10-05 14:42:28 +0800

    Bug 1405957 - Part2. Remove the unnecessary GetLayerState in CreateWebRenderCommands and adjust the related pref type/value. r=kats
    MozReview-Commit-ID: E8JvuOj6JtL

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5898,18 +5898,18 @@ pref("layers.mlgpu.enable-on-windows7", true);
 pref("layers.advanced.background-color", false);
 pref("layers.advanced.background-image", 2);
 pref("layers.advanced.border-layers", 2);
-pref("layers.advanced.boxshadow-inset-layers", 2);
-pref("layers.advanced.boxshadow-outer-layers", 2);
+pref("layers.advanced.boxshadow-inset-layers", false);
+pref("layers.advanced.boxshadow-outer-layers", false);
 pref("layers.advanced.bullet-layers", 2);
 pref("layers.advanced.button-foreground-layers", 2);
 pref("layers.advanced.canvas-background-color", 2);
-pref("layers.advanced.caret-layers", 2);
+pref("layers.advanced.caret-layers", false);
 pref("layers.advanced.columnRule-layers", 2);
 pref("layers.advanced.displaybuttonborder-layers", 2);
 pref("layers.advanced.image-layers", 2);
 pref("layers.advanced.outline-layers", 2);
-pref("layers.advanced.solid-color", 2);
-pref("layers.advanced.table", 2);
+pref("layers.advanced.solid-color", false);
+pref("layers.advanced.table", false);
 pref("layers.advanced.text-layers", 2);
 
 // Enable lowercased response header name
