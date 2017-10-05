cce423da1e0905080fdaa740c32679810f1c59b8
Jim Chen <nchen@mozilla.com>
2017-10-04 22:28:43 -0400

    Bug 1404144 - 1. Refactor child process code to support preloading; r=rbarker
    Refactor the code in GeckoProcessManager and GeckoServiceChildProcess so
    that, we can have a ChildConnection object that's bound but not started.
    This way we can bind the connection to preload Gecko child process, but
    hold off starting until told by Gecko main process.
    
    Some code is simplified. For example, `IChildProcess.stop` is removed in
    favor of killing the child process directly.
    
    MozReview-Commit-ID: 4XgmTuT0IAs

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoServiceChildProcess.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoServiceChildProcess.java
@@ -5,7 +5,6 @@
 
 package org.mozilla.gecko.process;
 
-import org.mozilla.gecko.annotation.JNITarget;
 import org.mozilla.gecko.annotation.WrapForJNI;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.IGeckoEditableParent;
@@ -29,15 +28,6 @@ public class GeckoServiceChildProcess extends Service {
 
     private static IProcessManager sProcessManager;
 
-    static private void stop() {
-        ThreadUtils.postToUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Process.killProcess(Process.myPid());
-            }
-        });
-    }
-
     @WrapForJNI(calledFrom = "gecko")
     private static IGeckoEditableParent getEditableParent(final long contentId,
                                                           final long tabId) {
@@ -49,69 +39,69 @@ public class GeckoServiceChildProcess extends Service {
         }
     }
 
+    @Override
     public void onCreate() {
         super.onCreate();
+
+        GeckoAppShell.ensureCrashHandling();
+        GeckoAppShell.setApplicationContext(getApplicationContext());
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
+    @Override
     public int onStartCommand(final Intent intent, final int flags, final int startId) {
-        return Service.START_STICKY;
+        return Service.START_NOT_STICKY;
     }
 
-    private Binder mBinder = new IChildProcess.Stub() {
-        @Override
-        public void stop() {
-            GeckoServiceChildProcess.stop();
-        }
-
+    private final Binder mBinder = new IChildProcess.Stub() {
         @Override
         public int getPid() {
             return Process.myPid();
         }
 
         @Override
-        public void start(final IProcessManager procMan,
-                          final String[] args,
-                          final ParcelFileDescriptor crashReporterPfd,
-                          final ParcelFileDescriptor ipcPfd) {
-            if (sProcessManager != null) {
-                Log.e(LOGTAG, "Attempting to start a service that has already been started.");
-                return;
+        public boolean start(final IProcessManager procMan,
+                             final String[] args,
+                             final ParcelFileDescriptor crashReporterPfd,
+                             final ParcelFileDescriptor ipcPfd) {
+            synchronized (GeckoServiceChildProcess.class) {
+                if (sProcessManager != null) {
+                    Log.e(LOGTAG, "Child process already started");
+                    return false;
+                }
+                sProcessManager = procMan;
             }
-            sProcessManager = procMan;
 
-            final int crashReporterFd = crashReporterPfd != null ? crashReporterPfd.detachFd() : -1;
+            final int crashReporterFd = crashReporterPfd != null ?
+                                        crashReporterPfd.detachFd() : -1;
             final int ipcFd = ipcPfd != null ? ipcPfd.detachFd() : -1;
+
             ThreadUtils.postToUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    GeckoAppShell.ensureCrashHandling();
-                    GeckoAppShell.setApplicationContext(getApplicationContext());
                     if (GeckoThread.initChildProcess(args, crashReporterFd, ipcFd)) {
                         GeckoThread.launch();
                     }
                 }
             });
+            return true;
         }
     };
 
+    @Override
     public IBinder onBind(final Intent intent) {
         GeckoLoader.setLastIntent(new SafeIntent(intent));
+        GeckoThread.launch(); // Preload Gecko.
         return mBinder;
     }
 
+    @Override
     public boolean onUnbind(Intent intent) {
         Log.i(LOGTAG, "Service has been unbound. Stopping.");
-        stop();
+        Process.killProcess(Process.myPid());
         return false;
     }
 
-    @JNITarget
-    static public final class geckomediaplugin extends GeckoServiceChildProcess {}
+    public static final class geckomediaplugin extends GeckoServiceChildProcess {}
 
-    @JNITarget
-    static public final class tab extends GeckoServiceChildProcess {}
+    public static final class tab extends GeckoServiceChildProcess {}
 }
