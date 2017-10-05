2e20b0d1289ae3d3ebf08125fcf0891fcbe86a57
Nika Layzell <nika@thelayzells.com>
2017-10-05 17:06:43 -0400

    Bug 1377351 - Part 5: Add a workaround for gcc 4.9 compiler bug, r=froydnj
    MozReview-Commit-ID: CHywpZ4fvXf

--- xpcom/string/nsTSubstring.cpp
+++ xpcom/string/nsTSubstring.cpp
@@ -530,6 +530,14 @@ nsTSubstring<T>::Assign(self_type&& aStr, const fallible_t& aFallible)
   return true;
 }
 
+// NOTE(nika): gcc 4.9 workaround. Remove when support is dropped.
+template <typename T>
+void
+nsTSubstring<T>::Assign(const literalstring_type& aStr)
+{
+  Assign(aStr.AsString());
+}
+
 template <typename T>
 void
 nsTSubstring<T>::Assign(const substring_tuple_type& aTuple)
