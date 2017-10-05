943e6636013c7a70928c5633a0af5c7b82282ab8
Andrea Marchesini <amarchesini@mozilla.com>
2017-10-05 07:41:42 +0200

    Bug 1403706 - Remove race conditions in temporary blob  - part 6 - Introducing PTemporaryIPCBlob, r=smaug

---
+++ dom/file/ipc/TemporaryIPCBlobParent.h
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_ipc_TemporaryIPCBlobParent_h
+#define mozilla_dom_ipc_TemporaryIPCBlobParent_h
+
+#include "mozilla/ipc/PTemporaryIPCBlob.h"
+#include "mozilla/ipc/PTemporaryIPCBlobParent.h"
+
+class nsIFile;
+
+namespace mozilla {
+namespace dom {
+
+class TemporaryIPCBlobParent final : public mozilla::ipc::PTemporaryIPCBlobParent
+{
+public:
+  explicit TemporaryIPCBlobParent();
+
+  mozilla::ipc::IPCResult
+  CreateAndShareFile();
+
+private:
+  ~TemporaryIPCBlobParent();
+
+  mozilla::ipc::IPCResult
+  RecvOperationDone(const bool& aSuccess,
+                    const nsCString& aContentType) override;
+
+  void
+  ActorDestroy(ActorDestroyReason aWhy) override;
+
+  mozilla::ipc::IPCResult
+  SendDeleteError(nsresult aRv);
+
+  nsCOMPtr<nsIFile> mFile;
+  bool mActive;
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_ipc_TemporaryIPCBlobParent_h
