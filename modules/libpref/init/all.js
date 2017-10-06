62680fd1d66ec8fd55774e7019ff5ac2949c6c8c
Kartikaya Gupta <kgupta@mozilla.com>
2017-10-06 09:29:45 -0400

    Bug 1406008 - Add pref for just dumping the parent process display list. r=jrmuizel
    MozReview-Commit-ID: Fdm2f5jbJ4z

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3126,6 +3126,7 @@ pref("layout.frame_rate", -1);
 // pref to dump the display list to the log. Useful for debugging drawing.
 pref("layout.display-list.dump", false);
 pref("layout.display-list.dump-content", false);
+pref("layout.display-list.dump-parent", false);
 
 // pref to control whether layout warnings that are hit quite often are enabled
 pref("layout.spammy_warnings.enabled", false);
