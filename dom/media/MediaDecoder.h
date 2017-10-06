c02b52cab9d85f22228a0237c26395503d5b3f31
JW Wang <jwwang@mozilla.com>
2017-10-06 15:48:38 +0800

    Bug 1405025 - Part 1: Ensure 'seeking' is fired before 'waiting'. r=jya, a=ritu
    Use MediaEventSource instead of state-mirroring to notify nextFrameStatus
    changes so we have more control over the order of events.
    
    MozReview-Commit-ID: 3DGtMbghEQm

--- dom/media/MediaDecoder.h
+++ dom/media/MediaDecoder.h
@@ -493,6 +493,8 @@ private:
     mMediaSeekable = false;
   }
 
+  void OnNextFrameStatus(MediaDecoderOwner::NextFrameStatus);
+
   void FinishShutdown();
 
   void ConnectMirrors(MediaDecoderStateMachine* aObject);
@@ -578,6 +580,9 @@ protected:
   // disabled.
   bool mHasSuspendTaint;
 
+  MediaDecoderOwner::NextFrameStatus mNextFrameStatus =
+    MediaDecoderOwner::NEXT_FRAME_UNAVAILABLE;
+
   // A listener to receive metadata updates from MDSM.
   MediaEventListener mTimedMetadataListener;
 
@@ -591,6 +596,7 @@ protected:
   MediaEventListener mOnEncrypted;
   MediaEventListener mOnWaitingForKey;
   MediaEventListener mOnDecodeWarning;
+  MediaEventListener mOnNextFrameStatus;
 
 protected:
   // PlaybackRate and pitch preservation status we should start at.
@@ -598,9 +604,6 @@ protected:
 
   // Buffered range, mirrored from the reader.
   Mirror<media::TimeIntervals> mBuffered;
-
-  // NextFrameStatus, mirrored from the state machine.
-  Mirror<MediaDecoderOwner::NextFrameStatus> mNextFrameStatus;
 
   // NB: Don't use mCurrentPosition directly, but rather CurrentPosition().
   Mirror<media::TimeUnit> mCurrentPosition;
