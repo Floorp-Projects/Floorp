aaa4b6ca2688627a9cea87aa1c8b2dbc2e8aeec0
Chung-Sheng Fu <cfu@mozilla.com>
2017-10-06 17:59:17 +0800

    Bug 1406294 - Refactor browser/components/resistfingerprinting/test/browser rounded window tests. r=smaug,timhuang
    MozReview-Commit-ID: GSUW3zWPF0K
    
    --HG--
    extra : rebase_source : b31f44e4eeef84af5d0b7867875810eceb5d4c54

--- browser/components/resistfingerprinting/test/browser/browser_roundedWindow_windowSetting_mid.js
+++ browser/components/resistfingerprinting/test/browser/browser_roundedWindow_windowSetting_mid.js
@@ -4,64 +4,8 @@
  *   test is for middle values.
  */
 
-const { classes: Cc, Constructor: CC, interfaces: Ci, utils: Cu } = Components;
-
-const TEST_DOMAIN = "http://example.net/";
-const TEST_PATH = TEST_DOMAIN + "browser/browser/components/resistfingerprinting/test/browser/";
-
-let gMaxAvailWidth;
-let gMaxAvailHeight;
-
-// We need the chrome UI size of popup windows for testing outerWidth/Height.
-let gPopupChromeUIWidth;
-let gPopupChromeUIHeight;
-
-const TESTCASES = [
-  { settingWidth: 600, settingHeight: 600, targetWidth: 600, targetHeight: 600,
-    initWidth: 200, initHeight: 100  },
-  { settingWidth: 599, settingHeight: 599, targetWidth: 600, targetHeight: 600,
-    initWidth: 200, initHeight: 100  },
-  { settingWidth: 401, settingHeight: 501, targetWidth: 600, targetHeight: 600,
-    initWidth: 200, initHeight: 100  },
-];
-
-add_task(async function setup() {
-  await SpecialPowers.pushPrefEnv({"set":
-    [["privacy.resistFingerprinting", true]]
-  });
-
-  // Calculate the popup window's chrome UI size for tests of outerWidth/Height.
-  let popUpChromeUISize = await calcPopUpWindowChromeUISize();
-
-  gPopupChromeUIWidth = popUpChromeUISize.chromeWidth;
-  gPopupChromeUIHeight = popUpChromeUISize.chromeHeight;
-
-  // Calculate the maximum available size.
-  let maxAvailSize = await calcMaximumAvailSize(gPopupChromeUIWidth,
-                                                gPopupChromeUIHeight);
-
-  gMaxAvailWidth = maxAvailSize.maxAvailWidth;
-  gMaxAvailHeight = maxAvailSize.maxAvailHeight;
-});
-
-add_task(async function test_window_size_setting() {
-  // Open a tab to test.
-  let tab = await BrowserTestUtils.openNewForegroundTab(
-    gBrowser, TEST_PATH + "file_dummy.html");
-
-  for (let test of TESTCASES) {
-    // Test window.innerWidth and window.innerHeight.
-    await testWindowSizeSetting(tab.linkedBrowser, test.settingWidth, test.settingHeight,
-                                test.targetWidth, test.targetHeight, test.initWidth,
-                                test.initHeight, false, gMaxAvailWidth, gMaxAvailHeight,
-                                gPopupChromeUIWidth, gPopupChromeUIHeight);
-
-    // test window.outerWidth and window.outerHeight.
-    await testWindowSizeSetting(tab.linkedBrowser, test.settingWidth, test.settingHeight,
-                                test.targetWidth, test.targetHeight, test.initWidth,
-                                test.initHeight, true, gMaxAvailWidth, gMaxAvailHeight,
-                                gPopupChromeUIWidth, gPopupChromeUIHeight);
-  }
-
-  await BrowserTestUtils.removeTab(tab);
-});
+WindowSettingTest.run([
+  {settingWidth: 600, settingHeight: 600, targetWidth: 600, targetHeight: 600, initWidth: 200, initHeight: 100},
+  {settingWidth: 599, settingHeight: 599, targetWidth: 600, targetHeight: 600, initWidth: 200, initHeight: 100},
+  {settingWidth: 401, settingHeight: 501, targetWidth: 600, targetHeight: 600, initWidth: 200, initHeight: 100}
+]);
