0bf978ad970452b815919bbf27d158f5c8594a84
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-07 16:40:12 +0200

    Backed out changeset 8149514f0473 (bug 1406042) for failing devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_property_provider.js. r=backout

--- devtools/client/webconsole/new-console-output/test/mochitest/test-bug-597136-external-script-errors.js
+++
@@ -1,9 +0,0 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
-/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
-
-function f() {
-  bogus.g();
-}
-
