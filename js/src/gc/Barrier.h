d3f2375ac64766c41710ea65852f760851491d7a
Jan de Mooij <jdemooij@mozilla.com>
2017-10-09 10:01:54 +0200

    Bug 1405999 part 3 - Inline AutoGCRooter constructors and add MOZ_ALWAYS_INLINE to some functions. r=jonco

--- js/src/gc/Barrier.h
+++ js/src/gc/Barrier.h
@@ -283,7 +283,7 @@ struct InternalBarrierMethods<Value>
         DispatchTyped(PreBarrierFunctor<Value>(), v);
     }
 
-    static void postBarrier(Value* vp, const Value& prev, const Value& next) {
+    static MOZ_ALWAYS_INLINE void postBarrier(Value* vp, const Value& prev, const Value& next) {
         MOZ_ASSERT(!CurrentThreadIsIonCompiling());
         MOZ_ASSERT(vp);
 
@@ -369,7 +369,7 @@ class WriteBarrieredBase : public BarrieredBase<T>,
 
   protected:
     void pre() { InternalBarrierMethods<T>::preBarrier(this->value); }
-    void post(const T& prev, const T& next) {
+    MOZ_ALWAYS_INLINE void post(const T& prev, const T& next) {
         InternalBarrierMethods<T>::postBarrier(&this->value, prev, next);
     }
 };
