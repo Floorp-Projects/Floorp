165fab2f859610cff221fbba2fd97a585c6ab60b
Jan Horak <jhorak@redhat.com>
2017-10-10 13:35:56 +0200

    Bug 1381815 - fixing dimensions of radio and checkbox for GTK 3.20+; r=karlt
    In the GTK < 3.20 the size of radio and checkbox toggle is determined by indicator
    spacing and indicator size. By  GTK 3.20+ it is replaced by standard box model
    (padding, margin, border). The patch fixes that while keeping the functionality
    for older GTK. The values are also cached by similar way as scrollbar metrics
    are cached now.
    
    The focus is no longer rendered by GTK but by Mozilla code, so the extra
    size for toggles has been removed from GetExtraSizeForWidget and toggles
    no longer render focus indicator.
    
    MozReview-Commit-ID: 1Wg5AgHy1Vz
    
    --HG--
    extra : rebase_source : 81437f45b7d32555942d21fccc9de4a561d85111

--- widget/gtk/nsNativeThemeGTK.cpp
+++ widget/gtk/nsNativeThemeGTK.cpp
@@ -1020,24 +1020,6 @@ nsNativeThemeGTK::GetExtraSizeForWidget(nsIFrame* aFrame, uint8_t aWidgetType,
     aExtra->left = aExtra->right = 1;
     break;
 
-  // Include the indicator spacing (the padding around the control).
-  case NS_THEME_CHECKBOX:
-  case NS_THEME_RADIO:
-    {
-      gint indicator_size, indicator_spacing;
-
-      if (aWidgetType == NS_THEME_CHECKBOX) {
-        moz_gtk_checkbox_get_metrics(&indicator_size, &indicator_spacing);
-      } else {
-        moz_gtk_radio_get_metrics(&indicator_size, &indicator_spacing);
-      }
-
-      aExtra->top = indicator_spacing;
-      aExtra->right = indicator_spacing;
-      aExtra->bottom = indicator_spacing;
-      aExtra->left = indicator_spacing;
-      break;
-    }
   case NS_THEME_BUTTON :
     {
       if (IsDefaultButton(aFrame)) {
@@ -1595,17 +1577,9 @@ nsNativeThemeGTK::GetMinimumWidgetSize(nsPresContext* aPresContext,
   case NS_THEME_CHECKBOX:
   case NS_THEME_RADIO:
     {
-      gint indicator_size, indicator_spacing;
-
-      if (aWidgetType == NS_THEME_CHECKBOX) {
-        moz_gtk_checkbox_get_metrics(&indicator_size, &indicator_spacing);
-      } else {
-        moz_gtk_radio_get_metrics(&indicator_size, &indicator_spacing);
-      }
-
-      // Include space for the indicator and the padding around it.
-      aResult->width = indicator_size;
-      aResult->height = indicator_size;
+      const ToggleGTKMetrics* metrics = GetToggleMetrics(aWidgetType == NS_THEME_RADIO);
+      aResult->width = metrics->minSizeWithBorder.width;
+      aResult->height = metrics->minSizeWithBorder.height;
     }
     break;
   case NS_THEME_TOOLBARBUTTON_DROPDOWN:
