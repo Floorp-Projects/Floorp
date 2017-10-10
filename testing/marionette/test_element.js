cb10f400b1a2fbd7f562aee05e6993e28064feb0
Andreas Tolfsen <ato@sny.no>
2017-10-10 14:51:27 +0100

    Bug 1275273 - Make WebDriver:IsElementSelected conform to spec. r=maja_zf
    Splits interaction.isElementSelected into two parts: one checking
    whether the element DOM properties are selected/checked, and the
    other checking accessibility.  This so this so that the selectedness
    can be unit tested, as we do not have the capability of standing
    up accessibility in the xpcshell tests.
    
    The second part of this change moves us away from atom.isElementSelected
    in favour of a specification conforming implementation in Marionette.
    This is a word-by-word implementation of the Is Element Selected
    command from WebDriver.
    
    MozReview-Commit-ID: 93WDKbPcEIB

--- testing/marionette/test_element.js
+++ testing/marionette/test_element.js
@@ -6,8 +6,28 @@ const {utils: Cu} = Components;
 
 Cu.import("chrome://marionette/content/element.js");
 
-let el = {
-  getBoundingClientRect: function() {
+class DOMElement {
+  constructor(tagName, attrs = {}) {
+    this.tagName = tagName;
+    this.localName = tagName;
+
+    for (let attr in attrs) {
+      this[attr] = attrs[attr];
+    }
+
+    if (this.localName == "option") {
+      this.selected = false;
+    }
+
+    if (this.localName == "input" && ["checkbox", "radio"].includes(this.type)) {
+      this.checked = false;
+    }
+  }
+
+  get nodeType() { return 1; }
+  get ELEMENT_NODE() { return 1; }
+
+  getBoundingClientRect() {
     return {
       top: 0,
       left: 0,
@@ -17,28 +37,59 @@ let el = {
   }
 };
 
+const domEl = new DOMElement("p");
+
+add_test(function test_isSelected() {
+  let checkbox = new DOMElement("input", {type: "checkbox"});
+  ok(!element.isSelected(checkbox));
+  checkbox.checked = true;
+  ok(element.isSelected(checkbox));
+
+  // selected is not a property of <input type=checkbox>
+  checkbox.selected = true;
+  checkbox.checked = false;
+  ok(!element.isSelected(checkbox));
+
+  let option = new DOMElement("option");
+  ok(!element.isSelected(option));
+  option.selected = true;
+  ok(element.isSelected(option));
+
+  // checked is not a property of <option>
+  option.checked = true;
+  option.selected = false;
+  ok(!element.isSelected(option));
+
+  // anything else should not be selected
+  for (let typ of [domEl, undefined, null, "foo", true, [], {}]) {
+    ok(!element.isSelected(typ));
+  }
+
+  run_next_test();
+});
+
 add_test(function test_coordinates() {
-  let p = element.coordinates(el);
+  let p = element.coordinates(domEl);
   ok(p.hasOwnProperty("x"));
   ok(p.hasOwnProperty("y"));
   equal("number", typeof p.x);
   equal("number", typeof p.y);
 
-  deepEqual({x: 50, y: 50}, element.coordinates(el));
-  deepEqual({x: 10, y: 10}, element.coordinates(el, 10, 10));
-  deepEqual({x: -5, y: -5}, element.coordinates(el, -5, -5));
+  deepEqual({x: 50, y: 50}, element.coordinates(domEl));
+  deepEqual({x: 10, y: 10}, element.coordinates(domEl, 10, 10));
+  deepEqual({x: -5, y: -5}, element.coordinates(domEl, -5, -5));
 
   Assert.throws(() => element.coordinates(null));
 
-  Assert.throws(() => element.coordinates(el, "string", undefined));
-  Assert.throws(() => element.coordinates(el, undefined, "string"));
-  Assert.throws(() => element.coordinates(el, "string", "string"));
-  Assert.throws(() => element.coordinates(el, {}, undefined));
-  Assert.throws(() => element.coordinates(el, undefined, {}));
-  Assert.throws(() => element.coordinates(el, {}, {}));
-  Assert.throws(() => element.coordinates(el, [], undefined));
-  Assert.throws(() => element.coordinates(el, undefined, []));
-  Assert.throws(() => element.coordinates(el, [], []));
+  Assert.throws(() => element.coordinates(domEl, "string", undefined));
+  Assert.throws(() => element.coordinates(domEl, undefined, "string"));
+  Assert.throws(() => element.coordinates(domEl, "string", "string"));
+  Assert.throws(() => element.coordinates(domEl, {}, undefined));
+  Assert.throws(() => element.coordinates(domEl, undefined, {}));
+  Assert.throws(() => element.coordinates(domEl, {}, {}));
+  Assert.throws(() => element.coordinates(domEl, [], undefined));
+  Assert.throws(() => element.coordinates(domEl, undefined, []));
+  Assert.throws(() => element.coordinates(domEl, [], []));
 
   run_next_test();
 });
