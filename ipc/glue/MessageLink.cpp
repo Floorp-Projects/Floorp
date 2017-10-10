ef3b2f88cc62513df78de7f7a5bde12a73bcc8d1
Gabriele Svelto <gsvelto@mozilla.com>
2017-10-10 12:06:35 +0200

    Bug 1402519 - Remove MOZ_CRASHREPORTER directives from ipc; r=billm
    MozReview-Commit-ID: 4mDW7cJqHDn
    
    --HG--
    extra : rebase_source : 17c51ed796733d4a512e1d6f47273318dcc26b11

--- ipc/glue/MessageLink.cpp
+++ ipc/glue/MessageLink.cpp
@@ -14,9 +14,7 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/DebugOnly.h"
 #include "nsDebug.h"
-#ifdef MOZ_CRASHREPORTER
 #include "nsExceptionHandler.h"
-#endif
 #include "nsISupportsImpl.h"
 #include "nsPrintfCString.h"
 #include "nsXULAppAPI.h"
@@ -161,10 +159,8 @@ void
 ProcessLink::SendMessage(Message *msg)
 {
     if (msg->size() > IPC::Channel::kMaximumMessageSize) {
-#ifdef MOZ_CRASHREPORTER
       CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("IPCMessageName"), nsDependentCString(msg->name()));
       CrashReporter::AnnotateCrashReport(NS_LITERAL_CSTRING("IPCMessageSize"), nsPrintfCString("%d", msg->size()));
-#endif
       MOZ_CRASH("IPC message size is too large");
     }
 
