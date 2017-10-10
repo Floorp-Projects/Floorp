99c7358944c6e064b7f141fb37b2725029ccb1ff
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-10 15:43:13 +0200

    Backed out changeset 0e5484914609 (bug 1395202) for build bustage at netwerk/protocol/http/HttpChannelParent.cpp:1523: 'class mozilla::net::HttpBaseChannel' has no member named 'GetCacheEntryId'. r=backout on a CLOSED TREE

--- netwerk/cache2/CacheEntry.h
+++ netwerk/cache2/CacheEntry.h
@@ -54,8 +54,6 @@ public:
   NS_DECL_NSICACHEENTRY
   NS_DECL_NSIRUNNABLE
 
-  static uint64_t GetNextId();
-
   CacheEntry(const nsACString& aStorageID, const nsACString& aURI, const nsACString& aEnhanceID,
              bool aUseDisk, bool aSkipSizeCheck, bool aPin);
 
@@ -385,8 +383,6 @@ private:
   int64_t mPredictedDataSize;
   mozilla::TimeStamp mLoadStart;
   uint32_t mUseCount;
-
-  const uint64_t mCacheEntryId;
 };
 
 
