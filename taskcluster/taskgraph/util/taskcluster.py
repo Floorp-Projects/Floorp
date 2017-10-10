dcfb0d4a0d98fb72d9008f31cc0eec51148f32a5
Rail Aliiev <rail@mozilla.com>
2017-10-10 15:00:57 -0400

    Bug 1407279 - Docker image changes on mozilla-beta result in failed decision tasks, Backed out changeset 83fdd1638d8e. r=dustin, a=ritu
    MozReview-Commit-ID: LYorA5QSOib
    
    --HG--
    extra : transplant_source : V%EC%F6%FE%FC%941%A5%7B%24g%B6%7C%C2%60%0D%129%8E%E4

--- taskcluster/taskgraph/util/taskcluster.py
+++ taskcluster/taskgraph/util/taskcluster.py
@@ -83,12 +83,7 @@ def get_index_url(index_path, use_proxy=False):
 
 
 def find_task_id(index_path, use_proxy=False):
-    try:
-        response = _do_request(get_index_url(index_path, use_proxy))
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 404:
-            raise KeyError("index path {} not found".format(index_path))
-        raise
+    response = _do_request(get_index_url(index_path, use_proxy))
     return response.json()['taskId']
 
 
