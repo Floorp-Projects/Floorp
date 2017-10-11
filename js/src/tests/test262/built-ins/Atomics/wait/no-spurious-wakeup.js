939bc6f811acd4515d8951744637e134f515d697
Lars T Hansen <lhansen@mozilla.com>
2017-10-11 11:16:49 +0200

    Bug 1361526 - merge from upstream test262 to kill intermittents. r=me

--- js/src/tests/test262/built-ins/Atomics/wait/no-spurious-wakeup.js
+++ js/src/tests/test262/built-ins/Atomics/wait/no-spurious-wakeup.js
@@ -2,6 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
+esid: sec-atomics.wait
 description: >
   Test that Atomics.wait actually waits and does not spuriously wake
   up when the memory value is changed.
@@ -14,7 +15,7 @@ $262.agent.receiveBroadcast(function (sab, id) {
   var ia = new Int32Array(sab);
   var then = Date.now();
   Atomics.wait(ia, 0, 0);
-  var diff = Date.now() - then;        // Should be about 1000 ms
+  var diff = Date.now() - then;        // Should be about 1000 ms but can be more
   $262.agent.report(diff);
   $262.agent.leaving();
 })
@@ -24,10 +25,10 @@ var ia = new Int32Array(new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT));
 
 $262.agent.broadcast(ia.buffer);
 $262.agent.sleep(500);                // Give the agent a chance to wait
-Atomics.store(ia, 0, 1);        // Change the value, should not wake the agent
+Atomics.store(ia, 0, 1);              // Change the value, should not wake the agent
 $262.agent.sleep(500);                // Wait some more so that we can tell
-Atomics.wake(ia, 0);                // Really wake it up
-assert.sameValue(Math.abs((getReport()|0) - 1000) < $ATOMICS_MAX_TIME_EPSILON, true);
+Atomics.wake(ia, 0);                  // Really wake it up
+assert.sameValue((getReport()|0) >= 1000 - $ATOMICS_MAX_TIME_EPSILON, true);
 
 function getReport() {
     var r;
