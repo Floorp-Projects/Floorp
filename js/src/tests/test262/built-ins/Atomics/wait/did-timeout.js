8b81bfc9d442a4910db5590c762a6e6d6fc13c6e
Lars T Hansen <lhansen@mozilla.com>
2017-10-11 11:16:49 +0200

    Bug 1361526 - merge from upstream test262 to kill intermittents. r=me
    --HG--
    extra : rebase_source : af91036a74264ff546c164e69b8ff6b5988d9506

--- js/src/tests/test262/built-ins/Atomics/wait/did-timeout.js
+++ js/src/tests/test262/built-ins/Atomics/wait/did-timeout.js
@@ -2,6 +2,7 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
+esid: sec-atomics.wait
 description: >
   Test that Atomics.wait returns the right result when it timed out and that
   the time to time out is reasonable.
@@ -14,7 +15,7 @@ $262.agent.receiveBroadcast(function (sab, id) {
   var ia = new Int32Array(sab);
   var then = Date.now();
   $262.agent.report(Atomics.wait(ia, 0, 0, 500)); // Timeout 500ms
-  $262.agent.report(Date.now() - then);
+  $262.agent.report(Date.now() - then);           // Actual time can be more than 500ms
   $262.agent.leaving();
 })
 `);
@@ -23,12 +24,12 @@ var ia = new Int32Array(new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT));
 
 $262.agent.broadcast(ia.buffer);
 assert.sameValue(getReport(), "timed-out");
-assert.sameValue(Math.abs((getReport()|0) - 500) < $ATOMICS_MAX_TIME_EPSILON, true);
+assert.sameValue((getReport()|0) >= 500 - $ATOMICS_MAX_TIME_EPSILON, true);
 
 function getReport() {
     var r;
     while ((r = $262.agent.getReport()) == null)
-	$262.agent.sleep(100);
+        $262.agent.sleep(100);
     return r;
 }
 
