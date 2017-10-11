702fbdf7c61e49e55ed554ce88b506c0be5687c1
Nicholas Nethercote <nnethercote@mozilla.com>
2017-10-11 19:03:23 +1100

    Bug 1407112 (part 1) - Merge nsPrefsFactory.cpp into Preferences.cpp. r=glandium.
    MozReview-Commit-ID: V1tONOw0wT
    
    --HG--
    extra : rebase_source : fb379c0856d77f4e1778bf00d4a44e6f30a573b2

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -10,6 +10,7 @@
 #include "mozilla/dom/PContent.h"
 #include "mozilla/HashFunctions.h"
 #include "mozilla/MemoryReporting.h"
+#include "mozilla/ModuleUtils.h"
 #include "mozilla/Omnijar.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/ResultExtensions.h"
@@ -84,6 +85,10 @@ public:
 #define WATCHING_PREF_RAII()
 
 #endif // DEBUG
+
+//===========================================================================
+// Core prefs code
+//===========================================================================
 
 class PrefCallback;
 
@@ -2301,3 +2306,51 @@ Preferences::GetDefaultType(const char* aPref)
 } // namespace mozilla
 
 #undef ENSURE_MAIN_PROCESS
+
+//===========================================================================
+// Module and factory stuff
+//===========================================================================
+
+NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(Preferences,
+                                         Preferences::GetInstanceForService)
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsPrefLocalizedString, Init)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsRelativeFilePref)
+
+static NS_DEFINE_CID(kPrefServiceCID, NS_PREFSERVICE_CID);
+static NS_DEFINE_CID(kPrefLocalizedStringCID, NS_PREFLOCALIZEDSTRING_CID);
+static NS_DEFINE_CID(kRelativeFilePrefCID, NS_RELATIVEFILEPREF_CID);
+
+static mozilla::Module::CIDEntry kPrefCIDs[] = {
+  { &kPrefServiceCID, true, nullptr, PreferencesConstructor },
+  { &kPrefLocalizedStringCID,
+    false,
+    nullptr,
+    nsPrefLocalizedStringConstructor },
+  { &kRelativeFilePrefCID, false, nullptr, nsRelativeFilePrefConstructor },
+  { nullptr }
+};
+
+static mozilla::Module::ContractIDEntry kPrefContracts[] = {
+  { NS_PREFSERVICE_CONTRACTID, &kPrefServiceCID },
+  { NS_PREFLOCALIZEDSTRING_CONTRACTID, &kPrefLocalizedStringCID },
+  { NS_RELATIVEFILEPREF_CONTRACTID, &kRelativeFilePrefCID },
+  // compatibility for extension that uses old service
+  { "@mozilla.org/preferences;1", &kPrefServiceCID },
+  { nullptr }
+};
+
+static void
+UnloadPrefsModule()
+{
+  Preferences::Shutdown();
+}
+
+static const mozilla::Module kPrefModule = { mozilla::Module::kVersion,
+                                             kPrefCIDs,
+                                             kPrefContracts,
+                                             nullptr,
+                                             nullptr,
+                                             nullptr,
+                                             UnloadPrefsModule };
+
+NSMODULE_DEFN(nsPrefModule) = &kPrefModule;
