e5ecd5213c44d7defd95b8540237310bf61156fd
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2017-10-12 16:55:42 +0900

    Bug 1407869 - Use Malgun Gothic as default on Korean on Nightly r=jfkthame
    MozReview-Commit-ID: 7PxPnBfXF7n

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3703,7 +3703,11 @@ pref("font.name-list.sans-serif.ja", "Meiryo, Yu Gothic, MS PGothic, MS Gothic, 
 pref("font.name-list.monospace.ja", "MS Gothic, MS Mincho, Meiryo, Yu Gothic, Yu Mincho, MS PGothic, MS PMincho");
 
 pref("font.name-list.serif.ko", "Batang, Gulim");
+#ifdef EARLY_BETA_OR_EARLIER
+pref("font.name-list.sans-serif.ko", "Malgun Gothic, Gulim");
+#else
 pref("font.name-list.sans-serif.ko", "Gulim, Malgun Gothic");
+#endif
 pref("font.name-list.monospace.ko", "GulimChe");
 pref("font.name-list.cursive.ko", "Gungsuh");
 
