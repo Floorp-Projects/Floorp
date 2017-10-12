51482ab58b7056ee903350dde54decbaa66f4ece
Bob Silverberg <bsilverberg@mozilla.com>
2017-10-12 17:31:27 -0400

    Bug 1408099 - Fix ExtensionPreferencesManager.getLevelOfControl to deal with undefined settings, r=aswan
    Prior to this, if getLevelOfControl was called before the setting was defined to the EPM, an
    exception was thrown. This fixes it by returning a level of control of "not_controllable"
    if the setting has not yet been defined.
    
    MozReview-Commit-ID: 96jXLoJlUI
    
    --HG--
    extra : rebase_source : 973389e39a4d83ec5c4e57407ef9cae08432ce5c

---
+++ toolkit/components/extensions/test/xpcshell/test_ext_browserSettings_homepage.js
@@ -0,0 +1,29 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+add_task(async function test_homepage_get_without_set() {
+  async function background() {
+    let homepage = await browser.browserSettings.homepageOverride.get({});
+    browser.test.sendMessage("homepage", homepage);
+  }
+
+  let extension = ExtensionTestUtils.loadExtension({
+    background,
+    manifest: {
+      permissions: ["browserSettings"],
+    },
+  });
+
+  let defaultHomepage = Services.prefs.getComplexValue(
+    "browser.startup.homepage", Ci.nsIPrefLocalizedString).data;
+
+  await extension.startup();
+  let homepage = await extension.awaitMessage("homepage");
+  equal(homepage.value, defaultHomepage,
+        "The homepageOverride setting has the expected value.");
+  equal(homepage.levelOfControl, "not_controllable",
+        "The homepageOverride setting has the expected levelOfControl.");
+
+  await extension.unload();
+});
