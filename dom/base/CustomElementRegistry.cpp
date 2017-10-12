b142e5383d3587a8e754a9af0d617d5ea607d177
Edgar Chen <echen@mozilla.com>
2017-10-12 17:01:41 +0800

    Bug 1407937 - Move mDefinition from CustomElementReacion to CustomElementUpgradeReaction; f=jdai; r=smaug
    MozReview-Commit-ID: 3JweEOgKAeo

--- dom/base/CustomElementRegistry.cpp
+++ dom/base/CustomElementRegistry.cpp
@@ -408,8 +408,7 @@ CustomElementRegistry::SyncInvokeReactions(nsIDocument::ElementCallbackType aTyp
   }
 
   UniquePtr<CustomElementReaction> reaction(Move(
-    MakeUnique<CustomElementCallbackReaction>(aDefinition,
-                                              Move(callback))));
+    MakeUnique<CustomElementCallbackReaction>(Move(callback))));
 
   RefPtr<SyncInvokeReactionRunnable> runnable =
     new SyncInvokeReactionRunnable(Move(reaction), aCustomElement);
@@ -455,8 +454,7 @@ CustomElementRegistry::EnqueueLifecycleCallback(nsIDocument::ElementCallbackType
 
   CustomElementReactionsStack* reactionsStack =
     docGroup->CustomElementReactionsStack();
-  reactionsStack->EnqueueCallbackReaction(aCustomElement, definition,
-                                          Move(callback));
+  reactionsStack->EnqueueCallbackReaction(aCustomElement, Move(callback));
 }
 
 void
@@ -1033,11 +1031,9 @@ CustomElementReactionsStack::EnqueueUpgradeReaction(Element* aElement,
 
 void
 CustomElementReactionsStack::EnqueueCallbackReaction(Element* aElement,
-                                                     CustomElementDefinition* aDefinition,
                                                      UniquePtr<CustomElementCallback> aCustomElementCallback)
 {
-  Enqueue(aElement, new CustomElementCallbackReaction(aDefinition,
-                                                      Move(aCustomElementCallback)));
+  Enqueue(aElement, new CustomElementCallbackReaction(Move(aCustomElementCallback)));
 }
 
 void
