bd778b233fcf79995263f3c0d54e397757cea2dc
Nick Thomas <nthomas@mozilla.com>
2017-10-13 12:50:41 +1300

    Bug 1408197 - fix beetmover by using a modern pip & setuptools, r=catlee a=release
    MozReview-Commit-ID: CFyYBcL1xTM

--- testing/mozharness/scripts/release/beet_mover.py
+++ testing/mozharness/scripts/release/beet_mover.py
@@ -147,6 +147,8 @@ class BeetMover(BaseScript, VirtualenvMixin, object):
                 # base index url where to find taskcluster artifact based on taskid
                 "artifact_base_url": 'https://queue.taskcluster.net/v1/task/{taskid}/artifacts/public/{subdir}',
                 "virtualenv_modules": [
+                    "pip==9.0.1",
+                    "setuptools==36.6.0",
                     "boto",
                     "PyYAML",
                     "Jinja2",
