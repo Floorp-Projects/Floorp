4877742d93574524ad453fbef3cf98a7f8210dd9
Emilio Cobos Álvarez <emilio@crisal.io>
2017-10-12 10:07:25 +0200

    Bug 1407864: Do not thrash the selector cache when parsing the same selector with different backend. r=heycam
    Since the presence of an entry with a null selector is different for Gecko and
    Servo, this seemed easier, and mimics nsLayoutStyleSheetCache.
    
    Also, this is going away soon anyway as soon as I get to implement the rest of
    the methods for stylo.
    
    MozReview-Commit-ID: DtHJbw8C0GX

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -1260,12 +1260,14 @@ public:
       nsDataHashtable<nsStringHashKey, SelectorList> mTable;
   };
 
-  SelectorCache& GetSelectorCache() {
-    if (!mSelectorCache) {
-      mSelectorCache =
-        new SelectorCache(EventTargetFor(mozilla::TaskCategory::Other));
+  SelectorCache& GetSelectorCache(mozilla::StyleBackendType aBackendType) {
+    mozilla::UniquePtr<SelectorCache>& cache =
+      aBackendType == mozilla::StyleBackendType::Servo
+        ? mServoSelectorCache : mGeckoSelectorCache;
+    if (!cache) {
+      cache.reset(new SelectorCache(EventTargetFor(mozilla::TaskCategory::Other)));
     }
-    return *mSelectorCache;
+    return *cache;
   }
   // Get the root <html> element, or return null if there isn't one (e.g.
   // if the root isn't <html>)
@@ -3203,8 +3205,13 @@ protected:
 private:
   mutable std::bitset<eDeprecatedOperationCount> mDeprecationWarnedAbout;
   mutable std::bitset<eDocumentWarningCount> mDocWarningWarnedAbout;
-  // Lazy-initialization to have mDocGroup initialized in prior to mSelectorCache.
-  nsAutoPtr<SelectorCache> mSelectorCache;
+
+  // Lazy-initialization to have mDocGroup initialized in prior to the
+  // SelectorCaches.
+  // FIXME(emilio): We can use a single cache when all CSSOM methods are
+  // implemented for the Servo backend.
+  mozilla::UniquePtr<SelectorCache> mServoSelectorCache;
+  mozilla::UniquePtr<SelectorCache> mGeckoSelectorCache;
 
 protected:
   ~nsIDocument();
