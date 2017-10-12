e6a9482db734e3579293869c6950f46c378d449c
J.C. Jones <jjones@mozilla.com>
2017-10-12 15:21:06 -0700

    Bug 1381190 - Change to COSE Algorithm identifiers for WebAuthn r=qdot,ttaubert
    The WD-06 (and later) WebAuthn specs choose to move to integer algorithm
    identifiers for the signatures [1], with a handful of algorithms identified [2].
    U2F devices only support ES256 (e.g., COSE ID "-7"), so that's all that is
    implemented here.
    
    Note that the spec also now requires that we accept empty lists of parameters,
    and in that case, the RP says they aren't picky, so this changes what happens
    when the parameter list is empty (but still aborts when the list is non-empty
    but doesn't have anything we can use) [3].
    
    There's a follow-on to move parameter-validation logic into the U2FTokenManager
    in Bug 1409220.
    
    [1] https://w3c.github.io/webauthn/#dictdef-publickeycredentialparameters
    [2] https://w3c.github.io/webauthn/#alg-identifier
    [3] https://w3c.github.io/webauthn/#createCredential bullet #12
    
    MozReview-Commit-ID: KgL7mQ9u1uq

---
+++ dom/webauthn/WebAuthnCoseIdentifiers.h
@@ -0,0 +1,36 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_WebAuthnCoseIdentifiers_h
+#define mozilla_dom_WebAuthnCoseIdentifiers_h
+
+#include "mozilla/dom/WebCryptoCommon.h"
+
+namespace mozilla {
+namespace dom {
+
+// From https://www.iana.org/assignments/cose/cose.xhtml#algorithms
+enum class CoseAlgorithmIdentifier : int32_t {
+  ES256 = -7
+};
+
+static nsresult
+CoseAlgorithmToWebCryptoId(const int32_t& aId, /* out */ nsString& aName)
+{
+  switch(static_cast<CoseAlgorithmIdentifier>(aId)) {
+    case CoseAlgorithmIdentifier::ES256:
+      aName.AssignLiteral(JWK_ALG_ECDSA_P_256);
+      break;
+    default:
+      return NS_ERROR_DOM_NOT_SUPPORTED_ERR;
+  }
+  return NS_OK;
+}
+
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_WebAuthnCoseIdentifiers_h
