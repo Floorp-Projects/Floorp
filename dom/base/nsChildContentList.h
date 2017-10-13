28c09c273b255f0e01663beadf638ee54235c9bf
btian <btian@mozilla.com>
2017-10-13 12:05:54 +0800

    Bug 1406395 - P2: Backout changeset 8f77d260780d (bug 1384661 part 2). r=smaug, a=abillings
    --HG--
    extra : source : 329296ede1cbcbd4969d4c185180f329fe7a4fd5

--- dom/base/nsChildContentList.h
+++ dom/base/nsChildContentList.h
@@ -8,7 +8,7 @@
 #define nsChildContentList_h__
 
 #include "nsISupportsImpl.h"
-#include "nsINodeList.h"      // base class
+#include "nsINodeList.h"            // base class
 #include "js/TypeDecls.h"     // for Handle, Value, JSObject, JSContext
 
 class nsIContent;
@@ -20,7 +20,7 @@ class nsINode;
  * and Item to its existing child list.
  * @see nsIDOMNodeList
  */
-class nsAttrChildContentList : public nsINodeList
+class nsAttrChildContentList final : public nsINodeList
 {
 public:
   explicit nsAttrChildContentList(nsINode* aNode)
@@ -42,7 +42,7 @@ public:
   virtual int32_t IndexOf(nsIContent* aContent) override;
   virtual nsIContent* Item(uint32_t aIndex) override;
 
-  virtual void DropReference()
+  void DropReference()
   {
     mNode = nullptr;
   }
@@ -52,56 +52,13 @@ public:
     return mNode;
   }
 
-protected:
-  virtual ~nsAttrChildContentList() {}
-
 private:
+  ~nsAttrChildContentList() {}
+
   // The node whose children make up the list.
   // This is a non-owning ref which is safe because it's set to nullptr by
   // DropReference() by the node slots get destroyed.
   nsINode* MOZ_NON_OWNING_REF mNode;
-};
-
-class nsParentNodeChildContentList final : public nsAttrChildContentList
-{
-public:
-  explicit nsParentNodeChildContentList(nsINode* aNode)
-    : nsAttrChildContentList(aNode)
-    , mIsCacheValid(false)
-  {
-    ValidateCache();
-  }
-
-  // nsIDOMNodeList interface
-  NS_DECL_NSIDOMNODELIST
-
-  // nsINodeList interface
-  virtual int32_t IndexOf(nsIContent* aContent) override;
-  virtual nsIContent* Item(uint32_t aIndex) override;
-
-  void DropReference() override
-  {
-    InvalidateCache();
-    nsAttrChildContentList::DropReference();
-  }
-
-  void InvalidateCache()
-  {
-    mIsCacheValid = false;
-    mCachedChildArray.Clear();
-  }
-
-private:
-  ~nsParentNodeChildContentList() {}
-
-  // Return true if validation succeeds, false otherwise
-  bool ValidateCache();
-
-  // Whether cached array of child nodes is valid
-  bool mIsCacheValid;
-
-  // Cached array of child nodes
-  AutoTArray<nsIContent*, 8> mCachedChildArray;
 };
 
 #endif /* nsChildContentList_h__ */
