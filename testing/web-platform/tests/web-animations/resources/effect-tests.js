bbcabecbae77e4eeb12ebcf49f0c57f1e135eeb0
Brian Birtles <birtles@gmail.com>
2017-10-13 12:27:56 +0900

    Bug 1406381 - Extend assert_computed_timing_for_each_phase to accommodate negative playback rates; r=hiro
    MozReview-Commit-ID: LpTRS6aMaWw

--- testing/web-platform/tests/web-animations/resources/effect-tests.js
+++ testing/web-platform/tests/web-animations/resources/effect-tests.js
@@ -7,33 +7,57 @@
 //
 //   {
 //     before, // value to test during before phase
-//     active, // value to test during at the very beginning of the active phase
-//             // or undefined if the active duration is zero,
+//     activeBoundary, // value to test at the very beginning of the active
+//                     // phase when playing forwards, or the very end of
+//                     // the active phase when playing backwards.
+//                     // This should be undefined if the active duration of
+//                     // the effect is zero.
 //     after,  // value to test during the after phase or undefined if the
 //             // active duration is infinite
 //   }
 //
 function assert_computed_timing_for_each_phase(animation, property, values) {
   const effect = animation.effect;
+  const timing = effect.getComputedTiming();
+
+  // The following calculations are based on the definitions here:
+  // https://w3c.github.io/web-animations/#animation-effect-phases-and-states
+  const beforeActive = Math.max(Math.min(timing.delay, timing.endTime), 0);
+  const activeAfter =
+    Math.max(Math.min(timing.delay + timing.activeDuration, timing.endTime), 0);
+  const direction = animation.playbackRate < 0 ? 'backwards' : 'forwards';
 
   // Before phase
+  if (direction === 'forwards') {
+    animation.currentTime = beforeActive - 1;
+  } else {
+    animation.currentTime = beforeActive;
+  }
   assert_equals(effect.getComputedTiming()[property], values.before,
                 `Value of ${property} in the before phase`);
 
   // Active phase
   if (effect.getComputedTiming().activeDuration > 0) {
-    animation.currentTime = effect.getComputedTiming().delay;
-    assert_equals(effect.getComputedTiming()[property], values.active,
-                  `Value of ${property} in the active phase`);
+    if (direction === 'forwards') {
+      animation.currentTime = beforeActive;
+    } else {
+      animation.currentTime = activeAfter;
+    }
+    assert_equals(effect.getComputedTiming()[property], values.activeBoundary,
+                  `Value of ${property} at the boundary of the active phase`);
   } else {
-    assert_equals(values.active, undefined,
+    assert_equals(values.activeBoundary, undefined,
                   'Test specifies a value to check during the active phase but'
                   + ' the animation has a zero duration');
   }
 
   // After phase
   if (effect.getComputedTiming().activeDuration !== Infinity) {
-    animation.finish();
+    if (direction === 'forwards') {
+      animation.currentTime = activeAfter;
+    } else {
+      animation.currentTime = activeAfter + 1;
+    }
     assert_equals(effect.getComputedTiming()[property], values.after,
                   `Value of ${property} in the after phase`);
   } else {
