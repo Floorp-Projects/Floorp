03c69a3e8ab0d0f7c2c057cdb91c1f4aa3e3dcca
Mark Banner <standard8@mozilla.com>
2017-10-13 17:04:48 +0100

    Bug 1246896 - Improve detection for mozilla/no-cpows-in-tests.js 'content' - only notify when content is used as an identifier. r=florian
    MozReview-Commit-ID: 1X38Wq4hUmZ

--- tools/lint/eslint/eslint-plugin-mozilla/tests/no-cpows-in-tests.js
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/no-cpows-in-tests.js
@@ -20,23 +20,29 @@ function wrapCode(code, filename = "browser_fake.js") {
   return {code, filename};
 }
 
-function invalidCode(code, item) {
+function invalidCode(code, item, type = "MemberExpression") {
   let message = `${item} is a possible Cross Process Object Wrapper (CPOW).`;
   let obj = wrapCode(code);
-  obj.errors = [{message, type: "MemberExpression"}];
+  obj.errors = [{message, type}];
   return obj;
 }
 
 ruleTester.run("no-cpows-in-tests", rule, {
   valid: [
     "window.document",
-    wrapCode("ContentTask.spawn(browser, null, () => { content.document; });")
+    wrapCode("ContentTask.spawn(browser, null, () => { content.document; });"),
+    wrapCode("let x = cssDocs.tooltip.content.contentDocument;"),
+    wrapCode("function test(content) { let x = content; }"),
+    wrapCode('let content = "content"; foo(content);')
   ],
   invalid: [
     invalidCode("let x = gBrowser.contentWindow;", "gBrowser.contentWindow"),
     invalidCode("let x = gBrowser.contentDocument;", "gBrowser.contentDocument"),
     invalidCode("let x = gBrowser.selectedBrowser.contentWindow;", "gBrowser.selectedBrowser.contentWindow"),
     invalidCode("let x = browser.contentDocument;", "browser.contentDocument"),
-    invalidCode("let x = window.content;", "window.content")
+    invalidCode("let x = window.content;", "window.content"),
+    invalidCode("content.document;", "content.document"),
+    invalidCode("let x = content;", "content", "Identifier"),
+    invalidCode("let x = gBrowser.contentWindow.wrappedJSObject", "gBrowser.contentWindow")
   ]
 });
