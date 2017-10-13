728300d5fb44eccc89710fc4ae1d333daab80509
David Keeler <dkeeler@mozilla.com>
2017-10-13 11:27:30 -0700

    bug 1406396 - work around NSS utils potentially loading spurious root cert modules r=mgoodwin
    NSS command-line utilities may add a built-in root certificate module with the
    name "Root Certs" if run on a profile that has a copy of the module file (which
    is an unexpected configuration in general for Firefox). This can cause breakage.
    To work around this, PSM now simply deletes any module named "Root Certs" at
    startup. In an effort to prevent PSM from deleting unrelated modules
    coincidentally named "Root Certs", we also prevent the user from using the
    Firefox UI to name modules "Root Certs".
    
    MozReview-Commit-ID: ABja3wpShO9

---
+++ security/manager/ssl/tests/unit/test_pkcs11_moduleDB.js
@@ -0,0 +1,28 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+"use strict";
+
+// Tests that adding modules with invalid names are prevented.
+
+// Ensure that the appropriate initialization has happened.
+do_get_profile();
+
+const gModuleDB = Cc["@mozilla.org/security/pkcs11moduledb;1"]
+                    .getService(Ci.nsIPKCS11ModuleDB);
+
+function run_test() {
+  let libraryFile = Services.dirsvc.get("CurWorkD", Ci.nsIFile);
+  libraryFile.append("pkcs11testmodule");
+  libraryFile.append(ctypes.libraryName("pkcs11testmodule"));
+  ok(libraryFile.exists(), "The pkcs11testmodule file should exist");
+
+  let pkcs11ModuleDB = Cc["@mozilla.org/security/pkcs11moduledb;1"]
+                         .getService(Ci.nsIPKCS11ModuleDB);
+  throws(() => pkcs11ModuleDB.addModule("Root Certs", libraryFile.path, 0, 0),
+         /NS_ERROR_ILLEGAL_VALUE/,
+         "Adding a module named 'Root Certs' should fail");
+  throws(() => pkcs11ModuleDB.addModule("", libraryFile.path, 0, 0),
+         /NS_ERROR_ILLEGAL_VALUE/,
+         "Adding a module with an empty name should fail.");
+}
