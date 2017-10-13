f1265f6eb487beb2006d3f9f4722b0acac9eae4d
Michael Froman <mfroman@mozilla.com>
2017-10-12 22:38:01 -0500

    Bug 1405940 - Fix Null Pointer dereference in sigslot::lock_block r=bwc
    Caused by several issues:
    1) We were allowing an answer with modified ufrag/pass to
       begin an ICE restart even if the offer didn't indicate
       it was restarting.
    2) This should no longer happen, but in cases where restart logic
       was started inappropriately, TransportLayerIce::SetParameters
       could get a null stream, and we check for that now.
    
    MozReview-Commit-ID: JFQ1zz3l5wY

--- media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
+++ media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
@@ -727,6 +727,7 @@ JsepSessionImpl::CreateOffer(const JsepOfferOptions& options,
                              std::string* offer)
 {
   mLastError.clear();
+  mLocalIceIsRestarting = options.mIceRestart.isSome() && *(options.mIceRestart);
 
   if (mState != kJsepStateStable) {
     JSEP_SET_ERROR("Cannot create offer in state " << GetStateStr(mState));
@@ -1994,6 +1995,15 @@ JsepSessionImpl::ValidateRemoteDescription(const Sdp& description)
     }
 
     bool differ = mSdpHelper.IceCredentialsDiffer(newMsection, oldMsection);
+
+    // Detect bad answer ICE restart when offer doesn't request ICE restart
+    if (mIsOfferer && differ && !mLocalIceIsRestarting) {
+      JSEP_SET_ERROR("Remote description indicates ICE restart but offer did not "
+                     "request ICE restart (new remote description changes either "
+                     "the ice-ufrag or ice-pwd)");
+      return NS_ERROR_INVALID_ARG;
+    }
+
     // Detect whether all the creds are the same or all are different
     if (!iceCredsDiffer.isSome()) {
       // for the first msection capture whether creds are different or same
