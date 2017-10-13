d682ea8c54c00528e4b7e51b1b0fab4fc3447197
Francois Marier <francois@mozilla.com>
2017-10-12 17:27:36 -0700

    Bug 1385461 - Move URL Classifier protobuf files to match other components. r=dimi
    MozReview-Commit-ID: 5N1iOpocVd8

--- toolkit/components/url-classifier/nsUrlClassifierUtils.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierUtils.cpp
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "chromium/safebrowsing.pb.h"
 #include "nsEscape.h"
 #include "nsString.h"
 #include "nsIURI.h"
@@ -11,7 +12,6 @@
 #include "nsReadableUtils.h"
 #include "plbase64.h"
 #include "nsPrintfCString.h"
-#include "safebrowsing.pb.h"
 #include "mozilla/Sprintf.h"
 #include "mozilla/Mutex.h"
 #include "nsIRedirectHistoryEntry.h"
