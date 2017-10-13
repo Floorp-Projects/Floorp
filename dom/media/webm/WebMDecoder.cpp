8eab523aafcd2c65ece813389b921c6d31228880
James Cheng <jacheng@mozilla.com>
2017-10-13 14:18:29 +0800

    Bug 1407919 - Part2 - Use the helper function to tell if it is a vp8/9 codec string. r=gerald
    MozReview-Commit-ID: G0AKSExgm3R

--- dom/media/webm/WebMDecoder.cpp
+++ dom/media/webm/WebMDecoder.cpp
@@ -10,6 +10,7 @@
 #include "AOMDecoder.h"
 #endif
 #include "MediaContainerType.h"
+#include "VideoUtils.h"
 
 namespace mozilla {
 
@@ -39,9 +40,9 @@ WebMDecoder::IsSupportedType(const MediaContainerType& aContainerType)
     }
     // Note: Only accept VP8/VP9 in a video container type, not in an audio
     // container type.
+
     if (isVideo &&
-        (codec.EqualsLiteral("vp8") || codec.EqualsLiteral("vp8.0") ||
-         codec.EqualsLiteral("vp9") || codec.EqualsLiteral("vp9.0"))) {
+        (IsVP8CodecString(codec) || IsVP9CodecString(codec))) {
       continue;
     }
 #ifdef MOZ_AV1
