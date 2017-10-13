63e8aa981e4600061e5931dbfc748e019fc1086c
Xidorn Quan <me@upsuper.org>
2017-10-13 12:54:18 +1100

    Bug 1407843 part 2 - Move tree pseudo matching code from nsTreeBodyFrame into nsCSSRuleProcessor. r=heycam
    MozReview-Commit-ID: FyPhBLGX2qD

--- layout/xul/tree/nsTreeStyleCache.cpp
+++ layout/xul/tree/nsTreeStyleCache.cpp
@@ -89,7 +89,7 @@ nsTreeStyleCache::GetStyleContext(nsICSSPseudoComparator* aComparator,
     } else {
       newResult = aPresContext->StyleSet()->AsGecko()->
         ResolveXULTreePseudoStyle(aContent->AsElement(), aPseudoElement,
-                                  aContext->AsGecko(), aComparator);
+                                  aContext->AsGecko(), aInputWord);
     }
 
     // Put the style context in our table, transferring the owning reference to the table.
