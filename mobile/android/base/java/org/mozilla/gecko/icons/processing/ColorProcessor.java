ee41415f9b8bd2c2eaf87b2d4e794cad9a47e134
Jan Henning <jh+bugzilla@buttercookie.de>
2017-10-14 19:23:03 +0200

    Bug 1403653 - Part 1 - Refactor getDominantColor. r=nechen
    We want to use the Palette library for getting a fallback accent colour for lightweight themes, however because of bug 1318667, we might have to continue using our own implementation of getDominantColor on x86 devices.
    
    Therefore we move this into BitmapUtils, so we can have a central location from which to switch between our own and the Palette library implementation.
    
    MozReview-Commit-ID: 52WsfZbW12x

--- mobile/android/base/java/org/mozilla/gecko/icons/processing/ColorProcessor.java
+++ mobile/android/base/java/org/mozilla/gecko/icons/processing/ColorProcessor.java
@@ -7,13 +7,10 @@ package org.mozilla.gecko.icons.processing;
 
 import android.graphics.Bitmap;
 import android.support.annotation.ColorInt;
-import android.support.v7.graphics.Palette;
-import android.util.Log;
 
 import org.mozilla.gecko.gfx.BitmapUtils;
 import org.mozilla.gecko.icons.IconRequest;
 import org.mozilla.gecko.icons.IconResponse;
-import org.mozilla.gecko.util.HardwareUtils;
 
 /**
  * Processor implementation to extract the dominant color from the icon and attach it to the icon
@@ -37,36 +34,8 @@ public class ColorProcessor implements Processor {
             return;
         }
 
-        if (HardwareUtils.isX86System()) {
-            // (Bug 1318667) We are running into crashes when using the palette library with
-            // specific icons on x86 devices. They take down the whole VM and are not recoverable.
-            // Unfortunately our release icon is triggering this crash. Until we can switch to a
-            // newer version of the support library where this does not happen, we are using our
-            // own slower implementation.
-            extractColorUsingCustomImplementation(response);
-        } else {
-            extractColorUsingPaletteSupportLibrary(response);
-        }
-    }
-
-    private void extractColorUsingPaletteSupportLibrary(final IconResponse response) {
-        try {
-            final Palette palette = Palette.from(response.getBitmap()).generate();
-            response.updateColor(palette.getVibrantColor(DEFAULT_COLOR) & 0x7FFFFFFF);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            // We saw the palette library fail with an ArrayIndexOutOfBoundsException intermittently
-            // in automation. In this case lets just swallow the exception and move on without a
-            // color. This is a valid condition and callers should handle this gracefully (Bug 1318560).
-            Log.e(LOGTAG, "Palette generation failed with ArrayIndexOutOfBoundsException", e);
-
-            response.updateColor(DEFAULT_COLOR);
-        }
-    }
-
-    private void extractColorUsingCustomImplementation(final IconResponse response) {
-        final int dominantColor = BitmapUtils.getDominantColor(response.getBitmap());
-
-        response.updateColor(dominantColor);
+        final @ColorInt int dominantColor = BitmapUtils.getDominantColor(response.getBitmap(), DEFAULT_COLOR);
+        response.updateColor(dominantColor & 0x7FFFFFFF);
     }
 
     /**
