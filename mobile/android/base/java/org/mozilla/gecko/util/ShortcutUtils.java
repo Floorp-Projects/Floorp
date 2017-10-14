502e8dd95ce11fec5efd11c927930f591c5ed2f6
Jan Henning <jh+bugzilla@buttercookie.de>
2017-10-14 19:24:02 +0200

    Bug 1403653 - Part 0 - Cleanup imports. r=JanH
    MozReview-Commit-ID: KLXJSCNTLSC

--- mobile/android/base/java/org/mozilla/gecko/util/ShortcutUtils.java
+++ mobile/android/base/java/org/mozilla/gecko/util/ShortcutUtils.java
@@ -16,7 +16,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.net.Uri;
 import android.util.Log;
 
 import org.mozilla.gecko.AppConstants.Versions;
@@ -26,7 +25,6 @@ import org.mozilla.gecko.db.BrowserDB;
 import org.mozilla.gecko.db.UrlAnnotations;
 import org.mozilla.gecko.gfx.BitmapUtils;
 
-import java.lang.reflect.Method;
 import java.lang.reflect.Constructor;
 
 public class ShortcutUtils {
