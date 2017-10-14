a5807a6e01207a8a9d2a44c9d7cbf47f5af02f27
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2017-10-13 17:57:15 -0700

    Bug 1408371: report AEC log dir through getter. r=ng,smaug
    MozReview-Commit-ID: 1Tb8nwYzMFt

--- media/webrtc/signaling/src/common/browser_logging/WebRtcLog.h
+++ media/webrtc/signaling/src/common/browser_logging/WebRtcLog.h
@@ -7,7 +7,7 @@
 
 #include "webrtc/common_types.h"
 
-void StartAecLog();
+nsCString StartAecLog();
 void StopAecLog();
 void StartWebRtcLog(uint32_t log_level = webrtc::kTraceDefault);
 void EnableWebRtcLog();
