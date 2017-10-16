bd9a48299cece4a27c347a40e7802d24e6325040
Jan Odvarko <odvarko@gmail.com>
2017-10-16 12:08:28 +0200

    Bug 1005755 - Stop using Connector as a singleton; r=rickychien
    MozReview-Commit-ID: KtBcN1JpEk6
    
    --HG--
    extra : rebase_source : d74a836fcab1352031528136e45e382a830a9f5e

--- devtools/client/netmonitor/src/connector/chrome-connector.js
+++ devtools/client/netmonitor/src/connector/chrome-connector.js
@@ -35,6 +35,14 @@ class ChromeConnector {
     this.connector.disconnect();
   }
 
+  pause() {
+    this.disconnect();
+  }
+
+  resume() {
+    this.setup();
+  }
+
   /**
    * currently all events are about "navigation" is not support on CDP
    */
