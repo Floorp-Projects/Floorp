5a130ee36eda3d89a379c73973e50fb4b3ada8af
Andreas Tolfsen <ato@sny.no>
2017-10-16 17:47:35 +0100

    Bug 1408508 - Move Context to browser module. r=maja_zf
    To avoid circular dependencies, where file A depends on B depending
    on A, we should strive towards fewer inter-dependencies and more
    autonomy between modules.
    
    For example, if testing/marionette/browser.js needs access to
    the Context enum it can currently only attain it by importing
    testing/marionette/driver.js.  Because driver.js imports browser.js,
    we create a circular dependency and Cu.import enters an infinite
    import recursion.
    
    MozReview-Commit-ID: LGiA9sy9xrd

--- testing/marionette/browser.js
+++ testing/marionette/browser.js
@@ -16,12 +16,50 @@ const {
 Cu.import("chrome://marionette/content/frame.js");
 const {WindowState} = Cu.import("chrome://marionette/content/wm.js", {});
 
-this.EXPORTED_SYMBOLS = ["browser", "WindowState"];
+this.EXPORTED_SYMBOLS = ["browser", "Context", "WindowState"];
 
 /** @namespace */
 this.browser = {};
 
 const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+
+/**
+ * Variations of Marionette contexts.
+ *
+ * Choosing a context through the <tt>Marionette:SetContext</tt>
+ * command directs all subsequent browsing context scoped commands
+ * to that context.
+ *
+ * @enum
+ */
+const Context = {
+  Chrome: "chrome",
+  Content: "content",
+};
+this.Context = Context;
+
+/**
+ * Gets the correct context from a string.
+ *
+ * @param {string} s
+ *     Context string serialisation.
+ *
+ * @return {Context}
+ *     Context.
+ *
+ * @throws {TypeError}
+ *     If <var>s</var> is not a context.
+ */
+Context.fromString = function(s) {
+  switch (s) {
+    case "chrome":
+      return Context.Chrome;
+    case "content":
+      return Context.Content;
+    default:
+      throw new TypeError(`Unknown context: ${s}`);
+  }
+};
 
 /**
  * Get the <code>&lt;xul:browser&gt;</code> for the specified tab.
