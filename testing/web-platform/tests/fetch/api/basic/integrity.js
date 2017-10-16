d4b41436a06a7fd128beae1475876ad89916f8ae
James Graham <james@hoppipolla.co.uk>
2017-10-16 16:02:25 +0100

    Bug 1410245 - Update web-platform-tests to revision a372dce582c21c911056273eec042fd9073cbd60, a=testonly
    MozReview-Commit-ID: CQzdR66hsXZ

--- testing/web-platform/tests/fetch/api/basic/integrity.js
+++ testing/web-platform/tests/fetch/api/basic/integrity.js
@@ -8,11 +8,10 @@ function integrity(desc, url, integrity, initRequestMode, shouldPass) {
   if (!!initRequestMode && initRequestMode !== "") {
     fetchRequestInit.mode = initRequestMode;
   }
-  var fetchPromise = fetch(url, fetchRequestInit);
 
   if (shouldPass) {
     promise_test(function(test) {
-      return fetchPromise.then(function(resp) {
+      return fetch(url, fetchRequestInit).then(function(resp) {
         if (initRequestMode !== "no-cors") {
           assert_equals(resp.status, 200, "Response's status is 200");
         } else {
@@ -23,7 +22,7 @@ function integrity(desc, url, integrity, initRequestMode, shouldPass) {
     }, desc);
   } else {
     promise_test(function(test) {
-      return promise_rejects(test, new TypeError(), fetchPromise);
+      return promise_rejects(test, new TypeError(), fetch(url, fetchRequestInit));
     }, desc);
   }
 }
