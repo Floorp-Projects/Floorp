1b5503bd8a5e79711536b8d09d7f4b9d69c78140
Boris Zbarsky <bzbarsky@mit.edu>
2017-10-16 10:58:09 -0400

    Bug 1354730.  Disable named property object (aka global scope polluter) behavior for all Xrays.  r=kmag
    MozReview-Commit-ID: KmbXFfSzH0N

--- js/xpconnect/tests/unit/test_xray_named_element_access.js
+++ js/xpconnect/tests/unit/test_xray_named_element_access.js
@@ -6,8 +6,6 @@ const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
 Cu.import("resource://gre/modules/Preferences.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
-const XRAY_PREF = "dom.allow_named_properties_object_for_xrays";
-
 add_task(async function() {
   let webnav = Services.appShell.createWindowlessBrowser(false);
 
@@ -21,18 +19,7 @@ add_task(async function() {
 
   window.document.body.innerHTML = '<div id="foo"></div>';
 
-  equal(Preferences.get(XRAY_PREF), 1, "Should have pref=1 by default");
-
-  Preferences.set(XRAY_PREF, 0);
-  equal(typeof window.foo, "object", "Should have named X-ray property access with pref=0");
-  equal(typeof unwrapped.foo, "object", "Should always have non-X-ray named property access");
-
-  Preferences.set(XRAY_PREF, 1);
-  equal(typeof window.foo, "object", "Should have named X-ray property access with pref=1");
-  equal(typeof unwrapped.foo, "object", "Should always have non-X-ray named property access");
-
-  Preferences.set(XRAY_PREF, 2);
-  equal(window.foo, undefined, "Should not have named X-ray property access with pref=2");
+  equal(window.foo, undefined, "Should not have named X-ray property access");
   equal(typeof unwrapped.foo, "object", "Should always have non-X-ray named property access");
 
   webnav.close();
