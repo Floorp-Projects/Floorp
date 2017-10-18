a0f777b70656b5437fbb847b41ce3d82a1560a6d
Alex Gaynor <agaynor@mozilla.com>
2017-10-18 10:27:34 -0400

    Bug 1409747 - fixed compilation on macOS with the sandbox disabled; r=haik
    MozReview-Commit-ID: F9lNO8OPKS4

--- dom/media/gmp/GMPChild.cpp
+++ dom/media/gmp/GMPChild.cpp
@@ -127,7 +127,7 @@ GetPluginFile(const nsAString& aPluginPath,
   return GetPluginFile(aPluginPath, unusedlibDir, aLibFile);
 }
 
-#if defined(XP_MACOSX) && defined(MOZ_GMP_SANDBOX)
+#if defined(XP_MACOSX)
 static nsCString
 GetNativeTarget(nsIFile* aFile)
 {
@@ -142,6 +142,7 @@ GetNativeTarget(nsIFile* aFile)
   return path;
 }
 
+#if defined(MOZ_GMP_SANDBOX)
 static bool
 GetPluginPaths(const nsAString& aPluginPath,
                nsCString &aPluginDirectoryPath,
@@ -230,7 +231,8 @@ GMPChild::SetMacSandboxInfo(MacSandboxPluginType aPluginType)
   mGMPLoader->SetSandboxInfo(&info);
   return true;
 }
-#endif // XP_MACOSX && MOZ_GMP_SANDBOX
+#endif // MOZ_GMP_SANDBOX
+#endif // XP_MACOSX
 
 bool
 GMPChild::Init(const nsAString& aPluginPath,
