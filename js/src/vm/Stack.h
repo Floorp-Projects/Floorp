1030d1700b93d6e0fffdb99716a00426724ceef9
Jan de Mooij <jdemooij@mozilla.com>
2017-10-18 17:12:47 +0100

    Bug 1409441 part 3 - Inline some Activation/JitActivation methods. r=bbouvier

--- js/src/vm/Stack.h
+++ js/src/vm/Stack.h
@@ -1322,8 +1322,8 @@ class Activation
     inline bool hasWasmExitFP() const;
 
     inline bool isProfiling() const;
-    void registerProfiling();
-    void unregisterProfiling();
+    inline void registerProfiling();
+    inline void unregisterProfiling();
 
     InterpreterActivation* asInterpreter() const {
         MOZ_ASSERT(isInterpreter());
