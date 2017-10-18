3bc133f2e6e18c0c53b4b6a7bfec0aace6af493d
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-18 18:28:01 +0200

    Backed out changeset c654a712755b (bug 1408461)
    --HG--
    extra : rebase_source : 623157bbabeec362882acc296b4d38e9ea20ddd7

--- third_party/rust/bitflags/src/example_generated.rs
+++ third_party/rust/bitflags/src/example_generated.rs
@@ -9,8 +9,8 @@ bitflags! {
         const FLAG_A       = 0b00000001;
         const FLAG_B       = 0b00000010;
         const FLAG_C       = 0b00000100;
-        const FLAG_ABC     = Self::FLAG_A.bits
-                           | Self::FLAG_B.bits
-                           | Self::FLAG_C.bits;
+        const FLAG_ABC     = FLAG_A.bits
+                           | FLAG_B.bits
+                           | FLAG_C.bits;
     }
 }
