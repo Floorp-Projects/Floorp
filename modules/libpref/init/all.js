c3529b3301f4c77270292190d2624932c9b439f7
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-10-19 15:18:51 +0200

    Bug 1392961 - Add preference to make VP9 the preferred video codec. r=jesup
    Behaviour is controlled through the media.navigator.video.vp9_preferred preference.
    
    MozReview-Commit-ID: J06ArFYNmTk

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -485,6 +485,7 @@ pref("media.navigator.video.h264.max_br", 0);
 pref("media.navigator.video.h264.max_mbps", 0);
 pref("media.peerconnection.video.h264_enabled", false);
 pref("media.peerconnection.video.vp9_enabled", true);
+pref("media.peerconnection.video.vp9_preferred", false);
 pref("media.getusermedia.aec", 1);
 pref("media.getusermedia.browser.enabled", false);
 pref("media.getusermedia.channels", 0);
