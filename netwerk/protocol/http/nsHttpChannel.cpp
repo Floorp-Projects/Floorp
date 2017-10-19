ebc794816a2e77ebe300ec258207f51fffb062b2
Dragana Damjanovic <dd.mozilla@gmail.com>
2017-10-19 00:06:00 -0400

    Bug 1406908 - Implement 425 (Too Early). r=mcmanus, r=mayhemer

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -2637,6 +2637,11 @@ nsHttpChannel::ContinueProcessResponse2(nsresult rv)
             mAuthRetryPending = true; // see DoAuthRetry
         }
         break;
+
+    case 425:
+        // Do not cache 425.
+        CloseCacheEntry(false);
+        MOZ_FALLTHROUGH; // process normally
     default:
         rv = ProcessNormal();
         MaybeInvalidateCacheEntryForSubsequentGet();
