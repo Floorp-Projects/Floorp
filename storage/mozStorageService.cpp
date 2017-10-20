fdba0aec6798557087e7a94c84192c0bd3042ac4
Gabriele Svelto <gsvelto@mozilla.com>
2017-10-20 11:00:21 +0200

    Bug 1402519 - Remove MOZ_CRASHREPORTER directives from storage; r=mak
    MozReview-Commit-ID: HYpSWoJaEAm

--- storage/mozStorageService.cpp
+++ storage/mozStorageService.cpp
@@ -12,6 +12,7 @@
 #include "nsAutoPtr.h"
 #include "nsCollationCID.h"
 #include "nsEmbedCID.h"
+#include "nsExceptionHandler.h"
 #include "nsThreadUtils.h"
 #include "mozStoragePrivateHelpers.h"
 #include "nsIXPConnect.h"
@@ -25,10 +26,6 @@
 
 #include "sqlite3.h"
 #include "mozilla/AutoSQLiteLifetime.h"
-
-#ifdef MOZ_CRASHREPORTER
-#include "nsExceptionHandler.h"
-#endif
 
 #ifdef XP_WIN
 // "windows.h" was included and it can #define lots of things we care about...
@@ -801,13 +798,11 @@ Service::Observe(nsISupports *, const char *aTopic, const char16_t *)
       getConnections(connections);
       for (uint32_t i = 0, n = connections.Length(); i < n; i++) {
         if (!connections[i]->isClosed()) {
-#ifdef MOZ_CRASHREPORTER
           // getFilename is only the leaf name for the database file,
           // so it shouldn't contain privacy-sensitive information.
           CrashReporter::AnnotateCrashReport(
             NS_LITERAL_CSTRING("StorageConnectionNotClosed"),
             connections[i]->getFilename());
-#endif
 #ifdef DEBUG
           printf_stderr("Storage connection not closed: %s",
                         connections[i]->getFilename().get());
