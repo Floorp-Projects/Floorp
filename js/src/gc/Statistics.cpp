86e513ba69ed76bd0e60c212f5f0945a5a170d48
Phil Ringnalda <philringnalda@gmail.com>
2017-10-19 20:40:31 -0700

    Backed out changeset 3159df8aca1a (bug 1386511) for browser_TelemetryGC.js timeouts
    MozReview-Commit-ID: 9OFS7FTjVis

--- js/src/gc/Statistics.cpp
+++ js/src/gc/Statistics.cpp
@@ -553,14 +553,6 @@ Statistics::renderNurseryJson(JSRuntime* rt) const
 UniqueChars
 Statistics::renderJsonMessage(uint64_t timestamp, bool includeSlices) const
 {
-    /*
-     * The format of the JSON message is specified by the GCMajorMarkerPayload
-     * type in perf.html
-     * https://github.com/devtools-html/perf.html/blob/master/src/types/markers.js#L62
-     *
-     * All the properties listed here are created within the timings property
-     * of the GCMajor marker.
-     */
     if (aborted)
         return DuplicateString("{status:\"aborted\"}"); // May return nullptr
 
@@ -570,11 +562,10 @@ Statistics::renderJsonMessage(uint64_t timestamp, bool includeSlices) const
     JSONPrinter json(printer);
 
     json.beginObject();
-    json.property("status", "completed");
     formatJsonDescription(timestamp, json);
 
     if (includeSlices) {
-        json.beginListProperty("slices_list");
+        json.beginListProperty("slices");
         for (unsigned i = 0; i < slices_.length(); i++)
             formatJsonSlice(i, json);
         json.endList();
@@ -618,13 +609,11 @@ Statistics::formatJsonDescription(uint64_t timestamp, JSONPrinter& json) const
     json.property("scc_sweep_max_pause", sccLongest, JSONPrinter::MILLISECONDS);
 
     json.property("nonincremental_reason", ExplainAbortReason(nonincrementalReason_));
-    json.property("allocated", uint64_t(preBytes)/1024/1024);
-    json.property("allocated_bytes", preBytes);
+    json.property("allocated", uint64_t(preBytes) / 1024 / 1024);
     json.property("added_chunks", getCount(STAT_NEW_CHUNK));
     json.property("removed_chunks", getCount(STAT_DESTROY_CHUNK));
     json.property("major_gc_number", startingMajorGCNumber);
     json.property("minor_gc_number", startingMinorGCNumber);
-    json.property("slice_number", startingSliceNumber);
 }
 
 void
@@ -890,7 +879,6 @@ Statistics::beginGC(JSGCInvocationKind kind)
 
     preBytes = runtime->gc.usage.gcBytes();
     startingMajorGCNumber = runtime->gc.majorGCCount();
-    startingSliceNumber = runtime->gc.gcNumber();
 }
 
 void
