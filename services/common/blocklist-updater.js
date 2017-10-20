0a687f249e187d60ae44f40c214bfca2e8055ef9
Tom Prince <mozilla@hocat.ca>
2017-10-19 21:26:20 -0600

    Bug 1404577: Actually disable loading blocklist dump in test_cert_blocklist.js. r=leplatrem
    The dump file isn't in the expected location in firefox tests, but is in
    thunderbird tests, so the preference to disable loading wasn't originally
    implemented.
    
    MozReview-Commit-ID: HvFqfC69yMQ
    
    --HG--
    extra : rebase_source : 1d358292f0ab94299e444f4d3e3454a2259d1a64

--- services/common/blocklist-updater.js
+++ services/common/blocklist-updater.js
@@ -153,6 +153,7 @@ this.checkVersions = async function() {
   Services.prefs.setIntPref(PREF_BLOCKLIST_CLOCK_SKEW_SECONDS, clockDifference);
   Services.prefs.setIntPref(PREF_BLOCKLIST_LAST_UPDATE, serverTimeMillis / 1000);
 
+  const loadDump = Services.prefs.getBoolPref(PREF_BLOCKLIST_LOAD_DUMP, true);
   // Iterate through the collections version info and initiate a synchronization
   // on the related blocklist client.
   let firstError;
@@ -161,7 +162,7 @@ this.checkVersions = async function() {
     const client = gBlocklistClients[collection];
     if (client && client.bucketName == bucket) {
       try {
-        await client.maybeSync(lastModified, serverTimeMillis);
+        await client.maybeSync(lastModified, serverTimeMillis, {loadDump});
       } catch (e) {
         if (!firstError) {
           firstError = e;
