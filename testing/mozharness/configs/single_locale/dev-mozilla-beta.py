1110ce50e6f4b87c317ee3f895f166a73c1952d4
Johan Lorenzo <jlorenzo@mozilla.com>
2017-10-20 16:41:29 +0200

    Bug 1407331 - Staging releases: Remove *-dev update channel r=bhearsum
    MozReview-Commit-ID: 2hevL6EQjmq

--- testing/mozharness/configs/single_locale/dev-mozilla-beta.py
+++ testing/mozharness/configs/single_locale/dev-mozilla-beta.py
@@ -1,7 +1,7 @@
 config = {
     "branch": "jamun",
     "nightly_build": True,
-    "update_channel": "beta-dev",
+    "update_channel": "beta",
 
     # l10n
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
