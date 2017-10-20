cdff2597b7c0b3c6d17a7c4979282d9364ea3d78
Johan Lorenzo <jlorenzo@mozilla.com>
2017-10-20 16:41:29 +0200

    Bug 1407331 - Staging releases: Remove *-dev update channel r=bhearsum
    MozReview-Commit-ID: 2hevL6EQjmq

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -19,11 +19,8 @@ if test "$OS_ARCH" = "WINNT"; then
     if test "$MOZ_UPDATE_CHANNEL" = "nightly" -o \
             "$MOZ_UPDATE_CHANNEL" = "nightly-try" -o \
             "$MOZ_UPDATE_CHANNEL" = "aurora" -o \
-            "$MOZ_UPDATE_CHANNEL" = "aurora-dev" -o \
             "$MOZ_UPDATE_CHANNEL" = "beta" -o \
-            "$MOZ_UPDATE_CHANNEL" = "beta-dev" -o \
-            "$MOZ_UPDATE_CHANNEL" = "release" -o \
-            "$MOZ_UPDATE_CHANNEL" = "release-dev"; then
+            "$MOZ_UPDATE_CHANNEL" = "release"; then
       if ! test "$MOZ_DEBUG"; then
         MOZ_STUB_INSTALLER=1
       fi
