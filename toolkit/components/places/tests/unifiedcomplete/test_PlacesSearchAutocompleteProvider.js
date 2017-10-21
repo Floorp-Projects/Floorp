7afeed5141deea0fcd92b41cf54a25b472a62d87
Leo <lejara@myseneca.ca>
2017-10-21 17:26:03 -0400

    Bug 1403502 - Move some xpcshell-tests from toolkit/components/places/unit to unifiedcomplete. r=Standard8

--- toolkit/components/places/tests/unifiedcomplete/test_PlacesSearchAutocompleteProvider.js
+++ toolkit/components/places/tests/unifiedcomplete/test_PlacesSearchAutocompleteProvider.js
@@ -4,14 +4,16 @@
 
 Cu.import("resource://gre/modules/PlacesSearchAutocompleteProvider.jsm");
 
-function run_test() {
-  // Tell the search service we are running in the US.  This also has the
-  // desired side-effect of preventing our geoip lookup.
-  Services.prefs.setBoolPref("browser.search.isUS", true);
-  Services.prefs.setCharPref("browser.search.countryCode", "US");
-  Services.prefs.setBoolPref("browser.search.geoSpecificDefaults", false);
-  run_next_test();
-}
+add_task(async function() {
+    // Tell the search service we are running in the US.  This also has the
+    // desired side-effect of preventing our geoip lookup.
+   Services.prefs.setBoolPref("browser.search.isUS", true);
+   Services.prefs.setCharPref("browser.search.countryCode", "US");
+   Services.prefs.setBoolPref("browser.search.geoSpecificDefaults", false);
+
+   Services.search.restoreDefaultEngines();
+   Services.search.resetToOriginalDefaultEngine();
+});
 
 add_task(async function search_engine_match() {
   let engine = await promiseDefaultSearchEngine();
