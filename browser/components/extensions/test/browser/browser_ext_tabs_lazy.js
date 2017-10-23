5d8bfc22054d374f0e5b37fda17cacb9ab176435
Thomas Nguyen <tnguyen@mozilla.com>
2017-10-23 18:15:40 +0800

    Bug 1345433 - Ensure tests load pass valid triggeringPrincipal. r=ckerschb
    MozReview-Commit-ID: LWcP7drDPwL

--- browser/components/extensions/test/browser/browser_ext_tabs_lazy.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_lazy.js
@@ -3,8 +3,8 @@
 const SESSION = {
   windows: [{
     tabs: [
-      {entries: [{url: "about:blank"}]},
-      {entries: [{url: "https://example.com/"}]},
+      {entries: [{url: "about:blank", triggeringPrincipal_base64}]},
+      {entries: [{url: "https://example.com/", triggeringPrincipal_base64}]},
     ],
   }],
 };
