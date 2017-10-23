69590f4f0cc05719da4994b55a27a8cb0d790528
Panos Astithas <past@mozilla.com>
2017-10-23 12:30:00 -0400

    Bug 1406234 - Remove historical search suggestions. r=mak, a=ritu
    MozReview-Commit-ID: loYMDaVqca

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -310,7 +310,7 @@ pref("browser.urlbar.maxRichResults", 10);
 pref("browser.urlbar.delay", 50);
 
 // The maximum number of historical search results to show.
-pref("browser.urlbar.maxHistoricalSearchSuggestions", 1);
+pref("browser.urlbar.maxHistoricalSearchSuggestions", 0);
 
 // The default behavior for the urlbar can be configured to use any combination
 // of the match filters with each additional filter adding more results (union).
