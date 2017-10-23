8b79428eb012033a5fa46d67c380e051709ca300
JW Wang <jwwang@mozilla.com>
2017-10-23 17:12:07 +0800

    Bug 1411808. P1 - run Update() loops off the main thread. r=gerald
    Note we will fix bugs required to run Update() off the main thread
    in the following patches.
    
    MozReview-Commit-ID: CYwT5kDjD9R
    
    --HG--
    extra : rebase_source : edef59353b5f56d5aecb6d58f5cc2a8dfb09c317
    extra : intermediate-source : 995a34fb6af54d3f4ad2de479d7777860a957f98
    extra : source : 8d86a14f877cf83584cf1e57020568037a35154a

--- dom/media/MediaCache.cpp
+++ dom/media/MediaCache.cpp
@@ -1174,7 +1174,7 @@ MediaCache::PredictNextUseForIncomingData(MediaCacheStream* aStream)
 void
 MediaCache::Update()
 {
-  NS_ASSERTION(NS_IsMainThread(), "Only call on main thread");
+  MOZ_ASSERT(sThread->IsOnCurrentThread());
 
   ReentrantMonitorAutoEnter mon(mReentrantMonitor);
 
@@ -1565,7 +1565,7 @@ MediaCache::QueueUpdate()
   // shutdown and get freed in the final cycle-collector cleanup.  So
   // don't leak a runnable in that case.
   nsCOMPtr<nsIRunnable> event = new UpdateEvent(this);
-  SystemGroup::Dispatch(TaskCategory::Other, event.forget());
+  sThread->Dispatch(event.forget());
 }
 
 void
