a9b4fa54d41077134b3a41496c6e6c2f2faba048
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-23 23:43:07 +0200

    Backed out changeset 2d9d7435eb63 (bug 1409871)

--- gfx/layers/client/ContentClient.cpp
+++ gfx/layers/client/ContentClient.cpp
@@ -148,7 +148,8 @@ ContentClient::BeginPaintBuffer(PaintedLayer* aLayer,
   if (mBuffer) {
     if (mBuffer->Lock(LockMode())) {
       // Do not modify result.mRegionToDraw or result.mContentType after this call.
-      // Do not modify the back buffer's bufferRect, bufferRotation, or didSelfCopy.
+      // Do not modify mBufferRect, mBufferRotation, or mDidSelfCopy,
+      // or call CreateBuffer before this call.
       FinalizeFrame(result.mRegionToDraw);
     } else {
       // Abandon everything and redraw it all. Ideally we'd reallocate and copy
