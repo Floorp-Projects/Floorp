9956539bd6839303759955c5d7bd63ce17fb00f5
Zibi Braniecki <zbraniecki@mozilla.com>
2017-10-23 08:04:24 -0700

    Bug 1362617 - Generalize MOZ_CHROME_MULTILOCALE to work for browser as well. r=gps,mshal,Pike,ted
    MozReview-Commit-ID: 80ldQPhNyi7

--- toolkit/mozapps/installer/packager.mk
+++ toolkit/mozapps/installer/packager.mk
@@ -32,7 +32,7 @@ export USE_ELF_HACK ELF_HACK_FLAGS
 # Override the value of OMNIJAR_NAME from config.status with the value
 # set earlier in this file.
 
-stage-package: $(MOZ_PKG_MANIFEST) $(MOZ_PKG_MANIFEST_DEPS)
+stage-package: multilocale.json locale-manifest.in $(MOZ_PKG_MANIFEST) $(MOZ_PKG_MANIFEST_DEPS)
 	OMNIJAR_NAME=$(OMNIJAR_NAME) \
 	NO_PKG_FILES="$(NO_PKG_FILES)" \
 	$(PYTHON) $(MOZILLA_DIR)/toolkit/mozapps/installer/packager.py $(DEFINES) $(ACDEFINES) \
@@ -182,3 +182,47 @@ hg-bundle:
 
 source-upload:
 	$(MAKE) upload UPLOAD_FILES='$(SOURCE_UPLOAD_FILES)' CHECKSUM_FILE='$(SOURCE_CHECKSUM_FILE)'
+
+
+ALL_LOCALES = $(if $(filter en-US,$(LOCALES)),$(LOCALES),$(LOCALES) en-US)
+LOCALE_LIST = $(foreach l,$(ALL_LOCALES),"$(l)")
+
+# Firefox uses @RESPATH@.
+# Fennec uses @BINPATH@ and doesn't have the @RESPATH@ variable defined.
+ifeq ($(MOZ_BUILD_APP),mobile/android)
+BASE_PATH:=@BINPATH@
+MULTILOCALE_DIR = $(DIST)/$(BINPATH)/res
+else
+BASE_PATH:=@RESPATH@
+MULTILOCALE_DIR = $(DIST)/$(RESPATH)/res
+endif
+
+# This version of the target uses MOZ_CHROME_MULTILOCALE to build multilocale.json
+# and places it in dist/bin/res - it should be used when packaging a build.
+multilocale.json: LOCALES?=$(MOZ_CHROME_MULTILOCALE)
+multilocale.json:
+	@$(MKDIR) -p "$(MULTILOCALE_DIR)"
+	echo '{"locales": [$(subst $(SPACE),$(COMMA)$(SPACE),$(LOCALE_LIST))]}' \
+	  > $(MULTILOCALE_DIR)/multilocale.json
+
+# This version of the target uses AB_CD to build multilocale.json and places it
+# in the $(XPI_NAME)/res dir - it should be used when repackaging a build.
+multilocale.json-%: LOCALES?=$(AB_CD)
+multilocale.json-%: MULTILOCALE_DIR=$(DIST)/xpi-stage/$(XPI_NAME)/res
+multilocale.json-%:
+	@$(MKDIR) -p "$(MULTILOCALE_DIR)"
+	echo '{"locales": [$(subst $(SPACE),$(COMMA)$(SPACE),$(LOCALE_LIST))]}' \
+	  > $(MULTILOCALE_DIR)/multilocale.json
+
+locale-manifest.in: LOCALES?=$(MOZ_CHROME_MULTILOCALE)
+locale-manifest.in: $(GLOBAL_DEPS) FORCE
+	printf '\n[multilocale]\n' > $@
+	printf '$(BASE_PATH)/res/multilocale.json\n' >> $@
+	for LOCALE in $(ALL_LOCALES) ;\
+	do \
+	  for ENTRY in $(MOZ_CHROME_LOCALE_ENTRIES) ;\
+		do \
+		  printf "$$ENTRY""$$LOCALE"'@JAREXT@\n' >> $@; \
+		  printf "$$ENTRY""$$LOCALE"'.manifest\n' >> $@; \
+	  done \
+	done
