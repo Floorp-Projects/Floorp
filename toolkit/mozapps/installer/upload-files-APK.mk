1cc7b428303e8a61eeaa8f53fa839a20db58e0fa
Nick Alexander <nalexander@mozilla.com>
2017-10-23 13:24:58 -0700

    Bug 1411688 - Part 1: Make --with-gradle handle single-locale repacks. r=snorp
    Single-locale repacks do the following:
    
    Download existing APK; unzip APK; update l10n resources; |mach package| with IS_LANGUAGE_REPACK=1.
    
    This is pretty hard to accommodate, but we can try.  The key issues
    here are to recognize when IS_LANGUAGE_REPACK=1 and not ask for l10n
    resources (in particular, strings.xml) to be generated.
    
    We do need to include the freshly built classes.dex when repackaging,
    because newer Gradle/aapt doesn't preserve the R.java IDs.
    
    MozReview-Commit-ID: 9FvQtmPOUjg

--- toolkit/mozapps/installer/upload-files-APK.mk
+++ toolkit/mozapps/installer/upload-files-APK.mk
@@ -94,6 +94,7 @@ repackage_fennec = \
       $(UNPACKAGE) \
       $(GECKO_APP_AP_PATH)/gecko-nodeps.ap_ \
     --omnijar $(MOZ_PKG_DIR)/$(OMNIJAR_NAME) \
+    --classes-dex $(GECKO_APP_AP_PATH)/classes.dex \
     --output $(PACKAGE:.apk=-unsigned-unaligned.apk) && \
   $(call RELEASE_SIGN_ANDROID_APK,$(PACKAGE:.apk=-unsigned-unaligned.apk),$(PACKAGE))
 
