d46057155b4974c4a865d8c2a2044b9abddd8be4
Sebastian Kaspari <s.kaspari@gmail.com>
2017-10-24 12:44:41 +0200

    [focus] Buddybuild prebuild: Add a dummy Adjust token so that we can build release builds and run unit tests for them. (https://github.com/mozilla-mobile/focus-android/pull/1490)

--- mobile/android/focus-android/buddybuild_prebuild.sh
+++ mobile/android/focus-android/buddybuild_prebuild.sh
@@ -3,4 +3,9 @@
 # Pre-build script for buddybuild
 # http://docs.buddybuild.com/docs/custom-prebuild-and-postbuild-steps#section-pre-build
 
+# Make sure the translations are not missing placeholders etc.
 python tools/l10n/check_translations.py
+
+# Add a dummy Adjust token so that we can build release builds and run unit tests for them.
+echo "--" > .adjust_token
+
