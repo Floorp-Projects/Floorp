f0d93073ce29d0755d6c1528c2cdfd5da260b343
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-24 18:48:42 +0200

    Backed out changeset cd2147b5bc3d (bug 1407695)

--- testing/web-platform/tests/webdriver/tests/cookies/get_named_cookie.py
+++ testing/web-platform/tests/webdriver/tests/cookies/get_named_cookie.py
@@ -1,6 +1,5 @@
 from tests.support.inline import inline
 from tests.support.fixtures import clear_all_cookies
-from datetime import datetime, timedelta
 
 def test_get_named_session_cookie(session, url):
     session.url = url("/common/blank.html")
@@ -31,35 +30,6 @@ def test_get_named_session_cookie(session, url):
 
     assert cookie["name"] == "foo"
     assert cookie["value"] == "bar"
-
-def test_get_named_cookie(session, url):
-    session.url = url("/common/blank.html")
-    clear_all_cookies(session)
-
-    # same formatting as Date.toUTCString() in javascript
-    utc_string_format = "%a, %d %b %Y %H:%M:%S"
-    a_year_from_now = (datetime.utcnow() + timedelta(days=365)).strftime(utc_string_format)
-    session.execute_script("document.cookie = 'foo=bar;expires=%s'" % a_year_from_now)
-
-    result = session.transport.send("GET", "session/%s/cookie" % session.session_id)
-    assert result.status == 200
-    assert "value" in result.body
-    assert isinstance(result.body["value"], list)
-    assert len(result.body["value"]) == 1
-    assert isinstance(result.body["value"][0], dict)
-
-    cookie = result.body["value"][0]
-    assert "name" in cookie
-    assert isinstance(cookie["name"], basestring)
-    assert "value" in cookie
-    assert isinstance(cookie["value"], basestring)
-    assert "expiry" in cookie
-    assert isinstance(cookie["expiry"], (int, long))
-
-    assert cookie["name"] == "foo"
-    assert cookie["value"] == "bar"
-    # convert from seconds since epoch
-    assert datetime.utcfromtimestamp(cookie["expiry"]).strftime(utc_string_format) == a_year_from_now
 
 def test_duplicated_cookie(session, url):
     session.url = url("/common/blank.html")
@@ -95,3 +65,4 @@ def test_duplicated_cookie(session, url):
 
     assert cookie["name"] == "hello"
     assert cookie["value"] == "newworld"
+
