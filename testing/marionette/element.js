4b10cb69dd81c721f2759486ab44deb5e33aa374
Andreas Tolfsen <ato@sny.no>
2017-10-24 17:38:55 +0100

    Bug 1410652 - Fix various API documentation in element module. r=whimboo
    MozReview-Commit-ID: AaGnolglX5n
    
    --HG--
    extra : rebase_source : de1c338530fbbb68f695c2af8de53a9fe9c1c8e0

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -90,8 +90,8 @@ element.Strategy = {
  * Stores known/seen elements and their associated web element
  * references.
  *
- * Elements are added by calling |add(el)| or |addAll(elements)|, and
- * may be queried by their web element reference using |get(element)|.
+ * Elements are added by calling {@link #add()} or {@link addAll()},
+ * and may be queried by their web element reference using {@link get()}.
  *
  * @class
  * @memberof element
@@ -117,7 +117,7 @@ element.Store = class {
    *
    * @return {Array.<WebElement>}
    *     List of the web element references associated with each element
-   *     from |els|.
+   *     from <var>els</var>.
    */
   addAll(els) {
     let add = this.add.bind(this);
@@ -253,11 +253,11 @@ element.Store = class {
  * document root or a given node.
  *
  * If |timeout| is above 0, an implicit search technique is used.
- * This will wait for the duration of |timeout| for the element
- * to appear in the DOM.
+ * This will wait for the duration of <var>timeout</var> for the
+ * element to appear in the DOM.
  *
- * See the |element.Strategy| enum for a full list of supported
- * search strategies that can be passed to |strategy|.
+ * See the {@link element.Strategy} enum for a full list of supported
+ * search strategies that can be passed to <var>strategy</var>.
  *
  * Available flags for <var>opts</var>:
  *
@@ -774,7 +774,7 @@ element.isSelected = function(el) {
  *     X- and Y coordinates.
  *
  * @throws TypeError
- *     If |xOffset| or |yOffset| are not numbers.
+ *     If <var>xOffset</var> or <var>yOffset</var> are not numbers.
  */
 element.coordinates = function(
     node, xOffset = undefined, yOffset = undefined) {
@@ -811,7 +811,7 @@ element.coordinates = function(
  *     the target's bounding box.
  *
  * @return {boolean}
- *     True if if |el| is in viewport, false otherwise.
+ *     True if if <var>el</var> is in viewport, false otherwise.
  */
 element.inViewport = function(el, x = undefined, y = undefined) {
   let win = el.ownerGlobal;
