22f769d3dc5810fa08338af1c8b31d63ace48b57
Edouard Oger <eoger@fastmail.com>
2017-10-24 17:57:03 -0400

    Bug 1395460 p2 - Remove about:accounts. r=tcsc
    MozReview-Commit-ID: CdNZRf4divV

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -27,9 +27,6 @@ let whitelist = [
   {sourceName: /web\/viewer\.css$/i,
    errorMessage: /Unknown property.*appearance/i,
    isFromDevTools: false},
-  // Tracked in bug 1004428.
-  {sourceName: /aboutaccounts\/(main|normalize)\.css$/i,
-   isFromDevTools: false},
   // Highlighter CSS uses a UA-only pseudo-class, see bug 985597.
   {sourceName: /highlighters\.css$/i,
    errorMessage: /Unknown pseudo-class.*moz-native-anonymous/i,
