ed240ca375c9e1bac03e2cdd736f132bf5fbcc2c
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-25 00:32:35 +0200

    Backed out changeset ba4a39241953 (bug 1386018) for xpcshell failures, e.g. toolkit/mozapps/extensions/test/xpcshell/test_signed_install.js on Linux debug. r=backout on a CLOSED TREE

--- browser/components/extensions/test/xpcshell/test_ext_url_overrides_newtab_update.js
+++ browser/components/extensions/test/xpcshell/test_ext_url_overrides_newtab_update.js
@@ -20,11 +20,11 @@ Cu.import("resource://testing-common/AddonTestUtils.jsm");
 
 const {
   createAppInfo,
+  createTempWebExtensionFile,
   promiseCompleteAllInstalls,
   promiseFindAddonUpdates,
   promiseShutdownManager,
   promiseStartupManager,
-  UpdateServer,
 } = AddonTestUtils;
 
 AddonTestUtils.init(this);
@@ -37,10 +37,30 @@ createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
 add_task(async function test_url_overrides_newtab_update() {
   const EXTENSION_ID = "test_url_overrides_update@tests.mozilla.org";
   const NEWTAB_URI = "webext-newtab-1.html";
+  const PREF_EM_CHECK_UPDATE_SECURITY = "extensions.checkUpdateSecurity";
 
-  let testServer = new UpdateServer();
-  do_register_cleanup(() => testServer.cleanup());
-  testServer.serveUpdate({
+  const testServer = createHttpServer();
+  const port = testServer.identity.primaryPort;
+
+  // The test extension uses an insecure update url.
+  Services.prefs.setBoolPref(PREF_EM_CHECK_UPDATE_SECURITY, false);
+
+  testServer.registerPathHandler("/test_update.json", (request, response) => {
+    response.write(`{
+      "addons": {
+        "${EXTENSION_ID}": {
+          "updates": [
+            {
+              "version": "2.0",
+              "update_link": "http://localhost:${port}/addons/test_url_overrides-2.0.xpi"
+            }
+          ]
+        }
+      }
+    }`);
+  });
+
+  let webExtensionFile = createTempWebExtensionFile({
     manifest: {
       version: "2.0",
       applications: {
@@ -51,6 +71,8 @@ add_task(async function test_url_overrides_newtab_update() {
     },
   });
 
+  testServer.registerFile("/addons/test_url_overrides-2.0.xpi", webExtensionFile);
+
   await promiseStartupManager();
 
   let extension = ExtensionTestUtils.loadExtension({
@@ -60,7 +82,7 @@ add_task(async function test_url_overrides_newtab_update() {
       "applications": {
         "gecko": {
           "id": EXTENSION_ID,
-          "update_url": testServer.updateUrl,
+          "update_url": `http://localhost:${port}/test_update.json`,
         },
       },
       chrome_url_overrides: {newtab: NEWTAB_URI},
--- browser/components/preferences/in-content/preferences.js
+++ browser/components/preferences/in-content/preferences.js
@@ -23,11 +23,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/AppConstants.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "ExtensionSettingsStore",
-                                  "resource://gre/modules/ExtensionSettingsStore.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "AddonManager",
-                                  "resource://gre/modules/AddonManager.jsm");
-
 var gLastHash = "";
 
 var gCategoryInits = new Map();
@@ -326,84 +321,4 @@ function appendSearchKeywords(aId, keywords) {
     keywords.push(searchKeywords);
   }
   element.setAttribute("searchkeywords", keywords.join(" "));
-}
-
-let extensionControlledContentIds = {
-  "privacy.containers": "browserContainersExtensionContent",
-  "homepage_override": "browserHomePageExtensionContent",
-  "newTabURL": "browserNewTabExtensionContent",
-  "defaultSearch": "browserDefaultSearchExtensionContent",
-};
-
-/**
-  * Check if a pref is being managed by an extension.
-  */
-async function getControllingExtensionInfo(type, settingName) {
-  await ExtensionSettingsStore.initialize();
-  return ExtensionSettingsStore.getSetting(type, settingName);
-}
-
-function getControllingExtensionEl(settingName) {
-  return document.getElementById(extensionControlledContentIds[settingName]);
-}
-
-async function handleControllingExtension(type, settingName) {
-  let info = await getControllingExtensionInfo(type, settingName);
-  let isControlled = info && !!info.id;
-
-  if (isControlled) {
-    showControllingExtension(settingName, info.id);
-  } else {
-    hideControllingExtension(settingName);
-  }
-
-  return isControlled;
-}
-
-async function showControllingExtension(settingName, extensionId) {
-  let extensionControlledContent = getControllingExtensionEl(settingName);
-  extensionControlledContent.classList.remove("extension-controlled-disabled");
-  // Tell the user what extension is controlling the setting.
-  let addon = await AddonManager.getAddonByID(extensionId);
-  const defaultIcon = "chrome://mozapps/skin/extensions/extensionGeneric.svg";
-  let stringParts = document
-    .getElementById("bundlePreferences")
-    .getString(`extensionControlled.${settingName}`)
-    .split("%S");
-  let description = extensionControlledContent.querySelector("description");
-
-  // Remove the old content from the description.
-  while (description.firstChild) {
-    description.firstChild.remove();
-  }
-
-  // Populate the description.
-  description.appendChild(document.createTextNode(stringParts[0]));
-  let image = document.createElement("image");
-  image.setAttribute("src", addon.iconURL || defaultIcon);
-  image.classList.add("extension-controlled-icon");
-  description.appendChild(image);
-  description.appendChild(document.createTextNode(` ${addon.name}`));
-  description.appendChild(document.createTextNode(stringParts[1]));
-
-  let disableButton = extensionControlledContent.querySelector("button");
-  if (disableButton) {
-    disableButton.hidden = false;
-  }
-
-  // Show the controlling extension row and hide the old label.
-  extensionControlledContent.hidden = false;
-}
-
-function hideControllingExtension(settingName) {
-  getControllingExtensionEl(settingName).hidden = true;
-}
-
-
-function makeDisableControllingExtension(type, settingName) {
-  return async function disableExtension() {
-    let {id} = await getControllingExtensionInfo(type, settingName);
-    let addon = await AddonManager.getAddonByID(id);
-    addon.userDisabled = true;
-  };
 }
