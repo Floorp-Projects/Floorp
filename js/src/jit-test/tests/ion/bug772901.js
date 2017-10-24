e0d164f1740468027f123812f91fb04c517a9c44
Tom Schuster <evilpies@gmail.com>
2017-10-24 20:48:14 +0200

    Bug 638054 - Mostly remove JS tests using watch/unwatch. r=jorendorff

--- js/src/jit-test/tests/ion/bug772901.js
+++ js/src/jit-test/tests/ion/bug772901.js
@@ -4,4 +4,4 @@ function f(x) {
     delete ((x)++);
     arguments[0] !== undefined;
 }
-f(1, x = [f.ArrayBuffer,unwatch.Int32Array], this, this, this) ;
+f(1, x = [f.ArrayBuffer, undefined], this, this, this) ;
--- js/src/tests/js1_8_5/regress/regress-635195.js
+++
@@ -1,8 +0,0 @@
-// Any copyright is dedicated to the Public Domain.
-// http://creativecommons.org/licenses/publicdomain/
-
-var obj = {set x(v) {}};
-obj.watch("x", function() { delete obj.x; });
-obj.x = "hi";  // don't assert
-
-reportCompare(0, 0, 'ok');
