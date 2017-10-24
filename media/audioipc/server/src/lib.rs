1225683a5355d6745b9bd2e5c2801d87e9e4c83b
Dan Glastonbury <dan.glastonbury@gmail.com>
2017-10-25 09:14:45 +1000

    Bug 1410107 - Grow audioipc server slabs as required. r=kinetik
    MozReview-Commit-ID: zFfex3LX7K

--- media/audioipc/server/src/lib.rs
+++ media/audioipc/server/src/lib.rs
@@ -53,6 +53,12 @@ use errors::*;
 // TODO: Remove and let caller allocate based on cubeb backend requirements.
 const SHM_AREA_SIZE: usize = 2 * 1024 * 1024;
 
+// The size in which the server conns slab is grown.
+const SERVER_CONN_CHUNK_SIZE: usize = 16;
+
+// The size in which the stream slab is grown.
+const STREAM_CONN_CHUNK_SIZE: usize = 64;
+
 // TODO: this should forward to the client.
 struct Callback {
     /// Size of input frame in bytes
@@ -163,7 +169,7 @@ impl ServerConn {
             io: io,
             token: None,
             // TODO: Handle increasing slab size. Pick a good default size.
-            streams: StreamSlab::with_capacity(64),
+            streams: StreamSlab::with_capacity(STREAM_CONN_CHUNK_SIZE),
             decoder: Decoder::new(),
             recv_buffer: BytesMut::with_capacity(4096),
             send_buffer: BytesMut::with_capacity(4096),
@@ -435,6 +441,14 @@ impl ServerConn {
             }
         ) {
             Ok(stream) => {
+                if !self.streams.has_available() {
+                    trace!(
+                        "server connection ran out of stream slots. reserving {} more.",
+                        STREAM_CONN_CHUNK_SIZE
+                    );
+                    self.streams.reserve_exact(STREAM_CONN_CHUNK_SIZE);
+                }
+
                 let stm_tok = match self.streams.vacant_entry() {
                     Some(entry) => {
                         debug!(
@@ -582,7 +596,7 @@ impl Server {
         Server {
             socket: socket,
             context: None,
-            conns: Slab::with_capacity(16)
+            conns: Slab::with_capacity(SERVER_CONN_CHUNK_SIZE)
         }
     }
 
@@ -597,6 +611,15 @@ impl Server {
             Ok(None) => panic!("accept returned EAGAIN unexpectedly"),
             Ok(Some((socket, _))) => socket,
         };
+
+        if !self.conns.has_available() {
+            trace!(
+                "server ran out of connection slots. reserving {} more.",
+                SERVER_CONN_CHUNK_SIZE
+            );
+            self.conns.reserve_exact(SERVER_CONN_CHUNK_SIZE);
+        }
+
         let token = match self.conns.vacant_entry() {
             Some(entry) => {
                 debug!("registering {:?}", entry.index());
