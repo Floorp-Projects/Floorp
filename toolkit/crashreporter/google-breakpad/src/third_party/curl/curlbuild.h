39611d7830a5d4e69dae8fe8fcdcb87fdfb61fc9
Dan Horák <dan@danny.cz>
2017-10-25 08:36:47 +0000

    bug 1411516 - list missing 64-bit arches in Breakpad's bundled curl. r=ted
    MozReview-Commit-ID: KNc8fUoCAEA
    
    --HG--
    extra : amend_source : c034faf81fd818adcba0748e3b704db1eaa5fe14

--- toolkit/crashreporter/google-breakpad/src/third_party/curl/curlbuild.h
+++ toolkit/crashreporter/google-breakpad/src/third_party/curl/curlbuild.h
@@ -155,7 +155,8 @@
 
 /* The size of `long', as computed by sizeof. */
 #if defined(_M_X64) || (defined(__x86_64__) && !defined(__ILP32__)) ||      \
-    defined(__aarch64__) || (defined(__mips__) && _MIPS_SIM == _ABI64)
+    defined(__aarch64__) || (defined(__mips__) && _MIPS_SIM == _ABI64) ||   \
+    defined(__powerpc64__) || defined(__s390x__) || defined(__LP64__)
 #define CURL_SIZEOF_LONG 8
 #else
 #define CURL_SIZEOF_LONG 4
