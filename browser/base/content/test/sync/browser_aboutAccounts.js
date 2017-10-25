0dd8ac04f38b6b2f309f3f0500b991c8d2812c9a
Edouard Oger <eoger@fastmail.com>
2017-10-25 16:41:37 -0400

    Bug 1411714 - Handle action=email in about:accounts. r=tcsc, a=ritu
    MozReview-Commit-ID: 5U6Qy78S1Nl

--- browser/base/content/test/sync/browser_aboutAccounts.js
+++ browser/base/content/test/sync/browser_aboutAccounts.js
@@ -130,12 +130,52 @@ var gTests = [
 },
 {
   desc: "Test action=signup - user logged in",
-  teardown: () => gBrowser.removeCurrentTab(),
+  async teardown() {
+    gBrowser.removeCurrentTab();
+    await signOut();
+  },
   async run() {
     const expected_url = "https://example.com/?is_sign_up";
     setPref("identity.fxaccounts.remote.signup.uri", expected_url);
     await setSignedInUser();
     let tab = await promiseNewTabLoadEvent("about:accounts?action=signup");
+    await fxAccounts.getSignedInUser();
+    // we expect "manage" to be shown.
+    await checkVisibilities(tab, {
+      stage: true, // parent of 'manage' and 'intro'
+      manage: true,
+      intro: false, // this is  "get started"
+      remote: false,
+      networkError: false
+    });
+  },
+},
+{
+  desc: "Test action=email - no user logged in",
+  teardown: () => gBrowser.removeCurrentTab(),
+  async run() {
+    const expected_url = "https://example.com/?is_email";
+    setPref("identity.fxaccounts.remote.email.uri", expected_url);
+    let [tab, url] = await promiseNewTabWithIframeLoadEvent("about:accounts?action=email");
+    is(url, expected_url, "action=email got the expected URL");
+    // we expect the remote iframe to be shown.
+    await checkVisibilities(tab, {
+      stage: false, // parent of 'manage' and 'intro'
+      manage: false,
+      intro: false, // this is  "get started"
+      remote: true,
+      networkError: false
+    });
+  },
+},
+{
+  desc: "Test action=email - user logged in",
+  teardown: () => gBrowser.removeCurrentTab(),
+  async run() {
+    const expected_url = "https://example.com/?is_email";
+    setPref("identity.fxaccounts.remote.email.uri", expected_url);
+    await setSignedInUser();
+    let tab = await promiseNewTabLoadEvent("about:accounts?action=email");
     await fxAccounts.getSignedInUser();
     // we expect "manage" to be shown.
     await checkVisibilities(tab, {
