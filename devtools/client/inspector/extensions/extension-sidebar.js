c50ffac88eff6bc30c91d42202e4161c1e6e8d03
Gabriel Luong <gabriel.luong@gmail.com>
2017-10-26 11:32:46 -0400

    Bug 1411160 - Adds an initial react/redux template for loading a Changes View in the inspector sidebar. r=pbro
    --HG--
    rename : devtools/client/inspector/fonts/components/App.js => devtools/client/inspector/fonts/components/FontsApp.js
    rename : devtools/client/inspector/layout/components/App.js => devtools/client/inspector/layout/components/LayoutApp.js

--- devtools/client/inspector/extensions/extension-sidebar.js
+++ devtools/client/inspector/extensions/extension-sidebar.js
@@ -4,10 +4,7 @@
 
 "use strict";
 
-const {
-  createElement, createFactory,
-} = require("devtools/client/shared/vendor/react");
-
+const { createElement, createFactory } = require("devtools/client/shared/vendor/react");
 const { Provider } = require("devtools/client/shared/vendor/react-redux");
 
 const ExtensionSidebarComponent = createFactory(require("./components/ExtensionSidebar"));
