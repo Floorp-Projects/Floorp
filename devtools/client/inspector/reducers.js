7a42c596783c88f2560e49e4a4e2fa983f24cce1
Gabriel Luong <gabriel.luong@gmail.com>
2017-10-26 12:38:09 -0400

    Bug 1411161 - Adds an initial react/redux template for loading an Events View in the inspector sidebar. r=pbro

--- devtools/client/inspector/reducers.js
+++ devtools/client/inspector/reducers.js
@@ -9,6 +9,7 @@
 
 exports.boxModel = require("devtools/client/inspector/boxmodel/reducers/box-model");
 exports.changes = require("devtools/client/inspector/changes/reducers/changes");
+exports.events = require("devtools/client/inspector/events/reducers/events");
 exports.extensionsSidebar = require("devtools/client/inspector/extensions/reducers/sidebar");
 exports.flexboxes = require("devtools/client/inspector/flexbox/reducers/flexboxes");
 exports.fontOptions = require("devtools/client/inspector/fonts/reducers/font-options");
