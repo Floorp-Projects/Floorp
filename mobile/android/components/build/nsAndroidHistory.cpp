dee87d13ab1ccfdf7617544e9e9e90fd5cee3864
Kris Maglione <maglione.k@gmail.com>
2017-10-25 20:56:41 -0700

    Bug 1409249: Follow-up: Fix Android build bustage. r=bustage
    MozReview-Commit-ID: 1I3z8Oqz9Qm

--- mobile/android/components/build/nsAndroidHistory.cpp
+++ mobile/android/components/build/nsAndroidHistory.cpp
@@ -30,7 +30,7 @@ NS_IMPL_ISUPPORTS(nsAndroidHistory, IHistory, nsIRunnable, nsITimerCallback, nsI
 nsAndroidHistory* nsAndroidHistory::sHistory = nullptr;
 
 /*static*/
-nsAndroidHistory*
+already_AddRefed<nsAndroidHistory>
 nsAndroidHistory::GetSingleton()
 {
   if (!sHistory) {
@@ -38,8 +38,7 @@ nsAndroidHistory::GetSingleton()
     NS_ENSURE_TRUE(sHistory, nullptr);
   }
 
-  NS_ADDREF(sHistory);
-  return sHistory;
+  return do_AddRef(sHistory);
 }
 
 nsAndroidHistory::nsAndroidHistory()
