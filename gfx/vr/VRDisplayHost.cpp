29b5c2cd8f8cc7be58c2e176acff216145da3926
Daosheng Mu <daoshengmu@gmail.com>
2017-10-26 17:45:44 +0800

    Bug 1406327 - Part 3: VRSystemManager for multi-threads; r=kip
    MozReview-Commit-ID: 4bE5hruFcT2

--- gfx/vr/VRDisplayHost.cpp
+++ gfx/vr/VRDisplayHost.cpp
@@ -10,6 +10,7 @@
 #include "ipc/VRLayerParent.h"
 #include "mozilla/layers/TextureHost.h"
 #include "mozilla/dom/GamepadBinding.h" // For GamepadMappingType
+#include "mozilla/layers/CompositorThread.h"
 #include "VRThread.h"
 
 #if defined(XP_WIN)
@@ -260,6 +261,7 @@ VRDisplayHost::SubmitFrame(VRLayerParent* aLayer,
 {
   AUTO_PROFILER_TRACING("VR", "SubmitFrameAtVRDisplayHost");
 
+  MOZ_ASSERT(NS_IsInCompositorThread());
   if ((mDisplayInfo.mGroupMask & aLayer->GetGroup()) == 0) {
     // Suppress layers hidden by the group mask
     return;
--- gfx/vr/gfxVROculus.cpp
+++ gfx/vr/gfxVROculus.cpp
@@ -364,9 +364,10 @@ VROculusSession::Refresh(bool aForceRefresh)
             "gfx::VROculusSession::Refresh",
             this,
             &VROculusSession::Refresh, true));
-
           return;
         }
+        // Calling ovr_SubmitFrame() must be at Compositor thread.
+        MOZ_ASSERT(NS_IsInCompositorThread());
         ovrLayerEyeFov layer;
         memset(&layer, 0, sizeof(layer));
         layer.Header.Type = ovrLayerType_Disabled;
