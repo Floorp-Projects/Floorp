73c42c6cf75c14de2b50b3319a16c285f9c58c2f
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-26 22:30:55 +0200

    Backed out changeset 2db16a02d6d1 (bug 1411161)

--- devtools/client/inspector/reducers.js
+++ devtools/client/inspector/reducers.js
@@ -9,7 +9,6 @@
 
 exports.boxModel = require("devtools/client/inspector/boxmodel/reducers/box-model");
 exports.changes = require("devtools/client/inspector/changes/reducers/changes");
-exports.events = require("devtools/client/inspector/events/reducers/events");
 exports.extensionsSidebar = require("devtools/client/inspector/extensions/reducers/sidebar");
 exports.flexboxes = require("devtools/client/inspector/flexbox/reducers/flexboxes");
 exports.fontOptions = require("devtools/client/inspector/fonts/reducers/font-options");
