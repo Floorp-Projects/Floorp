c0d609a3b629b7e19e26e665289476ea7ec9829d
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-26 22:30:55 +0200

    Backed out changeset 2db16a02d6d1 (bug 1411161)

--- devtools/client/preferences/devtools.js
+++ devtools/client/preferences/devtools.js
@@ -62,8 +62,6 @@ pref("devtools.inspector.colorWidget.enabled", false);
 pref("devtools.inspector.shapesHighlighter.enabled", true);
 // Enable the Changes View
 pref("devtools.changesview.enabled", false);
-// Enable the Events View
-pref("devtools.eventsview.enabled", false);
 // Enable the Flexbox Inspector
 pref("devtools.flexboxinspector.enabled", false);
 
