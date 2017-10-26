e3eca5624ed4fd1d5e420a41031f07666c7eb913
Daosheng Mu <daoshengmu@gmail.com>
2017-10-26 17:28:37 +0800

    Bug 1406327 - Part 2: When loading VR content, launching the VR listener thread; r=kip
    MozReview-Commit-ID: IyBzJyDEVdv

--- gfx/vr/VRManager.cpp
+++ gfx/vr/VRManager.cpp
@@ -227,6 +227,7 @@ VRManager::NotifyVsync(const TimeStamp& aVsyncTimestamp)
   if (bHaveEventListener || bHaveControllerListener) {
     // We are using a VR device, keep it alive
     mLastActiveTime = TimeStamp::Now();
+    mLastVRListenerThreadActiveTime = mLastActiveTime;
   } else if (mLastActiveTime.IsNull()) {
     Shutdown();
   } else {
@@ -259,7 +260,7 @@ void
 VRManager::RefreshVRDisplays(bool aMustDispatch)
 {
   nsTArray<RefPtr<gfx::VRDisplayHost> > displays;
-
+  mLastVRListenerThreadActiveTime = TimeStamp::Now();
   /** We don't wish to enumerate the same display from multiple managers,
    * so stop as soon as we get a display.
    * It is still possible to get multiple displays from a single manager,
@@ -308,13 +309,20 @@ VRManager::RefreshVRDisplays(bool aMustDispatch)
   }
 
   if (displayInfoChanged || displaySetChanged || aMustDispatch) {
-    DispatchVRDisplayInfoUpdate();
+    // Due to PVRManager is at Compositor thread. We have to post tasks
+    // to Compositor thread when sending to them the content processes.
+    MessageLoop* loop = CompositorThreadHolder::Loop();
+    loop->PostTask(
+      NewRunnableMethod("gfx::VRManager::DispatchVRDisplayInfoUpdate",
+                        this,
+                        &VRManager::DispatchVRDisplayInfoUpdate));
   }
 }
 
 void
 VRManager::DispatchVRDisplayInfoUpdate()
 {
+  MOZ_ASSERT(NS_IsInCompositorThread());
   nsTArray<VRDisplayInfo> update;
   GetVRDisplayInfo(update);
 
@@ -322,7 +330,6 @@ VRManager::DispatchVRDisplayInfoUpdate()
     Unused << iter.Get()->GetKey()->SendUpdateDisplayInfo(update);
   }
 }
-
 
 /**
  * Get any VR displays that have already been enumerated without
@@ -366,6 +373,12 @@ VRManager::GetVRControllerInfo(nsTArray<VRControllerInfo>& aControllerInfo)
     gfx::VRControllerHost* controller = iter.UserData();
     aControllerInfo.AppendElement(VRControllerInfo(controller->GetControllerInfo()));
   }
+}
+
+TimeStamp
+VRManager::GetLastVRListenerThreadActiveTime()
+{
+  return mLastVRListenerThreadActiveTime;
 }
 
 void
@@ -442,8 +455,22 @@ VRManager::NotifyGamepadChange(uint32_t aIndex, const T& aInfo)
   dom::GamepadChangeEventBody body(aInfo);
   dom::GamepadChangeEvent e(aIndex, dom::GamepadServiceType::VR, body);
 
+  // Due to PVRManager is at Compositor thread. We have to post
+  // tasks to Compositor thread.
+  MessageLoop* loop = CompositorThreadHolder::Loop();
+  loop->PostTask(
+    NewRunnableMethod<dom::GamepadChangeEvent>(
+                      "gfx::VRManager::NotifyGamepadChangeEventsToContent",
+                      this,
+                      &VRManager::NotifyGamepadChangeEventsToContent, e));
+}
+
+void
+VRManager::NotifyGamepadChangeEventsToContent(const dom::GamepadChangeEvent& aEvent)
+{
+  MOZ_ASSERT(NS_IsInCompositorThread());
   for (auto iter = mVRManagerParents.Iter(); !iter.Done(); iter.Next()) {
-    Unused << iter.Get()->GetKey()->SendGamepadUpdate(e);
+    Unused << iter.Get()->GetKey()->SendGamepadUpdate(aEvent);
   }
 }
 
@@ -469,6 +496,20 @@ VRManager::StopVibrateHaptic(uint32_t aControllerIdx)
 void
 VRManager::NotifyVibrateHapticCompleted(uint32_t aPromiseID)
 {
+  // Due to PVRManager is at Compositor thread. We have to post
+  // tasks to Compositor thread.
+  MessageLoop* loop = CompositorThreadHolder::Loop();
+  loop->PostTask(
+    NewRunnableMethod<uint32_t>(
+                      "gfx::VRManager::NotifyVibrateHapticCompletedToContent",
+                      this,
+                      &VRManager::NotifyVibrateHapticCompletedToContent, aPromiseID));
+}
+
+void
+VRManager::NotifyVibrateHapticCompletedToContent(uint32_t aPromiseID)
+{
+  MOZ_ASSERT(NS_IsInCompositorThread());
   for (auto iter = mVRManagerParents.Iter(); !iter.Done(); iter.Next()) {
     Unused << iter.Get()->GetKey()->SendReplyGamepadVibrateHaptic(aPromiseID);
   }
@@ -477,6 +518,7 @@ VRManager::NotifyVibrateHapticCompleted(uint32_t aPromiseID)
 void
 VRManager::DispatchSubmitFrameResult(uint32_t aDisplayID, const VRSubmitFrameResultInfo& aResult)
 {
+  MOZ_ASSERT(NS_IsInCompositorThread());
   for (auto iter = mVRManagerParents.Iter(); !iter.Done(); iter.Next()) {
     Unused << iter.Get()->GetKey()->SendDispatchSubmitFrameResult(aDisplayID, aResult);
   }
--- gfx/vr/ipc/VRManagerChild.cpp
+++ gfx/vr/ipc/VRManagerChild.cpp
@@ -115,7 +115,7 @@ VRManagerChild::InitSameProcess()
   sVRManagerChildSingleton = new VRManagerChild();
   sVRManagerParentSingleton = VRManagerParent::CreateSameProcess();
   sVRManagerChildSingleton->Open(sVRManagerParentSingleton->GetIPCChannel(),
-                                 VRListenerThreadHolder::Loop(),
+                                 mozilla::layers::CompositorThreadHolder::Loop(),
                                  mozilla::ipc::ChildSide);
 }
 
