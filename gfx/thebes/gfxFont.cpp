c396a1711247cac3d6d63e4cc09c95dfaf8b7a60
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-10-26 23:08:29 +0200

    Backed out changeset 0998d26202bd (bug 1408612) for reftest failures e.g. layout/reftests/svg/text-gradient-01.svg and layout/reftests/writing-mode/1193519-sideways-lr-decoration-1.html. r=backout on a CLOSED TREE

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -1568,7 +1568,7 @@ gfxFont::HasFeatureSet(uint32_t aFeature, bool& aFeatureOn)
  * processing here.
  */
 #define ToDeviceUnits(aAppUnits, aDevUnitsPerAppUnit) \
-    (aAppUnits * gfx::Float(aDevUnitsPerAppUnit))
+    (double(aAppUnits)*double(aDevUnitsPerAppUnit))
 
 static AntialiasMode Get2DAAMode(gfxFont::AntialiasOption aAAOption) {
   switch (aAAOption) {
@@ -1863,7 +1863,7 @@ private:
 // when a non-identity transform exists.  Use an offset factor so that
 // the second draw occurs at a constant offset in device pixels.
 
-gfx::Float
+double
 gfxFont::CalcXScale(DrawTarget* aDrawTarget)
 {
     // determine magnitude of a 1px x offset in device space
@@ -1873,7 +1873,7 @@ gfxFont::CalcXScale(DrawTarget* aDrawTarget)
         return 1.0;
     }
 
-    gfx::Float m = sqrtf(t.width * t.width + t.height * t.height);
+    double m = sqrt(t.width * t.width + t.height * t.height);
 
     NS_ASSERTION(m != 0.0, "degenerate transform while synthetic bolding");
     if (m == 0.0) {
@@ -2216,7 +2216,7 @@ gfxFont::Draw(const gfxTextRun *aTextRun, uint32_t aStart, uint32_t aEnd,
     // Synthetic-bold strikes are each offset one device pixel in run direction.
     // (these values are only needed if IsSyntheticBold() is true)
     if (IsSyntheticBold()) {
-        gfx::Float xscale = CalcXScale(aRunParams.context->GetDrawTarget());
+        double xscale = CalcXScale(aRunParams.context->GetDrawTarget());
         fontParams.synBoldOnePixelOffset = aRunParams.direction * xscale;
         if (xscale != 0.0) {
             // use as many strikes as needed for the the increased advance
