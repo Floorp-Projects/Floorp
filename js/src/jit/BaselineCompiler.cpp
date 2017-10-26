3f5089fe1e2ace3c2901cdf8890f03ee080529e1
Tooru Fujisawa <arai_a@mac.com>
2017-10-26 22:10:31 +0900

    Bug 1396499 - Part 2: Do not hold reference to expression stack values in GeneratorObject::suspend. r=jandem

--- js/src/jit/BaselineCompiler.cpp
+++ js/src/jit/BaselineCompiler.cpp
@@ -4837,22 +4837,23 @@ BaselineCompiler::emit_JSOP_RESUME()
     {
         masm.unboxObject(exprStackSlot, scratch2);
 
-        Register arrayLength = regs.takeAny();
+        Register initLength = regs.takeAny();
         masm.loadPtr(Address(scratch2, NativeObject::offsetOfElements()), scratch2);
-        masm.load32(Address(scratch2, ObjectElements::offsetOfLength()), arrayLength);
-        masm.store32(Imm32(0), Address(scratch2, ObjectElements::offsetOfLength()));
+        masm.load32(Address(scratch2, ObjectElements::offsetOfInitializedLength()), initLength);
+        masm.store32(Imm32(0), Address(scratch2, ObjectElements::offsetOfInitializedLength()));
 
         Label loop, loopDone;
         masm.bind(&loop);
-        masm.branchTest32(Assembler::Zero, arrayLength, arrayLength, &loopDone);
+        masm.branchTest32(Assembler::Zero, initLength, initLength, &loopDone);
         {
             masm.pushValue(Address(scratch2, 0));
+            masm.guardedCallPreBarrier(Address(scratch2, 0), MIRType::Value);
             masm.addPtr(Imm32(sizeof(Value)), scratch2);
-            masm.sub32(Imm32(1), arrayLength);
+            masm.sub32(Imm32(1), initLength);
             masm.jump(&loop);
         }
         masm.bind(&loopDone);
-        regs.add(arrayLength);
+        regs.add(initLength);
     }
 
     masm.bind(&noExprStack);
