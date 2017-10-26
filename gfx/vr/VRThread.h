1726d42a0c535b7c2b2fb2191142334561267bfa
Daosheng Mu <daoshengmu@gmail.com>
2017-10-26 16:51:14 +0800

    Bug 1406327 - Part 1: Shutdown VR listener thread when no VR content in seconds; r=dvander,kip
    MozReview-Commit-ID: AnYJT8WBkI7
    
    --HG--
    extra : rebase_source : 5d336ca6f5fb9fbcfd993e5e699d363a22bbe304

--- gfx/vr/VRThread.h
+++ gfx/vr/VRThread.h
@@ -27,21 +27,22 @@ public:
   static VRListenerThreadHolder* GetSingleton();
 
   static bool IsActive() {
-    return !!GetSingleton();
+    return !!GetSingleton() && !!Loop();
   }
 
   static void Start();
   static void Shutdown();
   static MessageLoop* Loop();
   static bool IsInVRListenerThread();
+  static TimeStamp GetStartTime();
 
 private:
   ~VRListenerThreadHolder();
 
-  base::Thread* const mThread;
-
   static base::Thread* CreateThread();
   static void DestroyThread(base::Thread* aThread);
+
+  base::Thread* const mThread;
 };
 
 base::Thread* VRListenerThread();
