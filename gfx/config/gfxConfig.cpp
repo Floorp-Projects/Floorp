76d8e6a7d51e1726b0c1a81945d406ac36f34296
Daniel Holbert <dholbert@cs.stanford.edu>
2017-10-27 15:55:37 -0700

    Bug 1412427 part 3: Replace 10 gfx usages of a particular non-standard emacs mode line with our standard mode line. r=jrmuizel
    This patch was mostly[1] automatically generated. I found the files to be fixed
    in this patch with the following command:
    grep -r "C++; indent-tabs-mode:" gfx
    
    ...and then I modified each of these files with the following script
    (where $1 is the filename to be modified):
    ###
    old="/\* -\*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -\*- \*/"
    new="/\* -\*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -\*- \*/"
    
    sed -i s%"^$old"%"$new"% $1
    ###
    
    [1] NOTE: Nearly all of these files use 2-space indentation, so it's correct
    (from a consistency perspective and a reflecting-reality perspective) that
    we're reducing their "c-basic-offset: 4" down to "c-basic-offset: 2" here.  The
    one exception is nsDeviceContext.h, which *does* actually use 4-space
    indentation right now -- so I'm leaving that file with "c-basic-offset: 4" in
    its mode line.  This manual change (reverting 1 character from the automated
    process) is the only piece of this patch that isn't automated.
    
    MozReview-Commit-ID: L4MbyeYSbfY
    
    --HG--
    extra : rebase_source : 8ddbe422471bfd8b0060e96fb1c8cd062f10f290

--- gfx/config/gfxConfig.cpp
+++ gfx/config/gfxConfig.cpp
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set sts=2 ts=8 sw=2 tw=99 et: */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
