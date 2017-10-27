c9b9d0955bbb10df4ce8bc8fd960c9b3b2c48e81
Chris Manchester <cmanchester@mozilla.com>
2017-10-27 09:39:44 -0700

    Bug 1412267 - Fix the inclusion of MOZ_DEBUG_FLAGS in the compile command line to depend on MOZ_DEBUG_SYMBOLS and MOZ_DEBUG. r=gps
    MozReview-Commit-ID: 4MgzRQCX5iW

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -335,8 +335,7 @@ class CompileFlags(ContextDerivedValue, dict):
              ('CFLAGS', 'C_LDFLAGS')),
             ('OS_CXXFLAGS', context.config.substs.get('OS_CXXFLAGS'),
              ('CXXFLAGS', 'CXX_LDFLAGS')),
-            ('DEBUG', (context.config.substs['MOZ_DEBUG_FLAGS'].split() if
-                       'MOZ_DEBUG_FLAGS' in context.config.substs else []),
+            ('DEBUG', self._debug_flags(),
              ('CFLAGS', 'CXXFLAGS', 'CXX_LDFLAGS', 'C_LDFLAGS')),
             ('CLANG_PLUGIN', context.config.substs.get('CLANG_PLUGIN_FLAGS'),
              ('CFLAGS', 'CXXFLAGS', 'CXX_LDFLAGS', 'C_LDFLAGS')),
@@ -359,6 +358,12 @@ class CompileFlags(ContextDerivedValue, dict):
                                  for k, v, _ in self.flag_variables))
         else:
             dict.__init__(self)
+
+    def _debug_flags(self):
+        if (self._context.config.substs.get('MOZ_DEBUG') or
+            self._context.config.substs.get('MOZ_DEBUG_SYMBOLS')):
+            return self._context.config.substs.get('MOZ_DEBUG_FLAGS', '').split()
+        return []
 
     def _warnings_as_errors(self):
         warnings_as_errors = self._context.config.substs.get('WARNINGS_AS_ERRORS')
