9525a7f97f29af84f852ff3b9d8bfb1e1fbeaa13
Daniel Holbert <dholbert@cs.stanford.edu>
2017-10-27 15:55:37 -0700

    Bug 1412427 part 2: Move vim mode line below emacs mode line in a few gfx files, for consistency. r=jrmuizel
    This patch doesn't modify the mode lines at all -- it just swaps their order,
    and makes each one its own C++ comment, separate from the MPL boilerplate
    comment.
    
    MozReview-Commit-ID: BEZJVj2sMuK

--- gfx/layers/ipc/ImageBridgeParent.cpp
+++ gfx/layers/ipc/ImageBridgeParent.cpp
@@ -1,6 +1,6 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* vim: set ts=2 sw=2 et tw=80: */
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
- * This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
