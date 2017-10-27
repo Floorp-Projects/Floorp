b8c8c56e4653fdfbb7e5bd14e20a12cc1b800c2c
Eric Rahm <erahm@mozilla.com>
2017-10-27 13:26:47 -0700

    Bug 1412416 - Part 2: Simplify ArenaStrdup for xpcom strings. r=froydnj
    Now that xpcom strings use templates we can combine their `ArenaStrdup`
    implementations. `nsTStringRepr` is used so as to allow handling of both
    `nsTLiteralString` and `nsTSubstring` types.
    
    --HG--
    extra : rebase_source : e77891da589f320507b45f524a3203b3dc9f38e6
    extra : histedit_source : 79d3b9a1add191563e0985f0c0e416bd29f24351

--- xpcom/tests/gtest/TestArenaAllocator.cpp
+++ xpcom/tests/gtest/TestArenaAllocator.cpp
@@ -293,6 +293,8 @@ TEST(ArenaAllocator, Extensions)
   const char16_t* const kTestStr = u"This is a wide test string.";
   char16_t* dup = mozilla::ArenaStrdup(kTestStr, a);
   EXPECT_TRUE(nsString(dup).Equals(kTestStr));
+
+  // Make sure it works with literal strings.
   NS_NAMED_LITERAL_STRING(wideStr, "A wide string.");
   nsLiteralString::char_type* wide = mozilla::ArenaStrdup(wideStr, a);
   EXPECT_TRUE(wideStr.Equals(wide));
@@ -300,4 +302,13 @@ TEST(ArenaAllocator, Extensions)
   NS_NAMED_LITERAL_CSTRING(cStr, "A c-string.");
   nsLiteralCString::char_type* cstr = mozilla::ArenaStrdup(cStr, a);
   EXPECT_TRUE(cStr.Equals(cstr));
+
+  // Make sure it works with normal strings.
+  nsAutoString x(u"testing wide");
+  nsAutoString::char_type* x_copy = mozilla::ArenaStrdup(x, a);
+  EXPECT_TRUE(x.Equals(x_copy));
+
+  nsAutoCString y("testing c-string");
+  nsAutoCString::char_type* y_copy = mozilla::ArenaStrdup(y, a);
+  EXPECT_TRUE(y.Equals(y_copy));
 }
