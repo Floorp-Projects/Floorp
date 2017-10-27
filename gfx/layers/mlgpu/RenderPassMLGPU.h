36801ac182fcf9e8b885ec862f84d215714cb912
Daniel Holbert <dholbert@cs.stanford.edu>
2017-10-27 15:55:37 -0700

    Bug 1412427 part 4: Indent under-indented MPL boilerplate comments by 1 space, for consistency & alignment. (whitespace-only) r=jrmuizel
    This patch was automatically generated. I found the files to be fixed in this
    patch with the following command:
    grep -r "^\* This Source Code" gfx
    
    ...and then I modified each of these files with the following script
    (where $1 is the filename to be modified):
    ###
    line1="\* This Source Code Form is subject to the terms of the Mozilla Public"
    line2="\* License, v\. 2\.0\. If a copy of the MPL was not distributed with this"
    line3="\* file, You can obtain one at http://mozilla\.org/MPL/2\.0/\. \*/"
    
    # Insert 1 space at beginning:
    sed -i s%"^$line1"%" $line1"% $1
    sed -i s%"^$line2"%" $line2"% $1
    sed -i s%"^$line3"%" $line3"% $1
    ###
    
    MozReview-Commit-ID: HXBMrfnhlVr

--- gfx/layers/mlgpu/RenderPassMLGPU.h
+++ gfx/layers/mlgpu/RenderPassMLGPU.h
@@ -1,8 +1,8 @@
 /* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #ifndef MOZILLA_GFX_RENDERPASSMLGPU_H
 #define MOZILLA_GFX_RENDERPASSMLGPU_H
