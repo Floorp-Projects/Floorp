e1747c7b8007d1440f5699eac5145861672a2a28
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-10-27 11:06:24 -0400

    Bug 1400503 - [tryselect] Separate 'common_arguments' into groups sub-parsers can opt-in to, r=armenzg
    This allows subparsers more control over which sets of arguments they need to
    implement.  For example, it doesn't make sense for the 'empty' selector to
    accept the preset arguments.  Now it can opt-out of those and only implement
    the 'push' arguments.
    
    MozReview-Commit-ID: GOfjcFtlfDD

--- tools/tryselect/mach_commands.py
+++ tools/tryselect/mach_commands.py
@@ -38,13 +38,14 @@ def fuzzy_parser():
     return FuzzyParser()
 
 
-def base_parser():
-    from tryselect.cli import BaseTryParser
-    return BaseTryParser()
+def empty_parser():
+    from tryselect.selectors.empty import EmptyParser
+    return EmptyParser()
 
 
 def generic_parser():
-    parser = base_parser()
+    from tryselect.cli import BaseTryParser
+    parser = BaseTryParser()
     parser.add_argument('argv', nargs=argparse.REMAINDER)
     return parser
 
@@ -149,7 +150,7 @@ class TrySelect(MachCommandBase):
     @SubCommand('try',
                 'empty',
                 description='Push to try without scheduling any tasks.',
-                parser=base_parser)
+                parser=empty_parser)
     def try_empty(self, **kwargs):
         """Push to try, running no builds or tests
 
