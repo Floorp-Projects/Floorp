b7ce21548339cc0b5ce20b6d77bccff3ea3bee5b
Gregory Szorc <gps@mozilla.com>
2017-10-27 16:05:03 -0700

    Bug 1412460 - Simplify config.guess invocation; r=mshal
    The config.guess file is checked into source control. I'm not sure
    under what conditions we wouldn't fail to find this file. So the
    $(wildcard) shouldn't be necessary.
    
    MozReview-Commit-ID: KCctPFW3Eab

--- client.mk
+++ client.mk
@@ -51,11 +51,7 @@ SH := /bin/sh
 PERL ?= perl
 PYTHON ?= $(shell which python2.7 > /dev/null 2>&1 && echo python2.7 || echo python)
 
-CONFIG_GUESS_SCRIPT := $(wildcard $(TOPSRCDIR)/build/autoconf/config.guess)
-ifdef CONFIG_GUESS_SCRIPT
-  CONFIG_GUESS := $(shell $(CONFIG_GUESS_SCRIPT))
-endif
-
+CONFIG_GUESS := $(shell $(TOPSRCDIR)/build/autoconf/config.guess)
 
 ####################################
 # Sanity checks
