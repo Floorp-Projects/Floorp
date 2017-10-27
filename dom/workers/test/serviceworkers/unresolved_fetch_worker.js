343bf30de9c0f79ce05a5871a02f82b44b5aa3b1
Ben Kelly <ben@wanderview.com>
2017-10-26 21:11:51 -0400

    Bug 1411746 Make tests that wait for controller change also wait for SW activation before claim(). r=tt

--- dom/workers/test/serviceworkers/unresolved_fetch_worker.js
+++ dom/workers/test/serviceworkers/unresolved_fetch_worker.js
@@ -14,6 +14,7 @@ onfetch = function(event) {
   event.respondWith((keepPromiseAlive = new Promise(function(res, rej) {})));
 }
 
-onactivate = function(event) {
+onmessage = function(event) {
   event.waitUntil(clients.claim());
+  self.onmessage = null;
 }
