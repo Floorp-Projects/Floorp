a276aa63b0b6e8c976f977686bc246b601e9bc7d
Gregory Szorc <gps@mozilla.com>
2017-10-27 14:47:06 -0700

    Bug 1412431 - Remove unused BASE_DIR variable; r=nalexander
    This variable has been unused since 683e59dc3094 (bug 1286934).
    
    MozReview-Commit-ID: FZ7D9mSIeME

--- build/sccache.mk
+++ build/sccache.mk
@@ -2,13 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifdef OBJDIR
-BASE_DIR = $(OBJDIR)
-else
-# OSX Universal builds only do upload in the first MOZ_BUILD_PROJECTS
-BASE_DIR = $(MOZ_OBJDIR)/$(firstword $(MOZ_BUILD_PROJECTS))
-endif
-
 preflight_all:
 	# Terminate any sccache server that might still be around
 	-$(TOPSRCDIR)/sccache2/sccache --stop-server > /dev/null 2>&1
