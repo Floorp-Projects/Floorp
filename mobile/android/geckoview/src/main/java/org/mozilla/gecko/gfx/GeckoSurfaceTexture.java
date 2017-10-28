32aefc877bb56b0f880096246d72aa6c3d288a44
Jamie Nicol <jnicol@mozilla.com>
2017-10-28 11:59:58 +0100

    Bug 1092294 - Use SurfaceTextures for painted content on android (preffed off). r=nical,snorp
    Add a new TextureClientData type, AndroidNativeWindowTextureData,
    backed by a SurfaceTexture in single-buffer mode. It uses the
    NativeWindow API, which provides producer-side access to the buffer.
    This provides a DrawTarget which can be used to paint directly in to
    the SurfaceTexture, which can then be composited using a SurfaceTextureHost.
    
    Due to API restrictions it is not possible to read from a NativeWindow
    while the corresponding SurfaceTexture has ownership of the
    buffer. TiledContentClient now handles that by painting the additional
    region that it cannot copy from the front buffer, if required.
    
    MozReview-Commit-ID: 1NZq6MQqwFq

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurfaceTexture.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurfaceTexture.java
@@ -80,9 +80,13 @@ public final class GeckoSurfaceTexture extends SurfaceTexture {
             return;
         }
 
-        super.releaseTexImage();
-        if (mListener != null) {
-            mListener.onReleaseTexImage();
+        try {
+            super.releaseTexImage();
+            if (mListener != null) {
+                mListener.onReleaseTexImage();
+            }
+        } catch (Exception e) {
+            Log.w(LOGTAG, "releaseTexImage() failed", e);
         }
     }
 
