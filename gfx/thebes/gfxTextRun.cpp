a7e307cc5d6194fc3121585975598a5f4e4cfe05
Jonathan Kew <jkew@mozilla.com>
2017-10-28 11:37:35 +0100

    Bug 1412355 - Replace the mXOffset/mYOffset fields in DetailedGlyph records with a gfx::Point that stores glyph offsets in line-orientation-relative coordinates. r=jrmuizel

--- gfx/thebes/gfxTextRun.cpp
+++ gfx/thebes/gfxTextRun.cpp
@@ -2758,7 +2758,6 @@ gfxFontGroup::InitScriptRun(DrawTarget* aDrawTarget,
                             gfxTextRun::DetailedGlyph detailedGlyph;
                             detailedGlyph.mGlyphID = mainFont->GetSpaceGlyph();
                             detailedGlyph.mAdvance = advance;
-                            detailedGlyph.mXOffset = detailedGlyph.mYOffset = 0;
                             gfxShapedText::CompressedGlyph g;
                             g.SetComplex(true, true, 1);
                             aTextRun->SetGlyphs(aOffset + index,
