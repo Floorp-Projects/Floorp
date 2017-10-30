f8aac134bec7c9cff0fb1dfb81ce49403fb9303b
Henri Sivonen <hsivonen@hsivonen.fi>
2017-10-30 17:15:19 +0200

    Bug 1364399 - Call DidBuildModel() when not flushing. r=smaug.
    MozReview-Commit-ID: CtYHqN8myb1

--- parser/html/nsHtml5TreeOperation.cpp
+++ parser/html/nsHtml5TreeOperation.cpp
@@ -811,7 +811,8 @@ nsHtml5TreeOperation::MarkMalformedIfScript(nsIContent* aNode)
 nsresult
 nsHtml5TreeOperation::Perform(nsHtml5TreeOpExecutor* aBuilder,
                               nsIContent** aScriptElement,
-                              bool* aInterrupted)
+                              bool* aInterrupted,
+                              bool* aStreamEnded)
 {
   switch(mOpCode) {
     case eTreeOpUninitialized: {
@@ -1034,7 +1035,7 @@ nsHtml5TreeOperation::Perform(nsHtml5TreeOpExecutor* aBuilder,
       return NS_OK;
     }
     case eTreeOpStreamEnded: {
-      aBuilder->DidBuildModel(false); // this causes a notifications flush anyway
+      *aStreamEnded = true;
       return NS_OK;
     }
     case eTreeOpSetStyleLineNumber: {
