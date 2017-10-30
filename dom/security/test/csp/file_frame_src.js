67c85139df5e173d62e0220ecc7855fe76c4474a
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2017-10-30 18:46:19 +0100

    Bug 1302667 - CSP: Test 'frame-src'. r=dveditz,mckinley

---
+++ dom/security/test/csp/file_frame_src.js
@@ -0,0 +1,14 @@
+let testframe = document.getElementById("testframe");
+testframe.onload = function() {
+  parent.postMessage({
+    result: "frame-allowed",
+    href: document.location.href,
+  }, "*");
+}
+testframe.onerror = function() {
+  parent.postMessage({
+    result: "frame-blocked",
+    href: document.location.href,
+  }, "*");
+}
+testframe.src = "file_frame_src_inner.html"
