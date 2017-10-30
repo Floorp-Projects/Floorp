0629cfee11b59a4f6f3ff66d7c94c7eb0237cd00
Ben Kelly <ben@wanderview.com>
2017-10-30 13:35:08 -0400

    Bug 1412864 Add ClientInfo and ClientState types. r=baku

---
+++ dom/clients/manager/ClientInfo.cpp
@@ -0,0 +1,114 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ClientInfo.h"
+
+#include "mozilla/dom/ClientIPCTypes.h"
+
+namespace mozilla {
+namespace dom {
+
+ClientInfo::ClientInfo(const nsID& aId,
+                       ClientType aType,
+                       const mozilla::ipc::PrincipalInfo& aPrincipalInfo,
+                       const TimeStamp& aCreationTime)
+  : mData(MakeUnique<IPCClientInfo>(aId, aType, aPrincipalInfo, aCreationTime,
+                                    EmptyCString(),
+                                    mozilla::dom::FrameType::None))
+{
+}
+
+ClientInfo::ClientInfo(const IPCClientInfo& aData)
+  : mData(MakeUnique<IPCClientInfo>(aData))
+{
+}
+
+ClientInfo::ClientInfo(const ClientInfo& aRight)
+{
+  operator=(aRight);
+}
+
+ClientInfo&
+ClientInfo::operator=(const ClientInfo& aRight)
+{
+  mData.reset();
+  mData = MakeUnique<IPCClientInfo>(*aRight.mData);
+  return *this;
+}
+
+ClientInfo::ClientInfo(ClientInfo&& aRight)
+  : mData(Move(aRight.mData))
+{
+}
+
+ClientInfo&
+ClientInfo::operator=(ClientInfo&& aRight)
+{
+  mData.reset();
+  mData = Move(aRight.mData);
+  return *this;
+}
+
+ClientInfo::~ClientInfo()
+{
+}
+
+const nsID&
+ClientInfo::Id() const
+{
+  return mData->id();
+}
+
+ClientType
+ClientInfo::Type() const
+{
+  return mData->type();
+}
+
+const mozilla::ipc::PrincipalInfo&
+ClientInfo::PrincipalInfo() const
+{
+  return mData->principalInfo();
+}
+
+const TimeStamp&
+ClientInfo::CreationTime() const
+{
+  return mData->creationTime();
+}
+
+const nsCString&
+ClientInfo::URL() const
+{
+  return mData->url();
+}
+
+void
+ClientInfo::SetURL(const nsACString& aURL)
+{
+  mData->url() = aURL;
+}
+
+FrameType
+ClientInfo::FrameType() const
+{
+  return mData->frameType();
+}
+
+void
+ClientInfo::SetFrameType(mozilla::dom::FrameType aFrameType)
+{
+  mData->frameType() = aFrameType;
+}
+
+const IPCClientInfo&
+ClientInfo::ToIPC() const
+{
+  return *mData;
+}
+
+} // namespace dom
+} // namespace mozilla
