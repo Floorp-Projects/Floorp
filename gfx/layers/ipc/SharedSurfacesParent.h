cb2cb3e7e9584cf436f34c3bc6b4be9b077e5d29
Andrew Osmond <aosmond@mozilla.com>
2017-10-30 13:10:29 -0400

    Bug 1331944 - Part 2. Add SharedSurfacesParent/Child to manage shared surfaces. r=jrmuizel

---
+++ gfx/layers/ipc/SharedSurfacesParent.h
@@ -0,0 +1,69 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_GFX_SHAREDSURFACESPARENT_H
+#define MOZILLA_GFX_SHAREDSURFACESPARENT_H
+
+#include <stdint.h>                     // for uint32_t
+#include "mozilla/Attributes.h"         // for override
+#include "mozilla/StaticPtr.h"          // for StaticAutoPtr
+#include "mozilla/RefPtr.h"             // for already_AddRefed
+#include "mozilla/ipc/SharedMemory.h"   // for SharedMemory, etc
+#include "mozilla/gfx/2D.h"             // for SurfaceFormat
+#include "mozilla/gfx/Point.h"          // for IntSize
+#include "mozilla/layers/LayersSurfaces.h"    // for SurfaceDescriptorShared
+#include "mozilla/webrender/WebRenderTypes.h" // for wr::ExternalImageId
+#include "nsRefPtrHashtable.h"
+
+namespace mozilla {
+namespace gfx {
+class DataSourceSurface;
+class SourceSurfaceSharedData;
+class SourceSurfaceSharedDataWrapper;
+} // namespace gfx
+
+namespace layers {
+
+class SharedSurfacesChild;
+
+class SharedSurfacesParent final
+{
+public:
+  static void Initialize();
+  static void Shutdown();
+
+  static already_AddRefed<gfx::DataSourceSurface>
+  Acquire(const wr::ExternalImageId& aId);
+
+  static bool Release(const wr::ExternalImageId& aId);
+
+  static void Add(const wr::ExternalImageId& aId,
+                  const SurfaceDescriptorShared& aDesc,
+                  base::ProcessId aPid);
+
+  static void Remove(const wr::ExternalImageId& aId);
+
+  static void DestroyProcess(base::ProcessId aPid);
+
+  ~SharedSurfacesParent();
+
+private:
+  friend class SharedSurfacesChild;
+
+  SharedSurfacesParent();
+
+  static void AddSameProcess(const wr::ExternalImageId& aId,
+                             gfx::SourceSurfaceSharedData* aSurface);
+  static void RemoveSameProcess(const wr::ExternalImageId& aId);
+
+  static StaticAutoPtr<SharedSurfacesParent> sInstance;
+
+  nsRefPtrHashtable<nsUint64HashKey, gfx::SourceSurfaceSharedDataWrapper> mSurfaces;
+};
+
+} // namespace layers
+} // namespace mozilla
+
+#endif
