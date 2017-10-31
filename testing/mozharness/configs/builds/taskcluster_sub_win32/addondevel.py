d84084cac22fbcd548dee49757e6f63c9f93ef8e
Tom Prince <mozilla@hocat.ca>
2017-10-31 17:17:23 -0600

    Bug 1418141: Combine taskcluster win32 mozharness configs. r=jlund
    MozReview-Commit-ID: ICMryQvdHCz
    
    --HG--
    extra : rebase_source : be395093eee9318c6c0f9fe0d9c900c66043cbaf

---
+++ testing/mozharness/configs/builds/taskcluster_sub_win32/addondevel.py
@@ -0,0 +1,8 @@
+config = {
+    # decides whether we want to use moz_sign_cmd in en+
+    'enable_signing': False,
+    'base_name': 'WINNT 5.2 add-on-devel %(branch)s',
+    'stage_platform': 'win32-add-on-devel',
+    'publish_nightly_en_US_routes': False,
+    'mozconfig_variant': 'add-on-devel',
+}
