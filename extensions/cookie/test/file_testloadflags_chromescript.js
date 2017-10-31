118972dd742033c93297ee23fe7caf349c780895
Valentin Gosu <valentin.gosu@gmail.com>
2017-10-31 13:02:28 +0100

    Bug 1407720 - Merge nsICookieManager and nsICookieManager2 r=nwgh
    MozReview-Commit-ID: HuzeeB43YZZ

--- extensions/cookie/test/file_testloadflags_chromescript.js
+++ extensions/cookie/test/file_testloadflags_chromescript.js
@@ -78,7 +78,7 @@ function getCookieCount(cs) {
 
 addMessageListener("init", ({ domain }) => {
   let cs = Cc["@mozilla.org/cookiemanager;1"]
-             .getService(Ci.nsICookieManager2);
+             .getService(Ci.nsICookieManager);
 
   info("we are going to remove these cookies");
 
@@ -106,7 +106,7 @@ addMessageListener("shutdown", () => {
   gObs.remove();
 
   let cs = Cc["@mozilla.org/cookiemanager;1"]
-             .getService(Ci.nsICookieManager2);
+             .getService(Ci.nsICookieManager);
   cs.removeAll();
   sendAsyncMessage("shutdown:return");
 });
