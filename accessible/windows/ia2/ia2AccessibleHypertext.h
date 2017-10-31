e509df8b29d981ab98db2bad7a7b1e5ee14c46c6
Phil Ringnalda <philringnalda@gmail.com>
2017-10-30 21:02:17 -0700

    Backed out changeset ae94e40d58e0 (bug 873444) for Windows build bustage CLOSED TREE
    MozReview-Commit-ID: 4iFXxTYBN1y

--- accessible/windows/ia2/ia2AccessibleHypertext.h
+++ accessible/windows/ia2/ia2AccessibleHypertext.h
@@ -11,13 +11,13 @@
 #include "nsISupports.h"
 
 #include "ia2AccessibleText.h"
-#include "AccessibleHypertext2.h"
+#include "AccessibleHypertext.h"
 
 namespace mozilla {
 namespace a11y {
 
 class ia2AccessibleHypertext : public ia2AccessibleText,
-                               public IAccessibleHypertext2
+                               public IAccessibleHypertext
 {
 public:
 
@@ -35,11 +35,6 @@ public:
   virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_hyperlinkIndex(
       /* [in] */ long charIndex,
       /* [retval][out] */ long* hyperlinkIndex);
-
-  // IAccessibleHypertext2
-  virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_hyperlinks(
-      /* [out, size_is(,*nHyperlinks)] */ IAccessibleHyperlink*** hyperlinks,
-      /* [out, retval] */ long* nHyperlinks);
 };
 
 } // namespace a11y
