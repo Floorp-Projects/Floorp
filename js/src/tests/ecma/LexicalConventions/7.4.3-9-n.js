9ceff1276b2ad43a83cbb7797e1c86195fd7e48f
André Bargull <andre.bargull@gmail.com>
2017-10-31 08:02:57 -0700

    Bug 1346234 - Part 22: Remove unnecessary calls to options() function from tests. r=Waldo
    --HG--
    extra : rebase_source : 68b2300758d710eb6da5c5ff9d1d3dd8a2e528f1

--- js/src/tests/ecma/LexicalConventions/7.4.3-9-n.js
+++ js/src/tests/ecma/LexicalConventions/7.4.3-9-n.js
@@ -30,21 +30,8 @@ var TITLE   = "Future Reserved Words";
 writeHeaderToLog( SECTION + " "+ TITLE);
 
 var actual = 'no error';
-var prefValue;
 
 DESCRIPTION = "var class = true";
-
-
-print("This test requires option javascript.options.strict enabled");
-
-if (!options().match(/strict/))
-{
-  options('strict');
-}
-if (!options().match(/werror/))
-{
-  options('werror');
-}
 
 try
 {
