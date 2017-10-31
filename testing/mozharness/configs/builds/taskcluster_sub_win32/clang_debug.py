0aca37c68073527d6079feb22f56a6a8d7ddd165
Tom Prince <mozilla@hocat.ca>
2017-10-31 17:17:23 -0600

    Bug 1418141: Combine taskcluster win32 mozharness configs. r=jlund
    MozReview-Commit-ID: ICMryQvdHCz

---
+++ testing/mozharness/configs/builds/taskcluster_sub_win32/clang_debug.py
@@ -0,0 +1,10 @@
+config = {
+    'perfherder_extra_options': ['static-analysis'],
+    'stage_platform': 'win32-st-an-debug',
+    'debug_build': True,
+    'env': {
+        'XPCOM_DEBUG_BREAK': 'stack-and-abort',
+    },
+    'mozconfig_variant': 'clang-debug',
+    'artifact_flag_build_variant_in_try': None,
+}
