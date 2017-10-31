91aa86db7ef0dde82a5b1cf1617125d8b7616b04
André Bargull <andre.bargull@gmail.com>
2017-10-31 08:02:56 -0700

    Bug 1346234 - Part 13: Remove call to testcase-dump method from browser.js. r=Waldo
    --HG--
    extra : rebase_source : a7a62a020a30c0aac5d7e75ec7ab6c3fabe16f50

--- js/src/tests/browser.js
+++ js/src/tests/browser.js
@@ -646,12 +646,6 @@ function jsTestDriverEnd()
   }
   else
   {
-    var testCases = getTestCases();
-    for (var i = 0; i < testCases.length; i++)
-    {
-      testCases[i].dump();
-    }
-
     // tell reftest the test is complete.
     document.documentElement.className = '';
     // tell Spider page is complete
