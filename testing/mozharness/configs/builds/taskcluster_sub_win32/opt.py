9ad773a916cb3a9167a449e68101723fe4a9d546
Tom Prince <mozilla@hocat.ca>
2017-10-31 17:17:23 -0600

    Bug 1418141: Combine taskcluster win32 mozharness configs. r=jlund
    MozReview-Commit-ID: ICMryQvdHCz
    
    --HG--
    extra : rebase_source : fa95cfff4e789d91763922233547a74c8a48fe0b

---
+++ testing/mozharness/configs/builds/taskcluster_sub_win32/opt.py
@@ -0,0 +1,10 @@
+config = {
+    'default_actions': [
+        'clone-tools',
+        'build',
+        'check-test',
+        'update',
+    ],
+    'stage_platform': 'win32',
+    'mozconfig_variant': 'nightly',
+}
---
+++ testing/mozharness/configs/builds/taskcluster_sub_win32/rusttests_opt.py
@@ -0,0 +1,5 @@
+config = {
+    'stage_platform': 'win32-rusttests',
+    'mozconfig_variant': 'rusttests',
+    'artifact_flag_build_variant_in_try': None,
+}
