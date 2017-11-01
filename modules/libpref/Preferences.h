1c6ee5e178f6ff0458bd1c3f3d0feeea4f24563d
Nicholas Nethercote <nnethercote@mozilla.com>
2017-11-01 13:41:14 +1100

    Bug 1413400 (part 1) - Move s{,Default}RootBranch into Preferences. r=froydnj.
    sRootBranch and sDefaultRootBranch have the same lifetime as sPreferences, so
    this patch makes them non-static nsCOMPtr<> members of Preferences.
    
    MozReview-Commit-ID: 1TLhh13ZpBI

--- modules/libpref/Preferences.h
+++ modules/libpref/Preferences.h
@@ -64,7 +64,7 @@ public:
 
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIPREFSERVICE
-  NS_FORWARD_NSIPREFBRANCH(sRootBranch->)
+  NS_FORWARD_NSIPREFBRANCH(mRootBranch->)
   NS_DECL_NSIOBSERVER
 
   Preferences();
@@ -94,14 +94,14 @@ public:
   static nsIPrefBranch* GetRootBranch()
   {
     NS_ENSURE_TRUE(InitStaticMembers(), nullptr);
-    return sRootBranch;
+    return sPreferences->mRootBranch;
   }
 
   // Returns shared default pref branch instance. NOTE: not addreffed.
   static nsIPrefBranch* GetDefaultRootBranch()
   {
     NS_ENSURE_TRUE(InitStaticMembers(), nullptr);
-    return sDefaultRootBranch;
+    return sPreferences->mDefaultRootBranch;
   }
 
   // Gets int or bool type pref value with default value if failed to get the
@@ -403,9 +403,10 @@ private:
   // mDirty and mSavePending will both be true.
   bool mSavePending = false;
 
+  nsCOMPtr<nsIPrefBranch> mRootBranch;
+  nsCOMPtr<nsIPrefBranch> mDefaultRootBranch;
+
   static Preferences* sPreferences;
-  static nsIPrefBranch* sRootBranch;
-  static nsIPrefBranch* sDefaultRootBranch;
   static bool sShutdown;
 
   // Init static members. Returns true on success.
