78cf691bc1d4d01e4bf52b2ad5541d42dc670268
Phil Ringnalda <philringnalda@gmail.com>
2017-10-31 20:27:48 -0700

    Backed out changeset fdebe274e971 (bug 1413092) for failures in test_mozbuild_reading.py CLOSED TREE
    MozReview-Commit-ID: JA9UPvYmWMF

--- browser/modules/test/browser/browser_bug1319078.js
+++ browser/modules/test/browser/browser_bug1319078.js
@@ -17,13 +17,13 @@ function incrementTest() {
 /**
  * In this test, we check that no popup appears if the element display is none.
  */
-add_task(async function test_display_none() {
+add_task(async function() {
   ok(gInvalidFormPopup,
      "The browser should have a popup to show when a form is invalid");
 
   incrementTest();
   let testPage =
-    "data:text/html;charset=utf-8," +
+    "data:text/html," +
     '<form target="t"><input type="url"  placeholder="url" value="http://" style="display: none;"><input id="s" type="button" value="check"></form>';
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
   await BrowserTestUtils.synthesizeMouse("#s", 0, 0, {}, gBrowser.selectedBrowser);
@@ -35,10 +35,10 @@ add_task(async function test_display_none() {
 /**
  * In this test, we check that no popup appears if the element visibility is hidden.
  */
-add_task(async function test_visibility_hidden() {
+add_task(async function() {
   incrementTest();
   let testPage =
-    "data:text/html;charset=utf-8," +
+    "data:text/html," +
     '<form target="t"><input type="url"  placeholder="url" value="http://" style="visibility: hidden;"><input id="s" type="button" value="check"></form>';
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
   await BrowserTestUtils.synthesizeMouse("#s", 0, 0, {}, gBrowser.selectedBrowser);
