5808e1f34c5618291841d084505c7ca073638421
Jan de Mooij <jdemooij@mozilla.com>
2017-11-01 11:40:17 +0100

    Bug 1394831 part 9 - Remove allowDictionary argument from addAccessorProperty. r=bhackett

--- js/src/vm/Shape-inl.h
+++ js/src/vm/Shape-inl.h
@@ -412,8 +412,7 @@ NativeObject::addDataProperty(JSContext* cx, HandleNativeObject obj, HandleId id
 
 /* static */ MOZ_ALWAYS_INLINE Shape*
 NativeObject::addAccessorProperty(JSContext* cx, HandleNativeObject obj, HandleId id,
-                                  GetterOp getter, SetterOp setter, unsigned attrs,
-                                  bool allowDictionary)
+                                  GetterOp getter, SetterOp setter, unsigned attrs)
 {
     MOZ_ASSERT(!JSID_IS_VOID(id));
     MOZ_ASSERT(obj->uninlinedNonProxyIsExtensible());
@@ -428,8 +427,7 @@ NativeObject::addAccessorProperty(JSContext* cx, HandleNativeObject obj, HandleI
         entry = &table->search<MaybeAdding::Adding>(id, keep);
     }
 
-    return addAccessorPropertyInternal(cx, obj, id, getter, setter, attrs, entry,
-                                       allowDictionary, keep);
+    return addAccessorPropertyInternal(cx, obj, id, getter, setter, attrs, entry, keep);
 }
 
 } /* namespace js */
