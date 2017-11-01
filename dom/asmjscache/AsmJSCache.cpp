efd74b93d892a8bef08d46b0c7e5b309a330d187
Shawn Huang <shuang@mozilla.com>
2017-11-01 11:14:20 +0800

    Bug 1389561 - Part 3: Wait for releasing all AsmJSCache parent actors in Client::ShutdownWorkThreads(). r=janv
    This patch implements Client::ShutdownWorkThreads for taking care of unreleased DirectoryLock and QuotaObject objects.

--- dom/asmjscache/AsmJSCache.cpp
+++ dom/asmjscache/AsmJSCache.cpp
@@ -67,12 +67,18 @@ namespace asmjscache {
 
 namespace {
 
+class ParentRunnable;
+
 // Anything smaller should compile fast enough that caching will just add
 // overhead.
 static const size_t sMinCachedModuleLength = 10000;
 
 // The number of characters to hash into the Metadata::Entry::mFastHash.
 static const unsigned sNumFastHashChars = 4096;
+
+// Track all live parent actors.
+typedef nsTArray<const ParentRunnable*> ParentActorArray;
+StaticAutoPtr<ParentActorArray> sLiveParentActors;
 
 nsresult
 WriteMetadataFile(nsIFile* aMetadataFile, const Metadata& aMetadata)
@@ -802,6 +808,13 @@ ParentRunnable::FinishOnOwningThread()
   FileDescriptorHolder::Finish();
 
   mDirectoryLock = nullptr;
+
+  MOZ_ASSERT(sLiveParentActors);
+  sLiveParentActors->RemoveElement(this);
+
+  if (sLiveParentActors->IsEmpty()) {
+    sLiveParentActors = nullptr;
+  }
 }
 
 NS_IMETHODIMP
@@ -1038,6 +1051,12 @@ AllocEntryParent(OpenMode aOpenMode,
 
   RefPtr<ParentRunnable> runnable =
     new ParentRunnable(aPrincipalInfo, aOpenMode, aWriteParams);
+
+  if (!sLiveParentActors) {
+    sLiveParentActors = new ParentActorArray();
+  }
+
+  sLiveParentActors->AppendElement(runnable);
 
   nsresult rv = NS_DispatchToMainThread(runnable);
   NS_ENSURE_SUCCESS(rv, nullptr);
@@ -1687,7 +1706,15 @@ public:
 
   void
   ShutdownWorkThreads() override
-  { }
+  {
+    AssertIsOnBackgroundThread();
+
+    if (sLiveParentActors) {
+      MOZ_ALWAYS_TRUE(SpinEventLoopUntil([&]() {
+        return !sLiveParentActors;
+      }));
+    }
+  }
 
 private:
   nsAutoRefCnt mRefCnt;
