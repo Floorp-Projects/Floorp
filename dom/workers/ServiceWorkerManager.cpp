b163a5642f9b90114d763ad1983daff2945cf65e
Bevis Tseng <btseng@mozilla.com>
2017-11-01 15:18:23 +0800

    Bug 1413466 - Fire events after iterating ServiceWorkerManager::mControlledDocuments is done. r=bkelly
    It's possible to modify mControlledDocuments from oncontrollerchange handler.
    We should fire events after iteration to prevent updating this hashtable while reading it.

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -3363,6 +3363,8 @@ void
 ServiceWorkerManager::FireControllerChange(ServiceWorkerRegistrationInfo* aRegistration)
 {
   AssertIsOnMainThread();
+
+  AutoTArray<nsCOMPtr<nsIDocument>, 16> documents;
   for (auto iter = mControlledDocuments.Iter(); !iter.Done(); iter.Next()) {
     if (iter.UserData() != aRegistration) {
       continue;
@@ -3373,6 +3375,12 @@ ServiceWorkerManager::FireControllerChange(ServiceWorkerRegistrationInfo* aRegis
       continue;
     }
 
+    documents.AppendElement(doc);
+  }
+
+  // Fire event after iterating mControlledDocuments is done to prevent
+  // modification by reentering from the event handlers during iteration.
+  for (auto& doc : documents) {
     FireControllerChangeOnDocument(doc);
   }
 }
