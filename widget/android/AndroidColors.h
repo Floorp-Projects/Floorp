d045d7514a47069ab0aeb12be334f1d9e8a12053
Jim Chen <nchen@mozilla.com>
2017-11-01 14:48:49 -0400

    Bug 1406168 - 5. Fix build; r=snorp
    Removing the previous cpp files moved AndroidBridge.cpp to a different
    compilation unit, and that caused some problems with other code that
    depended on AndroidBridge being in or not in their compilation unit.
    This patch fixes those (unwanted) dependencies.
    
    MozReview-Commit-ID: DJsk3iENsx2

--- widget/android/AndroidColors.h
+++ widget/android/AndroidColors.h
@@ -11,11 +11,11 @@
 namespace mozilla {
 namespace widget {
 
-static const Color sAndroidBackgroundColor(Color(1.0f, 1.0f, 1.0f));
-static const Color sAndroidBorderColor(Color(0.73f, 0.73f, 0.73f));
-static const Color sAndroidCheckColor(Color(0.19f, 0.21f, 0.23f));
-static const Color sAndroidDisabledColor(Color(0.88f, 0.88f, 0.88f));
-static const Color sAndroidActiveColor(Color(0.94f, 0.94f, 0.94f));
+static const gfx::Color sAndroidBackgroundColor(gfx::Color(1.0f, 1.0f, 1.0f));
+static const gfx::Color sAndroidBorderColor(gfx::Color(0.73f, 0.73f, 0.73f));
+static const gfx::Color sAndroidCheckColor(gfx::Color(0.19f, 0.21f, 0.23f));
+static const gfx::Color sAndroidDisabledColor(gfx::Color(0.88f, 0.88f, 0.88f));
+static const gfx::Color sAndroidActiveColor(gfx::Color(0.94f, 0.94f, 0.94f));
 
 } // namespace widget
 } // namespace mozilla
