da9eb82801cfc85d740e1fb8df1e5f39c32e41d7
Nick Alexander <nalexander@mozilla.com>
2017-11-02 09:47:10 -0700

    Bug 1411688 - Part 3: Set MOZ_UPDATE_CHANNEL in single-locale repacks. r=aki
    I'm seeing "try" in my repacks, when the underlying build has
    "nightly-try".  This should make the two agree.
    
    MozReview-Commit-ID: 45yE9Qwz0v7
    
    --HG--
    extra : rebase_source : 8b524470680e248c649bf3e4e532cdd5487ec538

--- testing/mozharness/scripts/mobile_l10n.py
+++ testing/mozharness/scripts/mobile_l10n.py
@@ -187,6 +187,18 @@ class MobileSingleLocale(MockMixin, LocalesMixin, ReleaseMixin,
         if 'MOZ_SIGNING_SERVERS' in os.environ:
             repack_env['MOZ_SIGN_CMD'] = \
                 subprocess.list2cmdline(self.query_moz_sign_cmd(formats=['jar']))
+
+        if self.query_is_nightly() or self.query_is_nightly_promotion():
+            if self.query_is_nightly():
+                # nightly promotion needs to set update_channel but not do all the 'IS_NIGHTLY'
+                # automation parts like uploading symbols for now
+                env["IS_NIGHTLY"] = "yes"
+            # in branch_specifics.py we might set update_channel explicitly
+            if c.get('update_channel'):
+                env["MOZ_UPDATE_CHANNEL"] = c['update_channel']
+            else:  # let's just give the generic channel based on branch
+                env["MOZ_UPDATE_CHANNEL"] = "nightly-%s" % (c['branch'],)
+
         self.repack_env = repack_env
         return self.repack_env
 
