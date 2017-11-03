cc13978df638cf5c377e7b9b5443a6991ecc99fe
Henrik Adolfsson <anting004@gmail.com>
2017-11-03 19:23:34 +0100

    Bug 1412179 - Implement fieldsets with non-empty legends. r=Gankro
    MozReview-Commit-ID: 2Ufrwu4x2LE

--- layout/forms/nsFieldSetFrame.cpp
+++ layout/forms/nsFieldSetFrame.cpp
@@ -163,27 +163,40 @@ nsDisplayFieldSetBorder::CreateWebRenderCommands(mozilla::wr::DisplayListBuilder
   auto frame = static_cast<nsFieldSetFrame*>(mFrame);
   auto offset = ToReferenceFrame();
   nsRect rect;
+  bool pushedClip = false;
 
   if (nsIFrame* legend = frame->GetLegend()) {
     rect = frame->VisualBorderRectRelativeToSelf() + offset;
-
-    // Legends require a "negative" clip around the text, which WR doesn't support yet.
     nsRect legendRect = legend->GetNormalRect() + offset;
+
     if (!legendRect.IsEmpty()) {
-      return false;
+      auto appUnitsPerDevPixel = frame->PresContext()->AppUnitsPerDevPixel();
+      auto layoutRect = LayoutDeviceRect::FromAppUnits(legendRect, appUnitsPerDevPixel);
+      wr::ComplexClipRegion region;
+      region.rect = aSc.ToRelativeLayoutRect(layoutRect);
+      region.mode = wr::ClipMode::ClipOut;
+      nsTArray<mozilla::wr::ComplexClipRegion> array{region};
+      wr::ClipId clip = aBuilder.DefineClip(Nothing(), Nothing(), region.rect, &array);
+      aBuilder.PushClip(clip);
+      pushedClip = true;
     }
+
   } else {
     rect = nsRect(offset, frame->GetRect().Size());
   }
 
-  return nsCSSRendering::CreateWebRenderCommandsForBorder(this,
-                                                          mFrame,
-                                                          rect,
-                                                          aBuilder,
-                                                          aResources,
-                                                          aSc,
-                                                          aManager,
-                                                          aDisplayListBuilder);
+  bool result = nsCSSRendering::CreateWebRenderCommandsForBorder(this,
+                                                                 mFrame,
+                                                                 rect,
+                                                                 aBuilder,
+                                                                 aResources,
+                                                                 aSc,
+                                                                 aManager,
+                                                                 aDisplayListBuilder);
+  if (pushedClip) {
+    aBuilder.PopClip();
+  }
+  return result;
 };
 
 void
