a1247d7bfd270e6944c2c416a92755cd605793d6
Botond Ballo <botond@mozilla.com>
2017-11-03 13:07:48 -0400

    Bug 951793 - Light refactoring to the fling handoff code. r=kats
    Passing FlingHandoffState around as an in-out parameter was making
    the next change (respecting overscroll-behavior) messy.
    
    MozReview-Commit-ID: 4wuoll20Jt7

--- gfx/layers/apz/src/APZCTreeManager.h
+++ gfx/layers/apz/src/APZCTreeManager.h
@@ -422,13 +422,14 @@ public:
    *                   start a fling (in this case the fling is given to the
    *                   first APZC in the chain)
    *
-   * aHandoffState.mVelocity will be modified depending on how much of that
-   * velocity has been consumed by APZCs in the overscroll hand-off chain.
+   * The return value is the "residual velocity", the portion of
+   * |aHandoffState.mVelocity| that was not consumed by APZCs in the
+   * handoff chain doing flings.
    * The caller can use this value to determine whether it should consume
-   * the excess velocity by going into an overscroll fling.
+   * the excess velocity by going into overscroll.
    */
-  void DispatchFling(AsyncPanZoomController* aApzc,
-                     FlingHandoffState& aHandoffState);
+  ParentLayerPoint DispatchFling(AsyncPanZoomController* aApzc,
+                                 const FlingHandoffState& aHandoffState);
 
   void StartScrollbarDrag(
       const ScrollableLayerGuid& aGuid,
