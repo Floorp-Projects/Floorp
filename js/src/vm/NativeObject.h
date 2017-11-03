34decd09c6e9e3e931a4bd78abf7bac6a49ce833
Steve Fink <sfink@mozilla.com>
2017-11-03 12:27:52 -0700

    Bug 903519 - Change Relocated marker to not confuse string vs object bit, r=jonco
    --HG--
    extra : rebase_source : 58f6ed8ba0692ad60b99c9d6a637231693e2316c
    extra : source : 6f3666e9540e849056347f7b9d8a40e41396115e
    extra : histedit_source : 315716a40a4ffba00fce14c8d58380128804e53d%2Cd69faed961ccbfc63d378ccf34c600d8698483f4

--- js/src/vm/NativeObject.h
+++ js/src/vm/NativeObject.h
@@ -1401,8 +1401,7 @@ class NativeObject : public ShapedObject
     }
 
     void setPrivateGCThing(gc::Cell* cell) {
-        MOZ_ASSERT_IF(IsMarkedBlack(this),
-                      !JS::GCThingIsMarkedGray(JS::GCCellPtr(cell, cell->getTraceKind())));
+        MOZ_ASSERT_IF(IsMarkedBlack(this), !cell->isMarkedGray());
         void** pprivate = &privateRef(numFixedSlots());
         privateWriteBarrierPre(pprivate);
         *pprivate = reinterpret_cast<void*>(cell);
