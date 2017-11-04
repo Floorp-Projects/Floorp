74efd7b084e6439289a3da5b49ac69611b70d4a1
cku <cku@mozilla.com>
2017-11-05 02:05:25 +0800

    Bug 1399787 - Part 6. Implement the bulk of a new PrintTargetEMF. r=jwatt
    A new subclass of PrintTarget.
    1. It uses PrintTargetSkPDF to generate one PDF FileDescriptor for one page.
    2. In a later patch, it then passes that FD to PDFium process for converting
       a PDF page to EMF contents.
    
    Implementation of integration with PDFium actor is added the subsequent patches.
    
    MozReview-Commit-ID: EcuBJHRW8Wk

---
+++ gfx/thebes/PrintTargetEMF.h
@@ -0,0 +1,62 @@
+/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef MOZILLA_GFX_PRINTTARGETEMF_H
+#define MOZILLA_GFX_PRINTTARGETEMF_H
+
+#include "PrintTargetSkPDF.h"
+
+/* include windows.h for the HDC definitions that we need. */
+#include <windows.h>
+
+namespace mozilla {
+namespace gfx {
+
+/**
+ * A new subclass of PrintTarget.
+ * 1. It uses PrintTargetSkPDF to generate one PDF file for one page.
+ * 2. It then passes the FileDescriptor of that generated PDF file to the
+ *    PDFium process for EMF conversion.
+ * 3. After getting the converted EMF contents from the PDFium process, it then
+ *    draws it onto the printer DC to finish one page printing task.
+ */
+class PrintTargetEMF final : public mozilla::gfx::PrintTarget
+{
+public:
+  typedef gfx::IntSize IntSize;
+
+  static already_AddRefed<PrintTargetEMF>
+  CreateOrNull(HDC aDC, const IntSize& aSizeInPoints);
+
+  nsresult BeginPrinting(const nsAString& aTitle,
+                                 const nsAString& aPrintToFileName,
+                                 int32_t aStartPage,
+                                 int32_t aEndPage) final;
+  nsresult EndPrinting() final;
+  nsresult AbortPrinting() final;
+  nsresult BeginPage() final;
+  nsresult EndPage() final;
+
+  already_AddRefed<DrawTarget>
+  MakeDrawTarget(const IntSize& aSize,
+                 DrawEventRecorder* aRecorder = nullptr) final;
+
+  already_AddRefed<DrawTarget>
+  GetReferenceDrawTarget(DrawEventRecorder* aRecorder) final;
+
+private:
+  PrintTargetEMF(HDC aDC, const IntSize& aSize);
+
+  nsString mTitle;
+  RefPtr<PrintTargetSkPDF> mTargetForCurrentPage;
+  nsCOMPtr<nsIFile>        mPDFFileForOnePage;
+  RefPtr<PrintTargetSkPDF> mRefTarget;
+  HDC mPrinterDC;
+};
+
+} // namespace gfx
+} // namespace mozilla
+
+#endif /* MOZILLA_GFX_PRINTTARGETEMF_H */
