8d5d5ad437d0bb1db55746c5fa8cae93b5f1abc1
cku <cku@mozilla.com>
2017-11-05 04:36:37 +0800

    Bug 1399787 - Part 11.a. Use PrintTargetEMF to print content documents. r=jwatt
    Before we introduce PrintTargetEMF, all PrintTargets finish page printing task
    before the end of PrintTarget::EndPage(). Unlike others, a page printing
    in PrintTargetEMF is done after receiving an async callback from the pdfium
    process. So we have both async and sync page printing behavior now. This patch
    is trying to make both of them work correctly while priting a content document.
    
    MozReview-Commit-ID: 2PHJToFlvtu
    
    --HG--
    extra : rebase_source : ee691497bd439c6505f1f92898a667a9e972fff3
    extra : source : f61eb00f83acf45511d8448922212dccb12b05aa

--- gfx/thebes/PrintTarget.h
+++ gfx/thebes/PrintTarget.h
@@ -6,6 +6,8 @@
 #ifndef MOZILLA_GFX_PRINTTARGET_H
 #define MOZILLA_GFX_PRINTTARGET_H
 
+#include <functional>
+
 #include "mozilla/RefPtr.h"
 #include "mozilla/gfx/2D.h"
 #include "nsISupportsImpl.h"
@@ -26,6 +28,7 @@ class DrawEventRecorder;
  */
 class PrintTarget {
 public:
+  typedef std::function<void(nsresult)> PageDoneCallback;
 
   NS_INLINE_DECL_REFCOUNTING(PrintTarget);
 
@@ -136,6 +139,17 @@ public:
    */
   virtual already_AddRefed<DrawTarget> GetReferenceDrawTarget(DrawEventRecorder* aRecorder);
 
+  /**
+   * If IsSyncPagePrinting returns true, then a user can assume the content of
+   * a page was already printed after EndPage().
+   * If IsSyncPagePrinting returns false, then a user should register a
+   * callback function using RegisterPageDoneCallback to receive page print
+   * done notifications.
+   */
+  virtual bool IsSyncPagePrinting() const { return true; }
+  void RegisterPageDoneCallback(PageDoneCallback&& aCallback);
+  void UnregisterPageDoneCallback();
+
   static void AdjustPrintJobNameForIPP(const nsAString& aJobName,
                                        nsCString& aAdjustedJobName);
   static void AdjustPrintJobNameForIPP(const nsAString& aJobName,
@@ -169,6 +183,8 @@ protected:
   // owned by mRecordingRefDT, so kept alive for our entire lifetime if set:
   DrawEventRecorder* mRecorder;
 #endif
+
+  PageDoneCallback mPageDoneCallback;
 };
 
 } // namespace gfx
