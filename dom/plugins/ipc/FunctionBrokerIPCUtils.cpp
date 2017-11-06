dbb0ff181a64e3dd7a999b9f3f78f7c77ae5fd9b
David Parks <dparks@mozilla.com>
2017-11-06 10:41:21 -0800

    Bug 1382251: Part 9 - Hook functions needed for SSL communication in NPAPI process r=jimm
    Adds Win32 networking APIs to the function broker so that we can run SSL communications outside of the sandbox.

--- dom/plugins/ipc/FunctionBrokerIPCUtils.cpp
+++ dom/plugins/ipc/FunctionBrokerIPCUtils.cpp
@@ -8,10 +8,28 @@
 
 #if defined(XP_WIN)
 
+#include <schannel.h>
+
+/* these defines are missing from mingw headers */
+#ifndef SP_PROT_TLS1_1_CLIENT
+#define SP_PROT_TLS1_1_CLIENT 0x00000200
+#endif
+
+#ifndef SP_PROT_TLS1_2_CLIENT
+#define SP_PROT_TLS1_2_CLIENT 0x00000800
+#endif
+
 namespace mozilla {
 namespace plugins {
 
 mozilla::LazyLogModule sPluginHooksLog("PluginHooks");
+
+static const DWORD SCHANNEL_SUPPORTED_PROTOCOLS =
+  SP_PROT_TLS1_CLIENT | SP_PROT_TLS1_1_CLIENT | SP_PROT_TLS1_2_CLIENT;
+
+static const DWORD SCHANNEL_SUPPORTED_FLAGS =
+  SCH_CRED_MANUAL_CRED_VALIDATION | SCH_CRED_NO_DEFAULT_CREDS |
+  SCH_CRED_REVOCATION_CHECK_END_CERT;
 
 void
 OpenFileNameIPC::CopyFromOfn(LPOPENFILENAMEW aLpofn)
@@ -201,6 +219,138 @@ OpenFileNameRetIPC::AddToOfn(LPOPENFILENAMEW aLpofn) const
   }
   aLpofn->nFileOffset = mFileOffset;
   aLpofn->nFileExtension = mFileExtension;
+}
+
+void
+IPCSchannelCred::CopyFrom(const PSCHANNEL_CRED& aSCred)
+{
+  // We assert that the aSCred fields take supported values.
+  // If they do not then we ignore the values we were given.
+  MOZ_ASSERT(aSCred->dwVersion == SCHANNEL_CRED_VERSION);
+  MOZ_ASSERT(aSCred->cCreds == 0);
+  MOZ_ASSERT(aSCred->paCred == nullptr);
+  MOZ_ASSERT(aSCred->hRootStore == nullptr);
+  MOZ_ASSERT(aSCred->cMappers == 0);
+  MOZ_ASSERT(aSCred->aphMappers == nullptr);
+  MOZ_ASSERT(aSCred->cSupportedAlgs == 0);
+  MOZ_ASSERT(aSCred->palgSupportedAlgs == nullptr);
+  MOZ_ASSERT((aSCred->grbitEnabledProtocols & SCHANNEL_SUPPORTED_PROTOCOLS) ==
+             aSCred->grbitEnabledProtocols);
+  mEnabledProtocols =
+    aSCred->grbitEnabledProtocols & SCHANNEL_SUPPORTED_PROTOCOLS;
+  mMinStrength = aSCred->dwMinimumCipherStrength;
+  mMaxStrength = aSCred->dwMaximumCipherStrength;
+  MOZ_ASSERT(aSCred->dwSessionLifespan == 0);
+  MOZ_ASSERT((aSCred->dwFlags & SCHANNEL_SUPPORTED_FLAGS) == aSCred->dwFlags);
+  mFlags = aSCred->dwFlags & SCHANNEL_SUPPORTED_FLAGS;
+  MOZ_ASSERT(aSCred->dwCredFormat == 0);
+}
+
+void
+IPCSchannelCred::CopyTo(PSCHANNEL_CRED& aSCred) const
+{
+  // Validate values as they come from an untrusted process.
+  memset(aSCred, 0, sizeof(SCHANNEL_CRED));
+  aSCred->dwVersion = SCHANNEL_CRED_VERSION;
+  aSCred->grbitEnabledProtocols =
+    mEnabledProtocols & SCHANNEL_SUPPORTED_PROTOCOLS;
+  aSCred->dwMinimumCipherStrength = mMinStrength;
+  aSCred->dwMaximumCipherStrength = mMaxStrength;
+  aSCred->dwFlags = mFlags & SCHANNEL_SUPPORTED_FLAGS;
+}
+
+void
+IPCInternetBuffers::CopyFrom(const LPINTERNET_BUFFERSA& aBufs)
+{
+  mBuffers.Clear();
+
+  LPINTERNET_BUFFERSA inetBuf = aBufs;
+  while (inetBuf) {
+    MOZ_ASSERT(inetBuf->dwStructSize == sizeof(INTERNET_BUFFERSA));
+    Buffer* ipcBuf = mBuffers.AppendElement();
+
+    ipcBuf->mHeader.SetIsVoid(inetBuf->lpcszHeader == nullptr);
+    if (inetBuf->lpcszHeader) {
+      ipcBuf->mHeader.Assign(inetBuf->lpcszHeader, inetBuf->dwHeadersLength);
+    }
+    ipcBuf->mHeaderTotal = inetBuf->dwHeadersTotal;
+
+    ipcBuf->mBuffer.SetIsVoid(inetBuf->lpvBuffer == nullptr);
+    if (inetBuf->lpvBuffer) {
+      ipcBuf->mBuffer.Assign(static_cast<char*>(inetBuf->lpvBuffer),
+                             inetBuf->dwBufferLength);
+    }
+    ipcBuf->mBufferTotal = inetBuf->dwBufferTotal;
+    inetBuf = inetBuf->Next;
+  }
+}
+
+void
+IPCInternetBuffers::CopyTo(LPINTERNET_BUFFERSA& aBufs) const
+{
+  MOZ_ASSERT(!aBufs);
+
+  LPINTERNET_BUFFERSA lastBuf = nullptr;
+  for (size_t idx = 0; idx < mBuffers.Length(); ++idx) {
+    const Buffer& ipcBuf = mBuffers[idx];
+    LPINTERNET_BUFFERSA newBuf =
+      static_cast<LPINTERNET_BUFFERSA>(moz_xcalloc(1, sizeof(INTERNET_BUFFERSA)));
+    if (idx == 0) {
+      aBufs = newBuf;
+    } else {
+      MOZ_ASSERT(lastBuf);
+      lastBuf->Next = newBuf;
+      lastBuf = newBuf;
+    }
+
+    newBuf->dwStructSize = sizeof(INTERNET_BUFFERSA);
+
+    newBuf->dwHeadersTotal = ipcBuf.mHeaderTotal;
+    if (!ipcBuf.mHeader.IsVoid()) {
+      newBuf->lpcszHeader =
+        static_cast<LPCSTR>(moz_xmalloc(ipcBuf.mHeader.Length()));
+      memcpy(const_cast<char*>(newBuf->lpcszHeader), ipcBuf.mHeader.Data(),
+             ipcBuf.mHeader.Length());
+      newBuf->dwHeadersLength = ipcBuf.mHeader.Length();
+    }
+
+    newBuf->dwBufferTotal = ipcBuf.mBufferTotal;
+    if (!ipcBuf.mBuffer.IsVoid()) {
+      newBuf->lpvBuffer = moz_xmalloc(ipcBuf.mBuffer.Length());
+      memcpy(newBuf->lpvBuffer, ipcBuf.mBuffer.Data(),
+             ipcBuf.mBuffer.Length());
+      newBuf->dwBufferLength = ipcBuf.mBuffer.Length();
+    }
+  }
+}
+
+/* static */ void
+IPCInternetBuffers::FreeBuffers(LPINTERNET_BUFFERSA& aBufs)
+{
+  if (!aBufs) {
+    return;
+  }
+  while (aBufs) {
+    LPINTERNET_BUFFERSA temp = aBufs->Next;
+    free(const_cast<char*>(aBufs->lpcszHeader));
+    free(aBufs->lpvBuffer);
+    free(aBufs);
+    aBufs = temp;
+  }
+}
+
+void
+IPCPrintDlg::CopyFrom(const LPPRINTDLGW& aDlg)
+{
+  // DLP: Trouble -- my prior impl "worked" but didn't return anything
+  // AFAIR.  So... ???  But it printed a page!!!  How?!
+  MOZ_ASSERT_UNREACHABLE("TODO: DLP:");
+}
+
+void
+IPCPrintDlg::CopyTo(LPPRINTDLGW& aDlg) const
+{
+  MOZ_ASSERT_UNREACHABLE("TODO: DLP:");
 }
 
 } // namespace plugins
