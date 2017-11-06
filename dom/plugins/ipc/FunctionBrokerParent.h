5de1dbff8bf28a38d72b0a240598a7ebd95558ae
David Parks <dparks@mozilla.com>
2017-11-06 10:29:15 -0800

    Bug 1382251: Part 6 - Start/stop new top-level brokering actors on their own threads; r=jld
    The FunctionBroker actors allow the NPAPI process (child) to run methods on the main process (parent).  Both the parent and the child run dedicated threads for this task -- this is a top-level protocol.

---
+++ dom/plugins/ipc/FunctionBrokerParent.h
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: sw=4 ts=4 et :
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_plugins_functionbrokerparent_h
+#define mozilla_plugins_functionbrokerparent_h
+
+#include "mozilla/plugins/PFunctionBrokerParent.h"
+
+namespace mozilla {
+namespace plugins {
+
+class FunctionBrokerThread;
+
+/**
+ * Top-level actor run on the process to which we broker calls from sandboxed
+ * plugin processes.
+ */
+class FunctionBrokerParent : public PFunctionBrokerParent
+{
+public:
+  static FunctionBrokerParent* Create(Endpoint<PFunctionBrokerParent>&& aParentEnd);
+  static void Destroy(FunctionBrokerParent* aInst);
+
+  void ActorDestroy(ActorDestroyReason aWhy) override;
+
+private:
+  explicit FunctionBrokerParent(FunctionBrokerThread* aThread,
+                                Endpoint<PFunctionBrokerParent>&& aParentEnd);
+  void ShutdownOnBrokerThread();
+  void Bind(Endpoint<PFunctionBrokerParent>&& aEnd);
+
+  nsAutoPtr<FunctionBrokerThread> mThread;
+  Monitor mMonitor;
+  bool mShutdownDone;
+};
+
+} // namespace plugins
+} // namespace mozilla
+
+#endif // mozilla_plugins_functionbrokerparent_hk
