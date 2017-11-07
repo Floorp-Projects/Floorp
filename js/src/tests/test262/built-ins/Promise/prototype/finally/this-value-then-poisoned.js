41348fbf04b7404b7821a522abd8a6e3e51bc87b
Sebastian Hengst <archaeopteryx@coole-files.de>
2017-11-07 20:48:17 +0200

    Backed out 3 changesets (bug 1019116) for failing tc-M(c3) in js/xpconnect/tests/chrome/test_xrayToJS.xul  r=backout on a CLOSED TREE
    Backed out changeset fc05d670729c (bug 1019116)
    Backed out changeset 2ab4309be28d (bug 1019116)
    Backed out changeset 7ca31c599c21 (bug 1019116)

--- js/src/tests/test262/built-ins/Promise/prototype/finally/this-value-then-poisoned.js
+++ js/src/tests/test262/built-ins/Promise/prototype/finally/this-value-then-poisoned.js
@@ -1,3 +1,4 @@
+// |reftest| skip -- Promise.prototype.finally is not supported
 // Copyright (C) 2017 Jordan Harband. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
