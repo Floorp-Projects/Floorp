7d9ef108ce46bb5b70a3c6546edf6723358a0beb
Lars T Hansen <lhansen@mozilla.com>
2017-11-08 11:44:53 +0100

    Bug 1423225 - Disable javascript.options.shared_memory. r=lth, r=jgraham, a=lizzard

--- dom/workers/test/serviceworkers/test_serviceworker_interfaces.js
+++ dom/workers/test/serviceworkers/test_serviceworker_interfaces.js
@@ -25,7 +25,7 @@ var ecmaGlobals =
   [
     "Array",
     "ArrayBuffer",
-    "Atomics",
+    {name: "Atomics", disabled: true},
     "Boolean",
     {name: "ByteLengthQueuingStrategy", optional: true},
     {name: "CountQueuingStrategy", optional: true},
@@ -56,7 +56,7 @@ var ecmaGlobals =
     "Reflect",
     "RegExp",
     "Set",
-    "SharedArrayBuffer",
+    {name: "SharedArrayBuffer", disabled: true},
     {name: "SIMD", nightly: true},
     "String",
     "Symbol",
@@ -256,7 +256,8 @@ function createInterfaceMap(version, userAgent) {
             (entry.nonReleaseAndroid === !(isAndroid && !isRelease) && isAndroid) ||
             (entry.desktop === !isDesktop) ||
             (entry.android === !isAndroid && !entry.nonReleaseAndroid && !entry.nightlyAndroid) ||
-            (entry.release === !isRelease)) {
+            (entry.release === !isRelease) ||
+            entry.disabled) {
           interfaceMap[entry.name] = false;
         } else if (entry.optional) {
           interfaceMap[entry.name] = "optional";
