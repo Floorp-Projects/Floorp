e487ded5089ad26715a74aaeb40f8e40fe8aa620
Jeff Walden <jwalden@mit.edu>
2017-11-07 16:03:52 -0800

    Bug 1414805 - Don't mishandle ASI after a ternary expression ending in an arrow function with block body, or such ternary appearing in a computed property name expression.  r=arai

--- js/src/tests/ecma_6/ArrowFunctions/arrow-not-as-end-of-statement.js
+++ js/src/tests/ecma_6/ArrowFunctions/arrow-not-as-end-of-statement.js
@@ -57,11 +57,52 @@ assertEq(typeof (a => {}), "function");
 for (var x in y => {})
   continue;
 
+assertEq(eval("a => {}, 17, 42;"), 42);
+assertEq(eval("42, a => {}, 17;"), 17);
+assertEq(typeof eval("17, 42, a => {};"), "function");
+
+assertEq(eval("1 ? 0 : a => {}, 17, 42;"), 42);
+assertEq(eval("42, 1 ? 0 : a => {}, 17;"), 17);
+assertEq(eval("17, 42, 1 ? 0 : a => {};"), 0);
+
 var z = { x: 0 ? 1 : async a => {} };
 assertEq(typeof z.x, "function");
 
 var q = 0 ? 1 : async () => {};
 assertEq(typeof q, "function");
+
+var m = 0 ? 42 : m = foo => {} // ASI
+/Q/g;
+assertEq(typeof m, "function");
+
+var { q: w = 0 ? 1 : a => {} } = {};
+assertEq(typeof w, "function");
+
+Function.prototype.c = 42;
+var { c } = 0 ? 1 : a => {} // ASI
+/Q/g;
+assertEq(c, 42);
+
+var c = 0 ? 1 : a => {}
+/Q/g;
+assertEq(typeof c, "function");
+delete Function.prototype.c;
+
+assertEq(typeof eval(0 ? 1 : a => {}), "function");
+
+var zoom = 1 ? a => {} : 357;
+assertEq(typeof zoom, "function");
+
+var { b = 0 ? 1 : a => {} } = {};
+assertEq(typeof b, "function");
+
+var [k = 0 ? 1 : a => {}] = [];
+assertEq(typeof k, "function");
+
+assertEq(typeof [0 ? 1 : a => {}][0], "function");
+
+var { [0 ? 1 : a => {}]: h } = { "a => {}": "boo-urns!" };
+assertEq(h, "boo-urns!");
 
 if (typeof reportCompare === "function")
   reportCompare(true, true);
