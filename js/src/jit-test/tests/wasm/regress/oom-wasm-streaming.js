321a1789d0c65a9134490c5254135644a6b2d172
Luke Wagner <luke@mozilla.com>
2017-11-08 09:15:26 -0800

    Bug 1414174 - Baldr: fix OOM handling in compileStreaming (r=bbouvier)

---
+++ js/src/jit-test/tests/wasm/regress/oom-wasm-streaming.js
@@ -0,0 +1,6 @@
+if (!this.oomAfterAllocations)
+    quit();
+oomAfterAllocations(1, 2);
+var x = wasmTextToBinary('(module (func (export "run") (result i32) i32.const 42))');
+WebAssembly.compileStreaming(x);
+drainJobQueue();
