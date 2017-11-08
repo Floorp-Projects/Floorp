1c13882dc4cd1a732b3b86d2b40cf5462a0bfa21
Paul Adenot <paul@paul.cx>
2017-11-08 15:16:06 +0100

    Bug 1397793 - Remove mRate from MediaEngineDefault and use GraphRate(). r=pehrsons
    MozReview-Commit-ID: D9x3wICAMNA
    
    --HG--
    extra : rebase_source : 6f863eccdf5044e1838a555f45b88f465503c1fc
    extra : histedit_source : 347fb57ff58138b8c949b8a2efb26b17e0a8c47d

--- dom/media/webrtc/MediaEngineDefault.cpp
+++ dom/media/webrtc/MediaEngineDefault.cpp
@@ -415,7 +415,7 @@ MediaEngineDefaultAudioSource::Start(SourceMediaStream* aStream, TrackID aID,
 
   // AddTrack will take ownership of segment
   AudioSegment* segment = new AudioSegment();
-  aStream->AddAudioTrack(aID, mRate, 0, segment, SourceMediaStream::ADDTRACK_QUEUED);
+  aStream->AddAudioTrack(aID, aStream->GraphRate(), 0, segment, SourceMediaStream::ADDTRACK_QUEUED);
 
   // Remember TrackID so we can finish later
   mTrackID = aID;
@@ -471,7 +471,7 @@ MediaEngineDefaultAudioSource::NotifyPull(MediaStreamGraph* aGraph,
   MOZ_ASSERT(aID == mTrackID);
   AudioSegment segment;
   // avoid accumulating rounding errors
-  TrackTicks desired = aSource->TimeToTicksRoundUp(mRate, aDesiredTime);
+  TrackTicks desired = aSource->TimeToTicksRoundUp(aGraph->GraphRate(), aDesiredTime);
   TrackTicks delta = desired - mLastNotify;
   mLastNotify += delta;
   AppendToSegment(segment, delta, aPrincipalHandle);
