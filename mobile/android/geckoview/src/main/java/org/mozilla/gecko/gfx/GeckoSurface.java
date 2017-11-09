5c5845d2a8ceb9b11a99a2fd1bc7b4c98507f97e
Nick Alexander <nalexander@mozilla.com>
2017-11-09 09:44:40 -0800

    Bug 1415778 - Part 3: Fix up Gradle. r=jchen
    This patch makes the :geckoview Gradle project only use
    o.m.geckoview.BuildConfig, and makes the :app Gradle project use all
    of the preprocessed code coming from the moz.build system.
    Eventually, we'll reduce that set of preprocessed code to only
    o.m.gecko.BuildConfig, which will then be produced by Gradle.
    
    MozReview-Commit-ID: Dnkde7axyZL

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurface.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurface.java
@@ -7,7 +7,6 @@ package org.mozilla.gecko.gfx;
 
 import android.graphics.SurfaceTexture;
 
-import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.view.Surface;
