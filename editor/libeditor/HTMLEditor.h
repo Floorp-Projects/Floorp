5e618b49798d3c592bf611d61712c8c546dc1613
Masayuki Nakano <masayuki@d-toybox.com>
2017-11-09 23:31:59 +0900

    Bug 1415800 - part 3: Redesign HTMLEditor::GetPriorHTMLNode() as similar to EditorBase::GetPrevious*() r=m_kato
    HTMLEditor::GetPriorHTMLNode() methods are similar to EditorBase::GetPriorNode()
    which was redesigned with the previous patch.
    
    So, it should be redesigned as
    HTMLEditor::GetPreviousEditableHTMLNode(nsINode&),
    HTMLEditor::GetPreviousEditableHTMLNode(const EditorRawDOMPoint&),
    HTMLEditor::GetPreviousEditableHTMLNodeInBlock(nsINode&) and
    HTMLEditor::GetPreviousEditableHTMLNodeInBlock(const EditorRawDOMPoint&).
    
    Note that HTMLEditor::GetPriorHTMLNode() are always return editable node.
    So, we don't need to create non-editable node methods for them.
    
    Although, I don't like the word "HTMLNode" because this can return SVG element
    or something too.  The additional feature of those methods is just checking
    given node is in active editing host.  So, they are for HTML editor, but not
    returning only HTML nodes.  However, I have no better idea with shorter name.
    
    MozReview-Commit-ID: 3J4IaBOFjzj

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -777,10 +777,38 @@ protected:
 
   nsIContent* GetNextHTMLSibling(nsINode* aNode);
 
-  nsIContent* GetPriorHTMLNode(nsINode* aNode, bool aNoBlockCrossing = false);
-  nsIContent* GetPriorHTMLNode(nsINode* aParent, int32_t aOffset,
-                               nsINode* aChildAtOffset,
-                               bool aNoBlockCrossing = false);
+  /**
+   * GetPreviousEditableHTMLNode*() methods are similar to
+   * EditorBase::GetPreviousEditableNode() but this won't return nodes outside
+   * active editing host.
+   */
+  nsIContent* GetPreviousEditableHTMLNode(nsINode& aNode)
+  {
+    return GetPreviousEditableHTMLNodeInternal(aNode, false);
+  }
+  nsIContent* GetPreviousEditableHTMLNodeInBlock(nsINode& aNode)
+  {
+    return GetPreviousEditableHTMLNodeInternal(aNode, true);
+  }
+  nsIContent* GetPreviousEditableHTMLNode(const EditorRawDOMPoint& aPoint)
+  {
+    return GetPreviousEditableHTMLNodeInternal(aPoint, false);
+  }
+  nsIContent* GetPreviousEditableHTMLNodeInBlock(
+                const EditorRawDOMPoint& aPoint)
+  {
+    return GetPreviousEditableHTMLNodeInternal(aPoint, true);
+  }
+
+  /**
+   * GetPreviousEditableHTMLNodeInternal() methods are common implementation
+   * of above methods.  Please don't use this method directly.
+   */
+  nsIContent* GetPreviousEditableHTMLNodeInternal(nsINode& aNode,
+                                                  bool aNoBlockCrossing);
+  nsIContent* GetPreviousEditableHTMLNodeInternal(
+                const EditorRawDOMPoint& aPoint,
+                bool aNoBlockCrossing);
 
   nsIContent* GetNextHTMLNode(nsINode* aNode, bool aNoBlockCrossing = false);
   nsIContent* GetNextHTMLNode(nsINode* aParent, int32_t aOffset,
