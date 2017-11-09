0a3d82976427311de63f03876137df24a4792b90
Mike Shal <mshal@mozilla.com>
2017-11-08 19:58:28 -0500

    Bug 1407432 - Remove unused builtins arg from Sandbox; r=froydnj
    MozReview-Commit-ID: JVukxIg04AT

--- python/mozbuild/mozbuild/frontend/sandbox.py
+++ python/mozbuild/mozbuild/frontend/sandbox.py
@@ -113,10 +113,10 @@ class Sandbox(dict):
         'set': set,
     })
 
-    def __init__(self, context, builtins=None, finder=default_finder):
+    def __init__(self, context, finder=default_finder):
         """Initialize a Sandbox ready for execution.
         """
-        self._builtins = builtins or self.BUILTINS
+        self._builtins = self.BUILTINS
         dict.__setitem__(self, '__builtins__', self._builtins)
 
         assert isinstance(self._builtins, ReadOnlyDict)
