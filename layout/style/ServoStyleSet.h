6147cf52098cc7c24510223c45d366a289b57078
Emilio Cobos Álvarez <emilio@crisal.io>
2017-11-09 18:29:36 +0100

    Bug 1414999: Synchronously clean style data from the DOM tree when the shell goes away. r=bz
    There's nothing preventing the flat tree from changing while the document
    doesn't have a shell. In that case, we really really don't want to lose track
    of elements with stale style data, since then we'll mess up.
    
    It's ok to _not_ clear the style data when the document goes into the BFCache
    though, because the document is thrown away if other document runs script and
    touches the cached DOM.
    
    MozReview-Commit-ID: 4W3xDAnnLPL

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -597,8 +597,7 @@ private:
     ResolveStyleLazilyInternal(dom::Element* aElement,
                                CSSPseudoElementType aPseudoType,
                                StyleRuleInclusion aRules =
-                                 StyleRuleInclusion::All,
-                               bool aIgnoreExistingStyles = false);
+                                 StyleRuleInclusion::All);
 
   void RunPostTraversalTasks();
 
