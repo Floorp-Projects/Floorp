af2d7335b8e342c43388909e48638822ae555197
Nicholas Nethercote <nnethercote@mozilla.com>
2017-11-10 15:21:27 +1100

    Bug 1416038 (part 7) - Remove nsISAXMutableAttributes. r=erahm
    There is a single implementation, nsSAXAttributes, and all the methods are
    unused... except for AddAttribute(), but that doesn't need to be declared in
    XPIDL because it's only used within nsSAXAttributes.cpp.
    
    --HG--
    extra : rebase_source : 9bf10f76be0f9e6821e35885b96d125f76209c9b

--- parser/xml/nsSAXAttributes.cpp
+++ parser/xml/nsSAXAttributes.cpp
@@ -5,7 +5,7 @@
 
 #include "nsSAXAttributes.h"
 
-NS_IMPL_ISUPPORTS(nsSAXAttributes, nsISAXAttributes, nsISAXMutableAttributes)
+NS_IMPL_ISUPPORTS(nsSAXAttributes, nsISAXAttributes)
 
 NS_IMETHODIMP
 nsSAXAttributes::GetIndexFromName(const nsAString &aURI,
@@ -181,7 +181,7 @@ nsSAXAttributes::GetValueFromQName(const nsAString &aQName,
   return NS_OK;
 }
 
-NS_IMETHODIMP
+nsresult
 nsSAXAttributes::AddAttribute(const nsAString &aURI,
                               const nsAString &aLocalName,
                               const nsAString &aQName,
@@ -192,7 +192,7 @@ nsSAXAttributes::AddAttribute(const nsAString &aURI,
   if (!att) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
-  
+
   att->uri = aURI;
   att->localName = aLocalName;
   att->qName = aQName;
@@ -202,131 +202,3 @@ nsSAXAttributes::AddAttribute(const nsAString &aURI,
   return NS_OK;
 }
 
-NS_IMETHODIMP
-nsSAXAttributes::Clear()
-{
-  mAttrs.Clear();
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::RemoveAttribute(uint32_t aIndex)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs.RemoveElementAt(aIndex);
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetAttributes(nsISAXAttributes *aAttributes)
-{
-  NS_ENSURE_ARG(aAttributes);
-
-  nsresult rv;
-  int32_t len;
-  rv = aAttributes->GetLength(&len);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  mAttrs.Clear();
-  SAXAttr *att;
-  int32_t i;
-  for (i = 0; i < len; ++i) {
-    att = mAttrs.AppendElement();
-    if (!att) {
-      return NS_ERROR_OUT_OF_MEMORY;
-    }
-    rv = aAttributes->GetURI(i, att->uri);
-    NS_ENSURE_SUCCESS(rv, rv);
-    rv = aAttributes->GetLocalName(i, att->localName);
-    NS_ENSURE_SUCCESS(rv, rv);
-    rv = aAttributes->GetQName(i, att->qName);
-    NS_ENSURE_SUCCESS(rv, rv);
-    rv = aAttributes->GetType(i, att->type);
-    NS_ENSURE_SUCCESS(rv, rv);
-    rv = aAttributes->GetValue(i, att->value);
-    NS_ENSURE_SUCCESS(rv, rv);
-  }
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetAttribute(uint32_t aIndex,
-                              const nsAString &aURI,
-                              const nsAString &aLocalName,
-                              const nsAString &aQName,
-                              const nsAString &aType,
-                              const nsAString &aValue)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-
-  SAXAttr &att = mAttrs[aIndex];
-  att.uri = aURI;
-  att.localName = aLocalName;
-  att.qName = aQName;
-  att.type = aType;
-  att.value = aValue;
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetLocalName(uint32_t aIndex, const nsAString &aLocalName)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs[aIndex].localName = aLocalName;
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetQName(uint32_t aIndex, const nsAString &aQName)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs[aIndex].qName = aQName;
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetType(uint32_t aIndex, const nsAString &aType)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs[aIndex].type = aType;
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetURI(uint32_t aIndex, const nsAString &aURI)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs[aIndex].uri = aURI;
-
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsSAXAttributes::SetValue(uint32_t aIndex, const nsAString &aValue)
-{
-  if (aIndex >= mAttrs.Length()) {
-    return NS_ERROR_FAILURE;
-  }
-  mAttrs[aIndex].value = aValue;
-
-  return NS_OK;
-}
