77dd49f0f49919b243d57a9a2c625e75d0c35bba
Masayuki Nakano <masayuki@d-toybox.com>
2017-11-10 14:43:13 +0900

    Bug 1416099 - part 2: Rename |sibling| in HTMLEditRules::ReturnInParagraph() to |brNode| r=m_kato
    The local variable |sibling| in HTMLEditRules::ReturnInParagraph() is set to
    aBRNode of SplitParagraph().  So, the name should be renamed to |brNode|.
    
    Then, the |brNode| should be cleared if it's not a <br> node actually.  However,
    SplitParagraph() doesn't allow aBRNode to be nullptr.  aBRNode is only used to
    remove it from the document if it's not necessary.  So, it should be able to be
    nullptr.  Therefore, this patch changes SplitParagraph() too.
    
    Note that SplitParagrah() is called only by ReturnInParagraph().  So, we don't
    need to worry about other callers.
    
    MozReview-Commit-ID: 7Ynk9m5F8Mi
    
    --HG--
    extra : rebase_source : e2583c70ad274fe74f48df687796ed71a66bdf98

--- editor/libeditor/HTMLEditRules.h
+++ editor/libeditor/HTMLEditRules.h
@@ -297,11 +297,29 @@ protected:
   EditActionResult ReturnInParagraph(Selection* aSelection, nsINode* aHeader,
                                      nsINode* aTextNode, int32_t aOffset,
                                      nsIContent* aChildAtOffset);
+
+  /**
+   * SplitParagraph() splits the parent block, aPara, at aSelNode - aOffset.
+   *
+   * @param aPara       The parent block to be split.
+   * @param aBRNode     Next <br> node if there is.  Otherwise, nullptr.
+   *                    If this is not nullptr, the <br> node may be removed.
+   * @param aSelection  The selection.
+   * @param aSelNode    Set the selection container to split aPara at.
+   *                    Actual container node will be set by this method.
+   *                    XXX: The only caller ReturnInParagraph() doesn't need
+   *                         this result.
+   * @param aOffset     Set the offset in the container.
+   *                    Actual offset will be set by this method.
+   *                    XXX: The only caller ReturnInParagraph() doesn't need
+   *                         this result.
+   */
   nsresult SplitParagraph(nsIDOMNode* aPara,
                           nsIContent* aBRNode,
                           Selection* aSelection,
                           nsCOMPtr<nsIDOMNode>* aSelNode,
                           int32_t* aOffset);
+
   nsresult ReturnInListItem(Selection& aSelection, Element& aHeader,
                             nsINode& aNode, int32_t aOffset);
   nsresult AfterEditInner(EditAction action,
