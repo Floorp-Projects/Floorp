80f18ce79bacb5271335974e532e82ec0b0a0c16
Jan de Mooij <jdemooij@mozilla.com>
2017-11-10 11:52:22 +0100

    Bug 1414340 part 1 - Remove non-standard array/generator comprehensions from browser code. r=mossop

--- dom/manifest/test/browser_ManifestObtainer_obtain.js
+++ dom/manifest/test/browser_ManifestObtainer_obtain.js
@@ -152,18 +152,21 @@ add_task(async function() {
     `http://www.example.com:80${defaultPath}`,
   ];
   // Open tabs an collect corresponding browsers
-  let browsers = [
-    for (url of tabURLs) BrowserTestUtils.addTab(gBrowser, url).linkedBrowser
-  ];
+  let browsers = tabURLs.map(url => BrowserTestUtils.addTab(gBrowser, url).linkedBrowser);
+
   // Once all the pages have loaded, run a bunch of tests in "parallel".
-  await Promise.all((
-    for (browser of browsers) BrowserTestUtils.browserLoaded(browser)
-  ));
+  await Promise.all((function*() {
+    for (let browser of browsers) {
+      yield BrowserTestUtils.browserLoaded(browser);
+    }
+  })());
   // Flood random browsers with requests. Once promises settle, check that
   // responses all pass.
-  const results = await Promise.all((
-    for (browser of randBrowsers(browsers, 50)) ManifestObtainer.browserObtainManifest(browser)
-  ));
+  const results = await Promise.all((function*() {
+    for (let browser of randBrowsers(browsers, 50)) {
+      yield ManifestObtainer.browserObtainManifest(browser);
+    }
+  })());
   const pass = results.every(manifest => manifest.name === 'pass');
   ok(pass, 'Expect every manifest to have name equal to `pass`.');
   //cleanup
