8f9cdfca8613a1d05c3dc44eafe6eee249e31201
Daisuke Akatsuka <dakatsuka@mozilla.com>
2017-11-13 16:41:04 +0900

    Bug 1406287 - Part 1: Implement basic layout. r=gl
    MozReview-Commit-ID: KAe8uupfQ3z

---
+++ devtools/client/inspector/animation/components/AnimationListContainer.js
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { createFactory, DOM: dom, PropTypes, PureComponent } =
+  require("devtools/client/shared/vendor/react");
+
+const AnimationList = createFactory(require("./AnimationList"));
+const AnimationListHeader = createFactory(require("./AnimationListHeader"));
+
+class AnimationListContainer extends PureComponent {
+  static get propTypes() {
+    return {
+      animations: PropTypes.arrayOf(PropTypes.object).isRequired,
+    };
+  }
+
+  render() {
+    const { animations } = this.props;
+
+    return dom.div(
+      {
+        className: "animation-list-container"
+      },
+      AnimationListHeader(),
+      AnimationList(
+        {
+          animations
+        }
+      )
+    );
+  }
+}
+
+module.exports = AnimationListContainer;
