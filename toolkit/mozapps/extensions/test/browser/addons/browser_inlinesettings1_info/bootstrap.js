15e00a365e0b75809dc71ef11e773abef52783d0
Dave Townsend <dtownsend@oxymoronical.com>
2017-11-13 15:09:00 -0800

    Bug 1414406: Remove the inline options feature for add-ons. r=aswan
    MozReview-Commit-ID: 4ycTaMzqWgx

--- toolkit/mozapps/extensions/test/browser/addons/browser_inlinesettings1_info/bootstrap.js
+++
@@ -1,9 +0,0 @@
-/* exported startup, shutdown, install, uninstall */
-function install(params, aReason) {
-}
-function uninstall(params, aReason) {
-}
-function startup(params, aReason) {
-}
-function shutdown(params, aReason) {
-}
