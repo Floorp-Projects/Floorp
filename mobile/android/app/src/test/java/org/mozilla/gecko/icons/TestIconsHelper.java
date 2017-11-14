3aa313d779919dc21e1a53adf87ff8ed90c4851c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2017-11-14 23:41:37 +0000

    Bug 1408044 - stop using/overriding about: in Fennec and use about:fennec/firefox instead, r=nalexander
    MozReview-Commit-ID: 3LQjMJZOuqc

--- mobile/android/app/src/test/java/org/mozilla/gecko/icons/TestIconsHelper.java
+++ mobile/android/app/src/test/java/org/mozilla/gecko/icons/TestIconsHelper.java
@@ -33,8 +33,8 @@ public class TestIconsHelper {
                 IconsHelper.guessDefaultFaviconURL("about:home"));
 
         Assert.assertEquals(
-                "about:",
-                IconsHelper.guessDefaultFaviconURL("about:"));
+                "about:firefox",
+                IconsHelper.guessDefaultFaviconURL("about:firefox"));
 
         Assert.assertEquals(
                 "about:addons",
