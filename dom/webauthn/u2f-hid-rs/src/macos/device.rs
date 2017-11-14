679af2576cde15d98d41c4626e3481044650fa6e
Tim Taubert <ttaubert@mozilla.com>
2017-11-14 11:39:29 +0100

    Bug 1398268 - [u2f-hid-rs] Rewrite macOS IOHIDManager communication and state machine r=jcj
    Review: https://github.com/jcjones/u2f-hid-rs/pull/52

--- dom/webauthn/u2f-hid-rs/src/macos/device.rs
+++ dom/webauthn/u2f-hid-rs/src/macos/device.rs
@@ -2,23 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-extern crate libc;
 extern crate log;
 
-use std::fmt;
-use std::io;
-use std::io::{Read, Write};
-use std::slice;
-use std::sync::mpsc::{channel, Sender, Receiver, RecvTimeoutError};
-use std::time::Duration;
-
-use core_foundation_sys::base::*;
-use libc::c_void;
-
 use consts::{CID_BROADCAST, HID_RPT_SIZE};
+use core_foundation_sys::base::*;
+use platform::iokit::*;
+use std::{fmt, io};
+use std::io::{Read, Write};
+use std::sync::mpsc::{Receiver, RecvTimeoutError};
+use std::time::Duration;
 use u2ftypes::U2FDevice;
-
-use super::iokit::*;
 
 const READ_TIMEOUT: u64 = 15;
 
@@ -26,46 +19,14 @@ pub struct Device {
     device_ref: IOHIDDeviceRef,
     cid: [u8; 4],
     report_rx: Receiver<Vec<u8>>,
-    report_send_void: *mut c_void,
-    scratch_buf_ptr: *mut u8,
 }
 
 impl Device {
-    pub fn new(device_ref: IOHIDDeviceRef) -> Self {
-        let (report_tx, report_rx) = channel();
-        let report_send_void = Box::into_raw(Box::new(report_tx)) as *mut c_void;
-
-        let scratch_buf = [0; HID_RPT_SIZE];
-        let scratch_buf_ptr = Box::into_raw(Box::new(scratch_buf)) as *mut u8;
-
-        unsafe {
-            IOHIDDeviceRegisterInputReportCallback(
-                device_ref,
-                scratch_buf_ptr,
-                HID_RPT_SIZE as CFIndex,
-                read_new_data_cb,
-                report_send_void,
-            );
-        }
-
+    pub fn new(device_ref: IOHIDDeviceRef, report_rx: Receiver<Vec<u8>>) -> Self {
         Self {
             device_ref,
             cid: CID_BROADCAST,
             report_rx,
-            report_send_void,
-            scratch_buf_ptr,
-        }
-    }
-}
-
-impl Drop for Device {
-    fn drop(&mut self) {
-        debug!("Dropping U2F device {}", self);
-
-        unsafe {
-            // Re-allocate raw pointers for destruction.
-            let _ = Box::from_raw(self.report_send_void as *mut Sender<Vec<u8>>);
-            let _ = Box::from_raw(self.scratch_buf_ptr as *mut [u8; HID_RPT_SIZE]);
         }
     }
 }
@@ -146,47 +107,5 @@ impl U2FDevice for Device {
 
     fn set_cid(&mut self, cid: [u8; 4]) {
         self.cid = cid;
-    }
-}
-
-// This is called from the RunLoop thread
-extern "C" fn read_new_data_cb(
-    context: *mut c_void,
-    _: IOReturn,
-    _: *mut c_void,
-    report_type: IOHIDReportType,
-    report_id: u32,
-    report: *mut u8,
-    report_len: CFIndex,
-) {
-    unsafe {
-        let tx = &mut *(context as *mut Sender<Vec<u8>>);
-
-        trace!(
-            "read_new_data_cb type={} id={} report={:?} len={}",
-            report_type,
-            report_id,
-            report,
-            report_len
-        );
-
-        let report_len = report_len as usize;
-        if report_len > HID_RPT_SIZE {
-            warn!(
-                "read_new_data_cb got too much data! {} > {}",
-                report_len,
-                HID_RPT_SIZE
-            );
-            return;
-        }
-
-        let data = slice::from_raw_parts(report, report_len).to_vec();
-
-        if let Err(e) = tx.send(data) {
-            // TOOD: This happens when the channel closes before this thread
-            // does. This is pretty common, but let's deal with stopping
-            // properly later.
-            warn!("Problem returning read_new_data_cb data for thread: {}", e);
-        };
     }
 }
