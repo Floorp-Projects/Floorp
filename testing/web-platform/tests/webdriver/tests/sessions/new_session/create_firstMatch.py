49b190ad8c02ac1ff14bead8a7f6ac3b085ed7a5
James Graham <james@hoppipolla.co.uk>
2017-11-16 13:35:10 +0000

    Bug 1419296 - Update web-platform-tests to fa27b785c62e67cc3a12201612db188c4fdd49ad, a=testonly
    MozReview-Commit-ID: 4P631tX3vv4

--- testing/web-platform/tests/webdriver/tests/sessions/new_session/create_firstMatch.py
+++ testing/web-platform/tests/webdriver/tests/sessions/new_session/create_firstMatch.py
@@ -8,5 +8,5 @@ from support.create import valid_data
 
 
 @pytest.mark.parametrize("key,value", flatten(product(*item) for item in valid_data))
-def test_valid(new_session, key, value):
-    resp = new_session({"capabilities": {"firstMatch": [{key: value}]}})
+def test_valid(new_session, add_browser_capabilites, key, value):
+    resp = new_session({"capabilities": {"firstMatch": [add_browser_capabilites({key: value})]}})
