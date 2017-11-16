2dae86e2ff0273a2336989ebda0c3ce79d7d5b82
Tim Nguyen <ntim.bugs@gmail.com>
2017-11-16 12:40:53 +0000

    Bug 1415878 - Accept both Chrome arrays and CSS colors for all properties. r=jaws
    MozReview-Commit-ID: 6NkIUZKWkBI

--- toolkit/components/extensions/test/browser/browser_ext_themes_tab_text.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_tab_text.js
@@ -9,7 +9,7 @@ add_task(async function setup() {
   });
 });
 
-add_task(async function test_support_toolbar_property() {
+add_task(async function test_support_tab_text_property_css_color() {
   const TAB_TEXT_COLOR = "#9400ff";
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
@@ -35,6 +35,36 @@ add_task(async function test_support_toolbar_property() {
   let selectedTab = document.querySelector(".tabbrowser-tab[selected]");
   Assert.equal(window.getComputedStyle(selectedTab).color,
     "rgb(" + hexToRGB(TAB_TEXT_COLOR).join(", ") + ")", "Selected tab text color should be set.");
+
+  await extension.unload();
+});
+
+add_task(async function test_support_tab_text_chrome_array() {
+  const TAB_TEXT_COLOR = [148, 0, 255];
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "theme": {
+        "images": {
+          "headerURL": "image1.png",
+        },
+        "colors": {
+          "accentcolor": FRAME_COLOR,
+          "textcolor": BACKGROUND_TAB_TEXT_COLOR,
+          "tab_text": TAB_TEXT_COLOR,
+        },
+      },
+    },
+    files: {
+      "image1.png": BACKGROUND,
+    },
+  });
+
+  await extension.startup();
+
+  info("Checking selected tab colors");
+  let selectedTab = document.querySelector(".tabbrowser-tab[selected]");
+  Assert.equal(window.getComputedStyle(selectedTab).color,
+    "rgb(" + TAB_TEXT_COLOR.join(", ") + ")", "Selected tab text color should be set.");
 
   await extension.unload();
 });
