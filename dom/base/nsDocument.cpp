14eb0311b4e38a7e535531b7da33a4645cd646d2
Boris Zbarsky <bzbarsky@mit.edu>
2017-11-17 01:10:01 -0500

    Bug 1417405 - Make sure to hold a strong ref on the stack to arguments passed to IsPotentiallyScrollable. r=smaug
    --HG--
    extra : rebase_source : abbd4b46d6c813b14aa767b1aab7b20567792a6b

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -11034,7 +11034,7 @@ nsIDocument::GetScrollingElement()
 {
   // Keep this in sync with IsScrollingElement.
   if (GetCompatibilityMode() == eCompatibility_NavQuirks) {
-    HTMLBodyElement* body = GetBodyElement();
+    RefPtr<HTMLBodyElement> body = GetBodyElement();
     if (body && !IsPotentiallyScrollable(body)) {
       return body;
     }
@@ -11055,6 +11055,7 @@ nsIDocument::IsScrollingElement(Element* aElement)
     return aElement == GetRootElement();
   }
 
+  // In the common case when aElement != body, avoid refcounting.
   HTMLBodyElement* body = GetBodyElement();
   if (aElement != body) {
     return false;
@@ -11063,7 +11064,8 @@ nsIDocument::IsScrollingElement(Element* aElement)
   // Now we know body is non-null, since aElement is not null.  It's the
   // scrolling element for the document if it itself is not potentially
   // scrollable.
-  return !IsPotentiallyScrollable(body);
+  RefPtr<HTMLBodyElement> strongBody(body);
+  return !IsPotentiallyScrollable(strongBody);
 }
 
 void
