e2b28062285797d7e1a42ca19e1c53b235a4de0c
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2017-11-16 21:58:29 -0800

    Bug 1417749 - Create a currency-amount Custom Element. r=marcosc
    MozReview-Commit-ID: 1XF7UqvhQSy

---
+++ toolkit/components/payments/res/components/currency-amount.js
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * <currency-amount value="7.5" currency="USD"></currency-amount>
+ */
+
+/* global ObservedPropertiesMixin */
+
+class CurrencyAmount extends ObservedPropertiesMixin(HTMLElement) {
+  static get observedAttributes() {
+    return ["currency", "value"];
+  }
+
+  render() {
+    let output = "";
+    try {
+      if (this.value && this.currency) {
+        let number = Number.parseFloat(this.value);
+        if (Number.isNaN(number) || !Number.isFinite(number)) {
+          throw new RangeError("currency-amount value must be a finite number");
+        }
+        const formatter = new Intl.NumberFormat(navigator.languages, {
+          style: "currency",
+          currency: this.currency,
+          currencyDisplay: "symbol",
+        });
+        output = formatter.format(this.value);
+      }
+    } finally {
+      this.textContent = output;
+    }
+  }
+}
+
+customElements.define("currency-amount", CurrencyAmount);
