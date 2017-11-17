6f02a7551b97a2aeb6c4f78aa68e80afca4c38a4
Andreea Pavel <apavel@mozilla.com>
2017-11-17 10:13:38 +0200

    Backed out 5 changesets (bug 1366083, bug 1410528, bug 1417267) for failing Spidermonkey jstests and jit-tests r=backout on a CLOSED TREE
    Backed out changeset a83b373d2d2e (bug 1417267)
    Backed out changeset 3059e975eef8 (bug 1366083)
    Backed out changeset 94d20ed3c062 (bug 1410528)
    Backed out changeset 5e1dc7fec0ff (bug 1410528)
    Backed out changeset ec0702d55b01 (bug 1410528)

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -7169,13 +7169,6 @@ typedef bool
 extern JS_PUBLIC_API(bool)
 SetGetPerformanceGroupsCallback(JSContext*, GetGroupsCallback, void*);
 
-/**
- * Hint that we expect a crash. Currently, the only thing that cares is the
- * breakpad injector, which (if loaded) will suppress minidump generation.
- */
-extern JS_PUBLIC_API(void)
-NoteIntentionalCrash();
-
 } /* namespace js */
 
 namespace js {
