c351c5f556c703a54c3fbc80625995c691fc734e
Michael Ratcliffe <mratcliffe@mozilla.com>
2017-11-17 13:59:12 +0000

    Bug 1418320 - JSON Viewer: Split HeadersToolbar, JsonToolbar and TextToolbar out to their own files r=Honza
    MozReview-Commit-ID: KdGLYV4rHB9
    
    --HG--
    extra : rebase_source : b0a9e75236d2b840dc51df00097b364bf125bb6f

---
+++ devtools/client/jsonview/components/TextToolbar.js
@@ -0,0 +1,75 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+ "use strict";
+
+ define(function (require, exports, module) {
+   const { Component } = require("devtools/client/shared/vendor/react");
+   const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+   const { createFactories } = require("devtools/client/shared/react-utils");
+   const { Toolbar, ToolbarButton } = createFactories(require("./reps/Toolbar"));
+
+   /**
+    * This object represents a toolbar displayed within the
+    * 'Raw Data' panel.
+    */
+   class TextToolbar extends Component {
+     static get propTypes() {
+       return {
+         actions: PropTypes.object,
+         isValidJson: PropTypes.bool
+       };
+     }
+
+     constructor(props) {
+       super(props);
+       this.onPrettify = this.onPrettify.bind(this);
+       this.onSave = this.onSave.bind(this);
+       this.onCopy = this.onCopy.bind(this);
+     }
+
+     // Commands
+
+     onPrettify(event) {
+       this.props.actions.onPrettify();
+     }
+
+     onSave(event) {
+       this.props.actions.onSaveJson();
+     }
+
+     onCopy(event) {
+       this.props.actions.onCopyJson();
+     }
+
+     render() {
+       return (
+         Toolbar({},
+           ToolbarButton({
+             className: "btn save",
+             onClick: this.onSave},
+             JSONView.Locale.$STR("jsonViewer.Save")
+           ),
+           ToolbarButton({
+             className: "btn copy",
+             onClick: this.onCopy},
+             JSONView.Locale.$STR("jsonViewer.Copy")
+           ),
+           this.props.isValidJson ?
+             ToolbarButton({
+               className: "btn prettyprint",
+               onClick: this.onPrettify},
+               JSONView.Locale.$STR("jsonViewer.PrettyPrint")
+             ) :
+             null
+         )
+       );
+     }
+   }
+
+   // Exports from this module
+   exports.TextToolbar = TextToolbar;
+ });
