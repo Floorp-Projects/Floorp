9474960c5228aa64901bf981b60c1deb85f22792
Andreea Pavel <apavel@mozilla.com>
2017-11-20 01:36:36 +0200

    Backed out 1 changesets (bug 1370040) for build bustage at /cocoa/VibrancyManager.mm:207 r=backout on a CLOSED TREE
    Backed out changeset 84c737eb3c6e (bug 1370040)

--- widget/cocoa/VibrancyManager.h
+++ widget/cocoa/VibrancyManager.h
@@ -100,20 +100,9 @@ public:
    */
   static bool SystemSupportsVibrancy();
 
-  /**
-   * Create an NSVisualEffectView for the specified vibrancy type. The return
-   * value is not autoreleased. We return an object of type NSView* because we
-   * compile with an SDK that does not contain a definition for
-   * NSVisualEffectView.
-   * @param aIsContainer Whether this NSView will have child views. This value
-   *                     affects hit testing: Container views will pass through
-   *                     hit testing requests to their children, and leaf views
-   *                     will be transparent to hit testing.
-   */
-  static NSView* CreateEffectView(VibrancyType aType, BOOL aIsContainer = NO);
-
 protected:
   void ClearVibrantRegion(const LayoutDeviceIntRegion& aVibrantRegion) const;
+  NSView* CreateEffectView(VibrancyType aType);
 
   const nsChildView& mCoordinateConverter;
   NSView* mContainerView;
