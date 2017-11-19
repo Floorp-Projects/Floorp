687326618c4dcceec34fba909d1b2ee3c49db914
Olli Pettay <Olli.Pettay@helsinki.fi>
2017-11-19 16:22:54 +0200

    Bug 1416450 - Ensure IntersectionObservers are deleted when adopting elements. r=mrbkap, a=gchang
    --HG--
    extra : source : ef82504a27826468cf9885b22380acefac277bda

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -4306,6 +4306,20 @@ enum nsPreviousIntersectionThreshold {
   eNonIntersecting = -1
 };
 
+static void
+IntersectionObserverPropertyDtor(void* aObject, nsAtom* aPropertyName,
+                                 void* aPropertyValue, void* aData)
+{
+  Element* element = static_cast<Element*>(aObject);
+  IntersectionObserverList* observers =
+    static_cast<IntersectionObserverList*>(aPropertyValue);
+  for (auto iter = observers->Iter(); !iter.Done(); iter.Next()) {
+    DOMIntersectionObserver* observer = iter.Key();
+    observer->UnlinkElement(*element);
+  }
+  delete observers;
+}
+
 void
 Element::RegisterIntersectionObserver(DOMIntersectionObserver* aObserver)
 {
@@ -4318,7 +4332,7 @@ Element::RegisterIntersectionObserver(DOMIntersectionObserver* aObserver)
     observers = new IntersectionObserverList();
     observers->Put(aObserver, eUninitialized);
     SetProperty(nsGkAtoms::intersectionobserverlist, observers,
-                nsINode::DeleteProperty<IntersectionObserverList>);
+                IntersectionObserverPropertyDtor, true);
     return;
   }
 
