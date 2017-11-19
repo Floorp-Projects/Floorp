11db6025dc5d1370e3395dd534185b4f80238603
Markus Stange <mstange@themasta.com>
2017-11-19 17:01:02 -0500

    Bug 1370040 - Use the public API -[NSVisualEffectView setMaskImage:] instead of the private API -[NSWindow _cornerMask]. r=spohl
    The idea is the following:
    Behind-window vibrancy is mostly rendered by the window server. For a given
    vibrant region of a window, the window server renders a vibrancy "backdrop",
    which is a blurred version of everything that's behind that region, modified
    with a color tint and blended in some way. Then it puts our actual window
    contents on top of that background.
    The backdrop's shape is usually a rectangle. If we don't want it to be a
    rectangle, we need to tell the window server about the shape that we want it to
    be. We can't just "draw" a different shape in our own rendering, because our
    own rendering is merely placed on top of the backdrop - but here we want to
    modify the shape of the backdrop itself.
    
    NSVisualEffectView lets us set a mask image on the view. If this view is the
    content view of a window, then the view will automatically communicate the mask
    image to the window server.
    
    Traditionally, our popup windows have had a ChildView as their content view. If
    we now want an NSVisualEffectView to be the content view of the window, then we
    need to nest the ChildView inside that NSVisualEffectView.
    But this NSVisualEffectView is only needed when the window is vibrant and the
    vibrancy backdrop needs to have a certain shape. This is the case for our menus
    which need to have rounded corners. If the window transitions to being
    non-vibrant, or not needing a special shape, then we can go back to the way our
    window's NSView hierarchy has worked traditionally. So we need to reparent
    NSViews during those transitions.
    
    MozReview-Commit-ID: Bo2VzjhhR0A

--- widget/cocoa/VibrancyManager.mm
+++ widget/cocoa/VibrancyManager.mm
@@ -101,7 +101,7 @@ AllowsVibrancyYes(id self, SEL _cmd)
 }
 
 static Class
-CreateEffectViewClass(BOOL aForegroundVibrancy)
+CreateEffectViewClass(BOOL aForegroundVibrancy, BOOL aIsContainer)
 {
   // Create a class called EffectView that inherits from NSVisualEffectView
   // and overrides the methods -[NSVisualEffectView drawRect:] and
@@ -112,8 +112,10 @@ CreateEffectViewClass(BOOL aForegroundVibrancy)
   Class EffectViewClass = objc_allocateClassPair(NSVisualEffectViewClass, className, 0);
   class_addMethod(EffectViewClass, @selector(drawRect:), (IMP)DrawRectNothing,
                   "v@:{CGRect={CGPoint=dd}{CGSize=dd}}");
-  class_addMethod(EffectViewClass, @selector(hitTest:), (IMP)HitTestNil,
-                  "@@:{CGPoint=dd}");
+  if (!aIsContainer) {
+    class_addMethod(EffectViewClass, @selector(hitTest:), (IMP)HitTestNil,
+                    "@@:{CGPoint=dd}");
+  }
   if (aForegroundVibrancy) {
     // Also override the -[NSView allowsVibrancy] method to return YES.
     class_addMethod(EffectViewClass, @selector(allowsVibrancy), (IMP)AllowsVibrancyYes, "I@:");
@@ -201,14 +203,15 @@ enum {
 - (void)setEmphasized:(BOOL)emphasized;
 @end
 
-NSView*
-VibrancyManager::CreateEffectView(VibrancyType aType)
+/* static */ NSView*
+VibrancyManager::CreateEffectView(VibrancyType aType, bool aIsContainer)
 {
-  static Class EffectViewClassWithoutForegroundVibrancy = CreateEffectViewClass(NO);
-  static Class EffectViewClassWithForegroundVibrancy = CreateEffectViewClass(YES);
+  static Class EffectViewClasses[2][2] = {
+    { CreateEffectViewClass(NO, NO), CreateEffectViewClass(NO, YES) },
+    { CreateEffectViewClass(YES, NO), CreateEffectViewClass(YES, YES) }
+  };
 
-  Class EffectViewClass = HasVibrantForeground(aType)
-    ? EffectViewClassWithForegroundVibrancy : EffectViewClassWithoutForegroundVibrancy;
+  Class EffectViewClass = EffectViewClasses[HasVibrantForeground(aType)][aIsContainer];
   NSView* effectView = [[EffectViewClass alloc] initWithFrame:NSZeroRect];
   [effectView performSelector:@selector(setAppearance:)
                    withObject:AppearanceForVibrancyType(aType)];
