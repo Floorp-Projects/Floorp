13bb20a932a8b757e534707bd3b07096c4da8370
JW Wang <jwwang@mozilla.com>
2017-11-20 14:21:22 +0800

    Bug 1418917. P2 - NotifyClientSuspended() should check mClosed. r=bechen,gerald
    MozReview-Commit-ID: 9mBPwfyfWCV

--- dom/media/MediaCache.cpp
+++ dom/media/MediaCache.cpp
@@ -2256,10 +2256,10 @@ MediaCacheStream::NotifyClientSuspended(bool aSuspended)
   RefPtr<ChannelMediaResource> client = mClient;
   nsCOMPtr<nsIRunnable> r = NS_NewRunnableFunction(
     "MediaCacheStream::NotifyClientSuspended", [client, this, aSuspended]() {
-      if (mClientSuspended != aSuspended) {
+      ReentrantMonitorAutoEnter mon(mMediaCache->GetReentrantMonitor());
+      if (!mClosed && mClientSuspended != aSuspended) {
         mClientSuspended = aSuspended;
         // mClientSuspended changes the decision of reading streams.
-        ReentrantMonitorAutoEnter mon(mMediaCache->GetReentrantMonitor());
         mMediaCache->QueueUpdate();
       }
     });
