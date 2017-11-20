7499ebd1db4fb0bb56c8cfdf428374897833babc
Martin Stransky <stransky@redhat.com>
2017-11-20 14:27:19 +0100

    Bug 1418829 - remove widget.allow-client-side-decoration preference, r=jhorak
    widget.allow-client-side-decoration is no longer used, titlebar rendering is controled by browser.tabs.drawInTitlebar only.
    
    MozReview-Commit-ID: LHdBio4Gfry

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4914,7 +4914,6 @@ pref("gfx.apitrace.enabled",false);
 pref("gfx.xrender.enabled",false);
 pref("widget.chrome.allow-gtk-dark-theme", false);
 pref("widget.content.allow-gtk-dark-theme", false);
-pref("widget.allow-client-side-decoration", false);
 #endif
 #endif
 
