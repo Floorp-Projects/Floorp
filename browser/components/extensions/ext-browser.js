5a190716cdb9facde857b002e470d70e0a222617
Bob Silverberg <bsilverberg@mozilla.com>
2017-11-20 17:00:20 -0500

    Bug 1415913 - tabs.create without a windowId should target only normal windows, r=aswan
    This introduces a new property to WindowTracker (on desktop only) called topNormalWindow
    which returns the topmost window which is not a popup window. That property is then used
    by tabs.create to identify the top "normal" window when a windowId is not passed to it.
    
    Bug 1421709 has been filed to look for other opportunities to use topNormalWindow
    in place of topWindow in our codebase.
    
    MozReview-Commit-ID: DcV93wO6FoV

--- browser/components/extensions/ext-browser.js
+++ browser/components/extensions/ext-browser.js
@@ -14,6 +14,8 @@
 
 XPCOMUtils.defineLazyModuleGetter(this, "PrivateBrowsingUtils",
                                   "resource://gre/modules/PrivateBrowsingUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "RecentWindow",
+                                  "resource:///modules/RecentWindow.jsm");
 
 var {
   ExtensionError,
@@ -172,6 +174,17 @@ class WindowTracker extends WindowTrackerBase {
 
   removeProgressListener(window, listener) {
     window.gBrowser.removeTabsProgressListener(listener);
+  }
+
+  /**
+   * @property {DOMWindow|null} topNormalWindow
+   *        The currently active, or topmost, browser window, or null if no
+   *        browser window is currently open.
+   *        Will return the topmost "normal" (i.e., not popup) window.
+   *        @readonly
+   */
+  get topNormalWindow() {
+    return RecentWindow.getMostRecentBrowserWindow({allowPopups: false});
   }
 }
 
