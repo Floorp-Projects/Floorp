291a4eb9691886b0d852376b6b4de584ed52f08b
Jim Chen <nchen@mozilla.com>
2017-11-20 14:15:35 -0500

    Bug 1417650 - Correctly specify flags for Tab:OpenUri; r=me a=gchang
    Pass in the flag names for Tab:OpenUri so we don't have to worry about
    synchronizing the flags in Java with ones in C++. r=me for trivial
    patch.
    
    MozReview-Commit-ID: BowjLV1s7nT
    
    --HG--
    extra : source : 11c2b95991519b35c04214060b2a2168f32cdad2

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -161,10 +161,6 @@ public abstract class GeckoApp extends GeckoActivity
     // for crash loop detection purposes.
     private static final int STARTUP_PHASE_DURATION_MS = 30 * 1000;
 
-    protected static final int LOAD_DEFAULT = 0;
-    protected static final int LOAD_NEW_TAB = 1;
-    protected static final int LOAD_SWITCH_TAB = 2;
-
     private static boolean sAlreadyLoaded;
 
     protected RelativeLayout mRootLayout;
@@ -1800,12 +1796,12 @@ public abstract class GeckoApp extends GeckoActivity
         } else if (ACTION_HOMESCREEN_SHORTCUT.equals(action)) {
             final GeckoBundle data = new GeckoBundle(2);
             data.putString("uri", uri);
-            data.putInt("flags", LOAD_SWITCH_TAB);
+            data.putString("flags", "OPEN_SWITCHTAB");
             getAppEventDispatcher().dispatch("Tab:OpenUri", data);
         } else if (Intent.ACTION_SEARCH.equals(action)) {
             final GeckoBundle data = new GeckoBundle(2);
             data.putString("uri", uri);
-            data.putInt("flags", LOAD_NEW_TAB);
+            data.putString("flags", "OPEN_NEWTAB");
             getAppEventDispatcher().dispatch("Tab:OpenUri", data);
         } else if (NotificationHelper.HELPER_BROADCAST_ACTION.equals(action)) {
             NotificationHelper.getInstance(getApplicationContext()).handleNotificationIntent(intent);
