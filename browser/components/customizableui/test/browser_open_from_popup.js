50f5bf47cd5ae3f74ecc02e0bfa2b66cbd7ff261
Tooru Fujisawa <arai_a@mac.com>
2017-11-21 03:41:56 +0900

    Bug 1418739 - Part 2: Remove unused delayedStartup parameter from BrowserTestUtils.waitForNewWindow. r=smaug

--- browser/components/customizableui/test/browser_open_from_popup.js
+++ browser/components/customizableui/test/browser_open_from_popup.js
@@ -4,7 +4,7 @@
  * Check that opening customize mode in a popup opens it in the main window.
  */
 add_task(async function open_customize_mode_from_popup() {
-  let promiseWindow = BrowserTestUtils.waitForNewWindow(true);
+  let promiseWindow = BrowserTestUtils.waitForNewWindow();
   ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
     content.window.open("about:blank", "_blank", "height=300,toolbar=no");
   });
