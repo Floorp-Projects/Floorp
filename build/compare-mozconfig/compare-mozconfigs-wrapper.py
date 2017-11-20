2ee15dbd8deb8933d80ef2717e2d768fe29f919c
Gregory Szorc <gps@mozilla.com>
2017-11-20 14:47:01 -0800

    Bug 1419182 - Consolidate mozconfig comparison logic into compare-mozconfigs.py; r=mshal
    Hopefully this makes it more obvious how all of this is hooked
    together.
    
    MozReview-Commit-ID: 1jeyIZgHYht
    
    --HG--
    extra : rebase_source : 961474b1c19eacdcfe5ed2d90ba0d1b16a5a29c3

--- build/compare-mozconfig/compare-mozconfigs-wrapper.py
+++ build/compare-mozconfig/compare-mozconfigs-wrapper.py
@@ -7,50 +7,28 @@ from __future__ import unicode_literals
 
 import logging
 import mozunit
+import os
 import subprocess
 import unittest
 
-from os import path
 from buildconfig import substs
 
 log = logging.getLogger(__name__)
 
-PLATFORMS = (
-    'linux32',
-    'linux64',
-    'macosx64',
-    'win32',
-    'win64',
-)
-
 
 class TestCompareMozconfigs(unittest.TestCase):
     def test_compare_mozconfigs(self):
-        """ A wrapper script that calls compare-mozconfig.py
-        based on the platform that the machine is building for"""
-        for platform in PLATFORMS:
-            log.info('Comparing platform %s' % platform)
-            python_exe = substs['PYTHON']
-            topsrcdir = substs['top_srcdir']
+        topsrcdir = substs['top_srcdir']
 
-            # construct paths and args for compare-mozconfig
-            browser_dir = path.join(topsrcdir, 'browser')
-            script_path = path.join(topsrcdir, 'build/compare-mozconfig/compare-mozconfigs.py')
-            beta_mozconfig_path = path.join(browser_dir, 'config/mozconfigs', platform, 'beta')
-            release_mozconfig_path = path.join(browser_dir, 'config/mozconfigs', platform, 'release')
-            nightly_mozconfig_path = path.join(browser_dir, 'config/mozconfigs', platform, 'nightly')
+        ret = subprocess.call([
+            substs['PYTHON'],
+            os.path.join(topsrcdir, 'build', 'compare-mozconfig',
+                         'compare-mozconfigs.py'),
+            topsrcdir
+        ])
 
-            log.info("Comparing beta against nightly mozconfigs")
-            ret_code = subprocess.call([python_exe, script_path, topsrcdir,
-                                        platform + ',' + beta_mozconfig_path +
-                                        ',' + nightly_mozconfig_path])
-            self.assertEqual(0, ret_code)
+        self.assertEqual(0, ret)
 
-            log.info("Comparing release against nightly mozconfigs")
-            ret_code = subprocess.call([python_exe, script_path, topsrcdir,
-                                        platform + ',' + release_mozconfig_path +
-                                        ',' + nightly_mozconfig_path])
-            self.assertEqual(0, ret_code)
 
 if __name__ == '__main__':
     mozunit.main()
