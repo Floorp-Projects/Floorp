8deb2a881baa0223d35688abf089dfc3ad95a8c8
Doug Thayer <dothayer@mozilla.com>
2017-11-21 11:28:40 -0800

    Bug 1417266 - Cache calls to CryptAcquireContext r=mak
    When running history imports, a large percentage of the time
    is spend in CryptAcquireContext. This caches this for all guid
    generation in the places system.
    
    MozReview-Commit-ID: 43lig8F7Uww

--- toolkit/components/places/nsNavHistory.h
+++ toolkit/components/places/nsNavHistory.h
@@ -29,6 +29,11 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/Atomics.h"
 
+#ifdef XP_WIN
+#include "WinUtils.h"
+#include <wincrypt.h>
+#endif
+
 #define QUERYUPDATE_TIME 0
 #define QUERYUPDATE_SIMPLE 1
 #define QUERYUPDATE_COMPLEX 2
@@ -488,6 +493,17 @@ public:
     return mBatchLevel > 0;
   }
 
+#ifdef XP_WIN
+  /**
+   * Get the cached HCRYPTPROV initialized in the nsNavHistory constructor.
+   */
+  nsresult GetCryptoProvider(HCRYPTPROV& aCryptoProvider) const {
+    NS_ENSURE_STATE(mCryptoProviderInitialized);
+    aCryptoProvider = mCryptoProvider;
+    return NS_OK;
+  }
+#endif
+
 private:
   ~nsNavHistory();
 
@@ -642,6 +658,13 @@ protected:
   // Used to enable and disable the observer notifications
   bool mCanNotify;
   nsCategoryCache<nsINavHistoryObserver> mCacheObservers;
+
+  // Used to cache the call to CryptAcquireContext, which is expensive
+  // when called thousands of times
+#ifdef XP_WIN
+  HCRYPTPROV mCryptoProvider;
+  bool mCryptoProviderInitialized;
+#endif
 };
 
 
