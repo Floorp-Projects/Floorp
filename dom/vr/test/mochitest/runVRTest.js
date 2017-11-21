052084a899ab27b96c8ecd9aa698d9451f0e9eca
Kearwood Gilbert <kgilbert@mozilla.com>
2017-11-21 14:18:16 -0800

    Bug 1407423 - Part 3: Update Puppet Device Implementation,r=daoshengmu - Update prefs to accomodate tests, disabling   enumeration throttling - Updated Puppet display and controller implementation   to act more like the actual devices. - Updated tests to ensure that they explicitly   create a VR mock display and don't create duplicate   mock displays.
    MozReview-Commit-ID: 6RPVqekG2je

--- dom/vr/test/mochitest/runVRTest.js
+++ dom/vr/test/mochitest/runVRTest.js
@@ -1,7 +1,9 @@
 function runVRTest(callback) {
   SpecialPowers.pushPrefEnv({"set" : [["dom.vr.puppet.enabled", true],
                                       ["dom.vr.require-gesture", false],
-                                      ["dom.vr.test.enabled", true]]},
+                                      ["dom.vr.test.enabled", true],
+                                      ["dom.vr.display.enumerate.interval", 0],
+                                      ["dom.vr.controller.enumerate.interval", 0]]},
   () => {
     VRServiceTest = navigator.requestVRServiceTest();
     callback();
