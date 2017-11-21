6c7c861a7040de5600aaecbf65e129b47d26a08c
Chun-Min Chang <chun.m.chang@gmail.com>
2017-11-21 18:10:19 +0800

    Bug 654787 - part8: Fire seeking and seeked events when looping back to the beginning; r=jwwang
    MozReview-Commit-ID: 2hYJfcCmvam

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -526,6 +526,10 @@ MediaDecoder::OnPlaybackEvent(MediaEventType aEvent)
     case MediaEventType::SeekStarted:
       SeekingStarted();
       break;
+    case MediaEventType::Loop:
+      GetOwner()->DispatchAsyncEvent(NS_LITERAL_STRING("seeking"));
+      GetOwner()->DispatchAsyncEvent(NS_LITERAL_STRING("seeked"));
+      break;
     case MediaEventType::Invalidate:
       Invalidate();
       break;
