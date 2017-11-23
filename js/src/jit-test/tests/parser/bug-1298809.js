18c60a42fed7cd274621441518e2399ce35e0e38
Jeff Walden <jwalden@mit.edu>
2017-11-23 12:52:33 -0500

    Bug 1416337 - Fix bug 1298809's test to function correctly on beta/release.  r=evilpie

--- js/src/jit-test/tests/parser/bug-1298809.js
+++ js/src/jit-test/tests/parser/bug-1298809.js
@@ -1,8 +1,11 @@
 if (getBuildConfiguration().release_or_beta) {
     eval(`
     function f() {
+      // The expression closure is deliberate here, testing the semicolon after
+      // one when it appears as a FunctionDeclaration from B.3.4
+      // FunctionDeclarations in IfStatement Statement Clauses.
       if (0)
-        function g() { x };
+        function g() x;
       else;
     }
     f();`)
