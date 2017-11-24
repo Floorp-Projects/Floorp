f8e1743ead12efaaad04911625521dd86009df51
James Teh <jteh@mozilla.com>
2017-11-24 15:12:37 +1000

    Bug 1418448 part 1: Accessible HandlerProvider: Clean up DynamicIA2Data correctly. r=aklotz
    DynamicIA2Data contains several BSTRs that need to be freed with SysFreeString.
    Previously, we just did a ZeroMemory without actually freeing them.
    This cleanup code is placed in a header file so it can be used by AccessibleHandler as well.
    
    MozReview-Commit-ID: 4SWuK9oMRYZ

--- accessible/ipc/win/HandlerProvider.cpp
+++ accessible/ipc/win/HandlerProvider.cpp
@@ -17,6 +17,7 @@
 #include "HandlerData_i.c"
 #include "mozilla/Assertions.h"
 #include "mozilla/a11y/AccessibleWrap.h"
+#include "mozilla/a11y/HandlerDataCleanup.h"
 #include "mozilla/dom/ContentChild.h"
 #include "mozilla/Move.h"
 #include "mozilla/mscom/AgileReference.h"
@@ -132,7 +133,8 @@ HandlerProvider::GetAndSerializePayload(const MutexAutoLock&,
   // Now that we have serialized payload, we should clean up any
   // BSTRs, interfaces, etc. fetched in BuildInitialIA2Data.
   CleanupStaticIA2Data(payload.mStaticData);
-  CleanupDynamicIA2Data(payload.mDynamicData);
+  // No need to zero memory, since payload is going out of scope.
+  CleanupDynamicIA2Data(payload.mDynamicData, false);
 }
 
 HRESULT
@@ -419,13 +421,6 @@ HandlerProvider::CleanupStaticIA2Data(StaticIA2Data& aData)
     aData.mIATableCell->Release();
   }
   ZeroMemory(&aData, sizeof(StaticIA2Data));
-}
-
-void
-HandlerProvider::CleanupDynamicIA2Data(DynamicIA2Data& aData)
-{
-  ::VariantClear(&aData.mRole);
-  ZeroMemory(&aData, sizeof(DynamicIA2Data));
 }
 
 void
