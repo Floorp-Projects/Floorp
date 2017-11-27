a48c92feb44e8fa68301659391d80e0a9c3ed206
James Graham <james@hoppipolla.co.uk>
2017-11-27 16:16:55 +0000

    Bug 1400716 - Explicitly drain all queues on exit, r=maja_zf
    The fact that the queues were not drained on exist seemed to cause an
    intermittent failure where one process tried to write to a queue that
    another had closed. Draining the queues explicitly should avoid this
    and ensure we surface hidden problems in the log.
    
    MozReview-Commit-ID: 8ulLNpIcj5z

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executormarionette.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executormarionette.py
@@ -175,7 +175,11 @@ class MarionetteProtocol(Protocol):
             # This can happen if there was a crash
             return
         if socket_timeout:
-            self.marionette.timeout.script = socket_timeout / 2
+            try:
+                self.marionette.timeout.script = socket_timeout / 2
+            except (socket.error, IOError):
+                self.logger.debug("Socket closed")
+                return
 
         self.marionette.switch_to_window(self.runner_handle)
         while True:
