372c99b8eb385caf4c6ee6e748550bb607c15d65
Dragana Damjanovic <dd.mozilla@gmail.com>
2017-11-27 21:12:31 +0100

    Bug 1404069 - Revert bug 1363372. r=mayhemer

--- netwerk/base/nsSocketTransportService2.h
+++ netwerk/base/nsSocketTransportService2.h
@@ -23,12 +23,6 @@
 #include "mozilla/UniquePtr.h"
 #include "PollableEvent.h"
 
-#if defined(_WIN64) && defined(WIN95)
-#include "WinDef.h"
-
-typedef PRStatus (*FileDesc2PlatformOverlappedIOHandleFunc)(PRFileDesc *fd, void **ol);
-#endif
-
 class nsASocketHandler;
 struct PRPollDesc;
 class nsIPrefBranch;
@@ -126,11 +120,6 @@ public:
                                                        !mSleepPhase; }
     PRIntervalTime MaxTimeForPrClosePref() {return mMaxTimeForPrClosePref; }
 
-#if defined(_WIN64) && defined(WIN95)
-    void AddOverlappedPendingSocket(PRFileDesc *aFd);
-    bool HasFileDesc2PlatformOverlappedIOHandleFunc();
-    PRStatus CallFileDesc2PlatformOverlappedIOHandleFunc(PRFileDesc *fd, void **ol);
-#endif
 protected:
 
     virtual ~nsSocketTransportService();
@@ -281,21 +270,6 @@ private:
     void StartPolling();
     void EndPolling();
 #endif
-
-#if defined(_WIN64) && defined(WIN95)       
-    // If TCP Fast Open is used on Windows an overlapped io is used. We need to
-    // wait until this io is finished or canceled before detroying its
-    // PRFileDesc.
-    nsTArray<PRFileDesc *> mOverlappedPendingSockets;
-
-    bool mFileDesc2PlatformOverlappedIOHandleFuncChecked;
-    HMODULE mNsprLibrary;
-    FileDesc2PlatformOverlappedIOHandleFunc mFileDesc2PlatformOverlappedIOHandleFunc;
-
-    void CheckFileDesc2PlatformOverlappedIOHandleFunc();
-    void CheckOverlappedPendingSocketsAreDone();
-#endif
-
 };
 
 extern nsSocketTransportService *gSocketTransportService;
