cfded25d122f57c661af325ab2178b2a3ecf1f01
Gabriel Luong <gabriel.luong@gmail.com>
2017-11-27 15:54:18 -0500

    Bug 1369945 - Display a split rule view panel in the inspector. r=bgrins
    MozReview-Commit-ID: If55vBPfU3W

--- devtools/client/inspector/rules/rules.js
+++ devtools/client/inspector/rules/rules.js
@@ -1598,7 +1598,10 @@ function RuleViewTool(inspector, window) {
   this.inspector.selection.on("new-node-front", this.onSelected);
   this.inspector.selection.on("pseudoclass", this.refresh);
   this.inspector.target.on("navigate", this.clearUserProperties);
+
+  this.inspector.ruleViewSideBar.on("ruleview-selected", this.onPanelSelected);
   this.inspector.sidebar.on("ruleview-selected", this.onPanelSelected);
+
   this.inspector.pageStyle.on("stylesheet-updated", this.refresh);
   this.inspector.walker.on("mutations", this.onMutations);
   this.inspector.walker.on("resize", this.onResized);
@@ -1611,7 +1614,9 @@ RuleViewTool.prototype = {
     if (!this.view) {
       return false;
     }
-    return this.inspector.sidebar.getCurrentTabID() == "ruleview";
+
+    return this.inspector.isSplitRuleViewEnabled ?
+      true : this.inspector.sidebar.getCurrentTabID() == "ruleview";
   },
 
   onSelected: function (event) {
