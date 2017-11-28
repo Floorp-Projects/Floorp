042edf3bad7e384e5931f272279f4ec924368baf
JerryShih <hshih@mozilla.com>
2017-11-28 20:04:00 +0800

    Bug 1415754 - check the surface status before use in RenderDXGITextureHostOGL::EnsureLockable(). r=jgilbert
    MozReview-Commit-ID: 4e0lyXsVOgT

--- gfx/webrender_bindings/RenderD3D11TextureHostOGL.cpp
+++ gfx/webrender_bindings/RenderD3D11TextureHostOGL.cpp
@@ -91,7 +91,9 @@ RenderDXGITextureHostOGL::EnsureLockable()
                                                    reinterpret_cast<EGLClientBuffer>(mHandle),
                                                    gl::GLContextEGL::Cast(mGL.get())->mConfig,
                                                    pbufferAttributes);
-    MOZ_ASSERT(mSurface);
+    if (!mSurface) {
+      return false;
+    }
 
     // Query the keyed-mutex.
     egl->fQuerySurfacePointerANGLE(egl->Display(),
