da9a6071c526162ab2a51bb30c146f94dcee5864
Dan Minor <dminor@mozilla.com>
2017-11-28 14:51:53 -0500

    Bug 1406936 - Copy state when reusing SendStream in ConfigureSendMediaCodecs; r=pehrsons
    This is necessary for changes to encoding constraints and simulcast encodings to
    be picked up.
    
    MozReview-Commit-ID: JpVN0ST70Cn

--- media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
+++ media/webrtc/signaling/src/media-conduit/VideoConduit.cpp
@@ -808,6 +808,11 @@ WebrtcVideoConduit::ConfigureSendMediaCodec(const VideoCodecConfig* codecConfig)
   // Recreating on PayloadType change may be overkill, but is safe.
   if (mSendStream) {
     if (!RequiresNewSendStream(*codecConfig)) {
+      {
+        MutexAutoLock lock(mCodecMutex);
+        mCurSendCodecConfig->mEncodingConstraints = codecConfig->mEncodingConstraints;
+        mCurSendCodecConfig->mSimulcastEncodings = codecConfig->mSimulcastEncodings;
+      }
       mSendStream->ReconfigureVideoEncoder(mEncoderConfig.CopyConfig());
       return kMediaConduitNoError;
     }
