4d7aca30be3b4e19f42cb5f88b07fbb8eac74594
Nika Layzell <nika@thelayzells.com>
2017-11-28 15:30:56 -0500

    Bug 1415158 - Don't notify ContentParent of a load if our IPC channel is closed. r=billm
    MozReview-Commit-ID: 4HBIalVCfno

--- netwerk/protocol/ftp/FTPChannelParent.cpp
+++ netwerk/protocol/ftp/FTPChannelParent.cpp
@@ -460,10 +460,13 @@ FTPChannelParent::OnStartRequest(nsIRequest* aRequest, nsISupports* aContext)
 
   // Send down any permissions which are relevant to this URL if we are
   // performing a document load.
-  PContentParent* pcp = Manager()->Manager();
-  DebugOnly<nsresult> rv =
-    static_cast<ContentParent*>(pcp)->AboutToLoadHttpFtpWyciwygDocumentForChild(chan);
-  MOZ_ASSERT(NS_SUCCEEDED(rv));
+  if (!mIPCClosed) {
+    PContentParent* pcp = Manager()->Manager();
+    MOZ_ASSERT(pcp, "We should have a manager if our IPC isn't closed");
+    DebugOnly<nsresult> rv =
+      static_cast<ContentParent*>(pcp)->AboutToLoadHttpFtpWyciwygDocumentForChild(chan);
+    MOZ_ASSERT(NS_SUCCEEDED(rv));
+  }
 
   int64_t contentLength;
   chan->GetContentLength(&contentLength);
