aebc9ad6f90055d3836e4a20fffb339c0b7f0b48
Mark Banner <standard8@mozilla.com>
2017-11-28 19:24:12 +0000

    Bug 1421968 - Enable ESLint rule mozilla/use-services by default, but whitelist it for various directories still to be fixed. r=mossop
    MozReview-Commit-ID: KOZWr7FiDQZ

--- browser/components/.eslintrc.js
+++ browser/components/.eslintrc.js
@@ -5,7 +5,5 @@ module.exports = {
     // XXX Bug 1326071 - This should be reduced down - probably to 20 or to
     // be removed & synced with the mozilla/recommended value.
     "complexity": ["error", 61],
-
-    "mozilla/use-services": "error",
   }
 };
