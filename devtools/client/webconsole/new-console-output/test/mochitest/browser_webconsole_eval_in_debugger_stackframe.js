cfd1a43f56e983ae99b9a6770520e930acf25f8f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2017-11-29 16:55:00 +0100

    Bug 1408936 - Enable browser_webconsole_eval_in_debugger_stackframe.js in new console frontend; r=bgrins.
    MozReview-Commit-ID: 6IiuekmfENG
    
    --HG--
    extra : rebase_source : c190b35c66dfb93db86aa38f6c23e48e2be15ab7

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_eval_in_debugger_stackframe.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_eval_in_debugger_stackframe.js
@@ -9,149 +9,76 @@
 "use strict";
 
 const TEST_URI = "http://example.com/browser/devtools/client/webconsole/" +
-                 "test/test-eval-in-stackframe.html";
+                 "new-console-output/test/mochitest/test-eval-in-stackframe.html";
 
-var gWebConsole, gJSTerm, gDebuggerWin, gThread, gDebuggerController;
-var gStackframes;
+add_task(async function () {
+  // Force the old debugger UI since it's directly used (see Bug 1301705).
+  await pushPref("devtools.debugger.new-debugger-frontend", false);
 
-// Force the old debugger UI since it's directly used (see Bug 1301705)
-Services.prefs.setBoolPref("devtools.debugger.new-debugger-frontend", false);
-registerCleanupFunction(function* () {
-  Services.prefs.clearUserPref("devtools.debugger.new-debugger-frontend");
-});
+  info("open the console");
+  const hud = await openNewTabAndConsole(TEST_URI);
+  const {jsterm} = hud;
 
-function test() {
-  loadTab(TEST_URI).then(() => {
-    openConsole().then(consoleOpened);
+  info("Check `foo` value");
+  let onResultMessage = waitForMessage(hud, "globalFooBug783499");
+  jsterm.execute("foo");
+  await onResultMessage;
+  ok(true, "|foo| value is correct");
+
+  info("Assign and check `foo2` value");
+  onResultMessage = waitForMessage(hud, "newFoo");
+  jsterm.execute("foo2 = 'newFoo'; window.foo2");
+  await onResultMessage;
+  ok(true, "'newFoo' is displayed after adding `foo2`");
+
+  info("Open the debugger and then select the console again");
+  const {panel} = await openDebugger();
+  const {activeThread, StackFrames: stackFrames} = panel.panelWin.DebuggerController;
+
+  await openConsole();
+
+  info("Check `foo + foo2` value");
+  onResultMessage = waitForMessage(hud, "globalFooBug783499newFoo");
+  jsterm.execute("foo + foo2");
+  await onResultMessage;
+
+  info("Select the debugger again");
+  await openDebugger();
+
+  const onFirstCallFramesAdded = activeThread.addOneTimeListener("framesadded");
+  // firstCall calls secondCall, which has a debugger statement, so we'll be paused.
+  ContentTask.spawn(gBrowser.selectedBrowser, {}, function () {
+    content.wrappedJSObject.firstCall();
   });
-}
+  await onFirstCallFramesAdded;
 
-function consoleOpened(hud) {
-  gWebConsole = hud;
-  gJSTerm = hud.jsterm;
-  gJSTerm.execute("foo").then(onExecuteFoo);
-}
+  info("frames added, select the console again");
+  await openConsole();
 
-function onExecuteFoo() {
-  isnot(gWebConsole.outputNode.textContent.indexOf("globalFooBug783499"), -1,
-        "|foo| value is correct");
+  info("Check `foo + foo2` value when paused");
+  onResultMessage = waitForMessage(hud, "globalFooBug783499foo2SecondCall");
+  jsterm.execute("foo + foo2");
+  ok(true, "`foo + foo2` from `secondCall()`");
 
-  gJSTerm.clearOutput();
+  info("select the debugger and select the frame (1)");
+  await openDebugger();
+  stackFrames.selectFrame(1);
+  await openConsole();
 
-  // Test for Bug 690529 - Web Console and Scratchpad should evaluate
-  // expressions in the scope of the content window, not in a sandbox.
-  executeSoon(() => {
-    gJSTerm.execute("foo2 = 'newFoo'; window.foo2").then(onNewFoo2);
+  info("Check `foo + foo2 + foo3` value when paused on a given frame");
+  onResultMessage = waitForMessage(hud, "fooFirstCallnewFoofoo3FirstCall");
+  jsterm.execute("foo + foo2 + foo3");
+  await onResultMessage;
+  ok(true, "`foo + foo2 + foo3` from `firstCall()`");
+
+  onResultMessage = waitForMessage(hud, "abbabug783499");
+  jsterm.execute("foo = 'abba'; foo3 = 'bug783499'; foo + foo3");
+  await onResultMessage;
+  ok(true, "`foo + foo3` updated in `firstCall()`");
+
+  await ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+    is(content.wrappedJSObject.foo, "globalFooBug783499", "`foo` in content window");
+    is(content.wrappedJSObject.foo2, "newFoo", "`foo2` in content window");
+    ok(!content.wrappedJSObject.foo3, "`foo3` was not added to the content window");
   });
-}
-
-function onNewFoo2(msg) {
-  is(gWebConsole.outputNode.textContent.indexOf("undefined"), -1,
-     "|undefined| is not displayed after adding |foo2|");
-
-  ok(msg, "output result found");
-
-  isnot(msg.textContent.indexOf("newFoo"), -1,
-        "'newFoo' is displayed after adding |foo2|");
-
-  gJSTerm.clearOutput();
-
-  info("openDebugger");
-  executeSoon(() => openDebugger().then(debuggerOpened));
-}
-
-function debuggerOpened(aResult) {
-  gDebuggerWin = aResult.panelWin;
-  gDebuggerController = gDebuggerWin.DebuggerController;
-  gThread = gDebuggerController.activeThread;
-  gStackframes = gDebuggerController.StackFrames;
-
-  info("openConsole");
-  executeSoon(() =>
-    openConsole().then(() =>
-      gJSTerm.execute("foo + foo2").then(onExecuteFooAndFoo2)
-    )
-  );
-}
-
-function onExecuteFooAndFoo2() {
-  let expected = "globalFooBug783499newFoo";
-  isnot(gWebConsole.outputNode.textContent.indexOf(expected), -1,
-        "|foo + foo2| is displayed after starting the debugger");
-
-  executeSoon(() => {
-    gJSTerm.clearOutput();
-
-    info("openDebugger");
-    openDebugger().then(() => {
-      gThread.addOneTimeListener("framesadded", onFramesAdded);
-
-      info("firstCall()");
-      ContentTask.spawn(gBrowser.selectedBrowser, {}, function* () {
-        content.wrappedJSObject.firstCall();
-      });
-    });
-  });
-}
-
-function onFramesAdded() {
-  info("onFramesAdded, openConsole() now");
-  executeSoon(() =>
-    openConsole().then(() =>
-      gJSTerm.execute("foo + foo2").then(onExecuteFooAndFoo2InSecondCall)
-    )
-  );
-}
-
-function onExecuteFooAndFoo2InSecondCall() {
-  let expected = "globalFooBug783499foo2SecondCall";
-  isnot(gWebConsole.outputNode.textContent.indexOf(expected), -1,
-        "|foo + foo2| from |secondCall()|");
-
-  function runOpenConsole() {
-    openConsole().then(() => {
-      gJSTerm.execute("foo + foo2 + foo3").then(onExecuteFoo23InFirstCall);
-    });
-  }
-
-  executeSoon(() => {
-    gJSTerm.clearOutput();
-
-    info("openDebugger and selectFrame(1)");
-
-    openDebugger().then(() => {
-      gStackframes.selectFrame(1);
-
-      info("openConsole");
-      executeSoon(() => runOpenConsole());
-    });
-  });
-}
-
-function onExecuteFoo23InFirstCall() {
-  let expected = "fooFirstCallnewFoofoo3FirstCall";
-  isnot(gWebConsole.outputNode.textContent.indexOf(expected), -1,
-        "|foo + foo2 + foo3| from |firstCall()|");
-
-  executeSoon(() =>
-    gJSTerm.execute("foo = 'abba'; foo3 = 'bug783499'; foo + foo3").then(
-                    onExecuteFooAndFoo3ChangesInFirstCall));
-}
-
-var onExecuteFooAndFoo3ChangesInFirstCall = Task.async(function*() {
-  let expected = "abbabug783499";
-  isnot(gWebConsole.outputNode.textContent.indexOf(expected), -1,
-        "|foo + foo3| updated in |firstCall()|");
-
-  yield ContentTask.spawn(gBrowser.selectedBrowser, null, function*() {
-    is(content.wrappedJSObject.foo, "globalFooBug783499",
-       "|foo| in content window");
-    is(content.wrappedJSObject.foo2, "newFoo", "|foo2| in content window");
-    ok(!content.wrappedJSObject.foo3,
-       "|foo3| was not added to the content window");
-  });
-
-  gWebConsole = gJSTerm = gDebuggerWin = gThread = gDebuggerController =
-    gStackframes = null;
-  executeSoon(finishTest);
 });
