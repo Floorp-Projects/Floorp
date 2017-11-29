7f8e905fdbce7d827ae7a0ea6e580b331b3c4f9b
Andrew Halberstadt <ahalberstadt@mozilla.com>
2017-11-29 12:00:30 -0500

    Bug 1209463 - [mozlog] Create a SummaryHandler class and refactor the 'mach' formatter to use it, r=jgraham
    The mach formatter gathers result counts and unexpected messages during the run
    to be dumped in a summary at the end. This is a pattern we'd like to repeat in
    several other formatters as well. Rather than re-implementing, this creates a
    handler class that does nothing but store the data. Formatters can then choose
    how to format this data and when to print it.
    
    MozReview-Commit-ID: HKtVr5PxfOy
    
    --HG--
    extra : rebase_source : 22789db1b2fea1e44f47ef1aa9b22b21a6e8649c

--- testing/mozbase/mozlog/mozlog/handlers/__init__.py
+++ testing/mozbase/mozlog/mozlog/handlers/__init__.py
@@ -6,8 +6,9 @@ from __future__ import absolute_import
 
 from .base import LogLevelFilter, StreamHandler, BaseHandler
 from .statushandler import StatusHandler
+from .summaryhandler import SummaryHandler
 from .bufferhandler import BufferHandler
 from .valgrindhandler import ValgrindHandler
 
 __all__ = ['LogLevelFilter', 'StreamHandler', 'BaseHandler',
-           'StatusHandler', 'BufferHandler', 'ValgrindHandler']
+           'StatusHandler', 'SummaryHandler', 'BufferHandler', 'ValgrindHandler']
