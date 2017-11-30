fe823ef158df39f8739b3818f5d169f25148a320
Andrea Marchesini <amarchesini@mozilla.com>
2017-11-30 20:00:23 +0100

    Bug 1421176 - nsMultiplexInputStream::Available() sanitize - mochitest - r=smaug
    This is a test to check if nsMultiplexInputStream supports empty inputStreams.

--- dom/file/ipc/tests/script_file.js
+++ dom/file/ipc/tests/script_file.js
@@ -7,7 +7,7 @@ addMessageListener("file.open", function (e) {
                    .QueryInterface(Ci.nsIProperties)
                    .get("ProfD", Ci.nsIFile);
   testFile.append("ipc_fileReader_testing");
-  testFile.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0o600);
+  testFile.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0o600);
 
   var outStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
                       .createInstance(Components.interfaces.nsIFileOutputStream);
@@ -20,5 +20,23 @@ addMessageListener("file.open", function (e) {
 
   File.createFromNsIFile(testFile).then(function(file) {
     sendAsyncMessage("file.opened", { file });
+  });
+});
+
+addMessageListener("emptyfile.open", function (e) {
+  var testFile = Cc["@mozilla.org/file/directory_service;1"]
+                   .getService(Ci.nsIDirectoryService)
+                   .QueryInterface(Ci.nsIProperties)
+                   .get("ProfD", Ci.nsIFile);
+  testFile.append("ipc_fileReader_testing");
+  testFile.createUnique(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 0o600);
+
+  var outStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+                      .createInstance(Components.interfaces.nsIFileOutputStream);
+  outStream.init(testFile, 0x02 | 0x08 | 0x20, // write, create, truncate
+                 0666, 0);
+
+  File.createFromNsIFile(testFile).then(function(file) {
+    sendAsyncMessage("emptyfile.opened", { file });
   });
 });
