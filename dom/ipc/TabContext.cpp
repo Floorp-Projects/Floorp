0592192546c534a33d46611c905c11f2cf96f236
Andrea Marchesini <amarchesini@mozilla.com>
2017-11-30 18:16:45 +0100

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 8 - ServiceWorkers enabled, r=asuth

--- dom/ipc/TabContext.cpp
+++ dom/ipc/TabContext.cpp
@@ -8,6 +8,7 @@
 #include "mozilla/dom/PTabContext.h"
 #include "mozilla/dom/TabParent.h"
 #include "mozilla/dom/TabChild.h"
+#include "mozilla/dom/DOMPreferences.h"
 #include "nsIScriptSecurityManager.h"
 #include "nsServiceManagerUtils.h"
 
@@ -246,7 +247,7 @@ MaybeInvalidTabContext::MaybeInvalidTabContext(const IPCTabContext& aParams)
       // It is meant as a temporary solution until service workers can
       // provide a TabChild equivalent. Don't allow this on b2g since
       // it might be used to escalate privileges.
-      if (!Preferences::GetBool("dom.serviceWorkers.enabled", false)) {
+      if (!DOMPreferences::ServiceWorkersEnabled()) {
         mInvalidReason = "ServiceWorkers should be enabled.";
         return;
       }
--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -21,7 +21,6 @@
 //   * First argument is the name of the pref.
 //   * The name of the function that updates the new value of a pref.
 
-WORKER_SIMPLE_PREF("dom.serviceWorkers.enabled", ServiceWorkersEnabled, SERVICEWORKERS_ENABLED)
 WORKER_SIMPLE_PREF("dom.serviceWorkers.testing.enabled", ServiceWorkersTestingEnabled, SERVICEWORKERS_TESTING_ENABLED)
 WORKER_SIMPLE_PREF("dom.serviceWorkers.openWindow.enabled", OpenWindowEnabled, OPEN_WINDOW_ENABLED)
 WORKER_SIMPLE_PREF("dom.storageManager.enabled", StorageManagerEnabled, STORAGEMANAGER_ENABLED)
