c8f7a24671810f0123e4eb2dd45eaee447dd2181
Jim Chen <nchen@mozilla.com>
2017-11-30 13:25:50 -0500

    Bug 1416316 - 1. Move overscroll to LayerSession; r=rbarker
    There is some overscroll handling code in NativePanZoomController that
    should be moved, along with other overscroll code in LayerView, to
    LayerSession. LayerSession now provides a getter for
    OverscrollEdgeEffect, which is cleaned up to have a public API with
    documentation.
    
    MozReview-Commit-ID: LkKHFS8OkR7
    
    --HG--
    extra : rebase_source : dc6b680b1cc7d7e2dd221091a63a208971d63c80

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/OverscrollEdgeEffect.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/OverscrollEdgeEffect.java
@@ -5,6 +5,8 @@
 
 package org.mozilla.gecko.gfx;
 
+import org.mozilla.gecko.util.ThreadUtils;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -12,25 +14,44 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.os.Build;
+import android.view.View;
 import android.widget.EdgeEffect;
 
 import java.lang.reflect.Field;
 
-public class OverscrollEdgeEffect implements Overscroll {
+public class OverscrollEdgeEffect {
     // Used to index particular edges in the edges array
     private static final int TOP = 0;
     private static final int BOTTOM = 1;
     private static final int LEFT = 2;
     private static final int RIGHT = 3;
 
+    /* package */ static final int AXIS_X = 0;
+    /* package */ static final int AXIS_Y = 1;
+
     // All four edges of the screen
     private final EdgeEffect[] mEdges = new EdgeEffect[4];
 
-    // The view we're showing this overscroll on.
-    private final LayerView mView;
+    private final LayerSession mSession;
+    private Runnable mInvalidationCallback;
+    private int mWidth;
+    private int mHeight;
 
-    public OverscrollEdgeEffect(final LayerView v) {
+    /* package */ OverscrollEdgeEffect(final LayerSession session) {
+        mSession = session;
+    }
+
+    /**
+     * Set the theme to use for overscroll from a given Context.
+     *
+     * @param context Context to use for the overscroll theme.
+     */
+    public void setTheme(final Context context) {
+        ThreadUtils.assertOnUiThread();
+
+        final PorterDuffXfermode mode = new PorterDuffXfermode(PorterDuff.Mode.SRC);
         Field paintField = null;
+
         if (Build.VERSION.SDK_INT >= 21) {
             try {
                 paintField = EdgeEffect.class.getDeclaredField("mPaint");
@@ -39,35 +60,62 @@ public class OverscrollEdgeEffect implements Overscroll {
             }
         }
 
-        mView = v;
-        Context context = v.getContext();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < mEdges.length; i++) {
             mEdges[i] = new EdgeEffect(context);
 
-            try {
-                if (paintField != null) {
-                    final Paint p = (Paint) paintField.get(mEdges[i]);
+            if (paintField == null) {
+                continue;
+            }
 
-                    // The Android EdgeEffect class uses a mode of SRC_ATOP here, which means it will only
-                    // draw the effect where there are non-transparent pixels in the destination. Since the LayerView
-                    // itself is fully transparent, it doesn't display at all. We need to use SRC instead.
-                    p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
-                }
+            try {
+                final Paint p = (Paint) paintField.get(mEdges[i]);
+
+                // The Android EdgeEffect class uses a mode of SRC_ATOP here, which means
+                // it will only draw the effect where there are non-transparent pixels in
+                // the destination. Since the LayerView itself is fully transparent, it
+                // doesn't display at all. We need to use SRC instead.
+                p.setXfermode(mode);
             } catch (IllegalAccessException e) {
             }
         }
     }
 
-    @Override
-    public void setSize(final int width, final int height) {
+    /**
+     * Set a Runnable that acts as a callback to invalidate the overscroll effect (for
+     * example, as a response to user fling for example). The Runnbale should schedule a
+     * future call to {@link #draw(Canvas)} as a result of the invalidation.
+     *
+     * @param runnable Invalidation Runnable.
+     * @see #getInvalidationCallback()
+     */
+    public void setInvalidationCallback(final Runnable runnable) {
+        ThreadUtils.assertOnUiThread();
+        mInvalidationCallback = runnable;
+    }
+
+    /**
+     * Get the current invalidatation Runnable.
+     *
+     * @return Invalidation Runnable.
+     * @see #setInvalidationCallback(Runnable)
+     */
+    public Runnable getInvalidationCallback() {
+        ThreadUtils.assertOnUiThread();
+        return mInvalidationCallback;
+    }
+
+    /* package */ void setSize(final int width, final int height) {
         mEdges[LEFT].setSize(height, width);
         mEdges[RIGHT].setSize(height, width);
         mEdges[TOP].setSize(width, height);
         mEdges[BOTTOM].setSize(width, height);
+
+        mWidth = width;
+        mHeight = height;
     }
 
-    private EdgeEffect getEdgeForAxisAndSide(final Axis axis, final float side) {
-        if (axis == Axis.Y) {
+    private EdgeEffect getEdgeForAxisAndSide(final int axis, final float side) {
+        if (axis == AXIS_Y) {
             if (side < 0) {
                 return mEdges[TOP];
             } else {
@@ -82,16 +130,7 @@ public class OverscrollEdgeEffect implements Overscroll {
         }
     }
 
-    private void invalidate() {
-        if (Build.VERSION.SDK_INT >= 16) {
-            mView.postInvalidateOnAnimation();
-        } else {
-            mView.postInvalidateDelayed(10);
-        }
-    }
-
-    @Override
-    public void setVelocity(final float velocity, final Axis axis) {
+    /* package */ void setVelocity(final float velocity, final int axis) {
         final EdgeEffect edge = getEdgeForAxisAndSide(axis, velocity);
 
         // If we're showing overscroll already, start fading it out.
@@ -102,29 +141,35 @@ public class OverscrollEdgeEffect implements Overscroll {
             edge.onAbsorb((int)velocity);
         }
 
-        invalidate();
+        if (mInvalidationCallback != null) {
+            mInvalidationCallback.run();
+        }
     }
 
-    @Override
-    public void setDistance(final float distance, final Axis axis) {
+    /* package */ void setDistance(final float distance, final int axis) {
         // The first overscroll event often has zero distance. Throw it out
         if (distance == 0.0f) {
             return;
         }
 
         final EdgeEffect edge = getEdgeForAxisAndSide(axis, (int)distance);
-        edge.onPull(distance / (axis == Axis.X ? mView.getWidth() : mView.getHeight()));
-        invalidate();
+        edge.onPull(distance / (axis == AXIS_X ? mWidth : mHeight));
+
+        if (mInvalidationCallback != null) {
+            mInvalidationCallback.run();
+        }
     }
 
-    @Override
+    /**
+     * Draw the overscroll effect on a Canvas.
+     *
+     * @param canvas Canvas to draw on.
+     */
     public void draw(final Canvas canvas) {
-        if (mView.mSession == null) {
-            return;
-        }
+        ThreadUtils.assertOnUiThread();
 
         final Rect pageRect = new Rect();
-        mView.mSession.getSurfaceBounds(pageRect);
+        mSession.getSurfaceBounds(pageRect);
 
         // If we're pulling an edge, or fading it out, draw!
         boolean invalidate = false;
@@ -145,8 +190,8 @@ public class OverscrollEdgeEffect implements Overscroll {
         }
 
         // If the edge effect is animating off screen, invalidate.
-        if (invalidate) {
-            invalidate();
+        if (invalidate && mInvalidationCallback != null) {
+            mInvalidationCallback.run();
         }
     }
 
