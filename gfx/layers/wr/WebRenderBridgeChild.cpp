f303d56ec5f9b48e33322816bfdb968d295e2306
Jeff Muizelaar <jmuizelaar@mozilla.com>
2017-11-30 07:16:21 -0500

    Bug 1406507 - Use an ipc::ByteBuf instead of ByteBuffer to send the DisplayList instead of a ByteBuffer. r=kats
    DONTBUILD because this is a backout/reland to make sure the code is associated
    with the right bug.
    
    MozReview-Commit-ID: GPQABR9ONld

--- gfx/layers/wr/WebRenderBridgeChild.cpp
+++ gfx/layers/wr/WebRenderBridgeChild.cpp
@@ -144,7 +144,9 @@ WebRenderBridgeChild::EndTransaction(const wr::LayoutSize& aContentSize,
   MOZ_ASSERT(!mDestroyed);
   MOZ_ASSERT(mIsInTransaction);
 
-  ByteBuffer dlData(Move(aDL.dl));
+  ByteBuf dlData(aDL.dl.inner.data, aDL.dl.inner.length, aDL.dl.inner.capacity);
+  aDL.dl.inner.capacity = 0;
+  aDL.dl.inner.data = nullptr;
 
   TimeStamp fwdTime;
 #if defined(ENABLE_FRAME_LATENCY_LOG)
