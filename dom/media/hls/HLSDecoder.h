20bf6724a550d7fb0ce224f2ff01b4cd8442799f
JW Wang <jwwang@mozilla.com>
2017-11-30 11:07:13 +0800

    Bug 1421875. P2 - move MediaDecoder::NotifyDataArrived() down the class hierarchy. r=bechen,gerald
    For it is never used by ChannelMediaDecoder.
    
    MozReview-Commit-ID: Jtvlj0iwTm7

--- dom/media/hls/HLSDecoder.h
+++ dom/media/hls/HLSDecoder.h
@@ -41,6 +41,9 @@ public:
   void Resume() override;
   void Shutdown() override;
 
+  // Called as data arrives on the underlying HLS player. Main thread only.
+  void NotifyDataArrived();
+
 private:
   friend class HLSResourceCallbacksSupport;
 
