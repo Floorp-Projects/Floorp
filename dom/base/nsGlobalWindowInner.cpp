df66e497b84040b14ea7d2a0f86b8be4100bf281
Andrea Marchesini <amarchesini@mozilla.com>
2017-11-30 18:16:44 +0100

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 2 - image bitmap, r=asuth

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -7505,7 +7505,7 @@ nsGlobalWindowInner::CreateImageBitmap(JSContext* aCx,
                                        const Sequence<ChannelPixelLayout>& aLayout,
                                        ErrorResult& aRv)
 {
-  if (!ImageBitmap::ExtensionsEnabled(aCx)) {
+  if (!DOMPreferences::ImageBitmapExtensionsEnabled()) {
     aRv.Throw(NS_ERROR_TYPE_ERR);
     return nullptr;
   }
