80f0fc10cedd91fc2970d6dd486e2de4d884ec9c
Valentin Gosu <valentin.gosu@gmail.com>
2017-12-02 23:15:55 +0100

    Bug 1420954 - Add NS_MutateURI helper function to chain nsIURIMutator setters in C++ r=bagder
    MozReview-Commit-ID: 79NlOm08ebz

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -3768,3 +3768,9 @@ Test_NormalizeIPv4(const nsACString& host, nsCString& result)
 {
     return nsStandardURL::NormalizeIPv4(host, result);
 }
+
+nsresult
+NS_GetURIMutator(nsIURI* aURI, nsIURIMutator** aMutator)
+{
+    return aURI->Mutate(aMutator);
+}
