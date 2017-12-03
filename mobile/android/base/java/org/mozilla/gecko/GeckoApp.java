24138b05eb86f1ad4022bd310d33e7afb96b2a26
Jan Henning <jh+bugzilla@buttercookie.de>
2017-12-03 21:16:03 +0100

    Bug 1413739 - Part 1 - Offer Firefox itself as an Assist App. r=nechen
    The technique for setting our icon is just a straight reimplementation
    of bug 1210242.
    
    Because of the way the new tab might be opened from within a
    processActionViewIntent Runnable, we can't enter editing mode by simply
    listening for an ACTION_ASSIST intent from within BrowserApp, as we need to
    enter editing mode *after* the correct tab has already been opened and selected
    and BrowserApp doesn't get any hint on when that Runnable might have run.
    
    Instead, we introduce a new tab event, so we can trigger editing mode at the
    right time via the tab itself.
    
    MozReview-Commit-ID: 8Bvv5TXyhhI

--- mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
+++ mobile/android/base/java/org/mozilla/gecko/GeckoApp.java
@@ -1411,8 +1411,10 @@ public abstract class GeckoApp extends GeckoActivity
 
         final String passedUri = getIntentURI(intent);
 
-        final boolean isExternalURL = passedUri != null;
-        final boolean isAboutHomeURL = isExternalURL && AboutPages.isAboutHome(passedUri);
+        final boolean intentHasURL = passedUri != null;
+        final boolean isAboutHomeURL = intentHasURL && AboutPages.isAboutHome(passedUri);
+        final boolean isAssistIntent = Intent.ACTION_ASSIST.equals(action);
+        final boolean needsNewForegroundTab = intentHasURL || isAssistIntent;
 
         // Start migrating as early as possible, can do this in
         // parallel with Gecko load.
@@ -1438,14 +1440,16 @@ public abstract class GeckoApp extends GeckoActivity
             handleSelectTabIntent(intent);
         // External URLs and new tab from widget should always be loaded regardless of whether Gecko is
         // already running.
-        } else if (isExternalURL) {
+        } else if (needsNewForegroundTab) {
             // Restore tabs before opening an external URL so that the new tab
             // is animated properly.
             Tabs.getInstance().notifyListeners(null, Tabs.TabEvents.RESTORED);
             processActionViewIntent(new Runnable() {
                 @Override
                 public void run() {
-                    if (isAboutHomeURL) {
+                    if (isAssistIntent) {
+                        Tabs.getInstance().addTab(Tabs.LOADURL_START_EDITING | Tabs.LOADURL_EXTERNAL);
+                    } else if (isAboutHomeURL) {
                         // respect the user preferences for about:home from external intent calls
                         loadStartupTab(Tabs.LOADURL_NEW_TAB, action);
                     } else {
@@ -1780,6 +1784,8 @@ public abstract class GeckoApp extends GeckoActivity
                     Tabs.getInstance().loadUrlWithIntentExtras(url, intent, flags);
                 }
             });
+        } else if (Intent.ACTION_ASSIST.equals(action)) {
+            Tabs.getInstance().addTab(Tabs.LOADURL_START_EDITING | Tabs.LOADURL_EXTERNAL);
         } else if (ACTION_HOMESCREEN_SHORTCUT.equals(action)) {
             final GeckoBundle data = new GeckoBundle(2);
             data.putString("uri", uri);
