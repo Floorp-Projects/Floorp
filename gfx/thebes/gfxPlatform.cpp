96ca047f2b00d2e9ee218bdbd3a299428332c1ff
James Willcox <snorp@snorp.net>
2017-12-04 08:56:08 -0600

    Bug 1421313 - Remove TexturePoolOGL. r=jgilbert, a=jcristau
    MozReview-Commit-ID: ADjiygVoHg3

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -94,10 +94,6 @@
 #include "GLContextProvider.h"
 #include "mozilla/gfx/Logging.h"
 
-#ifdef MOZ_WIDGET_ANDROID
-#include "TexturePoolOGL.h"
-#endif
-
 #ifdef USE_SKIA
 # ifdef __GNUC__
 #  pragma GCC diagnostic push
@@ -828,11 +824,6 @@ gfxPlatform::Init()
 
     GLContext::PlatformStartup();
 
-#ifdef MOZ_WIDGET_ANDROID
-    // Texture pool init
-    TexturePoolOGL::Init();
-#endif
-
     Preferences::RegisterCallbackAndCall(RecordingPrefChanged, "gfx.2d.recording");
 
     CreateCMSOutputProfile();
@@ -984,11 +975,6 @@ gfxPlatform::Shutdown()
     }
 
     gPlatform->mVsyncSource = nullptr;
-
-#ifdef MOZ_WIDGET_ANDROID
-    // Shut down the texture pool
-    TexturePoolOGL::Shutdown();
-#endif
 
     // Shut down the default GL context provider.
     GLContextProvider::Shutdown();
