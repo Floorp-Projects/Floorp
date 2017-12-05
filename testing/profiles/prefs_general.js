6ec796c190c063fcf48acdf13dd131eb0809228e
Noemi Erli <nerli@mozilla.com>
2017-12-05 12:18:33 +0200

    Backed out changeset e8f2d18830de (bug 1415536) for failing xpcshell netwerk/test/unit/test_network_activity.js  at least on Android r=backout on a CLOSED TREE

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -160,7 +160,7 @@ user_pref("dom.use_xbl_scopes_for_remote_xul", true);
 
 user_pref("captivedetect.canonicalURL", "http://%(server)s/captive-detect/success.txt");
 // Get network events.
-user_pref("network.activity.intervalMilliseconds", 5000);
+user_pref("network.activity.blipIntervalMilliseconds", 250);
 
 // We do not wish to display datareporting policy notifications as it might
 // cause other tests to fail. Tests that wish to test the notification functionality
