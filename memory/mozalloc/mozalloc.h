cefc0cb9e6197685b8fe0edcc043cd37fa8c83a0
Mike Hommey <mh+mozilla@glandium.org>
2017-12-05 17:34:19 +0900

    Bug 1423114 - Remove moz_xposix_memalign and moz_xvalloc. r=njn
    They are both infallible wrappers of posix_memalign and valloc.
    There is also moz_xmemalign, which wraps memalign, which is mostly
    always available as of bug 1402647.
    
    None of them are actually used, but it's still desirable to at least
    have one infallible variant, so keep moz_xmemalign and remove the other
    two.
    
    While here, we actually make both memalign and moz_xmemalign always
    available.

--- memory/mozalloc/mozalloc.h
+++ memory/mozalloc/mozalloc.h
@@ -97,24 +97,8 @@ MFBT_API char* moz_xstrndup(const char* str, size_t strsize)
     MOZ_ALLOCATOR;
 #endif /* if defined(HAVE_STRNDUP) */
 
-
-#if defined(HAVE_POSIX_MEMALIGN)
-MFBT_API MOZ_MUST_USE
-int moz_xposix_memalign(void **ptr, size_t alignment, size_t size);
-#endif /* if defined(HAVE_POSIX_MEMALIGN) */
-
-
-#if defined(HAVE_MEMALIGN)
 MFBT_API void* moz_xmemalign(size_t boundary, size_t size)
     MOZ_ALLOCATOR;
-#endif /* if defined(HAVE_MEMALIGN) */
-
-
-#if defined(HAVE_VALLOC)
-MFBT_API void* moz_xvalloc(size_t size)
-    MOZ_ALLOCATOR;
-#endif /* if defined(HAVE_VALLOC) */
-
 
 MOZ_END_EXTERN_C
 
