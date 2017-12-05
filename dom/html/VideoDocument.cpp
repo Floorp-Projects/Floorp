4cdf24258a4104a78b35456ebd4a987a19a1dadf
bechen@mozilla.com <bechen@mozilla.com>
2017-12-05 15:32:12 +0800

    Bug 1421004 - Break the mDocument reference cycle at Destroy(). r=jwwang,smaug
    We break the cycle reference between VideoDocument/Plugindocument and MediaDocumentStreamListener when the document be destroyed.
    
    MozReview-Commit-ID: Aowla95zMgg

--- dom/html/VideoDocument.cpp
+++ dom/html/VideoDocument.cpp
@@ -28,6 +28,14 @@ public:
                                      nsIContentSink*     aSink = nullptr);
   virtual void SetScriptGlobalObject(nsIScriptGlobalObject* aScriptGlobalObject);
 
+  virtual void Destroy()
+  {
+    if (mStreamListener) {
+      mStreamListener->DropDocumentRef();
+    }
+    MediaDocument::Destroy();
+  }
+
 protected:
 
   // Sets document <title> to reflect the file name and description.
