93cfe593a8487411779f64f2beb7934b6eee86f0
James Cheng <jacheng@mozilla.com>
2017-12-06 14:41:38 +0800

    Bug 1422669 - Part3 - Introduce rlz_lib::GetMachineId and its related helper functions. r=cpearce
    Chromium uses rlz_lib::GetMachineId as a factor when calculating storage id.
    
    MozReview-Commit-ID: AJbcnRXzi3m

--- dom/media/gmp/rlz/lib/machine_id.h
+++ dom/media/gmp/rlz/lib/machine_id.h
@@ -5,14 +5,28 @@
 #ifndef RLZ_LIB_MACHINE_ID_H_
 #define RLZ_LIB_MACHINE_ID_H_
 
+#include <string>
 #include <vector>
 
 namespace rlz_lib {
+
+// Gets the unique ID for the machine used for RLZ tracking purposes. On
+// Windows, this ID is derived from the Windows machine SID, and is the string
+// representation of a 20 byte hash + 4 bytes volum id + a 1 byte checksum.
+// Included in financial pings with events, unless explicitly forbidden by the
+// calling application.
+bool GetMachineId(std::string* machine_id);
 
 // Retrieves a raw machine identifier string and a machine-specific
 // 4 byte value. GetMachineId() will SHA1 |data|, append |more_data|, compute
 // the Crc8 of that, and return a hex-encoded string of that data.
 bool GetRawMachineId(std::vector<uint8_t>* data, int* more_data);
+
+namespace testing {
+bool GetMachineIdImpl(const base::string16& sid_string,
+                      int volume_id,
+                      std::string* machine_id);
+}  // namespace testing
 
 }  // namespace rlz_lib
 
