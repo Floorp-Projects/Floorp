7529e18cce52823d9a9b983f95a3b2ca262a014d
Eitan Isaacson <eitan@monotonous.org>
2017-12-06 19:10:42 +0100

    Bug 1331696 - P2. make the speech synth mochitests pass. r=jya
    MozReview-Commit-ID: 5fzgFZa8gcT
    
    --HG--
    extra : rebase_source : 7ba5a9de4aa60dcbae998a8a0a7b3974a7822687

--- dom/media/webspeech/synth/SpeechSynthesis.cpp
+++ dom/media/webspeech/synth/SpeechSynthesis.cpp
@@ -209,7 +209,7 @@ SpeechSynthesis::Pause()
   }
 
   if (mCurrentTask && !mSpeechQueue.IsEmpty() &&
-      mSpeechQueue.ElementAt(0)->GetState() != SpeechSynthesisUtterance::STATE_ENDED) {
+      mSpeechQueue.ElementAt(0)->GetState() == SpeechSynthesisUtterance::STATE_SPEAKING) {
     mCurrentTask->Pause();
   } else {
     mHoldQueue = true;
@@ -223,10 +223,11 @@ SpeechSynthesis::Resume()
     return;
   }
 
+  mHoldQueue = false;
+
   if (mCurrentTask) {
     mCurrentTask->Resume();
   } else {
-    mHoldQueue = false;
     AdvanceQueue();
   }
 }
