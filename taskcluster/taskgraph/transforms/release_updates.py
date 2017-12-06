c4ca9866dc584477bf4b3ff34f586d5533094deb
Aki Sasaki <asasaki@mozilla.com>
2017-12-05 19:35:45 -0800

    bug 1423081 - desktop release support in transforms/task.py r=callek
    - update notification scheme
    - update shipping phases to build, promote, push, ship
    - add binary transparency support
    
    MozReview-Commit-ID: DVQLKep77LM

--- taskcluster/taskgraph/transforms/release_updates.py
+++ taskcluster/taskgraph/transforms/release_updates.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
-Transform the beetmover task into an actual task description.
+Transform the update generation task into an actual task description.
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
