c8b9b9470ddacd992e45834a115c178da4a48150
jacheng@mozilla.com <jacheng@mozilla.com>
2017-12-06 06:33:02 +0000

    Bug 1422669 - Part1 - Restore librlz from Bug 1332530. r=cpearce
    Sync with the upstream(Chromium src/rlz/) and try to revert the functionality of Bug 1332530.
    rlz is linked into xul.
    
    MozReview-Commit-ID: HsjnBRPnifh

---
+++ dom/media/gmp/rlz/lib/machine_id.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RLZ_LIB_MACHINE_ID_H_
+#define RLZ_LIB_MACHINE_ID_H_
+
+#include <vector>
+
+namespace rlz_lib {
+
+// Retrieves a raw machine identifier string and a machine-specific
+// 4 byte value. GetMachineId() will SHA1 |data|, append |more_data|, compute
+// the Crc8 of that, and return a hex-encoded string of that data.
+bool GetRawMachineId(std::vector<uint8_t>* data, int* more_data);
+
+}  // namespace rlz_lib
+
+#endif  // RLZ_LIB_MACHINE_ID_H_
