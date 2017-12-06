26c1676e40ebe6c9dc93e8eeb1bd3d552dbb710f
James Willcox <snorp@snorp.net>
2017-12-06 09:52:30 -0600

    Back out bug 1421313 because of build breakage on a CLOSED TREE r=me
    MozReview-Commit-ID: HhNuCAUDU59

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -92,6 +92,10 @@
 #include "GLContextProvider.h"
 #include "mozilla/gfx/Logging.h"
 
+#ifdef MOZ_WIDGET_ANDROID
+#include "TexturePoolOGL.h"
+#endif
+
 #ifdef USE_SKIA
 # ifdef __GNUC__
 #  pragma GCC diagnostic push
@@ -816,6 +820,11 @@ gfxPlatform::Init()
 
     GLContext::PlatformStartup();
 
+#ifdef MOZ_WIDGET_ANDROID
+    // Texture pool init
+    TexturePoolOGL::Init();
+#endif
+
     Preferences::RegisterCallbackAndCall(RecordingPrefChanged, "gfx.2d.recording");
 
     CreateCMSOutputProfile();
@@ -967,6 +976,11 @@ gfxPlatform::Shutdown()
     }
 
     gPlatform->mVsyncSource = nullptr;
+
+#ifdef MOZ_WIDGET_ANDROID
+    // Shut down the texture pool
+    TexturePoolOGL::Shutdown();
+#endif
 
     // Shut down the default GL context provider.
     GLContextProvider::Shutdown();
