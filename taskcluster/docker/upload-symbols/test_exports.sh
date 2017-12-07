d9b3a9517e3a561d96c333b131f0f4e18efe7728
Ted Mielczarek <ted@mielczarek.org>
2017-12-07 08:59:54 -0500

    bug 1422740 - Remove the upload-symbols Docker image files. r=gps
    This Docker image was never built in-tree like the other images we use
    because it required a Socorro auth token, but the definition was put in
    the tree just to make it easy to find. Now that we're no longer using it
    we don't need it here anymore.
    
    MozReview-Commit-ID: KEEtSIIpc1x
    
    --HG--
    extra : rebase_source : e31d3adbe8550a2ecf503e24df36ea511bc2e9f4

--- taskcluster/docker/upload-symbols/test_exports.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-export SOCORRO_SYMBOL_UPLOAD_TOKEN_FILE=./socorro_token
-export ARTIFACT_TASKID=Hh5vLCaTRRO8Ql9X6XBdxg
-export GECKO_HEAD_REV=beed30cce69bc9783d417d3d29ce2c44989961ed
-export GECKO_HEAD_REPOSITORY=https://hg.mozilla.org/try/
-export SCRIPT_PATH=toolkit/crashreporter/tools/upload_symbols.py
