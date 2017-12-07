19046f77643c3890854110f4c9dc9d5fdc2af289
Michael Comella <michael.l.comella@gmail.com>
2017-12-06 17:25:00 -0800

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/1935: Add learn more button to add search engine.
    The URL is:
    https://support.mozilla.org/1/mobile/4.0/Android/en-US/add-search-engine
    
    antlam wanted the padding to change between items but hasn't given the specs
    yet so we can do that later.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/ManualAddSearchEngineSettingsFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/ManualAddSearchEngineSettingsFragment.java
@@ -4,20 +4,24 @@
 
 package org.mozilla.focus.settings;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-
 import org.mozilla.focus.R;
+import org.mozilla.focus.activity.InfoActivity;
 import org.mozilla.focus.search.ManualAddSearchEnginePreference;
 import org.mozilla.focus.search.SearchEngineManager;
 import org.mozilla.focus.telemetry.TelemetryWrapper;
+import org.mozilla.focus.utils.SupportUtils;
 
 public class ManualAddSearchEngineSettingsFragment extends SettingsFragment {
     @Override
@@ -42,6 +46,15 @@ public class ManualAddSearchEngineSettingsFragment extends SettingsFragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.learn_more:
+                final Context context = getActivity();
+                final String url = SupportUtils.getSumoURLForTopic(context, "add-search-engine");
+
+                final String title = ((AppCompatActivity) getActivity()).getSupportActionBar().getTitle().toString();
+                final Intent intent = InfoActivity.getIntentFor(context, url, title);
+                context.startActivity(intent);
+                return true;
+
             case R.id.menu_save_search_engine:
                 final View rootView = getView();
                 final String engineName = ((EditText) rootView.findViewById(R.id.edit_engine_name)).getText().toString();
