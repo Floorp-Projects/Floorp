a7e5c3516458b884ece3ac93de5bd5017c256610
Michael Comella <michael.l.comella@gmail.com>
2017-12-07 16:35:23 -0800

    [focus] Issue https://github.com/mozilla-mobile/focus-android/issues/1936: Validate add search engine URL with request.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/ManualAddSearchEngineSettingsFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/settings/ManualAddSearchEngineSettingsFragment.java
@@ -7,10 +7,14 @@ package org.mozilla.focus.settings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.WorkerThread;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -22,8 +26,21 @@ import org.mozilla.focus.search.ManualAddSearchEnginePreference;
 import org.mozilla.focus.search.SearchEngineManager;
 import org.mozilla.focus.telemetry.TelemetryWrapper;
 import org.mozilla.focus.utils.SupportUtils;
+import org.mozilla.focus.utils.UrlUtils;
+import org.mozilla.focus.utils.ViewUtils;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ManualAddSearchEngineSettingsFragment extends SettingsFragment {
+    private static String LOGTAG = "ManualAddSearchEngine";
+
+    // Set so the user doesn't have to wait *too* long. It's used twice: once for connecting and once for reading.
+    private static int SEARCH_QUERY_VALIDATION_TIMEOUT_MILLIS = 4000;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,18 +82,101 @@ public class ManualAddSearchEngineSettingsFragment extends SettingsFragment {
                 final ManualAddSearchEnginePreference pref = (ManualAddSearchEnginePreference) findPreference(getString(R.string.pref_key_manual_add_search_engine));
                 final boolean engineValid = pref.validateEngineNameAndShowError(engineName);
                 final boolean searchValid = pref.validateSearchQueryAndShowError(searchQuery);
+                final boolean isPartialSuccess = engineValid && searchValid;
 
-                final boolean isSuccess = engineValid && searchValid;
-                if (isSuccess) {
-                    final SharedPreferences sharedPreferences = getSearchEngineSharedPreferences();
-                    SearchEngineManager.addSearchEngine(sharedPreferences, getActivity(), engineName, searchQuery);
-                    Snackbar.make(rootView, R.string.search_add_confirmation, Snackbar.LENGTH_SHORT).show();
-                    getFragmentManager().popBackStack();
+                if (isPartialSuccess) {
+                    // TODO: add spinner, disable button until async task completes.
+                    // Hide the keyboard because:
+                    // - It's awkward to show the keyboard while waiting for a response
+                    // - We want it hidden when we return to the previous screen (on success)
+                    // - An expanded keyboard hides the success snackbar
+                    ViewUtils.hideKeyboard(rootView);
+                    new ValidateSearchEngineAsyncTask(this, engineName, searchQuery).execute();
+                } else {
+                    TelemetryWrapper.saveCustomSearchEngineEvent(false);
                 }
-                TelemetryWrapper.saveCustomSearchEngineEvent(isSuccess);
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private static class ValidateSearchEngineAsyncTask extends AsyncTask<Void, Void, Boolean> {
+        private final WeakReference<ManualAddSearchEngineSettingsFragment> thatWeakReference;
+        private final String engineName;
+        private final String query;
+
+        private ValidateSearchEngineAsyncTask(final ManualAddSearchEngineSettingsFragment that, final String engineName,
+                final String query) {
+            this.thatWeakReference = new WeakReference<>(that);
+            this.engineName = engineName;
+            this.query = query;
+        }
+
+        @Override
+        protected Boolean doInBackground(final Void... voids) {
+            final boolean isValidSearchQuery = isValidSearchQueryURL(query);
+            TelemetryWrapper.saveCustomSearchEngineEvent(isValidSearchQuery);
+            return isValidSearchQuery;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean isValidSearchQuery) {
+            super.onPostExecute(isValidSearchQuery);
+
+            final ManualAddSearchEngineSettingsFragment that = thatWeakReference.get();
+            if (that == null) {
+                Log.d(LOGTAG, "Fragment no longer exists when search query validation async task returned.");
+                return;
+            }
+
+            if (isValidSearchQuery) {
+                final SharedPreferences sharedPreferences = that.getSearchEngineSharedPreferences();
+                SearchEngineManager.addSearchEngine(sharedPreferences, that.getActivity(), engineName, query);
+                Snackbar.make(that.getView(), R.string.search_add_confirmation, Snackbar.LENGTH_SHORT).show();
+                that.getFragmentManager().popBackStack();
+            } else {
+                // TODO: show error.
+            }
+        }
+    }
+
+    @WorkerThread // makes network request.
+    private static boolean isValidSearchQueryURL(final String query) {
+        // TODO: we should share the code to substitute and normalize the search string (see SearchEngine.buildSearchUrl).
+        final String encodedTestQuery = Uri.encode("test");
+        final String normalizedSearchURLStr = UrlUtils.normalize(query);
+        final String searchURLStr = normalizedSearchURLStr.replaceAll("%s", encodedTestQuery);
+
+        final URL searchURL;
+        try {
+            searchURL = new URL(searchURLStr);
+        } catch (final MalformedURLException e) {
+            // Don't log exception to avoid leaking URL.
+            Log.d(LOGTAG, "Malformed URL: returning invalid search query");
+            return false;
+        }
+
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) searchURL.openConnection();
+            connection.setConnectTimeout(SEARCH_QUERY_VALIDATION_TIMEOUT_MILLIS);
+            connection.setReadTimeout(SEARCH_QUERY_VALIDATION_TIMEOUT_MILLIS);
+
+            // Checking for 200 is not perfect - what if a search engine redirects us? -
+            // but we're in a rush and it's good enough for now. One notable case this fails:
+            // if sites redirect to https, e.g. "duckduckgo.com/?q=%s" redirects to https and thus fails.
+            return connection.getResponseCode() == 200;
+
+        } catch (final IOException e) {
+            // Don't log exception to avoid leaking URL.
+            Log.d(LOGTAG, "Failure to get response code from server: returning invalid search query");
+            return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 }
