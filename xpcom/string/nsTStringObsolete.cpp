70d5608b9980abb43b21041f4d6b5f07085c09cb
Eric Rahm <erahm@mozilla.com>
2017-12-08 11:17:55 -0800

    Bug 1424120 - Part 7: Remove duplicated switch logic from ToInteger. r=njn
    --HG--
    extra : rebase_source : 039af2cc615cd3d3ad990c77e42847f1189e4625

--- xpcom/string/nsTStringObsolete.cpp
+++ xpcom/string/nsTStringObsolete.cpp
@@ -147,8 +147,6 @@ ToIntegerCommon(const nsTString<T>& aSrc,
     switch(*cp++) {
       case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
       case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-        done=true;
-        break;
       case '0': case '1': case '2': case '3': case '4':
       case '5': case '6': case '7': case '8': case '9':
         done=true;
