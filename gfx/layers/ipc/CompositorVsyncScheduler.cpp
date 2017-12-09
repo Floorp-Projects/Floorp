7435a5236224e22b48fcd1eac8fd763c5ea03354
Daosheng Mu <daoshengmu@gmail.com>
2017-12-09 14:46:14 +0800

    Bug 1420940 - Part 2: Skip the VR task when running at other threads instead of canceling the task. r=dvander
    MozReview-Commit-ID: 3axm3h7yMij

--- gfx/layers/ipc/CompositorVsyncScheduler.cpp
+++ gfx/layers/ipc/CompositorVsyncScheduler.cpp
@@ -117,7 +117,6 @@ CompositorVsyncScheduler::Destroy()
 
   CancelCurrentSetNeedsCompositeTask();
   CancelCurrentCompositeTask();
-  CancelCurrentVRTask();
 }
 
 void
@@ -141,13 +140,12 @@ CompositorVsyncScheduler::PostVRTask(TimeStamp aTimestamp)
 {
   MonitorAutoLock lockVR(mCurrentVRListenerTaskMonitor);
   if (mCurrentVRListenerTask == nullptr && VRListenerThreadHolder::Loop()) {
-    RefPtr<CancelableRunnable> task = NewCancelableRunnableMethod<TimeStamp>(
+    RefPtr<Runnable> task = NewRunnableMethod<TimeStamp>(
       "layers::CompositorVsyncScheduler::DispatchVREvents",
       this,
       &CompositorVsyncScheduler::DispatchVREvents,
       aTimestamp);
     mCurrentVRListenerTask = task;
-    MOZ_ASSERT(VRListenerThreadHolder::Loop());
     VRListenerThreadHolder::Loop()->PostDelayedTask(Move(task.forget()), 0);
   }
 }
@@ -251,18 +249,6 @@ CompositorVsyncScheduler::CancelCurrentCompositeTask()
 }
 
 void
-CompositorVsyncScheduler::CancelCurrentVRTask()
-{
-  // This function is only called by CompositorVsyncScheduler::Destroy().
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  MonitorAutoLock lockVR(mCurrentVRListenerTaskMonitor);
-  if (mCurrentVRListenerTask) {
-    mCurrentVRListenerTask->Cancel();
-    mCurrentVRListenerTask = nullptr;
-  }
-}
-
-void
 CompositorVsyncScheduler::Composite(TimeStamp aVsyncTimestamp)
 {
   MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
@@ -359,10 +345,15 @@ CompositorVsyncScheduler::DispatchTouchEvents(TimeStamp aVsyncTimestamp)
 void
 CompositorVsyncScheduler::DispatchVREvents(TimeStamp aVsyncTimestamp)
 {
-  MOZ_ASSERT(VRListenerThreadHolder::IsInVRListenerThread());
   {
     MonitorAutoLock lock(mCurrentVRListenerTaskMonitor);
     mCurrentVRListenerTask = nullptr;
+  }
+  // This only allows to be called by CompositorVsyncScheduler::PostVRTask()
+  // When the process is going to shutdown, the runnable has chance to be executed
+  // by other threads, we only want it to be run at VRListenerThread.
+  if (!VRListenerThreadHolder::IsInVRListenerThread()) {
+    return;
   }
 
   VRManager* vm = VRManager::Get();
