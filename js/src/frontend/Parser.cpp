0dea96b3980ea194d2bf34fb4d66e644b3298d6f
Jeff Walden <jwalden@mit.edu>
2017-12-12 15:20:57 -0600

    Bug 1424946 - Move GeneralParser::noteDestructuredPositionalFormalParameter into a baser class.  r=arai

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -1182,10 +1182,9 @@ GeneralParser<ParseHandler, CharT>::notePositionalFormalParameter(Node fn, Handl
     return true;
 }
 
-template <class ParseHandler, typename CharT>
+template <class ParseHandler>
 bool
-GeneralParser<ParseHandler, CharT>::noteDestructuredPositionalFormalParameter(Node fn,
-                                                                              Node destruct)
+PerHandlerParser<ParseHandler>::noteDestructuredPositionalFormalParameter(Node fn, Node destruct)
 {
     // Append an empty name to the positional formals vector to keep track of
     // argument slots when making FunctionScope::Data.
