63915cbabd18f5157e0a5bbc7befb6607ddce56c
Ben Kelly <ben@wanderview.com>
2017-12-12 15:44:47 -0500

    Bug 1293277 P2 Cleanup test_openWindow.html mochitest service worker a bit. r=baku

--- dom/workers/test/serviceworkers/openWindow_worker.js
+++ dom/workers/test/serviceworkers/openWindow_worker.js
@@ -60,9 +60,9 @@ onmessage = function(event) {
     promises.push(testForUrl("about:blank", "TypeError", null, results));
     promises.push(testForUrl("http://example.com", "InvalidAccessError", null, results));
     promises.push(testForUrl("_._*`InvalidURL", "InvalidAccessError", null, results));
-    Promise.all(promises).then(function(e) {
+    event.waitUntil(Promise.all(promises).then(function(e) {
       client.postMessage(results);
-    });
+    }));
   }
   if (event.data == "NEW_WINDOW") {
     window_count += 1;
@@ -72,7 +72,7 @@ onmessage = function(event) {
   }
 
   if (event.data == "CHECK_NUMBER_OF_WINDOWS") {
-    got_all_windows.then(function() {
+    event.waitUntil(got_all_windows.then(function() {
       return clients.matchAll();
     }).then(function(cl) {
       event.source.postMessage({result: cl.length == expected_window_count,
@@ -80,7 +80,7 @@ onmessage = function(event) {
       for (i = 0; i < cl.length; i++) {
         cl[i].postMessage("CLOSE");
       }
-    });
+    }));
   }
 }
 
@@ -101,16 +101,16 @@ onnotificationclick = function(e) {
 
   // redirect tests
   promises.push(testForUrl(redirect + "open_window/client.html", null,
-			   {url: same_origin}, results));
+                           {url: same_origin}, results));
   promises.push(testForUrl(redirect + different_origin, null, null, results));
 
   promises.push(testForUrl(redirect_xorigin + "open_window/client.html", null,
-			   null, results));
+                           null, results));
   promises.push(testForUrl(redirect_xorigin + same_origin, null,
-			   {url: same_origin}, results));
+                           {url: same_origin}, results));
 
-  Promise.all(promises).then(function(e) {
+  e.waitUntil(Promise.all(promises).then(function(e) {
     client.postMessage(results);
-  });
+  }));
 }
 
