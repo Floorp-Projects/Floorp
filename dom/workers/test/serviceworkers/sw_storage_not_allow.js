86c70b5f89f58d439ca77623f2f93f361e5e0581
Ben Kelly <ben@wanderview.com>
2017-12-13 09:58:26 -0500

    Bug 1424943 Fix test_error_reporting.html not to race network vs Clients API. r=tt

--- dom/workers/test/serviceworkers/sw_storage_not_allow.js
+++ dom/workers/test/serviceworkers/sw_storage_not_allow.js
@@ -1,16 +1,29 @@
 let clientId;
 addEventListener('fetch', function(event) {
-  if (event.request.url.includes('getClients')) {
-    // Excepted to fail since the storage access is not allowed.
-    self.clients.matchAll();
-  } else if (event.request.url.includes('getClient-stage1')) {
-    self.clients.matchAll().then(function(clients) {
+  event.respondWith(async function() {
+    if (event.request.url.includes('getClients')) {
+      // Expected to fail since the storage access is not allowed.
+      try {
+        await self.clients.matchAll();
+      } catch (e) {
+        // expected failure
+      }
+    } else if (event.request.url.includes('getClient-stage1')) {
+      let clients = await self.clients.matchAll();
       clientId = clients[0].id;
-    });
-  } else if (event.request.url.includes('getClient-stage2')) {
-    // Excepted to fail since the storage access is not allowed.
-    self.clients.get(clientId);
-  }
+    } else if (event.request.url.includes('getClient-stage2')) {
+      // Expected to fail since the storage access is not allowed.
+      try {
+        await self.clients.get(clientId);
+      } catch(e) {
+        // expected failure
+      }
+    }
+
+    // Pass through the network request once our various Clients API
+    // promises have completed.
+    return await fetch(event.request);
+  }());
 });
 
 addEventListener('message', function(event) {
