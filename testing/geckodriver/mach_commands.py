9ca0637e63acae2cf249c391d11125a2a47a9316
Cosmin Sabou <csabou@mozilla.com>
2017-12-13 18:05:32 +0200

    Backed out 2 changesets (bug 1395933) for build bustages on js/src/jit-test/tests/typedobject/bug-1415313.js r=backout on a CLOSED TREE
    Backed out changeset 81228d9e87e5 (bug 1395933)
    Backed out changeset 7827a7002f94 (bug 1395933)

--- testing/geckodriver/mach_commands.py
+++ testing/geckodriver/mach_commands.py
@@ -1,53 +1,40 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from __future__ import absolute_import, print_function, unicode_literals
-
 import argparse
 import os
 
+from mozbuild.base import (
+    MachCommandBase
+)
+
 from mach.decorators import (
-    Command,
     CommandArgument,
     CommandArgumentGroup,
     CommandProvider,
-    SubCommand,
-)
-
-from mozbuild.base import (
-    MachCommandBase,
-    MachCommandConditions as conditions,
+    Command,
 )
 
 
 @CommandProvider
-class GeckoDriver(MachCommandBase):
+class RunGeckodriver(MachCommandBase):
+    """Run the compiled program."""
 
-    @Command("geckodriver",
-        category="post-build",
-        description="WebDriver implementation for Gecko",
-        conditions=[conditions.is_firefox])
-    def geckodriver(self):
-        pass
+    @Command('geckodriver', category='post-build',
+        description='Run the geckodriver WebDriver implementation')
+    @CommandArgument('--binary', type=str,
+        help='Firefox binary (defaults to the local build).')
+    @CommandArgument('params', nargs='...',
+        help='Command-line arguments to be passed through to the program.')
 
-    @SubCommand("geckodriver", "run",
-        description="Run geckodriver.")
-    @CommandArgument("--binary", type=str,
-        help="Firefox binary (defaults to the local build).")
-    @CommandArgument("params", nargs="...",
-        help="Flags to be passed through to geckodriver.")
-    @CommandArgumentGroup("debugging")
-    @CommandArgument("--debug", action="store_true", group="debugging",
-        help="Enable the debugger. Not specifying a --debugger option will result in the default debugger being used.")
-    @CommandArgument("--debugger", default=None, type=str, group="debugging",
-        help="Name of debugger to use.")
-    @CommandArgument("--debugger-args", default=None, metavar="params", type=str,
-        group="debugging",
-        help="Flags to pass to the debugger itself; split as the Bourne shell would.")
+    @CommandArgumentGroup('debugging')
+    @CommandArgument('--debug', action='store_true', group='debugging',
+        help='Enable the debugger. Not specifying a --debugger option will result in the default debugger being used.')
+    @CommandArgument('--debugger', default=None, type=str, group='debugging',
+        help='Name of debugger to use.')
+    @CommandArgument('--debugger-args', default=None, metavar='params', type=str,
+        group='debugging',
+        help='Command-line arguments to pass to the debugger itself; split as the Bourne shell would.')
     def run(self, binary, params, debug, debugger, debugger_args):
         try:
-            binpath = self.get_binary_path("geckodriver")
+            binpath = self.get_binary_path('geckodriver')
         except Exception as e:
                 print("It looks like geckodriver isn't built. "
                       "Add ac_add_options --enable-geckodrver to your mozconfig ",
@@ -61,12 +48,12 @@ class GeckoDriver(MachCommandBase):
             args.extend(params)
 
         if binary is None:
-            binary = self.get_binary_path("app")
+            binary = self.get_binary_path('app')
 
         args.extend(["--binary", binary])
 
         if debug or debugger or debugger_args:
-            if "INSIDE_EMACS" in os.environ:
+            if 'INSIDE_EMACS' in os.environ:
                 self.log_manager.terminal_handler.setLevel(logging.WARNING)
 
             import mozdebug
@@ -97,33 +84,3 @@ class GeckoDriver(MachCommandBase):
 
         return self.run_process(args=args, ensure_exit_code=False,
             pass_thru=True)
-
-    @SubCommand("geckodriver", "build",
-        description="Build geckodriver.")
-    @CommandArgument("-v", "--verbose", action="store_true",
-        help="Verbose output for what commands the build is running.")
-    def build(self, verbose=False):
-        from mozbuild.controller.building import BuildDriver
-
-        self.log_manager.enable_all_structured_loggers()
-
-        driver = self._spawn(BuildDriver)
-        return driver.build(
-            what=["testing/geckodriver"],
-            verbose=verbose,
-            mach_context=self._mach_context)
-
-    @SubCommand("geckodriver", "test",
-        description="Run geckodriver unit tests.")
-    @CommandArgument("-v", "--verbose", action="store_true",
-        help="Verbose output for what commands the build is running.")
-    def test(self, verbose=False):
-        from mozbuild.controller.building import BuildDriver
-
-        self.log_manager.enable_all_structured_loggers()
-
-        driver = self._spawn(BuildDriver)
-        return driver.build(
-            what=["testing/geckodriver/check"],
-            verbose=verbose,
-            mach_context=self._mach_context)
