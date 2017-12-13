204dbeb0342aa05ac93e4054e88180eddf619cbc
Andrea Marchesini <amarchesini@mozilla.com>
2017-12-13 14:02:47 -0600

    Bug 1419771 - Introduce DOMPreferences, a thread-safe access to preferences for DOM - part 21 - Get rid of WorkerPrefs.h, r=asuth

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -4990,6 +4990,16 @@ template nsresult
 Preferences::AddAtomicBoolVarCache(Atomic<bool, Relaxed>*, const char*, bool);
 
 template nsresult
+Preferences::AddAtomicBoolVarCache(Atomic<bool, ReleaseAcquire>*,
+                                   const char*,
+                                   bool);
+
+template nsresult
+Preferences::AddAtomicBoolVarCache(Atomic<bool, SequentiallyConsistent>*,
+                                   const char*,
+                                   bool);
+
+template nsresult
 Preferences::AddAtomicIntVarCache(Atomic<int32_t, Relaxed>*,
                                   const char*,
                                   int32_t);
