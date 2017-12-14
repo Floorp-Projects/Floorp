cae5668a084d84a85df764628f79b58256f605f3
Chung-Sheng Fu <cfu@mozilla.com>
2017-12-14 15:59:57 -0600

    Bug 1413780 - Change permission name from canvas/extractData to canvas. r=johannh
    MozReview-Commit-ID: 40mesFLeRWU
    
    --HG--
    extra : rebase_source : d6dcab54875a8a557a75d63f25fc1b7844f54d21

--- browser/modules/test/unit/test_SitePermissions.js
+++ browser/modules/test/unit/test_SitePermissions.js
@@ -21,7 +21,7 @@ add_task(async function testPermissionsListing() {
   if (RESIST_FINGERPRINTING_ENABLED) {
     // Canvas permission should be hidden unless privacy.resistFingerprinting
     // is true.
-    expectedPermissions.push("canvas/extractData");
+    expectedPermissions.push("canvas");
   }
   Assert.deepEqual(SitePermissions.listPermissions().sort(), expectedPermissions.sort(),
     "Correct list of all permissions");
@@ -117,7 +117,7 @@ add_task(async function testExactHostMatch() {
   if (RESIST_FINGERPRINTING_ENABLED) {
     // Canvas permission should be hidden unless privacy.resistFingerprinting
     // is true.
-    exactHostMatched.push("canvas/extractData");
+    exactHostMatched.push("canvas");
   }
   let nonExactHostMatched = ["image", "cookie", "popup", "install", "shortcuts"];
 
@@ -203,19 +203,19 @@ add_task(async function testCanvasPermission() {
   let resistFingerprinting = Services.prefs.getBoolPref("privacy.resistFingerprinting", false);
   let uri = Services.io.newURI("https://example.com");
 
-  SitePermissions.set(uri, "canvas/extractData", SitePermissions.ALLOW);
+  SitePermissions.set(uri, "canvas", SitePermissions.ALLOW);
 
   // Canvas permission is hidden when privacy.resistFingerprinting is false.
   Services.prefs.setBoolPref("privacy.resistFingerprinting", false);
-  Assert.equal(SitePermissions.listPermissions().indexOf("canvas/extractData"), -1);
-  Assert.equal(SitePermissions.getAllByURI(uri).filter(permission => permission.id === "canvas/extractData").length, 0);
+  Assert.equal(SitePermissions.listPermissions().indexOf("canvas"), -1);
+  Assert.equal(SitePermissions.getAllByURI(uri).filter(permission => permission.id === "canvas").length, 0);
 
   // Canvas permission is visible when privacy.resistFingerprinting is true.
   Services.prefs.setBoolPref("privacy.resistFingerprinting", true);
-  Assert.notEqual(SitePermissions.listPermissions().indexOf("canvas/extractData"), -1);
-  Assert.notEqual(SitePermissions.getAllByURI(uri).filter(permission => permission.id === "canvas/extractData").length, 0);
+  Assert.notEqual(SitePermissions.listPermissions().indexOf("canvas"), -1);
+  Assert.notEqual(SitePermissions.getAllByURI(uri).filter(permission => permission.id === "canvas").length, 0);
 
-  SitePermissions.remove(uri, "canvas/extractData");
+  SitePermissions.remove(uri, "canvas");
   Services.prefs.setBoolPref("privacy.resistFingerprinting", resistFingerprinting);
 });
 
