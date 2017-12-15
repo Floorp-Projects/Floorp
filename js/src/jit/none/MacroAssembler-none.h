2b76618a4348c905844787bb210d00451f380f54
Ted Campbell <tcampbell@mozilla.com>
2017-12-15 11:38:39 -0600

    Bug 1425413 - Fix non-jit platform MacroAssembler failure. r=jorendorff
    MozReview-Commit-ID: FR1eUPTkMsb

--- js/src/jit/none/MacroAssembler-none.h
+++ js/src/jit/none/MacroAssembler-none.h
@@ -88,7 +88,7 @@ static constexpr Register WasmTableCallIndexReg { Registers::invalid_reg };
 static constexpr Register WasmTlsReg { Registers::invalid_reg };
 
 static constexpr uint32_t ABIStackAlignment = 4;
-static constexpr uint32_t CodeAlignment = 4;
+static constexpr uint32_t CodeAlignment = sizeof(void*);
 static constexpr uint32_t JitStackAlignment = 8;
 static constexpr uint32_t JitStackValueAlignment = JitStackAlignment / sizeof(Value);
 
