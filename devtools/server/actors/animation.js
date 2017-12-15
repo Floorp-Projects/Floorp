3f2d7849aa8d43217416116fa96c5b0546d9e0fd
Brian Birtles <birtles@gmail.com>
2017-12-15 14:55:32 -0600

    Bug 1425548 - Update references to Web Animations spec in devtools; r=hiro
    MozReview-Commit-ID: DHFgDwh00eO

--- devtools/server/actors/animation.js
+++ devtools/server/actors/animation.js
@@ -20,7 +20,7 @@
  *
  * References:
  * - WebAnimation spec:
- *   http://w3c.github.io/web-animations/
+ *   http://drafts.csswg.org/web-animations/
  * - WebAnimation WebIDL files:
  *   /dom/webidl/Animation*.webidl
  */
