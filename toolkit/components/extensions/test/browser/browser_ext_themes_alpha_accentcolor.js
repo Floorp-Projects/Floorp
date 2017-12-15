7db18c24b8edc28cb542a381d53976bac918a166
Tim Nguyen <ntim.bugs@gmail.com>
2017-12-15 13:52:41 -0600

    Bug 1423626 - Sanitize accent color to ignore alpha channel. r=jaws
    MozReview-Commit-ID: IKo55lZ8b10
    
    --HG--
    extra : rebase_source : 426a3febe9a487c5261fdd789f3c51ac80d354a0

---
+++ toolkit/components/extensions/test/browser/browser_ext_themes_alpha_accentcolor.js
@@ -0,0 +1,30 @@
+"use strict";
+
+add_task(async function test_alpha_accentcolor() {
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "theme": {
+        "images": {
+          "headerURL": "image1.png",
+        },
+        "colors": {
+          "accentcolor": "rgba(230, 128, 0, 0.1)",
+          "textcolor": TEXT_COLOR,
+        },
+      },
+    },
+    files: {
+      "image1.png": BACKGROUND,
+    },
+  });
+
+  await extension.startup();
+
+  let docEl = window.document.documentElement;
+  let style = window.getComputedStyle(docEl);
+
+  Assert.equal(style.backgroundColor, "rgb(230, 128, 0)",
+    "Window background color should be opaque");
+
+  await extension.unload();
+});
