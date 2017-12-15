1115c3d05302dd6249db865e1821bea9c100748b
Robert Strong <robert.bugzilla@gmail.com>
2017-12-15 08:33:44 -0800

    Bug 1413727 - Removes JAWS token from the update url and removes JAWS checks from the update url test. r=mhowell

--- toolkit/modules/tests/xpcshell/test_UpdateUtils_url.js
+++ toolkit/modules/tests/xpcshell/test_UpdateUtils_url.js
@@ -332,11 +332,6 @@ add_task(async function test_custom() {
 add_task(async function test_systemCapabilities() {
   let url = URL_PREFIX + "%SYSTEM_CAPABILITIES%/";
   let systemCapabilities = "ISET:" + getInstructionSet() + ",MEM:" + getMemoryMB();
-  if (AppConstants.platform == "win") {
-    // The default value for shouldBlockIncompatJaws in the mock
-    // Services.appinfo is false so the value should be JAWS:0
-    systemCapabilities += ",JAWS:0";
-  }
   Assert.equal(await getResult(url), systemCapabilities,
                "the url param for %SYSTEM_CAPABILITIES%" + MSG_SHOULD_EQUAL);
 });
