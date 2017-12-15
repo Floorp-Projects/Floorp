e2e14c73d57bdb31d7f73b8e1729144a50dfee69
Jean-Yves Avenard <jyavenard@mozilla.com>
2017-12-15 18:14:02 +0100

    Bug 1404997 - P24. Make AutoTaskQueue deliver runnables to nsIEventTarget. r=gerald
    Similar to bug 1300118 for TaskQueue.
    
    MozReview-Commit-ID: 8jIifvs2r4U

--- dom/media/VideoUtils.h
+++ dom/media/VideoUtils.h
@@ -18,6 +18,7 @@
 #include "mozilla/MozPromise.h"
 #include "mozilla/ReentrantMonitor.h"
 #include "mozilla/RefPtr.h"
+#include "mozilla/SharedThreadPool.h"
 #include "mozilla/UniquePtr.h"
 #include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
@@ -184,8 +185,6 @@ private:
   T& mVar;
   const T mValue;
 };
-
-class SharedThreadPool;
 
 // The MediaDataDecoder API blocks, with implementations waiting on platform
 // decoder tasks.  These platform decoder tasks are queued on a separate
