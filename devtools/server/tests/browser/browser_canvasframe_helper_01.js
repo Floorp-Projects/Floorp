fb90d06c08aac630f1ce625017b2f3ad7161cbaf
Alexandre Poirot <poirot.alex@gmail.com>
2017-12-18 02:21:41 -0800

    Bug 1425818 - Stop using CPOW in devtools/server/tests/browser/browser_canvasframe_helper_*.js. r=pbro
    MozReview-Commit-ID: 70IEX3Q5iGa

--- devtools/server/tests/browser/browser_canvasframe_helper_01.js
+++ devtools/server/tests/browser/browser_canvasframe_helper_01.js
@@ -6,86 +6,84 @@
 
 // Simple CanvasFrameAnonymousContentHelper tests.
 
-// This makes sure the 'domnode' protocol actor type is known when importing
-// highlighter.
-require("devtools/server/actors/inspector");
-const {HighlighterEnvironment} = require("devtools/server/actors/highlighters");
-
-const {
-  CanvasFrameAnonymousContentHelper
-} = require("devtools/server/actors/highlighters/utils/markup");
-
 const TEST_URL = "data:text/html;charset=utf-8,CanvasFrameAnonymousContentHelper test";
 
-add_task(function* () {
-  let browser = yield addTab(TEST_URL);
-  // eslint-disable-next-line mozilla/no-cpows-in-tests
-  let doc = browser.contentDocument;
+add_task(async function () {
+  let browser = await addTab(TEST_URL);
 
-  let nodeBuilder = () => {
-    let root = doc.createElement("div");
-    let child = doc.createElement("div");
-    child.style = "width:200px;height:200px;background:red;";
-    child.id = "child-element";
-    child.className = "child-element";
-    child.textContent = "test element";
-    root.appendChild(child);
-    return root;
-  };
+  await ContentTask.spawn(browser, null, async function () {
+    const {require} = Cu.import("resource://devtools/shared/Loader.jsm", {});
+    const {HighlighterEnvironment} = require("devtools/server/actors/highlighters");
+    const {
+      CanvasFrameAnonymousContentHelper
+    } = require("devtools/server/actors/highlighters/utils/markup");
+    let doc = content.document;
 
-  info("Building the helper");
-  let env = new HighlighterEnvironment();
-  env.initFromWindow(doc.defaultView);
-  let helper = new CanvasFrameAnonymousContentHelper(env, nodeBuilder);
+    let nodeBuilder = () => {
+      let root = doc.createElement("div");
+      let child = doc.createElement("div");
+      child.style = "width:200px;height:200px;background:red;";
+      child.id = "child-element";
+      child.className = "child-element";
+      child.textContent = "test element";
+      root.appendChild(child);
+      return root;
+    };
 
-  ok(helper.content instanceof AnonymousContent,
-    "The helper owns the AnonymousContent object");
-  ok(helper.getTextContentForElement,
-    "The helper has the getTextContentForElement method");
-  ok(helper.setTextContentForElement,
-    "The helper has the setTextContentForElement method");
-  ok(helper.setAttributeForElement,
-    "The helper has the setAttributeForElement method");
-  ok(helper.getAttributeForElement,
-    "The helper has the getAttributeForElement method");
-  ok(helper.removeAttributeForElement,
-    "The helper has the removeAttributeForElement method");
-  ok(helper.addEventListenerForElement,
-    "The helper has the addEventListenerForElement method");
-  ok(helper.removeEventListenerForElement,
-    "The helper has the removeEventListenerForElement method");
-  ok(helper.getElement,
-    "The helper has the getElement method");
-  ok(helper.scaleRootElement,
-    "The helper has the scaleRootElement method");
+    info("Building the helper");
+    let env = new HighlighterEnvironment();
+    env.initFromWindow(doc.defaultView);
+    let helper = new CanvasFrameAnonymousContentHelper(env, nodeBuilder);
 
-  is(helper.getTextContentForElement("child-element"), "test element",
-    "The text content was retrieve correctly");
-  is(helper.getAttributeForElement("child-element", "id"), "child-element",
-    "The ID attribute was retrieve correctly");
-  is(helper.getAttributeForElement("child-element", "class"), "child-element",
-    "The class attribute was retrieve correctly");
+    ok(helper.content instanceof content.AnonymousContent,
+      "The helper owns the AnonymousContent object");
+    ok(helper.getTextContentForElement,
+      "The helper has the getTextContentForElement method");
+    ok(helper.setTextContentForElement,
+      "The helper has the setTextContentForElement method");
+    ok(helper.setAttributeForElement,
+      "The helper has the setAttributeForElement method");
+    ok(helper.getAttributeForElement,
+      "The helper has the getAttributeForElement method");
+    ok(helper.removeAttributeForElement,
+      "The helper has the removeAttributeForElement method");
+    ok(helper.addEventListenerForElement,
+      "The helper has the addEventListenerForElement method");
+    ok(helper.removeEventListenerForElement,
+      "The helper has the removeEventListenerForElement method");
+    ok(helper.getElement,
+      "The helper has the getElement method");
+    ok(helper.scaleRootElement,
+      "The helper has the scaleRootElement method");
 
-  let el = helper.getElement("child-element");
-  ok(el, "The DOMNode-like element was created");
+    is(helper.getTextContentForElement("child-element"), "test element",
+      "The text content was retrieve correctly");
+    is(helper.getAttributeForElement("child-element", "id"), "child-element",
+      "The ID attribute was retrieve correctly");
+    is(helper.getAttributeForElement("child-element", "class"), "child-element",
+      "The class attribute was retrieve correctly");
 
-  is(el.getTextContent(), "test element",
-    "The text content was retrieve correctly");
-  is(el.getAttribute("id"), "child-element",
-    "The ID attribute was retrieve correctly");
-  is(el.getAttribute("class"), "child-element",
-    "The class attribute was retrieve correctly");
+    let el = helper.getElement("child-element");
+    ok(el, "The DOMNode-like element was created");
 
-  info("Destroying the helper");
-  helper.destroy();
-  env.destroy();
+    is(el.getTextContent(), "test element",
+      "The text content was retrieve correctly");
+    is(el.getAttribute("id"), "child-element",
+      "The ID attribute was retrieve correctly");
+    is(el.getAttribute("class"), "child-element",
+      "The class attribute was retrieve correctly");
 
-  ok(!helper.getTextContentForElement("child-element"),
-    "No text content was retrieved after the helper was destroyed");
-  ok(!helper.getAttributeForElement("child-element", "id"),
-    "No ID attribute was retrieved after the helper was destroyed");
-  ok(!helper.getAttributeForElement("child-element", "class"),
-    "No class attribute was retrieved after the helper was destroyed");
+    info("Destroying the helper");
+    helper.destroy();
+    env.destroy();
+
+    ok(!helper.getTextContentForElement("child-element"),
+      "No text content was retrieved after the helper was destroyed");
+    ok(!helper.getAttributeForElement("child-element", "id"),
+      "No ID attribute was retrieved after the helper was destroyed");
+    ok(!helper.getAttributeForElement("child-element", "class"),
+      "No class attribute was retrieved after the helper was destroyed");
+  });
 
   gBrowser.removeCurrentTab();
 });
--- devtools/server/tests/browser/browser_canvasframe_helper_03.js
+++ devtools/server/tests/browser/browser_canvasframe_helper_03.js
@@ -6,98 +6,108 @@
 
 // Test the CanvasFrameAnonymousContentHelper event handling mechanism.
 
-// This makes sure the 'domnode' protocol actor type is known when importing
-// highlighter.
-require("devtools/server/actors/inspector");
-
-const {HighlighterEnvironment} = require("devtools/server/actors/highlighters");
-
-const {
-  CanvasFrameAnonymousContentHelper
-} = require("devtools/server/actors/highlighters/utils/markup");
-
 const TEST_URL = "data:text/html;charset=utf-8,CanvasFrameAnonymousContentHelper test";
 
-add_task(function* () {
-  let browser = yield addTab(TEST_URL);
-  // eslint-disable-next-line mozilla/no-cpows-in-tests
-  let doc = browser.contentDocument;
+add_task(async function () {
+  let browser = await addTab(TEST_URL);
+  await ContentTask.spawn(browser, null, async function () {
+    const {require} = Cu.import("resource://devtools/shared/Loader.jsm", {});
+    const {HighlighterEnvironment} = require("devtools/server/actors/highlighters");
+    const {
+      CanvasFrameAnonymousContentHelper
+    } = require("devtools/server/actors/highlighters/utils/markup");
+    let doc = content.document;
 
-  let nodeBuilder = () => {
-    let root = doc.createElement("div");
-    let child = doc.createElement("div");
-    child.style = "pointer-events:auto;width:200px;height:200px;background:red;";
-    child.id = "child-element";
-    child.className = "child-element";
-    root.appendChild(child);
-    return root;
-  };
+    let nodeBuilder = () => {
+      let root = doc.createElement("div");
+      let child = doc.createElement("div");
+      child.style = "pointer-events:auto;width:200px;height:200px;background:red;";
+      child.id = "child-element";
+      child.className = "child-element";
+      root.appendChild(child);
+      return root;
+    };
 
-  info("Building the helper");
-  let env = new HighlighterEnvironment();
-  env.initFromWindow(doc.defaultView);
-  let helper = new CanvasFrameAnonymousContentHelper(env, nodeBuilder);
+    info("Building the helper");
+    let env = new HighlighterEnvironment();
+    env.initFromWindow(doc.defaultView);
+    let helper = new CanvasFrameAnonymousContentHelper(env, nodeBuilder);
 
-  let el = helper.getElement("child-element");
+    let el = helper.getElement("child-element");
 
-  info("Adding an event listener on the inserted element");
-  let mouseDownHandled = 0;
-  function onMouseDown(e, id) {
-    is(id, "child-element", "The mousedown event was triggered on the element");
-    ok(!e.originalTarget, "The originalTarget property isn't available");
-    mouseDownHandled++;
-  }
-  el.addEventListener("mousedown", onMouseDown);
+    info("Adding an event listener on the inserted element");
+    let mouseDownHandled = 0;
+    function onMouseDown(e, id) {
+      is(id, "child-element", "The mousedown event was triggered on the element");
+      ok(!e.originalTarget, "The originalTarget property isn't available");
+      mouseDownHandled++;
+    }
+    el.addEventListener("mousedown", onMouseDown);
 
-  info("Synthesizing an event on the inserted element");
-  let onDocMouseDown = once(doc, "mousedown");
-  synthesizeMouseDown(100, 100, doc.defaultView);
-  yield onDocMouseDown;
+    function once(target, event) {
+      return new Promise(done => {
+        target.addEventListener(event, done, { once: true });
+      });
+    }
 
-  is(mouseDownHandled, 1, "The mousedown event was handled once on the element");
+    info("Synthesizing an event on the inserted element");
+    let onDocMouseDown = once(doc, "mousedown");
+    synthesizeMouseDown(100, 100, doc.defaultView);
+    await onDocMouseDown;
 
-  info("Synthesizing an event somewhere else");
-  onDocMouseDown = once(doc, "mousedown");
-  synthesizeMouseDown(400, 400, doc.defaultView);
-  yield onDocMouseDown;
+    is(mouseDownHandled, 1, "The mousedown event was handled once on the element");
 
-  is(mouseDownHandled, 1, "The mousedown event was not handled on the element");
+    info("Synthesizing an event somewhere else");
+    onDocMouseDown = once(doc, "mousedown");
+    synthesizeMouseDown(400, 400, doc.defaultView);
+    await onDocMouseDown;
 
-  info("Removing the event listener");
-  el.removeEventListener("mousedown", onMouseDown);
+    is(mouseDownHandled, 1, "The mousedown event was not handled on the element");
 
-  info("Synthesizing another event after the listener has been removed");
-  // Using a document event listener to know when the event has been synthesized.
-  onDocMouseDown = once(doc, "mousedown");
-  synthesizeMouseDown(100, 100, doc.defaultView);
-  yield onDocMouseDown;
+    info("Removing the event listener");
+    el.removeEventListener("mousedown", onMouseDown);
 
-  is(mouseDownHandled, 1,
-    "The mousedown event hasn't been handled after the listener was removed");
+    info("Synthesizing another event after the listener has been removed");
+    // Using a document event listener to know when the event has been synthesized.
+    onDocMouseDown = once(doc, "mousedown");
+    synthesizeMouseDown(100, 100, doc.defaultView);
+    await onDocMouseDown;
 
-  info("Adding again the event listener");
-  el.addEventListener("mousedown", onMouseDown);
+    is(mouseDownHandled, 1,
+      "The mousedown event hasn't been handled after the listener was removed");
 
-  info("Destroying the helper");
-  env.destroy();
-  helper.destroy();
+    info("Adding again the event listener");
+    el.addEventListener("mousedown", onMouseDown);
 
-  info("Synthesizing another event after the helper has been destroyed");
-  // Using a document event listener to know when the event has been synthesized.
-  onDocMouseDown = once(doc, "mousedown");
-  synthesizeMouseDown(100, 100, doc.defaultView);
-  yield onDocMouseDown;
+    info("Destroying the helper");
+    env.destroy();
+    helper.destroy();
 
-  is(mouseDownHandled, 1,
-    "The mousedown event hasn't been handled after the helper was destroyed");
+    info("Synthesizing another event after the helper has been destroyed");
+    // Using a document event listener to know when the event has been synthesized.
+    onDocMouseDown = once(doc, "mousedown");
+    synthesizeMouseDown(100, 100, doc.defaultView);
+    await onDocMouseDown;
+
+    is(mouseDownHandled, 1,
+      "The mousedown event hasn't been handled after the helper was destroyed");
+
+    function synthesizeMouseDown(x, y, win) {
+      // We need to make sure the inserted anonymous content can be targeted by the
+      // event right after having been inserted, and so we need to force a sync
+      // reflow.
+      win.document.documentElement.offsetWidth;
+      // Minimal environment for EventUtils to work.
+      let EventUtils = {
+        window: content,
+        parent: content,
+        _EU_Ci: Components.interfaces,
+        _EU_Cc: Components.classes,
+      };
+      Services.scriptloader.loadSubScript("chrome://mochikit/content/tests/SimpleTest/EventUtils.js", EventUtils);
+      EventUtils.synthesizeMouseAtPoint(x, y, {type: "mousedown"}, win);
+    }
+  });
 
   gBrowser.removeCurrentTab();
 });
-
-function synthesizeMouseDown(x, y, win) {
-  // We need to make sure the inserted anonymous content can be targeted by the
-  // event right after having been inserted, and so we need to force a sync
-  // reflow.
-  win.document.documentElement.offsetWidth;
-  EventUtils.synthesizeMouseAtPoint(x, y, {type: "mousedown"}, win);
-}
