763255ed1874869b48dd43e527c318cae3a7e8bf
Aki Sasaki <asasaki@mozilla.com>
2017-12-18 11:26:19 -0800

    bug 1425985 - set MAX_DEPENDENCIES to 99. r=bhearsum
    `taskgraph.create` can add an additional dependency to tasks prior to task submission. The queue will error out if we submit a task with over 100 dependencies, so we should limit ourselves to 99 dependencies here.
    
    MozReview-Commit-ID: ClT0vjYBPp4

--- taskcluster/taskgraph/__init__.py
+++ taskcluster/taskgraph/__init__.py
@@ -10,7 +10,9 @@ GECKO = os.path.realpath(os.path.join(__file__, '..', '..', '..'))
 
 # Maximum number of dependencies a single task can have
 # https://docs.taskcluster.net/reference/platform/taskcluster-queue/references/api#createTask
-MAX_DEPENDENCIES = 100
+# specifies 100, but we also optionally add the decision task id as a dep in
+# taskgraph.create, so let's set this to 99.
+MAX_DEPENDENCIES = 99
 
 # Enable fast task generation for local debugging
 # This is normally switched on via the --fast/-F flag to `mach taskgraph`
