d7ceb5fcefbfaf53a84cd4d37edbc31ef65e5f2c
Ben Kelly <ben@wanderview.com>
2017-12-18 16:42:10 -0500

    Bug 1425614 P2 Make ClientManager and ClientHandle operation promises reject if called on a worker thread already shutting down. r=asuth

--- dom/clients/manager/ClientHandle.cpp
+++ dom/clients/manager/ClientHandle.cpp
@@ -57,6 +57,8 @@ ClientHandle::StartOp(const ClientOpConstructorArgs& aArgs)
       // Constructor failure will reject promise via ActorDestroy()
       return;
     }
+  }, [promise] {
+    promise->Reject(NS_ERROR_DOM_INVALID_STATE_ERR, __func__);
   });
 
   RefPtr<ClientOpPromise> ref = promise.get();
