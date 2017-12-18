ca61705b4347bd58adbdad6b727165f8dcf2ccbb
André Bargull <andre.bargull@gmail.com>
2017-12-18 08:54:56 -0800

    Bug 1415827 - Update in-tree ICU to release 60.2. rs=Waldo

--- intl/icu/source/data/locales/resfiles.mk
+++ intl/icu/source/data/locales/resfiles.mk
@@ -1,6 +1,6 @@
 # © 2016 and later: Unicode, Inc. and others.
 # License & terms of use: http://www.unicode.org/copyright.html#License
-GENRB_CLDR_VERSION = 32
+GENRB_CLDR_VERSION = 32.0.1
 # A list of txt's to build
 # Note:
 #
