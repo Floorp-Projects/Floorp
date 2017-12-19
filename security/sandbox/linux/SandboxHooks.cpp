0393c9235fd4991dbdae1bed3c0da6c5069f2fe2
Csoregi Natalia <ncsoregi@mozilla.com>
2017-12-19 02:49:50 +0200

    Backed out changeset 8a71f6e05783 (bug 1393287) for Hazard Build Bustage. r=backout on a CLOSED TREE

--- security/sandbox/linux/SandboxHooks.cpp
+++ security/sandbox/linux/SandboxHooks.cpp
@@ -4,52 +4,26 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "mozilla/Atomics.h"
 #include "mozilla/Types.h"
 
 #include <dlfcn.h>
 #include <signal.h>
 #include <errno.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/inotify.h>
 
 // Signal number used to enable seccomp on each thread.
-extern mozilla::Atomic<int> gSeccompTsyncBroadcastSignum;
-
-static bool
-SigSetNeedsFixup(const sigset_t* aSet)
-{
-  int tsyncSignum = gSeccompTsyncBroadcastSignum;
-
-  return aSet != nullptr &&
-         (sigismember(aSet, SIGSYS) ||
-          (tsyncSignum != 0 &&
-           sigismember(aSet, tsyncSignum)));
-}
-
-static void
-SigSetFixup(sigset_t* aSet)
-{
-  int tsyncSignum = gSeccompTsyncBroadcastSignum;
-  int rv = sigdelset(aSet, SIGSYS);
-  MOZ_RELEASE_ASSERT(rv == 0);
-  if (tsyncSignum != 0) {
-    rv = sigdelset(aSet, tsyncSignum);
-    MOZ_RELEASE_ASSERT(rv == 0);
-  }
-}
+extern int gSeccompTsyncBroadcastSignum;
 
 // This file defines a hook for sigprocmask() and pthread_sigmask().
 // Bug 1176099: some threads block SIGSYS signal which breaks our seccomp-bpf
 // sandbox. To avoid this, we intercept the call and remove SIGSYS.
 //
 // ENOSYS indicates an error within the hook function itself.
-static int
-HandleSigset(int (*aRealFunc)(int, const sigset_t*, sigset_t*),
-             int aHow, const sigset_t* aSet,
-             sigset_t* aOldSet, bool aUseErrno)
+static int HandleSigset(int (*aRealFunc)(int, const sigset_t*, sigset_t*),
+                        int aHow, const sigset_t* aSet,
+                        sigset_t* aOldSet, bool aUseErrno)
 {
   if (!aRealFunc) {
     if (aUseErrno) {
@@ -61,12 +35,22 @@ HandleSigset(int (*aRealFunc)(int, const sigset_t*, sigset_t*),
   }
 
   // Avoid unnecessary work
-  if (aHow == SIG_UNBLOCK || !SigSetNeedsFixup(aSet)) {
+  if (aSet == nullptr || aHow == SIG_UNBLOCK) {
     return aRealFunc(aHow, aSet, aOldSet);
   }
 
   sigset_t newSet = *aSet;
-  SigSetFixup(&newSet);
+  if (sigdelset(&newSet, SIGSYS) != 0 ||
+     (gSeccompTsyncBroadcastSignum &&
+      sigdelset(&newSet, gSeccompTsyncBroadcastSignum) != 0)) {
+    if (aUseErrno) {
+      errno = ENOSYS;
+      return -1;
+    }
+
+    return ENOSYS;
+  }
+
   return aRealFunc(aHow, &newSet, aOldSet);
 }
 
@@ -86,27 +70,6 @@ pthread_sigmask(int how, const sigset_t* set, sigset_t* oldset)
     dlsym(RTLD_NEXT, "pthread_sigmask");
 
   return HandleSigset(sRealFunc, how, set, oldset, false);
-}
-
-extern "C" MOZ_EXPORT int
-sigaction(int signum, const struct sigaction* act, struct sigaction* oldact)
-{
-  static auto sRealFunc =
-    (int (*)(int, const struct sigaction*, struct sigaction*))
-    dlsym(RTLD_NEXT, "sigaction");
-
-  if (!sRealFunc) {
-    errno = ENOSYS;
-    return -1;
-  }
-
-  if (act == nullptr || !SigSetNeedsFixup(&act->sa_mask)) {
-    return sRealFunc(signum, act, oldact);
-  }
-
-  struct sigaction newact = *act;
-  SigSetFixup(&newact.sa_mask);
-  return sRealFunc(signum, &newact, oldact);
 }
 
 extern "C" MOZ_EXPORT int
