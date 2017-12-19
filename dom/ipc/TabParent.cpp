fcd80d81abecb5ebe246752d4acc898f8119e58a
Mike Conley <mconley@mozilla.com>
2017-12-19 08:44:18 -0800

    Bug 1397426 - Expose renderLayers state via nsITabParent and lazily retrieve initial tab states in async tab switcher. r=billm
    MozReview-Commit-ID: IIMmwrgJUV6
    
    --HG--
    extra : rebase_source : f1faa794791a9c620a1cd7a28c67029134d63933
    extra : source : 8994162ee112a8c0a0b33f3404f04ebd9fad1285

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -2907,7 +2907,7 @@ TabParent::SetDocShellIsActive(bool isActive)
   // docshell is consider prerendered only if not active yet
   mIsPrerendered &= !isActive;
   mDocShellIsActive = isActive;
-  RenderLayers(isActive);
+  SetRenderLayers(isActive);
   Unused << SendSetDocShellIsActive(isActive);
 
   // update active accessible documents on windows
@@ -2949,7 +2949,7 @@ TabParent::GetIsPrerendered(bool* aIsPrerendered)
 }
 
 NS_IMETHODIMP
-TabParent::RenderLayers(bool aEnabled)
+TabParent::SetRenderLayers(bool aEnabled)
 {
   if (aEnabled == mRenderLayers) {
     if (aEnabled && mHasLayers && mPreserveLayers) {
@@ -2991,6 +2991,13 @@ TabParent::RenderLayers(bool aEnabled)
     cp->ForceTabPaint(this, mLayerTreeEpoch);
   }
 
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+TabParent::GetRenderLayers(bool* aResult)
+{
+  *aResult = mRenderLayers;
   return NS_OK;
 }
 
