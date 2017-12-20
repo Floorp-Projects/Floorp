637477858cca7171146beefe5c5710ec7a63fa54
Mark Banner <standard8@mozilla.com>
2017-12-20 13:26:51 +0000

    Bug 1423896 - Rename PlacesOrganizer.selectLeftPaneQuery to selectLeftPaneBuiltIn to better reflect what it is actually selecting. r=mak
    MozReview-Commit-ID: 6RJW5kWL82g

--- browser/components/places/tests/browser/browser_library_batch_delete.js
+++ browser/components/places/tests/browser/browser_library_batch_delete.js
@@ -44,7 +44,7 @@ add_task(async function test_create_and_batch_remove_bookmarks() {
 
   // Select and open the left pane "History" query.
   let PO = gLibrary.PlacesOrganizer;
-  PO.selectLeftPaneQuery("UnfiledBookmarks");
+  PO.selectLeftPaneBuiltIn("UnfiledBookmarks");
   Assert.notEqual(PO._places.selectedNode, null, "Selected unsorted bookmarks");
 
   let unsortedNode = PlacesUtils.asContainer(PO._places.selectedNode);
@@ -76,8 +76,8 @@ add_task(async function test_ensure_correct_selection_and_functionality() {
   let PO = gLibrary.PlacesOrganizer;
   let ContentTree = gLibrary.ContentTree;
   // Move selection forth and back.
-  PO.selectLeftPaneQuery("History");
-  PO.selectLeftPaneQuery("UnfiledBookmarks");
+  PO.selectLeftPaneBuiltIn("History");
+  PO.selectLeftPaneBuiltIn("UnfiledBookmarks");
   // Now select the "keepme" folder in the right pane and delete it.
   ContentTree.view.selectNode(ContentTree.view.result.root.getChild(0));
   Assert.equal(ContentTree.view.selectedNode.title, "keepme",
