af5bf12eff6ddc9f46b86e0b930ca9de987f6a73
Michael Comella <michael.l.comella@gmail.com>
2017-12-20 17:35:46 -0800

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/1955: Add javadoc for SupportUtils.topicStr.

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/SupportUtils.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/SupportUtils.java
@@ -26,6 +26,7 @@ public class SupportUtils {
         USAGE_DATA("usage-data"),
         WHATS_NEW("whats-new-focus-android-4");
 
+        /** The final path segment for a SUMO URL - see {@see #getSumoURLForTopic} */
         @VisibleForTesting final String topicStr;
 
         SumoTopic(final String topicStr) {
