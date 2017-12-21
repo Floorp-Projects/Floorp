80fed13ab49766fe181bdf1d6ab13858b17720fc
Dorel Luca <dluca@mozilla.com>
2017-12-21 21:03:35 +0200

    Backed out changeset d061700cc3c8 (bug 1378834) for failing Browser-chrome on toolkit/xre/test/browser_checkdllblockliststate.js on a CLOSED TREE

--- browser/branding/nightly/configure.sh
+++ browser/branding/nightly/configure.sh
@@ -2,6 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_DISPLAYNAME=FirefoxNightly
+MOZ_APP_DISPLAYNAME=Nightly
 MOZ_MACBUNDLE_ID=nightly
 
