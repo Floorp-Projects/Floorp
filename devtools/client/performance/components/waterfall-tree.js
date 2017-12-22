5213ac90afbf800d7541d732749aa86525ad90a5
Leo Huang <leo89214749@hotmail.com>
2017-12-22 12:10:00 +0800

    Bug 1426634 - Rename devtools/client/shared/components/Tree.js to VirtualizedTree.js; r=nchevobbe
    MozReview-Commit-ID: 80ebj9CJODR
    
    --HG--
    rename : devtools/client/shared/components/Tree.js => devtools/client/shared/components/VirtualizedTree.js
    extra : rebase_source : 78d24d5411b2d1dadae6778ba7f05c58641b6a00

--- devtools/client/performance/components/waterfall-tree.js
+++ devtools/client/performance/components/waterfall-tree.js
@@ -5,7 +5,7 @@
 
 const { Component, createFactory } = require("devtools/client/shared/vendor/react");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
-const Tree = createFactory(require("devtools/client/shared/components/Tree"));
+const Tree = createFactory(require("devtools/client/shared/components/VirtualizedTree"));
 const WaterfallTreeRow = createFactory(require("./waterfall-tree-row"));
 
 // Keep in sync with var(--waterfall-tree-row-height) in performance.css
