6e457ae2ec0a8cf11baa524ded2846e40b4bde11
Emilio Cobos Álvarez <emilio@crisal.io>
2017-12-22 11:01:19 +0100

    Bug 1427635: Fix DoCompareTreePosition frame tree version with null aCommonAncestor. r=xidorn
    It doesn't fill the ancestors of the first frame if aCommonAncestor is null,
    which means that we get garbage afterwards.
    
    MozReview-Commit-ID: G85dv7KM1Xd

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1913,8 +1913,11 @@ nsLayoutUtils::DoCompareTreePosition(nsIFrame* aFrame1,
   }
 
   AutoTArray<nsIFrame*,20> frame1Ancestors;
-  if (aCommonAncestor &&
-      !FillAncestors(aFrame1, aCommonAncestor, &frame1Ancestors)) {
+  // Note that the order of the condition is important. We need to fill the
+  // ancestors even if aCommonAncestor is null, otherwise the code below makes
+  // no sense.
+  if (!FillAncestors(aFrame1, aCommonAncestor, &frame1Ancestors) &&
+      aCommonAncestor) {
     // We reached the root of the frame tree ... if aCommonAncestor was set,
     // it is wrong
     return DoCompareTreePosition(aFrame1, aFrame2,
