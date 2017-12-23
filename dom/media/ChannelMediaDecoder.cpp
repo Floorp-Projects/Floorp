c20bfd39b574b8fa76d5bb84224195e0b5943e71
Ryan VanderMeulen <ryanvm@gmail.com>
2017-12-23 16:40:13 -0500

    Bug 1425996 - Add in a few more instances of Unused that got missed in the first patch. r=gerald

--- dom/media/ChannelMediaDecoder.cpp
+++ dom/media/ChannelMediaDecoder.cpp
@@ -337,6 +337,7 @@ ChannelMediaDecoder::NotifyDownloadEnded(nsresult aStatus)
       });
     nsresult rv = GetStateMachine()->OwnerThread()->Dispatch(r.forget());
     MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
+    Unused << rv;
     owner->DownloadSuspended();
     // NotifySuspendedStatusChanged will tell the element that download
     // has been suspended "by the cache", which is true since we never
@@ -403,6 +404,7 @@ ChannelMediaDecoder::DurationChanged()
     });
   nsresult rv = GetStateMachine()->OwnerThread()->Dispatch(r.forget());
   MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(rv));
+  Unused << rv;
 }
 
 void
