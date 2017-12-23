a45e796f42a051a75fb482531c2831fc71d15ccb
Stone Shih <sshih@mozilla.com>
2017-12-23 12:39:31 +0800

    Bug 1403743 - Keeping the mousemove coalescing enabled in the release build. r=smaug.

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1386,11 +1386,7 @@ pref("privacy.trackingprotection.lower_network_priority", false);
 pref("dom.event.contextmenu.enabled",       true);
 pref("dom.event.clipboardevents.enabled",   true);
 pref("dom.event.highrestimestamp.enabled",  true);
-#ifdef NIGHTLY_BUILD
 pref("dom.event.coalesce_mouse_move",       true);
-#else
-pref("dom.event.coalesce_mouse_move",       false);
-#endif
 
 pref("dom.webcomponents.enabled",           false);
 pref("dom.webcomponents.customelements.enabled", false);
