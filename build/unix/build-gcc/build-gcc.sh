f1dcc7b8db65a07e9120da8a63fc9b8d2e02843b
Mike Hommey <mh+mozilla@glandium.org>
2017-12-29 17:15:39 +0900

    Bug 1427344 - Add Debian CRT objects to GCC. r=nfroyd
    One of the last remaining differences when building Firefox on Debian
    with all the changes we've done so far is that linking against the
    system libc statically links some CRT objects. This causes massive
    differences in the resulting binaries because of slight differences
    in those objects (because they weren't compiled with the same compiler
    and because they're not for the exact same glibc version)
    
    In practice, their content difference don't cause any problem. If they
    did, we wouldn't be able to run our builds on newer systems than those
    we build them on. The only hypothetical risk would be to run on systems
    with a glibc older than Debian 7's, but those already can't run Firefox
    anyways (those systems don't have Gtk+3, which is a system requirement).
    
    AFAICT, this is only an hypothetical problem anyways, even such systems
    with Gtk+3 should be able to run those builds. Plus, this is a change
    that will happen anyways when switching to Debian-based build images,
    since they would be using the CRT objects from there. We're merely
    making it happen earlier so that the differences from switching to
    Debian-based build images are more tractable.
    
    Note we only do this when building GCC on Debian, allowing to roll back
    to CentOS-based toolchains by just switching back the toolchain jobs to
    use the desktop-build docker image again.

--- build/unix/build-gcc/build-gcc.sh
+++ build/unix/build-gcc/build-gcc.sh
@@ -118,6 +118,28 @@ build_gcc() {
 
   cd $root_dir/tools
   ln -s gcc gcc/bin/cc
+
+  if [ -f /etc/debian_version ]; then
+    cp -v /usr/lib32/crt*.o /usr/lib32/lib*_nonshared.a gcc/lib32
+    cp -v /usr/lib/x86_64-linux-gnu/crt*.o /usr/lib/x86_64-linux-gnu/lib*_nonshared.a gcc/lib64
+    cat > gcc/lib32/libc.so <<-EOF
+	OUTPUT_FORMAT(elf32-i386)
+	GROUP ( libc.so.6 libc_nonshared.a  AS_NEEDED ( ld-linux.so.2 ) )
+	EOF
+    cat > gcc/lib32/libpthread.so <<-EOF
+	OUTPUT_FORMAT(elf32-i386)
+	GROUP ( libpthread.so.0 libpthread_nonshared.a )
+	EOF
+    cat > gcc/lib64/libc.so <<-EOF
+	OUTPUT_FORMAT(elf64-x86-64)
+	GROUP ( libc.so.6 libc_nonshared.a  AS_NEEDED ( ld-linux-x86-64.so.2 ) )
+	EOF
+    cat > gcc/lib64/libpthread.so <<-EOF
+	OUTPUT_FORMAT(elf64-x86-64)
+	GROUP ( libpthread.so.0 libpthread_nonshared.a )
+	EOF
+  fi
+
   tar caf $root_dir/gcc.tar.xz gcc/
   popd
 }
