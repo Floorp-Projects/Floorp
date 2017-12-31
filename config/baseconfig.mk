34555aa279a1f29c3fd68996a206ce845447d16a
Mike Hommey <mh+mozilla@glandium.org>
2017-12-31 10:36:46 +0900

    Bug 1427455 - Remove unused variables from baseconfig.mk. r=froydnj
    Bug 1333826 removed their use.

--- config/baseconfig.mk
+++ config/baseconfig.mk
@@ -2,10 +2,7 @@
 # directly in python/mozbuild/mozbuild/base.py for gmake validation.
 # We thus use INCLUDED_AUTOCONF_MK to enable/disable some parts depending
 # whether a normal build is happening or whether the check is running.
-includedir := $(includedir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
-idldir = $(datadir)/idl/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
 installdir = $(libdir)/$(MOZ_APP_NAME)-$(MOZ_APP_VERSION)
-sdkdir = $(libdir)/$(MOZ_APP_NAME)-devel-$(MOZ_APP_VERSION)
 ifeq (.,$(DEPTH))
 DIST = dist
 else
