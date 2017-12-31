e21203ee939f7ae2335bb55ddc83c1724004f30e
Adam Langley <agl@chromium.org>
2017-12-31 15:37:27 -0800

    Bug 1420760 - Order webauthn CBOR map keys. r=jcj
    MozReview-Commit-ID: 6BsiL45dxa3

--- dom/webauthn/WebAuthnCBORUtil.cpp
+++ dom/webauthn/WebAuthnCBORUtil.cpp
@@ -34,14 +34,14 @@ CBOREncodePublicKeyObj(const CryptoBuffer& aPubKeyBuf,
   cbor::encoder encoder(cborPubKeyOut);
   encoder.write_map(3);
   {
-    encoder.write_string("alg");
-    encoder.write_string(JWK_ALG_ECDSA_P_256); // Always ES256 for U2F
-
     encoder.write_string("x");
     encoder.write_bytes(xBuf.Elements(), xBuf.Length());
 
     encoder.write_string("y");
     encoder.write_bytes(yBuf.Elements(), yBuf.Length());
+
+    encoder.write_string("alg");
+    encoder.write_string(JWK_ALG_ECDSA_P_256); // Always ES256 for U2F
   }
 
   if (!aPubKeyObj.Assign(cborPubKeyOut.data(), cborPubKeyOut.size())) {
@@ -76,23 +76,23 @@ CBOREncodeAttestationObj(const CryptoBuffer& aAuthDataBuf,
   cbor::encoder encoder(cborAttOut);
   encoder.write_map(3);
   {
-    encoder.write_string("authData");
-    encoder.write_bytes(aAuthDataBuf.Elements(), aAuthDataBuf.Length());
-
     encoder.write_string("fmt");
     encoder.write_string("fido-u2f");
 
     encoder.write_string("attStmt");
     encoder.write_map(2);
     {
+      encoder.write_string("sig");
+      encoder.write_bytes(aSignatureBuf.Elements(), aSignatureBuf.Length());
+
       encoder.write_string("x5c");
       // U2F wire protocol can only deliver 1 certificate, so it's never a chain
       encoder.write_array(1);
       encoder.write_bytes(aAttestationCertBuf.Elements(), aAttestationCertBuf.Length());
-
-      encoder.write_string("sig");
-      encoder.write_bytes(aSignatureBuf.Elements(), aSignatureBuf.Length());
     }
+
+    encoder.write_string("authData");
+    encoder.write_bytes(aAuthDataBuf.Elements(), aAuthDataBuf.Length());
   }
 
   if (!aAttestationObj.Assign(cborAttOut.data(), cborAttOut.size())) {
