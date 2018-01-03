8467e6d22aafbc04cccd00e88fc412683c2827c7
David Keeler <dkeeler@mozilla.com>
2018-01-03 14:20:47 -0800

    Bug 1412331 - Bump security preload information expiration dates to 2018-05-08 for Firefox 58. r=jcj, a=gchang

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1159,4 +1159,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1518982327222000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1525737600000000);
