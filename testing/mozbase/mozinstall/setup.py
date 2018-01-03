7f3a4f976fa5927fd1628ae9603293b1a00319dd
William Lachance <wlachance@mozilla.com>
2018-01-03 14:34:07 -0500

    Bug 1427837 - Bump mozinstall to 1.15 r=ahal
    MozReview-Commit-ID: LkcqILfkVwb
    
    --HG--
    extra : rebase_source : 29acc17c001c317eb851cfb09af1e451063ceda2

--- testing/mozbase/mozinstall/setup.py
+++ testing/mozbase/mozinstall/setup.py
@@ -1,3 +1,4 @@
+
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,7 +14,7 @@ try:
 except IOError:
     description = None
 
-PACKAGE_VERSION = '1.14'
+PACKAGE_VERSION = '1.15'
 
 deps = ['mozinfo >= 0.7',
         'mozfile >= 1.0',
