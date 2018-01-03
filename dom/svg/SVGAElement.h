d3abb0a6c59d7230018ee16901efdb406d692ac3
Boris Zbarsky <bzbarsky@mit.edu>
2018-01-03 14:21:22 -0500

    Bug 1423490 part 1.  Stop overriding SetAttr in SVGAElement.  r=bytesized
    This aligns SVGAElement with HTMLAnchorElement in terms of how they handle
    attribute changes.
    
    MozReview-Commit-ID: Azr6tTBaxTT

--- dom/svg/SVGAElement.h
+++ dom/svg/SVGAElement.h
@@ -56,13 +56,11 @@ public:
   virtual void GetLinkTarget(nsAString& aTarget) override;
   virtual already_AddRefed<nsIURI> GetHrefURI() const override;
   virtual EventStates IntrinsicState() const override;
-  using Element::SetAttr;
-  virtual nsresult SetAttr(int32_t aNameSpaceID, nsAtom* aName,
-                           nsAtom* aPrefix, const nsAString& aValue,
-                           nsIPrincipal* aSubjectPrincipal,
-                           bool aNotify) override;
-  virtual nsresult UnsetAttr(int32_t aNameSpaceID, nsAtom* aAttribute,
-                             bool aNotify) override;
+  virtual nsresult AfterSetAttr(int32_t aNameSpaceID, nsAtom* aName,
+                                const nsAttrValue* aValue,
+                                const nsAttrValue* aOldValue,
+                                nsIPrincipal* aMaybeScriptedPrincipal,
+                                bool aNotify) override;
 
   // Link
   virtual bool ElementHasHref() const override;
