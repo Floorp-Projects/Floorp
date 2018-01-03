a5a4f95a2351f97264ec7e21c6117aa67ea86eb6
Ted Mielczarek <ted@mielczarek.org>
2018-01-03 14:51:52 -0500

    bug 1401647 - Fix spidermonkey mozjs / rust-bindings builds. r=nalexander
    The spidermonkey mozjs and rust-bindings builds run sed on
    $topsrcdir/.cargo/config.in to generate the cargo config they use, but
    they previously only replaced the @top_srcdir@ substitution. This patch
    makes them replace any other substitutions with an empty value to add
    a bit of future-proofing.
    
    MozReview-Commit-ID: 1DzP9vXxHMD

--- taskcluster/scripts/builder/build-sm-mozjs-crate.sh
+++ taskcluster/scripts/builder/build-sm-mozjs-crate.sh
@@ -7,7 +7,7 @@ source $(dirname $0)/sm-tooltool-config.sh
 # Ensure that we have a .config/cargo that points us to our vendored crates
 # rather than to crates.io.
 cd "$SRCDIR/.cargo"
-sed -e "s|@top_srcdir@|$SRCDIR|" < config.in | tee config
+sed -e "s|@top_srcdir@|$SRCDIR|" -e 's|@[^@]*@||g' < config.in > config
 
 cd "$SRCDIR/js/src"
 
