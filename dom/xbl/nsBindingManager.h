f71e150d904914b96a15a3b33a4b1aff64090aaa
Emilio Cobos Álvarez <emilio@crisal.io>
2018-01-03 06:38:21 +0100

    Bug 1427677: Remove nsBindingManager::FindNestedInsertionPoint. r=bz
    No more callers!
    
    MozReview-Commit-ID: 1mxi4IjViuC

--- dom/xbl/nsBindingManager.h
+++ dom/xbl/nsBindingManager.h
@@ -171,9 +171,6 @@ public:
   // Called when the document is going away
   void DropDocumentReference();
 
-  nsIContent* FindNestedInsertionPoint(nsIContent* aContainer,
-                                       nsIContent* aChild);
-
   nsIContent* FindNestedSingleInsertionPoint(nsIContent* aContainer, bool* aMulti);
 
   bool AnyBindingHasDocumentStateDependency(mozilla::EventStates aStateMask);
