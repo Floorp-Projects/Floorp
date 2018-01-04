4b4ce8d7203334231d843d693fe13cb15f1f3e9e
Mats Palmgren <mats@mozilla.com>
2018-01-04 22:16:37 +0100

    Bug 1423761 - Make CorrectStyleParentFrame / UpdateStyleOfChildAnonBox deal with abs.pos. anon boxes.  r=dholbert
    For an OOF frame, the parent frame that's associated with our parent style
    context is the *placeholder's* parent -- not the OOF frame's parent.

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -9843,7 +9843,7 @@ nsFrame::CorrectStyleParentFrame(nsIFrame* aProspectiveParent,
       return parent;
     }
 
-    parent = parent->GetParent();
+    parent = parent->GetInFlowParent();
   } while (parent);
 
   if (aProspectiveParent->StyleContext()->GetPseudo() ==
@@ -10688,7 +10688,7 @@ nsIFrame::UpdateStyleOfChildAnonBox(nsIFrame* aChildFrame,
                                     ServoRestyleState& aRestyleState)
 {
 #ifdef DEBUG
-  nsIFrame* parent = aChildFrame->GetParent();;
+  nsIFrame* parent = aChildFrame->GetInFlowParent();
   if (aChildFrame->IsTableFrame()) {
     parent = parent->GetParent();
   }
