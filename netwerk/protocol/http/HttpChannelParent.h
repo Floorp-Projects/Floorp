e4df016bc265d75429996107eec4430d1b6af5db
Andrew Sutherland <asutherland@asutherland.org>
2018-01-04 18:38:07 -0500

    Bug 1416879 - Part 2: Allow for diversion cancellation and trigger for intercepted channels. r=bkelly, r=mayhemer a=gchang
    The diversion mechanism never expected to be dealing with data sourced
    from the content process, but that's exactly what happens with
    ServiceWorker-intercepted channels with the current child-intercept
    situation (which is being fixed).
    
    In order to allow timely cancellation of diverted intercepted
    channels, there needs to be a way to relay to the HttpChannelChild
    that it needs to be canceled so that the synthesized pump can be
    canceled and diversion can be marked as complete.  This patch adds
    such a mechanism to ADivertableParentChannel and PHttpChannel for the
    exclusive use of InterceptedHttpChannel and then uses it.
    
    --HG--
    extra : source : d156f6b687e1cd8717bf813ab1b944e87b67ed5d
    extra : intermediate-source : 7052da34a62d03ad510ad273f8cad970157fd5ba

--- netwerk/protocol/http/HttpChannelParent.h
+++ netwerk/protocol/http/HttpChannelParent.h
@@ -83,6 +83,7 @@ public:
   MOZ_MUST_USE nsresult SuspendForDiversion() override;
   MOZ_MUST_USE nsresult SuspendMessageDiversion() override;
   MOZ_MUST_USE nsresult ResumeMessageDiversion() override;
+  MOZ_MUST_USE nsresult CancelDiversion() override;
 
   // Calls OnStartRequest for "DivertTo" listener, then notifies child channel
   // that it should divert OnDataAvailable and OnStopRequest calls to this
