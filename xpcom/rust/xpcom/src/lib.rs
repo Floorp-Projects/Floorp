83be5d0e92dd66028157e8071c894691ed77d336
Nika Layzell <nika@thelayzells.com>
2018-01-04 17:21:54 -0500

    Bug 1293362 - Part 2: Add skeleton crates for xpcom bindings, r=froydnj
    MozReview-Commit-ID: H5nxsk4cg2E

---
+++ xpcom/rust/xpcom/src/lib.rs
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+
+extern crate libc;
+extern crate nsstring;
+extern crate nserror;
+
+// re-export the xpcom_macros macro
+#[macro_use]
+#[allow(unused_imports)]
+extern crate xpcom_macros;
+#[doc(hidden)]
+pub use xpcom_macros::*;
---
+++ xpcom/rust/xpcom/xpcom_macros/src/lib.rs
@@ -0,0 +1,9 @@
+#[macro_use]
+extern crate quote;
+
+extern crate syn;
+
+extern crate proc_macro;
+
+#[macro_use]
+extern crate lazy_static;
