be52f69dadc3ac56b25dd4fcbf041f4a726c1cc7
Andrew Sutherland <asutherland@asutherland.org>
2018-01-04 18:38:07 -0500

    Bug 1416879 - Part 2: Allow for diversion cancellation and trigger for intercepted channels. r=bkelly, r=mayhemer a=gchang
    The diversion mechanism never expected to be dealing with data sourced
    from the content process, but that's exactly what happens with
    ServiceWorker-intercepted channels with the current child-intercept
    situation (which is being fixed).
    
    In order to allow timely cancellation of diverted intercepted
    channels, there needs to be a way to relay to the HttpChannelChild
    that it needs to be canceled so that the synthesized pump can be
    canceled and diversion can be marked as complete.  This patch adds
    such a mechanism to ADivertableParentChannel and PHttpChannel for the
    exclusive use of InterceptedHttpChannel and then uses it.

--- netwerk/protocol/http/InterceptedHttpChannel.cpp
+++ netwerk/protocol/http/InterceptedHttpChannel.cpp
@@ -31,6 +31,7 @@ InterceptedHttpChannel::InterceptedHttpChannel(PRTime aCreationTime,
   , mResumeStartPos(0)
   , mSynthesizedOrReset(Invalid)
   , mCallingStatusAndProgress(false)
+  , mDiverting(false)
 {
   // Pre-set the creation and AsyncOpen times based on the original channel
   // we are intercepting.  We don't want our extra internal redirect to mask
@@ -491,6 +492,15 @@ InterceptedHttpChannel::Cancel(nsresult aStatus)
   MOZ_DIAGNOSTIC_ASSERT(NS_FAILED(aStatus));
   if (NS_SUCCEEDED(mStatus)) {
     mStatus = aStatus;
+  }
+
+  // Everything is suspended during diversion until it completes.  Since the
+  // intercepted channel could be a long-running stream, we need to request that
+  // cancellation be triggered in the child, completing the diversion and
+  // allowing cancellation to run to completion.
+  if (mDiverting) {
+    Unused << mParentChannel->CancelDiversion();
+    // (We want the pump to be canceled as well, so don't directly return.)
   }
 
   if (mPump) {
@@ -1083,6 +1093,7 @@ InterceptedHttpChannel::MessageDiversionStarted(ADivertableParentChannel* aParen
 {
   MOZ_ASSERT(!mParentChannel);
   mParentChannel = aParentChannel;
+  mDiverting = true;
   uint32_t suspendCount = mSuspendCount;
   while(suspendCount--) {
     mParentChannel->SuspendMessageDiversion();
@@ -1095,6 +1106,7 @@ InterceptedHttpChannel::MessageDiversionStop()
 {
   MOZ_ASSERT(mParentChannel);
   mParentChannel = nullptr;
+  mDiverting = false;
   return NS_OK;
 }
 
