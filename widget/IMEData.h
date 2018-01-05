587eb27432033ab6bbeca4cded6abba23806e4ec
Eugen Sawin <esawin@mozilla.com>
2018-01-05 08:28:00 -0500

    Bug 1428371 - Add missing member initialization in InputContext. r=masayuki

--- widget/IMEData.h
+++ widget/IMEData.h
@@ -275,6 +275,7 @@ struct InputContext final
   InputContext()
     : mOrigin(XRE_IsParentProcess() ? ORIGIN_MAIN : ORIGIN_CONTENT)
     , mMayBeIMEUnaware(false)
+    , mHasHandledUserInput(false)
     , mInPrivateBrowsing(false)
   {
   }
