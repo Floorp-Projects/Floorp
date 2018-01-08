67653ce16d7b67d1be721353ba1d0bc39759a498
Alfredo.Yang <ayang@mozilla.com>
2018-01-08 10:08:04 +0800

    Bug 1426773 - update rust mp4 parser. r=kinetik
    MozReview-Commit-ID: 8GzXp7Oq9Wx

--- media/mp4parse-rust/mp4parse/src/lib.rs
+++ media/mp4parse-rust/mp4parse/src/lib.rs
@@ -1376,7 +1376,8 @@ fn find_descriptor(data: &[u8], esds: &mut ES_Descriptor) -> Result<()> {
 
     let mut remains = data;
 
-    while !remains.is_empty() {
+    // Descriptor length should be more than 2 bytes.
+    while remains.len() > 2 {
         let des = &mut Cursor::new(remains);
         let tag = des.read_u8()?;
 
