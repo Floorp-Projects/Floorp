e10464a4653b82ccae27ae8fde1ba15ecb3d3a57
Andrea Marchesini <amarchesini@mozilla.com>
2018-01-08 14:05:07 +0100

    Bug 1419771 - Introduce DOMPrefs, a thread-safe access to preferences for DOM - part 22 - DOMPrefs initialized at startup, r=asuth

--- dom/base/DOMPrefs.h
+++ dom/base/DOMPrefs.h
@@ -13,86 +13,19 @@ namespace dom {
 class DOMPrefs final
 {
 public:
+  // This must be called on the main-thread.
+  static void Initialize();
+
   // Returns true if the browser.dom.window.dump.enabled pref is set.
   static bool DumpEnabled();
 
-  // Returns true if the canvas.imagebitmap_extensions.enabled pref is set.
-  static bool ImageBitmapExtensionsEnabled();
-  static bool ImageBitmapExtensionsEnabled(JSContext* aCx, JSObject* aObj);
+#define DOM_PREF(name, pref) static bool name();
+#define DOM_WEBIDL_PREF(name) static bool name(JSContext* aCx, JSObject* aObj);
 
-  // Returns true if the dom.caches.enabled pref is set.
-  static bool DOMCachesEnabled();
-  static bool DOMCachesEnabled(JSContext* aCx, JSObject* aObj);
+#include "DOMPrefsInternal.h"
 
-  // Returns true if the dom.caches.testing.enabled pref is set.
-  static bool DOMCachesTestingEnabled();
-
-  // Returns true if the dom.performance.enable_user_timing_logging pref is set.
-  static bool PerformanceLoggingEnabled();
-
-  // Returns true if the dom.webnotifications.enabled pref is set.
-  // Note that you should use NotificationEnabledInServiceWorkers if you need to
-  // enable Notification API for ServiceWorkers
-  static bool NotificationEnabled();
-
-  // Returns true if the dom.webnotifications.serviceworker.enabled pref is set.
-  static bool NotificationEnabledInServiceWorkers();
-  static bool NotificationEnabledInServiceWorkers(JSContext* aCx,
-                                                  JSObject* aObj);
-
-  // Returns true if the dom.webnotifications.requireinteraction.enabled pref is
-  // set.
-  static bool NotificationRIEnabled();
-  static bool NotificationRIEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.serviceWorkers.enabled pref is set.
-  static bool ServiceWorkersEnabled();
-  static bool ServiceWorkersEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.serviceWorkers.testing.enabled pref is set.
-  static bool ServiceWorkersTestingEnabled();
-
-  // Returns true if the dom.storageManager.enabled pref is set.
-  static bool StorageManagerEnabled();
-  static bool StorageManagerEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.promise_rejection_events.enabled pref is set.
-  static bool PromiseRejectionEventsEnabled();
-  static bool PromiseRejectionEventsEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.push.enabled pref is set.
-  static bool PushEnabled();
-  static bool PushEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.streams.enabled pref is set.
-  static bool StreamsEnabled();
-  static bool StreamsEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.requestcontext.enabled pref is set.
-  static bool RequestContextEnabled();
-  static bool RequestContextEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the gfx.offscreencanvas.enabled pref is set.
-  static bool OffscreenCanvasEnabled();
-  static bool OffscreenCanvasEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.webkitBlink.dirPicker.enabled pref is set.
-  static bool WebkitBlinkDirectoryPickerEnabled();
-  static bool WebkitBlinkDirectoryPickerEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.netinfo.enabled pref is set.
-  static bool NetworkInformationEnabled();
-  static bool NetworkInformationEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the dom.fetchObserver.enabled pref is set.
-  static bool FetchObserverEnabled();
-  static bool FetchObserverEnabled(JSContext* aCx, JSObject* aObj);
-
-  // Returns true if the privacy.resistFingerprinting pref is set.
-  static bool ResistFingerprintingEnabled();
-
-  // Returns true if the devtools.enabled pref is set.
-  static bool DevToolsEnabled();
+#undef DOM_PREF
+#undef DOM_WEBIDL_PREF
 };
 
 } // dom namespace
