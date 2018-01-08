c508e62b7e0c900201461a3e06fa4f5506d3dab4
Andrea Marchesini <amarchesini@mozilla.com>
2018-01-08 08:31:38 +0100

    Bug 1252998 - StorageActivityService - part 4 - Introduce ServiceWorkerCleanUp.jsm to clean up ServiceWorker data, r=asuth

--- browser/components/extensions/ext-browsingData.js
+++ browser/components/extensions/ext-browsingData.js
@@ -13,6 +13,8 @@ XPCOMUtils.defineLazyModuleGetter(this, "Services",
                                   "resource://gre/modules/Services.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "setTimeout",
                                   "resource://gre/modules/Timer.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "ServiceWorkerCleanUp",
+                                  "resource://gre/modules/ServiceWorkerCleanUp.jsm");
 
 XPCOMUtils.defineLazyServiceGetter(this, "serviceWorkerManager",
                                    "@mozilla.org/serviceworkers/manager;1",
@@ -153,22 +155,6 @@ const clearPluginData = options => {
   return sanitizer.items.pluginData.clear(makeRange(options));
 };
 
-const clearServiceWorkers = async function() {
-  // Clearing service workers does not support timestamps.
-  let yieldCounter = 0;
-
-  // Iterate through the service workers and remove them.
-  let serviceWorkers = serviceWorkerManager.getAllRegistrations();
-  for (let i = 0; i < serviceWorkers.length; i++) {
-    let sw = serviceWorkers.queryElementAt(i, Ci.nsIServiceWorkerRegistrationInfo);
-    let host = sw.principal.URI.host;
-    serviceWorkerManager.removeAndPropagate(host);
-    if (++yieldCounter % YIELD_PERIOD == 0) {
-      await new Promise(resolve => setTimeout(resolve, 0)); // Don't block the main thread too long.
-    }
-  }
-};
-
 const doRemoval = (options, dataToRemove, extension) => {
   if (options.originTypes &&
       (options.originTypes.protectedWeb || options.originTypes.extension)) {
@@ -209,7 +195,7 @@ const doRemoval = (options, dataToRemove, extension) => {
           removalPromises.push(clearPluginData(options));
           break;
         case "serviceWorkers":
-          removalPromises.push(clearServiceWorkers());
+          removalPromises.push(ServiceWorkerCleanUp.removeAll());
           break;
         default:
           invalidDataTypes.push(dataType);
