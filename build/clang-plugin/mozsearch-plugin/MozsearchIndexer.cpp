fcffd9cfcdcdad37b65fb90e6045c7c5fd205c40
Emilio Cobos Álvarez <emilio@crisal.io>
2018-01-08 20:21:53 +0100

    Bug 1428864: Fix indexer plugin build with clang >= 5. r=kats
    MozReview-Commit-ID: J1DKFkyXu0v

--- build/clang-plugin/mozsearch-plugin/MozsearchIndexer.cpp
+++ build/clang-plugin/mozsearch-plugin/MozsearchIndexer.cpp
@@ -111,7 +111,7 @@ public:
   virtual void MacroExpands(const Token &Tok, const MacroDefinition &Md,
                             SourceRange Range, const MacroArgs *Ma) override;
 #if CLANG_VERSION_MAJOR >= 5
-  virtual void MacroUndefined(const Token &tok, const MacroDefinition &md,
+  virtual void MacroUndefined(const Token &Tok, const MacroDefinition &Md,
                               const MacroDirective *Undef) override;
 #else
   virtual void MacroUndefined(const Token &Tok,
@@ -1421,8 +1421,8 @@ void PreprocessorHook::MacroExpands(const Token &Tok, const MacroDefinition &Md,
 }
 
 #if CLANG_VERSION_MAJOR >= 5
-void PreprocessorHook::MacroUndefined(const Token &tok,
-                                      const MacroDefinition &md,
+void PreprocessorHook::MacroUndefined(const Token &Tok,
+                                      const MacroDefinition &Md,
                                       const MacroDirective *Undef)
 #else
 void PreprocessorHook::MacroUndefined(const Token &Tok,
