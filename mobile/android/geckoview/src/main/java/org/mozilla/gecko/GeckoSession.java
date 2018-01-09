a49fff9467353891baaac20dccec77c86c4dbe64
Jim Chen <nchen@mozilla.com>
2018-01-09 16:13:48 -0500

    Bug 1427079 - Don't let disposed window be read from parcels; r=snorp
    It's possible for parcels derived from the session to outlast the
    session lifecycle. This patch makes us return null when trying to
    retrieve window objects using stale parcels.
    
    MozReview-Commit-ID: 3Vp6T3uCEBt

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoSession.java
@@ -296,7 +296,7 @@ public class GeckoSession extends LayerSession
         }
 
         @Override // IInterface
-        public IBinder asBinder() {
+        public Binder asBinder() {
             if (mBinder == null) {
                 mBinder = new Binder();
                 mBinder.attachInterface(this, Window.class.getName());
@@ -310,8 +310,16 @@ public class GeckoSession extends LayerSession
                                        GeckoBundle settings, String chromeUri,
                                        int screenId, boolean privateMode);
 
-        @WrapForJNI(dispatchTo = "proxy")
-        @Override protected native void disposeNative();
+        @Override // JNIObject
+        protected void disposeNative() {
+            // Detach ourselves from the binder as well, to prevent this window from being
+            // read from any parcels.
+            asBinder().attachInterface(null, Window.class.getName());
+            nativeDisposeNative();
+        }
+
+        @WrapForJNI(dispatchTo = "proxy", stubName = "DisposeNative")
+        private native void nativeDisposeNative();
 
         @WrapForJNI(dispatchTo = "proxy")
         public native void close();
