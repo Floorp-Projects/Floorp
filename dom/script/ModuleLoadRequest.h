8f0c2b3ab99b25e96c03a59cc8b8e9531f69bf27
Andrea Marchesini <amarchesini@mozilla.com>
2018-01-09 17:00:49 +0100

    Bug 1428745 - Remove support for version parameter from script loader, r=jonco

--- dom/script/ModuleLoadRequest.h
+++ dom/script/ModuleLoadRequest.h
@@ -45,7 +45,6 @@ public:
   // Create a top-level module load request.
   ModuleLoadRequest(nsIURI* aURI,
                     nsIScriptElement* aElement,
-                    ValidJSVersion aValidJSVersion,
                     CORSMode aCORSMode,
                     const SRIMetadata& aIntegrity,
                     nsIURI* aReferrer,
--- dom/script/ScriptLoadRequest.h
+++ dom/script/ScriptLoadRequest.h
@@ -29,11 +29,6 @@ enum class ScriptKind {
   eModule
 };
 
-enum class ValidJSVersion : bool {
-  eInvalid,
-  eValid
-};
-
 /*
  * A class that handles loading and evaluation of <script> elements.
  */
@@ -54,7 +49,6 @@ public:
   ScriptLoadRequest(ScriptKind aKind,
                     nsIURI* aURI,
                     nsIScriptElement* aElement,
-                    ValidJSVersion aValidJSVersion,
                     mozilla::CORSMode aCORSMode,
                     const SRIMetadata &aIntegrity,
                     nsIURI* aReferrer,
@@ -217,7 +211,6 @@ public:
   mozilla::Vector<uint8_t> mScriptBytecode;
   uint32_t mBytecodeOffset; // Offset of the bytecode in mScriptBytecode
 
-  ValidJSVersion mValidJSVersion;
   const nsCOMPtr<nsIURI> mURI;
   nsCOMPtr<nsIPrincipal> mTriggeringPrincipal;
   nsCOMPtr<nsIPrincipal> mOriginPrincipal;
