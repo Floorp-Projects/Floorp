6e3bb55bb881c4ca5b15523150224ecc1b88e6af
Thom Chiovoloni <tchiovoloni@mozilla.com>
2018-01-09 17:46:24 -0500

    Bug 1412083 - Avoid syncing preferences that refer to moz-extension or blob urls. r=eoger
    MozReview-Commit-ID: 9Ipq5z1dykr
    
    --HG--
    extra : rebase_source : a8616485879268ce31ef74f4b3fdbc3f4dc5a585

--- services/sync/tests/unit/prefs_test_prefs_store.js
+++ services/sync/tests/unit/prefs_test_prefs_store.js
@@ -12,6 +12,10 @@ pref("services.sync.prefs.sync.testing.dont.change", true);
 user_pref("services.sync.prefs.sync.testing.turned.off", false);
 pref("services.sync.prefs.sync.testing.nonexistent", true);
 pref("services.sync.prefs.sync.testing.default", true);
+pref("services.sync.prefs.sync.testing.synced.url", true);
+// We shouldn't sync the URL, or the flag that says we should sync the pref
+// (otherwise some other client might overwrite our local value).
+user_pref("services.sync.prefs.sync.testing.unsynced.url", true);
 
 // The preference values - these are all user_prefs, otherwise their value
 // will not be synced.
@@ -21,7 +25,12 @@ user_pref("testing.bool", true);
 user_pref("testing.dont.change", "Please don't change me.");
 user_pref("testing.turned.off", "I won't get synced.");
 user_pref("testing.not.turned.on", "I won't get synced either!");
+// Some url we don't want to sync
+user_pref("testing.unsynced.url", "moz-extension://d5d31b00-b944-4afb-bd3d-d0326551a0ae");
+user_pref("testing.synced.url", "https://www.example.com");
 
 // A pref that exists but still has the default value - will be synced with
 // null as the value.
 pref("testing.default", "I'm the default value");
+
+// A pref that shouldn't be synced
