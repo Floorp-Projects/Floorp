88086410feb804fd3217c80ad070054bf27932d6
Mark Banner <standard8@mozilla.com>
2018-01-10 17:25:52 +0000

    Bug 1429701 - Enable ESLint rule mozilla/use-services for dom/indexedDB. r=bevis
    MozReview-Commit-ID: 9OgcN56ziip

--- dom/indexedDB/test/unit/test_metadataRestore.js
+++ dom/indexedDB/test/unit/test_metadataRestore.js
@@ -57,17 +57,12 @@ function* testSteps()
       dbOptions: { version: 1, storage: "default" } }
   ];
 
-  let ios = SpecialPowers.Cc["@mozilla.org/network/io-service;1"]
-                         .getService(SpecialPowers.Ci.nsIIOService);
-
-  let ssm = SpecialPowers.Cc["@mozilla.org/scriptsecuritymanager;1"]
-                         .getService(SpecialPowers.Ci.nsIScriptSecurityManager);
-
   function openDatabase(params) {
     let request;
     if ("url" in params) {
-      let uri = ios.newURI(params.url);
-      let principal = ssm.createCodebasePrincipal(uri, {});
+      let uri = Services.io.newURI(params.url);
+      let principal = Services.scriptSecurityManager
+        .createCodebasePrincipal(uri, {});
       request = indexedDB.openForPrincipal(principal, params.dbName,
                                            params.dbOptions);
     } else {
