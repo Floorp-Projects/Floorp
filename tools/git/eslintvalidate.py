14685fa5e958982fa3867681a287a070ed85cd99
Andrew Halberstadt <ahalberstadt@mozilla.com>
2018-01-09 20:03:58 -0500

    Bug 1429223 - [lint] Obsolete the eslintvalidate hooks by printing error message with upgrade path, r=standard8
    This replaces the eslintvalidate hooks with an error message
    prompting the user to upgrade to tools/lint/hooks.py.
    
    The reasons for deprecating eslintvalidate are twofold:
    
    1) It only runs eslint, so developers might miss errors from
    other linters.
    
    2) It isn't as well maintained, and I've started to see
    reports of problems in the wild. It doesn't make sense to
    maintain two sets of hooks that do the same thing.
    
    MozReview-Commit-ID: CseeVIof2om
    
    --HG--
    extra : rebase_source : e859c368d14cd1bf7e7d85f0de5bbb89e88402d9

--- tools/git/eslintvalidate.py
+++ tools/git/eslintvalidate.py
@@ -2,10 +2,19 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "lint", "eslint"))
-from hook_helper import is_lintable, runESLint
+
+OBSOLETE = """
+ERROR: the eslintvalidate hook is obsolete. This commit went
+through, but ESlint didn't run. You can lint your changes
+after the fact by running:
+
+    $ mach lint --outgoing
+
+Please remove this hook and upgrade by following these
+instructions:
+https://firefox-source-docs.mozilla.org/tools/lint/usage.html#using-a-vcs-hook
+""".lstrip()
 
 
 def output(message):
@@ -13,23 +22,9 @@ def output(message):
 
 
 def eslint():
-    f = os.popen('git diff --cached --name-only --diff-filter=ACM')
-
-    files = [file for file in f.read().splitlines() if is_lintable(file)]
-
-    if len(files) == 0:
-        return True
-
-    print "Running ESLint..."
-
-    return runESLint(output, files)
+    output(OBSOLETE)
+    return False
 
 
 if __name__ == '__main__':
-    if not eslint():
-        output("Note: ESLint failed, but the commit will still happen. "
-               "Please fix before pushing.")
-
-    # We output successfully as that seems to be a better model. See
-    # https://bugzilla.mozilla.org/show_bug.cgi?id=1230300#c4 for more
-    # information.
+    eslint()
