a1474a6ed5cc47d42c57d3a16b3a67aade891e3e
Henrik Skupin <mail@hskupin.info>
2018-01-11 15:21:11 +0100

    Bug 1429759 - Allow Marionette tests to change the user profile. r=automatedtester
    If Marionette handles the instance of the application including the
    user profile, the tests should be able to change the user profile.
    
    This will enable tests to cover specific bugs in the application as
    caused by the profile name and path.
    
    To prevent dataloss changing the profile should only be allowed
    when the application is not running.
    
    MozReview-Commit-ID: JWQGV4JWP61
    
    --HG--
    extra : rebase_source : 18270e9790f11fffc2a33a231dbc8229a1bf12d2

--- testing/marionette/harness/marionette_harness/tests/unit/test_prefs.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_prefs.py
@@ -165,3 +165,38 @@ class TestPreferences(MarionetteTestCase):
             pass
 
         self.assertEquals(self.marionette.get_pref(self.prefs["string"]), "abc")
+
+
+class TestEnforcePreferences(MarionetteTestCase):
+
+    def setUp(self):
+        super(TestEnforcePreferences, self).setUp()
+
+        self.marionette.enforce_gecko_prefs({
+            "marionette.test.bool": True,
+            "marionette.test.int": 3,
+            "marionette.test.string": "testing",
+        })
+        self.marionette.set_context("chrome")
+
+    def tearDown(self):
+        self.marionette.quit(clean=True)
+
+        super(TestEnforcePreferences, self).tearDown()
+
+    def test_preferences_are_set(self):
+        self.assertTrue(self.marionette.get_pref("marionette.test.bool"))
+        self.assertEqual(self.marionette.get_pref("marionette.test.string"), "testing")
+        self.assertEqual(self.marionette.get_pref("marionette.test.int"), 3)
+
+    def test_change_preference(self):
+        self.assertTrue(self.marionette.get_pref("marionette.test.bool"))
+
+        self.marionette.enforce_gecko_prefs({"marionette.test.bool": False})
+
+        self.assertFalse(self.marionette.get_pref("marionette.test.bool"))
+
+    def test_restart_with_clean_profile(self):
+        self.marionette.restart(clean=True)
+
+        self.assertEqual(self.marionette.get_pref("marionette.test.bool"), None)
