9f40953ecc9ef430bc2a193617b6801aef1429db
Cameron McCormack <cam@mcc.id.au>
2018-01-11 16:17:57 +0800

    Bug 1427512 - Part 27: Remove nsIDOMCSSPrimitiveValue. r=xidorn,bz
    MozReview-Commit-ID: IcGKAjPWacZ

--- layout/style/nsROCSSPrimitiveValue.h
+++ layout/style/nsROCSSPrimitiveValue.h
@@ -10,7 +10,9 @@
 #define nsROCSSPrimitiveValue_h___
 
 #include "nsIDOMCSSValue.h"
-#include "nsIDOMCSSPrimitiveValue.h"
+#include "mozilla/dom/CSSPrimitiveValueBinding.h"
+#include "mozilla/dom/CSSValueBinding.h"
+
 #include "nsCSSKeywords.h"
 #include "CSSValue.h"
 #include "nsCOMPtr.h"
@@ -20,30 +22,16 @@ class nsIURI;
 class nsDOMCSSRect;
 class nsDOMCSSRGBColor;
 
-// There is no CSS_TURN constant on the CSSPrimitiveValue interface,
-// since that unit is newer than DOM Level 2 Style, and CSS OM will
-// probably expose CSS values in some other way in the future.  We
-// use this value in mType for "turn"-unit angles, but we define it
-// here to avoid exposing it to content.
-#define CSS_TURN 30U
-// Likewise we have some internal aliases for CSS_NUMBER that we don't
-// want to expose.
-#define CSS_NUMBER_INT32 31U
-#define CSS_NUMBER_UINT32 32U
-
 /**
  * Read-only CSS primitive value - a DOM object representing values in DOM
  * computed style.
  */
 class nsROCSSPrimitiveValue final : public mozilla::dom::CSSValue,
-                                    public nsIDOMCSSPrimitiveValue
+                                    public nsIDOMCSSValue
 {
 public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_AMBIGUOUS(nsROCSSPrimitiveValue, mozilla::dom::CSSValue)
-
-  // nsIDOMCSSPrimitiveValue
-  NS_DECL_NSIDOMCSSPRIMITIVEVALUE
 
   // nsIDOMCSSValue
   NS_DECL_NSIDOMCSSVALUE
@@ -54,18 +42,7 @@ public:
   virtual uint16_t CssValueType() const override final;
 
   // CSSPrimitiveValue
-  uint16_t PrimitiveType()
-  {
-    // New value types were introduced but not added to CSS OM.
-    // Return CSS_UNKNOWN to avoid exposing CSS_TURN to content.
-    if (mType > CSS_RGBCOLOR) {
-      if (mType == CSS_NUMBER_INT32 || mType == CSS_NUMBER_UINT32) {
-        return CSS_NUMBER;
-      }
-      return CSS_UNKNOWN;
-    }
-    return mType;
-  }
+  uint16_t PrimitiveType();
   void SetFloatValue(uint16_t aUnitType, float aValue,
                      mozilla::ErrorResult& aRv);
   float GetFloatValue(uint16_t aUnitType, mozilla::ErrorResult& aRv);
@@ -91,9 +68,13 @@ public:
   void SetAppUnits(float aValue);
   void SetIdent(nsCSSKeyword aKeyword);
   // FIXME: CSS_STRING should imply a string with "" and a need for escaping.
-  void SetString(const nsACString& aString, uint16_t aType = CSS_STRING);
+  void SetString(
+      const nsACString& aString,
+      uint16_t aType = mozilla::dom::CSSPrimitiveValueBinding::CSS_STRING);
   // FIXME: CSS_STRING should imply a string with "" and a need for escaping.
-  void SetString(const nsAString& aString, uint16_t aType = CSS_STRING);
+  void SetString(
+      const nsAString& aString,
+      uint16_t aType = mozilla::dom::CSSPrimitiveValueBinding::CSS_STRING);
   void SetURI(nsIURI *aURI);
   void SetColor(nsDOMCSSRGBColor* aColor);
   void SetRect(nsDOMCSSRect* aRect);
@@ -126,11 +107,11 @@ private:
   } mValue;
 };
 
-inline nsROCSSPrimitiveValue *mozilla::dom::CSSValue::AsPrimitiveValue()
+inline nsROCSSPrimitiveValue*
+mozilla::dom::CSSValue::AsPrimitiveValue()
 {
-  return CssValueType() == nsIDOMCSSValue::CSS_PRIMITIVE_VALUE ?
+  return CssValueType() == CSSValueBinding::CSS_PRIMITIVE_VALUE ?
     static_cast<nsROCSSPrimitiveValue*>(this) : nullptr;
 }
 
 #endif /* nsROCSSPrimitiveValue_h___ */
-
