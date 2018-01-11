163dfc2051a3f5877f1a99ec56e2db78efb8c84a
Cameron McCormack <cam@mcc.id.au>
2018-01-11 12:38:01 +0800

    Bug 1427419 - Part 25: Move inIDOMUtils.parseStyleSheet to InspectorUtils. r=bz
    MozReview-Commit-ID: FQlNRP8ycY3

--- layout/inspector/inDOMUtils.cpp
+++ layout/inspector/inDOMUtils.cpp
@@ -1093,27 +1093,35 @@ InspectorUtils::ClearPseudoClassLocks(GlobalObject& aGlobalObject,
   aElement.ClearStyleStateLocks();
 }
 
+/* static */ void
+InspectorUtils::ParseStyleSheet(GlobalObject& aGlobalObject,
+                                StyleSheet& aSheet,
+                                const nsAString& aInput,
+                                ErrorResult& aRv)
+{
+  RefPtr<CSSStyleSheet> geckoSheet = do_QueryObject(&aSheet);
+  if (geckoSheet) {
+    nsresult rv = geckoSheet->ReparseSheet(aInput);
+    if (NS_FAILED(rv)) {
+      aRv.Throw(rv);
+    }
+    return;
+  }
+
+  RefPtr<ServoStyleSheet> servoSheet = do_QueryObject(&aSheet);
+  if (servoSheet) {
+    nsresult rv = servoSheet->ReparseSheet(aInput);
+    if (NS_FAILED(rv)) {
+      aRv.Throw(rv);
+    }
+    return;
+  }
+
+  aRv.Throw(NS_ERROR_INVALID_POINTER);
+}
+
 } // namespace dom
 } // namespace mozilla
-
-NS_IMETHODIMP
-inDOMUtils::ParseStyleSheet(nsIDOMCSSStyleSheet *aSheet,
-                            const nsAString& aInput)
-{
-  RefPtr<CSSStyleSheet> geckoSheet = do_QueryObject(aSheet);
-  if (geckoSheet) {
-    NS_ENSURE_ARG_POINTER(geckoSheet);
-    return geckoSheet->ReparseSheet(aInput);
-  }
-
-  RefPtr<ServoStyleSheet> servoSheet = do_QueryObject(aSheet);
-  if (servoSheet) {
-    NS_ENSURE_ARG_POINTER(servoSheet);
-    return servoSheet->ReparseSheet(aInput);
-  }
-
-  return NS_ERROR_INVALID_POINTER;
-}
 
 NS_IMETHODIMP
 inDOMUtils::ScrollElementIntoView(nsIDOMElement *aElement)
