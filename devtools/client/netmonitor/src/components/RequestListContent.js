8a84c075d52ddd86ebaa1d3e1d37a62afbf9d121
Alexandre Poirot <poirot.alex@gmail.com>
2018-01-11 07:05:20 -0800

    Bug 1429824 - Prevent reflows related to scroll to bottom feature. r=Honza
    MozReview-Commit-ID: Hnr5tIvTeUv

--- devtools/client/netmonitor/src/components/RequestListContent.js
+++ devtools/client/netmonitor/src/components/RequestListContent.js
@@ -95,22 +95,25 @@ class RequestListContent extends Component {
       toggleDelay: REQUESTS_TOOLTIP_TOGGLE_DELAY,
       interactive: true
     });
+    // Set a flag on "scroll" event, used in componentWillUpdate, in order to help
+    // knowing if we should keep automatically scrolling to bottom without doing
+    // too many reflows.
+    this.scrolledManually = false;
     // Install event handler to hide the tooltip on scroll
     this.refs.contentEl.addEventListener("scroll", this.onScroll, true);
     this.onResize();
   }
 
-  componentWillUpdate(nextProps) {
+  componentDidUpdate(prevProps) {
     // Check if the list is scrolled to bottom before the UI update.
     // The scroll is ever needed only if new rows are added to the list.
-    const delta = nextProps.displayedRequests.size - this.props.displayedRequests.size;
-    this.shouldScrollBottom = delta > 0 && this.isScrolledToBottom();
-  }
-
-  componentDidUpdate(prevProps) {
+    const hasNewRequests = this.props.displayedRequests.size -
+      prevProps.displayedRequests.size > 0;
+    this.shouldScrollBottom = hasNewRequests && !this.scrolledManually;
+    this.ignoreNextScroll = hasNewRequests;
     let node = this.refs.contentEl;
     // Keep the list scrolled to bottom if a new row was added
-    if (this.shouldScrollBottom && node.scrollTop !== MAX_SCROLL_HEIGHT) {
+    if (this.shouldScrollBottom) {
       // Using maximum scroll height rather than node.scrollHeight to avoid sync reflow.
       node.scrollTop = MAX_SCROLL_HEIGHT;
     }
@@ -193,6 +196,14 @@ class RequestListContent extends Component {
    */
   onScroll() {
     this.tooltip.hide();
+
+    // Ignore scroll related to new requests being displayed
+    // To prevent slow reflows done in isScrolledToBottom.
+    if (this.ignoreNextScroll) {
+      this.ignoreNextScroll = false;
+      return;
+    }
+    this.scrolledManually = !this.isScrolledToBottom();
   }
 
   /**
