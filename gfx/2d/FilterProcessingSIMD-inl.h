be2f8d0420d6dde32e87e2cbecf62d1b707945fc
Andrew Osmond <aosmond@mozilla.com>
2018-01-11 08:48:46 -0500

    Bug 1428946 - Part 2. Make ApplyBlending_SIMD and ApplyArithmeticCombine_SIMD support the same surface for both inputs. r=bas

--- gfx/2d/FilterProcessingSIMD-inl.h
+++ gfx/2d/FilterProcessingSIMD-inl.h
@@ -291,29 +291,21 @@ ShuffleAndPackComponents(i32x4_t bbbb1234, i32x4_t gggg1234,
 }
 
 template<typename i32x4_t, typename i16x8_t, typename u8x16_t, BlendMode mode>
-inline already_AddRefed<DataSourceSurface>
-ApplyBlending_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInput2)
+inline void
+ApplyBlending_SIMD(const DataSourceSurface::ScopedMap& aInputMap1,
+                   const DataSourceSurface::ScopedMap& aInputMap2,
+                   const DataSourceSurface::ScopedMap& aOutputMap,
+                   const IntSize& aSize)
 {
-  IntSize size = aInput1->GetSize();
-  RefPtr<DataSourceSurface> target =
-    Factory::CreateDataSourceSurface(size, SurfaceFormat::B8G8R8A8);
-  if (!target) {
-    return nullptr;
-  }
+  uint8_t* source1Data = aInputMap1.GetData();
+  uint8_t* source2Data = aInputMap2.GetData();
+  uint8_t* targetData = aOutputMap.GetData();
+  int32_t targetStride = aOutputMap.GetStride();
+  int32_t source1Stride = aInputMap1.GetStride();
+  int32_t source2Stride = aInputMap2.GetStride();
 
-  DataSourceSurface::ScopedMap inputMap1(aInput1, DataSourceSurface::READ);
-  DataSourceSurface::ScopedMap inputMap2(aInput2, DataSourceSurface::READ);
-  DataSourceSurface::ScopedMap outputMap(target, DataSourceSurface::READ_WRITE);
-
-  uint8_t* source1Data = inputMap1.GetData();
-  uint8_t* source2Data = inputMap2.GetData();
-  uint8_t* targetData = outputMap.GetData();
-  int32_t targetStride = outputMap.GetStride();
-  int32_t source1Stride = inputMap1.GetStride();
-  int32_t source2Stride = inputMap2.GetStride();
-
-  for (int32_t y = 0; y < size.height; y++) {
-    for (int32_t x = 0; x < size.width; x += 4) {
+  for (int32_t y = 0; y < aSize.height; y++) {
+    for (int32_t x = 0; x < aSize.width; x += 4) {
       int32_t targetIndex = y * targetStride + 4 * x;
       int32_t source1Index = y * source1Stride + 4 * x;
       int32_t source2Index = y * source2Stride + 4 * x;
@@ -343,6 +335,27 @@ ApplyBlending_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInput2)
       u8x16_t result1234 = ShuffleAndPackComponents<i32x4_t,i16x8_t,u8x16_t>(blendedB, blendedG, blendedR, blendedA);
       simd::Store8(&targetData[targetIndex], result1234);
     }
+  }
+}
+
+template<typename i32x4_t, typename i16x8_t, typename u8x16_t, BlendMode mode>
+inline already_AddRefed<DataSourceSurface>
+ApplyBlending_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInput2)
+{
+  IntSize size = aInput1->GetSize();
+  RefPtr<DataSourceSurface> target =
+    Factory::CreateDataSourceSurface(size, SurfaceFormat::B8G8R8A8);
+  if (!target) {
+    return nullptr;
+  }
+
+  DataSourceSurface::ScopedMap inputMap1(aInput1, DataSourceSurface::READ);
+  DataSourceSurface::ScopedMap outputMap(target, DataSourceSurface::READ_WRITE);
+  if (aInput1->Equals(aInput2)) {
+    ApplyBlending_SIMD<i32x4_t,i16x8_t,u8x16_t,mode>(inputMap1, inputMap1, outputMap, size);
+  } else {
+    DataSourceSurface::ScopedMap inputMap2(aInput2, DataSourceSurface::READ);
+    ApplyBlending_SIMD<i32x4_t,i16x8_t,u8x16_t,mode>(inputMap1, inputMap2, outputMap, size);
   }
 
   return target.forget();
@@ -1030,27 +1043,19 @@ ArithmeticCombineTwoPixels(i16x8_t in1, i16x8_t in2,
 }
 
 template<typename i32x4_t, typename i16x8_t, typename u8x16_t>
-static already_AddRefed<DataSourceSurface>
-ApplyArithmeticCombine_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInput2,
+static void
+ApplyArithmeticCombine_SIMD(const DataSourceSurface::ScopedMap& aInputMap1,
+                            const DataSourceSurface::ScopedMap& aInputMap2,
+                            const DataSourceSurface::ScopedMap& aOutputMap,
+                            const IntSize& aSize,
                             Float aK1, Float aK2, Float aK3, Float aK4)
 {
-  IntSize size = aInput1->GetSize();
-  RefPtr<DataSourceSurface> target =
-  Factory::CreateDataSourceSurface(size, SurfaceFormat::B8G8R8A8);
-  if (!target) {
-    return nullptr;
-  }
-
-  DataSourceSurface::ScopedMap inputMap1(aInput1, DataSourceSurface::READ);
-  DataSourceSurface::ScopedMap inputMap2(aInput2, DataSourceSurface::READ);
-  DataSourceSurface::ScopedMap outputMap(target, DataSourceSurface::READ_WRITE);
-
-  uint8_t* source1Data = inputMap1.GetData();
-  uint8_t* source2Data = inputMap2.GetData();
-  uint8_t* targetData = outputMap.GetData();
-  uint32_t source1Stride = inputMap1.GetStride();
-  uint32_t source2Stride = inputMap2.GetStride();
-  uint32_t targetStride = outputMap.GetStride();
+  uint8_t* source1Data = aInputMap1.GetData();
+  uint8_t* source2Data = aInputMap2.GetData();
+  uint8_t* targetData = aOutputMap.GetData();
+  uint32_t source1Stride = aInputMap1.GetStride();
+  uint32_t source2Stride = aInputMap2.GetStride();
+  uint32_t targetStride = aOutputMap.GetStride();
 
   // The arithmetic combine filter does the following calculation:
   // result = k1 * in1 * in2 + k2 * in1 + k3 * in2 + k4
@@ -1072,8 +1077,8 @@ ApplyArithmeticCombine_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInpu
   i16x8_t k1And4 = simd::InterleaveLo16(k1, k4);
   i16x8_t k2And3 = simd::InterleaveLo16(k2, k3);
 
-  for (int32_t y = 0; y < size.height; y++) {
-    for (int32_t x = 0; x < size.width; x += 4) {
+  for (int32_t y = 0; y < aSize.height; y++) {
+    for (int32_t x = 0; x < aSize.width; x += 4) {
       uint32_t source1Index = y * source1Stride + 4 * x;
       uint32_t source2Index = y * source2Stride + 4 * x;
       uint32_t targetIndex = y * targetStride + 4 * x;
@@ -1093,6 +1098,31 @@ ApplyArithmeticCombine_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInpu
       // Pack and store.
       simd::Store8(&targetData[targetIndex], simd::PackAndSaturate16To8(result_12, result_34));
     }
+  }
+}
+
+template<typename i32x4_t, typename i16x8_t, typename u8x16_t>
+static already_AddRefed<DataSourceSurface>
+ApplyArithmeticCombine_SIMD(DataSourceSurface* aInput1, DataSourceSurface* aInput2,
+                            Float aK1, Float aK2, Float aK3, Float aK4)
+{
+  IntSize size = aInput1->GetSize();
+  RefPtr<DataSourceSurface> target =
+  Factory::CreateDataSourceSurface(size, SurfaceFormat::B8G8R8A8);
+  if (!target) {
+    return nullptr;
+  }
+
+  DataSourceSurface::ScopedMap inputMap1(aInput1, DataSourceSurface::READ);
+  DataSourceSurface::ScopedMap outputMap(target, DataSourceSurface::READ_WRITE);
+
+  if (aInput1->Equals(aInput2)) {
+    ApplyArithmeticCombine_SIMD<i32x4_t, i16x8_t, u8x16_t>(inputMap1, inputMap1, outputMap,
+                                                           size, aK1, aK2, aK3, aK4);
+  } else {
+    DataSourceSurface::ScopedMap inputMap2(aInput2, DataSourceSurface::READ);
+    ApplyArithmeticCombine_SIMD<i32x4_t, i16x8_t, u8x16_t>(inputMap1, inputMap2, outputMap,
+                                                           size, aK1, aK2, aK3, aK4);
   }
 
   return target.forget();
