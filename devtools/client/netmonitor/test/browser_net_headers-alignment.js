bb1c0d3310b37d3139af02e7e67ff829fd4e4de1
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-01-12 09:49:16 +0200

    Backed out changeset 76d24a5414eb (bug 1429721) for devtools failure on browser_net_headers-alignment.js. on a CLOSED TREE

--- devtools/client/netmonitor/test/browser_net_headers-alignment.js
+++ devtools/client/netmonitor/test/browser_net_headers-alignment.js
@@ -7,29 +7,41 @@
  * Bug 1360457 - Mis-alignment between headers and columns on overflow
  */
 
-add_task(async function () {
+add_task(function* () {
   requestLongerTimeout(4);
 
-  let { tab, monitor } = await initNetMonitor(INFINITE_GET_URL, true);
+  let { tab, monitor } = yield initNetMonitor(INFINITE_GET_URL, true);
   let { document, windowRequire, store } = monitor.panelWin;
   let Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
 
   store.dispatch(Actions.batchEnable(false));
 
   // Wait until the first request makes the empty notice disappear
-  await waitForRequestListToAppear();
+  yield waitForRequestListToAppear();
 
   let requestsContainer = document.querySelector(".requests-list-contents");
   ok(requestsContainer, "Container element exists as expected.");
   let headers = document.querySelector(".requests-list-headers");
   ok(headers, "Headers element exists as expected.");
 
-  await waitForRequestsToOverflowContainer();
+  yield waitForRequestsToOverflowContainer();
 
-  testColumnsAlignment(headers, requestsContainer);
+  // Get first request line, not child 0 as this is the headers
+  let firstRequestLine = requestsContainer.childNodes[1];
+
+  // Find number of columns
+  let numberOfColumns = headers.childElementCount;
+  for (let columnNumber = 0; columnNumber < numberOfColumns; columnNumber++) {
+    let aHeaderColumn = headers.childNodes[columnNumber];
+    let aRequestColumn = firstRequestLine.childNodes[columnNumber];
+    is(aHeaderColumn.getBoundingClientRect().left,
+       aRequestColumn.getBoundingClientRect().left,
+       "Headers for columns number " + columnNumber + " are aligned."
+    );
+  }
 
   // Stop doing requests.
-  await ContentTask.spawn(tab.linkedBrowser, {}, function* () {
+  yield ContentTask.spawn(tab.linkedBrowser, {}, function* () {
     content.wrappedJSObject.stopRequests();
   });
 
@@ -40,16 +52,16 @@ add_task(async function () {
     info("Waiting until the empty notice disappears and is replaced with the list");
     return waitUntil(() => !!document.querySelector(".requests-list-contents"));
   }
-});
 
-async function waitForRequestsToOverflowContainer(monitor, requestList) {
-  info("Waiting for enough requests to overflow the container");
-  while (true) {
-    info("Waiting for one network request");
-    await waitForNetworkEvents(monitor, 1);
-    if (requestList.scrollHeight > requestList.clientHeight) {
-      info("The list is long enough, returning");
-      return;
+  function* waitForRequestsToOverflowContainer() {
+    info("Waiting for enough requests to overflow the container");
+    while (true) {
+      info("Waiting for one network request");
+      yield waitForNetworkEvents(monitor, 1);
+      if (requestsContainer.scrollHeight > requestsContainer.clientHeight) {
+        info("The list is long enough, returning");
+        return;
+      }
     }
   }
-}
+});
