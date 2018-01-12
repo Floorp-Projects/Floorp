de7889f89601a32313fd4ee04ef52d70b3192788
Aki Sasaki <asasaki@mozilla.com>
2018-01-12 13:14:16 -0800

    bug 1430246 - add mar_tools_url to m-b and m-r. r=callek
    This change is needed for TC l10n. This will ride the trains with 59.
    
    MozReview-Commit-ID: 8UhVSwAj4Uh

--- testing/mozharness/configs/single_locale/mozilla-release.py
+++ testing/mozharness/configs/single_locale/mozilla-release.py
@@ -7,6 +7,9 @@ config = {
     # l10n
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
 
+    # mar
+    "mar_tools_url": os.environ["MAR_TOOLS_URL"],
+
     # repositories
     "mozilla_dir": "mozilla-release",
     "repos": [{
