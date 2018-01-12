cfc180feedc38f042c334fd270177b19731763ea
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-01-12 08:56:13 +0100

    Bug 1405340 - Enable browser_webconsole_hpkp_onvalid-headers.js in new console frontend;r=Honza.
    MozReview-Commit-ID: 4bcZR2HrFOd
    
    --HG--
    extra : rebase_source : 1c62f66a1ea29c12fc69295c43e637fc744e798c

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_hpkp_invalid-headers.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_hpkp_invalid-headers.js
@@ -1,74 +1,68 @@
-/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
-/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// Tests that errors about invalid HPKP security headers are logged to the web
-// console.
+// Tests that errors about invalid HPKP security headers are logged to the web console.
 
 "use strict";
 
-const TEST_URI = "data:text/html;charset=utf-8,Web Console HPKP invalid " +
-                 "header test";
+const TEST_URI = "data:text/html;charset=utf-8,Web Console HPKP invalid header test";
 const SJS_URL = "https://example.com/browser/devtools/client/webconsole/" +
-                "test/test_hpkp-invalid-headers.sjs";
-const LEARN_MORE_URI = "https://developer.mozilla.org/docs/Web/HTTP/" +
-                       "Public_Key_Pinning" + DOCS_GA_PARAMS;
-const NON_BUILTIN_ROOT_PREF = "security.cert_pinning.process_headers_from_" +
-                              "non_builtin_roots";
+                "new-console-output/test/mochitest/test_hpkp-invalid-headers.sjs";
+const LEARN_MORE_URI =
+  "https://developer.mozilla.org/docs/Web/HTTP/Public_Key_Pinning" + DOCS_GA_PARAMS;
+const NON_BUILTIN_ROOT_PREF =
+  "security.cert_pinning.process_headers_from_non_builtin_roots";
 
-add_task(function* () {
+add_task(async function () {
   registerCleanupFunction(() => {
     Services.prefs.clearUserPref(NON_BUILTIN_ROOT_PREF);
   });
 
-  yield loadTab(TEST_URI);
+  let hud = await openNewTabAndConsole(TEST_URI);
 
-  let hud = yield openConsole();
-
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?badSyntax",
     name: "Could not parse header error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that could not be " +
           "parsed successfully."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?noMaxAge",
     name: "No max-age error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that did not include " +
           "a \u2018max-age\u2019 directive."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?invalidIncludeSubDomains",
     name: "Invalid includeSubDomains error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that included an " +
           "invalid \u2018includeSubDomains\u2019 directive."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?invalidMaxAge",
     name: "Invalid max-age error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that included an " +
           "invalid \u2018max-age\u2019 directive."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?multipleIncludeSubDomains",
     name: "Multiple includeSubDomains error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that included " +
           "multiple \u2018includeSubDomains\u2019 directives."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?multipleMaxAge",
     name: "Multiple max-age error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that included " +
           "multiple \u2018max-age\u2019 directives."
   }, hud);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?multipleReportURIs",
     name: "Multiple report-uri error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that included " +
@@ -79,7 +73,7 @@ add_task(function* () {
   // true to have the PKP implementation return more specific errors.
   Services.prefs.setBoolPref(NON_BUILTIN_ROOT_PREF, true);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?pinsetDoesNotMatch",
     name: "Non-matching pinset error displayed successfully",
     text: "Public-Key-Pins: The site specified a header that did not include " +
@@ -88,7 +82,7 @@ add_task(function* () {
 
   Services.prefs.setBoolPref(NON_BUILTIN_ROOT_PREF, false);
 
-  yield* checkForMessage({
+  await navigateAndCheckForWarningMessage({
     url: SJS_URL + "?pinsetDoesNotMatch",
     name: "Non-built-in root error displayed successfully",
     text: "Public-Key-Pins: The certificate used by the site was not issued " +
@@ -97,30 +91,17 @@ add_task(function* () {
   }, hud);
 });
 
-function* checkForMessage(curTest, hud) {
-  hud.jsterm.clearOutput();
+async function navigateAndCheckForWarningMessage({name, text, url}, hud) {
+  hud.jsterm.clearOutput(true);
 
-  BrowserTestUtils.loadURI(gBrowser.selectedBrowser, curTest.url);
+  const onMessage = waitForMessage(hud, text, ".message.warning");
+  BrowserTestUtils.loadURI(gBrowser.selectedBrowser, url);
+  const {node} = await onMessage;
+  ok(node, name);
 
-  let results = yield waitForMessages({
-    webconsole: hud,
-    messages: [
-      {
-        name: curTest.name,
-        text: curTest.text,
-        category: CATEGORY_SECURITY,
-        severity: SEVERITY_WARNING,
-        objects: true,
-      },
-    ],
-  });
-
-  yield testClickOpenNewTab(hud, results);
-}
-
-function testClickOpenNewTab(hud, results) {
-  let warningNode = results[0].clickableElements[0];
-  ok(warningNode, "link element");
-  ok(warningNode.classList.contains("learn-more-link"), "link class name");
-  return simulateMessageLinkClick(warningNode, LEARN_MORE_URI);
+  let learnMoreNode = node.querySelector(".learn-more-link");
+  ok(learnMoreNode, `There is a "Learn more" link`);
+  const navigatedLink = await simulateLinkClick(learnMoreNode);
+  is(navigatedLink, LEARN_MORE_URI,
+    "Click on the learn more link navigates the user to the expected url");
 }
