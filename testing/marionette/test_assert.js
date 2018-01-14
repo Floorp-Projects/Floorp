f92fc1fd6a70c785ead09672acec245a2a8325f6
Noemi Erli <nerli@mozilla.com>
2018-01-14 18:28:42 +0200

    Backed out changeset e8ae7190dd15 (bug 1430109) for Wr failures in /builds/worker/workspace/build/tests/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py on a CLOSED TREE

--- testing/marionette/test_assert.js
+++ testing/marionette/test_assert.js
@@ -169,14 +169,27 @@ add_test(function test_string() {
   run_next_test();
 });
 
-add_test(function test_open() {
-  assert.open({closed: false});
+add_test(function test_window() {
+  assert.window({closed: false});
 
   for (let typ of [null, undefined, {closed: true}]) {
-    Assert.throws(() => assert.open(typ), NoSuchWindowError);
+    Assert.throws(() => assert.window(typ), NoSuchWindowError);
   }
 
-  Assert.throws(() => assert.open(null, "custom"), /custom/);
+  Assert.throws(() => assert.window(null, "custom"), /custom/);
+
+  run_next_test();
+});
+
+add_test(function test_contentBrowser() {
+  assert.contentBrowser({contentBrowser: 42, window: {closed: false}});
+
+  let closedWindow = {contentBrowser: 42, window: {closed: true}};
+  let noContentBrowser = {contentBrowser: null, window: {closed: false}};
+
+  for (let typ of [null, undefined, closedWindow, noContentBrowser]) {
+    Assert.throws(() => assert.contentBrowser(typ), NoSuchWindowError);
+  }
 
   run_next_test();
 });
