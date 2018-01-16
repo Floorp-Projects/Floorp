271544691d9ee8f3fd68b40b43ba530549669a82
Emilio Cobos Álvarez <emilio@crisal.io>
2018-01-16 20:48:58 +0100

    Bug 1430844: Add assertions that would've caught this. r=bz
    MozReview-Commit-ID: 1UfhIRn2We2

--- layout/base/GeckoRestyleManager.cpp
+++ layout/base/GeckoRestyleManager.cpp
@@ -562,6 +562,8 @@ GeckoRestyleManager::ProcessPendingRestyles()
   NS_PRECONDITION(PresContext()->Document(), "No document?  Pshaw!");
   NS_PRECONDITION(!nsContentUtils::IsSafeToRunScript(),
                   "Missing a script blocker!");
+  MOZ_ASSERT(!PresContext()->HasPendingMediaQueryUpdates(),
+             "Someone forgot to update media queries?");
 
   // First do any queued-up frame creation.  (We should really
   // merge this into the rest of the process, though; see bug 827239.)
