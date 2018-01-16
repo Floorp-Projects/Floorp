ecb122402892ef095a70bb7f49e2f76e49ac9299
Martin Stransky <stransky@redhat.com>
2018-01-16 10:26:58 +0100

    Bug 1430728 - [Wayland] - position wl_subsurface when attached GtkWidget allocation changes, r=jhorak
    MozReview-Commit-ID: 1q3XIouxavQ
    
    --HG--
    extra : rebase_source : 59fcb138c55fe1250b26c4e1ec120a14c8c4c4ad

--- widget/gtk/mozcontainer.cpp
+++ widget/gtk/mozcontainer.cpp
@@ -424,6 +424,17 @@ moz_container_size_allocate (GtkWidget     *widget,
                                allocation->width,
                                allocation->height);
     }
+
+#if defined(MOZ_WAYLAND)
+    // We need to position our subsurface according to GdkWindow
+    // when offset changes (GdkWindow is maximized for instance).
+    // see gtk-clutter-embed.c for reference.
+    if (container->subsurface) {
+        gint x, y;
+        gdk_window_get_position(gtk_widget_get_window(widget), &x, &y);
+        wl_subsurface_set_position(container->subsurface, x, y);
+    }
+#endif
 }
 
 void
