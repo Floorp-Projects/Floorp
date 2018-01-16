25fd711e726aa4c3dc7e1ca25467ab5d17b838bf
Aki Sasaki <asasaki@mozilla.com>
2018-01-16 15:12:31 -0800

    bug 1430353 - allow for an easier to read treeherder relpro symbol. r=bstack

--- taskcluster/taskgraph/actions/registry.py
+++ taskcluster/taskgraph/actions/registry.py
@@ -166,7 +166,9 @@ def register_callback_action(name, title, symbol, description, order=10000,
     def register_callback(cb):
         assert isinstance(cb, FunctionType), 'callback must be a function'
         assert isinstance(symbol, basestring), 'symbol must be a string'
-        assert 1 <= len(symbol) <= 25, 'symbol must be between 1 and 25 characters'
+        # Allow for json-e > 25 chars in the symbol.
+        if '$' not in symbol:
+            assert 1 <= len(symbol) <= 25, 'symbol must be between 1 and 25 characters'
         assert not mem['registered'], 'register_callback_action must be used as decorator'
         assert cb.__name__ not in callbacks, 'callback name {} is not unique'.format(cb.__name__)
 
