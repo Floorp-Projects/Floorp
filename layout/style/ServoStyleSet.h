7ce02a51df49ca3f0a16a2f4ed0cc84f66fa0d69
Emilio Cobos Álvarez <emilio@crisal.io>
2018-01-16 15:14:39 +0100

    Bug 1409672: Handle document state changes using the invalidation machinery. r=xidorn
    MozReview-Commit-ID: EoSMrYPS7dl

--- layout/style/ServoStyleSet.h
+++ layout/style/ServoStyleSet.h
@@ -146,6 +146,9 @@ public:
   // the relevant AppendSheet / RemoveSheet...
   void RecordStyleSheetChange(ServoStyleSheet*, StyleSheet::ChangeType) {}
 
+  // Runs style invalidation due to document state changes.
+  void InvalidateStyleForDocumentStateChanges(EventStates aStatesChanged);
+
   void RecordShadowStyleChange(dom::ShadowRoot* aShadowRoot) {
     // FIXME(emilio): When we properly support shadow dom we'll need to do
     // better.
