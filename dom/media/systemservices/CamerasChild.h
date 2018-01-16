b86d0e5f0dca17955d95363a552a942e43ac0690
Munro Mengjue Chiang <mchiang@mozilla.com>
2018-01-16 15:27:09 +0800

    Bug 1426129 - Hold CamerasChild via promoting "this" to a RefPtr. r=jib
    MozReview-Commit-ID: JdZqnGxlsBV

--- dom/media/systemservices/CamerasChild.h
+++ dom/media/systemservices/CamerasChild.h
@@ -148,7 +148,7 @@ class CamerasChild final : public PCamerasChild
 public:
   // We are owned by the PBackground thread only. CamerasSingleton
   // takes a non-owning reference.
-  NS_INLINE_DECL_REFCOUNTING(CamerasChild)
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CamerasChild)
 
   // IPC messages recevied, received on the PBackground thread
   // these are the actual callbacks with data
