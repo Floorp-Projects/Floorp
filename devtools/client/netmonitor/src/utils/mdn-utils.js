4704578a6a8604cfdb0c8afe61fe341ddb79c048
abhinav <abhinav.koppula@gmail.com>
2018-01-16 07:14:43 +0530

    Bug 1421213 - Clicking on the request status code should open the corresponding MDN page. r=nchevobbe
    MozReview-Commit-ID: JlU7pJiZ689

--- devtools/client/netmonitor/src/utils/mdn-utils.js
+++ devtools/client/netmonitor/src/utils/mdn-utils.js
@@ -141,8 +141,9 @@ const SUPPORTED_HTTP_CODES = [
 ];
 
 const MDN_URL = "https://developer.mozilla.org/docs/";
-const GA_PARAMS =
-  "?utm_source=mozilla&utm_medium=devtools-netmonitor&utm_campaign=default";
+const getGAParams = (panelId = "netmonitor") => {
+  return `?utm_source=mozilla&utm_medium=devtools-${panelId}&utm_campaign=default`;
+};
 
 /**
  * Get the MDN URL for the specified header.
@@ -156,7 +157,7 @@ function getHeadersURL(header) {
   let idx = SUPPORTED_HEADERS.findIndex(item =>
     item.toLowerCase() === lowerCaseHeader);
   return idx > -1 ?
-    `${MDN_URL}Web/HTTP/Headers/${SUPPORTED_HEADERS[idx] + GA_PARAMS}` : null;
+    `${MDN_URL}Web/HTTP/Headers/${SUPPORTED_HEADERS[idx] + getGAParams()}` : null;
 }
 
 /**
@@ -166,10 +167,11 @@ function getHeadersURL(header) {
  *
  * @return {string} The MDN URL for the HTTP status code, or null if not available.
  */
-function getHTTPStatusCodeURL(statusCode) {
+function getHTTPStatusCodeURL(statusCode, panelId) {
   let idx = SUPPORTED_HTTP_CODES.indexOf(statusCode);
   return idx > -1 ?
-    `${MDN_URL}Web/HTTP/Status/${SUPPORTED_HTTP_CODES[idx] + GA_PARAMS}` : null;
+    `${MDN_URL}Web/HTTP/Status/${SUPPORTED_HTTP_CODES[idx] + getGAParams(panelId)}`
+      : null;
 }
 
 /**
@@ -178,7 +180,7 @@ function getHTTPStatusCodeURL(statusCode) {
  * @return {string} the MDN URL of the Timings tag for Network Monitor.
  */
 function getNetMonitorTimingsURL() {
-  return `${MDN_URL}Tools/Network_Monitor${GA_PARAMS}#Timings`;
+  return `${MDN_URL}Tools/Network_Monitor${getGAParams()}#Timings`;
 }
 
 /**
@@ -187,7 +189,7 @@ function getNetMonitorTimingsURL() {
  * @return {string} The MDN URL for the documentation of Performance Analysis.
  */
 function getPerformanceAnalysisURL() {
-  return `${MDN_URL}Tools/Network_Monitor${GA_PARAMS}#Performance_analysis`;
+  return `${MDN_URL}Tools/Network_Monitor${getGAParams()}#Performance_analysis`;
 }
 
 module.exports = {
