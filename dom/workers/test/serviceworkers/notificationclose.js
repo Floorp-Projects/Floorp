14c398e935fe3e7229ce2909adf94426dbb14188
Tom Tung <shes050117@gmail.com>
2018-01-17 11:31:31 +0800

    Bug 1429093 - P2: A test to verify the close event is not allowed to window interaction. r=bkelly

--- dom/workers/test/serviceworkers/notificationclose.js
+++ dom/workers/test/serviceworkers/notificationclose.js
@@ -2,18 +2,27 @@
 // http://creativecommons.org/publicdomain/zero/1.0/
 //
 onnotificationclose = function(e) {
-  self.clients.matchAll().then(function(clients) {
-    if (clients.length === 0) {
-      dump("********************* CLIENTS LIST EMPTY! Test will timeout! ***********************\n");
-      return;
-    }
+  e.waitUntil(async function() {
+    let windowOpened = true;
+    await clients.openWindow("hello.html")
+      .catch(err => {
+        windowOpened = false;
+      });
 
-    clients.forEach(function(client) {
-      client.postMessage({ result: e.notification.data &&
-                                   e.notification.data['complex'] &&
-                                   e.notification.data['complex'][0] == "jsval" &&
-                                   e.notification.data['complex'][1] == 5 });
+    self.clients.matchAll().then(function(clients) {
+      if (clients.length === 0) {
+        dump("*** CLIENTS LIST EMPTY! Test will timeout! ***\n");
+        return;
+      }
 
+      clients.forEach(function(client) {
+        client.postMessage({ result: e.notification.data &&
+                                     e.notification.data['complex'] &&
+                                     e.notification.data['complex'][0] == "jsval" &&
+                                     e.notification.data['complex'][1] == 5,
+                             windowOpened: windowOpened});
+
+      });
     });
-  });
+  }());
 }
