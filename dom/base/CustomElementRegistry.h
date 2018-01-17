eb56380ce246de98615eb329aad472b12ce51e88
Edgar Chen <top12345tw@gmail.com>
2018-01-17 14:55:13 +0800

    Bug 1430951 - Avoid element name atomizing to improve performance of LookupCustomElementDefinition; r=smaug
    Since we are dealing with the element (nodeInfo->LocalName() and NameAtom() are the same value),
    we could use nodeInfo->NameAtom() instead.
    
    MozReview-Commit-ID: 4vIBDEM1Nwv

--- dom/base/CustomElementRegistry.h
+++ dom/base/CustomElementRegistry.h
@@ -376,7 +376,7 @@ public:
    * https://html.spec.whatwg.org/#look-up-a-custom-element-definition
    */
   CustomElementDefinition* LookupCustomElementDefinition(
-    const nsAString& aLocalName, nsAtom* aTypeAtom) const;
+    nsAtom* aNameAtom, nsAtom* aTypeAtom) const;
 
   CustomElementDefinition* LookupCustomElementDefinition(
     JSContext* aCx, JSObject *aConstructor) const;
