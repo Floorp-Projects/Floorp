3c7b9479c31571e8b2ccc8c9e4a3d7a9f0a22f34
Andreea Pavel <apavel@mozilla.com>
2018-01-19 01:38:11 +0200

    Backed out changeset 70c2890fb875 (bug 1429994) for static-analysis bustages on Windows

--- gfx/webrender_bindings/RenderCompositorOGL.h
+++ gfx/webrender_bindings/RenderCompositorOGL.h
@@ -10,7 +10,19 @@
 #include "mozilla/webrender/RenderCompositor.h"
 
 namespace mozilla {
+/*
+namespace gl {
+class GLContext;
+}
 
+namespace layers {
+class SyncObjectHost;
+}
+
+namespace widget {
+class CompositorWidget;
+}
+*/
 namespace wr {
 
 class RenderCompositorOGL : public RenderCompositor
@@ -30,7 +42,7 @@ public:
 
   gl::GLContext* gl() const override { return mGL; }
 
-  bool UseANGLE() const override { return false; }
+  bool UseANGLE() const override;
 
   LayoutDeviceIntSize GetClientSize() override;
 
