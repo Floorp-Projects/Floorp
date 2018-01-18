9476c59959e4ed207a367a7b695955dbf16454b0
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-01-18 15:01:27 +0900

    Bug 1430975 - Reduce elapsed time precision for transitioncancel event. r=boris
    See the table for elapsed time for each transition event;
    https://drafts.csswg.org/css-transitions-2/#event-dispatch
    
    MozReview-Commit-ID: DuLip0XkwtE

--- layout/style/nsTransitionManager.h
+++ layout/style/nsTransitionManager.h
@@ -310,7 +310,7 @@ struct TransitionEventInfo {
   TransitionEventInfo(const NonOwningAnimationTarget& aTarget,
                       EventMessage aMessage,
                       nsCSSPropertyID aProperty,
-                      const StickyTimeDuration& aElapsedTime,
+                      double aElapsedTime,
                       const TimeStamp& aTimeStamp,
                       dom::Animation* aAnimation)
     : mElement(aTarget.mElement)
@@ -321,7 +321,7 @@ struct TransitionEventInfo {
     // XXX Looks like nobody initialize WidgetEvent::time
     mEvent.mPropertyName =
       NS_ConvertUTF8toUTF16(nsCSSProps::GetStringValue(aProperty));
-    mEvent.mElapsedTime = aElapsedTime.ToSeconds();
+    mEvent.mElapsedTime = aElapsedTime;
     mEvent.mPseudoElement =
       AnimationCollection<dom::CSSTransition>::PseudoTypeAsString(
         aTarget.mPseudoType);
