79cb24a59a43e119d900614bd1b9046037d32806
Jim Blandy <jimb@mozilla.com>
2018-01-18 11:49:34 -0800

    Bug 1426467: Re-enqueue messages from workers when debugger pause ends; don't run them immediately. r=bkelly
    MozReview-Commit-ID: 1Yyjqz5S6tZ

---
+++ devtools/server/tests/mochitest/suspendTimeouts_worker.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Once content sends us a port connected to the mochitest, we simply echo every
+// message we receive back to content and the mochitest.
+onmessage = ({ data: { mochitestPort }}) => {
+  onmessage = ({ data }) => {
+    // Send a message to both content and the mochitest, which the main thread's
+    // event loop will attempt to deliver as step 2).
+    postMessage(`worker echo to content: ${data}`);
+    mochitestPort.postMessage(`worker echo to port: ${data}`);
+  };
+};
