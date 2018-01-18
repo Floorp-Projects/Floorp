5afeb1d562d1256dc345ce32cb88c10d2c46a976
sotaro <sotaro.ikeda.g@gmail.com>
2018-01-19 07:49:08 +0900

    Bug 1429994 - Use ID3D11Query to wait previous frame completion r=nical

--- gfx/webrender_bindings/RenderCompositorANGLE.h
+++ gfx/webrender_bindings/RenderCompositorANGLE.h
@@ -4,23 +4,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MOZILLA_GFX_RENDERCOMPOSITOR_OGL_H
-#define MOZILLA_GFX_RENDERCOMPOSITOR_OGL_H
+#ifndef MOZILLA_GFX_RENDERCOMPOSITOR_ANGLE_H
+#define MOZILLA_GFX_RENDERCOMPOSITOR_ANGLE_H
 
 #include "mozilla/webrender/RenderCompositor.h"
+
+struct ID3D11DeviceContext;
+struct ID3D11Device;
+struct ID3D11Query;
 
 namespace mozilla {
 
 namespace wr {
 
-class RenderCompositorOGL : public RenderCompositor
+class RenderCompositorANGLE : public RenderCompositor
 {
 public:
   static UniquePtr<RenderCompositor> Create(RefPtr<widget::CompositorWidget>&& aWidget);
 
-  RenderCompositorOGL(RefPtr<gl::GLContext>&& aGL,
-                        RefPtr<widget::CompositorWidget>&& aWidget);
-  virtual ~RenderCompositorOGL();
+  RenderCompositorANGLE(RefPtr<widget::CompositorWidget>&& aWidget);
+  virtual ~RenderCompositorANGLE();
+  bool Initialize();
 
   bool Destroy() override;
   bool BeginFrame() override;
@@ -30,12 +34,22 @@ public:
 
   gl::GLContext* gl() const override { return mGL; }
 
-  bool UseANGLE() const override { return false; }
+  bool UseANGLE() const override { return true; }
 
   LayoutDeviceIntSize GetClientSize() override;
 
 protected:
+  void InsertPresentWaitQuery();
+  void WaitForPreviousPresentQuery();
+
   RefPtr<gl::GLContext> mGL;
+
+  RefPtr<ID3D11Device> mDevice;
+  RefPtr<ID3D11DeviceContext> mCtx;
+
+  RefPtr<ID3D11Query> mWaitForPresentQuery;
+  RefPtr<ID3D11Query> mNextWaitForPresentQuery;
+
 };
 
 } // namespace wr
