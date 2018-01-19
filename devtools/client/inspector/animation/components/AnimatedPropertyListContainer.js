eb760cc6e8bb39f0490e8fa96033679013124903
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-01-19 15:21:15 +0900

    Bug 1416104 - Part 6: Implement keyframes list. r=gl
    MozReview-Commit-ID: 1fMQEGjps7u
    
    --HG--
    extra : rebase_source : 64f2c0dffec75f71a6e025e1d1d5cd560e1eea13

--- devtools/client/inspector/animation/components/AnimatedPropertyListContainer.js
+++ devtools/client/inspector/animation/components/AnimatedPropertyListContainer.js
@@ -6,18 +6,36 @@
 
 const { createFactory, PureComponent } = require("devtools/client/shared/vendor/react");
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 
 const AnimatedPropertyList = createFactory(require("./AnimatedPropertyList"));
 const AnimatedPropertyListHeader = createFactory(require("./AnimatedPropertyListHeader"));
 
 class AnimatedPropertyListContainer extends PureComponent {
+  static get propTypes() {
+    return {
+      animation: PropTypes.object.isRequired,
+      getAnimatedPropertyMap: PropTypes.func.isRequired,
+    };
+  }
+
   render() {
+    const {
+      animation,
+      getAnimatedPropertyMap,
+    } = this.props;
+
     return dom.div(
       {
         className: "animated-property-list-container"
       },
       AnimatedPropertyListHeader(),
-      AnimatedPropertyList()
+      AnimatedPropertyList(
+        {
+          animation,
+          getAnimatedPropertyMap,
+        }
+      )
     );
   }
 }
