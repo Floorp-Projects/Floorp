3361aa79f57f45477de9d7c9dbe824202b7da449
Tom Prince <mozilla@hocat.ca>
2018-01-18 23:39:30 -0700

    Bug 1424146: Remove unused configuration from L10N jobs; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D418

--- testing/mozharness/configs/single_locale/linux_devedition.py
+++ testing/mozharness/configs/single_locale/linux_devedition.py
@@ -2,22 +2,9 @@ config = {
     "platform": "linux",
     "stage_product": "firefox",
     "mozconfig": "src/browser/config/mozconfigs/linux32/l10n-mozconfig-devedition",
-    "bootstrap_env": {
-        "MOZ_OBJDIR": "obj-firefox",
-        "EN_US_BINARY_URL": "%(en_us_binary_url)s",
-        "MOZ_UPDATE_CHANNEL": "%(update_channel)s",
-        "DIST": "%(abs_objdir)s",
-        "L10NBASEDIR": "../../l10n",
-        "MOZ_MAKE_COMPLETE_MAR": "1",
-        'EN_US_PACKAGE_NAME': 'target.tar.bz2',
-    },
     "ssh_key_dir": "/home/mock_mozilla/.ssh",
-    "log_name": "single_locale",
     "objdir": "obj-firefox",
     "vcs_share_base": "/builds/hg-shared",
-
-    # balrog credential file:
-    'balrog_credentials_file': 'oauth.txt',
 
     # l10n
     "ignore_locales": ["en-US", "ja-JP-mac"],
