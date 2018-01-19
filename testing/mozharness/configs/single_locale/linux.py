21337c2e45e370915dd48a7963451242d5ae8032
Tom Prince <mozilla@hocat.ca>
2018-01-18 23:39:30 -0700

    Bug 1424146: Remove unused configuration from L10N jobs; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D418

--- testing/mozharness/configs/single_locale/linux.py
+++ testing/mozharness/configs/single_locale/linux.py
@@ -2,27 +2,14 @@ config = {
     "platform": "linux",
     "stage_product": "firefox",
     "mozconfig": "src/browser/config/mozconfigs/linux32/l10n-mozconfig",
-    "bootstrap_env": {
-        "MOZ_OBJDIR": "obj-firefox",
-        "EN_US_BINARY_URL": "%(en_us_binary_url)s",
-        "MOZ_UPDATE_CHANNEL": "%(update_channel)s",
-        "DIST": "%(abs_objdir)s",
-        "L10NBASEDIR": "../../l10n",
-        "MOZ_MAKE_COMPLETE_MAR": "1",
-        'EN_US_PACKAGE_NAME': 'target.tar.bz2',
-    },
     "ssh_key_dir": "/home/mock_mozilla/.ssh",
     "log_name": "single_locale",
     "objdir": "obj-firefox",
     "vcs_share_base": "/builds/hg-shared",
 
-    # balrog credential file:
-    'balrog_credentials_file': 'oauth.txt',
-
     # l10n
     "ignore_locales": ["en-US", "ja-JP-mac"],
     "l10n_dir": "l10n",
-    "locales_file": "src/browser/locales/all-locales",
     "locales_dir": "browser/locales",
     "hg_l10n_tag": "default",
 
--- testing/mozharness/configs/single_locale/macosx64.py
+++ testing/mozharness/configs/single_locale/macosx64.py
@@ -1,33 +1,15 @@
-import os
-
 config = {
     # mozconfig file to use, it depends on branch and platform names
     "platform": "macosx64",
     "stage_product": "firefox",
     "mozconfig": "src/browser/config/mozconfigs/macosx64/l10n-mozconfig",
-    "bootstrap_env": {
-        "SHELL": '/bin/bash',
-        "MOZ_OBJDIR": "obj-firefox",
-        "EN_US_BINARY_URL": "%(en_us_binary_url)s",
-        "MOZ_UPDATE_CHANNEL": "%(update_channel)s",
-        "MOZ_PKG_PLATFORM": "mac",
-        # "IS_NIGHTLY": "yes",
-        "DIST": "%(abs_objdir)s",
-        "L10NBASEDIR": "../../l10n",
-        "MOZ_MAKE_COMPLETE_MAR": "1",
-        'EN_US_PACKAGE_NAME': 'target.dmg',
-    },
     "ssh_key_dir": "~/.ssh",
-    "log_name": "single_locale",
     "objdir": "obj-firefox",
     "vcs_share_base": "/builds/hg-shared",
 
     "upload_env_extra": {
         "MOZ_PKG_PLATFORM": "mac",
     },
-
-    # balrog credential file:
-    'balrog_credentials_file': 'oauth.txt',
 
     # l10n
     "ignore_locales": ["en-US", "ja"],
