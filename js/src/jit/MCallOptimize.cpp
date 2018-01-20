8435163f90df7c19321425f2e8e78b14315ebc99
Jeff Walden <jwalden@mit.edu>
2018-01-19 16:20:44 -0800

    Bug 1431957 - Move Intl.NumberFormat functionality into builtin/intl/NumberFormat.*.  r=anba

--- js/src/jit/MCallOptimize.cpp
+++ js/src/jit/MCallOptimize.cpp
@@ -12,6 +12,7 @@
 
 #include "builtin/AtomicsObject.h"
 #include "builtin/Intl.h"
+#include "builtin/intl/NumberFormat.h"
 #include "builtin/MapObject.h"
 #include "builtin/SIMD.h"
 #include "builtin/TestingFunctions.h"
