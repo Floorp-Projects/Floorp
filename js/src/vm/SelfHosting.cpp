7ebfbc0eb942802545a08f4b99b351b818baf29f
Jeff Walden <jwalden@mit.edu>
2018-01-19 20:26:54 -0800

    Bug 1431957 - Move Intl.RelativeTimeFormat functionality into builtin/intl/RelativeTimeFormat.*.  r=anba

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -28,6 +28,7 @@
 #include "builtin/intl/DateTimeFormat.h"
 #include "builtin/intl/NumberFormat.h"
 #include "builtin/intl/PluralRules.h"
+#include "builtin/intl/RelativeTimeFormat.h"
 #include "builtin/MapObject.h"
 #include "builtin/ModuleObject.h"
 #include "builtin/Object.h"
