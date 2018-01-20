e484b041cadddc8eb6ac649781987075e268e91b
Jeff Walden <jwalden@mit.edu>
2018-01-19 18:37:09 -0800

    Bug 1431957 - Move Intl.DateTimeFormat functionality into builtin/intl/DateTimeFormat.*.  r=anba

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -25,6 +25,7 @@
 
 #include "builtin/Intl.h"
 #include "builtin/intl/Collator.h"
+#include "builtin/intl/DateTimeFormat.h"
 #include "builtin/intl/NumberFormat.h"
 #include "builtin/MapObject.h"
 #include "builtin/ModuleObject.h"
