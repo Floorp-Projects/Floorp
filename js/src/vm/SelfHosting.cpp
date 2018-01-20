26613775657286efe8167d3a400f10e506966f46
Jeff Walden <jwalden@mit.edu>
2018-01-19 19:03:21 -0800

    Bug 1431957 - Move Intl.PluralRules functionality into builtin/intl/PluralRules.*.  r=anba

--- js/src/vm/SelfHosting.cpp
+++ js/src/vm/SelfHosting.cpp
@@ -27,6 +27,7 @@
 #include "builtin/intl/Collator.h"
 #include "builtin/intl/DateTimeFormat.h"
 #include "builtin/intl/NumberFormat.h"
+#include "builtin/intl/PluralRules.h"
 #include "builtin/MapObject.h"
 #include "builtin/ModuleObject.h"
 #include "builtin/Object.h"
