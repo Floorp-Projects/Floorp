9b5528cb524297e59f9d68b9578a9a7f4f1d011b
Tom Ritter <tom@mozilla.com>
2018-01-22 15:01:49 -0600

    Bug 1432009 Fix MinGW build failure with d_write3.h r=jfkthame
    Bump mingw version to get the newest commit and do not include the
    un-needed dw-extras.h on MinGW (thanks Jacek!)
    
    MozReview-Commit-ID: OjO93XHCxs

--- taskcluster/scripts/misc/build-gcc-mingw32.sh
+++ taskcluster/scripts/misc/build-gcc-mingw32.sh
@@ -18,7 +18,7 @@ gcc_ext=xz
 binutils_version=2.27
 binutils_ext=bz2
 binutils_configure_flags="--target=i686-w64-mingw32"
-mingw_version=8bcc70834969d34e113c5614364c4661cfa5d1be
+mingw_version=0720cca3532c32103d999754a734a79061a749a3
 
 # GPG keys used to sign GCC (collected from 5.1.0, 5.4.0, 6.4.0)
 $GPG --import $data_dir/33C235A34C46AA3FFB293709A328C3A2C3C45C06.key
