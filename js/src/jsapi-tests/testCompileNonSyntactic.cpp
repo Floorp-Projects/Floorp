7944bf0f6e2250b38726f049bd6688690b3c4c68
Kris Maglione <maglione.k@gmail.com>
2018-01-22 18:10:28 -0800

    Bug 1393270: Follow-up: Fix style linter error again. r=bustage CLOSED TREE
    MozReview-Commit-ID: 4Z4EG940Zgw

--- js/src/jsapi-tests/testCompileNonSyntactic.cpp
+++ js/src/jsapi-tests/testCompileNonSyntactic.cpp
@@ -2,9 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "jsapi-tests/tests.h"
-
 #include "gc/GCInternals.h"
+#include "jsapi-tests/tests.h"
 #include "vm/Monitor.h"
 #include "vm/MutexIDs.h"
 
