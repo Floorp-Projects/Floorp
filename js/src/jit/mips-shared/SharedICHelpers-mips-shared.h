2766ecfe780a969417a206ffebc896a95a2e64aa
Jeff Walden <jwalden@mit.edu>
2018-01-23 13:00:54 -0800

    Bug 1432312 - Remove the now-unused EmitUnstowICValues function from SharedICHelpers.h.  rs=nbp
    --HG--
    extra : rebase_source : 58e5c368a92608ef9361ad6e18e41dd6a20335c3

--- js/src/jit/mips-shared/SharedICHelpers-mips-shared.h
+++ js/src/jit/mips-shared/SharedICHelpers-mips-shared.h
@@ -122,31 +122,6 @@ EmitStowICValues(MacroAssembler& masm, int values)
     }
 }
 
-inline void
-EmitUnstowICValues(MacroAssembler& masm, int values, bool discard = false)
-{
-    MOZ_ASSERT(values >= 0 && values <= 2);
-    switch(values) {
-      case 1:
-        // Unstow R0.
-        if (discard)
-            masm.addPtr(Imm32(sizeof(Value)), BaselineStackReg);
-        else
-            masm.popValue(R0);
-        break;
-      case 2:
-        // Unstow R0 and R1.
-        if (discard) {
-            masm.addPtr(Imm32(sizeof(Value) * 2), BaselineStackReg);
-        } else {
-            masm.popValue(R1);
-            masm.popValue(R0);
-        }
-        break;
-    }
-    masm.adjustFrame(-values * sizeof(Value));
-}
-
 template <typename AddrType>
 inline void
 EmitPreBarrier(MacroAssembler& masm, const AddrType& addr, MIRType type)
