302b874ffdaf71e2d7a526814011d7e072605a02
Magnus Melin <mkmelin+mozilla@iki.fi>
2018-01-23 23:31:17 +0200

    Bug 1408867 - Make sure specualtive connections are enabled for tests where it's assumed. r=mcmanus, a=lizzard

--- toolkit/mozapps/update/tests/unit_aus_update/downloadInterruptedRecovery.js
+++ toolkit/mozapps/update/tests/unit_aus_update/downloadInterruptedRecovery.js
@@ -8,6 +8,12 @@ function run_test() {
 
   debugDump("testing mar mar download interrupted recovery");
 
+  // This test assumes speculative connections enabled.
+  Services.prefs.setIntPref("network.http.speculative-parallel-limit", 6);
+  registerCleanupFunction(() => {
+    Services.prefs.clearUserPref("network.http.speculative-parallel-limit");
+  });
+
   Services.prefs.setBoolPref(PREF_APP_UPDATE_STAGING_ENABLED, false);
   start_httpserver();
   setUpdateURL(gURLData + gHTTPHandlerPath);
