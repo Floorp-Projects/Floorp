c69d04a58b8be775c4d0649db69a8b4fd6d1291e
Jean-Yves Avenard <jyavenard@mozilla.com>
2018-01-23 21:47:51 +0100

    Bug 1431221 - P9. Properly retrieve and set channel layout on windows. r=cpearce
    MozReview-Commit-ID: GKpOAgPP9qp

--- dom/media/platforms/wmf/WMFAudioMFTManager.cpp
+++ dom/media/platforms/wmf/WMFAudioMFTManager.cpp
@@ -84,6 +84,7 @@ AACAudioSpecificConfigToUserData(uint8_t aAACProfileLevelIndication,
 WMFAudioMFTManager::WMFAudioMFTManager(
   const AudioInfo& aConfig)
   : mAudioChannels(aConfig.mChannels)
+  , mChannelsMap(AudioConfig::ChannelLayout::UNKNOWN_MAP)
   , mAudioRate(aConfig.mRate)
 {
   MOZ_COUNT_CTOR(WMFAudioMFTManager);
@@ -209,6 +210,15 @@ WMFAudioMFTManager::UpdateOutputType()
 
   hr = type->GetUINT32(MF_MT_AUDIO_NUM_CHANNELS, &mAudioChannels);
   NS_ENSURE_TRUE(SUCCEEDED(hr), hr);
+
+  uint32_t channelsMap;
+  hr = type->GetUINT32(MF_MT_AUDIO_CHANNEL_MASK, &channelsMap);
+  if (SUCCEEDED(hr)) {
+    mChannelsMap = channelsMap;
+  } else {
+    LOG("Unable to retrieve channel layout. Ignoring");
+    mChannelsMap = AudioConfig::ChannelLayout::UNKNOWN_MAP;
+  }
 
   AudioConfig::ChannelLayout layout(mAudioChannels);
   if (!layout.IsValid()) {
@@ -339,7 +349,8 @@ WMFAudioMFTManager::Output(int64_t aStreamOffset,
                            numFrames,
                            Move(audioData),
                            mAudioChannels,
-                           mAudioRate);
+                           mAudioRate,
+                           mChannelsMap);
 
   #ifdef LOG_SAMPLE_DECODE
   LOG("Decoded audio sample! timestamp=%lld duration=%lld currentLength=%u",
