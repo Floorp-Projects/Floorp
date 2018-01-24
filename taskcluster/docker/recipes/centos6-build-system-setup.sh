7f7928ab0db12b7b92697644ab8e3f432c7ab576
Simon Fraser <sfraser@mozilla.com>
2018-01-24 12:50:04 +0000

    Bug 1432765 Linting compliance for docker recipes r=jlund
    MozReview-Commit-ID: KbTLTU87hyI

--- taskcluster/docker/recipes/centos6-build-system-setup.sh
+++ taskcluster/docker/recipes/centos6-build-system-setup.sh
@@ -3,14 +3,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-cd /setup
+cd /setup || exit
 
+# shellcheck source=taskcluster/docker/recipes/common.sh
 . /setup/common.sh
+# shellcheck source=taskcluster/docker/recipes/install-mercurial.sh
 . /setup/install-mercurial.sh
+# shellcheck source=taskcluster/docker/recipes/install-make.sh
 . /setup/install-make.sh
+# shellcheck source=taskcluster/docker/recipes/install-cmake.sh
 . /setup/install-cmake.sh
 
 if [ -f /setup/install-debug-symbols.sh ]; then
+    # shellcheck disable=SC1091
     . /setup/install-debug-symbols.sh
 fi
 
