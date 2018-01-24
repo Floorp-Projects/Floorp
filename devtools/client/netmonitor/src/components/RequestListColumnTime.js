9fe1d3046c9c9dda9348e1bb7d77a3485dd35137
Jan Odvarko <odvarko@gmail.com>
2018-01-24 19:13:52 +0100

    Bug 1431912 - Properly populate timing columns; r=ochameau a=lizzard
    MozReview-Commit-ID: 9IfVhp1Cdg7
    
    --HG--
    extra : source : 0b0e37f74f1591b37b8524a545551a3e92ba3ca1

---
+++ devtools/client/netmonitor/src/components/RequestListColumnTime.js
@@ -0,0 +1,98 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { Component } = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+const { getFormattedTime } = require("../utils/format-utils");
+const {
+  fetchNetworkUpdatePacket,
+  getResponseTime,
+  getStartTime,
+  getEndTime,
+} = require("../utils/request-utils");
+
+const { div } = dom;
+
+/**
+ * This component represents a column displaying selected
+ * timing value. There are following possible values this
+ * column can render:
+ * - Start Time
+ * - End Time
+ * - Response Time
+ * - Duration Time
+ * - Latency Time
+ */
+class RequestListColumnTime extends Component {
+  static get propTypes() {
+    return {
+      connector: PropTypes.object.isRequired,
+      firstRequestStartedMillis: PropTypes.number.isRequired,
+      item: PropTypes.object.isRequired,
+      type: PropTypes.oneOf([
+        "start",
+        "end",
+        "response",
+        "duration",
+        "latency",
+      ]).isRequired,
+    };
+  }
+
+  componentDidMount() {
+    let { item, connector } = this.props;
+    fetchNetworkUpdatePacket(connector.requestData, item, ["eventTimings"]);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let { item, connector } = nextProps;
+    fetchNetworkUpdatePacket(connector.requestData, item, ["eventTimings"]);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.getTime(this.props) !== this.getTime(nextProps);
+  }
+
+  getTime(props) {
+    let {
+      firstRequestStartedMillis,
+      item,
+      type,
+    } = props;
+
+    switch (type) {
+      case "start":
+        return getStartTime(item, firstRequestStartedMillis);
+      case "end":
+        return getEndTime(item, firstRequestStartedMillis);
+      case "response":
+        return getResponseTime(item, firstRequestStartedMillis);
+      case "duration":
+        return item.totalTime;
+      case "latency":
+        return item.eventTimings ? item.eventTimings.timings.wait : undefined;
+    }
+
+    return 0;
+  }
+
+  render() {
+    let { type } = this.props;
+    let time = getFormattedTime(this.getTime(this.props));
+
+    return (
+      div({
+        className: "requests-list-column requests-list-" + type + "-time",
+        title: time,
+      },
+        time
+      )
+    );
+  }
+}
+
+module.exports = RequestListColumnTime;
