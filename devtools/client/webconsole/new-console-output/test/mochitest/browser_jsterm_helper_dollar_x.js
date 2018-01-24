4e575360b725b25c02aaf7cf60d80f2d84ff6052
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-01-24 11:34:30 +0100

    Bug 1405352 - Split browser_webconsole_jsterm.js in multiple tests; r=Honza.
    The test was doing a lot of things and was hard to follow.
    Here we split it so we can have dedicated tests for each helpers.
    
    MozReview-Commit-ID: 6YGR3aq6jYn

---
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_helper_dollar_x.js
@@ -0,0 +1,31 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI = `data:text/html,
+<main>
+  <ul>
+    <li>First</li>
+    <li>Second</li>
+  </ul>
+  <aside>Sidebar</aside>
+</main>
+`;
+
+add_task(async function () {
+  const hud = await openNewTabAndConsole(TEST_URI);
+  const jsterm = hud.jsterm;
+
+  let onMessage = waitForMessage(hud, "Array [ li, li ]");
+  jsterm.execute("$x('.//li')");
+  let message = await onMessage;
+  ok(message, "`$x` worked");
+
+  onMessage = waitForMessage(hud, "<li>");
+  jsterm.execute("$x('.//li', document.body)[0]");
+  message = await onMessage;
+  ok(message, "`$x()` result can be used right away");
+});
---
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_instance_of.js
@@ -0,0 +1,27 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check instanceof correctness. See Bug 599940.
+const TEST_URI = "data:text/html,Test <code>instanceof</code> evaluation";
+
+add_task(async function () {
+  const hud = await openNewTabAndConsole(TEST_URI);
+  const {jsterm} = hud;
+
+  let onMessage = waitForMessage(hud, "true");
+  jsterm.execute("[] instanceof Array");
+  let message = await onMessage;
+  ok(message, "`instanceof Array` is correct");
+
+  onMessage = waitForMessage(hud, "true");
+  jsterm.execute("({}) instanceof Object");
+  message = await onMessage;
+  ok(message, "`instanceof Object` is correct");
+
+  onMessage = waitForMessage(hud, "false");
+  jsterm.execute("({}) instanceof Array");
+  message = await onMessage;
+  ok(message, "`instanceof Array` has expected result");
+});
