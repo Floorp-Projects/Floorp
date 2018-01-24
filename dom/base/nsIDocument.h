b3cac601f60d3b95f8511fa4c9a33789356ec347
Kris Maglione <maglione.k@gmail.com>
2018-01-24 14:56:48 -0800

    Bug 1432966: Sanitize HTML fragments created for chrome-privileged documents. r=bz f=gijs
    This is a short-term solution to our inability to apply CSP to
    chrome-privileged documents.
    
    Ideally, we should be preventing all inline script execution in
    chrome-privileged documents, since the reprecussions of XSS in chrome
    documents are much worse than in content documents. Unfortunately, that's not
    possible in the near term because a) we don't support CSP in system principal
    documents at all, and b) we rely heavily on inline JS in our static XUL.
    
    This stop-gap solution at least prevents some of the most common vectors of
    XSS attack, by automatically sanitizing any HTML fragment created for a
    chrome-privileged document.
    
    MozReview-Commit-ID: 5w17celRFr
    
    --HG--
    extra : rebase_source : 1c0a1448a06d5b65e548d9f5362d06cc6d865dbe
    extra : amend_source : 7184593019f238b86fd1e261941d8e8286fa4006

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -2873,6 +2873,8 @@ public:
     CreateAttributeNS(const nsAString& aNamespaceURI,
                       const nsAString& aQualifiedName,
                       mozilla::ErrorResult& rv);
+  void SetAllowUnsafeHTML(bool aAllow) { mAllowUnsafeHTML = aAllow; }
+  bool AllowUnsafeHTML() const;
   void GetInputEncoding(nsAString& aInputEncoding) const;
   already_AddRefed<mozilla::dom::Location> GetLocation() const;
   void GetReferrer(nsAString& aReferrer) const;
@@ -3555,6 +3557,10 @@ protected:
 
   // True if this document is for an SVG-in-OpenType font.
   bool mIsSVGGlyphsDocument : 1;
+
+  // True if unsafe HTML fragments should be allowed in chrome-privileged
+  // documents.
+  bool mAllowUnsafeHTML : 1;
 
   // Whether <style scoped> support is enabled in this document.
   enum { eScopedStyle_Unknown, eScopedStyle_Disabled, eScopedStyle_Enabled };
