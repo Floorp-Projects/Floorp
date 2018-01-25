cb3afe261485dcecdcab7d8173038d85773150ba
Aaron Klotz <aklotz@mozilla.com>
2018-01-25 13:45:21 -0700

    Bug 1433046 - Use WeakReferenceSupport::StabilizeRefCount instead of regular kung-fu death grips when aggregating. r=Jamie, a=RyanVM
    This fix is completely speculative, but I have strong reason to believe that
    we are having lifetime issues, and that refcount stabilization might be coming
    into play.
    
    The situation is this:
    
    Suppose we're aggregating an object, so we pass |this| as the outer IUnknown.
    The inner object might perform AddRef() and Release() on |this| during its
    initialization.
    
    But if we're in the process of creating the outer object, that refcount might
    not yet have been incremented by 1, so the inner object's invocation of the
    outer object's Release() could trigger a deletion.
    
    The way around this is to temporarily bump the refcount when aggregating another
    object. The key, though, is to not do this via AddRef() and Release(), but by
    direct maniuplation of the refcount variable, so that we don't trigger any of
    the self-deletion stuff.
    
    MozReview-Commit-ID: 3WA2AJvb6jY

--- ipc/mscom/WeakRef.h
+++ ipc/mscom/WeakRef.h
@@ -101,6 +101,23 @@ protected:
   virtual HRESULT WeakRefQueryInterface(REFIID aIid,
                                         IUnknown** aOutInterface) = 0;
 
+  class MOZ_RAII StabilizeRefCount final
+  {
+  public:
+    explicit StabilizeRefCount(WeakReferenceSupport& aObject);
+    ~StabilizeRefCount();
+
+    StabilizeRefCount(const StabilizeRefCount&) = delete;
+    StabilizeRefCount(StabilizeRefCount&&) = delete;
+    StabilizeRefCount& operator=(const StabilizeRefCount&) = delete;
+    StabilizeRefCount& operator=(StabilizeRefCount&&) = delete;
+
+  private:
+    WeakReferenceSupport& mObject;
+  };
+
+  friend class StabilizeRefCount;
+
 private:
   RefPtr<detail::SharedRef> mSharedRef;
   ULONG                     mRefCnt;
