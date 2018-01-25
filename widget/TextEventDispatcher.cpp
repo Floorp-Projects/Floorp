38c9ff1b63dc264a1002786e359cf026f80fce59
Masayuki Nakano <masayuki@d-toybox.com>
2018-01-26 00:16:14 +0900

    Bug 1440189 - part 1: Stop dispatching keypress event to the default event group in web content (only Nightly and early Beta) r=smaug
    UI Events declares that keypress event should be fired only when the keydown
    sequence produces some characters.  For conforming to UI Events and
    compatibility with the other browsers, we should stop dispatching keypress
    events for non-printable keys.
    
    For getting regression reports, we should enable this new behavior only
    on Nightly and early Beta.
    
    MozReview-Commit-ID: 5IIL9huejXH
    
    --HG--
    extra : rebase_source : 0abdbe84a50d6fd1b4d52521b92e7513483b197c

--- widget/TextEventDispatcher.cpp
+++ widget/TextEventDispatcher.cpp
@@ -45,7 +45,7 @@ TextEventDispatcher::TextEventDispatcher(nsIWidget* aWidget)
       &sDispatchKeyPressEventsOnlySystemGroupInContent,
       "dom.keyboardevent.keypress."
         "dispatch_non_printable_keys_only_system_group_in_content",
-      false);
+      true);
     sInitialized = true;
   }
 
