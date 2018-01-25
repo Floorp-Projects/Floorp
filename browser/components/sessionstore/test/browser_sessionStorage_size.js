2af1d9d442c0e05189710f5dd4931e71b4c6bdd9
Ryan VanderMeulen <ryanvm@gmail.com>
2018-01-25 09:28:22 -0500

    Bug 1432486 - Remove the unneeded FX_SESSION_RESTORE_DOM_STORAGE_SIZE_ESTIMATE_CHARS probe. r=mikedeboer

--- browser/components/sessionstore/test/browser_sessionStorage_size.js
+++ browser/components/sessionstore/test/browser_sessionStorage_size.js
@@ -10,47 +10,6 @@ const URL = "http://mochi.test:8888/browser/" +
 
 const OUTER_VALUE = "outer-value-" + RAND;
 
-function getEstimateChars() {
-  let snap;
-  if (gMultiProcessBrowser) {
-    snap = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                 false /* subsession */,
-                                                 false /* clear */).content.FX_SESSION_RESTORE_DOM_STORAGE_SIZE_ESTIMATE_CHARS;
-  } else {
-    snap = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                 false,
-                                                 false).parent.FX_SESSION_RESTORE_DOM_STORAGE_SIZE_ESTIMATE_CHARS;
-  }
-  if (!snap) {
-    return 0;
-  }
-  return snap.counts[4];
-}
-
-// Test that we record the size of messages.
-add_task(async function test_telemetry() {
-  Services.telemetry.canRecordExtended = true;
-
-  let prev = getEstimateChars();
-
-  let tab = BrowserTestUtils.addTab(gBrowser, URL);
-  let browser = tab.linkedBrowser;
-  await promiseBrowserLoaded(browser);
-
-  // Flush to make sure we submitted telemetry data.
-  await TabStateFlusher.flush(browser);
-
-  // There is no good way to make sure that the parent received the histogram entries from the child processes.
-  // Let's stick to the ugly, spinning the event loop until we have a good approach (Bug 1357509).
-  await BrowserTestUtils.waitForCondition(() => {
-    return getEstimateChars() > prev;
-  });
-
-  Assert.ok(true);
-  await promiseRemoveTab(tab);
-  Services.telemetry.canRecordExtended = false;
-});
-
 // Lower the size limit for DOM Storage content. Check that DOM Storage
 // is not updated, but that other things remain updated.
 add_task(async function test_large_content() {
