7953526ea236a2faa34aa6dad75b302b66e77f77
Mark Banner <standard8@mozilla.com>
2018-01-26 21:32:30 +0000

    Bug 1457835 - Enable ESLint for testing/mochitest (automatic changes). r=jmaher
    MozReview-Commit-ID: 4Et3MAxcEQl
    
    --HG--
    extra : rebase_source : b6456aa9dd18df77cfb7fbb5012de33bd6fed681

--- testing/mochitest/start_desktop.js
+++ testing/mochitest/start_desktop.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const flavor  = __webDriverArguments[0]["flavor"];
-const url = __webDriverArguments[0]["testUrl"];
+const flavor  = __webDriverArguments[0].flavor;
+const url = __webDriverArguments[0].testUrl;
 
 let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
           .getService(Ci.nsIWindowMediator);
@@ -11,5 +11,5 @@ let win = wm.getMostRecentWindow("navigator:browser");
 
 // mochikit's bootstrap.js has set up a listener for this event. It's
 // used so bootstrap.js knows which flavor and url to load.
-let ev = new CustomEvent('mochitest-load', {'detail': [flavor, url]});
+let ev = new CustomEvent("mochitest-load", {"detail": [flavor, url]});
 win.dispatchEvent(ev);
