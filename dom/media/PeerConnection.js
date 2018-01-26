c4a0cebf582834ae5117d8b20f471275242e69ae
Nico Grunbaum <none@none>
2018-01-26 13:40:30 -0800

    Bug 1433576 - bring rtp sources js timestamp and timebase up to spec r=jib
    MozReview-Commit-ID: 2joFQ41NaMp
    
    --HG--
    extra : rebase_source : e424cbbeb00d0b152609a4390d43d5ca6e4aceb7

--- dom/media/PeerConnection.js
+++ dom/media/PeerConnection.js
@@ -1274,7 +1274,7 @@ class RTCPeerConnection {
     // the local offset, likewise store it for reuse.
     if (cache.tsNowInRtpSourceTime !== undefined) {
       cache.tsNowInRtpSourceTime = this._impl.getNowInRtpSourceReferenceTime();
-      cache.jsTimestamp = new Date().getTime();
+      cache.jsTimestamp = this._win.performance.now() + this._win.performance.timeOrigin;
       cache.timestampOffset = cache.jsTimestamp - cache.tsNowInRtpSourceTime;
     }
     let id = receiver.track.id;
