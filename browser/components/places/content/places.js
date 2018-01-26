d78c929816786e681a2ffe4c48aa485d4a010d6f
Mark Banner <standard8@mozilla.com>
2018-01-26 14:46:30 +0000

    Bug 801391 - Set clickSelectsAll depending on the preference for the Bookmarks Library search box. r=mak
    MozReview-Commit-ID: 3IhkODtMpr7

--- browser/components/places/content/places.js
+++ browser/components/places/content/places.js
@@ -874,6 +874,9 @@ var PlacesSearchBox = {
    * Set up the gray text in the search bar as the Places View loads.
    */
   init: function PSB_init() {
+    if (Services.prefs.getBoolPref("browser.urlbar.clickSelectsAll", false)) {
+      this.searchFilter.setAttribute("clickSelectsAll", true);
+    }
     this.updateCollectionTitle();
   },
 
