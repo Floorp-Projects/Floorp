4e0dcdc8daee666db970d6b3bbccab608a788d51
Robert Longson <longsonr@gmail.com>
2018-01-26 10:21:34 -0800

    Bug 1419764 - Improve performance where a transform is set by direct matrix manipulation. r=dholbert
    MozReview-Commit-ID: zzDTSXtRiU

--- dom/svg/nsSVGElement.cpp
+++ dom/svg/nsSVGElement.cpp
@@ -621,7 +621,7 @@ nsSVGElement::ParseAttribute(int32_t aNamespaceID,
         // nsSVGAnimatedTransformList is/has been allocated:
         nsSVGAnimatedTransformList *transformList =
           GetAnimatedTransformList(DO_ALLOCATE);
-        rv = transformList->SetBaseValueString(aValue);
+        rv = transformList->SetBaseValueString(aValue, this);
         if (NS_FAILED(rv)) {
           transformList->ClearBaseValue();
         } else {
