30e65c41c25443147ae2a4c66257b6e610886df8
Alexander J. Vincent <ajvincent@gmail.com>
2018-01-26 17:13:34 -0800

    Bug 1426886 - Timer.jsm should validate callback argument is a function. r=mconley

--- toolkit/modules/tests/xpcshell/test_timer.js
+++ toolkit/modules/tests/xpcshell/test_timer.js
@@ -99,3 +99,23 @@ add_task(async function test_setIntervalWithTarget() {
     }, 100, target, 15, "hola");
   });
 });
+
+add_task(async function test_setTimeoutNonFunction() {
+  Assert.throws(() => { imported.setTimeout({}, 0); },
+                "callback is not a function in setTimeout");
+});
+
+add_task(async function test_setIntervalNonFunction() {
+  Assert.throws(() => { imported.setInterval({}, 0); },
+                "callback is not a function in setInterval");
+});
+
+add_task(async function test_setTimeoutWithTargetNonFunction() {
+  Assert.throws(() => { imported.setTimeoutWithTarget({}, 0); },
+                "callback is not a function in setTimeout");
+});
+
+add_task(async function test_setIntervalWithTargetNonFunction() {
+  Assert.throws(() => { imported.setIntervalWithTarget({}, 0); },
+                "callback is not a function in setInterval");
+});
