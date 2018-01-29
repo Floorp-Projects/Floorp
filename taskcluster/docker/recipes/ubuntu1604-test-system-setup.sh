230d920ca9d1b2da524aa55207f47340b3d92756
Simon Fraser <sfraser@mozilla.com>
2018-01-29 15:27:04 +0000

    Bug 1432627 Add shellcheck linter to CI r=ahal
    MozReview-Commit-ID: 2IYUsXunfab

--- taskcluster/docker/recipes/ubuntu1604-test-system-setup.sh
+++ taskcluster/docker/recipes/ubuntu1604-test-system-setup.sh
@@ -91,16 +91,13 @@ apt-get install -y -f "${apt_packages[@]}"
 
 dpkg-reconfigure locales
 
-# shellcheck source=taskcluster/docker/recipes/common.sh
 . /setup/common.sh
-# shellcheck source=taskcluster/docker/recipes/install-mercurial.sh
 . /setup/install-mercurial.sh
 
 pip install --upgrade pip
 
 pip install virtualenv
 
-# shellcheck source=taskcluster/docker/recipes/install-node.sh
 . /setup/install-node.sh
 
 # Install custom-built Debian packages.  These come from a set of repositories
