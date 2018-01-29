7001b4ff4c125e9597c42c5f47f2baa8ea988ff6
Andrea Marchesini <amarchesini@mozilla.com>
2018-01-29 20:51:32 +0100

    Bug 1433723 - URL in workers should use a proxy if the protocol is not http nor https, r=valentin

---
+++ dom/url/tests/protocol_worker.js
@@ -0,0 +1,25 @@
+function ok(a, msg) {
+  postMessage({ type: "status", status: !!a, msg });
+}
+
+function is(a, b, msg) {
+  ok(a === b, msg);
+}
+
+function finish() {
+  postMessage({ type: "finish" });
+}
+
+let url = new URL("http://example.com");
+is(url.protocol, "http:", "http: expected");
+
+url.protocol = "https:";
+is(url.protocol, "https:", "http: -> https:");
+
+url.protocol = "ftp:";
+is(url.protocol, "ftp:", "https: -> ftp:");
+
+url.protocol = "https:";
+is(url.protocol, "https:", "ftp: -> https:");
+
+finish();
