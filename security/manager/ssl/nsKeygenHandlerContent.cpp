a9c83a1bf2b756ef663d70a8394208772f9610a1
Boris Zbarsky <bzbarsky@mit.edu>
2018-01-29 23:29:11 -0500

    Bug 1432944 part 12.  Change nsIFormProcessor to pass Element, not nsIDOMHTMLElement, for elements.  r=mccr8
    MozReview-Commit-ID: K6w7FR254UC

--- security/manager/ssl/nsKeygenHandlerContent.cpp
+++ security/manager/ssl/nsKeygenHandlerContent.cpp
@@ -11,6 +11,7 @@
 #include "nsString.h"
 
 #include "mozilla/dom/ContentChild.h"
+#include "mozilla/dom/Element.h"
 #include "mozilla/Unused.h"
 
 #include "keythi.h"
@@ -19,6 +20,7 @@
 #include "nsKeygenHandler.h"
 
 using mozilla::dom::ContentChild;
+using mozilla::dom::Element;
 using mozilla::Unused;
 
 NS_IMPL_ISUPPORTS(nsKeygenFormProcessorContent, nsIFormProcessor)
@@ -32,7 +34,7 @@ nsKeygenFormProcessorContent::~nsKeygenFormProcessorContent()
 }
 
 nsresult
-nsKeygenFormProcessorContent::ProcessValue(nsIDOMHTMLElement* aElement,
+nsKeygenFormProcessorContent::ProcessValue(Element* aElement,
                                            const nsAString& aName,
                                            nsAString& aValue)
 {
