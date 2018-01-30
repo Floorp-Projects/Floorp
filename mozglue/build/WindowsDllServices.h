0213e8a4b6d8530c9742b375616678db28bd16b9
Aaron Klotz <aklotz@mozilla.com>
2018-01-30 14:23:10 -0700

    Bug 1430857: Part 1 - Refactor DllServices to make it possible to obtain them from anywhere in Gecko; r=jimm
    MozReview-Commit-ID: GfWata0eCc5
    
    --HG--
    extra : rebase_source : f280fec92c867d0adbe7b82c0e829eeb10fce5a9

--- mozglue/build/WindowsDllServices.h
+++ mozglue/build/WindowsDllServices.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_WindowsDllServices_h
-#define mozilla_WindowsDllServices_h
+#ifndef mozilla_glue_WindowsDllServices_h
+#define mozilla_glue_WindowsDllServices_h
 
 #include "mozilla/WindowsDllBlocklist.h"
 
@@ -22,6 +22,7 @@
 #include <winternl.h>
 
 namespace mozilla {
+namespace glue {
 namespace detail {
 
 class DllServicesBase
@@ -86,6 +87,7 @@ protected:
 
 #endif // defined(MOZILLA_INTERNAL_API)
 
+} // namespace glue
 } // namespace mozilla
 
-#endif // mozilla_WindowsDllServices_h
+#endif // mozilla_glue_WindowsDllServices_h
