b39f69d4538ea9b7f9bbdb2e4e08b5da0f9e885b
Boris Zbarsky <bzbarsky@mit.edu>
2018-01-29 23:10:52 -0500

    Bug 1432186 part 18.  Remove no-longer-needed nsIDOMNode-forwarding defines.  r=mccr8
    MozReview-Commit-ID: FbP5fVQ3c6m

--- dom/base/DocumentFragment.h
+++ dom/base/DocumentFragment.h
@@ -45,9 +45,6 @@ public:
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(DocumentFragment, FragmentOrElement)
 
-  // interface nsIDOMNode
-  NS_FORWARD_NSIDOMNODE_TO_NSINODE
-
   // interface nsIDOMDocumentFragment
   NS_DECL_NSIDOMDOCUMENTFRAGMENT
 
