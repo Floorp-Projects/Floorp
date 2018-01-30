5c73f1fe652105ae11aba406c4ef8345d8eda621
Aaron Klotz <aklotz@mozilla.com>
2018-01-30 14:23:10 -0700

    Bug 1430857: Part 1 - Refactor DllServices to make it possible to obtain them from anywhere in Gecko; r=jimm
    MozReview-Commit-ID: GfWata0eCc5

---
+++ toolkit/xre/WinDllServices.cpp
@@ -0,0 +1,54 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/WinDllServices.h"
+
+#include "mozilla/ClearOnShutdown.h"
+#include "mozilla/Services.h"
+#include "nsIObserverService.h"
+#include "nsString.h"
+
+namespace mozilla {
+
+const char* DllServices::kTopicDllLoadedMainThread = "dll-loaded-main-thread";
+const char* DllServices::kTopicDllLoadedNonMainThread = "dll-loaded-non-main-thread";
+
+static StaticRefPtr<DllServices> sInstance;
+
+DllServices*
+DllServices::Get()
+{
+  if (sInstance) {
+    return sInstance;
+  }
+
+  sInstance = new DllServices();
+  ClearOnShutdown(&sInstance);
+  return sInstance;
+}
+
+DllServices::DllServices()
+{
+  Enable();
+}
+
+void
+DllServices::NotifyDllLoad(const bool aIsMainThread, const nsString& aDllName)
+{
+  const char* topic;
+
+  if (aIsMainThread) {
+    topic = kTopicDllLoadedMainThread;
+  } else {
+    topic = kTopicDllLoadedNonMainThread;
+  }
+
+  nsCOMPtr<nsIObserverService> obsServ(mozilla::services::GetObserverService());
+  obsServ->NotifyObservers(nullptr, topic, aDllName.get());
+}
+
+} // namespace mozilla
+
