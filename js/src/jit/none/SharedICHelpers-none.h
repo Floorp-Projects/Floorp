2ff93d3b10ab85734f6be4d2a44e54addfef4e13
Jan de Mooij <jdemooij@mozilla.com>
2018-01-31 16:54:04 +0100

    Bug 1434279 - Remove unused EmitStowICValues. r=evilpie

--- js/src/jit/none/SharedICHelpers-none.h
+++ js/src/jit/none/SharedICHelpers-none.h
@@ -19,7 +19,6 @@ inline void EmitEnterTypeMonitorIC(MacroAssembler&, size_t v = 0) { MOZ_CRASH();
 inline void EmitReturnFromIC(MacroAssembler&) { MOZ_CRASH(); }
 inline void EmitChangeICReturnAddress(MacroAssembler&, Register) { MOZ_CRASH(); }
 inline void EmitBaselineLeaveStubFrame(MacroAssembler&, bool v = false) { MOZ_CRASH(); }
-inline void EmitStowICValues(MacroAssembler&, int) { MOZ_CRASH(); }
 inline void EmitStubGuardFailure(MacroAssembler&) { MOZ_CRASH(); }
 
 template <typename T> inline void EmitPreBarrier(MacroAssembler&, T, MIRType) { MOZ_CRASH(); }
