f021caf8ce824032f353b2f30ee9754fa224955b
Cosmin Sabou <csabou@mozilla.com>
2018-02-01 03:05:08 +0200

    Backed out 2 changesets (bug 1423840) for mass Talos failures due to forbidden connections. CLOSED TREE
    Backed out changeset e8b798a5205a (bug 1423840)
    Backed out changeset e500592d3551 (bug 1423840)

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -9,9 +9,8 @@ extern crate mp4parse_capi;
 extern crate nsstring;
 extern crate nserror;
 extern crate xpcom;
-extern crate netwerk_helper;
-extern crate prefs_parser;
 extern crate rust_url_capi;
+extern crate netwerk_helper;
 #[cfg(feature = "quantum_render")]
 extern crate webrender_bindings;
 #[cfg(feature = "cubeb_pulse_rust")]
