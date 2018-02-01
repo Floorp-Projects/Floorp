9e956efed75aa5dae579743655382f384d77b600
Tom Prince <mozilla@hocat.ca>
2018-01-31 17:26:41 -0700

    Bug 1429603: Remove unused `post_upload_extra`, `upload_environment` and `base_post_upload_cmd` from L10N mozharness scripts; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D537
    
    --HG--
    extra : rebase_source : 8377203bdb271a1813f4393e5290a3d5c1611ed1

--- testing/mozharness/configs/single_locale/staging.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "upload_environment": "stage",
-    'post_upload_extra': ['--bucket-prefix', 'net-mozaws-stage-delivery',
-                          '--url-prefix', 'http://ftp.stage.mozaws.net/',
-                          ],
-}
