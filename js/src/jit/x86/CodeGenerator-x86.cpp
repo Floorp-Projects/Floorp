0238ed5d8ab6d6401457f319375395df65a7532c
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2018-02-02 13:39:24 +0000

    Bug 1433111 - Zero the payload if the Value tag does not match the expected tag. r=jandem

--- js/src/jit/x86/CodeGenerator-x86.cpp
+++ js/src/jit/x86/CodeGenerator-x86.cpp
@@ -121,20 +121,28 @@ CodeGeneratorX86::visitUnbox(LUnbox* unbox)
 {
     // Note that for unbox, the type and payload indexes are switched on the
     // inputs.
+    Operand type = ToOperand(unbox->type());
+    Operand payload = ToOperand(unbox->payload());
+    Register output = ToRegister(unbox->output());
     MUnbox* mir = unbox->mir();
 
     JSValueTag tag = MIRTypeToTag(mir->type());
     if (mir->fallible()) {
-        masm.cmp32(ToOperand(unbox->type()), Imm32(tag));
+        masm.cmp32(type, Imm32(tag));
         bailoutIf(Assembler::NotEqual, unbox->snapshot());
     } else {
 #ifdef DEBUG
         Label ok;
-        masm.branch32(Assembler::Equal, ToOperand(unbox->type()), Imm32(tag), &ok);
+        masm.branch32(Assembler::Equal, type, Imm32(tag), &ok);
         masm.assumeUnreachable("Infallible unbox type mismatch");
         masm.bind(&ok);
 #endif
     }
+
+    // Note: If spectreValueMasking is disabled, then this instruction will
+    // default to a no-op as long as the lowering allocate the same register for
+    // the output and the payload.
+    masm.unboxNonDouble(type, payload, output, ValueTypeFromMIRType(mir->type()));
 }
 
 void
