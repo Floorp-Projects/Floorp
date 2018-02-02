beaed0f5656770418616c228c253c3ce81935c42
Emilio Cobos Álvarez <emilio@crisal.io>
2018-02-02 12:15:59 +0100

    Bug 1435214: Optimize @keyframes rule insertions. r=xidorn,hiro
    Also add some missing test for the tag name invalidations (bug 1407522) and an
    empty stylesheet just for sanity.
    
    MozReview-Commit-ID: AHwhZynLBv

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -395,6 +395,10 @@ void Gecko_NoteDirtyElement(RawGeckoElementBorrowed element);
 void Gecko_NoteDirtySubtreeForInvalidation(RawGeckoElementBorrowed element);
 void Gecko_NoteAnimationOnlyDirtyElement(RawGeckoElementBorrowed element);
 
+bool Gecko_AnimationNameMayBeReferencedFromStyle(
+  RawGeckoPresContextBorrowed pres_context,
+  nsAtom* name);
+
 // Incremental restyle.
 mozilla::CSSPseudoElementType Gecko_GetImplementedPseudo(RawGeckoElementBorrowed element);
 // We'd like to return `nsChangeHint` here, but bindgen bitfield enums don't
