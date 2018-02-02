edcfadf40e3c476d4f0f02b7852117d024fbca16
Nathan Froyd <froydnj@mozilla.com>
2018-02-02 13:55:05 -0500

    Bug 1434856 - move runnable prioritization checks outside of event queue locks; r=erahm
    Otherwise, we might enter JS, decide to GC, and deadlock because we were
    trying to dispatch tasks to the main thread's event queue while holding
    the lock for the event queue.

--- xpcom/threads/EventQueue.h
+++ xpcom/threads/EventQueue.h
@@ -18,6 +18,8 @@ namespace mozilla {
 class EventQueue final : public AbstractEventQueue
 {
 public:
+  static const bool SupportsPrioritization = false;
+
   EventQueue() {}
   explicit EventQueue(EventPriority aPriority);
 
