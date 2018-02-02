01295b9824d748fa6204c1faebbdb19caa4f5bba
Andreas Tolfsen <ato@sny.no>
2018-02-02 06:17:00 -0500

    Bug 1433422 - Add basic form control tests for Element Send Keys. r=automatedtester, a=test-only
    We are apparently missing even the most basic tests for Element
    Send Keys.  This is a moderate contribution.
    
    MozReview-Commit-ID: 9uWtTJ7MFZx

--- testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
+++ testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tests.support.asserts import assert_error, assert_same_element, assert_success
 from tests.support.inline import inline
 
@@ -10,6 +12,37 @@ def element_send_keys(session, element, text):
             element_id=element.id),
         {"text": text})
 
+
+def add_event_listeners(element):
+    element.session.execute_script("""
+        let [target] = arguments;
+        window.events = [];
+        for (let expected of ["focus", "blur", "change", "keypress", "keydown", "keyup", "input"]) {
+          target.addEventListener(expected, ({type}) => window.events.push(type));
+        }
+        """, args=(element,))
+
+
+def get_events(session):
+    return session.execute_script("return window.events")
+
+
+def test_input(session):
+    session.url = inline("<input>")
+    element = session.find.css("input", all=False)
+    assert element.property("value") == ""
+
+    element_send_keys(session, element, "foo")
+    assert element.property("value") == "foo"
+
+
+def test_textarea(session):
+    session.url = inline("<textarea>")
+    element = session.find.css("textarea", all=False)
+    assert element.property("value") == ""
+
+    element_send_keys(session, element, "foo")
+    assert element.property("value") == "foo"
 
 
 def test_input_append(session):
@@ -34,3 +67,28 @@ def test_textarea_append(session):
 
     element_send_keys(session, element, "c")
     assert element.property("value") == "abc"
+
+
+@pytest.mark.parametrize("tag", ["input", "textarea"])
+def test_events(session, tag):
+    session.url = inline("<%s>" % tag)
+    element = session.find.css(tag, all=False)
+    add_event_listeners(element)
+
+    element_send_keys(session, element, "foo")
+    assert element.property("value") == "foo"
+    assert get_events(session) == ["focus",
+                                   "keydown",
+                                   "keypress",
+                                   "input",
+                                   "keyup",
+                                   "keydown",
+                                   "keypress",
+                                   "input",
+                                   "keyup",
+                                   "keydown",
+                                   "keypress",
+                                   "input",
+                                   "keyup",
+                                   "change",
+                                   "blur"]
