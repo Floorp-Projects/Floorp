5268536db5b4b7b5791771d78ff490980395da47
Andreea Pavel <apavel@mozilla.com>
2018-02-03 01:12:45 +0200

    Backed out 6 changesets (bug 1423229) for failing browser chrome at browser/base/content/test/siteIdentity/browser_bug1045809.js and firefox functional at testing/firefox-ui/tests/functional/security/test_mixed_script_content_blocking.py TestMixedScriptContentBlocking.test_mixed_content_page and mochitest devtools at devtools/client/webconsole/test/browser_webconsole_block_mixedcontent_securityerrors.js on a CLOSED TREE
    Backed out changeset 2ee49f44e617 (bug 1423229)
    Backed out changeset 30d1575f5b16 (bug 1423229)
    Backed out changeset 39d76cb8c73e (bug 1423229)
    Backed out changeset 8ff1f049ac96 (bug 1423229)
    Backed out changeset c86887b7f327 (bug 1423229)
    Backed out changeset 5417194f3778 (bug 1423229)

--- mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
+++ mobile/android/geckoview_example/src/main/java/org/mozilla/geckoview_example/GeckoViewActivity.java
@@ -22,7 +22,6 @@ import org.mozilla.gecko.GeckoSession;
 import org.mozilla.gecko.GeckoSessionSettings;
 import org.mozilla.gecko.GeckoThread;
 import org.mozilla.gecko.GeckoView;
-import org.mozilla.gecko.GeckoSession.TrackingProtectionDelegate;
 import org.mozilla.gecko.util.GeckoBundle;
 
 public class GeckoViewActivity extends Activity {
@@ -73,9 +72,7 @@ public class GeckoViewActivity extends Activity {
         mGeckoView.setSession(mGeckoSession);
 
         mGeckoSession.setContentListener(new MyGeckoViewContent());
-        final MyTrackingProtection tp = new MyTrackingProtection();
-        mGeckoSession.setTrackingProtectionDelegate(tp);
-        mGeckoSession.setProgressListener(new MyGeckoViewProgress(tp));
+        mGeckoSession.setProgressListener(new MyGeckoViewProgress());
         mGeckoSession.setNavigationListener(new Navigation());
 
         final BasicGeckoViewPrompt prompt = new BasicGeckoViewPrompt(this);
@@ -88,13 +85,6 @@ public class GeckoViewActivity extends Activity {
 
         mGeckoView.getSettings().setBoolean(GeckoSessionSettings.USE_MULTIPROCESS,
                                             useMultiprocess);
-
-        mGeckoSession.enableTrackingProtection(
-              TrackingProtectionDelegate.CATEGORY_AD |
-              TrackingProtectionDelegate.CATEGORY_ANALYTIC |
-              TrackingProtectionDelegate.CATEGORY_SOCIAL
-        );
-
         loadSettings(getIntent());
         loadFromIntent(getIntent());
     }
@@ -198,18 +188,11 @@ public class GeckoViewActivity extends Activity {
     }
 
     private class MyGeckoViewProgress implements GeckoSession.ProgressListener {
-        private MyTrackingProtection mTp;
-
-        private MyGeckoViewProgress(final MyTrackingProtection tp) {
-            mTp = tp;
-        }
-
         @Override
         public void onPageStart(GeckoSession session, String url) {
             Log.i(LOGTAG, "Starting to load page at " + url);
             Log.i(LOGTAG, "zerdatime " + SystemClock.elapsedRealtime() +
                   " - page load start");
-            mTp.clearCounters();
         }
 
         @Override
@@ -217,7 +200,6 @@ public class GeckoViewActivity extends Activity {
             Log.i(LOGTAG, "Stopping page load " + (success ? "successfully" : "unsuccessfully"));
             Log.i(LOGTAG, "zerdatime " + SystemClock.elapsedRealtime() +
                   " - page load stop");
-            mTp.logCounters();
         }
 
         @Override
@@ -353,33 +335,6 @@ public class GeckoViewActivity extends Activity {
             }
             session.loadUri(uri);
             return true;
-        }
-    }
-
-    private class MyTrackingProtection implements GeckoSession.TrackingProtectionDelegate {
-        private int mBlockedAds = 0;
-        private int mBlockedAnalytics = 0;
-        private int mBlockedSocial = 0;
-
-        private void clearCounters() {
-            mBlockedAds = 0;
-            mBlockedAnalytics = 0;
-            mBlockedSocial = 0;
-        }
-
-        private void logCounters() {
-            Log.d(LOGTAG, "Trackers blocked: " + mBlockedAds + " ads, " +
-                  mBlockedAnalytics + " analytics, " +
-                  mBlockedSocial + " social");
-        }
-
-        @Override
-        public void onTrackerBlocked(final GeckoSession session, final String uri,
-                                     int categories) {
-            Log.d(LOGTAG, "onTrackerBlocked " + categories + " (" + uri + ")");
-            mBlockedAds += categories & TrackingProtectionDelegate.CATEGORY_AD;
-            mBlockedAnalytics += categories & TrackingProtectionDelegate.CATEGORY_ANALYTIC;
-            mBlockedSocial += categories & TrackingProtectionDelegate.CATEGORY_SOCIAL;
         }
     }
 }
