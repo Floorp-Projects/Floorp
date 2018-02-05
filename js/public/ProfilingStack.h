821f55ea6d484bc55ad61a6b4167098787d896b5
Markus Stange <mstange@themasta.com>
2018-02-05 16:41:29 -0500

    Bug 1385998 - Use ReleaseAcquire memory ordering when modifying the PseudoStack. r=froydnj
    Before this patch, the writes to stackPointer were done using sequentially-
    consistent stores. On x86 these compile to 'xchg' instructions, which showed up
    as the most expensive part of the PseudoStack overhead.
    
    MozReview-Commit-ID: IP9w9ievEXZ

--- js/public/ProfilingStack.h
+++ js/public/ProfilingStack.h
@@ -20,6 +20,80 @@ class JSTracer;
 
 class PseudoStack;
 
+// This file defines the classes PseudoStack and ProfileEntry.
+// The PseudoStack manages an array of ProfileEntries.
+// Usage:
+//
+//  PseudoStack* pseudoStack = ...;
+//
+//  // For CPP stack frames:
+//  pseudoStack->pushCppFrame(...);
+//  // Execute some code. When finished, pop the entry:
+//  pseudoStack->pop();
+//
+//  // For JS stack frames:
+//  pseudoStack->pushJSFrame(...);
+//  // Execute some code. When finished, pop the entry:
+//  pseudoStack->pop();
+//
+//
+// Concurrency considerations
+//
+// A thread's pseudo stack (and the entries inside it) is only modified by
+// that thread. However, the pseudo stack can be *read* by a different thread,
+// the sampler thread: Whenever the profiler wants to sample a given thread A,
+// the following happens:
+//  (1) Thread A is suspended.
+//  (2) The sampler thread (thread S) reads the PseudoStack of thread A,
+//      including all ProfileEntries that are currently in that stack
+//      (pseudoStack->entries[0..pseudoStack->stackSize()]).
+//  (3) Thread A is resumed.
+//
+// Thread suspension is achieved using platform-specific APIs; refer to each
+// platform's Sampler::SuspendAndSampleAndResumeThread implementation in
+// platform-*.cpp for details.
+//
+// When the thread is suspended, the values in pseudoStack->stackPointer and in
+// the entry range pseudoStack->entries[0..pseudoStack->stackPointer] need to
+// be in a consistent state, so that thread A does not read partially-
+// constructed profile entries. More specifically, we have two requirements:
+//  (1) When adding a new entry at the top of the stack, its ProfileEntry data
+//      needs to be put in place *before* the stackPointer is incremented, and
+//      the compiler + CPU need to know that this order matters.
+//  (2) When popping an entry from the stack and then preparing the
+//      ProfileEntry data for the next frame that is about to be pushed, the
+//      decrement of the stackPointer in pop() needs to happen *before* the
+//      ProfileEntry for the new frame is being popuplated, and the compiler +
+//      CPU need to know that this order matters.
+//
+// We can express the relevance of these orderings in multiple ways.
+// Option A is to make stackPointer an atomic with SequentiallyConsistent
+// memory ordering. This would ensure that no writes in thread A would be
+// reordered across any writes to stackPointer, which satisfies requirements
+// (1) and (2) at the same time. Option A is the simplest.
+// Option B is to use ReleaseAcquire memory ordering both for writes to
+// stackPointer *and* for writes to ProfileEntry fields. Release-stores ensure
+// that all writes that happened *before this write in program order* are not
+// reordered to happen after this write. ReleaseAcquire ordering places no
+// requirements on the ordering of writes that happen *after* this write in
+// program order.
+// Using release-stores for writes to stackPointer expresses requirement (1),
+// and using release-stores for writes to the ProfileEntry fields expresses
+// requirement (2).
+//
+// Option B is more complicated than option A, but has much better performance
+// on x86/64: In a microbenchmark run on a Macbook Pro from 2017, switching
+// from option A to option B reduced the overhead of pushing+popping a
+// ProfileEntry by 10 nanoseconds.
+// On x86/64, release-stores require no explicit hardware barriers or lock
+// instructions.
+// On ARM/64, option B may be slower than option A, because the compiler will
+// generate hardware barriers for every single release-store instead of just
+// for the writes to stackPointer. However, the actual performance impact of
+// this has not yet been measured on ARM, so we're currently using option B
+// everywhere. This is something that we may want to change in the future once
+// we've done measurements.
+
 namespace js {
 
 // A call stack can be specified to the JS engine such that all JS entry/exits
@@ -31,24 +105,38 @@ class ProfileEntry
 {
     // A ProfileEntry represents either a C++ profile entry or a JS one.
 
+    // WARNING WARNING WARNING
+    //
+    // All the fields below are Atomic<...,ReleaseAcquire>. This is needed so
+    // that writes to these fields are release-writes, which ensures that
+    // earlier writes in this thread don't get reordered after the writes to
+    // these fields. In particular, the decrement of the stack pointer in
+    // PseudoStack::pop() is a write that *must* happen before the values in
+    // this ProfileEntry are changed. Otherwise, the sampler thread might see
+    // an inconsistent state where the stack pointer still points to a
+    // ProfileEntry which has already been popped off the stack and whose
+    // fields have now been partially repopulated with new values.
+    // See the "Concurrency considerations" paragraph at the top of this file
+    // for more details.
+
     // Descriptive label for this entry. Must be a static string! Can be an
     // empty string, but not a null pointer.
-    const char* label_;
+    mozilla::Atomic<const char*, mozilla::ReleaseAcquire> label_;
 
     // An additional descriptive string of this entry which is combined with
     // |label_| in profiler output. Need not be (and usually isn't) static. Can
     // be null.
-    const char* dynamicString_;
+    mozilla::Atomic<const char*, mozilla::ReleaseAcquire> dynamicString_;
 
     // Stack pointer for non-JS entries, the script pointer otherwise.
-    void* spOrScript;
+    mozilla::Atomic<void*, mozilla::ReleaseAcquire> spOrScript;
 
     // Line number for non-JS entries, the bytecode offset otherwise.
-    int32_t lineOrPcOffset;
+    mozilla::Atomic<int32_t, mozilla::ReleaseAcquire> lineOrPcOffset;
 
-    // Bits 0..1 hold the Kind. Bits 2..3 are unused. Bits 4..12 hold the
+    // Bits 0...1 hold the Kind. Bits 2...3 are unused. Bits 4...12 hold the
     // Category.
-    uint32_t kindAndCategory_;
+    mozilla::Atomic<uint32_t, mozilla::ReleaseAcquire> kindAndCategory_;
 
     static int32_t pcToOffset(JSScript* aScript, jsbytecode* aPc);
 
@@ -160,7 +248,8 @@ class ProfileEntry
     // Note that the pointer returned might be invalid.
     JSScript* rawScript() const {
         MOZ_ASSERT(isJs());
-        return (JSScript*)spOrScript;
+        void* script = spOrScript;
+        return static_cast<JSScript*>(script);
     }
 
     // We can't know the layout of JSScript, so look in vm/GeckoProfiler.cpp.
@@ -198,8 +287,9 @@ RegisterContextProfilingEventMarker(JSContext* cx, void (*fn)(const char*));
 //
 // - When pushing a new entry, we increment the stack pointer -- making the new
 //   entry visible to the sampler thread -- only after the new entry has been
-//   fully written. The stack pointer is Atomic<> (with SequentiallyConsistent
-//   semantics) to ensure the incrementing is not reordered before the writes.
+//   fully written. The stack pointer is Atomic<uint32_t,ReleaseAcquire>, so
+//   the increment is a release-store, which ensures that this store is not
+//   reordered before the writes of the entry.
 //
 // - When popping an old entry, the only operation is the decrementing of the
 //   stack pointer, which is obviously atomic.
@@ -225,7 +315,8 @@ class PseudoStack
         }
 
         // This must happen at the end! The compiler will not reorder this
-        // update because stackPointer is Atomic.
+        // update because stackPointer is Atomic<..., ReleaseAcquire>, so any
+        // the writes above will not be reordered below the stackPointer store.
         // Do the read and the write as two separate statements, in order to
         // make it clear that we don't need an atomic increment, which would be
         // more expensive on x86 than the separate operations done here.
@@ -242,7 +333,9 @@ class PseudoStack
         }
 
         // This must happen at the end! The compiler will not reorder this
-        // update because stackPointer is Atomic.
+        // update because stackPointer is Atomic<..., ReleaseAcquire>, which
+        // makes this assignment a release-store, so the writes above will not
+        // be reordered to occur after the stackPointer store.
         // Do the read and the write as two separate statements, in order to
         // make it clear that we don't need an atomic increment, which would be
         // more expensive on x86 than the separate operations done here.
@@ -280,7 +373,13 @@ class PseudoStack
     // determine the number of valid samples in entries. When this is less
     // than MaxEntries, it refers to the first free entry past the top of the
     // in-use stack (i.e. entries[stackPointer - 1] is the top stack entry).
-    mozilla::Atomic<uint32_t, mozilla::SequentiallyConsistent> stackPointer;
+    //
+    // WARNING WARNING WARNING
+    //
+    // This is an atomic variable that uses ReleaseAcquire memory ordering.
+    // See the "Concurrency considerations" paragraph at the top of this file
+    // for more details.
+    mozilla::Atomic<uint32_t, mozilla::ReleaseAcquire> stackPointer;
 };
 
 #endif  /* js_ProfilingStack_h */
