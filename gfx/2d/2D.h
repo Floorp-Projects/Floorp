40964854b3b08a3270846844fa0237bf4de52998
Jamie Nicol <jnicol@mozilla.com>
2018-02-05 17:50:41 +0000

    Bug 1429508 - Make SnapshotTiled::GetDataSurface return a surface the size of backed tiles only. r=bas
    Fix the DrawTargetTiled::mRect initialization, and expose through the
    virtual function GetRect(). Make SnapshotTiled::GetDataSurface()
    allocate a surface the size of this rect, rather the XMost and
    YMost. Callers of SourceSurface::GetDataSurface() can query
    SourceSurface::GetRect() and apply the necessary offset themselves.
    
    MozReview-Commit-ID: C31FGirQ0oK

--- gfx/2d/2D.h
+++ gfx/2d/2D.h
@@ -369,6 +369,9 @@ public:
 
   virtual SurfaceType GetType() const = 0;
   virtual IntSize GetSize() const = 0;
+  virtual IntRect GetRect() const {
+    return IntRect(IntPoint(0, 0), GetSize());
+  }
   virtual SurfaceFormat GetFormat() const = 0;
 
   /** This returns false if some event has made this source surface invalid for
@@ -938,6 +941,9 @@ public:
   virtual already_AddRefed<SourceSurface> IntoLuminanceSource(LuminanceType aLuminanceType,
                                                               float aOpacity);
   virtual IntSize GetSize() const = 0;
+  virtual IntRect GetRect() const {
+    return IntRect(IntPoint(0, 0), GetSize());
+  }
 
   /**
    * If possible returns the bits to this DrawTarget for direct manipulation. While
