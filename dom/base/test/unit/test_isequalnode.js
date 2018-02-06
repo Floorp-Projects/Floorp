4982c74cc9a5a5f278d6a52154f0996b2c7340db
Boris Zbarsky <bzbarsky@mit.edu>
2018-02-06 13:03:44 -0500

    Bug 1435764 part 4.  Use NodeFilter in various system-JS places instead of hardcoding constant values.  r=kmag
    MozReview-Commit-ID: HuwuVXWcWQE

--- dom/base/test/unit/test_isequalnode.js
+++ dom/base/test/unit/test_isequalnode.js
@@ -3,6 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+Components.utils.importGlobalProperties(["NodeFilter"]);
+
 function run_test()
 {
   /*
@@ -373,10 +375,10 @@ function test_isEqualNode_wholeDoc()
   doc = ParseFile("isequalnode_data.xml");
   var doc2 = ParseFile("isequalnode_data.xml");
   var tw1 =
-    doc.createTreeWalker(doc, 0xFFFFFFFF /* NodeFilter.SHOW_ALL */,
+    doc.createTreeWalker(doc, NodeFilter.SHOW_ALL,
                          null);
   var tw2 =
-    doc2.createTreeWalker(doc2, 0xFFFFFFFF /* NodeFilter.SHOW_ALL */,
+    doc2.createTreeWalker(doc2, NodeFilter.SHOW_ALL,
                           null);
   do {
     check_eq_nodes(tw1.currentNode, tw2.currentNode);
