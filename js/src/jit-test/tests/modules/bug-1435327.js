fe71cdf782f153eb70e161a6b0719f7181364e4c
Jon Coppeard <jcoppeard@mozilla.com>
2018-02-06 11:53:39 +0000

    Bug 1435327 - Fix error checking while constructing module syntax error r=bbouvier

---
+++ js/src/jit-test/tests/modules/bug-1435327.js
@@ -0,0 +1,32 @@
+if (!('oomTest' in this))
+    quit();
+
+lfLogBuffer = `
+  let moduleRepo = {};
+  setModuleResolveHook(function(x, specifier) {
+        return moduleRepo[specifier];
+  });
+  let c = moduleRepo['c'] = parseModule("");
+  let d = moduleRepo['d'] = parseModule("import { a } from 'c'; a;");
+  d.declarationInstantiation();
+`;
+lfLogBuffer = lfLogBuffer.split('\n');
+var lfCodeBuffer = "";
+while (true) {
+    var line = lfLogBuffer.shift();
+    if (line == null) {
+        break;
+    } else {
+        lfCodeBuffer += line + "\n";
+    }
+}
+if (lfCodeBuffer) loadFile(lfCodeBuffer);
+function loadFile(lfVarx) {
+    try {
+        oomTest(function() {
+            let m = parseModule(lfVarx);
+            m.declarationInstantiation();
+            m.evaluation();
+        });
+    } catch (lfVare) {}
+}
