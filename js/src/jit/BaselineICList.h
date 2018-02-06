40c74e60139c30078a1e1afa828c1fd8c8015970
Matthew Gaudet <mgaudet@mozilla.com>
2018-02-06 11:43:25 -0500

    Bug 1433959 - Convert GetIntrinsic inline cache to CacheIR. r=tcampbell

--- js/src/jit/BaselineICList.h
+++ js/src/jit/BaselineICList.h
@@ -56,7 +56,6 @@ namespace jit {
     _(BindName_Fallback)                         \
                                                  \
     _(GetIntrinsic_Fallback)                     \
-    _(GetIntrinsic_Constant)                     \
                                                  \
     _(SetProp_Fallback)                          \
                                                  \
