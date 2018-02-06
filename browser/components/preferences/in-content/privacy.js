02bd1de7337bf44d4c20691ef6fca6ed7c06bb53
Gurzau Raul <rgurzau@mozilla.com>
2018-02-06 22:03:23 +0200

    Backed out 2 changesets (bug 1429593) for l10n issues
    Backed out changeset 9dfaa0724a42 (bug 1429593)
    Backed out changeset 20829d727fdf (bug 1429593)

--- browser/components/preferences/in-content/privacy.js
+++ browser/components/preferences/in-content/privacy.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* import-globals-from extensionControlled.js */
 /* import-globals-from preferences.js */
 
 /* FIXME: ESlint globals workaround should be removed once bug 1395426 gets fixed */
