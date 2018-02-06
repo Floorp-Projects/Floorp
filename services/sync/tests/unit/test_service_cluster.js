078bbbe8e44522dba7622dd5c16534c4dad79cdd
Mark Hammond <mhammond@skippinet.com.au>
2018-02-06 14:05:45 +1100

    Bug 1435929 - refactor browserid_identity.js to be less confusing and error prone. r=eoger,tcsc
    MozReview-Commit-ID: IJPQv4ZvJlp

--- services/sync/tests/unit/test_service_cluster.js
+++ services/sync/tests/unit/test_service_cluster.js
@@ -16,13 +16,12 @@ add_task(async function test_findCluster() {
     Service.identity._ensureValidToken = () => Promise.reject(new Error("Connection refused"));
 
     _("_findCluster() throws on network errors (e.g. connection refused).");
-    await Assert.rejects(Service._clusterManager._findCluster());
+    await Assert.rejects(Service.identity._findCluster());
 
-    Service.identity._ensureValidToken = () => Promise.resolve(true);
-    Service.identity._token = { endpoint: "http://weave.user.node" };
+    Service.identity._ensureValidToken = () => Promise.resolve({ endpoint: "http://weave.user.node" });
 
     _("_findCluster() returns the user's cluster node");
-    let cluster = await Service._clusterManager._findCluster();
+    let cluster = await Service.identity._findCluster();
     Assert.equal(cluster, "http://weave.user.node/");
 
   } finally {
@@ -37,19 +36,19 @@ add_task(async function test_setCluster() {
     _("Check initial state.");
     Assert.equal(Service.clusterURL, "");
 
-    Service._clusterManager._findCluster = () => "http://weave.user.node/";
+    Service.identity._findCluster = () => "http://weave.user.node/";
 
     _("Set the cluster URL.");
-    Assert.ok((await Service._clusterManager.setCluster()));
+    Assert.ok((await Service.identity.setCluster()));
     Assert.equal(Service.clusterURL, "http://weave.user.node/");
 
     _("Setting it again won't make a difference if it's the same one.");
-    Assert.ok(!(await Service._clusterManager.setCluster()));
+    Assert.ok(!(await Service.identity.setCluster()));
     Assert.equal(Service.clusterURL, "http://weave.user.node/");
 
     _("A 'null' response won't make a difference either.");
-    Service._clusterManager._findCluster = () => null;
-    Assert.ok(!(await Service._clusterManager.setCluster()));
+    Service.identity._findCluster = () => null;
+    Assert.ok(!(await Service.identity.setCluster()));
     Assert.equal(Service.clusterURL, "http://weave.user.node/");
   } finally {
     Svc.Prefs.resetBranch("");
