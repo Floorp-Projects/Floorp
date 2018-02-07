f740a7965376bbd28def355f826cfebe3e639147
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-02-07 22:07:08 +0000

    Bug 1436559 - stop doing busy-work in setOverLink and make textToSubURI available on Services.jsm, r=florian
    MozReview-Commit-ID: F63kE4GE67B

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1275,10 +1275,8 @@ nsContextMenu.prototype = {
     // Let's try to unescape it using a character set
     // in case the address is not ASCII.
     try {
-      const textToSubURI = Cc["@mozilla.org/intl/texttosuburi;1"].
-                           getService(Ci.nsITextToSubURI);
-      addresses = textToSubURI.unEscapeURIForUI(gContextMenuContentData.charSet,
-                                                addresses);
+      addresses = Services.textToSubURI.unEscapeURIForUI(gContextMenuContentData.charSet,
+                                                         addresses);
     } catch (ex) {
       // Do nothing.
     }
