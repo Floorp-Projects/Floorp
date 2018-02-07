8f165eed3c3a4c718a737db8f9d1d3dfce56308b
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-02-07 13:54:39 +0000

    Bug 1427726 - remove support for remote JAR files, r=michal
    MozReview-Commit-ID: H7aaTmj3FI1

--- modules/libjar/nsJARChannel.h
+++ modules/libjar/nsJARChannel.h
@@ -33,7 +33,6 @@ class nsInputStreamPump;
 //-----------------------------------------------------------------------------
 
 class nsJARChannel final : public nsIJARChannel
-                         , public mozilla::net::MemoryDownloader::IObserver
                          , public nsIStreamListener
                          , public nsIThreadRetargetableRequest
                          , public nsIThreadRetargetableStreamListener
@@ -66,12 +65,6 @@ private:
     nsresult CheckPendingEvents();
     void NotifyError(nsresult aError);
     void FireOnProgress(uint64_t aProgress);
-    virtual void OnDownloadComplete(mozilla::net::MemoryDownloader* aDownloader,
-                                    nsIRequest* aRequest,
-                                    nsISupports* aCtxt,
-                                    nsresult aStatus,
-                                    mozilla::net::MemoryDownloader::Data aData)
-        override;
 
     nsCString                       mSpec;
 
@@ -90,10 +83,6 @@ private:
     nsCOMPtr<nsISupports>           mListenerContext;
     nsCString                       mContentType;
     nsCString                       mContentCharset;
-    nsCString                       mContentDispositionHeader;
-    /* mContentDisposition is uninitialized if mContentDispositionHeader is
-     * empty */
-    uint32_t                        mContentDisposition;
     int64_t                         mContentLength;
     uint32_t                        mLoadFlags;
     nsresult                        mStatus;
@@ -108,7 +97,6 @@ private:
 
     bool                            mIsUnsafe;
 
-    mozilla::net::MemoryDownloader::Data mTempMem;
     nsCOMPtr<nsIInputStreamPump>    mPump;
     // mRequest is only non-null during OnStartRequest, so we'll have a pointer
     // to the request if we get called back via RetargetDeliveryTo.
@@ -122,9 +110,6 @@ private:
 
     // use StreamTransportService as background thread
     nsCOMPtr<nsIEventTarget>        mWorker;
-
-    // True if this channel should not download any remote files.
-    bool                            mBlockRemoteFiles;
 };
 
 #endif // nsJARChannel_h__
