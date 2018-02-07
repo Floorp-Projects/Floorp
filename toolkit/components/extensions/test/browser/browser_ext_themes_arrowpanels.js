32e59e4f61fafcacf201a7274132165d0d14daf9
Connor Masini <cjmasini@gmail.com>
2018-02-07 00:22:22 -0500

    Bug 1417880: Allow theming arrow panels(popups). r=jaws
    MozReview-Commit-ID: 9f1sSzaGZzf
    
    --HG--
    extra : rebase_source : 786617d658f1bf973da8f103dd41691488b7f20e

---
+++ toolkit/components/extensions/test/browser/browser_ext_themes_arrowpanels.js
@@ -0,0 +1,72 @@
+"use strict";
+
+function openIdentityPopup() {
+  let promise = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "popupshown");
+  gIdentityHandler._identityBox.click();
+  return promise;
+}
+
+function closeIdentityPopup() {
+  let promise = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "popuphidden");
+  gIdentityHandler._identityPopup.hidePopup();
+  return promise;
+}
+
+// This test checks applied WebExtension themes that attempt to change
+// popup properties
+
+add_task(async function test_popup_styling(browser, accDoc) {
+  const POPUP_BACKGROUND_COLOR = "#FF0000";
+  const POPUP_TEXT_COLOR = "#008000";
+  const POPUP_BORDER_COLOR = "#0000FF";
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "theme": {
+        "images": {
+          "headerURL": "image1.png",
+        },
+        "colors": {
+          "accentcolor": ACCENT_COLOR,
+          "textcolor": TEXT_COLOR,
+          "popup": POPUP_BACKGROUND_COLOR,
+          "popup_text": POPUP_TEXT_COLOR,
+          "popup_border": POPUP_BORDER_COLOR,
+        },
+      },
+    },
+    files: {
+      "image1.png": BACKGROUND,
+    },
+  });
+
+
+  await BrowserTestUtils.withNewTab({ gBrowser, url: "https://example.com" }, async function(browser) {
+    await extension.startup();
+
+    // Open the information arrow panel
+    await openIdentityPopup();
+
+    let arrowContent = document.getAnonymousElementByAttribute(gIdentityHandler._identityPopup, "class", "panel-arrowcontent");
+    let arrowContentComputedStyle = window.getComputedStyle(arrowContent);
+    // Ensure popup background color was set properly
+    Assert.equal(
+      arrowContentComputedStyle.getPropertyValue("background-color"),
+      `rgb(${hexToRGB(POPUP_BACKGROUND_COLOR).join(", ")})`,
+      "Popup background color should have been themed"
+    );
+
+    // Ensure popup text color was set properly
+    Assert.equal(
+      arrowContentComputedStyle.getPropertyValue("color"),
+      `rgb(${hexToRGB(POPUP_TEXT_COLOR).join(", ")})`,
+      "Popup text color should have been themed"
+     );
+
+    // Ensure popup border color was set properly
+    testBorderColor(arrowContent, POPUP_BORDER_COLOR)
+
+    await closeIdentityPopup();
+    await extension.unload();
+  });
+});
