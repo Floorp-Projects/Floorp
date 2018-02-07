cb9419b55ec4f56fd747819d02dbe278bc40dd0d
Dylan Roeh <droeh@mozilla.com>
2018-02-07 09:15:12 -0600

    Bug 1434126 - Call GeckoSession.closeWindow() from onDestroy() in CustomTabsActivity and WebAppActivity. r=snorp

--- mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
+++ mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
@@ -179,6 +179,7 @@ public class CustomTabsActivity extends AppCompatActivity
 
     @Override
     public void onDestroy() {
+        mGeckoSession.closeWindow();
         mTextSelection.destroy();
         mFormAssistPopup.destroy();
         mDoorHangerPopup.destroy();
