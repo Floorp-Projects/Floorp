31b1fb7d6d8ad7bb7ff663a20949df42c9d6a643
Mark Banner <standard8@mozilla.com>
2018-02-07 16:16:09 +0000

    Bug 1436389 - Update complexity rules to adapt for the algorithm in the new ESLint. r=mossop
    MozReview-Commit-ID: 1LlfamNvBEM

--- toolkit/components/passwordmgr/.eslintrc.js
+++ toolkit/components/passwordmgr/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   "rules": {
     // XXX Bug 1358949 - This should be reduced down - probably to 20 or to
     // be removed & synced with the mozilla/recommended value.
-    "complexity": ["error", 43],
+    "complexity": ["error", 56],
 
     "no-unused-vars": ["error", {"args": "none", "vars": "local", "varsIgnorePattern": "^(ids|ignored|unused)$"}],
   }
