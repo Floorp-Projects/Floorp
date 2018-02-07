6d75918055ed031505070b259ec18adffe0988ce
Kirk Steuber <ksteuber@mozilla.com>
2018-02-07 14:49:49 -0800

    Bug 1429150 - Make the path to the root of the test data directory available via a pref r=ahal
    In order to facilitate testing of enterprise policies that must be applied via a JSON at startup, the path to the test data will now be made available during testing.
    
    MozReview-Commit-ID: IUhXXsiPRYW
    
    --HG--
    extra : rebase_source : 0b7c405dc3ab3ad70280b78745944b879e923a37
    extra : source : eb38c57de57a288f67156271c09bd867b1fb2643

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -1841,6 +1841,10 @@ toolbar#nav-bar {
         # failing connection attempts, and hangs (bug 1397201)
         options.extraPrefs.append("marionette.log.level=%s" % "TRACE")
 
+        options.extraPrefs.append(
+            "mochitest.testRoot=%s" %
+            self.testRootAbs)
+
         # get extensions to install
         extensions = self.getExtensionsToInstall(options)
 
