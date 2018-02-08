5b70ea834b25dd55127a5ff5d2bb6ecfd3c09432
Jesse Schwartzentruber <jdschwa@gmail.com>
2018-02-08 16:58:12 -0500

    Bug 1425406 - Add a linux64 clang 6 (pre) toolchain with the macosx64 native sanitizer dylibs. r=froydnj
    MozReview-Commit-ID: Ig9xpBDcjNu
    
    --HG--
    extra : rebase_source : 278bd4fffb82d12e1bc4eb72458bdac3ba62e11f

--- build/build-clang/build-clang.py
+++ build/build-clang/build-clang.py
@@ -213,9 +213,10 @@ def build_one_stage(cc, cxx, asm, ld, ar, ranlib, libtool,
                        "-DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER",
                        "-DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY",
                        "-DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY",
-                       "-DCMAKE_MACOSX_RPATH=@executable_path",
+                       "-DCMAKE_MACOSX_RPATH=ON",
                        "-DCMAKE_OSX_ARCHITECTURES=x86_64",
                        "-DDARWIN_osx_ARCHS=x86_64",
+                       "-DDARWIN_osx_SYSROOT=%s" % slashify_path(os.getenv("CROSS_SYSROOT")),
                        "-DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-apple-darwin11"]
     build_package(build_dir, cmake_args)
 
@@ -371,6 +372,9 @@ if __name__ == "__main__":
     parser.add_argument('--clean', required=False,
                         action='store_true',
                         help="Clean the build directory")
+    parser.add_argument('--skip-tar', required=False,
+                        action='store_true',
+                        help="Skip tar packaging stage")
 
     args = parser.parse_args()
     config = json.load(args.config)
@@ -595,5 +599,6 @@ if __name__ == "__main__":
         prune_final_dir_for_clang_tidy(os.path.join(final_stage_dir, "clang"))
         package_name = "clang-tidy"
 
-    ext = "bz2" if is_darwin() or is_windows() else "xz"
-    build_tar_package("tar", "%s.tar.%s" % (package_name, ext), final_stage_dir, "clang")
+    if not args.skip_tar:
+        ext = "bz2" if is_darwin() or is_windows() else "xz"
+        build_tar_package("tar", "%s.tar.%s" % (package_name, ext), final_stage_dir, "clang")
