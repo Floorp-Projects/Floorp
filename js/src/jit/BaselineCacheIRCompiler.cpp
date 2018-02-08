c91be4bfc2dc2b0c0c71ad2030cfd6e407bc0340
Ted Campbell <tcampbell@mozilla.com>
2018-02-08 16:20:18 -0500

    Bug 1432764 - Support Debug OSR with CacheIR_Regular ICs on stack r=jandem
    MozReview-Commit-ID: 1ONbWxPpS4t

--- js/src/jit/BaselineCacheIRCompiler.cpp
+++ js/src/jit/BaselineCacheIRCompiler.cpp
@@ -2343,6 +2343,24 @@ ICCacheIR_Updated::stubDataStart()
     return reinterpret_cast<uint8_t*>(this) + stubInfo_->stubDataOffset();
 }
 
+/* static */ ICCacheIR_Regular*
+ICCacheIR_Regular::Clone(JSContext* cx, ICStubSpace* space, ICStub* firstMonitorStub,
+                         ICCacheIR_Regular& other)
+{
+    const CacheIRStubInfo* stubInfo = other.stubInfo();
+    MOZ_ASSERT(stubInfo->makesGCCalls());
+
+    size_t bytesNeeded = stubInfo->stubDataOffset() + stubInfo->stubDataSize();
+    void* newStub = space->alloc(bytesNeeded);
+    if (!newStub)
+        return nullptr;
+
+    ICCacheIR_Regular* res = new(newStub) ICCacheIR_Regular(other.jitCode(), stubInfo);
+    stubInfo->copyStubData(&other, res);
+    return res;
+}
+
+
 /* static */ ICCacheIR_Monitored*
 ICCacheIR_Monitored::Clone(JSContext* cx, ICStubSpace* space, ICStub* firstMonitorStub,
                            ICCacheIR_Monitored& other)
