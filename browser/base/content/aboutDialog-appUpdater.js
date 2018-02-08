228e35aab9259af2e76799b79a8e7b33126bcda5
Kirk Steuber <ksteuber@mozilla.com>
2018-02-08 09:41:54 -0800

    Bug 1429150 - Change nsIApplicationUpdateService::canCheckForUpdates implementation to respect new enterprise policy preventing app update r=Felipe,mhowell
    MozReview-Commit-ID: 7vPaDbONcRr

--- browser/base/content/aboutDialog-appUpdater.js
+++ browser/base/content/aboutDialog-appUpdater.js
@@ -137,8 +137,9 @@ appUpdater.prototype =
 
   // true when updating is disabled by an administrator.
   get updateDisabledAndLocked() {
-    return !this.updateEnabled &&
-           Services.prefs.prefIsLocked("app.update.enabled");
+    return (!this.updateEnabled &&
+           Services.prefs.prefIsLocked("app.update.enabled")) ||
+           !Services.policies.isAllowed("appUpdate");
   },
 
   // true when updating is enabled.
