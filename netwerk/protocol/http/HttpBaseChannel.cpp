caa46e7aeb475f1cf242a486bfd5eb619a5f11bd
Nicholas Hurley <hurley@mozilla.com>
2018-02-07 16:05:38 -0800

    Bug 1436517 - Limit access to the server-timing header to HTTPS contexts. r=mcmanus
    This also introduces a hidden pref to allow server-timing access from
    HTTP contexts for the purposes of our xpcshell tests. We'll remove that
    once we get h2 (and therefore tls test) support for server-timing
    trailers (https://bugzilla.mozilla.org/show_bug.cgi?id=1436601).
    
    This does not reject or otherwise error when receiving server-timing
    headers or trailers on non-HTTPS contexts, it just makes it unavailable
    outside the channel.
    
    MozReview-Commit-ID: qi4h0VQknE

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -4497,15 +4497,20 @@ HttpBaseChannel::GetServerTiming(nsIArray **aServerTiming)
 {
   NS_ENSURE_ARG_POINTER(aServerTiming);
 
-  nsTArray<nsCOMPtr<nsIServerTiming>> data;
-  nsresult rv = NS_OK;
-  nsCOMPtr<nsIMutableArray> array = do_CreateInstance(NS_ARRAY_CONTRACTID, &rv);
-  NS_ENSURE_SUCCESS(rv, rv);
+  bool isHTTPS = false;
+  if (gHttpHandler->AllowPlaintextServerTiming() ||
+      (NS_SUCCEEDED(mURI->SchemeIs("https", &isHTTPS)) && isHTTPS)) {
+    nsTArray<nsCOMPtr<nsIServerTiming>> data;
+    nsresult rv = NS_OK;
+    nsCOMPtr<nsIMutableArray> array = do_CreateInstance(NS_ARRAY_CONTRACTID, &rv);
+    NS_ENSURE_SUCCESS(rv, rv);
 
-  ParseServerTimingHeader(mResponseHead, array);
-  ParseServerTimingHeader(mResponseTrailers, array);
+    ParseServerTimingHeader(mResponseHead, array);
+    ParseServerTimingHeader(mResponseTrailers, array);
 
-  array.forget(aServerTiming);
+    array.forget(aServerTiming);
+  }
+
   return NS_OK;
 }
 
