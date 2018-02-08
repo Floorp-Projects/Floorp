f004749d6380b939d0bf570ec454cea06017c92f
Ted Campbell <tcampbell@mozilla.com>
2018-02-08 17:01:59 -0500

    Bug 1382458 - Make tests using inIon() more reliable r=nbp
    A number of jit-tests can fail if an ill-timed GC throws away their
    JitCode. This adds jit options to those tests to prevent JitCode being
    tossed.
    
    MozReview-Commit-ID: 3hay0oIl9Fg
    
    --HG--
    extra : rebase_source : f2ef811f7f25f960cf49414c614a549296252836

--- js/src/jit-test/tests/auto-regress/bug1343513-2.js
+++ js/src/jit-test/tests/auto-regress/bug1343513-2.js
@@ -1,4 +1,8 @@
 // |jit-test| error:RangeError
+setJitCompilerOption("ion.warmup.trigger", 50);
+setJitCompilerOption("offthread-compilation.enable", 0);
+gcPreserveCode();
+
 var i = 0;
 do {
     i++;
