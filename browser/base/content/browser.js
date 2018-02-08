2ac69d0cedf20d00771cd548178c92511918aa7e
Cosmin Sabou <csabou@mozilla.com>
2018-02-08 20:37:13 +0200

    Backed out changeset 46c758115832 (bug 1436559) for ESlint failure on /browser/components/search/test/browser_426329.js on a CLOSED TREE

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4334,17 +4334,17 @@ var XULBrowserWindow = {
   },
 
   setOverLink(url, anchorElt) {
-    if (url) {
-      url = Services.textToSubURI.unEscapeURIForUI("UTF-8", url);
+    const textToSubURI = Cc["@mozilla.org/intl/texttosuburi;1"].
+                         getService(Ci.nsITextToSubURI);
+    url = textToSubURI.unEscapeURIForUI("UTF-8", url);
 
-      // Encode bidirectional formatting characters.
-      // (RFC 3987 sections 3.2 and 4.1 paragraph 6)
-      url = url.replace(/[\u200e\u200f\u202a\u202b\u202c\u202d\u202e]/g,
-                        encodeURIComponent);
+    // Encode bidirectional formatting characters.
+    // (RFC 3987 sections 3.2 and 4.1 paragraph 6)
+    url = url.replace(/[\u200e\u200f\u202a\u202b\u202c\u202d\u202e]/g,
+                      encodeURIComponent);
 
-      if (gURLBar && gURLBar._mayTrimURLs /* corresponds to browser.urlbar.trimURLs */)
-        url = trimURL(url);
-    }
+    if (gURLBar && gURLBar._mayTrimURLs /* corresponds to browser.urlbar.trimURLs */)
+      url = trimURL(url);
 
     this.overLink = url;
     LinkTargetDisplay.update();
