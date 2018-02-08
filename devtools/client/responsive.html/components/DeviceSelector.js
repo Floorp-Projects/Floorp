2db77cb34125a2de680f3c42a49f435547312097
J. Ryan Stinnett <jryans@gmail.com>
2018-02-08 15:22:59 -0600

    Bug 1428816 - Rename deviceListState to loadableState. r=ochameau
    MozReview-Commit-ID: AWIP5njHV3M
    
    --HG--
    extra : rebase_source : 9456863fdbe024c9f351b4c24a8b45d6289b4f1c

--- devtools/client/responsive.html/components/DeviceSelector.js
+++ devtools/client/responsive.html/components/DeviceSelector.js
@@ -80,7 +80,7 @@ class DeviceSelector extends PureComponent {
     let state = devices.listState;
     let listContent;
 
-    if (state == Types.deviceListState.LOADED) {
+    if (state == Types.loadableState.LOADED) {
       listContent = [
         dom.option({
           value: "",
@@ -100,14 +100,14 @@ class DeviceSelector extends PureComponent {
           value: OPEN_DEVICE_MODAL_VALUE,
           title: "",
         }, getStr("responsive.editDeviceList"))];
-    } else if (state == Types.deviceListState.LOADING
-      || state == Types.deviceListState.INITIALIZED) {
+    } else if (state == Types.loadableState.LOADING
+      || state == Types.loadableState.INITIALIZED) {
       listContent = [dom.option({
         value: "",
         title: "",
         disabled: true,
       }, getStr("responsive.deviceListLoading"))];
-    } else if (state == Types.deviceListState.ERROR) {
+    } else if (state == Types.loadableState.ERROR) {
       listContent = [dom.option({
         value: "",
         title: "",
@@ -121,7 +121,7 @@ class DeviceSelector extends PureComponent {
         value: selectedDevice,
         title: selectedDevice,
         onChange: this.onSelectChange,
-        disabled: (state !== Types.deviceListState.LOADED),
+        disabled: (state !== Types.loadableState.LOADED),
       },
       ...listContent
     );
