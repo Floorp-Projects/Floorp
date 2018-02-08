2e6135201b6fd041e4a93833da762854a089936c
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-02-08 09:03:24 +0100

    Bug 1404364 - Enable browser_webconsole_output_copy.js in new console frontend; r=jdescottes.
    The part on the copy context menu was removed since we have a dedicated test
    for that.
    We also delete browser_webconsole_jsterm_copy.js which was doing the same thing.
    
    MozReview-Commit-ID: BGPkeHWb2bL

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_output_copy.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_output_copy.js
@@ -2,107 +2,40 @@
 /* vim: set ft=javascript ts=2 et sw=2 tw=80: */
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
-/* globals goUpdateCommand goDoCommand */
 
 "use strict";
 
-// See Bug 587617.
+// Test copy to clipboard on the console output. See Bug 587617.
+const TEST_URI = "data:text/html,Test copy to clipboard on the console output";
 
-const TEST_URI = "http://example.com/browser/devtools/client/webconsole/" +
-                 "test/test-console.html";
+add_task(async function () {
+  let hud = await openNewTabAndConsole(TEST_URI);
 
-var HUD, outputNode;
-
-add_task(function* () {
-  yield loadTab(TEST_URI);
-
-  let hud = yield openConsole();
-  yield consoleOpened(hud);
-  yield testContextMenuCopy();
-
-  HUD = outputNode = null;
-});
-
-function consoleOpened(hud) {
-  HUD = hud;
-
-  let deferred = defer();
-
-  // See bugs 574036, 586386 and 587617.
-  outputNode = HUD.outputNode;
-
-  HUD.jsterm.clearOutput();
-
-  let controller = top.document.commandDispatcher
-                               .getControllerForCommand("cmd_copy");
-  is(controller.isCommandEnabled("cmd_copy"), false, "cmd_copy is disabled");
-
-  ContentTask.spawn(gBrowser.selectedBrowser, null,
-    "() => content.console.log('Hello world! bug587617')");
-
-  waitForMessages({
-    webconsole: HUD,
-    messages: [{
-      text: "bug587617",
-      category: CATEGORY_WEBDEV,
-      severity: SEVERITY_LOG,
-    }],
-  }).then(([result]) => {
-    let msg = [...result.matched][0];
-    HUD.ui.output.selectMessage(msg);
-
-    outputNode.focus();
-
-    goUpdateCommand("cmd_copy");
-    controller = top.document.commandDispatcher
-                             .getControllerForCommand("cmd_copy");
-    is(controller.isCommandEnabled("cmd_copy"), true, "cmd_copy is enabled");
-
-    // Remove new lines and whitespace since getSelection() includes
-    // a new line between message and line number, but the clipboard doesn't
-    // @see bug 1119503
-    let selection = (HUD.iframeWindow.getSelection() + "")
-      .replace(/\r?\n|\r| /g, "");
-    isnot(selection.indexOf("bug587617"), -1,
-          "selection text includes 'bug587617'");
-
-    waitForClipboard((str) => {
-      // Strip out spaces for comparison ease
-      return selection.trim() == str.trim().replace(/ /g, "");
-    }, () => {
-      goDoCommand("cmd_copy");
-    }, deferred.resolve, deferred.resolve);
+  const smokeMessage = "Hello world!";
+  let onMessage = waitForMessage(hud, smokeMessage);
+  ContentTask.spawn(gBrowser.selectedBrowser, smokeMessage, function (msg) {
+    content.wrappedJSObject.console.log(msg);
   });
-  return deferred.promise;
-}
+  const {node} = await onMessage;
+  ok(true, "Message was logged");
 
-// Test that the context menu "Copy" (which has a different code path) works
-// properly as well.
-function testContextMenuCopy() {
-  let deferred = defer();
+  const selection = node.ownerDocument.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(node);
+  selection.removeAllRanges();
+  selection.addRange(range);
 
-  let contextMenuId = HUD.ui.outputWrapper.getAttribute("context");
-  let contextMenu = HUD.ui.document.getElementById(contextMenuId);
-  ok(contextMenu, "the output node has a context menu");
+  const selectionString = selection.toString().trim();
+  is(selectionString, smokeMessage, `selection has expected "${smokeMessage}" value`);
 
-  let copyItem = contextMenu.querySelector("*[command='cmd_copy']");
-  ok(copyItem, "the context menu on the output node has a \"Copy\" item");
-
-  // Remove new lines and whitespace since getSelection() includes
-  // a new line between message and line number, but the clipboard doesn't
-  // @see bug 1119503
-  let selection = (HUD.iframeWindow.getSelection() + "")
-    .replace(/\r?\n|\r| /g, "");
-
-  copyItem.doCommand();
-
-  waitForClipboard((str) => {
-    // Strip out spaces for comparison ease
-    return selection.trim() == str.trim().replace(/ /g, "");
-  }, () => {
-    goDoCommand("cmd_copy");
-  }, deferred.resolve, deferred.resolve);
-  HUD = outputNode = null;
-
-  return deferred.promise;
-}
+  await waitForClipboardPromise(
+    () => {
+      // The focus is on the JsTerm, so we need to blur it for the copy comand to work.
+      node.ownerDocument.activeElement.blur();
+      goDoCommand("cmd_copy");
+    },
+    data => {
+      return data.trim() === smokeMessage;
+    }
+  );
+});
