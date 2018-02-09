43c0e8642173ea0e9b46d7d0a3addf5a309e955f
Julian Descottes <jdescottes@mozilla.com>
2018-02-09 18:15:54 +0100

    Bug 1405648 - enable browser_webconsole_split_focus.js;r=Honza
    MozReview-Commit-ID: 6SLfByzAsFF
    
    --HG--
    extra : rebase_source : bf9d34dca74a5b7e843e5d0d4a28115f8b7232e0

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_split_focus.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_split_focus.js
@@ -3,62 +3,42 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
- "use strict";
+"use strict";
 
- function test() {
-  info("Test that the split console state is persisted");
+const TEST_URI = "data:text/html;charset=utf-8,<p>Web Console test for splitting</p>";
 
-  let toolbox;
-  let TEST_URI = "data:text/html;charset=utf-8,<p>Web Console test for " +
-                 "splitting</p>";
+add_task(async function () {
+  info("Test that the split console input is focused and restores the focus properly.");
 
-  Task.spawn(runner).then(finish);
+  let toolbox = await openNewTabAndToolbox(TEST_URI, "inspector");
+  ok(!toolbox.splitConsole, "Split console is hidden by default");
 
-  function* runner() {
-    info("Opening a tab while there is no user setting on split console pref");
-    let {tab} = yield loadTab(TEST_URI);
-    let target = TargetFactory.forTab(tab);
-    toolbox = yield gDevTools.showToolbox(target, "inspector");
+  info("Focusing the search box before opening the split console");
+  let inspector = toolbox.getPanel("inspector");
+  inspector.searchBox.focus();
 
-    ok(!toolbox.splitConsole, "Split console is hidden by default");
+  let activeElement = getActiveElement(inspector.panelDoc);
+  is(activeElement, inspector.searchBox, "Search box is focused");
 
-    info("Focusing the search box before opening the split console");
-    let inspector = toolbox.getPanel("inspector");
-    inspector.searchBox.focus();
+  await toolbox.openSplitConsole();
 
-    let activeElement = getActiveElement(inspector.panelDoc);
-    is(activeElement, inspector.searchBox, "Search box is focused");
+  ok(toolbox.splitConsole, "Split console is now visible");
 
-    yield toolbox.openSplitConsole();
+  activeElement = getActiveElement(toolbox.doc);
+  let inputNode = toolbox.getPanel("webconsole").hud.jsterm.inputNode;
+  is(activeElement, inputNode, "Split console input is focused by default");
 
-    ok(toolbox.splitConsole, "Split console is now visible");
+  await toolbox.closeSplitConsole();
 
-    // Use the binding element since jsterm.inputNode is a XUL textarea element.
-    activeElement = getActiveElement(toolbox.doc);
-    activeElement = activeElement.ownerDocument.getBindingParent(activeElement);
-    let inputNode = toolbox.getPanel("webconsole").hud.jsterm.inputNode;
-    is(activeElement, inputNode, "Split console input is focused by default");
+  info("Making sure that the search box is refocused after closing the split console");
+  activeElement = getActiveElement(inspector.panelDoc);
+  is(activeElement, inspector.searchBox, "Search box is focused");
+});
 
-    yield toolbox.closeSplitConsole();
-
-    info("Making sure that the search box is refocused after closing the " +
-         "split console");
-    activeElement = getActiveElement(inspector.panelDoc);
-    is(activeElement, inspector.searchBox, "Search box is focused");
-
-    yield toolbox.destroy();
+function getActiveElement(doc) {
+  let activeElement = doc.activeElement;
+  while (activeElement && activeElement.contentDocument) {
+    activeElement = activeElement.contentDocument.activeElement;
   }
-
-  function getActiveElement(doc) {
-    let activeElement = doc.activeElement;
-    while (activeElement && activeElement.contentDocument) {
-      activeElement = activeElement.contentDocument.activeElement;
-    }
-    return activeElement;
-  }
-
-  function finish() {
-    toolbox = TEST_URI = null;
-    finishTest();
-  }
+  return activeElement;
 }
