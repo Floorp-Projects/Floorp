d43339491730f4b3334d9a781ad16bf72bed30e0
Jan Odvarko <odvarko@gmail.com>
2018-02-09 17:29:30 +0100

    Bug 1435048 - Wait for async UI update; r=ochameau
    MozReview-Commit-ID: Co3WyAIzOLg
    
    --HG--
    extra : rebase_source : f162c0bce3bb0fe92bd3044036b7888bed799887

--- devtools/client/netmonitor/test/browser_net_status-bar.js
+++ devtools/client/netmonitor/test/browser_net_status-bar.js
@@ -34,11 +34,19 @@ add_task(async () => {
   ok(onContentLoad, "There must be DOMContentLoaded label");
   ok(onLoad, "There must be load label");
 
-  // The content should not be empty
-  ok(requestCount.textContent, "There must be request count label text");
-  ok(size.textContent, "There must be size label text");
-  ok(onContentLoad.textContent, "There must be DOMContentLoaded label text");
-  ok(onLoad.textContent, "There must be load label text");
+  // The content should not be empty. The UI update can also be async,
+  // so use waitUntil.
+  await waitUntil(() => requestCount.textContent);
+  ok(true, "There must be request count label text");
+
+  await waitUntil(() => size.textContent);
+  ok(true, "There must be size label text");
+
+  await waitUntil(() => onContentLoad.textContent);
+  ok(true, "There must be DOMContentLoaded label text");
+
+  await waitUntil(() => onLoad.textContent);
+  ok(true, "There must be load label text");
 
   return teardown(monitor);
 });
