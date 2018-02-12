9beab572d0cebb746082fdb55d32a9ede4038ddd
Johann Hofmann <jhofmann@mozilla.com>
2018-02-12 19:28:00 +0100

    Bug 1432743 - Move cookie settings to the Site Data section and remove the cookies dialog from preferences. r=Gijs
    After including cookies in the site data manager in bug 1421737, we would
    like to move the cookies settings to the site data section to give them
    more visibility and to unify site data and cookies.
    
    Our UR has shown that our differentiation between site data and cookies
    is not helpful to users and that they struggle with discovering the
    cookie settings that are hidden in the "custom history" menu.
    
    Since the cookie settings are quite powerful/potentially breaking,
    we changed the top level preference from a checkbox to a radiogroup,
    to be able to highlight the potential breakage associated with "deny".
    
    This grouping is also recommended by the webstorage spec:
    https://www.w3.org/TR/webstorage/#privacy
    
    The cookies dialog is not removed yet, because it is still accessible from
    Page Info, but bug 1348223 will likely remove it entirely.
    
    MozReview-Commit-ID: Adisn70Ks2Q
    
    --HG--
    extra : rebase_source : 333d6165c74d6ff664cd3d603c5d834bfbde4326

--- browser/components/preferences/in-content/privacy.js
+++ browser/components/preferences/in-content/privacy.js
@@ -274,12 +274,6 @@ var gPrivacyPane = {
       }
       return false;
     });
-    setEventListener("historyRememberCookies", "click", function(event) {
-      if (event.button == 0) {
-        gPrivacyPane.showCookies();
-      }
-      return false;
-    });
     setEventListener("historyDontRememberClear", "click", function(event) {
       if (event.button == 0) {
         gPrivacyPane.clearPrivateDataNow(true);
@@ -296,8 +290,6 @@ var gPrivacyPane = {
       gPrivacyPane.updateAutostart);
     setEventListener("cookieExceptions", "command",
       gPrivacyPane.showCookieExceptions);
-    setEventListener("showCookiesButton", "command",
-      gPrivacyPane.showCookies);
     setEventListener("clearDataSettings", "command",
       gPrivacyPane.showClearPrivateDataSettings);
     setEventListener("disableTrackingProtectionExtension", "command",
@@ -423,12 +415,6 @@ var gPrivacyPane = {
     ]);
     appendSearchKeywords("cookieExceptions", [
       bundlePrefs.getString("cookiepermissionstext"),
-    ]);
-    appendSearchKeywords("showCookiesButton", [
-      bundlePrefs.getString("cookiesAll"),
-      bundlePrefs.getString("removeAllCookies.label"),
-      bundlePrefs.getString("removeAllShownCookies.label"),
-      bundlePrefs.getString("removeSelectedCookies.label"),
     ]);
     appendSearchKeywords("trackingProtectionExceptions", [
       bundlePrefs.getString("trackingprotectionpermissionstitle"),
@@ -837,22 +823,24 @@ var gPrivacyPane = {
     acceptThirdPartyLabel.disabled = acceptThirdPartyMenu.disabled = !acceptCookies;
     keepUntil.disabled = menu.disabled = this._autoStartPrivateBrowsing || !acceptCookies;
 
-    return acceptCookies;
+    // Our top-level setting is a radiogroup that only sets "enable all"
+    // and "disable all", so convert the pref value accordingly.
+    return acceptCookies ? "0" : "2";
   },
 
   /**
-   * Enables/disables the "keep until" label and menulist in response to the
-   * "accept cookies" checkbox being checked or unchecked.
+   * Updates the "accept third party cookies" menu based on whether the
+   * "accept cookies" or "block cookies" radio buttons are selected.
    */
   writeAcceptCookies() {
     var accept = document.getElementById("acceptCookies");
     var acceptThirdPartyMenu = document.getElementById("acceptThirdPartyMenu");
 
     // if we're enabling cookies, automatically select 'accept third party always'
-    if (accept.checked)
+    if (accept.value == "0")
       acceptThirdPartyMenu.selectedIndex = 0;
 
-    return accept.checked ? 0 : 2;
+    return parseInt(accept.value, 10);
   },
 
   /**
@@ -904,13 +892,6 @@ var gPrivacyPane = {
     };
     gSubDialog.open("chrome://browser/content/preferences/permissions.xul",
       null, params);
-  },
-
-  /**
-   * Displays all the user's cookies in a dialog.
-   */
-  showCookies(aCategory) {
-    gSubDialog.open("chrome://browser/content/preferences/cookies.xul");
   },
 
   // CLEAR PRIVATE DATA
