9c5831300f20b3843aa97690a8cc684fb41418d9
arthur.iakab <aiakab@mozilla.com>
2018-02-12 12:09:21 +0200

    Backed out 7 changesets (bug 1429932) for frequently failing layout/reftests/table-background/backgr_layers-opacity.html a=backout
    Backed out changeset 24cf388d5d5b (bug 1429932)
    Backed out changeset 8f7a4df5efcc (bug 1429932)
    Backed out changeset c48601351975 (bug 1429932)
    Backed out changeset c91a2b710a65 (bug 1429932)
    Backed out changeset 5d146aec735e (bug 1429932)
    Backed out changeset db244e692584 (bug 1429932)
    Backed out changeset 39ffcc432d0c (bug 1429932)

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -1291,12 +1291,6 @@ protected:
                                          const char* aName,
                                          uint32_t aDelay);
 
-  struct TransactionInvalidations {
-    uint64_t mTransactionId;
-    nsTArray<nsRect> mInvalidations;
-  };
-  TransactionInvalidations* GetInvalidations(uint64_t aTransactionId);
-
   // IMPORTANT: The ownership implicit in the following member variables
   // has been explicitly checked.  If you add any members to this class,
   // please make the ownership explicit (pinkerton, scc).
@@ -1365,6 +1359,10 @@ protected:
 
   mozilla::UniquePtr<nsBidi> mBidiEngine;
 
+  struct TransactionInvalidations {
+    uint64_t mTransactionId;
+    nsTArray<nsRect> mInvalidations;
+  };
   AutoTArray<TransactionInvalidations, 4> mTransactions;
 
   // text performance metrics
@@ -1490,6 +1488,8 @@ protected:
   unsigned              mSuppressResizeReflow : 1;
 
   unsigned              mIsVisual : 1;
+
+  unsigned              mFireAfterPaintEvents : 1;
 
   unsigned              mIsChrome : 1;
   unsigned              mIsChromeOriginImage : 1;
