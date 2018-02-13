c79b55ded7d21984baf811257d60f8f34188435d
Sebastian Hengst <archaeopteryx@coole-files.de>
2018-02-13 16:53:56 +0200

    Backed out changeset 8507a552a358 (bug 1436517) for failing modified xpcshell test netwerk/test/unit/test_header_Server_Timing.js. a=backout

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -293,7 +293,6 @@ nsHttpHandler::nsHttpHandler()
     , mFastOpenStallsIdleTime(10)
     , mFastOpenStallsTimeout(20)
     , mActiveTabPriority(true)
-    , mAllowPlaintextServerTiming(false)
     , mProcessId(0)
     , mNextChannelId(1)
     , mLastActiveTabLoadOptimizationLock("nsHttpConnectionMgr::LastActiveTabLoadOptimization")
@@ -470,7 +469,6 @@ nsHttpHandler::Init()
         prefBranch->AddObserver(TCP_FAST_OPEN_STALLS_LIMIT, this, true);
         prefBranch->AddObserver(TCP_FAST_OPEN_STALLS_IDLE, this, true);
         prefBranch->AddObserver(TCP_FAST_OPEN_STALLS_TIMEOUT, this, true);
-        prefBranch->AddObserver(HTTP_PREF("allow-plaintext-server-timing"), this, false);
         PrefsChanged(prefBranch, nullptr);
     }
 
@@ -1779,10 +1777,6 @@ nsHttpHandler::PrefsChanged(nsIPrefBranch *prefs, const char *pref)
                 NS_WARNING("Wrong value for focused_window_transaction_ratio");
             }
         }
-    }
-
-    if (PREF_CHANGED(HTTP_PREF("allow-plaintext-server-timing"))) {
-        Unused << prefs->GetBoolPref(HTTP_PREF("allow-plaintext-server-timing"), &mAllowPlaintextServerTiming);
     }
 
     //
