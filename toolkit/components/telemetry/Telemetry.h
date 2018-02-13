9c6c1ed9e2c4e8a6b58e978313a82c56236624a4
Jeremy Lempereur <jeremy.lempereur@gmail.com>
2018-02-13 19:08:34 +0100

    Bug 1436680 - Allow non-templated uses of AutoCounter. r=Dexter
    In order to stay consistent with the AutoTimer api, allowed non-templates uses or AutoCounter.
    
    MozReview-Commit-ID: 9qnAeQTIY9T
    
    --HG--
    extra : rebase_source : 8c95992e2d291d628721febbc7b641b91c850288

--- toolkit/components/telemetry/Telemetry.h
+++ toolkit/components/telemetry/Telemetry.h
@@ -305,6 +305,41 @@ private:
   MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
 };
 
+class MOZ_RAII RuntimeAutoCounter
+{
+public:
+  explicit RuntimeAutoCounter(
+    HistogramID aId,
+    uint32_t counterStart = 0 MOZ_GUARD_OBJECT_NOTIFIER_PARAM)
+    : id(aId)
+    , counter(counterStart)
+  {
+    MOZ_GUARD_OBJECT_NOTIFIER_INIT;
+  }
+
+  ~RuntimeAutoCounter()
+  {
+    Accumulate(id, counter);
+  }
+
+  // Prefix increment only, to encourage good habits.
+  void operator++()
+  {
+    ++counter;
+  }
+
+  // Chaining doesn't make any sense, don't return anything.
+  void operator+=(int increment)
+  {
+    counter += increment;
+  }
+
+private:
+  HistogramID id;
+  uint32_t counter;
+  MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
+};
+
 template<HistogramID id>
 class MOZ_RAII AutoCounter {
 public:
