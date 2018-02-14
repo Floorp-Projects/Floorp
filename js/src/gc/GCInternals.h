6f6fe3bdada125b6d8173db1eb881d67901639c1
Jon Coppeard <jcoppeard@mozilla.com>
2018-02-14 10:54:13 +0000

    Bug 1437554 - Release the exclusive access lock when not collecting the atoms zone r=sfink

--- js/src/gc/GCInternals.h
+++ js/src/gc/GCInternals.h
@@ -35,11 +35,14 @@ class MOZ_RAII AutoTraceSession
     explicit AutoTraceSession(JSRuntime* rt, JS::HeapState state = JS::HeapState::Tracing);
     ~AutoTraceSession();
 
-    // Threads with an exclusive context can hit refillFreeList while holding
-    // the exclusive access lock. To avoid deadlocking when we try to acquire
-    // this lock during GC and the other thread is waiting, make sure we hold
-    // the exclusive access lock during GC sessions.
-    AutoLockForExclusiveAccess lock;
+    // Constructing an AutoTraceSession takes the exclusive access lock, but GC
+    // may release it during a trace session if we're not collecting the atoms
+    // zone.
+    mozilla::Maybe<AutoLockForExclusiveAccess> maybeLock;
+
+    AutoLockForExclusiveAccess& lock() {
+        return maybeLock.ref();
+    }
 
   protected:
     JSRuntime* runtime;
