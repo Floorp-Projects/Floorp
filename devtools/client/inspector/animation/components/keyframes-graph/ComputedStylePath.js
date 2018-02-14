c75d478edcff9cda1c214be25b14e607f248a1fd
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-02-14 23:18:12 +0900

    Bug 1416106 - Part 7: Implement color graph. r=gl
    MozReview-Commit-ID: 4ek6LXtsmKc

--- devtools/client/inspector/animation/components/keyframes-graph/ComputedStylePath.js
+++ devtools/client/inspector/animation/components/keyframes-graph/ComputedStylePath.js
@@ -133,9 +133,10 @@ class ComputedStylePath extends PureComponent {
    * Return react dom fron given path segments.
    *
    * @param {Array} segments
+   * @param {Object} style
    * @return {Element}
    */
-  renderPathSegments(segments) {
+  renderPathSegments(segments, style) {
     const { graphHeight } = this.props;
 
     for (const segment of segments) {
@@ -146,7 +147,7 @@ class ComputedStylePath extends PureComponent {
     d += toPathString(segments);
     d += `L${ segments[segments.length - 1].x },0 Z`;
 
-    return dom.path({ d });
+    return dom.path({ d, style });
   }
 }
 
