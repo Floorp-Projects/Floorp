5afa8a603f67aad58cc11c9eed9c068220f685de
Coroiu Cristina <ccoroiu@mozilla.com>
2018-02-14 23:47:10 +0200

    Backed out changeset 301951ce868f (bug 1428108) for xpcshell failures at browser/components/newtab/tests/xpcshell/test_AboutNewTabService.js on a CLOSED TREE

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1291,11 +1291,7 @@ pref("browser.newtabpage.activity-stream.enabled", true);
 pref("browser.newtabpage.activity-stream.prerender", true);
 pref("browser.newtabpage.activity-stream.aboutHome.enabled", true);
 #ifndef RELEASE_OR_BETA
-#ifdef MOZILLA_OFFICIAL
 pref("browser.newtabpage.activity-stream.debug", false);
-#else
-pref("browser.newtabpage.activity-stream.debug", true);
-#endif
 #endif
 
 pref("browser.library.activity-stream.enabled", true);
