fcc0c1f4f14dec848baea0e80eec0567b0abde73
Michael Ratcliffe <mratcliffe@mozilla.com>
2018-02-14 16:09:04 +0000

    Bug 1437851 - Enable browser_console_open_or_focus.js in new frontend r=jdescottes
    MozReview-Commit-ID: AXnwzSnaUtl

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_console_open_or_focus.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_console_open_or_focus.js
@@ -3,38 +3,38 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/* import-globals-from head.js */
+
 // Test that the "browser console" menu item opens or focuses (if already open)
 // the console window instead of toggling it open/close.
 
 "use strict";
 
-var {Tools} = require("devtools/client/definitions");
+const TEST_MESSAGE = "testmessage";
+const { Tools } = require("devtools/client/definitions");
 
-add_task(function* () {
+add_task(async function () {
   let currWindow, hud, mainWindow;
 
   mainWindow = Services.wm.getMostRecentWindow(null);
 
-  yield HUDService.openBrowserConsoleOrFocus();
+  await HUDService.openBrowserConsoleOrFocus();
 
   hud = HUDService.getBrowserConsole();
 
-  console.log("testmessage");
-  yield waitForMessages({
-    webconsole: hud,
-    messages: [{
-      text: "testmessage"
-    }],
-  });
+  console.log(TEST_MESSAGE);
+
+  await waitFor(() => findMessage(hud, TEST_MESSAGE));
+
   currWindow = Services.wm.getMostRecentWindow(null);
-  is(currWindow.document.documentURI, Tools.webConsole.oldWebConsoleURL,
+  is(currWindow.document.documentURI, Tools.webConsole.browserConsoleURL,
      "The Browser Console is open and has focus");
   mainWindow.focus();
-  yield HUDService.openBrowserConsoleOrFocus();
+  await HUDService.openBrowserConsoleOrFocus();
   currWindow = Services.wm.getMostRecentWindow(null);
-  is(currWindow.document.documentURI, Tools.webConsole.oldWebConsoleURL,
+  is(currWindow.document.documentURI, Tools.webConsole.browserConsoleURL,
      "The Browser Console is open and has focus");
-  yield HUDService.toggleBrowserConsole();
+  await HUDService.toggleBrowserConsole();
   hud = HUDService.getBrowserConsole();
   ok(!hud, "Browser Console has been closed");
 });
