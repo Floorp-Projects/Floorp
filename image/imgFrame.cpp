ea2add5d2aae09ce7d459baa69a23736bfe05912
Andrew Osmond <aosmond@mozilla.com>
2018-02-15 09:26:05 -0500

    Bug 1437886 - Prevent shared surfaces from being used without WebRender. r=nical
    Move the initialization of SharedSurfacesParent from the compositor
    thread creation to mirror the other WebRender-specific components, such
    as the render thread creation. Now it will only be created if WebRender
    is in use. Also prevent shared surfaces from being used by the image
    frame allocator, even if image.mem.shared is set -- there is no purpose
    in allowing this at present. It was causing startup crashes for users
    who requested image.mem.shared and/or WebRender via gfx.webrender.all
    but did not actually get WebRender at all. Surfaces would get allocated
    in the shared memory, try to register themselves with the WR render
    thread, and then crash since that thread was never created.

--- image/imgFrame.cpp
+++ image/imgFrame.cpp
@@ -101,7 +101,7 @@ AllocateBufferForImage(const IntSize& size,
   }
 #endif
 
-  if (!aIsAnimated && gfxPrefs::ImageMemShared()) {
+  if (!aIsAnimated && gfxVars::UseWebRender() && gfxPrefs::ImageMemShared()) {
     RefPtr<SourceSurfaceSharedData> newSurf = new SourceSurfaceSharedData();
     if (newSurf->Init(size, stride, format)) {
       return newSurf.forget();
