6cbc46715a76667640b81333ee5cd3a6a755a8db
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-02-16 19:18:00 +0000

    Bug 1350349 - collect data for feed reader usage to evaluate its future, r=mak,data-review=liuche
    This patch adds data collection for 6 different data points:
    - number of live bookmarks the user has
    - number of times the user subscribes to a feed
    - number of times we attempt to open feed preview
    - number of times the user opens a feed popup in the UI
    - number of times the user opens an entry from a feed popup in the UI.
    
    MozReview-Commit-ID: DG9JDFXCjai

--- browser/components/feeds/FeedWriter.js
+++ browser/components/feeds/FeedWriter.js
@@ -70,6 +70,7 @@ function FeedWriter() {
   this._subscribeCallback = null;
   this._defaultHandlerMenuItem = null;
 
+  Services.telemetry.scalarAdd("browser.feeds.preview_loaded", 1);
 
   XPCOMUtils.defineLazyGetter(this, "_mm", () =>
     this._window.QueryInterface(Ci.nsIInterfaceRequestor).
