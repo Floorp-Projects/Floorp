46ecaa40e026c7fabab7c7f7746e9de72863732f
Jeff Walden <jwalden@mit.edu>
2018-02-15 17:36:55 -0800

    Bug 1441657 - Implement mozilla::WrappingMultiply.  r=froydnj

--- js/src/jsmath.cpp
+++ js/src/jsmath.cpp
@@ -115,7 +115,7 @@ using mozilla::IsNegative;
 using mozilla::IsNegativeZero;
 using mozilla::PositiveInfinity;
 using mozilla::NegativeInfinity;
-using mozilla::WrapToSigned;
+using mozilla::WrappingMultiply;
 using JS::ToNumber;
 using JS::GenericNaN;
 
@@ -484,14 +484,13 @@ js::math_floor(JSContext* cx, unsigned argc, Value* vp)
 bool
 js::math_imul_handle(JSContext* cx, HandleValue lhs, HandleValue rhs, MutableHandleValue res)
 {
-    uint32_t a = 0, b = 0;
-    if (!lhs.isUndefined() && !ToUint32(cx, lhs, &a))
+    int32_t a = 0, b = 0;
+    if (!lhs.isUndefined() && !ToInt32(cx, lhs, &a))
         return false;
-    if (!rhs.isUndefined() && !ToUint32(cx, rhs, &b))
+    if (!rhs.isUndefined() && !ToInt32(cx, rhs, &b))
         return false;
 
-    uint32_t product = a * b;
-    res.setInt32(WrapToSigned(product));
+    res.setInt32(WrappingMultiply(a, b));
     return true;
 }
 
