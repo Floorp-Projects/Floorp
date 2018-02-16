65ce67881c9fe01fa8861dbb5e502c6bb8477b4d
Dão Gottwald <dao@mozilla.com>
2018-02-16 09:27:40 +0100

    Backed out changeset f23ac3f571cf
    MozReview-Commit-ID: FLl0Gd0oG6o

--- browser/base/content/test/performance/browser_windowopen_reflows.js
+++ browser/base/content/test/performance/browser_windowopen_reflows.js
@@ -36,6 +36,12 @@ if (Services.appinfo.OS == "WINNT" || Services.appinfo.OS == "Darwin") {
   EXPECTED_REFLOWS.push(
     {
       stack: [
+        "select@chrome://global/content/bindings/textbox.xml",
+        "focusAndSelectUrlBar@chrome://browser/content/browser.js",
+      ],
+    },
+    {
+      stack: [
         "rect@chrome://browser/content/browser-tabsintitlebar.js",
         "_update@chrome://browser/content/browser-tabsintitlebar.js",
         "init@chrome://browser/content/browser-tabsintitlebar.js",
