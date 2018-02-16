e37732b867feddbf1d063792870b9a3359247100
Zibi Braniecki <zbraniecki@mozilla.com>
2018-02-16 14:35:00 -0800

    Bug 1431260 - Migrate Extensions tests to mock locale availability. r=aswan
    MozReview-Commit-ID: KbFa11j69F1
    
    --HG--
    extra : rebase_source : dd8212a3e614dcdd833c9152478871160c82d550

--- toolkit/components/extensions/test/xpcshell/test_ext_i18n.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_i18n.js
@@ -215,7 +215,13 @@ add_task(async function test_i18n_negotiation() {
     } + `(${runTests})`,
   };
 
-  Components.manager.addBootstrappedManifestLocation(do_get_file("data/locales/"));
+  // At the moment extension language negotiation is tied to Firefox language
+  // negotiation result. That means that to test an extension in `fr`, we need
+  // to mock `fr` being available in Firefox and then request it.
+  //
+  // In the future, we should provide some way for tests to decouple their
+  // language selection from that of Firefox.
+  Services.locale.setAvailableLocales(["en-US", "fr", "jp"]);
 
   let contentPage = await ExtensionTestUtils.loadContentPage(`${BASE_URL}/file_sample.html`);
 
