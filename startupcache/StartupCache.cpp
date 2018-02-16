7337adec4563686d98c005bbc0296b7904e5c5c3
Eric Rahm <erahm@mozilla.com>
2018-02-16 15:30:47 -0800

    Bug 1439047 - Part 1: Make StartupCache::PutBuffer take ownership of the buffer. r=froydnj
    This avoids a redundant alloc and copy in `PutBuffer`. All existing callers
    were destroying the passed in buffer after the call.
    
    --HG--
    extra : rebase_source : 065505219d70d26bad49c7eba2cec8edf0e9939d
    extra : amend_source : 118eddad4dc901da02817c788fb98f6f4c85a3f0
    extra : source : 7f0cedfb4bd85bfe1a523168019864c9c6c0e665

--- startupcache/StartupCache.cpp
+++ startupcache/StartupCache.cpp
@@ -302,16 +302,13 @@ StartupCache::GetBuffer(const char* id, UniquePtr<char[]>* outbuf, uint32_t* len
 
 // Makes a copy of the buffer, client retains ownership of inbuf.
 nsresult
-StartupCache::PutBuffer(const char* id, const char* inbuf, uint32_t len)
+StartupCache::PutBuffer(const char* id, UniquePtr<char[]>&& inbuf, uint32_t len)
 {
   NS_ASSERTION(NS_IsMainThread(), "Startup cache only available on main thread");
   WaitOnWriteThread();
   if (StartupCache::gShutdownInitiated) {
     return NS_ERROR_NOT_AVAILABLE;
   }
-
-  auto data = MakeUnique<char[]>(len);
-  memcpy(data.get(), inbuf, len);
 
   nsCString idStr(id);
   // Cache it for now, we'll write all together later.
@@ -330,7 +327,7 @@ StartupCache::PutBuffer(const char* id, const char* inbuf, uint32_t len)
   }
 #endif
 
-  entry = new CacheEntry(Move(data), len);
+  entry = new CacheEntry(Move(inbuf), len);
   mTable.Put(idStr, entry);
   mPendingWrites.AppendElement(idStr);
   return ResetStartupWriteTimer();
