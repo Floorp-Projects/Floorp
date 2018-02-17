909be82cb25877b34f6d993a8a517a56963772b8
Mike Yusko <freshjelly12@yahoo.com>
2018-02-17 14:16:49 +0200

    Bug 1434904 - Remove dependency to testSize.html in test_elementsize.py r=ato
    MozReview-Commit-ID: EOteIE2ArhI

--- testing/marionette/harness/marionette_harness/tests/unit/test_elementsize.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_elementsize.py
@@ -4,15 +4,31 @@
 
 from __future__ import absolute_import
 
+import urllib
+
 from marionette_driver.by import By
 
 from marionette_harness import MarionetteTestCase
 
 
+TEST_SIZE = """
+    <?xml version="1.0"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>Test page for element size</title>
+  </head>
+  <body>
+    <p>Let's get the size of <a href='#' id='linkId'>some really cool link</a></p>
+  </body>
+</html>
+"""
+
+def inline(doc):
+    return "data:text/html;charset=utf-8,{}".format(urllib.quote(doc))
+
 class TestElementSize(MarionetteTestCase):
     def testShouldReturnTheSizeOfALink(self):
-        test_html = self.marionette.absolute_url("testSize.html")
-        self.marionette.navigate(test_html)
+        self.marionette.navigate(inline(TEST_SIZE))
         shrinko = self.marionette.find_element(By.ID, 'linkId')
         size = shrinko.rect
         self.assertTrue(size['width'] > 0)
