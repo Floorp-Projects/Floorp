489f073b87bee1642f5009e5872c7fee79b0f32f
Johann Hofmann <jhofmann@mozilla.com>
2018-02-18 22:48:06 +0100

    Bug 1425613 - Part 2 - Add a test for repeatedly called pref getters. r=florian
    MozReview-Commit-ID: 4doKsld9qGF

--- browser/components/tests/startupRecorder.js
+++ browser/components/tests/startupRecorder.js
@@ -49,7 +49,8 @@ function startupRecorder() {
       "image-drawing": new Set(),
       "image-loading": new Set(),
     },
-    code: {}
+    code: {},
+    prefStats: {},
   };
   this.done = new Promise(resolve => { this._resolve = resolve; });
 }
@@ -138,6 +139,8 @@ startupRecorder.prototype = {
         win.removeEventListener("MozAfterPaint", afterPaintListener);
         win = null;
         this.data.frames = paints;
+        this.data.prefStats = {};
+        Services.prefs.readStats((key, value) => this.data.prefStats[key] = value);
         paints = null;
         this._resolve();
         this._resolve = null;
