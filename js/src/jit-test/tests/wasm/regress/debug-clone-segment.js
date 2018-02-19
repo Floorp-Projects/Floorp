83b242a3f556af2620d68b3f04cf62f1db279fed
Benjamin Bouvier <benj@benj.me>
2018-02-19 12:16:47 +0100

    Bug 1439234: Use the correct segment when initializing a debug-mode cloned Code; r=luke
    MozReview-Commit-ID: 3isCrs5nkNM

---
+++ js/src/jit-test/tests/wasm/regress/debug-clone-segment.js
@@ -0,0 +1,34 @@
+var mod = new WebAssembly.Module(wasmTextToBinary(`
+    (module
+        (func (export "func_0") (result i32)
+         call 0
+        )
+    )
+`));
+
+var g = newGlobal();
+g.parent = this;
+g.eval("(" + function() {
+    var dbg = Debugger(parent);
+    dbg.onEnterFrame = function(frame) {}
+} + ")()");
+
+processModule(mod);
+processModule(mod);
+processModule(mod);
+processModule(mod);
+
+mod = new WebAssembly.Module(wasmTextToBinary(`
+(module (export "func_0" $func1) (func $func1))
+`));
+
+processModule(mod);
+processModule(mod);
+processModule(mod);
+processModule(mod);
+
+function processModule(module) {
+    try {
+        new WebAssembly.Instance(module).exports.func_0();
+    } catch(ex) {}
+}
