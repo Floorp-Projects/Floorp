0f2639c50e9da693f7113233d15cf44039ac1ec8
Sebastian Kaspari <s.kaspari@gmail.com>
2018-02-19 13:06:37 +0100

    [focus] Create build pipeline on taskcluster for pushes to the master branch. (https://github.com/mozilla-mobile/focus-android/issues/2032)
    This patch introduces a new task pipeline on taskcluster:
    
    Build -+--> Unit tests ----+- UI tests --> Preview release
            \-> Code quality -/
    
    If a task in the pipeline fails subsequent tasks won't be executed.
    
    The final "preview release" task will create signed beta APKs that
    can be used for testing purposes.

---
+++ mobile/android/focus-android/tools/taskcluster/schedule-master-build.py
@@ -0,0 +1,141 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+This script will be executed whenever a change is pushed to the
+master branch. It will schedule multiple child tasks that build
+the app, run tests and execute code quality tools:
+"""
+
+import datetime
+import json
+import taskcluster
+import os
+
+TASK_ID = os.environ.get('TASK_ID')
+REPO_URL = os.environ.get('GITHUB_HEAD_REPO_URL')
+BRANCH = os.environ.get('GITHUB_HEAD_BRANCH')
+COMMIT = os.environ.get('GITHUB_HEAD_SHA')
+OWNER = "skaspari@mozilla.com"
+SOURCE = "https://github.com/mozilla-mobile/focus-android/tree/master/tools/taskcluster"
+
+def generate_build_task():
+	return taskcluster.slugId(), generate_task(
+		name = "(Focus for Android) Build",
+		description = "Build Focus/Klar for Android from source code.",
+		command = 'echo "--" > .adjust_token && ./gradlew clean assemble')
+
+
+def generate_unit_test_task(buildTaskId):
+	return taskcluster.slugId(), generate_task(
+		name = "(Focus for Android) Unit tests",
+		description = "Run unit tests for Focus/Klar for Android.",
+		command = 'echo "--" > .adjust_token && ./gradlew clean test',
+		dependencies = [ buildTaskId ])
+
+
+def generate_code_quality_task(buildTaskId):
+	return taskcluster.slugId(), generate_task(
+		name = "(Focus for Android) Code quality",
+		description = "Run code quality tools on Focus/Klar for Android code base.",
+		command = 'echo "--" > .adjust_token && ./gradlew clean detektCheck ktlint lint pmd checkstyle findbugs',
+		dependencies = [ buildTaskId ])
+
+
+def generate_ui_test_task(dependencies):
+	return taskcluster.slugId(), generate_task(
+		name = "(Focus for Android) UI tests",
+		description = "Run UI tests for Focus/Klar for Android.",
+		command = 'echo "--" > .adjust_token && ./gradlew clean',
+		dependencies = dependencies)
+
+
+def generate_release_task(uiTestTaskId):
+	return taskcluster.slugId(), generate_task(
+		name = "(Focus for Android) Preview release",
+		description = "Build preview versions for testing Focus/Klar for Android.",
+		command = ('echo "--" > .adjust_token'
+			       ' && ./gradlew clean assembleBeta'
+			       ' && python tools/taskcluster/sign-preview-builds.py'),
+		dependencies = [ uiTestTaskId ],
+		scopes = [ "secrets:get:project/focus/preview-key-store" ],
+		artifacts = {
+			"public": {
+				"type": "directory",
+				"path": "/opt/focus-android/builds",
+				"expires": taskcluster.stringDate(taskcluster.fromNow('1 week'))
+			}
+		})
+
+
+def generate_task(name, description, command, dependencies = [], artifacts = {}, scopes = []):
+	created = datetime.datetime.now()
+	expires = taskcluster.fromNow('1 week')
+	deadline = taskcluster.fromNow('1 day')
+
+	return {
+	    "workerType": "github-worker",
+	    "taskGroupId": TASK_ID,
+	    "expires": taskcluster.stringDate(expires),
+	    "retries": 5,
+	    "created": taskcluster.stringDate(created),
+	    "tags": {},
+	    "priority": "lowest",
+	    "schedulerId": "taskcluster-github",
+	    "deadline": taskcluster.stringDate(deadline),
+	    "dependencies": [ TASK_ID ] + dependencies,
+	    "routes": [],
+	    "scopes": scopes,
+	    "requires": "all-completed",
+	    "payload": {
+	        "features": {
+	            "taskclusterProxy": True
+	        },
+	        "maxRunTime": 7200,
+	        "image": "mozillamobile/focus-android",
+	        "command": [
+	            "/bin/bash",
+	            "--login",
+	            "-c",
+				"git fetch %s %s && git config advice.detachedHead false && git checkout %s && %s" % (REPO_URL, BRANCH, COMMIT, command)
+	        ],
+	        "artifacts": artifacts,
+	        "deadline": taskcluster.stringDate(deadline)
+	    },
+		"provisionerId": "aws-provisioner-v1",
+		"metadata": {
+			"name": name,
+			"description": description,
+			"owner": OWNER,
+			"source": SOURCE
+		}
+	}
+
+
+def schedule_task(queue, taskId, task):
+	print "TASK", taskId
+	print json.dumps(task, indent=4, separators=(',', ': '))
+
+	result = queue.createTask(taskId, task)
+	print json.dumps(result)
+
+
+if __name__ == "__main__":
+	queue = taskcluster.Queue({ 'baseUrl': 'http://taskcluster/queue/v1' })
+
+	buildTaskId, buildTask = generate_build_task()
+	schedule_task(queue, buildTaskId, buildTask)
+
+	unitTestTaskId, unitTestTask = generate_unit_test_task(buildTaskId)
+	schedule_task(queue, unitTestTaskId, unitTestTask)
+
+	codeQualityTaskId, codeQualityTask = generate_code_quality_task(buildTaskId)
+	schedule_task(queue, codeQualityTaskId, codeQualityTask)
+
+	uiTestTaskId, uiTestTask = generate_ui_test_task([unitTestTaskId, codeQualityTaskId])
+	schedule_task(queue, uiTestTaskId, uiTestTask)
+
+	releaseTaskId, releaseTask = generate_release_task(uiTestTaskId)
+	schedule_task(queue, releaseTaskId, releaseTask)
+
