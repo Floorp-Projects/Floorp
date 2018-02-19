eb78122ad14f9b4c96fbfb80b81c9ac9283a36af
Ben Kelly <ben@wanderview.com>
2018-02-19 12:06:19 -0800

    Bug 1439099 P3 Remove ServiceWorker::MatchesDescriptor() in favor of Descriptor().Matches(). r=asuth

--- dom/serviceworkers/ServiceWorker.cpp
+++ dom/serviceworkers/ServiceWorker.cpp
@@ -141,15 +141,11 @@ ServiceWorker::PostMessage(JSContext* aCx, JS::Handle<JS::Value> aMessage,
   mInner->PostMessage(GetParentObject(), aCx, aMessage, aTransferable, aRv);
 }
 
-bool
-ServiceWorker::MatchesDescriptor(const ServiceWorkerDescriptor& aDescriptor) const
+
+const ServiceWorkerDescriptor&
+ServiceWorker::Descriptor() const
 {
-  // Compare everything in the descriptor except the state.  That is mutable
-  // and may not exactly match.
-  return mDescriptor.PrincipalInfo() == aDescriptor.PrincipalInfo() &&
-         mDescriptor.Scope() == aDescriptor.Scope() &&
-         mDescriptor.ScriptURL() == aDescriptor.ScriptURL() &&
-         mDescriptor.Id() == aDescriptor.Id();
+  return mDescriptor;
 }
 
 void
