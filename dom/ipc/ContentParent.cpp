36b48bece7cfdb7b2a0f5485eb37069d3b792a42
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2018-02-20 14:36:11 +0000

    Bug 1438886 - Prevent speculative execution after returning from GC-capable C++ code. r=jandem

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2007,8 +2007,8 @@ ContentParent::LaunchSubprocess(ProcessPriority aInitialPriority /* = PROCESS_PR
 
   for (unsigned int i = 0; i < prefsLen; i++) {
     const char* prefName = ContentPrefs::GetEarlyPref(i);
-    MOZ_ASSERT_IF(i > 0,
-                  strcmp(prefName, ContentPrefs::GetEarlyPref(i - 1)) > 0);
+    MOZ_ASSERT(i == 0 || strcmp(prefName, ContentPrefs::GetEarlyPref(i - 1)) > 0,
+               "Content process preferences should be sorted alphabetically.");
 
     if (!Preferences::MustSendToContentProcesses(prefName)) {
       continue;
