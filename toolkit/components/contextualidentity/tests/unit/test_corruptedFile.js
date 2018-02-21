021c6ae6ccf9b2c7c7b73b73472999dbc3011e26
Luca Greco <lgreco@mozilla.com>
2018-02-21 15:23:45 +0100

    Bug 1406181 - Add identity reserved for storage.local extension API to ContextualIdentityService. r=baku
    MozReview-Commit-ID: APi7yJnuDe2

--- toolkit/components/contextualidentity/tests/unit/test_corruptedFile.js
+++ toolkit/components/contextualidentity/tests/unit/test_corruptedFile.js
@@ -19,10 +19,9 @@ const COOKIE = {
   isHttpOnly: false,
   isSession: true,
   expiry: 2145934800,
-  originAttributes: { userContextId: 1 },
 };
 
-function createCookie() {
+function createCookie(userContextId) {
   Services.cookies.add(COOKIE.host,
                        COOKIE.path,
                        COOKIE.name,
@@ -31,15 +30,15 @@ function createCookie() {
                        COOKIE.isHttpOnly,
                        COOKIE.isSession,
                        COOKIE.expiry,
-                       COOKIE.originAttributes);
+                       {userContextId});
 }
 
-function hasCookie() {
+function hasCookie(userContextId) {
   let found = false;
-  let enumerator = Services.cookies.getCookiesFromHost(BASE_URL, COOKIE.originAttributes);
+  let enumerator = Services.cookies.getCookiesFromHost(BASE_URL, {userContextId});
   while (enumerator.hasMoreElements()) {
     let cookie = enumerator.getNext().QueryInterface(Ci.nsICookie);
-    if (cookie.originAttributes.userContextId == COOKIE.originAttributes.userContextId) {
+    if (cookie.originAttributes.userContextId == userContextId) {
       found = true;
       break;
     }
@@ -49,8 +48,28 @@ function hasCookie() {
 
 // Correpted file should delete all.
 add_task(async function corruptedFile() {
-  createCookie();
-  ok(hasCookie(), "We have the new cookie!");
+  const thumbnailPrivateId = ContextualIdentityService._defaultIdentities.filter(
+    identity => identity.name === "userContextIdInternal.thumbnail").pop().userContextId;
+
+  const webextStoragePrivateId = ContextualIdentityService._defaultIdentities.filter(
+    identity => identity.name === "userContextIdInternal.webextStorageLocal").pop().userContextId;
+
+  // Create a cookie in the default Firefox identity (userContextId 0).
+  createCookie(0);
+
+  // Create a cookie in the userContextId 1.
+  createCookie(1);
+
+  // Create a cookie in the thumbnail private userContextId.
+  createCookie(thumbnailPrivateId);
+
+  // Create a cookie in the extension storage private userContextId.
+  createCookie(webextStoragePrivateId);
+
+  ok(hasCookie(0), "We have the new cookie the default firefox identity!");
+  ok(hasCookie(1), "We have the new cookie in a public identity!");
+  ok(hasCookie(thumbnailPrivateId), "We have the new cookie in the thumbnail private identity!");
+  ok(hasCookie(webextStoragePrivateId), "We have the new cookie in the extension storage private identity!");
 
   // Let's create a corrupted file.
   await OS.File.writeAtomic(TEST_STORE_FILE_PATH, "{ vers",
@@ -59,8 +78,38 @@ add_task(async function corruptedFile() {
   let cis = ContextualIdentityService.createNewInstanceForTesting(TEST_STORE_FILE_PATH);
   ok(!!cis, "We have our instance of ContextualIdentityService");
 
-  equal(cis.getPublicIdentities().length, 4, "We should have the default identities");
+  equal(cis.getPublicIdentities().length, 4, "We should have the default public identities");
+
+  // Verify that when the containers.json file is being rebuilt, the computed lastUserContextId
+  // is the expected one.
+  equal(cis._lastUserContextId, thumbnailPrivateId,
+         "Expect cis._lastUserContextId to be equal to the thumbnails userContextId");
+
+  const privThumbnailIdentity = cis.getPrivateIdentity("userContextIdInternal.thumbnail");
+  equal(privThumbnailIdentity && privThumbnailIdentity.userContextId, thumbnailPrivateId,
+        "We should have the default thumbnail private identity");
+
+  const privWebextStorageIdentity = cis.getPrivateIdentity("userContextIdInternal.webextStorageLocal");
+  equal(privWebextStorageIdentity && privWebextStorageIdentity.userContextId,
+        webextStoragePrivateId,
+        "We should have the default extensions storage.local private identity");
 
   // Cookie is gone!
-  ok(!hasCookie(), "We should not have the new cookie!");
+  ok(!hasCookie(1), "We should not have the new cookie in the userContextId 1!");
+
+  // The data stored in the Firefox default userContextId (0), should have not be cleared.
+  ok(hasCookie(0), "We should not have the new cookie in the default Firefox identity!");
+
+  // The data stored in the non-public userContextId (e.g. thumbnails private identity)
+  // should have not be cleared.
+  ok(hasCookie(thumbnailPrivateId),
+     "We should have the new cookie in the thumbnail private userContextId!");
+  ok(hasCookie(webextStoragePrivateId),
+     "We should have the new cookie in the extension storage private userContextId!");
+
+  // Verify the version of the newly created containers.json file.
+  cis.save();
+  const stateFileText = await OS.File.read(TEST_STORE_FILE_PATH, {encoding: "utf-8"});
+  equal(JSON.parse(stateFileText).version, cis.LAST_CONTAINERS_JSON_VERSION,
+        "Expect the new containers.json file to have the expected version");
 });
