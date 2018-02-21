49ccd43f99eba64bc42384afc4311d91bc8d8c5b
Jared Wein <jwein@mozilla.com>
2018-02-21 14:16:35 -0800

    Bug 1429189 - Show shipping address errors on the summary screen. r=mattn
    MozReview-Commit-ID: LaXrvWliWna

---
+++ toolkit/components/payments/test/browser/browser_shippingaddresschange_error.js
@@ -0,0 +1,93 @@
+"use strict";
+
+add_task(addSampleAddressesAndBasicCard);
+
+add_task(async function test_show_error_on_addresschange() {
+  await BrowserTestUtils.withNewTab({
+    gBrowser,
+    url: BLANK_PAGE_URL,
+  }, async browser => {
+    let dialogReadyPromise = waitForWidgetReady();
+    // start by creating a PaymentRequest, and show it
+    await ContentTask.spawn(browser,
+                            {
+                              methodData: [PTU.MethodData.basicCard],
+                              details: PTU.Details.twoShippingOptions,
+                              options: PTU.Options.requestShippingOption,
+                            },
+                            PTU.ContentTasks.createAndShowRequest);
+
+    // get a reference to the UI dialog and the requestId
+    let [win] = await Promise.all([getPaymentWidget(), dialogReadyPromise]);
+    ok(win, "Got payment widget");
+    let requestId = paymentUISrv.requestIdForWindow(win);
+    ok(requestId, "requestId should be defined");
+    is(win.closed, false, "dialog should not be closed");
+
+    let frame = await getPaymentFrame(win);
+    ok(frame, "Got payment frame");
+
+    info("setting up the event handler for shippingoptionchange");
+    let EXPECTED_ERROR_TEXT = "Cannot ship with option 1 on days that end with Y";
+    await ContentTask.spawn(browser, {
+      eventName: "shippingoptionchange",
+      details: {
+        error: EXPECTED_ERROR_TEXT,
+        shippingOptions: [],
+        total: {
+          label: "Grand total is!!!!!: ",
+          amount: {
+            value: "12",
+            currency: "USD",
+          },
+        },
+      },
+    }, PTU.ContentTasks.updateWith);
+    await spawnPaymentDialogTask(frame,
+                                 PTU.DialogContentTasks.selectShippingOptionById,
+                                 "1");
+    info("awaiting the shippingoptionchange event");
+    await ContentTask.spawn(browser, {
+      eventName: "shippingoptionchange",
+    }, PTU.ContentTasks.awaitPaymentRequestEventPromise);
+
+    let errors = await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.getErrorDetails);
+    is(errors.text, EXPECTED_ERROR_TEXT, "Error text should be present on dialog");
+    let errorsVisible =
+      await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.isElementVisible, "#error-text");
+    ok(errorsVisible, "Error text should be visible");
+
+    info("setting up the event handler for shippingaddresschange");
+    await ContentTask.spawn(browser, {
+      eventName: "shippingaddresschange",
+      details: {
+        error: "",
+        shippingOptions: PTU.Details.twoShippingOptions.shippingOptions,
+        total: {
+          label: "Grand total is now: ",
+          amount: {
+            value: "24",
+            currency: "USD",
+          },
+        },
+      },
+    }, PTU.ContentTasks.updateWith);
+    await spawnPaymentDialogTask(frame,
+                                 PTU.DialogContentTasks.selectShippingAddressByCountry,
+                                 "DE");
+    info("awaiting the shippingaddresschange event");
+    await ContentTask.spawn(browser, {
+      eventName: "shippingaddresschange",
+    }, PTU.ContentTasks.awaitPaymentRequestEventPromise);
+    errors = await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.getErrorDetails);
+    is(errors.text, "", "Error text should not be present on dialog");
+    errorsVisible =
+      await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.isElementVisible, "#error-text");
+    ok(!errorsVisible, "Error text should not be visible");
+
+    info("clicking cancel");
+    spawnPaymentDialogTask(frame, PTU.DialogContentTasks.manuallyClickCancel);
+
+    await BrowserTestUtils.waitForCondition(() => win.closed, "dialog should be closed");
+  });
+});
