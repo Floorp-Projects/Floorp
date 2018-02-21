4fb607daf78bc83549af6155ed7ea946fb21d9d9
Cosmin Sabou <csabou@mozilla.com>
2018-02-21 10:12:45 +0200

    Backed out changeset 1fcc972d445b (bug 744965) for assertion failures on TestFloatingPoint.cpp on a CLOSED TREE

--- mfbt/MathAlgorithms.h
+++ mfbt/MathAlgorithms.h
@@ -116,10 +116,10 @@ template<> struct AbsReturnType<long double> { typedef long double Type; };
 } // namespace detail
 
 template<typename T>
-inline constexpr typename detail::AbsReturnType<T>::Type
+inline typename detail::AbsReturnType<T>::Type
 Abs(const T aValue)
 {
-  using ReturnType = typename detail::AbsReturnType<T>::Type;
+  typedef typename detail::AbsReturnType<T>::Type ReturnType;
   return aValue >= 0 ? ReturnType(aValue) : ~ReturnType(aValue) + 1;
 }
 
