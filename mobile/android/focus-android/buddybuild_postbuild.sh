082c09f9765a18a248c76e1a28bcc5890e5011d1
Sebastian Kaspari <s.kaspari@gmail.com>
2018-02-22 10:23:32 +0100

    [focus] Remove buddybuild pre/post build scripts. (https://github.com/mozilla-mobile/focus-android/issues/2032)

--- mobile/android/focus-android/buddybuild_postbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # Exit (and fail) immediately if any command in this scriptfails
-
-# Check that our APK files are not bigger than they should be
-python tools/metrics/apk_size.py
-
