6203a68e22231ecdcdfc064efa8f27e1c5bc6563
Nick Thomas <nthomas@mozilla.com>
2018-02-23 11:27:22 +1300

    Bug 1435638 - initialize repo using an ssh clone, r=jlund DONTBUILD

--- testing/mozharness/configs/partner_repacks/staging_release_mozilla-release_desktop.py
+++ testing/mozharness/configs/partner_repacks/staging_release_mozilla-release_desktop.py
@@ -3,4 +3,5 @@ config = {
     "log_name": "partner_repack",
     "repack_manifests_url": "git@github.com:mozilla-partners/repack-manifests.git",
     "repo_file": "https://raw.githubusercontent.com/mozilla/git-repo/master/repo",
+    "repo_url": "git@github.com:mozilla/git-repo.git",
 }
