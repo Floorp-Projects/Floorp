3a0c426d41eec1af8a777fe143cfaf8f8850b30a
Mike Shal <mshal@mozilla.com>
2018-02-22 13:31:10 -0500

    Bug 1319230 - Add a Taskcluster job to build with the tup backend; r=nalexander
    MozReview-Commit-ID: H3ZmKxXk2NP
    
    --HG--
    extra : rebase_source : 33924dff24ff5f38832c2e600a85a8b0a9a23bf6

---
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_tup.py
@@ -0,0 +1,35 @@
+import os
+
+config = {
+    'default_actions': [
+        'clobber',
+        'build',
+        'update',  # decided by query_is_nightly()
+    ],
+    'stage_platform': 'linux64-tup-opt',
+    'enable_signing': False,
+    'enable_talos_sendchange': False,
+    'env': {
+        'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
+        'DISPLAY': ':2',
+        'HG_SHARE_BASE_DIR': '/builds/hg-shared',
+        'MOZ_OBJDIR': '%(abs_obj_dir)s',
+        'MOZ_CRASHREPORTER_NO_REPORT': '1',
+        'LC_ALL': 'C',
+        'XPCOM_DEBUG_BREAK': 'stack-and-abort',
+        # 64 bit specific
+        'PATH': '/usr/local/bin:/usr/lib64/ccache:/bin:\
+/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:/tools/python27/bin:\
+/tools/python27-mercurial/bin:/home/cltbld/bin',
+        'LD_LIBRARY_PATH': '/tools/gcc-4.3.3/installed/lib64:%(abs_obj_dir)s/dist/bin',
+        'TINDERBOX_OUTPUT': '1',
+
+        # sccache doesn't work yet with tup due to the way the server is
+        # spawned, and because the server does the file I/O
+        'SCCACHE_DISABLE': '1',
+    },
+    'mozconfig_variant': 'tup',
+    'enable_count_ctors': False, # TODO: libxul.so needs to be linked for this to work
+    'disable_package_metrics': True, # TODO: the package needs to be created for this to work
+    'artifact_flag_build_variant_in_try': None,
+}
