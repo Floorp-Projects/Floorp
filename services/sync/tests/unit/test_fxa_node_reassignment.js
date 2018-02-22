f4a8c18f91bfad136689e78aef58016708614552
Edouard Oger <eoger@fastmail.com>
2018-02-22 16:30:39 +0800

    Bug 1439777 p2 - Remove weave:ui:* related code. r=markh,tcsc
    MozReview-Commit-ID: D1H36YeiJCS

--- services/sync/tests/unit/test_fxa_node_reassignment.js
+++ services/sync/tests/unit/test_fxa_node_reassignment.js
@@ -25,13 +25,6 @@ add_task(async function setup() {
 
   // Setup the FxA identity manager and cluster manager.
   Status.__authManager = Service.identity = new BrowserIDManager();
-
-  // None of the failures in this file should result in a UI error.
-  function onUIError() {
-    do_throw("Errors should not be presented in the UI.");
-  }
-  Svc.Obs.add("weave:ui:login:error", onUIError);
-  Svc.Obs.add("weave:ui:sync:error", onUIError);
 });
 
 
