fb14229cd3e5b953c84430791aaae5e7eef744aa
Dhi Aurrahman <diorahman@rockybars.com>
2018-02-23 09:01:08 +0700

    Bug 1427766 - Add a c++ test for GetHistogramName. r=chutten

--- toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
+++ toolkit/components/telemetry/tests/gtest/TestHistograms.cpp
@@ -18,6 +18,12 @@ TEST_F(TelemetryTestFixture, AccumulateCountHistogram)
   const uint32_t kExpectedValue = 100;
   AutoJSContextWithGlobal cx(mCleanGlobal);
 
+  const char* telemetryTestCountName = Telemetry::GetHistogramName(Telemetry::TELEMETRY_TEST_COUNT);
+  ASSERT_STREQ(telemetryTestCountName, "TELEMETRY_TEST_COUNT") << "The histogram name is wrong";
+
+  const char* invalidTelemetryName = Telemetry::GetHistogramName(Telemetry::HistogramCount);
+  ASSERT_EQ(invalidTelemetryName, nullptr) << "The histogram name is not returning invalid name";
+
   GetAndClearHistogram(cx.GetJSContext(), mTelemetry, NS_LITERAL_CSTRING("TELEMETRY_TEST_COUNT"),
                        false);
 
@@ -26,11 +32,11 @@ TEST_F(TelemetryTestFixture, AccumulateCountHistogram)
 
   // Get a snapshot for all the histograms
   JS::RootedValue snapshot(cx.GetJSContext());
-  GetSnapshots(cx.GetJSContext(), mTelemetry, "TELEMETRY_TEST_COUNT", &snapshot, false);
+  GetSnapshots(cx.GetJSContext(), mTelemetry, telemetryTestCountName, &snapshot, false);
 
   // Get the histogram from the snapshot
   JS::RootedValue histogram(cx.GetJSContext());
-  GetProperty(cx.GetJSContext(), "TELEMETRY_TEST_COUNT", snapshot, &histogram);
+  GetProperty(cx.GetJSContext(), telemetryTestCountName, snapshot, &histogram);
 
   // Get "sum" property from histogram
   JS::RootedValue sum(cx.GetJSContext());
