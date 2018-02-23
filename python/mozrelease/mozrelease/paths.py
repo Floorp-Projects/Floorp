d6163c106f202a93de817d7c3ed27ba42d28ccad
Ben Hearsum <bhearsum@mozilla.com>
2018-02-23 06:00:02 -0500

    bug 1398799: mozharness script to create update verify configs without relying on patcher configs. r=nthomas

---
+++ python/mozrelease/mozrelease/paths.py
@@ -0,0 +1,63 @@
+from __future__ import absolute_import
+
+from urlparse import urlunsplit
+
+product_ftp_map = {
+    "fennec": "mobile",
+}
+
+def product2ftp(product):
+    return product_ftp_map.get(product, product)
+
+
+def getCandidatesDir(product, version, buildNumber, protocol=None, server=None):
+    if protocol:
+        assert server is not None, "server is required with protocol"
+
+    product = product2ftp(product)
+    directory = "/{}/candidates/{}-candidates/build{}".format(
+        product, str(version), str(buildNumber)
+    )
+
+    if protocol:
+        return urlunsplit((protocol, server, directory, None, None))
+    else:
+        return directory
+
+
+def getReleasesDir(product, version=None, protocol=None, server=None):
+    if protocol:
+        assert server is not None, "server is required with protocol"
+
+    directory = "/{}/releases".format(product)
+    if version:
+        directory = "{}/{}".format(directory, version)
+
+    if protocol:
+        return urlunsplit((protocol, server, directory, None, None))
+    else:
+        return directory
+
+
+def getReleaseInstallerPath(productName, brandName, version, platform,
+                            locale='en-US'):
+    if productName not in ('fennec',):
+        if platform.startswith('linux'):
+            return '/'.join([p.strip('/') for p in [
+                platform, locale, '%s-%s.tar.bz2' % (productName, version)]])
+        elif 'mac' in platform:
+            return '/'.join([p.strip('/') for p in [
+                platform, locale, '%s %s.dmg' % (brandName, version)]])
+        elif platform.startswith('win'):
+            return '/'.join([p.strip('/') for p in [
+                platform, locale, '%s Setup %s.exe' % (brandName, version)]])
+        else:
+            raise "Unsupported platform"
+    else:
+        if platform.startswith('android'):
+            filename = '%s-%s.%s.android-arm.apk' % (
+                productName, version, locale)
+            return '/'.join([p.strip('/') for p in [
+                platform, locale, filename]])
+        else:
+            raise "Unsupported platform"
---
+++ python/mozrelease/mozrelease/platforms.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+
+# ftp -> update platform map
+update_platform_map = {
+    "android": ["Android_arm-eabi-gcc3"],
+    "android-api-11": ["Android_arm-eabi-gcc3"],
+    "android-api-15": ["Android_arm-eabi-gcc3"],
+    "android-api-15-old-id": ["Android_arm-eabi-gcc3"],
+    "android-api-16": ["Android_arm-eabi-gcc3"],
+    "android-api-16-old-id": ["Android_arm-eabi-gcc3"],
+    "android-x86": ["Android_x86-gcc3"],
+    "android-x86-old-id": ["Android_x86-gcc3"],
+    "android-aarch64": ["Android_aarch64-gcc3"],
+    "linux-i686": ["Linux_x86-gcc3"],
+    "linux-x86_64": ["Linux_x86_64-gcc3"],
+    "mac": ["Darwin_x86_64-gcc3-u-i386-x86_64",  # The main platofrm
+            "Darwin_x86-gcc3-u-i386-x86_64",
+            # We don"t ship builds with these build targets, but some users
+            # modify their builds in a way that has them report like these.
+            # See bug 1071576 for details.
+            "Darwin_x86-gcc3", "Darwin_x86_64-gcc3"],
+    "win32": ["WINNT_x86-msvc", "WINNT_x86-msvc-x86", "WINNT_x86-msvc-x64"],
+    "win64": ["WINNT_x86_64-msvc", "WINNT_x86_64-msvc-x64"],
+}
+
+# ftp -> shipped locales map
+sl_platform_map = {
+    "linux-i686": "linux",
+    "linux-x86_64": "linux",
+    "mac": "osx",
+    "win32": "win32",
+    "win64": "win64",
+}
+
+# ftp -> info file platform map
+info_file_platform_map = {
+    "linux-i686": "linux",
+    "linux-x86_64": "linux64",
+    "mac": "macosx64",
+    "win32": "win32",
+    "win64": "win64",
+}
+
+def ftp2updatePlatforms(platform):
+    return update_platform_map.get(platform, platform)
+
+def ftp2shippedLocales(platform):
+    return sl_platform_map.get(platform, platform)
+
+def shippedLocales2ftp(platform):
+    matches = []
+    try:
+        [matches.append(
+            k) for k, v in sl_platform_map.iteritems() if v == platform][0]
+        return matches
+    except IndexError:
+        return [platform]
+
+def ftp2infoFile(platform):
+    return info_file_platform_map.get(platform, platform)
