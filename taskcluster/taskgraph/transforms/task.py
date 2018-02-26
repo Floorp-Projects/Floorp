03c8a9b27c44d92eb62d2bad01a5419b47b95fba
Johan Lorenzo <jlorenzo@mozilla.com>
2018-02-26 15:21:54 +0100

    Bug 1433459 - part 2: Move aliases tasks to scriptworker r=mtabara,rail
    MozReview-Commit-ID: Ld6ZQPBZYjX
    
    --HG--
    extra : rebase_source : 83ce7b0d783bf90b362711958f5963c46f5bedf2

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -559,6 +559,9 @@ task_description_schema = Schema({
             Required('paths'): [basestring],
         }],
     }, {
+        Required('implementation'): 'bouncer-aliases',
+        Required('entries'): object,
+    }, {
         Required('implementation'): 'bouncer-submission',
         Required('locales'): [basestring],
         Required('entries'): object,
@@ -1113,6 +1116,15 @@ def build_balrog_payload(config, task, task_def):
                 'publish_rules': worker['publish-rules'],
                 'release_eta': config.params.get('release_eta') or '',
             })
+
+
+@payload_builder('bouncer-aliases')
+def build_bouncer_aliases_payload(config, task, task_def):
+    worker = task['worker']
+
+    task_def['payload'] = {
+        'aliases_entries': worker['entries']
+    }
 
 
 @payload_builder('bouncer-submission')
