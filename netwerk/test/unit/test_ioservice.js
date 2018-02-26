ca5c5bbf19d5f000e67aee71d5417121019aa997
Valentin Gosu <valentin.gosu@gmail.com>
2018-02-26 16:42:27 +0100

    Bug 1439931 - nsIIOService.extractScheme/net_ExtractURLScheme should lowercase its output r=mayhemer
    MozReview-Commit-ID: LZ2rqIdIoXQ

---
+++ netwerk/test/unit/test_ioservice.js
@@ -0,0 +1,11 @@
+"use strict";
+
+ChromeUtils.import('resource://gre/modules/Services.jsm');
+
+add_task(function test_extractScheme(){
+  equal(Services.io.extractScheme("HtTp://example.com"), "http");
+  Assert.throws(() => { Services.io.extractScheme("://example.com"); },
+                /NS_ERROR_MALFORMED_URI/, "missing scheme");
+  Assert.throws(() => { Services.io.extractScheme("ht%tp://example.com"); },
+                /NS_ERROR_MALFORMED_URI/, "bad scheme");
+});
