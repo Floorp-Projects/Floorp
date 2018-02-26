f77fba2c4c969ec35b5596c8405e072f28cd312f
Valentin Gosu <valentin.gosu@gmail.com>
2018-02-26 05:24:09 +0100

    Bug 1434766 - Make nsIFileURL attributes readonly r=mayhemer
    MozReview-Commit-ID: AN5EzyuuKuJ
    
    --HG--
    extra : rebase_source : 2d986fa39930469472f24a208d9b06650ddb78f4

--- netwerk/protocol/file/nsFileProtocolHandler.cpp
+++ netwerk/protocol/file/nsFileProtocolHandler.cpp
@@ -242,18 +242,12 @@ NS_IMETHODIMP
 nsFileProtocolHandler::NewFileURI(nsIFile *file, nsIURI **result)
 {
     NS_ENSURE_ARG_POINTER(file);
-    nsresult rv;
-
-    nsCOMPtr<nsIFileURL> url = new nsStandardURL(true);
-    if (!url)
-        return NS_ERROR_OUT_OF_MEMORY;
 
     // NOTE: the origin charset is assigned the value of the platform
     // charset by the SetFile method.
-    rv = url->SetFile(file);
-    if (NS_FAILED(rv)) return rv;
-
-    return CallQueryInterface(url, result);
+    return NS_MutateURI(new nsStandardURL::Mutator())
+             .Apply(NS_MutatorMethod(&nsIFileURLMutator::SetFile, file))
+             .Finalize(result);
 }
 
 NS_IMETHODIMP
