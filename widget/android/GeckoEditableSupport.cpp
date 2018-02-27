bb10e7fbe9772827d1236bed3243ac110f96f6cf
Masayuki Nakano <masayuki@d-toybox.com>
2018-02-27 17:24:35 +0900

    Bug 1343451 - part 5: Make GeckoEditableSupport dispatch dummy eKeyDown and eKeyUp event during composition always r=jchen
    On Android, GeckoEditableSupport has already dispatched eKeyDown event and
    eKeyUp event even during composition.  I.e., the pref which will be enabled
    by bug 354358 has already been set to true only on Android.
    
    On the other hand, GeckoEditableSupport does not dispatch them if content
    listens to "input", "compositionstart", "compositionupdate" or
    "compositionend".  So, different from the other platforms, we cannot test
    this behind pref ("dom.keyboardevent.dispatch_during_composition") even in
    Nightly.
    
    Therefore, this patch enables new behavior only when it's Nightly build or
    early Beta.  And sets mKeyCode and mKeyNameIndex of the dummy KeyboardEvents
    to NS_VK_PROCESSKEY and KEY_NAME_INDEX_Process.
    
    MozReview-Commit-ID: Fuy0Ir2xiO5
    
    --HG--
    extra : rebase_source : fade31954eaa1be8b7592977095ba8aebdd75104

--- widget/android/GeckoEditableSupport.cpp
+++ widget/android/GeckoEditableSupport.cpp
@@ -668,7 +668,6 @@ GeckoEditableSupport::OnKeyEvent(int32_t aAction, int32_t aKeyCode,
 /*
  * Send dummy key events for pages that are unaware of input events,
  * to provide web compatibility for pages that depend on key events.
- * Our dummy key events have 0 as the keycode.
  */
 void
 GeckoEditableSupport::SendIMEDummyKeyEvent(nsIWidget* aWidget, EventMessage msg)
@@ -678,7 +677,12 @@ GeckoEditableSupport::SendIMEDummyKeyEvent(nsIWidget* aWidget, EventMessage msg)
 
     WidgetKeyboardEvent event(true, msg, aWidget);
     event.mTime = PR_Now() / 1000;
-    MOZ_ASSERT(event.mKeyCode == 0);
+    // TODO: If we can know scan code of the key event which caused replacing
+    //       composition string, we should set mCodeNameIndex here.  Then,
+    //       we should rename this method because it becomes not a "dummy"
+    //       keyboard event.
+    event.mKeyCode = NS_VK_PROCESSKEY;
+    event.mKeyNameIndex = KEY_NAME_INDEX_Process;
     NS_ENSURE_SUCCESS_VOID(BeginInputTransaction(mDispatcher));
     mDispatcher->DispatchKeyboardEvent(msg, event, status);
 }
@@ -1027,12 +1031,19 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         AddIMETextChange(dummyChange);
     }
 
+    // Until fixing bug 354358 in release channel, we should always ignore
+    // "intl.ime.hack.on_ime_unaware_apps.fire_key_events_for_composition"
+    // pref in Nightly and early Beta for testing new regressions.
+#ifndef EARLY_BETA_OR_EARLIER
     if (mInputContext.mMayBeIMEUnaware) {
+#endif // #ifndef EARLY_BETA_OR_EARLIER
         SendIMEDummyKeyEvent(widget, eKeyDown);
         if (!mDispatcher || widget->Destroyed()) {
             return;
         }
+#ifndef EARLY_BETA_OR_EARLIER
     }
+#endif // #ifndef EARLY_BETA_OR_EARLIER
 
     if (composing) {
         mDispatcher->SetPendingComposition(string, mIMERanges);
@@ -1046,10 +1057,18 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         return;
     }
 
+    // XXX This works fine if user inputs with virtual keyboard.  However,
+    //     if user inputs with hardware keyboard, keyup event may come later.
+    //     So, perhaps, we need to record actual keyboard events before
+    //     this replacement of composition string.
+#ifndef EARLY_BETA_OR_EARLIER
     if (mInputContext.mMayBeIMEUnaware) {
+#endif // #ifndef EARLY_BETA_OR_EARLIER
         SendIMEDummyKeyEvent(widget, eKeyUp);
         // Widget may be destroyed after dispatching the above event.
+#ifndef EARLY_BETA_OR_EARLIER
     }
+#endif // #ifndef EARLY_BETA_OR_EARLIER
 }
 
 void
