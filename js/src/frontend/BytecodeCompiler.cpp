d5bde802f3dbc14e611625b16535021393ec11b5
Jason Orendorff <jorendorff@mozilla.com>
2018-02-27 15:09:09 -0600

    Bug 1275292 - Delete ParseNodeAllocator::freeTree() and friends. r=shu.

--- js/src/frontend/BytecodeCompiler.cpp
+++ js/src/frontend/BytecodeCompiler.cpp
@@ -343,7 +343,6 @@ BytecodeCompiler::compileScript(HandleObject environment, SharedContext* sc)
                 return nullptr;
             if (!NameFunctions(cx, pn))
                 return nullptr;
-            parser->handler.freeTree(pn);
 
             break;
         }
@@ -416,8 +415,6 @@ BytecodeCompiler::compileModule()
 
     if (!NameFunctions(cx, pn))
         return nullptr;
-
-    parser->handler.freeTree(pn);
 
     if (!builder.initModule())
         return nullptr;
