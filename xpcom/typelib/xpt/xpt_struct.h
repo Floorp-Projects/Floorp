085228e6d3368cbe55ac0807dc4b246812750ed5
Andrew McCreight <continuation@gmail.com>
2018-02-27 13:36:34 -0800

    Bug 1441677, part 6 - Get rid of macros for nsXPTParamInfo flags. r=njn
    Also, get rid of a gratuitous use of a trinary operator in
    nsXPCWrappedJSClass::CallMethod, clean up the style a little, and mark
    an unimplemented ctor as deleted.
    
    MozReview-Commit-ID: Kp64sMxyRWc

--- xpcom/typelib/xpt/xpt_struct.h
+++ xpcom/typelib/xpt/xpt_struct.h
@@ -261,21 +261,6 @@ struct XPTParamDescriptor {
   XPTTypeDescriptor type;
 };
 
-/* flag bits */
-#define XPT_PD_IN       0x80
-#define XPT_PD_OUT      0x40
-#define XPT_PD_RETVAL   0x20
-#define XPT_PD_SHARED   0x10
-#define XPT_PD_DIPPER   0x08
-#define XPT_PD_OPTIONAL 0x04
-
-#define XPT_PD_IS_IN(flags) (flags & XPT_PD_IN)
-#define XPT_PD_IS_OUT(flags) (flags & XPT_PD_OUT)
-#define XPT_PD_IS_RETVAL(flags) (flags & XPT_PD_RETVAL)
-#define XPT_PD_IS_SHARED(flags) (flags & XPT_PD_SHARED)
-#define XPT_PD_IS_DIPPER(flags) (flags & XPT_PD_DIPPER)
-#define XPT_PD_IS_OPTIONAL(flags) (flags & XPT_PD_OPTIONAL)
-
 /*
  * A MethodDescriptor is a variable-size record used to describe a single
  * interface method.
