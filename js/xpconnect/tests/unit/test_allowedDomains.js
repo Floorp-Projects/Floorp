c546946f8659daa563a3bb3d5ed03459029b9855
Florian Quèze <florian@queze.net>
2018-02-28 18:51:34 +0100

    Bug 1433175 - remove by hands some variations of Cc,Ci,Cu definitions, r=Standard8.

--- js/xpconnect/tests/unit/test_allowedDomains.js
+++ js/xpconnect/tests/unit/test_allowedDomains.js
@@ -1,35 +1,34 @@
 function run_test() {
-  var cu = Components.utils;
-  var sbMaster = cu.Sandbox(["http://www.a.com",
+  var sbMaster = Cu.Sandbox(["http://www.a.com",
                            "http://www.b.com",
                            "http://www.d.com"]);
-  var sbSubset = cu.Sandbox(["http://www.d.com",                           
+  var sbSubset = Cu.Sandbox(["http://www.d.com",
                            "http://www.a.com"]);
 
-  var sbA = cu.Sandbox("http://www.a.com");
-  var sbB = cu.Sandbox("http://www.b.com");
-  var sbC = cu.Sandbox("http://www.c.com");
+  var sbA = Cu.Sandbox("http://www.a.com");
+  var sbB = Cu.Sandbox("http://www.b.com");
+  var sbC = Cu.Sandbox("http://www.c.com");
 
-  sbMaster.objA = cu.evalInSandbox("var obj = {prop1:200}; obj", sbA);
-  sbMaster.objB = cu.evalInSandbox("var obj = {prop1:200}; obj", sbB);
-  sbMaster.objC = cu.evalInSandbox("var obj = {prop1:200}; obj", sbC);
-  sbMaster.objOwn = cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
+  sbMaster.objA = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbA);
+  sbMaster.objB = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbB);
+  sbMaster.objC = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbC);
+  sbMaster.objOwn = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
   
-  sbMaster.objSubset = cu.evalInSandbox("var obj = {prop1:200}; obj", sbSubset);
-  sbA.objMaster = cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
-  sbSubset.objMaster = cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
+  sbMaster.objSubset = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbSubset);
+  sbA.objMaster = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
+  sbSubset.objMaster = Cu.evalInSandbox("var obj = {prop1:200}; obj", sbMaster);
 
   var ret;
-  ret = cu.evalInSandbox("objA.prop1", sbMaster);
+  ret = Cu.evalInSandbox("objA.prop1", sbMaster);
   Assert.equal(ret, 200);
-  ret = cu.evalInSandbox("objB.prop1", sbMaster);
+  ret = Cu.evalInSandbox("objB.prop1", sbMaster);
   Assert.equal(ret, 200);
-  ret = cu.evalInSandbox("objSubset.prop1", sbMaster);
+  ret = Cu.evalInSandbox("objSubset.prop1", sbMaster);
   Assert.equal(ret, 200);
   
   function evalAndCatch(str, sb) {
     try {
-      ret = cu.evalInSandbox(str, sb);
+      ret = Cu.evalInSandbox(str, sb);
       Assert.ok(false, "unexpected pass")
     } catch (e) {    
       Assert.ok(e.message && e.message.includes("Permission denied to access property"));
@@ -41,7 +40,7 @@ function run_test() {
   evalAndCatch("objMaster.prop1", sbSubset);
   
   // Bug 777705:
-  sbMaster.Components = cu.getComponentsForScope(sbMaster);
+  sbMaster.Components = Cu.getComponentsForScope(sbMaster);
   Cu.evalInSandbox("Components.interfaces", sbMaster);
   Assert.ok(true);
 }
