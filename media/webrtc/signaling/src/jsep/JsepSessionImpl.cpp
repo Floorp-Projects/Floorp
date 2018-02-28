39168155e9f7311aa93a2ed0927d1a3de1be497e
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2018-02-27 21:55:01 -0800

    Bug 1363900: re-enable extmap ID remapping error. r=bwc
    MozReview-Commit-ID: 32Hog1LpIEU

--- media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
+++ media/webrtc/signaling/src/jsep/JsepSessionImpl.cpp
@@ -1816,14 +1816,10 @@ JsepSessionImpl::ValidateAnswer(const Sdp& offer, const Sdp& answer)
             }
 
             if (offExt.entry < 4096 && (offExt.entry != ansExt.entry)) {
-              // FIXME we do not return an error here, because Cisco Spark
-              // actually does respond with different extension ID's then we
-              // offer. See bug 1361206 for details.
-              MOZ_MTLOG(ML_WARNING, "[" << mName << "]: Answer changed id for "
-                        "extmap attribute at level " << i << " ("
-                        << offExt.extensionname << ") from " << offExt.entry
-                        << " to " << ansExt.entry << ".");
-              // return NS_ERROR_INVALID_ARG;
+              JSEP_SET_ERROR("Answer changed id for extmap attribute at level "
+                        << i << " (" << offExt.extensionname << ") from "
+                        << offExt.entry << " to " << ansExt.entry << ".");
+               return NS_ERROR_INVALID_ARG;
             }
 
             if (ansExt.entry >= 4096) {
