1819daf82cde4cc27271482d17cbbece1bda46fb
Nicholas Nethercote <nnethercote@mozilla.com>
2018-02-28 16:21:42 +1100

    Bug 1441754 - Shrink CallbackNode by combining mMatchKind and mNext into a tagged pointer. r=glandium
    The following table shows the effect of this change:
    
    >                           old 64-bit  new 64-bit  old 32-bit  new 32-bit
    > sizeof(CallbackNode)      40          32          20          16
    > size when heap allocated  48          32          32          16
    
    This reduces memory usage by about 15--40 KB per process.
    
    MozReview-Commit-ID: 4gXgGI3SiJz

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -756,8 +756,7 @@ public:
     : mDomain(moz_xstrdup(aDomain))
     , mFunc(aFunc)
     , mData(aData)
-    , mMatchKind(aMatchKind)
-    , mNext(nullptr)
+    , mNextAndMatchKind(aMatchKind)
   {
   }
 
@@ -770,10 +769,24 @@ public:
 
   void* Data() const { return mData; }
 
-  Preferences::MatchKind MatchKind() const { return mMatchKind; }
+  Preferences::MatchKind MatchKind() const
+  {
+    return static_cast<Preferences::MatchKind>(mNextAndMatchKind &
+                                               kMatchKindMask);
+  }
 
-  CallbackNode* Next() const { return mNext; }
-  void SetNext(CallbackNode* aNext) { mNext = aNext; }
+  CallbackNode* Next() const
+  {
+    return reinterpret_cast<CallbackNode*>(mNextAndMatchKind & kNextMask);
+  }
+
+  void SetNext(CallbackNode* aNext)
+  {
+    uintptr_t matchKind = mNextAndMatchKind & kMatchKindMask;
+    mNextAndMatchKind = reinterpret_cast<uintptr_t>(aNext);
+    MOZ_ASSERT((mNextAndMatchKind & kMatchKindMask) == 0);
+    mNextAndMatchKind |= matchKind;
+  }
 
   void AddSizeOfIncludingThis(MallocSizeOf aMallocSizeOf, PrefsSizes& aSizes)
   {
@@ -782,6 +795,9 @@ public:
   }
 
 private:
+  static const uintptr_t kMatchKindMask = uintptr_t(0x1);
+  static const uintptr_t kNextMask = ~kMatchKindMask;
+
   UniqueFreePtr<const char> mDomain;
 
   // If someone attempts to remove the node from the callback list while
@@ -789,8 +805,12 @@ private:
   // be removed at the end of NotifyCallbacks().
   PrefChangedFunc mFunc;
   void* mData;
-  Preferences::MatchKind mMatchKind;
-  CallbackNode* mNext;
+
+  // Conceptually this is two fields:
+  // - CallbackNode* mNext;
+  // - Preferences::MatchKind mMatchKind;
+  // They are combined into a tagged pointer to save memory.
+  uintptr_t mNextAndMatchKind;
 };
 
 static PLDHashTable* gHashTable;
