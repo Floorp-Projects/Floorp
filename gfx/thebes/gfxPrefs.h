5b79e8ad4bec536245c4440fa9a75b436a361bcc
Andrew Osmond <aosmond@mozilla.com>
2018-02-28 13:34:52 -0500

    Bug 523950 - Part 3. Add preferences to control animated image decoding behaviour. r=tnikkel
    image.animated.decode-on-demand.threshold-kb is the maximum size in kB
    that the aggregate frames of an animation can use before it starts to
    discard already displayed frames, and redecode them as necessary. The
    lower it is set to, the less overall memory we will consume at the
    expense of execution time for as long as the tab with the animation(s)
    above the threshold are kept open.
    
    image.animated.decode-on-demand.batch-size is the minimum number of
    frames we want to have buffered ahead of an animation's currently
    displayed frame. The decoding will request this number of frames at a
    time to maximize use of memory caching. Note that this is related to the
    above preference as well; increasing the batch size will in effect raise
    what the minimum threshold. This simplifies the logic in patches later
    in the series.

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -521,6 +521,8 @@ private:
   DECL_GFX_PREF(Live, "gl.require-hardware",                   RequireHardwareGL, bool, false);
   DECL_GFX_PREF(Live, "gl.use-tls-is-current",                 UseTLSIsCurrent, int32_t, 0);
 
+  DECL_GFX_PREF(Live, "image.animated.decode-on-demand.threshold-kb", ImageAnimatedDecodeOnDemandThresholdKB, uint32_t, 20480);
+  DECL_GFX_PREF(Live, "image.animated.decode-on-demand.batch-size", ImageAnimatedDecodeOnDemandBatchSize, uint32_t, 6);
   DECL_GFX_PREF(Live, "image.cache.factor2.threshold-surfaces", ImageCacheFactor2ThresholdSurfaces, int32_t, -1);
   DECL_GFX_PREF(Once, "image.cache.size",                      ImageCacheSize, int32_t, 5*1024*1024);
   DECL_GFX_PREF(Once, "image.cache.timeweight",                ImageCacheTimeWeight, int32_t, 500);
