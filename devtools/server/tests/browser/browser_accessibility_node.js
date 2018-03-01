fc156ce9b80402ad1b4d931f8ea1bbcf9300faf5
Dorel Luca <dluca@mozilla.com>
2018-03-02 01:48:52 +0200

    Backed out 2 changesets (bug 1428430) for devtools failure on /builds/worker/workspace/build/src/accessible/base/nsAccessibilityService.cpp:1045 a=backout
    Backed out changeset 7d9813d8cb47 (bug 1428430)
    Backed out changeset a638660a49fa (bug 1428430)

--- devtools/server/tests/browser/browser_accessibility_node.js
+++ devtools/server/tests/browser/browser_accessibility_node.js
@@ -10,8 +10,7 @@ add_task(async function () {
   let {client, walker, accessibility} =
     await initAccessibilityFrontForUrl(MAIN_DOMAIN + "doc_accessibility.html");
 
-  let a11yWalker = await accessibility.getWalker();
-  await accessibility.enable();
+  let a11yWalker = await accessibility.getWalker(walker);
   let buttonNode = await walker.querySelector(walker.rootNode, "#button");
   let accessibleFront = await a11yWalker.getAccessibleFor(buttonNode);
 
@@ -23,22 +22,37 @@ add_task(async function () {
     help: "",
     keyboardShortcut: "",
     childCount: 1,
-    domNodeType: 1,
-    indexInParent: 1,
-    states: ["focusable", "selectable text", "opaque", "enabled", "sensitive"],
-    actions: [ "Press" ],
-    attributes: {
-      "margin-top": "0px",
-      display: "inline-block",
-      "text-align": "center",
-      "text-indent": "0px",
-      "margin-left": "0px",
-      tag: "button",
-      "margin-right": "0px",
-      id: "button",
-      "margin-bottom": "0px"
-    }
+    domNodeType: 1
   });
+
+  info("Actions");
+  let actions = await accessibleFront.getActions();
+  is(actions.length, 1, "Accessible Front has correct number of actions");
+  is(actions[0], "Press", "Accessible Front default action is correct");
+
+  info("Index in parent");
+  let index = await accessibleFront.getIndexInParent();
+  is(index, 1, "Accessible Front has correct index in parent");
+
+  info("State");
+  let state = await accessibleFront.getState();
+  SimpleTest.isDeeply(state,
+    ["focusable", "selectable text", "opaque", "enabled", "sensitive"],
+    "Accessible Front has correct states");
+
+  info("Attributes");
+  let attributes = await accessibleFront.getAttributes();
+  SimpleTest.isDeeply(attributes, {
+    "margin-top": "0px",
+    display: "inline-block",
+    "text-align": "center",
+    "text-indent": "0px",
+    "margin-left": "0px",
+    tag: "button",
+    "margin-right": "0px",
+    id: "button",
+    "margin-bottom": "0px"
+  }, "Accessible Front has correct attributes");
 
   info("Children");
   let children = await accessibleFront.children();
@@ -48,8 +62,13 @@ add_task(async function () {
     role: "text leaf"
   });
 
-  await accessibility.disable();
-  await waitForA11yShutdown();
+  info("DOM Node");
+  let node = await accessibleFront.getDOMNode(walker);
+  is(node, buttonNode, "Accessible Front has correct DOM node");
+
+  let a11yShutdown = waitForA11yShutdown();
   await client.close();
+  forceCollections();
+  await a11yShutdown;
   gBrowser.removeCurrentTab();
 });
