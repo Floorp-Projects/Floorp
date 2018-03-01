44cccbfa7257fbdaff1f282e2cb6b0f711d06628
Yura Zenevich <yura.zenevich@gmail.com>
2018-03-01 09:48:41 -0500

    Bug 1428430 - added enable, disable, highlighter, picker a11y functionality. r=pbro, ochameau
    MozReview-Commit-ID: 7QsY75oJCtW

--- devtools/server/tests/browser/browser_accessibility_walker.js
+++ devtools/server/tests/browser/browser_accessibility_walker.js
@@ -10,10 +10,12 @@ add_task(async function () {
   let {client, walker, accessibility} =
     await initAccessibilityFrontForUrl(MAIN_DOMAIN + "doc_accessibility.html");
 
-  let a11yWalker = await accessibility.getWalker(walker);
+  let a11yWalker = await accessibility.getWalker();
   ok(a11yWalker, "The AccessibleWalkerFront was returned");
 
-  let a11yDoc = await a11yWalker.getDocument();
+  await accessibility.enable();
+  let rootNode = await walker.getRootNode();
+  let a11yDoc = await a11yWalker.getAccessibleFor(rootNode);
   ok(a11yDoc, "The AccessibleFront for root doc is created");
 
   let children = await a11yWalker.children();
@@ -29,6 +31,15 @@ add_task(async function () {
     name: "Accessible Button",
     role: "pushbutton"
   });
+
+  let ancestry = await a11yWalker.getAncestry(accessibleFront);
+  is(ancestry.length, 1, "Button is a direct child of a root document.");
+  is(ancestry[0].accessible, a11yDoc,
+    "Button's only ancestor is a root document");
+  is(ancestry[0].children.length, 3,
+    "Root doc should have correct number of children");
+  ok(ancestry[0].children.includes(accessibleFront),
+    "Button accessible front is in root doc's children");
 
   let browser = gBrowser.selectedBrowser;
 
@@ -67,9 +78,55 @@ add_task(async function () {
     () => ContentTask.spawn(browser, null, () =>
       content.document.getElementById("button").remove()));
 
-  let a11yShutdown = waitForA11yShutdown();
+  let shown = await a11yWalker.highlightAccessible(docChildren[0]);
+  ok(shown, "AccessibleHighlighter highlighted the node");
+
+  shown = await a11yWalker.highlightAccessible(a11yDoc);
+  ok(!shown, "AccessibleHighlighter does not highlight an accessible with no bounds");
+  await a11yWalker.unhighlight();
+
+  info("Checking AccessibleWalker picker functionality");
+  ok(a11yWalker.pick, "AccessibleWalker pick method exists");
+  ok(a11yWalker.pickAndFocus, "AccessibleWalker pickAndFocus method exists");
+  ok(a11yWalker.cancelPick, "AccessibleWalker cancelPick method exists");
+
+  let onPickerEvent = a11yWalker.once("picker-accessible-hovered");
+  await a11yWalker.pick();
+  await BrowserTestUtils.synthesizeMouseAtCenter("#h1", { type: "mousemove" }, browser);
+  let acc = await onPickerEvent;
+  checkA11yFront(acc, { name: "Accessibility Test" }, docChildren[0]);
+
+  onPickerEvent = a11yWalker.once("picker-accessible-previewed");
+  await BrowserTestUtils.synthesizeMouseAtCenter("#h1", { shiftKey: true }, browser);
+  acc = await onPickerEvent;
+  checkA11yFront(acc, { name: "Accessibility Test" }, docChildren[0]);
+
+  onPickerEvent = a11yWalker.once("picker-accessible-canceled");
+  await BrowserTestUtils.synthesizeKey("VK_ESCAPE", { type: "keydown" }, browser);
+  await onPickerEvent;
+
+  onPickerEvent = a11yWalker.once("picker-accessible-hovered");
+  await a11yWalker.pick();
+  await BrowserTestUtils.synthesizeMouseAtCenter("#h1", { type: "mousemove" }, browser);
+  await onPickerEvent;
+
+  onPickerEvent = a11yWalker.once("picker-accessible-picked");
+  await BrowserTestUtils.synthesizeMouseAtCenter("#h1", { }, browser);
+  acc = await onPickerEvent;
+  checkA11yFront(acc, { name: "Accessibility Test" }, docChildren[0]);
+
+  await a11yWalker.cancelPick();
+
+  info("Checking document-ready event fired by walker when top level accessible " +
+       "document is recreated.");
+  let reloaded = BrowserTestUtils.browserLoaded(browser);
+  let documentReady = a11yWalker.once("document-ready");
+  browser.reload();
+  await reloaded;
+  await documentReady;
+
+  await accessibility.disable();
+  await waitForA11yShutdown();
   await client.close();
-  forceCollections();
-  await a11yShutdown;
   gBrowser.removeCurrentTab();
 });
