32475594f46192231e54b384c415627a6997ab61
Brian Grinstead <bgrinstead@mozilla.com>
2018-03-02 09:42:55 -0800

    Bug 1442695 - Remove unused GetScrollbarPressStates method;r=spohl
    MozReview-Commit-ID: GA9kzatUKfp

--- widget/cocoa/nsNativeThemeCocoa.h
+++ widget/cocoa/nsNativeThemeCocoa.h
@@ -163,8 +163,6 @@ protected:
   void DrawResizer(CGContextRef cgContext, const HIRect& aRect, nsIFrame *aFrame);
 
   // Scrollbars
-  void GetScrollbarPressStates(nsIFrame *aFrame,
-                               mozilla::EventStates aButtonStates[]);
   nsIFrame* GetParentScrollbarFrame(nsIFrame *aFrame);
   bool IsParentScrollbarRolledOver(nsIFrame* aFrame);
 
