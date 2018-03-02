2af458e291941641d29d257baf6feaf630cd4c95
Daniel Zielas <zielas.daniel@gmail.com>
2018-03-02 19:00:03 +0100

    Bug 1420512 - Try unifying data structures for scrollbar container and scrollbar thumb info. r=botond
    MozReview-Commit-ID: 9zPkrA3CwsN

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -3383,7 +3383,7 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
                                        aBuilder->CurrentActiveScrolledRoot(),
                                        nsDisplayOwnLayerFlags::eNone,
                                        mozilla::layers::FrameMetrics::NULL_SCROLL_ID,
-                                       ScrollThumbData{}, /* aForceActive = */ false));
+                                       ScrollbarData{}, /* aForceActive = */ false));
     if (aCreatedContainerItem) {
       *aCreatedContainerItem = true;
     }
