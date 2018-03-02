bdfa57bfeafd4fec7a1a9a701674b0d410c69001
Andreea Pavel <apavel@mozilla.com>
2018-03-02 13:03:58 +0200

    Backed out changeset f15ab7984211 (bug 1433592) for failing browser chrome at browser/components/resistfingerprinting/test/browser/browser_spoofing_keyboard_event.js on a CLOSED TREE

--- dom/events/KeyboardEvent.cpp
+++ dom/events/KeyboardEvent.cpp
@@ -49,10 +49,15 @@ KeyboardEvent::AltKey(CallerType aCallerType)
 bool
 KeyboardEvent::CtrlKey(CallerType aCallerType)
 {
-  // We don't spoof this key when privacy.resistFingerprinting
-  // is enabled, because it is often used for command key
-  // combinations in web apps.
-  return mEvent->AsKeyboardEvent()->IsControl();
+  bool ctrlState = mEvent->AsKeyboardEvent()->IsControl();
+
+  if (!ShouldResistFingerprinting(aCallerType)) {
+    return ctrlState;
+  }
+
+  // We need to give a spoofed state for Control key since it could be used as a
+  // modifier key in certain asian keyboard layouts.
+  return GetSpoofedModifierStates(Modifier::MODIFIER_CONTROL, ctrlState);
 }
 
 bool
@@ -70,9 +75,6 @@ KeyboardEvent::ShiftKey(CallerType aCallerType)
 bool
 KeyboardEvent::MetaKey()
 {
-  // We don't spoof this key when privacy.resistFingerprinting
-  // is enabled, because it is often used for command key
-  // combinations in web apps.
   return mEvent->AsKeyboardEvent()->IsMeta();
 }
 
