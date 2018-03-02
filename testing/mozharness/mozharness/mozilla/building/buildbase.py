693d0387071b4396bcdd60422ee9487ef8923819
Nathan Froyd <froydnj@mozilla.com>
2018-03-02 08:34:39 -0500

    Bug 1442378 - part 4 - properly record omni.ja sizes for desktop platforms; r=jmaher
    Unlike Android installers, installers for desktop versions of Firefox
    contain two copies of omni.ja, and the code to check for omni.ja was
    ignoring both copies.  Let's special-case omni.ja so we get better
    numbers for our desktop platforms.

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -1604,6 +1604,13 @@ or run without that action (ie: --no-{action})"
                 for path, size in paths_with_sizes(installer):
                     name = os.path.basename(path)
                     if name in interests:
+                        # We have to be careful here: desktop Firefox installers
+                        # contain two omni.ja files: one for the general runtime,
+                        # and one for the browser proper.
+                        if name == 'omni.ja':
+                            containing_dir = os.path.basename(os.path.dirname(path))
+                            if containing_dir == 'browser':
+                                name = 'browser-omni.ja'
                         if name in subtests:
                             # File seen twice in same archive;
                             # ignore to avoid confusion.
