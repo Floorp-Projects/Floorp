724f46419e27fff6a1bd24088e3b157f91226513
J. Ryan Stinnett <jryans@gmail.com>
2018-03-02 19:14:05 -0600

    Bug 1436187 - Move existing shared head files to devtools/client/shared. r=bgrins
    Move shared-head.js and shared-redux-head.js into shared.
    
    MozReview-Commit-ID: 5NUxcl9gkLl
    
    --HG--
    rename : devtools/client/framework/test/shared-head.js => devtools/client/shared/test/shared-head.js
    rename : devtools/client/framework/test/shared-redux-head.js => devtools/client/shared/test/shared-redux-head.js
    extra : rebase_source : c6f6cd75a9c1a5349f8406f337c9c39f2f755dab

--- devtools/client/framework/test/head.js
+++ devtools/client/framework/test/head.js
@@ -3,10 +3,10 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/* import-globals-from shared-head.js */
+/* import-globals-from ../../shared/test/shared-head.js */
 
 // shared-head.js handles imports, constants, and utility functions
-Services.scriptloader.loadSubScript("chrome://mochitests/content/browser/devtools/client/framework/test/shared-head.js", this);
+Services.scriptloader.loadSubScript("chrome://mochitests/content/browser/devtools/client/shared/test/shared-head.js", this);
 
 const EventEmitter = require("devtools/shared/old-event-emitter");
 
