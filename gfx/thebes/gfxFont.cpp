03d2c242deb13b42a8231d9a7737b64decdaed12
Jonathan Kew <jkew@mozilla.com>
2018-03-05 21:57:57 +0000

    Bug 253143 - Do not add letter-spacing after zero-width format controls such as ZWSP. r=jwatt

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -3059,6 +3059,8 @@ gfxFont::ShapeTextWithoutWordCache(DrawTarget *aDrawTarget,
             aTextRun->SetIsTab(aOffset + i);
         } else if (ch == '\n') {
             aTextRun->SetIsNewline(aOffset + i);
+        } else if (GetGeneralCategory(ch) == HB_UNICODE_GENERAL_CATEGORY_FORMAT) {
+            aTextRun->SetIsFormattingControl(aOffset + i);
         } else if (IsInvalidControlChar(ch) &&
             !(aTextRun->GetFlags() & gfx::ShapedTextFlags::TEXT_HIDE_CONTROL_CHARACTERS)) {
             if (GetFontEntry()->IsUserFont() && HasCharacter(ch)) {
@@ -3277,6 +3279,8 @@ gfxFont::SplitAndInitTextRun(DrawTarget *aDrawTarget,
             aTextRun->SetIsTab(aRunStart + i);
         } else if (ch == '\n') {
             aTextRun->SetIsNewline(aRunStart + i);
+        } else if (GetGeneralCategory(ch) == HB_UNICODE_GENERAL_CATEGORY_FORMAT) {
+            aTextRun->SetIsFormattingControl(aRunStart + i);
         } else if (IsInvalidControlChar(ch) &&
             !(aTextRun->GetFlags() & gfx::ShapedTextFlags::TEXT_HIDE_CONTROL_CHARACTERS)) {
             if (GetFontEntry()->IsUserFont() && HasCharacter(ch)) {
