1cae0f47d1e6518bebaa2b8b4dc78ad2f48e1c75
Sylvestre Ledru <sledru@mozilla.com>
2018-03-05 09:33:02 +0100

    Bug 1443080 - Now that we use static call, some var instances are not needed anymore r=Ehsan
    MozReview-Commit-ID: AiuJIY8Gssl

--- dom/media/webaudio/blink/HRTFPanner.cpp
+++ dom/media/webaudio/blink/HRTFPanner.cpp
@@ -102,9 +102,6 @@ int HRTFPanner::calculateDesiredAzimuthIndexAndBlend(double azimuth, double& azi
     if (azimuth < 0)
         azimuth += 360.0;
 
-    HRTFDatabase* database = m_databaseLoader->database();
-    MOZ_ASSERT(database);
-
     int numberOfAzimuths = HRTFDatabase::numberOfAzimuths();
     const double angleBetweenAzimuths = 360.0 / numberOfAzimuths;
 
