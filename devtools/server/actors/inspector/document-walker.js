4afbb03a9cbdbfe5e47a90137c8c8e5e28868cfd
Julian Descottes <jdescottes@mozilla.com>
2018-03-05 14:27:45 +0100

    Bug 1053898 - Update DocumentWalker constructor to support showAnonymousContent parameter;r=pbro
    MozReview-Commit-ID: 6c7qlnbe5ND
    
    --HG--
    extra : rebase_source : b32b14c180d94082ae8ecdb6195daa6f44f40209

--- devtools/server/actors/inspector/document-walker.js
+++ devtools/server/actors/inspector/document-walker.js
@@ -30,12 +30,18 @@ const SKIP_TO_SIBLING = "SKIP_TO_SIBLING";
  *          Either SKIP_TO_PARENT or SKIP_TO_SIBLING. If the provided node is not
  *          compatible with the filter function for this walker, try to find a compatible
  *          one either in the parents or in the siblings of the node.
+ *        - {Boolean} showAnonymousContent
+ *          Pass true to let the walker return and traverse anonymous content.
+ *          When navigating host elements to which shadow DOM is attached, the light tree
+ *          will be visible only to a walker with showAnonymousContent=false. The shadow
+ *          tree will only be visible to a walker with showAnonymousContent=true.
  */
 function DocumentWalker(node, rootWin,
   {
     whatToShow = nodeFilterConstants.SHOW_ALL,
     filter = standardTreeWalkerFilter,
-    skipTo = SKIP_TO_PARENT
+    skipTo = SKIP_TO_PARENT,
+    showAnonymousContent = true
   } = {}) {
   if (Cu.isDeadWrapper(rootWin) || !rootWin.location) {
     throw new Error("Got an invalid root window in DocumentWalker");
@@ -43,7 +49,7 @@ function DocumentWalker(node, rootWin,
 
   this.walker = Cc["@mozilla.org/inspector/deep-tree-walker;1"]
     .createInstance(Ci.inIDeepTreeWalker);
-  this.walker.showAnonymousContent = true;
+  this.walker.showAnonymousContent = showAnonymousContent;
   this.walker.showSubDocuments = true;
   this.walker.showDocumentsAsNodes = true;
   this.walker.init(rootWin.document, whatToShow);
