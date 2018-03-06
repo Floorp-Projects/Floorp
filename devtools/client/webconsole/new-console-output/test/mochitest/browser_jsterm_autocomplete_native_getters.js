3ebf4c888126c5ba8417c9880329d37c9ce7b575
Mark Banner <standard8@mozilla.com>
2018-03-06 23:39:45 +0000

    Bug 1443661 - Remove unnecessary ESLint import-globals-from directives from devtools/client/webconsole/new-console-output/test/mochitest. r=nchevobbe
    MozReview-Commit-ID: IWHDVjZHb9z

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_autocomplete_native_getters.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_autocomplete_native_getters.js
@@ -3,8 +3,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/* import-globals-from head.js */
-
 // Tests that native getters (e.g. document.body) autocompletes in the web console.
 // See Bug 651501.
 
--- devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_hide_when_devtools_chrome_enabled_false.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_jsterm_hide_when_devtools_chrome_enabled_false.js
@@ -3,8 +3,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/* import-globals-from head.js */
-
 /*
  * Hide Browser Console JS input field if devtools.chrome.enabled is false.
  *
