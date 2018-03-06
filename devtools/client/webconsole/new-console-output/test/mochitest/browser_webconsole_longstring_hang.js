ca1d1c7f949b5eac8a80b705b7b43bb8f43e9fc3
Mark Banner <standard8@mozilla.com>
2018-03-06 22:50:58 +0000

    Bug 1443661 - Enable ESLint rule no-undef for test files in devtools/client/webconsole/new-console-output/. r=nchevobbe
    MozReview-Commit-ID: 39cCKczUMQI
    
    --HG--
    extra : rebase_source : 95819adebef10c8db626bc75f985f75f9e39cad8

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_longstring_hang.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_longstring_hang.js
@@ -5,6 +5,9 @@
 
 /* import-globals-from head.js */
 
+// XXX Remove this when the file is migrated to the new frontend.
+/* eslint-disable no-undef */
+
 // Test that very long strings do not hang the browser.
 
 "use strict";
