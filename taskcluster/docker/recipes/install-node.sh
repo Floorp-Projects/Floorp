cd8efbae2c6e101ee0bce4ab9e46bc0742066f30
Mark Banner <standard8@mozilla.com>
2018-03-06 19:37:13 +0000

    Bug 1443547 - Upgrade Docker images to node 8.9.4 & npm 5.6.0. r=dustin
    MozReview-Commit-ID: 3rrH3JcOrnu
    
    --HG--
    extra : rebase_source : bda65b81a15fd5098faf3c1a99a8c949e712e076

--- taskcluster/docker/recipes/install-node.sh
+++ taskcluster/docker/recipes/install-node.sh
@@ -3,10 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# This script installs Node v6.
+# This script installs Node v8.
 
-wget --progress=dot:mega https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-x64.tar.gz
-echo 'a9d9e6308931fa2a2b0cada070516d45b76d752430c31c9198933c78f8d54b17  node-v6.9.1-linux-x64.tar.gz' | sha256sum -c
-tar -C /usr/local -xz --strip-components 1 < node-v6.9.1-linux-x64.tar.gz
+wget --progress=dot:mega https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-x64.tar.gz
+echo '21fb4690e349f82d708ae766def01d7fec1b085ce1f5ab30d9bda8ee126ca8fc  node-v8.9.4-linux-x64.tar.gz' | sha256sum -c
+tar -C /usr/local -xz --strip-components 1 < node-v8.9.4-linux-x64.tar.gz
 node -v  # verify
 npm -v
