bd4a75b39dd03c53b3cf889b9cd2af9ff3969c0c
Jim Chen <nchen@mozilla.com>
2018-03-06 00:04:56 -0500

    Bug 1442255 - 5. Use extras bundle to initialize child processes; r=esawin
    Pass the extras bundle from the main process to the child process
    through IChildProcess.start, instead of through the Intent, which never
    worked because intent extras are not passed to Service.onBind.
    
    MozReview-Commit-ID: I2EToBNE2Y6

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoServiceChildProcess.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoServiceChildProcess.java
@@ -16,6 +16,7 @@ import org.mozilla.gecko.util.ThreadUtils;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.ParcelFileDescriptor;
 import android.os.Process;
@@ -61,8 +62,9 @@ public class GeckoServiceChildProcess extends Service {
         @Override
         public boolean start(final IProcessManager procMan,
                              final String[] args,
-                             final ParcelFileDescriptor crashReporterPfd,
+                             final Bundle extras,
                              final ParcelFileDescriptor ipcPfd,
+                             final ParcelFileDescriptor crashReporterPfd,
                              final ParcelFileDescriptor crashAnnotationPfd) {
             synchronized (GeckoServiceChildProcess.class) {
                 if (sProcessManager != null) {
@@ -72,15 +74,17 @@ public class GeckoServiceChildProcess extends Service {
                 sProcessManager = procMan;
             }
 
+            final int ipcFd = ipcPfd != null ? ipcPfd.detachFd() : -1;
             final int crashReporterFd = crashReporterPfd != null ?
                                         crashReporterPfd.detachFd() : -1;
-            final int ipcFd = ipcPfd != null ? ipcPfd.detachFd() : -1;
-            final int crashAnnotationFd = crashAnnotationPfd != null ? crashAnnotationPfd.detachFd() : -1;
+            final int crashAnnotationFd = crashAnnotationPfd != null ?
+                                          crashAnnotationPfd.detachFd() : -1;
 
             ThreadUtils.postToUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    if (GeckoThread.initChildProcess(args, crashReporterFd, ipcFd, crashAnnotationFd)) {
+                    if (GeckoThread.initChildProcess(args, extras, ipcFd, crashReporterFd,
+                                                     crashAnnotationFd)) {
                         GeckoThread.launch();
                     }
                 }
@@ -91,7 +95,6 @@ public class GeckoServiceChildProcess extends Service {
 
     @Override
     public IBinder onBind(final Intent intent) {
-        GeckoLoader.setLastIntent(new SafeIntent(intent));
         GeckoThread.launch(); // Preload Gecko.
         return mBinder;
     }
