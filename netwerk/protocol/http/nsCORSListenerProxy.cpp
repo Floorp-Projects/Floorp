ecd9c51e6f82f0416dfbf8acb63954216e725356
Honza Bambas <honzab.moz@firemni.cz>
2018-03-07 08:03:00 -0500

    Bug 1440567 - Add non-null checks around assignment of warning report to preflight HTTP channel in nsCORSListenerProxy::StartCORSPreflight. r=dveditz

--- netwerk/protocol/http/nsCORSListenerProxy.cpp
+++ netwerk/protocol/http/nsCORSListenerProxy.cpp
@@ -1522,7 +1522,9 @@ nsCORSListenerProxy::StartCORSPreflight(nsIChannel* aRequestChannel,
   // the warning reporter.
   RefPtr<nsHttpChannel> reqCh = do_QueryObject(aRequestChannel);
   RefPtr<nsHttpChannel> preCh = do_QueryObject(preHttp);
-  preCh->SetWarningReporter(reqCh->GetWarningReporter());
+  if (preCh && reqCh) { // there are other implementers of nsIHttpChannel
+    preCh->SetWarningReporter(reqCh->GetWarningReporter());
+  }
 
   nsTArray<nsCString> preflightHeaders;
   if (!aUnsafeHeaders.IsEmpty()) {
