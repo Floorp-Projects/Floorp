12e562c768c526db65c28588c1149dccc94f0693
Jim Chen <nchen@mozilla.com>
2018-03-07 16:12:51 -0500

    Bug 1442243 - 6. Add BaseSessionTest; r=snorp
    Add BaseSessionTest to provide some common boilerplate code, as well as
    Kotlin extension functions for operating on a GeckoSession.
    
    MozReview-Commit-ID: 73KTZN6Zo0K

---
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/BaseSessionTest.kt
@@ -0,0 +1,87 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package org.mozilla.geckoview.test
+
+import android.os.Parcel
+import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.test.rule.GeckoSessionTestRule
+
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.rules.ErrorCollector
+
+import kotlin.reflect.KClass
+
+/**
+ * Common base class for tests using GeckoSessionTestRule,
+ * providing the test rule and other utilities.
+ */
+open class BaseSessionTest(noErrorCollector: Boolean = false) {
+    companion object {
+        const val INVALID_URI = "http://www.test.invalid/"
+        const val HELLO_HTML_PATH = "/assets/www/hello.html"
+        const val HELLO2_HTML_PATH = "/assets/www/hello2.html"
+        const val NEW_SESSION_HTML_PATH = "/assets/www/newSession.html";
+        const val NEW_SESSION_CHILD_HTML_PATH = "/assets/www/newSession_child.html";
+    }
+
+    @get:Rule val sessionRule = GeckoSessionTestRule()
+
+    @get:Rule val errors = ErrorCollector()
+    fun <T> assertThat(reason: String, v: T, m: Matcher<T>) = sessionRule.assertThat(reason, v, m)
+
+    init {
+        if (!noErrorCollector) {
+            sessionRule.errorCollector = errors
+        }
+    }
+
+    fun GeckoSession.loadTestPath(path: String) =
+            this.loadUri(GeckoSessionTestRule.APK_URI_PREFIX + path.removePrefix("/"))
+
+    inline fun GeckoSession.toParcel(lambda: (Parcel) -> Unit) {
+        val parcel = Parcel.obtain()
+        try {
+            this.writeToParcel(parcel, 0)
+
+            val pos = parcel.dataPosition()
+            parcel.setDataPosition(0)
+
+            lambda(parcel)
+
+            assertThat("Read parcel matches written parcel",
+                       parcel.dataPosition(), Matchers.equalTo(pos))
+        } finally {
+            parcel.recycle()
+        }
+    }
+
+    fun GeckoSession.openWindow() =
+            sessionRule.openSession(this)
+
+    fun GeckoSession.waitForPageStop() =
+            sessionRule.waitForPageStop(this)
+
+    fun GeckoSession.waitForPageStops(count: Int) =
+            sessionRule.waitForPageStops(this, count)
+
+    fun GeckoSession.waitUntilCalled(ifce: KClass<*>, vararg methods: String) =
+            sessionRule.waitUntilCalled(this, ifce, *methods)
+
+    fun GeckoSession.waitUntilCalled(callback: Any) =
+            sessionRule.waitUntilCalled(this, callback)
+
+    fun GeckoSession.forCallbacksDuringWait(callback: Any) =
+            sessionRule.forCallbacksDuringWait(this, callback)
+
+    fun GeckoSession.delegateUntilTestEnd(callback: Any) =
+            sessionRule.delegateUntilTestEnd(this, callback)
+
+    fun GeckoSession.delegateDuringNextWait(callback: Any) =
+            sessionRule.delegateDuringNextWait(this, callback)
+
+    fun GeckoSession.synthesizeTap(x: Int, y: Int) =
+            sessionRule.synthesizeTap(this, x, y)
+}
