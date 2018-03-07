d21a6a76a47f9d9a47f9bc6b0f153ea3f5aa64bd
Noemi Erli <nerli@mozilla.com>
2018-03-07 20:55:26 +0200

    Backed out changeset 04fecea9eaf0 (bug 1443672) as per dholbert's request on a CLOSED TREE

--- layout/base/nsFrameTraversal.cpp
+++ layout/base/nsFrameTraversal.cpp
@@ -3,12 +3,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#include "nsFrameTraversal.h"
-
 #include "nsCOMPtr.h"
 #include "nsGkAtoms.h"
 
+#include "nsFrameTraversal.h"
 #include "nsFrameList.h"
 #include "nsPlaceholderFrame.h"
 #include "nsContainerFrame.h"
