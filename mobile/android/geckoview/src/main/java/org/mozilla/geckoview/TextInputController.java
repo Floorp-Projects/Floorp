2993ad4a2f458d88d57b4a5348e9f16b351a06a2
James Willcox <snorp@snorp.net>
2018-03-08 09:05:35 -0600

    Bug 1444092 - Use package visibility for TextInputController constructor r=droeh,jchen
    MozReview-Commit-ID: 2PVod2neTjU

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TextInputController.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TextInputController.java
@@ -100,8 +100,8 @@ public final class TextInputController {
     private final GeckoEditableChild mEditableChild = new GeckoEditableChild(mEditable);
     private Delegate mInputConnection;
 
-    public TextInputController(final @NonNull GeckoSession session,
-                               final @NonNull NativeQueue queue) {
+    /* package */ TextInputController(final @NonNull GeckoSession session,
+                                      final @NonNull NativeQueue queue) {
         mSession = session;
         mQueue = queue;
         mEditable.setDefaultEditableChild(mEditableChild);
