15f6ca70fd9d39222c862a4b3b5ba263443b039c
Boris Zbarsky <bzbarsky@mit.edu>
2018-03-07 23:35:48 -0500

    Backed out changeset 52d86f152c2d because it included bits that shouldn't have been in there leading to a CLOSED TREE.
    MozReview-Commit-ID: KWl43hdIGN2

--- dom/base/nsDOMTokenList.h
+++ dom/base/nsDOMTokenList.h
@@ -75,7 +75,7 @@ public:
   void Remove(const nsAString& aToken, mozilla::ErrorResult& aError);
   void Remove(const nsTArray<nsString>& aTokens,
               mozilla::ErrorResult& aError);
-  bool Replace(const nsAString& aToken,
+  void Replace(const nsAString& aToken,
                const nsAString& aNewToken,
                mozilla::ErrorResult& aError);
   bool Toggle(const nsAString& aToken,
@@ -99,7 +99,7 @@ protected:
                    const nsTArray<nsString>& aTokens);
   void RemoveInternal(const nsAttrValue* aAttr,
                       const nsTArray<nsString>& aTokens);
-  bool ReplaceInternal(const nsAttrValue* aAttr,
+  void ReplaceInternal(const nsAttrValue* aAttr,
                        const nsAString& aToken,
                        const nsAString& aNewToken);
   inline const nsAttrValue* GetParsedAttr();
