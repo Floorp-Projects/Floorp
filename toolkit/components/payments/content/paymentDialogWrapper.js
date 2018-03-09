568d0a2e27af321c7d6c586487be69adee94167b
Csoregi Natalia <ncsoregi@mozilla.com>
2018-03-09 02:46:15 +0200

    Backed out 2 changesets (bug 1440499) for ESlint failure on paymentDialogWrapper.js:53. CLOSED TREE
    Backed out changeset 4e2e081dad55 (bug 1440499)
    Backed out changeset 479b143d8828 (bug 1440499)

--- toolkit/components/payments/content/paymentDialogWrapper.js
+++ toolkit/components/payments/content/paymentDialogWrapper.js
@@ -47,33 +47,6 @@ var paymentDialogWrapper = {
    * Note: This method is async because formAutofillStorage plans to become async.
    *
    * @param {string} guid
-   * @returns {object} containing only the requested payer values.
-   */
-  async _convertProfileAddressToPayerData(guid) {
-    let addressData = profileStorage.addresses.get(guid);
-    if (!addressData) {
-      throw new Error(`Payer address not found: ${guid}`);
-    }
-
-    let {
-      requestPayerName,
-      requestPayerEmail,
-      requestPayerPhone,
-    } = this.request.paymentOptions;
-
-    let payerData = {
-      payerName: requestPayerName ? addressData.name : "",
-      payerEmail: requestPayerEmail ? addressData.email : "",
-      payerPhone: requestPayerPhone ? addressData.tel : "",
-    };
-
-    return payerData;
-  },
-
-  /**
-   * Note: This method is async because profileStorage plans to become async.
-   *
-   * @param {string} guid
    * @returns {nsIPaymentAddress}
    */
   async _convertProfileAddressToPaymentAddress(guid) {
@@ -402,7 +375,6 @@ var paymentDialogWrapper = {
   },
 
   async onPay({
-    selectedPayerAddressGUID: payerGUID,
     selectedPaymentCardGUID: paymentCardGUID,
     selectedPaymentCardSecurityCode: cardSecurityCode,
   }) {
@@ -416,18 +388,9 @@ var paymentDialogWrapper = {
       return;
     }
 
-    let {
-      payerName,
-      payerEmail,
-      payerPhone,
-    } = await this._convertProfileAddressToPayerData(payerGUID);
-
     this.pay({
       methodName: "basic-card",
       methodData,
-      payerName,
-      payerEmail,
-      payerPhone,
     });
   },
 
