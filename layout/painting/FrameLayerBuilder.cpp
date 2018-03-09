1eb093233dc72f4aa8a1e1ac4008316d7c1a5964
Bas Schouten <bschouten@mozilla.com>
2018-03-09 05:27:15 +0100

    Bug 1440753: Replace pixman regions with our own region code. r=mattwoodrow
    MozReview-Commit-ID: KPsTAw3Uwa2

--- layout/painting/FrameLayerBuilder.cpp
+++ layout/painting/FrameLayerBuilder.cpp
@@ -5934,7 +5934,7 @@ FrameLayerBuilder::RecomputeVisibilityForItems(nsTArray<AssignedDisplayItem>& aI
       newVisible.Sub(visible, removed);
       // Don't let the visible region get too complex.
       if (newVisible.GetNumRects() <= 15) {
-        visible = newVisible;
+        visible = Move(newVisible);
       }
     }
   }
