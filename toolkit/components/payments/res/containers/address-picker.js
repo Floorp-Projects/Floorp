bd65a659113f283243a348438b182075d3a6dae4
Sam Foster <sfoster@mozilla.com>
2018-03-09 11:49:21 -0800

    Bug 1444240 - Filter address options to eliminate dupes based on fields to display. r=MattN
    * Add an observed attribute to address-picker to render when address-fields is changed
    * Add a filter step to address-picker, to exclude addresses that represent duplicates given the requested fields
    * Filter out addresses that do not contain one or more of the requested fields
    * Use a default set of fields to establish duplicates and meet the bar for adding an address as an option
    
    MozReview-Commit-ID: 7lg4suNHv60

--- toolkit/components/payments/res/containers/address-picker.js
+++ toolkit/components/payments/res/containers/address-picker.js
@@ -13,6 +13,10 @@
  */
 
 class AddressPicker extends PaymentStateSubscriberMixin(HTMLElement) {
+  static get observedAttributes() {
+    return ["address-fields"];
+  }
+
   constructor() {
     super();
     this.dropdown = document.createElement("rich-select");
@@ -24,10 +28,67 @@ class AddressPicker extends PaymentStateSubscriberMixin(HTMLElement) {
     super.connectedCallback();
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.render(this.requestStore.getState());
+    }
+  }
+
+  /**
+   * De-dupe and filter addresses for the given set of fields that will be visible
+   *
+   * @param {object} addresses
+   * @param {array?} fieldNames - optional list of field names that be used when de-duping entries
+   * @returns {object} filtered copy of given addresses
+   */
+  filterAddresses(addresses, fieldNames = [
+    "address-level1",
+    "address-level2",
+    "country",
+    "name",
+    "postal-code",
+    "street-address",
+  ]) {
+    let uniques = new Set();
+    let result = {};
+    for (let [guid, address] of Object.entries(addresses)) {
+      let addressCopy = {};
+      let isMatch;
+      // exclude addresses that are missing any of the requested fields
+      for (let name of fieldNames) {
+        if (address[name]) {
+          isMatch = true;
+          addressCopy[name] = address[name];
+        } else {
+          isMatch = false;
+          break;
+        }
+      }
+      if (isMatch) {
+        let key = JSON.stringify(addressCopy);
+        // exclude duplicated addresses
+        if (!uniques.has(key)) {
+          uniques.add(key);
+          result[guid] = address;
+        }
+      }
+    }
+    return result;
+  }
+
   render(state) {
     let {savedAddresses} = state;
     let desiredOptions = [];
-    for (let [guid, address] of Object.entries(savedAddresses)) {
+    let fieldNames;
+    if (this.hasAttribute("address-fields")) {
+      let names = this.getAttribute("address-fields").split(/\s+/);
+      if (names.length) {
+        fieldNames = names;
+      }
+    }
+    let filteredAddresses = this.filterAddresses(savedAddresses, fieldNames);
+
+    for (let [guid, address] of Object.entries(filteredAddresses)) {
       let optionEl = this.dropdown.getOptionByValue(guid);
       if (!optionEl) {
         optionEl = document.createElement("address-option");
