a70f63a1ef81a3d15d29a0234be3c8efdef90ea9
Botond Ballo <botond@mozilla.com>
2018-03-09 17:26:24 -0500

    Bug 1443092 - Avoid calling SVGAnimatedEnumeration::AnimVal() from nsSVGUtils::GetBBox(). r=jwatt, a=RyanVM
    AnimVal() is a DOM getter, and it flushes animations, which we don't want
    in GetBBox() which is called from display list building cide and
    FrameLayerBuilder.
    
    MozReview-Commit-ID: 80DyTcGs5io

--- dom/svg/SVGClipPathElement.h
+++ dom/svg/SVGClipPathElement.h
@@ -37,6 +37,10 @@ public:
   // WebIDL
   already_AddRefed<SVGAnimatedEnumeration> ClipPathUnits();
 
+  // This is an internal method that does not flush style, and thus
+  // the answer may be out of date if there's a pending style flush.
+  bool IsUnitsObjectBoundingBox() const;
+
 protected:
 
   enum { CLIPPATHUNITS };
