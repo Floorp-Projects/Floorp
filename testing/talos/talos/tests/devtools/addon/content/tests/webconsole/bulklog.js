62de49567dec0a28524c17915aa65784c2a2b3f1
Julian Descottes <jdescottes@mozilla.com>
2018-03-09 09:03:11 +0100

    Bug 1441703 - Split DevTools performance test damp.js;r=ochameau
    MozReview-Commit-ID: Jwfe7RLxEg2

---
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/bulklog.js
@@ -0,0 +1,52 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { openToolbox, closeToolbox, getBrowserWindow, runTest, testSetup,
+        testTeardown, SIMPLE_URL } = require("chrome://damp/content/tests/head");
+
+module.exports = async function() {
+  let TOTAL_MESSAGES = 10;
+  let tab = await testSetup(SIMPLE_URL);
+  let messageManager = tab.linkedBrowser.messageManager;
+  let toolbox = await openToolbox("webconsole");
+  let webconsole = toolbox.getPanel("webconsole");
+
+  // Resolve once the last message has been received.
+  let allMessagesReceived = new Promise(resolve => {
+    function receiveMessages(messages) {
+      for (let m of messages) {
+        if (m.node.textContent.includes("damp " + TOTAL_MESSAGES)) {
+          webconsole.hud.ui.off("new-messages", receiveMessages);
+          // Wait for the console to redraw
+          getBrowserWindow().requestAnimationFrame(resolve);
+        }
+      }
+    }
+    webconsole.hud.ui.on("new-messages", receiveMessages);
+  });
+
+  // Load a frame script using a data URI so we can do logs
+  // from the page.  So this is running in content.
+  messageManager.loadFrameScript("data:,(" + encodeURIComponent(
+    `function () {
+      addMessageListener("do-logs", function () {
+        for (var i = 0; i < ${TOTAL_MESSAGES}; i++) {
+          content.console.log('damp', i+1, content);
+        }
+      });
+    }`
+  ) + ")()", true);
+
+  // Kick off the logging
+  messageManager.sendAsyncMessage("do-logs");
+
+  let test = runTest("console.bulklog");
+  await allMessagesReceived;
+  test.done();
+
+  await closeToolbox();
+  await testTeardown();
+};
---
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/objectexpand.js
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { openToolbox, closeToolboxAndLog, getBrowserWindow, runTest, testSetup,
+        testTeardown, SIMPLE_URL } = require("chrome://damp/content/tests/head");
+
+module.exports = async function() {
+  let tab = await testSetup(SIMPLE_URL);
+
+  let messageManager = tab.linkedBrowser.messageManager;
+  let toolbox = await openToolbox("webconsole");
+  let webconsole = toolbox.getPanel("webconsole");
+
+  // Resolve once the first message is received.
+  let onMessageReceived = new Promise(resolve => {
+    function receiveMessages(messages) {
+      for (let m of messages) {
+        resolve(m);
+      }
+    }
+    webconsole.hud.ui.once("new-messages", receiveMessages);
+  });
+
+  // Load a frame script using a data URI so we can do logs
+  // from the page.
+  messageManager.loadFrameScript("data:,(" + encodeURIComponent(
+    `function () {
+      addMessageListener("do-dir", function () {
+        content.console.dir(Array.from({length:1000}).reduce((res, _, i)=> {
+          res["item_" + i] = i;
+          return res;
+        }, {}));
+      });
+    }`
+  ) + ")()", true);
+
+  // Kick off the logging
+  messageManager.sendAsyncMessage("do-dir");
+
+  let test = runTest("console.objectexpand");
+  await onMessageReceived;
+  const tree = webconsole.hud.ui.outputNode.querySelector(".dir.message .tree");
+
+  // The tree can be collapsed since the properties are fetched asynchronously.
+  if (tree.querySelectorAll(".node").length === 1) {
+    // If this is the case, we wait for the properties to be fetched and displayed.
+    await new Promise(resolve => {
+      const observer = new (getBrowserWindow().MutationObserver)(mutations => {
+        resolve(mutations);
+        observer.disconnect();
+      });
+      observer.observe(tree, {
+        childList: true
+      });
+    });
+  }
+
+  test.done();
+
+  await closeToolboxAndLog("console.objectexpanded", toolbox);
+
+  await testTeardown();
+};
+
+
