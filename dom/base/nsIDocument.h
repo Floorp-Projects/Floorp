c672f396d6b6e7bb00707829d53cb3f1097e5de7
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-10 05:46:26 +0100

    Bug 1444580: Mark nsIDocument::Dispatch final. r=smaug
    MozReview-Commit-ID: 1xAE0WF8IFN

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -3210,8 +3210,8 @@ public:
   void NotifyIntersectionObservers();
 
   // Dispatch a runnable related to the document.
-  virtual nsresult Dispatch(mozilla::TaskCategory aCategory,
-                            already_AddRefed<nsIRunnable>&& aRunnable) override;
+  nsresult Dispatch(mozilla::TaskCategory aCategory,
+                    already_AddRefed<nsIRunnable>&& aRunnable) final;
 
   virtual nsISerialEventTarget*
   EventTargetFor(mozilla::TaskCategory aCategory) const override;
