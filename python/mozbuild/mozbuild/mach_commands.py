29234a852563e159080b8b54ad24e442f8f3171a
Geoff Lankow <geoff@darktrojan.net>
2018-03-11 21:55:45 +1300

    Bug 1444219 - Remove unnecessary call to MozbuildObject.from_environment r=froydnj
    MozReview-Commit-ID: 4KP2H9HVQbG
    
    --HG--
    extra : rebase_source : b2a7a8b8c382ad00487081c3ec99b7dc095283ba

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -1172,16 +1172,13 @@ class PackageFrontend(MachCommandBase):
         state_dir = self._mach_context.state_dir
         cache_dir = os.path.join(state_dir, 'package-frontend')
 
-        here = os.path.abspath(os.path.dirname(__file__))
-        build_obj = MozbuildObject.from_environment(cwd=here)
-
         hg = None
-        if conditions.is_hg(build_obj):
-            hg = build_obj.substs['HG']
+        if conditions.is_hg(self):
+            hg = self.substs['HG']
 
         git = None
-        if conditions.is_git(build_obj):
-            git = build_obj.substs['GIT']
+        if conditions.is_git(self):
+            git = self.substs['GIT']
 
         from mozbuild.artifacts import Artifacts
         artifacts = Artifacts(tree, self.substs, self.defines, job,
