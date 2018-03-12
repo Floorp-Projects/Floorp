9ac683cbb953e117afdb1c2ea7015eedbed97c57
Gabriel Luong <gabriel.luong@gmail.com>
2018-03-12 12:25:15 -0400

    Bug 1351685 - Remove the box model from the computed view. r=pbro

--- devtools/client/inspector/boxmodel/test/browser_boxmodel_pseudo-element.js
+++ devtools/client/inspector/boxmodel/test/browser_boxmodel_pseudo-element.js
@@ -99,21 +99,21 @@ const res1 = [
 
 add_task(function* () {
   yield addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
-  let {inspector, view} = yield openBoxModelView();
+  let {inspector, boxmodel} = yield openLayoutView();
   let node = yield getNodeFront("div", inspector);
   let children = yield inspector.markup.walker.children(node);
   let beforeElement = children.nodes[0];
 
   yield selectNode(beforeElement, inspector);
-  yield testInitialValues(inspector, view);
+  yield testInitialValues(inspector, boxmodel);
 });
 
-function* testInitialValues(inspector, view) {
+function* testInitialValues(inspector, boxmodel) {
   info("Test that the initial values of the box model are correct");
-  let viewdoc = view.document;
+  let doc = boxmodel.document;
 
   for (let i = 0; i < res1.length; i++) {
-    let elt = viewdoc.querySelector(res1[i].selector);
+    let elt = doc.querySelector(res1[i].selector);
     is(elt.textContent, res1[i].value,
        res1[i].selector + " has the right value.");
   }
