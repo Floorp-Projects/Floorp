e179330e70866705d4ae5aab043634768ca88849
Cosmin Sabou <csabou@mozilla.com>
2018-03-12 19:06:22 +0200

    Backed out changeset 57ba784a1cd9 (bug 1351685) for ESlint failure on browser_boxmodel_computed-accordion-state.js

--- devtools/client/inspector/boxmodel/test/head.js
+++ devtools/client/inspector/boxmodel/test/head.js
@@ -46,6 +46,36 @@ function isNodeVisible(node) {
 }
 
 /**
+ * Open the toolbox, with the inspector tool visible, and the computed view
+ * sidebar tab selected to display the box model view.
+ *
+ * @return {Promise} a promise that resolves when the inspector is ready and the box model
+ *         view is visible and ready.
+ */
+function openBoxModelView() {
+  return openInspectorSidebarTab("computedview").then(data => {
+    // The actual highligher show/hide methods are mocked in box model tests.
+    // The highlighter is tested in devtools/inspector/test.
+    function mockHighlighter({highlighter}) {
+      highlighter.showBoxModel = function () {
+        return promise.resolve();
+      };
+      highlighter.hideBoxModel = function () {
+        return promise.resolve();
+      };
+    }
+    mockHighlighter(data.toolbox);
+
+    return {
+      toolbox: data.toolbox,
+      inspector: data.inspector,
+      view: data.inspector.getPanel("computedview"),
+      testActor: data.testActor
+    };
+  });
+}
+
+/**
  * Wait for the boxmodel-view-updated event.
  *
  * @param  {InspectorPanel} inspector
