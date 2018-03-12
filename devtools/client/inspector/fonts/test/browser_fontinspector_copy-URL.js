dde580f2a6866917be2b6a527117098a766380cf
Patrick Brosset <pbrosset@mozilla.com>
2018-03-12 14:49:14 +0100

    Bug 1444327 - Bring back ability to copy font URL by adding context menu; r=jdescottes
    In the fonts panel UI prior to Firefox 60, remote font URLs used to be
    displayed in full in a text input field. It made it easy to copy them.
    With the redesign that happened in 60 (bug 1437548 and 1442001), getting
    the URL became harder. The URL isn't visible anymore easily. There's a link
    that can be clicked to load the URL in the browser, or it can also be copied
    from the @font-face CSS rule code section. But that's harder.
    
    This change adds a simple right-click context menu to the link that has
    one item only: copy link.
    
    MozReview-Commit-ID: 2oSMoWKYhTk
    
    --HG--
    extra : rebase_source : 0fa26ea6846924a270f14c1e0258dbf21e122e6f

---
+++ devtools/client/inspector/fonts/test/browser_fontinspector_copy-URL.js
@@ -0,0 +1,48 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that a context menu appears when right-clicking on a web font URL, and that the
+// URL can be copied to the clipboard thanks to it.
+
+const TEST_URI = URL_ROOT + "browser_fontinspector.html";
+
+add_task(async function () {
+  let { view, toolbox } = await openFontInspectorForURL(TEST_URI);
+  let viewDoc = view.document;
+  let win = viewDoc.defaultView;
+
+  let fontEl = getUsedFontsEls(viewDoc)[0];
+  let linkEl = fontEl.querySelector(".font-url");
+
+  // This is the container for all our popup menus in the toolbox. At this time, the
+  // copy menu (xul <menupopup> element) does not exist inside it yet.
+  let popupset = toolbox.doc.querySelector("popupset");
+
+  // It is only created when the following event is received in the Font component.
+  info("Right-clicking on the first font's link");
+  EventUtils.synthesizeMouse(linkEl, 2, 2, {type: "contextmenu", button: 2}, win);
+
+  // Now the menu exist and is in the process of getting shown. But since this happen
+  // asynchronously, it is fine only starting to listen for the popupshown event only now.
+  // Anyway, it isn't possible to start listening prior to this since the element does not
+  // exist before.
+  info("Waiting for the new menu to open");
+  let menu = popupset.querySelector("menupopup[menu-api]");
+  await once(menu, "popupshown");
+
+  let items = menu.querySelectorAll("menuitem");
+  is(items.length, 1, "There's only one menu item displayed");
+  is(items[0].getAttribute("label"), "Copy Link", "This is the right menu item");
+
+  info("Clicking the menu item and waiting for the clipboard to receive the URL");
+  let expected = linkEl.href;
+  await waitForClipboardPromise(() => items[0].click(), expected);
+
+  info("Closing the menu");
+  let onHidden = once(menu, "popuphidden");
+  menu.hidePopup();
+  await onHidden;
+});
