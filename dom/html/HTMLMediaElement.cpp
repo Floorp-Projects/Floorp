c9d40a1eaf1de4aa9c50e8d716abd8d23cae7626
Chris Pearce <cpearce@mozilla.com>
2018-03-12 13:05:04 +1300

    Bug 1434804 - Pause autoplayed media if they become audible. r=kamidphish
    Our autoplay blocking is trivial to defeat; just mute or volume=0 a video,
    play(), and then unmute, and then you're playing audibly.
    
    So this patch makes us pause() media that become audible atfter playback
    has started.
    
    MozReview-Commit-ID: 2RAtbohMGJO

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -2971,6 +2971,10 @@ HTMLMediaElement::SetVolume(double aVolume, ErrorResult& aRv)
   SetVolumeInternal();
 
   DispatchAsyncEvent(NS_LITERAL_STRING("volumechange"));
+
+  // We allow inaudible autoplay. But changing our volume may make this
+  // media audible. So pause if we are no longer supposed to be autoplaying.
+  PauseIfShouldNotBePlaying();
 }
 
 void
@@ -3020,6 +3024,18 @@ HTMLMediaElement::SetMutedInternal(uint32_t aMuted)
 }
 
 void
+HTMLMediaElement::PauseIfShouldNotBePlaying()
+{
+  if (GetPaused()) {
+    return;
+  }
+  if (!AutoplayPolicy::IsMediaElementAllowedToPlay(WrapNotNull(this))) {
+    ErrorResult rv;
+    Pause(rv);
+  }
+}
+
+void
 HTMLMediaElement::SetVolumeInternal()
 {
   float effectiveVolume = ComputedVolume();
@@ -3051,6 +3067,10 @@ HTMLMediaElement::SetMuted(bool aMuted)
   }
 
   DispatchAsyncEvent(NS_LITERAL_STRING("volumechange"));
+
+  // We allow inaudible autoplay. But changing our mute status may make this
+  // media audible. So pause if we are no longer supposed to be autoplaying.
+  PauseIfShouldNotBePlaying();
 }
 
 class HTMLMediaElement::StreamCaptureTrackSource :
