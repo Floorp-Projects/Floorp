9e35860596eda7f99bde581d2c3e06714f19c4d0
Lee Salzman <lsalzman@mozilla.com>
2018-03-12 16:37:10 -0400

    Bug 1444506 - part 3 - mozbuild fixes for Skia m66 update. r=jrmuizel

--- gfx/skia/generate_mozbuild.py
+++ gfx/skia/generate_mozbuild.py
@@ -34,14 +34,16 @@ if CONFIG['MOZ_OPTIMIZE']:
 footer = """
 
 # We allow warnings for third-party code that can be updated from upstream.
-ALLOW_COMPILER_WARNINGS = True
+AllowCompilerWarnings()
 
 FINAL_LIBRARY = 'gkmedias'
 LOCAL_INCLUDES += [
     'skia/include/c',
+    'skia/include/codec',
     'skia/include/config',
     'skia/include/core',
     'skia/include/effects',
+    'skia/include/encode',
     'skia/include/gpu',
     'skia/include/pathops',
     'skia/include/ports',
@@ -58,6 +60,7 @@ LOCAL_INCLUDES += [
     'skia/src/lazy',
     'skia/src/opts',
     'skia/src/sfnt',
+    'skia/src/shaders',
     'skia/src/sksl',
     'skia/src/utils',
     'skia/src/utils/mac',
@@ -66,7 +69,7 @@ LOCAL_INCLUDES += [
 
 if CONFIG['MOZ_WIDGET_TOOLKIT'] == 'windows':
     if CONFIG['CC_TYPE'] == 'gcc':
-        DEFINES['SK_JUMPER_USE_ASSEMBLY'] = False
+        DEFINES['SK_JUMPER_USE_ASSEMBLY'] = 0
     elif CONFIG['CPU_ARCH'] == 'x86':
         SOURCES['skia/src/jumper/SkJumper_generated_win.S'].flags += ['-safeseh']
     DEFINES['UNICODE'] = True
@@ -75,6 +78,9 @@ if CONFIG['MOZ_WIDGET_TOOLKIT'] == 'windows':
         'skia/src/fonts/SkFontMgr_indirect.cpp',
         'skia/src/fonts/SkRemotableFontMgr.cpp',
     ]
+elif CONFIG['MOZ_WIDGET_TOOLKIT'] in ('cocoa', 'uikit'):
+    # Workaround for buggy macOS drivers.
+    DEFINES['SK_DISABLE_EXPLICIT_GPU_RESOURCE_ALLOCATION'] = 1
 
 # We should autogenerate these SSE related flags.
 
@@ -174,9 +180,12 @@ def generate_opt_sources():
 
 def generate_platform_sources():
   sources = {}
-
+  platform_args = {
+    'win' : 'win_vc="C:/" win_sdk_version="00.0.00000.0"'
+  }
   for plat in platforms:
-    output = subprocess.check_output('cd skia && bin/gn gen out/{0} --args=\'target_os="{0}"\' > /dev/null && bin/gn desc out/{0} :skia sources'.format(plat), shell=True)
+    args = platform_args.get(plat, '')
+    output = subprocess.check_output('cd skia && bin/gn gen out/{0} --args=\'target_os="{0}" {1}\' > /dev/null && bin/gn desc out/{0} :skia sources'.format(plat, args), shell=True)
     if output:
       sources[plat] = parse_sources(output)
 
@@ -191,24 +200,21 @@ def generate_platform_sources():
 
 def generate_separated_sources(platform_sources):
   blacklist = [
-    'GrGLCreateNativeInterface',
+    'GrGLMakeNativeInterface',
     'GrGLCreateNullInterface',
     'GrGLAssembleInterface',
     'GrGLTestInterface',
-    'ImageEncoder',
+    'skia/src/android/',
+    'skia/src/atlastext/',
     'skia/src/c/',
     'skia/src/effects/Gr',
     'skia/src/effects/Sk',
     'skia/src/fonts/',
-    'skia/src/images/',
     'skia/src/jumper/SkJumper_generated_win.S',
+    'skia/src/ports/SkImageEncoder',
     'skia/src/ports/SkImageGenerator',
     'skia/src/gpu/vk/',
     'SkBitmapRegion',
-    'SkLight',
-    'SkRadialShadow',
-    'SkShadow',
-    'SkNormal',
     'SkLite',
     'codec',
     'SkWGL',
@@ -246,13 +252,14 @@ def generate_separated_sources(platform_sources):
     'common': {
       'skia/src/core/SkBlurImageFilter.cpp',
       'skia/src/core/SkGpuBlurUtils.cpp',
+      'skia/src/effects/GrCircleBlurFragmentProcessor.cpp',
+      'skia/src/effects/SkBlurMask.cpp',
+      'skia/src/effects/SkBlurMaskFilter.cpp',
       'skia/src/effects/SkDashPathEffect.cpp',
       'skia/src/effects/SkImageSource.cpp',
-      'skia/src/effects/SkLayerRasterizer.cpp',
-      'skia/src/gpu/gl/GrGLCreateNativeInterface_none.cpp',
+      'skia/src/gpu/gl/GrGLMakeNativeInterface_none.cpp',
       'skia/src/ports/SkDiscardableMemory_none.cpp',
       'skia/src/ports/SkMemory_mozalloc.cpp',
-      'skia/src/ports/SkImageEncoder_none.cpp',
       'skia/src/ports/SkImageGenerator_none.cpp',
     },
     'android': {
@@ -335,6 +342,7 @@ opt_whitelist = [
   'SkBlitMask',
   'SkBlitRow',
   'SkBlitter',
+  'SkJumper',
   'SkSpriteBlitter',
   'SkMatrix.cpp',
 ]
@@ -351,13 +359,16 @@ unified_blacklist = [
   'SkBlitter_Sprite.cpp',
   'SkScan_Antihair.cpp',
   'SkScan_AntiPath.cpp',
+  'SkScan_DAAPath.cpp',
   'SkParse.cpp',
   'SkPDFFont.cpp',
   'SkPictureData.cpp',
+  'skia/src/gpu/effects/',
   'GrResourceCache',
   'GrResourceProvider',
   'GrAA',
   'GrGL',
+  'GrCCPathProcessor',
   'GrMSAAPathRenderer.cpp',
   'GrNonAAFillRect',
   'GrPathUtils',
@@ -373,7 +384,9 @@ unified_blacklist = [
   'SkRTree.cpp',
   'SkVertices.cpp',
   'SkJumper',
-  'lex.layout.cpp',
+  'SkSLLexer.cpp',
+  'SkSLLayoutLexer.cpp',
+  'SkThreadedBMPDevice.cpp',
 ] + opt_whitelist
 
 def write_sources(f, values, indent):
