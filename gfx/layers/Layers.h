95f1424c1223b4938060c94d3a88fb7537e20af4
Kartikaya Gupta <kgupta@mozilla.com>
2018-03-12 16:06:37 -0400

    Bug 1443792 - Tighten the equivalence between a layer being scrollable and having an APZC. r=botond
    If we can assume that a layer being composited has an APZC at index i if and
    only if the frame metrics at index i is scrollable, then we can do the
    transformations in the next patch without any change in functionality.
    
    MozReview-Commit-ID: FRkvhwdd3nh

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -1798,6 +1798,10 @@ public:
   // and can be used anytime.
   // A layer has an APZC at index aIndex only-if GetFrameMetrics(aIndex).IsScrollable();
   // attempting to get an APZC for a non-scrollable metrics will return null.
+  // The reverse is also true (that if GetFrameMetrics(aIndex).IsScrollable()
+  // is true, then the layer will have an APZC), although that only holds on
+  // the compositor-side layer tree, and only after the APZ code has had a chance
+  // to rebuild its internal hit-testing tree using the layer tree.
   // The aIndex for these functions must be less than GetScrollMetadataCount().
   void SetAsyncPanZoomController(uint32_t aIndex, AsyncPanZoomController *controller);
   AsyncPanZoomController* GetAsyncPanZoomController(uint32_t aIndex) const;
