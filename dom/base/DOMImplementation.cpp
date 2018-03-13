633cf0f02a18c05921979d49a51eb259478c3bc5
Boris Zbarsky <bzbarsky@mit.edu>
2018-03-13 16:24:01 -0400

    Bug 1445140 part 1.  Remove the XPCOM-ish version of NS_NewDOMDocumentType.  r=mystor
    MozReview-Commit-ID: 6sEFIfMgqet

--- dom/base/DOMImplementation.cpp
+++ dom/base/DOMImplementation.cpp
@@ -60,7 +60,7 @@ DOMImplementation::CreateDocumentType(const nsAString& aQualifiedName,
   // Indicate that there is no internal subset (not just an empty one)
   RefPtr<DocumentType> docType =
     NS_NewDOMDocumentType(mOwner->NodeInfoManager(), name, aPublicId,
-                          aSystemId, VoidString(), aRv);
+                          aSystemId, VoidString());
   return docType.forget();
 }
 
@@ -141,16 +141,13 @@ DOMImplementation::CreateHTMLDocument(const nsAString& aTitle,
 
   NS_ENSURE_STATE(mOwner);
 
-  nsCOMPtr<nsIDOMDocumentType> doctype;
   // Indicate that there is no internal subset (not just an empty one)
-  nsresult rv = NS_NewDOMDocumentType(getter_AddRefs(doctype),
-                                      mOwner->NodeInfoManager(),
-                                      nsGkAtoms::html, // aName
-                                      EmptyString(), // aPublicId
-                                      EmptyString(), // aSystemId
-                                      VoidString()); // aInternalSubset
-  NS_ENSURE_SUCCESS(rv, rv);
-
+  RefPtr<DocumentType> doctype =
+    NS_NewDOMDocumentType(mOwner->NodeInfoManager(),
+                          nsGkAtoms::html, // aName
+                          EmptyString(), // aPublicId
+                          EmptyString(), // aSystemId
+                          VoidString()); // aInternalSubset
 
   nsCOMPtr<nsIGlobalObject> scriptHandlingObject =
     do_QueryReferent(mScriptObject);
@@ -158,12 +155,12 @@ DOMImplementation::CreateHTMLDocument(const nsAString& aTitle,
   NS_ENSURE_STATE(!mScriptObject || scriptHandlingObject);
 
   nsCOMPtr<nsIDOMDocument> document;
-  rv = NS_NewDOMDocument(getter_AddRefs(document),
-                         EmptyString(), EmptyString(),
-                         doctype, mDocumentURI, mBaseURI,
-                         mOwner->NodePrincipal(),
-                         true, scriptHandlingObject,
-                         DocumentFlavorLegacyGuess);
+  nsresult rv = NS_NewDOMDocument(getter_AddRefs(document),
+                                  EmptyString(), EmptyString(),
+                                  doctype, mDocumentURI, mBaseURI,
+                                  mOwner->NodePrincipal(),
+                                  true, scriptHandlingObject,
+                                  DocumentFlavorLegacyGuess);
   NS_ENSURE_SUCCESS(rv, rv);
   nsCOMPtr<nsIDocument> doc = do_QueryInterface(document);
 
