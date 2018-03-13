5931fb1728b244e448a0e091862f2c145886bee4
Vivek Dhingra <vivek3zero@gmail.com>
2018-03-13 18:20:08 -0400

    Bug 1397393 - Make transitioning between 2 themes with a dynamic theme smooth r=mconley
    MozReview-Commit-ID: Cjx0E2PN6GP
    
    --HG--
    extra : rebase_source : ea7ef2005de44ebb8b8e56b1acf47b6aac6ed863

--- toolkit/components/extensions/test/browser/head.js
+++ toolkit/components/extensions/test/browser/head.js
@@ -1,5 +1,6 @@
 /* exported ACCENT_COLOR, BACKGROUND, ENCODED_IMAGE_DATA, FRAME_COLOR, TAB_TEXT_COLOR,
-   TEXT_COLOR, TAB_BACKGROUND_TEXT_COLOR, imageBufferFromDataURI, hexToCSS, hexToRGB, testBorderColor */
+   TEXT_COLOR, TAB_BACKGROUND_TEXT_COLOR, imageBufferFromDataURI, hexToCSS, hexToRGB, testBorderColor,
+   waitForTransition */
 
 "use strict";
 
@@ -52,6 +53,12 @@ function hexToCSS(hex) {
 function imageBufferFromDataURI(encodedImageData) {
   let decodedImageData = atob(encodedImageData);
   return Uint8Array.from(decodedImageData, byte => byte.charCodeAt(0)).buffer;
+}
+
+function waitForTransition(element, propertyName) {
+  return BrowserTestUtils.waitForEvent(element, "transitionend", false, event => {
+    return event.target == element && event.propertyName == propertyName;
+  });
 }
 
 function testBorderColor(element, expected) {
