7808a0cd20a6f7108279fa54baba7f229f926a11
Margareta Eliza Balazs <ebalazs@mozilla.com>
2018-03-13 07:14:32 +0200

    Backed out 2 changesets (bug 1443942) for mda assertion failures in /build/build/src/dom/media/ChannelMediaResource.cpp
    Backed out changeset e2f69088b1d7 (bug 1443942)
    Backed out changeset 446bfe8412cb (bug 1443942)

--- dom/media/ChannelMediaDecoder.cpp
+++ dom/media/ChannelMediaDecoder.cpp
@@ -167,9 +167,9 @@ ChannelMediaDecoder::NotifyPrincipalChanged()
     return;
   }
   if (!mSameOriginMedia &&
-      Preferences::GetBool("media.block-midflight-redirects", true)) {
-    // Block mid-flight redirects to non CORS same origin destinations.
-    // See bugs 1441153, 1443942.
+      DecoderTraits::CrossOriginRedirectsProhibited(ContainerType())) {
+    // For some content types we block mid-flight channel redirects to cross
+    // origin destinations due to security constraints. See bug 1441153.
     LOG("ChannnelMediaDecoder prohibited cross origin redirect blocked.");
     NetworkError(MediaResult(NS_ERROR_DOM_BAD_URI,
                              "Prohibited cross origin redirect blocked"));
