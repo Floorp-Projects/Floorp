0a049fdcc06e27e50c3c54aae2cf5387a3bdc9b5
wambui <wambui.dev@gmail.com>
2018-03-13 11:56:13 +0300

    Bug 1400233 - Drop ContentWebElement.LegacyIdentifier key from Marionette; r=ato
    Remove the legacy key that Marionette uses in addition to the identifier key.
    
    MozReview-Commit-ID: 6IVtLxOJPoS

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -1452,7 +1452,6 @@ class WebElement {
     for (let key of keys) {
       switch (key) {
         case ContentWebElement.Identifier:
-        case ContentWebElement.LegacyIdentifier:
           return ContentWebElement.fromJSON(json);
 
         case ContentWebWindow.Identifier:
@@ -1511,8 +1510,7 @@ class WebElement {
 
   /**
    * Checks if <var>ref<var> is a {@link WebElement} reference,
-   * i.e. if it has {@link ContentWebElement.Identifier},
-   * {@link ContentWebElement.LegacyIdentifier}, or
+   * i.e. if it has {@link ContentWebElement.Identifier}, or
    * {@link ChromeWebElement.Identifier} as properties.
    *
    * @param {Object.<string, string>} obj
@@ -1526,7 +1524,6 @@ class WebElement {
     }
 
     if ((ContentWebElement.Identifier in obj) ||
-        (ContentWebElement.LegacyIdentifier in obj) ||
         (ContentWebWindow.Identifier in obj) ||
         (ContentWebFrame.Identifier in obj) ||
         (ChromeWebElement.Identifier in obj)) {
@@ -1556,24 +1553,22 @@ class ContentWebElement extends WebElement {
   toJSON() {
     return {
       [ContentWebElement.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
     };
   }
 
   static fromJSON(json) {
-    const {Identifier, LegacyIdentifier} = ContentWebElement;
+    const {Identifier} = ContentWebElement;
 
-    if (!(Identifier in json) && !(LegacyIdentifier in json)) {
+    if (!(Identifier in json)) {
       throw new InvalidArgumentError(
           pprint`Expected web element reference, got: ${json}`);
     }
 
-    let uuid = json[Identifier] || json[LegacyIdentifier];
+    let uuid = json[Identifier];
     return new ContentWebElement(uuid);
   }
 }
 ContentWebElement.Identifier = "element-6066-11e4-a52e-4f735466cecf";
-ContentWebElement.LegacyIdentifier = "ELEMENT";
 this.ContentWebElement = ContentWebElement;
 
 /**
@@ -1585,7 +1580,6 @@ class ContentWebWindow extends WebElement {
   toJSON() {
     return {
       [ContentWebWindow.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
     };
   }
 
@@ -1610,7 +1604,6 @@ class ContentWebFrame extends WebElement {
   toJSON() {
     return {
       [ContentWebFrame.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
     };
   }
 
@@ -1634,7 +1627,6 @@ class ChromeWebElement extends WebElement {
   toJSON() {
     return {
       [ChromeWebElement.Identifier]: this.uuid,
-      [ContentWebElement.LegacyIdentifier]: this.uuid,
     };
   }
 
