04900e278cfbfbc85133193ebb05a75b549ac6f8
Alex Gaynor <agaynor@mozilla.com>
2018-03-13 12:54:35 -0400

    Bug 1445249 - Part 2 - avoid calling OtherPid() where it might return an invalid pid on android; r=jld
    MozReview-Commit-ID: EXio3oNJy4U

--- ipc/glue/ProtocolUtils.cpp
+++ ipc/glue/ProtocolUtils.cpp
@@ -597,6 +597,12 @@ IToplevelProtocol::~IToplevelProtocol()
 base::ProcessId
 IToplevelProtocol::OtherPid() const
 {
+  return OtherPidMaybeInvalid();
+}
+
+base::ProcessId
+IToplevelProtocol::OtherPidMaybeInvalid() const
+{
   return mOtherPid;
 }
 
@@ -721,8 +727,19 @@ IToplevelProtocol::CreateSharedMemory(size_t aSize,
     Shmem::IHadBetterBeIPDLCodeCallingThis_OtherwiseIAmADoodyhead(),
     segment.get(),
     id);
+
+  base::ProcessId pid =
+#ifdef ANDROID
+    // We use OtherPidMaybeInvalid() because on Android this method is actually
+    // called on an unconnected protocol, but Android's shared memory
+    // implementation doesn't actually use the PID.
+    OtherPidMaybeInvalid();
+#else
+    OtherPid();
+#endif
+
   Message* descriptor = shmem.ShareTo(
-    Shmem::IHadBetterBeIPDLCodeCallingThis_OtherwiseIAmADoodyhead(), OtherPid(), MSG_ROUTING_CONTROL);
+    Shmem::IHadBetterBeIPDLCodeCallingThis_OtherwiseIAmADoodyhead(), pid, MSG_ROUTING_CONTROL);
   if (!descriptor) {
     return nullptr;
   }
