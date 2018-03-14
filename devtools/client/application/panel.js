d8d3951cb51b6bc03c869f4292d33070ca5543d6
Julian Descottes <jdescottes@mozilla.com>
2018-03-14 21:21:02 +0100

    Bug 1445197 - part 1: Create an application panel for DevTools;r=Honza,nchevobbe,sole
    Register a new application panel, disabled by default, with no icon, no shortcut
    and hidden from the devtools options.
    
    Set devtools.application.enabled to true to enable it.
    
    MozReview-Commit-ID: L5PXZVDCRlF
    
    --HG--
    extra : rebase_source : 465dd27241e81ee2f35794ce01cf0d06939e58f6

---
+++ devtools/client/application/panel.js
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/**
+ * DevTools panel responsible for the application tool, which lists and allows to debug
+ * service workers.
+ */
+class ApplicationPanel {
+  /**
+   * Constructor.
+   *
+   * @param {Window} panelWin
+   *        The frame/window dedicated to this panel.
+   * @param {Toolbox} toolbox
+   *        The toolbox instance responsible for this panel.
+   */
+  constructor(panelWin, toolbox) {
+    this.panelWin = panelWin;
+    this.toolbox = toolbox;
+  }
+
+  async open() {
+    if (!this.toolbox.target.isRemote) {
+      await this.toolbox.target.makeRemote();
+    }
+
+    this.panelWin.Application.bootstrap({
+      toolbox: this.toolbox,
+      panel: this,
+    });
+    this.emit("ready");
+    this.isReady = true;
+    return this;
+  }
+
+  destroy() {
+    this.panelWin.Application.destroy();
+    this.panelWin = null;
+    this.toolbox = null;
+    this.emit("destroyed");
+
+    return this;
+  }
+}
+
+exports.ApplicationPanel = ApplicationPanel;
