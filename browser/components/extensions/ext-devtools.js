8007c0babccb264ccc8e4ae6d0b4ca0bf3bccc36
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-03-14 18:12:17 +0100

    Bug 1382581 - Adapt extension code to the EventEmitter change in devtools/client/framework; r=rpl.
    MozReview-Commit-ID: 3I7kmXiPOqd

--- browser/components/extensions/ext-devtools.js
+++ browser/components/extensions/ext-devtools.js
@@ -237,7 +237,7 @@ class DevToolsPageDefinition {
     this.devtoolsPageForTarget = new Map();
   }
 
-  onThemeChanged(evt, themeName) {
+  onThemeChanged(themeName) {
     Services.ppmm.broadcastAsyncMessage("Extension:DevToolsThemeChanged", {themeName});
   }
 
@@ -304,7 +304,7 @@ initDevTools = function() {
   /* eslint-disable mozilla/balanced-listeners */
   // Create a devtools page context for a new opened toolbox,
   // based on the registered devtools_page definitions.
-  DevToolsShim.on("toolbox-created", (evt, toolbox) => {
+  DevToolsShim.on("toolbox-created", toolbox => {
     if (!toolbox.target.isLocalTab) {
       // Only local tabs are currently supported (See Bug 1304378 for additional details
       // related to remote targets support).
@@ -325,7 +325,7 @@ initDevTools = function() {
 
   // Destroy a devtools page context for a destroyed toolbox,
   // based on the registered devtools_page definitions.
-  DevToolsShim.on("toolbox-destroy", (evt, target) => {
+  DevToolsShim.on("toolbox-destroy", target => {
     if (!target.isLocalTab) {
       // Only local tabs are currently supported (See Bug 1304378 for additional details
       // related to remote targets support).
