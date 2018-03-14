010c3e8396c1b4c3a1d8a2dd74a8c645f6468b11
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-03-14 20:02:11 +0000

    Bug 1443870 - Update tests for lack of content-process blocklist. r=florian, a=jcristau
    MozReview-Commit-ID: 4PyUDd6BNsM

--- browser/base/content/test/plugins/head.js
+++ browser/base/content/test/plugins/head.js
@@ -9,22 +9,6 @@ ChromeUtils.defineModuleGetter(this, "PromiseUtils",
 // default browser under test in some of the functions below.
 /* global gTestBrowser */
 
-// The blocklist shim running in the content process does not initialize at
-// start up, so it's not active until we load content that needs to do a
-// check. This helper bypasses the delay to get the svc up and running
-// immediately. Note, call this after remote content has loaded.
-function promiseInitContentBlocklistSvc(aBrowser) {
-  return ContentTask.spawn(aBrowser, {}, async function() {
-    try {
-      // eslint-disable-next-line no-unused-expressions
-      Services.blocklist;
-    } catch (ex) {
-      return ex.message;
-    }
-    return null;
-  });
-}
-
 /**
   * Waits a specified number of miliseconds.
   *
@@ -237,10 +221,6 @@ async function asyncSetAndUpdateBlocklist(aURL, aBrowser) {
   }
   Services.prefs.setCharPref("extensions.blocklist.url", aURL);
   let localPromise = TestUtils.topicObserved("blocklist-updated");
-  let remotePromise;
-  if (doTestRemote) {
-    remotePromise = TestUtils.topicObserved("content-blocklist-updated");
-  }
   let blocklistNotifier = Cc["@mozilla.org/extensions/blocklist;1"]
                             .getService(Ci.nsITimerCallback);
   blocklistNotifier.notify(null);
@@ -248,7 +228,8 @@ async function asyncSetAndUpdateBlocklist(aURL, aBrowser) {
   await localPromise;
   if (doTestRemote) {
     info("*** waiting on remote load");
-    await remotePromise;
+    // Ensure content has been updated with the blocklist
+    await ContentTask.spawn(aBrowser, null, () => {});
   }
   info("*** blocklist loaded.");
 }
