7401efbcfa7a89ce94bf77cc19883612be0e1ad5
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-03-14 09:54:17 +0100

    Bug 1444823 - Execute messageManager.sendAsyncMessage after runTest; r=jdescottes.
    MozReview-Commit-ID: Ansa6okYRg

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/objectexpand.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/objectexpand.js
@@ -37,10 +37,10 @@ module.exports = async function() {
     }`
   ) + ")()", true);
 
+  let test = runTest("console.objectexpand");
   // Kick off the logging
   messageManager.sendAsyncMessage("do-dir");
 
-  let test = runTest("console.objectexpand");
   await onMessageReceived;
   const tree = webconsole.hud.ui.outputNode.querySelector(".dir.message .tree");
 
