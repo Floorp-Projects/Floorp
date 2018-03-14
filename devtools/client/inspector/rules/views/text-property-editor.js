7fc20a58f9e4f188386b8eb00aa4af24e574e91e
Andreea Pavel <apavel@mozilla.com>
2018-03-15 01:50:45 +0200

    Backed out 2 changesets (bug 1435373) for devtools failures at devtools/client/shared/test/browser_telemetry_button_eyedropper.js and devtools/client/inspector/rules/test/browser_rules_eyedropper.js and devtools/client/inspector/test/browser_inspector_highlighter-cssshape_04.js on a CLOSED TREE
    Backed out changeset 195379cf14f0 (bug 1435373)
    Backed out changeset 14a22276dc53 (bug 1435373)

--- devtools/client/inspector/rules/views/text-property-editor.js
+++ devtools/client/inspector/rules/views/text-property-editor.js
@@ -27,6 +27,7 @@ const COLOR_SWATCH_CLASS = "ruleview-colorswatch";
 const BEZIER_SWATCH_CLASS = "ruleview-bezierswatch";
 const FILTER_SWATCH_CLASS = "ruleview-filterswatch";
 const ANGLE_SWATCH_CLASS = "ruleview-angleswatch";
+const INSET_POINT_TYPES = ["top", "right", "bottom", "left"];
 const FONT_FAMILY_CLASS = "ruleview-font-family";
 const SHAPE_SWATCH_CLASS = "ruleview-shapeswatch";
 
@@ -93,10 +94,9 @@ function TextPropertyEditor(ruleEditor, property) {
   this._onSwatchPreview = this._onSwatchPreview.bind(this);
   this._onSwatchRevert = this._onSwatchRevert.bind(this);
   this._onValidate = this.ruleView.debounce(this._previewValue, 10, this);
-  this.onInContextEditorCommit = this.onInContextEditorCommit.bind(this);
-  this.onInContextEditorPreview = this.onInContextEditorPreview.bind(this);
   this.update = this.update.bind(this);
   this.updatePropertyState = this.updatePropertyState.bind(this);
+  this._onHoverShapePoint = this._onHoverShapePoint.bind(this);
 
   this._create();
   this.update();
@@ -319,6 +319,8 @@ TextPropertyEditor.prototype = {
         cssProperties: this.cssProperties,
         cssVariables: this.rule.elementStyle.variables,
       });
+
+      this.ruleView.highlighters.on("hover-shape-point", this._onHoverShapePoint);
     }
   },
 
@@ -339,7 +341,7 @@ TextPropertyEditor.prototype = {
   /**
    * Populate the span based on changes to the TextProperty.
    */
-  update: async function() {
+  update: function() {
     if (this.ruleView.isDestroyed) {
       return;
     }
@@ -514,22 +516,11 @@ TextPropertyEditor.prototype = {
       }).join("");
       shapeToggle.setAttribute("data-mode", mode);
 
-      try {
-        let shapesEditor =
-          await this.ruleView.highlighters.getInContextEditor("shapesEditor");
-        if (!shapesEditor) {
-          return;
-        }
-
-        shapesEditor.link(this.prop, shapeToggle, {
-          onPreview: this.onInContextEditorPreview,
-          onCommit: this.onInContextEditorCommit
-        });
-        // Mark this toggle if this property is being currently edited; unmark otherwise.
-        shapeToggle.classList.toggle("active", shapesEditor.activeProperty === this.prop);
-      } catch (err) {
-        // Remove toggle and, with it, any expectations of triggering an editor.
-        shapeToggle.remove();
+      let { highlighters, inspector } = this.ruleView;
+      if (highlighters.shapesHighlighterShown === inspector.selection.nodeFront &&
+          highlighters.state.shapes.options.mode === mode) {
+        shapeToggle.classList.add("active");
+        highlighters.highlightRuleViewShapePoint(highlighters.state.shapes.hoverPoint);
       }
     }
 
@@ -1044,24 +1035,65 @@ TextPropertyEditor.prototype = {
   },
 
   /**
-   * Live preview this property, without committing changes.
+   * Highlight the given shape point in the rule view. Called when "hover-shape-point"
+   * event is emitted.
    *
-   * @param {String} value
-   *        The value to set the current property to.
+   * @param {Event} event
+   *        The "hover-shape-point" event.
+   * @param {String} point
+   *        The point to highlight.
    */
-  onInContextEditorPreview(value) {
-    this.ruleEditor.rule.previewPropertyValue(this.prop, value);
+  _onHoverShapePoint: function(event, point) {
+    // If there is no shape toggle, or it is not active, return.
+    let shapeToggle = this.valueSpan.querySelector(".ruleview-shapeswatch.active");
+    if (!shapeToggle) {
+      return;
+    }
+
+    let view = this.ruleView;
+    let { highlighters } = view;
+    let ruleViewEl = view.element;
+    let selector = `.ruleview-shape-point.active`;
+    for (let pointNode of ruleViewEl.querySelectorAll(selector)) {
+      this._toggleShapePointActive(pointNode, false);
+    }
+
+    if (typeof point === "string") {
+      if (point.includes(",")) {
+        point = point.split(",")[0];
+      }
+      // Because one inset value can represent multiple points, inset points use classes
+      // instead of data.
+      selector = (INSET_POINT_TYPES.includes(point)) ?
+                 `.ruleview-shape-point.${point}` :
+                 `.ruleview-shape-point[data-point='${point}']`;
+      for (let pointNode of this.valueSpan.querySelectorAll(selector)) {
+        let nodeInfo = view.getNodeInfo(pointNode);
+        if (highlighters.isRuleViewShapePoint(nodeInfo)) {
+          this._toggleShapePointActive(pointNode, true);
+        }
+      }
+    }
   },
 
   /**
-   * Commit this property value. Triggers editor update.
+   * Toggle the class "active" on the given shape point in the rule view if the current
+   * inspector selection is highlighted by the shapes highlighter.
    *
-   * @param {String} value
-   *        The value to set the current property to.
+   * @param {NodeFront} node
+   *        The NodeFront of the shape point to toggle
+   * @param {Boolean} active
+   *        Whether the shape point should be active
    */
-  onInContextEditorCommit(value) {
-    this.prop.setValue(value);
-  }
+  _toggleShapePointActive: function(node, active) {
+    let { highlighters } = this.ruleView;
+    if (highlighters.inspector.selection.nodeFront !=
+        highlighters.shapesHighlighterShown) {
+      return;
+    }
+
+    node.classList.toggle("active", active);
+  },
 };
 
 module.exports = TextPropertyEditor;
