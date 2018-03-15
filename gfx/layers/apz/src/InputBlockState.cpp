9ab9425d9f44d23a927cd128985c0b4129561758
Kartikaya Gupta <kgupta@mozilla.com>
2018-03-15 15:25:09 -0400

    Bug 1445662 - Make the DPI non-static and bound to the controller thread. r=rhunt
    Since we can have multiple browser windows on multiple different
    displays with different DPIs, it doesn't make sense to have a single
    static DPI value shared across all APZCTreeManagers. Instead, each
    APZCTM should store its own DPI value for the display the window is on.
    Since the DPI is only ever read from the controller thread, we can make
    it bound to that thread, and update the setter code to also set it on
    that thread.
    
    As with the previous patch, the change in APZCTreeManagerParent is a
    no-op but allows making some other thread in the GPU process the controller
    thread. And the change in nsBaseWidget is a no-op everywhere except
    Android.

--- gfx/layers/apz/src/InputBlockState.cpp
+++ gfx/layers/apz/src/InputBlockState.cpp
@@ -6,7 +6,6 @@
 
 #include "InputBlockState.h"
 
-#include "APZCTreeManager.h"                // for APZCTreeManager::GetDPI
 #include "AsyncPanZoomController.h"         // for AsyncPanZoomController
 #include "ScrollAnimationPhysics.h"         // for kScrollSeriesTimeoutMs
 #include "gfxPrefs.h"                       // for gfxPrefs
@@ -890,9 +889,13 @@ TouchBlockState::UpdateSlopState(const MultiTouchInput& aInput,
     return false;
   }
   if (mInSlop) {
-    ScreenCoord threshold = aApzcCanConsumeEvents
-        ? AsyncPanZoomController::GetTouchStartTolerance()
-        : ScreenCoord(gfxPrefs::APZTouchMoveTolerance() * APZCTreeManager::GetDPI());
+    ScreenCoord threshold = 0;
+    // If the target was confirmed to null then the threshold doesn't
+    // matter anyway since the events will never be processed.
+    if (const RefPtr<AsyncPanZoomController>& apzc = GetTargetApzc()) {
+      threshold = aApzcCanConsumeEvents ? apzc->GetTouchStartTolerance()
+                                        : apzc->GetTouchMoveTolerance();
+    }
     bool stayInSlop = (aInput.mType == MultiTouchInput::MULTITOUCH_MOVE) &&
         (aInput.mTouches.Length() == 1) &&
         ((aInput.mTouches[0].mScreenPoint - mSlopOrigin).Length() < threshold);
