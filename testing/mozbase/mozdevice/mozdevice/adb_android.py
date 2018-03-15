d8fdaf351c92338a78e5812b31201bcb73abce29
Geoff Brown <gbrown@mozilla.com>
2018-03-15 10:57:48 -0600

    Bug 1445940 - adb_android.py - use activity name org.mozilla.gecko.BrowserApp when launching Fennec, r=nalexander
    Originally landed on autophone github in bug 1237755.

--- testing/mozbase/mozdevice/mozdevice/adb_android.py
+++ testing/mozbase/mozdevice/mozdevice/adb_android.py
@@ -439,7 +439,7 @@ class ADBAndroid(ADBDevice):
         if extra_args:
             extras['args'] = " ".join(extra_args)
 
-        self.launch_application(app_name, ".App",
+        self.launch_application(app_name, "org.mozilla.gecko.BrowserApp",
                                 intent, url=url, extras=extras,
                                 wait=wait, fail_if_running=fail_if_running,
                                 timeout=timeout)
