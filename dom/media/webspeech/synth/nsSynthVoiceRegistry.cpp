a04c12a6812a6ad0627f4776a7f8bd0c854428dc
Dorel Luca <dluca@mozilla.com>
2018-03-17 01:30:37 +0200

    Backed out changeset 231448afb53d (bug 1446505) for Mochitest failure on dom/media/webspeech/synth/test/test_bfcache.html. CLOSED TREE
    --HG--
    extra : amend_source : db6c502c4fe939ee072114a60636d3d1cbd96f1d

--- dom/media/webspeech/synth/nsSynthVoiceRegistry.cpp
+++ dom/media/webspeech/synth/nsSynthVoiceRegistry.cpp
@@ -175,7 +175,8 @@ nsSynthVoiceRegistry::GetInstance()
     ClearOnShutdown(&gSynthVoiceRegistry);
     if (XRE_IsParentProcess()) {
       // Start up all speech synth services.
-      NS_CreateServicesFromCategory(NS_SPEECH_SYNTH_STARTED, nullptr, nullptr);
+      NS_CreateServicesFromCategory(NS_SPEECH_SYNTH_STARTED, nullptr,
+        NS_SPEECH_SYNTH_STARTED);
     }
   }
 
