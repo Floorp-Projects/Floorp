d376aa72b6ba2369cd4fe5f23f5803d820b93381
Yuki Hiroshi <yuki@clear-code.com>
2018-03-15 23:06:22 -0300

    Bug 1440573 - Policy: Disable safe mode. r=jimm,felipe
    This policy disables the safe-mode UI entry points. In addition, only on Windows when using GPO, it also disables entering Safe Mode by holding down the Shift Key

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_disable_safemode.js
@@ -0,0 +1,42 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+add_task(async function setup() {
+  await setupPolicyEngineWithJson({
+    "policies": {
+      "DisableSafeMode": true
+    }
+  });
+});
+
+add_task(async function test_help_menu() {
+  buildHelpMenu();
+  let safeModeMenu = document.getElementById("helpSafeMode");
+  is(safeModeMenu.getAttribute("disabled"), "true",
+     "The `Restart with Add-ons Disabled...` item should be disabled");
+});
+
+add_task(async function test_safemode_from_about_support() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:support", false);
+
+  await ContentTask.spawn(tab.linkedBrowser, null, async function() {
+    let button = content.document.getElementById("restart-in-safe-mode-button");
+    is(button.getAttribute("disabled"), "true",
+       "The `Restart with Add-ons Disabled...` button should be disabled");
+  });
+
+  await BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function test_safemode_from_about_profiles() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "about:profiles", false);
+
+  await ContentTask.spawn(tab.linkedBrowser, null, async function() {
+    let button = content.document.getElementById("restart-in-safe-mode-button");
+    is(button.getAttribute("disabled"), "true",
+       "The `Restart with Add-ons Disabled...` button should be disabled");
+  });
+
+  await BrowserTestUtils.removeTab(tab);
+});
