78a7dcc5bed6cbadd9e4c2eabcdd054f558242f2
Brindusan Cristian <cbrindusan@mozilla.com>
2018-03-18 10:32:39 +0200

    Backed out changeset c8543a491242 (bug 1445963) for ESlint failure on html_post-array-data-test-page.html. CLOSED TREE

--- devtools/client/netmonitor/src/utils/sort-utils.js
+++ devtools/client/netmonitor/src/utils/sort-utils.js
@@ -13,16 +13,6 @@ function sortObjectKeys(object) {
   if (object == null) {
     return null;
   }
-
-  if (Array.isArray(object)) {
-    for (let i = 0; i < object.length; i++) {
-      if (typeof object[i] === "object") {
-        object[i] = sortObjectKeys(object[i]);
-      }
-    }
-    return object;
-  }
-
   return Object.keys(object).sort(function(left, right) {
     return left.toLowerCase().localeCompare(right.toLowerCase());
   }).reduce((acc, key) => {
--- devtools/client/netmonitor/test/browser_net_params_sorted.js
+++ devtools/client/netmonitor/test/browser_net_params_sorted.js
@@ -6,8 +6,9 @@
 /**
  * Tests whether keys in Params panel are sorted.
  */
+
 add_task(async function() {
-  let { tab, monitor } = await initNetMonitor(POST_ARRAY_DATA_URL);
+  let { tab, monitor } = await initNetMonitor(POST_DATA_URL);
   info("Starting test... ");
 
   let { document, store, windowRequire } = monitor.panelWin;
@@ -15,7 +16,7 @@ add_task(async function() {
 
   store.dispatch(Actions.batchEnable(false));
 
-  let wait = waitForNetworkEvents(monitor, 1);
+  let wait = waitForNetworkEvents(monitor, 2);
   await ContentTask.spawn(tab.linkedBrowser, {}, async function() {
     content.wrappedJSObject.performRequests();
   });
@@ -29,38 +30,9 @@ add_task(async function() {
   EventUtils.sendMouseEvent({ type: "click" },
     document.querySelector("#params-tab"));
 
-  // The Params panel should render the following
-  // POSTed JSON data structure:
-  //
-  // ▼ JSON
-  //   ▼ watches: […]
-  //       0: hello
-  //       1: how
-  //       2: are
-  //       3: you
-  //     ▼ 4: {…}
-  //         a: 10
-  //       ▼ b: […]
-  //           0: "a"
-  //           1: "c"
-  //           2: "b"
-  //         c: 15
-  let actualKeys = document.querySelectorAll(".treeTable .treeRow");
-  let expectedKeys = [
-    "JSON",
-    "watches: [...]",
-    "0: hello",
-    "1: how",
-    "2: are",
-    "3: you",
-    "4: {...}",
-    "a: 10",
-    "b: [...]",
-    "0: a",
-    "1: c",
-    "2: b",
-    "c: 15",
-  ];
+  let actualKeys = document.querySelectorAll(".treeLabel");
+  let expectedKeys = ["Query string", "baz", "foo", "type",
+                      "Form data", "baz", "foo"];
 
   for (let i = 0; i < actualKeys.length; i++) {
     is(actualKeys[i].innerText, expectedKeys[i],
