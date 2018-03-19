5b297d8598aadecb3cb08c1e7dd4a865600289d9
Boris Zbarsky <bzbarsky@mit.edu>
2018-03-19 15:45:34 -0400

    Bug 1446533 part 9.  Remove nsIDOMCharacterData::ReplaceData.  r=mystor
    MozReview-Commit-ID: Lei6xZ2rw2K

--- editor/libeditor/TextEditRules.cpp
+++ editor/libeditor/TextEditRules.cpp
@@ -1621,7 +1621,8 @@ TextEditRules::HideLastPWInput()
   nsCOMPtr<nsINode> selNode = GetTextNode(selection);
   NS_ENSURE_TRUE(selNode, NS_OK);
 
-  selNode->GetAsText()->ReplaceData(mLastStart, mLastLength, hiddenText);
+  selNode->GetAsText()->ReplaceData(mLastStart, mLastLength, hiddenText,
+                                    IgnoreErrors());
   // XXXbz Selection::Collapse/Extend take int32_t, but there are tons of
   // callsites... Converting all that is a battle for another day.
   selection->Collapse(selNode, start);
