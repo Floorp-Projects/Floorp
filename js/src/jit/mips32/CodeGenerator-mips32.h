57dd2ec8837fbede57e8cfd5edba10321ffdf099
Jan de Mooij <jdemooij@mozilla.com>
2018-03-19 14:30:50 +0100

    Bug 1446766 part 1 - Use a macro to declare CodeGenerator visit methods. r=nbp

--- js/src/jit/mips32/CodeGenerator-mips32.h
+++ js/src/jit/mips32/CodeGenerator-mips32.h
@@ -15,6 +15,10 @@ namespace jit {
 class CodeGeneratorMIPS : public CodeGeneratorMIPSShared
 {
   protected:
+    CodeGeneratorMIPS(MIRGenerator* gen, LIRGraph* graph, MacroAssembler* masm)
+      : CodeGeneratorMIPSShared(gen, graph, masm)
+    { }
+
     void testNullEmitBranch(Assembler::Condition cond, const ValueOperand& value,
                             MBasicBlock* ifTrue, MBasicBlock* ifFalse)
     {
@@ -36,53 +40,13 @@ class CodeGeneratorMIPS : public CodeGeneratorMIPSShared
     template <typename T>
     void emitWasmStoreI64(T* ins);
 
-  public:
-    void visitCompareB(LCompareB* lir);
-    void visitCompareBAndBranch(LCompareBAndBranch* lir);
-    void visitCompareBitwise(LCompareBitwise* lir);
-    void visitCompareBitwiseAndBranch(LCompareBitwiseAndBranch* lir);
-    void visitCompareI64(LCompareI64* lir);
-    void visitCompareI64AndBranch(LCompareI64AndBranch* lir);
-    void visitDivOrModI64(LDivOrModI64* lir);
-    void visitUDivOrModI64(LUDivOrModI64* lir);
-    void visitWasmLoadI64(LWasmLoadI64* ins);
-    void visitWasmUnalignedLoadI64(LWasmUnalignedLoadI64* lir);
-    void visitWasmStoreI64(LWasmStoreI64* ins);
-    void visitWasmUnalignedStoreI64(LWasmUnalignedStoreI64* ins);
-    void visitWasmSelectI64(LWasmSelectI64* lir);
-    void visitWasmReinterpretFromI64(LWasmReinterpretFromI64* lir);
-    void visitWasmReinterpretToI64(LWasmReinterpretToI64* lir);
-    void visitExtendInt32ToInt64(LExtendInt32ToInt64* lir);
-    void visitWrapInt64ToInt32(LWrapInt64ToInt32* lir);
-    void visitSignExtendInt64(LSignExtendInt64* ins);
-    void visitClzI64(LClzI64* ins);
-    void visitCtzI64(LCtzI64* ins);
-    void visitNotI64(LNotI64* ins);
-    void visitWasmTruncateToInt64(LWasmTruncateToInt64* ins);
-    void visitInt64ToFloatingPoint(LInt64ToFloatingPoint* lir);
-    void visitTestI64AndBranch(LTestI64AndBranch* lir);
-
-    // Out of line visitors.
-    void visitOutOfLineBailout(OutOfLineBailout* ool);
-  protected:
     ValueOperand ToValue(LInstruction* ins, size_t pos);
     ValueOperand ToTempValue(LInstruction* ins, size_t pos);
 
     // Functions for LTestVAndBranch.
     void splitTagForTest(const ValueOperand& value, ScratchTagScope& tag);
 
-  public:
-    CodeGeneratorMIPS(MIRGenerator* gen, LIRGraph* graph, MacroAssembler* masm)
-      : CodeGeneratorMIPSShared(gen, graph, masm)
-    { }
-
-  public:
-    void visitBox(LBox* box);
-    void visitBoxFloatingPoint(LBoxFloatingPoint* box);
-    void visitUnbox(LUnbox* unbox);
     void setReturnDoubleRegs(LiveRegisterSet* regs);
-    void visitWasmAtomicLoadI64(LWasmAtomicLoadI64* lir);
-    void visitWasmAtomicStoreI64(LWasmAtomicStoreI64* lir);
 };
 
 typedef CodeGeneratorMIPS CodeGeneratorSpecific;
