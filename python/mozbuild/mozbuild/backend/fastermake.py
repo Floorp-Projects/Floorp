db925742478f4dea68baaa993926c9138e594e09
Ted Mielczarek <ted@mielczarek.org>
2018-03-21 16:47:02 -0400

    bug 1447932 - Add support for localized files to fastermake backend. r=mshal
    Co-authored by Nick Alexander <nalexander@mozilla.com>
    
    This patch adds support to the fastermake backend for taking localized files
    from a locale directory as specified by the combination of --with-l10n-base
    and --enable-ui-locale. This allows artifact builds to be built localized
    with a different locale.
    
    MozReview-Commit-ID: 1bD9Gy0ewJ2

--- python/mozbuild/mozbuild/backend/fastermake.py
+++ python/mozbuild/mozbuild/backend/fastermake.py
@@ -14,6 +14,8 @@ from mozbuild.frontend.data import (
     FinalTargetPreprocessedFiles,
     FinalTargetFiles,
     JARManifest,
+    LocalizedFiles,
+    LocalizedPreprocessedFiles,
     XPIDLFile,
 )
 from mozbuild.makeutil import Makefile
@@ -31,6 +33,7 @@ class FasterMakeBackend(CommonBackend, PartialBackend):
         self._install_manifests = OrderedDefaultDict(InstallManifest)
 
         self._dependencies = OrderedDefaultDict(list)
+        self._l10n_dependencies = OrderedDefaultDict(list)
 
         self._has_xpidl = False
 
@@ -59,13 +62,29 @@ class FasterMakeBackend(CommonBackend, PartialBackend):
         elif isinstance(obj, (FinalTargetFiles,
                               FinalTargetPreprocessedFiles)) and \
                 obj.install_target.startswith('dist/bin'):
+            ab_cd = self.environment.substs['MOZ_UI_LOCALE'][0]
+            localized = isinstance(obj, (LocalizedFiles, LocalizedPreprocessedFiles))
             defines = obj.defines or {}
             if defines:
                 defines = defines.defines
             for path, files in obj.files.walk():
                 for f in files:
+                    # For localized files we need to find the file from the locale directory.
+                    if (localized and not isinstance(f, ObjDirPath) and ab_cd != 'en-US'):
+                        src = self.localized_path(obj.relsrcdir, f)
+
+                        dep_target = 'install-%s' % obj.install_target
+
+                        if '*' not in src:
+                            merge = mozpath.abspath(mozpath.join(self.environment.topobjdir,
+                                                                 'l10n_merge', obj.relsrcdir, f))
+                            self._l10n_dependencies[dep_target].append((merge, f.full_path, src))
+                            src = merge
+                    else:
+                        src = f.full_path
+
                     if isinstance(obj, FinalTargetPreprocessedFiles):
-                        self._add_preprocess(obj, f.full_path, path,
+                        self._add_preprocess(obj, src, path,
                                              target=f.target_basename,
                                              defines=defines)
                     elif '*' in f:
@@ -73,7 +92,7 @@ class FasterMakeBackend(CommonBackend, PartialBackend):
                             for p in mozpath.split(s):
                                 if '*' not in p:
                                     yield p + '/'
-                        prefix = ''.join(_prefix(f.full_path))
+                        prefix = ''.join(_prefix(src))
 
                         if '*' in f.target_basename:
                             target = path
@@ -83,11 +102,11 @@ class FasterMakeBackend(CommonBackend, PartialBackend):
                         self._install_manifests[obj.install_target] \
                             .add_pattern_link(
                                 prefix,
-                                f.full_path[len(prefix):],
+                                src[len(prefix):],
                                 target)
                     else:
                         self._install_manifests[obj.install_target].add_link(
-                            f.full_path,
+                            src,
                             mozpath.join(path, f.target_basename)
                         )
                     if isinstance(f, ObjDirPath):
@@ -151,10 +170,28 @@ class FasterMakeBackend(CommonBackend, PartialBackend):
         mk.add_statement('INSTALL_MANIFESTS = %s'
                          % ' '.join(self._install_manifests.keys()))
 
-        # Add dependencies we infered:
+        # Add dependencies we inferred:
         for target, deps in self._dependencies.iteritems():
             mk.create_rule([target]).add_dependencies(
                 '$(TOPOBJDIR)/%s' % d for d in deps)
+
+
+        # This is not great, but it's better to have some dependencies on these Python files.
+        python_deps = [
+            '$(TOPSRCDIR)/python/mozbuild/mozbuild/action/l10n_merge.py',
+            '$(TOPSRCDIR)/third_party/python/compare-locales/compare_locales/compare.py',
+            '$(TOPSRCDIR)/third_party/python/compare-locales/compare_locales/paths.py',
+        ]
+        # Add l10n dependencies we inferred:
+        for target, deps in self._l10n_dependencies.iteritems():
+            mk.create_rule([target]).add_dependencies(
+                '%s' % d[0] for d in deps)
+            for (merge, ref_file, l10n_file) in deps:
+                rule = mk.create_rule([merge]).add_dependencies(
+                    [ref_file, l10n_file] + python_deps)
+                rule.add_commands(['$(PYTHON) -m mozbuild.action.l10n_merge --output {} --ref-file {} --l10n-file {}'.format(merge, ref_file, l10n_file)])
+                # Add a dummy rule for the l10n file since it might not exist.
+                mk.create_rule([l10n_file])
 
         mk.add_statement('include $(TOPSRCDIR)/config/faster/rules.mk')
 
