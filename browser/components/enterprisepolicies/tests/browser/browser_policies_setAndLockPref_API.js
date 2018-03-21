57b7bf820e748367759666dc355119992a94ec5d
Felipe Gomes <felipc@gmail.com>
2018-03-21 13:10:36 -0300

    Bug 1446508 - Move the helper function checkLockedPref to a common location to be used by more tests. r=MattN

--- browser/components/enterprisepolicies/tests/browser/browser_policies_setAndLockPref_API.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policies_setAndLockPref_API.js
@@ -5,55 +5,31 @@
 
 let { Policies, setAndLockPref } = ChromeUtils.import("resource:///modules/policies/Policies.jsm", {});
 
-function checkPref(prefName, expectedValue) {
-  let prefType, prefValue;
-  switch (typeof(expectedValue)) {
-    case "boolean":
-      prefType = Services.prefs.PREF_BOOL;
-      prefValue = Services.prefs.getBoolPref(prefName);
-      break;
-
-    case "number":
-      prefType = Services.prefs.PREF_INT;
-      prefValue = Services.prefs.getIntPref(prefName);
-      break;
-
-    case "string":
-      prefType = Services.prefs.PREF_STRING;
-      prefValue = Services.prefs.getStringPref(prefName);
-      break;
-  }
-
-  ok(Services.prefs.prefIsLocked(prefName), `Pref ${prefName} is correctly locked`);
-  is(Services.prefs.getPrefType(prefName), prefType, `Pref ${prefName} has the correct type`);
-  is(prefValue, expectedValue, `Pref ${prefName} has the correct value`);
-}
-
 add_task(async function test_API_directly() {
   setAndLockPref("policies.test.boolPref", true);
-  checkPref("policies.test.boolPref", true);
+  checkLockedPref("policies.test.boolPref", true);
 
   // Check that a previously-locked pref can be changed
   // (it will be unlocked first).
   setAndLockPref("policies.test.boolPref", false);
-  checkPref("policies.test.boolPref", false);
+  checkLockedPref("policies.test.boolPref", false);
 
   setAndLockPref("policies.test.intPref", 0);
-  checkPref("policies.test.intPref", 0);
+  checkLockedPref("policies.test.intPref", 0);
 
   setAndLockPref("policies.test.stringPref", "policies test");
-  checkPref("policies.test.stringPref", "policies test");
+  checkLockedPref("policies.test.stringPref", "policies test");
 
   // Test that user values do not override the prefs, and the get*Pref call
   // still return the value set through setAndLockPref
   Services.prefs.setBoolPref("policies.test.boolPref", true);
-  checkPref("policies.test.boolPref", false);
+  checkLockedPref("policies.test.boolPref", false);
 
   Services.prefs.setIntPref("policies.test.intPref", 10);
-  checkPref("policies.test.intPref", 0);
+  checkLockedPref("policies.test.intPref", 0);
 
   Services.prefs.setStringPref("policies.test.stringPref", "policies test");
-  checkPref("policies.test.stringPref", "policies test");
+  checkLockedPref("policies.test.stringPref", "policies test");
 
   try {
     // Test that a non-integer value is correctly rejected, even though
@@ -120,9 +96,9 @@ add_task(async function test_API_through_policies() {
   is(Services.policies.status, Ci.nsIEnterprisePolicies.ACTIVE, "Engine is active");
 
   // The expected values come from config_setAndLockPref.json
-  checkPref("policies.test2.boolPref", true);
-  checkPref("policies.test2.intPref", 42);
-  checkPref("policies.test2.stringPref", "policies test 2");
+  checkLockedPref("policies.test2.boolPref", true);
+  checkLockedPref("policies.test2.intPref", 42);
+  checkLockedPref("policies.test2.stringPref", "policies test 2");
 
   delete Policies.bool_policy;
   delete Policies.int_policy;
