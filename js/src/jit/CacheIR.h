735ced9223e9b4c01f6c723c85a853ceaf275f12
Matthew Gaudet <mgaudet@mozilla.com>
2018-03-22 15:08:03 -0400

    Bug 1438727: [Part 3] Implement ADD+SUB for Boolean + Double|Int32 r=jandem
    --HG--
    extra : rebase_source : 692d2222bc661d40f979528bf202c785802c6f09

--- js/src/jit/CacheIR.h
+++ js/src/jit/CacheIR.h
@@ -182,6 +182,7 @@ extern const char* CacheKindNames[];
     _(GuardIsObject)                      \
     _(GuardIsObjectOrNull)                \
     _(GuardIsNullOrUndefined)             \
+    _(GuardIsBoolean)                     \
     _(GuardIsString)                      \
     _(GuardIsSymbol)                      \
     _(GuardIsNumber)                      \
@@ -544,6 +545,12 @@ class MOZ_RAII CacheIRWriter : public JS::CustomAutoRooter
     ObjOperandId guardIsObject(ValOperandId val) {
         writeOpWithOperandId(CacheOp::GuardIsObject, val);
         return ObjOperandId(val.id());
+    }
+    Int32OperandId guardIsBoolean(ValOperandId val) {
+        Int32OperandId res(nextOperandId_++);
+        writeOpWithOperandId(CacheOp::GuardIsBoolean, val);
+        writeOperandId(res);
+        return res;
     }
     StringOperandId guardIsString(ValOperandId val) {
         writeOpWithOperandId(CacheOp::GuardIsString, val);
@@ -1809,6 +1816,7 @@ class MOZ_RAII BinaryArithIRGenerator : public IRGenerator
 
     bool tryAttachInt32();
     bool tryAttachDouble();
+    bool tryAttachBooleanWithInt32();
 
   public:
     BinaryArithIRGenerator(JSContext* cx, HandleScript, jsbytecode* pc, ICState::Mode,
