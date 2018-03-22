d73809b86fca288d8deb802c862cda0285841d67
Peter Van der Beken <peterv@propagandism.org>
2018-03-22 11:31:05 +0100

    Bug 1453011 - Remove PostCreatePrototype. r=bz.
    --HG--
    extra : rebase_source : 6c096b964e43a3460a092b0f0b1ed9b512fd98c9

--- js/xpconnect/src/XPCWrappedNative.cpp
+++ js/xpconnect/src/XPCWrappedNative.cpp
@@ -198,8 +198,7 @@ XPCWrappedNative::WrapNewGlobal(xpcObjectHelper& nativeHelper,
     XPCWrappedNativeProto* proto =
         XPCWrappedNativeProto::GetNewOrUsed(scope,
                                             nativeHelper.GetClassInfo(),
-                                            scrProto,
-                                            /* callPostCreatePrototype = */ false);
+                                            scrProto);
     if (!proto)
         return NS_ERROR_FAILURE;
 
