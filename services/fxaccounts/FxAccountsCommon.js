b03a89225116ddd103b4dfead74b0c36faab6def
Edouard Oger <eoger@fastmail.com>
2018-03-22 13:32:10 -0400

    Bug 1448165 p2 - Remove un-used ON_FXA_UPDATE_NOTIFICATION. r=markh
    FxAccountsManager has been removed from the tree and now one else listened to
    this event.
    
    MozReview-Commit-ID: By7kkWbwBvW
    
    --HG--
    extra : rebase_source : 35b1ce32859e21b92d060835362400d1a356e81d

--- services/fxaccounts/FxAccountsCommon.js
+++ services/fxaccounts/FxAccountsCommon.js
@@ -60,7 +60,6 @@ exports.ONLOGIN_NOTIFICATION = "fxaccounts:onlogin";
 exports.ONVERIFIED_NOTIFICATION = "fxaccounts:onverified";
 exports.ONLOGOUT_NOTIFICATION = "fxaccounts:onlogout";
 // Internal to services/fxaccounts only
-exports.ON_FXA_UPDATE_NOTIFICATION = "fxaccounts:update";
 exports.ON_DEVICE_CONNECTED_NOTIFICATION = "fxaccounts:device_connected";
 exports.ON_DEVICE_DISCONNECTED_NOTIFICATION = "fxaccounts:device_disconnected";
 exports.ON_PROFILE_UPDATED_NOTIFICATION = "fxaccounts:profile_updated"; // Push
