166192a00ce3685877d94c58e964f23bf45524d8
Julian Descottes <jdescottes@mozilla.com>
2018-03-22 21:37:55 +0100

    Bug 1448178 - include actorId in unknown error packet from DevTools server;r=jryans
    This will allow client code to catch errors occuring on the server side when
    interacting with old-style actors. Before that change, the call from the client
    would just never resume.
    
    MozReview-Commit-ID: Ar6q5KapsDy

---
+++ devtools/server/tests/browser/error-actor.js
@@ -0,0 +1,30 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Test actor designed to check that clients are properly notified of errors when calling
+ * methods on old style actors.
+ */
+function ErrorActor(conn, tab) {
+  this.conn = conn;
+  this.tab = tab;
+}
+
+ErrorActor.prototype = {
+  actorPrefix: "error",
+
+  onError: function() {
+    throw new Error("error");
+  }
+};
+
+ErrorActor.prototype.requestTypes = {
+  "error": ErrorActor.prototype.onError
+};
+
+DebuggerServer.removeGlobalActor(ErrorActor);
+DebuggerServer.addGlobalActor(ErrorActor, "errorActor");
