0be7896f3a91b96702ba70dcafb29994d7ff888d
Emilio Cobos Álvarez <emilio@crisal.io>
2018-03-23 17:31:18 +0100

    Bug 1446108: followup: Make a destructor public to workaround bug 1448384 and reopen the CLOSED TREE. r=me
    MozReview-Commit-ID: BRnjUONXMud

--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -1206,11 +1206,10 @@ struct nsCSSValueSharedList final {
   {
   }
 
-private:
-  // Private destructor, to discourage deletion outside of Release():
+  // FIXME(bug 1448384): Should be private, to discourage deletion outside of
+  // Release(), but MSVC isn't happy with that.
   ~nsCSSValueSharedList();
 
-public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(nsCSSValueSharedList)
 
   void AppendToString(nsCSSPropertyID aProperty, nsAString& aResult) const;
