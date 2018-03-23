e467a3f8920d9e009f85827dd67990ac6685b505
Jason Laster <jason.laster.11@gmail.com>
2018-03-22 20:03:21 -0400

    Bug 1448431 - Don't pause on console expression exceptions. r=jimb
    MozReview-Commit-ID: JrQjvtBy3LP

--- devtools/server/tests/unit/test_pause_exceptions-02.js
+++ devtools/server/tests/unit/test_pause_exceptions-02.js
@@ -5,7 +5,7 @@
 
 /**
  * Test that setting pauseOnExceptions to true when the debugger isn't in a
- * paused state will cause the debuggee to pause when an exceptions is thrown.
+ * paused state will not cause the debuggee to pause when an exceptions is thrown.
  */
 
 var gDebuggee;
@@ -29,21 +29,21 @@ function run_test() {
 function test_pause_frame() {
   gThreadClient.pauseOnExceptions(true, false, function() {
     gThreadClient.addOneTimeListener("paused", function(event, packet) {
-      Assert.equal(packet.why.type, "exception");
-      Assert.equal(packet.why.exception, 42);
-      gThreadClient.resume(function() {
-        finishClient(gClient);
-      });
+      Assert.equal(packet.why.type, "debuggerStatement");
+      Assert.equal(packet.frame.where.line, 8);
+      gThreadClient.resume(() => finishClient(gClient));
     });
 
     /* eslint-disable */
-    gDebuggee.eval("(" + function () {
-      function stopMe() {
-        throw 42;
-      }
-      try {
-        stopMe();
-      } catch (e) {}
+    gDebuggee.eval("(" + function () {   // 1
+      function stopMe() {                // 2
+        throw 42;                        // 3
+      }                                  // 4
+      try {                              // 5
+        stopMe();                        // 6
+      } catch (e) {                      // 7
+        debugger;                        // 8
+      }                                  // 9
     } + ")()");
     /* eslint-enable */
   });
