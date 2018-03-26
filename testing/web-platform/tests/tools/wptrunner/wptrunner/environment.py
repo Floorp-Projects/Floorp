e331e9a9781452e8d6216a09c2c2325188b7218a
jgraham <james@hoppipolla.co.uk>
2018-03-26 14:59:43 +0000

    Bug 1441908 [wpt PR 9651] - Switch external_host to host_ip., a=testonly
    Automatic update from web-platform-testsSwitch external_host to host_ip. (#9258)
    
    When running tests in Firefox we don't require that the hostname is
    actually resolvable since we set up some internal DNS overrides. This
    means that we don't actually need to have web-platform.test defined in
    /etc/hosts. As a consequence when we check the server is responding,
    the check will typically fail for Firefox developers if we try to
    access by hostname. Previously we made use of a special dance in which
    external_host held the ip at the point of the check and was later
    replaced by host to actually run tests. But that changed, and instead
    we relied on the fact that we (accidentially) didn't actually fail in
    the case the servers couldn't be reached by host name. However in some
    cases things still failed because local DNS resolved web-patform.test
    to some ip address, causing hangs or other badness.
    
    In this patch we simply add a host_ip property to the config, and
    connect to that to test the servers are repopnsing rather than the
    hostname. We also actually fil if we didn't manage to connect to all
    the servers.
    
    --
    Run the firefox infrastructure tests without custom /etc/hosts
    
    wpt-commits: ee314b534530038d511661148251a6351d66e7e1, 26cd868a17844c1588d473c527179d41954768c7
    wpt-pr: 9651
    wpt-commits: ee314b534530038d511661148251a6351d66e7e1, 26cd868a17844c1588d473c527179d41954768c7
    wpt-pr: 9651

--- testing/web-platform/tests/tools/wptrunner/wptrunner/environment.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/environment.py
@@ -107,7 +107,8 @@ class TestEnvironment(object):
             cm.__enter__()
             self.env_extras_cms.append(cm)
 
-        self.servers = serve.start(self.config, self.ssl_env,
+        self.servers = serve.start(self.config,
+                                   self.ssl_env,
                                    self.get_routes())
         if self.options.get("supports_debugger") and self.debug_info and self.debug_info.interactive:
             self.ignore_interrupts()
@@ -153,9 +154,7 @@ class TestEnvironment(object):
         with open(default_config_path) as f:
             default_config = json.load(f)
 
-        #TODO: allow non-default configuration for ssl
-
-        local_config["external_host"] = self.options.get("external_host", None)
+        local_config["host_ip"] = self.options.get("host_ip", None)
         local_config["ssl"]["encrypt_after_connect"] = self.options.get("encrypt_after_connect", False)
 
         config = serve.merge_json(default_config, local_config)
@@ -234,20 +233,23 @@ class TestEnvironment(object):
             if not failed:
                 return
             time.sleep(0.5)
-        raise EnvironmentError("Servers failed to start (scheme:port): %s" % ("%s:%s" for item in failed))
+        raise EnvironmentError("Servers failed to start: %s" %
+                               ", ".join("%s:%s" % item for item in failed))
 
     def test_servers(self):
         failed = []
+        host = self.config.get("host_ip") or self.config.get("host")
         for scheme, servers in self.servers.iteritems():
             for port, server in servers:
                 if self.test_server_port:
                     s = socket.socket()
                     try:
-                        s.connect((self.config["host"], port))
+                        s.connect((host, port))
                     except socket.error:
-                        failed.append((scheme, port))
+                        failed.append((host, port))
                     finally:
                         s.close()
 
                 if not server.is_alive():
                     failed.append((scheme, port))
+        return failed
