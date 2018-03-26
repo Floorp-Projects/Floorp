6c50a19852f179c55012f11998e5c3c9e4ef2ecf
Matthew Gaudet <mgaudet@mozilla.com>
2018-03-26 09:58:19 -0400

    Bug 1438727: [Part 8] Implement JSOP_MUL in CacheIR r=tcampbell

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -3571,13 +3571,13 @@ IonBuilder::arithTrySharedStub(bool* emitted, JSOp op,
         break;
       case JSOP_ADD:
       case JSOP_SUB:
+      case JSOP_MUL:
         // If not disabled, prefer the cache IR stub.
         if (!JitOptions.disableCacheIRBinaryArith) {
             stub = MBinaryCache::New(alloc(), left, right);
             break;
         }
         MOZ_FALLTHROUGH;
-      case JSOP_MUL:
       case JSOP_DIV:
       case JSOP_MOD:
       case JSOP_POW:
