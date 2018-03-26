77626f8171caef30c16ba55e3d641313cfa3075b
Mike Ratcliffe <mratcliffe@mozilla.com>
2018-03-26 11:08:57 +0100

    Bug 1448798 - Change devtools/shared/telemetry.js to an ES6 class r=pbro
    MozReview-Commit-ID: 5sTSpCtTNSH

--- devtools/client/shared/telemetry.js
+++ devtools/client/shared/telemetry.js
@@ -10,179 +10,178 @@
 
 "use strict";
 
+const Services = require("Services");
 const TOOLS_OPENED_PREF = "devtools.telemetry.tools.opened.version";
 
-function Telemetry() {
-  // Bind pretty much all functions so that callers do not need to.
-  this.toolOpened = this.toolOpened.bind(this);
-  this.toolClosed = this.toolClosed.bind(this);
-  this.log = this.log.bind(this);
-  this.logScalar = this.logScalar.bind(this);
-  this.logKeyedScalar = this.logKeyedScalar.bind(this);
-  this.logOncePerBrowserVersion = this.logOncePerBrowserVersion.bind(this);
-  this.destroy = this.destroy.bind(this);
+class Telemetry {
+  constructor() {
+    // Bind pretty much all functions so that callers do not need to.
+    this.toolOpened = this.toolOpened.bind(this);
+    this.toolClosed = this.toolClosed.bind(this);
+    this.log = this.log.bind(this);
+    this.logScalar = this.logScalar.bind(this);
+    this.logKeyedScalar = this.logKeyedScalar.bind(this);
+    this.logOncePerBrowserVersion = this.logOncePerBrowserVersion.bind(this);
+    this.destroy = this.destroy.bind(this);
 
-  this._timers = new Map();
-}
+    this._timers = new Map();
+  }
 
-module.exports = Telemetry;
-
-var Services = require("Services");
-
-Telemetry.prototype = {
-  _histograms: {
-    toolbox: {
-      histogram: "DEVTOOLS_TOOLBOX_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_TOOLBOX_TIME_ACTIVE_SECONDS"
-    },
-    options: {
-      histogram: "DEVTOOLS_OPTIONS_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_OPTIONS_TIME_ACTIVE_SECONDS"
-    },
-    webconsole: {
-      histogram: "DEVTOOLS_WEBCONSOLE_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_WEBCONSOLE_TIME_ACTIVE_SECONDS"
-    },
-    browserconsole: {
-      histogram: "DEVTOOLS_BROWSERCONSOLE_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_BROWSERCONSOLE_TIME_ACTIVE_SECONDS"
-    },
-    inspector: {
-      histogram: "DEVTOOLS_INSPECTOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_INSPECTOR_TIME_ACTIVE_SECONDS"
-    },
-    ruleview: {
-      histogram: "DEVTOOLS_RULEVIEW_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_RULEVIEW_TIME_ACTIVE_SECONDS"
-    },
-    computedview: {
-      histogram: "DEVTOOLS_COMPUTEDVIEW_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_COMPUTEDVIEW_TIME_ACTIVE_SECONDS"
-    },
-    layoutview: {
-      histogram: "DEVTOOLS_LAYOUTVIEW_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_LAYOUTVIEW_TIME_ACTIVE_SECONDS"
-    },
-    fontinspector: {
-      histogram: "DEVTOOLS_FONTINSPECTOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_FONTINSPECTOR_TIME_ACTIVE_SECONDS"
-    },
-    animationinspector: {
-      histogram: "DEVTOOLS_ANIMATIONINSPECTOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_ANIMATIONINSPECTOR_TIME_ACTIVE_SECONDS"
-    },
-    jsdebugger: {
-      histogram: "DEVTOOLS_JSDEBUGGER_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_JSDEBUGGER_TIME_ACTIVE_SECONDS"
-    },
-    jsbrowserdebugger: {
-      histogram: "DEVTOOLS_JSBROWSERDEBUGGER_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_JSBROWSERDEBUGGER_TIME_ACTIVE_SECONDS"
-    },
-    styleeditor: {
-      histogram: "DEVTOOLS_STYLEEDITOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_STYLEEDITOR_TIME_ACTIVE_SECONDS"
-    },
-    shadereditor: {
-      histogram: "DEVTOOLS_SHADEREDITOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_SHADEREDITOR_TIME_ACTIVE_SECONDS"
-    },
-    webaudioeditor: {
-      histogram: "DEVTOOLS_WEBAUDIOEDITOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_WEBAUDIOEDITOR_TIME_ACTIVE_SECONDS"
-    },
-    canvasdebugger: {
-      histogram: "DEVTOOLS_CANVASDEBUGGER_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_CANVASDEBUGGER_TIME_ACTIVE_SECONDS"
-    },
-    performance: {
-      histogram: "DEVTOOLS_JSPROFILER_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_JSPROFILER_TIME_ACTIVE_SECONDS"
-    },
-    memory: {
-      histogram: "DEVTOOLS_MEMORY_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_MEMORY_TIME_ACTIVE_SECONDS"
-    },
-    netmonitor: {
-      histogram: "DEVTOOLS_NETMONITOR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_NETMONITOR_TIME_ACTIVE_SECONDS"
-    },
-    storage: {
-      histogram: "DEVTOOLS_STORAGE_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_STORAGE_TIME_ACTIVE_SECONDS"
-    },
-    dom: {
-      histogram: "DEVTOOLS_DOM_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_DOM_TIME_ACTIVE_SECONDS"
-    },
-    paintflashing: {
-      histogram: "DEVTOOLS_PAINTFLASHING_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_PAINTFLASHING_TIME_ACTIVE_SECONDS"
-    },
-    scratchpad: {
-      histogram: "DEVTOOLS_SCRATCHPAD_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_SCRATCHPAD_TIME_ACTIVE_SECONDS"
-    },
-    "scratchpad-window": {
-      histogram: "DEVTOOLS_SCRATCHPAD_WINDOW_OPENED_COUNT",
-    },
-    responsive: {
-      histogram: "DEVTOOLS_RESPONSIVE_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_RESPONSIVE_TIME_ACTIVE_SECONDS"
-    },
-    eyedropper: {
-      histogram: "DEVTOOLS_EYEDROPPER_OPENED_COUNT",
-    },
-    menueyedropper: {
-      histogram: "DEVTOOLS_MENU_EYEDROPPER_OPENED_COUNT",
-    },
-    pickereyedropper: {
-      histogram: "DEVTOOLS_PICKER_EYEDROPPER_OPENED_COUNT",
-    },
-    toolbareyedropper: {
-      scalar: "devtools.toolbar.eyedropper.opened",
-    },
-    copyuniquecssselector: {
-      scalar: "devtools.copy.unique.css.selector.opened",
-    },
-    copyfullcssselector: {
-      scalar: "devtools.copy.full.css.selector.opened",
-    },
-    copyxpath: {
-      scalar: "devtools.copy.xpath.opened",
-    },
-    developertoolbar: {
-      histogram: "DEVTOOLS_DEVELOPERTOOLBAR_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_DEVELOPERTOOLBAR_TIME_ACTIVE_SECONDS"
-    },
-    aboutdebugging: {
-      histogram: "DEVTOOLS_ABOUTDEBUGGING_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_ABOUTDEBUGGING_TIME_ACTIVE_SECONDS"
-    },
-    webide: {
-      histogram: "DEVTOOLS_WEBIDE_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_WEBIDE_TIME_ACTIVE_SECONDS"
-    },
-    webideNewProject: {
-      histogram: "DEVTOOLS_WEBIDE_NEW_PROJECT_COUNT",
-    },
-    webideImportProject: {
-      histogram: "DEVTOOLS_WEBIDE_IMPORT_PROJECT_COUNT",
-    },
-    custom: {
-      histogram: "DEVTOOLS_CUSTOM_OPENED_COUNT",
-      timerHistogram: "DEVTOOLS_CUSTOM_TIME_ACTIVE_SECONDS"
-    },
-    gridInspectorShowGridAreasOverlayChecked: {
-      scalar: "devtools.grid.showGridAreasOverlay.checked",
-    },
-    gridInspectorShowGridLineNumbersChecked: {
-      scalar: "devtools.grid.showGridLineNumbers.checked",
-    },
-    gridInspectorShowInfiniteLinesChecked: {
-      scalar: "devtools.grid.showInfiniteLines.checked",
-    },
-  },
+  get histograms() {
+    return {
+      toolbox: {
+        histogram: "DEVTOOLS_TOOLBOX_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_TOOLBOX_TIME_ACTIVE_SECONDS"
+      },
+      options: {
+        histogram: "DEVTOOLS_OPTIONS_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_OPTIONS_TIME_ACTIVE_SECONDS"
+      },
+      webconsole: {
+        histogram: "DEVTOOLS_WEBCONSOLE_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_WEBCONSOLE_TIME_ACTIVE_SECONDS"
+      },
+      browserconsole: {
+        histogram: "DEVTOOLS_BROWSERCONSOLE_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_BROWSERCONSOLE_TIME_ACTIVE_SECONDS"
+      },
+      inspector: {
+        histogram: "DEVTOOLS_INSPECTOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_INSPECTOR_TIME_ACTIVE_SECONDS"
+      },
+      ruleview: {
+        histogram: "DEVTOOLS_RULEVIEW_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_RULEVIEW_TIME_ACTIVE_SECONDS"
+      },
+      computedview: {
+        histogram: "DEVTOOLS_COMPUTEDVIEW_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_COMPUTEDVIEW_TIME_ACTIVE_SECONDS"
+      },
+      layoutview: {
+        histogram: "DEVTOOLS_LAYOUTVIEW_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_LAYOUTVIEW_TIME_ACTIVE_SECONDS"
+      },
+      fontinspector: {
+        histogram: "DEVTOOLS_FONTINSPECTOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_FONTINSPECTOR_TIME_ACTIVE_SECONDS"
+      },
+      animationinspector: {
+        histogram: "DEVTOOLS_ANIMATIONINSPECTOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_ANIMATIONINSPECTOR_TIME_ACTIVE_SECONDS"
+      },
+      jsdebugger: {
+        histogram: "DEVTOOLS_JSDEBUGGER_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_JSDEBUGGER_TIME_ACTIVE_SECONDS"
+      },
+      jsbrowserdebugger: {
+        histogram: "DEVTOOLS_JSBROWSERDEBUGGER_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_JSBROWSERDEBUGGER_TIME_ACTIVE_SECONDS"
+      },
+      styleeditor: {
+        histogram: "DEVTOOLS_STYLEEDITOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_STYLEEDITOR_TIME_ACTIVE_SECONDS"
+      },
+      shadereditor: {
+        histogram: "DEVTOOLS_SHADEREDITOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_SHADEREDITOR_TIME_ACTIVE_SECONDS"
+      },
+      webaudioeditor: {
+        histogram: "DEVTOOLS_WEBAUDIOEDITOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_WEBAUDIOEDITOR_TIME_ACTIVE_SECONDS"
+      },
+      canvasdebugger: {
+        histogram: "DEVTOOLS_CANVASDEBUGGER_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_CANVASDEBUGGER_TIME_ACTIVE_SECONDS"
+      },
+      performance: {
+        histogram: "DEVTOOLS_JSPROFILER_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_JSPROFILER_TIME_ACTIVE_SECONDS"
+      },
+      memory: {
+        histogram: "DEVTOOLS_MEMORY_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_MEMORY_TIME_ACTIVE_SECONDS"
+      },
+      netmonitor: {
+        histogram: "DEVTOOLS_NETMONITOR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_NETMONITOR_TIME_ACTIVE_SECONDS"
+      },
+      storage: {
+        histogram: "DEVTOOLS_STORAGE_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_STORAGE_TIME_ACTIVE_SECONDS"
+      },
+      dom: {
+        histogram: "DEVTOOLS_DOM_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_DOM_TIME_ACTIVE_SECONDS"
+      },
+      paintflashing: {
+        histogram: "DEVTOOLS_PAINTFLASHING_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_PAINTFLASHING_TIME_ACTIVE_SECONDS"
+      },
+      scratchpad: {
+        histogram: "DEVTOOLS_SCRATCHPAD_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_SCRATCHPAD_TIME_ACTIVE_SECONDS"
+      },
+      "scratchpad-window": {
+        histogram: "DEVTOOLS_SCRATCHPAD_WINDOW_OPENED_COUNT",
+      },
+      responsive: {
+        histogram: "DEVTOOLS_RESPONSIVE_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_RESPONSIVE_TIME_ACTIVE_SECONDS"
+      },
+      eyedropper: {
+        histogram: "DEVTOOLS_EYEDROPPER_OPENED_COUNT",
+      },
+      menueyedropper: {
+        histogram: "DEVTOOLS_MENU_EYEDROPPER_OPENED_COUNT",
+      },
+      pickereyedropper: {
+        histogram: "DEVTOOLS_PICKER_EYEDROPPER_OPENED_COUNT",
+      },
+      toolbareyedropper: {
+        scalar: "devtools.toolbar.eyedropper.opened",
+      },
+      copyuniquecssselector: {
+        scalar: "devtools.copy.unique.css.selector.opened",
+      },
+      copyfullcssselector: {
+        scalar: "devtools.copy.full.css.selector.opened",
+      },
+      copyxpath: {
+        scalar: "devtools.copy.xpath.opened",
+      },
+      developertoolbar: {
+        histogram: "DEVTOOLS_DEVELOPERTOOLBAR_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_DEVELOPERTOOLBAR_TIME_ACTIVE_SECONDS"
+      },
+      aboutdebugging: {
+        histogram: "DEVTOOLS_ABOUTDEBUGGING_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_ABOUTDEBUGGING_TIME_ACTIVE_SECONDS"
+      },
+      webide: {
+        histogram: "DEVTOOLS_WEBIDE_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_WEBIDE_TIME_ACTIVE_SECONDS"
+      },
+      webideNewProject: {
+        histogram: "DEVTOOLS_WEBIDE_NEW_PROJECT_COUNT",
+      },
+      webideImportProject: {
+        histogram: "DEVTOOLS_WEBIDE_IMPORT_PROJECT_COUNT",
+      },
+      custom: {
+        histogram: "DEVTOOLS_CUSTOM_OPENED_COUNT",
+        timerHistogram: "DEVTOOLS_CUSTOM_TIME_ACTIVE_SECONDS"
+      },
+      gridInspectorShowGridAreasOverlayChecked: {
+        scalar: "devtools.grid.showGridAreasOverlay.checked",
+      },
+      gridInspectorShowGridLineNumbersChecked: {
+        scalar: "devtools.grid.showGridLineNumbers.checked",
+      },
+      gridInspectorShowInfiniteLinesChecked: {
+        scalar: "devtools.grid.showInfiniteLines.checked",
+      }
+    };
+  }
 
   /**
    * Add an entry to a histogram.
@@ -191,8 +190,8 @@ Telemetry.prototype = {
    *         Used to look up the relevant histogram ID and log true to that
    *         histogram.
    */
-  toolOpened: function(id) {
-    let charts = this._histograms[id] || this._histograms.custom;
+  toolOpened(id) {
+    let charts = this.histograms[id] || this.histograms.custom;
 
     if (charts.histogram) {
       this.log(charts.histogram, true);
@@ -203,7 +202,7 @@ Telemetry.prototype = {
     if (charts.scalar) {
       this.logScalar(charts.scalar, 1);
     }
-  },
+  }
 
   /**
    * Record that an action occurred.  Aliases to `toolOpened`, so it's just for
@@ -212,17 +211,17 @@ Telemetry.prototype = {
    */
   actionOccurred(id) {
     this.toolOpened(id);
-  },
+  }
 
-  toolClosed: function(id) {
-    let charts = this._histograms[id];
+  toolClosed(id) {
+    let charts = this.histograms[id];
 
     if (!charts || !charts.timerHistogram) {
       return;
     }
 
     this.stopTimer(charts.timerHistogram);
-  },
+  }
 
   /**
    * Record the start time for a timing-based histogram entry.
@@ -230,9 +229,9 @@ Telemetry.prototype = {
    * @param String histogramId
    *        Histogram in which the data is to be stored.
    */
-  startTimer: function(histogramId) {
+  startTimer(histogramId) {
     this._timers.set(histogramId, new Date());
-  },
+  }
 
   /**
    * Stop the timer and log elasped time for a timing-based histogram entry.
@@ -242,7 +241,7 @@ Telemetry.prototype = {
    * @param String key [optional]
    *        Optional key for a keyed histogram.
    */
-  stopTimer: function(histogramId, key) {
+  stopTimer(histogramId, key) {
     let startTime = this._timers.get(histogramId);
     if (startTime) {
       let time = (new Date() - startTime) / 1000;
@@ -253,7 +252,7 @@ Telemetry.prototype = {
       }
       this._timers.delete(histogramId);
     }
-  },
+  }
 
   /**
    * Log a value to a histogram.
@@ -263,7 +262,7 @@ Telemetry.prototype = {
    * @param  value
    *         Value to store.
    */
-  log: function(histogramId, value) {
+  log(histogramId, value) {
     if (!histogramId) {
       return;
     }
@@ -275,7 +274,7 @@ Telemetry.prototype = {
       dump(`Warning: An attempt was made to write to the ${histogramId} ` +
            `histogram, which is not defined in Histograms.json\n`);
     }
-  },
+  }
 
   /**
    * Log a value to a scalar.
@@ -285,7 +284,7 @@ Telemetry.prototype = {
    * @param  value
    *         Value to store.
    */
-  logScalar: function(scalarId, value) {
+  logScalar(scalarId, value) {
     if (!scalarId) {
       return;
     }
@@ -303,7 +302,7 @@ Telemetry.prototype = {
       dump(`Warning: An attempt was made to write to the ${scalarId} ` +
            `scalar, which is not defined in Scalars.yaml\n`);
     }
-  },
+  }
 
   /**
    * Log a value to a keyed count scalar.
@@ -315,7 +314,7 @@ Telemetry.prototype = {
    * @param  value
    *         Value to store.
    */
-  logKeyedScalar: function(scalarId, key, value) {
+  logKeyedScalar(scalarId, key, value) {
     if (!scalarId) {
       return;
     }
@@ -333,7 +332,7 @@ Telemetry.prototype = {
       dump(`Warning: An attempt was made to write to the ${scalarId} ` +
            `scalar, which is not defined in Scalars.yaml\n`);
     }
-  },
+  }
 
   /**
    * Log a value to a keyed histogram.
@@ -345,7 +344,7 @@ Telemetry.prototype = {
    * @param  [value]
    *         Optional value to store.
    */
-  logKeyed: function(histogramId, key, value) {
+  logKeyed(histogramId, key, value) {
     if (histogramId) {
       try {
         let histogram = Services.telemetry.getKeyedHistogramById(histogramId);
@@ -360,7 +359,7 @@ Telemetry.prototype = {
              `histogram, which is not defined in Histograms.json\n`);
       }
     }
-  },
+  }
 
   /**
    * Log info about usage once per browser version. This allows us to discover
@@ -369,7 +368,7 @@ Telemetry.prototype = {
    * @param  {String} perUserHistogram
    *         Histogram in which the data is to be stored.
    */
-  logOncePerBrowserVersion: function(perUserHistogram, value) {
+  logOncePerBrowserVersion(perUserHistogram, value) {
     let currentVersion = Services.appinfo.version;
     let latest = Services.prefs.getCharPref(TOOLS_OPENED_PREF);
     let latestObj = JSON.parse(latest);
@@ -383,11 +382,13 @@ Telemetry.prototype = {
       Services.prefs.setCharPref(TOOLS_OPENED_PREF, latest);
       this.log(perUserHistogram, value);
     }
-  },
+  }
 
-  destroy: function() {
+  destroy() {
     for (let histogramId of this._timers.keys()) {
       this.stopTimer(histogramId);
     }
   }
-};
+}
+
+module.exports = Telemetry;
