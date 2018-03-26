67efaf227d47cce9c218c3cd7b33a253e8213209
Greg Thompson <grt@chromium.org>
2018-03-26 14:56:49 +0000

    Bug 1442558 [wpt PR 9745] - Revert "ServiceWorker: Add WPT to check if CSP sandbox is respected", a=testonly
    Automatic update from web-platform-testsRevert "ServiceWorker: Add WPT to check if CSP sandbox is respected"
    
    This reverts commit 4ffad4c407bc6e60b6c2f021a5a26abb8dc438d2.
    
    Reason for revert: New test consistently fails on multiple platforms:
    
    * external/wpt/service-workers/service-worker/worker-in-sandboxed-iframe-by-csp-fetch-event.https.html
    * virtual/navigation-mojo-response/external/wpt/service-workers/service-worker/worker-in-sandboxed-iframe-by-csp-fetch-event.https.html
    
    (https://ci.chromium.org/buildbot/chromium.webkit/WebKit%20Mac10.10/43786)
    
    Original change's description:
    > ServiceWorker: Add WPT to check if CSP sandbox is respected
    >
    > Bug: 771815
    > Change-Id: I11dc3cf67e3e40465f612f71fc318fd7061b6581
    > Reviewed-on: https://chromium-review.googlesource.com/915683
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#540467}
    
    TBR=falken@chromium.org,horo@chromium.org,kinuko@chromium.org,clamy@chromium.org,shimazu@chromium.org
    
    Change-Id: I30d4c54e78656e473d0c32622b01004122a3be75
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 771815
    Reviewed-on: https://chromium-review.googlesource.com/945828
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540484}
    
    wpt-commits: ceb8d5d0f661060c8af3086b4861b22ad0267f56
    wpt-pr: 9745
    wpt-commits: ceb8d5d0f661060c8af3086b4861b22ad0267f56
    wpt-pr: 9745

--- testing/web-platform/tests/service-workers/service-worker/resources/sandboxed-iframe-fetch-event-iframe.py
+++ testing/web-platform/tests/service-workers/service-worker/resources/sandboxed-iframe-fetch-event-iframe.py
@@ -7,9 +7,6 @@ def main(request, response):
       body = f.read()
     return (header, body)
 
-  if 'sandbox' in request.GET:
-    header.append(('Content-Security-Policy',
-                   'sandbox %s' % request.GET['sandbox']))
   with open(os.path.join(os.path.dirname(__file__),
                          'sandboxed-iframe-fetch-event-iframe.html'), 'r') as f:
     body = f.read()
