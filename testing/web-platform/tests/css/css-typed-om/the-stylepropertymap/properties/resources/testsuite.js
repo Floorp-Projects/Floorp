d7a1ce2952c7dea7bce6789a931f88d57b330001
Darren Shen <shend@chromium.org>
2018-03-26 14:48:17 +0000

    Bug 1442130 [wpt PR 9730] - [css-typed-om] Ensure properties works with css-wide keywords / var refs, a=testonly
    Automatic update from web-platform-tests[css-typed-om] Ensure properties works with css-wide keywords / var refs
    
    There are two things that should always be supported with any property:
    - css-wide keywords like 'initial'
    - var refs like 'var(--A)'
    
    This patch adds tests to ensure that this works for all the different
    types of properties. Unfortunately, supporting this in shorthands
    is quite difficult (we get shorthands as strings, so to reify them
    we have to parse, but we can't parse in reification) so we'll leave
    that for the future.
    
    Bug: 816722
    Change-Id: I1d344b73d011db81cfc3880be54823302534fd36
    Reviewed-on: https://chromium-review.googlesource.com/942121
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: nainar <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#540055}
    
    wpt-commits: 9c3df0ea37661b7dbb9d19a7e7384c05483a2fd9
    wpt-pr: 9730
    wpt-commits: 9c3df0ea37661b7dbb9d19a7e7384c05483a2fd9
    wpt-pr: 9730

--- testing/web-platform/tests/css/css-typed-om/the-stylepropertymap/properties/resources/testsuite.js
+++ testing/web-platform/tests/css/css-typed-om/the-stylepropertymap/properties/resources/testsuite.js
@@ -31,6 +31,13 @@ const gCssWideKeywordsExamples = [
   },
 ];
 
+const gVarReferenceExamples = [
+  {
+    description: 'a var() reference',
+    input: new CSSUnparsedValue([' ', new CSSVariableReferenceValue('--A')])
+  },
+];
+
 const gTestSyntaxExamples = {
   '<length>': {
     description: 'a length',
@@ -328,6 +335,13 @@ function runPropertyTests(propertyName, testCases) {
     null, // should be as specified
     () => {}, // could be anything
     'CSS-wide keywords');
+
+  // Every property should support values containing var() references.
+  testPropertyValid(propertyName,
+    gVarReferenceExamples,
+    null, // should be as specified
+    () => {}, // could compute to anything
+    'var() references');
 
   for (const testCase of testCases) {
     // <image> is a special case
