7453417d6a84b34d5d9d351b91af6326d723c872
Xidorn Quan <me@upsuper.org>
2018-03-27 11:09:08 +1100

    Bug 1449039 part 6 - Remove CSS_PROPERTY_IGNORED_WHEN_COLORS_DISABLED. r=emilio
    MozReview-Commit-ID: IQI2CUUsfUd
    
    --HG--
    extra : rebase_source : f280c2c936e878f9deac22133b063a2514c6d538

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -134,10 +134,6 @@
 #define CSS_PROPERTY_APPLIES_TO_FIRST_LETTER_AND_FIRST_LINE \
   (CSS_PROPERTY_APPLIES_TO_FIRST_LETTER | CSS_PROPERTY_APPLIES_TO_FIRST_LINE)
 
-// Note that 'background-color' is ignored differently from the other
-// properties that have this set, but that's just special-cased.
-#define CSS_PROPERTY_IGNORED_WHEN_COLORS_DISABLED (1<<4)
-
 // A property that needs to have image loads started when a URL value
 // for the property is used for an element.  This is supported only
 // for a few possible value formats: image directly in the value; list
