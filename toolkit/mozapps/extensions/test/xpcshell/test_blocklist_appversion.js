87060ac710c128560ea694ecc09c6ce85c58eb6d
Kris Maglione <maglione.k@gmail.com>
2018-03-27 14:47:47 -0700

    Bug 1449255: Part 7 - Rename test_bug449027 to test_blocklist_appversion.js and modernize. r=aswan
    MozReview-Commit-ID: EUsLNs9WaOI

--- toolkit/mozapps/extensions/test/xpcshell/test_blocklist_appversion.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_blocklist_appversion.js
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-const URI_EXTENSION_BLOCKLIST_DIALOG = "chrome://mozapps/content/extensions/blocklist.xul";
 
-ChromeUtils.import("resource://testing-common/httpd.js");
-ChromeUtils.import("resource://testing-common/MockRegistrar.jsm");
+const Cm = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+
+var testserver = AddonTestUtils.createHttpServer({hosts: ["example.com"]});
+testserver.registerDirectory("/data/", do_get_file("data"));
 
 var ADDONS = [{
   id: "test_bug449027_1@tests.mozilla.org",
@@ -225,8 +226,6 @@ var PLUGINS = [
   new MockPluginTag("test_bug449027_25", "5", false, true, true)
 ];
 
-var gCallback = null;
-var gTestserver = null;
 var gNewBlocks = [];
 
 // A fake plugin host for the blocklist service to use
@@ -236,75 +235,46 @@ var PluginHost = {
     return PLUGINS;
   },
 
-  QueryInterface(iid) {
-    if (iid.equals(Ci.nsIPluginHost)
-     || iid.equals(Ci.nsISupports))
-      return this;
-
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  }
+  QueryInterface: XPCOMUtils.generateQI(["nsIPluginHost"]),
 };
 
-// Don't need the full interface, attempts to call other methods will just
-// throw which is just fine
-var WindowWatcher = {
-  openWindow(parent, url, name, features, args) {
-    // Should be called to list the newly blocklisted items
-    Assert.equal(url, URI_EXTENSION_BLOCKLIST_DIALOG);
-    Assert.notEqual(gCallback, null);
-
-    args = args.wrappedJSObject;
-
-    gNewBlocks = [];
-    var list = args.list;
-    for (let listItem of list)
-      gNewBlocks.push(listItem.name + " " + listItem.version);
-
-    // Call the callback after the blocklist has finished up
-    do_timeout(0, gCallback);
+var BlocklistPrompt = {
+  prompt(list) {
+    gNewBlocks = list.map(item => `${item.name} ${item.version}`);
   },
 
-  QueryInterface(iid) {
-    if (iid.equals(Ci.nsIWindowWatcher)
-     || iid.equals(Ci.nsISupports))
-      return this;
-
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  }
+  QueryInterface: XPCOMUtils.generateQI(["nsIBlocklistPrompt"]),
 };
 
-MockRegistrar.register("@mozilla.org/plugin/host;1", PluginHost);
-MockRegistrar.register("@mozilla.org/embedcomp/window-watcher;1", WindowWatcher);
 
-function create_addon(addon) {
-  var installrdf = "<?xml version=\"1.0\"?>\n" +
-                   "\n" +
-                   "<RDF xmlns=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
-                   "     xmlns:em=\"http://www.mozilla.org/2004/em-rdf#\">\n" +
-                   "  <Description about=\"urn:mozilla:install-manifest\">\n" +
-                   "    <em:id>" + addon.id + "</em:id>\n" +
-                   "    <em:version>" + addon.version + "</em:version>\n" +
-                   "    <em:bootstrap>true</em:bootstrap>\n" +
-                   "    <em:targetApplication>\n" +
-                   "      <Description>\n" +
-                   "        <em:id>xpcshell@tests.mozilla.org</em:id>\n" +
-                   "        <em:minVersion>3</em:minVersion>\n" +
-                   "        <em:maxVersion>3</em:maxVersion>\n" +
-                   "      </Description>\n" +
-                   "    </em:targetApplication>\n" +
-                   "    <em:name>" + addon.name + "</em:name>\n" +
-                   "  </Description>\n" +
-                   "</RDF>\n";
-  var target = gProfD.clone();
-  target.append("extensions");
-  target.append(addon.id);
-  target.append("install.rdf");
-  target.create(target.NORMAL_FILE_TYPE, 0o644);
-  var stream = Cc["@mozilla.org/network/file-output-stream;1"]
-                 .createInstance(Ci.nsIFileOutputStream);
-  stream.init(target, 0x04 | 0x08 | 0x20, 0o664, 0); // write, create, truncate
-  stream.write(installrdf, installrdf.length);
-  stream.close();
+async function loadBlocklist(file) {
+  let blocklistUpdated = TestUtils.topicObserved("blocklist-updated");
+
+  Services.prefs.setCharPref("extensions.blocklist.url",
+                             "http://example.com/data/" + file);
+  Services.blocklist.QueryInterface(Ci.nsITimerCallback).notify(null);
+
+  await blocklistUpdated;
+}
+
+MockRegistrar.register("@mozilla.org/plugin/host;1", PluginHost);
+
+let factory = XPCOMUtils.generateSingletonFactory(function() { return BlocklistPrompt; });
+Cm.registerFactory(Components.ID("{26d32654-30c7-485d-b983-b4d2568aebba}"),
+                   "Blocklist Prompt",
+                   "@mozilla.org/addons/blocklist-prompt;1", factory);
+
+function createAddon(addon) {
+  return promiseInstallXPI({
+    name: addon.name,
+    id: addon.id,
+    version: addon.version,
+    bootstrap: true,
+    targetApplications: [{
+      id: "xpcshell@tests.mozilla.org",
+      minVersion: "3",
+      maxVersion: "3"}],
+  });
 }
 
 /**
@@ -312,106 +282,64 @@ function create_addon(addon) {
  * If a lastTest is provided checks that the notification dialog got passed
  * the newly blocked items compared to the previous test.
  */
-function check_state(test, lastTest, callback) {
-  AddonManager.getAddonsByIDs(ADDONS.map(a => a.id), function(addons) {
-    for (var i = 0; i < ADDONS.length; i++) {
-      var blocked = addons[i].blocklistState == Ci.nsIBlocklistService.STATE_BLOCKED;
-      if (blocked != ADDONS[i][test])
-        do_throw("Blocklist state did not match expected for extension " + (i + 1) + ", test " + test);
-    }
+async function checkState(test, lastTest, callback) {
+  let addons = await AddonManager.getAddonsByIDs(ADDONS.map(a => a.id));
 
+  for (var i = 0; i < ADDONS.length; i++) {
+    var blocked = addons[i].blocklistState == Ci.nsIBlocklistService.STATE_BLOCKED;
+    equal(blocked, ADDONS[i][test],
+          `Blocklist state should match expected for extension ${i + 1}, test ${test}`);
+  }
+
+  for (i = 0; i < PLUGINS.length; i++) {
+    equal(PLUGINS[i].blocklisted, PLUGINS[i][test],
+          `Blocklist state should match expected for plugin ${i + 1}, test ${test}`);
+  }
+
+  if (lastTest) {
+    var expected = 0;
     for (i = 0; i < PLUGINS.length; i++) {
-      if (PLUGINS[i].blocklisted != PLUGINS[i][test])
-        do_throw("Blocklist state did not match expected for plugin " + (i + 1) + ", test " + test);
-    }
-
-    if (lastTest) {
-      var expected = 0;
-      for (i = 0; i < PLUGINS.length; i++) {
-        if (PLUGINS[i][test] && !PLUGINS[i][lastTest]) {
-          if (!gNewBlocks.includes(PLUGINS[i].name + " " + PLUGINS[i].version))
-            do_throw("Plugin " + (i + 1) + " should have been listed in the blocklist notification for test " + test);
-          expected++;
-        }
+      if (PLUGINS[i][test] && !PLUGINS[i][lastTest]) {
+        ok(gNewBlocks.includes(`${PLUGINS[i].name} ${PLUGINS[i].version}`),
+           `Plugin ${i + 1} should have been listed in the blocklist notification for test ${test}`);
+        expected++;
       }
-
-      Assert.equal(expected, gNewBlocks.length);
     }
-    executeSoon(callback);
-  });
+
+    Assert.equal(expected, gNewBlocks.length);
+  }
 }
 
-function load_blocklist(file) {
-  Services.prefs.setCharPref("extensions.blocklist.url", "http://localhost:" + gPort + "/data/" + file);
-  var blocklist = Cc["@mozilla.org/extensions/blocklist;1"]
-                    .getService(Ci.nsITimerCallback);
-  blocklist.notify(null);
-}
-
-function run_test() {
-  // Setup for test
-  dump("Setting up tests\n");
-  // Rather than keeping lots of identical add-ons in version control, just
-  // write them into the profile.
-  for (let addon of ADDONS)
-    create_addon(addon);
-
+add_task(async function test() {
   createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "3", "8");
-  startupManager();
+  await promiseStartupManager();
 
-  gTestserver = new HttpServer();
-  gTestserver.registerDirectory("/data/", do_get_file("data"));
-  gTestserver.start(-1);
-  gPort = gTestserver.identity.primaryPort;
+  for (let addon of ADDONS) {
+    await createAddon(addon);
+  }
 
-  do_test_pending();
-  check_test_pt1();
-}
+  let addons = await AddonManager.getAddonsByIDs(ADDONS.map(a => a.id));
+  for (var i = 0; i < ADDONS.length; i++) {
+    ok(addons[i], `Addon ${i + 1} should have been correctly installed`);
+  }
 
-/**
- * Checks the initial state is correct
- */
-function check_test_pt1() {
-  dump("Checking pt 1\n");
-
-  AddonManager.getAddonsByIDs(ADDONS.map(a => a.id), function(addons) {
-    for (var i = 0; i < ADDONS.length; i++) {
-      if (!addons[i])
-        do_throw("Addon " + (i + 1) + " did not get installed correctly");
-    }
-
-    executeSoon(function checkstate1() { check_state("start", null, run_test_pt2); });
-  });
-}
+  await checkState("start");
+});
 
 /**
  * Load the toolkit based blocks
  */
-function run_test_pt2() {
-  dump("Running test pt 2\n");
-  gCallback = check_test_pt2;
-  load_blocklist("test_bug449027_toolkit.xml");
-}
+add_task(async function test_pt2() {
+  await loadBlocklist("test_bug449027_toolkit.xml");
 
-function check_test_pt2() {
-  dump("Checking pt 2\n");
-  check_state("toolkitBlocks", "start", run_test_pt3);
-}
+  await checkState("toolkitBlocks", "start");
+});
 
 /**
  * Load the application based blocks
  */
-function run_test_pt3() {
-  dump("Running test pt 3\n");
-  gCallback = check_test_pt3;
-  load_blocklist("test_bug449027_app.xml");
-}
+add_task(async function test_pt3() {
+  await loadBlocklist("test_bug449027_app.xml");
 
-function check_test_pt3() {
-  dump("Checking pt 3\n");
-  check_state("appBlocks", "toolkitBlocks", end_test);
-}
-
-function end_test() {
-  gTestserver.stop(do_test_finished);
-}
+  await checkState("appBlocks", "toolkitBlocks");
+});
