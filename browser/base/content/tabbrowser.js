a33ff16e41fb7d9afe8a2cb58c901c54f002d99c
Mike Conley <mconley@mozilla.com>
2018-03-27 16:01:36 -0400

    Bug 1408026 - Remove early renderLayers for preloaded browsers now that Activity Stream takes care of it. r=florian
    MozReview-Commit-ID: L9ROVXggr5k

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1775,7 +1775,6 @@ window._gBrowser = {
 
     browser.loadURI(BROWSER_NEW_TAB_URL);
     browser.docShellIsActive = false;
-    browser.renderLayers = true;
     browser._urlbarFocused = true;
 
     // Make sure the preloaded browser is loaded with desired zoom level
