622142c54bc71f9bb1567bb906d59850e26bc7b5
Chris H-C <chutten@mozilla.com>
2018-03-27 17:01:28 -0400

    bug 1440673 - Allow TelemetryScalar.h to be included in tests r=Dexter
    TelemetryComms includes some ipc headers we don't have in the gtest build.
    
    MozReview-Commit-ID: 9gK6KPHeUIa

--- toolkit/components/telemetry/TelemetryScalar.h
+++ toolkit/components/telemetry/TelemetryScalar.h
@@ -8,12 +8,20 @@
 
 #include "mozilla/TelemetryScalarEnums.h"
 #include "mozilla/TelemetryProcessEnums.h"
-#include "ipc/TelemetryComms.h"
 
 // This module is internal to Telemetry. It encapsulates Telemetry's
 // scalar accumulation and storage logic. It should only be used by
 // Telemetry.cpp. These functions should not be used anywhere else.
 // For the public interface to Telemetry functionality, see Telemetry.h.
+
+namespace mozilla {
+namespace Telemetry {
+  struct ScalarAction;
+  struct KeyedScalarAction;
+  struct DiscardedData;
+  struct DynamicScalarDefinition;
+} // namespace Telemetry
+} // namespace mozilla
 
 namespace TelemetryScalar {
 
