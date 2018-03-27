99ea4b677c4857b8075a32ae5d4a2b41390cab81
Tom Ritter <tom@mozilla.com>
2018-03-26 23:27:39 -0500

    Bug 1397757 Add Learn More link for the Canvas Permission Prompt r=johannh a=jcristau
    MozReview-Commit-ID: 5pR8VAmFwqM
    
    --HG--
    extra : source : a6fba6948018606931d31509773c40f83d9c7396

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6758,6 +6758,7 @@ var CanvasPermissionPromptHelper = {
     let options = {
       checkbox,
       name: uri.asciiHost,
+      learnMoreURL: Services.urlFormatter.formatURLPref("app.support.baseURL") + "fingerprint-permission",
     };
     PopupNotifications.show(browser, aTopic, message, this._notificationIcon,
                             mainAction, secondaryActions, options);
