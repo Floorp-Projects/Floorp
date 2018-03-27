068aabc68cd3f5533812e4f5035fee0764a05302
Benjamin Bouvier <benj@benj.me>
2018-03-27 18:07:06 +0200

    Bug 1449213: Create WebAssembly.Global objects for imported globals that received a primitive; r=luke

--- js/src/wasm/WasmModule.h
+++ js/src/wasm/WasmModule.h
@@ -149,9 +149,8 @@ class Module : public JS::WasmModule
     bool instantiateTable(JSContext* cx,
                           MutableHandleWasmTableObject table,
                           SharedTableVector* tables) const;
-    bool instantiateGlobalExports(JSContext* cx,
-                                  const ValVector& globalImportValues,
-                                  WasmGlobalObjectVector& globalObjs) const;
+    bool instantiateGlobals(JSContext* cx, const ValVector& globalImportValues,
+                            WasmGlobalObjectVector& globalObjs) const;
     bool initSegments(JSContext* cx,
                       HandleWasmInstanceObject instance,
                       Handle<FunctionVector> funcImports,
