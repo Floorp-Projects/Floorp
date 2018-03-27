3903838e6a4422f9fafe839d1841b2a458530d07
Dave Hunt <dhunt@mozilla.com>
2018-03-27 12:59:40 +0100

    Bug 1437593 - Add support for using Pipfiles for managing virtual environment dependencies; r=ted
    MozReview-Commit-ID: GWFJZfYWi5Y
    
    --HG--
    extra : rebase_source : 3b357bb6e39f8d008ebb982293c21fe3036dac8e

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -748,6 +748,14 @@ class MozbuildObject(ProcessExecutionMixin):
     def _set_log_level(self, verbose):
         self.log_manager.terminal_handler.setLevel(logging.INFO if not verbose else logging.DEBUG)
 
+    def activate_pipenv(self, path):
+        if not os.path.exists(path):
+            raise Exception('Pipfile not found: %s.' % path)
+        self._activate_virtualenv()
+        pipenv_reqs = os.path.join(self.topsrcdir, 'python/mozbuild/mozbuild/pipenv.txt')
+        self.virtualenv_manager.install_pip_requirements(pipenv_reqs, require_hashes=False, vendored=True)
+        self.virtualenv_manager.activate_pipenv(path)
+
 
 class MachCommandBase(MozbuildObject):
     """Base class for mach command providers that wish to be MozbuildObjects.
