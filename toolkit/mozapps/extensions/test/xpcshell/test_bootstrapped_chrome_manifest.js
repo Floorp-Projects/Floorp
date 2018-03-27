6f102e6193e13b2fafea5029468f1a9cc12f1cc0
Kris Maglione <maglione.k@gmail.com>
2018-03-27 15:46:16 -0700

    Bug 1449255: Part 12a - Rename test_bug953156 to test_bootstrapped_chrome_manifest.js and modernize. r=aswan
    MozReview-Commit-ID: HU1bBCTD207

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bootstrapped_chrome_manifest.js
@@ -0,0 +1,45 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+const ADDON = {
+  "install.rdf": {
+    "id": "bug675371@tests.mozilla.org",
+    "version": "1.0",
+    "bootstrap": "true",
+    "name": "Bug 675371 Test",
+    "description": "Test Description",
+    "targetApplications": [
+      {
+        "id": "xpcshell@tests.mozilla.org",
+        "minVersion": "1",
+        "maxVersion": "1"
+      }
+    ]
+  },
+  "chrome.manifest": `content bug675371 .`,
+  "test.js": `var active = true;`,
+};
+
+add_task(async function run_test() {
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+  await promiseStartupManager();
+});
+
+add_task(async function test() {
+  let {addon} = await AddonTestUtils.promiseInstallXPI(ADDON);
+
+  Assert.ok(addon.isActive);
+
+  // Tests that chrome.manifest is registered when the addon is installed.
+  var target = { active: false };
+  Services.scriptloader.loadSubScript("chrome://bug675371/content/test.js", target);
+  Assert.ok(target.active);
+
+  await promiseShutdownManager();
+
+  // Tests that chrome.manifest remains registered at app shutdown.
+  target.active = false;
+  Services.scriptloader.loadSubScript("chrome://bug675371/content/test.js", target);
+  Assert.ok(target.active);
+});
