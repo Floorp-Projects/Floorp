53625cd8830d75862ab0259c96c227b3bb2ba17b
Yura Zenevich <yura.zenevich@gmail.com>
2018-03-27 15:55:43 -0400

    Bug 1449212 - Adding enabled accessibility services information to core ping. r=sdaswani, a=RyanVM
    MozReview-Commit-ID: 3dwvGvya5Xk
    
    --HG--
    extra : source : 8ab320324b9fea099696c4bea8b301baa148199a

--- mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryCorePingDelegate.java
+++ mobile/android/base/java/org/mozilla/gecko/telemetry/TelemetryCorePingDelegate.java
@@ -6,11 +6,13 @@
 
 package org.mozilla.gecko.telemetry;
 
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
 import org.mozilla.gecko.BrowserApp;
 import org.mozilla.gecko.GeckoApp;
 import org.mozilla.gecko.GeckoProfile;
@@ -31,6 +33,7 @@ import org.mozilla.gecko.util.StringUtils;
 import org.mozilla.gecko.util.ThreadUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * An activity-lifecycle delegate for uploading the core ping.
@@ -185,6 +188,16 @@ public class TelemetryCorePingDelegate extends BrowserAppDelegateWithReference
         final String campaignId = CampaignIdMeasurements.getCampaignIdFromPrefs(context);
         if (campaignId != null) {
             pingBuilder.setOptCampaignId(campaignId);
+        }
+
+        final AccessibilityManager accessibilityManager = (AccessibilityManager)
+            context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        if (accessibilityManager != null) {
+            final List<AccessibilityServiceInfo> enabledServices =
+                accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+            if (enabledServices.size() > 0) {
+                pingBuilder.setOptAccessibility(enabledServices);
+            }
         }
     }
 
