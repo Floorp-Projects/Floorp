c9cb59bbb8325bfa7989cb53a24c7f7d3feaef36
arthur.iakab <aiakab@mozilla.com>
2018-03-27 06:48:42 +0300

    Backed out 2 changesets (bug 1439014) for failing mochitest chrome and test verify on tools/profiler/tests/chrome/test_profile_with_trackopts.html
    Backed out changeset 57426696adaf (bug 1439014)
    Backed out changeset a13074f87bb0 (bug 1439014)

--- tools/profiler/core/RegisteredThread.h
+++ tools/profiler/core/RegisteredThread.h
@@ -197,14 +197,13 @@ public:
   // Request that this thread start JS sampling. JS sampling won't actually
   // start until a subsequent PollJSSampling() call occurs *and* mContext has
   // been set.
-  void StartJSSampling(bool aTrackOptimizations)
+  void StartJSSampling()
   {
     // This function runs on-thread or off-thread.
 
     MOZ_RELEASE_ASSERT(mJSSampling == INACTIVE ||
                        mJSSampling == INACTIVE_REQUESTED);
     mJSSampling = ACTIVE_REQUESTED;
-    mJSTrackOptimizations = aTrackOptimizations;
   }
 
   // Request that this thread stop JS sampling. JS sampling won't actually stop
@@ -237,8 +236,6 @@ public:
       if (mJSSampling == ACTIVE_REQUESTED) {
         mJSSampling = ACTIVE;
         js::EnableContextProfilingStack(mContext, true);
-        JS_SetGlobalJitCompilerOption(mContext, JSJITCOMPILER_TRACK_OPTIMIZATIONS,
-                                      mJSTrackOptimizations);
         js::RegisterContextProfilingEventMarker(mContext, profiler_add_marker);
 
       } else if (mJSSampling == INACTIVE_REQUESTED) {
@@ -307,8 +304,6 @@ private:
     ACTIVE = 2,
     INACTIVE_REQUESTED = 3,
   } mJSSampling;
-
-  bool mJSTrackOptimizations;
 };
 
 #endif  // RegisteredThread_h
