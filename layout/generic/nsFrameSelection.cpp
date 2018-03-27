86c46292904c510daebfa13b69fd54900f344dde
Boris Zbarsky <bzbarsky@mit.edu>
2018-03-27 00:35:21 -0400

    Bug 1447889 part 10. Remove nsIDOMRange::DeleteContents. r=mystor
    MozReview-Commit-ID: IoXz0pS6zAa

--- layout/generic/nsFrameSelection.cpp
+++ layout/generic/nsFrameSelection.cpp
@@ -2839,8 +2839,6 @@ nsFrameSelection::SetAncestorLimiter(nsIContent *aLimiter)
 nsresult
 nsFrameSelection::DeleteFromDocument()
 {
-  nsresult res;
-
   // If we're already collapsed, then we do nothing (bug 719503).
   bool isCollapsed;
   int8_t index = GetIndexFromSelectionType(SelectionType::eNormal);
@@ -2856,9 +2854,11 @@ nsFrameSelection::DeleteFromDocument()
   RefPtr<Selection> selection = mDomSelections[index];
   for (uint32_t rangeIdx = 0; rangeIdx < selection->RangeCount(); ++rangeIdx) {
     RefPtr<nsRange> range = selection->GetRangeAt(rangeIdx);
-    res = range->DeleteContents();
-    if (NS_FAILED(res))
-      return res;
+    ErrorResult res;
+    range->DeleteContents(res);
+    if (res.Failed()) {
+      return res.StealNSResult();
+    }
   }
 
   // Collapse to the new location.
