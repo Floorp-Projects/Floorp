6f3465c9772894e3d9e61bcc69f0bdecb916be5a
Margareta Eliza Balazs <ebalazs@mozilla.com>
2018-03-28 07:26:10 +0300

    Backed out changeset 6af40fb76692 (bug 1372406) for Doc lint failure in builds/worker/checkouts/gecko/docs-out/html/main/_staging/python/mach.commands.rst on a CLOSED TREE

---
+++ browser/components/extensions/ext-c-menus.js
@@ -0,0 +1,189 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+// The ext-* files are imported into the same scopes.
+/* import-globals-from ../../../toolkit/components/extensions/ext-c-toolkit.js */
+
+var {
+  withHandlingUserInput,
+} = ExtensionUtils;
+
+// If id is not specified for an item we use an integer.
+// This ID need only be unique within a single addon. Since all addon code that
+// can use this API runs in the same process, this local variable suffices.
+var gNextMenuItemID = 0;
+
+// Map[Extension -> Map[string or id, ContextMenusClickPropHandler]]
+var gPropHandlers = new Map();
+
+// The contextMenus API supports an "onclick" attribute in the create/update
+// methods to register a callback. This class manages these onclick properties.
+class ContextMenusClickPropHandler {
+  constructor(context) {
+    this.context = context;
+    // Map[string or integer -> callback]
+    this.onclickMap = new Map();
+    this.dispatchEvent = this.dispatchEvent.bind(this);
+  }
+
+  // A listener on contextMenus.onClicked that forwards the event to the only
+  // listener, if any.
+  dispatchEvent(info, tab) {
+    let onclick = this.onclickMap.get(info.menuItemId);
+    if (onclick) {
+      // No need for runSafe or anything because we are already being run inside
+      // an event handler -- the event is just being forwarded to the actual
+      // handler.
+      onclick(info, tab);
+    }
+  }
+
+  // Sets the `onclick` handler for the given menu item.
+  // The `onclick` function MUST be owned by `this.context`.
+  setListener(id, onclick) {
+    if (this.onclickMap.size === 0) {
+      this.context.childManager.getParentEvent("menusInternal.onClicked").addListener(this.dispatchEvent);
+      this.context.callOnClose(this);
+    }
+    this.onclickMap.set(id, onclick);
+
+    let propHandlerMap = gPropHandlers.get(this.context.extension);
+    if (!propHandlerMap) {
+      propHandlerMap = new Map();
+    } else {
+      // If the current callback was created in a different context, remove it
+      // from the other context.
+      let propHandler = propHandlerMap.get(id);
+      if (propHandler && propHandler !== this) {
+        propHandler.unsetListener(id);
+      }
+    }
+    propHandlerMap.set(id, this);
+    gPropHandlers.set(this.context.extension, propHandlerMap);
+  }
+
+  // Deletes the `onclick` handler for the given menu item.
+  // The `onclick` function MUST be owned by `this.context`.
+  unsetListener(id) {
+    if (!this.onclickMap.delete(id)) {
+      return;
+    }
+    if (this.onclickMap.size === 0) {
+      this.context.childManager.getParentEvent("menusInternal.onClicked").removeListener(this.dispatchEvent);
+      this.context.forgetOnClose(this);
+    }
+    let propHandlerMap = gPropHandlers.get(this.context.extension);
+    propHandlerMap.delete(id);
+    if (propHandlerMap.size === 0) {
+      gPropHandlers.delete(this.context.extension);
+    }
+  }
+
+  // Deletes the `onclick` handler for the given menu item, if any, regardless
+  // of the context where it was created.
+  unsetListenerFromAnyContext(id) {
+    let propHandlerMap = gPropHandlers.get(this.context.extension);
+    let propHandler = propHandlerMap && propHandlerMap.get(id);
+    if (propHandler) {
+      propHandler.unsetListener(id);
+    }
+  }
+
+  // Remove all `onclick` handlers of the extension.
+  deleteAllListenersFromExtension() {
+    let propHandlerMap = gPropHandlers.get(this.context.extension);
+    if (propHandlerMap) {
+      for (let [id, propHandler] of propHandlerMap) {
+        propHandler.unsetListener(id);
+      }
+    }
+  }
+
+  // Removes all `onclick` handlers from this context.
+  close() {
+    for (let id of this.onclickMap.keys()) {
+      this.unsetListener(id);
+    }
+  }
+}
+
+this.menusInternal = class extends ExtensionAPI {
+  getAPI(context) {
+    let onClickedProp = new ContextMenusClickPropHandler(context);
+
+    let api = {
+      menus: {
+        create(createProperties, callback) {
+          if (createProperties.id === null) {
+            createProperties.id = ++gNextMenuItemID;
+          }
+          let {onclick} = createProperties;
+          delete createProperties.onclick;
+          context.childManager.callParentAsyncFunction("menusInternal.create", [
+            createProperties,
+          ]).then(() => {
+            if (onclick) {
+              onClickedProp.setListener(createProperties.id, onclick);
+            }
+            if (callback) {
+              callback();
+            }
+          });
+          return createProperties.id;
+        },
+
+        update(id, updateProperties) {
+          let {onclick} = updateProperties;
+          delete updateProperties.onclick;
+          return context.childManager.callParentAsyncFunction("menusInternal.update", [
+            id,
+            updateProperties,
+          ]).then(() => {
+            if (onclick) {
+              onClickedProp.setListener(id, onclick);
+            } else if (onclick === null) {
+              onClickedProp.unsetListenerFromAnyContext(id);
+            }
+            // else onclick is not set so it should not be changed.
+          });
+        },
+
+        remove(id) {
+          onClickedProp.unsetListenerFromAnyContext(id);
+          return context.childManager.callParentAsyncFunction("menusInternal.remove", [
+            id,
+          ]);
+        },
+
+        removeAll() {
+          onClickedProp.deleteAllListenersFromExtension();
+
+          return context.childManager.callParentAsyncFunction("menusInternal.removeAll", []);
+        },
+
+        onClicked: new EventManager(context, "menus.onClicked", fire => {
+          let listener = (info, tab) => {
+            withHandlingUserInput(context.contentWindow,
+                                  () => fire.sync(info, tab));
+          };
+
+          let event = context.childManager.getParentEvent("menusInternal.onClicked");
+          event.addListener(listener);
+          return () => {
+            event.removeListener(listener);
+          };
+        }).api(),
+      },
+    };
+
+    const result = {};
+    if (context.extension.hasPermission("menus")) {
+      result.menus = api.menus;
+    }
+    if (context.extension.hasPermission("contextMenus")) {
+      result.contextMenus = api.menus;
+    }
+    return result;
+  }
+};
---
+++ toolkit/components/extensions/ext-c-toolkit.js
@@ -0,0 +1,86 @@
+"use strict";
+
+ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+
+ChromeUtils.defineModuleGetter(this, "Services",
+                               "resource://gre/modules/Services.jsm");
+
+// These are defined on "global" which is used for the same scopes as the other
+// ext-c-*.js files.
+/* exported EventManager */
+/* global EventManager: false */
+
+global.EventManager = ExtensionCommon.EventManager;
+
+extensions.registerModules({
+  backgroundPage: {
+    url: "chrome://extensions/content/ext-c-backgroundPage.js",
+    scopes: ["addon_child"],
+    manifest: ["background"],
+    paths: [
+      ["extension", "getBackgroundPage"],
+      ["runtime", "getBackgroundPage"],
+    ],
+  },
+  contentScripts: {
+    url: "chrome://extensions/content/ext-c-contentScripts.js",
+    scopes: ["addon_child"],
+    paths: [
+      ["contentScripts"],
+    ],
+  },
+  extension: {
+    url: "chrome://extensions/content/ext-c-extension.js",
+    scopes: ["addon_child", "content_child", "devtools_child", "proxy_script"],
+    paths: [
+      ["extension"],
+    ],
+  },
+  i18n: {
+    url: "chrome://extensions/content/ext-i18n.js",
+    scopes: ["addon_child", "content_child", "devtools_child", "proxy_script"],
+    paths: [
+      ["i18n"],
+    ],
+  },
+  runtime: {
+    url: "chrome://extensions/content/ext-c-runtime.js",
+    scopes: ["addon_child", "content_child", "devtools_child", "proxy_script"],
+    paths: [
+      ["runtime"],
+    ],
+  },
+  storage: {
+    url: "chrome://extensions/content/ext-c-storage.js",
+    scopes: ["addon_child", "content_child", "devtools_child", "proxy_script"],
+    paths: [
+      ["storage"],
+    ],
+  },
+  test: {
+    url: "chrome://extensions/content/ext-c-test.js",
+    scopes: ["addon_child", "content_child", "devtools_child", "proxy_script"],
+    paths: [
+      ["test"],
+    ],
+  },
+  webRequest: {
+    url: "chrome://extensions/content/ext-c-webRequest.js",
+    scopes: ["addon_child"],
+    paths: [
+      ["webRequest"],
+    ],
+  },
+});
+
+if (AppConstants.MOZ_BUILD_APP === "browser") {
+  extensions.registerModules({
+    identity: {
+      url: "chrome://extensions/content/ext-c-identity.js",
+      scopes: ["addon_child"],
+      paths: [
+        ["identity"],
+      ],
+    },
+  });
+}
