0a460e67a4883a7fddc0b8ee97c5754078a6ac08
Drew Willcoxon <adw@mozilla.com>
2018-03-28 11:28:13 -0700

    Bug 1221539 - Add search engine discovery to the page action menu. Part 1: Page action changes. r=Gijs
    MozReview-Commit-ID: DGy3sBibpRW

--- browser/extensions/webcompat-reporter/test/browser/browser_report_site_issue.js
+++ browser/extensions/webcompat-reporter/test/browser/browser_report_site_issue.js
@@ -8,6 +8,7 @@ add_task(async function test_opened_page() {
 
   let tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_PAGE);
 
+  await openPageActions();
   let webcompatButton = document.getElementById(WC_PAGE_ACTION_ID);
   ok(webcompatButton, "Report Site Issue button exists.");
 
@@ -20,7 +21,6 @@ add_task(async function test_opened_page() {
       resolve(tab);
     }, { once: true });
   });
-  openPageActions();
   webcompatButton.click();
   let tab2 = await newTabPromise;
   await screenshotPromise;
