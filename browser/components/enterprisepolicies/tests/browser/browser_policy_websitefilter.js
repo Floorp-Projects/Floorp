5d34a4f502421302a1a2c7025b344271ff6a5db2
Felipe Gomes <felipc@gmail.com>
2018-03-28 09:29:17 -0500

    Bug 1429178 - Policy: Implement website blocklist/allowlist. r=mixedpuppy a=RyanVM

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_websitefilter.js
@@ -0,0 +1,25 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+const SUPPORT_FILES_PATH = "http://mochi.test:8888/browser/browser/components/enterprisepolicies/tests/browser";
+const BLOCKED_PAGE   = `${SUPPORT_FILES_PATH}/policy_websitefilter_block.html`;
+const EXCEPTION_PAGE = `${SUPPORT_FILES_PATH}/policy_websitefilter_exception.html`;
+
+add_task(async function test() {
+  await setupPolicyEngineWithJson({
+    "policies": {
+      "WebsiteFilter": {
+        "Block": [
+          "*://mochi.test/*policy_websitefilter_*"
+        ],
+        "Exceptions": [
+          "*://mochi.test/*_websitefilter_exception*"
+        ]
+      }
+    }
+  });
+
+  await checkBlockedPage(BLOCKED_PAGE, true);
+  await checkBlockedPage(EXCEPTION_PAGE, false);
+});
