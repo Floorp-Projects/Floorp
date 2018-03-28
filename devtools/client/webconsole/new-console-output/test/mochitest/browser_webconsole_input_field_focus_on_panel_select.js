f137579c7c3809fac16ba36a362b9c45aea5fd5f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-03-28 17:35:32 +0200

    Bug 1405343 - Migrate browser_webconsole_input_field_focus_on_panel_select.js to the new frontend; r=bgrins.
    The test needed some changes in new-console.js in order to pass.
    This patch adds a listener on webconsole-selected to focus the
    console input. What's funny is that the listener wasn't set, but
    it was removed in new-webconsole.js destroy function (probably a
    leftover from the webconsole.js -> new-webconsole.js copy).
    We also add the openInspector helper function in head.js.
    
    MozReview-Commit-ID: 1q6ON6XMkKB

--- devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_input_field_focus_on_panel_select.js
+++ devtools/client/webconsole/new-console-output/test/mochitest/browser_webconsole_input_field_focus_on_panel_select.js
@@ -3,35 +3,32 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// XXX Remove this when the file is migrated to the new frontend.
-/* eslint-disable no-undef */
-
 // Test that the JS input field is focused when the user switches back to the
 // web console from other tools, see bug 891581.
 
 "use strict";
 
-const TEST_URI = "data:text/html;charset=utf8,<p>hello";
+const TEST_URI = "data:text/html;charset=utf8,<p>Test console input focus";
 
 add_task(async function() {
-  await loadTab(TEST_URI);
-  let hud = await openConsole();
-  hud.jsterm.clearOutput();
+  let hud = await openNewTabAndConsole(TEST_URI);
 
-  is(hud.jsterm.inputNode.hasAttribute("focused"), true,
-     "inputNode should be focused");
+  let inputNode = hud.jsterm.inputNode;
+  const filterInput = hud.ui.outputNode.querySelector(".text-filter");
 
-  hud.ui.filterBox.focus();
+  info("Focus after console is opened");
+  ok(hasFocus(inputNode), "input node is focused after console is opened");
 
-  is(hud.ui.filterBox.hasAttribute("focused"), true,
-     "filterBox should be focused");
+  filterInput.focus();
+  ok(hasFocus(filterInput), "filter input should be focused");
 
-  is(hud.jsterm.inputNode.hasAttribute("focused"), false,
-     "inputNode shouldn't be focused");
+  is(hasFocus(inputNode), false, "input node is not focused anymore");
 
+  info("Go to the inspector panel");
   await openInspector();
-  hud = await openConsole();
 
-  is(hud.jsterm.inputNode.hasAttribute("focused"), true,
-     "inputNode should be focused");
+  info("Go back to the console");
+  await openConsole();
+
+  ok(hasFocus(inputNode), "input node is focused when coming from a different panel");
 });
