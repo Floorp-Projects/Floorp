8cce5bb3190646e12e0bd397fa92397cb8ee576f
Yura Zenevich <yura.zenevich@gmail.com>
2018-03-28 11:40:49 -0400

    Bug 1447112 - added front end parts of accessibility inspector tool. r=pbro, yulia, jdescottes, flod
    MozReview-Commit-ID: JU3WrXuqFJi

---
+++ devtools/client/accessibility/test/browser_accessibility_sidebar.js
@@ -0,0 +1,66 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI = `<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Accessibility Panel Test</title>
+  </head>
+  <body></body>
+</html>`;
+
+/**
+ * Test data has the format of:
+ * {
+ *   desc     {String}    description for better logging
+ *   action   {Function}  An optional action that needs to be performed before
+ *                        the state of the tree and the sidebar can be checked.
+ *   expected {JSON}      An expected states for the tree and the sidebar.
+ * }
+ */
+const tests = [{
+  desc: "Test the initial accessibility sidebar state.",
+  expected: {
+    sidebar: {
+      name: "Accessibility Panel Test",
+      role: "document",
+      actions: [],
+      value: "",
+      description: "",
+      help: "",
+      keyboardShortcut: "",
+      childCount: 0,
+      indexInParent: 0,
+      states: ["readonly", "focusable", "opaque", "enabled", "sensitive"]
+    }
+  }
+}, {
+  desc: "Mark document as disabled for accessibility.",
+  action: async ({ browser }) => ContentTask.spawn(browser, {}, () =>
+    content.document.body.setAttribute("aria-disabled", true)),
+  expected: {
+    sidebar: {
+      states: ["unavailable", "readonly", "focusable", "opaque"]
+    }
+  }
+}, {
+  desc: "Append a new child to the document.",
+  action: async ({ browser }) => ContentTask.spawn(browser, {}, () => {
+    let doc = content.document;
+    let button = doc.createElement("button");
+    button.textContent = "Press Me!";
+    doc.body.appendChild(button);
+  }),
+  expected: {
+    sidebar: {
+      childCount: 1
+    }
+  }
+}];
+
+/**
+ * Test that checks the Accessibility panel sidebar.
+ */
+addA11yPanelTestsTask(tests, TEST_URI, "Test Accessibility panel sidebar.");
