a112bca48d438d1de123ff2c26380ce534e6e7fc
Henrik Skupin <mail@hskupin.info>
2018-03-28 11:13:21 +0200

    Bug 1449499 - Fix fullscreen_window.py:test_handle_prompt_missing_value for wrong dialog reference. r=ato
    MozReview-Commit-ID: GhQzudT6D1u

--- testing/web-platform/tests/webdriver/tests/fullscreen_window.py
+++ testing/web-platform/tests/webdriver/tests/fullscreen_window.py
@@ -121,7 +121,7 @@ def test_handle_prompt_missing_value(session, create_dialog):
 
     assert_error(response, "unexpected alert open")
     assert_dialog_handled(session, "dismiss #1")
-    assert read_global(session, "accept1") == None
+    assert read_global(session, "dismiss1") == None
 
     create_dialog("confirm", text="dismiss #2", result_var="dismiss2")
 
