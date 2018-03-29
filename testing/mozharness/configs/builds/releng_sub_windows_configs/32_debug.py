891e50f2230be5ac4e6e33358f6ecb7dcc84d45f
David Major <dmajor@mozilla.com>
2018-03-29 15:11:42 -0400

    Bug 1448306: Update path to BinScope 2014 and make it available to all Windows builds. r=froydnj

--- testing/mozharness/configs/builds/releng_sub_windows_configs/32_debug.py
+++ testing/mozharness/configs/builds/releng_sub_windows_configs/32_debug.py
@@ -11,7 +11,6 @@ config = {
     'debug_build': True,
     #### 32 bit build specific #####
     'env': {
-        'BINSCOPE': 'C:/Program Files (x86)/Microsoft/SDL BinScope/BinScope.exe',
         'HG_SHARE_BASE_DIR': 'C:/builds/hg-shared',
         'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
         'MOZ_CRASHREPORTER_NO_REPORT': '1',
