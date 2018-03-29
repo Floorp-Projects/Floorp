45075b92aba050ad86590437a180d0e1fe82ee93
Coroiu Cristina <ccoroiu@mozilla.com>
2018-03-29 12:21:52 +0300

    Backed out changeset 4d4d373e1619 (bug 1389581)  mochitest failures Permission denied to create wrapper for object of class XPCComponents_Interfaces on a CLOSED TREE

--- js/xpconnect/tests/unit/test_components.js
+++ js/xpconnect/tests/unit/test_components.js
@@ -1,21 +1,49 @@
 function run_test() {
   var sb1 = Cu.Sandbox("http://www.blah.com");
-  var sb2 = Cu.Sandbox(this);
+  var sb2 = Cu.Sandbox("http://www.blah.com");
+  var sb3 = Cu.Sandbox(this);
+  var sb4 = Cu.Sandbox("http://www.other.com");
   var rv;
+
+  // Components is normally hidden from content on the XBL scope chain, but we
+  // expose it to content here to make sure that the security wrappers work
+  // regardless.
+  [sb1, sb2, sb4].forEach(function(x) { x.Components = Cu.getComponentsForScope(x); });
 
   // non-chrome accessing chrome Components
   sb1.C = Components;
-  checkThrows("C.interfaces", sb1);
   checkThrows("C.utils", sb1);
   checkThrows("C.classes", sb1);
 
-  // non-chrome accessing own Components: shouldn't exist.
-  Assert.equal(Cu.evalInSandbox("typeof Components", sb1), 'undefined');
+  // non-chrome accessing own Components
+  Assert.equal(Cu.evalInSandbox("typeof Components.interfaces", sb1), 'object');
+  Assert.equal(Cu.evalInSandbox("typeof Components.utils", sb1), 'undefined');
+  Assert.equal(Cu.evalInSandbox("typeof Components.classes", sb1), 'undefined');
+
+  // Make sure an unprivileged Components is benign.
+  var C2 = Cu.evalInSandbox("Components", sb2);
+  var whitelist = ['interfaces', 'interfacesByID', 'results', 'isSuccessCode', 'QueryInterface'];
+  for (var prop in Components) {
+    info("Checking " + prop);
+    Assert.equal((prop in C2), whitelist.includes(prop));
+  }
+
+  // non-chrome same origin
+  sb1.C2 = C2;
+  Assert.equal(Cu.evalInSandbox("typeof C2.interfaces", sb1), 'object');
+  Assert.equal(Cu.evalInSandbox("typeof C2.utils", sb1), 'undefined');
+  Assert.equal(Cu.evalInSandbox("typeof C2.classes", sb1), 'undefined');
 
   // chrome accessing chrome
-  sb2.C = Components;
-  rv = Cu.evalInSandbox("C.utils", sb2);
+  sb3.C = Components;
+  rv = Cu.evalInSandbox("C.utils", sb3);
   Assert.equal(rv, Cu);
+
+  // non-chrome cross origin
+  sb4.C2 = C2;
+  checkThrows("C2.interfaces", sb4);
+  checkThrows("C2.utils", sb4);
+  checkThrows("C2.classes", sb4);
 }
 
 function checkThrows(expression, sb) {
