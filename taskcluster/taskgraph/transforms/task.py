89eeb648efc119d7ea860cd122a9c7d52fbd6ecd
Johan Lorenzo <jlorenzo@mozilla.com>
2018-03-30 18:22:38 +0200

    Bug 1441353 - part 1: Add AMO tasks for signing and uploading r=Callek
    MozReview-Commit-ID: LplkEtjR2Mt

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -551,6 +551,14 @@ task_description_schema = Schema({
             Required('paths'): [basestring],
         }],
     }, {
+        Required('implementation'): 'sign-and-push-addons',
+        Required('channel'): Any('listed', 'unlisted'),
+        Required('upstream-artifacts'): [{
+            Required('taskId'): taskref_or_string,
+            Required('taskType'): basestring,
+            Required('paths'): [basestring],
+        }],
+    }, {
         Required('implementation'): 'shipit',
         Required('release-name'): basestring,
     }, {
@@ -1099,7 +1107,7 @@ def build_push_apk_payload(config, task, task_def):
 
     task_def['payload'] = {
         'commit': worker['commit'],
-        'upstreamArtifacts':  worker['upstream-artifacts'],
+        'upstreamArtifacts': worker['upstream-artifacts'],
         'google_play_track': worker['google-play-track'],
     }
 
@@ -1122,6 +1130,16 @@ def build_ship_it_payload(config, task, task_def):
 
     task_def['payload'] = {
         'release_name': worker['release-name']
+    }
+
+
+@payload_builder('sign-and-push-addons')
+def build_sign_and_push_addons_payload(config, task, task_def):
+    worker = task['worker']
+
+    task_def['payload'] = {
+        'channel': worker['channel'],
+        'upstreamArtifacts': worker['upstream-artifacts'],
     }
 
 
