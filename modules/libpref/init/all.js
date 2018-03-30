71b430f70eed1c5a148ca12ca1cb8357777739f0
Andrew Swan <aswan@mozilla.com>
2018-03-30 16:38:42 -0700

    Bug 1450388 Part 3: Implement persistent option for EventManager r=kmag
    MozReview-Commit-ID: BtccacvzhE8

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5051,6 +5051,8 @@ pref("extensions.webextensions.protocol.remote", true);
 // Disable tab hiding API by default.
 pref("extensions.webextensions.tabhide.enabled", false);
 
+pref("extensions.webextensions.background-delayed-startup", false);
+
 // Report Site Issue button
 pref("extensions.webcompat-reporter.newIssueEndpoint", "https://webcompat.com/issues/new");
 #if defined(MOZ_DEV_EDITION) || defined(NIGHTLY_BUILD)
