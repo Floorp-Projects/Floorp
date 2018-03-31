60f5ddd57d0d3415c0b5356d31d8f1f977c66d56
Robert Longson <longsonr@gmail.com>
2018-03-31 18:50:10 +0100

    Bug 1446617 - support the SVG 2 path attribute for textPath elements r=dholbert

--- dom/svg/SVGAnimatedPathSegList.h
+++ dom/svg/SVGAnimatedPathSegList.h
@@ -74,6 +74,11 @@ public:
   void ClearAnimValue(nsSVGElement *aElement);
 
   /**
+   * Empty paths are not rendered.
+   */
+  bool IsRendered() const;
+
+  /**
    * Needed for correct DOM wrapper construction since GetAnimValue may
    * actually return the baseVal!
    */
