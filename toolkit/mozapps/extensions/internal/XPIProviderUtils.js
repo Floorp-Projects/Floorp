9a813802d372cdcdaffa1206d761d3b61803b5b0
Andrew Swan <aswan@mozilla.com>
2018-04-02 14:42:56 -0700

    Bug 1450801 Part 2: Remove AddonManager support for telemetry experiments. r=kmag
    MozReview-Commit-ID: FkQd7Ia5Tv4
    
    --HG--
    extra : rebase_source : 21ca3916b88492bc6d998673cdeaac4140cfb8f4
    extra : histedit_source : c84a3ee00b4edf6c9b96fba8545f18ed12093222

--- toolkit/mozapps/extensions/internal/XPIProviderUtils.js
+++ toolkit/mozapps/extensions/internal/XPIProviderUtils.js
@@ -206,16 +206,7 @@ Object.assign(DBAddonInternal.prototype, {
   },
 
   toJSON() {
-    let jsonData = copyProperties(this, PROP_JSON_FIELDS);
-
-    // Experiments are serialized as disabled so they aren't run on the next
-    // startup.
-    if (this.type == "experiment") {
-      jsonData.userDisabled = true;
-      jsonData.active = false;
-    }
-
-    return jsonData;
+    return copyProperties(this, PROP_JSON_FIELDS);
   },
 
   get inDatabase() {
