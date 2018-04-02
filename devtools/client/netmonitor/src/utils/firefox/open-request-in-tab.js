aa14f7c3a4b79062eef1d9b465db02bac1247d54
Zhang Junzhi <zjz@zjz.name>
2018-04-02 16:42:13 +0800

    Bug 1448553 - Part 3: Decodeds Punycode-encoded international domain names in the Network Monitor so that they are displayed as human-readable Unicode text. r=Honza
    The Punycode-encoded international domain names are human-unreadable, so they
    should be displayed as human-readable Unicode text. This commit decodes this
    kind of names in the Network Monitor.
    
    MozReview-Commit-ID: HlGOVZi1lIm

--- devtools/client/netmonitor/src/utils/firefox/open-request-in-tab.js
+++ devtools/client/netmonitor/src/utils/firefox/open-request-in-tab.js
@@ -3,6 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /* eslint-disable mozilla/reject-some-requires */
 
+// This file is a chrome-API-dependent version of the module
+// devtools/client/netmonitor/src/utils/open-request-in-tab.js, so that it can
+// take advantage of utilizing chrome APIs. But because of this, it isn't
+// intended to be used in Chrome-API-free applications, such as the Launchpad.
+//
+// Please keep in mind that if the feature in this file has changed, don't
+// forget to also change that accordingly in
+// devtools/client/netmonitor/src/utils/open-request-in-tab.js.
+
 "use strict";
 
 let { Cc, Ci } = require("chrome");
