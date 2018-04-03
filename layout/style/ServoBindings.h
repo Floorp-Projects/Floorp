875c15a5a93bbb762123e5195d2e06460bc425d5
Bobby Holley <bobbyholley@gmail.com>
2018-04-03 10:50:06 -0700

    Bug 1451421 - Stop using Gecko namespace ids in servo. r=xidorn
    MozReview-Commit-ID: 2532dHCGPXW

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -636,10 +636,6 @@ nscolor Gecko_GetLookAndFeelSystemColor(int32_t color_id,
 
 void Gecko_AddPropertyToSet(nsCSSPropertyIDSetBorrowedMut, nsCSSPropertyID);
 
-// Register a namespace and get a namespace id.
-// Returns -1 on error (OOM)
-int32_t Gecko_RegisterNamespace(nsAtom* ns);
-
 // Style-struct management.
 #define STYLE_STRUCT(name)                                            \
   void Gecko_Construct_Default_nsStyle##name(                         \
