448e0693e7e0fc9739e024bc2a423f29643cc835
Xidorn Quan <me@upsuper.org>
2018-04-04 08:42:10 +1000

    Bug 1449087 part 2 - Use Servo data to back @font-face rule. r=emilio
    This patch does the following things:
    * Create a new class ServoFontFaceRule for CSSOM of @font-face rule
      which mostly follows how nsCSSFontFaceRule was implemented.
    * Remove the old nsCSSFontFaceRule and binding code to create it.
    * Have FontFace backed by Servo data via making mRule and mDescriptors
      of the class hold RawServoFontFaceRule like ServoFontFaceRule.
    
    To keep this patch small, it effectively just delays the conversion
    from Servo data to nsCSSValue from parsing to using. This may cause
    worse performance if the font set is flushed repeatedly. Supposing we
    don't flush font set very frequently, it may not be a big deal.
    
    We may still want to remove the intermediate nsCSSValue conversion at
    some point, and have everything converted to their final form directly
    when used, but that can happen in followups.
    
    There are some unfortunate bits from this change:
    * We lose style sheet for logging in FontFaceSet. This is probably not
      all that worse, because we wouldn't have that before either if the
      page doesn't use CSSOM to visit it. But we should figure out some
      approach to fix it anyway.
    * InspectorFontFace no longer shares the same rule object as CSSOM.
      This isn't really a problem if the @font-face rule isn't very mutable.
      Unless we want to make the rule returned from InspectorFontFace to be
      mutable (i.e. via inspector), not using the same object probably isn't
      too bad.
    
    This patch switches the code we use to serialize stuff in FontFace and
    CSSFontFaceRule, which leads to some failures in tests. Specifically,
    the expected changes including:
    * Value of font-family now can be serialized to identifier sequence like
      font-family property. The old code always serializes it to string,
      but it doesn't seem to have different requirement than the property.
      Blink can serialize to identifier as well.
    * Family name inside local() is also changed to use the same way as
      family names elsewhere (i.e. can be identifier sequence). Blink has
      the same behavior as the old code, but I don't think it's a big deal.
    * The order of descriptors serialized gets changed. I don't think it
      matters at all.
    * Empty string as font-family via using string syntax is no longer
      considered invalid for FontFace. I don't find it is mentioned anywhere
      that it should be specifically treated invalid.
    
    
    MozReview-Commit-ID: 32Fk3Fi9uTs

---
+++ layout/style/ServoFontFaceRule.h
@@ -0,0 +1,112 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_ServoFontFaceRule_h
+#define mozilla_ServoFontFaceRule_h
+
+#include "mozilla/ServoBindingTypes.h"
+#include "mozilla/css/Rule.h"
+#include "nsICSSDeclaration.h"
+
+namespace mozilla {
+
+// A ServoFontFaceRuleDecl is always embeded in a ServoFontFaceRule.
+class ServoFontFaceRule;
+class ServoFontFaceRuleDecl final : public nsICSSDeclaration
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMCSSSTYLEDECLARATION_HELPER
+
+  already_AddRefed<dom::CSSValue>
+  GetPropertyCSSValue(const nsAString& aProp, ErrorResult& aRv) final;
+  using nsICSSDeclaration::GetPropertyCSSValue;
+
+  nsINode* GetParentObject() final;
+  void IndexedGetter(uint32_t aIndex, bool& aFound, nsAString& aPropName) final;
+
+  void GetPropertyValue(nsCSSFontDesc aFontDescID, nsAString& aResult) const;
+
+  JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto) final;
+
+protected:
+  // For accessing the constructor.
+  friend class ServoFontFaceRule;
+
+  explicit ServoFontFaceRuleDecl(already_AddRefed<RawServoFontFaceRule> aDecl)
+    : mRawRule(Move(aDecl)) {}
+
+  ~ServoFontFaceRuleDecl() = default;
+
+  inline ServoFontFaceRule* ContainingRule();
+  inline const ServoFontFaceRule* ContainingRule() const;
+
+  RefPtr<RawServoFontFaceRule> mRawRule;
+
+private:
+  void* operator new(size_t size) CPP_THROW_NEW = delete;
+};
+
+class ServoFontFaceRule final : public css::Rule
+{
+public:
+  ServoFontFaceRule(already_AddRefed<RawServoFontFaceRule> aRawRule,
+                    uint32_t aLine, uint32_t aColumn)
+    : css::Rule(aLine, aColumn)
+    , mDecl(Move(aRawRule))
+  {}
+
+  ServoFontFaceRule(const ServoFontFaceRule&) = delete;
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS_INHERITED(
+      ServoFontFaceRule, css::Rule)
+  bool IsCCLeaf() const final;
+
+  RawServoFontFaceRule* Raw() const { return mDecl.mRawRule; }
+
+  // WebIDL interface
+  uint16_t Type() const final;
+  void GetCssText(nsAString& aCssText) const final;
+  nsICSSDeclaration* Style();
+
+  // Methods of mozilla::css::Rule
+  already_AddRefed<css::Rule> Clone() const final;
+  size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf)
+    const final;
+
+  JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aGivenProto) final;
+
+#ifdef DEBUG
+  void List(FILE* out = stdout, int32_t aIndent = 0) const final;
+#endif
+
+private:
+  virtual ~ServoFontFaceRule() = default;
+
+  // For computing the offset of mDecl.
+  friend class ServoFontFaceRuleDecl;
+
+  ServoFontFaceRuleDecl mDecl;
+};
+
+inline ServoFontFaceRule*
+ServoFontFaceRuleDecl::ContainingRule()
+{
+  return reinterpret_cast<ServoFontFaceRule*>
+    (reinterpret_cast<char*>(this) - offsetof(ServoFontFaceRule, mDecl));
+}
+
+inline const ServoFontFaceRule*
+ServoFontFaceRuleDecl::ContainingRule() const
+{
+  return reinterpret_cast<const ServoFontFaceRule*>
+    (reinterpret_cast<const char*>(this) - offsetof(ServoFontFaceRule, mDecl));
+}
+
+} // namespace mozilla
+
+#endif // mozilla_ServoFontFaceRule_h
