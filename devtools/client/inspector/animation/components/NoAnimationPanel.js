a1b32fec8dac4dd807128d202a8940aa0513cf90
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-04-03 09:12:53 +0900

    Bug 1449497: Use Component instead of PureComponent. r=gl

--- devtools/client/inspector/animation/components/NoAnimationPanel.js
+++ devtools/client/inspector/animation/components/NoAnimationPanel.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-const { PureComponent } = require("devtools/client/shared/vendor/react");
+const { Component } = require("devtools/client/shared/vendor/react");
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 const { connect } = require("devtools/client/shared/vendor/react-redux");
@@ -13,7 +13,7 @@ const { LocalizationHelper } = require("devtools/shared/l10n");
 const L10N =
   new LocalizationHelper("devtools/client/locales/animationinspector.properties");
 
-class NoAnimationPanel extends PureComponent {
+class NoAnimationPanel extends Component {
   static get propTypes() {
     return {
       elementPickerEnabled: PropTypes.bool.isRequired,
