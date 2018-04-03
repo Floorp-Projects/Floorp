69b1b1498193f6ecf8de40c4030b0406b733a899
Xidorn Quan <me@upsuper.org>
2018-04-04 08:42:10 +1000

    Bug 1449087 part 1 - Upgrade cssparser to 0.23.4 for serialization fix. r=emilio
    MozReview-Commit-ID: IJ2OqSjn1NM

--- third_party/rust/itoa/benches/bench.rs
+++ third_party/rust/itoa/benches/bench.rs
@@ -1,3 +1,7 @@
+#![cfg_attr(feature = "i128", feature(i128_type, i128))]
+
+#![cfg_attr(feature = "cargo-clippy", allow(cast_lossless))]
+
 #![feature(test)]
 #![allow(non_snake_case)]
 
@@ -5,15 +9,21 @@ extern crate itoa;
 extern crate test;
 
 macro_rules! benches {
-    ($($name:ident($value:expr),)*) => {
-        mod bench_itoa {
+    (
+        $(
+            $(#[$attr:meta])*
+            $name:ident($value:expr)
+        ),*
+    ) => {
+        mod bench_itoa_write {
             use test::{Bencher, black_box};
             $(
+                $(#[$attr])*
                 #[bench]
                 fn $name(b: &mut Bencher) {
                     use itoa;
 
-                    let mut buf = Vec::with_capacity(20);
+                    let mut buf = Vec::with_capacity(40);
 
                     b.iter(|| {
                         buf.clear();
@@ -23,14 +33,33 @@ macro_rules! benches {
             )*
         }
 
-        mod bench_fmt {
+        mod bench_itoa_fmt {
             use test::{Bencher, black_box};
             $(
+                $(#[$attr])*
+                #[bench]
+                fn $name(b: &mut Bencher) {
+                    use itoa;
+
+                    let mut buf = String::with_capacity(40);
+
+                    b.iter(|| {
+                        buf.clear();
+                        itoa::fmt(&mut buf, black_box($value)).unwrap()
+                    });
+                }
+            )*
+        }
+
+        mod bench_std_fmt {
+            use test::{Bencher, black_box};
+            $(
+                $(#[$attr])*
                 #[bench]
                 fn $name(b: &mut Bencher) {
                     use std::io::Write;
 
-                    let mut buf = Vec::with_capacity(20);
+                    let mut buf = Vec::with_capacity(40);
 
                     b.iter(|| {
                         buf.clear();
@@ -42,11 +71,16 @@ macro_rules! benches {
     }
 }
 
-benches!(
-    bench_0u64(0u64),
-    bench_HALFu64(<u32>::max_value() as u64),
-    bench_MAXu64(<u64>::max_value()),
+benches!{
+    bench_u64_0(0u64),
+    bench_u64_half(<u32>::max_value() as u64),
+    bench_u64_max(<u64>::max_value()),
 
-    bench_0i16(0i16),
-    bench_MINi16(<i16>::min_value()),
-);
+    bench_i16_0(0i16),
+    bench_i16_min(<i16>::min_value()),
+
+    #[cfg(feature = "i128")]
+    bench_u128_0(0u128),
+    #[cfg(feature = "i128")]
+    bench_u128_max(<u128>::max_value())
+}
