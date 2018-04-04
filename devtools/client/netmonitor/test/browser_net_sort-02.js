377b1b94170d5784631c6d1703e8077452a1907f
Lucas Luna Souza <lucas.lunasouza@mail.utoronto.ca>
2018-04-04 09:03:54 -0400

    Bug 1435232 - Apply new status code design to details panel. r=Honza

--- devtools/client/netmonitor/test/browser_net_sort-02.js
+++ devtools/client/netmonitor/test/browser_net_sort-02.js
@@ -243,7 +243,7 @@ add_task(async function() {
     let requestItems = document.querySelectorAll(".request-list-item");
     for (let requestItem of requestItems) {
       requestItem.scrollIntoView();
-      let requestsListStatus = requestItem.querySelector(".requests-list-status");
+      let requestsListStatus = requestItem.querySelector(".status-code");
       EventUtils.sendMouseEvent({ type: "mouseover" }, requestsListStatus);
       await waitUntil(() => requestsListStatus.title);
     }
