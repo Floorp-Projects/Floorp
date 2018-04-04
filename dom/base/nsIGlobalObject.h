155557a2e7a6307401c41c0658e6cc494451b78d
Henri Sivonen <hsivonen@hsivonen.fi>
2018-04-04 15:57:17 +0300

    Bug 218415 - Add window.event. r=smaug.
    window.event is set on the wrong window when the target and the
    callback are from different realms and the callback is an XPCOM
    callback.
    
    MozReview-Commit-ID: HXeUIicdMuT

--- dom/base/nsIGlobalObject.h
+++ dom/base/nsIGlobalObject.h
@@ -26,6 +26,7 @@
 
 class nsCycleCollectionTraversalCallback;
 class nsIPrincipal;
+class nsPIDOMWindowInner;
 
 namespace mozilla {
 class DOMEventTargetHelper;
@@ -48,8 +49,12 @@ class nsIGlobalObject : public nsISupports,
   bool mIsDying;
 
 protected:
+
+  bool mIsInnerWindow;
+
   nsIGlobalObject()
    : mIsDying(false)
+   , mIsInnerWindow(false)
   {}
 
 public:
@@ -130,6 +135,8 @@ public:
   virtual RefPtr<mozilla::dom::ServiceWorkerRegistration>
   GetOrCreateServiceWorkerRegistration(const mozilla::dom::ServiceWorkerRegistrationDescriptor& aDescriptor);
 
+  // Returns a pointer to this object as an inner window if this is one or nullptr otherwise.
+  nsPIDOMWindowInner* AsInnerWindow();
 protected:
   virtual ~nsIGlobalObject();
 
