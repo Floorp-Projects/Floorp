edad7fc2b43ba120553b246138a0ff16ac273d13
Kris Maglione <maglione.k@gmail.com>
2018-04-03 17:19:26 -0700

    Bug 1451463: Make Quitter a WebExtension experiment. r=aswan
    Differential Revision: https://phabricator.services.mozilla.com/D12478

--- tools/quitter/contentscript.js
+++ tools/quitter/contentscript.js
@@ -2,34 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* eslint-env mozilla/frame-script */
+"use strict";
 
-function Quitter() {
-}
+/* eslint-env webextensions */
 
-Quitter.prototype = {
-  toString() { return "[Quitter]"; },
-  quit() { sendSyncMessage("Quitter.Quit", {}); },
+const Quitter = {
+  quit() { browser.runtime.sendMessage("quit"); },
 };
 
-// This is a frame script, so it may be running in a content process.
-// In any event, it is targeted at a specific "tab", so we listen for
-// the DOMWindowCreated event to be notified about content windows
-// being created in this context.
-
-function QuitterManager() {
-  addEventListener("DOMWindowCreated", this, false);
-}
-
-QuitterManager.prototype = {
-  handleEvent: function handleEvent(aEvent) {
-    var quitter = new Quitter(window);
-    var window = aEvent.target.defaultView;
-    window.wrappedJSObject.Quitter = Cu.cloneInto({
-      toString: quitter.toString.bind(quitter),
-      quit: quitter.quit.bind(quitter),
-    }, window, {cloneFunctions: true});
-  },
-};
-
-var quittermanager = new QuitterManager();
+window.wrappedJSObject.Quitter = cloneInto(Quitter, window, {cloneFunctions: true});
