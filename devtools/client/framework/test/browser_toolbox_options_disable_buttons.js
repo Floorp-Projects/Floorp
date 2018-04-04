f1b05cb9f0ce44a81dbf7dafd5cbb9cba15fc961
Mark Banner <standard8@mozilla.com>
2018-04-04 21:52:00 +0100

    Bug 1451659 - Remove obsolete import-globals-from definitions that also stop ESLint from running in devtools. r=jdescottes
    MozReview-Commit-ID: DNAhKkQCkui
    
    --HG--
    extra : rebase_source : e17e17efc203cb0336dc546c7d07a1ede1c6f095

--- devtools/client/framework/test/browser_toolbox_options_disable_buttons.js
+++ devtools/client/framework/test/browser_toolbox_options_disable_buttons.js
@@ -3,7 +3,6 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/* import-globals-from shared-head.js */
 "use strict";
 
 const TEST_URL = "data:text/html;charset=utf8,test for dynamically " +
