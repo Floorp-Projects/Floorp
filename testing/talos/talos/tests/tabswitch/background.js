c878d02458a21bdef400144f85f73229c1c2c70b
Mike Conley <mconley@mozilla.com>
2018-04-04 18:20:28 -0400

    Bug 1451460 - Stub out WebExtension bits for tps. r=rwood
    MozReview-Commit-ID: 5lbluH6PPjB

---
+++ testing/talos/talos/tests/tabswitch/background.js
@@ -0,0 +1,13 @@
+/* globals browser */
+
+/**
+ * The TPS test is a Pageloader test, meaning that the tps.manifest file
+ * tells Talos to load a particular page. The loading of that page signals
+ * the start of the test. It's also where results need to go, as the
+ * Talos gunk augments the loaded page with a special tpRecordTime
+ * function that is used to report results.
+ */
+
+let frameScriptPath = "content/tabswitch-content-process.js";
+
+browser.tps.setup({ frameScriptPath });
