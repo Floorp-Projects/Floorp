5d20b9186f3298f339e895f64e8d62dc59e37762
Johan Lorenzo <jlorenzo@mozilla.com>
2018-04-04 17:41:28 +0200

    Backed out changeset 95e838a56093 for breaking decision task
    --HG--
    rename : taskcluster/ci/release-snap-repackage/kind.yml => taskcluster/ci/release-snap/kind.yml
    rename : taskcluster/taskgraph/transforms/release_snap_repackage.py => taskcluster/taskgraph/transforms/release_snap.py

--- taskcluster/taskgraph/transforms/release_snap.py
+++ taskcluster/taskgraph/transforms/release_snap.py
@@ -33,4 +33,6 @@ def format(config, tasks):
             resolve_keyed_by(env, k, 'snap envs', project=config.params['project'])
             task['worker']['env'][k] = env[k].format(**format_params)
 
+        resolve_keyed_by(task, 'scopes', 'snap scopes', project=config.params['project'])
+
         yield task
