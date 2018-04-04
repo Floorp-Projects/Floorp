823f7f78af5ad2a93a5673b7636fb05a1e4b1b30
Kirk Steuber <ksteuber@mozilla.com>
2018-04-04 10:56:20 -0700

    Bug 1429164 - Add enterprise policy to prevent showing the profile import wizard r=Felipe,Gijs
    MozReview-Commit-ID: Lg7r1NrFbKl
    
    --HG--
    extra : rebase_source : d81e065a5e99e15c24dcb4a64dff4b0341f6efc4

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_disable_profile_import.js
@@ -0,0 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+async function openLibrary() {
+  return new Promise(resolve => {
+    let library = window.openDialog("chrome://browser/content/places/places.xul", "",
+                                    "chrome,toolbar=yes,dialog=no,resizable");
+    waitForFocus(() => resolve(library), library);
+  });
+}
+
+add_task(async function test_disable_profile_import() {
+  await setupPolicyEngineWithJson({
+    "policies": {
+      "DisableProfileImport": true
+    }
+  });
+  let library = await openLibrary();
+
+  let menu = library.document.getElementById("maintenanceButtonPopup");
+  let promisePopupShown = BrowserTestUtils.waitForEvent(menu, "popupshown");
+  menu.openPopup();
+  await promisePopupShown;
+
+  let profileImportButton = library.document.getElementById("browserImport");
+  is(profileImportButton.disabled, true, "Profile Import button should be disabled");
+
+  let promisePopupHidden = BrowserTestUtils.waitForEvent(menu, "popuphidden");
+  menu.hidePopup();
+  await promisePopupHidden;
+
+  await BrowserTestUtils.closeWindow(library);
+
+  checkLockedPref("browser.newtabpage.activity-stream.migrationExpired", true);
+});
