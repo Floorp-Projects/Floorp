fcfba432908c912455c52077fae92950a6a8f248
Julian Descottes <jdescottes@mozilla.com>
2018-04-05 21:39:10 +0200

    Bug 1445197 - part 4: Implement application panel UI to display all workers;r=Honza,nchevobbe,sole
    Add redux, a store, listen to events that can lead to a change in the workers
    list to update the store.
    
    MozReview-Commit-ID: Fo0jn7Cldep

--- devtools/client/application/src/components/App.js
+++ devtools/client/application/src/components/App.js
@@ -4,15 +4,35 @@
 
 "use strict";
 
-const { Component } = require("devtools/client/shared/vendor/react");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+const { createFactory, Component } = require("devtools/client/shared/vendor/react");
+const { connect } = require("devtools/client/shared/vendor/react-redux");
 const { div } = require("devtools/client/shared/vendor/react-dom-factories");
 
+const WorkerList = createFactory(require("./WorkerList"));
+
+/**
+ * This is the main component for the application panel.
+ */
 class App extends Component {
+  static get propTypes() {
+    return {
+      client: PropTypes.object.isRequired,
+      workers: PropTypes.object.isRequired,
+      serviceContainer: PropTypes.object.isRequired,
+    };
+  }
+
   render() {
-    return (
-      div({className: "application"}, "application panel content")
-    );
+    let { workers, client, serviceContainer } = this.props;
+
+    return div({className: "application"},
+      WorkerList({ workers, client, serviceContainer }));
   }
 }
 
-module.exports = App;
+// Exports
+
+module.exports = connect(
+  (state) => ({ workers: state.workers.list }),
+)(App);
