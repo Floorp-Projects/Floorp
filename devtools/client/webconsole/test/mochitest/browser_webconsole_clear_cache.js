3ed08165ac7391cd1116a23047ee30a2e0b7926e
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-04-05 18:54:06 +0200

    Bug 1450186 - Clear cached messages when user clear messages; r=Honza.
    This patch adds a new middleware which takes care of clearing the messages
    caches when the user clear the messages through the UI or via console.clear.
    Tests are added for both webconsole and browser console to make sure we don't regress.
    
    MozReview-Commit-ID: IaBQycsBAS9

---
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_clear_cache.js
@@ -0,0 +1,64 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Check that clearing the output also clears the console cache.
+
+"use strict";
+
+const TEST_URI = "data:text/html;charset=utf8,Test clear cache";
+
+add_task(async function() {
+  const tab = await addTab(TEST_URI);
+  let hud = await openConsole(tab);
+
+  const CACHED_MESSAGE = "CACHED_MESSAGE";
+  await logTextToConsole(hud, CACHED_MESSAGE);
+
+  info("Close and re-open the console");
+  await closeToolbox();
+  hud = await openConsole(tab);
+
+  await waitFor(() => findMessage(hud, CACHED_MESSAGE));
+
+  info("Click the clear output button");
+  let onOutputCleared = waitFor(() => !findMessage(hud, CACHED_MESSAGE));
+  hud.ui.window.document.querySelector(".devtools-clear-icon").click();
+  await onOutputCleared;
+
+  info("Close and re-open the console");
+  await closeToolbox();
+  hud = await openConsole(tab);
+
+  info("Log a smoke message in order to know that the console is ready");
+  await logTextToConsole(hud, "Smoke message");
+  is(findMessage(hud, CACHED_MESSAGE), null, "The cached message is not visible anymore");
+
+  // Test that we also clear the cache when calling console.clear().
+  const NEW_CACHED_MESSAGE = "NEW_CACHED_MESSAGE";
+  await logTextToConsole(hud, NEW_CACHED_MESSAGE);
+
+  info("Send a console.clear() from the content page");
+  const onConsoleCleared = waitForMessage(hud, "Console was cleared");
+  ContentTask.spawn(gBrowser.selectedBrowser, {}, () => {
+    content.wrappedJSObject.console.clear();
+  });
+  await onConsoleCleared;
+
+  info("Close and re-open the console");
+  await closeToolbox();
+  hud = await openConsole(tab);
+
+  info("Log a smoke message in order to know that the console is ready");
+  await logTextToConsole(hud, "Second smoke message");
+  is(findMessage(hud, NEW_CACHED_MESSAGE), null,
+    "The new cached message is not visible anymore");
+});
+
+function logTextToConsole(hud, text) {
+  const onMessage = waitForMessage(hud, text);
+  ContentTask.spawn(gBrowser.selectedBrowser, text, function(str) {
+    content.wrappedJSObject.console.log(str);
+  });
+  return onMessage;
+}
