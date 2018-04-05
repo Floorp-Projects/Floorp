4ad526970e6d5187daf9a733685716324cfb6448
Brian Birtles <birtles@gmail.com>
2018-04-05 10:13:21 +0900

    Bug 1444301 - Add accelerator support to MenuItem component; r=jryans
    MozReview-Commit-ID: IMbZ73ty5cM

--- devtools/client/framework/components/toolbox-toolbar.js
+++ devtools/client/framework/components/toolbox-toolbar.js
@@ -282,7 +282,7 @@ function showMeatballMenu(menuButton, {hostTypes, selectTool, L10N, toolbox}) {
   menu.append(new MenuItem({
     id: "toolbox-meatball-menu-settings",
     label: L10N.getStr("toolbox.meatballMenu.settings.label"),
-    // TODO: Show "F1" acceltext once MenuItem supports it.
+    accelerator: L10N.getStr("toolbox.help.key"),
     click: () => selectTool("options"),
   }));
 
