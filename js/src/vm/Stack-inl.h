ace2071bbfb6f7795fda29d5e65b17f37783df60
Gurzau Raul <rgurzau@mozilla.com>
2018-04-08 04:20:09 +0300

    Backed out 4 changesets (bug 1445973) for failing on tests/saved-stacks/bug-1445973-quick.js on a CLOSED TREE
    Backed out changeset 2ed6d9d910b0 (bug 1445973)
    Backed out changeset 014fdec944a9 (bug 1445973)
    Backed out changeset 1a11067ab1e6 (bug 1445973)
    Backed out changeset e427340ec249 (bug 1445973)

--- js/src/vm/Stack-inl.h
+++ js/src/vm/Stack-inl.h
@@ -1037,16 +1037,6 @@ LiveSavedFrameCache::FramePtr::setHasCachedSavedFrame() {
     ptr.match(SetHasCachedMatcher());
 }
 
-struct LiveSavedFrameCache::FramePtr::ClearHasCachedMatcher {
-    template<typename Frame>
-    void match(Frame* f) { f->clearHasCachedSavedFrame(); }
-};
-
-inline void
-LiveSavedFrameCache::FramePtr::clearHasCachedSavedFrame() {
-    ptr.match(ClearHasCachedMatcher());
-}
-
 } /* namespace js */
 
 #endif /* vm_Stack_inl_h */
