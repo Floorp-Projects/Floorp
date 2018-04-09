ee4295d73c13b5550f741da23d93f9e70089552e
Alex Gaynor <agaynor@mozilla.com>
2018-04-09 10:48:09 -0400

    Bug 1445234 - Use ContiguousEnumSerializer for serializing nsCursor. r=smaug, a=RyanVM

--- widget/WidgetMessageUtils.h
+++ widget/WidgetMessageUtils.h
@@ -34,6 +34,12 @@ struct ParamTraits<LookAndFeelInt>
   }
 };
 
+template<>
+struct ParamTraits<nsCursor>
+  : public ContiguousEnumSerializer<nsCursor, eCursor_standard, eCursorCount>
+{
+};
+
 } // namespace IPC
 
 #endif // WidgetMessageUtils_h
