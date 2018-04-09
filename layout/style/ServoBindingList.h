7224186265965ee9b615f13c6478981a245fdcc4
Xidorn Quan <me@upsuper.org>
2018-04-09 14:33:30 +1000

    Bug 1452534 part 1 - Use Servo code to check whether a property is inherited. r=emilio
    MozReview-Commit-ID: GtJWDmPlsAq

--- layout/style/ServoBindingList.h
+++ layout/style/ServoBindingList.h
@@ -860,6 +860,8 @@ SERVO_BINDING_FUNC(Servo_ParseFontShorthandForMatching, bool,
 
 SERVO_BINDING_FUNC(Servo_Property_IsShorthand, bool,
                    const nsACString* name, bool* found);
+SERVO_BINDING_FUNC(Servo_Property_IsInherited, bool,
+                   const nsACString* name);
 SERVO_BINDING_FUNC(Servo_PseudoClass_GetStates, uint64_t,
                    const nsACString* name)
 
