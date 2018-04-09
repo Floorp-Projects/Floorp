3a337abf8a2d1a0a5903b496e0c46614e9e1feee
Marco Bonardo <mbonardo@mozilla.com>
2018-04-09 15:38:45 +0200

    Bug 1452621 - Cleanup some tag queries related code. r=standard8
    MozReview-Commit-ID: L8L3i5W1CHe
    
    --HG--
    extra : rebase_source : 50b547644e39238402985072d400022c83a33a7a

--- browser/components/places/tests/browser/browser_bookmarkProperties_readOnlyRoot.js
+++ browser/components/places/tests/browser/browser_bookmarkProperties_readOnlyRoot.js
@@ -1,16 +1,16 @@
 "use strict";
 
-add_task(async function() {
-  info("Bug 479348 - Properties on a root should be read-only.");
-
+add_task(async function test_dialog() {
+  info("Bug 479348 - Properties dialog on a root should be read-only.");
   await withSidebarTree("bookmarks", async function(tree) {
     tree.selectItems([PlacesUtils.bookmarks.unfiledGuid]);
-    Assert.ok(tree.controller.isCommandEnabled("placesCmd_show:info"),
-              "'placesCmd_show:info' on current selected node is enabled");
+    Assert.ok(!tree.controller.isCommandEnabled("placesCmd_show:info"),
+              "'placesCmd_show:info' on current selected node is disabled");
 
     await withBookmarksDialog(
       true,
       function openDialog() {
+        // Even if the cmd is disabled, we can execute it regardless.
         tree.controller.doCommand("placesCmd_show:info");
       },
       async function test(dialogWin) {
@@ -28,4 +28,26 @@ add_task(async function() {
       }
     );
   });
+});
+
+add_task(async function test_library() {
+  info("Bug 479348 - Library info pane on a root should be read-only.");
+  let library = await promiseLibrary("UnfiledBookmarks");
+  registerCleanupFunction(async function() {
+    await promiseLibraryClosed(library);
+  });
+  let PlacesOrganizer = library.PlacesOrganizer;
+  let tree = PlacesOrganizer._places;
+  tree.focus();
+  Assert.ok(!tree.controller.isCommandEnabled("placesCmd_show:info"),
+            "'placesCmd_show:info' on current selected node is disabled");
+
+  // Check that the pane is read-only.
+  Assert.ok(library.gEditItemOverlay.readOnly, "Info pane is read-only");
+
+  // Check that name picker is read only
+  let namepicker = library.document.getElementById("editBMPanel_namePicker");
+  Assert.ok(namepicker.readOnly, "Name field is read-only");
+  Assert.equal(namepicker.value,
+    PlacesUtils.getString("OtherBookmarksFolderTitle"), "Node title is correct");
 });
