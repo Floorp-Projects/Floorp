a60233ba3003b108081d00d2d03d87920d7975c9
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-04-09 12:26:39 +0200

    Bug 1403448 - Migrate longString test to the new frontend; r=Honza.
    browser_webconsole_longstring_hang.js is renamed and browser_webconsole_longstring_expand.js
    is removed since the test were doing the same thing.
    The support file for the test is no more used, so we remove it as well.
    
    MozReview-Commit-ID: Fb2JVtzWSA5

---
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_longstring.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that very long strings can be expanded and collapsed, and do not hang the browser.
+
+"use strict";
+
+const TEST_URI = "data:text/html,<meta charset=utf8>Test LongString hang";
+
+const LONGSTRING =
+  `foobar${"a".repeat(9000)}foobaz${"abbababazomglolztest".repeat(100)}boom!`;
+
+add_task(async function() {
+  let hud = await openNewTabAndConsole(TEST_URI);
+
+  info("Log a longString");
+  let onMessage = waitForMessage(hud, LONGSTRING.slice(0, 50));
+  ContentTask.spawn(gBrowser.selectedBrowser, LONGSTRING, str => {
+    content.console.log(str);
+  });
+
+  const {node} = await onMessage;
+  const arrow = node.querySelector(".arrow");
+  ok(arrow, "longString expand arrow is shown");
+
+  info("wait for long string expansion");
+  const onLongStringFullTextDisplayed = waitFor(() => findMessage(hud, LONGSTRING));
+  arrow.click();
+  await onLongStringFullTextDisplayed;
+
+  ok(true, "The full text of the longString is displayed");
+
+  info("wait for long string collapse");
+  const onLongStringCollapsed = waitFor(() => !findMessage(hud, LONGSTRING));
+  arrow.click();
+  await onLongStringCollapsed;
+
+  ok(true, "The longString can be collapsed");
+});
