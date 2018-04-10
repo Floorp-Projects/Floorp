833fe43cb629b001eb01f9f42d67503fb9a4c4a8
Alexander Surkov <surkov.alexander@gmail.com>
2018-04-10 14:02:19 -0400

    Bug 1446671 - convert mochitest/textcaret/test_browserui.xul test into a browser test, r=yzen, gijs

---
+++ accessible/tests/browser/events/browser_test_textcaret.js
@@ -0,0 +1,43 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Caret move events checker.
+ */
+function caretMoveChecker(target, caretOffset) {
+  return function(event) {
+    let cmEvent = event.QueryInterface(nsIAccessibleCaretMoveEvent);
+    return cmEvent.accessible == getAccessible(target) && cmEvent.caretOffset == caretOffset;
+  };
+}
+
+async function checkURLBarCaretEvents() {
+  let url = "about:mozilla";
+  let newWin = await BrowserTestUtils.openNewBrowserWindow({ url });
+
+  let urlbarInputEl = newWin.document.getElementById("urlbar").inputField;
+  let urlbarInput = getAccessible(urlbarInputEl, [ nsIAccessibleText ]);
+
+  let onCaretMove = waitForEvents([
+    [ EVENT_TEXT_CARET_MOVED, caretMoveChecker(urlbarInput, url.length) ],
+    [ EVENT_FOCUS, urlbarInput ]
+  ]);
+
+  urlbarInput.caretOffset = -1;
+  await onCaretMove;
+  ok(true, "Caret move in URL bar #1");
+
+  onCaretMove = waitForEvent(
+    EVENT_TEXT_CARET_MOVED, caretMoveChecker(urlbarInput, 0)
+  );
+
+  urlbarInput.caretOffset = 0;
+  await onCaretMove;
+  ok(true, "Caret move in URL bar #2");
+
+  await BrowserTestUtils.closeWindow(newWin);
+}
+
+add_task(checkURLBarCaretEvents);
