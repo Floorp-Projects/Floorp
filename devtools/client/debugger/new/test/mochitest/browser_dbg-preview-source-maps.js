3fd510c7b5d3dc7d2160689479d220534a0432e0
Jason Laster <jason.laster.11@gmail.com>
2018-04-10 13:59:38 -0400

    Bug 1453066 - Update Debugger Frontend v34. r=jdescottes

--- devtools/client/debugger/new/test/mochitest/browser_dbg-preview-source-maps.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-preview-source-maps.js
@@ -1,24 +1,11 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-function getCoordsFromPosition(cm, { line, ch }) {
-  return cm.charCoords({ line: ~~line, ch: ~~ch });
-}
 
-function hoverAtPos(dbg, { line, ch }) {
-  info(`Hovering at (${line}, ${ch})\n`);
-  const cm = getCM(dbg);
-  const coords = getCoordsFromPosition(cm, { line: line - 1, ch });
-  const tokenEl = dbg.win.document.elementFromPoint(coords.left, coords.top);
-  const previewed = waitForDispatch(dbg, "SET_PREVIEW");
-  tokenEl.dispatchEvent(
-    new MouseEvent("mouseover", {
-      bubbles: true,
-      cancelable: true,
-      view: dbg.win
-    })
-  );
-  return previewed;
+async function assertNoTooltip(dbg) {
+  await waitForTime(200);
+  const el = findElement(dbg, "tooltip");
+  is(el, null, "Tooltip should not exist")
 }
 
 function assertPreviewTooltip(dbg, { result, expression }) {
@@ -58,12 +45,21 @@ add_task(async function() {
   await waitForPaused(dbg);
   await waitForSelectedSource(dbg, "times2");
 
-  await hoverAtPos(dbg, { line: 2, ch: 9 });
-  assertPreviewTooltip(dbg, { result: 4, expression: "x" });
+  info(`Test previewing in the original location`)
+  await assertPreviews(dbg, [
+    { line: 2, column: 10, result: 4, expression: "x" }
+  ]);
 
+  info(`Test previewing in the generated location`)
   await dbg.actions.jumpToMappedSelectedLocation();
   await waitForSelectedSource(dbg, "bundle.js");
+  await assertPreviews(dbg, [
+    { line: 70, column: 11, result: 4, expression: "x" }
+  ]);
 
-  await hoverAtPos(dbg, { line: 70, ch: 10 });
-  assertPreviewTooltip(dbg, { result: 4, expression: "x" });
+
+  info(`Test that you can not preview in another original file`);
+  await selectSource(dbg, "output");
+  await hoverAtPos(dbg, { line: 2, ch: 16 });
+  await assertNoTooltip(dbg)
 });
