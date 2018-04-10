977cfb469926506c22af6e1272fbdb0d15afea11
Michael Ratcliffe <mratcliffe@mozilla.com>
2018-04-10 13:31:06 +0100

    Bug 1452929 - Fix eslint errors in devtools/server/tests/browser/browser_storage_browser_toolbox_indexeddb.js r=miker
    MozReview-Commit-ID: B5hgHqMcKSQ

--- devtools/server/tests/browser/browser_storage_browser_toolbox_indexeddb.js
+++ devtools/server/tests/browser/browser_storage_browser_toolbox_indexeddb.js
@@ -2,6 +2,8 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
+/* import-globals-from storage-helpers.js */
+
 "use strict";
 
 const { StorageFront } = require("devtools/shared/fronts/storage");
