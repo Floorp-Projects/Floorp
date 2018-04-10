c8bbb590af257580c7e30692873ddc262929fddd
arthur.iakab <aiakab@mozilla.com>
2018-04-10 22:09:31 +0300

    Backed out 2 changesets (bug 1449885) For frequently failing devtools on browser_parsable_css.js on a CLOSED TREE
    Backed out changeset 47b3480dc0af (bug 1449885)
    Backed out changeset 7ee122acafd1 (bug 1449885)

--- devtools/client/inspector/rules/models/rule.js
+++ devtools/client/inspector/rules/models/rule.js
@@ -252,7 +252,7 @@ Rule.prototype = {
       // relevant properties here.
       for (let index in modifications.changedDeclarations) {
         let newValue = modifications.changedDeclarations[index];
-        this.textProps[index].updateValue(newValue);
+        this.textProps[index].noticeNewValue(newValue);
       }
       // Recompute and redisplay the computed properties.
       for (let prop of this.textProps) {
