dbc2d6cbca9ead88e567768bc4ae5efaf91f362e
Tooru Fujisawa <arai_a@mac.com>
2018-04-10 12:18:34 +0900

    Bug 1448531 - Part 1: Use BrowserTestUtils.waitForNewTab in tests for drag-and-drop URLs for tab. r=enndeakin

--- browser/base/content/test/general/browser_newTabDrop.js
+++ browser/base/content/test/general/browser_newTabDrop.js
@@ -1,3 +1,5 @@
+const ANY_URL = undefined;
+
 registerCleanupFunction(async function cleanup() {
   while (gBrowser.tabs.length > 1) {
     BrowserTestUtils.removeTab(gBrowser.tabs[gBrowser.tabs.length - 1]);
@@ -18,39 +20,85 @@ add_task(async function test_setup() {
 });
 
 // New Tab Button opens any link.
-add_task(async function() { await dropText("mochi.test/first", 1); });
-add_task(async function() { await dropText("javascript:'bad'", 1); });
-add_task(async function() { await dropText("jAvascript:'bad'", 1); });
-add_task(async function() { await dropText("mochi.test/second", 1); });
-add_task(async function() { await dropText("data:text/html,bad", 1); });
-add_task(async function() { await dropText("mochi.test/third", 1); });
+add_task(async function single_url() {
+  await dropText("mochi.test/first",
+                 ["http://www.mochi.test/first"]);
+});
+add_task(async function single_javascript() {
+  await dropText("javascript:'bad'",
+                 ["javascript:'bad'"]);
+});
+add_task(async function single_javascript_capital() {
+  await dropText("jAvascript:'bad'",
+                 ["javascript:'bad'"]);
+});
+add_task(async function single_url2() {
+  await dropText("mochi.test/second",
+                 ["http://www.mochi.test/second"]);
+});
+add_task(async function single_data_url() {
+  await dropText("data:text/html,bad",
+                 ["data:text/html,bad"]);
+});
+add_task(async function single_url3() {
+  await dropText("mochi.test/third",
+                 ["http://www.mochi.test/third"]);
+});
 
 // Single text/plain item, with multiple links.
-add_task(async function() { await dropText("mochi.test/1\nmochi.test/2", 2); });
-add_task(async function() { await dropText("javascript:'bad1'\nmochi.test/3", 2); });
-add_task(async function() { await dropText("mochi.test/4\ndata:text/html,bad1", 2); });
+add_task(async function multiple_urls() {
+  await dropText("mochi.test/1\nmochi.test/2",
+                 [
+                   "http://www.mochi.test/1",
+                   "http://www.mochi.test/2",
+                 ]);
+});
+add_task(async function multiple_urls_javascript() {
+  await dropText("javascript:'bad1'\nmochi.test/3",
+                 [
+                   "javascript:'bad1'",
+                   "http://www.mochi.test/3",
+                 ]);
+});
+add_task(async function multiple_urls_data() {
+  await dropText("mochi.test/4\ndata:text/html,bad1",
+                 [
+                   "http://www.mochi.test/4",
+                   "data:text/html,bad1",
+                 ]);
+});
 
 // Multiple text/plain items, with single and multiple links.
-add_task(async function() {
+add_task(async function multiple_items_single_and_multiple_links() {
   await drop([[{type: "text/plain",
                 data: "mochi.test/5"}],
               [{type: "text/plain",
-                data: "mochi.test/6\nmochi.test/7"}]], 3);
+                data: "mochi.test/6\nmochi.test/7"}]],
+             [
+               "http://www.mochi.test/5",
+               "http://www.mochi.test/6",
+               "http://www.mochi.test/7",
+             ]);
 });
 
 // Single text/x-moz-url item, with multiple links.
 // "text/x-moz-url" has titles in even-numbered lines.
-add_task(async function() {
+add_task(async function single_moz_url_multiple_links() {
   await drop([[{type: "text/x-moz-url",
-                data: "mochi.test/8\nTITLE8\nmochi.test/9\nTITLE9"}]], 2);
+                data: "mochi.test/8\nTITLE8\nmochi.test/9\nTITLE9"}]],
+             [
+               "http://www.mochi.test/8",
+               "http://www.mochi.test/9",
+             ]);
 });
 
 // Single item with multiple types.
-add_task(async function() {
+add_task(async function single_item_multiple_types() {
   await drop([[{type: "text/plain",
                 data: "mochi.test/10"},
                {type: "text/x-moz-url",
-                data: "mochi.test/11\nTITLE11"}]], 1);
+                data: "mochi.test/11\nTITLE11"}]],
+             ["http://www.mochi.test/11"]);
 });
 
 // Warn when too many URLs are dropped.
@@ -59,7 +107,14 @@ add_task(async function multiple_tabs_under_max() {
   for (let i = 0; i < 5; i++) {
     urls.push("mochi.test/multi" + i);
   }
-  await dropText(urls.join("\n"), 5);
+  await dropText(urls.join("\n"),
+                 [
+                   "http://www.mochi.test/multi0",
+                   "http://www.mochi.test/multi1",
+                   "http://www.mochi.test/multi2",
+                   "http://www.mochi.test/multi3",
+                   "http://www.mochi.test/multi4",
+                 ]);
 });
 add_task(async function multiple_tabs_over_max_accept() {
   await pushPrefs(["browser.tabs.maxOpenBeforeWarn", 4]);
@@ -70,7 +125,14 @@ add_task(async function multiple_tabs_over_max_accept() {
   for (let i = 0; i < 5; i++) {
     urls.push("mochi.test/accept" + i);
   }
-  await dropText(urls.join("\n"), 5);
+  await dropText(urls.join("\n"),
+                 [
+                   "http://www.mochi.test/accept0",
+                   "http://www.mochi.test/accept1",
+                   "http://www.mochi.test/accept2",
+                   "http://www.mochi.test/accept3",
+                   "http://www.mochi.test/accept4",
+                 ]);
 
   await confirmPromise;
 
@@ -85,7 +147,7 @@ add_task(async function multiple_tabs_over_max_cancel() {
   for (let i = 0; i < 5; i++) {
     urls.push("mochi.test/cancel" + i);
   }
-  await dropText(urls.join("\n"), 0);
+  await dropText(urls.join("\n"), []);
 
   await confirmPromise;
 
@@ -102,7 +164,12 @@ add_task(async function multiple_urls() {
     mochi.test/urls3
     non url1
     non url2
-`, 4);
+`, [
+  "http://www.mochi.test/urls0",
+  "http://www.mochi.test/urls1",
+  "http://www.mochi.test/urls2",
+  "http://www.mochi.test/urls3",
+]);
 });
 
 // Open single search if there's no URL.
@@ -111,16 +178,18 @@ add_task(async function multiple_text() {
     non url0
     non url1
     non url2
-`, 1);
+`, [
+  ANY_URL
+]);
 });
 
-function dropText(text, expectedTabOpenCount = 0) {
-  return drop([[{type: "text/plain", data: text}]], expectedTabOpenCount);
+function dropText(text, expectedURLs) {
+  return drop([[{type: "text/plain", data: text}]], expectedURLs);
 }
 
-async function drop(dragData, expectedTabOpenCount = 0) {
+async function drop(dragData, expectedURLs) {
   let dragDataString = JSON.stringify(dragData);
-  info(`Starting test for datagData:${dragDataString}; expectedTabOpenCount:${expectedTabOpenCount}`);
+  info(`Starting test for dragData:${dragDataString}; expectedURLs.length:${expectedURLs.length}`);
   let EventUtils = {};
   Services.scriptloader.loadSubScript("chrome://mochikit/content/tests/SimpleTest/EventUtils.js", EventUtils);
 
@@ -131,36 +200,17 @@ async function drop(dragData, expectedTabOpenCount = 0) {
   ok(newTabButton, "New Tab button exists");
 
   let awaitDrop = BrowserTestUtils.waitForEvent(newTabButton, "drop");
-  let actualTabOpenCount = 0;
-  let openedTabs = [];
-  let promiseTabsOpen = null;
-  let resolve = null;
-  let handler = function(event) {
-    openedTabs.push(event.target);
-    actualTabOpenCount++;
-    if (actualTabOpenCount == expectedTabOpenCount && resolve) {
-      resolve();
-    }
-  };
-  gBrowser.tabContainer.addEventListener("TabOpen", handler);
-  if (expectedTabOpenCount) {
-    promiseTabsOpen = new Promise(r => { resolve = r; });
-  }
+
+  let loadedPromises = expectedURLs.map(
+    url => BrowserTestUtils.waitForNewTab(gBrowser, url, false, true));
 
   EventUtils.synthesizeDrop(dragSrcElement, newTabButton, dragData, "link", window);
 
-  if (promiseTabsOpen) {
-    await promiseTabsOpen;
-    info("Got TabOpen event");
-    for (let tab of openedTabs) {
-      BrowserTestUtils.removeTab(tab);
-    }
+  let tabs = await Promise.all(loadedPromises);
+  for (let tab of tabs) {
+    BrowserTestUtils.removeTab(tab);
   }
 
   await awaitDrop;
   ok(true, "Got drop event");
-
-  gBrowser.tabContainer.removeEventListener("TabOpen", handler);
-  is(actualTabOpenCount, expectedTabOpenCount,
-     "No other tabs are opened");
 }
