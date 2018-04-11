541d6f6f0043bac10dc5286a516f12c46fe70c72
Kyle Machulis <kyle@nonpolynomial.com>
2018-04-11 16:16:13 -0700

    Bug 1453517 - Remove nsPopupWindowManager and related XPCOM bits; r=bz
    MozReview-Commit-ID: JIDdZfnNN8E
    
    --HG--
    extra : rebase_source : 8b92fec383a21f80fceb87502e9a7accdb7dd74f

--- extensions/cookie/nsCookieModule.cpp
+++ extensions/cookie/nsCookieModule.cpp
@@ -7,7 +7,6 @@
 #include "mozilla/ModuleUtils.h"
 #include "nsIServiceManager.h"
 #include "nsPermissionManager.h"
-#include "nsPopupWindowManager.h"
 #include "nsICategoryManager.h"
 #include "nsCookiePermission.h"
 #include "nsString.h"
@@ -15,24 +14,20 @@
 // Define the constructor function for the objects
 NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(nsIPermissionManager,
   nsPermissionManager::GetXPCOMSingleton)
-NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(nsPopupWindowManager, Init)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsCookiePermission)
 
 NS_DEFINE_NAMED_CID(NS_PERMISSIONMANAGER_CID);
-NS_DEFINE_NAMED_CID(NS_POPUPWINDOWMANAGER_CID);
 NS_DEFINE_NAMED_CID(NS_COOKIEPERMISSION_CID);
 
 
 static const mozilla::Module::CIDEntry kCookieCIDs[] = {
     { &kNS_PERMISSIONMANAGER_CID, false, nullptr, nsIPermissionManagerConstructor },
-    { &kNS_POPUPWINDOWMANAGER_CID, false, nullptr, nsPopupWindowManagerConstructor },
     { &kNS_COOKIEPERMISSION_CID, false, nullptr, nsCookiePermissionConstructor },
     { nullptr }
 };
 
 static const mozilla::Module::ContractIDEntry kCookieContracts[] = {
     { NS_PERMISSIONMANAGER_CONTRACTID, &kNS_PERMISSIONMANAGER_CID },
-    { NS_POPUPWINDOWMANAGER_CONTRACTID, &kNS_POPUPWINDOWMANAGER_CID },
     { NS_COOKIEPERMISSION_CONTRACTID, &kNS_COOKIEPERMISSION_CID },
     { nullptr }
 };
