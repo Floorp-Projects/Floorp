e992d4abf64ff1b2f651674817de1acab1cfec14
Sebastian Kaspari <s.kaspari@gmail.com>
2018-04-11 20:53:24 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/49: Updatesystem-engine module to implement concept-engine interfaces.

--- mobile/android/android-components/components/browser/engine-system/src/main/java/mozilla/components/engine/system/SystemEngine.kt
+++ mobile/android/android-components/components/browser/engine-system/src/main/java/mozilla/components/engine/system/SystemEngine.kt
@@ -4,6 +4,27 @@
 
 package mozilla.components.engine.system
 
+import android.content.Context
+import android.util.AttributeSet
 import mozilla.components.engine.Engine
+import mozilla.components.engine.EngineSession
+import mozilla.components.engine.EngineView
 
-class SystemEngine : Engine
+/**
+ * WebView-based implementation of the Engine interface.
+ */
+class SystemEngine : Engine {
+    /**
+     * Create a new WebView-based EngineView implementation.
+     */
+    override fun createView(context: Context, attrs: AttributeSet?): EngineView {
+        return SystemEngineView(context, attrs)
+    }
+
+    /**
+     * Create a new WebView-based EngineSession implementation.
+     */
+    override fun createSession(): EngineSession {
+        return SystemEngineSession()
+    }
+}
--- mobile/android/android-components/components/browser/engine-system/src/main/java/mozilla/components/engine/system/SystemEngineView.kt
+++ mobile/android/android-components/components/browser/engine-system/src/main/java/mozilla/components/engine/system/SystemEngineView.kt
@@ -4,6 +4,59 @@
 
 package mozilla.components.engine.system
 
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.FrameLayout
+import mozilla.components.engine.EngineSession
 import mozilla.components.engine.EngineView
+import java.lang.ref.WeakReference
 
-class SystemEngineView : EngineView
+/**
+ * WebView-based implementation of EngineView.
+ */
+class SystemEngineView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr), EngineView {
+    internal val currentWebView = createWebView(context)
+    private var session: SystemEngineSession? = null
+
+    init {
+        // Currently this implementation supports only a single WebView. Eventually this
+        // implementation should be able to maintain at least two WebView instances to be able to
+        // animate the views when switching sessions.
+        addView(currentWebView)
+    }
+
+    /**
+     * Render the content of the given session.
+     */
+    override fun render(session: EngineSession) {
+        val internalSession = session as SystemEngineSession
+        this.session = internalSession
+
+        internalSession.view = WeakReference(this)
+
+        internalSession.urlToLoad?.let {
+            currentWebView.loadUrl(internalSession.urlToLoad)
+            internalSession.urlToLoad = null
+        }
+    }
+
+    private fun createWebView(context: Context): WebView {
+        val webView = WebView(context)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                url?.let {
+                    session?.internalNotifyObservers { onLocationChange(it) }
+                }
+            }
+        }
+
+        return webView
+    }
+}
