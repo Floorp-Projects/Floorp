f5f6daf52966c59344563e866279ac5857d1a383
Sebastian Kaspari <s.kaspari@gmail.com>
2018-04-11 21:14:43 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/49: browser-toolbar: Add very simple mechanism to display URL.

--- mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/toolbar/BrowserToolbar.kt
+++ mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/toolbar/BrowserToolbar.kt
@@ -4,4 +4,27 @@
 
 package mozilla.components.toolbar
 
-class BrowserToolbar
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.TextView
+import mozilla.components.definition.toolbar.ToolbarConcept
+
+/**
+ * A customizable toolbar for browsers.
+ */
+class BrowserToolbar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr), ToolbarConcept {
+    private val urlView = TextView(context)
+
+    init {
+        addView(urlView)
+    }
+
+    override fun displayUrl(url: String) {
+        urlView.text = url
+    }
+}
