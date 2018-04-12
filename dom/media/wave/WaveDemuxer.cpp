6f723b5fe5d496ef61d48a91993b568c6ed4d2d6
Nathan Froyd <froydnj@mozilla.com>
2018-04-12 15:28:21 -0400

    Bug 1453437 - return UniquePtr from MediaRawData::CreateWriter; r=gerald
    Returning UniquePtr is nicer than returning raw pointers, and has the
    nice side effect of forcing us to clean up the uses of nsAutoPtr that
    were hanging about.

--- dom/media/wave/WaveDemuxer.cpp
+++ dom/media/wave/WaveDemuxer.cpp
@@ -12,7 +12,6 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/EndianUtils.h"
 #include "BufferReader.h"
-#include "nsAutoPtr.h"
 #include "VideoUtils.h"
 #include "TimeUnits.h"
 
@@ -515,7 +514,7 @@ WAVTrackDemuxer::GetNextChunk(const MediaByteRange& aRange)
   RefPtr<MediaRawData> datachunk = new MediaRawData();
   datachunk->mOffset = aRange.mStart;
 
-  nsAutoPtr<MediaRawDataWriter> chunkWriter(datachunk->CreateWriter());
+  UniquePtr<MediaRawDataWriter> chunkWriter(datachunk->CreateWriter());
   if (!chunkWriter->SetSize(aRange.Length())) {
     return nullptr;
   }
@@ -559,7 +558,7 @@ WAVTrackDemuxer::GetFileHeader(const MediaByteRange& aRange)
   RefPtr<MediaRawData> fileHeader = new MediaRawData();
   fileHeader->mOffset = aRange.mStart;
 
-  nsAutoPtr<MediaRawDataWriter> headerWriter(fileHeader->CreateWriter());
+  UniquePtr<MediaRawDataWriter> headerWriter(fileHeader->CreateWriter());
   if (!headerWriter->SetSize(aRange.Length())) {
     return nullptr;
   }
