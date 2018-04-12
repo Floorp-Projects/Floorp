f412eb58aeb9fe5c493edfe1eabc6c0956d0c882
Paul Adenot <paul@paul.cx>
2018-04-12 14:23:03 +0200

    Bug 1456115 - Re-serialize inbound NotifyPull. r=jya
    We made NotifyPull parallel to try to lower the load, and we initially measured
    an improvement. However, we did the measurements with a profiler that did an
    aggregation of the results. Our results had an high variance, so the mean load
    was in fact not meaningful.
    
    More careful measurement performed without doing any aggregation show that,
    under load, relying on the fact that the scheduler schedules the tasks on time
    is too risky, and that the code is fast enough to not have to parallelize.
    
    MozReview-Commit-ID: CMhSn8Sc0OO
    
    --HG--
    extra : rebase_source : cfb41f861089bce9e10446bee81c13f8565ba90e

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.cpp
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.cpp
@@ -2216,18 +2216,6 @@ public:
     NotifyPullImpl(aDesiredTime);
   }
 
-  RefPtr<SourceMediaStream::NotifyPullPromise> AsyncNotifyPull(
-    MediaStreamGraph* aGraph,
-    StreamTime aDesiredTime) override
-  {
-    RefPtr<PipelineListener> self = this;
-    return InvokeAsync(mTaskQueue, __func__, [self, aDesiredTime]() {
-      self->NotifyPullImpl(aDesiredTime);
-      return SourceMediaStream::NotifyPullPromise::CreateAndResolve(true,
-                                                                    __func__);
-    });
-  }
-
 private:
   ~PipelineListener()
   {
