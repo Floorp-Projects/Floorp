4cfc71aabac2f24399045a34f9dd3c1b18dc6fd8
Sebastian Kaspari <s.kaspari@gmail.com>
2018-04-12 10:35:22 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/49: (feature-toolbar) Add feature implementation connecting a toolbar implementation with the session module.

---
+++ mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarFeature.kt
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.feature.toolbar
+
+import mozilla.components.definition.toolbar.ToolbarConcept
+import mozilla.components.session.SessionManager
+
+/**
+ * Feature implementation for connecting a toolbar implementation with the session module.
+ */
+class ToolbarFeature(
+    sessionManager: SessionManager,
+    toolbar: ToolbarConcept
+) {
+    private val presenter = ToolbarPresenter(sessionManager, toolbar)
+
+    /**
+     * Start feature: App is in the foreground.
+     */
+    fun start() {
+        presenter.start()
+    }
+
+    /**
+     * Stop feature: App is in the background.
+     */
+    fun stop() {
+        presenter.stop()
+    }
+}
---
+++ mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarPresenter.kt
@@ -0,0 +1,60 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.feature.toolbar
+
+import mozilla.components.definition.toolbar.ToolbarConcept
+import mozilla.components.session.Session
+import mozilla.components.session.SessionManager
+
+/**
+ * Presenter implementation for a toolbar implementation in order to update the toolbar whenever
+ * the state of the selected session changes.
+ */
+class ToolbarPresenter(
+    private val sessionManager: SessionManager,
+    private val toolbar: ToolbarConcept
+) : SessionManager.Observer, Session.Observer {
+    var session: Session = sessionManager.selectedSession
+
+    /**
+     * Start presenter: Display data in toolbar.
+     */
+    fun start() {
+        session = sessionManager.selectedSession
+
+        sessionManager.register(this)
+        session.register(this)
+
+        initializeView()
+    }
+
+    /**
+     * Stop presenter from updating the view.
+     */
+    fun stop() {
+        sessionManager.unregister(this)
+        session.unregister(this)
+    }
+
+    /**
+     * A new session has been selected: Update toolbar to display data of new session.
+     */
+    override fun onSessionSelected(session: Session) {
+        this.session.unregister(this)
+
+        this.session = session
+        session.register(this)
+
+        initializeView()
+    }
+
+    private fun initializeView() {
+        toolbar.displayUrl(session.url)
+    }
+
+    override fun onUrlChanged() {
+        toolbar.displayUrl(session.url)
+    }
+}
