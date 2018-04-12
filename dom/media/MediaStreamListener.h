4b68d8dc1ff5114b8d82106442b49ddf8d0be786
Paul Adenot <paul@paul.cx>
2018-04-12 14:23:03 +0200

    Bug 1456115 - Re-serialize inbound NotifyPull. r=jya
    We made NotifyPull parallel to try to lower the load, and we initially measured
    an improvement. However, we did the measurements with a profiler that did an
    aggregation of the results. Our results had an high variance, so the mean load
    was in fact not meaningful.
    
    More careful measurement performed without doing any aggregation show that,
    under load, relying on the fact that the scheduler schedules the tasks on time
    is too risky, and that the code is fast enough to not have to parallelize.
    
    MozReview-Commit-ID: CMhSn8Sc0OO

--- dom/media/MediaStreamListener.h
+++ dom/media/MediaStreamListener.h
@@ -61,14 +61,6 @@ public:
    * some reason, then data before aDesiredTime may not be played immediately.
    */
   virtual void NotifyPull(MediaStreamGraph* aGraph, StreamTime aDesiredTime) {}
-  virtual RefPtr<SourceMediaStream::NotifyPullPromise> AsyncNotifyPull(
-    MediaStreamGraph* aGraph,
-    StreamTime aDesiredTime)
-  {
-    NotifyPull(aGraph, aDesiredTime);
-    return SourceMediaStream::NotifyPullPromise::CreateAndResolve(true,
-                                                                  __func__);
-  }
 
   enum Blocking {
     BLOCKED,
