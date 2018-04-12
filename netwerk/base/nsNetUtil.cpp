cc9b64914475e4f97b3b2a14a785bb5921e6396e
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2018-04-12 12:52:51 +0200

    Bug 1452496 - Discard same-site cookie in cross site context. r=valentin, a=jcristau

--- netwerk/base/nsNetUtil.cpp
+++ netwerk/base/nsNetUtil.cpp
@@ -2152,7 +2152,7 @@ NS_IsSafeTopLevelNav(nsIChannel* aChannel)
   return requestHead->IsSafeMethod();
 }
 
-bool NS_IsTopLevelForeign(nsIChannel* aChannel)
+bool NS_IsSameSiteForeign(nsIChannel* aChannel, nsIURI* aHostURI)
 {
   if (!aChannel) {
     return false;
@@ -2161,8 +2161,14 @@ bool NS_IsTopLevelForeign(nsIChannel* aChannel)
   if (!loadInfo) {
     return false;
   }
-  if (loadInfo->GetExternalContentPolicyType() != nsIContentPolicy::TYPE_DOCUMENT) {
-    return false;
+  nsCOMPtr<nsIURI> uri;
+  if (loadInfo->GetExternalContentPolicyType() == nsIContentPolicy::TYPE_DOCUMENT) {
+    // for loads of TYPE_DOCUMENT we query the hostURI from the triggeringPricnipal
+    // which returns the URI of the document that caused the navigation.
+    loadInfo->TriggeringPrincipal()->GetURI(getter_AddRefs(uri));
+  }
+  else {
+    uri = aHostURI;
   }
 
   nsCOMPtr<mozIThirdPartyUtil> thirdPartyUtil =
@@ -2170,9 +2176,6 @@ bool NS_IsTopLevelForeign(nsIChannel* aChannel)
   if (!thirdPartyUtil) {
     return false;
   }
-
-  nsCOMPtr<nsIURI> uri;
-  loadInfo->TriggeringPrincipal()->GetURI(getter_AddRefs(uri));
 
   bool isForeign = false;
   thirdPartyUtil->IsThirdPartyChannel(aChannel, uri, &isForeign);
