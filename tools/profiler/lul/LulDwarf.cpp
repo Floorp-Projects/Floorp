7f841de419bb025413672e91eb9de32328dbd331
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2018-04-12 18:22:03 +0900

    Bug 1450185 - Implement DWARF stack walker for aarch64. r=jseward
    Since aarch64's DWARF doesn't have pc register, I use x29 (link register) if
    not first frame.
    
    I test by gtest on Linux/aarch64, and profiler works on Android/aarch64.
    
    EM_AARCH64 might not be defined on our builders since headers are old, so
    this define is needed.
    
    MozReview-Commit-ID: 8VDb5i0vwBT
    
    --HG--
    extra : rebase_source : abfe58624dabc2551deb03527db4be3b93490206

--- tools/profiler/lul/LulDwarf.cpp
+++ tools/profiler/lul/LulDwarf.cpp
@@ -1902,6 +1902,25 @@ unsigned int DwarfCFIToModule::RegisterNames::ARM() {
   return 13 * 8;
 }
 
+// Per ARM IHI 0057A, section 3.1
+unsigned int DwarfCFIToModule::RegisterNames::ARM64() {
+  /*
+   8 "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",
+   8 "x8",  "x9",  "x10", "x11", "x12", "x13", "x14", "x15",
+   8 "x16"  "x17", "x18", "x19", "x20", "x21", "x22", "x23",
+   8 "x24", "x25", "x26", "x27", "x28", "x29",  "x30","sp",
+   8 "",    "",    "",    "",    "",    "",    "",    "",
+   8 "",    "",    "",    "",    "",    "",    "",    "",
+   8 "",    "",    "",    "",    "",    "",    "",    "",
+   8 "",    "",    "",    "",    "",    "",    "",    "",
+   8 "v0",  "v1",  "v2",  "v3",  "v4",  "v5",  "v6",  "v7",
+   8 "v8",  "v9",  "v10", "v11", "v12", "v13", "v14", "v15",
+   8 "v16", "v17", "v18", "v19", "v20", "v21", "v22,  "v23",
+   8 "v24", "x25", "x26,  "x27", "v28", "v29", "v30", "v31",
+  */
+  return 12 * 8;
+}
+
 unsigned int DwarfCFIToModule::RegisterNames::MIPS() {
   /*
    8 "$zero", "$at",  "$v0",  "$v1",  "$a0",   "$a1",  "$a2",  "$a3",
