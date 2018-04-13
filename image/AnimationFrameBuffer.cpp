2e06ac3032cfbe56adbc04bf223b01834c50e3e8
Andrew Osmond <aosmond@mozilla.com>
2018-04-13 10:58:52 -0400

    Bug 1444537 - Part 3. Fix how redecode errors could cause animated image state inconsistencies. r=tnikkel
    We can discard frames from an animated image if the memory footprint
    exceeds the threshold. This will cause us to redecode frames on demand
    instead. However decoders can fail to produce the same results on
    subsequent runs due to differences in memory pressure, etc. If this
    happens our state can get inconsistent. In particular, if we keep
    failing on the first frame, we end up in an infinite loop on the decoder
    thread.
    
    Since we don't have the owning image to signal, as we had to release our
    reference to it after the first pass, we can do little but stop decoding.
    From the user's perspective, the animation will come to a stop.

--- image/AnimationFrameBuffer.cpp
+++ image/AnimationFrameBuffer.cpp
@@ -17,6 +17,7 @@ AnimationFrameBuffer::AnimationFrameBuffer()
   , mInsertIndex(0)
   , mGetIndex(0)
   , mSizeKnown(false)
+  , mRedecodeError(false)
 { }
 
 void
@@ -71,7 +72,16 @@ AnimationFrameBuffer::Insert(RawAccessFrameRef&& aFrame)
     // and we did not keep all of the frames. Replace whatever is there
     // (probably an empty frame) with the new frame.
     MOZ_ASSERT(MayDiscard());
-    MOZ_ASSERT(mInsertIndex < mFrames.Length());
+
+    // The first decode produced fewer frames than the redecodes, presumably
+    // because it hit an out-of-memory error which later attempts avoided. Just
+    // stop the animation because we can't tell the image that we have more
+    // frames now.
+    if (mInsertIndex >= mFrames.Length()) {
+      mRedecodeError = true;
+      mPending = 0;
+      return false;
+    }
 
     if (mInsertIndex > 0) {
       MOZ_ASSERT(!mFrames[mInsertIndex]);
@@ -127,9 +137,23 @@ AnimationFrameBuffer::Insert(RawAccessFrameRef&& aFrame)
 bool
 AnimationFrameBuffer::MarkComplete()
 {
+  // We may have stopped decoding at a different point in the animation than we
+  // did previously. That means the decoder likely hit a new error, e.g. OOM.
+  // This will prevent us from advancing as well, because we are missing the
+  // required frames to blend.
+  //
+  // XXX(aosmond): In an ideal world, we would be generating full frames, and
+  // the consumer of our data doesn't care about our internal state. It simply
+  // knows about the first frame, the current frame, and how long to display the
+  // current frame.
+  if (NS_WARN_IF(mInsertIndex != mFrames.Length())) {
+    MOZ_ASSERT(mSizeKnown);
+    mRedecodeError = true;
+    mPending = 0;
+  }
+
   // We reached the end of the animation, the next frame we get, if we get
   // another, will be the first frame again.
-  MOZ_ASSERT(mInsertIndex == mFrames.Length());
   mInsertIndex = 0;
 
   // Since we only request advancing when we want to resume at a certain point
@@ -231,7 +255,7 @@ AnimationFrameBuffer::AdvanceInternal()
     }
   }
 
-  if (!mSizeKnown || MayDiscard()) {
+  if (!mRedecodeError && (!mSizeKnown || MayDiscard())) {
     // Calculate how many frames we have requested ahead of the current frame.
     size_t buffered = mPending;
     if (mGetIndex > mInsertIndex) {
@@ -276,13 +300,6 @@ AnimationFrameBuffer::Reset()
     return false;
   }
 
-  // If we are over the threshold, then we know we will have missing frames in
-  // our buffer. The easiest thing to do is to drop everything but the first
-  // frame and go back to the initial state.
-  bool restartDecoder = mPending == 0;
-  mInsertIndex = 0;
-  mPending = 2 * mBatch;
-
   // Discard all frames besides the first, because the decoder always expects
   // that when it re-inserts a frame, it is not present. (It doesn't re-insert
   // the first frame.)
@@ -290,6 +307,16 @@ AnimationFrameBuffer::Reset()
     RawAccessFrameRef discard = Move(mFrames[i]);
   }
 
+  mInsertIndex = 0;
+
+  // If we hit an error after redecoding, we never want to restart decoding.
+  if (mRedecodeError) {
+    MOZ_ASSERT(mPending == 0);
+    return false;
+  }
+
+  bool restartDecoder = mPending == 0;
+  mPending = 2 * mBatch;
   return restartDecoder;
 }
 
