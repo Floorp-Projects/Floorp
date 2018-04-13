1987d705d78c697606376d73fce370d2bbc3e64a
Andrew Halberstadt <ahalberstadt@mozilla.com>
2018-04-13 18:22:46 -0400

    Bug 1445944 - [mozbase] Bump mozprofile and mozrunner version numbers r=rwood
    Neither package has any backwards incompatible changes. However, I decided to
    bump the major version for mozrunner as this is the first time it's switching
    to SemVer (mozprofile already made the switch). It seemed cleaner to have the
    switch happen after a major bump.
    
    In both cases, I've added major version upper limit guards to each dependency.
    This way we won't break these packages in the future when we land backwards
    incompatible changes to the dependendencies.
    
    MozReview-Commit-ID: 5SIpMGTS3cc

--- testing/mozbase/mozrunner/setup.py
+++ testing/mozbase/mozrunner/setup.py
@@ -8,18 +8,19 @@ import sys
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'mozrunner'
-PACKAGE_VERSION = '6.15'
+PACKAGE_VERSION = '7.0.0'
 
 desc = """Reliable start/stop/configuration of Mozilla Applications (Firefox, Thunderbird, etc.)"""
 
-deps = ['mozdevice >= 0.37',
-        'mozfile >= 1.0',
-        'mozinfo >= 0.7',
-        'mozlog >= 3.0',
-        'mozprocess >= 0.23',
-        'mozprofile >= 0.18',
-        'six >= 1.10.0',
-        ]
+deps = [
+    'mozdevice>=0.37,<1',
+    'mozfile==1.*',
+    'mozinfo>=0.7,<1',
+    'mozlog==3.*',
+    'mozprocess>=0.23,<1',
+    'mozprofile>=1.1.0,<2',
+    'six>=1.10.0,<2',
+]
 
 EXTRAS_REQUIRE = {'crash': ['mozcrash >= 1.0']}
 
