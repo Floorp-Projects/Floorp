2142dd16c85290f81621ef8ed52a2a9a1c77d248
Jeff Walden <jwalden@mit.edu>
2018-04-13 14:30:23 -0700

    |this->|-qualify some templated member functions to help some buggy compilers along.  No bug, r=me
    --HG--
    extra : rebase_source : 657ffe6afa1a5a6afe70a2090602b661a22890d4

--- js/public/UbiNode.h
+++ js/public/UbiNode.h
@@ -757,14 +757,14 @@ class Node {
     template<typename T>
     T* as() const {
         MOZ_ASSERT(isLive());
-        MOZ_ASSERT(is<T>());
+        MOZ_ASSERT(this->is<T>());
         return static_cast<T*>(base()->ptr);
     }
 
     template<typename T>
     T* asOrNull() const {
         MOZ_ASSERT(isLive());
-        return is<T>() ? static_cast<T*>(base()->ptr) : nullptr;
+        return this->is<T>() ? static_cast<T*>(base()->ptr) : nullptr;
     }
 
     // If this node refers to something that can be represented as a JavaScript
