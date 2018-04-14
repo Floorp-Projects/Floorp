943ee7be976c30659662cae75d75c3b504967239
Emilio Cobos Álvarez <emilio@crisal.io>
2018-04-14 15:04:53 +0200

    Bug 1454157: Tell layout about default content going away. r=smaug
    Pretty much the same way we do for Shadow DOM.
    
    There are a couple more places broken, but provided our fronted folks don't do
    weird stuff nothing bad should happen, and the assertion that this allows me to
    add should catch those if they do, so I'm punting on it for now.
    
    MozReview-Commit-ID: Bgr41C4zGgn

--- dom/xbl/XBLChildrenElement.cpp
+++ dom/xbl/XBLChildrenElement.cpp
@@ -44,6 +44,33 @@ XBLChildrenElement::BeforeSetAttr(int32_t aNamespaceID, nsAtom* aName,
   return nsXMLElement::BeforeSetAttr(aNamespaceID, aName, aValue, aNotify);
 }
 
+void
+XBLChildrenElement::DoRemoveDefaultContent(bool aNotify)
+{
+  // Default content is going away, need to tell layout about it first.
+  MOZ_ASSERT(HasChildren(), "Why bothering?");
+  MOZ_ASSERT(GetParentElement());
+
+  // We don't want to do this from frame construction while setting up the
+  // binding initially.
+  if (aNotify) {
+    Element* parent = GetParentElement();
+    if (nsIDocument* doc = parent->GetComposedDoc()) {
+      if (nsIPresShell* shell = doc->GetShell()) {
+        shell->DestroyFramesForAndRestyle(parent);
+      }
+    }
+  }
+
+  for (nsIContent* child = static_cast<nsINode*>(this)->GetFirstChild();
+       child;
+       child = child->GetNextSibling()) {
+    MOZ_ASSERT(!child->GetPrimaryFrame());
+    MOZ_ASSERT(!child->IsElement() || !child->AsElement()->HasServoData());
+    child->SetXBLInsertionPoint(nullptr);
+  }
+}
+
 } // namespace dom
 } // namespace mozilla
 
