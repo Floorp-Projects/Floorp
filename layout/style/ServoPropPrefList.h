9b466fa2d1e544c764eac920fc71df81001ce546
Xidorn Quan <me@upsuper.org>
2018-04-16 16:23:33 +1000

    Bug 1454297 - Remove CSS_PROP_LOGICAL and friends. r=emilio
    MozReview-Commit-ID: Euzbjf9qkmO
    
    --HG--
    extra : rebase_source : 9b76e8b303d89f65e28ced8cb15b7422f6d31775

--- layout/style/ServoPropPrefList.h
+++ layout/style/ServoPropPrefList.h
@@ -13,17 +13,13 @@ namespace mozilla {
     const bool SERVO_PREF_ENABLED_##id_ = !(sizeof(pref_) == 1);
 #define CSS_PROP_SHORTHAND(name_, id_, method_, flags_, pref_)  \
     const bool SERVO_PREF_ENABLED_##id_ = !(sizeof(pref_) == 1);
-#define CSS_PROP_LIST_INCLUDE_LOGICAL
 #include "nsCSSPropList.h"
-#undef CSS_PROP_LIST_INCLUDE_LOGICAL
 #undef CSS_PROP
 #undef CSS_PROP_SHORTHAND
 
 #define CSS_PROP_ALIAS(aliasname_, aliasid_, id_, method_, pref_)  \
     const bool SERVO_PREF_ENABLED_##aliasid_ = !(sizeof(pref_) == 1);
-#define CSS_PROP_ALIAS_LIST_INCLUDE_LOGICAL
 #include "nsCSSPropAliasList.h"
-#undef CSS_PROP_ALIAS_LIST_INCLUDE_LOGICAL
 #undef CSS_PROP_ALIAS
 
 }
