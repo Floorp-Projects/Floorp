b31b6bb0eea64c5507eeda56b6640ddf8b30df93
Alexandre Poirot <poirot.alex@gmail.com>
2018-04-16 06:56:06 -0700

    Bug 1454373 - Make defer module use DOM Promises. r=jryans
    MozReview-Commit-ID: 2QWSgHcM7aR

--- devtools/shared/defer.js
+++ devtools/shared/defer.js
@@ -4,9 +4,6 @@
 
 "use strict";
 
-// See bug 1273941 to understand this choice of promise.
-const Promise = require("promise");
-
 /**
  * Returns a deferred object, with a resolve and reject property.
  * https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/Promise.jsm/Deferred
