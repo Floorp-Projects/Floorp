5153b29720ef1873ac5fdbf64dbb936d1affb13d
Jeff Muizelaar <jmuizelaar@mozilla.com>
2018-04-16 18:00:35 -0400

    Bug 1454507. Avoid asserting that we don't end up with an empty result. r=mstange
    This adds a crash test that would previously trigger the assert.
    
    --HG--
    extra : rebase_source : f167a85bf219cf3eac1192be05c297fc5e383072

--- gfx/webrender_bindings/src/moz2d_renderer.rs
+++ gfx/webrender_bindings/src/moz2d_renderer.rs
@@ -238,7 +238,9 @@ fn dump_blob_index(blob: &[u8], dirty_rect: Box2d) {
 
 fn check_result(result: &[u8]) -> () {
     let mut index = BlobReader::new(result);
-    assert!(index.reader.has_more(), "Unexpectedly empty result. This blob should just have been deleted");
+    // we might get an empty result here because sub groups are not tightly bound
+    // and we'll sometimes have display items that end up with empty bounds in
+    // the blob image.
     while index.reader.has_more() {
         let e = index.read_entry();
         dlog!("result bounds: {} {} {:?}", e.end, e.extra_end, e.bounds);
