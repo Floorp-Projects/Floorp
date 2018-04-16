61b0638917aeb0f7e7a6ff1fa15ac7da75ea0c82
Matt Woodrow <mwoodrow@mozilla.com>
2018-04-16 14:23:47 +1200

    Bug 1453668 - Restrict the retained display list stacking context optimization to frames that are also containing blockks for position:fixed. r=miko
    This makes the optimization rarer, but is significantly simpler, since we should now be guaranteed that all placeholder frames have their out of flow frames in the same stacking context.
    
    MozReview-Commit-ID: 1Nf8Sx1dca7
    
    --HG--
    extra : rebase_source : 74856420fdf6108fe749c94418a20bc9faa6fc5e

--- layout/painting/RetainedDisplayListBuilder.cpp
+++ layout/painting/RetainedDisplayListBuilder.cpp
@@ -752,7 +752,8 @@ ProcessFrame(nsIFrame* aFrame, nsDisplayListBuilder& aBuilder,
     }
 
     if (currentFrame != aBuilder.RootReferenceFrame() &&
-        currentFrame->IsStackingContext()) {
+        currentFrame->IsStackingContext() &&
+        currentFrame->IsFixedPosContainingBlock()) {
       CRR_LOG("Frame belongs to stacking context frame %p\n", currentFrame);
       // If we found an intermediate stacking context with an existing display item
       // then we can store the dirty rect there and stop. If we couldn't find one then
