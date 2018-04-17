a7ef225557682773447552c21917bdcbd9d72b0a
Ben Kelly <ben@wanderview.com>
2018-04-17 07:46:14 -0700

    Bug 1454646 P1 Add a ServiceWorkerContainer::Inner scaffold. r=baku

---
+++ dom/serviceworkers/ServiceWorkerContainerImpl.h
@@ -0,0 +1,40 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_serviceworkercontainerimpl_h__
+#define mozilla_dom_serviceworkercontainerimpl_h__
+
+#include "ServiceWorkerContainer.h"
+
+namespace mozilla {
+namespace dom {
+
+// Lightweight serviceWorker APIs collection.
+class ServiceWorkerContainerImpl final : public ServiceWorkerContainer::Inner
+{
+  ~ServiceWorkerContainerImpl() = default;
+
+public:
+  RefPtr<ServiceWorkerRegistrationPromise>
+  Register(const nsAString& aScriptURL,
+           const RegistrationOptions& aOptions) override;
+
+  RefPtr<ServiceWorkerRegistrationPromise>
+  GetRegistration(const nsAString& aURL) override;
+
+  RefPtr<ServiceWorkerRegistrationListPromise>
+  GetRegistrations() override;
+
+  RefPtr<ServiceWorkerRegistrationPromise>
+  GetReady() override;
+
+  NS_INLINE_DECL_REFCOUNTING(ServiceWorkerContainerImpl, override)
+};
+
+} // namespace dom
+} // namespace mozilla
+
+#endif /* mozilla_dom_serviceworkercontainerimpl_h__ */
