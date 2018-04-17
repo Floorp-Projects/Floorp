08165faf0d07afe6443751383d4fc176945b09ef
Brindusan Cristian <cbrindusan@mozilla.com>
2018-04-17 19:08:21 +0300

    Backed out 2 changesets (bug 1436662) for build bustages on /installer/windows. CLOSED TREE
    Backed out changeset fcb756834abb (bug 1436662)
    Backed out changeset d6bf22099e21 (bug 1436662)

--- browser/build.mk
+++ browser/build.mk
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+installer:
+	@$(MAKE) -C browser/installer installer
+
 package:
 	@$(MAKE) -C browser/installer
 
