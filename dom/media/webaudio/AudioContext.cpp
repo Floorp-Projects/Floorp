73fad68e4df5a084e7a10826104c69819112911f
Tom Ritter <tom@mozilla.com>
2018-04-17 15:35:51 -0500

    Bug 1446346 - Do not clamp or jitter the AudioContext's CurrentTime if its interval is larger than our precision. r=padenot
    MozReview-Commit-ID: Bc1cto3pBKL

--- dom/media/webaudio/AudioContext.cpp
+++ dom/media/webaudio/AudioContext.cpp
@@ -662,25 +662,26 @@ AudioContext::DestinationStream() const
   }
   return nullptr;
 }
-
 double
 AudioContext::CurrentTime()
 {
   MediaStream* stream = Destination()->Stream();
+  double rawTime = stream->StreamTimeToSeconds(stream->GetCurrentTime());
 
-  if (!mIsStarted &&
-    stream->StreamTimeToSeconds(stream->GetCurrentTime()) == 0) {
-      return 0;
+  // CurrentTime increments in intervals of 128/sampleRate. If the Timer
+  // Precision Reduction is smaller than this interval, the jittered time
+  // can always be reversed to the raw step of the interval. In that case
+  // we can simply return the un-reduced time; and avoid breaking tests.
+  // We have to convert each variable into a common magnitude, we choose ms.
+  if ((128/mSampleRate) * 1000.0 > nsRFPService::TimerResolution() / 1000.0) {
+    return rawTime;
   }
-
   // The value of a MediaStream's CurrentTime will always advance forward; it will never
   // reset (even if one rewinds a video.) Therefore we can use a single Random Seed
   // initialized at the same time as the object.
   return nsRFPService::ReduceTimePrecisionAsSecs(
-    stream->StreamTimeToSeconds(stream->GetCurrentTime()),
-    GetRandomTimelineSeed());
+    rawTime, GetRandomTimelineSeed());
 }
-
 void AudioContext::DisconnectFromOwner()
 {
   mIsDisconnecting = true;
