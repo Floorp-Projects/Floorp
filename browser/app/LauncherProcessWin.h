e62252c75fccc5a22dd3160471e8c725fc07e346
Aaron Klotz <aklotz@mozilla.com>
2018-04-17 13:48:21 -0600

    Bug 1454745: Skeletal bootstrap process; r=mhowell

---
+++ browser/app/LauncherProcessWin.h
@@ -0,0 +1,18 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_LauncherProcessWin_h
+#define mozilla_LauncherProcessWin_h
+
+namespace mozilla {
+
+bool RunAsLauncherProcess(int& argc, wchar_t* argv[]);
+int LauncherMain(int argc, wchar_t* argv[]);
+
+} // namespace mozilla
+
+#endif // mozilla_LauncherProcessWin_h
+
