5001f488293cade215dc9f775c30904800cbbc88
Shane Caraveo <scaraveo@mozilla.com>
2018-04-17 10:06:13 -0500

    Bug 1450565 use GET for initial authorization request, r=mossop a=jcristau
    MozReview-Commit-ID: 70PQSeoxvOF

--- toolkit/components/extensions/ext-identity.js
+++ toolkit/components/extensions/ext-identity.js
@@ -14,7 +14,7 @@ var {
 const checkRedirected = (url, redirectURI) => {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
-    xhr.open("HEAD", url);
+    xhr.open("GET", url);
     // We expect this if the user has not authenticated.
     xhr.onload = () => {
       reject(0);
