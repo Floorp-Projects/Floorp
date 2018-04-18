a8eb5ba1511513486c63c312432e857517a5aa20
Dale Harvey <dale@arandomurl.com>
2018-04-18 15:48:31 +0100

    Bug 1454705 - Fix display of share panel in Address bar. r=Gijs
    MozReview-Commit-ID: 9lxU0plGqYa
    
    --HG--
    extra : rebase_source : 49e521087a4673ea87f64ec5a7be30eac3750393

--- browser/base/content/test/urlbar/browser_page_action_menu_share_mac.js
+++ browser/base/content/test/urlbar/browser_page_action_menu_share_mac.js
@@ -7,6 +7,8 @@
 /* global sinon */
 Services.scriptloader.loadSubScript("resource://testing-common/sinon-2.3.2.js");
 
+const URL = "http://example.org/";
+
 // Keep track of title of service we chose to share with
 let sharedTitle;
 let sharedUrl;
@@ -38,11 +40,7 @@ registerCleanupFunction(async function() {
 });
 
 add_task(async function shareURL() {
-  // Open an actionable page so that the main page action button appears.  (It
-  // does not appear on about:blank for example.)
-  let url = "http://example.org/";
-
-  await BrowserTestUtils.withNewTab(url, async () => {
+  await BrowserTestUtils.withNewTab(URL, async () => {
     // Open the panel.
     await promisePageActionPanelOpen();
 
@@ -67,6 +65,59 @@ add_task(async function shareURL() {
                  "Shared with the correct title");
     Assert.equal(sharedUrl, "http://example.org/",
                  "Shared correct URL");
+  });
+});
 
+add_task(async function shareURLAddressBar() {
+  await BrowserTestUtils.withNewTab(URL, async () => {
+    // Open pageAction panel
+    await promisePageActionPanelOpen();
+
+    // Right click the Share button
+    let contextMenuPromise = promisePanelShown("pageActionContextMenu");
+    let shareURLButton = document.getElementById("pageAction-panel-shareURL");
+    EventUtils.synthesizeMouseAtCenter(shareURLButton, {
+      type: "contextmenu",
+      button: 2,
+    });
+    await contextMenuPromise;
+
+    // Click "Add to Address Bar"
+    contextMenuPromise = promisePanelHidden("pageActionContextMenu");
+    let ctxMenuButton = document.querySelector("#pageActionContextMenu " +
+                                               ".pageActionContextMenuItem");
+    EventUtils.synthesizeMouseAtCenter(ctxMenuButton, {});
+    await contextMenuPromise;
+
+    // Wait for the Share button to be added
+    await BrowserTestUtils.waitForCondition(() => {
+      return document.getElementById("pageAction-urlbar-shareURL");
+    }, "Waiting for the share url button to be added to url bar");
+
+
+    // Press the Share button
+    let shareButton = document.getElementById("pageAction-urlbar-shareURL");
+    let viewPromise = promisePageActionPanelShown();
+    EventUtils.synthesizeMouseAtCenter(shareButton, {});
+    await viewPromise;
+
+    // Ensure we have share providers
+    let panel = document.getElementById("pageAction-urlbar-shareURL-subview-body");
+    Assert.equal(panel.childNodes.length, 1, "Has correct share receivers");
+
+    // Remove the Share URL button from the Address bar so we dont interfere
+    // with future tests
+    contextMenuPromise = promisePanelShown("pageActionContextMenu");
+    EventUtils.synthesizeMouseAtCenter(shareButton, {
+      type: "contextmenu",
+      button: 2,
+    });
+    await contextMenuPromise;
+
+    contextMenuPromise = promisePanelHidden("pageActionContextMenu");
+    ctxMenuButton = document.querySelector("#pageActionContextMenu " +
+                                           ".pageActionContextMenuItem");
+    EventUtils.synthesizeMouseAtCenter(ctxMenuButton, {});
+    await contextMenuPromise;
   });
 });
