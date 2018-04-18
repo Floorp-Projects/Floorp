9f3a97ebbe61d27b57e442f99d0ad8619bdd6d55
Tom Prince <mozilla@hocat.ca>
2018-04-18 11:02:02 -0600

    Bug 1442545: [partner-repack] Add repack partner builds; r=Callek, a=release
    Differential Revision: https://phabricator.services.mozilla.com/D980
    
    --HG--
    extra : transplant_source : %00%B9%D3S%A6%B7%21%A8%7D%B3%E2%3B%B5%F5%A9%04%F9%E8%D4%24

--- taskcluster/taskgraph/transforms/partner_repack.py
+++ taskcluster/taskgraph/transforms/partner_repack.py
@@ -9,15 +9,55 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from taskgraph.transforms.base import TransformSequence
 from taskgraph.util.schema import resolve_keyed_by
+from taskgraph.util.scriptworker import get_release_config
+from taskgraph.util.partners import check_if_partners_enabled
 
 
 transforms = TransformSequence()
+
+transforms.add(check_if_partners_enabled)
 
 
 @transforms.add
 def resolve_properties(config, tasks):
     for task in tasks:
-        for property in ("repack_manifests_url", ):
-            property = "worker.properties.{}".format(property)
+        for property in ("REPACK_MANIFESTS_URL", ):
+            property = "worker.env.{}".format(property)
             resolve_keyed_by(task, property, property, **config.params)
-            yield task
+
+        if task['worker']['env']['REPACK_MANIFESTS_URL'].startswith('git@'):
+            task.setdefault('scopes', []).append(
+                'secrets:get:project/releng/gecko/build/level-{level}/partner-github-ssh'.format(
+                    **config.params
+                )
+            )
+
+        yield task
+
+
+@transforms.add
+def make_label(config, tasks):
+    for task in tasks:
+        task['label'] = "{}-{}".format(config.kind, task['name'])
+        yield task
+
+
+@transforms.add
+def add_command_arguments(config, tasks):
+    release_config = get_release_config(config)
+    for task in tasks:
+        # add the MOZHARNESS_OPTIONS, eg version=61.0, build-number=1, platform=win64
+        task['run']['options'] = [
+            'version={}'.format(release_config['version']),
+            'build-number={}'.format(release_config['build_number']),
+            'platform={}'.format(task['attributes']['build_platform'].split('-')[0]),
+        ]
+
+        # The upstream taskIds are stored a special environment variable, because we want to use
+        # task-reference's to resolve dependencies, but the string handling of MOZHARNESS_OPTIONS
+        # blocks that. It's space-separated string of ids in the end.
+        task['worker']['env']['UPSTREAM_TASKIDS'] = {
+            'task-reference': ' '.join(['<{}>'.format(dep) for dep in task['dependencies']])
+        }
+
+        yield task
