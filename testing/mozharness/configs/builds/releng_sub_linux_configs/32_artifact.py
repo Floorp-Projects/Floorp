ff80ea77b4f99eab4f360f17e604cf66d3b47271
Gregory Szorc <gps@mozilla.com>
2018-04-18 13:47:17 -0700

    Bug 1455111 - Remove references to /tools/gcc-4.3.3; r=ted
    This path was used on buildbot. It doesn't exist in a Taskcluster/Docker
    world.
    
    MozReview-Commit-ID: KHeRGcfIftO
    
    --HG--
    extra : rebase_source : 75232e7a5f65db4f2ea216349c20922bdf5c408b

--- testing/mozharness/configs/builds/releng_sub_linux_configs/32_artifact.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/32_artifact.py
@@ -42,7 +42,6 @@ config = {
         'PATH': '/usr/local/bin:/usr/lib/ccache:\
 /bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:\
 /tools/python27/bin:/tools/python27-mercurial/bin:/home/cltbld/bin',
-        'LD_LIBRARY_PATH': "/tools/gcc-4.3.3/installed/lib",
     },
     'mozconfig_variant': 'artifact',
     #########################################################################
--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_debug_artifact.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_debug_artifact.py
@@ -39,7 +39,6 @@ config = {
         'PATH': '/usr/local/bin:/usr/lib64/ccache:/bin:\
 /usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:/tools/python27/bin:\
 /tools/python27-mercurial/bin:/home/cltbld/bin',
-        'LD_LIBRARY_PATH': "/tools/gcc-4.3.3/installed/lib64",
         ##
     },
     'mozconfig_variant': 'debug-artifact',
