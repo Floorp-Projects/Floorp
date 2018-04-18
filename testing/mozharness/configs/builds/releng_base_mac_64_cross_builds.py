192289983bf9369e90b072179f6374cae9aca460
Gregory Szorc <gps@mozilla.com>
2018-04-18 14:04:51 -0700

    Bug 1455111 - Remove references to /tools/python27; r=ted
    This path should be dead in a post-buildbot Taskcluster/Docker world.
    
    MozReview-Commit-ID: KRsRlfFADvD

--- testing/mozharness/configs/builds/releng_base_mac_64_cross_builds.py
+++ testing/mozharness/configs/builds/releng_base_mac_64_cross_builds.py
@@ -44,7 +44,7 @@ config = {
         ## 64 bit specific
         'PATH': '/usr/local/bin:/usr/lib64/ccache:/bin:'
                 '/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:'
-                '/tools/git/bin:/tools/python27/bin:'
+                '/tools/git/bin:'
                 '/home/cltbld/bin',
         ##
     },
--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_debug_artifact.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_debug_artifact.py
@@ -37,7 +37,7 @@ config = {
         'XPCOM_DEBUG_BREAK': 'stack-and-abort',
         ## 64 bit specific
         'PATH': '/usr/local/bin:/usr/lib64/ccache:/bin:\
-/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:/tools/python27/bin:\
+/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/git/bin:\
 /home/cltbld/bin',
         ##
     },
