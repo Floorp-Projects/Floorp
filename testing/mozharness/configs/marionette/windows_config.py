f74be9a080b6f1f98226f2a223f3315924a471d1
Tom Prince <mozilla@hocat.ca>
2018-04-19 16:46:50 -0600

    Bug 1452179: [mozharness] Get rid of references to private pypi server; r=bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D999

--- testing/mozharness/configs/marionette/windows_config.py
+++ testing/mozharness/configs/marionette/windows_config.py
@@ -12,11 +12,6 @@ config = {
         'hg': 'c:/mozilla-build/hg/hg',
     },
 
-    "find_links": [
-        "http://pypi.pvt.build.mozilla.org/pub",
-        "http://pypi.pub.build.mozilla.org/pub",
-    ],
-
     "buildbot_json_path": "buildprops.json",
 
     "default_actions": [
