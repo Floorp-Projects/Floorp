4e1f530c08499ca020977f2084335005fdf07c2b
Kris Maglione <maglione.k@gmail.com>
2018-04-19 12:49:19 -0700

    Bug 1455406: Convert test_same_site_cookies_webextension to an xpcshell test. r=mixedpuppy
    MozReview-Commit-ID: BFAmZUn349n
    
    --HG--
    rename : toolkit/components/extensions/test/mochitest/test_same_site_cookies_webextension.html => toolkit/components/extensions/test/xpcshell/test_ext_same_site_cookies.js
    extra : rebase_source : 1b7bfbdedf7e023ebbd66568f39ff4c27b031640
    extra : amend_source : 9e56278784b6ff7b17daaeaf20a74fdc9d1ac346

--- toolkit/components/extensions/test/xpcshell/test_ext_same_site_cookies.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_same_site_cookies.js
@@ -1,17 +1,44 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>Bug 1454914: Exempt web-extensions from same-site cookie policy</title>
-  <script type="text/javascript" src="/tests/SimpleTest/SimpleTest.js"></script>
-  <script type="text/javascript" src="/tests/SimpleTest/AddTask.js"></script>
-  <script type="text/javascript" src="/tests/SimpleTest/ExtensionTestUtils.js"></script>
-  <script type="text/javascript" src="head.js"></script>
-  <link rel="stylesheet" type="text/css" href="/tests/SimpleTest/test.css"/>
-</head>
-<body>
-
-<script type="text/javascript">
 "use strict";
+
+const server = createHttpServer({hosts: ["example.com"]});
+
+const WIN = `<html><body>dummy page setting a same-site cookie</body></html>`;
+
+// Small red image.
+const IMG_BYTES = atob(
+  "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12" +
+  "P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==");
+
+server.registerPathHandler("/same_site_cookies", (request, response) => {
+  // avoid confusing cache behaviors
+  response.setHeader("Cache-Control", "no-cache", false);
+
+  if (request.queryString === "loadWin") {
+    response.write(WIN);
+    return;
+  }
+
+  // using startsWith and discard the math random
+  if (request.queryString.startsWith("loadImage")) {
+    response.setHeader("Set-Cookie", "myKey=mySameSiteExtensionCookie; samesite=strict", true);
+    response.setHeader("Content-Type", "image/png");
+    response.write(IMG_BYTES);
+    return;
+  }
+
+  if (request.queryString === "loadXHR") {
+    let cookie = "noCookie";
+    if (request.hasHeader("Cookie")) {
+      cookie = request.getHeader("Cookie");
+    }
+    response.setHeader("Content-Type", "text/plain");
+    response.write(cookie);
+    return;
+  }
+
+  // We should never get here, but just in case return something unexpected.
+  response.write("D'oh");
+});
 
 /* Description of the test:
  * (1) We load an image from mochi.test which sets a same site cookie
@@ -27,7 +54,7 @@ add_task(async function test_webRequest_same_site_cookie_access() {
       ],
       content_scripts: [{
         matches: ["http://example.com/*"],
-        run_at: "document_start",
+        run_at: "document_end",
         js: ["content_script.js"],
       }],
     },
@@ -37,7 +64,7 @@ add_task(async function test_webRequest_same_site_cookie_access() {
         if (msg === "verify-same-site-cookie-moz-extension") {
           let xhr = new XMLHttpRequest();
           try {
-            xhr.open("GET", "http://example.com/tests/toolkit/components/extensions/test/mochitest/file_same_site_cookies_webextension.sjs?loadXHR", true);
+            xhr.open("GET", "http://example.com/same_site_cookies?loadXHR", true);
             xhr.onload = function() {
               browser.test.assertEq("myKey=mySameSiteExtensionCookie", xhr.responseText,
                                     "cookie should be accessible from moz-extension context");
@@ -60,7 +87,7 @@ add_task(async function test_webRequest_same_site_cookie_access() {
         let myImage = document.createElement("img");
         // Set the src via wrappedJSObject so the load is triggered with the
         // content page's principal rather than ours.
-        myImage.wrappedJSObject.setAttribute("src", "http://example.com/tests/toolkit/components/extensions/test/mochitest/file_same_site_cookies_webextension.sjs?loadImage" + Math.random());
+        myImage.wrappedJSObject.setAttribute("src", "http://example.com/same_site_cookies?loadImage" + Math.random());
         myImage.onload = function() {
           browser.test.log("image onload");
           browser.test.sendMessage("image-loaded-and-same-site-cookie-set");
@@ -74,15 +101,14 @@ add_task(async function test_webRequest_same_site_cookie_access() {
   });
 
   await extension.startup();
-  let win = window.open("http://example.com/tests/toolkit/components/extensions/test/mochitest/file_same_site_cookies_webextension.sjs?loadWin");
+
+  let contentPage = await ExtensionTestUtils.loadContentPage("http://example.com/same_site_cookies?loadWin");
+
   await extension.awaitMessage("image-loaded-and-same-site-cookie-set");
+
   extension.sendMessage("verify-same-site-cookie-moz-extension");
   await extension.awaitMessage("same-site-cookie-test-done");
-  win.close();
+
+  await contentPage.close();
   await extension.unload();
 });
-
-</script>
-
-</body>
-</html>
