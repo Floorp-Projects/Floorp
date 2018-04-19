78714f6d8c2b2cefc553afd15e3ff86ded8a202f
Cameron McCormack <cam@mcc.id.au>
2018-04-19 17:21:00 +0300

    Bug 1133172 - Remove SVGSVGElement.{pixel,screenPixel}UnitToMillimeter{X,Y}. r=longonsr,bz
    MozReview-Commit-ID: KKy0J8oQYJj

--- dom/svg/SVGSVGElement.h
+++ dom/svg/SVGSVGElement.h
@@ -115,10 +115,6 @@ public:
   already_AddRefed<SVGAnimatedLength> Y();
   already_AddRefed<SVGAnimatedLength> Width();
   already_AddRefed<SVGAnimatedLength> Height();
-  float PixelUnitToMillimeterX();
-  float PixelUnitToMillimeterY();
-  float ScreenPixelToMillimeterX();
-  float ScreenPixelToMillimeterY();
   bool UseCurrentView();
   float CurrentScale();
   void SetCurrentScale(float aCurrentScale);
