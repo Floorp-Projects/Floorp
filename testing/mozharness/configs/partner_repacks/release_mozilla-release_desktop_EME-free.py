392d9c150331c299a64b6d7892832fc85ec4772f
Nick Thomas <nthomas@mozilla.com>
2018-04-19 10:01:17 -0600

    Bug 1440546: [partner-repack] Mozharness support for new partner repack script; r=Callek, a=release
    Differential Revision: https://phabricator.services.mozilla.com/D973

--- testing/mozharness/configs/partner_repacks/release_mozilla-release_desktop_EME-free.py
+++ testing/mozharness/configs/partner_repacks/release_mozilla-release_desktop_EME-free.py
@@ -1,7 +1,7 @@
 config = {
     "appName": "Firefox",
     "log_name": "partner_repack",
-    "repack_manifests_url": "git@github.com:mozilla-partners/repack-manifests.git",
+    "repack_manifests_url": "git@github.com:mozilla-partners/mozilla-EME-free-manifest.git",
     "repo_file": "https://raw.githubusercontent.com/mozilla/git-repo/master/repo",
     "secret_files": [
         {
--- testing/mozharness/configs/partner_repacks/staging_release_mozilla-release_desktop.py
+++ testing/mozharness/configs/partner_repacks/staging_release_mozilla-release_desktop.py
@@ -3,5 +3,13 @@ config = {
     "log_name": "partner_repack",
     "repack_manifests_url": "git@github.com:mozilla-partners/repack-manifests.git",
     "repo_file": "https://raw.githubusercontent.com/mozilla/git-repo/master/repo",
-    "repo_url": "git@github.com:mozilla/git-repo.git",
+    "secret_files": [
+        {
+            "filename": "/builds/partner-github-ssh",
+            "secret_name": "project/releng/gecko/build/level-%(scm-level)s/partner-github-ssh",
+            "min_scm_level": 3,
+            "mode": 0o600,
+        },
+    ],
+    "ssh_key": "/builds/partner-github-ssh",
 }
