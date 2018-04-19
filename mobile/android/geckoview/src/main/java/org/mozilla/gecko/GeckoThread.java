b409eb3af638cb5dc3312fd49a409b843ecc9666
James Willcox <snorp@snorp.net>
2018-04-19 15:13:56 -0500

    Bug 1433968 - Add crash reporting control to GeckoRuntimeSettings r=esawin,jchen
    MozReview-Commit-ID: TQ7hvekIVJ

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoThread.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoThread.java
@@ -28,7 +28,6 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -126,8 +125,10 @@ public class GeckoThread extends Thread {
     private static int uiThreadId;
 
     // Main process parameters
-    public static final int FLAG_DEBUGGING = 1; // Debugging mode.
-    public static final int FLAG_PRELOAD_CHILD = 2; // Preload child during main thread start.
+    public static final int FLAG_DEBUGGING = 1 << 0; // Debugging mode.
+    public static final int FLAG_PRELOAD_CHILD = 1 << 1; // Preload child during main thread start.
+    public static final int FLAG_ENABLE_NATIVE_CRASHREPORTER = 1 << 2; // Enable native crash reporting
+    public static final int FLAG_ENABLE_JAVA_CRASHREPORTER = 1 << 3; // Enable java crash reporting
 
     private static final String EXTRA_ARGS = "args";
     private static final String EXTRA_PREFS_FD = "prefsFd";
@@ -183,11 +184,14 @@ public class GeckoThread extends Thread {
                              /* fd */ -1, /* fd */ -1, /* fd */ -1);
     }
 
-    public static boolean initChildProcess(final String[] args, final Bundle extras,
-                                           final int prefsFd, final int ipcFd,
+    public static boolean initChildProcess(final String[] args,
+                                           final Bundle extras,
+                                           final int flags,
+                                           final int prefsFd,
+                                           final int ipcFd,
                                            final int crashFd,
                                            final int crashAnnotationFd) {
-        return INSTANCE.init(/* profile */ null, args, extras, /* flags */ 0,
+        return INSTANCE.init(/* profile */ null, args, extras, flags,
                              prefsFd, ipcFd, crashFd, crashAnnotationFd);
     }
 
@@ -379,7 +383,17 @@ public class GeckoThread extends Thread {
             if (!INSTANCE.mInitialized) {
                 return null;
             }
-            return INSTANCE.mExtras;
+            return new Bundle(INSTANCE.mExtras);
+        }
+    }
+
+    public static int getActiveFlags() {
+        synchronized (INSTANCE) {
+            if (!INSTANCE.mInitialized) {
+                return 0;
+            }
+
+            return INSTANCE.mFlags;
         }
     }
 
@@ -464,7 +478,20 @@ public class GeckoThread extends Thread {
             Log.i(LOGTAG, "RunGecko - args = " + TextUtils.join(" ", args));
         }
 
-        final ArrayList<String> env = getEnvFromExtras(mExtras);
+        final List<String> env = getEnvFromExtras(mExtras);
+
+        // In Gecko, the native crash reporter is enabled by default in opt builds, and
+        // disabled by default in debug builds.
+        if ((mFlags & FLAG_ENABLE_NATIVE_CRASHREPORTER) == 0 && !BuildConfig.DEBUG_BUILD) {
+            env.add(0, "MOZ_CRASHREPORTER_DISABLE=1");
+        } else if ((mFlags & FLAG_ENABLE_NATIVE_CRASHREPORTER) != 0 && BuildConfig.DEBUG_BUILD) {
+            env.add(0, "MOZ_CRASHREPORTER=1");
+        }
+
+        if ((mFlags & FLAG_ENABLE_JAVA_CRASHREPORTER) != 0) {
+            GeckoAppShell.ensureCrashHandling();
+        }
+
         GeckoLoader.setupGeckoEnvironment(context, context.getFilesDir().getPath(), env);
 
         // And go.
