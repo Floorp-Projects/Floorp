4f534c06b9d99c03528fe7e84090ff418770c479
Johann Hofmann <jhofmann@mozilla.com>
2018-04-19 17:22:11 +0200

    Bug 1451307 - Part 2 - Move trackingUI tests out of b/b/c/test/general into their own subdirectory. r=Gijs
    MozReview-Commit-ID: 5e2Hwt9jcX5

--- browser/base/content/test/trackingUI/browser_trackingUI_2.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_2.js
@@ -4,14 +4,8 @@
  * See also Bugs 1175327, 1043801, 1178985.
  */
 
-const PREF = "privacy.trackingprotection.enabled";
-const PB_PREF = "privacy.trackingprotection.pbmode.enabled";
-const BENIGN_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/benignPage.html";
-const TRACKING_PAGE = "http://tracking.example.org/browser/browser/base/content/test/general/trackingPage.html";
 var TrackingProtection = null;
 var tabbrowser = null;
-
-var {UrlClassifierTestUtils} = ChromeUtils.import("resource://testing-common/UrlClassifierTestUtils.jsm", {});
 
 registerCleanupFunction(function() {
   TrackingProtection = tabbrowser = null;
