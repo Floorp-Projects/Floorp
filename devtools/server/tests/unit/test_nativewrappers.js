ca1e80f478efd2f5a0e59599e285e64e61fe5fb4
Boris Zbarsky <bzbarsky@mit.edu>
2018-04-20 23:04:40 -0400

    Bug 1453869 part 9.  Stop creating DOMParser by contract in devtools.  r=tromey
    There are some extra hoops here because devtools has a lint to prevent Cu.importGlobalProperties, which is the normal way one would import a WebIDL constructor.
    
    MozReview-Commit-ID: 2mdNI6N1z5B

--- devtools/server/tests/unit/test_nativewrappers.js
+++ devtools/server/tests/unit/test_nativewrappers.js
@@ -20,12 +20,14 @@ function run_test() {
 
   let g2 = testGlobal("test2");
   g2.g = g;
-  g2.eval("(" + function createBadEvent() {
-    let parser = Cc["@mozilla.org/xmlextras/domparser;1"]
-        .createInstance(Ci.nsIDOMParser);
+  // Not using the "stringify a function" trick because that runs afoul of the
+  // Cu.importGlobalProperties lint and we don't need it here anyway.
+  g2.eval(`(function createBadEvent() {
+    Cu.importGlobalProperties(["DOMParser"]);
+    let parser = new DOMParser();
     let doc = parser.parseFromString("<foo></foo>", "text/xml");
     g.stopMe(doc.createEvent("MouseEvent"));
-  } + ")()");
+  } )()`);
 
   dbg.enabled = false;
 }
