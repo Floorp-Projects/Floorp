1c3691b3db3a4c82cef4e517fb57306c19de646b
Boris Zbarsky <bzbarsky@mit.edu>
2018-04-20 23:01:24 -0400

    Bug 1453869 part 2.  Get rid of the unused nsIDOMParser::Init method.  r=mrbkap
    MozReview-Commit-ID: B3HVscqYE6G

--- dom/base/DOMParser.h
+++ dom/base/DOMParser.h
@@ -85,6 +85,31 @@ private:
     MOZ_ASSERT(aOwner);
   }
 
+  /**
+   * Initialize the principal and document and base URIs that the parser should
+   * use for documents it creates.  If this is not called, then a null
+   * principal and its URI will be used.  When creating a DOMParser via the JS
+   * constructor, this will be called automatically.  This method may only be
+   * called once.  If this method fails, all following parse attempts will
+   * fail.
+   *
+   * @param principal The principal to use for documents we create.
+   *                  If this is null, a codebase principal will be created
+   *                  based on documentURI; in that case the documentURI must
+   *                  be non-null.
+   * @param documentURI The documentURI to use for the documents we create.
+   *                    If null, the principal's URI will be used;
+   *                    in that case, the principal must be non-null and its
+   *                    URI must be non-null.
+   * @param baseURI The baseURI to use for the documents we create.
+   *                If null, the documentURI will be used.
+   * @param scriptObject The object from which the context for event handling
+   *                     can be got.
+   */
+  nsresult Init(nsIPrincipal* aPrincipal, nsIURI* aDocumentURI,
+                nsIURI* aBaseURI, nsIGlobalObject* aSriptObjet);
+
+
   nsresult InitInternal(nsISupports* aOwner, nsIPrincipal* prin,
                         nsIURI* documentURI, nsIURI* baseURI);
 
