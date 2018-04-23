496416e09510e84b8315f71483664c0a106e1bfb
Andreea Pavel <apavel@mozilla.com>
2018-04-24 02:13:38 +0300

    Backed out changeset e2573db9d14e (bug 1403965) for failing browser chrome at browser/components/extensions/test/browser/test-oop-extensions/browser_ext_sidebarAction_runtime.js on a CLOSED TREE

--- browser/base/content/webext-panels.js
+++ browser/base/content/webext-panels.js
@@ -93,13 +93,6 @@ var gBrowser = {
   },
 };
 
-function updatePosition() {
-  let browser = document.getElementById("webext-panels-browser");
-  if (browser && browser.isRemoteBrowser) {
-    browser.frameLoader.requestUpdatePosition();
-  }
-}
-
 function loadPanel(extensionId, extensionUrl, browserStyle) {
   let browserEl = document.getElementById("webext-panels-browser");
   if (browserEl) {
