f92e0266fe662dd4776dd7cb50f585daca646c08
Dustin J. Mitchell <dustin@mozilla.com>
2018-04-23 21:20:00 +0000

    Bug 1415868 - Remove support for register_action_task; r=jonasfj,tomprince
    In a post-actions-as-hooks world, users will not have scopes to create tasks,
    so this mode of action definition will not be possible. This is not currently
    used from Treeherder (it links to
    https://tools.taskcluster.net/tasks/<taskid>/interactive instead)
    
    This drops support for the JSON-e-only interactive action; that action is not
    currently used from treeherder, so that should have no impact for users.
    
    MozReview-Commit-ID: 9i3POpjahAc

--- taskcluster/taskgraph/actions/registry.py
+++ taskcluster/taskgraph/actions/registry.py
@@ -36,71 +36,6 @@ def is_json(data):
     return True
 
 
-def register_task_action(name, title, description, order, context, schema=None):
-    """
-    Register an action task that can be triggered from supporting
-    user interfaces, such as Treeherder.
-
-    Most actions will create intermediate action tasks that call back into
-    in-tree python code. To write such an action please use
-    :func:`register_callback_action`.
-
-    This function is to be used a decorator for a function that returns a task
-    template, see :doc:`specification <action-spec>` for details on the
-    templating features. The decorated function will be given decision task
-    parameters, which can be embedded in the task template that is returned.
-
-    Parameters
-    ----------
-    name : str
-        An identifier for this action, used by UIs to find the action.
-    title : str
-        A human readable title for the action to be used as label on a button
-        or text on a link for triggering the action.
-    description : str
-        A human readable description of the action in **markdown**.
-        This will be display as tooltip and in dialog window when the action
-        is triggered. This is a good place to describe how to use the action.
-    order : int
-        Order of the action in menus, this is relative to the ``order`` of
-        other actions declared.
-    context : list of dict
-        List of tag-sets specifying which tasks the action is can take as input.
-        If no tag-sets is specified as input the action is related to the
-        entire task-group, and won't be triggered with a given task.
-
-        Otherwise, if ``context = [{'k': 'b', 'p': 'l'}, {'k': 't'}]`` will only
-        be displayed in the context menu for tasks that has
-        ``task.tags.k == 'b' && task.tags.p = 'l'`` or ``task.tags.k = 't'``.
-        Esentially, this allows filtering on ``task.tags``.
-    schema : dict
-        JSON schema specifying input accepted by the action.
-        This is optional and can be left ``null`` if no input is taken.
-
-    Returns
-    -------
-    function
-        To be used as decorator for the function that builds the task template.
-        The decorated function will be given decision parameters and may return
-        ``None`` instead of a task template, if the action is disabled.
-    """
-    assert isinstance(name, basestring), 'name must be a string'
-    assert isinstance(title, basestring), 'title must be a string'
-    assert isinstance(description, basestring), 'description must be a string'
-    assert isinstance(order, int), 'order must be an integer'
-    assert callable(schema) or is_json(schema), 'schema must be a JSON compatible  object'
-    mem = {"registered": False}  # workaround nonlocal missing in 2.x
-
-    def register_task_template_builder(task_template_builder):
-        assert not mem['registered'], 'register_task_action must be used as decorator'
-        actions.append(Action(
-            name.strip(), title.strip(), description.strip(), order, context,
-            schema, task_template_builder,
-        ))
-        mem['registered'] = True
-    return register_task_template_builder
-
-
 def register_callback_action(name, title, symbol, description, order=10000,
                              context=[], available=lambda parameters: True, schema=None):
     """
@@ -164,16 +99,21 @@ def register_callback_action(name, title, symbol, description, order=10000,
     mem = {"registered": False}  # workaround nonlocal missing in 2.x
 
     def register_callback(cb):
+        assert isinstance(name, basestring), 'name must be a string'
+        assert isinstance(title, basestring), 'title must be a string'
+        assert isinstance(description, basestring), 'description must be a string'
+        assert isinstance(order, int), 'order must be an integer'
+        assert callable(schema) or is_json(schema), 'schema must be a JSON compatible object'
         assert isinstance(cb, FunctionType), 'callback must be a function'
-        assert isinstance(symbol, basestring), 'symbol must be a string'
         # Allow for json-e > 25 chars in the symbol.
         if '$' not in symbol:
             assert 1 <= len(symbol) <= 25, 'symbol must be between 1 and 25 characters'
+        assert isinstance(symbol, basestring), 'symbol must be a string'
+
         assert not mem['registered'], 'register_callback_action must be used as decorator'
         assert cb.__name__ not in callbacks, 'callback name {} is not unique'.format(cb.__name__)
 
-        @register_task_action(name, title, description, order, context, schema)
-        def build_callback_action_task(parameters, graph_config):
+        def task_template_builder(parameters, graph_config):
             if not available(parameters):
                 return None
 
@@ -221,6 +161,10 @@ def register_callback_action(name, title, symbol, description, order=10000,
                     },
                     'in': taskcluster_yml['tasks'][0]
                 }
+
+        actions.append(Action(
+            name.strip(), title.strip(), description.strip(), order, context,
+            schema, task_template_builder))
 
         mem['registered'] = True
         callbacks[cb.__name__] = cb
@@ -298,10 +242,6 @@ def _load(graph_config):
     for f in os.listdir(actions_dir):
         if f.endswith('.py') and f not in ('__init__.py', 'registry.py', 'util.py'):
             __import__('taskgraph.actions.' + f[:-3])
-        if f.endswith('.yml'):
-            with open(os.path.join(actions_dir, f), 'r') as d:
-                frontmatter, template = yaml.safe_load_all(d)
-                register_task_action(**frontmatter)(lambda _p, _g: template)
     return callbacks, actions
 
 
