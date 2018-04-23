5085f05d3e0a77502f7c2d56e41d5ea9306593e2
Jim Chen <nchen@mozilla.com>
2018-04-23 12:07:34 -0400

    Bug 1454441 - 3. Move remote debugging setting to runtime; r=esawin,snorp
    Move the remote debugging setting to GeckoRuntimeSettings and use it in
    geckoview_example.
    
    MozReview-Commit-ID: G2IINILQAOm

--- mobile/android/base/java/org/mozilla/gecko/webapps/WebAppActivity.java
+++ mobile/android/base/java/org/mozilla/gecko/webapps/WebAppActivity.java
@@ -93,10 +93,6 @@ public class WebAppActivity extends AppCompatActivity
 
         final GeckoSessionSettings settings = new GeckoSessionSettings();
         settings.setBoolean(GeckoSessionSettings.USE_MULTIPROCESS, false);
-        settings.setBoolean(
-            GeckoSessionSettings.USE_REMOTE_DEBUGGER,
-            GeckoSharedPrefs.forApp(this).getBoolean(
-                GeckoPreferences.PREFS_DEVTOOLS_REMOTE_USB_ENABLED, false));
         mGeckoSession = new GeckoSession(settings);
         mGeckoView.setSession(mGeckoSession, GeckoRuntime.getDefault(this));
 
