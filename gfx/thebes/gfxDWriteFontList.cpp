6fe5024795e0a7dc0487518271e08dd10283af38
Jonathan Kew <jkew@mozilla.com>
2018-04-23 18:42:49 +0100

    Bug 1436048 followup, fix for Windows build bustage. r=emilio(irc) on a CLOSED TREE

--- gfx/thebes/gfxDWriteFontList.cpp
+++ gfx/thebes/gfxDWriteFontList.cpp
@@ -408,7 +408,7 @@ gfxDWriteFontEntry::CopyFontTable(uint32_t aTableTag,
     // potential cmap discrepancies, see bug 629386.
     // Ditto for Hebrew, bug 837498.
     if (mFont && pFontList->UseGDIFontTableAccess() &&
-        !(mStyle && UsingArabicOrHebrewScriptSystemLocale()) &&
+        !(!IsUpright() && UsingArabicOrHebrewScriptSystemLocale()) &&
         !mFont->IsSymbolFont())
     {
         LOGFONTW logfont = { 0 };
