a1d88e5fb9bd3477e5c2d4a585106d1f939637d6
Paul Adenot <paul@paul.cx>
2018-04-23 11:55:19 +0200

    Bug 1456115 - Make last_sample_rate_ in `neteq_impl.cc` atomic. r=dminor
    This allows removing locking, and allows other threads to progress without
    taking the lock, hence lowering the probability that the lock will be taken for
    a long time when we need to pull NeqEQ.
    
    MozReview-Commit-ID: HMO67A0hher
    
    --HG--
    extra : rebase_source : aa5c77895eb57d60b61d9a8a5822e53593348830

--- media/webrtc/trunk/webrtc/modules/audio_coding/neteq/neteq_impl.cc
+++ media/webrtc/trunk/webrtc/modules/audio_coding/neteq/neteq_impl.cc
@@ -461,7 +461,6 @@ rtc::Optional<uint32_t> NetEqImpl::GetPlayoutTimestamp() const {
 }
 
 int NetEqImpl::last_output_sample_rate_hz() const {
-  rtc::CritScope lock(&crit_sect_);
   return last_output_sample_rate_hz_;
 }
 
