b6dcf8af72c423066689f88e55662ba946f9743a
James Teh <jteh@mozilla.com>
2018-04-23 10:42:06 +1000

    Bug 1454866 part 2: Make the buttons in the Site Security panel keyboard navigable. r=johannh
    Previously, the only button that could be accessed with the keyboard was the Back button.
    
    MozReview-Commit-ID: 2khSExFNkVP

--- browser/base/content/test/siteIdentity/browser_identityPopup_focus.js
+++ browser/base/content/test/siteIdentity/browser_identityPopup_focus.js
@@ -25,3 +25,40 @@ add_task(async function testIdentityPopupFocusKeyboard() {
   });
 });
 
+// Access the Site Security panel, then move focus with the tab key.
+// Tabbing should be able to reach the More Information button.
+add_task(async function testSiteSecurityTabOrder() {
+  await SpecialPowers.pushPrefEnv({"set": [["accessibility.tabfocus", 7]]});
+  await BrowserTestUtils.withNewTab("https://example.com", async function() {
+    // 1. Access the identity popup.
+    let focused = BrowserTestUtils.waitForEvent(gIdentityHandler._identityBox, "focus");
+    gIdentityHandler._identityBox.focus();
+    await focused;
+    let shown = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "popupshown");
+    EventUtils.sendString(" ");
+    await shown;
+    is(Services.focus.focusedElement, document.getElementById("identity-popup-security-expander"));
+
+    // 2. Access the Site Security section.
+    let securityView = document.getElementById("identity-popup-securityView");
+    shown = BrowserTestUtils.waitForEvent(securityView, "ViewShown");
+    EventUtils.sendString(" ");
+    await shown;
+
+    // 3. First press of tab should focus the Back button.
+    let backButton = gIdentityHandler._identityPopup.querySelector(".subviewbutton-back");
+    // Wait for focus to move somewhere. We use focusin because focus doesn't bubble.
+    focused = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "focusin");
+    EventUtils.sendKey("tab");
+    await focused;
+    is(Services.focus.focusedElement, backButton);
+
+    // 4. Second press of tab should move to the More Information button.
+    let moreInfoButton = document.getElementById("identity-popup-more-info");
+    focused = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "focusin");
+    EventUtils.sendKey("tab");
+    await focused;
+    isnot(Services.focus.focusedElement, backButton);
+    is(Services.focus.focusedElement, moreInfoButton);
+  });
+});
