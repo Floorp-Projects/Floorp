e0deb511eb6f4ad8e9fdacdb68ad0ed8cabce0b2
Andrew Osmond <aosmond@mozilla.com>
2018-04-23 07:57:13 -0400

    Bug 1453801 - Part 1. Ensure WebRenderUserData objects are freed after a tab switch. r=sotaro

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -1070,10 +1070,7 @@ void
 WebRenderCommandBuilder::Destroy()
 {
   mLastCanvasDatas.Clear();
-  RemoveUnusedAndResetWebRenderUserData();
-  // UserDatas should only be in the used state during a call to WebRenderCommandBuilder::BuildWebRenderCommands
-  // The should always be false upon return from BuildWebRenderCommands().
-  MOZ_RELEASE_ASSERT(mWebRenderUserDatas.Count() == 0);
+  ClearCachedResources();
 }
 
 void
@@ -1830,10 +1827,10 @@ WebRenderCommandBuilder::RemoveUnusedAndResetWebRenderUserData()
 void
 WebRenderCommandBuilder::ClearCachedResources()
 {
-  for (auto iter = mWebRenderUserDatas.Iter(); !iter.Done(); iter.Next()) {
-    WebRenderUserData* data = iter.Get()->GetKey();
-    data->ClearCachedResources();
-  }
+  RemoveUnusedAndResetWebRenderUserData();
+  // UserDatas should only be in the used state during a call to WebRenderCommandBuilder::BuildWebRenderCommands
+  // The should always be false upon return from BuildWebRenderCommands().
+  MOZ_RELEASE_ASSERT(mWebRenderUserDatas.Count() == 0);
 }
 
 
