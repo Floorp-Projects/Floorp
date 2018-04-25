ce747b87c6527c9c28498da591d5fea39a91533b
Miko Mynttinen <mikokm@gmail.com>
2018-04-25 22:54:18 +0200

    Bug 1455944 - Do not paint nsDisplayOpacity children when opacity push/pop markers have empty paint rect r=mattwoodrow
    MozReview-Commit-ID: 5BgHOFjW34H
    
    --HG--
    extra : rebase_source : 0fc76b610ee3149c525e342988fac5b5028feeeb

--- layout/painting/FrameLayerBuilder.cpp
+++ layout/painting/FrameLayerBuilder.cpp
@@ -6283,6 +6283,21 @@ struct ClipTracker {
   gfxContext* mContext;
 };
 
+static void
+UpdateOpacityNesting(int& aOpacityNesting, DisplayItemEntryType aType)
+{
+  if (aType == DisplayItemEntryType::PUSH_OPACITY ||
+      aType == DisplayItemEntryType::PUSH_OPACITY_WITH_BG) {
+    aOpacityNesting++;
+  }
+
+  if (aType == DisplayItemEntryType::POP_OPACITY) {
+    aOpacityNesting--;
+  }
+
+  MOZ_ASSERT(aOpacityNesting >= 0);
+}
+
 void
 FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
                               const nsIntRect& aRect,
@@ -6302,7 +6317,13 @@ FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
 
   DisplayItemClip currentClip, tmpClip;
 
+  // Tracks opacity nesting level for item level clipping.
   int opacityNesting = 0;
+
+  // Tracks opacity nesting level for skipping items between opacity markers,
+  // when opacity has empty visible rect set.
+  int emptyOpacityNesting = 0;
+
   ClipTracker clipTracker(aContext);
 
   for (uint32_t i = 0; i < aItems.Length(); ++i) {
@@ -6315,9 +6336,14 @@ FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
     }
 
     const nsRect& visibleRect = item->GetVisibleRect();
+    const nsRect paintRect = visibleRect.Intersect(boundRect);
 
-    nsRect paintRect = visibleRect.Intersect(boundRect);
-    if (paintRect.IsEmpty()) {
+    if (paintRect.IsEmpty() || emptyOpacityNesting > 0) {
+      // In order for this branch to be hit, either this item has an empty paint
+      // rect and nothing would be drawn, or a PUSH_OPACITY marker before this
+      // item had an empty paint rect. In the latter case, the items are skipped
+      // until POP_OPACITY markers bring |emptyOpacityNesting| back to 0.
+      UpdateOpacityNesting(emptyOpacityNesting, cdi.mType);
       continue;
     }
 
@@ -6335,7 +6361,6 @@ FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
         cdi.mType == DisplayItemEntryType::PUSH_OPACITY_WITH_BG) {
       clipTracker.PopClipIfNeeded(opacityNesting);
       PushOpacity(aContext, paintRect, cdi, appUnitsPerDevPixel);
-      opacityNesting++;
     }
 
     if (cdi.mType == DisplayItemEntryType::POP_OPACITY) {
@@ -6345,10 +6370,10 @@ FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
       clipTracker.PopClipIfNeeded(opacityNesting);
       aContext->PopGroupAndBlend();
       aContext->Restore();
-      opacityNesting--;
     }
 
     if (cdi.mType != DisplayItemEntryType::ITEM) {
+      UpdateOpacityNesting(opacityNesting, cdi.mType);
       continue;
     }
 
@@ -6396,6 +6421,7 @@ FrameLayerBuilder::PaintItems(nsTArray<AssignedDisplayItem>& aItems,
 
   clipTracker.PopClipIfNeeded(opacityNesting);
   MOZ_ASSERT(opacityNesting == 0);
+  MOZ_ASSERT(emptyOpacityNesting == 0);
 }
 
 /**
