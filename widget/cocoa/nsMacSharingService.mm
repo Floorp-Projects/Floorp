b4ccd4de211b5031e310127cde34b9fca2c67d6c
Dale Harvey <dale@arandomurl.com>
2018-04-25 08:23:32 +0100

    Bug 1455310 - Add title data and activity to sharing. r=mstange
    MozReview-Commit-ID: IL4sqvRwSwu
    
    --HG--
    extra : rebase_source : 7179fc3e2ef112574b02b03a56f150d0e3617d4e

--- widget/cocoa/nsMacSharingService.mm
+++ widget/cocoa/nsMacSharingService.mm
@@ -9,12 +9,45 @@
 #include "nsCocoaUtils.h"
 #include "mozilla/MacStringHelpers.h"
 
+NS_IMPL_ISUPPORTS(nsMacSharingService, nsIMacSharingService)
+
 // List of sharingProviders that we do not want to expose to
 // the user, because they are duplicates or do not work correctly
 // within the context
-NSArray *filteredProviderTitles = @[@"Add to Reading List", @"Mail"];
+NSArray *filteredProviderNames = @[
+  @"com.apple.share.System.add-to-safari-reading-list",
+  @"com.apple.share.Mail.compose"
+];
 
-NS_IMPL_ISUPPORTS(nsMacSharingService, nsIMacSharingService)
+NSString* const remindersServiceName =
+  @"com.apple.reminders.RemindersShareExtension";
+
+// Reminders use activities to share data
+NSUserActivity *shareActivity;
+
+// Expose the id so we can pass reference through to JS and back
+@interface NSSharingService (ExposeName)
+- (id)name;
+@end
+
+// Clean up the activity once the share is complete
+@interface SharingServiceDelegate : NSObject <NSSharingServiceDelegate>
+@end
+
+@implementation SharingServiceDelegate
+
+- (void)sharingService:(NSSharingService *)sharingService
+        didShareItems:(NSArray *)items {
+  [shareActivity invalidate];
+}
+
+- (void)sharingService:(NSSharingService*)service
+        didFailToShareItems:(NSArray*)items error:(NSError*)error {
+  [shareActivity invalidate];
+}
+@end
+
+SharingServiceDelegate* shareDelegate = [[SharingServiceDelegate alloc] init];
 
 static NSString*
 NSImageToBase64(const NSImage* aImage)
@@ -41,30 +74,31 @@ SetStrAttribute(JSContext* aCx,
 }
 
 nsresult
-nsMacSharingService::GetSharingProviders(const nsAString& aUrlToShare,
+nsMacSharingService::GetSharingProviders(const nsAString& aPageUrl,
                                          JSContext* aCx,
                                          JS::MutableHandleValue aResult)
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
   JS::Rooted<JSObject*> array(aCx, JS_NewArrayObject(aCx, 0));
-  NSURL* url = [NSURL URLWithString:nsCocoaUtils::ToNSString(aUrlToShare)];
+  NSURL* url = [NSURL URLWithString:nsCocoaUtils::ToNSString(aPageUrl)];
 
   NSArray* sharingService = [NSSharingService
                              sharingServicesForItems:[NSArray arrayWithObject:url]];
   int32_t serviceCount = 0;
 
   for (NSSharingService *currentService in sharingService) {
-    if (![filteredProviderTitles containsObject:currentService.title]) {
-      JS::Rooted<JSObject*> obj(aCx, JS_NewPlainObject(aCx));
-
-      SetStrAttribute(aCx, obj, "title", currentService.title);
-      SetStrAttribute(aCx, obj, "menuItemTitle", currentService.menuItemTitle);
-      SetStrAttribute(aCx, obj, "image", NSImageToBase64(currentService.image));
-
-      JS::Rooted<JS::Value> element(aCx, JS::ObjectValue(*obj));
-      JS_SetElement(aCx, array, serviceCount++, element);
+    if ([filteredProviderNames containsObject:[currentService name]]) {
+      continue;
     }
+    JS::Rooted<JSObject*> obj(aCx, JS_NewPlainObject(aCx));
+
+    SetStrAttribute(aCx, obj, "name", [currentService name]);
+    SetStrAttribute(aCx, obj, "menuItemTitle", currentService.menuItemTitle);
+    SetStrAttribute(aCx, obj, "image", NSImageToBase64(currentService.image));
+
+    JS::Rooted<JS::Value> element(aCx, JS::ObjectValue(*obj));
+    JS_SetElement(aCx, array, serviceCount++, element);
   }
 
   aResult.setObject(*array);
@@ -74,23 +108,37 @@ nsMacSharingService::GetSharingProviders(const nsAString& aUrlToShare,
 }
 
 NS_IMETHODIMP
-nsMacSharingService::ShareUrl(const nsAString& aShareTitle,
-                              const nsAString& aUrlToShare)
+nsMacSharingService::ShareUrl(const nsAString& aServiceName,
+                              const nsAString& aPageUrl,
+                              const nsAString& aPageTitle)
 {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
 
-  NSString* titleString = nsCocoaUtils::ToNSString(aShareTitle);
-  NSURL* url = [NSURL URLWithString:nsCocoaUtils::ToNSString(aUrlToShare)];
+  NSString* serviceName = nsCocoaUtils::ToNSString(aServiceName);
+  NSURL* pageUrl = [NSURL URLWithString:nsCocoaUtils::ToNSString(aPageUrl)];
+  NSString* pageTitle = nsCocoaUtils::ToNSString(aPageTitle);
+  NSSharingService* service = [NSSharingService
+                               sharingServiceNamed:serviceName];
 
-  NSArray* sharingService = [NSSharingService
-                             sharingServicesForItems:[NSArray arrayWithObject:url]];
-
-  for (NSSharingService *currentService in sharingService) {
-    if ([currentService.title isEqualToString:titleString]) {
-      [currentService performWithItems:@[url]];
-      break;
+  // Reminders fetch its data from an activity, not the share data
+  if ([[service name] isEqual:remindersServiceName]) {
+    shareActivity = [[NSUserActivity alloc]
+                     initWithActivityType:NSUserActivityTypeBrowsingWeb];
+    if ([pageUrl.scheme hasPrefix:@"http"]) {
+      [shareActivity setWebpageURL:pageUrl];
     }
+    [shareActivity setTitle:pageTitle];
+    [shareActivity becomeCurrent];
   }
+
+  // Twitter likes the the title as an additional share item
+  NSArray* toShare = [[service name] isEqual:NSSharingServiceNamePostOnTwitter]
+    ? @[pageUrl, pageTitle]
+    : @[pageUrl];
+
+  [service setDelegate:shareDelegate];
+  [service setSubject:pageTitle];
+  [service performWithItems:toShare];
 
   return NS_OK;
 
