5e3f95c227338f43617b28ab0b1082f6adc3286f
Jan Odvarko <odvarko@gmail.com>
2018-04-26 19:28:10 +0200

    Bug 1456129 - Extend HAR toolbar with import; r=davidwalsh
    MozReview-Commit-ID: 2g5sVon7xpk
    
    --HG--
    extra : rebase_source : 8044e25d9af8367c430ad1e0b3ebeaba14993a47

--- devtools/client/netmonitor/src/utils/menu.js
+++ devtools/client/netmonitor/src/utils/menu.js
@@ -24,6 +24,10 @@ function showMenu(items, options) {
   // Build the menu object from provided menu items.
   let menu = new Menu();
   items.forEach((item) => {
+    if (item == "-") {
+      item = { type: "separator" };
+    }
+
     let menuItem = new MenuItem(item);
     let subItems = item.submenu;
 
