8ee80e7a76d1573117a66f31a27fe7a90205010c
Henrik Skupin <mail@hskupin.info>
2018-04-26 22:35:26 +0200

    Bug 1396185 - [wdspec] Remove unwanted docstring references to the spec. r=ato
    MozReview-Commit-ID: AMWiBBFO1jP

--- testing/web-platform/tests/webdriver/tests/navigation/get_title.py
+++ testing/web-platform/tests/webdriver/tests/navigation/get_title.py
@@ -8,8 +8,7 @@ from tests.support.inline import inline
 def read_global(session, name):
     return session.execute_script("return %s;" % name)
 
-# 1. If the current top-level browsing context is no longer open, return error
-#    with error code no such window.
+
 def test_title_from_closed_context(session, create_window):
     new_window = create_window()
     session.window_handle = new_window
@@ -20,19 +19,7 @@ def test_title_from_closed_context(session, create_window):
 
     assert_error(result, "no such window")
 
-# [...]
-# 2. Handle any user prompts and return its value if it is an error.
-# [...]
-# In order to handle any user prompts a remote end must take the following
-# steps:
-# 2. Run the substeps of the first matching user prompt handler:
-#
-#    [...]
-#    - dismiss state
-#      1. Dismiss the current user prompt.
-#    [...]
-#
-# 3. Return success.
+
 def test_title_handle_prompt_dismiss(new_session, add_browser_capabilites):
     _, session = new_session({"capabilities": {"alwaysMatch": add_browser_capabilites({"unhandledPromptBehavior": "dismiss"})}})
     session.url = inline("<title>WD doc title</title>")
@@ -67,19 +54,7 @@ def test_title_handle_prompt_dismiss(new_session, add_browser_capabilites):
     assert_dialog_handled(session, "dismiss #3")
     assert read_global(session, "dismiss3") == None
 
-# [...]
-# 2. Handle any user prompts and return its value if it is an error.
-# [...]
-# In order to handle any user prompts a remote end must take the following
-# steps:
-# 2. Run the substeps of the first matching user prompt handler:
-#
-#    [...]
-#    - accept state
-#      1. Accept the current user prompt.
-#    [...]
-#
-# 3. Return success.
+
 def test_title_handle_prompt_accept(new_session, add_browser_capabilites):
     _, session = new_session({"capabilities": {"alwaysMatch": add_browser_capabilites({"unhandledPromptBehavior": "accept"})}})
     session.url = inline("<title>WD doc title</title>")
@@ -113,18 +88,7 @@ def test_title_handle_prompt_accept(new_session, add_browser_capabilites):
     assert_dialog_handled(session, "accept #3")
     assert read_global(session, "accept3") == "" or read_global(session, "accept3") == "undefined"
 
-# [...]
-# 2. Handle any user prompts and return its value if it is an error.
-# [...]
-# In order to handle any user prompts a remote end must take the following
-# steps:
-# 2. Run the substeps of the first matching user prompt handler:
-#
-#    [...]
-#    - missing value default state
-#    - not in the table of simple dialogs
-#      1. Dismiss the current user prompt.
-#      2. Return error with error code unexpected alert open.
+
 def test_title_handle_prompt_missing_value(session, create_dialog):
     session.url = inline("<title>WD doc title</title>")
     create_dialog("alert", text="dismiss #1", result_var="dismiss1")
@@ -176,16 +140,7 @@ def test_title_handle_prompt_missing_value(session, create_dialog):
 #                                    "session/%s/title" % session.session_id)
 #    assert_error(result, "unexpected alert open")
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
-# [...]
-# The title attribute must, on getting, run the following algorithm:
-# [...]
-# 2. Otherwise, let value be the child text content of the title element [...]
-# [...]
-# 4. Return value.
+
 def test_title_from_top_context(session):
     session.url = inline("<title>Foobar</title><h2>Hello</h2>")
 
@@ -193,20 +148,7 @@ def test_title_from_top_context(session):
                                     "session/%s/title" % session.session_id)
     assert_success(result, read_global(session, "document.title"))
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
-# [...]
-# The title attribute must, on getting, run the following algorithm:
-# [...]
-# 2. Otherwise, let value be the child text content of the title element [...]
-#
-#    The title element of a document is the first title element in the document
-#    (in tree order), if there is one, or null otherwise.
-#
-# [...]
-# 4. Return value.
+
 def test_title_with_duplicate_element(session):
     session.url = inline("<title>First</title><title>Second</title>")
 
@@ -215,17 +157,7 @@ def test_title_with_duplicate_element(session):
 
     assert_success(result, read_global(session, "document.title"))
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
-# [...]
-# The title attribute must, on getting, run the following algorithm:
-# [...]
-# 2. Otherwise, let value be the child text content of the title element, or
-#    the empty string if the title element is null.
-# [...]
-# 4. Return value.
+
 def test_title_without_element(session):
     session.url = inline("<h2>Hello</h2>")
 
@@ -234,10 +166,7 @@ def test_title_without_element(session):
 
     assert_success(result, read_global(session, "document.title"))
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
+
 def test_title_after_modification(session):
     session.url = inline("<title>Initial</title><h2>Hello</h2>")
     session.execute_script("document.title = 'updated'")
@@ -247,16 +176,7 @@ def test_title_after_modification(session):
 
     assert_success(result, read_global(session, "document.title"))
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
-# [...]
-# The title attribute must, on getting, run the following algorithm:
-# [...]
-# 2. Otherwise, let value be the child text content of the title element [...]
-# 3. Strip and collapse ASCII whitespace in value.
-# 4. Return value.
+
 def test_title_strip_and_collapse(session):
     document = "<title>   a b\tc\nd\t \n e\t\n </title><h2>Hello</h2>"
     session.url = inline(document)
@@ -266,10 +186,7 @@ def test_title_strip_and_collapse(session):
 
     assert_success(result, read_global(session, "document.title"))
 
-# [...]
-# 3. Let title be the initial value of the title IDL attribute of the current
-#    top-level browsing context's active document.
-# 4. Return success with data title.
+
 def test_title_from_frame(session, create_frame):
     session.url = inline("<title>Parent</title>parent")
 
