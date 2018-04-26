cb54ece0d5fe04828ab32aa95416c2b1b547e5b2
Boris Zbarsky <bzbarsky@mit.edu>
2018-04-26 15:28:30 -0400

    Bug 1457156.  Rename nsPIDOMWindow::mFocusedNode to reflect that it's an Element now.  r=mccr8

--- dom/xul/nsXULElement.cpp
+++ dom/xul/nsXULElement.cpp
@@ -598,7 +598,7 @@ nsXULElement::PerformAccesskey(bool aKeyCausesActivation,
 
               // Return true if the element became focused.
               nsPIDOMWindowOuter* window = OwnerDoc()->GetWindow();
-              focused = (window && window->GetFocusedNode());
+              focused = (window && window->GetFocusedElement());
             }
           }
         }
