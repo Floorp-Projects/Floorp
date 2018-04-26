2d7d260a1e6fc94246bf651b364eae3f56a84e08
Ed Lee <edilee@mozilla.com>
2018-04-26 09:00:00 -0700

    Bug 1457192 - Add AS Router, frecency bonus and bug fixes to Activity Stream. r=ursula
    MozReview-Commit-ID: KUm67PMMNI1
    
    --HG--
    extra : rebase_source : 9ff58f5a09c0769fd51203ee94cf3fbdb40120fe

--- browser/extensions/activity-stream/test/unit/message-center/constants.js
+++
@@ -1,3 +0,0 @@
-export const CHILD_TO_PARENT_MESSAGE_NAME = "MessageCenter:child-to-parent";
-export const PARENT_TO_CHILD_MESSAGE_NAME = "MessageCenter:parent-to-child";
-export const EXPERIMENT_PREF = "messageCenterExperimentEnabled";
