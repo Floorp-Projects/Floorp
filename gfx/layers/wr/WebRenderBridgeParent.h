5846115845c59006cf87bd81103e1414ee7da85d
Dorel Luca <dluca@mozilla.com>
2018-04-26 05:07:47 +0300

    Backed out 2 changesets (bug 1456679) for Reftests failure. CLOSED TREE
    Backed out changeset c40209504a45 (bug 1456679)
    Backed out changeset 351ae90cc6a0 (bug 1456679)

--- gfx/layers/wr/WebRenderBridgeParent.h
+++ gfx/layers/wr/WebRenderBridgeParent.h
@@ -214,10 +214,8 @@ private:
   bool ShouldParentObserveEpoch();
   mozilla::ipc::IPCResult HandleShutdown();
 
-  // Returns true if there is any animation (including animations in delay
-  // phase).
-  bool AdvanceAnimations();
-  bool SampleAnimations(nsTArray<wr::WrOpacityProperty>& aOpacityArray,
+  void AdvanceAnimations();
+  void SampleAnimations(nsTArray<wr::WrOpacityProperty>& aOpacityArray,
                         nsTArray<wr::WrTransformProperty>& aTransformArray);
 
   CompositorBridgeParent* GetRootCompositorBridgeParent() const;
