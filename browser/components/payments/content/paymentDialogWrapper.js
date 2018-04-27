b70aff71777102391b183b07c6b75c798ca39de1
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2018-04-26 18:08:34 -0700

    Bug 1457287 - Follow-up to fix browser_address_edit.js. r=jaws on a CLOSED TREE
    MozReview-Commit-ID: 1StIu61pvwD

--- browser/components/payments/content/paymentDialogWrapper.js
+++ browser/components/payments/content/paymentDialogWrapper.js
@@ -24,7 +24,7 @@ XPCOMUtils.defineLazyGetter(this, "formAutofillStorage", () => {
   let storage;
   try {
     storage = ChromeUtils.import("resource://formautofill/FormAutofillStorage.jsm", {})
-                                .storage;
+                         .formAutofillStorage;
     storage.initialize();
   } catch (ex) {
     storage = null;
