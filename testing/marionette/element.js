7b1dbf5a3f40b769092ece668258445c34ffa0ad
David Burns <dburns@mozilla.com>
2018-04-28 00:18:53 +0100

    Bug 1387678 - Remove unused reference to container variable; r=ato
    MozReview-Commit-ID: IVWtmbSuXiw
    
    --HG--
    extra : rebase_source : a7d75cb615cdfbc4b4a8d55c5be9d7ca57e0e970

--- testing/marionette/element.js
+++ testing/marionette/element.js
@@ -1189,14 +1189,7 @@ element.getInViewCentrePoint = function(rect, window) {
 element.getPointerInteractablePaintTree = function(el) {
   const doc = el.ownerDocument;
   const win = doc.defaultView;
-  const container = {frame: win};
   const rootNode = el.getRootNode();
-
-  // Include shadow DOM host only if the element's root node is not the
-  // owner document.
-  if (rootNode !== doc) {
-    container.shadowRoot = rootNode;
-  }
 
   // pointer-interactable elements tree, step 1
   if (!el.isConnected) {
