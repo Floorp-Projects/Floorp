29267bec68ade7f16cf7344ea0beab4306481171
Boris Zbarsky <bzbarsky@mit.edu>
2018-04-26 23:37:34 -0400

    Bug 1455674 part 20.  Get rid of nsIDOMElement.  r=qdot

--- dom/svg/SVGFEImageElement.cpp
+++ dom/svg/SVGFEImageElement.cpp
@@ -43,7 +43,7 @@ nsSVGElement::StringInfo SVGFEImageElement::sStringInfo[3] =
 // nsISupports methods
 
 NS_IMPL_ISUPPORTS_INHERITED(SVGFEImageElement, SVGFEImageElementBase,
-                            nsIDOMNode, nsIDOMElement,
+                            nsIDOMNode,
                             imgINotificationObserver, nsIImageLoadingContent)
 
 //----------------------------------------------------------------------
