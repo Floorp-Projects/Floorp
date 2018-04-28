7d7d2a75cd126ee17fb0104c8bee0c3f9b8f9f1d
Brad Werth <bwerth@mozilla.com>
2018-04-27 18:10:51 -0700

    Bug 1457288 - Part 2: Clamp shape-margin distance field operands to prevent int multiplication overflow when allocating. r=mats

--- layout/generic/nsFloatManager.cpp
+++ layout/generic/nsFloatManager.cpp
@@ -636,7 +636,32 @@ protected:
   // function returns aIntervals.Length().
   static size_t MinIntervalIndexContainingY(const nsTArray<nsRect>& aIntervals,
                                             const nscoord aTargetY);
+
+  // These types, constants, and functions are useful for ShapeInfos that
+  // allocate a distance field. Efficient distance field calculations use
+  // integer values that are 5X the Euclidean distance. MAX_MARGIN_5X is the
+  // largest possible margin that we can calculate (in 5X integer dev pixels),
+  // given these constraints.
+  typedef uint16_t dfType;
+  static const dfType MAX_CHAMFER_VALUE;
+  static const dfType MAX_MARGIN;
+  static const dfType MAX_MARGIN_5X;
+
+  // This function returns a typed, overflow-safe value of aShapeMargin in
+  // 5X integer dev pixels.
+  static dfType CalcUsedShapeMargin5X(nscoord aShapeMargin,
+                                      int32_t aAppUnitsPerDevPixel);
 };
+
+const nsFloatManager::ShapeInfo::dfType
+nsFloatManager::ShapeInfo::MAX_CHAMFER_VALUE = 11;
+
+const nsFloatManager::ShapeInfo::dfType
+nsFloatManager::ShapeInfo::MAX_MARGIN = (std::numeric_limits<dfType>::max() -
+                                         MAX_CHAMFER_VALUE) / 5;
+
+const nsFloatManager::ShapeInfo::dfType
+nsFloatManager::ShapeInfo::MAX_MARGIN_5X = MAX_MARGIN * 5;
 
 /////////////////////////////////////////////////////////////////////////////
 // EllipseShapeInfo
@@ -783,29 +808,8 @@ nsFloatManager::EllipseShapeInfo::EllipseShapeInfo(const nsPoint& aCenter,
   // Euclidean distance. The distances will be approximately 5x the true
   // distance, quantized in integer units. The 5x is factored away in the
   // comparison which builds the intervals.
-
-  // Our distance field has to be able to hold values equal to the
-  // maximum shape-margin value that we care about faithfully rendering,
-  // times 5. A 16-bit unsigned int can represent up to ~ 65K which means
-  // we can handle a margin up to ~ 13K device pixels. That's good enough
-  // for practical usage. Any supplied shape-margin value higher than this
-  // maximum will be clamped.
-  typedef uint16_t dfType;
-  const dfType MAX_CHAMFER_VALUE = 11;
-  const dfType MAX_MARGIN = (std::numeric_limits<dfType>::max() -
-                             MAX_CHAMFER_VALUE) / 5;
-  const dfType MAX_MARGIN_5X = MAX_MARGIN * 5;
-
-  // Convert aShapeMargin to dev pixels, convert that into 5x-dev-pixel
-  // space, then clamp to MAX_MARGIN_5X.
-  float shapeMarginDevPixels =
-    NSAppUnitsToFloatPixels(aShapeMargin, aAppUnitsPerDevPixel);
-  int32_t shapeMarginDevPixelsInt5X =
-    NSToIntRound(5.0f * shapeMarginDevPixels);
-  NS_WARNING_ASSERTION(shapeMarginDevPixelsInt5X <= MAX_MARGIN_5X,
-                       "shape-margin is too large and is being clamped.");
-  dfType usedMargin5X = (dfType)std::min((int32_t)MAX_MARGIN_5X,
-                                         shapeMarginDevPixelsInt5X);
+  dfType usedMargin5X = CalcUsedShapeMargin5X(aShapeMargin,
+                                              aAppUnitsPerDevPixel);
 
   const LayoutDeviceIntSize bounds =
     LayoutDevicePixel::FromAppUnitsRounded(mRadii,
@@ -822,8 +826,13 @@ nsFloatManager::EllipseShapeInfo::EllipseShapeInfo(const nsPoint& aCenter,
   // we don't want to deal with casting them from unsigned ints.
   static const int32_t iExpand = 2;
   static const int32_t bExpand = 2;
-  const int32_t iSize = bounds.width + iExpand;
-  const int32_t bSize = bounds.height + bExpand;
+
+  // Clamp the size of our distance field sizes to prevent multiplication
+  // overflow.
+  static const int32_t DF_SIDE_MAX =
+    floor(sqrt((double)(std::numeric_limits<int32_t>::max())));
+  const int32_t iSize = std::min(bounds.width + iExpand, DF_SIDE_MAX);
+  const int32_t bSize = std::min(bounds.height + bExpand, DF_SIDE_MAX);
   auto df = MakeUniqueFallible<dfType[]>(iSize * bSize);
   if (!df) {
     // Without a distance field, we can't reason about the float area.
@@ -1557,29 +1566,8 @@ nsFloatManager::ImageShapeInfo::ImageShapeInfo(
     // approximately 5x the true distance, quantized in integer units.
     // The 5x is factored away in the comparison used in the final
     // pass which builds the intervals.
-
-    // Our distance field has to be able to hold values equal to the
-    // maximum shape-margin value that we care about faithfully rendering,
-    // times 5. A 16-bit unsigned int can represent up to ~ 65K which means
-    // we can handle a margin up to ~ 13K device pixels. That's good enough
-    // for practical usage. Any supplied shape-margin value higher than this
-    // maximum will be clamped.
-    typedef uint16_t dfType;
-    const dfType MAX_CHAMFER_VALUE = 11;
-    const dfType MAX_MARGIN = (std::numeric_limits<dfType>::max() -
-                               MAX_CHAMFER_VALUE) / 5;
-    const dfType MAX_MARGIN_5X = MAX_MARGIN * 5;
-
-    // Convert aShapeMargin to dev pixels, convert that into 5x-dev-pixel
-    // space, then clamp to MAX_MARGIN_5X.
-    float shapeMarginDevPixels =
-      NSAppUnitsToFloatPixels(aShapeMargin, aAppUnitsPerDevPixel);
-    int32_t shapeMarginDevPixelsInt5X =
-      NSToIntRound(5.0f * shapeMarginDevPixels);
-    NS_WARNING_ASSERTION(shapeMarginDevPixelsInt5X <= MAX_MARGIN_5X,
-                         "shape-margin is too large and is being clamped.");
-    dfType usedMargin5X = (dfType)std::min((int32_t)MAX_MARGIN_5X,
-                                           shapeMarginDevPixelsInt5X);
+    dfType usedMargin5X = CalcUsedShapeMargin5X(aShapeMargin,
+                                                aAppUnitsPerDevPixel);
 
     // Allocate our distance field.  The distance field has to cover
     // the entire aMarginRect, since aShapeMargin could bleed into it,
@@ -1600,7 +1588,6 @@ nsFloatManager::ImageShapeInfo::ImageShapeInfo(
     // math to work, but we don't want to deal with casting them from
     // unsigned ints.
     static int32_t kExpansionPerSide = 2;
-
     // Since dfOffset will be used in comparisons against expanded region
     // pixel values, it's convenient to add expansion amounts to dfOffset in
     // both axes, to simplify comparison math later.
@@ -1614,8 +1601,14 @@ nsFloatManager::ImageShapeInfo::ImageShapeInfo(
     const LayoutDeviceIntSize marginRectDevPixels =
       LayoutDevicePixel::FromAppUnitsRounded(aMarginRect.Size(),
                                              aAppUnitsPerDevPixel);
-    const int32_t wEx = marginRectDevPixels.width + (kExpansionPerSide * 2);
-    const int32_t hEx = marginRectDevPixels.height + (kExpansionPerSide * 2);
+    // Clamp the size of our distance field sizes to prevent multiplication
+    // overflow.
+    static const int32_t DF_SIDE_MAX =
+      floor(sqrt((double)(std::numeric_limits<int32_t>::max())));
+    const int32_t wEx = std::min(marginRectDevPixels.width +
+                                 (kExpansionPerSide * 2), DF_SIDE_MAX);
+    const int32_t hEx = std::min(marginRectDevPixels.height +
+                                 (kExpansionPerSide * 2), DF_SIDE_MAX);
 
     // Since the margin-box size is CSS controlled, and large values will
     // generate large wEx and hEx values, we do a falliable allocation for
@@ -2555,6 +2548,34 @@ nsFloatManager::ShapeInfo::MinIntervalIndexContainingY(
   }
 
   return endIdx;
+}
+
+/* static */ nsFloatManager::ShapeInfo::dfType
+nsFloatManager::ShapeInfo::CalcUsedShapeMargin5X(
+  nscoord aShapeMargin,
+  int32_t aAppUnitsPerDevPixel)
+{
+  // Our distance field has to be able to hold values equal to the
+  // maximum shape-margin value that we care about faithfully rendering,
+  // times 5. A 16-bit unsigned int can represent up to ~ 65K which means
+  // we can handle a margin up to ~ 13K device pixels. That's good enough
+  // for practical usage. Any supplied shape-margin value higher than this
+  // maximum will be clamped.
+  static const float MAX_MARGIN_5X_FLOAT = (float)MAX_MARGIN_5X;
+
+  // Convert aShapeMargin to dev pixels, convert that into 5x-dev-pixel
+  // space, then clamp to MAX_MARGIN_5X_FLOAT.
+  float shapeMarginDevPixels5X = 5.0f *
+    NSAppUnitsToFloatPixels(aShapeMargin, aAppUnitsPerDevPixel);
+  NS_WARNING_ASSERTION(shapeMarginDevPixels5X <= MAX_MARGIN_5X_FLOAT,
+                       "shape-margin is too large and is being clamped.");
+
+  // We calculate a minimum in float space, which takes care of any overflow
+  // or infinity that may have occurred earlier from multiplication of
+  // too-large aShapeMargin values.
+  float usedMargin5XFloat = std::min(shapeMarginDevPixels5X,
+                                     MAX_MARGIN_5X_FLOAT);
+  return (dfType)NSToIntRound(usedMargin5XFloat);
 }
 
 /* static */ UniquePtr<nscoord[]>
