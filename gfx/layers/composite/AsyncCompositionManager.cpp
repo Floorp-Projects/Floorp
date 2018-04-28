c8dd3aa945c80a89e8220fb26042823f4ccd37ed
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-04-28 18:08:15 +0900

    Bug 1457249 - Assert that there is an animation value set by animations when we skip calculation for newly animation value. r=kats
    MozReview-Commit-ID: F85nAmKIGCA

--- gfx/layers/composite/AsyncCompositionManager.cpp
+++ gfx/layers/composite/AsyncCompositionManager.cpp
@@ -684,11 +684,15 @@ SampleAnimations(Layer* aLayer,
             // Sanity check that the animation value is surely unchanged.
             switch (animations[0].property()) {
               case eCSSProperty_opacity:
+                MOZ_ASSERT(
+                  layer->AsHostLayer()->GetShadowOpacitySetByAnimation());
                 MOZ_ASSERT(FuzzyEqualsMultiplicative(
                   layer->AsHostLayer()->GetShadowOpacity(),
                   *(aStorage->GetAnimationOpacity(layer->GetCompositorAnimationsId()))));
                 break;
               case eCSSProperty_transform: {
+                MOZ_ASSERT(
+                  layer->AsHostLayer()->GetShadowTransformSetByAnimation());
                 AnimatedValue* transform =
                   aStorage->GetAnimatedValue(layer->GetCompositorAnimationsId());
                 MOZ_ASSERT(
