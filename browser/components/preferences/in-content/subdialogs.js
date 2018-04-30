1f009d7dff4c7197615f92cef034c14fabfb89f0
Zibi Braniecki <zbraniecki@mozilla.com>
2018-04-30 16:58:22 -0700

    Bug 1457252 - Ensure l10n and initialization are done before sizing the subdialog. r=Gijs
    MozReview-Commit-ID: 8YMpRuzz04A
    
    --HG--
    extra : rebase_source : 1fea4647d8aaa28e100acac18ef473679b134672

--- browser/components/preferences/in-content/subdialogs.js
+++ browser/components/preferences/in-content/subdialogs.js
@@ -257,9 +257,23 @@ SubDialog.prototype = {
     this._overlay.style.opacity = "0.01";
   },
 
-  _onLoad(aEvent) {
+  async _onLoad(aEvent) {
     if (aEvent.target.contentWindow.location == "about:blank") {
       return;
+    }
+
+    // In order to properly calculate the sizing of the subdialog, we need to
+    // ensure that all of the l10n is done.
+    if (aEvent.target.contentDocument.l10n) {
+      await aEvent.target.contentDocument.l10n.ready;
+    }
+
+    // Some subdialogs may want to perform additional, asynchronous steps during initializations.
+    //
+    // In that case, we expect them to define a Promise which will delay measuring
+    // until the promise is fulfilled.
+    if (aEvent.target.contentDocument.mozSubdialogReady) {
+      await aEvent.target.contentDocument.mozSubdialogReady;
     }
 
     // Do this on load to wait for the CSS to load and apply before calculating the size.
