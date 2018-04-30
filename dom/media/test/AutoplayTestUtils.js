af9c92271189edd52716e4c48f5e9c6bccd6a2e9
Chris Pearce <cpearce@mozilla.com>
2018-04-30 17:40:22 +1200

    Bug 1457048 - Test that whitelisted origins are able to autoplay. r=bryce
    MozReview-Commit-ID: 9kLIx8MzCY8

--- dom/media/test/AutoplayTestUtils.js
+++ dom/media/test/AutoplayTestUtils.js
@@ -25,3 +25,15 @@ function log(msg) {
   log_pane.appendChild(document.createTextNode(msg));
   log_pane.appendChild(document.createElement("br"));
 }
+
+const autoplayPermission = "autoplay-media";
+
+async function pushAutoplayAllowedPermission() {
+  return new Promise((resolve, reject) => {
+    SpecialPowers.pushPermissions([{
+      'type': autoplayPermission,
+      'allow': true,
+      'context': document
+    }], resolve);
+  });
+}
