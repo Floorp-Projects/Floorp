4a97a3f59f5d10bdf9402602c8fb86f19daf4952
Justin Wood <Callek@gmail.com>
2018-05-01 19:32:38 -0400

    Bug 1458395 - Do not use stub installer on esr60. r=aki
    On ESR[60] stub installer isn't designed to work, and we expect most users of esr will have no use for a stub installer. However this poses some problems where the taskgraph assumes that any Nightly generates a stub installer and thus it should be available along the way.
    
    The patch hardcodes the list of branches that do not need a stub installer, though we'll want to clean up this specification in some way, as Thunderbird will likely need it to be cleaner and we may want on-change builds to use this logic (e.g. for on-change l10n).
    
    Differential Revision: https://phabricator.services.mozilla.com/D1082

--- taskcluster/taskgraph/transforms/bouncer_submission.py
+++ taskcluster/taskgraph/transforms/bouncer_submission.py
@@ -92,6 +92,9 @@ def make_task_worker(config, jobs):
         resolve_keyed_by(
             job, 'scopes', item_name=job['name'], project=config.params['project']
         )
+        resolve_keyed_by(
+            job, 'bouncer-products', item_name=job['name'], project=config.params['project']
+        )
 
         # No need to filter out ja-JP-mac, we need to upload both; but we do
         # need to filter out the platforms they come with
--- testing/mozharness/configs/repackage/win32_signed.py
+++ testing/mozharness/configs/repackage/win32_signed.py
@@ -6,9 +6,14 @@ download_config = {
         "target.zip": os.environ.get("SIGNED_ZIP"),
         "setup.exe": os.environ.get("SIGNED_SETUP"),
         "mar.exe": os.environ.get("UNSIGNED_MAR"),
+}
+
+if not os.environ.get("NO_STUB_INSTALLER"):
+    # Some channels, like esr don't build a stub installer
+    download_config.update({
         # stub installer is only built on win32
         "setup-stub.exe": os.environ.get("SIGNED_SETUP_STUB"),
-    }
+    })
 
 repackage_config = [[
         "installer",
@@ -23,13 +28,17 @@ repackage_config = [[
         "-i", "{abs_work_dir}\\inputs\\target.zip",
         "--mar", "{abs_work_dir}\\inputs\\mar.exe",
         "-o", "{output_home}\\target.complete.mar",
-    ], [
+    ]]
+
+if not os.environ.get("NO_STUB_INSTALLER"):
+    # Some channels, like esr don't build a stub installer
+    repackage_config.append([
         "installer",
         "--tag", "{abs_mozilla_dir}\\browser\\installer\\windows\\stub.tag",
-         "--setupexe", "{abs_work_dir}\\inputs\\setup-stub.exe",
-         "-o", "{output_home}\\target.stub-installer.exe",
+        "--setupexe", "{abs_work_dir}\\inputs\\setup-stub.exe",
+        "-o", "{output_home}\\target.stub-installer.exe",
         "--sfx-stub", "other-licenses/7zstub/firefox/7zSD.sfx",
-    ]]
+    ])
 
 config = {
     "input_home": "{abs_work_dir}\\inputs",
