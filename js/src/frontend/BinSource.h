1762b3f52c0a047460e219ccfa837a8b4273696e
Eric Faust <efaustbmo@gmail.com>
2018-04-30 17:04:04 -0700

    Bug 1409815 - Part 1: Implement CompileGlobalBinASTScript. (r=Waldo, r=Yoric, r=jorendorff)

--- js/src/frontend/BinSource.h
+++ js/src/frontend/BinSource.h
@@ -16,6 +16,7 @@
 
 #include "mozilla/Maybe.h"
 
+#include "frontend/BCEParserHandle.h"
 #include "frontend/BinToken.h"
 #include "frontend/BinTokenReaderMultipart.h"
 #include "frontend/BinTokenReaderTester.h"
@@ -75,28 +76,6 @@ class BinASTParserBase: private JS::AutoGCRooter
 
 
   public:
-    ObjectBox* newObjectBox(JSObject* obj) {
-        MOZ_ASSERT(obj);
-
-        /*
-         * We use JSContext.tempLifoAlloc to allocate parsed objects and place them
-         * on a list in this Parser to ensure GC safety. Thus the tempLifoAlloc
-         * arenas containing the entries must be alive until we are done with
-         * scanning, parsing and code generation for the whole script or top-level
-         * function.
-         */
-
-         ObjectBox* objbox = alloc_.new_<ObjectBox>(obj, traceListHead_);
-         if (!objbox) {
-             ReportOutOfMemory(cx_);
-             return nullptr;
-        }
-
-        traceListHead_ = objbox;
-
-        return objbox;
-    }
-
     ParseNode* allocParseNode(size_t size) {
         MOZ_ASSERT(size == sizeof(ParseNode));
         return static_cast<ParseNode*>(nodeAlloc_.allocNode());
@@ -138,7 +117,7 @@ class BinASTParserBase: private JS::AutoGCRooter
  * recoverable.
  */
 template<typename Tok>
-class BinASTParser : public BinASTParserBase, public ErrorReporter
+class BinASTParser : public BinASTParserBase, public ErrorReporter, public BCEParserHandle
 {
   public:
     using Tokenizer = Tok;
@@ -228,6 +207,41 @@ class BinASTParser : public BinASTParserBase, public ErrorReporter
         return this->options_;
     }
 
+  public:
+    virtual ObjectBox* newObjectBox(JSObject* obj) override {
+        MOZ_ASSERT(obj);
+
+        /*
+         * We use JSContext.tempLifoAlloc to allocate parsed objects and place them
+         * on a list in this Parser to ensure GC safety. Thus the tempLifoAlloc
+         * arenas containing the entries must be alive until we are done with
+         * scanning, parsing and code generation for the whole script or top-level
+         * function.
+         */
+
+         ObjectBox* objbox = alloc_.new_<ObjectBox>(obj, traceListHead_);
+         if (!objbox) {
+             ReportOutOfMemory(cx_);
+             return nullptr;
+        }
+
+        traceListHead_ = objbox;
+
+        return objbox;
+    }
+
+
+    virtual ErrorReporter& errorReporter() override {
+        return *this;
+    }
+    virtual const ErrorReporter& errorReporter() const override {
+        return *this;
+    }
+
+    virtual FullParseHandler& astGenerator() override {
+        return factory_;
+    }
+
     virtual void lineAndColumnAt(size_t offset, uint32_t* line, uint32_t* column) const override {
         *line = lineAt(offset);
         *column = columnAt(offset);
@@ -266,6 +280,7 @@ class BinASTParser : public BinASTParserBase, public ErrorReporter
         return this->options_.filename();
     }
 
+  private: // Implement ErrorReporter
     Maybe<Tokenizer> tokenizer_;
     VariableDeclarationKind variableDeclarationKind_;
 
