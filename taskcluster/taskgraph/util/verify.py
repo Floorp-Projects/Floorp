397efe5e3f77d0f570af4f38ff1de1d57533254b
Chris AtLee <catlee@mozilla.com>
2018-05-01 16:07:19 -0400

    Bug 1450029: Remove buildbot/buildbot-bridge references from taskcluster r=aki,tomprince
    MozReview-Commit-ID: Hu9ju4XVQpA

--- taskcluster/taskgraph/util/verify.py
+++ taskcluster/taskgraph/util/verify.py
@@ -11,7 +11,6 @@ import os
 import sys
 
 from .. import GECKO
-from taskgraph.util.bbb_validation import valid_bbb_builders
 
 logger = logging.getLogger(__name__)
 base_path = os.path.join(GECKO, 'taskcluster', 'docs')
@@ -143,14 +142,9 @@ def verify_dependency_tiers(task, taskgraph, scratch_pad):
             return tier
 
         for task in taskgraph.tasks.itervalues():
-            # Buildbot bridge tasks cannot have tiers, so we cannot enforce
-            # this check for them
-            if task.task.get("workerType") == "buildbot-bridge":
-                continue
             tier = tiers[task.label]
             for d in task.dependencies.itervalues():
-                if taskgraph[d].task.get("workerType") in ("buildbot-bridge",
-                                                           "always-optimized"):
+                if taskgraph[d].task.get("workerType") == "always-optimized":
                     continue
                 if "dummy" in taskgraph[d].kind:
                     continue
@@ -159,29 +153,6 @@ def verify_dependency_tiers(task, taskgraph, scratch_pad):
                         '{} (tier {}) cannot depend on {} (tier {})'
                         .format(task.label, printable_tier(tier),
                                 d, printable_tier(tiers[d])))
-
-
-@verifications.add('optimized_task_graph')
-def verify_bbb_builders_valid(task, taskgraph, scratch_pad):
-    """
-        This function ensures that any task which is run
-        in buildbot (via buildbot-bridge) is using a recognized buildername.
-
-        If you see an unexpected failure with a task due to this check, please
-        see the IRC Channel, #releng.
-    """
-    if task is None:
-        return
-    valid_builders = valid_bbb_builders()
-    if valid_builders is None:
-        return
-    if task.task.get('workerType') == 'buildbot-bridge':
-        buildername = task.task['payload']['buildername']
-        if buildername not in valid_builders:
-            logger.warning(
-                '{} uses an invalid buildbot buildername ("{}") '
-                ' - contact #releng for help'
-                .format(task.label, buildername))
 
 
 @verifications.add('optimized_task_graph')
