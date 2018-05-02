3101fbd957e25eafd0c56bb65692946803b12dba
Chris H-C <chutten@mozilla.com>
2018-05-02 10:55:31 -0400

    bug 1447217 - Stop loading addon manager in activeticks test r=Dexter
    MozReview-Commit-ID: 2VPpcz9hOvT

--- toolkit/components/telemetry/tests/unit/test_TelemetrySession_activeTicks.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetrySession_activeTicks.js
@@ -7,10 +7,7 @@ ChromeUtils.import("resource://gre/modules/TelemetrySession.jsm", this);
 
 
 add_task(async function test_setup() {
-  // Addon manager needs a profile directory
   do_get_profile();
-  loadAddonManager("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
-  finishAddonManagerStartup();
   // Make sure we don't generate unexpected pings due to pref changes.
   await setEmptyPrefWatchlist();
 });
