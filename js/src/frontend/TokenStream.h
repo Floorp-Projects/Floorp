3af48bd1a0a5cd92bb409040781be69c30abe48c
Jeff Walden <jwalden@mit.edu>
2018-05-01 19:06:27 -0700

    Bug 1459384 - Move decimal number tokenizing into a separate function and eliminate the final two labels from TokenStreamSpecific::getTokenInternal.  r=arai
    --HG--
    extra : rebase_source : 4876163dca864162d0ae9a963e5551f93fbc923b

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -1369,6 +1369,46 @@ class MOZ_STACK_CLASS TokenStreamSpecific
 
     MOZ_MUST_USE bool putIdentInTokenbuf(const CharT* identStart);
 
+    /**
+     * Tokenize a decimal number that begins at |numStart| into the provided
+     * token.
+     *
+     * |c| must be one of these values:
+     *
+     *   1. The first decimal digit in the integral part of a decimal number
+     *      not starting with '0' or '.', e.g. '1' for "17", '3' for "3.14", or
+     *      '8' for "8.675309e6".
+     *
+     *   In this case, the next |getCharIgnoreEOL()| must return the code unit
+     *   after |c| in the overall number.
+     *
+     *   2. The '.' in a "."/"0."-prefixed decimal number or the 'e'/'E' in a
+     *      "0e"/"0E"-prefixed decimal number, e.g. ".17", "0.42", or "0.1e3".
+     *
+     *   In this case, the next |getCharIgnoreEOL()| must return the code unit
+     *   *after* the first decimal digit *after* the '.'.  So the next code
+     *   unit would be '7' in ".17", '2' in "0.42", 'e' in "0.4e+8", or '/' in
+     *   "0.5/2" (three separate tokens).
+     *
+     *   3. The code unit after the '0' where "0" is the entire number token.
+     *
+     *   In this case, the next |getCharIgnoreEOL()| returns the code unit
+     *   after |c|.
+     *
+     *   4. (Non-strict mode code only)  The first '8' or '9' in a "noctal"
+     *      number that begins with a '0' but contains a non-octal digit in its
+     *      integer part so is interpreted as decimal, e.g. '9' in "09.28" or
+     *      '8' in "0386" or '9' in "09+7" (three separate tokens").
+     *
+     *   In this case, the next |getCharIgnoreEOL()| returns the code unit
+     *   after |c|: '.', '6', or '+' in the examples above.
+     *
+     * This interface is super-hairy and horribly stateful.  Unfortunately, its
+     * hair merely reflects the intricacy of ECMAScript numeric literal syntax.
+     * And incredibly, it *improves* on the goto-based horror that predated it.
+     */
+    MOZ_MUST_USE bool decimalNumber(int c, Token* tp, const CharT* numStart);
+
   public:
     // Advance to the next token.  If the token stream encountered an error,
     // return false.  Otherwise return true and store the token kind in |*ttp|.
@@ -1564,6 +1604,14 @@ class MOZ_STACK_CLASS TokenStreamSpecific
     void consumeKnownChar(int32_t expect) {
         int32_t c;
         MOZ_ALWAYS_TRUE(getChar(&c));
+        MOZ_ASSERT(c == expect);
+    }
+
+    void consumeKnownCharIgnoreEOL(int32_t expect) {
+#ifdef DEBUG
+        auto c =
+#endif
+            getCharIgnoreEOL();
         MOZ_ASSERT(c == expect);
     }
 
