9ad2ef9e8af52a1a469334bc186214b58da80b7b
Jan-Erik Rediger <jrediger@mozilla.com>
2018-05-02 15:56:03 +0200

    Bug 1320052 - Update external snapshot callers to not deal with subsessions. r=chutten
    MozReview-Commit-ID: DGTer5oMnwd

--- toolkit/components/osfile/tests/xpcshell/test_telemetry.js
+++ toolkit/components/osfile/tests/xpcshell/test_telemetry.js
@@ -28,14 +28,12 @@ add_task(async function test_startup() {
   let READY = "OSFILE_WORKER_READY_MS";
 
   let before = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                     false,
                                                      false).parent;
 
   // Launch the OS.File worker
   await File.getCurrentDirectory();
 
   let after = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                    false,
                                                     false).parent;
 
 
@@ -52,7 +50,6 @@ add_task(async function test_writeAtomic() {
   let LABEL = "OSFILE_WRITEATOMIC_JANK_MS";
 
   let before = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                     false,
                                                      false).parent;
 
   // Perform a write.
@@ -60,7 +57,6 @@ add_task(async function test_writeAtomic() {
   await File.writeAtomic(path, LABEL, { tmpPath: path + ".tmp" } );
 
   let after = Services.telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN,
-                                                    false,
                                                     false).parent;
 
   Assert.equal(getCount(after[LABEL]), getCount(before[LABEL]) + 1);
