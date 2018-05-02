82975ba3fb6ba437b664189741ba97b3bebd87b4
Jeff Boek <jeff@jeffboek.com>
2018-05-02 12:08:40 -0700

    [focus] For https://github.com/mozilla-mobile/focus-android/issues/2563 - Fixes missing locale

--- mobile/android/focus-android/tools/l10n/locales.py
+++ mobile/android/focus-android/tools/l10n/locales.py
@@ -94,9 +94,10 @@ RELEASE_LOCALES = [
 	"uk",
 	"ur",
 	"vi",
+	"wo",
+	"zam",
 	"zh-CN",
-	"zh-TW",
-	"zam"
+	"zh-TW"
 ]
 
 # This is the list of locales that we want to take automated screenshots of
@@ -105,8 +106,7 @@ RELEASE_LOCALES = [
 # verify their work in progress.
 ADDITIONAL_SCREENSHOT_LOCALES = [
 	"ixl",
-	"lt",
-	"wo"
+	"lt"
 ]
 
 # Those are locales that we take automated screenshots of.
