c91667a188e0285a0d25c6202d351ec0d54935c5
Henrik Skupin <mail@hskupin.info>
2018-05-03 15:50:43 +0200

    Bug 1414776 - [fxui] Reset security.mixed_content.upgrade_display_content preference for mixed content test. r=maja_zf
    This regressed by bug 1435733 which didn't clear the pref in tearDown of the test, causing
    following tests to still have this preference set.
    
    MozReview-Commit-ID: n9conybbQz

--- testing/firefox-ui/tests/functional/security/test_mixed_content_page.py
+++ testing/firefox-ui/tests/functional/security/test_mixed_content_page.py
@@ -12,6 +12,7 @@ class TestMixedContentPage(PuppeteerMixin, MarionetteTestCase):
         super(TestMixedContentPage, self).setUp()
 
         self.marionette.set_pref('security.mixed_content.upgrade_display_content', False)
+
         self.locationbar = self.browser.navbar.locationbar
         self.identity_popup = self.locationbar.identity_popup
 
@@ -19,6 +20,8 @@ class TestMixedContentPage(PuppeteerMixin, MarionetteTestCase):
 
     def tearDown(self):
         try:
+            self.marionette.clear_pref('security.mixed_content.upgrade_display_content')
+
             self.identity_popup.close(force=True)
         finally:
             super(TestMixedContentPage, self).tearDown()
