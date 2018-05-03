613e18b0483487d8b417038980ce04460fc3703d
Tom Ritter <tom@mozilla.com>
2018-05-03 09:14:12 -0500

    Bug 1458897 Fix svn revert in build-clang so one can re-run it successfully r=glandium
    svn revert requires a path, and does not take a revision.  This isn't an
    issue on build machines because we do a fresh checkout every time.
    
    But if you're trying to run build-clang locally, with existing checkouts,
    it will:
    1) successfully svn update
    2) run svn revert, saying "Skipped <rev>"
          (except you don't see it because of -q)
    3) svn revert returns a successfull eror code
    4) patch fails because the file was never reverted and it attempts
       to re-apply the patch
    
    Also I think the revert command needs to come first.
    
    MozReview-Commit-ID: 4OfrJNZwJNU
    
    --HG--
    extra : rebase_source : b3474e8048b3110f3f5948c3351923c02735ca4d

--- build/build-clang/build-clang.py
+++ build/build-clang/build-clang.py
@@ -139,8 +139,8 @@ def svn_co(source_dir, url, directory, revision):
 
 
 def svn_update(directory, revision):
+    run_in(directory, ["svn", "revert", "-q", "-R", "."])
     run_in(directory, ["svn", "update", "-q", "-r", revision])
-    run_in(directory, ["svn", "revert", "-q", "-R", revision])
 
 
 def is_darwin():
