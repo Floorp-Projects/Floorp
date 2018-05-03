fdae4f85f29ee39f0e020f97e939dac4d9fc2d8e
Tom Prince <mozilla@hocat.ca>
2018-05-03 04:25:03 -0600

    Bug 1461784: [release-promotion] Always respect previous graphs, even on try; r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D1290

--- taskcluster/taskgraph/actions/release_promotion.py
+++ taskcluster/taskgraph/actions/release_promotion.py
@@ -356,6 +356,9 @@ def release_promotion_action(parameters, graph_config, input, task_group_id, tas
     parameters['release_enable_partners'] = release_enable_partners
     parameters['release_partners'] = input.get('release_partners')
     parameters['release_enable_emefree'] = release_enable_emefree
+    # When doing staging releases on try, we still want to re-use tasks from
+    # previous graphs.
+    parameters['optimize_target_tasks'] = True
 
     partner_config = input.get('release_partner_config')
     if not partner_config and (release_enable_emefree or release_enable_partners):
