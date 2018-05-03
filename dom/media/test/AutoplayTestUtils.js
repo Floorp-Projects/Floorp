ecdf00625bab6f5c499dab7d428f33581c9fe5a7
Dorel Luca <dluca@mozilla.com>
2018-05-03 08:39:36 +0300

    Backed out 2 changesets (bug 1457048) for multiple failures. CLOSED TREE
    Backed out changeset a5d71f8bf413 (bug 1457048)
    Backed out changeset cd70fc188bc8 (bug 1457048)

--- dom/media/test/AutoplayTestUtils.js
+++ dom/media/test/AutoplayTestUtils.js
@@ -25,15 +25,3 @@ function log(msg) {
   log_pane.appendChild(document.createTextNode(msg));
   log_pane.appendChild(document.createElement("br"));
 }
-
-const autoplayPermission = "autoplay-media";
-
-async function pushAutoplayAllowedPermission() {
-  return new Promise((resolve, reject) => {
-    SpecialPowers.pushPermissions([{
-      'type': autoplayPermission,
-      'allow': true,
-      'context': document
-    }], resolve);
-  });
-}
