ced481ced5abdf9648b19ab74a0d719dae1d33e4
Geoffrey Sneddon <me@gsnedders.com>
2018-05-03 22:05:27 +0000

    Bug 1448567 [wpt PR 10166] - Add alternate_hosts, a=testonly
    Automatic update from web-platform-testsFix #2669: Add alternate_hosts (#10166)
    
    --
    
    wpt-commits: dfeea56c848137e18c75e1f931f4b3e1497ea61b
    wpt-pr: 10166

--- testing/web-platform/tests/tools/serve/test_serve.py
+++ testing/web-platform/tests/tools/serve/test_serve.py
@@ -1,12 +1,22 @@
 from . import serve
 
 def test_make_hosts_file():
-    hosts = serve.make_hosts_file({
-        "domains": {"www": "www.foo.bar.test", "www1": "www1.foo.bar.test"},
-        "not_domains": {"aaa": "aaa.foo.bar.test", "bbb": "bbb.foo.bar.test"}
-    }, "127.1.1.1")
+    c = serve.Config(browser_host="foo.bar", alternate_hosts={"alt": "foo2.bar"})
+    hosts = serve.make_hosts_file(c, "192.168.42.42")
     lines = hosts.split("\n")
-    assert "127.1.1.1\twww.foo.bar.test" in lines
-    assert "127.1.1.1\twww1.foo.bar.test" in lines
-    assert "0.0.0.0\taaa.foo.bar.test" in lines
-    assert "0.0.0.0\tbbb.foo.bar.test" in lines
+    assert set(lines) == {"",
+                          "0.0.0.0\tnonexistent-origin.foo.bar",
+                          "0.0.0.0\tnonexistent-origin.foo2.bar",
+                          "192.168.42.42\tfoo.bar",
+                          "192.168.42.42\tfoo2.bar",
+                          "192.168.42.42\twww.foo.bar",
+                          "192.168.42.42\twww.foo2.bar",
+                          "192.168.42.42\twww1.foo.bar",
+                          "192.168.42.42\twww1.foo2.bar",
+                          "192.168.42.42\twww2.foo.bar",
+                          "192.168.42.42\twww2.foo2.bar",
+                          "192.168.42.42\txn--lve-6lad.foo.bar",
+                          "192.168.42.42\txn--lve-6lad.foo2.bar",
+                          "192.168.42.42\txn--n8j6ds53lwwkrqhv28a.foo.bar",
+                          "192.168.42.42\txn--n8j6ds53lwwkrqhv28a.foo2.bar"}
+    assert lines[-1] == ""
