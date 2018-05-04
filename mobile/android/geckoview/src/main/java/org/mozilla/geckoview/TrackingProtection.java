f69a6f9dd0a009099523cd4ad74bce7b382baf1b
Eugen Sawin <esawin@mozilla.com>
2018-05-04 21:13:41 +0200

    Bug 1463772 - [1.4] Move and refactor tracking protection blocklist selection. r=snorp,jchen

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TrackingProtection.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TrackingProtection.java
@@ -6,49 +6,45 @@
 
 package org.mozilla.geckoview;
 
-import android.util.Log;
-
-import org.mozilla.gecko.PrefsHelper;
 import org.mozilla.geckoview.GeckoSession.TrackingProtectionDelegate;
 
 /* package */ class TrackingProtection {
-    private static final boolean DEBUG = false;
-
-    private static final String LOGTAG = "GeckoViewTrackingProtection";
-    private static final String TRACKERS_PREF = "urlclassifier.trackingTable";
-    private static final String LISTS_PREF =
-        "browser.safebrowsing.provider.mozilla.lists";
-
+    private static final String TEST = "test-track-simple";
     private static final String AD = "ads-track-digest256";
     private static final String ANALYTIC = "analytics-track-digest256";
     private static final String SOCIAL = "social-track-digest256";
     private static final String CONTENT = "content-track-digest256";
 
-    private String buildPrefValue(int categories) {
+    /* package */ static String buildPrefValue(int categories) {
         StringBuilder builder = new StringBuilder();
-        builder.append("test-track-simple");
 
-        if ((categories & TrackingProtectionDelegate.CATEGORY_AD) ==
-            TrackingProtectionDelegate.CATEGORY_AD) {
-            builder.append(",").append(AD);
+        if ((categories == TrackingProtectionDelegate.CATEGORY_NONE)) {
+            return "";
         }
-        if ((categories & TrackingProtectionDelegate.CATEGORY_ANALYTIC) ==
-            TrackingProtectionDelegate.CATEGORY_ANALYTIC) {
-            builder.append(",").append(ANALYTIC);
+        if ((categories & TrackingProtectionDelegate.CATEGORY_TEST) != 0) {
+            builder.append(TEST).append(',');
         }
-        if ((categories & TrackingProtectionDelegate.CATEGORY_SOCIAL) ==
-            TrackingProtectionDelegate.CATEGORY_SOCIAL) {
-            builder.append(",").append(SOCIAL);
+        if ((categories & TrackingProtectionDelegate.CATEGORY_AD) != 0) {
+            builder.append(AD).append(',');
         }
-        if ((categories & TrackingProtectionDelegate.CATEGORY_CONTENT) ==
-            TrackingProtectionDelegate.CATEGORY_CONTENT) {
-            builder.append(",").append(CONTENT);
+        if ((categories & TrackingProtectionDelegate.CATEGORY_ANALYTIC) != 0) {
+            builder.append(ANALYTIC).append(',');
         }
-        return builder.toString();
+        if ((categories & TrackingProtectionDelegate.CATEGORY_SOCIAL) != 0) {
+            builder.append(SOCIAL).append(',');
+        }
+        if ((categories & TrackingProtectionDelegate.CATEGORY_CONTENT) != 0) {
+            builder.append(CONTENT).append(',');
+        }
+        // Trim final ','.
+        return builder.substring(0, builder.length() - 1);
     }
 
     /* package */ static int listToCategory(final String list) {
         int category = 0;
+        if (list.indexOf(TEST) != -1) {
+            category |= TrackingProtectionDelegate.CATEGORY_TEST;
+        }
         if (list.indexOf(AD) != -1) {
             category |= TrackingProtectionDelegate.CATEGORY_AD;
         }
@@ -62,31 +58,5 @@ import org.mozilla.geckoview.GeckoSession.TrackingProtectionDelegate;
             category |= TrackingProtectionDelegate.CATEGORY_CONTENT;
         }
         return category;
-    }
-
-    private final GeckoSession mSession;
-
-    /* package */ TrackingProtection(final GeckoSession session) {
-        mSession = session;
-    }
-
-    public void enable(final int categories) {
-        if (categories == 0) {
-            disable();
-            return;
-        }
-
-        final String prefValue = buildPrefValue(categories);
-        PrefsHelper.setPref(TRACKERS_PREF, prefValue);
-
-        mSession.getSettings().setBoolean(
-            GeckoSessionSettings.USE_TRACKING_PROTECTION, true);
-
-        if (DEBUG) Log.d(LOGTAG, "Tracking protection enabled (" + prefValue + ")");
-    }
-
-    public void disable() {
-        mSession.getSettings().setBoolean(
-            GeckoSessionSettings.USE_TRACKING_PROTECTION, false);
     }
 }
