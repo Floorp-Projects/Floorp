86ea9eb2b36938260e0fd14da2bde3ef0dd581da
Xidorn Quan <me@upsuper.org>
2018-05-04 15:20:58 +1000

    Bug 1454591 part 6 - Remove CSS_PROP_LIST_ONLY_COMPONENTS_OF_ALL_SHORTHAND. r=heycam
    MozReview-Commit-ID: RoKvxNSSdo

--- layout/style/GenerateServoCSSPropList.py
+++ layout/style/GenerateServoCSSPropList.py
@@ -85,15 +85,10 @@ def generate_header(output, data):
                 flags = "CSSPropFlags(0)"
             params = [prop.name, prop.id, method, flags, pref]
 
-        is_component_of_all = not is_internal and prop.name not in ["direction", "unicode-bidi"]
-        if not is_component_of_all:
-            output.write("#ifndef CSS_PROP_LIST_ONLY_COMPONENTS_OF_ALL_SHORTHAND\n")
         if is_internal:
             output.write("#ifndef CSS_PROP_LIST_EXCLUDE_INTERNAL\n")
         output.write("{}({})\n".format(MACRO_NAMES[prop.type()], ", ".join(params)))
         if is_internal:
-            output.write("#endif\n")
-        if not is_component_of_all:
             output.write("#endif\n")
 
     output.write("""
