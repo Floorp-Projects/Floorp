531d363d659503a856352b72699f3e36df8be669
Sebastian Kaspari <s.kaspari@gmail.com>
2018-05-04 16:05:34 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/pull/16: Create library skeleton.

---
+++ mobile/android/android-components/fretboard/src/test/java/mozilla/components/service/fretboard/FretboardTest.kt
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.service.fretboard
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class FretboardTest {
+    @Test
+    fun dummyTest() {
+        assertEquals(1, 1)
+    }
+}
