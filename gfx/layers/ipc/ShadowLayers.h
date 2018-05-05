ac1648320e68e54089ed20685a02b4df72e68b37
Doug Thayer <dothayer@mozilla.com>
2018-05-05 15:46:26 -0700

    Bug 1265824 - Wait on texture handles with IPC r=jld,mattwoodrow
    There's a lot going on here, but it all fits under the idea of
    being able to communicate about texture locking statuses
    without spinning on IsReadLocked. This is a bit of a trade -
    we could just always allocate/grab a texture from the pool,
    which would put a smaller cap on the amount of time we can
    possibly spend when a texture is locked. However, this eats
    up more CPU and memory than waiting on the textures to unlock,
    and could take longer, especially if there were a large number
    of textures which we just need to wait for for a short amount
    of time. In any case, we very rarely hit the case where we
    actually need to wait on the sync IPC to the compositor - most
    of the time the textures are already unlocked.
    
    There is also an async IPC call in here, which we make before
    flushing async paints. This just causes the compositor to
    check whether the GPU is done with its textures or not and
    unlock them if it is. This helps us avoid the case where we
    take a long time painting asynchronously, turn IPC back on at
    the end of that, and then have to wait for the compositor
    to to get into TiledLayerBufferComposite::UseTiles before
    getting a response. Specifically this eliminates several talos
    regressions which use ASAP mode.
    
    Lastly, there seem to be no other cases of static Monitors
    being used. This seems like it falls under similar use cases
    as StaticMutexes, so I added it in. I can move it into its own
    file if we think it might be generally useful in the future.
    
    MozReview-Commit-ID: IYQLwUqMxg2
    
    --HG--
    extra : rebase_source : 67f6fee8b89933561a48e6f7f531b6969893a574

--- gfx/layers/ipc/ShadowLayers.h
+++ gfx/layers/ipc/ShadowLayers.h
@@ -361,6 +361,9 @@ public:
   virtual void UpdateFwdTransactionId() override;
   virtual uint64_t GetFwdTransactionId() override;
 
+  void UpdateTextureLocks();
+  void SyncTextures(const nsTArray<uint64_t>& aSerials);
+
   void ReleaseLayer(const LayerHandle& aHandle);
 
   bool InForwarderThread() override {
