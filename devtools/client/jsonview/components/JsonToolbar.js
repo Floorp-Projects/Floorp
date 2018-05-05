2f49407684491e358856ca88a2dc5579e4d2a77a
Ognjen Galic <smclt30p@gmail.com>
2018-05-05 12:40:18 +0200

    Bug 1264557 - Add expand all/collapse all buttons. r=honza
    This adds buttons to collapse and expand the JSON tree. If the file
    is larger than 100kB the "Expand All" button is hidden for performance
    reasonds.
    
    --HG--
    extra : histedit_source : 3fa4d8e5523afbc423ebc5a6d803bdb84100f9d7

--- devtools/client/jsonview/components/JsonToolbar.js
+++ devtools/client/jsonview/components/JsonToolbar.js
@@ -15,6 +15,9 @@ define(function(require, exports, module) {
   const { SearchBox } = createFactories(require("./SearchBox"));
   const { Toolbar, ToolbarButton } = createFactories(require("./reps/Toolbar"));
 
+  /* 100kB file */
+  const EXPAND_THRESHOLD = 100 * 1024;
+
   /**
    * This template represents a toolbar within the 'JSON' panel.
    */
@@ -22,6 +25,7 @@ define(function(require, exports, module) {
     static get propTypes() {
       return {
         actions: PropTypes.object,
+        dataSize: PropTypes.number,
       };
     }
 
@@ -29,6 +33,8 @@ define(function(require, exports, module) {
       super(props);
       this.onSave = this.onSave.bind(this);
       this.onCopy = this.onCopy.bind(this);
+      this.onCollapse = this.onCollapse.bind(this);
+      this.onExpand = this.onExpand.bind(this);
     }
 
     // Commands
@@ -41,6 +47,14 @@ define(function(require, exports, module) {
       this.props.actions.onCopyJson();
     }
 
+    onCollapse(event) {
+      this.props.actions.onCollapse();
+    }
+
+    onExpand(event) {
+      this.props.actions.onExpand();
+    }
+
     render() {
       return (
         Toolbar({},
@@ -49,6 +63,13 @@ define(function(require, exports, module) {
           ),
           ToolbarButton({className: "btn copy", onClick: this.onCopy},
             JSONView.Locale.$STR("jsonViewer.Copy")
+          ),
+          ToolbarButton({className: "btn collapse", onClick: this.onCollapse},
+            JSONView.Locale.$STR("jsonViewer.CollapseAll")
+          ),
+          this.props.dataSize > EXPAND_THRESHOLD ? undefined :
+          ToolbarButton({className: "btn expand", onClick: this.onExpand},
+            JSONView.Locale.$STR("jsonViewer.ExpandAll")
           ),
           SearchBox({
             actions: this.props.actions
