4bed5fb3654aa0c6f35a511383ec1cce81e2c7b6
Brian Birtles <birtles@gmail.com>
2018-05-07 10:48:48 +0900

    Bug 1456394 - Move KeyframeEffect attribute setters to KeyframeEffectReadOnly; r=bz,hiro
    It might seem a bit odd to move the setters to the read-only class that we are
    ultimately planning to drop but the reason for doing this is that
    KeyframeEffectReadOnly.cpp has a *lot* more code than KeyframeEffect.cpp.
    
    In order to simplify code archaeology we take the following approach:
    
      1. Move code from KeyframeEffect.{h,cpp} to KeyframeEffectReadOnly.{h,cpp}.
      2. Delete KeyframeEffect.{h,cpp}.
      3. Rename KeyframeEffectReadOnly.{h,cpp} to KeyframeEffect.{h,cpp}.
    
    Note that at least steps 2 and 3 must be performed in separate patches as
    mercurial does not successfully track renames when the target name already
    exists.
    
    MozReview-Commit-ID: LwJoxGJitKR

--- dom/animation/KeyframeEffectReadOnly.cpp
+++ dom/animation/KeyframeEffectReadOnly.cpp
@@ -100,16 +100,60 @@ KeyframeEffectReadOnly::WrapObject(JSContext* aCx,
   return KeyframeEffectReadOnlyBinding::Wrap(aCx, this, aGivenProto);
 }
 
-IterationCompositeOperation
-KeyframeEffectReadOnly::IterationComposite() const
+IterationCompositeOperation KeyframeEffectReadOnly::IterationComposite(
+  CallerType /*aCallerType*/) const
 {
   return mEffectOptions.mIterationComposite;
+}
+
+void
+KeyframeEffectReadOnly::SetIterationComposite(
+  const IterationCompositeOperation& aIterationComposite,
+  CallerType aCallerType)
+{
+  // Ignore iterationComposite if the Web Animations API is not enabled,
+  // then the default value 'Replace' will be used.
+  if (!nsDocument::IsWebAnimationsEnabled(aCallerType)) {
+    return;
+  }
+
+  if (mEffectOptions.mIterationComposite == aIterationComposite) {
+    return;
+  }
+
+  if (mAnimation && mAnimation->IsRelevant()) {
+    nsNodeUtils::AnimationChanged(mAnimation);
+  }
+
+  mEffectOptions.mIterationComposite = aIterationComposite;
+  RequestRestyle(EffectCompositor::RestyleType::Layer);
 }
 
 CompositeOperation
 KeyframeEffectReadOnly::Composite() const
 {
   return mEffectOptions.mComposite;
+}
+
+void
+KeyframeEffectReadOnly::SetComposite(const CompositeOperation& aComposite)
+{
+  if (mEffectOptions.mComposite == aComposite) {
+    return;
+  }
+
+  mEffectOptions.mComposite = aComposite;
+
+  if (mAnimation && mAnimation->IsRelevant()) {
+    nsNodeUtils::AnimationChanged(mAnimation);
+  }
+
+  if (mTarget) {
+    RefPtr<ComputedStyle> computedStyle = GetTargetComputedStyle();
+    if (computedStyle) {
+      UpdateProperties(computedStyle);
+    }
+  }
 }
 
 void
@@ -858,6 +902,48 @@ KeyframeEffectReadOnly::GetTarget(
     default:
       NS_NOTREACHED("Animation of unsupported pseudo-type");
       aRv.SetNull();
+  }
+}
+
+void
+KeyframeEffectReadOnly::SetTarget(
+  const Nullable<ElementOrCSSPseudoElement>& aTarget)
+{
+  Maybe<OwningAnimationTarget> newTarget = ConvertTarget(aTarget);
+  if (mTarget == newTarget) {
+    // Assign the same target, skip it.
+    return;
+  }
+
+  if (mTarget) {
+    UnregisterTarget();
+    ResetIsRunningOnCompositor();
+
+    RequestRestyle(EffectCompositor::RestyleType::Layer);
+
+    nsAutoAnimationMutationBatch mb(mTarget->mElement->OwnerDoc());
+    if (mAnimation) {
+      nsNodeUtils::AnimationRemoved(mAnimation);
+    }
+  }
+
+  mTarget = newTarget;
+
+  if (mTarget) {
+    UpdateTargetRegistration();
+    RefPtr<ComputedStyle> computedStyle = GetTargetComputedStyle();
+    if (computedStyle) {
+      UpdateProperties(computedStyle);
+    }
+
+    MaybeUpdateFrameForCompositor();
+
+    RequestRestyle(EffectCompositor::RestyleType::Layer);
+
+    nsAutoAnimationMutationBatch mb(mTarget->mElement->OwnerDoc());
+    if (mAnimation) {
+      nsNodeUtils::AnimationAdded(mAnimation);
+    }
   }
 }
 
