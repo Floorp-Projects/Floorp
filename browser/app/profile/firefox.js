a8b5729d89a274be01c8859a2263f2e8185ccf43
Ursula Sarracini <usarracini@mozilla.com>
2018-05-07 15:21:44 -0400

    Bug 1458621 - Move Pocket pref to firefox.js r=dmose
    MozReview-Commit-ID: KDS1wez8erg

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1613,6 +1613,7 @@ pref("browser.migrate.chrome.history.maxAgeInDays", 180);
 pref("dom.mozBrowserFramesEnabled", true);
 
 pref("extensions.pocket.enabled", true);
+pref("extensions.pocket.oAuthConsumerKey", "40249-e88c401e1b1f2242d9e441c4");
 
 pref("signon.schemeUpgrades", true);
 
