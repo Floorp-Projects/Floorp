823a284ecb65e07c0c07422e9d3b172aec722dcc
Tiberius Oros <toros@mozilla.com>
2018-05-07 22:06:54 +0300

    Backed out changeset 8a4ebd5b130d (bug 1458793) for frequently failing browser/base/content/test/performance/browser_tabclose.js on Linux on a CLOSED TREE

--- browser/base/content/test/performance/browser_tabclose.js
+++ browser/base/content/test/performance/browser_tabclose.js
@@ -22,6 +22,8 @@ const EXPECTED_REFLOWS = [
 add_task(async function() {
   await ensureNoPreloadedBrowser();
 
+  let firstTabRect = gBrowser.selectedTab.getBoundingClientRect();
+
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
   await BrowserTestUtils.waitForCondition(() => tab._fullyOpen);
 
@@ -30,7 +32,6 @@ add_task(async function() {
     document.getAnonymousElementByAttribute(gBrowser.tabContainer,
                                             "anonid", "tabs-newtab-button")
             .getBoundingClientRect();
-  let inRange = (val, min, max) => min <= val && val <= max;
 
   // Add a reflow observer and open a new tab.
   await withPerfObserver(async function() {
@@ -65,8 +66,16 @@ add_task(async function() {
              // In tab strip
              r.y1 >= tabStripRect.top && r.y2 <= tabStripRect.bottom &&
              r.x1 >= tabStripRect.left && r.x2 <= tabStripRect.right &&
-             // Width of one tab plus tab separator(s)
-             inRange(r.w - gBrowser.selectedTab.clientWidth, 0, 2)
+             // Width of one tab.
+             r.w == gBrowser.selectedTab.clientWidth
+          },
+          {name: "bug 1446454 - the border between tabs should be painted at" +
+                 " the same time as the tab switch",
+           condition: r =>
+             // In tab strip
+             r.y1 >= tabStripRect.top && r.y2 <= tabStripRect.bottom &&
+             // 1px border, 1px before the end of the first tab.
+             r.w == 1 && r.x1 == firstTabRect.right - 1
           },
           {name: "bug 1446449 - spurious tab switch spinner",
            condition: r =>
