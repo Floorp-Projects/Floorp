e8559eefe1ed5741dbff46a981bf57e55c2b1037
Kris Maglione <maglione.k@gmail.com>
2018-05-08 01:39:03 -0700

    Bug 1459854: Stop eating the console output from talos warm-up runs. r=jmaher
    This makes debugging failures extremely difficult, and tends to confuse
    developers who add debugging logs and can't understand why they're not showing
    up.
    
    MozReview-Commit-ID: Wajt2JczuY

--- testing/talos/talos/ffsetup.py
+++ testing/talos/talos/ffsetup.py
@@ -15,7 +15,6 @@ import mozfile
 import mozinfo
 import mozrunner
 from mozlog import get_proxy_logger
-from mozprocess import ProcessHandlerMixin
 from mozprofile.profile import Profile
 from talos import utils
 from talos.gecko_profile import GeckoProfile
@@ -155,9 +154,7 @@ class FFSetup(object):
         runner = runner_cls(profile=self.profile_dir,
                             binary=self.browser_config["browser_path"],
                             cmdargs=args,
-                            env=self.env,
-                            process_class=ProcessHandlerMixin,
-                            process_args={})
+                            env=self.env)
 
         runner.start(outputTimeout=30)
         proc = runner.process_handler
