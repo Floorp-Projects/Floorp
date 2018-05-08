6933285cebbaad9fe36d3042e40cfb50d21821ef
Jim Chen <nchen@mozilla.com>
2018-05-08 17:38:11 -0400

    Bug 1459501 - Fix race in EventDispatcher queuing; r=esawin
    There's a race in EventDispatcher, where the ready state can change
    during a dispatch. In that case, we can end up neither dispatching an
    event nor queuing it, which effectively drops the event.
    
    MozReview-Commit-ID: GvjSUzjBrsT
    
    --HG--
    extra : rebase_source : bed4e368b5ab1cc7653daea57f20ab25dfa1f125

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/EventDispatcher.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/EventDispatcher.java
@@ -236,23 +236,29 @@ public final class EventDispatcher extends JNIObject {
      */
     public void dispatch(final String type, final GeckoBundle message,
                          final EventCallback callback) {
+        final boolean isGeckoReady;
         synchronized (this) {
-            if (isReadyForDispatchingToGecko() && mAttachedToGecko &&
-                hasGeckoListener(type)) {
+            isGeckoReady = isReadyForDispatchingToGecko();
+            if (isGeckoReady && mAttachedToGecko && hasGeckoListener(type)) {
                 dispatchToGecko(type, message, JavaCallbackDelegate.wrap(callback));
                 return;
             }
         }
 
-        if (!dispatchToThreads(type, message, /* callback */ callback)) {
-            Log.w(LOGTAG, "No listener for " + type);
-        }
+        dispatchToThreads(type, message, callback, isGeckoReady);
     }
 
     @WrapForJNI(calledFrom = "gecko")
     private boolean dispatchToThreads(final String type,
                                       final GeckoBundle message,
                                       final EventCallback callback) {
+        return dispatchToThreads(type, message, callback, /* isGeckoReady */ true);
+    }
+
+    private boolean dispatchToThreads(final String type,
+                                      final GeckoBundle message,
+                                      final EventCallback callback,
+                                      final boolean isGeckoReady) {
         final List<BundleEventListener> geckoListeners;
         synchronized (mGeckoThreadListeners) {
             geckoListeners = mGeckoThreadListeners.get(type);
@@ -289,7 +295,7 @@ public final class EventDispatcher extends JNIObject {
             return true;
         }
 
-        if (!isReadyForDispatchingToGecko()) {
+        if (!isGeckoReady) {
             // Usually, we discard an event if there is no listeners for it by
             // the time of the dispatch. However, if Gecko(View) is not ready and
             // there is no listener for this event that's possibly headed to
@@ -303,6 +309,7 @@ public final class EventDispatcher extends JNIObject {
             return true;
         }
 
+        Log.w(LOGTAG, "No listener for " + type);
         return false;
     }
 
