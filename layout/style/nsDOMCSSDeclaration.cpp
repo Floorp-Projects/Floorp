de550d360c56ffb0654dea05f9ca8932a66907d1
Emilio Cobos Álvarez <emilio@crisal.io>
2018-05-08 06:31:12 +0200

    Bug 1408301: Remove getPropertyCSSValue. r=bz
    It's been removed for a while on Nightly without any known regressions. This
    gives us a full beta cycle of telemetry and two nightly cycles without the API
    before shipping.
    
    This only removes the API, followup work will replace serialization by Servo's,
    and remove the remaining DOM interfaces.
    
    MozReview-Commit-ID: 2m1taYg5xEr

--- layout/style/nsDOMCSSDeclaration.cpp
+++ layout/style/nsDOMCSSDeclaration.cpp
@@ -144,14 +144,6 @@ nsDOMCSSDeclaration::Length()
   return 0;
 }
 
-already_AddRefed<dom::CSSValue>
-nsDOMCSSDeclaration::GetPropertyCSSValue(const nsAString& aPropertyName, ErrorResult& aRv)
-{
-  // We don't support CSSValue yet so we'll just return null...
-
-  return nullptr;
-}
-
 void
 nsDOMCSSDeclaration::IndexedGetter(uint32_t aIndex, bool& aFound, nsAString& aPropName)
 {
