df3a104363d4a23e4caedc4bf3fdd5bfc3711aec
David Major <dmajor@mozilla.com>
2018-05-08 17:37:25 -0400

    Bug 1458270 - Clean up in the shutdown observer. r=milan, a=RyanVM
    --HG--
    extra : source : 74cb61ae46eec86a761dfd8b245dff06bbf14634

--- widget/GfxInfoX11.cpp
+++ widget/GfxInfoX11.cpp
@@ -275,14 +275,18 @@ GfxInfo::GetFeatureStatusImpl(int32_t aFeature,
                               OperatingSystem* aOS /* = nullptr */)
 
 {
-  GetData();
-
   NS_ENSURE_ARG_POINTER(aStatus);
   *aStatus = nsIGfxInfo::FEATURE_STATUS_UNKNOWN;
   aSuggestedDriverVersion.SetIsVoid(true);
   OperatingSystem os = OperatingSystem::Linux;
   if (aOS)
     *aOS = os;
+
+  if (mShutdownOccurred) {
+    return NS_OK;
+  }
+
+  GetData();
 
   if (mGLMajorVersion == 1) {
     // We're on OpenGL 1. In most cases that indicates really old hardware.
