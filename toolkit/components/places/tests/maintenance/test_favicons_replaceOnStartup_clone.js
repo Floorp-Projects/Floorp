9b38afbc29d2cfd85614d54e25ed3e05654fbff1
Marco Bonardo <mbonardo@mozilla.com>
2018-05-09 14:53:59 +0200

    Bug 1432583 - Better corruption handling for favicons.sqlite. r=standard8
    MozReview-Commit-ID: LEotg2dbibB

---
+++ toolkit/components/places/tests/maintenance/test_favicons_replaceOnStartup_clone.js
@@ -0,0 +1,14 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests that history initialization correctly handles a request to forcibly
+// replace the current database.
+
+add_task(async function() {
+  // In reality, this won't try to clone the database, because attached
+  // databases cannot be supported when cloning. This test also verifies that.
+  await test_database_replacement("../migration/favicons_v41.sqlite",
+                                  "favicons.sqlite",
+                                  true,
+                                  PlacesUtils.history.DATABASE_STATUS_CREATE);
+});
