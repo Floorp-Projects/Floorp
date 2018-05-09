4479d80c10c7f2b027fec172d2dfedba7727b39b
Andrew Osmond <aosmond@mozilla.com>
2018-05-09 09:31:07 -0400

    Bug 1453454 - Improve SourceBuffer support for large encoded image data. r=jrmuizel
    Regardless of the size of an encoded image, SourceBuffer::Compact would
    try to consolidate all of the chunks into a single chunk. If an image is
    quite large, it can be actively harmful to do this, because we want a
    very large contiguous chunk of memory for no real reason, and spend
    extra time on the main thread doing the memcpy/consolidation.
    
    Instead we now cap out the chunk size at 20MB. If we start allocating
    chunks of this size, we will not perform compacting when we have
    received all of the data. (Save for realloc'ing the last chunk since it
    probably isn't full.)
    
    On a related note, if we hit an out-of-memory condition in the middle of
    appending data to the SourceBuffer, we would swallow the error. This is
    because nsIInputStream::ReadSegments will succeed if any data was
    written. This leaves the SourceBuffer out of sync. We now propogate this
    error up properly to the higher levels.
    
    fixup

--- image/SourceBuffer.h
+++ image/SourceBuffer.h
@@ -370,6 +370,14 @@ public:
    */
   static const size_t MIN_CHUNK_CAPACITY = 4096;
 
+  /**
+   * The maximum chunk capacity we'll allocate. This was historically the
+   * maximum we would preallocate based on the network size. We may adjust it
+   * in the future based on the IMAGE_DECODE_CHUNKS telemetry to ensure most
+   * images remain in a single chunk.
+   */
+  static const size_t MAX_CHUNK_CAPACITY = 20*1024*1024;
+
 private:
   friend class SourceBufferIterator;
 
@@ -454,7 +462,9 @@ private:
   };
 
   nsresult AppendChunk(Maybe<Chunk>&& aChunk);
-  Maybe<Chunk> CreateChunk(size_t aCapacity, bool aRoundUp = true);
+  Maybe<Chunk> CreateChunk(size_t aCapacity,
+                           size_t aExistingCapacity = 0,
+                           bool aRoundUp = true);
   nsresult Compact();
   static size_t RoundedUpCapacity(size_t aCapacity);
   size_t FibonacciCapacityWithMinimum(size_t aMinCapacity);
@@ -504,6 +514,9 @@ private:
 
   /// Count of active consumers.
   uint32_t mConsumerCount;
+
+  /// True if compacting has been performed.
+  bool mCompacted;
 };
 
 } // namespace image
