e218ab3e3b3fe0ad48dbd65ccc949d98d229060c
David Keeler <dkeeler@mozilla.com>
2018-05-09 10:19:37 -0700

    bug 1460350 - re-land bug 1415991 (remove support for verifying signed unpacked add-ons) r=kmag
    (Backed out changeset 6bbf8dc0b86e (which was a backout of changeset 0a5795108e0a))
    
    MozReview-Commit-ID: EZFn7dLBcdh
    
    --HG--
    extra : rebase_source : 8fac1e33a7f108a248ecde35779b2c63ce7d9172

--- security/manager/ssl/tests/unit/test_signed_dir.js
+++ security/manager/ssl/tests/unit/test_signed_dir.js
@@ -2,9 +2,8 @@
 // http://creativecommons.org/publicdomain/zero/1.0/
 "use strict";
 
-// Tests that signed extensions extracted/unpacked into a directory work pass
-// verification when non-tampered, and fail verification when tampered via
-// various means.
+// Tests that signed extensions extracted/unpacked into a directory do not pass
+// signature verification, because that's no longer supported.
 
 const { ZipUtils } = ChromeUtils.import("resource://gre/modules/ZipUtils.jsm", {});
 
@@ -28,61 +27,14 @@ var gTarget = FileUtils.getDir("TmpD", ["test_signed_dir"]);
 gTarget.createUnique(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
 
 /**
- * Each property below is optional. Defining none of them means "don't tamper".
- *
- * @typedef {TamperInstructions}
- * @type Object
- * @property {String[][]} copy
- *           Format: [[path,newname], [path2,newname2], ...]
- *           Copy the file located at |path| and name it |newname|.
- * @property {String[]} delete
- *           List of paths to files to delete.
- * @property {String[]} corrupt
- *           List of paths to files to corrupt.
- */
-
-/**
  * Extracts the signed XPI into a directory, and tampers the files in that
  * directory if instructed.
  *
- * @param {TamperInstructions} tamper
- *        Instructions on whether to tamper any files, and if so, how.
  * @returns {nsIFile}
  *          The directory where the XPI was extracted to.
  */
-function prepare(tamper) {
+function prepare() {
   ZipUtils.extractFiles(gSignedXPI, gTarget);
-
-  // copy files
-  if (tamper.copy) {
-    tamper.copy.forEach(i => {
-      let f = gTarget.clone();
-      i[0].split("/").forEach(seg => { f.append(seg); });
-      f.copyTo(null, i[1]);
-    });
-  }
-
-  // delete files
-  if (tamper.delete) {
-    tamper.delete.forEach(i => {
-      let f = gTarget.clone();
-      i.split("/").forEach(seg => { f.append(seg); });
-      f.remove(true);
-    });
-  }
-
-  // corrupt files
-  if (tamper.corrupt) {
-    tamper.corrupt.forEach(i => {
-      let f = gTarget.clone();
-      i.split("/").forEach(seg => { f.append(seg); });
-      let s = FileUtils.openFileOutputStream(f, FileUtils.MODE_WRONLY);
-      const str = "Kilroy was here";
-      s.write(str, str.length);
-      s.close();
-    });
-  }
-
   return gTarget;
 }
 
@@ -96,8 +48,8 @@ function checkResult(expectedRv, dir, resolve) {
   };
 }
 
-function verifyDirAsync(expectedRv, tamper) {
-  let targetDir = prepare(tamper);
+function verifyDirAsync(expectedRv) {
+  let targetDir = prepare();
   return new Promise((resolve, reject) => {
     certdb.verifySignedDirectoryAsync(
       Ci.nsIX509CertDB.AddonsPublicRoot, targetDir,
@@ -105,93 +57,8 @@ function verifyDirAsync(expectedRv, tamper) {
   });
 }
 
-//
-// the tests
-//
-add_task(async function testValid() {
-  await verifyDirAsync(Cr.NS_OK, {} /* no tampering */);
-});
-
-add_task(async function testNoMetaDir() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_NOT_SIGNED,
-                       {delete: ["META-INF"]});
-});
-
-add_task(async function testEmptyMetaDir() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_NOT_SIGNED,
-                       {delete: ["META-INF/mozilla.rsa",
-                                 "META-INF/mozilla.sf",
-                                 "META-INF/manifest.mf"]});
-});
-
-add_task(async function testTwoRSAFiles() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {copy: [["META-INF/mozilla.rsa", "extra.rsa"]]});
-});
-
-add_task(async function testCorruptRSAFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {corrupt: ["META-INF/mozilla.rsa"]});
-});
-
-add_task(async function testMissingSFFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {delete: ["META-INF/mozilla.sf"]});
-});
-
-add_task(async function testCorruptSFFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {corrupt: ["META-INF/mozilla.sf"]});
-});
-
-add_task(async function testExtraInvalidSFFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_UNSIGNED_ENTRY,
-                       {copy: [["META-INF/mozilla.rsa", "extra.sf"]]});
-});
-
-add_task(async function testExtraValidSFFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_UNSIGNED_ENTRY,
-                       {copy: [["META-INF/mozilla.sf", "extra.sf"]]});
-});
-
-add_task(async function testMissingManifest() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {delete: ["META-INF/manifest.mf"]});
-});
-
-add_task(async function testCorruptManifest() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MANIFEST_INVALID,
-                       {corrupt: ["META-INF/manifest.mf"]});
-});
-
-add_task(async function testMissingFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_ENTRY_MISSING,
-                       {delete: ["bootstrap.js"]});
-});
-
-add_task(async function testCorruptFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MODIFIED_ENTRY,
-                       {corrupt: ["bootstrap.js"]});
-});
-
-add_task(async function testExtraFile() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_UNSIGNED_ENTRY,
-                       {copy: [["bootstrap.js", "extra"]]});
-});
-
-add_task(async function testMissingFileInDir() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_ENTRY_MISSING,
-                       {delete: ["lib/ui.js"]});
-});
-
-add_task(async function testCorruptFileInDir() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_MODIFIED_ENTRY,
-                       {corrupt: ["lib/ui.js"]});
-});
-
-add_task(async function testExtraFileInDir() {
-  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_UNSIGNED_ENTRY,
-                       {copy: [["lib/ui.js", "extra"]]});
+add_task(async function testAPIFails() {
+  await verifyDirAsync(Cr.NS_ERROR_SIGNED_JAR_NOT_SIGNED);
 });
 
 registerCleanupFunction(function() {
