9023f6cc6d479197eed6a3609c27a62df41cf8eb
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-05-09 09:07:37 +0100

    Bug 962248 - rewrite zoom test to be less terrible, r=dthayer
    MozReview-Commit-ID: ByUM61fsOgh

--- browser/components/customizableui/test/browser_934951_zoom_in_toolbar.js
+++ browser/components/customizableui/test/browser_934951_zoom_in_toolbar.js
@@ -5,17 +5,26 @@
 
 "use strict";
 
-const kTimeoutInMS = 20000;
+let gZoomResetButton;
+
+async function waitForZoom(zoom) {
+  if (parseInt(gZoomResetButton.label) == zoom) {
+    return;
+  }
+  await promiseAttributeMutation(gZoomResetButton, "label", v => {
+    return parseInt(v) == zoom;
+  });
+}
 
 // Bug 934951 - Zoom controls percentage label doesn't update when it's in the toolbar and you navigate.
 add_task(async function() {
   CustomizableUI.addWidgetToArea("zoom-controls", CustomizableUI.AREA_NAVBAR);
+  gZoomResetButton = document.getElementById("zoom-reset-button");
   let tab1 = BrowserTestUtils.addTab(gBrowser, "about:mozilla");
   await BrowserTestUtils.browserLoaded(tab1.linkedBrowser);
   let tab2 = BrowserTestUtils.addTab(gBrowser, "about:robots");
   await BrowserTestUtils.browserLoaded(tab2.linkedBrowser);
   gBrowser.selectedTab = tab1;
-  let zoomResetButton = document.getElementById("zoom-reset-button");
 
   registerCleanupFunction(() => {
     info("Cleaning up.");
@@ -24,68 +33,33 @@ add_task(async function() {
     gBrowser.removeTab(tab1);
   });
 
-  is(parseInt(zoomResetButton.label, 10), 100, "Default zoom is 100% for about:mozilla");
-  let zoomChangePromise = BrowserTestUtils.waitForEvent(window, "FullZoomChange");
+  is(parseInt(gZoomResetButton.label, 10), 100, "Default zoom is 100% for about:mozilla");
   FullZoom.enlarge();
-  await zoomChangePromise;
-  is(parseInt(zoomResetButton.label, 10), 110, "Zoom is changed to 110% for about:mozilla");
+  await waitForZoom(110);
+  is(parseInt(gZoomResetButton.label, 10), 110, "Zoom is changed to 110% for about:mozilla");
 
-  let tabSelectPromise = promiseObserverNotification("browser-fullZoom:location-change");
+  let tabSelectPromise = TestUtils.topicObserved("browser-fullZoom:location-change");
   gBrowser.selectedTab = tab2;
   await tabSelectPromise;
-  await new Promise(resolve => executeSoon(resolve));
-  is(parseInt(zoomResetButton.label, 10), 100, "Default zoom is 100% for about:robots");
+  await waitForZoom(100);
+  is(parseInt(gZoomResetButton.label, 10), 100, "Default zoom is 100% for about:robots");
 
   gBrowser.selectedTab = tab1;
-  let zoomResetPromise = BrowserTestUtils.waitForEvent(window, "FullZoomChange");
+  await waitForZoom(110);
   FullZoom.reset();
-  await zoomResetPromise;
-  is(parseInt(zoomResetButton.label, 10), 100, "Default zoom is 100% for about:mozilla");
+  await waitForZoom(100);
+  is(parseInt(gZoomResetButton.label, 10), 100, "Default zoom is 100% for about:mozilla");
 
   // Test zoom label updates while navigating pages in the same tab.
   FullZoom.enlarge();
-  await zoomChangePromise;
-  is(parseInt(zoomResetButton.label, 10), 110, "Zoom is changed to 110% for about:mozilla");
-  let attributeChangePromise = promiseAttributeMutation(zoomResetButton, "label", (v) => {
-    return parseInt(v, 10) == 100;
-  });
+  await waitForZoom(110);
+  is(parseInt(gZoomResetButton.label, 10), 110, "Zoom is changed to 110% for about:mozilla");
   await promiseTabLoadEvent(tab1, "about:home");
-  await attributeChangePromise;
-  is(parseInt(zoomResetButton.label, 10), 100, "Default zoom is 100% for about:home");
-  await promiseTabHistoryNavigation(-1, function() {
-    return parseInt(zoomResetButton.label, 10) == 110;
-  });
-  is(parseInt(zoomResetButton.label, 10), 110, "Zoom is still 110% for about:mozilla");
+  await waitForZoom(100);
+  is(parseInt(gZoomResetButton.label, 10), 100, "Default zoom is 100% for about:home");
+  gBrowser.selectedBrowser.goBack();
+  await waitForZoom(110);
+  is(parseInt(gZoomResetButton.label, 10), 110, "Zoom is still 110% for about:mozilla");
   FullZoom.reset();
 });
 
-function promiseObserverNotification(aObserver) {
-  return new Promise((resolve, reject) => {
-    function notificationCallback(e) {
-      Services.obs.removeObserver(notificationCallback, aObserver);
-      clearTimeout(timeoutId);
-      resolve();
-    }
-    let timeoutId = setTimeout(() => {
-      Services.obs.removeObserver(notificationCallback, aObserver);
-      reject("Notification '" + aObserver + "' did not happen within 20 seconds.");
-    }, kTimeoutInMS);
-    Services.obs.addObserver(notificationCallback, aObserver);
-  });
-}
-
-function promiseTabSelect() {
-  return new Promise((resolve, reject) => {
-    let container = window.gBrowser.tabContainer;
-    let timeoutId = setTimeout(() => {
-      container.removeEventListener("TabSelect", callback);
-      reject("TabSelect did not happen within 20 seconds");
-    }, kTimeoutInMS);
-    function callback(e) {
-      container.removeEventListener("TabSelect", callback);
-      clearTimeout(timeoutId);
-      executeSoon(resolve);
-    }
-    container.addEventListener("TabSelect", callback);
-  });
-}
