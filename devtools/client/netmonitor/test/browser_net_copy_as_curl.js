2d767e09e0d7de5cb249489746f25b48385a5e72
Bogdan Tara <btara@mozilla.com>
2018-05-09 03:51:45 +0300

    Backed out changeset c4608ac6ced9 (bug 1452442) for ES Lint failure on /builds/worker/checkouts/gecko/devtools/client/netmonitor/test/browser_net_copy_as_curl.js CLOSED TREE

--- devtools/client/netmonitor/test/browser_net_copy_as_curl.js
+++ devtools/client/netmonitor/test/browser_net_copy_as_curl.js
@@ -25,13 +25,8 @@ add_task(async function() {
   }
 
   // Construct the expected command
-  const SIMPLE_BASE = [
-    "curl " + quote(SIMPLE_SJS)
-  ];
-  const SLOW_BASE = [
-    "curl " + quote(SLOW_SJS)
-  ];
   const BASE_RESULT = [
+    "curl " + quote(SIMPLE_SJS),
     "--compressed",
     header("User-Agent: " + navigator.userAgent),
     header("Accept: */*"),
@@ -61,33 +56,11 @@ add_task(async function() {
 
   // GET request, no cookies (first request)
   await performRequest("GET");
-  await testClipboardContent([
-    ...SIMPLE_BASE,
-    ...BASE_RESULT
-  ]);
-  // Check to make sure it is still OK after we view the response (bug#1452442)
-  await selectIndexAndWaitForSourceEditor(monitor, 0);
-  await testClipboardContent([
-    ...SIMPLE_BASE,
-    ...BASE_RESULT
-  ]);
+  await testClipboardContent(BASE_RESULT);
 
   // GET request, cookies set by previous response
   await performRequest("GET");
   await testClipboardContent([
-    ...SIMPLE_BASE,
-    ...BASE_RESULT,
-    ...COOKIE_PARTIAL_RESULT
-  ]);
-
-  // Unfinished request (bug#1378464, bug#1420513)
-  let wait = waitForNetworkEvents(monitor, 0);
-  await ContentTask.spawn(tab.linkedBrowser, SLOW_SJS, async function (url) {
-    content.wrappedJSObject.performRequest(url, "GET", null);
-  });
-  await wait;
-  await testClipboardContent([
-    ...SLOW_BASE,
     ...BASE_RESULT,
     ...COOKIE_PARTIAL_RESULT
   ]);
@@ -95,7 +68,6 @@ add_task(async function() {
   // POST request
   await performRequest("POST", POST_PAYLOAD);
   await testClipboardContent([
-    ...SIMPLE_BASE,
     ...BASE_RESULT,
     ...COOKIE_PARTIAL_RESULT,
     ...POST_PARTIAL_RESULT
@@ -104,7 +76,6 @@ add_task(async function() {
   // HEAD request
   await performRequest("HEAD");
   await testClipboardContent([
-    ...SIMPLE_BASE,
     ...BASE_RESULT,
     ...COOKIE_PARTIAL_RESULT,
     ...HEAD_PARTIAL_RESULT
@@ -132,14 +103,9 @@ add_task(async function() {
     EventUtils.sendMouseEvent({ type: "contextmenu" },
       document.querySelectorAll(".request-list-item")[0]);
 
-    /* Ensure that the copy as cURL option is always visible */
-    let copyUrlParamsNode = monitor.panelWin.parent.document
-      .querySelector("#request-list-context-copy-as-curl");
-    is(!!copyUrlParamsNode, true,
-      "The \"Copy as cURL\" context menu item should not be hidden.");
-
     await waitForClipboardPromise(function setup() {
-      copyUrlParamsNode.click();
+      monitor.panelWin.parent.document
+        .querySelector("#request-list-context-copy-as-curl").click();
     }, function validate(result) {
       if (typeof result !== "string") {
         return false;
@@ -165,6 +131,6 @@ add_task(async function() {
         expectedResult.every(param => actual.includes(param));
     });
 
-    info("Clipboard contains a cURL command for item " + (items.length - 1));
+    info("Clipboard contains a cURL command for the currently selected item's url.");
   }
 });
