ceba72f036543feec40fc05b95d20937839316d0
Philip Jägenstedt <philip@foolip.org>
2018-05-10 18:28:33 +0000

    Bug 1459608 [wpt PR 10870] - Move http/ tests into fetch/ and html/, a=testonly
    Automatic update from web-platform-testsMove http/ tests into fetch/ and html/ (#10870)
    
    As discussed in https://github.com/w3c/web-platform-tests/pull/10839.
    --
    
    wpt-commits: e1fb329f9cddd385706f78ecba642c84c6936c9f
    wpt-pr: 10870
    
    
    --HG--
    rename : testing/web-platform/tests/http/content_length.html => testing/web-platform/tests/fetch/content-length/content-length.html
    rename : testing/web-platform/tests/http/content_length.html.headers => testing/web-platform/tests/fetch/content-length/content-length.html.headers
    rename : testing/web-platform/tests/http/basic-auth-cache-test-ref.html => testing/web-platform/tests/fetch/http-cache/basic-auth-cache-test-ref.html
    rename : testing/web-platform/tests/http/refresh/navigate.window.js => testing/web-platform/tests/html/browsers/browsing-the-web/navigating-across-documents/refresh/navigate.window.js
    rename : testing/web-platform/tests/http/refresh/resources/multiple.asis => testing/web-platform/tests/html/browsers/browsing-the-web/navigating-across-documents/refresh/resources/multiple.asis
    rename : testing/web-platform/tests/http/refresh/resources/refresh.py => testing/web-platform/tests/html/browsers/browsing-the-web/navigating-across-documents/refresh/resources/refresh.py
    rename : testing/web-platform/tests/http/refresh/resources/refreshed.txt => testing/web-platform/tests/html/browsers/browsing-the-web/navigating-across-documents/refresh/resources/refreshed.txt
    rename : testing/web-platform/tests/http/refresh/subresource.any.js => testing/web-platform/tests/html/browsers/browsing-the-web/navigating-across-documents/refresh/subresource.any.js

--- testing/web-platform/tests/http/refresh/resources/refresh.py
+++
@@ -1,4 +0,0 @@
-def main(request, response):
-    response.headers.set("Content-Type", "text/plain")
-    response.headers.set("Refresh", "0;./refreshed.txt?\x80\xFF") # Test byte to Unicode conversion
-    response.content = "Not refreshed.\n"
