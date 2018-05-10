252d614b63390eb95ef75adc3e0e65cdb8f81de6
James Graham <james@hoppipolla.co.uk>
2018-05-10 15:43:30 +0100

    Bug 1265584 - Output asserts to wptreport.json, r=maja_zf
    In the future we want to update metadata using the wptreport.json data, since
    that's much much smaller and easier to parse. In addition, this is
    required to determine if a test fully passed, so it makes sense to
    store it here.
    
    MozReview-Commit-ID: ErYT33BhqGg

--- testing/web-platform/tests/tools/wptrunner/wptrunner/formatters.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/formatters.py
@@ -49,12 +49,16 @@ class WptreportFormatter(BaseFormatter):
     def test_status(self, data):
         subtest = self.create_subtest(data)
         subtest["status"] = data["status"]
+        if "expected" in data:
+            subtest["expected"] = data["expected"]
         if "message" in data:
             subtest["message"] = data["message"]
 
     def test_end(self, data):
         test = self.find_or_create_test(data)
         test["status"] = data["status"]
+        if "expected" in data:
+            test["expected"] = data["expected"]
         if "message" in data:
             test["message"] = data["message"]
 
