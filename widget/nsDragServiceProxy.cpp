8a976cc5e7d06a6a740aa1683bc1874f39584278
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2018-05-11 19:46:15 +0200

    Bug 1460940 - Remove nsIDOMDocument uses in widget/. r=bz
    MozReview-Commit-ID: Rxvwm6zfrB

--- widget/nsDragServiceProxy.cpp
+++ widget/nsDragServiceProxy.cpp
@@ -51,9 +51,8 @@ nsDragServiceProxy::InvokeDragSessionImpl(nsIArray* aArrayTransferables,
                                           nsIScriptableRegion* aRegion,
                                           uint32_t aActionType)
 {
-  nsCOMPtr<nsIDocument> doc = do_QueryInterface(mSourceDocument);
-  NS_ENSURE_STATE(doc->GetDocShell());
-  TabChild* child = TabChild::GetFrom(doc->GetDocShell());
+  NS_ENSURE_STATE(mSourceDocument->GetDocShell());
+  TabChild* child = TabChild::GetFrom(mSourceDocument->GetDocShell());
   NS_ENSURE_STATE(child);
   nsTArray<mozilla::dom::IPCDataTransfer> dataTransfers;
   nsContentUtils::TransferablesToIPCTransferables(aArrayTransferables,
