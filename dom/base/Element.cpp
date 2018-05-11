07907cb49acfe8a23fdc0f023140d1e0a96e3e23
Timothy Guan-tin Chien <timdream@gmail.com>
2018-05-11 12:44:46 -0700

    Bug 1460962 - Support customized built-in element in XUL r=smaug
    This patch enables us to specify a custom element type with |is| attribute
    or property when creating a XUL element. Because non-dashed names are valid
    custom element names in XUL (bug 1446247), other checks has to modified
    accordingly.
    
    The checks I am settling with are
    1) Forbids the custom built-in element names to be a non-dashed name.
    2) Forbids the custom built-in element to extend a dashed built-in element name.
    
    This also ensures the custom built-in element types don't take on the same
    name as the element name it extends.
    
    MozReview-Commit-ID: GCQ9RnfvvrC

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -4278,10 +4278,27 @@ Element::SetCustomElementData(CustomElementData* aData)
   #if DEBUG
     nsAtom* name = NodeInfo()->NameAtom();
     nsAtom* type = aData->GetCustomElementType();
-    if (nsContentUtils::IsCustomElementName(name, NodeInfo()->NamespaceID())) {
-      MOZ_ASSERT(type == name);
-    } else {
-      MOZ_ASSERT(type != name);
+    if (NodeInfo()->NamespaceID() == kNameSpaceID_XHTML) {
+      if (nsContentUtils::IsCustomElementName(name, kNameSpaceID_XHTML)) {
+        MOZ_ASSERT(type == name);
+      } else {
+        MOZ_ASSERT(type != name);
+      }
+    } else { // kNameSpaceID_XUL
+      // Check to see if the tag name is a dashed name.
+      if (nsContentUtils::IsNameWithDash(name)) {
+        // Assert that a tag name with dashes is always an autonomous custom
+        // element.
+        MOZ_ASSERT(type == name);
+      } else {
+        // Could still be an autonomous custom element with a non-dashed tag name.
+        // Need the check below for sure.
+        if (type != name) {
+          // Assert that the name of the built-in custom element type is always
+          // a dashed name.
+          MOZ_ASSERT(nsContentUtils::IsNameWithDash(type));
+        }
+      }
     }
   #endif
   slots->mCustomElementData = aData;
