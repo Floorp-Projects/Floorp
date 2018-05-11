5567a5beadf518134fcaad13bd2d04e28ca50268
Andrew McCreight <continuation@gmail.com>
2018-05-11 11:38:58 -0700

    Bug 1460636 - Don't trace jsids on ObjectGroup in the cycle collector. r=jonco, r=sfink, a=RyanVM
    For some reason, the CC spends a lot of time tracing jsids on
    ObjectGroups when an addon is installed. This patch avoids that by
    adding a canSkipJsids flag to JSTracer, and using it in
    ObjectGroup::traceChildren. If this is true, then the tracer is free
    to not report every jsid. This flag is set to true for the two CC
    tracers.
    
    MozReview-Commit-ID: CWFqQEr0SxV

--- js/public/TracingAPI.h
+++ js/public/TracingAPI.h
@@ -86,6 +86,7 @@ class JS_PUBLIC_API(JSTracer)
     bool isCallbackTracer() const { return tag_ == TracerKindTag::Callback; }
     inline JS::CallbackTracer* asCallbackTracer();
     bool traceWeakEdges() const { return traceWeakEdges_; }
+    bool canSkipJsids() const { return canSkipJsids_; }
 #ifdef DEBUG
     bool checkEdges() { return checkEdges_; }
 #endif
@@ -104,6 +105,7 @@ class JS_PUBLIC_API(JSTracer)
 #endif
       , tag_(tag)
       , traceWeakEdges_(true)
+      , canSkipJsids_(false)
     {}
 
 #ifdef DEBUG
@@ -123,6 +125,7 @@ class JS_PUBLIC_API(JSTracer)
   protected:
     TracerKindTag tag_;
     bool traceWeakEdges_;
+    bool canSkipJsids_;
 };
 
 namespace JS {
@@ -252,6 +255,12 @@ class JS_PUBLIC_API(CallbackTracer) : public JSTracer
   protected:
     void setTraceWeakEdges(bool value) {
         traceWeakEdges_ = value;
+    }
+
+    // If this is set to false, then the tracer will skip some jsids
+    // to improve performance. This is needed for the cycle collector.
+    void setCanSkipJsids(bool value) {
+        canSkipJsids_ = value;
     }
 
   private:
