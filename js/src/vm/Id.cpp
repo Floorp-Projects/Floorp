d0d1350545e1a3ac6c9d4d0d3966bb3eb03f0d25
Jan de Mooij <jdemooij@mozilla.com>
2018-05-11 12:01:32 +0200

    Bug 1460341 - Give jsid a constructor that initializes it to a void id. r=jonco,bz

--- js/src/vm/Id.cpp
+++ js/src/vm/Id.cpp
@@ -7,10 +7,9 @@
 #include "js/Id.h"
 #include "js/RootingAPI.h"
 
-const jsid JSID_EMPTY = { size_t(JSID_TYPE_SYMBOL) };
+const jsid JSID_EMPTY = jsid::fromRawBits(size_t(JSID_TYPE_SYMBOL));
 
 static const jsid voidIdValue = JSID_VOID;
 static const jsid emptyIdValue = JSID_EMPTY;
 const JS::HandleId JSID_VOIDHANDLE = JS::HandleId::fromMarkedLocation(&voidIdValue);
 const JS::HandleId JSID_EMPTYHANDLE = JS::HandleId::fromMarkedLocation(&emptyIdValue);
-
