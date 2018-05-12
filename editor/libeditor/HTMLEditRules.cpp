5983a789e133c211a68664fc9bb12e247c74b651
Masayuki Nakano <masayuki@d-toybox.com>
2018-05-12 11:25:23 +0900

    Bug 1460509 - part 20: Make HTMLEditRules::MakeSureElemStartsAndEndsOnCR() and HTMLEditRules::MakeSureElemStartsOrEndsOnCR() return NS_ERROR_EDITOR_DESTROYED if they cause destroying the editor r=m_kato
    MozReview-Commit-ID: I8HcvO9IB7x
    
    --HG--
    extra : rebase_source : 6ca2e3a58b84a150504c73a17bda6cc825a10176

--- editor/libeditor/HTMLEditRules.cpp
+++ editor/libeditor/HTMLEditRules.cpp
@@ -9802,9 +9802,6 @@ HTMLEditRules::RemoveAlignment(nsINode& aNode,
   return NS_OK;
 }
 
-// Let's insert a BR as first (resp. last) child of aNode if its
-// first (resp. last) child is not a block nor a BR, and if the
-// previous (resp. next) sibling is not a block nor a BR
 nsresult
 HTMLEditRules::MakeSureElemStartsOrEndsOnCR(nsINode& aNode,
                                             bool aStarts)
@@ -9842,6 +9839,9 @@ HTMLEditRules::MakeSureElemStartsOrEndsOnCR(nsINode& aNode,
     RefPtr<Element> brElement =
       HTMLEditorRef().InsertBrElementWithTransaction(SelectionRef(),
                                                      pointToInsert);
+    if (NS_WARN_IF(!CanHandleEditAction())) {
+      return NS_ERROR_EDITOR_DESTROYED;
+    }
     if (NS_WARN_IF(!brElement)) {
       return NS_ERROR_FAILURE;
     }
