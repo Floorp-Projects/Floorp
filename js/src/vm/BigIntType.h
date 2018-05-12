9684972b62008e0224769ceb062910fa68cd56df
Robin Templeton <robin@igalia.com>
2018-05-11 19:42:48 -0700

    bug 1366287 - Part 2.0: Use GMP integers to represent BigInt values.  r=jwalden
    Also link to libgmp and initialize it with custom memory allocation
    functions.

--- js/src/vm/BigIntType.h
+++ js/src/vm/BigIntType.h
@@ -7,6 +7,8 @@
 #ifndef vm_BigIntType_h
 #define vm_BigIntType_h
 
+#include <gmp.h>
+
 #include "gc/Barrier.h"
 #include "gc/GC.h"
 #include "gc/Heap.h"
@@ -23,7 +25,10 @@ class BigInt final : public js::gc::TenuredCell
   private:
     // The minimum allocation size is currently 16 bytes (see
     // SortedArenaList in gc/ArenaList.h).
-    uint8_t unused_[js::gc::MinCellSize];
+    union {
+        mpz_t num_;
+        uint8_t unused_[js::gc::MinCellSize];
+    };
 
   public:
     // Allocate and initialize a BigInt value
@@ -41,6 +46,8 @@ class BigInt final : public js::gc::TenuredCell
 
     static JSLinearString* toString(JSContext* cx, BigInt* x);
     bool toBoolean();
+
+    static void init();
 
     static BigInt* copy(JSContext* cx, Handle<BigInt*> x);
 };
