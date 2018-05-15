1920a7a1400bf1256d243f110d3c75948b58b8c6
prathiksha <prathikshaprasadsuman@gmail.com>
2018-05-15 11:08:19 -0700

    Bug 1461455 - Show the total header in FTU wizard pages. r=MattN
    MozReview-Commit-ID: 3EMT3nZ2sYe
    
    --HG--
    extra : rebase_source : b79438bc92935d23577b2d63cc7923950fc6761a

--- browser/components/payments/res/containers/address-picker.js
+++ browser/components/payments/res/containers/address-picker.js
@@ -22,10 +22,12 @@ export default class AddressPicker extends PaymentStateSubscriberMixin(HTMLEleme
     this.dropdown = new RichSelect();
     this.dropdown.addEventListener("change", this);
     this.addLink = document.createElement("a");
+    this.addLink.className = "add-link";
     this.addLink.href = "javascript:void(0)";
     this.addLink.textContent = this.dataset.addLinkLabel;
     this.addLink.addEventListener("click", this);
     this.editLink = document.createElement("a");
+    this.editLink.className = "edit-link";
     this.editLink.href = "javascript:void(0)";
     this.editLink.textContent = this.dataset.editLinkLabel;
     this.editLink.addEventListener("click", this);
