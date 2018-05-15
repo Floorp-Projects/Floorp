7e4a66f58c6a34ea27ee8894640e126595af3581
Masayuki Nakano <masayuki@d-toybox.com>
2018-05-15 17:04:28 +0900

    Bug 1460509 - part 42: Make HTMLEditRules::BustUpInlinesAtBRs() return NS_ERROR_EDITOR_DESTROYED if it causes destroying the editor r=m_kato
    MozReview-Commit-ID: 5KGPE8mPqge
    
    --HG--
    extra : rebase_source : 9de693ba99508ebc5b5c235cbc2ed589676fc045

--- editor/libeditor/HTMLEditRules.h
+++ editor/libeditor/HTMLEditRules.h
@@ -504,9 +504,22 @@ protected:
              TouchContent aTouchContent = TouchContent::yes);
   void LookInsideDivBQandList(nsTArray<OwningNonNull<nsINode>>& aNodeArray);
   nsresult BustUpInlinesAtRangeEndpoints(RangeItem& inRange);
-  nsresult BustUpInlinesAtBRs(
-             nsIContent& aNode,
-             nsTArray<OwningNonNull<nsINode>>& aOutArrayOfNodes);
+
+  /**
+   * BustUpInlinesAtBRs() splits before all <br> elements in aNode.  All <br>
+   * nodes will be moved before right node at splitting its parent.  Finally,
+   * this returns all <br> elements, every left node and aNode with
+   * aOutArrayNodes.
+   *
+   * @param aNode               An inline container element.
+   * @param aOutArrayOfNodes    All found <br> elements, left nodes (may not
+   *                            be set if <br> is at start edge of aNode) and
+   *                            aNode itself.
+   */
+  MOZ_MUST_USE nsresult
+  BustUpInlinesAtBRs(nsIContent& aNode,
+                     nsTArray<OwningNonNull<nsINode>>& aOutArrayOfNodes);
+
   /**
    * GetHiestInlineParent() returns the highest inline node parent between
    * aNode and the editing host.  Even if the editing host is an inline
