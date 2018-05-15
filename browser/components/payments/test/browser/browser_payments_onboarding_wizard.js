d6a1fb4efcaf1d26d06898a7d45178e7ac97c6fa
prathiksha <prathikshaprasadsuman@gmail.com>
2018-05-15 11:08:19 -0700

    Bug 1461455 - Show the total header in FTU wizard pages. r=MattN
    MozReview-Commit-ID: 3EMT3nZ2sYe

--- browser/components/payments/test/browser/browser_payments_onboarding_wizard.js
+++ browser/components/payments/test/browser/browser_payments_onboarding_wizard.js
@@ -30,6 +30,11 @@ add_task(async function test_onboarding_wizard_without_saved_addresses() {
       }, "Address page is shown first if there are no saved addresses");
     });
 
+    info("Check if the total header is visible on the address page during on-boarding");
+    let isHeaderVisible =
+      spawnPaymentDialogTask(frame, PTU.DialogContentTasks.isElementVisible, "header");
+    ok(isHeaderVisible, "Total Header is visible on the address page during on-boarding");
+
     info("Closing the payment dialog");
     spawnPaymentDialogTask(frame, async function() {
       content.document.getElementById("address-page-cancel-button").click();
@@ -50,6 +55,7 @@ add_task(async function test_onboarding_wizard_with_saved_addresses() {
       await setupPaymentDialog(browser, {
         methodData: [PTU.MethodData.basicCard],
         details: PTU.Details.total60USD,
+        options: PTU.Options.requestShippingOption,
         merchantTaskFn: PTU.ContentTasks.createAndShowRequest,
       });
 
@@ -67,9 +73,61 @@ add_task(async function test_onboarding_wizard_with_saved_addresses() {
       }, "Payment summary page is shown when there are saved addresses");
     });
 
+    info("Check if the total header is visible on payments summary page");
+    let isHeaderVisible =
+      spawnPaymentDialogTask(frame, PTU.DialogContentTasks.isElementVisible, "header");
+    ok(isHeaderVisible, "Total Header is visible on the payment summary page");
+
+    // Click on the Add/Edit buttons in the payment summary page to check if
+    // the total header is visible on the address page and the basic card page.
+    let buttons = ["address-picker[selected-state-key='selectedShippingAddress'] .add-link",
+      "address-picker[selected-state-key='selectedShippingAddress'] .edit-link",
+      "payment-method-picker .add-link",
+      "payment-method-picker .edit-link"];
+    for (let button of buttons) {
+      // eslint-disable-next-line no-shadow
+      await spawnPaymentDialogTask(frame, async function(button) {
+        let {
+          PaymentTestUtils: PTU,
+        } = ChromeUtils.import("resource://testing-common/PaymentTestUtils.jsm", {});
+
+        content.document.querySelector(button).click();
+
+        if (button.startsWith("address")) {
+          await ContentTaskUtils.waitForCondition(() =>
+            content.document.querySelector("address-form .back-button"),
+                                                  "Address page is rendered");
+          await PTU.DialogContentUtils.waitForState(content, (state) => {
+            return state.page.id == "address-page";
+          }, "Address page is shown");
+        } else {
+          await ContentTaskUtils.waitForCondition(() =>
+            content.document.querySelector("basic-card-form .back-button"),
+                                                  "Basic card page is rendered");
+          await PTU.DialogContentUtils.waitForState(content, (state) => {
+            return state.page.id == "basic-card-page";
+          }, "Basic card page is shown");
+        }
+      }, button);
+
+      isHeaderVisible =
+        await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.isElementVisible, "header");
+      ok(!isHeaderVisible, "Total Header is hidden on the address page");
+
+      if (button.startsWith("address")) {
+        spawnPaymentDialogTask(frame, async function() {
+          content.document.querySelector("address-form .back-button").click();
+        });
+      } else {
+        spawnPaymentDialogTask(frame, async function() {
+          content.document.querySelector("basic-card-form .back-button").click();
+        });
+      }
+    }
+
     info("Closing the payment dialog");
     spawnPaymentDialogTask(frame, async function() {
-      content.document.getElementById("address-page-cancel-button").click();
+      content.document.getElementById("cancel").click();
     });
     await BrowserTestUtils.waitForCondition(() => win.closed, "dialog should be closed");
 
