4599da58e96a9259ed5ef4d3a27aad741a9798b7
Raymond FOrbes <rforbes@mozilla.com>
2018-05-15 11:46:04 -0700

    Bug 1460781 - Add non debug build coverage build to taskcluster r=marco
    MozReview-Commit-ID: H3AjJ20uspC
    ***
    Bug 1460781 - update platform for opt build of coverage
    
    MozReview-Commit-ID: DwX2D8KryNb
    ***
    Bug 1460781 - fix type in buildbase
    
    MozReview-Commit-ID: 2PsW54jtNU8
    ***
    Bug 1460781 - incorporate changes requested from review
    
    MozReview-Commit-ID: 5hm6HAncogh

---
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_code_coverage_debug.py
@@ -0,0 +1,28 @@
+import os
+
+config = {
+    'default_actions': [
+        'clobber',
+        'build',
+        'check-test',
+        'update',  # decided by query_is_nightly()
+    ],
+    'stage_platform': 'linux64-ccov',
+    #### 64 bit build specific #####
+    'env': {
+        'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
+        'DISPLAY': ':2',
+        'HG_SHARE_BASE_DIR': '/builds/hg-shared',
+        'MOZ_OBJDIR': '%(abs_obj_dir)s',
+        'TINDERBOX_OUTPUT': '1',
+        'TOOLTOOL_CACHE': '/builds/tooltool_cache',
+        'TOOLTOOL_HOME': '/builds',
+        'MOZ_CRASHREPORTER_NO_REPORT': '1',
+        'LC_ALL': 'C',
+        ## 64 bit specific
+        'PATH': '/usr/local/bin:/bin:\
+/usr/bin:/usr/local/sbin:/usr/sbin:/sbin',
+    },
+    'mozconfig_variant': 'code-coverage-debug',
+    #######################
+}
