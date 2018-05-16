0b28215c42061f61e07912152ee92244c36fac7d
Mark Banner <standard8@mozilla.com>
2018-05-16 14:39:29 +0100

    Bug 1461966 - Enable ESLint rule mozilla/use-services for extensions/pref. r=mkaply
    MozReview-Commit-ID: 77pSaX70zdJ

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,15 +39,6 @@ module.exports = {
     "env": {
       "mozilla/browser-window": true
     }
-  },{
-    // XXX Bug 1421969. These files/directories are still being fixed,
-    // so turn off mozilla/use-services for them for now.
-    "files": [
-      "extensions/pref/**",
-    ],
-    "rules": {
-      "mozilla/use-services": "off",
-    }
   }, {
     // XXX Bug 1436303. These directories are still being fixed, so turn off
     // mozilla/no-cc-etc for now.
