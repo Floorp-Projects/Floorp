f4677edd9ca37571264d0148ed1fc48705bc5a78
Brindusan Cristian <cbrindusan@mozilla.com>
2018-05-17 19:48:34 +0300

    Backed out changeset 35089098baf8 (bug 1447338) for mochitest chrome failures on test_perf-settings-features.html CLOSED TREE

--- devtools/client/performance-new/store/reducers.js
+++ devtools/client/performance-new/store/reducers.js
@@ -88,7 +88,7 @@ function entries(state = 10000000, action) {
  * The features that are enabled for the profiler.
  * @param {array} state
  */
-function features(state = ["js", "stackwalk", "responsiveness"], action) {
+function features(state = ["js", "stackwalk"], action) {
   switch (action.type) {
     case "CHANGE_FEATURES":
       return action.features;
