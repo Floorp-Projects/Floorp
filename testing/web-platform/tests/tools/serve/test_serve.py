19633055bf568f9514a0f3ed34a89f1e760b6c4b
Mike Pennisi <mike@mikepennisi.com>
2018-05-17 14:08:44 +0000

    Bug 1459723 [wpt PR 10878] - Improve definition of non-existent origin, a=testonly
    Automatic update from web-platform-tests[wptserve] Correct domain configuration on *nix
    
    Because Windows and UNIX-like systems differ in how they interpret the
    IP address `0.0.0.0`, the hosts file contents generated for use with the
    WPT server must take the platform into account.
    
    Extend the hosts file generation logic and introduce "infrastructure
    assumptions" tests to assert the inavailibility of the domain.
    
    --
    [infra] Rename nonexistent domain
    
    Contributors have expressed confusion over the term "nonexistent origin"
    to describe a domain that is not intended to exist. Rename the subdomain
    to "nonexistent" to clarify its role.
    
    --
    
    wpt-commits: 136154864a88788cb58d6ff079e6d030cbe8c60f, 927cd2216d34dc2f9824a6899476f26b6b11dd31
    wpt-pr: 10878

--- testing/web-platform/tests/tools/serve/test_serve.py
+++ testing/web-platform/tests/tools/serve/test_serve.py
@@ -1,4 +1,5 @@
 import pickle
+import platform
 import os
 
 import pytest
@@ -8,13 +9,36 @@ from . import serve
 from .serve import Config
 
 
-def test_make_hosts_file():
+@pytest.mark.skipif(platform.uname()[0] == "Windows",
+                    reason="Expected contents are platform-dependent")
+def test_make_hosts_file_nix():
     c = Config(browser_host="foo.bar", alternate_hosts={"alt": "foo2.bar"})
     hosts = serve.make_hosts_file(c, "192.168.42.42")
     lines = hosts.split("\n")
     assert set(lines) == {"",
-                          "0.0.0.0\tnonexistent-origin.foo.bar",
-                          "0.0.0.0\tnonexistent-origin.foo2.bar",
+                          "192.168.42.42\tfoo.bar",
+                          "192.168.42.42\tfoo2.bar",
+                          "192.168.42.42\twww.foo.bar",
+                          "192.168.42.42\twww.foo2.bar",
+                          "192.168.42.42\twww1.foo.bar",
+                          "192.168.42.42\twww1.foo2.bar",
+                          "192.168.42.42\twww2.foo.bar",
+                          "192.168.42.42\twww2.foo2.bar",
+                          "192.168.42.42\txn--lve-6lad.foo.bar",
+                          "192.168.42.42\txn--lve-6lad.foo2.bar",
+                          "192.168.42.42\txn--n8j6ds53lwwkrqhv28a.foo.bar",
+                          "192.168.42.42\txn--n8j6ds53lwwkrqhv28a.foo2.bar"}
+    assert lines[-1] == ""
+
+@pytest.mark.skipif(platform.uname()[0] != "Windows",
+                    reason="Expected contents are platform-dependent")
+def test_make_hosts_file_windows():
+    c = Config(browser_host="foo.bar", alternate_hosts={"alt": "foo2.bar"})
+    hosts = serve.make_hosts_file(c, "192.168.42.42")
+    lines = hosts.split("\n")
+    assert set(lines) == {"",
+                          "0.0.0.0\tnonexistent.foo.bar",
+                          "0.0.0.0\tnonexistent.foo2.bar",
                           "192.168.42.42\tfoo.bar",
                           "192.168.42.42\tfoo2.bar",
                           "192.168.42.42\twww.foo.bar",
