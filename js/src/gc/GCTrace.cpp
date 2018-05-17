89af68b5af49ce69eb263db4959940197c0d5ff6
Paul Bone <pbone@mozilla.com>
2018-05-17 20:57:06 +1000

    Bug 1460098 - Fix undefined symbol in NoOpt builds on a CLOSED TREE r=bustage
    Ensure that gcTracer is defined even when JS_GC_TRACE is not.

--- js/src/gc/GCTrace.cpp
+++ js/src/gc/GCTrace.cpp
@@ -4,8 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifdef JS_GC_TRACE
-
 #include "gc/GCTrace.h"
 
 #include <stdio.h>
@@ -23,6 +21,8 @@ namespace js {
 namespace gc {
 
 GCTrace gcTracer;
+
+#ifdef JS_GC_TRACE
 
 JS_STATIC_ASSERT(NumAllocKinds == unsigned(AllocKind::LIMIT));
 JS_STATIC_ASSERT(LastObjectAllocKind == unsigned(AllocKind::OBJECT_LAST));
@@ -254,7 +254,7 @@ GCTrace::traceMajorGCEnd()
     TraceEvent(gcTraceFile, TraceEventMajorGCEnd);
 }
 
+#endif
+
 } // js
 } // gc
-
-#endif
