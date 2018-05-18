574e1467e0af524f9ef437f72ea8b7b963b013d8
Michael Kaply <mozilla@kaply.com>
2018-05-18 16:12:32 -0500

    Bug 1461345 - Move browser.search.order to list.json. r=adw
    MozReview-Commit-ID: 4eaWb8mzIm6
    
    --HG--
    extra : rebase_source : 83f01cadfca9ec44d52ba7f737ab9d23f004f044

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -2740,6 +2740,7 @@ SearchService.prototype = {
   __sortedEngines: null,
   _visibleDefaultEngines: [],
   _searchDefault: null,
+  _searchOrder: null,
   get _sortedEngines() {
     if (!this.__sortedEngines)
       return this._buildSortedEngineList();
@@ -2983,6 +2984,7 @@ SearchService.prototype = {
         this._currentEngine = null;
         this._visibleDefaultEngines = [];
         this._searchDefault = null;
+        this._searchOrder = null;
         this._metaData = {};
         this._cacheFileJSON = null;
 
@@ -3499,6 +3501,13 @@ SearchService.prototype = {
     } else {
       this._searchDefault = searchSettings.default.searchDefault;
     }
+
+    if (searchRegion && searchRegion in searchSettings &&
+        "searchOrder" in searchSettings[searchRegion]) {
+      this._searchOrder = searchSettings[searchRegion].searchOrder;
+    } else {
+      this._searchOrder = searchSettings.default.searchOrder;
+    }
   },
 
   _parseListTxt: function SRCH_SVC_parseListTxt(list, uris) {
@@ -3616,6 +3625,12 @@ SearchService.prototype = {
       var engineName;
       var prefName;
 
+      // The original default engine should always be first in the list
+      if (this.originalDefaultEngine) {
+        this.__sortedEngines.push(this.originalDefaultEngine);
+        addedEngines[this.originalDefaultEngine.name] = this.originalDefaultEngine;
+      }
+
       try {
         var extras =
           Services.prefs.getChildList(BROWSER_SEARCH_PREF + "order.extra.");
@@ -3645,6 +3660,17 @@ SearchService.prototype = {
 
         this.__sortedEngines.push(engine);
         addedEngines[engine.name] = engine;
+      }
+
+      if (this._searchOrder) {
+        for (let engineName of this._searchOrder) {
+          engine = this._engines[engineName];
+          if (!engine || engine.name in addedEngines)
+            continue;
+
+          this.__sortedEngines.push(engine);
+          addedEngines[engine.name] = engine;
+        }
       }
     }
 
@@ -3779,6 +3805,11 @@ SearchService.prototype = {
 
       if (!(engineName in engineOrder))
         engineOrder[engineName] = i++;
+    }
+
+    // Now look at list.json
+    for (let engineName of this._searchOrder) {
+      engineOrder[engineName] = i++;
     }
 
     LOG("getDefaultEngines: engineOrder: " + engineOrder.toSource());
@@ -4176,6 +4207,13 @@ SearchService.prototype = {
           let engineName = getLocalizedPref(prefName);
           if (!engineName)
             break;
+          if (result.name == engineName) {
+            sendSubmissionURL = true;
+            break;
+          }
+        }
+
+        for (let engineName of this._searchOrder) {
           if (result.name == engineName) {
             sendSubmissionURL = true;
             break;
