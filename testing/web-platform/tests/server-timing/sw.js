20254748e9259dfe2ea01f5b8df2b4148ee10299
Yoav Weiss <yoav@yoav.ws>
2018-05-19 20:20:34 +0000

    Bug 1462609 [wpt PR 11013] - Server-Timing: clean up service-worker test, a=testonly
    Automatic update from web-platform-testsMerge pull request #11013 from cvazac/master
    
    Server-Timing: clean up service-worker test
    --
    
    wpt-commits: 5478850d0df6d1d0f2e8c10f77f62d3c6e2dd589
    wpt-pr: 11013

--- testing/web-platform/tests/server-timing/sw.js
+++ testing/web-platform/tests/server-timing/sw.js
@@ -1,10 +1,19 @@
 importScripts('/resources/testharness.js')
 
-promise_test(async (test) => {
-  return fetch('./sw.js').then(function(response) {
-    assert_not_equals(typeof performance.getEntriesByName(response.url)[0].serverTiming,
-      'undefined',
-      'An instance of `PerformanceResourceTiming` should have a `serverTiming` attribute in the Service Worker context.')
-    done()
+promise_test((test) => {
+  return fetch('./sw.js').then((response) => {
+    return new Promise((resolve, reject) => {
+      step_timeout(() => {
+        const entry = performance.getEntriesByName(response.url)[0]
+        if (!entry) {
+          reject('no entry: ' + response.url)
+        }
+
+        assert_not_equals(typeof entry.serverTiming,
+          'undefined',
+          'An instance of `PerformanceResourceTiming` should have a `serverTiming` attribute in the Service Worker context.')
+        resolve()
+      }, 250)
+    })
   })
 })
