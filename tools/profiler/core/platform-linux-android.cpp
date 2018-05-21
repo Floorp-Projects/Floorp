312df2b110ccc0a2c148f4b9a004ce7f41313ef2
Mike Hommey <mh+mozilla@glandium.org>
2018-05-21 12:04:22 +0900

    Bug 1463035 - Remove MOZ_SIGNAL_TRAMPOLINE. r=jchen
    For some reason, GNU as is not happy with the assembly generated after
    bug 1238661 anymore on Debian armel.
    
    OTOH, as mentioned in bug 1238661 comment 4, we actually don't need this
    workaround anymore, so let's just kill it.

--- tools/profiler/core/platform-linux-android.cpp
+++ tools/profiler/core/platform-linux-android.cpp
@@ -60,7 +60,6 @@
 #include <stdarg.h>
 
 #include "prenv.h"
-#include "mozilla/LinuxSignal.h"
 #include "mozilla/PodOperations.h"
 #include "mozilla/DebugOnly.h"
 
@@ -277,7 +276,7 @@ Sampler::Sampler(PSLockRef aLock)
 
   // Request profiling signals.
   struct sigaction sa;
-  sa.sa_sigaction = MOZ_SIGNAL_TRAMPOLINE(SigprofHandler);
+  sa.sa_sigaction = SigprofHandler;
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = SA_RESTART | SA_SIGINFO;
   if (sigaction(SIGPROF, &sa, &mOldSigprofHandler) != 0) {
