22377b52cc604721c1fa5de96b94f06914e64a99
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-05-21 12:02:12 +0200

    Bug 1463057 - Fix expanding object in console.error message. r=Honza, a=RyanVM
    There was a race between the fetching of the object properties and the
    expansion of the stacktrace of the console.error message. In the end, the
    message was re-rendered, without the object being expanded.
    To prevent such thing, we stop the propagation of the click on the Tree
    node. Here the fix is done directly in the Reps bundle so it can be applied
    on beta, without unrelated code changes we'd have if doing a full bundle release.
    The fix will be backported to the Tree components so it's included in the
    bundle on the next reps release.
    A test is added to make sure we don't regress this.
    
    MozReview-Commit-ID: Di8YxJ7P1Wl

---
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_console_error_expand_object.js
@@ -0,0 +1,27 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check console.error calls with expandable object.
+const TEST_URI = "data:text/html;charset=utf8,<h1>test console.error with objects</h1>";
+
+add_task(async function() {
+  let toolbox = await openNewTabAndToolbox(TEST_URI, "webconsole");
+  let hud = toolbox.getCurrentPanel().hud;
+
+  const onMessagesLogged = waitForMessage(hud, "myError");
+  ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+    content.wrappedJSObject.console.error("myError", {a: "a", b: "b"});
+  });
+  const {node} = await onMessagesLogged;
+
+  let objectInspectors = [...node.querySelectorAll(".tree")];
+  is(objectInspectors.length, 1, "There is the expected number of object inspectors");
+  const [oi] = objectInspectors;
+  oi.querySelector(".node .arrow").click();
+  await waitFor(() => oi.querySelectorAll(".node").length > 1);
+  ok(true, "The object can be expanded");
+});
