027367efb8f6a529f18506d39263979da2264035
James Graham <james@hoppipolla.co.uk>
2018-05-21 11:37:35 +0100

    Bug 1354232 - Add support for LSAN to mozlog, r=ahal, mccr8
    This adds two new actions to mozlog, one for reporting an LSAN
    failure, and one for reporting the summary.
    
    MozReview-Commit-ID: D7ep27SrI1n

--- testing/mozbase/mozleak/mozleak/lsan.py
+++ testing/mozbase/mozleak/mozleak/lsan.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
 import re
 
 
@@ -5,19 +10,22 @@ class LSANLeaks(object):
 
     """
     Parses the log when running an LSAN build, looking for interesting stack frames
-    in allocation stacks, and prints out reports.
+    in allocation stacks
     """
 
-    def __init__(self, logger):
+    def __init__(self, logger, scope=None, allowed=None):
         self.logger = logger
         self.inReport = False
         self.fatalError = False
         self.symbolizerError = False
-        self.foundFrames = set([])
+        self.foundFrames = set()
         self.recordMoreFrames = None
         self.currStack = None
-        self.allowedMatch = None
         self.maxNumRecordedFrames = 4
+        self.summaryData = None
+        self.scope = scope
+        self.allowedMatch = None
+        self.sawError = False
 
         # Don't various allocation-related stack frames, as they do not help much to
         # distinguish different leaks.
@@ -40,36 +48,53 @@ class LSANLeaks(object):
         self.stackFrameRegExp = re.compile("    #\d+ 0x[0-9a-f]+ in ([^(</]+)")
         self.sysLibStackFrameRegExp = re.compile(
             "    #\d+ 0x[0-9a-f]+ \(([^+]+)\+0x[0-9a-f]+\)")
+        self.summaryRegexp = re.compile(
+            "SUMMARY: AddressSanitizer: (\d+) byte\(s\) leaked in (\d+) allocation\(s\).")
+        self.setAllowed(allowed)
+
+    def setAllowed(self, allowedLines):
+        if not allowedLines:
+            self.allowedRegexp = None
+        else:
+            self.allowedRegexp = re.compile(
+                "^" + "|".join([re.escape(f) for f in allowedLines]))
 
     def log(self, line):
         if re.match(self.startRegExp, line):
             self.inReport = True
-            return
+            # Downgrade this from an ERROR
+            self.sawError = True
+            return "LeakSanitizer: detected memory leaks"
 
         if re.match(self.fatalErrorRegExp, line):
             self.fatalError = True
-            return
+            return line
 
         if re.match(self.symbolizerOomRegExp, line):
             self.symbolizerError = True
-            return
+            return line
 
         if not self.inReport:
-            return
+            return line
 
         if line.startswith("Direct leak") or line.startswith("Indirect leak"):
             self._finishStack()
             self.recordMoreFrames = True
             self.currStack = []
-            return
+            return line
 
-        if line.startswith("SUMMARY: AddressSanitizer"):
+        summaryData = self.summaryRegexp.match(line)
+        if summaryData:
+            assert self.summaryData is None
             self._finishStack()
             self.inReport = False
+            self.summaryData = (int(item) for item in summaryData.groups())
+            # We don't return the line here because we want to control whether the
+            # leak is seen as an expected failure later
             return
 
         if not self.recordMoreFrames:
-            return
+            return line
 
         stackFrame = re.match(self.stackFrameRegExp, line)
         if stackFrame:
@@ -77,7 +102,7 @@ class LSANLeaks(object):
             frame = stackFrame.group(1).split()[-1]
             if not re.match(self.skipListRegExp, frame):
                 self._recordFrame(frame)
-            return
+            return line
 
         sysLibStackFrame = re.match(self.sysLibStackFrameRegExp, line)
         if sysLibStackFrame:
@@ -87,45 +112,59 @@ class LSANLeaks(object):
 
         # If we don't match either of these, just ignore the frame.
         # We'll end up with "unknown stack" if everything is ignored.
+        return line
 
     def process(self):
         failures = 0
 
+        if self.summaryData:
+            allowed = all(allowed for _, allowed in self.foundFrames)
+            self.logger.lsan_summary(*self.summaryData, allowed=allowed)
+            self.summaryData = None
+
         if self.fatalError:
-            self.logger.error("TEST-UNEXPECTED-FAIL | LeakSanitizer | LeakSanitizer "
-                              "has encountered a fatal error.")
+            self.logger.error("LeakSanitizer | LeakSanitizer has encountered a fatal error.")
             failures += 1
 
         if self.symbolizerError:
-            self.logger.error("TEST-UNEXPECTED-FAIL | LeakSanitizer | LLVMSymbolizer "
-                              "was unable to allocate memory.")
+            self.logger.error("LeakSanitizer | LLVMSymbolizer was unable to allocate memory.\n"
+                              "This will cause leaks that "
+                              "should be ignored to instead be reported as an error")
             failures += 1
-            self.logger.info("TEST-INFO | LeakSanitizer | This will cause leaks that "
-                             "should be ignored to instead be reported as an error")
 
         if self.foundFrames:
-            self.logger.info("TEST-INFO | LeakSanitizer | To show the "
-                             "addresses of leaked objects add report_objects=1 to LSAN_OPTIONS")
-            self.logger.info("TEST-INFO | LeakSanitizer | This can be done "
-                             "in testing/mozbase/mozrunner/mozrunner/utils.py")
+            self.logger.info("LeakSanitizer | To show the "
+                             "addresses of leaked objects add report_objects=1 to LSAN_OPTIONS\n"
+                             "This can be done in testing/mozbase/mozrunner/mozrunner/utils.py")
 
-        for f in self.foundFrames:
-            self.logger.error(
-                "TEST-UNEXPECTED-FAIL | LeakSanitizer | leak at " + f)
-            failures += 1
+            for frames, allowed in self.foundFrames:
+                self.logger.lsan_leak(frames, scope=self.scope, allowed_match=allowed)
+                if not allowed:
+                    failures += 1
+
+        if self.sawError and not (self.summaryData or
+                                  self.foundFrames or
+                                  self.fatalError or
+                                  self.symbolizerError):
+            self.logger.error("LeakSanitizer | Memory leaks detected but no leak report generated")
+
+        self.sawError = False
 
         return failures
 
     def _finishStack(self):
         if self.recordMoreFrames and len(self.currStack) == 0:
-            self.currStack = ["unknown stack"]
+            self.currStack = {"unknown stack"}
         if self.currStack:
-            self.foundFrames.add(", ".join(self.currStack))
+            self.foundFrames.add((tuple(self.currStack), self.allowedMatch))
             self.currStack = None
+            self.allowedMatch = None
         self.recordMoreFrames = False
         self.numRecordedFrames = 0
 
     def _recordFrame(self, frame):
+        if self.allowedMatch is None and self.allowedRegexp is not None:
+            self.allowedMatch = frame if self.allowedRegexp.match(frame) else None
         self.currStack.append(frame)
         self.numRecordedFrames += 1
         if self.numRecordedFrames >= self.maxNumRecordedFrames:
