c83a81c947ada75b360e9742a9b00c95ad5b094f
Jan de Mooij <jdemooij@mozilla.com>
2018-05-22 15:00:18 +0200

    Bug 1461938 part 9 - Turn wasm::Compartment into wasm::Realm. r=luke

--- js/src/wasm/WasmRealm.h
+++ js/src/wasm/WasmRealm.h
@@ -16,30 +16,29 @@
  * limitations under the License.
  */
 
-#ifndef wasm_compartment_h
-#define wasm_compartment_h
+#ifndef wasm_realm_h
+#define wasm_realm_h
 
 #include "wasm/WasmJS.h"
 
 namespace js {
 namespace wasm {
 
-// wasm::Compartment lives in JSCompartment and contains the wasm-related
-// per-compartment state. wasm::Compartment tracks every live instance in the
-// compartment and must be notified, via registerInstance(), of any new
-// WasmInstanceObject.
+// wasm::Realm lives in JS::Realm and contains the wasm-related per-realm state.
+// wasm::Realm tracks every live instance in the realm and must be notified, via
+// registerInstance(), of any new WasmInstanceObject.
 
-class Compartment
+class Realm
 {
     JSRuntime* runtime_;
     InstanceVector instances_;
 
   public:
-    explicit Compartment(JSRuntime* rt);
-    ~Compartment();
+    explicit Realm(JSRuntime* rt);
+    ~Realm();
 
     // Before a WasmInstanceObject can be considered fully constructed and
-    // valid, it must be registered with the Compartment. If this method fails,
+    // valid, it must be registered with the Realm. If this method fails,
     // an error has been reported and the instance object must be abandoned.
     // After a successful registration, an Instance must call
     // unregisterInstance() before being destroyed.
@@ -47,24 +46,24 @@ class Compartment
     bool registerInstance(JSContext* cx, HandleWasmInstanceObject instanceObj);
     void unregisterInstance(Instance& instance);
 
-    // Return a vector of all live instances in the compartment. The lifetime of
+    // Return a vector of all live instances in the realm. The lifetime of
     // these Instances is determined by their owning WasmInstanceObject.
     // Note that accessing instances()[i]->object() triggers a read barrier
     // since instances() is effectively a weak list.
 
     const InstanceVector& instances() const { return instances_; }
 
-    // Ensure all Instances in this JSCompartment have profiling labels created.
+    // Ensure all Instances in this Realm have profiling labels created.
 
     void ensureProfilingLabels(bool profilingEnabled);
 
     // about:memory reporting
 
-    void addSizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf, size_t* compartmentTables);
+    void addSizeOfExcludingThis(mozilla::MallocSizeOf mallocSizeOf, size_t* realmTables);
 };
 
 // Interrupt all running wasm Instances that have been registered with
-// wasm::Compartments in the given JSContext.
+// wasm::Realms in the given JSContext.
 
 extern void
 InterruptRunningCode(JSContext* cx);
@@ -79,4 +78,4 @@ ResetInterruptState(JSContext* cx);
 } // namespace wasm
 } // namespace js
 
-#endif // wasm_compartment_h
+#endif // wasm_realm_h
