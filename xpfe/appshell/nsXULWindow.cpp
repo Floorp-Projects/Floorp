9448da68d5658fe35e2c9ef4564be3c2890354b2
Emilio Cobos Álvarez <emilio@crisal.io>
2018-05-22 21:37:29 +0200

    Bug 1458999 - Nullcheck for release, assert elsewhere. r=enn, a=RyanVM
    This should allow us to figure out what's killing the window mid-flight, in case
    there's something to fix in the chrome JS code.
    
    MozReview-Commit-ID: 1xx1AVdspB2

--- xpfe/appshell/nsXULWindow.cpp
+++ xpfe/appshell/nsXULWindow.cpp
@@ -451,6 +451,9 @@ NS_IMETHODIMP nsXULWindow::Create()
 
 NS_IMETHODIMP nsXULWindow::Destroy()
 {
+  MOZ_DIAGNOSTIC_ASSERT(!mSyncingAttributesToWidget,
+                        "Destroying the window from SyncAttributesToWidget?");
+
   if (!mWindow)
      return NS_OK;
 
@@ -1550,6 +1553,11 @@ void nsXULWindow::SyncAttributesToWidget()
   if (!windowElement)
     return;
 
+  AutoRestore<bool> scope { mSyncingAttributesToWidget };
+  mSyncingAttributesToWidget = true;
+
+  MOZ_DIAGNOSTIC_ASSERT(mWindow, "No widget on SyncAttributesToWidget?");
+
   nsAutoString attr;
 
   // "hidechrome" attribute
@@ -1557,6 +1565,8 @@ void nsXULWindow::SyncAttributesToWidget()
                                  nsGkAtoms::_true, eCaseMatters)) {
     mWindow->HideWindowChrome(true);
   }
+
+  NS_ENSURE_TRUE_VOID(mWindow);
 
   // "chromemargin" attribute
   nsIntMargin margins;
@@ -1566,11 +1576,15 @@ void nsXULWindow::SyncAttributesToWidget()
     mWindow->SetNonClientMargins(tmp);
   }
 
+  NS_ENSURE_TRUE_VOID(mWindow);
+
   // "windowtype" attribute
   windowElement->GetAttribute(WINDOWTYPE_ATTRIBUTE, attr);
   if (!attr.IsEmpty()) {
     mWindow->SetWindowClass(attr);
   }
+
+  NS_ENSURE_TRUE_VOID(mWindow);
 
   // "id" attribute for icon
   windowElement->GetAttribute(NS_LITERAL_STRING("id"), attr);
@@ -1579,17 +1593,25 @@ void nsXULWindow::SyncAttributesToWidget()
   }
   mWindow->SetIcon(attr);
 
+  NS_ENSURE_TRUE_VOID(mWindow);
+
   // "drawtitle" attribute
   windowElement->GetAttribute(NS_LITERAL_STRING("drawtitle"), attr);
   mWindow->SetDrawsTitle(attr.LowerCaseEqualsLiteral("true"));
+
+  NS_ENSURE_TRUE_VOID(mWindow);
 
   // "toggletoolbar" attribute
   windowElement->GetAttribute(NS_LITERAL_STRING("toggletoolbar"), attr);
   mWindow->SetShowsToolbarButton(attr.LowerCaseEqualsLiteral("true"));
 
+  NS_ENSURE_TRUE_VOID(mWindow);
+
   // "fullscreenbutton" attribute
   windowElement->GetAttribute(NS_LITERAL_STRING("fullscreenbutton"), attr);
   mWindow->SetShowsFullScreenButton(attr.LowerCaseEqualsLiteral("true"));
+
+  NS_ENSURE_TRUE_VOID(mWindow);
 
   // "macanimationtype" attribute
   windowElement->GetAttribute(NS_LITERAL_STRING("macanimationtype"), attr);
