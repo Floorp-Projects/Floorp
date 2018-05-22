a3a03a754694aa8548f68d02b2d15ce1b449edc4
Jason Laster <jason.laster.11@gmail.com>
2018-05-22 09:07:10 -0400

    Bug 1463415 - Update Debugger Frontend v57. r=dwalsh
    MozReview-Commit-ID: JArKRKwmWjF

--- devtools/client/shared/components/reps/reps.js
+++ devtools/client/shared/components/reps/reps.js
@@ -4441,11 +4441,12 @@ class Tree extends Component {
           return;
         }
 
-        const { explicitOriginalTarget } = nativeEvent;
+        const { relatedTarget } = nativeEvent;
+
         // Only set default focus to the first tree node if the focus came
         // from outside the tree (e.g. by tabbing to the tree from other
         // external elements).
-        if (explicitOriginalTarget !== this.treeRef && !this.treeRef.contains(explicitOriginalTarget)) {
+        if (relatedTarget !== this.treeRef && !this.treeRef.contains(relatedTarget)) {
           this._focus(traversal[0].item);
         }
       },
