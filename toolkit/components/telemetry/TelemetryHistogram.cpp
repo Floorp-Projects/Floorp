e25cf757808e689e751e04e7a3d5f0965c79a4cb
Alessio Placitelli <alessio.placitelli@gmail.com>
2018-05-22 17:12:59 +0200

    Bug 1459144 - Allocate enough memory for all the processes when deserializing histograms. r=chutten
    This is needed as we might end up serializing only one process, but its id might
    be greater than 1 when deserializing.
    
    MozReview-Commit-ID: EzXOOhlNV1Y

--- toolkit/components/telemetry/TelemetryHistogram.cpp
+++ toolkit/components/telemetry/TelemetryHistogram.cpp
@@ -2812,7 +2812,7 @@ TelemetryHistogram::DeserializeHistograms(JSContext* aCx, JS::HandleValue aData)
 
   // Make sure we have enough storage for all the processes.
   PersistedHistogramStorage histogramsToUpdate;
-  if (!histogramsToUpdate.resize(processes.length())) {
+  if (!histogramsToUpdate.resize(static_cast<uint32_t>(ProcessID::Count))) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
 
@@ -2968,7 +2968,7 @@ TelemetryHistogram::DeserializeKeyedHistograms(JSContext* aCx, JS::HandleValue a
 
   // Make sure we have enough storage for all the processes.
   PersistedKeyedHistogramStorage histogramsToUpdate;
-  if (!histogramsToUpdate.resize(processes.length())) {
+  if (!histogramsToUpdate.resize(static_cast<uint32_t>(ProcessID::Count))) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
 
