1d11da756cc82ed2f724a540b600b4f4b1e97375
Jason Laster <jason.laster.11@gmail.com>
2018-05-22 12:24:27 -0400

    Bug 1463481 - Update Debugger Frontend v58. r=dwalsh
    MozReview-Commit-ID: JS9wvG9Qmw6

--- devtools/client/debugger/new/src/utils/preview.js
+++ devtools/client/debugger/new/src/utils/preview.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.isImmutablePreview = isImmutablePreview;
 exports.isImmutable = isImmutable;
 exports.isReactComponent = isReactComponent;
 exports.isConsole = isConsole;
@@ -11,33 +12,28 @@ exports.isConsole = isConsole;
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 const IMMUTABLE_FIELDS = ["_root", "__ownerID", "__altered", "__hash"];
+const REACT_FIELDS = ["_reactInternalInstance", "_reactInternalFiber"];
+
+function isImmutablePreview(result) {
+  return result && isImmutable(result.preview);
+}
 
 function isImmutable(result) {
-  if (!result || !result.preview) {
+  if (!result || typeof result.ownProperties != "object") {
     return;
   }
 
-  const ownProperties = result.preview.ownProperties;
-
-  if (!ownProperties) {
-    return;
-  }
-
+  const ownProperties = result.ownProperties;
   return IMMUTABLE_FIELDS.every(field => Object.keys(ownProperties).includes(field));
 }
 
 function isReactComponent(result) {
-  if (!result || !result.preview) {
+  if (!result || typeof result.ownProperties != "object") {
     return;
   }
 
-  const ownProperties = result.preview.ownProperties;
-
-  if (!ownProperties) {
-    return;
-  }
-
-  return Object.keys(ownProperties).includes("_reactInternalInstance") || Object.keys(ownProperties).includes("_reactInternalFiber");
+  const ownProperties = result.ownProperties;
+  return REACT_FIELDS.some(field => Object.keys(ownProperties).includes(field));
 }
 
 function isConsole(expression) {
