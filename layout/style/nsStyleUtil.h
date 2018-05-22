2698440ab3d25f8e58c76a2239a0baed432689a5
Emilio Cobos Álvarez <emilio@crisal.io>
2018-05-22 13:58:24 +0200

    Bug 1463396: Remove unused nsStyleUtil::AppendSerializedFontSrc. r=xidorn
    MozReview-Commit-ID: FG6SMUQAj89

--- layout/style/nsStyleUtil.h
+++ layout/style/nsStyleUtil.h
@@ -116,9 +116,6 @@ public:
       nsTimingFunction::Type aType,
       nsAString& aResult);
 
-  static void AppendSerializedFontSrc(const nsCSSValue& aValue,
-                                      nsAString& aResult);
-
   // convert bitmask value to keyword name for a functional alternate
   static void GetFunctionalAlternatesName(int32_t aFeature,
                                           nsAString& aFeatureName);
