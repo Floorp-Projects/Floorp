11ae2207cf24c56d0cd2b27a9e7489fe5417733e
Masayuki Nakano <masayuki@d-toybox.com>
2018-05-22 20:15:05 +0900

    Bug 1463327 - part 4: Add comments to explain which kind of methods should be public methods r=m_kato
    MozReview-Commit-ID: 6l8ZiMWEYCI
    
    --HG--
    extra : rebase_source : 024625012c917b17710016b91db10bfbaace2134

--- editor/libeditor/TextEditor.h
+++ editor/libeditor/TextEditor.h
@@ -39,6 +39,16 @@ class TextEditor : public EditorBase
                  , public nsIEditorMailSupport
 {
 public:
+  /****************************************************************************
+   * NOTE: DO NOT MAKE YOUR NEW METHODS PUBLIC IF they are called by other
+   *       classes under libeditor except EditorEventListener and
+   *       HTMLEditorEventListener because each public method which may fire
+   *       eEditorInput event will need to instantiate new stack class for
+   *       managing input type value of eEditorInput and cache some objects
+   *       for smarter handling.  In other words, when you add new root
+   *       method to edit the DOM tree, you can make your new method public.
+   ****************************************************************************/
+
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(TextEditor, EditorBase)
 
@@ -163,6 +173,15 @@ public:
   void OnCompositionEnd(WidgetCompositionEvent& aCompositionEndEvent);
 
 protected: // May be called by friends.
+  /****************************************************************************
+   * Some classes like TextEditRules, HTMLEditRules, WSRunObject which are
+   * part of handling edit actions are allowed to call the following protected
+   * methods.  However, those methods won't prepare caches of some objects
+   * which are necessary for them.  So, if you want some following methods
+   * to do that for you, you need to create a wrapper method in public scope
+   * and call it.
+   ****************************************************************************/
+
   // Overrides of EditorBase
   virtual nsresult RemoveAttributeOrEquivalent(
                      Element* aElement,
