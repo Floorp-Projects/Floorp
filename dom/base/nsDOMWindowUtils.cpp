0408cbb49e7d618ff512dce8da41860301432e06
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-05-23 18:18:43 +0900

    Bug 1463605 - Check continuation or IB split sibling frames in nsDOMWindowUtils::CheckAndClearPaintedState. r=mstange
    MozReview-Commit-ID: CgyErEGeZJS
    
    --HG--
    extra : rebase_source : 1e179f64a39e0cab6b32074fa6ebf6c1884c8b32

--- dom/base/nsDOMWindowUtils.cpp
+++ dom/base/nsDOMWindowUtils.cpp
@@ -2907,7 +2907,14 @@ nsDOMWindowUtils::CheckAndClearPaintedState(Element* aElement, bool* aResult)
     }
   }
 
-  *aResult = frame->CheckAndClearPaintedState();
+  while (frame) {
+    if (!frame->CheckAndClearPaintedState()) {
+      *aResult = false;
+      return NS_OK;
+    }
+    frame = nsLayoutUtils::GetNextContinuationOrIBSplitSibling(frame);
+  }
+  *aResult = true;
   return NS_OK;
 }
 
