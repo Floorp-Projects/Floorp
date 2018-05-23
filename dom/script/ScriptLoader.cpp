a844ec84c432d4dd24d4464b1b4f6888e038ab86
Jon Coppeard <jcoppeard@mozilla.com>
2018-05-23 09:30:13 +0100

    Bug 1427610 - Implement import.meta in the browser r=bkelly

--- dom/script/ScriptLoader.cpp
+++ dom/script/ScriptLoader.cpp
@@ -785,6 +785,34 @@ HostResolveImportedModule(JSContext* aCx, JS::Handle<JSObject*> aModule,
   return ms->ModuleRecord();
 }
 
+bool
+HostPopulateImportMeta(JSContext* aCx, JS::Handle<JSObject*> aModule,
+                       JS::Handle<JSObject*> aMetaObject)
+{
+  MOZ_DIAGNOSTIC_ASSERT(aModule);
+
+  JS::Value value = JS::GetModuleHostDefinedField(aModule);
+  if (value.isUndefined()) {
+    JS_ReportErrorASCII(aCx, "Module script not found");
+    return false;
+  }
+
+  auto script = static_cast<ModuleScript*>(value.toPrivate());
+  MOZ_DIAGNOSTIC_ASSERT(script->ModuleRecord() == aModule);
+
+  nsAutoCString url;
+  MOZ_DIAGNOSTIC_ASSERT(script->BaseURL());
+  MOZ_ALWAYS_SUCCEEDS(script->BaseURL()->GetAsciiSpec(url));
+
+  JS::Rooted<JSString*> urlString(aCx, JS_NewStringCopyZ(aCx, url.get()));
+  if (!urlString) {
+    JS_ReportOutOfMemory(aCx);
+    return false;
+  }
+
+  return JS_DefineProperty(aCx, aMetaObject, "url", urlString, JSPROP_ENUMERATE);
+}
+
 static void
 EnsureModuleResolveHook(JSContext* aCx)
 {
@@ -794,6 +822,7 @@ EnsureModuleResolveHook(JSContext* aCx)
   }
 
   JS::SetModuleResolveHook(rt, HostResolveImportedModule);
+  JS::SetModuleMetadataHook(rt, HostPopulateImportMeta);
 }
 
 void
