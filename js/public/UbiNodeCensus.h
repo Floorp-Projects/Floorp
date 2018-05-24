f0a6988ed3c638970cf3b1e737923220c4be6b20
Jon Coppeard <jcoppeard@mozilla.com>
2018-05-24 09:40:46 +0100

    Bug 1463723 - Remove ubi::Census' pointer to the atoms zone r=fitzgen

--- js/public/UbiNodeCensus.h
+++ js/public/UbiNodeCensus.h
@@ -199,9 +199,8 @@ struct Census {
     // is an element of the set. If the targetZones set is empty, then nodes in
     // all zones are considered.
     JS::ZoneSet targetZones;
-    Zone* atomsZone;
 
-    explicit Census(JSContext* cx) : cx(cx), atomsZone(nullptr) { }
+    explicit Census(JSContext* cx) : cx(cx) { }
 
     MOZ_MUST_USE JS_PUBLIC_API(bool) init();
 };
