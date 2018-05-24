c40f290d5a55112b803311f6cc8dadff15f5cdab
Belén Albeza <balbeza@mozilla.com>
2018-05-24 18:01:39 +0200

    Bug 1465717 - Missing test for opening links when devtools are in a separate window. r=jdescottes
    MozReview-Commit-ID: 2yK63fjjXvs

---
+++ devtools/client/application/test/browser_application_panel_open-links.js
@@ -0,0 +1,41 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const { Toolbox } = require("devtools/client/framework/toolbox");
+
+/**
+ * Check that links work when the devtools are detached in a separate window.
+ */
+
+const TAB_URL = URL_ROOT + "service-workers/empty.html";
+
+add_task(async function() {
+  await enableApplicationPanel();
+
+  const {
+    panel,
+    toolbox
+  } = await openNewTabAndApplicationPanel(TAB_URL);
+
+  const doc = panel.panelWin.document;
+
+  // detach devtools in a separate window
+  await toolbox.switchHost(Toolbox.HostType.WINDOW);
+
+  // click on the link and wait for the new tab to open
+  const onTabLoaded = BrowserTestUtils
+    .waitForNewTab(gBrowser, "about:debugging#workers");
+  doc.querySelector(".js-trusted-link").click();
+  info("Opening link in a new tab.");
+  const newTab = await onTabLoaded;
+
+  // We only need to check that newTab is truthy since
+  // BrowserTestUtils.waitForNewTab checks the URL.
+  ok(newTab, "The expected tab was opened.");
+
+  // close the tab
+  info("Closing the tab.");
+  await BrowserTestUtils.removeTab(newTab);
+});
