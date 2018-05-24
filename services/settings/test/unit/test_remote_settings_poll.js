af1ced8604ee63760b0a40c46869f9f5cd040c88
Mathieu Leplatre <mathieu@mozilla.com>
2018-05-24 15:32:40 +0200

    Bug 1463750 - Move RemoteSettings to services/settings/ r=mgoodwin
    MozReview-Commit-ID: 7IwUysPBHZu
    
    --HG--
    rename : services/common/remote-settings.js => services/settings/remote-settings.js
    rename : services/common/tests/unit/test_remote_settings.js => services/settings/test/unit/test_remote_settings.js
    rename : services/common/tests/unit/test_remote_settings_jexl_filters.js => services/settings/test/unit/test_remote_settings_jexl_filters.js
    rename : services/common/tests/unit/test_remote_settings_poll.js => services/settings/test/unit/test_remote_settings_poll.js
    extra : rebase_source : 67b2fdbf7b4230ba9e9858ea7a0409a6f4d5ec75

--- services/settings/test/unit/test_remote_settings_poll.js
+++ services/settings/test/unit/test_remote_settings_poll.js
@@ -1,6 +1,8 @@
+ChromeUtils.import("resource://gre/modules/Services.jsm");
 ChromeUtils.import("resource://testing-common/httpd.js");
+
 const { UptakeTelemetry } = ChromeUtils.import("resource://services-common/uptake-telemetry.js", {});
-const { RemoteSettings } = ChromeUtils.import("resource://services-common/remote-settings.js", {});
+const { RemoteSettings } = ChromeUtils.import("resource://services-settings/remote-settings.js", {});
 
 var server;
 
