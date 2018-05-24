ccdb1e358f93a710e0ffa99811e7ebf576d2a90c
abhinav <abhinav.koppula@gmail.com>
2018-05-25 02:43:14 +0530

    Bug 1462886 - Fix for filter flag breaking the netmonitor when using invalid values. r=Honza, a=RyanVM
    MozReview-Commit-ID: 5cvUX2sb7k0
    
    --HG--
    extra : source : b19a39acb93990a0df7b6e696bca119cab3ac738

--- devtools/client/netmonitor/test/browser_net_filter-flags.js
+++ devtools/client/netmonitor/test/browser_net_filter-flags.js
@@ -257,6 +257,9 @@ add_task(async function() {
   setFreetextFilter("size:34");
   await testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 0]);
 
+  setFreetextFilter("size:0kb");
+  await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
   // Testing the lower bound
   setFreetextFilter("size:9.659k");
   await testContents([0, 0, 0, 0, 0, 0, 0, 0, 1, 0]);
@@ -279,6 +282,9 @@ add_task(async function() {
   setFreetextFilter("transferred:248");
   await testContents([0, 0, 1, 1, 0, 0, 0, 0, 0, 1]);
 
+  setFreetextFilter("transferred:0kb");
+  await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
   // Test larger-than
   setFreetextFilter("larger-than:-1");
   await testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
@@ -298,6 +304,9 @@ add_task(async function() {
   setFreetextFilter("larger-than:10.732k");
   await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
+  setFreetextFilter("larger-than:0kb");
+  await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
   // Test transferred-larger-than
   setFreetextFilter("transferred-larger-than:-1");
   await testContents([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
@@ -312,6 +321,9 @@ add_task(async function() {
   await testContents([0, 1, 0, 1, 0, 0, 0, 0, 1, 0]);
 
   setFreetextFilter("transferred-larger-than:10.73k");
+  await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  setFreetextFilter("transferred-larger-than:0kb");
   await testContents([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   // Test cause
