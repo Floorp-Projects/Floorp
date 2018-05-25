9d2274bc09798320c5b123cf56678e1dc26d1267
Anny Gakhokidze <agakhokidze@mozilla.com>
2018-05-25 10:35:43 -0400

    Bug 1464205 - Clear cached values unconditionally in DataTransferItemList::SetDataWithPrincipal, r=Nika
    MozReview-Commit-ID: 9HLkDwNkdo7
    
    --HG--
    extra : rebase_source : 95a67b968bba5a49f805418486b5072c98ee0390

--- dom/events/DataTransferItemList.cpp
+++ dom/events/DataTransferItemList.cpp
@@ -360,10 +360,8 @@ DataTransferItemList::SetDataWithPrincipal(const nsAString& aType,
 
         DataTransferItem::eKind oldKind = item->Kind();
         item->SetData(aData);
-        if (oldKind != item->Kind()) {
-          // Types list may have changed, even if aIndex == 0.
-          mDataTransfer->TypesListMayHaveChanged();
-        }
+
+        mDataTransfer->TypesListMayHaveChanged();
 
         if (aIndex != 0) {
           // If the item changes from being a file to not a file or vice-versa,
