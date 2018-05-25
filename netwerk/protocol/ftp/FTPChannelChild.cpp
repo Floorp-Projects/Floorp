ff71e7a11d6535607b87e9c8447c76cdaec8461c
Bogdan Tara <btara@mozilla.com>
2018-05-25 17:16:34 +0300

    Backed out changeset 6cb4b8d4daad (bug 1459666) for build bustages on /netwerk/protocol/ftp/FTPChannelChild.cpp CLOSED TREE

--- netwerk/protocol/ftp/FTPChannelChild.cpp
+++ netwerk/protocol/ftp/FTPChannelChild.cpp
@@ -312,11 +312,6 @@ FTPChannelChild::DoOnStartRequest(const nsresult& aChannelStatus,
                                   const nsCString& aEntityID,
                                   const URIParams& aURI)
 {
-  mDuringOnStart = true;
-  auto clearDuringFlag = mozilla::MakeScopeExit([this] {
-    this->mDuringOnStart = false;
-  });
-
   LOG(("FTPChannelChild::DoOnStartRequest [this=%p]\n", this));
 
   // mFlushedForDiversion and mDivertingToParent should NEVER be set at this
@@ -896,11 +891,6 @@ FTPChannelChild::DivertToParent(ChannelDiverterChild **aChild)
                    IsShuttingDown(), NS_ERROR_FAILURE);
 
   LOG(("FTPChannelChild::DivertToParent [this=%p]\n", this));
-
-  // This method should only be called during OnStartRequest.
-  if (!mDuringOnStart) {
-    return NS_ERROR_NOT_AVAILABLE;
-  }
 
   // We must fail DivertToParent() if there's no parent end of the channel (and
   // won't be!) due to early failure.
