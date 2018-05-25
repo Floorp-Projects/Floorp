5b784f2193e63a3949a45d2215328c3a70341324
Henrik Skupin <mail@hskupin.info>
2018-05-25 23:12:45 +0200

    Bug 1448792 - [wdspec] Add basic tests and user prompt tests for Element Send Keys. r=ato
    MozReview-Commit-ID: KzfN4OLM1YC

--- testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
+++ testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
@@ -9,7 +9,7 @@ from tests.support.asserts import (
 from tests.support.inline import inline
 
 
-def send_keys_to_element(session, element, text):
+def element_send_keys(session, element, text):
     return session.transport.send(
         "POST", "/session/{session_id}/element/{element_id}/value".format(
             session_id=session.session_id,
@@ -36,7 +36,7 @@ def test_input(session):
     element = session.find.css("input", all=False)
     assert element.property("value") == ""
 
-    send_keys_to_element(session, element, "foo")
+    element_send_keys(session, element, "foo")
     assert element.property("value") == "foo"
     assert_element_has_focus(element)
 
@@ -46,7 +46,7 @@ def test_textarea(session):
     element = session.find.css("textarea", all=False)
     assert element.property("value") == ""
 
-    send_keys_to_element(session, element, "foo")
+    element_send_keys(session, element, "foo")
     assert element.property("value") == "foo"
     assert_element_has_focus(element)
 
@@ -56,10 +56,10 @@ def test_input_append(session):
     element = session.find.css("input", all=False)
     assert element.property("value") == "a"
 
-    send_keys_to_element(session, element, "b")
+    element_send_keys(session, element, "b")
     assert element.property("value") == "ab"
 
-    send_keys_to_element(session, element, "c")
+    element_send_keys(session, element, "c")
     assert element.property("value") == "abc"
 
 
@@ -68,10 +68,10 @@ def test_textarea_append(session):
     element = session.find.css("textarea", all=False)
     assert element.property("value") == "a"
 
-    send_keys_to_element(session, element, "b")
+    element_send_keys(session, element, "b")
     assert element.property("value") == "ab"
 
-    send_keys_to_element(session, element, "c")
+    element_send_keys(session, element, "c")
     assert element.property("value") == "abc"
 
 
@@ -81,7 +81,7 @@ def test_events(session, tag):
     element = session.find.css(tag, all=False)
     add_event_listeners(element)
 
-    send_keys_to_element(session, element, "foo")
+    element_send_keys(session, element, "foo")
     assert element.property("value") == "foo"
     assert get_events(session) == ["focus",
                                    "keydown",
@@ -103,5 +103,5 @@ def test_not_blurred(session, tag):
     session.url = inline("<%s>" % tag)
     element = session.find.css(tag, all=False)
 
-    send_keys_to_element(session, element, "")
+    element_send_keys(session, element, "")
     assert_element_has_focus(element)
--- testing/web-platform/tests/webdriver/tests/element_send_keys/send_keys.py
+++ testing/web-platform/tests/webdriver/tests/element_send_keys/send_keys.py
@@ -1,4 +1,6 @@
-from tests.support.asserts import assert_success
+import pytest
+
+from tests.support.asserts import assert_error, assert_success
 from tests.support.inline import inline
 
 
@@ -17,3 +19,34 @@ def test_null_response_value(session):
     response = element_send_keys(session, element, "foo")
     value = assert_success(response)
     assert value is None
+
+
+@pytest.mark.parametrize("value", [True, None, 1, [], {}])
+def test_invalid_text_type(session, value):
+    session.url = inline("<input>")
+    element = session.find.css("input", all=False)
+
+    response = element_send_keys(session, element, value)
+    assert_error(response, "invalid argument")
+
+
+def test_no_browsing_context(session, create_window):
+    session.window_handle = create_window()
+
+    session.url = inline("<input>")
+    element = session.find.css("input", all=False)
+
+    session.close()
+
+    response = element_send_keys(session, element, "foo")
+    assert_error(response, "no such window")
+
+
+def test_stale_element(session):
+    session.url = inline("<input>")
+    element = session.find.css("input", all=False)
+
+    session.refresh()
+
+    response = element_send_keys(session, element, "foo")
+    assert_error(response, "stale element reference")
