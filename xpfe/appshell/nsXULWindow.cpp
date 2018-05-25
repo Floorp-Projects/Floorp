cf7f56194258f8abeaea59f23a51da97436d96ea
Emilio Cobos Álvarez <emilio@crisal.io>
2018-05-25 20:58:56 +0200

    Bug 1463819 - Account for the possibility of SyncAttributesToWidget destroying the window. r=enn, a=RyanVM
    MozReview-Commit-ID: 8O3We8wnSGk

--- xpfe/appshell/nsXULWindow.cpp
+++ xpfe/appshell/nsXULWindow.cpp
@@ -451,9 +451,6 @@ NS_IMETHODIMP nsXULWindow::Create()
 
 NS_IMETHODIMP nsXULWindow::Destroy()
 {
-  MOZ_DIAGNOSTIC_ASSERT(!mSyncingAttributesToWidget,
-                        "Destroying the window from SyncAttributesToWidget?");
-
   if (!mWindow)
      return NS_OK;
 
@@ -1127,13 +1124,15 @@ void nsXULWindow::OnChromeLoaded()
     mChromeLoaded = true;
     ApplyChromeFlags();
     SyncAttributesToWidget();
-    SizeShell();
-
-    if (mShowAfterLoad) {
-      SetVisibility(true);
-      // At this point the window may have been closed during Show(), so
-      // nsXULWindow::Destroy may already have been called. Take care!
+    if (mWindow) {
+      SizeShell();
+      if (mShowAfterLoad) {
+        SetVisibility(true);
+      }
     }
+    // At this point the window may have been closed already during Show() or
+    // SyncAttributesToWidget(), so nsXULWindow::Destroy may already have been
+    // called. Take care!
   }
   mPersistentAttributesMask |= PAD_POSITION | PAD_SIZE | PAD_MISC;
 }
@@ -1552,9 +1551,6 @@ void nsXULWindow::SyncAttributesToWidget()
   nsCOMPtr<dom::Element> windowElement = GetWindowDOMElement();
   if (!windowElement)
     return;
-
-  AutoRestore<bool> scope { mSyncingAttributesToWidget };
-  mSyncingAttributesToWidget = true;
 
   MOZ_DIAGNOSTIC_ASSERT(mWindow, "No widget on SyncAttributesToWidget?");
 
@@ -2303,7 +2299,9 @@ nsXULWindow::BeforeStartLayout()
 {
   ApplyChromeFlags();
   SyncAttributesToWidget();
-  SizeShell();
+  if (mWindow) {
+    SizeShell();
+  }
   return NS_OK;
 }
 
