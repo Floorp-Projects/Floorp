e786f103f4f59680dbf1a9f09b0d9030ac09fc5d
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-05-25 16:47:18 +0900

    Bug 1454954 - Part 2: Test the animation state in case of the currentTime is over endTime. r=pbro
    MozReview-Commit-ID: 39jPrDAuWHM
    
    --HG--
    extra : rebase_source : acff011a04a19781abdf935e3e07b415cbc8ceb2

---
+++ devtools/client/inspector/animation/test/browser_animation_pause-resume-button_end-time.js
@@ -0,0 +1,47 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test whether the animation can rewind if the current time is over end time when
+// the resume button clicked.
+
+add_task(async function() {
+  await addTab(URL_ROOT + "doc_simple_animation.html");
+  await removeAnimatedElementsExcept([".animated", ".end-delay", ".long"]);
+  const { animationInspector, panel } = await openAnimationInspector();
+
+  info("Check animations state after resuming with infinite animation");
+  info("Make the current time of animation to be over its end time");
+  await clickOnCurrentTimeScrubberController(animationInspector, panel, 1);
+  info("Resume animations");
+  await clickOnPauseResumeButton(animationInspector, panel);
+  await wait(1000);
+  assertPlayState(animationInspector.state.animations,
+                  ["running", "finished", "finished"]);
+  await clickOnCurrentTimeScrubberController(animationInspector, panel, 0);
+
+  info("Check animations state after resuming without infinite animation");
+  info("Remove infinite animation");
+  await setClassAttribute(animationInspector, ".animated", "ball still");
+  info("Make the current time of animation to be over its end time");
+  await clickOnCurrentTimeScrubberController(animationInspector, panel, 1);
+  info("Resume animations");
+  await clickOnPauseResumeButton(animationInspector, panel);
+  assertPlayState(animationInspector.state.animations, ["running", "running"]);
+  assertCurrentTimeLessThanDuration(animationInspector.state.animations);
+});
+
+function assertPlayState(animations, expectedState) {
+  animations.forEach((animation, index) => {
+    is(animation.state.playState, expectedState[index],
+       `The playState of animation [${ index }] should be ${ expectedState[index] }`);
+  });
+}
+
+function assertCurrentTimeLessThanDuration(animations) {
+  animations.forEach((animation, index) => {
+    ok(animation.state.currentTime < animation.state.duration,
+       "The current time of animation[${ index }] should be less than its duration");
+  });
+}
