19707c9a26afefdd589ba6d6a52f688c0a33d4b3
Mark Striemer <mstriemer@mozilla.com>
2018-05-28 15:33:26 -0500

    Bug 1425347 - Hide system add-ons by default in about:debugging r=jdescottes
    MozReview-Commit-ID: 4bhF6Zxsn6w

--- devtools/client/aboutdebugging/test/browser_addons_debug_info.js
+++ devtools/client/aboutdebugging/test/browser_addons_debug_info.js
@@ -1,6 +1,9 @@
 "use strict";
 
+const { Preferences } = ChromeUtils.import("resource://gre/modules/Preferences.jsm", {});
+
 const UUID_REGEX = /^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/;
+const SHOW_SYSTEM_ADDONS_PREF = "devtools.aboutdebugging.showSystemAddons";
 
 function testFilePath(container, expectedFilePath) {
   // Verify that the path to the install location is shown next to its label.
@@ -118,4 +121,27 @@ add_task(async function testUnknownManifestProperty() {
   await uninstallAddon({document, id: addonId, name: addonName});
 
   await closeAboutDebugging(tab);
+});
+
+add_task(async function testSystemAddonsHidden() {
+  await pushPref(SHOW_SYSTEM_ADDONS_PREF, false);
+
+  const { document } = await openAboutDebugging("addons");
+  const systemAddonsShown = () => !!document.getElementById("system-extensions");
+
+  await waitForInitialAddonList(document);
+
+  ok(!systemAddonsShown(), "System extensions are hidden");
+
+  Preferences.set(SHOW_SYSTEM_ADDONS_PREF, true);
+
+  await waitUntil(systemAddonsShown);
+
+  ok(systemAddonsShown(), "System extensions are now shown");
+
+  Preferences.set(SHOW_SYSTEM_ADDONS_PREF, false);
+
+  await waitUntil(() => !systemAddonsShown());
+
+  ok(!systemAddonsShown(), "System extensions are hidden again");
 });
