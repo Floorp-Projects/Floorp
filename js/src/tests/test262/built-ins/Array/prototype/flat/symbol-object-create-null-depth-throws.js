7ae1095aec7bd6affbbe88dc08a24269117effa5
Tom Schuster <evilpies@gmail.com>
2018-05-29 13:22:30 +0200

    Bug 1465039 - Update test262. r=anba
    --HG--
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/array-like-objects.js => js/src/tests/test262/built-ins/Array/prototype/flat/array-like-objects.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/bound-function-call.js => js/src/tests/test262/built-ins/Array/prototype/flat/bound-function-call.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/browser.js => js/src/tests/test262/built-ins/Array/prototype/flat/browser.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/empty-array-elements.js => js/src/tests/test262/built-ins/Array/prototype/flat/empty-array-elements.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/empty-object-elements.js => js/src/tests/test262/built-ins/Array/prototype/flat/empty-object-elements.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/length.js => js/src/tests/test262/built-ins/Array/prototype/flat/length.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/name.js => js/src/tests/test262/built-ins/Array/prototype/flat/name.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/non-numeric-depth-should-not-throw.js => js/src/tests/test262/built-ins/Array/prototype/flat/non-numeric-depth-should-not-throw.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/non-object-ctor-throws.js => js/src/tests/test262/built-ins/Array/prototype/flat/non-object-ctor-throws.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/null-undefined-elements.js => js/src/tests/test262/built-ins/Array/prototype/flat/null-undefined-elements.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/null-undefined-input-throws.js => js/src/tests/test262/built-ins/Array/prototype/flat/null-undefined-input-throws.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/positive-infinity.js => js/src/tests/test262/built-ins/Array/prototype/flat/positive-infinity.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/prop-desc.js => js/src/tests/test262/built-ins/Array/prototype/flat/prop-desc.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/shell.js => js/src/tests/test262/built-ins/Array/prototype/flat/shell.js
    rename : js/src/tests/test262/built-ins/Array/prototype/flatten/symbol-object-create-null-depth-throws.js => js/src/tests/test262/built-ins/Array/prototype/flat/symbol-object-create-null-depth-throws.js
    extra : rebase_source : 26a1d0aab8066b287b5d4041f7fc5b4679aa93f1

--- js/src/tests/test262/built-ins/Array/prototype/flat/symbol-object-create-null-depth-throws.js
+++ js/src/tests/test262/built-ins/Array/prototype/flat/symbol-object-create-null-depth-throws.js
@@ -1,18 +1,18 @@
 // Copyright (C) 2018 Shilpi Jain and Michael Ficarra. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-array.prototype.flatten
+esid: sec-array.prototype.flat
 description: >
     if the argument is a Symbol or Object null, it throws exception
-features: [Array.prototype.flatten]
+features: [Array.prototype.flat]
 ---*/
 
 assert.throws(TypeError, function() {
-  [].flatten(Symbol());
+  [].flat(Symbol());
 }, 'symbol value');
 
 assert.throws(TypeError, function() {
-  [].flatten(Object.create(null));
+  [].flat(Object.create(null));
 }, 'object create null');
 
 reportCompare(0, 0);
--- js/src/tests/test262/language/expressions/less-than/bigint-and-string.js
+++ js/src/tests/test262/language/expressions/less-than/bigint-and-string.js
@@ -26,6 +26,15 @@ assert.sameValue(-1n < '1', true, 'The result of (-1n < "1") is true');
 assert.sameValue('-1' < 1n, true, 'The result of ("-1" < 1n) is true');
 assert.sameValue(-1n < '-1', false, 'The result of (-1n < "-1") is false');
 assert.sameValue('-1' < -1n, false, 'The result of ("-1" < -1n) is false');
+assert.sameValue('0x10' < 15n, false, 'The result of ("0x10" < 15n) is false');
+assert.sameValue('0x10' < 16n, false, 'The result of ("0x10" < 16n) is false');
+assert.sameValue('0x10' < 17n, true, 'The result of ("0x10" < 17n) is true');
+assert.sameValue('0o10' < 7n, false, 'The result of ("0o10" < 7n) is false');
+assert.sameValue('0o10' < 8n, false, 'The result of ("0o10" < 8n) is false');
+assert.sameValue('0o10' < 9n, true, 'The result of ("0o10" < 9n) is true');
+assert.sameValue('0b10' < 1n, false, 'The result of ("0b10" < 1n) is false');
+assert.sameValue('0b10' < 2n, false, 'The result of ("0b10" < 2n) is false');
+assert.sameValue('0b10' < 3n, true, 'The result of ("0b10" < 3n) is true');
 
 assert.sameValue(
   9007199254740993n < '9007199254740992',
@@ -50,4 +59,5 @@ assert.sameValue(
   false,
   'The result of ("-9007199254740992" < -9007199254740993n) is false'
 );
+
 reportCompare(0, 0);
