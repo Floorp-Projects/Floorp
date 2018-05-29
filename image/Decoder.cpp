ac148192261b1c1c97b38dec2538ae489626d889
Andrew Osmond <aosmond@mozilla.com>
2018-05-29 08:36:13 -0400

    Bug 1462355 - Part 9. Lock animated imgFrame objects at creation rather than deferring. r=tnikkel

--- image/Decoder.cpp
+++ image/Decoder.cpp
@@ -356,8 +356,6 @@ Decoder::AllocateFrameInternal(const gfx::IntSize& aOutputSize,
 
   if (frameNum == 1) {
     MOZ_ASSERT(aPreviousFrame, "Must provide a previous frame when animated");
-    aPreviousFrame->SetRawAccessOnly();
-
     // If we dispose of the first frame by clearing it, then the first frame's
     // refresh area is all of itself.
     // RESTORE_PREVIOUS is invalid (assumed to be DISPOSE_CLEAR).
@@ -370,8 +368,6 @@ Decoder::AllocateFrameInternal(const gfx::IntSize& aOutputSize,
   }
 
   if (frameNum > 0) {
-    ref->SetRawAccessOnly();
-
     // Some GIFs are huge but only have a small area that they animate. We only
     // need to refresh that small area when frame 0 comes around again.
     mFirstFrameRefreshArea.UnionRect(mFirstFrameRefreshArea, frame->GetRect());
