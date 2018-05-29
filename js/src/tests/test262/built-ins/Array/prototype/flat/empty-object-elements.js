13f19a9109f83024410e14d12771aff611b3d421
Tom Schuster <evilpies@gmail.com>
2018-05-29 13:22:30 +0200

    Bug 1465039 - Update test262. r=anba

--- js/src/tests/test262/built-ins/Array/prototype/flat/empty-object-elements.js
+++ js/src/tests/test262/built-ins/Array/prototype/flat/empty-object-elements.js
@@ -1,24 +1,24 @@
 // Copyright (C) 2018 Shilpi Jain and Michael Ficarra. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-array.prototype.flatten
+esid: sec-array.prototype.flat
 description: >
     arrays with empty object elements
 includes: [compareArray.js]
-features: [Array.prototype.flatten]
+features: [Array.prototype.flat]
 ---*/
 
 var a = {},
   b = {};
 
-assert.compareArray([a].flatten(), [a]);
-assert.compareArray([a, [b]].flatten(), [a, b]);
+assert.compareArray([a].flat(), [a]);
+assert.compareArray([a, [b]].flat(), [a, b]);
 assert.compareArray([
   [a], b
-].flatten(), [a, b]);
+].flat(), [a, b]);
 assert.compareArray([
   [a],
   [b]
-].flatten(), [a, b]);
+].flat(), [a, b]);
 
 reportCompare(0, 0);
