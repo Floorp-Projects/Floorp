27f02c11a83c167406136934ba3e48bd651e90a3
David Parks <dparks@mozilla.com>
2018-05-29 10:49:24 -0700

    Bug 1462979 - Fix printf formatting in FunctionHook logging. r=jimm, a=RyanVM
    Use %ls instead of %S, which is now checked by an assertion.

--- dom/plugins/ipc/FunctionBrokerIPCUtils.h
+++ dom/plugins/ipc/FunctionBrokerIPCUtils.h
@@ -286,7 +286,7 @@ struct ParamTraits<OpenFileNameIPC>
 
   static void Log(const paramType& aParam, std::wstring* aLog)
   {
-    aLog->append(StringPrintf(L"[%S, %S, %S, %S]", aParam.mFilter.c_str(),
+    aLog->append(StringPrintf(L"[%ls, %ls, %ls, %ls]", aParam.mFilter.c_str(),
                               aParam.mCustomFilterIn.c_str(), aParam.mFile.c_str(),
                               aParam.mTitle.c_str()));
   }
@@ -320,7 +320,7 @@ struct ParamTraits<OpenFileNameRetIPC>
 
   static void Log(const paramType& aParam, std::wstring* aLog)
   {
-    aLog->append(StringPrintf(L"[%S, %S, %S, %d, %d]", aParam.mCustomFilterOut.c_str(),
+    aLog->append(StringPrintf(L"[%ls, %ls, %ls, %d, %d]", aParam.mCustomFilterOut.c_str(),
                               aParam.mFile.c_str(), aParam.mFileTitle.c_str(),
                               aParam.mFileOffset, aParam.mFileExtension));
   }
