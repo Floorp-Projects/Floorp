e4b61cc7d324acead32acd736973bb9fc745e769
Tom Schuster <evilpies@gmail.com>
2018-05-29 13:22:30 +0200

    Bug 1465039 - Update test262. r=anba

--- js/src/tests/test262/built-ins/Array/prototype/flat/array-like-objects.js
+++ js/src/tests/test262/built-ins/Array/prototype/flat/array-like-objects.js
@@ -1,11 +1,11 @@
 // Copyright (C) 2018 Shilpi Jain and Michael Ficarra. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-array.prototype.flatten
+esid: sec-array.prototype.flat
 description: >
     array-like objects can be flattened
 includes: [compareArray.js]
-features: [Array.prototype.flatten]
+features: [Array.prototype.flat]
 ---*/
 
 function getArgumentsObject() {
@@ -13,21 +13,21 @@ function getArgumentsObject() {
 }
 
 var a = getArgumentsObject([1], [2]);
-var actual = [].flatten.call(a);
+var actual = [].flat.call(a);
 assert.compareArray(actual, [1, 2], 'arguments objects');
 
 var a = {
   length: 1,
   0: [1],
 };
-var actual = [].flatten.call(a);
+var actual = [].flat.call(a);
 assert.compareArray(actual, [1], 'array-like objects');
 
 var a = {
   length: undefined,
   0: [1],
 };
-var actual = [].flatten.call(a);
+var actual = [].flat.call(a);
 assert.compareArray(actual, [], 'array-like objects; undefined length');
 
 reportCompare(0, 0);
--- js/src/tests/test262/built-ins/Array/prototype/flat/prop-desc.js
+++ js/src/tests/test262/built-ins/Array/prototype/flat/prop-desc.js
@@ -1,23 +1,23 @@
 // Copyright (C) 2018 Shilpi Jain and Michael Ficarra. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
-esid: sec-array.prototype.flatten
+esid: sec-array.prototype.flat
 es6id: 22.1.3
 description: Property type and descriptor.
 info: >
   17 ECMAScript Standard Built-in Objects
 includes: [propertyHelper.js]
-features: [Array.prototype.flatten]
+features: [Array.prototype.flat]
 ---*/
 
 assert.sameValue(
-  typeof Array.prototype.flatten,
+  typeof Array.prototype.flat,
   'function',
-  '`typeof Array.prototype.flatten` is `function`'
+  '`typeof Array.prototype.flat` is `function`'
 );
 
-verifyNotEnumerable(Array.prototype, 'flatten');
-verifyWritable(Array.prototype, 'flatten');
-verifyConfigurable(Array.prototype, 'flatten');
+verifyNotEnumerable(Array.prototype, 'flat');
+verifyWritable(Array.prototype, 'flat');
+verifyConfigurable(Array.prototype, 'flat');
 
 reportCompare(0, 0);
