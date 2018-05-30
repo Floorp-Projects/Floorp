b14ee48f19496a8cfddf54e301632fe8ec790b54
Julian Viereck <julian.viereck@gmail.com>
2018-05-30 16:50:37 +0200

    Bug 1124238 - The autofill placeholder should not override keywords. r=mak
    MozReview-Commit-ID: D6gBdkQxMrW

--- toolkit/components/places/tests/unifiedcomplete/test_keywords.js
+++ toolkit/components/places/tests/unifiedcomplete/test_keywords.js
@@ -5,9 +5,9 @@
 add_task(async function test_non_keyword() {
   info("Searching for non-keyworded entry should autoFill it");
   await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://mozilla.org/test/"),
+    uri: Services.io.newURI("http://mozilla.org/test/"),
   });
-  await addBookmark({ uri: NetUtil.newURI("http://mozilla.org/test/") });
+  await addBookmark({ uri: Services.io.newURI("http://mozilla.org/test/") });
   await check_autocomplete({
     search: "moz",
     autofilled: "mozilla.org/",
@@ -19,9 +19,9 @@ add_task(async function test_non_keyword() {
 add_task(async function test_keyword() {
   info("Searching for keyworded entry should not autoFill it");
   await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://mozilla.org/test/"),
+    uri: Services.io.newURI("http://mozilla.org/test/"),
   });
-  await addBookmark({ uri: NetUtil.newURI("http://mozilla.org/test/"), keyword: "moz" });
+  await addBookmark({ uri: Services.io.newURI("http://mozilla.org/test/"), keyword: "moz" });
   await check_autocomplete({
     search: "moz",
     autofilled: "moz",
@@ -33,9 +33,9 @@ add_task(async function test_keyword() {
 add_task(async function test_more_than_keyword() {
   info("Searching for more than keyworded entry should autoFill it");
   await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://mozilla.org/test/"),
+    uri: Services.io.newURI("http://mozilla.org/test/"),
   });
-  await addBookmark({ uri: NetUtil.newURI("http://mozilla.org/test/"), keyword: "moz" });
+  await addBookmark({ uri: Services.io.newURI("http://mozilla.org/test/"), keyword: "moz" });
   await check_autocomplete({
     search: "mozi",
     autofilled: "mozilla.org/",
@@ -47,9 +47,9 @@ add_task(async function test_more_than_keyword() {
 add_task(async function test_less_than_keyword() {
   info("Searching for less than keyworded entry should autoFill it");
   await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://mozilla.org/test/"),
+    uri: Services.io.newURI("http://mozilla.org/test/"),
   });
-  await addBookmark({ uri: NetUtil.newURI("http://mozilla.org/test/"), keyword: "moz" });
+  await addBookmark({ uri: Services.io.newURI("http://mozilla.org/test/"), keyword: "moz" });
   await check_autocomplete({
     search: "mo",
     autofilled: "mozilla.org/",
@@ -61,13 +61,53 @@ add_task(async function test_less_than_keyword() {
 add_task(async function test_keyword_casing() {
   info("Searching for keyworded entry is case-insensitive");
   await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://mozilla.org/test/"),
+    uri: Services.io.newURI("http://mozilla.org/test/"),
   });
-  await addBookmark({ uri: NetUtil.newURI("http://mozilla.org/test/"), keyword: "moz" });
+  await addBookmark({ uri: Services.io.newURI("http://mozilla.org/test/"), keyword: "moz" });
   await check_autocomplete({
     search: "MoZ",
     autofilled: "MoZ",
     completed: "MoZ"
   });
+  await cleanup();
+});
+
+add_task(async function test_less_then_equal_than_keyword_bug_1124238() {
+  info("Searching for less than keyworded entry should autoFill it");
+  await PlacesTestUtils.addVisits({
+    uri: Services.io.newURI("http://mozilla.org/test/"),
+    transition: TRANSITION_TYPED
+  });
+  await PlacesTestUtils.addVisits("http://mozilla.com/");
+  addBookmark({ uri: Services.io.newURI("http://mozilla.com/"), keyword: "moz" });
+
+  let input = await check_autocomplete({
+    search: "mo",
+    autofilled: "mozilla.org/",
+    completed: "http://mozilla.org/",
+  });
+
+  info(input.textValue);
+
+  // Emulate the input of an additional character. As the input matches a
+  // keyword, the completion should equal the keyword and not the URI as before.
+  input.textValue = "moz";
+  await check_autocomplete({
+    input,
+    search: "moz",
+    autofilled: "moz",
+    completed: "moz",
+  });
+
+  // Emulate the input of an additional character. The input doesn't match a
+  // keyword anymore, it should be autofilled.
+  input.textValue = "moz";
+  await check_autocomplete({
+    input,
+    search: "mozi",
+    autofilled: "mozilla.org/",
+    completed: "http://mozilla.org/",
+  });
+
   await cleanup();
 });
