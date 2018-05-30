6d661561ce85e7a32aa6f1b6aa73bf5f6a889f20
Andreas Tolfsen <ato@sny.no>
2018-05-30 18:29:35 +0100

    Bug 1465527 - Print stacks from parent tests in mach formatter. r=jgraham
    Parent tests may also have stacktraces and this patch prints and
    formats them the same way we do for subtests' stacks.
    
    MozReview-Commit-ID: 64gfPWuQnHd

--- testing/mozbase/mozlog/mozlog/formatters/machformatter.py
+++ testing/mozbase/mozlog/mozlog/formatters/machformatter.py
@@ -99,12 +99,14 @@ class MachFormatter(base.BaseFormatter):
         name = data.get("subtest", test)
         rv = "%s %s" % (self._format_expected(
             data["status"], data.get("expected", data["status"])), name)
-        if 'message' in data:
-            rv += " - %s" % data['message']
-
-        if 'stack' in data:
-            rv += "\n%s\n" % self.term.dim(data['stack'].strip('\n'))
+        if "message" in data:
+            rv += " - %s" % data["message"]
+        if "stack" in data:
+            rv += self._format_stack(data["stack"])
         return rv
+
+    def _format_stack(self, stack):
+        return "\n%s\n" % self.term.dim(stack.strip("\n"))
 
     def _format_suite_summary(self, suite, summary):
         count = summary['counts']
@@ -198,6 +200,8 @@ class MachFormatter(base.BaseFormatter):
                 message = unexpected[0].get("message", "")
                 if message:
                     rv += " - %s" % message
+                if "stack" in data:
+                    rv += self._format_stack(data["stack"])
             elif not self.verbose:
                 rv += "\n"
                 for d in unexpected:
