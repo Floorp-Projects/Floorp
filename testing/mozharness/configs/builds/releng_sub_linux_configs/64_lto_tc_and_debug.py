328af1b8c64b47accf5c36a7155822e77a86d80c
Tom Ritter <tom@mozilla.com>
2018-05-30 12:27:25 -0500

    Bug 1457482 Add an LTO Build Target r=glandium
    This build target doesn't have LTO enabled on it (yet)
    
    MozReview-Commit-ID: 56tAHMyvH7o

---
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_lto_tc_and_debug.py
@@ -0,0 +1,11 @@
+config = {
+    'default_actions': [
+        'clobber',
+        'build',
+        'check-test',
+        # 'update',
+    ],
+    'stage_platform': 'linux64-lto-debug',
+    'debug_build': True,
+    'mozconfig_variant': 'debug-lto',
+}
