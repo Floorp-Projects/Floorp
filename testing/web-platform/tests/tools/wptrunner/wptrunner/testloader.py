ea0aeacc865d4200152be187e5e34dad14406a0b
James Graham <james@hoppipolla.co.uk>
2018-05-31 15:53:32 +0100

    Bug 1354232 - Support a __dir__.ini file in the metadata root, r=maja_zf
    MozReview-Commit-ID: KACe0XQNsHl

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testloader.py
@@ -516,7 +516,7 @@ class TestLoader(object):
     def load_dir_metadata(self, test_manifest, metadata_path, test_path):
         rv = []
         path_parts = os.path.dirname(test_path).split(os.path.sep)
-        for i in xrange(1,len(path_parts) + 1):
+        for i in xrange(len(path_parts) + 1):
             path = os.path.join(metadata_path, os.path.sep.join(path_parts[:i]), "__dir__.ini")
             if path not in self.directory_manifests:
                 self.directory_manifests[path] = manifestexpected.get_dir_manifest(path,
