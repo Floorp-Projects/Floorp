76b6f154a256564266ecee585588c6ea89091ec5
Xidorn Quan <me@upsuper.org>
2018-05-31 14:11:01 +1000

    Bug 1465628 part 2 - Add InspectorUtils API for getting property preferences. r=emilio
    MozReview-Commit-ID: G3ZjnC4cTKD
    
    --HG--
    extra : rebase_source : c1795326b8ef0af46b624d07516edad032e95547

--- layout/inspector/InspectorUtils.cpp
+++ layout/inspector/InspectorUtils.cpp
@@ -399,6 +399,19 @@ InspectorUtils::GetCSSPropertyNames(GlobalObject& aGlobalObject,
 }
 
 /* static */ void
+InspectorUtils::GetCSSPropertyPrefs(GlobalObject& aGlobalObject,
+                                    nsTArray<PropertyPref>& aResult)
+{
+  for (const auto* src = nsCSSProps::kPropertyPrefTable;
+       src->mPropID != eCSSProperty_UNKNOWN; src++) {
+    PropertyPref& dest = *aResult.AppendElement();
+    const nsCString& name = nsCSSProps::GetStringValue(src->mPropID);
+    dest.mName.Assign(NS_ConvertASCIItoUTF16(name));
+    dest.mPref.AssignASCII(src->mPref);
+  }
+}
+
+/* static */ void
 InspectorUtils::GetSubpropertiesForCSSProperty(GlobalObject& aGlobal,
                                                const nsAString& aProperty,
                                                nsTArray<nsString>& aResult,
