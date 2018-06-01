d22865b094220ba796328cf12cbdc4737417cb27
Gurzau Raul <rgurzau@mozilla.com>
2018-06-02 02:42:52 +0300

    Backed out changeset 838c0ab9cbdb (bug 1464084) for Linux static bustage on a CLOSED TREE

--- python/mozbuild/mozbuild/action/generate_symbols_file.py
+++ python/mozbuild/mozbuild/action/generate_symbols_file.py
@@ -42,8 +42,6 @@ def generate_symbols_file(output, *args):
 
     symbols = [s.strip() for s in pp.out.getvalue().splitlines() if s.strip()]
 
-    libname, ext = os.path.splitext(os.path.basename(output.name))
-
     if buildconfig.substs['OS_TARGET'] == 'WINNT':
         # A def file is generated for MSVC link.exe that looks like the
         # following:
@@ -68,13 +66,14 @@ def generate_symbols_file(output, *args):
         # those platforms, and to DATA on Windows, so that the "DATA" part
         # is, in fact, part of the symbol name as far as the symbols variable
         # is concerned.
+        libname, ext = os.path.splitext(os.path.basename(output.name))
         assert ext == '.def'
         output.write('LIBRARY %s\nEXPORTS\n  %s\n'
                      % (libname, '\n  '.join(symbols)))
     elif buildconfig.substs['GCC_USE_GNU_LD']:
         # A linker version script is generated for GNU LD that looks like the
         # following:
-        # liblibrary.so {
+        # {
         # global:
         #   symbol1;
         #   symbol2;
@@ -82,8 +81,8 @@ def generate_symbols_file(output, *args):
         # local:
         #   *;
         # };
-        output.write('%s {\nglobal:\n  %s;\nlocal:\n  *;\n};'
-                     % (libname, ';\n  '.join(symbols)))
+        output.write('{\nglobal:\n  %s;\nlocal:\n  *;\n};'
+                     % ';\n  '.join(symbols))
     elif buildconfig.substs['OS_TARGET'] == 'Darwin':
         # A list of symbols is generated for Apple ld that simply lists all
         # symbols, with an underscore prefix.
