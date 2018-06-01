4311f61c78249c2908018b15546a13a3d352872f
Henrik Skupin <mail@hskupin.info>
2018-06-01 14:53:20 +0200

    Bug 1448792 - [marionette][wdspec] Make Element Send Keys fire change event for <input type=file>. r=ato
    Adds a wdspec test to ensure the latest WebDriver specification
    changes for https://github.com/w3c/webdriver/pull/1262.
    
    MozReview-Commit-ID: CQqq8VjBobU

--- testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
+++ testing/web-platform/tests/webdriver/tests/element_send_keys/form_controls.py
@@ -1,19 +1,6 @@
-import pytest
-
-from tests.support.asserts import assert_element_has_focus, assert_events_equal
+from tests.support.asserts import assert_element_has_focus
 from tests.support.inline import inline
 
-@pytest.fixture
-def tracked_events():
-    return [
-            "blur",
-            "change",
-            "focus",
-            "input",
-            "keydown",
-            "keypress",
-            "keyup",
-            ]
 
 def element_send_keys(session, element, text):
     return session.transport.send(
@@ -65,39 +52,3 @@ def test_textarea_append(session):
 
     element_send_keys(session, element, "c")
     assert element.property("value") == "abc"
-
-
-@pytest.mark.parametrize("tag", ["input", "textarea"])
-def test_events(session, add_event_listeners, tracked_events, tag):
-    expected_events = [
-        "focus",
-        "keydown",
-        "keypress",
-        "input",
-        "keyup",
-        "keydown",
-        "keypress",
-        "input",
-        "keyup",
-        "keydown",
-        "keypress",
-        "input",
-        "keyup",
-    ]
-
-    session.url = inline("<%s>" % tag)
-    element = session.find.css(tag, all=False)
-    add_event_listeners(element, tracked_events)
-
-    element_send_keys(session, element, "foo")
-    assert element.property("value") == "foo"
-    assert_events_equal(session, expected_events)
-
-
-@pytest.mark.parametrize("tag", ["input", "textarea"])
-def test_not_blurred(session, tag):
-    session.url = inline("<%s>" % tag)
-    element = session.find.css(tag, all=False)
-
-    element_send_keys(session, element, "")
-    assert_element_has_focus(element)
