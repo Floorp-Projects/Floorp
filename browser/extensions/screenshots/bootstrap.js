cc7c4e3b25b4d33ca94d3158eaa64935eee371eb
Tomislav Jovanovic <tomica@gmail.com>
2018-06-03 00:25:56 +0200

    Bug 1466349 - Part 2: Pass addonData to embedded Screenshots WE, add permissions. r=aswan, r=ianbicking, a=RyanVM
    MozReview-Commit-ID: GLwjp32sj2i
    
    --HG--
    extra : source : e04c7ccfd50e5877126c28aad60336b267189dc5

--- browser/extensions/screenshots/bootstrap.js
+++ browser/extensions/screenshots/bootstrap.js
@@ -121,9 +121,10 @@ const LibraryButton = {
 
 const APP_STARTUP = 1;
 const APP_SHUTDOWN = 2;
-let startupReason;
+let addonData, startupReason;
 
 function startup(data, reason) { // eslint-disable-line no-unused-vars
+  addonData = data;
   startupReason = reason;
   if (reason === APP_STARTUP) {
     appStartupObserver.register();
@@ -177,7 +178,7 @@ function handleStartup() {
 }
 
 function start(webExtension) {
-  return webExtension.startup(startupReason).then((api) => {
+  return webExtension.startup(startupReason, addonData).then((api) => {
     api.browser.runtime.onMessage.addListener(handleMessage);
     LibraryButton.init(webExtension);
     initPhotonPageAction(api, webExtension);
