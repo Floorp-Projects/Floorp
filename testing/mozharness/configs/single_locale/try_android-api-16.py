4f57b5b0069dc10ab1ec98f0bc4220d5e23bc11e
Justin Wood <Callek@gmail.com>
2018-06-03 21:06:48 -0400

    Bug 1466222 - Get rid of unused tools_repo config entry. r=catlee
    for "Cleanup l10n mozharness config files."
    
    Differential Revision: https://phabricator.services.mozilla.com/D1532

--- testing/mozharness/configs/single_locale/try_android-api-16.py
+++ testing/mozharness/configs/single_locale/try_android-api-16.py
@@ -13,7 +13,6 @@ config = {
     "locales_file": "%s/mobile/locales/l10n-changesets.json" % MOZILLA_DIR,
     "locales_dir": "mobile/android/locales",
     "ignore_locales": ["en-US"],
-    "tools_repo": "https://hg.mozilla.org/build/tools",
     "tooltool_config": {
         "manifest": "mobile/android/config/tooltool-manifests/android/releng.manifest",
         "output_dir": "%(abs_work_dir)s/" + MOZILLA_DIR,
