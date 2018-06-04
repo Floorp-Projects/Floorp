a5bcb1a345b6f52016558c076b146fcf08a10749
Justin Wood <Callek@gmail.com>
2018-06-03 21:09:45 -0400

    Bug 1466222 - Get rid of unused stage_platform config entry. r=catlee
    for "Cleanup l10n mozharness config files."
    
    Differential Revision: https://phabricator.services.mozilla.com/D1533
    
    --HG--
    extra : rebase_source : 1fd45494e49ab6fef3fdd1cf950ff395c3a0b092

--- testing/mozharness/configs/single_locale/tc_android-api-16.py
+++ testing/mozharness/configs/single_locale/tc_android-api-16.py
@@ -1,5 +1,4 @@
 config = {
-    "stage_platform": "android-api-16",
     "locales_file": "src/mobile/locales/l10n-changesets.json",
     "mozconfig": "src/mobile/android/config/mozconfigs/android-api-16/l10n-nightly",
     "tooltool_config": {
