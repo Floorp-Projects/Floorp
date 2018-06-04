322d7ed5cbf98ae50eb3908437bce7a08b5fac10
Cameron McCormack <cam@mcc.id.au>
2018-06-04 16:22:10 +1000

    Bug 1466448 - Part 2: Remove unused ReflowOutput::mFlags. r=mats

--- layout/mathml/nsMathMLmrootFrame.cpp
+++ layout/mathml/nsMathMLmrootFrame.cpp
@@ -187,8 +187,7 @@ nsMathMLmrootFrame::Reflow(nsPresContext*          aPresContext,
   nsIFrame* childFrame = mFrames.FirstChild();
   while (childFrame) {
     // ask our children to compute their bounding metrics
-    ReflowOutput childDesiredSize(aReflowInput,
-                                         aDesiredSize.mFlags);
+    ReflowOutput childDesiredSize(aReflowInput);
     WritingMode wm = childFrame->GetWritingMode();
     LogicalSize availSize = aReflowInput.ComputedSize(wm);
     availSize.BSize(wm) = NS_UNCONSTRAINEDSIZE;
