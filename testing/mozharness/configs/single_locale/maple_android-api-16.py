79a9955598d7ec374d9cea296ed658b83438c01a
Justin Wood <Callek@gmail.com>
2018-06-04 20:52:46 -0400

    Bug 1466222 - Cleanup handling of en_us_binary_url. r=catlee
    for "Cleanup l10n mozharness config files." (Batch 2)
    
    Differential Revision: https://phabricator.services.mozilla.com/D1549

--- testing/mozharness/configs/single_locale/maple_android-api-16.py
+++ testing/mozharness/configs/single_locale/maple_android-api-16.py
@@ -2,8 +2,6 @@ import os
 
 BRANCH = "maple"
 MOZILLA_DIR = BRANCH
-EN_US_BINARY_URL = "http://archive.mozilla.org/pub/" \
-                   "mobile/nightly/latest-maple-android-api-16/en-US"
 
 config = {
     "branch": BRANCH,
@@ -27,7 +25,7 @@ config = {
         # so ugly, bug 951238
         "LD_LIBRARY_PATH": "/lib:/tools/gcc-4.7.2-0moz1/lib:/tools/gcc-4.7.2-0moz1/lib64",
         "MOZ_OBJDIR": "obj-firefox",
-        "EN_US_BINARY_URL": os.environ.get("EN_US_BINARY_URL", EN_US_BINARY_URL),
+        "EN_US_BINARY_URL": os.environ["EN_US_BINARY_URL"],
         "MOZ_UPDATE_CHANNEL": "nightly-maple",
     },
     "upload_branch": "%s-android-api-16" % BRANCH,
--- testing/mozharness/configs/single_locale/mozilla-release_android-api-16.py
+++ testing/mozharness/configs/single_locale/mozilla-release_android-api-16.py
@@ -3,7 +3,6 @@ BRANCH = "mozilla-release"
 MOZ_UPDATE_CHANNEL = "release"
 MOZILLA_DIR = BRANCH
 OBJDIR = "obj-firefox"
-EN_US_BINARY_URL = None
 HG_SHARE_BASE_DIR = "/builds/hg-shared"
 
 config = {
@@ -29,7 +28,7 @@ config = {
     "repack_env": {
         # so ugly, bug 951238
         "LD_LIBRARY_PATH": "/lib:/tools/gcc-4.7.2-0moz1/lib:/tools/gcc-4.7.2-0moz1/lib64",
-        "EN_US_BINARY_URL": os.environ.get("EN_US_BINARY_URL", EN_US_BINARY_URL),
+        "EN_US_BINARY_URL": os.environ["EN_US_BINARY_URL"],
         "MOZ_OBJDIR": OBJDIR,
         "MOZ_UPDATE_CHANNEL": MOZ_UPDATE_CHANNEL,
     },
