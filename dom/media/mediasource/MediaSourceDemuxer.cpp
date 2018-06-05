5ae9999e57a05cecee672cf96de7692ead7c9e32
Jean-Yves Avenard <jyavenard@mozilla.com>
2018-06-05 13:48:59 +0200

    Bug 1466606 - Add diagnostic code. r=bryce
    Temporary diagnostic code to help determine what the problem might be.
    
    MozReview-Commit-ID: D5Dm32aZ1Ia

--- dom/media/mediasource/MediaSourceDemuxer.cpp
+++ dom/media/mediasource/MediaSourceDemuxer.cpp
@@ -464,7 +464,8 @@ MediaSourceTrackDemuxer::DoSeek(const TimeUnit& aTime)
     mManager->GetSample(mType,
                         TimeUnit::Zero(),
                         result);
-  MOZ_ASSERT(NS_SUCCEEDED(result) && sample);
+  mManager->AssertHasNextSampleIndex(mType);
+  MOZ_DIAGNOSTIC_ASSERT(NS_SUCCEEDED(result) && sample);
   mNextSample = Some(sample);
   mReset = false;
   {
@@ -505,6 +506,7 @@ MediaSourceTrackDemuxer::DoGetSamples(int32_t aNumSamples)
   if (mNextSample) {
     sample = mNextSample.ref();
     mNextSample.reset();
+    mManager->AssertHasNextSampleIndex(mType);
   } else {
     MediaResult result = NS_OK;
     sample = mManager->GetSample(mType, MediaSourceDemuxer::EOS_FUZZ, result);
@@ -518,6 +520,7 @@ MediaSourceTrackDemuxer::DoGetSamples(int32_t aNumSamples)
       }
       return SamplesPromise::CreateAndReject(result, __func__);
     }
+    mManager->AssertHasNextSampleIndex(mType);
   }
   RefPtr<SamplesHolder> samples = new SamplesHolder;
   samples->mSamples.AppendElement(sample);
