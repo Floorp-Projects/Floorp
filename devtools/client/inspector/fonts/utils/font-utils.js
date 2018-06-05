2dc2cc6e4fccfe51e169be7cdc7b357155929f75
Razvan Caliman <rcaliman@mozilla.com>
2018-06-05 13:59:38 +0200

    Bug 1465020 - Do not trim whitespace from variable font axis tag names. r=gl
    MozReview-Commit-ID: 6pdCAPhJHze

--- devtools/client/inspector/fonts/utils/font-utils.js
+++ devtools/client/inspector/fonts/utils/font-utils.js
@@ -67,9 +67,12 @@ module.exports = {
       .reduce((acc, pair) => {
         // Tags are always in quotes. Split by quote and filter excessive whitespace.
         pair = pair.split(/["']/).filter(part => part.trim() !== "");
-        const tag = pair[0].trim();
+        const tag = pair[0];
         const value = pair[1].trim();
-        acc[tag] = value;
+        // Axis tags shorter or longer than 4 characters are invalid. Whitespace is valid.
+        if (tag.length === 4) {
+          acc[tag] = value;
+        }
         return acc;
       }, {});
 
