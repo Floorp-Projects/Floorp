dd451a63fdf617da3972bd166bc58ff333af3fd7
Dorel Luca <dluca@mozilla.com>
2018-06-05 05:34:47 +0300

    Backed out 25 changesets (bug 1466222) as requested by the dev for it will break android nightly if merged to m-c
    Backed out changeset 86369008e22a (bug 1466222)
    Backed out changeset 8c0f4cb39d9e (bug 1466222)
    Backed out changeset c09c662c896b (bug 1466222)
    Backed out changeset 718fc2af5617 (bug 1466222)
    Backed out changeset 432dfe24f258 (bug 1466222)
    Backed out changeset 92ed011504c2 (bug 1466222)
    Backed out changeset 2a373427f708 (bug 1466222)
    Backed out changeset 61a6f45946d0 (bug 1466222)
    Backed out changeset d7ea3bf4e138 (bug 1466222)
    Backed out changeset b58c16c87f12 (bug 1466222)
    Backed out changeset 070e3b014309 (bug 1466222)
    Backed out changeset 9aa786375caf (bug 1466222)
    Backed out changeset 40292a1aabf8 (bug 1466222)
    Backed out changeset 7fc1c68ceeab (bug 1466222)
    Backed out changeset ed1a9704f0b2 (bug 1466222)
    Backed out changeset 616af8e0b5e3 (bug 1466222)
    Backed out changeset 6c75102894df (bug 1466222)
    Backed out changeset 79d7e88f2364 (bug 1466222)
    Backed out changeset 9611aead3e0e (bug 1466222)
    Backed out changeset 62b5f549fc15 (bug 1466222)
    Backed out changeset a184e835718f (bug 1466222)
    Backed out changeset fac69e74940e (bug 1466222)
    Backed out changeset 109a9044283a (bug 1466222)
    Backed out changeset 3a113595b666 (bug 1466222)
    Backed out changeset 784d3bfa1281 (bug 1466222)

--- testing/mozharness/configs/single_locale/jamun.py
+++ testing/mozharness/configs/single_locale/jamun.py
@@ -9,9 +9,25 @@ config = {
     # l10n
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
 
-    # mar - passed in environ from taskcluster
-    "mar_tools_url": os.environ["MAR_TOOLS_URL"],
+    # mar
+    "mar_tools_url": os.environ.get(
+        "MAR_TOOLS_URL",
+        # Default to fetching from ftp rather than setting an environ var
+        "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central/mar-tools/%(platform)s"
+    ),
 
+    # repositories
+    "repos": [{
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/build/tools",
+        "branch": "default",
+        "dest": "tools",
+    }, {
+        "vcs": "hg",
+        "repo": "https://hg.mozilla.org/projects/jamun",
+        "revision": "%(revision)s",
+        "dest": "jamun",
+    }],
     # purge options
     'is_automation': True,
 }
