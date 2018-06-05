5a22dd980758aee6bf90e67d0d8ee9c208374f3a
Nazım Can Altınova <canaltinova@gmail.com>
2018-06-05 15:17:08 +0200

    Bug 1451289 - Part 8: Merge ServoSupportsRule and CSSSupportsRule r=emilio
    MozReview-Commit-ID: HX6NJNGJi0p

--- layout/style/CSSSupportsRule.cpp
+++ layout/style/CSSSupportsRule.cpp
@@ -6,12 +6,68 @@
 
 #include "mozilla/dom/CSSSupportsRule.h"
 
+#include "mozilla/css/GroupRule.h"
 #include "mozilla/dom/CSSSupportsRuleBinding.h"
+#include "mozilla/ServoBindings.h"
 
 using namespace mozilla::css;
 
 namespace mozilla {
 namespace dom {
+
+CSSSupportsRule::CSSSupportsRule(RefPtr<RawServoSupportsRule> aRawRule,
+                                 uint32_t aLine, uint32_t aColumn)
+  : css::ConditionRule(Servo_SupportsRule_GetRules(aRawRule).Consume(),
+                       aLine, aColumn)
+  , mRawRule(std::move(aRawRule))
+{
+}
+
+NS_IMPL_ADDREF_INHERITED(CSSSupportsRule, ConditionRule)
+NS_IMPL_RELEASE_INHERITED(CSSSupportsRule, ConditionRule)
+
+// QueryInterface implementation for SupportsRule
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CSSSupportsRule)
+NS_INTERFACE_MAP_END_INHERITING(ConditionRule)
+
+#ifdef DEBUG
+/* virtual */ void
+CSSSupportsRule::List(FILE* out, int32_t aIndent) const
+{
+  nsAutoCString str;
+  for (int32_t i = 0; i < aIndent; i++) {
+    str.AppendLiteral("  ");
+  }
+  Servo_SupportsRule_Debug(mRawRule, &str);
+  fprintf_stderr(out, "%s\n", str.get());
+}
+#endif
+
+void
+CSSSupportsRule::GetConditionText(nsAString& aConditionText)
+{
+  Servo_SupportsRule_GetConditionText(mRawRule, &aConditionText);
+}
+
+void
+CSSSupportsRule::SetConditionText(const nsAString& aConditionText,
+                                  ErrorResult& aRv)
+{
+  aRv.Throw(NS_ERROR_NOT_IMPLEMENTED);
+}
+
+/* virtual */ void
+CSSSupportsRule::GetCssText(nsAString& aCssText) const
+{
+  Servo_SupportsRule_GetCssText(mRawRule, &aCssText);
+}
+
+/* virtual */ size_t
+CSSSupportsRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
+{
+  // TODO Implement this!
+  return aMallocSizeOf(this);
+}
 
 /* virtual */ JSObject*
 CSSSupportsRule::WrapObject(JSContext* aCx,
