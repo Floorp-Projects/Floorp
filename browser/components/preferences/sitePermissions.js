b4f37d3a663f0ffdda1416d9ce9c9cc51f1de823
Kanika Saini <>
2018-06-06 15:43:26 +0530

    Bug 1467062 - Permissions Policy added to configure camera, microphone, location and notifications r=Felipe
    MozReview-Commit-ID: IqpEboxHjjt

--- browser/components/preferences/sitePermissions.js
+++ browser/components/preferences/sitePermissions.js
@@ -107,6 +107,10 @@ var gSitePermissionsManager = {
       this._checkbox.checked = false;
     }
 
+    if (Services.prefs.prefIsLocked(this._defaultPermissionStatePrefName)) {
+      this._checkbox.disabled = true;
+    }
+
     this._loadPermissions();
     await this.buildPermissionsList();
 
