d99eee373b82b92c5447cc4e9ea4c6eb12b7ce54
Andreas Tolfsen <ato@sny.no>
2018-06-06 14:40:05 +0100

    Bug 1464469 - Use MarionetteLog throughout. r=whimboo
    Replace direct invocations of Log.jsm with the new Marionette-specific
    logger.
    
    The patch also moves the Log.get() call to a lazy getter.
    
    MozReview-Commit-ID: H1HoAgL2Cvs

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -5,7 +5,6 @@
 "use strict";
 /* global XPCNativeWrapper */
 
-ChromeUtils.import("resource://gre/modules/Log.jsm");
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 
@@ -47,6 +46,7 @@ const {pprint} = ChromeUtils.import("chrome://marionette/content/format.js", {})
 ChromeUtils.import("chrome://marionette/content/interaction.js");
 ChromeUtils.import("chrome://marionette/content/l10n.js");
 ChromeUtils.import("chrome://marionette/content/legacyaction.js");
+const {Log} = ChromeUtils.import("chrome://marionette/content/log.js", {});
 ChromeUtils.import("chrome://marionette/content/modal.js");
 const {MarionettePrefs} = ChromeUtils.import("chrome://marionette/content/prefs.js", {});
 ChromeUtils.import("chrome://marionette/content/proxy.js");
@@ -57,6 +57,7 @@ const {
   TimedPromise,
 } = ChromeUtils.import("chrome://marionette/content/sync.js", {});
 
+XPCOMUtils.defineLazyGetter(this, "logger", Log.get);
 XPCOMUtils.defineLazyGlobalGetters(this, ["URL"]);
 
 this.EXPORTED_SYMBOLS = ["GeckoDriver"];
@@ -76,7 +77,6 @@ const SUPPORTED_STRATEGIES = new Set([
   element.Strategy.AnonAttribute,
 ]);
 
-const logger = Log.repository.getLogger("Marionette");
 const globalMessageManager = Services.mm;
 
 /**
