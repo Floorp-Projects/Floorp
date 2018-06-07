8c5dff3ef1451cef2e0ae6d1abfd8acba228348c
Coroiu Cristina <ccoroiu@mozilla.com>
2018-06-07 14:37:40 +0300

    Backed out 15 changesets (bug 1466222) for breaking android nightly a=backout
    Backed out changeset 7bb7694f5d5d (bug 1466222)
    Backed out changeset 185a82778d94 (bug 1466222)
    Backed out changeset 18fe0eb0545c (bug 1466222)
    Backed out changeset bb28c2d9d5f5 (bug 1466222)
    Backed out changeset 38d7f885284e (bug 1466222)
    Backed out changeset 0970518ff4bd (bug 1466222)
    Backed out changeset c71a6be37cfc (bug 1466222)
    Backed out changeset 507fe13a3867 (bug 1466222)
    Backed out changeset 3c9e951bdc9e (bug 1466222)
    Backed out changeset 259365b927a7 (bug 1466222)
    Backed out changeset 705e5e28693b (bug 1466222)
    Backed out changeset 7b9f759f30fc (bug 1466222)
    Backed out changeset b50f13740021 (bug 1466222)
    Backed out changeset 7cf0da1e476b (bug 1466222)
    Backed out changeset 9250705be918 (bug 1466222)

--- testing/mozharness/configs/single_locale/mozilla-esr60.py
+++ testing/mozharness/configs/single_locale/mozilla-esr60.py
@@ -2,9 +2,9 @@ import os
 
 config = {
     "nightly_build": True,
-    "branch": "mozilla-release",
-    "en_us_binary_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-release/",
-    "update_channel": "release",
+    "branch": "mozilla-esr60",
+    "en_us_binary_url": "http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-esr60/",
+    "update_channel": "esr",
 
     # l10n
     "hg_l10n_base": "https://hg.mozilla.org/l10n-central",
--- testing/mozharness/configs/single_locale/win32_devedition.py
+++ testing/mozharness/configs/single_locale/win32_devedition.py
@@ -1,4 +1,5 @@
 config = {
+    "platform": "win32",
     "app_name": "browser",
     "mozconfig_platform": "win32",
     "mozconfig_variant": "l10n-mozconfig-devedition",
@@ -12,10 +13,12 @@ config = {
     # l10n
     "ignore_locales": ["en-US", "ja-JP-mac"],
     "l10n_dir": "l10n",
+    "locales_file": "src/browser/locales/all-locales",
     "locales_dir": "browser/locales",
     "hg_l10n_tag": "default",
 
     # MAR
+    "application_ini": "application.ini",
     "local_mar_tool_dir": "dist\\host\\bin",
     "mar": "mar.exe",
     "mbsdiff": "mbsdiff.exe",
