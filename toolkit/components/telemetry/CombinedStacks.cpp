67f23cdd720e36b47aa826b8daccbe4ab3af48e4
Carl Corcoran <ccorcoran@mozilla.com>
2018-06-08 12:49:32 +0200

    Bug 1467734: Add ability to remove frames from CombinedStacks; r=Dexter
    MozReview-Commit-ID: EudrP5G3iaJ

--- toolkit/components/telemetry/CombinedStacks.cpp
+++ toolkit/components/telemetry/CombinedStacks.cpp
@@ -36,7 +36,8 @@ CombinedStacks::GetModule(unsigned aIndex) const {
 size_t
 CombinedStacks::AddStack(const Telemetry::ProcessedStack& aStack) {
   // Advance the indices of the circular queue holding the stacks.
-  size_t index = mNextIndex++ % mMaxStacksCount;
+  mNextIndex = (mNextIndex + 1) % mMaxStacksCount;
+  size_t index = mNextIndex;
   // Grow the vector up to the maximum size, if needed.
   if (mStacks.size() < mMaxStacksCount) {
     mStacks.resize(mStacks.size() + 1);
@@ -95,6 +96,25 @@ CombinedStacks::SizeOfExcludingThis() const {
     n += s.capacity() * sizeof(Telemetry::ProcessedStack::Frame);
   }
   return n;
+}
+
+void
+CombinedStacks::RemoveStack(unsigned aIndex) {
+  MOZ_ASSERT(aIndex < mStacks.size());
+
+  mStacks.erase(mStacks.begin() + aIndex);
+
+  if (aIndex < mNextIndex) {
+    if (mNextIndex == 0) {
+      mNextIndex = mStacks.size();
+    } else {
+      mNextIndex--;
+    }
+  }
+
+  if (mNextIndex > mStacks.size()) {
+    mNextIndex = mStacks.size();
+  }
 }
 
 #if defined(MOZ_GECKO_PROFILER)
