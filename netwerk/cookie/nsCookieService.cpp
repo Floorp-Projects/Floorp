d987da8b600398b945a74b37b3be06c1719fdf0a
Rob Wu <rob@robwu.nl>
2018-06-09 01:21:02 +0200

    Bug 1351663 - Ensure that OriginAttributes is initialized when nsCookieService::Add receives a SameSite parameter r=valentin
    InitializeOriginAttributes takes aArgc and only initializes the
    parameter when aArgc is 1. nsCookieService::Add takes another optional
    parameter, namely aSameSite. If a caller sets this SameSite flag, then
    InitializeOriginAttributes would skip the initialization of the
    OriginAttributes.
    
    This was caught by a private browsing test in
    toolkit/components/extensions/test/mochitest/test_ext_cookies.html
    (after I added support for SameSite flag in the extension API)
    
    MozReview-Commit-ID: HLfte7x1X7T
    
    --HG--
    extra : rebase_source : 1feb84ceca157d8c5ec8575c6336cc606c3504fe

--- netwerk/cookie/nsCookieService.cpp
+++ netwerk/cookie/nsCookieService.cpp
@@ -2559,13 +2559,13 @@ nsCookieService::Add(const nsACString &aHost,
                      JSContext*        aCx,
                      uint8_t           aArgc)
 {
-  MOZ_ASSERT(aArgc == 0 || aArgc == 1);
+  MOZ_ASSERT(aArgc == 0 || aArgc == 1 || aArgc == 2);
 
   OriginAttributes attrs;
   nsresult rv = InitializeOriginAttributes(&attrs,
                                            aOriginAttributes,
                                            aCx,
-                                           aArgc,
+                                           aArgc == 0 ? 0 : 1,
                                            u"nsICookieManager.add()",
                                            u"2");
   NS_ENSURE_SUCCESS(rv, rv);
