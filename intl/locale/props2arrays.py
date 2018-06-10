91907444b3ae0d1c5cd9375fe1b86857a9688877
Sylvestre Ledru <sledru@mozilla.com>
2018-06-10 14:25:47 +0200

    Bug 1468273 - Fix flake8/pep8 in intl/ r=froydnj
    MozReview-Commit-ID: IDqjd7nBRS
    
    --HG--
    extra : rebase_source : 1679528701e0cab59fb827224f19eced9c8fda00

--- intl/locale/props2arrays.py
+++ intl/locale/props2arrays.py
@@ -2,26 +2,24 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import sys
 
 def main(header, propFile):
-  mappings = {}
+    mappings = {}
 
-  with open(propFile, 'r') as f:
-    for line in f:
-      line = line.strip()
-      if not line.startswith('#'):
-        parts = line.split("=", 1)
-        if len(parts) == 2 and len(parts[0]) > 0:
-          mappings[parts[0].strip()] = parts[1].strip()
- 
-  keys = mappings.keys()
-  keys.sort()
+    with open(propFile, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line.startswith('#'):
+                parts = line.split("=", 1)
+                if len(parts) == 2 and len(parts[0]) > 0:
+                    mappings[parts[0].strip()] = parts[1].strip()
 
-  header.write("// This is a generated file. Please do not edit.\n")
-  header.write("// Please edit the corresponding .properties file instead.\n")
+    keys = mappings.keys()
+    keys.sort()
 
-  entries = ['{ "%s", "%s", %d }'
-             % (key, mappings[key], len(mappings[key])) for key in keys]
-  header.write(',\n'.join(entries) + '\n')
+    header.write("// This is a generated file. Please do not edit.\n")
+    header.write("// Please edit the corresponding .properties file instead.\n")
 
+    entries = ['{ "%s", "%s", %d }'
+               % (key, mappings[key], len(mappings[key])) for key in keys]
+    header.write(',\n'.join(entries) + '\n')
