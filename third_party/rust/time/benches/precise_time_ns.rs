ecd221fa64185a11bc8cd1e838b4c87a1ee69283
Andreas Tolfsen <ato@sny.no>
2018-06-14 12:58:45 -0700

    Bug 1441204 - Upgrade time crate from 0.1.38 to 0.1.40. r=maja_zf
    MozReview-Commit-ID: 3bdeeSwyE8Q
    
    --HG--
    extra : rebase_source : b3d57f59cc40534f9aee57140e759e435c10ee43

--- third_party/rust/time/benches/precise_time_ns.rs
+++ third_party/rust/time/benches/precise_time_ns.rs
@@ -7,5 +7,8 @@ use test::Bencher;
 
 #[bench]
 fn bench_precise_time_ns(b: &mut Bencher) {
-    b.iter(|| time::precise_time_ns())
+    b.iter(|| {
+        time::precise_time_ns();
+        time::precise_time_ns();
+    });
 }
