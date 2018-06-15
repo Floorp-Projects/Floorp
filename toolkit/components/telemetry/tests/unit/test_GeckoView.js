d906af1226ba5aaa0edb1a509b27b033b58a8235
Jan-Erik Rediger <jrediger@mozilla.com>
2018-06-15 09:49:02 -0700

    Bug 1466490 - Extract common test functionality into a header. r=Dexter
    MozReview-Commit-ID: 81Gj0FyCBqG

--- toolkit/components/telemetry/tests/unit/test_GeckoView.js
+++ toolkit/components/telemetry/tests/unit/test_GeckoView.js
@@ -7,59 +7,6 @@ ChromeUtils.import("resource://gre/modules/PromiseUtils.jsm", this);
 ChromeUtils.import("resource://gre/modules/Services.jsm", this);
 ChromeUtils.import("resource://testing-common/ContentTaskUtils.jsm", this);
 
-const Telemetry = Services.telemetry;
-const TelemetryGeckoView = Cc["@mozilla.org/telemetry/geckoview-testing;1"]
-                             .createInstance(Ci.nsITelemetryGeckoViewTesting);
-
-/**
- * Run a file in the content process.
- * @param aFileName - The file to execute in the content process.
- * @return {Promise} A promise resolved after the execution in the other process
- *         finishes.
- */
-async function run_in_child(aFileName) {
-  const PREF_GECKOVIEW_MODE = "toolkit.telemetry.isGeckoViewMode";
-  // We don't ship GeckoViewTelemetryController.jsm outside of Android. If
-  // |toolkit.telemetry.isGeckoViewMode| is true, this makes Gecko crash on
-  // other platforms because ContentProcessSingleton.js requires it. Work
-  // around this by temporarily setting the pref to false.
-  const currentValue = Services.prefs.getBoolPref(PREF_GECKOVIEW_MODE, false);
-  Services.prefs.setBoolPref(PREF_GECKOVIEW_MODE, false);
-  await run_test_in_child(aFileName);
-  Services.prefs.setBoolPref(PREF_GECKOVIEW_MODE, currentValue);
-}
-
-/**
- * Builds a promise to wait for the GeckoView data loading to finish.
- * @return {Promise} A promise resolved when the data loading finishes.
- */
-function waitGeckoViewLoadComplete() {
-  return new Promise(resolve => {
-    Services.obs.addObserver(function observe() {
-      Services.obs.removeObserver(observe, "internal-telemetry-geckoview-load-complete");
-      resolve();
-    }, "internal-telemetry-geckoview-load-complete");
-  });
-}
-
-/**
- * This function waits until the desired histogram is reported into the
- * snapshot of the relevant process.
- * @param aHistogramName - The name of the histogram to look for.
- * @param aProcessName - The name of the process to look in.
- * @param aKeyed - Whether or not to look in keyed snapshots.
- */
-async function waitForSnapshotData(aHistogramName, aProcessName, aKeyed) {
-  await ContentTaskUtils.waitForCondition(() => {
-    const data = aKeyed
-      ? Telemetry.snapshotKeyedHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN, false)
-      : Telemetry.snapshotHistograms(Ci.nsITelemetry.DATASET_RELEASE_CHANNEL_OPTIN, false);
-
-    return (aProcessName in data)
-           && (aHistogramName in data[aProcessName]);
-  });
-}
-
 add_task(async function setup() {
   // Init the profile.
   let profileDir = do_get_profile(true);
@@ -90,8 +37,8 @@ add_task(async function test_persistContentHistograms() {
   await run_in_child("test_GeckoView_content_histograms.js");
 
   // Wait for the data to be collected by the parent process.
-  await waitForSnapshotData("TELEMETRY_TEST_MULTIPRODUCT", "content", false /* aKeyed */);
-  await waitForSnapshotData("TELEMETRY_TEST_KEYED_COUNT", "content", true /* aKeyed */);
+  await waitForHistogramSnapshotData("TELEMETRY_TEST_MULTIPRODUCT", "content", false /* aKeyed */);
+  await waitForHistogramSnapshotData("TELEMETRY_TEST_KEYED_COUNT", "content", true /* aKeyed */);
 
   // Force persisting the measurements to file.
   TelemetryGeckoView.forcePersist();
