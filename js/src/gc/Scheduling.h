f02c9e85aa4f3d4b3833fd8d1a91d4341f65f596
Wander Lairson Costa <wcosta@mozilla.com>
2018-06-17 08:20:10 -0700

    Bug 1465505: Replace PRMJ_Now() by mozilla::TimeStamp r=jonco
    Notice as TimeStamp is not an integral type, it can't be wrapped by
    mozilla::Atomic. There two uses of it, which seems to not harm the code,
    but I am not entirily sure.
    
    MozReview-Commit-ID: 3A1HL3L3tkW

--- js/src/gc/Scheduling.h
+++ js/src/gc/Scheduling.h
@@ -387,9 +387,9 @@ class GCSchedulingTunables
      * JSGC_HIGH_FREQUENCY_TIME_LIMIT
      *
      * We enter high-frequency mode if we GC a twice within this many
-     * microseconds. This value is stored directly in microseconds.
+     * microseconds.
      */
-    MainThreadData<uint64_t> highFrequencyThresholdUsec_;
+    MainThreadData<mozilla::TimeDuration> highFrequencyThresholdUsec_;
 
     /*
      * JSGC_HIGH_FREQUENCY_LOW_LIMIT
@@ -448,7 +448,7 @@ class GCSchedulingTunables
     double allocThresholdFactorAvoidInterrupt() const { return allocThresholdFactorAvoidInterrupt_; }
     size_t zoneAllocDelayBytes() const { return zoneAllocDelayBytes_; }
     bool isDynamicHeapGrowthEnabled() const { return dynamicHeapGrowthEnabled_; }
-    uint64_t highFrequencyThresholdUsec() const { return highFrequencyThresholdUsec_; }
+    const mozilla::TimeDuration &highFrequencyThresholdUsec() const { return highFrequencyThresholdUsec_; }
     uint64_t highFrequencyLowLimitBytes() const { return highFrequencyLowLimitBytes_; }
     uint64_t highFrequencyHighLimitBytes() const { return highFrequencyHighLimitBytes_; }
     double highFrequencyHeapGrowthMax() const { return highFrequencyHeapGrowthMax_; }
@@ -493,10 +493,10 @@ class GCSchedulingState
 
     bool inHighFrequencyGCMode() const { return inHighFrequencyGCMode_; }
 
-    void updateHighFrequencyMode(uint64_t lastGCTime, uint64_t currentTime,
+    void updateHighFrequencyMode(const mozilla::TimeStamp &lastGCTime, const mozilla::TimeStamp &currentTime,
                                  const GCSchedulingTunables& tunables) {
         inHighFrequencyGCMode_ =
-            tunables.isDynamicHeapGrowthEnabled() && lastGCTime &&
+            tunables.isDynamicHeapGrowthEnabled() && !lastGCTime.IsNull() &&
             lastGCTime + tunables.highFrequencyThresholdUsec() > currentTime;
     }
 };
