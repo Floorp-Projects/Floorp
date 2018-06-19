9c36800bd7c71b6faf3a6b5f7e295d817ed83200
catalin.badea392@gmail.com <catalin.badea392@gmail.com>
2018-06-19 12:21:18 +0300

    Bug 1469385 - Remove InsertChildAt_Deprecated and RemoveChildAt_Deprecated, r=bz,smaug

--- dom/html/HTMLSelectElement.h
+++ dom/html/HTMLSelectElement.h
@@ -247,9 +247,6 @@ public:
   virtual bool IsHTMLFocusable(bool aWithMouse, bool* aIsFocusable, int32_t* aTabIndex) override;
   virtual nsresult InsertChildBefore(nsIContent* aKid, nsIContent* aBeforeThis,
                                      bool aNotify) override;
-  virtual nsresult InsertChildAt_Deprecated(nsIContent* aKid, uint32_t aIndex,
-                                            bool aNotify) override;
-  virtual void RemoveChildAt_Deprecated(uint32_t aIndex, bool aNotify) override;
   virtual void RemoveChildNode(nsIContent* aKid, bool aNotify) override;
 
   // Overriden nsIFormControl methods
