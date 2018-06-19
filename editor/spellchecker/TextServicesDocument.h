51e4d2154d6fa5a5840beda6d6205fa16a9a5434
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2018-06-19 16:00:35 +0900

    Bug 1421510 - Part 2. Use nsComposeTxtSrvFilter directly. r=masayuki
    nsITextServicesFilter is builtin class, so we can store nsComposeTxtSrvFilter
    instead of nsITextServicesFilter.
    
    MozReview-Commit-ID: ErZQwWC0Wjx
    
    --HG--
    extra : rebase_source : 7b327a0dbdf12e2e085d109e66e5140ac6d18773

--- editor/spellchecker/TextServicesDocument.h
+++ editor/spellchecker/TextServicesDocument.h
@@ -14,12 +14,12 @@
 #include "nsTArray.h"
 #include "nscore.h"
 
+class nsComposeTxtSrvFilter;
 class nsIContent;
 class nsIContentIterator;
 class nsIEditor;
 class nsINode;
 class nsISelectionController;
-class nsITextServicesFilter;
 class nsRange;
 
 namespace mozilla {
@@ -58,7 +58,7 @@ private:
   nsCOMPtr<nsIContent> mNextTextBlock;
   nsTArray<OffsetEntry*> mOffsetTable;
   RefPtr<nsRange> mExtent;
-  nsCOMPtr<nsITextServicesFilter> mTxtSvcFilter;
+  RefPtr<nsComposeTxtSrvFilter> mTxtSvcFilter;
 
   int32_t mSelStartIndex;
   int32_t mSelStartOffset;
@@ -110,7 +110,7 @@ public:
    * @param aFilter             The filter to be used while iterating over
    *                            content.
    */
-  nsresult SetFilter(nsITextServicesFilter* aFilter);
+  nsresult SetFilter(nsComposeTxtSrvFilter* aFilter);
 
   /**
    * Returns the text in the current text block.
