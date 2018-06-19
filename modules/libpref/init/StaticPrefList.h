aaf7e6d9c3d4086e98c361f35dae931712944ad5
Ben Kelly <ben@wanderview.com>
2018-06-19 11:26:25 -0400

    Bug 1469024 Define service worker parent intercept pref in StaticPrefList and read it once at startup. r=asuth

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -105,6 +105,11 @@ VARCACHE_PREF(
   bool, true
 )
 
+// If true. then the service worker interception and the ServiceWorkerManager
+// will live in the parent process.  This only takes effect on browser start.
+// Note, this is not currently safe to use for normal browsing yet.
+PREF("dom.serviceWorkers.parent_intercept", bool, false)
+
 //---------------------------------------------------------------------------
 // Full-screen prefs
 //---------------------------------------------------------------------------
