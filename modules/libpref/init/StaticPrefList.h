7eb39dc14ed56beb3b350b6e4cd524c36cd096f5
Tarek Ziadé <tarek@mozilla.com>
2018-06-19 16:14:06 +0200

    Bug 1464571 - fixes DOM Worker performance counters - r=baku,froydnj
    Now uses StaticPrefs instead of DOMPrefs, and how we count dispatches for Workers.
    
    MozReview-Commit-ID: DTumwcI5bG

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -105,6 +105,12 @@ VARCACHE_PREF(
   bool, true
 )
 
+VARCACHE_PREF(
+  "dom.performance.enable_scheduler_timing",
+  dom_performance_enable_scheduler_timing,
+  RelaxedAtomicBool, false
+)
+
 //---------------------------------------------------------------------------
 // Full-screen prefs
 //---------------------------------------------------------------------------
