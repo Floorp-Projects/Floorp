f97f15a5a7d6464a15fdd1020683e7f70d609ec1
Greg Mierzwinski <gmierz1@live.ca>
2018-06-19 16:09:16 -0400

    Bug 1472575 - Add delay to baseline coverage tests. r=jmaher
    This patch adds a 30 second delay to the baseline coverage tests to help stabilize their code coverage results over time.
    
    MozReview-Commit-ID: C3StSX7RdM9

--- testing/mochitest/baselinecoverage/browser_chrome/browser_baselinecoverage.js
+++ testing/mochitest/baselinecoverage/browser_chrome/browser_baselinecoverage.js
@@ -1,8 +1,13 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
+/* eslint-disable mozilla/no-arbitrary-setTimeout */
 
 "use strict";
 
+const { setTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm", {});
+
 add_task(async function() {
+  requestLongerTimeout(2);
   ok(true, "Collecting baseline coverage for javascript (.js) file types.");
+  await new Promise((c) => setTimeout(c, 30 * 1000));
 });
