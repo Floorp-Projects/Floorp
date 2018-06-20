f514d23b51587f464a4a3b8540264cd5926c15c2
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-06-20 22:41:27 +0100

    Bug 1470002 - remove pagehide message passing from browser code, r=mixedpuppy
    MozReview-Commit-ID: 18hsSwHsWzz

--- browser/base/content/content.js
+++ browser/base/content/content.js
@@ -341,13 +341,6 @@ addEventListener("pageshow", function(event) {
     });
   }
 });
-addEventListener("pagehide", function(event) {
-  if (event.target == content.document) {
-    sendAsyncMessage("PageVisibility:Hide", {
-      persisted: event.persisted,
-    });
-  }
-});
 
 var PageMetadataMessenger = {
   init() {
