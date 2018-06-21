30be205426b4158edee8b4c6c761f033934d1982
Gabriele Svelto <gsvelto@mozilla.com>
2018-06-22 00:35:08 +0200

    Bug 1469914 - Prevent the HAL from registering duplicate observers; r=froydnj
    This also replaces the custom logic in ObserverList with an nsTObserverArray
    which has all the necessary logic for stable iteration over a potentially
    changing list of items. Unused dependencies were also removed.
    
    --HG--
    extra : source : 1a83516d2ee5939052c5fb226b81563a0d114ff9

--- hal/Hal.cpp
+++ hal/Hal.cpp
@@ -20,10 +20,7 @@
 #include "nsJSUtils.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/Observer.h"
-#include "mozilla/Services.h"
-#include "mozilla/StaticPtr.h"
 #include "mozilla/dom/ContentChild.h"
-#include "mozilla/dom/ContentParent.h"
 #include "mozilla/dom/ScreenOrientation.h"
 #include "WindowIdentifier.h"
 
@@ -183,43 +180,28 @@ class ObserversManager
 {
 public:
   void AddObserver(Observer<InfoType>* aObserver) {
-    if (!mObservers) {
-      mObservers = new mozilla::ObserverList<InfoType>();
-    }
+    mObservers.AddObserver(aObserver);
 
-    mObservers->AddObserver(aObserver);
-
-    if (mObservers->Length() == 1) {
+    if (mObservers.Length() == 1) {
       EnableNotifications();
     }
   }
 
   void RemoveObserver(Observer<InfoType>* aObserver) {
-    bool removed = mObservers && mObservers->RemoveObserver(aObserver);
+    bool removed = mObservers.RemoveObserver(aObserver);
     if (!removed) {
       return;
     }
 
-    if (mObservers->Length() == 0) {
+    if (mObservers.Length() == 0) {
       DisableNotifications();
 
       OnNotificationsDisabled();
-
-      delete mObservers;
-      mObservers = nullptr;
     }
   }
 
   void BroadcastInformation(const InfoType& aInfo) {
-    // It is possible for mObservers to be nullptr here on some platforms,
-    // because a call to BroadcastInformation gets queued up asynchronously
-    // while RemoveObserver is running (and before the notifications are
-    // disabled). The queued call can then get run after mObservers has
-    // been nulled out. See bug 757025.
-    if (!mObservers) {
-      return;
-    }
-    mObservers->Broadcast(aInfo);
+    mObservers.Broadcast(aInfo);
   }
 
 protected:
@@ -228,7 +210,7 @@ protected:
   virtual void OnNotificationsDisabled() {}
 
 private:
-  mozilla::ObserverList<InfoType>* mObservers;
+  mozilla::ObserverList<InfoType> mObservers;
 };
 
 template <class InfoType>
