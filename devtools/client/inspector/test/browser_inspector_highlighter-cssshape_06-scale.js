818d660315213a496149eca7320915010433f8d6
Razvan Caliman <rcaliman@mozilla.com>
2018-06-21 16:26:00 +0200

    Bug 1466225 - (Part 2) Split scale and transform shape path editor test to prevent exceeding max test run time. r=pbro
    MozReview-Commit-ID: CT0bGAWmU36
    
    --HG--
    extra : rebase_source : 784d8cd695a1c1543b25976fd81db2f75adf666e

--- devtools/client/inspector/test/browser_inspector_highlighter-cssshape_06-scale.js
+++ devtools/client/inspector/test/browser_inspector_highlighter-cssshape_06-scale.js
@@ -18,7 +18,7 @@ add_task(async function() {
   const highlighters = view.highlighters;
   const config = { inspector, view, highlighters, testActor, helper };
 
-  await testTranslate(config);
+  await testScale(config);
 });
 
 async function setup(config) {
@@ -33,7 +33,7 @@ async function teardown(config) {
   await toggleShapesHighlighter(view, selector, property, false);
 }
 
-async function testTranslate(config) {
+async function testScale(config) {
   const { testActor, helper, highlighters } = config;
   const options = { transformMode: true };
   const property = "clip-path";
@@ -42,35 +42,80 @@ async function testTranslate(config) {
     await setup({selector, property, options, ...config});
     const { mouse } = helper;
 
-    const { center, width, height } = await getBoundingBoxInPx({selector, ...config});
-    const [x, y] = center;
+    const { nw, width,
+          height, center } = await getBoundingBoxInPx({selector, ...config});
+
+    // if the top or left edges are not visible, move the shape so it is.
+    if (nw[0] < 0 || nw[1] < 0) {
+      const [x, y] = center;
+      const dx = Math.max(0, -nw[0]);
+      const dy = Math.max(0, -nw[1]);
+      await mouse.down(x, y, selector);
+      await mouse.move(x + dx, y + dy, selector);
+      await mouse.up(x + dx, y + dy, selector);
+      await testActor.reflow();
+      nw[0] += dx;
+      nw[1] += dy;
+    }
     const dx = width / 10;
     const dy = height / 10;
     let onShapeChangeApplied;
 
-    info(`Translating ${selector}`);
+    info("Scaling from nw");
     onShapeChangeApplied = highlighters.once("shapes-highlighter-changes-applied");
-    await mouse.down(x, y, selector);
-    await mouse.move(x + dx, y + dy, selector);
-    await mouse.up(x + dx, y + dy, selector);
+    await mouse.down(nw[0], nw[1], selector);
+    await mouse.move(nw[0] + dx, nw[1] + dy, selector);
+    await mouse.up(nw[0] + dx, nw[1] + dy, selector);
     await testActor.reflow();
     await onShapeChangeApplied;
 
-    let newBB = await getBoundingBoxInPx({selector, ...config});
-    isnot(newBB.center[0], x, `${selector} translated on y axis`);
-    isnot(newBB.center[1], y, `${selector} translated on x axis`);
+    const nwBB = await getBoundingBoxInPx({selector, ...config});
+    isnot(nwBB.nw[0], nw[0], `${selector} nw moved right after nw scale`);
+    isnot(nwBB.nw[1], nw[1], `${selector} nw moved down after nw scale`);
+    isnot(nwBB.width, width, `${selector} width reduced after nw scale`);
+    isnot(nwBB.height, height, `${selector} height reduced after nw scale`);
 
-    info(`Translating ${selector} back`);
+    info("Scaling from ne");
     onShapeChangeApplied = highlighters.once("shapes-highlighter-changes-applied");
-    await mouse.down(x + dx, y + dy, selector);
-    await mouse.move(x, y, selector);
-    await mouse.up(x, y, selector);
+    await mouse.down(nwBB.ne[0], nwBB.ne[1], selector);
+    await mouse.move(nwBB.ne[0] - dx, nwBB.ne[1] + dy, selector);
+    await mouse.up(nwBB.ne[0] - dx, nwBB.ne[1] + dy, selector);
     await testActor.reflow();
     await onShapeChangeApplied;
 
-    newBB = await getBoundingBoxInPx({selector, ...config});
-    is(newBB.center[0], x, `${selector} translated back on x axis`);
-    is(newBB.center[1], y, `${selector} translated back on y axis`);
+    const neBB = await getBoundingBoxInPx({selector, ...config});
+    isnot(neBB.ne[0], nwBB.ne[0], `${selector} ne moved right after ne scale`);
+    isnot(neBB.ne[1], nwBB.ne[1], `${selector} ne moved down after ne scale`);
+    isnot(neBB.width, nwBB.width, `${selector} width reduced after ne scale`);
+    isnot(neBB.height, nwBB.height, `${selector} height reduced after ne scale`);
+
+    info("Scaling from sw");
+    onShapeChangeApplied = highlighters.once("shapes-highlighter-changes-applied");
+    await mouse.down(neBB.sw[0], neBB.sw[1], selector);
+    await mouse.move(neBB.sw[0] + dx, neBB.sw[1] - dy, selector);
+    await mouse.up(neBB.sw[0] + dx, neBB.sw[1] - dy, selector);
+    await testActor.reflow();
+    await onShapeChangeApplied;
+
+    const swBB = await getBoundingBoxInPx({selector, ...config});
+    isnot(swBB.sw[0], neBB.sw[0], `${selector} sw moved right after sw scale`);
+    isnot(swBB.sw[1], neBB.sw[1], `${selector} sw moved down after sw scale`);
+    isnot(swBB.width, neBB.width, `${selector} width reduced after sw scale`);
+    isnot(swBB.height, neBB.height, `${selector} height reduced after sw scale`);
+
+    info("Scaling from se");
+    onShapeChangeApplied = highlighters.once("shapes-highlighter-changes-applied");
+    await mouse.down(swBB.se[0], swBB.se[1], selector);
+    await mouse.move(swBB.se[0] - dx, swBB.se[1] - dy, selector);
+    await mouse.up(swBB.se[0] - dx, swBB.se[1] - dy, selector);
+    await testActor.reflow();
+    await onShapeChangeApplied;
+
+    const seBB = await getBoundingBoxInPx({selector, ...config});
+    isnot(seBB.se[0], swBB.se[0], `${selector} se moved right after se scale`);
+    isnot(seBB.se[1], swBB.se[1], `${selector} se moved down after se scale`);
+    isnot(seBB.width, swBB.width, `${selector} width reduced after se scale`);
+    isnot(seBB.height, swBB.height, `${selector} height reduced after se scale`);
 
     await teardown({selector, property, ...config});
   }
