1a6541561d50c5c66057cbbd5dbe0feaaefc261b
Richard Pappalardo <rpappalax@gmail.com>
2018-06-20 17:02:16 -0700

    [focus] Fixes SearchEngineSelectionTest - Closes https://github.com/mozilla-mobile/focus-android/issues/2763

--- mobile/android/focus-android/app/src/androidTestWebview/java/org/mozilla/focus/activity/SearchEngineSelectionTest.java
+++ mobile/android/focus-android/app/src/androidTestWebview/java/org/mozilla/focus/activity/SearchEngineSelectionTest.java
@@ -3,7 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
 package org.mozilla.focus.activity;
 
 import android.content.Context;
@@ -18,10 +17,13 @@ import android.widget.RadioButton;
 
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mozilla.focus.helpers.TestHelper;
+
+import java.util.Arrays;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
@@ -31,13 +33,19 @@ import static org.mozilla.focus.helpers.TestHelper.waitingTime;
 import static org.mozilla.focus.helpers.TestHelper.webPageLoadwaitingTime;
 
 // This test checks the search engine can be changed
-@Ignore("Need to pick another search engine")
+@RunWith(Parameterized.class)
 public class SearchEngineSelectionTest {
+    @Parameterized.Parameter
+    public String mSearchEngine;
+
+    @Parameterized.Parameters
+    public static Iterable<? extends Object> data() {
+        return Arrays.asList("Google", "DuckDuckGo");
+    }
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule
             = new ActivityTestRule<MainActivity>(MainActivity.class) {
-
         @Override
         protected void beforeActivityLaunched() {
             super.beforeActivityLaunched();
@@ -59,66 +67,77 @@ public class SearchEngineSelectionTest {
 
     @Test
     public void SearchTest() throws UiObjectNotFoundException {
-
-        UiObject SearchEngineSelection = TestHelper.settingsList.getChild(new UiSelector()
+        UiObject settingsMenu = TestHelper.settingsList.getChild(new UiSelector()
                 .className("android.widget.LinearLayout")
                 .instance(1));
-        UiObject searchName = SearchEngineSelection.getChild(new UiSelector()
+
+        UiObject searchEngineSelectorLabel = settingsMenu.getChild(new UiSelector()
                 .resourceId("android:id/title")
+                .text("Search")
                 .enabled(true));
 
+        String searchString = String.format("mozilla focus - %s Search", mSearchEngine);
         UiObject googleWebView = TestHelper.mDevice.findObject(new UiSelector()
-                .description("mozilla focus - Google Search")
+                .description(searchString)
                 .className("android.webkit.WebView"));
 
-        /* Go to Settings and select the Search Engine */
+        // Open [settings menu] and select Search
         assertTrue(TestHelper.inlineAutocompleteEditText.waitForExists(waitingTime));
-
         openSettings();
-
-        /* Set the search engine to Google */
-        SearchEngineSelection.click();
-
-        /* Get the dynamically generated search engine list from this settings page */
-        UiScrollable SearchEngineList = new UiScrollable(new UiSelector()
-                .resourceId(TestHelper.getAppName() + ":id/search_engine_group").enabled(true));
-
-        UiObject GoogleSelection = SearchEngineList.getChildByText(new UiSelector()
-                .className(RadioButton.class), "Google");
-
-        GoogleSelection.waitForExists(waitingTime);
-        GoogleSelection.click();
-        TestHelper.pressBackKey();
-        // Now it's changed to Google
-        assertTrue(searchName.getText().equals("Google"));
         TestHelper.settingsHeading.waitForExists(waitingTime);
+        searchEngineSelectorLabel.click();
+
+        // Open [settings menu] > [search engine menu] and click "Default" label
+        // note: we don't click on default search engine itself cause this will change
+        UiObject defaultSearchEngineLabel = TestHelper.mDevice.findObject(new UiSelector()
+                .text("Default")
+                .resourceId("android:id/summary"));
+        defaultSearchEngineLabel.waitForExists(waitingTime);
+        defaultSearchEngineLabel.click();
+
+        // Open [settings menu] > [search engine menu] > [search engine list menu]
+        // then select desired search engine
+        UiScrollable searchEngineList = new UiScrollable(new UiSelector()
+                .resourceId(TestHelper.getAppName() + ":id/search_engine_group").enabled(true));
+        UiObject defaultEngineSelection = searchEngineList.getChildByText(new UiSelector()
+                .className(RadioButton.class), mSearchEngine);
+        defaultEngineSelection.waitForExists(waitingTime);
+        assertTrue(defaultEngineSelection.getText().equals(mSearchEngine));
+        defaultEngineSelection.click();
+        TestHelper.pressBackKey();
+        TestHelper.settingsHeading.waitForExists(waitingTime);
+        UiObject defaultSearchEngine = TestHelper.mDevice.findObject(new UiSelector()
+                .text(mSearchEngine)
+                .resourceId("android:id/title"));
+        assertTrue(defaultSearchEngine.getText().equals(mSearchEngine));
+        TestHelper.pressBackKey();
         TestHelper.pressBackKey();
 
-        /* load blank spaces and press enter key for search, it should not do anything */
+        // Do search on blank spaces and press enter for search (should not do anything)
         TestHelper.inlineAutocompleteEditText.waitForExists(waitingTime);
         TestHelper.inlineAutocompleteEditText.clearTextField();
         TestHelper.inlineAutocompleteEditText.setText("   ");
         TestHelper.pressEnterKey();
         assertTrue(TestHelper.inlineAutocompleteEditText.exists());
 
-        /* Now do some search */
+        // Do search on text string
         TestHelper.inlineAutocompleteEditText.clearTextField();
         TestHelper.inlineAutocompleteEditText.setText("mozilla focus");
         TestHelper.hint.waitForExists(waitingTime);
+        assertTrue(TestHelper.inlineAutocompleteEditText.exists());
 
-        // Check the search hint bar is correctly displayed
+        // Check that search hint bar correctly displayed
         assertTrue(TestHelper.hint.getText().equals("Search for mozilla focus"));
         TestHelper.hint.click();
-
-        /* Browser shows google search webview*/
         googleWebView.waitForExists(waitingTime);
         TestHelper.progressBar.waitForExists(webPageLoadwaitingTime);
         Assert.assertTrue(TestHelper.progressBar.waitUntilGone(webPageLoadwaitingTime));
-        assertTrue (TestHelper.browserURLbar.getText().contains("google"));
-        assertTrue (TestHelper.browserURLbar.getText().contains("mozilla"));
-        assertTrue (TestHelper.browserURLbar.getText().contains("focus"));
+        // Search for words: <Google|DuckDuckGo|etc.>, mozilla, focus
+        assertTrue(TestHelper.browserURLbar.getText().contains(mSearchEngine.toLowerCase()));
+        assertTrue(TestHelper.browserURLbar.getText().contains("mozilla"));
+        assertTrue(TestHelper.browserURLbar.getText().contains("focus"));
 
-        /* tap url bar, and check it displays search term instead of URL */
+        // Tap URL bar, check it displays search term (instead of URL)
         TestHelper.browserURLbar.click();
         TestHelper.inlineAutocompleteEditText.waitForExists(waitingTime);
         assertEquals(TestHelper.inlineAutocompleteEditText.getText(), "mozilla focus");
@@ -126,18 +145,19 @@ public class SearchEngineSelectionTest {
         googleWebView.waitForExists(waitingTime);
         TestHelper.progressBar.waitForExists(webPageLoadwaitingTime);
         Assert.assertTrue(TestHelper.progressBar.waitUntilGone(webPageLoadwaitingTime));
-        assertTrue (TestHelper.browserURLbar.getText().contains("google"));
-        assertTrue (TestHelper.browserURLbar.getText().contains("mozilla"));
-        assertTrue (TestHelper.browserURLbar.getText().contains("focus"));
+        // Search for words: <Google|DuckDuckGo|etc.>, mozilla, focus
+        assertTrue(TestHelper.browserURLbar.getText().contains(mSearchEngine.toLowerCase()));
+        assertTrue(TestHelper.browserURLbar.getText().contains("mozilla"));
+        assertTrue(TestHelper.browserURLbar.getText().contains("focus"));
 
-         /* Now do another search */
+        // Do another search on text string
         TestHelper.browserURLbar.click();
         TestHelper.inlineAutocompleteEditText.waitForExists(waitingTime);
         TestHelper.inlineAutocompleteEditText.clearTextField();
         TestHelper.inlineAutocompleteEditText.setText("mozilla focus");
         TestHelper.hint.waitForExists(waitingTime);
 
-        // Check the search hint bar is correctly displayed
+        // Check search hint bar correctly displayed
         assertTrue(TestHelper.hint.getText().equals("Search for mozilla focus"));
         TestHelper.hint.click();
     }
