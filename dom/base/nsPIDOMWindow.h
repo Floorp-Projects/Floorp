6d0c39faee640b73e084538e1548db3c988e9d8d
Chris Pearce <cpearce@mozilla.com>
2018-06-22 11:57:24 +1200

    Bug 1463919 - Add AutoplayRequest to encapsulate asking for autoplay permission. r=smaug
    Add an implementation of nsIContentPermissionRequest to encapsulate requesting
    permission from the user to autoplay audible media.
    
    All documents in the tab request permission using the top level document's
    origin, so the AutoplayRequest instance for a tab is stored on the top level
    content window of the tab.
    
    AutoplayRequest ensures that there's only a single prompt shown at once.
    
    MozReview-Commit-ID: 2u3aLnEa21z

--- dom/base/nsPIDOMWindow.h
+++ dom/base/nsPIDOMWindow.h
@@ -45,6 +45,7 @@ typedef uint32_t SuspendTypes;
 
 namespace mozilla {
 class ThrottledEventQueue;
+class AutoplayRequest;
 namespace dom {
 class AudioContext;
 class ClientInfo;
@@ -613,6 +614,10 @@ public:
   virtual nsISerialEventTarget*
   EventTargetFor(mozilla::TaskCategory aCategory) const = 0;
 
+  // Returns the AutoplayRequest that documents in this window should use
+  // to request permission to autoplay.
+  already_AddRefed<mozilla::AutoplayRequest> GetAutoplayRequest();
+
 protected:
   void CreatePerformanceObjectIfNeeded();
 
@@ -695,6 +700,11 @@ protected:
 
   // The number of open WebSockets.
   uint32_t mNumOfOpenWebSockets;
+
+  // If we're in the process of requesting permission for this window to
+  // play audible media, or we've already been granted permission by the
+  // user, this is non-null, and encapsulates the request.
+  RefPtr<mozilla::AutoplayRequest> mAutoplayRequest;
 };
 
 NS_DEFINE_STATIC_IID_ACCESSOR(nsPIDOMWindowInner, NS_PIDOMWINDOWINNER_IID)
