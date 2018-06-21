a3c7fb2e87f2a0adb9396b17acae56b6c4d969d5
Mike Hommey <mh+mozilla@glandium.org>
2018-06-21 18:13:03 +0900

    Bug 1470127 - Move binary checks to a standalone script. r=froydnj
    We perform, on the binaries we build, a series of check, that are
    implemented as half-baked make commands, invoked after linking them.
    
    - check libstdc++ symbol versions to ensure binary compatibility with
      a baseline.
    - check glibc symbol versions to ensure binary compatibility with a
      baseline.
    - check that target binaries don't contain text relocations.
    - check that libmozglue is linked before libc on android.
    - on libxul, check that NSModules are laid out correctly.
    - on libxul, check that there is more than one PT_LOAD segment.
    
    Those checks happen to work where they matter, but their setup is
    unreliable. For example, the checks for symbol versions are supposed to
    work for libclang-plugin on cross osx builds, but in fact, don't,
    because the readelf path doesn't exist, and the command doesn't fail in
    that case.
    
    So move them all to a standalone script, performing the checks more
    thoroughly (especially the NSModules one, where we now also check that
    they are all adjacent), and more verbosely.

--- toolkit/library/libxul.mk
+++ toolkit/library/libxul.mk
@@ -14,15 +14,3 @@ PP_TARGETS += LIBXUL_AUTOLOAD
 LIBXUL_AUTOLOAD = $(topsrcdir)/toolkit/library/libxul.so-gdb.py.in
 LIBXUL_AUTOLOAD_FLAGS := -Dtopsrcdir=$(abspath $(topsrcdir))
 endif
-
-ifdef _MSC_VER
-get_first_and_last = dumpbin -exports $1 | grep _NSModule@@ | sort -k 3 | sed -n 's/^.*?\([^@]*\)@@.*$$/\1/;1p;$$p'
-else
-get_first_and_last = $(TOOLCHAIN_PREFIX)nm -g $1 | grep _NSModule$$ | grep -vw refptr | sort | sed -n 's/^.* _*\([^ ]*\)$$/\1/;1p;$$p'
-endif
-
-LOCAL_CHECKS = test "$$($(get_first_and_last) | xargs echo)" != "start_kPStaticModules_NSModule end_kPStaticModules_NSModule" && echo "NSModules are not ordered appropriately" && exit 1 || exit 0
-
-ifeq (Linux,$(OS_ARCH))
-LOCAL_CHECKS += ; test "$$($(TOOLCHAIN_PREFIX)readelf -l $1 | awk '$1 == "LOAD" { t += 1 } END { print t }')" -le 1 && echo "Only one PT_LOAD segment" && exit 1 || exit 0
-endif
