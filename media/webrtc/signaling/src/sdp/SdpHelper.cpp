bfea03ea24d0651c421be14a33fa334e8c2d0bfd
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2018-06-22 15:49:03 -0700

    Bug 1335206: enable emitting datachannel draft 21 format. r=bwc
    MozReview-Commit-ID: 9zWz8ct4ERZ
    
    --HG--
    extra : rebase_source : 83a7fc8f3d20e59436bbc37fe93dc00ec8c30683

--- media/webrtc/signaling/src/sdp/SdpHelper.cpp
+++ media/webrtc/signaling/src/sdp/SdpHelper.cpp
@@ -176,7 +176,7 @@ SdpHelper::DisableMsection(Sdp* sdp, SdpMediaSection* msection)
       msection->AddCodec("120", "VP8", 90000, 1);
       break;
     case SdpMediaSection::kApplication:
-      msection->AddDataChannel("rejected", 0, 0, 0);
+      msection->AddDataChannel("webrtc-datachannel", 0, 0, 0);
       break;
     default:
       // We need to have something here to fit the grammar, this seems safe
@@ -716,9 +716,7 @@ SdpMediaSection::Protocol
 SdpHelper::GetProtocolForMediaType(SdpMediaSection::MediaType type)
 {
   if (type == SdpMediaSection::kApplication) {
-    return SdpMediaSection::kDtlsSctp;
-    // TODO switch to offer the new SCTP SDP (Bug 1335206)
-    //return SdpMediaSection::kUdpDtlsSctp;
+    return SdpMediaSection::kUdpDtlsSctp;
   }
 
   return SdpMediaSection::kUdpTlsRtpSavpf;
