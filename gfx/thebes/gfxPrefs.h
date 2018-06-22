d7f0607ae30ed2ceb8f70b3b8898ebabbcc441e1
Daosheng Mu <daoshengmu@gmail.com>
2018-06-22 16:30:14 -0700

    Bug 1430038 - Part 1: Add VR process to the process list; r=kip, jimm
    Summary: MozReview-Commit-ID: AWyFur2gLCQ
    
    Tags: #secure-revision
    
    Differential Revision: https://phabricator.services.mozilla.com/D2876
    
    MozReview-Commit-ID: HHGDiXyaqnB

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -391,6 +391,7 @@ private:
   DECL_GFX_PREF(Live, "dom.vr.puppet.enabled",                 VRPuppetEnabled, bool, false);
   DECL_GFX_PREF(Live, "dom.vr.puppet.submitframe",             VRPuppetSubmitFrame, uint32_t, 0);
   DECL_GFX_PREF(Live, "dom.vr.display.rafMaxDuration",         VRDisplayRafMaxDuration, uint32_t, 50);
+  DECL_GFX_PREF(Once, "dom.vr.process.enabled",                VRProcessEnabled, bool, false);
   DECL_GFX_PREF(Once, "dom.vr.service.enabled",                VRServiceEnabled, bool, false);
   DECL_GFX_PREF(Live, "dom.w3c_pointer_events.enabled",        PointerEventsEnabled, bool, false);
 
