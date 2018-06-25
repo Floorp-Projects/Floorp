151202748f7d5cde14a8da20e3205fc4fb080152
Emily Kager <ekager@mills.edu>
2018-06-25 14:19:28 -0700

    [focus] Coordinator behavior + find in page margins Closes https://github.com/mozilla-mobile/focus-android/issues/2776 + Closes https://github.com/mozilla-mobile/focus-android/issues/2725

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.java
@@ -26,6 +26,7 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -36,11 +37,13 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.webkit.CookieManager;
 import android.webkit.URLUtil;
 import android.widget.FrameLayout;
@@ -152,6 +155,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
     private int findInPageViewHeight;
     private TextView findInPageQuery;
     private TextView findInPageResultTextView;
+    private ImageButton findInPageNext;
+    private ImageButton findInPagePrevious;
+    private ImageButton closeFindInPage;
 
     private IWebView.FullscreenCallback fullscreenCallback;
 
@@ -293,14 +299,25 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                     }
                 }
         );
+        findInPageQuery.setOnClickListener(this);
+        findInPageQuery.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    ViewUtils.hideKeyboard(findInPageQuery);
+                    findInPageQuery.setCursorVisible(false);
+                }
+                return false;
+            }
+        });
 
-        final ImageButton findInPagePrevious = view.findViewById(R.id.previousResult);
+        findInPagePrevious = view.findViewById(R.id.previousResult);
         findInPagePrevious.setOnClickListener(this);
 
-        final ImageButton findInPageNext = view.findViewById(R.id.nextResult);
+        findInPageNext = view.findViewById(R.id.nextResult);
         findInPageNext.setOnClickListener(this);
 
-        final ImageButton closeFindInPage = view.findViewById(R.id.close_find_in_page);
+        closeFindInPage = view.findViewById(R.id.close_find_in_page);
         closeFindInPage.setOnClickListener(this);
 
         setBlockingEnabled(session.isBlockingEnabled());
@@ -1116,7 +1133,14 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
                 TelemetryWrapper.findInPageMenuEvent();
                 break;
 
+            case R.id.queryText:
+                findInPageQuery.setCursorVisible(true);
+                break;
+
             case R.id.nextResult: {
+                ViewUtils.hideKeyboard(findInPageQuery);
+                findInPageQuery.setCursorVisible(false);
+
                 final IWebView webView = getWebView();
                 if (webView == null) {
                     break;
@@ -1127,6 +1151,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             }
 
             case R.id.previousResult: {
+                ViewUtils.hideKeyboard(findInPageQuery);
+                findInPageQuery.setCursorVisible(false);
+
                 final IWebView webView = getWebView();
                 if (webView == null) {
                     break;
@@ -1321,7 +1348,9 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
         findInPageView.setVisibility(View.VISIBLE);
         findInPageQuery.requestFocus();
 
-        swipeRefresh.setPadding(0, 0, 0, findInPageViewHeight);
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) swipeRefresh.getLayoutParams();
+        params.bottomMargin = findInPageViewHeight;
+        swipeRefresh.setLayoutParams(params);
     }
 
     private void hideFindInPage() {
@@ -1335,7 +1364,10 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
         findInPageView.setVisibility(View.GONE);
         findInPageQuery.setText("");
         findInPageQuery.clearFocus();
-        swipeRefresh.setPadding(0, 0, 0, 0);
+
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) swipeRefresh.getLayoutParams();
+        params.bottomMargin = 0;
+        swipeRefresh.setLayoutParams(params);
         ViewUtils.hideKeyboard(findInPageQuery);
     }
 
@@ -1348,6 +1380,10 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
         if (numberOfMatches > 0) {
             // We don't want the presentation of the activeMatchOrdinal to be zero indexed. So let's
             // increment it by one.
+            findInPageNext.setColorFilter(getResources().getColor(R.color.photonWhite));
+            findInPageNext.setAlpha(1.0F);
+            findInPagePrevious.setColorFilter(getResources().getColor(R.color.photonWhite));
+            findInPagePrevious.setAlpha(1.0F);
             activeMatchOrdinal++;
             final String visibleString = String.format(context.getString(R.string.find_in_page_result), activeMatchOrdinal, numberOfMatches);
             final String accessibleString = String.format(context.getString(R.string.find_in_page_result), activeMatchOrdinal, numberOfMatches);
@@ -1355,6 +1391,10 @@ public class BrowserFragment extends WebFragment implements View.OnClickListener
             findInPageResultTextView.setText(visibleString);
             findInPageResultTextView.setContentDescription(accessibleString);
         } else {
+            findInPageNext.setColorFilter(getResources().getColor(R.color.photonGrey10));
+            findInPageNext.setAlpha(0.4F);
+            findInPagePrevious.setColorFilter(getResources().getColor(R.color.photonWhite));
+            findInPagePrevious.setAlpha(0.4F);
             findInPageResultTextView.setText("");
             findInPageResultTextView.setContentDescription("");
         }
