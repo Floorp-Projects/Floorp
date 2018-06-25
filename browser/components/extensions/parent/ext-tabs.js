f4189ea8e7012b6b8443d069380337b4a56e9e3b
Kris Maglione <maglione.k@gmail.com>
2018-06-24 17:33:55 -0700

    Bug 1470023: Lazily load PluginContent.jsm when it's first required. r=felipe
    MozReview-Commit-ID: 2n9NP5mEEcG

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -76,7 +76,7 @@ let tabListener = {
   },
 
   onLocationChange(browser, webProgress, request, locationURI, flags) {
-    if (webProgress.isTopLevel) {
+    if (webProgress.isTopLevel && locationURI.spec !== "about:blank") {
       let {gBrowser} = browser.ownerGlobal;
       let nativeTab = gBrowser.getTabForBrowser(browser);
 
@@ -596,7 +596,9 @@ this.tabs = class extends ExtensionAPI {
               window.focusAndSelectUrlBar();
             }
 
-            if (createProperties.url && createProperties.url !== window.BROWSER_NEW_TAB_URL) {
+            if (createProperties.url &&
+                createProperties.url !== "about:blank" &&
+                createProperties.url !== window.BROWSER_NEW_TAB_URL) {
               // We can't wait for a location change event for about:newtab,
               // since it may be pre-rendered, in which case its initial
               // location change event has already fired.
