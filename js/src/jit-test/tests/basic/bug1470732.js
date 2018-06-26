091c89133d4ed0eedcf37cc737d459463b2c4346
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-06-26 18:43:09 +0300

    Backed out 2 changesets (bug 1470732) for bustages on bug1470732.js . CLOSED TREE
    Backed out changeset 4a596e6a7ccb (bug 1470732)
    Backed out changeset 0eded40b1124 (bug 1470732)

--- js/src/jit-test/tests/basic/bug1470732.js
+++
@@ -1,9 +0,0 @@
-var i = 0;
-while(i++ < 500) {
-  evalInWorker(`
-    setJitCompilerOption("baseline.warmup.trigger", 10);
-  `);
-  let m = parseModule("");
-  m.declarationInstantiation();
-}
-
--- js/src/vm/HelperThreads.cpp
+++ js/src/vm/HelperThreads.cpp
@@ -297,11 +297,6 @@ CancelOffThreadIonCompileLocked(const CompilationSelector& selector, bool discar
     for (size_t i = 0; i < worklist.length(); i++) {
         jit::IonBuilder* builder = worklist[i];
         if (IonBuilderMatches(selector, builder)) {
-            // Once finished, builders are handled by a Linked list which is
-            // allocated with the IonBuilder class which is contained in the
-            // LifoAlloc-ated structure. Thus we need it to be mutable.
-            worklist[i]->alloc().lifoAlloc()->setReadWrite();
-
             FinishOffThreadIonCompile(builder, lock);
             HelperThreadState().remove(worklist, &i);
         }
@@ -2267,11 +2262,8 @@ void
 GlobalHelperThreadState::trace(JSTracer* trc)
 {
     AutoLockHelperThreadState lock;
-    for (auto builder : ionWorklist(lock)) {
-        builder->alloc().lifoAlloc()->setReadWrite();
+    for (auto builder : ionWorklist(lock))
         builder->trace(trc);
-        builder->alloc().lifoAlloc()->setReadOnly();
-    }
     for (auto builder : ionFinishedList(lock))
         builder->trace(trc);
 
