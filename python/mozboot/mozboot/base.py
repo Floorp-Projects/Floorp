e75d208e28f2029485dbb0820b06118346ef49b8
Mike Hommey <mh+mozilla@glandium.org>
2018-06-26 18:05:23 +0900

    Bug 1447116 - Require rust 1.26. r=froydnj
    We're well overdue for an upgrade of the rust compiler requirements.
    Now that we're building with 1.28 (albeit a beta, due to be bumped when
    it's released), we can bump the requirement away from 1.24 which is now
    old. 1.27 is too new, though, so settle for the older 1.26.

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -150,7 +150,7 @@ MODERN_MERCURIAL_VERSION = LooseVersion('4.3.3')
 MODERN_PYTHON_VERSION = LooseVersion('2.7.3')
 
 # Upgrade rust older than this.
-MODERN_RUST_VERSION = LooseVersion('1.24.0')
+MODERN_RUST_VERSION = LooseVersion('1.26.0')
 
 
 class BaseBootstrapper(object):
