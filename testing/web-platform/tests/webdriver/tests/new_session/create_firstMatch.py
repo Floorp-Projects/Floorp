79185224532f79614ef0b452ffc135dc441a5eb1
Henrik Skupin <mail@hskupin.info>
2018-06-27 16:40:18 +0200

    Bug 1470530 - [wdspec] Move "add_browser_capabilities" fixture under "new_session". r=ato
    The "add_browser_capabilities" fixture is only used for new session tests,
    and as such makes sense to only expose the fixture for that specific
    sub directory.
    
    MozReview-Commit-ID: 7FYuz9lVRxH

--- testing/web-platform/tests/webdriver/tests/new_session/create_firstMatch.py
+++ testing/web-platform/tests/webdriver/tests/new_session/create_firstMatch.py
@@ -10,7 +10,7 @@ from tests.new_session.support.create import valid_data
 
 
 @pytest.mark.parametrize("key,value", flatten(product(*item) for item in valid_data))
-def test_valid(new_session, add_browser_capabilites, key, value):
+def test_valid(new_session, add_browser_capabilities, key, value):
     response, _ = new_session({"capabilities": {
-        "firstMatch": [add_browser_capabilites({key: value})]}})
+        "firstMatch": [add_browser_capabilities({key: value})]}})
     assert_success(response)
--- testing/web-platform/tests/webdriver/tests/new_session/invalid_capabilities.py
+++ testing/web-platform/tests/webdriver/tests/new_session/invalid_capabilities.py
@@ -11,16 +11,16 @@ def test_invalid_capabilites(new_session, value):
 
 
 @pytest.mark.parametrize("value", [None, 1, "{}", []])
-def test_invalid_always_match(new_session, add_browser_capabilites, value):
-    capabilities = {"alwaysMatch": value, "firstMatch": [add_browser_capabilites({})]}
+def test_invalid_always_match(new_session, add_browser_capabilities, value):
+    capabilities = {"alwaysMatch": value, "firstMatch": [add_browser_capabilities({})]}
 
     response, _ = new_session({"capabilities": capabilities})
     assert_error(response, "invalid argument")
 
 
 @pytest.mark.parametrize("value", [None, 1, "[]", {}])
-def test_invalid_first_match(new_session, add_browser_capabilites, value):
-    capabilities = {"alwaysMatch": add_browser_capabilites({}), "firstMatch": value}
+def test_invalid_first_match(new_session, add_browser_capabilities, value):
+    capabilities = {"alwaysMatch": add_browser_capabilities({}), "firstMatch": value}
 
     response, _ = new_session({"capabilities": capabilities})
     assert_error(response, "invalid argument")
@@ -52,12 +52,12 @@ invalid_data = [
 @pytest.mark.parametrize("body", [lambda key, value: {"alwaysMatch": {key: value}},
                                   lambda key, value: {"firstMatch": [{key: value}]}])
 @pytest.mark.parametrize("key,value", flatten(product(*item) for item in invalid_data))
-def test_invalid_values(new_session, add_browser_capabilites, body, key, value):
+def test_invalid_values(new_session, add_browser_capabilities, body, key, value):
     capabilities = body(key, value)
     if "alwaysMatch" in capabilities:
-        capabilities["alwaysMatch"] = add_browser_capabilites(capabilities["alwaysMatch"])
+        capabilities["alwaysMatch"] = add_browser_capabilities(capabilities["alwaysMatch"])
     else:
-        capabilities["firstMatch"][0] = add_browser_capabilites(capabilities["firstMatch"][0])
+        capabilities["firstMatch"][0] = add_browser_capabilities(capabilities["firstMatch"][0])
 
     response, _ = new_session({"capabilities": capabilities})
     assert_error(response, "invalid argument")
@@ -91,12 +91,12 @@ invalid_extensions = [
 @pytest.mark.parametrize("body", [lambda key, value: {"alwaysMatch": {key: value}},
                                   lambda key, value: {"firstMatch": [{key: value}]}])
 @pytest.mark.parametrize("key", invalid_extensions)
-def test_invalid_extensions(new_session, add_browser_capabilites, body, key):
+def test_invalid_extensions(new_session, add_browser_capabilities, body, key):
     capabilities = body(key, {})
     if "alwaysMatch" in capabilities:
-        capabilities["alwaysMatch"] = add_browser_capabilites(capabilities["alwaysMatch"])
+        capabilities["alwaysMatch"] = add_browser_capabilities(capabilities["alwaysMatch"])
     else:
-        capabilities["firstMatch"][0] = add_browser_capabilites(capabilities["firstMatch"][0])
+        capabilities["firstMatch"][0] = add_browser_capabilities(capabilities["firstMatch"][0])
 
     response, _ = new_session({"capabilities": capabilities})
     assert_error(response, "invalid argument")
