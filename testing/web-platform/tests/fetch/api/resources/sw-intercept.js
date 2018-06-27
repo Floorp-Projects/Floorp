66a9ac9f63446d63c96619bea6006bd5644e3d2c
Dorel Luca <dluca@mozilla.com>
2018-06-27 17:53:26 +0300

    Backed out 2 changesets (bug 1394102) for Web platform test failures. CLOSED TREE
    Backed out changeset 4133f5b222de (bug 1394102)
    Backed out changeset 39784ddfdbb0 (bug 1394102)

--- testing/web-platform/tests/fetch/api/resources/sw-intercept.js
+++ testing/web-platform/tests/fetch/api/resources/sw-intercept.js
@@ -5,16 +5,6 @@ async function broadcast(msg) {
 }
 
 addEventListener('fetch', event => {
-  if (event.request.url.endsWith('?signal')) {
-    event.waitUntil(broadcast("ready"));
-    event.respondWith(new Promise((resolve, reject) => {
-      event.request.signal.onabort = () => {
-        broadcast("aborted");
-        reject();
-      };
-    }));
-  } else {
-    event.waitUntil(broadcast(event.request.url));
-    event.respondWith(fetch(event.request));
-  }
+  event.waitUntil(broadcast(event.request.url));
+  event.respondWith(fetch(event.request));
 });
