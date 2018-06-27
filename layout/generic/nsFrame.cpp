9a603e108d6fdf88459dbf7e9ea219c8f2d177f7
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-06-27 09:08:36 +0900

    Bug 1471174 - Constify nsIFrame::IsScrolledOutOfView. r=birtles
    The reason why const_cast is used for nsLayoutUtils::GetNearestScrollableFrame
    is that if we changed the function as well, it ends up scattering const_cast
    in most call sites of the function.  That's because GetNearestScrollableFrame
    has a do_QueryFrame call for the given nsIFrame* and returns the queried frame,
    so it will be like this;
    
     const nsIScrollableFrame* GetNearestScrollableFrame(const nsIFrame*, ..)
    
    Most call sites of this function are then calls do_QueryFrame for the returned
    nsIScrollableFrame*.
    
    MozReview-Commit-ID: EwccKUITL89

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -10959,12 +10959,12 @@ nsIFrame::IsStackingContext()
 }
 
 static bool
-IsFrameScrolledOutOfView(nsIFrame* aTarget,
+IsFrameScrolledOutOfView(const nsIFrame* aTarget,
                          const nsRect& aTargetRect,
-                         nsIFrame* aParent)
+                         const nsIFrame* aParent)
 {
   nsIScrollableFrame* scrollableFrame =
-    nsLayoutUtils::GetNearestScrollableFrame(aParent,
+    nsLayoutUtils::GetNearestScrollableFrame(const_cast<nsIFrame*>(aParent),
       nsLayoutUtils::SCROLLABLE_SAME_DOC |
       nsLayoutUtils::SCROLLABLE_FIXEDPOS_FINDS_ROOT |
       nsLayoutUtils::SCROLLABLE_INCLUDE_HIDDEN);
@@ -11008,7 +11008,7 @@ IsFrameScrolledOutOfView(nsIFrame* aTarget,
 }
 
 bool
-nsIFrame::IsScrolledOutOfView()
+nsIFrame::IsScrolledOutOfView() const
 {
   nsRect rect = GetVisualOverflowRectRelativeToSelf();
   return IsFrameScrolledOutOfView(this, rect, this);
