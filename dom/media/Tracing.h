8bb03d3e821c8a2a6e11859d8cc492d331a03386
Andreas Pehrson <pehrsons@mozilla.com>
2018-06-27 15:34:21 +0200

    Bug 1471588 - Include sample rate in budget comment. r=padenot
    MozReview-Commit-ID: Ge74gpH3mFI

--- dom/media/Tracing.h
+++ dom/media/Tracing.h
@@ -111,7 +111,8 @@ private:
                    uint64_t aDuration,
                    uint64_t aPID,
                    uint64_t aThread,
-                   uint64_t aFrames);
+                   uint64_t aFrames,
+                   uint64_t aSampleRate);
 
   // The logger to use. It musdt have a lifetime longer than the block an
   // instance of this class traces.
