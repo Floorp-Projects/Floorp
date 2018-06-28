76ad6a7d39cf11504eeb0392bb641ea7c4c81d3e
Andreea Pavel <apavel@mozilla.com>
2018-06-28 03:00:49 +0300

    Backed out changeset 5d8c20358cac (bug 756165) for [@ MOZ_CrashOOL] crashes on a CLOSED TREE

--- dom/websocket/WebSocket.cpp
+++ dom/websocket/WebSocket.cpp
@@ -63,7 +63,6 @@
 #include "nsIWebSocketListener.h"
 #include "nsProxyRelease.h"
 #include "nsWeakReference.h"
-#include "nsINSSErrorsService.h"
 
 #define OPEN_EVENT_STRING NS_LITERAL_STRING("open")
 #define MESSAGE_EVENT_STRING NS_LITERAL_STRING("message")
@@ -822,15 +821,6 @@ WebSocketImpl::ScheduleConnectionCloseEvents(nsISupports* aContext,
     }
 
     if (NS_FAILED(aStatusCode)) {
-      uint32_t errorClass;
-      nsCOMPtr<nsINSSErrorsService> errSvc =
-        do_GetService("@mozilla.org/nss_errors_service;1");
-      // If GetErrorClass succeeds the error is TLS related and we need to set
-      // status code to 1015.
-      if (errSvc && NS_SUCCEEDED(errSvc->GetErrorClass(aStatusCode, &errorClass))) {
-        mCloseEventCode = 1015;
-      }
-
       ConsoleError();
       mFailed = true;
     }
