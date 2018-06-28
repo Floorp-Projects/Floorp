df6ed17445451c2cf55c67d6a963c34dab024475
Margareta Eliza Balazs <ebalazs@mozilla.com>
2018-06-28 13:51:18 +0300

    Backed out 2 changesets (bug 1394102) for wpt failures in /fetch/api/abort/serviceworker-intercepted.https.html
    Backed out changeset 63b21854d800 (bug 1394102)
    Backed out changeset 834072e2974e (bug 1394102)

--- testing/web-platform/tests/fetch/api/resources/sw-intercept.js
+++ testing/web-platform/tests/fetch/api/resources/sw-intercept.js
@@ -5,16 +5,6 @@ async function broadcast(msg) {
 }
 
 addEventListener('fetch', event => {
-  if (event.request.url.endsWith('?signal')) {
-    event.respondWith(new Promise((resolve, reject) => {
-      event.request.signal.onabort = () => {
-        broadcast("aborted");
-        reject();
-      };
-      event.waitUntil(broadcast("ready"));
-    }));
-  } else {
-    event.waitUntil(broadcast(event.request.url));
-    event.respondWith(fetch(event.request));
-  }
+  event.waitUntil(broadcast(event.request.url));
+  event.respondWith(fetch(event.request));
 });
