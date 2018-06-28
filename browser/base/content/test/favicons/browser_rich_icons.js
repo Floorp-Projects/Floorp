0f2c3bd64852268af186c1a4fdc79eff9069daf6
Dave Townsend <dtownsend@oxymoronical.com>
2018-06-28 16:06:09 -0700

    Bug 1453751: Load favicons in the content process. r=mak, r=gijs, r=aswan, r=mixedpuppy
    Summary:
    This moves the load of favicons into the content process. We use the same logic
    for finding favicons (based on waiting until none have shown up for a short
    time) but then load the favicon and convert it to a data uri which we then
    dispatch to the parent process. Along the way this fixes asssociating the load
    with the tab for WebExtension and devtools, fixes CSP usage for the load, fixes
    expiry detection of the favicon and stops us from loading the same resource
    twice.
    
    This change also merges the prefs browser.chrome.site_icons and
    browser.chrome.favicons leaving just the former controlling favicon loading. It
    adds the pref browser.chrome.guess_favicon to allow disabling guessing where
    a favicon might be located for a site (at <hostname>/favicon.ico). This is
    mainly to allow disabling this in tests where those additional yet automatic
    requests are uninteresting for the test.
    
    There are multiple clean-ups that can follow this but this is a first step along
    that path.
    
    MozReview-Commit-ID: E0Cs59UnxaF
    
    Reviewers: mak
    
    Tags: #secure-revision
    
    Bug #: 1453751
    
    Differential Revision: https://phabricator.services.mozilla.com/D1672
    Differential Revision: https://phabricator.services.mozilla.com/D1673
    Differential Revision: https://phabricator.services.mozilla.com/D1674
    Differential Revision: https://phabricator.services.mozilla.com/D1850
    Differential Revision: https://phabricator.services.mozilla.com/D1869

--- browser/base/content/test/favicons/browser_rich_icons.js
+++ browser/base/content/test/favicons/browser_rich_icons.js
@@ -9,61 +9,29 @@ add_task(async function test_richIcons() {
   const URL = ROOT + "file_rich_icon.html";
   const EXPECTED_ICON = ROOT + "moz.png";
   const EXPECTED_RICH_ICON = ROOT + "rich_moz_2.png";
-  // One regular icon and one rich icon. Note that ContentLinkHandler will
-  // choose the best rich icon if there are multiple candidates available
-  // in the page.
-  const EXPECTED_ICON_LOADS = 2;
-  let loadCount = 0;
-  let tabIconUri;
-  let richIconUri;
 
-  const promiseMessage = new Promise(resolve => {
-    const mm = window.messageManager;
-    mm.addMessageListener("Link:SetIcon", function listenForSetIcon(msg) {
-      // Ignore the chrome favicon sets on the tab before the actual page load.
-      if (msg.data.url === "chrome://branding/content/icon32.png")
-        return;
-
-      if (!msg.data.canUseForTab)
-        richIconUri = msg.data.url;
-
-      if (++loadCount === EXPECTED_ICON_LOADS) {
-        mm.removeMessageListener("Link:SetIcon", listenForSetIcon);
-        resolve();
-      }
-    });
-  });
+  let tabPromises = Promise.all([
+    waitForFaviconMessage(true, EXPECTED_ICON),
+    waitForFaviconMessage(false, EXPECTED_RICH_ICON),
+  ]);
 
   const tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, URL);
-  await promiseMessage;
+  let [tabIcon, richIcon] = await tabPromises;
 
-  is(richIconUri, EXPECTED_RICH_ICON, "should choose the largest rich icon");
+  is(richIcon.iconURL, EXPECTED_RICH_ICON, "should choose the largest rich icon");
+  is(tabIcon.iconURL, EXPECTED_ICON, "should use the non-rich icon for the tab");
 
-  // Because there is debounce logic in ContentLinkHandler.jsm to reduce the
-  // favicon loads, we have to wait some time before checking that icon was
-  // stored properly.
-  await BrowserTestUtils.waitForCondition(() => {
-    tabIconUri = gBrowser.getIcon();
-    return tabIconUri != null;
-  }, "wait for icon load to finish", 100, 20);
-  is(tabIconUri, EXPECTED_ICON, "should use the non-rich icon for the tab");
   BrowserTestUtils.removeTab(tab);
 });
 
 add_task(async function test_maskIcons() {
   const URL = ROOT + "file_mask_icon.html";
-  // First we need to clear the failed favicons cache, since previous tests
-  // likely added this non-existing icon, and useDefaultIcon would skip it.
-  PlacesUtils.favicons.removeFailedFavicon(makeURI("http://mochi.test:8888/favicon.ico"));
   const EXPECTED_ICON = "http://mochi.test:8888/favicon.ico";
-  let tabIconUri;
 
+  let promise = waitForFaviconMessage(true, EXPECTED_ICON);
   const tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, URL);
+  let tabIcon = await promise;
+  is(tabIcon.iconURL, EXPECTED_ICON, "should ignore the mask icons and load the root favicon");
 
-  await BrowserTestUtils.waitForCondition(() => {
-    tabIconUri = gBrowser.getIcon();
-    return tabIconUri != null;
-  }, "wait for icon load to finish", 100, 20);
-  is(tabIconUri, EXPECTED_ICON, "should ignore the mask icons and load the root favicon");
   BrowserTestUtils.removeTab(tab);
 });
