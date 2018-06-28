87c9bda333def00eb9aa102bf7466ec1499d2e3c
Dave Townsend <dtownsend@oxymoronical.com>
2018-06-28 16:06:09 -0700

    Bug 1453751: Load favicons in the content process. r=mak, r=gijs, r=aswan, r=mixedpuppy
    Summary:
    This moves the load of favicons into the content process. We use the same logic
    for finding favicons (based on waiting until none have shown up for a short
    time) but then load the favicon and convert it to a data uri which we then
    dispatch to the parent process. Along the way this fixes asssociating the load
    with the tab for WebExtension and devtools, fixes CSP usage for the load, fixes
    expiry detection of the favicon and stops us from loading the same resource
    twice.
    
    This change also merges the prefs browser.chrome.site_icons and
    browser.chrome.favicons leaving just the former controlling favicon loading. It
    adds the pref browser.chrome.guess_favicon to allow disabling guessing where
    a favicon might be located for a site (at <hostname>/favicon.ico). This is
    mainly to allow disabling this in tests where those additional yet automatic
    requests are uninteresting for the test.
    
    There are multiple clean-ups that can follow this but this is a first step along
    that path.
    
    MozReview-Commit-ID: E0Cs59UnxaF
    
    Reviewers: mak
    
    Tags: #secure-revision
    
    Bug #: 1453751
    
    Differential Revision: https://phabricator.services.mozilla.com/D1672
    Differential Revision: https://phabricator.services.mozilla.com/D1673
    Differential Revision: https://phabricator.services.mozilla.com/D1674
    Differential Revision: https://phabricator.services.mozilla.com/D1850
    Differential Revision: https://phabricator.services.mozilla.com/D1869

--- extensions/cookie/test/browser_test_favicon.js
+++ extensions/cookie/test/browser_test_favicon.js
@@ -1,9 +1,9 @@
-// tests third party cookie blocking using a favicon load directly from chrome.
-// in this case, the docshell of the channel is chrome, not content; thus
-// the cookie should be considered third party.
+// Tests third party cookie blocking using a favicon loaded from a different
+// domain. The cookie should be considered third party.
 
 add_task(async function() {
   const iconUrl = "http://example.org/browser/extensions/cookie/test/damonbowling.jpg";
+  const pageUrl = "http://example.com/browser/extensions/cookie/test/file_favicon.html";
 	await SpecialPowers.pushPrefEnv({"set": [["network.cookie.cookieBehavior", 1]]});
 
   let promise = TestUtils.topicObserved("cookie-rejected", subject => {
@@ -11,13 +11,12 @@ add_task(async function() {
     return uri.spec == iconUrl;
   });
 
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, "http://example.com/");
+  // Kick off a page load that will load the favicon.
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, pageUrl);
   registerCleanupFunction(async function() {
     BrowserTestUtils.removeTab(tab);
   });
 
-  // Kick off a favicon load.
-  gBrowser.setIcon(tab, iconUrl);
   await promise;
   ok(true, "foreign favicon cookie was blocked");
 });
