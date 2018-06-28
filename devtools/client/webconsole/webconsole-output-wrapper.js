7e212bb462facb4ac961ec3b93fe980b7995dedd
yulia <ystartsev@mozilla.com>
2018-06-28 12:00:55 +0200

    Bug 1464461 - Make dispatchMessagesAdd use batching; r=nchevobbe a=ritu
    MozReview-Commit-ID: 7sCFIfC2ovi
    
    --HG--
    extra : source : f1f577e0d6f47476d008e6afd99f5d7d3668a518
    extra : histedit_source : e38540c85e0d1edcca39535ddf7eb7b80f019769

--- devtools/client/webconsole/webconsole-output-wrapper.js
+++ devtools/client/webconsole/webconsole-output-wrapper.js
@@ -250,12 +250,12 @@ WebConsoleOutputWrapper.prototype = {
       promise = Promise.resolve();
     }
 
-    this.batchedMessagesAdd(packet);
+    this.batchedMessageAdd(packet);
     return promise;
   },
 
   dispatchMessagesAdd: function(messages) {
-    store.dispatch(actions.messagesAdd(messages));
+    this.batchedMessagesAdd(messages);
   },
 
   dispatchMessagesClear: function() {
@@ -362,8 +362,13 @@ WebConsoleOutputWrapper.prototype = {
     this.setTimeoutIfNeeded();
   },
 
-  batchedMessagesAdd: function(message) {
+  batchedMessageAdd: function(message) {
     this.queuedMessageAdds.push(message);
+    this.setTimeoutIfNeeded();
+  },
+
+  batchedMessagesAdd: function(messages) {
+    this.queuedMessageAdds = this.queuedMessageAdds.concat(messages);
     this.setTimeoutIfNeeded();
   },
 
