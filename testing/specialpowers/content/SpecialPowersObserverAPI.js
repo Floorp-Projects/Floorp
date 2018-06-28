193c9c55244fee155876643fb5838fd982b47a77
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-06-28 09:40:32 +0100

    Bug 1471573 - Make SpecialPowers RequestDumpCoverageCounters and RequestResetCoverageCounters async messages that wait on the dumping/resetting to actually happen. r=jmaher
    --HG--
    extra : rebase_source : 4bc9d1823d3257b1a9f55df7b2e63487d5dd7f97

--- testing/specialpowers/content/SpecialPowersObserverAPI.js
+++ testing/specialpowers/content/SpecialPowersObserverAPI.js
@@ -11,6 +11,7 @@ XPCOMUtils.defineLazyModuleGetters(this, {
   ExtensionTestCommon: "resource://testing-common/ExtensionTestCommon.jsm",
   NetUtil: "resource://gre/modules/NetUtil.jsm",
   Services: "resource://gre/modules/Services.jsm",
+  PerTestCoverageUtils: "resource://testing-common/PerTestCoverageUtils.jsm",
 });
 
 this.SpecialPowersError = function(aMsg) {
@@ -554,16 +555,16 @@ SpecialPowersObserverAPI.prototype = {
       }
 
       case "SPRequestDumpCoverageCounters": {
-        let codeCoverage = Cc["@mozilla.org/tools/code-coverage;1"].
-                           getService(Ci.nsICodeCoverage);
-        codeCoverage.dumpCounters();
+        PerTestCoverageUtils.afterTest().then(() =>
+          this._sendReply(aMessage, "SPRequestDumpCoverageCounters", {})
+        );
         return undefined; // See comment at the beginning of this function.
       }
 
       case "SPRequestResetCoverageCounters": {
-        let codeCoverage = Cc["@mozilla.org/tools/code-coverage;1"].
-                           getService(Ci.nsICodeCoverage);
-        codeCoverage.resetCounters();
+        PerTestCoverageUtils.beforeTest().then(() =>
+          this._sendReply(aMessage, "SPRequestResetCoverageCounters", {})
+        );
         return undefined; // See comment at the beginning of this function.
       }
 
