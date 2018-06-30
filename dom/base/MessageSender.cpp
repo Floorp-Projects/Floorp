675c4d2509189a5b261cf5bc62a3e965a5c61c91
Emilio Cobos Álvarez <emilio@crisal.io>
2018-06-30 06:13:46 +0200

    Bug 1472403: Simplify generated image content. r=bz,dholbert
    This way we reuse the same machinery everywhere for the content property.
    
    The only difference is that we need to look at the parent style for content
    instead of just our style, and at a given index.
    
    Again, this is fine because changing content reframes, so no chance to mess up.
    
    This allows the generated content stuff to not implement nsImageLoadingContent
    and all that stuff, nor deal with events, which makes it much simpler IMO.
    
    Now it just tracks an index. We may not even need for it to be an HTML element,
    but I've kept that for now.
    
    I added a crashtest that used to crash because of the bogus
    nsCSSFrameConstructor code which trusted the node name without checking it was
    native anonymous.
    
    Differential Revision: https://phabricator.services.mozilla.com/D1897
    
    MozReview-Commit-ID: 1pAzIvRRVnL

--- dom/base/MessageSender.cpp
+++ dom/base/MessageSender.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/MessageSender.h"
+#include "mozilla/dom/MessageBroadcaster.h"
 
 namespace mozilla {
 namespace dom {
