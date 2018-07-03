0b0f08aedbad19beaea23d871bf04767e919bbf9
Nathan Froyd <froydnj@mozilla.com>
2018-07-03 17:04:27 -0400

    Bug 1472806 - fix -Wpessimizing-move warnings in Interceptor.cpp; r=aklotz
    There's no need to invoke std::move here, because Get() is already
    returning a temporary that can be moved into the RefPtr.

--- ipc/mscom/Interceptor.cpp
+++ ipc/mscom/Interceptor.cpp
@@ -244,7 +244,7 @@ Interceptor::Create(STAUniquePtr<IUnknown> aTarget, IInterceptorSink* aSink,
 
   detail::LiveSetAutoLock lock(GetLiveSet());
 
-  RefPtr<IWeakReference> existingWeak(std::move(GetLiveSet().Get(aTarget.get())));
+  RefPtr<IWeakReference> existingWeak(GetLiveSet().Get(aTarget.get()));
   if (existingWeak) {
     RefPtr<IWeakReferenceSource> existingStrong;
     if (SUCCEEDED(existingWeak->ToStrongRef(getter_AddRefs(existingStrong)))) {
@@ -891,7 +891,7 @@ Interceptor::DisconnectRemotesForTarget(IUnknown* aTarget)
 
   // It is not an error if the interceptor doesn't exist, so we return
   // S_FALSE instead of an error in that case.
-  RefPtr<IWeakReference> existingWeak(std::move(GetLiveSet().Get(aTarget)));
+  RefPtr<IWeakReference> existingWeak(GetLiveSet().Get(aTarget));
   if (!existingWeak) {
     return S_FALSE;
   }
