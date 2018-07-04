dfd438c078f2210570be077953dcacbac3b45775
Coroiu Cristina <ccoroiu@mozilla.com>
2018-07-05 00:24:27 +0300

    Backed out 2 changesets (bug 1470333) for xpcshell failures at browser/extensions/formautofill/test/unit/test_getFormInputDetails.js
    Backed out changeset dbea1942a32f (bug 1470333)
    Backed out changeset 440ee3fab72a (bug 1470333)

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -42,6 +42,7 @@ const whitelist = {
     "resource://gre/modules/XPCOMUtils.jsm",
 
     // Logging related
+    "resource://gre/modules/Console.jsm", // bug 1470333
     "resource://gre/modules/Log.jsm",
 
     // Session store
