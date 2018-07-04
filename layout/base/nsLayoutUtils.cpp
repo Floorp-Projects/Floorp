3601d76a6c7427001589248c5605ebfb311b9101
Bas Schouten <bschouten@mozilla.com>
2018-07-04 18:52:10 +0000

    Bug 1473376: Ensure hit test regions never grow excessively complex. r=kats

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -9471,6 +9471,17 @@ nsLayoutUtils::TransformToAncestorAndCombineRegions(
   }
   nsRegion* dest = isPrecise ? aPreciseTargetDest : aImpreciseTargetDest;
   dest->OrWith(transformedRegion.ToRegion());
+  // If the region becomes too complex this has a large performance impact.
+  // We limit its complexity here.
+  if (dest->GetNumRects() > 12) {
+    dest->SimplifyOutward(6);
+    if (isPrecise) {
+      aPreciseTargetDest->OrWith(*aImpreciseTargetDest);
+      *aImpreciseTargetDest = std::move(*aPreciseTargetDest);
+      aImpreciseTargetDest->SimplifyOutward(6);
+      *aPreciseTargetDest = nsRegion();
+    }
+  }
 }
 
 /* static */ bool
