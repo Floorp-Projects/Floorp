9b3d45345615440464ef3d4dc08f6ba077393bfb
Vlad Baicu <vlad.baicu@softvision.ro>
2018-07-05 19:35:16 +0300

    Bug 1465102 - Updated NotificationService for Oreo. r=JanH
    Modified NotificationClient to use new start/stop intents for NotificationService for
    Android O. We are keeping a reference to the notification the service was first
    started with in order to comply with the mandatory startForeground call for Oreo.
    
    MozReview-Commit-ID: 4CzM4pvANJt

--- mobile/android/base/java/org/mozilla/gecko/notifications/NotificationService.java
+++ mobile/android/base/java/org/mozilla/gecko/notifications/NotificationService.java
@@ -10,28 +10,52 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.R;
 
 public final class NotificationService extends Service {
+    /**
+     * Start Intent of the service must contain a notification extra.
+     */
     public static final String EXTRA_NOTIFICATION = "notification";
+    /**
+     * Stop Intent of the service must contain the stop action extra.
+     */
+    public static final String EXTRA_ACTION_STOP = "action_stop";
+
+    /**
+     * We are holding a reference to the notification this service was started with in order to use it
+     * again when we queue the stop action thus satisfying the Oreo limitations.
+     */
+    private Notification currentNotification;
 
     @Override // Service
     public int onStartCommand(final Intent intent, final int flags, final int startId) {
-        final Notification notification = intent.getParcelableExtra(EXTRA_NOTIFICATION);
-        if (notification != null) {
-            // Start foreground notification.
-            startForeground(R.id.foregroundNotification, notification);
-            return START_NOT_STICKY;
-        }
-
-        // Stop foreground notification
-        stopForeground(true);
-        stopSelfResult(startId);
+        handleIntent(intent, startId);
         return START_NOT_STICKY;
     }
 
     @Override // Service
     public IBinder onBind(final Intent intent) {
         return null;
+    }
+
+    private void handleIntent(Intent intent, int startId) {
+        //Start Intent
+        if (intent.hasExtra(EXTRA_NOTIFICATION)) {
+            currentNotification = intent.getParcelableExtra(EXTRA_NOTIFICATION);
+            startForeground(R.id.foregroundNotification, currentNotification);
+        }
+
+        //Stop Intent
+        if (intent.hasExtra(NotificationService.EXTRA_ACTION_STOP)) {
+            //Call necessary for Oreo+ only
+            if (!AppConstants.Versions.preO) {
+                startForeground(R.id.foregroundNotification, currentNotification);
+            }
+
+            stopForeground(true);
+            stopSelfResult(startId);
+        }
     }
 }
