7ab03fb6a2219756425db79ccbbb8cbc1ab9afa2
Tarek Ziadé <tarek@mozilla.com>
2018-07-06 13:43:08 +0200

    Bug 1472718 - Convert ChromeUtils.requestIOActivity to a Promise - r=baku,valentin
    Changes:
    
    - The API now returns a Promise containing a sequence of IOActivityData dictionnaries.
    - All the code related to notifications and XPCOM is removed.
    - The counters are no longer reset to 0 when the API is called
    
    MozReview-Commit-ID: 7J2EgFqDgf

--- netwerk/base/IOActivityMonitor.h
+++ netwerk/base/IOActivityMonitor.h
@@ -7,88 +7,45 @@
 #ifndef IOActivityMonitor_h___
 #define IOActivityMonitor_h___
 
+#include "mozilla/dom/ChromeUtilsBinding.h"
 #include "nsCOMPtr.h"
 #include "nscore.h"
 #include "nsClassHashtable.h"
 #include "nsDataHashtable.h"
 #include "nsHashKeys.h"
-#include "nsIIOActivityData.h"
 #include "nsISupports.h"
-#include "nsITimer.h"
 #include "prinrval.h"
 #include "prio.h"
 #include "private/pprio.h"
 #include <stdint.h>
 
-namespace mozilla { namespace net {
+namespace mozilla {
+
+namespace dom {
+  class Promise;
+}
+
+namespace net {
 
 #define IO_ACTIVITY_ENABLED_PREF "io.activity.enabled"
-#define IO_ACTIVITY_INTERVAL_PREF "io.activity.intervalMilliseconds"
 
-//
-// IOActivity keeps track of the amount of data
-// sent and received for an FD / Location
-//
-struct IOActivity {
-  // the resource location, can be:
-  // - socket://ip:port
-  // - file://absolute/path
-  nsCString location;
+typedef nsDataHashtable<nsCStringHashKey, dom::IOActivityDataDictionary> Activities;
 
-  // bytes received/read (rx) and sent/written (tx)
-  uint32_t rx;
-  uint32_t tx;
-
-  explicit IOActivity(const nsACString& aLocation) {
-    location.Assign(aLocation);
-    rx = 0;
-    tx = 0;
-  }
-
-  // Returns true if no data was transferred
-  bool Inactive() {
-    return rx == 0 && tx == 0;
-  }
-
-  // Sets the data to zero
-  void Reset() {
-    rx = 0;
-    tx = 0;
-  }
-};
-
-typedef nsClassHashtable<nsCStringHashKey, IOActivity> Activities;
-
-// XPCOM Wrapper for an IOActivity
-class IOActivityData final : public nsIIOActivityData
-{
-public:
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIIOACTIVITYDATA
-  explicit IOActivityData(IOActivity aActivity)
-      : mActivity(aActivity) {}
-private:
-  ~IOActivityData() = default;
-  IOActivity mActivity;
-};
 
 // IOActivityMonitor has several roles:
 // - maintains an IOActivity per resource and updates it
-// - sends a dump of the activities to observers that wants
-//   to get that info, via a timer
+// - sends a dump of the activities to a promise via RequestActivities
 class IOActivityMonitor final
-    : public nsITimerCallback
-    , public nsINamed
+    : public nsINamed
 {
 public:
   IOActivityMonitor();
 
   NS_DECL_THREADSAFE_ISUPPORTS
-  NS_DECL_NSITIMERCALLBACK
   NS_DECL_NSINAMED
 
   // initializes and destroys the singleton
-  static nsresult Init(int32_t aInterval);
+  static nsresult Init();
   static nsresult Shutdown();
 
   // collect amounts of data that are written/read by location
@@ -101,25 +58,17 @@ public:
   static nsresult Write(PRFileDesc *fd, uint32_t aAmount);
 
   static bool IsActive();
-
-  // collects activities and notifies observers
-  // this method can be called manually or via the timer callback
-  static nsresult NotifyActivities();
+  static void RequestActivities(dom::Promise* aPromise);
 private:
-  virtual ~IOActivityMonitor() = default;
-  nsresult Init_Internal(int32_t aInterval);
-  nsresult Shutdown_Internal();
-
-  IOActivity* GetActivity(const nsACString& location);
-  nsresult Write_Internal(const nsACString& location, uint32_t aAmount);
-  nsresult Read_Internal(const nsACString& location, uint32_t aAmount);
-  nsresult NotifyActivities_Internal();
+  ~IOActivityMonitor() = default;
+  nsresult InitInternal();
+  nsresult ShutdownInternal();
+  bool IncrementActivity(const nsACString& location, uint32_t aRx, uint32_t aTx);
+  nsresult WriteInternal(const nsACString& location, uint32_t aAmount);
+  nsresult ReadInternal(const nsACString& location, uint32_t aAmount);
+  void RequestActivitiesInternal(dom::Promise* aPromise);
 
   Activities mActivities;
-
-  // timer used to send notifications
-  uint32_t mInterval;
-  nsCOMPtr<nsITimer> mTimer;
   // protects mActivities accesses
   Mutex mLock;
 };
