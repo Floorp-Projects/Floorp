0156d014463eca9165ee0ca0ea0b02aeedc68a1b
Neil Deakin <neil@mozilla.com>
2018-07-05 20:14:29 -0400

    Bug 1437638, move frame loader property from XULElement to XULFrameElement, removing many checks that only apply to child frames instead of every XUL element. Since it is assumed that most frames/browsers will have frame loaders created for them, and that there aren't many of them, we can use a member field instead of slots, so remove the slot property, r=bz

--- dom/xul/XULFrameElement.h
+++ dom/xul/XULFrameElement.h
@@ -16,11 +16,13 @@
 #include "nsXULElement.h"
 
 class nsIWebNavigation;
+class nsFrameLoader;
 
 namespace mozilla {
 namespace dom {
 
-class XULFrameElement final : public nsXULElement
+class XULFrameElement final : public nsXULElement,
+                              public nsIFrameLoaderOwner
 {
 public:
   explicit XULFrameElement(already_AddRefed<mozilla::dom::NodeInfo>& aNodeInfo)
@@ -28,17 +30,63 @@ public:
   {
   }
 
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(XULFrameElement, nsXULElement)
+
+  // XULFrameElement.webidl
   nsIDocShell* GetDocShell();
   already_AddRefed<nsIWebNavigation> GetWebNavigation();
   already_AddRefed<nsPIDOMWindowOuter> GetContentWindow();
   nsIDocument* GetContentDocument();
+
+  // nsIFrameLoaderOwner / MozFrameLoaderOwner
+  NS_IMETHOD_(already_AddRefed<nsFrameLoader>) GetFrameLoader() override
+  {
+    return do_AddRef(mFrameLoader);
+  }
+
+  NS_IMETHOD_(void) InternalSetFrameLoader(nsFrameLoader* aFrameLoader) override
+  {
+    mFrameLoader = aFrameLoader;
+  }
+
+  void PresetOpenerWindow(mozIDOMWindowProxy* aWindow, ErrorResult& aRv)
+  {
+    mOpener = do_QueryInterface(aWindow);
+  }
+
+  void SwapFrameLoaders(mozilla::dom::HTMLIFrameElement& aOtherLoaderOwner,
+                        mozilla::ErrorResult& rv);
+  void SwapFrameLoaders(XULFrameElement& aOtherLoaderOwner,
+                        mozilla::ErrorResult& rv);
+  void SwapFrameLoaders(nsIFrameLoaderOwner* aOtherLoaderOwner,
+                        mozilla::ErrorResult& rv);
+
+  // nsIContent
+  virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
+                              nsIContent* aBindingParent,
+                              bool aCompileEventHandlers) override;
+  virtual void UnbindFromTree(bool aDeep, bool aNullParent) override;
+  virtual void DestroyContent() override;
+
+
+  virtual nsresult AfterSetAttr(int32_t aNamespaceID, nsAtom* aName,
+                                const nsAttrValue* aValue,
+                                const nsAttrValue* aOldValue,
+                                nsIPrincipal* aSubjectPrincipal,
+                                bool aNotify) override;
 
 protected:
   virtual ~XULFrameElement()
   {
   }
 
+  RefPtr<nsFrameLoader> mFrameLoader;
+  nsCOMPtr<nsPIDOMWindowOuter> mOpener;
+
   JSObject* WrapNode(JSContext *aCx, JS::Handle<JSObject*> aGivenProto) override;
+
+  void LoadSrc();
 };
 
 } // namespace dom
