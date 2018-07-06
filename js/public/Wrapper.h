7ecfdba44bc175f5bd58d4823788d81d36a0c66f
Jan de Mooij <jdemooij@mozilla.com>
2018-07-06 12:53:38 +0200

    Bug 1468752 part 2 - Define Wrapper::wrapperHandler in Wrapper.h. r=bz
    IsCrossCompartmentWrapper calls IsWrapper and then had an out-of-line call to Wrapper::wrapperHandler, but that's just loading the proxy handler and we already did that in IsWrapper. Having Wrapper::wrapperHandler in the header file means the compiler can compile IsCrossCompartmentWrapper much better.

--- js/public/Wrapper.h
+++ js/public/Wrapper.h
@@ -143,7 +143,7 @@ class JS_FRIEND_API(Wrapper) : public ForwardingProxyHandler
 
     static JSObject* Renew(JSObject* existing, JSObject* obj, const Wrapper* handler);
 
-    static const Wrapper* wrapperHandler(const JSObject* wrapper);
+    static inline const Wrapper* wrapperHandler(const JSObject* wrapper);
 
     static JSObject* wrappedObject(JSObject* wrapper);
 
@@ -345,6 +345,13 @@ IsCrossCompartmentWrapper(const JSObject* obj)
 {
     return IsWrapper(obj) &&
            (Wrapper::wrapperHandler(obj)->flags() & Wrapper::CROSS_COMPARTMENT);
+}
+
+/* static */ inline const Wrapper*
+Wrapper::wrapperHandler(const JSObject* wrapper)
+{
+    MOZ_ASSERT(IsWrapper(wrapper));
+    return static_cast<const Wrapper*>(GetProxyHandler(wrapper));
 }
 
 // Given a JSObject, returns that object stripped of wrappers. If
