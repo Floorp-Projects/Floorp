0d504e92c3ee03af89085fff2d8a9bec6e0d8b95
Neil Deakin <neil@mozilla.com>
2018-07-05 20:14:29 -0400

    Bug 1437638, move frame loader property from XULElement to XULFrameElement, removing many checks that only apply to child frames instead of every XUL element. Since it is assumed that most frames/browsers will have frame loaders created for them, and that there aren't many of them, we can use a member field instead of slots, so remove the slot property, r=bz

--- dom/xul/XULFrameElement.cpp
+++ dom/xul/XULFrameElement.cpp
@@ -7,11 +7,33 @@
 #include "nsCOMPtr.h"
 #include "nsIContent.h"
 #include "nsFrameLoader.h"
+#include "mozilla/AsyncEventDispatcher.h"
+#include "mozilla/dom/HTMLIFrameElement.h"
 #include "mozilla/dom/XULFrameElement.h"
 #include "mozilla/dom/XULFrameElementBinding.h"
 
 namespace mozilla {
 namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(XULFrameElement)
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(XULFrameElement,
+                                                  nsXULElement)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mFrameLoader);
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mOpener);
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(XULFrameElement,
+                                                nsXULElement)
+  if (tmp->mFrameLoader) {
+    tmp->mFrameLoader->Destroy();
+  }
+  NS_IMPL_CYCLE_COLLECTION_UNLINK(mFrameLoader, mOpener)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_IMPL_ISUPPORTS_CYCLE_COLLECTION_INHERITED(XULFrameElement,
+                                             nsXULElement,
+                                             nsIFrameLoaderOwner)
 
 JSObject*
 XULFrameElement::WrapNode(JSContext *aCx, JS::Handle<JSObject*> aGivenProto)
@@ -51,6 +73,137 @@ XULFrameElement::GetContentDocument()
 {
   nsCOMPtr<nsPIDOMWindowOuter> win = GetContentWindow();
   return win ? win->GetDoc() : nullptr;
+}
+
+void
+XULFrameElement::LoadSrc()
+{
+  if (!IsInUncomposedDoc() ||
+      !OwnerDoc()->GetRootElement() ||
+      OwnerDoc()->GetRootElement()->
+        NodeInfo()->Equals(nsGkAtoms::overlay, kNameSpaceID_XUL)) {
+      return;
+  }
+  RefPtr<nsFrameLoader> frameLoader = GetFrameLoader();
+  if (!frameLoader) {
+    // Check if we have an opener we need to be setting
+    nsCOMPtr<nsPIDOMWindowOuter> opener = mOpener;
+    if (!opener) {
+      // If we are a primary xul-browser, we want to take the opener property!
+      nsCOMPtr<nsPIDOMWindowOuter> window = OwnerDoc()->GetWindow();
+      if (AttrValueIs(kNameSpaceID_None, nsGkAtoms::primary,
+                      nsGkAtoms::_true, eIgnoreCase) && window) {
+        opener = window->TakeOpenerForInitialContentBrowser();
+      }
+    }
+    mOpener = nullptr;
+
+    // false as the last parameter so that xul:iframe/browser/editor
+    // session history handling works like dynamic html:iframes.
+    // Usually xul elements are used in chrome, which doesn't have
+    // session history at all.
+    mFrameLoader = nsFrameLoader::Create(this, opener, false);
+    if (NS_WARN_IF(!mFrameLoader)) {
+      return;
+    }
+
+    (new AsyncEventDispatcher(this,
+                              NS_LITERAL_STRING("XULFrameLoaderCreated"),
+                              CanBubble::eYes))->RunDOMEventWhenSafe();
+  }
+
+  mFrameLoader->LoadFrame(false);
+}
+
+void
+XULFrameElement::SwapFrameLoaders(HTMLIFrameElement& aOtherLoaderOwner,
+                                  ErrorResult& rv)
+{
+  aOtherLoaderOwner.SwapFrameLoaders(this, rv);
+}
+
+void
+XULFrameElement::SwapFrameLoaders(XULFrameElement& aOtherLoaderOwner,
+                                  ErrorResult& rv)
+{
+  if (&aOtherLoaderOwner == this) {
+    // nothing to do
+    return;
+  }
+
+  aOtherLoaderOwner.SwapFrameLoaders(this, rv);
+}
+
+void
+XULFrameElement::SwapFrameLoaders(nsIFrameLoaderOwner* aOtherLoaderOwner,
+                                  mozilla::ErrorResult& rv)
+{
+  RefPtr<nsFrameLoader> loader = GetFrameLoader();
+  RefPtr<nsFrameLoader> otherLoader = aOtherLoaderOwner->GetFrameLoader();
+  if (!loader || !otherLoader) {
+    rv.Throw(NS_ERROR_NOT_IMPLEMENTED);
+    return;
+  }
+
+  rv = loader->SwapWithOtherLoader(otherLoader, this, aOtherLoaderOwner);
+}
+
+nsresult
+XULFrameElement::BindToTree(nsIDocument* aDocument,
+                            nsIContent* aParent,
+                            nsIContent* aBindingParent,
+                            bool aCompileEventHandlers)
+{
+  nsresult rv = nsXULElement::BindToTree(aDocument, aParent, aBindingParent, aCompileEventHandlers);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (aDocument) {
+    NS_ASSERTION(!nsContentUtils::IsSafeToRunScript(),
+                 "Missing a script blocker!");
+    // We're in a document now.  Kick off the frame load.
+    LoadSrc();
+  }
+
+  return NS_OK;
+}
+
+void
+XULFrameElement::UnbindFromTree(bool aDeep, bool aNullParent)
+{
+  RefPtr<nsFrameLoader> frameLoader = GetFrameLoader();
+  if (frameLoader) {
+    frameLoader->Destroy();
+  }
+  mFrameLoader = nullptr;
+
+  nsXULElement::UnbindFromTree(aDeep, aNullParent);
+}
+
+void
+XULFrameElement::DestroyContent()
+{
+  RefPtr<nsFrameLoader> frameLoader = GetFrameLoader();
+  if (frameLoader) {
+    frameLoader->Destroy();
+  }
+  mFrameLoader = nullptr;
+
+  nsXULElement::DestroyContent();
+}
+
+nsresult
+XULFrameElement::AfterSetAttr(int32_t aNamespaceID, nsAtom* aName,
+                              const nsAttrValue* aValue,
+                              const nsAttrValue* aOldValue,
+                              nsIPrincipal* aSubjectPrincipal,
+                              bool aNotify)
+{
+  if (aNamespaceID == kNameSpaceID_None && aName == nsGkAtoms::src && aValue) {
+    LoadSrc();
+  }
+
+  return nsXULElement::AfterSetAttr(aNamespaceID, aName,
+                                    aValue, aOldValue, aSubjectPrincipal, aNotify);
 }
 
 } // namespace dom
