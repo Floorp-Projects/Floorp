7742b9908350b53258ce1adbbc959ba4d1df674b
Bobby Holley <bobbyholley@gmail.com>
2018-07-07 07:20:50 +0000

    Bug 1447856 - Make the watchdog tests actually wait for completion. r=arai, a=test-only
    MozReview-Commit-ID: KjvUxJSrmtk
    
    Differential Revision: https://phabricator.services.mozilla.com/D2017

--- js/xpconnect/tests/unit/test_watchdog_disable.js
+++ js/xpconnect/tests/unit/test_watchdog_disable.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-async function testBody() {
+function testBody() {
   setWatchdogEnabled(false);
-  await checkWatchdog(false);
+  return checkWatchdog(false);
 }
--- js/xpconnect/tests/unit/test_watchdog_toggle.js
+++ js/xpconnect/tests/unit/test_watchdog_toggle.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-async function testBody() {
+function testBody() {
   var defaultBehavior = isWatchdogEnabled();
   setWatchdogEnabled(!defaultBehavior);
   setWatchdogEnabled(defaultBehavior);
-  await checkWatchdog(defaultBehavior);
+  return checkWatchdog(defaultBehavior);
 }
