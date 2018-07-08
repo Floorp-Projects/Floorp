be6d4ad80e5a50cb13e9e942a0ba2b47111f8d25
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2018-07-07 23:23:43 -0700

    Bug 1469464 - Display the currency code with the total. r=sfoster
    MozReview-Commit-ID: JJIbFBAYC9U

--- browser/components/payments/res/components/currency-amount.js
+++ browser/components/payments/res/components/currency-amount.js
@@ -5,33 +5,68 @@
 "use strict";
 
 /**
- * <currency-amount value="7.5" currency="USD"></currency-amount>
+ * <currency-amount value="7.5" currency="USD" display-code></currency-amount>
  */
 
 import ObservedPropertiesMixin from "../mixins/ObservedPropertiesMixin.js";
 
 export default class CurrencyAmount extends ObservedPropertiesMixin(HTMLElement) {
   static get observedAttributes() {
-    return ["currency", "value"];
+    return [
+      "currency",
+      "display-code",
+      "value",
+    ];
+  }
+
+  constructor() {
+    super();
+    this._currencyAmountTextNode = document.createTextNode("");
+    this._currencyCodeElement = document.createElement("span");
+    this._currencyCodeElement.classList.add("currency-code");
+  }
+
+  connectedCallback() {
+    if (super.connectedCallback) {
+      super.connectedCallback();
+    }
+
+    this.append(this._currencyAmountTextNode, this._currencyCodeElement);
   }
 
   render() {
-    let output = "";
+    let currencyAmount = "";
+    let currencyCode = "";
     try {
       if (this.value && this.currency) {
         let number = Number.parseFloat(this.value);
         if (Number.isNaN(number) || !Number.isFinite(number)) {
           throw new RangeError("currency-amount value must be a finite number");
         }
-        const formatter = new Intl.NumberFormat(navigator.languages, {
+        const symbolFormatter = new Intl.NumberFormat(navigator.languages, {
           style: "currency",
           currency: this.currency,
           currencyDisplay: "symbol",
         });
-        output = formatter.format(this.value);
+        currencyAmount = symbolFormatter.format(this.value);
+
+        if (this.displayCode !== null) {
+          // XXX: Bug 1473772 will move the separator to a Fluent string.
+          currencyAmount += " ";
+
+          const codeFormatter = new Intl.NumberFormat(navigator.languages, {
+            style: "currency",
+            currency: this.currency,
+            currencyDisplay: "code",
+          });
+          let parts = codeFormatter.formatToParts(this.value);
+          let currencyPart = parts.find(part => part.type == "currency");
+          currencyCode = currencyPart.value;
+        }
       }
     } finally {
-      this.textContent = output;
+      this._currencyAmountTextNode.textContent = currencyAmount;
+      this._currencyCodeElement.textContent = currencyCode;
     }
   }
 }
