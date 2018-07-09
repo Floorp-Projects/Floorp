87bf0f7f230c7c19e3acd2c4e6f409c192ba31df
Ben Kelly <ben@wanderview.com>
2018-07-09 16:02:41 -0700

    Bug 1459209 P15 Implement RemoteServiceWorkerContainerImpl::GetReady() across IPC to the parent-side SWM. r=mrbkap
    --HG--
    extra : rebase_source : 119a1cb66828c3dbb21cce86aca67793f0657321

--- dom/serviceworkers/ServiceWorkerContainerProxy.cpp
+++ dom/serviceworkers/ServiceWorkerContainerProxy.cpp
@@ -126,5 +126,32 @@ ServiceWorkerContainerProxy::GetRegistrations(const ClientInfo& aClientInfo)
   return promise;
 }
 
+RefPtr<ServiceWorkerRegistrationPromise>
+ServiceWorkerContainerProxy::GetReady(const ClientInfo& aClientInfo)
+{
+  AssertIsOnBackgroundThread();
+
+  RefPtr<ServiceWorkerRegistrationPromise::Private> promise =
+    new ServiceWorkerRegistrationPromise::Private(__func__);
+
+  nsCOMPtr<nsIRunnable> r = NS_NewRunnableFunction(__func__,
+    [aClientInfo, promise] () mutable {
+      auto scopeExit = MakeScopeExit([&] {
+        promise->Reject(NS_ERROR_DOM_INVALID_STATE_ERR, __func__);
+      });
+
+      RefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
+      NS_ENSURE_TRUE_VOID(swm);
+
+      swm->WhenReady(aClientInfo)->ChainTo(promise.forget(), __func__);
+
+      scopeExit.release();
+    });
+
+  MOZ_ALWAYS_SUCCEEDS(SystemGroup::Dispatch(TaskCategory::Other, r.forget()));
+
+  return promise;
+}
+
 } // namespace dom
 } // namespace mozilla
