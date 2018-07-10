8a442a48484868175d32d5b83d7436b061eed95e
James Graham <james@hoppipolla.co.uk>
2018-07-10 10:09:43 +0100

    Bug 1476053 - Add an option not to store state when running wpt-update, r=jdm
    This adds some overhead and can be annoying since it requries explicitly aborting failed jobs.
    The state storage isn't very useful for just udpating metadata (the typical gecko usecase), but
    is useful for performing syncs (a typical servo usecase). Therefore add a --no-store-state option
    and set it by default in the gecko mach frontend.
    
    MozReview-Commit-ID: LhEcMkyuRHD
    
    --HG--
    extra : rebase_source : 0f5c22b2a1337f7ca370cae31514792528d234a3

--- testing/web-platform/tests/tools/wptrunner/wptrunner/wptcommandline.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/wptcommandline.py
@@ -554,8 +554,12 @@ def create_parser_update(product_choices=None):
     parser.add_argument("--stability", nargs="?", action="store", const="unstable", default=None,
         help=("Reason for disabling tests. When updating test results, disable tests that have "
               "inconsistent results across many runs with the given reason."))
-    parser.add_argument("--continue", action="store_true", help="Continue a previously started run of the update script")
-    parser.add_argument("--abort", action="store_true", help="Clear state from a previous incomplete run of the update script")
+    parser.add_argument("--no-store-state", action="store_false", dest="store_state",
+                        help="Store state so that steps can be resumed after failure")
+    parser.add_argument("--continue", action="store_true",
+                        help="Continue a previously started run of the update script")
+    parser.add_argument("--abort", action="store_true",
+                        help="Clear state from a previous incomplete run of the update script")
     parser.add_argument("--exclude", action="store", nargs="*",
                         help="List of glob-style paths to exclude when syncing tests")
     parser.add_argument("--include", action="store", nargs="*",
