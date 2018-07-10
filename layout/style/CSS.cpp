5c44ca0c7fa1b4f584596b81a0c78420cf9d3a94
Cameron McCormack <cam@mcc.id.au>
2018-07-10 15:51:38 +1000

    Bug 1474513 - Simplify CSS::Supports. r=emilio
    MozReview-Commit-ID: Lw9K7uRknfv

--- layout/style/CSS.cpp
+++ layout/style/CSS.cpp
@@ -19,47 +19,12 @@
 namespace mozilla {
 namespace dom {
 
-struct SupportsParsingInfo
-{
-  nsIURI* mDocURI;
-  nsIURI* mBaseURI;
-  nsIPrincipal* mPrincipal;
-};
-
-static nsresult
-GetParsingInfo(const GlobalObject& aGlobal,
-               SupportsParsingInfo& aInfo)
-{
-  nsGlobalWindowInner* win = xpc::WindowOrNull(aGlobal.Get());
-  if (!win) {
-    return NS_ERROR_FAILURE;
-  }
-
-  nsCOMPtr<nsIDocument> doc = win->GetDoc();
-  if (!doc) {
-    return NS_ERROR_FAILURE;
-  }
-
-  aInfo.mDocURI = nsCOMPtr<nsIURI>(doc->GetDocumentURI()).get();
-  aInfo.mBaseURI = nsCOMPtr<nsIURI>(doc->GetBaseURI()).get();
-  aInfo.mPrincipal = win->GetPrincipal();
-  return NS_OK;
-}
-
 /* static */ bool
 CSS::Supports(const GlobalObject& aGlobal,
               const nsAString& aProperty,
               const nsAString& aValue,
               ErrorResult& aRv)
 {
-  SupportsParsingInfo info;
-
-  nsresult rv = GetParsingInfo(aGlobal, info);
-  if (NS_FAILED(rv)) {
-    aRv.Throw(rv);
-    return false;
-  }
-
   NS_ConvertUTF16toUTF8 property(aProperty);
   NS_ConvertUTF16toUTF8 value(aValue);
   return Servo_CSSSupports2(&property, &value);
@@ -70,14 +35,6 @@ CSS::Supports(const GlobalObject& aGlobal,
               const nsAString& aCondition,
               ErrorResult& aRv)
 {
-  SupportsParsingInfo info;
-
-  nsresult rv = GetParsingInfo(aGlobal, info);
-  if (NS_FAILED(rv)) {
-    aRv.Throw(rv);
-    return false;
-  }
-
   NS_ConvertUTF16toUTF8 cond(aCondition);
   return Servo_CSSSupports(&cond);
 }
