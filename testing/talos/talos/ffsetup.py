3d5fdcb2b17daab36a94f7090070f5399aaeff95
Kartikaya Gupta <kgupta@mozilla.com>
2018-07-10 07:45:08 -0400

    Bug 1471650 - Profile WebRender threads in talos if WebRender is enabled. r=jmaher
    MozReview-Commit-ID: DriYeGhqvIZ

--- testing/talos/talos/ffsetup.py
+++ testing/talos/talos/ffsetup.py
@@ -201,7 +201,8 @@ class FFSetup(object):
         if upload_dir and self.test_config.get('gecko_profile'):
             self.gecko_profile = GeckoProfile(upload_dir,
                                               self.browser_config,
-                                              self.test_config)
+                                              self.test_config,
+                                              str(os.getenv('MOZ_WEBRENDER')) == '1')
             self.gecko_profile.update_env(self.env)
 
     def clean(self):
