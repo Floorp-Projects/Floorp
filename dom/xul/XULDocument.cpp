7e68e78474842bbe8c0b8f1585b166af1f5cfda8
Brendan Dahl <brendan.dahl@gmail.com>
2018-07-12 13:55:55 -0700

    Bug 1475262 - Only ignore XUL persist on top level windows. r=smaug a=lizzard
    Fixes the incorrect assumption that all XUL <window> elements are top level
    windows and will have their attribute persistence handled by the
    nsXULWindow. An example of where this assumption fails is the password
    manager which opens a <window> within a sub dialog that has no nsXULWindow.
    
    MozReview-Commit-ID: 1f59QPIz42s

--- dom/xul/XULDocument.cpp
+++ dom/xul/XULDocument.cpp
@@ -1166,9 +1166,11 @@ XULDocument::Persist(Element* aElement, int32_t aNameSpaceID,
         return mLocalStore->RemoveValue(uri, id, attrstr);
     }
 
-    // Persisting attributes to windows is handled by nsXULWindow.
+    // Persisting attributes to top level windows is handled by nsXULWindow.
     if (aElement->IsXULElement(nsGkAtoms::window)) {
-        return NS_OK;
+        if (nsCOMPtr<nsIXULWindow> win = GetXULWindowIfToplevelChrome()) {
+           return NS_OK;
+        }
     }
 
     return mLocalStore->SetValue(uri, id, attrstr, valuestr);
@@ -1758,9 +1760,12 @@ XULDocument::ApplyPersistentAttributesToElements(const nsAString &aID,
                  continue;
             }
 
-            // Applying persistent attributes to windows is handled by nsXULWindow.
+            // Applying persistent attributes to top level windows is handled
+            // by nsXULWindow.
             if (element->IsXULElement(nsGkAtoms::window)) {
-                continue;
+                if (nsCOMPtr<nsIXULWindow> win = GetXULWindowIfToplevelChrome()) {
+                    continue;
+                }
             }
 
             Unused << element->SetAttr(kNameSpaceID_None, attr, value, true);
