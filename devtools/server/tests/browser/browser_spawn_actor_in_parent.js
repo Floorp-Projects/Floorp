835840ac9907b951f45c20c1d97e9059ac8aab05
Alexandre Poirot <poirot.alex@gmail.com>
2018-07-16 09:53:28 -0700

    Bug 1477988 - Implements DebuggerServer.spawnActorInParentProcess. r=jryans
    This new API allows to instanciate an actor in the parent process from actors running in the parent process.
    The created actors are returned to the client from the actors running in content,
    but after that, the client communicates directly with the created actors running in the parent process.
    
    MozReview-Commit-ID: 5B3wRQ94UEx
    
    --HG--
    extra : rebase_source : 808ea31fe8a467d0185fc27056c6524873cddbce

---
+++ devtools/server/tests/browser/browser_spawn_actor_in_parent.js
@@ -0,0 +1,48 @@
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test DebuggerServerConnection.spawnActorInParentProcess.
+// This test instanciates a first test actor "InContentActor" that uses
+// spawnActorInParentProcess to instanciate the second test actor "InParentActor"
+
+const ACTOR_URL = "chrome://mochitests/content/browser/devtools/server/tests/browser/test-spawn-actor-in-parent.js";
+
+const { InContentFront, InParentFront } = require(ACTOR_URL);
+
+add_task(async function() {
+  await addTab("data:text/html;charset=utf-8,foo");
+
+  info("Register target-scoped actor in the content process");
+  await registerActorInContentProcess(ACTOR_URL, {
+    prefix: "inContent",
+    constructor: "InContentActor",
+    type: { target: true },
+  });
+
+  initDebuggerServer();
+  const client = new DebuggerClient(DebuggerServer.connectPipe());
+  const form = await connectDebuggerClient(client);
+  const inContentFront = InContentFront(client, form);
+  const isInContent = await inContentFront.isInContent();
+  ok(isInContent, "ContentActor really runs in the content process");
+  const formSpawn = await inContentFront.spawnInParent(ACTOR_URL);
+  const inParentFront = InParentFront(client, formSpawn);
+  const {
+    args,
+    isInParent,
+    conn,
+    mm
+  } = await inParentFront.test();
+  is(args[0], 1, "first actor constructor arg is correct");
+  is(args[1], 2, "first actor constructor arg is correct");
+  is(args[2], 3, "first actor constructor arg is correct");
+  ok(isInParent, "The ParentActor really runs in the parent process");
+  ok(conn, "`conn`, first contructor argument is a DebuggerServerConnection instance");
+  is(mm, "ChromeMessageSender", "`mm`, last constructor argument is a message manager");
+
+  await client.close();
+  gBrowser.removeCurrentTab();
+});
