2ec98e736f4caf93b3f4ba7e5d347066496ba2a0
Tim Nguyen <ntim.bugs@gmail.com>
2018-07-16 14:36:35 +0100

    Bug 1475978 - Add chromeOuterWindowID global to mozilla/frame-script ESLint environment. r=mconley
    MozReview-Commit-ID: FYfTDDY2d8j
    
    --HG--
    extra : rebase_source : b225e887b279305043511d8d7be5b92f642e7420

--- tools/lint/eslint/eslint-plugin-mozilla/lib/environments/frame-script.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/environments/frame-script.js
@@ -14,6 +14,7 @@ module.exports = {
     addWeakMessageListener: false,
     atob: false,
     btoa: false,
+    chromeOuterWindowID: false,
     content: false,
     docShell: false,
     privateNoteIntentionalCrash: false,
