2a1a759dac0bb2d01fcd8de8c123792e3b2c4a7b
Shane Caraveo <scaraveo@mozilla.com>
2018-07-16 09:08:30 -0300

    Bug 1455405 - Fix intermittent by using real events. r=rpl, a=test-only
    This test was testing that files are loaded/executed/etc in the page, but
    what we really care about is that the webrequest api works.  Other tests
    are responsible for stuff like css and js actually work.  The patch does
    maintain (fixed) the js test, but removes the css test for lack of a good
    way to properly wait for css to apply.
    
    MozReview-Commit-ID: B2uByaxNeK2

--- toolkit/components/extensions/test/mochitest/file_script_good.js
+++ toolkit/components/extensions/test/mochitest/file_script_good.js
@@ -1,3 +1,12 @@
 "use strict";
 
 window.success = window.success ? window.success + 1 : 1;
+
+{
+  let scripts = document.getElementsByTagName("script");
+  let url = new URL(scripts[scripts.length - 1].src);
+  let flag = url.searchParams.get("q");
+  if (flag) {
+    window.postMessage(flag, "*");
+  }
+}
