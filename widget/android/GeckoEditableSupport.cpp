a7923af05b170a18f21b0b539d1bf09688a34885
Jim Chen <nchen@mozilla.com>
2018-07-17 11:22:34 -0400

    Bug 1470786 - 1. Support async text changes from replacing text; r=esawin a=lizzard
    Currently, we expect editing operations in
    GeckoEditableSupport::OnImeReplaceText to cause synchronous text change
    notifications. However, under e10s, text change notifications can be
    asynchornous. The new code keeps track of active OnImeReplaceText calls,
    and look for async text changes before replying to the calls.
    
    MozReview-Commit-ID: INM3JLmQebK

--- widget/android/GeckoEditableSupport.cpp
+++ widget/android/GeckoEditableSupport.cpp
@@ -908,6 +908,11 @@ GeckoEditableSupport::FlushIMEChanges(FlushChangesFlag aFlags)
         mEditable->OnSelectionChange(selStart, selEnd);
         flushOnException();
     }
+
+    while (mIMEActiveReplaceTextCount) {
+        mIMEActiveReplaceTextCount--;
+        OnImeSynchronize();
+    }
 }
 
 void
@@ -966,11 +971,26 @@ void
 GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
                                        jni::String::Param aText)
 {
-    AutoIMESynchronize as(this);
+    mIMEActiveReplaceTextCount++;
+
+    if (!DoReplaceText(aStart, aEnd, aText)) {
+        // We did not process the event, so reply to it now.
+        mIMEActiveReplaceTextCount--;
+        OnImeSynchronize();
+    }
+}
+
+bool
+GeckoEditableSupport::DoReplaceText(int32_t aStart, int32_t aEnd,
+                                    jni::String::Param aText)
+{
+    // Return true if processed and we should reply to the OnImeReplaceText
+    // event later. Return false if _not_ processed and we should reply to the
+    // OnImeReplaceText event now.
 
     if (mIMEMaskEventsCount > 0) {
         // Not focused; still reply to events, but don't do anything else.
-        return;
+        return false;
     }
 
     if (mWindow) {
@@ -981,8 +1001,8 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         Replace text in Gecko thread from aStart to aEnd with the string text.
     */
     nsCOMPtr<nsIWidget> widget = GetWidget();
-    NS_ENSURE_TRUE_VOID(mDispatcher && widget);
-    NS_ENSURE_SUCCESS_VOID(BeginInputTransaction(mDispatcher));
+    NS_ENSURE_TRUE(mDispatcher && widget, false);
+    NS_ENSURE_SUCCESS(BeginInputTransaction(mDispatcher), false);
 
     RefPtr<TextComposition> composition(GetComposition());
     MOZ_ASSERT(!composition || !composition->IsEditorHandlingEvent());
@@ -1028,7 +1048,7 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
                 }
             }
             mIMEKeyEvents.Clear();
-            return;
+            return true;
         }
 
         if (aStart != aEnd) {
@@ -1038,7 +1058,7 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
             event.mTime = PR_Now() / 1000;
             widget->DispatchEvent(&event, status);
             if (!mDispatcher || widget->Destroyed()) {
-                return;
+                return false;
             }
         }
     } else if (composition->String().Equals(string)) {
@@ -1056,7 +1076,7 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         mInputContext.mMayBeIMEUnaware) {
         SendIMEDummyKeyEvent(widget, eKeyDown);
         if (!mDispatcher || widget->Destroyed()) {
-            return;
+            return false;
         }
     }
 
@@ -1069,7 +1089,7 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         mDispatcher->CommitComposition(status, &string);
     }
     if (!mDispatcher || widget->Destroyed()) {
-        return;
+        return false;
     }
 
     if (sDispatchKeyEventsInCompositionForAnyApps ||
@@ -1077,6 +1097,7 @@ GeckoEditableSupport::OnImeReplaceText(int32_t aStart, int32_t aEnd,
         SendIMEDummyKeyEvent(widget, eKeyUp);
         // Widget may be destroyed after dispatching the above event.
     }
+    return true;
 }
 
 void
@@ -1308,6 +1329,7 @@ GeckoEditableSupport::NotifyIME(TextEventDispatcher* aTextEventDispatcher,
             RefPtr<GeckoEditableSupport> self(this);
             nsAppShell::PostEvent([this, self] {
                 if (!mIMEFocusCount) {
+                    mIMEActiveReplaceTextCount = 0;
                     mEditable->NotifyIME(EditableListener::NOTIFY_IME_OF_BLUR);
                     OnRemovedFrom(mDispatcher);
                 }
