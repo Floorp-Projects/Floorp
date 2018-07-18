79542351684fedfcf19040d42841ab16f57bbc8a
Nika Layzell <nika@thelayzells.com>
2018-07-18 00:20:38 -0400

    Bug 1477432 - Part 5: Add a refreshCID method to JSCID objects, r=mccr8
    This lets us get rid of the method 'initalize', which currently needs the
    internal representation of JSCID.
    
    This particular method is removed entirely in Part 9, and only exists to keep
    intermediate states building & running.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2282

--- js/xpconnect/src/XPCComponents.cpp
+++ js/xpconnect/src/XPCComponents.cpp
@@ -677,13 +677,6 @@ nsXPCComponents_Classes::Resolve(nsIXPConnectWrappedNative* wrapper,
     return NS_OK;
 }
 
-NS_IMETHODIMP
-nsXPCComponents_Classes::Initialize(nsIJSCID* cid,
-                                    const char* str)
-{
-    return cid->Initialize(str);
-}
-
 
 /***************************************************************************/
 /***************************************************************************/
