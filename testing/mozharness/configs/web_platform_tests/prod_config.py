68d0e1032fe01db1f6982acf7ce344871454d8f5
James Graham <james@hoppipolla.co.uk>
2018-07-20 13:29:05 +0100

    Bug 1477256 - Use certificate files from upstream in web-platform-tests, r=cactusmachete
    These files are duplicates of each other, so for simplicity we may as well use the
    upstream copies to avoid falling out of sync.
    
    MozReview-Commit-ID: JpG3joT8NNJ

--- testing/mozharness/configs/web_platform_tests/prod_config.py
+++ testing/mozharness/configs/web_platform_tests/prod_config.py
@@ -8,9 +8,9 @@ config = {
         "--prefs-root=%(test_path)s/prefs",
         "--processes=1",
         "--config=%(test_path)s/wptrunner.ini",
-        "--ca-cert-path=%(test_path)s/certs/cacert.pem",
-        "--host-key-path=%(test_path)s/certs/web-platform.test.key",
-        "--host-cert-path=%(test_path)s/certs/web-platform.test.pem",
+        "--ca-cert-path=%(test_path)s/tests/tools/certs/cacert.pem",
+        "--host-key-path=%(test_path)s/tests/tools/certs/web-platform.test.key",
+        "--host-cert-path=%(test_path)s/tests/tools/certs/web-platform.test.pem",
         "--certutil-binary=%(test_install_path)s/bin/certutil",
     ],
 
