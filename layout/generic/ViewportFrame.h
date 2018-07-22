c27ea08e39eab0b1406c93b31093f1640962d163
Jeremy Lempereur <jeremy.lempereur@gmail.com>
2018-07-22 21:49:38 +0200

    Bug 1471708 - Rename the "scroll-position clamping scroll port size" to "visual viewport size". r=botond
    MozReview-Commit-ID: IWPzXSF5jfR

--- layout/generic/ViewportFrame.h
+++ layout/generic/ViewportFrame.h
@@ -68,7 +68,7 @@ public:
 
   /**
    * Adjust aReflowInput to account for scrollbars and pres shell
-   * GetScrollPositionClampingScrollPortSizeSet and
+   * GetVisualViewportSizeSet and
    * GetContentDocumentFixedPositionMargins adjustments.
    * @return the rect to use as containing block rect
    */
