720eb84fdbb04a60986ebd1119c4c00827b33915
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2018-07-22 02:07:16 -0700

    Bug 1463538 - Fail PaymentRequest UI tests upon console errors. r=jaws
    MozReview-Commit-ID: 2IzUaENZjfl

--- browser/components/payments/test/browser/head.js
+++ browser/components/payments/test/browser/head.js
@@ -313,11 +313,23 @@ function cleanupFormAutofillStorage() {
 }
 
 add_task(async function setup_head() {
+  SpecialPowers.registerConsoleListener(function onConsoleMessage(msg) {
+    if (msg.isWarning || !msg.errorMessage) {
+      // Ignore warnings and non-errors.
+      return;
+    }
+    if (msg.category == "CSP_CSPViolationWithURI" && msg.errorMessage.includes("at inline")) {
+      // Ignore unknown CSP error.
+      return;
+    }
+    ok(false, msg.message || msg.errorMessage);
+  });
   await setupFormAutofillStorage();
   registerCleanupFunction(function cleanup() {
     paymentSrv.cleanup();
     cleanupFormAutofillStorage();
     Services.prefs.clearUserPref(RESPONSE_TIMEOUT_PREF);
+    SpecialPowers.postConsoleSentinel();
   });
 });
 
