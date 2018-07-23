1784c2f3a02b7a181c0fd65f159954e19502fe90
Tim Nguyen <ntim.bugs@gmail.com>
2018-07-23 18:46:40 +0100

    Bug 1347207 - Implement theme_experiment manifest field. r=jaws
    MozReview-Commit-ID: DuUiVAMcti2
    
    --HG--
    extra : rebase_source : e76ade5a02df1cb87165ce4c8148908b2a3b35d8

--- toolkit/components/extensions/parent/ext-theme.js
+++ toolkit/components/extensions/parent/ext-theme.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* global windowTracker, EventManager, EventEmitter */
+/* global windowTracker, EventManager, EventEmitter, AddonManager */
 
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 
@@ -36,7 +36,7 @@ class Theme {
    * @param {string} extension Extension that created the theme.
    * @param {Integer} windowId The windowId where the theme is applied.
    */
-  constructor({extension, details, windowId}) {
+  constructor({extension, details, windowId, experiment}) {
     this.extension = extension;
     this.details = details;
     this.windowId = windowId;
@@ -45,6 +45,24 @@ class Theme {
       icons: {},
     };
 
+    if (experiment) {
+      const canRunExperiment = !AppConstants.MOZ_ALLOW_LEGACY_EXTENSIONS &&
+        Services.prefs.getBoolPref("extensions.legacy.enabled");
+      if (canRunExperiment) {
+        this.lwtStyles.experimental = {
+          colors: {},
+          images: {},
+          properties: {},
+        };
+        const {baseURI} = this.extension;
+        if (experiment.stylesheet) {
+          experiment.stylesheet = baseURI.resolve(experiment.stylesheet);
+        }
+        this.experiment = experiment;
+      } else {
+        throw new Error("This extension is not allowed to run theme experiments");
+      }
+    }
     this.load();
   }
 
@@ -88,6 +106,9 @@ class Theme {
     }
     onUpdatedEmitter.emit("theme-updated", this.details, this.windowId);
 
+    if (this.experiment) {
+      lwtData.experiment = this.experiment;
+    }
     LightweightThemeManager.fallbackThemeData = this.lwtStyles;
     Services.obs.notifyObservers(null,
                                  "lightweight-theme-styling-update",
@@ -163,7 +184,13 @@ class Theme {
         case "ntp_text":
           this.lwtStyles[color] = cssColor;
           break;
+        default:
+          if (this.experiment && this.experiment.colors && color in this.experiment.colors) {
+            this.lwtStyles.experimental.colors[color] = cssColor;
+          }
+          break;
       }
+
     }
   }
 
@@ -192,6 +219,12 @@ class Theme {
         case "theme_frame": {
           let resolvedURL = baseURI.resolve(val);
           this.lwtStyles.headerURL = resolvedURL;
+          break;
+        }
+        default: {
+          if (this.experiment && this.experiment.images && image in this.experiment.images) {
+            this.lwtStyles.experimental.images[image] = baseURI.resolve(val);
+          }
           break;
         }
       }
@@ -285,6 +318,12 @@ class Theme {
           this.lwtStyles.backgroundsTiling = tiling.join(",");
           break;
         }
+        default: {
+          if (this.experiment && this.experiment.properties && property in this.experiment.properties) {
+            this.lwtStyles.experimental.properties[property] = val;
+          }
+          break;
+        }
       }
     }
   }
@@ -314,10 +353,12 @@ this.theme = class extends ExtensionAPI {
   onManifestEntry(entryName) {
     let {extension} = this;
     let {manifest} = extension;
+    let {theme, theme_experiment} = manifest;
 
     defaultTheme = new Theme({
       extension,
-      details: manifest.theme,
+      details: theme,
+      experiment: theme_experiment,
     });
   }
 
@@ -366,6 +407,7 @@ this.theme = class extends ExtensionAPI {
             extension,
             details,
             windowId,
+            experiment: this.extension.manifest.theme_experiment,
           });
         },
         reset: (windowId) => {
