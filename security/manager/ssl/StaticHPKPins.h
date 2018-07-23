7702c907e7cbb03eef15b57b88eb0ad9f2b030e9
Cosmin Sabou <csabou@mozilla.com>
2018-07-23 15:26:37 +0300

    Backed out changeset 469f678d1f15 for xpcshell failures on test_remote_settings_poll.js. CLOSED TREE

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -1165,4 +1165,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1540807403868000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1540462178806000);
