6e929c08d4d0b40808c330c18622a278c5b29fcb
Cosmin Sabou <csabou@mozilla.com>
2018-07-23 16:29:05 +0300

    Backed out changeset f840add2cdd4 (bug 1474163) for causing mochitest frequent leaks.

--- browser/base/content/contentTheme.js
+++ browser/base/content/contentTheme.js
@@ -46,14 +46,17 @@ const ContentThemeController = {
    */
   init() {
     addEventListener("LightweightTheme:Set", this);
+
+    const event = new CustomEvent("LightweightTheme:Support", {bubbles: true});
+    document.dispatchEvent(event);
   },
 
   /**
    * Handle theme updates from the frame script.
    * @param {Object} event object containing the theme update.
    */
-  handleEvent({ type, detail }) {
-    if (type == "LightweightTheme:Set") {
+  handleEvent({ detail }) {
+    if (detail.type == "LightweightTheme:Update") {
       let {data} = detail;
       if (!data) {
         data = {};
