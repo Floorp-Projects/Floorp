0d5d9519517af47fbd1b338c06bd925f68a1dded
Nathan Froyd <froydnj@mozilla.com>
2018-07-24 14:30:53 -0400

    Bug 1476973 - part 3 - rationalize the backend's IDLManager; r=gps
    The IDLManager in the moz.build backend is a bit weird.  It maintains a
    bunch of per-IDL file information, some of which isn't used, and
    attempts to map module names to information about what entities the
    module needs to be built.  The former is done with Python dicts, and the
    latter with Python tuples, both resulting in some contortions by the
    clients of IDLManager to specify exactly what they need.
    
    Let's clean this up, by making IDLManager to more clearly do two jobs:
    
    1. Keep track of whether IDL files are globally unique; and
    2. Map module names to the information needed to build them.
    
    In the case of #2, we store everything as a straight Python object, so
    we can use actual property accesses everywhere.  We also provide a
    stems() function on IDLManager to make some client code more
    straightforward.
    
    Doing this makes IDLManager much more XPIDL module-centric, and paves
    the way for the same change to be made in the frontend as well.

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -1063,9 +1063,8 @@ class RecursiveMakeBackend(CommonBackend):
         for p in ('Makefile', 'backend.mk', '.deps/.mkdir.done'):
             build_files.add_optional_exists(p)
 
-        for idl in manager.idls.values():
-            self._install_manifests['dist_include'].add_optional_exists('%s.h'
-                % idl['root'])
+        for stem in manager.idl_stems():
+            self._install_manifests['dist_include'].add_optional_exists('%s.h' % stem)
 
         for module in manager.modules:
             build_files.add_optional_exists(mozpath.join('.deps',
@@ -1077,10 +1076,10 @@ class RecursiveMakeBackend(CommonBackend):
         mk = Makefile()
         all_directories = set()
 
-        for module in xpt_modules:
-            sources, directories = modules[module]
-            all_directories |= directories
-            deps = sorted(sources)
+        for module_name in xpt_modules:
+            module = manager.modules[module_name]
+            all_directories |= module.directories
+            deps = sorted(module.idl_files)
 
             # It may seem strange to have the .idl files listed as
             # prerequisites both here and in the auto-generated .pp files.
@@ -1093,10 +1092,9 @@ class RecursiveMakeBackend(CommonBackend):
             # reference to the new .idl. Since the new .idl presumably has
             # an mtime newer than the .xpt, it will trigger xpt generation.
 
-            mk.add_statement('%s_deps = %s' % (module, ' '.join(d.full_path
-                                                                for d in deps)))
+            mk.add_statement('%s_deps = %s' % (module_name, ' '.join(deps)))
 
-            build_files.add_optional_exists('%s.xpt' % module)
+            build_files.add_optional_exists('%s.xpt' % module_name)
 
         mk.add_statement('all_idl_dirs = %s' % ' '.join(sorted(all_directories)))
 
