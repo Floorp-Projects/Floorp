e935de80966070c1b0de7921a1d0a79ade233e85
Nathan Froyd <froydnj@mozilla.com>
2018-07-24 14:30:53 -0400

    Bug 1476973 - part 4 - emit XPIDLModule, rather than multiple XPIDLFile; r=gps
    XPIDL files are logically grouped together into a module, but the
    current model for the moz.build frontend is that we emit individual
    XPIDLFile objects, and leave it to the backend to reconstruct
    module-ness from those.  This setup causes a small amout of useless work
    to be done (e.g. see XPIDLFile handling in
    RecursiveMakeBackend.consume_object; such handling should only be done
    once), and it would be cleaner to have the objects emitted reflect the
    build system concepts as closely as possible.  To that end, let's emit
    XPIDLModule objects instead, which fortunately requires relatively few
    changes.

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -192,22 +192,20 @@ class ComputedFlags(ContextDerived):
                     flags[dest_var].extend(value)
         return flags.items()
 
-class XPIDLFile(ContextDerived):
-    """Describes an XPIDL file to be compiled."""
+class XPIDLModule(ContextDerived):
+    """Describes an XPIDL module to be compiled."""
 
     __slots__ = (
-        'source_path',
-        'basename',
-        'module',
+        'name',
+        'idl_files',
     )
 
-    def __init__(self, context, source, module):
+    def __init__(self, context, name, idl_files):
         ContextDerived.__init__(self, context)
 
-        assert isinstance(source, SourcePath)
-        self.source_path = source
-        self.basename = mozpath.basename(source)
-        self.module = module
+        assert all(isinstance(idl, SourcePath) for idl in idl_files)
+        self.name = name
+        self.idl_files = idl_files
 
 class BaseDefines(ContextDerived):
     """Context derived container object for DEFINES/HOST_DEFINES,
