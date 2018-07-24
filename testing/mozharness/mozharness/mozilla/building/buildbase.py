62e4daad39cc18087ba2333d45b73499605a5b28
Tudor-Gabriel Vîjială <tvijiala@mozilla.com>
2018-07-24 11:53:37 +0100

    Bug 1473313 - Part 4: Add build and test platforms for android emulator code coverage. r=gbrown
    MozReview-Commit-ID: IkSKdB7ZeYi
    
    --HG--
    extra : rebase_source : 1cff84a03c8f590ac3df5e0dc1ccf217fd2589c3

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -428,6 +428,7 @@ class BuildOptionParser(object):
         'api-16': 'builds/releng_sub_%s_configs/%s_api_16.py',
         'api-16-artifact': 'builds/releng_sub_%s_configs/%s_api_16_artifact.py',
         'api-16-debug': 'builds/releng_sub_%s_configs/%s_api_16_debug.py',
+        'api-16-debug-ccov': 'builds/releng_sub_%s_configs/%s_api_16_debug_ccov.py',
         'api-16-debug-artifact': 'builds/releng_sub_%s_configs/%s_api_16_debug_artifact.py',
         'api-16-gradle': 'builds/releng_sub_%s_configs/%s_api_16_gradle.py',
         'api-16-gradle-artifact': 'builds/releng_sub_%s_configs/%s_api_16_gradle_artifact.py',
