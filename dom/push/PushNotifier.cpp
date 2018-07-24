197f6a40782314afc8ed1df19ec6b7dbcb53c8d9
Kris Maglione <maglione.k@gmail.com>
2018-07-23 17:41:06 -0700

    Bug 1477579: Part 3 - Avoid duplicating static strings in category manager entries. r=froydnj
    Much like the component manager, many of the strings that we use for category
    manager entries are statically allocated. There's no need to duplicate these
    strings.
    
    This patch changes the category manager APIs to take nsACStrings rather than
    raw pointers, and to pass literal nsCStrings when we know we have a literal
    string to begin with. When adding the category entry, it then skips making
    copies of any strings with the LITERAL flag.
    
    MozReview-Commit-ID: EJEcYSdNMWs
    ***
    amend-catman

--- dom/push/PushNotifier.cpp
+++ dom/push/PushNotifier.cpp
@@ -320,9 +320,7 @@ PushDispatcher::DoNotifyObservers(nsISupports *aSubject, const char *aTopic,
     do_GetService(NS_CATEGORYMANAGER_CONTRACTID);
   if (catMan) {
     nsCString contractId;
-    nsresult rv = catMan->GetCategoryEntry("push",
-                                           mScope.BeginReading(),
-                                           getter_Copies(contractId));
+    nsresult rv = catMan->GetCategoryEntry("push", mScope, contractId);
     if (NS_SUCCEEDED(rv)) {
       // Ensure the service is created - we don't need to do anything with
       // it though - we assume the service constructor attaches a listener.
