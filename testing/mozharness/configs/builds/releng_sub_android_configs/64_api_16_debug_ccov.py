364b7d98964505cd9de3b3bb90f8e7b4cfb79394
Tudor-Gabriel Vîjială <tvijiala@mozilla.com>
2018-07-24 11:53:37 +0100

    Bug 1473313 - Part 4: Add build and test platforms for android emulator code coverage. r=gbrown
    MozReview-Commit-ID: IkSKdB7ZeYi

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_debug_ccov.py
@@ -0,0 +1,15 @@
+config = {
+    'base_name': 'Android armv7 api-16+ %(branch)s debug coverage',
+    'stage_platform': 'android-api-16-debug-ccov',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16/debug-ccov',
+    'multi_locale_config_platform': 'android',
+    'debug_build': True,
+    'postflight_build_mach_commands': [
+        ['android',
+         'archive-geckoview',
+        ],
+        ['android',
+         'archive-geckoview-coverage-artifacts',
+        ],
+    ],
+}
