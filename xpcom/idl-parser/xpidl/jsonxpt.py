0866987b3061d40512190dd8a9c5d8fd17e75e24
Nika Layzell <nika@thelayzells.com>
2018-07-24 16:31:09 -0400

    Bug 1474369 - Part 8: Rename from Sequence to Array in xpidl, r=mccr8
    Summary:
    This more closely matches the C++ names, and reflects the fact that the
    reflected type is not WebIDL's mozilla::dom::Sequence. The reasoning behind this
    type difference is for ergonomics, due to xpidl only being exposed to internal
    JS code.
    
    Depends On D2335
    
    Reviewers: mccr8!
    
    Tags: #secure-revision
    
    Bug #: 1474369
    
    Differential Revision: https://phabricator.services.mozilla.com/D2337

--- xpcom/idl-parser/xpidl/jsonxpt.py
+++ xpcom/idl-parser/xpidl/jsonxpt.py
@@ -62,11 +62,11 @@ def get_type(type, calltype, iid_is=None, size_is=None):
             ret['size_is'] = size_is
         return ret
 
-    if isinstance(type, xpidl.Sequence):
-        # NB: For a Sequence<T> we pass down the iid_is to get the type of T.
+    if isinstance(type, xpidl.Array):
+        # NB: For a Array<T> we pass down the iid_is to get the type of T.
         #     This allows Arrays of InterfaceIs types to work.
         return {
-            'tag': 'TD_SEQUENCE',
+            'tag': 'TD_ARRAY',
             'element': get_type(type.type, calltype, iid_is),
         }
 
