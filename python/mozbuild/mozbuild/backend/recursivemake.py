6b6a7faea4fdbecca5c71ddae89424de81b741c4
Nathan Froyd <froydnj@mozilla.com>
2018-07-24 14:30:53 -0400

    Bug 1476973 - part 5 - make xpidl variables simply expanded; r=gps
    These kind of variables are slightly more efficient in make.

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -1092,11 +1092,11 @@ class RecursiveMakeBackend(CommonBackend):
             # reference to the new .idl. Since the new .idl presumably has
             # an mtime newer than the .xpt, it will trigger xpt generation.
 
-            mk.add_statement('%s_deps = %s' % (module_name, ' '.join(deps)))
+            mk.add_statement('%s_deps := %s' % (module_name, ' '.join(deps)))
 
             build_files.add_optional_exists('%s.xpt' % module_name)
 
-        mk.add_statement('all_idl_dirs = %s' % ' '.join(sorted(all_directories)))
+        mk.add_statement('all_idl_dirs := %s' % ' '.join(sorted(all_directories)))
 
         rules = StringIO()
         mk.dump(rules, removal_guard=False)
