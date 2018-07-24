f03960010a37e83c1f573bddba48e6a1945f0859
Csoregi Natalia <ncsoregi@mozilla.com>
2018-07-24 11:34:09 +0300

    Backed out changeset 438598073b9b (bug 1474540) for assertion failure at ScriptSettings.cpp:493 on Android. CLOSED TREE

--- dom/script/ScriptSettings.h
+++ dom/script/ScriptSettings.h
@@ -228,8 +228,7 @@ public:
   MOZ_MUST_USE bool Init(nsIGlobalObject* aGlobalObject);
 
   // This is a helper that grabs the native global associated with aObject and
-  // invokes the above Init() with that. aObject must not be a cross-compartment
-  // wrapper: CCWs are not associated with a single global.
+  // invokes the above Init() with that.
   MOZ_MUST_USE bool Init(JSObject* aObject);
 
   // Unsurprisingly, this uses aCx and enters the compartment of aGlobalObject.
