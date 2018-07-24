edf3d172a7baab962b2dfab469b75661c23c7930
Tudor-Gabriel Vîjială <tvijiala@mozilla.com>
2018-07-24 11:51:54 +0100

    Bug 1473313 - Part 3: Extend CodeCoverageMixin to handle java code coverage tools. r=gbrown,marco
    MozReview-Commit-ID: Lld5XLUMb3Y

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -731,6 +731,12 @@ def enable_code_coverage(config, tests):
             if 'opt' in test['build-platform'] or 'fuzzing' in test['build-platform']:
                 test['run-on-projects'] = []
                 continue
+            # Skip this transform for android code coverage builds.
+            if 'android' in test['build-platform']:
+                test.setdefault('fetches', {}).setdefault('fetch', []).append('grcov-linux-x86_64')
+                test['mozharness'].setdefault('extra-options', []).append('--java-code-coverage')
+                yield test
+                continue
             test['mozharness'].setdefault('extra-options', []).append('--code-coverage')
             test['instance-size'] = 'xlarge'
             # Ensure we always run on the projects defined by the build, unless the test
