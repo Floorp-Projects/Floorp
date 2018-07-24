61bbd225e79bf01c8671d471fdfb355b1f6059b0
Gurzau Raul <rgurzau@mozilla.com>
2018-07-24 11:16:21 +0300

    Backed out 4 changesets (bug 1463538) for failing on /payments/test/browser/browser_payments_onboarding_wizard.js
    Backed out changeset 1c945ede0071 (bug 1463538)
    Backed out changeset 8ccd9f4b230d (bug 1463538)
    Backed out changeset e078fb39875e (bug 1463538)
    Backed out changeset 798bf0d7928c (bug 1463538)

--- browser/components/payments/res/mixins/PaymentStateSubscriberMixin.js
+++ browser/components/payments/res/mixins/PaymentStateSubscriberMixin.js
@@ -32,7 +32,7 @@ export let requestStore = new PaymentsStore({
     // selectedStateKey: "",
   },
   request: {
-    completeStatus: "",
+    completeStatus: "initial",
     tabId: null,
     topLevelPrincipal: {URI: {displayHost: null}},
     requestId: null,
