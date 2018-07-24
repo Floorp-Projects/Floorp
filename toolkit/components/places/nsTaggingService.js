02bc93e36537028e34572453cd46437a620563d1
Marco Bonardo <mbonardo@mozilla.com>
2018-07-24 18:45:13 +0000

    Bug 1452068 - Move MAX_TAG_LENGTH to the bookmarking API. r=Standard8
    The tagging API is moving to the bookmarking API, this is one step
    
    Differential Revision: https://phabricator.services.mozilla.com/D2315

--- toolkit/components/places/nsTaggingService.js
+++ toolkit/components/places/nsTaggingService.js
@@ -114,7 +114,7 @@ TaggingService.prototype = {
         // want to change it.
         tag.__defineGetter__("name", () => this._tagFolders[tag.id]);
       } else if (typeof(idOrName) == "string" && idOrName.length > 0 &&
-               idOrName.length <= Ci.nsITaggingService.MAX_TAG_LENGTH) {
+               idOrName.length <= PlacesUtils.bookmarks.MAX_TAG_LENGTH) {
         // This is a tag name.
         tag.name = trim ? idOrName.trim() : idOrName;
         // We can't know the id at this point, since a previous tag could
