921385b274c7293f6ebda0d7a3490d3fb27b8030
Aki Sasaki <asasaki@mozilla.com>
2018-07-24 10:21:51 -0700

    bug 1477747 - combine action task-graph-{suffix}.json into one. r=dustin
    MozReview-Commit-ID: J3uaiMcTOBb
    
    --HG--
    extra : rebase_source : 5f13ffadc77b7bfb391922dc2aeaa03f79e3dddf
    extra : source : b1f82bccc0660681d74689f0a8a84beea3b4cf59

--- taskcluster/taskgraph/decision.py
+++ taskcluster/taskgraph/decision.py
@@ -315,3 +315,19 @@ def write_artifact(filename, data):
             f.write(json.dumps(data))
     else:
         raise TypeError("Don't know how to write to {}".format(filename))
+
+
+def read_artifact(filename):
+    path = os.path.join(ARTIFACTS_DIR, filename)
+    if filename.endswith('.yml'):
+        with open(path, 'r') as f:
+            return yaml.load(f)
+    elif filename.endswith('.json'):
+        with open(path, 'r') as f:
+            return json.load(f)
+    elif filename.endswith('.gz'):
+        import gzip
+        with gzip.open(path, 'rb') as f:
+            return json.load(f)
+    else:
+        raise TypeError("Don't know how to read {}".format(filename))
