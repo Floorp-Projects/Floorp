63d2bd30c14a887ccd09c49a1a549d927604eb86
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-07-25 15:35:01 +0000

    Bug 1358443 - remove reflows from adjustHeight itself, r=mak,florian
    This goes back to relying on rows being the same height.
    Places where we replace the popup will likely not use the richlistbox,
    and we no longer have code that changes the height or exceeds the maximum
    number of visible children with a scrollbar, so we should be OK.
    
    To determine the padding on the richlistbox and the height of the initial
    row, I've used a promiseDocumentFlushed callback. It's possible this causes
    flicker the first time the popup opens. I can't see any, but it's quite
    possible I'm missing something.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2242

--- browser/base/content/test/performance/browser_urlbar_keyed_search.js
+++ browser/base/content/test/performance/browser_urlbar_keyed_search.js
@@ -32,22 +32,6 @@ const EXPECTED_REFLOWS_FIRST_OPEN = [
 
   {
     stack: [
-      "adjustHeight@chrome://global/content/bindings/autocomplete.xml",
-      "onxblpopupshown@chrome://global/content/bindings/autocomplete.xml"
-    ],
-    maxCount: 5, // This number should only ever go down - never up.
-  },
-
-  {
-    stack: [
-      "adjustHeight@chrome://global/content/bindings/autocomplete.xml",
-      "_invalidate/this._adjustHeightTimeout<@chrome://global/content/bindings/autocomplete.xml",
-    ],
-    maxCount: 51, // This number should only ever go down - never up.
-  },
-
-  {
-    stack: [
       "_handleOverflow@chrome://global/content/bindings/autocomplete.xml",
       "handleOverUnderflow@chrome://global/content/bindings/autocomplete.xml",
       "_reuseAcItem@chrome://global/content/bindings/autocomplete.xml",
