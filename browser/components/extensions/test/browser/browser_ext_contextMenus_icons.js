b2e7a36b2d838433be2bfd51b6d62018ee49c11d
Rob Wu <rob@robwu.nl>
2018-07-25 18:06:03 +0200

    Bug 1373234 - Small cleanup in browser_ext_contextMenus*.js tests r=mixedpuppy
    Use sendMessage/awaitMessage instead of notifyPass/awaitFinish in tests
    where this is not the last completion callback.
    
    Remove an unneeded browser.contextMenus.onClicked.addListener call.
    
    MozReview-Commit-ID: Akpla9tiAlD

--- browser/components/extensions/test/browser/browser_ext_contextMenus_icons.js
+++ browser/components/extensions/test/browser/browser_ext_contextMenus_icons.js
@@ -35,7 +35,7 @@ add_task(async function test_root_icon() {
       browser.contextMenus.create({
         title: "child",
       }, () => {
-        browser.test.notifyPass("contextmenus-icons");
+        browser.test.sendMessage("contextmenus-icons");
       });
     },
   });
@@ -48,7 +48,7 @@ add_task(async function test_root_icon() {
   };
 
   await extension.startup();
-  await extension.awaitFinish("contextmenus-icons");
+  await extension.awaitMessage("contextmenus-icons");
 
   let extensionMenu = await openExtensionContextMenu();
 
@@ -124,7 +124,7 @@ add_task(async function test_child_icon() {
             18: "green_icon.png",
           },
         }, () => {
-          browser.test.notifyPass("extra-contextmenu-items-added");
+          browser.test.sendMessage("extra-contextmenu-items-added");
         });
       });
 
@@ -156,7 +156,7 @@ add_task(async function test_child_icon() {
   await closeContextMenu();
 
   extension.sendMessage("add-additional-contextmenu-items");
-  await extension.awaitFinish("extra-contextmenu-items-added");
+  await extension.awaitMessage("extra-contextmenu-items-added");
 
   contextMenu = await openExtensionContextMenu();
 
