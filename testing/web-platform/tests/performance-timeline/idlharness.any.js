7da77d5ae8288b10790110856a1439bd6c27f3cd
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-07-25 17:32:58 +0000

    Bug 1476189 [wpt PR 12014] - Expand coverage of performance-timeline IDL test, a=testonly
    Automatic update from web-platform-testsExpand coverage of performance-timeline IDL test (#12014)
    
    --
    
    wpt-commits: 0eead5e8bc6f214a59baa3096e061a96705f4d78
    wpt-pr: 12014

--- testing/web-platform/tests/performance-timeline/idlharness.any.js
+++ testing/web-platform/tests/performance-timeline/idlharness.any.js
@@ -6,22 +6,47 @@
 
 'use strict';
 
-idl_test(
-  ['performance-timeline'],
-  ['hr-time', 'dom'],
-  idl_array => {
+promise_test(async t => {
+  const observe = new Promise((resolve, reject) => {
     try {
-      const callback = (e, o) => {};
-      self.observerEntry = new ResizeObserverEntry(callback);
+      self.observer = new PerformanceObserver((entries, observer) => {
+        self.entryList = entries;
+        self.mark = entries.getEntries()[0];
+        resolve();
+      });
+      observer.observe({ entryTypes: ['mark'] });
+      performance.mark('test');
     } catch (e) {
-      // Will be surfaced when entry is undefined below.
+      reject(e);
     }
+  });
+  const timeout = new Promise((_, reject) => {
+    t.step_timeout(() => reject('Timed out waiting for observation'), 3000);
+  });
+  const user = await fetch('/interfaces/user-timing.idl').then(r => r.text());
+  const execute_test = () => {
+    idl_test(
+      ['performance-timeline'],
+      ['hr-time', 'dom'],
+      idl_array => {
+        idl_array.add_idls(user, {only: ['PerformanceMark']});
+        idl_array.add_objects({
+          Performance: ['performance'],
+          // NOTE: PerformanceMark cascadingly tests PerformanceEntry
+          PerformanceMark: ['mark'],
+          PerformanceObserver: ['observer'],
+          PerformanceObserverEntryList: ['entryList'],
+        });
+      },
+      'Test IDL implementation of performance-timeline API'
+    );
+  };
 
-    idl_array.add_objects({
-      Performance: ['performance'],
-      // PerformanceEntry implicitly tested in user-timing.
-      ResizeObserverEntry: ['observerEntry'],
-    });
-  },
-  'Test IDL implementation of performance-timeline API'
-);
+  return Promise.race([observe, timeout]).then(
+    execute_test,
+    reason => {
+      execute_test();
+      return Promise.reject(reason);
+    }
+  );
+})
