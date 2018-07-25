8051d8bf5716e6ded5a9a56bc7d13c8082c80614
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2018-07-25 12:37:51 -0700

    Bug 1476204 - Handle autofill record update state changes in the unpriv. PR forms. r=jaws
    This is easier to understand as we don't have to round-trip the whole success and error states to the privileged wrapper which could potentially lead to stale state changes.
    This is also much simpler for the basic-card-form as it doesn't need a lot of the complexity of the previous implementation.
    
    * Move selectedStateKey from page to address-page since it doesn't apply to basic-card-page
    
    MozReview-Commit-ID: B4kiZNWElGI

--- browser/components/payments/res/debugging.js
+++ browser/components/payments/res/debugging.js
@@ -333,6 +333,11 @@ let buttonActions = {
     requestStore.setState({});
   },
 
+  saveVisibleForm() {
+    // Bypasses field validation which is useful to test error handling.
+    paymentDialog.querySelector("#main-container > .page:not([hidden])").saveRecord();
+  },
+
   setAddresses1() {
     paymentDialog.setStateFromParent({savedAddresses: ADDRESSES_1});
   },
--- browser/components/payments/res/mixins/PaymentStateSubscriberMixin.js
+++ browser/components/payments/res/mixins/PaymentStateSubscriberMixin.js
@@ -20,6 +20,7 @@ export let requestStore = new PaymentsStore({
   },
   "address-page": {
     guid: null,
+    selectedStateKey: null,
     title: "",
   },
   "payment-summary": {
@@ -29,7 +30,6 @@ export let requestStore = new PaymentsStore({
     previousId: null,
     // onboardingWizard: true,
     // error: "",
-    // selectedStateKey: "",
   },
   request: {
     completeStatus: "",
