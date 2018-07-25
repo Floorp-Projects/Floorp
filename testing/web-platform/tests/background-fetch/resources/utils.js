e49487e06724273ea61caadd0d87ea1304f8d2d4
Peter Beverloo <peter@chromium.org>
2018-07-25 17:35:28 +0000

    Bug 1476481 [wpt PR 12043] - [Background Fetch] Support layout tests for the feature, a=testonly
    Automatic update from web-platform-tests[Background Fetch] Support layout tests for the feature
    
    An implementation of BackgroundFetchDelegate has been provided to
    content shell which enables Background Fetches, without UI, to finish.
    It uses the Download Service through a new dependency. This dependency
    will be removed once the Download Service has been servicified, as
    //content can depend on it directly from that point forward.
    
    In context of the WPT tests, additional machinery has been added to
    enable communication with the Service Worker, required for it to tell
    the test about the result of the various Background Fetch events. The
    "fetch.https.window.js" file now verifies the behaviour and results
    of a single fetched file.
    
    Bug: 864561
    
    Change-Id: If53e6755ae8ea924712f5bdace4df4f658bc53d7
    Reviewed-on: https://chromium-review.googlesource.com/1135536
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#575919}
    
    --
    
    wpt-commits: bf11d1a166660d75383135e9d1610aa987857309
    wpt-pr: 12043

--- testing/web-platform/tests/background-fetch/resources/utils.js
+++ testing/web-platform/tests/background-fetch/resources/utils.js
@@ -2,6 +2,18 @@
 
 let nextBackgroundFetchId = 0;
 
+// Waits for a single message received from a registered Service Worker.
+async function getMessageFromServiceWorker() {
+  return new Promise(resolve => {
+    function listener(event) {
+      navigator.serviceWorker.removeEventListener('message', listener);
+      resolve(event.data);
+    }
+
+    navigator.serviceWorker.addEventListener('message', listener);
+  });
+}
+
 // Registers the instrumentation Service Worker located at "resources/sw.js"
 // with a scope unique to the test page that's running, and waits for it to be
 // activated. The Service Worker will be unregistered automatically.
@@ -26,6 +38,10 @@ async function registerAndActivateServiceWorker(test) {
 function backgroundFetchTest(func, description) {
   promise_test(async t => {
     const serviceWorkerRegistration = await registerAndActivateServiceWorker(t);
+    serviceWorkerRegistration.active.postMessage(null /* unused */);
+
+    assert_equals(await getMessageFromServiceWorker(), 'ready');
+
     return func(t, serviceWorkerRegistration.backgroundFetch);
   }, description);
 }
