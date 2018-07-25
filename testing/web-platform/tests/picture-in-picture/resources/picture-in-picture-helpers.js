c6f4c578f99fc72d840a8a7b49275707f0970c83
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-07-25 18:33:25 +0000

    Bug 1477455 [wpt PR 12115] - Clean up picture-in-picture tests, a=testonly
    Automatic update from web-platform-testsClean up picture-in-picture tests (#12115)
    
    --
    
    wpt-commits: 887ea84fdfe8ccf82311416963296104151e3914
    wpt-pr: 12115

--- testing/web-platform/tests/picture-in-picture/resources/picture-in-picture-helpers.js
+++ testing/web-platform/tests/picture-in-picture/resources/picture-in-picture-helpers.js
@@ -4,38 +4,19 @@ if (!('pictureInPictureEnabled' in document)) {
   }
 }
 
-function callWithTrustedClick(callback) {
-  return new Promise((resolve, reject) => {
-    let button = document.createElement('button');
-    button.textContent = 'click to continue test';
-    button.style.display = 'block';
-    button.style.fontSize = '20px';
-    button.style.padding = '10px';
-    button.onclick = () => {
-      document.body.removeChild(button);
-      resolve(callback());
-    };
-    document.body.appendChild(button);
-    test_driver.click(button).catch(_ => reject('Click failed'));
-  });
-}
-
 function loadVideo(activeDocument, sourceUrl) {
   return new Promise((resolve, reject) => {
     const document = activeDocument || window.document;
     const video = document.createElement('video');
     video.src = sourceUrl || '/media/movie_5.ogv';
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-    video.onerror = error => {
-      reject(error);
-    };
+    video.onloadedmetadata = () => { resolve(video); };
+    video.onerror = error => { reject(error); };
   });
 }
 
 // Calls requestPictureInPicture() in a context that's 'allowed to request PiP'.
 function requestPictureInPictureWithTrustedClick(videoElement) {
-  return callWithTrustedClick(
-      () => videoElement.requestPictureInPicture());
+  return test_driver.bless(
+    'request picture in picture',
+    () => videoElement.requestPictureInPicture());
 }
