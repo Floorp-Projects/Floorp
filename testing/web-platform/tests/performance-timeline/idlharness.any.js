b60b699ec8212f7756a604b026f284093cec4fbb
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-07-25 17:26:22 +0000

    Bug 1474864 [wpt PR 11911] - Update the performance-timeline IDL file + test, a=testonly
    Automatic update from web-platform-testsUpdate the performance-timeline IDL file + test (#11911)
    
    --
    
    wpt-commits: 7cd84ed72cc6a04c752b25ea6e91bcc024c27214
    wpt-pr: 11911

--- testing/web-platform/tests/performance-timeline/idlharness.any.js
+++ testing/web-platform/tests/performance-timeline/idlharness.any.js
@@ -6,21 +6,22 @@
 
 'use strict';
 
-promise_test(async () => {
-  const idl_array = new IdlArray();
-  const idl = await fetch("/interfaces/performance-timeline.idl").then(r => r.text());
-  const dom = await fetch("/interfaces/dom.idl").then(r => r.text());
-  const hrtime = await fetch("/interfaces/hr-time.idl").then(r => r.text());
+idl_test(
+  ['performance-timeline'],
+  ['hr-time', 'dom'],
+  idl_array => {
+    try {
+      const callback = (e, o) => {};
+      self.observerEntry = new ResizeObserverEntry(callback);
+    } catch (e) {
+      // Will be surfaced when entry is undefined below.
+    }
 
-  // create first mark
-  self.performance.mark("mark");
-
-  idl_array.add_idls(idl);
-  idl_array.add_dependency_idls(hrtime);
-  idl_array.add_dependency_idls(dom);
-  idl_array.add_objects({
-    Performance: ["performance"],
-    PerformanceMark: [self.performance.getEntriesByName("mark")[0]],
-  });
-  idl_array.test();
-}, "Test IDL implementation of performance-timeline API");
+    idl_array.add_objects({
+      Performance: ['performance'],
+      // PerformanceEntry implicitly tested in user-timing.
+      ResizeObserverEntry: ['observerEntry'],
+    });
+  },
+  'Test IDL implementation of performance-timeline API'
+);
