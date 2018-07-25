e4da6338f1ae403d0da34818dd3543eddb7b9205
Sebastian Hengst <archaeopteryx@coole-files.de>
2018-07-25 17:19:14 +0300

    Backed out 2 changesets (bug 1451513) for likely causing frequent mochitest suite hangs (bug 1414495). a=backout
    Backed out changeset 604167986098 (bug 1451513)
    Backed out changeset afc5c4bd60da (bug 1451513)

--- toolkit/mozapps/extensions/test/xpcshell/test_registerchrome.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_registerchrome.js
@@ -16,7 +16,6 @@ add_task(async function() {
   let uri2 = getFileURI("manifest.json");
 
   let overrideURL = Services.io.newURI("chrome://global/content/foo");
-  let contentURL = Services.io.newURI("chrome://test/content/foo");
   let localeURL = Services.io.newURI("chrome://global/locale/foo");
 
   let origOverrideURL = registry.convertChromeURL(overrideURL);
@@ -25,29 +24,23 @@ add_task(async function() {
   // eslint-disable-next-line no-unused-vars
   let entry1 = aomStartup.registerChrome(uri1, [
     ["override", "chrome://global/content/foo", file1.spec],
-    ["content", "test", file2.spec + "/"],
     ["locale", "global", "en-US", file2.spec + "/"],
   ]);
 
   let entry2 = aomStartup.registerChrome(uri2, [
     ["override", "chrome://global/content/foo", file2.spec],
-    ["content", "test", file1.spec + "/"],
     ["locale", "global", "en-US", file1.spec + "/"],
   ]);
 
   // Initially, the second entry should override the first.
   equal(registry.convertChromeURL(overrideURL).spec, file2.spec);
-  let file = file1.spec + "/foo";
-  equal(registry.convertChromeURL(contentURL).spec, file);
-  equal(registry.convertChromeURL(localeURL).spec, file);
+  equal(registry.convertChromeURL(localeURL).spec, file1.spec + "/foo");
 
-  // After destroying the second entry, the first entry should now take
+  // After destroying the second entry, the first entry should not take
   // precedence.
   entry2.destruct();
   equal(registry.convertChromeURL(overrideURL).spec, file1.spec);
-  file = file2.spec + "/foo";
-  equal(registry.convertChromeURL(contentURL).spec, file);
-  equal(registry.convertChromeURL(localeURL).spec, file);
+  equal(registry.convertChromeURL(localeURL).spec, file2.spec + "/foo");
 
   // After dropping the reference to the first entry and allowing it to
   // be GCed, we should be back to the original entries.
@@ -56,9 +49,6 @@ add_task(async function() {
   Cu.forceCC();
   equal(registry.convertChromeURL(overrideURL).spec, origOverrideURL.spec);
   equal(registry.convertChromeURL(localeURL).spec, origLocaleURL.spec);
-  Assert.throws(() => registry.convertChromeURL(contentURL),
-                e => e.result == Cr.NS_ERROR_FILE_NOT_FOUND,
-                "chrome://test/ should no longer be registered");
 });
 
 add_task(async function() {
@@ -68,6 +58,7 @@ add_task(async function() {
     ["foo"],
     [{}],
     [[]],
+    [["content", "foo", "bar"]],
     [["locale", "global"]],
     [["locale", "global", "en", "foo", "foo"]],
     [["override", "en"]],
