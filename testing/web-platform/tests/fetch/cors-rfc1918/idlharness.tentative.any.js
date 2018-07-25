f289ae65db174c2dd3defed64391efeb722e495c
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-07-25 05:41:27 +0300

    Backed out 2 changesets (bug 1475445) for wpt failures on idlharness.tentative.any.sharedworker.html. CLOSED TREE
    Backed out changeset 348610d9a937 (bug 1475445)
    Backed out changeset 8fdb94dcb58d (bug 1475445)

--- testing/web-platform/tests/fetch/cors-rfc1918/idlharness.tentative.any.js
+++ testing/web-platform/tests/fetch/cors-rfc1918/idlharness.tentative.any.js
@@ -1,24 +1,20 @@
-// META: global=window,worker
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 
-'use strict';
+promise_test(async () => {
+  const idl = await fetch('/interfaces/cors-rfc1918.idl').then(r => r.text());
+  const html = await fetch('/interfaces/html.idl').then(r => r.text());
+  const dom = await fetch('/interfaces/dom.idl').then(r => r.text());
 
-// https://wicg.github.io/cors-rfc1918/
+  const idlArray = new IdlArray();
+  idlArray.add_idls(idl);
+  idlArray.add_dependency_idls(html);
+  idlArray.add_dependency_idls(dom);
 
-idl_test(
-  ['cors-rfc1918'],
-  ['html', 'dom'],
-  idlArray => {
-    if (self.GLOBAL.isWorker()) {
-      idlArray.add_objects({
-        WorkerGlobalScope: ['self'],
-      });
-    } else {
-      idlArray.add_objects({
-        Document: ['document'],
-      });
-    }
-  },
-  'Test CORS RFC1918 interfaces'
-);
+  const objects = {
+    Document: ['document'],
+    WorkerGlobalScope: ['self'],
+  };
+  idlArray.add_objects(objects);
+  idlArray.test();
+}, 'Test CORS RFC1918 interfaces');
