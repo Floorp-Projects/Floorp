b2b698695f813a2b18327ee4a476af48d2dd5203
Andreas Farre <farre@mozilla.com>
2018-07-26 10:31:00 +0300

    Bug 1445659 - Create basic Browsing Context in Content Parent and Child. r=peterv Add BrowsingContext to allow the tree structure of docshells to exist in several processes simultaneously. This is a first step towards allowing a tree structure preserving separation of docshells across processes.

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -15,6 +15,7 @@
 #include "mozilla/UniquePtr.h"
 #include "mozilla/WeakPtr.h"
 
+#include "mozilla/dom/BrowsingContext.h"
 #include "mozilla/dom/ProfileTimelineMarkerBinding.h"
 #include "mozilla/gfx/Matrix.h"
 #include "mozilla/dom/ChildSHistory.h"
@@ -378,6 +379,9 @@ public:
   // Returns true if the current load is a force reload (started by holding
   // shift while triggering reload)
   bool IsForceReloading();
+
+  already_AddRefed<mozilla::dom::BrowsingContext>
+  GetBrowsingContext() const;
 
 private: // member functions
   friend class nsDSURIContentListener;
@@ -907,7 +911,6 @@ private: // data members
 #endif /* DEBUG */
 
   nsID mHistoryID;
-  nsString mName;
   nsString mTitle;
   nsString mCustomUserAgent;
   nsCString mOriginalUriString;
@@ -932,6 +935,7 @@ private: // data members
   RefPtr<mozilla::dom::ChildSHistory> mSessionHistory;
   nsCOMPtr<nsIWebBrowserFind> mFind;
   nsCOMPtr<nsICommandManager> mCommandManager;
+  RefPtr<mozilla::dom::BrowsingContext> mBrowsingContext;
 
   // Dimensions of the docshell
   nsIntRect mBounds;
