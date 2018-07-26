052b7ba0e5af01e9c7b919bf202c711159c088dd
Henrik Skupin <mail@hskupin.info>
2018-07-26 22:53:34 +0200

    Bug 1439995 - [wdspec] Refactor user_prompt tests for Execute Script / Execute Async Script. r=ato
    To align with all the other commands the user_prompts.py test file
    has to contain tests for handling an open user prompt when calling
    the command.
    
    In case of both Execute Script and Execute Async Script the file
    contained tests where a user prompt got opened during the script
    execution. Tests for the latter situation are moved to a different
    test file.
    
    MozReview-Commit-ID: JeAROkAH9vk
    
    --HG--
    rename : testing/web-platform/meta/webdriver/tests/execute_async_script/user_prompts.py.ini => testing/web-platform/meta/webdriver/tests/execute_async_script/execute_async.py.ini
    rename : testing/web-platform/meta/webdriver/tests/execute_script/user_prompts.py.ini => testing/web-platform/meta/webdriver/tests/execute_script/execute.py.ini
    rename : testing/web-platform/tests/webdriver/tests/execute_async_script/user_prompts.py => testing/web-platform/tests/webdriver/tests/execute_async_script/execute_async.py
    rename : testing/web-platform/tests/webdriver/tests/execute_script/user_prompts.py => testing/web-platform/tests/webdriver/tests/execute_script/execute.py
    extra : rebase_source : 96353b743afee774da93e6318400175d9cd6bff4

--- testing/web-platform/tests/webdriver/tests/execute_async_script/user_prompts.py
+++ testing/web-platform/tests/webdriver/tests/execute_async_script/user_prompts.py
@@ -2,9 +2,7 @@
 
 import pytest
 
-from webdriver import error
-
-from tests.support.asserts import assert_success
+from tests.support.asserts import assert_dialog_handled, assert_error, assert_success
 
 
 def execute_async_script(session, script, args=None):
@@ -19,82 +17,57 @@ def execute_async_script(session, script, args=None):
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "accept"})
 @pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_accept(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
+def test_handle_prompt_accept(session, create_dialog, dialog_type):
+    create_dialog(dialog_type, text="dialog")
 
-    session.title
-    with pytest.raises(error.NoSuchAlertException):
-        session.alert.accept()
+    response = execute_async_script(session, "arguments[0]()")
+    assert_success(response)
+
+    assert_dialog_handled(session, expected_text="dialog")
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "accept and notify"})
 @pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_accept_and_notify(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
+def test_handle_prompt_accept_and_notify(session, create_dialog, dialog_type):
+    create_dialog(dialog_type, text="dialog")
 
-    with pytest.raises(error.UnexpectedAlertOpenException):
-        session.title
-    with pytest.raises(error.NoSuchAlertException):
-        session.alert.accept()
+    response = execute_async_script(session, "arguments[0]()")
+    assert_error(response, "unexpected alert open")
+
+    assert_dialog_handled(session, expected_text="dialog")
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "dismiss"})
 @pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_dismiss(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
+def test_handle_prompt_dismiss(session, create_dialog, dialog_type):
+    create_dialog(dialog_type, text="dialog")
 
-    session.title
-    with pytest.raises(error.NoSuchAlertException):
-        session.alert.dismiss()
+    response = execute_async_script(session, "arguments[0]()")
+    assert_success(response)
+
+    assert_dialog_handled(session, expected_text="dialog")
 
 
 @pytest.mark.capabilities({"unhandledPromptBehavior": "dismiss and notify"})
 @pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_dismiss_and_notify(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
+def test_handle_prompt_dissmiss_and_notify(session, create_dialog, dialog_type):
+    create_dialog(dialog_type, text="dialog")
 
-    with pytest.raises(error.UnexpectedAlertOpenException):
-        session.title
-    with pytest.raises(error.NoSuchAlertException):
-        session.alert.dismiss()
+    response = execute_async_script(session, "arguments[0]()")
+    assert_error(response, "unexpected alert open")
+
+    assert_dialog_handled(session, expected_text="dialog")
 
 
-@pytest.mark.capabilities({"unhandledPromptBehavior": "ignore"})
-@pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_ignore(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
-
-    with pytest.raises(error.UnexpectedAlertOpenException):
-        session.title
-    session.alert.dismiss()
+def test_handle_prompt_ignore():
+    """TODO"""
 
 
 @pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_default(session, dialog_type):
-    response = execute_async_script(session, "window.{}('Hello');".format(dialog_type))
-    assert_success(response, None)
+def test_handle_prompt_default(session, create_dialog, dialog_type):
+    create_dialog(dialog_type, text="dialog")
 
-    with pytest.raises(error.UnexpectedAlertOpenException):
-        session.title
-    with pytest.raises(error.NoSuchAlertException):
-        session.alert.dismiss()
+    response = execute_async_script(session, "arguments[0]()")
+    assert_error(response, "unexpected alert open")
 
-
-@pytest.mark.capabilities({"unhandledPromptBehavior": "accept"})
-@pytest.mark.parametrize("dialog_type", ["alert", "confirm", "prompt"])
-def test_handle_prompt_twice(session, dialog_type):
-    response = execute_async_script(
-        session, "window.{0}('Hello');window.{0}('Bye');".format(dialog_type))
-    assert_success(response, None)
-
-    session.alert.dismiss()
-    # The first alert has been accepted by the user prompt handler, the second one remains.
-    # FIXME: this is how browsers currently work, but the spec should clarify if this is the
-    #        expected behavior, see https://github.com/w3c/webdriver/issues/1153.
-    assert session.alert.text == "Bye"
-    session.alert.dismiss()
+    assert_dialog_handled(session, expected_text="dialog")
