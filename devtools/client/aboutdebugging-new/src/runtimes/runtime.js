e31142cbcd2e9799bfa0eac11809d6b9bd5c4c68
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-07-26 17:53:11 +0900

    Bug 1471795 - Part 3: Implement runtimes pane. r=jdescottes,ladybenko
    MozReview-Commit-ID: 1X14YIYL27x
    
    --HG--
    extra : rebase_source : 0029ff4b0a7ce83142f04849c8e9a7f2f2eccd94

--- devtools/client/aboutdebugging-new/src/runtimes/runtime.js
+++ devtools/client/aboutdebugging-new/src/runtimes/runtime.js
@@ -8,6 +8,23 @@
  * This class represents a runtime, such as a remote Firefox.
  */
 class Runtime {
+  /**
+   * Return icon of this runtime on sidebar.
+   * Subclass should override this method.
+   * @return {String}
+   */
+  getIcon() {
+    throw new Error("Subclass of Runtime should override getIcon()");
+  }
+
+  /**
+   * Return name of this runtime on sidebar.
+   * Subclass should override this method.
+   * @return {String}
+   */
+  getName() {
+    throw new Error("Subclass of Runtime should override getName()");
+  }
 }
 
 module.exports = Runtime;
