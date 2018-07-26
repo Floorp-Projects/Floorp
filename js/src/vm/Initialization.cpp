1af3359ceab0c892d062e444b81d24d2bef8b15e
Jon Coppeard <jcoppeard@mozilla.com>
2018-07-26 17:23:18 +0100

    Bug 1477761 - Fix sketchy timestamp computation for unlimited slice budget r=sfink

--- js/src/vm/Initialization.cpp
+++ js/src/vm/Initialization.cpp
@@ -88,6 +88,8 @@ JS::detail::InitWithFailureDiagnostic(bool isDebugBuild)
 
     PRMJ_NowInit();
 
+    js::SliceBudget::Init();
+
     // The first invocation of `ProcessCreation` creates a temporary thread
     // and crashes if that fails, i.e. because we're out of memory. To prevent
     // that from happening at some later time, get it out of the way during
