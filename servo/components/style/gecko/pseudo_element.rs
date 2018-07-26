3b996e0968a3ec3200866d6c7df1fa30834e942e
Cosmin Sabou <csabou@mozilla.com>
2018-07-26 07:38:47 +0300

    Backed out 2 changesets (bug 1475197) for causing linux build bustages because of compile errors.
    Backed out changeset 108e22d963cb (bug 1475197)
    Backed out changeset 4bd5f8588422 (bug 1475197)

--- servo/components/style/gecko/pseudo_element.rs
+++ servo/components/style/gecko/pseudo_element.rs
@@ -15,7 +15,6 @@ use properties::longhands::display::computed_value::T as Display;
 use selector_parser::{NonTSPseudoClass, PseudoElementCascadeType, SelectorImpl};
 use std::fmt;
 use string_cache::Atom;
-use thin_slice::ThinBoxedSlice;
 use values::serialize_atom_identifier;
 
 include!(concat!(
