25b5f10ae38fd0e7ec6b047be8a7cb7cac93ada5
Kris Maglione <maglione.k@gmail.com>
2018-07-26 16:36:16 -0700

    Bug 1476405: Follow-up: Handle nsThread cleanup for threads that never shutdown. r=me
    --HG--
    extra : source : cb7f7cc326875b2fd28d4a63101b07360a6606fd
    extra : histedit_source : db0deda75879e4626a1c095d8e2845bbcaa753b4%2Cb7f6d26232e23f97ab171519a943768a50575977

--- xpcom/threads/nsThread.cpp
+++ xpcom/threads/nsThread.cpp
@@ -405,6 +405,13 @@ nsThread::ThreadList()
   return sList;
 }
 
+/* static */ void
+nsThread::ClearThreadList()
+{
+  OffTheBooksMutexAutoLock mal(ThreadListMutex());
+  while (ThreadList().popFirst()) {}
+}
+
 /* static */ nsThreadEnumerator
 nsThread::Enumerate()
 {
@@ -871,6 +878,13 @@ nsThread::ShutdownComplete(NotNull<nsThreadShutdownContext*> aContext)
 {
   MOZ_ASSERT(mThread);
   MOZ_ASSERT(aContext->mTerminatingThread == this);
+
+  {
+    OffTheBooksMutexAutoLock mal(ThreadListMutex());
+    if (isInList()) {
+      removeFrom(ThreadList());
+    }
+  }
 
   if (aContext->mAwaitingShutdownAck) {
     // We're in a synchronous shutdown, so tell whatever is up the stack that
