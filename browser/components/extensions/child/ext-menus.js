fcfb48870204894d3872c022be933b81723a48c5
Rob Wu <rob@robwu.nl>
2018-07-27 19:06:00 +0200

    Bug 1454627 - Run "onclick" event of menu with user input r=mixedpuppy
    MozReview-Commit-ID: r1URJfOKUB

--- browser/components/extensions/child/ext-menus.js
+++ browser/components/extensions/child/ext-menus.js
@@ -32,7 +32,7 @@ class ContextMenusClickPropHandler {
       // No need for runSafe or anything because we are already being run inside
       // an event handler -- the event is just being forwarded to the actual
       // handler.
-      onclick(info, tab);
+      withHandlingUserInput(this.context.contentWindow, () => onclick(info, tab));
     }
   }
 
