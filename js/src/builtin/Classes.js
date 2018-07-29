8eada9c364d820a3fab8004cdd45a3c4fa84d8aa
Jim Blandy <jimb@mozilla.com>
2018-07-28 20:26:46 -0700

    Bug 1473272 - Don't treat classes with default constructors as self-hosted code. r=tcampbell

--- js/src/builtin/Classes.js
+++ js/src/builtin/Classes.js
@@ -2,16 +2,14 @@
 // it to our newly made class, we will be sure to set it up with the correct name
 // and .prototype, so that everything works properly.
 
+// The template classes should have all their code on a single line, so that
+// bytecodes' line offsets within the script don't contribute spurious offsets
+// when we transplant them into the real class definition. Punting on column numbers.
+
 var DefaultDerivedClassConstructor =
-    class extends null {
-        constructor(...args) {
-            super(...allowContentIter(args));
-        }
-    };
+    class extends null { constructor(...args) { super(...allowContentIter(args)); } };
 MakeDefaultConstructor(DefaultDerivedClassConstructor);
 
 var DefaultBaseClassConstructor =
-    class {
-        constructor() { }
-    };
+    class { constructor() { } };
 MakeDefaultConstructor(DefaultBaseClassConstructor);
