a2719e529a4f81b8e3c4b8eb6836a51a4f9ee7d3
Kris Maglione <maglione.k@gmail.com>
2018-07-29 12:36:45 -0700

    Bug 1479310: Don't load content-UITour.js until needed. r=Mossop
    MozReview-Commit-ID: 55EPBxUYv9o

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -37,6 +37,12 @@ XPCOMUtils.defineLazyProxy(this, "PopupBlocking", () => {
   return new tmp.PopupBlocking(global);
 });
 
+XPCOMUtils.defineLazyProxy(this, "ShieldFrameListener", () => {
+  let tmp = {};
+  ChromeUtils.import("resource://normandy-content/ShieldFrameListener.jsm", tmp);
+  return new tmp.ShieldFrameListener(global);
+});
+
 XPCOMUtils.defineLazyProxy(this, "SelectionSourceContent",
   "resource://gre/modules/SelectionSourceContent.jsm");
 
@@ -433,3 +439,5 @@ let ExtFind = {
 };
 
 ExtFind.init();
+
+addEventListener("ShieldPageEvent", ShieldFrameListener, false, true);
