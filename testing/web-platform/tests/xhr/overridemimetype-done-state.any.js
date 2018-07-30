0359ca751ce0c703c0d30585f54a48f1412832db
Boris Zbarsky <bzbarsky@mit.edu>
2018-07-30 11:51:52 -0400

    Bug 1478743 part 3.  Remove bogus clearing of the response when xhr.overrideMimeType throws.  r=baku

--- testing/web-platform/tests/xhr/overridemimetype-done-state.any.js
+++ testing/web-platform/tests/xhr/overridemimetype-done-state.any.js
@@ -7,10 +7,13 @@ var test = async_test();
 var client = new XMLHttpRequest();
 client.onreadystatechange = test.step_func( function() {
   if (client.readyState !== 4) return;
+  var text = client.responseText;
+  assert_not_equals(text, "");
   assert_throws("InvalidStateError", function() { client.overrideMimeType('application/xml;charset=Shift-JIS'); });
   if (GLOBAL.isWindow()) {
     assert_equals(client.responseXML, null);
   }
+  assert_equals(client.responseText, text);
   test.done();
 });
 client.open("GET", "resources/status.py?type="+encodeURIComponent('text/plain;charset=iso-8859-1')+'&content=%3Cmsg%3E%83%65%83%58%83%67%3C%2Fmsg%3E');
