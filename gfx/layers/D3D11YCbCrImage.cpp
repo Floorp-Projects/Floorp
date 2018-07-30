a5de8459b69e7b9a655235285c18965c86df62e8
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-07-31 08:13:19 +0900

    Bug 1479234 - Include missing headers in D3D11YCbCrImage.cpp and VRLayerChild.cpp. r=hiro
    MozReview-Commit-ID: C8VQO46Lm1

--- gfx/layers/D3D11YCbCrImage.cpp
+++ gfx/layers/D3D11YCbCrImage.cpp
@@ -5,6 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "D3D11YCbCrImage.h"
+
+#include "gfx2DGlue.h"
 #include "YCbCrUtils.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/gfx/DeviceManagerDx.h"
