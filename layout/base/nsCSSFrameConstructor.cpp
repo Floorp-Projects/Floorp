410bea1552ce013e285286d2a9912f6c04d0b272
Emilio Cobos Álvarez <emilio@crisal.io>
2018-07-31 18:50:06 +0200

    No bug - Fix two typos. r=me
    MozReview-Commit-ID: F8vq8l4HwCQ

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -7506,7 +7506,7 @@ nsCSSFrameConstructor::ContentRangeInserted(nsIContent* aStartChild,
     // new nodes, and it has no frame, make a frame construction item for
     // it, because it might need a frame now.  No need to do this if our
     // parent type is not block, though, since WipeContainingBlock
-    // already handles that sitation.
+    // already handles that situation.
     AddTextItemIfNeeded(state, insertion, aStartChild->GetPreviousSibling(),
                         items);
   }
@@ -7528,7 +7528,7 @@ nsCSSFrameConstructor::ContentRangeInserted(nsIContent* aStartChild,
     // new nodes, and it has no frame, make a frame construction item for
     // it, because it might need a frame now.  No need to do this if our
     // parent type is not block, though, since WipeContainingBlock
-    // already handles that sitation.
+    // already handles that situation.
     AddTextItemIfNeeded(state, insertion, aEndChild, items);
   }
 
