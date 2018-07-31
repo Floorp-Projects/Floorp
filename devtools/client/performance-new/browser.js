d948e64c34cf7df97a43f60035338ff831b02c0a
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-07-31 19:52:32 +0300

    Backed out changeset 0a485a8db67c (bug 1472921) for debug-mochitest-chrome failures on test_perf-settings-interval.html

--- devtools/client/performance-new/browser.js
+++ devtools/client/performance-new/browser.js
@@ -111,9 +111,7 @@ async function getRecordingPreferences(preferenceFront, defaultSettings = {}) {
     ),
   ]);
 
-  // The pref stores the value in usec.
-  const newInterval = interval / 1000;
-  return { entries, interval: newInterval, features, threads };
+  return { entries, interval, features, threads };
 }
 
 /**
@@ -132,8 +130,7 @@ async function setRecordingPreferences(preferenceFront, settings) {
     ),
     preferenceFront.setIntPref(
       `devtools.performance.recording.interval`,
-      // The pref stores the value in usec.
-      settings.interval * 1000
+      settings.interval
     ),
     preferenceFront.setCharPref(
       `devtools.performance.recording.features`,
