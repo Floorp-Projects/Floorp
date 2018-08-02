f03e82ef07c2d447edb916d2bf1854f8abf50901
Brian Birtles <birtles@gmail.com>
2018-08-02 09:59:24 +0900

    Bug 1474234 - Don't pass internal props onto child button in MenuButton; r=jdescottes
    Otherwise we will get errors like:
    
    console.error: "Warning: React does not recognize the `menuId` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `menuid` instead. If you accidentally passed it from a parent component, remove it from the DOM element.\n    in button (created by MenuButton)\n    in MenuButton (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in ToolboxToolbar (created by ToolboxController)\n    in ToolboxController"
    
    console.error: "Warning: React does not recognize the `menuPosition` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `menuposition` instead. If you accidentally passed it from a parent component, remove it from the DOM element.\n    in button (created by MenuButton)\n    in MenuButton (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in ToolboxToolbar (created by ToolboxController)\n    in ToolboxController"
    
    console.error: "Warning: React does not recognize the `menuOffset` prop on a DOM element. If you intentionally want it to appear in the DOM as a custom attribute, spell it as lowercase `menuoffset` instead. If you accidentally passed it from a parent component, remove it from the DOM element.\n    in button (created by MenuButton)\n    in MenuButton (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in div (created by ToolboxToolbar)\n    in ToolboxToolbar (created by ToolboxController)\n    in ToolboxController"
    
    It's also not what we intend to do.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2640

--- devtools/client/shared/components/menu/MenuButton.js
+++ devtools/client/shared/components/menu/MenuButton.js
@@ -17,6 +17,15 @@ const {
   HTMLTooltip,
 } = require("devtools/client/shared/widgets/tooltip/HTMLTooltip");
 
+// Return a copy of |obj| minus |fields|.
+const omit = (obj, fields) => {
+  const objCopy = { ...obj };
+  for (const field of fields) {
+    delete objCopy[field];
+  }
+  return objCopy;
+};
+
 class MenuButton extends PureComponent {
   static get propTypes() {
     return {
@@ -266,7 +275,9 @@ class MenuButton extends PureComponent {
     );
 
     const buttonProps = {
-      ...this.props,
+      // Pass through any props set on the button, except the ones we handle
+      // here.
+      ...omit(this.props, Object.keys(MenuButton.propTypes)),
       onClick: this.onClick,
       "aria-expanded": this.state.expanded,
       "aria-haspopup": "menu",
