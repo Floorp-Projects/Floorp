1fdd7f8fc32f7ede9b2dd5647ef53117fb56496e
Olli Pettay <Olli.Pettay@helsinki.fi>
2018-08-02 14:55:09 +0300

    Bug 1480130 - Shadow DOM interferes with Cmd+click handling, r=felipe

--- browser/base/content/test/general/browser_contentAltClick.js
+++ browser/base/content/test/general/browser_contentAltClick.js
@@ -17,12 +17,14 @@ ChromeUtils.defineModuleGetter(this, "Downloads",
 
 function setup() {
   Services.prefs.setBoolPref("browser.altClickSave", true);
+  Services.prefs.setBoolPref("dom.webcomponents.shadowdom.enabled", true);
 
   let testPage =
     "data:text/html," +
     '<p><a id="commonlink" href="http://mochi.test/moz/">Common link</a></p>' +
     '<p><math id="mathxlink" xmlns="http://www.w3.org/1998/Math/MathML" xlink:type="simple" xlink:href="http://mochi.test/moz/"><mtext>MathML XLink</mtext></math></p>' +
-    '<p><svg id="svgxlink" xmlns="http://www.w3.org/2000/svg" width="100px" height="50px" version="1.1"><a xlink:type="simple" xlink:href="http://mochi.test/moz/"><text transform="translate(10, 25)">SVG XLink</text></a></svg></p>';
+    '<p><svg id="svgxlink" xmlns="http://www.w3.org/2000/svg" width="100px" height="50px" version="1.1"><a xlink:type="simple" xlink:href="http://mochi.test/moz/"><text transform="translate(10, 25)">SVG XLink</text></a></svg></p><br>' +
+    '<span id="host"></span><script>document.getElementById("host").attachShadow({mode: "closed"}).appendChild(document.getElementById("commonlink").cloneNode(true));</script>';
 
   return BrowserTestUtils.openNewForegroundTab(gBrowser, testPage);
 }
@@ -39,6 +41,7 @@ async function clean_up() {
   await PlacesUtils.history.clear();
 
   Services.prefs.clearUserPref("browser.altClickSave");
+  Services.prefs.clearUserPref("dom.webcomponents.shadowdom.enabled");
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
 }
 
@@ -66,6 +69,34 @@ add_task(async function test_alt_click() {
 
   is(downloads.length, 1, "1 downloads");
   is(downloads[0].source.url, "http://mochi.test/moz/", "Downloaded #commonlink element");
+
+  await clean_up();
+});
+
+add_task(async function test_alt_click_shadow_dom() {
+  await setup();
+
+  let downloadList = await Downloads.getList(Downloads.ALL);
+  let downloads = [];
+  let downloadView;
+  // When 1 download has been attempted then resolve the promise.
+  let finishedAllDownloads = new Promise( (resolve) => {
+    downloadView = {
+      onDownloadAdded(aDownload) {
+        downloads.push(aDownload);
+        resolve();
+      },
+    };
+  });
+  await downloadList.addView(downloadView);
+  await BrowserTestUtils.synthesizeMouseAtCenter("#host", {altKey: true}, gBrowser.selectedBrowser);
+
+  // Wait for all downloads to be added to the download list.
+  await finishedAllDownloads;
+  await downloadList.removeView(downloadView);
+
+  is(downloads.length, 1, "1 downloads");
+  is(downloads[0].source.url, "http://mochi.test/moz/", "Downloaded #commonlink element in shadow DOM.");
 
   await clean_up();
 });
