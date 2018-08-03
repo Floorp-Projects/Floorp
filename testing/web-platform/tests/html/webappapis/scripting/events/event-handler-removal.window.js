b5cb9765aa26f38b488a19e102e3d75ca5ce9472
Timothy Gu <timothygu99@gmail.com>
2018-08-03 22:22:17 +0000

    Bug 1480185 [wpt PR 12262] - HTML: Capture event handler removal errors correctly, a=testonly
    Automatic update from web-platform-testsHTML: Capture event handler removal errors correctly
    
    --
    
    wpt-commits: 74938e820dbb52a9fa50b6a2ff906c7187e547d5
    wpt-pr: 12262

--- testing/web-platform/tests/html/webappapis/scripting/events/event-handler-removal.window.js
+++ testing/web-platform/tests/html/webappapis/scripting/events/event-handler-removal.window.js
@@ -1,14 +1,17 @@
-test(() => {
+let firstEventHandler;
+
+test(t => {
   var i = 0;
-  var uncalled = "assert_unreached('First event handler.');"
+  firstEventHandler = t.unreached_func('First event handler.');
+  var uncalled = "firstEventHandler();";
   var button = document.createElement('button');
-  button.addEventListener('click', () => { assert_equals(++i, 1); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 1) }), false);
   button.setAttribute('onclick', uncalled);                             // event handler is activated here
-  button.addEventListener('click', () => { assert_equals(++i, 2); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 2) }), false);
   button.onclick = null;                                                // but de-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 3); }, false);
-  button.onclick = () => { assert_equals(++i, 4); };                    // and re-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 5); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 3) }), false);
+  button.onclick = t.step_func(() => { assert_equals(++i, 4); });       // and re-activated here
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 5) }), false);
   button.click()
   assert_equals(button.getAttribute("onclick"), uncalled)
   assert_equals(i, 5);
@@ -26,45 +29,48 @@ test(() => {
   assert_equals(happened, 1);
 }, "Event handler set through content attribute should be re-activated even if content is the same.");
 
-test(() => {
+test(t => {
   var i = 0;
-  var uncalled = "assert_unreached('First event handler.');"
+  firstEventHandler = t.unreached_func('First event handler.');
+  var uncalled = "firstEventHandler();";
   var button = document.createElement('button');
-  button.addEventListener('click', () => { assert_equals(++i, 1); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 1) }), false);
   button.setAttribute('onclick', uncalled);                             // event handler is activated here
-  button.addEventListener('click', () => { assert_equals(++i, 2); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 2) }), false);
   button.removeAttribute('onclick');                                    // but de-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 3); }, false);
-  button.onclick = () => { assert_equals(++i, 4); };                    // and re-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 5); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 3) }), false);
+  button.onclick = t.step_func(() => { assert_equals(++i, 4); });       // and re-activated here
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 5) }), false);
   button.click()
   assert_equals(i, 5);
 }, "Event handler set through content attribute should be deactivated when the content attribute is removed.");
 test(t => {
   var i = 0;
-  var uncalled = "assert_unreached('First event handler.');"
+  firstEventHandler = t.unreached_func('First event handler.');
+  var uncalled = "firstEventHandler();";
   var button = document.createElement('button');
-  button.addEventListener('click', () => { assert_equals(++i, 1); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 1) }), false);
   button.onclick = t.unreached_func('First event handler.');            // event handler is activated here
-  button.addEventListener('click', () => { assert_equals(++i, 2); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 2) }), false);
   button.onclick = null;                                                // but de-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 3); }, false);
-  button.onclick = () => { assert_equals(++i, 4); };                    // and re-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 5); }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 3) }), false);
+  button.onclick = t.step_func(() => { assert_equals(++i, 4); });       // and re-activated here
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 5) }), false);
   button.click()
   assert_equals(i, 5);
 }, "Event handler set through IDL should be deactivated when the IDL attribute is set to null.");
 test(t => {
   var i = 0;
-  var uncalled = "assert_unreached('First event handler.');"
+  firstEventHandler = t.unreached_func('First event handler.');
+  var uncalled = "firstEventHandler();";
   var button = document.createElement('button');
-  button.addEventListener('click', () => { assert_equals(++i, 1) }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 1) }), false);
   button.onclick = t.unreached_func('First event handler.');            // event handler is activated here
-  button.addEventListener('click', () => { assert_equals(++i, 3) }, false);
-  button.removeAttribute('onclick');                                    // but de-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 4) }, false);
-  button.onclick = () => { assert_equals(++i, 2); };                    // and re-activated here
-  button.addEventListener('click', () => { assert_equals(++i, 5) }, false);
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 3) }), false);
+  button.removeAttribute('onclick');                                    // and NOT de-activated here
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 4) }), false);
+  button.onclick = t.step_func(() => { assert_equals(++i, 2); });
+  button.addEventListener('click', t.step_func(() => { assert_equals(++i, 5) }), false);
   button.click()
   assert_equals(i, 5);
 }, "Event handler set through IDL should NOT be deactivated when the content attribute is removed.");
