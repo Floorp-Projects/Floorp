abf5a16722fbd5e8bcff2ea5b2e772c468f684b2
Julian Descottes <jdescottes@mozilla.com>
2018-08-03 07:23:33 +0000

    Bug 1479416 - Add Connect sidebar item to aboutdebugging-new;r=daisuke
    This patch adds:
    - a new action actions/ui:selectPage
      - takes a page id in argument
      - returns type PAGE_SELECTED, with selected page id as 'page'
    - a new reducer reducers/ui-state
      - on PAGE_SELECTED, updates the state by setting 'page' in 'selectedPage'
    
    The state of the application after this patch looks as follows:
    {
      ui: {
        selectedPage: String
      }
    }
    
    As we add more devices the way we identify the selected page might change
    (maybe use an index instead of an id) but that is good enough for now.
    
    The "dispatch" helper is forwarded to the App component via connect.
    From that point, components are responsible for forwarding the dispatch
    property to all components who need to dispatch actions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2549
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/aboutdebugging-new/src/create-store.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { createStore } = require("devtools/client/shared/vendor/redux");
+
+const rootReducer = require("./reducers/index");
+const { UiState } = require("./reducers/ui-state");
+
+exports.configureStore = function() {
+  const initialState = {
+    ui: new UiState()
+  };
+
+  return createStore(rootReducer, initialState);
+};
---
+++ devtools/client/aboutdebugging-new/src/reducers/ui-state.js
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {
+  PAGE_SELECTED,
+  PAGES
+} = require("../constants");
+
+function UiState() {
+  return {
+    selectedPage: PAGES.THIS_FIREFOX
+  };
+}
+
+function uiReducer(state = UiState(), action) {
+  switch (action.type) {
+    case PAGE_SELECTED: {
+      const { page } = action;
+      return { selectedPage: page };
+    }
+
+    default:
+      return state;
+  }
+}
+
+module.exports = {
+  UiState,
+  uiReducer,
+};
