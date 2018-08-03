e3a9a9e23dbd75a78c798e7dd69711915a66f155
Aaron Klotz <aklotz@mozilla.com>
2018-08-03 13:45:35 -0600

    Bug 1480913: Convert extra_args field in talos firefox setup from a str to a list; r=rwood

--- testing/talos/talos/config.py
+++ testing/talos/talos/config.py
@@ -19,7 +19,7 @@ class ConfigurationError(Exception):
 
 DEFAULTS = dict(
     # args to pass to browser
-    extra_args='',
+    extra_args=[],
     buildid='testbuildid',
     init_url='getInfo.html',
     env={'NO_EM_RESTART': '1'},
