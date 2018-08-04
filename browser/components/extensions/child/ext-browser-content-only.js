c38a05236f3dd3923d85c12de8bbefd4644a60f0
Rob Wu <rob@robwu.nl>
2018-08-04 18:09:49 +0200

    Bug 1325814 - Add extension API to find menu target r=mixedpuppy
    - Add info.targetElementId to menus.onShown event.
    
    - Add info.targetElementId to menus.onClicked event.
    
    - Add menus.getTargetElement API that is available to all contexts,
      including content scripts, which allows extensions to get the DOM
      element for a given targetElementId.
    
    - Add new schema instead of re-using schemas/menus.json to avoid sending
      too much schema data (of the existing menus API) to content processes.
    
    MozReview-Commit-ID: 6Onf7jZlIho

---
+++ browser/components/extensions/child/ext-browser-content-only.js
@@ -0,0 +1,11 @@
+"use strict";
+
+extensions.registerModules({
+  menusChild: {
+    url: "chrome://browser/content/child/ext-menus-child.js",
+    scopes: ["content_child"],
+    paths: [
+      ["menus"],
+    ],
+  },
+});
