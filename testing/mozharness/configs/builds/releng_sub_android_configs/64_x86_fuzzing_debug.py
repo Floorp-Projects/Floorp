203888e2985637561418dac808aa3099186adb8b
Jesse Schwartzentruber <truber@mozilla.com>
2018-08-04 09:46:02 +0300

    Bug 1475573 - Create --enable-fuzzing debug build job for Android x86 firefox. r=froydnj,nalexander
    Reviewers: froydnj, nalexander
    
    Reviewed By: froydnj, nalexander
    
    Subscribers: froydnj
    
    Bug #: 1475573
    
    Differential Revision: https://phabricator.services.mozilla.com/D2428

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_fuzzing_debug.py
@@ -0,0 +1,6 @@
+config = {
+    'base_name': 'Android 4.2 x86 %(branch)s debug fuzzing build',
+    'stage_platform': 'android-x86-fuzzing-debug',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86/debug-fuzzing',
+    'debug_build': True,
+}
