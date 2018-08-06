4f70cfcab39b7187345e42fe85f1694cb8f132cc
Johann Hofmann <jhofmann@mozilla.com>
2018-08-06 12:43:59 +0200

    Bug 1476218 - Part 1 - Update Tracking Protection section in the identity popup for Content Blocking. r=nhnt11
    This commit switches the identity popup to use "Content Blocking" as a top-level section
    instead of Tracking Protection, which is now demoted to a category of Content Blocking.
    
    To keep this change halfway reviewable, I avoided renaming variables, classNames, ids, etc.
    that use the term trackingProtection if they were otherwise unaffected by this patch. There
    will be other patches that can do this in the future
    
    Differential Revision: https://phabricator.services.mozilla.com/D2775

--- browser/base/content/browser-contentblocking.js
+++ browser/base/content/browser-contentblocking.js
@@ -2,20 +2,155 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var FastBlock = {
+  PREF_ENABLED: "browser.fastblock.enabled",
+
+  get categoryItem() {
+    delete this.categoryItem;
+    return this.categoryItem = document.getElementById("identity-popup-content-blocking-category-fastblock");
+  },
+
+  init() {
+    XPCOMUtils.defineLazyPreferenceGetter(this, "enabled", this.PREF_ENABLED, false);
+  },
+};
+
 var TrackingProtection = {
-  // If the user ignores the doorhanger, we stop showing it after some time.
-  MAX_INTROS: 20,
   PREF_ENABLED_GLOBALLY: "privacy.trackingprotection.enabled",
   PREF_ENABLED_IN_PRIVATE_WINDOWS: "privacy.trackingprotection.pbmode.enabled",
-  PREF_APP_MENU_TOGGLE: "privacy.trackingprotection.appMenuToggle.enabled",
-  PREF_ANIMATIONS_ENABLED: "toolkit.cosmeticAnimations.enabled",
   enabledGlobally: false,
   enabledInPrivateWindows: false,
-  container: null,
+
+  get categoryItem() {
+    delete this.categoryItem;
+    return this.categoryItem =
+      document.getElementById("identity-popup-content-blocking-category-tracking-protection");
+  },
+
+  get appMenuButton() {
+    delete this.appMenuButton;
+    return this.appMenuButton = document.getElementById("appMenu-tp-toggle");
+  },
+
+  strings: {
+    get enableTooltip() {
+      delete this.enableTooltip;
+      return this.enableTooltip =
+        gNavigatorBundle.getString("trackingProtection.toggle.enable.tooltip");
+    },
+
+    get disableTooltip() {
+      delete this.disableTooltip;
+      return this.disableTooltip =
+        gNavigatorBundle.getString("trackingProtection.toggle.disable.tooltip");
+    },
+
+    get disableTooltipPB() {
+      delete this.disableTooltipPB;
+      return this.disableTooltipPB =
+        gNavigatorBundle.getString("trackingProtection.toggle.disable.pbmode.tooltip");
+    },
+
+    get enableTooltipPB() {
+      delete this.enableTooltipPB;
+      return this.enableTooltipPB =
+        gNavigatorBundle.getString("trackingProtection.toggle.enable.pbmode.tooltip");
+    },
+  },
+
+  init() {
+    this.updateEnabled();
+
+    this.enabledHistogramAdd(this.enabledGlobally);
+    this.disabledPBMHistogramAdd(!this.enabledInPrivateWindows);
+
+    Services.prefs.addObserver(this.PREF_ENABLED_GLOBALLY, this);
+    Services.prefs.addObserver(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, this);
+  },
+
+  uninit() {
+    Services.prefs.removeObserver(this.PREF_ENABLED_GLOBALLY, this);
+    Services.prefs.removeObserver(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, this);
+  },
+
+  observe() {
+    this.updateEnabled();
+  },
+
+  get enabled() {
+    return this.enabledGlobally ||
+           (this.enabledInPrivateWindows &&
+            PrivateBrowsingUtils.isWindowPrivate(window));
+  },
+
+  enabledHistogramAdd(value) {
+    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
+      return;
+    }
+    Services.telemetry.getHistogramById("TRACKING_PROTECTION_ENABLED").add(value);
+  },
+
+  disabledPBMHistogramAdd(value) {
+    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
+      return;
+    }
+    Services.telemetry.getHistogramById("TRACKING_PROTECTION_PBM_DISABLED").add(value);
+  },
+
+  onGlobalToggleCommand() {
+    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
+      Services.prefs.setBoolPref(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, !this.enabledInPrivateWindows);
+    } else {
+      Services.prefs.setBoolPref(this.PREF_ENABLED_GLOBALLY, !this.enabledGlobally);
+    }
+  },
+
+  updateEnabled() {
+    this.enabledGlobally =
+      Services.prefs.getBoolPref(this.PREF_ENABLED_GLOBALLY);
+    this.enabledInPrivateWindows =
+      Services.prefs.getBoolPref(this.PREF_ENABLED_IN_PRIVATE_WINDOWS);
+
+    if (!ContentBlocking.contentBlockingUIEnabled) {
+      ContentBlocking.updateEnabled();
+    }
+
+    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
+      this.appMenuButton.setAttribute("tooltiptext", this.enabledInPrivateWindows ?
+        this.strings.disableTooltipPB : this.strings.enableTooltipPB);
+      this.appMenuButton.setAttribute("enabled", this.enabledInPrivateWindows);
+      this.appMenuButton.setAttribute("aria-pressed", this.enabledInPrivateWindows);
+    } else {
+      this.appMenuButton.setAttribute("tooltiptext", this.enabledGlobally ?
+        this.strings.disableTooltip : this.strings.enableTooltip);
+      this.appMenuButton.setAttribute("enabled", this.enabledGlobally);
+      this.appMenuButton.setAttribute("aria-pressed", this.enabledGlobally);
+    }
+  },
+};
+
+
+var ContentBlocking = {
+  // If the user ignores the doorhanger, we stop showing it after some time.
+  MAX_INTROS: 20,
+  PREF_ENABLED: "browser.contentblocking.enabled",
+  PREF_UI_ENABLED: "browser.contentblocking.ui.enabled",
+  PREF_APP_MENU_TOGGLE: "privacy.trackingprotection.appMenuToggle.enabled",
+  PREF_ANIMATIONS_ENABLED: "toolkit.cosmeticAnimations.enabled",
   content: null,
   icon: null,
   activeTooltipText: null,
   disabledTooltipText: null,
+
+  // A list of blockers that will be displayed in the categories list
+  // when blockable content is detected. A blocker must be an object
+  // with at least the following two properties:
+  //  - enabled: Whether the blocker is currently turned on.
+  //  - categoryItem: The DOM item that represents the entry in the category list.
+  //
+  // It may also contain an init() and uninit() function, which will be called
+  // on ContentBlocking.init() and ContentBlocking.uninit().
+  blockers: [FastBlock, TrackingProtection],
 
   get _baseURIForChannelClassifier() {
     // Convert document URI into the format used by
@@ -32,8 +167,7 @@ var TrackingProtection = {
 
   init() {
     let $ = selector => document.querySelector(selector);
-    this.container = $("#tracking-protection-container");
-    this.content = $("#tracking-protection-content");
+    this.content = $("#identity-popup-content-blocking-content");
     this.icon = $("#tracking-protection-icon");
     this.appMenuContainer = $("#appMenu-tp-container");
     this.appMenuSeparator = $("#appMenu-tp-separator");
@@ -41,27 +175,25 @@ var TrackingProtection = {
     this.animatedIcon = $("#tracking-protection-icon-animatable-image");
     this.animatedIcon.addEventListener("animationend", () => this.iconBox.removeAttribute("animate"));
 
-    this.appMenuButton = $("#appMenu-tp-toggle");
-
-    this.enableTooltip =
-      gNavigatorBundle.getString("trackingProtection.toggle.enable.tooltip");
-    this.disableTooltip =
-      gNavigatorBundle.getString("trackingProtection.toggle.disable.tooltip");
-    this.enableTooltipPB =
-      gNavigatorBundle.getString("trackingProtection.toggle.enable.pbmode.tooltip");
-    this.disableTooltipPB =
-      gNavigatorBundle.getString("trackingProtection.toggle.disable.pbmode.tooltip");
-
     this.updateAnimationsEnabled = () => {
       this.iconBox.toggleAttribute("animationsenabled",
         Services.prefs.getBoolPref(this.PREF_ANIMATIONS_ENABLED, false));
     };
 
+    for (let blocker of this.blockers) {
+      if (blocker.init) {
+        blocker.init();
+      }
+    }
+
     this.updateAnimationsEnabled();
 
     Services.prefs.addObserver(this.PREF_ANIMATIONS_ENABLED, this.updateAnimationsEnabled);
 
-    this.updateEnabled();
+    XPCOMUtils.defineLazyPreferenceGetter(this, "contentBlockingEnabled", this.PREF_ENABLED, false,
+      this.updateEnabled.bind(this));
+    XPCOMUtils.defineLazyPreferenceGetter(this, "contentBlockingUIEnabled", this.PREF_UI_ENABLED, false,
+      this.updateUIEnabled.bind(this));
 
     this.updateAppMenuToggle = () => {
       if (Services.prefs.getBoolPref(this.PREF_APP_MENU_TOGGLE, false)) {
@@ -73,44 +205,40 @@ var TrackingProtection = {
       }
     };
 
-    Services.prefs.addObserver(this.PREF_ENABLED_GLOBALLY, this);
-    Services.prefs.addObserver(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, this);
     Services.prefs.addObserver(this.PREF_APP_MENU_TOGGLE, this.updateAppMenuToggle);
 
     this.updateAppMenuToggle();
+    this.updateEnabled();
+    this.updateUIEnabled();
 
     this.activeTooltipText =
       gNavigatorBundle.getString("trackingProtection.icon.activeTooltip");
     this.disabledTooltipText =
       gNavigatorBundle.getString("trackingProtection.icon.disabledTooltip");
-
-    this.enabledHistogramAdd(this.enabledGlobally);
-    this.disabledPBMHistogramAdd(!this.enabledInPrivateWindows);
   },
 
   uninit() {
-    Services.prefs.removeObserver(this.PREF_ENABLED_GLOBALLY, this);
-    Services.prefs.removeObserver(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, this);
+    for (let blocker of this.blockers) {
+      if (blocker.uninit) {
+        blocker.uninit();
+      }
+    }
+
     Services.prefs.removeObserver(this.PREF_APP_MENU_TOGGLE, this.updateAppMenuToggle);
     Services.prefs.removeObserver(this.PREF_ANIMATIONS_ENABLED, this.updateAnimationsEnabled);
   },
 
-  observe() {
-    this.updateEnabled();
-  },
-
   get enabled() {
-    return this.enabledGlobally ||
-           (this.enabledInPrivateWindows &&
-            PrivateBrowsingUtils.isWindowPrivate(window));
+    return this.contentBlockingUIEnabled ? this.contentBlockingEnabled : TrackingProtection.enabled;
   },
 
-  onGlobalToggleCommand() {
-    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
-      Services.prefs.setBoolPref(this.PREF_ENABLED_IN_PRIVATE_WINDOWS, !this.enabledInPrivateWindows);
-    } else {
-      Services.prefs.setBoolPref(this.PREF_ENABLED_GLOBALLY, !this.enabledGlobally);
-    }
+  updateEnabled() {
+    this.content.toggleAttribute("enabled", this.enabled);
+  },
+
+  updateUIEnabled() {
+    this.content.toggleAttribute("contentBlockingUI", this.contentBlockingUIEnabled);
+    this.updateEnabled();
   },
 
   hideIdentityPopupAndReload() {
@@ -120,41 +248,6 @@ var TrackingProtection = {
 
   openPreferences(origin) {
     openPreferences("privacy-trackingprotection", { origin });
-  },
-
-  updateEnabled() {
-    this.enabledGlobally =
-      Services.prefs.getBoolPref(this.PREF_ENABLED_GLOBALLY);
-    this.enabledInPrivateWindows =
-      Services.prefs.getBoolPref(this.PREF_ENABLED_IN_PRIVATE_WINDOWS);
-
-    this.content.setAttribute("enabled", this.enabled);
-
-    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
-      this.appMenuButton.setAttribute("enabled", this.enabledInPrivateWindows);
-      this.appMenuButton.setAttribute("aria-pressed", this.enabledInPrivateWindows);
-      this.appMenuButton.setAttribute("tooltiptext", this.enabledInPrivateWindows ?
-        this.disableTooltipPB : this.enableTooltipPB);
-    } else {
-      this.appMenuButton.setAttribute("enabled", this.enabledGlobally);
-      this.appMenuButton.setAttribute("aria-pressed", this.enabledGlobally);
-      this.appMenuButton.setAttribute("tooltiptext", this.enabledGlobally ?
-        this.disableTooltip : this.enableTooltip);
-    }
-  },
-
-  enabledHistogramAdd(value) {
-    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
-      return;
-    }
-    Services.telemetry.getHistogramById("TRACKING_PROTECTION_ENABLED").add(value);
-  },
-
-  disabledPBMHistogramAdd(value) {
-    if (PrivateBrowsingUtils.isWindowPrivate(window)) {
-      return;
-    }
-    Services.telemetry.getHistogramById("TRACKING_PROTECTION_PBM_DISABLED").add(value);
   },
 
   eventsHistogramAdd(value) {
@@ -185,7 +278,8 @@ var TrackingProtection = {
     // Don't deal with about:, file: etc.
     if (!baseURI) {
       this.cancelAnimation();
-      this.iconBox.removeAttribute("state");
+      this.iconBox.removeAttribute("active");
+      this.iconBox.removeAttribute("hasException");
       return;
     }
 
@@ -198,6 +292,18 @@ var TrackingProtection = {
 
     let isBlocking = state & Ci.nsIWebProgressListener.STATE_BLOCKED_TRACKING_CONTENT;
     let isAllowing = state & Ci.nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT;
+    let detected = isBlocking || isAllowing;
+
+    // We consider the shield state as "active" when any kind of blocking-related
+    // activity occurs on the page (blocking or allowing). Since we have several
+    // content blockers, we need to go through them individually to figure out which
+    // ones are actually turned on or off.
+    // This state will be overriden later if there's an exception set for this site.
+    let active = this.enabled && detected;
+
+    for (let blocker of this.blockers) {
+      blocker.categoryItem.classList.toggle("blocked", this.enabled && blocker.enabled);
+    }
 
     // Check whether the user has added an exception for this site.
     let hasException = false;
@@ -208,62 +314,36 @@ var TrackingProtection = {
         "trackingprotection") == Services.perms.ALLOW_ACTION;
     }
 
-    if (hasException) {
-      this.iconBox.setAttribute("hasException", "true");
-      this.content.setAttribute("hasException", "true");
-    } else {
-      this.iconBox.removeAttribute("hasException");
-      this.content.removeAttribute("hasException");
-    }
+    this.content.toggleAttribute("detected", detected);
+    this.content.toggleAttribute("hasException", hasException);
 
-    if (isBlocking && this.enabled) {
-      if (isSimulated) {
-        this.cancelAnimation();
-      } else if (webProgress.isTopLevel) {
-        this.iconBox.setAttribute("animate", "true");
-      }
+    this.iconBox.toggleAttribute("active", active);
+    this.iconBox.toggleAttribute("hasException", this.enabled && hasException);
 
-      this.iconBox.setAttribute("tooltiptext", this.activeTooltipText);
-      this.iconBox.setAttribute("state", "blocked-tracking-content");
-      this.content.setAttribute("state", "blocked-tracking-content");
+    if (isSimulated) {
+      this.cancelAnimation();
+    } else if (active && webProgress.isTopLevel) {
+      this.iconBox.setAttribute("animate", "true");
 
       // Open the tracking protection introduction panel, if applicable.
-      if (this.enabledGlobally) {
+      if (TrackingProtection.enabledGlobally) {
         let introCount = Services.prefs.getIntPref("privacy.trackingprotection.introCount");
-        if (introCount < TrackingProtection.MAX_INTROS) {
+        if (introCount < this.MAX_INTROS) {
           Services.prefs.setIntPref("privacy.trackingprotection.introCount", ++introCount);
           Services.prefs.savePrefFile(null);
           this.showIntroPanel();
         }
       }
+    }
 
+    if (hasException) {
+      this.iconBox.setAttribute("tooltiptext", this.disabledTooltipText);
+      this.shieldHistogramAdd(1);
+    } else if (active) {
+      this.iconBox.setAttribute("tooltiptext", this.activeTooltipText);
       this.shieldHistogramAdd(2);
-    } else if (isAllowing) {
-      if (isSimulated) {
-        this.cancelAnimation();
-      } else if (webProgress.isTopLevel) {
-        this.iconBox.setAttribute("animate", "true");
-      }
-
-      // Only show the shield when TP is enabled for now.
-      if (this.enabled) {
-        this.iconBox.setAttribute("tooltiptext", this.disabledTooltipText);
-        this.iconBox.setAttribute("state", "loaded-tracking-content");
-        this.shieldHistogramAdd(1);
-      } else {
-        this.iconBox.removeAttribute("tooltiptext");
-        this.iconBox.removeAttribute("state");
-        this.shieldHistogramAdd(0);
-      }
-
-      // Warn in the control center even with TP disabled.
-      this.content.setAttribute("state", "loaded-tracking-content");
     } else {
       this.iconBox.removeAttribute("tooltiptext");
-      this.iconBox.removeAttribute("state");
-      this.content.removeAttribute("state");
-
-      // We didn't show the shield
       this.shieldHistogramAdd(0);
     }
 
@@ -311,7 +391,7 @@ var TrackingProtection = {
   dontShowIntroPanelAgain() {
     // This function may be called in private windows, but it does not change
     // any preference unless Tracking Protection is enabled globally.
-    if (this.enabledGlobally) {
+    if (TrackingProtection.enabledGlobally) {
       Services.prefs.setIntPref("privacy.trackingprotection.introCount",
                                 this.MAX_INTROS);
       Services.prefs.savePrefFile(null);
