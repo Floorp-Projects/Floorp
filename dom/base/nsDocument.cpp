1a8c36d58a30e7fbe0b98df454b52f684fe81d4a
Edgar Chen <echen@mozilla.com>
2018-08-07 07:47:00 +0000

    Bug 1127588 - Fix the DOMNodeRemoved mutation event isn't fired when updating document.title; r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D2774
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -6245,13 +6245,13 @@ nsIDocument::SetTitle(const nsAString& aTitle, ErrorResult& aRv)
   }
 #endif
 
-  // Batch updates so that mutation events don't change "the title
-  // element" under us
-  mozAutoDocUpdate updateBatch(this, true);
-
+  Maybe<mozAutoDocUpdate> updateBatch;
   nsCOMPtr<Element> title = GetTitleElement();
   if (rootElement->IsSVGElement(nsGkAtoms::svg)) {
     if (!title) {
+      // Batch updates so that mutation events don't change "the title
+      // element" under us
+      updateBatch.emplace(this, true);
       RefPtr<mozilla::dom::NodeInfo> titleInfo =
         mNodeInfoManager->GetNodeInfo(nsGkAtoms::title, nullptr,
                                       kNameSpaceID_SVG,
@@ -6265,6 +6265,9 @@ nsIDocument::SetTitle(const nsAString& aTitle, ErrorResult& aRv)
     }
   } else if (rootElement->IsHTMLElement()) {
     if (!title) {
+      // Batch updates so that mutation events don't change "the title
+      // element" under us
+      updateBatch.emplace(this, true);
       Element* head = GetHeadElement();
       if (!head) {
         return;
