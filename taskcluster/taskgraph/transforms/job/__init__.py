1521b171c9fa368d8f7e5fb0ec4ca9a95ce181d3
Tom Prince <mozilla@hocat.ca>
2018-08-09 12:51:31 -0600

    Bug 1484012: [taskgraph] Allow windows mozharness tasks to use `fetch-content`; r=gps
    Differential Revision: https://phabricator.services.mozilla.com/D3557

--- taskcluster/taskgraph/transforms/job/__init__.py
+++ taskcluster/taskgraph/transforms/job/__init__.py
@@ -220,8 +220,13 @@ def use_fetches(config, jobs):
         env = job.setdefault('worker', {}).setdefault('env', {})
         env['MOZ_FETCHES'] = {'task-reference': json.dumps(job_fetches, sort_keys=True)}
 
-        workdir = job['run'].get('workdir', '/builds/worker')
-        env.setdefault('MOZ_FETCHES_DIR', '{}/fetches'.format(workdir))
+        impl, os = worker_type_implementation(job['worker-type'])
+        if os == 'windows':
+            env.setdefault('MOZ_FETCHES_DIR', 'fetches')
+        else:
+            workdir = job['run'].get('workdir', '/builds/worker')
+            env.setdefault('MOZ_FETCHES_DIR', '{}/fetches'.format(workdir))
+
         yield job
 
 
