ecd7e0d4d06beea437b415a5d73a51aee54d010a
Joel Maher <jmaher@mozilla.com>
2018-08-09 19:32:34 +0000

    Bug 1480870 - Summary:fix xperf imports and fix errors with NULL values in parsing csv rows. r=rwood
    Differential Revision: https://phabricator.services.mozilla.com/D3022
    
    --HG--
    extra : moz-landing-system : lando

--- testing/talos/talos/xtalos/etlparser.py
+++ testing/talos/talos/xtalos/etlparser.py
@@ -14,7 +14,7 @@ import subprocess
 import sys
 
 import mozfile
-from . import xtalos
+import xtalos
 
 EVENTNAME_INDEX = 0
 PROCESS_INDEX = 2
@@ -66,7 +66,15 @@ def filterOutHeader(data):
     # 0 means we are in the header
     # 1+ means that we are past the header
     state = -1
-    for row in data:
+    done = False
+    while not done:
+        try:
+            row = data.next()
+        except StopIteration:
+            done = True
+            break
+        except csv.Error:
+            continue
 
         if not len(row):
             continue
