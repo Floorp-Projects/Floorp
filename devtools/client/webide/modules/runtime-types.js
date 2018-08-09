5bd9b4eab11ab2ff7158b35d3deb17bc35c193cc
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-08-09 14:27:56 +0900

    Bug 1469054 - Split Runtime.RuntimeTypes into a new file to import the RuntimeTypes in a file which is importing runtimes.js. r=jdescottes
    MozReview-Commit-ID: LVfyW3aaSxU

---
+++ devtools/client/webide/modules/runtime-types.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// These type strings are used for logging events to Telemetry.
+// You must update Histograms.json if new types are added.
+const RuntimeTypes = {
+  USB: "USB",
+  WIFI: "WIFI",
+  REMOTE: "REMOTE",
+  LOCAL: "LOCAL",
+  OTHER: "OTHER"
+};
+
+exports.RuntimeTypes = RuntimeTypes;
