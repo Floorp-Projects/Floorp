5d23c3122357490f940b6cb64289d5c0fbc40e20
Andrea Marchesini <amarchesini@mozilla.com>
2018-08-09 10:45:14 -0400

    Bug 1480354 - Better approach for nested blobs construction. r=smaug, a=RyanVM

--- dom/file/BlobSet.cpp
+++ dom/file/BlobSet.cpp
@@ -59,6 +59,21 @@ nsresult
 BlobSet::AppendBlobImpl(BlobImpl* aBlobImpl)
 {
   NS_ENSURE_ARG_POINTER(aBlobImpl);
+
+  // If aBlobImpl is a MultipartBlobImpl, let's append the sub-blobImpls
+  // instead.
+  const nsTArray<RefPtr<BlobImpl>>* subBlobs = aBlobImpl->GetSubBlobImpls();
+  if (subBlobs) {
+    for (BlobImpl* subBlob : *subBlobs) {
+      nsresult rv = AppendBlobImpl(subBlob);
+      if (NS_WARN_IF(NS_FAILED(rv))) {
+        return rv;
+      }
+    }
+
+    return NS_OK;
+  }
+
   if (NS_WARN_IF(!mBlobImpls.AppendElement(aBlobImpl, fallible))) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
