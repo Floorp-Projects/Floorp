3f2a72d71460d977b20e1edb1679fd669eb7dec7
Chris Manchester <cmanchester@mozilla.com>
2018-08-10 22:29:35 +0000

    Bug 1482516 - Use set comparison to determine whether the list of build backend outputs has changed. r=gps,firefox-build-system-reviewers
    If a backend output file is deleted outside of the build system the non-zero
    "created" count can make the current check erroneously think the list of
    outputs has changed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3129

--- python/mozbuild/mozbuild/backend/base.py
+++ python/mozbuild/mozbuild/backend/base.py
@@ -169,8 +169,7 @@ class BuildBackend(LoggingMixin):
                 pass
 
         # Write out the list of backend files generated, if it changed.
-        if self._deleted_count or self._created_count or \
-                not os.path.exists(list_file):
+        if backend_output_list != self._backend_output_files:
             with self._write_file(list_file) as fh:
                 fh.write('\n'.join(sorted(self._backend_output_files)))
         else:
