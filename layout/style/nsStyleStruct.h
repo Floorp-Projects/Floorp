1a3d3cd1dd209d4185c8e8062642725922081825
Cosmin Sabou <csabou@mozilla.com>
2018-08-10 07:17:58 +0300

    Backed out changeset b580e5b9f9e4 (bug 1476495) for wpt reftest failures on contain-layout-ink-overflow. CLOSED TREE

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -2347,19 +2347,6 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStyleDisplay
            !IsInternalTableStyleExceptCell();
   }
 
-  bool IsContainLayout() const {
-    // Note: The spec for layout containment says it should
-    // have no effect on non-atomic, inline-level boxes. We
-    // don't check for these here because we don't know
-    // what type of element is involved. Callers are
-    // responsible for checking if the box in question is
-    // non-atomic and inline-level, and creating an
-    // exemption as necessary.
-    return (NS_STYLE_CONTAIN_LAYOUT & mContain) &&
-            !IsInternalRubyDisplayType() &&
-            !IsInternalTableStyleExceptCell();
-  }
-
   bool IsContainSize() const {
     // Note: The spec for size containment says it should
     // have no effect on non-atomic, inline-level boxes. We
