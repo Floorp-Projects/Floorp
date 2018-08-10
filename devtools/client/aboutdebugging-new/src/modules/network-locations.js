e71b75d6794b0e9a08d8daeaed9f48433be0d360
Julian Descottes <jdescottes@mozilla.com>
2018-08-10 09:38:55 +0200

    Bug 1482054 - Create module to manage network locations;r=daisuke
    This patch introduces a new preference:
      devtools.aboutdebugging.network-locations
    
    - this preference is a string pref, that should be a stringified
      JavaScript array.
    - the default value is "[]".
    - the module supports getting, adding, removing and observing
      network locations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3096

---
+++ devtools/client/aboutdebugging-new/src/modules/network-locations.js
@@ -0,0 +1,62 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const Services = require("Services");
+
+const NETWORK_LOCATIONS_PREF = "devtools.aboutdebugging.network-locations";
+
+/**
+ * This module provides a collection of helper methods to read and update network
+ * locations monitored by about-debugging.
+ */
+
+function addNetworkLocationsObserver(listener) {
+  Services.prefs.addObserver(NETWORK_LOCATIONS_PREF, listener);
+}
+exports.addNetworkLocationsObserver = addNetworkLocationsObserver;
+
+/**
+ * Read the current preference value for aboutdebugging network locations.
+ * Will throw if the value cannot be parsed or is not an array.
+ */
+function _parsePreferenceAsArray() {
+  const pref = Services.prefs.getStringPref(NETWORK_LOCATIONS_PREF, "[]");
+  const parsedValue = JSON.parse(pref);
+  if (!Array.isArray(parsedValue)) {
+    throw new Error("Expected array value in " + NETWORK_LOCATIONS_PREF);
+  }
+  return parsedValue;
+}
+
+function getNetworkLocations() {
+  try {
+    return _parsePreferenceAsArray();
+  } catch (e) {
+    Services.prefs.clearUserPref(NETWORK_LOCATIONS_PREF);
+    return [];
+  }
+}
+exports.getNetworkLocations = getNetworkLocations;
+
+function addNetworkLocation(location) {
+  const locations = getNetworkLocations();
+  const locationsSet = new Set(locations);
+  locationsSet.add(location);
+
+  Services.prefs.setStringPref(NETWORK_LOCATIONS_PREF,
+    JSON.stringify([...locationsSet]));
+}
+exports.addNetworkLocation = addNetworkLocation;
+
+function removeNetworkLocation(location) {
+  const locations = getNetworkLocations();
+  const locationsSet = new Set(locations);
+  locationsSet.delete(location);
+
+  Services.prefs.setStringPref(NETWORK_LOCATIONS_PREF,
+    JSON.stringify([...locationsSet]));
+}
+exports.removeNetworkLocation = removeNetworkLocation;
