f0746a4d99383f9d6edf462d05217248c64320d2
Aaron Klotz <aklotz@mozilla.com>
2018-08-10 12:03:54 -0600

    Bug 1463483: Add session store analysis to xperf; r=jmaher
    This patch uses the new xperf analyzer to determine the duration of time from
    the start of the first firefox process to the session store window restored
    event.
    
    I'd like to have this running for a little bit to collect some data points
    before we flip the switch to enable the launcher process by default.

--- testing/talos/talos/xtalos/parse_xperf.py
+++ testing/talos/talos/xtalos/parse_xperf.py
@@ -10,7 +10,39 @@ import subprocess
 import sys
 
 import etlparser
+import mozfile
 import xtalos
+from xperf_analyzer import (ProcessStart, SessionStoreWindowRestored,
+                            XPerfAttribute, XPerfFile, XPerfInterval)
+
+
+def run_session_restore_analysis(debug=False, **kwargs):
+    required = ('csv_filename', 'outputFile')
+    for r in required:
+        if r not in kwargs:
+            raise xtalos.XTalosError('%s required' % r)
+
+    final_output_file = "%s_session_restore_stats%s" % os.path.splitext(
+        kwargs['outputFile'])
+
+    output = 'time_to_session_store_window_restored_ms\n'
+
+    with XPerfFile(csvfile=kwargs['csv_filename'], debug=debug) as xperf:
+        fx_start = ProcessStart('firefox.exe')
+        ss_window_restored = SessionStoreWindowRestored()
+
+        interval = XPerfInterval(fx_start, ss_window_restored)
+        xperf.add_attr(interval)
+
+        xperf.analyze()
+
+        output += "%.3f\n" % (interval.get_results()[XPerfAttribute.RESULT])
+
+    with open(final_output_file, 'w') as out:
+        out.write(output)
+
+    if debug:
+        etlparser.uploadFile(final_output_file)
 
 
 def stop(xperf_path, debug=False):
@@ -57,6 +89,12 @@ def stop_from_config(config_file=None, debug=False, **kwargs):
                                     error_filename=kwargs['error_filename'],
                                     processID=kwargs['processID'])
 
+    csv_base = '%s.csv' % kwargs['etl_filename']
+    run_session_restore_analysis(csv_filename=csv_base, debug=debug, **kwargs)
+
+    if not debug:
+        mozfile.remove(csv_base)
+
 
 def main(args=sys.argv[1:]):
 
@@ -64,7 +102,7 @@ def main(args=sys.argv[1:]):
     parser = xtalos.XtalosOptions()
     args = parser.parse_args(args)
 
-    # start xperf
+    # stop xperf
     try:
         stop_from_config(config_file=args.configFile,
                          debug=args.debug_level >= xtalos.DEBUG_INFO,
