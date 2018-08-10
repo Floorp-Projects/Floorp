c50d2b7c2045319a3793268395d459edab10f585
Yura Zenevich <yura.zenevich@gmail.com>
2018-08-10 15:33:51 -0400

    Bug 1482454 - Display a "New" indicator to promote the accessibility panel. r=gl
    MozReview-Commit-ID: 4T9a0Rxw2db

--- devtools/client/framework/components/ToolboxTab.js
+++ devtools/client/framework/components/ToolboxTab.js
@@ -41,7 +41,7 @@ class ToolboxTab extends Component {
   render() {
     const {panelDefinition, currentToolId, highlightedTools, selectTool,
            focusedButton, focusButton} = this.props;
-    const {id, extensionId, tooltip, label, iconOnly} = panelDefinition;
+    const {id, extensionId, tooltip, label, iconOnly, badge} = panelDefinition;
     const isHighlighted = id === currentToolId;
 
     const className = [
@@ -81,7 +81,15 @@ class ToolboxTab extends Component {
           {
             className: "devtools-tab-label"
           },
-          label
+          label,
+          badge && !isHighlighted ?
+            span(
+              {
+                className: "devtools-tab-badge"
+              },
+              badge
+            ) :
+            null
         )
     );
   }
