ea0ab49f584adc9ec1535813172c744b2a97c2a6
Ed Lee <edilee@mozilla.com>
2018-08-11 13:26:39 +0000

    Bug 1482565 - Support and show @Яндекс and @百度 searches for yandex and baidu r=mkaply
    Depends on D3133
    Depends on D3033
    
    Differential Revision: https://phabricator.services.mozilla.com/D3139

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -1006,8 +1006,8 @@ const ENGINE_ALIASES = new Map([
   ["ebay", ["@ebay"]],
   ["bing", ["@bing"]],
   ["ddg", ["@duckduckgo", "@ddg"]],
-  ["yandex", ["@yandex"]],
-  ["baidu", ["@baidu"]],
+  ["yandex", ["@\u044F\u043D\u0434\u0435\u043A\u0441", "@yandex"]],
+  ["baidu", ["@\u767E\u5EA6", "@baidu"]],
 ]);
 
 function getInternalAliases(engine) {
