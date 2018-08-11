0d443a3096fba204ae3bd9ca5754c1a5e185c363
Jean-Yves Avenard <jyavenard@mozilla.com>
2018-08-11 20:23:58 +0200

    Bug 1482605 - Don't error on Opus channel mapping version 2. r=padenot
    We do not perform any channels placement, instead we leave the channels in the order they come in.
    
    Add corresponding tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3153

--- dom/media/test/manifest.js
+++ dom/media/test/manifest.js
@@ -264,6 +264,8 @@ var gPlayTests = [
   { name:"detodos-short.opus", type:"audio/ogg; codecs=opus", duration:0.22 },
   // Opus data in a webm container
   { name:"detodos-short.webm", type:"audio/webm; codecs=opus", duration:0.26 },
+  // Opus in webm channel mapping=2 sample file
+  { name:"opus-mapping2.webm", type:"audio/webm; codecs=opus", duration:10.01 },
   { name:"bug1066943.webm", type:"audio/webm; codecs=opus", duration:1.383 },
 
   // Multichannel Opus in an ogg container
@@ -285,6 +287,8 @@ var gPlayTests = [
   { name:"ambisonics.mp4", type:"audio/mp4", duration:16.48 },
   // Opus in MP4 channel mapping=0 sample file
   { name:"opus-sample.mp4", type:"audio/mp4; codecs=opus", duration:10.92 },
+  // Opus in MP4 channel mapping=2 sample file
+  { name:"opus-mapping2.mp4", type:"audio/mp4; codecs=opus", duration:10.0 },
 
   { name:"small-shot.m4a", type:"audio/mp4", duration:0.29 },
   { name:"small-shot.mp3", type:"audio/mpeg", duration:0.27 },
