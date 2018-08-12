a528faad7350cf2399aad2de57901cdb9a152711
Emilio Cobos Álvarez <emilio@crisal.io>
2018-08-12 18:17:48 +0000

    Bug 1482694 - Don't clear the insertion point from ClearInsertedChildren if we're not the insertion point the node is assigned to. r=smaug
    What's going on is that nested XBL insertion points are completely unsound, and
    we leave all sorts of kids in mInsertedChildren when bindings dynamically
    change.
    
    So if bindings change in a particular way so that the innermost insertion point
    is cleared, but the outermost bindings aren't, like this, we end up with an
    inconsistent flattened tree.
    
    This prevents the inconsistent flattened tree in this case, though what ought
    to happen in the SetXBLInsertionPoint(nullptr) case is setting the insertion
    point to the outer insertion point.
    
    But we don't keep track of all our insertion points, and I don't think it's
    worth to fix that given XBL is going away unless it gives us more problems. See
    also bug 1425362 & co.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3163

--- dom/xbl/XBLChildrenElement.h
+++ dom/xbl/XBLChildrenElement.h
@@ -73,7 +73,9 @@ public:
   void ClearInsertedChildren()
   {
     for (auto* child : mInsertedChildren) {
-      child->SetXBLInsertionPoint(nullptr);
+      if (child->GetXBLInsertionPoint() == this) {
+        child->SetXBLInsertionPoint(nullptr);
+      }
     }
     mInsertedChildren.Clear();
 
