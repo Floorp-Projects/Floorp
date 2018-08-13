1e6d3dd9e670778a0a6f1214b1e712aca2eee83c
Francois Marier <francois@mozilla.com>
2018-08-13 10:06:37 +0000

    Bug 1480448 - Honor browser.contentblocking.enabled in Fastblock. r=xeonchen!
    Differential Revision: https://phabricator.services.mozilla.com/D3135

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -627,11 +627,13 @@ nsHttpChannel::CheckFastBlocked()
     LOG(("nsHttpChannel::CheckFastBlocked [this=%p]\n", this));
 
     static bool sFastBlockInited = false;
+    static bool sIsContentBlockingEnabled = false;
     static bool sIsFastBlockEnabled = false;
     static uint32_t sFastBlockTimeout = 0;
 
     if (!sFastBlockInited) {
         sFastBlockInited = true;
+        Preferences::AddBoolVarCache(&sIsContentBlockingEnabled, "browser.contentblocking.enabled");
         Preferences::AddBoolVarCache(&sIsFastBlockEnabled, "browser.fastblock.enabled");
         Preferences::AddUintVarCache(&sFastBlockTimeout, "browser.fastblock.timeout");
     }
@@ -641,7 +643,7 @@ nsHttpChannel::CheckFastBlocked()
         return false;
     }
 
-    if (!sIsFastBlockEnabled ||
+    if (!sIsContentBlockingEnabled || !sIsFastBlockEnabled ||
         IsContentPolicyTypeWhitelistedForFastBlock(mLoadInfo) ||
         !timestamp) {
         return false;
