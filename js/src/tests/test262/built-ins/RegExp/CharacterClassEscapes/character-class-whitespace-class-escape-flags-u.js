c2cebcfbe0a263da6b44517c2005bf351d72d4d6
Jason Orendorff <jorendorff@mozilla.com>
2018-08-14 13:35:50 -0500

    Bug 1483380 - Update test262 from upstream. r=anba

---
+++ js/src/tests/test262/built-ins/RegExp/CharacterClassEscapes/character-class-whitespace-class-escape-flags-u.js
@@ -0,0 +1,72 @@
+// Copyright (C) 2018 Leo Balter.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: prod-CharacterClassEscape
+description: >
+    Compare range for Whitespace class escape, \\s with flags ug
+info: |
+    This is a generated test, please checkout https://github.com/bocoup/test262-regexp-generator
+    for any changes.
+
+    CharacterClassEscape[U] ::
+        d
+        D
+        s
+        S
+        w
+        W
+
+    21.2.2.12 CharacterClassEscape
+    
+    The production CharacterClassEscape :: d evaluates as follows:
+        Return the ten-element set of characters containing the characters 0 through 9 inclusive.
+    The production CharacterClassEscape :: D evaluates as follows:
+        Return the set of all characters not included in the set returned by CharacterClassEscape :: d.
+    The production CharacterClassEscape :: s evaluates as follows:
+        Return the set of characters containing the characters that are on the right-hand side of
+        the WhiteSpace or LineTerminator productions.
+    The production CharacterClassEscape :: S evaluates as follows:
+        Return the set of all characters not included in the set returned by CharacterClassEscape :: s.
+    The production CharacterClassEscape :: w evaluates as follows:
+        Return the set of all characters returned by WordCharacters().
+    The production CharacterClassEscape :: W evaluates as follows:
+        Return the set of all characters not included in the set returned by CharacterClassEscape :: w.
+features: [String.fromCodePoint]
+---*/
+
+const chunks = [];
+const totalChunks = Math.ceil(0x10ffff / 0x10000);
+
+for (let codePoint = 0; codePoint < 0x10FFFF; codePoint++) {
+    // split strings to avoid a super long one;
+    chunks[codePoint % totalChunks] += String.fromCodePoint(codePoint);
+}
+
+const re = /\s/ug;
+const matchingRange = /[\t-\r \xA0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000\uFEFF]/ug;
+
+const errors = [];
+
+function matching(str) {
+    return str.replace(re, '') === str.replace(matchingRange, '');
+}
+
+for (const str of chunks) {
+    if (!matching(str)) {
+        // Error, let's find out where
+        for (const char of str) {
+            if (!matching(char)) {
+                errors.push('0x' + char.codePointAt(0).toString(16));
+            }
+        }
+    }
+};
+
+assert.sameValue(
+    errors.length,
+    0,
+    'Expected matching code points, but received: ' + errors.join(',')
+);
+
+reportCompare(0, 0);
---
+++ js/src/tests/test262/built-ins/RegExp/property-escapes/character-class-range-no-dash-end.js
@@ -0,0 +1,21 @@
+// |reftest| skip error:SyntaxError -- regexp-unicode-property-escapes is not supported
+// Copyright 2018 Mathias Bynens. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+author: Mathias Bynens
+description: >
+  NonemptyClassRangesNoDash :: ClassAtomNoDash - ClassAtom ClassRanges
+
+  It is a Syntax Error if IsCharacterClass of ClassAtomNoDash is true or
+  IsCharacterClass of ClassAtom is true.
+esid: sec-patterns-static-semantics-early-errors
+negative:
+  phase: parse
+  type: SyntaxError
+features: [regexp-unicode-property-escapes]
+---*/
+
+throw "Test262: This statement should not be evaluated.";
+
+/[\uFFFF-\p{Hex}]/u;
