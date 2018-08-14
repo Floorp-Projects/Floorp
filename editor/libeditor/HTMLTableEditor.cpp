36208776537d07b6384fe912f5d167085323bd19
Masayuki Nakano <masayuki@d-toybox.com>
2018-08-14 08:36:59 +0000

    Bug 1482016 - part 1: Create HTMLEditor::SelectContentInternal() for internal use of HTMLEditor::SelectElement() r=m_kato
    For making it possible HTMLEditor::SelectElement() to distinguish if it's
    called by outer class or editor itself, this patch creates
    HTMLEditor::SelectContentInternal().
    
    Differential Revision: https://phabricator.services.mozilla.com/D3001

--- editor/libeditor/HTMLTableEditor.cpp
+++ editor/libeditor/HTMLTableEditor.cpp
@@ -3075,7 +3075,9 @@ HTMLEditor::SetSelectionAfterTableEdit(Element* aTable,
     if (cell) {
       if (aSelected) {
         // Reselect the cell
-        SelectElement(cell);
+        DebugOnly<nsresult> rv = SelectContentInternal(*selection, *cell);
+        NS_WARNING_ASSERTION(NS_SUCCEEDED(rv),
+          "Failed to select the cell");
         return;
       }
 
