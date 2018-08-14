eebf6a802d60573c7cb1654dde21c25463c2f039
Brian Hackett <bhackett1024@gmail.com>
2018-08-14 00:40:15 +0000

    Bug 1481351 - Fix some issues around PBrowser::Show message handling, tidy up web replay child headers, r=mccr8.

--- toolkit/recordreplay/ipc/ChildIPC.h
+++ toolkit/recordreplay/ipc/ChildIPC.h
@@ -18,51 +18,52 @@ class VsyncObserver;
 namespace recordreplay {
 namespace child {
 
-// Naively replaying a child process execution will not perform any IPC. When
-// the replaying process attempts to make system calls that communicate with
-// the parent, function redirections are invoked that simply replay the values
-// which those calls produced in the original recording.
-//
-// The replayed process needs to be able to communicate with the parent in some
-// ways, however. IPDL messages need to be sent to the compositor in the parent
-// to render graphics, and the parent needs to send messages to the client to
-// control and debug the replay.
-//
-// This file manages the real IPC which occurs in a replaying process. New
-// threads --- which did not existing while recording --- are spawned to manage
-// IPC with the middleman process, and IPDL actors are created up front for use
-// in communicating with the middleman using the PReplay protocol.
-
-///////////////////////////////////////////////////////////////////////////////
-// Public API
-///////////////////////////////////////////////////////////////////////////////
+// This file has the public API for definitions used in facilitating IPC
+// between a child recording/replaying process and the middleman process.
 
 // Initialize replaying IPC state. This is called once during process startup,
 // and is a no-op if the process is not recording/replaying.
 void InitRecordingOrReplayingProcess(int* aArgc, char*** aArgv);
 
+// Get the IDs of the middleman and parent processes.
 base::ProcessId MiddlemanProcessId();
 base::ProcessId ParentProcessId();
 
+// Create a normal checkpoint, if no such checkpoint has been created yet.
+void MaybeCreateInitialCheckpoint();
+
+///////////////////////////////////////////////////////////////////////////////
+// Painting Coordination
+///////////////////////////////////////////////////////////////////////////////
+
+// In child processes, paints do not occur in response to vsyncs from the UI
+// process: when a page is updating rapidly these events occur sporadically and
+// cause the tab's graphics to not accurately reflect the tab's state at that
+// point in time. When viewing a normal tab this is no problem because the tab
+// will be painted with the correct graphics momentarily, but when the tab can
+// be rewound and paused this behavior is visible.
+//
+// This API is used to trigger artificial vsyncs whenever the page is updated.
+// SetVsyncObserver is used to tell the child code about any singleton vsync
+// observer that currently exists, and NotifyVsyncObserver is used to trigger
+// a vsync on that observer at predictable points, e.g. the top of the main
+// thread's event loop.
 void SetVsyncObserver(VsyncObserver* aObserver);
 void NotifyVsyncObserver();
 
-void NotifyPaint();
+// Similarly to the vsync handling above, in order to ensure that the tab's
+// graphics accurately reflect its state, we want to perform paints
+// synchronously after a vsync has occurred. When a paint is about to happen,
+// the main thread calls NotifyPaintStart, and after the compositor thread has
+// been informed about the update the main thread calls WaitForPaintToComplete
+// to block until the compositor thread has finished painting and called
+// NotifyPaintComplete.
 void NotifyPaintStart();
-void NotifyPaintComplete();
 void WaitForPaintToComplete();
+void NotifyPaintComplete();
 
+// Get a draw target which the compositor thread can paint to.
 already_AddRefed<gfx::DrawTarget> DrawTargetForRemoteDrawing(LayoutDeviceIntSize aSize);
-
-// Notify the middleman that the recording was flushed.
-void NotifyFlushedRecording();
-
-// Notify the middleman about an AlwaysMarkMajorCheckpoints directive.
-void NotifyAlwaysMarkMajorCheckpoints();
-
-// Mark a time span when the main thread is idle.
-void BeginIdleTime();
-void EndIdleTime();
 
 } // namespace child
 } // namespace recordreplay
