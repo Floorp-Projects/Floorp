be9b63b1e294c12ba6ea524b45eed1f330e315ae
Alexandre Poirot <poirot.alex@gmail.com>
2018-08-14 08:11:57 -0700

    Bug 1482070 - Move ContentProcessListener to its own file. r=Honza
    Summary: Depends On D3600
    
    Reviewers: Honza!
    
    Tags: #secure-revision
    
    Bug #: 1482070
    
    Differential Revision: https://phabricator.services.mozilla.com/D3601
    
    MozReview-Commit-ID: CHReMMFXFHT

--- devtools/server/actors/webconsole/listeners.js
+++ devtools/server/actors/webconsole/listeners.js
@@ -12,46 +12,6 @@ const {CONSOLE_WORKER_IDS, WebConsoleUtils} = require("devtools/server/actors/we
 
 loader.lazyRequireGetter(this, "EventEmitter", "devtools/shared/event-emitter");
 
-// Process script used to forward console calls from content processes to parent process
-const CONTENT_PROCESS_SCRIPT = "resource://devtools/server/actors/webconsole/content-process-forward.js";
-
-
-/**
- * Forward console message calls from content processes to the parent process.
- * Used by Browser console and toolbox to see messages from all processes.
- *
- * @constructor
- * @param object owner
- *        The listener owner which needs to implement:
- *        - onConsoleAPICall(message)
- */
-function ContentProcessListener(listener) {
-  this.listener = listener;
-
-  Services.ppmm.addMessageListener("Console:Log", this);
-  Services.ppmm.loadProcessScript(CONTENT_PROCESS_SCRIPT, true);
-}
-
-exports.ContentProcessListener = ContentProcessListener;
-
-ContentProcessListener.prototype = {
-  receiveMessage(message) {
-    const logMsg = message.data;
-    logMsg.wrappedJSObject = logMsg;
-    this.listener.onConsoleAPICall(logMsg);
-  },
-
-  destroy() {
-    // Tell the content processes to stop listening and forwarding messages
-    Services.ppmm.broadcastAsyncMessage("DevTools:StopForwardingContentProcessMessage");
-
-    Services.ppmm.removeMessageListener("Console:Log", this);
-    Services.ppmm.removeDelayedProcessScript(CONTENT_PROCESS_SCRIPT);
-
-    this.listener = null;
-  }
-};
-
 /**
  * Forward `DOMContentLoaded` and `load` events with precise timing
  * of when events happened according to window.performance numbers.
