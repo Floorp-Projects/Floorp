d95fba398c03dc782519312b39853f51a3eaa982
Gabriel Luong <gabriel.luong@gmail.com>
2018-08-15 17:27:51 -0400

    Bug 1467572 - Part 15: Fix unit tests for RDM to work with the new RDM design. r=Honza
    - Various element IDs and strings were renamed in the preceeding patches before this. This fixes
    the strings and element ID reference in the RDM unit tests.
    - Fixed RDM unit tests to work with new native context menu which used to be <select> elements.

--- devtools/client/responsive.html/test/browser/browser_touch_device.js
+++ devtools/client/responsive.html/test/browser/browser_touch_device.js
@@ -49,7 +49,7 @@ async function testDefaults(ui) {
   info("Test Defaults");
 
   await testTouchEventsOverride(ui, false);
-  testViewportDeviceSelectLabel(ui, "no device selected");
+  testViewportDeviceMenuLabel(ui, "Responsive");
 }
 
 async function testChangingDevice(ui) {
@@ -58,7 +58,7 @@ async function testChangingDevice(ui) {
   await selectDevice(ui, testDevice.name);
   await waitForViewportResizeTo(ui, testDevice.width, testDevice.height);
   await testTouchEventsOverride(ui, true);
-  testViewportDeviceSelectLabel(ui, testDevice.name);
+  testViewportDeviceMenuLabel(ui, testDevice.name);
 }
 
 async function testResizingViewport(ui, device, touch) {
@@ -74,7 +74,7 @@ async function testResizingViewport(ui, device, touch) {
     await deviceRemoved;
   }
   await testTouchEventsOverride(ui, touch);
-  testViewportDeviceSelectLabel(ui, "no device selected");
+  testViewportDeviceMenuLabel(ui, "Responsive");
 }
 
 async function testEnableTouchSimulation(ui) {
