35ca69ab6d1e15637bf04d1aa002d5a618a73be8
Cameron McCormack <cam@mcc.id.au>
2018-08-15 15:46:00 +1000

    Bug 1482782 - Part 1: Generate nsGkAtomList.h from a Python file. r=njn,hsivonen
    Reviewers: njn!, hsivonen!
    
    Tags: #secure-revision
    
    Bug #: 1482782
    
    Differential Revision: https://phabricator.services.mozilla.com/D3280

---
+++ xpcom/ds/Atom.py
@@ -0,0 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+class Atom():
+    def __init__(self, ident, string):
+        self.ident = ident
+        self.string = string
