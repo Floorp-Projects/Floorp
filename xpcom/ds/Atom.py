0b89d2391bad599b070ca33445a67d31a9ca20ee
Cameron McCormack <cam@mcc.id.au>
2018-08-15 15:52:42 +1000

    Bug 1483121 - Generate static atom hash in StaticAtoms.py. r=njn,emilio
    Summary: Depends On D3286
    
    Reviewers: njn!, emilio!
    
    Tags: #secure-revision
    
    Bug #: 1483121
    
    Differential Revision: https://phabricator.services.mozilla.com/D3295

--- xpcom/ds/Atom.py
+++ xpcom/ds/Atom.py
@@ -9,6 +9,7 @@ class Atom():
         self.string = string
         self.ty = ty
         self.atom_type = self.__class__.__name__
+        self.hash = hash_string(string)
 
 
 class PseudoElementAtom(Atom):
@@ -29,3 +30,25 @@ class NonInheritingAnonBoxAtom(AnonBoxAtom):
 class InheritingAnonBoxAtom(AnonBoxAtom):
     def __init__(self, ident, string):
         AnonBoxAtom.__init__(self, ident, string)
+
+
+GOLDEN_RATIO_U32 = 0x9E3779B9
+
+
+def rotate_left_5(value):
+    return ((value << 5) | (value >> 27)) & 0xFFFFFFFF
+
+
+def wrapping_multiply(x, y):
+    return (x * y) & 0xFFFFFFFF
+
+
+# Calculate the precomputed hash of the static atom. This is a port of
+# mozilla::HashString(const char16_t*), which is what we use for atomizing
+# strings. An assertion in nsAtomTable::RegisterStaticAtoms ensures that
+# the value we compute here matches what HashString() would produce.
+def hash_string(s):
+    h = 0
+    for c in s:
+        h = wrapping_multiply(GOLDEN_RATIO_U32, rotate_left_5(h) ^ ord(c))
+    return h
