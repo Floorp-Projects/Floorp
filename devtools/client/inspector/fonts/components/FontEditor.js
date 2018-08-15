a0f5dcc7fca85361cee8644a5355b226fb057591
Razvan Caliman <rcaliman@mozilla.com>
2018-08-15 12:11:17 +0000

    Bug 1474731 - Custom styles for select elements in font editor. r=gl
    MozReview-Commit-ID: GvDlrCdYfkL
    
    Bug 1474731 - Misc styling tweaks for font editor. r=gl
    
    MozReview-Commit-ID: IVQHbjrXtEl
    
    Bug 1474731 - Dark theme for font editor. r=gl
    
    MozReview-Commit-ID: 9dqTa6KXA1E
    
    Differential Revision: https://phabricator.services.mozilla.com/D3232

--- devtools/client/inspector/fonts/components/FontEditor.js
+++ devtools/client/inspector/fonts/components/FontEditor.js
@@ -58,7 +58,7 @@ class FontEditor extends PureComponent {
   }
 
   /**
-   * Get a container with the rendered FontPropertyValue components with editing controls
+   * Get an array of FontPropertyValue components with editing controls
    * for of the given variable font axes. If no axes were given, return null.
    * If an axis has a value in the fontEditor store (i.e.: it was declared in CSS or
    * it was changed using the font editor), use its value, otherwise use the font axis
@@ -69,33 +69,27 @@ class FontEditor extends PureComponent {
    * @param  {Object} editedAxes
    *         Object with axes and values edited by the user or predefined in the CSS
    *         declaration for font-variation-settings.
-   * @return {DOMNode|null}
+   * @return {Array|null}
    */
   renderAxes(fontAxes = [], editedAxes) {
     if (!fontAxes.length) {
       return null;
     }
 
-    const controls = fontAxes.map(axis => {
+    return fontAxes.map(axis => {
       return FontPropertyValue({
         key: axis.tag,
+        className: "font-control-axis",
+        label: axis.name,
         min: axis.minValue,
         max: axis.maxValue,
-        value: editedAxes[axis.tag] || axis.defaultValue,
-        step: this.getAxisStep(axis.minValue, axis.maxValue),
-        label: axis.name,
         name: axis.tag,
         onChange: this.props.onPropertyChange,
-        unit: null
+        step: this.getAxisStep(axis.minValue, axis.maxValue),
+        unit: null,
+        value: editedAxes[axis.tag] || axis.defaultValue,
       });
     });
-
-    return dom.div(
-      {
-        className: "font-axes-controls"
-      },
-      controls
-    );
   }
 
   renderFamilesNotUsed(familiesNotUsed = []) {
@@ -257,7 +251,7 @@ class FontEditor extends PureComponent {
     // Generate the dropdown.
     const instanceSelect = dom.select(
       {
-        className: "font-control-input",
+        className: "font-control-input font-value-select",
         onChange: (e) => {
           const instance = fontInstances.find(inst => e.target.value === inst.name);
           instance && this.props.onInstanceChange(instance.name, instance.values);
