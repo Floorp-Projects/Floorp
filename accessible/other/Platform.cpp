b72e23fda4d69c7c028e74bbab31f6ba58a68b11
Cosmin Sabou <csabou@mozilla.com>
2018-08-15 04:39:43 +0300

    Backed out changeset dad95d849302 (bug 1479591) for causing linux debug leaks and security issues.

--- accessible/other/Platform.cpp
+++ accessible/other/Platform.cpp
@@ -67,9 +67,4 @@ a11y::ProxyVirtualCursorChangeEvent(ProxyAccessible*, ProxyAccessible*,
                                     int32_t, int32_t, int16_t, int16_t, bool)
 {
 }
-
-void
-a11y::ProxyScrollingEvent(ProxyAccessible*, uint32_t, uint32_t, uint32_t, uint32_t)
-{
-}
 #endif
