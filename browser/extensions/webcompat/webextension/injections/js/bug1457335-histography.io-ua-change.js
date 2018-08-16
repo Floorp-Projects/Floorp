42bc719f0626833e6ad040f827f9af8d612d1ef6
Dennis Schubert <dschubert@mozilla.com>
2018-08-16 14:00:13 +0000

    Bug 1481395 - Import WebCompat GoFaster v2.0.1 to Desktop. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D2835
    
    --HG--
    extra : moz-landing-system : lando

---
+++ browser/extensions/webcompat/webextension/injections/js/bug1457335-histography.io-ua-change.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * Bug 1457335 - histography.io - Override UA & navigator.vendor
+ * WebCompat issue #1804 - https://webcompat.com/issues/1804
+ *
+ * This site is using a strict matching of navigator.userAgent and
+ * navigator.vendor to allow access for Safari or Chrome. Here, we set the
+ * values appropriately so we get recognized as Chrome.
+ */
+
+/* globals exportFunction */
+
+const CHROME_UA = navigator.userAgent + " Chrome for WebCompat";
+
+Object.defineProperty(window.navigator.wrappedJSObject, "userAgent", {
+  get: exportFunction(function() {
+    return CHROME_UA;
+  }, window),
+
+  set: exportFunction(function() {}, window)
+});
+
+Object.defineProperty(window.navigator.wrappedJSObject, "vendor", {
+  get: exportFunction(function() {
+    return "Google Inc.";
+  }, window),
+
+  set: exportFunction(function() {}, window)
+});
