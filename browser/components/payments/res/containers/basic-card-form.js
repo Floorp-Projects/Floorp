fa0ec6e22fd96da9455cf36d61245979f81fe569
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-08-18 00:49:17 +0300

    Backed out changeset 32e2d0ddc56a (bug 1477100) for build bustages on editDialog.css. CLOSED TREE

--- browser/components/payments/res/containers/basic-card-form.js
+++ browser/components/payments/res/containers/basic-card-form.js
@@ -188,9 +188,6 @@ export default class BasicCardForm extends PaymentStateSubscriberMixin(PaymentRe
         billingAddressSelect.value = Object.keys(addresses)[0];
       }
     }
-    // Need to recalculate the populated state since
-    // billingAddressSelect is updated after loadRecord.
-    this.formHandler.updatePopulatedState(billingAddressSelect);
 
     this.updateRequiredState();
     this.updateSaveButtonState();
