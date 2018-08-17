5f46b0ffb66c6ef6d907d7f6a073245ad0c7b981
Emilio Cobos Álvarez <emilio@crisal.io>
2018-08-17 09:19:47 +0000

    Bug 1483882 - Teach IDTracker about Shadow DOM. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D3533

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -4951,16 +4951,6 @@ nsIDocument::MozSetImageElement(const nsAString& aImageElementId,
   }
 }
 
-Element*
-nsIDocument::LookupImageElement(const nsAString& aId)
-{
-  if (aId.IsEmpty())
-    return nullptr;
-
-  nsIdentifierMapEntry* entry = mIdentifierMap.GetEntry(aId);
-  return entry ? entry->GetImageIdElement() : nullptr;
-}
-
 void
 nsIDocument::DispatchContentLoadedEvents()
 {
