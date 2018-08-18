a5ab0eb31bce3e91c4c910cd48f9c50eebde4468
Brian Hackett <bhackett1024@gmail.com>
2018-08-18 05:39:01 -1000

    Bug 1483319 Part 1 - Transmit console message arguments as debuggee values, r=jimb.
    --HG--
    extra : rebase_source : 543fc9485766c0cbfa8d187cec225088ab16c385

--- devtools/server/actors/replay/debugger.js
+++ devtools/server/actors/replay/debugger.js
@@ -162,7 +162,8 @@ ReplayDebugger.prototype = {
   },
 
   findAllConsoleMessages() {
-    return this._sendRequest({ type: "findConsoleMessages" });
+    const messages = this._sendRequest({ type: "findConsoleMessages" });
+    return messages.map(this._convertConsoleMessage.bind(this));
   },
 
   /////////////////////////////////////////////////////////
@@ -262,6 +263,21 @@ ReplayDebugger.prototype = {
   },
 
   /////////////////////////////////////////////////////////
+  // Console Message methods
+  /////////////////////////////////////////////////////////
+
+  _convertConsoleMessage(message) {
+    // Console API message arguments need conversion to debuggee values, but
+    // other contents of the message can be left alone.
+    if (message.messageType == "ConsoleAPI" && message.arguments) {
+      for (let i = 0; i < message.arguments.length; i++) {
+        message.arguments[i] = this._convertValue(message.arguments[i]);
+      }
+    }
+    return message;
+  },
+
+  /////////////////////////////////////////////////////////
   // Handlers
   /////////////////////////////////////////////////////////
 
@@ -306,7 +322,10 @@ ReplayDebugger.prototype = {
                                () => handler.call(this, this.getNewestFrame()));
   },
 
-  _getNewConsoleMessage() { return this._sendRequest({ type: "getNewConsoleMessage" }); },
+  _getNewConsoleMessage() {
+    const message = this._sendRequest({ type: "getNewConsoleMessage" });
+    return this._convertConsoleMessage(message);
+  },
 
   get onConsoleMessage() {
     return this._breakpointKindGetter("ConsoleMessage");
@@ -598,8 +617,8 @@ ReplayDebuggerObject.prototype = {
   asEnvironment: NYI,
   executeInGlobal: NYI,
   executeInGlobalWithBindings: NYI,
-  makeDebuggeeValue: NYI,
 
+  makeDebuggeeValue: NotAllowed,
   preventExtensions: NotAllowed,
   seal: NotAllowed,
   freeze: NotAllowed,
