85c848613f8871ead15f713e27afed5c4f002c8b
Jeff Walden <jwalden@mit.edu>
2018-08-20 17:11:32 -0500

    Bug 1484420 - Move locale-related functions into js/public/LocaleSensitive.h that isn't #include'd in jsapi.h.  r=anba

--- dom/workers/RuntimeService.cpp
+++ dom/workers/RuntimeService.cpp
@@ -29,6 +29,7 @@
 #include "mozilla/ipc/BackgroundChild.h"
 #include "GeckoProfiler.h"
 #include "jsfriendapi.h"
+#include "js/LocaleSensitive.h"
 #include "mozilla/AbstractThread.h"
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/AsyncEventDispatcher.h"
