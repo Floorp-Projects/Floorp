0302eb2a2da6c512b19db13cc5848c2c4e462f7c
Lars T Hansen <lhansen@mozilla.com>
2018-08-21 21:13:12 +0200

    Bug 1394420 - jit-generate atomic ops to be called from c++.  r=nbp, r=froydnj
    SpiderMonkey (and eventually DOM) will sometimes access shared memory
    from multiple threads without synchronization; this is a natural
    consequence of the JS memory model + JS/DOM specs.  We have always had
    a hardware-specific abstraction layer for these accesses, to isolate
    code from the details of how unsynchronized / racy access is handled.
    This layer has been written in C++ and has several problems:
    
    - In C++, racy access is undefined behavior, and the abstraction layer
      is therefore inherently unsafe, especially in the presence of
      inlining, PGO, and clever compilers.  (And TSAN will start
      complaining, too.)
    
    - Some of the compiler intrinsics that are used in the C++ abstraction
      layer are not actually the right primitives -- they assume C++, ie
      non-racy, semantics, and may not implement the correct barriers in
      all cases.
    
    - There are few guarantees that the synchronization implemented by the
      C++ primitives is actually compatible with the synchronization used
      by jitted code.
    
    - While x86 and ARM have 8-byte synchronized access (CMPXCHG8B and
      LDREXD/STREXD), some C++ compilers do not support their use well or
      at all, leading to occasional hardship for porting teams.
    
    This patch solves all these problems by jit-generating the racy access
    abstraction layer in the form of C++-compatible functions that: do not
    trigger UB in the C++ code; do not depend on possibly-incorrect
    intrinsics but instead always emit the proper barriers; are guaranteed
    to be JIT-compatible; and support x86 properly.
    
    Mostly this code is straightforward: each access function is a short,
    nearly prologue- and epilogue-less, sequence of instructions that
    performs a normal load or store or appropriately synchronized
    operation (CMPXCHG or similar).  Safe-for-races memcpy and memmove are
    trickier but are handled by combining some C++ code with several
    jit-generated functions that perform unrolled copies for various block
    sizes and alignments.
    
    The performance story is not completely satisfactory:
    
    On the one hand, we don't regress anything because copying
    unshared-to-unshared we do not use the new primitives but instead the
    C++ compiler's optimized memcpy and standard memory loads and stores.
    
    On the other hand, performance with shared memory is lower than
    performance with unshared memory.  TypedArray.prototype.set() is a
    good test case.  When the source and target arrays have the same type,
    the engine uses a memcpy; shared memory copying is 3x slower than
    unshared memory for 100,000 8K copies (Uint8).  However, when the
    source and target arrays are slightly different types (Uint8 vs Int8)
    the engine uses individual loads and stores, which for shared memory
    turns into two calls per byte being moved; in this case, shared memory
    is 127x slower than unshared memory.  (All numbers on x64 Linux.)
    
    Can we live with the very significant slowdown in the latter case?  It
    depends on the applications we envision for shared memory.  Primarily,
    shared memory will be used as wasm heap memory, in which case most
    applications that need to move data will use all Uint8Array arrays and
    the slowdown is OK.  But it is clearly a type of performance cliff.
    
    We can reduce the overhead by jit-generating more code, specifically
    code to perform the load, convert, and store in common cases.  More
    interestingly, and simpler, we can probably use memcpy in all cases by
    copying first (fairly fast) and then running a local fixup.  A bug
    should be filed for this but IMO we're OK with the current solution.
    
    (Memcpy can also be further sped up in platform-specific ways by
    generating cleverer code that uses REP MOVS or SIMD or similar.)
    
    --HG--
    extra : rebase_source : aada616ffa85501af6477a3e9867cfaa79f3b8e0

--- js/src/jit/arm/MacroAssembler-arm.cpp
+++ js/src/jit/arm/MacroAssembler-arm.cpp
@@ -5290,10 +5290,11 @@ void MacroAssembler::wasmAtomicLoad64(const wasm::MemoryAccessDesc& access,
 }
 
 template <typename T>
-static void WasmCompareExchange64(MacroAssembler& masm,
-                                  const wasm::MemoryAccessDesc& access,
-                                  const T& mem, Register64 expect,
-                                  Register64 replace, Register64 output) {
+static void CompareExchange64(MacroAssembler& masm,
+                              const wasm::MemoryAccessDesc* access,
+                              const Synchronization& sync, const T& mem,
+                              Register64 expect, Register64 replace,
+                              Register64 output) {
   MOZ_ASSERT(expect != replace && replace != output && output != expect);
 
   MOZ_ASSERT((replace.low.code() & 1) == 0);
@@ -5308,11 +5309,13 @@ static void WasmCompareExchange64(MacroAssembler& masm,
   SecondScratchRegisterScope scratch2(masm);
   Register ptr = ComputePointerForAtomic(masm, mem, scratch2);
 
-  masm.memoryBarrierBefore(access.sync());
+  masm.memoryBarrierBefore(sync);
 
   masm.bind(&again);
   BufferOffset load = masm.as_ldrexd(output.low, output.high, ptr);
-  masm.append(access, load.getOffset());
+  if (access) {
+    masm.append(*access, load.getOffset());
+  }
 
   masm.as_cmp(output.low, O2Reg(expect.low));
   masm.as_cmp(output.high, O2Reg(expect.high), MacroAssembler::Equal);
@@ -5326,7 +5329,7 @@ static void WasmCompareExchange64(MacroAssembler& masm,
   masm.as_b(&again, MacroAssembler::Equal);
   masm.bind(&done);
 
-  masm.memoryBarrierAfter(access.sync());
+  masm.memoryBarrierAfter(sync);
 }
 
 void MacroAssembler::wasmCompareExchange64(const wasm::MemoryAccessDesc& access,
@@ -5334,7 +5337,8 @@ void MacroAssembler::wasmCompareExchange64(const wasm::MemoryAccessDesc& access,
                                            Register64 expect,
                                            Register64 replace,
                                            Register64 output) {
-  WasmCompareExchange64(*this, access, mem, expect, replace, output);
+  CompareExchange64(*this, &access, access.sync(), mem, expect, replace,
+                    output);
 }
 
 void MacroAssembler::wasmCompareExchange64(const wasm::MemoryAccessDesc& access,
@@ -5342,7 +5346,14 @@ void MacroAssembler::wasmCompareExchange64(const wasm::MemoryAccessDesc& access,
                                            Register64 expect,
                                            Register64 replace,
                                            Register64 output) {
-  WasmCompareExchange64(*this, access, mem, expect, replace, output);
+  CompareExchange64(*this, &access, access.sync(), mem, expect, replace,
+                    output);
+}
+
+void MacroAssembler::compareExchange64(const Synchronization& sync,
+                                       const Address& mem, Register64 expect,
+                                       Register64 replace, Register64 output) {
+  CompareExchange64(*this, nullptr, sync, mem, expect, replace, output);
 }
 
 template <typename T>
