c44c15e0b47449cb55aa111a3271286ae5c01a54
Jan de Mooij <jdemooij@mozilla.com>
2018-08-21 12:11:52 +0200

    Bug 1466118 part 3 - Replace assertSameCompartmentDebugOnly with JSContext::debugOnlyCheck. r=luke

--- js/src/vm/JSContext.h
+++ js/src/vm/JSContext.h
@@ -956,6 +956,7 @@ struct JSContext : public JS::RootingContext,
     void removeUnhandledRejectedPromise(JSContext* cx, js::HandleObject promise);
 
     template <class... Args> inline void releaseCheck(const Args&... args);
+    template <class... Args> MOZ_ALWAYS_INLINE void debugOnlyCheck(const Args&... args);
 }; /* struct JSContext */
 
 inline JS::Result<>
