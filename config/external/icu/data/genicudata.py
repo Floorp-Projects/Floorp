53b770da19ffacc440bf2f6a7e6e0e8c45dc944f
Nathan Froyd <froydnj@mozilla.com>
2018-08-21 11:00:34 -0400

    Bug 1480558 - part 4 - add icudata support for aarch64 windows; r=mshal
    yasm doesn't support aarch64, and trying to use GNU as with an MSVC
    build seems like sadness waiting to happen.  Instead, we'll generate our
    own assembly file that armasm64 will accept.

---
+++ config/external/icu/data/genicudata.py
@@ -0,0 +1,14 @@
+# -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
+# vim: set filetype=python:
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+def main(output, data_file, data_symbol):
+    output.write('''    AREA .rdata,ALIGN=4,DATA,READONLY
+    EXPORT _{data_symbol}[DATA]
+_{data_symbol}
+    INCBIN {data_file}
+    END
+'''.format(data_file=data_file, data_symbol=data_symbol))
