71db5a64fdf5cd00c72a0a10c1b04f16fb1cd47a
Gabriel Luong <gabriel.luong@gmail.com>
2018-08-22 13:18:46 -0400

    Bug 1485115 - Format the React and Redux components in the Box Model and Grid Inspector. r=rcaliman

--- devtools/client/inspector/boxmodel/components/BoxModelEditable.js
+++ devtools/client/inspector/boxmodel/components/BoxModelEditable.js
@@ -18,9 +18,9 @@ class BoxModelEditable extends PureComponent {
       direction: PropTypes.string,
       focusable: PropTypes.bool.isRequired,
       level: PropTypes.string,
+      onShowBoxModelEditor: PropTypes.func.isRequired,
       property: PropTypes.string.isRequired,
       textContent: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-      onShowBoxModelEditor: PropTypes.func.isRequired,
     };
   }
 
@@ -49,23 +49,25 @@ class BoxModelEditable extends PureComponent {
                  box !== "position" &&
                  textContent.toString().length > LONG_TEXT_ROTATE_LIMIT;
 
-    return dom.p(
-      {
-        className: `boxmodel-${box}
-                    ${direction ? " boxmodel-" + direction : "boxmodel-" + property}
-                    ${rotate ? " boxmodel-rotate" : ""}`,
-      },
-      dom.span(
+    return (
+      dom.p(
         {
-          className: "boxmodel-editable",
-          "data-box": box,
-          tabIndex: box === level && focusable ? 0 : -1,
-          title: property,
-          ref: span => {
-            this.boxModelEditable = span;
-          },
+          className: `boxmodel-${box}
+                      ${direction ? " boxmodel-" + direction : "boxmodel-" + property}
+                      ${rotate ? " boxmodel-rotate" : ""}`,
         },
-        textContent
+        dom.span(
+          {
+            className: "boxmodel-editable",
+            "data-box": box,
+            tabIndex: box === level && focusable ? 0 : -1,
+            title: property,
+            ref: span => {
+              this.boxModelEditable = span;
+            },
+          },
+          textContent
+        )
       )
     );
   }
