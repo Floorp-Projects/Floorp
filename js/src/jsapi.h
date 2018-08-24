57a233fa2afebe7da3eff34ef16165d8cf487572
Jeff Walden <jwalden@mit.edu>
2018-08-24 14:00:35 -0500

    Bug 1486577 - Move JS::SourceBufferHolder into a new public js/SourceBufferHolder.h header.  r=jandem

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -38,6 +38,7 @@
 #include "js/Realm.h"
 #include "js/RefCounted.h"
 #include "js/RootingAPI.h"
+#include "js/SourceBufferHolder.h"
 #include "js/Stream.h"
 #include "js/TracingAPI.h"
 #include "js/UniquePtr.h"
@@ -300,114 +301,6 @@ JS_PUBLIC_API(bool)
 JS_StringHasBeenPinned(JSContext* cx, JSString* str);
 
 namespace JS {
-
-/**
- * Container class for passing in script source buffers to the JS engine.  This
- * not only groups the buffer and length values, it also provides a way to
- * optionally pass ownership of the buffer to the JS engine without copying.
- * Rules for use:
- *
- *  1) The data array must be allocated with js_malloc() or js_realloc() if
- *     ownership is being granted to the SourceBufferHolder.
- *  2) If ownership is not given to the SourceBufferHolder, then the memory
- *     must be kept alive until the JS compilation is complete.
- *  3) Any code calling SourceBufferHolder::take() must guarantee to keep the
- *     memory alive until JS compilation completes.  Normally only the JS
- *     engine should be calling take().
- *
- * Example use:
- *
- *    size_t length = 512;
- *    char16_t* chars = js_pod_malloc<char16_t>(length);
- *    JS::SourceBufferHolder srcBuf(chars, length, JS::SourceBufferHolder::GiveOwnership);
- *    JS::Compile(cx, options, srcBuf);
- */
-class SourceBufferHolder final
-{
-  public:
-    enum Ownership {
-      NoOwnership,
-      GiveOwnership
-    };
-
-    SourceBufferHolder(const char16_t* data, size_t dataLength, Ownership ownership)
-      : data_(data),
-        length_(dataLength),
-        ownsChars_(ownership == GiveOwnership)
-    {
-        fixEmptyBuffer();
-    }
-
-    SourceBufferHolder(UniqueTwoByteChars&& data, size_t dataLength)
-      : data_(data.release()),
-        length_(dataLength),
-        ownsChars_(true)
-    {
-        fixEmptyBuffer();
-    }
-
-    SourceBufferHolder(SourceBufferHolder&& other)
-      : data_(other.data_),
-        length_(other.length_),
-        ownsChars_(other.ownsChars_)
-    {
-        other.data_ = nullptr;
-        other.length_ = 0;
-        other.ownsChars_ = false;
-    }
-
-    ~SourceBufferHolder() {
-        if (ownsChars_)
-            js_free(const_cast<char16_t*>(data_));
-    }
-
-    // Access the underlying source buffer without affecting ownership.
-    const char16_t* get() const { return data_; }
-
-    // Length of the source buffer in char16_t code units (not bytes)
-    size_t length() const { return length_; }
-
-    // Returns true if the SourceBufferHolder owns the buffer and will free
-    // it upon destruction.  If true, it is legal to call take().
-    bool ownsChars() const { return ownsChars_; }
-
-    // Retrieve and take ownership of the underlying data buffer.  The caller
-    // is now responsible for calling js_free() on the returned value, *but only
-    // after JS script compilation has completed*.
-    //
-    // After the buffer has been taken the SourceBufferHolder functions as if
-    // it had been constructed on an unowned buffer;  get() and length() still
-    // work.  In order for this to be safe the taken buffer must be kept alive
-    // until after JS script compilation completes as noted above.
-    //
-    // Note, it's the caller's responsibility to check ownsChars() before taking
-    // the buffer.  Taking and then free'ing an unowned buffer will have dire
-    // consequences.
-    char16_t* take() {
-        MOZ_ASSERT(ownsChars_);
-        ownsChars_ = false;
-        return const_cast<char16_t*>(data_);
-    }
-
-  private:
-    SourceBufferHolder(SourceBufferHolder&) = delete;
-    SourceBufferHolder& operator=(SourceBufferHolder&) = delete;
-
-    void fixEmptyBuffer() {
-        // Ensure that null buffers properly return an unowned, empty,
-        // null-terminated string.
-        static const char16_t NullChar_ = 0;
-        if (!get()) {
-            data_ = &NullChar_;
-            length_ = 0;
-            ownsChars_ = false;
-        }
-    }
-
-    const char16_t* data_;
-    size_t length_;
-    bool ownsChars_;
-};
 
 struct TranscodeSource;
 
