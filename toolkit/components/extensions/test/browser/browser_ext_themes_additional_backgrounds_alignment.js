ca22c6e0b8b12a6f277f9f5043f58c279aa6d75c
Tim Nguyen <ntim.bugs@gmail.com>
2018-08-24 21:03:11 +0100

    Bug 1486018 - Make additional_backgrounds_alignment default to right top. r=dao

--- toolkit/components/extensions/test/browser/browser_ext_themes_additional_backgrounds_alignment.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_additional_backgrounds_alignment.js
@@ -3,9 +3,8 @@
 /* globals InspectorUtils */
 
 // Case 1 - When there is a headerURL image and additional_backgrounds_alignment is not specified.
-// So background-position should default to "left top"
+// So background-position should default to "right top"
 add_task(async function test_default_additional_backgrounds_alignment() {
-  const LEFT_TOP = "0% 0%";
   const RIGHT_TOP = "100% 0%";
 
   let extension = ExtensionTestUtils.loadExtension({
@@ -44,7 +43,7 @@ add_task(async function test_default_additional_backgrounds_alignment() {
 
   Assert.equal(
     toolboxCS.getPropertyValue("background-position"),
-    LEFT_TOP,
+    RIGHT_TOP,
     toolbox.id + " only contains default additional backgrounds alignment property"
   );
 
