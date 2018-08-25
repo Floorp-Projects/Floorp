d4ab8943928ff0bbaba24f7034233b374eb8c912
Ehsan Akhgari <ehsan@mozilla.com>
2018-08-25 02:48:50 -0400

    Bug 1486926 - Part 1: Make the Disable Protection button in the control centre UI work for the reject foreign cookie behavior; r=baku

--- netwerk/cookie/CookieServiceParent.cpp
+++ netwerk/cookie/CookieServiceParent.cpp
@@ -161,7 +161,10 @@ CookieServiceParent::TrackCookieLoad(nsIChannel *aChannel)
   nsCOMPtr<nsIHttpChannel> httpChannel = do_QueryInterface(aChannel);
   if (httpChannel) {
     isTrackingResource = httpChannel->GetIsTrackingResource();
-    if (isForeign && isTrackingResource &&
+    // Check first-party storage access even for non-tracking resources, since
+    // we will need the result when computing the access rights for the reject
+    // foreign cookie behavior mode.
+    if (isForeign &&
         AntiTrackingCommon::IsFirstPartyStorageAccessGrantedFor(httpChannel,
                                                                 uri)) {
       storageAccessGranted = true;
