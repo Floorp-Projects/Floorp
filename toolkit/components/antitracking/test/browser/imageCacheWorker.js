260179a08e2524ef5ec08286fb277bc0a17c8688
Ehsan Akhgari <ehsan@mozilla.com>
2018-08-25 01:59:28 -0400

    Bug 1486185 - Part 2: Ensure that anti-tracking tests are run in a mode where blocking is active but the top-level site is white-listed for content blocking; r=baku

--- toolkit/components/antitracking/test/browser/imageCacheWorker.js
+++ toolkit/components/antitracking/test/browser/imageCacheWorker.js
@@ -24,6 +24,7 @@ AntiTracking.runTest("Image cache - should load the image twice.",
     runExtraTests: false,
     blockingByCookieBehavior,
     blockingByContentBlocking,
+    blockingByAllowList,
     callback: async _ => {
       // Let's load the image twice here as well.
       let img = document.createElement("img");
