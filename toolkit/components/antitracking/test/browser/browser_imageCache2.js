3b1733c97bc8418cf295eba4f3fa96ebc981d968
arthur.iakab <aiakab@mozilla.com>
2018-08-27 17:25:31 +0300

    Backed out 2 changesets (bug 1486185)for build bustages on Logging.h CLOSED TREE
    Backed out changeset fc7618dd27c9 (bug 1486185)
    Backed out changeset 83388bace8ff (bug 1486185)

--- toolkit/components/antitracking/test/browser/browser_imageCache2.js
+++ toolkit/components/antitracking/test/browser/browser_imageCache2.js
@@ -2,7 +2,6 @@ ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 let blockingByCookieBehavior = false;
 let blockingByContentBlocking = true;
-let blockingByAllowList = false;
 
 let rootDir = getRootDirectory(gTestPath);
 let jar = getJar(rootDir);
