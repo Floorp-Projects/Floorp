59a56e84ec9cbef647494ac032d1b6ad61b8b80e
Andrew Halberstadt <ahalberstadt@mozilla.com>
2018-08-27 13:37:28 +0000

    Bug 1460856 - [mozlint] Display suppressed warnings count in the summary and stylish formatters r=Standard8
    Depends on D3821
    
    Differential Revision: https://phabricator.services.mozilla.com/D3992

---
+++ python/mozlint/mozlint/util/string.py
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, unicode_literals
+
+
+def pluralize(s, num):
+    if num != 1:
+        s += 's'
+    return str(num) + ' ' + s
