47b25bfccadab0fd955ef7065b740f899b751152
Xidorn Quan <me@upsuper.org>
2018-08-27 15:49:01 +0000

    Bug 1486329 - Fix some clang-cl warnings in accessible. r=eeejay
    Specifically this fixes pessimizing-move warnings and some others.
    
    Differential Revision: https://phabricator.services.mozilla.com/D4302
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/ipc/win/PlatformChild.cpp
+++ accessible/ipc/win/PlatformChild.cpp
@@ -70,14 +70,14 @@ PlatformChild::PlatformChild()
 
   UniquePtr<mozilla::mscom::RegisteredProxy> customProxy;
   mozilla::mscom::EnsureMTA([&customProxy]() -> void {
-    customProxy = std::move(mozilla::mscom::RegisterProxy());
+    customProxy = mozilla::mscom::RegisterProxy();
   });
   mCustomProxy = std::move(customProxy);
 
   // IA2 needs to be registered in both the main thread's STA as well as the MTA
   UniquePtr<mozilla::mscom::RegisteredProxy> ia2ProxyMTA;
   mozilla::mscom::EnsureMTA([&ia2ProxyMTA]() -> void {
-    ia2ProxyMTA = std::move(mozilla::mscom::RegisterProxy(L"ia2marshal.dll"));
+    ia2ProxyMTA = mozilla::mscom::RegisterProxy(L"ia2marshal.dll");
   });
   mIA2ProxyMTA = std::move(ia2ProxyMTA);
 }
