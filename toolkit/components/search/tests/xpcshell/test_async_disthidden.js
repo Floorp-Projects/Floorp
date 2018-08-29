4c346bdcb388a41a458e46c1824f15482388e679
Michael Kaply <mozilla@kaply.com>
2018-08-29 19:51:11 +0000

    Bug 1485508 - Properly construct MozParameterized search URL. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D4090

--- toolkit/components/search/tests/xpcshell/test_async_disthidden.js
+++ toolkit/components/search/tests/xpcshell/test_async_disthidden.js
@@ -28,9 +28,9 @@ add_task(async function test_disthidden() {
   Assert.ok(Services.search.isInitialized);
 
   let engines = Services.search.getEngines();
-  // From data/list.json - only 5 out of 6
+  // From data/list.json - only 6 out of 7
   // since one is hidden
-  Assert.equal(engines.length, 5);
+  Assert.equal(engines.length, 6);
 
   // Verify that the Test search engine is not available
   let engine = Services.search.getEngineByName("Test search engine");
