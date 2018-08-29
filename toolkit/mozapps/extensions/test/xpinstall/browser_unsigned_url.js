ad5ad924dcdfaedb7ab200878600469c7e4076e4
Jonathan Kingston <jkt@mozilla.com>
2018-08-29 16:08:55 +0100

    Bug 1485305 - toolkit/mozapps/extensions/ Ensure loadURI always passes a triggeringPrincipal() r=Mossop
    Differential Revision: https://phabricator.services.mozilla.com/D4561
    
    --HG--
    extra : source : 3fbb2f8e9aa84b8181d8684457ad0006fc21edc6

--- toolkit/mozapps/extensions/test/xpinstall/browser_unsigned_url.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_unsigned_url.js
@@ -8,7 +8,7 @@ function test() {
 
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, "about:blank");
   BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser).then(() => {
-    gBrowser.loadURI(TESTROOT + "unsigned.xpi");
+    BrowserTestUtils.loadURI(gBrowser, TESTROOT + "unsigned.xpi");
   });
 }
 
