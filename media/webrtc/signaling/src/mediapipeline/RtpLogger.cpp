51b2f38d606905dca3688af6f0a7fa98dba8fead
Dan Minor <dminor@mozilla.com>
2018-08-29 17:10:17 +0000

    Bug 1486817 - Use separate log modules for mediapipeline and rtplogger; r=bwc
    This restores the separate mediapipeline and RtpLogger lazy log modules that
    were unified with the Signaling log module in Bug 1402334.
    
    Differential Revision: https://phabricator.services.mozilla.com/D4478
    
    --HG--
    extra : moz-landing-system : lando

--- media/webrtc/signaling/src/mediapipeline/RtpLogger.cpp
+++ media/webrtc/signaling/src/mediapipeline/RtpLogger.cpp
@@ -6,8 +6,6 @@
 
 #include "RtpLogger.h"
 
-#include "CSFLog.h"
-
 #include <ctime>
 #include <iomanip>
 #include <sstream>
@@ -21,25 +19,21 @@
 // Logging context
 using namespace mozilla;
 
-static const char* rlLogTag = "RtpLogger";
-#ifdef LOGTAG
-#undef LOGTAG
-#endif
-#define LOGTAG rlLogTag
+mozilla::LazyLogModule gRtpLoggerLog("RtpLogger");
 
 namespace mozilla {
 
 bool RtpLogger::IsPacketLoggingOn() {
-  return CSFLogTestLevel(CSF_LOG_DEBUG);
+  return MOZ_LOG_TEST(gRtpLoggerLog, LogLevel::Debug);
 }
 
 void RtpLogger::LogPacket(const MediaPacket& packet, bool input,
                           std::string desc) {
-  if (CSFLogTestLevel(CSF_LOG_DEBUG)) {
+  if (MOZ_LOG_TEST(gRtpLoggerLog, LogLevel::Debug)) {
     bool isRtp = (packet.type() == MediaPacket::RTP);
     std::stringstream ss;
     /* This creates text2pcap compatible format, e.g.:
-     *  RTCP_PACKET O 10:36:26.864934  000000 80 c8 00 06 6d ... 
+     *  RTCP_PACKET O 10:36:26.864934  000000 80 c8 00 06 6d ...
      */
     ss << (input ? "I " : "O ");
     std::time_t t = std::time(nullptr);
@@ -63,10 +57,10 @@ void RtpLogger::LogPacket(const MediaPacket& packet, bool input,
     for (size_t i=0; i < packet.len(); ++i) {
       ss << " " << std::setw(2) << (int)packet.data()[i];
     }
-    CSFLogDebug(LOGTAG, "%s%s%s", desc.c_str(),
-                (isRtp ? " RTP_PACKET " : " RTCP_PACKET "), ss.str().c_str());
+    MOZ_LOG(gRtpLoggerLog, LogLevel::Debug,
+            ("%s%s%s", desc.c_str(),
+             (isRtp ? " RTP_PACKET " : " RTCP_PACKET "), ss.str().c_str()));
   }
 }
 
 }  // end of namespace
-
