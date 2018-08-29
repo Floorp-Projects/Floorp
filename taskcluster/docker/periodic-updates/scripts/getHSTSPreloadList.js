bcee61dda151b81722c7310898505fd410a66424
David Keeler <dkeeler@mozilla.com>
2018-08-29 12:04:16 +0000

    Bug 1479918 - Batch requests in getHSTSPreloadList.js to avoid livelock. r=sfraser
    Prior to this patch, getHSTSPreloadList.js would queue an XHR for every preload
    list candidate site. This meant that there would be ~50,000 requests in flight
    simultaneously. Simply processing these requests caused them to all time out,
    and no useful work was done. This patch resolves them in batches of 250 to
    avoid this issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D3622

--- taskcluster/docker/periodic-updates/scripts/getHSTSPreloadList.js
+++ taskcluster/docker/periodic-updates/scripts/getHSTSPreloadList.js
@@ -258,23 +258,29 @@ function spinResolve(promise) {
 }
 
 async function probeHSTSStatuses(inHosts) {
-  let promises = [];
-
-  dump("Examining " + inHosts.length + " hosts.\n");
+  let totalLength = inHosts.length;
+  dump("Examining " + totalLength + " hosts.\n");
 
   // Debug/testing on a small number of hosts
   // while (inHosts.length > 40000) {
 
+  // Make requests in batches of 250. Otherwise, we have too many in-flight
+  // requests and the time it takes to process them causes them all to time out.
+  let allResults = [];
   while (inHosts.length > 0) {
-    let host = inHosts.shift();
-    promises.push(getHSTSStatus(host));
+    let promises = [];
+    for (let i = 0; i < 250 && inHosts.length > 0; i++) {
+      let host = inHosts.shift();
+      promises.push(getHSTSStatus(host));
+    }
+    let results = await Promise.all(promises);
+    let progress = (100 * (totalLength - inHosts.length) / totalLength).toFixed(2);
+    dump(progress + "% done\n");
+    allResults = allResults.concat(results);
   }
 
-  dump("Waiting for " + promises.length + " responses.\n");
-
-  let result = await Promise.all(promises);
-  dump("HSTS Probe received " + result.length + " statuses.\n");
-  return result;
+  dump("HSTS Probe received " + allResults.length + " statuses.\n");
+  return allResults;
 }
 
 function readCurrentList(filename) {
