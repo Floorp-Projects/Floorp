c7c96baf753562cfac5ef130d5a57e228c371aab
Jonathan Kingston <jkt@mozilla.com>
2018-08-29 15:49:12 +0100

    Bug 1485305 - toolkit/ Ensure loadURI always passes a triggeringPrincipal() r=Mossop
    Differential Revision: https://phabricator.services.mozilla.com/D4563
    
    --HG--
    extra : source : 390737876a61c5da48a1957d284d3c315fbd541a
    extra : intermediate-source : f0b190c1cfb5d2df482f1051bbc0ff98de71235b

--- toolkit/components/places/tests/browser/browser_bug399606.js
+++ toolkit/components/places/tests/browser/browser_bug399606.js
@@ -31,7 +31,7 @@ add_task(async function() {
     expectedURI = uri;
     let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
     PlacesObservers.addListener(["page-visited"], onVisitsListener);
-    gBrowser.loadURI(uri);
+    BrowserTestUtils.loadURI(gBrowser, uri);
     await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false, uri);
     await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false, uri);
     await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, false, uri);
