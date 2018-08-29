2d349d59bd6e518ca8416e980694cf4393c7f024
Paul Adenot <paul@paul.cx>
2018-08-29 14:38:43 +0200

    Bug 1487057 - Part 1 - Move audio related classes in MediaEngineWebRTC.h to their own header files, clean up includes slightly. r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D5435
    
    --HG--
    extra : rebase_source : d0b5668aaf127a9456e9e12e1cc4f464a02559c9

--- dom/media/webrtc/MediaEngineWebRTCAudio.cpp
+++ dom/media/webrtc/MediaEngineWebRTCAudio.cpp
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "MediaEngineWebRTC.h"
+#include "MediaEngineWebRTCAudio.h"
 
 #include <stdio.h>
 #include <algorithm>
