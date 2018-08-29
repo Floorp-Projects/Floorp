051499a512878eb96653970067596cc7159c3f19
Jonathan Kingston <jkt@mozilla.com>
2018-08-29 15:43:27 +0100

    Bug 1485305 - browser/ Ensure loadURI always passes a triggeringPrincipal() r=Mossop
    Differential Revision: https://phabricator.services.mozilla.com/D4551

--- browser/components/preferences/in-content/sync.js
+++ browser/components/preferences/in-content/sync.js
@@ -358,7 +358,9 @@ var gSyncPane = {
     // Get the <browser> element hosting us.
     let browser = window.docShell.chromeEventHandler;
     // And tell it to load our URL.
-    browser.loadURI(url);
+    browser.loadURI(url, {
+      triggeringPrincipal: Services.scriptSecurityManager.createNullPrincipal({}),
+    });
   },
 
   async signIn() {
