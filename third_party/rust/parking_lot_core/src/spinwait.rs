65e7b0bbd167d9e5d67d7e5f0947862c11f6b073
Nathan Froyd <froydnj@mozilla.com>
2018-08-28 21:37:30 -0400

    Bug 1485409 - update crates to dispense with winapi 0.2.8 requirement; r=glandium
    winapi 0.2.8 is used by a number of crates in our dependency graph.  The
    newest version of winapi is 0.3.4, which is a significant restructuring
    and also more amenable to further development, e.g. adding AArch64
    support.  This patch does the easy work of updating as many things as
    possible to winapi 0.3.4 via a simple `cargo update`:
    
    cargo update -p atty:0.2.2 -p fs2 -p msdos_time -p parking_lot_core -p aho-corasick
    
    and then vendoring the results of those changes.

--- third_party/rust/parking_lot_core/src/spinwait.rs
+++ third_party/rust/parking_lot_core/src/spinwait.rs
@@ -5,24 +5,39 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-#[cfg(windows)]
-use kernel32;
 #[cfg(unix)]
 use libc;
+#[cfg(windows)]
+use winapi;
 #[cfg(not(any(windows, unix)))]
 use std::thread;
-#[cfg(not(feature = "nightly"))]
-use std::sync::atomic::{fence, Ordering};
+use std::sync::atomic::spin_loop_hint;
 
 // Yields the rest of the current timeslice to the OS
 #[cfg(windows)]
 #[inline]
 fn thread_yield() {
+    // Note that this is manually defined here rather than using the definition
+    // through `winapi`. The `winapi` definition comes from the `synchapi`
+    // header which enables the "synchronization.lib" library. It turns out,
+    // however that `Sleep` comes from `kernel32.dll` so this activation isn't
+    // necessary.
+    //
+    // This was originally identified in rust-lang/rust where on MinGW the
+    // libsynchronization.a library pulls in a dependency on a newer DLL not
+    // present in older versions of Windows. (see rust-lang/rust#49438)
+    //
+    // This is a bit of a hack for now and ideally we'd fix MinGW's own import
+    // libraries, but that'll probably take a lot longer than patching this here
+    // and avoiding the `synchapi` feature entirely.
+    extern "system" {
+        fn Sleep(a: winapi::shared::minwindef::DWORD);
+    }
     unsafe {
         // We don't use SwitchToThread here because it doesn't consider all
         // threads in the system and the thread we are waiting for may not get
         // selected.
-        kernel32::Sleep(0);
+        Sleep(0);
     }
 }
 #[cfg(unix)]
@@ -38,43 +53,12 @@ fn thread_yield() {
     thread::yield_now();
 }
 
-// Wastes some CPU time for the given number of iterations, preferably also
+// Wastes some CPU time for the given number of iterations,
 // using a hint to indicate to the CPU that we are spinning.
-#[cfg(all(feature = "nightly", any(target_arch = "x86", target_arch = "x86_64")))]
 #[inline]
 fn cpu_relax(iterations: u32) {
     for _ in 0..iterations {
-        unsafe {
-            asm!("pause" ::: "memory" : "volatile");
-        }
-    }
-}
-#[cfg(all(feature = "nightly", target_arch = "aarch64"))]
-#[inline]
-fn cpu_relax(iterations: u32) {
-    for _ in 0..iterations {
-        unsafe {
-            asm!("yield" ::: "memory" : "volatile");
-        }
-    }
-}
-#[cfg(all(feature = "nightly",
-          not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "aarch64"))))]
-#[inline]
-fn cpu_relax(iterations: u32) {
-    for _ in 0..iterations {
-        unsafe {
-            asm!("" ::: "memory" : "volatile");
-        }
-    }
-}
-#[cfg(not(feature = "nightly"))]
-#[inline]
-fn cpu_relax(iterations: u32) {
-    // This is a bit tricky: we rely on the fact that LLVM doesn't optimize
-    // atomic operations and effectively treats them as volatile.
-    for _ in 0..iterations {
-        fence(Ordering::SeqCst);
+        spin_loop_hint()
     }
 }
 
@@ -85,14 +69,6 @@ pub struct SpinWait {
 
 impl SpinWait {
     /// Creates a new `SpinWait`.
-    #[cfg(feature = "nightly")]
-    #[inline]
-    pub const fn new() -> SpinWait {
-        SpinWait { counter: 0 }
-    }
-
-    /// Creates a new `SpinWait`.
-    #[cfg(not(feature = "nightly"))]
     #[inline]
     pub fn new() -> SpinWait {
         SpinWait { counter: 0 }
