eb713176f9b238ed5cd2b8a05d84c213b24b2901
Jason Orendorff <jorendorff@mozilla.com>
2018-08-30 11:09:46 -0500

    Bug 1434391 - Fix OOM handling bug in Debugger (and eliminate an unnecessary wrap() call). r=jimb

---
+++ js/src/jit-test/tests/debug/bug1434391.js
@@ -0,0 +1,6 @@
+var g = newGlobal();
+var dbg = new Debugger();
+var gw = dbg.addDebuggee(g);
+oomTest(function () {
+    assertEq(gw.executeInGlobal("(42).toString(0)").throw.errorMessageName, "JSMSG_BAD_RADIX");
+});
