c23b4dc9ef33de064f75b39a311fba3f9949d9eb
Matt Howell <mhowell@mozilla.com>
2018-08-30 11:21:52 -0700

    Bug 1486376 Part 3 - Apply our existing patches to the LZMA SDK code. r=agashlin
    This simply applies the file other-licenses/7zstub/mozila_customizations.diff,
    which did not need to be changed in this patch series because it applies cleanly
    to the LZMA SDK code, which is organized in the same way as the 7-zip code.

--- other-licenses/7zstub/src/CPP/7zip/Bundles/SFXSetup/resource.h
+++ other-licenses/7zstub/src/CPP/7zip/Bundles/SFXSetup/resource.h
@@ -4,3 +4,5 @@
 #define IDS_EXTRACTION_ERROR_MESSAGE     8
 #define IDS_CANNOT_CREATE_FOLDER      3003
 #define IDS_PROGRESS_EXTRACTING       3300
+#define IDS_MIN_OS_TITLE                70
+#define IDS_MIN_OS_TEXT                 71
