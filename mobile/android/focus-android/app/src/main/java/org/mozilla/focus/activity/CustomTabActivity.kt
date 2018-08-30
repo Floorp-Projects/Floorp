cbcc730e0b0350835600afcefe72c7488c845a44
Emily Kager <ekager@mozilla.com>
2018-08-30 16:04:29 -0700

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/3248 Override Kotlin variables with getter

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/CustomTabActivity.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/CustomTabActivity.kt
@@ -19,7 +19,8 @@ class CustomTabActivity : MainActivity() {
         SessionManager.getInstance().getCustomTabSessionByCustomTabIdOrThrow(customTabId)
     }
 
-    override val isCustomTabMode: Boolean = true
+    override val isCustomTabMode: Boolean
+        get() = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,5 +44,6 @@ class CustomTabActivity : MainActivity() {
         }
     }
 
-    override val currentSessionForActivity = customTabSession
+    override val currentSessionForActivity
+        get() = customTabSession
 }
