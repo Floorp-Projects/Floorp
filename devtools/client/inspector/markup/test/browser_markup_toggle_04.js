0d812e2b6be8459dd0bfda708fbe232cdf15e131
Gabriel Luong <gabriel.luong@gmail.com>
2018-08-30 11:29:23 -0400

    Bug 1486801 - Clicking on the [...] should expand the markup container. r=jdescottes

---
+++ devtools/client/inspector/markup/test/browser_markup_toggle_04.js
@@ -0,0 +1,36 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test expanding elements by clicking on expand badge.
+
+const TEST_URL = URL_ROOT + "doc_markup_toggle.html";
+
+add_task(async function() {
+  const {inspector, testActor} = await openInspectorForURL(TEST_URL);
+
+  info("Getting the container for the UL parent element");
+  const container = await getContainerForSelector("ul", inspector);
+  ok(!container.mustExpand, "UL element !mustExpand");
+  ok(container.canExpand, "UL element canExpand");
+
+  info("Clicking on the UL parent expand badge, and waiting for children");
+  const onChildren = waitForChildrenUpdated(inspector);
+  const onUpdated = inspector.once("inspector-updated");
+
+  EventUtils.synthesizeMouseAtCenter(container.editor.expandBadge, {},
+    inspector.markup.doc.defaultView);
+  await onChildren;
+  await onUpdated;
+
+  info("Checking that child LI elements have been created");
+  const numLi = await testActor.getNumberOfElementMatches("li");
+  for (let i = 0; i < numLi; i++) {
+    const liContainer = await getContainerForSelector(
+      `li:nth-child(${i + 1})`, inspector);
+    ok(liContainer, "A container for the child LI element was created");
+  }
+  ok(container.expanded, "Parent UL container is expanded");
+});
