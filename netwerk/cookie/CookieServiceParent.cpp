40a8c122d38a47d547d1581bb7f4b86ae041a6be
Andrea Marchesini <amarchesini@mozilla.com>
2018-08-31 11:02:33 +0200

    Bug 1487331 - STATE_BLOCKED_TRACKING_COOKIES must tell why cookies are blocked, r=ehsan
    Introduce these new blocking state values:
    
    const unsigned long STATE_COOKIES_BLOCKED_BY_PERMISSION = 0x10000000;
    const unsigned long STATE_COOKIES_BLOCKED_TRACKER       = 0x20000000;
    const unsigned long STATE_COOKIES_BLOCKED_ALL           = 0x40000000;
    const unsigned long STATE_COOKIES_BLOCKED_FOREIGN       = 0x80000000;

--- netwerk/cookie/CookieServiceParent.cpp
+++ netwerk/cookie/CookieServiceParent.cpp
@@ -166,7 +166,8 @@ CookieServiceParent::TrackCookieLoad(nsIChannel *aChannel)
     // foreign cookie behavior mode.
     if (isForeign &&
         AntiTrackingCommon::IsFirstPartyStorageAccessGrantedFor(httpChannel,
-                                                                uri)) {
+                                                                uri,
+                                                                nullptr)) {
       storageAccessGranted = true;
     }
   }
