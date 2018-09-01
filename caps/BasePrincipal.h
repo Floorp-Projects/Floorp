60e0469efcdc69fb4a575c6047f898645296f22f
Nika Layzell <nika@thelayzells.com>
2018-09-01 00:52:00 +0000

    Bug 1485177 - Add |siteOrigin| information to nsIPrincipal, r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D4140

--- caps/BasePrincipal.h
+++ caps/BasePrincipal.h
@@ -86,6 +86,7 @@ public:
   NS_IMETHOD GetIsInIsolatedMozBrowserElement(bool* aIsInIsolatedMozBrowserElement) final;
   NS_IMETHOD GetUserContextId(uint32_t* aUserContextId) final;
   NS_IMETHOD GetPrivateBrowsingId(uint32_t* aPrivateBrowsingId) final;
+  NS_IMETHOD GetSiteOrigin(nsACString& aOrigin) override;
 
   virtual bool AddonHasPermission(const nsAtom* aPerm);
 
