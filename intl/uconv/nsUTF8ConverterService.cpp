8b7e752a1f1319a5c485f119dc55ff9a811e2b47
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-03 10:36:30 -0400

    Bug 1488115 - Part 3: Move nsIUTF8ConverterService::ConvertStringToUTF8() to nsMIMEHeaderParamImpl.cpp since that is the only consumer site; r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D4874

--- intl/uconv/nsUTF8ConverterService.cpp
+++ intl/uconv/nsUTF8ConverterService.cpp
@@ -6,69 +6,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "nsString.h"
 #include "nsUTF8ConverterService.h"
-#include "nsEscape.h"
-#include "mozilla/Encoding.h"
 
 using namespace mozilla;
 
 NS_IMPL_ISUPPORTS(nsUTF8ConverterService, nsIUTF8ConverterService)
-
-static nsresult
-ToUTF8(const nsACString& aString,
-       const char* aCharset,
-       bool aAllowSubstitution,
-       nsACString& aResult)
-{
-  if (!aCharset || !*aCharset)
-    return NS_ERROR_INVALID_ARG;
-
-  auto encoding = Encoding::ForLabelNoReplacement(MakeStringSpan(aCharset));
-  if (!encoding) {
-    return NS_ERROR_UCONV_NOCONV;
-  }
-  if (aAllowSubstitution) {
-    nsresult rv = encoding->DecodeWithoutBOMHandling(aString, aResult);
-    if (NS_SUCCEEDED(rv)) {
-      return NS_OK;
-    }
-    return rv;
-  }
-  return encoding->DecodeWithoutBOMHandlingAndWithoutReplacement(aString,
-                                                                 aResult);
-}
-
-NS_IMETHODIMP
-nsUTF8ConverterService::ConvertStringToUTF8(const nsACString& aString,
-                                            const char* aCharset,
-                                            bool aSkipCheck,
-                                            bool aAllowSubstitution,
-                                            uint8_t aOptionalArgc,
-                                            nsACString& aUTF8String)
-{
-  bool allowSubstitution = (aOptionalArgc == 1) ? aAllowSubstitution : true;
-
-  // return if ASCII only or valid UTF-8 providing that the ASCII/UTF-8
-  // check is requested. It may not be asked for if a caller suspects
-  // that the input is in non-ASCII 7bit charset (ISO-2022-xx, HZ) or
-  // it's in a charset other than UTF-8 that can be mistaken for UTF-8.
-  if (!aSkipCheck && (IsASCII(aString) || IsUTF8(aString))) {
-    aUTF8String = aString;
-    return NS_OK;
-  }
-
-  aUTF8String.Truncate();
-
-  nsresult rv = ToUTF8(aString, aCharset, allowSubstitution, aUTF8String);
-
-  // additional protection for cases where check is skipped and  the input
-  // is actually in UTF-8 as opposed to aCharset. (i.e. caller's hunch
-  // was wrong.) We don't check ASCIIness assuming there's no charset
-  // incompatible with ASCII (we don't support EBCDIC).
-  if (aSkipCheck && NS_FAILED(rv) && IsUTF8(aString)) {
-    aUTF8String = aString;
-    return NS_OK;
-  }
-
-  return rv;
-}
 
