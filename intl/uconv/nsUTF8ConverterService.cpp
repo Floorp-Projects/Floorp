1a28ba6ef62cb40b6653e3e5843bf84f17da5e69
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-03 10:22:43 -0400

    Bug 1488115 - Part 1: Remove nsIUTF8ConverterService::ConvertURISpecToUTF8() since it is unused; r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D4872

--- intl/uconv/nsUTF8ConverterService.cpp
+++ intl/uconv/nsUTF8ConverterService.cpp
@@ -72,38 +72,3 @@ nsUTF8ConverterService::ConvertStringToUTF8(const nsACString& aString,
   return rv;
 }
 
-NS_IMETHODIMP
-nsUTF8ConverterService::ConvertURISpecToUTF8(const nsACString& aSpec,
-                                             const char* aCharset,
-                                             nsACString& aUTF8Spec)
-{
-  // assume UTF-8 if the spec contains unescaped non-ASCII characters.
-  // No valid spec in Mozilla would break this assumption.
-  if (!IsASCII(aSpec)) {
-    aUTF8Spec = aSpec;
-    return NS_OK;
-  }
-
-  aUTF8Spec.Truncate();
-
-  nsAutoCString unescapedSpec;
-  // NS_UnescapeURL does not fill up unescapedSpec unless there's at least
-  // one character to unescape.
-  bool written = NS_UnescapeURL(PromiseFlatCString(aSpec).get(),
-                                aSpec.Length(),
-                                esc_OnlyNonASCII,
-                                unescapedSpec);
-
-  if (!written) {
-    aUTF8Spec = aSpec;
-    return NS_OK;
-  }
-  // return if ASCII only or escaped UTF-8
-  if (IsASCII(unescapedSpec) || IsUTF8(unescapedSpec)) {
-    aUTF8Spec = unescapedSpec;
-    return NS_OK;
-  }
-
-  return ToUTF8(unescapedSpec, aCharset, true, aUTF8Spec);
-}
-
