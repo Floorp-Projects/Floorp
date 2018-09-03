4ec0a2aa8b240aabd0a64fbd0f5af5f6a15d031e
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-03 11:16:56 -0400

    Bug 1488238 - Remove the XPCOM component registration for nsApplicationCache; r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D4878

--- netwerk/cache/nsApplicationCache.h
+++ netwerk/cache/nsApplicationCache.h
@@ -3,6 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "nsIApplicationCache.h"
+#include "nsWeakReference.h"
+#include "mozilla/RefPtr.h"
+#include "nsString.h"
+
+class nsOfflineCacheDevice;
+
 class nsApplicationCache : public nsIApplicationCache
                          , public nsSupportsWeakReference
 {
