ae8df55a5eb9568c9cf09954d2d55bdc92f1d12d
Lars T Hansen <lhansen@mozilla.com>
2018-09-03 20:02:38 +0200

    Bug 1487329 - Select wasm baseline compiler if content opts into gc types. r=bbouvier
    Creates a level of indirection to encapsulate the compilation
    parameters (mode, tier, debug, gc-enabled) so as to allow their
    computation to be delayed.  This centers on the new struct
    CompilerEnvironment, defined in WasmValidate.h.
    
    After this change, compiler selection is driven by the presence of the
    gc-feature-opt-in section.  We finalize the values in
    CompilerEnvironment after having parsed that section.  (That parsing
    is still under #ifdef.)
    
    --wasm-gc is still used as a higher-level control; if it is not
    present there will be no gc support at all.  But once we remove that
    flag, very little will change here; all code that reads that flag can
    instead pass HasGcTypes::True, and compiler selection will be entirely
    driven by the presence of the opt-in section.
    
    There are a few too many uses of HasGcTypes::False here; most of these
    will disappear along with the --wasm-gc flag.
    
    --HG--
    extra : rebase_source : cbbc09dc7b1ae7777b66c8ba4b1c39c3e4195c46

--- js/src/wasm/WasmBaselineCompile.cpp
+++ js/src/wasm/WasmBaselineCompile.cpp
@@ -3382,7 +3382,7 @@ class BaseCompiler final : public BaseCompilerInterface
 
         GenerateFunctionPrologue(masm,
                                  env_.funcTypes[func_.index]->id,
-                                 env_.mode == CompileMode::Tier1 ? Some(func_.index) : Nothing(),
+                                 env_.mode() == CompileMode::Tier1 ? Some(func_.index) : Nothing(),
                                  &offsets_);
 
         // Initialize DebugFrame fields before the stack overflow trap so that
@@ -10352,7 +10352,7 @@ js::wasm::BaselineCompileFunctions(const ModuleEnvironment& env, LifoAlloc& lifo
                                    const FuncCompileInputVector& inputs, CompiledCode* code,
                                    UniqueChars* error)
 {
-    MOZ_ASSERT(env.tier == Tier::Baseline);
+    MOZ_ASSERT(env.tier() == Tier::Baseline);
     MOZ_ASSERT(env.kind == ModuleKind::Wasm);
 
     // The MacroAssembler will sometimes access the jitContext.
