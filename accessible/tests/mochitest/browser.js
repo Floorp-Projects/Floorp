096a9c36b0ca98ca89c69c19d47d402737e3f5bc
Zibi Braniecki <zbraniecki@mozilla.com>
2018-09-03 21:59:56 +0100

    Bug 1488286 - Use a dummy data: URI as a default URL for accessibility tests. r=mconley
    --HG--
    extra : rebase_source : e4f39a4c2db74b54ca3c378b87e9b366d555807a

--- accessible/tests/mochitest/browser.js
+++ accessible/tests/mochitest/browser.js
@@ -117,7 +117,7 @@ function openBrowserWindowIntl() {
   gBrowserContext.browserWnd =
     window.openDialog(AppConstants.BROWSER_CHROME_URL,
                       "_blank", params,
-                      gBrowserContext.startURL);
+                      gBrowserContext.startURL || "data:text/html,<html></html>");
 
   whenDelayedStartupFinished(browserWindow(), function() {
     addA11yLoadEvent(startBrowserTests, browserWindow());
