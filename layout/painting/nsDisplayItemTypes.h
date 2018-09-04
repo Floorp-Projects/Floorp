dd36e216528a78ae59b86a1c4d5991b79ef33456
Miko Mynttinen <mikokm@gmail.com>
2018-09-04 20:46:21 +0000

    Bug 1484966 - Part 3: Clang format layout/painting/ r=mattwoodrow
    Depends on D3870
    
    Differential Revision: https://phabricator.services.mozilla.com/D3871

--- layout/painting/nsDisplayItemTypes.h
+++ layout/painting/nsDisplayItemTypes.h
@@ -8,60 +8,72 @@
 
 /**
  * It's useful to be able to dynamically check the type of certain items.
- * Every subclass of nsDisplayItem must have a new type added here for the purposes
- * of easy comparison and matching of items in different display lists.
+ * Every subclass of nsDisplayItem must have a new type added here for the
+ * purposes of easy comparison and matching of items in different display lists.
  */
 
 #ifndef NSDISPLAYITEMTYPES_H_
 #define NSDISPLAYITEMTYPES_H_
 
-enum class DisplayItemType {
+enum class DisplayItemType
+{
   TYPE_ZERO = 0, /** Spacer so that the first item starts at 1 */
 
-#define DECLARE_DISPLAY_ITEM_TYPE(name,flags) TYPE_##name,
+#define DECLARE_DISPLAY_ITEM_TYPE(name, flags) TYPE_##name,
 #include "nsDisplayItemTypesList.h"
 #undef DECLARE_DISPLAY_ITEM_TYPE
 
   TYPE_MAX
 };
 
-enum {
+enum
+{
   // Number of bits needed to represent all types
   TYPE_BITS = 8
 };
 
-enum DisplayItemFlags {
+enum DisplayItemFlags
+{
   TYPE_RENDERS_NO_IMAGES = 1 << 0
 };
 
-inline const char* DisplayItemTypeName(DisplayItemType aType)
+inline const char*
+DisplayItemTypeName(DisplayItemType aType)
 {
   switch (aType) {
-#define DECLARE_DISPLAY_ITEM_TYPE(name,flags) case DisplayItemType::TYPE_##name: return #name;
+#define DECLARE_DISPLAY_ITEM_TYPE(name, flags)                                 \
+  case DisplayItemType::TYPE_##name:                                           \
+    return #name;
 #include "nsDisplayItemTypesList.h"
 #undef DECLARE_DISPLAY_ITEM_TYPE
 
-    default: return "TYPE_UNKNOWN";
+    default:
+      return "TYPE_UNKNOWN";
   }
 }
 
-inline uint8_t GetDisplayItemFlagsForType(DisplayItemType aType)
+inline uint8_t
+GetDisplayItemFlagsForType(DisplayItemType aType)
 {
-  static const uint8_t flags[static_cast<uint32_t>(DisplayItemType::TYPE_MAX)] = {
-    0
-#define DECLARE_DISPLAY_ITEM_TYPE(name,flags) ,flags
+  static const uint8_t flags[static_cast<uint32_t>(DisplayItemType::TYPE_MAX)] =
+    { 0
+#define DECLARE_DISPLAY_ITEM_TYPE(name, flags) , flags
 #include "nsDisplayItemTypesList.h"
 #undef DECLARE_DISPLAY_ITEM_TYPE
-  };
+    };
 
   return flags[static_cast<uint32_t>(aType)];
 }
 
-inline DisplayItemType GetDisplayItemTypeFromKey(uint32_t aDisplayItemKey)
+inline DisplayItemType
+GetDisplayItemTypeFromKey(uint32_t aDisplayItemKey)
 {
   static const uint32_t typeMask = (1 << TYPE_BITS) - 1;
-  DisplayItemType type = static_cast<DisplayItemType>(aDisplayItemKey & typeMask);
-  NS_ASSERTION(type >= DisplayItemType::TYPE_ZERO && type < DisplayItemType::TYPE_MAX, "Invalid display item type!");
+  DisplayItemType type =
+    static_cast<DisplayItemType>(aDisplayItemKey & typeMask);
+  NS_ASSERTION(type >= DisplayItemType::TYPE_ZERO &&
+                 type < DisplayItemType::TYPE_MAX,
+               "Invalid display item type!");
   return type;
 }
 
--- layout/painting/nsDisplayListInvalidation.h
+++ layout/painting/nsDisplayListInvalidation.h
@@ -58,15 +58,9 @@ public:
    *
    * @param aOffset Offset to shift by.
    */
-  virtual void MoveBy(const nsPoint& aOffset)
-  {
-    mBounds.MoveBy(aOffset);
-  }
+  virtual void MoveBy(const nsPoint& aOffset) { mBounds.MoveBy(aOffset); }
 
-  virtual bool InvalidateForSyncDecodeImages() const
-  {
-    return false;
-  }
+  virtual bool InvalidateForSyncDecodeImages() const { return false; }
 
   /**
    * Bounds of the display item
@@ -83,14 +77,16 @@ public:
 class nsDisplayItemGenericGeometry : public nsDisplayItemGeometry
 {
 public:
-  nsDisplayItemGenericGeometry(nsDisplayItem* aItem, nsDisplayListBuilder* aBuilder);
+  nsDisplayItemGenericGeometry(nsDisplayItem* aItem,
+                               nsDisplayListBuilder* aBuilder);
 
   void MoveBy(const nsPoint& aOffset) override;
 
   nsRect mBorderRect;
 };
 
-bool ShouldSyncDecodeImages(nsDisplayListBuilder* aBuilder);
+bool
+ShouldSyncDecodeImages(nsDisplayListBuilder* aBuilder);
 
 /**
  * nsImageGeometryMixin is a mixin for geometry items that draw images.
@@ -101,7 +97,7 @@ bool ShouldSyncDecodeImages(nsDisplayListBuilder* aBuilder);
  * that is inheriting from it. See nsDisplayItemGenericImageGeometry for an
  * example.
  */
-template <typename T>
+template<typename T>
 class nsImageGeometryMixin
 {
 public:
@@ -176,7 +172,8 @@ public:
                                     nsDisplayListBuilder* aBuilder)
     : nsDisplayItemGenericGeometry(aItem, aBuilder)
     , nsImageGeometryMixin(aItem, aBuilder)
-  { }
+  {
+  }
 
   bool InvalidateForSyncDecodeImages() const override
   {
@@ -187,7 +184,8 @@ public:
 class nsDisplayItemBoundsGeometry : public nsDisplayItemGeometry
 {
 public:
-  nsDisplayItemBoundsGeometry(nsDisplayItem* aItem, nsDisplayListBuilder* aBuilder);
+  nsDisplayItemBoundsGeometry(nsDisplayItem* aItem,
+                              nsDisplayListBuilder* aBuilder);
 
   bool mHasRoundedCorners;
 };
@@ -210,7 +208,8 @@ class nsDisplayBackgroundGeometry
   , public nsImageGeometryMixin<nsDisplayBackgroundGeometry>
 {
 public:
-  nsDisplayBackgroundGeometry(nsDisplayBackgroundImage* aItem, nsDisplayListBuilder* aBuilder);
+  nsDisplayBackgroundGeometry(nsDisplayBackgroundImage* aItem,
+                              nsDisplayListBuilder* aBuilder);
 
   void MoveBy(const nsPoint& aOffset) override;
 
@@ -226,7 +225,8 @@ public:
 class nsDisplayThemedBackgroundGeometry : public nsDisplayItemGeometry
 {
 public:
-  nsDisplayThemedBackgroundGeometry(nsDisplayThemedBackground* aItem, nsDisplayListBuilder* aBuilder);
+  nsDisplayThemedBackgroundGeometry(nsDisplayThemedBackground* aItem,
+                                    nsDisplayListBuilder* aBuilder);
 
   void MoveBy(const nsPoint& aOffset) override;
 
@@ -237,7 +237,8 @@ public:
 class nsDisplayBoxShadowInnerGeometry : public nsDisplayItemGeometry
 {
 public:
-  nsDisplayBoxShadowInnerGeometry(nsDisplayItem* aItem, nsDisplayListBuilder* aBuilder);
+  nsDisplayBoxShadowInnerGeometry(nsDisplayItem* aItem,
+                                  nsDisplayListBuilder* aBuilder);
 
   void MoveBy(const nsPoint& aOffset) override;
 
@@ -262,7 +263,8 @@ public:
                               nscolor aColor)
     : nsDisplayItemBoundsGeometry(aItem, aBuilder)
     , mColor(aColor)
-  { }
+  {
+  }
 
   nscolor mColor;
 };
@@ -277,7 +279,8 @@ public:
     : nsDisplayItemBoundsGeometry(aItem, aBuilder)
     , mRegion(aRegion)
     , mColor(aColor)
-  { }
+  {
+  }
 
   void MoveBy(const nsPoint& aOffset) override;
 
@@ -298,7 +301,8 @@ public:
   nsPoint mFrameOffsetToReferenceFrame;
 };
 
-class nsDisplayMaskGeometry : public nsDisplaySVGEffectGeometry
+class nsDisplayMaskGeometry
+  : public nsDisplaySVGEffectGeometry
   , public nsImageGeometryMixin<nsDisplayMaskGeometry>
 {
 public:
@@ -314,7 +318,8 @@ public:
   bool mHandleOpacity;
 };
 
-class nsDisplayFilterGeometry : public nsDisplaySVGEffectGeometry
+class nsDisplayFilterGeometry
+  : public nsDisplaySVGEffectGeometry
   , public nsImageGeometryMixin<nsDisplayFilterGeometry>
 {
 public:
@@ -362,7 +367,8 @@ public:
                            float aOpacity)
     : nsDisplayItemGenericGeometry(aItem, aBuilder)
     , mOpacity(aOpacity)
-  {}
+  {
+  }
 
   float mOpacity;
 };
@@ -377,14 +383,16 @@ public:
     : nsDisplayItemGeometry(aItem, aBuilder)
     , mTransform(aTransform)
     , mAppUnitsPerDevPixel(aAppUnitsPerDevPixel)
-  {}
+  {
+  }
 
   void MoveBy(const nsPoint& aOffset) override
   {
     nsDisplayItemGeometry::MoveBy(aOffset);
     mTransform.PostTranslate(
       NSAppUnitsToFloatPixels(aOffset.x, mAppUnitsPerDevPixel),
-      NSAppUnitsToFloatPixels(aOffset.y, mAppUnitsPerDevPixel), 0.0f);
+      NSAppUnitsToFloatPixels(aOffset.y, mAppUnitsPerDevPixel),
+      0.0f);
   }
 
   mozilla::gfx::Matrix4x4Flagged mTransform;
