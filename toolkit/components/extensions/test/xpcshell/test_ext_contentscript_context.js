d7e758ca6e5800260e72358e3856235477f7f19b
Rob Wu <rob@robwu.nl>
2018-09-04 13:17:10 +0000

    Bug 1462121 - Improve reliability of context.contentWindow/active r=zombie
    - Register pagehide/pageshow events in the system group and ignore
      synthetic events to avoid interference from web pages.
    - Remove unused docShell member.
    - Fix memory leak in bfcached windows, by ensuring that BaseContext
      instances can be GC'd when an extension is unloaded, even if the
      context is associated with a page in the bfcache.
    - Ensure that context.contentWindow and context.active always have an
      accurate value.
    
    The latter is achieved by moving all contentWindow tracking logic in a
    new helper class "InnerWindowReference".
    
    Differential Revision: https://phabricator.services.mozilla.com/D4072
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/extensions/test/xpcshell/test_ext_contentscript_context.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_contentscript_context.js
@@ -10,7 +10,7 @@ server.registerPathHandler("/dummy", (request, response) => {
   response.write("<!DOCTYPE html><html></html>");
 });
 
-add_task(async function test_contentscript_context() {
+function loadExtension() {
   function contentScript() {
     browser.test.sendMessage("content-script-ready");
 
@@ -22,10 +22,10 @@ add_task(async function test_contentscript_context() {
     });
   }
 
-  let extension = ExtensionTestUtils.loadExtension({
+  return ExtensionTestUtils.loadExtension({
     manifest: {
       content_scripts: [{
-        "matches": ["http://example.com/dummy"],
+        "matches": ["http://example.com/dummy*"],
         "js": ["content_script.js"],
         "run_at": "document_start",
       }],
@@ -35,7 +35,10 @@ add_task(async function test_contentscript_context() {
       "content_script.js": contentScript,
     },
   });
+}
 
+add_task(async function test_contentscript_context() {
+  let extension = loadExtension();
   await extension.startup();
 
   let contentPage = await ExtensionTestUtils.loadContentPage("http://example.com/dummy");
@@ -73,5 +76,139 @@ add_task(async function test_contentscript_context() {
 
   await contentPage.close();
   await extension.awaitMessage("content-script-hide");
+  await extension.unload();
+});
+
+add_task(async function test_contentscript_context_unload_while_in_bfcache() {
+  let contentPage = await ExtensionTestUtils.loadContentPage("http://example.com/dummy?first");
+  let extension = loadExtension();
+  await extension.startup();
+  await extension.awaitMessage("content-script-ready");
+
+  // Get the content script context and check that it points to the correct window.
+  await contentPage.spawn(extension.id, async extensionId => {
+    let {DocumentManager} = ChromeUtils.import("resource://gre/modules/ExtensionContent.jsm", {});
+    // Save context so we can verify that contentWindow is nulled after unload.
+    this.context = DocumentManager.getContext(extensionId, this.content);
+
+    Assert.equal(this.context.contentWindow, this.content, "Context's contentWindow property is correct");
+
+    this.contextUnloadedPromise = new Promise(resolve => {
+      this.context.callOnClose({close: resolve});
+    });
+    this.pageshownPromise = new Promise(resolve => {
+      this.content.addEventListener("pageshow", () => {
+        // Yield to the event loop once more to ensure that all pageshow event
+        // handlers have been dispatched before fulfilling the promise.
+        let {setTimeout} = ChromeUtils.import("resource://gre/modules/Timer.jsm", {});
+        setTimeout(resolve, 0);
+      }, {once: true, mozSystemGroup: true});
+    });
+
+    // Navigate so that the content page is hidden in the bfcache.
+    this.content.location = "http://example.org/dummy?second";
+  });
+
+  await extension.awaitMessage("content-script-hide");
+
+  await extension.unload();
+  await contentPage.spawn(null, async () => {
+    await this.contextUnloadedPromise;
+    Assert.equal(this.context.unloaded, true, "Context has been unloaded");
+
+    // Normally, when a page is not in the bfcache, context.contentWindow is
+    // not null when the callOnClose handler is invoked (this is checked by the
+    // previous subtest).
+    // Now wait a little bit and check again to ensure that the contentWindow
+    // property is not somehow restored.
+    await new Promise(resolve => this.content.setTimeout(resolve, 0));
+    Assert.equal(this.context.contentWindow, null, "Context's contentWindow property is null");
+
+    // Navigate back so the content page is resurrected from the bfcache.
+    this.content.history.back();
+
+    await this.pageshownPromise;
+
+    Assert.equal(this.context.contentWindow, null, "Context's contentWindow property is null after restore from bfcache");
+  });
+
+  await contentPage.close();
+});
+
+add_task(async function test_contentscript_context_valid_during_execution() {
+  // This test does the following:
+  // - Load page
+  // - Load extension; inject content script.
+  // - Navigate page; pagehide triggered.
+  // - Navigate back; pageshow triggered.
+  // - Close page; pagehide, unload triggered.
+  // At each of these last four events, the validity of the context is checked.
+
+  function contentScript() {
+    browser.test.sendMessage("content-script-ready");
+    window.wrappedJSObject.checkContextIsValid("Context is valid on execution");
+
+    window.addEventListener("pagehide", () => {
+      window.wrappedJSObject.checkContextIsValid("Context is valid on pagehide");
+      browser.test.sendMessage("content-script-hide");
+    }, true);
+    window.addEventListener("pageshow", () => {
+      window.wrappedJSObject.checkContextIsValid("Context is valid on pageshow");
+
+      // This unload listener is registered after pageshow, to ensure that the
+      // page can be stored in the bfcache at the previous pagehide.
+      window.addEventListener("unload", () => {
+        window.wrappedJSObject.checkContextIsValid("Context is valid on unload");
+        browser.test.sendMessage("content-script-unload");
+      });
+
+      browser.test.sendMessage("content-script-show");
+    });
+  }
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      content_scripts: [{
+        "matches": ["http://example.com/dummy*"],
+        "js": ["content_script.js"],
+      }],
+    },
+
+    files: {
+      "content_script.js": contentScript,
+    },
+  });
+
+  let contentPage = await ExtensionTestUtils.loadContentPage("http://example.com/dummy?first");
+  await contentPage.spawn(extension.id, async extensionId => {
+    let context;
+    let checkContextIsValid = (description) => {
+      if (!context) {
+        let {DocumentManager} = ChromeUtils.import("resource://gre/modules/ExtensionContent.jsm", {});
+        context = DocumentManager.getContext(extensionId, this.content);
+      }
+      Assert.equal(context.contentWindow, this.content, `${description}: contentWindow`);
+      Assert.equal(context.active, true, `${description}: active`);
+    };
+    Cu.exportFunction(checkContextIsValid, this.content, {defineAs: "checkContextIsValid"});
+  });
+  await extension.startup();
+  await extension.awaitMessage("content-script-ready");
+
+  await contentPage.spawn(extension.id, async extensionId => {
+    // Navigate so that the content page is frozen in the bfcache.
+    this.content.location = "http://example.org/dummy?second";
+  });
+
+  await extension.awaitMessage("content-script-hide");
+  await contentPage.spawn(null, async () => {
+    // Navigate back so the content page is resurrected from the bfcache.
+    this.content.history.back();
+  });
+
+  await extension.awaitMessage("content-script-show");
+  await contentPage.close();
+  await extension.awaitMessage("content-script-hide");
+  await extension.awaitMessage("content-script-unload");
   await extension.unload();
 });
