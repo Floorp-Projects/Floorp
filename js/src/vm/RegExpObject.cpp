84c43c83967141d3a96cd8f10964bcb9fa6f44a8
Jon Coppeard <jcoppeard@mozilla.com>
2018-09-05 11:25:36 +0100

    Bug 1488463 - Handle OOM in RegExpShared::compile() r=nbp

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -1044,8 +1044,10 @@ RegExpShared::compile(JSContext* cx, MutableHandleRegExpShared re, HandleAtom pa
         // compilation.jitCode (to ensure no purging happens between adding the
         // tables and setting the JIT code).
         for (size_t i = 0; i < tables.length(); i++) {
-            if (!re->addTable(std::move(tables[i])))
+            if (!re->addTable(std::move(tables[i]))) {
+                ReportOutOfMemory(cx);
                 return false;
+            }
         }
         compilation.jitCode = code.jitCode;
     } else if (code.byteCode) {
