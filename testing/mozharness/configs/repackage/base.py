00b813c0f262c12c7d27b753c920b1b8cdfd8189
Rob Lemley <rob@thunderbird.net>
2018-09-07 17:51:02 +0000

    Bug 1489355 - Windows installer build fixes r=Callek a=tomprince
    Bug 1484264 made changes to how the repackage tasks is configured.
    The package-name field was hard-coded to "firefox". This field is later
    used to build the Windows installer, and Thunderbird requires that it
    be set to "thunderbird" or the build fails.
    
    This patch changes package-name to be a templated field like the others.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5222

--- testing/mozharness/configs/repackage/base.py
+++ testing/mozharness/configs/repackage/base.py
@@ -1,4 +1,5 @@
 config = {
+    "package-name": "firefox",
     "installer-tag": "browser/installer/windows/app.tag",
     "stub-installer-tag": "browser/installer/windows/stub.tag",
     "sfx-stub": "other-licenses/7zstub/firefox/7zSD.sfx",
