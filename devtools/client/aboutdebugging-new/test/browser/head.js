eef1913bcbc94492e5fba970ff25f0165d8159e4
Julian Descottes <jdescottes@mozilla.com>
2018-09-07 13:28:51 +0200

    Bug 1489219 - Add basic mochitest for This Firefox page;r=daisuke,ladybenko
    --HG--
    extra : rebase_source : 69e9be6640d8abd44299ad488487bf0723274346

---
+++ devtools/client/aboutdebugging-new/test/browser/head.js
@@ -0,0 +1,35 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* eslint-env browser */
+/* eslint no-unused-vars: [2, {"vars": "local"}] */
+/* import-globals-from ../../../shared/test/shared-head.js */
+
+"use strict";
+
+// Load the shared-head file first.
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/shared/test/shared-head.js",
+  this);
+
+/**
+ * Enable the new about:debugging panel.
+ */
+async function enableNewAboutDebugging() {
+  await pushPref("devtools.aboutdebugging.new-enabled", true);
+}
+
+async function openAboutDebugging(page, win) {
+  await enableNewAboutDebugging();
+
+  info("opening about:debugging");
+  const tab = await addTab("about:debugging", { window: win });
+  const browser = tab.linkedBrowser;
+  const document = browser.contentDocument;
+  const window = browser.contentWindow;
+
+  info("Wait until the main about debugging container is available");
+  await waitUntil(() => document.querySelector(".app"));
+
+  return { tab, document, window };
+}
