820278c472d915b8abb472caa9267bc71097c0ed
Tom Ritter <tom@mozilla.com>
2018-09-07 16:47:33 +0000

    Bug 1465800 Create an x64 MinGW-Clang Browser Build job r=froydnj
    MozReview-Commit-ID: 7TSCY52wTpR
    
    Differential Revision: https://phabricator.services.mozilla.com/D4553

---
+++ testing/mozharness/configs/builds/releng_sub_windows_configs/64_mingwclang.py
@@ -0,0 +1,7 @@
+config = {
+    'base_name': 'WINNT_6.1_MINGWCLANG_%(branch)s',
+    'platform': 'win64-mingwclang',
+    'stage_platform': 'win64-mingwclang',
+    'mozconfig_platform': 'win64',
+    'mozconfig_variant': 'mingwclang',
+}
