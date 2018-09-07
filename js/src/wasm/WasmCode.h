a1a3e786a3688a7ce02f99734fdd2fa3f6942cc0
Benjamin Bouvier <benj@benj.me>
2018-09-07 13:45:24 +0200

    Bug 1469027: Introduce a new switch to determine if we should compile with Cranelift; r=luke

--- js/src/wasm/WasmCode.h
+++ js/src/wasm/WasmCode.h
@@ -588,7 +588,7 @@ class CodeTier
         if (tier == Tier::Baseline) {
             return mutexid::WasmLazyStubsTier1;
         }
-        MOZ_ASSERT(tier == Tier::Ion);
+        MOZ_ASSERT(tier == Tier::Optimized);
         return mutexid::WasmLazyStubsTier2;
     }
 
