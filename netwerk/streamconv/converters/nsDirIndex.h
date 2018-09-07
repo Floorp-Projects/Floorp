2f81fcb4cd3a1fa43868ba14cc10a5268c70d1ec
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-06 22:13:44 -0400

    Bug 1489365 - Part 1: Remove the XPCOM component registration for nsDirIndex; r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D5228

--- netwerk/streamconv/converters/nsDirIndex.h
+++ netwerk/streamconv/converters/nsDirIndex.h
@@ -3,11 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef nsDirIndex_h__
+#define nsDirIndex_h__
+
 #include "nsIDirIndex.h"
 #include "nsString.h"
 #include "mozilla/Attributes.h"
-
-/* CID: {f6913e2e-1dd1-11b2-84be-f455dee342af} */
 
 class nsDirIndex final : public nsIDirIndex {
 
@@ -28,3 +29,5 @@ protected:
     int64_t mSize;
     PRTime mLastModified;
 };
+
+#endif
