97b09cfe0de6007d2389228f8cf114dcb9babe4d
Julian Descottes <jdescottes@mozilla.com>
2018-09-07 17:41:44 +0200

    Bug 1489219 - Wait until runtime page is ready;r=daisuke,ladybenko

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_thisfirefox.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_thisfirefox.js
@@ -18,7 +18,10 @@ const EXPECTED_TARGET_PANES = [
 ];
 
 add_task(async function() {
-  const { document } = await openAboutDebugging();
+  const { document, tab } = await openAboutDebugging();
+
+  // Wait until the client connection was established.
+  await waitUntil(() => document.querySelector(".js-runtime-page"));
 
   // Check that the selected sidebar item is "This Firefox"
   const selectedSidebarItem = document.querySelector(".js-sidebar-item-selected");
@@ -36,4 +39,6 @@ add_task(async function() {
     ok(paneTitles.includes(expectedPane),
       "Expected debug target category found: " + expectedPane);
   });
+
+  await removeTab(tab);
 });
