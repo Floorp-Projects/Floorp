793ee3390ce25de09806822b62a4750afc3451fb
L. David Baron <dbaron@dbaron.org>
2018-09-10 21:56:53 +0000

    Bug 1489297 patch 3 - Report MemoryErrorCorrection from WMI. r=aklotz
    Differential Revision: https://phabricator.services.mozilla.com/D5201

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3708,6 +3708,18 @@ static bool QueryOneWMIProperty(IWbemServices* aServices,
  * crashes. This function is best-effort; failures shouldn't burden the
  * caller. COM must be initialized before calling.
  */
+
+static const char kMemoryErrorCorrectionValues[][15] = {
+  "Reserved", // 0
+  "Other", // 1
+  "Unknown", // 2
+  "None", // 3
+  "Parity", // 4
+  "Single-bit ECC", // 5
+  "Multi-bit ECC", // 6
+  "CRC" // 7
+};
+
 static void AnnotateWMIData()
 {
   RefPtr<IWbemLocator> locator;
@@ -3746,6 +3758,23 @@ static void AnnotateWMIData()
     CrashReporter::AnnotateCrashReport(
       CrashReporter::Annotation::BIOS_Manufacturer,
       NS_ConvertUTF16toUTF8(V_BSTR(&value)));
+  }
+
+  VariantClear(&value);
+
+  // Annotate information about type of memory error correction.
+  if (QueryOneWMIProperty(services, L"Win32_PhysicalMemoryArray",
+                          L"MemoryErrorCorrection", &value) &&
+      V_VT(&value) == VT_I4) {
+    long valueInt = V_I4(&value);
+    nsCString valueString;
+    if (valueInt < 0 || valueInt >= ArrayLength(kMemoryErrorCorrectionValues)) {
+      valueString.AssignLiteral("Unexpected value");
+    } else {
+      valueString.AssignASCII(kMemoryErrorCorrectionValues[valueInt]);
+    }
+    CrashReporter::AnnotateCrashReport(
+      CrashReporter::Annotation::MemoryErrorCorrection, valueString);
   }
 
   VariantClear(&value);
