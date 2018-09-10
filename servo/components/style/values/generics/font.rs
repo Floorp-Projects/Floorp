d34223c4cbac9daaadc7de946508ce015c36cf84
Daniel Varga <dvarga@mozilla.com>
2018-09-10 13:36:56 +0300

    Backed out changeset 85d1a0c3f60c (bug 1489676) for mochitest failures at dom/animation/test/mozilla/test_distance_of_path_function.html on a CLOSED TREE

--- servo/components/style/values/generics/font.rs
+++ servo/components/style/values/generics/font.rs
@@ -13,6 +13,7 @@ use std::fmt::{self, Write};
 use std::io::Cursor;
 use style_traits::{CssWriter, KeywordsCollectFn, ParseError};
 use style_traits::{SpecifiedValueInfo, StyleParseErrorKind, ToCss};
+use values::distance::{ComputeSquaredDistance, SquaredDistance};
 
 /// https://drafts.csswg.org/css-fonts-4/#feature-tag-value
 #[derive(Clone, Debug, Eq, MallocSizeOf, PartialEq, SpecifiedValueInfo, ToComputedValue)]
@@ -46,16 +47,7 @@ where
 ///
 /// https://drafts.csswg.org/css-fonts-4/#font-variation-settings-def
 #[derive(
-    Animate,
-    Clone,
-    ComputeSquaredDistance,
-    Debug,
-    Eq,
-    MallocSizeOf,
-    PartialEq,
-    SpecifiedValueInfo,
-    ToComputedValue,
-    ToCss,
+    Animate, Clone, Debug, Eq, MallocSizeOf, PartialEq, SpecifiedValueInfo, ToComputedValue, ToCss,
 )]
 pub struct VariationValue<Number> {
     /// A four-character tag, packed into a u32 (one byte per character).
@@ -63,6 +55,19 @@ pub struct VariationValue<Number> {
     pub tag: FontTag,
     /// The actual value.
     pub value: Number,
+}
+
+impl<Number> ComputeSquaredDistance for VariationValue<Number>
+where
+    Number: ComputeSquaredDistance,
+{
+    #[inline]
+    fn compute_squared_distance(&self, other: &Self) -> Result<SquaredDistance, ()> {
+        if self.tag != other.tag {
+            return Err(());
+        }
+        self.value.compute_squared_distance(&other.value)
+    }
 }
 
 /// A value both for font-variation-settings and font-feature-settings.
