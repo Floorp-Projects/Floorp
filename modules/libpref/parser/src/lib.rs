178bce304962b3c6d1e8b5835b32d3ee24d9f358
Nicholas Nethercote <nnethercote@mozilla.com>
2018-09-11 09:41:37 +1000

    Bug 1490115 - Handle unaccompanied low surrogate pairs in the prefs parser. r=glandium
    Currently they cause the `String::from_utf16()` call to return an error result,
    and then the subsequent `unwrap()` on that result aborts.

--- modules/libpref/parser/src/lib.rs
+++ modules/libpref/parser/src/lib.rs
@@ -600,8 +600,7 @@ impl<'t> Parser<'t> {
     }
 
     // Returns false if we hit EOF without closing the comment.
-    fn match_multi_line_comment(&mut self) -> bool
-    {
+    fn match_multi_line_comment(&mut self) -> bool {
         loop {
             match self.get_char() {
                 b'*' => {
@@ -854,7 +853,7 @@ impl<'t> Parser<'t> {
                                         } else {
                                             self.string_error_token(
                                                 &mut token,
-                                                "invalid low surrogate value after high surrogate");
+                                                "invalid low surrogate after high surrogate");
                                             continue;
                                         }
                                     }
@@ -864,6 +863,11 @@ impl<'t> Parser<'t> {
                                         &mut token, "expected low surrogate after high surrogate");
                                     continue;
                                 }
+                            } else if 0xdc00 == (0xfc00 & value) {
+                                // Unaccompanied low surrogate value.
+                                self.string_error_token(
+                                    &mut token, "expected high surrogate before low surrogate");
+                                continue;
                             } else if value == 0 {
                                 self.string_error_token(&mut token, "\\u0000 is not allowed");
                                 continue;
