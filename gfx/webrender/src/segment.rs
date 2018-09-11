29baeff536c20d63143c4ed2190348f8f0fd08f4
Jeff Muizelaar <jmuizelaar@mozilla.com>
2018-09-11 00:51:10 -0400

    Bug 1490019. Update webrender to commit 27e3bc4293d4a5057346739bfe8b7a163cf068f3

--- gfx/webrender/src/segment.rs
+++ gfx/webrender/src/segment.rs
@@ -395,12 +395,11 @@ impl SegmentBuilder {
             None => return,
         };
 
+        let mut items = self.items;
+
         // First, filter out any items that don't intersect
         // with the visible bounding rect.
-        let mut items: Vec<Item> = self.items
-            .into_iter()
-            .filter(|item| item.rect.intersects(&bounding_rect))
-            .collect();
+        items.retain(|item| item.rect.intersects(&bounding_rect));
 
         // Create events for each item
         let mut x_events = Vec::new();
