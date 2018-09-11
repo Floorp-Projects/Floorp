cf1e3a8926a3ff835b9ef5fa51a42730a88c0b3b
Michael Kaply <mozilla@kaply.com>
2018-09-11 11:09:36 -0500

    Bug 1469400 - Add additional codes to blacklist. r=mikedeboer
    --HG--
    extra : rebase_source : b860a28dc25a1a592fe22299ea882c14140e1077

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -3121,6 +3121,11 @@ SearchService.prototype = {
 
   _blackList: [
     "blacklist=true",
+    "hspart=lvs",
+    "form=CONBDF",
+    "clid=2308146",
+    "fr=mcafee",
+    "PC=MC0",
   ],
 
   _addEngineToStore: function SRCH_SVC_addEngineToStore(aEngine) {
