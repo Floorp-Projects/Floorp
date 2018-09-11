bea9349f3dd0dfb3d18cbc19a6165e89f5e65851
Jason Orendorff <jorendorff@mozilla.com>
2018-09-11 13:00:52 +0000

    Bug 1488417 - Even better error message on property access on undefined/null variable. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D5197

--- js/src/jit-test/tests/ion/bug913749.js
+++ js/src/jit-test/tests/ion/bug913749.js
@@ -15,7 +15,7 @@ for (var i = 0; i < 3; i++) {
 	x.toString();
 	assertEq(0, 1);
     } catch (e) {
-	assertEq(e.message === `y is undefined, can't access property "length" of it` ||
+	assertEq(e.message === `y is undefined; can't access its "length" property` ||
 		 e.message === "undefined has no properties", true);
     }
 }
