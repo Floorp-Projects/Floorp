48619535cc315fde9ebe04e2ccaaf647711ae5b4
Shane Caraveo <scaraveo@mozilla.com>
2018-09-12 18:52:52 +0000

    Bug 1488055 fix loading ext-browser-content script in sidebar when browser_style=false, r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D5196
    
    --HG--
    extra : moz-landing-system : lando

---
+++ browser/components/extensions/test/browser/browser_ext_sidebarAction_click.js
@@ -0,0 +1,51 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+add_task(async function test_sidebar_click_isAppTab_behavior() {
+  function sidebarScript() {
+    browser.tabs.onUpdated.addListener(function onUpdated(tabId, changeInfo, tab) {
+      if (changeInfo.status == "complete" && tab.url == "http://mochi.test:8888/") {
+        browser.tabs.remove(tab.id);
+        browser.test.notifyPass("sidebar-click");
+      }
+    });
+    window.addEventListener("load", () => {
+      browser.test.sendMessage("sidebar-ready");
+    }, {once: true});
+  }
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "sidebar_action": {
+        "default_panel": "panel.html",
+        "browser_style": false,
+      },
+      "permissions": ["tabs"],
+    },
+    useAddonManager: "temporary",
+
+    files: {
+      "panel.html": `
+        <!DOCTYPE html>
+        <html>
+          <head>
+          <meta charset="utf-8">
+          </head>
+          <script src="panel.js" type="text/javascript"></script>
+          <a id="testlink" href="http://mochi.test:8888">Bugzilla</a>
+        </html>`,
+      "panel.js": sidebarScript,
+    },
+  });
+
+  await extension.startup();
+  await extension.awaitMessage("sidebar-ready");
+
+  // This test fails if docShell.isAppTab has not been set to true.
+  let content = SidebarUI.browser.contentWindow;
+  await BrowserTestUtils.synthesizeMouseAtCenter("#testlink", {}, content.gBrowser.selectedBrowser);
+  await extension.awaitFinish("sidebar-click");
+
+  await extension.unload();
+});
