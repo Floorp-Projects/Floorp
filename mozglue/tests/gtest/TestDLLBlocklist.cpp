c050561b96ae7a80a4ed71023a7f7b335267aa24
Carl Corcoran <ccorcoran@mozilla.com>
2018-09-12 12:14:10 +0200

    Bug 1490624: Correcting incorrect captitalization of windows.h r=bustage-fix

--- mozglue/tests/gtest/TestDLLBlocklist.cpp
+++ mozglue/tests/gtest/TestDLLBlocklist.cpp
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include <Windows.h>
+#include <windows.h>
 #include <shlwapi.h>
 #include <winternl.h>
 #include "gtest/gtest.h"
