72810a285c1e43c3822a5fa6760eb918af7bae69
Jan de Mooij <jdemooij@mozilla.com>
2018-09-13 13:51:46 +0000

    Bug 1490993 part 1 - Always use braces for if/for/while statements in js/src/jit/x64. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D5762

--- js/src/jit/x64/Assembler-x64.cpp
+++ js/src/jit/x64/Assembler-x64.cpp
@@ -90,10 +90,11 @@ ABIArgGenerator::next(MIRType type)
             stackOffset_ += sizeof(uint64_t);
             break;
         }
-        if (type == MIRType::Float32)
+        if (type == MIRType::Float32) {
             current_ = ABIArg(FloatArgRegs[floatRegIndex_++].asSingle());
-        else
+        } else {
             current_ = ABIArg(FloatArgRegs[floatRegIndex_++]);
+        }
         break;
       case MIRType::Int8x16:
       case MIRType::Int16x8:
@@ -140,8 +141,9 @@ Assembler::addPendingJump(JmpSrc src, ImmPtr target, RelocationKind reloc)
 
     // Emit reloc before modifying the jump table, since it computes a 0-based
     // index. This jump is not patchable at runtime.
-    if (reloc == RelocationKind::JITCODE)
+    if (reloc == RelocationKind::JITCODE) {
         writeRelocation(src, reloc);
+    }
     enoughMemory_ &= jumps_.append(RelativePatch(src.offset(), target.value, reloc));
 }
 
@@ -181,8 +183,9 @@ Assembler::PatchJumpEntry(uint8_t* entry, uint8_t* target)
 void
 Assembler::finish()
 {
-    if (oom())
+    if (oom()) {
         return;
+    }
 
     if (!jumps_.length()) {
         // Since we may be folowed by non-executable data, eagerly insert an
@@ -200,8 +203,9 @@ Assembler::finish()
     // jump relocation buffer if any JitCode references exist and must be
     // tracked for GC.
     MOZ_ASSERT_IF(jumpRelocations_.length(), jumpRelocations_.length() >= sizeof(uint32_t));
-    if (jumpRelocations_.length())
+    if (jumpRelocations_.length()) {
         *(uint32_t*)jumpRelocations_.buffer() = extendedJumpTable_;
+    }
 
     // Zero the extended jumps table.
     for (size_t i = 0; i < jumps_.length(); i++) {
@@ -270,8 +274,9 @@ class RelocationIterator
     }
 
     bool read() {
-        if (!reader_.more())
+        if (!reader_.more()) {
             return false;
+        }
         offset_ = reader_.readUnsigned();
         extOffset_ = reader_.readUnsigned();
         return true;
