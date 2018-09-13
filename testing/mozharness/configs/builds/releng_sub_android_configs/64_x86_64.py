354e9932723ece491b75e37a81817fa6c1e363dd
Jim Chen <nchen@mozilla.com>
2018-09-13 12:09:26 -0400

    Bug 1480834 - 6. Add Android x86-64 configs; r=nalexander
    Add config files for building Android for x86-64.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5603

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64.py
@@ -0,0 +1,7 @@
+config = {
+    'base_name': 'Android x86-64 API 21+ %(branch)s',
+    'stage_platform': 'android-x86_64',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86_64/nightly',
+    'multi_locale_config_platform': 'android',
+    'artifact_flag_build_variant_in_try': 'x86_64-artifact',
+}
---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_artifact.py
@@ -0,0 +1,6 @@
+config = {
+    'base_name': 'Android 5.0 x86-64 %(branch)s Artifact build',
+    'stage_platform': 'android-x86_64',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86_64/nightly-artifact',
+    'tooltool_manifest_src': 'mobile/android/config/tooltool-manifests/android-x86/releng.manifest',
+}
