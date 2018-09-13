9380fab02200f2200dd40c628218cdcd89ea8883
Andrew McCreight <continuation@gmail.com>
2018-09-13 18:37:48 +0000

    Bug 1489690, part 3 - De-COM the XUL sort service r=bzbarsky
    There's only one method that is used, and we never need to create any
    instances of the object, so turn it into a single method.
    
    Depends on D5592
    
    Differential Revision: https://phabricator.services.mozilla.com/D5593

--- dom/xul/nsXULSortService.h
+++ dom/xul/nsXULSortService.h
@@ -10,29 +10,34 @@
 #ifndef nsXULSortService_h
 #define nsXULSortService_h
 
-#include "nsISupportsImpl.h"
-#include "nsIXULSortService.h"
+#include "nsAString.h"
+#include "nsError.h"
 
-////////////////////////////////////////////////////////////////////////
-// ServiceImpl
-//
-//   This is the sort service.
-//
-class XULSortServiceImpl : public nsIXULSortService
-{
-public:
-  XULSortServiceImpl(void) {}
+namespace mozilla {
 
-protected:
-  virtual ~XULSortServiceImpl(void) {}
+namespace dom {
+class Element;
+} // namespace dom
 
-public:
-  // nsISupports
-  NS_DECL_ISUPPORTS
+/**
+ * Sort the contents of the widget containing <code>aNode</code>
+ * using <code>aSortKey</code> as the comparison key, and
+ * <code>aSortDirection</code> as the direction.
+ *
+ * @param aNode A node in the XUL widget whose children are to be sorted.
+ * @param aSortKey The value to be used as the comparison key.
+ * @param aSortHints One or more hints as to how to sort:
+ *
+ *   ascending: to sort the contents in ascending order
+ *   descending: to sort the contents in descending order
+ *   comparecase: perform case sensitive comparisons
+ *   integer: treat values as integers, non-integers are compared as strings
+ *   twostate: don't allow the natural (unordered state)
+ */
+nsresult XULWidgetSort(dom::Element* aNode,
+                       const nsAString& aSortKey,
+                       const nsAString& aSortHints);
 
-  // nsISortService
-  NS_DECL_NSIXULSORTSERVICE
-
-};
+} // namespace mozilla
 
 #endif // nsXULSortService_h
