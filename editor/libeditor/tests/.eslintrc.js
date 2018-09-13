be974d6c731608f525bb3a959f0f685a48843174
Mark Banner <standard8@mozilla.com>
2018-09-13 07:59:47 +0000

    Bug 1489980 - Enable ESLint for editor/ - Manual fixes. r=masayuki
    This enables the editor directory to be linted, and fixes the remaining issues raised by ESLint. Various rules were fixed here including, no-shadow, no-undef, no-unused-vars and others.
    
    I've generally gone conservative, disabling rules where it doesn't make sense to fix them (e.g. sometimes suggests use-services for tests, but it is only used once, or within a Chrome script).
    
    Depends on D5585
    
    Differential Revision: https://phabricator.services.mozilla.com/D5587

--- editor/libeditor/tests/.eslintrc.js
+++ editor/libeditor/tests/.eslintrc.js
@@ -4,5 +4,13 @@ module.exports = {
   "extends": [
     "plugin:mozilla/browser-test",
     "plugin:mozilla/mochitest-test",
-  ]
+  ],
+
+  "plugins": [
+    "no-unsanitized",
+  ],
+
+  "rules": {
+    "no-unsanitized/property": "off",
+  },
 };
