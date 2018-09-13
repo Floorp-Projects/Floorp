3444db64965239f7e4c2692f5630da3eb14b1491
Gian-Carlo Pascutto <gcp@mozilla.com>
2018-09-13 13:28:07 +0000

    Bug 1490413 - Update Rust license checks for new cargo formats. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D5679

--- python/mozbuild/mozbuild/vendor_rust.py
+++ python/mozbuild/mozbuild/vendor_rust.py
@@ -128,64 +128,108 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
 
         return cargo
 
-    def _check_licenses(self, vendor_dir):
-        # A whitelist of acceptable license identifiers for the
-        # packages.license field from https://spdx.org/licenses/.  Cargo
-        # documentation claims that values are checked against the above
-        # list and that multiple entries can be separated by '/'.  We
-        # choose to list all combinations instead for the sake of
-        # completeness and because some entries below obviously do not
-        # conform to the format prescribed in the documentation.
-        #
-        # It is insufficient to have additions to this whitelist reviewed
-        # solely by a build peer; any additions must be checked by somebody
-        # competent to review licensing minutiae.
+    # A whitelist of acceptable license identifiers for the
+    # packages.license field from https://spdx.org/licenses/.  Cargo
+    # documentation claims that values are checked against the above
+    # list and that multiple entries can be separated by '/'.  We
+    # choose to list all combinations instead for the sake of
+    # completeness and because some entries below obviously do not
+    # conform to the format prescribed in the documentation.
+    #
+    # It is insufficient to have additions to this whitelist reviewed
+    # solely by a build peer; any additions must be checked by somebody
+    # competent to review licensing minutiae.
 
-        # Licenses for code used at runtime. Please see the above comment before
-        # adding anything to this list.
-        RUNTIME_LICENSE_WHITELIST = [
-            'Apache-2.0',
-            'Apache-2.0 / MIT',
-            'Apache-2.0/MIT',
-            'Apache-2 / MIT',
-            'BSD-2-Clause',
-            'CC0-1.0',
-            'ISC',
-            'ISC/Apache-2.0',
-            'MIT',
-            'MIT / Apache-2.0',
-            'MIT/Apache-2.0',
-            'MIT OR Apache-2.0',
-            'MPL-2.0',
-            'Unlicense/MIT',
+    # Licenses for code used at runtime. Please see the above comment before
+    # adding anything to this list.
+    RUNTIME_LICENSE_WHITELIST = [
+        'Apache-2.0',
+        'BSD-2-Clause',
+        'CC0-1.0',
+        'ISC',
+        'MIT',
+        'MPL-2.0',
+        'Unlicense',
+    ]
+
+    # Licenses for code used at build time (e.g. code generators). Please see the above
+    # comments before adding anything to this list.
+    BUILDTIME_LICENSE_WHITELIST = {
+        'BSD-3-Clause': [
+            'adler32',
+            'bindgen',
+            'fuchsia-zircon',
+            'fuchsia-zircon-sys',
         ]
+    }
 
-        # Licenses for code used at build time (e.g. code generators). Please see the above
-        # comments before adding anything to this list.
-        BUILDTIME_LICENSE_WHITELIST = {
-            'BSD-3-Clause': [
-                'adler32',
-                'bindgen',
-                'fuchsia-zircon',
-                'fuchsia-zircon-sys',
-            ]
-        }
+    # This whitelist should only be used for packages that use a
+    # license-file and for which the license-file entry has been
+    # reviewed.  The table is keyed by package names and maps to the
+    # sha256 hash of the license file that we reviewed.
+    #
+    # As above, it is insufficient to have additions to this whitelist
+    # reviewed solely by a build peer; any additions must be checked by
+    # somebody competent to review licensing minutiae.
+    RUNTIME_LICENSE_FILE_PACKAGE_WHITELIST = {
+        # MIT
+        'deque': '6485b8ed310d3f0340bf1ad1f47645069ce4069dcc6bb46c7d5c6faf41de1fdb',
+    }
 
-        # This whitelist should only be used for packages that use a
-        # license-file and for which the license-file entry has been
-        # reviewed.  The table is keyed by package names and maps to the
-        # sha256 hash of the license file that we reviewed.
-        #
-        # As above, it is insufficient to have additions to this whitelist
-        # reviewed solely by a build peer; any additions must be checked by
-        # somebody competent to review licensing minutiae.
-        RUNTIME_LICENSE_FILE_PACKAGE_WHITELIST = {
-            # MIT
-            'deque': '6485b8ed310d3f0340bf1ad1f47645069ce4069dcc6bb46c7d5c6faf41de1fdb',
-        }
+    @staticmethod
+    def runtime_license(license_string):
+        """Cargo docs say:
+        ---
+        https://doc.rust-lang.org/cargo/reference/manifest.html
 
+        This is an SPDX 2.1 license expression for this package.  Currently
+        crates.io will validate the license provided against a whitelist of
+        known license and exception identifiers from the SPDX license list
+        2.4.  Parentheses are not currently supported.
+
+        Multiple licenses can be separated with a `/`, although that usage
+        is deprecated.  Instead, use a license expression with AND and OR
+        operators to get more explicit semantics.
+        ---
+        But I have no idea how you can meaningfully AND licenses, so
+        we will abort if that is detected. We'll handle `/` and OR as
+        equivalent and approve is any is in our approved list."""
+
+        if re.search(r'\s+AND', license_string):
+            return False
+
+        license_list = re.split(r'\s*/\s*|\s+OR\s+', license_string)
+        if any(license in VendorRust.RUNTIME_LICENSE_WHITELIST for license in license_list):
+            return True
+        return False
+
+    def _check_licenses(self, vendor_dir):
         LICENSE_LINE_RE = re.compile(r'\s*license\s*=\s*"([^"]+)"')
         LICENSE_FILE_LINE_RE = re.compile(r'\s*license[-_]file\s*=\s*"([^"]+)"')
+
+        def verify_acceptable_license(package, license):
+            self.log(logging.DEBUG, 'package_license', {},
+                     'has license {}'.format(license))
+
+            if not self.runtime_license(license):
+                if license not in self.BUILDTIME_LICENSE_WHITELIST:
+                    self.log(logging.ERROR, 'package_license_error', {},
+                            '''Package {} has a non-approved license: {}.
+
+    Please request license review on the package's license.  If the package's license
+    is approved, please add it to the whitelist of suitable licenses.
+    '''.format(package, license))
+                    return False
+                elif package not in self.BUILDTIME_LICENSE_WHITELIST[license]:
+                    self.log(logging.ERROR, 'package_license_error', {},
+                            '''Package {} has a license that is approved for build-time dependencies: {}
+    but the package itself is not whitelisted as being a build-time only package.
+
+    If your package is build-time only, please add it to the whitelist of build-time
+    only packages. Otherwise, you need to request license review on the package's license.
+    If the package's license is approved, please add it to the whitelist of suitable licenses.
+    '''.format(package, license))
+                    return False
 
         def check_package(package):
             self.log(logging.DEBUG, 'package_check', {},
@@ -219,34 +263,13 @@ Please commit or stash these changes before vendoring, or re-run with `--ignore-
 
                 if license_matches:
                     license = license_matches[0].group(1)
-                    self.log(logging.DEBUG, 'package_license', {},
-                             'has license {}'.format(license))
-
-                    if license not in RUNTIME_LICENSE_WHITELIST:
-                        if license not in BUILDTIME_LICENSE_WHITELIST:
-                            self.log(logging.ERROR, 'package_license_error', {},
-                                 '''Package {} has a non-approved license: {}.
-
-Please request license review on the package's license.  If the package's license
-is approved, please add it to the whitelist of suitable licenses.
-'''.format(package, license))
-                            return False
-                        elif package not in BUILDTIME_LICENSE_WHITELIST[license]:
-                            self.log(logging.ERROR, 'package_license_error', {},
-                                 '''Package {} has a license that is approved for build-time dependencies: {}
-but the package itself is not whitelisted as being a build-time only package.
-
-If your package is build-time only, please add it to the whitelist of build-time
-only packages. Otherwise, you need to request license review on the package's license.
-If the package's license is approved, please add it to the whitelist of suitable licenses.
-'''.format(package, license))
-                            return False
+                    verify_acceptable_license(package, license)
                 else:
                     license_file = license_file_matches[0].group(1)
                     self.log(logging.DEBUG, 'package_license_file', {},
                              'has license-file {}'.format(license_file))
 
-                    if package not in RUNTIME_LICENSE_FILE_PACKAGE_WHITELIST:
+                    if package not in self.RUNTIME_LICENSE_FILE_PACKAGE_WHITELIST:
                         self.log(logging.ERROR, 'package_license_file_unknown', {},
                                  '''Package {} has an unreviewed license file: {}.
 
@@ -255,7 +278,7 @@ to the whitelist of packages whose licenses are suitable.
 '''.format(package, license_file))
                         return False
 
-                    approved_hash = LICENSE_FILE_PACKAGE_WHITELIST[package]
+                    approved_hash = self.RUNTIME_LICENSE_FILE_PACKAGE_WHITELIST[package]
                     license_contents = open(os.path.join(vendor_dir, package, license_file), 'r').read()
                     current_hash = hashlib.sha256(license_contents).hexdigest()
                     if current_hash != approved_hash:
