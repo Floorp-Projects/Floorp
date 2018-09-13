f05a93dc27940d99e59131a1ced3076c00b814fd
Julian Descottes <jdescottes@mozilla.com>
2018-09-13 09:51:21 +0100

    Bug 1489872 - Extract network locations list and form to dedicated components;r=daisuke,ladybenko
    Summary:
    Depends on D5384. Straightforward extraction of networks list and form as
    dedicated components. Related CSS extracted as well.
    
    Reviewers: daisuke, ladybenko
    
    Reviewed By: daisuke, ladybenko
    
    Bug #: 1489872
    
    Differential Revision: https://phabricator.services.mozilla.com/D5385

--- devtools/client/aboutdebugging-new/src/components/connect/ConnectPage.js
+++ devtools/client/aboutdebugging-new/src/components/connect/ConnectPage.js
@@ -8,13 +8,10 @@ const { createFactory, PureComponent } = require("devtools/client/shared/vendor/
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 
-const {
-  addNetworkLocation,
-  removeNetworkLocation
-} = require("../../modules/network-locations");
-
 const ConnectSection = createFactory(require("./ConnectSection"));
 const ConnectSteps = createFactory(require("./ConnectSteps"));
+const NetworkLocationsForm = createFactory(require("./NetworkLocationsForm"));
+const NetworkLocationsList = createFactory(require("./NetworkLocationsList"));
 
 const USB_ICON_SRC = "chrome://devtools/skin/images/aboutdebugging-connect-icon.svg";
 const WIFI_ICON_SRC = "chrome://devtools/skin/images/aboutdebugging-connect-icon.svg";
@@ -24,13 +21,6 @@ class ConnectPage extends PureComponent {
   static get propTypes() {
     return {
       networkLocations: PropTypes.arrayOf(PropTypes.object).isRequired,
-    };
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      locationInputValue: ""
     };
   }
 
@@ -75,61 +65,9 @@ class ConnectPage extends PureComponent {
         icon: GLOBE_ICON_SRC,
         title: "Via Network Location",
       },
-      dom.ul(
-        {},
-        networkLocations.map(location =>
-          dom.li(
-            {
-              className: "connect-page__network-location"
-            },
-            dom.span(
-              {
-                className: "ellipsis-text"
-              },
-              location
-            ),
-            dom.button(
-              {
-                className: "aboutdebugging-button",
-                onClick: () => removeNetworkLocation(location)
-              },
-              "Remove"
-            )
-          )
-        )
-      ),
-      dom.hr(
-        {
-          className: "connect-page__network__separator"
-        }
-      ),
-      dom.form(
-        {
-          className: "connect-page__network-form",
-          onSubmit: (e) => {
-            const locationInputValue = this.state.locationInputValue;
-            if (locationInputValue) {
-              addNetworkLocation(locationInputValue);
-              this.setState({ locationInputValue: "" });
-            }
-            e.preventDefault();
-          }
-        },
-        dom.span({}, "Host:port"),
-        dom.input({
-          className: "connect-page__network-form__input",
-          placeholder: "localhost:6080",
-          type: "text",
-          value: this.state.locationInputValue,
-          onChange: (e) => {
-            const locationInputValue = e.target.value;
-            this.setState({ locationInputValue });
-          }
-        }),
-        dom.button({
-          className: "aboutdebugging-button"
-        }, "Add")
-      )
+      NetworkLocationsList({ networkLocations }),
+      dom.hr({ className: "connect-page__network__separator" }),
+      NetworkLocationsForm(),
     );
   }
 
