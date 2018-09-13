ae55a2bd7a76e4659002c6f492efd311ad8b90dd
Gian-Carlo Pascutto <gcp@mozilla.com>
2018-09-13 13:28:07 +0000

    Bug 1490413 - Update Rust license checks for new cargo formats. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D5679
    
    --HG--
    extra : moz-landing-system : lando

---
+++ python/mozbuild/mozbuild/test/test_licenses.py
@@ -0,0 +1,37 @@
+import unittest
+
+from StringIO import StringIO
+import os
+import sys
+import os.path
+import mozunit
+
+from mozbuild.preprocessor import Preprocessor
+
+from mozbuild.vendor_rust import VendorRust
+
+class TestLicenses(unittest.TestCase):
+  """
+  Unit tests for the Rust Vendoring stuff
+  """
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def testLicense(self):
+    self.assertEquals(VendorRust.runtime_license('Apache-2.0'), True)
+    self.assertEquals(VendorRust.runtime_license('MIT'), True)
+    self.assertEquals(VendorRust.runtime_license('GPL'), False)
+    self.assertEquals(VendorRust.runtime_license('MIT /GPL'), True)
+    self.assertEquals(VendorRust.runtime_license('GPL/ Proprietary'), False)
+    self.assertEquals(VendorRust.runtime_license('GPL AND MIT'), False)
+    self.assertEquals(VendorRust.runtime_license('ISC\tAND\tMIT'), False)
+    self.assertEquals(VendorRust.runtime_license('GPL OR MIT'), True)
+    self.assertEquals(VendorRust.runtime_license('ALLIGATOR MIT'), False)
+    pass
+
+if __name__ == '__main__':
+  mozunit.main()
