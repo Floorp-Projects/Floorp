7384081942b17989ffa1f92a1f0c34a8f9e129cf
Matt Woodrow <mwoodrow@mozilla.com>
2018-09-14 02:20:46 +0000

    Bug 1489337 - Don't set preserve-3d to true when creating WebRender commands for nsDisplayPerspective since it's not needed. r=jrmuizel
    MozReview-Commit-ID: GiRda3WyTEu
    
    Differential Revision: https://phabricator.services.mozilla.com/D5214

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -9388,6 +9388,8 @@ nsDisplayPerspective::CreateWebRenderCommands(
   Point3D roundedOrigin(NS_round(newOrigin.x), NS_round(newOrigin.y), 0);
 
   gfx::Matrix4x4 transformForSC = gfx::Matrix4x4::Translation(roundedOrigin);
+  
+  nsIFrame* perspectiveFrame = mFrame->GetContainingBlock(nsIFrame::SKIP_SCROLLED_FRAME);
 
   nsTArray<mozilla::wr::WrFilterOp> filters;
   StackingContextHelper sc(aSc,
@@ -9401,7 +9403,7 @@ nsDisplayPerspective::CreateWebRenderCommands(
                            &perspectiveMatrix,
                            gfx::CompositionOp::OP_OVER,
                            !BackfaceIsHidden(),
-                           true);
+                           perspectiveFrame->Extend3DContext());
 
   return mList.CreateWebRenderCommands(
     aBuilder, aResources, sc, aManager, aDisplayListBuilder);
