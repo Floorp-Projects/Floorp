03f228e910fa8a87a0f071fc6146fb9c31e7596e
Joel Maher <jmaher@mozilla.com>
2018-09-14 13:51:05 +0000

    Bug 1458242 - add DOMContentFlushed to tp6 as a measurement. r=rwood
    add DOMContentFlushed to tp6, collect it and return the geometric mean of all measurements.
    
    Differential Revision: https://phabricator.services.mozilla.com/D5797

--- testing/profiles/raptor/user.js
+++ testing/profiles/raptor/user.js
@@ -1,6 +1,7 @@
 // Preferences file used by the raptor harness
 /* globals user_pref */
 user_pref("dom.performance.time_to_non_blank_paint.enabled", true);
+user_pref("dom.performance.time_to_dom_content_flushed.enabled", true);
 
 // required for geckoview logging
 user_pref("geckoview.console.enabled", true);
