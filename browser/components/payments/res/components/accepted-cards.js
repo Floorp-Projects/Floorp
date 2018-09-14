1aedc1ca7b924d028d29b6dcd03c9fe9e0b3cfe6
Sam Foster <sfoster@mozilla.com>
2018-09-14 20:28:41 +0000

    Bug 1429181 - Consider supportedNetworks when determining if payment method is valid. r=MattN
    * A new accepted-cards element to represent the labeled list of card icons
    * Add the accepted cards section to the summary and card add/edit page
    * mochitest for the accepted-cards element
    * Make cc-type a required field and validate it against the list of supported networks
    * Add verification of the pay button disabling when card network is not supported
    
    Depends on D5823
    
    Differential Revision: https://phabricator.services.mozilla.com/D5824

---
+++ browser/components/payments/res/components/accepted-cards.js
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import PaymentStateSubscriberMixin from "../mixins/PaymentStateSubscriberMixin.js";
+import paymentRequest from "../paymentRequest.js";
+/* import-globals-from ../unprivileged-fallbacks.js */
+
+/**
+ * <accepted-cards></accepted-cards>
+ */
+
+export default class AcceptedCards extends PaymentStateSubscriberMixin(HTMLElement) {
+  constructor() {
+    super();
+
+    this._listEl = document.createElement("ul");
+    this._listEl.classList.add("accepted-cards-list");
+    this._labelEl = document.createElement("span");
+    this._labelEl.classList.add("accepted-cards-label");
+  }
+
+  connectedCallback() {
+    this.label = this.getAttribute("label");
+    this.appendChild(this._labelEl);
+
+    this._listEl.textContent = "";
+    let allNetworks = PaymentDialogUtils.getCreditCardNetworks();
+    for (let network of allNetworks) {
+      let item = document.createElement("li");
+      item.classList.add("accepted-cards-item");
+      item.dataset.networkId = network;
+      this._listEl.appendChild(item);
+    }
+    this.appendChild(this._listEl);
+    // Only call the connected super callback(s) once our markup is fully
+    // connected
+    super.connectedCallback();
+  }
+
+  render(state) {
+    let acceptedNetworks = paymentRequest.getAcceptedNetworks(state.request);
+    for (let item of this._listEl.children) {
+      let network = item.dataset.networkId;
+      item.hidden = !(network && acceptedNetworks.includes(network));
+    }
+  }
+
+  set label(value) {
+    this._labelEl.textContent = value;
+  }
+}
+
+customElements.define("accepted-cards", AcceptedCards);
