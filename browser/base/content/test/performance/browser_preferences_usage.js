2c13d5e9d73f238ea0f2ad0f6ea36e68d6c7fde7
Robert Strong <robert.bugzilla@gmail.com>
2018-09-14 23:44:37 +0000

    Bug 1358342 - Remove the aushelper system add-on. r=florian
    The aushelper add-on is being removed and this test needs the minimum number of values in extensions.getAddons.cache.enabled lowered so it passes
    
    Differential Revision: https://phabricator.services.mozilla.com/D5892

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -96,7 +96,7 @@ add_task(async function startup() {
       // This is accessed in debug only.
     },
     "extensions.getAddons.cache.enabled": {
-      min: 7,
+      min: 6,
       max: 55,
     },
 
