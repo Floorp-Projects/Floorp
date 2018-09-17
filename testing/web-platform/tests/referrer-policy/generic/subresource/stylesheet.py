09803e5951d8e8eddd34ef8e1bde5021293ad3a5
Thomas Nguyen <tnguyen@mozilla.com>
2018-09-17 05:38:26 +0000

    Bug 1330487 - Part 9: Add referencing svg from css wpt test r=heycam
    MozReview-Commit-ID: Fl643GY6Jhc
    
    Differential Revision: https://phabricator.services.mozilla.com/D1943

--- testing/web-platform/tests/referrer-policy/generic/subresource/stylesheet.py
+++ testing/web-platform/tests/referrer-policy/generic/subresource/stylesheet.py
@@ -3,14 +3,41 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import subresource
 
 def generate_payload(request, server_data):
-    return subresource.get_template("stylesheet.css.template") % {"id": request.GET["id"]}
+    data = ('{"headers": %(headers)s}') % server_data
+    type = 'image'
+    if "type" in request.GET:
+        type = request.GET["type"]
+
+    if "id" in request.GET:
+        request.server.stash.put(request.GET["id"], data)
+
+    if type == 'image':
+        return subresource.get_template("image.css.template") % {"id": request.GET["id"]}
+
+    elif type == 'svg':
+        return subresource.get_template("svg.css.template") % {
+            "id": request.GET["id"],
+            "property": request.GET["property"]}
 
 def generate_import_rule(request, server_data):
-    data = "@import url('%(url)s?id=%(id)s');" % {
+    type = 'image'
+    property = None;
+    if "type" in request.GET:
+        type = request.GET["type"]
+    if type == "svg" and "property" in request.GET:
+        property = request.GET["property"]
+    if property is None:
+        return "@import url('%(url)s?id=%(id)s&type=%(type)s');" % {
+            "id": request.GET["id"],
+            "url": subresource.create_redirect_url(request, cross_origin = True),
+            "type": type
+        }
+    return "@import url('%(url)s?id=%(id)s&type=%(type)s&property=%(property)s');" % {
         "id": request.GET["id"],
-        "url": subresource.create_redirect_url(request, cross_origin = True)
+        "url": subresource.create_redirect_url(request, cross_origin = True),
+        "type": type,
+        "property": property
     }
-    return data
 
 def main(request, response):
     payload_generator = lambda data: generate_payload(request, data)
