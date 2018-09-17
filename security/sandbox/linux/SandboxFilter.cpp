0551d90104f1fc60bafee4877b9e2e987c34a7aa
Jed Davis <jld@mozilla.com>
2018-09-17 16:43:52 +0000

    Bug 1489735 - Quietly deny sched_setaffinity in content process sandbox r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D5908

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -1101,6 +1101,11 @@ public:
       return Allow();
 
 #ifdef DESKTOP
+    case __NR_sched_setaffinity:
+      return Error(EPERM);
+#endif
+
+#ifdef DESKTOP
     case __NR_pipe2:
       return Allow();
 
