03a1349a98f1eb6cbc695c33dfcdcb1daa36f37c
Tom Prince <mozilla@hocat.ca>
2018-09-17 18:09:03 +0000

    Bug 1490094: [taskgraph] Add a transform to skip tasks by release type; r=bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D5782

---
+++ taskcluster/taskgraph/transforms/release.py
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Transforms for release tasks
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+def run_on_releases(config, jobs):
+    """
+    Filter out jobs with `run-on-releases` set, and that don't match the
+    `release_type` paramater.
+    """
+    for job in jobs:
+        release_type = config.params['release_type']
+        run_on_release_types = job.pop('run-on-releases', None)
+
+        if run_on_release_types is None or release_type in run_on_release_types:
+            yield job
