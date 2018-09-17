fee7847f797c5597e6bd74039df880f7771511be
Miko Mynttinen <mikokm@gmail.com>
2018-09-17 14:41:08 +0000

    Bug 1488599 - Part 1: Add RetainedDisplayListData that will store frame invalidation information r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D5245

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -2375,6 +2375,8 @@ public:
    */
   static bool AreRetainedDisplayListsEnabled();
 
+  static bool DisplayRootHasRetainedDisplayListBuilder(nsIFrame* aFrame);
+
   /**
    * Find a suitable scale for a element (aFrame's content) over the course of any
    * animations and transitions of the CSS transform property on the
