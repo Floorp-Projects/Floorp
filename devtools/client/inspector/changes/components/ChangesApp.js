d5d2274e18ffb31c5772c8fcad07c0340e2e4643
Razvan Caliman <rcaliman@mozilla.com>
2018-09-17 15:06:29 +0000

    Bug 1478448 - (Part 4) Render tracked style changes. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D3329
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/changes/components/ChangesApp.js
+++ devtools/client/inspector/changes/components/ChangesApp.js
@@ -6,15 +6,71 @@
 
 const { PureComponent } = require("devtools/client/shared/vendor/react");
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 const { connect } = require("devtools/client/shared/vendor/react-redux");
 
 class ChangesApp extends PureComponent {
+  static get propTypes() {
+    return {
+      changes: PropTypes.object.isRequired
+    };
+  }
+
+  renderMutations(remove = {}, add = {}) {
+    const removals = Object.entries(remove).map(([prop, value]) => {
+      return dom.div(
+        { className: "line diff-remove"},
+        `${prop}: ${value};`
+      );
+    });
+
+    const additions = Object.entries(add).map(([prop, value]) => {
+      return dom.div(
+        { className: "line diff-add"},
+        `${prop}: ${value};`
+      );
+    });
+
+    return [removals, additions];
+  }
+
+  renderSelectors(selectors = {}) {
+    return Object.keys(selectors).map(sel => {
+      return dom.details(
+        { className: "selector", open: true },
+        dom.summary(
+          {
+            title: sel,
+          },
+          sel),
+        this.renderMutations(selectors[sel].remove, selectors[sel].add)
+      );
+    });
+  }
+
+  renderDiff(diff = {}) {
+    // Render groups of style sources: stylesheets, embedded styles and inline styles
+    return Object.keys(diff).map(href => {
+      return dom.details(
+        { className: "source", open: true },
+        dom.summary(
+          {
+            title: href,
+          },
+          href),
+        // Render groups of selectors
+        this.renderSelectors(diff[href])
+      );
+    });
+  }
+
   render() {
     return dom.div(
       {
         className: "theme-sidebar inspector-tabpanel",
         id: "sidebar-panel-changes"
-      }
+      },
+      this.renderDiff(this.props.changes.diff)
     );
   }
 }
