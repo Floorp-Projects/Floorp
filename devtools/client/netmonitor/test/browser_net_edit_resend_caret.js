7a871745f79ff6f541c27594e544026131b6ad95
Tom Glowka <glowka.tom@gmail.com>
2018-09-17 13:04:37 +0000

    Bug 1478361 - Simplify custom headers editing to prevent caret reset; r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D5410
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/netmonitor/test/browser_net_edit_resend_caret.js
@@ -0,0 +1,66 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests if position of caret does not change (resets to the end) after setting
+ * header's value to empty string.
+ */
+
+add_task(async function() {
+  const { tab, monitor } = await initNetMonitor(SIMPLE_URL);
+  info("Starting test... ");
+
+  const { document, store, windowRequire, parent } = monitor.panelWin;
+  const parentDocument = parent.document;
+  const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
+  store.dispatch(Actions.batchEnable(false));
+
+  // Reload to have one request in the list.
+  const waitForEvents = waitForNetworkEvents(monitor, 1);
+  tab.linkedBrowser.loadURI(SIMPLE_URL);
+  await waitForEvents;
+
+  // Open context menu and execute "Edit & Resend".
+  const firstRequest = document.querySelectorAll(".request-list-item")[0];
+  const waitForHeaders = waitUntil(() => document.querySelector(".headers-overview"));
+  EventUtils.sendMouseEvent({ type: "mousedown" }, firstRequest);
+  await waitForHeaders;
+  await waitForRequestData(store, ["requestHeaders"]);
+  EventUtils.sendMouseEvent({ type: "contextmenu" }, firstRequest);
+
+  // Open "New Request" form
+  const contextResend =  parentDocument.querySelector("#request-list-context-resend");
+  contextResend.click();
+  await waitUntil(() => document.querySelector("#custom-headers-value"));
+  const headersTextarea = document.querySelector("#custom-headers-value");
+  const {
+    getSelectedRequest
+  } = windowRequire("devtools/client/netmonitor/src/selectors/index");
+  const request = getSelectedRequest(store.getState());
+  const hostHeader = request.requestHeaders.headers[0];
+
+  // Close the open context menu, otherwise sendString will not work
+  EventUtils.synthesizeKey("VK_ESCAPE", {});
+  headersTextarea.focus();
+
+  // Clean value of host header
+  const headersContent = headersTextarea.value;
+  const start = "Host: ".length;
+  const end = headersContent.indexOf("\n");
+  headersTextarea.setSelectionRange(start, end);
+  EventUtils.synthesizeKey("VK_DELETE", {});
+
+  ok(getSelectedRequest(store.getState()).requestHeaders.headers[0] !== hostHeader,
+    "Value of Host header was edited and should change"
+  );
+
+  ok(headersTextarea.selectionStart === start && headersTextarea.selectionEnd === start,
+    "Position of caret should not change"
+  );
+
+  return teardown(monitor);
+});
