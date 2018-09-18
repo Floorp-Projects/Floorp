6471490b0a43746eccd15bd974fedab0f11008f6
Karl Tomlinson <karlt+@karlt.net>
2018-09-18 09:28:28 +0000

    Bug 1492014 remove now-unused WorkletThread::mWorkletLoadInfo r=baku
    Depends on D6107
    
    Differential Revision: https://phabricator.services.mozilla.com/D6108

--- dom/worklet/Worklet.cpp
+++ dom/worklet/Worklet.cpp
@@ -14,6 +14,7 @@
 #include "mozilla/dom/Response.h"
 #include "mozilla/dom/ScriptSettings.h"
 #include "mozilla/dom/ScriptLoader.h"
+#include "mozilla/dom/WorkletImpl.h"
 #include "js/CompilationAndEvaluation.h"
 #include "js/SourceBufferHolder.h"
 #include "nsIInputStreamPump.h"
