460f7c65bacf30faef5a024e37f31c78475dbec8
Gabriel Luong <gabriel.luong@gmail.com>
2018-09-18 09:46:01 -0400

    Bug 1478397 - Part 10: Adjust the flexbox header to match the design spec. r=rcaliman

--- devtools/client/inspector/flexbox/components/FlexContainer.js
+++ devtools/client/inspector/flexbox/components/FlexContainer.js
@@ -4,16 +4,16 @@
 
 "use strict";
 
-const { createFactory, createRef, PureComponent } = require("devtools/client/shared/vendor/react");
+const {
+  createElement,
+  createRef,
+  Fragment,
+  PureComponent,
+} = require("devtools/client/shared/vendor/react");
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 const { translateNodeFrontToGrip } = require("devtools/client/inspector/shared/utils");
 
-loader.lazyGetter(this, "FlexItemSelector", function() {
-  return createFactory(require("./FlexItemSelector"));
-});
-
-// Reps
 const { REPS, MODE } = require("devtools/client/shared/components/reps/reps");
 const { Rep } = REPS;
 const ElementNode = REPS.ElementNode;
@@ -28,8 +28,6 @@ class FlexContainer extends PureComponent {
       onHideBoxModelHighlighter: PropTypes.func.isRequired,
       onSetFlexboxOverlayColor: PropTypes.func.isRequired,
       onShowBoxModelHighlighterForNode: PropTypes.func.isRequired,
-      onToggleFlexboxHighlighter: PropTypes.func.isRequired,
-      onToggleFlexItemShown: PropTypes.func.isRequired,
       setSelectedNode: PropTypes.func.isRequired,
     };
   }
@@ -40,7 +38,6 @@ class FlexContainer extends PureComponent {
     this.colorValueEl = createRef();
     this.swatchEl = createRef();
 
-    this.onFlexboxCheckboxClick = this.onFlexboxCheckboxClick.bind(this);
     this.onFlexboxInspectIconClick = this.onFlexboxInspectIconClick.bind(this);
     this.setFlexboxColor = this.setFlexboxColor.bind(this);
   }
@@ -77,49 +74,10 @@ class FlexContainer extends PureComponent {
     this.props.onSetFlexboxOverlayColor(color);
   }
 
-  onFlexboxCheckboxClick(e) {
-    // If the click was on the svg icon to select the node in the inspector, bail out.
-    const originalTarget = e.nativeEvent && e.nativeEvent.explicitOriginalTarget;
-    if (originalTarget && originalTarget.namespaceURI === "http://www.w3.org/2000/svg") {
-      // We should be able to cancel the click event propagation after the following reps
-      // issue is implemented : https://github.com/devtools-html/reps/issues/95 .
-      e.preventDefault();
-      return;
-    }
-
-    const {
-      flexbox,
-      onToggleFlexboxHighlighter,
-    } = this.props;
-
-    onToggleFlexboxHighlighter(flexbox.nodeFront);
-  }
-
   onFlexboxInspectIconClick(nodeFront) {
     const { setSelectedNode } = this.props;
     setSelectedNode(nodeFront, { reason: "layout-panel" });
     nodeFront.scrollIntoView().catch(e => console.error(e));
-  }
-
-  renderFlexItemSelector() {
-    const {
-      flexbox,
-      onToggleFlexItemShown,
-    } = this.props;
-    const {
-      flexItems,
-      flexItemShown,
-    } = flexbox;
-
-    if (!flexItemShown) {
-      return null;
-    }
-
-    return FlexItemSelector({
-      flexItem: flexItems.find(item => item.nodeFront.actorID === flexItemShown),
-      flexItems,
-      onToggleFlexItemShown,
-    });
   }
 
   render() {
@@ -130,56 +88,36 @@ class FlexContainer extends PureComponent {
     } = this.props;
     const {
       color,
-      highlighted,
       nodeFront,
     } = flexbox;
 
-    return (
-      dom.div({ className: "flex-container devtools-monospace" },
-        dom.div({},
-          dom.label({},
-            dom.input(
-              {
-                className: "devtools-checkbox-toggle",
-                checked: highlighted,
-                onChange: this.onFlexboxCheckboxClick,
-                type: "checkbox",
-              }
-            ),
-            Rep(
-              {
-                defaultRep: ElementNode,
-                mode: MODE.TINY,
-                object: translateNodeFrontToGrip(nodeFront),
-                onDOMNodeMouseOut: () => onHideBoxModelHighlighter(),
-                onDOMNodeMouseOver: () => onShowBoxModelHighlighterForNode(nodeFront),
-                onInspectIconClick: () => this.onFlexboxInspectIconClick(nodeFront),
-              }
-            )
-          ),
-          dom.div(
-            {
-              className: "layout-color-swatch",
-              ref: this.swatchEl,
-              style: {
-                backgroundColor: color,
-              },
-              title: color,
-            }
-          ),
-          // The SwatchColorPicker relies on the nextSibling of the swatch element to
-          // apply the selected color. This is why we use a span in display: none for
-          // now. Ideally we should modify the SwatchColorPickerTooltip to bypass this
-          // requirement. See https://bugzilla.mozilla.org/show_bug.cgi?id=1341578
-          dom.span(
-            {
-              className: "layout-color-value",
-              ref: this.colorValueEl,
-            },
-            color
-          )
-        ),
-        this.renderFlexItemSelector()
+    return createElement(Fragment, null,
+      Rep({
+        defaultRep: ElementNode,
+        mode: MODE.TINY,
+        object: translateNodeFrontToGrip(nodeFront),
+        onDOMNodeMouseOut: () => onHideBoxModelHighlighter(),
+        onDOMNodeMouseOver: () => onShowBoxModelHighlighterForNode(nodeFront),
+        onInspectIconClick: () => this.onFlexboxInspectIconClick(nodeFront),
+      }),
+      dom.div({
+        className: "layout-color-swatch",
+        ref: this.swatchEl,
+        style: {
+          backgroundColor: color,
+        },
+        title: color,
+      }),
+      // The SwatchColorPicker relies on the nextSibling of the swatch element to
+      // apply the selected color. This is why we use a span in display: none for
+      // now. Ideally we should modify the SwatchColorPickerTooltip to bypass this
+      // requirement. See https://bugzilla.mozilla.org/show_bug.cgi?id=1341578
+      dom.span(
+        {
+          className: "layout-color-value",
+          ref: this.colorValueEl,
+        },
+        color
       )
     );
   }
