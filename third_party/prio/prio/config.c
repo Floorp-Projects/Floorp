88ee2451ac4c303d0aa84f8b4de64ca8cdb46cfd
Robert Helmer <rhelmer@mozilla.com>
2018-09-18 04:36:49 +0000

    Bug 1491289 - re-vendor libprio to pick up fixes for using system NSS r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D6082
    
    --HG--
    extra : moz-landing-system : lando

--- third_party/prio/prio/config.c
+++ third_party/prio/prio/config.c
@@ -1,47 +1,47 @@
 /*
  * Copyright (c) 2018, Henry Corrigan-Gibbs
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 #include <mprio.h>
 #include <stdlib.h>
 
 #include "config.h"
-#include "params.h"
 #include "mparray.h"
+#include "params.h"
 #include "rand.h"
 #include "util.h"
 
 // The PrioConfig object stores "2^k-th roots of unity" modulo
-// the prime modulus we use for all arithmetic. We use 
-// these roots to perform fast FFT-style polynomial 
+// the prime modulus we use for all arithmetic. We use
+// these roots to perform fast FFT-style polynomial
 // interpolation and evaluation.
 //
-// In particular, we use a prime modulus p such that 
+// In particular, we use a prime modulus p such that
 //    p = (2^k)q + 1.
 // The roots are integers such that r^{2^k} = 1 mod p.
 static SECStatus
-initialize_roots (MPArray arr, const char *values[])
+initialize_roots(MPArray arr, const char* values[])
 {
   // TODO: Read in only the number of roots of unity we need.
   // Right now we read in all 4096 roots whether or not we use
   // them all.
-  for (int i=0; i < arr->len; i++) {
-    MP_CHECK (mp_read_radix (&arr->data[i], values[i], 16));
+  for (int i = 0; i < arr->len; i++) {
+    MP_CHECK(mp_read_radix(&arr->data[i], values[i], 16));
   }
-  
+
   return SECSuccess;
 }
 
-PrioConfig 
-PrioConfig_new (int n_fields, PublicKey server_a, PublicKey server_b,
-    const unsigned char *batch_id, unsigned int batch_id_len)
+PrioConfig
+PrioConfig_new(int n_fields, PublicKey server_a, PublicKey server_b,
+               const unsigned char* batch_id, unsigned int batch_id_len)
 {
   SECStatus rv = SECSuccess;
-  PrioConfig cfg = malloc (sizeof (*cfg));
+  PrioConfig cfg = malloc(sizeof(*cfg));
   if (!cfg)
     return NULL;
 
@@ -61,72 +61,73 @@ PrioConfig_new (int n_fields, PublicKey server_a, PublicKey server_b,
     goto cleanup;
   }
 
-  P_CHECKA (cfg->batch_id = malloc (batch_id_len));
-  strncpy ((char *)cfg->batch_id, (char *)batch_id, batch_id_len);
+  P_CHECKA(cfg->batch_id = malloc(batch_id_len));
+  strncpy((char*)cfg->batch_id, (char*)batch_id, batch_id_len);
 
-  MP_CHECKC (mp_init (&cfg->modulus));
-  MP_CHECKC (mp_read_radix (&cfg->modulus, Modulus, 16)); 
+  MP_CHECKC(mp_init(&cfg->modulus));
+  MP_CHECKC(mp_read_radix(&cfg->modulus, Modulus, 16));
 
   // Compute  2^{-1} modulo M
-  MP_CHECKC (mp_init (&cfg->inv2));
-  mp_set (&cfg->inv2, 2);
-  MP_CHECKC (mp_invmod (&cfg->inv2, &cfg->modulus, &cfg->inv2)); 
+  MP_CHECKC(mp_init(&cfg->inv2));
+  mp_set(&cfg->inv2, 2);
+  MP_CHECKC(mp_invmod(&cfg->inv2, &cfg->modulus, &cfg->inv2));
 
-  P_CHECKA (cfg->roots = MPArray_new (cfg->n_roots));
-  P_CHECKA (cfg->rootsInv = MPArray_new (cfg->n_roots));
-  MP_CHECKC (initialize_roots (cfg->roots, Roots)); 
-  MP_CHECKC (initialize_roots (cfg->rootsInv, RootsInv)); 
+  P_CHECKA(cfg->roots = MPArray_new(cfg->n_roots));
+  P_CHECKA(cfg->rootsInv = MPArray_new(cfg->n_roots));
+  MP_CHECKC(initialize_roots(cfg->roots, Roots));
+  MP_CHECKC(initialize_roots(cfg->rootsInv, RootsInv));
 
 cleanup:
   if (rv != SECSuccess) {
-    PrioConfig_clear (cfg);
+    PrioConfig_clear(cfg);
     return NULL;
   }
 
   return cfg;
 }
 
-PrioConfig 
-PrioConfig_newTest (int nFields)
+PrioConfig
+PrioConfig_newTest(int nFields)
 {
-  return PrioConfig_new (nFields, NULL, NULL, 
-      (unsigned char *)"testBatch", 9);
-}
-
-void 
-PrioConfig_clear (PrioConfig cfg)
-{
-  if (!cfg) return;
-  if (cfg->batch_id) free (cfg->batch_id);
-  MPArray_clear (cfg->roots);
-  MPArray_clear (cfg->rootsInv);
-  mp_clear (&cfg->modulus);
-  mp_clear (&cfg->inv2);
-  free (cfg);
-}
-
-int 
-PrioConfig_numDataFields (const_PrioConfig cfg)
-{
-  return cfg->num_data_fields;
-
-}
-
-SECStatus 
-Prio_init (void)
-{
-  return rand_init ();
+  return PrioConfig_new(nFields, NULL, NULL, (unsigned char*)"testBatch", 9);
 }
 
 void
-Prio_clear (void)
+PrioConfig_clear(PrioConfig cfg)
 {
-  rand_clear ();
+  if (!cfg)
+    return;
+  if (cfg->batch_id)
+    free(cfg->batch_id);
+  MPArray_clear(cfg->roots);
+  MPArray_clear(cfg->rootsInv);
+  mp_clear(&cfg->modulus);
+  mp_clear(&cfg->inv2);
+  free(cfg);
 }
 
-int PrioConfig_hPoints (const_PrioConfig cfg)
+int
+PrioConfig_numDataFields(const_PrioConfig cfg)
+{
+  return cfg->num_data_fields;
+}
+
+SECStatus
+Prio_init(void)
+{
+  return rand_init();
+}
+
+void
+Prio_clear(void)
+{
+  rand_clear();
+}
+
+int
+PrioConfig_hPoints(const_PrioConfig cfg)
 {
   const int mul_gates = cfg->num_data_fields + 1;
-  const int N = next_power_of_two (mul_gates);
+  const int N = next_power_of_two(mul_gates);
   return N;
 }
--- third_party/prio/prio/serial.c
+++ third_party/prio/prio/serial.c
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2018, Henry Corrigan-Gibbs
- * 
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 #include <mprio.h>
@@ -17,277 +17,277 @@
 
 #define MSGPACK_OK 0
 
-static SECStatus 
-serial_write_mp_int (msgpack_packer *pk, const mp_int *n)
+static SECStatus
+serial_write_mp_int(msgpack_packer* pk, const mp_int* n)
 {
   SECStatus rv = SECSuccess;
-  unsigned int n_size = mp_unsigned_octet_size (n);  
+  unsigned int n_size = mp_unsigned_octet_size(n);
 
   unsigned char data[n_size];
-  MP_CHECK (mp_to_fixlen_octets (n, data, n_size));
+  MP_CHECK(mp_to_fixlen_octets(n, data, n_size));
 
-  P_CHECK (msgpack_pack_str (pk, n_size));
-  P_CHECK (msgpack_pack_str_body (pk, data, n_size));
+  P_CHECK(msgpack_pack_str(pk, n_size));
+  P_CHECK(msgpack_pack_str_body(pk, data, n_size));
   return rv;
 }
 
 static SECStatus
-object_to_mp_int (msgpack_object *obj, mp_int *n, const mp_int *max)
+object_to_mp_int(msgpack_object* obj, mp_int* n, const mp_int* max)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (obj != NULL);
-  P_CHECKCB (obj->type == MSGPACK_OBJECT_STR);
-  P_CHECKCB (n != NULL);
+  P_CHECKCB(obj != NULL);
+  P_CHECKCB(obj->type == MSGPACK_OBJECT_STR);
+  P_CHECKCB(n != NULL);
 
   msgpack_object_str s = obj->via.str;
-  P_CHECKCB (s.ptr != NULL);
-  MP_CHECKC (mp_read_unsigned_octets (n, (unsigned char *)s.ptr, s.size));
+  P_CHECKCB(s.ptr != NULL);
+  MP_CHECKC(mp_read_unsigned_octets(n, (unsigned char*)s.ptr, s.size));
 
-  P_CHECKCB (mp_cmp_z (n) >= 0);
-  P_CHECKCB (mp_cmp (n, max) < 0);
+  P_CHECKCB(mp_cmp_z(n) >= 0);
+  P_CHECKCB(mp_cmp(n, max) < 0);
 
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_mp_int (msgpack_unpacker *upk, mp_int *n, const mp_int *max)
+static SECStatus
+serial_read_mp_int(msgpack_unpacker* upk, mp_int* n, const mp_int* max)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (n != NULL);
-  P_CHECKCB (max != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(n != NULL);
+  P_CHECKCB(max != NULL);
 
   msgpack_unpacked res;
-  msgpack_unpacked_init (&res);
-  UP_CHECK (msgpack_unpacker_next (upk, &res))
+  msgpack_unpacked_init(&res);
+  UP_CHECK(msgpack_unpacker_next(upk, &res))
 
   msgpack_object obj = res.data;
-  P_CHECKC (object_to_mp_int (&obj, n, max));
+  P_CHECKC(object_to_mp_int(&obj, n, max));
 
 cleanup:
-  msgpack_unpacked_destroy (&res);
+  msgpack_unpacked_destroy(&res);
 
   return rv;
 }
 
-static SECStatus 
-serial_read_int (msgpack_unpacker *upk, int *n)
+static SECStatus
+serial_read_int(msgpack_unpacker* upk, int* n)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (n != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(n != NULL);
 
   msgpack_unpacked res;
-  msgpack_unpacked_init (&res);
-  UP_CHECK (msgpack_unpacker_next (upk, &res))
+  msgpack_unpacked_init(&res);
+  UP_CHECK(msgpack_unpacker_next(upk, &res))
 
   msgpack_object obj = res.data;
-  P_CHECKCB (obj.type == MSGPACK_OBJECT_POSITIVE_INTEGER);
+  P_CHECKCB(obj.type == MSGPACK_OBJECT_POSITIVE_INTEGER);
 
   *n = obj.via.i64;
 
 cleanup:
-  msgpack_unpacked_destroy (&res);
+  msgpack_unpacked_destroy(&res);
 
   return rv;
 }
 
-static SECStatus 
-serial_write_mp_array (msgpack_packer *pk, const_MPArray arr)
+static SECStatus
+serial_write_mp_array(msgpack_packer* pk, const_MPArray arr)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (arr != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(arr != NULL);
 
-  P_CHECK (msgpack_pack_array (pk, arr->len));
+  P_CHECK(msgpack_pack_array(pk, arr->len));
   for (int i = 0; i < arr->len; i++) {
-    P_CHECK (serial_write_mp_int (pk, &arr->data[i]));
+    P_CHECK(serial_write_mp_int(pk, &arr->data[i]));
   }
 
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_mp_array (msgpack_unpacker *upk, MPArray arr, size_t len, const mp_int
-    *max)
+static SECStatus
+serial_read_mp_array(msgpack_unpacker* upk, MPArray arr, size_t len,
+                     const mp_int* max)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (arr != NULL);
-  P_CHECKCB (max != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(arr != NULL);
+  P_CHECKCB(max != NULL);
 
   msgpack_unpacked res;
-  msgpack_unpacked_init (&res);
-  UP_CHECK (msgpack_unpacker_next (upk, &res))
+  msgpack_unpacked_init(&res);
+  UP_CHECK(msgpack_unpacker_next(upk, &res))
 
   msgpack_object obj = res.data;
-  P_CHECKCB (obj.type == MSGPACK_OBJECT_ARRAY);
+  P_CHECKCB(obj.type == MSGPACK_OBJECT_ARRAY);
 
   msgpack_object_array objarr = obj.via.array;
-  P_CHECKCB (objarr.size == len);
+  P_CHECKCB(objarr.size == len);
 
-  P_CHECKC (MPArray_resize (arr, len));
-  for (unsigned int i=0; i<len; i++) {
-    P_CHECKC (object_to_mp_int (&objarr.ptr[i], &arr->data[i], max));
+  P_CHECKC(MPArray_resize(arr, len));
+  for (unsigned int i = 0; i < len; i++) {
+    P_CHECKC(object_to_mp_int(&objarr.ptr[i], &arr->data[i], max));
   }
 
 cleanup:
-  msgpack_unpacked_destroy (&res);
+  msgpack_unpacked_destroy(&res);
 
   return rv;
 }
 
-
-static SECStatus 
-serial_write_beaver_triple (msgpack_packer *pk, const_BeaverTriple t)
+static SECStatus
+serial_write_beaver_triple(msgpack_packer* pk, const_BeaverTriple t)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (t != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(t != NULL);
 
-  P_CHECK (serial_write_mp_int (pk, &t->a)); 
-  P_CHECK (serial_write_mp_int (pk, &t->b)); 
-  P_CHECK (serial_write_mp_int (pk, &t->c)); 
+  P_CHECK(serial_write_mp_int(pk, &t->a));
+  P_CHECK(serial_write_mp_int(pk, &t->b));
+  P_CHECK(serial_write_mp_int(pk, &t->c));
 
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_beaver_triple (msgpack_unpacker *pk, BeaverTriple t, const mp_int *max)
+static SECStatus
+serial_read_beaver_triple(msgpack_unpacker* pk, BeaverTriple t,
+                          const mp_int* max)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (t != NULL);
-  P_CHECKCB (max != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(t != NULL);
+  P_CHECKCB(max != NULL);
 
-  P_CHECK (serial_read_mp_int (pk, &t->a, max)); 
-  P_CHECK (serial_read_mp_int (pk, &t->b, max)); 
-  P_CHECK (serial_read_mp_int (pk, &t->c, max)); 
+  P_CHECK(serial_read_mp_int(pk, &t->a, max));
+  P_CHECK(serial_read_mp_int(pk, &t->b, max));
+  P_CHECK(serial_read_mp_int(pk, &t->c, max));
 
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_write_server_a_data (msgpack_packer *pk, const struct server_a_data *A)
+static SECStatus
+serial_write_server_a_data(msgpack_packer* pk, const struct server_a_data* A)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (A != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(A != NULL);
 
-  P_CHECK (serial_write_mp_array (pk, A->data_shares)); 
-  P_CHECK (serial_write_mp_array (pk, A->h_points)); 
+  P_CHECK(serial_write_mp_array(pk, A->data_shares));
+  P_CHECK(serial_write_mp_array(pk, A->h_points));
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_server_a_data (msgpack_unpacker *upk, struct server_a_data *A,
-    const_PrioConfig cfg)
+static SECStatus
+serial_read_server_a_data(msgpack_unpacker* upk, struct server_a_data* A,
+                          const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (A != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(A != NULL);
 
-  P_CHECK (serial_read_mp_array (upk, A->data_shares, cfg->num_data_fields,
-        &cfg->modulus)); 
-  P_CHECK (serial_read_mp_array (upk, A->h_points, PrioConfig_hPoints (cfg),
-        &cfg->modulus)); 
-
-cleanup:
-  return rv;
-}
-
-static SECStatus 
-serial_write_prg_seed (msgpack_packer *pk, const PrioPRGSeed *seed)
-{
-  SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (seed != NULL);
-
-  P_CHECK (msgpack_pack_str (pk, PRG_SEED_LENGTH));
-  P_CHECK (msgpack_pack_str_body (pk, seed, PRG_SEED_LENGTH));
+  P_CHECK(serial_read_mp_array(upk, A->data_shares, cfg->num_data_fields,
+                               &cfg->modulus));
+  P_CHECK(serial_read_mp_array(upk, A->h_points, PrioConfig_hPoints(cfg),
+                               &cfg->modulus));
 
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_prg_seed (msgpack_unpacker *upk, PrioPRGSeed *seed)
+static SECStatus
+serial_write_prg_seed(msgpack_packer* pk, const PrioPRGSeed* seed)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (seed != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(seed != NULL);
+
+  P_CHECK(msgpack_pack_str(pk, PRG_SEED_LENGTH));
+  P_CHECK(msgpack_pack_str_body(pk, seed, PRG_SEED_LENGTH));
+
+cleanup:
+  return rv;
+}
+
+static SECStatus
+serial_read_prg_seed(msgpack_unpacker* upk, PrioPRGSeed* seed)
+{
+  SECStatus rv = SECSuccess;
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(seed != NULL);
 
   msgpack_unpacked res;
-  msgpack_unpacked_init (&res);
-  UP_CHECK (msgpack_unpacker_next (upk, &res))
+  msgpack_unpacked_init(&res);
+  UP_CHECK(msgpack_unpacker_next(upk, &res))
 
   msgpack_object obj = res.data;
-  P_CHECKCB (obj.type == MSGPACK_OBJECT_STR);
+  P_CHECKCB(obj.type == MSGPACK_OBJECT_STR);
 
   msgpack_object_str s = obj.via.str;
-  P_CHECKCB (s.size == PRG_SEED_LENGTH);
-  memcpy (seed, s.ptr, PRG_SEED_LENGTH);
+  P_CHECKCB(s.size == PRG_SEED_LENGTH);
+  memcpy(seed, s.ptr, PRG_SEED_LENGTH);
 
 cleanup:
-  msgpack_unpacked_destroy (&res);
+  msgpack_unpacked_destroy(&res);
 
   return rv;
 }
 
-static SECStatus 
-serial_write_server_b_data (msgpack_packer *pk, const struct server_b_data *B)
+static SECStatus
+serial_write_server_b_data(msgpack_packer* pk, const struct server_b_data* B)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (B != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(B != NULL);
 
-  rv = serial_write_prg_seed (pk, &B->seed);
+  rv = serial_write_prg_seed(pk, &B->seed);
 cleanup:
   return rv;
 }
 
-static SECStatus 
-serial_read_server_b_data (msgpack_unpacker *upk, struct server_b_data *B)
+static SECStatus
+serial_read_server_b_data(msgpack_unpacker* upk, struct server_b_data* B)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (B != NULL);
-  
-  rv =serial_read_prg_seed (upk, &B->seed);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(B != NULL);
+
+  rv = serial_read_prg_seed(upk, &B->seed);
 cleanup:
   return rv;
 }
 
-SECStatus 
-serial_write_packet_client (msgpack_packer *pk, const_PrioPacketClient p,
-    const_PrioConfig cfg)
+SECStatus
+serial_write_packet_client(msgpack_packer* pk, const_PrioPacketClient p,
+                           const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(p != NULL);
 
-  P_CHECK (msgpack_pack_str (pk, cfg->batch_id_len));
-  P_CHECK (msgpack_pack_str_body (pk, cfg->batch_id, cfg->batch_id_len));
+  P_CHECK(msgpack_pack_str(pk, cfg->batch_id_len));
+  P_CHECK(msgpack_pack_str_body(pk, cfg->batch_id, cfg->batch_id_len));
 
-  P_CHECK (serial_write_beaver_triple (pk, p->triple));
+  P_CHECK(serial_write_beaver_triple(pk, p->triple));
 
-  P_CHECK (serial_write_mp_int (pk, &p->f0_share));
-  P_CHECK (serial_write_mp_int (pk, &p->g0_share));
-  P_CHECK (serial_write_mp_int (pk, &p->h0_share));
+  P_CHECK(serial_write_mp_int(pk, &p->f0_share));
+  P_CHECK(serial_write_mp_int(pk, &p->g0_share));
+  P_CHECK(serial_write_mp_int(pk, &p->h0_share));
 
-  P_CHECK (msgpack_pack_int (pk, p->for_server));
+  P_CHECK(msgpack_pack_int(pk, p->for_server));
 
   switch (p->for_server) {
     case PRIO_SERVER_A:
-      P_CHECK (serial_write_server_a_data (pk, &p->shares.A));
+      P_CHECK(serial_write_server_a_data(pk, &p->shares.A));
       break;
     case PRIO_SERVER_B:
-      P_CHECK (serial_write_server_b_data (pk, &p->shares.B));
+      P_CHECK(serial_write_server_b_data(pk, &p->shares.B));
       break;
     default:
       return SECFailure;
@@ -297,146 +297,144 @@ cleanup:
   return rv;
 }
 
-SECStatus 
-serial_read_server_id (msgpack_unpacker *upk, PrioServerId *s)
+SECStatus
+serial_read_server_id(msgpack_unpacker* upk, PrioServerId* s)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (s != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(s != NULL);
 
   int serv;
-  P_CHECK (serial_read_int (upk, &serv));
-  P_CHECKCB (serv == PRIO_SERVER_A || serv == PRIO_SERVER_B);
+  P_CHECK(serial_read_int(upk, &serv));
+  P_CHECKCB(serv == PRIO_SERVER_A || serv == PRIO_SERVER_B);
   *s = serv;
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-serial_read_packet_client (msgpack_unpacker *upk, PrioPacketClient p,
-    const_PrioConfig cfg)
+SECStatus
+serial_read_packet_client(msgpack_unpacker* upk, PrioPacketClient p,
+                          const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(p != NULL);
   msgpack_unpacked res;
-  msgpack_unpacked_init (&res);
-  UP_CHECK (msgpack_unpacker_next (upk, &res))
+  msgpack_unpacked_init(&res);
+  UP_CHECK(msgpack_unpacker_next(upk, &res))
 
   msgpack_object obj = res.data;
-  P_CHECKCB (obj.type == MSGPACK_OBJECT_STR);
+  P_CHECKCB(obj.type == MSGPACK_OBJECT_STR);
 
   msgpack_object_str s = obj.via.str;
-  P_CHECKCB (s.size == cfg->batch_id_len);
-  P_CHECKCB (!memcmp (s.ptr, (char *)cfg->batch_id, cfg->batch_id_len));
+  P_CHECKCB(s.size == cfg->batch_id_len);
+  P_CHECKCB(!memcmp(s.ptr, (char*)cfg->batch_id, cfg->batch_id_len));
 
-  P_CHECK (serial_read_beaver_triple (upk, p->triple, &cfg->modulus));
+  P_CHECK(serial_read_beaver_triple(upk, p->triple, &cfg->modulus));
 
-  P_CHECK (serial_read_mp_int (upk, &p->f0_share, &cfg->modulus));
-  P_CHECK (serial_read_mp_int (upk, &p->g0_share, &cfg->modulus));
-  P_CHECK (serial_read_mp_int (upk, &p->h0_share, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->f0_share, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->g0_share, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->h0_share, &cfg->modulus));
 
-  P_CHECK (serial_read_server_id (upk, &p->for_server));
+  P_CHECK(serial_read_server_id(upk, &p->for_server));
 
   switch (p->for_server) {
     case PRIO_SERVER_A:
-      P_CHECK (serial_read_server_a_data (upk, &p->shares.A, cfg));
+      P_CHECK(serial_read_server_a_data(upk, &p->shares.A, cfg));
       break;
     case PRIO_SERVER_B:
-      P_CHECK (serial_read_server_b_data (upk, &p->shares.B));
+      P_CHECK(serial_read_server_b_data(upk, &p->shares.B));
       break;
     default:
       return SECFailure;
   }
 
 cleanup:
-  msgpack_unpacked_destroy (&res);
+  msgpack_unpacked_destroy(&res);
   return rv;
 }
 
-
-SECStatus 
-PrioPacketVerify1_write (const_PrioPacketVerify1 p, msgpack_packer *pk)
+SECStatus
+PrioPacketVerify1_write(const_PrioPacketVerify1 p, msgpack_packer* pk)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(p != NULL);
 
-  P_CHECK (serial_write_mp_int (pk, &p->share_d));
-  P_CHECK (serial_write_mp_int (pk, &p->share_e));
+  P_CHECK(serial_write_mp_int(pk, &p->share_d));
+  P_CHECK(serial_write_mp_int(pk, &p->share_e));
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-PrioPacketVerify1_read (PrioPacketVerify1 p, msgpack_unpacker *upk,
-    const_PrioConfig cfg)
+SECStatus
+PrioPacketVerify1_read(PrioPacketVerify1 p, msgpack_unpacker* upk,
+                       const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(p != NULL);
 
-  P_CHECK (serial_read_mp_int (upk, &p->share_d, &cfg->modulus));
-  P_CHECK (serial_read_mp_int (upk, &p->share_e, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->share_d, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->share_e, &cfg->modulus));
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-PrioPacketVerify2_write (const_PrioPacketVerify2 p, msgpack_packer *pk)
+SECStatus
+PrioPacketVerify2_write(const_PrioPacketVerify2 p, msgpack_packer* pk)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (pk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECKCB(p != NULL);
 
-  P_CHECK (serial_write_mp_int (pk, &p->share_out));
+  P_CHECK(serial_write_mp_int(pk, &p->share_out));
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-PrioPacketVerify2_read (PrioPacketVerify2 p, msgpack_unpacker *upk,
-    const_PrioConfig cfg)
+SECStatus
+PrioPacketVerify2_read(PrioPacketVerify2 p, msgpack_unpacker* upk,
+                       const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (upk != NULL);
-  P_CHECKCB (p != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECKCB(p != NULL);
 
-  P_CHECK (serial_read_mp_int (upk, &p->share_out, &cfg->modulus));
+  P_CHECK(serial_read_mp_int(upk, &p->share_out, &cfg->modulus));
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-PrioTotalShare_write (const_PrioTotalShare t, msgpack_packer *pk)
+SECStatus
+PrioTotalShare_write(const_PrioTotalShare t, msgpack_packer* pk)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (t != NULL);
-  P_CHECKCB (pk != NULL);
-  P_CHECK (msgpack_pack_int (pk, t->idx));
-  P_CHECK (serial_write_mp_array (pk, t->data_shares));
+  P_CHECKCB(t != NULL);
+  P_CHECKCB(pk != NULL);
+  P_CHECK(msgpack_pack_int(pk, t->idx));
+  P_CHECK(serial_write_mp_array(pk, t->data_shares));
 
 cleanup:
   return rv;
 }
 
-SECStatus 
-PrioTotalShare_read (PrioTotalShare t, msgpack_unpacker *upk, 
-    const_PrioConfig cfg)
+SECStatus
+PrioTotalShare_read(PrioTotalShare t, msgpack_unpacker* upk,
+                    const_PrioConfig cfg)
 {
   SECStatus rv = SECSuccess;
-  P_CHECKCB (t != NULL);
-  P_CHECKCB (upk != NULL);
-  P_CHECK (serial_read_server_id (upk, &t->idx));
-  P_CHECK (serial_read_mp_array (upk, t->data_shares, cfg->num_data_fields,
-        &cfg->modulus));
+  P_CHECKCB(t != NULL);
+  P_CHECKCB(upk != NULL);
+  P_CHECK(serial_read_server_id(upk, &t->idx));
+  P_CHECK(serial_read_mp_array(upk, t->data_shares, cfg->num_data_fields,
+                               &cfg->modulus));
 
 cleanup:
   return rv;
 }
-
