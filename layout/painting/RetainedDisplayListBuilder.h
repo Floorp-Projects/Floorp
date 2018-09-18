e7e6a10f9425e3ff2a8ec1c38e4bcbf7d7869c78
Ciure Andrei <aciure@mozilla.com>
2018-09-18 10:39:23 +0300

    Backed out 2 changesets (bug 1488599) for causing crashes a=backout
    Backed out changeset b08b9f2693cd (bug 1488599)
    Backed out changeset bba3a8028837 (bug 1488599)

--- layout/painting/RetainedDisplayListBuilder.h
+++ layout/painting/RetainedDisplayListBuilder.h
@@ -9,77 +9,10 @@
 
 #include "nsDisplayList.h"
 #include "mozilla/Maybe.h"
-#include "mozilla/TypedEnumBits.h"
 
 namespace mozilla {
 class DisplayListChecker;
 } // namespace mozilla
-
-
-/**
- * RetainedDisplayListData contains frame invalidation information. It is stored
- * in root frames, and used by RetainedDisplayListBuilder.
- * Currently this is implemented as a map of frame pointers to flags.
- */
-struct RetainedDisplayListData
-{
-  NS_DECLARE_FRAME_PROPERTY_DELETABLE(DisplayListData, RetainedDisplayListData)
-
-  enum class FrameFlags : uint8_t
-  {
-    None = 0,
-    Modified = 1 << 0,
-    HasProps = 1 << 1,
-    HadWillChange = 1 << 2
-  };
-
-  /**
-   * Removes all the frames from this RetainedDisplayListData.
-   */
-  void Clear() { mFrames.Clear(); }
-
-  /**
-   * Returns a mutable reference to flags set for the given |aFrame|. If the
-   * frame does not exist in this RetainedDisplayListData, it is added with
-   * default constructible flags FrameFlags::None.
-   */
-  FrameFlags& Flags(nsIFrame* aFrame) { return mFrames.GetOrInsert(aFrame); }
-
-  /**
-   * Returns flags set for the given |aFrame|, or FrameFlags::None if the frame
-   * is not in this RetainedDisplayListData.
-   */
-  FrameFlags GetFlags(nsIFrame* aFrame) const { return mFrames.Get(aFrame); }
-
-  /**
-   * Returns an iterator to the underlying frame storage.
-   */
-  auto Iterator() { return mFrames.Iter(); }
-
-  /**
-   * Removes the given |aFrame| from this RetainedDisplayListData.
-   */
-  bool Remove(nsIFrame* aFrame) { return mFrames.Remove(aFrame); }
-
-private:
-  nsDataHashtable<nsPtrHashKey<nsIFrame>, FrameFlags> mFrames;
-};
-
-MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(RetainedDisplayListData::FrameFlags)
-
-/**
- * Returns RetainedDisplayListData property for the given |aRootFrame|, or
- * nullptr if the property is not set.
- */
-RetainedDisplayListData*
-GetRetainedDisplayListData(nsIFrame* aRootFrame);
-
-/**
- * Returns RetainedDisplayListData property for the given |aRootFrame|. Creates
- * and sets a new RetainedDisplayListData property if it is not already set.
- */
-RetainedDisplayListData*
-GetOrSetRetainedDisplayListData(nsIFrame* aRootFrame);
 
 struct RetainedDisplayListBuilder
 {
