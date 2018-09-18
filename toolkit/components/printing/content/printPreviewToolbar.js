f3c1f28cb1e1329641c6f90d4929cfb0abc17939
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-09-18 07:50:59 +0000

    Bug 1474852 - Removed obsolete icon attributes from toolkit r=dao
    Differential Revision: https://phabricator.services.mozilla.com/D6053

--- toolkit/components/printing/content/printPreviewToolbar.js
+++ toolkit/components/printing/content/printPreviewToolbar.js
@@ -16,7 +16,7 @@ customElements.define("printpreview-toolbar", class PrintPreviewToolbar extends 
   connectedCallback() {
     window.addEventListener("unload", this.disconnectedCallback, { once: true });
     this.appendChild(MozXULElement.parseXULToFragment(`
-      <button id="print-preview-print" label="&print.label;" accesskey="&print.accesskey;" oncommand="this.parentNode.print();" icon="print"/>
+      <button id="print-preview-print" label="&print.label;" accesskey="&print.accesskey;" oncommand="this.parentNode.print();"/>
       <button id="print-preview-pageSetup" label="&pageSetup.label;" accesskey="&pageSetup.accesskey;" oncommand="this.parentNode.doPageSetup();"/>
       <vbox align="center" pack="center">
         <label value="&page.label;" accesskey="&page.accesskey;" control="print-preview-pageNumber"/>
@@ -63,7 +63,7 @@ customElements.define("printpreview-toolbar", class PrintPreviewToolbar extends 
       <toolbarseparator class="toolbarseparator-primary"/>
       <checkbox id="print-preview-simplify" label="&simplifyPage.label;" checked="false" disabled="true" accesskey="&simplifyPage.accesskey;" tooltiptext-disabled="&simplifyPage.disabled.tooltip;" tooltiptext-enabled="&simplifyPage.enabled.tooltip;" oncommand="this.parentNode.simplify();"/>
       <toolbarseparator class="toolbarseparator-primary"/>
-      <button label="&close.label;" accesskey="&close.accesskey;" oncommand="PrintUtils.exitPrintPreview();" icon="close"/>
+      <button label="&close.label;" accesskey="&close.accesskey;" oncommand="PrintUtils.exitPrintPreview();"/>
       <data id="print-preview-prompt-title" value="&customPrompt.title;"/>
     `, [
       "chrome://global/locale/printPreview.dtd",
