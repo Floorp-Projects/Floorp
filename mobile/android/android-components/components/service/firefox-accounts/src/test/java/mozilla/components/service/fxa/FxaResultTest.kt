09a03ca3f20dd3bf1b3efb27f8ed8bbe2a9a2117
Thom Chiovoloni <tchiovoloni@mozilla.com>
2018-09-19 11:58:19 -0700

    [components] Add 'async' versions of each FxaResult test

--- mobile/android/android-components/components/service/firefox-accounts/src/test/java/mozilla/components/service/fxa/FxaResultTest.kt
+++ mobile/android/android-components/components/service/firefox-accounts/src/test/java/mozilla/components/service/fxa/FxaResultTest.kt
@@ -25,6 +25,22 @@ class FxaResultTest {
     }
 
     @Test
+    fun thenWithResultAsync() {
+        var chainComplete = false
+        val result = FxaResult<Int>()
+
+        result.then { value: Int ->
+            assertEquals(value, 42)
+            chainComplete = true
+            FxaResult<Void>()
+        }
+
+        result.complete(42)
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
     fun thenWithException() {
         var chainComplete = false
 
@@ -36,6 +52,26 @@ class FxaResultTest {
             chainComplete = true
             FxaResult()
         })
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun thenWithExceptionAsync() {
+        var chainComplete = false
+
+        val result = FxaResult<Void>()
+
+        result.then({ value: Void ->
+            assertNull("valueListener should not be called", value)
+            FxaResult<Void>()
+        }, { value: Exception ->
+            assertEquals(value.message, "exception message")
+            chainComplete = true
+            FxaResult()
+        })
+
+        result.completeExceptionally(Exception("exception message"))
 
         assertTrue(chainComplete)
     }
@@ -55,6 +91,29 @@ class FxaResultTest {
             chainComplete = true
             FxaResult()
         })
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun thenWithThrownExceptionAsync() {
+        var chainComplete = false
+
+        val result = FxaResult<Int>()
+
+        result.then { _: Int ->
+            throw Exception("exception message")
+            FxaResult<Void>()
+        }.then({
+            fail("valueListener should not be called")
+            FxaResult<Void>()
+        }, { value: Exception ->
+            assertEquals(value.message, "exception message")
+            chainComplete = true
+            FxaResult()
+        })
+
+        result.complete(42)
 
         assertTrue(chainComplete)
     }
@@ -82,6 +141,96 @@ class FxaResultTest {
     }
 
     @Test
+    fun resultChainingAsyncInitial() {
+        var chainComplete = false
+
+        val result = FxaResult<Int>()
+
+        result.then { value: Int ->
+            assertEquals(value, 42)
+            FxaResult.fromValue("string")
+        }.then { value: String ->
+            assertEquals(value, "string")
+            FxaResult.fromException<Void>(Exception("exception message"))
+        }.then({
+            fail("valueListener should not be called")
+            FxaResult<Void>()
+        }, { value: Exception ->
+            assertEquals(value.message, "exception message")
+            chainComplete = true
+            FxaResult()
+        })
+
+        result.complete(42)
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun resultChainingAsyncInOrder() {
+        var chainComplete = false
+
+        val resultInt = FxaResult<Int>()
+        val resultString = FxaResult<String>()
+        val resultError = FxaResult<Void>()
+
+        resultInt.then { value: Int ->
+            assertEquals(value, 42)
+            resultString
+        }.then { value: String ->
+            assertEquals(value, "string")
+            resultError
+        }.then({
+            fail("valueListener should not be called")
+            FxaResult<Void>()
+        }, { value: Exception ->
+            assertEquals(value.message, "exception message")
+            chainComplete = true
+            FxaResult()
+        })
+
+        resultInt.complete(42)
+
+        resultString.complete("string")
+
+        resultError.completeExceptionally(Exception("exception message"))
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun resultChainingAsyncOutOfOrder() {
+        var chainComplete = false
+
+        val resultInt = FxaResult<Int>()
+        val resultString = FxaResult<String>()
+        val resultError = FxaResult<Void>()
+
+        resultInt.then { value: Int ->
+            assertEquals(value, 42)
+            resultString
+        }.then { value: String ->
+            assertEquals(value, "string")
+            resultError
+        }.then({
+            fail("valueListener should not be called")
+            FxaResult<Void>()
+        }, { value: Exception ->
+            assertEquals(value.message, "exception message")
+            chainComplete = true
+            FxaResult()
+        })
+
+        resultError.completeExceptionally(Exception("exception message"))
+
+        resultString.complete("string")
+
+        resultInt.complete(42)
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
     fun whenComplete() {
         var chainComplete = false
 
@@ -96,6 +245,21 @@ class FxaResultTest {
         assertTrue(chainComplete)
     }
 
+    @Test
+    fun whenCompleteAsync() {
+        var chainComplete = false
+        val result = FxaResult<Int>()
+        result.then { value: Int ->
+            assertEquals(value, 42)
+            FxaResult.fromValue("string")
+        }.whenComplete { value: String ->
+            assertEquals(value, "string")
+            chainComplete = true
+        }
+        result.complete(42)
+
+        assertTrue(chainComplete)
+    }
 
     @Test
     fun nullableResult() {
@@ -111,6 +275,26 @@ class FxaResultTest {
             assertEquals(value, 4)
             chainComplete = true
         }
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun nullableResultAsync() {
+        var chainComplete = false
+
+        val nullableAndNull: FxaResult<Int?> = FxaResult()
+
+        nullableAndNull.then { value ->
+            assertEquals(value, null)
+            val nullableButExists: FxaResult<Int?> = FxaResult.fromValue(4)
+            nullableButExists
+        }.whenComplete { value ->
+            assertEquals(value, 4)
+            chainComplete = true
+        }
+
+        nullableAndNull.complete(null)
 
         assertTrue(chainComplete)
     }
@@ -138,6 +322,33 @@ class FxaResultTest {
     }
 
     @Test
+    fun exceptionChainSkipsMissingAsync() {
+        var chainComplete = false
+        val resultInt = FxaResult<Int>()
+        val resultErr = FxaResult<String>()
+
+        resultInt.then { value: Int ->
+            assertEquals(value, 42)
+            resultErr
+        }.then { value: String ->
+            fail("Shouldn't call onValue (1)")
+            FxaResult.fromValue("dummy 1")
+        }.then({ value: String ->
+            fail("Shouldn't call onValue (2)")
+            FxaResult.fromValue("dummy 2")
+        }, { value: Exception ->
+            assertEquals(value.message, "test")
+            chainComplete = true
+            FxaResult()
+        })
+
+        resultInt.complete(42)
+        resultErr.completeExceptionally(Exception("test"))
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
     fun exceptionChainHandlesThrows() {
         var chainComplete = false
 
@@ -157,6 +368,32 @@ class FxaResultTest {
             assertEquals(value, "value")
             chainComplete = true
         }
+
+        assertTrue(chainComplete)
+    }
+
+    @Test
+    fun exceptionChainHandlesThrowsAsync() {
+        var chainComplete = false
+        val resultErr = FxaResult<Int>()
+        resultErr.then({ value: Int ->
+            fail("Shouldn't call onValue (1)")
+            FxaResult.fromValue("dummy value 0")
+        }, { error ->
+            assertEquals(error.message, "test 1")
+            throw Exception("test 2")
+        }).then({
+            fail("Shouldn't call onValue (2)")
+            FxaResult.fromValue("dummy value 1")
+        }, { error ->
+            assertEquals(error.message, "test 2")
+            FxaResult.fromValue("value")
+        }).whenComplete { value ->
+            assertEquals(value, "value")
+            chainComplete = true
+        }
+
+        resultErr.completeExceptionally(Exception("test 1"))
 
         assertTrue(chainComplete)
     }
