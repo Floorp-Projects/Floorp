32fef2da52ad4b430d925a4dead08e3d5584d3cc
Matthew Gaudet <mgaudet@mozilla.com>
2018-09-19 23:16:15 +0000

    Bug 1462081 - Reorder ICUpdatedStubs to improve 64 bit packing r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D6290

--- js/src/jit/BaselineIC.h
+++ js/src/jit/BaselineIC.h
@@ -905,16 +905,16 @@ class ICCacheIR_Monitored : public ICMonitoredStub
 class ICUpdatedStub : public ICStub
 {
   protected:
-    // Pointer to the start of the type updating stub chain.
-    ICStub* firstUpdateStub_;
-
     static const uint32_t MAX_OPTIMIZED_STUBS = 8;
     uint32_t numOptimizedStubs_;
 
+    // Pointer to the start of the type updating stub chain.
+    ICStub* firstUpdateStub_;
+
     ICUpdatedStub(Kind kind, JitCode* stubCode)
       : ICStub(kind, ICStub::Updated, stubCode),
-        firstUpdateStub_(nullptr),
-        numOptimizedStubs_(0)
+        numOptimizedStubs_(0),
+        firstUpdateStub_(nullptr)
     {}
 
   public:
