d3b588550fd2f6b998bd8a44c821d84dc0d7e0ea
Rob Wu <rob@robwu.nl>
2018-09-19 10:57:48 +0000

    Bug 1393570 - Support cookieStoreId in windows.create extension API r=Gijs,rpl
    Other (internal API) changes besides extension API changes:
    
    - This also introduces support for opening a window with multiple tabs
      in a non-default container tab.
    
    - This also adds LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL to the
      gBrowser.loadTabs call, unless allowInheritPrincipal is set.
      For backwards-compatibility, this flag defaults to true.
    
    Depends on D4928
    
    Differential Revision: https://phabricator.services.mozilla.com/D4929

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1433,6 +1433,7 @@ window._gBrowser = {
   },
 
   loadTabs(aURIs, {
+    allowInheritPrincipal,
     allowThirdPartyFixup,
     inBackground,
     newIndex,
@@ -1489,6 +1490,9 @@ window._gBrowser = {
         flags |= Ci.nsIWebNavigation.LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP |
           Ci.nsIWebNavigation.LOAD_FLAGS_FIXUP_SCHEME_TYPOS;
       }
+      if (!allowInheritPrincipal) {
+        flags |= Ci.nsIWebNavigation.LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL;
+      }
       try {
         browser.loadURI(aURIs[0], {
           flags,
@@ -1501,6 +1505,7 @@ window._gBrowser = {
       }
     } else {
       let params = {
+        allowInheritPrincipal,
         ownerTab: owner,
         skipAnimation: multiple,
         allowThirdPartyFixup,
@@ -1521,6 +1526,7 @@ window._gBrowser = {
     let tabNum = targetTabIndex;
     for (let i = 1; i < aURIs.length; ++i) {
       let params = {
+        allowInheritPrincipal,
         skipAnimation: true,
         allowThirdPartyFixup,
         postData: postDatas && postDatas[i],
