f8b4c7df40116d849339faec814c847a157b51f4
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-09-19 08:24:25 +0000

    Bug 1492101: Store a preference which includes extension existence after drag and drap instead of when closing toolbox. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D6225

--- devtools/client/framework/test/head.js
+++ devtools/client/framework/test/head.js
@@ -314,6 +314,16 @@ async function dndToolTab(toolbox, dragTarget, dropTarget, passedTargets = []) {
     EventUtils.synthesizeMouse(containerEl, 0, 0,
                                { type: "mouseout" }, containerEl.ownerGlobal);
   }
+
+  // Wait for updating the preference.
+  await new Promise(resolve => {
+    const onUpdated = () => {
+      Services.prefs.removeObserver("devtools.toolbox.tabsOrder", onUpdated);
+      resolve();
+    };
+
+    Services.prefs.addObserver("devtools.toolbox.tabsOrder", onUpdated);
+  });
 }
 
 function assertToolTabOrder(toolbox, expectedOrder) {
