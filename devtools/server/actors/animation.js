728e35a9902cc08457a0a3753f2373e1e3f9583b
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-09-19 02:03:49 +0000

    Bug 1490583: Remove actor related code that no longer used. r=pbro
    Differential Revision: https://phabricator.services.mozilla.com/D5842

--- devtools/server/actors/animation.js
+++ devtools/server/actors/animation.js
@@ -295,15 +295,6 @@ var AnimationPlayerActor = protocol.ActorClassWithSpec(animationPlayerSpec, {
    * @return {Object}
    */
   getState: function() {
-    // Remember the startTime each time getState is called, it may be useful
-    // when animations get paused. As in, when an animation gets paused, its
-    // startTime goes back to null, but the front-end might still be interested
-    // in knowing what the previous startTime was. So everytime it is set,
-    // remember it and send it along with the newState.
-    if (this.player.startTime) {
-      this.previousStartTime = this.player.startTime;
-    }
-
     // Note that if you add a new property to the state object, make sure you
     // add the corresponding property in the AnimationPlayerFront' initialState
     // getter.
@@ -311,7 +302,6 @@ var AnimationPlayerActor = protocol.ActorClassWithSpec(animationPlayerSpec, {
       type: this.getType(),
       // startTime is null whenever the animation is paused or waiting to start.
       startTime: this.player.startTime,
-      previousStartTime: this.previousStartTime,
       currentTime: this.player.currentTime,
       playState: this.player.playState,
       playbackRate: this.player.playbackRate,
@@ -414,76 +404,6 @@ var AnimationPlayerActor = protocol.ActorClassWithSpec(animationPlayerSpec, {
     if (hasChanged) {
       this.emit("changed", this.getCurrentState());
     }
-  },
-
-  /**
-   * Pause the player.
-   */
-  pause: function() {
-    this.player.pause();
-    return this.player.ready;
-  },
-
-  /**
-   * Play the player.
-   * This method only returns when the animation has left its pending state.
-   */
-  play: function() {
-    this.player.play();
-    return this.player.ready;
-  },
-
-  /**
-   * Simply exposes the player ready promise.
-   *
-   * When an animation is created/paused then played, there's a short time
-   * during which its playState is pending, before being set to running.
-   *
-   * If you either created a new animation using the Web Animations API or
-   * paused/played an existing one, and then want to access the playState, you
-   * might be interested to call this method.
-   * This is especially important for tests.
-   */
-  ready: function() {
-    return this.player.ready;
-  },
-
-  /**
-   * Set the current time of the animation player.
-   */
-  setCurrentTime: function(currentTime) {
-    // The spec is that the progress of animation is changed
-    // if the time of setCurrentTime is during the endDelay.
-    // We should prevent the time
-    // to make the same animation behavior as the original.
-    // Likewise, in case the time is less than 0.
-    const timing = this.player.effect.getComputedTiming();
-    if (timing.delay < 0) {
-      currentTime += timing.delay;
-    }
-    if (currentTime < 0) {
-      currentTime = 0;
-    } else if (currentTime * this.player.playbackRate > timing.endTime) {
-      currentTime = timing.endTime;
-    }
-    this.player.currentTime = currentTime * this.player.playbackRate;
-  },
-
-  /**
-   * Set the playback rate of the animation player.
-   */
-  setPlaybackRate: function(playbackRate) {
-    this.player.updatePlaybackRate(playbackRate);
-    return this.player.ready;
-  },
-
-  /**
-   * Get data about the keyframes of this animation player.
-   * @return {Object} Returns a list of frames, each frame containing the list
-   * animated properties as well as the frame's offset.
-   */
-  getFrames: function() {
-    return this.player.effect.getKeyframes();
   },
 
   /**
@@ -810,53 +730,6 @@ exports.AnimationsActor = protocol.ActorClassWithSpec(animationsSpec, {
   },
 
   /**
-   * Pause all animations in the current targetActor's frames.
-   */
-  pauseAll: function() {
-    // Until the WebAnimations API provides a way to play/pause via the document
-    // timeline, we have to iterate through the whole DOM to find all players.
-    for (const player of
-         this.getAllAnimations(this.targetActor.window.document, true)) {
-      this.pauseSync(player);
-    }
-    this.allAnimationsPaused = true;
-  },
-
-  /**
-   * Play all animations in the current targetActor's frames.
-   * This method only returns when animations have left their pending states.
-   */
-  playAll: function() {
-    // Until the WebAnimations API provides a way to play/pause via the document
-    // timeline, we have to iterate through the whole DOM to find all players.
-    for (const player of
-      this.getAllAnimations(this.targetActor.window.document, true)) {
-      this.playSync(player);
-    }
-    this.allAnimationsPaused = false;
-  },
-
-  toggleAll: function() {
-    if (this.allAnimationsPaused) {
-      this.playAll();
-    } else {
-      this.pauseAll();
-    }
-  },
-
-  /**
-   * Toggle (play/pause) several animations at the same time.
-   * @param {Array} players A list of AnimationPlayerActor objects.
-   * @param {Boolean} shouldPause If set to true, the players will be paused,
-   * otherwise they will be played.
-   */
-  toggleSeveral: function(players, shouldPause) {
-    return Promise.all(players.map(player => {
-      return shouldPause ? player.pause() : player.play();
-    }));
-  },
-
-  /**
    * Pause given animations.
    *
    * @param {Array} actors A list of AnimationPlayerActor.
@@ -887,19 +760,8 @@ exports.AnimationsActor = protocol.ActorClassWithSpec(animationsSpec, {
    * @param {Array} players A list of AnimationPlayerActor.
    * @param {Number} time The new currentTime.
    * @param {Boolean} shouldPause Should the players be paused too.
-   * @param {Object} options
-   *                 - relativeToCreatedTime: Set current path with createdTime.
    */
-  setCurrentTimes: function(players, time, shouldPause, options) {
-    // For backward compatibility for old animation inspector.
-    // We can drop following procedures after dropping old one.
-    if (!options.relativeToCreatedTime) {
-      return Promise.all(players.map(player => {
-        const pause = shouldPause ? player.pause() : Promise.resolve();
-        return pause.then(() => player.setCurrentTime(time));
-      }));
-    }
-
+  setCurrentTimes: function(players, time, shouldPause) {
     for (const actor of players) {
       const player = actor.player;
 
@@ -915,13 +777,14 @@ exports.AnimationsActor = protocol.ActorClassWithSpec(animationsSpec, {
 
   /**
    * Set the playback rate of several animations at the same time.
-   * @param {Array} players A list of AnimationPlayerActor.
+   * @param {Array} actors A list of AnimationPlayerActor.
    * @param {Number} rate The new rate.
    */
   setPlaybackRates: function(players, rate) {
-    return Promise.all(
-      players.map(player => player.setPlaybackRate(rate))
-    );
+    return Promise.all(players.map(({ player }) => {
+      player.updatePlaybackRate(rate);
+      return player.ready;
+    }));
   },
 
   /**
