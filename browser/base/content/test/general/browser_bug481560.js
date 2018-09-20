9e177a258415f40df5fe6c1b35433e6fabc16c27
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2018-09-20 14:17:32 +0000

    Bug 1492782 - modernize a few tests and reduce CPOW usage in browser/base/content/test/general/, r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D6375

--- browser/base/content/test/general/browser_bug481560.js
+++ browser/base/content/test/general/browser_bug481560.js
@@ -1,21 +1,16 @@
-function test() {
-  waitForExplicitFinish();
+add_task(async function testTabCloseShortcut() {
+  let win = await BrowserTestUtils.openNewBrowserWindow();
+  await SimpleTest.promiseFocus(win);
 
-  whenNewWindowLoaded(null, function(win) {
-    waitForFocus(function() {
-      function onTabClose() {
-        ok(false, "shouldn't have gotten the TabClose event for the last tab");
-      }
-      var tab = win.gBrowser.selectedTab;
-      tab.addEventListener("TabClose", onTabClose);
+  function onTabClose() {
+    ok(false, "shouldn't have gotten the TabClose event for the last tab");
+  }
+  var tab = win.gBrowser.selectedTab;
+  tab.addEventListener("TabClose", onTabClose);
 
-      EventUtils.synthesizeKey("w", { accelKey: true }, win);
+  EventUtils.synthesizeKey("w", { accelKey: true }, win);
 
-      ok(win.closed, "accel+w closed the window immediately");
+  ok(win.closed, "accel+w closed the window immediately");
 
-      tab.removeEventListener("TabClose", onTabClose);
-
-      finish();
-    }, win);
-  });
-}
+  tab.removeEventListener("TabClose", onTabClose);
+});
