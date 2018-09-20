8f5e3224043e79c9c4b5371fee57784d020f94d3
Ahilya Sinha <asinha@mozilla.com>
2018-09-20 12:07:58 +0000

    Bug 1473915 - Set up infra so we can move the wpt-manifest out of tree r=gps
    Changes the wpt manifest path to the topobjdir instead so it can be moved out of tree.
    Other changes so that the manifest download and update, and |mach wpt| and |mach test <wpt-test>| work with the new path.
    The manifest is also downloaded and updated when creating the tests-archive to ensure that it exists when we run tests on TC.
    
    MozReview-Commit-ID: Fp6UsKJjhTU
    
    Differential Revision: https://phabricator.services.mozilla.com/D5312
    
    --HG--
    extra : moz-landing-system : lando

--- testing/testsuite-targets.mk
+++ testing/testsuite-targets.mk
@@ -153,8 +153,11 @@ endif
 package-tests-prepare-dest:
 	$(NSINSTALL) -D $(test_archive_dir)
 
+download-wpt-manifest:
+	$(call py_action,download_wpt_manifest)
+
 define package_archive
-package-tests-$(1): stage-all package-tests-prepare-dest
+package-tests-$(1): stage-all package-tests-prepare-dest download-wpt-manifest
 	$$(call py_action,test_archive, \
 		$(1) \
 		'$$(abspath $$(test_archive_dir))/$$(PKG_BASENAME).$(1).tests.$(2)')
@@ -268,6 +271,7 @@ check::
   xpcshell-tests \
   jstestbrowser \
   package-tests \
+  download-wpt-manifest \
   package-tests-prepare-dest \
   package-tests-common \
   make-stage-dir \
