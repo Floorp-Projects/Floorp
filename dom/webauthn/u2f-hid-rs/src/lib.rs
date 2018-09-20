ce0bc556efa11c75d9faa0f0373a7f0007d725f6
J.C. Jones <jjones@mozilla.com>
2018-09-20 22:04:15 +0000

    Bug 1492973 - Update u2f-hid-rs to 0.2.1 r=froydnj
    Moves winapi dependency to 0.3.5 and updates rust-fmt
    
    Differential Revision: https://phabricator.services.mozilla.com/D6436

--- dom/webauthn/u2f-hid-rs/src/lib.rs
+++ dom/webauthn/u2f-hid-rs/src/lib.rs
@@ -36,11 +36,12 @@ pub mod platform;
 #[path = "windows/mod.rs"]
 pub mod platform;
 
-#[cfg(
-    not(
-        any(target_os = "linux", target_os = "freebsd", target_os = "macos", target_os = "windows")
-    )
-)]
+#[cfg(not(any(
+    target_os = "linux",
+    target_os = "freebsd",
+    target_os = "macos",
+    target_os = "windows"
+)))]
 #[path = "stub/mod.rs"]
 pub mod platform;
 
