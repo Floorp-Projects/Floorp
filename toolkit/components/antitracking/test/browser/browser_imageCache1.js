7f1812ab691fef8544f3252ded4edd530e919142
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-20 15:45:52 +0000

    Bug 1491061 - Part 2: Add tests to ensure that third-party cookie blocking restrictions under content blocking also follow UI prefs r=baku
    Depends on D5887
    
    Differential Revision: https://phabricator.services.mozilla.com/D5888

--- toolkit/components/antitracking/test/browser/browser_imageCache1.js
+++ toolkit/components/antitracking/test/browser/browser_imageCache1.js
@@ -2,7 +2,10 @@ ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 let cookieBehavior = BEHAVIOR_REJECT_TRACKER;
 let blockingByContentBlocking = false;
+let blockingByContentBlockingUI = true;
+let blockingByContentBlockingRTUI = true;
 let blockingByAllowList = false;
+let expectedBlockingNotifications = true;
 
 let rootDir = getRootDirectory(gTestPath);
 let jar = getJar(rootDir);
