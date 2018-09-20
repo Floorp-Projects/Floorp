16d4c1c5e8c532a1a59f89b085a80471e280baed
yulia <ystartsev@mozilla.com>
2018-09-20 13:41:26 +0000

    Bug 1485378 - Replace CallWatcher actor with helper; r=jdescottes
    creates a CallWatcherActor only for tests and migrates other functionality to the helper
    
    Differential Revision: https://phabricator.services.mozilla.com/D5534

---
+++ devtools/client/canvasdebugger/test/call-watcher-actor.js
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const protocol = require("devtools/shared/protocol");
+const {
+  callWatcherSpec
+} = require("chrome://mochitests/content/browser/devtools/client/canvasdebugger/test/call-watcher-spec");
+const {CallWatcher} = require("devtools/server/actors/utils/call-watcher");
+
+/**
+ * This actor observes function calls on certain objects or globals.
+ * It wraps the CallWatcher Helper so that it can be observed by tests
+ */
+exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
+  initialize: function(conn, targetActor) {
+    protocol.Actor.prototype.initialize.call(this, conn);
+    CallWatcher.call(this, conn, targetActor);
+  },
+  destroy: function(conn) {
+    protocol.Actor.prototype.destroy.call(this, conn);
+    this.finalize();
+  },
+  ...CallWatcher.prototype,
+});
--- devtools/server/actors/utils/call-watcher.js
+++ devtools/server/actors/utils/call-watcher.js
@@ -6,247 +6,35 @@
 /* global XPCNativeWrapper */
 
 const {Cu} = require("chrome");
-const protocol = require("devtools/shared/protocol");
 const ChromeUtils = require("ChromeUtils");
 // base-loader.js is a JSM without .jsm file extension, so it has to be loaded
 // via ChromeUtils.import and not require() which would consider it as a CommonJS module
 const {serializeStack, parseStack} = ChromeUtils.import("resource://devtools/shared/base-loader.js", {});
 
-const { functionCallSpec, callWatcherSpec } = require("devtools/shared/specs/call-watcher");
-const { CallWatcherFront } = require("devtools/shared/fronts/call-watcher");
+const {
+  METHOD_FUNCTION,
+  GETTER_FUNCTION,
+  SETTER_FUNCTION
+} = require("devtools/shared/fronts/function-call");
+
+const { FunctionCallActor } = require("devtools/server/actors/utils/function-call");
 
 /**
- * This actor contains information about a function call, like the function
- * type, name, stack, arguments, returned value etc.
+ * This helper observes function calls on certain objects or globals.
  */
-var FunctionCallActor = protocol.ActorClassWithSpec(functionCallSpec, {
-  /**
-   * Creates the function call actor.
-   *
-   * @param DebuggerServerConnection conn
-   *        The server connection.
-   * @param DOMWindow window
-   *        The content window.
-   * @param string global
-   *        The name of the global object owning this function, like
-   *        "CanvasRenderingContext2D" or "WebGLRenderingContext".
-   * @param object caller
-   *        The object owning the function when it was called.
-   *        For example, in `foo.bar()`, the caller is `foo`.
-   * @param number type
-   *        Either METHOD_FUNCTION, METHOD_GETTER or METHOD_SETTER.
-   * @param string name
-   *        The called function's name.
-   * @param array stack
-   *        The called function's stack, as a list of { name, file, line } objects.
-   * @param number timestamp
-   *        The performance.now() timestamp when the function was called.
-   * @param array args
-   *        The called function's arguments.
-   * @param any result
-   *        The value returned by the function call.
-   * @param boolean holdWeak
-   *        Determines whether or not FunctionCallActor stores a weak reference
-   *        to the underlying objects.
-   */
-  initialize: function(
-    conn,
-    [window, global, caller, type, name, stack, timestamp, args, result],
-    holdWeak
-  ) {
-    protocol.Actor.prototype.initialize.call(this, conn);
+function CallWatcher(conn, targetActor) {
+  this.targetActor = targetActor;
+  this.conn = conn;
+  this._onGlobalCreated = this._onGlobalCreated.bind(this);
+  this._onGlobalDestroyed = this._onGlobalDestroyed.bind(this);
+  this._onContentFunctionCall = this._onContentFunctionCall.bind(this);
+  this.targetActor.on("window-ready", this._onGlobalCreated);
+  this.targetActor.on("window-destroyed", this._onGlobalDestroyed);
+}
 
-    this.details = {
-      global: global,
-      type: type,
-      name: name,
-      stack: stack,
-      timestamp: timestamp
-    };
+exports.CallWatcher = CallWatcher;
 
-    // Store a weak reference to all objects so we don't
-    // prevent natural GC if `holdWeak` was passed into
-    // setup as truthy.
-    if (holdWeak) {
-      const weakRefs = {
-        window: Cu.getWeakReference(window),
-        caller: Cu.getWeakReference(caller),
-        args: Cu.getWeakReference(args),
-        result: Cu.getWeakReference(result),
-      };
-
-      Object.defineProperties(this.details, {
-        window: { get: () => weakRefs.window.get() },
-        caller: { get: () => weakRefs.caller.get() },
-        args: { get: () => weakRefs.args.get() },
-        result: { get: () => weakRefs.result.get() },
-      });
-    } else {
-      // Otherwise, hold strong references to the objects.
-      this.details.window = window;
-      this.details.caller = caller;
-      this.details.args = args;
-      this.details.result = result;
-    }
-
-    // The caller, args and results are string names for now. It would
-    // certainly be nicer if they were Object actors. Make this smarter, so
-    // that the frontend can inspect each argument, be it object or primitive.
-    // Bug 978960.
-    this.details.previews = {
-      caller: this._generateStringPreview(caller),
-      args: this._generateArgsPreview(args),
-      result: this._generateStringPreview(result)
-    };
-  },
-
-  /**
-   * Customize the marshalling of this actor to provide some generic information
-   * directly on the Front instance.
-   */
-  form: function() {
-    return {
-      actor: this.actorID,
-      type: this.details.type,
-      name: this.details.name,
-      file: this.details.stack[0].file,
-      line: this.details.stack[0].line,
-      timestamp: this.details.timestamp,
-      callerPreview: this.details.previews.caller,
-      argsPreview: this.details.previews.args,
-      resultPreview: this.details.previews.result
-    };
-  },
-
-  /**
-   * Gets more information about this function call, which is not necessarily
-   * available on the Front instance.
-   */
-  getDetails: function() {
-    const { type, name, stack, timestamp } = this.details;
-
-    // Since not all calls on the stack have corresponding owner files (e.g.
-    // callbacks of a requestAnimationFrame etc.), there's no benefit in
-    // returning them, as the user can't jump to the Debugger from them.
-    for (let i = stack.length - 1; ;) {
-      if (stack[i].file) {
-        break;
-      }
-      stack.pop();
-      i--;
-    }
-
-    // XXX: Use grips for objects and serialize them properly, in order
-    // to add the function's caller, arguments and return value. Bug 978957.
-    return {
-      type: type,
-      name: name,
-      stack: stack,
-      timestamp: timestamp
-    };
-  },
-
-  /**
-   * Serializes the arguments so that they can be easily be transferred
-   * as a string, but still be useful when displayed in a potential UI.
-   *
-   * @param array args
-   *        The source arguments.
-   * @return string
-   *         The arguments as a string.
-   */
-  _generateArgsPreview: function(args) {
-    const { global, name, caller } = this.details;
-
-    // Get method signature to determine if there are any enums
-    // used in this method.
-    let methodSignatureEnums;
-
-    const knownGlobal = CallWatcherFront.KNOWN_METHODS[global];
-    if (knownGlobal) {
-      const knownMethod = knownGlobal[name];
-      if (knownMethod) {
-        const isOverloaded = typeof knownMethod.enums === "function";
-        if (isOverloaded) {
-          methodSignatureEnums = knownMethod.enums(args);
-        } else {
-          methodSignatureEnums = knownMethod.enums;
-        }
-      }
-    }
-
-    const serializeArgs = () => args.map((arg, i) => {
-      // XXX: Bug 978960.
-      if (arg === undefined) {
-        return "undefined";
-      }
-      if (arg === null) {
-        return "null";
-      }
-      if (typeof arg == "function") {
-        return "Function";
-      }
-      if (typeof arg == "object") {
-        return "Object";
-      }
-      // If this argument matches the method's signature
-      // and is an enum, change it to its constant name.
-      if (methodSignatureEnums && methodSignatureEnums.has(i)) {
-        return getBitToEnumValue(global, caller, arg);
-      }
-      return arg + "";
-    });
-
-    return serializeArgs().join(", ");
-  },
-
-  /**
-   * Serializes the data so that it can be easily be transferred
-   * as a string, but still be useful when displayed in a potential UI.
-   *
-   * @param object data
-   *        The source data.
-   * @return string
-   *         The arguments as a string.
-   */
-  _generateStringPreview: function(data) {
-    // XXX: Bug 978960.
-    if (data === undefined) {
-      return "undefined";
-    }
-    if (data === null) {
-      return "null";
-    }
-    if (typeof data == "function") {
-      return "Function";
-    }
-    if (typeof data == "object") {
-      return "Object";
-    }
-    return data + "";
-  }
-});
-
-/**
- * This actor observes function calls on certain objects or globals.
- */
-exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
-  initialize: function(conn, targetActor) {
-    protocol.Actor.prototype.initialize.call(this, conn);
-    this.targetActor = targetActor;
-    this._onGlobalCreated = this._onGlobalCreated.bind(this);
-    this._onGlobalDestroyed = this._onGlobalDestroyed.bind(this);
-    this._onContentFunctionCall = this._onContentFunctionCall.bind(this);
-    this.targetActor.on("window-ready", this._onGlobalCreated);
-    this.targetActor.on("window-destroyed", this._onGlobalDestroyed);
-  },
-  destroy: function(conn) {
-    protocol.Actor.prototype.destroy.call(this, conn);
-    this.targetActor.off("window-ready", this._onGlobalCreated);
-    this.targetActor.off("window-destroyed", this._onGlobalDestroyed);
-    this.finalize();
-  },
-
+CallWatcher.prototype = {
   /**
    * Lightweight listener invoked whenever an instrumented function is called
    * while recording. We're doing this to avoid the event emitter overhead,
@@ -288,6 +76,8 @@ exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
    * actor is destroyed.
    */
   finalize: function() {
+    this.targetActor.off("window-ready", this._onGlobalCreated);
+    this.targetActor.off("window-destroyed", this._onGlobalDestroyed);
     if (!this._initialized) {
       return;
     }
@@ -397,7 +187,7 @@ exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
         }
 
         if (self._recording) {
-          const type = CallWatcherFront.METHOD_FUNCTION;
+          const type = METHOD_FUNCTION;
           const stack = getStack(name);
           const now = self.targetActor.window.performance.now();
           const timestamp = now - self._timestampEpoch;
@@ -431,7 +221,7 @@ exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
           const result = Cu.waiveXrays(originalGetter.apply(this, args));
 
           if (self._recording) {
-            const type = CallWatcherFront.GETTER_FUNCTION;
+            const type = GETTER_FUNCTION;
             const stack = getStack(name);
             const now = self.targetActor.window.performance.now();
             const timestamp = now - self._timestampEpoch;
@@ -447,7 +237,7 @@ exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
           originalSetter.apply(this, args);
 
           if (self._recording) {
-            const type = CallWatcherFront.SETTER_FUNCTION;
+            const type = SETTER_FUNCTION;
             const stack = getStack(name);
             const now = self.targetActor.window.performance.now();
             const timestamp = now - self._timestampEpoch;
@@ -549,64 +339,7 @@ exports.CallWatcherActor = protocol.ActorClassWithSpec(callWatcherSpec, {
       this.emit("call", functionCall);
     }
   }
-});
-
-/**
- * A lookup table for cross-referencing flags or properties with their name
- * assuming they look LIKE_THIS most of the time.
- *
- * For example, when gl.clear(gl.COLOR_BUFFER_BIT) is called, the actual passed
- * argument's value is 16384, which we want identified as "COLOR_BUFFER_BIT".
- */
-var gEnumRegex = /^[A-Z][A-Z0-9_]+$/;
-var gEnumsLookupTable = {};
-
-// These values are returned from errors, or empty values,
-// and need to be ignored when checking arguments due to the bitwise math.
-var INVALID_ENUMS = [
-  "INVALID_ENUM", "NO_ERROR", "INVALID_VALUE", "OUT_OF_MEMORY", "NONE"
-];
-
-function getBitToEnumValue(type, object, arg) {
-  let table = gEnumsLookupTable[type];
-
-  // If mapping not yet created, do it on the first run.
-  if (!table) {
-    table = gEnumsLookupTable[type] = {};
-
-    for (const key in object) {
-      if (key.match(gEnumRegex)) {
-        // Maps `16384` to `"COLOR_BUFFER_BIT"`, etc.
-        table[object[key]] = key;
-      }
-    }
-  }
-
-  // If a single bit value, just return it.
-  if (table[arg]) {
-    return table[arg];
-  }
-
-  // Otherwise, attempt to reduce it to the original bit flags:
-  // `16640` -> "COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT"
-  const flags = [];
-  for (let flag in table) {
-    if (INVALID_ENUMS.includes(table[flag])) {
-      continue;
-    }
-
-    // Cast to integer as all values are stored as strings
-    // in `table`
-    flag = flag | 0;
-    if (flag && (arg & flag) === flag) {
-      flags.push(table[flag]);
-    }
-  }
-
-  // Cache the combined bitmask value
-  table[arg] = flags.join(" | ") || arg;
-  return table[arg];
-}
+};
 
 /**
  * Creates a new error from an error that originated from content but was called
