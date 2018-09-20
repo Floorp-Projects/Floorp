d0800c709e5c73d5f824b06e1d135fe14f7c0487
Jared Wein <jwein@mozilla.com>
2018-09-20 17:13:26 +0000

    Bug 1491791 - Update the Save button state after the 'change' event. r=MattN
    The validity of the cc-number field is only updated to a failure state after the 'change' event in the EditCreditCard class, but there was no code within basic-card-form.js to update the Save button enabled state after the 'change' event had been handled by EditCreditCard.
    
    Differential Revision: https://phabricator.services.mozilla.com/D6282
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/payments/test/browser/browser_card_edit.js
+++ browser/components/payments/test/browser/browser_card_edit.js
@@ -172,6 +172,28 @@ async function add_link(aOptions = {}) {
 
     await verifyPersistCheckbox(frame, cardOptions);
 
+    await spawnPaymentDialogTask(frame, async function checkSaveButtonUpdatesOnCCNumberChange() {
+      let {
+        PaymentTestUtils: PTU,
+      } = ChromeUtils.import("resource://testing-common/PaymentTestUtils.jsm", {});
+
+      let button = content.document.querySelector(`basic-card-form button.primary`);
+      ok(!button.disabled, "Save button should not be disabled");
+
+      let field = content.document.getElementById("cc-number");
+      field.focus();
+      EventUtils.sendString("a", content.window);
+      button.focus();
+
+      ok(button.disabled, "Save button should be disabled with incorrect number");
+
+      field.focus();
+      content.fillField(field, PTU.BasicCards.JaneMasterCard["cc-number"]);
+      button.focus();
+
+      ok(!button.disabled, "Save button should be enabled with correct number");
+    });
+
     await spawnPaymentDialogTask(frame, PTU.DialogContentTasks.clickPrimaryButton);
 
     await spawnPaymentDialogTask(frame, async function waitForSummaryPage(testArgs = {}) {
