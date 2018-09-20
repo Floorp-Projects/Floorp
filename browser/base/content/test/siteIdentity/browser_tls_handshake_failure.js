661d36f66c09146cdc45a5a89232394a60ee7cb8
Dana Keeler <dkeeler@mozilla.com>
2018-09-20 20:57:38 +0000

    bug 1492424 - check if the TLS handshake failed in nsSecureBrowserUIImpl r=Gijs
    The site identity security indicator machinery treats connections where the TLS
    handshake failed as insecure (also referred to as "unknown identity"). Before
    bug 1468222, such cases were easily detectable as the SSLStatus field of the
    relevant nsITransportSecurityInfo would be null. When we merged nsISSLStatus
    into nsITransportSecurityInfo, we didn't take this differentiation into account.
    This patch brings back the prior behavior by checking if the securityInfo's
    securityState indicates that the handshake failed (i.e. it is
    STATE_IS_INSECURE).
    
    Differential Revision: https://phabricator.services.mozilla.com/D6316

---
+++ browser/base/content/test/siteIdentity/browser_tls_handshake_failure.js
@@ -0,0 +1,25 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests that the site identity indicator is properly updated for connections
+// where the TLS handshake fails.
+// See bug 1492424.
+
+add_task(async function() {
+  let rootURI = getRootDirectory(gTestPath).replace("chrome://mochitests/content",
+                                                    "https://example.com");
+  await BrowserTestUtils.withNewTab(rootURI + "dummy_page.html", async (browser) => {
+    let identityMode = window.document.getElementById("identity-box").className;
+    is(identityMode, "verifiedDomain", "identity should be secure before");
+
+    const TLS_HANDSHAKE_FAILURE_URI = "https://ssl3.example.com/";
+    // Try to connect to a server where the TLS handshake will fail.
+    browser.loadURI(TLS_HANDSHAKE_FAILURE_URI);
+    await BrowserTestUtils.browserLoaded(browser, false, TLS_HANDSHAKE_FAILURE_URI, true);
+
+    let newIdentityMode = window.document.getElementById("identity-box").className;
+    is(newIdentityMode, "unknownIdentity", "identity should be unknown (not secure) after");
+  });
+});
