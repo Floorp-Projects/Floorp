7bc68cc81904341b74bbd6ba53925b7a980aae00
Markus Stange <mstange@themasta.com>
2018-09-21 03:38:39 +0000

    Bug 1483616 - Ignore optional m specifier in PUBLIC and FUNC lines. r=kmag
    These specifiers were added in
    https://github.com/google/breakpad/commit/b1226959a25b6a5311801d6f204b088c706e7c25
    
    Differential Revision: https://phabricator.services.mozilla.com/D6455

--- browser/components/extensions/ParseBreakpadSymbols-worker.js
+++ browser/components/extensions/ParseBreakpadSymbols-worker.js
@@ -15,7 +15,7 @@ function parse(text) {
   // PUBLIC 3fc74 0 test_public_symbol
   //
   // FUNC 40330 8e 0 test_func_symbol
-  const symbolRegex = /\nPUBLIC ([0-9a-f]+) [0-9a-f]+ (.*)|\nFUNC ([0-9a-f]+) [0-9a-f]+ [0-9a-f]+ (.*)/g;
+  const symbolRegex = /\nPUBLIC ([0-9a-f]+)(?: m)? [0-9a-f]+ (.*)|\nFUNC ([0-9a-f]+)(?: m)? [0-9a-f]+ [0-9a-f]+ (.*)/g;
 
   let match;
   let approximateLength = 0;
