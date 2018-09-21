6082e77828836e78690b249c65cec4a33594b7a7
Iain Ireland <iireland@mozilla.com>
2018-09-21 01:51:11 +0000

    Bug 1491350: Clear pending OOM exceptions before returning from hasNativeProperty / getNativePropertyByValue r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D6394

---
+++ js/src/jit-test/tests/baseline/bug1491337.js
@@ -0,0 +1,19 @@
+if (!('oomTest' in this))
+    quit();
+
+oomTest(new Function(`
+  let kJSEmbeddingMaxTypes = 1000000;
+  let kJSEmbeddingMaxFunctions = 1000000;
+  let kJSEmbeddingMaxImports = 100000;
+  const known_failures = {};
+  function test(func, description) {
+    known_failures[description]
+  }
+  function testLimit(name, min, limit, gen) {
+    test(() => {}, \`Validate \${name} mininum\`);
+    test(() => {}, \`Async compile \${name} over limit\`);
+  }
+  testLimit("types", 1, kJSEmbeddingMaxTypes, (builder, count) => {});
+  testLimit("functions", 1, kJSEmbeddingMaxFunctions, (builder, count) => {});
+  testLimit("imports", 1, kJSEmbeddingMaxImports, (builder, count) => {});
+`));
