564d1ebe923a816ef5484a9e1df1b5e3cc3fb2f5
Jonathan Watt <jwatt@jwatt.org>
2018-09-21 21:16:49 +0100

    Bug 1495554. Call StopObserving in SVGRenderingObserver's dtor instead of individual subclasses' dtors. r=longsonr
    Differential Revision: https://phabricator.services.mozilla.com/D7328

--- layout/svg/SVGObserverUtils.cpp
+++ layout/svg/SVGObserverUtils.cpp
@@ -230,11 +230,10 @@ class SVGIDRenderingObserver : public SVGRenderingObserver
 public:
   SVGIDRenderingObserver(URLAndReferrerInfo* aURI, nsIContent* aObservingContent,
                          bool aReferenceImage);
-  virtual ~SVGIDRenderingObserver() {
-    StopObserving();
-  }
 
 protected:
+  virtual ~SVGIDRenderingObserver() = default; // non-public
+
   Element* GetReferencedElementWithoutObserving() override {
     return mObservedElementTracker.get();
   }
