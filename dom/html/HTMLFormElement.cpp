5f0be07390d0312ac08ab5ef5927bbcb036d783d
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-22 20:29:07 -0400

    Bug 1493563 - Part 5: Present the old state and the content blocking log to the web progress listeners; r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D6595

--- dom/html/HTMLFormElement.cpp
+++ dom/html/HTMLFormElement.cpp
@@ -2131,7 +2131,9 @@ HTMLFormElement::OnStatusChange(nsIWebProgress* aWebProgress,
 NS_IMETHODIMP
 HTMLFormElement::OnSecurityChange(nsIWebProgress* aWebProgress,
                                   nsIRequest* aRequest,
-                                  uint32_t state)
+                                  uint32_t aOldState,
+                                  uint32_t aState,
+                                  const nsAString& aContentBlockingLogJSON)
 {
   MOZ_ASSERT_UNREACHABLE("notification excluded in AddProgressListener(...)");
   return NS_OK;
