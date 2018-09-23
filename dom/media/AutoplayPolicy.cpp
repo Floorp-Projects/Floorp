7e30c253fc33c30b0e37d8893d7ea0b65233cafd
alwu <alwu@mozilla.com>
2018-09-23 22:11:06 +0000

    Bug 1493027 - part1 : access permissions without creating MediaManager. r=cpearce
    If a site had been granted persistent permissions, but hasn't accessed navigator.mediaDevices yet.
    Then, we can't read the permission because MediaManager hasn't be created yet.
    
    We should read these permissions without loading MediaManager.
    
    In addition, I noticed that even user only requests 'screen' permission, we would also add the 'camera'
    and 'microphone' permission in the nsIPermissionManager. That makes us no way to print the log to distinguish
    what actually permission user granted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D6540

--- dom/media/AutoplayPolicy.cpp
+++ dom/media/AutoplayPolicy.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/AutoplayPermissionManager.h"
 #include "mozilla/dom/HTMLMediaElement.h"
 #include "mozilla/dom/HTMLMediaElementBinding.h"
+#include "nsGlobalWindowInner.h"
 #include "nsIAutoplay.h"
 #include "nsContentUtils.h"
 #include "nsIDocument.h"
@@ -62,17 +63,30 @@ ApproverDocOf(const nsIDocument& aDocument)
 }
 
 static bool
+IsActivelyCapturingOrHasAPermission(nsPIDOMWindowInner* aWindow)
+{
+  // Pages which have been granted permission to capture WebRTC camera or
+  // microphone or screen are assumed to be trusted, and are allowed to autoplay.
+  if (MediaManager::GetIfExists()) {
+    return MediaManager::GetIfExists()->IsActivelyCapturingOrHasAPermission(aWindow->WindowID());
+  }
+
+  auto principal = nsGlobalWindowInner::Cast(aWindow)->GetPrincipal();
+  return (nsContentUtils::IsExactSitePermAllow(principal, "camera") ||
+          nsContentUtils::IsExactSitePermAllow(principal, "microphone") ||
+          nsContentUtils::IsExactSitePermAllow(principal, "screen"));
+}
+
+static bool
 IsWindowAllowedToPlay(nsPIDOMWindowInner* aWindow)
 {
   if (!aWindow) {
     return false;
   }
 
-  // Pages which have been granted permission to capture WebRTC camera or
-  // microphone are assumed to be trusted, and are allowed to autoplay.
-  MediaManager* manager = MediaManager::GetIfExists();
-  if (manager &&
-      manager->IsActivelyCapturingOrHasAPermission(aWindow->WindowID())) {
+  if (IsActivelyCapturingOrHasAPermission(aWindow)) {
+    AUTOPLAY_LOG("Allow autoplay as document has camera or microphone or screen"
+                 " permission.");
     return true;
   }
 
