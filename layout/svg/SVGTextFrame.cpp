0c4f25af5801ba1a4ed2af99b14a23b1d0419235
Robert Longson <longsonr@gmail.com>
2018-09-23 15:53:03 +0100

    Bug 1474982 - Fix asertion failure when lengthAdjust value is empty r=heycam

--- layout/svg/SVGTextFrame.cpp
+++ layout/svg/SVGTextFrame.cpp
@@ -5224,11 +5224,14 @@ SVGTextFrame::DoGlyphPositioning()
   SVGTextContentElement* element = static_cast<SVGTextContentElement*>(GetContent());
   nsSVGLength2* textLengthAttr =
     element->GetAnimatedLength(nsGkAtoms::textLength);
+  uint16_t lengthAdjust =
+    element->EnumAttributes()[SVGTextContentElement::LENGTHADJUST].GetAnimValue();
   bool adjustingTextLength = textLengthAttr->IsExplicitlySet();
   float expectedTextLength = textLengthAttr->GetAnimValue(element);
 
-  if (adjustingTextLength && expectedTextLength < 0.0f) {
-    // If textLength="" is less than zero, ignore it.
+  if (adjustingTextLength &&
+      (expectedTextLength < 0.0f || lengthAdjust == LENGTHADJUST_UNKNOWN)) {
+    // If textLength="" is less than zero or lengthAdjust is unknown, ignore it.
     adjustingTextLength = false;
   }
 
@@ -5277,8 +5280,6 @@ SVGTextFrame::DoGlyphPositioning()
     float actualTextLength =
       static_cast<float>(presContext->AppUnitsToGfxUnits(frameLength) * factor);
 
-    uint16_t lengthAdjust =
-      element->EnumAttributes()[SVGTextContentElement::LENGTHADJUST].GetAnimValue();
     switch (lengthAdjust) {
       case LENGTHADJUST_SPACINGANDGLYPHS:
         // Scale the glyphs and their positions.
