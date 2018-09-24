235807f3a6704fcfe31ac5f5cc973c860e4cec73
Matthew Gaudet <mgaudet@mozilla.com>
2018-09-24 15:22:57 +0000

    Bug 1488786 - Add an IC for sparse array element access r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D5706

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -290,6 +290,9 @@ GetPropIRGenerator::tryAttachStub()
             if (tryAttachArgumentsObjectArg(obj, objId, indexId)) {
                 return true;
             }
+            if (tryAttachGenericElement(obj, objId, index, indexId)) {
+                return true;
+            }
 
             trackAttached(IRGenerator::NotAttached);
             return false;
@@ -2341,6 +2344,31 @@ GetPropIRGenerator::tryAttachUnboxedElementHole(HandleObject obj, ObjOperandId o
     trackAttached("UnboxedElementHole");
     return true;
 }
+
+bool
+GetPropIRGenerator::tryAttachGenericElement(HandleObject obj, ObjOperandId objId,
+                                            uint32_t index, Int32OperandId indexId)
+{
+    if (!obj->isNative()) {
+        return false;
+    }
+
+    // To allow other types to attach in the non-megamorphic case we test the specific
+    // matching native reciever; however, once megamorphic we can attach for any native
+    if (mode_ == ICState::Mode::Megamorphic) {
+        writer.guardIsNativeObject(objId);
+    } else {
+        NativeObject* nobj = &obj->as<NativeObject>();
+        TestMatchingNativeReceiver(writer, nobj, objId);
+    }
+    writer.callNativeGetElementResult(objId, indexId);
+    writer.typeMonitorResult();
+
+    trackAttached(mode_ == ICState::Mode::Megamorphic
+                  ? "GenericElementMegamorphic": "GenericElement");
+    return true;
+}
+
 
 bool
 GetPropIRGenerator::tryAttachProxyElement(HandleObject obj, ObjOperandId objId)
