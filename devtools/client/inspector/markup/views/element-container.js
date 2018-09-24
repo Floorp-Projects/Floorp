bf3c6b0125d91882c827f612ffa37e973ac737d7
Gabriel Luong <gabriel.luong@gmail.com>
2018-09-24 13:45:13 -0400

    Bug 1317102 - Part 2: Refactor the highlighter event handlers into ElementEditor and prevent flickering of the display badge when toggled from a click. r=jdescottes
    - Refactor the highlighter shown/hidden event handlers from MarkupElementContainer in ElementEditor.
    We do this because we want to be a bit more lazy when we set these on/off highlighters to only when
    there is only a display badge and appropriate display type.
    - This fixes a flickering bug cause by the event handlers when you click on the display badges in the markup view.
    STR:
    1. Have 2 grids shown on the markup view.
    2. Toggle on one of them
    3. Toggle the other one.
    4. See that the active state toggles on, off and on.
    We want to toggle the active state manually instead of waiting for the highlighter event handler
    to toggle its active state when we click on the display badge. This is because waiting for
    the highlight event handler to toggle the active state will cause a brief delay from showing
    the active state when the user clicked on the display badge. To fix this, we added a way
    to start/stop the highlighter event handlers.

--- devtools/client/inspector/markup/views/element-container.js
+++ devtools/client/inspector/markup/views/element-container.js
@@ -34,16 +34,6 @@ function MarkupElementContainer(markupView, node) {
   MarkupContainer.prototype.initialize.call(this, markupView, node,
     "elementcontainer");
 
-  this.onFlexboxHighlighterChange = this.onFlexboxHighlighterChange.bind(this);
-  this.onGridHighlighterChange = this.onGridHighlighterChange.bind(this);
-
-  this.markup.highlighters.on("flexbox-highlighter-hidden",
-    this.onFlexboxHighlighterChange);
-  this.markup.highlighters.on("flexbox-highlighter-shown",
-    this.onFlexboxHighlighterChange);
-  this.markup.highlighters.on("grid-highlighter-hidden", this.onGridHighlighterChange);
-  this.markup.highlighters.on("grid-highlighter-shown", this.onGridHighlighterChange);
-
   if (node.nodeType === nodeConstants.ELEMENT_NODE) {
     this.editor = new ElementEditor(this, node);
   } else {
@@ -54,49 +44,12 @@ function MarkupElementContainer(markupView, node) {
 }
 
 MarkupElementContainer.prototype = extend(MarkupContainer.prototype, {
-  destroy: function() {
-    this.markup.highlighters.off("flexbox-highlighter-hidden",
-      this.onFlexboxHighlighterChange);
-    this.markup.highlighters.off("flexbox-highlighter-shown",
-      this.onFlexboxHighlighterChange);
-    this.markup.highlighters.off("grid-highlighter-hidden", this.onGridHighlighterChange);
-    this.markup.highlighters.off("grid-highlighter-shown", this.onGridHighlighterChange);
-
-    MarkupContainer.prototype.destroy.call(this);
-  },
-
   onContainerClick: function(event) {
     if (!event.target.hasAttribute("data-event")) {
       return;
     }
 
     this._buildEventTooltipContent(event.target);
-  },
-
-  /**
-   * Handler for "flexbox-highlighter-hidden" and "flexbox-highlighter-shown" event
-   * emitted from the HighlightersOverlay. Toggles the active state of the display badge
-   * if it matches the highlighted flex container node.
-   */
-  onFlexboxHighlighterChange: function() {
-    if (!this.editor._displayBadge) {
-      return;
-    }
-    this.editor._displayBadge.classList.toggle("active",
-      this.markup.highlighters.flexboxHighlighterShown === this.node);
-  },
-
-  /**
-   * Handler for "grid-highlighter-hidden" and "grid-highlighter-shown" event emitted from
-   * the HighlightersOverlay. Toggles the active state of the display badge if it matches
-   * the highlighted grid node.
-   */
-  onGridHighlighterChange: function() {
-    if (!this.editor._displayBadge) {
-      return;
-    }
-    this.editor._displayBadge.classList.toggle("active",
-      this.markup.highlighters.gridHighlighters.has(this.node));
   },
 
   async _buildEventTooltipContent(target) {
