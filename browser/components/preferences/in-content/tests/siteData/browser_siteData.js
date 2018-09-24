3c6400c1512043608260c6a6c7d3820b8fcfb971
Andrea Marchesini <amarchesini@mozilla.com>
2018-09-24 11:37:57 +0200

    Bug 1260399 - Remove deprecated messages in nsICookieManager methods - part 2 - nsICookieManager.getCookiesFromHost(), r=ehsan

--- browser/components/preferences/in-content/tests/siteData/browser_siteData.js
+++ browser/components/preferences/in-content/tests/siteData/browser_siteData.js
@@ -152,10 +152,10 @@ add_task(async function() {
 
   // Get the exact creation date from the cookies (to avoid intermittents
   // from minimal time differences, since we round up to minutes).
-  let cookiesEnum1 = Services.cookies.getCookiesFromHost(uri.host);
+  let cookiesEnum1 = Services.cookies.getCookiesFromHost(uri.host, {});
   // We made two valid cookies for example.com.
   cookiesEnum1.getNext();
-  let cookiesEnum2 = Services.cookies.getCookiesFromHost(uri2.host);
+  let cookiesEnum2 = Services.cookies.getCookiesFromHost(uri2.host, {});
   let cookie1 = cookiesEnum1.getNext().QueryInterface(Ci.nsICookie2);
   let cookie2 = cookiesEnum2.getNext().QueryInterface(Ci.nsICookie2);
 
