53bed206289f765d415f860023a60521662d8a8b
Dave Townsend <dtownsend@oxymoronical.com>
2018-09-24 14:55:37 +0000

    Bug 1493310: Add a way to access certain functionality of nsXREDirProvider for testing purposes. r=froydnj
    In order to be able to have even basic xpcshell tests for bug 1474285 I need
    nsXREDirProvider to be accessible and to be able to override a couple of the
    directories it returns.
    
    Differential Revision: https://phabricator.services.mozilla.com/D6544

--- toolkit/xre/nsXREDirProvider.h
+++ toolkit/xre/nsXREDirProvider.h
@@ -9,12 +9,20 @@
 #include "nsIDirectoryService.h"
 #include "nsIProfileMigrator.h"
 #include "nsIFile.h"
+#include "nsIXREDirProvider.h"
 
 #include "nsCOMPtr.h"
 #include "nsCOMArray.h"
 #include "mozilla/Attributes.h"
 
+// {5573967d-f6cf-4c63-8e0e-9ac06e04d62b}
+#define NS_XREDIRPROVIDER_CID \
+  { 0x5573967d, 0xf6cf, 0x4c63, \
+    { 0x8e, 0x0e, 0x9a, 0xc0, 0x6e, 0x04, 0xd6, 0x2b } }
+#define NS_XREDIRPROVIDER_CONTRACTID "@mozilla.org/xre/directory-provider;1"
+
 class nsXREDirProvider final : public nsIDirectoryServiceProvider2,
+                               public nsIXREDirProvider,
                                public nsIProfileStartup
 {
 public:
@@ -25,6 +33,7 @@ public:
 
   NS_DECL_NSIDIRECTORYSERVICEPROVIDER
   NS_DECL_NSIDIRECTORYSERVICEPROVIDER2
+  NS_DECL_NSIXREDIRPROVIDER
   NS_DECL_NSIPROFILESTARTUP
 
   nsXREDirProvider();
@@ -35,7 +44,7 @@ public:
                       nsIDirectoryServiceProvider* aAppProvider = nullptr);
   ~nsXREDirProvider();
 
-  static nsXREDirProvider* GetSingleton();
+  static already_AddRefed<nsXREDirProvider> GetSingleton();
 
   nsresult GetUserProfilesRootDir(nsIFile** aResult);
   nsresult GetUserProfilesLocalDir(nsIFile** aResult);
@@ -72,8 +81,7 @@ public:
   /**
    * Get a hash for the install directory.
    */
-  nsresult GetInstallHash(nsAString & aPathHash,
-                          bool aUseCompatibilityMode = false);
+  nsresult GetInstallHash(nsAString & aPathHash, bool aUseCompatibilityMode);
 
   /**
    * Get the directory under which update directory is created.
