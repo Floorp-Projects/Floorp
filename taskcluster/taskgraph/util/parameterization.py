6f37e64f07bcf0b58fad30e1a04a08b8d15d38cb
Dustin J. Mitchell <dustin@mozilla.com>
2018-09-25 20:26:55 +0000

    Bug 1492664 - add {artifact-reference: ..}; r=tomprince
    This provides an easy way to encode an artifact URL in static data such as
    taskcluster/ci/nightly-l10n/kind.yml, without knowing in advance the format of
    the URL.

--- taskcluster/taskgraph/util/parameterization.py
+++ taskcluster/taskgraph/util/parameterization.py
@@ -4,24 +4,27 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import os
 import re
+import taskcluster_urls
 
 from taskgraph.util.time import json_time_from_now
+from taskgraph.util.taskcluster import get_root_url
 
 TASK_REFERENCE_PATTERN = re.compile('<([^>]+)>')
+ARTIFACT_REFERENCE_PATTERN = re.compile('<([^/]+)/([^>]+)>')
 
 
-def _recurse(val, param_name, param_fn):
-    param_keys = [param_name]
-
+def _recurse(val, param_fns):
     def recurse(val):
         if isinstance(val, list):
             return [recurse(v) for v in val]
         elif isinstance(val, dict):
-            if val.keys() == param_keys:
-                return param_fn(val[param_name])
-            else:
-                return {k: recurse(v) for k, v in val.iteritems()}
+            if len(val) == 1:
+                for param_key, param_fn in param_fns.items():
+                    if val.keys() == [param_key]:
+                        return param_fn(val[param_key])
+            return {k: recurse(v) for k, v in val.iteritems()}
         else:
             return val
     return recurse(val)
@@ -29,20 +32,47 @@ def _recurse(val, param_name, param_fn):
 
 def resolve_timestamps(now, task_def):
     """Resolve all instances of `{'relative-datestamp': '..'}` in the given task definition"""
-    return _recurse(task_def, 'relative-datestamp', lambda v: json_time_from_now(v, now))
+    return _recurse(task_def, {
+        'relative-datestamp': lambda v: json_time_from_now(v, now),
+    })
 
 
 def resolve_task_references(label, task_def, dependencies):
-    """Resolve all instances of `{'task-reference': '..<..>..'}` in the given task
-    definition, using the given dependencies"""
-    def repl(match):
-        key = match.group(1)
-        try:
-            return dependencies[key]
-        except KeyError:
-            # handle escaping '<'
-            if key == '<':
-                return key
-            raise KeyError("task '{}' has no dependency named '{}'".format(label, key))
+    """Resolve all instances of
+      {'task-reference': '..<..>..'}
+    and
+      {'artifact-reference`: '..<dependency/artifact/path>..'}
+    in the given task definition, using the given dependencies"""
 
-    return _recurse(task_def, 'task-reference', lambda v: TASK_REFERENCE_PATTERN.sub(repl, v))
+    def task_reference(val):
+        def repl(match):
+            key = match.group(1)
+            try:
+                return dependencies[key]
+            except KeyError:
+                # handle escaping '<'
+                if key == '<':
+                    return key
+                raise KeyError("task '{}' has no dependency named '{}'".format(label, key))
+
+        return TASK_REFERENCE_PATTERN.sub(repl, val)
+
+    def artifact_reference(val):
+        def repl(match):
+            dependency, artifact_name = match.group(1, 2)
+
+            try:
+                dependency_task_id = dependencies[dependency]
+            except KeyError:
+                raise KeyError("task '{}' has no dependency named '{}'".format(label, dependency))
+
+            return taskcluster_urls.api(
+                get_root_url(), 'queue', 'v1',
+                'task/{}/artifacts/{}'.format(dependency_task_id, artifact_name))
+
+        return ARTIFACT_REFERENCE_PATTERN.sub(repl, val)
+
+    return _recurse(task_def, {
+        'task-reference': task_reference,
+        'artifact-reference': artifact_reference,
+    })
