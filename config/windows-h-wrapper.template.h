75e63a0b60baf8e17cddd60b14081ba8ea94c079
Nika Layzell <nika@thelayzells.com>
2018-09-25 17:34:53 +0200

    Bug 1448426 - Wrap windows.h to avoid problematic define statements, r=froydnj,glandium
    By default, windows.h exposes a large number of problematic define statements
    which are UpperCamelCase, such as a define from `CreateWindow` to
    `CreateWindow{A,W}`.
    
    As many of these names are generic (e.g. CreateFile, CreateWindow), they can
    mess up Gecko code that may legitimately have its own methods with the same
    names.
    
    The header also defines some traditional SCREAMING_SNAKE_CASE defines which
    can mess up our code by conflicting with local values.
    
    This patch adds a simple code generator which generates wrappers for these
    defines, and uses them to wrap the windows.h wrapper using the `stl_wrappers`
    mechanism, allowing us to use windows.h in more places.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10932

---
+++ config/windows-h-wrapper.template.h
@@ -0,0 +1,55 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_windows_h
+#define mozilla_windows_h
+
+// Include the "real" windows.h header. On clang/gcc, this can be done with the
+// `include_next` feature, however MSVC requires a direct include path.
+//
+// Also turn off deprecation warnings, as we may be wrapping deprecated fns.
+
+#if defined(__GNUC__) || defined(__clang__)
+#  pragma GCC system_header
+#  include_next <windows.h>
+
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#else
+#  include <${header_path}>
+
+#  pragma warning(push)
+#  pragma warning(disable: 4996 4995)
+#endif // defined(__GNUC__) || defined(__clang__)
+
+// Check if the header should be disabled
+#if defined(MOZ_DISABLE_WINDOWS_WRAPPER)
+#define MOZ_WINDOWS_WRAPPER_DISABLED_REASON "explicitly disabled"
+
+#elif !defined(__cplusplus)
+#define MOZ_WINDOWS_WRAPPER_DISABLED_REASON "non-C++ source file"
+
+#elif !defined(__GNUC__) && !defined(__clang__) && !defined(_DLL)
+#define MOZ_WINDOWS_WRAPPER_DISABLED_REASON "non-dynamic RTL"
+
+#else
+// We're allowed to wrap in the current context. Define `MOZ_WRAPPED_WINDOWS_H`
+// to note that fact, and perform the wrapping.
+#define MOZ_WRAPPED_WINDOWS_H
+extern "C++" {
+
+${decls}
+
+} // extern "C++"
+#endif // enabled
+
+#if defined(__GNUC__) || defined(__clang__)
+#  pragma GCC diagnostic pop
+#else
+#  pragma warning(pop)
+#endif // defined(__GNUC__) || defined(__clang__)
+
+#endif // !defined(mozilla_windows_h)
