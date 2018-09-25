a327440df48570556fde8d71c904f8bedc8eb29e
Brendan Dahl <bdahl@mozilla.com>
2018-09-25 16:00:13 +0000

    Bug 1492327 - Mimic XUL activeElement behavior for XUL in XHTML. r=smaug
    Return the document element as the activeElement when there is no body
    element, the document is chrome privileged, and the document element
    is a XUL element.
    
    MozReview-Commit-ID: JFDLAqOmLTS
    
    Differential Revision: https://phabricator.services.mozilla.com/D6448

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1106,9 +1106,7 @@ window._gBrowser = {
         newBrowser._urlbarFocused &&
         gURLBar &&
         gURLBar.focused;
-      // In an HTML document (built with MOZ_BROWSER_XHTML), the activeElement
-      // can be null, so check before attempting to blur it (Bug 1485157).
-      if (!keepFocusOnUrlBar && document.activeElement) {
+      if (!keepFocusOnUrlBar) {
         // Clear focus so that _adjustFocusAfterTabSwitch can detect if
         // some element has been focused and respect that.
         document.activeElement.blur();
