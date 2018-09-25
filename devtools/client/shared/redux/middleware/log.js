3695cdf5759c52934bc11093a72b0a7ad278f7ba
Alexandre Poirot <poirot.alex@gmail.com>
2018-09-25 19:34:52 +0300

    Bug 1473513 - Backed out changeset 6fc241b06e0b for failures on mobile/android/tests/browser/chrome/test_debugger_server.html CLOSED TREE
    Summary:
    MozReview-Commit-ID: 6dCPXvBYhsJ
    
    Depends on D6810
    
    Bug #: 1473513
    
    Differential Revision: https://phabricator.services.mozilla.com/D6811

--- devtools/client/shared/redux/middleware/log.js
+++ devtools/client/shared/redux/middleware/log.js
@@ -10,18 +10,8 @@
 function log({ dispatch, getState }) {
   return next => action => {
     try {
-      // whitelist of fields, rather than printing the whole object
-      console.log("[DISPATCH] action type:", action.type);
-      /*
-       * USE WITH CAUTION!! This will output everything from an action object,
-       * and these can be quite large. Printing out large objects will slow
-       * down tests and cause test failures
-       *
-       * console.log("[DISPATCH]", JSON.stringify(action, null, 2));
-       */
+      console.log("[DISPATCH]", JSON.stringify(action, null, 2));
     } catch (e) {
-      // this occurs if JSON.stringify throws.
-      console.warn(e);
       console.log("[DISPATCH]", action);
     }
     next(action);
