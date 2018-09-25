1909662130f0c73d3a9eba7f9c0de4a1e7612ad0
Ehsan Akhgari <ehsan@mozilla.com>
2018-09-25 05:40:16 +0000

    Bug 1493682 - Part 3: Add tests for the new prefs r=baku
    The image cache tests didn't follow the previous naming convention
    in order to make it clearer how the tests are set up using the
    naming conventions of the files.
    
    These tests should be unified into a single file soon.
    
    Depends on D6748
    
    Differential Revision: https://phabricator.services.mozilla.com/D6749
    
    --HG--
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache1-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache1-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache12.js => toolkit/components/antitracking/test/browser/browser_imageCache10-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache12.js => toolkit/components/antitracking/test/browser/browser_imageCache10-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache12.js => toolkit/components/antitracking/test/browser/browser_imageCache11-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache12.js => toolkit/components/antitracking/test/browser/browser_imageCache11-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache15.js => toolkit/components/antitracking/test/browser/browser_imageCache13-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache15.js => toolkit/components/antitracking/test/browser/browser_imageCache13-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache15.js => toolkit/components/antitracking/test/browser/browser_imageCache14-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache15.js => toolkit/components/antitracking/test/browser/browser_imageCache14-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache2-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache2-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache4-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache3.js => toolkit/components/antitracking/test/browser/browser_imageCache4-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache5-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache5-2.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache6-1.js
    rename : toolkit/components/antitracking/test/browser/browser_imageCache7.js => toolkit/components/antitracking/test/browser/browser_imageCache6-2.js
    extra : moz-landing-system : lando

---
+++ toolkit/components/antitracking/test/browser/browser_imageCache4-1.js
@@ -0,0 +1,18 @@
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+let cookieBehavior = BEHAVIOR_REJECT_TRACKER;
+let blockingByContentBlocking = true;
+let blockingByContentBlockingUI = true;
+let blockingByContentBlockingRTUI = false;
+let blockingByAllowList = true;
+let expectedBlockingNotifications = true;
+
+let rootDir = getRootDirectory(gTestPath);
+let jar = getJar(rootDir);
+if (jar) {
+  let tmpdir = extractJarToTmp(jar);
+  rootDir = "file://" + tmpdir.path + "/";
+}
+/* import-globals-from imageCacheWorker.js */
+Services.scriptloader.loadSubScript(rootDir + "imageCacheWorker.js", this);
+
