ad8c17d35db166cce50355102956b053bc76cc2e
Dustin J. Mitchell <dustin@mozilla.com>
2018-09-25 20:18:19 +0000

    Bug 1492664 - set TASKCLUSTER_ROOT_URL and TASKCLUSTER_PROXY_URL; r=tomprince,glandium
    Eventually, workers will provide these variables directly
    (https://bugzilla.mozilla.org/show_bug.cgi?id=1460015).  But for now, this
    ensures that TASKCLUSTER_ROOT_URL is set everywhere, and TASKCLUSTER_PROXY_URL
    is set wherever the proxy is active.
    
    The setup for the mach commands defaults to https://taskcluster.net for user
    convenience.  When the production instance's URL changes, we can simply change
    that default.
    
    This changes the docker build process propagate TASKCLUSTER_ROOT_URL into the
    docker images where necessary (using %ARG), specifically to create URLs for
    debian repo paths.

--- taskcluster/docker/debian-base/setup_packages.sh
+++ taskcluster/docker/debian-base/setup_packages.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+TASKCLUSTER_ROOT_URL=$1
+shift
+
+# duplicate the functionality of taskcluster-lib-urls, but in bash..
+if [ "$TASKCLUSTER_ROOT_URL" = "https://taskcluster.net" ]; then
+    queue_base='https://queue.taskcluster.net/v1'
+else
+    queue_base="$TASKCLUSTER_ROOT_URL/api/queue/v1"
+fi
+
+
 for task in "$@"; do
-  echo "deb [trusted=yes] https://queue.taskcluster.net/v1/task/$task/artifacts/public/build/ debian/" > "/etc/apt/sources.list.d/99$task.list"
+  echo "adding package source $queue_base/task/$task/artifacts/public/build/"
+  echo "deb [trusted=yes] $queue_base/task/$task/artifacts/public/build/ debian/" > "/etc/apt/sources.list.d/99$task.list"
 done
--- tools/tryselect/tasks.py
+++ tools/tryselect/tasks.py
@@ -55,6 +55,9 @@ def invalidate(cache, root):
 
 
 def generate_tasks(params, full, root):
+    # Ensure that TASKCLUSTER_ROOT_URL is set
+    taskgraph.set_root_url_env()
+
     params = params or "project=mozilla-central"
 
     # Try to delete the old taskgraph cache directory.
