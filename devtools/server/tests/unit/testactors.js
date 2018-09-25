a420673b7fb807edd392abcae98409c5ecc611e0
yulia <ystartsev@mozilla.com>
2018-09-25 08:47:10 +0000

    Bug 1473513 - move ActorRegistry module to server/actors/utils; r=ochameau
    Depends on D6477
    
    Differential Revision: https://phabricator.services.mozilla.com/D6479

--- devtools/server/tests/unit/testactors.js
+++ devtools/server/tests/unit/testactors.js
@@ -7,7 +7,7 @@ const { LazyPool, createExtraActors } = require("devtools/shared/protocol/lazy-p
 const { RootActor } = require("devtools/server/actors/root");
 const { ThreadActor } = require("devtools/server/actors/thread");
 const { DebuggerServer } = require("devtools/server/main");
-const { ActorRegistry } = require("devtools/server/actor-registry");
+const { ActorRegistry } = require("devtools/server/actors/utils/actor-registry");
 const { TabSources } = require("devtools/server/actors/utils/TabSources");
 const makeDebugger = require("devtools/server/actors/utils/make-debugger");
 
