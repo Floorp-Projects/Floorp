48c56bd0e312d4fe75c4ae1f1e187f227d6db42c
yulia <ystartsev@mozilla.com>
2018-09-26 10:06:43 +0200

    Bug 1473513 - separate registerModule behavior from DebuggerServer; r=ochameau
    Summary:
    MozReview-Commit-ID: 3GsXRxcIKfx
    
    Depends on D6473
    
    Reviewers: ochameau
    
    Reviewed By: ochameau
    
    Subscribers: reviewbot
    
    Bug #: 1473513
    
    Differential Revision: https://phabricator.services.mozilla.com/D6474

--- devtools/client/debugger/test/mochitest/browser_dbg_globalactor.js
+++ devtools/client/debugger/test/mochitest/browser_dbg_globalactor.js
@@ -13,7 +13,7 @@ add_task(async function() {
   DebuggerServer.init();
   DebuggerServer.registerAllActors();
 
-  DebuggerServer.registerModule(ACTORS_URL, {
+  ActorRegistry.registerModule(ACTORS_URL, {
     prefix: "testOne",
     constructor: "TestActor1",
     type: { global: true },
