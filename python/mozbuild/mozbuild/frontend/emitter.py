415a9500909e41e8630451bb5ccfeca9fa3129fd
Edwin Gao <egao@mozilla.com>
2018-09-26 20:07:25 +0000

    Bug 1305743 - Make failure to find mozinfo.json a fatal error r=gbrown,ahal a=reland
    - moved placement of the raise_exception computation to be after the initial objdir path computation.
    - original implementation was missing detection for cases where user may pass in a False flag to explicitly suppress errors.
    - added unit tests to check for scenarios where raise_exception flag is passed in as part of positional argument.
    - changed argument to include a **kwargs argument for caller to modify default exception behavior.
    - default behavior is to raise exceptions if mozinfo.json cannot be found.
    - disabled TreeMetadataEmitter from calling mozinfo.find_and_update_from_json and setting the self.info variable since it was not referenced anywhere else after the initial setup.
    
    Depends on D6859
    
    Differential Revision: https://phabricator.services.mozilla.com/D6860
    
    --HG--
    extra : source : 6d25c44c39ea33ebe25829d945b00963dad39ea4
    extra : amend_source : 5c7a556b122f887113bae16356ffcf4bd064b620

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -110,18 +110,6 @@ class TreeMetadataEmitter(LoggingMixin):
         self.populate_logger()
 
         self.config = config
-
-        mozinfo.find_and_update_from_json(config.topobjdir)
-
-        # Python 2.6 doesn't allow unicode keys to be used for keyword
-        # arguments. This gross hack works around the problem until we
-        # rid ourselves of 2.6.
-        self.info = {}
-        for k, v in mozinfo.info.items():
-            if isinstance(k, unicode):
-                k = k.encode('ascii')
-            self.info[k] = v
-
         self._libs = OrderedDefaultDict(list)
         self._binaries = OrderedDict()
         self._compile_dirs = set()
