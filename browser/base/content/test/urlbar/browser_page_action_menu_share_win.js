fe5b933f2b96a989fdf9cd312d7eec433a6159a0
Dale Harvey <dale@arandomurl.com>
2018-09-26 21:09:00 +0100

    Bug 1363169 - Add support for native windows share. r=gijs, r=aklotz

---
+++ browser/base/content/test/urlbar/browser_page_action_menu_share_win.js
@@ -0,0 +1,45 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* global sinon */
+Services.scriptloader.loadSubScript("resource://testing-common/sinon-2.3.2.js");
+
+const TEST_URL = getRootDirectory(gTestPath) + "browser_page_action_menu_share_win.html";
+
+// Keep track of site details we are sharing
+let sharedUrl, sharedTitle;
+
+let stub = sinon.stub(BrowserPageActions.shareURL, "_windowsUIUtils").get(() => {
+  return {
+    shareUrl(url, title) {
+      sharedUrl = url;
+      sharedTitle = title;
+    },
+  };
+});
+
+registerCleanupFunction(async function() {
+  stub.restore();
+  delete window.sinon;
+});
+
+add_task(async function shareURL() {
+  await BrowserTestUtils.withNewTab(TEST_URL, async () => {
+    // Open the panel.
+    await promisePageActionPanelOpen();
+
+    // Click Share URL.
+    let shareURLButton = document.getElementById("pageAction-panel-shareURL");
+    let hiddenPromise = promisePageActionPanelHidden();
+    EventUtils.synthesizeMouseAtCenter(shareURLButton, {});
+
+    await hiddenPromise;
+
+    Assert.equal(sharedUrl, TEST_URL,
+                 "Shared correct URL");
+    Assert.equal(sharedTitle, "Windows Sharing",
+                 "Shared with the correct title");
+  });
+});
