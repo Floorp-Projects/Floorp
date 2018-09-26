36cc0e5fa7277103f3e86875239f7ecaa99900c8
Noemi Erli <nerli@mozilla.com>
2018-09-26 08:54:03 +0300

    Backed out 2 changesets (bug 1479035) for build bustages bustages netwerk/cache2/target on CLOSED TREE
    Backed out changeset 5f8706213610 (bug 1479035)
    Backed out changeset a03a61d6d724 (bug 1479035)

--- ipc/chromium/src/base/platform_thread_posix.cc
+++ ipc/chromium/src/base/platform_thread_posix.cc
@@ -35,6 +35,10 @@ void InitThreading();
 #endif
 
 static void* ThreadFunc(void* closure) {
+  // Create a nsThread wrapper for the current platform thread, and register it
+  // with the thread manager.
+  (void) NS_GetCurrentThread();
+
   PlatformThread::Delegate* delegate =
       static_cast<PlatformThread::Delegate*>(closure);
   delegate->ThreadMain();
