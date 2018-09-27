170d12c2c0b6955828ff05130a8d75622dc64779
Julien Wajsberg <felash@gmail.com>
2018-09-27 14:33:41 +0000

    Bug 1494337 - Do not display the 'triggers refresh' note if there's no option needing it r=ochameau
    Depends on D6923
    
    Differential Revision: https://phabricator.services.mozilla.com/D6920

--- devtools/client/framework/toolbox-options.js
+++ devtools/client/framework/toolbox-options.js
@@ -467,6 +467,10 @@ OptionsPanel.prototype = {
     } else {
       // Hide the checkbox and label
       this.disableJSNode.parentNode.style.display = "none";
+
+      const triggersPageRefreshLabel =
+        this.panelDoc.getElementById("triggers-page-refresh-label");
+      triggersPageRefreshLabel.style.display = "none";
     }
   },
 
