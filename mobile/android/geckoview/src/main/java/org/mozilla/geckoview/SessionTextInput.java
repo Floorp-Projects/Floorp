19435e0b46bd5ab286bab1ea5c4d4577895c4bde
Jim Chen <nchen@mozilla.com>
2018-09-27 10:59:50 -0400

    Bug 1492704 - 2. Don't update node during auto-fill focus event; r=droeh
    Don't update the focused node in the node cache during auto-fill focus
    events, because we don't necessarily know if the focused node should be
    in the node cahce (e.g. the focused node could be from another window
    than the one we're handling currently).
    
    Differential Revision: https://phabricator.services.mozilla.com/D6758

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/SessionTextInput.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/SessionTextInput.java
@@ -734,7 +734,6 @@ public final class SessionTextInput {
         final int id;
         if (message != null) {
             id = message.getInt("id");
-            mAutoFillNodes.put(id, message);
         } else {
             id = View.NO_ID;
         }
