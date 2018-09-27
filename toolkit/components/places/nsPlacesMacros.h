26ec02b273f4de52cb6e25697017875f42c8ce3a
David Major <dmajor@mozilla.com>
2018-09-27 08:58:55 -0400

    Bug 1494077: Remove unused macro and its supporting #define to fix a unified build problem. r=mak

--- toolkit/components/places/nsPlacesMacros.h
+++ toolkit/components/places/nsPlacesMacros.h
@@ -6,10 +6,6 @@
 #include "nsIConsoleService.h"
 #include "nsIScriptError.h"
 
-#ifndef __FUNCTION__
-#define __FUNCTION__ __func__
-#endif
-
 // Call a method on each observer in a category cache, then call the same
 // method on the observer array.
 #define NOTIFY_OBSERVERS(canFire, array, type, method)                         \
@@ -53,19 +49,3 @@
     }                                                                          \
     return ret.forget();                                                       \
   }
-
-#define PLACES_WARN_DEPRECATED()                                               \
-  PR_BEGIN_MACRO                                                               \
-  nsCString msg(__FUNCTION__);                                                 \
-  msg.AppendLiteral(" is deprecated and will be removed in the next version.");\
-  NS_WARNING(msg.get());                                                       \
-  nsCOMPtr<nsIConsoleService> cs = do_GetService(NS_CONSOLESERVICE_CONTRACTID);\
-  if (cs) {                                                                    \
-    nsCOMPtr<nsIScriptError> e = do_CreateInstance(NS_SCRIPTERROR_CONTRACTID); \
-    if (e && NS_SUCCEEDED(e->Init(NS_ConvertUTF8toUTF16(msg), EmptyString(),   \
-                                  EmptyString(), 0, 0,                         \
-                                  nsIScriptError::errorFlag, "Places"))) {     \
-      cs->LogMessage(e);                                                       \
-    }                                                                          \
-  }                                                                            \
-  PR_END_MACRO
