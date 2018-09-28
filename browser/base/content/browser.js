45e7e08e6d069b0e9b02f8671ded8a8b605727b9
Jared Wein <jwein@mozilla.com>
2018-09-28 14:48:40 +0000

    Bug 1480456 - Add 'Select All Tabs' into tab context menu. r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D7129
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5709,6 +5709,7 @@ function onViewToolbarsPopupShowing(aEvent, aInsertPoint) {
     let haveMultipleTabs = gBrowser.visibleTabs.length > 1;
     document.getElementById("toolbar-context-reloadAllTabs").disabled = !haveMultipleTabs;
 
+    document.getElementById("toolbar-context-selectAllTabs").disabled = gBrowser.allTabsSelected();
     document.getElementById("toolbar-context-undoCloseTab").disabled =
       SessionStore.getClosedTabCount(window) == 0;
     return;
