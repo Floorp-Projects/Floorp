b66b2ca7ad033181790dabd676d153430d646011
Jan Henning <jh+bugzilla@buttercookie.de>
2018-09-29 15:55:01 +0000

    Bug 1428670 - Part 0: Fix mixed tabs and spaces. r=dbaron
    Differential Revision: https://phabricator.services.mozilla.com/D5576
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -2589,7 +2589,7 @@ SizeComputationInput::InitOffsets(WritingMode aWM,
   else if (aPadding) { // padding is an input arg
     ComputedPhysicalPadding() = *aPadding;
     needPaddingProp = mFrame->StylePadding()->IsWidthDependent() ||
-	  (mFrame->GetStateBits() & NS_FRAME_REFLOW_ROOT);
+    (mFrame->GetStateBits() & NS_FRAME_REFLOW_ROOT);
   }
   else {
     needPaddingProp = ComputePadding(aWM, aPercentBasis, aFrameType);
