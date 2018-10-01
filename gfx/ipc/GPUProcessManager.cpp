f4c89bfb5562bb3ed01a3493138898ac9dd688c5
sotaro <sotaro.ikeda.g@gmail.com>
2018-10-01 12:09:18 +0900

    Bug 1494538 - Fallback to sofware when GPU process is disabled on Windows r=mattwoodrow

--- gfx/ipc/GPUProcessManager.cpp
+++ gfx/ipc/GPUProcessManager.cpp
@@ -196,6 +196,12 @@ GPUProcessManager::DisableGPUProcess(const char* aMessage)
   // crash, then we need to tell the content processes again, because they
   // need to rebind to the UI process.
   HandleProcessLost();
+
+  // On Windows, always fallback to software.
+  // The assumption is that something in the graphics driver is crashing.
+#if XP_WIN
+  FallbackToSoftware("GPU Process is disabled, fallback to software solution.");
+#endif
 }
 
 bool
@@ -501,24 +507,26 @@ GPUProcessManager::OnRemoteProcessDeviceReset(GPUProcessHost* aHost)
   if (ShouldLimitDeviceResets(mDeviceResetCount, delta)) {
     DestroyProcess();
     DisableGPUProcess("GPU processed experienced too many device resets");
-
-    // Reaches the limited TDR attempts, fallback to software solution.
-    gfxConfig::SetFailed(Feature::HW_COMPOSITING,
-      FeatureStatus::Blocked,
-      "Too many attemps of D3D11 creation, fallback to software solution.");
-    gfxConfig::SetFailed(Feature::D3D11_COMPOSITING,
-      FeatureStatus::Blocked,
-      "Too many attemps of D3D11 creation, fallback to software solution.");
-    gfxConfig::SetFailed(Feature::DIRECT2D,
-      FeatureStatus::Blocked,
-      "Too many attemps of D3D11 creation, fallback to software solution.");
-
     HandleProcessLost();
     return;
   }
 
   RebuildRemoteSessions();
   NotifyListenersOnCompositeDeviceReset();
+}
+
+void
+GPUProcessManager::FallbackToSoftware(const char* aMessage)
+{
+  gfxConfig::SetFailed(Feature::HW_COMPOSITING,
+    FeatureStatus::Blocked,
+    aMessage);
+  gfxConfig::SetFailed(Feature::D3D11_COMPOSITING,
+    FeatureStatus::Blocked,
+    aMessage);
+  gfxConfig::SetFailed(Feature::DIRECT2D,
+    FeatureStatus::Blocked,
+    aMessage);
 }
 
 void
