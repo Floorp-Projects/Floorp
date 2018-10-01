f83a54699b527ae9dc0f1999243c88813278eff0
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-10-01 07:33:16 +0000

    Bug 1494170: Show proper runtime's name and version. r=jdescottes
    Depends on D6881
    
    Differential Revision: https://phabricator.services.mozilla.com/D7036
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/aboutdebugging-new/src/reducers/runtimes-state.js
+++ devtools/client/aboutdebugging-new/src/reducers/runtimes-state.js
@@ -40,7 +40,7 @@ function RuntimesState(networkRuntimes = []) {
 function runtimesReducer(state = RuntimesState(), action) {
   switch (action.type) {
     case CONNECT_RUNTIME_SUCCESS: {
-      const { id, client } = action.runtime;
+      const { id, client, info } = action.runtime;
 
       // Find the array of runtimes that contains the updated runtime.
       const runtime = findRuntimeById(id, state);
@@ -50,7 +50,7 @@ function runtimesReducer(state = RuntimesState(), action) {
       // Add the new client to the runtime.
       const updatedRuntimes = runtimesToUpdate.map(r => {
         if (r.id === id) {
-          return Object.assign({}, r, { client });
+          return Object.assign({}, r, { client, info });
         }
         return r;
       });
@@ -68,7 +68,7 @@ function runtimesReducer(state = RuntimesState(), action) {
       // Remove the client from the updated runtime.
       const updatedRuntimes = runtimesToUpdate.map(r => {
         if (r.id === id) {
-          return Object.assign({}, r, { client: null });
+          return Object.assign({}, r, { client: null, info: null });
         }
         return r;
       });
