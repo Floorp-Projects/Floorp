cab9ebf01007162c019782dba5fc20b8b6c84745
Andrew Osmond <aosmond@mozilla.com>
2018-10-01 14:47:17 -0400

    Bug 1504699 - Part 4. Add method to SharedSurfacesChild to cast to a shared surface. r=nical
    Differential Revision: https://phabricator.services.mozilla.com/D10900

--- gfx/layers/ipc/SharedSurfacesChild.h
+++ gfx/layers/ipc/SharedSurfacesChild.h
@@ -80,6 +80,13 @@ public:
   static Maybe<wr::ExternalImageId>
   GetExternalId(const gfx::SourceSurfaceSharedData* aSurface);
 
+  /**
+   * Get the surface (or its underlying surface) as a SourceSurfaceSharedData
+   * pointer, if valid.
+   */
+  static gfx::SourceSurfaceSharedData*
+  AsSourceSurfaceSharedData(gfx::SourceSurface* aSurface);
+
   static nsresult UpdateAnimation(ImageContainer* aContainer,
                                   gfx::SourceSurface* aSurface,
                                   const gfx::IntRect& aDirtyRect);
