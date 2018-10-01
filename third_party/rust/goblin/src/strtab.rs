56e10a560b71f715aac7f1fe9c8ccd3f70e03f67
Markus Stange <mstange@themasta.com>
2018-10-01 20:24:42 +0000

    Bug 1457481 - Run mach vendor rust. r=froydnj,erahm
    Most importantly, this picks up "object" and "goblin" for ELF binary parsing.
    We only use the ELF code from goblin, so the mach-O parsing code gets
    eliminated by the linker. Overall, this increases the Android installer size
    by 20KB.
    
    Try pushes for reference:
    before: https://treeherder.mozilla.org/#/jobs?repo=try&revision=834b56dc5ab3d63a43a32f740ee8212296ac726d&selectedJob=201600899
    after: https://treeherder.mozilla.org/#/jobs?repo=try&revision=6983b27e8d3cb715d3b7e6cbd276683f6466e3cc&selectedJob=201600475
    
    installer size: 34524820 -> 34542861 (34.52MB -> 34.54MB)
    
    $ mach vendor rust
        Updating registry `https://github.com/rust-lang/crates.io-index`
          Adding goblin v0.0.17
          Adding memmap v0.6.2
          Adding miniz-sys v0.1.10
          Adding object v0.10.0
          Adding parity-wasm v0.31.3
          Adding plain v0.2.3
          Adding profiler_helper v0.1.0 (file:///Users/mstange/code/mozilla/tools/profiler/rust-helper)
          Adding scroll v0.9.1
          Adding scroll_derive v0.9.5
          Adding syn v0.15.5
          Adding thin-vec v0.1.0
          Adding uuid v0.6.5
     0:30.11 The following files exceed the filesize limit of 102400:
    
    third_party/rust/miniz-sys/miniz.c
    third_party/rust/syn-0.14.6/src/expr.rs
    third_party/rust/syn-0.14.6/src/gen/fold.rs
    third_party/rust/syn-0.14.6/src/gen/visit.rs
    third_party/rust/syn-0.14.6/src/gen/visit_mut.rs
    
    The syn dependency is not compiled for goblin, as far as I can tell - it's only
    needed for the 'syn' feature of scroll_derive, and scroll does not ask for
    scroll_derive/syn.
    
    object -> goblin -> scroll -> scroll_derive -/-> syn
    
    But it looks like other versions of syn were already in the tree.
    
    Depends on D7021
    
    Differential Revision: https://phabricator.services.mozilla.com/D7023
    
    --HG--
    rename : third_party/rust/syn/src/parsers.rs => third_party/rust/syn-0.14.6/src/parsers.rs
    rename : third_party/rust/syn/src/verbatim.rs => third_party/rust/syn-0.14.6/src/verbatim.rs
    rename : third_party/rust/uuid/.travis.yml => third_party/rust/uuid-0.5.1/.travis.yml
    rename : third_party/rust/uuid/src/rustc_serialize.rs => third_party/rust/uuid-0.5.1/src/rustc_serialize.rs
    rename : third_party/rust/uuid/src/serde.rs => third_party/rust/uuid-0.5.1/src/serde.rs
    extra : moz-landing-system : lando

---
+++ third_party/rust/goblin/src/strtab.rs
@@ -0,0 +1,139 @@
+//! A byte-offset based string table.
+//! Commonly used in ELF binaries, Unix archives, and even PE binaries.
+
+use core::ops::Index;
+use core::slice;
+use core::str;
+use core::fmt;
+use scroll::{self, ctx, Pread};
+if_alloc! {
+    use error;
+    use alloc::vec::Vec;
+}
+
+/// A common string table format which is indexed by byte offsets (and not
+/// member index). Constructed using [`parse`](#method.parse)
+/// with your choice of delimiter. Please be careful.
+pub struct Strtab<'a> {
+    bytes: &'a[u8],
+    delim: ctx::StrCtx,
+}
+
+#[inline(always)]
+fn get_str(offset: usize, bytes: &[u8], delim: ctx::StrCtx) -> scroll::Result<&str> {
+    bytes.pread_with::<&str>(offset, delim)
+}
+
+impl<'a> Strtab<'a> {
+    /// Construct a new strtab with `bytes` as the backing string table, using `delim` as the delimiter between entries
+    pub fn new (bytes: &'a [u8], delim: u8) -> Self {
+        Strtab { delim: ctx::StrCtx::Delimiter(delim), bytes: bytes }
+    }
+    /// Construct a strtab from a `ptr`, and a `size`, using `delim` as the delimiter
+    pub unsafe fn from_raw(ptr: *const u8, size: usize, delim: u8) -> Strtab<'a> {
+        Strtab { delim: ctx::StrCtx::Delimiter(delim), bytes: slice::from_raw_parts(ptr, size) }
+    }
+    #[cfg(feature = "alloc")]
+    /// Parses a strtab from `bytes` at `offset` with `len` size as the backing string table, using `delim` as the delimiter
+    pub fn parse(bytes: &'a [u8], offset: usize, len: usize, delim: u8) -> error::Result<Strtab<'a>> {
+        let (end, overflow) = offset.overflowing_add(len);
+        if overflow || end > bytes.len () {
+            return Err(error::Error::Malformed(format!("Strtable size ({}) + offset ({}) is out of bounds for {} #bytes. Overflowed: {}", len, offset, bytes.len(), overflow)));
+        }
+        Ok(Strtab { bytes: &bytes[offset..end], delim: ctx::StrCtx::Delimiter(delim) })
+    }
+    #[cfg(feature = "alloc")]
+    /// Converts the string table to a vector, with the original `delim` used to separate the strings
+    pub fn to_vec(self) -> error::Result<Vec<&'a str>> {
+        let len = self.bytes.len();
+        let mut strings = Vec::with_capacity(len);
+        let mut i = 0;
+        while i < len {
+            let string = self.get(i).unwrap()?;
+            i = i + string.len() + 1;
+            strings.push(string);
+        }
+        Ok(strings)
+    }
+    /// Safely parses and gets a str reference from the backing bytes starting at byte `offset`.
+    /// If the index is out of bounds, `None` is returned.
+    /// Requires `feature = "alloc"`
+    #[cfg(feature = "alloc")]
+    pub fn get(&self, offset: usize) -> Option<error::Result<&'a str>> {
+        if offset >= self.bytes.len() {
+            None
+        } else {
+            Some(get_str(offset, self.bytes, self.delim).map_err(|e| e.into()))
+        }
+    }
+    /// Gets a str reference from the backing bytes starting at byte `offset`.
+    /// If the index is out of bounds, `None` is returned. Panics if bytes are invalid UTF-8.
+    pub fn get_unsafe(&self, offset: usize) -> Option<&'a str> {
+        if offset >= self.bytes.len() {
+            None
+        } else {
+            Some(get_str(offset, self.bytes, self.delim).unwrap())
+        }
+    }
+}
+
+impl<'a> fmt::Debug for Strtab<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "delim: {:?} {:?}", self.delim, str::from_utf8(self.bytes))
+    }
+}
+
+impl<'a> Default for Strtab<'a> {
+    fn default() -> Strtab<'a> {
+        Strtab { bytes: &[], delim: ctx::StrCtx::default() }
+    }
+}
+
+impl<'a> Index<usize> for Strtab<'a> {
+    type Output = str;
+    /// Gets str reference at starting at byte `offset`.
+    /// **NB**: this will panic if the underlying bytes are not valid utf8, or the offset is invalid
+    #[inline(always)]
+    fn index(&self, offset: usize) -> &Self::Output {
+        // This can't delegate to get() because get() requires #[cfg(features = "alloc")]
+        // It's also slightly less useful than get() because the lifetime -- specified by the Index
+        // trait -- matches &self, even though we could return &'a instead
+        get_str(offset, self.bytes, self.delim).unwrap()
+    }
+}
+
+#[test]
+fn as_vec_no_final_null() {
+    let bytes = b"\0printf\0memmove\0busta";
+    let strtab = unsafe { Strtab::from_raw(bytes.as_ptr(), bytes.len(), 0x0) };
+    let vec = strtab.to_vec().unwrap();
+    assert_eq!(vec.len(), 4);
+    assert_eq!(vec, vec!["", "printf", "memmove", "busta"]);
+}
+
+#[test]
+fn as_vec_no_first_null_no_final_null() {
+    let bytes = b"printf\0memmove\0busta";
+    let strtab = unsafe { Strtab::from_raw(bytes.as_ptr(), bytes.len(), 0x0) };
+    let vec = strtab.to_vec().unwrap();
+    assert_eq!(vec.len(), 3);
+    assert_eq!(vec, vec!["printf", "memmove", "busta"]);
+}
+
+#[test]
+fn to_vec_final_null() {
+    let bytes = b"\0printf\0memmove\0busta\0";
+    let strtab = unsafe { Strtab::from_raw(bytes.as_ptr(), bytes.len(), 0x0) };
+    let vec = strtab.to_vec().unwrap();
+    assert_eq!(vec.len(), 4);
+    assert_eq!(vec, vec!["", "printf", "memmove", "busta"]);
+}
+
+#[test]
+fn to_vec_newline_delim() {
+    let bytes = b"\nprintf\nmemmove\nbusta\n";
+    let strtab = unsafe { Strtab::from_raw(bytes.as_ptr(), bytes.len(), '\n' as u8) };
+    let vec = strtab.to_vec().unwrap();
+    assert_eq!(vec.len(), 4);
+    assert_eq!(vec, vec!["", "printf", "memmove", "busta"]);
+}
--- third_party/rust/memmap/src/windows.rs
+++ third_party/rust/memmap/src/windows.rs
@@ -1,121 +1,211 @@
-extern crate fs2;
-extern crate kernel32;
-extern crate winapi;
-
 use std::{io, mem, ptr};
 use std::fs::File;
 use std::os::raw::c_void;
-use std::os::windows::io::AsRawHandle;
+use std::os::windows::io::{AsRawHandle, RawHandle};
 
-use self::fs2::FileExt;
-
-use ::Protection;
-use ::MmapOptions;
-
-impl Protection {
-
-    /// Returns the `Protection` as a flag appropriate for a call to `CreateFileMapping`.
-    fn as_mapping_flag(self) -> winapi::DWORD {
-        match self {
-            Protection::Read => winapi::PAGE_READONLY,
-            Protection::ReadWrite => winapi::PAGE_READWRITE,
-            Protection::ReadCopy => winapi::PAGE_READONLY,
-            Protection::ReadExecute => winapi::PAGE_EXECUTE_READ,
-        }
-    }
-
-    /// Returns the `Protection` as a flag appropriate for a call to `MapViewOfFile`.
-    fn as_view_flag(self) -> winapi::DWORD {
-        match self {
-            Protection::Read => winapi::FILE_MAP_READ,
-            Protection::ReadWrite => winapi::FILE_MAP_ALL_ACCESS,
-            Protection::ReadCopy => winapi::FILE_MAP_COPY,
-            Protection::ReadExecute => winapi::FILE_MAP_READ | winapi::FILE_MAP_EXECUTE,
-        }
-    }
-}
+use winapi::shared::basetsd::SIZE_T;
+use winapi::shared::minwindef::DWORD;
+use winapi::um::memoryapi::{
+    FILE_MAP_ALL_ACCESS,
+    FILE_MAP_COPY,
+    FILE_MAP_EXECUTE,
+    FILE_MAP_READ,
+    FILE_MAP_WRITE,
+    CreateFileMappingW,
+    VirtualProtect,
+    MapViewOfFile,
+    UnmapViewOfFile,
+    FlushViewOfFile,
+};
+use winapi::um::handleapi::{
+    CloseHandle,
+    INVALID_HANDLE_VALUE,
+};
+use winapi::um::sysinfoapi::GetSystemInfo;
+use winapi::um::winnt::{
+    PAGE_EXECUTE_READ,
+    PAGE_EXECUTE_READWRITE,
+    PAGE_EXECUTE_WRITECOPY,
+    PAGE_READONLY,
+    PAGE_READWRITE,
+    PAGE_WRITECOPY,
+};
 
 pub struct MmapInner {
     file: Option<File>,
     ptr: *mut c_void,
     len: usize,
+    copy: bool,
 }
 
 impl MmapInner {
-
-    pub fn open(file: &File, prot: Protection, offset: usize, len: usize) -> io::Result<MmapInner> {
+    /// Creates a new `MmapInner`.
+    ///
+    /// This is a thin wrapper around the `CreateFileMappingW` and `MapViewOfFile` system calls.
+    pub fn new(
+        file: &File,
+        protect: DWORD,
+        access: DWORD,
+        offset: usize,
+        len: usize,
+        copy: bool,
+    ) -> io::Result<MmapInner> {
         let alignment = offset % allocation_granularity();
         let aligned_offset = offset - alignment;
         let aligned_len = len + alignment;
 
         unsafe {
-            let handle = kernel32::CreateFileMappingW(file.as_raw_handle(),
-                                                      ptr::null_mut(),
-                                                      prot.as_mapping_flag(),
-                                                      0,
-                                                      0,
-                                                      ptr::null());
+            let handle = CreateFileMappingW(
+                file.as_raw_handle(),
+                ptr::null_mut(),
+                protect,
+                0,
+                0,
+                ptr::null(),
+            );
             if handle == ptr::null_mut() {
                 return Err(io::Error::last_os_error());
             }
 
-            let ptr = kernel32::MapViewOfFile(handle,
-                                              prot.as_view_flag(),
-                                              (aligned_offset >> 16 >> 16) as winapi::DWORD,
-                                              (aligned_offset & 0xffffffff) as winapi::DWORD,
-                                              aligned_len as winapi::SIZE_T);
-            kernel32::CloseHandle(handle);
+            let ptr = MapViewOfFile(
+                handle,
+                access,
+                (aligned_offset >> 16 >> 16) as DWORD,
+                (aligned_offset & 0xffffffff) as DWORD,
+                aligned_len as SIZE_T,
+            );
+            CloseHandle(handle);
 
             if ptr == ptr::null_mut() {
                 Err(io::Error::last_os_error())
             } else {
                 Ok(MmapInner {
-                    file: Some(try!(file.duplicate())),
+                    file: Some(file.try_clone()?),
                     ptr: ptr.offset(alignment as isize),
                     len: len as usize,
+                    copy: copy,
                 })
             }
         }
     }
 
-    pub fn anonymous(len: usize, prot: Protection, _options: MmapOptions) -> io::Result<MmapInner> {
+    pub fn map(len: usize, file: &File, offset: usize) -> io::Result<MmapInner> {
+        let write = protection_supported(file.as_raw_handle(), PAGE_READWRITE);
+        let exec = protection_supported(file.as_raw_handle(), PAGE_EXECUTE_READ);
+        let mut access = FILE_MAP_READ;
+        let protection = match (write, exec) {
+            (true, true) => {
+                access |= FILE_MAP_WRITE | FILE_MAP_EXECUTE;
+                PAGE_EXECUTE_READWRITE
+            }
+            (true, false) => {
+                access |= FILE_MAP_WRITE;
+                PAGE_READWRITE
+            }
+            (false, true) => {
+                access |= FILE_MAP_EXECUTE;
+                PAGE_EXECUTE_READ
+            }
+            (false, false) => PAGE_READONLY,
+        };
+
+        let mut inner = MmapInner::new(file, protection, access, offset, len, false)?;
+        if write || exec {
+            inner.make_read_only()?;
+        }
+        Ok(inner)
+    }
+
+    pub fn map_exec(len: usize, file: &File, offset: usize) -> io::Result<MmapInner> {
+        let write = protection_supported(file.as_raw_handle(), PAGE_READWRITE);
+        let mut access = FILE_MAP_READ | FILE_MAP_EXECUTE;
+        let protection = if write {
+            access |= FILE_MAP_WRITE;
+            PAGE_EXECUTE_READWRITE
+        } else {
+            PAGE_EXECUTE_READ
+        };
+
+        let mut inner = MmapInner::new(file, protection, access, offset, len, false)?;
+        if write {
+            inner.make_exec()?;
+        }
+        Ok(inner)
+    }
+
+    pub fn map_mut(len: usize, file: &File, offset: usize) -> io::Result<MmapInner> {
+        let exec = protection_supported(file.as_raw_handle(), PAGE_EXECUTE_READ);
+        let mut access = FILE_MAP_READ | FILE_MAP_WRITE;
+        let protection = if exec {
+            access |= FILE_MAP_EXECUTE;
+            PAGE_EXECUTE_READWRITE
+        } else {
+            PAGE_READWRITE
+        };
+
+        let mut inner = MmapInner::new(file, protection, access, offset, len, false)?;
+        if exec {
+            inner.make_mut()?;
+        }
+        Ok(inner)
+    }
+
+    pub fn map_copy(len: usize, file: &File, offset: usize) -> io::Result<MmapInner> {
+        let exec = protection_supported(file.as_raw_handle(), PAGE_EXECUTE_READWRITE);
+        let mut access = FILE_MAP_COPY;
+        let protection = if exec {
+            access |= FILE_MAP_EXECUTE;
+            PAGE_EXECUTE_WRITECOPY
+        } else {
+            PAGE_WRITECOPY
+        };
+
+        let mut inner = MmapInner::new(file, protection, access, offset, len, true)?;
+        if exec {
+            inner.make_mut()?;
+        }
+        Ok(inner)
+    }
+
+    pub fn map_anon(len: usize, _stack: bool) -> io::Result<MmapInner> {
         unsafe {
             // Create a mapping and view with maximum access permissions, then use `VirtualProtect`
             // to set the actual `Protection`. This way, we can set more permissive protection later
             // on.
             // Also see https://msdn.microsoft.com/en-us/library/windows/desktop/aa366537.aspx
 
-            let handle = kernel32::CreateFileMappingW(winapi::INVALID_HANDLE_VALUE,
-                                                      ptr::null_mut(),
-                                                      winapi::PAGE_EXECUTE_READWRITE,
-                                                      (len >> 16 >> 16) as winapi::DWORD,
-                                                      (len & 0xffffffff) as winapi::DWORD,
-                                                      ptr::null());
+            let handle = CreateFileMappingW(
+                INVALID_HANDLE_VALUE,
+                ptr::null_mut(),
+                PAGE_EXECUTE_READWRITE,
+                (len >> 16 >> 16) as DWORD,
+                (len & 0xffffffff) as DWORD,
+                ptr::null(),
+            );
             if handle == ptr::null_mut() {
                 return Err(io::Error::last_os_error());
             }
-            let access = winapi::FILE_MAP_ALL_ACCESS | winapi::FILE_MAP_EXECUTE;
-            let ptr = kernel32::MapViewOfFile(handle,
-                                              access,
-                                              0,
-                                              0,
-                                              len as winapi::SIZE_T);
-            kernel32::CloseHandle(handle);
+            let access = FILE_MAP_ALL_ACCESS | FILE_MAP_EXECUTE;
+            let ptr = MapViewOfFile(handle, access, 0, 0, len as SIZE_T);
+            CloseHandle(handle);
 
             if ptr == ptr::null_mut() {
                 return Err(io::Error::last_os_error());
             }
 
             let mut old = 0;
-            let result = kernel32::VirtualProtect(ptr,
-                                                  len as winapi::SIZE_T,
-                                                  prot.as_mapping_flag(),
-                                                  &mut old);
+            let result = VirtualProtect(
+                ptr,
+                len as SIZE_T,
+                PAGE_READWRITE,
+                &mut old,
+            );
             if result != 0 {
                 Ok(MmapInner {
                     file: None,
                     ptr: ptr,
                     len: len as usize,
+                    copy: false,
                 })
             } else {
                 Err(io::Error::last_os_error())
@@ -124,13 +214,15 @@ impl MmapInner {
     }
 
     pub fn flush(&self, offset: usize, len: usize) -> io::Result<()> {
-        try!(self.flush_async(offset, len));
-        if let Some(ref file) = self.file { file.sync_data() } else { Ok(()) }
+        self.flush_async(offset, len)?;
+        if let Some(ref file) = self.file {
+            file.sync_data()?;
+        }
+        Ok(())
     }
 
     pub fn flush_async(&self, offset: usize, len: usize) -> io::Result<()> {
-        let result = unsafe { kernel32::FlushViewOfFile(self.ptr.offset(offset as isize),
-                                                        len as winapi::SIZE_T) };
+        let result = unsafe { FlushViewOfFile(self.ptr.offset(offset as isize), len as SIZE_T) };
         if result != 0 {
             Ok(())
         } else {
@@ -138,17 +230,14 @@ impl MmapInner {
         }
     }
 
-    pub fn set_protection(&mut self, prot: Protection) -> io::Result<()> {
+    fn virtual_protect(&mut self, protect: DWORD) -> io::Result<()> {
         unsafe {
             let alignment = self.ptr as usize % allocation_granularity();
-            let ptr = self.ptr.offset(- (alignment as isize));
-            let aligned_len = self.len as winapi::SIZE_T + alignment as winapi::SIZE_T;
+            let ptr = self.ptr.offset(-(alignment as isize));
+            let aligned_len = self.len as SIZE_T + alignment as SIZE_T;
 
             let mut old = 0;
-            let result = kernel32::VirtualProtect(ptr,
-                                                  aligned_len,
-                                                  prot.as_mapping_flag(),
-                                                  &mut old);
+            let result = VirtualProtect(ptr, aligned_len, protect, &mut old);
 
             if result != 0 {
                 Ok(())
@@ -158,14 +247,37 @@ impl MmapInner {
         }
     }
 
+    pub fn make_read_only(&mut self) -> io::Result<()> {
+        self.virtual_protect(PAGE_READONLY)
+    }
+
+    pub fn make_exec(&mut self) -> io::Result<()> {
+        if self.copy {
+            self.virtual_protect(PAGE_EXECUTE_WRITECOPY)
+        } else {
+            self.virtual_protect(PAGE_EXECUTE_READ)
+        }
+    }
+
+    pub fn make_mut(&mut self) -> io::Result<()> {
+        if self.copy {
+            self.virtual_protect(PAGE_WRITECOPY)
+        } else {
+            self.virtual_protect(PAGE_READWRITE)
+        }
+    }
+
+    #[inline]
     pub fn ptr(&self) -> *const u8 {
         self.ptr as *const u8
     }
 
+    #[inline]
     pub fn mut_ptr(&mut self) -> *mut u8 {
         self.ptr as *mut u8
     }
 
+    #[inline]
     pub fn len(&self) -> usize {
         self.len
     }
@@ -175,20 +287,32 @@ impl Drop for MmapInner {
     fn drop(&mut self) {
         let alignment = self.ptr as usize % allocation_granularity();
         unsafe {
-            let ptr = self.ptr.offset(- (alignment as isize));
-            assert!(kernel32::UnmapViewOfFile(ptr) != 0,
-                    "unable to unmap mmap: {}", io::Error::last_os_error());
+            let ptr = self.ptr.offset(-(alignment as isize));
+            assert!(UnmapViewOfFile(ptr) != 0,
+                    "unable to unmap mmap: {}",
+                    io::Error::last_os_error());
         }
     }
 }
 
-unsafe impl Sync for MmapInner { }
-unsafe impl Send for MmapInner { }
+unsafe impl Sync for MmapInner {}
+unsafe impl Send for MmapInner {}
+
+fn protection_supported(handle: RawHandle, protection: DWORD) -> bool {
+    unsafe {
+        let handle = CreateFileMappingW(handle, ptr::null_mut(), protection, 0, 0, ptr::null());
+        if handle == ptr::null_mut() {
+            return false;
+        }
+        CloseHandle(handle);
+        true
+    }
+}
 
 fn allocation_granularity() -> usize {
     unsafe {
         let mut info = mem::zeroed();
-        kernel32::GetSystemInfo(&mut info);
+        GetSystemInfo(&mut info);
         return info.dwAllocationGranularity as usize;
     }
 }
