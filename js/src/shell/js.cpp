200b8bdf789fd6f59db8e7e8e5a21685e18eca78
Lars T Hansen <lhansen@mozilla.com>
2018-10-02 10:42:30 +0200

    Bug 1495662 - cranelift presence must not disable wasm-gc.  r=bbouvier

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -10062,13 +10062,19 @@ SetContextOptions(JSContext* cx, const OptionParser& op)
 #endif
 #ifdef ENABLE_WASM_GC
     enableWasmGc = op.getBoolOption("wasm-gc");
-# ifdef ENABLE_WASM_CRANELIFT
-    enableWasmGc = false;
-# endif
 #endif
     enableTestWasmAwaitTier2 = op.getBoolOption("test-wasm-await-tier2");
     enableAsyncStacks = !op.getBoolOption("no-async-stacks");
     enableStreams = op.getBoolOption("enable-streams");
+
+#if defined ENABLE_WASM_GC && defined ENABLE_WASM_CRANELIFT
+    // Note, once we remove --wasm-gc this test will no longer make any sense
+    // and we'll need a better solution.
+    if (enableWasmGc && wasmForceCranelift) {
+        fprintf(stderr, "Do not combine --wasm-gc and --wasm-force-cranelift, they are incompatible.\n");
+        return false;
+    }
+#endif
 
     JS::ContextOptionsRef(cx).setBaseline(enableBaseline)
                              .setIon(enableIon)
