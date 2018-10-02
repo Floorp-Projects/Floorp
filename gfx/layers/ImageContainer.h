aa6a92d381cbf160f0e8e2e06e408f2c18ef9c30
Andrew Osmond <aosmond@mozilla.com>
2018-10-02 13:28:22 -0400

    Bug 1428558 - Part 6. Integrate SharedSurfacesAnimation with ImageContainer. r=nical
    This patch embeds a SharedSurfacesAnimation object inside an
    ImageContainer. This allows any consumers of the container to get the
    single shared ImageKey for an animation, despite whatever surfaces may
    be held inside the container at a given time.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7503

--- gfx/layers/ImageContainer.h
+++ gfx/layers/ImageContainer.h
@@ -162,6 +162,7 @@ class ImageCompositeNotification;
 class ImageContainer;
 class ImageContainerChild;
 class SharedPlanarYCbCrImage;
+class SharedSurfacesAnimation;
 class PlanarYCbCrImage;
 class TextureClient;
 class KnowsCompositor;
@@ -640,6 +641,13 @@ public:
 
   void DropImageClient();
 
+  SharedSurfacesAnimation* GetSharedSurfacesAnimation() const
+  {
+    return mSharedAnimation;
+  }
+
+  SharedSurfacesAnimation* EnsureSharedSurfacesAnimation();
+
 private:
   typedef mozilla::RecursiveMutex RecursiveMutex;
 
@@ -699,6 +707,8 @@ private:
   // frames to the compositor through transactions in the main thread rather than
   // asynchronusly using the ImageBridge IPDL protocol.
   RefPtr<ImageClient> mImageClient;
+
+  nsAutoPtr<SharedSurfacesAnimation> mSharedAnimation;
 
   bool mIsAsync;
   CompositableHandle mAsyncContainerHandle;
