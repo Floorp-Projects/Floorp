109b13b5a16a5b4237b5da16d6fab07c8d405d6b
Mike Conley <mconley@mozilla.com>
2018-10-02 17:59:23 +0000

    Bug 1492482 - Remove CPOW usage from browser_1119088.js. r=mstange
    Depends on D6957
    
    Differential Revision: https://phabricator.services.mozilla.com/D6958

--- browser/components/shell/test/browser_1119088.js
+++ browser/components/shell/test/browser_1119088.js
@@ -2,41 +2,53 @@
 let NS_OSX_PICTURE_DOCUMENTS_DIR = "Pct";
 let NS_MAC_USER_LIB_DIR = "ULibDir";
 
+/**
+ * Tests "Set As Desktop Background" on macOS.
+ */
+add_task(async function() {
+  await BrowserTestUtils.withNewTab({
+    gBrowser,
+    url: "about:logo",
+  }, async (browser) => {
+    let dirSvc = Cc["@mozilla.org/file/directory_service;1"].
+                 getService(Ci.nsIDirectoryServiceProvider);
 
-function onPageLoad() {
-  let dirSvc = Cc["@mozilla.org/file/directory_service;1"].
-               getService(Ci.nsIDirectoryServiceProvider);
+    let desktopBackgroundDb = dirSvc.getFile(NS_MAC_USER_LIB_DIR, {});
+    desktopBackgroundDb.append("Application Support");
+    desktopBackgroundDb.append("Dock");
+    let desktopBackgroundDbBackup = desktopBackgroundDb.clone();
+    desktopBackgroundDb.append("desktoppicture.db");
+    desktopBackgroundDbBackup.append("desktoppicture.db.backup");
 
-  let desktopBackgroundDb = dirSvc.getFile(NS_MAC_USER_LIB_DIR, {});
-  desktopBackgroundDb.append("Application Support");
-  desktopBackgroundDb.append("Dock");
-  let desktopBackgroundDbBackup = desktopBackgroundDb.clone();
-  desktopBackgroundDb.append("desktoppicture.db");
-  desktopBackgroundDbBackup.append("desktoppicture.db.backup");
+    ok(desktopBackgroundDb.exists(),
+       "Desktop background database must exist for test to run.");
 
-  ok(desktopBackgroundDb.exists(),
-     "Desktop background database must exist for test to run.");
+    if (desktopBackgroundDbBackup.exists()) {
+      desktopBackgroundDbBackup.remove(false);
+    }
 
-  if (desktopBackgroundDbBackup.exists()) {
-    desktopBackgroundDbBackup.remove(false);
-  }
+    desktopBackgroundDb.copyTo(null, desktopBackgroundDbBackup.leafName);
 
-  desktopBackgroundDb.copyTo(null, desktopBackgroundDbBackup.leafName);
+    let wpFile = dirSvc.getFile(NS_OSX_PICTURE_DOCUMENTS_DIR, {});
+    wpFile.append("logo.png");
+    if (wpFile.exists()) {
+      wpFile.remove(false);
+    }
 
-  let wpFile = dirSvc.getFile(NS_OSX_PICTURE_DOCUMENTS_DIR, {});
-  wpFile.append("logo.png");
-  if (wpFile.exists()) {
-    wpFile.remove(false);
-  }
+    let shell = Cc["@mozilla.org/browser/shell-service;1"].
+                getService(Ci.nsIShellService);
 
-  let shell = Cc["@mozilla.org/browser/shell-service;1"].
-              getService(Ci.nsIShellService);
+    // For simplicity, we're going to reach in and access the image on the
+    // page directly, which means the page shouldn't be running in a remote
+    // browser. Thankfully, about:logo runs in the parent process for now.
+    Assert.ok(!gBrowser.selectedBrowser.isRemoteBrowser,
+              "image can be accessed synchronously from the parent process");
 
-  let image = gBrowser.contentDocumentAsCPOW.images[0];
-  shell.setDesktopBackground(image, 0, "logo.png");
+    let image = gBrowser.selectedBrowser.contentDocument.images[0];
+    shell.setDesktopBackground(image, 0, "logo.png");
 
-  setTimeout(function() {
-    ok(wpFile.exists(), "Desktop background was written to disk.");
+    await BrowserTestUtils.waitForCondition(() => wpFile.exists());
+    info("Desktop background was written to disk.");
 
     desktopBackgroundDbBackup.moveTo(null, desktopBackgroundDb.leafName);
     wpFile.remove(false);
@@ -49,16 +61,5 @@ function onPageLoad() {
       Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
     process.init(killall);
     process.run(true, dockArg, 1);
-
-    gBrowser.removeCurrentTab();
-    finish();
-  }, 1000);
-}
-
-function test() {
-  gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
-  BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser).then(onPageLoad, false, "about:logo");
-  BrowserTestUtils.loadURI(gBrowser, "about:logo");
-
-  waitForExplicitFinish();
-}
+  });
+});
