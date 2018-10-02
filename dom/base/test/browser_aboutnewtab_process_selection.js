7d567843953f6b3151b61c5764b3f0cdcec16420
Mike Conley <mconley@mozilla.com>
2018-10-02 18:02:11 +0000

    Bug 1492482 - Remove CPOW usage from browser_aboutnewtab_process_selection.js. r=Felipe
    Depends on D6967
    
    Differential Revision: https://phabricator.services.mozilla.com/D6968

--- dom/base/test/browser_aboutnewtab_process_selection.js
+++ dom/base/test/browser_aboutnewtab_process_selection.js
@@ -18,9 +18,10 @@ async function ensurePreloaded(gBrowser) {
   gBrowser._createPreloadBrowser();
   // We cannot use the regular BrowserTestUtils helper for waiting here, since that
   // would try to insert the preloaded browser, which would only break things.
-  await BrowserTestUtils.waitForCondition( () => {
-    let doc = gBrowser._preloadedBrowser.contentDocumentAsCPOW;
-    return doc && doc.readyState == "complete";
+  await ContentTask.spawn(gBrowser._preloadedBrowser, null, async () => {
+    await ContentTaskUtils.waitForCondition(() => {
+      return content.document && content.document.readyState == "complete";
+    });
   });
 }
 
