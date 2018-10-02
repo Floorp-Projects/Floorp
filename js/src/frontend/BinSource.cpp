575633db007885cc2a2f45cb3122290a0c86237c
Eric Faust <efaustbmo@gmail.com>
2018-10-01 20:41:48 -0700

    Bug 1494930 - Part 3: Don't generate both JSOP_DEFVAR and JSOP_DEFFUN for toplevel functions. (r=arai)

--- js/src/frontend/BinSource.cpp
+++ js/src/frontend/BinSource.cpp
@@ -166,6 +166,12 @@ BinASTParser<Tok>::buildFunctionBox(GeneratorKind generatorKind,
         atom = name->name();
     }
 
+    if (parseContext_ && syntax == FunctionSyntaxKind::Statement) {
+        auto ptr = parseContext_->varScope().lookupDeclaredName(atom);
+        MOZ_ASSERT(ptr);
+        ptr->value()->alterKind(DeclarationKind::BodyLevelFunction);
+    }
+
     // Allocate the function before walking down the tree.
     RootedFunction fun(cx_);
     BINJS_TRY_VAR(fun, AllocNewFunction(cx_, atom, syntax, generatorKind, functionAsyncKind, nullptr));
