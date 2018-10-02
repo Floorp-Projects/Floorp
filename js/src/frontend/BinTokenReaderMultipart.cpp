b0d2a032df20cf77f0faf272636b9c3621b47f3a
Eric Faust <efaustbmo@gmail.com>
2018-10-01 20:41:48 -0700

    Bug 1494930 - Part 1: Various BinAST parser correctness fixes. (r=arai)
    --HG--
    extra : rebase_source : 3b27e7a06d62b25abd124d8284104256906507ab

--- js/src/frontend/BinTokenReaderMultipart.cpp
+++ js/src/frontend/BinTokenReaderMultipart.cpp
@@ -148,7 +148,7 @@ BinTokenReaderMultipart::readHeader()
         if (byteLen == 2 && *current_ == 255 && *(current_ + 1) == 0) {
             atom = nullptr;
         } else {
-            BINJS_TRY_VAR(atom, Atomize(cx_, (const char*)current_, byteLen));
+            BINJS_TRY_VAR(atom, AtomizeUTF8Chars(cx_, (const char*)current_, byteLen));
         }
 
         // Populate `atomsTable_`: i => atom.
