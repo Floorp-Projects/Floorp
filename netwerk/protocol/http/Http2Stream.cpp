dd1b60a6cb313390991fbdc5e2833526160946fe
Nicholas Hurley <hurley@mozilla.com>
2018-10-03 09:40:23 +0000

    Bug 1493204 - Add pushed streams to the priority tree. r=dragana
    Previously, we had not put pushed streams in the priority tree, we just
    let them be top-level items in the tree. With this change, we will put
    them into the tree initially based on the priority of the associated
    stream. The only exception is if the associated stream is either a
    Leader or Urgent Start (in which case, we will turn the pushed streams
    into followers).
    
    Once the pushed stream is matched with a request generated by gecko,
    that pushed stream will be re-prioritized based on the priority gecko
    has for the request, just like a regular pulled stream.
    
    This also allows us to re-prioritize pushed streams into the background
    on tab switch (we assume that, before they are matched, they belong to
    the same window as the associated stream).
    
    Differential Revision: https://phabricator.services.mozilla.com/D7223

--- netwerk/protocol/http/Http2Stream.cpp
+++ netwerk/protocol/http/Http2Stream.cpp
@@ -51,6 +51,8 @@ Http2Stream::Http2Stream(nsAHttpTransaction *httpTransaction,
   , mAllHeadersReceived(0)
   , mQueued(0)
   , mSocketTransport(session->SocketTransport())
+  , mCurrentForegroundTabOuterContentWindowId(windowId)
+  , mTransactionTabId(0)
   , mTransaction(httpTransaction)
   , mChunkSize(session->SendingChunkSize())
   , mRequestBlockedOnRead(0)
@@ -73,8 +75,6 @@ Http2Stream::Http2Stream(nsAHttpTransaction *httpTransaction,
   , mTotalRead(0)
   , mPushSource(nullptr)
   , mAttempting0RTT(false)
-  , mCurrentForegroundTabOuterContentWindowId(windowId)
-  , mTransactionTabId(0)
   , mIsTunnel(false)
   , mPlainTextTunnel(false)
 {
@@ -789,6 +789,9 @@ Http2Stream::AdjustPushedPriority()
   if (mPushSource->RecvdFin() || mPushSource->RecvdReset())
     return;
 
+  // Ensure we pick up the right dependency to place the pushed stream under.
+  UpdatePriorityDependency();
+
   EnsureBuffer(mTxInlineFrame, mTxInlineFrameUsed + Http2Session::kFrameHeaderBytes + 5,
                mTxInlineFrameUsed, mTxInlineFrameSize);
   uint8_t *packet = mTxInlineFrame.get() + mTxInlineFrameUsed;
@@ -798,12 +801,13 @@ Http2Stream::AdjustPushedPriority()
                               Http2Session::FRAME_TYPE_PRIORITY, 0,
                               mPushSource->mStreamID);
 
-  mPushSource->SetPriority(mPriority);
-  memset(packet + Http2Session::kFrameHeaderBytes, 0, 4);
+  mPushSource->SetPriorityDependency(mPriority, mPriorityDependency);
+  uint32_t wireDep = PR_htonl(mPriorityDependency);
+  memcpy(packet + Http2Session::kFrameHeaderBytes, &wireDep, 4);
   memcpy(packet + Http2Session::kFrameHeaderBytes + 4, &mPriorityWeight, 1);
 
-  LOG3(("AdjustPushedPriority %p id 0x%X to weight %X\n", this, mPushSource->mStreamID,
-        mPriorityWeight));
+  LOG3(("AdjustPushedPriority %p id 0x%X to dep %X weight %X\n", this, mPushSource->mStreamID,
+        mPriorityDependency, mPriorityWeight));
 }
 
 void
@@ -1241,13 +1245,10 @@ Http2Stream::SetPriority(uint32_t newPriority)
 }
 
 void
-Http2Stream::SetPriorityDependency(uint32_t newDependency, uint8_t newWeight,
-                                   bool exclusive)
+Http2Stream::SetPriorityDependency(uint32_t newPriority, uint32_t newDependency)
 {
-  // undefined what it means when the server sends a priority frame. ignore it.
-  LOG3(("Http2Stream::SetPriorityDependency %p 0x%X received dependency=0x%X "
-        "weight=%u exclusive=%d", this, mStreamID, newDependency, newWeight,
-        exclusive));
+  SetPriority(newPriority);
+  mPriorityDependency = newDependency;
 }
 
 static uint32_t
@@ -1333,6 +1334,19 @@ Http2Stream::UpdatePriorityDependency()
 void
 Http2Stream::TopLevelOuterContentWindowIdChanged(uint64_t windowId)
 {
+  if (!mStreamID) {
+    // For pushed streams, we ignore the direct call from the session and
+    // instead let it come to the internal function from the pushed stream, so
+    // we don't accidentally send two PRIORITY frames for the same stream.
+    return;
+  }
+
+  TopLevelOuterContentWindowIdChangedInternal(windowId);
+}
+
+void
+Http2Stream::TopLevelOuterContentWindowIdChangedInternal(uint64_t windowId)
+{
   MOZ_ASSERT(gHttpHandler->ActiveTabPriority());
 
   LOG3(("Http2Stream::TopLevelOuterContentWindowIdChanged "
@@ -1368,8 +1382,12 @@ Http2Stream::TopLevelOuterContentWindowIdChanged(uint64_t windowId)
           "depends on stream 0x%X\n", this, mPriorityDependency));
   }
 
-  if (mStreamID) {
-    mSession->SendPriorityFrame(mStreamID, mPriorityDependency, mPriorityWeight);
+  uint32_t modifyStreamID = mStreamID;
+  if (!modifyStreamID && mPushSource) {
+    modifyStreamID = mPushSource->StreamID();
+  }
+  if (modifyStreamID) {
+    mSession->SendPriorityFrame(modifyStreamID, mPriorityDependency, mPriorityWeight);
   }
 }
 
