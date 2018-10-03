6c4bd03a9bb383813d33ac5820a73696f437a3cf
Tim Nguyen <ntim.bugs@gmail.com>
2018-10-03 08:55:43 +0200

    Bug 1491688 - Introduce ::-moz-tree-row(blur) selector. r=emilio

--- layout/xul/tree/nsTreeBodyFrame.cpp
+++ layout/xul/tree/nsTreeBodyFrame.cpp
@@ -1848,6 +1848,8 @@ nsTreeBodyFrame::PrefillPropertyArray(int32_t aRowIndex, nsTreeColumn* aCol)
   // focus
   if (mFocused)
     mScratchArray.AppendElement(nsGkAtoms::focus);
+  else
+    mScratchArray.AppendElement(nsGkAtoms::blur);
 
   // sort
   bool sorted = false;
