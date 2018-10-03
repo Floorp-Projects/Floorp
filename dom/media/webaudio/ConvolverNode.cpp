54b126a5cf85a4c33cbcbd39b34d5934e51ffd63
Paul Adenot <paul@paul.cx>
2018-10-03 23:15:23 +0000

    Bug 1493779 - Gracefuly handle OOM when setting a buffer on ConvolverNode.  r=karlt
    When OOMing when allocating the temporary buffer, we return an error from the
    ctor via an output parameter, and make the ConvolverNode output silence.
    Additionaly, a warning is issued each time we fail to set a buffer to the buffer
    property of a ConvolverNode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D6936

--- dom/media/webaudio/ConvolverNode.cpp
+++ dom/media/webaudio/ConvolverNode.cpp
@@ -28,8 +28,9 @@ class ConvolverNodeEngine final : public AudioNodeEngine
 {
   typedef PlayingRefChangeHandler PlayingRefChanged;
 public:
-  ConvolverNodeEngine(AudioNode* aNode, bool aNormalize)
+  ConvolverNodeEngine(AudioNode* aNode, bool aNormalize, uint64_t aWindowID)
     : AudioNodeEngine(aNode)
+    , mWindowID(aWindowID)
     , mUseBackgroundThreads(!aNode->Context()->IsOffline())
     , mNormalize(aNormalize)
   {
@@ -140,8 +141,16 @@ public:
       aBuffer.ChannelCount() == 1 ? RightConvolverMode::Direct
       : RightConvolverMode::Always;
 
+    bool allocationFailure = false;
     mReverb = new WebCore::Reverb(aBuffer, MaxFFTSize, mUseBackgroundThreads,
-                                  mNormalize, mSampleRate);
+                                  mNormalize, mSampleRate, &allocationFailure);
+    if (allocationFailure) {
+      // If the allocation failed, this AudioNodeEngine is going to output
+      // silence. This is signaled to developers in the console.
+      mReverb = nullptr;
+      WebAudioUtils::LogToDeveloperConsole(mWindowID,
+                                           "ConvolverNodeAllocationError");
+    }
   }
 
   void AllocateReverbInput(const AudioBlock& aInput,
@@ -196,6 +205,7 @@ private:
   // Keeping mReverbInput across process calls avoids unnecessary reallocation.
   AudioBlock mReverbInput;
   nsAutoPtr<WebCore::Reverb> mReverb;
+  uint64_t mWindowID;
   // Tracks samples of the tail remaining to be output.  INT32_MIN is a
   // special value to indicate that the end of any previous tail has been
   // handled.
@@ -397,7 +407,8 @@ ConvolverNode::ConvolverNode(AudioContext* aContext)
               ChannelInterpretation::Speakers)
   , mNormalize(true)
 {
-  ConvolverNodeEngine* engine = new ConvolverNodeEngine(this, mNormalize);
+  uint64_t windowID = aContext->GetParentObject()->WindowID();
+  ConvolverNodeEngine* engine = new ConvolverNodeEngine(this, mNormalize, windowID);
   mStream = AudioNodeStream::Create(aContext, engine,
                                     AudioNodeStream::NO_STREAM_FLAGS,
                                     aContext->Graph());
