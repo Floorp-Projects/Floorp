c068d0c94577aaa93a7cb256ad1b4bec323fdab3
Mark Banner <standard8@mozilla.com>
2018-10-03 14:44:19 +0000

    Bug 1496080 - Fix ./mach eslint --fix. r=ahal
    Insert the argument in the correct place when passing to node.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7609

--- tools/lint/eslint/__init__.py
+++ tools/lint/eslint/__init__.py
@@ -74,7 +74,7 @@ def lint(paths, config, binary=None, fix=None, setup=None, **lintargs):
 
     # eslint requires that --fix be set before the --ext argument.
     if fix:
-        cmd_args.insert(1, '--fix')
+        cmd_args.insert(2, '--fix')
 
     shell = False
     if os.environ.get('MSYSTEM') in ('MINGW32', 'MINGW64'):
