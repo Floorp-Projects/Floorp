08f48510ff3a3f99ffd71717cef27753622bfbd5
Nick Alexander <nalexander@mozilla.com>
2018-10-03 18:05:27 +0000

    Bug 1443332 - Fold APK signing from android-common.mk into upload-files-APK.mk. r=firefox-build-system-reviewers,mshal
    This merely centralizes logic that was formerly used at multiple sites
    into the single remaining use site.
    
    None of the JAVA* flags have been used for a long time.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7313

--- toolkit/mozapps/installer/upload-files-APK.mk
+++ toolkit/mozapps/installer/upload-files-APK.mk
@@ -6,7 +6,34 @@
 # split into its own file to increase comprehension of
 # upload-files.mk.
 
-include $(MOZILLA_DIR)/config/android-common.mk
+# DEBUG_JARSIGNER always debug signs.
+DEBUG_JARSIGNER=$(PYTHON) $(abspath $(topsrcdir)/mobile/android/debug_sign_tool.py) \
+  --keytool=$(KEYTOOL) \
+  --jarsigner=$(JARSIGNER) \
+  $(NULL)
+
+# RELEASE_JARSIGNER release signs if possible.
+ifdef MOZ_SIGN_CMD
+RELEASE_JARSIGNER := $(MOZ_SIGN_CMD) -f jar
+else
+RELEASE_JARSIGNER := $(DEBUG_JARSIGNER)
+endif
+
+# $(1) is the full path to input:  foo-debug-unsigned-unaligned.apk.
+# $(2) is the full path to output: foo.apk.
+# Use this like: $(call RELEASE_SIGN_ANDROID_APK,foo-debug-unsigned-unaligned.apk,foo.apk)
+#
+# The |zip -d| there to handle re-signing previously signed APKs.  Gradle
+# produces signed, unaligned APK files, but this expects unsigned, unaligned
+# APK files.  The |zip -d| discards any existing signature, turning a signed,
+# unaligned APK into an unsigned, unaligned APK.  Sadly |zip -q| doesn't
+# silence a warning about "nothing to do" so we pipe to /dev/null.
+RELEASE_SIGN_ANDROID_APK = \
+  cp $(1) $(2)-unaligned.apk && \
+  ($(ZIP) -d $(2)-unaligned.apk 'META-INF/*' > /dev/null || true) && \
+  $(RELEASE_JARSIGNER) $(2)-unaligned.apk && \
+  $(ZIPALIGN) -f -v 4 $(2)-unaligned.apk $(2) && \
+  $(RM) $(2)-unaligned.apk
 
 # Files packed into the APK root.  Packing files into the APK root is not
 # supported by modern Android build systems, including Gradle, so don't add to
