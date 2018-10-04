9a738c29b84d94452ffc247494734a44781d6b5d
Tom Schuster <evilpies@gmail.com>
2018-10-04 10:31:45 +0000

    Bug 396370 - Gtk (XDnd) Image/File drag and drop support r=karlt
    Based on a patch from Marco Pesenti Gritti (11 years ago)
    
    Depends on D7407
    
    Differential Revision: https://phabricator.services.mozilla.com/D7408
    
    --HG--
    extra : moz-landing-system : lando

--- widget/gtk/nsDragService.h
+++ widget/gtk/nsDragService.h
@@ -126,6 +126,8 @@ public:
                                  GtkSelectionData *selection_data,
                                  guint32           aTime);
 
+    void SourceBeginDrag(GdkDragContext *aContext);
+
     // set the drag icon during drag-begin
     void SetDragIcon(GdkDragContext* aContext);
 
