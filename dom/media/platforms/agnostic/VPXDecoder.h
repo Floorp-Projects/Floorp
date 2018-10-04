3f410e2c62c46f1d22eff18c354f46b50008a60a
Jean-Yves Avenard <jyavenard@mozilla.com>
2018-10-04 09:43:57 +0000

    Bug 1322234 - P3. Add method to read a VP9 profile from a VP9 bitstream r=TD-Linux
    Depends on D7616
    
    Differential Revision: https://phabricator.services.mozilla.com/D7647

--- dom/media/platforms/agnostic/VPXDecoder.h
+++ dom/media/platforms/agnostic/VPXDecoder.h
@@ -56,6 +56,10 @@ public:
   // Return the frame dimensions for a sample for the specified codec.
   static gfx::IntSize GetFrameSize(Span<const uint8_t> aBuffer, Codec aCodec);
 
+  // Return the VP9 profile as per https://www.webmproject.org/vp9/profiles/
+  // Return negative value if error.
+  static int GetVP9Profile(Span<const uint8_t> aBuffer);
+
 private:
   ~VPXDecoder();
   RefPtr<DecodePromise> ProcessDecode(MediaRawData* aSample);
