5c40ac23c9d60ea61ac14f54b31f7890a021f8d7
Emilio Cobos Álvarez <emilio@crisal.io>
2018-10-04 19:41:22 +0200

    Bug 1496014 - Fix shape-outside invalidation. r=dbaron
    It's not really invalidated anywhere, and the float manager code only checks for
    float region changes. Extend it so that it knows about shapes as well, and avoid
    reframing due to a bogus nsChangeHint_ScrollbarChange which really meant to be
    UpdateOverflow, which really is useless in this situation..
    
    Differential Revision: https://phabricator.services.mozilla.com/D7583

--- layout/generic/BlockReflowInput.cpp
+++ layout/generic/BlockReflowInput.cpp
@@ -713,6 +713,52 @@ FloatMarginISize(const ReflowInput& aCBReflowInput,
            ConvertTo(cbwm, wm).ISize(cbwm);
 }
 
+// A frame property that stores the last shape source / margin / etc. if there's
+// any shape, in order to invalidate the float area properly when it changes.
+//
+// TODO(emilio): This could really belong to GetRegionFor / StoreRegionFor, but
+// when I tried it was a bit awkward because of the logical -> physical
+// conversion that happens there.
+//
+// Maybe all this code could be refactored to make this cleaner, but keeping the
+// two properties separated was slightly nicer.
+struct ShapeInvalidationData
+{
+
+  StyleShapeSource mShapeOutside;
+  float mShapeImageThreshold = 0.0;
+  nsStyleCoord mShapeMargin;
+
+  ShapeInvalidationData() = default;
+
+  explicit ShapeInvalidationData(const nsStyleDisplay& aDisplay)
+  {
+    Update(aDisplay);
+  }
+
+  static bool IsNeeded(const nsStyleDisplay& aDisplay)
+  {
+    return aDisplay.mShapeOutside.GetType() != StyleShapeSourceType::None;
+  }
+
+  void Update(const nsStyleDisplay& aDisplay)
+  {
+    MOZ_ASSERT(IsNeeded(aDisplay));
+    mShapeOutside = aDisplay.mShapeOutside;
+    mShapeImageThreshold = aDisplay.mShapeImageThreshold;
+    mShapeMargin = aDisplay.mShapeMargin;
+  }
+
+  bool Matches(const nsStyleDisplay& aDisplay) const
+  {
+    return mShapeOutside == aDisplay.mShapeOutside &&
+           mShapeImageThreshold == aDisplay.mShapeImageThreshold &&
+           mShapeMargin == aDisplay.mShapeMargin;
+  }
+};
+
+NS_DECLARE_FRAME_PROPERTY_DELETABLE(ShapeInvalidationDataProperty, ShapeInvalidationData)
+
 bool
 BlockReflowInput::FlowAndPlaceFloat(nsIFrame* aFloat)
 {
@@ -732,6 +778,9 @@ BlockReflowInput::FlowAndPlaceFloat(nsIFrame* aFloat)
   // The float's old region, so we can propagate damage.
   LogicalRect oldRegion = nsFloatManager::GetRegionFor(wm, aFloat,
                                                        ContainerSize());
+
+  ShapeInvalidationData* invalidationData =
+    aFloat->GetProperty(ShapeInvalidationDataProperty());
 
   // Enforce CSS2 9.5.1 rule [2], i.e., make sure that a float isn't
   // ``above'' another float that preceded it in the flow.
@@ -996,9 +1045,14 @@ BlockReflowInput::FlowAndPlaceFloat(nsIFrame* aFloat)
   // store region
   nsFloatManager::StoreRegionFor(wm, aFloat, region, ContainerSize());
 
-  // If the float's dimensions have changed, note the damage in the
+  const bool invalidationDataNeeded =
+    ShapeInvalidationData::IsNeeded(*floatDisplay);
+
+  // If the float's dimensions or shape have changed, note the damage in the
   // float manager.
-  if (!region.IsEqualEdges(oldRegion)) {
+  if (!region.IsEqualEdges(oldRegion) ||
+      !!invalidationData != invalidationDataNeeded ||
+      (invalidationData && !invalidationData->Matches(*floatDisplay))) {
     // XXXwaterson conservative: we could probably get away with noting
     // less damage; e.g., if only height has changed, then only note the
     // area into which the float has grown or from which the float has
@@ -1006,6 +1060,18 @@ BlockReflowInput::FlowAndPlaceFloat(nsIFrame* aFloat)
     nscoord blockStart = std::min(region.BStart(wm), oldRegion.BStart(wm));
     nscoord blockEnd = std::max(region.BEnd(wm), oldRegion.BEnd(wm));
     FloatManager()->IncludeInDamage(blockStart, blockEnd);
+  }
+
+  if (invalidationDataNeeded) {
+    if (invalidationData) {
+      invalidationData->Update(*floatDisplay);
+    } else {
+      aFloat->SetProperty(ShapeInvalidationDataProperty(),
+                          new ShapeInvalidationData(*floatDisplay));
+    }
+  } else if (invalidationData) {
+    invalidationData = nullptr;
+    aFloat->DeleteProperty(ShapeInvalidationDataProperty());
   }
 
   if (!reflowStatus.IsFullyComplete()) {
