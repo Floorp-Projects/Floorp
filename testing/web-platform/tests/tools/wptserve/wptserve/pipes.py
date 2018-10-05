b3ef6542eecf334b7dfdf6514ae836739eb61c60
andypaicu <andypaicu@chromium.org>
2018-10-05 14:20:58 +0000

    Bug 1495009 [wpt PR 13261] - Revert pipes change made in PR#13228, a=testonly
    Automatic update from web-platform-testsRevert pipes change made in PR#13228 (#13261)
    
    --
    
    wpt-commits: a56b0171854750ead65a32cb1cc0005a3fad1e11
    wpt-pr: 13261

--- testing/web-platform/tests/tools/wptserve/wptserve/pipes.py
+++ testing/web-platform/tests/tools/wptserve/wptserve/pipes.py
@@ -471,23 +471,20 @@ def template(request, content, escape_type="html"):
             raise Exception("Undefined template variable %s" % field)
 
         while tokens:
-            ttype, tfield = tokens.popleft()
+            ttype, field = tokens.popleft()
             if ttype == "index":
-                value = value[tfield]
+                value = value[field]
             elif ttype == "arguments":
-                value = value(request, *tfield)
+                value = value(request, *field)
             else:
                 raise Exception(
-                    "unexpected token type %s (token '%r'), expected ident or arguments" % (ttype, tfield)
+                    "unexpected token type %s (token '%r'), expected ident or arguments" % (ttype, field)
                 )
 
         assert isinstance(value, (int, (binary_type, text_type))), tokens
 
         if variable is not None:
             variables[variable] = value
-
-        if field == "GET" and not isinstance(value, str):
-            value = value.decode("utf-8")
 
         escape_func = {"html": lambda x:escape(x, quote=True),
                        "none": lambda x:x}[escape_type]
