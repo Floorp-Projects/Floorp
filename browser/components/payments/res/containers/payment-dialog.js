16a28d34b1a7594cbe8d8da289661d6e93a28496
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2018-10-05 15:37:25 +0000

    Bug 1496698 - Remove Custom Elements polyfill from Web Payments UI. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D7863

--- browser/components/payments/res/containers/payment-dialog.js
+++ browser/components/payments/res/containers/payment-dialog.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import "../vendor/custom-elements.min.js";
-
 import PaymentStateSubscriberMixin from "../mixins/PaymentStateSubscriberMixin.js";
 import paymentRequest from "../paymentRequest.js";
 
