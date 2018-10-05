04ebfd677e6843be5aba00a4a063d28221c066d5
Alexandre Poirot <poirot.alex@gmail.com>
2018-10-05 08:31:55 +0000

    Bug 1461714 - Land debugger's main module sources r=jlast
    MozReview-Commit-ID: DXYRD1hWarA
    
    Depends on D1741
    
    Differential Revision: https://phabricator.services.mozilla.com/D7622

--- devtools/client/debugger/new/src/main.js
+++ devtools/client/debugger/new/src/main.js
@@ -1,35 +1,24 @@
-"use strict";
-
-var _react = require("devtools/client/shared/vendor/react");
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = require("devtools/client/shared/vendor/react-dom");
-
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
-var _devtoolsEnvironment = require("devtools/client/debugger/new/dist/vendors").vendored["devtools-environment"];
-
-var _client = require("./client/index");
-
-var _bootstrap = require("./utils/bootstrap");
-
-var _sourceQueue = require("./utils/source-queue");
-
-var _sourceQueue2 = _interopRequireDefault(_sourceQueue);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { isFirefoxPanel } from "devtools-environment";
+
+import { onConnect } from "./client";
+import { teardownWorkers } from "./utils/bootstrap";
+import sourceQueue from "./utils/source-queue";
+
 function unmountRoot() {
   const mount = document.querySelector("#mount .launchpad-root");
-
-  _reactDom2.default.unmountComponentAtNode(mount);
+  ReactDOM.unmountComponentAtNode(mount);
 }
 
-if ((0, _devtoolsEnvironment.isFirefoxPanel)()) {
+if (isFirefoxPanel()) {
   module.exports = {
     bootstrap: ({
       threadClient,
@@ -37,45 +26,38 @@ if ((0, _devtoolsEnvironment.isFirefoxPanel)()) {
       debuggerClient,
       sourceMaps,
       toolboxActions
-    }) => {
-      return (0, _client.onConnect)({
-        tab: {
-          clientType: "firefox"
+    }: any) => {
+      return onConnect(
+        {
+          tab: { clientType: "firefox" },
+          tabConnection: {
+            tabTarget,
+            threadClient,
+            debuggerClient
+          }
         },
-        tabConnection: {
-          tabTarget,
-          threadClient,
-          debuggerClient
+        {
+          services: { sourceMaps },
+          toolboxActions
         }
-      }, {
-        services: {
-          sourceMaps
-        },
-        toolboxActions
-      });
+      );
     },
     destroy: () => {
       unmountRoot();
-
-      _sourceQueue2.default.clear();
-
-      (0, _bootstrap.teardownWorkers)();
+      sourceQueue.clear();
+      teardownWorkers();
     }
   };
 } else {
-  const {
-    bootstrap,
-    L10N
-  } = require("devtools/shared/flags");
+  const { bootstrap, L10N } = require("devtools-launchpad");
 
-  window.L10N = L10N; // $FlowIgnore:
+  window.L10N = L10N;
+  // $FlowIgnore:
+  window.L10N.setBundle(require("../assets/panel/debugger.properties"));
 
-  window.L10N.setBundle(require("devtools/shared/flags"));
-  bootstrap(_react2.default, _reactDom2.default).then(connection => {
-    (0, _client.onConnect)(connection, {
-      services: {
-        sourceMaps: require("devtools/client/shared/source-map/index.js")
-      },
+  bootstrap(React, ReactDOM).then(connection => {
+    onConnect(connection, {
+      services: { sourceMaps: require("devtools-source-map") },
       toolboxActions: {}
     });
   });
