0aceef10ab6f5c5d328d7589c6e41ea4216ec8fd
Sebastian Kaspari <s.kaspari@gmail.com>
2018-10-05 18:03:12 +0200

    [components] Set version to 0.27.0.

--- mobile/android/android-components/buildSrc/src/main/java/Config.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Config.kt
@@ -7,7 +7,7 @@ object Config {
 
     // This version number should follow semantic versioning (MAJOR.MINOR.PATCH).
     // See https://semver.org/
-    const val componentsVersion = "0.26.0"
+    const val componentsVersion = "0.27.0"
 
     // Synchronized build configuration for all modules
     const val compileSdkVersion = 27
