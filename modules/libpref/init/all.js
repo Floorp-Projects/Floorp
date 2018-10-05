a2970b52690a5a866e87b421a2ea3d8ae4e6afb9
Gurzau Raul <rgurzau@mozilla.com>
2018-10-05 03:49:08 +0300

    Backed out changeset edc506b37439 (bug 1495300) for failing at /browser_toolbox_options_disable_buttons.js on a CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1091,8 +1091,6 @@ pref("devtools.recordreplay.enabled", true);
 pref("devtools.recordreplay.enableRewinding", true);
 #endif
 
-pref("devtools.recordreplay.mvp.enabled", false);
-
 // view source
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
