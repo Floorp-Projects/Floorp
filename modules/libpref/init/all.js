ac5abf5c6108c82382327f14ef3c21b4c207b95c
Tarek Ziadé <tarek@mozilla.com>
2018-10-05 15:54:03 +0000

    Bug 1496506 - Flip the pref for the new about:performance r=florian,baku
    This activates by default the new about:performance page and its underlying
    counters in Firefox
    
    MozReview-Commit-ID: LccL6zBHUHm
    
    Differential Revision: https://phabricator.services.mozilla.com/D7884
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -171,7 +171,7 @@ pref("dom.enable_performance_navigation_timing", true);
 pref("dom.performance.enable_notify_performance_timing", false);
 
 // Enable collecting of docgroup activity in the scheduler
-pref("dom.performance.enable_scheduler_timing", false);
+pref("dom.performance.enable_scheduler_timing", true);
 
 // Enable Permission API's .revoke() method
 pref("dom.permissions.revoke.enable", false);
@@ -5070,7 +5070,7 @@ pref("extensions.webextensions.ExtensionStorageIDB.enabled", false);
 #endif
 
 // if enabled, store execution times for API calls
-pref("extensions.webextensions.enablePerformanceCounters", false);
+pref("extensions.webextensions.enablePerformanceCounters", true);
 
 // Maximum age in milliseconds of performance counters in children
 // When reached, the counters are sent to the main process and
