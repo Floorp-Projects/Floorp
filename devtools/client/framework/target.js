b143c57852f5b4952b0fe1a2120627ccc5f2fde3
Gurzau Raul <rgurzau@mozilla.com>
2018-10-05 03:49:08 +0300

    Backed out changeset edc506b37439 (bug 1495300) for failing at /browser_toolbox_options_disable_buttons.js on a CLOSED TREE

--- devtools/client/framework/target.js
+++ devtools/client/framework/target.js
@@ -446,10 +446,6 @@ TabTarget.prototype = {
     return !this.window;
   },
 
-  get canRewind() {
-    return this.activeTab.traits.canRewind;
-  },
-
   getExtensionPathName(url) {
     // Return the url if the target is not a webextension.
     if (!this.isWebExtension) {
@@ -849,10 +845,6 @@ WorkerTarget.prototype = {
 
   get client() {
     return this._workerClient.client;
-  },
-
-  get canRewind() {
-    return false;
   },
 
   destroy: function() {
