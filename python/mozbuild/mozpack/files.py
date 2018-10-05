148577fc45e87a0240afa0b099cdc29d6d7537ca
Ciure Andrei <aciure@mozilla.com>
2018-10-05 22:11:05 +0300

    Backed out 2 changesets (bug 1418464) for test_packager.py build bustages CLOSED TREE
    Backed out changeset d6794cb231e1 (bug 1418464)
    Backed out changeset cc793c2d8cee (bug 1418464)

--- python/mozbuild/mozpack/files.py
+++ python/mozbuild/mozpack/files.py
@@ -21,7 +21,6 @@ from mozpack.executables import (
     strip,
     may_elfhack,
     elfhack,
-    xz_compress,
 )
 from mozpack.chrome.manifest import (
     ManifestEntry,
@@ -281,10 +280,6 @@ class ExecutableFile(File):
     File class for executable and library files on OS/2, OS/X and ELF systems.
     (see mozpack.executables.is_executable documentation).
     '''
-    def __init__(self, path, xz_compress=False):
-        File.__init__(self, path)
-        self.xz_compress = xz_compress
-
     def copy(self, dest, skip_if_older=True):
         real_dest = dest
         if not isinstance(dest, basestring):
@@ -294,7 +289,7 @@ class ExecutableFile(File):
         assert isinstance(dest, basestring)
         # If File.copy didn't actually copy because dest is newer, check the
         # file sizes. If dest is smaller, it means it is already stripped and
-        # elfhacked and xz_compressed, so we can skip.
+        # elfhacked, so we can skip.
         if not File.copy(self, dest, skip_if_older) and \
                 os.path.getsize(self.path) > os.path.getsize(dest):
             return False
@@ -303,8 +298,6 @@ class ExecutableFile(File):
                 strip(dest)
             if may_elfhack(dest):
                 elfhack(dest)
-            if self.xz_compress:
-                xz_compress(dest)
         except ErrorMessage:
             os.remove(dest)
             raise
