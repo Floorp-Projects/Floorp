2048175328048a5da13a6323fe5e7e4745c8d8f9
Andrew Swan <aswan@mozilla.com>
2018-10-05 21:30:08 -0700

    Bug 1496649 Make tresize a webextension r=mconley
    This is a pretty straightforward conversion.  We deliberately continue
    to use chrome: urls in this test since those are loaded in the parent
    process so test measurements will not include additional noise from
    IPC to a content process.

---
+++ testing/talos/talos/tests/tresize/addon/api.js
@@ -0,0 +1,48 @@
+"use strict";
+
+ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  Services: "resource://gre/modules/Services.jsm",
+});
+
+XPCOMUtils.defineLazyServiceGetter(this, "aomStartup",
+                                   "@mozilla.org/addons/addon-manager-startup;1",
+                                   "amIAddonManagerStartup");
+
+/* globals ExtensionAPI */
+
+const PREFIX = "tresize@mozilla.org";
+
+this.tresize = class extends ExtensionAPI {
+  onStartup() {
+    const manifestURI = Services.io.newURI("manifest.json", null, this.extension.rootURI);
+    this.chromeHandle = aomStartup.registerChrome(manifestURI, [
+      ["content", "tresize", "chrome/"],
+    ]);
+
+    let {baseURI} = this.extension;
+
+    this.listener = function listener({target, data}) {
+      let win = target.ownerGlobal;
+      Services.scriptloader.loadSubScript(baseURI.resolve("/content/Profiler.js"), win);
+      Services.scriptloader.loadSubScript(baseURI.resolve("/content/tresize.js"), win);
+
+      function sendResult(result) {
+        target.messageManager.sendAsyncMessage(`${PREFIX}:chrome-run-reply`, {id: data.id, result});
+      }
+      win.runTest(sendResult, data.locationSearch);
+    };
+
+    Services.mm.addMessageListener(`${PREFIX}:chrome-run-message`, this.listener);
+
+    this.framescriptURL = baseURI.resolve("/content/framescript.js");
+    Services.mm.loadFrameScript(this.framescriptURL, true);
+  }
+
+  onShutdown() {
+    Services.mm.removeDelayedFrameScript(this.framescriptURL);
+    Services.mm.removeMessageListener(`${PREFIX}:chrome-run-message`, this.listener);
+    this.chromeHandle.destruct();
+  }
+};
