1b90794ae0b88a4f4926c9c42fe07ed493a33aa5
Jan Beich <jbeich@FreeBSD.org>
2018-10-08 05:12:00 +0300

    Bug 1497169 - Unbreak bundled libevent on FreeBSD 12. r=froydnj

--- ipc/chromium/src/third_party/libevent/include/event2/util.h
+++ ipc/chromium/src/third_party/libevent/include/event2/util.h
@@ -842,7 +842,7 @@ int evutil_secure_rng_init(void);
 EVENT2_EXPORT_SYMBOL
 int evutil_secure_rng_set_urandom_device_file(char *fname);
 
-#if !defined(__OpenBSD__) && !defined(ANDROID)
+#if !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(ANDROID)
 /** Seed the random number generator with extra random bytes.
 
     You should almost never need to call this function; it should be
