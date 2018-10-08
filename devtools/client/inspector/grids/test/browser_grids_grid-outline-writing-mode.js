34f7b8b16c8ecaeb76367766f2674f4a020e16db
Gabriel Luong <gabriel.luong@gmail.com>
2018-10-08 13:40:47 -0400

    Bug 1317102 - Part 6: Hide the grid outline when multiple grids are highlighted. r=pbro

--- devtools/client/inspector/grids/test/browser_grids_grid-outline-writing-mode.js
+++ devtools/client/inspector/grids/test/browser_grids_grid-outline-writing-mode.js
@@ -61,6 +61,7 @@ const TEST_URI = `
 `;
 
 add_task(async function() {
+  await pushPref("devtools.gridinspector.maxHighlighters", 1);
   await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
 
   const { inspector, gridInspector } = await openLayoutView();
