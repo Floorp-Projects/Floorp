a6faf157508423e35b3bc6bd3b8d06f22ef0aada
Jan Beich <jbeich@FreeBSD.org>
2018-10-08 05:12:00 +0300

    Bug 1497169 - Unbreak bundled libevent on FreeBSD 12. r=froydnj, a=NPOTB
    --HG--
    extra : source : 4ef3760a29be76f5c30707d1330e13459b684eab

--- ipc/chromium/src/third_party/libevent/evutil_rand.c
+++ ipc/chromium/src/third_party/libevent/evutil_rand.c
@@ -192,7 +192,7 @@ evutil_secure_rng_get_bytes(void *buf, size_t n)
 	ev_arc4random_buf(buf, n);
 }
 
-#if !defined(__OpenBSD__) && !defined(ANDROID)
+#if !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(ANDROID)
 void
 evutil_secure_rng_add_bytes(const char *buf, size_t n)
 {
