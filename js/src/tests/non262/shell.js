855b5e0f8e2c83f8cc6d7691aa3f0ac1fbf96572
Christian Holler <choller@mozilla.com>
2018-10-08 16:05:37 +0000

    Bug 1496868 - Make assertThrowsValue and assertThrowsInstanceOf overridable. r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D7952

--- js/src/tests/non262/shell.js
+++ js/src/tests/non262/shell.js
@@ -111,39 +111,43 @@
     }
   };
 
-  global.assertThrowsValue = function assertThrowsValue(f, val, msg) {
-    var fullmsg;
-    try {
-      f();
-    } catch (exc) {
-      if ((exc === val) === (val === val) && (val !== 0 || 1 / exc === 1 / val))
-        return;
-      fullmsg = "Assertion failed: expected exception " + val + ", got " + exc;
-    }
-    if (fullmsg === undefined)
-      fullmsg = "Assertion failed: expected exception " + val + ", no exception thrown";
-    if (msg !== undefined)
-      fullmsg += " - " + msg;
-    throw new Error(fullmsg);
-  };
+  if (typeof global.assertThrowsValue === 'undefined') {
+    global.assertThrowsValue = function assertThrowsValue(f, val, msg) {
+      var fullmsg;
+      try {
+        f();
+      } catch (exc) {
+        if ((exc === val) === (val === val) && (val !== 0 || 1 / exc === 1 / val))
+          return;
+        fullmsg = "Assertion failed: expected exception " + val + ", got " + exc;
+      }
+      if (fullmsg === undefined)
+        fullmsg = "Assertion failed: expected exception " + val + ", no exception thrown";
+      if (msg !== undefined)
+        fullmsg += " - " + msg;
+      throw new Error(fullmsg);
+    };
+  }
 
-  global.assertThrowsInstanceOf = function assertThrowsInstanceOf(f, ctor, msg) {
-    var fullmsg;
-    try {
-      f();
-    } catch (exc) {
-      if (exc instanceof ctor)
-        return;
-      fullmsg = `Assertion failed: expected exception ${ctor.name}, got ${exc}`;
-    }
+  if (typeof global.assertThrowsInstanceOf === 'undefined') {
+    global.assertThrowsInstanceOf = function assertThrowsInstanceOf(f, ctor, msg) {
+      var fullmsg;
+      try {
+        f();
+      } catch (exc) {
+        if (exc instanceof ctor)
+          return;
+        fullmsg = `Assertion failed: expected exception ${ctor.name}, got ${exc}`;
+      }
 
-    if (fullmsg === undefined)
-      fullmsg = `Assertion failed: expected exception ${ctor.name}, no exception thrown`;
-    if (msg !== undefined)
-      fullmsg += " - " + msg;
+      if (fullmsg === undefined)
+        fullmsg = `Assertion failed: expected exception ${ctor.name}, no exception thrown`;
+      if (msg !== undefined)
+        fullmsg += " - " + msg;
 
-    throw new Error(fullmsg);
-  };
+      throw new Error(fullmsg);
+    };
+  }
 
   global.assertDeepEq = (function(){
     var call = Function.prototype.call,
