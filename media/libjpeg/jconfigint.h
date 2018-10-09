21a09cdf6916bc18d0be5c18596b7a9bbdb7c3df
Ryan VanderMeulen <ryanvm@gmail.com>
2018-10-09 15:23:57 +0000

    Bug 1425835 - Update libjpeg-turbo to version 2.0.0. r=aosmond
    Also includes the fix for upstream issue #288 to avoid crashes on some older Win7 systems.
    
    Differential Revision: https://phabricator.services.mozilla.com/D7758

--- media/libjpeg/jconfigint.h
+++ media/libjpeg/jconfigint.h
@@ -1,7 +1,37 @@
-#define VERSION "1.5.2"
-#define BUILD "2017-07-07"
-#define PACKAGE_NAME "libjpeg-turbo"
+/* libjpeg-turbo build number */
+#define BUILD "20181004"
 
 /* Need to use Mozilla-specific function inlining. */
 #include "mozilla/Attributes.h"
 #define INLINE MOZ_ALWAYS_INLINE
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libjpeg-turbo"
+
+/* Version number of package */
+#define VERSION "2.0.0"
+
+/* The size of `size_t', as computed by sizeof. */
+#ifdef HAVE_64BIT_BUILD
+#define SIZEOF_SIZE_T 8
+#else
+#define SIZEOF_SIZE_T 4
+#endif
+
+/* Define if your compiler has __builtin_ctzl() and sizeof(unsigned long) == sizeof(size_t). */
+#ifndef _MSC_VER
+#define HAVE_BUILTIN_CTZL 1
+#endif
+
+/* Define to 1 if you have the <intrin.h> header file. */
+#ifdef _MSC_VER
+#define HAVE_INTRIN_H 1
+#endif
+
+#if defined(_MSC_VER) && defined(HAVE_INTRIN_H)
+#if (SIZEOF_SIZE_T == 8)
+#define HAVE_BITSCANFORWARD64
+#elif (SIZEOF_SIZE_T == 4)
+#define HAVE_BITSCANFORWARD
+#endif
+#endif
