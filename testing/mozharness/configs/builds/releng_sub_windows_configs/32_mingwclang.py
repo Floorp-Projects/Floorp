116a9564001f1dad72c410d38bfe8108d5779b8c
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-10-09 19:26:51 +0300

    Backed out changeset df47d3ad9cc6 (bug 1475998) for Windows MinGW bustages.

--- testing/mozharness/configs/builds/releng_sub_windows_configs/32_mingwclang.py
+++
@@ -1,7 +0,0 @@
-config = {
-    'base_name': 'WINNT_5.2_MINGWCLANG_%(branch)s',
-    'platform': 'win32-mingwclang',
-    'stage_platform': 'win32-mingwclang',
-    'mozconfig_platform': 'win32',
-    'mozconfig_variant': 'mingwclang',
-}
