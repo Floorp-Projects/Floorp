d76fc75015a5f3aa0a4a866dc9b1e4279f728b3d
Jason Orendorff <jorendorff@mozilla.com>
2018-10-09 15:34:35 +0000

    Bug 1445854 - Part 3: Use GenericCreatePrototype in more places. r=arai
    Depends on D7667
    
    Differential Revision: https://phabricator.services.mozilla.com/D7783

--- js/src/builtin/BigInt.h
+++ js/src/builtin/BigInt.h
@@ -24,6 +24,7 @@ class BigIntObject : public NativeObject
   public:
     static const ClassSpec classSpec_;
     static const Class class_;
+    static const Class protoClass_;
 
     static JSObject* create(JSContext* cx, JS::Handle<JS::BigInt*> bi);
 
