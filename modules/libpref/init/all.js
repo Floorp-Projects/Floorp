ab78780c77abec587d6d558b14bd0a4a53e45ae3
Jean-Yves Avenard <jyavenard@mozilla.com>
2018-10-09 20:36:18 +0000

    Bug 1496529 - P8. Add media.navigator.mediadatadecoder_h264_enabled preference. r=drno
    For now, the H264 decoding support is rather sturdy. It handles change of resolution and content smoothly thanks to the H264Converter.
    The VP8/VP9 one however, not so much.
    
    So we make a preference to only enable H264 for now.
    
    Depends on D7895
    
    Differential Revision: https://phabricator.services.mozilla.com/D7908
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -447,6 +447,8 @@ pref("media.navigator.video.max_fr", 60);
 pref("media.navigator.video.h264.level", 31); // 0x42E01f - level 3.1
 pref("media.navigator.video.h264.max_br", 0);
 pref("media.navigator.video.h264.max_mbps", 0);
+pref("media.navigator.mediadatadecoder_enabled", false);
+pref("media.navigator.mediadatadecoder_h264_enabled", false);
 pref("media.peerconnection.video.vp9_enabled", true);
 pref("media.peerconnection.video.vp9_preferred", false);
 pref("media.getusermedia.aec", 1);
