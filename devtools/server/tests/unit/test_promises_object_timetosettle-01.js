e03170e01c447214720363779a31e923e4f24268
Alexandre Poirot <poirot.alex@gmail.com>
2018-10-09 09:31:08 +0000

    Bug 1492265 - Use a content process target actor for xpcshell debugging. r=jdescottes
    MozReview-Commit-ID: J9XTgC0EBPG
    
    Depends on D7415
    
    Differential Revision: https://phabricator.services.mozilla.com/D7416

--- devtools/server/tests/unit/test_promises_object_timetosettle-01.js
+++ devtools/server/tests/unit/test_promises_object_timetosettle-01.js
@@ -19,7 +19,6 @@ add_task(async function() {
   ok(Promise.toString().includes("native code"), "Expect native DOM Promise.");
 
   // We have to attach the chrome target actor before playing with the PromiseActor
-  await attachTarget(client, parentProcessActors);
   await testGetTimeToSettle(client, parentProcessActors, () => {
     const p = new Promise(() => {});
     p.name = "p";
