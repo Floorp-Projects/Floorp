e31a432ca59054e838307b62cc2ff5c858e602b1
David Walsh <dwalsh@mozilla.com>
2018-10-08 20:23:46 -0500

    Bug 1314057 - Move worker actor and console tests to shared. r=loganfsmyth
    --HG--
    rename : devtools/client/debugger/test/mochitest/browser_dbg_WorkerTargetActor.attach.js => devtools/client/shared/test/browser_dbg_WorkerTargetActor.attach.js
    rename : devtools/client/debugger/test/mochitest/browser_dbg_worker-console-01.js => devtools/client/shared/test/browser_dbg_worker-console-01.js
    rename : devtools/client/debugger/test/mochitest/browser_dbg_worker-console-02.js => devtools/client/shared/test/browser_dbg_worker-console-02.js
    rename : devtools/client/debugger/test/mochitest/browser_dbg_worker-console-03.js => devtools/client/shared/test/browser_dbg_worker-console-03.js
    rename : devtools/client/debugger/test/mochitest/browser_dbg_worker-console-04.js => devtools/client/shared/test/browser_dbg_worker-console-04.js
    rename : devtools/client/debugger/test/mochitest/code_WorkerTargetActor.attach-worker1.js => devtools/client/shared/test/code_WorkerTargetActor.attach-worker1.js
    rename : devtools/client/debugger/test/mochitest/code_WorkerTargetActor.attach-worker2.js => devtools/client/shared/test/code_WorkerTargetActor.attach-worker2.js
    rename : devtools/client/debugger/test/mochitest/code_WorkerTargetActor.attachThread-worker.js => devtools/client/shared/test/code_WorkerTargetActor.attachThread-worker.js
    rename : devtools/client/debugger/test/mochitest/code_frame-script.js => devtools/client/shared/test/code_frame-script.js
    rename : devtools/client/debugger/test/mochitest/doc_WorkerTargetActor.attach-tab1.html => devtools/client/shared/test/doc_WorkerTargetActor.attach-tab1.html
    rename : devtools/client/debugger/test/mochitest/doc_WorkerTargetActor.attach-tab2.html => devtools/client/shared/test/doc_WorkerTargetActor.attach-tab2.html
    rename : devtools/client/debugger/test/mochitest/doc_WorkerTargetActor.attachThread-tab.html => devtools/client/shared/test/doc_WorkerTargetActor.attachThread-tab.html

--- devtools/client/shared/test/browser_dbg_worker-console-03.js
+++ devtools/client/shared/test/browser_dbg_worker-console-03.js
@@ -1,38 +1,45 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
 // Check to make sure that a worker can be attached to a toolbox
 // and that the console works.
+
+// Import helpers for the workers
+/* import-globals-from helper_workers.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/shared/test/helper_workers.js",
+  this);
 
 var TAB_URL = EXAMPLE_URL + "doc_WorkerTargetActor.attachThread-tab.html";
 var WORKER_URL = "code_WorkerTargetActor.attachThread-worker.js";
 
 // Test to see if creating the pause from the console works.
 add_task(async function testPausedByConsole() {
-  let {client, tab, tabClient, workerClient, toolbox, gDebugger} =
-    await initWorkerDebugger(TAB_URL, WORKER_URL);
+  const dbg = await initWorkerDebugger(TAB_URL, WORKER_URL);
+  const {client, tab, workerClient, toolbox} = dbg;
 
-  let gTarget = gDebugger.gTarget;
-  let gResumeButton = gDebugger.document.getElementById("resume");
-  let gResumeKey = gDebugger.document.getElementById("resumeKey");
-
-  let jsterm = await getSplitConsole(toolbox);
+  const jsterm = await getSplitConsole(toolbox);
   let executed = await jsterm.execute("10000+1");
   ok(executed.textContent.includes("10001"),
       "Text for message appeared correct");
 
-  let oncePaused = gTarget.once("thread-paused");
-  EventUtils.sendMouseEvent({ type: "mousedown" }, gResumeButton, gDebugger);
-  let pausedExecution = jsterm.execute("10000+2");
+  await clickElement(dbg, "pause");
+
+  const pausedExecution = jsterm.execute("10000+2");
 
   info("Executed a command with 'break on next' active, waiting for pause");
-  await oncePaused;
+  await waitForPaused(dbg);
 
   executed = await jsterm.execute("10000+3");
   ok(executed.textContent.includes("10003"),
       "Text for message appeared correct");
 
   info("Waiting for a resume");
-  let onceResumed = gTarget.once("thread-resumed");
-  EventUtils.sendMouseEvent({ type: "mousedown" }, gResumeButton, gDebugger);
-  await onceResumed;
+  await clickElement(dbg, "resume");
 
   executed = await pausedExecution;
   ok(executed.textContent.includes("10002"),
---
+++ devtools/client/shared/test/code_WorkerTargetActor.attachThread-worker.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function f() {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+
+  return [a, b, c];
+}
+
+self.onmessage = function(event) {
+  if (event.data == "ping") {
+    f();
+    postMessage("pong");
+  }
+};
+
+postMessage("load");
