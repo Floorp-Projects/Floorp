86c0ba4fda8825112551965ac6c71bec7a5c56f4
Randell Jesup <rjesup@jesup.org>
2018-10-09 22:28:56 -0400

    Bug 1464509: Add memory replacer with counters to the Gecko profiler r=glandium

---
+++ tools/profiler/core/memory_hooks.h
@@ -0,0 +1,18 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef memory_hooks_h
+#define memory_hooks_h
+
+#if defined(MOZ_REPLACE_MALLOC) && defined(MOZ_PROFILER_MEMORY)
+namespace mozilla {
+namespace profiler {
+void install_memory_counter(bool aInstall);
+}
+}
+#endif
+
+#endif
