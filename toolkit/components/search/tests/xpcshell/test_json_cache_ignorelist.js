87e9bd57770f96741087821eef627666b4e369b2
Michael Kaply <mozilla@kaply.com>
2018-10-10 17:11:37 +0000

    Bug 1475989 - add support for loadPath blocking r=mikedeboer
    Differential Revision: https://phabricator.services.mozilla.com/D7620

--- toolkit/components/search/tests/xpcshell/test_json_cache_ignorelist.js
+++ toolkit/components/search/tests/xpcshell/test_json_cache_ignorelist.js
@@ -13,7 +13,7 @@ var cacheTemplate, appPluginsPath, profPlugins;
  * Test reading from search.json.mozlz4
  */
 function run_test() {
-  let cacheTemplateFile = do_get_file("data/search_blacklist.json");
+  let cacheTemplateFile = do_get_file("data/search_ignorelist.json");
   cacheTemplate = readJSONFile(cacheTemplateFile);
   cacheTemplate.buildID = getAppInfo().platformBuildID;
 
