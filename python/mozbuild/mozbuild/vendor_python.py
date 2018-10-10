715653b765c6f49d14ba01578f1072b35a0a9084
Ryan VanderMeulen <ryanvm@gmail.com>
2018-10-10 12:29:18 -0400

    Backed out 5 changesets (bug 1481612, bug 1483651) for merge conflicts with bug 1490253.
    Backed out changeset de7e35a459e3 (bug 1481612)
    Backed out changeset eb440bc9fed1 (bug 1481612)
    Backed out changeset 7f8cad00dc2e (bug 1483651)
    Backed out changeset 1ecd5ddcdd79 (bug 1481612)
    Backed out changeset c8f12706c421 (bug 1481612)

--- python/mozbuild/mozbuild/vendor_python.py
+++ python/mozbuild/mozbuild/vendor_python.py
@@ -5,7 +5,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
-import shutil
 import subprocess
 
 import mozfile
@@ -17,7 +16,7 @@ from mozpack.files import FileFinder
 
 class VendorPython(MozbuildObject):
 
-    def vendor(self, packages=None, with_windows_wheel=False):
+    def vendor(self, packages=None):
         self.populate_logger()
         self.log_manager.enable_unstructured()
 
@@ -25,8 +24,6 @@ class VendorPython(MozbuildObject):
             self.topsrcdir, os.path.join('third_party', 'python'))
 
         packages = packages or []
-        if with_windows_wheel and len(packages) != 1:
-            raise Exception('--with-windows-wheel is only supported for a single package!')
         pipenv = self.ensure_pipenv()
 
         for package in packages:
@@ -54,21 +51,6 @@ class VendorPython(MozbuildObject):
                     '--dest', tmp,
                     '--no-binary', ':all:',
                     '--disable-pip-version-check'])
-                if with_windows_wheel:
-                    # This is hardcoded to CPython 2.7 for win64, which is good
-                    # enough for what we need currently. If we need psutil for Python 3
-                    # in the future that coudl be added here as well.
-                    self.virtualenv_manager._run_pip([
-                        'download',
-                        '--dest', tmp,
-                        '--no-deps',
-                        '--only-binary', ':all:',
-                        '--platform', 'win_amd64',
-                        '--implementation', 'cp',
-                        '--python-version', '27',
-                        '--abi', 'none',
-                        '--disable-pip-version-check',
-                        packages[0]])
                 self._extract(tmp, vendor_dir)
 
         self.repository.add_remove_files(vendor_dir)
@@ -77,31 +59,8 @@ class VendorPython(MozbuildObject):
         """extract source distribution into vendor directory"""
         finder = FileFinder(src)
         for path, _ in finder.find('*'):
-            base, ext = os.path.splitext(path)
-            if ext == '.whl':
-                # Wheels would extract into a directory with the name of the package, but
-                # we want the platform signifiers, minus the version number.
-                # Wheel filenames look like:
-                # {distribution}-{version}(-{build tag})?-{python tag}-{abi tag}-{platform tag}
-                bits = base.split('-')
-
-                # Remove the version number.
-                bits.pop(1)
-                target = os.path.join(dest, '-'.join(bits))
-                mozfile.remove(target)  # remove existing version of vendored package
-                os.mkdir(target)
-                mozfile.extract(os.path.join(finder.base, path), target)
-            else:
-                # packages extract into package-version directory name and we strip the version
-                tld = mozfile.extract(os.path.join(finder.base, path), dest)[0]
-                target = os.path.join(dest, tld.rpartition('-')[0])
-                mozfile.remove(target)  # remove existing version of vendored package
-                mozfile.move(tld, target)
-            # If any files inside the vendored package were symlinks, turn them into normal files
-            # because hg.mozilla.org forbids symlinks in the repository.
-            link_finder = FileFinder(target)
-            for _, f in link_finder.find('**'):
-                if os.path.islink(f.path):
-                    link_target = os.path.realpath(f.path)
-                    os.unlink(f.path)
-                    shutil.copyfile(link_target, f.path)
+            # packages extract into package-version directory name and we strip the version
+            tld = mozfile.extract(os.path.join(finder.base, path), dest)[0]
+            target = os.path.join(dest, tld.rpartition('-')[0])
+            mozfile.remove(target)  # remove existing version of vendored package
+            mozfile.move(tld, target)
