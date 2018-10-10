d9d6f34977a8402dd67a4d80348713260407b1a2
Tom Prince <mozilla@hocat.ca>
2018-10-10 23:08:18 +0000

    Bug 1498019: [release] Don't set shipping phase based on target_tasks; r=aki
    The phase of a task doesn't depend on which phase graph it is being generated in.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8303
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/taskgraph/transforms/beetmover.py
+++ taskcluster/taskgraph/transforms/beetmover.py
@@ -12,7 +12,6 @@ from taskgraph.util.attributes import copy_attributes_from_dependent_job
 from taskgraph.util.schema import validate_schema, Schema
 from taskgraph.util.scriptworker import (get_beetmover_bucket_scope,
                                          get_beetmover_action_scope,
-                                         get_phase,
                                          get_worker_type_for_scope)
 from taskgraph.util.taskcluster import get_artifact_prefix
 from taskgraph.transforms.task import task_description_schema
@@ -140,7 +139,7 @@ beetmover_description_schema = Schema({
     # locale is passed only for l10n beetmoving
     Optional('locale'): basestring,
 
-    Optional('shipping-phase'): task_description_schema['shipping-phase'],
+    Required('shipping-phase'): task_description_schema['shipping-phase'],
     Optional('shipping-product'): task_description_schema['shipping-product'],
 })
 
@@ -198,7 +197,6 @@ def make_task_description(config, jobs):
 
         bucket_scope = get_beetmover_bucket_scope(config)
         action_scope = get_beetmover_action_scope(config)
-        phase = get_phase(config)
 
         task = {
             'label': label,
@@ -209,7 +207,7 @@ def make_task_description(config, jobs):
             'attributes': attributes,
             'run-on-projects': dep_job.attributes.get('run_on_projects'),
             'treeherder': treeherder,
-            'shipping-phase': phase,
+            'shipping-phase': job['shipping-phase'],
         }
 
         yield task
