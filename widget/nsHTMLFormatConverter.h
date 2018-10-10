6fcac07b5ef810768524b951619d652fb4cecce4
Tom Schuster <evilpies@gmail.com>
2018-10-10 20:26:06 +0000

    Bug 1497831 - Use nsTArray instead of nsIArray for nsIFormatConverter. r=smaug
    Depends on D8194
    
    Differential Revision: https://phabricator.services.mozilla.com/D8195
    
    --HG--
    extra : moz-landing-system : lando

--- widget/nsHTMLFormatConverter.h
+++ widget/nsHTMLFormatConverter.h
@@ -25,8 +25,6 @@ public:
 protected:
   virtual ~nsHTMLFormatConverter();
 
-  nsresult AddFlavorToList ( nsCOMPtr<nsIMutableArray>& inList, const char* inFlavor ) ;
-
   NS_IMETHOD ConvertFromHTMLToUnicode(const nsAutoString & aFromStr, nsAutoString & aToStr);
   NS_IMETHOD ConvertFromHTMLToAOLMail(const nsAutoString & aFromStr, nsAutoString & aToStr);
 
