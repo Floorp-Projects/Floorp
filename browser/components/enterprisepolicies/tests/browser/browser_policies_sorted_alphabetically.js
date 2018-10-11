7d4ee76a9d519e02896d2abc0f7ed909cfe9dcc9
Felipe Gomes <felipc@gmail.com>
2018-10-11 21:36:56 +0000

    Bug 1498032 - Support flat policies on Mac. r=spohl
    Differential Revision: https://phabricator.services.mozilla.com/D8302
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/enterprisepolicies/tests/browser/browser_policies_sorted_alphabetically.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policies_sorted_alphabetically.js
@@ -23,3 +23,8 @@ add_task(async function test_policies_sorted() {
   checkArrayIsSorted(Object.keys(schema.properties), "policies-schema.json is alphabetically sorted.");
   checkArrayIsSorted(Object.keys(Policies), "Policies.jsm is alphabetically sorted.");
 });
+
+add_task(async function check_naming_conventions() {
+  let { schema } = ChromeUtils.import("resource:///modules/policies/schema.jsm", {});
+  is(Object.keys(schema.properties).some(key => key.includes("__")), false, "Can't use __ in a policy name as it's used as a delimiter");
+});
