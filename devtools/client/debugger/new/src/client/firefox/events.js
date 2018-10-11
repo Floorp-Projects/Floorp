f0f253f80796379ef297a6000c89ff8e2b20a4cb
Alexandre Poirot <poirot.alex@gmail.com>
2018-10-11 15:46:28 +0000

    Bug 1485660 - Switch from listening from DebuggerClient to TargetFront. r=jdescottes
    * debugger-controller and events.js are special and require to support two cases because this is
    the only production codepath that can have a TabTarget or a WorkerTarget.
    Thus, leading to either TargetFront or WorkerClient on target.activeTab.
    * webide.js doesn't need to listen for tabNavigated, this is redundant with tabListChanged.
    * application's initializer. In case you are wondering this code can't be spawn against a WorkerTarget.
    The application panel doesn't work in worker toolboxes.
    * The code modified in target is in TabTarget, so we don't have to support the WorkerClient case, we always have a TargetFront here.
    * I tried to update the doc file the best I can but this all feel outdated.
    
    MozReview-Commit-ID: 2hGchebfIub
    
    Depends on D7458
    
    Differential Revision: https://phabricator.services.mozilla.com/D7459

--- devtools/client/debugger/new/src/client/firefox/events.js
+++ devtools/client/debugger/new/src/client/firefox/events.js
@@ -41,7 +41,13 @@ function setupEvents(dependencies) {
     });
 
     if (threadClient._parent) {
-      threadClient._parent.addListener("workerListChanged", workerListChanged);
+      // Parent may be BrowsingContextTargetFront and be protocol.js.
+      // Or DebuggerClient/WorkerClient and still be old fashion actor.
+      if (threadClient._parent.on) {
+        threadClient._parent.on("workerListChanged", workerListChanged);
+      } else {
+        threadClient._parent.addListener("workerListChanged", workerListChanged);
+      }
     }
   }
 }
