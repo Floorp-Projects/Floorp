b572526d9c7fff214bcdc92e81e59beddc981389
Thomas Wisniewski <wisniewskit@gmail.com>
2018-10-11 15:53:23 +0000

    Bug 1451485 - convert webcompat-reporter to a webextension; r=aswan,Pike
    convert webcompat-reporter to a webextension
    
    Differential Revision: https://phabricator.services.mozilla.com/D6575
    
    --HG--
    rename : browser/extensions/webcompat-reporter/skin/lightbulb.svg => browser/extensions/webcompat-reporter/icons/lightbulb.svg
    extra : moz-landing-system : lando

--- browser/extensions/webcompat-reporter/test/browser/browser_report_site_issue.js
+++ browser/extensions/webcompat-reporter/test/browser/browser_report_site_issue.js
@@ -1,19 +1,22 @@
+"use strict";
+
 /* Test that clicking on the Report Site Issue button opens a new tab
    and sends a postMessaged blob to it. */
 add_task(async function test_opened_page() {
   requestLongerTimeout(2);
 
+  const serverLanding = await startIssueServer();
+
   // ./head.js sets the value for PREF_WC_REPORTER_ENDPOINT
   await SpecialPowers.pushPrefEnv({set: [
     [PREF_WC_REPORTER_ENABLED, true],
-    [PREF_WC_REPORTER_ENDPOINT, NEW_ISSUE_PAGE],
+    [PREF_WC_REPORTER_ENDPOINT, serverLanding],
   ]});
 
   let tab1 = await BrowserTestUtils.openNewForegroundTab(gBrowser, TEST_PAGE);
 
   await openPageActions();
-  let webcompatButton = document.getElementById(WC_PAGE_ACTION_ID);
-  ok(webcompatButton, "Report Site Issue button exists.");
+  await isPanelItemEnabled();
 
   let screenshotPromise;
   let newTabPromise = new Promise(resolve => {
@@ -22,23 +25,58 @@ add_task(async function test_opened_page() {
       screenshotPromise = BrowserTestUtils.waitForContentEvent(
         tab.linkedBrowser, "ScreenshotReceived", false, null, true);
       resolve(tab);
-    }, { once: true });
+    }, {once: true});
   });
-  webcompatButton.click();
+  document.getElementById(WC_PAGE_ACTION_PANEL_ID).click();
   let tab2 = await newTabPromise;
   await screenshotPromise;
 
-  await ContentTask.spawn(tab2.linkedBrowser, {TEST_PAGE}, function(args) {
+  await ContentTask.spawn(tab2.linkedBrowser, {TEST_PAGE}, async function(args) {
+    async function isGreen(dataUrl) {
+      const getPixel = await new Promise(resolve => {
+        const myCanvas = content.document.createElement("canvas");
+        const ctx = myCanvas.getContext("2d");
+        const img = new content.Image();
+        img.onload = () => {
+          ctx.drawImage(img, 0, 0);
+          resolve((x, y) => {
+            return ctx.getImageData(x, y, 1, 1).data;
+          });
+        };
+        img.src = dataUrl;
+      });
+      function isPixelGreenFuzzy(p) { // jpeg, so it will be off slightly
+        const fuzz = 4;
+        return p[0] < fuzz && Math.abs(p[1] - 128) < fuzz && p[2] < fuzz;
+      }
+      ok(isPixelGreenFuzzy(getPixel(0, 0)), "The pixels were green");
+    }
+
     let doc = content.document;
     let urlParam = doc.getElementById("url").innerText;
     let preview = doc.getElementById("screenshot-preview");
     is(urlParam, args.TEST_PAGE, "Reported page is correctly added to the url param");
 
     let detailsParam = doc.getElementById("details").innerText;
-    ok(typeof JSON.parse(detailsParam) == "object", "Details param is a stringified JSON object.");
+    const details = JSON.parse(detailsParam);
+    ok(typeof details == "object", "Details param is a stringified JSON object.");
+    ok(Array.isArray(details.consoleLog), "Details has a consoleLog array.");
+    ok(typeof details.buildID == "string", "Details has a buildID string.");
+    ok(typeof details.channel == "string", "Details has a channel string.");
+    ok(typeof details.hasTouchScreen == "boolean", "Details has a hasTouchScreen flag.");
+    ok(typeof details["mixed active content blocked"] == "boolean", "Details has a mixed active content blocked flag.");
+    ok(typeof details["mixed passive content blocked"] == "boolean", "Details has a mixed passive content blocked flag.");
+    ok(typeof details["tracking content blocked"] == "string", "Details has a tracking content blocked string.");
+    ok(typeof details["gfx.webrender.all"] == "boolean", "Details has gfx.webrender.all.");
+    ok(typeof details["gfx.webrender.blob-images"] == "boolean", "Details has gfx.webrender.blob-images.");
+    ok(typeof details["gfx.webrender.enabled"] == "boolean", "Details has gfx.webrender.enabled.");
+    ok(typeof details["image.mem.shared"] == "boolean", "Details has image.mem.shared.");
 
     is(preview.innerText, "Pass", "A Blob object was successfully transferred to the test page.");
-    ok(preview.style.backgroundImage.startsWith("url(\"data:image/png;base64,iVBOR"), "A green screenshot was successfully postMessaged");
+
+    const bgUrl = preview.style.backgroundImage.match(/url\(\"(.*)\"\)/)[1];
+    ok(bgUrl.startsWith("data:image/jpeg;base64,"), "A jpeg screenshot was successfully postMessaged");
+    await isGreen(bgUrl);
   });
 
   BrowserTestUtils.removeTab(tab2);
