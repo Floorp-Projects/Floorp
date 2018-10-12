4b9ba8390f8aafc48245f23fd4591dabce3ec690
Paul Bone <pbone@mozilla.com>
2018-10-12 20:17:58 +1100

    Bug 1498177 - Use size_t for integer values particularly for comparisons r=sfink
    These values were compared as floats, where there was no sagnificant
    difference and the assertion failed.

---
+++ js/src/jit-test/tests/gc/bug-1498177.js
@@ -0,0 +1,17 @@
+
+let a, b;
+for (i=0; i < 300000; i++) {
+    let c = { a: a, b: b };
+    a = b;
+    b = c;
+}
+
+gc();
+
+// GCRuntime::setHighFrequencyHighLimit will change the low value to be one
+// byte lower than the high value (if necessary).  But this blew up later
+// when the values were mistakingly cast to float then compared, rather than
+// kept as size_t.
+gcparam('highFrequencyHighLimit', 99);
+
+
