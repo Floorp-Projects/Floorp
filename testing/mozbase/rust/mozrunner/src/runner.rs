aa2f476cf8c338c73e02de831549e31799038f00
Alex Gaynor <agaynor@mozilla.com>
2018-10-12 17:26:21 +0000

    Bug 1498247 - Part 3 - switch from using the deprecated env::home_dir() to the dirs crate; r=jgraham
    Depends on D8578
    
    Differential Revision: https://phabricator.services.mozilla.com/D8579

--- testing/mozbase/rust/mozrunner/src/runner.rs
+++ testing/mozbase/rust/mozrunner/src/runner.rs
@@ -311,8 +311,8 @@ pub mod platform {
 #[cfg(target_os = "macos")]
 pub mod platform {
     use path::{find_binary, is_binary};
-    use std::env;
     use std::path::PathBuf;
+    use dirs;
 
     /// Searches the system path for `firefox-bin`, then looks for
     /// `Applications/Firefox.app/Contents/MacOS/firefox-bin` under both `/`
@@ -322,7 +322,7 @@ pub mod platform {
             return Some(path);
         }
 
-        let home = env::home_dir();
+        let home = dirs::home_dir();
         for &(prefix_home, trial_path) in [
             (
                 false,
