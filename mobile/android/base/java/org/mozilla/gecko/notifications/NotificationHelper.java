77f8de2029229f645b2644515e54d8c57cb157ed
Brindusan Cristian <cbrindusan@mozilla.com>
2018-10-12 03:25:55 +0300

    Backed out changeset b57be7d7343c (bug 1493864) for build bustages on android-findbugs. CLOSED TREE

--- mobile/android/base/java/org/mozilla/gecko/notifications/NotificationHelper.java
+++ mobile/android/base/java/org/mozilla/gecko/notifications/NotificationHelper.java
@@ -116,10 +116,6 @@ public final class NotificationHelper implements BundleEventListener {
          * Leanplum notification channel - use only when <code>AppConstants.MOZ_ANDROID_MMA</code> is true.
          */
         LP_DEFAULT,
-        /**
-         * HTML5 web site notifications
-         */
-        SITE_NOTIFICATIONS,
     }
 
     // Holds the mapping between the Channel enum used by the rest of our codebase and the
@@ -147,9 +143,6 @@ public final class NotificationHelper implements BundleEventListener {
 
         final String SYNCED_TABS_CHANNEL_TAG = "synced-tabs-notification-channel";
         put(Channel.SYNCED_TABS, SYNCED_TABS_CHANNEL_TAG);
-
-        final String SITE_NOTIFICATIONS_CHANNEL_TAG = "site-notifications";
-        put(Channel.SITE_NOTIFICATIONS, SITE_NOTIFICATIONS_CHANNEL_TAG);
     }};
 
     // These are channels we no longer require and want to retire from Android's settings UI.
@@ -267,12 +260,6 @@ public final class NotificationHelper implements BundleEventListener {
                             NotificationManager.IMPORTANCE_LOW);
                 }
                 break;
-
-                case SITE_NOTIFICATIONS: {
-                    channel = new NotificationChannel(mDefinedNotificationChannels.get(definedChannel),
-                            mContext.getString(R.string.site_notifications_channel),
-                            NotificationManager.IMPORTANCE_DEFAULT);
-                }
 
                 case DEFAULT:
                 default: {
