cf0cd8688c212af5797b0b6b12edfb72f57590ab
Daniel Varga <dvarga@mozilla.com>
2018-10-13 06:46:17 +0300

    Backed out 5 changesets (bug 1498410) for browser-chrome failures at browser/base/content/test/performance/browser_preferences_usage.js
    Backed out changeset 4842c9240ccf (bug 1498410)
    Backed out changeset 9ba024979004 (bug 1498410)
    Backed out changeset db5f7c857952 (bug 1498410)
    Backed out changeset 40f06570e491 (bug 1498410)
    Backed out changeset 0a7fd350ec1d (bug 1498410)

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -98,8 +98,17 @@ add_task(async function startup() {
       // This is accessed in debug only.
     },
     "extensions.getAddons.cache.enabled": {
-      min: 5,
+      min: 6,
       max: 55,
+    },
+
+    // Disabling screenshots in the default test profile triggers some
+    // work in the chrome registry that reads this pref.  This can be removed
+    // when bootstrapped extensions are gone, or even when screenshots
+    // moves away from bootstrap (bug 1422437)
+    "chrome.override_package.global": {
+      min: 0,
+      max: 50,
     },
   };
 
