0b013f52aa9a8af854e7a2922d2f4e0eaec9c917
Cosmin Sabou <csabou@mozilla.com>
2018-10-13 04:51:18 +0300

    Backed out changeset f7c32e7abf72 (bug 1498458) for causing SM build bustages on oom-allocating-arraybuffer-contents.

--- js/src/jit-test/tests/typedarray/oom-allocating-arraybuffer-contents.js
+++
@@ -1,8 +0,0 @@
-// |jit-test| skip-if: !('oomTest' in this)
-
-oomTest(function test() {
-  // The original missing OOM check was after failure to allocate ArrayBuffer
-  // contents, in the ctor call -- the particular operations after that aren't
-  // important.
-  new Uint8ClampedArray(256).toLocaleString('hi');
-});
