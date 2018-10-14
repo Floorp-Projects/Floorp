2b5f80fe2aaf5bc740ea71aee74a8bfec902591a
Cameron McCormack <cam@mcc.id.au>
2018-10-14 00:06:15 +0000

    Bug 1498755 - Part 12: Remove unused main thread FFI refcounting r=emilio
    Depends on D8653
    
    Differential Revision: https://phabricator.services.mozilla.com/D8654
    
    --HG--
    extra : moz-landing-system : lando

--- layout/style/GeckoBindings.h
+++ layout/style/GeckoBindings.h
@@ -61,15 +61,6 @@ const bool GECKO_IS_NIGHTLY = false;
   void Gecko_Release##name_##ArbitraryThread(class_* aPtr)                    \
   { NS_RELEASE(aPtr); }
 
-#define NS_DECL_FFI_REFCOUNTING(class_, name_)  \
-  void Gecko_##name_##_AddRef(class_* aPtr);    \
-  void Gecko_##name_##_Release(class_* aPtr);
-#define NS_IMPL_FFI_REFCOUNTING(class_, name_)                    \
-  void Gecko_##name_##_AddRef(class_* aPtr)                       \
-    { MOZ_ASSERT(NS_IsMainThread()); NS_ADDREF(aPtr); }           \
-  void Gecko_##name_##_Release(class_* aPtr)                      \
-    { MOZ_ASSERT(NS_IsMainThread()); NS_RELEASE(aPtr); }
-
 extern "C" {
 
 class ServoBundledURI
