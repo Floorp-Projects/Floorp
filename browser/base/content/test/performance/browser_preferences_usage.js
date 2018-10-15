0f1298e8666477fe82260f7cf5ee9fc50329c924
Jared Hirsch <ohai@6a68.net>
2018-10-15 19:04:34 +0000

    Bug 1498410 - Part 6 - Update browser_preferences_usage test for non-bootstrapped Screenshots;r=aswan
    MozReview-Commit-ID: Bdwv9rFBwm3
    
    Differential Revision: https://phabricator.services.mozilla.com/D8753

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -92,7 +92,7 @@ add_task(async function startup() {
     },
     "layout.css.dpi": {
       min: 45,
-      max: 75,
+      max: 81,
     },
     "network.loadinfo.skip_type_assertion": {
       // This is accessed in debug only.
