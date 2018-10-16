96243f143e35dd122a8cf0c05e6bda15640ec677
Luca Greco <lgreco@mozilla.com>
2018-10-16 17:54:47 +0000

    Bug 1393760 - Avoid to fire multiple new-node-front events for undefined nodeFront. r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D8856

--- devtools/client/framework/selection.js
+++ devtools/client/framework/selection.js
@@ -140,8 +140,16 @@ Selection.prototype = {
       nodeFront = parentNode;
     }
 
+    if (this._nodeFront == null && nodeFront == null) {
+      // Avoid to notify multiple "unselected" events with a null/undefined nodeFront
+      // (e.g. once when the webpage start to navigate away from the current webpage,
+      // and then again while the new page is being loaded).
+      return;
+    }
+
     this._isSlotted = isSlotted;
     this._nodeFront = nodeFront;
+
     this.emit("new-node-front", nodeFront, this.reason);
   },
 
