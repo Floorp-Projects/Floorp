d69003537d407214ba09efc858c37f2b30469e68
Chris Manchester <cmanchester@mozilla.com>
2018-10-16 22:21:36 +0000

    Bug 1498031 - Merge code paths for running configure between Tup and Make based backends. r=firefox-build-system-reviewers,mshal
    This addresses a related issue along the way: a build that results in running
    configure would not update the value of self.config_environment (and therefore
    self.substs) as seen from the build driver, so out of date values would have
    been used.
    
    The changes to Makefile.in and client.mk made exploit the assumption that by
    he time anything in the Make build is running, config.status is up to date.
    Users running "make" without the benefit of "mach" will need to manually run
    configure when necessary in order to take this into account.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8473
    
    --HG--
    extra : moz-landing-system : lando

--- python/mozbuild/mozbuild/controller/building.py
+++ python/mozbuild/mozbuild/controller/building.py
@@ -1039,37 +1039,45 @@ class BuildDriver(MozbuildObject):
             except Exception:
                 pass
 
-            if config is None:
+            # Record whether a clobber was requested so we can print
+            # a special message later if the build fails.
+            clobber_requested = False
+
+            # Write out any changes to the current mozconfig in case
+            # they should invalidate configure.
+            self._write_mozconfig_json()
+
+            previous_backend = None
+            if config is not None:
+                previous_backend = config.substs.get('BUILD_BACKENDS', [None])[0]
+
+            config_rc = None
+            # Even if we have a config object, it may be out of date
+            # if something that influences its result has changed.
+            if config is None or build_out_of_date(mozpath.join(self.topobjdir,
+                                                                'config.status'),
+                                                   mozpath.join(self.topobjdir,
+                                                                'config_status_deps.in')):
+                if previous_backend and 'Make' not in previous_backend:
+                    clobber_requested = self._clobber_configure()
+
                 config_rc = self.configure(buildstatus_messages=True,
                                            line_handler=output.on_line)
+
                 if config_rc != 0:
                     return config_rc
 
-                config = self.config_environment
+                config = self.reload_config_environment()
+
+            active_backend = config.substs.get('BUILD_BACKENDS', [None])[0]
 
             status = None
-            active_backend = config.substs.get('BUILD_BACKENDS', [None])[0]
-            if active_backend and 'Make' not in active_backend:
-                # Record whether a clobber was requested so we can print
-                # a special message later if the build fails.
-                clobber_requested = False
-                # Write out any changes to the current mozconfig in case
-                # they should invalidate configure.
-                self._write_mozconfig_json()
-                # Even if we have a config object, it may be out of date
-                # if something that influences its result has changed.
-                if build_out_of_date(mozpath.join(self.topobjdir,
-                                                  'config.status'),
-                                     mozpath.join(self.topobjdir,
-                                                  'config_status_deps.in')):
-                    clobber_requested = self._clobber_configure()
-                    config_rc = self.configure(buildstatus_messages=True,
-                                               line_handler=output.on_line)
-                    if config_rc != 0:
-                        return config_rc
-                elif backend_out_of_date(mozpath.join(self.topobjdir,
-                                                      'backend.%sBackend' %
-                                                      active_backend)):
+
+            if 'Make' not in active_backend:
+                if (not config_rc and
+                    backend_out_of_date(mozpath.join(self.topobjdir,
+                                                     'backend.%sBackend' %
+                                                     active_backend))):
                     print('Build configuration changed. Regenerating backend.')
                     args = [config.substs['PYTHON'],
                             mozpath.join(self.topobjdir, 'config.status')]
@@ -1179,7 +1187,6 @@ class BuildDriver(MozbuildObject):
 
             # Try to run the active build backend's post-build step, if possible.
             try:
-                config = self.config_environment
                 active_backend = config.substs.get('BUILD_BACKENDS', [None])[0]
                 if active_backend:
                     backend_cls = get_backend_class(active_backend)(config)
@@ -1455,8 +1462,6 @@ class BuildDriver(MozbuildObject):
         mozconfig_mk = os.path.join(self.topobjdir, '.mozconfig.mk')
         with FileAvoidWrite(mozconfig_mk) as fh:
             fh.write(b'\n'.join(mozconfig_filtered_lines))
-
-        self._write_mozconfig_json()
 
         # Copy the original mozconfig to the objdir.
         mozconfig_objdir = os.path.join(self.topobjdir, '.mozconfig')
