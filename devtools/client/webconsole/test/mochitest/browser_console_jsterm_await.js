009520fc350179199098f902a36d8a0a792a2e02
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-10-16 13:57:32 +0000

    Bug 1498300 - Add top-level await support to the Browser Console; r=bgrins.
    This patch moves the parserService from the toolbox,
    which isn't accessible in the case of the Browser Console,
    to the console itself.
    A lightweight test is added to ensure top-level await is
    supported in the browser console.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8816

---
+++ devtools/client/webconsole/test/mochitest/browser_console_jsterm_await.js
@@ -0,0 +1,46 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// This is a lightweight version of browser_jsterm_await.js to only ensure top-level await
+// support in the Browser Console.
+
+"use strict";
+
+const TEST_URI = "data:text/html;charset=utf-8,Top-level await Browser Console test";
+
+add_task(async function() {
+  // Enable await mapping.
+  await pushPref("devtools.debugger.features.map-await-expression", true);
+
+  // Run test with legacy JsTerm
+  await pushPref("devtools.webconsole.jsterm.codeMirror", false);
+  await performTests();
+  // And then run it with the CodeMirror-powered one.
+  await pushPref("devtools.webconsole.jsterm.codeMirror", true);
+  await performTests();
+});
+
+async function performTests() {
+  await addTab(TEST_URI);
+  const hud = await HUDService.toggleBrowserConsole();
+
+  const executeAndWaitForResultMessage = (input, expectedOutput) =>
+    executeAndWaitForMessage(hud, input, expectedOutput, ".result");
+
+  info("Evaluate a top-level await expression");
+  const simpleAwait = `await new Promise(r => setTimeout(() => r(["await1"]), 500))`;
+  await executeAndWaitForResultMessage(
+    simpleAwait,
+    `Array [ "await1" ]`,
+  );
+
+  // Check that the resulting promise of the async iife is not displayed.
+  const messages = hud.ui.outputNode.querySelectorAll(".message .message-body");
+  const messagesText = Array.from(messages).map(n => n.textContent).join(" - ");
+  is(messagesText.includes("Promise {"), false, "The output does not contain a Promise");
+  ok(messagesText.includes(simpleAwait) && messagesText.includes(`Array [ "await1" ]`),
+    "The output contains the the expected messages");
+
+  info("Close the Browser console");
+  await HUDService.toggleBrowserConsole();
+}
