753cbc969e53ebe9f19f438c5283de1e9e287e3e
Dylan Roeh <droeh@mozilla.com>
2018-10-16 12:23:42 -0500

    Bug 1495786 - Replace ambiguous Booleans with AllowOrDeny, an enum with clearly named values. r=snorp,jchen

---
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/AllowOrDeny.java
@@ -0,0 +1,14 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * vim: ts=4 sw=4 expandtab:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.geckoview;
+
+/**
+ * This represents a decision to allow or deny a request.
+ */
+public enum AllowOrDeny {
+    ALLOW, DENY;
+}
