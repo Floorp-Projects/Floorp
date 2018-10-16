370bf2a55ea44ab67e36db3fb3f89981502f2027
Matthew Gaudet <mgaudet@mozilla.com>
2018-10-16 15:43:37 -0400

    Bug 1499544 - [Part 1] Foundational implementation of BytecodeIterator r=djvj
    This is an initial skeleton of an inteface for bytecode. There are 6
    major portions included in this patch.
    
    - RawBytecode: A typedef to expose jsbytecode* in a controlled manner, to allow
      easy future audits.
    
    - BytecodeLocation: This is an immutable view of a single program operation (a
      bytecode). There is is some basic functionality in here included for
      experimentation sake.
    
    - BytecodeLocationOffset: An immutable view of a source location offset, (and
      RawBytecodeLocationOffset, another typedef to ease future auditing)
    
    - BytecodeIterator: A forward iterator over BytecodeLocations, using program
      text order.
    
    - AllBytecodeIterable: A wrapper class that allows the use of a range-based
      for-loop over BytecodeLocations in a Script.
    
    - Modifications to JSScript to expose BytecodeLocations and answer some queries
      about containment.
    
    The interface is experimentally tested using JSScript::initScriptCounts as a
    simple playground for demonstrating the use of the interface.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8918

---
+++ js/src/vm/BytecodeLocation.h
@@ -0,0 +1,148 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=8 sts=4 et sw=4 tw=99:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef vm_BytecodeLocation_h
+#define vm_BytecodeLocation_h
+
+#include "js/TypeDecls.h"
+#include "vm/BytecodeUtil.h"
+
+namespace js {
+
+typedef uint32_t RawBytecodeLocationOffset;
+
+
+class BytecodeLocationOffset
+{
+    RawBytecodeLocationOffset rawOffset_;
+
+  public:
+    explicit BytecodeLocationOffset(RawBytecodeLocationOffset offset)
+      : rawOffset_(offset)
+    {}
+
+    RawBytecodeLocationOffset rawOffset() const {
+        return rawOffset_;
+    }
+
+};
+
+
+typedef jsbytecode* RawBytecode;
+
+// A immutable representation of a program location
+//
+class BytecodeLocation
+{
+    RawBytecode rawBytecode_;
+#ifdef DEBUG
+    const JSScript* debugOnlyScript_;
+#endif
+
+    // Construct a new BytecodeLocation, while borrowing scriptIdentity
+    // from some other BytecodeLocation.
+    BytecodeLocation(const BytecodeLocation& loc, RawBytecode pc)
+      : rawBytecode_(pc)
+#ifdef DEBUG
+      , debugOnlyScript_(loc.debugOnlyScript_)
+#endif
+    {
+        MOZ_ASSERT(isValid());
+    }
+
+  public:
+    BytecodeLocation(const JSScript* script, RawBytecode pc)
+      : rawBytecode_(pc)
+#ifdef DEBUG
+      , debugOnlyScript_(script)
+#endif
+    {
+        MOZ_ASSERT(isValid());
+    }
+
+    RawBytecode toRawBytecode() const {
+        return rawBytecode_;
+    }
+
+    // Return true if this bytecode location is valid for the given script.
+    // This includes the location 1-past the end of the bytecode.
+    bool isValid(const JSScript* script) const;
+
+    // Return true if this bytecode location is within the bounds of the
+    // bytecode for a given script.
+    bool isInBounds(const JSScript* script) const;
+
+    bool operator==(const BytecodeLocation& other) const {
+        MOZ_ASSERT(this->debugOnlyScript_ == other.debugOnlyScript_);
+        return rawBytecode_ == other.rawBytecode_;
+    }
+
+    bool operator!=(const BytecodeLocation& other) const {
+        return !(other == *this);
+    }
+
+    bool operator<(const BytecodeLocation& other) const {
+        MOZ_ASSERT(this->debugOnlyScript_ == other.debugOnlyScript_);
+        return rawBytecode_ < other.rawBytecode_;
+    }
+
+    // It is traditional to represent the rest of the relational operators
+    // using operator<, so we don't need to assert for these.
+    bool operator>(const BytecodeLocation& other) const {
+        return other < *this;
+    }
+
+    bool operator<=(const BytecodeLocation& other) const {
+        return !(other < *this);
+    }
+
+    bool operator>=(const BytecodeLocation& other) const {
+        return !(*this < other);
+    }
+
+    // Return the next bytecode
+    BytecodeLocation next() const {
+        return BytecodeLocation(*this, rawBytecode_ + GetBytecodeLength(rawBytecode_));
+    }
+
+    // Add an offset.
+    BytecodeLocation operator+(const BytecodeLocationOffset& offset) {
+        return BytecodeLocation(*this, rawBytecode_ + offset.rawOffset());
+    }
+
+    // Identity Checks
+    bool is(JSOp op) const {
+        MOZ_ASSERT(isInBounds());
+        return getOp() == op;
+    }
+
+    bool isJumpTarget() const {
+        return BytecodeIsJumpTarget(getOp());
+    }
+
+    // Accessors:
+    JSOp getOp() const {
+        return JSOp(*rawBytecode_);
+    }
+
+#ifdef DEBUG
+    // To ease writing assertions
+    bool isValid() const {
+        return isValid(debugOnlyScript_);
+    }
+
+    bool isInBounds() const {
+        return isInBounds(debugOnlyScript_);
+    }
+#endif
+
+};
+
+
+}
+
+
+#endif
