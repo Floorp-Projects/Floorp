ae5e9a71986230016ec3350cb06ce20e01b21371
Mark Striemer <mstriemer@mozilla.com>
2018-10-17 18:15:24 +0000

    Bug 1495708 - Remove theme info bar and update plugin notice r=aswan,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D8105

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -2369,13 +2369,6 @@ var gListView = {
     } else {
       document.getElementById("plugindeprecation-notice").hidden = true;
     }
-
-    if (Preferences.get("extensions.getAddons.themes.browseURL", "")) {
-      document.getElementById("getthemes-learnmore-link")
-        .setAttribute("href", Services.urlFormatter.formatURLPref("extensions.getAddons.themes.browseURL"));
-    } else {
-      document.getElementById("getthemes-container").hidden = true;
-    }
   },
 
   show(aType, aRequest) {
