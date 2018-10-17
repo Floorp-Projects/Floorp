d92f8581d83df3837443aa5e9b0e122dc023c38b
yulia <ystartsev@mozilla.com>
2018-10-17 10:40:48 +0000

    Bug 1499056 - Use target.getFront for screenshot front; r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D8866

--- devtools/shared/fronts/screenshot.js
+++ devtools/shared/fronts/screenshot.js
@@ -20,19 +20,4 @@ const ScreenshotFront = protocol.FrontClassWithSpec(screenshotSpec, {
   }
 });
 
-// A cache of created fronts: WeakMap<Client, Front>
-const knownFronts = new WeakMap();
-
-/**
- * Create a screenshot front only when needed
- */
-function getScreenshotFront(target) {
-  let front = knownFronts.get(target.client);
-  if (front == null && target.form.screenshotActor != null) {
-    front = new ScreenshotFront(target.client, target.form);
-    knownFronts.set(target.client, front);
-  }
-  return front;
-}
-
-exports.getScreenshotFront = getScreenshotFront;
+exports.ScreenshotFront = ScreenshotFront;
