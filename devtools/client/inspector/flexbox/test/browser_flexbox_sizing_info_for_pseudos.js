8bc98179e9866412566cfede84fbd4334a314331
Patrick Brosset <pbrosset@mozilla.com>
2018-10-17 14:24:57 +0000

    Bug 1499322 - Support pseudo elements in the flexbox inspector; r=rcaliman
    On the server, when looking for a flex container for a node, we were bailing
    out if the displayType of the node was null. It was null for pseudo-elements.
    This value was returned by the displayType getter in the NodeActor class.
    Now, the reason for this dates to 4 years ago in bug 1139937 where trying to
    get the display style of a pseudo-element was done in a way to failed. So we
    just decided to return null at that point. It doesn't fail anymore, we're
    able to return, say, "block" if a pseudo-element has a display:block style.
    So I've removed the checks that returned null and that fixed the issue here.
    
    The other part of the fix that was need is in the FlexItemActor class on the
    server too. This class can be created for a pseudo-element too.
    It accesses element.style without checking if that property exists. However it
    does not exist for pseudo-elements. So we needed to add a check for that.
    It's not a problem to just skip it in this case because pseudo-elements can't
    have inline styles.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8873
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/inspector/flexbox/test/browser_flexbox_sizing_info_for_pseudos.js
@@ -0,0 +1,38 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that the flex item sizing UI also appears for pseudo elements.
+
+const TEST_URI = URL_ROOT + "doc_flexbox_pseudos.html";
+
+add_task(async function() {
+  await addTab(TEST_URI);
+  const { inspector, flexboxInspector } = await openLayoutView();
+  const { document: doc } = flexboxInspector;
+
+  info("Select the ::before pseudo-element in the inspector");
+  const containerNode = await getNodeFront(".container", inspector);
+  const { nodes } = await inspector.walker.children(containerNode);
+  const beforeNode = nodes[0];
+
+  const onFlexItemSizingRendered = waitForDOM(doc, "ul.flex-item-sizing");
+  const onFlexItemOutlineRendered = waitForDOM(doc, ".flex-outline-container");
+  await selectNode(beforeNode, inspector);
+  const [flexSizingContainer] = await onFlexItemSizingRendered;
+  const [flexOutlineContainer] = await onFlexItemOutlineRendered;
+
+  ok(flexSizingContainer, "The flex sizing exists in the DOM");
+  ok(flexOutlineContainer, "The flex outline exists in the DOM");
+
+  info("Check that the various sizing sections are displayed");
+  const allSections = [...flexSizingContainer.querySelectorAll(".section")];
+  ok(allSections.length, "Sizing sections are displayed");
+
+  info("Check that the various parts of the outline are displayed");
+  const [basis, final] = [...flexOutlineContainer.querySelectorAll(
+    ".flex-outline-basis, .flex-outline-final")];
+  ok(basis && final, "The final and basis parts of the outline exist");
+});
