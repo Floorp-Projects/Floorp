93d515c00b71c9f294fa08fe1bec341ee59360c4
Marco Zehe <mzehe@mozilla.com>
2018-10-18 07:06:45 +0000

    Bug 1499702 - Clean up break statements in EventQueue::CoalesceEvents(), r=surkov
    Differential Revision: https://phabricator.services.mozilla.com/D8971

--- accessible/base/EventQueue.cpp
+++ accessible/base/EventQueue.cpp
@@ -111,7 +111,8 @@ EventQueue::CoalesceEvents()
           return;
         }
       }
-    } break; // case eCoalesceOfSameType
+      break; // case eCoalesceOfSameType
+    }
 
     case AccEvent::eCoalesceSelectionChange:
     {
@@ -129,8 +130,8 @@ EventQueue::CoalesceEvents()
           }
         }
       }
-
-    } break; // eCoalesceSelectionChange
+      break; // eCoalesceSelectionChange
+    }
 
     case AccEvent::eCoalesceStateChange:
     {
@@ -172,7 +173,8 @@ EventQueue::CoalesceEvents()
         }
 
       }
-    } break; // eCoalesceTextSelChange
+      break; // eCoalesceTextSelChange
+    }
 
     case AccEvent::eRemoveDupes:
     {
@@ -187,7 +189,8 @@ EventQueue::CoalesceEvents()
           return;
         }
       }
-    } break; // case eRemoveDupes
+      break; // case eRemoveDupes
+    }
 
     default:
       break; // case eAllowDupes, eDoNotEmit
