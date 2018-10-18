583cda7af664b5b7c70cf03d8b03a2fdd748bc3b
Coroiu Cristina <ccoroiu@mozilla.com>
2018-10-18 06:23:14 +0300

    Backed out 2 changesets (bug 1398354) for spidermonkey and hazard bustages at js/src/jsapi-tests/testStringIsArrayIndex.cpp on a CLOSED TREE
    Backed out changeset 344b7e258ea5 (bug 1398354)
    Backed out changeset 8e41c385edb9 (bug 1398354)

--- dom/html/HTMLAllCollection.h
+++ dom/html/HTMLAllCollection.h
@@ -16,15 +16,14 @@
 
 class nsContentList;
 class nsHTMLDocument;
+class nsIContent;
 class nsINode;
 
 namespace mozilla {
 namespace dom {
 
-class Element;
-class OwningHTMLCollectionOrElement;
+class OwningNodeOrHTMLCollection;
 template<typename> struct Nullable;
-template<typename> class Optional;
 
 class HTMLAllCollection final : public nsISupports
                               , public nsWrapperCache
@@ -41,32 +40,32 @@ public:
   nsINode* GetParentObject() const;
 
   uint32_t Length();
-  Element* IndexedGetter(uint32_t aIndex, bool& aFound)
+  nsIContent* Item(uint32_t aIndex);
+  void Item(const nsAString& aName, Nullable<OwningNodeOrHTMLCollection>& aResult)
   {
-    Element* result = Item(aIndex);
+    NamedItem(aName, aResult);
+  }
+  nsIContent* IndexedGetter(uint32_t aIndex, bool& aFound)
+  {
+    nsIContent* result = Item(aIndex);
     aFound = !!result;
     return result;
   }
 
   void NamedItem(const nsAString& aName,
-                 Nullable<OwningHTMLCollectionOrElement>& aResult)
+                 Nullable<OwningNodeOrHTMLCollection>& aResult)
   {
     bool found = false;
     NamedGetter(aName, found, aResult);
   }
   void NamedGetter(const nsAString& aName,
                    bool& aFound,
-                   Nullable<OwningHTMLCollectionOrElement>& aResult);
+                   Nullable<OwningNodeOrHTMLCollection>& aResult);
   void GetSupportedNames(nsTArray<nsString>& aNames);
-
-  void Item(const Optional<nsAString>& aNameOrIndex,
-            Nullable<OwningHTMLCollectionOrElement>& aResult);
-
-  void LegacyCall(JS::Handle<JS::Value>,
-                  const Optional<nsAString>& aNameOrIndex,
-                  Nullable<OwningHTMLCollectionOrElement>& aResult)
+  void LegacyCall(JS::Handle<JS::Value>, const nsAString& aName,
+                  Nullable<OwningNodeOrHTMLCollection>& aResult)
   {
-    Item(aNameOrIndex, aResult);
+    NamedItem(aName, aResult);
   }
 
 private:
@@ -76,11 +75,6 @@ private:
    * Returns the HTMLCollection for document.all[aID], or null if there isn't one.
    */
   nsContentList* GetDocumentAllList(const nsAString& aID);
-
-  /**
-   * Helper for indexed getter and spec Item() method.
-   */
-  Element* Item(uint32_t aIndex);
 
   RefPtr<nsHTMLDocument> mDocument;
   RefPtr<nsContentList> mCollection;
