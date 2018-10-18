e2ced1ab715a9c0c88e51ffb3a8e0bc535fc784d
Andrea Marchesini <amarchesini@mozilla.com>
2018-10-18 11:08:37 +0200

    Bug 1499788 - midi feature policy should have 'self' as default allowlist, r=qdot

--- dom/security/featurepolicy/FeaturePolicyUtils.cpp
+++ dom/security/featurepolicy/FeaturePolicyUtils.cpp
@@ -28,7 +28,7 @@ static FeatureMap sSupportedFeatures[] = {
   { "fullscreen", FeaturePolicyUtils::FeaturePolicyValue::eAll },
   { "geolocation", FeaturePolicyUtils::FeaturePolicyValue::eAll },
   { "microphone", FeaturePolicyUtils::FeaturePolicyValue::eSelf },
-  { "midi", FeaturePolicyUtils::FeaturePolicyValue::eAll },
+  { "midi", FeaturePolicyUtils::FeaturePolicyValue::eSelf },
   { "payment", FeaturePolicyUtils::FeaturePolicyValue::eAll },
   // TODO: not supported yet!!!
   { "speaker", FeaturePolicyUtils::FeaturePolicyValue::eSelf },
