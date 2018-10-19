696737110ff35fb7ce07a85e55098d30e7faae78
Nika Layzell <nika@thelayzells.com>
2018-10-19 18:00:59 -0400

    Bug 1500944 - Part 1: Store the set of active WindowGlobalParent objects in ChromeBrowsingContext, r=farre
    This allows getting the set of all window globals for a given browsing context.
    This is less useful at the moment as the active window global is not exposed as
    such. That will be added as a follow-up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9393

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef BrowsingContext_h
-#define BrowsingContext_h
+#ifndef mozilla_dom_BrowsingContext_h
+#define mozilla_dom_BrowsingContext_h
 
 #include "mozilla/LinkedList.h"
 #include "mozilla/Maybe.h"
@@ -143,4 +143,5 @@ class BrowsingContext : public nsWrapperCache,
 
 }  // namespace dom
 }  // namespace mozilla
-#endif
+
+#endif  // !defined(mozilla_dom_BrowsingContext_h)
