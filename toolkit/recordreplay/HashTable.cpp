22defec623c8085fa521875e07216334723c94a6
Brian Hackett <bhackett1024@gmail.com>
2018-10-20 09:22:11 -0600

    Bug 1488453 - Use release mode assertions in HashTable, r=mccr8.

--- toolkit/recordreplay/HashTable.cpp
+++ toolkit/recordreplay/HashTable.cpp
@@ -87,7 +87,7 @@ class StableHashTableInfo
   // Get an existing key in the table.
   KeyInfo* FindKeyInfo(HashNumber aOriginalHash, const void* aKey, HashInfo** aHashInfo = nullptr) {
     HashToKeyMap::iterator iter = mHashToKey.find(aOriginalHash);
-    MOZ_ASSERT(iter != mHashToKey.end());
+    MOZ_RELEASE_ASSERT(iter != mHashToKey.end());
 
     HashInfo* hashInfo = iter->second.get();
     for (KeyInfo& keyInfo : hashInfo->mKeys) {
@@ -114,7 +114,7 @@ public:
   }
 
   ~StableHashTableInfo() {
-    MOZ_ASSERT(mHashToKey.empty());
+    MOZ_RELEASE_ASSERT(mHashToKey.empty());
     DeallocateMemory(mCallbackStorage, CallbackStorageCapacity, MemoryKind::Tracked);
   }
 
@@ -175,7 +175,7 @@ public:
 
   HashNumber GetOriginalHashNumber(const void* aKey) {
     KeyToHashMap::iterator iter = mKeyToHash.find(aKey);
-    MOZ_ASSERT(iter != mKeyToHash.end());
+    MOZ_RELEASE_ASSERT(iter != mKeyToHash.end());
     return iter->second;
   }
 
@@ -212,7 +212,7 @@ public:
   }
 
   HashNumber GetLastNewHash(const void* aKey) {
-    MOZ_ASSERT(aKey == mLastKey);
+    MOZ_RELEASE_ASSERT(aKey == mLastKey);
     return mLastNewHash;
   }
 
@@ -302,7 +302,7 @@ WrapPLHashAllocEntry(void* aAllocPrivate, const void* aKey)
     // the hashes are supplied directly to the table and we don't have a chance
     // to modify them. Fortunately, none of these tables are iterated in a way
     // that can cause the replay to diverge, so just punt in these cases.
-    MOZ_ASSERT(info->IsEmpty());
+    MOZ_RELEASE_ASSERT(info->IsEmpty());
   }
 
   return info->mAllocOps
