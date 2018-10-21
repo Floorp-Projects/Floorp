d88d0632df62c3745399d6679f9cd47b58f6be4f
Nika Layzell <nika@thelayzells.com>
2018-10-20 20:48:14 -0400

    Bug 1489301 - Part 6: Remove references to 'System' from WebIDL.py, r=bzbarsky
    'Exposed=System' is no longer used in any webidl files, so we can kill it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9401

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -17199,7 +17199,7 @@ class GlobalGenRoots():
         curr = CGList([], "\n")
 
         descriptors = config.getDescriptors(hasInterfaceObject=True,
-                                            isExposedInSystemGlobals=True,
+                                            isExposedInWindow=True,
                                             register=True)
         properties = [desc.name for desc in descriptors]
 
@@ -17244,7 +17244,7 @@ class GlobalGenRoots():
         defineIncludes = [CGHeaders.getDeclarationFilename(desc.interface)
                           for desc in config.getDescriptors(hasInterfaceObject=True,
                                                             register=True,
-                                                            isExposedInSystemGlobals=True)]
+                                                            isExposedInWindow=True)]
         defineIncludes.append("nsThreadUtils.h")  # For NS_IsMainThread
         defineIncludes.append("js/Id.h")  # For jsid
         defineIncludes.append("mozilla/dom/WebIDLGlobalNameHash.h")
