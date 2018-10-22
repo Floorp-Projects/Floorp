f456a0b215c367915354ff7df4bbfe404c57e083
Gurzau Raul <rgurzau@mozilla.com>
2018-10-22 20:02:13 +0300

    Backed out 5 changesets (bug 1468754) for linting failure at checkouts/gecko/devtools/server/actors/utils/actor-registry.js on a CLOSED TREE
    Backed out changeset c36e8383aa3a (bug 1468754)
    Backed out changeset c98ac7503111 (bug 1468754)
    Backed out changeset ddd043a258e0 (bug 1468754)
    Backed out changeset e6ff3400a4f6 (bug 1468754)
    Backed out changeset 817018fb5aa4 (bug 1468754)

--- devtools/client/inspector/changes/ChangesView.js
+++ devtools/client/inspector/changes/ChangesView.js
@@ -13,23 +13,14 @@ const ChangesApp = createFactory(require("./components/ChangesApp"));
 
 const {
   resetChanges,
-  trackChange,
 } = require("./actions/changes");
 
 class ChangesView {
   constructor(inspector) {
     this.inspector = inspector;
     this.store = this.inspector.store;
-    this.toolbox = this.inspector.toolbox;
 
-    this.onAddChange = this.onAddChange.bind(this);
-    this.onClearChanges = this.onClearChanges.bind(this);
     this.destroy = this.destroy.bind(this);
-
-    // Get the Changes front, and listen to it.
-    this.changesFront = this.toolbox.target.getFront("changes");
-    this.changesFront.on("add-change", this.onAddChange);
-    this.changesFront.on("clear-changes", this.onClearChanges);
 
     this.init();
   }
@@ -47,33 +38,6 @@ class ChangesView {
     // TODO: save store and restore/replay on refresh.
     // Bug 1478439 - https://bugzilla.mozilla.org/show_bug.cgi?id=1478439
     this.inspector.target.once("will-navigate", this.destroy);
-
-    // Sync the store to the changes stored on the server. The
-    // syncChangesToServer() method is async, but we don't await it since
-    // this method itself is NOT async. The call will be made in its own
-    // time, which is fine since it definitionally brings us up-to-date
-    // with the server at that moment.
-    this.syncChangesToServer();
-  }
-
-  async syncChangesToServer() {
-    // Empty the store.
-    this.onClearChanges();
-
-    // Add back in all the changes from the changesFront.
-    const changes = await this.changesFront.allChanges();
-    changes.forEach((change) => {
-      this.onAddChange(change);
-    });
-  }
-
-  onAddChange(change) {
-    // Turn data into a suitable change to send to the store.
-    this.store.dispatch(trackChange(change));
-  }
-
-  onClearChanges() {
-    this.store.dispatch(resetChanges());
   }
 
   /**
@@ -81,14 +45,8 @@ class ChangesView {
    */
   destroy() {
     this.store.dispatch(resetChanges());
-
-    this.changesFront.off("add-change", this.onAddChange);
-    this.changesFront.off("clear-changes", this.onClearChanges);
-    this.changesFront = null;
-
     this.inspector = null;
     this.store = null;
-    this.toolbox = null;
   }
 }
 
