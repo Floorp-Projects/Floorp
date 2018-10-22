51d8d87da29aa4dfd767381754b0cc612996f5da
Colin Lee <mncolinlee@gmail.com>
2018-10-22 12:29:31 -0500

    [focus] Fix formatting and add flags

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/EraseAndOpenShortcutActivity.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/activity/EraseAndOpenShortcutActivity.kt
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package org.mozilla.focus.activity
 
 import android.app.Activity
@@ -6,19 +10,18 @@ import android.os.Bundle
 import org.mozilla.focus.ext.components
 import org.mozilla.focus.telemetry.TelemetryWrapper
 
-class EraseAndOpenShortcutActivity: Activity(){
+class EraseAndOpenShortcutActivity : Activity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    components.sessionManager.removeSessions()
+        components.sessionManager.removeSessions()
 
-    TelemetryWrapper.eraseAndOpenShortcutEvent()
+        TelemetryWrapper.eraseAndOpenShortcutEvent()
 
-    val intent = Intent(this, MainActivity::class.java)
-    intent.action = MainActivity.ACTION_OPEN
-    startActivity(intent)
-
-  }
-
+        val intent = Intent(this, MainActivity::class.java)
+        intent.action = MainActivity.ACTION_OPEN
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+    }
 }
