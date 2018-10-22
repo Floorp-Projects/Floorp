5ba2a10b435d587f751e0746234d725c3e6afdc9
No Jun Park <npark@mozilla.com>
2018-10-22 15:51:52 -0400

    [focus] add uploading of GV_Example when Focus uploads

--- mobile/android/focus-android/tools/taskcluster/upload_apk_nimbledroid.py
+++ mobile/android/focus-android/tools/taskcluster/upload_apk_nimbledroid.py
@@ -7,11 +7,10 @@ This script talks to the taskcluster secrets service to obtain the
 Nimbledroid account key and upload Klar and Focus apk to Nimbledroid for perf analysis.
 """
 
-import base64
-import os
 import taskcluster
 import requests
 import json
+import urllib2
 
 url = "https://nimbledroid.com/api/v2/apks"
 
@@ -31,6 +30,14 @@ def uploadApk(apk,key):
 	print('Reponse Payload:')
 	print json.dumps(response.json(), indent=4)
 
+def uploadGeckoViewExampleApk(key):
+	apk_url = 'https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.mobile.android-api-16-opt/artifacts/public/build/geckoview_example.apk'
+
+	apk_data = urllib2.urlopen(apk_url).read()
+	with open('./geckoview_example_nd.apk', 'wb') as f:
+    		f.write(apk_data)
+	uploadApk({'apk' : open('geckoview_example_nd.apk')},key)
+
 # Get JSON data from taskcluster secrets service
 secrets = taskcluster.Secrets({'baseUrl': 'http://taskcluster/secrets/v1'})
 data = secrets.get('project/focus/nimbledroid')
@@ -39,5 +46,7 @@ data = secrets.get('project/focus/nimbledroid')
 klar_file = {'apk': open('app/build/outputs/apk/klarArm/nightly/app-klar-arm-nightly-unsigned.apk')}
 # focus_file = {'apk': open('app/build/outputs/apk/focusArm/release/app-focus-arm-release-unsigned.apk')}
 
+# also upload the latest geckoview example from:
 uploadApk(klar_file, data['secret']['api_key'])
+uploadGeckoViewExampleApk(data['secret']['api_key'])
 # uploadApk(focus_file, data['secret']['api_key'])
