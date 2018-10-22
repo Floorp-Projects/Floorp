eace2a721861e9a40c08b1e912ee40f490ed92d9
Jan-Erik Rediger <jrediger@mozilla.com>
2018-10-22 15:26:30 +0000

    Bug 1468761 - Migrate Telemetry tests to always assume packed histograms r=chutten
    Depends on D9235
    
    Differential Revision: https://phabricator.services.mozilla.com/D9236

--- toolkit/components/telemetry/tests/unit/test_TelemetryFlagClear.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryFlagClear.js
@@ -3,11 +3,11 @@
 
 function run_test() {
   let testFlag = Services.telemetry.getHistogramById("TELEMETRY_TEST_FLAG");
-  equal(JSON.stringify(testFlag.snapshot().counts), "[1,0,0]", "Original value is correct");
+  deepEqual(testFlag.snapshot().values, {0: 1, 1: 0}, "Original value is correct");
   testFlag.add(1);
-  equal(JSON.stringify(testFlag.snapshot().counts), "[0,1,0]", "Value is correct after ping.");
+  deepEqual(testFlag.snapshot().values, {0: 0, 1: 1, 2: 0}, "Value is correct after ping");
   testFlag.clear();
-  equal(JSON.stringify(testFlag.snapshot().counts), "[1,0,0]", "Value is correct after calling clear()");
+  deepEqual(testFlag.snapshot().values, {0: 1, 1: 0}, "Value is correct after calling clear()");
   testFlag.add(1);
-  equal(JSON.stringify(testFlag.snapshot().counts), "[0,1,0]", "Value is correct after ping.");
+  deepEqual(testFlag.snapshot().values, {0: 0, 1: 1, 2: 0}, "Value is correct after ping");
 }
