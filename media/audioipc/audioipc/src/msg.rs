af490a6f95553858fa1b352dcc78525a86e5b6cc
Matthew Gregan <kinetik@flim.org>
2018-10-23 16:46:52 +1300

    Bug 1501148 - Refactor AudioIPC to make way for multiple OS backends.  r=chunmin
    --HG--
    extra : rebase_source : c189d532815de05bc24ee93913d470d8f6a422ef

--- media/audioipc/audioipc/src/msg.rs
+++ media/audioipc/audioipc/src/msg.rs
@@ -1,8 +1,48 @@
-use iovec::IoVec;
+// Copyright © 2017 Mozilla Foundation
+//
+// This program is made available under an ISC-style license.  See the
+// accompanying file LICENSE for details.
+
 use iovec::unix as iovec;
+use iovec::IoVec;
 use libc;
+use std::os::unix::io::{AsRawFd, RawFd};
 use std::{cmp, io, mem, ptr};
-use std::os::unix::io::RawFd;
+
+// Extend sys::os::unix::net::UnixStream to support sending and receiving a single file desc.
+// We can extend UnixStream by using traits, eliminating the need to introduce a new wrapped
+// UnixStream type.
+pub trait RecvMsg {
+    fn recv_msg(
+        &mut self,
+        iov: &mut [&mut IoVec],
+        cmsg: &mut [u8],
+    ) -> io::Result<(usize, usize, i32)>;
+}
+
+pub trait SendMsg {
+    fn send_msg(&mut self, iov: &[&IoVec], cmsg: &[u8]) -> io::Result<usize>;
+}
+
+impl<T: AsRawFd> RecvMsg for T {
+    fn recv_msg(
+        &mut self,
+        iov: &mut [&mut IoVec],
+        cmsg: &mut [u8],
+    ) -> io::Result<(usize, usize, i32)> {
+        #[cfg(target_os = "linux")]
+        let flags = libc::MSG_CMSG_CLOEXEC;
+        #[cfg(not(target_os = "linux"))]
+        let flags = 0;
+        recv_msg_with_flags(self.as_raw_fd(), iov, cmsg, flags)
+    }
+}
+
+impl<T: AsRawFd> SendMsg for T {
+    fn send_msg(&mut self, iov: &[&IoVec], cmsg: &[u8]) -> io::Result<usize> {
+        send_msg_with_flags(self.as_raw_fd(), iov, cmsg, 0)
+    }
+}
 
 fn cvt(r: libc::ssize_t) -> io::Result<usize> {
     if r == -1 {
