9978e851df05c6d602f32113c62a22670f39d1d7
Andrew McCreight <continuation@gmail.com>
2018-10-23 23:20:38 +0000

    Bug 1492584, part 3 - Remove JS component loader registration. r=kmag
    After the previous patches, we no longer rely on the component manager
    to incidentally start up XPConnect when we load the JS loader service
    or to hold the JS component loader alive, so the do_GetService() call
    for the JS loader in XPCOMInit.cpp can be removed. After that is done,
    the JS loader is no longer used as an XPCOM component, so all of the
    boilerplate for that can be removed.
    
    Depends on D8757
    
    Differential Revision: https://phabricator.services.mozilla.com/D8758
    
    --HG--
    extra : moz-landing-system : lando

--- js/xpconnect/src/XPCModule.h
+++ js/xpconnect/src/XPCModule.h
@@ -6,7 +6,6 @@
 
 #include "xpcprivate.h"
 #include "mozilla/ModuleUtils.h"
-#include "mozJSComponentLoader.h"
 #include "mozJSSubScriptLoader.h"
 
 /* Module implementation for the xpconnect library. */
@@ -21,22 +20,16 @@
 #define MOZJSSUBSCRIPTLOADER_CONTRACTID "@mozilla.org/moz/jssubscript-loader;1"
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsJSID)
-
-NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(mozJSComponentLoader,
-                                         mozJSComponentLoader::GetOrCreate);
 NS_GENERIC_FACTORY_CONSTRUCTOR(mozJSSubScriptLoader)
 
 NS_DEFINE_NAMED_CID(NS_JS_ID_CID);
-NS_DEFINE_NAMED_CID(MOZJSCOMPONENTLOADER_CID);
 NS_DEFINE_NAMED_CID(MOZ_JSSUBSCRIPTLOADER_CID);
 
 #define XPCONNECT_CIDENTRIES                                                  \
   { &kNS_JS_ID_CID, false, nullptr,  nsJSIDConstructor },                     \
-  { &kMOZJSCOMPONENTLOADER_CID, false, nullptr, mozJSComponentLoaderConstructor },\
   { &kMOZ_JSSUBSCRIPTLOADER_CID, false, nullptr, mozJSSubScriptLoaderConstructor },
 
 #define XPCONNECT_CONTRACTS                                                   \
-  { MOZJSCOMPONENTLOADER_CONTRACTID, &kMOZJSCOMPONENTLOADER_CID },            \
   { MOZJSSUBSCRIPTLOADER_CONTRACTID, &kMOZ_JSSUBSCRIPTLOADER_CID },
 
 nsresult xpcModuleCtor();
