3445ffa25479e13630717e1fbffbf9490f51bc9a
Tom Prince <mozilla@hocat.ca>
2018-10-23 21:27:42 +0000

    Bug 1497860: Move mozconfig variant definitions to taskcluster config. r=ted
    Differential Revision: https://phabricator.services.mozilla.com/D8452

--- testing/mozharness/configs/builds/releng_sub_mac_configs/64_cross_noopt_debug.py
+++ testing/mozharness/configs/builds/releng_sub_mac_configs/64_cross_noopt_debug.py
@@ -1,10 +1,6 @@
 import os
 
 config = {
-    'default_actions': [
-        'clobber',
-        'build',
-    ],
     'stage_platform': 'macosx64-noopt-debug',
     'debug_build': True,
     #### 64 bit build specific #####
@@ -23,7 +19,6 @@ config = {
                 '/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin',
         ##
     },
-    'mozconfig_variant': 'cross-noopt-debug',
     'artifact_flag_build_variant_in_try': 'cross-debug-artifact',
     #######################
 }
