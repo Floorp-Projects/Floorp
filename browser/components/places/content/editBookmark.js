2cb50cf0ffc006feb76e4b735c11136333b6c8f9
Mark Banner <standard8@mozilla.com>
2018-10-23 17:58:43 +0000

    Bug 1501337 - Fix a leak and uncleared listeners in places code relating to the edit bookmarks views. r=mak, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D9546

--- browser/components/places/content/editBookmark.js
+++ browser/components/places/content/editBookmark.js
@@ -440,6 +440,7 @@ var gEditItemOverlay = {
 
     if (this._observersAdded) {
       PlacesUtils.bookmarks.removeObserver(this);
+      window.removeEventListener("unload", this);
       this._observersAdded = false;
     }
 
