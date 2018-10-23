2d23134dbb9f05fb27caed07bb94968948d40c9f
Aki Sasaki <asasaki@mozilla.com>
2018-10-23 21:12:51 +0000

    Bug 1499254 - add primary-dependency support. r=tomprince,Callek
    Depends on D9194
    
    Differential Revision: https://phabricator.services.mozilla.com/D9196
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/taskgraph/transforms/name_sanity.py
+++ taskcluster/taskgraph/transforms/name_sanity.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 Generate labels for tasks without names, consistently.
-Uses attributes from 'dependent-task'.
+Uses attributes from `dependent-task` or `primary-dependency`.
 """
 
 from __future__ import absolute_import, print_function, unicode_literals
@@ -19,7 +19,10 @@ def make_label(config, jobs):
     """ Generate a sane label for a new task constructed from a dependency
     Using attributes from the dependent job and the current task kind"""
     for job in jobs:
-        dep_job = job['dependent-task']
+        if 'primary-dependency' in job:
+            dep_job = job['primary-dependency']
+        else:
+            dep_job = job['dependent-task']
         attr = dep_job.attributes.get
 
         if attr('locale', job.get('locale')):
