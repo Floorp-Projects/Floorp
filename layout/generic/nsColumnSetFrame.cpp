4e6b53ce88e634a8b85ade1c2344f36bec50d7c4
Ting-Yu Lin <tlin@mozilla.com>
2018-10-23 13:08:59 +0000

    Bug 1501145 Part 1 - Assign default values to ReflowConfig fields, and explicitly initialize them in ChooseColumnStrategy. r=mats
    The original statement is neat, but one cannot easily associates the values
    computed in ChooseColumnStrategy with the actually field name in ReflowConfig.
    
    Remove the aFeasibleBSize and aInfeasibleBSize for ChooseColumnStrategy()
    because they're not used.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9467
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/nsColumnSetFrame.cpp
+++ layout/generic/nsColumnSetFrame.cpp
@@ -315,12 +315,8 @@ nsColumnSetFrame::ClampUsedColumnWidth(const nsStyleCoord& aColumnWidth)
 
 nsColumnSetFrame::ReflowConfig
 nsColumnSetFrame::ChooseColumnStrategy(const ReflowInput& aReflowInput,
-                                       bool aForceAuto = false,
-                                       nscoord aFeasibleBSize = NS_INTRINSICSIZE,
-                                       nscoord aInfeasibleBSize = 0)
+                                       bool aForceAuto = false)
 {
-  nscoord knownFeasibleBSize = aFeasibleBSize;
-  nscoord knownInfeasibleBSize = aInfeasibleBSize;
   WritingMode wm = aReflowInput.GetWritingMode();
 
   const nsStyleColumn* colStyle = StyleColumn();
@@ -456,9 +452,18 @@ nsColumnSetFrame::ChooseColumnStrategy(const ReflowInput& aReflowInput,
                  colBSize,
                  colGap);
 
-  ReflowConfig config = { numColumns, colISize, expectedISizeLeftOver, colGap,
-                          colBSize, isBalancing, knownFeasibleBSize,
-                          knownInfeasibleBSize, computedBSize, consumedBSize };
+  ReflowConfig config;
+  config.mBalanceColCount = numColumns;
+  config.mColISize = colISize;
+  config.mExpectedISizeLeftOver = expectedISizeLeftOver;
+  config.mColGap = colGap;
+  config.mColMaxBSize = colBSize;
+  config.mIsBalancing = isBalancing;
+  config.mKnownFeasibleBSize = NS_INTRINSICSIZE;
+  config.mKnownInfeasibleBSize = 0;
+  config.mComputedBSize = computedBSize;
+  config.mConsumedBSize = consumedBSize;
+
   return config;
 }
 
