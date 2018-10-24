0be0e4ac76ee3cc2a8da952681d4f86a3268f900
Kannan Vijayan <kvijayan@mozilla.com>
2018-10-24 12:16:32 -0400

    Bug 1500255 - Handle objects with null prototype in stub-generator for oob array setelems. r=tcampbell

---
+++ js/src/jit-test/tests/cacheir/bug1500255.js
@@ -0,0 +1,10 @@
+
+setJitCompilerOption("offthread-compilation.enable", 0);
+setJitCompilerOption("ion.warmup.trigger", 0);
+
+foo();
+
+function foo() {
+    Array.prototype.__proto__ = null;
+    Array.prototype[1] = 'bar';
+}
