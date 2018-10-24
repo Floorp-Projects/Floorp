c90081d3409bb4d0c92f864d09875f096921ea91
Julien Wajsberg <felash@gmail.com>
2018-10-24 12:09:37 +0000

    Bug 1489745 - Convert the preference sample-frequency-khz to hz r=ochameau,mstange
    The int preference profiler.sample-frequency-khz didn't make it possible
    to reduce the overhead in tests, because we could have intervals bigger
    than 1ms. With this change we can now do it.
    
    Depends on D8547
    
    Differential Revision: https://phabricator.services.mozilla.com/D8548

--- devtools/client/performance/modules/global.js
+++ devtools/client/performance/modules/global.js
@@ -30,7 +30,7 @@ exports.PREFS = new PrefsHelper("devtools.performance", {
   "memory-sample-probability": ["Float", "memory.sample-probability"],
   "memory-max-log-length": ["Int", "memory.max-log-length"],
   "profiler-buffer-size": ["Int", "profiler.buffer-size"],
-  "profiler-sample-frequency": ["Int", "profiler.sample-frequency-khz"],
+  "profiler-sample-frequency": ["Int", "profiler.sample-frequency-hz"],
   // TODO: re-enable once we flame charts via bug 1148663.
   "enable-memory-flame": ["Bool", "ui.enable-memory-flame"],
 });
