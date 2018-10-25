2160f559d8955a32f27ae0379b5ee11bd7324090
Andrea Marchesini <amarchesini@mozilla.com>
2018-10-25 10:44:12 +0200

    Bug 1499995 - part 3 - Tests for nested iframes controlled by ServiceWorkers. r=ehsan a=RyanVM
    --HG--
    extra : rebase_source : bd818fd200ac327dd846a4ccd8389c5264c546ba
    extra : source : 2393a7d9f454e3d5fc310c5ee4a1cd94fea42ab2

--- dom/serviceworkers/test/browser_antitracking_subiframes.js
+++ dom/serviceworkers/test/browser_antitracking_subiframes.js
@@ -1,12 +1,7 @@
-
-const BEHAVIOR_ACCEPT         = Ci.nsICookieService.BEHAVIOR_ACCEPT;
 const BEHAVIOR_REJECT_TRACKER = Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER;
 
-let {UrlClassifierTestUtils} =
-  ChromeUtils.import("resource://testing-common/UrlClassifierTestUtils.jsm", {});
-
 const TOP_DOMAIN = "http://mochi.test:8888/";
-const SW_DOMAIN = "https://tracking.example.org/";
+const SW_DOMAIN = "https://example.org/";
 
 const TOP_TEST_ROOT = getRootDirectory(gTestPath)
   .replace("chrome://mochitests/content/", TOP_DOMAIN);
@@ -16,20 +11,20 @@ const SW_TEST_ROOT = getRootDirectory(gTestPath)
 const TOP_EMPTY_PAGE = `${TOP_TEST_ROOT}empty_with_utils.html`;
 const SW_REGISTER_PAGE = `${SW_TEST_ROOT}empty_with_utils.html`;
 const SW_IFRAME_PAGE = `${SW_TEST_ROOT}page_post_controlled.html`;
-// An empty script suffices for our SW needs; it's by definition no-fetch.
 const SW_REL_SW_SCRIPT = "empty.js";
 
 /**
- * Set up a no-fetch-optimized ServiceWorker on a domain that will be covered by
- * tracking protection (but is not yet).  Once the SW is installed, activate TP
- * and create a tab that embeds that tracking-site in an iframe.
+ * Set up a ServiceWorker on a domain that will be used as 3rd party iframe.
+ * That 3rd party frame should be controlled by the ServiceWorker.
+ * After that, we open a second iframe into the first one. That should not be
+ * controlled.
  */
 add_task(async function() {
   await SpecialPowers.pushPrefEnv({'set': [
     ['dom.serviceWorkers.enabled', true],
     ['dom.serviceWorkers.exemptFromPerDomainMax', true],
     ['dom.serviceWorkers.testing.enabled', true],
-    ['network.cookie.cookieBehavior', BEHAVIOR_ACCEPT],
+    ['network.cookie.cookieBehavior', BEHAVIOR_REJECT_TRACKER],
   ]});
 
   // Open the top-level page.
@@ -39,46 +34,53 @@ add_task(async function() {
     opening: SW_REGISTER_PAGE
   });
 
-  // ## Install SW
-  info("Installing SW");
+  // Install SW
+  info("Registering a SW: " + SW_REL_SW_SCRIPT);
   await ContentTask.spawn(
     topTab.linkedBrowser,
     { sw: SW_REL_SW_SCRIPT },
     async function({ sw }) {
       // Waive the xray to use the content utils.js script functions.
       await content.wrappedJSObject.registerAndWaitForActive(sw);
+      // User interaction
+      content.document.userInteractionForTesting();
     }
   );
 
-  // Enable Anti-tracking.
-  await SpecialPowers.pushPrefEnv({'set': [
-    ['privacy.trackingprotection.enabled', false],
-    ["privacy.trackingprotection.pbmode.enabled", false],
-    ["privacy.trackingprotection.annotate_channels", true],
-    ['network.cookie.cookieBehavior', BEHAVIOR_REJECT_TRACKER],
-  ]});
-  await UrlClassifierTestUtils.addTestTrackers();
-
-  // Open the top-level URL.
   info("Loading a new top-level URL: " + TOP_EMPTY_PAGE);
   let browserLoadedPromise = BrowserTestUtils.browserLoaded(topTab.linkedBrowser);
   await BrowserTestUtils.loadURI(topTab.linkedBrowser, TOP_EMPTY_PAGE);
   await browserLoadedPromise;
 
   // Create Iframe in the top-level page and verify its state.
+  info("Creating iframe and checking if controlled");
   let { controlled } = await ContentTask.spawn(
     topTab.linkedBrowser,
     { url: SW_IFRAME_PAGE },
     async function ({ url }) {
+      content.document.userInteractionForTesting();
       const payload =
         await content.wrappedJSObject.createIframeAndWaitForMessage(url);
       return payload;
     }
   );
 
-  ok(!controlled, "Should not be controlled!");
+  ok(controlled, "Should be controlled!");
 
-  // ## Cleanup
+  // Create a nested Iframe.
+  info("Creating nested-iframe and checking if controlled");
+  let { nested_controlled } = await ContentTask.spawn(
+    topTab.linkedBrowser,
+    { url: SW_IFRAME_PAGE },
+    async function ({ url }) {
+      const payload =
+        await content.wrappedJSObject.createNestedIframeAndWaitForMessage(url);
+      return payload;
+    }
+  );
+
+  ok(!nested_controlled, "Should not be controlled!");
+
   info("Loading the SW unregister page: " + SW_REGISTER_PAGE);
   browserLoadedPromise = BrowserTestUtils.browserLoaded(topTab.linkedBrowser);
   await BrowserTestUtils.loadURI(topTab.linkedBrowser, SW_REGISTER_PAGE);
