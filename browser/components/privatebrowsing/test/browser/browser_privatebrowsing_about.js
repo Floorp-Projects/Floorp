eb0bcb63864786c6cc4edaa5143f0b8c984d34d3
Ehsan Akhgari <ehsan@mozilla.com>
2018-10-25 16:46:49 +0000

    Bug 1501286 - Part 3: Remove support for tracking protection UI in about:privatebrowsing r=johannh
    Depends on D9520
    
    Differential Revision: https://phabricator.services.mozilla.com/D9521

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_about.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_about.js
@@ -4,7 +4,6 @@
 
 const TP_PB_ENABLED_PREF = "privacy.trackingprotection.pbmode.enabled";
 const CB_ENABLED_PREF = "browser.contentblocking.enabled";
-const CB_UI_ENABLED_PREF = "browser.contentblocking.ui.enabled";
 
 /**
  * Opens a new private window and loads "about:privatebrowsing" there.
@@ -54,36 +53,7 @@ add_task(async function test_links() {
   Services.prefs.setCharPref("app.support.baseURL", "https://example.com/");
   Services.prefs.setCharPref("privacy.trackingprotection.introURL",
                              "https://example.com/tour");
-  Services.prefs.setBoolPref(CB_UI_ENABLED_PREF, false);
   registerCleanupFunction(function() {
-    Services.prefs.clearUserPref(CB_UI_ENABLED_PREF);
-    Services.prefs.clearUserPref("privacy.trackingprotection.introURL");
-    Services.prefs.clearUserPref("app.support.baseURL");
-  });
-
-  let { win, tab } = await openAboutPrivateBrowsing();
-
-  await testLinkOpensTab({ win, tab,
-    elementId: "learnMore",
-    expectedUrl: "https://example.com/private-browsing",
-  });
-
-  await testLinkOpensUrl({ win, tab,
-    elementId: "startTour",
-    expectedUrl: "https://example.com/tour?variation=0",
-  });
-
-  await BrowserTestUtils.closeWindow(win);
-});
-
-add_task(async function test_links_CB() {
-  // Use full version and change the remote URLs to prevent network access.
-  Services.prefs.setCharPref("app.support.baseURL", "https://example.com/");
-  Services.prefs.setCharPref("privacy.trackingprotection.introURL",
-                             "https://example.com/tour");
-  Services.prefs.setBoolPref(CB_UI_ENABLED_PREF, true);
-  registerCleanupFunction(function() {
-    Services.prefs.clearUserPref(CB_UI_ENABLED_PREF);
     Services.prefs.clearUserPref("privacy.trackingprotection.introURL");
     Services.prefs.clearUserPref("app.support.baseURL");
   });
@@ -118,57 +88,15 @@ function waitForPrefChanged(pref) {
 
 /**
  * Tests the action to disable and re-enable Tracking Protection in
- * "about:privatebrowsing".
- */
-add_task(async function test_toggleTrackingProtection() {
-  // Use tour version but disable Tracking Protection.
-  Services.prefs.setBoolPref(TP_PB_ENABLED_PREF, true);
-  // For good measure, check that content blocking being off
-  // has no impact if the contentblocking UI is not shown.
-  Services.prefs.setBoolPref(CB_ENABLED_PREF, false);
-  Services.prefs.setBoolPref(CB_UI_ENABLED_PREF, false);
-
-  registerCleanupFunction(function() {
-    Services.prefs.clearUserPref(TP_PB_ENABLED_PREF);
-    Services.prefs.clearUserPref(CB_ENABLED_PREF);
-    Services.prefs.clearUserPref(CB_UI_ENABLED_PREF);
-  });
-
-  let { win, tab } = await openAboutPrivateBrowsing();
-
-  // Set up the observer for the preference change before triggering the action.
-  let promisePrefChanged = waitForPrefChanged(TP_PB_ENABLED_PREF);
-  await ContentTask.spawn(tab, {}, async function() {
-    is(content.document.getElementById("tpToggle").checked, true, "toggle is active");
-    content.document.getElementById("tpButton").click();
-  });
-  await promisePrefChanged;
-  ok(!Services.prefs.getBoolPref(TP_PB_ENABLED_PREF), "Tracking Protection is disabled.");
-
-  promisePrefChanged = waitForPrefChanged(TP_PB_ENABLED_PREF);
-  await ContentTask.spawn(tab, {}, async function() {
-    is(content.document.getElementById("tpToggle").checked, false, "toggle is not active");
-    content.document.getElementById("tpButton").click();
-  });
-  await promisePrefChanged;
-  ok(Services.prefs.getBoolPref(TP_PB_ENABLED_PREF), "Tracking Protection is enabled.");
-
-  await BrowserTestUtils.closeWindow(win);
-});
-
-/**
- * Tests the action to disable and re-enable Tracking Protection in
  * "about:privatebrowsing" when content blocking is disabled.
  */
 add_task(async function test_toggleTrackingProtectionContentBlocking() {
   Services.prefs.setBoolPref(TP_PB_ENABLED_PREF, true);
   Services.prefs.setBoolPref(CB_ENABLED_PREF, false);
-  Services.prefs.setBoolPref(CB_UI_ENABLED_PREF, true);
 
   registerCleanupFunction(function() {
     Services.prefs.clearUserPref(TP_PB_ENABLED_PREF);
     Services.prefs.clearUserPref(CB_ENABLED_PREF);
-    Services.prefs.clearUserPref(CB_UI_ENABLED_PREF);
   });
 
   let { win, tab } = await openAboutPrivateBrowsing();
