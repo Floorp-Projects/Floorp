09d4b61b4327d170c6bd9c1e12d159bf00849c8c
Ehsan Akhgari <ehsan@mozilla.com>
2018-10-25 16:52:19 +0000

    Bug 1501286 - Part 2: Remove support for tracking protection UI from Control Centre r=johannh
    Depends on D9519
    
    Differential Revision: https://phabricator.services.mozilla.com/D9520
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/trackingUI/browser_trackingUI_appMenu_toggle.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_appMenu_toggle.js
@@ -4,7 +4,6 @@
 "use strict";
 
 const CB_PREF = "browser.contentblocking.enabled";
-const CB_UI_PREF = "browser.contentblocking.ui.enabled";
 const TOGGLE_PREF = "browser.contentblocking.global-toggle.enabled";
 
 ChromeUtils.import("resource://testing-common/CustomizableUITestUtils.jsm", this);
@@ -13,7 +12,6 @@ ChromeUtils.import("resource://testing-common/CustomizableUITestUtils.jsm", this
 // normal windows and private windows.
 add_task(async function testGlobalToggle() {
   await SpecialPowers.pushPrefEnv({set: [
-    [CB_UI_PREF, true],
     [TOGGLE_PREF, true],
   ]});
 
@@ -42,52 +40,4 @@ add_task(async function testGlobalToggle() {
   is(Services.prefs.getBoolPref(CB_PREF), true);
 
   Services.prefs.clearUserPref(CB_PREF);
-});
-
-// Test that the app menu toggle correctly flips the TP pref in
-// normal windows and private windows.
-add_task(async function testGlobalToggleTP() {
-  await SpecialPowers.pushPrefEnv({set: [
-    [CB_UI_PREF, false],
-    [TOGGLE_PREF, true],
-  ]});
-
-  async function runTest(privateWindow) {
-    let win = await BrowserTestUtils.openNewBrowserWindow({private: privateWindow});
-
-    let panelUIButton = await TestUtils.waitForCondition(() => win.document.getElementById("PanelUI-menu-button"));
-
-    let prefName = privateWindow ? "privacy.trackingprotection.pbmode.enabled" :
-      "privacy.trackingprotection.enabled";
-
-    info("Opening main menu");
-
-    let promiseShown = BrowserTestUtils.waitForEvent(win.PanelUI.mainView, "ViewShown");
-    panelUIButton.click();
-    await promiseShown;
-
-    info("Opened main menu");
-
-    let toggle = win.document.getElementById("appMenu-tp-toggle");
-
-    Services.prefs.setBoolPref(prefName, false);
-    await TestUtils.waitForCondition(() => toggle.getAttribute("enabled") == "false");
-
-    Services.prefs.setBoolPref(prefName, true);
-    await TestUtils.waitForCondition(() => toggle.getAttribute("enabled") == "true");
-
-    toggle.click();
-    is(Services.prefs.getBoolPref(prefName), false);
-
-    toggle.click();
-    is(Services.prefs.getBoolPref(prefName), true);
-
-    Services.prefs.clearUserPref(prefName);
-
-    await BrowserTestUtils.closeWindow(win);
-  }
-
-  // Run once in private and once in normal window.
-  await runTest(true);
-  await runTest(false);
 });
