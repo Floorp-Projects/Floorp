7cf94dd9bc5d1189c31c74a56629506395c9f086
Razvan Caliman <rcaliman@mozilla.com>
2018-10-25 13:42:05 +0000

    Bug 1499049 - (Part 5) Refactor Changes panel to render diff of nested CSS rule structures; r=pbro
    Depends on D8721
    
    - Externalize reusable CSSDeclaration React component;
    - Introduce the ability to render rules with unlimited levels of nesting, but cap indentation in the UI at a reasonable level;
    - Remove accordion behavior from rules, but keep for sources;
    - Cleanup CSS styles for Changes panel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D8722
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/inspector/changes/components/CSSDeclaration.js
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { PureComponent } = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+
+class CSSDeclaration extends PureComponent {
+  static get propTypes() {
+    return {
+      property: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      className: PropTypes.string,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      className: "",
+    };
+  }
+
+  render() {
+    const { property, value, className } = this.props;
+
+    return dom.div({ className },
+      dom.span({ className: "declaration-name theme-fg-color5"}, property),
+      ":",
+      dom.span({ className: "declaration-value theme-fg-color1"}, value),
+      ";"
+    );
+  }
+}
+
+module.exports = CSSDeclaration;
