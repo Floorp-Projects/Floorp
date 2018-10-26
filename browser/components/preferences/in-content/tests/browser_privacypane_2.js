0841dc1a65f1f6ee48e0be2a0fac026a17b0909d
Ehsan Akhgari <ehsan@mozilla.com>
2018-10-26 07:01:43 +0000

    Bug 1502237 - Remove the pref for controlling whether the tracker cookie blocking setting would appear in the Cookies and Site Data drop-down r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D9878
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/preferences/in-content/tests/browser_privacypane_2.js
+++ browser/components/preferences/in-content/tests/browser_privacypane_2.js
@@ -7,10 +7,6 @@ if (jar) {
 /* import-globals-from privacypane_tests_perwindow.js */
 Services.scriptloader.loadSubScript(rootDir + "privacypane_tests_perwindow.js", this);
 
-SpecialPowers.pushPrefEnv({"set":
-  [["browser.contentblocking.cookies-site-data.ui.reject-trackers.enabled", false]],
-});
-
 run_test_subset([
   test_pane_visibility,
   test_dependent_elements,
