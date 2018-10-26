8416fded306fedd3d6fa88fa0d548a7561ab3d18
Kyle Machulis <kyle@nonpolynomial.com>
2018-10-26 03:50:37 +0000

    Bug 1492648 - Move from nsDocShellLoadInfo to nsDocShellLoadState r=bzbarsky,nika
    Creates the nsDocShellLoadState object, which is basically
    nsDocShellLoadInfo plus a few extra fields to make it usable as a
    single argument to nsDocShell::LoadURI (and eventually
    nsDocShell::InternalLoad).
    
    Subframe history handling is a huge logic block in
    nsDocShell::LoadURI, which is only used on history loads. This patch
    also extracts the logic out into its own function to make the body of
    LoadURI clearer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D6944

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -124,7 +124,7 @@
 #include "nsString.h"
 #include "nsISupportsPrimitives.h"
 #include "mozilla/Telemetry.h"
-#include "nsDocShellLoadInfo.h"
+#include "nsDocShellLoadState.h"
 #include "nsWebBrowser.h"
 
 #ifdef XP_WIN
@@ -958,7 +958,7 @@ TabChild::ProvideWindow(mozIDOMWindowProxy* aParent,
                         bool aPositionSpecified, bool aSizeSpecified,
                         nsIURI* aURI, const nsAString& aName,
                         const nsACString& aFeatures, bool aForceNoOpener,
-                        nsDocShellLoadInfo* aLoadInfo, bool* aWindowIsNew,
+                        nsDocShellLoadState* aLoadState, bool* aWindowIsNew,
                         mozIDOMWindowProxy** aReturn)
 {
     *aReturn = nullptr;
@@ -1002,7 +1002,7 @@ TabChild::ProvideWindow(mozIDOMWindowProxy* aParent,
                                    aName,
                                    aFeatures,
                                    aForceNoOpener,
-                                   aLoadInfo,
+                                   aLoadState,
                                    aWindowIsNew,
                                    aReturn);
 }
