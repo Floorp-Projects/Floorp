7393016268dc93e1c3c00355b93855496fbe4572
David Walsh <dwalsh@mozilla.com>
2018-10-26 13:55:04 -0500

    Bug 1502224 - Update debugger frontend v96 r=jdescottes
    Test Plan: https://treeherder.mozilla.org/#/jobs?repo=try&revision=59f88434009d15d88731f658a1f0b177dbf2d008
    
    Tags: #secure-revision
    
    Bug #: 1502224
    
    Differential Revision: https://phabricator.services.mozilla.com/D9936

--- devtools/client/debugger/new/test/mochitest/browser_dbg-outline.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-outline.js
@@ -8,7 +8,6 @@ function getItems(dbg) {
 function getNthItem(dbg, index) {
   return findElement(dbg, "outlineItem", index);
 }
-
 // Tests that the editor highlights the correct location when the
 // debugger pauses
 add_task(async function() {
@@ -57,72 +56,4 @@ add_task(async function() {
     "doEval()",
     "Alphabetized first function is correct"
   );
-});
-
-// Tests the outline pane fuzzy filtering of outline items
-add_task(async function() {
-  const dbg = await initDebugger("doc-scripts.html");
-  await selectSource(dbg, "long", 1);
-  findElementWithSelector(dbg, ".outline-tab").click();
-
-  // turn off alphetical sort if active
-  const alphabetizeButton = findElementWithSelector(
-    dbg,
-    ".outline-footer button"
-  );
-  if (alphabetizeButton.className === "active") {
-    alphabetizeButton.click();
-  }
-
-  const outlineFilter = findElementWithSelector(dbg, ".outline-filter-input");
-  ok(outlineFilter, "Outline filter is present");
-
-  outlineFilter.blur();
-  ok(
-    !findElementWithSelector(dbg, ".outline-filter-input.focused"),
-    "does not have class focused when not focused"
-  );
-
-  outlineFilter.focus();
-  ok(
-    findElementWithSelector(dbg, ".outline-filter-input.focused"),
-    "has class focused when focused"
-  );
-
-  is(getItems(dbg).length, 9, "9 items in the unfiltered list");
-
-  // filter all items starting with t
-  type(dbg, "t");
-  is(getItems(dbg).length, 4, "4 items in the list after 't' filter");
-  ok(getItems(dbg)[0].textContent.includes("TodoModel(key)"), "item TodoModel");
-  ok(
-    getItems(dbg)[1].textContent.includes("toggleAll(checked)"),
-    "item toggleAll"
-  );
-  ok(
-    getItems(dbg)[2].textContent.includes("toggle(todoToToggle)"),
-    "item toggle"
-  );
-  ok(getItems(dbg)[3].textContent.includes("testModel()"), "item testModel");
-
-  // filter using term 'tog'
-  type(dbg, "og");
-  is(getItems(dbg).length, 2, "2 items in the list after 'tog' filter");
-  ok(
-    getItems(dbg)[0].textContent.includes("toggleAll(checked)"),
-    "item toggleAll"
-  );
-  ok(
-    getItems(dbg)[1].textContent.includes("toggle(todoToToggle)"),
-    "item toggle"
-  );
-
-  pressKey(dbg, "Escape");
-  is(getItems(dbg).length, 9, "9 items in the list after escape pressed");
-
-  // check that the term 'todo' includes items with todo
-  type(dbg, "todo");
-  is(getItems(dbg).length, 2, "2 items in the list after 'todo' filter");
-  ok(getItems(dbg)[0].textContent.includes("TodoModel(key)"), "item TodoModel");
-  ok(getItems(dbg)[1].textContent.includes("addTodo(title)"), "item addTodo");
 });
