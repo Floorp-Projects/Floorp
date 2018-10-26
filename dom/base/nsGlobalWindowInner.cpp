33ed3fc76be16c33e45ea901f81029d86bbc0225
Daosheng Mu <daoshengmu@gmail.com>
2018-10-26 17:26:32 +0000

    Bug 1501869 - Part 2: Stop calling VR tasks when they are at background. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D9880

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -4446,6 +4446,22 @@ nsGlobalWindowInner::ResetVRTelemetry(bool aUpdate)
   }
 }
 
+void
+nsGlobalWindowInner::StartVRActivity()
+{
+  if (mVREventObserver) {
+    mVREventObserver->StartActivity();
+  }
+}
+
+void
+nsGlobalWindowInner::StopVRActivity()
+{
+  if (mVREventObserver) {
+    mVREventObserver->StopActivity();
+  }
+}
+
 #ifndef XP_WIN // This guard should match the guard at the callsite.
 static bool ShouldShowFocusRingIfFocusedByMouse(nsIContent* aNode)
 {
