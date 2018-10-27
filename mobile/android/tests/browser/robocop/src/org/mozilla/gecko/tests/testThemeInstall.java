3c3f54c865d42899b0b97272d39ad1b1cefc764f
Jan Henning <jh+bugzilla@buttercookie.de>
2018-10-27 14:46:08 +0200

    Bug 1429488 - Part 3: Test minimal Android static theme support. r=gbrown
    This tests that the moz-extension URI for the headerURL will be properly
    rewritten into a file URI before being passed to the Android front-end.
    
    The JS part of the test has been adapted for Robocop from toolkit/mozapps/
    extensions/test/browser/browser_webapi_theme.js.
    
    Likewise, the test extension is based on browser_theme.xpi from the same
    directory, but modified to include a real image file that can actually be
    persisted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10727

---
+++ mobile/android/tests/browser/robocop/src/org/mozilla/gecko/tests/testThemeInstall.java
@@ -0,0 +1,79 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * vim: ts=4 sw=4 expandtab:
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package org.mozilla.gecko.tests;
+
+import org.mozilla.gecko.EventDispatcher;
+import org.mozilla.gecko.util.BundleEventListener;
+import org.mozilla.gecko.util.EventCallback;
+import org.mozilla.gecko.util.GeckoBundle;
+
+import java.io.File;
+import java.net.URI;
+
+import static org.mozilla.gecko.tests.helpers.AssertionHelper.fFail;
+
+public class testThemeInstall extends JavascriptTest implements BundleEventListener {
+    private EventCallback mFinishCallback;
+    private boolean mFinished;
+
+    public testThemeInstall() {
+        super("testThemeInstall.js");
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        // Even inside a test environment, accessing mozAddonManager is only possible from this URL.
+        mBaseHostnameUrl = "https://example.com:443/tests";
+
+        EventDispatcher.getInstance().registerUiThreadListener(this, "LightweightTheme:Update");
+        EventDispatcher.getInstance().registerUiThreadListener(this, "Robocop:WaitOnUI");
+    }
+
+    @Override
+    public void handleMessage(String event, GeckoBundle message, EventCallback callback) {
+        if ("Robocop:WaitOnUI".equals(event)) {
+            mFinishCallback = callback;
+            checkTestFinished();
+            return;
+        }
+
+        mAsserter.is(event, "LightweightTheme:Update", "Received LightweightTheme:Update event");
+        try {
+            GeckoBundle themeData = message.getBundle("data");
+            String headerURL = themeData.getString("headerURL");
+            mAsserter.todo(headerURL.contains("/lightweighttheme-header"),
+                    "Theme update has the expected headerURL",null);
+            mAsserter.todo(headerURL.startsWith("file://"),
+                    "headerURL was transformed to file:// URL", null);
+            File themeFile = new File(new URI(headerURL).getPath());
+            mAsserter.todo(themeFile.exists(),
+                    "file pointed to by headerURL was successfully persisted", null);
+            mAsserter.todo(themeFile.length() > 0,
+                    "theme file is not empty", null);
+        } catch (Exception e) {
+            fFail("Event does not contain expected data: " + e.getMessage());
+        }
+
+        mFinished = true;
+        checkTestFinished();
+    }
+
+    private void checkTestFinished() {
+        if (mFinished && mFinishCallback != null) {
+            mFinishCallback.sendSuccess(null);
+        }
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        EventDispatcher.getInstance().unregisterUiThreadListener(this, "LightweightTheme:Update");
+        EventDispatcher.getInstance().unregisterUiThreadListener(this, "Robocop:WaitOnUI");
+    }
+}
