97a084eb6c08ed40a40e650a06ccdc78f12943ba
Ehsan Akhgari <ehsan@mozilla.com>
2018-10-29 14:59:24 +0000

    Bug 1502774 - Part 3: Remove nsAuthModule r=valentin
    Depends on D10026
    
    Differential Revision: https://phabricator.services.mozilla.com/D10027

--- extensions/auth/nsIAuthModule.cpp
+++ extensions/auth/nsIAuthModule.cpp
@@ -2,28 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "mozilla/ModuleUtils.h"
-#include "nsCRT.h"
 #include "nsIAuthModule.h"
-
-//-----------------------------------------------------------------------------
-#include "nsAuthGSSAPI.h"
 #if defined( USE_SSPI )
 #include "nsAuthSSPI.h"
 #else
 #include "nsAuthSambaNTLM.h"
 #endif
+#include "nsCRT.h"
+#include "nsAuthGSSAPI.h"
 #include "nsAuthSASL.h"
 #include "nsNTLMAuthModule.h"
 #include "nsNSSComponent.h"
-
-static const mozilla::Module::CIDEntry kAuthCIDs[] = {
-  { nullptr }
-};
-
-static const mozilla::Module::ContractIDEntry kAuthContracts[] = {
-  { nullptr }
-};
 
 // static
 already_AddRefed<nsIAuthModule>
@@ -73,30 +62,4 @@ nsIAuthModule::CreateInstance(const char* aType)
   return auth.forget();
 }
 
-//-----------------------------------------------------------------------------
 mozilla::LazyLogModule gNegotiateLog("negotiateauth");
-
-// setup nspr logging ...
-static nsresult
-InitNegotiateAuth()
-{
-  return NS_OK;
-}
-
-static void
-DestroyNegotiateAuth()
-{
-  nsAuthGSSAPI::Shutdown();
-}
-
-static const mozilla::Module kAuthModule = {
-  mozilla::Module::kVersion,
-  kAuthCIDs,
-  kAuthContracts,
-  nullptr,
-  nullptr,
-  InitNegotiateAuth,
-  DestroyNegotiateAuth
-};
-
-NSMODULE_DEFN(nsAuthModule) = &kAuthModule;
