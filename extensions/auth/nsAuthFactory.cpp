b37a88f3b9acc54a849a9ffaab86afc3656a5adf
Ehsan Akhgari <ehsan@mozilla.com>
2018-10-29 14:40:29 +0000

    Bug 1502774 - Part 2: Remove XPCOM component registrations for auth module classes r=valentin
    Depends on D10025
    
    Differential Revision: https://phabricator.services.mozilla.com/D10026

--- extensions/auth/nsAuthFactory.cpp
+++ extensions/auth/nsAuthFactory.cpp
@@ -3,205 +3,75 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/ModuleUtils.h"
-#include "nsAuth.h"
+#include "nsCRT.h"
+#include "nsIAuthModule.h"
 
 //-----------------------------------------------------------------------------
-
-#define NS_NEGOTIATEAUTH_CID                       \
-{ /* 96ec4163-efc8-407a-8735-007fb26be4e8 */       \
-  0x96ec4163,                                      \
-  0xefc8,                                          \
-  0x407a,                                          \
-  {0x87, 0x35, 0x00, 0x7f, 0xb2, 0x6b, 0xe4, 0xe8} \
-}
-#define NS_GSSAUTH_CID                             \
-{ /* dc8e21a0-03e4-11da-8cd6-0800200c9a66 */       \
-  0xdc8e21a0,                                      \
-  0x03e4,                                          \
-  0x11da,                                          \
-  {0x8c, 0xd6, 0x08, 0x00, 0x20, 0x0c, 0x9a, 0x66} \
-}
-
 #include "nsAuthGSSAPI.h"
-
 #if defined( USE_SSPI )
 #include "nsAuthSSPI.h"
-
-static nsresult
-nsSysNTLMAuthConstructor(nsISupports *outer, REFNSIID iid, void **result)
-{
-  if (outer)
-    return NS_ERROR_NO_AGGREGATION;
-
-  nsAuthSSPI *auth = new nsAuthSSPI(PACKAGE_TYPE_NTLM);
-  if (!auth)
-    return NS_ERROR_OUT_OF_MEMORY;
-
-  NS_ADDREF(auth);
-  nsresult rv = auth->QueryInterface(iid, result);
-  NS_RELEASE(auth);
-  return rv;
-}
-
-static nsresult
-nsKerbSSPIAuthConstructor(nsISupports *outer, REFNSIID iid, void **result)
-{
-  if (outer)
-    return NS_ERROR_NO_AGGREGATION;
-
-  nsAuthSSPI *auth = new nsAuthSSPI(PACKAGE_TYPE_KERBEROS);
-  if (!auth)
-    return NS_ERROR_OUT_OF_MEMORY;
-
-  NS_ADDREF(auth);
-  nsresult rv = auth->QueryInterface(iid, result);
-  NS_RELEASE(auth);
-  return rv;
-}
-
-#define NS_SYSNTLMAUTH_CID                         \
-{ /* dc195987-6e9a-47bc-b1fd-ab895d398833 */       \
-  0xdc195987,                                      \
-  0x6e9a,                                          \
-  0x47bc,                                          \
-  {0xb1, 0xfd, 0xab, 0x89, 0x5d, 0x39, 0x88, 0x33} \
-}
-
-#define NS_NEGOTIATEAUTHSSPI_CID                   \
-{ /* 78d3b0c0-0241-11da-8cd6-0800200c9a66 */       \
-  0x78d3b0c0,                                      \
-  0x0241,                                          \
-  0x11da,                                          \
-  {0x8c, 0xd6, 0x08, 0x00, 0x20, 0x0c, 0x9a, 0x66} \
-}
-
-#define NS_KERBAUTHSSPI_CID                        \
-{ /* 8c3a0e20-03e5-11da-8cd6-0800200c9a66 */       \
-  0x8c3a0e20,                                      \
-  0x03e5,                                          \
-  0x11da,                                          \
-  {0x8c, 0xd6, 0x08, 0x00, 0x20, 0x0c, 0x9a, 0x66} \
-}
-
 #else
-
-#define NS_SAMBANTLMAUTH_CID                       \
-{ /* bc54f001-6eb0-4e32-9f49-7e064d8e70ef */       \
-  0xbc54f001,                                      \
-  0x6eb0,                                          \
-  0x4e32,                                          \
-  {0x9f, 0x49, 0x7e, 0x06, 0x4d, 0x8e, 0x70, 0xef} \
-}
-
 #include "nsAuthSambaNTLM.h"
-static nsresult
-nsSambaNTLMAuthConstructor(nsISupports *outer, REFNSIID iid, void **result)
-{
-  if (outer)
-    return NS_ERROR_NO_AGGREGATION;
-
-  RefPtr<nsAuthSambaNTLM> auth = new nsAuthSambaNTLM();
-  if (!auth)
-    return NS_ERROR_OUT_OF_MEMORY;
-
-  nsresult rv = auth->SpawnNTLMAuthHelper();
-  if (NS_FAILED(rv)) {
-    // Failure here probably means that cached credentials were not available
-    return rv;
-  }
-
-  return auth->QueryInterface(iid, result);
-}
-
 #endif
-
-static nsresult
-nsKerbGSSAPIAuthConstructor(nsISupports *outer, REFNSIID iid, void **result)
-{
-  if (outer)
-    return NS_ERROR_NO_AGGREGATION;
-
-  nsAuthGSSAPI *auth = new nsAuthGSSAPI(PACKAGE_TYPE_KERBEROS);
-  if (!auth)
-    return NS_ERROR_OUT_OF_MEMORY;
-
-  NS_ADDREF(auth);
-  nsresult rv = auth->QueryInterface(iid, result);
-  NS_RELEASE(auth);
-  return rv;
-}
-
-static nsresult
-nsGSSAPIAuthConstructor(nsISupports *outer, REFNSIID iid, void **result)
-{
-  if (outer)
-    return NS_ERROR_NO_AGGREGATION;
-
-  nsAuthGSSAPI *auth = new nsAuthGSSAPI(PACKAGE_TYPE_NEGOTIATE);
-  if (!auth)
-    return NS_ERROR_OUT_OF_MEMORY;
-
-  NS_ADDREF(auth);
-  nsresult rv = auth->QueryInterface(iid, result);
-  NS_RELEASE(auth);
-  return rv;
-}
-
-
-#if defined( USE_SSPI )
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsAuthSSPI)
-#endif
-
-#define NS_AUTHSASL_CID                            \
-{ /* 815e42e0-72cc-480f-934b-148e33c228a6 */       \
-  0x815e42e0,                                      \
-  0x72cc,                                          \
-  0x480f,                                          \
-  {0x93, 0x4b, 0x14, 0x8e, 0x33, 0xc2, 0x28, 0xa6} \
-}
-
 #include "nsAuthSASL.h"
-NS_GENERIC_FACTORY_CONSTRUCTOR(nsAuthSASL)
-
-NS_DEFINE_NAMED_CID(NS_GSSAUTH_CID);
-NS_DEFINE_NAMED_CID(NS_NEGOTIATEAUTH_CID);
-#if defined( USE_SSPI )
-NS_DEFINE_NAMED_CID(NS_NEGOTIATEAUTHSSPI_CID);
-NS_DEFINE_NAMED_CID(NS_KERBAUTHSSPI_CID);
-NS_DEFINE_NAMED_CID(NS_SYSNTLMAUTH_CID);
-#else
-NS_DEFINE_NAMED_CID(NS_SAMBANTLMAUTH_CID);
-#endif
-NS_DEFINE_NAMED_CID(NS_AUTHSASL_CID);
-
+#include "nsNTLMAuthModule.h"
+#include "nsNSSComponent.h"
 
 static const mozilla::Module::CIDEntry kAuthCIDs[] = {
-  { &kNS_GSSAUTH_CID, false, nullptr, nsKerbGSSAPIAuthConstructor },
-  { &kNS_NEGOTIATEAUTH_CID, false, nullptr, nsGSSAPIAuthConstructor },
-#if defined( USE_SSPI )
-  { &kNS_NEGOTIATEAUTHSSPI_CID, false, nullptr, nsAuthSSPIConstructor },
-  { &kNS_KERBAUTHSSPI_CID, false, nullptr, nsKerbSSPIAuthConstructor },
-  { &kNS_SYSNTLMAUTH_CID, false, nullptr, nsSysNTLMAuthConstructor },
-#else
-  { &kNS_SAMBANTLMAUTH_CID, false, nullptr, nsSambaNTLMAuthConstructor },
-#endif
-  { &kNS_AUTHSASL_CID, false, nullptr, nsAuthSASLConstructor },
   { nullptr }
 };
 
 static const mozilla::Module::ContractIDEntry kAuthContracts[] = {
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "kerb-gss", &kNS_GSSAUTH_CID },
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "negotiate-gss", &kNS_NEGOTIATEAUTH_CID },
-#if defined( USE_SSPI )
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "negotiate-sspi", &kNS_NEGOTIATEAUTHSSPI_CID },
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "kerb-sspi", &kNS_KERBAUTHSSPI_CID },
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "sys-ntlm", &kNS_SYSNTLMAUTH_CID },
-#elif !defined(XP_MACOSX)
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "sys-ntlm", &kNS_SAMBANTLMAUTH_CID },
-#endif
-  { NS_AUTH_MODULE_CONTRACTID_PREFIX "sasl-gssapi", &kNS_AUTHSASL_CID },
   { nullptr }
 };
+
+// static
+already_AddRefed<nsIAuthModule>
+nsIAuthModule::CreateInstance(const char* aType)
+{
+  nsCOMPtr<nsIAuthModule> auth;
+  if (!nsCRT::strcmp(aType, "kerb-gss")) {
+    auth = new nsAuthGSSAPI(PACKAGE_TYPE_KERBEROS);
+  } else if (!nsCRT::strcmp(aType, "negotiate-gss")) {
+    auth = new nsAuthGSSAPI(PACKAGE_TYPE_NEGOTIATE);
+#if defined( USE_SSPI )
+  } else if (!nsCRT::strcmp(aType, "negotiate-sspi")) {
+    auth = new nsAuthSSPI();
+  } else if (!nsCRT::strcmp(aType, "kerb-sspi")) {
+    auth = new nsAuthSSPI(PACKAGE_TYPE_KERBEROS);
+  } else if (!nsCRT::strcmp(aType, "sys-ntlm")) {
+    auth = new nsAuthSSPI(PACKAGE_TYPE_NTLM);
+#elif !defined(XP_MACOSX)
+  } else if (!nsCRT::strcmp(aType, "sys-ntlm")) {
+    RefPtr<nsAuthSambaNTLM> sambaAuth = new nsAuthSambaNTLM();
+
+    nsresult rv = sambaAuth->SpawnNTLMAuthHelper();
+    if (NS_FAILED(rv)) {
+      // Failure here probably means that cached credentials were not available
+      return nullptr;
+    }
+
+    auth = sambaAuth.forget();
+#endif
+  } else if (!nsCRT::strcmp(aType, "sasl-gssapi")) {
+    auth = new nsAuthSASL();
+  } else if (!nsCRT::strcmp(aType, "ntlm") &&
+    XRE_IsParentProcess() &&
+    EnsureNSSInitializedChromeOrContent()) {
+    RefPtr<nsNTLMAuthModule> ntlmAuth = new nsNTLMAuthModule();
+
+    nsresult rv = ntlmAuth->InitTest();
+    if (NS_FAILED(rv)) {
+      return nullptr;
+    }
+
+    auth = ntlmAuth.forget();
+  } else {
+    return nullptr;
+  }
+
+  return auth.forget();
+}
 
 //-----------------------------------------------------------------------------
 mozilla::LazyLogModule gNegotiateLog("negotiateauth");
