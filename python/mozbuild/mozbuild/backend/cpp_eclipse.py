9e5b66fad17e44cd92c8b0efb50b702bc2488fb9
Jonathan Watt <jwatt@jwatt.org>
2018-10-30 15:56:29 +0000

    Bug 1501507. Make the Eclipse CDT project ignore some directories when indexing. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D9606
    
    --HG--
    extra : rebase_source : f06113a208005e56c00bb5c12159b21b51d1465c

--- python/mozbuild/mozbuild/backend/cpp_eclipse.py
+++ python/mozbuild/mozbuild/backend/cpp_eclipse.py
@@ -414,7 +414,11 @@ CPROJECT_TEMPLATE_FILEINFO = """                                        <fileInf
                                                 </tool>
                                         </fileInfo>
 """
-CPROJECT_TEMPLATE_FOOTER = """                                </configuration>
+CPROJECT_TEMPLATE_FOOTER = """
+					<sourceEntries>
+						<entry excluding="**/lib*|**/third_party/|tree/*.xcodeproj/|tree/.cargo/|tree/.vscode/|tree/build/|tree/extensions/|tree/gfx/angle/|tree/gfx/cairo/|tree/gfx/skia/skia/|tree/intl/icu/|tree/js/|tree/media/|tree/modules/freetype2|tree/modules/pdfium/|tree/netwerk/|tree/netwerk/sctp|tree/netwerk/srtp|tree/nsprpub/lib|tree/nsprpub/pr/src|tree/other-licenses/|tree/parser/|tree/python/|tree/security/nss/|tree/tools/" flags="VALUE_WORKSPACE_PATH" kind="sourcePath" name=""/>
+					</sourceEntries>
+                                </configuration>
                         </storageModule>
                         <storageModule moduleId="org.eclipse.cdt.core.externalSettings"/>
                 </cconfiguration>
