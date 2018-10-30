19c145a72836b4004eae0dfadccff593f5e341f0
Tom Prince <mozilla@hocat.ca>
2018-10-30 18:21:36 +0000

    Bug 1492128: [mozrelease] Fix flake8 errors in buglist_creator; r=mtabara
    Differential Revision: https://phabricator.services.mozilla.com/D10152

--- python/mozrelease/mozrelease/buglist_creator.py
+++ python/mozrelease/mozrelease/buglist_creator.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function
+
 import logging
 import os
 import re
@@ -11,7 +18,10 @@ BACKOUT_PREFIX = 'Backouts since previous changeset: '
 BUGZILLA_BUGLIST_TEMPLATE = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id={bugs}'
 BUG_NUMBER_REGEX = r'bug \d+'
 CHANGELOG_TO_FROM_STRING = '{product}_{version}_RELEASE'
-CHANGESET_URL_TEMPLATE = 'https://hg.mozilla.org/{release_branch}/{logtype}?fromchange={from_version}&tochange={to_version}&full=1'
+CHANGESET_URL_TEMPLATE = (
+    'https://hg.mozilla.org/{release_branch}/{logtype}'
+    '?fromchange={from_version}&tochange={to_version}&full=1'
+)
 FULL_CHANGESET_PREFIX = 'Full Mercurial changelog: '
 LIST_DESCRIPTION_TEMPLATE = 'Comparing Mercurial tag {from_version} to {to_version}:'
 MAX_BUGS_IN_BUGLIST = 250
@@ -27,7 +37,8 @@ def create_bugs_url(release):
     Creates list of bugs and backout bugs for release-drivers email
 
     :param release: dict -> containing information about release, from Ship-It
-    :return: str -> description of compared releases, with Bugzilla links containing all bugs in changeset
+    :return: str -> description of compared releases, with Bugzilla links
+        containing all bugs in changeset
     """
     try:
         # Extract the important data, ignore if beta1 release
@@ -46,7 +57,8 @@ def create_bugs_url(release):
         # Get all Hg tags for this branch, determine the previous version
         tag_url = MERCURIAL_TAGS_URL_TEMPLATE.format(release_branch=branch)
         mercurial_tags_json = requests.get(tag_url).json()
-        previous_version_tag = get_previous_tag_version(product, current_version_dot, current_version_tag, mercurial_tags_json)
+        previous_version_tag = get_previous_tag_version(
+            product, current_version_dot, current_version_tag, mercurial_tags_json)
 
         # Get the changeset between these versions, parse for all unique bugs and backout bugs
         resp = requests.get(CHANGESET_URL_TEMPLATE.format(release_branch=branch,
@@ -58,15 +70,17 @@ def create_bugs_url(release):
 
         # Return a descriptive string with links if any relevant bugs are found
         if unique_bugs or unique_backout_bugs:
-            description_string = LIST_DESCRIPTION_TEMPLATE.format(from_version=previous_version_tag,
-                                                                  to_version=current_version_tag)
+            description_string = LIST_DESCRIPTION_TEMPLATE.format(
+                from_version=previous_version_tag,
+                to_version=current_version_tag)
 
             changeset_html = CHANGESET_URL_TEMPLATE.format(release_branch=branch,
                                                            from_version=previous_version_tag,
                                                            to_version=current_revision,
                                                            logtype='pushloghtml')
 
-            return format_return_value(description_string, unique_bugs, unique_backout_bugs, changeset_html)
+            return format_return_value(
+                description_string, unique_bugs, unique_backout_bugs, changeset_html)
         else:
             return NO_BUGS
 
@@ -115,11 +129,13 @@ def create_short_url_with_prefix(buglist, backout_buglist):
         if set_of_bugs and len(set_of_bugs) < MAX_BUGS_IN_BUGLIST:
             try:
                 long_bugzilla_link = BUGZILLA_BUGLIST_TEMPLATE.format(bugs='%2C'.join(set_of_bugs))
-                url = requests.get(URL_SHORTENER_TEMPLATE.format(url=long_bugzilla_link)).json()['url']
+                response = requests.get(URL_SHORTENER_TEMPLATE.format(url=long_bugzilla_link))
+                url = response.json()['url']
                 url = prefix + url + '\n'
 
-                # If the Bugzilla link fails despite limiting the number of bugs, don't make the url and continue
             except (KeyError, ValueError):
+                # If the Bugzilla link fails despite limiting the number of
+                # bugs, don't make the url and continue
                 url = ''
         else:
             url = ''
@@ -139,8 +155,12 @@ def tag_version_to_dot_version_parse(tag):
     return parse_version(dot_version)
 
 
-def get_previous_tag_version(product, current_version_dot, current_version_tag, mercurial_tags_json):
-    """Gets the previous hg version tag for the product and branch, given the current version tag"""
+def get_previous_tag_version(
+    product, current_version_dot, current_version_tag, mercurial_tags_json,
+):
+    """
+    Gets the previous hg version tag for the product and branch, given the current version tag
+    """
 
     def _invalid_tag_filter(tag):
         """Filters by product and removes incorrect major version + base, end releases"""
@@ -163,13 +183,16 @@ def get_previous_tag_version(product, current_version_dot, current_version_tag, 
     dot_tag_version_mapping = sorted(dot_tag_version_mapping, key=itemgetter(0))
 
     # Find where the current version is and go back one to get the previous version
-    next_version_index = map(itemgetter(0), dot_tag_version_mapping).index(parse_version(current_version_dot)) - 1
+    next_version_index = (
+        map(itemgetter(0), dot_tag_version_mapping).index(parse_version(current_version_dot)) - 1
+    )
 
     return dot_tag_version_mapping[next_version_index][1]
 
 
 def format_return_value(description, unique_bugs, unique_backout_bugs, changeset_html):
-    reg_bugs_link, backout_bugs_link = create_short_url_with_prefix(unique_bugs, unique_backout_bugs)
+    reg_bugs_link, backout_bugs_link = create_short_url_with_prefix(
+        unique_bugs, unique_backout_bugs)
     changeset_full = FULL_CHANGESET_PREFIX + changeset_html
     return_str = '{description}\n{regular_bz_url}{backout_bz_url}{changeset_full}'\
         .format(description=description, regular_bz_url=reg_bugs_link,
