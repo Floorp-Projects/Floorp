f9d6a0088bfddcb61099749a6b96be6f915822d8
Chris Manchester <cmanchester@mozilla.com>
2018-10-30 18:12:30 +0000

    Bug 1501178 - Prioritize directories containing rust compilation in the make backend. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D10268

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -811,7 +811,11 @@ class RecursiveMakeBackend(CommonBackend):
 
         def add_category_rules(category, roots, graph):
             rule = root_deps_mk.create_rule(['recurse_%s' % category])
-            rule.add_dependencies(roots)
+            # Directories containing rust compilations don't generally depend
+            # on other directories in the tree, so putting them first here will
+            # start them earlier in the build.
+            rule.add_dependencies(chain((r for r in roots if 'rust' in r),
+                                        (r for r in roots if 'rust' not in r)))
             for target, deps in sorted(graph.items()):
                 if deps:
                     rule = root_deps_mk.create_rule([target])
