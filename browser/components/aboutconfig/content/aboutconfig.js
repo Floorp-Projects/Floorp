6ce85c3d990d775e17bec9f5dddfcbf6e95becdf
Vincent Cote <vincent.cote@uleth.ca>
2018-10-30 09:48:32 +0000

    Bug 1497637 - Pull and display all settings into the new "about:config" page. r=paolo
    Differential Revision: https://phabricator.services.mozilla.com/D8469

--- browser/components/aboutconfig/content/aboutconfig.js
+++ browser/components/aboutconfig/content/aboutconfig.js
@@ -2,4 +2,28 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* aboutconfig.js */
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+ChromeUtils.import("resource://gre/modules/Preferences.jsm");
+
+let gPrefArray;
+
+function onLoad() {
+  gPrefArray = Services.prefs.getChildList("").map(name => ({
+    name,
+    value: Preferences.get(name),
+    hasUserValue: Services.prefs.prefHasUserValue(name),
+  }));
+
+  gPrefArray.sort((a, b) => a.name > b.name);
+
+  let fragment = document.createDocumentFragment();
+
+  for (let pref of gPrefArray) {
+    let listItem = document.createElement("li");
+    listItem.textContent = pref.name + " || " +
+      (pref.hasUserValue ? "Modified" : "Default") + " || " +
+      pref.value.constructor.name + " || " + pref.value;
+    fragment.appendChild(listItem);
+  }
+  document.getElementById("list").appendChild(fragment);
+}
