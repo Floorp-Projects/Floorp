e391d956de95bfc83a4dc1da011166285ab41cd6
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-10-30 21:24:04 +0000

    Bug 1486960 - Pretend there is only a touch screen device if touch similation is enabled on RDM. #firefox-style-system-reviewers r=emilio
    When the touch similation is enabled on RDM, TOUCHEVENTS_OVERRIDE_ENABLED is
    propagated to all descendants docShells, so that we can just query it and return
    PointerCapabilities::Coarse for the case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10203

--- layout/style/nsMediaFeatures.cpp
+++ layout/style/nsMediaFeatures.cpp
@@ -268,6 +268,17 @@ GetPointerCapabilities(nsIDocument* aDocument, LookAndFeel::IntID aID)
 {
   MOZ_ASSERT(aID == LookAndFeel::eIntID_PrimaryPointerCapabilities ||
              aID == LookAndFeel::eIntID_AllPointerCapabilities);
+  MOZ_ASSERT(aDocument);
+
+  if (nsIDocShell* docShell = aDocument->GetDocShell()) {
+    // The touch-events-override happens only for the Responsive Design Mode so
+    // that we don't need to care about ResistFingerprinting.
+    uint32_t touchEventsOverride = nsIDocShell::TOUCHEVENTS_OVERRIDE_NONE;
+    docShell->GetTouchEventsOverride(&touchEventsOverride);
+    if (touchEventsOverride == nsIDocShell::TOUCHEVENTS_OVERRIDE_ENABLED) {
+      return PointerCapabilities::Coarse;
+    }
+  }
 
   // The default value is mouse-type pointer.
   const PointerCapabilities kDefaultCapabilities =
