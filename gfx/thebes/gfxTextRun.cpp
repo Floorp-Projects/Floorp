385784948e8a033e8dbbca6f445dc62a06b5c9b9
Jonathan Kew <jkew@mozilla.com>
2018-10-30 12:55:35 +0000

    Bug 1493743 - Make BuildFontList robust against an invalid FontFamilyName record with no name atom. r=lsalzman, a=RyanVM
    --HG--
    extra : source : 3f6bc2bf601b3083708fa2337df894e8a22a9b14

--- gfx/thebes/gfxTextRun.cpp
+++ gfx/thebes/gfxTextRun.cpp
@@ -1830,7 +1830,11 @@ gfxFontGroup::BuildFontList()
     // lookup fonts in the fontlist
     for (const FontFamilyName& name : mFamilyList.GetFontlist()->mNames) {
         if (name.IsNamed()) {
-            AddPlatformFont(nsAtomCString(name.mName), fonts);
+            if (name.mName) {
+                AddPlatformFont(nsAtomCString(name.mName), fonts);
+            } else {
+                MOZ_ASSERT_UNREACHABLE("broken FontFamilyName, no atom!");
+            }
         } else {
             pfl->AddGenericFonts(name.mType, mStyle.language, fonts);
             if (mTextPerf) {
