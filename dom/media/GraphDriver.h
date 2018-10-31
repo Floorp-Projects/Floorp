6dc2ae383758602032d7b8ff3bb2afdf79b72e57
Karl Tomlinson <karlt+@karlt.net>
2018-10-31 00:54:14 +0000

    Bug 1503132 remove unnecessary SystemClockDriver::mWaitState r=padenot
    mWaitState is only ever tested against WAITSTATE_WAITING_INDEFINITELY and so
    the other values are equivalent.  It is never WAITSTATE_WAITING_INDEFINITELY
    on leaving (or entering) WaitForNextIteration and so there is no need to set
    it in WakeUp().
    
    Differential Revision: https://phabricator.services.mozilla.com/D10162

--- dom/media/GraphDriver.h
+++ dom/media/GraphDriver.h
@@ -314,22 +314,6 @@ private:
   TimeStamp mInitialTimeStamp;
   TimeStamp mLastTimeStamp;
 
-  // This enum specifies the wait state of the driver.
-  enum WaitState {
-    // RunThread() is running normally
-    WAITSTATE_RUNNING,
-    // RunThread() is paused waiting for its next iteration, which will
-    // happen soon
-    WAITSTATE_WAITING_FOR_NEXT_ITERATION,
-    // RunThread() is paused indefinitely waiting for something to change
-    WAITSTATE_WAITING_INDEFINITELY,
-    // Something has signaled RunThread() to wake up immediately,
-    // but it hasn't done so yet
-    WAITSTATE_WAKING_UP
-  };
-  // This must be access with the monitor.
-  WaitState mWaitState;
-
   // This is true if this SystemClockDriver runs the graph because we could not
   // open an audio stream.
   bool mIsFallback;
