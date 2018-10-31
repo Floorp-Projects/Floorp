007e4be03f825449edae3dcc10ad05c4e6123a08
Ted Mielczarek <ted@mielczarek.org>
2018-10-31 20:03:53 +0000

    bug 1483941 - prompt to enable build system telemetry in bootstrap. r=nalexander
    This change adds a prompt to enable build system telemetry as part of
    bootstrap. The prompt will only be shown if the build.telemetry config value
    is not present, so users will not be prompted again if they have already
    opted-in. However, if a user answers 'no' we don't save that value to
    the config file because the default is to not send telemetry, so unless
    they manually add `telemetry = false` to their config file they will be
    prompted again the next time they run bootstrap.
    
    The config value is always written to `~/.mozbuild/machrc` where we store
    other Firefox build-related state. A standalone function is used to write
    the config file so that we can do so even when running from bootstrap.py
    outside of the context of a mach command.
    
    As part of this change a `prompt_yesno` method is added to `BaseBootstrapper`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9781

--- python/mozboot/mozboot/mach_commands.py
+++ python/mozboot/mozboot/mach_commands.py
@@ -16,6 +16,8 @@ from mach.decorators import (
 @CommandProvider
 class Bootstrap(object):
     """Bootstrap system and mach for optimal development experience."""
+    def __init__(self, context):
+        self._context = context
 
     @Command('bootstrap', category='devenv',
              description='Install required system packages for building.')
@@ -32,9 +34,12 @@ class Bootstrap(object):
     def bootstrap(self, application_choice=None, no_interactive=False, no_system_changes=False):
         from mozboot.bootstrap import Bootstrapper
 
-        bootstrapper = Bootstrapper(choice=application_choice,
-                                    no_interactive=no_interactive,
-                                    no_system_changes=no_system_changes)
+        bootstrapper = Bootstrapper(
+            choice=application_choice,
+            no_interactive=no_interactive,
+            no_system_changes=no_system_changes,
+            mach_context=self._context,
+        )
         bootstrapper.bootstrap()
 
 
