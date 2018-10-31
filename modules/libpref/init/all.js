fa69b86c5bb205fb8c37215cec6c9a2944935607
Dragana Damjanovic <dd.mozilla@gmail.com>
2018-10-31 14:20:35 +0000

    Bug 1503573 - Disable TFO all together. r=bagder
    Differential Revision: https://phabricator.services.mozilla.com/D10365

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4865,13 +4865,7 @@ pref("network.tcp.keepalive.retry_interval", 1); // seconds
 pref("network.tcp.keepalive.probe_count", 4);
 #endif
 
-#if !defined(EARLY_BETA_OR_EARLIER)
 pref("network.tcp.tcp_fastopen_enable", false);
-#elif  defined(XP_WIN) || defined(XP_MACOSX)
-pref("network.tcp.tcp_fastopen_enable", true);
-#else
-pref("network.tcp.tcp_fastopen_enable", false);
-#endif
 
 pref("network.tcp.tcp_fastopen_consecutive_failure_limit", 5);
 // We are trying to detect stalled tcp connections that use TFO and TLS
