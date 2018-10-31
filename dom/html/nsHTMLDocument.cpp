86d3f4ea4229762fd0ab89ad9fdd8006aea8eb4b
Boris Zbarsky <bzbarsky@mit.edu>
2018-10-31 14:34:12 -0400

    Bug 1503659 - Back out some debugging instrumentation from Firefox 64, now that it's in beta. r=mccr8, a=RyanVM

--- dom/html/nsHTMLDocument.cpp
+++ dom/html/nsHTMLDocument.cpp
@@ -202,11 +202,7 @@ NS_IMPL_ISUPPORTS_CYCLE_COLLECTION_INHERITED(nsHTMLDocument,
 JSObject*
 nsHTMLDocument::WrapNode(JSContext* aCx, JS::Handle<JSObject*> aGivenProto)
 {
-  JSObject* obj = HTMLDocument_Binding::Wrap(aCx, this, aGivenProto);
-  if (!obj) {
-      MOZ_CRASH("Looks like bug 1488480/1405521, with nsHTMLDocument::WrapNode failing");
-  }
-  return obj;
+  return HTMLDocument_Binding::Wrap(aCx, this, aGivenProto);
 }
 
 nsresult
