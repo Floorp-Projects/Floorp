bc89eefbaf242a2116e94abfa5f678b2456a57c5
Julian Descottes <jdescottes@mozilla.com>
2018-11-01 11:12:39 +0000

    Bug 1467712 - Update all test wrappers forwarding to ok;r=Standard8
    Depends on D10418
    
    Differential Revision: https://phabricator.services.mozilla.com/D10419

--- gfx/layers/apz/test/mochitest/apz_test_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_utils.js
@@ -263,7 +263,12 @@ function runSubtestsSeriallyInFreshWindows(aSubtests) {
         w.isApzSubtest = true;
         w.SimpleTest = SimpleTest;
         w.is = function(a, b, msg) { return is(a, b, aFile + " | " + msg); };
-        w.ok = function(cond, name, diag) { return ok(cond, aFile + " | " + name, diag); };
+        w.ok = function(cond, msg) {
+          arguments[1] = aFile + " | " + msg;
+          // Forward all arguments to SimpleTest.ok where we will check that ok() was
+          // called with at most 2 arguments.
+          return SimpleTest.ok.apply(SimpleTest, arguments);
+        };
         if (test.onload) {
           w.addEventListener('load', function(e) { test.onload(w); }, { once: true });
         }
