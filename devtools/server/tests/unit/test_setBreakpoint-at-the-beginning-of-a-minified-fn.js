8ef4bd09cc51dab464c3a674d353e4b655b2484e
Logan Smyth <loganfsmyth@gmail.com>
2018-11-01 19:09:41 +0000

    Bug 1503436 - Only add breakpoints to first/last offset of script if column is directly in script. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D10384

---
+++ devtools/server/tests/unit/test_setBreakpoint-at-the-beginning-of-a-minified-fn.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var SOURCE_URL = getFileUrl("setBreakpoint-on-column-minified.js");
+
+async function run_test() {
+  do_test_pending();
+  const { createRootActor } = require("xpcshell-test/testactors");
+  DebuggerServer.setRootActor(createRootActor);
+  DebuggerServer.init(() => true);
+  const global = createTestGlobal("test");
+  DebuggerServer.addTestGlobal(global);
+
+  const client = new DebuggerClient(DebuggerServer.connectPipe());
+  await connect(client);
+
+  const { tabs } = await listTabs(client);
+  const tab = findTab(tabs, "test");
+  const [, targetFront] = await attachTarget(client, tab);
+  const [, threadClient] = await attachThread(targetFront);
+  await resume(threadClient);
+
+  const promise = waitForNewSource(threadClient, SOURCE_URL);
+  loadSubScript(SOURCE_URL, global);
+  const { source } = await promise;
+  const sourceClient = threadClient.source(source);
+
+  // Pause inside of the nested function so we can make sure that we don't
+  // add any other breakpoints at other places on this line.
+  const location = { line: 3, column: 47 };
+  let [packet, breakpointClient] = await setBreakpoint(
+    sourceClient,
+    location
+  );
+
+  Assert.ok(!packet.isPending);
+  Assert.equal(false, "actualLocation" in packet);
+
+  packet = await executeOnNextTickAndWaitForPause(function() {
+    Cu.evalInSandbox("f()", global);
+  }, client);
+
+  Assert.equal(packet.type, "paused");
+  const why = packet.why;
+  Assert.equal(why.type, "breakpoint");
+  Assert.equal(why.actors.length, 1);
+  Assert.equal(why.actors[0], breakpointClient.actor);
+
+  const frame = packet.frame;
+  const where = frame.where;
+  Assert.equal(where.source.actor, source.actor);
+  Assert.equal(where.line, location.line);
+  Assert.equal(where.column, 52);
+
+  const variables = frame.environment.bindings.variables;
+  Assert.equal(variables.a.value.type, "undefined");
+  Assert.equal(variables.b.value.type, "undefined");
+  Assert.equal(variables.c.value.type, "undefined");
+
+  await resume(threadClient);
+  await close(client);
+  do_test_finished();
+}
