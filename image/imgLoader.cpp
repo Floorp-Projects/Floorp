b6550f11591959b675bc9d1022dfe71afab1cedd
Andrew Osmond <aosmond@mozilla.com>
2018-11-01 09:00:51 -0400

    Bug 1503850 - Remove expired WebP telemetry probes. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D10550

--- image/imgLoader.cpp
+++ image/imgLoader.cpp
@@ -17,7 +17,6 @@
 #include "mozilla/Preferences.h"
 #include "mozilla/ChaosMode.h"
 #include "mozilla/LoadInfo.h"
-#include "mozilla/Telemetry.h"
 
 #include "nsImageModule.h"
 #include "imgRequestProxy.h"
@@ -1414,9 +1413,6 @@ void imgLoader::GlobalInit()
   RegisterStrongAsyncMemoryReporter(sMemReporter);
   RegisterImagesContentUsedUncompressedDistinguishedAmount(
     imgMemoryReporter::ImagesContentUsedUncompressedDistinguishedAmount);
-
-  Telemetry::ScalarSet(Telemetry::ScalarID::IMAGES_WEBP_PROBE_OBSERVED, false);
-  Telemetry::ScalarSet(Telemetry::ScalarID::IMAGES_WEBP_CONTENT_OBSERVED, false);
 }
 
 void imgLoader::ShutdownMemoryReporter()
