7b023d6dd2ab3627326d84b429f5f7c0fe6d14ef
Tooru Fujisawa <arai_a@mac.com>
2018-11-02 15:51:05 +0900

    Bug 1501928 - Part 2: Remove MUST_MATCH_TOKEN_FUNC* macros. r=Waldo

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -91,15 +91,6 @@ using BindingNameVector = Vector<BindingName, 6>;
         }                                                                                   \
     JS_END_MACRO
 
-#define MUST_MATCH_TOKEN_FUNC_MOD_OR(func, modifier, errorNumber, failureValue) \
-    MUST_MATCH_TOKEN_INTERNAL((func)(token), modifier, error(errorNumber), failureValue)
-
-#define MUST_MATCH_TOKEN_FUNC_OR(func, errorNumber, failureValue) \
-    MUST_MATCH_TOKEN_FUNC_MOD_OR(func, TokenStream::None, errorNumber, failureValue)
-
-#define MUST_MATCH_TOKEN_FUNC(func, errorNumber) \
-    MUST_MATCH_TOKEN_FUNC_OR(func, errorNumber, null())
-
 #define MUST_MATCH_TOKEN_MOD_WITH_REPORT_OR(tt, modifier, errorReport, failureValue) \
     MUST_MATCH_TOKEN_INTERNAL(token == tt, modifier, errorReport, failureValue)
 
@@ -153,15 +144,16 @@ GeneralParser<ParseHandler, Unit>::asFinalParser() const
 }
 
 template <class ParseHandler, typename Unit>
+template <typename ConditionT>
 bool
-GeneralParser<ParseHandler, Unit>::mustMatchToken(TokenKind expected, Modifier modifier,
-                                                  unsigned errorNumber)
+GeneralParser<ParseHandler, Unit>::mustMatchTokenInternal(ConditionT condition, Modifier modifier,
+                                                          unsigned errorNumber)
 {
     TokenKind actual;
     if (!tokenStream.getToken(&actual, modifier)) {
         return false;
     }
-    if (actual != expected) {
+    if (!condition(actual)) {
         error(errorNumber);
         return false;
     }
@@ -4897,7 +4889,9 @@ Parser<FullParseHandler, Unit>::namedImportsOrNamespaceImport(TokenKind tt,
             return false;
         }
 
-        MUST_MATCH_TOKEN_FUNC_OR(TokenKindIsPossibleIdentifierName, JSMSG_NO_BINDING_NAME, false);
+        if (!mustMatchToken(TokenKindIsPossibleIdentifierName, JSMSG_NO_BINDING_NAME)) {
+            return false;
+        }
 
         NameNodeType importName = newName(context->names().star);
         if (!importName) {
@@ -5519,7 +5513,9 @@ GeneralParser<ParseHandler, Unit>::exportClause(uint32_t begin)
             return null();
         }
         if (foundAs) {
-            MUST_MATCH_TOKEN_FUNC(TokenKindIsPossibleIdentifierName, JSMSG_NO_EXPORT_NAME);
+            if (!mustMatchToken(TokenKindIsPossibleIdentifierName, JSMSG_NO_EXPORT_NAME)) {
+                return null();
+            }
         }
 
         NameNodeType exportName = newName(anyChars.currentName());
