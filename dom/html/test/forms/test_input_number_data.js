9b0d7c5e6642df6ca16a7604ff933192b47ed76a
Jonathan Watt <jwatt@jwatt.org>
2018-11-02 10:10:42 +0000

    Bug 1509057. Disable <input type=number> support for grouping separators in user input. r=mats
    Differential Revision: https://phabricator.services.mozilla.com/D12554

--- dom/html/test/forms/test_input_number_data.js
+++ dom/html/test/forms/test_input_number_data.js
@@ -16,15 +16,17 @@ var tests = [
     langTag: "de", inputWithGrouping: "123.456,78",
     inputWithoutGrouping: "123456,78", value: 123456.78
   },
+  // Bug 1509057 disables grouping separators for now, so this test isn't
+  // currently relevant.
   // Extra german test to check that a locale that uses '.' as its grouping
   // separator doesn't result in it being invalid (due to step mismatch) due
   // to the de-localization code mishandling numbers that look like other
   // numbers formatted for English speakers (i.e. treating this as 123.456
   // instead of 123456):
-  { desc: "German (test 2)",
-    langTag: "de", inputWithGrouping: "123.456",
-    inputWithoutGrouping: "123456", value: 123456
-  },
+  //{ desc: "German (test 2)",
+  //  langTag: "de", inputWithGrouping: "123.456",
+  //  inputWithoutGrouping: "123456", value: 123456
+  //},
   { desc: "Hebrew",
     langTag: "he", inputWithGrouping: "123,456.78",
     inputWithoutGrouping: "123456.78", value: 123456.78
@@ -33,6 +35,6 @@ var tests = [
 
 var invalidTests = [
   // Right now this will pass in a 'de' build, but not in the 'en' build that
-  // are used for testing. See bug .
+  // are used for testing. See bug 1216831.
   // { desc: "Invalid German", langTag: "de", input: "12.34" }
 ];
