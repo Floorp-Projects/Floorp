6c41e00dff813398ce7b9a3cab92d35430e2e550
rjairath <rishabhjairath@gmail.com>
2018-11-02 09:04:20 +0000

    Bug 1494045 - Remove mURIFixup and use Services.uriFixup r=dao
    Differential Revision: https://phabricator.services.mozilla.com/D10015

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -25,7 +25,6 @@ window._gBrowser = {
 
     XPCOMUtils.defineLazyServiceGetters(this, {
       serializationHelper: ["@mozilla.org/network/serialization-helper;1", "nsISerializationHelper"],
-      mURIFixup: ["@mozilla.org/docshell/urifixup;1", "nsIURIFixup"],
     });
 
     Services.obs.addObserver(this, "contextual-identity-updated");
@@ -826,7 +825,7 @@ window._gBrowser = {
     // XXX https://bugzilla.mozilla.org/show_bug.cgi?id=22183#c239
     try {
       if (docElement.getAttribute("chromehidden").includes("location")) {
-        var uri = this.mURIFixup.createExposableURI(
+        var uri = Services.uriFixup.createExposableURI(
           aBrowser.currentURI);
         if (uri.scheme == "about")
           newTitle = uri.spec + sep + newTitle;
@@ -1249,7 +1248,7 @@ window._gBrowser = {
       // See if we can use the URI as the title.
       if (browser.currentURI.displaySpec) {
         try {
-          title = this.mURIFixup.createExposableURI(browser.currentURI).displaySpec;
+          title = Services.uriFixup.createExposableURI(browser.currentURI).displaySpec;
         } catch (ex) {
           title = browser.currentURI.displaySpec;
         }
