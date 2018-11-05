596dc62140a7cbb727b80222cb9f1adec9536a85
alwu <alwu@mozilla.com>
2018-11-05 22:02:20 +0000

    Bug 1498440 - part1 : implement MDSM's 'LoopingDecodingState'. r=jya
    This state is used to handle decoding when the media is in seamless looping. The advantage to create a new state is that we can totally separate looping and non-looping codes and then they won't affect each other anymore.
    
    The new state will be responsible for
    (1) time adjustment
    (2) handle EOS and seek to the first sample
    
    Differential Revision: https://phabricator.services.mozilla.com/D9186

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -190,6 +190,7 @@ public:
     DECODER_STATE_DORMANT,
     DECODER_STATE_DECODING_FIRSTFRAME,
     DECODER_STATE_DECODING,
+    DECODER_STATE_LOOPING_DECODING,
     DECODER_STATE_SEEKING,
     DECODER_STATE_BUFFERING,
     DECODER_STATE_COMPLETED,
@@ -304,6 +305,7 @@ private:
   class DormantState;
   class DecodingFirstFrameState;
   class DecodingState;
+  class LoopingDecodingState;
   class SeekingState;
   class AccurateSeekingState;
   class NextFrameSeekingState;
