7172e8936638be0f3aee1ccae44b68a016ac0a2c
byron jones <glob@mozilla.com>
2018-11-06 17:11:40 +0000

    Bug 1504932 - Replace libcubeb's README_Mozilla file with moz.yaml r=kinetik
    Differential Revision: https://phabricator.services.mozilla.com/D10995

--- media/libcubeb/update.sh
+++ media/libcubeb/update.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 # Usage: sh update.sh <upstream_src_directory>
+
 set -e
+
+[[ -n "$1" ]] || ( echo "syntax: $0 update_src_directory"; exit 1 )
+[[ -e "$1/src/cubeb.c" ]] || ( echo "$1: cubeb not found"; exit 1 )
 
 cp $1/AUTHORS .
 cp $1/LICENSE .
@@ -60,8 +65,10 @@ if [ -d $1/.git ]; then
   rev=$(cd $1 && git rev-parse --verify HEAD)
   date=$(cd $1 && git show -s --format=%ci HEAD)
   dirty=$(cd $1 && git diff-index --name-only HEAD)
-  pre_rev=$(grep -o '[[:xdigit:]]\{40\}' README_MOZILLA)
+  set +e
+  pre_rev=$(grep -o '[[:xdigit:]]\{40\}' moz.yaml)
   commits=$(cd $1 && git log --pretty=format:'%h - %s' $pre_rev..$rev)
+  set -e
 fi
 
 if [ -n "$rev" ]; then
@@ -70,11 +77,15 @@ if [ -n "$rev" ]; then
     version=$version-dirty
     echo "WARNING: updating from a dirty git repository."
   fi
-  sed -i.bak -e "/The git commit ID used was/ s/[0-9a-f]\{40\}\(-dirty\)\{0,1\} .\{1,100\}/$version ($date)/" README_MOZILLA
-  rm README_MOZILLA.bak
-  echo -e "Pick commits:\n$commits"
+  sed -i.bak -e "s/^ *release:.*/  release: \"$version ($date)\"/" moz.yaml
+  if [[ ! "$( grep "$version" moz.yaml )" ]]; then
+    echo "Updating moz.yaml failed."
+    exit 1
+  fi
+  rm moz.yaml.bak
+  [[ -n "$commits" ]] && echo -e "Pick commits:\n$commits"
 else
-  echo "Remember to update README_MOZILLA with the version details."
+  echo "Remember to update moz.yaml with the version details."
 fi
 
 echo "Applying disable-assert.patch on top of $rev"
