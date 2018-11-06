3e2a4f69ad54e06991e76b1d1fa8b5a975f9ba34
Eitan Isaacson <eitan@monotonous.org>
2018-11-06 04:35:51 +0000

    Bug 1502187 - Implement native part of viewport caching. r=Jamie a=jcristau
    Depends on D9865
    
    Differential Revision: https://phabricator.services.mozilla.com/D9866

--- accessible/android/DocAccessibleWrap.h
+++ accessible/android/DocAccessibleWrap.h
@@ -7,6 +7,7 @@
 #define mozilla_a11y_DocAccessibleWrap_h__
 
 #include "DocAccessible.h"
+#include "nsITimer.h"
 
 namespace mozilla {
 namespace a11y {
@@ -16,6 +17,8 @@ class DocAccessibleWrap : public DocAccessible
 public:
   DocAccessibleWrap(nsIDocument* aDocument, nsIPresShell* aPresShell);
   virtual ~DocAccessibleWrap();
+
+  virtual nsresult HandleAccEvent(AccEvent* aEvent) override;
 
   /**
    * Manage the mapping from id to Accessible.
@@ -27,11 +30,24 @@ public:
   void RemoveID(uint32_t aID) { mIDToAccessibleMap.Remove(aID); }
   AccessibleWrap* GetAccessibleByID(int32_t aID) const;
 
+  enum {
+    eBatch_Viewport = 0
+  };
+
 protected:
   /*
    * This provides a mapping from 32 bit id to accessible objects.
    */
   nsDataHashtable<nsUint32HashKey, AccessibleWrap*> mIDToAccessibleMap;
+
+  virtual void DoInitialUpdate() override;
+
+private:
+  void CacheViewport();
+
+  static void CacheViewportCallback(nsITimer* aTimer, void* aDocAccParam);
+
+  nsCOMPtr<nsITimer> mCacheRefreshTimer;
 };
 
 } // namespace a11y
