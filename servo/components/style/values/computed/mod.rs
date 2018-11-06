0f9cca687db1594e32e100e8307527dbfd64588f
Cameron McCormack <cam@mcc.id.au>
2018-11-06 23:03:24 +0000

    Bug 1504034 - Part 3: Store the Rust QuotePair slice as the computed quotes value in Gecko style structs r=emilio
    Depends on D10651
    
    Differential Revision: https://phabricator.services.mozilla.com/D10652

--- servo/components/style/values/computed/mod.rs
+++ servo/components/style/values/computed/mod.rs
@@ -64,7 +64,7 @@ pub use self::length::{CalcLengthOrPercentage, Length, LengthOrNumber, LengthOrP
 pub use self::length::{LengthOrPercentageOrAuto, LengthOrPercentageOrNone, MaxLength, MozLength};
 pub use self::length::{CSSPixelLength, ExtremumLength, NonNegativeLength};
 pub use self::length::{NonNegativeLengthOrPercentage, NonNegativeLengthOrPercentageOrAuto};
-pub use self::list::Quotes;
+pub use self::list::{QuotePair, Quotes};
 #[cfg(feature = "gecko")]
 pub use self::list::ListStyleType;
 pub use self::motion::OffsetPath;
