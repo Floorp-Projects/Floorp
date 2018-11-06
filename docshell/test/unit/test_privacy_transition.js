a99e5d88f3da21168718519099364ec0cb3768bf
Dorel Luca <dluca@mozilla.com>
2018-11-06 22:40:36 +0200

    Backed out 2 changesets (bug 1496082) for mochitest faiures in docshell/test/navigation/test_bug270414.html
    Backed out changeset 776b54474da9 (bug 1496082)
    Backed out changeset 0852730f9ecd (bug 1496082)

--- docshell/test/unit/test_privacy_transition.js
+++ docshell/test/unit/test_privacy_transition.js
@@ -3,18 +3,24 @@ var gNotifications = 0;
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 var observer = {
-  QueryInterface: ChromeUtils.generateQI(["nsIPrivacyTransitionObserver", "nsISupportsWeakReference"]),
-
-  privateModeChanged(enabled) {
-    gNotifications++;
+  QueryInterface: function(iid) {
+    if (Ci.nsIPrivacyTransitionObserver.equals(iid) ||
+        Ci.nsISupportsWeakReference.equals(iid) ||
+        Ci.nsISupports.equals(iid))
+      return this;
+    throw Cr.NS_ERROR_NO_INTERFACE;
   },
-};
+
+  privateModeChanged: function(enabled) {
+    gNotifications++;
+  }
+}
 
 function run_test() {
   let windowlessBrowser = Services.appShell.createWindowlessBrowser(false);
   windowlessBrowser.docShell.addWeakPrivacyTransitionObserver(observer);
-  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId: 1});
-  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId: 0});
+  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId : 1});
+  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId : 0});
   windowlessBrowser.close();
   Assert.equal(gNotifications, 2);
 }
