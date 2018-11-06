5837c1aa892663495a3a988dcf089b12c1a718b2
Raphael Pierzina <rpierzina@mozilla.com>
2018-11-06 11:20:29 +0000

    Bug 1500298 - Refactor TelemetryTestCase and add logging. r=Dexter,whimboo, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D10581
    
    --HG--
    extra : source : 1b11540e60c7f6072c411797d868a27043dc2da5

--- toolkit/components/telemetry/tests/marionette/harness/telemetry_harness/testcase.py
+++ toolkit/components/telemetry/tests/marionette/harness/telemetry_harness/testcase.py
@@ -3,7 +3,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
-import re
 import simplejson as json
 import time
 import zlib
@@ -22,14 +21,48 @@ class TelemetryTestCase(PuppeteerMixin, MarionetteTestCase):
 
     def __init__(self, *args, **kwargs):
         super(TelemetryTestCase, self).__init__(*args, **kwargs)
-        self.ping_list = []
+        self.pings = []
 
     def setUp(self, *args, **kwargs):
         super(TelemetryTestCase, self).setUp(*args, **kwargs)
+
+        @httpd.handlers.handler
+        def pings_handler(request, response):
+            """Handler for HTTP requests to the ping server."""
+
+            request_data = request.body
+
+            if request.headers.get("Content-Encoding") == "gzip":
+                request_data = zlib.decompress(request_data, zlib.MAX_WBITS | 16)
+
+            ping_data = json.loads(request_data)
+
+            # Store JSON data to self.pings to be used by wait_for_pings()
+            self.pings.append(ping_data)
+
+            ping_type = ping_data["type"]
+
+            log_message = "pings_handler received '{}' ping".format(ping_type)
+
+            if ping_type == "main":
+                ping_reason = ping_data["payload"]["info"]["reason"]
+                log_message = "{} with reason '{}'".format(log_message, ping_reason)
+
+            self.logger.info(log_message)
+
+            status_code = 200
+            content = "OK"
+            headers = [
+                ("Content-Type", "text/plain"),
+                ("Content-Length", len(content)),
+            ]
+
+            return (status_code, headers, content)
+
         self.httpd = httpd.FixtureServer(self.testvars['server_root'])
-        ping_route = [("POST", re.compile('/pings'), self.pings)]
-        self.httpd.routes.extend(ping_route)
+        self.httpd.router.register("POST", '/pings*', pings_handler)
         self.httpd.start()
+
         self.ping_server_url = '{}pings'.format(self.httpd.get_url('/'))
 
         telemetry_prefs = {
@@ -50,30 +83,40 @@ class TelemetryTestCase(PuppeteerMixin, MarionetteTestCase):
         # Wait 5 seconds to ensure that telemetry has reinitialized
         time.sleep(5)
 
-    def wait_for_ping(self, action_func, ping_filter_func):
-        # Filter pings initially using `ping_filter_func`
-        self.ping_list = [p for p in self.ping_list if ping_filter_func(p)]
+    def wait_for_pings(self, action_func, ping_filter_func, count):
+        """Call the given action and wait for pings to come in and return
+        the `count` number of pings, that match the given filter.
+        """
+        # Keep track of the current number of pings
+        current_num_pings = len(self.pings)
 
-        current_num_pings = len(self.ping_list)
-
-        if callable(action_func):
-            action_func()
+        # New list to store new pings that satisfy the filter
+        filtered_pings = []
 
         def wait_func(*args, **kwargs):
-            # Filter pings based on type and reason to make sure right ping is captured.
-            self.ping_list = [p for p in self.ping_list if ping_filter_func(p)]
-            return len(self.ping_list) > current_num_pings
+            # Ignore existing pings in self.pings
+            new_pings = self.pings[current_num_pings:]
+
+            # Filter pings to make sure we wait for the correct ping type
+            filtered_pings[:] = [p for p in new_pings if ping_filter_func(p)]
+
+            return len(filtered_pings) >= count
+
+        self.logger.info(
+            "wait_for_pings running action '{action}'.".format(
+                action=action_func.__name__,
+            )
+        )
+
+        # Call given action and wait for a ping
+        action_func()
 
         try:
             Wait(self.marionette, 60).until(wait_func)
         except Exception as e:
-            self.fail('Error generating ping: {}'.format(e.message))
+            self.fail("Error waiting for ping: {}".format(e.message))
 
-        # TODO: Bug 1380748 - Pings are being cached between test
-        # runs when using --repeat flag in marionette harness
-        # Causes the assert to fail.
-        # assert len(self.ping_list) == 1
-        return self.ping_list.pop()
+        return filtered_pings[:count]
 
     def restart_browser(self):
         """Restarts browser while maintaining the same profile and session."""
@@ -112,15 +155,3 @@ class TelemetryTestCase(PuppeteerMixin, MarionetteTestCase):
     def tearDown(self, *args, **kwargs):
         self.httpd.stop()
         super(TelemetryTestCase, self).tearDown()
-
-    def pings(self, request, response):
-        json_data = json.loads(unpack(request.headers, request.body))
-        self.ping_list.append(json_data)
-        return 200
-
-
-def unpack(headers, data):
-    if "Content-Encoding" in headers and headers["Content-Encoding"] == "gzip":
-        return zlib.decompress(data, zlib.MAX_WBITS | 16)
-    else:
-        return data
