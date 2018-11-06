415ae552bb124f23c72c0df7ba26d76fe68dd15f
Toby Ward <tobyfrederickward@gmail.com>
2018-11-06 14:48:43 +0000

    Bug 1496082: Enable ESLint for docshell/test/navigation and docshell/test/unit (automatic fixes only). r=bzbarsky
    Enabled ESLint for:
    
    * docshell/test/navigation/**
    * docshell/test/unit/**
    * docshell/test/unit_ipc/**
    
    Changed .eslintignore to allow for this and ran ./mach eslint --fix on the above directories and checked automatic fixes
    
    Differential Revision: https://phabricator.services.mozilla.com/D9430
    
    --HG--
    extra : moz-landing-system : lando

--- docshell/test/unit/test_privacy_transition.js
+++ docshell/test/unit/test_privacy_transition.js
@@ -3,24 +3,18 @@ var gNotifications = 0;
 ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 var observer = {
-  QueryInterface: function(iid) {
-    if (Ci.nsIPrivacyTransitionObserver.equals(iid) ||
-        Ci.nsISupportsWeakReference.equals(iid) ||
-        Ci.nsISupports.equals(iid))
-      return this;
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  },
+  QueryInterface: ChromeUtils.generateQI(["nsIPrivacyTransitionObserver", "nsISupportsWeakReference"]),
 
-  privateModeChanged: function(enabled) {
+  privateModeChanged(enabled) {
     gNotifications++;
-  }
-}
+  },
+};
 
 function run_test() {
   let windowlessBrowser = Services.appShell.createWindowlessBrowser(false);
   windowlessBrowser.docShell.addWeakPrivacyTransitionObserver(observer);
-  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId : 1});
-  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId : 0});
+  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId: 1});
+  windowlessBrowser.docShell.setOriginAttributes({privateBrowsingId: 0});
   windowlessBrowser.close();
   Assert.equal(gNotifications, 2);
 }
