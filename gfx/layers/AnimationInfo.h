b1370b2928137c086761be539bf69028c06867af
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-11-07 03:13:04 +0000

    Bug 1504884 - Wrap AnimationInfo::mAnimationGeneration into Maybe<> and returns Nothing() if there is no animation. r=birtles
    So that we don't generate anymore unnecessary change hints in
    RestyleManager::AddLayerChangesForAnimation for the layer has no corresponding
    animations.
    
    Depends on D11105
    
    Differential Revision: https://phabricator.services.mozilla.com/D11106

--- gfx/layers/AnimationInfo.h
+++ gfx/layers/AnimationInfo.h
@@ -44,8 +44,11 @@ public:
   // SetBaseTransformForNextTransaction.)
   Animation* AddAnimationForNextTransaction();
 
-  void SetAnimationGeneration(uint64_t aCount) { mAnimationGeneration = aCount; }
-  uint64_t GetAnimationGeneration() { return mAnimationGeneration; }
+  void SetAnimationGeneration(uint64_t aCount)
+  {
+    mAnimationGeneration = Some(aCount);
+  }
+  Maybe<uint64_t> GetAnimationGeneration() { return mAnimationGeneration; }
 
   // ClearAnimations clears animations on this layer.
   void ClearAnimations();
@@ -76,7 +79,7 @@ protected:
   InfallibleTArray<AnimData> mAnimationData;
   // If this layer is used for OMTA, then this counter is used to ensure we
   // stay in sync with the animation manager
-  uint64_t mAnimationGeneration;
+  Maybe<uint64_t> mAnimationGeneration;
   RefPtr<RawServoAnimationValue> mBaseAnimationStyle;
   bool mMutated;
 };
