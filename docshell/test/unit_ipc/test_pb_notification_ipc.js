136c06312701cb17ed6bf0013d149c05f9b61e42
Toby Ward <tobyfrederickward@gmail.com>
2018-11-07 13:55:47 +0000

    Bug 1496082: Enable ESLint for docshell/test/navigation and docshell/test/unit (automatic fixes only). r=bzbarsky
    Enabled ESLint for:
    
    * docshell/test/navigation/**
    * docshell/test/unit/**
    * docshell/test/unit_ipc/**
    
    Changed .eslintignore to allow for this and ran ./mach eslint --fix on the above directories and checked automatic fixes
    
    Differential Revision: https://phabricator.services.mozilla.com/D9430

--- docshell/test/unit_ipc/test_pb_notification_ipc.js
+++ docshell/test/unit_ipc/test_pb_notification_ipc.js
@@ -1,14 +1,14 @@
 function run_test() {
   var notifications = 0;
   var obs = {
-    observe: function(aSubject, aTopic, aData) {
+    observe(aSubject, aTopic, aData) {
       Assert.equal(aTopic, "last-pb-context-exited");
       notifications++;
-    }
+    },
   };
   var os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
   os.addObserver(obs, "last-pb-context-exited");
- 
+
   run_test_in_child("../unit/test_pb_notification.js",
                     function() {
                       Assert.equal(notifications, 1);
