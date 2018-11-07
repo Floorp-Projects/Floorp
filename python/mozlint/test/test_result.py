0388a17c4487f0fb485b1e0b26a02e0d42a2a1a7
Andrew Halberstadt <ahalberstadt@mozilla.com>
2018-11-07 15:21:31 +0000

    Bug 1503599 - [mozlint] Default lineno to 0 in result.Issue if unset, r=jgraham
    This assumes that a lineno of 0 denotes a "file-level" issue, e.g an issue
    associated with the filename. In the future it might be better to treat these
    "file-level" issues as first class citizens. This would involve updating things
    like formatters, editor integrations and review bot to not assume a lineno.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10383

---
+++ python/mozlint/test/test_result.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+import mozunit
+
+from mozlint.result import Issue
+
+
+def test_issue_defaults():
+    issue = Issue('linter', 'path', 'message', None)
+    assert issue.lineno == 0
+    assert issue.column is None
+    assert issue.level == 'error'
+
+    issue = Issue('linter', 'path', 'message', '1', column='2')
+    assert issue.lineno == 1
+    assert issue.column == 2
+
+
+if __name__ == '__main__':
+    mozunit.main()
