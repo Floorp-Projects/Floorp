1610592c49f7f77120ab6ae1ce8bb70595edb78a
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-11-07 17:46:05 +0000

    Bug 1497448: Rename closeAllListener to closeAllSocketListeners. r=ochameau,jdescottes
    Depends on D10868
    
    Differential Revision: https://phabricator.services.mozilla.com/D11150
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/shared/transport/tests/unit/test_dbgsocket.js
+++ devtools/shared/transport/tests/unit/test_dbgsocket.js
@@ -77,10 +77,10 @@ async function test_socket_shutdown() {
   Assert.equal(DebuggerServer.listeningSockets, 2);
   gExtraListener.close();
   Assert.equal(DebuggerServer.listeningSockets, 1);
-  Assert.ok(DebuggerServer.closeAllListeners());
+  Assert.ok(DebuggerServer.closeAllSocketListeners());
   Assert.equal(DebuggerServer.listeningSockets, 0);
   // Make sure closing the listener twice does nothing.
-  Assert.ok(!DebuggerServer.closeAllListeners());
+  Assert.ok(!DebuggerServer.closeAllSocketListeners());
   Assert.equal(DebuggerServer.listeningSockets, 0);
 
   info("Connecting to a server socket at " + new Date().toTimeString());
