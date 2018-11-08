52296004c44c02fa8dae34420c0754cdeb739287
Botond Ballo <botond@mozilla.com>
2018-11-08 22:24:48 +0000

    Bug 1496864 - Move getResolution() into apz_test_native_utils.js. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D11229
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
@@ -1,5 +1,11 @@
 // Utilities for synthesizing of native events.
 
+function getResolution() {
+  let resolution = { value: -1 }; // bogus value in case DWU fails us
+  SpecialPowers.getDOMWindowUtils(window).getResolution(resolution);
+  return resolution.value;
+}
+
 function getPlatform() {
   if (navigator.platform.indexOf("Win") == 0) {
     return "windows";
