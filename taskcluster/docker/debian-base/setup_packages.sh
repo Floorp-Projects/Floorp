d26ef2b1a581caf53a32128cc52d545e257e3323
Mike Hommey <mh+mozilla@glandium.org>
2018-11-08 17:14:16 +0900

    Backed out 6 changesets (bug 1504906 and bug 1505652) to give time to toolchains and docker images to build without blocking other landings.
    Backed out changeset 2fe1e2b7d9c6 (bug 1504906)
    Backed out changeset 27b4002951a4 (bug 1504906)
    Backed out changeset f7a685b16579 (bug 1504906)
    Backed out changeset f8064dbb8009 (bug 1504906)
    Backed out changeset f899fbb4a5d7 (bug 1504906)
    Backed out changeset 3f71db4aef73 (bug 1505652)

--- taskcluster/docker/debian-base/setup_packages.sh
+++ taskcluster/docker/debian-base/setup_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for task in "$@"; do
-  echo "deb [trusted=yes] https://queue.taskcluster.net/v1/task/$task/artifacts/public/build/ debian/" > "/etc/apt/sources.list.d/99$task.list"
+  echo "deb [trusted=yes] https://queue.taskcluster.net/v1/task/$task/runs/0/artifacts/public/build/ debian/" > "/etc/apt/sources.list.d/99$task.list"
 done
