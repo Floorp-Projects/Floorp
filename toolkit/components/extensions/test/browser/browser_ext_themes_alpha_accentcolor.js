08d7abd8e4bc7580c25c7cffdc8e0e72245354f4
Jared Wein <jwein@mozilla.com>
2018-11-08 22:35:51 +0000

    Bug 1505328 - Remove the theme transition from the title bar. r=dao
    This patch solves half of the problem but it will still show the transition when the window regains focus. I am OK with removing the transition on the title bar, we will still have it on the navbar and bookmarks toolbar.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11282

--- toolkit/components/extensions/test/browser/browser_ext_themes_alpha_accentcolor.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_alpha_accentcolor.js
@@ -18,15 +18,10 @@ add_task(async function test_alpha_accentcolor() {
     },
   });
 
-  // Add the event listener before loading the extension
-  let docEl = window.document.documentElement;
-  let transitionPromise = waitForTransition(docEl, "background-color");
-
   await extension.startup();
 
-  // wait for theme transition to end
-  await transitionPromise;
-
+  // Add the event listener before loading the extension
+  let docEl = window.document.documentElement;
   let style = window.getComputedStyle(docEl);
 
   Assert.equal(style.backgroundColor, "rgb(230, 128, 0)",
