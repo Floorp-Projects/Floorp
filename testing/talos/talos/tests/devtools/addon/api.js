6f7b36c5e5451cb4b8105e6591cd8bd10f680655
Andrew Swan <aswan@mozilla.com>
2018-11-07 18:52:06 -0800

    Bug 1500198 Convert talos damp addon to a webextension r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D9337

---
+++ testing/talos/talos/tests/devtools/addon/api.js
@@ -0,0 +1,25 @@
+"use strict";
+
+ChromeUtils.defineModuleGetter(this, "Services",
+                               "resource://gre/modules/Services.jsm");
+
+/* globals ExtensionAPI */
+this.damp = class extends ExtensionAPI {
+  getAPI(context) {
+    return {
+      damp: {
+        startTest() {
+          let {rootURI} = context.extension;
+          let window = context.xulWindow;
+          if (!("Damp" in window)) {
+            let script = rootURI.resolve("content/damp.js");
+            Services.scriptloader.loadSubScript(script, window);
+          }
+
+          let damp = new window.Damp();
+          return damp.startTest(rootURI);
+        },
+      },
+    };
+  }
+};
