a929c4971c65d1082d012fd8d1b8dba68edfa233
Marco Bonardo <mbonardo@mozilla.com>
2018-11-08 09:59:27 +0000

    Bug 1502392 - Remove support for browser.urlbar.autocomplete.enabled. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D11245
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/places/tests/unifiedcomplete/test_autocomplete_functional.js
+++ toolkit/components/places/tests/unifiedcomplete/test_autocomplete_functional.js
@@ -4,22 +4,6 @@
 
 // Functional tests for inline autocomplete
 
-const PREF_AUTOCOMPLETE_ENABLED = "browser.urlbar.autocomplete.enabled";
-
-add_task(async function test_disabling_autocomplete() {
-  info("Check disabling autocomplete disables autofill");
-  Services.prefs.setBoolPref(PREF_AUTOCOMPLETE_ENABLED, false);
-  await PlacesTestUtils.addVisits({
-    uri: NetUtil.newURI("http://visit.mozilla.org"),
-  });
-  await check_autocomplete({
-    search: "vis",
-    autofilled: "vis",
-    completed: "vis",
-  });
-  await cleanup();
-});
-
 add_task(async function test_urls_order() {
   info("Add urls, check for correct order");
   let places = [{ uri: NetUtil.newURI("http://visit1.mozilla.org") },
@@ -65,24 +49,4 @@ add_task(async function test_complete_fragment() {
     completed: "http://smokey.mozilla.org/foo?bacon=delicious#bar",
   });
   await cleanup();
-});
-
-add_task(async function test_autocomplete_enabled_pref() {
-  Services.prefs.setBoolPref(PREF_AUTOCOMPLETE_ENABLED, false);
-  let types = ["history", "bookmark", "openpage"];
-  for (let type of types) {
-    Assert.equal(Services.prefs.getBoolPref("browser.urlbar.suggest." + type), false,
-                 "suggest." + type + "pref should be false");
-  }
-  Services.prefs.setBoolPref(PREF_AUTOCOMPLETE_ENABLED, true);
-  for (let type of types) {
-    Assert.equal(Services.prefs.getBoolPref("browser.urlbar.suggest." + type), true,
-                 "suggest." + type + "pref should be true");
-  }
-
-  // Clear prefs.
-  Services.prefs.clearUserPref(PREF_AUTOCOMPLETE_ENABLED);
-  for (let type of types) {
-    Services.prefs.clearUserPref("browser.urlbar.suggest." + type);
-  }
 });
