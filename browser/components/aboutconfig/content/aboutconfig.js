a2d6c5543d77047efef7a3dd63ad349e969dc879
Luke Schwalfenberg <lschwalfenberg@gmail.com>
2018-11-08 13:31:28 +0000

    Bug 1500162 - Display the default values of "localized" preferences. r=paolo
    Differential Revision: https://phabricator.services.mozilla.com/D10326

--- browser/components/aboutconfig/content/aboutconfig.js
+++ browser/components/aboutconfig/content/aboutconfig.js
@@ -8,11 +8,24 @@ ChromeUtils.import("resource://gre/modules/Preferences.jsm");
 let gPrefArray;
 
 function onLoad() {
-  gPrefArray = Services.prefs.getChildList("").map(name => ({
-    name,
-    value: Preferences.get(name),
-    hasUserValue: Services.prefs.prefHasUserValue(name),
-  }));
+  gPrefArray = Services.prefs.getChildList("").map(function(name) {
+    let pref = {
+      name,
+      value: Preferences.get(name),
+      hasUserValue: Services.prefs.prefHasUserValue(name),
+    };
+    // Try in case it's a localized string.
+    // Throws an exception if there is no equivalent value in the localized files for the pref.
+    // If an execption is thrown the pref value is set to the empty string.
+    try {
+      if (!pref.hasUserValue && /^chrome:\/\/.+\/locale\/.+\.properties/.test(pref.value)) {
+        pref.value = Services.prefs.getComplexValue(name, Ci.nsIPrefLocalizedString).data;
+      }
+    } catch (ex) {
+      pref.value = "";
+    }
+    return pref;
+  });
 
   gPrefArray.sort((a, b) => a.name > b.name);
 
