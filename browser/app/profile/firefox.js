3886d757d7cb0c381ee0247c0c6928ff983fec7e
Haik Aftandilian <haftandilian@mozilla.com>
2018-11-09 19:55:07 +0000

    Bug 1501126 - [Mac] Enable sandbox early startup by default on Nightly r=Alex_Gaynor
    Set security.sandbox.content.mac.earlyinit=true to enable sandbox early startup by default on Nightly only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11218
    
    --HG--
    extra : moz-landing-system : lando

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1035,9 +1035,9 @@ pref("security.sandbox.gmp.win32k-disable", false);
 #endif
 
 #if defined(NIGHTLY_BUILD) && defined(XP_MACOSX) && defined(MOZ_SANDBOX)
-// Start the Mac sandbox immediately during child process startup instead
+// Start the Mac sandbox early during child process startup instead
 // of when messaged by the parent after the message loop is running.
-pref("security.sandbox.content.mac.earlyinit", false);
+pref("security.sandbox.content.mac.earlyinit", true);
 #endif
 
 #if defined(XP_MACOSX) && defined(MOZ_SANDBOX) && defined(MOZ_CONTENT_SANDBOX)
