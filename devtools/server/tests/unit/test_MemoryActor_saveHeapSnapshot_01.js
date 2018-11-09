26ebb91e5e027aca207155ffccab31c4719a2563
Alexandre Poirot <poirot.alex@gmail.com>
2018-11-09 09:38:50 +0000

    Bug 1505175 - Cleanup head_dbg.js from unecessary usages of rootForm. r=yulia
    MozReview-Commit-ID: 2ZfghtMuAH2
    
    Depends on D11153
    
    Differential Revision: https://phabricator.services.mozilla.com/D11154

--- devtools/server/tests/unit/test_MemoryActor_saveHeapSnapshot_01.js
+++ devtools/server/tests/unit/test_MemoryActor_saveHeapSnapshot_01.js
@@ -8,11 +8,16 @@
 
 const { OS } = require("resource://gre/modules/osfile.jsm");
 
-const run_test = makeMemoryActorTest(async function(client, memoryFront) {
+add_task(async () => {
+  const { client, memoryFront } = await createTabMemoryFront();
+
   const snapshotFilePath = await memoryFront.saveHeapSnapshot();
   ok(!!(await OS.File.stat(snapshotFilePath)),
      "Should have the heap snapshot file");
   const snapshot = ChromeUtils.readHeapSnapshot(snapshotFilePath);
   ok(snapshot instanceof HeapSnapshot,
      "And we should be able to read a HeapSnapshot instance from the file");
+
+  await memoryFront.detach();
+  await finishClient(client);
 });
