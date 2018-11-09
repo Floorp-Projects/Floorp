a78affb771b07a0a3e72e63ecb535823bb020f72
Michael Froman <mfroman@mozilla.com>
2018-11-09 00:12:46 +0000

    Bug 1471535 - pt7 - Add remote decoding in dom/media/ipc alongside gpu decoding. r=jya,mattwoodrow,jld
    Depends on D8488
    
    Differential Revision: https://phabricator.services.mozilla.com/D8489

---
+++ dom/media/ipc/RemoteDecoderManagerChild.h
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=99: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef include_dom_media_ipc_RemoteDecoderManagerChild_h
+#define include_dom_media_ipc_RemoteDecoderManagerChild_h
+#include "mozilla/PRemoteDecoderManagerChild.h"
+
+namespace mozilla {
+
+class RemoteDecoderManagerChild final : public PRemoteDecoderManagerChild
+{
+public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(RemoteDecoderManagerChild)
+
+  // Can only be called from the manager thread
+  static RemoteDecoderManagerChild* GetSingleton();
+
+  // Can be called from any thread.
+  static nsIThread* GetManagerThread();
+  static AbstractThread* GetManagerAbstractThread();
+
+  // Main thread only
+  static void InitForContent(
+                  Endpoint<PRemoteDecoderManagerChild>&& aVideoManager);
+  static void Shutdown();
+
+  bool CanSend();
+
+protected:
+  void InitIPDL();
+
+  void ActorDestroy(ActorDestroyReason aWhy) override;
+  void DeallocPRemoteDecoderManagerChild() override;
+
+  PRemoteVideoDecoderChild* AllocPRemoteVideoDecoderChild(
+                                const VideoInfo& aVideoInfo,
+                                const float& aFramerate,
+                                const CreateDecoderParams::OptionSet& aOptions,
+                                bool* aSuccess,
+                                nsCString* aErrorDescription) override;
+  bool DeallocPRemoteVideoDecoderChild(
+           PRemoteVideoDecoderChild* actor) override;
+
+private:
+  // Main thread only
+  static void InitializeThread();
+
+  RemoteDecoderManagerChild() = default;
+  ~RemoteDecoderManagerChild() = default;
+
+  static void Open(Endpoint<PRemoteDecoderManagerChild>&& aEndpoint);
+
+  RefPtr<RemoteDecoderManagerChild> mIPDLSelfRef;
+
+  // Should only ever be accessed on the manager thread.
+  bool mCanSend = false;
+};
+
+} // namespace mozilla
+
+#endif // include_dom_media_ipc_RemoteDecoderManagerChild_h
---
+++ dom/media/ipc/RemoteDecoderModule.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=99: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef include_dom_media_ipc_RemoteDecoderModule_h
+#define include_dom_media_ipc_RemoteDecoderModule_h
+#include "PlatformDecoderModule.h"
+
+namespace mozilla {
+
+// A PDM implementation that creates a RemoteMediaDataDecoder (a
+// MediaDataDecoder) that proxies to a RemoteVideoDecoderChild.
+// A decoder child will talk to its respective decoder parent
+// (RemoteVideoDecoderParent) on the RDD process.
+class RemoteDecoderModule : public PlatformDecoderModule
+{
+public:
+  RemoteDecoderModule() = default;
+
+  nsresult Startup() override;
+
+  bool SupportsMimeType(const nsACString& aMimeType,
+                        DecoderDoctorDiagnostics* aDiagnostics) const override;
+
+  already_AddRefed<MediaDataDecoder> CreateVideoDecoder(
+    const CreateDecoderParams& aParams) override;
+
+  already_AddRefed<MediaDataDecoder> CreateAudioDecoder(
+    const CreateDecoderParams& aParams) override
+  {
+    return nullptr;
+  }
+};
+
+} // namespace mozilla
+
+#endif // include_dom_media_ipc_RemoteDecoderModule_h
