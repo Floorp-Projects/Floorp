de2efbb6bd61248810faf54b741383898d55ea85
Cosmin Sabou <csabou@mozilla.com>
2018-11-10 12:05:48 +0200

    Backed out 2 changesets (bug 1501056) for causing SM build bustages on wasm/jsapi/constructor/compile.any.js.
    Backed out changeset ab51b28ab2a6 (bug 1501056)
    Backed out changeset 2f5beb3bf504 (bug 1501056)

--- testing/web-platform/tests/wasm/jsapi/constructor/compile.any.js
+++ testing/web-platform/tests/wasm/jsapi/constructor/compile.any.js
@@ -62,11 +62,6 @@ test(() => {
 promise_test(t => {
   const buffer = new Uint8Array();
   return promise_rejects(t, new WebAssembly.CompileError(), WebAssembly.compile(buffer));
-}, "Empty buffer");
-
-test(() => {
-  const buffer = new Uint8Array(Array.from(emptyModuleBinary).concat([0, 0]));
-  assert_throws(new WebAssembly.CompileError(), () => WebAssembly.compile(buffer));
 }, "Invalid code");
 
 promise_test(() => {
