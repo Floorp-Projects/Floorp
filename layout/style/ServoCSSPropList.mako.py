1d780cecc298b6fa61358b1c8e4741208083ac15
Emilio Cobos Álvarez <emilio@crisal.io>
2018-11-11 17:57:41 +0100

    Bug 1492958 - Move user-select outside of mako. r=xidorn
    This is the first step to unprefix user-select.
    
    This has no behavior change, it's just a nicer way to do the same thing which
    allows us to unship individual values more easily using parse(condition).
    
    Differential Revision: https://phabricator.services.mozilla.com/D11580

--- layout/style/ServoCSSPropList.mako.py
+++ layout/style/ServoCSSPropList.mako.py
@@ -110,6 +110,7 @@ SERIALIZED_PREDEFINED_TYPES = [
     "Translate",
     "TimingFunction",
     "TransformStyle",
+    "UserSelect",
     "background::BackgroundSize",
     "basic_shape::ClippingShape",
     "basic_shape::FloatAreaShape",
