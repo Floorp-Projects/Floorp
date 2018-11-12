edcd1c640ef4ced9b59e2db8a0bae22b6982884b
Ciure Andrei <aciure@mozilla.com>
2018-11-12 13:55:03 +0200

    Backed out changeset 25ccc02a44ff (bug 1504751) for high frequency browser_aboutURLs.js failures CLOSED TREE

--- browser/base/content/test/static/browser_misused_characters_in_strings.js
+++ browser/base/content/test/static/browser_misused_characters_in_strings.js
@@ -153,6 +153,10 @@ let gWhitelist = [{
     key: "tos",
     type: "double-quote",
   }, {
+    file: "aboutNetworking.dtd",
+    key: "aboutNetworking.logTutorial",
+    type: "single-quote",
+  }, {
     file: "browser.dtd",
     key: "addonPostInstallMessage.label",
     type: "single-quote",
