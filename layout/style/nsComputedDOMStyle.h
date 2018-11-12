89eee2303f5b607ad38a0ddbc57a09dda9abea71
Boris Chiou <boris.chiou@gmail.com>
2018-11-12 19:21:22 +0000

    Bug 1505200 - Part 2: Serialize Rotate by servo. r=emilio
    So we can drop a lot of code.
    
    Depends on D11247
    
    Differential Revision: https://phabricator.services.mozilla.com/D11401
    
    --HG--
    extra : moz-landing-system : lando

--- layout/style/nsComputedDOMStyle.h
+++ layout/style/nsComputedDOMStyle.h
@@ -365,7 +365,6 @@ private:
   already_AddRefed<CSSValue> DoGetOverflowClipBoxInline();
   already_AddRefed<CSSValue> DoGetTouchAction();
   already_AddRefed<CSSValue> DoGetTransform();
-  already_AddRefed<CSSValue> DoGetRotate();
   already_AddRefed<CSSValue> DoGetTransformOrigin();
   already_AddRefed<CSSValue> DoGetPerspective();
   already_AddRefed<CSSValue> DoGetPerspectiveOrigin();
