22d2fd6ea2ea5cdc6ce0d600f2e10fdbf45a1dcc
Drew Willcoxon <adw@mozilla.com>
2018-11-12 15:45:39 +0000

    Bug 1504854 - Autofilled search shortcuts should show a single "Search with Engine" heuristic result in the popup. r=mak, a=jcristau
    All we need to do is set `_searchEngineAliasMatch` with an empty query so that we don't try to add any more results. That hits the existing case where the user types out a full @ alias and we show only the "search with engine" heuristic result.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11547

--- toolkit/components/places/UnifiedComplete.js
+++ toolkit/components/places/UnifiedComplete.js
@@ -1136,7 +1136,7 @@ Search.prototype = {
     return true;
   },
 
-  async _matchSearchEngineTokenAlias() {
+  async _matchSearchEngineTokenAliasForAutofill() {
     // We need a single "@engine" search token.
     if (this._searchTokens.length != 1) {
       return false;
@@ -1174,6 +1174,17 @@ Search.prototype = {
             icon: engine.iconURI ? engine.iconURI.spec : null,
           });
           this._result.setDefaultIndex(0);
+
+          // Set _searchEngineAliasMatch with an empty query so that we don't
+          // attempt to add any more matches.  When a token alias is autofilled,
+          // the only match should be the one we just added.
+          this._searchEngineAliasMatch = {
+            engine,
+            alias: aliasPreservingUserCase,
+            query: "",
+            isTokenAlias: true,
+          };
+
           return true;
         }
       }
@@ -1246,7 +1257,7 @@ Search.prototype = {
     }
 
     if (this.pending && shouldAutofill) {
-      let matched = await this._matchSearchEngineTokenAlias();
+      let matched = await this._matchSearchEngineTokenAliasForAutofill();
       if (matched) {
         return true;
       }
