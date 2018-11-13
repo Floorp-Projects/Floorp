d2dd5da830ff0ee0d03c82421d3882434aabde64
Gabriel Luong <gabriel.luong@gmail.com>
2018-11-13 14:02:49 -0500

    Bug 1476395 - Add mochitests for the flexbox inspector sidebar. r=pbro

---
+++ devtools/client/inspector/flexbox/test/browser_flexbox_empty_state.js
@@ -0,0 +1,23 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that a message is displayed when no flex container is selected.
+
+const TEST_URI = `
+  <div></div>
+`;
+
+add_task(async function() {
+  await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
+  const { inspector, flexboxInspector } = await openLayoutView();
+  const { document: doc } = flexboxInspector;
+  const { highlighters } = inspector;
+
+  info("Checking the initial state of the Flexbox Inspector.");
+  ok(doc.querySelector(".flexbox-pane .devtools-sidepanel-no-result",
+    "A message is displayede when no flex container is selected."));
+  ok(!highlighters.flexboxHighlighterShown,
+    "No flexbox highlighter exists in the highlighters overlay.");
+});
