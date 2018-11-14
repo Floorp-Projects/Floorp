9351d666e7292bc78c337fd7c19cb8581ea7bf15
Andreea Pavel <apavel@mozilla.com>
2018-11-14 23:04:59 +0200

    Backed out changeset 2bc6db0a2574 (bug 1486524) android raptor failures ona  CLOSED TREE

--- python/mozbuild/mozbuild/action/package_fennec_apk.py
+++ python/mozbuild/mozbuild/action/package_fennec_apk.py
@@ -84,7 +84,16 @@ def package_fennec_apk(inputs=[], omni_ja=None,
     for assets_dir in assets_dirs:
         finder = FileFinder(assets_dir)
         for p, f in finder.find('**'):
-            add(mozpath.join('assets', p), f)
+            compress = None  # Take default from Jarrer.
+            if p.endswith('.so'):
+                # Asset libraries are special.
+                if f.open().read(5)[1:] == '7zXZ':
+                    print('%s is already compressed' % p)
+                    # We need to store (rather than deflate) compressed libraries
+                    # (even if we don't compress them ourselves).
+                    compress = False
+
+            add(mozpath.join('assets', p), f, compress=compress)
 
     for lib_dir in lib_dirs:
         finder = FileFinder(lib_dir)
