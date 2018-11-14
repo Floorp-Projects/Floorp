eb6196f361b2ad2fde57c5af71fa711411499589
Andrea Marchesini <amarchesini@mozilla.com>
2018-11-14 15:12:31 +0200

    Bug 1498510 - Move nsICSPEventListener out of CSP object, r=ckerschb a=pascalc

--- caps/nsScriptSecurityManager.cpp
+++ caps/nsScriptSecurityManager.cpp
@@ -63,6 +63,8 @@
 #include "mozilla/dom/ScriptSettings.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/StaticPtr.h"
+#include "mozilla/dom/WorkerCommon.h"
+#include "mozilla/dom/WorkerPrivate.h"
 #include "nsContentUtils.h"
 #include "nsJSUtils.h"
 #include "nsILoadInfo.h"
@@ -482,6 +484,15 @@ nsScriptSecurityManager::ContentSecurityPolicyPermitsJSAction(JSContext *cx,
     if (!csp)
         return true;
 
+    nsCOMPtr<nsICSPEventListener> cspEventListener;
+    if (!NS_IsMainThread()) {
+      WorkerPrivate* workerPrivate =
+        mozilla::dom::GetWorkerPrivateFromContext(cx);
+      if (workerPrivate) {
+        cspEventListener = workerPrivate->CSPEventListener();
+      }
+    }
+
     bool evalOK = true;
     bool reportViolation = false;
     rv = csp->GetAllowsEval(&reportViolation, &evalOK);
@@ -519,6 +530,7 @@ nsScriptSecurityManager::ContentSecurityPolicyPermitsJSAction(JSContext *cx,
         }
         csp->LogViolationDetails(nsIContentSecurityPolicy::VIOLATION_TYPE_EVAL,
                                  nullptr, // triggering element
+                                 cspEventListener,
                                  fileName,
                                  scriptSample,
                                  lineNum,
