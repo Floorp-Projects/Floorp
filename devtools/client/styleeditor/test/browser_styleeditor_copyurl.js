0d83521621458ae5b5a5b9a8de175ea3f55c12a0
Micah Tigley <mtigley@mozilla.com>
2018-11-14 01:55:02 +0000

    Bug 1114936 - Add a context menu item to copy a stylesheet's URL in the Style Editor. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D11419

---
+++ devtools/client/styleeditor/test/browser_styleeditor_copyurl.js
@@ -0,0 +1,32 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test to check the 'Copy URL' functionality in the context menu item for stylesheets.
+
+const TESTCASE_URI = TEST_BASE_HTTPS + "simple.html";
+
+add_task(async function() {
+  const { ui } = await openStyleEditorForURL(TESTCASE_URI);
+
+  const onContextMenuShown = new Promise((resolve) => {
+    ui._contextMenu.addEventListener("popupshown", () => {
+      resolve();
+    }, {once: true});
+  });
+
+  info("Right-click the first stylesheet editor.");
+  const editor = ui.editors[0];
+  const stylesheetEl = editor.summary.querySelector(".stylesheet-name");
+  await EventUtils.synthesizeMouseAtCenter(stylesheetEl, {button: 2, type: "contextmenu"},
+                                           ui._window);
+  await onContextMenuShown;
+
+  is(ui._copyUrlItem.getAttribute("hidden"), "false", "Copy URL menu item is showing.");
+
+  info("Click on Copy URL menu item and wait for the URL to be copied to the clipboard.");
+  await waitForClipboardPromise(() => ui._copyUrlItem.click(),
+                                ui._contextMenuStyleSheet.href);
+});
