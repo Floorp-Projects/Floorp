020b2bdbe9037de871bae44103c261f7e0bad048
Dorel Luca <dluca@mozilla.com>
2018-11-14 19:00:29 +0200

    Backed out 4 changesets (bug 1496503) for Valgrind bustage. CLOSED TREE
    Backed out changeset 033a89b3e00d (bug 1496503)
    Backed out changeset a0f255b660ce (bug 1496503)
    Backed out changeset 963d8ac1cfee (bug 1496503)
    Backed out changeset 43e44f8439ec (bug 1496503)

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -120,7 +120,9 @@ using mozilla::ipc::CrashReporterClient;
 
 // From toolkit/library/rust/shared/lib.rs
 extern "C" {
+  void install_rust_panic_hook();
   void install_rust_oom_hook();
+  bool get_rust_panic_reason(char** reason, size_t* length);
 }
 
 
@@ -906,8 +908,12 @@ WriteEscapedMozCrashReason(PlatformWriter& aWriter)
 {
   const char *reason;
   size_t len;
+  char *rust_panic_reason;
+  bool rust_panic = get_rust_panic_reason(&rust_panic_reason, &len);
 
-  if (gMozCrashReason != nullptr) {
+  if (rust_panic) {
+    reason = rust_panic_reason;
+  } else if (gMozCrashReason != nullptr) {
     reason = gMozCrashReason;
     len = strlen(reason);
   } else {
@@ -1736,6 +1742,7 @@ nsresult SetExceptionHandler(nsIFile* aXREDirectory,
 
   oldTerminateHandler = std::set_terminate(&TerminateHandler);
 
+  install_rust_panic_hook();
   install_rust_oom_hook();
 
   InitThreadAnnotation();
@@ -3600,6 +3607,8 @@ SetRemoteExceptionHandler(const nsACString& crashPipe,
 
   oldTerminateHandler = std::set_terminate(&TerminateHandler);
 
+  install_rust_panic_hook();
+
   // we either do remote or nothing, no fallback to regular crash reporting
   return gExceptionHandler->IsOutOfProcess();
 }
@@ -3646,6 +3655,8 @@ SetRemoteExceptionHandler()
 
   oldTerminateHandler = std::set_terminate(&TerminateHandler);
 
+  install_rust_panic_hook();
+
   // we either do remote or nothing, no fallback to regular crash reporting
   return gExceptionHandler->IsOutOfProcess();
 }
@@ -3673,6 +3684,8 @@ SetRemoteExceptionHandler(const nsACString& crashPipe)
   mozalloc_set_oom_abort_handler(AnnotateOOMAllocationSize);
 
   oldTerminateHandler = std::set_terminate(&TerminateHandler);
+
+  install_rust_panic_hook();
 
   // we either do remote or nothing, no fallback to regular crash reporting
   return gExceptionHandler->IsOutOfProcess();
