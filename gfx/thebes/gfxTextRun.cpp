3e8830ea67c43155a05e826d30f2577de3bea168
Jeff Muizelaar <jrmuizel@gmail.com>
2018-11-13 19:17:03 -0500

    Bug 1507021. Add a SnappedClip function to gfxContext. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D11839

--- gfx/thebes/gfxTextRun.cpp
+++ gfx/thebes/gfxTextRun.cpp
@@ -561,12 +561,10 @@ struct MOZ_STACK_CLASS BufferAlphaColor {
     void PushSolidColor(const gfxRect& aBounds, const Color& aAlphaColor, uint32_t appsPerDevUnit)
     {
         mContext->Save();
-        mContext->NewPath();
-        mContext->SnappedRectangle(gfxRect(aBounds.X() / appsPerDevUnit,
+        mContext->SnappedClip(gfxRect(aBounds.X() / appsPerDevUnit,
                     aBounds.Y() / appsPerDevUnit,
                     aBounds.Width() / appsPerDevUnit,
                     aBounds.Height() / appsPerDevUnit));
-        mContext->Clip();
         mContext->SetColor(Color(aAlphaColor.r, aAlphaColor.g, aAlphaColor.b));
         mContext->PushGroupForBlendBack(gfxContentType::COLOR_ALPHA, aAlphaColor.a);
     }
