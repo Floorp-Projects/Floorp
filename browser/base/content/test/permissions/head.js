29583a15f04ba00918badd4c22d804a92fa77be2
alwu <alwu@mozilla.com>
2018-11-14 01:36:26 +0000

    Bug 1493766 - part4 : add test. r=johannh
    Add a test to ensure the temporary autoplay permission is sync correctly between JS and C++ side.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11711

--- browser/base/content/test/permissions/head.js
+++ browser/base/content/test/permissions/head.js
@@ -1,1 +1,8 @@
 ChromeUtils.import("resource:///modules/SitePermissions.jsm", this);
+
+async function openTabContextMenu() {
+  let contextMenu = document.getElementById("tabContextMenu");
+  let popupShownPromise = BrowserTestUtils.waitForEvent(contextMenu, "popupshown");
+  EventUtils.synthesizeMouseAtCenter(gBrowser.selectedTab, {type: "contextmenu", button: 2});
+  await popupShownPromise;
+}
