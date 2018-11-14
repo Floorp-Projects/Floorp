46b22a84c3e1e1b38d8453433edfbc944ab5e2a0
Jack Smith <jack@jackdeansmith.com>
2018-11-14 23:09:50 +0000

    Bug 1491676 - Moves out of space warning strings r=flod,Gijs,zbraniecki
    Depends on D8553
    
    Differential Revision: https://phabricator.services.mozilla.com/D8554
    
    --HG--
    extra : moz-landing-system : lando

--- python/l10n/fluent_migrations/bug_1491676_preferences_properties.py
+++ python/l10n/fluent_migrations/bug_1491676_preferences_properties.py
@@ -17,7 +17,7 @@ def migrate(ctx):
         "toolkit/toolkit/preferences/preferences.ftl",
         "toolkit/toolkit/preferences/preferences.ftl",
         transforms_from(
-"""
+            """
 password-not-set =
         .value = { COPY(from_path, "password_not_set") }
 failed-pw-change = { COPY(from_path, "failed_pw_change") }
@@ -32,3 +32,146 @@ pw-change-failed-title = { COPY(from_path, "pw_change_failed_title") }
 pw-remove-button =
     .label = { COPY(from_path, "pw_remove_button") }
 """, from_path="toolkit/chrome/mozapps/preferences/preferences.properties"))
+
+    ctx.add_transforms(
+        "browser/browser/preferences/preferences.ftl",
+        "browser/browser/preferences/preferences.ftl",
+        transforms_from(
+            """
+space-alert-learn-more-button =
+        .label = { COPY(from_path, "spaceAlert.learnMoreButton.label")}
+        .accesskey = { COPY(from_path, "spaceAlert.learnMoreButton.accesskey")}
+space-alert-under-5gb-ok-button =
+        .label = { COPY(from_path, "spaceAlert.under5GB.okButton.label")}
+        .accesskey = { COPY(from_path, "spaceAlert.under5GB.okButton.accesskey")}
+""", from_path="browser/chrome/browser/preferences/preferences.properties"))
+
+    ctx.add_transforms(
+        "browser/browser/preferences/preferences.ftl",
+        "browser/browser/preferences/preferences.ftl",
+        [
+            FTL.Message(
+                id=FTL.Identifier("space-alert-under-5gb-message"),
+                value=REPLACE(
+                    "browser/chrome/browser/preferences/preferences.properties",
+                    "spaceAlert.under5GB.message",
+                    {
+                        "%S": TERM_REFERENCE(
+                            "-brand-short-name"
+                        ),
+                    }
+                )
+            ),
+            FTL.Message(
+                id=FTL.Identifier("space-alert-over-5gb-pref-button"),
+                attributes=[
+                    FTL.Attribute(
+                        id=FTL.Identifier("label"),
+                        value=FTL.Pattern(
+                            elements=[
+                                FTL.Placeable(
+                                    expression=FTL.SelectExpression(
+                                        selector=FTL.CallExpression(
+                                            callee=FTL.Function("PLATFORM")
+                                        ),
+                                        variants=[
+                                            FTL.Variant(
+                                                key=FTL.VariantName("windows"),
+                                                default=False,
+                                                value=COPY(
+                                                    "browser/chrome/browser/preferences/preferences.properties",
+                                                    "spaceAlert.over5GB.prefButtonWin.label"
+                                                )
+                                            ),
+                                            FTL.Variant(
+                                                key=FTL.VariantName("other"),
+                                                default=True,
+                                                value=COPY(
+                                                    "browser/chrome/browser/preferences/preferences.properties",
+                                                    "spaceAlert.over5GB.prefButton.label"
+                                                )
+                                            ),
+                                        ]
+                                    )
+                                )
+                            ]
+                        )
+                    ),
+                    FTL.Attribute(
+                        id=FTL.Identifier("accesskey"),
+                        value=FTL.Pattern(
+                            elements=[
+                                FTL.Placeable(
+                                    expression=FTL.SelectExpression(
+                                        selector=FTL.CallExpression(
+                                            callee=FTL.Function("PLATFORM")
+                                        ),
+                                        variants=[
+                                            FTL.Variant(
+                                                key=FTL.VariantName("windows"),
+                                                default=False,
+                                                value=COPY(
+                                                    "browser/chrome/browser/preferences/preferences.properties",
+                                                    "spaceAlert.over5GB.prefButtonWin.accesskey"
+                                                )
+                                            ),
+                                            FTL.Variant(
+                                                key=FTL.VariantName("other"),
+                                                default=True,
+                                                value=COPY(
+                                                    "browser/chrome/browser/preferences/preferences.properties",
+                                                    "spaceAlert.over5GB.prefButton.accesskey"
+                                                )
+                                            ),
+                                        ]
+                                    )
+                                )
+                            ]
+                        )
+                    ),
+                ]
+            ),
+            FTL.Message(
+                id=FTL.Identifier("space-alert-over-5gb-message"),
+                value=FTL.Pattern(
+                    elements=[
+                        FTL.Placeable(
+                            expression=FTL.SelectExpression(
+                                selector=FTL.CallExpression(
+                                    callee=FTL.Function("PLATFORM")
+                                ),
+                                variants=[
+                                    FTL.Variant(
+                                        key=FTL.VariantName("windows"),
+                                        default=False,
+                                        value=REPLACE(
+                                            "browser/chrome/browser/preferences/preferences.properties",
+                                            "spaceAlert.over5GB.messageWin1",
+                                            {
+                                                "%S": TERM_REFERENCE(
+                                                    "-brand-short-name"
+                                                ),
+                                            }
+                                        )
+                                    ),
+                                    FTL.Variant(
+                                        key=FTL.VariantName("other"),
+                                        default=True,
+                                        value=REPLACE(
+                                            "browser/chrome/browser/preferences/preferences.properties",
+                                            "spaceAlert.over5GB.message1",
+                                            {
+                                                "%S": TERM_REFERENCE(
+                                                    "-brand-short-name"
+                                                ),
+                                            }
+                                        )
+                                    ),
+                                ]
+                            )
+                        )
+                    ]
+                )
+            ),
+        ],
+    )
