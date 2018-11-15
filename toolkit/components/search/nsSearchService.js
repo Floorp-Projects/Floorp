e53e3f6faad27ef7689d7ef9747f970e0a085e71
Mark Banner <standard8@mozilla.com>
2018-11-15 14:45:20 +0000

    Bug 1505456 - Move recordSearchURLTelemetry to its own module. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D11188

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -189,40 +189,6 @@ const SEARCH_DEFAULT_UPDATE_INTERVAL = 7;
 // from the server doesn't specify an interval.
 const SEARCH_GEO_DEFAULT_UPDATE_INTERVAL = 2592000; // 30 days.
 
-// Used to identify various parameters (query, code, etc.) in search URLS.
-const SEARCH_PROVIDER_INFO = {
-  "google": {
-    "regexp": /^https:\/\/www\.(google)\.(?:.+)\/search/,
-    "queryParam": "q",
-    "codeParam": "client",
-    "codePrefixes": ["firefox"],
-    "followonParams": ["oq", "ved", "ei"],
-  },
-  "duckduckgo": {
-    "regexp": /^https:\/\/(duckduckgo)\.com\//,
-    "queryParam": "q",
-    "codeParam": "t",
-    "codePrefixes": ["ff"],
-  },
-  "yahoo": {
-    "regexp": /^https:\/\/(?:.*)search\.(yahoo)\.com\/search/,
-    "queryParam": "p",
-  },
-  "baidu": {
-    "regexp": /^https:\/\/www\.(baidu)\.com\/(?:s|baidu)/,
-    "queryParam": "wd",
-    "codeParam": "tn",
-    "codePrefixes": ["monline_dg"],
-    "followonParams": ["oq"],
-  },
-  "bing": {
-    "regexp": /^https:\/\/www\.(bing)\.com\/search/,
-    "queryParam": "q",
-    "codeParam": "pc",
-    "codePrefixes": ["MOZ", "MZ"],
-  },
-};
-
 const SEARCH_COUNTS_HISTOGRAM_KEY = "SEARCH_COUNTS";
 /**
  * Prefixed to all search debug output.
@@ -4562,67 +4528,6 @@ SearchService.prototype = {
     return new ParseSubmissionResult(mapEntry.engine, terms, offset, length);
   },
 
-  __searchProviderInfo: null,
-  get _searchProviderInfo() {
-    if (!this.__searchProviderInfo) {
-      this.__searchProviderInfo = SEARCH_PROVIDER_INFO;
-    }
-    return this.__searchProviderInfo;
-  },
-
-  recordSearchURLTelemetry(url) {
-    let entry = Object.entries(this._searchProviderInfo).find(
-      ([_, info]) => info.regexp.test(url)
-    );
-    if (!entry) {
-      return;
-    }
-    let [provider, searchProviderInfo] = entry;
-    let queries = new URLSearchParams(url.split("#")[0].split("?")[1]);
-    if (!queries.get(searchProviderInfo.queryParam)) {
-      return;
-    }
-    // Default to organic to simplify things.
-    // We override type in the sap cases.
-    let type = "organic";
-    let code;
-    if (searchProviderInfo.codeParam) {
-      code = queries.get(searchProviderInfo.codeParam);
-      if (code &&
-          searchProviderInfo.codePrefixes.some(p => code.startsWith(p))) {
-        if (searchProviderInfo.followonParams &&
-           searchProviderInfo.followonParams.some(p => queries.has(p))) {
-          type = "sap-follow-on";
-        } else {
-          type = "sap";
-        }
-      } else if (provider == "bing") {
-        // Bing requires lots of extra work related to cookies.
-        let secondaryCode = queries.get("form");
-        // This code is used for all Bing follow-on searches.
-        if (secondaryCode == "QBRE") {
-          for (let cookie of Services.cookies.getCookiesFromHost("www.bing.com", {})) {
-            if (cookie.name == "SRCHS") {
-              // If this cookie is present, it's probably an SAP follow-on.
-              // This might be an organic follow-on in the same session,
-              // but there is no way to tell the difference.
-              if (searchProviderInfo.codePrefixes.some(p => cookie.value.startsWith("PC=" + p))) {
-                type = "sap-follow-on";
-                code = cookie.value.split("=")[1];
-                break;
-              }
-            }
-          }
-        }
-      }
-    }
-
-    let payload = `${provider}.in-content:${type}:${code || "none"}`;
-    let histogram = Services.telemetry.getKeyedHistogramById(SEARCH_COUNTS_HISTOGRAM_KEY);
-    histogram.add(payload);
-    LOG("nsSearchService::recordSearchURLTelemetry: " + payload);
-  },
-
   // nsIObserver
   observe: function SRCH_SVC_observe(aEngine, aTopic, aVerb) {
     switch (aTopic) {
@@ -4662,18 +4567,6 @@ SearchService.prototype = {
         // Locales are removed during shutdown, so ignore this message
         if (!Services.startup.shuttingDown) {
           this._asyncReInit();
-        }
-        break;
-
-      case "test:setSearchProviderInfo":
-        if (aVerb) {
-          let infoByProvider = JSON.parse(aVerb);
-          for (let info of Object.values(infoByProvider)) {
-            info.regexp = new RegExp(info.regexp);
-          }
-          this.__searchProviderInfo = infoByProvider;
-        } else {
-          this.__searchProviderInfo = SEARCH_PROVIDER_INFO;
         }
         break;
     }
