6941dc26d111d897c21a74e5a0e23045b1e8c872
Alessio Placitelli <alessio.placitelli@gmail.com>
2018-11-15 17:10:24 +0100

    [components] Fix the klint issue

--- mobile/android/android-components/components/service/glean/src/main/java/mozilla/components/service/glean/storages/GenericScalarStorageEngine.kt
+++ mobile/android/android-components/components/service/glean/src/main/java/mozilla/components/service/glean/storages/GenericScalarStorageEngine.kt
@@ -7,7 +7,6 @@ package mozilla.components.service.glean.storages
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import android.support.annotation.VisibleForTesting
 import mozilla.components.service.glean.Lifetime
 import mozilla.components.support.base.log.logger.Logger
 import org.json.JSONObject
