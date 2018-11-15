5e72dc5a5be575240336439146e7986fc377d568
Brad Werth <bwerth@mozilla.com>
2018-11-15 16:20:40 +0000

    Bug 1506687 Part 2: Make FlexItemValues also provide the item's position and size. r=dholbert
    Depends on D11782
    
    Differential Revision: https://phabricator.services.mozilla.com/D11783
    
    --HG--
    extra : moz-landing-system : lando

--- dom/flex/FlexItemValues.h
+++ dom/flex/FlexItemValues.h
@@ -39,10 +39,14 @@ public:
   }
 
   nsINode* GetNode() const;
+  double MainPosition() const;
+  double MainSize() const;
   double MainBaseSize() const;
   double MainDeltaSize() const;
   double MainMinSize() const;
   double MainMaxSize() const;
+  double CrossPosition() const;
+  double CrossSize() const;
   double CrossMinSize() const;
   double CrossMaxSize() const;
   FlexItemClampState ClampState() const;
@@ -51,11 +55,15 @@ protected:
   RefPtr<FlexLineValues> mParent;
   RefPtr<nsINode> mNode;
 
-  // These sizes are all CSS pixel units.
+  // These measurements are all CSS pixel units.
+  double mMainPosition;
+  double mMainSize;
   double mMainBaseSize;
   double mMainDeltaSize;
   double mMainMinSize;
   double mMainMaxSize;
+  double mCrossPosition;
+  double mCrossSize;
   double mCrossMinSize;
   double mCrossMaxSize;
   FlexItemClampState mClampState;
