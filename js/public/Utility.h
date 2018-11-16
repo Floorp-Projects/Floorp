33282a5166d47068ca6d81e60f7defdd0fec510a
Jan de Mooij <jdemooij@mozilla.com>
2018-11-16 17:12:44 +0000

    Bug 1507721 - Simplify AutoEnterOOMUnsafeRegion by adding an explicit inUnsafeRegion_ flag to the OOM simulator. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D12103

--- js/public/Utility.h
+++ js/public/Utility.h
@@ -142,22 +142,24 @@ class FailureSimulator
     uint64_t maxChecks_ = UINT64_MAX;
     uint64_t counter_ = 0;
     bool failAlways_ = true;
+    bool inUnsafeRegion_ = false;
 
   public:
     uint64_t maxChecks() const {
         return maxChecks_;
     }
-    void setMaxChecks(uint64_t value) {
-        maxChecks_ = value;
-    }
     uint64_t counter() const {
         return counter_;
+    }
+    void setInUnsafeRegion(bool b) {
+        MOZ_ASSERT(inUnsafeRegion_ != b);
+        inUnsafeRegion_ = b;
     }
     uint32_t targetThread() const {
         return targetThread_;
     }
     bool isThreadSimulatingAny() const {
-        return targetThread_ && targetThread_ == js::oom::GetThreadType();
+        return targetThread_ && targetThread_ == js::oom::GetThreadType() && !inUnsafeRegion_;
     }
     bool isThreadSimulating(Kind kind) const {
         return kind_ == kind && isThreadSimulatingAny();
@@ -319,25 +321,17 @@ struct MOZ_RAII JS_PUBLIC_DATA(AutoEnterOOMUnsafeRegion)
 
 #if defined(DEBUG) || defined(JS_OOM_BREAKPOINT)
     AutoEnterOOMUnsafeRegion()
-      : oomEnabled_(oom::simulator.isThreadSimulatingAny() &&
-                    oom::simulator.maxChecks() != UINT64_MAX),
-        oomAfter_(0)
+      : oomEnabled_(oom::simulator.isThreadSimulatingAny())
     {
         if (oomEnabled_) {
             MOZ_ALWAYS_TRUE(owner_.compareExchange(nullptr, this));
-            oomAfter_ = (int64_t(oom::simulator.maxChecks()) -
-                         int64_t(oom::simulator.counter()));
-            oom::simulator.setMaxChecks(UINT64_MAX);
+            oom::simulator.setInUnsafeRegion(true);
         }
     }
 
     ~AutoEnterOOMUnsafeRegion() {
         if (oomEnabled_) {
-            MOZ_ASSERT(oom::simulator.maxChecks() == UINT64_MAX);
-            int64_t maxChecks = int64_t(oom::simulator.counter()) + oomAfter_;
-            MOZ_ASSERT(maxChecks >= 0,
-                       "alloc count + oom limit exceeds range, your oom limit is probably too large");
-            oom::simulator.setMaxChecks(maxChecks);
+            oom::simulator.setInUnsafeRegion(false);
             MOZ_ALWAYS_TRUE(owner_.compareExchange(this, nullptr));
         }
     }
@@ -347,7 +341,6 @@ struct MOZ_RAII JS_PUBLIC_DATA(AutoEnterOOMUnsafeRegion)
     static mozilla::Atomic<AutoEnterOOMUnsafeRegion*> owner_;
 
     bool oomEnabled_;
-    int64_t oomAfter_;
 #endif
 };
 
