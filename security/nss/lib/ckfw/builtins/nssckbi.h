1538b46c7939bf9fc4e2b22d1423057c6f9b7608
J.C. Jones <jc@mozilla.com>
2018-11-16 18:24:27 +0000

    Bug 1501587 - land NSS 59ef418bf925 UPGRADE_NSS_RELEASE, r=me

--- security/nss/lib/ckfw/builtins/nssckbi.h
+++ security/nss/lib/ckfw/builtins/nssckbi.h
@@ -46,8 +46,8 @@
  * It's recommend to switch back to 0 after having reached version 98/99.
  */
 #define NSS_BUILTINS_LIBRARY_VERSION_MAJOR 2
-#define NSS_BUILTINS_LIBRARY_VERSION_MINOR 28
-#define NSS_BUILTINS_LIBRARY_VERSION "2.28"
+#define NSS_BUILTINS_LIBRARY_VERSION_MINOR 30
+#define NSS_BUILTINS_LIBRARY_VERSION "2.30"
 
 /* These version numbers detail the semantic changes to the ckfw engine. */
 #define NSS_BUILTINS_HARDWARE_VERSION_MAJOR 1
