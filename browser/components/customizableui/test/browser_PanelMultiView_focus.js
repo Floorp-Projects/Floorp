6d78b20542f8293e4434bd76fdd448e6bdea8370
James Teh <jteh@mozilla.com>
2018-11-16 22:51:59 +0000

    Bug 1506503: PanelMultiView: Automatically focus the first item if a panel is opened via the keyboard. r=johannh,paolo
    Previously, we had specific code to do this for the "View site information" button (#identity-box) when activated via the keyboard.
    To work well for keyboard and screen reader users, all such popups (e.g. Firefox menu, Page Actions, etc.) should do this.
    These are all based on panelMultiView.
    The arguments passed to PanelMultiView.openPopup can include the event which triggered the popup.
    We now use this to detect keypress events and focus the first item in the panel in that case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11605

---
+++ browser/components/customizableui/test/browser_PanelMultiView_focus.js
@@ -0,0 +1,66 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Test the focus behavior when opening PanelViews.
+ */
+
+ChromeUtils.import("resource:///modules/PanelMultiView.jsm");
+
+let gAnchor;
+let gPanel;
+let gMainView;
+let gMainButton;
+
+add_task(async function setup() {
+  let navBar = document.getElementById("nav-bar");
+  gAnchor = document.createXULElement("toolbarbutton");
+  // Must be focusable in order for key presses to work.
+  gAnchor.style["-moz-user-focus"] = "normal";
+  navBar.appendChild(gAnchor);
+  gPanel = document.createXULElement("panel");
+  navBar.appendChild(gPanel);
+  let panelMultiView = document.createXULElement("panelmultiview");
+  panelMultiView.setAttribute("mainViewId", "testMainView");
+  gPanel.appendChild(panelMultiView);
+  gMainView = document.createXULElement("panelview");
+  gMainView.id = "testMainView";
+  panelMultiView.appendChild(gMainView);
+  gMainButton = document.createXULElement("button");
+  gMainView.appendChild(gMainButton);
+
+  let onPress = event => PanelMultiView.openPopup(gPanel, gAnchor, {
+    triggerEvent: event,
+  });
+  gAnchor.addEventListener("keypress", onPress);
+  gAnchor.addEventListener("click", onPress);
+
+  registerCleanupFunction(() => {
+    gAnchor.remove();
+    gPanel.remove();
+  });
+});
+
+// Activate the main view by pressing a key. Focus should be moved inside.
+add_task(async function testMainViewByKeypress() {
+  gAnchor.focus();
+  await gCUITestUtils.openPanelMultiView(gPanel, gMainView,
+    () => EventUtils.synthesizeKey(" "));
+  Assert.equal(document.activeElement, gMainButton,
+    "Focus on button in main view");
+  await gCUITestUtils.hidePanelMultiView(gPanel,
+    () => PanelMultiView.hidePopup(gPanel));
+});
+
+// Activate the main view by clicking the mouse. Focus should not be moved
+// inside.
+add_task(async function testMainViewByClick() {
+  await gCUITestUtils.openPanelMultiView(gPanel, gMainView,
+    () => gAnchor.click());
+  Assert.notEqual(document.activeElement, gMainButton,
+    "Focus not on button in main view");
+  await gCUITestUtils.hidePanelMultiView(gPanel,
+    () => PanelMultiView.hidePopup(gPanel));
+});
