19d1471647979618f0c8bd66f09cc9c828872857
Jason Orendorff <jorendorff@mozilla.com>
2018-11-16 13:30:47 +0000

    Bug 1503406 - Fix cross-compartment bug in ReadableStreamTee_Pull. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D11697

---
+++ js/src/jit-test/tests/stream/bug-1503406.js
@@ -0,0 +1,13 @@
+let g = newGlobal();
+let reader = g.eval(`
+    let stream = new ReadableStream({
+        start(controller) {
+            controller.enqueue([]);
+        },
+    });
+    let [b1, b2] = stream.tee();
+    b1.getReader();
+`);
+let read = new ReadableStream({}).getReader().read;
+drainJobQueue();  // let the stream be started before reading
+read.call(reader);
