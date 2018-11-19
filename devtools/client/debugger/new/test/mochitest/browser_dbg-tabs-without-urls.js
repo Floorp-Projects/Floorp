70404f6a96dbb6ba52a7523298682b5e297bc6c1
Logan Smyth <loganfsmyth@gmail.com>
2018-11-19 23:37:10 +0000

    Bug 1507911 - Update Debugger Frontend v103 r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D12188
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-tabs-without-urls.js
@@ -0,0 +1,31 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+function countTabs(dbg) {
+  return findElement(dbg, "sourceTabs").children.length;
+}
+
+// Test that URL-less sources have tabs added to the UI but 
+// do not persist upon reload
+add_task(async function() {
+  const dbg = await initDebugger("doc-scripts.html", "simple1", "simple2");
+
+  await selectSource(dbg, "simple1");
+  await selectSource(dbg, "simple2");
+
+  is(countTabs(dbg), 2);
+
+  invokeInTab("doEval");
+  await waitForPaused(dbg);
+  await resume(dbg);
+  is(countTabs(dbg), 3);
+
+  invokeInTab("doEval");
+  await waitForPaused(dbg);
+  await resume(dbg);
+  is(countTabs(dbg), 4);
+
+  // Test reloading the debugger
+  await reload(dbg, "simple1", "simple2");
+  is(countTabs(dbg), 2);
+});
