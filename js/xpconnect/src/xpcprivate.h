765cf67ac7a2301e54c94f2fa85f2736db6b2aee
Timothy Guan-tin Chien <timdream@gmail.com>
2018-11-19 18:46:57 +0000

    Bug 1508147 - Re-remove unused CompartmentPrivate::isAddonCompartment, r=bholley
    It was removed in bug 1478275, but re-introduced because of a false rebase in bug 1431255.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12331

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -2941,11 +2941,6 @@ public:
     // True if this is a sandbox compartment. See xpc::CreateSandboxObject.
     bool isSandboxCompartment;
 
-    // True if EnsureAddonCompartment has been called for this compartment.
-    // Note that this is false for extensions that ship with the browser, like
-    // browser/extensions/activity-stream.
-    bool isAddonCompartment;
-
     // This is only ever set during mochitest runs when enablePrivilege is called.
     // It's intended as a temporary stopgap measure until we can finish ripping out
     // enablePrivilege. Once set, this value is never unset (i.e., it doesn't follow
