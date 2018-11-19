292c295604659efd2d455a3bb5a6c84a73bdfebc
Ehsan Akhgari <ehsan@mozilla.com>
2018-11-19 15:25:33 +0000

    Bug 1504728 - Part 1: Add a content blocking notification for indicating when a top-level page is using cookies or site data r=baku,valentin,johannh
    Differential Revision: https://phabricator.services.mozilla.com/D11122

--- netwerk/protocol/data/DataChannelParent.cpp
+++ netwerk/protocol/data/DataChannelParent.cpp
@@ -39,6 +39,13 @@ DataChannelParent::NotifyTrackingProtectionDisabled()
 }
 
 NS_IMETHODIMP
+DataChannelParent::NotifyCookieAllowed()
+{
+    // Nothing to do.
+    return NS_OK;
+}
+
+NS_IMETHODIMP
 DataChannelParent::NotifyTrackingCookieBlocked(uint32_t aRejectedReason)
 {
     // Nothing to do.
