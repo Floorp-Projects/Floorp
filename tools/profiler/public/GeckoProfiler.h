23a3141a2454dc6dc247a24d0fc644f1539f4468
Ted Campbell <tcampbell@mozilla.com>
2018-11-19 17:41:27 +0000

    Bug 1508180 - Use uppercase high-order macro names in profiler. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D12235

--- tools/profiler/public/GeckoProfiler.h
+++ tools/profiler/public/GeckoProfiler.h
@@ -114,50 +114,50 @@ class TimeStamp;
 //---------------------------------------------------------------------------
 
 // Higher-order macro containing all the feature info in one place. Define
-// |macro| appropriately to extract the relevant parts. Note that the number
+// |MACRO| appropriately to extract the relevant parts. Note that the number
 // values are used internally only and so can be changed without consequence.
 // Any changes to this list should also be applied to the feature list in
 // browser/components/extensions/schemas/geckoProfiler.json.
-#define PROFILER_FOR_EACH_FEATURE(macro) \
+#define PROFILER_FOR_EACH_FEATURE(MACRO) \
   /* Profile Java code (Android only). */ \
-  macro(0, "java", Java) \
+  MACRO(0, "java", Java) \
   \
   /* Get the JS engine to expose the JS stack to the profiler */ \
-  macro(1, "js", JS) \
+  MACRO(1, "js", JS) \
   \
   /* Include the C++ leaf node if not stackwalking. */ \
   /* The DevTools profiler doesn't want the native addresses. */ \
-  macro(2, "leaf", Leaf) \
+  MACRO(2, "leaf", Leaf) \
   \
   /* Add main thread I/O to the profile. */ \
-  macro(3, "mainthreadio", MainThreadIO) \
+  MACRO(3, "mainthreadio", MainThreadIO) \
   \
   /* Add memory measurements (e.g. RSS). */ \
-  macro(4, "memory", Memory) \
+  MACRO(4, "memory", Memory) \
   \
   /* Do not include user-identifiable information. */ \
-  macro(5, "privacy", Privacy) \
+  MACRO(5, "privacy", Privacy) \
   \
   /* Collect thread responsiveness information. */ \
-  macro(6, "responsiveness", Responsiveness) \
+  MACRO(6, "responsiveness", Responsiveness) \
   \
   /* Take a snapshot of the window on every composition. */ \
-  macro(7, "screenshots", Screenshots) \
+  MACRO(7, "screenshots", Screenshots) \
   \
   /* Disable parallel traversal in styling. */ \
-  macro(8, "seqstyle", SequentialStyle) \
+  MACRO(8, "seqstyle", SequentialStyle) \
   \
   /* Walk the C++ stack. Not available on all platforms. */ \
-  macro(9, "stackwalk", StackWalk) \
+  MACRO(9, "stackwalk", StackWalk) \
   \
   /* Start profiling with feature TaskTracer. */ \
-  macro(10, "tasktracer", TaskTracer) \
+  MACRO(10, "tasktracer", TaskTracer) \
   \
   /* Profile the registered secondary threads. */ \
-  macro(11, "threads", Threads) \
+  MACRO(11, "threads", Threads) \
   \
   /* Have the JavaScript engine track JIT optimizations. */ \
-  macro(12, "trackopts", TrackOptimizations)
+  MACRO(12, "trackopts", TrackOptimizations)
 
 struct ProfilerFeature
 {
