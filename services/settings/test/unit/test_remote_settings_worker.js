76f7a8ad5eda61c70139669fe8a2c98f163ce60b
Mathieu Leplatre <mathieu@mozilla.com>
2018-11-20 14:00:06 +0000

    Bug 1502146 -  Reduce impact of RemoteSettings synchronization on main thread r=Gijs
    Move JSON dump loading and CanonicalJSON serialization to a worker to reduce impact on main thread
    
    Differential Revision: https://phabricator.services.mozilla.com/D10064

---
+++ services/settings/test/unit/test_remote_settings_worker.js
@@ -0,0 +1,50 @@
+/* import-globals-from ../../../common/tests/unit/head_helpers.js */
+
+ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+const { RemoteSettingsWorker } = ChromeUtils.import("resource://services-settings/RemoteSettingsWorker.jsm", {});
+const { Kinto } = ChromeUtils.import("resource://services-common/kinto-offline-client.js", {});
+
+const IS_ANDROID = AppConstants.platform == "android";
+
+add_task(async function test_canonicaljson_merges_remote_into_local() {
+  const localRecords = [{ id: "1", title: "title 1" }, { id: "2", title: "title 2" }, { id: "3", title: "title 3" }];
+  const remoteRecords = [{ id: "2", title: "title b" }, { id: "3", deleted: true }];
+  const timestamp = 42;
+
+  const serialized = await RemoteSettingsWorker.canonicalStringify(localRecords, remoteRecords, timestamp);
+
+  Assert.equal('{"data":[{"id":"1","title":"title 1"},{"id":"2","title":"title b"}],"last_modified":"42"}', serialized);
+});
+
+
+add_task(async function test_import_json_dump_into_idb() {
+  if (IS_ANDROID) {
+    // Skip test: we don't ship remote settings dumps on Android (see package-manifest).
+    return;
+  }
+  const kintoCollection = new Kinto({
+    bucket: "main",
+    adapter: Kinto.adapters.IDB,
+    adapterOptions: { dbName: "remote-settings" },
+  }).collection("language-dictionaries");
+  const { data: before } = await kintoCollection.list();
+  Assert.equal(before.length, 0);
+
+  await RemoteSettingsWorker.importJSONDump("main", "language-dictionaries");
+
+  const { data: after } = await kintoCollection.list();
+  Assert.ok(after.length > 0);
+});
+
+
+add_task(async function test_throws_error_if_worker_fails() {
+  let error;
+  try {
+    await RemoteSettingsWorker.canonicalStringify(null, [], 42);
+  } catch (e) {
+    error = e;
+  }
+  Assert.equal("TypeError: localRecords is null; can't access its \"concat\" property", error.message);
+});
