2704f0b07c5bc6022c6c09312a1b0c8942ccd7b3
Ed Lee <edilee@mozilla.com>
2018-11-20 19:35:08 +0000

    Bug 1507610 - Allow running find-dupes without all of MOZ_AUTOMATION r=mshal
    Differential Revision: https://phabricator.services.mozilla.com/D12065
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/installer/packager.mk
+++ toolkit/mozapps/installer/packager.mk
@@ -13,6 +13,10 @@ ifndef PACKAGER_NO_LIBS
 libs:: make-package
 endif
 
+ifdef MOZ_AUTOMATION
+RUN_FIND_DUPES ?= $(MOZ_AUTOMATION)
+endif
+
 export USE_ELF_HACK ELF_HACK_FLAGS
 
 # Override the value of OMNIJAR_NAME from config.status with the value
@@ -34,9 +38,9 @@ stage-package: multilocale.txt locale-manifest.in $(MOZ_PKG_MANIFEST) $(MOZ_PKG_
 		$(addprefix --compress ,$(JAR_COMPRESSION)) \
 		$(MOZ_PKG_MANIFEST) '$(DIST)' '$(DIST)'/$(MOZ_PKG_DIR)$(if $(MOZ_PKG_MANIFEST),,$(_BINPATH)) \
 		$(if $(filter omni,$(MOZ_PACKAGER_FORMAT)),$(if $(NON_OMNIJAR_FILES),--non-resource $(NON_OMNIJAR_FILES)))
-ifdef MOZ_AUTOMATION
+ifdef RUN_FIND_DUPES
 	$(PYTHON) $(MOZILLA_DIR)/toolkit/mozapps/installer/find-dupes.py $(DEFINES) $(ACDEFINES) $(MOZ_PKG_DUPEFLAGS) $(DIST)/$(MOZ_PKG_DIR)
-endif # MOZ_AUTOMATION
+endif # RUN_FIND_DUPES
 ifndef MOZ_IS_COMM_TOPDIR
 	# Package mozharness
 	$(call py_action,test_archive, \
