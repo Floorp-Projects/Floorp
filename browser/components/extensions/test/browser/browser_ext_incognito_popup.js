b8b0e4b612cfbf7122f4f45c5a72944f856aa3d7
Shane Caraveo <scaraveo@mozilla.com>
2018-11-20 18:24:38 +0000

    Bug 1506653 support disabling page action for private windows, r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D11805
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/extensions/test/browser/browser_ext_incognito_popup.js
+++ browser/components/extensions/test/browser/browser_ext_incognito_popup.js
@@ -106,3 +106,32 @@ add_task(async function testIncognitoPopup() {
   await extension.awaitFinish("incognito");
   await extension.unload();
 });
+
+add_task(async function test_pageAction_incognito_not_allowed() {
+  const URL = "http://example.com/";
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      "permissions": ["*://example.com/*"],
+      "page_action": {
+        "show_matches": ["<all_urls>"],
+        "pinned": true,
+      },
+    },
+    incognitoOverride: "not_allowed",
+  });
+
+  await extension.startup();
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, URL, true, true);
+  let privateWindow = await BrowserTestUtils.openNewBrowserWindow({private: true});
+  await BrowserTestUtils.openNewForegroundTab(privateWindow.gBrowser, URL, true, true);
+
+  let elem = await getPageActionButton(extension, window);
+  ok(elem, "pageAction button state correct in non-PB");
+
+  elem = await getPageActionButton(extension, privateWindow);
+  ok(!elem, "pageAction button state correct in private window");
+
+  BrowserTestUtils.removeTab(tab);
+  await BrowserTestUtils.closeWindow(privateWindow);
+  await extension.unload();
+});
