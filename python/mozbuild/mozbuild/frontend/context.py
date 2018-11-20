355e9a39dfc30ec307b266009454bfa31c8e7eef
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-11-21 01:38:34 +0200

    Backed out 3 changesets (bug 1503776) for xperf failures
    Backed out changeset 596f70b5d5b0 (bug 1503776)
    Backed out changeset 2d2d1dfd04d4 (bug 1503776)
    Backed out changeset 3eb49d9e73bb (bug 1503776)

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -1855,10 +1855,6 @@ VARIABLES = {
         """List of manifest files defining marionette-layout tests.
         """),
 
-    'MARIONETTE_GPU_MANIFESTS': (ManifestparserManifestList, list,
-        """List of manifest files defining marionette-gpu tests.
-        """),
-
     'MARIONETTE_UNIT_MANIFESTS': (ManifestparserManifestList, list,
         """List of manifest files defining marionette-unit tests.
         """),
