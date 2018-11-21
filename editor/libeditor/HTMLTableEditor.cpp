670a5e4b3132afcc66a61c4158db9a6a4f85c6b5
Masayuki Nakano <masayuki@d-toybox.com>
2018-11-21 09:30:40 +0000

    Bug 1504911 - part 5: Make HTMLEditor::InsertTableCellsWithTransaction() create AutoPlaceholderBatch and AutoTopLevelEditSubActionNotifier r=m_kato
    Currently, calling nsITableEditor.insertTableCell() does not cause dispatching
    "input" event since it does not create AutoPlaceholderBatch.  Additionally,
    different from InsertTableRowsWithTransaction() and
    InsertTableColumnsWithTransaction(), it does not create
    AutoTopLevelEditSubActionNotifier.
    
    Because of those APIs should work similarly, we should make it creates
    both auto class instances.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12248

--- editor/libeditor/HTMLTableEditor.cpp
+++ editor/libeditor/HTMLTableEditor.cpp
@@ -229,6 +229,12 @@ HTMLEditor::InsertTableCellsWithTransaction(int32_t aNumberOfCellsToInsert,
       MOZ_ASSERT_UNREACHABLE("Invalid InsertPosition");
   }
 
+  AutoPlaceholderBatch treateAsOneTransaction(*this);
+  // Prevent auto insertion of BR in new cell until we're done
+  AutoTopLevelEditSubActionNotifier maybeTopLevelEditSubAction(
+                                      *this, EditSubAction::eInsertNode,
+                                      nsIEditor::eNext);
+
   // We control selection resetting after the insert.
   AutoSelectionSetterAfterTableEdit setCaret(*this, table,
                                              cellDataAtSelection.mCurrent.mRow,
