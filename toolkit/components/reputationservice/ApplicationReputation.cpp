651760fe3626b5f54bb6748b7a0dde2bf6f70377
Dimi Lee <dlee@mozilla.com>
2018-11-21 19:43:06 +0000

    Bug 1397546 - Allow Application Reputation lookups when the blacklist/whitelist is missing. r=francois
    Before landing this patch, download protection doesn't trigger a remote lookup when
    blocklist or allowlist is empty.
    
    After landing this patch, download protection triggers a remote lookup
    regardless if blocklist or allowlist is empty.
    
    This is safer default behavior in download protection.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11621
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/reputationservice/ApplicationReputation.cpp
+++ toolkit/components/reputationservice/ApplicationReputation.cpp
@@ -1422,29 +1422,6 @@ PendingLookup::SendRemoteQueryInternal()
     return NS_ERROR_NOT_AVAILABLE;
   }
 
-  // If the blocklist or allowlist is empty (so we couldn't do local lookups),
-  // bail
-  {
-    nsAutoCString table;
-    NS_ENSURE_SUCCESS(Preferences::GetCString(PREF_DOWNLOAD_BLOCK_TABLE,
-                                              table),
-                      NS_ERROR_NOT_AVAILABLE);
-    if (table.IsEmpty()) {
-      LOG(("Blocklist is empty [this = %p]", this));
-      return NS_ERROR_NOT_AVAILABLE;
-    }
-  }
-  {
-    nsAutoCString table;
-    NS_ENSURE_SUCCESS(Preferences::GetCString(PREF_DOWNLOAD_ALLOW_TABLE,
-                                              table),
-                      NS_ERROR_NOT_AVAILABLE);
-    if (table.IsEmpty()) {
-      LOG(("Allowlist is empty [this = %p]", this));
-      return NS_ERROR_NOT_AVAILABLE;
-    }
-  }
-
   LOG(("Sending remote query for application reputation [this = %p]",
        this));
   // We did not find a local result, so fire off the query to the
