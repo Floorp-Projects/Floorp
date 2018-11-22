28e3488eebd43a3ffb77ae9db91e44143df71f00
k88hudson <k88hudson@gmail.com>
2018-11-22 18:37:34 +0200

    Bug 1506225 - Support blocking by campaign id in ASR r=r1cky a=jcristau
    Reviewers: r1cky
    
    Reviewed By: r1cky
    
    Bug #: 1506225
    
    Differential Revision: https://phabricator.services.mozilla.com/D12582
    
    --HG--
    extra : rebase_source : a92a4c060160f512a8e85955d3db8126abe5e7c9
    extra : histedit_source : cdb09907348030a79d4d024478c89a40ecbe6139

--- browser/components/newtab/data/content/activity-stream.bundle.js
+++ browser/components/newtab/data/content/activity-stream.bundle.js
@@ -1817,7 +1817,7 @@ class ASRouterAdmin extends react__WEBPACK_IMPORTED_MODULE_1___default.a.PureCom
 
   renderMessageItem(msg) {
     const isCurrent = msg.id === this.state.lastMessageId;
-    const isBlocked = this.state.messageBlockList.includes(msg.id);
+    const isBlocked = this.state.messageBlockList.includes(msg.id) || this.state.messageBlockList.includes(msg.campaign);
     const impressions = this.state.messageImpressions[msg.id] ? this.state.messageImpressions[msg.id].length : 0;
 
     let itemClassName = "message-item";
