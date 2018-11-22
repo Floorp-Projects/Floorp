f35b17dd6547f9bc49a238284ee8032085429b0a
Jed Davis <jld@mozilla.com>
2018-11-22 00:06:14 +0000

    Bug 1446161 - Remove the earlier attempt at async launch. r=spohl,mccr8
    The first attempt at async launch tried to hide the asynchrony inside
    IPC, by making the process seem to be launched enough to construct new
    channels and send it messages, and lazily blocking on the pid/handle.
    Unfortunately, in practice we wind up needing the pid/handle immediately,
    and this requirement is too deeply embedded in IPC for that to be viable.
    
    (The alternative that will be used instead -- exposing process launch via
    an explicitly asynchronous promise interface -- is made simpler by
    Project Fission's upcoming rewrite of how the DOM requests new content
    processes.)
    
    Depends on D8941
    
    Differential Revision: https://phabricator.services.mozilla.com/D8942

--- ipc/glue/GeckoChildProcessHost.h
+++ ipc/glue/GeckoChildProcessHost.h
@@ -72,8 +72,6 @@ public:
   bool SyncLaunch(StringVector aExtraOpts=StringVector(),
                   int32_t timeoutMs=0);
 
-  virtual void OnProcessHandleReady(ProcessHandle aProcessHandle);
-  virtual void OnProcessLaunchError();
   virtual void OnChannelConnected(int32_t peer_pid) override;
   virtual void OnMessageReceived(IPC::Message&& aMsg) override;
   virtual void OnChannelError() override;
