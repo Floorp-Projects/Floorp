473a5aa3bb7cb61b4864e5a1fec148294b268d44
arthur.iakab <aiakab@mozilla.com>
2018-11-22 07:57:52 +0200

    Backed out 9 changesets (bug 1500948, bug 1500949, bug 1448426, bug 1487249, bug 1500950, bug 1500944) for causing talos crashes on  ts_paint | application crashed [@ MOZ_CrashOOL(char const*, int, char const*)]
    Backed out changeset 429776feea6b (bug 1500950)
    Backed out changeset a300e9ae7a01 (bug 1500949)
    Backed out changeset fed1a518579a (bug 1500948)
    Backed out changeset a71376319ad7 (bug 1500944)
    Backed out changeset 9c5c35cb10aa (bug 1500944)
    Backed out changeset b6c97b4db60a (bug 1448426)
    Backed out changeset 160a1dbe0be5 (bug 1487249)
    Backed out changeset 8caf55928df2 (bug 1487249)
    Backed out changeset 176131b18cb4 (bug 1487249)

--- dom/base/nsDocument.cpp
+++ dom/base/nsDocument.cpp
@@ -270,8 +270,6 @@
 
 #include "mozilla/DocLoadingTimelineMarker.h"
 
-#include "mozilla/dom/WindowGlobalChild.h"
-
 #include "nsISpeculativeConnect.h"
 
 #include "mozilla/MediaManager.h"
@@ -3109,13 +3107,6 @@ nsIDocument::SetDocumentURI(nsIURI* aURI)
   // need to refresh the hrefs of all the links on the page.
   if (!equalBases) {
     RefreshLinkHrefs();
-  }
-
-  // Tell our WindowGlobalParent that the document's URI has been changed.
-  nsPIDOMWindowInner* inner = GetInnerWindow();
-  WindowGlobalChild* wgc = inner ? inner->GetWindowGlobalChild() : nullptr;
-  if (wgc) {
-    Unused << wgc->SendUpdateDocumentURI(mDocumentURI);
   }
 }
 
--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -634,8 +634,7 @@ MessageChannel::MessageChannel(const char* aName,
     mPeerPidSet(false),
     mPeerPid(-1),
     mIsPostponingSends(false),
-    mBuildIDsConfirmedMatch(false),
-    mIsSameThreadChannel(false)
+    mBuildIDsConfirmedMatch(false)
 {
     MOZ_COUNT_CTOR(ipc::MessageChannel);
 
@@ -965,35 +964,6 @@ MessageChannel::CommonThreadOpenInit(MessageChannel *aTargetChan, Side aSide)
 
     mLink = new ThreadLink(this, aTargetChan);
     mSide = aSide;
-}
-
-bool
-MessageChannel::OpenOnSameThread(MessageChannel* aTargetChan,
-                                 mozilla::ipc::Side aSide)
-{
-    CommonThreadOpenInit(aTargetChan, aSide);
-
-    Side oppSide = UnknownSide;
-    switch (aSide) {
-      case ChildSide: oppSide = ParentSide; break;
-      case ParentSide: oppSide = ChildSide; break;
-      case UnknownSide: break;
-    }
-    mIsSameThreadChannel = true;
-
-    // XXX(nika): Avoid setting up a monitor for same thread channels? We
-    // shouldn't need it.
-    mMonitor = new RefCountedMonitor();
-
-    mChannelState = ChannelOpening;
-    aTargetChan->CommonThreadOpenInit(this, oppSide);
-
-    aTargetChan->mIsSameThreadChannel = true;
-    aTargetChan->mMonitor = mMonitor;
-
-    mChannelState = ChannelConnected;
-    aTargetChan->mChannelState = ChannelConnected;
-    return true;
 }
 
 bool
@@ -1505,8 +1475,6 @@ MessageChannel::Send(Message* aMsg, Message* aReply)
     // Sanity checks.
     AssertWorkerThread();
     mMonitor->AssertNotCurrentThreadOwns();
-    MOZ_RELEASE_ASSERT(!mIsSameThreadChannel,
-                       "sync send over same-thread channel will deadlock!");
 
 #ifdef OS_WIN
     SyncStackFrame frame(this, false);
@@ -1712,8 +1680,6 @@ MessageChannel::Call(Message* aMsg, Message* aReply)
     UniquePtr<Message> msg(aMsg);
     AssertWorkerThread();
     mMonitor->AssertNotCurrentThreadOwns();
-    MOZ_RELEASE_ASSERT(!mIsSameThreadChannel,
-                       "intr call send over same-thread channel will deadlock!");
 
 #ifdef OS_WIN
     SyncStackFrame frame(this, true);
@@ -2490,9 +2456,6 @@ MessageChannel::WaitForSyncNotify(bool /* aHandleWindowsMessages */)
     }
 #endif
 
-    MOZ_RELEASE_ASSERT(!mIsSameThreadChannel,
-                       "Wait on same-thread channel will deadlock!");
-
     TimeDuration timeout = (kNoTimeout == mTimeoutMs) ?
                            TimeDuration::Forever() :
                            TimeDuration::FromMilliseconds(mTimeoutMs);
@@ -2805,10 +2768,6 @@ MessageChannel::SynchronouslyClose()
     AssertWorkerThread();
     mMonitor->AssertCurrentThreadOwns();
     mLink->SendClose();
-
-    MOZ_RELEASE_ASSERT(!mIsSameThreadChannel || ChannelClosed == mChannelState,
-                       "same-thread channel failed to synchronously close?");
-
     while (ChannelClosed != mChannelState)
         mMonitor->Wait();
 }
