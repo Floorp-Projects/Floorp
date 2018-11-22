4dd82ffbcb8926fc95347cb11fce28934143f011
Chris Pearce <cpearce@mozilla.com>
2018-11-22 21:36:09 +0000

    Bug 1503776 - Save default value of WebRender rollout pref to user pref, check on startup. r=mattwoodrow
    Normandy's Preference Rollout code sets default values on prefs, not user
    values (see uses of PrefUtils.setPref() in PreferenceRolloutAction.jsm).
    Default prefs are not persistent; unlike user prefs, changes to default pref
    values are not stored on disk. Changes to default values are only made on the
    in-memory copy of the pref's value, and thus don't survive a browser restart.
    Normandy changes the rolled out prefs early on in the startup of the browser,
    but not before gfxPlatform::Init() runs. So that means gfx can't use Normandy
    pref rollout to gradually rollout WebRender to release, as
    gfxPlatform::InitWebRenderConfig() won't see the rolled out version of the
    pref in time to turn on WebRender.
    
    So to work around this, add a profile-before-change shutdown observer that
    saves the default value of the gfx.webrender.all.qualified pref to a new user
    pref, gfx.webrender.all.qualified.default. We check that on startup and
    emulate the behavior that the pref system would have if that pref default
    value had already been set by Normandy.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10527
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/thebes/gfxPrefs.h
+++ gfx/thebes/gfxPrefs.h
@@ -532,6 +532,11 @@ private:
 
   DECL_GFX_PREF(Once, "gfx.webrender.all",                     WebRenderAll, bool, false);
   DECL_GFX_PREF(Once, "gfx.webrender.all.qualified",           WebRenderAllQualified, bool, true);
+  DECL_GFX_PREF(Once,
+                "gfx.webrender.all.qualified.default",
+                WebRenderAllQualifiedDefault,
+                bool,
+                false);
   DECL_GFX_PREF(Live, "gfx.webrender.blob-images",             WebRenderBlobImages, bool, true);
   DECL_GFX_PREF(Live, "gfx.webrender.blob.invalidation",       WebRenderBlobInvalidation, bool, false);
   DECL_GFX_PREF(Live, "gfx.webrender.blob.paint-flashing",     WebRenderBlobPaintFlashing, bool, false);
