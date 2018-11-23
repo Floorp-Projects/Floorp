6dca985d876113912326c2984f5114f1c66ffe3f
byron jones <glob@mozilla.com>
2018-11-23 13:00:07 +0000

    Bug 1507051 - replace libyuv's README_MOZILLA with moz.yaml; r=jesup
    Replace README_MOZILLA with moz.yaml, various fixups for update.py, and create
    a missing patch file from Bug 1491848.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11891

--- media/libyuv/update.py
+++ media/libyuv/update.py
@@ -3,89 +3,104 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import argparse
+import datetime
 import os
 import re
 import shutil
-import sys
-import datetime
-import subprocess
 import tarfile
 import urllib
-from pprint import pprint
-from StringIO import StringIO
+from subprocess import Popen, PIPE, STDOUT
 
-def prepare_upstream(prefix, commit=None):
+
+def prepare_upstream(base, commit):
     upstream_url = 'https://chromium.googlesource.com/libyuv/libyuv'
-    shutil.rmtree(os.path.join(base, 'libyuv/'))
-    print(upstream_url + '/+archive/' + commit + '.tar.gz')
-    urllib.urlretrieve(upstream_url + '/+archive/' + commit + '.tar.gz', 'libyuv.tar.gz')
-    tarfile.open('libyuv.tar.gz').extractall(path='libyuv')
-    os.remove(os.path.join(base, 'libyuv.tar.gz'))
-    os.chdir(base)
-    return commit
+    tarball_file = os.path.join(base, 'libyuv.tar.gz')
+    lib_path = os.path.join(base, 'libyuv')
 
-def get_commit_date(prefix, commit=None):
+    print(upstream_url + '/+archive/' + commit + '.tar.gz')
+    urllib.urlretrieve(upstream_url + '/+archive/' + commit + '.tar.gz',
+                       tarball_file)
+    shutil.rmtree(lib_path)
+    tarfile.open(tarball_file).extractall(path=lib_path)
+    os.remove(tarball_file)
+
+    shutil.copy2(os.path.join(lib_path, "LICENSE"), os.path.join(base, "LICENSE"))
+
+
+def get_commit_date(commit):
     upstream_url = 'https://chromium.googlesource.com/libyuv/libyuv/+/' + commit
-    text = urllib.urlopen(upstream_url).readlines()
-    text = "".join(text)
-    regex = '<tr><th class="Metadata-title">committer</th><td>.+</td><td>[^\s]+ ([0-9a-zA-Z: ]+)\s*\+*[0-9]*</td></tr>'
-    date = re.search(regex, text).groups(0)[0]
+    text = urllib.urlopen(upstream_url).read()
+    regex = r'<tr><th class="Metadata-title">committer</th>' \
+            r'<td>.+</td><td>[^\s]+ ([0-9a-zA-Z: ]+)\s*\+*[0-9]*</td></tr>'
+    date = re.search(regex, text).group(1)
     return datetime.datetime.strptime(date, "%b %d %H:%M:%S %Y")
 
-def cleanup_upstream():
+
+def cleanup_upstream(base):
     os.remove(os.path.join(base, 'libyuv/.gitignore'))
 
-def apply_patches():
-    # Patch to update gyp build files
-    os.system("patch -p3 < update_gyp.patch")
-    # Patch to fix build errors
-    os.system("patch -p3 < fix_build_errors.patch")
-    # Patch to make mjpeg printfs optional at build time
-    os.system("patch -p3 < make_mjpeg_printfs_optional.patch")
-    # Patch to allow disabling of inline ASM and AVX2 code
-    os.system("patch -p3 < allow_disabling_asm_avx2.patch")
-    # Patch to add H444ToARGB() variant
-    os.system("patch -p3 < add_H444ToARGB.patch")
-    # Patch to avoid selecting neon codepaths on AArch64 Windows
-    os.system("patch -p3 < aarch64-windows-noneon.patch")
 
-def update_readme(commit, commitdate):
-    with open('README_MOZILLA') as f:
-        readme = f.read()
+def apply_patches(base):
+    patches = [
+        # update gyp build files
+        "update_gyp.patch",
+        # fix build errors
+        'fix_build_errors.patch',
+        # make mjpeg printfs optional at build time
+        'make_mjpeg_printfs_optional.patch',
+        # allow disabling of inline ASM and AVX2 code
+        'allow_disabling_asm_avx2.patch',
+        # add H444ToARGB() variant
+        'add_H444ToARGB.patch',
+        # avoid selecting neon codepaths on AArch64 Windows
+        'aarch64-windows-noneon.patch',
+        # fix the x86 mingw-clang build
+        'bug_1491848.patch',
+    ]
 
-    if 'The git commit ID last used to import was ' in readme:
-        new_readme = re.sub('The git commit ID last used to import was [v\.a-f0-9]+ \(.+\)',
-            'The git commit ID last used to import was %s (%s)' % (commit, commitdate), readme)
-    else:
-        new_readme = "%s\n\nThe git commit ID last used to import was %s\n" % (readme, commit)
+    for patch in patches:
+        print('\nApplying patch %s' % patch)
+        with open(os.path.join(base, patch)) as f:
+            Popen(["patch", "-p3"], stdin=f, cwd=base).wait()
 
-    if readme != new_readme:
-        with open('README_MOZILLA', 'w') as f:
-            f.write(new_readme)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='''Update libyuv''')
-    parser.add_argument('--debug', dest='debug', action="store_true")
+def update_moz_yaml(base, commit, commitdate):
+    moz_yaml_file = os.path.join(base, 'moz.yaml')
+    with open(moz_yaml_file) as f:
+        moz_yaml = f.read()
+
+    new_moz_yaml = re.sub(r'\n\s+release:.+\n',
+                          '\n  release: "%s (%s)"\n' % (commit, commitdate),
+                          moz_yaml)
+
+    if moz_yaml != new_moz_yaml:
+        with open(moz_yaml_file, 'w') as f:
+            f.write(new_moz_yaml)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Update libyuv')
     parser.add_argument('--no-patches', dest='no_patches', action="store_true")
-    parser.add_argument('--commit', dest='commit', type=str, default='master')
-
+    parser.add_argument('--commit', dest='commit', default='master')
     args = parser.parse_args()
 
     commit = args.commit
-    DEBUG = args.debug
     no_patches = args.no_patches
+    base = os.path.realpath(os.path.dirname(__file__))
 
-    base = os.path.abspath(os.curdir)
-    prefix = os.path.join(base, 'libyuv/')
-
-    commit = prepare_upstream(prefix, commit)
-    commitdate = get_commit_date(prefix, commit)
+    prepare_upstream(base, commit)
+    commitdate = get_commit_date(commit)
 
     if not no_patches:
-        apply_patches()
+        apply_patches(base)
 
-    update_readme(commit, commitdate)
+    update_moz_yaml(base, commit, commitdate)
 
-    print('Patches applied; run "hg addremove --similarity 70 libyuv" before committing changes')
+    print('\nPatches applied; '
+          'run "hg addremove --similarity 70 libyuv" before committing changes')
 
-    cleanup_upstream()
+    cleanup_upstream(base)
+
+
+if __name__ == '__main__':
+    main()
