ff98163b4081fa43580486ece8141d36cefc4c14
James Willcox <snorp@snorp.net>
2018-11-23 20:32:40 +0000

    Bug 1485045 - Make Java parts of GeckoView independent from build ABI r=jchen
    This allows us to use the same Java code for any native platform,
    enabling a "fat" AAR.
    
    Differential Revision: https://phabricator.services.mozilla.com/D11497

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/HardwareUtils.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/HardwareUtils.java
@@ -15,6 +15,11 @@ import android.util.Log;
 import org.mozilla.gecko.SysInfo;
 import org.mozilla.geckoview.BuildConfig;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public final class HardwareUtils {
     private static final String LOGTAG = "GeckoHardwareUtils";
 
@@ -30,13 +35,14 @@ public final class HardwareUtils {
     private static volatile boolean sIsSmallTablet;
     private static volatile boolean sIsTelevision;
 
+    private static volatile File sLibDir;
+    private static volatile int sMachineType = -1;
+
     private HardwareUtils() {
     }
 
     public static void init(Context context) {
         if (sInited) {
-            // This is unavoidable, given that HardwareUtils is called from background services.
-            Log.d(LOGTAG, "HardwareUtils already inited.");
             return;
         }
 
@@ -53,6 +59,7 @@ public final class HardwareUtils {
             }
         }
 
+        sLibDir = new File(context.getApplicationInfo().nativeLibraryDir);
         sInited = true;
     }
 
@@ -120,6 +127,68 @@ public final class HardwareUtils {
         return getPreferredAbi();
     }
 
+    private static final int ELF_MACHINE_UNKNOWN = 0;
+    private static final int ELF_MACHINE_X86 = 0x03;
+    private static final int ELF_MACHINE_X86_64 = 0x3e;
+    private static final int ELF_MACHINE_ARM = 0x28;
+    private static final int ELF_MACHINE_AARCH64 = 0xb7;
+
+    private static int readElfMachineType(final File file) {
+        try (final FileInputStream is = new FileInputStream(file)) {
+            final byte[] buf = new byte[19];
+            int count = 0;
+            while (count != buf.length) {
+                count += is.read(buf, count, buf.length - count);
+            }
+
+            int machineType = buf[18];
+            if (machineType < 0) {
+                machineType += 256;
+            }
+
+            return machineType;
+        } catch (FileNotFoundException e) {
+            Log.w(LOGTAG, String.format("Failed to open %s", file.getAbsolutePath()));
+            return ELF_MACHINE_UNKNOWN;
+        } catch (IOException e) {
+            Log.w(LOGTAG, "Failed to read library", e);
+            return ELF_MACHINE_UNKNOWN;
+        }
+    }
+
+    private static String machineTypeToString(int machineType) {
+        switch (machineType) {
+            case ELF_MACHINE_X86:
+                return "x86";
+            case ELF_MACHINE_X86_64:
+                return "x86_64";
+            case ELF_MACHINE_ARM:
+                return "arm";
+            case ELF_MACHINE_AARCH64:
+                return "aarch64";
+            case ELF_MACHINE_UNKNOWN:
+            default:
+                return String.format("unknown (0x%x)", machineType);
+        }
+    }
+
+    private static void initMachineType() {
+        if (sMachineType >= 0) {
+            return;
+        }
+
+        sMachineType = readElfMachineType(new File(sLibDir, System.mapLibraryName("mozglue")));
+    }
+
+    /**
+     * @return The ABI of the libraries installed for this app.
+     */
+    public static String getLibrariesABI() {
+        initMachineType();
+
+        return machineTypeToString(sMachineType);
+    }
+
     /**
      * @return false if the current system is not supported (e.g. APK/system ABI mismatch).
      */
@@ -130,14 +199,16 @@ public final class HardwareUtils {
             return false;
         }
 
+        initMachineType();
+
         // See http://developer.android.com/ndk/guides/abis.html
         final boolean isSystemX86 = isX86System();
         final boolean isSystemARM = !isSystemX86 && isARMSystem();
         final boolean isSystemARM64 = isARM64System();
 
-        boolean isAppARM = BuildConfig.ANDROID_CPU_ARCH.startsWith("armeabi-v7a");
-        boolean isAppARM64 = BuildConfig.ANDROID_CPU_ARCH.startsWith("arm64-v8a");
-        boolean isAppX86 = BuildConfig.ANDROID_CPU_ARCH.startsWith("x86");
+        final boolean isAppARM = sMachineType == ELF_MACHINE_ARM;
+        final boolean isAppARM64 = sMachineType == ELF_MACHINE_AARCH64;
+        final boolean isAppX86 = sMachineType == ELF_MACHINE_X86;
 
         // Only reject known incompatible ABIs. Better safe than sorry.
         if ((isSystemX86 && isAppARM) || (isSystemARM && isAppX86)) {
@@ -150,7 +221,7 @@ public final class HardwareUtils {
         }
 
         Log.w(LOGTAG, "Unknown app/system ABI combination: " +
-                      BuildConfig.MOZ_APP_ABI + " / " + getRealAbi());
+                      machineTypeToString(sMachineType) + " / " + getRealAbi());
         return true;
     }
 }
