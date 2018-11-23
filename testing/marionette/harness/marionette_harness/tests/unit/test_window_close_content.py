d32937264a3d617d9b7679d1a20dff4c224fb0f7
Henrik Skupin <mail@hskupin.info>
2018-11-23 07:55:40 +0000

    Bug 1507803 - [marionette] Allow to open a chrome window via the WindowManager mixin class. r=ato
    The current implemenation for opening new chrome windows via the
    WindowManager mixin class is racy because it doesn't wait for the
    newly opened window to have focus and being activated first.
    
    The patch adds a new method to the WindowManager class, which is
    waits for those events. It can then be used across all existent
    Marionette unit tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12180

--- testing/marionette/harness/marionette_harness/tests/unit/test_window_close_content.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_window_close_content.py
@@ -35,15 +35,7 @@ class TestCloseWindow(WindowManagerMixin, MarionetteTestCase):
 
     @skip_if_mobile("Interacting with chrome windows not available for Fennec")
     def test_close_chrome_window_for_non_browser_window(self):
-
-        def open_window_with_js():
-            with self.marionette.using_context("chrome"):
-                self.marionette.execute_script("""
-                  window.open('chrome://marionette/content/test.xul',
-                              'foo', 'chrome,centerscreen');
-                """)
-
-        win = self.open_window(trigger=open_window_with_js)
+        win = self.open_chrome_window("chrome://marionette/content/test.xul")
         self.marionette.switch_to_window(win)
 
         self.assertIn(win, self.marionette.chrome_window_handles)
