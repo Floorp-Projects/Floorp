15791ed77b16da3decc59a78eccd9ac532c8023d
Razvan Caliman <rcaliman@mozilla.com>
2018-11-23 16:25:18 +0000

    Bug 1503924 - Trim source URLs to filename in Changes panel. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D12588

--- devtools/client/inspector/changes/utils/changes-utils.js
+++ devtools/client/inspector/changes/utils/changes-utils.js
@@ -4,6 +4,8 @@
 
 "use strict";
 
+const { getFormatStr, getStr } = require("./l10n");
+
 /**
 * Generate a hash that uniquely identifies a stylesheet or element style attribute.
 *
@@ -54,5 +56,39 @@ function getRuleHash(ruleData) {
   return `${atRules}${selector}${ruleIndex}`;
 }
 
+/**
+ * Get a human-friendly style source path to display in the Changes panel.
+ * For element inline styles, return a string indicating that.
+ * For inline stylesheets, return a string indicating that plus the stylesheet's index.
+ * For URLs, return just the stylesheet filename.
+ *
+ * @param {Object} source
+ *        Information about the style source. Contains:
+ *        - type: {String} One of "element" or "stylesheet"
+ *        - href: {String|null} Stylesheet URL or document URL for elmeent inline styles
+ *        - index: {Number} Position of the stylesheet in its document's stylesheet list.
+ * @return {String}
+ */
+function getSourceForDisplay(source) {
+  let href;
+
+  switch (source.type) {
+    case "element":
+      href = getStr("changes.elementStyleLabel");
+      break;
+    case "stylesheet":
+      if (source.href) {
+        const url = new URL(source.href);
+        href = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
+      } else {
+        href = getFormatStr("changes.inlineStyleSheetLabel", `#${source.index}`);
+      }
+      break;
+  }
+
+  return href;
+}
+
+module.exports.getSourceForDisplay = getSourceForDisplay;
 module.exports.getSourceHash = getSourceHash;
 module.exports.getRuleHash = getRuleHash;
