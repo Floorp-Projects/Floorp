dc5e8fada4cf5318ad8038273786d473722e9741
Johann Hofmann <jhofmann@mozilla.com>
2018-11-26 10:17:58 +0100

    Bug 1450784 - Add a new error page for MOZILLA_PKIX_ERROR_MITM_DETECTED. r=nhnt11,keeler
    Differential Revision: https://phabricator.services.mozilla.com/D12874

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -956,8 +956,10 @@ pref("security.alternate_certificate_error_page", "certerror");
 // Enable the new certificate error pages.
 #ifdef EARLY_BETA_OR_EARLIER
 pref("browser.security.newcerterrorpage.enabled", true);
+pref("browser.security.newcerterrorpage.mitm.enabled", true);
 #else
 pref("browser.security.newcerterrorpage.enabled", false);
+pref("browser.security.newcerterrorpage.mitm.enabled", false);
 #endif
 
 pref("security.certerrors.recordEventTelemetry", true);
