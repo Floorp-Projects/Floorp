dd58685bef06784359ad70e1d802f3b3c3e9ebb1
Edouard Oger <eoger@fastmail.com>
2018-11-26 17:26:30 -0500

    [components] Bump application services binaries version

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -29,9 +29,9 @@ private object Versions {
     const val sentry = "1.7.14"
     const val okhttp = "3.12.0"
 
-    const val mozilla_fxa = "0.10.0"
-    const val mozilla_sync_logins = "0.10.0"
-    const val mozilla_places = "0.10.0"
+    const val mozilla_fxa = "0.11.0"
+    const val mozilla_sync_logins = "0.11.0"
+    const val mozilla_places = "0.11.0"
     const val servo = "0.0.1.20181017.aa95911"
 }
 
