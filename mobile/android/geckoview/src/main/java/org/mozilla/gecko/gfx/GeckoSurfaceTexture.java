f64c0599d5020a7e7e6b92444bd607dae8c88a26
John Lin <jolin@mozilla.com>
2018-11-26 19:41:29 +0000

    Bug 1486659 - p2: expose native GL blitter to Java. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D11938

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurfaceTexture.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/GeckoSurfaceTexture.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.mozilla.gecko.annotation.WrapForJNI;
+import org.mozilla.gecko.mozglue.JNIObject;
 
 /* package */ final class GeckoSurfaceTexture extends SurfaceTexture {
     private static final String LOGTAG = "GeckoSurfaceTexture";
@@ -34,6 +35,8 @@ import org.mozilla.gecko.annotation.WrapForJNI;
     private GeckoSurfaceTexture.Callbacks mListener;
     private AtomicInteger mUseCount;
     private boolean mFinalized;
+
+    private NativeGLBlitHelper mBlitter;
 
     private GeckoSurfaceTexture(int handle) {
         super(0);
@@ -120,6 +123,9 @@ import org.mozilla.gecko.annotation.WrapForJNI;
 
     @Override
     public synchronized void release() {
+        if (mBlitter != null) {
+            mBlitter.disposeNative();
+        }
         try {
             super.release();
             synchronized (sSurfaceTextures) {
@@ -274,8 +280,28 @@ import org.mozilla.gecko.annotation.WrapForJNI;
         }
     }
 
+    /* package */ synchronized void configureSnapshot(GeckoSurface target, int width, int height) {
+        mBlitter = NativeGLBlitHelper.create(mHandle, target, width, height);
+    }
+
+    /* package */ synchronized void takeSnapshot() {
+        mBlitter.blit();
+    }
+
     public interface Callbacks {
         void onUpdateTexImage();
         void onReleaseTexImage();
+    }
+
+    @WrapForJNI
+    public static class NativeGLBlitHelper extends JNIObject {
+        public native static NativeGLBlitHelper create(int textureHandle,
+                                                       GeckoSurface targetSurface,
+                                                       int width,
+                                                       int height);
+        public native void blit();
+
+        @Override
+        protected native void disposeNative();
     }
 }
