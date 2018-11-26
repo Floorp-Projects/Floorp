c5d7ba98c6af87ec56c9860cb1317d645b83f5bb
Jon Coppeard <jcoppeard@mozilla.com>
2018-11-26 16:11:44 +0000

    Bug 1508672 - Enable passing test262 dynamic import tests r=jorendorff

--- js/src/tests/test262/language/expressions/dynamic-import/catch/nested-while-import-catch-eval-rqstd-abrupt-urierror.js
+++ js/src/tests/test262/language/expressions/dynamic-import/catch/nested-while-import-catch-eval-rqstd-abrupt-urierror.js
@@ -1,4 +1,3 @@
-// |reftest| skip -- dynamic-import is not supported
 // This file was procedurally generated from the following sources:
 // - src/dynamic-import/eval-rqstd-abrupt-urierror.case
 // - src/dynamic-import/catch/nested-while.template
