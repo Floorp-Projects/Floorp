9670d54612f2848dcd09c60fad9cea5099f522a5
Emilio Cobos Álvarez <emilio@crisal.io>
2018-11-27 19:52:13 +0000

    Bug 1509718 - Get rid of mBorderWidthTable. r=jwatt
    It has little to no use these days.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12857
    
    --HG--
    extra : moz-landing-system : lando

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -48,6 +48,8 @@ using namespace mozilla::dom;
 /* static */ const int32_t nsStyleGridLine::kMinLine;
 /* static */ const int32_t nsStyleGridLine::kMaxLine;
 
+static const nscoord kMediumBorderWidth = nsPresContext::CSSPixelsToAppUnits(3);
+
 // We set the size limit of style structs to 504 bytes so that when they
 // are allocated by Servo side with Arc, the total size doesn't exceed
 // 512 bytes, which minimizes allocator slop.
@@ -283,8 +285,7 @@ nsStyleBorder::nsStyleBorder(const nsPresContext* aContext)
     mBorderRadius.Set(corner, nsStyleCoord(0, nsStyleCoord::CoordConstructor));
   }
 
-  nscoord medium =
-    (StaticPresData::Get()->GetBorderWidthTable())[NS_STYLE_BORDER_WIDTH_MEDIUM];
+  nscoord medium = kMediumBorderWidth;
   NS_FOR_CSS_SIDES(side) {
     mBorderImageSlice.Set(side, nsStyleCoord(1.0f, eStyleUnit_Percent));
     mBorderImageWidth.Set(side, nsStyleCoord(1.0f, eStyleUnit_Factor));
@@ -445,8 +446,7 @@ nsStyleBorder::CalcDifference(const nsStyleBorder& aNewData) const
 }
 
 nsStyleOutline::nsStyleOutline(const nsPresContext* aContext)
-  : mOutlineWidth((StaticPresData::Get()
-                     ->GetBorderWidthTable())[NS_STYLE_BORDER_WIDTH_MEDIUM])
+  : mOutlineWidth(kMediumBorderWidth)
   , mOutlineOffset(0)
   , mOutlineColor(StyleComplexColor::CurrentColor())
   , mOutlineStyle(StyleBorderStyle::None)
@@ -668,8 +668,7 @@ nsStyleColumn::nsStyleColumn(const nsPresContext* aContext)
   : mColumnWidth(eStyleUnit_Auto)
   , mColumnRuleColor(StyleComplexColor::CurrentColor())
   , mColumnRuleStyle(StyleBorderStyle::None)
-  , mColumnRuleWidth((StaticPresData::Get()
-                        ->GetBorderWidthTable())[NS_STYLE_BORDER_WIDTH_MEDIUM])
+  , mColumnRuleWidth(kMediumBorderWidth)
   , mTwipsPerPixel(aContext->AppUnitsPerDevPixel())
 {
   MOZ_COUNT_CTOR(nsStyleColumn);
