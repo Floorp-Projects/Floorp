a339846a3667e908c6e9eefe120368c822017f67
Kartikaya Gupta <kgupta@mozilla.com>
2018-11-27 19:46:23 +0000

    Bug 1510026 - Send a sync IPC round-trip to the compositor after every crashtest. r=mattwoodrow
    This sends a sync IPC message to the compositor after each crashtest, to
    ensure that any stuff inflight in the compositor settles down. In
    particular this should round-trip through all of the relevant compositor
    threads (i.e. for WebRender it should ensure the scene builder, render
    backend, and renderer threads all get flushed).
    
    Differential Revision: https://phabricator.services.mozilla.com/D13033

--- layout/tools/reftest/reftest-content.js
+++ layout/tools/reftest/reftest-content.js
@@ -693,9 +693,7 @@ function WaitForTestEnd(contentRootElement, inPrintMode, spellCheckedElements) {
             };
             os.addObserver(flushWaiter, "apz-repaints-flushed");
 
-            var willSnapshot = (gCurrentTestType != TYPE_SCRIPT) &&
-                               (gCurrentTestType != TYPE_LOAD) &&
-                               (gCurrentTestType != TYPE_PRINT);
+            var willSnapshot = IsSnapshottableTestType();
             var noFlush =
                 !(contentRootElement &&
                   contentRootElement.classList.contains("reftest-no-flush"));
@@ -754,6 +752,17 @@ function WaitForTestEnd(contentRootElement, inPrintMode, spellCheckedElements) {
               }
               CheckLayerAssertions(contentRootElement);
             }
+
+            if (!IsSnapshottableTestType()) {
+              // If we're not snapshotting the test, at least do a sync round-trip
+              // to the compositor to ensure that all the rendering messages
+              // related to this test get processed. Otherwise problems triggered
+              // by this test may only manifest as failures in a later test.
+              LogInfo("MakeProgress: Doing sync flush to compositor");
+              gFailureReason = "timed out while waiting for sync compositor flush"
+              windowUtils().syncFlushCompositor();
+            }
+
             LogInfo("MakeProgress: Completed");
             state = STATE_COMPLETED;
             gFailureReason = "timed out while taking snapshot (bug in harness?)";
@@ -1094,14 +1103,19 @@ function LogInfo(str)
     }
 }
 
+function IsSnapshottableTestType()
+{
+    // Script, load-only, and PDF-print tests do not need any snapshotting.
+    return !(gCurrentTestType == TYPE_SCRIPT ||
+             gCurrentTestType == TYPE_LOAD ||
+             gCurrentTestType == TYPE_PRINT);
+}
+
 const SYNC_DEFAULT = 0x0;
 const SYNC_ALLOW_DISABLE = 0x1;
 function SynchronizeForSnapshot(flags)
 {
-    if (gCurrentTestType == TYPE_SCRIPT ||
-        gCurrentTestType == TYPE_LOAD ||
-        gCurrentTestType == TYPE_PRINT) {
-        // Script, load-only, and PDF-print tests do not need any snapshotting.
+    if (!IsSnapshottableTestType()) {
         return;
     }
 
