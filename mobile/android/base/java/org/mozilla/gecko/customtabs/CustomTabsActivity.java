29cc0db15f410eafabd37e329a61d6118b1fffae
Eugen Sawin <esawin@mozilla.com>
2018-11-27 20:30:47 +0000

    Bug 1509266 - [2.0] Update onContextMenu delegate implementations. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D12968
    
    --HG--
    extra : moz-landing-system : lando

--- mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
+++ mobile/android/base/java/org/mozilla/gecko/customtabs/CustomTabsActivity.java
@@ -695,11 +695,12 @@ public class CustomTabsActivity extends AppCompatActivity
     }
 
     @Override
-    public void onContextMenu(GeckoSession session, int screenX, int screenY,
-                              final String uri, int elementType,
-                              final String elementSrc) {
-
-        final String content = uri != null ? uri : elementSrc != null ? elementSrc : "";
+    public void onContextMenu(final GeckoSession session,
+                              int screenX, int screenY,
+                              final ContextElement element) {
+        final String content = element.linkUri != null
+                               ? element.linkUri
+                               : element.srcUri != null ? element.srcUri : "";
         final Uri validUri = WebApps.getValidURL(content);
         if (validUri == null) {
             return;
