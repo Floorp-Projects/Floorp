bd9afa5c32bec277708c07878ca170c516f07220
Jason Laster <jlaster@mozilla.com>
2018-11-27 18:02:34 -0500

    Bug 1510430 - Update Debugger Frontend v104. r=dwalsh

--- devtools/client/debugger/new/src/components/App.js
+++ devtools/client/debugger/new/src/components/App.js
@@ -242,6 +242,15 @@ class App extends Component<Props, State> {
     }));
   }
 
+  // Important so that the tabs chevron updates appropriately when
+  // the user resizes the left or right columns
+  triggerEditorPaneResize() {
+    const editorPane = window.document.querySelector(".editor-pane");
+    if (editorPane) {
+      editorPane.dispatchEvent(new Event("resizeend"));
+    }
+  }
+
   renderLayout = () => {
     const { startPanelCollapsed, endPanelCollapsed } = this.props;
     const horizontal = this.isHorizontal();
@@ -282,6 +291,7 @@ class App extends Component<Props, State> {
           />
         }
         endPanelCollapsed={endPanelCollapsed}
+        onResizeEnd={this.triggerEditorPaneResize}
       />
     );
   };
