0b51ecdd304fba08c6e65c34163ca67d039f0a1d
Tom Prince <mozilla@hocat.ca>
2018-11-27 18:47:14 +0000

    Bug 1482395: [mozrelease] Add support for generating update metadata; r=mtabara
    Differential Revision: https://phabricator.services.mozilla.com/D12707

---
+++ python/mozrelease/mozrelease/balrog.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+def _generate_show_url(context, entry):
+    url = entry['url']
+    return {
+        "actions": "showURL",
+        "openURL": url.format(**context),
+    }
+
+
+def _generate_product_details(context, entry):
+    url = entry['url']
+    return {
+        'detailsURL': url.format(**context),
+        'type': 'minor',
+    }
+
+
+_FIELD_TYPES = {
+    'show-url': _generate_show_url,
+    'product-details': _generate_product_details,
+}
+
+
+def _generate_conditions(context, entry):
+    if 'release-types' in entry and context['release-type'] not in entry['release-types']:
+        return None
+    if 'blob-types' in entry and context['blob-type'] not in entry['blob-types']:
+        return None
+    if 'products' in entry and context['product'] not in entry['products']:
+        return None
+
+    conditions = {}
+    if 'locales' in entry:
+        conditions['locales'] = entry['locales']
+    if 'versions' in entry:
+        conditions['versions'] = [
+            version.format(**context)
+            for version in entry['versions']
+        ]
+    if 'update-channel' in entry:
+        conditions['channels'] = [
+            entry['update-channel'] + suffix
+            for suffix in ('', '-localtest', '-cdntest')
+        ]
+    return conditions
+
+
+def generate_update_properties(context, config):
+    result = []
+    for entry in config:
+        fields = _FIELD_TYPES[entry['type']](context, entry)
+        conditions = _generate_conditions(context, entry.get('conditions', {}))
+
+        if conditions is not None:
+            result.append({
+                'fields': fields,
+                'for': conditions,
+            })
+    return result
---
+++ python/mozrelease/test/test_balrog.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import mozunit
+import pytest
+
+import json
+from mozrelease.util import load as yaml_load
+from pathlib2 import Path
+
+from mozrelease.balrog import generate_update_properties
+from mozilla_version.gecko import GeckoVersion
+
+DATA_PATH = Path(__file__).parent.joinpath('data')
+
+
+@pytest.mark.parametrize('context,config_file,output_file', [
+    ({
+        'release-type': 'release',
+        'product': 'firefox',
+        'version': GeckoVersion.parse('62.0.3'),
+    }, 'whatsnew-62.0.3.yml', 'Firefox-62.0.3.update.json'),
+    ({
+        'release-type': 'beta',
+        'product': 'firefox',
+        'version': GeckoVersion.parse('62.0b11'),
+    }, 'whatsnew-62.0.3.yml', 'Firefox-62.0b11.update.json'),
+])
+def test_update_properties(context, config_file, output_file):
+    with DATA_PATH.joinpath(config_file).open('r', encoding='utf-8') as f:
+        config = yaml_load(f)
+
+    update_line = generate_update_properties(context, config)
+
+    assert update_line == json.load(DATA_PATH.joinpath(output_file).open('r', encoding='utf-8'))
+
+
+if __name__ == '__main__':
+    mozunit.main()
