abef265350f7c144b666ba8f3087a5bf430f04b7
Julian Descottes <jdescottes@mozilla.com>
2018-11-27 12:53:26 +0000

    Bug 1492700 - Migrate AdbRuntime to a class;r=daisuke
    Depends on D12766
    
    Differential Revision: https://phabricator.services.mozilla.com/D12767

--- devtools/shared/adb/adb-runtime.js
+++ devtools/shared/adb/adb-runtime.js
@@ -7,72 +7,67 @@
 const { RuntimeTypes } = require("devtools/client/webide/modules/runtime-types");
 const { ADB } = require("devtools/shared/adb/adb");
 
-function AdbRuntime(adbDevice, model, socketPath) {
-  this._adbDevice = adbDevice;
-  this._model = model;
-  this._socketPath = socketPath;
-}
+class AdbRuntime {
+  constructor(adbDevice, model, socketPath) {
+    this.type = RuntimeTypes.USB;
 
-AdbRuntime.prototype = {
-  type: RuntimeTypes.USB,
+    this._adbDevice = adbDevice;
+    this._model = model;
+    this._socketPath = socketPath;
+  }
+
+  get id() {
+    return this._adbDevice.id + "|" + this._socketPath;
+  }
+
+  get deviceName() {
+    return this._model || this._adbDevice.id;
+  }
+
+  get shortName() {
+    return `Firefox ${this._channel()}`;
+  }
+
+  get name() {
+    return `Firefox ${this._channel()} on Android (${this.deviceName})`;
+  }
+
   connect(connection) {
     return ADB.prepareTCPConnection(this._socketPath).then(port => {
       connection.host = "localhost";
       connection.port = port;
       connection.connect();
     });
-  },
-  get id() {
-    return this._adbDevice.id + "|" + this._socketPath;
-  },
-};
-
-AdbRuntime.prototype._channel = function() {
-  const packageName = this._packageName();
-
-  switch (packageName) {
-    case "org.mozilla.firefox":
-      return "";
-    case "org.mozilla.firefox_beta":
-      return "Beta";
-    case "org.mozilla.fennec":
-    case "org.mozilla.fennec_aurora":
-      // This package name is now the one for Firefox Nightly distributed
-      // through the Google Play Store since "dawn project"
-      // cf. https://bugzilla.mozilla.org/show_bug.cgi?id=1357351#c8
-      return "Nightly";
-    default:
-      return "Custom";
   }
-};
 
-AdbRuntime.prototype._packageName = function() {
-  // If using abstract socket address, it is "@org.mozilla.firefox/..."
-  // If using path base socket, it is "/data/data/<package>...""
-  // Until Fennec 62 only supports path based UNIX domain socket, but
-  // Fennec 63+ supports both path based and abstract socket.
-  return this._socketPath.startsWith("@") ?
-    this._socketPath.substr(1).split("/")[0] :
-    this._socketPath.split("/")[3];
-};
+  _channel() {
+    const packageName = this._packageName();
 
-Object.defineProperty(AdbRuntime.prototype, "shortName", {
-  get() {
-    return `Firefox ${this._channel()}`;
-  },
-});
+    switch (packageName) {
+      case "org.mozilla.firefox":
+        return "";
+      case "org.mozilla.firefox_beta":
+        return "Beta";
+      case "org.mozilla.fennec":
+      case "org.mozilla.fennec_aurora":
+        // This package name is now the one for Firefox Nightly distributed
+        // through the Google Play Store since "dawn project"
+        // cf. https://bugzilla.mozilla.org/show_bug.cgi?id=1357351#c8
+        return "Nightly";
+      default:
+        return "Custom";
+    }
+  }
 
-Object.defineProperty(AdbRuntime.prototype, "deviceName", {
-  get() {
-    return this._model || this._adbDevice.id;
-  },
-});
-
-Object.defineProperty(AdbRuntime.prototype, "name", {
-  get() {
-    const channel = this._channel();
-    return "Firefox " + channel + " on Android (" + this.deviceName + ")";
-  },
-});
+  _packageName() {
+    // If using abstract socket address, it is "@org.mozilla.firefox/..."
+    // If using path base socket, it is "/data/data/<package>...""
+    // Until Fennec 62 only supports path based UNIX domain socket, but
+    // Fennec 63+ supports both path based and abstract socket.
+    return this._socketPath.startsWith("@") ?
+      this._socketPath.substr(1).split("/")[0] :
+      this._socketPath.split("/")[3];
+  }
+}
 
 exports.AdbRuntime = AdbRuntime;
