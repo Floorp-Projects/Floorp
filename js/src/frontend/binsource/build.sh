53da82eb9972d44ce5d687ade89285a1e78897e7
Tooru Fujisawa <arai_a@mac.com>
2018-11-29 01:03:45 +0900

    Bug 1508063 - Part 2: Move auto-generated enum to BinASTEnum.h. r=Yoric

--- js/src/frontend/binsource/build.sh
+++ js/src/frontend/binsource/build.sh
@@ -5,4 +5,5 @@ cargo run -- \
       ../BinSource.yaml \
       --out-class ../BinSource-auto.h    \
       --out-impl ../BinSource-auto.cpp   \
+      --out-enum ../BinASTEnum.h    \
       --out-token ../BinToken.h
