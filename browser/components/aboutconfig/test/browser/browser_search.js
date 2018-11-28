699dfee95a3cf69d304aa9b87c7cecba20b754b9
Csoregi Natalia <ncsoregi@mozilla.com>
2018-11-29 00:00:08 +0200

    Backed out changeset 4deb028f32d6 (bug 1497727) for eslint failure on browser_edit.js:44. CLOSED TREE

--- browser/components/aboutconfig/test/browser/browser_search.js
+++ browser/components/aboutconfig/test/browser/browser_search.js
@@ -37,11 +37,8 @@ add_task(async function test_search() {
     await ContentTask.spawn(browser, prefArray, aPrefArray => {
       let filteredPrefArray =
           aPrefArray.filter(pref => pref.includes("button"));
-      // Adding +1 to the list since button does not match an exact
-      // preference name then a row is added for the user to add a
-      // new button preference if desired
       Assert.equal(content.document.getElementById("prefs").childElementCount,
-                   filteredPrefArray.length + 1);
+                   filteredPrefArray.length);
 
       // Test empty search returns all preferences.
       let search = content.document.getElementById("search");
@@ -62,9 +59,8 @@ add_task(async function test_search() {
 
     EventUtils.sendKey("return");
     await ContentTask.spawn(browser, prefArray, aPrefArray => {
-      // Expecting 1 row to be returned since it offers the ability to add
       Assert.equal(content.document.getElementById("prefs").childElementCount,
-                   1);
+                   0);
 
       // Test added preferences search returns 2 preferences.
       let search = content.document.getElementById("search");
