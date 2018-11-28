05a6adee89628efc234b09ebc3d26da57dc9f74a
Tooru Fujisawa <arai_a@mac.com>
2018-11-29 01:03:46 +0900

    Bug 1508063 - Part 5: Move non-auto-generated part of BinASTParser into BinASTParserPerTokenizer. r=Yoric

--- js/src/jsapi-tests/testBinASTReader.cpp
+++ js/src/jsapi-tests/testBinASTReader.cpp
@@ -23,7 +23,7 @@
 
 #include "jsapi.h"
 
-#include "frontend/BinASTParserPerTokenizer.h"
+#include "frontend/BinASTParser.h"
 #include "frontend/FullParseHandler.h"
 #include "frontend/ParseContext.h"
 #include "frontend/Parser.h"
