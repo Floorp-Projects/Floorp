f2028b4c38bff2a50ed6aa1763f6dc5ee62b0cc4
Narcis Beleuzu <nbeleuzu@mozilla.com>
2018-11-28 11:57:10 +0200

    Backed out changeset dfacecb798cc (bug 1510430) for dt failures on browser_dbg-sourcemaps-bogus.js. a=backout

--- devtools/client/debugger/new/test/mochitest/browser_dbg-breakpoints-cond.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-breakpoints-cond.js
@@ -81,13 +81,4 @@ add_task(async function() {
   bp = findBreakpoint(dbg, "simple2", 5);
   is(bp.condition, "1", "breakpoint is created with the condition");
   assertEditorBreakpoint(dbg, 5, true);
-
-  const bpCondition = waitForDispatch(dbg, "SET_BREAKPOINT_CONDITION");
-  //right click breakpoint in breakpoints list
-  rightClickElement(dbg, "breakpointItem", 3)
-  // select "remove condition";
-  selectMenuItem(dbg, 8);
-  await bpCondition;
-  bp = findBreakpoint(dbg, "simple2", 5);
-  is(bp.condition, undefined, "breakpoint condition removed");
 });
