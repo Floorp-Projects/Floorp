75873c1b8a853ef63a10ec62a5bc7ead8cf8254a
Jan Varga <jan.varga@gmail.com>
2018-11-29 21:49:58 +0100

    Bug 1286798 - Part 51: Add tests for archive and shadow database clearing; r=asuth

--- dom/localstorage/test/unit/test_migration.js
+++ dom/localstorage/test/unit/test_migration.js
@@ -11,7 +11,16 @@ function* testSteps()
     { url: "http://localhost", attrs: {} },
     { url: "http://www.mozilla.org", attrs: {} },
     { url: "http://example.com", attrs: {} },
-    { url: "http://example.org", attrs: { userContextId: 5 } }
+    { url: "http://example.org", attrs: { userContextId: 5 } },
+
+    { url: "http://origin.test", attrs: {} },
+
+    { url: "http://prefix.test", attrs: {} },
+    { url: "http://prefix.test", attrs: { userContextId: 10 } },
+
+    { url: "http://pattern.test", attrs: { userContextId: 15 } },
+    { url: "http://pattern.test:8080", attrs: { userContextId: 15 } },
+    { url: "https://pattern.test", attrs: { userContextId: 15 } },
   ];
 
   const data = {
@@ -19,9 +28,34 @@ function* testSteps()
     value: "bar"
   };
 
+  function verifyData(clearedOrigins) {
+    info("Getting storages");
+
+    let storages = [];
+    for (let i = 0; i < principalInfos.length; i++) {
+      let principalInfo = principalInfos[i];
+      let principal = getPrincipal(principalInfo.url, principalInfo.attrs);
+      let storage = getLocalStorage(principal);
+      storages.push(storage);
+    }
+
+    info("Verifying data");
+
+    for (let i = 0; i < storages.length; i++) {
+      let value = storages[i].getItem(data.key + i);
+      if (clearedOrigins.includes(i)) {
+        is(value, null, "Correct value");
+      } else {
+        is(value, data.value + i, "Correct value");
+      }
+    }
+  }
+
   info("Setting pref");
 
   Services.prefs.setBoolPref("dom.storage.next_gen", true);
+
+  info("Stage 1 - Testing archived data migration");
 
   info("Clearing");
 
@@ -36,21 +70,60 @@ function* testSteps()
   // mach xpcshell-test --interactive dom/localstorage/test/unit/create_db.js
   installPackage("migration_profile");
 
-  info("Getting storages");
+  verifyData([]);
 
-  let storages = [];
-  for (let i = 0; i < principalInfos.length; i++) {
-    let principalInfo = principalInfos[i];
-    let principal = getPrincipal(principalInfo.url, principalInfo.attrs);
-    let storage = getLocalStorage(principal);
-    storages.push(storage);
-  }
+  info("Stage 2 - Testing archived data clearing");
 
-  info("Verifying data");
+  for (let type of ["origin", "prefix", "pattern"]) {
+    info("Clearing");
 
-  for (let i = 0; i < storages.length; i++) {
-    let value = storages[i].getItem(data.key + i);
-    is(value, data.value + i, "Correct value");
+    clear(continueToNextStepSync);
+    yield undefined;
+
+    info("Installing package");
+
+    // See the comment for the first installPackage() call.
+    installPackage("migration_profile");
+
+    let clearedOrigins = [];
+
+    switch (type) {
+      case "origin": {
+        let principal = getPrincipal("http://origin.test", {});
+        clearOrigin(principal, "default", continueToNextStepSync);
+        yield undefined;
+
+        clearedOrigins.push(4);
+
+        break;
+      }
+
+      case "prefix": {
+        let principal = getPrincipal("http://prefix.test", {});
+        clearOriginsByPrefix(principal, "default", continueToNextStepSync);
+        yield undefined;
+
+        clearedOrigins.push(5, 6);
+
+        break;
+      }
+
+      case "pattern": {
+        clearOriginsByPattern(JSON.stringify({ userContextId: 15 }),
+                              continueToNextStepSync);
+        yield undefined;
+
+        clearedOrigins.push(7, 8, 9);
+
+        break;
+      }
+
+      default: {
+        throw("Unknown type: " + type);
+      }
+    }
+
+    verifyData(clearedOrigins);
   }
 
   finishTest();
