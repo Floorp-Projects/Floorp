b98cdb66ff6758c1432b916c4a54a9b65ed7713a
Jan Varga <jan.varga@gmail.com>
2018-11-29 21:47:34 +0100

    Bug 1286798 - Part 7: Teach QuotaManager's OriginParser to handle file://UNIVERSAL_FILE_URI_ORIGIN; r=asuth
    See bug 1340710 which introduced this kind of URI.

---
+++ dom/quota/test/unit/test_specialOrigins.js
@@ -0,0 +1,44 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+async function testSteps()
+{
+  const origins = [
+    {
+      path: "storage/default/file+++UNIVERSAL_FILE_URI_ORIGIN",
+      url: "file:///Test/test.html",
+      persistence: "default"
+    },
+  ];
+
+  info("Setting pref");
+
+  SpecialPowers.setBoolPref("security.fileuri.strict_origin_policy", false);
+
+  info("Creating origin directories");
+
+  for (let origin of origins) {
+    let originDir = getRelativeFile(origin.path);
+    originDir.create(Ci.nsIFile.DIRECTORY_TYPE, parseInt("0755", 8));
+  }
+
+  info("Initializing origin directories");
+
+  for (let origin of origins) {
+    let result;
+
+    try {
+      let request = initOrigin(getPrincipal(origin.url),
+                               origin.persistence);
+      result = await requestFinished(request);
+
+      ok(true, "Should not have thrown");
+    } catch(ex) {
+      ok(false, "Should not have thrown");
+    }
+
+    ok(!result, "Origin directory wasn't created");
+  }
+}
