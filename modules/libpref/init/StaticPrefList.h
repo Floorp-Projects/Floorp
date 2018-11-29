6d2981f66290c9ba316ce047b56d68d27e063ce7
Masayuki Nakano <masayuki@d-toybox.com>
2018-11-29 15:18:17 +0000

    Bug 1496288 - part 2: Stop dispatching non-printable "keypress" events, set keyCode or charCode value to the other value when it's zero, start to dispatch "keydown" and "keyup" events during composition, and enable window.event by default r=smaug
    First one is for bug 968056.  UI Events declares that "keypress" events should
    be dispatched only when a key press produces one or more characters (and also
    discussing about Enter key press for backward compatibility).  Therefore, we
    should stop dispatching "keypress" events for non-printable keys (like arrow keys,
    Ctrl + A, etc) in the default group on content for conforming to UI Events, but
    keep dispatching them in chrome and in the system group on content.
    
    Next one is for bug 1479964.  No spec declares that what value should be set
    to keyCode and charCode value of "keypress" events because declaring that would
    break existing web apps which handle them with UA string.  However, some web
    apps assume that if window.event is available, keyCode value of "keypress" event
    is set to a Unicode code point of inputting character.  We set only charCode
    to it, but the other browsers sets both keyCode and charCode to a Unicode
    code point of inputting character.  Therefore, we need to follow the other
    browsers' behavior for this because changing value from zero to non-zero is
    safer than non-zero to non-zero or zero.
    
    And also next one is for bug 354358.  UI Events declares that "keydown" and
    "keyup" events represent physical key state and should be fired even during
    composition.  As far as the reported issues, this behavior is expected mainly
    by Korean web developers and some web apps depend on the behavior.  Therefore,
    we need to start to dispatch them during composition.
    
    Finally, for bug 218415, we should enable window.event.  This is declared by
    the Living DOM Standard.  A lot of web apps depend on window.event and the
    last blocker, bug 1479964 was fixed so that it is the time to enable this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13376
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -205,17 +205,11 @@ VARCACHE_PREF(
 
 // If this is true, "keypress" event's keyCode value and charCode value always
 // become same if the event is not created/initialized by JS.
-#ifdef NIGHTLY_BUILD
-# define PREF_VALUE  true
-#else
-# define PREF_VALUE  false
-#endif
 VARCACHE_PREF(
   "dom.keyboardevent.keypress.set_keycode_and_charcode_to_same_value",
    dom_keyboardevent_keypress_set_keycode_and_charcode_to_same_value,
-  bool, PREF_VALUE
+  bool, true
 )
-#undef PREF_VALUE
 
 // NOTE: This preference is used in unit tests. If it is removed or its default
 // value changes, please update test_sharedMap_var_caches.js accordingly.
