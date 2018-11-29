34ec32bc757857460103073e0ffe19319540bafb
Paolo Amadini <paolo.mozmail@amadzone.org>
2018-11-29 15:12:24 +0000

    Bug 1502856 - Fix intermittent failure surfaced by test-verify. r=me
    Just waiting for l10n.ready isn't enough, so the test now verifies the button class instead of its text content.

--- browser/components/aboutconfig/test/browser/browser_locked.js
+++ browser/components/aboutconfig/test/browser/browser_locked.js
@@ -42,28 +42,28 @@ add_task(async function test_locked() {
       let lockedPref = getRow("browser.search.searchEnginesURL");
       Assert.ok(lockedPref.classList.contains("locked"));
       Assert.equal(getValue("browser.search.searchEnginesURL"), "https://addons.mozilla.org/%LOCALE%/firefox/search-engines/");
-      Assert.equal(getButton("browser.search.searchEnginesURL").textContent, "Edit");
+      Assert.ok(getButton("browser.search.searchEnginesURL").classList.contains("button-edit"));
       Assert.equal(getButton("browser.search.searchEnginesURL").disabled, true);
 
       // Test locked default boolean pref.
       lockedPref = getRow("accessibility.AOM.enabled");
       Assert.ok(lockedPref.classList.contains("locked"));
       Assert.equal(getValue("accessibility.AOM.enabled"), "false");
-      Assert.equal(getButton("accessibility.AOM.enabled").textContent, "Toggle");
+      Assert.ok(getButton("accessibility.AOM.enabled").classList.contains("button-toggle"));
       Assert.equal(getButton("accessibility.AOM.enabled").disabled, true);
 
       // Test locked user added pref.
       lockedPref = getRow("test.aboutconfig.a");
       Assert.ok(lockedPref.classList.contains("locked"));
       Assert.equal(getValue("test.aboutconfig.a"), "");
-      Assert.equal(getButton("test.aboutconfig.a").textContent, "Edit");
+      Assert.ok(getButton("test.aboutconfig.a").classList.contains("button-edit"));
       Assert.equal(getButton("test.aboutconfig.a").disabled, true);
 
       // Test pref not locked
       let unlockedPref = getRow("accessibility.indicator.enabled");
       Assert.equal(unlockedPref.classList.contains("locked"), false);
       Assert.equal(getValue("accessibility.indicator.enabled"), "false");
-      Assert.equal(getButton("accessibility.indicator.enabled").textContent, "Toggle");
+      Assert.ok(getButton("accessibility.indicator.enabled").classList.contains("button-toggle"));
       Assert.equal(getButton("accessibility.indicator.enabled").disabled, false);
     });
   });
