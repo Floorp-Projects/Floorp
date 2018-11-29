3e8d91818f00b11655cf0d038a103722842eecec
Eugen Sawin <esawin@mozilla.com>
2018-11-29 22:06:32 +0000

    Bug 1511012 - [1.0] Ensure non-null scheme before accessing it. r=droeh
    Differential Revision: https://phabricator.services.mozilla.com/D13455

--- mobile/android/base/java/org/mozilla/gecko/webapps/WebApps.java
+++ mobile/android/base/java/org/mozilla/gecko/webapps/WebApps.java
@@ -73,6 +73,9 @@ public final class WebApps {
             return null;
         }
         final String scheme = uri.getScheme();
+        if (scheme == null) {
+            return null;
+        }
         // currently we only support http and https to open in Firefox
         if (scheme.equals("http") || scheme.equals("https")) {
             return uri;
