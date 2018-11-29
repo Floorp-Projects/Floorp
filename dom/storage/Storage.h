b968ceb8137387dae0a19db0f93d8f8080d97fbf
Jan Varga <jan.varga@gmail.com>
2018-11-29 21:48:11 +0100

    Bug 1286798 - Part 18: Verify that data is persisted on disk; r=asuth,mrbkap
    New methods open() and close() are added to the Storage WebIDL interface. They are only available when a pref is set and are only intended for testing. There's also a new method resetStoragesForPrincipal() which is used as a callback for close() since datastores don't release directory locks immediately. resetStoragesForPrincipal() requests an exclusive lock for given origin, so it must wait for any exising shared locks to be released.

--- dom/storage/Storage.h
+++ dom/storage/Storage.h
@@ -110,6 +110,14 @@ public:
 
   bool IsSessionOnly() const { return mIsSessionOnly; }
 
+  virtual void
+  Open(nsIPrincipal& aSubjectPrincipal, ErrorResult& aRv)
+  { }
+
+  virtual void
+  Close(nsIPrincipal& aSubjectPrincipal, ErrorResult& aRv)
+  { }
+
   // aStorage can be null if this method is called by LocalStorageCacheChild.
   //
   // aImmediateDispatch is for use by child IPC code (LocalStorageCacheChild)
