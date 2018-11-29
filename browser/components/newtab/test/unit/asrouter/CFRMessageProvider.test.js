3a06037f251d5a14a833558f490accfd83310066
Ursula Sarracini <ursulasarracini@gmail.com>
2018-11-29 17:43:06 +0000

    Bug 1510678 - Change recommended addons for Firefox 64 CFR release. r=k88hudson	a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D13437

--- browser/components/newtab/test/unit/asrouter/CFRMessageProvider.test.js
+++ browser/components/newtab/test/unit/asrouter/CFRMessageProvider.test.js
@@ -5,41 +5,22 @@ const REGULAR_IDS = [
   "FACEBOOK_CONTAINER",
   "GOOGLE_TRANSLATE",
   "YOUTUBE_ENHANCE",
-  "WIKIPEDIA_CONTEXT_MENU_SEARCH",
-  "REDDIT_ENHANCEMENT",
+  // These are excluded for now.
+  // "WIKIPEDIA_CONTEXT_MENU_SEARCH",
+  // "REDDIT_ENHANCEMENT",
 ];
 
 describe("CFRMessageProvider", () => {
-  it("should have a total of 12 messages", () => {
-    assert.lengthOf(messages, 12);
+  it("should have a total of 3 messages", () => {
+    assert.lengthOf(messages, 3);
   });
-  it("should two variants for each of the five regular addons", () => {
+  it("should have one message each for the three regular addons", () => {
     for (const id of REGULAR_IDS) {
-      const cohort1 = messages.find(msg => msg.id === `${id}_1`);
-      assert.ok(cohort1, `contains one day cohort for ${id}`);
-      assert.deepEqual(cohort1.frequency, {lifetime: 1}, "one day cohort has the right frequency cap");
-      assert.include(cohort1.targeting, `(providerCohorts.cfr in ["one_per_day", "nightly"])`);
-
       const cohort3 = messages.find(msg => msg.id === `${id}_3`);
       assert.ok(cohort3, `contains three day cohort for ${id}`);
       assert.deepEqual(cohort3.frequency, {lifetime: 3}, "three day cohort has the right frequency cap");
       assert.include(cohort3.targeting, `(providerCohorts.cfr == "three_per_day")`);
-
-      assert.deepEqual(cohort1.content, cohort3.content, "cohorts should have the same content");
     }
-  });
-  it("should have the two amazon cohorts", () => {
-    const cohort1 = messages.find(msg => msg.id === `AMAZON_ASSISTANT_1`);
-    const cohort3 = messages.find(msg => msg.id === `AMAZON_ASSISTANT_3`);
-    assert.deepEqual(cohort1.content, cohort3.content, "cohorts should have the same content");
-
-    assert.ok(cohort1, `contains one day cohort for amazon`);
-    assert.deepEqual(cohort1.frequency, {lifetime: 1}, "one day cohort has the right frequency cap");
-    assert.include(cohort1.targeting, `(providerCohorts.cfr == "one_per_day_amazon"`);
-
-    assert.ok(cohort3, `contains three day cohort for amazon`);
-    assert.deepEqual(cohort3.frequency, {lifetime: 3}, "three day cohort has the right frequency cap");
-    assert.include(cohort3.targeting, `(providerCohorts.cfr == "three_per_day_amazon")`);
   });
   it("should always have xpinstallEnabled as targeting if it is an addon", () => {
     for (const message of messages) {
