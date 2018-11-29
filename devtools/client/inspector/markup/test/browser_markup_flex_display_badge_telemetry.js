eb222629612f2fb4b9c7a2bf8526336b2e277db8
Gabriel Luong <gabriel.luong@gmail.com>
2018-11-29 18:45:43 -0500

    Bug 1509907 - Add telemetry to track flexbox highlighter usage. r=miker
    This also adds a telemetry count for the grid highlighter being turned on by the
    markup view, which was not in place when we added the telemetry for the grid
    highlighter.

---
+++ devtools/client/inspector/markup/test/browser_markup_flex_display_badge_telemetry.js
@@ -0,0 +1,48 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that the telemetry is correct when the flexbox highlighter is activated from
+// the markup view.
+
+const TEST_URI = `
+  <style type="text/css">
+    #flex {
+      display: flex;
+    }
+  </style>
+  <div id="flex"></div>
+`;
+
+add_task(async function() {
+  await pushPref("devtools.inspector.flexboxHighlighter.enabled", true);
+  await pushPref("devtools.flexboxinspector.enabled", true);
+  await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
+  startTelemetry();
+  const { inspector } = await openLayoutView();
+  const { highlighters } = inspector;
+
+  await selectNode("#flex", inspector);
+  const flexContainer = await getContainerForSelector("#flex", inspector);
+  const flexDisplayBadge = flexContainer.elt.querySelector(
+    ".inspector-badge.interactive[data-display]");
+
+  info("Toggling ON the flexbox highlighter from the flex display badge.");
+  const onHighlighterShown = highlighters.once("flexbox-highlighter-shown");
+  flexDisplayBadge.click();
+  await onHighlighterShown;
+
+  info("Toggling OFF the flexbox highlighter from the flex display badge.");
+  const onHighlighterHidden = highlighters.once("flexbox-highlighter-hidden");
+  flexDisplayBadge.click();
+  await onHighlighterHidden;
+
+  checkResults();
+});
+
+function checkResults() {
+  checkTelemetry("devtools.markup.flexboxhighlighter.opened", "", 1, "scalar");
+  checkTelemetry("DEVTOOLS_FLEXBOX_HIGHLIGHTER_TIME_ACTIVE_SECONDS", "", null,
+    "hasentries");
+}
