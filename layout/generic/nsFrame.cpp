ddde052adac0c98329d299fa61d53ead969d6694
Emilio Cobos Álvarez <emilio@crisal.io>
2018-11-30 06:19:21 +0000

    Bug 1510942 - Fix the position of the caret when the caret frame is a reference frame. r=mattwoodrow
    This is consistent with what we do for non-stacking-contexts. The
    GetCaretFrame() == child check may be able to go away, I'm not sure, so I've
    left it for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13384
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -3182,6 +3182,7 @@ nsIFrame::BuildDisplayListForStackingContext(nsDisplayListBuilder* aBuilder,
     aBuilder->Check();
     BuildDisplayList(aBuilder, set);
     aBuilder->Check();
+    aBuilder->DisplayCaret(this, set.Content());
 
     // Blend modes are a real pain for retained display lists. We build a blend
     // container item if the built list contains any blend mode items within
@@ -3856,7 +3857,7 @@ nsIFrame::BuildDisplayListForChild(nsDisplayListBuilder*   aBuilder,
     nsDisplayListBuilder::AutoContainerASRTracker contASRTracker(aBuilder);
     child->BuildDisplayListForStackingContext(aBuilder, &list, &canSkipWrapList);
     wrapListASR = contASRTracker.GetContainerASR();
-    if (aBuilder->DisplayCaret(child, &list)) {
+    if (aBuilder->GetCaretFrame() == child) {
       canSkipWrapList = false;
     }
   } else {
