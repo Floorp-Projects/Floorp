f49bdd6863879d9741ad83269f8c08a5d975495b
Johann Hofmann <jhofmann@mozilla.com>
2018-11-30 21:57:46 +0000

    Bug 1501992 - Add tests for the cookies subpanel in the control center. r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D13146

--- browser/base/content/test/trackingUI/browser_trackingUI_categories.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_categories.js
@@ -16,9 +16,7 @@ registerCleanupFunction(function() {
 
 add_task(async function testCategoryLabelsInControlPanel() {
   await BrowserTestUtils.withNewTab("http://www.example.com", async function() {
-    let promisePanelOpen = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "popupshown");
-    gIdentityHandler._identityBox.click();
-    await promisePanelOpen;
+    await openIdentityPopup();
 
     let preferencesButton = document.getElementById("tracking-protection-preferences-button");
     ok(preferencesButton.label, "The preferencesButton label exists");
@@ -40,6 +38,58 @@ add_task(async function testCategoryLabelsInControlPanel() {
       gNavigatorBundle.getString("contentBlocking.category.custom"));
     is(preferencesButton.label, gNavigatorBundle.getString("contentBlocking.category.custom"),
       "The preferencesButton label has been changed to custom");
+  });
+});
+
+add_task(async function testSubcategoryLabels() {
+  await BrowserTestUtils.withNewTab("http://www.example.com", async function() {
+    let categoryLabel =
+      document.getElementById("identity-popup-content-blocking-tracking-protection-state-label");
+
+    Services.prefs.setBoolPref(TP_PREF, true);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.trackers.blocked.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.trackers.blocked.label"));
+
+    Services.prefs.setBoolPref(TP_PREF, false);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.trackers.allowed.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.trackers.allowed.label"));
+
+    categoryLabel =
+      document.getElementById("identity-popup-content-blocking-cookies-state-label");
+
+    Services.prefs.setIntPref(TPC_PREF, Ci.nsICookieService.BEHAVIOR_ACCEPT);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.cookies.allowed.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.cookies.allowed.label"));
+
+    Services.prefs.setIntPref(TPC_PREF, Ci.nsICookieService.BEHAVIOR_REJECT);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.cookies.allBlocked.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.cookies.allBlocked.label"));
+
+    Services.prefs.setIntPref(TPC_PREF, Ci.nsICookieService.BEHAVIOR_REJECT_FOREIGN);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.cookies.3rdPartyBlocked.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.cookies.3rdPartyBlocked.label"));
+
+    Services.prefs.setIntPref(TPC_PREF, Ci.nsICookieService.BEHAVIOR_REJECT_TRACKER);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.cookies.trackersBlocked.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.cookies.trackersBlocked.label"));
+
+    Services.prefs.setIntPref(TPC_PREF, Ci.nsICookieService.BEHAVIOR_LIMIT_FOREIGN);
+    await TestUtils.waitForCondition(() => categoryLabel.textContent ==
+      gNavigatorBundle.getString("contentBlocking.cookies.unvisitedBlocked.label"),
+      "The category label has updated correctly");
+    is(categoryLabel.textContent, gNavigatorBundle.getString("contentBlocking.cookies.unvisitedBlocked.label"));
   });
 });
 
