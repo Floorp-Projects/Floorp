bfddcfb70e6311c4730f78378fe5ad0e782ae645
Henri Sivonen <hsivonen@hsivonen.fi>
2018-11-30 07:42:26 +0000

    Bug 1509507 - Update to encoding_rs 0.8.13 to fix a panic in UTF-8 to UTF-16 decode. r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D13253
    
    --HG--
    extra : moz-landing-system : lando

--- third_party/rust/encoding_rs/src/lib.rs
+++ third_party/rust/encoding_rs/src/lib.rs
@@ -15,7 +15,7 @@
         new_ret_no_self
     )
 )]
-#![doc(html_root_url = "https://docs.rs/encoding_rs/0.8.12")]
+#![doc(html_root_url = "https://docs.rs/encoding_rs/0.8.13")]
 
 //! encoding_rs is a Gecko-oriented Free Software / Open Source implementation
 //! of the [Encoding Standard](https://encoding.spec.whatwg.org/) in Rust.
