8b38ca84c8330c603e385a993fac0368eb883fc2
Michael Froman <mfroman@mozilla.com>
2018-11-30 00:46:17 +0000

    Bug 1452146 - flip av1 and rdd on for OSX and Win. r=drno
    Differential Revision: https://phabricator.services.mozilla.com/D13482

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -1103,11 +1103,17 @@ VARCACHE_PREF(
 )
 #undef PREF_VALUE
 
+#if defined(XP_WIN) || defined(XP_MACOSX)
+# define PREF_VALUE true
+#else
+# define PREF_VALUE false
+#endif
 VARCACHE_PREF(
   "media.rdd-process.enabled",
    MediaRddProcessEnabled,
-  RelaxedAtomicBool, false
+  RelaxedAtomicBool, PREF_VALUE
 )
+#undef PREF_VALUE
 
 VARCACHE_PREF(
   "media.rdd-process.startup_timeout_ms",
@@ -1418,11 +1424,17 @@ VARCACHE_PREF(
 )
 
 // AV1
+#if defined(XP_WIN) || defined(XP_MACOSX)
+# define PREF_VALUE true
+#else
+# define PREF_VALUE false
+#endif
 VARCACHE_PREF(
   "media.av1.enabled",
    MediaAv1Enabled,
-  RelaxedAtomicBool, false
+  RelaxedAtomicBool, PREF_VALUE
 )
+#undef PREF_VALUE
 VARCACHE_PREF(
   "media.av1.use-dav1d",
    MediaAv1UseDav1d,
