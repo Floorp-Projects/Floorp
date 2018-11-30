587eb415a6a5120a9c08964173bf3f93b035eb3e
Erica Wright <ewright@mozilla.com>
2018-11-30 14:30:18 +0000

    Bug 1501980 - Add a mode label to the Content Blocking section in the main menu. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D13170

--- browser/base/content/test/trackingUI/browser_trackingUI_categories.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_categories.js
@@ -4,6 +4,8 @@ const TP_PB_PREF = "privacy.trackingprotection.pbmode.enabled";
 const TPC_PREF = "network.cookie.cookieBehavior";
 const TT_PREF = "urlclassifier.trackingTable";
 
+ChromeUtils.import("resource://testing-common/CustomizableUITestUtils.jsm", this);
+
 registerCleanupFunction(function() {
   Services.prefs.clearUserPref(TP_PREF);
   Services.prefs.clearUserPref(TP_PB_PREF);
@@ -38,5 +40,33 @@ add_task(async function testCategoryLabelsInControlPanel() {
       gNavigatorBundle.getString("contentBlocking.category.custom"));
     is(preferencesButton.label, gNavigatorBundle.getString("contentBlocking.category.custom"),
       "The preferencesButton label has been changed to custom");
+  });
+});
+
+add_task(async function testCategoryLabelsInAppMenu() {
+  await BrowserTestUtils.withNewTab("http://www.example.com", async function() {
+    let cuiTestUtils = new CustomizableUITestUtils(window);
+    await cuiTestUtils.openMainMenu();
+
+    let appMenuCategoryLabel = document.getElementById("appMenu-tp-category");
+    ok(appMenuCategoryLabel.value, "The appMenuCategory label exists");
+
+    Services.prefs.setStringPref(CAT_PREF, "strict");
+    await TestUtils.waitForCondition(() => appMenuCategoryLabel.value ==
+      gNavigatorBundle.getString("contentBlocking.category.strict"));
+    is(appMenuCategoryLabel.value, gNavigatorBundle.getString("contentBlocking.category.strict"),
+      "The appMenuCategory label has been changed to strict");
+
+    Services.prefs.setStringPref(CAT_PREF, "standard");
+    await TestUtils.waitForCondition(() => appMenuCategoryLabel.value ==
+      gNavigatorBundle.getString("contentBlocking.category.standard"));
+    is(appMenuCategoryLabel.value, gNavigatorBundle.getString("contentBlocking.category.standard"),
+      "The appMenuCategory label has been changed to standard");
+
+    Services.prefs.setStringPref(CAT_PREF, "custom");
+    await TestUtils.waitForCondition(() => appMenuCategoryLabel.value ==
+      gNavigatorBundle.getString("contentBlocking.category.custom"));
+    is(appMenuCategoryLabel.value, gNavigatorBundle.getString("contentBlocking.category.custom"),
+      "The appMenuCategory label has been changed to custom");
   });
 });
