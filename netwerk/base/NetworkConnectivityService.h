30fb81d78d2b9cbb8219763c8327b4bf163ddfc2
Valentin Gosu <valentin.gosu@gmail.com>
2018-12-02 23:29:27 +0000

    Bug 1502025 - Use captive portal endpoints for connectivity checks r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D13334

--- netwerk/base/NetworkConnectivityService.h
+++ netwerk/base/NetworkConnectivityService.h
@@ -12,12 +12,15 @@ namespace net {
 
 class NetworkConnectivityService : public nsINetworkConnectivityService,
                                    public nsIObserver,
-                                   public nsIDNSListener {
+                                   public nsIDNSListener,
+                                   public nsIStreamListener {
  public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSINETWORKCONNECTIVITYSERVICE
   NS_DECL_NSIOBSERVER
   NS_DECL_NSIDNSLISTENER
+  NS_DECL_NSISTREAMLISTENER
+  NS_DECL_NSIREQUESTOBSERVER
 
   nsresult Init();
   static already_AddRefed<NetworkConnectivityService> GetSingleton();
@@ -32,11 +35,17 @@ class NetworkConnectivityService : public nsINetworkConnectivityService,
   // Will be set to OK if the DNS request returned in IP of this type,
   //                NOT_AVAILABLE if that type of resolution is not available
   //                UNKNOWN if the check wasn't performed
-  int32_t mDNSv4 = nsINetworkConnectivityService::UNKNOWN;
-  int32_t mDNSv6 = nsINetworkConnectivityService::UNKNOWN;
+  ConnectivityState mDNSv4 = nsINetworkConnectivityService::UNKNOWN;
+  ConnectivityState mDNSv6 = nsINetworkConnectivityService::UNKNOWN;
+
+  ConnectivityState mIPv4 = nsINetworkConnectivityService::UNKNOWN;
+  ConnectivityState mIPv6 = nsINetworkConnectivityService::UNKNOWN;
 
   nsCOMPtr<nsICancelable> mDNSv4Request;
   nsCOMPtr<nsICancelable> mDNSv6Request;
+
+  nsCOMPtr<nsIChannel> mIPv4Channel;
+  nsCOMPtr<nsIChannel> mIPv6Channel;
 };
 
 }  // namespace net
