df8d44c957ccb9377c1a71c2c0b1988f474455b4
Emilio Cobos Álvarez <emilio@crisal.io>
2018-12-02 13:46:06 -0500

    Bug 1511811 - Use https for the MPL license URL.
    Cherry-picks https://github.com/servo/servo/pull/22198

--- servo/support/gecko/nsstring/src/lib.rs
+++ servo/support/gecko/nsstring/src/lib.rs
@@ -1,6 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 //! This module provides rust bindings for the XPCOM string types.
 //!
