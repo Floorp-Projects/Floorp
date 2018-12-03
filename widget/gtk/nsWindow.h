fcdee6dedb2d21d2341df81f1de61cc57f7dc5b5
Martin Stransky <stransky@redhat.com>
2018-12-03 12:48:52 +0000

    Bug 1491808 - [Linux/CSD/Titlebar] Force toplevel window repaint when titlebar rendering is enabled and its state changes, r=bzbarsky
    Titlebar (StyleAppearance::MozWindowTitlebar) style depends on toplevel window focus
    and we need to redraw it when toplevel window focus changes.
    
    Unfortunately according to https://gitlab.gnome.org/GNOME/gtk/issues/1395
    the toplevel window focus can't be used here as we can have active but unfocused
    toplevel window during drag & drop.
    
    Gtk+ controls window active appearance by window-state-event signal
    but gecko uses focus-in/out signals, so we need to repaint the titlebar
    when window-state-event comes *after* focus-in/out signals.
    
    We can't call mWidgetListener->WindowActivated() (and WindowDeactivated())
    as it was already called from focus-in/out handlers before window-state-event.
    
    Depends on D13051
    
    Differential Revision: https://phabricator.services.mozilla.com/D13052
    
    --HG--
    extra : moz-landing-system : lando

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -625,6 +625,8 @@ class nsWindow final : public nsBaseWidget {
 
   virtual int32_t RoundsWidgetCoordinatesTo() override;
 
+  void ForceTitlebarRedraw();
+
   /**
    * |mIMContext| takes all IME related stuff.
    *
