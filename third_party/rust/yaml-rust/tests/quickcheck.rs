7241d208b23f365a9bc7d2c5399fd7fc770be83d
Alex Gaynor <agaynor@mozilla.com>
2018-12-03 19:23:36 +0000

    Bug 1510272 - upgraded yaml-rust to 0.4.2; r=ted
    This resolves a cargo audit warning in yaml-rust<4.1.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13067

--- third_party/rust/yaml-rust/tests/quickcheck.rs
+++ third_party/rust/yaml-rust/tests/quickcheck.rs
@@ -3,19 +3,20 @@ extern crate yaml_rust;
 extern crate quickcheck;
 
 use quickcheck::TestResult;
-use yaml_rust::{Yaml, YamlLoader, YamlEmitter};
 use std::error::Error;
+use yaml_rust::{Yaml, YamlEmitter, YamlLoader};
 
 quickcheck! {
     fn test_check_weird_keys(xs: Vec<String>) -> TestResult {
         let mut out_str = String::new();
+        let input = Yaml::Array(xs.into_iter().map(Yaml::String).collect());
         {
             let mut emitter = YamlEmitter::new(&mut out_str);
-            emitter.dump(&Yaml::Array(xs.into_iter().map(Yaml::String).collect())).unwrap();
+            emitter.dump(&input).unwrap();
         }
-        if let Err(err) = YamlLoader::load_from_str(&out_str) {
-            return TestResult::error(err.description());
+        match YamlLoader::load_from_str(&out_str) {
+            Ok(output) => TestResult::from_bool(output.len() == 1 && input == output[0]),
+            Err(err) => TestResult::error(err.description()),
         }
-        TestResult::passed()
     }
 }
