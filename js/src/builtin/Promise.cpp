42465c5f51f16792ae5a2b3c10d05ddfe3928274
Jason Orendorff <jorendorff@mozilla.com>
2018-12-04 19:54:05 +0000

    Bug 1510598 - Handle uncatchable errors in RejectPromiseWithPendingError. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D13434

--- js/src/builtin/Promise.cpp
+++ js/src/builtin/Promise.cpp
@@ -3430,9 +3430,16 @@ static bool OriginalPromiseThenBuiltin(JSContext* cx, HandleValue promiseVal,
 
 MOZ_MUST_USE bool js::RejectPromiseWithPendingError(
     JSContext* cx, Handle<PromiseObject*> promise) {
-  // Not much we can do about uncatchable exceptions, just bail.
+  if (!cx->isExceptionPending()) {
+    // Reject the promise, but also propagate this uncatchable error.
+    mozilla::Unused << PromiseObject::reject(cx, promise, UndefinedHandleValue);
+    return false;
+  }
+
   RootedValue exn(cx);
-  if (!GetAndClearException(cx, &exn)) return false;
+  if (!GetAndClearException(cx, &exn)) {
+    return false;
+  }
   return PromiseObject::reject(cx, promise, exn);
 }
 
