1aa689d92158e7b3cfdff6aa5d611064937782d3
Kris Maglione <maglione.k@gmail.com>
2018-12-05 16:02:30 -0500

    Bug 1505522: Part 3 - Remove unused getHeapAllocatedAsync method. r=erahm
    Differential Revision: https://phabricator.services.mozilla.com/D13873

--- xpcom/base/nsMemoryReporterManager.cpp
+++ xpcom/base/nsMemoryReporterManager.cpp
@@ -2404,49 +2404,6 @@ nsMemoryReporterManager::GetHeapAllocated(int64_t* aAmount) {
 #endif
 }
 
-NS_IMETHODIMP
-nsMemoryReporterManager::GetHeapAllocatedAsync(
-    nsIHeapAllocatedCallback* aCallback) {
-#ifdef HAVE_JEMALLOC_STATS
-  if (!mThreadPool) {
-    return NS_ERROR_UNEXPECTED;
-  }
-
-  RefPtr<nsIMemoryReporterManager> self{this};
-  nsMainThreadPtrHandle<nsIHeapAllocatedCallback> mainThreadCallback(
-      new nsMainThreadPtrHolder<nsIHeapAllocatedCallback>(
-          "HeapAllocatedCallback", aCallback));
-
-  nsCOMPtr<nsIRunnable> getHeapAllocatedRunnable = NS_NewRunnableFunction(
-      "nsMemoryReporterManager::GetHeapAllocatedAsync",
-      [self, mainThreadCallback]() mutable {
-        MOZ_ASSERT(!NS_IsMainThread());
-
-        int64_t heapAllocated = 0;
-        nsresult rv = self->GetHeapAllocated(&heapAllocated);
-
-        nsCOMPtr<nsIRunnable> resultCallbackRunnable = NS_NewRunnableFunction(
-            "nsMemoryReporterManager::GetHeapAllocatedAsync",
-            [mainThreadCallback, heapAllocated, rv]() mutable {
-              MOZ_ASSERT(NS_IsMainThread());
-
-              if (NS_FAILED(rv)) {
-                mainThreadCallback->Callback(0);
-                return;
-              }
-
-              mainThreadCallback->Callback(heapAllocated);
-            });  // resultCallbackRunnable.
-
-        Unused << NS_DispatchToMainThread(resultCallbackRunnable);
-      });  // getHeapAllocatedRunnable.
-
-  return mThreadPool->Dispatch(getHeapAllocatedRunnable, NS_DISPATCH_NORMAL);
-#else
-  return NS_ERROR_NOT_AVAILABLE;
-#endif
-}
-
 // This has UNITS_PERCENTAGE, so it is multiplied by 100x.
 NS_IMETHODIMP
 nsMemoryReporterManager::GetHeapOverheadFraction(int64_t* aAmount) {
