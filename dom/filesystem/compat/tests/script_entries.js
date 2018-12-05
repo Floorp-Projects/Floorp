be50d1484e31dd8d73f288299b07570299e17905
Adam Pucciano <aspucciano@myseneca.ca>
2018-12-05 02:24:58 +0000

    Bug 1508989 - Enable ESLint for dom/filesystem / (automatic changes) r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D13770

--- dom/filesystem/compat/tests/script_entries.js
+++ dom/filesystem/compat/tests/script_entries.js
@@ -2,41 +2,41 @@ Cu.importGlobalProperties(["File", "Directory"]);
 
 var tmpFile, tmpDir;
 
-addMessageListener("entries.open", function (e) {
+addMessageListener("entries.open", function(e) {
   tmpFile = Cc["@mozilla.org/file/directory_service;1"]
               .getService(Ci.nsIDirectoryService)
               .QueryInterface(Ci.nsIProperties)
-              .get('TmpD', Ci.nsIFile)
-  tmpFile.append('file.txt');
+              .get("TmpD", Ci.nsIFile);
+  tmpFile.append("file.txt");
   tmpFile.createUnique(Ci.nsIFile.FILE_TYPE, 0o600);
 
   tmpDir = Cc["@mozilla.org/file/directory_service;1"]
               .getService(Ci.nsIDirectoryService)
               .QueryInterface(Ci.nsIProperties)
-              .get('TmpD', Ci.nsIFile)
+              .get("TmpD", Ci.nsIFile);
 
-  tmpDir.append('dir-test');
+  tmpDir.append("dir-test");
   tmpDir.createUnique(Ci.nsIFile.DIRECTORY_TYPE, 0o700);
 
   var file1 = tmpDir.clone();
-  file1.append('foo.txt');
+  file1.append("foo.txt");
   file1.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600);
 
   var dir1 = tmpDir.clone();
-  dir1.append('subdir');
+  dir1.append("subdir");
   dir1.create(Ci.nsIFile.DIRECTORY_TYPE, 0o700);
 
   var file2 = dir1.clone();
-  file2.append('bar..txt'); // Note the double ..
+  file2.append("bar..txt"); // Note the double ..
   file2.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0o600);
 
   var dir2 = dir1.clone();
-  dir2.append('subsubdir');
+  dir2.append("subsubdir");
   dir2.create(Ci.nsIFile.DIRECTORY_TYPE, 0o700);
 
   File.createFromNsIFile(tmpFile).then(function(file) {
     sendAsyncMessage("entries.opened", {
-      data: [ new Directory(tmpDir.path), file ]
+      data: [ new Directory(tmpDir.path), file ],
     });
   });
 });
