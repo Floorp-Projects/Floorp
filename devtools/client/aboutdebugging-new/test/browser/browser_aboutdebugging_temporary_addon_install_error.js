c3afb85995136dc0e6b47f429ffdf81721c6f9c6
Julian Descottes <jdescottes@mozilla.com>
2018-12-07 15:59:03 +0000

    Bug 1488500 - Add test for temporary extension install error;r=daisuke
    Depends on D12571
    
    Differential Revision: https://phabricator.services.mozilla.com/D12572

---
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_temporary_addon_install_error.js
@@ -0,0 +1,46 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* import-globals-from head-addons-script.js */
+
+"use strict";
+
+// Load addons helpers
+Services.scriptloader.loadSubScript(CHROME_URL_ROOT + "head-addons-script.js", this);
+
+/**
+ * Test that the installation error messages are displayed when installing temporary
+ * extensions.
+ */
+
+const BAD_EXTENSION_PATH = "resources/bad-extension/manifest.json";
+const EXTENSION_PATH = "resources/test-temporary-extension/manifest.json";
+const EXTENSION_NAME = "test-temporary-extension";
+
+add_task(async function() {
+  const { document, tab } = await openAboutDebugging();
+
+  info("Install a bad extension");
+  // Do not use installTemporaryAddon here since the install will fail.
+  prepareMockFilePicker(BAD_EXTENSION_PATH);
+  document.querySelector(".js-temporary-extension-install-button").click();
+
+  info("Wait until the install error message appears");
+  await waitUntil(() => document.querySelector(".js-error-message"));
+  const installError = document.querySelector(".js-error-message");
+  ok(installError.textContent.includes("JSON.parse: unexpected keyword"),
+    "The expected installation error is displayed: " + installError.textContent);
+
+  info("Install a valid extension to make the message disappear");
+  await installTemporaryExtension(EXTENSION_PATH, EXTENSION_NAME, document);
+
+  info("Wait until the error message disappears");
+  await waitUntil(() => !document.querySelector(".js-error-message"));
+
+  info("Wait for the temporary addon to be displayed as a debug target");
+  await waitUntil(() => findDebugTargetByText(EXTENSION_NAME, document));
+
+  await removeTemporaryExtension(EXTENSION_NAME, document);
+
+  await removeTab(tab);
+});
