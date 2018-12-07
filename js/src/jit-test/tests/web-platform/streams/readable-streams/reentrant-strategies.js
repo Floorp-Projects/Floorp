9fa1c39ef39eb346f894c2a3d1af09c945e57207
Jason Orendorff <jorendorff@mozilla.com>
2018-12-07 20:04:08 +0000

    Bug 1512050 - Add not-yet-fully-passing web-platform tests about readable streams to jit-test anyway. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D13750

---
+++ js/src/jit-test/tests/web-platform/streams/readable-streams/reentrant-strategies.js
@@ -0,0 +1,4 @@
+// |jit-test| error: ReferenceError: WritableStream is not defined
+
+load(libdir + "web-platform-testharness.js");
+load(libdir + "../../../../testing/web-platform/tests/streams/readable-streams/reentrant-strategies.js");
