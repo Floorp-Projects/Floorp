831f070701df5980148f09f17ab9320f338d11d5
Yura Zenevich <yura.zenevich@gmail.com>
2018-12-10 20:43:49 +0000

    Bug 1434888 - set accessibility panel command key to Shift + F10. r=gl
    MozReview-Commit-ID: HkL5dDfo0p9
    
    Differential Revision: https://phabricator.services.mozilla.com/D14095

--- devtools/startup/devtools-startup.js
+++ devtools/startup/devtools-startup.js
@@ -170,6 +170,12 @@ XPCOMUtils.defineLazyGetter(this, "KeyShortcuts", function() {
       shortcut: KeyShortcutsBundle.GetStringFromName("dom.commandkey"),
       modifiers,
     },
+    // Key for opening the Accessibility Panel
+    {
+      toolId: "accessibility",
+      shortcut: KeyShortcutsBundle.GetStringFromName("accessibility.commandkey"),
+      modifiers: "shift",
+    },
   ];
 
   if (isMac) {
