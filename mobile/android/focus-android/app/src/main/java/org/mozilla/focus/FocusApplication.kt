50c11b1c4e63118f8334c7635266f6d57f522f4c
Emily Kager <ekager@mozilla.com>
2018-12-10 14:18:57 -0800

    [focus] RunBlocking Timeout not working

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/FocusApplication.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/FocusApplication.kt
@@ -7,10 +7,13 @@ package org.mozilla.focus
 
 import android.os.StrictMode
 import android.support.v7.preference.PreferenceManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeoutOrNull
 import mozilla.components.service.fretboard.Fretboard
 import mozilla.components.service.fretboard.source.kinto.KintoExperimentSource
@@ -33,9 +36,14 @@ import org.mozilla.focus.utils.StethoWrapper
 import org.mozilla.focus.web.CleanupSessionObserver
 import org.mozilla.focus.web.WebViewProvider
 import java.io.File
+import kotlin.coroutines.CoroutineContext
 
-class FocusApplication : LocaleAwareApplication() {
+class FocusApplication : LocaleAwareApplication(), CoroutineScope {
     lateinit var fretboard: Fretboard
+
+    private var job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 
     companion object {
         private const val FRETBOARD_BLOCKING_NETWORK_READ_TIMEOUT = 10000L
@@ -56,44 +64,47 @@ class FocusApplication : LocaleAwareApplication() {
 
         PreferenceManager.setDefaultValues(this, R.xml.settings, false)
 
-        runBlocking {
-            loadExperiments()
+        loadExperiments()
 
-            enableStrictMode()
+        enableStrictMode()
 
-            components.searchEngineManager.apply {
-                launch(IO) {
-                    load(this@FocusApplication)
-                }
-
-                registerForLocaleUpdates(this@FocusApplication)
+        components.searchEngineManager.apply {
+            launch(IO) {
+                load(this@FocusApplication)
             }
 
-            TelemetryWrapper.init(this@FocusApplication)
-            AdjustHelper.setupAdjustIfNeeded(this@FocusApplication)
-
-            visibilityLifeCycleCallback = VisibilityLifeCycleCallback(this@FocusApplication)
-            registerActivityLifecycleCallbacks(visibilityLifeCycleCallback)
-
-            components.sessionManager.apply {
-                register(NotificationSessionObserver(this@FocusApplication))
-                register(TelemetrySessionObserver())
-                register(CleanupSessionObserver(this@FocusApplication))
-            }
+            registerForLocaleUpdates(this@FocusApplication)
         }
+
+        TelemetryWrapper.init(this@FocusApplication)
+        AdjustHelper.setupAdjustIfNeeded(this@FocusApplication)
+
+        visibilityLifeCycleCallback = VisibilityLifeCycleCallback(this@FocusApplication)
+        registerActivityLifecycleCallbacks(visibilityLifeCycleCallback)
+
+        components.sessionManager.apply {
+            register(NotificationSessionObserver(this@FocusApplication))
+            register(TelemetrySessionObserver())
+            register(CleanupSessionObserver(this@FocusApplication))
+        }
+
+        async(IO) { updateExperiments() }
     }
 
-    private suspend fun loadExperiments() = coroutineScope {
+    private fun loadExperiments() {
         val experimentsFile = File(filesDir, EXPERIMENTS_JSON_FILENAME)
         val experimentSource = KintoExperimentSource(
-                EXPERIMENTS_BASE_URL, EXPERIMENTS_BUCKET_NAME, EXPERIMENTS_COLLECTION_NAME)
+            EXPERIMENTS_BASE_URL, EXPERIMENTS_BUCKET_NAME, EXPERIMENTS_COLLECTION_NAME
+        )
         fretboard = Fretboard(experimentSource, FlatFileExperimentStorage(experimentsFile))
         fretboard.loadExperiments()
         WebViewProvider.determineEngine(this@FocusApplication)
-        launch(IO) {
-            withTimeoutOrNull(FRETBOARD_BLOCKING_NETWORK_READ_TIMEOUT) {
-                fretboard.updateExperiments() // then update disk and memory from the network
-            }
+    }
+
+    private suspend fun updateExperiments() = coroutineScope {
+        val updateExperimentsAsync = async { fretboard.updateExperiments() }
+        withTimeoutOrNull(FRETBOARD_BLOCKING_NETWORK_READ_TIMEOUT) {
+            updateExperimentsAsync.await() // then update disk and memory from the network
         }
     }
 
