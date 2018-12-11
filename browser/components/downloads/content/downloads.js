c4770d1073e6fde4c7934a0c7da5d0aa6f7f18bc
Paolo Amadini <paolo.mozmail@amadzone.org>
2018-12-11 18:27:01 +0000

    Bug 1513149 - Hovering the action buttons doesn't display the command associated with the button. r=Gijs, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D14172

--- browser/components/downloads/content/downloads.js
+++ browser/components/downloads/content/downloads.js
@@ -824,30 +824,35 @@ var DownloadsView = {
    * Mouse listeners to handle selection on hover.
    */
   onDownloadMouseOver(aEvent) {
-    if (aEvent.originalTarget.classList.contains("downloadButton")) {
-      aEvent.target.classList.add("downloadHoveringButton");
+    let item = aEvent.target.closest("richlistitem,richlistbox");
+    if (item.localName != "richlistitem") {
+      return;
     }
-    if (!(this.contextMenuOpen || this.subViewOpen) &&
-        aEvent.target.parentNode == this.richListBox) {
-      this.richListBox.selectedItem = aEvent.target;
+
+    if (aEvent.target.classList.contains("downloadButton")) {
+      item.classList.add("downloadHoveringButton");
+    }
+
+    if (!this.contextMenuOpen && !this.subViewOpen) {
+      this.richListBox.selectedItem = item;
     }
   },
 
   onDownloadMouseOut(aEvent) {
-    if (aEvent.originalTarget.classList.contains("downloadButton")) {
-      aEvent.target.classList.remove("downloadHoveringButton");
+    let item = aEvent.target.closest("richlistitem,richlistbox");
+    if (item.localName != "richlistitem") {
+      return;
     }
-    if (!(this.contextMenuOpen || this.subViewOpen) &&
-        aEvent.target.parentNode == this.richListBox) {
-      // If the destination element is outside of the richlistitem, clear the
-      // selection.
-      let element = aEvent.relatedTarget;
-      while (element && element != aEvent.target) {
-        element = element.parentNode;
-      }
-      if (!element) {
-        this.richListBox.selectedIndex = -1;
-      }
+
+    if (aEvent.target.classList.contains("downloadButton")) {
+      item.classList.remove("downloadHoveringButton");
+    }
+
+    // If the destination element is outside of the richlistitem, clear the
+    // selection.
+    if (!this.contextMenuOpen && !this.subViewOpen &&
+        !item.contains(aEvent.relatedTarget)) {
+      this.richListBox.selectedIndex = -1;
     }
   },
 
