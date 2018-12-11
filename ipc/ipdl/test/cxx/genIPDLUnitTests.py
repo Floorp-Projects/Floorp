79c450dbdb5c4b2677372a062325ca2ba883225c
Alex Gaynor <agaynor@mozilla.com>
2018-12-11 18:12:22 +0000

    Bug 1513073 - make the IPDL compiler's code python3 syntax friendly; r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D14102
    
    --HG--
    extra : moz-landing-system : lando

--- ipc/ipdl/test/cxx/genIPDLUnitTests.py
+++ ipc/ipdl/test/cxx/genIPDLUnitTests.py
@@ -2,12 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import print_function
+
 import string
 import sys
 
 
 def usage():
-    print >>sys.stderr, """
+    print("""
 %s template_file -t unit_tests... -e extra_protocols...
 
   TEMPLATE_FILE is used to generate to generate the unit-tester .cpp
@@ -15,7 +17,7 @@ def usage():
   EXTRA_PROTOCOLS are top-level protocols for subprocesses that can be
                   spawned in tests but are not unit tests in and of
                   themselves
-""" % (sys.argv[0])
+""" % (sys.argv[0]), file=sys.stderr)
     sys.exit(1)
 
 
