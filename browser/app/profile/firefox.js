2fc1ce8fc59c9b981d492f0c347fea575d81e37d
Mathieu Leplatre <mathieu@mozilla.com>
2018-12-11 16:10:21 +0000

    Bug 1506175 - Fetch recipes from Remote Settings r=mythmon,Gijs
    Instead of obtaining the recipes from the Normandy server, obtain them from RemoteSettings
    
    Differential Revision: https://phabricator.services.mozilla.com/D11490

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1742,6 +1742,7 @@ pref("app.normandy.first_run", true);
 pref("app.normandy.logging.level", 50); // Warn
 pref("app.normandy.run_interval_seconds", 21600); // 6 hours
 pref("app.normandy.shieldLearnMoreUrl", "https://support.mozilla.org/1/firefox/%VERSION%/%OS%/%LOCALE%/shield");
+pref("app.normandy.remotesettings.enabled", false);
 #ifdef MOZ_DATA_REPORTING
 pref("app.shield.optoutstudies.enabled", true);
 #else
