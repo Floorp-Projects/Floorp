6448b30e9cda5e39e0d02cf0654dd5f544d404e0
Alexandre Poirot <poirot.alex@gmail.com>
2018-12-11 15:19:43 +0000

    Bug 1512220 - Refactor debugger test using testGlobal to threadClientTest helper. r=yulia
    This isn't removing listTabs calls as these tests were using attachTestTab,
    but all these tests are using testGlobal helper whereas they don't have to if they were using threadClientTest helper.
    
    MozReview-Commit-ID: FPtYlDvYUeD
    
    Depends on D13900
    
    Differential Revision: https://phabricator.services.mozilla.com/D13901
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/tests/unit/test_reattach-thread.js
+++ devtools/server/tests/unit/test_reattach-thread.js
@@ -7,49 +7,13 @@
  * Test that reattaching to a previously detached thread works.
  */
 
-var gClient, gDebuggee, gThreadClient, gTargetFront;
-
-function run_test() {
-  initTestDebuggerServer();
-  gDebuggee = testGlobal("test-reattach");
-  DebuggerServer.addTestGlobal(gDebuggee);
-
-  const transport = DebuggerServer.connectPipe();
-  gClient = new DebuggerClient(transport);
-  gClient.connect().then(() => {
-    attachTestTab(gClient, "test-reattach", (reply, targetFront) => {
-      gTargetFront = targetFront;
-      test_attach();
-    });
-  });
-  do_test_pending();
-}
-
-function test_attach() {
-  gTargetFront.attachThread({}).then(([response, threadClient]) => {
-    Assert.equal(threadClient.state, "paused");
-    gThreadClient = threadClient;
-    threadClient.resume(test_detach);
-  });
-}
-
-function test_detach() {
-  gThreadClient.detach(() => {
-    Assert.equal(gThreadClient.state, "detached");
-    Assert.equal(gTargetFront.thread, null);
-    test_reattach();
-  });
-}
-
-function test_reattach() {
-  gTargetFront.attachThread({}).then(([response, threadClient]) => {
-    Assert.notEqual(gThreadClient, threadClient);
-    Assert.equal(threadClient.state, "paused");
-    Assert.equal(gTargetFront.thread, threadClient);
-    threadClient.resume(cleanup);
-  });
-}
-
-function cleanup() {
-  gClient.close().then(do_test_finished);
-}
+add_task(threadClientTest(async ({ threadClient, debuggee, client, targetFront }) => {
+  await threadClient.detach();
+  Assert.equal(threadClient.state, "detached");
+  Assert.equal(targetFront.thread, null);
+  const [, newThreadClient] = await targetFront.attachThread({});
+  Assert.notEqual(threadClient, newThreadClient);
+  Assert.equal(newThreadClient.state, "paused");
+  Assert.equal(targetFront.thread, newThreadClient);
+  await newThreadClient.resume();
+}));
