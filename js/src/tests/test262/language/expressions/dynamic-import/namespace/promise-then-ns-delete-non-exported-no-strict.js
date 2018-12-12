8b69fe3ff4e44e924dca2dcdf6e0b97be0c36782
André Bargull <andre.bargull@gmail.com>
2018-12-12 08:45:47 -0800

    Bug 1512989 - Part 2: Fix browser jstests failures. r=jorendorff

--- js/src/tests/test262/language/expressions/dynamic-import/namespace/promise-then-ns-delete-non-exported-no-strict.js
+++ js/src/tests/test262/language/expressions/dynamic-import/namespace/promise-then-ns-delete-non-exported-no-strict.js
@@ -1,3 +1,4 @@
+// |reftest| skip-if(!xulRuntime.shell) -- dynamic-import is not enabled unconditionally
 // This file was procedurally generated from the following sources:
 // - src/dynamic-import/ns-delete-non-exported-no-strict.case
 // - src/dynamic-import/namespace/promise.template
