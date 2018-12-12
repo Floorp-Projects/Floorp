b8e53cf46b66466508a8946b6f14f7e0ca45d8a0
Andrea Marchesini <amarchesini@mozilla.com>
2018-12-12 01:00:05 +0100

    Bug 1459264 - Allow just 1 FilePicker per event, r=smaug

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -3292,6 +3292,9 @@ class nsContentUtils {
 
   static PopupControlState GetPopupControlState() { return sPopupControlState; }
 
+  static void PopupStatePusherCreated();
+  static void PopupStatePusherDestroyed();
+
   // Get a serial number for a newly created inner or outer window.
   static uint32_t InnerOrOuterWindowCreated();
   // Record that an inner or outer window has been destroyed.
@@ -3301,7 +3304,14 @@ class nsContentUtils {
     return sInnerOrOuterWindowCount;
   }
 
-  static bool CanShowPopup(nsIPrincipal* aPrincipal);
+  // This method checks if the principal is allowed by open popups by user
+  // permissions. In this case, the caller should not block popups.
+  static bool CanShowPopupByPermission(nsIPrincipal* aPrincipal);
+
+  // This method returns true if the caller is allowed to show a popup, and it
+  // consumes the popup token for the current event. There is just 1 popup
+  // allowed per event.
+  static bool TryUsePopupOpeningToken();
 
   /**
    * Serializes a JSON-like JS::Value into a string.
@@ -3495,6 +3505,11 @@ class nsContentUtils {
   static mozilla::LazyLogModule sDOMDumpLog;
 
   static PopupControlState sPopupControlState;
+  static uint32_t sPopupStatePusherCount;
+
+  // This token is by default set to false. When a popup/filePicker is shown, it
+  // is set to true.
+  static bool sUnusedPopupToken;
 
   static int32_t sInnerOrOuterWindowCount;
   static uint32_t sInnerOrOuterWindowSerialCounter;
