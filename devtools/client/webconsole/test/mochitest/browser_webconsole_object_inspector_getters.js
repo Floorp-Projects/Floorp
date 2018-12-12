a4efb35861ee2f84177c3ed821899f28b4409a84
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-12-12 14:50:16 +0000

    Bug 1513505 - Fix invoke getter on prototype's property; r=jdescottes.
    This lands the fix done in the debugger Reps
    for ObjectInspector (https://github.com/devtools-html/debugger.html/pull/7484\),
    and add a test to ensure we don't regress this.
    We take this as an opportunity to put some
    object inspector helpers in head.js so we
    don't repeat ourselves too much.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14240
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/test/mochitest/browser_webconsole_object_inspector_getters.js
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_object_inspector_getters.js
@@ -60,8 +60,8 @@ add_task(async function() {
   const node = await waitFor(() => findMessage(hud, "oi-test"));
   const oi = node.querySelector(".tree");
 
-  expandNode(oi);
-  await waitFor(() => getOiNodes(oi).length > 1);
+  expandObjectInspectorNode(oi);
+  await waitFor(() => getObjectInspectorNodes(oi).length > 1);
 
   await testStringGetter(oi);
   await testNumberGetter(oi);
@@ -76,267 +76,227 @@ add_task(async function() {
 });
 
 async function testStringGetter(oi) {
-  let node = getOiNode(oi, "myStringGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myStringGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myStringGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myStringGetter")));
 
-  node = getOiNode(oi, "myStringGetter");
+  node = findObjectInspectorNode(oi, "myStringGetter");
   ok(node.textContent.includes(`myStringGetter: "hello"`),
     "String getter now has the expected text content");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
 }
 
 async function testNumberGetter(oi) {
-  let node = getOiNode(oi, "myNumberGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myNumberGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myNumberGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myNumberGetter")));
 
-  node = getOiNode(oi, "myNumberGetter");
+  node = findObjectInspectorNode(oi, "myNumberGetter");
   ok(node.textContent.includes(`myNumberGetter: 123`),
     "Number getter now has the expected text content");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
 }
 
 async function testUndefinedGetter(oi) {
-  let node = getOiNode(oi, "myUndefinedGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myUndefinedGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myUndefinedGetter")));
+  await waitFor(() => !getObjectInspectorInvokeGetterButton(
+    findObjectInspectorNode(oi, "myUndefinedGetter")));
 
-  node = getOiNode(oi, "myUndefinedGetter");
+  node = findObjectInspectorNode(oi, "myUndefinedGetter");
   ok(node.textContent.includes(`myUndefinedGetter: undefined`),
     "undefined getter now has the expected text content");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
 }
 
 async function testNullGetter(oi) {
-  let node = getOiNode(oi, "myNullGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myNullGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myNullGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myNullGetter")));
 
-  node = getOiNode(oi, "myNullGetter");
+  node = findObjectInspectorNode(oi, "myNullGetter");
   ok(node.textContent.includes(`myNullGetter: null`),
     "null getter now has the expected text content");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
 }
 
 async function testObjectGetter(oi) {
-  let node = getOiNode(oi, "myObjectGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myObjectGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myObjectGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myObjectGetter")));
 
-  node = getOiNode(oi, "myObjectGetter");
+  node = findObjectInspectorNode(oi, "myObjectGetter");
   ok(node.textContent.includes(`myObjectGetter: Object { foo: "bar" }`),
     "object getter now has the expected text content");
-  is(nodeIsExpandable(node), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(node), true, "The node can be expanded");
 
-  expandNode(node);
-  await waitFor(() => getChildrenNodes(node).length > 0);
+  expandObjectInspectorNode(node);
+  await waitFor(() => getObjectInspectorChildrenNodes(node).length > 0);
   checkChildren(node, [`foo: "bar"`, `<prototype>`]);
 }
 
 async function testArrayGetter(oi) {
-  let node = getOiNode(oi, "myArrayGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myArrayGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myArrayGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myArrayGetter")));
 
-  node = getOiNode(oi, "myArrayGetter");
+  node = findObjectInspectorNode(oi, "myArrayGetter");
   ok(node.textContent.includes(
     `myArrayGetter: Array(1000) [ 0, 1, 2, ${ELLIPSIS} ]`),
     "Array getter now has the expected text content - ");
-  is(nodeIsExpandable(node), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(node), true, "The node can be expanded");
 
-  expandNode(node);
-  await waitFor(() => getChildrenNodes(node).length > 0);
-  const children = getChildrenNodes(node);
+  expandObjectInspectorNode(node);
+  await waitFor(() => getObjectInspectorChildrenNodes(node).length > 0);
+  const children = getObjectInspectorChildrenNodes(node);
 
   const firstBucket = children[0];
   ok(firstBucket.textContent.includes(`[0${ELLIPSIS}99]`), "Array has buckets");
 
-  is(nodeIsExpandable(firstBucket), true, "The bucket can be expanded");
-  expandNode(firstBucket);
-  await waitFor(() => getChildrenNodes(firstBucket).length > 0);
+  is(isObjectInspectorNodeExpandable(firstBucket), true, "The bucket can be expanded");
+  expandObjectInspectorNode(firstBucket);
+  await waitFor(() => getObjectInspectorChildrenNodes(firstBucket).length > 0);
   checkChildren(firstBucket, Array.from({length: 100}, (_, i) => `${i}: ${i}`));
 }
 
 async function testMapGetter(oi) {
-  let node = getOiNode(oi, "myMapGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myMapGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myMapGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myMapGetter")));
 
-  node = getOiNode(oi, "myMapGetter");
+  node = findObjectInspectorNode(oi, "myMapGetter");
   ok(node.textContent.includes(`myMapGetter: Map`),
     "map getter now has the expected text content");
-  is(nodeIsExpandable(node), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(node), true, "The node can be expanded");
 
-  expandNode(node);
-  await waitFor(() => getChildrenNodes(node).length > 0);
+  expandObjectInspectorNode(node);
+  await waitFor(() => getObjectInspectorChildrenNodes(node).length > 0);
   checkChildren(node, [`size`, `<entries>`, `<prototype>`]);
 
-  const entriesNode = getOiNode(oi, "<entries>");
-  expandNode(entriesNode);
-  await waitFor(() => getChildrenNodes(entriesNode).length > 0);
+  const entriesNode = findObjectInspectorNode(oi, "<entries>");
+  expandObjectInspectorNode(entriesNode);
+  await waitFor(() => getObjectInspectorChildrenNodes(entriesNode).length > 0);
   // "→" character. Need to be instantiated this way for the test to pass.
   const arrow = String.fromCodePoint(8594);
   checkChildren(entriesNode, [`foo ${arrow} Object { ${ELLIPSIS} }`]);
 
-  const entryNode = getChildrenNodes(entriesNode)[0];
-  expandNode(entryNode);
-  await waitFor(() => getChildrenNodes(entryNode).length > 0);
+  const entryNode = getObjectInspectorChildrenNodes(entriesNode)[0];
+  expandObjectInspectorNode(entryNode);
+  await waitFor(() => getObjectInspectorChildrenNodes(entryNode).length > 0);
   checkChildren(entryNode, [`<key>: "foo"`, `<value>: Object { ${ELLIPSIS} }`]);
 }
 
 async function testProxyGetter(oi) {
-  let node = getOiNode(oi, "myProxyGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myProxyGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myProxyGetter")));
+  await waitFor(() =>
+    !getObjectInspectorInvokeGetterButton(findObjectInspectorNode(oi, "myProxyGetter")));
 
-  node = getOiNode(oi, "myProxyGetter");
+  node = findObjectInspectorNode(oi, "myProxyGetter");
   ok(node.textContent.includes(`myProxyGetter: Proxy`),
     "proxy getter now has the expected text content");
-  is(nodeIsExpandable(node), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(node), true, "The node can be expanded");
 
-  expandNode(node);
-  await waitFor(() => getChildrenNodes(node).length > 0);
+  expandObjectInspectorNode(node);
+  await waitFor(() => getObjectInspectorChildrenNodes(node).length > 0);
   checkChildren(node, [`<target>`, `<handler>`]);
 
-  const targetNode = getOiNode(oi, "<target>");
-  expandNode(targetNode);
-  await waitFor(() => getChildrenNodes(targetNode).length > 0);
+  const targetNode = findObjectInspectorNode(oi, "<target>");
+  expandObjectInspectorNode(targetNode);
+  await waitFor(() => getObjectInspectorChildrenNodes(targetNode).length > 0);
   checkChildren(targetNode, [`a: 1`, `<prototype>`]);
 
-  const handlerNode = getOiNode(oi, "<handler>");
-  expandNode(handlerNode);
-  await waitFor(() => getChildrenNodes(handlerNode).length > 0);
+  const handlerNode = findObjectInspectorNode(oi, "<handler>");
+  expandObjectInspectorNode(handlerNode);
+  await waitFor(() => getObjectInspectorChildrenNodes(handlerNode).length > 0);
   checkChildren(handlerNode, [`get:`, `<prototype>`]);
 }
 
 async function testThrowingGetter(oi) {
-  let node = getOiNode(oi, "myThrowingGetter");
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  let node = findObjectInspectorNode(oi, "myThrowingGetter");
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
-  await waitFor(() => !getInvokeGetterButton(getOiNode(oi, "myThrowingGetter")));
+  await waitFor(() => !getObjectInspectorInvokeGetterButton(
+    findObjectInspectorNode(oi, "myThrowingGetter")));
 
-  node = getOiNode(oi, "myThrowingGetter");
+  node = findObjectInspectorNode(oi, "myThrowingGetter");
   ok(node.textContent.includes(`myThrowingGetter: Error: "myError"`),
     "throwing getter does show the error");
-  is(nodeIsExpandable(node), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(node), true, "The node can be expanded");
 
-  expandNode(node);
-  await waitFor(() => getChildrenNodes(node).length > 0);
+  expandObjectInspectorNode(node);
+  await waitFor(() => getObjectInspectorChildrenNodes(node).length > 0);
   checkChildren(node,
     [`columnNumber`, `fileName`, `lineNumber`, `message`, `stack`, `<prototype>`]);
 }
 
 async function testLongStringGetter(oi, longString) {
-  const getLongStringNode = () => getOiNode(oi, "myLongStringGetter");
+  const getLongStringNode = () => findObjectInspectorNode(oi, "myLongStringGetter");
   const node = getLongStringNode();
-  is(nodeIsExpandable(node), false, "The node can't be expanded");
-  const invokeButton = getInvokeGetterButton(node);
+  is(isObjectInspectorNodeExpandable(node), false, "The node can't be expanded");
+  const invokeButton = getObjectInspectorInvokeGetterButton(node);
   ok(invokeButton, "There is an invoke button as expected");
 
   invokeButton.click();
   await waitFor(() =>
     getLongStringNode().textContent.includes(`myLongStringGetter: "ab ab`));
   ok(true, "longstring getter shows the initial text");
-  is(nodeIsExpandable(getLongStringNode()), true, "The node can be expanded");
+  is(isObjectInspectorNodeExpandable(getLongStringNode()), true,
+    "The node can be expanded");
 
-  expandNode(getLongStringNode());
+  expandObjectInspectorNode(getLongStringNode());
   await waitFor(() =>
     getLongStringNode().textContent.includes(`myLongStringGetter: "${longString}"`));
   ok(true, "the longstring was expanded");
 }
 
 function checkChildren(node, expectedChildren) {
-  const children = getChildrenNodes(node);
+  const children = getObjectInspectorChildrenNodes(node);
   is(children.length, expectedChildren.length,
     "There is the expected number of children");
   children.forEach((child, index) => {
     ok(child.textContent.includes(expectedChildren[index]),
       `Expected "${expectedChildren[index]}" child`);
-  });
-}
-
-function nodeIsExpandable(node) {
-  return !!getNodeArrow(node);
-}
-
-function expandNode(node) {
-  const arrow = getNodeArrow(node);
-  if (!arrow) {
-    ok(false, "Node can't be expanded");
-    return;
-  }
-  arrow.click();
-}
-
-function getNodeArrow(node) {
-  return node.querySelector(".arrow");
-}
-
-function getOiNodes(oi) {
-  return oi.querySelectorAll(".tree-node");
-}
-
-function getChildrenNodes(node) {
-  const getLevel = n => parseInt(n.getAttribute("aria-level"), 10);
-  const level = getLevel(node);
-  const childLevel = level + 1;
-  const children = [];
-
-  let currentNode = node;
-  while (currentNode.nextSibling && getLevel(currentNode.nextSibling) === childLevel) {
-    currentNode = currentNode.nextSibling;
-    children.push(currentNode);
-  }
-
-  return children;
-}
-
-function getInvokeGetterButton(node) {
-  return node.querySelector(".invoke-getter");
-}
-
-function getOiNode(oi, nodeLabel) {
-  return [...oi.querySelectorAll(".tree-node")].find(node => {
-    const label = node.querySelector(".object-label");
-    if (!label) {
-      return false;
-    }
-    return label.textContent === nodeLabel;
   });
 }
