f2b352516d17cd49e0c2e27a107e95f00ca8e75d
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-12-13 16:02:31 +0000

    Bug 1440779: shut off experiments when the optoutstudies pref is disabled r=mythmon,Gijs
    I think this should have been here before but it wasn't.
    
    To make it easier to distinguish recipes generated by
    preferenceStudyFactory, give them a unique name with the unused
    _preferenceStudyFactoryId.
    
    Depends on D13574
    
    Differential Revision: https://phabricator.services.mozilla.com/D13575

--- toolkit/components/normandy/test/browser/browser_ShieldPreferences.js
+++ toolkit/components/normandy/test/browser/browser_ShieldPreferences.js
@@ -2,6 +2,7 @@
 
 ChromeUtils.import("resource://gre/modules/Services.jsm", this);
 ChromeUtils.import("resource://normandy/lib/AddonStudies.jsm", this);
+ChromeUtils.import("resource://normandy/lib/PreferenceExperiments.jsm", this);
 ChromeUtils.import("resource://normandy/lib/ShieldPreferences.jsm", this);
 
 const OPT_OUT_STUDIES_ENABLED_PREF = "app.shield.optoutstudies.enabled";
@@ -29,6 +30,40 @@ decorate_task(
     ok(
       !newStudy1.active && !newStudy2.active,
       "Setting the opt-out pref to false stops all active opt-out studies."
+    );
+  }
+);
+
+decorate_task(
+  withMockPreferences,
+  PreferenceExperiments.withMockExperiments([
+    preferenceStudyFactory({active: true}),
+    preferenceStudyFactory({active: true}),
+  ]),
+  withStub(PreferenceExperiments, "stop"),
+  async function testDisableExperimentsWhenOptOutDisabled(mockPreferences, [study1, study2], stopStub) {
+
+    mockPreferences.set(OPT_OUT_STUDIES_ENABLED_PREF, true);
+    let stopArgs = [];
+    let stoppedBoth = new Promise(resolve => {
+      let calls = 0;
+      stopStub.callsFake(function() {
+        stopArgs.push(Array.from(arguments));
+        calls++;
+        if (calls == 2) {
+          resolve();
+        }
+      });
+    });
+    Services.prefs.setBoolPref(OPT_OUT_STUDIES_ENABLED_PREF, false);
+    await stoppedBoth;
+
+    Assert.deepEqual(
+      stopArgs,
+      [
+        [study1.name, { reason: "general-opt-out" }],
+        [study2.name, { reason: "general-opt-out" }],
+      ],
     );
   }
 );
