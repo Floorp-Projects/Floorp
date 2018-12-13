113b3dfca1538da21e3a37289e907c079649fe49
matthias <matthias@kammueller.eu>
2018-12-13 14:26:20 +0000

    Bug 1500542 - Added ability to disable the disclaimer page of the new about:config. r=paolo
    Differential Revision: https://phabricator.services.mozilla.com/D13870

--- browser/components/aboutconfig/test/browser/browser_warning.js
+++ browser/components/aboutconfig/test/browser/browser_warning.js
@@ -3,6 +3,14 @@
 
 const PAGE_URL = "chrome://browser/content/aboutconfig/aboutconfig.html";
 
+add_task(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.aboutConfig.showWarning", true],
+    ],
+  });
+});
+
 add_task(async function test_load_warningpage() {
   await BrowserTestUtils.withNewTab({
     gBrowser,
@@ -14,6 +22,17 @@ add_task(async function test_load_warningpage() {
       Assert.equal(content.document.getElementsByTagName("button").length, 1);
       Assert.equal(content.document.getElementById("search"), undefined);
       Assert.equal(content.document.getElementById("prefs"), undefined);
+
+      // Disable checkbox and reload.
+      content.document.getElementById("showWarningNextTime").click();
+      content.document.querySelector("button").click();
     });
+  });
+  await BrowserTestUtils.withNewTab({
+    gBrowser,
+    url: PAGE_URL,
+  }, browser => {
+    Assert.ok(content.document.getElementById("search"));
+    Assert.ok(content.document.getElementById("prefs"));
   });
 });
