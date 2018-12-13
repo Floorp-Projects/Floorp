688f3f7ee76b4d59e4be8d03e33d94272c58105c
Tom Prince <mozilla@hocat.ca>
2018-12-13 21:01:39 +0000

    No bug: [taskgraph] Use `trust-domain` to determine what index routes to verify; r=dustin
    Differential Revision: https://phabricator.services.mozilla.com/D14477

--- taskcluster/taskgraph/generator.py
+++ taskcluster/taskgraph/generator.py
@@ -252,7 +252,7 @@ class TaskGraphGenerator(object):
             logger.info(
                 "Limiting kinds to {target_kind} and dependencies".format(
                     target_kind=self._target_kind))
-            kind_graph = kind_graph.transitive_closure({self._target_kind})
+            kind_graph = kind_graph.transitive_closure({self._target_kind, 'docker-image'})
 
         logger.info("Generating full task set")
         all_tasks = {}
@@ -272,7 +272,7 @@ class TaskGraphGenerator(object):
         full_task_set = TaskGraph(all_tasks, Graph(set(all_tasks), set()))
         self.verify_attributes(all_tasks)
         self.verify_run_using()
-        yield verifications('full_task_set', full_task_set)
+        yield verifications('full_task_set', full_task_set, graph_config)
 
         logger.info("Generating full task graph")
         edges = set()
@@ -284,7 +284,7 @@ class TaskGraphGenerator(object):
                                     Graph(full_task_set.graph.nodes, edges))
         logger.info("Full task graph contains %d tasks and %d dependencies" % (
             len(full_task_set.graph.nodes), len(edges)))
-        yield verifications('full_task_graph', full_task_graph)
+        yield verifications('full_task_graph', full_task_graph, graph_config)
 
         logger.info("Generating target task set")
         target_task_set = TaskGraph(dict(all_tasks),
@@ -300,7 +300,7 @@ class TaskGraphGenerator(object):
                 old_len - len(target_tasks),
                 len(target_tasks)))
 
-        yield verifications('target_task_set', target_task_set)
+        yield verifications('target_task_set', target_task_set, graph_config)
 
         logger.info("Generating target task graph")
         # include all docker-image build tasks here, in case they are needed for a graph morph
@@ -316,7 +316,7 @@ class TaskGraphGenerator(object):
         target_task_graph = TaskGraph(
             {l: all_tasks[l] for l in target_graph.nodes},
             target_graph)
-        yield verifications('target_task_graph', target_task_graph)
+        yield verifications('target_task_graph', target_task_graph, graph_config)
 
         logger.info("Generating optimized task graph")
         existing_tasks = parameters.get('existing_tasks')
@@ -328,13 +328,13 @@ class TaskGraphGenerator(object):
                                                                     do_not_optimize,
                                                                     existing_tasks=existing_tasks)
 
-        yield verifications('optimized_task_graph', optimized_task_graph)
+        yield verifications('optimized_task_graph', optimized_task_graph, graph_config)
 
         morphed_task_graph, label_to_taskid = morph(
             optimized_task_graph, label_to_taskid, parameters)
 
         yield 'label_to_taskid', label_to_taskid
-        yield verifications('morphed_task_graph', morphed_task_graph)
+        yield verifications('morphed_task_graph', morphed_task_graph, graph_config)
 
     def _run_until(self, name):
         while name not in self._run_results:
