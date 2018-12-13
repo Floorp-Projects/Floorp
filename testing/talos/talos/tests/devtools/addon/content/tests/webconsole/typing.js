eedf1c8f965dda528238937b2b493af7b2f1322f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2018-12-13 17:38:06 +0000

    Bug 1513244 - Extract fetchAutocompleteProperties from JsTerm into a Redux action; r=Honza.
    The functions needed to build the autocomplete request are moved
    to the serviceContainer so it can be called from everywhere in
    the code, and not only from the JsTerm.
    
    Differential Revision: https://phabricator.services.mozilla.com/D12939

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/typing.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/typing.js
@@ -44,7 +44,7 @@ module.exports = async function() {
     const onPopupOpened = jsterm.autocompletePopup.once("popup-opened");
     jsterm.insertStringAtCursor(char);
     // We need to trigger autocompletion update to show the popup.
-    jsterm.fetchAutocompletionProperties();
+    jsterm.props.autocompleteUpdate();
     await onPopupOpened;
   }
 
