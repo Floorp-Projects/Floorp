df588009cb134b1f01639200ae094b394cd24291
George Roman <george.roman.99@gmail.com>
2018-12-13 13:59:57 +0000

    Bug 1510072 [wpt PR 14242] - Kill test processes whenever any error occurs, a=testonly
    Automatic update from web-platform-tests
    Kill test processes whenever any error occurs
    
    --
    
    wpt-commits: 86c56c1db8a96b32077f71ffdd99e8ac884f0f1a
    wpt-pr: 14242

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorservo.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorservo.py
@@ -124,7 +124,7 @@ class ServoTestharnessExecutor(ProcessTestExecutor):
                     self.proc.wait()
                 else:
                     self.proc.kill()
-        except KeyboardInterrupt:
+        except:
             self.proc.kill()
             raise
 
