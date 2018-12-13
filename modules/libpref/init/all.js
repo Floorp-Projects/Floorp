b19314e00aa2fb904d260000dff52d36b98a5877
Junior Hsu <juhsu@mozilla.com>
2018-12-13 23:41:33 +0000

    Bug 1513135 - disable flow control of HTTP e10s back pressure r=dragana a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D14500

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1900,9 +1900,11 @@ pref("network.http.rcwn.max_wait_before_racing_ms", 500);
 // all available active connections.
 pref("network.http.focused_window_transaction_ratio", "0.9");
 
+// XXX Disable for intranet downloading issue.
 // This is the size of the flow control window (KB) (i.e., the amount of data
-// that the parent can send to the child before getting an ack)
-pref("network.http.send_window_size", 1024);
+// that the parent can send to the child before getting an ack). 0 for disable
+// the flow control.
+pref("network.http.send_window_size", 0);
 
 // Whether or not we give more priority to active tab.
 // Note that this requires restart for changes to take effect.
