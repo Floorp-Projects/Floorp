766c9fc88ac0338b9331544eaba0f85146ccafb0
Jason Orendorff <jorendorff@mozilla.com>
2018-12-14 18:15:56 +0000

    Bug 1513570 - Part 4: Back out the jit-tests added in bug 1503012. r=Ms2ger
    Differential Revision: https://phabricator.services.mozilla.com/D14317

--- js/src/jit-test/tests/web-platform/streams/readable-streams/garbage-collection.js
+++
@@ -1,4 +0,0 @@
-load(libdir + "web-platform-testharness.js");
-load_web_platform_test("streams/resources/test-utils.js");
-load_web_platform_test("streams/resources/rs-utils.js");
-load_web_platform_test("streams/readable-streams/garbage-collection.any.js");
