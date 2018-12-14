b50541ce3264ae391f2d869afc040340d330bfa3
Jason Orendorff <jorendorff@mozilla.com>
2018-12-14 18:15:56 +0000

    Bug 1513570 - Part 4: Back out the jit-tests added in bug 1503012. r=Ms2ger
    Differential Revision: https://phabricator.services.mozilla.com/D14317
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jit-test/tests/web-platform/streams/readable-streams/constructor.js
+++
@@ -1,3 +0,0 @@
-load(libdir + "web-platform-testharness.js");
-load_web_platform_test("streams/resources/constructor-ordering.js");
-load_web_platform_test("streams/readable-streams/constructor.any.js");
