52110ba8bc74789658e85a1cc68ca61f3b81dea8
Sebastian Kaspari <s.kaspari@gmail.com>
2018-12-17 21:54:12 +0100

    [components] Enforce that all (transitive) dependencies are using the same support library version as we do.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Synchronized version numbers for dependencies used by (some) modules
-private object Versions {
+object Versions {
     const val kotlin = "1.3.10"
     const val coroutines = "1.0.1"
 
