b102a3f9b4e76afd3bbc0e22c35336375e27c5c1
Sebastian Kaspari <s.kaspari@gmail.com>
2018-12-17 20:21:22 +0100

    [components] (Merge day) browser-engine-gecko-nightly -> 66

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 internal object GeckoVersions {
-    const val nightly_version = "65.0.20181129095546"
+    const val nightly_version = "66.0.20181217093726"
     const val beta_version = "65.0.20181211223337"
     const val release_version = "64.0.20181214004633"
 }
