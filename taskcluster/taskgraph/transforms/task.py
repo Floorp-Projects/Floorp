6b216e574ca0c969e8a1112b8ced35e4da457c86
Dustin J. Mitchell <dustin@mozilla.com>
2018-12-18 05:37:41 +0000

    Bug 1492664 - set TASKCLUSTER_ROOT_URL and TASKCLUSTER_PROXY_URL; r=tomprince,glandium
    Eventually, workers will provide these variables directly
    (https://bugzilla.mozilla.org/show_bug.cgi?id=1460015).  But for now, this
    ensures that TASKCLUSTER_ROOT_URL is set everywhere, and TASKCLUSTER_PROXY_URL
    is set wherever the proxy is active.
    
    The setup for the mach commands defaults to https://taskcluster.net for user
    convenience.  When the production instance's URL changes, we can simply change
    that default.
    
    This changes the docker build process to propagate TASKCLUSTER_ROOT_URL into
    the docker images, and for good measure includes some code to use that value to
    generate debian repo paths.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14196

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -499,6 +499,11 @@ def build_docker_worker_payload(config, task, task_def):
         else:
             raise Exception("unknown docker image type")
 
+    # propagate our TASKCLUSTER_ROOT_URL to the task; note that this will soon
+    # be provided directly by the worker, making this redundant:
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1460015
+    worker['env']['TASKCLUSTER_ROOT_URL'] = os.environ['TASKCLUSTER_ROOT_URL']
+
     features = {}
 
     if worker.get('relengapi-proxy'):
@@ -530,6 +535,11 @@ def build_docker_worker_payload(config, task, task_def):
         worker['env']['SCCACHE_IDLE_TIMEOUT'] = '0'
     else:
         worker['env']['SCCACHE_DISABLE'] = '1'
+
+    # this will soon be provided directly by the worker:
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1460015
+    if features.get('taskclusterProxy'):
+        worker['env']['TASKCLUSTER_PROXY_URL'] = 'http://taskcluster'
 
     capabilities = {}
 
@@ -763,6 +773,11 @@ def build_generic_worker_payload(config, task, task_def):
 
     env = worker.get('env', {})
 
+    # propagate our TASKCLUSTER_ROOT_URL to the task; note that this will soon
+    # be provided directly by the worker, making this redundant:
+    # https://bugzilla.mozilla.org/show_bug.cgi?id=1460015
+    env['TASKCLUSTER_ROOT_URL'] = os.environ['TASKCLUSTER_ROOT_URL']
+
     if task.get('needs-sccache'):
         env['USE_SCCACHE'] = '1'
         # Disable sccache idle shutdown.
@@ -817,7 +832,9 @@ def build_generic_worker_payload(config, task, task_def):
 
     if worker.get('taskcluster-proxy'):
         features['taskclusterProxy'] = True
-        worker['env']['TASKCLUSTER_PROXY_URL'] = 'http://taskcluster/'
+        # this will soon be provided directly by the worker:
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1460015
+        worker['env']['TASKCLUSTER_PROXY_URL'] = 'http://taskcluster'
 
     if worker.get('run-as-administrator', False):
         features['runAsAdministrator'] = True
@@ -1315,6 +1332,11 @@ def build_always_optimized_payload(config, task, task_def):
 })
 def build_macosx_engine_payload(config, task, task_def):
     worker = task['worker']
+
+    # propagate our TASKCLUSTER_ROOT_URL to the task; note that this will soon
+    # be provided directly by the worker, making this redundant
+    worker.setdefault('env', {})['TASKCLUSTER_ROOT_URL'] = os.environ['TASKCLUSTER_ROOT_URL']
+
     artifacts = map(lambda artifact: {
         'name': artifact['name'],
         'path': artifact['path'],
