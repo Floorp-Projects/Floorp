dbb2ca6930844860d7ee6bd2719e53292ac9f93b
yulia <ystartsev@mozilla.com>
2018-12-18 08:46:18 +0000

    Bug 1508656- migrate togglePicker to nodePicker; r=ochameau
    Depends on D12119
    
    Differential Revision: https://phabricator.services.mozilla.com/D12122

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -1280,7 +1280,7 @@ Toolbox.prototype = {
     if (currentPanel.togglePicker) {
       currentPanel.togglePicker(focus);
     } else {
-      this.highlighterUtils.togglePicker(focus);
+      this.inspector.nodePicker.togglePicker(focus);
     }
   },
 
