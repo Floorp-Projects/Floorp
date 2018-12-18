abd59029503d6fd603310b0c58d2016c1af49604
alwu <alwu@mozilla.com>
2018-12-18 18:11:56 +0000

    Bug 1513681 - part1 : dispatch 'GloballyAutoplayBlocked' event when site is permanent blocked. r=cpearce,smaug
    This event is used to notify tab that this site is permanently blocked and we should show the blocking icon for it. Patch2 will handle following details.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14794

--- dom/base/nsIDocument.h
+++ dom/base/nsIDocument.h
@@ -3390,6 +3390,10 @@ class nsIDocument : public nsINode,
 
   void ReportShadowDOMUsage();
 
+  // When the doc is blocked permanantly, we would dispatch event to notify
+  // front-end side to show blocking icon.
+  void MaybeNotifyAutoplayBlocked();
+
   // Sets flags for media autoplay telemetry.
   void SetDocTreeHadAudibleMedia();
   void SetDocTreeHadPlayRevoked();
