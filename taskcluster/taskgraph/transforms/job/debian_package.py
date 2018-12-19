c18ee639c6a334be961a311a98bf3eb486325b8d
Margareta Eliza Balazs <ebalazs@mozilla.com>
2018-12-19 11:45:29 +0200

    Backed out 16 changesets (bug 1492664) for breaking developer artifact builds, requested by standard8 a=backout
    Backed out changeset 31e500489665 (bug 1492664)
    Backed out changeset f4945658d45f (bug 1492664)
    Backed out changeset 6d17291b8b92 (bug 1492664)
    Backed out changeset 90f3faa36137 (bug 1492664)
    Backed out changeset 0b229b00818a (bug 1492664)
    Backed out changeset 5eb2c77d70a9 (bug 1492664)
    Backed out changeset e1ebad5d89c5 (bug 1492664)
    Backed out changeset 3017e5890739 (bug 1492664)
    Backed out changeset c8b7e620eabf (bug 1492664)
    Backed out changeset d3dfbd848236 (bug 1492664)
    Backed out changeset 5c92bb5ac895 (bug 1492664)
    Backed out changeset fb7cfca6ebc3 (bug 1492664)
    Backed out changeset 0c4101230d4d (bug 1492664)
    Backed out changeset b93a0fcc86f3 (bug 1492664)
    Backed out changeset 6dc9522ee0bf (bug 1492664)
    Backed out changeset 85d7f8b330eb (bug 1492664)

--- taskcluster/taskgraph/transforms/job/debian_package.py
+++ taskcluster/taskgraph/transforms/job/debian_package.py
@@ -9,7 +9,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 import re
-import taskcluster_urls
 
 from taskgraph.util.schema import Schema
 from voluptuous import Any, Optional, Required
@@ -153,8 +152,6 @@ def docker_worker_debian_package(config, job, taskdesc):
             dist=run['dist'],
         )
 
-    queue_url = taskcluster_urls.api(os.environ['TASKCLUSTER_ROOT_URL'], 'queue', 'v1', '')
-
     # We can't depend on docker images (since docker images depend on packages),
     # so we inline the whole script here.
     worker['command'] = [
@@ -174,7 +171,8 @@ def docker_worker_debian_package(config, job, taskdesc):
         # Add sources for packages coming from other package tasks.
         'apt-get install -yyq apt-transport-https ca-certificates && '
         'for task in $PACKAGES; do '
-        '  echo "deb [trusted=yes] {queue_url}task/$task/artifacts/public/build/ debian/" '
+        '  echo "deb [trusted=yes] https://queue.taskcluster.net/v1/task'
+        '/$task/artifacts/public/build/ debian/" '
         '>> /etc/apt/sources.list; '
         'done && '
         # Install the base utilities required to build debian packages.
@@ -200,7 +198,6 @@ def docker_worker_debian_package(config, job, taskdesc):
         'apt-ftparchive sources debian | gzip -c9 > debian/Sources.gz && '
         'apt-ftparchive packages debian | gzip -c9 > debian/Packages.gz'
         .format(
-            queue_url=queue_url,
             package=package,
             snapshot=run['snapshot'],
             dist=run['dist'],
