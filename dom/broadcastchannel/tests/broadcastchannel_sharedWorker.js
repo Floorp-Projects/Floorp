d525dcfeae705d31ced46904fea19a4149386517
Alex Kong <jrkong.hfd@gmail.com>
2018-12-19 22:38:44 +0000

    Bug 1508988 - Enable ESLint for dom/abort/, dom/asmjscache/, dom/battery/, dom/broadcastchannel/ and dom/console/ (automatic changes). r=Standard8,nika
    Differential Revision: https://phabricator.services.mozilla.com/D14131

--- dom/broadcastchannel/tests/broadcastchannel_sharedWorker.js
+++ dom/broadcastchannel/tests/broadcastchannel_sharedWorker.js
@@ -1,12 +1,12 @@
 onconnect = function(evt) {
   evt.ports[0].onmessage = function(evt) {
-    var bc = new BroadcastChannel('foobar');
-    bc.addEventListener('message', function(event) {
+    var bc = new BroadcastChannel("foobar");
+    bc.addEventListener("message", function(event) {
       bc.postMessage(event.data == "hello world from the window" ?
                        "hello world from the worker" : "KO");
       bc.close();
     });
 
     evt.target.postMessage("READY");
-  }
-}
+  };
+};
