ec9628f8c0ea90d5d3020249b567d848cb738b72
Diego Pino Garcia <dpino@igalia.com>
2018-12-20 03:28:00 +0200

    Bug 1446158 - Create a handleEvent custom element mixin to forward events from handleEvent to on* methods. r=MattN

--- browser/components/payments/res/containers/payment-dialog.js
+++ browser/components/payments/res/containers/payment-dialog.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import HandleEventMixin from "../mixins/HandleEventMixin.js";
 import PaymentStateSubscriberMixin from "../mixins/PaymentStateSubscriberMixin.js";
 import paymentRequest from "../paymentRequest.js";
 
@@ -26,7 +27,8 @@ import "./shipping-option-picker.js";
  * being exported once tests stop depending on it.
  */
 
-export default class PaymentDialog extends PaymentStateSubscriberMixin(HTMLElement) {
+export default class PaymentDialog extends
+    HandleEventMixin(PaymentStateSubscriberMixin(HTMLElement)) {
   constructor() {
     super();
     this._template = document.getElementById("payment-dialog-template");
@@ -77,22 +79,20 @@ export default class PaymentDialog extends PaymentStateSubscriberMixin(HTMLEleme
     super.disconnectedCallback();
   }
 
-  handleEvent(event) {
-    if (event.type == "click") {
-      switch (event.currentTarget) {
-        case this._viewAllButton:
-          let orderDetailsShowing = !this.requestStore.getState().orderDetailsShowing;
-          this.requestStore.setState({ orderDetailsShowing });
-          break;
-        case this._payButton:
-          this.pay();
-          break;
-        case this._manageText:
-          if (event.target instanceof HTMLAnchorElement) {
-            this.openPreferences(event);
-          }
-          break;
-      }
+  onClick(event) {
+    switch (event.currentTarget) {
+      case this._viewAllButton:
+        let orderDetailsShowing = !this.requestStore.getState().orderDetailsShowing;
+        this.requestStore.setState({ orderDetailsShowing });
+        break;
+      case this._payButton:
+        this.pay();
+        break;
+      case this._manageText:
+        if (event.target instanceof HTMLAnchorElement) {
+          this.openPreferences(event);
+        }
+        break;
     }
   }
 
