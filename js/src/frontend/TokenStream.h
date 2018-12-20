1eac67079886e041e0e9b43f5e30de55cb946fde
Jeff Walden <jwalden@mit.edu>
2018-12-20 01:59:04 -0800

    Bug 1504947 - Enable JS column numbers as being counts of code points, not counts of code units.  r=tcampbell from back when the full patch series enabling this got r+, except this final hunk didn't land at that time because of bug 1542106
    --HG--
    extra : rebase_source : 087dd2d91722b48d606b9fd3eb0735a1ff274566

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -1872,7 +1872,7 @@ class TokenStart {
 // we counted code units.  Set this to 0 to keep returning counts of code units
 // (even for UTF-8, which is clearly wrong, but we don't ship UTF-8 yet so this
 // is fine until we can fix users that depend on code-unit counting).
-#define JS_COLUMN_DIMENSION_IS_CODE_POINTS 0
+#define JS_COLUMN_DIMENSION_IS_CODE_POINTS 1
 
 template <typename Unit, class AnyCharsAccess>
 class GeneralTokenStreamChars : public SpecializedTokenStreamCharsBase<Unit> {
