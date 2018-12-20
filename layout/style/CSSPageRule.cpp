9e7e8893751c4fa11d5488d2b9828505f2d24eb9
Emilio Cobos Álvarez <emilio@crisal.io>
2018-12-20 19:33:09 +0000

    Bug 1515707 - Reformat the files touched in the previous commit. r=Ehsan
    Depends on D15125
    
    Differential Revision: https://phabricator.services.mozilla.com/D15126

--- layout/style/CSSPageRule.cpp
+++ layout/style/CSSPageRule.cpp
@@ -34,125 +34,125 @@ NS_INTERFACE_MAP_BEGIN(CSSPageRuleDeclaration)
   }
 NS_INTERFACE_MAP_END_INHERITING(nsDOMCSSDeclaration)
 
-  NS_IMPL_ADDREF_USING_AGGREGATOR(CSSPageRuleDeclaration, Rule())
-  NS_IMPL_RELEASE_USING_AGGREGATOR(CSSPageRuleDeclaration, Rule())
+NS_IMPL_ADDREF_USING_AGGREGATOR(CSSPageRuleDeclaration, Rule())
+NS_IMPL_RELEASE_USING_AGGREGATOR(CSSPageRuleDeclaration, Rule())
 
-  /* nsDOMCSSDeclaration implementation */
+/* nsDOMCSSDeclaration implementation */
 
-  css::Rule* CSSPageRuleDeclaration::GetParentRule() { return Rule(); }
+css::Rule* CSSPageRuleDeclaration::GetParentRule() { return Rule(); }
 
-  nsINode* CSSPageRuleDeclaration::GetParentObject() {
-    return Rule()->GetParentObject();
+nsINode* CSSPageRuleDeclaration::GetParentObject() {
+  return Rule()->GetParentObject();
+}
+
+DeclarationBlock* CSSPageRuleDeclaration::GetOrCreateCSSDeclaration(
+    Operation aOperation, DeclarationBlock** aCreated) {
+  return mDecls;
+}
+
+nsresult CSSPageRuleDeclaration::SetCSSDeclaration(
+    DeclarationBlock* aDecl, MutationClosureData* aClosureData) {
+  MOZ_ASSERT(aDecl, "must be non-null");
+  CSSPageRule* rule = Rule();
+
+  if (aDecl != mDecls) {
+    mDecls->SetOwningRule(nullptr);
+    RefPtr<DeclarationBlock> decls = aDecl;
+    Servo_PageRule_SetStyle(rule->Raw(), decls->Raw());
+    mDecls = decls.forget();
+    mDecls->SetOwningRule(rule);
   }
 
-  DeclarationBlock* CSSPageRuleDeclaration::GetOrCreateCSSDeclaration(
-      Operation aOperation, DeclarationBlock * *aCreated) {
-    return mDecls;
+  return NS_OK;
+}
+
+nsIDocument* CSSPageRuleDeclaration::DocToUpdate() { return nullptr; }
+
+nsDOMCSSDeclaration::ParsingEnvironment
+CSSPageRuleDeclaration::GetParsingEnvironment(
+    nsIPrincipal* aSubjectPrincipal) const {
+  return GetParsingEnvironmentForRule(Rule());
+}
+
+// -- CSSPageRule --------------------------------------------------
+
+CSSPageRule::CSSPageRule(RefPtr<RawServoPageRule> aRawRule, StyleSheet* aSheet,
+                         css::Rule* aParentRule, uint32_t aLine,
+                         uint32_t aColumn)
+    : css::Rule(aSheet, aParentRule, aLine, aColumn),
+      mRawRule(std::move(aRawRule)),
+      mDecls(Servo_PageRule_GetStyle(mRawRule).Consume()) {}
+
+NS_IMPL_ADDREF_INHERITED(CSSPageRule, css::Rule)
+NS_IMPL_RELEASE_INHERITED(CSSPageRule, css::Rule)
+
+// QueryInterface implementation for PageRule
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CSSPageRule)
+NS_INTERFACE_MAP_END_INHERITING(css::Rule)
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(CSSPageRule)
+
+NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN_INHERITED(CSSPageRule, css::Rule)
+  // Keep this in sync with IsCCLeaf.
+
+  // Trace the wrapper for our declaration.  This just expands out
+  // NS_IMPL_CYCLE_COLLECTION_TRACE_PRESERVED_WRAPPER which we can't use
+  // directly because the wrapper is on the declaration, not on us.
+  tmp->mDecls.TraceWrapper(aCallbacks, aClosure);
+NS_IMPL_CYCLE_COLLECTION_TRACE_END
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(CSSPageRule, css::Rule)
+  // Keep this in sync with IsCCLeaf.
+
+  // Unlink the wrapper for our declaraton.  This just expands out
+  // NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER which we can't use
+  // directly because the wrapper is on the declaration, not on us.
+  tmp->mDecls.ReleaseWrapper(static_cast<nsISupports*>(p));
+  tmp->mDecls.mDecls->SetOwningRule(nullptr);
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(CSSPageRule, css::Rule)
+  // Keep this in sync with IsCCLeaf.
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+bool CSSPageRule::IsCCLeaf() const {
+  if (!Rule::IsCCLeaf()) {
+    return false;
   }
 
-  nsresult CSSPageRuleDeclaration::SetCSSDeclaration(
-      DeclarationBlock * aDecl, MutationClosureData * aClosureData) {
-    MOZ_ASSERT(aDecl, "must be non-null");
-    CSSPageRule* rule = Rule();
+  return !mDecls.PreservingWrapper();
+}
 
-    if (aDecl != mDecls) {
-      mDecls->SetOwningRule(nullptr);
-      RefPtr<DeclarationBlock> decls = aDecl;
-      Servo_PageRule_SetStyle(rule->Raw(), decls->Raw());
-      mDecls = decls.forget();
-      mDecls->SetOwningRule(rule);
-    }
-
-    return NS_OK;
-  }
-
-  nsIDocument* CSSPageRuleDeclaration::DocToUpdate() { return nullptr; }
-
-  nsDOMCSSDeclaration::ParsingEnvironment
-  CSSPageRuleDeclaration::GetParsingEnvironment(nsIPrincipal *
-                                                aSubjectPrincipal) const {
-    return GetParsingEnvironmentForRule(Rule());
-  }
-
-  // -- CSSPageRule --------------------------------------------------
-
-  CSSPageRule::CSSPageRule(RefPtr<RawServoPageRule> aRawRule,
-                           StyleSheet * aSheet, css::Rule * aParentRule,
-                           uint32_t aLine, uint32_t aColumn)
-      : css::Rule(aSheet, aParentRule, aLine, aColumn),
-        mRawRule(std::move(aRawRule)),
-        mDecls(Servo_PageRule_GetStyle(mRawRule).Consume()) {}
-
-  NS_IMPL_ADDREF_INHERITED(CSSPageRule, css::Rule)
-  NS_IMPL_RELEASE_INHERITED(CSSPageRule, css::Rule)
-
-  // QueryInterface implementation for PageRule
-  NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CSSPageRule)
-  NS_INTERFACE_MAP_END_INHERITING(css::Rule)
-
-  NS_IMPL_CYCLE_COLLECTION_CLASS(CSSPageRule)
-
-  NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN_INHERITED(CSSPageRule, css::Rule)
-    // Keep this in sync with IsCCLeaf.
-
-    // Trace the wrapper for our declaration.  This just expands out
-    // NS_IMPL_CYCLE_COLLECTION_TRACE_PRESERVED_WRAPPER which we can't use
-    // directly because the wrapper is on the declaration, not on us.
-    tmp->mDecls.TraceWrapper(aCallbacks, aClosure);
-  NS_IMPL_CYCLE_COLLECTION_TRACE_END
-
-  NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(CSSPageRule, css::Rule)
-    // Keep this in sync with IsCCLeaf.
-
-    // Unlink the wrapper for our declaraton.  This just expands out
-    // NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER which we can't use
-    // directly because the wrapper is on the declaration, not on us.
-    tmp->mDecls.ReleaseWrapper(static_cast<nsISupports*>(p));
-    tmp->mDecls.mDecls->SetOwningRule(nullptr);
-  NS_IMPL_CYCLE_COLLECTION_UNLINK_END
-
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(CSSPageRule, css::Rule)
-    // Keep this in sync with IsCCLeaf.
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
-
-  bool CSSPageRule::IsCCLeaf() const {
-    if (!Rule::IsCCLeaf()) {
-      return false;
-    }
-
-    return !mDecls.PreservingWrapper();
-  }
-
-  size_t CSSPageRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const {
-    // TODO Implement this!
-    return aMallocSizeOf(this);
-  }
+size_t CSSPageRule::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const {
+  // TODO Implement this!
+  return aMallocSizeOf(this);
+}
 
 #ifdef DEBUG
-  void CSSPageRule::List(FILE * out, int32_t aIndent) const {
-    nsAutoCString str;
-    for (int32_t i = 0; i < aIndent; i++) {
-      str.AppendLiteral("  ");
-    }
-    Servo_PageRule_Debug(mRawRule, &str);
-    fprintf_stderr(out, "%s\n", str.get());
+void CSSPageRule::List(FILE* out, int32_t aIndent) const {
+  nsAutoCString str;
+  for (int32_t i = 0; i < aIndent; i++) {
+    str.AppendLiteral("  ");
   }
+  Servo_PageRule_Debug(mRawRule, &str);
+  fprintf_stderr(out, "%s\n", str.get());
+}
 #endif
 
-  /* CSSRule implementation */
+/* CSSRule implementation */
 
-  void CSSPageRule::GetCssText(nsAString & aCssText) const {
-    Servo_PageRule_GetCssText(mRawRule, &aCssText);
-  }
+void CSSPageRule::GetCssText(nsAString& aCssText) const {
+  Servo_PageRule_GetCssText(mRawRule, &aCssText);
+}
 
-  /* CSSPageRule implementation */
+/* CSSPageRule implementation */
 
-  nsICSSDeclaration* CSSPageRule::Style() { return &mDecls; }
+nsICSSDeclaration* CSSPageRule::Style() { return &mDecls; }
 
-  JSObject* CSSPageRule::WrapObject(JSContext * aCx,
-                                    JS::Handle<JSObject*> aGivenProto) {
-    return CSSPageRule_Binding::Wrap(aCx, this, aGivenProto);
-  }
+JSObject* CSSPageRule::WrapObject(JSContext* aCx,
+                                  JS::Handle<JSObject*> aGivenProto) {
+  return CSSPageRule_Binding::Wrap(aCx, this, aGivenProto);
+}
 
 }  // namespace dom
 }  // namespace mozilla
