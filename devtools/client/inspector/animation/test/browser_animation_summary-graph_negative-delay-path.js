11d1ca61117e7ad79245018e987b929967091844
Daisuke Akatsuka <dakatsuka@mozilla.com>
2018-12-20 12:41:38 +0000

    Bug 1513706 - Part 2: Add a test for negative delay. r=pbro
    Depends on D14838
    
    Differential Revision: https://phabricator.services.mozilla.com/D14839

--- devtools/client/inspector/animation/test/browser_animation_summary-graph_negative-delay-path.js
+++ devtools/client/inspector/animation/test/browser_animation_summary-graph_negative-delay-path.js
@@ -13,10 +13,51 @@ const TEST_DATA = [
   },
   {
     targetClass: "delay-negative",
-    expectedPath: [
+    expectedIterationPathList: [
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 50 },
+        { x: 250000, y: 75 },
+        { x: 500000, y: 100 },
+        { x: 500000, y: 0 },
+      ],
+    ],
+    expectedNegativePath: [
       { x: -500000, y: 0 },
       { x: -250000, y: 25 },
       { x: 0, y: 50 },
+      { x: 0, y: 0 },
+    ],
+  },
+  {
+    targetClass: "delay-negative-25",
+    expectedIterationPathList: [
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 25 },
+        { x: 750000, y: 100 },
+        { x: 750000, y: 0 },
+      ],
+    ],
+    expectedNegativePath: [
+      { x: -250000, y: 0 },
+      { x: 0, y: 25 },
+      { x: 0, y: 0 },
+    ],
+  },
+  {
+    targetClass: "delay-negative-75",
+    expectedIterationPathList: [
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 75 },
+        { x: 250000, y: 100 },
+        { x: 250000, y: 0 },
+      ],
+    ],
+    expectedNegativePath: [
+      { x: -750000, y: 0 },
+      { x: 0, y: 75 },
       { x: 0, y: 0 },
     ],
   },
@@ -27,7 +68,8 @@ add_task(async function() {
   await removeAnimatedElementsExcept(TEST_DATA.map(t => `.${ t.targetClass }`));
   const { panel } = await openAnimationInspector();
 
-  for (const { targetClass, expectedPath } of TEST_DATA) {
+  for (const { targetClass,
+               expectedIterationPathList, expectedNegativePath } of TEST_DATA) {
     const animationItemEl =
       findAnimationItemElementsByTargetSelector(panel, `.${ targetClass }`);
 
@@ -35,14 +77,35 @@ add_task(async function() {
     const negativeDelayPathEl =
       animationItemEl.querySelector(".animation-negative-delay-path");
 
-    if (expectedPath) {
+    if (expectedNegativePath) {
       ok(negativeDelayPathEl,
         "The negative delay path element should be in animation item element");
       const pathEl = negativeDelayPathEl.querySelector("path");
-      assertPathSegments(pathEl, true, expectedPath);
+      assertPathSegments(pathEl, true, expectedNegativePath);
     } else {
       ok(!negativeDelayPathEl,
         "The negative delay path element should not be in animation item element");
+    }
+
+    if (!expectedIterationPathList) {
+      // We don't need to test for iteration path.
+      continue;
+    }
+
+    info(`Checking computed timing path existance for ${ targetClass }`);
+    const computedTimingPathEl =
+      animationItemEl.querySelector(".animation-computed-timing-path");
+    ok(computedTimingPathEl,
+       "The computed timing path element should be in each animation item element");
+
+    info(`Checking iteration path list for ${ targetClass }`);
+    const iterationPathEls =
+      computedTimingPathEl.querySelectorAll(".animation-iteration-path");
+    is(iterationPathEls.length, expectedIterationPathList.length,
+       `Number of iteration path should be ${ expectedIterationPathList.length }`);
+
+    for (const [j, iterationPathEl] of iterationPathEls.entries()) {
+      assertPathSegments(iterationPathEl, true, expectedIterationPathList[j]);
     }
   }
 });
