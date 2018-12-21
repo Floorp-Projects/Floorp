9e75df0e6de63440a71ae972c5a04689028ae352
Chris Manchester <cmanchester@mozilla.com>
2018-12-21 19:02:11 +0000

    Bug 1507344 - Add configure machinery for MOZ_PROFILE_USE and merging profile. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D13863

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -1174,6 +1174,7 @@ class GeneratedFile(ContextDerived):
             '.rs',
             'node.stub', # To avoid VPATH issues with installing node files: https://bugzilla.mozilla.org/show_bug.cgi?id=1461714#c55
             'android_apks', # We need to compile Java to generate JNI wrappers for native code compilation to consume.
+            '.profdata',
         )
         self.required_for_compile = [f for f in self.outputs if f.endswith(suffixes) or 'stl_wrappers/' in f]
 
