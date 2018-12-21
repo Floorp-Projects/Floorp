7ba2f8b080948d649312476fb9121085077624e6
Dorel Luca <dluca@mozilla.com>
2018-12-22 01:17:42 +0200

    Backed out changeset e8700e52f777 (bug 1515901) for build bustage. CLOSED TREE

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -32,10 +32,7 @@ from .mozconfig import (
     MozconfigLoader,
 )
 from .pythonutil import find_python3_executable
-from .util import (
-    memoize,
-    memoized_property,
-)
+from .util import memoized_property
 from .virtualenv import VirtualenvManager
 
 
@@ -214,12 +211,6 @@ class MozbuildObject(ProcessExecutionMixin):
 
         return self._virtualenv_manager
 
-    @staticmethod
-    @memoize
-    def get_mozconfig(topsrcdir, path):
-        loader = MozconfigLoader(topsrcdir)
-        return loader.read_mozconfig(path=path)
-
     @property
     def mozconfig(self):
         """Returns information about the current mozconfig file.
@@ -227,7 +218,8 @@ class MozbuildObject(ProcessExecutionMixin):
         This a dict as returned by MozconfigLoader.read_mozconfig()
         """
         if not isinstance(self._mozconfig, dict):
-            self._mozconfig = self.get_mozconfig(self.topsrcdir, self._mozconfig)
+            loader = MozconfigLoader(self.topsrcdir)
+            self._mozconfig = loader.read_mozconfig(path=self._mozconfig)
 
         return self._mozconfig
 
