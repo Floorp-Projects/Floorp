45dfbfc83643c0b7b00c5f55af4d927d164bbf6e
Andreas Pehrson <apehrson@mozilla.com>
2018-12-21 20:03:48 +0000

    Bug 1513973 - Enable pulling at the same time as starting audio sources. r=jib
    Otherwise we risk building up a buffer in the microphone source from when
    Start() is run until pulling is enabled. This manifests itself as input latency
    to the user.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15194
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/webrtc/MediaEngineSource.h
+++ dom/media/webrtc/MediaEngineSource.h
@@ -148,6 +148,8 @@ class MediaEngineSourceInterface {
    *
    * If this is the first AllocationHandle to start, the underlying device
    * will be started.
+   *
+   * NB: Audio sources handle the enabling of pulling themselves.
    */
   virtual nsresult Start(const RefPtr<const AllocationHandle>& aHandle) = 0;
 
@@ -196,6 +198,8 @@ class MediaEngineSourceInterface {
    *
    * Double-stopping a given allocation handle is allowed and will return NS_OK.
    * This is necessary sometimes during shutdown.
+   *
+   * NB: Audio sources handle the disabling of pulling themselves.
    */
   virtual nsresult Stop(const RefPtr<const AllocationHandle>& aHandle) = 0;
 
