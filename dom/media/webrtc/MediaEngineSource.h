e20f49a8e356f7957c07663adc7c166b8cbd66fa
Dorel Luca <dluca@mozilla.com>
2018-12-21 23:28:51 +0200

    Backed out 5 changesets (bug 1513973) for browser-chrome failures in browser/base/content/test/webrtc/browser_devices_get_user_media_screen.js. CLOSED TREE
    Backed out changeset 2ddf61b1db11 (bug 1513973)
    Backed out changeset 6c839f548745 (bug 1513973)
    Backed out changeset 5669b30f2265 (bug 1513973)
    Backed out changeset 6781e633d62c (bug 1513973)
    Backed out changeset f1892f178011 (bug 1513973)

--- dom/media/webrtc/MediaEngineSource.h
+++ dom/media/webrtc/MediaEngineSource.h
@@ -137,10 +137,10 @@ class MediaEngineSourceInterface {
    *
    * This must be called before Start for the given AllocationHandle.
    */
-  virtual void SetTrack(const RefPtr<const AllocationHandle>& aHandle,
-                        const RefPtr<SourceMediaStream>& aStream,
-                        TrackID aTrackID,
-                        const PrincipalHandle& aPrincipal) = 0;
+  virtual nsresult SetTrack(const RefPtr<const AllocationHandle>& aHandle,
+                            const RefPtr<SourceMediaStream>& aStream,
+                            TrackID aTrackID,
+                            const PrincipalHandle& aPrincipal) = 0;
 
   /**
    * Called by MediaEngine to start feeding data to the track associated with
@@ -148,8 +148,6 @@ class MediaEngineSourceInterface {
    *
    * If this is the first AllocationHandle to start, the underlying device
    * will be started.
-   *
-   * NB: Audio sources handle the enabling of pulling themselves.
    */
   virtual nsresult Start(const RefPtr<const AllocationHandle>& aHandle) = 0;
 
@@ -198,8 +196,6 @@ class MediaEngineSourceInterface {
    *
    * Double-stopping a given allocation handle is allowed and will return NS_OK.
    * This is necessary sometimes during shutdown.
-   *
-   * NB: Audio sources handle the disabling of pulling themselves.
    */
   virtual nsresult Stop(const RefPtr<const AllocationHandle>& aHandle) = 0;
 
