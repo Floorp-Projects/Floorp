b0536a8ca1f67b64b1c1fbae2857acfe767f7967
Alastor Wu <alwu@mozilla.com>
2018-12-21 06:40:10 +0000

    Bug 1511235 - Part 3: Ensure video is visible before starting test. r=jya,baku, a=RyanVM
    Add testing function to know whether video is visible or not.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14667

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -1634,6 +1634,10 @@ bool HTMLMediaElement::IsVideoDecodingSuspended() const {
   return mDecoder && mDecoder->IsVideoDecodingSuspended();
 }
 
+bool HTMLMediaElement::IsVisible() const {
+  return mVisibilityState == Visibility::APPROXIMATELY_VISIBLE;
+}
+
 already_AddRefed<layers::Image> HTMLMediaElement::GetCurrentImage() {
   MarkAsTainted();
 
@@ -6278,6 +6282,9 @@ void HTMLMediaElement::OnVisibilityChange(Visibility aNewVisibility) {
       ("OnVisibilityChange(): %s\n", VisibilityString(aNewVisibility)));
 
   mVisibilityState = aNewVisibility;
+  if (StaticPrefs::MediaTestVideoSuspend()) {
+    DispatchAsyncEvent(NS_LITERAL_STRING("visibilitychanged"));
+  }
 
   if (!mDecoder) {
     return;
