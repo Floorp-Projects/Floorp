91680f301be5d1ba9a20b0b193cd6cd39e7b090d
Alastor Wu <alwu@mozilla.com>
2018-12-21 06:40:10 +0000

    Bug 1511235 - Part 3: Ensure video is visible before starting test. r=jya,baku, a=RyanVM
    Add testing function to know whether video is visible or not.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14667
    
    --HG--
    extra : source : b8a2ab65c47930c8c2fa1a80da1be46221aeb27b
    extra : histedit_source : 2280555e3fffd4f3fff0e83a81339cc34a7a95e2

--- dom/media/test/background_video.js
+++ dom/media/test/background_video.js
@@ -48,6 +48,28 @@ function appendVideoToDoc(url, token, width, height) {
   return v;
 }
 
+
+/**
+ * @param {HTMLMediaElement} video Video element with under test.
+ * @returns {Promise} Promise that is resolved when video 'visibilitychanged' event fires.
+ */
+function waitUntilVisible(video) {
+  let videoChrome = SpecialPowers.wrap(video);
+  if (videoChrome.isVisible) {
+    return Promise.resolve();
+  }
+
+  return new Promise(resolve => {
+    videoChrome.addEventListener("visibilitychanged", () => {
+      if (videoChrome.isVisible) {
+        ok(true, `${video.token} is visible.`);
+        videoChrome.removeEventListener("visibilitychanged", this);
+        resolve();
+      }
+    });
+  });
+}
+
 /**
  * @param {HTMLMediaElement} video Video element under test.
  * @returns {Promise} Promise that is resolved when video 'playing' event fires.
