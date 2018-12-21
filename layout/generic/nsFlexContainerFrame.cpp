c7b59395f2ea687b970b9a94e00945df6c045bd3
Sylvestre Ledru ext:(%20and%20Karl%20Tomlinson%20%3Ckarlt%2B%40karlt.net%3E) <sledru@mozilla.com>
2018-12-21 03:07:05 +0000

    Bug 1515556 comment fix-ups for the tree reformat (layout/) r=Ehsan,heycam
    Differential Revision: https://phabricator.services.mozilla.com/D13125
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/nsFlexContainerFrame.cpp
+++ layout/generic/nsFlexContainerFrame.cpp
@@ -1535,8 +1535,8 @@ static bool ResolveAutoFlexBasisFromRatio(
              "Should only be called to resolve an 'auto' flex-basis");
   // If the flex item has ...
   //  - an intrinsic aspect ratio,
-  //  - a [used] flex-basis of 'main-size' [auto?] [We have this, if we're
-  //  here.]
+  //  - a [used] flex-basis of 'main-size' [auto?]
+  //    [We have this, if we're here.]
   //  - a definite cross size
   // then the flex base size is calculated from its inner cross size and the
   // flex item’s intrinsic aspect ratio.
