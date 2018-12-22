b16eb27e77137f0d988fa0b22e3bac88eb971358
Jan de Mooij <jdemooij@mozilla.com>
2018-12-22 21:42:23 +0000

    Bug 1515754 - Change Debugger::onPromiseSettled to check promise->realm()->isDebuggee() instead of cx->realm()->isDebuggee(). r=arai
    As discussed on IRC this is hard to trigger, but this is more consistent with
    the cross-compartment code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15214

--- js/src/vm/Debugger-inl.h
+++ js/src/vm/Debugger-inl.h
@@ -104,7 +104,7 @@
 
 /* static */ void js::Debugger::onPromiseSettled(
     JSContext* cx, Handle<PromiseObject*> promise) {
-  if (MOZ_UNLIKELY(cx->realm()->isDebuggee())) {
+  if (MOZ_UNLIKELY(promise->realm()->isDebuggee())) {
     slowPathPromiseHook(cx, Debugger::OnPromiseSettled, promise);
   }
 }
