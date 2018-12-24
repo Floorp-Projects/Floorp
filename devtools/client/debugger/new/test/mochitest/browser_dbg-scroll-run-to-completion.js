2326ff61658a67bc0d639c190837dbc934652e79
Brian Hackett <bhackett1024@gmail.com>
2018-12-24 05:52:31 -1000

    Bug 1044074 - Add test that scroll event handlers do not break run-to-completion, no_r.

---
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-scroll-run-to-completion.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+add_task(async function() {
+  const dbg = await initDebugger("doc-scroll-run-to-completion.html");
+  invokeInTab("pauseOnce", "doc-scroll-run-to-completion.html");
+  await waitForPaused(dbg);
+  assertPausedLocation(dbg);
+
+  const threadClient = dbg.toolbox.threadClient;
+  await checkEvaluateInTopFrame(threadClient, 'window.scrollBy(0, 10);', undefined);
+
+  // checkEvaluateInTopFrame does an implicit resume for some reason.
+  await waitForPaused(dbg);
+
+  resume(dbg);
+  await once(Services.ppmm, "test passed");
+});
