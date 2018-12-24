cb23fbc0e99fc6bff8b9b01c1d6d151672e09059
Ciure Andrei <aciure@mozilla.com>
2018-12-24 18:44:38 +0200

    Backed out 2 changesets (bug 1507595) for Android build bustages, missing aboutSupport.dtd CLOSED TREE
    Backed out changeset 485ea25a3b82 (bug 1507595)
    Backed out changeset 28805dd1b97a (bug 1507595)

--- browser/base/content/test/about/browser_aboutSupport.js
+++ browser/base/content/test/about/browser_aboutSupport.js
@@ -8,20 +8,25 @@ ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 add_task(async function() {
   await BrowserTestUtils.withNewTab({ gBrowser, url: "about:support" }, async function(browser) {
+    const strings = Services.strings.createBundle(
+                      "chrome://global/locale/aboutSupport.properties");
+    let allowedStates = [strings.GetStringFromName("found"),
+                         strings.GetStringFromName("missing")];
+
     let keyGoogleStatus = await ContentTask.spawn(browser, null, async function() {
       let textBox = content.document.getElementById("key-google-box");
-      await ContentTaskUtils.waitForCondition(() => document.l10n.getAttributes(textBox).id,
+      await ContentTaskUtils.waitForCondition(() => textBox.textContent.trim(),
         "Google API key status loaded");
-      return document.l10n.getAttributes(textBox).id;
+      return textBox.textContent;
     });
-    ok(keyGoogleStatus, "Google API key status shown");
+    ok(allowedStates.includes(keyGoogleStatus), "Google API key status shown");
 
     let keyMozillaStatus = await ContentTask.spawn(browser, null, async function() {
       let textBox = content.document.getElementById("key-mozilla-box");
-      await ContentTaskUtils.waitForCondition(() => document.l10n.getAttributes(textBox).id,
+      await ContentTaskUtils.waitForCondition(() => textBox.textContent.trim(),
         "Mozilla API key status loaded");
-      return document.l10n.getAttributes(textBox).id;
+      return textBox.textContent;
     });
-    ok(keyMozillaStatus, "Mozilla API key status shown");
+    ok(allowedStates.includes(keyMozillaStatus), "Mozilla API key status shown");
   });
 });
