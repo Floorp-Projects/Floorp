2208bac8b45ebe2e899a652d58864d6b43441c67
longsonr <longsonr@gmail.com>
2018-12-24 19:24:45 +0000

    Bug 1516175 - Move nsSMILFloatType into the mozilla namespace r=birtles

--- dom/smil/SMILFloatType.cpp
+++ dom/smil/SMILFloatType.cpp
@@ -4,51 +4,52 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsSMILFloatType.h"
+#include "SMILFloatType.h"
 #include "nsSMILValue.h"
 #include "nsDebug.h"
 #include <math.h>
 
-void nsSMILFloatType::Init(nsSMILValue& aValue) const {
+namespace mozilla {
+
+void SMILFloatType::Init(nsSMILValue& aValue) const {
   MOZ_ASSERT(aValue.IsNull(), "Unexpected value type");
   aValue.mU.mDouble = 0.0;
   aValue.mType = this;
 }
 
-void nsSMILFloatType::Destroy(nsSMILValue& aValue) const {
+void SMILFloatType::Destroy(nsSMILValue& aValue) const {
   MOZ_ASSERT(aValue.mType == this, "Unexpected SMIL value");
   aValue.mU.mDouble = 0.0;
   aValue.mType = nsSMILNullType::Singleton();
 }
 
-nsresult nsSMILFloatType::Assign(nsSMILValue& aDest,
-                                 const nsSMILValue& aSrc) const {
+nsresult SMILFloatType::Assign(nsSMILValue& aDest,
+                               const nsSMILValue& aSrc) const {
   MOZ_ASSERT(aDest.mType == aSrc.mType, "Incompatible SMIL types");
   MOZ_ASSERT(aDest.mType == this, "Unexpected SMIL value");
   aDest.mU.mDouble = aSrc.mU.mDouble;
   return NS_OK;
 }
 
-bool nsSMILFloatType::IsEqual(const nsSMILValue& aLeft,
-                              const nsSMILValue& aRight) const {
+bool SMILFloatType::IsEqual(const nsSMILValue& aLeft,
+                            const nsSMILValue& aRight) const {
   MOZ_ASSERT(aLeft.mType == aRight.mType, "Incompatible SMIL types");
   MOZ_ASSERT(aLeft.mType == this, "Unexpected type for SMIL value");
 
   return aLeft.mU.mDouble == aRight.mU.mDouble;
 }
 
-nsresult nsSMILFloatType::Add(nsSMILValue& aDest,
-                              const nsSMILValue& aValueToAdd,
-                              uint32_t aCount) const {
+nsresult SMILFloatType::Add(nsSMILValue& aDest, const nsSMILValue& aValueToAdd,
+                            uint32_t aCount) const {
   MOZ_ASSERT(aValueToAdd.mType == aDest.mType, "Trying to add invalid types");
   MOZ_ASSERT(aValueToAdd.mType == this, "Unexpected source type");
   aDest.mU.mDouble += aValueToAdd.mU.mDouble * aCount;
   return NS_OK;
 }
 
-nsresult nsSMILFloatType::ComputeDistance(const nsSMILValue& aFrom,
-                                          const nsSMILValue& aTo,
-                                          double& aDistance) const {
+nsresult SMILFloatType::ComputeDistance(const nsSMILValue& aFrom,
+                                        const nsSMILValue& aTo,
+                                        double& aDistance) const {
   MOZ_ASSERT(aFrom.mType == aTo.mType, "Trying to compare different types");
   MOZ_ASSERT(aFrom.mType == this, "Unexpected source type");
 
@@ -60,10 +61,10 @@ nsresult nsSMILFloatType::ComputeDistance(const nsSMILValue& aFrom,
   return NS_OK;
 }
 
-nsresult nsSMILFloatType::Interpolate(const nsSMILValue& aStartVal,
-                                      const nsSMILValue& aEndVal,
-                                      double aUnitDistance,
-                                      nsSMILValue& aResult) const {
+nsresult SMILFloatType::Interpolate(const nsSMILValue& aStartVal,
+                                    const nsSMILValue& aEndVal,
+                                    double aUnitDistance,
+                                    nsSMILValue& aResult) const {
   MOZ_ASSERT(aStartVal.mType == aEndVal.mType,
              "Trying to interpolate different types");
   MOZ_ASSERT(aStartVal.mType == this, "Unexpected types for interpolation");
@@ -76,3 +77,5 @@ nsresult nsSMILFloatType::Interpolate(const nsSMILValue& aStartVal,
 
   return NS_OK;
 }
+
+}  // namespace mozilla
