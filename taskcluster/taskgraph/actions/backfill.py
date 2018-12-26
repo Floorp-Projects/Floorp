7c960b922435f3de98498e01ddf92fd0f693bede
Joel Maher <jmaher@mozilla.com>
2018-12-26 18:43:44 +0000

    Bug 1516452 - fix typo in taskgraph to 'dumps' instead of 'dums'. r=bc,Callek
    fix typo in taskgraph to 'dumps' instead of 'dums'
    
    Differential Revision: https://phabricator.services.mozilla.com/D15390

--- taskcluster/taskgraph/actions/backfill.py
+++ taskcluster/taskgraph/actions/backfill.py
@@ -142,7 +142,7 @@ def backfill_action(parameters, graph_config, input, task_group_id, task_id, tas
                         verify_args.append('--gpu-required')
 
                     if 'testPath' in input:
-                        task.task['payload']['env']['MOZHARNESS_TEST_PATHS'] = json.dums({
+                        task.task['payload']['env']['MOZHARNESS_TEST_PATHS'] = json.dumps({
                             task.task['extra']['suite']['flavor']: input['testPath']
                         })
 
