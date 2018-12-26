2c0484a4b6b690fc83e0199aacbadaae777a48d9
Masayuki Nakano <masayuki@d-toybox.com>
2018-12-26 08:17:32 +0000

    Bug 1516323 - Make IMContextWrapper::OnKeyEvent() check whether coming key event is already in the posting event queue r=m_kato
    According to the log of bug 1498823, ibus won't set IBUS_IGNORED_MASK to
    modifier flags when it synthesizes the event for asynchronous handling in
    some environments.
    
    Currently, we assume that iBus and Fcitx set IBUS_IGNORED_MASK or
    FcitxKeyState_IgnoredMask.  So, we put both real key events and synthesized
    key events into the posting event queue and that causes using a lot of
    memory until the editor is blurred.  Fortunately, timestamp of synthesized
    key events are always same as their original events.  Therefore, we can look
    for original event from the positing event queue.
    
    Although we have gotten no bug reports about this issue of Fcitx, but this
    patch adds same hack for Fcitx too because the runtime cost is not
    expensive but the symptom is really serious.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15321

--- widget/gtk/IMContextWrapper.cpp
+++ widget/gtk/IMContextWrapper.cpp
@@ -838,6 +838,24 @@ KeyHandlingState IMContextWrapper::OnKeyEvent(
         // has already been handled by another process and it wasn't
         // used by IME.
         bool isHandlingAsyncEvent = !!(aEvent->state & IBUS_IGNORED_MASK);
+        if (!isHandlingAsyncEvent) {
+          // On some environments, IBUS_IGNORED_MASK flag is not set as
+          // expected.  In such case, we keep pusing all events into the queue.
+          // I.e., that causes eating a lot of memory until it's blurred.
+          // Therefore, we need to check whether there is same timestamp event
+          // in the queue.  This redundant cost should be low because in most
+          // causes, key events in the queue should be 2 or 4.
+          isHandlingAsyncEvent =
+              mPostingKeyEvents.IndexOf(aEvent) != GdkEventKeyQueue::NoIndex();
+          if (isHandlingAsyncEvent) {
+            MOZ_LOG(gGtkIMLog, LogLevel::Info,
+                    ("0x%p   OnKeyEvent(), aEvent->state does not have "
+                     "IBUS_IGNORED_MASK but "
+                     "same event in the queue.  So, assuming it's a "
+                     "synthesized event",
+                     this));
+          }
+        }
 
         // ibus won't send back key press events in a dead key sequcne.
         if (mMaybeInDeadKeySequence && aEvent->type == GDK_KEY_PRESS) {
@@ -866,10 +884,11 @@ KeyHandlingState IMContextWrapper::OnKeyEvent(
 
         if (isHandlingAsyncEvent) {
           MOZ_LOG(gGtkIMLog, LogLevel::Info,
-                  ("0x%p   OnKeyEvent(), aEvent->state has "
-                   "IBUS_IGNORED_MASK, so, it won't be handled "
-                   "asynchronously anymore. Removing posted events from "
-                   "the queue",
+                  ("0x%p   OnKeyEvent(), aEvent->state has IBUS_IGNORED_MASK "
+                   "or aEvent is in the "
+                   "posting event queue, so, it won't be handled "
+                   "asynchronously anymore. Removing "
+                   "the posted events from the queue",
                    this));
           maybeHandledAsynchronously = false;
           mPostingKeyEvents.RemoveEvent(aEvent);
@@ -885,6 +904,25 @@ KeyHandlingState IMContextWrapper::OnKeyEvent(
         // it wasn't used by IME.
         bool isHandlingAsyncEvent =
             !!(aEvent->state & FcitxKeyState_IgnoredMask);
+        if (!isHandlingAsyncEvent) {
+          // On some environments, FcitxKeyState_IgnoredMask flag *might* be not
+          // set as expected. If there were such cases, we'd keep pusing all
+          // events into the queue.  I.e., that would cause eating a lot of
+          // memory until it'd be blurred.  Therefore, we should check whether
+          // there is same timestamp event in the queue.  This redundant cost
+          // should be low because in most causes, key events in the queue
+          // should be 2 or 4.
+          isHandlingAsyncEvent =
+              mPostingKeyEvents.IndexOf(aEvent) != GdkEventKeyQueue::NoIndex();
+          if (isHandlingAsyncEvent) {
+            MOZ_LOG(gGtkIMLog, LogLevel::Info,
+                    ("0x%p   OnKeyEvent(), aEvent->state does not have "
+                     "FcitxKeyState_IgnoredMask "
+                     "but same event in the queue.  So, assuming it's a "
+                     "synthesized event",
+                     this));
+          }
+        }
 
         // fcitx won't send back key press events in a dead key sequcne.
         if (mMaybeInDeadKeySequence && aEvent->type == GDK_KEY_PRESS) {
@@ -910,9 +948,10 @@ KeyHandlingState IMContextWrapper::OnKeyEvent(
         if (isHandlingAsyncEvent) {
           MOZ_LOG(gGtkIMLog, LogLevel::Info,
                   ("0x%p   OnKeyEvent(), aEvent->state has "
-                   "FcitxKeyState_IgnoredMask, so, it won't be handled "
-                   "asynchronously anymore. Removing posted events from "
-                   "the queue",
+                   "FcitxKeyState_IgnoredMask or aEvent is in "
+                   "the posting event queue, so, it won't be handled "
+                   "asynchronously anymore. "
+                   "Removing the posted events from the queue",
                    this));
           maybeHandledAsynchronously = false;
           mPostingKeyEvents.RemoveEvent(aEvent);
@@ -1000,11 +1039,14 @@ KeyHandlingState IMContextWrapper::OnKeyEvent(
   MOZ_LOG(gGtkIMLog, LogLevel::Debug,
           ("0x%p   OnKeyEvent(), succeeded, filterThisEvent=%s "
            "(isFiltered=%s, mFallbackToKeyEvent=%s, "
-           "maybeHandledAsynchronously=%s), mCompositionState=%s, "
-           "mMaybeInDeadKeySequence=%s",
+           "maybeHandledAsynchronously=%s), mPostingKeyEvents.Length()=%zu, "
+           "mCompositionState=%s, mMaybeInDeadKeySequence=%s, "
+           "mKeyboardEventWasDispatched=%s, mKeyboardEventWasConsumed=%s",
            this, ToChar(filterThisEvent), ToChar(isFiltered),
            ToChar(mFallbackToKeyEvent), ToChar(maybeHandledAsynchronously),
-           GetCompositionStateName(), ToChar(mMaybeInDeadKeySequence)));
+           mPostingKeyEvents.Length(), GetCompositionStateName(),
+           ToChar(mMaybeInDeadKeySequence), ToChar(mKeyboardEventWasDispatched),
+           ToChar(mKeyboardEventWasConsumed)));
 
   if (filterThisEvent) {
     return KeyHandlingState::eHandled;
