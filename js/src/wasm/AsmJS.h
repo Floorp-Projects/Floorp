43520e003c39754b5817fc8ad23904a4712c5ff7
Jeff Walden <jwalden@mit.edu>
2018-12-26 20:59:49 -0600

    Bug 1516697 - Make wasm/AsmJS.h minimally include only what it needs.  r=arai

--- js/src/wasm/AsmJS.h
+++ js/src/wasm/AsmJS.h
@@ -16,10 +16,26 @@
  * limitations under the License.
  */
 
-#ifndef asmjs_asmjs_h
-#define asmjs_asmjs_h
+#ifndef wasm_AsmJS_h
+#define wasm_AsmJS_h
 
-#include "NamespaceImports.h"
+#include "mozilla/Attributes.h"  // MOZ_MUST_USE
+
+#include <stdint.h>  // uint32_t
+
+#include "js/CallArgs.h"  // JSNative
+
+struct JSContext;
+class JSFunction;
+
+namespace JS {
+
+union Value;
+
+template <typename T>
+class Handle;
+
+}  // namespace JS
 
 namespace js {
 
@@ -67,15 +83,16 @@ extern bool IsAsmJSCompilationAvailable(JSContext* cx, unsigned argc,
 extern bool IsAsmJSModule(JSContext* cx, unsigned argc, JS::Value* vp);
 
 extern bool IsAsmJSModuleLoadedFromCache(JSContext* cx, unsigned argc,
-                                         Value* vp);
+                                         JS::Value* vp);
 
 extern bool IsAsmJSFunction(JSContext* cx, unsigned argc, JS::Value* vp);
 
 // asm.js toString/toSource support:
 
-extern JSString* AsmJSFunctionToString(JSContext* cx, HandleFunction fun);
+extern JSString* AsmJSFunctionToString(JSContext* cx,
+                                       JS::Handle<JSFunction*> fun);
 
-extern JSString* AsmJSModuleToString(JSContext* cx, HandleFunction fun,
+extern JSString* AsmJSModuleToString(JSContext* cx, JS::Handle<JSFunction*> fun,
                                      bool isToSource);
 
 // asm.js heap:
@@ -84,4 +101,4 @@ extern bool IsValidAsmJSHeapLength(uint32_t length);
 
 }  // namespace js
 
-#endif  // asmjs_asmjs_h
+#endif  // wasm_AsmJS_h
