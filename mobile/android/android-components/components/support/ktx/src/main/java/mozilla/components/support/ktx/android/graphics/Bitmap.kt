cc025846220b2bd6ed02217a11371c8502f9e4ce
Michael Comella <michael.l.comella@gmail.com>
2018-12-31 12:12:41 -0800

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/1633: Add Bitmap.withRoundedCorners.
    I verified this was working correctly by adding code locally to the
    sample browser.
    
    This has been adapted from Firefox for Echo Show:
      https://github.com/mozilla-mobile/firefox-echo-show/blob/986826e0782b3df4babde905b4549feec438d28f/app/src/main/java/org/mozilla/focus/ext/Bitmap.kt#L36

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/graphics/Bitmap.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/graphics/Bitmap.kt
@@ -5,6 +5,11 @@
 package mozilla.components.support.ktx.android.graphics
 
 import android.graphics.Bitmap
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Shader.TileMode
+import android.support.annotation.CheckResult
 import android.util.Base64
 import java.io.ByteArrayOutputStream
 
@@ -16,4 +21,27 @@ fun Bitmap.toDataUri(): String {
     compress(Bitmap.CompressFormat.PNG, 100, stream)
     val encodedImage = Base64.encodeToString(stream.toByteArray(), Base64.DEFAULT)
     return "data:image/png;base64," + encodedImage
+}
+
+/**
+ * Returns a new bitmap that is the receiver Bitmap with four rounded corners;
+ * the receiver is unmodified.
+ *
+ * This operation is expensive: it requires allocating an identical Bitmap and copying
+ * all of the Bitmap's pixels. Consider these theoretically cheaper alternatives:
+ * - android:background= a drawable with rounded corners
+ * - Wrap your bitmap's ImageView with a layout that masks your view with rounded corners (e.g. CardView)
+ */
+@CheckResult
+fun Bitmap.withRoundedCorners(cornerRadiusPx: Float): Bitmap {
+    val roundedBitmap = Bitmap.createBitmap(width, height, config)
+    val canvas = Canvas(roundedBitmap)
+    val paint = Paint().apply {
+        isAntiAlias = true
+        shader = BitmapShader(this@withRoundedCorners, TileMode.CLAMP, TileMode.CLAMP)
+    }
+
+    canvas.drawRoundRect(0.0f, 0.0f, width.toFloat(), height.toFloat(),
+            cornerRadiusPx, cornerRadiusPx, paint)
+    return roundedBitmap
 }
