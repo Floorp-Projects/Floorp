2da9e648243699f78e4a32e08ce60d856d43406f
Paolo Amadini <paolo.mozmail@amadzone.org>
2019-01-02 09:57:59 +0000

    Bug 1517213 - Part 1 - Share more selectors in tests. r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D15569

--- browser/components/aboutconfig/test/browser/browser_locked.js
+++ browser/components/aboutconfig/test/browser/browser_locked.js
@@ -24,28 +24,28 @@ add_task(async function test_locked() {
     let lockedPref = this.getRow("browser.search.searchEnginesURL");
     Assert.ok(lockedPref.hasClass("locked"));
     Assert.equal(lockedPref.value, "https://addons.mozilla.org/%LOCALE%/firefox/search-engines/");
-    Assert.ok(lockedPref.firstButton.classList.contains("button-edit"));
-    Assert.ok(lockedPref.firstButton.disabled);
+    Assert.ok(lockedPref.editColumnButton.classList.contains("button-edit"));
+    Assert.ok(lockedPref.editColumnButton.disabled);
 
     // Test locked default boolean pref.
     lockedPref = this.getRow("accessibility.AOM.enabled");
     Assert.ok(lockedPref.hasClass("locked"));
     Assert.equal(lockedPref.value, "false");
-    Assert.ok(lockedPref.firstButton.classList.contains("button-toggle"));
-    Assert.ok(lockedPref.firstButton.disabled);
+    Assert.ok(lockedPref.editColumnButton.classList.contains("button-toggle"));
+    Assert.ok(lockedPref.editColumnButton.disabled);
 
     // Test locked user added pref.
     lockedPref = this.getRow("test.aboutconfig.a");
     Assert.ok(lockedPref.hasClass("locked"));
     Assert.equal(lockedPref.value, "");
-    Assert.ok(lockedPref.firstButton.classList.contains("button-edit"));
-    Assert.ok(lockedPref.firstButton.disabled);
+    Assert.ok(lockedPref.editColumnButton.classList.contains("button-edit"));
+    Assert.ok(lockedPref.editColumnButton.disabled);
 
     // Test pref not locked
     let unlockedPref = this.getRow("accessibility.indicator.enabled");
     Assert.ok(!unlockedPref.hasClass("locked"));
     Assert.equal(unlockedPref.value, "false");
-    Assert.ok(unlockedPref.firstButton.classList.contains("button-toggle"));
-    Assert.ok(!unlockedPref.firstButton.disabled);
+    Assert.ok(unlockedPref.editColumnButton.classList.contains("button-toggle"));
+    Assert.ok(!unlockedPref.editColumnButton.disabled);
   });
 });
