fc966356d772d37b93ddc24e972f847370ca0266
Nicolas Chevobbe <nchevobbe@users.noreply.github.com>
2019-01-03 20:11:07 +0000

    Bug 1519019 - Uplift fix to CallStack Group with anonymous function. r=jlast, a=RyanVM
    This is the same patch as in Bug 1517572, applied on the beta branch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16156

--- devtools/client/debugger/new/test/mochitest/browser_dbg-call-stack.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-call-stack.js
@@ -17,6 +17,43 @@ function toggleButton(dbg) {
   return callStackBody.querySelector(".show-more");
 }
 
+// Create an HTTP server to simulate an angular app with anonymous functions
+// and return the URL.
+function createMockAngularPage() {
+  const httpServer = createTestHTTPServer();
+
+  httpServer.registerContentType("html", "text/html");
+  httpServer.registerContentType("js", "application/javascript");
+
+  const htmlFilename = "angular-mock.html"
+  httpServer.registerPathHandler(`/${htmlFilename}`, function(request, response) {
+      response.setStatusLine(request.httpVersion, 200, "OK");
+      response.write(`
+        <html>
+            <button class="pause">Click me</button>
+            <script type="text/javascript" src="angular.js"></script>
+        </html>`
+      );
+    }
+  );
+
+  // Register an angular.js file in order to create a Group with anonymous functions in
+  // the callstack panel.
+  httpServer.registerPathHandler("/angular.js", function(request, response) {
+    response.setHeader("Content-Type", "application/javascript");
+    response.write(`
+      document.querySelector("button.pause").addEventListener("click", () => {
+        (function() {
+          debugger;
+        })();
+      })
+    `);
+  });
+
+  const port = httpServer.identity.primaryPort;
+  return `http://localhost:${port}/${htmlFilename}`;
+}
+
 add_task(async function() {
   const dbg = await initDebugger("doc-script-switching.html");
 
@@ -51,4 +88,28 @@ add_task(async function() {
   is(button.innerText, "Collapse rows", "toggle button should be collapsed");
   is(frames.length, 22, "All of the frames should be shown");
   await waitForSelectedSource(dbg, "frames.js");
+});
+
+
+add_task(async function() {
+  const url = createMockAngularPage();
+  const tab = await addTab(url);
+  info("Open debugger");
+  const toolbox = await openToolboxForTab(tab, "jsdebugger");
+  const dbg = createDebuggerContext(toolbox);
+
+  const found = findElement(dbg, "callStackBody");
+  is(found, null, "Call stack is hidden");
+
+  ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+    content.document.querySelector("button.pause").click();
+  });
+
+  await waitForPaused(dbg);
+  const $group = findElementWithSelector(dbg, ".frames .frames-group");
+  is($group.querySelector(".title").textContent,
+    "<anonymous>", "Group has expected frame title");
+  is($group.querySelector(".badge").textContent, "2", "Group has expected badge");
+  is($group.querySelector(".location").textContent, "Angular",
+    "Group has expected location");
 });
