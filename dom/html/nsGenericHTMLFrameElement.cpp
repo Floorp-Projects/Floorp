e678699e5f6fc58046e9b9ef3906bf2da28d3a51
Ciure Andrei <aciure@mozilla.com>
2019-01-04 01:20:26 +0200

    Bug 1517467 fix nsGenericHTMLFrameElement.cpp CLOSED TREE

--- dom/html/nsGenericHTMLFrameElement.cpp
+++ dom/html/nsGenericHTMLFrameElement.cpp
@@ -76,7 +76,6 @@ Document* nsGenericHTMLFrameElement::GetContentDocument(
     return nullptr;
   }
 
-<<<<<<< working copy
   nsPIDOMWindowOuter* window = bc->GetDOMWindow();
   if (!window) {
     // Either our browsing context contents are out-of-process (in which case
