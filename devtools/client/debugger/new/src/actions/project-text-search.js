3f2c16e039ebbcd9c7c3cef55c50f62966c83a43
Gary Blackwood <gary@garyblackwood.co.uk>
2019-01-03 15:11:51 -0500

    Bug 1517572 - [release 116] Add a way to stop an ongoing project search (#7621). r=dwalsh

--- devtools/client/debugger/new/src/actions/project-text-search.js
+++ devtools/client/debugger/new/src/actions/project-text-search.js
@@ -13,12 +13,21 @@ import { findSourceMatches } from "../workers/search";
 import { getSource, hasPrettySource, getSourceList } from "../selectors";
 import { isThirdParty } from "../utils/source";
 import { loadSourceText } from "./sources/loadSourceText";
-import { statusType } from "../reducers/project-text-search";
+import {
+  statusType,
+  getTextSearchOperation,
+  getTextSearchStatus
+} from "../reducers/project-text-search";
 
 import type { Action, ThunkArgs } from "./types";
+import type { SearchOperation } from "../reducers/project-text-search";
 
 export function addSearchQuery(query: string): Action {
   return { type: "ADD_QUERY", query };
+}
+
+export function addOngoingSearch(ongoingSearch: SearchOperation): Action {
+  return { type: "ADD_ONGOING_SEARCH", ongoingSearch };
 }
 
 export function clearSearchQuery(): Action {
@@ -48,12 +57,31 @@ export function updateSearchStatus(status: string): Action {
   return { type: "UPDATE_STATUS", status };
 }
 
-export function closeProjectSearch(): Action {
-  return { type: "CLOSE_PROJECT_SEARCH" };
+export function closeProjectSearch() {
+  return ({ dispatch, getState }: ThunkArgs) => {
+    dispatch(stopOngoingSearch());
+    dispatch({ type: "CLOSE_PROJECT_SEARCH" });
+  };
+}
+
+export function stopOngoingSearch() {
+  return ({ dispatch, getState }: ThunkArgs) => {
+    const state = getState();
+    const ongoingSearch = getTextSearchOperation(state);
+    const status = getTextSearchStatus(state);
+    if (ongoingSearch && status !== statusType.done) {
+      ongoingSearch.cancel();
+      dispatch(updateSearchStatus(statusType.cancelled));
+    }
+  };
 }
 
 export function searchSources(query: string) {
-  return async ({ dispatch, getState }: ThunkArgs) => {
+  let cancelled = false;
+
+  const search = async ({ dispatch, getState }: ThunkArgs) => {
+    dispatch(stopOngoingSearch());
+    await dispatch(addOngoingSearch(search));
     await dispatch(clearSearchResults());
     await dispatch(addSearchQuery(query));
     dispatch(updateSearchStatus(statusType.fetching));
@@ -61,11 +89,20 @@ export function searchSources(query: string) {
       source => !hasPrettySource(getState(), source.id) && !isThirdParty(source)
     );
     for (const source of validSources) {
+      if (cancelled) {
+        return;
+      }
       await dispatch(loadSourceText(source));
       await dispatch(searchSource(source.id, query));
     }
     dispatch(updateSearchStatus(statusType.done));
   };
+
+  search.cancel = () => {
+    cancelled = true;
+  };
+
+  return search;
 }
 
 export function searchSource(sourceId: string, query: string) {
