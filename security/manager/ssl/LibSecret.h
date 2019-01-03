9b09ad92eaccb0f2479ec62508e4c2673fd9ae3b
Dana Keeler <dkeeler@mozilla.com>
2019-01-03 00:39:45 +0000

    bug 1498909 - dynamically load libsecret at runtime if available r=franziskus,jcj
    Enough linux-based systems don't have libsecret that we can't make it a
    requirement on linux. For those that do, however, we can dynamically load the
    library at runtime. For those that don't, we can fall back to NSS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9969

--- security/manager/ssl/LibSecret.h
+++ security/manager/ssl/LibSecret.h
@@ -4,66 +4,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MOZ_LIB_SECRET
-#error LibSecret OSKeyStore included when MOZ_LIB_SECRET is not defined!
-#endif
-
 #ifndef LibSecret_h
 #define LibSecret_h
 
 #include "OSKeyStore.h"
 
-#include <libsecret/secret.h>
-#include <memory>
-#include <vector>
-
 #include "nsString.h"
 
-struct ScopedDelete {
-  void operator()(SecretService* ss) {
-    if (ss) g_object_unref(ss);
-  }
-  void operator()(SecretCollection* sc) {
-    if (sc) g_object_unref(sc);
-  }
-  void operator()(GError* error) {
-    if (error) g_error_free(error);
-  }
-  void operator()(GList* list) {
-    if (list) g_list_free(list);
-  }
-  void operator()(SecretValue* val) {
-    if (val) secret_value_unref(val);
-  }
-  void operator()(SecretItem* val) {
-    if (val) g_object_unref(val);
-  }
-  void operator()(char* val) {
-    if (val) secret_password_free(val);
-  }
-};
-
-template <class T>
-struct ScopedMaybeDelete {
-  void operator()(T* ptr) {
-    if (ptr) {
-      ScopedDelete del;
-      del(ptr);
-    }
-  }
-};
-
-#define SCOPED(x) typedef std::unique_ptr<x, ScopedMaybeDelete<x>> Scoped##x
-
-SCOPED(SecretService);
-SCOPED(SecretCollection);
-SCOPED(GError);
-SCOPED(GList);
-SCOPED(SecretValue);
-SCOPED(SecretItem);
-typedef std::unique_ptr<char, ScopedMaybeDelete<char>> ScopedPassword;
-
-#undef SCOPED
+nsresult MaybeLoadLibSecret();
 
 class LibSecret final : public AbstractOSKeyStore {
  public:
