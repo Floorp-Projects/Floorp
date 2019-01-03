e3e8924e0d08b52694500bc4a08b77560e30c6a0
Dana Keeler <dkeeler@mozilla.com>
2019-01-03 00:39:45 +0000

    bug 1498909 - dynamically load libsecret at runtime if available r=franziskus,jcj
    Enough linux-based systems don't have libsecret that we can't make it a
    requirement on linux. For those that do, however, we can dynamically load the
    library at runtime. For those that don't, we can fall back to NSS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9969

--- security/manager/ssl/tests/unit/test_oskeystore.js
+++ security/manager/ssl/tests/unit/test_oskeystore.js
@@ -161,3 +161,48 @@ add_task(async function() {
 
   await delete_all_secrets();
 });
+
+// Test that using a recovery phrase works.
+add_task(async function() {
+  await delete_all_secrets();
+
+  let keystore = Cc["@mozilla.org/security/oskeystore;1"]
+                   .getService(Ci.nsIOSKeyStore);
+
+  let recoveryPhrase = await keystore.asyncGenerateSecret(LABELS[0]);
+  ok(recoveryPhrase, "A recovery phrase should've been created.");
+
+  let text = new Uint8Array([0x01, 0x00, 0x01]);
+  let ciphertext = await keystore.asyncEncryptBytes(LABELS[0], text.length, text);
+  ok(ciphertext, "We should have a ciphertext now.");
+
+  await keystore.asyncDeleteSecret(LABELS[0]);
+  // Decrypting should fail after deleting the secret.
+  await keystore.asyncDecryptBytes(LABELS[0], ciphertext)
+    .then(() => ok(false, "decrypting didn't throw as expected after deleting the secret"))
+    .catch(() => ok(true, "decrypting threw as expected after deleting the secret"));
+
+  await keystore.asyncRecoverSecret(LABELS[0], recoveryPhrase);
+  let plaintext = await keystore.asyncDecryptBytes(LABELS[0], ciphertext);
+  ok(plaintext.toString() == text.toString(), "Decrypted plaintext should be the same as text.");
+
+  await delete_all_secrets();
+});
+
+// Test that trying to use a non-base64 recovery phrase fails.
+add_task(async function() {
+  await delete_all_secrets();
+
+  let keystore = Cc["@mozilla.org/security/oskeystore;1"]
+                   .getService(Ci.nsIOSKeyStore);
+  await keystore.asyncRecoverSecret(LABELS[0], "@##$^&*()#$^&*(@#%&*_")
+    .then(() => ok(false, "base64-decoding non-base64 should have failed but didn't"))
+    .catch(() => ok(true, "base64-decoding non-base64 failed as expected"));
+
+  ok(!await keystore.asyncSecretAvailable(LABELS[0]),
+     "we didn't recover a secret, so the secret shouldn't be available");
+  let recoveryPhrase = await keystore.asyncGenerateSecret(LABELS[0]);
+  ok(recoveryPhrase && recoveryPhrase.length > 0,
+     "we should be able to re-use that label to generate a new secret");
+  await delete_all_secrets();
+});
