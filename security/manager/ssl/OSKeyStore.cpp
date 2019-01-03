74e6b5cabe01c40a637b5e0a7b69de856cfdf91d
Dana Keeler <dkeeler@mozilla.com>
2019-01-03 00:39:45 +0000

    bug 1498909 - dynamically load libsecret at runtime if available r=franziskus,jcj
    Enough linux-based systems don't have libsecret that we can't make it a
    requirement on linux. For those that do, however, we can dynamically load the
    library at runtime. For those that don't, we can fall back to NSS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D9969
    
    --HG--
    extra : moz-landing-system : lando

--- security/manager/ssl/OSKeyStore.cpp
+++ security/manager/ssl/OSKeyStore.cpp
@@ -12,12 +12,13 @@
 #include "nsXPCOM.h"
 #include "pk11pub.h"
 
-#ifdef MOZ_LIB_SECRET
-#include "LibSecret.h"
-#elif defined(XP_MACOSX)
+#if defined(XP_MACOSX)
 #include "KeychainSecret.h"
 #elif defined(XP_WIN)
 #include "CredentialManagerSecret.h"
+#elif defined(MOZ_WIDGET_GTK)
+#include "LibSecret.h"
+#include "NSSKeyStore.h"
 #else
 #include "NSSKeyStore.h"
 #endif
@@ -27,8 +28,6 @@ NS_IMPL_ISUPPORTS(OSKeyStore, nsIOSKeyStore, nsIObserver)
 using namespace mozilla;
 using dom::Promise;
 
-mozilla::LazyLogModule gOSKeyStoreLog("oskeystore");
-
 OSKeyStore::OSKeyStore()
     : mKs(nullptr), mKsThread(nullptr), mKsIsNSSKeyStore(false) {
   MOZ_ASSERT(NS_IsMainThread());
@@ -36,12 +35,17 @@ OSKeyStore::OSKeyStore()
     return;
   }
 
-#ifdef MOZ_LIB_SECRET
-  mKs.reset(new LibSecret());
-#elif defined(XP_MACOSX)
+#if defined(XP_MACOSX)
   mKs.reset(new KeychainSecret());
 #elif defined(XP_WIN)
   mKs.reset(new CredentialManagerSecret());
+#elif defined(MOZ_WIDGET_GTK)
+  if (NS_SUCCEEDED(MaybeLoadLibSecret())) {
+    mKs.reset(new LibSecret());
+  } else {
+    mKs.reset(new NSSKeyStore());
+    mKsIsNSSKeyStore = true;
+  }
 #else
   mKs.reset(new NSSKeyStore());
   mKsIsNSSKeyStore = true;
