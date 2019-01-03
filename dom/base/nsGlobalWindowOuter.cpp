a128f0c35c212d4bf3a1ae01cab73d9166b20ec1
Jeff Walden <jwalden@mit.edu>
2019-01-03 15:37:01 -0600

    Bug 1517624 - Move JSPropertySpec and JSFunctionSpec definitions into a new js/public/PropertySpec.h header.  r=arai
    --HG--
    extra : rebase_source : b80911e01f984ee9f99fbbc6c4552ef3d2178587

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -74,6 +74,7 @@
 // Helper Classes
 #include "nsJSUtils.h"
 #include "jsapi.h"
+#include "js/PropertySpec.h"
 #include "js/Wrapper.h"
 #include "nsCharSeparatedTokenizer.h"
 #include "nsReadableUtils.h"
