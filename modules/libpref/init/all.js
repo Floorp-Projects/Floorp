28b6241ea7029b292cf786025ba24795ab576f76
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2019-01-04 07:21:53 +0000

    Bug 1517682 - Drop layout.css.scroll-behavior.property-enabled pref. r=heycam
    It was enabled by default in bug 1041833 (for desktops) and
    bug 1087562 (for Fennect).
    
    Depends on D15706
    
    Differential Revision: https://phabricator.services.mozilla.com/D15707

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3104,9 +3104,6 @@ pref("layout.css.outline-style-auto.enabled", false);
 // Is CSSOM-View scroll-behavior and its MSD smooth scrolling enabled?
 pref("layout.css.scroll-behavior.enabled", true);
 
-// Is the CSSOM-View scroll-behavior CSS property enabled?
-pref("layout.css.scroll-behavior.property-enabled", true);
-
 // Tuning of the smooth scroll motion used by CSSOM-View scroll-behavior.
 // Spring-constant controls the strength of the simulated MSD
 // (Mass-Spring-Damper)
