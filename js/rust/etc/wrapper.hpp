b57a125e58bf717efd78f52a90e80acf8ec77d40
Jeff Walden <jwalden@mit.edu>
2019-01-04 12:32:07 -0600

    Bug 1517624 - Followup fix for the sm-rust job, r=bustage, r=me as trivial

--- js/rust/etc/wrapper.hpp
+++ js/rust/etc/wrapper.hpp
@@ -19,6 +19,7 @@ typedef uint32_t HashNumber;
 #include "js/Date.h"
 #include "js/Initialization.h"
 #include "js/MemoryMetrics.h"
+#include "js/PropertySpec.h"
 #include "js/SourceText.h"
 #include "js/StructuredClone.h"
 
