ca04e99cddad1228f31c231b85aeabe09403e4c9
Dave Hunt <dhunt@mozilla.com>
2019-01-04 17:04:27 +0000

    Bug 1517270 - Add pageload measurement types to --print-tests output; r=rwood
    Differential Revision: https://phabricator.services.mozilla.com/D15642

---
+++ testing/raptor/test/test_print_tests.py
@@ -0,0 +1,45 @@
+import os
+
+import mozunit
+import pytest
+
+from raptor import cmdline
+
+
+def test_pageload_subtests(capsys, monkeypatch, tmpdir):
+    # cmdline.py is hard-coded to use raptor.ini from the same directory so we need
+    # to monkey patch os.dirname, which is not ideal. If we could make --print-tests
+    # respect the --test path that would be much better.
+    def mock(path):
+        return str(tmpdir)
+    monkeypatch.setattr(os.path, "dirname", mock)
+    manifest = tmpdir.join("raptor.ini")
+    manifest.write("""
+[DEFAULT]
+type = pageload
+apps = firefox
+
+[raptor-subtest-1]
+measure = foo, bar
+
+[raptor-subtest-2]
+""")
+    with pytest.raises(SystemExit):
+        cmdline.parse_args(["--print-tests"])
+    captured = capsys.readouterr()
+    assert captured.out == """
+Raptor Tests Available for Firefox Desktop
+==========================================
+
+raptor
+  type: pageload
+  subtests:
+    raptor-subtest-1 (foo, bar)
+    raptor-subtest-2
+
+Done.
+"""
+
+
+if __name__ == '__main__':
+    mozunit.main()
