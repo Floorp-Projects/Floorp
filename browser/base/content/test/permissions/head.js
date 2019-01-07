c3d58cfb329e7786de61317a6dca61782061881a
alwu <alwu@mozilla.com>
2019-01-07 18:32:35 +0000

    Bug 1513039 - part2 : remove temporary autoplay permission tests. r=florian
    These tests were added in bug 1493766 for testing caching temporary autoplay permission.
    
    Differential Revision: https://phabricator.services.mozilla.com/D14326
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/permissions/head.js
+++ browser/base/content/test/permissions/head.js
@@ -1,8 +1,1 @@
 ChromeUtils.import("resource:///modules/SitePermissions.jsm", this);
-
-async function openTabContextMenu() {
-  let contextMenu = document.getElementById("tabContextMenu");
-  let popupShownPromise = BrowserTestUtils.waitForEvent(contextMenu, "popupshown");
-  EventUtils.synthesizeMouseAtCenter(gBrowser.selectedTab, {type: "contextmenu", button: 2});
-  await popupShownPromise;
-}
