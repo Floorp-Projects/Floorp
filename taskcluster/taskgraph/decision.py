715395ccd687c5f43f455a8e7da15154730c6ed0
Dustin J. Mitchell <dustin@mozilla.com>
2019-01-07 21:06:20 +0000

    Bug 1513480 - stop writing out deprecated runnable-jobs.json.gz r=emorley
    Differential Revision: https://phabricator.services.mozilla.com/D15849

--- taskcluster/taskgraph/decision.py
+++ taskcluster/taskgraph/decision.py
@@ -158,7 +158,6 @@ def taskgraph_decision(options, parameters=None):
     write_artifact('full-task-graph.json', full_task_json)
 
     # write out the public/runnable-jobs.json file
-    write_artifact('runnable-jobs.json.gz', full_task_graph_to_runnable_jobs(full_task_json))
     write_artifact('runnable-jobs.json', full_task_graph_to_runnable_jobs(full_task_json))
 
     # this is just a test to check whether the from_json() function is working
