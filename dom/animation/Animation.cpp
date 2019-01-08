4b3e165e2f8c69a4ca738d4598a39888db0f3e28
Brian Birtles <birtles@gmail.com>
2019-01-08 06:45:18 +0000

    Bug 1518374 - Fix a bunch of fallout from changes to mozilla::dom::Document; r=hiro
    Bug 1517241 renamed nsIDocument to mozilla::dom::Document but unfortunately in
    the process it messed up the ordering of includes which, according to the coding
    style[1], should be alphabetically sorted.
    
    Also, in TimingParams.cpp it didn't add the dom::* prefix so when the unified
    build chunking changes, if the "using namespace mozilla::dom" declaration
    disappears from the chunk, it will fail to build.
    
    [1] https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Coding_Style#CC_practices
    
    Depends on D15902
    
    Differential Revision: https://phabricator.services.mozilla.com/D15903

--- dom/animation/Animation.cpp
+++ dom/animation/Animation.cpp
@@ -8,6 +8,7 @@
 #include "AnimationUtils.h"
 #include "mozilla/dom/AnimationBinding.h"
 #include "mozilla/dom/AnimationPlaybackEvent.h"
+#include "mozilla/dom/Document.h"
 #include "mozilla/dom/DocumentTimeline.h"
 #include "mozilla/AnimationEventDispatcher.h"
 #include "mozilla/AnimationTarget.h"
@@ -16,7 +17,6 @@
 #include "mozilla/TypeTraits.h"     // For std::forward<>
 #include "nsAnimationManager.h"     // For CSSAnimation
 #include "nsDOMMutationObserver.h"  // For nsAutoAnimationMutationBatch
-#include "mozilla/dom/Document.h"
 #include "nsIPresShell.h"           // For nsIPresShell
 #include "nsThreadUtils.h"  // For nsRunnableMethod and nsRevocableEventPtr
 #include "nsTransitionManager.h"      // For CSSTransition
