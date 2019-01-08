9f20b8d995ad34ba0d0d6f5b1d476230d6ef2130
Sebastian Kaspari <s.kaspari@gmail.com>
2019-01-08 10:11:38 +0100

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/1489: InlineAutocompleteEditText: Remove unused import.

--- mobile/android/android-components/components/ui/autocomplete/src/main/java/mozilla/components/ui/autocomplete/InlineAutocompleteEditText.kt
+++ mobile/android/android-components/components/ui/autocomplete/src/main/java/mozilla/components/ui/autocomplete/InlineAutocompleteEditText.kt
@@ -20,7 +20,6 @@ import android.text.TextUtils
 import android.text.TextWatcher
 import android.text.style.BackgroundColorSpan
 import android.util.AttributeSet
-import android.util.Log
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
