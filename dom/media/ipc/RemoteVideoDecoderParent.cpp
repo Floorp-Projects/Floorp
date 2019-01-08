71f2cb57bfb5e8df0323e8eccb54dd0eaa893c85
John Lin <jolin@mozilla.com>
2019-01-08 20:57:34 +0000

    Bug 1508434 - p1: Support explicitly resetting seek threshold. r=jya
    Usually the threshold is reset internally in MediaDataDecoder subclasses
    that support the hint in their Flush() implementations so the value
    will start fresh after seeking completed. But sometimes when there are
    consecutive seek requests, MediaFormatReader::DecoderData::Flush() could
    return early because DecoderData::mFlushed stays true when there is no
    sample demuxed yet, and the threshold will not be cleared. Also, in
    MediaFormatReader::SetVideoDecodeThreshold() we decide not to set the
    hint when the seek target is close to EOS by checking the existence of
    the next keyframe, and that could fail when there are gaps between MSE
    buffered ranges. To make sure the hint is never out of date, we should
    clear it rather than leaving it untouched.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15227

--- dom/media/ipc/RemoteVideoDecoderParent.cpp
+++ dom/media/ipc/RemoteVideoDecoderParent.cpp
@@ -204,7 +204,9 @@ mozilla::ipc::IPCResult RemoteVideoDecoderParent::RecvSetSeekThreshold(
     const int64_t& aTime) {
   MOZ_ASSERT(!mDestroyed);
   MOZ_ASSERT(OnManagerThread());
-  mDecoder->SetSeekThreshold(TimeUnit::FromMicroseconds(aTime));
+  mDecoder->SetSeekThreshold(aTime == INT64_MIN
+                                 ? TimeUnit::Invalid()
+                                 : TimeUnit::FromMicroseconds(aTime));
   return IPC_OK();
 }
 
