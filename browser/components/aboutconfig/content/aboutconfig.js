eb32faadeedc85786ac0183e4302ddb8b436ea4b
Paolo Amadini <paolo.mozmail@amadzone.org>
2019-01-08 15:08:04 +0000

    Bug 1501410 - Part 4 - Make the filter case insensitive. r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D15947
    
    --HG--
    extra : rebase_source : b3a2788b0ac8444b2942aa6aeeacd4a5c954ec90

--- browser/components/aboutconfig/content/aboutconfig.js
+++ browser/components/aboutconfig/content/aboutconfig.js
@@ -26,7 +26,7 @@ let gElementToPrefMap = new WeakMap();
 let gPrefInEdit = null;
 
 /**
- * Substring that should be contained in the preference name.
+ * Lowercase substring that should be contained in the preference name.
  */
 let gFilterString = null;
 
@@ -81,7 +81,7 @@ class PrefRow {
   }
 
   get matchesFilter() {
-    return !gFilterString || this.name.includes(gFilterString);
+    return !gFilterString || this.name.toLowerCase().includes(gFilterString);
   }
 
   _setupElement() {
@@ -326,7 +326,7 @@ function filterPrefs() {
   gDeletedPrefs.clear();
 
   let searchName = document.getElementById("search").value.trim();
-  gFilterString = searchName;
+  gFilterString = searchName.toLowerCase();
   let prefArray = [...gExistingPrefs.values()];
   if (gFilterString) {
     prefArray = prefArray.filter(pref => pref.matchesFilter);
