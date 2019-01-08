f10ced4f9d8a9d96141d0e016a1e4e27933d25aa
Andrea Marchesini <amarchesini@mozilla.com>
2019-01-08 23:05:41 +0100

    Bug 1513298 - Fingerprinting url-classifier feature, r=dimi

--- toolkit/components/antitracking/AntiTrackingCommon.cpp
+++ toolkit/components/antitracking/AntiTrackingCommon.cpp
@@ -1372,7 +1372,9 @@ nsresult AntiTrackingCommon::IsOnContentBlockingAllowList(
   if ((aPurpose == eStorageChecks &&
        !StaticPrefs::browser_contentblocking_allowlist_storage_enabled()) ||
       (aPurpose == eTrackingAnnotations &&
-       !StaticPrefs::browser_contentblocking_allowlist_annotations_enabled())) {
+       !StaticPrefs::browser_contentblocking_allowlist_annotations_enabled()) ||
+      (aPurpose == eFingerprinting &&
+       !StaticPrefs::privacy_trackingprotection_fingerprinting_enabled())) {
     LOG(
         ("Attempting to check the content blocking allow list aborted because "
          "the third-party cookies UI has been disabled."));
