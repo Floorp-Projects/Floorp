0058d992adebac2e2536d4c61e3c67631302b423
Johan Lorenzo <jlorenzo@mozilla.com>
2019-01-08 15:36:25 +0100

    [components] Bug 1512631 - part 12: Run builds as a single command

--- mobile/android/android-components/automation/taskcluster/decision_task_publish.py
+++ mobile/android/android-components/automation/taskcluster/decision_task_publish.py
@@ -70,8 +70,8 @@ def generate_build_task(version, artifact_info, is_snapshot, is_staging):
         command=(
             checkout +
             ' && ./gradlew --no-daemon clean ' +
-            ' && ./gradlew --no-daemon ' + snapshot_flag + gradle_tasks_for_this_module_only +
-            ' && ./gradlew --no-daemon uploadArchives zipMavenArtifacts'
+            snapshot_flag + gradle_tasks_for_this_module_only +
+            ' uploadArchives zipMavenArtifacts'
         ),
         features={
             'chainOfTrust': True,
