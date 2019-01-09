9ee3439023b86e3614384526e273dfe8e645764b
Nathan Froyd <froydnj@mozilla.com>
2019-01-08 19:31:40 -0500

    Bug 1513615 - part 2 - move some code around in PostTimerEvent; r=glandium
    Doing this code movement separately will ideally make the next part of
    this work easier to review.  The idea is that we want to extract all the
    necessary information from `timer` before we pass ownership of it into
    the newly-allocated nsTimerEvent.

--- xpcom/threads/TimerThread.cpp
+++ xpcom/threads/TimerThread.cpp
@@ -715,6 +715,16 @@ already_AddRefed<nsTimerImpl> TimerThread::PostTimerEvent(
   // event, so we can avoid firing a timer that was re-initialized after being
   // canceled.
 
+#ifdef MOZ_TASK_TRACER
+  // During the dispatch of TimerEvent, we overwrite the current TraceInfo
+  // partially with the info saved in timer earlier, and restore it back by
+  // AutoSaveCurTraceInfo.
+  AutoSaveCurTraceInfo saveCurTraceInfo;
+  (timer->GetTracedTask()).SetTLSTraceInfo();
+#endif
+
+  nsCOMPtr<nsIEventTarget> target = timer->mEventTarget;
+
   void* p = nsTimerEvent::operator new(sizeof(nsTimerEvent));
   if (!p) {
     return timer.forget();
@@ -725,15 +735,6 @@ already_AddRefed<nsTimerImpl> TimerThread::PostTimerEvent(
     event->mInitTime = TimeStamp::Now();
   }
 
-#ifdef MOZ_TASK_TRACER
-  // During the dispatch of TimerEvent, we overwrite the current TraceInfo
-  // partially with the info saved in timer earlier, and restore it back by
-  // AutoSaveCurTraceInfo.
-  AutoSaveCurTraceInfo saveCurTraceInfo;
-  (timer->GetTracedTask()).SetTLSTraceInfo();
-#endif
-
-  nsCOMPtr<nsIEventTarget> target = timer->mEventTarget;
   event->SetTimer(timer.forget());
 
   nsresult rv;
