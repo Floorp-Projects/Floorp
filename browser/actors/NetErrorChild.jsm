10a234deb5112f0745bb34bd38b6a2e21c45d990
Johann Hofmann <jhofmann@mozilla.com>
2019-01-09 17:23:18 +0000

    Bug 1414804 - Fix test for expired certificate "helpful" messaging to users with broken system clocks. r=Gijs
    We initially thought we would need a new test certificate to be able to test
    clock skew, but I found that we can reliably produce the clock skew page by
    making Firefox believe that the validity range of the cert used by
    expired.example.com is the correct date. How we do that is described in
    more detail in the test comments.
    
    This patch also updates the formatter for the new clock skew page to not
    show hours and minutes, as it did previously on the old clock skew warnings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13690

--- browser/actors/NetErrorChild.jsm
+++ browser/actors/NetErrorChild.jsm
@@ -475,6 +475,9 @@ class NetErrorChild extends ActorChild {
 
         let now = Date.now();
         let certRange = this._getCertValidityRange(docShell);
+        let formatter = new Services.intl.DateTimeFormat(undefined, {
+          dateStyle: "short",
+        });
 
         let approximateDate = now - difference * 1000;
         // If the difference is more than a day, we last fetched the date in the last 5 days,
@@ -482,9 +485,6 @@ class NetErrorChild extends ActorChild {
         if (Math.abs(difference) > 60 * 60 * 24 && (now - lastFetched) <= 60 * 60 * 24 * 5 &&
             certRange.notBefore < approximateDate && certRange.notAfter > approximateDate) {
           clockSkew = true;
-          let formatter = new Services.intl.DateTimeFormat(undefined, {
-            dateStyle: "short",
-          });
           let systemDate = formatter.format(new Date());
           // negative difference means local time is behind server time
           approximateDate = formatter.format(new Date(approximateDate));
@@ -514,9 +514,6 @@ class NetErrorChild extends ActorChild {
           // since the build date.
           if (buildDate > systemDate && new Date(certRange.notAfter) > buildDate) {
             clockSkew = true;
-            let formatter = new Services.intl.DateTimeFormat(undefined, {
-              dateStyle: "short",
-            });
 
             doc.getElementById("wrongSystemTimeWithoutReference_URL")
               .textContent = doc.location.hostname;
@@ -527,8 +524,7 @@ class NetErrorChild extends ActorChild {
         if (!newErrorPagesEnabled) {
           break;
         }
-        let dateOptions = { year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
-        let systemDate = new Services.intl.DateTimeFormat(undefined, dateOptions).format(new Date());
+        let systemDate = formatter.format(new Date());
         doc.getElementById("wrongSystemTime_systemDate1").textContent = systemDate;
         if (clockSkew) {
           doc.body.classList.add("illustrated", "clockSkewError");
---
+++ browser/base/content/test/about/browser_aboutCertError_clockSkew.js
@@ -0,0 +1,84 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const PREF_SERVICES_SETTINGS_CLOCK_SKEW_SECONDS = "services.settings.clock_skew_seconds";
+const PREF_SERVICES_SETTINGS_LAST_FETCHED = "services.settings.last_update_seconds";
+const PREF_NEW_CERT_ERRORS = "browser.security.newcerterrorpage.enabled";
+
+add_task(async function checkWrongSystemTimeWarning() {
+  Services.prefs.setBoolPref(PREF_NEW_CERT_ERRORS, true);
+
+  async function setUpPage() {
+    let browser;
+    let certErrorLoaded;
+    await BrowserTestUtils.openNewForegroundTab(gBrowser, () => {
+      gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, "https://expired.example.com/");
+      browser = gBrowser.selectedBrowser;
+      certErrorLoaded = BrowserTestUtils.waitForErrorPage(browser);
+    }, false);
+
+    info("Loading and waiting for the cert error");
+    await certErrorLoaded;
+
+    return ContentTask.spawn(browser, null, async function() {
+      let doc = content.document;
+      let div = doc.getElementById("errorShortDescText");
+      let systemDateDiv = doc.getElementById("wrongSystemTime_systemDate1");
+      let learnMoreLink = doc.getElementById("learnMoreLink");
+
+      await ContentTaskUtils.waitForCondition(() => div.textContent.includes("update your computer clock"),
+         "Correct error message found");
+
+      return {
+        divDisplay: content.getComputedStyle(div).display,
+        text: div.textContent,
+        systemDate: systemDateDiv.textContent,
+        learnMoreLink: learnMoreLink.href,
+      };
+    });
+  }
+
+  // Pretend that we recently updated our kinto clock skew pref
+  Services.prefs.setIntPref(PREF_SERVICES_SETTINGS_LAST_FETCHED, Math.floor(Date.now() / 1000));
+
+  let formatter = new Services.intl.DateTimeFormat(undefined, {
+    dateStyle: "short",
+  });
+
+  // For this test, we want to trick Firefox into believing that
+  // the local system time (as returned by Date.now()) is wrong.
+  // Because we don't want to actually change the local system time,
+  // we will do the following:
+
+  // Take the validity date of our test page (expired.example.com).
+  let expiredDate = new Date("2010/01/05 12:00");
+  let localDate = Date.now();
+
+  // Compute the difference between the server date and the correct
+  // local system date.
+  let skew = Math.floor((localDate - expiredDate) / 1000);
+
+  // Make it seem like our reference server agrees that the certificate
+  // date is correct by recording the difference as clock skew.
+  Services.prefs.setIntPref(PREF_SERVICES_SETTINGS_CLOCK_SKEW_SECONDS, skew);
+
+  let localDateFmt = formatter.format(localDate);
+
+  info("Loading a bad cert page with a skewed clock");
+  let message = await setUpPage();
+
+  isnot(message.divDisplay, "none", "Wrong time message information is visible");
+  ok(message.text.includes("update your computer clock"),
+     "Correct error message found");
+  ok(message.text.includes("expired.example.com"), "URL found in error message");
+  ok(message.systemDate.includes(localDateFmt), "Correct local date displayed");
+  ok(message.learnMoreLink.includes("time-errors"), "time-errors in the Learn More URL");
+
+  BrowserTestUtils.removeTab(gBrowser.selectedTab);
+
+  Services.prefs.clearUserPref(PREF_SERVICES_SETTINGS_LAST_FETCHED);
+  Services.prefs.clearUserPref(PREF_SERVICES_SETTINGS_CLOCK_SKEW_SECONDS);
+  Services.prefs.clearUserPref(PREF_NEW_CERT_ERRORS);
+});
