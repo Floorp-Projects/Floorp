6b988936dadb46021ae353d747f7c3e2b43dc73c
Adam Gashlin <agashlin@mozilla.com>
2019-01-09 19:57:31 +0000

    Bug 1514898 - Disable user impersonation on release r=rstrong
    This introduces a DISABLE_USER_IMPERSONATION define when EARLY_BETA_OR_EARLIER is set, if that is present the maintenance service will not attempt to get an impersonation token for the user's updater process, and the updater will not attempt to use any token it is given. The bulk of the changes are restoring the old failure status codes and the tests that expect them, sorry for the noise that causes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15781
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/update/tests/unit_base_updater/head_update.js
+++ toolkit/mozapps/update/tests/unit_base_updater/head_update.js
@@ -6,3 +6,7 @@ const IS_SERVICE_TEST = false;
 
 /* import-globals-from ../data/xpcshellUtilsAUS.js */
 load("../data/xpcshellUtilsAUS.js");
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  AppConstants: "resource://gre/modules/AppConstants.jsm",
+});
