0f45460246fdc2f82d0fde1a1cf8d163c4039d4c
Doug Thayer <dothayer@mozilla.com>
2019-01-10 16:59:47 +0000

    Bug 1441308 - Add pref to disable texture cache clear r=bholley
    To facilitate testing of document splitting before it is preffed on,
    I'm adding a pref to disable clearing the texture cache, since this
    will currently crash the browser with doc splitting on.
    
    Depends on D13840
    
    Differential Revision: https://phabricator.services.mozilla.com/D13841

--- gfx/wr/webrender/src/texture_cache.rs
+++ gfx/wr/webrender/src/texture_cache.rs
@@ -555,6 +555,13 @@ impl TextureCache {
     /// Clear all standalone textures in the cache.
     pub fn clear_standalone(&mut self) {
         debug_assert!(!self.now.is_valid());
+        // This pref just helps us avoid crashes when we begin using multiple documents.
+        // What we need to do for clear to work correctly with multiple documents is
+        // to ensure that we generate frames for all documents whenever we do this.
+        if self.debug_flags.contains(DebugFlags::TEXTURE_CACHE_DBG_DISABLE_SHRINK) {
+            return;
+        }
+
         let mut per_doc_data = mem::replace(&mut self.per_doc_data, FastHashMap::default());
         for (&_, doc_data) in per_doc_data.iter_mut() {
             let standalone_entry_handles = mem::replace(
@@ -573,6 +580,13 @@ impl TextureCache {
 
     /// Clear all shared textures in the cache.
     pub fn clear_shared(&mut self) {
+        // This pref just helps us avoid crashes when we begin using multiple documents.
+        // What we need to do for clear to work correctly with multiple documents is
+        // to ensure that we generate frames for all documents whenever we do this.
+        if self.debug_flags.contains(DebugFlags::TEXTURE_CACHE_DBG_DISABLE_SHRINK) {
+            return;
+        }
+
         self.unset_doc_data();
         let mut per_doc_data = mem::replace(&mut self.per_doc_data, FastHashMap::default());
         for (&_, doc_data) in per_doc_data.iter_mut() {
