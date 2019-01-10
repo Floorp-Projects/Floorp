53ffe1263e4a10cbff0cdd68881c28e750cc78a2
Julian Descottes <jdescottes@mozilla.com>
2019-01-10 17:28:27 +0000

    Bug 1510654 - Extract service worker actors to dedicated files;r=ochameau,ladybenko
    Depends on D15377
    
    Differential Revision: https://phabricator.services.mozilla.com/D15378

---
+++ devtools/server/actors/worker/push-subscription.js
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const protocol = require("devtools/shared/protocol");
+const { pushSubscriptionSpec } =
+  require("devtools/shared/specs/worker/push-subscription");
+
+const PushSubscriptionActor = protocol.ActorClassWithSpec(pushSubscriptionSpec, {
+  initialize(conn, subscription) {
+    protocol.Actor.prototype.initialize.call(this, conn);
+    this._subscription = subscription;
+  },
+
+  form(detail) {
+    if (detail === "actorid") {
+      return this.actorID;
+    }
+    const subscription = this._subscription;
+    return {
+      actor: this.actorID,
+      endpoint: subscription.endpoint,
+      pushCount: subscription.pushCount,
+      lastPush: subscription.lastPush,
+      quota: subscription.quota,
+    };
+  },
+
+  destroy() {
+    this._subscription = null;
+    protocol.Actor.prototype.destroy.call(this);
+  },
+});
+exports.PushSubscriptionActor = PushSubscriptionActor;
