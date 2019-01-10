e613beacdd766a8486680781607e46e9215c17eb
Tooru Fujisawa <arai_a@mac.com>
2019-01-10 02:53:50 +0000

    Bug 1510586 - Integrate clang-format into BinAST code generator. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D15059

--- js/src/frontend/binsource/build.sh
+++ js/src/frontend/binsource/build.sh
@@ -7,3 +7,11 @@ cargo run -- \
       --out-impl ../BinASTParser.cpp   \
       --out-enum ../BinASTEnum.h    \
       --out-token ../BinToken.h
+
+MACH=../../../../mach
+
+${MACH} clang-format --path \
+        ../BinASTParser.h \
+        ../BinASTParser.cpp \
+        ../BinASTEnum.h \
+        ../BinToken.h
