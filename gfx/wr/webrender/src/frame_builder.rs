908948027e954664f364d3f1c5087422edcf13a3
Doug Thayer <dothayer@mozilla.com>
2019-01-10 16:59:29 +0000

    Bug 1441308 - Make GpuCache document-aware r=kvark
    GpuCache can currently evict things out from underneath docs which
    are not updating this frame. This makes its roots document-specific,
    so that we only evict items for currently updating documents.
    
    Depends on D13343
    
    Differential Revision: https://phabricator.services.mozilla.com/D13840

--- gfx/wr/webrender/src/frame_builder.rs
+++ gfx/wr/webrender/src/frame_builder.rs
@@ -392,7 +392,7 @@ impl FrameBuilder {
             .set(self.prim_store.prim_count());
 
         resource_cache.begin_frame(stamp);
-        gpu_cache.begin_frame(stamp.frame_id());
+        gpu_cache.begin_frame(stamp);
 
         let mut transform_palette = TransformPalette::new();
         clip_scroll_tree.update_tree(
@@ -498,7 +498,7 @@ impl FrameBuilder {
             }
         }
 
-        let gpu_cache_frame_id = gpu_cache.end_frame(gpu_cache_profile);
+        let gpu_cache_frame_id = gpu_cache.end_frame(gpu_cache_profile).frame_id();
 
         render_tasks.write_task_data(device_pixel_scale);
 
