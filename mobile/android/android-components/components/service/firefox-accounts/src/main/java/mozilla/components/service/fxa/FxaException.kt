491a7426202eb7d8b57aa2a06e1edb9ea025003d
Edouard Oger <eoger@fastmail.com>
2019-01-10 18:42:48 -0500

    [components] Add FxaException.Network documentation
    Connects with https://github.com/mozilla/application-services/pull/535

--- mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/FxaException.kt
+++ mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/FxaException.kt
@@ -6,6 +6,7 @@ package mozilla.components.service.fxa
  * Broken down into the following subclasses:
  *
  * - `FxaException.Unauthorized`: Thrown when the operation requires additional authorization.
+ * - `FxaException.Network`: Thrown on a network error.
  * - `FxaException.Panic`: Thrown when the Rust library hits an assertion or panic (this is always
  *   a bug).
  * - `FxaException.Unspecified`: Thrown when the Rust library hits an unexpected error that isn't
