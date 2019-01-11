ba80fe4582621045b0d584ec3b53c1fed2dfb4eb
Steve Fink <sfink@mozilla.com>
2019-01-11 15:43:56 -0800

    Bug 1507322 - mark implicit edges via mark stack instead of eagerly, r=jonco
    --HG--
    extra : topic : weakmap.stack
    extra : rebase_source : 9c48dcb63a197b01fcca393f0510e4ef29a3ff45
    extra : amend_source : 9b6189116b138863ab65a025eb39eb232dbd5bc6

--- js/src/gc/Marking.cpp
+++ js/src/gc/Marking.cpp
@@ -677,6 +677,10 @@ void GCMarker::markImplicitEdges(T* thing) {
   markImplicitEdgesHelper<typename ImplicitEdgeHolderType<T*>::Type>(thing);
 }
 
+template void GCMarker::markImplicitEdges(JSObject*);
+template void GCMarker::markImplicitEdges(JSScript*);
+template void GCMarker::markImplicitEdges(LazyScript*);
+
 }  // namespace js
 
 template <typename T>
@@ -866,7 +870,6 @@ void js::GCMarker::markAndPush(T* thing) {
     return;
   }
   pushTaggedPtr(thing);
-  markImplicitEdges(thing);
 }
 namespace js {
 template <>
@@ -1026,6 +1029,10 @@ void LazyScript::traceChildren(JSTracer* trc) {
   GCPtrFunction* innerFunctions = this->innerFunctions();
   for (auto i : IntegerRange(numInnerFunctions())) {
     TraceEdge(trc, &innerFunctions[i], "lazyScriptInnerFunction");
+  }
+
+  if (trc->isMarkingTracer()) {
+    return GCMarker::fromTracer(trc)->markImplicitEdges(this);
   }
 }
 inline void js::GCMarker::eagerlyMarkChildren(LazyScript* thing) {
