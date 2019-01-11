788d8b245457bdef9d1cf062d051e6db45a3b6ae
Cameron McCormack <cam@mcc.id.au>
2019-01-11 03:12:28 +0000

    Bug 1519006 - Stop null checking infallible style sheet accessors in nsDocumentViewer::CreateStyleSet r=xidorn
    Differential Revision: https://phabricator.services.mozilla.com/D16151
    
    --HG--
    extra : moz-landing-system : lando

--- layout/style/nsLayoutStylesheetCache.cpp
+++ layout/style/nsLayoutStylesheetCache.cpp
@@ -50,11 +50,11 @@ nsresult nsLayoutStylesheetCache::Observe(nsISupports* aSubject,
 }
 
 #define STYLE_SHEET(identifier_, url_, lazy_)                                  \
-  StyleSheet* nsLayoutStylesheetCache::identifier_##Sheet() {                  \
+  NotNull<StyleSheet*> nsLayoutStylesheetCache::identifier_##Sheet() {         \
     if (lazy_ && !m##identifier_##Sheet) {                                     \
       LoadSheetURL(url_, &m##identifier_##Sheet, eAgentSheetFeatures, eCrash); \
     }                                                                          \
-    return m##identifier_##Sheet;                                              \
+    return WrapNotNull(m##identifier_##Sheet);                                 \
   }
 #include "mozilla/UserAgentStyleSheetList.h"
 #undef STYLE_SHEET
