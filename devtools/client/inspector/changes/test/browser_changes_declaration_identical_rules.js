fd78dd4a36a80f86d8906628b8d9d554434cf3de
Razvan Caliman <rcaliman@mozilla.com>
2019-01-11 11:24:34 +0000

    Bug 1510790 - Introduce Redux selector for tracked changes to return the nested tree structure. r=pbro, a=RyanVM
    This patch introduces a Redux selector for the Changes slice. A selector here is a fancy word for a method that returns a subset from the state without having to expose the state's structure to the consumer. It is also useful for returning a derived version of the state which we're using here.
    
    The tracked changed CSS rules in the Redux state are not stored in a nested structure. A shallow structure is easier to query and to work with in our use case. But the Changes panel needs to display CSS rules in a hierarchical structure (ex: a style rule which is a child of a @media rule). To do this, the React component used to build up the nested structure as it consumed the changes from the Redux state. To prevent rendering duplicates of rules (once as part of an ancestor's children and once as a standalone rule), the React component kept a reference of rules it had previously rendered. This had a flaw because it didn't account for the rule's stylesheet. The problem was that rules with identical selectors from different stylesheets would not all be rendered because they would be accidentally marked as previously rendered.
    
    This is too much knowledge of the business logic for the React component anyway.
    
    The Redux state should present itself in a way that's simple for the React component to consume. Hence the introduction of the `getChangesTree()` selector in this patch. This method allows us to present a comfortable structure to React while keeping the Redux structure comfortable for us to work with. This separation enables increased flexibility to restructure the Redux state without impacting the React components.
    
    More about Redux selectors here: https://redux.js.org/recipes/computing-derived-data
    
    Differential Revision: https://phabricator.services.mozilla.com/D16068
    
    --HG--
    rename : devtools/client/inspector/changes/moz.build => devtools/client/inspector/changes/selectors/moz.build
    extra : source : ebee32dc7abedd8621353f4ee27af9cf077e427e

---
+++ devtools/client/inspector/changes/test/browser_changes_declaration_identical_rules.js
@@ -0,0 +1,57 @@
+/* vim: set ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test tracking changes to CSS declarations in different stylesheets but in rules
+// with identical selectors.
+
+const TEST_URI = `
+  <style type='text/css'>
+    div {
+      color: red;
+    }
+  </style>
+  <style type='text/css'>
+    div {
+      font-size: 1em;
+    }
+  </style>
+  <div></div>
+`;
+
+add_task(async function() {
+  await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
+  const { inspector, view: ruleView } = await openRuleView();
+  const { document: doc, store } = selectChangesView(inspector);
+
+  await selectNode("div", inspector);
+  const rule1 = getRuleViewRuleEditor(ruleView, 1).rule;
+  const rule2 = getRuleViewRuleEditor(ruleView, 2).rule;
+  const prop1 = rule1.textProps[0];
+  const prop2 = rule2.textProps[0];
+  let onTrackChange;
+
+  onTrackChange = waitUntilAction(store, "TRACK_CHANGE");
+  info("Disable the declaration in the first rule");
+  await togglePropStatus(ruleView, prop1);
+  info("Wait for change to be tracked");
+  await onTrackChange;
+
+  onTrackChange = waitUntilAction(store, "TRACK_CHANGE");
+  info("Disable the declaration in the second rule");
+  await togglePropStatus(ruleView, prop2);
+  info("Wait for change to be tracked");
+  await onTrackChange;
+
+  const removeDecl = getRemovedDeclarations(doc);
+  is(removeDecl.length, 2, "Two declarations tracked as removed");
+  // The last of the two matching rules shows up first in Rule view given that the
+  // specificity is the same. This is correct. If the properties were the same, the latest
+  // declaration would overwrite the first and thus show up on top.
+  is(removeDecl[0].property, "font-size", "Correct property name for second declaration");
+  is(removeDecl[0].value, "1em", "Correct property value for second declaration");
+  is(removeDecl[1].property, "color", "Correct property name for first declaration");
+  is(removeDecl[1].value, "red", "Correct property value for first declaration");
+});
