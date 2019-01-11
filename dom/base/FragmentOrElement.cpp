21fa01c4852602eaa40a63cfda8c786efbf82f32
Masayuki Nakano <masayuki@d-toybox.com>
2019-01-11 01:53:10 +0000

    Bug 181137 - part 9: Move kNSURIs in Document.cpp and FragmentOrElement.cpp into their users r=smaug
    Due to renaming nsContentIterator.cpp to ContentIterator.cpp, Document.cpp
    and FragmentOrElement.cpp are compiled in a unified cpp file now.  However,
    both of them have same name constant, kNSURIs and some build systems claim
    that it in FragmentOrElement.cpp is never used.
    
    Fortunately, each of them is used only by one method.  Therefore, this patch
    moves the each declaration into each user method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16186

--- dom/base/FragmentOrElement.cpp
+++ dom/base/FragmentOrElement.cpp
@@ -1748,11 +1748,6 @@ NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_THIS_BEGIN(FragmentOrElement)
   return FragmentOrElement::CanSkipThis(tmp);
 NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_THIS_END
 
-static const char* kNSURIs[] = {" ([none])", " (xmlns)",  " (xml)",
-                                " (xhtml)",  " (XLink)",  " (XSLT)",
-                                " (XBL)",    " (MathML)", " (RDF)",
-                                " (XUL)",    " (SVG)",    " (XML Events)"};
-
 // We purposefully don't TRAVERSE_BEGIN_INHERITED here.  All the bits
 // we should traverse should be added here or in nsINode::Traverse.
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INTERNAL(FragmentOrElement)
@@ -1793,6 +1788,10 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INTERNAL(FragmentOrElement)
       orphan.AppendLiteral(" (orphan)");
     }
 
+    static const char* kNSURIs[] = {" ([none])", " (xmlns)",  " (xml)",
+                                    " (xhtml)",  " (XLink)",  " (XSLT)",
+                                    " (XBL)",    " (MathML)", " (RDF)",
+                                    " (XUL)",    " (SVG)",    " (XML Events)"};
     const char* nsuri = nsid < ArrayLength(kNSURIs) ? kNSURIs[nsid] : "";
     SprintfLiteral(name, "FragmentOrElement%s %s%s%s%s %s", nsuri,
                    localName.get(), NS_ConvertUTF16toUTF8(id).get(),
