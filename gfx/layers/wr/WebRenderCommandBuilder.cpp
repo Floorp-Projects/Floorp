22baed2b58b3970f2a697f55ef5c2ad46c1022f9
Jeff Muizelaar <jrmuizel@gmail.com>
2019-01-11 19:43:32 +0000

    Bug 1519444. Don't enforce a minimum size for blobs. r=mstange
    WebRender can handle small image without much overhead. Allowing
    small images will give us more predictable behaviour because
    we don't end up merging things unneccessarily.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16337
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/wr/WebRenderCommandBuilder.cpp
+++ gfx/layers/wr/WebRenderCommandBuilder.cpp
@@ -1081,14 +1081,14 @@ static bool IsItemProbablyActive(nsDisplayItem* aItem,
       Matrix t2d;
       bool is2D = t.Is2D(&t2d);
       GP("active: %d\n", transformItem->MayBeAnimated(aDisplayListBuilder));
-      return transformItem->MayBeAnimated(aDisplayListBuilder) || !is2D ||
-             HasActiveChildren(*transformItem->GetChildren(),
-                               aDisplayListBuilder);
+      return transformItem->MayBeAnimated(aDisplayListBuilder, false) ||
+             !is2D || HasActiveChildren(*transformItem->GetChildren(),
+                                        aDisplayListBuilder);
     }
     case DisplayItemType::TYPE_OPACITY: {
       nsDisplayOpacity* opacityItem = static_cast<nsDisplayOpacity*>(aItem);
       bool active = opacityItem->NeedsActiveLayer(aDisplayListBuilder,
-                                                  opacityItem->Frame());
+                                                  opacityItem->Frame(), false);
       GP("active: %d\n", active);
       return active || HasActiveChildren(*opacityItem->GetChildren(),
                                          aDisplayListBuilder);
