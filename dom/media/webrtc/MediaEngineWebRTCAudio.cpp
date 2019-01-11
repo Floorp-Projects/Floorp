83094216d4c3904e0c6096a4e91d2de477c1b4ff
Paul Adenot <paul@paul.cx>
2019-01-11 17:22:24 +0000

    Bug 1499426 - Align the lifetime of AudioInputProcessing with the lifetime of MediaEngineWebRTCAudio. r=achronop
    Differential Revision: https://phabricator.services.mozilla.com/D16201

--- dom/media/webrtc/MediaEngineWebRTCAudio.cpp
+++ dom/media/webrtc/MediaEngineWebRTCAudio.cpp
@@ -520,6 +520,9 @@ void MediaEngineWebRTCMicrophoneSource::SetTrack(
   aStream->AddAudioTrack(aTrackID, aStream->GraphRate(), segment,
                          SourceMediaStream::ADDTRACK_QUEUED);
 
+  mInputProcessing = new AudioInputProcessing(mDeviceMaxChannelCount, mStream,
+                                              mTrackID, mPrincipal);
+
   LOG("Stream %p registered for microphone capture", aStream.get());
 }
 
@@ -565,9 +568,6 @@ nsresult MediaEngineWebRTCMicrophoneSource::Start(
     // because we can only have one MSG per document.
     return NS_ERROR_FAILURE;
   }
-
-  mInputProcessing = new AudioInputProcessing(mDeviceMaxChannelCount, mStream,
-                                              mTrackID, mPrincipal);
 
   RefPtr<MediaEngineWebRTCMicrophoneSource> that = this;
   NS_DispatchToMainThread(media::NewRunnableFrom(
