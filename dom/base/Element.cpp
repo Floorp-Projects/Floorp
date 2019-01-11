5dc4d81ca111ce39106deb97b189607ee062b1dc
James Teh <jteh@mozilla.com>
2019-01-11 04:52:50 +0000

    Bug 1514687 - Allow accessibility code to focus XUL radio buttons without selecting them. r=bgrins,smaug,MarcoZ,paolo
    1. This requires exposing radiogroup's focusedItem property to C++.
    Unfortunately, there's no existing equivalent in nsIDOMXULSelectControlItemElement.
    radiogroup is the only element that needs this, so a new interface has been created for it.
    
    2. Accessibility uses focusedItem instead of selectedItem when setting focus.
    
    3. When an item is focused, accessibility needs to be notified.
    This is done using a DOMMenuItemActive event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15295
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -153,6 +153,7 @@
 #include "nsIDOMXULControlElement.h"
 #include "nsIDOMXULMenuListElement.h"
 #include "nsIDOMXULMultSelectCntrlEl.h"
+#include "nsIDOMXULRadioGroupElement.h"
 #include "nsIDOMXULRelatedElement.h"
 #include "nsIDOMXULMultSelectCntrlEl.h"
 #include "nsIDOMXULSelectCntrlEl.h"
@@ -4060,6 +4061,12 @@ already_AddRefed<nsIDOMXULMenuListElement> Element::AsXULMenuList() {
 already_AddRefed<nsIDOMXULMultiSelectControlElement>
 Element::AsXULMultiSelectControl() {
   nsCOMPtr<nsIDOMXULMultiSelectControlElement> value;
+  GetCustomInterface(getter_AddRefs(value));
+  return value.forget();
+}
+
+already_AddRefed<nsIDOMXULRadioGroupElement> Element::AsXULRadioGroup() {
+  nsCOMPtr<nsIDOMXULRadioGroupElement> value;
   GetCustomInterface(getter_AddRefs(value));
   return value.forget();
 }
