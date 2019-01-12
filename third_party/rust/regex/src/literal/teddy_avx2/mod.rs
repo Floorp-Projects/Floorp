7af9b96ac5c89faec867cd28acc7a39a3dcc960c
Dorel Luca <dluca@mozilla.com>
2019-01-12 03:43:46 +0200

    Backed out 2 changesets (bug 1516337) for build bustage. CLOSED TREE
    Backed out changeset 3c4b8e03e722 (bug 1516337)
    Backed out changeset 4fc377013db5 (bug 1516337)

--- third_party/rust/regex/src/literal/teddy_avx2/mod.rs
+++ third_party/rust/regex/src/literal/teddy_avx2/mod.rs
@@ -1,14 +1,16 @@
 pub use self::imp::*;
 
 #[cfg(all(
+    feature = "unstable",
     regex_runtime_teddy_avx2,
-    target_arch = "x86_64",
+    any(target_arch = "x86_64"),
 ))]
 mod imp;
 
 #[cfg(not(all(
+    feature = "unstable",
     regex_runtime_teddy_avx2,
-    target_arch = "x86_64",
+    any(target_arch = "x86_64"),
 )))]
 #[path = "fallback.rs"]
 mod imp;
