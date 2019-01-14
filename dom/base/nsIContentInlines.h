4f6ffa5a12e48d2b5a58cba2ae6bd0b3d1747d19
Emilio Cobos Álvarez <emilio@crisal.io>
2019-01-14 17:03:55 +0100

    Bug 1511563 - Don't make NAC editable by default. r=masayuki
    I think this is a more consistent fix for the issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16521

--- dom/base/nsIContentInlines.h
+++ dom/base/nsIContentInlines.h
@@ -153,9 +153,14 @@ inline bool nsINode::IsEditable() const {
     return true;
   }
 
-  Document* doc = GetUncomposedDoc();
+  // All editable anonymous content should be made explicitly editable via the
+  // NODE_IS_EDITABLE flag.
+  if (IsInNativeAnonymousSubtree()) {
+    return false;
+  }
 
   // Check if the node is in a document and the document is in designMode.
+  Document* doc = GetUncomposedDoc();
   return doc && doc->HasFlag(NODE_IS_EDITABLE);
 }
 
