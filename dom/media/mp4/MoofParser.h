88f428f0578e6bcd9d4fcd58fa1e9c51e48bbf03
Bryce Van Dyk <bvandyk@mozilla.com>
2019-01-14 16:33:03 +0000

    Bug 1519617 - Update MoofParser to handle tracks using track_id 0. r=jya
    Using track_id 0 is forbidden by the mp4 spec, however, some sites still serve
    media using this track_id. We've been using the 0 track ID to trigger special
    handling in the MoofParser where we will parse multiple tracks, and this led us
    to be tolerant of tracks using this reserved id (though we likely had some bugs
    due to this).
    
    Since sites are using this track_id, and as other browsers (and Firefox until I
    broke this) tolerate such media, we should too. In order to do so correctly, we
    should no longer us track_id=0 as a special case in the MoofParser, and instead
    have an explicit flag, which is what this patch does.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16428

--- dom/media/mp4/MoofParser.h
+++ dom/media/mp4/MoofParser.h
@@ -267,14 +267,18 @@ DDLoggedTypeDeclName(MoofParser);
 
 class MoofParser : public DecoderDoctorLifeLogger<MoofParser> {
  public:
-  MoofParser(ByteStream* aSource, uint32_t aTrackId, bool aIsAudio)
+  MoofParser(ByteStream* aSource, uint32_t aTrackId, bool aIsAudio,
+             bool aIsMultitrackParser = false)
       : mSource(aSource),
         mOffset(0),
         mTrex(aTrackId),
         mIsAudio(aIsAudio),
-        mLastDecodeTime(0) {
-    // Setting the mTrex.mTrackId to 0 is a nasty work around for calculating
-    // the composition range for MSE. We need an array of tracks.
+        mLastDecodeTime(0),
+        mIsMultitrackParser(aIsMultitrackParser) {
+    // Setting mIsMultitrackParser is a nasty work around for calculating
+    // the composition range for MSE that causes the parser to parse multiple
+    // tracks. Ideally we'd store an array of tracks with different metadata
+    // for each.
     DDLINKCHILD("source", aSource);
   }
   bool RebuildFragmentedIndex(const mozilla::MediaByteRangeSet& aByteRanges);
@@ -326,6 +330,7 @@ class MoofParser : public DecoderDoctorLifeLogger<MoofParser> {
   nsTArray<MediaByteRange> mMediaRanges;
   bool mIsAudio;
   uint64_t mLastDecodeTime;
+  bool mIsMultitrackParser;
 };
 }  // namespace mozilla
 
