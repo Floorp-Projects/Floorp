cd5f035b70669b46e1cb26dd484d22fbfa0bcfb5
Mark Banner <standard8@mozilla.com>
2019-01-15 12:20:48 +0000

    Bug 1506126 - Support clearing history for selected entries. r=dao
    Differential Revision: https://phabricator.services.mozilla.com/D16332

--- browser/base/content/test/urlbar/browser_urlbar_remove_match.js
+++ browser/base/content/test/urlbar/browser_urlbar_remove_match.js
@@ -1,4 +1,3 @@
-/* eslint-disable mozilla/no-arbitrary-setTimeout */
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
@@ -28,4 +27,38 @@ add_task(async function test_remove_history() {
 
   gURLBar.popup.hidePopup();
   await promisePopupHidden(gURLBar.popup);
+});
+
+// We shouldn't be able to remove a bookmark item.
+add_task(async function test_remove_bookmark_doesnt() {
+  const TEST_URL = "http://dont.remove.me/from_urlbar/";
+  await PlacesUtils.bookmarks.insert({
+    parentGuid: PlacesUtils.bookmarks.unfiledGuid,
+    title: "test",
+    url: TEST_URL,
+  });
+
+  registerCleanupFunction(async function() {
+    await PlacesUtils.bookmarks.eraseEverything();
+  });
+
+  await promiseAutocompleteResultPopup("from_urlbar");
+  let result = await waitForAutocompleteResultAt(1);
+  Assert.equal(result.getAttribute("ac-value"), TEST_URL, "Found the expected result");
+
+  EventUtils.synthesizeKey("KEY_ArrowDown");
+  Assert.equal(gURLBar.popup.richlistbox.selectedIndex, 1);
+  let options = AppConstants.platform == "macosx" ? { shiftKey: true } : {};
+  EventUtils.synthesizeKey("KEY_Delete", options);
+
+  // We don't have an easy way of determining if the event was process or not,
+  // so let any event queues clear before testing.
+  await new Promise(resolve => setTimeout(resolve, 0));
+  await PlacesTestUtils.promiseAsyncUpdates();
+
+  gURLBar.popup.hidePopup();
+  await promisePopupHidden(gURLBar.popup);
+
+  Assert.ok(await PlacesUtils.bookmarks.fetch({url: TEST_URL}),
+    "Should still have the URL bookmarked.");
 });
