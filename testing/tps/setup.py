56ef20fbbf4c42a87408d7dc9ab6a02bec7ff6d8
terrameijar <terrameijar@gmail.com>
2019-01-15 15:58:52 +0000

    Bug 1506611 - Fixed linter errors in testing/tps. r=ahal
    Before this change, testing/tps had 19 linter errors. These have been resolved.
    
    Differential Revision: https://phabricator.services.mozilla.com/D15846

--- testing/tps/setup.py
+++ testing/tps/setup.py
@@ -17,7 +17,7 @@ deps = ['httplib2 == 0.9.2',
         'mozprofile ~= 2.1',
         'mozrunner ~= 7.2',
         'mozversion == 1.5',
-       ]
+        ]
 
 # we only support python 2.6+ right now
 assert sys.version_info[0] == 2
@@ -29,8 +29,8 @@ setup(name='tps',
       long_description="""\
 """,
       classifiers=['Programming Language :: Python :: 2.7',
-		   'Programming Language :: Python :: 2 :: Only',
-		  ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+                   'Programming Language :: Python :: 2 :: Only',
+                   ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='Mozilla Automation and Tools team',
       author_email='tools@lists.mozilla.org',
