41ba7afe3bf67002dd5296f60456164df2c572a9
Mike Hommey <mh+mozilla@glandium.org>
2019-01-15 15:28:00 +0000

    Bug 1520108 - Move MOZ_CONFIGURE_OPTIONS to toolkit/moz.configure. r=froydnj
    It is only used to fill about:buildconfig.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16526

--- python/mozbuild/mozbuild/test/configure/test_moz_configure.py
+++ python/mozbuild/mozbuild/test/configure/test_moz_configure.py
@@ -5,62 +5,11 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from mozunit import main
-from mozpack import path as mozpath
 
 from common import BaseConfigureTest
 
 
 class TestMozConfigure(BaseConfigureTest):
-    def test_moz_configure_options(self):
-        def get_value_for(args=[], environ={}, mozconfig=''):
-            sandbox = self.get_sandbox({}, {}, args, environ, mozconfig)
-
-            # Add a fake old-configure option
-            sandbox.option_impl('--with-foo', nargs='*',
-                                help='Help missing for old configure options')
-
-            # Remove all implied options, otherwise, getting
-            # all_configure_options below triggers them, and that triggers
-            # configure parts that aren't expected to run during this test.
-            del sandbox._implied_options[:]
-            result = sandbox._value_for(sandbox['all_configure_options'])
-            shell = mozpath.abspath('/bin/sh')
-            return result.replace('CONFIG_SHELL=%s ' % shell, '')
-
-        self.assertEquals('--enable-application=browser',
-                          get_value_for(['--enable-application=browser']))
-
-        self.assertEquals('--enable-application=browser '
-                          'MOZ_VTUNE=1',
-                          get_value_for(['--enable-application=browser',
-                                         'MOZ_VTUNE=1']))
-
-        value = get_value_for(
-            environ={'MOZ_VTUNE': '1'},
-            mozconfig='ac_add_options --enable-project=js')
-
-        self.assertEquals('--enable-project=js MOZ_VTUNE=1',
-                          value)
-
-        # --disable-js-shell is the default, so it's filtered out.
-        self.assertEquals('--enable-application=browser',
-                          get_value_for(['--enable-application=browser',
-                                         '--disable-js-shell']))
-
-        # Normally, --without-foo would be filtered out because that's the
-        # default, but since it is a (fake) old-configure option, it always
-        # appears.
-        self.assertEquals('--enable-application=browser --without-foo',
-                          get_value_for(['--enable-application=browser',
-                                         '--without-foo']))
-        self.assertEquals('--enable-application=browser --with-foo',
-                          get_value_for(['--enable-application=browser',
-                                         '--with-foo']))
-
-        self.assertEquals("--enable-application=browser '--with-foo=foo bar'",
-                          get_value_for(['--enable-application=browser',
-                                         '--with-foo=foo bar']))
-
     def test_nsis_version(self):
         this = self
 
