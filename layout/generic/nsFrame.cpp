0d127ab2288abb16e0f55fdb87aba06b3a7793ae
Emilio Cobos Álvarez <emilio@crisal.io>
2019-01-15 15:05:45 +0100

    Bug 1520154 - Move cursor to cbindgen. r=boris
    The only reason it was on style_traits is so that they could use it from some
    other crates, but Servo eventually ends up getting the value from an integer, so
    may as well pass it around and do that in the end of the process anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16557

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -5042,17 +5042,17 @@ void nsIFrame::AssociateImage(const nsStyleImage& aImage,
 
 nsresult nsFrame::GetCursor(const nsPoint& aPoint, nsIFrame::Cursor& aCursor) {
   FillCursorInformationFromStyle(StyleUI(), aCursor);
-  if (NS_STYLE_CURSOR_AUTO == aCursor.mCursor) {
+  if (StyleCursorKind::Auto == aCursor.mCursor) {
     // If this is editable, I-beam cursor is better for most elements.
     aCursor.mCursor = (mContent && mContent->IsEditable())
-                          ? NS_STYLE_CURSOR_TEXT
-                          : NS_STYLE_CURSOR_DEFAULT;
+                          ? StyleCursorKind::Text
+                          : StyleCursorKind::Default;
   }
-  if (NS_STYLE_CURSOR_TEXT == aCursor.mCursor &&
+  if (StyleCursorKind::Text == aCursor.mCursor &&
       GetWritingMode().IsVertical()) {
     // Per CSS UI spec, UA may treat value 'text' as
     // 'vertical-text' for vertical text.
-    aCursor.mCursor = NS_STYLE_CURSOR_VERTICAL_TEXT;
+    aCursor.mCursor = StyleCursorKind::VerticalText;
   }
 
   return NS_OK;
