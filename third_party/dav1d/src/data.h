0e1ed989387533454e854525a7a9752d3d2da2b1
Alex Chronopoulos <achronop@gmail.com>
2019-01-16 00:05:25 +0000

    Bug 1520174 - Update dav1d from upstream to f813285. r=TD-Linux
    Differential Revision: https://phabricator.services.mozilla.com/D16562
    
    --HG--
    extra : moz-landing-system : lando

--- third_party/dav1d/src/data.h
+++ third_party/dav1d/src/data.h
@@ -30,9 +30,29 @@
 
 #include "dav1d/data.h"
 
+void dav1d_data_ref(Dav1dData *dst, const Dav1dData *src);
+
 /**
  * Move a data reference.
  */
 void dav1d_data_move_ref(Dav1dData *dst, Dav1dData *src);
+
+/**
+ * Copy the source properties to the destitionatin and increase the
+ * user_data's reference count (if it's not NULL).
+ */
+void dav1d_data_props_copy(Dav1dDataProps *dst, const Dav1dDataProps *src);
+
+uint8_t *dav1d_data_create_internal(Dav1dData *buf, size_t sz);
+int dav1d_data_wrap_internal(Dav1dData *buf, const uint8_t *ptr, size_t sz,
+                             void (*free_callback)(const uint8_t *data,
+                                                   void *user_data),
+                             void *user_data);
+int dav1d_data_wrap_user_data_internal(Dav1dData *buf,
+                                       const uint8_t *user_data,
+                                       void (*free_callback)(const uint8_t *user_data,
+                                                             void *cookie),
+                                       void *cookie);
+void dav1d_data_unref_internal(Dav1dData *buf);
 
 #endif /* __DAV1D_SRC_DATA_H__ */
