ef5fc686eefe03e9843a3ca67edabf54868bd9b8
Robert Strong <robert.bugzilla@gmail.com>
2019-01-16 19:27:48 -0800

    Bug 599233 - add about dialog app update tests. r=mhowell Adds disabledForTesting checks to the about dialog app update code Makes a copy of update.sjs for browser-chrome tests so the changes don't break chrome tests Prepares for the removal of the chrome tests and the old app update UI Adds 14 tests for app update in the about dialog

--- toolkit/mozapps/update/tests/browser/browser_updatesBasicPrompt.js
+++ toolkit/mozapps/update/tests/browser/browser_updatesBasicPrompt.js
@@ -5,7 +5,6 @@ add_task(async function testBasicPrompt() {
   await UpdateUtils.setAppUpdateAutoEnabled(false);
 
   let updateParams = "promptWaitTime=0";
-  gUseTestUpdater = true;
 
   await runUpdateTest(updateParams, 1, [
     {
