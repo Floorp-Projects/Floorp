2723a1f79bfe84de37d5e7dcb3d9de775b65e296
Jan de Mooij <jdemooij@mozilla.com>
2019-01-18 13:14:58 +0000

    Bug 1284719 part 1 - Emit hoisted top-level functions directly in the prologue, remove switchToPrologue. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D16947

--- js/src/frontend/BytecodeEmitter.h
+++ js/src/frontend/BytecodeEmitter.h
@@ -400,8 +400,10 @@ struct MOZ_STACK_CLASS BytecodeEmitter {
   ptrdiff_t prologueOffset() const {
     return prologue.code.end() - prologue.code.begin();
   }
-  void switchToMain() { current = &main; }
-  void switchToPrologue() { current = &prologue; }
+  void switchToMain() {
+    MOZ_ASSERT(inPrologue());
+    current = &main;
+  }
   bool inPrologue() const { return current == &prologue; }
 
   SrcNotesVector& notes() const {
@@ -773,6 +775,8 @@ struct MOZ_STACK_CLASS BytecodeEmitter {
   template <typename InnerEmitter>
   MOZ_MUST_USE bool wrapWithDestructuringTryNote(int32_t iterDepth,
                                                  InnerEmitter emitter);
+
+  MOZ_MUST_USE bool defineHoistedTopLevelFunctions(ParseNode* body);
 
   // Check if the value on top of the stack is "undefined". If so, replace
   // that value on the stack with the value defined by |defaultExpr|.
