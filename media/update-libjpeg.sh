1b65a4f599e8c9d22b958a8c1eb1e40333cbaff6
Andrew Osmond <aosmond@mozilla.com>
2019-01-18 12:04:21 -0500

    Bug 1520760 - Fix AVX2 detection to ensure we have all required CPU parameters. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D16994

--- media/update-libjpeg.sh
+++ media/update-libjpeg.sh
@@ -19,7 +19,7 @@ tag=${2-HEAD}
 cd $srcdir/libjpeg
 cp win/jsimdcfg.inc simd/
 
-revert_files="1050342.diff assembly-tables.diff externalize-table.diff jconfig.h jconfigint.h jpeg_nbits_table.c moz.build MOZCHANGES mozilla.diff simd/jsimdcfg.inc"
+revert_files="1050342.diff assembly-tables.diff externalize-table.diff 1520760-avx2-detection.diff jconfig.h jconfigint.h jpeg_nbits_table.c moz.build MOZCHANGES mozilla.diff simd/jsimdcfg.inc"
 if test -d ${topsrcdir}/.hg; then
     hg revert --no-backup $revert_files
 elif test -d ${topsrcdir}/.git; then
@@ -30,3 +30,4 @@ patch -p0 -i mozilla.diff
 patch -p0 -i 1050342.diff
 patch -p3 -i externalize-table.diff
 patch -p3 -i assembly-tables.diff
+patch -p0 -i 1520760-avx2-detection.diff
