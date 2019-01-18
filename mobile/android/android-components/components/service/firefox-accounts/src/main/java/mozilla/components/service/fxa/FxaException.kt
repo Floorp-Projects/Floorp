e8b29c37721b341d6bd61a700e42cf7c976588ff
Grisha Kruglov <gkruglov@mozilla.com>
2019-01-18 15:50:34 -0800

    [components] Pre: typealiases for sub-classes of FxaException

--- mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/FxaException.kt
+++ mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/FxaException.kt
@@ -1,16 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 package mozilla.components.service.fxa
 
 /**
- * Wrapper class for the exceptions thrown in the Rust library.
- *
- * Broken down into the following subclasses:
- *
- * - `FxaException.Unauthorized`: Thrown when the operation requires additional authorization.
- * - `FxaException.Network`: Thrown on a network error.
- * - `FxaException.Panic`: Thrown when the Rust library hits an assertion or panic (this is always
- *   a bug).
- * - `FxaException.Unspecified`: Thrown when the Rust library hits an unexpected error that isn't
- *   a panic. This may indicate library misuse, network errors, etc.
- *
+ * High-level exception class for the exceptions thrown in the Rust library.
  */
 typealias FxaException = org.mozilla.fxaclient.internal.FxaException
+
+/**
+ * Thrown on a network error.
+ */
+typealias FxaNetworkException = org.mozilla.fxaclient.internal.FxaException.Network
+
+/**
+ * Thrown when the Rust library hits an assertion or panic (this is always a bug).
+ */
+typealias FxaPanicException = org.mozilla.fxaclient.internal.FxaException.Panic
+
+/**
+ * Thrown when the operation requires additional authorization.
+ */
+typealias FxaUnauthorizedException = org.mozilla.fxaclient.internal.FxaException.Unauthorized
+
+/**
+ * Thrown when the Rust library hits an unexpected error that isn't a panic.
+ * This may indicate library misuse, network errors, etc.
+ */
+typealias FxaUnspecifiedException = org.mozilla.fxaclient.internal.FxaException.Unspecified
