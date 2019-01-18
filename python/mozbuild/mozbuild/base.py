693ee6d714912e5fbbfe25e9552acc3fd3aa4a3c
Chris Manchester <cmanchester@mozilla.com>
2019-01-18 19:03:24 +0000

    Bug 1520516 - Set verbose logging for cargo when -v is passed to |./mach build|. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D16907

--- python/mozbuild/mozbuild/base.py
+++ python/mozbuild/mozbuild/base.py
@@ -649,6 +649,8 @@ class MozbuildObject(ProcessExecutionMixin):
 
         if silent:
             args.append('-s')
+        else:
+            args.append('BUILD_VERBOSE_LOG=1')
 
         # Print entering/leaving directory messages. Some consumers look at
         # these to measure progress.
