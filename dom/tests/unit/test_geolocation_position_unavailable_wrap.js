a12b9e7b37ba56bda1511affd2636c953472c6a8
Muchtar Salimov <msalimov@myseneca.ca>
2019-01-18 18:56:46 +0000

    Bug 1508984 - Enable ESLint for dom/tests/unit/ and dom/system (manual changes) r=Standard8,Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D13624
    
    --HG--
    extra : moz-landing-system : lando

--- dom/tests/unit/test_geolocation_position_unavailable_wrap.js
+++ dom/tests/unit/test_geolocation_position_unavailable_wrap.js
@@ -1,8 +1,9 @@
-function run_test() {
-  var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
-  prefs.setBoolPref("geo.wifi.scan", false);
+ChromeUtils.import("resource://gre/modules/Services.jsm");
 
-  prefs.setCharPref("geo.wifi.uri", "UrlNotUsedHere");
-  prefs.setBoolPref("dom.testing.ignore_ipc_principal", true);
+function run_test() {
+  Services.prefs.setBoolPref("geo.wifi.scan", false);
+
+  Services.prefs.setCharPref("geo.wifi.uri", "UrlNotUsedHere");
+  Services.prefs.setBoolPref("dom.testing.ignore_ipc_principal", true);
   run_test_in_child("./test_geolocation_position_unavailable.js");
 }
