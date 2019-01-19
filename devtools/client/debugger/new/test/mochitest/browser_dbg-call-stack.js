1a4a93f38bee1c56cb8abe5e5cb3e4d02d220e74
Jason Laster <jlaster@mozilla.com>
2019-01-19 11:45:21 -0500

    Revert "Bug 1520957 - [release 119] Redesign call stack group (#7624). r=dwalsh"
    This reverts commit f2114eb41cb3a50251392bcd45f449d719a16f32.

--- devtools/client/debugger/new/test/mochitest/browser_dbg-call-stack.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-call-stack.js
@@ -107,7 +107,9 @@ add_task(async function() {
 
   await waitForPaused(dbg);
   const $group = findElementWithSelector(dbg, ".frames .frames-group");
+  is($group.querySelector(".title").textContent,
+    "<anonymous>", "Group has expected frame title");
   is($group.querySelector(".badge").textContent, "2", "Group has expected badge");
-  is($group.querySelector(".group-description-name").textContent, "Angular",
+  is($group.querySelector(".location").textContent, "Angular",
     "Group has expected location");
 });
