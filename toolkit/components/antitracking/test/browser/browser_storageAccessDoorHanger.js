eb5345d4eb64624bff785c45ce2e3c38f457d3b5
Ciure Andrei <aciure@mozilla.com>
2019-01-21 17:48:11 +0200

    Backed out 2 changesets (bug 1516889) for failing browser_storageAccessDoorHanger.js CLOSED TREE
    Backed out changeset fe40c77b54b4 (bug 1516889)
    Backed out changeset 95fee3425c55 (bug 1516889)

--- toolkit/components/antitracking/test/browser/browser_storageAccessDoorHanger.js
+++ toolkit/components/antitracking/test/browser/browser_storageAccessDoorHanger.js
@@ -9,13 +9,12 @@ const BLOCK = 0;
 const ALLOW = 1;
 const ALLOW_ON_ANY_SITE = 2;
 
-async function testDoorHanger(choice, showPrompt, useEscape, topPage, maxConcurrent) {
+async function testDoorHanger(choice, showPrompt, topPage, maxConcurrent) {
   info(`Running doorhanger test with choice #${choice}, showPrompt: ${showPrompt} and ` +
-       `useEscape: ${useEscape}, topPage: ${topPage}, maxConcurrent: ${maxConcurrent}`);
+       `topPage: ${topPage}, maxConcurrent: ${maxConcurrent}`);
 
   if (!showPrompt) {
     is(choice, ALLOW, "When not showing a prompt, we can only auto-grant");
-    ok(!useEscape, "When not showing a prompt, we should not be trying to use the Esc key");
   }
 
   await SpecialPowers.flushPrefEnv();
@@ -51,7 +50,6 @@ async function testDoorHanger(choice, showPrompt, useEscape, topPage, maxConcurr
       addEventListener("message", function onMessage(e) {
         if (e.data.startsWith("choice:")) {
           window.choice = e.data.split(":")[1];
-          window.useEscape = e.data.split(":")[3];
           removeEventListener("message", onMessage);
           resolve();
         }
@@ -133,11 +131,7 @@ async function testDoorHanger(choice, showPrompt, useEscape, topPage, maxConcurr
     Assert.ok(notification, "Should have gotten the notification");
 
     if (choice == BLOCK) {
-      if (useEscape) {
-        EventUtils.synthesizeKey("KEY_Escape", {}, window);
-      } else {
-        await clickMainAction();
-      }
+      await clickMainAction();
     } else if (choice == ALLOW) {
       await clickSecondaryAction(choice - 1);
     } else if (choice == ALLOW_ON_ANY_SITE) {
@@ -153,7 +147,6 @@ async function testDoorHanger(choice, showPrompt, useEscape, topPage, maxConcurr
                              { page: url,
                                callback: runChecks.toString(),
                                choice,
-                               useEscape,
                              },
                              async function(obj) {
     await new content.Promise(resolve => {
@@ -181,8 +174,7 @@ async function testDoorHanger(choice, showPrompt, useEscape, topPage, maxConcurr
         }
 
         if (event.data == "getchoice") {
-          ifr.contentWindow.postMessage("choice:" + obj.choice +
-                                        ":useEscape:" + obj.useEscape, "*");
+          ifr.contentWindow.postMessage("choice:" + obj.choice, "*");
           return;
         }
 
@@ -311,19 +303,16 @@ async function cleanUp() {
 async function runRound(topPage, showPrompt, maxConcurrent) {
   if (showPrompt) {
     await preparePermissionsFromOtherSites(topPage);
-    await testDoorHanger(BLOCK, showPrompt, true, topPage, maxConcurrent);
+    await testDoorHanger(BLOCK, showPrompt, topPage, maxConcurrent);
     await cleanUp();
     await preparePermissionsFromOtherSites(topPage);
-    await testDoorHanger(BLOCK, showPrompt, false, topPage, maxConcurrent);
+    await testDoorHanger(ALLOW, showPrompt, topPage, maxConcurrent);
     await cleanUp();
     await preparePermissionsFromOtherSites(topPage);
-    await testDoorHanger(ALLOW, showPrompt, false, topPage, maxConcurrent);
-    await cleanUp();
-    await preparePermissionsFromOtherSites(topPage);
-    await testDoorHanger(ALLOW_ON_ANY_SITE, showPrompt, false, topPage, maxConcurrent);
+    await testDoorHanger(ALLOW_ON_ANY_SITE, showPrompt, topPage, maxConcurrent);
   } else {
     await preparePermissionsFromOtherSites(topPage);
-    await testDoorHanger(ALLOW, showPrompt, false, topPage, maxConcurrent);
+    await testDoorHanger(ALLOW, showPrompt, topPage, maxConcurrent);
   }
   await cleanUp();
 }
--- toolkit/modules/PopupNotifications.jsm
+++ toolkit/modules/PopupNotifications.jsm
@@ -253,8 +253,7 @@ function PopupNotifications(tabbrowser, panel,
         // Ignore focused elements inside the notification.
         getNotificationFromElement(focusedElement) == notification ||
         notification.contains(focusedElement)) {
-      let escAction = notification.notification.options.escAction;
-      this._onButtonEvent(aEvent, escAction, "esc-press", notification);
+      this._onButtonEvent(aEvent, "secondarybuttoncommand", "esc-press", notification);
     }
   };
 
@@ -468,11 +467,6 @@ PopupNotifications.prototype = {
    *                     notification description header text. Usually a host name or
    *                     addon name. This is similar to name, and only used in case
    *                     where message contains two "<>" placeholders.
-   *        escAction:
-   *                     An optional string indicating the action to take when the
-   *                     Esc key is pressed. This should be set to the name of the
-   *                     command to run. If not provided, "secondarybuttoncommand"
-   *                     will be used.
    * @returns the Notification object corresponding to the added notification.
    */
   show: function PopupNotifications_show(browser, id, message, anchorID,
@@ -492,15 +486,6 @@ PopupNotifications.prototype = {
 
     let notification = new Notification(id, message, anchorID, mainAction,
                                         secondaryActions, browser, this, options);
-
-    if (options) {
-      let escAction = options.escAction;
-      if (escAction != "buttoncommand" ||
-          escAction != "secondarybuttoncommand") {
-        escAction = "secondarybuttoncommand";
-      }
-      notification.options.escAction = escAction;
-    }
 
     if (options && options.dismissed)
       notification.dismissed = true;
@@ -846,7 +831,7 @@ PopupNotifications.prototype = {
       popupnotification.setAttribute("popupid", n.id);
       popupnotification.setAttribute("oncommand", "PopupNotifications._onCommand(event);");
       if (Services.prefs.getBoolPref("privacy.permissionPrompts.showCloseButton")) {
-        popupnotification.setAttribute("closebuttoncommand", "PopupNotifications._onButtonEvent(event, '" + n.options.escAction + "', 'esc-press');");
+        popupnotification.setAttribute("closebuttoncommand", "PopupNotifications._onButtonEvent(event, 'secondarybuttoncommand', 'esc-press');");
       } else {
         popupnotification.setAttribute("closebuttoncommand", `PopupNotifications._dismiss(event, ${TELEMETRY_STAT_DISMISSAL_CLOSE_BUTTON});`);
       }
