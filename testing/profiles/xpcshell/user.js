8f231a60e9e5d9b4269ccac22a3f0dd867e165f0
Margareta Eliza Balazs <ebalazs@mozilla.com>
2019-01-21 16:13:56 +0200

    Backed out 4 changesets (bug 1517025) for multiple failures e.g.: toolkit/components/telemetry/tests/unit/test_PingAPI.js CLOSED TREE
    Backed out changeset 1e173178e49f (bug 1517025)
    Backed out changeset c29889dea969 (bug 1517025)
    Backed out changeset 48c7d643d2fa (bug 1517025)
    Backed out changeset 419cb778d531 (bug 1517025)

--- testing/profiles/xpcshell/user.js
+++ testing/profiles/xpcshell/user.js
@@ -1,16 +1,16 @@
 // Base preferences file used by the xpcshell harness
 /* globals user_pref */
 /* eslint quotes: 0 */
-user_pref("app.normandy.api_url", "https://localhost/selfsupport-dummy/");
-user_pref("browser.safebrowsing.downloads.remote.url", "https://localhost/safebrowsing-dummy");
-user_pref("browser.search.geoip.url", "https://localhost/geoip-dummy");
-user_pref("extensions.systemAddon.update.url", "http://localhost/dummy-system-addons.xml");
+user_pref("app.normandy.api_url", "https://%(server)s/selfsupport-dummy/");
+user_pref("browser.safebrowsing.downloads.remote.url", "https://%(server)s/safebrowsing-dummy");
+user_pref("browser.search.geoip.url", "https://%(server)s/geoip-dummy");
+user_pref("extensions.systemAddon.update.url", "http://%(server)s/dummy-system-addons.xml");
 // Always use network provider for geolocation tests
 // so we bypass the OSX dialog raised by the corelocation provider
 user_pref("geo.provider.testing", true);
 user_pref("media.gmp-manager.updateEnabled", false);
-user_pref("media.gmp-manager.url.override", "http://localhost/dummy-gmp-manager.xml");
-user_pref("toolkit.telemetry.server", "https://localhost/telemetry-dummy");
+user_pref("media.gmp-manager.url.override", "http://%(server)s/dummy-gmp-manager.xml");
+user_pref("toolkit.telemetry.server", "https://%(server)s/telemetry-dummy");
 // The process priority manager only shifts priorities when it has at least
 // one active tab. xpcshell tabs don't have any active tabs, which would mean
 // all processes would run at low priority, which is not desirable, so we
