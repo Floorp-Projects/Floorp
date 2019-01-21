cce9cef3891f15ccca3f2966e14cb056679b4a48
longsonr <longsonr@gmail.com>
2019-01-21 13:08:12 +0000

    Bug 1519253 - Move nsSMILInterval and nsSMILRepeatCount to the mozilla namespace r=birtles

--- dom/smil/SMILRepeatCount.cpp
+++ dom/smil/SMILRepeatCount.cpp
@@ -4,7 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsSMILRepeatCount.h"
+#include "SMILRepeatCount.h"
 
-/*static*/ const double nsSMILRepeatCount::kNotSet = -1.0;
-/*static*/ const double nsSMILRepeatCount::kIndefinite = -2.0;
+namespace mozilla {
+
+/*static*/ const double SMILRepeatCount::kNotSet = -1.0;
+/*static*/ const double SMILRepeatCount::kIndefinite = -2.0;
+
+}  // namespace mozilla
+
