f94f1dbf3c5bfba5b6add1d2cc2e2da40325a362
Brindusan Cristian <cbrindusan@mozilla.com>
2019-01-22 16:40:55 +0200

    Backed out changeset df49bc87bba1 (bug 1506784) for devtools failures at browser_all_files_referenced.js. CLOSED TREE

--- devtools/client/framework/components/DebugTargetInfo.js
+++ devtools/client/framework/components/DebugTargetInfo.js
@@ -6,8 +6,6 @@
 const { PureComponent } = require("devtools/client/shared/vendor/react");
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
-const { CONNECTION_TYPES } =
-  require("devtools/client/shared/remote-debugging/remote-client-manager");
 
 /**
  * This is header that should be displayed on top of the toolbox when using
@@ -17,8 +15,6 @@ class DebugTargetInfo extends PureComponent {
   static get propTypes() {
     return {
       deviceDescription: PropTypes.shape({
-        channel: PropTypes.string.isRequired,
-        connectionType: PropTypes.string,
         deviceName: PropTypes.string,
         name: PropTypes.string.isRequired,
         version: PropTypes.string.isRequired,
@@ -28,100 +24,37 @@ class DebugTargetInfo extends PureComponent {
     };
   }
 
+  getTargetText() {
+    const { L10N, toolbox } = this.props;
+    const name = toolbox.target.name;
+    const type = L10N.getStr("toolbox.debugTargetInfo.type.tab");
+    return L10N.getFormatStr("toolbox.debugTargetInfo.targetLabel", name, type);
+  }
+
   getRuntimeText() {
     const { deviceDescription, L10N } = this.props;
-    const { name, version, connectionType } = deviceDescription;
-
-    return (connectionType === CONNECTION_TYPES.THIS_FIREFOX)
-      ? L10N.getFormatStr("toolbox.debugTargetInfo.runtimeLabel.thisFirefox", version)
-      : L10N.getFormatStr("toolbox.debugTargetInfo.runtimeLabel", name, version);
-  }
-
-  getAssetsForConnectionType() {
-    const { connectionType } = this.props.deviceDescription;
-
-    const BASE_IMG_URL = "chrome://devtools/skin/images/";
-    switch (connectionType) {
-      case CONNECTION_TYPES.USB:
-        return {
-          image: `${BASE_IMG_URL}aboutdebugging-usb-icon.svg`,
-          l10nId: "toolbox.debugTargetInfo.connection.usb",
-        };
-      case CONNECTION_TYPES.NETWORK:
-        return {
-          image: `${BASE_IMG_URL}aboutdebugging-globe-icon.svg`,
-          l10nId: "toolbox.debugTargetInfo.connection.network",
-        };
-    }
-  }
-
-  shallRenderConnection() {
-    const { connectionType } = this.props.deviceDescription;
-    const renderableTypes = [
-      CONNECTION_TYPES.USB,
-      CONNECTION_TYPES.NETWORK,
-    ];
-
-    return renderableTypes.includes(connectionType);
-  }
-
-  renderConnection() {
-    const { connectionType } = this.props.deviceDescription;
-    const { image, l10nId } = this.getAssetsForConnectionType();
-
-    return dom.span(
-      {
-        className: "iconized-label",
-      },
-      dom.img({ src: image, alt: `${connectionType} icon`}),
-      this.props.L10N.getStr(l10nId),
-    );
-  }
-
-  renderRuntime() {
-    const { channel, deviceName } = this.props.deviceDescription;
-
-    const channelIcon =
-      (channel === "release" || channel === "beta" || channel === "aurora") ?
-      `chrome://devtools/skin/images/aboutdebugging-firefox-${ channel }.svg` :
-      "chrome://devtools/skin/images/aboutdebugging-firefox-nightly.svg";
-
-    return dom.span(
-      {
-        className: "iconized-label",
-      },
-      dom.img({ src: channelIcon, className: "channel-icon" }),
-      dom.b({ className: "ellipsis-text" }, this.getRuntimeText()),
-      dom.span({ className: "ellipsis-text" }, deviceName),
-    );
-  }
-
-  renderTarget() {
-    const title = this.props.toolbox.target.name;
-    const url = this.props.toolbox.target.url;
-    // TODO: https://bugzilla.mozilla.org/show_bug.cgi?id=1520723
-    //       Show actual favicon (currently toolbox.target.activeTab.favicon
-    //       is unpopulated)
-    const favicon = "chrome://devtools/skin/images/aboutdebugging-globe-icon.svg";
-
-    return dom.span(
-      {
-        className: "iconized-label",
-      },
-      dom.img({ src: favicon, alt: "favicon"}),
-      title ? dom.b({ className: "ellipsis-text"}, title) : null,
-      dom.span({ className: "ellipsis-text" }, url),
-    );
+    const { name, deviceName, version } = deviceDescription;
+    const localizationType =
+      deviceName ? "toolbox.debugTargetInfo.runtimeLabelWithDeviceName"
+                 : "toolbox.debugTargetInfo.runtimeLabel";
+    return L10N.getFormatStr(localizationType, name, version, deviceName);
   }
 
   render() {
+    const { deviceDescription } = this.props;
+    const { channel } = deviceDescription;
+    const icon =
+      (channel === "release" || channel === "beta" || channel === "aurora")
+        ? `chrome://devtools/skin/images/aboutdebugging-firefox-${ channel }.svg`
+        : "chrome://devtools/skin/images/aboutdebugging-firefox-nightly.svg";
+
     return dom.header(
       {
         className: "debug-target-info",
       },
-      this.shallRenderConnection() ? this.renderConnection() : null,
-      this.renderRuntime(),
-      this.renderTarget(),
+      dom.img({ src: icon }),
+      dom.span({}, this.getRuntimeText()),
+      dom.span({ className: "target" }, this.getTargetText()),
     );
   }
 }
