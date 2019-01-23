97aef020487d34a853a762cc8bba915ce7da8ea5
Rob Wu <rob@robwu.nl>
2019-01-23 09:54:36 +0000

    Bug 1520047 - Fix memory leak in places trees r=mak
    The places-tree destructor lacks a `result.removeObserver(this.view);`
    call before the assignment to `result.root.containerOpen`, which is
    causing a memory leak.
    
    This patch fixes the leak by removing the `result.root.containerOpen`
    assignment, since the correct logic already exists in the `setTree`
    method of `PlacesTreeView`, which is called upon `this.view = null;`
    (XULTreeElement::SetView -> nsTreeBodyFrame::SetView -> setTree).
    
    Differential Revision: https://phabricator.services.mozilla.com/D17241

---
+++ browser/components/places/tests/browser/browser_library_tree_leak.js
@@ -0,0 +1,22 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+
+"use strict";
+
+add_task(async function bookmark_leak_window() {
+  // A library window has two trees after selecting a bookmark item:
+  // A left tree (#placesList) and a right tree (#placeContent).
+  // Upon closing the window, both trees are destructed, in an unspecified
+  // order. In bug 1520047, a memory leak was observed when the left tree
+  // was destroyed last.
+
+  let library = await promiseLibrary("BookmarksToolbar");
+  let tree = library.document.getElementById("placesList");
+  tree.selectItems(["toolbar_____"]);
+
+  await synthesizeClickOnSelectedTreeCell(tree, {type: "mousedown"});
+  await promiseLibraryClosed(library);
+
+  Assert.ok(true, "Closing a window after selecting a node in the tree should not cause a leak");
+});
+
