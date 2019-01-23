0b7d9a987d4ed5ab44d41cda135e5303d2e823e0
Jared Wein <jwein@mozilla.com>
2019-01-23 17:03:32 +0000

    Bug 1521170 - Add a rule that prevents calling some Array and String accessor methods without using the return value. r=Standard8,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D17020

---
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/use-returnValue.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require("../lib/rules/use-returnValue");
+var RuleTester = require("eslint/lib/testers/rule-tester");
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+function invalidCode(code, methodName) {
+  let message = `{Array/String}.${methodName} doesn't modify the instance in-place`;
+  return {code, errors: [{message, type: "ExpressionStatement"}]};
+}
+
+ruleTester.run("use-returnValue", rule, {
+  valid: [
+    "a = foo.concat(bar)",
+    "b = bar.concat([1,3,4])",
+    "c = baz.concat()",
+    "d = qux.join(' ')",
+    "e = quux.slice(1)",
+  ],
+  invalid: [
+    invalidCode("foo.concat(bar)", "concat"),
+    invalidCode("bar.concat([1,3,4])", "concat"),
+    invalidCode("baz.concat()", "concat"),
+    invalidCode("qux.join(' ')", "join"),
+    invalidCode("quux.slice(1)", "slice"),
+  ],
+});
