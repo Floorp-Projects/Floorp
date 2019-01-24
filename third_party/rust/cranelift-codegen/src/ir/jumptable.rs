97cb21c5414adb836670648c1056daba84dae1bb
Benjamin Bouvier <benj@benj.me>
2019-01-24 10:51:17 +0100

    Bug 1522173: Bump Cranelift to 0.28; r=sunfish
    --HG--
    extra : rebase_source : 0f772aa20c592a46cfdbc83a64b218a4568ff28d
    extra : histedit_source : 7148969780d2ba4fb5aeecdcb413855a4babef33

--- third_party/rust/cranelift-codegen/src/ir/jumptable.rs
+++ third_party/rust/cranelift-codegen/src/ir/jumptable.rs
@@ -3,9 +3,9 @@
 //! Jump tables are declared in the preamble and assigned an `ir::entities::JumpTable` reference.
 //! The actual table of destinations is stored in a `JumpTableData` struct defined in this module.
 
-use ir::entities::Ebb;
-use std::fmt::{self, Display, Formatter};
-use std::slice::{Iter, IterMut};
+use crate::ir::entities::Ebb;
+use core::fmt::{self, Display, Formatter};
+use core::slice::{Iter, IterMut};
 use std::vec::Vec;
 
 /// Contents of a jump table.
@@ -83,8 +83,8 @@ impl Display for JumpTableData {
 #[cfg(test)]
 mod tests {
     use super::JumpTableData;
-    use entity::EntityRef;
-    use ir::Ebb;
+    use crate::entity::EntityRef;
+    use crate::ir::Ebb;
     use std::string::ToString;
 
     #[test]
