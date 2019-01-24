cd9bcbcf9f52468bf40879de7431af1f5464a90f
Emilio Cobos Álvarez <emilio@crisal.io>
2019-01-24 17:31:20 +0000

    Bug 1522555 - Remove a lot of dead code. r=kvark
    We can always pull this back from history should we need it, but given
    how broken the texture caching of pictures was (see the recent text shadow
    story) I suspect we won't.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17524

--- gfx/wr/webrender/src/display_list_flattener.rs
+++ gfx/wr/webrender/src/display_list_flattener.rs
@@ -390,7 +390,6 @@ impl<'a> DisplayListFlattener<'a> {
             prim_list,
             main_scroll_root,
             LayoutRect::max_rect(),
-            &self.clip_store,
             Some(tile_cache),
             PictureOptions::default(),
         ));
@@ -1215,7 +1214,6 @@ impl<'a> DisplayListFlattener<'a> {
                 let extra_instance = sc.cut_flat_item_sequence(
                     &mut self.prim_store,
                     &mut self.interners,
-                    &self.clip_store,
                 );
                 (sc.is_3d(), extra_instance)
             },
@@ -1366,7 +1364,6 @@ impl<'a> DisplayListFlattener<'a> {
                 ),
                 stacking_context.spatial_node_index,
                 max_clip,
-                &self.clip_store,
                 None,
                 PictureOptions::default(),
             ))
@@ -1416,7 +1413,6 @@ impl<'a> DisplayListFlattener<'a> {
                     ),
                     stacking_context.spatial_node_index,
                     max_clip,
-                    &self.clip_store,
                     None,
                     PictureOptions::default(),
                 ))
@@ -1462,7 +1458,6 @@ impl<'a> DisplayListFlattener<'a> {
                     ),
                     stacking_context.spatial_node_index,
                     max_clip,
-                    &self.clip_store,
                     None,
                     PictureOptions::default(),
                 ))
@@ -1506,7 +1501,6 @@ impl<'a> DisplayListFlattener<'a> {
                     ),
                     stacking_context.spatial_node_index,
                     max_clip,
-                    &self.clip_store,
                     None,
                     PictureOptions::default(),
                 ))
@@ -1853,7 +1847,6 @@ impl<'a> DisplayListFlattener<'a> {
                                 ),
                                 pending_shadow.clip_and_scroll.spatial_node_index,
                                 max_clip,
-                                &self.clip_store,
                                 None,
                                 options,
                             ))
@@ -2598,7 +2591,6 @@ impl FlattenedStackingContext {
         &mut self,
         prim_store: &mut PrimitiveStore,
         interners: &mut Interners,
-        clip_store: &ClipStore,
     ) -> Option<PrimitiveInstance> {
         if !self.is_3d() || self.primitives.is_empty() {
             return None
@@ -2626,7 +2618,6 @@ impl FlattenedStackingContext {
                 ),
                 self.spatial_node_index,
                 LayoutRect::max_rect(),
-                clip_store,
                 None,
                 PictureOptions::default(),
             ))
