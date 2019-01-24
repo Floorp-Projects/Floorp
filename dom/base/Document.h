ea7eaa36e4658a5992d68dd598c182c4829174c3
Alphan Chen <alchen@mozilla.com>
2019-01-24 12:53:28 +0000

    Bug 1497146 part 3 - Convert FormData.jsm to C++ [restore() part] r=peterv,mikedeboer
    Differential Revision: https://phabricator.services.mozilla.com/D12672
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -3564,6 +3564,8 @@ class Document : public nsINode,
   void SetDocTreeHadAudibleMedia();
   void SetDocTreeHadPlayRevoked();
 
+  mozilla::dom::XPathEvaluator* XPathEvaluator();
+
  protected:
   void DoUpdateSVGUseElementShadowTrees();
 
@@ -3692,8 +3694,6 @@ class Document : public nsINode,
   void SetContentTypeInternal(const nsACString& aType);
 
   nsCString GetContentTypeInternal() const { return mContentType; }
-
-  mozilla::dom::XPathEvaluator* XPathEvaluator();
 
   // Update our frame request callback scheduling state, if needed.  This will
   // schedule or unschedule them, if necessary, and update
