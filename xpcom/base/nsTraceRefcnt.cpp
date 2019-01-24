0eb5d04802449bd8f4600c6f69a4ccd6296efa13
Andrew McCreight <continuation@gmail.com>
2019-01-24 21:30:44 +0000

    Bug 1521191, part 1 - Add method to disable dump statistics. r=froydnj
    In order to test the test harness's handling of a process failing to
    produce a leak log, add a special function that disables the bloat log
    output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17534
    
    --HG--
    extra : moz-landing-system : lando

--- xpcom/base/nsTraceRefcnt.cpp
+++ xpcom/base/nsTraceRefcnt.cpp
@@ -117,6 +117,7 @@ static unsigned gActivityTLS = BAD_TLS_INDEX;
 
 static bool gInitialized;
 static nsrefcnt gInitCount;
+static bool gDisableDumpStatistics = false;
 
 static FILE* gBloatLog = nullptr;
 static FILE* gRefcntsLog = nullptr;
@@ -398,6 +399,10 @@ nsresult nsTraceRefcnt::DumpStatistics() {
              "Calling DumpStatistics more than once may result in "
              "bogus positive or negative leaks being reported");
   gDumpedStatistics = true;
+
+  if (gDisableDumpStatistics) {
+    return NS_OK;
+  }
 
   // Don't try to log while we hold the lock, we'd deadlock.
   AutoRestore<LoggingType> saveLogging(gLogging);
@@ -1163,3 +1168,5 @@ void nsTraceRefcnt::SetActivityIsLegal(bool aLegal) {
 
   PR_SetThreadPrivate(gActivityTLS, reinterpret_cast<void*>(!aLegal));
 }
+
+void nsTraceRefcnt::DisableDumpStatistics() { gDisableDumpStatistics = true; }
