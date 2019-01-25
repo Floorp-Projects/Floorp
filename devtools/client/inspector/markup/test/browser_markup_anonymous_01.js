4117cc895cdbbe2fe016293017221e2857832bc3
Timothy Guan-tin Chien <timdream@gmail.com>
2019-01-25 13:12:26 +0000

    Bug 1507895 - Part I, Remove the videocontrols binding r=smaug
    This patch removes the XBL videocontrols binding and make <video>
    to always use the UA Widget to generate controls.
    
    DevTools tests that look for NAC is switched to use <input type=file>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17571

--- devtools/client/inspector/markup/test/browser_markup_anonymous_01.js
+++ devtools/client/inspector/markup/test/browser_markup_anonymous_01.js
@@ -30,15 +30,15 @@ add_task(async function() {
 
   const native = await getNodeFront("#native", inspector);
 
-  // Markup looks like: <div><video controls /></div>
+  // Markup looks like: <div><input type="file"></div>
   const nativeChildren = await inspector.walker.children(native);
   is(nativeChildren.nodes.length, 1, "Children returned from walker");
 
-  info("Checking the video element");
-  const video = nativeChildren.nodes[0];
-  ok(!video.isAnonymous, "<video> is not anonymous");
+  info("Checking the input element");
+  const child = nativeChildren.nodes[0];
+  ok(!child.isAnonymous, "<input type=file> is not anonymous");
 
-  const videoChildren = await inspector.walker.children(video);
-  is(videoChildren.nodes.length, 0,
-    "No native children returned from walker for <video> by default");
+  const grandchildren = await inspector.walker.children(child);
+  is(grandchildren.nodes.length, 0,
+    "No native children returned from walker for <input type=file> by default");
 });
