3445e6d632f4df88a17a49d70b52faa9f196aa1e
Raphael Pierzina <rpierzina@mozilla.com>
2019-01-28 09:53:23 +0000

    Bug 1521770 - Refactor telemetry-tests-client suite and remove firefox-puppeteer; r=janerik
    Depends on D17648
    
    Differential Revision: https://phabricator.services.mozilla.com/D17649

--- toolkit/components/telemetry/tests/marionette/harness/setup.py
+++ toolkit/components/telemetry/tests/marionette/harness/setup.py
@@ -6,7 +6,7 @@ import os
 
 from setuptools import setup, find_packages
 
-PACKAGE_VERSION = '0.1'
+PACKAGE_VERSION = "0.1"
 
 THIS_DIR = os.path.dirname(os.path.realpath(__name__))
 
@@ -16,29 +16,33 @@ def read(*parts):
         return f.read()
 
 
-setup(name='telemetry-harness',
-      version=PACKAGE_VERSION,
-      description=("""Custom Marionette runner classes and entry scripts for Telemetry
-specific Marionette tests."""),
-      classifiers=[
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      keywords='mozilla',
-      author='Firefox Test Engineering Team',
-      author_email='firefox-test-engineering@mozilla.org',
-      url='https://developer.mozilla.org/en-US/docs/Mozilla/QA/telemetry_harness',
-      license='MPL 2.0',
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=read('requirements.txt').splitlines(),
-      include_package_data=True,
-      entry_points="""
+setup(
+    name="telemetry-harness",
+    version=PACKAGE_VERSION,
+    description=(
+        "Custom Marionette runner classes and entry scripts for "
+        "Telemetry specific Marionette tests."
+    ),
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="mozilla",
+    author="Firefox Test Engineering Team",
+    author_email="firefox-test-engineering@mozilla.org",
+    url="https://developer.mozilla.org/en-US/docs/Mozilla/QA/telemetry_harness",
+    license="MPL 2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=read("requirements.txt").splitlines(),
+    entry_points="""
         [console_scripts]
         telemetry-harness = telemetry_harness.runtests:cli
-    """)
+    """,
+)
