0ca891980ff3082083fef95d0e5edd9ab0162712
Jan Odvarko <odvarko@gmail.com>
2019-01-28 13:32:51 +0000

    Bug 1521049 - Broken context menu on JSTerm icon; r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D17245

---
+++ devtools/client/webconsole/test/mochitest/browser_jsterm_context_menu_labels.js
@@ -0,0 +1,26 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests that context menu for CodeMirror is properly localized.
+
+"use strict";
+
+const TEST_URI = `data:text/html;charset=utf8,<p>test page</p>`;
+
+add_task(async function() {
+  const hud = await openNewTabAndConsole(TEST_URI);
+  const { jsterm } = hud;
+
+  // Open context menu and wait until it's visible
+  const element = jsterm.node.querySelector(".CodeMirror-wrap");
+  const menuPopup = await openContextMenu(hud, element);
+
+  // Check label of the 'undo' menu item.
+  const undoMenuItem = menuPopup.querySelector("#editmenu-undo");
+  await waitUntil(() => !!undoMenuItem.getAttribute("label"));
+
+  is(undoMenuItem.getAttribute("label"), "Undo",
+    "Undo is visible and localized");
+});
