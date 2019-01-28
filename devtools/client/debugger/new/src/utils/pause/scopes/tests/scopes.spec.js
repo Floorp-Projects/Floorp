ebed82cd02f80a928f3f0e70d670595bd15bb2bb
Brian Hackett <bhackett1024@gmail.com>
2019-01-28 15:04:47 -0500

    Bug 1523386 - [release 121] Refactor src/utils/pause unit tests. (#7798). r=dwalsh

--- devtools/client/debugger/new/src/utils/pause/scopes/tests/scopes.spec.js
+++ devtools/client/debugger/new/src/utils/pause/scopes/tests/scopes.spec.js
@@ -2,32 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+// @flow
+
 import { getScopes } from "..";
+import {
+  makeMockFrame,
+  makeMockScope,
+  makeWhyNormal,
+  makeWhyThrow,
+  mockScopeAddVariable
+} from "../../../test-mockup";
+
+import type { Scope } from "../../../../types";
+import type { RenderableScope } from "../getScope";
+
+function convertScope(scope: Scope): RenderableScope {
+  return (scope: any);
+}
 
 describe("scopes", () => {
   describe("getScopes", () => {
     it("single scope", () => {
-      const pauseData = {
-        frame: {
-          this: {}
-        }
-      };
+      const pauseData = makeWhyNormal();
+      const scope = makeMockScope("actor1");
+      const selectedFrame = makeMockFrame(undefined, undefined, scope);
 
-      const selectedFrame = {
-        scope: {
-          actor: "actor1",
-          type: "block",
-          bindings: {
-            arguments: [],
-            variables: {}
-          },
-          parent: null
-        },
-        this: {}
-      };
-
-      const frameScopes = selectedFrame.scope;
+      const frameScopes = convertScope(selectedFrame.scope);
       const scopes = getScopes(pauseData, selectedFrame, frameScopes);
+      if (!scopes) {
+        throw new Error("missing scopes");
+      }
       expect(scopes[0].path).toEqual("actor1-1");
       expect(scopes[0].contents[0]).toEqual({
         name: "<this>",
@@ -37,65 +41,31 @@ describe("scopes", () => {
     });
 
     it("second scope", () => {
-      const pauseData = {
-        frame: {
-          this: {}
-        }
-      };
+      const pauseData = makeWhyNormal();
+      const scope0 = makeMockScope("actor2");
+      const scope1 = makeMockScope("actor1", undefined, scope0);
+      const selectedFrame = makeMockFrame(undefined, undefined, scope1);
+      mockScopeAddVariable(scope0, "foo");
 
-      const selectedFrame = {
-        scope: {
-          actor: "actor1",
-          type: "block",
-          bindings: {
-            arguments: [],
-            variables: {}
-          },
-          parent: {
-            actor: "actor2",
-            type: "block",
-            bindings: {
-              arguments: [],
-              variables: {
-                foo: {}
-              }
-            }
-          }
-        },
-        this: {}
-      };
-
-      const frameScopes = selectedFrame.scope;
+      const frameScopes = convertScope(selectedFrame.scope);
       const scopes = getScopes(pauseData, selectedFrame, frameScopes);
+      if (!scopes) {
+        throw new Error("missing scopes");
+      }
       expect(scopes[1].path).toEqual("actor2-2");
       expect(scopes[1].contents[0]).toEqual({
         name: "foo",
         path: "actor2-2/foo",
-        contents: {}
+        contents: { value: null }
       });
     });
 
     it("returning scope", () => {
-      const why = {
-        frameFinished: {
-          return: "to sender"
-        }
-      };
+      const why = makeWhyNormal("to sender");
+      const scope = makeMockScope("actor1");
+      const selectedFrame = makeMockFrame(undefined, undefined, scope);
 
-      const selectedFrame = {
-        scope: {
-          actor: "actor1",
-          type: "block",
-          bindings: {
-            arguments: [],
-            variables: {}
-          },
-          parent: null
-        },
-        this: {}
-      };
-
-      const frameScopes = selectedFrame.scope;
+      const frameScopes = convertScope(selectedFrame.scope);
       const scopes = getScopes(why, selectedFrame, frameScopes);
       expect(scopes).toMatchObject([
         {
@@ -121,26 +91,11 @@ describe("scopes", () => {
     });
 
     it("throwing scope", () => {
-      const why = {
-        frameFinished: {
-          throw: "a party"
-        }
-      };
+      const why = makeWhyThrow("a party");
+      const scope = makeMockScope("actor1");
+      const selectedFrame = makeMockFrame(undefined, undefined, scope);
 
-      const selectedFrame = {
-        scope: {
-          actor: "actor1",
-          type: "block",
-          bindings: {
-            arguments: [],
-            variables: {}
-          },
-          parent: null
-        },
-        this: {}
-      };
-
-      const frameScopes = selectedFrame.scope;
+      const frameScopes = convertScope(selectedFrame.scope);
       const scopes = getScopes(why, selectedFrame, frameScopes);
       expect(scopes).toMatchObject([
         {
