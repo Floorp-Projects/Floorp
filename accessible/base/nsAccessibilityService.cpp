139370c337aca94666ebe6f1b1231c6a5334e3e5
Marco Zehe <mzehe@mozilla.com>
2019-01-29 00:05:53 +0000

    Bug 646216 - Thin out the tree by only creating accessibles for relevant divs, r=Jamie,timdream
    Relevant divs are:
    
    * Those that have an ID attribute. This is important so anchors still work.
    * Those whose first or last child is a text-only node.
    * Those whose first or last child has an inline frame.
    
    We now discard divs that are not display:block; or display:inline-block;. We also discard divs that are part of an anonymous subtree.
    
    We stop creating divs from the eHyperTextType frame type alltogether.
    
    Note that because of shadow DOM properties in the video controls, two additional divs with IDs require role="none" in the media controls widget code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17348

--- accessible/base/nsAccessibilityService.cpp
+++ accessible/base/nsAccessibilityService.cpp
@@ -20,6 +20,7 @@
 #include "HTMLTableAccessibleWrap.h"
 #include "HyperTextAccessibleWrap.h"
 #include "RootAccessible.h"
+#include "StyleInfo.h"
 #include "nsAccUtils.h"
 #include "nsArrayUtils.h"
 #include "nsAttrName.h"
@@ -1408,8 +1409,10 @@ nsAccessibilityService::CreateAccessibleByFrameType(nsIFrame* aFrame,
       newAcc = new HTMLTextFieldAccessible(aContent, document);
       break;
     case eHyperTextType:
-      if (!aContent->IsAnyOfHTMLElements(nsGkAtoms::dt, nsGkAtoms::dd))
+      if (!aContent->IsAnyOfHTMLElements(nsGkAtoms::dt, nsGkAtoms::dd,
+                                         nsGkAtoms::div)) {
         newAcc = new HyperTextAccessibleWrap(aContent, document);
+      }
       break;
 
     case eImageType:
