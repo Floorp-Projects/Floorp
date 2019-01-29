40ebc5bbed11a93b46d11d11284bac0895f304f8
Razvan Maries <rmaries@mozilla.com>
2019-01-29 05:30:25 +0200

    Backed out changeset 64d26b5a25b8 (bug 1516704) for bc failures at browser_ext_sessions_incognito.js. CLOSED TREE

--- browser/components/extensions/test/browser/browser_ext_sessions_forgetClosedWindow.js
+++ browser/components/extensions/test/browser/browser_ext_sessions_forgetClosedWindow.js
@@ -2,7 +2,14 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-function getExtension(incognitoOverride) {
+add_task(async function test_sessions_forget_closed_window() {
+  async function openAndCloseWindow(url = "http://example.com") {
+    let win = await BrowserTestUtils.openNewBrowserWindow();
+    await BrowserTestUtils.loadURI(win.gBrowser.selectedBrowser, url);
+    await BrowserTestUtils.browserLoaded(win.gBrowser.selectedBrowser);
+    await BrowserTestUtils.closeWindow(win);
+  }
+
   function background() {
     browser.test.onMessage.addListener((msg, sessionId) => {
       if (msg === "check-sessions") {
@@ -15,32 +22,23 @@ function getExtension(incognitoOverride) {
             browser.test.sendMessage("forgot-window");
           },
           error => {
-            browser.test.sendMessage("forget-reject", error.message);
+            browser.test.assertEq(
+              error.message,
+              `Could not find closed window using sessionId ${sessionId}.`);
+            browser.test.sendMessage("forget-reject");
           }
         );
       }
     });
   }
 
-  return ExtensionTestUtils.loadExtension({
+  let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       permissions: ["sessions", "tabs"],
     },
     background,
-    incognitoOverride,
   });
-}
 
-async function openAndCloseWindow(url = "http://example.com", privateWin) {
-  let win = await BrowserTestUtils.openNewBrowserWindow({private: privateWin});
-  let tab = await BrowserTestUtils.openNewForegroundTab(win.gBrowser, url);
-  let sessionUpdatePromise = BrowserTestUtils.waitForSessionStoreUpdate(tab);
-  await BrowserTestUtils.closeWindow(win);
-  await sessionUpdatePromise;
-}
-
-add_task(async function test_sessions_forget_closed_window() {
-  let extension = getExtension();
   await extension.startup();
 
   await openAndCloseWindow("about:config");
@@ -48,6 +46,7 @@ add_task(async function test_sessions_forget_closed_window() {
 
   extension.sendMessage("check-sessions");
   let recentlyClosed = await extension.awaitMessage("recentlyClosed");
+  let recentlyClosedLength = recentlyClosed.length;
   let recentlyClosedWindow = recentlyClosed[0].window;
 
   // Check that forgetting a window works properly
@@ -55,51 +54,20 @@ add_task(async function test_sessions_forget_closed_window() {
   await extension.awaitMessage("forgot-window");
   extension.sendMessage("check-sessions");
   let remainingClosed = await extension.awaitMessage("recentlyClosed");
-  is(remainingClosed.length, recentlyClosed.length - 1,
+  is(remainingClosed.length, recentlyClosedLength - 1,
      "One window was forgotten.");
   is(remainingClosed[0].window.sessionId, recentlyClosed[1].window.sessionId,
      "The correct window was forgotten.");
 
   // Check that re-forgetting the same window fails properly
   extension.sendMessage("forget-window", recentlyClosedWindow.sessionId);
-  let errMsg = await extension.awaitMessage("forget-reject");
-  is(errMsg, `Could not find closed window using sessionId ${recentlyClosedWindow.sessionId}.`);
-
+  await extension.awaitMessage("forget-reject");
   extension.sendMessage("check-sessions");
   remainingClosed = await extension.awaitMessage("recentlyClosed");
-  is(remainingClosed.length, recentlyClosed.length - 1,
+  is(remainingClosed.length, recentlyClosedLength - 1,
      "No extra window was forgotten.");
   is(remainingClosed[0].window.sessionId, recentlyClosed[1].window.sessionId,
      "The correct window remains.");
 
   await extension.unload();
-});
-
-add_task(async function test_sessions_forget_closed_window_private() {
-  SpecialPowers.pushPrefEnv({set: [
-    ["extensions.allowPrivateBrowsingByDefault", false],
-  ]});
-
-  let pb_extension = getExtension("spanning");
-  await pb_extension.startup();
-  let extension = getExtension("not_allowed");
-  await extension.startup();
-
-  await openAndCloseWindow("about:config", true);
-  await openAndCloseWindow("about:robots", true);
-
-  pb_extension.sendMessage("check-sessions");
-  let recentlyClosed = await pb_extension.awaitMessage("recentlyClosed");
-  let recentlyClosedWindow = recentlyClosed[0].window;
-
-  extension.sendMessage("forget-window", recentlyClosedWindow.sessionId);
-  await extension.awaitMessage("forgot-window");
-  extension.sendMessage("check-sessions");
-  let remainingClosed = await extension.awaitMessage("recentlyClosed");
-  is(remainingClosed.length, recentlyClosed.length - 1,
-     "One window was forgotten.");
-  ok(!recentlyClosedWindow.incognito, "not an incognito window");
-
-  await extension.unload();
-  await pb_extension.unload();
 });
