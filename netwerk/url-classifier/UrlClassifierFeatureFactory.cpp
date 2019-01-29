3e5afa3f7479a8ed78c333e3d985d541776f362a
Andrea Marchesini <amarchesini@mozilla.com>
2019-01-29 10:11:33 +0100

    Bug 1522265 - Moving malware, phishing and blocked URIs to features - part 1 - feature no channel, r=dimi

--- netwerk/url-classifier/UrlClassifierFeatureFactory.cpp
+++ netwerk/url-classifier/UrlClassifierFeatureFactory.cpp
@@ -11,6 +11,7 @@
 #include "UrlClassifierFeatureFingerprinting.h"
 #include "UrlClassifierFeatureFlash.h"
 #include "UrlClassifierFeatureLoginReputation.h"
+#include "UrlClassifierFeatureNoChannel.h"
 #include "UrlClassifierFeatureTrackingProtection.h"
 #include "UrlClassifierFeatureTrackingAnnotation.h"
 #include "UrlClassifierFeatureCustomTables.h"
@@ -30,6 +31,7 @@ namespace net {
   UrlClassifierFeatureFingerprinting::MaybeShutdown();
   UrlClassifierFeatureFlash::MaybeShutdown();
   UrlClassifierFeatureLoginReputation::MaybeShutdown();
+  UrlClassifierFeatureNoChannel::MaybeShutdown();
   UrlClassifierFeatureTrackingAnnotation::MaybeShutdown();
   UrlClassifierFeatureTrackingProtection::MaybeShutdown();
 }
@@ -127,6 +129,12 @@ UrlClassifierFeatureFactory::GetFeatureByName(const nsACString& aName) {
     return feature.forget();
   }
 
+  // NoChannel features
+  feature = UrlClassifierFeatureNoChannel::GetIfNameMatches(aName);
+  if (feature) {
+    return feature.forget();
+  }
+
   return nullptr;
 }
 
@@ -168,9 +176,18 @@ UrlClassifierFeatureFactory::GetFeatureByName(const nsACString& aName) {
   }
 
   // Flash features
-  nsTArray<nsCString> features;
-  UrlClassifierFeatureFlash::GetFeatureNames(features);
-  aArray.AppendElements(features);
+  {
+    nsTArray<nsCString> features;
+    UrlClassifierFeatureFlash::GetFeatureNames(features);
+    aArray.AppendElements(features);
+  }
+
+  // NoChannel features
+  {
+    nsTArray<nsCString> features;
+    UrlClassifierFeatureNoChannel::GetFeatureNames(features);
+    aArray.AppendElements(features);
+  }
 }
 
 /* static */ already_AddRefed<nsIUrlClassifierFeature>
