0482099be6265cb44145f2031ff0cac664b3599a
Brian Hackett <bhackett1024@gmail.com>
2019-01-29 07:07:42 -1000

    Bug 1523492 - Make web replay tests tier 2, r=jmaher.
    --HG--
    extra : rebase_source : 685f592a18e28d172842745ade37852e23a85a6d

--- testing/mozharness/configs/unittests/mac_unittest.py
+++ testing/mozharness/configs/unittests/mac_unittest.py
@@ -144,6 +144,7 @@ config = {
         "mochitest-webgl2-deqp": ["--subsuite=webgl2-deqp"],
         "mochitest-devtools-chrome": ["--flavor=browser", "--subsuite=devtools"],
         "mochitest-devtools-chrome-chunked": ["--flavor=browser", "--subsuite=devtools", "--chunk-by-runtime"],
+        "mochitest-devtools-webreplay": ["--flavor=browser", "--subsuite=devtools-webreplay"],
         "a11y": ["--flavor=a11y"],
     },
     # local reftest suites
