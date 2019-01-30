c4d358e2e3ef9d98e937200ac6a5cfdc1659575b
Alex Gaynor <agaynor@mozilla.com>
2019-01-30 21:37:11 +0000

    Bug 1513687 - remove chromium's random code from IPC in favor of our own; r=froydnj a=lizzard
    This includes deleting several unused functions. Our own code does a better job
    of using the preferred platform APIs for random numbers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18120

--- ipc/chromium/src/base/rand_util.cc
+++ ipc/chromium/src/base/rand_util.cc
@@ -12,6 +12,8 @@
 
 #include "base/basictypes.h"
 #include "base/logging.h"
+#include "mozilla/RandomNum.h"
+
 
 namespace base {
 
@@ -19,23 +21,10 @@ int RandInt(int min, int max) {
   DCHECK(min <= max);
 
   uint64_t range = static_cast<int64_t>(max) - min + 1;
-  uint64_t number = base::RandUint64();
-  int result = min + static_cast<int>(number % range);
+  mozilla::Maybe<uint64_t> number = mozilla::RandomUint64();
+  MOZ_RELEASE_ASSERT(number.isSome());
+  int result = min + static_cast<int>(number.value() % range);
   DCHECK(result >= min && result <= max);
-  return result;
-}
-
-double RandDouble() {
-  // We try to get maximum precision by masking out as many bits as will fit
-  // in the target type's mantissa, and raising it to an appropriate power to
-  // produce output in the range [0, 1).  For IEEE 754 doubles, the mantissa
-  // is expected to accommodate 53 bits.
-
-  COMPILE_ASSERT(std::numeric_limits<double>::radix == 2, otherwise_use_scalbn);
-  static const int kBits = std::numeric_limits<double>::digits;
-  uint64_t random_bits = base::RandUint64() & ((GG_UINT64_C(1) << kBits) - 1);
-  double result = ldexp(static_cast<double>(random_bits), -1 * kBits);
-  DCHECK(result >= 0.0 && result < 1.0);
   return result;
 }
 
