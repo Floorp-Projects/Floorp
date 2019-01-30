38a84ff8d18bd15d88ddc84b44f5ae639718df74
Luca Greco <lgreco@mozilla.com>
2019-01-30 18:05:34 +0000

    Bug 1398518 - Fix test_ext_notifications.html intermittency and re-enable it. r=mixedpuppy, a=test-only
    Based on what I've been able to reproduce locally using --verify, there is a chance that
    the notifications created by the test case named `test_notifications_populated_getAll`
    may have been removed before the `browser.notifications.getAll` is going to retrieve them,
    and when this happens the test gets stuck because `browser.test.notifyPass("getAll populated");`
    is never reached, and the test timeouts.
    
    This patch includes the following changes to prevent the intermittent failure described above:
    
    - add a new head_notifications.js support file and create a new MockAlertsService test helper that
      loads a chrome script which replace the alerts service with a mock service (based on a
      similar mock defined in dom/notification/test/mochitest/MockServices.js), the mocked alert
      service doesn't close the notification until the test case itself has called
      `MockAlertsService.clearNotifications` (so that the test shouldn't fail intermittently as
      described above).
    
    - applies the changes needed on test_ext_notifications.html to make use of the MockAlertsService
    
    Differential Revision: https://phabricator.services.mozilla.com/D17863

---
+++ toolkit/components/extensions/test/mochitest/head_notifications.js
@@ -0,0 +1,108 @@
+"use strict";
+
+/* exported MockAlertsService */
+
+function mockServicesChromeScript() {
+  const MOCK_ALERTS_CID = Components.ID("{48068bc2-40ab-4904-8afd-4cdfb3a385f3}");
+  const ALERTS_SERVICE_CONTRACT_ID = "@mozilla.org/alerts-service;1";
+
+  const {setTimeout} = ChromeUtils.import("resource://gre/modules/Timer.jsm", {});
+  const registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+
+  let activeNotifications = Object.create(null);
+
+  const mockAlertsService = {
+    showPersistentNotification: function(persistentData, alert, alertListener) {
+      this.showAlert(alert, alertListener);
+    },
+
+    showAlert: function(alert, listener) {
+      activeNotifications[alert.name] = {
+        listener: listener,
+        cookie: alert.cookie,
+        title: alert.title,
+      };
+
+      // fake async alert show event
+      if (listener) {
+        setTimeout(function() {
+          listener.observe(null, "alertshow", alert.cookie);
+        }, 100);
+      }
+    },
+
+    showAlertNotification: function(imageUrl, title, text, textClickable,
+                                    cookie, alertListener, name) {
+      this.showAlert({
+        name: name,
+        cookie: cookie,
+        title: title,
+      }, alertListener);
+    },
+
+    closeAlert: function(name) {
+      let alertNotification = activeNotifications[name];
+      if (alertNotification) {
+        if (alertNotification.listener) {
+          alertNotification.listener.observe(null, "alertfinished", alertNotification.cookie);
+        }
+        delete activeNotifications[name];
+      }
+    },
+
+    QueryInterface: ChromeUtils.generateQI(["nsIAlertsService"]),
+
+    createInstance: function(outer, iid) {
+      if (outer != null) {
+        throw Cr.NS_ERROR_NO_AGGREGATION;
+      }
+      return this.QueryInterface(iid);
+    },
+  };
+
+  registrar.registerFactory(MOCK_ALERTS_CID, "alerts service",
+                            ALERTS_SERVICE_CONTRACT_ID,
+                            mockAlertsService);
+
+  function closeAllNotifications() {
+    for (let alertName of Object.keys(activeNotifications)) {
+      mockAlertsService.closeAlert(alertName);
+    }
+  }
+
+  const {addMessageListener, sendAsyncMessage} = this;
+
+  addMessageListener("mock-alert-service:unregister", () => {
+    closeAllNotifications();
+    activeNotifications = null;
+    registrar.unregisterFactory(MOCK_ALERTS_CID, mockAlertsService);
+    sendAsyncMessage("mock-alert-service:unregistered");
+  });
+
+  addMessageListener("mock-alert-service:close-notifications", closeAllNotifications);
+
+  sendAsyncMessage("mock-alert-service:registered");
+}
+
+const MockAlertsService = {
+  async register() {
+    if (this._chromeScript) {
+      throw new Error("MockAlertsService already registered");
+    }
+    this._chromeScript = SpecialPowers.loadChromeScript(mockServicesChromeScript);
+    await this._chromeScript.promiseOneMessage("mock-alert-service:registered");
+  },
+  async unregister() {
+    if (!this._chromeScript) {
+      throw new Error("MockAlertsService not registered");
+    }
+    this._chromeScript.sendAsyncMessage("mock-alert-service:unregister");
+    return this._chromeScript.promiseOneMessage("mock-alert-service:unregistered").then(() => {
+      this._chromeScript.destroy();
+      this._chromeScript = null;
+    });
+  },
+  async closeNotifications() {
+    await this._chromeScript.sendAsyncMessage("mock-alert-service:close-notifications");
+  },
+};
