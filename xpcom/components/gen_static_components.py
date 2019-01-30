1c03bd05b91ba236405a1107ad467688b6d2f7cb
Kris Maglione <maglione.k@gmail.com>
2019-01-29 17:46:27 -0800

    Bug 1524688: Part 1a - Support static registration JS components. r=mccr8

--- xpcom/components/gen_static_components.py
+++ xpcom/components/gen_static_components.py
@@ -218,6 +218,8 @@ class ModuleEntry(object):
         self.legacy_constructor = data.get('legacy_constructor', None)
         self.init_method = data.get('init_method', [])
 
+        self.jsm = data.get('jsm', None)
+
         self.external = data.get('external', not (self.headers or
                                                   self.legacy_constructor))
         self.singleton = data.get('singleton', False)
@@ -236,7 +238,15 @@ class ModuleEntry(object):
                 str(self.cid), ', '.join(map(repr, self.contract_ids)),
                 str_))
 
-        if self.external:
+        if self.jsm:
+            if not self.constructor:
+                error("JavaScript components must specify a constructor")
+
+            for prop in ('init_method', 'legacy_constructor', 'headers'):
+                if getattr(self, prop):
+                    error("JavaScript components may not specify a '%s' "
+                          "property" % prop)
+        elif self.external:
             if self.constructor or self.legacy_constructor:
                 error("Externally-constructed components may not specify "
                       "'constructor' or 'legacy_constructor' properties")
@@ -299,7 +309,14 @@ class ModuleEntry(object):
                     % self.legacy_constructor)
             return res
 
-        if self.external:
+        if self.jsm:
+            res += (
+                '      nsCOMPtr<nsISupports> inst;\n'
+                '      MOZ_TRY(ConstructJSMComponent(NS_LITERAL_CSTRING(%s),\n'
+                '                                    %s,\n'
+                '                                    getter_AddRefs(inst)));'
+                '\n' % (json.dumps(self.jsm), json.dumps(self.constructor)))
+        elif self.external:
             res += ('      nsCOMPtr<nsISupports> inst = '
                     'mozCreateComponent<%s>();\n' % self.type)
         else:
