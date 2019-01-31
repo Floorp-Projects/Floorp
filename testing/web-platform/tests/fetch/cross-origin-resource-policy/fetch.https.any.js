73e6d8ac96c3207a82c04fe718467af62111ef22
youennf <youennf@users.noreply.github.com>
2019-01-31 18:57:31 +0000

    Bug 1523562 [wpt PR 14907] - Split fetch CORP tests as HTTP and HTTPS., a=testonly
    Automatic update from web-platform-tests
    Split fetch CORP tests as HTTP and HTTPS. (#14907)
    
    Split fetch CORP tests as HTTP and HTTPS. Run service worker tests for HTTPS only.
    fetch.https.any.js contains a subset of tests from fetch.any.js.
    
    --
    
    wpt-commits: f15f912b29d110e0e7bd9d7fc44c5f4da47e6732
    wpt-pr: 14907

--- testing/web-platform/tests/fetch/cross-origin-resource-policy/fetch.https.any.js
+++ testing/web-platform/tests/fetch/cross-origin-resource-policy/fetch.https.any.js
@@ -1,12 +1,10 @@
-// META: global=window,dedicatedworker,sharedworker
+// META: global=window,worker
 // META: script=/common/get-host-info.sub.js
 
 const host = get_host_info();
 const path = "/fetch/cross-origin-resource-policy/";
-const localBaseURL = host.HTTP_ORIGIN + path;
-const sameSiteBaseURL = "http://" + host.ORIGINAL_HOST + ":" + host.HTTP_PORT2 + path;
-const notSameSiteBaseURL = host.HTTP_NOTSAMESITE_ORIGIN + path;
-const httpsBaseURL = host.HTTPS_ORIGIN + path;
+const localBaseURL = host.HTTPS_ORIGIN + path;
+const notSameSiteBaseURL = host.HTTPS_NOTSAMESITE_ORIGIN + path;
 
 promise_test(async () => {
     const response = await fetch("./resources/hello.py?corp=same-origin");
@@ -37,24 +35,6 @@ promise_test((test) => {
     const remoteURL = notSameSiteBaseURL + "resources/hello.py?corp=same-site";
     return promise_rejects(test, new TypeError, fetch(remoteURL, { mode: "no-cors" }));
 }, "Cross-origin no-cors fetch with a 'Cross-Origin-Resource-Policy: same-site' response header.");
-
-promise_test((test) => {
-    const remoteURL = httpsBaseURL + "resources/hello.py?corp=same-site";
-    return promise_rejects(test, new TypeError, fetch(remoteURL, { mode: "no-cors" }));
-}, "Cross-scheme (HTTP to HTTPS) no-cors fetch to a same-site URL with a 'Cross-Origin-Resource-Policy: same-site' response header.");
-
-promise_test((test) => {
-    const remoteURL = httpsBaseURL + "resources/hello.py?corp=same-origin";
-    return promise_rejects(test, new TypeError, fetch(remoteURL, { mode : "no-cors" }));
-}, "Cross-origin no-cors fetch to a same-site URL with a 'Cross-Origin-Resource-Policy: same-origin' response header.");
-
-promise_test(async (test) => {
-    const remoteSameSiteURL = sameSiteBaseURL + "resources/hello.py?corp=same-site";
-
-    await fetch(remoteSameSiteURL, { mode: "no-cors" });
-
-    return promise_rejects(test, new TypeError, fetch(sameSiteBaseURL + "resources/hello.py?corp=same-origin", { mode: "no-cors" }));
-}, "Valid cross-origin no-cors fetch with a 'Cross-Origin-Resource-Policy: same-site' response header.");
 
 promise_test((test) => {
     const finalURL = notSameSiteBaseURL + "resources/hello.py?corp=same-origin";
