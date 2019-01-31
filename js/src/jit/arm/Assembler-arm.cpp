69de5e0d13e62da43d09b174bab855201463c070
Lars T Hansen <lhansen@mozilla.com>
2019-01-31 13:48:48 +0100

    Bug 1524201 - add message to static_assert.  r=bugfix, CLOSED TREE

--- js/src/jit/arm/Assembler-arm.cpp
+++ js/src/jit/arm/Assembler-arm.cpp
@@ -1115,8 +1115,8 @@ VFPRegister VFPRegister::singleOverlay(unsigned int which) const {
   return VFPRegister(code_, Single);
 }
 
-// This is necessary for sintOverlay() to make any kind of sense.
-static_assert(FloatRegisters::TotalDouble <= 16);
+static_assert(FloatRegisters::TotalDouble <= 16,
+              "We assume that every Double register also has an Integer personality");
 
 VFPRegister VFPRegister::sintOverlay(unsigned int which) const {
   MOZ_ASSERT(!_isInvalid);
