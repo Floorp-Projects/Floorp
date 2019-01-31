b495597ff9c64a45183c61c379d92f75793ca300
Luca Greco <lgreco@mozilla.com>
2019-01-31 17:45:28 +0000

    Bug 1518486 - Reuse shared loadTestSubscript test helper in other extensions mochitest-browser test files. r=mixedpuppy
    Depends on D15949
    
    Differential Revision: https://phabricator.services.mozilla.com/D15950

--- browser/components/extensions/test/browser/browser_ext_browserAction_popup_resize_bottom.js
+++ browser/components/extensions/test/browser/browser_ext_browserAction_popup_resize_bottom.js
@@ -2,8 +2,7 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-Services.scriptloader.loadSubScript(new URL("head_browserAction.js", gTestPath).href,
-                                    this);
+loadTestSubscript("head_browserAction.js");
 
 add_task(async function testSetup() {
   Services.prefs.setBoolPref("toolkit.cosmeticAnimations.enabled", false);
