2caaf76949c66c8e4238a14ace8d34e06be7ee9d
Yi Gu <yigu@chromium.org>
2019-01-31 18:30:25 +0000

    Bug 1523562 [wpt PR 14802] - [Animation Worklet] Upstream worklet animation with invalid effect test to WPT, a=testonly
    Automatic update from web-platform-tests
    [Animation Worklet] Upstream worklet animation with invalid effect test to WPT
    
    This CL upstreams worklet animation with invalid effect test to WPT and
    adds necessary API to the helper file. There is no behavior change to
    the test.
    
    Bug: 915352
    Change-Id: I59af33fe7cf6ec390c06f13c5c884a1ba4fbb078
    Reviewed-on: https://chromium-review.googlesource.com/c/1405288
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#621632}
    
    --
    
    wpt-commits: c0ad38ead2b3d39a2c5baf72890dbcd67cb85bb9
    wpt-pr: 14802

--- testing/web-platform/tests/animation-worklet/common.js
+++ testing/web-platform/tests/animation-worklet/common.js
@@ -1,5 +1,13 @@
 'use strict';
 
+function registerPassthroughAnimator() {
+  return runInAnimationWorklet(`
+    registerAnimator('passthrough', class {
+      animate(currentTime, effect) { effect.localTime = currentTime; }
+    });
+  `);
+}
+
 function runInAnimationWorklet(code) {
   return CSS.animationWorklet.addModule(
     URL.createObjectURL(new Blob([code], {type: 'text/javascript'}))
