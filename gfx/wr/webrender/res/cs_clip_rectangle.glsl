4db94822bda01c5e6942ac5a1b3d402ff651089a
shindli <shindli@mozilla.com>
2019-01-31 22:02:27 +0200

    Backed out changeset 86daa5d406b6 (bug 1523882) for wrench bustage in reftests/boxshadow/box-shadow-huge-radius.png CLOSED TREE

--- gfx/wr/webrender/res/cs_clip_rectangle.glsl
+++ gfx/wr/webrender/res/cs_clip_rectangle.glsl
@@ -72,8 +72,7 @@ void main(void) {
         prim_transform,
         clip_transform,
         area,
-        cmi.sub_rect,
-        cmi.snap_offsets
+        cmi.sub_rect
     );
 
     vLocalPos = vi.local_pos;
