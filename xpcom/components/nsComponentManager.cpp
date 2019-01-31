b034573e197e80b094641f82d21fa4b15c55bfe5
Kris Maglione <maglione.k@gmail.com>
2019-01-30 16:55:45 -0800

    Bug 1524688: Part 64 - Reduce component manager allocations. r=froydnj
    With most of the JS components converted to static registration, the string
    arena and component hashtables are much smaller than the minimum space we
    allocate for them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18474

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -338,7 +338,7 @@ nsresult nsComponentManagerImpl::Create(nsISupports* aOuter, REFNSIID aIID,
   return gComponentManager->QueryInterface(aIID, aResult);
 }
 
-static const int CONTRACTID_HASHTABLE_INITIAL_LENGTH = 128;
+static const int CONTRACTID_HASHTABLE_INITIAL_LENGTH = 32;
 
 nsComponentManagerImpl::nsComponentManagerImpl()
     : mFactories(CONTRACTID_HASHTABLE_INITIAL_LENGTH),
