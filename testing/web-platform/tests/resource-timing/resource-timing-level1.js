4051f68b24e1609b407f3e20783abcc2edb5693b
Yoav Weiss <yoav@yoav.ws>
2019-01-31 15:44:18 +0000

    Bug 1517720 [wpt PR 14713] - Fix resource-timing.html document.domain test - take 2, a=testonly
    Automatic update from web-platform-tests
    Fix resource-timing.html document.domain test - take 2 (#14713)
    
    * Fix resource-timing.html document.domain test
    
    The document.domain test on resource-timing.html was not passing on any
    browser. Looking into it, the test was broken.
    This CL fixes it, removes it from TestExpectations, as well as rename it
    as many more tests were added since this was *the* L1 test suite.
    
    Change-Id: Ibaa26032ec36a652aba352c5bb08ba6975d8b3f6
    Reviewed-on: https://chromium-review.googlesource.com/c/1387475
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#618590}
    
    * Moved the document domain test out of the other legacy tests, to verify that it is running from the right domain
    
    --
    
    wpt-commits: 2ecc3df3498165fd368a1c45ee7f87e4aacbf439
    wpt-pr: 14713

--- testing/web-platform/tests/resource-timing/resource-timing-level1.js
+++ testing/web-platform/tests/resource-timing/resource-timing-level1.js
@@ -53,34 +53,6 @@ window.onload =
                             "'Src' of new <iframe> must be 'about:blank'.");
                     }
             },
-            {
-                description: "Setting 'document.domain' does not effect same-origin checks",
-                test:
-                    function (test) {
-                        initiateFetch(
-                            test,
-                            "iframe",
-                            canonicalize("iframe-setdomain.sub.html"),
-                            function (initiator, entry) {
-                                // Ensure that the script inside the IFrame has successfully changed the IFrame's domain.
-                                assert_throws(
-                                    null,
-                                    function () {
-                                        assert_not_equals(frame.contentWindow.document, null);
-                                    },
-                                    "Test Error: IFrame is not recognized as cross-domain.");
-
-                                // To verify that setting 'document.domain' did not change the results of the timing allow check,
-                                // verify that the following non-zero properties return their value.
-                                ["domainLookupStart", "domainLookupEnd", "connectStart", "connectEnd"]
-                                    .forEach(function(property) {
-                                        assert_greater_than(entry.connectEnd, 0,
-                                            "Property should be non-zero because timing allow check ignores 'document.domain'.");
-                                    });
-                                test.done();
-                            });
-                    }
-            }
         ];
 
         // Create cached/uncached tests from the following array of templates.  For each template entry,
@@ -324,7 +296,7 @@ window.onload =
             // Multiple browsers seem to cheat a bit and race img.onLoad and setting responseEnd.  Microsoft https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/2379187
             // Yield for 100ms to workaround a suspected race where window.onload fires before
             //     script visible side-effects from the wininet/urlmon thread have finished.
-            window.setTimeout(
+            test.step_timeout(
                 test.step_func(
                     function () {
                         performance
@@ -468,7 +440,7 @@ window.onload =
             when invoked. */
         function createOnloadCallbackFn(test, initiator, url, onloadCallback) {
             // Remember the number of entries on the timeline prior to initiating the fetch:
-            var beforeEntryCount = performance.getEntries().length;
+            var beforeEntryCount = performance.getEntriesByType("resource").length;
 
             return test.step_func(
                 function() {
@@ -480,7 +452,7 @@ window.onload =
                         }
                     }
 
-                    var entries = performance.getEntries();
+                    var entries = performance.getEntriesByType("resource");
                     var candidateEntry = entries[entries.length - 1];
 
                     switch (entries.length - beforeEntryCount)
