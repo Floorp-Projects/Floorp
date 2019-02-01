68db8d2c989331e473dac8bb5b10b529f469b4e7
Dustin J. Mitchell <dustin@mozilla.com>
2019-02-01 23:27:47 +0000

    Bug 1524358 - use ownTaskId, instead of as_slugid('decision'), in .taskcluster.yml r=Callek,tomprince a=lizzard
    This is a forward-looking change to allow evaluation of .taskcluster.yml in
    situations where as_slugid is not defined -- such as in the Taskcluster hooks
    service.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18269
    
    --HG--
    extra : source : 7fb53ad56f8539f82f0d0cee4124a4e4847e796f

--- taskcluster/taskgraph/cron/decision.py
+++ taskcluster/taskgraph/cron/decision.py
@@ -43,15 +43,6 @@ def make_decision_task(params, root, symbol, arguments=[]):
         params['repository_url'],
         params['head_rev'])
 
-    slugids = {}
-
-    def as_slugid(name):
-        # https://github.com/taskcluster/json-e/issues/164
-        name = name[0]
-        if name not in slugids:
-            slugids[name] = slugid.nice()
-        return slugids[name]
-
     # provide a similar JSON-e context to what mozilla-taskcluster provides:
     # https://docs.taskcluster.net/reference/integrations/mozilla-taskcluster/docs/taskcluster-yml
     # but with a different tasks_for and an extra `cron` section
@@ -78,7 +69,7 @@ def make_decision_task(params, root, symbol, arguments=[]):
             'quoted_args': ' '.join(pipes.quote(a) for a in arguments),
         },
         'now': current_json_time(),
-        'as_slugid': as_slugid,
+        'ownTaskId': slugid.nice(),
     }
 
     rendered = jsone.render(taskcluster_yml, context)
