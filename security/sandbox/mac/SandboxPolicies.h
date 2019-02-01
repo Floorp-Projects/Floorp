2cb9537672dc70ac42c80aa1078529205f0145e5
Haik Aftandilian <haftandilian@mozilla.com>
2019-02-01 22:09:24 +0000

    Bug 1506495 - Whitelist /Library and ~/Library ColorSync Profile directories r=Alex_Gaynor a=lizzard
    Whitelist the /Library and ~/Library ColorSync profile directories allowing gfx.color_management.display_profile to be used to load color profiles from those locations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18390

--- security/sandbox/mac/SandboxPolicies.h
+++ security/sandbox/mac/SandboxPolicies.h
@@ -251,6 +251,7 @@ static const char contentSandboxRules[] = R"SANDBOX_LITERAL(
   (allow file-read-data (literal "/Library/Preferences/.GlobalPreferences.plist"))
 
   (allow file-read*
+      (subpath "/Library/ColorSync/Profiles")
       (subpath "/Library/Spelling")
       (literal "/")
       (literal "/private/tmp")
@@ -258,6 +259,7 @@ static const char contentSandboxRules[] = R"SANDBOX_LITERAL(
       (home-literal "/.CFUserTextEncoding")
       (home-literal "/Library/Preferences/com.apple.DownloadAssessment.plist")
       (home-subpath "/Library/Colors")
+      (home-subpath "/Library/ColorSync/Profiles")
       (home-subpath "/Library/Keyboard Layouts")
       (home-subpath "/Library/Input Methods")
       (home-subpath "/Library/Spelling"))
