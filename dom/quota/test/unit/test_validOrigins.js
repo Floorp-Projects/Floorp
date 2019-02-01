15907fc6e1607dfe0b54601cc696ecb41121fe6a
Tom Tung <shes050117@gmail.com>
2019-02-01 13:04:14 +0000

    Bug 1519822 - Support IPv6 URL on OriginParser; r=asuth
    The supporting format for IPv6 URL based on
    https://url.spec.whatwg.org/#host-representation
    
    Differential Revision: https://phabricator.services.mozilla.com/D18213
    
    --HG--
    extra : moz-landing-system : lando

---
+++ dom/quota/test/unit/test_validOrigins.js
@@ -0,0 +1,35 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+// Use initOrigin to test the operation of the origin parser on a list of URLs
+// we should support. If the origin doesn't parse, then initOrigin will throw an
+// exception (and potentially MOZ_ASSERT under debug builds). Handling of
+// obsolete or invalid origins is handled in other test files.
+async function testSteps()
+{
+  const testingURLs = [
+    // General
+    "https://example.com",
+    // About
+    "about:home",
+    // IPv6
+    "https://[::]",
+    "https://[ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff]",
+    "http://[2010:836B:4179::836B:4179]:80",
+    "https://[::FFFF:129.144.52.38]"
+  ]
+
+  for (let testingURL of testingURLs) {
+    info("Testing " + testingURL);
+    try {
+      let request = initOrigin(getPrincipal(testingURL), "default");
+      await requestFinished(request);
+
+      ok(true, "Should not have thrown");
+    } catch(ex) {
+      ok(false, "Should not have thrown");
+    }
+  }
+}
