1cd52b1c57081b7dda878479fad6e3d87f02395c
Masayuki Nakano <masayuki@d-toybox.com>
2019-02-02 02:20:25 +0000

    Bug 1466208 - part 9: Create PresShell::EventHandler::MaybeHandleEventWithAnotherPresShell() to handle event with another PresShell if necessary r=smaug
    Let's move the redirection of coming event in
    PresShell::EventHandler::HandleEvent() into a method.  This makes the caller
    easier to read.
    
    Differential Revision: https://phabricator.services.mozilla.com/D16959
    
    --HG--
    extra : moz-landing-system : lando

--- layout/base/PresShell.h
+++ layout/base/PresShell.h
@@ -616,6 +616,28 @@ class PresShell final : public nsIPresShell,
                                            Document* aRetargetDocument,
                                            nsIFrame* aFrameForPresShell);
 
+    /**
+     * MaybeHandleEventWithAnotherPresShell() may handle aGUIEvent with another
+     * PresShell.
+     *
+     * @param aFrameForPresShell        Set aFrame of HandleEvent() which called
+     *                                  this method.
+     * @param aGUIEvent                 Handling event.
+     * @param aEventStatus              [in/out] EventStatus of aGUIEvent.
+     * @param aRv                       [out] Returns error if this gets an
+     *                                  error handling the event.
+     * @return                          false if caller needs to keep handling
+     *                                  the event by itself.
+     *                                  true if caller shouldn't keep handling
+     *                                  the event.  Note that when no PresShell
+     *                                  can handle the event, this returns true.
+     */
+    MOZ_CAN_RUN_SCRIPT
+    bool MaybeHandleEventWithAnotherPresShell(nsIFrame* aFrameForPresShell,
+                                              WidgetGUIEvent* aGUIEvent,
+                                              nsEventStatus* aEventStatus,
+                                              nsresult* aRv);
+
     MOZ_CAN_RUN_SCRIPT
     nsresult RetargetEventToParent(WidgetGUIEvent* aGUIEvent,
                                    nsEventStatus* aEventStatus);
