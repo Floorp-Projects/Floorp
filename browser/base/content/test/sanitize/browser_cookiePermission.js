4d9b30f2cca4fb4f68103454e33736f7cf7fab30
Andrea Marchesini <amarchesini@mozilla.com>
2019-02-04 14:25:51 +0000

    Bug 1524674 - Cleanup site data with custom permissions per subdomains - Tests. r=johannh a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D18498
    
    --HG--
    extra : source : 7d4ae80c96b29bdd39ae929a27903b9123bb848f

--- browser/base/content/test/sanitize/browser_cookiePermission.js
+++ browser/base/content/test/sanitize/browser_cookiePermission.js
@@ -66,11 +66,12 @@ function checkCookie(host, originAttributes) {
 async function deleteOnShutdown(opt) {
   // Let's clean up all the data.
   await new Promise(resolve => {
-    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value => resolve());
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, resolve);
   });
 
   await SpecialPowers.pushPrefEnv({"set": [
     ["network.cookie.lifetimePolicy", opt.lifetimePolicy],
+    ["browser.sanitizer.loglevel", "All"],
   ]});
 
   // Custom permission without considering OriginAttributes
@@ -275,11 +276,12 @@ add_task(async function deleteStorageInAboutURL() {
 
   // Let's clean up all the data.
   await new Promise(resolve => {
-    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value => resolve());
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, resolve);
   });
 
   await SpecialPowers.pushPrefEnv({"set": [
     ["network.cookie.lifetimePolicy", Ci.nsICookieService.ACCEPT_SESSION],
+    ["browser.sanitizer.loglevel", "All"],
   ]});
 
   // Let's create a tab with some data.
@@ -307,11 +309,12 @@ add_task(async function deleteStorageOnlyCustomPermissionInAboutURL() {
 
   // Let's clean up all the data.
   await new Promise(resolve => {
-    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value => resolve());
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, resolve);
   });
 
   await SpecialPowers.pushPrefEnv({"set": [
     ["network.cookie.lifetimePolicy", Ci.nsICookieService.ACCEPT_NORMALLY],
+    ["browser.sanitizer.loglevel", "All"],
   ]});
 
   // Custom permission without considering OriginAttributes
@@ -338,4 +341,60 @@ add_task(async function deleteStorageOnlyCustomPermissionInAboutURL() {
   });
 
   Services.perms.remove(uri, "cookie");
+});
+
+// 2 domains: www.mozilla.org (session-only) mozilla.org (allowed) - after the
+// cleanp, mozilla.org must have data.
+add_task(async function subDomains() {
+  info("Test subdomains and custom setting");
+
+  // Let's clean up all the data.
+  await new Promise(resolve => {
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, resolve);
+  });
+
+  await SpecialPowers.pushPrefEnv({"set": [
+    ["network.cookie.lifetimePolicy", Ci.nsICookieService.ACCEPT_NORMALLY ],
+    ["browser.sanitizer.loglevel", "All"],
+  ]});
+
+  // Domains and data
+  let uriA = Services.io.newURI("https://www.mozilla.org");
+  Services.perms.add(uriA, "cookie", Ci.nsICookiePermission.ACCESS_SESSION);
+
+  Services.cookies.add(uriA.host, "/test", "a", "b",
+    false, false, false, Date.now() + 24000 * 60 * 60, {},
+    Ci.nsICookie2.SAMESITE_UNSET);
+
+  await createIndexedDB(uriA.host, {});
+
+  let uriB = Services.io.newURI("https://mozilla.org");
+  Services.perms.add(uriB, "cookie", Ci.nsICookiePermission.ACCESS_ALLOW);
+
+  Services.cookies.add(uriB.host, "/test", "c", "d",
+    false, false, false, Date.now() + 24000 * 60 * 60, {},
+    Ci.nsICookie2.SAMESITE_UNSET);
+
+  await createIndexedDB(uriB.host, {});
+
+  // Check
+  ok(await checkCookie(uriA.host, {}), "We have cookies for URI: " + uriA.host);
+  ok(await checkIndexedDB(uriA.host, {}), "We have IDB for URI: " + uriA.host);
+  ok(await checkCookie(uriB.host, {}), "We have cookies for URI: " + uriB.host);
+  ok(await checkIndexedDB(uriB.host, {}), "We have IDB for URI: " + uriB.host);
+
+  // Cleaning up
+  await Sanitizer.runSanitizeOnShutdown();
+
+  // Check again
+  ok(!(await checkCookie(uriA.host, {})), "We should not have cookies for URI: " + uriA.host);
+  ok(!(await checkIndexedDB(uriA.host, {})), "We should not have IDB for URI: " + uriA.host);
+
+  // Note that cookies are stored per base domain...
+  ok(!(await checkCookie(uriB.host, {})), "We should not have cookies for URI: " + uriB.host);
+  ok(await checkIndexedDB(uriB.host, {}), "We should have IDB for URI: " + uriB.host);
+
+  // Cleaning up permissions
+  Services.perms.remove(uriA, "cookie");
+  Services.perms.remove(uriB, "cookie");
 });
