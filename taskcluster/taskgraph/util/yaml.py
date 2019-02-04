d2877004a8102d9fe9ba3adc767631e7c747b1c6
Tom Prince <mozilla@hocat.ca>
2019-02-04 22:18:57 +0000

    Bug 1524639: [taskgraph] Force yaml to be loaded as unicode; r=dustin
    Differential Revision: https://phabricator.services.mozilla.com/D18375

--- taskcluster/taskgraph/util/yaml.py
+++ taskcluster/taskgraph/util/yaml.py
@@ -5,7 +5,30 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import os
-import yaml
+
+from yaml.loader import SafeLoader
+
+
+class UnicodeLoader(SafeLoader):
+    def construct_yaml_str(self, node):
+        return self.construct_scalar(node)
+
+
+UnicodeLoader.add_constructor(
+    'tag:yaml.org,2002:str',
+    UnicodeLoader.construct_yaml_str)
+
+
+def load_stream(stream):
+    """
+    Parse the first YAML document in a stream
+    and produce the corresponding Python object.
+    """
+    loader = UnicodeLoader(stream)
+    try:
+        return loader.get_single_data()
+    finally:
+        loader.dispose()
 
 
 def load_yaml(*parts):
@@ -13,4 +36,4 @@ def load_yaml(*parts):
     useful for loading kind configuration files from the kind path."""
     filename = os.path.join(*parts)
     with open(filename, "rb") as f:
-        return yaml.safe_load(f)
+        return load_stream(f)
