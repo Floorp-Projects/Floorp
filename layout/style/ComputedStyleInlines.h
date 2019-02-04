3fe47839c0a4cddbaff605f88b1f2588fafc2260
Emilio Cobos Álvarez <emilio@crisal.io>
2019-02-04 18:57:50 +0100

    Bug 1525134 - Move image loads out of the style struct accessors. r=heycam
    After this I can pass the document from the caller to ResolveSameStructsAs, and
    get rid of the pres context pointer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18600

--- layout/style/ComputedStyleInlines.h
+++ layout/style/ComputedStyleInlines.h
@@ -21,67 +21,17 @@
 
 namespace mozilla {
 
-#define STYLE_STRUCT(name_)                                       \
-  const nsStyle##name_* ComputedStyle::Style##name_() {           \
-    return DoGetStyle##name_<true>();                             \
-  }                                                               \
-  const nsStyle##name_* ComputedStyle::ThreadsafeStyle##name_() { \
-    if (mozilla::IsInServoTraversal()) {                          \
-      return ComputedData()->GetStyle##name_();                   \
-    }                                                             \
-    return Style##name_();                                        \
-  }                                                               \
-  const nsStyle##name_* ComputedStyle::PeekStyle##name_() {       \
-    return DoGetStyle##name_<false>();                            \
+void ComputedStyle::StartImageLoads(dom::Document& aDocument,
+                                    const ComputedStyle* aOldStyle) {
+  MOZ_ASSERT(NS_IsMainThread());
+#define STYLE_STRUCT(name_)                                      \
+  if (nsStyle##name_::kHasTriggerImageLoads) {                   \
+    auto* old = aOldStyle ? aOldStyle->Style##name_() : nullptr; \
+    auto* current = const_cast<nsStyle##name_*>(Style##name_()); \
+    current->TriggerImageLoads(aDocument, old);                  \
   }
 #include "nsStyleStructList.h"
 #undef STYLE_STRUCT
-
-// Helper functions for GetStyle* and PeekStyle*
-#define STYLE_STRUCT(name_)                                         \
-  template <bool aComputeData>                                      \
-  const nsStyle##name_* ComputedStyle::DoGetStyle##name_() {        \
-    const auto kStructID = StyleStructID::name_;                    \
-    const bool needToCompute = !HasRequestedStruct(kStructID);      \
-    if (!aComputeData && needToCompute) {                           \
-      return nullptr;                                               \
-    }                                                               \
-    const nsStyle##name_* data = ComputedData()->GetStyle##name_(); \
-    /* perform any remaining main thread work on the struct */      \
-    if (needToCompute) {                                            \
-      MOZ_ASSERT(NS_IsMainThread());                                \
-      MOZ_ASSERT(!mozilla::IsInServoTraversal());                   \
-      const_cast<nsStyle##name_*>(data)->TriggerImageLoads(         \
-          *mPresContext->Document(), nullptr);                      \
-      /* the ComputedStyle owns the struct */                       \
-      SetRequestedStruct(kStructID);                                \
-    }                                                               \
-    return data;                                                    \
-  }
-#include "nsStyleStructList.h"
-#undef STYLE_STRUCT
-
-void ComputedStyle::StartBackgroundImageLoads() {
-  // Just get our background struct; that should do the trick
-  StyleBackground();
-}
-
-void ComputedStyle::ResolveSameStructsAs(const ComputedStyle* aOther) {
-  // Only resolve structs that are not already resolved in this struct.
-  auto newBits = aOther->mRequestedStructs & ~mRequestedStructs;
-
-#define STYLE_STRUCT(name_)                                                    \
-  if (nsStyle##name_::kHasTriggerImageLoads &&                                 \
-      (newBits & StyleStructConstants::BitFor(StyleStructID::name_))) {        \
-    const nsStyle##name_* data = ComputedData()->GetStyle##name_();            \
-    const nsStyle##name_* oldData = aOther->ComputedData()->GetStyle##name_(); \
-    const_cast<nsStyle##name_*>(data)->TriggerImageLoads(                      \
-        *mPresContext->Document(), oldData);                                   \
-  }
-#include "nsStyleStructList.h"
-#undef STYLE_STRUCT
-
-  mRequestedStructs |= newBits;
 }
 
 }  // namespace mozilla
