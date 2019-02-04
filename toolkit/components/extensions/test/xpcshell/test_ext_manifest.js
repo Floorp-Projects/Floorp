ff166974f0f79ee11582adb55160d523cbd1e1fb
Bogdan Tara <btara@mozilla.com>
2019-02-04 19:09:13 +0200

    Backed out changeset 2e087a053898 (bug 1524001) for test_ext_management.js failures CLOSED TREE

--- toolkit/components/extensions/test/xpcshell/test_ext_manifest.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_manifest.js
@@ -2,42 +2,48 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-async function testIconPaths(icon, manifest, expectedError) {
-  let normalized = await ExtensionTestUtils.normalizeManifest(manifest);
-
-  if (expectedError) {
-    ok(expectedError.test(normalized.error),
-       `Should have an error for ${JSON.stringify(icon)}`);
-  } else {
-    ok(!normalized.error, `Should not have an error ${JSON.stringify(icon)}`);
-  }
-}
-
 add_task(async function test_manifest() {
   let badpaths = ["", " ", "\t", "http://foo.com/icon.png"];
   for (let path of badpaths) {
-    await testIconPaths(path, {
-      "icons": path,
-    }, /Error processing icons/);
-
-    await testIconPaths(path, {
-      "icons": {
-        "16": path,
+    let normalized = await ExtensionTestUtils.normalizeManifest({
+      "browser_action": {
+        "default_icon": path,
       },
-    }, /Error processing icons/);
+    });
+
+    ok(/Error processing browser_action.default_icon/.test(normalized.error),
+       `Should have an error for ${JSON.stringify(path)}`);
+
+    normalized = await ExtensionTestUtils.normalizeManifest({
+      "browser_action": {
+        "default_icon": {
+          "16": path,
+        },
+      },
+    });
+
+    ok(/Error processing browser_action.default_icon/.test(normalized.error),
+       `Should have an error for ${JSON.stringify(path)}`);
   }
 
   let paths = ["icon.png", "/icon.png", "./icon.png", "path to an icon.png", " icon.png"];
   for (let path of paths) {
-    // manifest.icons is an object
-    await testIconPaths(path, {
-      "icons": path,
-    }, /Error processing icons/);
-
-    await testIconPaths(path, {
-      "icons": {
-        "16": path,
+    let normalized = await ExtensionTestUtils.normalizeManifest({
+      "browser_action": {
+        "default_icon": {
+          "16": path,
+        },
       },
     });
+
+    ok(!normalized.error, `Should not have an error ${JSON.stringify(path)}`);
+
+    normalized = await ExtensionTestUtils.normalizeManifest({
+      "browser_action": {
+        "default_icon": path,
+      },
+    });
+
+    ok(!normalized.error, `Should not have an error ${JSON.stringify(path)}`);
   }
 });
