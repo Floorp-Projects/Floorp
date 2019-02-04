0925e07162336029e03c1eea648b44073781cb58
Ehsan Akhgari <ehsan@mozilla.com>
2019-02-04 15:44:03 -0500

    Bug 1525208 - Part 6: Remove some needless AddRef/Release calls in the loop inside ThirdPartyUtil::IsThirdPartyWindow(); r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D18648

--- dom/base/ThirdPartyUtil.cpp
+++ dom/base/ThirdPartyUtil.cpp
@@ -154,13 +154,11 @@ ThirdPartyUtil::IsThirdPartyWindow(mozIDOMWindowProxy* aWindow, nsIURI* aURI,
     }
   }
 
-  nsCOMPtr<nsPIDOMWindowOuter> current = nsPIDOMWindowOuter::From(aWindow),
-                               parent;
-  nsCOMPtr<nsIURI> parentURI;
+  nsPIDOMWindowOuter* current = nsPIDOMWindowOuter::From(aWindow);
   do {
     // We use GetScriptableParent rather than GetParent because we consider
     // <iframe mozbrowser> to be a top-level frame.
-    parent = current->GetScriptableParent();
+    nsPIDOMWindowOuter* parent = current->GetScriptableParent();
     // We don't use SameCOMIdentity here since we know that nsPIDOMWindowOuter
     // is only implemented by nsGlobalWindowOuter, so different objects of that
     // type will not have different nsISupports COM identities, and checking the
@@ -172,6 +170,7 @@ ThirdPartyUtil::IsThirdPartyWindow(mozIDOMWindowProxy* aWindow, nsIURI* aURI,
       return NS_OK;
     }
 
+    nsCOMPtr<nsIURI> parentURI;
     rv = GetURIFromWindow(parent, getter_AddRefs(parentURI));
     NS_ENSURE_SUCCESS(rv, rv);
 
@@ -184,7 +183,6 @@ ThirdPartyUtil::IsThirdPartyWindow(mozIDOMWindowProxy* aWindow, nsIURI* aURI,
     }
 
     current = parent;
-    currentURI = parentURI;
   } while (1);
 
   MOZ_ASSERT_UNREACHABLE("should've returned");
