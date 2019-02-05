b2f1744a536ce35e02e7fe366aaa8cde869c1c79
Tom Prince <mozilla@hocat.ca>
2019-02-05 23:21:39 +0000

    Bug 1524639: [taksgraph] Simplify dependency creation in single_dep transforms; r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D18747

--- taskcluster/taskgraph/transforms/beetmover_geckoview.py
+++ taskcluster/taskgraph/transforms/beetmover_geckoview.py
@@ -96,8 +96,7 @@ def make_task_description(config, jobs):
             )
         )
 
-        dependent_kind = str(dep_job.kind)
-        dependencies = {dependent_kind: dep_job.label}
+        dependencies = {dep_job.kind: dep_job.label}
 
         attributes = copy_attributes_from_dependent_job(dep_job)
         attributes.update(job.get('attributes', {}))
