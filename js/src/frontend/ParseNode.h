f8a9aa54305d9f0e7b298058eadf28c9c6eca7c4
Tooru Fujisawa <arai_a@mac.com>
2019-02-05 14:57:47 +0000

    Bug 1518391 - Part 1: Remove unused case/code from function node ops. r=jorendorff
    pn_op for CodeNode can only be JSOP_NOP, JSOP_LAMBDA, and JSOP_LAMBDA_ARROW.
    Removed other cases.
    Also removed the override with JSOP_FUNWITHPROTO given the field is never read
    after the function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18325

--- js/src/frontend/ParseNode.h
+++ js/src/frontend/ParseNode.h
@@ -1514,15 +1514,8 @@ class CodeNode : public ParseNode {
 
   bool functionIsHoisted() const {
     MOZ_ASSERT(isKind(ParseNodeKind::Function));
-    MOZ_ASSERT(
-        isOp(JSOP_LAMBDA) ||        // lambda
-        isOp(JSOP_LAMBDA_ARROW) ||  // arrow function
-        isOp(JSOP_DEFFUN) ||        // non-body-level function statement
-        isOp(JSOP_NOP) ||           // body-level function stmt in global code
-        isOp(JSOP_GETLOCAL) ||      // body-level function stmt in function code
-        isOp(JSOP_GETARG) ||        // body-level function redeclaring formal
-        isOp(JSOP_INITLEXICAL));    // block-level function stmt
-    return !isOp(JSOP_LAMBDA) && !isOp(JSOP_LAMBDA_ARROW) && !isOp(JSOP_DEFFUN);
+    MOZ_ASSERT(isOp(JSOP_LAMBDA) || isOp(JSOP_LAMBDA_ARROW) || isOp(JSOP_NOP));
+    return isOp(JSOP_NOP);
   }
 };
 
