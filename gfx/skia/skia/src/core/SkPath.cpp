15ee3de028d09857ef10c728935112b44b8b7618
Lee Salzman <lsalzman@mozilla.com>
2019-02-05 17:39:51 -0500

    Bug 1525433 - Check path convexity after transforms. r=rhunt, a=RyanVM
    --HG--
    extra : source : f8e19c32d3dd480c166cef39b24ed352b986195e
    extra : histedit_source : cb4a640cd89d1789b9f40fd68938202ac2c24feb

--- gfx/skia/skia/src/core/SkPath.cpp
+++ gfx/skia/skia/src/core/SkPath.cpp
@@ -1786,6 +1786,13 @@ static void subdivide_cubic_to(SkPath* path, const SkPoint pts[4],
 }
 
 void SkPath::transform(const SkMatrix& matrix, SkPath* dst) const {
+    if (matrix.isIdentity()) {
+        if (dst != nullptr && dst != this) {
+            *dst = *this;
+        }
+        return;
+    }
+
     SkDEBUGCODE(this->validate();)
     if (dst == nullptr) {
         dst = (SkPath*)this;
@@ -1833,13 +1840,20 @@ void SkPath::transform(const SkMatrix& matrix, SkPath* dst) const {
         matrix.mapPoints(ed.points(), ed.pathRef()->countPoints());
         dst->fFirstDirection = SkPathPriv::kUnknown_FirstDirection;
     } else {
+        Convexity convexity = fConvexity;
+
         SkPathRef::CreateTransformedCopy(&dst->fPathRef, *fPathRef.get(), matrix);
 
         if (this != dst) {
             dst->fLastMoveToIndex = fLastMoveToIndex;
             dst->fFillType = fFillType;
-            dst->fConvexity.store(fConvexity);
             dst->fIsVolatile = fIsVolatile;
+        }
+
+        if (matrix.isScaleTranslate() && SkPathPriv::IsAxisAligned(*this)) {
+            dst->fConvexity = convexity;
+        } else {
+            dst->fConvexity = kUnknown_Convexity;
         }
 
         if (SkPathPriv::kUnknown_FirstDirection == fFirstDirection) {
@@ -1854,7 +1868,6 @@ void SkPath::transform(const SkMatrix& matrix, SkPath* dst) const {
             } else if (det2x2 > 0) {
                 dst->fFirstDirection = fFirstDirection.load();
             } else {
-                dst->fConvexity = kUnknown_Convexity;
                 dst->fFirstDirection = SkPathPriv::kUnknown_FirstDirection;
             }
         }
