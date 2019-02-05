3362171b4b9dfba9db99e0a04ece1c0818106cdf
Nathan Froyd <froydnj@mozilla.com>
2019-02-05 16:15:58 -0500

    Bug 1486994 - avoid automatic installs of taskcluster packages on non-x86-64 machines; r=lth
    Presumably, if somebody is working on a non-x86-64 Linux box, they know
    enough to install all of these packages manually.

--- python/mozboot/mozboot/linux_common.py
+++ python/mozboot/mozboot/linux_common.py
@@ -6,7 +6,13 @@
 # needed to install Stylo and Node dependencies.  This class must come before
 # BaseBootstrapper in the inheritance list.
 
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
+
+import os
+
+
+def is_non_x86_64():
+    return os.uname()[4] != 'x86_64'
 
 
 class StyloInstall(object):
@@ -15,6 +21,12 @@ class StyloInstall(object):
 
     def ensure_stylo_packages(self, state_dir, checkout_root):
         from mozboot import stylo
+
+        if is_non_x86_64():
+            print('Cannot install bindgen clang and cbindgen packages from taskcluster.\n'
+                  'Please install these packages manually.')
+            return
+
         self.install_toolchain_artifact(state_dir, checkout_root, stylo.LINUX_CLANG)
         self.install_toolchain_artifact(state_dir, checkout_root, stylo.LINUX_CBINDGEN)
 
@@ -24,6 +36,11 @@ class NodeInstall(object):
         pass
 
     def ensure_node_packages(self, state_dir, checkout_root):
+        if is_non_x86_64():
+            print('Cannot install node package from taskcluster.\n'
+                  'Please install this package manually.')
+            return
+
         from mozboot import node
         self.install_toolchain_artifact(state_dir, checkout_root, node.LINUX)
 
@@ -33,4 +50,9 @@ class ClangStaticAnalysisInstall(object):
         pass
 
     def ensure_clang_static_analysis_package(self, checkout_root):
+        if is_non_x86_64():
+            print('Cannot install static analysis tools from taskcluster.\n'
+                  'Please install these tools manually.')
+            return
+
         self.install_toolchain_static_analysis(checkout_root)
