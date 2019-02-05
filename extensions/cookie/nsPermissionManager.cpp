e8a32a4d8e00dd1cd9f7a45ce5b76ab207d048d4
Cosmin Sabou <csabou@mozilla.com>
2019-02-05 21:04:45 +0200

    Backed out 8 changesets (bug 1525208) for assertion mass failures on dom/base/ThirdPartyUtil. CLOSED TREE
    Backed out changeset 7a679beef53e (bug 1525208)
    Backed out changeset 4a97d6a5440f (bug 1525208)
    Backed out changeset 40cdbb0f3e33 (bug 1525208)
    Backed out changeset 461bf050580c (bug 1525208)
    Backed out changeset 529fc4945c00 (bug 1525208)
    Backed out changeset 80d33523be6e (bug 1525208)
    Backed out changeset 6afabd86b213 (bug 1525208)
    Backed out changeset bc4389577ba8 (bug 1525208)

--- extensions/cookie/nsPermissionManager.cpp
+++ extensions/cookie/nsPermissionManager.cpp
@@ -47,10 +47,8 @@
 #include "nsPrintfCString.h"
 #include "mozilla/AbstractThread.h"
 #include "ExpandedPrincipal.h"
-#include "mozilla/StaticPtr.h"
-#include "mozilla/ClearOnShutdown.h"
 
-static mozilla::StaticRefPtr<nsPermissionManager> gPermissionManager;
+static nsPermissionManager* gPermissionManager = nullptr;
 
 using namespace mozilla;
 using namespace mozilla::dom;
@@ -980,21 +978,10 @@ nsPermissionManager::GetXPCOMSingleton() {
   if (NS_SUCCEEDED(permManager->Init())) {
     // Note: This is cleared in the nsPermissionManager destructor.
     gPermissionManager = permManager.get();
-    ClearOnShutdown(&gPermissionManager);
     return permManager.forget();
   }
 
   return nullptr;
-}
-
-// static
-nsPermissionManager* nsPermissionManager::GetInstance() {
-  if (!gPermissionManager) {
-    // Hand off the creation of the permission manager to GetXPCOMSingleton.
-    nsCOMPtr<nsIPermissionManager> permManager = GetXPCOMSingleton();
-  }
-
-  return gPermissionManager;
 }
 
 nsresult nsPermissionManager::Init() {
