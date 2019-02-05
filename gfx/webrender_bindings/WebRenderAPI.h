9e135c5cd114a3f30aa96d8e222ea7e184f01ac5
Lee Salzman <lsalzman@mozilla.com>
2019-02-05 16:29:11 -0500

    Bug 1524509 - track whether a WR stacking context is animated separately from whether it should rasterize locally. r=jrmuizel

--- gfx/webrender_bindings/WebRenderAPI.h
+++ gfx/webrender_bindings/WebRenderAPI.h
@@ -333,7 +333,13 @@ struct MOZ_STACK_CLASS StackingContextParams : public WrStackingContextParams {
   const gfx::Matrix4x4* mBoundTransform = nullptr;
   const gfx::Matrix4x4* mTransformPtr = nullptr;
   Maybe<nsDisplayTransform*> mDeferredTransformItem;
+  // Whether the stacking context is possibly animated. This alters how coordinates
+  // are transformed/snapped to invalidate less when transforms change frequently.
   bool mAnimated = false;
+  // Whether items should be rasterized in a local space that is (mostly) invariant
+  // to transforms, i.e. disabling subpixel AA and screen space pixel snapping on
+  // text runs that would only make sense in screen space.
+  bool mRasterizeLocally = false;
 };
 
 /// This is a simple C++ wrapper around WrState defined in the rust bindings.
