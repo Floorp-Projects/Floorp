135dc8f643767bd05f1c27989de86617d0d5066b
Dave Townsend <dtownsend@oxymoronical.com>
2019-02-06 14:18:35 -0800

    Bug 1518639: Implement windows remoting server and client. r=jimm
    Implements the windows remove client and server based on the current remoting
    code in nsNativeAppSupportWin.cpp. Makes the hidden window classname encode both
    program name and profile name. nsNativeAppSupportWin is now just used for
    setting up the console.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19076

--- toolkit/components/remote/nsWinRemoteClient.cpp
+++ toolkit/components/remote/nsWinRemoteClient.cpp
@@ -6,6 +6,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsWinRemoteClient.h"
+#include "nsWinRemoteUtils.h"
+
+#include <windows.h>
+
+using namespace mozilla;
 
 nsresult nsWinRemoteClient::Init() {
   return NS_OK;
@@ -16,5 +21,37 @@ nsresult nsWinRemoteClient::SendCommandLine(const char *aProgram, const char *aP
                                             const char *aDesktopStartupID,
                                             char **aResponse, bool *aSucceeded) {
   *aSucceeded = false;
+
+  nsString className;
+  BuildClassName(aProgram, aProfile, className);
+
+  HWND handle = ::FindWindowW(className.get(), 0);
+
+  if (!handle) {
+    return NS_OK;
+  }
+
+  WCHAR *cmd = ::GetCommandLineW();
+  WCHAR cwd[MAX_PATH];
+  _wgetcwd(cwd, MAX_PATH);
+
+  // Construct a narrow UTF8 buffer <commandline>\0<workingdir>\0
+  NS_ConvertUTF16toUTF8 utf8buffer(cmd);
+  utf8buffer.Append('\0');
+  WCHAR *cwdPtr = cwd;
+  AppendUTF16toUTF8(MakeStringSpan(reinterpret_cast<char16_t *>(cwdPtr)),
+                    utf8buffer);
+  utf8buffer.Append('\0');
+
+  // We used to set dwData to zero, when we didn't send the working dir.
+  // Now we're using it as a version number.
+  COPYDATASTRUCT cds = {1, utf8buffer.Length(), (void *)utf8buffer.get()};
+  // Bring the already running Mozilla process to the foreground.
+  // nsWindow will restore the window (if minimized) and raise it.
+  ::SetForegroundWindow(handle);
+  ::SendMessage(handle, WM_COPYDATA, 0, (LPARAM)&cds);
+
+  *aSucceeded = true;
+
   return NS_OK;
 }
