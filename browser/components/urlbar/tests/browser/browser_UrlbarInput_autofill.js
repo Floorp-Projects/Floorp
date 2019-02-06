7eff442b15804928cdae93774023c43d1dfbab1b
Narcis Beleuzu <nbeleuzu@mozilla.com>
2019-02-06 19:42:16 +0200

    Backed out changeset f89acdbfa432 (bug 1524718) for bc failures on browser_urlbar_locationchange_urlbar_edit_dos.js. CLOSED TREE

--- browser/components/urlbar/tests/browser/browser_UrlbarInput_autofill.js
+++ browser/components/urlbar/tests/browser/browser_UrlbarInput_autofill.js
@@ -2,24 +2,34 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 /**
- * Tests the autofill functionality of UrlbarInput.
+ * Tests the UrlbarInput.autofill() method.  It needs to be tested in the actual
+ * browser because UrlbarInput relies on the trimURL() function defined in the
+ * browser window's global scope, and this test hits the path that calls it.
  */
 
 "use strict";
 
 add_task(async function test() {
-  gURLBar.setValueFromResult({
-    autofill: {
-      value: "foobar",
-      selectionStart: "foo".length,
-      selectionEnd: "foobar".length,
-    },
-    type: UrlbarUtils.RESULT_TYPE.URL,
-  });
-  Assert.equal(gURLBar.value, "foobar",
-    "The input value should be correct");
-  Assert.equal(gURLBar.selectionStart, "foo".length,
-    "The start of the selection should be correct");
-  Assert.equal(gURLBar.selectionEnd, "foobar".length,
-    "The end of the selection should be correct");
+  let tests = [
+    // [initial value, autofill value, expected selected substring in new value]
+    ["foo", "foobar", "bar"],
+    ["FOO", "foobar", "bar"],
+    ["fOo", "foobar", "bar"],
+    ["foo", "quuxbar", ""],
+    ["FOO", "quuxbar", ""],
+    ["fOo", "quuxbar", ""],
+  ];
+
+  gURLBar.focus();
+  for (let [initial, autofill, expectedSelected] of tests) {
+    gURLBar.value = initial;
+    gURLBar.autofill(autofill);
+    let expectedValue = initial + expectedSelected;
+    Assert.equal(gURLBar.value, expectedValue,
+      "The input value should be correct");
+    Assert.equal(gURLBar.selectionStart, initial.length,
+      "The start of the selection should be correct");
+    Assert.equal(gURLBar.selectionEnd, expectedValue.length,
+      "The end of the selection should be correct");
+  }
 });
