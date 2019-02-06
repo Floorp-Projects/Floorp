51a70c87efc2b2ea512c81e7104fd777228a16f4
Kris Maglione <maglione.k@gmail.com>
2019-02-05 21:25:21 -0800

    Bug 1525511: Part 5 - Get rid of most LWTManager gunk in tests. r=aswan
    Differential Revision: https://phabricator.services.mozilla.com/D18788

--- toolkit/mozapps/extensions/test/browser/browser_bug591465.js
+++ toolkit/mozapps/extensions/test/browser/browser_bug591465.js
@@ -5,25 +5,9 @@
 // Bug 591465 - Context menu of add-ons miss context related state change entries
 
 
-var tempScope = {};
-ChromeUtils.import("resource://gre/modules/LightweightThemeManager.jsm", tempScope);
-var LightweightThemeManager = tempScope.LightweightThemeManager;
-
-
 var gManagerWindow;
 var gProvider;
 var gContextMenu;
-var gLWTheme = {
-                id: "4",
-                version: "1",
-                name: "Bling",
-                description: "SO MUCH BLING!",
-                author: "Pixel Pusher",
-                homepageURL: "http://mochi.test:8888/data/index.html",
-                headerURL: "http://mochi.test:8888/data/header.png",
-                previewURL: "http://mochi.test:8888/data/preview.png",
-                iconURL: "http://mochi.test:8888/data/icon.png",
-              };
 
 
 add_task(async function setup() {
@@ -217,85 +201,6 @@ add_test(function() {
 
   info("Opening context menu on disabled theme item");
   el.parentNode.ensureElementIsVisible(el);
-  EventUtils.synthesizeMouse(el, 4, 4, { }, gManagerWindow);
-  EventUtils.synthesizeMouse(el, 4, 4, { type: "contextmenu", button: 2 }, gManagerWindow);
-});
-
-
-add_test(function() {
-  LightweightThemeManager.currentTheme = gLWTheme;
-
-  var el = get_addon_element(gManagerWindow, "4@personas.mozilla.org");
-
-  gContextMenu.addEventListener("popupshown", function() {
-    check_contextmenu(true, true, false, false, false);
-
-    gContextMenu.hidePopup();
-    run_next_test();
-  }, {once: true});
-
-  info("Opening context menu on enabled LW theme item");
-  el.parentNode.ensureElementIsVisible(el);
-  EventUtils.synthesizeMouse(el, 4, 4, { }, gManagerWindow);
-  EventUtils.synthesizeMouse(el, 4, 4, { type: "contextmenu", button: 2 }, gManagerWindow);
-});
-
-
-add_test(function() {
-  LightweightThemeManager.currentTheme = null;
-
-  var el = get_addon_element(gManagerWindow, "4@personas.mozilla.org");
-
-  gContextMenu.addEventListener("popupshown", function() {
-    check_contextmenu(true, false, false, false, false);
-
-    gContextMenu.hidePopup();
-    run_next_test();
-  }, {once: true});
-
-  info("Opening context menu on disabled LW theme item");
-  el.parentNode.ensureElementIsVisible(el);
-  EventUtils.synthesizeMouse(el, 4, 4, { }, gManagerWindow);
-  EventUtils.synthesizeMouse(el, 4, 4, { type: "contextmenu", button: 2 }, gManagerWindow);
-});
-
-
-add_test(async function() {
-  LightweightThemeManager.currentTheme = gLWTheme;
-
-  gManagerWindow.loadView("addons://detail/4@personas.mozilla.org");
-  await wait_for_view_load(gManagerWindow);
-  gContextMenu.addEventListener("popupshown", function() {
-    check_contextmenu(true, true, false, true, false);
-
-    gContextMenu.hidePopup();
-    run_next_test();
-  }, {once: true});
-
-  info("Opening context menu on enabled LW theme, in detail view");
-  var el = gManagerWindow.document.querySelector("#detail-view .detail-view-container");
-  EventUtils.synthesizeMouse(el, 4, 4, { }, gManagerWindow);
-  EventUtils.synthesizeMouse(el, 4, 4, { type: "contextmenu", button: 2 }, gManagerWindow);
-});
-
-
-add_test(async function() {
-  LightweightThemeManager.currentTheme = null;
-
-  gManagerWindow.loadView("addons://detail/4@personas.mozilla.org");
-  await wait_for_view_load(gManagerWindow);
-  gContextMenu.addEventListener("popupshown", async function() {
-    check_contextmenu(true, false, false, true, false);
-
-    gContextMenu.hidePopup();
-
-    let aAddon = await AddonManager.getAddonByID("4@personas.mozilla.org");
-    aAddon.uninstall();
-    run_next_test();
-  }, {once: true});
-
-  info("Opening context menu on disabled LW theme, in detail view");
-  var el = gManagerWindow.document.querySelector("#detail-view .detail-view-container");
   EventUtils.synthesizeMouse(el, 4, 4, { }, gManagerWindow);
   EventUtils.synthesizeMouse(el, 4, 4, { type: "contextmenu", button: 2 }, gManagerWindow);
 });
