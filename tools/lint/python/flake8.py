891456497ac73b82454776858ce8850909bfda7c
Andrew Halberstadt <ahalberstadt@mozilla.com>
2019-02-07 20:17:02 +0000

    Bug 1515746 - [flake8] Unsupport subdir .flake8 files and use new 'per-file-ignores' config instead, r=egao
    This removes all .flake8 files except for the one at the root of the repo.
    Instead we use the new 'per-file-ignores' config introduced in 3.7. To ignore
    specific errors in a subdirectory, add a line like this to the root .flake8:
    
    [per-file-ignores]
        path/to/subdir/*: E100, F200, ...
    
    The reasons for this change are:
    
    1. Unblock flake8 blacklist (bug 1367092).
    2. Simplify configuration and code.
    3. Encourage more consistent styling.
    4. Improve performance.
    5. Greater editor consistency.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18354

--- tools/lint/python/flake8.py
+++ tools/lint/python/flake8.py
@@ -8,13 +8,11 @@ import platform
 import signal
 import subprocess
 import sys
-from collections import defaultdict
 
 from mozprocess import ProcessHandlerMixin
 
 from mozlint import result
 from mozlint.util import pip
-from mozlint.pathutils import get_ancestors_by_name
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -95,16 +93,6 @@ class Flake8Process(ProcessHandlerMixin):
         signal.signal(signal.SIGINT, orig)
 
 
-def run_process(config, cmd):
-    proc = Flake8Process(config, cmd)
-    proc.run()
-    try:
-        proc.wait()
-    except KeyboardInterrupt:
-        proc.kill()
-        return 1
-
-
 def setup(root):
     if not pip.reinstall_program(FLAKE8_REQUIREMENTS_PATH):
         print(FLAKE8_INSTALL_ERROR)
@@ -116,42 +104,33 @@ def lint(paths, config, **lintargs):
     global results
     results = []
 
+    config_path = os.path.join(lintargs['root'], '.flake8')
     cmdargs = [
         os.path.join(bindir, 'flake8'),
+        '--config', config_path,
         '--format', '{"path":"%(path)s","lineno":%(row)s,'
                     '"column":%(col)s,"rule":"%(code)s","message":"%(text)s"}',
         '--filename', ','.join(['*.{}'.format(e) for e in config['extensions']]),
     ]
 
-    fix_cmdargs = [
-        os.path.join(bindir, 'autopep8'),
-        '--global-config', os.path.join(lintargs['root'], '.flake8'),
-        '--in-place', '--recursive',
-    ]
+    if lintargs.get('fix'):
+        fix_cmdargs = [
+            os.path.join(bindir, 'autopep8'),
+            '--global-config', config_path,
+            '--in-place', '--recursive',
+        ]
 
-    if config.get('exclude'):
-        fix_cmdargs.extend(['--exclude', ','.join(config['exclude'])])
+        if config.get('exclude'):
+            fix_cmdargs.extend(['--exclude', ','.join(config['exclude'])])
 
-    # Run any paths with a .flake8 file in the directory separately so
-    # it gets picked up. This means only .flake8 files that live in
-    # directories that are explicitly included will be considered.
-    # See bug 1277851
-    paths_by_config = defaultdict(list)
-    for path in paths:
-        configs = get_ancestors_by_name('.flake8', path, lintargs['root'])
-        paths_by_config[os.pathsep.join(configs) if configs else 'default'].append(path)
+        subprocess.call(fix_cmdargs + paths)
 
-    for configs, paths in paths_by_config.items():
-        if lintargs.get('fix'):
-            subprocess.call(fix_cmdargs + paths)
-
-        cmd = cmdargs[:]
-        if configs != 'default':
-            configs = reversed(configs.split(os.pathsep))
-            cmd.extend(['--append-config={}'.format(c) for c in configs])
-
-        cmd.extend(paths)
-        if run_process(config, cmd):
-            break
+    proc = Flake8Process(config, cmdargs + paths)
+    proc.run()
+    try:
+        proc.wait()
+    except KeyboardInterrupt:
+        proc.kill()
+        return 1
 
     return results
