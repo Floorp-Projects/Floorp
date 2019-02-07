23ba40496ca4c919d6df017d5496ac049a9d034d
Bogdan Tara <btara@mozilla.com>
2019-02-07 03:22:07 +0200

    Backed out changeset 41a84186eb77 (bug 1445739) for test_startup.js failures CLOSED TREE

--- browser/extensions/formautofill/background.js
+++ browser/extensions/formautofill/background.js
@@ -5,7 +5,4 @@
 browser.runtime.onUpdateAvailable.addListener(details => {
   // By listening to but ignoring this event, any updates will
   // be delayed until the next browser restart.
-  // Note that if we ever wanted to change this, we should make
-  // sure we manually invalidate the startup cache using the
-  // startupcache-invalidate notification.
 });
