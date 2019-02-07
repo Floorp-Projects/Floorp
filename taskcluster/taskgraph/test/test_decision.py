31fc46e43f8097b6f1131daa7f4a4b2a21f6bfbf
Dustin J. Mitchell <dustin@mozilla.com>
2019-02-07 02:10:42 +0000

    Bug 1525421 - parse out just the try syntax from the commit message r=tomprince
    This mirrors what mozilla-taskcluster does in
    https://github.com/taskcluster/mozilla-taskcluster/blob/cb3de4c31e418f7dc1d89cffeeb28ad6a9dae610/src/jobs/taskcluster_graph.js#L46-L58
    
    Differential Revision: https://phabricator.services.mozilla.com/D18748
    
    --HG--
    extra : source : 7d289fe21aecd01d80482a92b95c4000c761a7b8

--- taskcluster/taskgraph/test/test_decision.py
+++ taskcluster/taskgraph/test/test_decision.py
@@ -109,6 +109,25 @@ class TestGetDecisionParameters(unittest.TestCase):
             self.assertEqual(params['try_options'], None)
             self.assertEqual(params['try_task_config'], ttc)
 
+    def test_try_syntax_from_message_empty(self):
+        self.assertEqual(decision.try_syntax_from_message(''), '')
+
+    def test_try_syntax_from_message_no_try_syntax(self):
+        self.assertEqual(decision.try_syntax_from_message('abc | def'), '')
+
+    def test_try_syntax_from_message_initial_try_syntax(self):
+        self.assertEqual(decision.try_syntax_from_message('try: -f -o -o'), 'try: -f -o -o')
+
+    def test_try_syntax_from_message_initial_try_syntax_multiline(self):
+        self.assertEqual(
+            decision.try_syntax_from_message('try: -f -o -o\nabc\ndef'),
+            'try: -f -o -o')
+
+    def test_try_syntax_from_message_embedded_try_syntax_multiline(self):
+        self.assertEqual(
+            decision.try_syntax_from_message('some stuff\ntry: -f -o -o\nabc\ndef'),
+            'try: -f -o -o')
+
 
 if __name__ == '__main__':
     main()
