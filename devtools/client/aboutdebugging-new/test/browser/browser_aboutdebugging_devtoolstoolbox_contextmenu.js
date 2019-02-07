fc99d157f8bb0cec5a640ea50830946c95fcddba
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-02-07 06:13:39 +0000

    Bug 1523237: Introduce common function which opens/closes about:devtools-toolbox. r=jdescottes
    Depends on D18778
    
    Differential Revision: https://phabricator.services.mozilla.com/D18825
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_contextmenu.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_contextmenu.js
@@ -14,37 +14,20 @@ add_task(async function() {
   prepareCollapsibilitiesTest();
 
   const { document, tab, window } = await openAboutDebugging();
+  const { devtoolsBrowser, devtoolsTab } =
+    await openAboutDevtoolsToolbox(document, tab, window);
 
-  info("Show about:devtools-toolbox page");
-  const target = findDebugTargetByText("about:debugging", document);
-  ok(target, "about:debugging tab target appeared");
-  const inspectButton = target.querySelector(".js-debug-target-inspect-button");
-  ok(inspectButton, "Inspect button for about:debugging appeared");
-  inspectButton.click();
-  await Promise.all([
-    waitUntil(() => tab.nextElementSibling),
-    waitForRequestsToSettle(window.AboutDebugging.store),
-    gDevTools.once("toolbox-ready"),
-  ]);
-
-  info("Wait for about:devtools-toolbox tab will be selected");
-  const devtoolsTab = tab.nextElementSibling;
-  await waitUntil(() => gBrowser.selectedTab === devtoolsTab);
   info("Check whether the menu item which opens devtools is disabled");
   const rootDocument = devtoolsTab.ownerDocument;
-  await assertContextMenu(rootDocument, gBrowser.selectedBrowser,
+  await assertContextMenu(rootDocument, devtoolsBrowser,
                           ".debug-target-info", false);
 
   info("Force to select about:debugging page");
   gBrowser.selectedTab = tab;
   info("Check whether the menu item which opens devtools is enabled");
-  await assertContextMenu(rootDocument, gBrowser.selectedBrowser, "#mount", true);
+  await assertContextMenu(rootDocument, devtoolsBrowser, "#mount", true);
 
-  await removeTab(devtoolsTab);
-  await Promise.all([
-    waitForRequestsToSettle(window.AboutDebugging.store),
-    gDevTools.once("toolbox-destroyed"),
-  ]);
+  await closeAboutDevtoolsToolbox(devtoolsTab, window);
   await removeTab(tab);
 });
 
