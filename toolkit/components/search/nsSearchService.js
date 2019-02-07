0f5db8f16a2ef0e72b1f979af566b141a756d4a2
Michael Kaply <mozilla@kaply.com>
2019-02-07 21:14:17 +0000

    Bug 1516028 - Don't allow a null originalDefaultEngine. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D17879

--- toolkit/components/search/nsSearchService.js
+++ toolkit/components/search/nsSearchService.js
@@ -2724,8 +2724,8 @@ SearchService.prototype = {
   // Get the original Engine object that is the default for this region,
   // ignoring changes the user may have subsequently made.
   get originalDefaultEngine() {
-    let defaultEngine = this.getVerifiedGlobalAttr("searchDefault");
-    if (!defaultEngine) {
+    let defaultEngineName = this.getVerifiedGlobalAttr("searchDefault");
+    if (!defaultEngineName) {
       // We only allow the old defaultenginename pref for distributions
       // We can't use isPartnerBuild because we need to allow reading
       // of the defaultengine name pref for funnelcakes.
@@ -2734,21 +2734,28 @@ SearchService.prototype = {
         let nsIPLS = Ci.nsIPrefLocalizedString;
 
         try {
-          defaultEngine = defaultPrefB.getComplexValue("defaultenginename", nsIPLS).data;
+          defaultEngineName = defaultPrefB.getComplexValue("defaultenginename", nsIPLS).data;
         } catch (ex) {
           // If the default pref is invalid (e.g. an add-on set it to a bogus value)
           // use the default engine from the list.json.
           // This should eventually be the common case. We should only have the
           // defaultenginename pref for distributions.
           // Worst case, getEngineByName will just return null, which is the best we can do.
-          defaultEngine = this._searchDefault;
+          defaultEngineName = this._searchDefault;
         }
       } else {
-        defaultEngine = this._searchDefault;
+        defaultEngineName = this._searchDefault;
       }
     }
 
-    return this.getEngineByName(defaultEngine);
+    let defaultEngine = this.getEngineByName(defaultEngineName);
+    if (!defaultEngine) {
+      // Something unexpected as happened. In order to recover the original default engine,
+      // use the first visible engine which us what currentEngine will use.
+      return this._getSortedEngines(false)[0];
+    }
+
+    return defaultEngine;
   },
 
   resetToOriginalDefaultEngine: function SRCH_SVC__resetToOriginalDefaultEngine() {
