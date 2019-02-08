d19f89680dbd24cb233bab2c50facb56bbe44755
Bob Owen <bobowencode@gmail.com>
2019-02-08 17:17:52 +0000

    Bug 1515088 Part 2: Set LoaderThreads to 1 in the RTL_USER_PROCESS_PARAMETERS structure on child process start-up. r=aklotz a=lizzard

--- security/sandbox/chromium/sandbox/win/src/win_utils.h
+++ security/sandbox/chromium/sandbox/win/src/win_utils.h
@@ -107,7 +107,8 @@ bool ResolveRegistryName(base::string16 name, base::string16* resolved_name);
 // |child_process|, at the specified |address|, preserving the original write
 // protection attributes. Returns true on success.
 bool WriteProtectedChildMemory(HANDLE child_process, void* address,
-                               const void* buffer, size_t length);
+                               const void* buffer, size_t length,
+                               DWORD writeProtection = PAGE_WRITECOPY);
 
 // Returns true if the provided path points to a pipe.
 bool IsPipe(const base::string16& path);
