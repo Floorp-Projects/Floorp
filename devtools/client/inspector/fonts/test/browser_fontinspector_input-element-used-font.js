c10e0167f94669f447ef924de3fc5099294bf21e
Daniel Holbert <dholbert@cs.stanford.edu>
2019-02-08 01:53:29 +0000

    Bug 1526057: Adjust a test to remove reference to no-longer-recognized devtools fonteditor pref. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D19078

--- devtools/client/inspector/fonts/test/browser_fontinspector_input-element-used-font.js
+++ devtools/client/inspector/fonts/test/browser_fontinspector_input-element-used-font.js
@@ -8,8 +8,6 @@ const TEST_URI = URL_ROOT + "doc_browser_fontinspector.html";
 // in its font tab.
 // Non-regression test for https://bugzilla.mozilla.org/show_bug.cgi?id=1435469
 add_task(async function() {
-  await pushPref("devtools.inspector.fonteditor.enabled", true);
-
   const { inspector, view } = await openFontInspectorForURL(TEST_URI);
   const viewDoc = view.document;
 
