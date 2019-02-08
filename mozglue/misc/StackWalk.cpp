294faf00d8800d882ddbff4f4a997d01ed925b58
Eric Rahm <erahm@mozilla.com>
2019-02-08 20:22:21 +0000

    Bug 1520962 - Update stackwalking bounds check to avoid aStackEnd. r=dmajor a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D19210

--- mozglue/misc/StackWalk.cpp
+++ mozglue/misc/StackWalk.cpp
@@ -853,7 +853,7 @@ void FramePointerStackWalk(MozWalkStackCallback aCallback, uint32_t aSkipFrames,
     // a little if the stack has been corrupted.)
     // We don't need to check against the begining of the stack because
     // we can assume that aBp > sp
-    if (next <= aBp || next > aStackEnd || (uintptr_t(next) & 3)) {
+    if (next <= aBp || next >= aStackEnd || (uintptr_t(next) & 3)) {
       break;
     }
 #  if (defined(__ppc__) && defined(XP_MACOSX)) || defined(__powerpc64__)
