5051f2ac01bb5417fbfef00f5a9bf869ad2a21d2
Logan Smyth <loganfsmyth@gmail.com>
2019-02-08 22:38:16 +0000

    Bug 1518661 - Part 2: Avoid separate count in pause point test. r=jlast
    Making changes to this test is kind of annoying because the count and
    the array items need to be kept in sync. This just avoids that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17660

--- devtools/client/debugger/new/test/mochitest/browser_dbg-pause-points.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-pause-points.js
@@ -4,8 +4,7 @@
 requestLongerTimeout(2);
 
 async function stepOvers(dbg, count, onStep = () => {}) {
-  let i = 0;
-  while (i++ <= count) {
+  for (let i = 0; i < count; i++) {
     await dbg.actions.stepOver();
     await waitForPaused(dbg);
     onStep(dbg.getState());
@@ -15,11 +14,11 @@ function formatSteps(steps) {
   return steps.map(loc => `(${loc.join(",")})`).join(", ")
 }
 
-async function testCase(dbg, { name, count, steps }) {
+async function testCase(dbg, { name, steps }) {
   invokeInTab(name);
   let locations = [];
 
-  await stepOvers(dbg, count, state => {
+  await stepOvers(dbg, steps.length, state => {
     const {line, column} = dbg.selectors.getTopFrame(state).location
     locations.push([line, column]);
   });
@@ -34,26 +33,22 @@ add_task(async function test() {
   await selectSource(dbg, "pause-points.js")
   await testCase(dbg, {
     name: "statements",
-    count: 7,
     steps: [[9,2], [10,4], [10,13], [11,2], [11,21], [12,2], [12,12], [13,0]]
   });
 
   await testCase(dbg, {
     name: "expressions",
-    count: 4,
     steps: [[40,2], [41,2], [41,8], [42,8], [43,0]]
   });
 
   await testCase(dbg, {
     name: "sequences",
-    count: 5,
     steps: [[23,2], [25,8], [29,8], [31,4], [34,2], [37,0]]
 
   });
 
   await testCase(dbg, {
     name: "flow",
-    count: 8,
     steps: [[16,2], [17,12], [18,6], [19,2], [19,8], [19,17], [19,8], [19,17], [19,8]]
   });
 });
