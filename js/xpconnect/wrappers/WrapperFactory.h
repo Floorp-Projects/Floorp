d910b50f4661d6c47ddfa9e29829b4e8e8c6c850
Boris Zbarsky <bzbarsky@mit.edu>
2019-02-11 21:07:45 +0000

    Bug 1526624.  Fix Xray waivers to deal with multiple globals per compartment. r=bholley
    In the new setup, they are still same-compartment with their target, but may
    not be same-realm (due to transplants).
    
    We could make them be same-realm by adjusting FixWaiverAfterTransplant, but
    this is conceptually simpler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19261

--- js/xpconnect/wrappers/WrapperFactory.h
+++ js/xpconnect/wrappers/WrapperFactory.h
@@ -99,8 +99,6 @@ class WrapperFactory {
   static bool WaiveXrayAndWrap(JSContext* cx, JS::MutableHandleObject object);
 };
 
-extern const js::Wrapper XrayWaiver;
-
 }  // namespace xpc
 
 #endif /* _xpc_WRAPPERFACTORY_H */
