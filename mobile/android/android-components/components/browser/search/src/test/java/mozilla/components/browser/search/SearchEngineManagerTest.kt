88e5530a6fcba29dc0ea1758dda821969106098c
Christian Sadilek <christian.sadilek@gmail.com>
2019-02-11 15:42:11 -0500

    [components] Revert "Re-enable SearchEngineManagerTest that intermittently failed"
    This reverts commit https://github.com/mozilla-mobile/firefox-android/commit/e9ce0e56978f3bd2d8e00067aed69be9e4045aad.

--- mobile/android/android-components/components/browser/search/src/test/java/mozilla/components/browser/search/SearchEngineManagerTest.kt
+++ mobile/android/android-components/components/browser/search/src/test/java/mozilla/components/browser/search/SearchEngineManagerTest.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.runBlocking
 import mozilla.components.browser.search.provider.SearchEngineProvider
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
@@ -112,6 +113,7 @@ class SearchEngineManagerTest {
         }
     }
 
+    @Ignore
     @Test
     fun `locale update broadcast will trigger reload`() {
         runBlocking {
