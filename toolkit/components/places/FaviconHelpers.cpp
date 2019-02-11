55515b8a0469bdb6ab502b8aa2ffcb5f179ff9d1
Marco Bonardo <mbonardo@mozilla.com>
2019-02-11 17:27:29 +0000

    Bug 1519058 - Some bookmark favicons disappear after clearing history. r=Standard8 a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D19154

--- toolkit/components/places/FaviconHelpers.cpp
+++ toolkit/components/places/FaviconHelpers.cpp
@@ -204,11 +204,14 @@ nsresult SetIconInfo(const RefPtr<Database>& aDB, IconData& aIcon,
       "(icon_url, fixed_icon_url_hash, width, root, expire_ms, data) "
       "VALUES (:url, hash(fixup_url(:url)), :width, :root, :expire, :data) ");
   NS_ENSURE_STATE(insertStmt);
+  // ReplaceFaviconData may replace data for an already existing icon, and in
+  // that case it won't have the page uri at hand, thus it can't tell if the
+  // icon is a root icon or not. For that reason, never overwrite a root = 1.
   nsCOMPtr<mozIStorageStatement> updateStmt = aDB->GetStatement(
       "UPDATE moz_icons SET width = :width, "
       "expire_ms = :expire, "
       "data = :data, "
-      "root = :root "
+      "root = (root  OR :root) "
       "WHERE id = :id ");
   NS_ENSURE_STATE(updateStmt);
 
@@ -814,7 +817,10 @@ AsyncAssociateIconToPage::Run() {
   nsresult rv;
   if (shouldUpdateIcon) {
     rv = SetIconInfo(DB, mIcon);
-    NS_ENSURE_SUCCESS(rv, rv);
+    if (NS_FAILED(rv)) {
+      (void)transaction.Commit();
+      return rv;
+    }
 
     mIcon.status = (mIcon.status & ~(ICON_STATUS_CACHED)) | ICON_STATUS_SAVED;
   }
@@ -1031,6 +1037,7 @@ AsyncReplaceFaviconData::Run() {
   nsresult rv = SetIconInfo(DB, mIcon, true);
   if (rv == NS_ERROR_NOT_AVAILABLE) {
     // There's no previous icon to replace, we don't need to do anything.
+    (void)transaction.Commit();
     return NS_OK;
   }
   NS_ENSURE_SUCCESS(rv, rv);
