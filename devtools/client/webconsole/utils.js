412d3c25c5a25b13a25f755dfd78b12670c0974f
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-02-11 17:29:24 +0000

    Bug 1526883 - Cleanup devtools/client/webconsole/utils.js; r=bgrins.
    Some functions or properties are not used anymore in the
    codebase, so we can safely delete them.
    Other ones are only used in Scratchpad or VariableView,
    which we want to get rid off in the next months. We move
    those functions directly in Scratchpad/VariableView.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19377

--- devtools/client/webconsole/utils.js
+++ devtools/client/webconsole/utils.js
@@ -9,22 +9,8 @@
 const {Cc, Ci} = require("chrome");
 const Services = require("Services");
 
-// Match the function name from the result of toString() or toSource().
-//
-// Examples:
-// (function foobar(a, b) { ...
-// function foobar2(a) { ...
-// function() { ...
-const REGEX_MATCH_FUNCTION_NAME = /^\(?function\s+([^(\s]+)\s*\(/;
-
 // Number of terminal entries for the self-xss prevention to go away
 const CONSOLE_ENTRY_THRESHOLD = 5;
-
-exports.CONSOLE_WORKER_IDS = [
-  "SharedWorker",
-  "ServiceWorker",
-  "Worker",
-];
 
 var WebConsoleUtils = {
 
@@ -44,50 +30,6 @@ var WebConsoleUtils = {
   },
 
   /**
-   * Clone an object.
-   *
-   * @param object object
-   *        The object you want cloned.
-   * @param boolean recursive
-   *        Tells if you want to dig deeper into the object, to clone
-   *        recursively.
-   * @param function [filter]
-   *        Optional, filter function, called for every property. Three
-   *        arguments are passed: key, value and object. Return true if the
-   *        property should be added to the cloned object. Return false to skip
-   *        the property.
-   * @return object
-   *         The cloned object.
-   */
-  cloneObject: function(object, recursive, filter) {
-    if (typeof object != "object") {
-      return object;
-    }
-
-    let temp;
-
-    if (Array.isArray(object)) {
-      temp = [];
-      Array.forEach(object, function(value, index) {
-        if (!filter || filter(index, value, object)) {
-          temp.push(recursive ? WebConsoleUtils.cloneObject(value) : value);
-        }
-      });
-    } else {
-      temp = {};
-      for (const key in object) {
-        const value = object[key];
-        if (object.hasOwnProperty(key) &&
-            (!filter || filter(key, value, object))) {
-          temp[key] = recursive ? WebConsoleUtils.cloneObject(value) : value;
-        }
-      }
-    }
-
-    return temp;
-  },
-
-  /**
    * Copies certain style attributes from one element to another.
    *
    * @param Node from
@@ -102,113 +44,6 @@ var WebConsoleUtils = {
     to.style.fontSize = style.fontSize;
     to.style.fontWeight = style.fontWeight;
     to.style.fontStyle = style.fontStyle;
-  },
-
-  /**
-   * Determine if the given request mixes HTTP with HTTPS content.
-   *
-   * @param string request
-   *        Location of the requested content.
-   * @param string location
-   *        Location of the current page.
-   * @return boolean
-   *         True if the content is mixed, false if not.
-   */
-  isMixedHTTPSRequest: function(request, location) {
-    try {
-      const requestURI = Services.io.newURI(request);
-      const contentURI = Services.io.newURI(location);
-      return (contentURI.scheme == "https" && requestURI.scheme != "https");
-    } catch (ex) {
-      return false;
-    }
-  },
-
-  /**
-   * Helper function to deduce the name of the provided function.
-   *
-   * @param function function
-   *        The function whose name will be returned.
-   * @return string
-   *         Function name.
-   */
-  getFunctionName: function(func) {
-    let name = null;
-    if (func.name) {
-      name = func.name;
-    } else {
-      let desc;
-      try {
-        desc = func.getOwnPropertyDescriptor("displayName");
-      } catch (ex) {
-        // Ignore.
-      }
-      if (desc && typeof desc.value == "string") {
-        name = desc.value;
-      }
-    }
-    if (!name) {
-      try {
-        const str = (func.toString() || func.toSource()) + "";
-        name = (str.match(REGEX_MATCH_FUNCTION_NAME) || [])[1];
-      } catch (ex) {
-        // Ignore.
-      }
-    }
-    return name;
-  },
-
-  /**
-   * Get the object class name. For example, the |window| object has the Window
-   * class name (based on [object Window]).
-   *
-   * @param object object
-   *        The object you want to get the class name for.
-   * @return string
-   *         The object class name.
-   */
-  getObjectClassName: function(object) {
-    if (object === null) {
-      return "null";
-    }
-    if (object === undefined) {
-      return "undefined";
-    }
-
-    const type = typeof object;
-    if (type != "object") {
-      // Grip class names should start with an uppercase letter.
-      return type.charAt(0).toUpperCase() + type.substr(1);
-    }
-
-    let className;
-
-    try {
-      className = ((object + "").match(/^\[object (\S+)\]$/) || [])[1];
-      if (!className) {
-        className = ((object.constructor + "")
-                     .match(/^\[object (\S+)\]$/) || [])[1];
-      }
-      if (!className && typeof object.constructor == "function") {
-        className = this.getFunctionName(object.constructor);
-      }
-    } catch (ex) {
-      // Ignore.
-    }
-
-    return className;
-  },
-
-  /**
-   * Check if the given value is a grip with an actor.
-   *
-   * @param mixed grip
-   *        Value you want to check if it is a grip with an actor.
-   * @return boolean
-   *         True if the given value is a grip with an actor.
-   */
-  isActorGrip: function(grip) {
-    return grip && typeof (grip) == "object" && grip.actor;
   },
 
   /**
@@ -233,54 +68,6 @@ var WebConsoleUtils = {
       WebConsoleUtils._usageCount = newUC;
       Services.prefs.setIntPref("devtools.selfxss.count", newUC);
     }
-  },
-  /**
-   * The inputNode "paste" event handler generator. Helps prevent
-   * self-xss attacks
-   *
-   * @param Element inputField
-   * @param Element notificationBox
-   * @returns A function to be added as a handler to 'paste' and
-   *'drop' events on the input field
-   */
-  pasteHandlerGen: function(inputField, notificationBox, msg, okstring) {
-    const handler = function(event) {
-      if (WebConsoleUtils.usageCount >= CONSOLE_ENTRY_THRESHOLD) {
-        inputField.removeEventListener("paste", handler);
-        inputField.removeEventListener("drop", handler);
-        return true;
-      }
-      if (notificationBox.getNotificationWithValue("selfxss-notification")) {
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-      }
-
-      const notification = notificationBox.appendNotification(msg,
-        "selfxss-notification", null,
-        notificationBox.PRIORITY_WARNING_HIGH, null,
-        function(eventType) {
-          // Cleanup function if notification is dismissed
-          if (eventType == "removed") {
-            inputField.removeEventListener("keyup", pasteKeyUpHandler);
-          }
-        });
-
-      function pasteKeyUpHandler(event2) {
-        const value = inputField.value || inputField.textContent;
-        if (value.includes(okstring)) {
-          notificationBox.removeNotification(notification);
-          inputField.removeEventListener("keyup", pasteKeyUpHandler);
-          WebConsoleUtils.usageCount = CONSOLE_ENTRY_THRESHOLD;
-        }
-      }
-      inputField.addEventListener("keyup", pasteKeyUpHandler);
-
-      event.preventDefault();
-      event.stopPropagation();
-      return false;
-    };
-    return handler;
   },
 };
 
