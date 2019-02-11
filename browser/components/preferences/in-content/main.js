d3fb35a08519d6b8d3c58406de87f2b1f4b1a9fd
Brindusan Cristian <cbrindusan@mozilla.com>
2019-02-11 19:32:16 +0200

    Backed out changeset e785008a884f (bug 1506102) for bc failures at browser_browser_languages_subdialog.js.

--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -784,7 +784,7 @@ var gMainPane = {
    * that the user would like to switch to after confirmation.
    */
   async setBrowserLocales(selected) {
-    let available = await getAvailableLocales();
+    let available = Services.locale.availableLocales;
     let localeNames = Services.intl.getLocaleDisplayNames(undefined, available);
     let locales = available.map((code, i) => ({code, name: localeNames[i]}));
     locales.sort((a, b) => a.name > b.name);
