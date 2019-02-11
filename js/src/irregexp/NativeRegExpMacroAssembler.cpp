f888b4bd3d3e7dd8d609890ac11050c8f0c38f25
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2019-02-11 13:07:15 +0000

    Bug 1522298 - ARM64: Ensure that the emulated stack pointer is restored when returning from WASM. r=bbouvier,sstangl
    Differential Revision: https://phabricator.services.mozilla.com/D19185
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/irregexp/NativeRegExpMacroAssembler.cpp
+++ js/src/irregexp/NativeRegExpMacroAssembler.cpp
@@ -132,7 +132,7 @@ NativeRegExpMacroAssembler::GenerateCode(JSContext* cx, bool match_only)
     masm.Str(PseudoStackPointer64, vixl::MemOperand(sp, -16, vixl::PreIndex));
 
     // Initialize the PSP from the SP.
-    masm.initStackPtr();
+    masm.initPseudoStackPtr();
 #endif
 
     // Push non-volatile registers which might be modified by jitcode.
