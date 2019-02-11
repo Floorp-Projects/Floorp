dce896f8a0c459479b822dc1e4efdb3baa29689a
shindli <shindli@mozilla.com>
2019-02-11 16:59:28 +0200

    Backed out 5 changesets (bug 1523495) for reftest failures in file:///builds/worker/workspace/build/tests/reftest/tests/gfx/tests/reftest/1523776.html CLOSED TREE
    Backed out changeset 3d8ac233a5d1 (bug 1523495)
    Backed out changeset 09de2f84aa1c (bug 1523495)
    Backed out changeset 23406bc33980 (bug 1523495)
    Backed out changeset 8d99e173cdf6 (bug 1523495)
    Backed out changeset 8db55f13f405 (bug 1523495)

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -2914,17 +2914,10 @@ impl PicturePrimitive {
                 // blur results, inflate that clipped area by the blur range, and
                 // then intersect with the total screen rect, to minimize the
                 // allocation size.
-                let mut device_rect = clipped
+                let device_rect = clipped
                     .inflate(inflation_factor, inflation_factor)
                     .intersection(&unclipped.to_i32())
                     .unwrap();
-                // Adjust the size to avoid introducing sampling errors during the down-scaling passes.
-                // what would be even better is to rasterize the picture at the down-scaled size
-                // directly.
-                device_rect.size = RenderTask::adjusted_blur_source_size(
-                    device_rect.size,
-                    blur_std_deviation,
-                );
 
                 let uv_rect_kind = calculate_uv_rect_kind(
                     &pic_rect,
@@ -2963,7 +2956,7 @@ impl PicturePrimitive {
             PictureCompositeMode::Filter(FilterOp::DropShadow(offset, blur_radius, color)) => {
                 let blur_std_deviation = blur_radius * frame_context.device_pixel_scale.0;
                 let blur_range = (blur_std_deviation * BLUR_SAMPLE_SCALE).ceil() as i32;
-                let rounded_std_dev = blur_std_deviation.round();
+
                 // The clipped field is the part of the picture that is visible
                 // on screen. The unclipped field is the screen-space rect of
                 // the complete picture, if no screen / clip-chain was applied
@@ -2972,13 +2965,10 @@ impl PicturePrimitive {
                 // blur results, inflate that clipped area by the blur range, and
                 // then intersect with the total screen rect, to minimize the
                 // allocation size.
-                let mut device_rect = clipped.inflate(blur_range, blur_range)
-                        .intersection(&unclipped.to_i32())
-                        .unwrap();
-                device_rect.size = RenderTask::adjusted_blur_source_size(
-                    device_rect.size,
-                    rounded_std_dev,
-                );
+                let device_rect = clipped
+                    .inflate(blur_range, blur_range)
+                    .intersection(&unclipped.to_i32())
+                    .unwrap();
 
                 let uv_rect_kind = calculate_uv_rect_kind(
                     &pic_rect,
@@ -3002,7 +2992,7 @@ impl PicturePrimitive {
                 let picture_task_id = frame_state.render_tasks.add(picture_task);
 
                 let blur_render_task = RenderTask::new_blur(
-                    rounded_std_dev,
+                    blur_std_deviation.round(),
                     picture_task_id,
                     frame_state.render_tasks,
                     RenderTargetKind::Color,
