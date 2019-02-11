04c289462f90549093e0eab6f821921a69c516f9
Dave Townsend <dtownsend@oxymoronical.com>
2019-02-11 15:29:34 +0000

    Bug 1525297: Don't use empty profiles for dedicated profiles and don't show the welcome page in this case. r=froydnj
    Using an old default profile that is empty (like from bug 1518591) causes us to
    also show the user a welcome page when that isn't necessary.
    
    Instead leave old empty profiles alone (older versions will use them as the
    default), create a new profile but don't set the flag to say that the old
    default was skipped.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19243

--- toolkit/profile/xpcshell/test_update_unknown_dedicated.js
+++ toolkit/profile/xpcshell/test_update_unknown_dedicated.js
@@ -1,6 +1,7 @@
 /*
- * Tests that an old-style default profile not previously used by any build gets
- * updated to a dedicated profile for this build.
+ * Tests that an old-style default profile not previously used by any build
+ * doesn't get updated to a dedicated profile for this build and we don't set
+ * the flag to show the user info about dedicated profiles.
  */
 
 add_task(async () => {
@@ -17,27 +18,36 @@ add_task(async () => {
 
   let service = getProfileService();
   let { profile: selectedProfile, didCreate } = selectStartupProfile();
-  checkStartupReason("firstrun-claimed-default");
+  checkStartupReason("firstrun-created-default");
 
   let profileData = readProfilesIni();
   let installData = readInstallsIni();
 
   Assert.ok(profileData.options.startWithLastProfile, "Should be set to start with the last profile.");
-  Assert.equal(profileData.profiles.length, 1, "Should have the right number of profiles.");
+  Assert.equal(profileData.profiles.length, 2, "Should have the right number of profiles.");
+
+  // The name ordering is different for dev edition.
+  if (AppConstants.MOZ_DEV_EDITION) {
+    profileData.profiles.reverse();
+  }
 
   let profile = profileData.profiles[0];
   Assert.equal(profile.name, PROFILE_DEFAULT, "Should have the right name.");
   Assert.equal(profile.path, defaultProfile.leafName, "Should be the original default profile.");
   Assert.ok(profile.default, "Should be marked as the old-style default.");
+  profile = profileData.profiles[1];
+  Assert.equal(profile.name, DEDICATED_NAME, "Should have the right name.");
+  Assert.notEqual(profile.path, defaultProfile.leafName, "Should not be the original default profile.");
+  Assert.ok(!profile.default, "Should not be marked as the old-style default.");
 
   Assert.equal(Object.keys(installData.installs).length, 1, "Should be a default for installs.");
   Assert.equal(installData.installs[hash].default, profile.path, "Should have the right default profile.");
-  Assert.ok(!installData.installs[hash].locked, "Should not have locked as we didn't create this profile for this install.");
+  Assert.ok(installData.installs[hash].locked, "Should have locked as we created this profile for this install.");
 
   checkProfileService(profileData, installData);
 
-  Assert.ok(!didCreate, "Should not have created a new profile.");
+  Assert.ok(didCreate, "Should have created a new profile.");
   Assert.ok(!service.createdAlternateProfile, "Should not have created an alternate profile.");
-  Assert.ok(selectedProfile.rootDir.equals(defaultProfile), "Should be using the right directory.");
-  Assert.equal(selectedProfile.name, PROFILE_DEFAULT);
+  Assert.ok(!selectedProfile.rootDir.equals(defaultProfile), "Should not be using the old directory.");
+  Assert.equal(selectedProfile.name, DEDICATED_NAME);
 });
