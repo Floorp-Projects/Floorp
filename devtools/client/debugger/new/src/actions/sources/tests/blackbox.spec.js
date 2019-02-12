f9fc573f8aec1c39b89650cd8fd374f616d67c59
derek-li <33161837+derek-li@users.noreply.github.com>
2019-02-11 17:00:06 -0800

    Bug 1527122 - [release 125] [Sources] RelativeSources attributes were not updated (#7910). r=dwalsh

---
+++ devtools/client/debugger/new/src/actions/sources/tests/blackbox.spec.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+
+import {
+  actions,
+  selectors,
+  createStore,
+  makeSource
+} from "../../../utils/test-head";
+
+describe("blackbox", () => {
+  it("should blackbox a source", async () => {
+    const store = createStore({ blackBox: async () => true });
+    const { dispatch, getState } = store;
+
+    const foo1CSR = makeSource("foo1");
+    await dispatch(actions.newSource(foo1CSR));
+    await dispatch(actions.toggleBlackBox(foo1CSR.source));
+
+    const fooSource = selectors.getSource(getState(), "foo1");
+    const relativeSources = selectors.getRelativeSourcesForThread(
+      getState(),
+      foo1CSR.sourceActor.thread
+    );
+
+    expect(relativeSources[fooSource.id].isBlackBoxed).toEqual(true);
+    expect(fooSource.isBlackBoxed).toEqual(true);
+  });
+});
