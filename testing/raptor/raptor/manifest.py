f63de12181d22e70080f145ff82bba5757fbd542
Florin Strugariu <bebe@mozilla.ro>
2019-02-12 14:33:49 +0000

    Bug 1520447 For raptor tp6 on windows use mitmproxy executable instead of creating a separate python environment r=rwood,davehunt
    Differential Revision: https://phabricator.services.mozilla.com/D16802

--- testing/raptor/raptor/manifest.py
+++ testing/raptor/raptor/manifest.py
@@ -19,7 +19,7 @@ required_settings = ['apps', 'type', 'page_cycles', 'test_url', 'measure',
                      'unit', 'lower_is_better', 'alert_threshold']
 
 playback_settings = ['playback_binary_manifest', 'playback_pageset_manifest',
-                     'playback_recordings', 'python3_win_manifest']
+                     'playback_recordings']
 
 
 def filter_app(tests, values):
