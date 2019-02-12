7ae1752a3db51ba4a8658d42cf2a16e7c540f413
Rob Wu <rob@robwu.nl>
2019-02-12 14:04:39 +0000

    Bug 1522918 - Fully remove optional permission data upon uninstall r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D18217

--- toolkit/components/extensions/test/xpcshell/test_ext_permissions_uninstall.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_permissions_uninstall.js
@@ -17,6 +17,40 @@ const observer = {
   },
 };
 
+// Look up the cached permissions, if any.
+async function getCachedPermissions(extensionId) {
+  const NotFound = Symbol("extension ID not found in permissions cache");
+  try {
+    return await ExtensionParent.StartupCache.permissions.get(extensionId, () => {
+      // Throw error to prevent the key from being created.
+      throw NotFound;
+    });
+  } catch (e) {
+    if (e === NotFound) {
+      return null;
+    }
+    throw e;
+  }
+}
+
+// Look up the permissions from the file. Internal methods are used to avoid
+// inadvertently changing the permissions in the cache or JSON file.
+async function getStoredPermissions(extensionId) {
+  // The two _get calls that follow are expected to return the same object if
+  // the entry exists in the JSON file, otherwise we expect two different
+  // objects (with the same default properties).
+  let perms1 = await ExtensionPermissions._get(extensionId);
+  let perms2 = await ExtensionPermissions._get(extensionId);
+  if (perms1 === perms2) {
+    // There is an entry in the file.
+    return perms1;
+  }
+  // Sanity check: The returned object should be empty.
+  Assert.deepEqual(perms1, perms2, "Expected same permission values");
+  Assert.deepEqual(perms1, {origins: [], permissions: []}, "Expected empty permissions");
+  return null;
+}
+
 add_task(async function setup() {
   Services.prefs.setBoolPref("extensions.webextOptionalPermissionPrompts", true);
   Services.obs.addObserver(observer, "webextension-optional-permission-prompt");
@@ -62,9 +96,31 @@ add_task(async function test_permissions_removed() {
   let perms = await ExtensionPermissions.get(id);
   equal(perms.permissions.length, 1, "optional permission added");
 
+  Assert.deepEqual(await getCachedPermissions(id), {
+    permissions: ["idle"],
+    origins: [],
+  }, "Optional permission added to cache");
+  Assert.deepEqual(await getStoredPermissions(id), {
+    permissions: ["idle"],
+    origins: [],
+  }, "Optional permission added to persistent file");
+
   await extension.unload();
+
+  // Directly read from the internals instead of using ExtensionPermissions.get,
+  // because the latter will lazily cache the extension ID.
+  Assert.deepEqual(await getCachedPermissions(id), null, "Cached permissions removed");
+  Assert.deepEqual(await getStoredPermissions(id), null, "Stored permissions removed");
 
   perms = await ExtensionPermissions.get(id);
   equal(perms.permissions.length, 0, "no permissions after uninstall");
   equal(perms.origins.length, 0, "no origin permissions after uninstall");
+
+  // The public ExtensionPermissions.get method should not store (empty)
+  // permissions in the persistent JSON file. Polluting the cache is not ideal,
+  // but acceptable since the cache will eventually be cleared, and non-test
+  // code is not likely to call ExtensionPermissions.get() for non-installed
+  // extensions anyway.
+  Assert.deepEqual(await getCachedPermissions(id), perms, "Permissions cached");
+  Assert.deepEqual(await getStoredPermissions(id), null, "Permissions not saved");
 });
