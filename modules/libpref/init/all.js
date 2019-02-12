adf4cc7da3fcff04f39b57d4951bf6d86e5e6aea
Andrea Marchesini <amarchesini@mozilla.com>
2019-02-12 12:02:07 +0000

    Bug 433274 - Allow popups from context menu events, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D19476

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1291,7 +1291,7 @@ pref("dom.allow_scripts_to_close_windows",          false);
 pref("dom.require_user_interaction_for_beforeunload", true);
 
 pref("dom.popup_maximum",                           20);
-pref("dom.popup_allowed_events", "change click dblclick mouseup pointerup notificationclick reset submit touchend");
+pref("dom.popup_allowed_events", "change click dblclick mouseup pointerup notificationclick reset submit touchend contextmenu");
 
 pref("dom.disable_open_click_delay", 1000);
 pref("dom.serviceWorkers.disable_open_click_delay", 1000);
