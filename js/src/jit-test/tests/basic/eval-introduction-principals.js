89d5c0ae3e0a630d8c8fb4f81c5622263a2802ef
Logan Smyth <loganfsmyth@gmail.com>
2019-02-12 23:46:27 +0000

    Bug 1518661 - Part 3: Adjust call-expression positions. r=jimb,jorendorff
    When we mark call expressions as breakpoints, we want to make it as likely
    as possible that the call has its own unique positon. The existing logic
    means that it is more likely that the beginning of a call will align
    with the start of an expression statement or other debuggable step point.
    By using the property-access location, we're less likely to collide.
    
    Thid also adds a new bytecodes that were missed in the original code that
    added this position handling logic.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17661

--- js/src/jit-test/tests/basic/eval-introduction-principals.js
+++ js/src/jit-test/tests/basic/eval-introduction-principals.js
@@ -12,5 +12,5 @@ contentGlobal.eval("\n" +
 // line numbers, and there's no trace of the myAPI call between the two
 // evals.
 assertEq(stack, "@eval line 2 > eval:1:16\n" +
-                "contentTest@eval:2:26\n" +
+                "contentTest@eval:2:33\n" +
                 "@eval:3:1\n");
