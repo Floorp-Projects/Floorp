d89bce1d490bf3c55abe0aada7c050de00a5b166
Glenn Watson <github@intuitionlibrary.com>
2019-02-12 06:23:01 +0000

    Bug 1526856 - Reduce GPU cache uploads a bit. r=kvark,nical
    By retaining a global GPU cache handle for a dummy image block, we
    can reduce the per-frame GPU cache uploads quite a bit, which
    helps with compositor time.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19326
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/wr/webrender/src/tiling.rs
+++ gfx/wr/webrender/src/tiling.rs
@@ -12,6 +12,7 @@ use debug_render::DebugItem;
 use device::{Texture};
 #[cfg(feature = "pathfinder")]
 use euclid::{TypedPoint2D, TypedVector2D};
+use frame_builder::FrameGlobalResources;
 use gpu_cache::{GpuCache};
 use gpu_types::{BorderInstance, BlurDirection, BlurInstance, PrimitiveHeaders, ScalingInstance};
 use gpu_types::{TransformData, TransformPalette, ZBufferIdGenerator};
@@ -58,6 +59,7 @@ pub struct RenderTargetContext<'a, 'rc> {
     pub surfaces: &'a [SurfaceInfo],
     pub scratch: &'a PrimitiveScratchBuffer,
     pub screen_world_rect: WorldRect,
+    pub globals: &'a FrameGlobalResources,
 }
 
 /// Represents a number of rendering operations on a surface.
