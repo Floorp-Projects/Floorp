0ae9145c60b51b1c417f332a3768b2a029f0a947
Jim Blandy <jimb@mozilla.com>
2019-02-12 08:16:16 +0000

    Bug 1145201: Replace EnqueuePromiseJobCallback and GetIncumbentGlobalCallback with new JobQueue abstract base class. r=arai,smaug
    While the behavior of ECMAScript Promises and their associated job queue is
    covered by the ECMAScript standard, the HTML specification amends that with
    additional behavior the web platform requires. To support this, SpiderMonkey
    provides hooks the embedding can set to replace SpiderMonkey's queue with its
    own implementation.
    
    At present, these hooks are C-style function-pointer-and-void-pointer pairs,
    which are awkward to handle and mistake-prone, as passing a function the wrong
    void* is not a type error. Later patches in this series must add new hooks,
    making a bad situation worse.
    
    A C++ abstract base class is a well-typed alternative. This introduces a new
    `JS::JobQueue` abstract class, and adapts SpiderMonkey's internal job queue and
    Gecko's customization to use it. `GetIncumbentGlobalCallback` and
    `EnqueuePromiseJobCallback` become virtual methods.
    
    Within SpiderMonkey, the patch gathers the various fields of JSContext that
    implement the internal queue into their own type, js::InternalJobQueue. Various
    jsfriendapi functions become veneers for calls to methods specific to the
    derived class. The InternalJobQueue type itself remains private to SpiderMonkey,
    as it uses types like TraceableFifo, derived from Fifo, that are not part of
    SpiderMonkey's public API.
    
    Within Gecko, CycleCollectedJSContext acquires JS::JobQueue as a private base
    class, and a few static methods are cleaned up nicely.
    
    There are a few other hooks defined in js/public/Promise.h that might make sense
    to turn into virtual methods on JobQueue. For example,
    DispatchToEventLoopCallback, used for resolving promises of results from
    off-main-thread tasks, is probably necessarily connected to the JobQueue
    implementation in use, so it might not be sensible to set one without the other.
    But it was left unchanged to reduce this patch's size.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17544
    
    --HG--
    extra : moz-landing-system : lando

--- xpcom/base/CycleCollectedJSContext.cpp
+++ xpcom/base/CycleCollectedJSContext.cpp
@@ -127,9 +127,8 @@ void CycleCollectedJSContext::InitializeCommon() {
 
   NS_GetCurrentThread()->SetCanInvokeJS(true);
 
-  JS::SetGetIncumbentGlobalCallback(mJSContext, GetIncumbentGlobalCallback);
+  JS::SetJobQueue(mJSContext, this);
 
-  JS::SetEnqueuePromiseJobCallback(mJSContext, EnqueuePromiseJobCallback, this);
   JS::SetPromiseRejectionTrackerCallback(mJSContext,
                                          PromiseRejectionTrackerCallback, this);
   mUncaughtRejections.init(mJSContext,
@@ -255,8 +254,7 @@ class PromiseJobRunnable final : public MicroTaskRunnable {
   bool mPropagateUserInputEventHandling;
 };
 
-/* static */
-JSObject* CycleCollectedJSContext::GetIncumbentGlobalCallback(JSContext* aCx) {
+JSObject* CycleCollectedJSContext::getIncumbentGlobal(JSContext* aCx) {
   nsIGlobalObject* global = mozilla::dom::GetIncumbentGlobal();
   if (global) {
     return global->GetGlobalJSObject();
@@ -264,14 +262,11 @@ JSObject* CycleCollectedJSContext::GetIncumbentGlobalCallback(JSContext* aCx) {
   return nullptr;
 }
 
-/* static */
-bool CycleCollectedJSContext::EnqueuePromiseJobCallback(
+bool CycleCollectedJSContext::enqueuePromiseJob(
     JSContext* aCx, JS::HandleObject aPromise, JS::HandleObject aJob,
-    JS::HandleObject aAllocationSite, JS::HandleObject aIncumbentGlobal,
-    void* aData) {
-  CycleCollectedJSContext* self = static_cast<CycleCollectedJSContext*>(aData);
-  MOZ_ASSERT(aCx == self->Context());
-  MOZ_ASSERT(Get() == self);
+    JS::HandleObject aAllocationSite, JS::HandleObject aIncumbentGlobal) {
+  MOZ_ASSERT(aCx == Context());
+  MOZ_ASSERT(Get() == this);
 
   nsIGlobalObject* global = nullptr;
   if (aIncumbentGlobal) {
@@ -280,8 +275,21 @@ bool CycleCollectedJSContext::EnqueuePromiseJobCallback(
   JS::RootedObject jobGlobal(aCx, JS::CurrentGlobalOrNull(aCx));
   RefPtr<PromiseJobRunnable> runnable = new PromiseJobRunnable(
       aPromise, aJob, jobGlobal, aAllocationSite, global);
-  self->DispatchToMicroTask(runnable.forget());
+  DispatchToMicroTask(runnable.forget());
   return true;
+}
+
+void CycleCollectedJSContext::runJobs(JSContext* aCx) {
+  MOZ_ASSERT(aCx == Context());
+  MOZ_ASSERT(Get() == this);
+  PerformMicroTaskCheckPoint();
+}
+
+bool CycleCollectedJSContext::empty() const {
+  // This is our override of JS::JobQueue::empty. Since that interface is only
+  // concerned with the ordinary microtask queue, not the debugger microtask
+  // queue, we only report on the former.
+  return mPendingMicroTaskRunnables.empty();
 }
 
 /* static */
@@ -593,4 +601,5 @@ void CycleCollectedJSContext::PerformDebuggerMicroTaskCheckpoint() {
 
   AfterProcessMicrotasks();
 }
+
 }  // namespace mozilla
