129b94380830753dbb234d6ed3b1fdcb58b73d60
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-02-12 06:29:52 +0000

    Bug 1524474 - Add test for top-level await dynamic import; r=bgrins.
    Differential Revision: https://phabricator.services.mozilla.com/D19134

--- devtools/client/webconsole/test/mochitest/.eslintrc.js
+++ devtools/client/webconsole/test/mochitest/.eslintrc.js
@@ -2,5 +2,11 @@
 
 module.exports = {
   // Extend from the shared list of defined globals for mochitests.
-  "extends": "../../../../.eslintrc.mochitests.js"
+  "extends": "../../../../.eslintrc.mochitests.js",
+  "overrides": [{
+    "files": ["test-dynamic-import.js"],
+    "parserOptions": {
+      "sourceType": "module",
+    },
+  }]
 };
