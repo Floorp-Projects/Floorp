432850e0fdb807b3418419a7c6e0e356df8e77fa
Logan Smyth <loganfsmyth@gmail.com>
2019-02-12 23:46:27 +0000

    Bug 1518661 - Part 3: Adjust call-expression positions. r=jimb,jorendorff
    When we mark call expressions as breakpoints, we want to make it as likely
    as possible that the call has its own unique positon. The existing logic
    means that it is more likely that the beginning of a call will align
    with the start of an expression statement or other debuggable step point.
    By using the property-access location, we're less likely to collide.
    
    Thid also adds a new bytecodes that were missed in the original code that
    added this position handling logic.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17661

--- devtools/client/debugger/new/test/mochitest/browser_dbg-pause-points.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-pause-points.js
@@ -33,7 +33,18 @@ add_task(async function test() {
   await selectSource(dbg, "pause-points.js")
   await testCase(dbg, {
     name: "statements",
-    steps: [[9,2], [10,4], [10,13], [11,2], [11,21], [12,2], [12,12], [13,0]]
+    steps: [
+      [9, 2],
+      [10, 4],
+      [10, 13],
+      [11, 2],
+      [11, 10],
+      [11, 21],
+      [11, 29],
+      [12, 2],
+      [12, 12],
+      [13, 0]
+    ]
   });
 
   await testCase(dbg, {
@@ -44,11 +55,23 @@ add_task(async function test() {
   await testCase(dbg, {
     name: "sequences",
     steps: [[23,2], [25,8], [29,8], [31,4], [34,2], [37,0]]
-
   });
 
   await testCase(dbg, {
     name: "flow",
-    steps: [[16,2], [17,12], [18,6], [19,2], [19,8], [19,17], [19,8], [19,17], [19,8]]
+    steps: [
+      [16, 2],
+      [17, 12],
+      [17, 20],
+      [18, 6],
+      [19, 2],
+      [19, 8],
+      [19, 17],
+      [19, 25],
+      [19, 8],
+      [19, 17],
+      [19, 25],
+      [19, 8]
+    ]
   });
 });
