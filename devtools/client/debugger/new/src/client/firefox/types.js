ad4caf7a989ca1f9c47f671dadbd7071f92cc172
Jason Laster <jason.laster.11@gmail.com>
2019-02-11 17:01:00 -0800

    Bug 1527122 - [release 125] [client] remove backward compatibility in a couple of cases (#7916). r=dwalsh

--- devtools/client/debugger/new/src/client/firefox/types.js
+++ devtools/client/debugger/new/src/client/firefox/types.js
@@ -77,7 +77,7 @@ export type FramePacket = {
   depth?: number,
   oldest?: boolean,
   type: "pause" | "call",
-  where: ActualLocation
+  where: {| actor: string, line: number, column: number |}
 };
 
 /**
