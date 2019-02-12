bd75dfb01456d02fae59dad4fc62e10f9447b3ba
Logan Smyth <loganfsmyth@gmail.com>
2019-02-12 23:45:18 +0000

    Bug 1518661 - Part 1: Pass PropertyName* directly instead of creating ID. r=jimb,jorendorff
    This is just a bit of cleanup I'd noticed while writing new implementations of these.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17659

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -6411,9 +6411,8 @@ class DebuggerScriptGetOffsetLocationMatcher {
       column = flowData[r.frontOffset()].column();
     }
 
-    RootedId id(cx_, NameToId(cx_->names().lineNumber));
     RootedValue value(cx_, NumberValue(lineno));
-    if (!DefineDataProperty(cx_, result_, id, value)) {
+    if (!DefineDataProperty(cx_, result_, cx_->names().lineNumber, value)) {
       return false;
     }
 
@@ -6461,9 +6460,8 @@ class DebuggerScriptGetOffsetLocationMatcher {
       return false;
     }
 
-    RootedId id(cx_, NameToId(cx_->names().lineNumber));
     RootedValue value(cx_, NumberValue(lineno));
-    if (!DefineDataProperty(cx_, result_, id, value)) {
+    if (!DefineDataProperty(cx_, result_, cx_->names().lineNumber, value)) {
       return false;
     }
 
@@ -6681,9 +6679,8 @@ class DebuggerScriptGetAllColumnOffsetsMatcher {
       return false;
     }
 
-    RootedId id(cx_, NameToId(cx_->names().lineNumber));
     RootedValue value(cx_, NumberValue(lineno));
-    if (!DefineDataProperty(cx_, entry, id, value)) {
+    if (!DefineDataProperty(cx_, entry, cx_->names().lineNumber, value)) {
       return false;
     }
 
@@ -6692,9 +6689,8 @@ class DebuggerScriptGetAllColumnOffsetsMatcher {
       return false;
     }
 
-    id = NameToId(cx_->names().offset);
     value = NumberValue(offset);
-    if (!DefineDataProperty(cx_, entry, id, value)) {
+    if (!DefineDataProperty(cx_, entry, cx_->names().offset, value)) {
       return false;
     }
 
