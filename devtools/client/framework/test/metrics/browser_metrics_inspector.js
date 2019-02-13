b85491b0a35a550001b4cf488481560fd8af1483
Noemi Erli <nerli@mozilla.com>
2019-02-13 15:32:27 +0200

    Backed out 4 changesets (bug 1500074, bug 1500072) for devtools failures in browser_browserloader_mocks.js CLOSED TREE
    Backed out changeset e381b765ee73 (bug 1500074)
    Backed out changeset 7cb353ea123d (bug 1500072)
    Backed out changeset 9d792d03c36b (bug 1500072)
    Backed out changeset a11b90e9e698 (bug 1500072)

--- devtools/client/framework/test/metrics/browser_metrics_inspector.js
+++ devtools/client/framework/test/metrics/browser_metrics_inspector.js
@@ -21,9 +21,47 @@ add_task(async function() {
   // The inspector does not use a dedicated browser loader.
   const loaders = [loader.provider.loader];
 
-  runMetricsTest({
-    filterString: "devtools/client/inspector",
-    loaders,
-    panelName: "inspector",
-  });
+  const allModules = getFilteredModules("", loaders);
+  const inspectorModules = getFilteredModules("devtools/client/inspector", loaders);
+
+  const allModulesCount = allModules.length;
+  const inspectorModulesCount = inspectorModules.length;
+
+  const allModulesChars = countCharsInModules(allModules);
+  const inspectorModulesChars = countCharsInModules(inspectorModules);
+
+  const PERFHERDER_DATA = {
+    framework: {
+      name: "devtools",
+    },
+    suites: [{
+      name: "inspector-metrics",
+      value: allModulesChars,
+      subtests: [
+        {
+          name: "inspector-modules",
+          value: inspectorModulesCount,
+        },
+        {
+          name: "inspector-chars",
+          value: inspectorModulesChars,
+        },
+        {
+          name: "all-modules",
+          value: allModulesCount,
+        },
+        {
+          name: "all-chars",
+          value: allModulesChars,
+        },
+      ],
+    }],
+  };
+  info("PERFHERDER_DATA: " + JSON.stringify(PERFHERDER_DATA));
+
+  // Simply check that we found valid values.
+  ok(allModulesCount > inspectorModulesCount &&
+     inspectorModulesCount > 0, "Successfully recorded module count for Inspector");
+  ok(allModulesChars > inspectorModulesChars &&
+     inspectorModulesChars > 0, "Successfully recorded char count for Inspector");
 });
