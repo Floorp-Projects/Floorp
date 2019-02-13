062c87059ced1fdc5e42de0d890b65f347f1c846
Julian Descottes <jdescottes@mozilla.com>
2019-02-13 10:04:34 +0000

    Bug 1500072 - Add metrics test for netmonitor;r=ochameau
    Depends on D19638
    
    Differential Revision: https://phabricator.services.mozilla.com/D19639

---
+++ devtools/client/framework/test/metrics/browser_metrics_netmonitor.js
@@ -0,0 +1,31 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from ../../../shared/test/shared-head.js */
+
+/**
+ * This test records the number of modules loaded by DevTools, as well as the total count
+ * of characters in those modules, when opening the netmonitor. These metrics are
+ * retrieved by perfherder via logs.
+ */
+
+const TEST_URL = "data:text/html;charset=UTF-8,<div>Netmonitor modules load test</div>";
+
+add_task(async function() {
+  const toolbox = await openNewTabAndToolbox(TEST_URL, "netmonitor");
+  const panel = toolbox.getCurrentPanel();
+
+  // Retrieve the browser loader dedicated to the Netmonitor.
+  const netmonitorLoader = panel.panelWin.getBrowserLoaderForWindow();
+  const loaders = [loader.provider.loader, netmonitorLoader.loader];
+
+  runMetricsTest({
+    filterString: "devtools/client/netmonitor",
+    loaders,
+    panelName: "netmonitor",
+  });
+});
