3f7f2bdc8897c3f7575326863be4401d17e36f49
Cosmin Sabou <csabou@mozilla.com>
2019-02-13 17:00:13 +0200

    Backed out 4 changesets (bug 1500074, bug 1500072) for devtools failures on browser_require_raw.js. CLOSED TREE
    Backed out changeset 81a368ce3847 (bug 1500074)
    Backed out changeset 7e6a4063398d (bug 1500072)
    Backed out changeset 4003039a82c0 (bug 1500072)
    Backed out changeset accf4033d8cc (bug 1500072)

--- devtools/client/framework/test/metrics/browser_metrics_webconsole.js
+++ devtools/client/framework/test/metrics/browser_metrics_webconsole.js
@@ -17,15 +17,53 @@ const TEST_URL = "data:text/html;charset=UTF-8,<div>Webconsole modules load test
 
 add_task(async function() {
   const toolbox = await openNewTabAndToolbox(TEST_URL, "webconsole");
-  const panel = toolbox.getCurrentPanel();
+  const hud = toolbox.getCurrentPanel().hud;
 
   // Retrieve the browser loader dedicated to the WebConsole.
-  const webconsoleLoader = panel._frameWindow.getBrowserLoaderForWindow();
+  const webconsoleLoader = hud.ui.browserLoader;
   const loaders = [loader.provider.loader, webconsoleLoader.loader];
 
-  runMetricsTest({
-    filterString: "devtools/client/webconsole",
-    loaders,
-    panelName: "webconsole",
-  });
+  const allModules = getFilteredModules("", loaders);
+  const webconsoleModules = getFilteredModules("devtools/client/webconsole", loaders);
+
+  const allModulesCount = allModules.length;
+  const webconsoleModulesCount = webconsoleModules.length;
+
+  const allModulesChars = countCharsInModules(allModules);
+  const webconsoleModulesChars = countCharsInModules(webconsoleModules);
+
+  const PERFHERDER_DATA = {
+    framework: {
+      name: "devtools",
+    },
+    suites: [{
+      name: "webconsole-metrics",
+      value: allModulesChars,
+      subtests: [
+        {
+          name: "webconsole-modules",
+          value: webconsoleModulesCount,
+        },
+        {
+          name: "webconsole-chars",
+          value: webconsoleModulesChars,
+        },
+        {
+          name: "all-modules",
+          value: allModulesCount,
+        },
+        {
+          name: "all-chars",
+          value: allModulesChars,
+        },
+      ],
+    }],
+  };
+  info("PERFHERDER_DATA: " + JSON.stringify(PERFHERDER_DATA));
+
+  // Simply check that we found valid values.
+  ok(allModulesCount > webconsoleModulesCount &&
+     webconsoleModulesCount > 0, "Successfully recorded module count for WebConsole");
+  ok(allModulesChars > webconsoleModulesChars &&
+     webconsoleModulesChars > 0, "Successfully recorded char count for WebConsole");
 });
