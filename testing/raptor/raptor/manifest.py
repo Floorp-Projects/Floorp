c9a0f2eb6cca12f9c8d0390a4eac4e468477ee82
Cosmin Sabou <csabou@mozilla.com>
2019-02-13 17:04:19 +0200

    Backed out changeset a5ad662d5bbb (bug 1520447) for android build bustages. CLOSED TREE

--- testing/raptor/raptor/manifest.py
+++ testing/raptor/raptor/manifest.py
@@ -19,7 +19,7 @@ required_settings = ['apps', 'type', 'page_cycles', 'test_url', 'measure',
                      'unit', 'lower_is_better', 'alert_threshold']
 
 playback_settings = ['playback_binary_manifest', 'playback_pageset_manifest',
-                     'playback_recordings']
+                     'playback_recordings', 'python3_win_manifest']
 
 
 def filter_app(tests, values):
