e94de1e0080b83589ae0220649a225444cbba9b5
Mike Hommey <mh+mozilla@glandium.org>
2019-02-14 16:01:50 +0900

    Bug 1527798 - Upgrade toolchain tasks using MSVC to 15.8.4. r=froydnj
    It turns out version 15.4.2 spawns a vctip process that sticks after the
    build, and since bug 1527798, that breaks unmounting caches because the
    process has a handle on msvcp140.dll, which lies in the source
    directory. The problem goes away with 15.8.4, so upgrade all toolchain
    tasks to that.
    
    That's the same version as we're using on x86/x86-64 MSVC builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19752

--- taskcluster/scripts/misc/build-gn-win32.sh
+++ taskcluster/scripts/misc/build-gn-win32.sh
@@ -7,14 +7,14 @@ WORKSPACE=$PWD
 UPLOAD_DIR=$WORKSPACE/public/build
 COMPRESS_EXT=bz2
 
-VSPATH="$WORKSPACE/build/src/vs2017_15.4.2"
+VSPATH="$WORKSPACE/build/src/vs2017_15.8.4"
 
-export INCLUDE="${VSPATH}/VC/include:${VSPATH}/VC/atlmfc/include:${VSPATH}/SDK/Include/10.0.15063.0/ucrt:${VSPATH}/SDK/Include/10.0.15063.0/shared:${VSPATH}/SDK/Include/10.0.15063.0/um:${VSPATH}/SDK/Include/10.0.15063.0/winrt:${VSPATH}/DIA SDK/include"
-export LIB="${VSPATH}/VC/lib/x86:${VSPATH}/VC/atlmfc/lib/x86:${VSPATH}/SDK/lib/10.0.15063.0/ucrt/x86:${VSPATH}/SDK/lib/10.0.15063.0/um/x86:${VSPATH}/DIA SDK/lib"
+export INCLUDE="${VSPATH}/VC/include:${VSPATH}/VC/atlmfc/include:${VSPATH}/SDK/Include/10.0.17134.0/ucrt:${VSPATH}/SDK/Include/10.0.17134.0/shared:${VSPATH}/SDK/Include/10.0.17134.0/um:${VSPATH}/SDK/Include/10.0.17134.0/winrt:${VSPATH}/DIA SDK/include"
+export LIB="${VSPATH}/VC/lib/x86:${VSPATH}/VC/atlmfc/lib/x86:${VSPATH}/SDK/lib/10.0.17134.0/ucrt/x86:${VSPATH}/SDK/lib/10.0.17134.0/um/x86:${VSPATH}/DIA SDK/lib"
 
 export PATH="$WORKSPACE/build/src/ninja/bin:$PATH"
 export PATH="$WORKSPACE/build/src/mingw64/bin:$PATH"
-export PATH="${VSPATH}/VC/bin/Hostx64/x86:${VSPATH}/VC/bin/Hostx64/x64:${VSPATH}/VC/bin/Hostx86/x86:${VSPATH}/SDK/bin/10.0.15063.0/x64:${VSPATH}/DIA SDK/bin:${PATH}"
+export PATH="${VSPATH}/VC/bin/Hostx64/x86:${VSPATH}/VC/bin/Hostx64/x64:${VSPATH}/VC/bin/Hostx86/x86:${VSPATH}/SDK/bin/10.0.17134.0/x64:${VSPATH}/DIA SDK/bin:${PATH}"
 export PATH="${VSPATH}/VC/redist/x86/Microsoft.VC141.CRT:${VSPATH}/SDK/Redist/ucrt/DLLs/x86:${PATH}"
 
 cd $WORKSPACE/build/src
