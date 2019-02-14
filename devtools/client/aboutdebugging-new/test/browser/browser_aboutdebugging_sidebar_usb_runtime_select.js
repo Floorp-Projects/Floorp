38eace1603fcb10fb48a82488a4e7c9e90932848
Julian Descottes <jdescottes@mozilla.com>
2019-02-14 09:58:11 +0000

    Bug 1518581 - Require mocks helper in aboutdebugging head.js;r=daisuke
    Depends on D18234
    Most tests will now need to use the mocks helper to avoid loading the regular client-wrapper.
    
    Differential Revision: https://phabricator.services.mozilla.com/D18235

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_usb_runtime_select.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_usb_runtime_select.js
@@ -3,9 +3,6 @@
 
 "use strict";
 
-/* import-globals-from helper-mocks.js */
-Services.scriptloader.loadSubScript(CHROME_URL_ROOT + "helper-mocks.js", this);
-
 const RUNTIME_DEVICE_ID = "1234";
 const RUNTIME_DEVICE_NAME = "A device";
 
