d8a35fa024b40bed81b8d2567b661a12d135c8ce
Julian Seward <jseward@acm.org>
2019-02-14 10:25:42 +0100

    Bug 1527259 - Fix wasm-baseline stackmap boundaries at function calls.  r=lhansen.
    The boundaries of Baseline-created stackmaps, specifically the lowest and
    highest addressed stack words covered by each map, are not quite correct.  For
    Baseline-to-Baseline calls, this isn't a problem; the map boundaries in this
    domain are internally self-consistent.  But when calling to or from
    Ion-generated code, it becomes apparent that the Ion maps (bug 1517924)
    disagree with Baseline maps in such a way that, depending on the direction of
    the call, either there could be words on the stack covered by neither map
    (caller nor callee), or there could be words covered by both maps.
    
    In detail: Baseline so far assumes that the size of the outbound argument area
    is stackArgAreaSize(callee arg types).  That however 16-aligns the size and in
    the process can introduce up to 12 bytes of padding.  In Baseline that doesn't
    matter; via Ion, however, that 12 bytes can (and has been seen to) belong to
    the caller's spill area.  So it shouldn't be covered by the callee's map.
    
    The fix is simple in essence: split stackArgAreaSize into pre- and
    post-alignment parts and use the pre- rather than the post-alignment part, as
    required.
    
    This patch also takes the opportunity to:
    
    * rename StackMapGenerator::framePushedBeforePushingCallArgs_ to
      framePushedExcludingOutboundCallArgs_, for clarity and consistency with
      ongoing Ion map work
    
    * similarly, rename nStackArgBytes in BaseCompiler::beginFunction
    
    * create new files WasmGC.{h,cpp}, as a home for GC-related code that is used
      by more than one of the compilers.
    
    * move stackArgAreaSize into WasmGC.h, and split it into pre/post-aligned
      variants.
    
    * fix a related error in a comment showing the general layout of stack frames.

---
+++ js/src/wasm/WasmGC.h
@@ -0,0 +1,62 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ *
+ * Copyright 2019 Mozilla Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef wasm_gc_h
+#define wasm_gc_h
+
+#include "jit/MacroAssembler.h"
+
+namespace js {
+namespace wasm {
+
+using namespace js::jit;
+
+// StackArgAreaSizeUnaligned returns the size, in bytes, of the stack arg area
+// size needed to pass |argTypes|, excluding any alignment padding beyond the
+// size of the area as a whole.  The size is as determined by the platforms
+// native ABI.
+//
+// StackArgAreaSizeAligned returns the same, but rounded up to the nearest 16
+// byte boundary.
+//
+// Note, StackArgAreaSize{Unaligned,Aligned}() must process all the arguments
+// in order to take into account all necessary alignment constraints.  The
+// signature must include any receiver argument -- in other words, it must be
+// the complete native-ABI-level call signature.
+template <class T>
+static inline size_t StackArgAreaSizeUnaligned(const T& argTypes) {
+  ABIArgIter<const T> i(argTypes);
+  while (!i.done()) {
+     i++;
+  }
+  return i.stackBytesConsumedSoFar();
+}
+
+static inline size_t AlignStackArgAreaSize(size_t unalignedSize) {
+  return AlignBytes(unalignedSize, 16u);
+}
+
+template <class T>
+static inline size_t StackArgAreaSizeAligned(const T& argTypes) {
+  return AlignStackArgAreaSize(StackArgAreaSizeUnaligned(argTypes));
+}
+
+}  // namespace wasm
+}  // namespace js
+
+#endif  // wasm_gc_h
