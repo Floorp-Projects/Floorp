77bb8376e6233c6a2d68333a1cba646e6a0a2243
Michael Kaply <mozilla@kaply.com>
2019-02-14 23:25:08 +0000

    Bug 1527766 - Add enterprise policy for support menu. r=Felipe,flod
    Differential Revision: https://phabricator.services.mozilla.com/D19722

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_support_menu.js
@@ -0,0 +1,28 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+add_task(async function setup() {
+  await setupPolicyEngineWithJson({
+    "policies": {
+      "SupportMenu": {
+        "Title": "Title",
+        "URL": "https://example.com/",
+        "AccessKey": "T",
+      },
+    },
+  });
+});
+
+add_task(async function test_help_menu() {
+  buildHelpMenu();
+  let supportMenu = document.getElementById("helpPolicySupport");
+  is(supportMenu.hidden, false,
+     "The policy menu should be visible.");
+  is(supportMenu.getAttribute("label"), "Title",
+     "The policy menu should have the correct title.");
+  is(supportMenu.getAttribute("href"), "https://example.com/",
+     "The policy menu should have the correct URL.");
+  is(supportMenu.getAttribute("accesskey"), "T",
+     "The policy menu should have the correct access key.");
+});
