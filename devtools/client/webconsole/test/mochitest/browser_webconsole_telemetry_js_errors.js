956a15ad0ecf7e46684427838c8ced86e45f610d
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-02-14 13:48:12 +0000

    Bug 1525682 - Add back the telemetry probe monitoring the error messages issued by SpiderMonkey. r=miker
    This patch adds the DEVTOOLS_JAVASCRIPT_ERROR_DISPLAYED telemetry probe
    back. It existed in the old frontend and was removed when the old
    frontend was removed.
    A test is added to ensure this is working correctly (and that we won't
    delete it by mistake again in the future).
    
    Differential Revision: https://phabricator.services.mozilla.com/D18930
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_telemetry_js_errors.js
@@ -0,0 +1,48 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Tests the DEVTOOLS_JAVASCRIPT_ERROR_DISPLAYED telemetry event.
+
+"use strict";
+
+const TEST_URI = `data:text/html,<meta charset=utf8><script>document()</script>`;
+
+add_task(async function() {
+  startTelemetry();
+
+  const hud = await openNewTabAndConsole(TEST_URI);
+
+  info("Check that the error message is logged in telemetry with the expected key");
+  await waitFor(() => findMessage(hud, "is not a function"));
+  checkErrorDisplayedTelemetry("JSMSG_NOT_FUNCTION", 1);
+
+  await refreshTab();
+
+  info("Reloading the page (and having the same error) increments the sum");
+  await waitFor(() => findMessage(hud, "is not a function"));
+  checkErrorDisplayedTelemetry("JSMSG_NOT_FUNCTION", 2);
+
+  info("Evaluating an expression resulting in the same error increments the sum");
+  let onMessage = waitForMessage(hud, "window is not a function");
+  hud.jsterm.execute("window()");
+  await onMessage;
+  checkErrorDisplayedTelemetry("JSMSG_NOT_FUNCTION", 3);
+
+  info("Evaluating an expression resulting in another error is logged in telemetry");
+  onMessage = waitForMessage(hud, "repeat count must be non-negative");
+  hud.jsterm.execute(`"a".repeat(-1)`);
+  await onMessage;
+  checkErrorDisplayedTelemetry("JSMSG_NEGATIVE_REPETITION_COUNT", 1);
+
+  onMessage = waitForMessage(hud, "repeat count must be non-negative");
+  hud.jsterm.execute(`"b".repeat(-1)`);
+  await onMessage;
+  checkErrorDisplayedTelemetry("JSMSG_NEGATIVE_REPETITION_COUNT", 2);
+});
+
+function checkErrorDisplayedTelemetry(key, count) {
+  checkTelemetry(
+    "DEVTOOLS_JAVASCRIPT_ERROR_DISPLAYED", key, {0: 0, 1: count, 2: 0}, "array");
+}
