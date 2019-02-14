bd470a0354afbcadede6e34c013f229cf8e34553
Emilio Cobos Álvarez <emilio@crisal.io>
2019-02-14 19:03:04 +0100

    Bug 1527972 - Use Rust types for some misc properties. r=jwatt
    -moz-tab-size, border-image-outset and border-image-slice.
    
    This is not a particularly interesting patch, just removes some code. We can
    remove way more code when a few related properties are also ported.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19825

--- layout/style/nsComputedDOMStyle.h
+++ layout/style/nsComputedDOMStyle.h
@@ -297,9 +297,7 @@ class nsComputedDOMStyle final : public nsDOMCSSDeclaration,
   already_AddRefed<CSSValue> DoGetBorderTopRightRadius();
 
   /* Border Image */
-  already_AddRefed<CSSValue> DoGetBorderImageSlice();
   already_AddRefed<CSSValue> DoGetBorderImageWidth();
-  already_AddRefed<CSSValue> DoGetBorderImageOutset();
 
   /* Box Shadow */
   already_AddRefed<CSSValue> DoGetBoxShadow();
@@ -333,7 +331,6 @@ class nsComputedDOMStyle final : public nsDOMCSSDeclaration,
   already_AddRefed<CSSValue> DoGetTextShadow();
   already_AddRefed<CSSValue> DoGetLetterSpacing();
   already_AddRefed<CSSValue> DoGetWordSpacing();
-  already_AddRefed<CSSValue> DoGetTabSize();
   already_AddRefed<CSSValue> DoGetWebkitTextStrokeWidth();
 
   /* Display properties */
