40e0ac9225af8834c4d3dc4f31bf121dd9d31dcf
Shane Caraveo <scaraveo@mozilla.com>
2019-02-14 12:44:07 +0000

    Bug 1525125 block newtab and homepage overrides in private windows r=rpl,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D18730
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/nsBrowserContentHandler.js
+++ browser/components/nsBrowserContentHandler.js
@@ -207,7 +207,7 @@ function openBrowserWindow(cmdLine, triggeringPrincipal, urlOrUrlList, postData 
   let args;
   if (!urlOrUrlList) {
     // Just pass in the defaultArgs directly. We'll use system principal on the other end.
-    args = [gBrowserContentHandler.defaultArgs];
+    args = [gBrowserContentHandler.getDefaultArgs(forcePrivate)];
   } else {
     let pService = Cc["@mozilla.org/toolkit/profile-service;1"].
                   getService(Ci.nsIToolkitProfileService);
@@ -522,7 +522,7 @@ nsBrowserContentHandler.prototype = {
 
   /* nsIBrowserHandler */
 
-  get defaultArgs() {
+  getDefaultArgs(forcePrivate = false) {
     var prefb = Services.prefs;
 
     if (!gFirstWindow) {
@@ -607,7 +607,7 @@ nsBrowserContentHandler.prototype = {
     try {
       var choice = prefb.getIntPref("browser.startup.page");
       if (choice == 1 || choice == 3)
-        startPage = HomePage.get();
+        startPage = forcePrivate ? HomePage.getPrivate() : HomePage.get();
     } catch (e) {
       Cu.reportError(e);
     }
@@ -623,6 +623,10 @@ nsBrowserContentHandler.prototype = {
       return overridePage + "|" + startPage;
 
     return overridePage || startPage || "about:blank";
+  },
+
+  get defaultArgs() {
+    return this.getDefaultArgs(PrivateBrowsingUtils.permanentPrivateBrowsing);
   },
 
   mFeatures: null,
