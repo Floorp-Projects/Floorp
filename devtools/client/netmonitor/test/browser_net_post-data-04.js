b6bbbc95822699691dc33c8bd9e642b2770fe230
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2019-02-14 15:56:17 +0000

    Bug 1514750 - Show JSON POST payload as plain text view also. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D19262

--- devtools/client/netmonitor/test/browser_net_post-data-04.js
+++ devtools/client/netmonitor/test/browser_net_post-data-04.js
@@ -22,28 +22,32 @@ add_task(async function() {
   // Execute requests.
   await performRequests(monitor, tab, 1);
 
-  // Wait for all tree view updated by react
-  wait = waitForDOM(document, "#params-panel .tree-section");
+  // Wait for all tree view updated by react, Bug 1514750 - wait for editor also
+  const waitSections = waitForDOM(document, "#params-panel .tree-section", 2);
+  const waitSourceEditor = waitForDOM(document, "#params-panel .CodeMirror-code");
   store.dispatch(Actions.toggleNetworkDetails());
   EventUtils.sendMouseEvent({ type: "click" },
     document.querySelector("#params-tab"));
-  await wait;
-
+  await Promise.all([waitSections, waitSourceEditor]);
   const tabpanel = document.querySelector("#params-panel");
 
   ok(tabpanel.querySelector(".treeTable"),
     "The request params doesn't have the indended visibility.");
-  ok(tabpanel.querySelector(".editor-mount") === null,
+  // Bug 1514750 - Show JSON params in plain text view also
+  ok(tabpanel.querySelector(".CodeMirror-code"),
     "The request post data doesn't have the indended visibility.");
-
-  is(tabpanel.querySelectorAll(".tree-section").length, 1,
-    "There should be 1 tree sections displayed in this tabpanel.");
+  is(tabpanel.querySelectorAll(".tree-section").length, 2,
+    "There should be 2 tree sections displayed in this tabpanel.");
   is(tabpanel.querySelectorAll(".empty-notice").length, 0,
     "The empty notice should not be displayed in this tabpanel.");
 
-  is(tabpanel.querySelector(".tree-section .treeLabel").textContent,
-    L10N.getStr("jsonScopeName"),
-    "The JSON section doesn't have the correct title.");
+  const treeSections = tabpanel.querySelectorAll(".tree-section");
+
+  is(treeSections[0].querySelector(".treeLabel").textContent,
+    L10N.getStr("jsonScopeName"), "The post section doesn't have the correct title.");
+  is(treeSections[1].querySelector(".treeLabel").textContent,
+    L10N.getStr("paramsPostPayload"),
+    "The post section doesn't have the correct title.");
 
   const labels = tabpanel
     .querySelectorAll("tr:not(.tree-section) .treeLabelCell .treeLabel");
@@ -52,6 +56,9 @@ add_task(async function() {
 
   is(labels[0].textContent, "a", "The JSON var name was incorrect.");
   is(values[0].textContent, "1", "The JSON var value was incorrect.");
+
+  ok(tabpanel.querySelector(".CodeMirror-code").textContent.includes('{"a":1}'),
+    "The text shown in the source editor is incorrect.");
 
   return teardown(monitor);
 });
