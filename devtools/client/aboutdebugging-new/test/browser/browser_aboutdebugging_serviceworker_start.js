77c42b6fb2fcba273829ee7cdd99470c5156e72a
Julian Descottes <jdescottes@mozilla.com>
2019-02-14 08:27:36 +0000

    Bug 1518581 - Silence worker updates if they are not required by the test;r=daisuke,ochameau
    Depends on D18236
    Worker updates are very hard to predict and we have many intermittents caused by pending requests
    for workers while we destroy the client.
    
    Discussed with @ochameau about the potential solutions, and disabling worker updates in tests that don't need them seems a good option. A complementary solution can be to wait for debugger-client waitForRequestsToSettle (different from our current helper in head.js).
    
    Differential Revision: https://phabricator.services.mozilla.com/D18237

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_serviceworker_start.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_serviceworker_start.js
@@ -25,7 +25,7 @@ add_task(async function() {
   await pushPref("dom.serviceWorkers.idle_timeout", 1000);
   await pushPref("dom.serviceWorkers.idle_extended_timeout", 1000);
 
-  const { document, tab } = await openAboutDebugging();
+  const { document, tab } = await openAboutDebugging({ enableWorkerUpdates: true });
 
   // Open a tab that registers a basic service worker.
   const swTab = await addTab(SW_TAB_URL);
