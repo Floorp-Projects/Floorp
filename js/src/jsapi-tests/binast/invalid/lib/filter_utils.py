effc51f5c556990b08274e7efa2c77a6ff4a100f
Tooru Fujisawa <arai_a@mac.com>
2019-02-15 10:26:28 +0000

    Bug 1527791 - Part 1: Add testcase for invalid AssertedPositionalParameterName fields. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D19883

--- js/src/jsapi-tests/binast/invalid/lib/filter_utils.py
+++ js/src/jsapi-tests/binast/invalid/lib/filter_utils.py
@@ -3,6 +3,8 @@
 # All functions raise exception on unexpected case, to avoid overlooking AST
 # change.
 
+import copy
+
 
 def assert_tagged_tuple(obj):
     """
@@ -26,6 +28,42 @@ def assert_list(obj):
     type_ = obj['@TYPE']
     if type_ != 'list':
         raise Exception('expected a list, got {}'.format(type_))
+
+
+def assert_unsigned_long(obj):
+    """
+    Assert that the object is a unsigned long
+
+    :param obj (dict)
+           The unsigned long object
+    """
+    type_ = obj['@TYPE']
+    if type_ != 'unsigned long':
+        raise Exception('expected a unsigned long, got {}'.format(type_))
+
+
+def assert_string(obj):
+    """
+    Assert that the object is a string
+
+    :param obj (dict)
+           The string object
+    """
+    type_ = obj['@TYPE']
+    if type_ != 'string':
+        raise Exception('expected a string, got {}'.format(type_))
+
+
+def assert_identifier_name(obj):
+    """
+    Assert that the object is a identifier name
+
+    :param obj (dict)
+           The identifier name object
+    """
+    type_ = obj['@TYPE']
+    if type_ != 'identifier name':
+        raise Exception('expected a identifier name, got {}'.format(type_))
 
 
 def assert_interface(obj, expected_name):
@@ -124,7 +162,7 @@ def get_element(obj, i):
     assert_list(obj)
     elements = obj['@VALUE']
     if i >= len(elements):
-        raise Exception('Out of bound: {} < {}'.format(i, len(elements)))
+        raise Exception('Out of bound: {} >= {}'.format(i, len(elements)))
     return elements[i]
 
 
@@ -144,7 +182,7 @@ def replace_element(obj, i, value):
     assert_list(obj)
     elements = obj['@VALUE']
     if i >= len(elements):
-        raise Exception('Out of bound: {} < {}'.format(i, len(elements)))
+        raise Exception('Out of bound: {} >= {}'.format(i, len(elements)))
     elements[i] = value
 
 
@@ -176,5 +214,63 @@ def remove_element(obj, i):
     assert_list(obj)
     elements = obj['@VALUE']
     if i >= len(elements):
-        raise Exception('Out of bound: {} < {}'.format(i, len(elements)))
+        raise Exception('Out of bound: {} >= {}'.format(i, len(elements)))
     del elements[i]
+
+
+def copy_tagged_tuple(obj):
+    """
+    Deep copy a tagged tuple
+
+    :param obj (dict)
+           the tagged tuple
+    :return (dict)
+            The copied tagged tuple
+    """
+    assert_tagged_tuple(obj)
+    return copy.deepcopy(obj)
+
+
+def set_unsigned_long(obj, value):
+    """
+    Set unsigned long value
+
+    :param obj (dict)
+           the unsigned long object
+    :param value (int)
+           the value to set
+    """
+    assert_unsigned_long(obj)
+    if type(value) is not int:
+        raise Exception('passed value is not int: {}'.format(value))
+    obj['@VALUE'] = value
+
+
+def set_string(obj, value):
+    """
+    Set string value
+
+    :param obj (dict)
+           the string object
+    :param value (int)
+           the value to set
+    """
+    assert_string(obj)
+    if type(value) is not str:
+        raise Exception('passed value is not string: {}'.format(value))
+    obj['@VALUE'] = value
+
+
+def set_identifier_name(obj, value):
+    """
+    Set identifier name value
+
+    :param obj (dict)
+           the identifier name object
+    :param value (int)
+           the value to set
+    """
+    assert_identifier_name(obj)
+    if type(value) is not str:
+        raise Exception('passed value is not string: {}'.format(value))
+    obj['@VALUE'] = value
