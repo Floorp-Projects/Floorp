5c1c0ba2d15b356c6984497c0de7e557e2cd844b
Kris Maglione <maglione.k@gmail.com>
2019-02-14 17:17:36 -0800

    Backout most of bug 1524687 for Windows PGO xpcshell selftest failures. r=backout

--- extensions/cookie/nsCookieModule.cpp
+++ extensions/cookie/nsCookieModule.cpp
@@ -3,10 +3,35 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/ModuleUtils.h"
+#include "nsIServiceManager.h"
+#include "nsPermissionManager.h"
+#include "nsICategoryManager.h"
 #include "nsCookiePermission.h"
+#include "nsString.h"
 
-namespace mozilla {
+// Define the constructor function for the objects
+NS_GENERIC_FACTORY_SINGLETON_CONSTRUCTOR(nsIPermissionManager,
+                                         nsPermissionManager::GetXPCOMSingleton)
 
-void CookieModuleDtor() { nsCookiePermission::Shutdown(); }
+NS_DEFINE_NAMED_CID(NS_PERMISSIONMANAGER_CID);
 
-}  // namespace mozilla
+static const mozilla::Module::CIDEntry kCookieCIDs[] = {
+    {&kNS_PERMISSIONMANAGER_CID, false, nullptr,
+     nsIPermissionManagerConstructor},
+    {nullptr}};
+
+static const mozilla::Module::ContractIDEntry kCookieContracts[] = {
+    {NS_PERMISSIONMANAGER_CONTRACTID, &kNS_PERMISSIONMANAGER_CID}, {nullptr}};
+
+static void CookieModuleDtor() { nsCookiePermission::Shutdown(); }
+
+static const mozilla::Module kCookieModule = {mozilla::Module::kVersion,
+                                              kCookieCIDs,
+                                              kCookieContracts,
+                                              nullptr,
+                                              nullptr,
+                                              nullptr,
+                                              CookieModuleDtor};
+
+NSMODULE_DEFN(nsCookieModule) = &kCookieModule;
---
+++ extensions/spellcheck/src/mozSpellCheckerFactory.cpp
@@ -0,0 +1,31 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/ModuleUtils.h"
+#include "mozHunspell.h"
+#include "mozPersonalDictionary.h"
+#include "nsIFile.h"
+
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(mozHunspell, Init)
+NS_GENERIC_FACTORY_CONSTRUCTOR_INIT(mozPersonalDictionary, Init)
+
+NS_DEFINE_NAMED_CID(MOZ_HUNSPELL_CID);
+NS_DEFINE_NAMED_CID(MOZ_PERSONALDICTIONARY_CID);
+
+static const mozilla::Module::CIDEntry kSpellcheckCIDs[] = {
+    {&kMOZ_HUNSPELL_CID, false, nullptr, mozHunspellConstructor},
+    {&kMOZ_PERSONALDICTIONARY_CID, false, nullptr,
+     mozPersonalDictionaryConstructor},
+    {nullptr}};
+
+static const mozilla::Module::ContractIDEntry kSpellcheckContracts[] = {
+    {MOZ_HUNSPELL_CONTRACTID, &kMOZ_HUNSPELL_CID},
+    {MOZ_PERSONALDICTIONARY_CONTRACTID, &kMOZ_PERSONALDICTIONARY_CID},
+    {nullptr}};
+
+const mozilla::Module kSpellcheckModule = {
+    mozilla::Module::kVersion, kSpellcheckCIDs, kSpellcheckContracts, nullptr};
+
+NSMODULE_DEFN(mozSpellCheckerModule) = &kSpellcheckModule;
