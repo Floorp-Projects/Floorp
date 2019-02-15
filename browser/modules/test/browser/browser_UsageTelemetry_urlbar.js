02d5b74d1268ff78826f7c9aed1d8971c3aa764d
Mark Banner <standard8@mozilla.com>
2019-02-15 14:57:13 +0000

    Bug 1515083 - Extend urlbar telemetry tests to cover most reported result types. r=adw
    This add checks for everything bar 'preloaded-top-site' which isn't used/enabled at the moment (bug 1527888).
    
    Depends on D19709
    
    Differential Revision: https://phabricator.services.mozilla.com/D19784

--- browser/modules/test/browser/browser_UsageTelemetry_urlbar.js
+++ browser/modules/test/browser/browser_UsageTelemetry_urlbar.js
@@ -1,3 +1,11 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+/**
+ * This file tests urlbar telemetry with search related actions.
+ */
+
 "use strict";
 
 const SCALAR_URLBAR = "browser.engagement.navigation.urlbar";
