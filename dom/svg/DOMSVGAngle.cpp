8e1810987f168a35028c5d919b6b4c94c53d212a
longsonr <longsonr@gmail.com>
2019-02-15 08:41:37 +0000

    Bug 1523178 - Merge SVGAngle and nsSVGOrientType into SVGOrient r=jwatt
    --HG--
    rename : dom/svg/SVGAngle.cpp => dom/svg/SVGOrient.cpp
    rename : dom/svg/SVGAngle.h => dom/svg/SVGOrient.h

--- dom/svg/DOMSVGAngle.cpp
+++ dom/svg/DOMSVGAngle.cpp
@@ -5,7 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "DOMSVGAngle.h"
-#include "SVGAngle.h"
+#include "SVGOrient.h"
 #include "mozilla/dom/SVGAngleBinding.h"
 #include "mozilla/dom/SVGSVGElement.h"
 
@@ -19,7 +19,7 @@ NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(DOMSVGAngle, Release)
 
 DOMSVGAngle::DOMSVGAngle(SVGSVGElement* aSVGElement)
     : mSVGElement(aSVGElement), mType(DOMSVGAngle::CreatedValue) {
-  mVal = new SVGAngle();
+  mVal = new SVGOrient();
   mVal->Init();
 }
 
@@ -30,6 +30,7 @@ JSObject* DOMSVGAngle::WrapObject(JSContext* aCx,
 
 uint16_t DOMSVGAngle::UnitType() const {
   if (mType == AnimValue) {
+    mSVGElement->FlushAnimations();
     return mVal->mAnimValUnit;
   }
   return mVal->mBaseValUnit;
@@ -37,6 +38,7 @@ uint16_t DOMSVGAngle::UnitType() const {
 
 float DOMSVGAngle::Value() const {
   if (mType == AnimValue) {
+    mSVGElement->FlushAnimations();
     return mVal->GetAnimValue();
   }
   return mVal->GetBaseValue();
@@ -103,8 +105,9 @@ void DOMSVGAngle::SetValueAsString(const nsAString& aValue, ErrorResult& rv) {
 
 void DOMSVGAngle::GetValueAsString(nsAString& aValue) {
   if (mType == AnimValue) {
-    mVal->GetAnimValueString(aValue);
+    mSVGElement->FlushAnimations();
+    mVal->GetAnimAngleValueString(aValue);
   } else {
-    mVal->GetBaseValueString(aValue);
+    mVal->GetBaseAngleValueString(aValue);
   }
 }
