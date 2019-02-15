18171dfc117bb5d3c34550f825966bd50aba6f68
Myk Melez <myk@mykzilla.org>
2019-02-15 18:33:30 +0000

    Bug 1527104 - use rkv::OwnedValue instead of internal OwnedValue in kvstore r=nika
    The kvstore crate that landed in bug 1490496 uses an internal implementation of OwnedValue because the rkv crate's equivalent was insufficient at the time that kvstore was being developed.
    
    rkv's OwnedValue has since evolved to support kvstore's use cases, and bug 1525392 is updating mozilla-central to the latest version of rkv; so we should replace kvstore's internal OwnedValue with rkv::OwnedValue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19436

--- toolkit/components/kvstore/src/lib.rs
+++ toolkit/components/kvstore/src/lib.rs
@@ -12,7 +12,6 @@ extern crate log;
 extern crate moz_task;
 extern crate nserror;
 extern crate nsstring;
-extern crate ordered_float;
 extern crate rkv;
 extern crate storage_variant;
 #[macro_use]
@@ -28,8 +27,8 @@ use libc::c_void;
 use moz_task::{create_thread, TaskRunnable};
 use nserror::{nsresult, NS_ERROR_FAILURE, NS_ERROR_NO_AGGREGATION, NS_OK};
 use nsstring::{nsACString, nsCString};
-use owned_value::{owned_to_variant, variant_to_owned, OwnedValue};
-use rkv::{Rkv, SingleStore};
+use owned_value::{owned_to_variant, variant_to_owned};
+use rkv::{OwnedValue, Rkv, SingleStore};
 use std::{
     ptr,
     sync::{Arc, RwLock},
@@ -328,6 +327,6 @@ impl KeyValuePair {
     }
 
     fn get_value(&self) -> Result<RefPtr<nsIVariant>, KeyValueError> {
-        Ok(owned_to_variant(self.value.clone()))
+        Ok(owned_to_variant(self.value.clone())?)
     }
 }
--- toolkit/components/kvstore/src/owned_value.rs
+++ toolkit/components/kvstore/src/owned_value.rs
@@ -6,41 +6,28 @@ use error::KeyValueError;
 use libc::int32_t;
 use nserror::NsresultExt;
 use nsstring::nsString;
-use ordered_float::OrderedFloat;
-use rkv::Value;
+use rkv::OwnedValue;
 use storage_variant::{
     GetDataType, VariantType, DATA_TYPE_BOOL, DATA_TYPE_DOUBLE, DATA_TYPE_EMPTY, DATA_TYPE_INT32,
     DATA_TYPE_VOID, DATA_TYPE_WSTRING,
 };
 use xpcom::{interfaces::nsIVariant, RefPtr};
 
-// This is implemented in rkv but is incomplete there.  We implement a subset
-// to give KeyValuePair ownership over its value, so it can #[derive(xpcom)].
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum OwnedValue {
-    Bool(bool),
-    I64(i64),
-    F64(OrderedFloat<f64>),
-    Str(String),
-}
-
-pub fn value_to_owned(value: Option<Value>) -> Result<OwnedValue, KeyValueError> {
-    match value {
-        Some(Value::Bool(val)) => Ok(OwnedValue::Bool(val)),
-        Some(Value::I64(val)) => Ok(OwnedValue::I64(val)),
-        Some(Value::F64(val)) => Ok(OwnedValue::F64(val)),
-        Some(Value::Str(val)) => Ok(OwnedValue::Str(val.to_owned())),
-        Some(_value) => Err(KeyValueError::UnexpectedValue),
-        None => Err(KeyValueError::UnexpectedValue),
-    }
-}
-
-pub fn owned_to_variant(owned: OwnedValue) -> RefPtr<nsIVariant> {
+pub fn owned_to_variant(owned: OwnedValue) -> Result<RefPtr<nsIVariant>, KeyValueError> {
     match owned {
-        OwnedValue::Bool(val) => val.into_variant(),
-        OwnedValue::I64(val) => val.into_variant(),
-        OwnedValue::F64(OrderedFloat(val)) => val.into_variant(),
-        OwnedValue::Str(ref val) => nsString::from(val).into_variant(),
+        OwnedValue::Bool(val) => Ok(val.into_variant()),
+        OwnedValue::I64(val) => Ok(val.into_variant()),
+        OwnedValue::F64(val) => Ok(val.into_variant()),
+        OwnedValue::Str(ref val) => Ok(nsString::from(val).into_variant()),
+
+        // kvstore doesn't (yet?) support these types of OwnedValue,
+        // and we should never encounter them, but we need to exhaust
+        // all possible variants of the OwnedValue enum.
+        OwnedValue::Instant(_) => Err(KeyValueError::UnsupportedOwned),
+        OwnedValue::Json(_) => Err(KeyValueError::UnsupportedOwned),
+        OwnedValue::U64(_) => Err(KeyValueError::UnsupportedOwned),
+        OwnedValue::Uuid(_) => Err(KeyValueError::UnsupportedOwned),
+        OwnedValue::Blob(_) => Err(KeyValueError::UnsupportedOwned),
     }
 }
 
@@ -56,7 +43,7 @@ pub fn variant_to_owned(variant: &nsIVariant) -> Result<Option<OwnedValue>, KeyV
         DATA_TYPE_DOUBLE => {
             let mut val: f64 = 0.0;
             unsafe { variant.GetAsDouble(&mut val) }.to_result()?;
-            Ok(Some(OwnedValue::F64(val.into())))
+            Ok(Some(OwnedValue::F64(val)))
         }
         DATA_TYPE_WSTRING => {
             let mut val: nsString = nsString::new();
@@ -70,6 +57,6 @@ pub fn variant_to_owned(variant: &nsIVariant) -> Result<Option<OwnedValue>, KeyV
             Ok(Some(OwnedValue::Bool(val)))
         }
         DATA_TYPE_EMPTY | DATA_TYPE_VOID => Ok(None),
-        unsupported_type => Err(KeyValueError::UnsupportedType(unsupported_type)),
+        unsupported_type => Err(KeyValueError::UnsupportedVariant(unsupported_type)),
     }
 }
