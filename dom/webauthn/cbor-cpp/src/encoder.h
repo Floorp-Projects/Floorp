65f65d020836e44d5208ffb681569374e55ec907
Sylvestre Ledru <sledru@mozilla.com>
2019-02-17 00:54:15 +0000

    Bug 1528492 - Revert '1511181 - Reformat everything to the Google coding style' r=jcj
    Differential Revision: https://phabricator.services.mozilla.com/D20065
    
    --HG--
    extra : moz-landing-system : lando

--- dom/webauthn/cbor-cpp/src/encoder.h
+++ dom/webauthn/cbor-cpp/src/encoder.h
@@ -7,12 +7,13 @@
 
        http://www.apache.org/licenses/LICENSE-2.0
 
-           Unless required by applicable law or agreed to in writing, software
-           distributed under the License is distributed on an "AS IS" BASIS,
-           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-   implied. See the License for the specific language governing permissions and
-           limitations under the License.
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
 */
+
 
 #ifndef __CborEncoder_H_
 #define __CborEncoder_H_
@@ -21,48 +22,47 @@
 #include <string>
 
 namespace cbor {
-class encoder {
- private:
-  output *_out;
+    class encoder {
+    private:
+        output *_out;
+    public:
+        explicit encoder(output &out);
 
- public:
-  explicit encoder(output &out);
+        ~encoder();
 
-  ~encoder();
+        void write_bool(bool value);
 
-  void write_bool(bool value);
+        void write_int(int value);
 
-  void write_int(int value);
+        void write_int(long long value);
 
-  void write_int(long long value);
+        void write_int(unsigned int value);
 
-  void write_int(unsigned int value);
+        void write_int(unsigned long long value);
 
-  void write_int(unsigned long long value);
+        void write_bytes(const unsigned char *data, unsigned int size);
 
-  void write_bytes(const unsigned char *data, unsigned int size);
+        void write_string(const char *data, unsigned int size);
 
-  void write_string(const char *data, unsigned int size);
+        void write_string(const std::string str);
 
-  void write_string(const std::string str);
+        void write_array(int size);
 
-  void write_array(int size);
+        void write_map(int size);
 
-  void write_map(int size);
+        void write_tag(const unsigned int tag);
 
-  void write_tag(const unsigned int tag);
+        void write_special(int special);
 
-  void write_special(int special);
+        void write_null();
 
-  void write_null();
+        void write_undefined();
 
-  void write_undefined();
+    private:
+        void write_type_value(int major_type, unsigned int value);
 
- private:
-  void write_type_value(int major_type, unsigned int value);
+        void write_type_value(int major_type, unsigned long long value);
+    };
+}
 
-  void write_type_value(int major_type, unsigned long long value);
-};
-}  // namespace cbor
-
-#endif  //__CborEncoder_H_
+#endif //__CborEncoder_H_
--- dom/webauthn/cbor-cpp/src/output_dynamic.cpp
+++ dom/webauthn/cbor-cpp/src/output_dynamic.cpp
@@ -7,11 +7,11 @@
 
        http://www.apache.org/licenses/LICENSE-2.0
 
-           Unless required by applicable law or agreed to in writing, software
-           distributed under the License is distributed on an "AS IS" BASIS,
-           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-   implied. See the License for the specific language governing permissions and
-           limitations under the License.
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
 */
 
 #include "output_dynamic.h"
@@ -21,40 +21,49 @@
 
 using namespace cbor;
 
+
 void output_dynamic::init(unsigned int initalCapacity) {
-  this->_capacity = initalCapacity;
-  this->_buffer = new unsigned char[initalCapacity];
-  this->_offset = 0;
+    this->_capacity = initalCapacity;
+    this->_buffer = new unsigned char[initalCapacity];
+    this->_offset = 0;
 }
 
-output_dynamic::output_dynamic() { init(256); }
+output_dynamic::output_dynamic() {
+    init(256);
+}
 
 output_dynamic::output_dynamic(unsigned int inital_capacity) {
-  init(inital_capacity);
+    init(inital_capacity);
 }
 
-output_dynamic::~output_dynamic() { delete _buffer; }
+output_dynamic::~output_dynamic() {
+    delete _buffer;
+}
 
-unsigned char *output_dynamic::data() { return _buffer; }
+unsigned char *output_dynamic::data() {
+    return _buffer;
+}
 
-unsigned int output_dynamic::size() { return _offset; }
+unsigned int output_dynamic::size() {
+    return _offset;
+}
 
 void output_dynamic::put_byte(unsigned char value) {
-  if (_offset < _capacity) {
-    _buffer[_offset++] = value;
-  } else {
-    _capacity *= 2;
-    _buffer = (unsigned char *)realloc(_buffer, _capacity);
-    _buffer[_offset++] = value;
-  }
+    if(_offset < _capacity) {
+        _buffer[_offset++] = value;
+    } else {
+        _capacity *= 2;
+        _buffer = (unsigned char *) realloc(_buffer, _capacity);
+        _buffer[_offset++] = value;
+    }
 }
 
 void output_dynamic::put_bytes(const unsigned char *data, int size) {
-  while (_offset + size > _capacity) {
-    _capacity *= 2;
-    _buffer = (unsigned char *)realloc(_buffer, _capacity);
-  }
+    while(_offset + size > _capacity) {
+        _capacity *= 2;
+        _buffer = (unsigned char *) realloc(_buffer, _capacity);
+    }
 
-  memcpy(_buffer + _offset, data, size);
-  _offset += size;
+    memcpy(_buffer + _offset, data, size);
+    _offset += size;
 }
