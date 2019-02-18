a2939933f8d3cc9d840d792fb3e79954739dc9ad
Ms2ger <Ms2ger@igalia.com>
2019-02-18 17:53:49 +0100

    Bug 1528770 - Support release_or_beta key in wpt-in-jstests; r=jgraham

--- js/src/tests/jstests.py
+++ js/src/tests/jstests.py
@@ -380,6 +380,7 @@ def load_wpt_tests(xul_tester, requested_paths, excluded_paths, update_manifest=
                                     "firefox",
                                     debug=xul_tester.test("isDebugBuild"),
                                     extras=run_info_extras)
+    run_info["release_or_beta"] = xul_tester.test("getBuildConfiguration().release_or_beta")
 
     path_filter = testloader.TestFilter(test_manifests,
                                         include=requested_paths,
