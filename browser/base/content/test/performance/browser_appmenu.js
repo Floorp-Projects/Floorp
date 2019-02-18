f374111f74cf6c77b1da9bc3492230cee602488a
Mark Banner <standard8@mozilla.com>
2019-02-18 16:12:35 +0000

    Bug 1522512 - Port most of the browser performance tests to QuantumBar. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D20154

--- browser/base/content/test/performance/browser_appmenu.js
+++ browser/base/content/test/performance/browser_appmenu.js
@@ -42,7 +42,7 @@ const EXPECTED_APPMENU_OPEN_REFLOWS = [
 add_task(async function() {
   await ensureNoPreloadedBrowser();
 
-  let textBoxRect = document.getAnonymousElementByAttribute(gURLBar,
+  let textBoxRect = document.getAnonymousElementByAttribute(gURLBar.textbox,
     "anonid", "moz-input-box").getBoundingClientRect();
   let menuButtonRect =
     document.getElementById("PanelUI-menu-button").getBoundingClientRect();
