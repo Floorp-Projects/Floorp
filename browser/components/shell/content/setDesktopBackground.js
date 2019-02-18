94fd20950e6231c5e88095eb726014df1c9a77fa
Mark Banner <standard8@mozilla.com>
2019-02-18 15:53:45 +0000

    Bug 1528760 - Fix issues in the setDesktopBackground dialog following the ChromeUtils.import global changes. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D20180
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/shell/content/setDesktopBackground.js
+++ browser/components/shell/content/setDesktopBackground.js
@@ -2,8 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const {AppConstants} = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
-const {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
+/* import-globals-from ../../../base/content/utilityOverlay.js */
 
 var gSetBackground = {
   _position: AppConstants.platform == "macosx" ? "STRETCH" : "",
