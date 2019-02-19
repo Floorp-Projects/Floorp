4b13e47e2f6122eb984a327a9a2ec487f44d7c32
Dana Keeler <dkeeler@mozilla.com>
2019-02-19 20:20:37 +0000

    bug 1527372 - write out and shut down DataStorage all at once so that clearing on shutdown works properly r=Ehsan
    In bug 1470918, a shared thread was introduced that did the reading/writing work
    for all DataStorage instances. To ensure all state was written out at shutdown,
    the original patch implemented a two stage strategy that in the first stage
    queued an event for each DataStorage that would write out its contents and then
    in the second stage ran these events to completion. This conflicted with how
    Firefox's sanitization implementation would make sure that data was cleared at
    shutdown, if requested by the user. If the DataStorage objects observed the
    first shutdown notification before the sanitization implementation, the existing
    data would be queued for writing and any further updates would be prevented.
    Thus, when the sanitizer tried to clear data held by DataStorage instances,
    nothing would happen and the data would be written to disk.
    
    This patch fixes this issue by implementing DataStorage shutdown in a single
    stage that can run after sanitization. Since sDataStorages knows of the
    existence of every DataStorage, we can simply iterate through its entries, queue
    write events for each one, and then run those events to completion in one pass.
    
    This patch also fixes bug 1528019 (gDataStorageSharedThread could leak if
    NS_NewNamedThread failed) because the fix is trivial and not worth handling in
    an entirely separate bug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20034
    
    --HG--
    extra : moz-landing-system : lando

---
+++ security/manager/ssl/tests/unit/test_sss_sanitizeOnShutdown.js
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+// The purpose of this test is to ensure that Firefox sanitizes site security
+// service data on shutdown if configured to do so.
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  TestUtils: "resource://testing-common/TestUtils.jsm",
+  Sanitizer: "resource:///modules/Sanitizer.jsm",
+});
+
+Sanitizer.onStartup();
+
+function getStateFileContents() {
+  let stateFile = do_get_profile();
+  stateFile.append(SSS_STATE_FILE_NAME);
+  ok(stateFile.exists());
+  return readFile(stateFile);
+}
+
+add_task(async function run_test() {
+  Services.prefs.setIntPref("test.datastorage.write_timer_ms", 100);
+  do_get_profile();
+  let SSService = Cc["@mozilla.org/ssservice;1"]
+                    .getService(Ci.nsISiteSecurityService);
+  let secInfo = new FakeTransportSecurityInfo();
+  let header = "max-age=50000";
+  SSService.processHeader(Ci.nsISiteSecurityService.HEADER_HSTS,
+                          Services.io.newURI("http://example.com"),
+                          header,
+                          secInfo,
+                          0,
+                          Ci.nsISiteSecurityService.SOURCE_ORGANIC_REQUEST);
+  await TestUtils.topicObserved("data-storage-written", (_, data) => data == SSS_STATE_FILE_NAME);
+  let stateFileContents = getStateFileContents();
+  ok(stateFileContents.includes("example.com"), "should have written out state file");
+
+  // Configure Firefox to clear this data on shutdown.
+  Services.prefs.setBoolPref(Sanitizer.PREF_SHUTDOWN_BRANCH + "siteSettings", true);
+  Services.prefs.setBoolPref(Sanitizer.PREF_SANITIZE_ON_SHUTDOWN, true);
+
+  // Simulate shutdown.
+  Services.obs.notifyObservers(null, "profile-change-teardown");
+  Services.obs.notifyObservers(null, "profile-before-change");
+
+  equal(getStateFileContents(), "", "state file should be empty");
+});
