f5e11f678d5e830264e4353c723fde5fc5b93f5d
Drew Willcoxon <adw@mozilla.com>
2019-02-19 00:03:39 +0000

    Bug 1525909 - Autofill only when input.selectionEnd is equal to input.value.length. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D20001

--- browser/components/urlbar/tests/browser/browser_UrlbarInput_autofill.js
+++ browser/components/urlbar/tests/browser/browser_UrlbarInput_autofill.js
@@ -7,7 +7,7 @@
 
 "use strict";
 
-add_task(async function test() {
+add_task(async function setValueFromResult() {
   gURLBar.setValueFromResult({
     autofill: {
       value: "foobar",
@@ -22,4 +22,33 @@ add_task(async function test() {
     "The start of the selection should be correct");
   Assert.equal(gURLBar.selectionEnd, "foobar".length,
     "The end of the selection should be correct");
+});
+
+add_task(async function noAutofillWhenCaretNotAtEnd() {
+  gURLBar.focus();
+
+  // Autofill is disabled when the new search starts with the previous search,
+  // so to make sure that doesn't mess up this test, trigger a search now.
+  EventUtils.sendString("blah");
+
+  // Add a visit that can be autofilled.
+  await PlacesUtils.history.clear();
+  await PlacesTestUtils.addVisits([{
+    uri: "http://example.com/",
+  }]);
+
+  // Fill the input with xample.com.
+  gURLBar.inputField.value = "xample.com";
+
+  // Move the caret to the beginning and type e.
+  gURLBar.selectionStart = 0;
+  gURLBar.selectionEnd = 0;
+  EventUtils.sendString("e");
+
+  // Get the first result.
+  let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
+  Assert.ok(!result.autofill, "The first result should not be autofill");
+
+  await UrlbarTestUtils.promisePopupClose(window);
+  await PlacesUtils.history.clear();
 });
