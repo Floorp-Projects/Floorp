efbb45f3a6155787042b41c3f0e8f0d3c985bfe9
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-02-19 01:12:19 +0000

    Bug 1500350: Re-layout for runtime controllers. r=ladybenko,jdescottes
    Depends on D18196
    
    Differential Revision: https://phabricator.services.mozilla.com/D18198

--- devtools/client/aboutdebugging-new/src/components/RuntimePage.js
+++ devtools/client/aboutdebugging-new/src/components/RuntimePage.js
@@ -18,6 +18,7 @@ const DebugTargetPane = createFactory(require("./debugtarget/DebugTargetPane"));
 const ExtensionDebugSetting = createFactory(require("./ExtensionDebugSetting"));
 const ExtensionDetail = createFactory(require("./debugtarget/ExtensionDetail"));
 const InspectAction = createFactory(require("./debugtarget/InspectAction"));
+const Message = createFactory(require("./shared/Message"));
 const ProfilerDialog = createFactory(require("./ProfilerDialog"));
 const RuntimeInfo = createFactory(require("./RuntimeInfo"));
 const ServiceWorkerAction = createFactory(require("./debugtarget/ServiceWorkerAction"));
@@ -30,7 +31,7 @@ const TemporaryExtensionInstaller =
 const WorkerDetail = createFactory(require("./debugtarget/WorkerDetail"));
 
 const Actions = require("../actions/index");
-const { DEBUG_TARGET_PANE, PAGE_TYPES, RUNTIMES } = require("../constants");
+const { DEBUG_TARGET_PANE, MESSAGE_LEVEL, PAGE_TYPES, RUNTIMES } = require("../constants");
 const Types = require("../types/index");
 
 const { getCurrentRuntimeDetails } = require("../modules/runtimes-state-helper");
@@ -68,40 +69,17 @@ class RuntimePage extends PureComponent {
     this.props.dispatch(Actions.showProfilerDialog());
   }
 
-  renderRemoteRuntimeActions() {
-    const { runtimeDetails, runtimeId, dispatch } = this.props;
+  renderConnectionPromptSetting() {
+    const { dispatch, runtimeDetails, runtimeId } = this.props;
     const { connectionPromptEnabled } = runtimeDetails;
-
-    if (runtimeId === RUNTIMES.THIS_FIREFOX) {
-      // Connection prompt and Profiling are only available on remote runtimes.
-      return null;
-    }
-
-    return [
-      dom.div(
-        {
-          className: "connection-prompt-setting",
-          key: "connection-prompt-setting",
-        },
-        ConnectionPromptSetting({ connectionPromptEnabled, dispatch }),
-      ),
-      dom.p(
-        {},
-        Localized(
-          {
-            id: "about-debugging-runtime-profile-button",
-            key: "profile-runtime-button",
-          },
-          dom.button(
-            {
-              className: "default-button js-profile-runtime-button",
-              onClick: () => this.onProfilerButtonClick(),
-            },
-            "Profile Runtime"
-          ),
-        ),
-      ),
-    ];
+    // do not show the connection prompt setting in 'This Firefox'
+    return runtimeId !== RUNTIMES.THIS_FIREFOX
+             ? ConnectionPromptSetting({
+               className: "runtime-actions__end",
+               connectionPromptEnabled,
+               dispatch,
+             })
+             : null;
   }
 
   renderDebugTargetPane(name, targets, actionComponent,
@@ -130,9 +108,90 @@ class RuntimePage extends PureComponent {
   }
 
   renderExtensionDebugSetting() {
-    const { runtimeDetails, dispatch } = this.props;
-    const { extensionDebugEnabled } = runtimeDetails;
-    return ExtensionDebugSetting({ extensionDebugEnabled, dispatch });
+    const { dispatch, runtimeDetails } = this.props;
+    const { extensionDebugEnabled, info } = runtimeDetails;
+    return isExtensionDebugSettingNeeded(info.type)
+             ? ExtensionDebugSetting({
+                 className: "runtime-actions__start",
+                 dispatch,
+                 extensionDebugEnabled,
+             })
+             : null;
+  }
+
+  renderProfileButton() {
+    const { runtimeId } = this.props;
+
+    return runtimeId !== RUNTIMES.THIS_FIREFOX
+         ? Localized(
+           {
+             id: "about-debugging-runtime-profile-button",
+           },
+           dom.button(
+             {
+               className: "default-button runtime-actions__start " +
+                          "js-profile-runtime-button",
+               onClick: () => this.onProfilerButtonClick(),
+             },
+             "Profile Runtime"
+           ),
+         )
+         : null;
+  }
+
+  renderRuntimeActions() {
+    return dom.div(
+      {
+        className: "runtime-actions",
+      },
+      this.renderConnectionPromptSetting(),
+      this.renderProfileButton(),
+      this.renderExtensionDebugSetting(),
+      this.renderTemporaryExtensionInstaller(),
+    );
+  }
+
+  renderTemporaryExtensionInstaller() {
+    const { dispatch, runtimeDetails, temporaryInstallError } = this.props;
+    const { type } = runtimeDetails.info;
+    return isSupportedDebugTargetPane(type, DEBUG_TARGET_PANE.TEMPORARY_EXTENSION)
+             ? TemporaryExtensionInstaller({
+                 className: "runtime-actions__end",
+                 dispatch,
+                 temporaryInstallError,
+             })
+             : null;
+  }
+
+  renderTemporaryExtensionInstallError() {
+    const { runtimeDetails, temporaryInstallError } = this.props;
+    const { type } = runtimeDetails.info;
+
+    if (!temporaryInstallError ||
+        !isSupportedDebugTargetPane(type, DEBUG_TARGET_PANE.TEMPORARY_EXTENSION)) {
+      return null;
+    }
+
+    return Message(
+      {
+        level: MESSAGE_LEVEL.ERROR,
+      },
+      dom.div(
+        {},
+        Localized(
+          {
+            id: "about-debugging-tmp-extension-install-error",
+          },
+          dom.span({}, "There was an error during the temporary add-on installation")
+        ),
+        dom.div(
+          {
+            className: "technical-text",
+          },
+          temporaryInstallError
+        )
+      )
+    );
   }
 
   render() {
@@ -146,7 +205,6 @@ class RuntimePage extends PureComponent {
       showProfilerDialog,
       tabs,
       temporaryExtensions,
-      temporaryInstallError,
     } = this.props;
 
     if (!runtimeDetails) {
@@ -155,25 +213,17 @@ class RuntimePage extends PureComponent {
       return null;
     }
 
-    const { type } = runtimeDetails.info;
     const { compatibilityReport } = runtimeDetails;
+
     return dom.article(
       {
         className: "page js-runtime-page",
       },
       RuntimeInfo(runtimeDetails.info),
-      this.renderRemoteRuntimeActions(),
+      this.renderRuntimeActions(),
       runtimeDetails.serviceWorkersAvailable ? null : ServiceWorkersWarning(),
       CompatibilityWarning({ compatibilityReport }),
-      // show component to enable/disable extension debugging.
-      isExtensionDebugSettingNeeded(type)
-        ? this.renderExtensionDebugSetting()
-        : null,
-      isSupportedDebugTargetPane(type, DEBUG_TARGET_PANE.TEMPORARY_EXTENSION)
-        ? TemporaryExtensionInstaller({
-            dispatch,
-            temporaryInstallError,
-        }) : null,
+      this.renderTemporaryExtensionInstallError(),
       this.renderDebugTargetPane("Temporary Extensions",
                                  temporaryExtensions,
                                  TemporaryExtensionAction,
