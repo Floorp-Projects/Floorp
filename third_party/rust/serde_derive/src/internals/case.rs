3ea9c674d58be483feb1295169cec61e61af7e4a
Csoregi Natalia <ncsoregi@mozilla.com>
2019-02-20 18:20:49 +0200

    Backed out 2 changesets (bug 1529117) for failures on webdriver/tests/new_session/invalid_capabilities.py. CLOSED TREE
    Backed out changeset 27882e7ca2a9 (bug 1529117)
    Backed out changeset fcfafe134f70 (bug 1529117)

--- third_party/rust/serde_derive/src/internals/case.rs
+++ third_party/rust/serde_derive/src/internals/case.rs
@@ -1,3 +1,11 @@
+// Copyright 2017 Serde Developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! Code to convert the Rust-styled field/variant (e.g. `my_field`, `MyType`) to the
 //! case of the source (e.g. `my-field`, `MY_FIELD`).
 
@@ -10,7 +18,7 @@ use std::str::FromStr;
 use self::RenameRule::*;
 
 /// The different possible ways to change case of fields in a struct, or variants in an enum.
-#[derive(Copy, Clone, PartialEq)]
+#[derive(PartialEq)]
 pub enum RenameRule {
     /// Don't apply a default rename rule.
     None,
