53d216ad5e4e5738bf1518a773b99725148c40db
yulia <ystartsev@mozilla.com>
2019-02-20 10:49:09 +0000

    Bug 1485664 - adjust events to front events; r=ochameau
    this patch introduces events for the webconsoleFront -- not all of them are being listened
    to by their subscribers yet, but this is the bulk of the effort to move events to Protocol.js
    
    Differential Revision: https://phabricator.services.mozilla.com/D18832

--- devtools/shared/specs/webconsole.js
+++ devtools/shared/specs/webconsole.js
@@ -15,7 +15,7 @@ types.addDictType("console.traits", {
 
 types.addDictType("console.startlisteners", {
   startedListeners: "array:string",
-  nativeConsoleAPI: "boolean",
+  nativeConsoleAPI: "nullable:boolean",
   traits: "console.traits",
 });
 
@@ -39,6 +39,61 @@ types.addDictType("console.cachedmessages", {
 
 const webconsoleSpecPrototype = {
   typeName: "console",
+
+  events: {
+    evaluationResult: {
+      resultID: Option(0, "string"),
+      type: "evaluationResult",
+      awaitResult: Option(0, "nullable:boolean"),
+      errorMessageName: Option(0, "nullable:string"),
+      exception: Option(0, "nullable:json"),
+      exceptionMessage: Option(0, "nullable:string"),
+      exceptionDocURL: Option(0, "nullable:string"),
+      frame: Option(0, "nullable:json"),
+      helperResult: Option(0, "nullable:json"),
+      input: Option(0, "nullable:string"),
+      notes: Option(0, "nullable:string"),
+      result: Option(0, "nullable:json"),
+      timestamp: Option(0, "string"),
+      topLevelAwaitRejected: Option(0, "nullable:boolean"),
+    },
+    fileActivity: {
+      uri: Option(0, "string"),
+    },
+    pageError: {
+      pageError: Option(0, "json"),
+    },
+    logMessage: {
+      message: Option(0, "json"),
+      timeStamp: Option(0, "string"),
+    },
+    consoleAPICall: {
+      message: Option(0, "json"),
+    },
+    reflowActivity: {
+      interruptible: Option(0, "boolean"),
+      start: Option(0, "number"),
+      end: Option(0, "number"),
+      sourceURL: Option(0, "nullable:string"),
+      sourceLine: Option(0, "nullable:number"),
+      functionName: Option(0, "nullable:string"),
+    },
+    // This event is modified re-emitted on the client as "networkEvent".
+    // In order to avoid a naming collision, we rename the server event.
+    serverNetworkEvent: {
+      type: "networkEvent",
+      eventActor: Option(0, "json"),
+    },
+    inspectObject: {
+      objectActor: Option(0, "json"),
+    },
+    lastPrivateContextExited: {
+    },
+    documentEvent: {
+      name: Option(0, "string"),
+      time: Option(0, "string"),
+    },
+  },
 
   methods: {
     startListeners: {
