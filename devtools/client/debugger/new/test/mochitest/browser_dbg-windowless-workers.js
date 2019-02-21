9558d756435636e9bc03c28cfaf79da8daafde65
Jason Laster <jlaster@mozilla.com>
2019-02-21 00:38:54 +0000

    Bug 1519130 - Enable windowless worker debugging. r=davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D16197

--- devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers.js
@@ -3,6 +3,19 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+
+function threadIsPaused(dbg, index) {
+  return findElement(dbg, "threadsPaneItem", index).querySelector(
+    ".pause-badge"
+  );
+}
+
+function threadIsSelected(dbg, index) {
+  return findElement(dbg, "threadsPaneItem", index).classList.contains(
+    "selected"
+  );
+}
+
 // Test basic windowless worker functionality: the main thread and worker can be
 // separately controlled from the same debugger.
 add_task(async function() {
@@ -42,4 +55,47 @@ add_task(async function() {
 
   await stepOver(dbg);
   assertPausedAtSourceAndLine(dbg, mainThreadSource.id, 10);
+
+  is(
+    findElement(dbg, "threadSourceTreeHeader", 2).innerText,
+    "simple-worker.js",
+    "simple-worker.js - worker is shown in the source tree"
+  );
+
+  clickElement(dbg, "threadSourceTreeSourceNode", 2, 2);
+
+  await waitForElement(dbg, "threadSourceTreeSourceNode", 2, 3);
+
+  is(
+    findElement(dbg, "threadSourceTreeSourceNode", 2, 3).innerText,
+    "simple-worker.js",
+    "simple-worker.js - source is shown in the source tree"
+  );
+
+  is(
+    findElement(dbg, "threadSourceTreeSourceNode", 2, 3).innerText,
+    "simple-worker.js",
+    "simple-worker.js - source is shown in the source tree"
+  );
+  // Threads Pane
+  is(
+    findElement(dbg, "threadsPaneItem", 1).innerText,
+    "Main Thread",
+    "Main Thread is shown in the threads pane"
+  );
+
+  is(
+    findElement(dbg, "threadsPaneItem", 2).innerText,
+    "simple-worker.js",
+    "simple-worker.js is shown in the threads pane"
+  );
+
+  ok(threadIsSelected(dbg, 1), "main thread is selected");
+  ok(threadIsPaused(dbg, 1), "main thread is paused");
+  ok(threadIsPaused(dbg, 2), "simple-worker.js is paused");
+  is(findAllElements(dbg, "frames").length, 1, "Main Thread has one frame");
+
+  clickElement(dbg, "threadsPaneItem", 2);
+  await waitFor(() => threadIsSelected(dbg, 2));
+  is(findAllElements(dbg, "frames").length, 1, "simple worker has one frame");
 });
