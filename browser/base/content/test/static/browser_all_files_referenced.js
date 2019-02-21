d259530ce550dbf278b98aaec83f232532ee1d90
Oana Pop Rus <opoprus@mozilla.com>
2019-02-21 14:28:56 +0200

    Backed out changeset d28d0498bd7b (bug 1517493) for bc failures at browser_firstPartyIsolation_saveAs.js

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -131,8 +131,6 @@ var whitelist = [
   {file: "resource://app/modules/translation/YandexTranslator.jsm"},
 
   // Starting from here, files in the whitelist are bugs that need fixing.
-  // Bug 1529127 (only string left referenced in widget/windows/nsDataObj.cpp)
-  {file: "chrome://browser/locale/pageInfo.properties"},
   // Bug 1339424 (wontfix?)
   {file: "chrome://browser/locale/taskbar.properties",
    platforms: ["linux", "macosx"]},
