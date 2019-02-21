21e72bfc3eccdd8685dc14e3669df86e3e9ce7c4
Bogdan Tara <btara@mozilla.com>
2019-02-21 21:18:14 +0200

    Backed out changeset 8704f4b3c8c0 (bug 1524049) for browser-chrome failures CLOSED TREE

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -1220,19 +1220,11 @@ VARCACHE_PREF(
   RelaxedAtomicInt32, 5000
 )
 
-#if defined(XP_WIN) && !defined(_ARM64_)
-# define PREF_VALUE true
-#elif defined(XP_MACOSX)
-# define PREF_VALUE true
-#else
-# define PREF_VALUE false
-#endif
 VARCACHE_PREF(
   "media.rdd-vorbis.enabled",
    MediaRddVorbisEnabled,
-  RelaxedAtomicBool, PREF_VALUE
+  RelaxedAtomicBool, false
 )
-#undef PREF_VALUE
 
 #ifdef ANDROID
 
