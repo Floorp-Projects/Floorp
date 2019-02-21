b87aaeaee4de58462fcc7380069d4bcd9d336274
sotaro <sotaro.ikeda.g@gmail.com>
2019-02-22 08:31:12 +0900

    Bug 1528865 - Change SwapChain's BufferUsage as to add DXGI_USAGE_SHADER_INPUT r=kvark
    DXGI_USAGE_SHADER_INPUT is set for improving performance of copying from framebuffer to texture on intel gpu.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20622

--- gfx/webrender_bindings/RenderCompositorANGLE.cpp
+++ gfx/webrender_bindings/RenderCompositorANGLE.cpp
@@ -158,7 +158,9 @@ bool RenderCompositorANGLE::Initialize() {
     desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     desc.SampleDesc.Count = 1;
     desc.SampleDesc.Quality = 0;
-    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    // DXGI_USAGE_SHADER_INPUT is set for improving performanc of copying from
+    // framebuffer to texture on intel gpu.
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT;
     // Do not use DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL, since it makes HWND
     // unreusable.
     // desc.BufferCount = 2;
@@ -186,7 +188,9 @@ bool RenderCompositorANGLE::Initialize() {
     swapDesc.BufferDesc.RefreshRate.Denominator = 1;
     swapDesc.SampleDesc.Count = 1;
     swapDesc.SampleDesc.Quality = 0;
-    swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    // DXGI_USAGE_SHADER_INPUT is set for improving performanc of copying from
+    // framebuffer to texture on intel gpu.
+    swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT;
     swapDesc.BufferCount = 1;
     swapDesc.OutputWindow = hwnd;
     swapDesc.Windowed = TRUE;
@@ -281,7 +285,9 @@ void RenderCompositorANGLE::CreateSwapChainForDCompIfPossible(
   desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
   desc.SampleDesc.Count = 1;
   desc.SampleDesc.Quality = 0;
-  desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  // DXGI_USAGE_SHADER_INPUT is set for improving performanc of copying from
+  // framebuffer to texture on intel gpu.
+  desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT;
   if (useTripleBuffering) {
     desc.BufferCount = 3;
   } else {
