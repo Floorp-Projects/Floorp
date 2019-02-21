2a3cdabcfe0ad5d2c360b95a0952594d2e1d9770
Michael Froman <mfroman@mozilla.com>
2019-02-21 16:30:13 +0000

    Bug 1524049 - pref on vorbis decoding on RDD process. r=drno
    Differential Revision: https://phabricator.services.mozilla.com/D20607
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -1220,11 +1220,19 @@ VARCACHE_PREF(
   RelaxedAtomicInt32, 5000
 )
 
+#if defined(XP_WIN) && !defined(_ARM64_)
+# define PREF_VALUE true
+#elif defined(XP_MACOSX)
+# define PREF_VALUE true
+#else
+# define PREF_VALUE false
+#endif
 VARCACHE_PREF(
   "media.rdd-vorbis.enabled",
    MediaRddVorbisEnabled,
-  RelaxedAtomicBool, false
+  RelaxedAtomicBool, PREF_VALUE
 )
+#undef PREF_VALUE
 
 #ifdef ANDROID
 
