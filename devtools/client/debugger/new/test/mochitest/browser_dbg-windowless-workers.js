5666349a16dc29afc5f77d690e778ce1ab2f6ab6
Ciure Andrei <aciure@mozilla.com>
2019-02-22 21:18:08 +0200

    Backed out changeset 0bf221eea049 (bug 1529427) for perma failing browser_dbg-windowless-workers.js CLOSED TREE

--- devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers.js
@@ -25,59 +25,36 @@ add_task(async function() {
   const mainThread = dbg.toolbox.threadClient.actor;
 
   const workers = await getWorkers(dbg);
-  ok(workers.length == 2, "Got two workers");
-  const worker1Thread = workers[0].actor;
-  const worker2Thread = workers[1].actor;
+  ok(workers.length == 1, "Got one worker");
+  const workerThread = workers[0].actor;
 
   const mainThreadSource = findSource(dbg, "doc-windowless-workers.html");
   const workerSource = findSource(dbg, "simple-worker.js");
 
-  info("Test pausing in the main thread");
   assertNotPaused(dbg);
+
   await dbg.actions.breakOnNext();
   await waitForPaused(dbg, "doc-windowless-workers.html");
-  assertPausedAtSourceAndLine(dbg, mainThreadSource.id, 10);
 
-  info("Test pausing in a worker");
-  await dbg.actions.selectThread(worker1Thread);
+  // We should be paused at the timer in doc-windowless-workers.html
+  assertPausedAtSourceAndLine(dbg, mainThreadSource.id, 9);
+
+  await dbg.actions.selectThread(workerThread);
   assertNotPaused(dbg);
+
   await dbg.actions.breakOnNext();
   await waitForPaused(dbg, "simple-worker.js");
+
+  // We should be paused at the timer in simple-worker.js
   assertPausedAtSourceAndLine(dbg, workerSource.id, 3);
 
-  info("Test stepping in a worker");
   await stepOver(dbg);
   assertPausedAtSourceAndLine(dbg, workerSource.id, 4);
 
-  info("Test resuming in a worker");
-  await resume(dbg);
-  assertNotPaused(dbg);
+  await dbg.actions.selectThread(mainThread);
 
-  info("Test stepping in the main thread");
-  dbg.actions.selectThread(mainThread);
   await stepOver(dbg);
-  assertPausedAtSourceAndLine(dbg, mainThreadSource.id, 11);
-
-  info("Test resuming in the mainThread");
-  await resume(dbg);
-  assertNotPaused(dbg);
-
-  info("Test pausing in both workers");
-  await addBreakpoint(dbg, "simple-worker", 10);
-  invokeInTab("sayHello");
-  dbg.actions.selectThread(worker1Thread);
-  await waitForPaused(dbg);
-  assertPausedAtSourceAndLine(dbg, workerSource.id, 10);
-
-  dbg.actions.selectThread(worker2Thread);
-  await waitForPaused(dbg);
-  assertPausedAtSourceAndLine(dbg, workerSource.id, 10);
-
-  info("Test stepping in second worker and not the first");
-  await stepOver(dbg);
-  assertPausedAtSourceAndLine(dbg, workerSource.id, 11);
-  dbg.actions.selectThread(worker1Thread);
-  assertPausedAtSourceAndLine(dbg, workerSource.id, 10);
+  assertPausedAtSourceAndLine(dbg, mainThreadSource.id, 10);
 
   is(
     findElement(dbg, "threadSourceTreeHeader", 2).innerText,
--- devtools/client/debugger/new/test/mochitest/examples/simple-worker.js
+++ devtools/client/debugger/new/test/mochitest/examples/simple-worker.js
@@ -5,7 +5,3 @@ function timer() {
 }
 
 setInterval(timer, 1000);
-
-self.onmessage = () => {
-  console.log('hi')
-}
