24787216b404d19a4af37323ce22b35d89e06865
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2019-02-22 15:12:48 +0000

    Bug 1474143 - Switch earlyformsubmit pwmgr observers to DOMFormBeforeSubmit listeners. r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D16652
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/content.js
+++ browser/base/content/content.js
@@ -41,6 +41,12 @@ function shouldIgnoreLoginManagerEvent(event) {
   return event.target.nodePrincipal.isNullPrincipal;
 }
 
+addEventListener("DOMFormBeforeSubmit", function(event) {
+  if (shouldIgnoreLoginManagerEvent(event)) {
+    return;
+  }
+  LoginManagerContent.onDOMFormBeforeSubmit(event);
+});
 addEventListener("DOMFormHasPassword", function(event) {
   if (shouldIgnoreLoginManagerEvent(event)) {
     return;
