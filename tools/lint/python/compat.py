58092a1dca3313bcbec03e77d6e559705cc306e7
Andrew Halberstadt <ahalberstadt@mozilla.com>
2019-02-22 21:18:22 +0000

    Bug 1367092 - [lint] Move py2/py3 linter's exludes logic into mozlint, r=egao
    This will be re-used by the flake8 linter, so moving it into mozlint for
    re-useability.
    
    Depends on D20493
    
    Differential Revision: https://phabricator.services.mozilla.com/D20494

--- tools/lint/python/compat.py
+++ tools/lint/python/compat.py
@@ -9,11 +9,10 @@ import os
 from distutils.spawn import find_executable
 
 import mozfile
-import mozpack.path as mozpath
-from mozpack.files import FileFinder
 from mozprocess import ProcessHandlerMixin
 
 from mozlint import result
+from mozlint.pathutils import expand_exclusions
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -56,20 +55,7 @@ def run_linter(python, paths, config, **lintargs):
             return 1
         return []
 
-    root = lintargs['root']
-    pattern = "**/*.py"
-    exclude = [mozpath.join(root, e) for e in config.get('exclude', [])]
-    files = []
-    for path in paths:
-        path = mozpath.normsep(path)
-        if os.path.isfile(path):
-            files.append(path)
-            continue
-
-        ignore = [e[len(path):].lstrip('/') for e in exclude
-                  if mozpath.commonprefix((path, e)) == path]
-        finder = FileFinder(path, ignore=ignore)
-        files.extend([os.path.join(path, p) for p, f in finder.find(pattern)])
+    files = expand_exclusions(paths, config, lintargs['root'])
 
     with mozfile.NamedTemporaryFile(mode='w') as fh:
         fh.write('\n'.join(files))
