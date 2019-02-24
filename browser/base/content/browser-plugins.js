7a5c7bab52656e6ab1e57b4a7063612e94eb1a0b
Razvan Maries <rmaries@mozilla.com>
2019-02-24 12:17:38 +0200

    Backed out changeset 8c7ac177290e (bug 1479335) for ES Lint fialure. CLOSED TREE

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -280,7 +280,7 @@ var gPluginHandler = {
 
       let options = {
         dismissed: !showNow,
-        hideClose: true,
+        hideClose: !Services.prefs.getBoolPref("privacy.permissionPrompts.showCloseButton"),
         persistent: showNow,
         eventCallback: this._clickToPlayNotificationEventCallback,
         primaryPlugin: primaryPluginPermission,
