16cd2bc721bd5f231eff75779b56149b0f965aea
Matthew Gaudet <mgaudet@mozilla.com>
2019-02-25 13:58:15 +0000

    Bug 1524419 - Incrementally touch stack on arm64 r=tcampbell a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D19992

--- js/src/jit/arm64/Trampoline-arm64.cpp
+++ js/src/jit/arm64/Trampoline-arm64.cpp
@@ -100,6 +100,21 @@ void JitRuntime::generateEnterJIT(JSContext* cx, MacroAssembler& masm) {
   //  8    - frameDescriptor
   //  8    - returnAddress (16-byte aligned, pushed by callee)
 
+  // Touch frame incrementally (a requirement for Windows).
+  //
+  // Use already saved callee-save registers r20 and r21 as temps.
+  //
+  // This has to be done outside the ScratchRegisterScope, as the temps are
+  // under demand inside the touchFrameValues call.
+
+  // Give sp 16-byte alignment and sync stack pointers.
+  masm.andToStackPtr(Imm32(~0xff));
+  // We needn't worry about the Gecko Profiler mark because touchFrameValues
+  // touches in large increments.
+  masm.touchFrameValues(reg_argc, r20, r21);
+  // Restore stack pointer, preserved above.
+  masm.moveToStackPtr(r19);
+
   // Push the argument vector onto the stack.
   // WARNING: destructively modifies reg_argv
   {
