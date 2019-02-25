b7884525b26fc916c26771bb930a11c0cf74c567
Brian Birtles <birtles@gmail.com>
2019-02-25 15:02:12 +0900

    Bug 1509466 - Pass frame request callbacks along with their handles to nsRefreshDriver; r=farre
    In the next patch in this series we want to compare the handle of frame
    callbacks we are about to run,  with a set of canceled handles stored on the
    document. This patch makes us pass the handles along with the callbacks so we
    can do that.
    
    Incidentally doing this allows us to just swap array elements when building up
    the refresh driver's set of callbacks to run. That is hopefully a little more
    efficient than running the implicit conversion operator on each item and then
    appending to an array.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20973

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -2922,16 +2922,27 @@ class Document : public nsINode,
 
   SVGSVGElement* GetSVGRootElement() const;
 
+  struct FrameRequest {
+    FrameRequest(FrameRequestCallback& aCallback, int32_t aHandle);
+
+    // Comparator operators to allow RemoveElementSorted with an
+    // integer argument on arrays of FrameRequest
+    bool operator==(int32_t aHandle) const { return mHandle == aHandle; }
+    bool operator<(int32_t aHandle) const { return mHandle < aHandle; }
+
+    RefPtr<FrameRequestCallback> mCallback;
+    int32_t mHandle;
+  };
+
   nsresult ScheduleFrameRequestCallback(FrameRequestCallback& aCallback,
                                         int32_t* aHandle);
   void CancelFrameRequestCallback(int32_t aHandle);
 
-  typedef nsTArray<RefPtr<FrameRequestCallback>> FrameRequestCallbackList;
   /**
    * Put this document's frame request callbacks into the provided
    * list, and forget about them.
    */
-  void TakeFrameRequestCallbacks(FrameRequestCallbackList& aCallbacks);
+  void TakeFrameRequestCallbacks(nsTArray<FrameRequest>& aCallbacks);
 
   /**
    * @return true if this document's frame request callbacks should be
@@ -4379,8 +4390,6 @@ class Document : public nsINode,
   nsPIDOMWindowInner* mWindow;
 
   nsCOMPtr<nsIDocumentEncoder> mCachedEncoder;
-
-  struct FrameRequest;
 
   nsTArray<FrameRequest> mFrameRequestCallbacks;
 
