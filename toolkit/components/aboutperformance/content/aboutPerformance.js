9218a72e424b94d55e507e152f0a0c0ed4749a0b
Dimi Lee <dlee@mozilla.com>
2019-02-25 14:11:00 +0200

    Bug 1527658 - Use asyncClassifyLocalWithFeatures API in aboutPerformance. r=baku a=lizzard Summary: The error happened because aboutPerformance provided an unused parameter to Classify API which is removed in Bug 1501458.
    Check tracking protection tables via Classify API is not supported now.
    We should use asyncClassifyLocalWithFeatures API instead.
    
    Reviewers: baku
    
    Reviewed By: baku
    
    Bug #: 1527658
    
    Differential Revision: https://phabricator.services.mozilla.com/D19793

--- toolkit/components/aboutperformance/content/aboutPerformance.js
+++ toolkit/components/aboutperformance/content/aboutPerformance.js
@@ -251,13 +251,19 @@ var State = {
       if (host.startsWith("about:") || host.startsWith("moz-nullprincipal"))
         return false;
 
-      let principal =
-        Services.scriptSecurityManager.createCodebasePrincipalFromOrigin("http://" + host);
+      let uri = Services.io.newURI("http://" + host);
       let classifier =
         Cc["@mozilla.org/url-classifier/dbservice;1"].getService(Ci.nsIURIClassifier);
-      classifier.classify(principal, null, true,
-                          (aErrorCode, aList, aProvider, aFullHash) => {
-        this._trackingState.set(host, aErrorCode == Cr.NS_ERROR_TRACKING_URI);
+      let feature = classifier.getFeatureByName("tracking-protection");
+      if (!feature) {
+        return false;
+      }
+
+      classifier.asyncClassifyLocalWithFeatures(uri, [feature],
+        Ci.nsIUrlClassifierFeature.blacklist, list => {
+          if (list.length) {
+            this._trackingState.set(host, true);
+          }
       });
     }
     return this._trackingState.get(host);
