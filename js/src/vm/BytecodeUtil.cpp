ae499f4f2176b46b173e914fe57dc42ddde1b34f
Andreea Pavel <apavel@mozilla.com>
2019-02-25 12:56:18 +0200

    Backed out changeset 95e3e6f91e6e (bug 1528803) for build bustages on a CLOSED TREE

--- js/src/vm/BytecodeUtil.cpp
+++ js/src/vm/BytecodeUtil.cpp
@@ -1961,20 +1961,6 @@ bool ExpressionDecompiler::decompilePC(jsbytecode* pc, uint8_t defIndex) {
 #endif
       break;
 
-    case JSOP_TONUMERIC:
-      return write("(tonumeric ") && decompilePCForStackOperand(pc, -1) &&
-             write(")");
-
-    case JSOP_INC:
-      return write("(inc ") && decompilePCForStackOperand(pc, -1) && write(")");
-
-    case JSOP_DEC:
-      return write("(dec ") && decompilePCForStackOperand(pc, -1) && write(")");
-
-    case JSOP_BIGINT:
-      script->getConst(GET_UINT32_INDEX(pc)).toBigInt()->dump(sprinter);
-      return !sprinter.hadOutOfMemory();
-
     default:
       break;
   }
