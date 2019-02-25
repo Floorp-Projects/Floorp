b9c78a6e4958079222fc7d3fb240d4aac35626a3
Narcis Beleuzu <nbeleuzu@mozilla.com>
2019-02-25 13:32:22 +0000

    Bug 1524495 - Temporarily disable raptor-unity-webgl-geckoview due to frequent failures r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D20939

--- testing/raptor/test/test_manifest.py
+++ testing/raptor/test/test_manifest.py
@@ -131,7 +131,7 @@ def test_get_raptor_test_list_geckoview(create_args):
                        test="raptor-unity-webgl")
 
     test_list = get_raptor_test_list(args, mozinfo.os)
-    assert len(test_list) == 1
+    assert len(test_list) == 0
     assert test_list[0]['name'] == 'raptor-unity-webgl-geckoview'
 
 
