ed87301bde5ee339ca4a8fcd03c987fc3a35dff0
Timothy Nikkel <tnikkel@gmail.com>
2019-02-26 00:16:36 -0600

    Bug 1505871. C++ code to get component transfer filter data into webrender. r=jrmuizel
    Have to use a pointer/size pair to transfer the value list to rust.
    
    We use a "filter holder" that contains an nsTArray that owns the values.

--- gfx/layers/wr/WebRenderLayerManager.h
+++ gfx/layers/wr/WebRenderLayerManager.h
@@ -79,7 +79,7 @@ class WebRenderLayerManager final : public LayerManager {
       EndTransactionFlags aFlags = END_DEFAULT) override;
   void EndTransactionWithoutLayer(
       nsDisplayList* aDisplayList, nsDisplayListBuilder* aDisplayListBuilder,
-      nsTArray<wr::FilterOp>&& aFilters = nsTArray<wr::FilterOp>(),
+      WrFiltersHolder&& aFilters = WrFiltersHolder(),
       WebRenderBackgroundData* aBackground = nullptr);
   virtual void EndTransaction(
       DrawPaintedLayerCallback aCallback, void* aCallbackData,
