449d37684b73183341a4e729cd41129a1f65cb9a
Timothy Nikkel <tnikkel@gmail.com>
2019-02-25 22:45:16 -0600

    Bug 1505871. Implement the necessary things for interning of filter data. r=gw

--- gfx/webrender_bindings/webrender_ffi.h
+++ gfx/webrender_bindings/webrender_ffi.h
@@ -47,7 +47,9 @@ void gecko_profiler_end_marker(const char* name);
   macro(linear_grad);                      \
   macro(radial_grad);                      \
   macro(picture);                          \
-  macro(text_run);
+  macro(text_run);                         \
+  macro(filterdata);
+
 
 // Prelude of types necessary before including webrender_ffi_generated.h
 namespace mozilla {
