06734363b3310dcd3fe58d9b0095e46bd1f80d0b
Patrick Brosset <pbrosset@mozilla.com>
2019-02-26 09:33:52 +0000

    Bug 1363990 - New pref to beautify HTML code when copying from the inspector; r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D21030
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/shared/utils.js
+++ devtools/client/inspector/shared/utils.js
@@ -11,7 +11,6 @@ const promise = require("promise");
 loader.lazyRequireGetter(this, "KeyCodes", "devtools/client/shared/keycodes", true);
 loader.lazyRequireGetter(this, "getCSSLexer", "devtools/shared/css/lexer", true);
 loader.lazyRequireGetter(this, "parseDeclarations", "devtools/shared/css/parsing-utils", true);
-loader.lazyRequireGetter(this, "clipboardHelper", "devtools/shared/platform/clipboard");
 
 const HTML_NS = "http://www.w3.org/1999/xhtml";
 
@@ -81,21 +80,6 @@ function blurOnMultipleProperties(cssProperties) {
       }
     }, 0);
   };
-}
-
-/**
- * Copy the content of a longString (via a promise resolving a
- * LongStringActor) to the clipboard.
- *
- * @param  {Promise} longStringActorPromise
- *         promise expected to resolve a LongStringActor instance
- * @return {Promise} promise resolving (with no argument) when the
- *         string is sent to the clipboard
- */
-function copyLongString(longStringActorPromise) {
-  return getLongString(longStringActorPromise).then(string => {
-    clipboardHelper.copyString(string);
-  }).catch(console.error);
 }
 
 /**
@@ -228,7 +212,6 @@ function translateNodeFrontToGrip(nodeFront) {
 exports.advanceValidate = advanceValidate;
 exports.appendText = appendText;
 exports.blurOnMultipleProperties = blurOnMultipleProperties;
-exports.copyLongString = copyLongString;
 exports.createChild = createChild;
 exports.getLongString = getLongString;
 exports.getSelectorFromGrip = getSelectorFromGrip;
