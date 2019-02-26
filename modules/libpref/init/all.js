333e603a649a091ef6d9c0544aa97ad454795853
Martin Stransky <stransky@redhat.com>
2019-02-26 17:46:15 +0000

    Bug 1530625 - Enable system titlebar on Gnome/X.org sessions due mutter Bug 1530625 and disable titlebar X shape mask on mutter/X.org, r=lsalzman a=lizzard
    - Detect current desktop session and disable shape mask on Mutter/X.org due to Bug 1530252 (mutter bug).
    - Use system titlebar on Mutter/X.org as we can't draw the titlebar reliably.
    - Remove widget.default-hidden-titlebar from prefs. When it's defined it overrides default titlebar detection heuristics now.
    - Don't use shape masks at all on Mutter/x.org. When system titlebar is hidden in this case (by user choice) it has opaque corners unless
      argb visual is selected.
    - Use Window manager decorations on Gnome Classic session, that works better than client decorations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21203

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5994,7 +5994,3 @@ pref("prio.enabled", false);
 // External.AddSearchProvider is deprecated and it will be removed in the next
 // cycles.
 pref("dom.sidebar.enabled", true);
-
-#if defined(MOZ_WIDGET_GTK)
-pref("widget.default-hidden-titlebar", true);
-#endif
