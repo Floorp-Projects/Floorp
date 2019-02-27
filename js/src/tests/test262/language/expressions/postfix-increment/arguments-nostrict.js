daf33f20ab05c20138e365783c9781d67d65ebb1
Jeff Walden <jwalden@mit.edu>
2019-02-26 17:06:48 -0800

    Bug 1530879 - Update the test262 import to upstream rev b337eaf7c7b9bb90991132d0018fdd1d441545ae.  r=jorendorff

--- js/src/tests/test262/language/expressions/postfix-increment/arguments-nostrict.js
+++ js/src/tests/test262/language/expressions/postfix-increment/arguments-nostrict.js
@@ -4,28 +4,23 @@
 /*---
 esid: sec-update-expressions-static-semantics-early-errors
 description: >
-  It is an early Reference Error if AssignmentTargetType of LeftHandSideExpression is invalid. (arguments)
+  In non-strict code, "arguments++" does not produce an early error.
 info: |
 
-  sec-update-expressions-static-semantics-assignmenttargettype
+  sec-identifiers-static-semantics-assignmenttargettype
 
-    UpdateExpression : LeftHandSideExpression ++
-
-    Return invalid.
+    1. If this IdentifierReference is contained in strict mode code and StringValue of Identifier is "eval" or  "arguments", return strict.
+    2. Return simple.
 
   sec-update-expressions-static-semantics-early-errors
 
     UpdateExpression : LeftHandSideExpression ++
 
     It is an early Reference Error if AssignmentTargetType of LeftHandSideExpression is invalid.
+    It is an early Syntax Error if AssignmentTargetType of LeftHandSideExpression is strict.
 
 flags: [noStrict]
-negative:
-  phase: parse
-  type: ReferenceError
 ---*/
-
-$DONOTEVALUATE();
 
 arguments++;
 
