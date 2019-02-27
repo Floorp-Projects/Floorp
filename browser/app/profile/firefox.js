1fbb9d54862f3f67340d8b422329f2eeb138b70e
Gabriele Svelto <gsvelto@mozilla.com>
2019-02-27 22:24:27 +0000

    Bug 675539 - Unload tabs in low-memory scenarios r=mconley
    This adds a mechanism that discards tabs when the browser detects a low-memory
    scenario. Tabs are discarded in LRU order prioritizing regular tabs over
    pinned ones, pinned ones over tabs playing audio and all of the previous over
    pinned tabs playing audio.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20476

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -480,6 +480,13 @@ pref("browser.tabs.remote.separatePrivilegedContentProcess", true);
 // Turn on HTTP response process selection.
 pref("browser.tabs.remote.useHTTPResponseProcessSelection", true);
 
+// Unload tabs on low-memory on nightly.
+#ifdef RELEASE_OR_BETA
+pref("browser.tabs.unloadOnLowMemory", false);
+#else
+pref("browser.tabs.unloadOnLowMemory", true);
+#endif
+
 pref("browser.ctrlTab.recentlyUsedOrder", true);
 
 // By default, do not export HTML at shutdown.
