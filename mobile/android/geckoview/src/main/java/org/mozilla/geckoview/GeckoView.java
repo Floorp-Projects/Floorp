eda7a08ec6e113b0d12f404be6b5f1369feab446
Oana Pop Rus <opoprus@mozilla.com>
2019-02-27 14:42:59 +0200

    Backed out changeset b63cae045a63 (bug 1462018) for geckoview failures

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoView.java
@@ -17,7 +17,6 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -166,22 +165,6 @@ public class GeckoView extends FrameLayout {
 
         public boolean shouldPinOnScreen() {
             return mDisplay != null ? mDisplay.shouldPinOnScreen() : false;
-        }
-
-        /**
-         * Request a {@link Bitmap} of the visible portion of the web page currently being
-         * rendered.
-         *
-         * @return A {@link GeckoResult} that completes with a {@link Bitmap} containing
-         * the pixels and size information of the currently visible rendered web page.
-         */
-        @UiThread
-        @NonNull GeckoResult<Bitmap> capturePixels() {
-            if (mDisplay == null) {
-                return GeckoResult.fromException(new IllegalStateException("Display must be created before pixels can be captured"));
-            }
-
-            return mDisplay.capturePixels();
         }
     }
 
@@ -725,19 +708,5 @@ public class GeckoView extends FrameLayout {
             }
         }
         mSession.getTextInput().autofill(strValues);
-    }
-
-    /**
-     * Request a {@link Bitmap} of the visible portion of the web page currently being
-     * rendered.
-     *
-     * See {@link GeckoDisplay#capturePixels} for more details.
-     *
-     * @return A {@link GeckoResult} that completes with a {@link Bitmap} containing
-     * the pixels and size information of the currently visible rendered web page.
-     */
-    @UiThread
-    public @NonNull GeckoResult<Bitmap> capturePixels() {
-        return mDisplay.capturePixels();
     }
 }
