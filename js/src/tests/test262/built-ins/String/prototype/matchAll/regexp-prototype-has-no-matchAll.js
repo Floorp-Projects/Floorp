8afb0f881aea20283093f4aa18562a06bb90bbc2
Jeff Walden <jwalden@mit.edu>
2019-02-26 17:06:46 -0800

    Bug 1530879 - Update the test262 import to upstream rev 8175f6c05eef922c062e8f5bfae0faaef9404202.  rs=jorendorff

--- js/src/tests/test262/built-ins/String/prototype/matchAll/regexp-prototype-has-no-matchAll.js
+++ js/src/tests/test262/built-ins/String/prototype/matchAll/regexp-prototype-has-no-matchAll.js
@@ -19,6 +19,8 @@ features: [Symbol.matchAll, String.prototype.matchAll]
 includes: [compareArray.js, compareIterator.js, regExpUtils.js]
 ---*/
 
+assert.sameValue(typeof String.prototype.matchAll, "function");
+
 delete RegExp.prototype[Symbol.matchAll];
 var str = '/a/g*/b/g';
 
