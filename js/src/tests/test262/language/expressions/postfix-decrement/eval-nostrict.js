cdb981e2575a37b0317328b6c0505c7f839aed1e
Jeff Walden <jwalden@mit.edu>
2019-02-26 17:06:48 -0800

    Bug 1530879 - Update the test262 import to upstream rev b337eaf7c7b9bb90991132d0018fdd1d441545ae.  r=jorendorff

--- js/src/tests/test262/language/expressions/postfix-decrement/eval-nostrict.js
+++ js/src/tests/test262/language/expressions/postfix-decrement/eval-nostrict.js
@@ -4,28 +4,23 @@
 /*---
 esid: sec-update-expressions
 description: >
-  It is an early Reference Error if AssignmentTargetType of LeftHandSideExpression is invalid. (eval)
+  In non-strict code, "eval--" does not produce an early error.
 info: |
 
-  sec-update-expressions-static-semantics-assignmenttargettype
+  sec-identifiers-static-semantics-assignmenttargettype
 
-    UpdateExpression : LeftHandSideExpression --
-
-    Return invalid.
+    If this IdentifierReference is contained in strict mode code and StringValue of Identifier is "eval" or  "arguments", return strict.
+    Return simple.
 
   sec-update-expressions-static-semantics-early-errors
 
     UpdateExpression : LeftHandSideExpression --
 
     It is an early Reference Error if AssignmentTargetType of LeftHandSideExpression is invalid.
+    It is an early Syntax Error if AssignmentTargetType of LeftHandSideExpression is strict.
 
 flags: [noStrict]
-negative:
-  phase: parse
-  type: ReferenceError
 ---*/
-
-$DONOTEVALUATE();
 
 eval--;
 
