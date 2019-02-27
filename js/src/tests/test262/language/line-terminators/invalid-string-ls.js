3abd27d854bf78c8efe8718e778c7a33bbc8065f
Jeff Walden <jwalden@mit.edu>
2019-02-26 17:06:47 -0800

    Bug 1530879 - Update the test262 import to upstream rev 3468dad291251d4759ab6270d4fb8b75befa8d93.  r=jorendorff

---
+++ js/src/tests/test262/language/line-terminators/invalid-string-ls.js
@@ -0,0 +1,17 @@
+// |reftest| error:SyntaxError
+// Copyright 2019 Mike Pennisi.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+info: LINE SEPARATOR (U+2028) within strings is not allowed
+es5id: 7.3_A2.2_T1
+esid: sec-line-terminators
+description: Insert LINE SEPARATOR (\u2028) into string
+negative:
+  phase: parse
+  type: SyntaxError
+---*/
+
+$DONOTEVALUATE();
+
+' '
