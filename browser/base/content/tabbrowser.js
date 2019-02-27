7fd80b6ed99dc25c792b7cc12aaaba1ad74120e6
James Willcox <snorp@snorp.net>
2019-02-27 15:57:44 +0000

    Bug 1524992 - Treat command line URIs as external r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D20890

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1486,6 +1486,7 @@ window._gBrowser = {
     triggeringPrincipal,
     csp,
     userContextId,
+    fromExternal,
   } = {}) {
     if (!aURIs.length) {
       return;
@@ -1537,6 +1538,9 @@ window._gBrowser = {
       if (!allowInheritPrincipal) {
         flags |= Ci.nsIWebNavigation.LOAD_FLAGS_DISALLOW_INHERIT_PRINCIPAL;
       }
+      if (fromExternal) {
+        flags |= Ci.nsIWebNavigation.LOAD_FLAGS_FROM_EXTERNAL;
+      }
       try {
         browser.loadURI(aURIs[0], {
           flags,
@@ -1559,6 +1563,7 @@ window._gBrowser = {
         triggeringPrincipal,
         bulkOrderedOpen: multiple,
         csp,
+        fromExternal,
       };
       if (newIndex > -1) {
         params.index = newIndex;
@@ -1580,6 +1585,7 @@ window._gBrowser = {
         triggeringPrincipal,
         bulkOrderedOpen: true,
         csp,
+        fromExternal,
       };
       if (targetTabIndex > -1) {
         params.index = ++tabNum;
