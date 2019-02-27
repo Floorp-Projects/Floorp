cde6aa9ebeb90811ee293ec26c688b0d580aec7b
Dorel Luca <dluca@mozilla.com>
2019-02-27 12:00:27 +0200

    Backed out 2 changesets (bug 1335740) for devtools failures. CLOSED TREE
    Backed out changeset 1bddabb7bafb (bug 1335740)
    Backed out changeset e31483efc331 (bug 1335740)
    
    --HG--
    rename : testing/web-platform/meta/mediacapture-streams/MediaDevices-getSupportedConstraints.https.html.ini => testing/web-platform/meta/mediacapture-streams/MediaDevices-getSupportedConstraints.html.ini
    rename : testing/web-platform/tests/mediacapture-streams/MediaDevices-getSupportedConstraints.https.html => testing/web-platform/tests/mediacapture-streams/MediaDevices-getSupportedConstraints.html
    extra : rebase_source : e6fc9bc5fc3eac450e65d6ff38b672b01a4cbe5d

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -2375,9 +2375,6 @@ RefPtr<MediaManager::StreamPromise> MediaManager::GetUserMedia(
   bool privileged =
       isChrome ||
       Preferences::GetBool("media.navigator.permission.disabled", false);
-  bool isSecure = aWindow->IsSecureContext();
-  // Note: isHTTPS is for legacy telemetry only! Use isSecure for security, as
-  // it handles things like https iframes in http pages correctly.
   bool isHTTPS = false;
   bool isHandlingUserInput = EventStateManager::IsHandlingUserInput();
   docURI->SchemeIs("https", &isHTTPS);
@@ -2432,10 +2429,8 @@ RefPtr<MediaManager::StreamPromise> MediaManager::GetUserMedia(
         __func__);
   }
 
-  // Disallow access to null principal pages and http pages (unless pref)
-  if (principal->GetIsNullPrincipal() ||
-      !(isSecure ||
-        Preferences::GetBool("media.getusermedia.insecure.enabled", false))) {
+  // Disallow access to null principal pages.
+  if (principal->GetIsNullPrincipal()) {
     return StreamPromise::CreateAndReject(
         MakeRefPtr<MediaMgrError>(MediaMgrError::Name::NotAllowedError),
         __func__);
@@ -2796,9 +2791,8 @@ RefPtr<MediaManager::StreamPromise> MediaManager::GetUserMedia(
       ->Then(
           GetCurrentThreadSerialEventTarget(), __func__,
           [self, windowID, c, windowListener, sourceListener, askPermission,
-           prefs, isSecure, isHandlingUserInput, callID, principalInfo,
-           isChrome, devices,
-           resistFingerprinting](const char* badConstraint) mutable {
+           prefs, isHTTPS, isHandlingUserInput, callID, principalInfo, isChrome,
+           devices, resistFingerprinting](const char* badConstraint) mutable {
             LOG("GetUserMedia: starting post enumeration promise2 success "
                 "callback!");
 
@@ -2888,7 +2882,7 @@ RefPtr<MediaManager::StreamPromise> MediaManager::GetUserMedia(
                                    callID.BeginReading());
             } else {
               auto req = MakeRefPtr<GetUserMediaRequest>(
-                  window, callID, c, isSecure, isHandlingUserInput);
+                  window, callID, c, isHTTPS, isHandlingUserInput);
               if (!Preferences::GetBool("media.navigator.permission.force") &&
                   array->Length() > 1) {
                 // there is at least 1 pending gUM request
