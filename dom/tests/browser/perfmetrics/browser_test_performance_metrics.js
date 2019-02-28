96cb980e63c14a31f6476b84e307767f4eb3ea61
Brian Hackett <bhackett1024@gmail.com>
2019-02-28 11:52:12 -1000

    Bug 1522246 - Fix browser_test_performance_metrics.js test, r=tarek.
    --HG--
    rename : dom/tests/browser/shared_worker.js => dom/tests/browser/perfmetrics/shared_worker.js
    extra : rebase_source : f9f6b44886fffe48bddcb1907213e04a24718b55

--- dom/tests/browser/perfmetrics/browser_test_performance_metrics.js
+++ dom/tests/browser/perfmetrics/browser_test_performance_metrics.js
@@ -138,9 +138,7 @@ add_task(async function test() {
     Assert.ok(isTopLevel, "example.com as a top level window");
     Assert.ok(aboutMemoryFound, "about:memory");
     Assert.ok(heapUsage > 0, "got some memory value reported");
-    // FIXME bug 1522246. the worker performance improvements in this bug cause
-    // the shared worker to shut down too quickly to report any info.
-    // Assert.ok(sharedWorker, "We got some info from a shared worker");
+    Assert.ok(sharedWorker, "We got some info from a shared worker");
     let numCounters = counterIds.length;
     Assert.ok(numCounters > 5, "This test generated at least " + numCounters + " unique counters");
 
