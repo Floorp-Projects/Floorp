8e8eac15059ddcbdca06b46db6d89ac4bfe92777
Jason Laster <jason.laster.11@gmail.com>
2019-02-28 14:37:27 -0500

    Bug 1531350 - [release 128] [Breakpoints] memoize breakpoint position fetching (#8001). r=dwalsh

---
+++ devtools/client/debugger/new/src/actions/breakpoints/tests/breakpointPositions.spec.js
@@ -0,0 +1,73 @@
+// @flow
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+import {
+  actions,
+  selectors,
+  createStore,
+  makeSource,
+  waitForState
+} from "../../../utils/test-head";
+
+describe("breakpointPositions", () => {
+  it("fetches positions", async () => {
+    const store = createStore({
+      getBreakpointPositions: async () => ({ "9": [1] })
+    });
+
+    const { dispatch, getState } = store;
+    await dispatch(actions.newSource(makeSource("foo")));
+
+    dispatch(actions.setBreakpointPositions("foo"));
+
+    await waitForState(store, state =>
+      selectors.hasBreakpointPositions(state, "foo")
+    );
+
+    expect(
+      selectors.getBreakpointPositionsForSource(getState(), "foo")
+    ).toEqual([
+      {
+        location: { line: 9, column: 1, sourceId: "foo" },
+        generatedLocation: { line: 9, column: 1, sourceId: "foo" }
+      }
+    ]);
+  });
+
+  it("doesn't re-fetch positions", async () => {
+    let resolve = _ => {};
+    let count = 0;
+    const store = createStore({
+      getBreakpointPositions: () =>
+        new Promise(r => {
+          count++;
+          resolve = r;
+        })
+    });
+
+    const { dispatch, getState } = store;
+    await dispatch(actions.newSource(makeSource("foo")));
+
+    dispatch(actions.setBreakpointPositions("foo"));
+    dispatch(actions.setBreakpointPositions("foo"));
+
+    resolve({ "9": [1] });
+    await waitForState(store, state =>
+      selectors.hasBreakpointPositions(state, "foo")
+    );
+
+    expect(
+      selectors.getBreakpointPositionsForSource(getState(), "foo")
+    ).toEqual([
+      {
+        location: { line: 9, column: 1, sourceId: "foo" },
+        generatedLocation: { line: 9, column: 1, sourceId: "foo" }
+      }
+    ]);
+
+    expect(count).toEqual(1);
+  });
+});
