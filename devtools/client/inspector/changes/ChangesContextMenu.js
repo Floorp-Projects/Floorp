bcb6a028325bbaaf3eed793813c8e615113e80db
Razvan Caliman <rcaliman@mozilla.com>
2019-02-28 17:30:02 +0000

    Bug 1529599 - (Part 1) Add Copy Declaration to Changes panel context menu; r=gl
    Adds a new option to the context menu which is visible only when it's invoked on a CSS declaration.
    Builds a string with the property name and value and copies to the clipboard.
    If the declaration is marked as removed, the string is wrapped in a comment block.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21526

--- devtools/client/inspector/changes/ChangesContextMenu.js
+++ devtools/client/inspector/changes/ChangesContextMenu.js
@@ -28,6 +28,7 @@ class ChangesContextMenu {
     // Window object to which the Changes panel belongs to.
     this.window = this.document.defaultView;
 
+    this._onCopyDeclaration = this.view.copyDeclaration.bind(this.view);
     this._onCopyRule = this.view.copyRule.bind(this.view);
     this._onCopySelection = this.view.copySelection.bind(this.view);
     this._onSelectAll = this._onSelectAll.bind(this);
@@ -55,14 +56,22 @@ class ChangesContextMenu {
     });
     menu.append(menuitemCopy);
 
+    const declEl = target.closest(".changes__declaration");
     const ruleEl = target.closest("[data-rule-id]");
     const ruleId = ruleEl ? ruleEl.dataset.ruleId : null;
 
-    if (ruleId) {
+    if (ruleId || declEl) {
       // Copy Rule option
       menu.append(new MenuItem({
         label: getStr("changes.contextmenu.copyRule"),
         click: () => this._onCopyRule(ruleId),
+      }));
+
+      // Copy Declaration option. Visible only if there is a declaration element target.
+      menu.append(new MenuItem({
+        label: getStr("changes.contextmenu.copyDeclaration"),
+        click: () => this._onCopyDeclaration(declEl),
+        visible: !!declEl,
       }));
 
       menu.append(new MenuItem({
