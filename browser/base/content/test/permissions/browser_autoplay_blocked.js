a3deec31bf3536c807f735b9803df34e56a5cb69
Dale Harvey <dale@arandomurl.com>
2019-02-28 20:17:18 +0000

    Bug 1522053 - Copy global permissions into new browsers r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D21279

--- browser/base/content/test/permissions/browser_autoplay_blocked.js
+++ browser/base/content/test/permissions/browser_autoplay_blocked.js
@@ -4,6 +4,9 @@
 
 const AUTOPLAY_PAGE  = getRootDirectory(gTestPath).replace("chrome://mochitests/content", "https://example.com") + "browser_autoplay_blocked.html";
 
+const AUTOPLAY_PREF = "media.autoplay.default";
+const AUTOPLAY_PERM = "autoplay-media";
+
 function openIdentityPopup() {
   let promise = BrowserTestUtils.waitForEvent(gIdentityHandler._identityPopup, "popupshown");
   gIdentityHandler._identityBox.click();
@@ -21,9 +24,24 @@ function autoplayBlockedIcon() {
                                 ".blocked-permission-icon.autoplay-media-icon");
 }
 
+async function blockedIconShown(browser) {
+  // May need to wait for `GloballyAutoplayBlocked` event before showing icon.
+  if (BrowserTestUtils.is_hidden(autoplayBlockedIcon())) {
+    await BrowserTestUtils.waitForEvent(browser, "GloballyAutoplayBlocked");
+  }
+  ok(!BrowserTestUtils.is_hidden(autoplayBlockedIcon()), "Blocked icon is shown");
+}
+
+add_task(async function setup() {
+  registerCleanupFunction(() => {
+    Services.perms.removeAll();
+    Services.prefs.clearUserPref(AUTOPLAY_PREF);
+  });
+});
+
 add_task(async function testMainViewVisible() {
 
-  Services.prefs.setIntPref("media.autoplay.default", Ci.nsIAutoplay.ALLOWED);
+  Services.prefs.setIntPref(AUTOPLAY_PREF, Ci.nsIAutoplay.ALLOWED);
 
   await BrowserTestUtils.withNewTab(AUTOPLAY_PAGE, async function() {
     let permissionsList = document.getElementById("identity-popup-permission-list");
@@ -36,21 +54,17 @@ add_task(async function testMainViewVisible() {
     await closeIdentityPopup();
   });
 
-  Services.prefs.setIntPref("media.autoplay.default", Ci.nsIAutoplay.BLOCKED);
+  Services.prefs.setIntPref(AUTOPLAY_PREF, Ci.nsIAutoplay.BLOCKED);
 
   await BrowserTestUtils.withNewTab(AUTOPLAY_PAGE, async function(browser) {
     let permissionsList = document.getElementById("identity-popup-permission-list");
     let emptyLabel = permissionsList.nextElementSibling.nextElementSibling;
 
-    if (BrowserTestUtils.is_hidden(autoplayBlockedIcon())) {
-      // The block icon would be showed after tab receives `GloballyAutoplayBlocked` event.
-      await BrowserTestUtils.waitForEvent(browser, "GloballyAutoplayBlocked");
-    }
-    ok(!BrowserTestUtils.is_hidden(autoplayBlockedIcon()), "Blocked icon is shown");
+    await blockedIconShown(browser);
 
     await openIdentityPopup();
     ok(BrowserTestUtils.is_hidden(emptyLabel), "List of permissions is not empty");
-    let labelText = SitePermissions.getPermissionLabel("autoplay-media");
+    let labelText = SitePermissions.getPermissionLabel(AUTOPLAY_PERM);
     let labels = permissionsList.querySelectorAll(".identity-popup-permission-label");
     is(labels.length, 1, "One permission visible in main view");
     is(labels[0].textContent, labelText, "Correct value");
@@ -71,10 +85,36 @@ add_task(async function testMainViewVisible() {
     await closeIdentityPopup();
 
     let uri = Services.io.newURI(AUTOPLAY_PAGE);
-    let state = SitePermissions.get(uri, "autoplay-media").state;
+    let state = SitePermissions.get(uri, AUTOPLAY_PERM).state;
     Assert.equal(state, SitePermissions.ALLOW);
   });
 
   Services.perms.removeAll();
-  Services.prefs.clearUserPref("media.autoplay.default");
+});
+
+add_task(async function testGloballyBlockedOnNewWindow() {
+  Services.prefs.setIntPref(AUTOPLAY_PREF, Ci.nsIAutoplay.BLOCKED);
+
+  let uri = Services.io.newURI(AUTOPLAY_PAGE);
+
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, uri.spec);
+  await blockedIconShown(tab.linkedBrowser);
+
+  Assert.deepEqual(SitePermissions.get(uri, AUTOPLAY_PERM, tab.linkedBrowser), {
+    state: SitePermissions.BLOCK,
+    scope: SitePermissions.SCOPE_PERSISTENT,
+  });
+
+  let promiseWin = BrowserTestUtils.waitForNewWindow();
+  gBrowser.replaceTabWithWindow(tab);
+  let win = await promiseWin;
+  tab = win.gBrowser.selectedTab;
+
+  Assert.deepEqual(SitePermissions.get(uri, AUTOPLAY_PERM, tab.linkedBrowser), {
+    state: SitePermissions.BLOCK,
+    scope: SitePermissions.SCOPE_PERSISTENT,
+  });
+
+  SitePermissions.remove(uri, AUTOPLAY_PERM, tab.linkedBrowser);
+  await BrowserTestUtils.closeWindow(win);
 });
