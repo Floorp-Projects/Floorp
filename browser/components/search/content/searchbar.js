ad31e7d17a65cd951183e8fea86ad177eee57986
Mike de Boer <mdeboer@mozilla.com>
2019-03-01 15:51:50 +0000

    Bug 1530086 - Fix the method signature used by searchbar scripts for addEngine(). r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D21706
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/search/content/searchbar.js
+++ browser/components/search/content/searchbar.js
@@ -355,7 +355,7 @@ class MozSearchbar extends MozXULElement {
         this.currentEngine = target.engine;
       } else if (target.classList.contains("addengine-item")) {
         // Select the installed engine if the installation succeeds.
-        Services.search.addEngine(target.getAttribute("uri"), null,
+        Services.search.addEngine(target.getAttribute("uri"),
           target.getAttribute("src"), false).then(engine => this.currentEngine = engine);
       } else {
         return;
