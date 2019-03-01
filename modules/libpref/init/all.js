6769f76db87ca27349a40d6156df13f2c782fdae
Josef Citrine <jcit@pm.me>
2019-03-01 18:49:44 +0000

    Bug 1485264 - Removed dom.event.highrestimestamp.enabled pref r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D21580

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1448,7 +1448,6 @@ pref("privacy.resistFingerprinting.reduceTimerPrecision.jitter", true);
 
 pref("dom.event.contextmenu.enabled",       true);
 pref("dom.event.clipboardevents.enabled",   true);
-pref("dom.event.highrestimestamp.enabled",  true);
 pref("dom.event.coalesce_mouse_move",       true);
 
 pref("javascript.enabled",                  true);
