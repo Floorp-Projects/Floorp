ac3504d5843a8e2f81aa4459450081f6165d3778
Mark Striemer <mstriemer@mozilla.com>
2019-03-01 15:35:04 +0000

    Bug 1525090 - Use AddonListeners in HTML about:addons r=aswan,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D19574

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -1,17 +1,28 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* eslint max-len: ["error", 80] */
 /* exported initialize, hide, show */
 
 "use strict";
 
-const {XPCOMUtils} = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+const {XPCOMUtils} = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm");
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   AddonManager: "resource://gre/modules/AddonManager.jsm",
 });
 
 const PLUGIN_ICON_URL = "chrome://global/skin/plugins/pluginGeneric.svg";
+const PERMISSION_MASKS = {
+  enable: AddonManager.PERM_CAN_ENABLE,
+  disable: AddonManager.PERM_CAN_DISABLE,
+  uninstall: AddonManager.PERM_CAN_UNINSTALL,
+};
+
+function hasPermission(addon, permission) {
+  return !!(addon.permissions & PERMISSION_MASKS[permission]);
+}
 
 let _templates = {};
 
@@ -29,30 +40,44 @@ function importTemplate(name) {
   throw new Error(`Unknown template: ${name}`);
 }
 
-class ListView {
-  constructor({param, root}) {
-    this.type = param;
-    this.root = root;
+/**
+ * A card component for managing an add-on. It should be initialized by setting
+ * the add-on with `setAddon()` before being connected to the document.
+ *
+ *    let card = document.createElement("addon-card");
+ *    card.setAddon(addon);
+ *    document.body.appendChild(card);
+ */
+class AddonCard extends HTMLElement {
+  constructor() {
+    super();
+    this.connected = false;
   }
 
-  async getAddons() {
-    let addons = await AddonManager.getAddonsByTypes([this.type]);
-    addons = addons.filter(addon => !addon.isSystem);
-
-    // Sort by name.
-    addons.sort((a, b) => a.name.localeCompare(b.name));
-    return addons;
-  }
-
-  setEnableLabel(button, disabled) {
-    if (disabled) {
-      document.l10n.setAttributes(button, "enable-addon-button");
-    } else {
-      document.l10n.setAttributes(button, "disable-addon-button");
+  connectedCallback() {
+    if (this.connected) {
+      return;
     }
+    this.connected = true;
+    this.render();
   }
 
-  updateCard(card, addon) {
+  /**
+   * Set the add-on for this card. The card will be populated based on the
+   * add-on when it is connected to the DOM.
+   *
+   * @param {AddonWrapper} addon The add-on to use.
+   */
+  setAddon(addon) {
+    this.addon = addon;
+  }
+
+  /**
+   * Update the card's contents based on the previously set add-on. This should
+   * be called if there has been a change to the add-on.
+   */
+  update() {
+    let {addon, card} = this;
     let icon;
     if (addon.type == "plugin") {
       icon = PLUGIN_ICON_URL;
@@ -62,19 +87,31 @@ class ListView {
     card.querySelector(".addon-icon").src = icon;
     card.querySelector(".addon-name").textContent = addon.name;
     card.querySelector(".addon-description").textContent = addon.description;
+    card.querySelector('[action="remove"]').hidden =
+      !hasPermission(addon, "uninstall");
 
-    this.setEnableLabel(
-      card.querySelector('[action="toggle-disabled"]'), addon.userDisabled);
+    let disableButton = card.querySelector('[action="toggle-disabled"]');
+    let disableAction = addon.userDisabled ? "enable" : "disable";
+    document.l10n.setAttributes(
+      disableButton, `${disableAction}-addon-button`);
+    disableButton.hidden = !hasPermission(addon, disableAction);
   }
 
-  renderAddonCard(addon) {
-    let card = importTemplate("card").firstElementChild;
-    card.setAttribute("addon-id", addon.id);
+  render() {
+    this.textContent = "";
+
+    let {addon} = this;
+    if (!addon) {
+      throw new Error("addon-card must be initialized with setAddon()");
+    }
+
+    this.card = importTemplate("card").firstElementChild;
+    this.setAttribute("addon-id", addon.id);
 
     // Set the contents.
-    this.updateCard(card, addon);
+    this.update();
 
-    card.addEventListener("click", async (e) => {
+    this.addEventListener("click", async (e) => {
       switch (e.target.getAttribute("action")) {
         case "toggle-disabled":
           if (addon.userDisabled) {
@@ -82,58 +119,299 @@ class ListView {
           } else {
             await addon.disable();
           }
-          this.render();
           break;
         case "remove":
           await addon.uninstall();
-          this.render();
           break;
       }
     });
-    return card;
+
+    this.appendChild(this.card);
+  }
+}
+customElements.define("addon-card", AddonCard);
+
+/**
+ * A list view for add-ons of a certain type. It should be initialized with the
+ * type of add-on to render and have section data set before being connected to
+ * the document.
+ *
+ *    let list = document.createElement("addon-list");
+ *    list.type = "plugin";
+ *    list.setSections([{
+ *      headingId: "plugin-section-heading",
+ *      filterFn: addon => !addon.isSystem,
+ *    }]);
+ *    document.body.appendChild(list);
+ */
+class AddonList extends HTMLElement {
+  constructor() {
+    super();
+    this.connected = false;
+    this.sections = [];
   }
 
-  renderSections({disabledFrag, enabledFrag}) {
-    let viewFrag = importTemplate("list");
-    let enabledSection = viewFrag.querySelector('[type="enabled"]');
-    let disabledSection = viewFrag.querySelector('[type="disabled"]');
-
-    // Set the cards or remove the section if empty.
-    let setSection = (section, cards) => {
-      if (cards.children.length > 0) {
-        section.appendChild(cards);
-      } else {
-        section.remove();
-      }
-    };
-
-    setSection(enabledSection, enabledFrag);
-    setSection(disabledSection, disabledFrag);
-
-    return viewFrag;
+  async connectedCallback() {
+    if (this.connected) {
+      return;
+    }
+    this.connected = true;
+    // Register the listener and get the add-ons, these operations should
+    // happpen as close to each other as possible.
+    this.registerListener();
+    // Render the initial view.
+    this.render(await this.getAddons());
   }
 
-  async render() {
-    let addons = await this.getAddons();
+  disconnectedCallback() {
+    // Remove content and stop listening until this is connected again.
+    this.connected = false;
+    this.textContent = "";
+    this.removeListener();
+  }
 
-    let disabledFrag = document.createDocumentFragment();
-    let enabledFrag = document.createDocumentFragment();
+  /**
+   * Configure the sections in the list.
+   *
+   * @param {object[]} sections
+   *        The options for the section. Each entry in the array should have:
+   *          headingId: The fluent id for the section's heading.
+   *          filterFn: A function that determines if an add-on belongs in
+   *                    the section.
+   */
+  setSections(sections) {
+    this.sections = sections.map(section => Object.assign({}, section));
+  }
 
-    // Populate fragments for the enabled and disabled sections.
+  /**
+   * Set the add-on type for this list. This will be used to filter the add-ons
+   * that are displayed.
+   *
+   * @param {string} val The type to filter on.
+   */
+  set type(val) {
+    this.setAttribute("type", val);
+  }
+
+  get type() {
+    return this.getAttribute("type");
+  }
+
+  getSection(index) {
+    return this.sections[index].node;
+  }
+
+  getCards(section) {
+    return section.querySelectorAll("addon-card");
+  }
+
+  getCard(addon) {
+    return this.querySelector(`addon-card[addon-id="${addon.id}"]`);
+  }
+
+  sortByFn(aAddon, bAddon) {
+    return aAddon.name.localeCompare(bAddon.name);
+  }
+
+  async getAddons() {
+    if (!this.type) {
+      throw new Error(`type must be set to find add-ons`);
+    }
+
+    // Find everything matching our type.
+    let addons = await AddonManager.getAddonsByTypes([this.type]);
+
+    // Sort by name.
+    addons.sort(this.sortByFn);
+
+    // Put the add-ons into the sections, an add-on goes in the first section
+    // that it matches the filterFn for. It might not go in any section.
+    let sectionedAddons = this.sections.map(() => []);
     for (let addon of addons) {
-      let card = this.renderAddonCard(addon);
-      if (addon.userDisabled) {
-        disabledFrag.appendChild(card);
-      } else {
-        enabledFrag.appendChild(card);
+      let index = this.sections.findIndex(({filterFn}) => filterFn(addon));
+      if (index != -1) {
+        sectionedAddons[index].push(addon);
       }
     }
 
-    // Put the sections into the main template.
-    let frag = this.renderSections({disabledFrag, enabledFrag});
+    return sectionedAddons;
+  }
 
-    this.root.textContent = "";
-    this.root.appendChild(frag);
+  createSectionHeading(headingIndex) {
+    let {headingId} = this.sections[headingIndex];
+    let heading = document.createElement("h2");
+    heading.classList.add("list-section-heading");
+    document.l10n.setAttributes(heading, headingId);
+    return heading;
+  }
+
+  updateSectionIfEmpty(section) {
+    // The header is added before any add-on cards, so if there's only one
+    // child then it's the header. In that case we should empty out the section.
+    if (section.children.length == 1) {
+      section.textContent = "";
+    }
+  }
+
+  insertCardInto(card, sectionIndex) {
+    let section = this.getSection(sectionIndex);
+    let sectionCards = this.getCards(section);
+
+    // If this is the first card in the section, create the heading.
+    if (sectionCards.length == 0) {
+      section.appendChild(this.createSectionHeading(sectionIndex));
+    }
+
+    // Find where to insert the card.
+    let insertBefore = Array.from(sectionCards).find(
+      otherCard => this.sortByFn(card.addon, otherCard.addon) < 0);
+    // This will append if insertBefore is null.
+    section.insertBefore(card, insertBefore || null);
+  }
+
+  addAddon(addon) {
+    // Only insert add-ons of the right type.
+    if (addon.type != this.type) {
+      this.sendEvent("skip-add", "type-mismatch");
+      return;
+    }
+
+    let insertSection = this.sections.findIndex(
+      ({filterFn}) => filterFn(addon));
+
+    // Don't add the add-on if it doesn't go in a section.
+    if (insertSection == -1) {
+      return;
+    }
+
+    // Create and insert the card.
+    let card = document.createElement("addon-card");
+    card.setAddon(addon);
+    this.insertCardInto(card, insertSection);
+    this.sendEvent("add", {id: addon.id});
+  }
+
+  sendEvent(name, detail) {
+    this.dispatchEvent(new CustomEvent(name, {detail}));
+  }
+
+  removeAddon(addon) {
+    let card = this.getCard(addon);
+    if (card) {
+      let section = card.parentNode;
+      card.remove();
+      this.updateSectionIfEmpty(section);
+      this.sendEvent("remove", {id: addon.id});
+    }
+  }
+
+  updateAddon(addon) {
+    let card = this.getCard(addon);
+    if (card) {
+      let sectionIndex = this.sections.findIndex(s => s.filterFn(addon));
+      if (sectionIndex != -1) {
+        card.update();
+        // Move the card, if needed. This will allow an animation between
+        // page sections and provides clearer events for testing.
+        if (card.parentNode.getAttribute("section") != sectionIndex) {
+          let oldSection = card.parentNode;
+          this.insertCardInto(card, sectionIndex);
+          this.updateSectionIfEmpty(oldSection);
+          this.sendEvent("move", {id: addon.id});
+        } else {
+          this.sendEvent("update", {id: addon.id});
+        }
+      } else {
+        this.removeAddon(addon);
+      }
+    } else {
+      // Add the add-on, this will do nothing if it shouldn't be in the list.
+      this.addAddon(addon);
+    }
+  }
+
+  renderSection(addons, index) {
+    let section = document.createElement("section");
+    section.setAttribute("section", index);
+
+    // Render the heading and add-ons if there are any.
+    if (addons.length > 0) {
+      section.appendChild(this.createSectionHeading(index));
+
+      for (let addon of addons) {
+        let card = document.createElement("addon-card");
+        card.setAddon(addon);
+        section.appendChild(card);
+      }
+    }
+
+    return section;
+  }
+
+  render(sectionedAddons) {
+    this.textContent = "";
+
+    // Render the sections.
+    let frag = document.createDocumentFragment();
+
+    for (let i = 0; i < sectionedAddons.length; i++) {
+      this.sections[i].node = this.renderSection(sectionedAddons[i], i);
+      frag.appendChild(this.sections[i].node);
+    }
+
+    this.appendChild(frag);
+    this.sendEvent("rendered");
+  }
+
+  registerListener() {
+    AddonManager.addAddonListener(this);
+  }
+
+  removeListener() {
+    AddonManager.removeAddonListener(this);
+  }
+
+  onEnabled(addon) {
+    this.updateAddon(addon);
+  }
+
+  onDisabled(addon) {
+    this.updateAddon(addon);
+  }
+
+  onInstalled(addon) {
+    this.addAddon(addon);
+  }
+
+  onUninstalled(addon) {
+    this.removeAddon(addon);
+  }
+}
+customElements.define("addon-list", AddonList);
+
+class ListView {
+  constructor({param, root}) {
+    this.type = param;
+    this.root = root;
+  }
+
+  async render() {
+    let list = document.createElement("addon-list");
+    list.type = this.type;
+    list.setSections([{
+      headingId: "addons-enabled-heading",
+      filterFn: addon => !addon.hidden && addon.isActive,
+    }, {
+      headingId: "addons-disabled-heading",
+      filterFn: addon => !addon.hidden && !addon.isActive,
+    }]);
+
+    await new Promise(resolve => {
+      list.addEventListener("rendered", resolve, {once: true});
+
+      this.root.textContent = "";
+      this.root.appendChild(list);
+    });
   }
 }
 
@@ -145,6 +423,11 @@ let root = null;
  */
 function initialize() {
   root = document.getElementById("main");
+  window.addEventListener("unload", () => {
+    // Clear out the root node so the disconnectedCallback will trigger
+    // properly and all of the custom elements can cleanup.
+    root.textContent = "";
+  }, {once: true});
 }
 
 /**
--- toolkit/mozapps/extensions/test/browser/browser_html_list_view.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_list_view.js
@@ -1,3 +1,5 @@
+/* eslint max-len: ["error", 80] */
+
 let gManagerWindow;
 let gCategoryUtilities;
 
@@ -14,8 +16,30 @@ function closeView() {
   return close_manager(gManagerWindow);
 }
 
+const SECTION_INDEXES = {
+  enabled: 0,
+  disabled: 1,
+};
 function getSection(doc, type) {
-  return doc.querySelector(`.list-section[type="${type}"]`);
+  return doc.querySelector(`section[section="${SECTION_INDEXES[type]}"]`);
+}
+
+function getTestCards(root) {
+  return root.querySelectorAll('addon-card[addon-id$="@mochi.test"]');
+}
+
+function getCardByAddonId(root, id) {
+  return root.querySelector(`addon-card[addon-id="${id}"]`);
+}
+
+function isEmpty(el) {
+  return el.children.length == 0;
+}
+
+function waitForThemeChange(list) {
+  // Wait for two move events. One theme will be enabled and another disabled.
+  let moveCount = 0;
+  return BrowserTestUtils.waitForEvent(list, "move", () => ++moveCount == 2);
 }
 
 add_task(async function enableHtmlViews() {
@@ -23,6 +47,22 @@ add_task(async function enableHtmlViews() {
     set: [["extensions.htmlaboutaddons.enabled", true]],
   });
 });
+
+let extensionsCreated = 0;
+
+function createExtensions(manifestExtras) {
+  return manifestExtras.map(extra => ExtensionTestUtils.loadExtension({
+    manifest: {
+      name: "Test extension",
+      applications: {gecko: {id: `test-${extensionsCreated++}@mochi.test`}},
+      icons: {
+        32: "test-icon.png",
+      },
+      ...extra,
+    },
+    useAddonManager: "temporary",
+  }));
+}
 
 add_task(async function testExtensionList() {
   let extension = ExtensionTestUtils.loadExtension({
@@ -43,12 +83,17 @@ add_task(async function testExtensionList() {
   let win = await loadInitialView("extension");
   let doc = win.document;
 
+  // Find the addon-list to listen for events.
+  let list = doc.querySelector("addon-list");
+
   // There shouldn't be any disabled extensions.
-  is(getSection(doc, "disabled"), null, "The disabled section is hidden");
+  let disabledSection = getSection(doc, "disabled");
+  ok(isEmpty(disabledSection), "The disabled section is empty");
 
   // The loaded extension should be in the enabled list.
   let enabledSection = getSection(doc, "enabled");
-  let card = enabledSection.querySelector('[addon-id="test@mochi.test"]');
+  ok(!isEmpty(enabledSection), "The enabled section isn't empty");
+  let card = getCardByAddonId(enabledSection, "test@mochi.test");
   ok(card, "The card is in the enabled section");
 
   // Check the properties of the card.
@@ -62,15 +107,14 @@ add_task(async function testExtensionList() {
   is(doc.l10n.getAttributes(disableButton).id, "disable-addon-button",
      "The button has the disable label");
 
-  let disabled = TestUtils.waitForCondition(() => getSection(doc, "disabled"));
+  let disabled = BrowserTestUtils.waitForEvent(list, "move");
   disableButton.click();
   await disabled;
+  is(card.parentNode, disabledSection,
+    "The card is now in the disabled section");
 
   // The disable button is now enable.
-  let disabledSection = getSection(doc, "disabled");
-  card = disabledSection.querySelector('[addon-id="test@mochi.test"]');
-  let enableButton = card.querySelector('[action="toggle-disabled"]');
-  is(doc.l10n.getAttributes(enableButton).id, "enable-addon-button",
+  is(doc.l10n.getAttributes(disableButton).id, "enable-addon-button",
      "The button has the enable label");
 
   // Remove the add-on.
@@ -78,12 +122,227 @@ add_task(async function testExtensionList() {
   is(doc.l10n.getAttributes(removeButton).id, "remove-addon-button",
      "The button has the remove label");
 
-  let removed = TestUtils.waitForCondition(() => !getSection(doc, "disabled"));
+  let removed = BrowserTestUtils.waitForEvent(list, "remove");
   removeButton.click();
   await removed;
 
   addon = await AddonManager.getAddonByID("test@mochi.test");
   ok(!addon, "The addon is not longer found");
+
+  await extension.unload();
+  await closeView(win);
+});
+
+add_task(async function testExtensionReordering() {
+  let extensions = createExtensions([
+    {name: "Extension One"},
+    {name: "This is last"},
+    {name: "An extension, is first"},
+  ]);
+
+  await Promise.all(extensions.map(extension => extension.startup()));
+
+  let win = await loadInitialView("extension");
+  let doc = win.document;
+
+  // Get a reference to the addon-list for events.
+  let list = doc.querySelector("addon-list");
+
+  // Find the related cards, they should all have @mochi.test ids.
+  let enabledSection = getSection(doc, "enabled");
+  let cards = getTestCards(enabledSection);
+
+  is(cards.length, 3, "Each extension has an addon-card");
+
+  let order = Array.from(cards).map(card => card.addon.name);
+  Assert.deepEqual(order, [
+    "An extension, is first",
+    "Extension One",
+    "This is last",
+  ], "The add-ons are sorted by name");
+
+  // Disable the second extension.
+  let disabledSection = getSection(doc, "disabled");
+  ok(isEmpty(disabledSection), "The disabled section is initially empty");
+
+  // Disable the add-ons in a different order.
+  let reorderedCards = [cards[1], cards[0], cards[2]];
+  for (let {addon} of reorderedCards) {
+    let moved = BrowserTestUtils.waitForEvent(list, "move");
+    await addon.disable();
+    await moved;
+  }
+
+  order = Array.from(getTestCards(disabledSection))
+    .map(card => card.addon.name);
+  Assert.deepEqual(order, [
+    "An extension, is first",
+    "Extension One",
+    "This is last",
+  ], "The add-ons are sorted by name");
+
+  // All of our installed add-ons are disabled, install a new one.
+  let [newExtension] = createExtensions([{name: "Extension New"}]);
+  let added = BrowserTestUtils.waitForEvent(list, "add");
+  await newExtension.startup();
+  await added;
+
+  let [newCard] = getTestCards(enabledSection);
+  is(newCard.addon.name, "Extension New",
+     "The new add-on is in the enabled list");
+
+  // Enable everything again.
+  for (let {addon} of cards) {
+    let moved = BrowserTestUtils.waitForEvent(list, "move");
+    await addon.enable();
+    await moved;
+  }
+
+  order = Array.from(getTestCards(enabledSection))
+    .map(card => card.addon.name);
+  Assert.deepEqual(order, [
+    "An extension, is first",
+    "Extension New",
+    "Extension One",
+    "This is last",
+  ], "The add-ons are sorted by name");
+
+  // Remove the new extension.
+  let removed = BrowserTestUtils.waitForEvent(list, "remove");
+  await newExtension.unload();
+  await removed;
+  is(newCard.parentNode, null, "The new card has been removed");
+
+  await Promise.all(extensions.map(extension => extension.unload()));
+  await closeView(win);
+});
+
+add_task(async function testThemeList() {
+  let theme = ExtensionTestUtils.loadExtension({
+    manifest: {
+      applications: {gecko: {id: "theme@mochi.test"}},
+      name: "My theme",
+      theme: {},
+    },
+    useAddonManager: "temporary",
+  });
+
+  let win = await loadInitialView("theme");
+  let doc = win.document;
+
+  let list = doc.querySelector("addon-list");
+
+  let cards = getTestCards(list);
+  is(cards.length, 0, "There are no test themes to start");
+
+  let added = BrowserTestUtils.waitForEvent(list, "add");
+  await theme.startup();
+  await added;
+
+  cards = getTestCards(list);
+  is(cards.length, 1, "There is now one custom theme");
+
+  let [card] = cards;
+  is(card.addon.name, "My theme", "The card is for the test theme");
+
+  let enabledSection = getSection(doc, "enabled");
+  let disabledSection = getSection(doc, "disabled");
+
+  is(card.parentNode, enabledSection,
+     "The new theme card is in the enabled section");
+  is(enabledSection.querySelectorAll("addon-card").length,
+     1, "There is one enabled theme");
+
+  let themesChanged = waitForThemeChange(list);
+  card.querySelector('[action="toggle-disabled"]').click();
+  await themesChanged;
+
+  is(card.parentNode, disabledSection,
+     "The card is now in the disabled section");
+  is(enabledSection.querySelectorAll("addon-card").length,
+     1, "There is one enabled theme");
+
+  await theme.unload();
+  await closeView(win);
+});
+
+add_task(async function testBuiltInThemeButtons() {
+  let win = await loadInitialView("theme");
+  let doc = win.document;
+
+  // Find the addon-list to listen for events.
+  let list = doc.querySelector("addon-list");
+  let enabledSection = getSection(doc, "enabled");
+  let disabledSection = getSection(doc, "disabled");
+
+  let defaultTheme = getCardByAddonId(doc, "default-theme@mozilla.org");
+  let darkTheme = getCardByAddonId(doc, "firefox-compact-dark@mozilla.org");
+
+  // Check that themes are in the expected spots.
+  is(defaultTheme.parentNode, enabledSection, "The default theme is enabled");
+  is(darkTheme.parentNode, disabledSection, "The dark theme is disabled");
+
+  // The default theme shouldn't have remove or disable options.
+  let defaultButtons = {
+    toggleDisabled: defaultTheme.querySelector('[action="toggle-disabled"]'),
+    remove: defaultTheme.querySelector('[action="remove"]'),
+  };
+  is(defaultButtons.toggleDisabled.hidden, true, "Disable is hidden");
+  is(defaultButtons.remove.hidden, true, "Remove is hidden");
+
+  // The dark theme should have an enable button, but not remove.
+  let darkButtons = {
+    toggleDisabled: darkTheme.querySelector('[action="toggle-disabled"]'),
+    remove: darkTheme.querySelector('[action="remove"]'),
+  };
+  is(darkButtons.toggleDisabled.hidden, false, "Enable is visible");
+  is(darkButtons.remove.hidden, true, "Remove is hidden");
+
+  // Enable the dark theme and check the buttons again.
+  let themesChanged = waitForThemeChange(list);
+  darkButtons.toggleDisabled.click();
+  await themesChanged;
+
+  // Check the buttons.
+  is(defaultButtons.toggleDisabled.hidden, false, "Enable is visible");
+  is(defaultButtons.remove.hidden, true, "Remove is hidden");
+  is(darkButtons.toggleDisabled.hidden, false, "Disable is visible");
+  is(darkButtons.remove.hidden, true, "Remove is hidden");
+
+  // Disable the dark theme.
+  themesChanged = waitForThemeChange(list);
+  darkButtons.toggleDisabled.click();
+  await themesChanged;
+
+  // The themes are back to their starting posititons.
+  is(defaultTheme.parentNode, enabledSection, "Default is enabled");
+  is(darkTheme.parentNode, disabledSection, "Dark is disabled");
+
+  await closeView(win);
+});
+
+add_task(async function testOnlyTypeIsShown() {
+  let win = await loadInitialView("theme");
+  let doc = win.document;
+
+  // Find the addon-list to listen for events.
+  let list = doc.querySelector("addon-list");
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      name: "Test extension",
+      applications: {gecko: {id: "test@mochi.test"}},
+    },
+    useAddonManager: "temporary",
+  });
+
+  let skipped = BrowserTestUtils.waitForEvent(
+    list, "skip-add", (e) => e.detail == "type-mismatch");
+  await extension.startup();
+  await skipped;
+
+  let cards = getTestCards(list);
+  is(cards.length, 0, "There are no test extension cards");
 
   await extension.unload();
   await closeView(win);
