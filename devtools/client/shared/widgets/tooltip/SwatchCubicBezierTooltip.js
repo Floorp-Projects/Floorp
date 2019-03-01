bc4c2f206591ea14f19a1078a37e78c17a2f4809
paarmita <paarmita1998@gmail.com>
2019-03-01 15:19:11 +0000

    Bug 1530275- Replace defer usage with async/await in SwatchCubicBezierTooltip.js; r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D21658

--- devtools/client/shared/widgets/tooltip/SwatchCubicBezierTooltip.js
+++ devtools/client/shared/widgets/tooltip/SwatchCubicBezierTooltip.js
@@ -4,7 +4,6 @@
 
 "use strict";
 
-const defer = require("devtools/shared/defer");
 const {CubicBezierWidget} = require("devtools/client/shared/widgets/CubicBezierWidget");
 const SwatchBasedEditorTooltip = require("devtools/client/shared/widgets/tooltip/SwatchBasedEditorTooltip");
 
@@ -39,7 +38,7 @@ class SwatchCubicBezierTooltip extends SwatchBasedEditorTooltip {
    * the instance of the widget
    */
 
-  setCubicBezierContent(bezier) {
+  async setCubicBezierContent(bezier) {
     const { doc } = this.tooltip;
     this.tooltip.panel.innerHTML = "";
 
@@ -49,16 +48,8 @@ class SwatchCubicBezierTooltip extends SwatchBasedEditorTooltip {
     this.tooltip.panel.appendChild(container);
     this.tooltip.setContentSize({ width: 510, height: 370 });
 
-    const def = defer();
-
-    // Wait for the tooltip to be shown before calling instanciating the widget
-    // as it expect its DOM elements to be visible.
-    this.tooltip.once("shown", () => {
-      const widget = new CubicBezierWidget(container, bezier);
-      def.resolve(widget);
-    });
-
-    return def.promise;
+    await this.tooltip.once("shown");
+    return new CubicBezierWidget(container, bezier);
   }
 
   /**
