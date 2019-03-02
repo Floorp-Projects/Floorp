879e7658df51947494e049aa2c503789e98364f0
Mark Banner <standard8@mozilla.com>
2019-03-02 08:40:02 +0000

    Bug 1531347 - Port autoFill tests to QuantumBar. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D21710

--- browser/components/urlbar/tests/browser/browser_autoFill_backspaced.js
+++ browser/components/urlbar/tests/browser/browser_autoFill_backspaced.js
@@ -1,30 +1,36 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 /* This test ensures that backspacing autoFilled values still allows to
  * confirm the remaining value.
  */
 
+"use strict";
+
 async function test_autocomplete(data) {
-  let {desc, typed, autofilled, modified, keys, action, onAutoFill} = data;
+  let {desc, typed, autofilled, modified, keys, type, onAutoFill} = data;
   info(desc);
 
   await promiseAutocompleteResultPopup(typed);
-  is(gURLBar.textValue, autofilled, "autofilled value is as expected");
+  Assert.equal(gURLBar.textValue, autofilled, "autofilled value is as expected");
   if (onAutoFill)
     onAutoFill();
 
   keys.forEach(key => EventUtils.synthesizeKey(key));
 
-  is(gURLBar.textValue, modified, "backspaced value is as expected");
+  Assert.equal(gURLBar.textValue, modified, "backspaced value is as expected");
 
   await promiseSearchComplete();
 
-  ok(gURLBar.popup.richlistbox.itemChildren.length > 0, "Should get at least 1 result");
-  let result = gURLBar.popup.richlistbox.itemChildren[0];
-  let type = result.getAttribute("type");
-  let types = type.split(/\s+/);
-  ok(types.includes(action), `The type attribute "${type}" includes the expected action "${action}"`);
+  Assert.greater(UrlbarTestUtils.getResultCount(window), 0,
+    "Should get at least 1 result");
 
-  gURLBar.popup.hidePopup();
-  await promisePopupHidden(gURLBar.popup);
+  let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
+
+  Assert.equal(result.type, type,
+    "Should have the correct result type");
+
+  await UrlbarTestUtils.promisePopupClose(window);
   gURLBar.blur();
 }
 
@@ -47,14 +53,14 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "exam",
                             keys: ["KEY_Delete"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                           });
   await test_autocomplete({ desc: "DELETE the final slash should visit",
                             typed: "example.com",
                             autofilled: "example.com/",
                             modified: "example.com",
                             keys: ["KEY_Delete"],
-                            action: "visiturl",
+                            type: UrlbarUtils.RESULT_TYPE.URL,
                           });
 
   await test_autocomplete({ desc: "BACK_SPACE the autofilled part should search",
@@ -62,14 +68,14 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "exam",
                             keys: ["KEY_Backspace"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                           });
   await test_autocomplete({ desc: "BACK_SPACE the final slash should visit",
                             typed: "example.com",
                             autofilled: "example.com/",
                             modified: "example.com",
                             keys: ["KEY_Backspace"],
-                            action: "visiturl",
+                            type: UrlbarUtils.RESULT_TYPE.URL,
                           });
 
   await test_autocomplete({ desc: "DELETE the autofilled part, then BACK_SPACE, should search",
@@ -77,14 +83,14 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "exa",
                             keys: ["KEY_Delete", "KEY_Backspace"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                           });
   await test_autocomplete({ desc: "DELETE the final slash, then BACK_SPACE, should search",
                             typed: "example.com",
                             autofilled: "example.com/",
                             modified: "example.co",
                             keys: ["KEY_Delete", "KEY_Backspace"],
-                            action: "visiturl",
+                            type: UrlbarUtils.RESULT_TYPE.URL,
                           });
 
   await test_autocomplete({ desc: "BACK_SPACE the autofilled part, then BACK_SPACE, should search",
@@ -92,14 +98,14 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "exa",
                             keys: ["KEY_Backspace", "KEY_Backspace"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                           });
   await test_autocomplete({ desc: "BACK_SPACE the final slash, then BACK_SPACE, should search",
                             typed: "example.com",
                             autofilled: "example.com/",
                             modified: "example.co",
                             keys: ["KEY_Backspace", "KEY_Backspace"],
-                            action: "visiturl",
+                            type: UrlbarUtils.RESULT_TYPE.URL,
                           });
 
   await test_autocomplete({ desc: "BACK_SPACE after blur should search",
@@ -107,7 +113,7 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "e",
                             keys: ["KEY_Backspace"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                             onAutoFill: () => {
                               gURLBar.blur();
                               gURLBar.focus();
@@ -120,7 +126,7 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "e",
                             keys: ["KEY_Delete"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                             onAutoFill: () => {
                               gURLBar.blur();
                               gURLBar.focus();
@@ -133,7 +139,7 @@ add_task(async function() {
                             autofilled: "example.com/",
                             modified: "e",
                             keys: ["KEY_Backspace", "KEY_Backspace"],
-                            action: "searchengine",
+                            type: UrlbarUtils.RESULT_TYPE.SEARCH,
                             onAutoFill: () => {
                               gURLBar.blur();
                               gURLBar.focus();
