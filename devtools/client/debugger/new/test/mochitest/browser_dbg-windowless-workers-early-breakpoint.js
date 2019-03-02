a672ed074d65aa0c0771efcf96b82fbe6e903259
Jason Laster <jlaster@mozilla.com>
2019-03-02 03:14:59 +0000

    Bug 1530056 - fix browser_dbg-windowless-workers.js intermittent. r=bhackett
    Differential Revision: https://phabricator.services.mozilla.com/D21800

--- devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers-early-breakpoint.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-windowless-workers-early-breakpoint.js
@@ -9,6 +9,11 @@ add_task(async function() {
 
   const workerSource = findSource(dbg, "simple-worker.js");
 
+  // NOTE: by default we do not wait on worker
+  // commands to complete because the thread could be
+  // shutting down.
+  dbg.client.waitForWorkers(true);
+
   await addBreakpoint(dbg, workerSource, 1);
   invokeInTab("startWorker");
   await waitForPaused(dbg, "simple-worker.js");
