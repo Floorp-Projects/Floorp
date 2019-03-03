9b7efa3398ae67febe2fe9c8fe550bc09983d98f
Dzmitry Malyshau <dmalyshau@mozilla.com>
2019-03-03 21:14:49 +0000

    Bug 1531930 - Interneration refactoring r=gw
    Remove the intern_types module in favor of the associated Internable types that we already have.
    The only bit of magic I had to do is around serialization bounds, and it's nicely isolated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21797

---
+++ gfx/wr/webrender/src/prim_store/interned.rs
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// list of all interned primitives to match enumerate_interners!
+
+pub use prim_store::borders::{ImageBorder, NormalBorderPrim};
+pub use prim_store::image::{Image, YuvImage};
+pub use prim_store::line_dec::{LineDecoration};
+pub use prim_store::gradient::{LinearGradient, RadialGradient};
+pub use prim_store::picture::Picture;
+pub use prim_store::text_run::TextRun;
