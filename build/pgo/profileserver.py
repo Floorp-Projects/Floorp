be960187e192bc95f83acfde61183208a4b410fd
Andreea Pavel <apavel@mozilla.com>
2019-03-05 01:36:59 +0200

    Backed out 3 changesets (bug 1524992) for valgrind failures on a CLOSED TREE
    Backed out changeset face9d1e8868 (bug 1524992)
    Backed out changeset ae1e20a595c4 (bug 1524992)
    Backed out changeset b0b9322c7a59 (bug 1524992)

--- build/pgo/profileserver.py
+++ build/pgo/profileserver.py
@@ -6,22 +6,23 @@
 
 import json
 import os
+import sys
 
 from buildconfig import substs
 from mozbuild.base import MozbuildObject
 from mozfile import TemporaryDirectory
 from mozhttpd import MozHttpd
-from mozprofile import Preferences
-from mozrunner import CLI
+from mozprofile import FirefoxProfile, Preferences
+from mozprofile.permissions import ServerLocations
+from mozrunner import FirefoxRunner, CLI
 from six import string_types
-
-from marionette_driver.marionette import Marionette
 
 PORT = 8888
 
 PATH_MAPPINGS = {
     '/js-input/webkit/PerformanceTests': 'third_party/webkit/PerformanceTests',
 }
+
 
 if __name__ == '__main__':
     cli = CLI()
@@ -38,11 +39,15 @@ if __name__ == '__main__':
         k: os.path.join(build.topsrcdir, v)
         for k, v in PATH_MAPPINGS.items()
     }
-    httpd = MozHttpd(
-        port=PORT,
-        docroot=os.path.join(build.topsrcdir, "build", "pgo"),
-        path_mappings=path_mappings)
+    httpd = MozHttpd(port=PORT,
+                     docroot=os.path.join(build.topsrcdir, "build", "pgo"),
+                     path_mappings=path_mappings)
     httpd.start(block=False)
+
+    locations = ServerLocations()
+    locations.add_host(host='127.0.0.1',
+                       port=PORT,
+                       options='primary,privileged')
 
     with TemporaryDirectory() as profilePath:
         # TODO: refactor this into mozprofile
@@ -50,26 +55,30 @@ if __name__ == '__main__':
         with open(os.path.join(profile_data_dir, 'profiles.json'), 'r') as fh:
             base_profiles = json.load(fh)['profileserver']
 
-        prefpaths = [
-            os.path.join(profile_data_dir, profile, 'user.js')
-            for profile in base_profiles
-        ]
+        prefpaths = [os.path.join(profile_data_dir, profile, 'user.js')
+                     for profile in base_profiles]
 
         prefs = {}
         for path in prefpaths:
             prefs.update(Preferences.read_prefs(path))
 
-        interpolation = {
-            "server": "%s:%d" % httpd.httpd.server_address,
-            "OOP": "false"
-        }
+        interpolation = {"server": "%s:%d" % httpd.httpd.server_address,
+                         "OOP": "false"}
         for k, v in prefs.items():
             if isinstance(v, string_types):
                 v = v.format(**interpolation)
             prefs[k] = Preferences.cast(v)
 
-        env = os.environ
-        env["XPCOM_DEBUG_BREAK"] = "1"
+        profile = FirefoxProfile(profile=profilePath,
+                                 preferences=prefs,
+                                 addons=[os.path.join(
+                                     build.topsrcdir, 'tools', 'quitter',
+                                     'quitter@mozilla.org.xpi')],
+                                 locations=locations)
+
+        env = os.environ.copy()
+        env["MOZ_CRASHREPORTER_NO_REPORT"] = "1"
+        env["XPCOM_DEBUG_BREAK"] = "warn"
 
         # For VC12+, make sure we can find the right bitness of pgort1x0.dll
         if not substs.get('HAVE_64BIT_BUILD'):
@@ -85,35 +94,51 @@ if __name__ == '__main__':
         # Add MOZ_OBJDIR to the env so that cygprofile.cpp can use it.
         env["MOZ_OBJDIR"] = build.topobjdir
 
-        jarlog = env.get("JARLOG_FILE")
-        if jarlog:
-            abs_jarlog = os.path.abspath(jarlog)
-            env["MOZ_JAR_LOG_FILE"] = abs_jarlog
-            print("jarlog: %s" % abs_jarlog)
-
         # Write to an output file if we're running in automation
-        process_args = []
-        logfile = None
+        process_args = {}
         if 'UPLOAD_PATH' in env:
-            logfile = os.path.join(env['UPLOAD_PATH'], 'profile-run.log')
+            process_args['logfile'] = os.path.join(env['UPLOAD_PATH'], 'profile-run-1.log')
 
         # Run Firefox a first time to initialize its profile
-        driver = Marionette(bin=binary, prefs=prefs, gecko_log=logfile)
-        driver.start_session()
-        driver.restart(in_app=True)
+        runner = FirefoxRunner(profile=profile,
+                               binary=binary,
+                               cmdargs=['data:text/html,<script>Quitter.quit()</script>'],
+                               env=env,
+                               process_args=process_args)
+        runner.start()
+        ret = runner.wait()
+        if ret:
+            print("Firefox exited with code %d during profile initialization"
+                  % ret)
+            logfile = process_args.get('logfile')
+            if logfile:
+                print("Firefox output (%s):" % logfile)
+                with open(logfile) as f:
+                    print(f.read())
+            httpd.stop()
+            sys.exit(ret)
 
-        # Now generate the profile and wait for it to complete
-        driver.navigate("http://localhost:%d/index.html" % PORT)
-        driver.execute_async_script(
-            '''
-            const [resolve] = arguments;
-            window.addEventListener('message', event => {
-                if (event.data === 'quit') {
-                    resolve();
-                }
-            });
-            ''',
-            script_timeout=999999999)
+        jarlog = os.getenv("JARLOG_FILE")
+        if jarlog:
+            env["MOZ_JAR_LOG_FILE"] = os.path.abspath(jarlog)
+            print("jarlog: %s" % env["MOZ_JAR_LOG_FILE"])
 
-        driver.quit(in_app=True)
+        if 'UPLOAD_PATH' in env:
+            process_args['logfile'] = os.path.join(env['UPLOAD_PATH'], 'profile-run-2.log')
+        cmdargs = ["http://localhost:%d/index.html" % PORT]
+        runner = FirefoxRunner(profile=profile,
+                               binary=binary,
+                               cmdargs=cmdargs,
+                               env=env,
+                               process_args=process_args)
+        runner.start(debug_args=debug_args, interactive=interactive)
+        ret = runner.wait()
         httpd.stop()
+        if ret:
+            print("Firefox exited with code %d during profiling" % ret)
+            logfile = process_args.get('logfile')
+            if logfile:
+                print("Firefox output (%s):" % logfile)
+                with open(logfile) as f:
+                    print(f.read())
+            sys.exit(ret)
