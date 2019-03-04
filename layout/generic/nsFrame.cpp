3c1ee0bb2a7f841e6c9f99347fb718cb536c8552
Daniel Holbert <dholbert@cs.stanford.edu>
2019-03-04 19:24:01 +0000

    Bug 1531025 part 2: Skip an unnecessary border-radius calculation if the radii are 0 (to avoid division by 0). r=boris
    This patch avoids a potential division by 0 (one that's unlikely to be
    triggered by real content), for correctness and robustness.
    
    This patch isn't really changing the logic, because the newly-guarded code is
    already guarded by a "length < sum" check, and "length" is expected to be
    nonnegative [1], which means "sum" would already have been nonzero in cases
    where the existing strict less-than comparison returned true.
    
    [1] One special case where we can have negative `length` values here:
    if this is an outline with an `outline-offset` that is sufficiently negative
    to entirely cancel out the outlined element's border-box size, then that can
    produce a negative `length` (i.e. the outline box will have a negative size,
    conceptually). In that scenario, we were previously dividing by 0 here and
    working with bogus huge/infinite negative values as a result, but we should
    not do that.
    
    Depends on D21578
    
    Differential Revision: https://phabricator.services.mozilla.com/D21579

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -1715,11 +1715,10 @@ bool nsIFrame::ComputeBorderRadii(const BorderRadius& aBorderRadius,
     nscoord sum = aRadii[hc1] + aRadii[hc2];
     if (sum) {
       haveRadius = true;
-    }
-
-    // avoid floating point division in the normal case
-    if (length < sum) {
-      ratio = std::min(ratio, double(length) / sum);
+      // avoid floating point division in the normal case
+      if (length < sum) {
+        ratio = std::min(ratio, double(length) / sum);
+      }
     }
   }
   if (ratio < 1.0) {
