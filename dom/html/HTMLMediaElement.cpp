59d36a909fb72571588031a09cd3e3d139b4739e
Alastor Wu <alwu@mozilla.com>
2019-03-04 17:25:52 +0000

    Bug 1526355 - part1 : notify the front end side to show the blocking icon if the site is still being blocked after it's back from the bfcache. r=cpearce a=lizzard
    In order to display blocking icon when the document comes back from the bfcache, we have to notify front end what's the current blocking status.
    
    As the front end side would clear blocking autoplay information when nagivation occurs, and the media might not invoke the play again when they comes back from the bfcache.
    
    Therefore, we should notify front end side that the site is still being blocked, and we should show blocking icon for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21582

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -3670,6 +3670,7 @@ void HTMLMediaElement::DispatchEventsWhenPlayWasNotAllowed() {
 #endif
   OwnerDoc()->MaybeNotifyAutoplayBlocked();
   ReportToConsole(nsIScriptError::warningFlag, "BlockAutoplayError");
+  mHasEverBeenBlockedForAutoplay = true;
 }
 
 void HTMLMediaElement::PlayInternal(bool aHandlingUserInput) {
@@ -5898,6 +5899,14 @@ void HTMLMediaElement::SuspendOrResumeElement(bool aPauseElement,
       if (mEventDeliveryPaused) {
         mEventDeliveryPaused = false;
         DispatchPendingMediaEvents();
+      }
+      // If the media element has been blocked and isn't still allowed to play
+      // when it comes back from the bfcache, we would notify front end to show
+      // the blocking icon in order to inform user that the site is still being
+      // blocked.
+      if (mHasEverBeenBlockedForAutoplay &&
+          !AutoplayPolicy::IsAllowedToPlay(*this)) {
+        OwnerDoc()->MaybeNotifyAutoplayBlocked();
       }
     }
   }
