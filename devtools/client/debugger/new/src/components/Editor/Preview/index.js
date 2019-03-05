fc3218962016a6e3f1bffa1df280f6fd3b47d6e4
David Walsh <dwalsh@mozilla.com>
2019-03-05 19:28:27 +0000

    Bug 1532397 - Fix multiple highlight issues in debugger r=jlast
    Replaces previous commit which was backed out for flow issue
    
    Differential Revision: https://phabricator.services.mozilla.com/D22142
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/debugger/new/src/components/Editor/Preview/index.js
+++ devtools/client/debugger/new/src/components/Editor/Preview/index.js
@@ -49,11 +49,14 @@ function inPopup(e) {
 }
 
 function getElementFromPos(pos: DOMRect) {
-  // $FlowIgnore
-  return document.elementFromPoint(
-    pos.x + pos.width / 2,
-    pos.y + pos.height / 2
-  );
+  // We need to use element*s*AtPoint because the tooltip overlays
+  // the token and thus an undesirable element may be returned
+  const elementsAtPoint = [
+    // $FlowIgnore
+    ...document.elementsFromPoint(pos.x + pos.width / 2, pos.y + pos.height / 2)
+  ];
+
+  return elementsAtPoint.find(el => el.className.startsWith("cm-"));
 }
 
 class Preview extends PureComponent<Props, State> {
