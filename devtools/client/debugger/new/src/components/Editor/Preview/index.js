edd8d7fc40e4e706d08c56a6f1465c441059e6a2
Noemi Erli <nerli@mozilla.com>
2019-03-05 19:04:35 +0200

    Backed out changeset ed389b1ea7da (bug 1532397) for debugger failure CLOSED TREE

--- devtools/client/debugger/new/src/components/Editor/Preview/index.js
+++ devtools/client/debugger/new/src/components/Editor/Preview/index.js
@@ -50,14 +50,10 @@ function inPopup(e) {
 
 function getElementFromPos(pos: DOMRect) {
   // $FlowIgnore
-  // We need to use element*s*AtPoint because the tooltip overlays
-  // the token and thus an undesirable element may be returned
-  const elementsAtPoint = [...document.elementsFromPoint(
+  return document.elementFromPoint(
     pos.x + pos.width / 2,
     pos.y + pos.height / 2
-  )];
-
-  return elementsAtPoint.find(el => el.className.startsWith("cm-"));
+  );
 }
 
 class Preview extends PureComponent<Props, State> {
