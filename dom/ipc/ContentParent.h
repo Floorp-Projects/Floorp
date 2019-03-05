48f7ebaf7a05b46d11cdb821ce0e6c94e4e106ca
Nika Layzell <nika@thelayzells.com>
2019-03-05 17:33:52 +0000

    Bug 1530550 - Synchronize mOpener and mIsActivatedByUserGesture with SYNCED_BC_FIELD, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D21134

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -618,9 +618,6 @@ class ContentParent final : public PContentParent,
   mozilla::ipc::IPCResult RecvDetachBrowsingContext(BrowsingContext* aContext,
                                                     bool aMoveToBFCache);
 
-  mozilla::ipc::IPCResult RecvSetOpenerBrowsingContext(
-      BrowsingContext* aContext, BrowsingContext* aOpener);
-
   mozilla::ipc::IPCResult RecvWindowClose(BrowsingContext* aContext,
                                           bool aTrustedCaller);
   mozilla::ipc::IPCResult RecvWindowFocus(BrowsingContext* aContext);
@@ -628,9 +625,6 @@ class ContentParent final : public PContentParent,
   mozilla::ipc::IPCResult RecvWindowPostMessage(
       BrowsingContext* aContext, const ClonedMessageData& aMessage,
       const PostMessageData& aData);
-
-  mozilla::ipc::IPCResult RecvSetUserGestureActivation(
-      BrowsingContext* aContext, bool aNewValue);
 
   FORWARD_SHMEM_ALLOCATOR_TO(PContentParent)
 
