2f0f64f3fbc08aefde12ab7e4730b29f43b52c3d
Dave Townsend <dtownsend@oxymoronical.com>
2019-03-05 12:51:44 -0800

    Bug 1529879: Block changing the profile list when another process has changed it. r=froydnj,Gijs,flod
    On startup we record the size and modified time of the profile lists. If
    changed we refuse to flush any new changes to disk. Also adds a getter to check
    if they've changed so the UI can do something sensible.
    
    All attempts to flush are now checked for success. In some cases in early
    startup the failure mode isn't great, we just quit startup. The assumption
    though is that it's extremely unlikely that the files will have changed on disk
    in the time between when they are read and when profile selection occurs, likely
    less than a second later.
    
    The profile reset flow is changed to only delete the old profile and flush once
    all the migration has completed, so if something fails the user gets back to
    their old profile.
    
    In testing I ended up having to fix bug 1522584 so background file deletions on
    a background thread are safer.
    
    I haven't implemented any UI tests right now since making modifications to the
    profiles means modifying the actual user's profiles which I'm not keen to do.
    See bug 1539868.
    
    Differential Revision: https://phabricator.services.mozilla.com/D25278
    
    --HG--
    extra : rebase_source : b9fb01c5f2faaf7d534800b700bb02b8c88af023
    extra : source : ad5ac4d5c8f7240809a205be2960924813f1e705

---
+++ toolkit/profile/xpcshell/test_conflict_installs.js
@@ -0,0 +1,52 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/*
+ * Tests that the profile service refuses to flush when the install.ini file
+ * has been modified.
+ */
+
+function check_unchanged(service) {
+  Assert.ok(!service.isListOutdated, "Should not have detected a modification.");
+  try {
+    service.flush();
+    Assert.ok(true, "Should have flushed.");
+  } catch (e) {
+    Assert.ok(false, "Should have succeeded flushing.");
+  }
+}
+
+function check_outdated(service) {
+  Assert.ok(service.isListOutdated, "Should have detected a modification.");
+  try {
+    service.flush();
+    Assert.ok(false, "Should have failed to flush.");
+  } catch (e) {
+    Assert.equal(e.result, Cr.NS_ERROR_DATABASE_CHANGED, "Should have refused to flush.");
+  }
+}
+
+add_task(async () => {
+  let service = getProfileService();
+
+  Assert.ok(!service.isListOutdated, "Should not be modified yet.");
+
+  let installsini = gDataHome.clone();
+  installsini.append("installs.ini");
+
+  Assert.ok(!installsini.exists(), "File should not exist yet.");
+  installsini.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0o644);
+  check_outdated(service);
+
+  installsini.remove(false);
+  // We have to do profile selection to actually have any install data.
+  selectStartupProfile();
+  check_unchanged(service);
+
+  let oldTime = installsini.lastModifiedTime;
+  installsini.lastModifiedTime = oldTime - 10000;
+  check_outdated(service);
+
+  // We can't reset the modification time back to exactly what it was, so I
+  // guess we can't do much more here :(
+});
