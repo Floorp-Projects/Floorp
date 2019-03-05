cfb1da8b20d92014bea3f0edf8dd1cc1ed62d4f2
Brindusan Cristian <cbrindusan@mozilla.com>
2019-03-06 00:31:41 +0200

    Backed out changeset ff14d3c64e53 (bug 1532414) for buid bustages at WebGLContextExtensions.cpp. CLOSED TREE

--- dom/canvas/WebGLContextExtensions.cpp
+++ dom/canvas/WebGLContextExtensions.cpp
@@ -107,15 +107,6 @@ bool WebGLContext::IsExtensionSupported(dom::CallerType callerType,
 bool WebGLContext::IsExtensionSupported(WebGLExtensionID ext) const {
   if (mDisableExtensions) return false;
 
-  bool shouldResistFingerprinting =
-      mCanvasElement ?
-                     // If we're constructed from a canvas element
-          nsContentUtils::ShouldResistFingerprinting(GetOwnerDoc())
-                     :
-                     // If we're constructed from an offscreen canvas
-          nsContentUtils::ShouldResistFingerprinting(
-              mOffscreenCanvas->GetOwnerGlobal()->PrincipalOrNull());
-
   switch (ext) {
     // In alphabetical order
     // ANGLE_
@@ -210,10 +201,10 @@ bool WebGLContext::IsExtensionSupported(WebGLExtensionID ext) const {
 
     case WebGLExtensionID::WEBGL_debug_renderer_info:
       return Preferences::GetBool("webgl.enable-debug-renderer-info", false) &&
-             !shouldResistFingerprinting;
+             !nsContentUtils::ShouldResistFingerprinting();
 
     case WebGLExtensionID::WEBGL_debug_shaders:
-      return !shouldResistFingerprinting;
+      return !nsContentUtils::ShouldResistFingerprinting();
 
     case WebGLExtensionID::WEBGL_depth_texture:
       return WebGLExtensionDepthTexture::IsSupported(this);
