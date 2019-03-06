8f63d37f9d2f559669d9ec28565ecc14c56bda5e
Ian Moody <moz-ian@perix.co.uk>
2019-03-06 22:03:48 +0000

    Bug 1529363 - Rewrite browser_420786.js test to use add_task and BTU.withNewTab. r=paolo
    Currently the test fails since it throws "TypeError: content is null".  This
    hasn't been noticed since this test only runs when gsettings is absent and
    presumably all the test machines have it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D20545

--- browser/components/shell/test/browser_420786.js
+++ browser/components/shell/test/browser_420786.js
@@ -3,84 +3,76 @@ const DG_IMAGE_KEY = DG_BACKGROUND + "/picture_filename";
 const DG_OPTION_KEY = DG_BACKGROUND + "/picture_options";
 const DG_DRAW_BG_KEY = DG_BACKGROUND + "/draw_background";
 
-function onPageLoad() {
-  gBrowser.selectedBrowser.removeEventListener("load", onPageLoad, true);
+add_task(async function() {
+  await BrowserTestUtils.withNewTab({
+    gBrowser,
+    url: "about:logo",
+  }, (browser) => {
+    try {
+      // If GSettings is available, then the GConf tests
+      // will fail
+      Cc["@mozilla.org/gsettings-service;1"].
+        getService(Ci.nsIGSettingsService).
+        getCollectionForSchema("org.gnome.desktop.background");
+      todo(false, "This test doesn't work when GSettings is available");
+      return;
+    } catch (e) { }
 
-  var brandName = Services.strings.createBundle("chrome://branding/locale/brand.properties").
-                  GetStringFromName("brandShortName");
+    var brandName = Services.strings.createBundle("chrome://branding/locale/brand.properties").
+                    GetStringFromName("brandShortName");
 
-  var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
-               getService(Ci.nsIDirectoryServiceProvider);
-  var homeDir = dirSvc.getFile("Home", {});
+    var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
+                 getService(Ci.nsIDirectoryServiceProvider);
+    var homeDir = dirSvc.getFile("Home", {});
 
-  var wpFile = homeDir.clone();
-  wpFile.append(brandName + "_wallpaper.png");
+    var wpFile = homeDir.clone();
+    wpFile.append(brandName + "_wallpaper.png");
 
-  // Backup the existing wallpaper so that this test doesn't change the user's
-  // settings.
-  var wpFileBackup = homeDir.clone();
-  wpFileBackup.append(brandName + "_wallpaper.png.backup");
+    // Backup the existing wallpaper so that this test doesn't change the user's
+    // settings.
+    var wpFileBackup = homeDir.clone();
+    wpFileBackup.append(brandName + "_wallpaper.png.backup");
 
-  if (wpFileBackup.exists())
-    wpFileBackup.remove(false);
+    if (wpFileBackup.exists())
+      wpFileBackup.remove(false);
 
-  if (wpFile.exists())
-    wpFile.copyTo(null, wpFileBackup.leafName);
+    if (wpFile.exists())
+      wpFile.copyTo(null, wpFileBackup.leafName);
 
-  var shell = Cc["@mozilla.org/browser/shell-service;1"].
-              getService(Ci.nsIShellService);
-  var gconf = Cc["@mozilla.org/gnome-gconf-service;1"].
-              getService(Ci.nsIGConfService);
+    var shell = Cc["@mozilla.org/browser/shell-service;1"].
+                getService(Ci.nsIShellService);
+    var gconf = Cc["@mozilla.org/gnome-gconf-service;1"].
+                getService(Ci.nsIGConfService);
 
-  var prevImageKey = gconf.getString(DG_IMAGE_KEY);
-  var prevOptionKey = gconf.getString(DG_OPTION_KEY);
-  var prevDrawBgKey = gconf.getBool(DG_DRAW_BG_KEY);
+    var prevImageKey = gconf.getString(DG_IMAGE_KEY);
+    var prevOptionKey = gconf.getString(DG_OPTION_KEY);
+    var prevDrawBgKey = gconf.getBool(DG_DRAW_BG_KEY);
 
-  var image = content.document.images[0];
+    var image = content.document.images[0];
 
-  function checkWallpaper(position, expectedGConfPosition) {
-    shell.setDesktopBackground(image, position, "");
-    ok(wpFile.exists(), "Wallpaper was written to disk");
-    is(gconf.getString(DG_IMAGE_KEY), wpFile.path,
-       "Wallpaper file GConf key is correct");
-    is(gconf.getString(DG_OPTION_KEY), expectedGConfPosition,
-       "Wallpaper position GConf key is correct");
-  }
+    function checkWallpaper(position, expectedGConfPosition) {
+      shell.setDesktopBackground(image, position, "");
+      ok(wpFile.exists(), "Wallpaper was written to disk");
+      is(gconf.getString(DG_IMAGE_KEY), wpFile.path,
+         "Wallpaper file GConf key is correct");
+      is(gconf.getString(DG_OPTION_KEY), expectedGConfPosition,
+         "Wallpaper position GConf key is correct");
+    }
 
-  checkWallpaper(Ci.nsIShellService.BACKGROUND_TILE, "wallpaper");
-  checkWallpaper(Ci.nsIShellService.BACKGROUND_STRETCH, "stretched");
-  checkWallpaper(Ci.nsIShellService.BACKGROUND_CENTER, "centered");
-  checkWallpaper(Ci.nsIShellService.BACKGROUND_FILL, "zoom");
-  checkWallpaper(Ci.nsIShellService.BACKGROUND_FIT, "scaled");
+    checkWallpaper(Ci.nsIShellService.BACKGROUND_TILE, "wallpaper");
+    checkWallpaper(Ci.nsIShellService.BACKGROUND_STRETCH, "stretched");
+    checkWallpaper(Ci.nsIShellService.BACKGROUND_CENTER, "centered");
+    checkWallpaper(Ci.nsIShellService.BACKGROUND_FILL, "zoom");
+    checkWallpaper(Ci.nsIShellService.BACKGROUND_FIT, "scaled");
 
-  // Restore GConf and wallpaper
+    // Restore GConf and wallpaper
 
-  gconf.setString(DG_IMAGE_KEY, prevImageKey);
-  gconf.setString(DG_OPTION_KEY, prevOptionKey);
-  gconf.setBool(DG_DRAW_BG_KEY, prevDrawBgKey);
+    gconf.setString(DG_IMAGE_KEY, prevImageKey);
+    gconf.setString(DG_OPTION_KEY, prevOptionKey);
+    gconf.setBool(DG_DRAW_BG_KEY, prevDrawBgKey);
 
-  wpFile.remove(false);
-  if (wpFileBackup.exists())
-    wpFileBackup.moveTo(null, wpFile.leafName);
-
-  gBrowser.removeCurrentTab();
-  finish();
-}
-
-function test() {
-  try {
-    // If GSettings is available, then the GConf tests
-    // will fail
-    Cc["@mozilla.org/gsettings-service;1"].
-      getService(Ci.nsIGSettingsService).
-      getCollectionForSchema("org.gnome.desktop.background");
-    todo(false, "This test doesn't work when GSettings is available");
-    return;
-  } catch (e) { }
-
-  gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
-  gBrowser.selectedBrowser.addEventListener("load", onPageLoad, true);
-  content.location = "about:logo";
-
-  waitForExplicitFinish();
-}
+    wpFile.remove(false);
+    if (wpFileBackup.exists())
+      wpFileBackup.moveTo(null, wpFile.leafName);
+  });
+});
