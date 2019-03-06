b272dea39b7d0c91e372ef6030414ddba5e7b7cb
Mike Pennisi <mike@mikepennisi.com>
2019-03-06 16:53:06 +0000

    Bug 1531229 [wpt PR 15556] - [wptrunner] Do not use undefined method, a=testonly
    Automatic update from web-platform-tests
    [wptrunner] Do not use undefined method
    
    The Selenium wire protocol does not define a "set window rect" method
    [1]. Implement the desired behavior by composing the "set window
    position" and "set window size" methods.
    
    [1] https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol
    
    --
    
    wpt-commits: 823542ced8f584e121f6196702e1a7e5958530ed
    wpt-pr: 15556

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorselenium.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/executorselenium.py
@@ -369,7 +369,8 @@ class SeleniumRefTestExecutor(RefTestExecutor):
             """return [window.outerWidth - window.innerWidth,
                        window.outerHeight - window.innerHeight];"""
         )
-        self.protocol.webdriver.set_window_rect(0, 0, 800 + width_offset, 600 + height_offset)
+        self.protocol.webdriver.set_window_size(0, 0)
+        self.protocol.webdriver.set_window_position(800 + width_offset, 600 + height_offset)
 
         result = self.implementation.run_test(test)
 
