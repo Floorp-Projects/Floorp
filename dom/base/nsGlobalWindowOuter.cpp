e75ec47463949031f8923d695fc8452ca155c7eb
Boris Zbarsky <bzbarsky@mit.edu>
2019-03-06 19:12:47 +0000

    Bug 1533103.  Temporarily disable compartment-sharing for content windows.  r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D22362

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -1782,6 +1782,7 @@ struct MOZ_STACK_CLASS CompartmentFinderState {
   JS::Compartment* compartment;
 };
 
+#if 0 /* Temporarily disabled; will reenable in bug 1533105 */
 static JS::CompartmentIterResult FindSameOriginCompartment(
     JSContext* aCx, void* aData, JS::Compartment* aCompartment) {
   auto* data = static_cast<CompartmentFinderState*>(aData);
@@ -1806,6 +1807,7 @@ static JS::CompartmentIterResult FindSameOriginCompartment(
   data->compartment = aCompartment;
   return JS::CompartmentIterResult::Stop;
 }
+#endif
 
 static JS::RealmCreationOptions& SelectZone(
     JSContext* aCx, nsIPrincipal* aPrincipal, nsGlobalWindowInner* aNewInner,
@@ -1826,6 +1828,7 @@ static JS::RealmCreationOptions& SelectZone(
 
     // If we have a top-level window, use its zone.
     if (top && top->GetGlobalJSObject()) {
+#if 0 /* Temporarily disabled; will reenable in bug 1533105 */
       JS::Zone* zone = JS::GetObjectZone(top->GetGlobalJSObject());
       // Now try to find an existing compartment that's same-origin
       // with our principal.
@@ -1834,6 +1837,7 @@ static JS::RealmCreationOptions& SelectZone(
       if (data.compartment) {
         return aOptions.setExistingCompartment(data.compartment);
       }
+#endif
       return aOptions.setNewCompartmentInExistingZone(top->GetGlobalJSObject());
     }
   }
