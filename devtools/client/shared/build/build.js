f9a50bdd2ba86cd677766451124639f374cd2bef
Jason Laster <jlaster@mozilla.com>
2019-03-06 05:10:25 +0000

    Revert "Backed out changeset 7f51bc3757d9 (bug 1497839) for ESlint failure at build-debugger.js. CLOSED TREE"
    This reverts commit 2b1fea9435191242f8aadc04da3ed1b0e0d99b02.
    
    Bug 1497839 - fix eslint issues for babel build. r=jlast
    
    Differential Revision: https://phabricator.services.mozilla.com/D22252

---
+++ devtools/client/shared/build/build.js
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+/* globals process, __filename, __dirname */
+
+"use strict";
+
+const Babel = require("./babel");
+const fs = require("fs");
+const _path = require("path");
+
+const defaultPlugins = [
+  "transform-flow-strip-types",
+  "transform-class-properties",
+  "transform-react-jsx",
+];
+
+function transform(filePath) {
+// Use the extra plugins only for the debugger
+  const plugins = filePath.includes("devtools/client/debugger/new")
+    ? require("./build-debugger")(filePath)
+    : defaultPlugins;
+
+  const doc = fs.readFileSync(filePath, "utf8");
+  const out = Babel.transform(doc, { plugins });
+
+  return out.code;
+}
+
+const deps = [
+  __filename,
+  _path.resolve(__dirname, "babel.js"),
+];
+
+for (let i = 2; i < process.argv.length; i++) {
+  const srcPath = process.argv[i];
+  const code = transform(srcPath);
+  const filePath = _path.basename(srcPath);
+  fs.writeFileSync(filePath, code);
+  deps.push(srcPath);
+}
+
+// Print all dependencies prefixed with 'dep:' in order to help node.py, the script that
+// calls this module, to report back the precise list of all dependencies.
+console.log(deps.map(file => "dep:" + file).join("\n"));
