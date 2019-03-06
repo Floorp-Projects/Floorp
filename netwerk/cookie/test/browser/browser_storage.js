b48ba532abeaac7ef01543bc2e20d38b970984f8
Andrea Marchesini <amarchesini@mozilla.com>
2019-03-06 17:03:33 +0000

    Bug 1525245 - Stabilize cookiePolicy/cookiePermission for live documents - part 11 - windows/workers/documents must keep the current cookie settings and ignore changes, r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D18960

--- netwerk/cookie/test/browser/browser_storage.js
+++ netwerk/cookie/test/browser/browser_storage.js
@@ -1,16 +1,16 @@
 CookiePolicyHelper.runTest("SessionStorage", {
-  cookieJarAccessAllowed: async _ => {
+  cookieJarAccessAllowed: async w => {
     try {
-      content.sessionStorage.foo = 42;
+      w.sessionStorage.foo = 42;
       ok(true, "SessionStorage works");
     } catch (e) {
       ok(false, "SessionStorage works");
     }
   },
 
-  cookieJarAccessDenied: async _ => {
+  cookieJarAccessDenied: async w => {
     try {
-      content.sessionStorage.foo = 42;
+      w.sessionStorage.foo = 42;
       ok(false, "SessionStorage doesn't work");
     } catch (e) {
       ok(true, "SessionStorage doesn't work");
@@ -20,22 +20,22 @@ CookiePolicyHelper.runTest("SessionStorage", {
 });
 
 CookiePolicyHelper.runTest("LocalStorage", {
-  cookieJarAccessAllowed: async _ => {
+  cookieJarAccessAllowed: async w => {
     try {
-      content.localStorage.foo = 42;
+      w.localStorage.foo = 42;
       ok(true, "LocalStorage works");
     } catch (e) {
       ok(false, "LocalStorage works");
     }
   },
 
-  cookieJarAccessDenied: async _ => {
+  cookieJarAccessDenied: async w => {
     try {
-      content.localStorage.foo = 42;
+      w.localStorage.foo = 42;
       ok(false, "LocalStorage doesn't work");
     } catch (e) {
       ok(true, "LocalStorage doesn't work");
-      is(e.name, "SecurityError", "We want a security error message.");
+      is(e.name, "TypeError", "We want a security error message.");
     }
   },
 });
