b40e132d4c7ba26cc36d0c5379c8474ec10cd500
Bobby Holley <bobbyholley@gmail.com>
2019-03-06 20:32:32 +0000

    Bug 1531196 - Add a "small-screen" debug pref to shrink the overlay a bit. r=gw
    Adaptive sizing would be better, but this is the simplest path to
    victory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21757

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -965,6 +965,7 @@ pref("gfx.webrender.debug.show-overdraw", false);
 pref("gfx.webrender.debug.slow-frame-indicator", false);
 pref("gfx.webrender.debug.picture-caching", false);
 pref("gfx.webrender.debug.primitives", false);
+pref("gfx.webrender.debug.small-screen", false);
 pref("gfx.webrender.dl.dump-parent", false);
 pref("gfx.webrender.dl.dump-content", false);
 pref("gfx.webrender.picture-caching", true);
