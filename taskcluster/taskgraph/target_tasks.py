153a3278221b7002bf72860ab3a85281fd92e033
Gregory Mierzwinski <gmierz2@outlook.com>
2019-03-06 19:45:34 +0000

    Bug 1476372 - Add fetch tasks for raptor chromium builds. r=rwood,glandium,tomprince
    Differential Revision: https://phabricator.services.mozilla.com/D21372
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -570,6 +570,15 @@ def target_tasks_customv8_update(full_task_graph, parameters, graph_config):
     return ['toolchain-linux64-custom-v8']
 
 
+@_target_task('chromium_update')
+def target_tasks_chromium_update(full_task_graph, parameters, graph_config):
+    """Select tasks required for building latest chromium versions."""
+    return ['fetch-linux64-chromium',
+            'fetch-win32-chromium',
+            'fetch-win64-chromium',
+            'fetch-mac-chromium']
+
+
 @_target_task('pipfile_update')
 def target_tasks_pipfile_update(full_task_graph, parameters, graph_config):
     """Select the set of tasks required to perform nightly in-tree pipfile updates
