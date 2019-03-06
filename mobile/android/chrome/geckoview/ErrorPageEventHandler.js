60d0ba74292ea642b90cf2ea3e9d33dc6838ac0e
Dana Keeler <dkeeler@mozilla.com>
2019-03-06 20:46:32 +0000

    bug 1531924 - remove unnecessary nsIBadCertListener2 in Firefox for Android and improve adding certificate error overrides r=snorp
    Before this patch, Firefox for Android (and GeckoView) would make an extra
    (unnecessary) XHR to gather the information it needed to add a certificate error
    override. However, the docShell already has the required information (via
    failedChannel.securityInfo), so this patch makes it so.
    
    Differential Revision: https://phabricator.services.mozilla.com/D21791
    
    --HG--
    extra : moz-landing-system : lando

--- mobile/android/chrome/geckoview/ErrorPageEventHandler.js
+++ mobile/android/chrome/geckoview/ErrorPageEventHandler.js
@@ -4,13 +4,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const {GeckoViewUtils} = ChromeUtils.import("resource://gre/modules/GeckoViewUtils.jsm");
+const {PrivateBrowsingUtils} = ChromeUtils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 var {XPCOMUtils} = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
 var {Services} = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 const {debug, warn} = GeckoViewUtils.initLogging("ErrorPageEventHandler"); // eslint-disable-line no-unused-vars
-
-ChromeUtils.defineModuleGetter(this, "SSLExceptions",
-                               "resource://gre/modules/SSLExceptions.jsm");
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   Services: "resource://gre/modules/Services.jsm",
@@ -35,19 +33,28 @@ var ErrorPageEventHandler = {
           let perm = errorDoc.getElementById("permanentExceptionButton");
           let temp = errorDoc.getElementById("temporaryExceptionButton");
           if (target == temp || target == perm) {
-            // Handle setting a cert exception and reloading the page
-            try {
-              // Add a new SSL exception for this URL
-              let uri = Services.io.newURI(errorDoc.location.href);
-              let sslExceptions = new SSLExceptions();
-
-              if (target == perm)
-                sslExceptions.addPermanentException(uri, errorDoc.defaultView);
-              else
-                sslExceptions.addTemporaryException(uri, errorDoc.defaultView);
-            } catch (e) {
-              warn `Failed to set cert exception: ${e}`;
+            // Handle setting an cert exception and reloading the page
+            let uri = Services.io.newURI(errorDoc.location.href);
+            let docShell = BrowserApp.selectedBrowser.docShell;
+            let securityInfo = docShell.failedChannel.securityInfo;
+            securityInfo.QueryInterface(Ci.nsITransportSecurityInfo);
+            let cert = securityInfo.serverCert;
+            let overrideService = Cc["@mozilla.org/security/certoverride;1"]
+                                    .getService(Ci.nsICertOverrideService);
+            let flags = 0;
+            if (securityInfo.isUntrusted) {
+              flags |= overrideService.ERROR_UNTRUSTED;
             }
+            if (securityInfo.isDomainMismatch) {
+              flags |= overrideService.ERROR_MISMATCH;
+            }
+            if (securityInfo.isNotValidAtThisTime) {
+              flags |= overrideService.ERROR_TIME;
+            }
+            let temporary = (target == temp) ||
+                            PrivateBrowsingUtils.isWindowPrivate(errorDoc.defaultView);
+            overrideService.rememberValidityOverride(uri.asciiHost, uri.port, cert, flags,
+                                                     temporary);
             errorDoc.location.reload();
           } else if (target == errorDoc.getElementById("getMeOutOfHereButton")) {
             errorDoc.location = "about:home";
