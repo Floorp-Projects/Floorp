a3c37b1535455b3803491a660c6abcac7652a413
Emilio Cobos Álvarez <emilio@crisal.io>
2019-03-06 17:13:14 +0000

    Bug 1533040 - Remove invalid NS_WARNING_ASSERTION. r=dholbert
    We can totally get there with a negative percentage basis, see comment 0 for an
    example.
    
    We could keep the warning like:
    
      NS_WARNING_ASSERTION(clamping_mode == StyleAllowedNumericType::All ||
                           basis >= 0, "nscoord overflow?");
    
    Which will catch cases where the style system would refuse to parse a negative
    <length-percentage>, but we got a negative percent basis, which would be weird.
    
    But that's a bit misleading since right now at least we rely on the caller to do
    the appropriate clamping. I also think that NS_WARNING_ASSERTION is not very
    useful, since we're not very likely to catch stuff with it. But anyhow, your
    call.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22328

--- layout/style/nsStyleCoord.h
+++ layout/style/nsStyleCoord.h
@@ -141,12 +141,10 @@ nscoord LengthPercentage::Resolve(T aPercentageGetter,
     return ToLength();
   }
   nscoord basis = aPercentageGetter();
-  NS_WARNING_ASSERTION(basis >= 0, "nscoord overflow?");
   return length.ToAppUnits() + aPercentageRounder(basis * Percentage());
 }
 
 nscoord LengthPercentage::Resolve(nscoord aPercentageBasis) const {
-  NS_WARNING_ASSERTION(aPercentageBasis >= 0, "nscoord overflow?");
   return Resolve([=] { return aPercentageBasis; }, NSToCoordFloorClamped);
 }
 
