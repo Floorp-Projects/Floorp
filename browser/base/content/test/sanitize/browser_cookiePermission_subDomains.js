5559d59466aa171294d31cb99a5e1763ab9da018
Andrea Marchesini <amarchesini@mozilla.com>
2019-03-06 10:24:10 +0000

    Bug 1509638 - Split browser_cookiePermission.js test, r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D22078
    
    --HG--
    rename : browser/base/content/test/sanitize/browser_cookiePermission.js => browser/base/content/test/sanitize/browser_cookiePermission_aboutURL.js
    rename : browser/base/content/test/sanitize/browser_cookiePermission.js => browser/base/content/test/sanitize/browser_cookiePermission_containers.js
    rename : browser/base/content/test/sanitize/browser_cookiePermission.js => browser/base/content/test/sanitize/browser_cookiePermission_subDomains.js
    extra : moz-landing-system : lando

---
+++ browser/base/content/test/sanitize/browser_cookiePermission_subDomains.js
@@ -0,0 +1,58 @@
+const {Sanitizer} = ChromeUtils.import("resource:///modules/Sanitizer.jsm");
+const {SiteDataTestUtils} = ChromeUtils.import("resource://testing-common/SiteDataTestUtils.jsm");
+
+// 2 domains: www.mozilla.org (session-only) mozilla.org (allowed) - after the
+// cleanp, mozilla.org must have data.
+add_task(async function subDomains() {
+  info("Test subdomains and custom setting");
+
+  // Let's clean up all the data.
+  await new Promise(resolve => {
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, resolve);
+  });
+
+  await SpecialPowers.pushPrefEnv({"set": [
+    ["network.cookie.lifetimePolicy", Ci.nsICookieService.ACCEPT_NORMALLY ],
+    ["browser.sanitizer.loglevel", "All"],
+  ]});
+
+  // Domains and data
+  let uriA = Services.io.newURI("https://www.mozilla.org");
+  Services.perms.add(uriA, "cookie", Ci.nsICookiePermission.ACCESS_SESSION);
+
+  Services.cookies.add(uriA.host, "/test", "a", "b",
+    false, false, false, Date.now() + 24000 * 60 * 60, {},
+    Ci.nsICookie2.SAMESITE_UNSET);
+
+  await createIndexedDB(uriA.host, {});
+
+  let uriB = Services.io.newURI("https://mozilla.org");
+  Services.perms.add(uriB, "cookie", Ci.nsICookiePermission.ACCESS_ALLOW);
+
+  Services.cookies.add(uriB.host, "/test", "c", "d",
+    false, false, false, Date.now() + 24000 * 60 * 60, {},
+    Ci.nsICookie2.SAMESITE_UNSET);
+
+  await createIndexedDB(uriB.host, {});
+
+  // Check
+  ok(await checkCookie(uriA.host, {}), "We have cookies for URI: " + uriA.host);
+  ok(await checkIndexedDB(uriA.host, {}), "We have IDB for URI: " + uriA.host);
+  ok(await checkCookie(uriB.host, {}), "We have cookies for URI: " + uriB.host);
+  ok(await checkIndexedDB(uriB.host, {}), "We have IDB for URI: " + uriB.host);
+
+  // Cleaning up
+  await Sanitizer.runSanitizeOnShutdown();
+
+  // Check again
+  ok(!(await checkCookie(uriA.host, {})), "We should not have cookies for URI: " + uriA.host);
+  ok(!(await checkIndexedDB(uriA.host, {})), "We should not have IDB for URI: " + uriA.host);
+
+  // Note that cookies are stored per base domain...
+  ok(!(await checkCookie(uriB.host, {})), "We should not have cookies for URI: " + uriB.host);
+  ok(await checkIndexedDB(uriB.host, {}), "We should have IDB for URI: " + uriB.host);
+
+  // Cleaning up permissions
+  Services.perms.remove(uriA, "cookie");
+  Services.perms.remove(uriB, "cookie");
+});
