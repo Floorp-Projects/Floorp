0e1767510a23eb464e8ea942bd2cb9a24e54e919
Dão Gottwald <dao@mozilla.com>
2019-03-07 11:47:26 +0000

    Bug 1532719 - Reset _pinnedUnscrollable flag when unpinning a tab. r=Gijs a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D22474

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -622,6 +622,7 @@ window._gBrowser = {
     this.moveTabTo(aTab, this._numPinnedTabs - 1);
     aTab.removeAttribute("pinned");
     aTab.style.marginInlineStart = "";
+    aTab._pinnedUnscrollable = false;
     this._updateTabBarForPinnedTabs();
     this._notifyPinnedStatus(aTab);
   },
