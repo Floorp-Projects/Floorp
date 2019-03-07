e726943dcf8e7169c16c3b33120ddcd0e55f85e2
Jason Orendorff <jorendorff@mozilla.com>
2019-03-07 00:01:49 +0000

    Bug 1531830 - Part 2: Update test262 to run matchAll tests unconditionally. r=jwalden
    The previous patch makes both String.prototype.matchAll and Symbol.matchAll
    unconditional features.
    
    This patch was made by first making the changes to test262-update.py, then
    re-running it against the test262 revision already indicated by
    js/src/tests/test262/GIT-INFO: `python test262-update.py --revision
    59b89a1c834faadc359aecc882587b513877b59b`.
    
    Depends on D22370
    
    Differential Revision: https://phabricator.services.mozilla.com/D22371
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/tests/test262/built-ins/String/prototype/matchAll/prop-desc.js
+++ js/src/tests/test262/built-ins/String/prototype/matchAll/prop-desc.js
@@ -1,4 +1,3 @@
-// |reftest| skip-if(!String.prototype.hasOwnProperty('matchAll')) -- String.prototype.matchAll is not enabled unconditionally
 // Copyright (C) 2018 Jordan Harband. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 /*---
