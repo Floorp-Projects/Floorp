d8a1aaa5de2693decb739cf88e87676c7b9c39ce
Mike Conley <mconley@mozilla.com>
2019-03-07 19:04:35 +0000

    Bug 1529762 - Send PageStyle information up from pageshow events only after idle. r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D22521
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/general/browser_page_style_menu_update.js
+++ browser/base/content/test/general/browser_page_style_menu_update.js
@@ -3,29 +3,6 @@
 const PAGE = "http://example.com/browser/browser/base/content/test/general/page_style_sample.html";
 
 /**
- * Stylesheets are updated for a browser after the pageshow event.
- * This helper function returns a Promise that waits for that pageshow
- * event, and then resolves on the next tick to ensure that gPageStyleMenu
- * has had a chance to update the stylesheets.
- *
- * @param browser
- *        The <xul:browser> to wait for.
- * @return Promise
- */
-function promiseStylesheetsUpdated(browser) {
-  return ContentTask.spawn(browser, { PAGE }, async function(args) {
-    return new Promise((resolve) => {
-      addEventListener("pageshow", function onPageShow(e) {
-        if (e.target.location == args.PAGE) {
-          removeEventListener("pageshow", onPageShow);
-          content.setTimeout(resolve, 0);
-        }
-      });
-    });
-  });
-}
-
-/**
  * Tests that the Page Style menu shows the currently
  * selected Page Style after a new one has been selected.
  */
@@ -49,13 +26,8 @@ add_task(async function() {
   target.click();
 
   // Now we need to wait for the content process to send its stylesheet
-  // update for the selected tab to the parent. Because messages are
-  // guaranteed to be sent in order, we'll make sure we do the check
-  // after the parent has been updated by yielding until the child
-  // has finished running a ContentTask for us.
-  await ContentTask.spawn(browser, {}, async function() {
-    dump("\nJust wasting some time.\n");
-  });
+  // update for the selected tab to the parent.
+  await promiseStylesheetsUpdated(browser);
 
   gPageStyleMenu.fillPopup(menupopup);
   // gPageStyleMenu empties out the menu between opens, so we need
