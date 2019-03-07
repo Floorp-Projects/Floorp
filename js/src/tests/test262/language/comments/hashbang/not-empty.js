5a25b281102b08c500ad9e2f65736d42c7cd8d50
André Bargull <andre.bargull@gmail.com>
2019-03-07 14:40:20 +0000

    Bug 1533168 - Part 1: Update test262 - Mar 06 2018 edition. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D22402

--- js/src/tests/test262/language/comments/hashbang/not-empty.js
+++ js/src/tests/test262/language/comments/hashbang/not-empty.js
@@ -1,4 +1,8 @@
 #! these characters should be treated as a comment
+
+// Copyright (C) 2019 Leo Balter. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
 /*---
 esid: pending
 description: >
