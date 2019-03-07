539ac0a987267f38c2f0d4ee9416030454dca9f1
prathiksha <prathikshaprasadsuman@gmail.com>
2019-03-07 00:06:42 +0000

    Bug 1530029 - Pressing enter on the autocomplete footer should open the password manager dialog. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D21603

--- toolkit/components/passwordmgr/test/browser/browser_autocomplete_footer.js
+++ toolkit/components/passwordmgr/test/browser/browser_autocomplete_footer.js
@@ -21,6 +21,24 @@ function loginList() {
   ];
 }
 
+function openPopup(popup, browser) {
+  return new Promise(async (resolve) => {
+    let promiseShown = BrowserTestUtils.waitForEvent(popup, "popupshown");
+
+    await SimpleTest.promiseFocus(browser);
+    info("content window focused");
+
+    // Focus the username field to open the popup.
+    await ContentTask.spawn(browser, null, function openAutocomplete() {
+      content.document.getElementById("form-basic-username").focus();
+    });
+
+    let shown = await promiseShown;
+    ok(shown, "autocomplete popup shown");
+    resolve(shown);
+  });
+}
+
 /**
  * Initialize logins and set prefs needed for the test.
  */
@@ -35,27 +53,16 @@ add_task(async function test_initialize() {
   }
 });
 
-add_task(async function test_autocomplete_footer() {
+add_task(async function test_autocomplete_footer_onclick() {
   let url = TEST_HOSTNAME + BASIC_FORM_PAGE_PATH;
   await BrowserTestUtils.withNewTab({
     gBrowser,
     url,
-  }, async function(browser) {
+  }, async function footer_onclick(browser) {
     let popup = document.getElementById("PopupAutoComplete");
     ok(popup, "Got popup");
 
-    let promiseShown = BrowserTestUtils.waitForEvent(popup, "popupshown");
-
-    await SimpleTest.promiseFocus(browser);
-    info("content window focused");
-
-    // Focus the username field to open the popup.
-    await ContentTask.spawn(browser, null, function openAutocomplete() {
-      content.document.getElementById("form-basic-username").focus();
-    });
-
-    await promiseShown;
-    ok(promiseShown, "autocomplete shown");
+    await openPopup(popup, browser);
 
     let footer = popup.querySelector(`[originaltype="loginsFooter"]`);
     ok(footer, "Got footer richlistitem");
@@ -65,6 +72,44 @@ add_task(async function test_autocomplete_footer() {
     }, "Waiting for footer to become visible");
 
     EventUtils.synthesizeMouseAtCenter(footer, {});
+    await TestUtils.waitForCondition(() => {
+      return Services.wm.getMostRecentWindow("Toolkit:PasswordManager") !== null;
+    }, "Waiting for the password manager dialog to open");
+    info("Login dialog was opened");
+
+    let window = Services.wm.getMostRecentWindow("Toolkit:PasswordManager");
+    await TestUtils.waitForCondition(() => {
+      return window.document.getElementById("filter").value == "example.com";
+    }, "Waiting for the search string to filter logins");
+
+    window.close();
+    popup.hidePopup();
+  });
+});
+
+add_task(async function test_autocomplete_footer_keydown() {
+  let url = TEST_HOSTNAME + BASIC_FORM_PAGE_PATH;
+  await BrowserTestUtils.withNewTab({
+    gBrowser,
+    url,
+  }, async function footer_enter_keydown(browser) {
+    let popup = document.getElementById("PopupAutoComplete");
+    ok(popup, "Got popup");
+
+    await openPopup(popup, browser);
+
+    let footer = popup.querySelector(`[originaltype="loginsFooter"]`);
+    ok(footer, "Got footer richlistitem");
+
+    await TestUtils.waitForCondition(() => {
+      return !EventUtils.isHidden(footer);
+    }, "Waiting for footer to become visible");
+
+    await EventUtils.synthesizeKey("KEY_ArrowDown");
+    await EventUtils.synthesizeKey("KEY_ArrowDown");
+    await EventUtils.synthesizeKey("KEY_ArrowDown");
+    await EventUtils.synthesizeKey("KEY_Enter");
+
     await TestUtils.waitForCondition(() => {
       return Services.wm.getMostRecentWindow("Toolkit:PasswordManager") !== null;
     }, "Waiting for the password manager dialog to open");
