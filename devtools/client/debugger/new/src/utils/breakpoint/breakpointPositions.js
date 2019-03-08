86b00bcb81ef9bdc58c7018cbe94b426007caef4
Jason Laster <jlaster@mozilla.com>
2019-03-08 13:39:06 +0000

    Bug 1533489 - findBreakpoint should be context aware. r=loganfsmyth
    Differential Revision: https://phabricator.services.mozilla.com/D22558

--- devtools/client/debugger/new/src/utils/breakpoint/breakpointPositions.js
+++ devtools/client/debugger/new/src/utils/breakpoint/breakpointPositions.js
@@ -5,19 +5,18 @@
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 import { comparePosition } from "../location";
-import type {
-  BreakpointPositions,
-  SourceLocation,
-  Position
-} from "../../types";
+import { getSelectedLocation } from "../source-maps";
+import type { BreakpointPositions, SourceLocation } from "../../types";
 
 export function findPosition(
   positions: ?BreakpointPositions,
-  location: Position | SourceLocation
+  location: SourceLocation
 ) {
   if (!positions) {
     return null;
   }
 
-  return positions.find(pos => comparePosition(pos.location, location));
+  return positions.find(pos =>
+    comparePosition(getSelectedLocation(pos, location), location)
+  );
 }
