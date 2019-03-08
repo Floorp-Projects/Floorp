d859ea19e036abe66c7ff35e45bd4b697e909ab7
Alastor Wu <alwu@mozilla.com>
2019-03-08 03:10:45 +0000

    Bug 1509446 - part1 : update active cues list when cue's active state changed. r=jya
    According to spec [1], `activeCues` should represent a subset of the text track cues whose active flag was set when the script started.
    
    We should only depend on the `TimeMarchesOn` algorithm which will change cue's active state, and then add or remove cue to `activeCues`.
    
    [1] https://html.spec.whatwg.org/multipage/media.html#dom-texttrack-activecues
    
    Differential Revision: https://phabricator.services.mozilla.com/D22147

--- dom/media/TextTrack.h
+++ dom/media/TextTrack.h
@@ -69,7 +69,6 @@ class TextTrack final : public DOMEventTargetHelper {
   }
 
   TextTrackCueList* GetActiveCues();
-  void UpdateActiveCueList();
   void GetActiveCueArray(nsTArray<RefPtr<TextTrackCue> >& aCues);
 
   TextTrackReadyState ReadyState() const;
@@ -98,6 +97,10 @@ class TextTrack final : public DOMEventTargetHelper {
   void DispatchAsyncTrustedEvent(const nsString& aEventName);
 
   bool IsLoaded();
+
+  // Called when associated cue's active flag has been changed, and then we
+  // would add or remove the cue to the active cue list.
+  void NotifyCueActiveStateChanged(TextTrackCue* aCue);
 
  private:
   ~TextTrack();
