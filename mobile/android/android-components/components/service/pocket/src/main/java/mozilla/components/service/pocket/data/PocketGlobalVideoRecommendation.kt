632f029fb3c22439b5feb76327d04a83be3683e1
Michael Comella <michael.l.comella@gmail.com>
2019-03-07 16:52:06 -0800

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/651 - review: Add kdoc for PocketGlobalVideoRecommendation properties; Author.

--- mobile/android/android-components/components/service/pocket/src/main/java/mozilla/components/service/pocket/data/PocketGlobalVideoRecommendation.kt
+++ mobile/android/android-components/components/service/pocket/src/main/java/mozilla/components/service/pocket/data/PocketGlobalVideoRecommendation.kt
@@ -6,6 +6,18 @@ package mozilla.components.service.pocket.data
 
 /**
  * A recommended video as returned from the Pocket Global Video Recommendation endpoint v2.
+ *
+ * @property id a unique identifier for this recommendation.
+ * @property url a url to the video.
+ * @property tvURL a url to the video on pages formatted for TV form factors (e.g. YouTube.com/tv).
+ * @property title the title of the video.
+ * @property excerpt a summary of the video.
+ * @property domain the domain where the video appears, e.g. "youtube.com".
+ * @property imageSrc a url to a still image representing the video.
+ * @property publishedTimestamp unknown: please ask for clarification if needed. This may be "0".
+ * @property sortId the index of this recommendation in the list which is sorted by date added to the API results.
+ * @property popularitySortId the index of this recommendation in the list if the list was sorted by popularity.
+ * @property authors the authors or publishers of this recommendation; unclear if this can be empty.
  */
 data class PocketGlobalVideoRecommendation(
     val id: Int,
@@ -21,6 +33,13 @@ data class PocketGlobalVideoRecommendation(
     val authors: List<Author>
 ) {
 
+    /**
+     * An author or publisher of a [PocketGlobalVideoRecommendation].
+     *
+     * @property id a unique identifier for this author.
+     * @property name the name of this author.
+     * @property url a url to the author's video content channel (e.g. a YouTube channel).
+     */
     data class Author(
         val id: String,
         val name: String,
