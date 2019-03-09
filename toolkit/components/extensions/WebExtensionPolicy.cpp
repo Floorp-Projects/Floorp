952e4d8c455721aa3763d730f6c509a0510ddd7e
Bogdan Tara <btara@mozilla.com>
2019-03-09 03:46:18 +0200

    Backed out 5 changesets (bug 1518863) For ES Lint failure a=backout
    Backed out changeset 11506a4e0fd8 (bug 1518863)
    Backed out changeset d94de0e08f52 (bug 1518863)
    Backed out changeset 4d96aa8bbf57 (bug 1518863)
    Backed out changeset b7e78201e26c (bug 1518863)
    Backed out changeset 037957225790 (bug 1518863)

--- toolkit/components/extensions/WebExtensionPolicy.cpp
+++ toolkit/components/extensions/WebExtensionPolicy.cpp
@@ -174,10 +174,6 @@ WebExtensionPolicy::WebExtensionPolicy(GlobalObject& aGlobal,
     mContentScripts.AppendElement(std::move(contentScript));
   }
 
-  if (aInit.mReadyPromise.WasPassed()) {
-    mReadyPromise = &aInit.mReadyPromise.Value();
-  }
-
   nsresult rv = NS_NewURI(getter_AddRefs(mBaseURI), aInit.mBaseURL);
   if (NS_FAILED(rv)) {
     aRv.Throw(rv);
@@ -458,14 +454,6 @@ bool WebExtensionPolicy::CanAccessWindow(
   nsIDocShell* docShell = aWindow.get()->GetDocShell();
   nsCOMPtr<nsILoadContext> loadContext = do_QueryInterface(docShell);
   return !(loadContext && loadContext->UsePrivateBrowsing());
-}
-
-void WebExtensionPolicy::GetReadyPromise(JSContext* aCx, JS::MutableHandleObject aResult) const {
-  if (mReadyPromise) {
-    aResult.set(mReadyPromise->PromiseObj());
-  } else {
-    aResult.set(nullptr);
-  }
 }
 
 NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(WebExtensionPolicy, mParent,
