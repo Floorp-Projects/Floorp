614dc1133edce12f5d7d85d0ae375fd37c6770fa
Tom Prince <mozilla@hocat.ca>
2019-03-09 00:05:49 +0000

    Bug 1527858: Vendor redo==2.0.3; r=aki
    There was an error in logging multiple tries. This updates redo to a version with fixed logging.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22806

--- third_party/python/redo/setup.py
+++ third_party/python/redo/setup.py
@@ -6,13 +6,11 @@ except ImportError:
 
 setup(
     name="redo",
-    version="2.0.2",
+    version="2.0.3",
     description="Utilities to retry Python callables.",
     author="Ben Hearsum",
     author_email="ben@hearsum.ca",
     packages=["redo"],
-    entry_points={
-        "console_scripts": ["retry = redo.cmd:main"],
-    },
+    entry_points={"console_scripts": ["retry = redo.cmd:main"]},
     url="https://github.com/bhearsum/redo",
 )
