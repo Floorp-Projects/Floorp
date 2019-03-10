6b0586b87adffa92ed8d2bff2a2573eefb19de09
Sylvestre Ledru <sledru@mozilla.com>
2019-03-10 15:29:42 +0000

    Bug 1531176 - about:support: also support the split of the key r=florian,flod a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D22676

--- browser/base/content/test/about/browser_aboutSupport.js
+++ browser/base/content/test/about/browser_aboutSupport.js
@@ -8,13 +8,22 @@ ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 add_task(async function() {
   await BrowserTestUtils.withNewTab({ gBrowser, url: "about:support" }, async function(browser) {
-    let keyGoogleStatus = await ContentTask.spawn(browser, null, async function() {
-      let textBox = content.document.getElementById("key-google-box");
+    let keyLocationServiceGoogleStatus = await ContentTask.spawn(browser, null, async function() {
+      let textBox = content.document.getElementById("key-location-service-google-box");
       await ContentTaskUtils.waitForCondition(() => content.document.l10n.getAttributes(textBox).id,
-        "Google API key status loaded");
+        "Google location service API key status loaded");
       return content.document.l10n.getAttributes(textBox).id;
     });
-    ok(keyGoogleStatus, "Google API key status shown");
+    ok(keyLocationServiceGoogleStatus, "Google location service API key status shown");
+
+    let keySafebrowsingGoogleStatus = await ContentTask.spawn(browser, null, async function() {
+      let textBox = content.document.getElementById("key-safebrowsing-google-box");
+      await ContentTaskUtils.waitForCondition(() => content.document.l10n.getAttributes(textBox).id,
+        "Google Safebrowsing API key status loaded");
+      return content.document.l10n.getAttributes(textBox).id;
+    });
+    ok(keySafebrowsingGoogleStatus, "Google Safebrowsing API key status shown");
+
 
     let keyMozillaStatus = await ContentTask.spawn(browser, null, async function() {
       let textBox = content.document.getElementById("key-mozilla-box");
