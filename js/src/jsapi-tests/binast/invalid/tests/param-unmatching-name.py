721a333b62b8a20d16b9de346f252dee7dd13fc0
Tooru Fujisawa <arai_a@mac.com>
2019-03-11 16:04:04 +0000

    Bug 1533711 - Provide class with method chain in filter_utils.py for BinAST invalid file test. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D22888

--- js/src/jsapi-tests/binast/invalid/tests/param-unmatching-name.py
+++ js/src/jsapi-tests/binast/invalid/tests/param-unmatching-name.py
@@ -2,23 +2,19 @@ def filter_ast(ast):
     # Set different parameter name than one in scope.
     import filter_utils as utils
 
-    utils.assert_interface(ast, 'Script')
-    global_stmts = utils.get_field(ast, 'statements')
-
-    func_stmt = utils.get_element(global_stmts, 0)
-    utils.assert_interface(func_stmt, 'EagerFunctionDeclaration')
-
-    func_contents = utils.get_field(func_stmt, 'contents')
-    utils.assert_interface(func_contents, 'FunctionOrMethodContents')
-
-    params = utils.get_field(func_contents, 'params')
-    utils.assert_interface(params, 'FormalParameters')
-    params_items = utils.get_field(params, 'items')
-
-    param = utils.get_element(params_items, 0)
-    utils.assert_interface(param, 'BindingIdentifier')
-
-    name = utils.get_field(param, "name")
-    utils.set_identifier_name(name, "b")
+    utils.wrap(ast) \
+        .assert_interface('Script') \
+        .field('statements') \
+        .elem(0) \
+        .assert_interface('EagerFunctionDeclaration') \
+        .field('contents') \
+        .assert_interface('FunctionOrMethodContents') \
+        .field('params') \
+        .assert_interface('FormalParameters') \
+        .field('items') \
+        .elem(0) \
+        .assert_interface('BindingIdentifier') \
+        .field('name') \
+        .set_identifier_name('b')
 
     return ast
