5461d29613f6247d9cca7e47f9dc5780db3a0bc3
Thomas Daede <tdaede@mozilla.com>
2019-03-11 18:00:13 +0000

    Bug 1521186 - Download nasm toolchain on Windows and Linux. r=firefox-build-system-reviewers,mshal
    Differential Revision: https://phabricator.services.mozilla.com/D22798

--- python/mozboot/mozboot/centosfedora.py
+++ python/mozboot/mozboot/centosfedora.py
@@ -7,10 +7,10 @@ from __future__ import absolute_import
 import platform
 
 from mozboot.base import BaseBootstrapper
-from mozboot.linux_common import NodeInstall, StyloInstall, ClangStaticAnalysisInstall
+from mozboot.linux_common import NasmInstall, NodeInstall, StyloInstall, ClangStaticAnalysisInstall
 
 
-class CentOSFedoraBootstrapper(NodeInstall, StyloInstall,
+class CentOSFedoraBootstrapper(NasmInstall, NodeInstall, StyloInstall,
                                ClangStaticAnalysisInstall, BaseBootstrapper):
     def __init__(self, distro, version, dist_id, **kwargs):
         BaseBootstrapper.__init__(self, **kwargs)
