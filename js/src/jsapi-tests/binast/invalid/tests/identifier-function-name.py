3308d9687dd871ed6fcab85474ec68f1b121ad12
Tooru Fujisawa <arai_a@mac.com>
2019-03-11 16:04:04 +0000

    Bug 1533711 - Provide class with method chain in filter_utils.py for BinAST invalid file test. r=Yoric
    Differential Revision: https://phabricator.services.mozilla.com/D22888

--- js/src/jsapi-tests/binast/invalid/tests/identifier-function-name.py
+++ js/src/jsapi-tests/binast/invalid/tests/identifier-function-name.py
@@ -2,17 +2,14 @@ def filter_ast(ast):
     # AssignmentTargetIdentifier with non-identifier string.
     import filter_utils as utils
 
-    utils.assert_interface(ast, 'Script')
-    global_stmts = utils.get_field(ast, 'statements')
-
-    fun_decl = utils.get_element(global_stmts, 0)
-    utils.assert_interface(fun_decl, 'EagerFunctionDeclaration')
-
-    binding = utils.get_field(fun_decl, 'name')
-    utils.assert_interface(binding, 'BindingIdentifier')
-
-    name = utils.get_field(binding, 'name')
-
-    utils.set_identifier_name(name, '1')
+    utils.wrap(ast) \
+        .assert_interface('Script') \
+        .field('statements') \
+        .elem(0) \
+        .assert_interface('EagerFunctionDeclaration') \
+        .field('name') \
+        .assert_interface('BindingIdentifier') \
+        .field('name') \
+        .set_identifier_name('1')
 
     return ast
--- js/src/jsapi-tests/binast/invalid/tests/wtf-8-interface-name.py
+++ js/src/jsapi-tests/binast/invalid/tests/wtf-8-interface-name.py
@@ -2,12 +2,11 @@ def filter_ast(ast):
     # Put WTF-8 string into interface name.
     import filter_utils as utils
 
-    utils.assert_interface(ast, 'Script')
-    global_stmts = utils.get_field(ast, 'statements')
-
-    expr_stmt = utils.get_element(global_stmts, 0)
-    utils.assert_interface(expr_stmt, 'ExpressionStatement')
-
-    utils.set_interface_name(expr_stmt, u'\uD83E_\uDD9D')
+    utils.wrap(ast) \
+        .assert_interface('Script') \
+        .field('statements') \
+        .elem(0) \
+        .assert_interface('ExpressionStatement') \
+        .set_interface_name(u'\uD83E_\uDD9D')
 
     return ast
