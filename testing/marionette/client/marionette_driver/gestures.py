4b889bad47ceff6fc958f30482ea0e40694282fe
Henrik Skupin <mail@hskupin.info>
2019-03-11 20:33:49 +0000

    Bug 1533786 - [marionette] Add support for the WebDriver Actions API. r=ato
    Differential Revision: https://phabricator.services.mozilla.com/D22757
    
    --HG--
    extra : moz-landing-system : lando

--- testing/marionette/client/marionette_driver/gestures.py
+++ testing/marionette/client/marionette_driver/gestures.py
@@ -4,7 +4,7 @@
 
 from __future__ import absolute_import
 
-from .marionette import MultiActions, Actions
+from .legacy_actions import MultiActions, Actions
 
 
 def smooth_scroll(marionette_session, start_element, axis, direction,
