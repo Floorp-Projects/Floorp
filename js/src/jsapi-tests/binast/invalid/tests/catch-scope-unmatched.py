160af4b3bf0ba0ca00f41682b39ce88f993675f8
Tooru Fujisawa <arai_a@mac.com>
2019-03-11 14:36:08 +0000

    Bug 1533620 - Verify that catch scope has correct item. r=Yoric
    Depends on D22647
    
    Differential Revision: https://phabricator.services.mozilla.com/D22648

---
+++ js/src/jsapi-tests/binast/invalid/tests/catch-scope-unmatched.py
@@ -0,0 +1,25 @@
+def filter_ast(ast):
+    # catch with unmatched AssertedBoundName
+    import filter_utils as utils
+
+    utils.assert_interface(ast, 'Script')
+    global_stmts = utils.get_field(ast, 'statements')
+
+    try_stmt = utils.get_element(global_stmts, 0)
+    utils.assert_interface(try_stmt, 'TryCatchStatement')
+
+    catch = utils.get_field(try_stmt, 'catchClause')
+    utils.assert_interface(catch, 'CatchClause')
+
+    scope = utils.get_field(catch, 'bindingScope')
+    utils.assert_interface(scope, 'AssertedBoundNamesScope')
+
+    names = utils.get_field(scope, 'boundNames')
+    bound_name = utils.get_element(names, 0)
+    utils.assert_interface(bound_name, 'AssertedBoundName')
+
+    name = utils.get_field(bound_name, 'name')
+
+    utils.set_identifier_name(name, 'b')
+
+    return ast
