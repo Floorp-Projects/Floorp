e917b2bb03501ab07836c08baa7125fa325d03b3
James Willcox <snorp@snorp.net>
2019-03-12 19:22:16 +0000

    Bug 1524992 - Followup to add newline r=mbrubeck,rbarker
    Differential Revision: https://phabricator.services.mozilla.com/D23192

--- testing/profiles/unittest-required/user.js
+++ testing/profiles/unittest-required/user.js
@@ -248,5 +248,6 @@ user_pref("toolkit.telemetry.initDelay", 99999999);
 
 // We use data: to tell the Quitter extension to quit.
 user_pref("security.data_uri.block_toplevel_data_uri_navigations", false);
+
 // We use data: to tell the Quitter extension to quit.
 user_pref("security.data_uri.block_toplevel_data_uri_navigations", false);
