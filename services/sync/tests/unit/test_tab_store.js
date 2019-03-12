fbcc2b613c9c3e5f5b0bed6ff9c9903cc68f41f3
Edouard Oger <eoger@fastmail.com>
2019-03-12 19:32:40 +0000

    Bug 1532514 - Update sinon to v7.2.7. r=markh
    Differential Revision: https://phabricator.services.mozilla.com/D22046

--- services/sync/tests/unit/test_tab_store.js
+++ services/sync/tests/unit/test_tab_store.js
@@ -112,7 +112,7 @@ add_task(async function test_createRecord() {
   // actual max we can fit.
   equal(record.tabs.length, 2672);
 
-  let maxSizeStub = sinon.stub(Service, "getMemcacheMaxRecordPayloadSize", () => 512 * 1024);
+  let maxSizeStub = sinon.stub(Service, "getMemcacheMaxRecordPayloadSize").callsFake(() => 512 * 1024);
   try {
     numtabs = 5400;
     _("Modify the max record payload size and create a big record");
