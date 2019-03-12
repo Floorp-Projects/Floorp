0061053ab78f6258596f2da55cb9a46fc991ea9e
Brian Hackett <bhackett1024@gmail.com>
2019-03-12 10:11:48 -1000

    Bug 1534772 - Evaluate expressions when selecting new threads, r=jlast.

--- devtools/client/debugger/new/src/reducers/pause.js
+++ devtools/client/debugger/new/src/reducers/pause.js
@@ -193,9 +193,6 @@ function update(
       });
     }
 
-    case "TRAVEL_TO":
-      return updateThreadState({ ...action.data.paused });
-
     case "MAP_SCOPES": {
       const { frame, status, value } = action;
       const selectedFrameId = frame.id;
