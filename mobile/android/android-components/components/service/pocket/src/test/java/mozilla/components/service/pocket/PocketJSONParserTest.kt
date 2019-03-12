c94e257897524639d9c1e24726ed4fc38ba15076
Michael Comella <michael.l.comella@gmail.com>
2019-03-11 17:25:59 -0700

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/651: Move PocketTestResource test data into TestResource class.
    This will allow for better re-use.

--- mobile/android/android-components/components/service/pocket/src/test/java/mozilla/components/service/pocket/PocketJSONParserTest.kt
+++ mobile/android/android-components/components/service/pocket/src/test/java/mozilla/components/service/pocket/PocketJSONParserTest.kt
@@ -38,43 +38,7 @@ class PocketJSONParserTest {
 
     @Test
     fun `WHEN parsing valid global video recommendations THEN pocket videos are returned`() {
-        val expectedSubset = listOf(
-            PocketGlobalVideoRecommendation(
-                id = 27587,
-                url = "https://www.youtube.com/watch?v=953Qt4FnAcU",
-                tvURL = "https://www.youtube.com/tv#/watch/video/idle?v=953Qt4FnAcU",
-                title = "How a Master Pastry Chef Uses Architecture to Make Sky High Pastries",
-                excerpt = "At New York City's International Culinary Center, host Rebecca DeAngelis makes a modern day croquembouche with architect-turned pastry chef Jansen Chan",
-                domain = "youtube.com",
-                imageSrc = "https://img-getpocket.cdn.mozilla.net/direct?url=http%3A%2F%2Fimg.youtube.com%2Fvi%2F953Qt4FnAcU%2Fmaxresdefault.jpg&resize=w450",
-                publishedTimestamp = "0",
-                sortId = 0,
-                popularitySortId = 20,
-                authors = listOf(PocketGlobalVideoRecommendation.Author(
-                    id = "96612022",
-                    name = "Eater",
-                    url = "http://www.youtube.com/channel/UCRzPUBhXUZHclB7B5bURFXw"
-                ))
-            ),
-            PocketGlobalVideoRecommendation(
-                id = 27581,
-                url = "https://www.youtube.com/watch?v=GHZ7-kq3GDQ",
-                tvURL = "https://www.youtube.com/tv#/watch/video/idle?v=GHZ7-kq3GDQ",
-                title = "How Does Having Too Much Power Affect Your Brain?",
-                excerpt = "Power is tied to a hierarchical escalator that we rise up through decisions and actions. But once we have power, how does it affect our brain and behavior? How Our Brains Respond to People Who Aren't Like Us - https://youtu.be/KIwe_O0am4URead More:The age of adolescencehttps://www.thelancet.com/jour",
-                domain = "youtube.com",
-                imageSrc = "https://img-getpocket.cdn.mozilla.net/direct?url=http%3A%2F%2Fimg.youtube.com%2Fvi%2FGHZ7-kq3GDQ%2Fmaxresdefault.jpg&resize=w450",
-                publishedTimestamp = "0",
-                sortId = 1,
-                popularitySortId = 17,
-                authors = listOf(PocketGlobalVideoRecommendation.Author(
-                    id = "96612138",
-                    name = "Seeker",
-                    url = "http://www.youtube.com/channel/UCzWQYUVCpZqtN93H8RR44Qw"
-                ))
-            )
-        )
-
+        val expectedSubset = PocketTestResource.videoRecommendationFirstTwo
         val pocketJSON = PocketTestResource.POCKET_VIDEO_RECOMMENDATION.get()
         val actualVideos = parser.jsonToGlobalVideoRecommendations(pocketJSON)
 
