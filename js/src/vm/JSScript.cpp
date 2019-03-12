f1635c6c2c4eda1d57dd51e47c17dd849e2dbe07
Ted Campbell <tcampbell@mozilla.com>
2019-03-12 02:40:59 +0000

    Bug 1533755 - Add SharedScriptData constructor. r=jwalden
    This is based on PrivateScriptData approach. We make sure to call all
    default constructors even if the compiler will optimize away.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22717

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -672,6 +672,66 @@ XDRResult js::PrivateScriptData::XDR(XDRState<mode>* xdr, HandleScript script,
   return Ok();
 }
 
+// Placement-new elements of an array. This should optimize away for types with
+// trivial default initiation.
+template <typename T>
+static void DefaultInitializeElements(void* arrayPtr, size_t length) {
+  uintptr_t elem = reinterpret_cast<uintptr_t>(arrayPtr);
+  MOZ_ASSERT(elem % alignof(T) == 0);
+
+  for (size_t i = 0; i < length; ++i) {
+    new (reinterpret_cast<void*>(elem)) T;
+    elem += sizeof(T);
+  }
+}
+
+/* static */ size_t SharedScriptData::AllocationSize(uint32_t codeLength,
+                                                     uint32_t noteLength,
+                                                     uint32_t natoms) {
+  size_t size = sizeof(SharedScriptData);
+
+  size += natoms * sizeof(GCPtrAtom);
+  size += codeLength * sizeof(jsbytecode);
+  size += noteLength * sizeof(jssrcnote);
+
+  return size;
+}
+
+// Placement-new elements of an array. This should optimize away for types with
+// trivial default initiation.
+template <typename T>
+void SharedScriptData::initElements(size_t offset, size_t length) {
+  uintptr_t base = reinterpret_cast<uintptr_t>(this);
+  DefaultInitializeElements<T>(reinterpret_cast<void*>(base + offset), length);
+}
+
+SharedScriptData::SharedScriptData(uint32_t codeLength, uint32_t noteLength,
+                                   uint32_t natoms)
+    : codeLength_(codeLength), noteLength_(noteLength), natoms_(natoms) {
+  // Variable-length data begins immediately after SharedScriptData itself.
+  size_t cursor = sizeof(*this);
+
+  // Default-initialize trailing arrays.
+
+  static_assert(alignof(SharedScriptData) >= alignof(GCPtrAtom),
+                "Incompatible alignment");
+  initElements<GCPtrAtom>(cursor, natoms);
+  cursor += natoms * sizeof(GCPtrAtom);
+
+  static_assert(alignof(GCPtrAtom) >= alignof(jsbytecode),
+                "Incompatible alignment");
+  initElements<jsbytecode>(cursor, codeLength);
+  cursor += codeLength * sizeof(jsbytecode);
+
+  static_assert(alignof(jsbytecode) >= alignof(jssrcnote),
+                "Incompatible alignment");
+  initElements<jssrcnote>(cursor, noteLength);
+  cursor += noteLength * sizeof(jssrcnote);
+
+  // Sanity check
+  MOZ_ASSERT(AllocationSize(codeLength, noteLength, natoms) == cursor);
+}
+
 template <XDRMode mode>
 /* static */
 XDRResult SharedScriptData::XDR(XDRState<mode>* xdr, HandleScript script) {
@@ -2869,41 +2929,21 @@ bool ScriptSource::setSourceMapURL(JSContext* cx,
  */
 
 SharedScriptData* js::SharedScriptData::new_(JSContext* cx, uint32_t codeLength,
-                                             uint32_t srcnotesLength,
+                                             uint32_t noteLength,
                                              uint32_t natoms) {
-  size_t dataLength = natoms * sizeof(GCPtrAtom) + codeLength + srcnotesLength;
-  size_t allocLength = offsetof(SharedScriptData, data_) + dataLength;
-  auto entry =
-      reinterpret_cast<SharedScriptData*>(cx->pod_malloc<uint8_t>(allocLength));
-  if (!entry) {
-    ReportOutOfMemory(cx);
+  // Compute size including trailing arrays
+  size_t size = AllocationSize(codeLength, noteLength, natoms);
+
+  // Allocate contiguous raw buffer
+  void* raw = cx->pod_malloc<uint8_t>(size);
+  MOZ_ASSERT(uintptr_t(raw) % alignof(SharedScriptData) == 0);
+  if (!raw) {
     return nullptr;
   }
 
-  /* Diagnostic for Bug 1399373.
-   * We expect bytecode is always non-empty. */
-  MOZ_DIAGNOSTIC_ASSERT(codeLength > 0);
-
-  entry->refCount_ = 0;
-  entry->natoms_ = natoms;
-  entry->codeLength_ = codeLength;
-  entry->noteLength_ = srcnotesLength;
-
-  /*
-   * Call constructors to initialize the storage that will be accessed as a
-   * GCPtrAtom array via atoms().
-   */
-  static_assert(offsetof(SharedScriptData, data_) % sizeof(GCPtrAtom) == 0,
-                "atoms must have GCPtrAtom alignment");
-  GCPtrAtom* atoms = entry->atoms();
-  for (unsigned i = 0; i < natoms; ++i) {
-    new (&atoms[i]) GCPtrAtom();
-  }
-
-  // Sanity check the dataLength() computation
-  MOZ_ASSERT(entry->dataLength() == dataLength);
-
-  return entry;
+  // Constuct the SharedScriptData. Trailing arrays are uninitialized but
+  // GCPtrs are put into a safe state.
+  return new (raw) SharedScriptData(codeLength, noteLength, natoms);
 }
 
 inline js::ScriptBytecodeHasher::Lookup::Lookup(SharedScriptData* data)
@@ -2992,19 +3032,6 @@ void js::FreeScriptData(JSRuntime* rt) {
   }
 
   table.clear();
-}
-
-// Placement-new elements of an array. This should optimize away for types with
-// trivial default initiation.
-template <typename T>
-static void DefaultInitializeElements(void* arrayPtr, size_t length) {
-  uintptr_t elem = reinterpret_cast<uintptr_t>(arrayPtr);
-  MOZ_ASSERT(elem % alignof(T) == 0);
-
-  for (size_t i = 0; i < length; ++i) {
-    new (reinterpret_cast<T*>(elem)) T;
-    elem += sizeof(T);
-  }
 }
 
 /* static */
