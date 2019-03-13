46a76e6cb4c56950a7ec96e122684498ad9bb2f4
Boris Zbarsky <bzbarsky@mit.edu>
2019-03-13 00:43:48 +0000

    Bug 1534370 part 1.  Annotate doCommandWithParams as MOZ_CAN_RUN_SCRIPT.  r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D23036

--- dom/commandhandler/nsBaseCommandController.cpp
+++ dom/commandhandler/nsBaseCommandController.cpp
@@ -129,13 +129,12 @@ nsBaseCommandController::DoCommandWithParams(const char* aCommand,
   NS_ENSURE_ARG_POINTER(aCommand);
   NS_ENSURE_STATE(mCommandTable);
 
-  nsISupports* context = mCommandContextRawPtr;
-  nsCOMPtr<nsISupports> weak;
+  nsCOMPtr<nsISupports> context = mCommandContextRawPtr;
   if (!context) {
-    weak = do_QueryReferent(mCommandContextWeakPtr);
-    context = weak;
+    context = do_QueryReferent(mCommandContextWeakPtr);
   }
-  return mCommandTable->DoCommandParams(aCommand, aParams, context);
+  nsCOMPtr<nsIControllerCommandTable> commandTable(mCommandTable);
+  return commandTable->DoCommandParams(aCommand, aParams, context);
 }
 
 NS_IMETHODIMP
