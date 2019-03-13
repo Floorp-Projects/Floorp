f6f051e0186ee509ecc3c936b0a137d7ab0bc76d
Chris Pearce <cpearce@mozilla.com>
2019-03-13 09:24:52 +0000

    Bug 1530245 - Launch sandbox from new remote sandbox broker process. r=jld,bobowen
    Depends on D22051
    
    Differential Revision: https://phabricator.services.mozilla.com/D22052

---
+++ security/sandbox/win/src/remotesandboxbroker/RemoteSandboxBrokerProcessChild.cpp
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "RemoteSandboxBrokerProcessChild.h"
+
+#include "mozilla/ipc/IOThreadChild.h"
+#include "mozilla/BackgroundHangMonitor.h"
+
+using mozilla::ipc::IOThreadChild;
+
+namespace mozilla {
+
+RemoteSandboxBrokerProcessChild::RemoteSandboxBrokerProcessChild(
+    ProcessId aParentPid)
+    : ProcessChild(aParentPid) {}
+
+RemoteSandboxBrokerProcessChild::~RemoteSandboxBrokerProcessChild() {}
+
+bool RemoteSandboxBrokerProcessChild::Init(int aArgc, char* aArgv[]) {
+  BackgroundHangMonitor::Startup();
+  return mSandboxBrokerChild.Init(ParentPid(), IOThreadChild::message_loop(),
+                                  IOThreadChild::channel());
+}
+
+void RemoteSandboxBrokerProcessChild::CleanUp() {
+  BackgroundHangMonitor::Shutdown();
+}
+
+}  // namespace mozilla
