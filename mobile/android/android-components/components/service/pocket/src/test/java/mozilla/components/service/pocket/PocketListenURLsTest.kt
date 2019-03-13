c313a3da5eae9787aecf58c8869ea7ad2622e851
Michael Comella <michael.l.comella@gmail.com>
2019-03-12 17:37:04 -0700

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/2336: Add PocketListenURLs.

---
+++ mobile/android/android-components/components/service/pocket/src/test/java/mozilla/components/service/pocket/PocketListenURLsTest.kt
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.service.pocket
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class PocketListenURLsTest {
+
+    private lateinit var urls: PocketListenURLs
+
+    @Before
+    fun setUp() {
+        urls = PocketListenURLs()
+    }
+
+    @Test
+    fun `WHEN getting the articleservice url THEN the last path segment is for articleservice`() {
+        assertEquals("articleservice", urls.articleService.lastPathSegment)
+    }
+}
