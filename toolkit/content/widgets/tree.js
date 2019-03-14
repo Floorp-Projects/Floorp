822a58f809dedf680c82663af3be7c1df76a2a0c
Alexander Surkov <surkov.alexander@gmail.com>
2019-03-14 21:44:54 +0000

    Bug 1534685 - make all custom elements to use the attribute inheritance base implementation, r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D23541
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/content/widgets/tree.js
+++ toolkit/content/widgets/tree.js
@@ -519,46 +519,33 @@
             <hbox flex="1" class="tree-bodybox">
               <html:slot name="treechildren"></html:slot>
             </hbox>
-            <scrollbar height="0" minwidth="0" minheight="0" orient="vertical" inherits="collapsed=hidevscroll" style="position:relative; z-index:2147483647;" oncontextmenu="event.stopPropagation(); event.preventDefault();" onclick="event.stopPropagation(); event.preventDefault();" ondblclick="event.stopPropagation();" oncommand="event.stopPropagation();"></scrollbar>
+            <scrollbar height="0" minwidth="0" minheight="0" orient="vertical"
+                       class="hidevscroll-scrollbar"
+                       style="position:relative; z-index:2147483647;"
+                       oncontextmenu="event.stopPropagation(); event.preventDefault();"
+                       onclick="event.stopPropagation(); event.preventDefault();"
+                       ondblclick="event.stopPropagation();"
+                       oncommand="event.stopPropagation();"></scrollbar>
           </hbox>
           <textbox class="tree-input" left="0" top="0" hidden="true"></textbox>
         </stack>
-        <hbox inherits="collapsed=hidehscroll">
+        <hbox class="hidehscroll-box">
           <scrollbar orient="horizontal" flex="1" increment="16" style="position:relative; z-index:2147483647;" oncontextmenu="event.stopPropagation(); event.preventDefault();" onclick="event.stopPropagation(); event.preventDefault();" ondblclick="event.stopPropagation();" oncommand="event.stopPropagation();"></scrollbar>
-          <scrollcorner inherits="collapsed=hidevscroll" oncontextmenu="event.stopPropagation(); event.preventDefault();" onclick="event.stopPropagation(); event.preventDefault();" ondblclick="event.stopPropagation();" oncommand="event.stopPropagation();"></scrollcorner>
+          <scrollcorner class="hidevscroll-scrollcorner"
+                        oncontextmenu="event.stopPropagation(); event.preventDefault();"
+                        onclick="event.stopPropagation(); event.preventDefault();"
+                        ondblclick="event.stopPropagation();"
+                        oncommand="event.stopPropagation();"></scrollcorner>
         </hbox>
       `));
     }
 
-    static get observedAttributes() {
-      return [
-        "hidehscroll",
-        "hidevscroll",
-      ];
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      if (this.isConnectedAndReady && oldValue != newValue) {
-        this._updateAttributes();
-      }
-    }
-
-    _updateAttributes() {
-      for (let [ el, attrs ] of this._inheritedAttributeMap.entries()) {
-        for (let attr of attrs) {
-          this.inheritAttribute(el, attr);
-        }
-      }
-    }
-
-    get _inheritedAttributeMap() {
-      if (!this.__inheritedAttributeMap) {
-        this.__inheritedAttributeMap = new Map();
-        for (let el of this.shadowRoot.querySelectorAll("[inherits]")) {
-          this.__inheritedAttributeMap.set(el, el.getAttribute("inherits").split(","));
-        }
-      }
-      return this.__inheritedAttributeMap;
+    static get inheritedAttributes() {
+      return {
+        ".hidehscroll-box": "collapsed=hidehscroll",
+        ".hidevscroll-scrollbar": "collapsed=hidevscroll",
+        ".hidevscroll-scrollcorner": "collapsed=hidevscroll",
+      };
     }
 
     connectedCallback() {
@@ -574,7 +561,7 @@
       this.setAttribute("hidehscroll", "true");
       this.setAttribute("clickthrough", "never");
 
-      this._updateAttributes();
+      this.initializeAttributeInheritance();
 
       this.pageUpOrDownMovesSelection = !/Mac/.test(navigator.platform);
 
