3ed69b71f6215054a9426b0b6e69a5ea6ac085f4
Ola Gasidlo <ogasidlo@mozilla.com>
2019-03-15 14:19:17 +0000

    Bug 1505128 - Add feature to disconnect from a device, r=ladybenko,daisuke
    Added disconnect from device funtion + button to be displayed in runtime page when device is connected
    
    Differential Revision: https://phabricator.services.mozilla.com/D18588

--- devtools/client/aboutdebugging-new/src/components/RuntimeInfo.js
+++ devtools/client/aboutdebugging-new/src/components/RuntimeInfo.js
@@ -8,8 +8,11 @@ const { createFactory, PureComponent } = require("devtools/client/shared/vendor/
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 
+const Actions = require("../actions/index");
 const FluentReact = require("devtools/client/shared/vendor/fluent-react");
 const Localized = createFactory(FluentReact.Localized);
+
+const { RUNTIMES } = require("../constants");
 
 /**
  * This component displays runtime information.
@@ -17,15 +20,16 @@ const Localized = createFactory(FluentReact.Localized);
 class RuntimeInfo extends PureComponent {
   static get propTypes() {
     return {
+      dispatch: PropTypes.func.isRequired,
       icon: PropTypes.string.isRequired,
       deviceName: PropTypes.string,
       name: PropTypes.string.isRequired,
       version: PropTypes.string.isRequired,
+      runtimeId: PropTypes.string.isRequired,
     };
   }
-
   render() {
-    const { icon, deviceName, name, version } = this.props;
+    const { icon, deviceName, name, version, runtimeId, dispatch } = this.props;
 
     return dom.h1(
       {
@@ -57,14 +61,16 @@ class RuntimeInfo extends PureComponent {
           },
           deviceName
         ) : null,
-      dom.button(
-        {
-          className: "default-button runtime-info__action",
-          // Implementation ongoing in Bug 1505128.
-          disabled: true,
-        },
-        "Disconnect"
-      )
+      runtimeId !== RUNTIMES.THIS_FIREFOX ?
+        dom.button(
+          {
+            className: "default-button runtime-info__action qa-runtime-info__action",
+            onClick() {
+              dispatch(Actions.disconnectRuntime(runtimeId, true));
+            },
+          },
+          "Disconnect"
+        ) : null,
     );
   }
 }
