d20600ae81bf21e85cb2120845e97835f5b7cc59
Michael Comella <michael.l.comella@gmail.com>
2019-03-15 16:09:08 -0700

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/2336: Correct test naming in PocketEndpointTest.

--- mobile/android/android-components/components/service/pocket/src/test/java/mozilla/components/service/pocket/PocketEndpointTest.kt
+++ mobile/android/android-components/components/service/pocket/src/test/java/mozilla/components/service/pocket/PocketEndpointTest.kt
@@ -87,23 +87,23 @@ class PocketEndpointTest {
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun `WHEN getInstance is called with a blank API key THEN an exception is thrown`() {
+    fun `WHEN newInstance is called with a blank API key THEN an exception is thrown`() {
         PocketEndpoint.newInstance(client, " ", VALID_USER_AGENT)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun `WHEN getInstance is called with a blank user agent THEN an exception is thrown`() {
+    fun `WHEN newInstance is called with a blank user agent THEN an exception is thrown`() {
         PocketEndpoint.newInstance(client, VALID_API_KEY, " ")
     }
 
     @Test
-    fun `WHEN getInstance is called with valid args THEN no exception is thrown`() {
+    fun `WHEN newInstance is called with valid args THEN no exception is thrown`() {
         PocketEndpoint.newInstance(client, VALID_API_KEY, VALID_USER_AGENT)
     }
 
     @Test
     @Ignore("Requires a network request; run locally to test end-to-end behavior")
-    fun `WHEN making an end-to-end request THEN 20 results are returned`() {
+    fun `WHEN making an end-to-end global video recommendations request THEN 20 results are returned`() {
         // To use this test locally, add the API key file (see API_KEY comments) and comment out ignore annotation.
         val client = HttpURLConnectionClient()
         val apiKey = PocketTestResource.API_KEY.get()
