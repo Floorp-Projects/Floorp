7a0b20a1da992625faa8a54addcc05ca0bcc824a
Sebastian Kaspari <s.kaspari@gmail.com>
2019-03-15 13:32:28 +0100

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/1499: Rewrite gradle publish script based on "android-maven-publish" plugin.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -23,7 +23,7 @@ object Versions {
 
     const val dokka = "0.9.17"
     const val android_gradle_plugin = "3.2.1"
-    const val maven_gradle_plugin = "2.1"
+    const val android_maven_publish_plugin = "3.6.2"
     const val lint = "26.3.2"
 
     const val sentry = "1.7.21"
@@ -76,7 +76,7 @@ object Dependencies {
     const val tools_dokka = "org.jetbrains.dokka:dokka-android-gradle-plugin:${Versions.dokka}"
     const val tools_androidgradle = "com.android.tools.build:gradle:${Versions.android_gradle_plugin}"
     const val tools_kotlingradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-    const val tools_mavengradle = "com.github.dcendents:android-maven-gradle-plugin:${Versions.maven_gradle_plugin}"
+    const val tools_androidmavenpublish = "digital.wup:android-maven-publish:${Versions.android_maven_publish_plugin}"
 
     const val tools_lint = "com.android.tools.lint:lint:${Versions.lint}"
     const val tools_lintapi = "com.android.tools.lint:lint-api:${Versions.lint}"
