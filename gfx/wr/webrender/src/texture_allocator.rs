2f7292d5521aa55bb74e172de5095606c410eaa1
Noemi Erli <nerli@mozilla.com>
2019-03-15 16:00:33 +0200

    Backed out 2 changesets (bug 1535273) for webrender bustages CLOSED TREE
    Backed out changeset ae26f98bfa7c (bug 1535273)
    Backed out changeset df478b79c9b3 (bug 1535273)

--- gfx/wr/webrender/src/texture_allocator.rs
+++ gfx/wr/webrender/src/texture_allocator.rs
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use api::units::{DeviceIntPoint, DeviceIntRect, DeviceIntSize};
+use util;
 
 //TODO: gather real-world statistics on the bin usage in order to assist the decision
 // on where to place the size thresholds.
@@ -166,10 +167,10 @@ impl ArrayAllocationTracker {
         }
 
         // Add the guillotined rects back to the free list.
-        if !new_free_rect_to_right.is_empty() {
+        if !util::rect_is_empty(&new_free_rect_to_right) {
             self.push(chosen.slice, new_free_rect_to_right);
         }
-        if !new_free_rect_to_bottom.is_empty() {
+        if !util::rect_is_empty(&new_free_rect_to_bottom) {
             self.push(chosen.slice, new_free_rect_to_bottom);
         }
     }
