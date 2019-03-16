c5ef7fcec96ae6e30871b039712c977cdd17f70e
Narcis Beleuzu <nbeleuzu@mozilla.com>
2019-03-16 06:11:21 +0200

    Backed out changeset 6e1f82657583 for debugger failures on Breakpoints.css . CLOSED TREE

--- devtools/client/debugger/new/src/components/Editor/ColumnBreakpoints.js
+++ devtools/client/debugger/new/src/components/Editor/ColumnBreakpoints.js
@@ -7,6 +7,7 @@
 import React, { Component } from "react";
 
 import ColumnBreakpoint from "./ColumnBreakpoint";
+import "./ColumnBreakpoints.css";
 
 import { getSelectedSource, visibleColumnBreakpoints } from "../../selectors";
 import { connect } from "../../utils/connect";
