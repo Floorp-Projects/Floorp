22a42d62303eb16a764c2a076accdf6d74ef9b9a
Mike Shal <mshal@mozilla.com>
2019-03-18 23:52:31 +0000

    Bug 632954 - Add an explicit __llvm_profile_dump() call for Android; r=snorp
    When Android shuts down the ndk process, it doesn't call the registered
    atexit() handlers, which is normally where the profile data gets written
    to file. Since the PGO test suite closes the browser when it is
    finished, the nativeRun routine can manually call out to
    __llvm_profile_dump() before returning.
    
    This method has a downside that only the profile data from the calling
    library gets written out, rather than for the whole process. Since we
    are most interested in optimizing libxul, a new hook is added in
    Bootstrap to make sure we get the profile data for the right library.
    
    Differential Revision: https://phabricator.services.mozilla.com/D22817

--- toolkit/xre/Bootstrap.cpp
+++ toolkit/xre/Bootstrap.cpp
@@ -8,6 +8,12 @@
 
 #include "AutoSQLiteLifetime.h"
 
+#ifdef MOZ_WIDGET_ANDROID
+#  ifdef MOZ_PROFILE_GENERATE
+extern "C" int __llvm_profile_dump(void);
+#  endif
+#endif
+
 namespace mozilla {
 
 class BootstrapImpl final : public Bootstrap {
@@ -75,6 +81,14 @@ class BootstrapImpl final : public Bootstrap {
       JNIEnv* aEnv, const XRE_AndroidChildFds& aFds) override {
     ::XRE_SetAndroidChildFds(aEnv, aFds);
   }
+
+#  ifdef MOZ_PROFILE_GENERATE
+  virtual void XRE_WriteLLVMProfData() override {
+    __android_log_print(ANDROID_LOG_INFO, "GeckoLibLoad",
+                        "Calling __llvm_profile_dump()");
+    __llvm_profile_dump();
+  }
+#  endif
 #endif
 
 #ifdef LIBFUZZER
