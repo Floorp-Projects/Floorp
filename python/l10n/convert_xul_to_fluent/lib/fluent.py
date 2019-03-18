81ddec33405cd2e6c0212c9b8bdb71bdb6a35029
Nicholas Cowles <cowlesni@msu.edu>
2019-03-18 18:05:13 +0000

    Bug 1517528 - Capstone: convert about:privatebrowsing using gandalf's conversion script, r=Gijs,jaws,zbraniecki,flod
    Differential Revision: https://phabricator.services.mozilla.com/D19250
    
    --HG--
    extra : moz-landing-system : lando

---
+++ python/l10n/convert_xul_to_fluent/lib/fluent.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from syntax import ast
+from syntax.serializer import FluentSerializer
+
+
+def get_value_from_dtd(name, dtd):
+    return dtd[name[1:-1]]['value']
+
+
+def build_ftl(messages, dtd, data):
+    res = ast.Resource()
+
+    for id_str in messages:
+        msg = messages[id_str]
+        l10n_id = ast.Identifier(id_str)
+        val = None
+        attrs = []
+        if msg['value']:
+            dtd_val = get_value_from_dtd(msg['value'], dtd)
+            val = ast.Pattern([ast.TextElement(dtd_val)])
+        for attr_name in msg['attrs']:
+            dtd_val = get_value_from_dtd(msg['attrs'][attr_name], dtd)
+            attr_val = ast.Pattern([ast.TextElement(dtd_val)])
+            attrs.append(ast.Attribute(ast.Identifier(attr_name), attr_val))
+
+        m = ast.Message(l10n_id, val, attrs)
+        res.body.append(m)
+
+    serializer = FluentSerializer()
+    return serializer.serialize(res)
