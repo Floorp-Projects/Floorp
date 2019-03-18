56c4777eb7bc046c6af122b4ddacaa8bfdb0bd40
tanhengyeow <E0032242@u.nus.edu>
2019-03-18 09:56:56 +0000

    Bug 1409920 - add 404 as a filter option for the network panel. r=Honza
    Allow number to be mapped to "status-code:<number>"
    
    Differential Revision: https://phabricator.services.mozilla.com/D21183

--- devtools/client/netmonitor/src/constants.js
+++ devtools/client/netmonitor/src/constants.js
@@ -337,6 +337,59 @@ const TIMING_KEYS = [
 // Default width of columns (which are not defined in DEFAULT_COLUMNS_DATA) is 8%
 const MIN_COLUMN_WIDTH = 30; // in px
 const DEFAULT_COLUMN_WIDTH = 8; // in %
+/**
+ * A mapping of HTTP status codes.
+ */
+const SUPPORTED_HTTP_CODES = [
+  "100",
+  "101",
+  "200",
+  "201",
+  "202",
+  "203",
+  "204",
+  "205",
+  "206",
+  "300",
+  "301",
+  "302",
+  "303",
+  "304",
+  "307",
+  "308",
+  "400",
+  "401",
+  "403",
+  "404",
+  "405",
+  "406",
+  "407",
+  "408",
+  "409",
+  "410",
+  "411",
+  "412",
+  "413",
+  "414",
+  "415",
+  "416",
+  "417",
+  "418",
+  "422",
+  "425",
+  "426",
+  "428",
+  "429",
+  "431",
+  "451",
+  "500",
+  "501",
+  "502",
+  "503",
+  "504",
+  "505",
+  "511",
+];
 
 const general = {
   ACTIVITY_TYPE,
@@ -352,6 +405,7 @@ const general = {
   TIMING_KEYS,
   MIN_COLUMN_WIDTH,
   DEFAULT_COLUMN_WIDTH,
+  SUPPORTED_HTTP_CODES,
 };
 
 // flatten constants
