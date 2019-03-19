3905101073d2e974649e9886ebcb9bcf56892c5f
Jesse Schwartzentruber <truber@mozilla.com>
2019-03-19 17:01:38 +0000

    Bug 1519851 - Create --enable-fuzzing debug build job for MacOSX. r=dustin
    Differential Revision: https://phabricator.services.mozilla.com/D19862
    
    --HG--
    extra : moz-landing-system : lando

---
+++ testing/mozharness/configs/builds/releng_sub_mac_configs/64_cross_fuzzing_debug.py
@@ -0,0 +1,24 @@
+import os
+
+config = {
+    'stage_platform': 'macosx64-fuzzing-debug',
+    'debug_build': True,
+    #### 64 bit build specific #####
+    'env': {
+        'MOZBUILD_STATE_PATH': os.path.join(os.getcwd(), '.mozbuild'),
+        'HG_SHARE_BASE_DIR': '/builds/hg-shared',
+        'MOZ_OBJDIR': '%(abs_obj_dir)s',
+        'TINDERBOX_OUTPUT': '1',
+        'TOOLTOOL_CACHE': '/builds/tooltool_cache',
+        'TOOLTOOL_HOME': '/builds',
+        'MOZ_CRASHREPORTER_NO_REPORT': '1',
+        'LC_ALL': 'C',
+        'XPCOM_DEBUG_BREAK': 'stack-and-abort',
+        ## 64 bit specific
+        'PATH': '/tools/python/bin:/opt/local/bin:/usr/bin:'
+                '/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin',
+        ##
+    },
+    #######################
+    'artifact_flag_build_variant_in_try': None,
+}
