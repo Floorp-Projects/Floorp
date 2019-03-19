88f2c1265c2f829d59ebffc839e028c1a9dc9d45
Coroiu Cristina <ccoroiu@mozilla.com>
2019-03-19 10:35:21 +0200

    Backed out 2 changesets (bug 1257982) for build bustage at build/src/js/src/frontend/ParseContext.h on a CLOSED TREE
    Backed out changeset e73fdb7ff05d (bug 1257982)
    Backed out changeset 13425f1eca22 (bug 1257982)

--- js/src/frontend/ParseContext.h
+++ js/src/frontend/ParseContext.h
@@ -78,11 +78,6 @@ class UsedNameTracker {
 
     UsedNameInfo(UsedNameInfo&& other) : uses_(std::move(other.uses_)) {}
 
-    UsedNameInfo& operator=(UsedNameInfo&& other) {
-      uses_ = std::move(other.uses_);
-      return *this;
-    }
-
     bool noteUsedInScope(uint32_t scriptId, uint32_t scopeId) {
       if (uses_.empty() || uses_.back().scopeId < scopeId) {
         return uses_.append(Use{scriptId, scopeId});
@@ -110,13 +105,11 @@ class UsedNameTracker {
     }
   };
 
-  using UsedNameMap = GCHashMap<JSAtom*, UsedNameInfo, DefaultHasher<JSAtom*>>;
+  using UsedNameMap = HashMap<JSAtom*, UsedNameInfo, DefaultHasher<JSAtom*>>;
 
  private:
-  // The map of names to chains of uses. UsedNameTracker is not used in LIFO
-  // order with Rooteds, so must use PersistentRooted (for the arbitrary
-  // ordering capability, not for persistence.)
-  PersistentRooted<UsedNameMap> map_;
+  // The map of names to chains of uses.
+  UsedNameMap map_;
 
   // Monotonically increasing id for all nested scripts.
   uint32_t scriptCounter_;
@@ -126,7 +119,7 @@ class UsedNameTracker {
 
  public:
   explicit UsedNameTracker(JSContext* cx)
-      : map_(cx, cx), scriptCounter_(0), scopeCounter_(0) {}
+      : map_(cx), scriptCounter_(0), scopeCounter_(0) {}
 
   uint32_t nextScriptId() {
     MOZ_ASSERT(scriptCounter_ != UINT32_MAX,
@@ -671,13 +664,5 @@ class ParseContext : public Nestable<ParseContext> {
 }  // namespace frontend
 
 }  // namespace js
-
-namespace JS {
-
-template <>
-struct GCPolicy<js::frontend::UsedNameTracker::UsedNameInfo>
-    : public IgnoreGCPolicy {};
-
-}  // namespace JS
 
 #endif  // frontend_ParseContext_h
