26e0094e0ff1f1fa5c5400f34c6f2cca1f59da52
Boris Zbarsky <bzbarsky@mit.edu>
2019-03-19 20:53:55 +0000

    Bug 1535384 part 9.  Remove MOZ_CAN_RUN_SCRIPT_BOUNDARY for u2f callbacks.  r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D23780

--- dom/webauthn/WebAuthnTransactionChild.h
+++ dom/webauthn/WebAuthnTransactionChild.h
@@ -25,14 +25,23 @@ class WebAuthnTransactionChild final : public PWebAuthnTransactionChild {
   NS_INLINE_DECL_REFCOUNTING(WebAuthnTransactionChild);
   explicit WebAuthnTransactionChild(WebAuthnManagerBase* aManager);
 
+  // MOZ_CAN_RUN_SCRIPT_BOUNDARY until we can do MOZ_CAN_RUN_SCRIPT in
+  // IPDL-generated things.
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY
   mozilla::ipc::IPCResult RecvConfirmRegister(
       const uint64_t& aTransactionId,
       const WebAuthnMakeCredentialResult& aResult);
 
+  // MOZ_CAN_RUN_SCRIPT_BOUNDARY until we can do MOZ_CAN_RUN_SCRIPT in
+  // IPDL-generated things.
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY
   mozilla::ipc::IPCResult RecvConfirmSign(
       const uint64_t& aTransactionId,
       const WebAuthnGetAssertionResult& aResult);
 
+  // MOZ_CAN_RUN_SCRIPT_BOUNDARY until we can do MOZ_CAN_RUN_SCRIPT in
+  // IPDL-generated things.
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY
   mozilla::ipc::IPCResult RecvAbort(const uint64_t& aTransactionId,
                                     const nsresult& aError);
 
