e0be560c3d5b7082406ec020e5feacd9d2c7c25c
Ehsan Akhgari <ehsan@mozilla.com>
2019-03-19 06:26:01 +0000

    Bug 1536158 - Split browser_blockingServiceWorkers.js into two tests so that if we end up disabling part of it, we don't lose all of our test coverage; r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D23891
    
    --HG--
    rename : toolkit/components/antitracking/test/browser/browser_blockingServiceWorkers.js => toolkit/components/antitracking/test/browser/browser_blockingServiceWorkersStorageAccessAPI.js
    extra : moz-landing-system : lando

--- toolkit/components/antitracking/test/browser/browser_blockingServiceWorkers.js
+++ toolkit/components/antitracking/test/browser/browser_blockingServiceWorkers.js
@@ -1,5 +1,3 @@
-requestLongerTimeout(2);
-
 AntiTracking.runTest("ServiceWorkers",
   async _ => {
     await navigator.serviceWorker.register("empty.js").then(
@@ -17,67 +15,3 @@ AntiTracking.runTest("ServiceWorkers",
    ["dom.ipc.processCount", 1],
    ["dom.serviceWorkers.enabled", true],
    ["dom.serviceWorkers.testing.enabled", true]]);
-
-AntiTracking.runTest("ServiceWorkers and Storage Access API",
-  async _ => {
-    /* import-globals-from storageAccessAPIHelpers.js */
-    await noStorageAccessInitially();
-
-    await navigator.serviceWorker.register("empty.js").then(
-      _ => { ok(false, "ServiceWorker cannot be used!"); },
-      _ => { ok(true, "ServiceWorker cannot be used!"); }).
-      catch(e => ok(false, "Promise rejected: " + e));
-
-    /* import-globals-from storageAccessAPIHelpers.js */
-    await callRequestStorageAccess();
-
-    if (SpecialPowers.Services.prefs.getIntPref("network.cookie.cookieBehavior") == SpecialPowers.Ci.nsICookieService.BEHAVIOR_REJECT) {
-      await navigator.serviceWorker.register("empty.js").then(
-        _ => { ok(false, "ServiceWorker cannot be used!"); },
-        _ => { ok(true, "ServiceWorker cannot be used!"); }).
-        catch(e => ok(false, "Promise rejected: " + e));
-    } else {
-      await navigator.serviceWorker.register("empty.js").then(
-        reg => { ok(true, "ServiceWorker can be used!"); return reg; },
-        _ => { ok(false, "ServiceWorker cannot be used! " + _); }).then(
-        reg => reg.unregister(),
-        _ => { ok(false, "unregister failed"); }).
-        catch(e => ok(false, "Promise rejected: " + e));
-    }
-  },
-  async _ => {
-    /* import-globals-from storageAccessAPIHelpers.js */
-    if (allowListed) {
-      await hasStorageAccessInitially();
-    } else {
-      await noStorageAccessInitially();
-    }
-
-    await navigator.serviceWorker.register("empty.js").then(
-      reg => { ok(true, "ServiceWorker can be used!"); return reg; },
-      _ => { ok(false, "ServiceWorker cannot be used!"); }).then(
-      reg => reg.unregister(),
-      _ => { ok(false, "unregister failed"); }).
-      catch(e => ok(false, "Promise rejected: " + e));
-
-    /* import-globals-from storageAccessAPIHelpers.js */
-    await callRequestStorageAccess();
-
-    // For non-tracking windows, calling the API is a no-op
-    await navigator.serviceWorker.register("empty.js").then(
-      reg => { ok(true, "ServiceWorker can be used!"); return reg; },
-      _ => { ok(false, "ServiceWorker cannot be used!"); }).then(
-      reg => reg.unregister(),
-      _ => { ok(false, "unregister failed"); }).
-      catch(e => ok(false, "Promise rejected: " + e));
-  },
-  async _ => {
-    await new Promise(resolve => {
-      Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value => resolve());
-    });
-  },
-  [["dom.serviceWorkers.exemptFromPerDomainMax", true],
-   ["dom.ipc.processCount", 1],
-   ["dom.serviceWorkers.enabled", true],
-   ["dom.serviceWorkers.testing.enabled", true]],
-  false, false);
