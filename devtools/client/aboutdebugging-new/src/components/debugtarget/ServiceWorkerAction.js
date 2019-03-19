66a2f63ce7e5a93924320b44337700afe62e1492
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-03-19 11:20:30 +0000

    Bug 1529518: Move Start/Push/Unregister buttons of service worker item to bottom. r=ladybenko
    Depends on D21892
    
    Differential Revision: https://phabricator.services.mozilla.com/D21893

--- devtools/client/aboutdebugging-new/src/components/debugtarget/ServiceWorkerAction.js
+++ devtools/client/aboutdebugging-new/src/components/debugtarget/ServiceWorkerAction.js
@@ -5,18 +5,13 @@
 "use strict";
 
 const { createFactory, PureComponent } = require("devtools/client/shared/vendor/react");
-const dom = require("devtools/client/shared/vendor/react-dom-factories");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
 const { connect } = require("devtools/client/shared/vendor/react-redux");
-
-const FluentReact = require("devtools/client/shared/vendor/fluent-react");
-const Localized = createFactory(FluentReact.Localized);
 
 const { getCurrentRuntimeDetails } = require("../../modules/runtimes-state-helper");
 
 const InspectAction = createFactory(require("./InspectAction"));
 
-const Actions = require("../../actions/index");
 const Types = require("../../types/index");
 const { SERVICE_WORKER_STATUSES } = require("../../constants");
 
@@ -27,44 +22,10 @@ class ServiceWorkerAction extends PureComponent {
   static get propTypes() {
     return {
       dispatch: PropTypes.func.isRequired,
-      // Provided by wrapping the component with FluentReact.withLocalization.
-      getString: PropTypes.func.isRequired,
       // Provided by redux state
       runtimeDetails: Types.runtimeDetails.isRequired,
       target: Types.debugTarget.isRequired,
     };
-  }
-
-  push() {
-    const { dispatch, target } = this.props;
-    dispatch(Actions.pushServiceWorker(target.id));
-  }
-
-  start() {
-    const { dispatch, target } = this.props;
-    dispatch(Actions.startServiceWorker(target.details.registrationFront));
-  }
-
-  unregister() {
-    const { dispatch, target } = this.props;
-    dispatch(Actions.unregisterServiceWorker(target.details.registrationFront));
-  }
-
-  _renderButton({ className, disabled, key, labelId, onClick }) {
-    return Localized(
-      {
-        id: labelId,
-        key,
-      },
-      dom.button(
-        {
-          className,
-          disabled,
-          onClick: e => onClick(),
-        },
-        labelId,
-      )
-    );
   }
 
   _renderInspectAction() {
@@ -76,68 +37,11 @@ class ServiceWorkerAction extends PureComponent {
     });
   }
 
-  _renderPushButton() {
-    return this._renderButton({
-      className: "default-button js-push-button",
-      disabled: this.props.runtimeDetails.isMultiE10s,
-      key: "service-worker-push-button",
-      labelId: "about-debugging-worker-action-push",
-      onClick: this.push.bind(this),
-    });
-  }
-
-  _renderStartButton() {
-    return this._renderButton({
-      className: "default-button js-start-button",
-      disabled: this.props.runtimeDetails.isMultiE10s,
-      key: "service-worker-start-button",
-      labelId: "about-debugging-worker-action-start",
-      onClick: this.start.bind(this),
-    });
-  }
-
-  _renderUnregisterButton() {
-    return this._renderButton({
-      className: "default-button js-unregister-button",
-      key: "service-worker-unregister-button",
-      labelId: "about-debugging-worker-action-unregister",
-      onClick: this.unregister.bind(this),
-    });
-  }
-
-  _renderAction() {
-    const { status } = this.props.target.details;
-
-    switch (status) {
-      case SERVICE_WORKER_STATUSES.RUNNING:
-        return [
-          this._renderUnregisterButton(),
-          this._renderPushButton(),
-          this._renderInspectAction(),
-        ];
-      case SERVICE_WORKER_STATUSES.REGISTERING:
-        // Only inspect is available if the service worker is not active.
-        return [
-          this._renderInspectAction(),
-        ];
-      case SERVICE_WORKER_STATUSES.STOPPED:
-        return [
-          this._renderUnregisterButton(),
-          this._renderStartButton(),
-        ];
-      default:
-        console.error("Unexpected service worker status: " + status);
-        return [];
-    }
-  }
-
   render() {
-    return dom.div(
-      {
-        className: "toolbar",
-      },
-      this._renderAction()
-    );
+    const { status } = this.props.target.details;
+    const shallRenderInspectAction = status === SERVICE_WORKER_STATUSES.RUNNING ||
+                                     status === SERVICE_WORKER_STATUSES.REGISTERING;
+    return shallRenderInspectAction ? this._renderInspectAction() : null;
   }
 }
 
@@ -147,5 +51,4 @@ const mapStateToProps = state => {
   };
 };
 
-module.exports = FluentReact.withLocalization(
-  connect(mapStateToProps)(ServiceWorkerAction));
+module.exports = connect(mapStateToProps)(ServiceWorkerAction);
