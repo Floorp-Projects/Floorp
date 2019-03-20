73fd975583a14999b2ef6ef0be9ef95612e6d6a4
Christian Holler <choller@mozilla.com>
2019-03-20 14:40:44 +0000

    Bug 1526258 - Implement fuzzing layer in networking for socket emulation. r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D23194

---
+++ netwerk/base/FuzzyLayer.h
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef FuzzyLayer_h__
+#define FuzzyLayer_h__
+
+#include "prerror.h"
+
+namespace mozilla {
+namespace net {
+
+nsresult AttachFuzzyIOLayer(PRFileDesc *fd);
+
+}  // namespace net
+}  // namespace mozilla
+
+#endif  // FuzzyLayer_h__
