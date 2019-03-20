8d640df330370ba5f0f133a2f41eafa982bd0842
Julian Descottes <jdescottes@mozilla.com>
2019-03-20 17:59:08 +0000

    Bug 1487581 - Remove the wifi debugging preference;r=ladybenko
    Depends on D23864. Wifi preference is not used anymore, removing it as well for consistency.
    
    Differential Revision: https://phabricator.services.mozilla.com/D23865
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/preferences/devtools-client.js
+++ devtools/client/preferences/devtools-client.js
@@ -341,8 +341,6 @@ pref("devtools.responsive.showUserAgentInput", false);
 
 // Enable new about:debugging.
 pref("devtools.aboutdebugging.new-enabled", false);
-// Enable the wifi feature.
-pref("devtools.aboutdebugging.wifi", false);
 // Show process debug targets.
 pref("devtools.aboutdebugging.process-debugging", false);
 // Stringified array of network locations that users can connect to.
