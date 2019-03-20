3c4f3ce9d4f252c27651baef76007c942feec04a
Noemi Erli <nerli@mozilla.com>
2019-03-20 11:58:18 +0200

    Backed out 12 changesets (bug 632954) for causing Android Bpgo(run) pending jobs CLOSED TREE
    Backed out changeset 429c96e4de32 (bug 632954)
    Backed out changeset de8beacc5eb4 (bug 632954)
    Backed out changeset c151ebf303ca (bug 632954)
    Backed out changeset b96dd954a456 (bug 632954)
    Backed out changeset 26031d362333 (bug 632954)
    Backed out changeset 097f141a499d (bug 632954)
    Backed out changeset 6f5fc0d644dd (bug 632954)
    Backed out changeset 53d3443e55d9 (bug 632954)
    Backed out changeset 503bcac73583 (bug 632954)
    Backed out changeset 142ae187478d (bug 632954)
    Backed out changeset 0615c775a0cf (bug 632954)
    Backed out changeset 3dfc0e4f8e7c (bug 632954)

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_profile_use.py
+++
@@ -1,5 +0,0 @@
-config = {
-    'stage_platform': 'android-api-16',
-    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16/profile-use',
-    'multi_locale_config_platform': 'android',
-}
