f87c067abc4fff39067b607de5edc61ef2ee6fca
Lars T Hansen <lhansen@mozilla.com>
2019-03-21 13:06:47 +0100

    Bug 1537733 - Use halt instructions for haltingAlign.  r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D24334

--- js/src/jit/shared/IonAssemblerBufferWithConstantPools.h
+++ js/src/jit/shared/IonAssemblerBufferWithConstantPools.h
@@ -1103,6 +1103,10 @@ struct AssemblerBufferWithConstantPools
   }
 
   void align(unsigned alignment) {
+    align(alignment, alignFillInst_);
+  }
+
+  void align(unsigned alignment, uint32_t pattern) {
     MOZ_ASSERT(mozilla::IsPowerOfTwo(alignment));
     MOZ_ASSERT(alignment >= InstSize);
 
@@ -1127,7 +1131,7 @@ struct AssemblerBufferWithConstantPools
 
     inhibitNops_ = true;
     while ((sizeExcludingCurrentPool() & (alignment - 1)) && !this->oom()) {
-      putInt(alignFillInst_);
+      putInt(pattern);
     }
     inhibitNops_ = false;
   }
