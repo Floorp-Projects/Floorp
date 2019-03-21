b3b192650e68db3873b0799fa6c400baa6626c53
James Graham <james@hoppipolla.co.uk>
2019-03-21 12:40:01 +0000

    Bug 1536804 - Factor out a base class for the wpt metadata compilers, r=ato
    The manifest compilers follow a mostly common design so it seems
    reasonable to put the common parts into a base class, and allow
    subclasses to override the functionaility where required.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24177

--- testing/web-platform/tests/tools/wptrunner/wptrunner/manifestexpected.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/manifestexpected.py
@@ -2,7 +2,7 @@ import os
 import urlparse
 
 from wptmanifest.backends import static
-from wptmanifest.backends.static import ManifestItem
+from wptmanifest.backends.base import ManifestItem
 
 import expected
 
@@ -98,7 +98,7 @@ def leak_threshold(node):
 
 
 class ExpectedManifest(ManifestItem):
-    def __init__(self, name, test_path, url_base):
+    def __init__(self, node, test_path, url_base):
         """Object representing all the tests in a particular manifest
 
         :param name: Name of the AST Node associated with this object.
@@ -107,13 +107,14 @@ class ExpectedManifest(ManifestItem):
         :param test_path: Path of the test file associated with this manifest.
         :param url_base: Base url for serving the tests in this manifest
         """
+        name = node.data
         if name is not None:
             raise ValueError("ExpectedManifest should represent the root node")
         if test_path is None:
             raise ValueError("ExpectedManifest requires a test path")
         if url_base is None:
             raise ValueError("ExpectedManifest requires a base url")
-        ManifestItem.__init__(self, name)
+        ManifestItem.__init__(self, node)
         self.child_map = {}
         self.test_path = test_path
         self.url_base = url_base
@@ -229,13 +230,14 @@ class DirectoryManifest(ManifestItem):
     def lsan_max_stack_depth(self):
         return int_prop("lsan-max-stack-depth", self)
 
+
 class TestNode(ManifestItem):
-    def __init__(self, name):
+    def __init__(self, node, **kwargs):
         """Tree node associated with a particular test in a manifest
 
         :param name: name of the test"""
-        assert name is not None
-        ManifestItem.__init__(self, name)
+        assert node.data is not None
+        ManifestItem.__init__(self, node, **kwargs)
         self.updated_expected = []
         self.new_expected = []
         self.subtests = {}
@@ -318,12 +320,6 @@ class TestNode(ManifestItem):
 
 
 class SubtestNode(TestNode):
-    def __init__(self, name):
-        """Tree node associated with a particular subtest in a manifest
-
-        :param name: name of the subtest"""
-        TestNode.__init__(self, name)
-
     @property
     def is_empty(self):
         if self._data:
