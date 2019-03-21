8e005e602ef3adb46ea6b2b1f1006d3d08202923
Emilio Cobos Álvarez <emilio@crisal.io>
2019-03-21 23:42:08 +0100

    Bug 1536586 - followup: Remove unused #[macro_use]s.
    CLOSED TREE

--- servo/ports/geckolib/tests/lib.rs
+++ servo/ports/geckolib/tests/lib.rs
@@ -13,10 +13,8 @@
 
 extern crate atomic_refcell;
 extern crate cssparser;
-#[macro_use]
 extern crate cstr;
 extern crate geckoservo;
-#[macro_use]
 extern crate log;
 extern crate malloc_size_of;
 extern crate num_traits;
