1ba95db63b4e3f762d7201f756f3413492956a88
Noemi Erli <nerli@mozilla.com>
2019-03-21 20:39:02 +0200

    Backed out changeset cdb61e9a5308 (bug 1520587) for failures in test_feature_mainthreadio.js

--- tools/profiler/core/memory_hooks.cpp
+++ tools/profiler/core/memory_hooks.cpp
@@ -214,9 +214,6 @@ void install_memory_counter(bool aInstall) {
       return;
     }
   }
-  if (aInstall) {
-    sCounter->Clear();
-  }
   // start counting memory allocations, or stop
   jemalloc_replace_dynamic(aInstall ? replace_init : nullptr);
 }
