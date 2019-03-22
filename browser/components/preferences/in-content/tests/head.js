3028ef133e1c373af7ed8f59c3aa5676c1320014
Brian Grinstead <bgrinstead@mozilla.com>
2019-03-22 20:49:53 +0000

    Bug 1536752 - Remove sync pane from about:preferences when identity.fxaccounts.enabled is false r=Gijs a=pascalc
    This fixes a bug in where we render out the text content of the template when searching.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24544

--- browser/components/preferences/in-content/tests/head.js
+++ browser/components/preferences/in-content/tests/head.js
@@ -109,7 +109,9 @@ function waitForEvent(aSubject, aEventName, aTimeoutMs, aTarget) {
 
 function openPreferencesViaOpenPreferencesAPI(aPane, aOptions) {
   return new Promise(resolve => {
-    let finalPrefPaneLoaded = TestUtils.topicObserved("sync-pane-loaded", () => true);
+    let finalPaneEvent = Services.prefs.getBoolPref("identity.fxaccounts.enabled") ?
+                           "sync-pane-loaded" : "privacy-pane-loaded";
+    let finalPrefPaneLoaded = TestUtils.topicObserved(finalPaneEvent, () => true);
     gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, "about:blank");
     openPreferences(aPane);
     let newTabBrowser = gBrowser.selectedBrowser;
@@ -142,9 +144,9 @@ async function evaluateSearchResults(keyword, searchReults) {
   for (let i = 0; i < mainPrefTag.childElementCount; i++) {
     let child = mainPrefTag.children[i];
     if (searchReults.includes(child.id)) {
-      is_element_visible(child, "Should be in search results");
+      is_element_visible(child, `${child.id} should be in search results`);
     } else if (child.id) {
-      is_element_hidden(child, "Should not be in search results");
+      is_element_hidden(child, `${child.id} should not be in search results`);
     }
   }
 }
