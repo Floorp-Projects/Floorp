16344e8acdf7fb7c4d08c1416a6cda2aa4958fc7
Mike Conley <mconley@mozilla.com>
2019-03-22 23:32:32 +0000

    Bug 1536209 - Add regression test to ensure we can re-request Picture-in-Picture on the same page. r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D24429

---
+++ toolkit/components/pictureinpicture/tests/browser_rerequestPiP.js
@@ -0,0 +1,28 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests that if a pre-existing Picture-in-Picture window exists, and a
+ * different video is requested to open in Picture-in-Picture, that the
+ * original Picture-in-Picture window closes and a new one is opened.
+ */
+add_task(async () => {
+  await BrowserTestUtils.withNewTab({
+    url: TEST_PAGE,
+    gBrowser,
+  }, async browser => {
+    let pipWin = await triggerPictureInPicture(browser, "with-controls");
+    ok(pipWin, "Got Picture-in-Picture window.");
+
+    let pipClosed = BrowserTestUtils.domWindowClosed(pipWin);
+    let pipWin2 = await triggerPictureInPicture(browser, "no-controls");
+    await pipClosed;
+    ok(true, "Original Picture-in-Picture window closed.");
+
+    pipClosed = BrowserTestUtils.domWindowClosed(pipWin2);
+    pipWin2.close();
+    await pipClosed;
+  });
+});
