d3bf6efcf0cc252187fdebf11c8b49df7e40327f
Matthew Gaudet <mgaudet@mozilla.com>
2019-03-22 15:31:27 +0000

    Bug 1505574 - Unhook Unboxed Objects option and disable by default r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D24036

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -5562,9 +5562,6 @@ JS_PUBLIC_API void JS_SetGlobalJitCompilerOption(JSContext* cx,
     case JSJITCOMPILER_TRACK_OPTIMIZATIONS:
       jit::JitOptions.disableOptimizationTracking = !value;
       break;
-    case JSJITCOMPILER_UNBOXED_OBJECTS:
-      jit::JitOptions.disableUnboxedObjects = !value;
-      break;
     case JSJITCOMPILER_SPECTRE_INDEX_MASKING:
       jit::JitOptions.spectreIndexMasking = !!value;
       break;
