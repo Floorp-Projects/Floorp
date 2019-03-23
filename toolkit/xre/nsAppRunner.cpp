8aa7013d57eb20d82e2983b5b677a5ae79617730
Jed Davis <jld@mozilla.com>
2019-03-23 10:25:18 +0000

    Bug 1516325. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D22638

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -2904,6 +2904,7 @@ static bool CheckForUserMismatch() { return false; }
 static void IncreaseDescriptorLimits() {
 #ifdef XP_UNIX
   // Increase the fd limit to accomodate IPC resources like shared memory.
+  // See also the Darwin case in config/external/nspr/pr/moz.build
   static const rlim_t kFDs = 4096;
   struct rlimit rlim;
 
