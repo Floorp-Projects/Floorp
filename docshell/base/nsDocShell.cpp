cee6f62d1225e1602baba8d97373e347469f3c50
Botond Ballo <botond@mozilla.com>
2019-03-23 20:23:44 +0000

    Bug 1531535 - Use the visual smooth scrolling API in nsDocShell::SetCurScrollPosEx() if appropriate. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D24554

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -5657,11 +5657,10 @@ nsresult nsDocShell::SetCurScrollPosEx(int32_t aCurHorizontalPos,
     return NS_OK;
   }
 
-  // TODO: If scrollMode == SMOOTH_MSD, this will effectively override that
-  // and jump to the target position instantly. A proper solution here would
-  // involve giving nsIScrollableFrame a visual viewport smooth scrolling API.
   shell->ScrollToVisual(targetPos, layers::FrameMetrics::eMainThread,
-                        nsIPresShell::ScrollMode::eInstant);
+                        scrollMode == nsIScrollableFrame::INSTANT
+                            ? nsIPresShell::ScrollMode::eInstant
+                            : nsIPresShell::ScrollMode::eSmooth);
 
   return NS_OK;
 }
