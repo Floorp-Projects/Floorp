a2f6a3ad5f42cc6a6755975da50f7d2f04b33cfc
Kartikaya Gupta <kgupta@mozilla.com>
2019-03-24 15:34:17 +0000

    Bug 1537734 - Update wrench android build to use offline mode in gradle. r=nalexander
    The updated cargo-apk version now correctly handles the `--frozen` flag
    and additionally translates it to the `--offline` flag when invoking
    gradle. This makes the gradle build fail instead of attempting network
    fetches. To make the offline gradle build work, we set up a build.gradle
    snippet that points to the maven repositories from the gradle toolchain
    artifact, and have cargo-apk use that instead of the default jcenter()
    repository.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24485

--- taskcluster/scripts/misc/wrench-android-build.sh
+++ taskcluster/scripts/misc/wrench-android-build.sh
@@ -7,11 +7,31 @@ mv wrench-deps/{vendor,.cargo,cargo-apk} gfx/wr
 popd
 
 pushd "${GECKO_PATH}/gfx/wr/wrench"
+# The following maven links are equivalent to GRADLE_MAVEN_REPOSITORIES, try
+# and keep in sync
+cat > build.gradle.inc <<END
+buildscript {
+    repositories {
+        maven{ url uri('file:${GECKO_PATH}/android-gradle-dependencies/google') }
+        maven{ url uri('file:${GECKO_PATH}/android-gradle-dependencies/jcenter') }
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:3.1.4'
+    }
+}
+allprojects {
+    repositories {
+        maven{ url uri('file:${GECKO_PATH}/android-gradle-dependencies/google') }
+        maven{ url uri('file:${GECKO_PATH}/android-gradle-dependencies/jcenter') }
+    }
+}
+END
 # These things come from the toolchain dependencies of the job that invokes
 # this script (webrender-wrench-android-build).
-export PATH="${PATH}:${GECKO_PATH}/rustc/bin:${GECKO_PATH}/android-gradle-dependencies/gradle-dist/bin"
 export PATH="${PATH}:${GECKO_PATH}/rustc/bin"
 export ANDROID_HOME="${GECKO_PATH}/android-sdk-linux"
 export NDK_HOME="${GECKO_PATH}/android-ndk"
+export CARGO_APK_GRADLE_COMMAND="${GECKO_PATH}/android-gradle-dependencies/gradle-dist/bin/gradle"
+export CARGO_APK_BUILD_GRADLE_INC="${PWD}/build.gradle.inc"
 ../cargo-apk/bin/cargo-apk build --frozen --verbose
 popd
