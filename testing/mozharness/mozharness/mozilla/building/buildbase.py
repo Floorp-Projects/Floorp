8ce6c4a53d9fdcd80c9c830a1aaabd9fbad4172d
Tom Prince <mozilla@hocat.ca>
2019-03-25 16:00:34 +0000

    Bug 1538134: [mozharness] Remove support for unused `builds/branch_specifics.py` config; r=aki a=tomprince
    We have moved per-branch configuration to taskcluster, so we can remove that
    support in mozharness.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24564
    
    --HG--
    extra : source : d5c2b9a1c21565ea40ff93bbedf431f093aa327c
    extra : amend_source : 0b54b1f7ffe035cfca54884ba6f4ba2709ed391f

--- testing/mozharness/mozharness/mozilla/building/buildbase.py
+++ testing/mozharness/mozharness/mozilla/building/buildbase.py
@@ -13,7 +13,6 @@ author: Jordan Lund
 import json
 
 import os
-import pprint
 import time
 import uuid
 import copy
@@ -255,7 +254,7 @@ class BuildingConfig(BaseConfig):
         # eg ('builds/branch_specifics.py', {'foo': 'bar'})
         all_config_dicts = []
         # important config files
-        variant_cfg_file = branch_cfg_file = pool_cfg_file = ''
+        variant_cfg_file = pool_cfg_file = ''
 
         # we want to make the order in which the options were given
         # not matter. ie: you can supply --branch before --build-pool
@@ -264,12 +263,11 @@ class BuildingConfig(BaseConfig):
         # ### The order from highest precedence to lowest is:
         # # There can only be one of these...
         # 1) build_pool: this can be either staging, pre-prod, and prod cfgs
-        # 2) branch: eg: mozilla-central, cedar, cypress, etc
-        # 3) build_variant: these could be known like asan and debug
+        # 2) build_variant: these could be known like asan and debug
         #                   or a custom config
         #
         # # There can be many of these
-        # 4) all other configs: these are any configs that are passed with
+        # 3) all other configs: these are any configs that are passed with
         #                       --cfg and --opt-cfg. There order is kept in
         #                       which they were passed on the cmd line. This
         #                       behaviour is maintains what happens by default
@@ -282,16 +280,13 @@ class BuildingConfig(BaseConfig):
                 if cf == BuildOptionParser.build_pool_cfg_file:
                     pool_cfg_file = all_config_files[i]
 
-            if cf == BuildOptionParser.branch_cfg_file:
-                branch_cfg_file = all_config_files[i]
-
             if cf == options.build_variant:
                 variant_cfg_file = all_config_files[i]
 
         # now remove these from the list if there was any.
         # we couldn't pop() these in the above loop as mutating a list while
         # iterating through it causes spurious results :)
-        for cf in [pool_cfg_file, branch_cfg_file, variant_cfg_file]:
+        for cf in [pool_cfg_file, variant_cfg_file]:
             if cf:
                 all_config_files.remove(cf)
 
@@ -310,14 +305,6 @@ class BuildingConfig(BaseConfig):
                 (variant_cfg_file, parse_config_file(variant_cfg_file))
             )
         config_paths = options.config_paths or ['.']
-        if branch_cfg_file:
-            # take only the specific branch, if present
-            branch_configs = parse_config_file(branch_cfg_file,
-                                               search_path=config_paths + [DEFAULT_CONFIG_PATH])
-            if branch_configs.get(options.branch or ""):
-                all_config_dicts.append(
-                    (branch_cfg_file, branch_configs[options.branch])
-                )
         if pool_cfg_file:
             # take only the specific pool. If we are here, the pool
             # must be present
@@ -391,7 +378,6 @@ class BuildOptionParser(object):
         'tup': 'builds/releng_sub_%s_configs/%s_tup.py',
     }
     build_pool_cfg_file = 'builds/build_pool_specifics.py'
-    branch_cfg_file = 'builds/branch_specifics.py'
 
     @classmethod
     def _query_pltfrm_and_bits(cls, target_option, options):
@@ -507,9 +493,7 @@ class BuildOptionParser(object):
 
     @classmethod
     def set_build_branch(cls, option, opt, value, parser):
-        # first let's add the branch_specific file where there may be branch
-        # specific keys/values. Then let's store the branch name we are using
-        parser.values.config_files.append(cls.branch_cfg_file)
+        # Store the branch name we are using
         setattr(parser.values, option.dest, value)  # the branch name
 
     @classmethod
@@ -570,12 +554,7 @@ BUILD_BASE_CONFIG_OPTIONS = [
         "callback": BuildOptionParser.set_build_branch,
         "type": "string",
         "dest": "branch",
-        "help": "This sets the branch we will be building this for."
-                " If this branch is in branch_specifics.py, update our"
-                " config with specific keys/values from that. See"
-                " %s for possibilites" % (
-                    BuildOptionParser.branch_cfg_file,
-                )}],
+        "help": "This sets the branch we will be building this for."}],
     [['--scm-level'], {
         "action": "store",
         "type": "int",
@@ -659,24 +638,12 @@ class BuildScript(AutomationMixin,
                 BuildOptionParser.bits
             )
         build_pool_cfg = BuildOptionParser.build_pool_cfg_file
-        branch_cfg = BuildOptionParser.branch_cfg_file
 
         cfg_match_msg = "Script was run with '%(option)s %(type)s' and \
 '%(type)s' matches a key in '%(type_config_file)s'. Updating self.config with \
 items from that key's value."
-        pf_override_msg = "The branch '%(branch)s' has custom behavior for the \
-platform '%(platform)s'. Updating self.config with the following from \
-'platform_overrides' found in '%(pf_cfg_file)s':"
 
         for i, (target_file, target_dict) in enumerate(cfg_files_and_dicts):
-            if branch_cfg and branch_cfg in target_file:
-                self.info(
-                    cfg_match_msg % {
-                        'option': '--branch',
-                        'type': c['branch'],
-                        'type_config_file': BuildOptionParser.branch_cfg_file
-                    }
-                )
             if build_pool_cfg and build_pool_cfg in target_file:
                 self.info(
                     cfg_match_msg % {
@@ -693,20 +660,6 @@ platform '%(platform)s'. Updating self.config with the following from \
                         'type_config_file': variant_cfg,
                     }
                 )
-        if c.get("platform_overrides"):
-            if c['stage_platform'] in c['platform_overrides'].keys():
-                self.info(
-                    pf_override_msg % {
-                        'branch': c['branch'],
-                        'platform': c['stage_platform'],
-                        'pf_cfg_file': BuildOptionParser.branch_cfg_file
-                    }
-                )
-                branch_pf_overrides = c['platform_overrides'][
-                    c['stage_platform']
-                ]
-                self.info(pprint.pformat(branch_pf_overrides))
-                c.update(branch_pf_overrides)
         self.info('To generate a config file based upon options passed and '
                   'config files used, run script as before but extend options '
                   'with "--dump-config"')
@@ -810,7 +763,8 @@ or run without that action (ie: --no-{action})"
         env['MOZ_BUILD_DATE'] = self.query_buildid()
 
         if self.query_is_nightly() or self.query_is_nightly_promotion():
-            # in branch_specifics.py we might set update_channel explicitly
+            # taskcluster sets the update channel for shipping builds
+            # explicitly
             if c.get('update_channel'):
                 update_channel = c['update_channel']
                 if isinstance(update_channel, unicode):
