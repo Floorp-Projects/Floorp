3e756a4e5de347f738551beba555735506cb6784
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-03-26 15:17:42 +0000

    Bug 1540170 - release assert if something tries to start the history service before profile startup, r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D24910
    
    --HG--
    extra : rebase_source : c21f2cceca996630f4e0d8442c0282e361073505

--- parser/xml/test/unit/test_sanitizer.js
+++ parser/xml/test/unit/test_sanitizer.js
@@ -1,7 +1,28 @@
+const {AppConstants} = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+if (AppConstants.platform != "android") {
+  // We load HTML documents, which try to track link state, which requires
+  // the history service, which requires a profile.
+  do_get_profile();
+}
+
 function run_test() {
   // vectors by the html5security project (https://code.google.com/p/html5security/ & Creative Commons 3.0 BY), see CC-BY-LICENSE for the full license
   load("results.js"); // gives us a `vectors' array
   /* import-globals-from ./results.js */
+
+  if (AppConstants.platform != "android") {
+    // xpcshell tests are weird. They fake shutdown after the test finishes. This upsets this test
+    // because it will try to create the history service to check for visited state on the links
+    // we're parsing.
+    // Creating the history service midway through shutdown breaks.
+    // We can't catch this in the history component because we're not *actually* shutting down,
+    // and so the app startup's service's `shuttingDown` bool is false, even though normally that
+    // is set to true *before* profile-change-teardown notifications are fired.
+    // To work around this, just force the history service to be created earlier:
+
+    let {PlacesUtils} = ChromeUtils.import("resource://gre/modules/PlacesUtils.jsm");
+    Assert.ok(PlacesUtils.history.databaseStatus <= 1, "ensure places database is successfully initialized.");
+  }
 
   var ParserUtils =  Cc["@mozilla.org/parserutils;1"].getService(Ci.nsIParserUtils);
   var sanitizeFlags = ParserUtils.SanitizerCidEmbedsOnly | ParserUtils.SanitizerDropForms | ParserUtils.SanitizerDropNonCSSPresentation;
