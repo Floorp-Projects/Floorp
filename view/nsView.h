d531d124739231ecc18b02a88006a14bdd30a39a
Adam Gashlin <agashlin@mozilla.com>
2019-03-26 10:49:50 -0700

    Bug 1498973: Don't show a popup that should be hidden. r=jmathies a=lizzard
    This includes the fix for the bug 1534935 regression.

--- view/nsView.h
+++ view/nsView.h
@@ -393,6 +393,7 @@ class nsView final : public nsIWidgetListener {
       const mozilla::TimeStamp& aCompositeStart,
       const mozilla::TimeStamp& aCompositeEnd) override;
   virtual void RequestRepaint() override;
+  virtual bool ShouldNotBeVisible() override;
   virtual nsEventStatus HandleEvent(mozilla::WidgetGUIEvent* aEvent,
                                     bool aUseAttachedEvents) override;
 
