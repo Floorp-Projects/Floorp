ff30d4d6f60db35853c40bc471ac5fca40a48827
Olli Pettay <Olli.Pettay@helsinki.fi>
2019-03-26 21:40:29 +0200

    Bug 1538794, add AnonymousContent.getTargetIdForEvent to be able to detect the event target in anonymous content, r=baku
    Differential Revision: https://phabricator.services.mozilla.com//D24955

--- dom/base/AnonymousContent.cpp
+++ dom/base/AnonymousContent.cpp
@@ -197,5 +197,16 @@ void AnonymousContent::GetComputedStylePropertyValue(
   aRv = cs->GetPropertyValue(aPropertyName, aResult);
 }
 
+void AnonymousContent::GetTargetIdForEvent(Event& aEvent, DOMString& aResult)
+{
+  nsCOMPtr<Element> el = do_QueryInterface(aEvent.GetOriginalTarget());
+  if (el && el->IsInNativeAnonymousSubtree() && mContentNode->Contains(el)) {
+    aResult.SetKnownLiveAtom(el->GetID(), DOMString::eTreatNullAsNull);
+    return;
+  }
+
+  aResult.SetNull();
+}
+
 }  // namespace dom
 }  // namespace mozilla
