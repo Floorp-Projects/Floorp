6006ae89476a0eb53b457c4ca1a1d3ddd086d3df
André Bargull <andre.bargull@gmail.com>
2019-03-26 12:27:54 -0700

    Bug 1526394: Treat self-hosted functions the same as non-interpreted functions in Debugger code. r=jorendorff

---
+++ js/src/jit-test/tests/debug/Environment-selfhosted-builtins.js
@@ -0,0 +1,15 @@
+// The environment of self-hosted builtins is not exposed to the debugger and
+// instead is reported as |undefined| just like native builtins.
+
+let g = newGlobal({newCompartment: true});
+
+let dbg = new Debugger();
+let gw = dbg.addDebuggee(g);
+
+// Array is a known native builtin function.
+let nativeBuiltin = gw.makeDebuggeeValue(g.Array);
+assertEq(nativeBuiltin.environment, undefined);
+
+// Array.prototype[@@iterator] is a known self-hosted builtin function.
+let selfhostedBuiltin = gw.makeDebuggeeValue(g.Array.prototype[Symbol.iterator]);
+assertEq(selfhostedBuiltin.environment, undefined);
