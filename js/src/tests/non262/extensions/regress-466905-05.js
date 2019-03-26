b55001e95ae1d42988eac44b0ed83ee890f2227c
André Bargull <andre.bargull@gmail.com>
2019-03-26 15:03:38 +0000

    Bug 1537923 - Part 2: Don't skip shell-only tests for all test configurations. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D24388

--- js/src/tests/non262/extensions/regress-466905-05.js
+++ js/src/tests/non262/extensions/regress-466905-05.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- no-op in browser, fails in shell - see bug 554793
+// |reftest| skip-if(!xulRuntime.shell) -- requires evalcx
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
