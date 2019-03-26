ee5540ae058abe0b5a5edbea7bc49f0ec51ada9e
violet <violet.bugreport@gmail.com>
2019-03-26 19:10:24 +0000

    Bug 1469155 - Add overloading for std::nullptr_t for RangedPtr equality operator r=froydnj
    Templated pointer can't match |nullptr|, we should add overloading for
    std::nullptr_t specifically to be able to support |nullptr|.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24925

--- mfbt/tests/TestRange.cpp
+++ mfbt/tests/TestRange.cpp
@@ -15,5 +15,10 @@ static_assert(IsConvertible<Range<int>, Range<const int>>::value,
 static_assert(!IsConvertible<Range<const int>, Range<int>>::value,
               "Range should not drop const in conversion");
 
+void test_RangeToBoolConversionShouldCompile() {
+  auto dummy = bool{Range<int>{}};
+  (void)dummy;
+}
+
 // We need a proper program so we have someplace to hang the static_asserts.
 int main() { return 0; }
