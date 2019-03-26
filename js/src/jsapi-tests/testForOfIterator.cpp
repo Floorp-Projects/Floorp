c40e81a2e904c328c3a7aaa176c0eada8fa54586
Jeff Walden <jwalden@mit.edu>
2019-03-26 17:10:44 +0000

    Bug 1537536 - Move JS::ForOfIterator to its own header.  r=arai,bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D24257
    
    --HG--
    rename : js/src/jsapi.h => js/public/ForOfIterator.h
    extra : moz-landing-system : lando

--- js/src/jsapi-tests/testForOfIterator.cpp
+++ js/src/jsapi-tests/testForOfIterator.cpp
@@ -5,6 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "js/ForOfIterator.h"
 #include "jsapi-tests/tests.h"
 
 BEGIN_TEST(testForOfIterator_basicNonIterable) {
