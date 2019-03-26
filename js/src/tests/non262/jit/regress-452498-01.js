32ac1b069ddeed7dec71d072f3db675d8ca3effd
André Bargull <andre.bargull@gmail.com>
2019-03-26 15:02:13 +0000

    Bug 1537923 - Part 1: Use "random" or "slow" instead of "skipped" for unreliable or slow tests. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D24387

--- js/src/tests/non262/jit/regress-452498-01.js
+++ js/src/tests/non262/jit/regress-452498-01.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- bogus perf test (bug 540512)
+// |reftest| random -- bogus perf test (bug 540512)
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- js/src/tests/non262/regress/regress-3649-n.js
+++ js/src/tests/non262/regress/regress-3649-n.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- skip test due to random oom related errors.
+// |reftest| random -- may cause random oom related errors.
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
