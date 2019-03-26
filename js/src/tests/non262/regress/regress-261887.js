5505c396ef335a9357777f509a4a69465eb3e869
André Bargull <andre.bargull@gmail.com>
2019-03-26 15:03:42 +0000

    Bug 1537923 - Part 3: Unskip passing tests. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D24389

--- js/src/tests/non262/regress/regress-261887.js
+++ js/src/tests/non262/regress/regress-261887.js
@@ -1,4 +1,3 @@
-// |reftest| skip -- we violate the spec here with our new iterators
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
