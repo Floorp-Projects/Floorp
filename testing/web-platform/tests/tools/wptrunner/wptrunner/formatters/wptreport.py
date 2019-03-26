84fb0c1d2357a6b8311a6394d29180b1d5a80013
Luke Zielinski <44509484+LukeZielinski@users.noreply.github.com>
2019-03-26 11:48:47 +0000

    Bug 1533340 [wpt PR 15655] - Split wptrunner formatters into separate files in their own directory, a=testonly
    Automatic update from web-platform-tests
    Split wptrunner formatters into separate files in their own directory (#15655)
    
    We have 2 formatters (wptreport, wptscreenshot) in the same file (formatters.py) right now. A third formatter (for better chromium integration) is being created and, instead of adding it to formatters.py, I propose creating a new formatters directory and splitting each formatter into its own file.
    --
    
    wpt-commits: 08c6fdd9aedf35e128554dece6a7cae676e4e162
    wpt-pr: 15655

--- testing/web-platform/tests/tools/wptrunner/wptrunner/formatters/wptreport.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/formatters/wptreport.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 from mozlog.structured.formatters.base import BaseFormatter
-from .executors.base import strip_server
+from ..executors.base import strip_server
 
 
 LONE_SURROGATE_RE = re.compile(u"[\uD800-\uDFFF]")
@@ -159,25 +159,3 @@ class WptreportFormatter(BaseFormatter):
         scope_data["total"].append({"bytes": data["bytes"],
                                     "threshold": data.get("threshold", 0),
                                     "process": data["process"]})
-
-
-class WptscreenshotFormatter(BaseFormatter):
-    """Formatter that outputs screenshots in the format expected by wpt.fyi."""
-
-    def __init__(self):
-        self.cache = set()
-
-    def suite_start(self, data):
-        # TODO: ask wpt.fyi for known hashes.
-        pass
-
-    def test_end(self, data):
-        if "reftest_screenshots" not in data.get("extra", {}):
-            return
-        output = ""
-        for item in data["extra"]["reftest_screenshots"]:
-            if type(item) != dict or item["hash"] in self.cache:
-                continue
-            self.cache.add(item["hash"])
-            output += "data:image/png;base64,{}\n".format(item["screenshot"])
-        return output if output else None
