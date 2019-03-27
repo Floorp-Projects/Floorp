1d06767ef8106ecd21ae28314f1423bf2da93667
Tom Prince <mozilla@hocat.ca>
2019-03-27 16:52:20 +0000

    Bug 1488313: [taskgraph] Move some functions for interacting with existing taskgraph to a more generic location; r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D13150

--- taskcluster/taskgraph/actions/util.py
+++ taskcluster/taskgraph/actions/util.py
@@ -22,38 +22,16 @@ from taskgraph.taskgraph import TaskGraph
 from taskgraph.optimize import optimize_task_graph
 from taskgraph.util.taskcluster import (
     get_session,
-    find_task_id,
     get_artifact,
     list_tasks,
     parse_time,
     CONCURRENCY,
 )
+from taskgraph.util.taskgraph import (
+    find_decision_task,
+)
 
 logger = logging.getLogger(__name__)
-
-
-def find_decision_task(parameters, graph_config):
-    """Given the parameters for this action, find the taskId of the decision
-    task"""
-    return find_task_id('{}.v2.{}.pushlog-id.{}.decision'.format(
-        graph_config['trust-domain'],
-        parameters['project'],
-        parameters['pushlog_id']))
-
-
-def find_existing_tasks_from_previous_kinds(full_task_graph, previous_graph_ids,
-                                            rebuild_kinds):
-    """Given a list of previous decision/action taskIds and kinds to ignore
-    from the previous graphs, return a dictionary of labels-to-taskids to use
-    as ``existing_tasks`` in the optimization step."""
-    existing_tasks = {}
-    for previous_graph_id in previous_graph_ids:
-        label_to_taskid = get_artifact(previous_graph_id, "public/label-to-taskid.json")
-        kind_labels = set(t.label for t in full_task_graph.tasks.itervalues()
-                          if t.attributes['kind'] not in rebuild_kinds)
-        for label in set(label_to_taskid.keys()).intersection(kind_labels):
-            existing_tasks[label] = label_to_taskid[label]
-    return existing_tasks
 
 
 def fetch_graph_and_labels(parameters, graph_config):
