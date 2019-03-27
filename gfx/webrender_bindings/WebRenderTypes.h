ec23629d9362c31c97532d125c93b88631a8b485
Kartikaya Gupta <kgupta@mozilla.com>
2019-03-27 11:16:52 +0000

    Bug 1538768 - Ensure that RenderRootArray<bool> is properly initialized to false on creation. r=dthayer
    Differential Revision: https://phabricator.services.mozilla.com/D24950

--- gfx/webrender_bindings/WebRenderTypes.h
+++ gfx/webrender_bindings/WebRenderTypes.h
@@ -18,6 +18,7 @@
 #include "mozilla/layers/LayersTypes.h"
 #include "mozilla/PodOperations.h"
 #include "mozilla/Range.h"
+#include "mozilla/TypeTraits.h"
 #include "mozilla/Variant.h"
 #include "Units.h"
 #include "nsStyleConsts.h"
@@ -91,12 +92,11 @@ template <typename T>
 class RenderRootArray : public Array<T, kRenderRootCount> {
   typedef Array<T, kRenderRootCount> Super;
  public:
-  RenderRootArray() {}
-
-  explicit RenderRootArray(T aDefault) {
-    for (auto renderRoot : kRenderRoots) {
-      (*this)[renderRoot] = aDefault;
-    }
+  RenderRootArray() {
+    if (IsPod<T>::value) {
+      // Ensure primitive types get initialized to 0/false.
+      PodArrayZero(*this);
+    } // else C++ will default-initialize the array elements for us
   }
 
   T& operator[](wr::RenderRoot aIndex) {
