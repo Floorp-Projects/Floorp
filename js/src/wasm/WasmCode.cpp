b8c8d32e27d607fe007a9971cfacda0320c69388
Luke Wagner <luke@mozilla.com>
2019-03-26 20:09:43 -0500

    Bug 1529957 - Baldr: create lazy stubs even more lazily (r=bbouvier)
    Differential Revision: https://phabricator.services.mozilla.com/D25010
    
    --HG--
    extra : rebase_source : 560d5732dac4862d4913a4510708f77cadd0eae5

--- js/src/wasm/WasmCode.cpp
+++ js/src/wasm/WasmCode.cpp
@@ -832,8 +832,10 @@ bool LazyStubTier::hasStub(uint32_t funcIndex) const {
 
 void* LazyStubTier::lookupInterpEntry(uint32_t funcIndex) const {
   size_t match;
-  MOZ_ALWAYS_TRUE(BinarySearch(ProjectLazyFuncIndex(exports_), 0,
-                               exports_.length(), funcIndex, &match));
+  if (!BinarySearch(ProjectLazyFuncIndex(exports_), 0, exports_.length(),
+                    funcIndex, &match)) {
+    return nullptr;
+  }
   const LazyFuncExport& fe = exports_[match];
   const LazyStubSegment& stub = *stubSegments_[fe.lazyStubSegmentIndex];
   return stub.base() + stub.codeRanges()[fe.funcCodeRangeIndex].begin();
