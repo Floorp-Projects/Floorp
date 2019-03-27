c1531bc28a1ffc4a763c407fb194fe5595e1514a
Jan de Mooij <jdemooij@mozilla.com>
2019-03-27 18:57:58 +0000

    Bug 1382650 part 4 - Split Ion warmup threshold JitOption in 'normal' and 'full' options. r=nbp
    Also adds a javascript.options.ion.full.threshold browser pref and similar shell
    flags.
    
    This doesn't rename the existing prefs yet.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24156

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1456,6 +1456,7 @@ pref("javascript.options.baselinejit.threshold", 10);
 pref("javascript.options.ion",              true);
 //Duplicated in JitOptions - ensure both match.
 pref("javascript.options.ion.threshold",    1000);
+pref("javascript.options.ion.full.threshold", 100000);
 //Duplicated in JitOptions - ensure both match.
 pref("javascript.options.ion.frequent_bailout_threshold", 10);
 pref("javascript.options.asmjs",            true);
