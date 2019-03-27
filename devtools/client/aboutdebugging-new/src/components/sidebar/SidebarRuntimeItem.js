8e142e099dc207e4e2bdbf5b29743548b847f9bf
Noemi Erli <nerli@mozilla.com>
2019-03-27 23:34:14 +0200

    Backed out changeset e855e9c6b125 (bug 1537772) for devtools failures in browser_aboutdebugging_process_main.js CLOSED TREE

--- devtools/client/aboutdebugging-new/src/components/sidebar/SidebarRuntimeItem.js
+++ devtools/client/aboutdebugging-new/src/components/sidebar/SidebarRuntimeItem.js
@@ -55,7 +55,7 @@ class SidebarRuntimeItem extends PureComponent {
     const { deviceName, getString, isUnknown, name } = this.props;
 
     const displayName = isUnknown ?
-      getString("about-debugging-sidebar-runtime-item-waiting-for-browser") : name;
+      getString("about-debugging-sidebar-runtime-item-waiting-for-runtime") : name;
 
     const localizationId = deviceName
       ? "about-debugging-sidebar-runtime-item-name"
