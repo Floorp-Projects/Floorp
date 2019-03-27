c34cf5cbe2e3a158d88f0d5cc30d908601f3bbd4
Haik Aftandilian <haftandilian@mozilla.com>
2019-03-27 19:23:32 +0000

    Bug 1452278 - Part 1 - Make macOS nsOSHelperAppService::GetFromTypeAndExtension() not call OS MIME API's in content r=bzbarsky,froydnj
    Add a new nsExternalHelperAppService derived class named nsOSHelperAppServiceChild to be used for the MIME service, external helper app service, and external protocol service interfaces in child processes. nsOSHelperAppServiceChild overrides some methods used to get MIME and external protocol handler information from the OS and implements these methods by remoting the calls to the parent process.
    
    This is necessary because, on Mac, querying the OS for helper application info from sandboxed content processes is unreliable and has buggy side effects.
    
    For now, only use the new class on Mac.
    
    Android and unix file changes r+ by gcp.
    Windows files changes r+ by bobowen.
    Sync messages review r+ by nfroyd.
    
    MozReview-Commit-ID: 63BiS6VCxfn
    
    Differential Revision: https://phabricator.services.mozilla.com/D15620

---
+++ uriloader/exthandler/nsMIMEInfoChild.h
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsMIMEInfoChild_h
+#define nsMIMEInfoChild_h
+
+#include "nsMIMEInfoImpl.h"
+
+/*
+ * A platform-generic nsMIMEInfo implementation to be used in child process
+ * generic code that needs a MIMEInfo with limited functionality.
+ */
+class nsChildProcessMIMEInfo : public nsMIMEInfoImpl {
+ public:
+  explicit nsChildProcessMIMEInfo(const char* aMIMEType = "")
+      : nsMIMEInfoImpl(aMIMEType) {}
+
+  explicit nsChildProcessMIMEInfo(const nsACString& aMIMEType)
+      : nsMIMEInfoImpl(aMIMEType) {}
+
+  nsChildProcessMIMEInfo(const nsACString& aType, HandlerClass aClass)
+      : nsMIMEInfoImpl(aType, aClass) {}
+
+  NS_IMETHOD LaunchWithFile(nsIFile* aFile) override {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  };
+
+ protected:
+  virtual MOZ_MUST_USE nsresult LoadUriInternal(nsIURI* aURI) override {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  };
+
+#ifdef DEBUG
+  virtual MOZ_MUST_USE nsresult LaunchDefaultWithFile(nsIFile* aFile) override {
+    return NS_ERROR_UNEXPECTED;
+  }
+#endif
+  static MOZ_MUST_USE nsresult OpenApplicationWithURI(nsIFile* aApplication,
+                                                      const nsCString& aURI) {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+
+  NS_IMETHOD GetDefaultDescription(nsAString& aDefaultDescription) override {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  };
+};
+
+#endif
