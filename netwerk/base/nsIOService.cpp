5accc60c122ac6587061c35e5a7685ff260c47f8
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2019-03-28 16:40:27 +0000

    Bug 1537883: Silently fail in case aPrincipal is null within SpeculativeConnectInternal. r=valentin,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D25218

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -1765,20 +1765,12 @@ nsresult nsIOService::SpeculativeConnectInternal(
 
   MOZ_ASSERT(aPrincipal, "We expect passing a principal here.");
 
-  // Bug 1537883: Remove the code for dumping the JS Stack in case aPrincipal
-  // is null. It's only used to determine the JS callsite which passes null
-  // as the principal to SpeculativeConnect().
   if (!aPrincipal) {
-    JSContext *cx = nsContentUtils::GetCurrentJSContext();
-    if (cx) {
-      JS::UniqueChars chars = xpc_PrintJSStack(cx,
-                                               /*showArgs=*/false,
-                                               /*showLocals=*/false,
-                                               /*showThisProps=*/false);
-      CrashReporter::AnnotateCrashReport(CrashReporter::Annotation::Bug_1537883,
-                                         chars.get());
-      MOZ_CRASH("aPrincipal can not be null");
-    }
+    // Bug 1537883, fail silently in case aPrincipal is null rather
+    // than having the browser crash because we cannot create a
+    // loadInfo without a principal. Within Bug 1539853 we should
+    // resolve why aPrincipal is null here and remove the silent fail.
+    return NS_OK;
   }
 
   // dummy channel used to create a TCP connection.
