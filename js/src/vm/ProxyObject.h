2a78ebb5e3568edb1156b025e1d8bc0bb69d0bf4
Matthew Gaudet <mgaudet@mozilla.com>
2019-03-28 15:02:59 +0000

    Bug 1536439 - [Part 1] Removing Shaped Objects r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D25072

--- js/src/vm/ProxyObject.h
+++ js/src/vm/ProxyObject.h
@@ -8,7 +8,7 @@
 #define vm_ProxyObject_h
 
 #include "js/Proxy.h"
-#include "vm/ShapedObject.h"
+#include "vm/JSObject.h"
 
 namespace js {
 
@@ -16,14 +16,14 @@ namespace js {
  * This is the base class for the various kinds of proxy objects.  It's never
  * instantiated.
  *
- * Proxy objects use ShapedObject::shape_ primarily to record flags.  Property
+ * Proxy objects use JSObject::shape_ primarily to record flags.  Property
  * information, &c. is all dynamically computed.
  *
  * There is no class_ member to force specialization of JSObject::is<T>().
  * The implementation in JSObject is incorrect for proxies since it doesn't
  * take account of the handler type.
  */
-class ProxyObject : public ShapedObject {
+class ProxyObject : public JSObject {
   // GetProxyDataLayout computes the address of this field.
   detail::ProxyDataLayout data;
 
