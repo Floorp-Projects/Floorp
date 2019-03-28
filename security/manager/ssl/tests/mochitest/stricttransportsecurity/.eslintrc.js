e30b6f7da324f9841f57d310efc069406cda287b
Mark Banner <standard8@mozilla.com>
2019-03-28 09:38:14 +0000

    Bug 1415265 - Remove now unnecessary .eslintrc.js files or entries. r=mossop
    Differential Revision: https://phabricator.services.mozilla.com/D23850

--- security/manager/ssl/tests/mochitest/stricttransportsecurity/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  // mochitest-chrome tests also exist in this directory, but don't appear to
-  // use anything not also available to plain mochitests. Since plain mochitests
-  // are the majority, we take the safer option and only extend the
-  // mochitest-plain eslintrc file.
-  "extends": "plugin:mozilla/mochitest-test"
-};
