3d5d7fb1392526b2be61797317228ede2b56c6da
Mike Conley <mconley@mozilla.com>
2019-03-28 14:10:34 +0000

    Bug 1538987 - Collect Telemetry on process priority switching. data-review=chutten, r=gsvelto a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D25137
    
    --HG--
    extra : rebase_source : ecf62eca10c0b3fd2fb6625387c34d392ae970ee
    extra : source : 7fff0ba3ac2a6d07f572b44fbce747c7c3b0ef26

--- dom/ipc/ProcessPriorityManager.cpp
+++ dom/ipc/ProcessPriorityManager.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/IntegerPrintfMacros.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/Services.h"
+#include "mozilla/Telemetry.h"
 #include "mozilla/Unused.h"
 #include "mozilla/Logging.h"
 #include "nsPrintfCString.h"
@@ -493,6 +494,9 @@ void ProcessPriorityManagerImpl::TabActivityChanged(TabParent* aTabParent,
     return;
   }
 
+  Telemetry::ScalarAdd(
+      Telemetry::ScalarID::DOM_CONTENTPROCESS_OS_PRIORITY_CHANGE_CONSIDERED, 1);
+
   pppm->TabActivityChanged(aTabParent, aIsActive);
 }
 
@@ -795,6 +799,15 @@ void ParticularProcessPriorityManager::SetPriorityNow(
   ProcessPriority oldPriority = mPriority;
 
   mPriority = aPriority;
+
+  if (oldPriority < mPriority) {
+    Telemetry::ScalarAdd(
+        Telemetry::ScalarID::DOM_CONTENTPROCESS_OS_PRIORITY_RAISED, 1);
+  } else {
+    Telemetry::ScalarAdd(
+        Telemetry::ScalarID::DOM_CONTENTPROCESS_OS_PRIORITY_LOWERED, 1);
+  }
+
   hal::SetProcessPriority(Pid(), mPriority);
 
   if (oldPriority != mPriority) {
