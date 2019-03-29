9e2b4b6ab74e73fd7818f204d113c341d840793e
J.C. Jones <jjones@mozilla.com>
2019-03-29 17:16:13 +0000

    Bug 1539541 - Enable FIDO U2F API, and permit registrations for Google Accounts r=keeler,qdot a=pascalc
    Per the thread "Intent-to-Ship: Backward-Compatibility FIDO U2F support for
    Google Accounts" on dev-platform [0], this bug is to:
    
      1. Enable the security.webauth.u2f by default, to ride the trains
    
      2. Remove the aOp == U2FOperation::Sign check from EvaluateAppID in
         WebAuthnUtil.cpp, permitting the Google override to work for Register as
         well as Sign.
    
    This would enable Firefox users to use FIDO U2F API on most all sites, subject
    to the algorithm limitations discussed in the section "Thorny issues in
    enabling our FIDO U2F API implementation" of that post.
    
    [0] https://groups.google.com/d/msg/mozilla.dev.platform/q5cj38hGTEA/lC834665BQAJ
    
    Differential Revision: https://phabricator.services.mozilla.com/D25241

--- security/manager/ssl/security-prefs.js
+++ security/manager/ssl/security-prefs.js
@@ -115,7 +115,7 @@ pref("security.pki.netscape_step_up_policy", 2);
 pref("security.pki.certificate_transparency.mode", 0);
 
 // Hardware Origin-bound Second Factor Support
-pref("security.webauth.u2f", false);
+pref("security.webauth.u2f", true);
 pref("security.webauth.webauthn", true);
 // Only one of "enable_softtoken" and "enable_usbtoken" can be true
 // at a time.
