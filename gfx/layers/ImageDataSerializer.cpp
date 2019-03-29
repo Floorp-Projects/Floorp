fa62ace3f1667f389689d24c37770d7c2a36a499
Gurzau Raul <rgurzau@mozilla.com>
2019-03-29 06:16:14 +0200

    Backed out changeset b64006992c1c (bug 1479145) for failing at /client/CanvasClient.cpp on a CLOSED TREE.

--- gfx/layers/ImageDataSerializer.cpp
+++ gfx/layers/ImageDataSerializer.cpp
@@ -22,8 +22,7 @@ namespace ImageDataSerializer {
 using namespace gfx;
 
 int32_t ComputeRGBStride(SurfaceFormat aFormat, int32_t aWidth) {
-  // Some drivers require an alignment of 32 bytes for efficient texture upload.
-  return GetAlignedStride<32>(aWidth, BytesPerPixel(aFormat));
+  return GetAlignedStride<4>(aWidth, BytesPerPixel(aFormat));
 }
 
 int32_t GetRGBStride(const RGBDescriptor& aDescriptor) {
