41f025d5769c1736e371503ef7c6eff2ae0fe678
Gabriele Svelto <gsvelto@mozilla.com>
2019-03-29 19:17:56 +0000

    Bug 1536850 - Properly detect when the browser is shutting down to avoid taking content process minidumps too late r=Ehsan
    This code was already present but it had two flaws: the first one was that the
    variable used to prevent minidumps from being taken when shutting down was not
    being initialized which caused it to be true most of the time and thus
    prevented captures even in valid scenarios. The second was that we relied on
    the "profile-before-change" change event to detect shutdown but this often
    happens after we've already started closing tabs and shutting down processes
    thus the minidump generation would often race against it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24691

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -134,6 +134,7 @@
 #include "nsFrameMessageManager.h"
 #include "nsHashPropertyBag.h"
 #include "nsIAlertsService.h"
+#include "nsIAppStartup.h"
 #include "nsIClipboard.h"
 #include "nsICookie.h"
 #include "nsContentPermissionHelper.h"
@@ -2267,7 +2268,6 @@ ContentParent::ContentParent(ContentParent* aOpener,
       mRemoteWorkerActors(0),
       mNumDestroyingTabs(0),
       mLifecycleState(LifecycleState::LAUNCHING),
-      mShuttingDown(false),
       mIsForBrowser(!mRemoteType.IsEmpty()),
       mRecordReplayState(aRecordReplayState),
       mRecordingFile(aRecordingFile),
@@ -2958,8 +2958,6 @@ ContentParent::Observe(nsISupports* aSubject, const char* aTopic,
                        const char16_t* aData) {
   if (mSubprocess && (!strcmp(aTopic, "profile-before-change") ||
                       !strcmp(aTopic, "xpcom-shutdown"))) {
-    mShuttingDown = true;
-
     // Make sure that our process will get scheduled.
     ProcessPriorityManager::SetProcessPriority(this,
                                                PROCESS_PRIORITY_FOREGROUND);
@@ -3379,7 +3377,8 @@ void ContentParent::GeneratePairedMinidump(const char* aReason) {
   // Something has gone wrong to get us here, so we generate a minidump
   // of the parent and child for submission to the crash server unless we're
   // already shutting down.
-  if (mCrashReporter && !mShuttingDown &&
+  nsCOMPtr<nsIAppStartup> appStartup = components::AppStartup::Service();
+  if (mCrashReporter && !appStartup->GetShuttingDown() &&
       Preferences::GetBool("dom.ipc.tabs.createKillHardCrashReports", false)) {
     // GeneratePairedMinidump creates two minidumps for us - the main
     // one is for the content process we're about to kill, and the other
