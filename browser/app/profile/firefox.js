6e1ae7e86a7e9ea3a60354fed0a2edaa47af8c18
Frederik Braun <fbraun@mozilla.com>
2019-03-29 12:47:21 -0700

    Bug 1512511 - Remove extra-privileges for input.mozilla.org. r=Gijs,MattN
    There are domains that we give extra permissions.
    Those should be hosted and operated by Firefox.
    input.mozilla.org isn't and it also doesnt use extra permissions anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13948

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1713,7 +1713,7 @@ pref("print.use_simplify_page", true);
 
 // Space separated list of URLS that are allowed to send objects (instead of
 // only strings) through webchannels. This list is duplicated in mobile/android/app/mobile.js
-pref("webchannel.allowObject.urlWhitelist", "https://content.cdn.mozilla.net https://input.mozilla.org https://support.mozilla.org https://install.mozilla.org");
+pref("webchannel.allowObject.urlWhitelist", "https://content.cdn.mozilla.net https://support.mozilla.org https://install.mozilla.org");
 
 // Whether or not the browser should scan for unsubmitted
 // crash reports, and then show a notification for submitting
