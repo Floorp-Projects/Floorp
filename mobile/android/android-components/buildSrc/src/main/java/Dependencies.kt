86c4b5b589b2b238ebab4af19732568bf969b7af
Colin Lee <mncolinlee@gmail.com>
2019-03-29 16:19:16 -0500

    [components] Upgrading A-S Fixes https://github.com/mozilla/application-services/issues/880: [Bug] createBookmark() creates a separator

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -34,7 +34,7 @@ object Versions {
     const val paging = "1.0.1"
     const val zxing = "3.3.0"
 
-    const val mozilla_appservices = "0.22.0"
+    const val mozilla_appservices = "0.23.0"
     const val servo = "0.0.1.20181017.aa95911"
 }
 
