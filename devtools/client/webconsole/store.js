ba758523adce2d765fa365b30e703a960320778b
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-03-30 11:08:26 +0000

    Bug 1524276 - Add groupWarnings preference in console redux state. r=bgrins.
    This will be used to enable groups of warning messages (Tracking Protection,
    CSP, CORS, …).
    
    Differential Revision: https://phabricator.services.mozilla.com/D23550

--- devtools/client/webconsole/store.js
+++ devtools/client/webconsole/store.js
@@ -50,6 +50,7 @@ function configureStore(webConsoleUI, options = {}) {
   const sidebarToggle = getBoolPref(PREFS.FEATURES.SIDEBAR_TOGGLE);
   const jstermCodeMirror = getBoolPref(PREFS.FEATURES.JSTERM_CODE_MIRROR);
   const autocomplete = getBoolPref(PREFS.FEATURES.AUTOCOMPLETE);
+  const groupWarnings = getBoolPref(PREFS.FEATURES.GROUP_WARNINGS);
   const historyCount = getIntPref(PREFS.UI.INPUT_HISTORY_COUNT);
 
   const initialState = {
@@ -59,6 +60,7 @@ function configureStore(webConsoleUI, options = {}) {
       jstermCodeMirror,
       autocomplete,
       historyCount,
+      groupWarnings,
     }),
     filters: FilterState({
       error: getBoolPref(PREFS.FILTER.ERROR),
