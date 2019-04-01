97cc18f3c6b5736b6fbaeb98e3e3bbab5ccbee12
arthur.iakab <aiakab@mozilla.com>
2019-04-02 02:36:51 +0300

    Backed out changeset 48d248c9f5fd (bug 1532201) for causing multiple build bustages on TextureHostOGL.cpp CLOSED TREEE

--- gfx/gl/GLScreenBuffer.cpp
+++ gfx/gl/GLScreenBuffer.cpp
@@ -88,7 +88,9 @@ UniquePtr<SurfaceFactory> GLScreenBuffer::CreateFactory(
     factory = MakeUnique<SurfaceFactory_GLTexture>(mGLContext, caps, ipcChannel,
                                                    mFlags);
 #elif defined(MOZ_WIDGET_ANDROID)
-    if (XRE_IsParentProcess() && !gfxPrefs::WebGLSurfaceTextureEnabled()) {
+    // XXX WebRender does not support SurfaceFactory_EGLImage usage.
+    if (XRE_IsParentProcess() && !gfxPrefs::WebGLSurfaceTextureEnabled() &&
+        backend != layers::LayersBackend::LAYERS_WR) {
       factory = SurfaceFactory_EGLImage::Create(gl, caps, ipcChannel, flags);
     } else {
       factory =
