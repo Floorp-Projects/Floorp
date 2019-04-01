bc5ee77d1cfa9c653253babccedc30f9f7a6003b
Emilio Cobos Álvarez <emilio@crisal.io>
2019-04-01 18:48:10 +0000

    Bug 1537594 - Make the fixed generic family a parse-time alias to monospace. r=jfkthame
    As discussed on IRC, fixed is only used for prefs right now, and:
    
     * We already copy the fixed size to the monospace font.
     * We already serialize the fixed family as "monospace" in the style system.
    
    So it already works somewhat inconsistently. Making it an alias makes it
    work consistently.
    
    Differential Revision: https://phabricator.services.mozilla.com/D24288

--- gfx/thebes/gfxPlatformFontList.cpp
+++ gfx/thebes/gfxPlatformFontList.cpp
@@ -958,11 +958,6 @@ void gfxPlatformFontList::GetFontFamiliesFromGenericFamilies(
 
 gfxPlatformFontList::PrefFontList* gfxPlatformFontList::GetPrefFontsLangGroup(
     StyleGenericFontFamily aGenericType, eFontPrefLang aPrefLang) {
-  // treat -moz-fixed as monospace
-  if (aGenericType == StyleGenericFontFamily::MozFixed) {
-    aGenericType = StyleGenericFontFamily::Monospace;
-  }
-
   if (aGenericType == StyleGenericFontFamily::MozEmoji) {
     // Emoji font has no lang
     PrefFontList* prefFonts = mEmojiPrefFont.get();
@@ -1359,7 +1354,6 @@ nsAtom* gfxPlatformFontList::GetLangGroup(nsAtom* aLanguage) {
     case StyleGenericFontFamily::SansSerif:
       return kGeneric_sans_serif;
     case StyleGenericFontFamily::Monospace:
-    case StyleGenericFontFamily::MozFixed:
       return kGeneric_monospace;
     case StyleGenericFontFamily::Cursive:
       return kGeneric_cursive;
