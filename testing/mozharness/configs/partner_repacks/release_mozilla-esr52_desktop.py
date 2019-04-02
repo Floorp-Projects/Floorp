66487dcaa057b74aa98c96a4891046bb7c500caf
Tom Prince <mozilla@hocat.ca>
2019-04-02 21:20:32 +0000

    No bug: [mozharness] Remove unused esr52 partner repack configuration; r=nthomas
    Differential Revision: https://phabricator.services.mozilla.com/D25701

--- testing/mozharness/configs/partner_repacks/release_mozilla-esr52_desktop.py
+++
@@ -1,6 +0,0 @@
-config = {
-    "appName": "Firefox",
-    "log_name": "partner_repack",
-    "repack_manifests_url": "git@github.com:mozilla-partners/mozilla-sha1-manifest",
-    "repo_file": "https://raw.githubusercontent.com/mozilla/git-repo/master/repo",
-}
