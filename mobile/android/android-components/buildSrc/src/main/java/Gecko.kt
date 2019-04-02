980be948d1ded44e8739a10ac9c5174909526b3f
MickeyMoz <sebastian@mozilla.com>
2019-04-02 14:09:59 +0000

    [components] Update GeckoView (20190402-140958)

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 internal object GeckoVersions {
-    const val nightly_version = "68.0.20190401094905"
+    const val nightly_version = "68.0.20190402083512"
     const val beta_version = "67.0.20190318154932"
     const val release_version = "66.0.20190320150847"
 }
