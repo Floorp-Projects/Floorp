4eb4e9830365299c64e5b2828884bb98b944483d
Matthew Gaudet <mgaudet@mozilla.com>
2019-04-03 14:05:12 +0000

    Bug 1536768 - Check completeness of ObjectGroupDispatch in opt builds r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D25304

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -1488,13 +1488,13 @@ void CodeGenerator::visitObjectGroupDispatch(LObjectGroupDispatch* lir) {
 
   if (!mir->hasFallback()) {
     MOZ_ASSERT(lastBranch.isInitialized());
-#ifdef DEBUG
+
     Label ok;
     lastBranch.relink(&ok);
     lastBranch.emit(masm);
     masm.assumeUnreachable("Unexpected ObjectGroup");
     masm.bind(&ok);
-#endif
+
     if (!isNextBlock(lastBlock)) {
       masm.jump(lastBlock->label());
     }
