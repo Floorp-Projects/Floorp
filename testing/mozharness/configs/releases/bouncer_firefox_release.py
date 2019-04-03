251b0fb270b60f84ff2b5d976367af7aafd3100e
Tom Prince <mozilla@hocat.ca>
2019-04-03 20:33:48 +0000

    Bug 1541122: [mozharness] Simplify bouncer-check configuration to not include paths; r=mtabara
    These existed for when bouncer was updated via mozharness, and they are unused in bouncer-check.
    By removing the paths, we make the configuration easier to read and update.
    
    Differential Revision: https://phabricator.services.mozilla.com/D25838

--- testing/mozharness/configs/releases/bouncer_firefox_release.py
+++ testing/mozharness/configs/releases/bouncer_firefox_release.py
@@ -4,188 +4,85 @@ config = {
         "installer": {
             "product-name": "Firefox-%(version)s",
             "check_uptake": True,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/releases/%(version)s/linux-i686/:lang/firefox-%(version)s.tar.bz2",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/releases/%(version)s/linux-x86_64/:lang/firefox-%(version)s.tar.bz2",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/releases/%(version)s/mac/:lang/Firefox%%20%(version)s.dmg",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Setup%%20%(version)s.exe",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/win64/:lang/Firefox%%20Setup%%20%(version)s.exe",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
         "installer-ssl": {
             "product-name": "Firefox-%(version)s-SSL",
             "check_uptake": True,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/releases/%(version)s/linux-i686/:lang/firefox-%(version)s.tar.bz2",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/releases/%(version)s/linux-x86_64/:lang/firefox-%(version)s.tar.bz2",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/releases/%(version)s/mac/:lang/Firefox%%20%(version)s.dmg",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Setup%%20%(version)s.exe",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/win64/:lang/Firefox%%20Setup%%20%(version)s.exe",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
         "msi": {
             "product-name": "Firefox-%(version)s-msi-SSL",
             "check_uptake": True,
-            "paths": {
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Setup%%20%(version)s.msi",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/win64/:lang/Firefox%%20Setup%%20%(version)s.msi",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "win",
+                "win64",
+            ],
         },
         "stub-installer": {
             "product-name": "Firefox-%(version)s-stub",
             "check_uptake": True,
-            "paths": {
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Installer.exe",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/win32/:lang/Firefox%%20Installer.exe",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "win",
+                "win64",
+            ],
         },
         "complete-mar": {
             "product-name": "Firefox-%(version)s-Complete",
             "check_uptake": True,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/releases/%(version)s/update/linux-i686/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/releases/%(version)s/update/linux-x86_64/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/releases/%(version)s/update/mac/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/update/win32/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/update/win64/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
         "complete-mar-candidates": {
             "product-name": "Firefox-%(version)sbuild%(build_number)s-Complete",
             "check_uptake": False,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/linux-i686/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/linux-x86_64/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/mac/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/win32/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/win64/:lang/firefox-%(version)s.complete.mar",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
     },
     "partials": {
         "releases-dir": {
             "product-name": "Firefox-%(version)s-Partial-%(prev_version)s",
             "check_uptake": True,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/releases/%(version)s/update/linux-i686/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/releases/%(version)s/update/linux-x86_64/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/releases/%(version)s/update/mac/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/releases/%(version)s/update/win32/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/releases/%(version)s/update/win64/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
         "candidates-dir": {
             "product-name": "Firefox-%(version)sbuild%(build_number)s-Partial-%(prev_version)sbuild%(prev_build_number)s",
             "check_uptake": False,
-            "paths": {
-                "linux": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/linux-i686/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "linux",
-                },
-                "linux64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/linux-x86_64/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "linux64",
-                },
-                "macosx64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/mac/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "osx",
-                },
-                "win32": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/win32/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "win",
-                },
-                "win64": {
-                    "path": "/firefox/candidates/%(version)s-candidates/build%(build_number)s/update/win64/:lang/firefox-%(prev_version)s-%(version)s.partial.mar",
-                    "bouncer-platform": "win64",
-                },
-            },
+            "platforms": [
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+            ],
         },
     },
 }
