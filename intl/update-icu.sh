a63aa3bd74bb94ee44b865dc506e950344a621c3
André Bargull <andre.bargull@gmail.com>
2019-04-04 11:52:54 +0300

    Bug 1533481 - Part 4: Prepare update to ICU 64. r=jwalden!
    Summary:
    
    - ICU no longer supports removing resource files manually, instead the new data
      filter builder needs to be used.
    - This includes test data, which also must be preserved in ICU 64.
    - umutex.h contains an implicit call to `new` in ICU64 which needs to be allowed
      in check_vanilla_allocations.py.
    - Drive-by: Make ICU with multiple jobs for faster rebuilds.
    
    Reviewers: jwalden
    
    Reviewed By: jwalden
    
    Bug #: 1533481
    
    Differential Revision: https://phabricator.services.mozilla.com/D25264

--- intl/update-icu.sh
+++ intl/update-icu.sh
@@ -44,42 +44,10 @@ git -C ${tmpclonedir} log -1 > ${icu_dir}/GIT-INFO
 # Clean up after ourselves.
 rm -rf ${tmpclonedir}
 
-# remove layoutex, tests, and samples, but leave makefiles in place
+# Remove layoutex, tests, and samples, but leave makefiles and test data in place.
 find ${icu_dir}/source/layoutex -name '*Makefile.in' -prune -or -type f -print | xargs rm
-find ${icu_dir}/source/test -name '*Makefile.in' -prune -or -type f -print | xargs rm
+find ${icu_dir}/source/test -name '*Makefile.in' -prune -or -name 'testdata' -prune -or -type f -print | xargs rm
 find ${icu_dir}/source/samples -name '*Makefile.in' -prune -or -type f -print | xargs rm
-
-# remove data that we currently don't need
-rm -rf ${icu_dir}/source/data/brkitr/*
-rm ${icu_dir}/source/data/lang/*.mk
-rm ${icu_dir}/source/data/lang/*.txt
-rm ${icu_dir}/source/data/mappings/*.mk
-find ${icu_dir}/source/data/mappings \
-    -name ibm-37_P100-1995.ucm -prune -or \
-    -name ibm-1047_P100-1995.ucm -prune -or \
-    -name '*.ucm' -print | xargs rm
-rm ${icu_dir}/source/data/rbnf/*
-rm ${icu_dir}/source/data/region/*.mk
-rm ${icu_dir}/source/data/region/*.txt
-rm ${icu_dir}/source/data/translit/*
-rm ${icu_dir}/source/data/unit/*.mk
-rm ${icu_dir}/source/data/unit/*.txt
-
-# Remove all exemplar cities 'ec'. (bug 1225401 and bug 1345336)
-find ${icu_dir}/source/data/zone \
-    -name root.txt -prune -or \
-    -name tzdbNames.txt -prune -or \
-    -name '*.txt' -print | xargs sed -i '/^\s*ec{\".*\"}$/ { d }'
-# Remove empty time zone entries after exemplar cities removal.
-find ${icu_dir}/source/data/zone \
-    -name root.txt -prune -or \
-    -name tzdbNames.txt -prune -or \
-    -name '*.txt' -print | xargs sed -i '/^\s*\"[A-Z][a-zA-Z:_-]*\"{/{N; s/^\s*\"[A-Z][a-zA-Z:_-]*\"{\n\s*}// }; /^$/d'
-# And finally remove any empty 'zoneStrings' entries.
-find ${icu_dir}/source/data/zone \
-    -name root.txt -prune -or \
-    -name tzdbNames.txt -prune -or \
-    -name '*.txt' -print | xargs sed -i '/^\s*zoneStrings{/{N; s/^\s*zoneStrings{\n\s*}// }; /^$/d'
 
 for patch in \
  bug-915735 \
