67d4a1421aaa6764217c7394059eb5fa8c867602
Alexander Surkov <surkov.alexander@gmail.com>
2019-04-04 14:14:52 +0000

    Bug 1495357 - convert wizard-buttons binding to a Custom Element, r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D23392

--- toolkit/content/widgets/wizard.js
+++ toolkit/content/widgets/wizard.js
@@ -7,6 +7,10 @@
 // This is loaded into chrome windows with the subscript loader. Wrap in
 // a block to prevent accidentally leaking globals onto `window`.
 {
+const {AppConstants} = ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+
+const kDTDs = [ "chrome://global/locale/wizard.dtd" ];
+
 class MozWizardPage extends MozXULElement {
   constructor() {
     super();
@@ -29,4 +33,144 @@ class MozWizardPage extends MozXULElement {
 }
 
 customElements.define("wizardpage", MozWizardPage);
+
+class MozWizardButtons extends MozXULElement {
+  connectedCallback() {
+    this.textContent = "";
+    this.appendChild(MozXULElement.parseXULToFragment(this._markup, kDTDs));
+
+    this._wizardButtonDeck = this.querySelector(".wizard-next-deck");
+
+    this.initializeAttributeInheritance();
+
+    const listeners = [
+      ["back", () => document.documentElement.rewind()],
+      ["next", () => document.documentElement.advance()],
+      ["finish", () => document.documentElement.advance()],
+      ["cancel", () => document.documentElement.cancel()],
+      ["extra1", () => document.documentElement.extra1()],
+      ["extra2", () => document.documentElement.extra2()],
+    ];
+    for (let [name, listener] of listeners) {
+      let btn = this.getButton(name);
+      if (btn) {
+        btn.addEventListener("command", listener);
+      }
+    }
+  }
+
+  static get inheritedAttributes() {
+    return AppConstants.platform == "macosx" ? {
+      "[dlgtype='finish']": "hidefinishbutton",
+      "[dlgtype='next']": "lastpage",
+    } : null;
+  }
+
+  get _markup() {
+    if (AppConstants.platform == "macosx") {
+      return `
+        <vbox flex="1">
+          <hbox class="wizard-buttons-btm">
+            <button class="wizard-button" dlgtype="extra1" hidden="true"/>
+            <button class="wizard-button" dlgtype="extra2" hidden="true"/>
+            <button label="&button-cancel-mac.label;"
+                    class="wizard-button" dlgtype="cancel"/>
+            <spacer flex="1"/>
+            <button label="&button-back-mac.label;"
+                    accesskey="&button-back-mac.accesskey;"
+                    class="wizard-button wizard-nav-button" dlgtype="back"/>
+            <button label="&button-next-mac.label;"
+                    accesskey="&button-next-mac.accesskey;"
+                    class="wizard-button wizard-nav-button" dlgtype="next"
+                    default="true" />
+            <button label="&button-finish-mac.label;" class="wizard-button"
+                    dlgtype="finish" default="true" />
+          </hbox>
+        </vbox>`;
+    }
+
+    let buttons = AppConstants.platform == "linux" ? `
+      <button label="&button-cancel-unix.label;"
+              class="wizard-button"
+              dlgtype="cancel"/>
+      <spacer style="width: 24px;"/>
+      <button label="&button-back-unix.label;"
+              accesskey="&button-back-unix.accesskey;"
+              class="wizard-button" dlgtype="back"/>
+      <deck class="wizard-next-deck">
+        <hbox>
+          <button label="&button-finish-unix.label;"
+                  class="wizard-button"
+                  dlgtype="finish" default="true" flex="1"/>
+        </hbox>
+        <hbox>
+          <button label="&button-next-unix.label;"
+                  accesskey="&button-next-unix.accesskey;"
+                  class="wizard-button" dlgtype="next"
+                  default="true" flex="1"/>
+        </hbox>
+      </deck>` : `
+      <button label="&button-back-win.label;"
+              accesskey="&button-back-win.accesskey;"
+              class="wizard-button" dlgtype="back"/>
+      <deck class="wizard-next-deck">
+        <hbox>
+          <button label="&button-finish-win.label;"
+                  class="wizard-button"
+                  dlgtype="finish" default="true" flex="1"/>
+        </hbox>
+        <hbox>
+          <button label="&button-next-win.label;"
+                  accesskey="&button-next-win.accesskey;"
+                  class="wizard-button" dlgtype="next"
+                  default="true" flex="1"/>
+        </hbox>
+      </deck>
+      <button label="&button-cancel-win.label;"
+              class="wizard-button"
+              dlgtype="cancel"/>`;
+
+    return `
+      <vbox class="wizard-buttons-box-1" flex="1">
+        <separator class="wizard-buttons-separator groove"/>
+        <hbox class="wizard-buttons-box-2">
+          <button class="wizard-button" dlgtype="extra1" hidden="true"/>
+          <button class="wizard-button" dlgtype="extra2" hidden="true"/>
+          <spacer flex="1" anonid="spacer"/>
+          ${buttons}
+        </hbox>
+      </vbox>`;
+  }
+
+  onPageChange() {
+    if (AppConstants.platform == "macosx") {
+      this.setAttribute("hidefinishbutton",
+                        !(this.getAttribute("lastpage") == "true"));
+    } else if (this.getAttribute("lastpage") == "true") {
+      this._wizardButtonDeck.setAttribute("selectedIndex", 0);
+    } else {
+      this._wizardButtonDeck.setAttribute("selectedIndex", 1);
+    }
+  }
+
+  getButton(type) {
+    return this.querySelector(`[dlgtype="${type}"]`);
+  }
+
+  get defaultButton() {
+    const kXULNS =
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+    let buttons = this._wizardButtonDeck.selectedPanel.
+      getElementsByTagNameNS(kXULNS, "button");
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].getAttribute("default") == "true" &&
+          !buttons[i].hidden && !buttons[i].disabled) {
+        return buttons[i];
+      }
+    }
+    return null;
+  }
+}
+
+customElements.define("wizard-buttons", MozWizardButtons);
 }
