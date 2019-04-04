663f9a19bc1d34c69c759ea6d62cacf9a9b131d3
Yura Zenevich <yura.zenevich@gmail.com>
2019-04-04 14:12:37 +0000

    Bug 1538281 - make tree view row keyboard navigation consistent with other shared components. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D24538
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/shared/focus.js
+++ devtools/client/shared/focus.js
@@ -4,16 +4,19 @@
 
 "use strict";
 
-// Simplied selector targetting elements that can receive the focus, full
-// version at
-// http://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
-// .
-exports.focusableSelector = [
-  "a[href]:not([tabindex='-1'])",
-  "button:not([disabled]):not([tabindex='-1'])",
-  "iframe:not([tabindex='-1'])",
-  "input:not([disabled]):not([tabindex='-1'])",
-  "select:not([disabled]):not([tabindex='-1'])",
-  "textarea:not([disabled]):not([tabindex='-1'])",
-  "[tabindex]:not([tabindex='-1'])",
-].join(", ");
+// Make this available to both AMD and CJS environments
+define(function(require, exports, module) {
+  // Simplied selector targetting elements that can receive the focus, full
+  // version at
+  // http://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
+  // .
+  module.exports.focusableSelector = [
+    "a[href]:not([tabindex='-1'])",
+    "button:not([disabled]):not([tabindex='-1'])",
+    "iframe:not([tabindex='-1'])",
+    "input:not([disabled]):not([tabindex='-1'])",
+    "select:not([disabled]):not([tabindex='-1'])",
+    "textarea:not([disabled]):not([tabindex='-1'])",
+    "[tabindex]:not([tabindex='-1'])",
+  ].join(", ");
+});
