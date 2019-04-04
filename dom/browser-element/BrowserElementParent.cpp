0d4d56b83884358c3ac3e73282b09b251df5a2ae
Boris Zbarsky <bzbarsky@mit.edu>
2019-04-04 08:45:14 +0000

    Bug 1541479.  Stop using AutoJSContext in BrowserElementParent.  r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D25948

--- dom/browser-element/BrowserElementParent.cpp
+++ dom/browser-element/BrowserElementParent.cpp
@@ -131,13 +131,18 @@ BrowserElementParent::DispatchOpenWindowEvent(Element* aOpenerFrameElement,
   detail.mFeatures = aFeatures;
   detail.mFrameElement = aPopupFrameElement;
 
-  AutoJSContext cx;
-  JS::Rooted<JS::Value> val(cx);
-
   nsIGlobalObject* sgo = aPopupFrameElement->OwnerDoc()->GetScopeObject();
   if (!sgo) {
     return BrowserElementParent::OPEN_WINDOW_IGNORED;
   }
+
+  AutoJSAPI jsapi;
+  if (!jsapi.Init(sgo)) {
+    return BrowserElementParent::OPEN_WINDOW_IGNORED;
+  }
+
+  JSContext* cx = jsapi.cx();
+  JS::Rooted<JS::Value> val(cx);
 
   JS::Rooted<JSObject*> global(cx, sgo->GetGlobalJSObject());
   JSAutoRealm ar(cx, global);
