fbb1036cee7e8c5d1353f5ffd222805fb16c3318
Botond Ballo <botond@mozilla.com>
2019-04-04 22:11:40 +0000

    Bug 1531962 - Extract an MVMContext interface used by MobileViewportManager to interact with its surroundings. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D26246
    
    --HG--
    extra : moz-landing-system : lando

---
+++ layout/base/GeckoMVMContext.h
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GeckoMVMContext_h_
+#define GeckoMVMContext_h_
+
+#include "MVMContext.h"
+
+#include "mozilla/Attributes.h"  // for MOZ_NON_OWNING_REF
+#include "mozilla/RefPtr.h"
+#include "nsCOMPtr.h"
+
+class nsIPresShell;
+
+namespace mozilla {
+namespace dom {
+class Document;
+class EventTarget;
+}  // namespace dom
+
+/**
+ * An implementation of MVMContext that uses actual Gecko components.
+ * This is intended for production use (whereas TestMVMContext is intended for
+ * testing.)
+ */
+class GeckoMVMContext : public MVMContext {
+ public:
+  explicit GeckoMVMContext(dom::Document* aDocument, nsIPresShell* aPresShell);
+  void AddEventListener(const nsAString& aType, nsIDOMEventListener* aListener,
+                        bool aUseCapture) override;
+  void RemoveEventListener(const nsAString& aType,
+                           nsIDOMEventListener* aListener,
+                           bool aUseCapture) override;
+  void AddObserver(nsIObserver* aObserver, const char* aTopic,
+                   bool aOwnsWeak) override;
+  void RemoveObserver(nsIObserver* aObserver, const char* aTopic) override;
+  void Destroy() override;
+
+  nsViewportInfo GetViewportInfo(
+      const ScreenIntSize& aDisplaySize) const override;
+  CSSToLayoutDeviceScale CSSToDevPixelScale() const override;
+  float GetResolution() const override;
+  bool SubjectMatchesDocument(nsISupports* aSubject) const override;
+  Maybe<CSSRect> CalculateScrollableRectForRSF() const override;
+  bool IsResolutionUpdatedByApz() const override;
+  LayoutDeviceMargin ScrollbarAreaToExcludeFromCompositionBounds()
+      const override;
+  Maybe<LayoutDeviceIntSize> GetContentViewerSize() const override;
+  bool AllowZoomingForDocument() const override;
+
+  void SetResolutionAndScaleTo(float aResolution) override;
+  void SetVisualViewportSize(const CSSSize& aSize) override;
+  void UpdateDisplayPortMargins() override;
+  void Reflow(const CSSSize& aNewSize, const CSSSize& aOldSize) override;
+
+ private:
+  RefPtr<dom::Document> mDocument;
+  // raw ref since the presShell owns this
+  nsIPresShell* MOZ_NON_OWNING_REF mPresShell;
+  nsCOMPtr<dom::EventTarget> mEventTarget;
+};
+
+}  // namespace mozilla
+
+#endif  // GeckoMVMContext_h_
