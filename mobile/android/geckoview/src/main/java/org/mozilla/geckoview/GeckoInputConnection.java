68cc516cea0596c475272c60b0b1e240e0d9dbbc
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2019-04-05 11:31:08 +0000

    Bug 1530486 - FFTV+GV can't close Keyboard popup when pressing hardware back r=esawin
    `EditorInfo.IME_ACTION_PREVIOUS` is used when clicking `Previous` key on FireTV's software keyboard.  This key on System's WebView closes software keyboard and doesn't generate any keyboard event such as `ENTER`. So we should close it like System's WebView.
    
    Differential Revision: https://phabricator.services.mozilla.com/D25913

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoInputConnection.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoInputConnection.java
@@ -228,6 +228,22 @@ import java.lang.reflect.Proxy;
     }
 
     @Override
+    public boolean performEditorAction(final int editorAction) {
+        if (editorAction == EditorInfo.IME_ACTION_PREVIOUS) {
+            // This action is [Previous] key on FireTV's keyboard.
+            // [Previous] closes software keyboard, and don't generate any keyboard event.
+            getView().post(new Runnable() {
+                @Override
+                public void run() {
+                    getInputDelegate().hideSoftInput(mSession);
+                }
+            });
+            return true;
+        }
+        return super.performEditorAction(editorAction);
+    }
+
+    @Override
     public ExtractedText getExtractedText(final ExtractedTextRequest req, final int flags) {
         if (req == null)
             return null;
