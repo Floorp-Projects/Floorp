d7be8a0205e1edf09fbd545c17a4f3cbc36b99a8
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-04-05 12:40:23 +0000

    Bug 1537622 - add rudimentary support for linting XUL files, r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D24298
    
    --HG--
    rename : tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js => tools/lint/eslint/eslint-plugin-mozilla/lib/processors/processor-helpers.js
    extra : moz-landing-system : lando

--- tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/processors/xbl-bindings.js
@@ -11,99 +11,7 @@
 
 const NS_XBL = "http://www.mozilla.org/xbl";
 
-let sax = require("sax");
-
-// Converts sax's error message to something that eslint will understand
-let errorRegex = /(.*)\nLine: (\d+)\nColumn: (\d+)\nChar: (.*)/;
-function parseError(err) {
-  let matches = err.message.match(errorRegex);
-  if (!matches) {
-    return null;
-  }
-
-  return {
-    fatal: true,
-    message: matches[1],
-    line: parseInt(matches[2]) + 1,
-    column: parseInt(matches[3]),
-  };
-}
-
-let entityRegex = /&[\w][\w-\.]*;/g;
-
-// A simple sax listener that generates a tree of element information
-function XMLParser(parser) {
-  this.parser = parser;
-  parser.onopentag = this.onOpenTag.bind(this);
-  parser.onclosetag = this.onCloseTag.bind(this);
-  parser.ontext = this.onText.bind(this);
-  parser.onopencdata = this.onOpenCDATA.bind(this);
-  parser.oncdata = this.onCDATA.bind(this);
-  parser.oncomment = this.onComment.bind(this);
-
-  this.document = {
-    local: "#document",
-    uri: null,
-    children: [],
-    comments: [],
-  };
-  this._currentNode = this.document;
-}
-
-XMLParser.prototype = {
-  parser: null,
-
-  onOpenTag(tag) {
-    let node = {
-      parentNode: this._currentNode,
-      local: tag.local,
-      namespace: tag.uri,
-      attributes: {},
-      children: [],
-      comments: [],
-      textContent: "",
-      textLine: this.parser.line,
-      textColumn: this.parser.column,
-      textEndLine: this.parser.line,
-    };
-
-    for (let attr of Object.keys(tag.attributes)) {
-      if (tag.attributes[attr].uri == "") {
-        node.attributes[attr] = tag.attributes[attr].value;
-      }
-    }
-
-    this._currentNode.children.push(node);
-    this._currentNode = node;
-  },
-
-  onCloseTag(tagname) {
-    this._currentNode.textEndLine = this.parser.line;
-    this._currentNode = this._currentNode.parentNode;
-  },
-
-  addText(text) {
-    this._currentNode.textContent += text;
-  },
-
-  onText(text) {
-    // Replace entities with some valid JS token.
-    this.addText(text.replace(entityRegex, "null"));
-  },
-
-  onOpenCDATA() {
-    // Turn the CDATA opening tag into whitespace for indent alignment
-    this.addText(" ".repeat("<![CDATA[".length));
-  },
-
-  onCDATA(text) {
-    this.addText(text);
-  },
-
-  onComment(text) {
-    this._currentNode.comments.push(text);
-  },
-};
+let XMLParser = require("./processor-helpers").XMLParser;
 
 // -----------------------------------------------------------------------------
 // Processor Definition
@@ -204,20 +112,10 @@ module.exports = {
     scriptLines = [];
     lineMap = [];
 
-    // Non-strict allows us to ignore many errors from entities and
-    // preprocessing at the expense of failing to report some XML errors.
-    // Unfortunately it also throws away the case of tagnames and attributes
-    let parser = sax.parser(false, {
-      lowercase: true,
-      xmlns: true,
-    });
-
-    parser.onerror = function(err) {
-      xmlParseError = parseError(err);
-    };
-
-    let xp = new XMLParser(parser);
-    parser.write(text);
+    let xp = new XMLParser(text);
+    if (xp.lastError) {
+      xmlParseError = xp.lastError;
+    }
 
     // Sanity checks to make sure we're dealing with an XBL document
     let document = xp.document;
