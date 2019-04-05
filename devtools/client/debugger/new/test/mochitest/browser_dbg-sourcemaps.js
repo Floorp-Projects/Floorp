236fc4b63f23bf83f3ec7e9d3458e6a5b3ce1f07
David Walsh <dwalsh@mozilla.com>
2019-04-05 17:33:08 +0000

    Bug 1541563 - Sync 131 - Fix sourcemaps test click position after SVG update r=loganfsmyth
    Differential Revision: https://phabricator.services.mozilla.com/D25991

--- devtools/client/debugger/new/test/mochitest/browser_dbg-sourcemaps.js
+++ devtools/client/debugger/new/test/mochitest/browser_dbg-sourcemaps.js
@@ -57,7 +57,10 @@ add_task(async function() {
     "times2.js",
     "opts.js"
   );
-  const { getState } = dbg;
+  const {
+    selectors: { getBreakpointCount },
+    getState
+  } = dbg;
 
   ok(true, "Original sources exist");
   const bundleSrc = findSource(dbg, "bundle.js");
@@ -88,7 +91,7 @@ add_task(async function() {
 
   // Test breaking on a breakpoint
   await addBreakpoint(dbg, "entry.js", 15);
-  is(dbg.selectors.getBreakpointCount(getState()), 1, "One breakpoint exists");
+  is(getBreakpointCount(getState()), 1, "One breakpoint exists");
   assertBreakpointExists(dbg, entrySrc, 15);
 
   invokeInTab("keepMeAlive");
