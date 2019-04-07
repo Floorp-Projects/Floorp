ceaf4b5ab5b85ee45d127a0cf5dda1a090842db6
Narcis Beleuzu <nbeleuzu@mozilla.com>
2019-04-07 16:53:58 +0300

    Bug 1536220 - Enable IonMonkey on ARM64 in-browser. r=nbp a=pascalc
    Summary: Enable IonMonkey on ARM64 in-browser.
    
    Test Plan: treeherder
    
    Reviewers: nbp
    
    Reviewed By: nbp
    
    Subscribers: jandem
    
    Bug #: 1536220
    
    Differential Revision: https://phabricator.services.mozilla.com/D23929

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1467,11 +1467,7 @@ pref("javascript.options.unboxed_objects",  false);
 pref("javascript.options.baselinejit",      true);
 //Duplicated in JitOptions - ensure both match.
 pref("javascript.options.baselinejit.threshold", 10);
-#ifdef NO_ION
-pref("javascript.options.ion",              false);
-#else
 pref("javascript.options.ion",              true);
-#endif
 //Duplicated in JitOptions - ensure both match.
 pref("javascript.options.ion.threshold",    1000);
 pref("javascript.options.ion.full.threshold", 100000);
