fe83b723b7223c0afed016ba574242fea2cd5919
Emilio Cobos Álvarez <emilio@crisal.io>
2019-04-08 18:19:28 +0000

    Bug 1542193 - Fix handling of select's direction in e10s. r=mconley
    In bug 1375476 I fixed one of the places but missed the other. I'll refactor
    them in a bit.
    
    I wish I could run the select tests locally to extend them properly...
    
    Differential Revision: https://phabricator.services.mozilla.com/D26517
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/content/widgets/browser-custom-element.js
+++ toolkit/content/widgets/browser-custom-element.js
@@ -1326,7 +1326,7 @@ class MozBrowser extends MozElements.MozElementMixin(XULFrameElement) {
           }
 
           let menulist = document.getElementById(this.getAttribute("selectmenulist"));
-          menulist.menupopup.style.direction = data.direction;
+          menulist.menupopup.style.direction = data.style.direction;
 
           let zoom = Services.prefs.getBoolPref("browser.zoom.full") ||
             this.isSyntheticDocument ? this._fullZoom : this._textZoom;
