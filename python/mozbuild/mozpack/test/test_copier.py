f611290984823faa9bcca588f95d2dfe36dd11fa
Justin Wood <Callek@gmail.com>
2019-04-08 15:20:45 -0400

    Bug 1542963 - Insert unicode_literals into python/mozbuild/mozpack and called mozbuild/* files. r=#build
    Lint python/mozbuild/{mozbuild,mozpack}.
    
    This makes sure we use byte strings (type(b'')) in many places.
    This also has a few places where we know we want unicode, by enforcing it.
    
    This code also has a few places where we call into windows API's which expect either all args to be unicode,
    or all args to be bytestrings (not-unicode) so make sure those calls align.
    
    This is the most risky of the stack, since there are some mach commands and codepaths that are neither excercised in automation
    nor excercised in tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D26645
    
    --HG--
    extra : rebase_source : 1ee367576bede0318a2db05644693d4f440a250a
    extra : intermediate-source : 2516c350cba6f27eb204f0087728721abbb0e085
    extra : source : 6351a29c5765c13c39ef6186f9e5e911e00420e7

--- python/mozbuild/mozpack/test/test_copier.py
+++ python/mozbuild/mozpack/test/test_copier.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
 from mozpack.copier import (
     FileCopier,
@@ -42,21 +42,21 @@ class BaseTestFileRegistry(MatchTestTemplate):
 
     def do_test_file_registry(self, registry):
         self.registry = registry
-        self.registry.add('foo', GeneratedFile('foo'))
-        bar = GeneratedFile('bar')
+        self.registry.add('foo', GeneratedFile(b'foo'))
+        bar = GeneratedFile(b'bar')
         self.registry.add('bar', bar)
         self.assertEqual(self.registry.paths(), ['foo', 'bar'])
         self.assertEqual(self.registry['bar'], bar)
 
         self.assertRaises(ErrorMessage, self.registry.add, 'foo',
-                          GeneratedFile('foo2'))
+                          GeneratedFile(b'foo2'))
 
         self.assertRaises(ErrorMessage, self.registry.remove, 'qux')
 
         self.assertRaises(ErrorMessage, self.registry.add, 'foo/bar',
-                          GeneratedFile('foobar'))
+                          GeneratedFile(b'foobar'))
         self.assertRaises(ErrorMessage, self.registry.add, 'foo/bar/baz',
-                          GeneratedFile('foobar'))
+                          GeneratedFile(b'foobar'))
 
         self.assertEqual(self.registry.paths(), ['foo', 'bar'])
 
@@ -81,7 +81,7 @@ class BaseTestFileRegistry(MatchTestTemplate):
         self.registry.remove('foo/qux')
         self.assertEqual(self.registry.paths(), ['bar', 'foo/bar', 'foo/baz'])
 
-        self.registry.add('foo/qux', GeneratedFile('fooqux'))
+        self.registry.add('foo/qux', GeneratedFile(b'fooqux'))
         self.assertEqual(self.registry.paths(), ['bar', 'foo/bar', 'foo/baz',
                                                  'foo/qux'])
         self.registry.remove('foo/b*')
@@ -98,28 +98,28 @@ class BaseTestFileRegistry(MatchTestTemplate):
 
         # Can't add a file if it requires a directory in place of a
         # file we also require.
-        self.registry.add('foo', GeneratedFile('foo'))
+        self.registry.add('foo', GeneratedFile(b'foo'))
         self.assertRaises(ErrorMessage, self.registry.add, 'foo/bar',
-                          GeneratedFile('foobar'))
+                          GeneratedFile(b'foobar'))
 
         # Can't add a file if we already have a directory there.
-        self.registry.add('bar/baz', GeneratedFile('barbaz'))
+        self.registry.add('bar/baz', GeneratedFile(b'barbaz'))
         self.assertRaises(ErrorMessage, self.registry.add, 'bar',
-                          GeneratedFile('bar'))
+                          GeneratedFile(b'bar'))
 
         # Bump the count of things that require bar/ to 2.
-        self.registry.add('bar/zot', GeneratedFile('barzot'))
+        self.registry.add('bar/zot', GeneratedFile(b'barzot'))
         self.assertRaises(ErrorMessage, self.registry.add, 'bar',
-                          GeneratedFile('bar'))
+                          GeneratedFile(b'bar'))
 
         # Drop the count of things that require bar/ to 1.
         self.registry.remove('bar/baz')
         self.assertRaises(ErrorMessage, self.registry.add, 'bar',
-                          GeneratedFile('bar'))
+                          GeneratedFile(b'bar'))
 
         # Drop the count of things that require bar/ to 0.
         self.registry.remove('bar/zot')
-        self.registry.add('bar/zot', GeneratedFile('barzot'))
+        self.registry.add('bar/zot', GeneratedFile(b'barzot'))
 
 
 class TestFileRegistry(BaseTestFileRegistry, unittest.TestCase):
@@ -142,16 +142,16 @@ class TestFileRegistry(BaseTestFileRegistry, unittest.TestCase):
     def test_required_directories(self):
         self.registry = FileRegistry()
 
-        self.registry.add('foo', GeneratedFile('foo'))
+        self.registry.add('foo', GeneratedFile(b'foo'))
         self.assertEqual(self.registry.required_directories(), set())
 
-        self.registry.add('bar/baz', GeneratedFile('barbaz'))
+        self.registry.add('bar/baz', GeneratedFile(b'barbaz'))
         self.assertEqual(self.registry.required_directories(), {'bar'})
 
-        self.registry.add('bar/zot', GeneratedFile('barzot'))
+        self.registry.add('bar/zot', GeneratedFile(b'barzot'))
         self.assertEqual(self.registry.required_directories(), {'bar'})
 
-        self.registry.add('bar/zap/zot', GeneratedFile('barzapzot'))
+        self.registry.add('bar/zap/zot', GeneratedFile(b'barzapzot'))
         self.assertEqual(self.registry.required_directories(), {'bar', 'bar/zap'})
 
         self.registry.remove('bar/zap/zot')
@@ -163,7 +163,7 @@ class TestFileRegistry(BaseTestFileRegistry, unittest.TestCase):
         self.registry.remove('bar/zot')
         self.assertEqual(self.registry.required_directories(), set())
 
-        self.registry.add('x/y/z', GeneratedFile('xyz'))
+        self.registry.add('x/y/z', GeneratedFile(b'xyz'))
         self.assertEqual(self.registry.required_directories(), {'x', 'x/y'})
 
 
@@ -175,8 +175,8 @@ class TestFileRegistrySubtree(BaseTestFileRegistry, unittest.TestCase):
 
     def create_registry(self):
         registry = FileRegistry()
-        registry.add('foo/bar', GeneratedFile('foo/bar'))
-        registry.add('baz/qux', GeneratedFile('baz/qux'))
+        registry.add('foo/bar', GeneratedFile(b'foo/bar'))
+        registry.add('baz/qux', GeneratedFile(b'baz/qux'))
         return FileRegistrySubtree('base/root', registry)
 
     def test_file_registry_subtree(self):
@@ -205,12 +205,12 @@ class TestFileCopier(TestWithTmpDir):
 
     def test_file_copier(self):
         copier = FileCopier()
-        copier.add('foo/bar', GeneratedFile('foobar'))
-        copier.add('foo/qux', GeneratedFile('fooqux'))
-        copier.add('foo/deep/nested/directory/file', GeneratedFile('fooz'))
-        copier.add('bar', GeneratedFile('bar'))
-        copier.add('qux/foo', GeneratedFile('quxfoo'))
-        copier.add('qux/bar', GeneratedFile(''))
+        copier.add('foo/bar', GeneratedFile(b'foobar'))
+        copier.add('foo/qux', GeneratedFile(b'fooqux'))
+        copier.add('foo/deep/nested/directory/file', GeneratedFile(b'fooz'))
+        copier.add('bar', GeneratedFile(b'bar'))
+        copier.add('qux/foo', GeneratedFile(b'quxfoo'))
+        copier.add('qux/bar', GeneratedFile(b''))
 
         result = copier.copy(self.tmpdir)
         self.assertEqual(self.all_files(self.tmpdir), set(copier.paths()))
@@ -224,7 +224,7 @@ class TestFileCopier(TestWithTmpDir):
         self.assertEqual(result.removed_directories, set())
 
         copier.remove('foo')
-        copier.add('test', GeneratedFile('test'))
+        copier.add('test', GeneratedFile(b'test'))
         result = copier.copy(self.tmpdir)
         self.assertEqual(self.all_files(self.tmpdir), set(copier.paths()))
         self.assertEqual(self.all_dirs(self.tmpdir), set(['qux']))
@@ -241,7 +241,7 @@ class TestFileCopier(TestWithTmpDir):
         dest = self.tmppath('dest')
 
         copier = FileCopier()
-        copier.add('foo/bar/baz', GeneratedFile('foobarbaz'))
+        copier.add('foo/bar/baz', GeneratedFile(b'foobarbaz'))
 
         os.makedirs(self.tmppath('dest/foo'))
         dummy = self.tmppath('dummy')
@@ -271,7 +271,7 @@ class TestFileCopier(TestWithTmpDir):
         dest = self.tmppath('dest')
 
         copier = FileCopier()
-        copier.add('foo/bar/baz', GeneratedFile('foobarbaz'))
+        copier.add('foo/bar/baz', GeneratedFile(b'foobarbaz'))
 
         os.makedirs(self.tmppath('dest/foo'))
         dummy = self.tmppath('dummy')
@@ -345,14 +345,14 @@ class TestFileCopier(TestWithTmpDir):
         os.chmod(self.tmpdir, 0o400)
 
         copier = FileCopier()
-        copier.add('dummy', GeneratedFile('content'))
+        copier.add('dummy', GeneratedFile(b'content'))
         result = copier.copy(self.tmpdir)
         self.assertEqual(result.removed_files_count, 1)
         self.assertFalse(os.path.exists(p))
 
     def test_no_remove(self):
         copier = FileCopier()
-        copier.add('foo', GeneratedFile('foo'))
+        copier.add('foo', GeneratedFile(b'foo'))
 
         with open(self.tmppath('bar'), 'a'):
             pass
@@ -375,7 +375,7 @@ class TestFileCopier(TestWithTmpDir):
 
     def test_no_remove_empty_directories(self):
         copier = FileCopier()
-        copier.add('foo', GeneratedFile('foo'))
+        copier.add('foo', GeneratedFile(b'foo'))
 
         with open(self.tmppath('bar'), 'a'):
             pass
@@ -430,10 +430,10 @@ class TestFileCopier(TestWithTmpDir):
         dest = self.tmppath('dest')
 
         copier = FileCopier()
-        copier.add('foo/bar/baz', GeneratedFile('foobarbaz'))
-        copier.add('foo/bar/qux', GeneratedFile('foobarqux'))
-        copier.add('foo/hoge/fuga', GeneratedFile('foohogefuga'))
-        copier.add('foo/toto/tata', GeneratedFile('footototata'))
+        copier.add('foo/bar/baz', GeneratedFile(b'foobarbaz'))
+        copier.add('foo/bar/qux', GeneratedFile(b'foobarqux'))
+        copier.add('foo/hoge/fuga', GeneratedFile(b'foohogefuga'))
+        copier.add('foo/toto/tata', GeneratedFile(b'footototata'))
 
         os.makedirs(os.path.join(dest, 'bar'))
         with open(os.path.join(dest, 'bar', 'bar'), 'w') as fh:
@@ -450,7 +450,7 @@ class TestFileCopier(TestWithTmpDir):
                          {'foo/bar', 'foo/hoge', 'foo/toto', 'bar'})
 
         copier2 = FileCopier()
-        copier2.add('foo/hoge/fuga', GeneratedFile('foohogefuga'))
+        copier2.add('foo/hoge/fuga', GeneratedFile(b'foohogefuga'))
 
         # We expect only files copied from the first copier to be removed,
         # not the extra file that was there beforehand.
@@ -480,24 +480,24 @@ class TestJarrer(unittest.TestCase):
 
     def test_jarrer(self):
         copier = Jarrer()
-        copier.add('foo/bar', GeneratedFile('foobar'))
-        copier.add('foo/qux', GeneratedFile('fooqux'))
-        copier.add('foo/deep/nested/directory/file', GeneratedFile('fooz'))
-        copier.add('bar', GeneratedFile('bar'))
-        copier.add('qux/foo', GeneratedFile('quxfoo'))
-        copier.add('qux/bar', GeneratedFile(''))
+        copier.add('foo/bar', GeneratedFile(b'foobar'))
+        copier.add('foo/qux', GeneratedFile(b'fooqux'))
+        copier.add('foo/deep/nested/directory/file', GeneratedFile(b'fooz'))
+        copier.add('bar', GeneratedFile(b'bar'))
+        copier.add('qux/foo', GeneratedFile(b'quxfoo'))
+        copier.add('qux/bar', GeneratedFile(b''))
 
         dest = MockDest()
         copier.copy(dest)
         self.check_jar(dest, copier)
 
         copier.remove('foo')
-        copier.add('test', GeneratedFile('test'))
+        copier.add('test', GeneratedFile(b'test'))
         copier.copy(dest)
         self.check_jar(dest, copier)
 
         copier.remove('test')
-        copier.add('test', GeneratedFile('replaced-content'))
+        copier.add('test', GeneratedFile(b'replaced-content'))
         copier.copy(dest)
         self.check_jar(dest, copier)
 
@@ -516,8 +516,8 @@ class TestJarrer(unittest.TestCase):
 
     def test_jarrer_compress(self):
         copier = Jarrer()
-        copier.add('foo/bar', GeneratedFile('ffffff'))
-        copier.add('foo/qux', GeneratedFile('ffffff'), compress=False)
+        copier.add('foo/bar', GeneratedFile(b'ffffff'))
+        copier.add('foo/qux', GeneratedFile(b'ffffff'), compress=False)
 
         dest = MockDest()
         copier.copy(dest)
