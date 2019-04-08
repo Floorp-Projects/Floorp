af3f4757c65e855ea7f27d4cb71d82b0a02a8ab3
Andreea Pavel <apavel@mozilla.com>
2019-04-08 22:25:14 +0300

    Backed out changeset 8ef30ebad16c (bug 1541130) build bustages a=backout

--- xpcom/build/GeckoProcessTypes.h
+++ xpcom/build/GeckoProcessTypes.h
@@ -4,16 +4,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// GECKO_PROCESS_TYPE(enum-name, string-name, XRE_Is${NAME}Process, process-bin-type)
+// GECKO_PROCESS_TYPE(enum-name, string-name, XRE_Is${NAME}Process)
 // Note that string-name is exposed to various things like telemetry
 // and the crash reporter, so it should not be changed casually.
-//
-// process-bin-type should be either Self or PluginContainer, and determines
-// whether the child process may be started using the same binary as the parent
-// process, or whether to use plugin-container (Note that whether or not this
-// value is actually obeyed depends on platform and build configuration. Do not
-// use this value directly, but rather use XRE_GetChildProcBinPathType to
-// resolve this).
 //
 // The values generated for the GeckoProcessType enum are dependent on
 // the ordering of the GECKO_PROCESS_TYPE invocations in this file, and
@@ -21,18 +14,18 @@
 // do not reorder lines in this file.
 //
 // Please add new process types at the end of this list.
-GECKO_PROCESS_TYPE(Default, "default", Parent, Self)
-GECKO_PROCESS_TYPE(Plugin, "plugin", Plugin, PluginContainer)
-GECKO_PROCESS_TYPE(Content, "tab", Content, Self)
-GECKO_PROCESS_TYPE(IPDLUnitTest, "ipdlunittest", IPDLUnitTest, PluginContainer)
+GECKO_PROCESS_TYPE(Default, "default", Parent)
+GECKO_PROCESS_TYPE(Plugin, "plugin", Plugin)
+GECKO_PROCESS_TYPE(Content, "tab", Content)
+GECKO_PROCESS_TYPE(IPDLUnitTest, "ipdlunittest", IPDLUnitTest)
 // Gecko Media Plugin process.
-GECKO_PROCESS_TYPE(GMPlugin, "gmplugin", GMPlugin, PluginContainer)
+GECKO_PROCESS_TYPE(GMPlugin, "gmplugin", GMPlugin)
 // GPU and compositor process.
-GECKO_PROCESS_TYPE(GPU, "gpu", GPU, Self)
+GECKO_PROCESS_TYPE(GPU, "gpu", GPU)
 // VR process.
-GECKO_PROCESS_TYPE(VR, "vr", VR, Self)
+GECKO_PROCESS_TYPE(VR, "vr", VR)
 // Remote Data Decoder process.
-GECKO_PROCESS_TYPE(RDD, "rdd", RDD, Self)
+GECKO_PROCESS_TYPE(RDD, "rdd", RDD)
 // Socket process
-GECKO_PROCESS_TYPE(Socket, "socket", Socket, Self)
-GECKO_PROCESS_TYPE(RemoteSandboxBroker, "sandbox", RemoteSandboxBroker, PluginContainer)
+GECKO_PROCESS_TYPE(Socket, "socket", Socket)
+GECKO_PROCESS_TYPE(RemoteSandboxBroker, "sandbox", RemoteSandboxBroker)
