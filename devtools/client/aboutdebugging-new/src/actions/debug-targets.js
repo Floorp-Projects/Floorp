5b663e7187c61be543f41004ecb013cc0e439e2f
Belén Albeza <balbeza@mozilla.com>
2019-04-08 09:18:58 +0000

    Bug 1539344 - Part 1: share constants between about:debugging and DebugTargetInfo r=jdescottes,Ola
    Differential Revision: https://phabricator.services.mozilla.com/D25374
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/aboutdebugging-new/src/actions/debug-targets.js
+++ devtools/client/aboutdebugging-new/src/actions/debug-targets.js
@@ -57,14 +57,14 @@ function inspectDebugTarget(type, id) {
       // updated so this is not an issue. On remote runtimes however, trying to inspect a
       // worker a second time after closing the corresponding about:devtools-toolbox tab
       // will fail. See Bug 1534201.
-      window.open(`about:devtools-toolbox?type=${type.toLowerCase()}&id=${id}`);
+      window.open(`about:devtools-toolbox?type=${type}&id=${id}`);
     } else {
-      window.open(`about:devtools-toolbox?type=${type.toLowerCase()}&id=${id}` +
+      window.open(`about:devtools-toolbox?type=${type}&id=${id}` +
                   `&remoteId=${remoteId}`);
     }
 
     dispatch(Actions.recordTelemetryEvent("inspect", {
-      "target_type": type,
+      "target_type": type.toUpperCase(),
       "runtime_type": runtime.type,
     }));
   };
