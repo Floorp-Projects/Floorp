27ad290b5e585e9f5936deb462ee10f02cbe3ee8
Michael Froman <mfroman@mozilla.com>
2019-04-09 21:13:16 +0000

    Bug 1539029 - pt 1 - give RDD its own process selector. r=froydnj
    Originally, RDD reused the GPU process selector since they were
    using all the same services, and it reduced the number of places
    that had to be touched.  Now that RDD needs pref handling, it
    needs its own process selector to avoid GPU inheriting pref
    handling.
    
    Differential Revision: https://phabricator.services.mozilla.com/D26566

--- widget/cocoa/nsWidgetFactory.mm
+++ widget/cocoa/nsWidgetFactory.mm
@@ -135,7 +135,7 @@ static const mozilla::Module::CIDEntry kWidgetCIDs[] = {
     {&kNS_COLORPICKER_CID, false, NULL, nsColorPickerConstructor,
      mozilla::Module::MAIN_PROCESS_ONLY},
     {&kNS_APPSHELL_CID, false, NULL, nsAppShellConstructor,
-     mozilla::Module::ALLOW_IN_GPU_VR_AND_SOCKET_PROCESS},
+     mozilla::Module::ALLOW_IN_GPU_RDD_VR_AND_SOCKET_PROCESS},
     {&kNS_SOUND_CID, false, NULL, nsSoundConstructor, mozilla::Module::MAIN_PROCESS_ONLY},
     {&kNS_TRANSFERABLE_CID, false, NULL, nsTransferableConstructor},
     {&kNS_HTMLFORMATCONVERTER_CID, false, NULL, nsHTMLFormatConverterConstructor},
@@ -165,7 +165,7 @@ static const mozilla::Module::ContractIDEntry kWidgetContracts[] = {
     {"@mozilla.org/filepicker;1", &kNS_FILEPICKER_CID, mozilla::Module::MAIN_PROCESS_ONLY},
     {"@mozilla.org/colorpicker;1", &kNS_COLORPICKER_CID, mozilla::Module::MAIN_PROCESS_ONLY},
     {"@mozilla.org/widget/appshell/mac;1", &kNS_APPSHELL_CID,
-     mozilla::Module::ALLOW_IN_GPU_VR_AND_SOCKET_PROCESS},
+     mozilla::Module::ALLOW_IN_GPU_RDD_VR_AND_SOCKET_PROCESS},
     {"@mozilla.org/sound;1", &kNS_SOUND_CID, mozilla::Module::MAIN_PROCESS_ONLY},
     {"@mozilla.org/widget/transferable;1", &kNS_TRANSFERABLE_CID},
     {"@mozilla.org/widget/htmlformatconverter;1", &kNS_HTMLFORMATCONVERTER_CID},
@@ -207,4 +207,4 @@ extern const mozilla::Module kWidgetModule = {mozilla::Module::kVersion,
                                               NULL,
                                               nsAppShellInit,
                                               nsWidgetCocoaModuleDtor,
-                                              mozilla::Module::ALLOW_IN_GPU_VR_AND_SOCKET_PROCESS};
+                                              mozilla::Module::ALLOW_IN_GPU_RDD_VR_AND_SOCKET_PROCESS};
