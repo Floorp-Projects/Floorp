f9114beee7a6f36f3a863bf072cd57623620f0ca
Jeff Walden <jwalden@mit.edu>
2019-04-09 13:56:18 -0700

    Bug 1543455 - Rename JS::EvaluateUtf8 to JS::Evaluate and make it take |JS::SourceText<mozilla::Utf8Unit>&| for consistency with the UTF-16 form of the API.  r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D26949
    
    --HG--
    extra : rebase_source : d84c7c1081442d0e45aba942dbdd16e626be3047

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -18,6 +18,7 @@
 #include "mozilla/Sprintf.h"
 #include "mozilla/TimeStamp.h"
 #include "mozilla/Unused.h"
+#include "mozilla/Utf8.h"
 #include "mozilla/Variant.h"
 
 #include <chrono>
@@ -162,6 +163,7 @@ using mozilla::Nothing;
 using mozilla::NumberEqualsInt32;
 using mozilla::TimeDuration;
 using mozilla::TimeStamp;
+using mozilla::Utf8Unit;
 using mozilla::Variant;
 
 // Avoid an unnecessary NSPR dependency on Linux and OS X just for the shell.
@@ -972,8 +974,13 @@ static bool InitModuleLoader(JSContext* cx) {
   options.werrorOption = true;
   options.strictOption = true;
 
+  JS::SourceText<Utf8Unit> srcBuf;
+  if (!srcBuf.init(cx, std::move(src), srcLen)) {
+    return false;
+  }
+
   RootedValue rv(cx);
-  return JS::EvaluateUtf8(cx, options, src.get(), srcLen, &rv);
+  return JS::Evaluate(cx, options, srcBuf, &rv);
 }
 
 static bool GetModuleImportHook(JSContext* cx,
@@ -10129,8 +10136,13 @@ static MOZ_MUST_USE bool ProcessArgs(JSContext* cx, OptionParser* op) {
       JS::CompileOptions opts(cx);
       opts.setFileAndLine("-e", 1);
 
+      JS::SourceText<Utf8Unit> srcBuf;
+      if (!srcBuf.init(cx, code, strlen(code), JS::SourceOwnership::Borrowed)) {
+        return false;
+      }
+
       RootedValue rval(cx);
-      if (!JS::EvaluateUtf8(cx, opts, code, strlen(code), &rval)) {
+      if (!JS::Evaluate(cx, opts, srcBuf, &rval)) {
         return false;
       }
 
