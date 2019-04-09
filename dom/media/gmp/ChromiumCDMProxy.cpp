1def9c5ba79fe57fe600556a38172459933b0974
Bryce Van Dyk <bvandyk@mozilla.com>
2019-04-09 15:05:01 +0000

    Bug 1540136 - P1: Log more info in ChromiumCDMProxy dtor and Shutdown. r=cpearce
    Also remove unneeded MOZ_COUNT_[CTOR|DTOR] macros. We already get similar
    functionality from NS_INLINE_DECL_THREADSAFE_REFCOUNTING.
    
    Differential Revision: https://phabricator.services.mozilla.com/D26204
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/gmp/ChromiumCDMProxy.cpp
+++ dom/media/gmp/ChromiumCDMProxy.cpp
@@ -29,10 +29,11 @@ ChromiumCDMProxy::ChromiumCDMProxy(dom::MediaKeys* aKeys,
       mCDMMutex("ChromiumCDMProxy"),
       mGMPThread(GetGMPAbstractThread()) {
   MOZ_ASSERT(NS_IsMainThread());
-  MOZ_COUNT_CTOR(ChromiumCDMProxy);
 }
 
-ChromiumCDMProxy::~ChromiumCDMProxy() { MOZ_COUNT_DTOR(ChromiumCDMProxy); }
+ChromiumCDMProxy::~ChromiumCDMProxy() {
+  EME_LOG("ChromiumCDMProxy::~ChromiumCDMProxy(this=%p)", this);
+}
 
 void ChromiumCDMProxy::Init(PromiseId aPromiseId, const nsAString& aOrigin,
                             const nsAString& aTopLevelOrigin,
@@ -305,7 +306,7 @@ void ChromiumCDMProxy::RemoveSession(const nsAString& aSessionId,
 
 void ChromiumCDMProxy::Shutdown() {
   MOZ_ASSERT(NS_IsMainThread());
-  EME_LOG("ChromiumCDMProxy::Shutdown(this=%p)", this);
+  EME_LOG("ChromiumCDMProxy::Shutdown(this=%p) mCDM=%p", this, mCDM.get());
   mKeys.Clear();
   RefPtr<gmp::ChromiumCDMParent> cdm;
   {
