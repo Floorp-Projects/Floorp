0a97a2b84e5181c1256b7b831c728317e67314bc
Chris Manchester <cmanchester@mozilla.com>
2019-04-09 09:16:15 +0000

    Bug 1542707 - Don't replace 'nightly' jobs with artifact builds on try. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D26658
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/taskgraph/transforms/build.py
+++ taskcluster/taskgraph/transforms/build.py
@@ -153,8 +153,9 @@ def use_artifact(config, jobs):
     else:
         use_artifact = False
     for job in jobs:
-        if config.kind == 'build' and use_artifact and \
-                job.get('index', {}).get('job-name') in ARTIFACT_JOBS:
+        if (config.kind == 'build' and use_artifact and
+            not job.get('attributes', {}).get('nightly', False) and
+            job.get('index', {}).get('job-name') in ARTIFACT_JOBS):
             job['treeherder']['symbol'] += 'a'
             job['worker']['env']['USE_ARTIFACT'] = '1'
         yield job
