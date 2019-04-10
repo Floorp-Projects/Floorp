99ad98c9512752c2f17b885490a8666d16e57b0e
Daniel Varga <dvarga@mozilla.com>
2019-04-10 21:09:46 +0300

    Backed out 4 changesets (bug 1458385) for nightly updates fail. a=backout
    Backed out changeset 09338587b68e (bug 1458385)
    Backed out changeset f7791b680d46 (bug 1458385)
    Backed out changeset cc06a7beb3d1 (bug 1458385)
    Backed out changeset 3b10487587c3 (bug 1458385)

--- testing/mozharness/scripts/repackage.py
+++ testing/mozharness/scripts/repackage.py
@@ -93,7 +93,6 @@ class Repackage(BaseScript):
                 command=command,
                 cwd=dirs['abs_mozilla_dir'],
                 halt_on_failure=True,
-                env=self.query_env(),
             )
 
     def _run_tooltool(self):
