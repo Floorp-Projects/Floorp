2602fd0f0bbabc7ae5cb9d830f0ce6a706166e76
Andrew Osmond <aosmond@mozilla.com>
2019-04-10 12:35:20 -0400

    Bug 1540200 - Part 1. Move picture local rect calculation to update visibility. r=kvark
    We currently calculate a picture's local rect when we are doing the
    first picture traversal. It was composed of the union of the clipped
    local rects of its children. However the true local rect of a picture is
    the union of the snapped clipped local rects of its children. The
    snapping is done in device space, but we won't know the exact transform
    until we establish the raster roots, which is based on the picture's
    local rect.
    
    As such, we create an estimated local rect which is how we currently
    calculate the local rect. Then once the raster roots have been selected,
    we recalculate the local rect of the picture based on its children
    during update visibility.
    
    This patch should have not contain any functional changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28881

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -978,7 +978,7 @@ impl AlphaBatchBuilder {
                 let prim_cache_address = gpu_cache.get_address(&ctx.globals.default_image_handle);
 
                 let prim_header = PrimitiveHeader {
-                    local_rect: picture.local_rect,
+                    local_rect: picture.snapped_local_rect,
                     local_clip_rect: prim_info.combined_local_clip_rect,
                     task_address,
                     specific_prim_address: prim_cache_address,
@@ -1016,7 +1016,7 @@ impl AlphaBatchBuilder {
                             ).unwrap_or(OPAQUE_TASK_ADDRESS);
 
                             let prim_header = PrimitiveHeader {
-                                local_rect: pic.local_rect,
+                                local_rect: pic.snapped_local_rect,
                                 local_clip_rect: prim_info.combined_local_clip_rect,
                                 task_address,
                                 specific_prim_address: GpuCacheAddress::invalid(),
@@ -1131,7 +1131,7 @@ impl AlphaBatchBuilder {
                                 // the local bounds of the picture extend to within the edge tiles.
                                 let local_clip_rect = prim_info
                                     .combined_local_clip_rect
-                                    .intersection(&picture.local_rect)
+                                    .intersection(&picture.snapped_local_rect)
                                     .and_then(|rect| {
                                         rect.intersection(&tile_cache.local_clip_rect)
                                     });
@@ -1643,7 +1643,7 @@ impl AlphaBatchBuilder {
                                 };
 
                                 let prim_header = PrimitiveHeader {
-                                    local_rect: picture.local_rect,
+                                    local_rect: picture.snapped_local_rect,
                                     local_clip_rect: prim_info.combined_local_clip_rect,
                                     task_address,
                                     specific_prim_address: prim_cache_address,
