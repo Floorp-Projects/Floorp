d29ef7f195bd8cb06308e88b3cd77c3c24b9bdb9
Andreea Pavel <apavel@mozilla.com>
2019-04-11 16:06:27 +0300

    Backed out 4 changesets (bug 1532993, bug 1538731) for failing dt on a CLOSED TREE
    Backed out changeset f7050f15a28d (bug 1532993)
    Backed out changeset f1615630af59 (bug 1532993)
    Backed out changeset ff05581f5fe0 (bug 1538731)
    Backed out changeset 769d9b3bdd4e (bug 1538731)

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_reload.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_devtoolstoolbox_reload.js
@@ -3,23 +3,8 @@
 
 "use strict";
 
-// Test can run for a long time on debug platforms.
-requestLongerTimeout(5);
-
 /* import-globals-from helper-collapsibilities.js */
 Services.scriptloader.loadSubScript(CHROME_URL_ROOT + "helper-collapsibilities.js", this);
-
-const TOOLS = [
-  "inspector",
-  "webconsole",
-  "jsdebugger",
-  "styleeditor",
-  "performance",
-  "memory",
-  "netmonitor",
-  "storage",
-  "accessibility",
-];
 
 /**
  * Test whether about:devtools-toolbox display correctly after reloading.
@@ -28,20 +13,14 @@ add_task(async function() {
   info("Force all debug target panes to be expanded");
   prepareCollapsibilitiesTest();
 
-  for (const toolId of TOOLS) {
-    await testReloadAboutDevToolsToolbox(toolId);
-  }
-});
-
-async function testReloadAboutDevToolsToolbox(toolId) {
   const { document, tab, window } = await openAboutDebugging();
   await selectThisFirefoxPage(document, window.AboutDebugging.store);
   const { devtoolsBrowser, devtoolsTab, devtoolsWindow } =
     await openAboutDevtoolsToolbox(document, tab, window);
 
-  info(`Select tool: ${toolId}`);
+  info("Select webconsole tool");
   const toolbox = getToolbox(devtoolsWindow);
-  await toolbox.selectTool(toolId);
+  await toolbox.selectTool("webconsole");
 
   info("Reload about:devtools-toolbox page");
   devtoolsBrowser.reload();
@@ -54,4 +33,4 @@ async function testReloadAboutDevToolsToolbox(toolId) {
 
   await closeAboutDevtoolsToolbox(document, devtoolsTab, window);
   await removeTab(tab);
-}
+});
