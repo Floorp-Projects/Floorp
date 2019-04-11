132a33520a2664bc55c57233d42e4ea96cf8bc24
Andrew Osmond <aosmond@mozilla.com>
2019-04-11 15:30:48 -0400

    Bug 1543584 - Always rasterize SVGs but clamp the maximum size. r=tnikkel
    SVG performance with the fallback path with WebRender is very bad. This
    patch avoids fallback by always producing a rasterized surface we store
    in SurfaceCache, but also clamping the size consistently to a configured
    maximum. This will cause us to upscale rasterized SVGs which is
    undesirable visually but is a lower risk change that we can uplift to
    beta than fixing the underlying performance issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27159

--- image/SurfaceCache.h
+++ image/SurfaceCache.h
@@ -434,6 +434,16 @@ struct SurfaceCache {
    */
   static bool IsLegalSize(const IntSize& aSize);
 
+  /**
+   * @return clamped size for the given vector image size to rasterize at.
+   */
+  static IntSize ClampVectorSize(const IntSize& aSize);
+
+  /**
+   * @return clamped size for the given image and size to rasterize at.
+   */
+  static IntSize ClampSize(const ImageKey aImageKey, const IntSize& aSize);
+
  private:
   virtual ~SurfaceCache() = 0;  // Forbid instantiation.
 };
