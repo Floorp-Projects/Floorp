bf67a988df2a60f8d9ecf6e724220af2a0958faf
Tim Nguyen <ntim.bugs@gmail.com>
2019-04-11 10:36:24 +0000

    Bug 1543626 - Make browser_all_files_referenced.js parse theme_experiment manifest field. r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D27047

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -177,10 +177,6 @@ var whitelist = [
    platforms: ["linux", "win"]},
   // Referenced by the webcompat system addon for localization
   {file: "resource://gre/localization/en-US/toolkit/about/aboutCompat.ftl"},
-  // These files are dynamically injected by LightweightThemeConsumer.jsm
-  // meaning the test doesn't detect them.
-  {file: "resource://app/modules/themes/dark/experiment.css"},
-  {file: "resource://app/modules/themes/light/experiment.css"},
 ];
 
 whitelist = new Set(whitelist.filter(item =>
@@ -341,6 +337,11 @@ async function parseJsonManifest(uri) {
         gReferencesFromCode.set(script, null);
       }
     }
+  }
+
+  if (data.theme_experiment && data.theme_experiment.stylesheet) {
+    let stylesheet = uri.resolve(data.theme_experiment.stylesheet);
+    gReferencesFromCode.set(stylesheet, null);
   }
 
   return null;
