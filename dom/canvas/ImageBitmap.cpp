7d3979430a03b6b5207e8cdf8e5ef179052eac6e
Dorel Luca <dluca@mozilla.com>
2019-04-11 05:28:34 +0300

    Backed out changeset aa0feb50773a (bug 1540221) for WPT failures in /html/semantics/embedded-content/the-canvas-element/security.pattern.fillStyle.sub.html. CLOSED TREE

--- dom/canvas/ImageBitmap.cpp
+++ dom/canvas/ImageBitmap.cpp
@@ -412,6 +412,33 @@ class CreateImageFromRawDataInMainThreadSyncTask final
   const Maybe<IntRect>& mCropRect;
 };
 
+static bool CheckSecurityForElements(bool aIsWriteOnly, bool aCORSUsed,
+                                     nsIPrincipal* aPrincipal) {
+  if (aIsWriteOnly || !aPrincipal) {
+    return false;
+  }
+
+  if (!aCORSUsed) {
+    nsIGlobalObject* incumbentSettingsObject = GetIncumbentGlobal();
+    if (NS_WARN_IF(!incumbentSettingsObject)) {
+      return false;
+    }
+
+    nsIPrincipal* principal = incumbentSettingsObject->PrincipalOrNull();
+    if (NS_WARN_IF(!principal) || !(principal->Subsumes(aPrincipal))) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool CheckSecurityForElements(
+    const nsLayoutUtils::SurfaceFromElementResult& aRes) {
+  return CheckSecurityForElements(aRes.mIsWriteOnly, aRes.mCORSUsed,
+                                  aRes.mPrincipal);
+}
+
 /*
  * A wrapper to the nsLayoutUtils::SurfaceFromElement() function followed by the
  * security checking.
@@ -430,7 +457,8 @@ static already_AddRefed<SourceSurface> GetSurfaceFromElement(
     return nullptr;
   }
 
-  *aWriteOnly = res.mIsWriteOnly;
+  // check write-only mode
+  *aWriteOnly = !CheckSecurityForElements(res);
 
   return surface.forget();
 }
@@ -843,7 +871,7 @@ already_AddRefed<ImageBitmap> ImageBitmap::CreateInternal(
   // Check security.
   nsCOMPtr<nsIPrincipal> principal = aVideoEl.GetCurrentVideoPrincipal();
   bool CORSUsed = aVideoEl.GetCORSMode() != CORS_NONE;
-  bool writeOnly = CheckWriteOnlySecurity(CORSUsed, principal);
+  bool writeOnly = !CheckSecurityForElements(false, CORSUsed, principal);
 
   // Create ImageBitmap.
   RefPtr<layers::Image> data = aVideoEl.GetCurrentImage();
