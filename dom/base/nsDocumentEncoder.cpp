28c5295e5ada9870b3ddc718a20fe6168b346533
Andrea Marchesini <amarchesini@mozilla.com>
2019-04-11 14:13:24 +0000

    Bug 1542530 - Selection.toString() works correctly when text is selected together with a set of div+svg elements, r=mats
    Differential Revision: https://phabricator.services.mozilla.com/D27064

--- dom/base/nsDocumentEncoder.cpp
+++ dom/base/nsDocumentEncoder.cpp
@@ -623,7 +623,7 @@ nsresult nsDocumentEncoder::SerializeRangeNodes(nsRange* aRange, nsINode* aNode,
         }
       }
 
-      if (endOffset) {
+      if (startOffset < endOffset) {
         // serialize the children of this node that are in the range
         nsIContent* childAsNode = content->GetFirstChild();
         int32_t j = 0;
