895aa799441bf25e68ce37dea54be7f2f518fb70
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2019-04-12 03:54:18 +0000

    Bug 1543585 - Clean up finding words for spellchecker. r=masayuki
    `mozSpellChecker::Replace` and `mozSpellChecker::NextMisspelledWord` have a loop
    to find word from text content. But `mozEnglishWordUtils::FindNextWord` always
    returns `NS_OK` and some code doesn't check error even if `nsresult` local
    variable is used.
    
    So I would like to clean up this loop.
    
    - `mozEnglishWordUtils::FindNextWord` should return true if word is found
    - We should use reference type for some `TextServicesDocument`'s methods.
    - Add more check for error
    
    Differential Revision: https://phabricator.services.mozilla.com/D27037

--- extensions/spellcheck/src/mozEnglishWordUtils.cpp
+++ extensions/spellcheck/src/mozEnglishWordUtils.cpp
@@ -31,71 +31,76 @@ bool mozEnglishWordUtils::ucIsAlpha(char16_t aChar) {
   return nsUGenCategory::kLetter == mozilla::unicode::GetGenCategory(aChar);
 }
 
-nsresult mozEnglishWordUtils::FindNextWord(const char16_t *word,
-                                           uint32_t length, uint32_t offset,
-                                           int32_t *begin, int32_t *end) {
+bool mozEnglishWordUtils::FindNextWord(const nsAString &aWord, uint32_t offset,
+                                       int32_t *begin, int32_t *end) {
+  if (offset >= aWord.Length()) {
+    *begin = -1;
+    *end = -1;
+    return false;
+  }
+
+  const char16_t *word = aWord.BeginReading();
+  uint32_t length = aWord.Length();
   const char16_t *p = word + offset;
   const char16_t *endbuf = word + length;
   const char16_t *startWord = p;
-  if (p < endbuf) {
-    // XXX These loops should be modified to handle non-BMP characters.
-    // if previous character is a word character, need to advance out of the
-    // word
-    if (offset > 0 && ucIsAlpha(*(p - 1))) {
-      while (p < endbuf && ucIsAlpha(*p)) p++;
-    }
-    while ((p < endbuf) && (!ucIsAlpha(*p))) {
+
+  // XXX These loops should be modified to handle non-BMP characters.
+  // if previous character is a word character, need to advance out of the
+  // word
+  if (offset > 0 && ucIsAlpha(*(p - 1))) {
+    while (p < endbuf && ucIsAlpha(*p)) {
       p++;
     }
-    startWord = p;
-    while ((p < endbuf) && ((ucIsAlpha(*p)) || (*p == '\''))) {
-      p++;
-    }
+  }
+  while ((p < endbuf) && (!ucIsAlpha(*p))) {
+    p++;
+  }
+  startWord = p;
+  while ((p < endbuf) && ((ucIsAlpha(*p)) || (*p == '\''))) {
+    p++;
+  }
 
-    // we could be trying to break down a url, we don't want to break a url into
-    // parts, instead we want to find out if it really is a url and if so, skip
-    // it, advancing startWord to a point after the url.
+  // we could be trying to break down a url, we don't want to break a url into
+  // parts, instead we want to find out if it really is a url and if so, skip
+  // it, advancing startWord to a point after the url.
 
-    // before we spend more time looking to see if the word is a url, look for a
-    // url identifer and make sure that identifer isn't the last character in
-    // the word fragment.
-    if ((p < endbuf - 1) && (*p == ':' || *p == '@' || *p == '.')) {
-      // ok, we have a possible url...do more research to find out if we really
-      // have one and determine the length of the url so we can skip over it.
+  // before we spend more time looking to see if the word is a url, look for a
+  // url identifer and make sure that identifer isn't the last character in
+  // the word fragment.
+  if ((p < endbuf - 1) && (*p == ':' || *p == '@' || *p == '.')) {
+    // ok, we have a possible url...do more research to find out if we really
+    // have one and determine the length of the url so we can skip over it.
 
-      if (mURLDetector) {
-        int32_t startPos = -1;
-        int32_t endPos = -1;
+    if (mURLDetector) {
+      int32_t startPos = -1;
+      int32_t endPos = -1;
 
-        mURLDetector->FindURLInPlaintext(startWord, endbuf - startWord,
-                                         p - startWord, &startPos, &endPos);
+      mURLDetector->FindURLInPlaintext(startWord, endbuf - startWord,
+                                       p - startWord, &startPos, &endPos);
 
-        // ok, if we got a url, adjust the array bounds, skip the current url
-        // text and find the next word again
-        if (startPos != -1 && endPos != -1) {
-          startWord = p + endPos + 1;  // skip over the url
-          p = startWord;               // reset p
+      // ok, if we got a url, adjust the array bounds, skip the current url
+      // text and find the next word again
+      if (startPos != -1 && endPos != -1) {
+        startWord = p + endPos + 1;  // skip over the url
 
-          // now recursively call FindNextWord to search for the next word now
-          // that we have skipped the url
-          return FindNextWord(word, length, startWord - word, begin, end);
-        }
+        // now recursively call FindNextWord to search for the next word now
+        // that we have skipped the url
+        return FindNextWord(aWord, startWord - word, begin, end);
       }
     }
-
-    while ((p > startWord) &&
-           (*(p - 1) == '\'')) {  // trim trailing apostrophes
-      p--;
-    }
-  } else {
-    startWord = endbuf;
   }
+
+  while ((p > startWord) && (*(p - 1) == '\'')) {  // trim trailing apostrophes
+    p--;
+  }
+
   if (startWord == endbuf) {
     *begin = -1;
     *end = -1;
-  } else {
-    *begin = startWord - word;
-    *end = p - word;
+    return false;
   }
-  return NS_OK;
+  *begin = startWord - word;
+  *end = p - word;
+  return true;
 }
