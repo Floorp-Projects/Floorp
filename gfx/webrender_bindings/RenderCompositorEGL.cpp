b35f31bf45c73eb415f08eb902cb7ff58b4a1a59
Martin Stransky <stransky@redhat.com>
2019-04-12 08:08:03 +0000

    Bug 1535893 - Use SharedGL in RenderCompositorEGL on wayland, r=sotaro
    Differential Revision: https://phabricator.services.mozilla.com/D26800

--- gfx/webrender_bindings/RenderCompositorEGL.cpp
+++ gfx/webrender_bindings/RenderCompositorEGL.cpp
@@ -29,43 +29,17 @@ namespace wr {
 /* static */
 UniquePtr<RenderCompositor> RenderCompositorEGL::Create(
     RefPtr<widget::CompositorWidget> aWidget) {
-#ifdef MOZ_WIDGET_ANDROID
+#ifdef MOZ_WAYLAND
+  if (GDK_IS_X11_DISPLAY(gdk_display_get_default())) {
+    return nullptr;
+  }
+#endif
   if (!RenderThread::Get()->SharedGL()) {
     gfxCriticalNote << "Failed to get shared GL context";
     return nullptr;
   }
   return MakeUnique<RenderCompositorEGL>(aWidget);
-#elif defined(MOZ_WAYLAND)
-  // Disabled SharedGL() usage on wayland. Its usage caused flicker when
-  // multiple windows were opened. See Bug 1535893.
-  if (GDK_IS_X11_DISPLAY(gdk_display_get_default())) {
-    return nullptr;
-  }
-  RefPtr<gl::GLContext> gl;
-  gl = CreateGLContext();
-  if (!gl) {
-    return nullptr;
-  }
-  return MakeUnique<RenderCompositorEGL>(gl, aWidget);
-#endif
 }
-
-#ifdef MOZ_WAYLAND
-/* static */ already_AddRefed<gl::GLContext>
-RenderCompositorEGL::CreateGLContext() {
-  // Create GLContext with dummy EGLSurface.
-  RefPtr<gl::GLContext> gl =
-      gl::GLContextProviderEGL::CreateForCompositorWidget(
-          nullptr, /* aWebRender */ true, /* aForceAccelerated */ true);
-  if (!gl || !gl->MakeCurrent()) {
-    gfxCriticalNote << "Failed GL context creation for WebRender: "
-                    << gfx::hexa(gl.get());
-    return nullptr;
-  }
-
-  return gl.forget();
-}
-#endif
 
 EGLSurface RenderCompositorEGL::CreateEGLSurface() {
   EGLSurface surface = EGL_NO_SURFACE;
@@ -77,19 +51,9 @@ EGLSurface RenderCompositorEGL::CreateEGLSurface() {
   return surface;
 }
 
-#ifdef MOZ_WIDGET_ANDROID
 RenderCompositorEGL::RenderCompositorEGL(
     RefPtr<widget::CompositorWidget> aWidget)
     : RenderCompositor(std::move(aWidget)), mEGLSurface(EGL_NO_SURFACE) {}
-#elif defined(MOZ_WAYLAND)
-RenderCompositorEGL::RenderCompositorEGL(
-    RefPtr<gl::GLContext> aGL, RefPtr<widget::CompositorWidget> aWidget)
-    : RenderCompositor(std::move(aWidget)),
-      mGL(aGL),
-      mEGLSurface(EGL_NO_SURFACE) {
-  MOZ_ASSERT(mGL);
-}
-#endif
 
 RenderCompositorEGL::~RenderCompositorEGL() { DestroyEGLSurface(); }
 
@@ -100,7 +64,6 @@ bool RenderCompositorEGL::BeginFrame() {
     // Destroy EGLSurface if it exists.
     DestroyEGLSurface();
     mEGLSurface = CreateEGLSurface();
-    gl::GLContextEGL::Cast(gl())->SetEGLSurfaceOverride(mEGLSurface);
     if (mEGLSurface) {
       const auto* egl = gl::GLLibraryEGL::Get();
       // Make eglSwapBuffers() non-blocking on wayland
@@ -108,7 +71,7 @@ bool RenderCompositorEGL::BeginFrame() {
     }
   }
 #endif
-  if (!gl()->MakeCurrent()) {
+  if (!MakeCurrent()) {
     gfxCriticalNote << "Failed to make render context current, can't draw.";
     return false;
   }
@@ -147,12 +110,7 @@ bool RenderCompositorEGL::Resume() {
 }
 
 gl::GLContext* RenderCompositorEGL::gl() const {
-#ifdef MOZ_WIDGET_ANDROID
   return RenderThread::Get()->SharedGL();
-#elif defined(MOZ_WAYLAND)
-  MOZ_ASSERT(mGL);
-  return mGL;
-#endif
 }
 
 bool RenderCompositorEGL::MakeCurrent() {
