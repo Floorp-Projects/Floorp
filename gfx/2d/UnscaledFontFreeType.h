4140aef08fe53b667366125c80c83aea4aa907ff
Lee Salzman <lsalzman@mozilla.com>
2019-04-12 12:48:56 +0000

    Bug 1511131 - init UnscaledFontFreeType's face from file when creating scaled fonts. r=jnicol
    Differential Revision: https://phabricator.services.mozilla.com/D27126
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/2d/UnscaledFontFreeType.h
+++ gfx/2d/UnscaledFontFreeType.h
@@ -56,6 +56,8 @@ class UnscaledFontFreeType : public UnscaledFont {
   bool GetWRFontDescriptor(WRFontDescriptorOutput aCb, void* aBaton) override;
 
 #ifdef MOZ_WIDGET_ANDROID
+  FT_Face InitFace();
+
   already_AddRefed<ScaledFont> CreateScaledFont(
       Float aGlyphSize, const uint8_t* aInstanceData,
       uint32_t aInstanceDataLength, const FontVariation* aVariations,
