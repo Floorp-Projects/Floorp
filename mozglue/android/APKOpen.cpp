6eda1cf67c3a76176f21d0bd7ebca569b6742d91
Doug Thayer <dothayer@mozilla.com>
2019-04-12 02:17:48 +0000

    Bug 1538279 - Only readahead DLLs in parent process r=glandium
    There shouldn't be any need to do this for content processes as
    the DLL should already be in the system file cache.
    
    Differential Revision: https://phabricator.services.mozilla.com/D26017

--- mozglue/android/APKOpen.cpp
+++ mozglue/android/APKOpen.cpp
@@ -196,7 +196,8 @@ static mozglueresult loadGeckoLibs() {
   getrusage(RUSAGE_THREAD, &usage1_thread);
   getrusage(RUSAGE_SELF, &usage1);
 
-  gBootstrap = GetBootstrap(getUnpackedLibraryName("libxul.so").get());
+  gBootstrap = GetBootstrap(getUnpackedLibraryName("libxul.so").get(),
+                            LibLoadingStrategy::ReadAhead);
   if (!gBootstrap) {
     __android_log_print(ANDROID_LOG_ERROR, "GeckoLibLoad",
                         "Couldn't get a handle to libxul!");
