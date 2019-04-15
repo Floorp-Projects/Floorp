1dbaeacab4496a3c0cd77594f951ff02aeaf9ab4
Philip Jägenstedt <philip@foolip.org>
2019-04-15 14:35:13 +0000

    Bug 1536634 [wpt PR 15908] - Add --short-reporting-delay flag for Chrome, a=testonly
    Automatic update from web-platform-tests
    Add --short-reporting-delay flag for Chrome (#15908)
    
    --
    
    wpt-commits: 2bff51519c9ab00dc00c8c6e31c06775017d73fc
    wpt-pr: 15908

--- testing/web-platform/tests/tools/wpt/run.py
+++ testing/web-platform/tests/tools/wpt/run.py
@@ -284,6 +284,9 @@ class Chrome(BrowserSetup):
         # Allow WebRTC tests to call getUserMedia.
         kwargs["binary_args"] += ["--use-fake-ui-for-media-stream", "--use-fake-device-for-media-stream"]
 
+        # Shorten delay for Reporting <https://w3c.github.io/reporting/>.
+        kwargs["binary_args"].append("--short-reporting-delay")
+
 
 class ChromeAndroid(BrowserSetup):
     name = "chrome_android"
