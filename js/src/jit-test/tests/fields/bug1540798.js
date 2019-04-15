a9bc7b3e7e435005d7c42e8ed06ddcb2a4013cdf
Ashley Hauck <khyperia@mozilla.com>
2019-04-15 19:16:29 +0000

    Bug 1540798 - Add testcase for fuzzbug. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D27557

---
+++ js/src/jit-test/tests/fields/bug1540798.js
@@ -0,0 +1,14 @@
+try { evaluate(`
+class constructor  { get;                                           } // Long line is long
+// Long line XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+test();
+function test() {
+    try {
+      x;
+    } catch (v) {
+      gc();
+    }
+}
+test();
+`); } catch(exc) {}
+uneval(this);
