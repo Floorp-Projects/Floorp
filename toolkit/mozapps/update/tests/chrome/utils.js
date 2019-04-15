d1a3741601a1f5ca669caae2b0a438eaa25b735c
Kirk Steuber <ksteuber@mozilla.com>
2019-04-15 19:44:45 +0000

    Bug 1520321 - Use BITS in nsUpdateService r=rstrong
    nsUpdateService should use BITS for download. If the BITS download fails, it will fallback to the existing download mechanism (nsIIncrementalDownload).
    
    Differential Revision: https://phabricator.services.mozilla.com/D25162
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/update/tests/chrome/utils.js
+++ toolkit/mozapps/update/tests/chrome/utils.js
@@ -843,6 +843,7 @@ function setupPrefs() {
   Services.prefs.setIntPref(PREF_APP_UPDATE_PROMPTWAITTIME, 0);
   Services.prefs.setBoolPref(PREF_APP_UPDATE_SILENT, false);
   Services.prefs.setBoolPref(PREF_APP_UPDATE_DOORHANGER, false);
+  Services.prefs.setBoolPref(PREF_APP_UPDATE_BITS_ENABLED, false);
 }
 
 /**
@@ -907,6 +908,10 @@ function resetPrefs() {
 
   if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_DOORHANGER)) {
     Services.prefs.clearUserPref(PREF_APP_UPDATE_DOORHANGER);
+  }
+
+  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_BITS_ENABLED)) {
+    Services.prefs.clearUserPref(PREF_APP_UPDATE_BITS_ENABLED);
   }
 }
 
--- toolkit/mozapps/update/tests/data/shared.js
+++ toolkit/mozapps/update/tests/data/shared.js
@@ -19,6 +19,7 @@ ChromeUtils.defineModuleGetter(this, "UpdateUtils",
 const PREF_APP_UPDATE_AUTO                       = "app.update.auto";
 const PREF_APP_UPDATE_BACKGROUNDERRORS           = "app.update.backgroundErrors";
 const PREF_APP_UPDATE_BACKGROUNDMAXERRORS        = "app.update.backgroundMaxErrors";
+const PREF_APP_UPDATE_BITS_ENABLED               = "app.update.BITS.enabled";
 const PREF_APP_UPDATE_CANCELATIONS               = "app.update.cancelations";
 const PREF_APP_UPDATE_CHANNEL                    = "app.update.channel";
 const PREF_APP_UPDATE_DOORHANGER                 = "app.update.doorhanger";
