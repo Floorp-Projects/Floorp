05d949e049f78be1319aa18273ba932e04cea642
Masayuki Nakano <masayuki@d-toybox.com>
2019-04-15 07:29:17 +0000

    Bug 1543966 - part 2: Make "HTML editor commands" and "document state commands" singleton classes r=m_kato
    Some "HTML editor commands" are stateful due to storing tag name to handle it
    with specific command.  However, we can make it stateless with retrieving
    tag name from command name once per command only when it's necessary.  The
    runtime cost must be really cheap since we can map it with hash table.
    
    This patch makes them stateless and singleton classes.  So, we can save
    footprint and allocation runtime cost with this change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27407

--- editor/libeditor/HTMLEditorController.cpp
+++ editor/libeditor/HTMLEditorController.cpp
@@ -9,66 +9,34 @@
 #include "mozilla/mozalloc.h"            // for operator new
 #include "nsControllerCommandTable.h"    // for nsControllerCommandTable
 #include "nsError.h"                     // for NS_OK
-#include "nsGkAtoms.h"                   // for nsGkAtoms, nsGkAtoms::a, etc
 
 namespace mozilla {
 
-#define NS_REGISTER_ONE_COMMAND(_cmdClass, _cmdName)            \
-  {                                                             \
-    _cmdClass *theCmd = new _cmdClass();                        \
-    aCommandTable->RegisterCommand(                             \
-        _cmdName, static_cast<nsIControllerCommand *>(theCmd)); \
-  }
-
-#define NS_REGISTER_FIRST_COMMAND(_cmdClass, _cmdName) \
-  {                                                    \
-    _cmdClass *theCmd = new _cmdClass();               \
-    aCommandTable->RegisterCommand(                    \
-        _cmdName, static_cast<nsIControllerCommand *>(theCmd));
-
-#define NS_REGISTER_NEXT_COMMAND(_cmdClass, _cmdName) \
-  aCommandTable->RegisterCommand(_cmdName,            \
-                                 static_cast<nsIControllerCommand *>(theCmd));
-
-#define NS_REGISTER_LAST_COMMAND(_cmdClass, _cmdName)                          \
-  aCommandTable->RegisterCommand(_cmdName,                                     \
-                                 static_cast<nsIControllerCommand *>(theCmd)); \
-  }
-
-#define NS_REGISTER_STYLE_COMMAND(_cmdClass, _cmdName, _styleTag) \
-  {                                                               \
-    _cmdClass *theCmd = new _cmdClass(_styleTag);                 \
-    aCommandTable->RegisterCommand(                               \
-        _cmdName, static_cast<nsIControllerCommand *>(theCmd));   \
-  }
-
-#define NS_REGISTER_TAG_COMMAND(_cmdClass, _cmdName, _tagName)  \
-  {                                                             \
-    _cmdClass *theCmd = new _cmdClass(_tagName);                \
-    aCommandTable->RegisterCommand(                             \
-        _cmdName, static_cast<nsIControllerCommand *>(theCmd)); \
+#define NS_REGISTER_COMMAND(_cmdClass, _cmdName)                        \
+  {                                                                     \
+    aCommandTable->RegisterCommand(                                     \
+        _cmdName,                                                       \
+        static_cast<nsIControllerCommand *>(_cmdClass::GetInstance())); \
   }
 
 // static
 nsresult HTMLEditorController::RegisterEditorDocStateCommands(
     nsControllerCommandTable *aCommandTable) {
   // observer commands for document state
-  NS_REGISTER_FIRST_COMMAND(DocumentStateCommand, "obs_documentCreated")
-  NS_REGISTER_NEXT_COMMAND(DocumentStateCommand, "obs_documentWillBeDestroyed")
-  NS_REGISTER_LAST_COMMAND(DocumentStateCommand, "obs_documentLocationChanged")
+  NS_REGISTER_COMMAND(DocumentStateCommand, "obs_documentCreated")
+  NS_REGISTER_COMMAND(DocumentStateCommand, "obs_documentWillBeDestroyed")
+  NS_REGISTER_COMMAND(DocumentStateCommand, "obs_documentLocationChanged")
 
   // commands that may get or change state
-  NS_REGISTER_FIRST_COMMAND(SetDocumentStateCommand, "cmd_setDocumentModified")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand, "cmd_setDocumentUseCSS")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand, "cmd_setDocumentReadOnly")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand, "cmd_insertBrOnReturn")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand,
-                           "cmd_defaultParagraphSeparator")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand, "cmd_enableObjectResizing")
-  NS_REGISTER_NEXT_COMMAND(SetDocumentStateCommand,
-                           "cmd_enableInlineTableEditing")
-  NS_REGISTER_LAST_COMMAND(SetDocumentStateCommand,
-                           "cmd_enableAbsolutePositionEditing")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_setDocumentModified")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_setDocumentUseCSS")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_setDocumentReadOnly")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_insertBrOnReturn")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_defaultParagraphSeparator")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_enableObjectResizing")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand, "cmd_enableInlineTableEditing")
+  NS_REGISTER_COMMAND(SetDocumentStateCommand,
+                      "cmd_enableAbsolutePositionEditing")
 
   return NS_OK;
 }
@@ -77,74 +45,96 @@ nsresult HTMLEditorController::RegisterEditorDocStateCommands(
 nsresult HTMLEditorController::RegisterHTMLEditorCommands(
     nsControllerCommandTable *aCommandTable) {
   // Edit menu
-  NS_REGISTER_ONE_COMMAND(PasteNoFormattingCommand, "cmd_pasteNoFormatting");
+  NS_REGISTER_COMMAND(PasteNoFormattingCommand, "cmd_pasteNoFormatting");
 
   // indent/outdent
-  NS_REGISTER_ONE_COMMAND(IndentCommand, "cmd_indent");
-  NS_REGISTER_ONE_COMMAND(OutdentCommand, "cmd_outdent");
+  NS_REGISTER_COMMAND(IndentCommand, "cmd_indent");
+  NS_REGISTER_COMMAND(OutdentCommand, "cmd_outdent");
 
   // Styles
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_bold", nsGkAtoms::b);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_italic", nsGkAtoms::i);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_underline",
-                            nsGkAtoms::u);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_tt", nsGkAtoms::tt);
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_bold");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_italic");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_underline");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_tt");
 
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_strikethrough",
-                            nsGkAtoms::strike);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_superscript",
-                            nsGkAtoms::sup);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_subscript",
-                            nsGkAtoms::sub);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_nobreak",
-                            nsGkAtoms::nobr);
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_strikethrough");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_superscript");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_subscript");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_nobreak");
 
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_em", nsGkAtoms::em);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_strong",
-                            nsGkAtoms::strong);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_cite", nsGkAtoms::cite);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_abbr", nsGkAtoms::abbr);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_acronym",
-                            nsGkAtoms::acronym);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_code", nsGkAtoms::code);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_samp", nsGkAtoms::samp);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_var", nsGkAtoms::var);
-  NS_REGISTER_STYLE_COMMAND(StyleUpdatingCommand, "cmd_removeLinks",
-                            nsGkAtoms::href);
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_em");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_strong");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_cite");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_abbr");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_acronym");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_code");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_samp");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_var");
+  NS_REGISTER_COMMAND(StyleUpdatingCommand, "cmd_removeLinks");
 
   // lists
-  NS_REGISTER_STYLE_COMMAND(ListCommand, "cmd_ol", nsGkAtoms::ol);
-  NS_REGISTER_STYLE_COMMAND(ListCommand, "cmd_ul", nsGkAtoms::ul);
-  NS_REGISTER_STYLE_COMMAND(ListItemCommand, "cmd_dt", nsGkAtoms::dt);
-  NS_REGISTER_STYLE_COMMAND(ListItemCommand, "cmd_dd", nsGkAtoms::dd);
-  NS_REGISTER_ONE_COMMAND(RemoveListCommand, "cmd_removeList");
+  NS_REGISTER_COMMAND(ListCommand, "cmd_ol");
+  NS_REGISTER_COMMAND(ListCommand, "cmd_ul");
+  NS_REGISTER_COMMAND(ListItemCommand, "cmd_dt");
+  NS_REGISTER_COMMAND(ListItemCommand, "cmd_dd");
+  NS_REGISTER_COMMAND(RemoveListCommand, "cmd_removeList");
 
   // format stuff
-  NS_REGISTER_ONE_COMMAND(ParagraphStateCommand, "cmd_paragraphState");
-  NS_REGISTER_ONE_COMMAND(FontFaceStateCommand, "cmd_fontFace");
-  NS_REGISTER_ONE_COMMAND(FontSizeStateCommand, "cmd_fontSize");
-  NS_REGISTER_ONE_COMMAND(FontColorStateCommand, "cmd_fontColor");
-  NS_REGISTER_ONE_COMMAND(BackgroundColorStateCommand, "cmd_backgroundColor");
-  NS_REGISTER_ONE_COMMAND(HighlightColorStateCommand, "cmd_highlight");
+  NS_REGISTER_COMMAND(ParagraphStateCommand, "cmd_paragraphState");
+  NS_REGISTER_COMMAND(FontFaceStateCommand, "cmd_fontFace");
+  NS_REGISTER_COMMAND(FontSizeStateCommand, "cmd_fontSize");
+  NS_REGISTER_COMMAND(FontColorStateCommand, "cmd_fontColor");
+  NS_REGISTER_COMMAND(BackgroundColorStateCommand, "cmd_backgroundColor");
+  NS_REGISTER_COMMAND(HighlightColorStateCommand, "cmd_highlight");
 
-  NS_REGISTER_ONE_COMMAND(AlignCommand, "cmd_align");
-  NS_REGISTER_ONE_COMMAND(RemoveStylesCommand, "cmd_removeStyles");
+  NS_REGISTER_COMMAND(AlignCommand, "cmd_align");
+  NS_REGISTER_COMMAND(RemoveStylesCommand, "cmd_removeStyles");
 
-  NS_REGISTER_ONE_COMMAND(IncreaseFontSizeCommand, "cmd_increaseFont");
-  NS_REGISTER_ONE_COMMAND(DecreaseFontSizeCommand, "cmd_decreaseFont");
+  NS_REGISTER_COMMAND(IncreaseFontSizeCommand, "cmd_increaseFont");
+  NS_REGISTER_COMMAND(DecreaseFontSizeCommand, "cmd_decreaseFont");
 
   // Insert content
-  NS_REGISTER_ONE_COMMAND(InsertHTMLCommand, "cmd_insertHTML");
-  NS_REGISTER_TAG_COMMAND(InsertTagCommand, "cmd_insertLinkNoUI", nsGkAtoms::a);
-  NS_REGISTER_TAG_COMMAND(InsertTagCommand, "cmd_insertImageNoUI",
-                          nsGkAtoms::img);
-  NS_REGISTER_TAG_COMMAND(InsertTagCommand, "cmd_insertHR", nsGkAtoms::hr);
+  NS_REGISTER_COMMAND(InsertHTMLCommand, "cmd_insertHTML");
+  NS_REGISTER_COMMAND(InsertTagCommand, "cmd_insertLinkNoUI");
+  NS_REGISTER_COMMAND(InsertTagCommand, "cmd_insertImageNoUI");
+  NS_REGISTER_COMMAND(InsertTagCommand, "cmd_insertHR");
 
-  NS_REGISTER_ONE_COMMAND(AbsolutePositioningCommand, "cmd_absPos");
-  NS_REGISTER_ONE_COMMAND(DecreaseZIndexCommand, "cmd_decreaseZIndex");
-  NS_REGISTER_ONE_COMMAND(IncreaseZIndexCommand, "cmd_increaseZIndex");
+  NS_REGISTER_COMMAND(AbsolutePositioningCommand, "cmd_absPos");
+  NS_REGISTER_COMMAND(DecreaseZIndexCommand, "cmd_decreaseZIndex");
+  NS_REGISTER_COMMAND(IncreaseZIndexCommand, "cmd_increaseZIndex");
 
   return NS_OK;
+}
+
+// static
+void HTMLEditorController::Shutdown() {
+  // EditorDocStateCommands
+  DocumentStateCommand::Shutdown();
+  SetDocumentStateCommand::Shutdown();
+
+  // HTMLEditorCommands
+  PasteNoFormattingCommand::Shutdown();
+  IndentCommand::Shutdown();
+  OutdentCommand::Shutdown();
+  StyleUpdatingCommand::Shutdown();
+  ListCommand::Shutdown();
+  ListItemCommand::Shutdown();
+  RemoveListCommand::Shutdown();
+  ParagraphStateCommand::Shutdown();
+  FontFaceStateCommand::Shutdown();
+  FontSizeStateCommand::Shutdown();
+  FontColorStateCommand::Shutdown();
+  BackgroundColorStateCommand::Shutdown();
+  HighlightColorStateCommand::Shutdown();
+  AlignCommand::Shutdown();
+  RemoveStylesCommand::Shutdown();
+  IncreaseFontSizeCommand::Shutdown();
+  DecreaseFontSizeCommand::Shutdown();
+  InsertHTMLCommand::Shutdown();
+  InsertTagCommand::Shutdown();
+  AbsolutePositioningCommand::Shutdown();
+  DecreaseZIndexCommand::Shutdown();
+  IncreaseZIndexCommand::Shutdown();
 }
 
 }  // namespace mozilla
