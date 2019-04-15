49b38fb1eeb3336b31bd991d8a0a48f3fa9de44a
Frederik Braun <fbraun@mozilla.com>
2019-04-15 08:03:58 +0000

    Bug 1544008 - re-allow systemprincipal load for discovery pane r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D27291

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -777,6 +777,29 @@ static void AssertSystemPrincipalMustNotLoadRemoteDocuments(
       !nsContentUtils::SchemeIs(finalURI, "ftp")) {
     return;
   }
+
+  // FIXME The discovery feature in about:addons uses the SystemPrincpal.
+  // We should remove this exception with bug 1544011.
+  static nsAutoCString sDiscoveryPrePath;
+  static bool recvdPrefValue = false;
+  if (!recvdPrefValue) {
+    nsAutoCString discoveryURLString;
+    Preferences::GetCString("extensions.webservice.discoverURL", discoveryURLString);
+    // discoverURL is by default suffixed with parameters in path like /%LOCALE%/
+    // so, we use the prePath for comparison
+    nsCOMPtr<nsIURI> discoveryURL;
+    NS_NewURI(getter_AddRefs(discoveryURL), discoveryURLString);
+    if (discoveryURL) {
+      discoveryURL->GetPrePath(sDiscoveryPrePath);
+    }
+    recvdPrefValue = true;
+  }
+  nsAutoCString requestedPrePath;
+  finalURI->GetPrePath(requestedPrePath);
+  if (requestedPrePath.Equals(sDiscoveryPrePath)) {
+    return;
+  }
+
   if (xpc::AreNonLocalConnectionsDisabled()) {
     bool disallowSystemPrincipalRemoteDocuments = Preferences::GetBool(
         "security.disallow_non_local_systemprincipal_in_tests");
