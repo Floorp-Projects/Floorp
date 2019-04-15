4cd7d62e31aeb7d8b0c04c4f8c24310810033b14
Ed Lee <edilee@mozilla.com>
2019-04-15 14:12:34 +0000

    Bug 1544126 - Add FxA snippet, pinned targeting and bug fixes to Activity Stream r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D27387

--- browser/components/newtab/content-src/asrouter/templates/template-manifest.jsx
+++ browser/components/newtab/content-src/asrouter/templates/template-manifest.jsx
@@ -2,6 +2,7 @@ import {EOYSnippet} from "./EOYSnippet/EOYSnippet";
 import {FXASignupSnippet} from "./FXASignupSnippet/FXASignupSnippet";
 import {NewsletterSnippet} from "./NewsletterSnippet/NewsletterSnippet";
 import {SendToDeviceSnippet} from "./SendToDeviceSnippet/SendToDeviceSnippet";
+import {SimpleBelowSearchSnippet} from "./SimpleBelowSearchSnippet/SimpleBelowSearchSnippet";
 import {SimpleSnippet} from "./SimpleSnippet/SimpleSnippet";
 
 // Key names matching schema name of templates
@@ -11,4 +12,5 @@ export const SnippetsTemplates = {
   fxa_signup_snippet: FXASignupSnippet,
   send_to_device_snippet: SendToDeviceSnippet,
   eoy_snippet: EOYSnippet,
+  simple_below_search_snippet: SimpleBelowSearchSnippet,
 };
