27b3144890714926f37af484ed0d39f4a63c816c
Tom Prince <mozilla@hocat.ca>
2019-04-15 20:21:19 +0000

    Bug 1450012: [taskgraph] Move rerun action to retrigger.py; r=dustin
    In order to prevent retriggers for release tasks, we will cause the `retrigger`
    action to rerun instead, so move the cdoe togehter.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27205

--- taskcluster/taskgraph/actions/retrigger.py
+++ taskcluster/taskgraph/actions/retrigger.py
@@ -22,6 +22,8 @@ from taskgraph.util import taskcluster
 
 logger = logging.getLogger(__name__)
 
+RERUN_STATES = ('exception', 'failed')
+
 
 @register_callback_action(
     title='Retrigger',
@@ -111,3 +113,43 @@ def retrigger_action(parameters, graph_config, input, task_group_id, task_id):
 
         logger.info('Scheduled {}{}(time {}/{})'.format(label, with_downstream, i+1, times))
     combine_task_graph_files(list(range(times)))
+
+
+@register_callback_action(
+    title='Rerun',
+    name='rerun',
+    generic=True,
+    symbol='rr',
+    description=(
+        'Rerun a task.\n\n'
+        'This only works on failed or exception tasks in the original taskgraph,'
+        ' and is CoT friendly.'
+    ),
+    order=300,
+    context=[{}],
+    schema={
+        'type': 'object',
+        'properties': {}
+    }
+)
+def rerun_action(parameters, graph_config, input, task_group_id, task_id):
+    task = taskcluster.get_task_definition(task_id)
+    parameters = dict(parameters)
+    decision_task_id, full_task_graph, label_to_taskid = fetch_graph_and_labels(
+        parameters, graph_config)
+    label = task['metadata']['name']
+    if task_id not in label_to_taskid.values():
+        logger.error(
+            "Refusing to rerun {}: taskId {} not in decision task {} label_to_taskid!".format(
+                label, task_id, decision_task_id
+            )
+        )
+
+    status = taskcluster.status_task(task_id)
+    if status not in RERUN_STATES:
+        logger.warning(
+            "No need to to rerun {}: state '{}' not in {}!".format(label, status, RERUN_STATES)
+        )
+        return
+    taskcluster.rerun_task(task_id)
+    logger.info('Reran {}'.format(label))
