c411b93c35b292345707d2a8a24f3d7b951bb9f0
Myk Melez <myk@mykzilla.org>
2019-04-16 16:03:10 +0000

    Bug 1543795 - configure lmdb-rkv-sys to use a smaller MDB_IDL_LOGN size r=nanj,glandium
    Configure the lmdb-rkv-sys Rust crate to use a smaller MDB_IDL_LOGN size in order to reduce allocations when opening an LMDB environment in read-write mode.
    
    @glandium I adopted the configuration strategy you suggested of creating a "feature" for each reasonable value for the MDB_IDL_LOGN macro.  Fortunately, the range of reasonable values is fairly small.
    
    @nanj Based on your evalution in https://github.com/mozilla/lmdb/pull/2, a value of "9" for this macro should aggressively reduce the allocations while still supporting our existing use cases.  But I'm open to increasing it, if you think a higher initial value would be preferable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27559
    
    --HG--
    extra : moz-landing-system : lando

--- third_party/rust/lmdb-rkv-sys/build.rs
+++ third_party/rust/lmdb-rkv-sys/build.rs
@@ -4,6 +4,34 @@ extern crate cc;
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(feature = "mdb_idl_logn_8")]
+const MDB_IDL_LOGN: u8 = 8;
+#[cfg(feature = "mdb_idl_logn_9")]
+const MDB_IDL_LOGN: u8 = 9;
+#[cfg(feature = "mdb_idl_logn_10")]
+const MDB_IDL_LOGN: u8 = 10;
+#[cfg(feature = "mdb_idl_logn_11")]
+const MDB_IDL_LOGN: u8 = 11;
+#[cfg(feature = "mdb_idl_logn_12")]
+const MDB_IDL_LOGN: u8 = 12;
+#[cfg(feature = "mdb_idl_logn_13")]
+const MDB_IDL_LOGN: u8 = 13;
+#[cfg(feature = "mdb_idl_logn_14")]
+const MDB_IDL_LOGN: u8 = 14;
+#[cfg(feature = "mdb_idl_logn_15")]
+const MDB_IDL_LOGN: u8 = 15;
+#[cfg(not(any(
+    feature = "mdb_idl_logn_8",
+    feature = "mdb_idl_logn_9",
+    feature = "mdb_idl_logn_10",
+    feature = "mdb_idl_logn_11",
+    feature = "mdb_idl_logn_12",
+    feature = "mdb_idl_logn_13",
+    feature = "mdb_idl_logn_14",
+    feature = "mdb_idl_logn_15",
+)))]
+const MDB_IDL_LOGN: u8 = 16;
+
 fn main() {
     let mut lmdb: PathBuf = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
     lmdb.push("lmdb");
@@ -12,6 +40,7 @@ fn main() {
 
     if !pkg_config::find_library("liblmdb").is_ok() {
         cc::Build::new()
+                    .define("MDB_IDL_LOGN", Some(MDB_IDL_LOGN.to_string().as_str()))
                     .file(lmdb.join("mdb.c"))
                     .file(lmdb.join("midl.c"))
                     // https://github.com/LMDB/lmdb/blob/LMDB_0.9.21/libraries/liblmdb/Makefile#L25
