4f1acdac6ba83bf1cecbc2000c02f94e38ee05e9
Alastor Wu <alwu@mozilla.com>
2019-04-16 17:19:13 +0000

    Bug 1540746 - TimeUnitToFrames() should handle the case where the input TimeUnit is overflow. r=jya
    If the input has been overflow, we don't need to calculate frames and will directly return invalid result to let caller handle the error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27010

--- dom/media/VideoUtils.cpp
+++ dom/media/VideoUtils.cpp
@@ -66,7 +66,7 @@ CheckedInt64 UsecsToFrames(int64_t aUsecs, uint32_t aRate) {
 
 // Format TimeUnit as number of frames at given rate.
 CheckedInt64 TimeUnitToFrames(const TimeUnit& aTime, uint32_t aRate) {
-  return UsecsToFrames(aTime.ToMicroseconds(), aRate);
+  return aTime.IsValid() ? UsecsToFrames(aTime.ToMicroseconds(), aRate) : aTime;
 }
 
 nsresult SecondsToUsecs(double aSeconds, int64_t& aOutUsecs) {
