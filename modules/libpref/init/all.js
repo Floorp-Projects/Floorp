539b7f8912ebd426e447c93bd83b6c249ebc6333
Mike Conley <mconley@mozilla.com>
2019-04-17 20:10:45 +0000

    Bug 1539567 - Move Picture-in-Picture prefs outside of MOZ_WEBRTC ifdef block. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D27806
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -426,6 +426,11 @@ pref("media.decoder-doctor.verbose", false);
 // URL to report decode issues
 pref("media.decoder-doctor.new-issue-endpoint", "https://webcompat.com/issues/new");
 
+pref("media.videocontrols.picture-in-picture.enabled", false);
+pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+pref("media.videocontrols.picture-in-picture.video-toggle.flyout-enabled", false);
+pref("media.videocontrols.picture-in-picture.video-toggle.flyout-wait-ms", 5000);
+
 #ifdef MOZ_WEBRTC
 pref("media.navigator.enabled", true);
 pref("media.navigator.video.enabled", true);
@@ -444,11 +449,6 @@ pref("media.peerconnection.sdp.rust.compare", true);
 pref("media.peerconnection.sdp.rust.enabled", false);
 pref("media.peerconnection.sdp.rust.compare", false);
 #endif
-
-pref("media.videocontrols.picture-in-picture.enabled", false);
-pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
-pref("media.videocontrols.picture-in-picture.video-toggle.flyout-enabled", false);
-pref("media.videocontrols.picture-in-picture.video-toggle.flyout-wait-ms", 5000);
 
 pref("media.webrtc.debug.trace_mask", 0);
 pref("media.webrtc.debug.multi_log", false);
