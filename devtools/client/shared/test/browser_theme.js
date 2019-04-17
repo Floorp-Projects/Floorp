7e49280207bf1949c8d7d6cda23407a6a343fd1c
Florens Verschelde <florens@fvsch.com>
2019-04-17 14:06:58 +0000

    Bug 1543324 - Part 2: Rename --theme-body-color-* variables to --theme-text-color-*; r=ntim
    Differential Revision: https://phabricator.services.mozilla.com/D27107

--- devtools/client/shared/test/browser_theme.js
+++ devtools/client/shared/test/browser_theme.js
@@ -81,7 +81,7 @@ function testColorExistence() {
     "body-background", "sidebar-background", "contrast-background",
     "tab-toolbar-background", "toolbar-background", "selection-background",
     "selection-color", "selection-background-hover", "splitter-color",
-    "comment", "body-color", "body-color-alt", "text-color-strong",
+    "comment", "body-color", "text-color-alt", "text-color-inactive", "text-color-strong",
     "highlight-green", "highlight-blue", "highlight-bluegrey", "highlight-purple",
     "highlight-lightorange", "highlight-orange", "highlight-red", "highlight-pink",
   ];
