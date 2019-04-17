fee1df6e9b774ddc08cfeabc711a66ad4e638572
derek-li <33161837+derek-li@users.noreply.github.com>
2019-04-17 21:31:12 +0000

    Bug 1545165 - Make WhyPaused more visible r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D27939
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/debugger/src/components/SecondaryPanes/Frames/index.js
+++ devtools/client/debugger/src/components/SecondaryPanes/Frames/index.js
@@ -8,12 +8,10 @@ import React, { Component } from "react";
 import { connect } from "../../../utils/connect";
 import PropTypes from "prop-types";
 
-import type { Frame, Why, ThreadContext } from "../../../types";
+import type { Frame, ThreadContext } from "../../../types";
 
 import FrameComponent from "./Frame";
 import Group from "./Group";
-
-import renderWhyPaused from "./WhyPaused";
 
 import actions from "../../../actions";
 import { collapseFrames, formatCopyName } from "../../../utils/pause/frames";
@@ -23,7 +21,6 @@ import {
   getFrameworkGroupingState,
   getSelectedFrame,
   getCallStackFrames,
-  getPauseReason,
   getCurrentThread,
   getThreadContext
 } from "../../../selectors";
@@ -37,7 +34,6 @@ type Props = {
   frames: Array<Frame>,
   frameworkGroupingOn: boolean,
   selectedFrame: Object,
-  why: Why,
   selectFrame: typeof actions.selectFrame,
   toggleBlackBox: Function,
   toggleFrameworkGrouping: Function,
@@ -196,7 +192,7 @@ class Frames extends Component<Props, State> {
   }
 
   render() {
-    const { frames, disableFrameTruncate, why } = this.props;
+    const { frames, disableFrameTruncate } = this.props;
 
     if (!frames) {
       return (
@@ -211,7 +207,6 @@ class Frames extends Component<Props, State> {
     return (
       <div className="pane frames">
         {this.renderFrames(frames)}
-        {renderWhyPaused(why)}
         {disableFrameTruncate ? null : this.renderToggleButton(frames)}
       </div>
     );
@@ -223,7 +218,6 @@ Frames.contextTypes = { l10n: PropTypes.object };
 const mapStateToProps = state => ({
   cx: getThreadContext(state),
   frames: getCallStackFrames(state),
-  why: getPauseReason(state, getCurrentThread(state)),
   frameworkGroupingOn: getFrameworkGroupingState(state),
   selectedFrame: getSelectedFrame(state, getCurrentThread(state))
 });
---
+++ devtools/client/debugger/src/components/SecondaryPanes/WhyPaused.js
@@ -0,0 +1,110 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+import React, { PureComponent } from "react";
+import { connect } from "../../utils/connect";
+import AccessibleImage from "../shared/AccessibleImage";
+
+import { getPauseReason } from "../../utils/pause";
+import {
+  getCurrentThread,
+  getPaneCollapse,
+  getPauseReason as getWhy
+} from "../../selectors";
+import type { Grip, ExceptionReason } from "../../types";
+
+import "./WhyPaused.css";
+
+type Props = {
+  endPanelCollapsed: boolean,
+  delay: ?number,
+  why: ExceptionReason
+};
+
+type State = {
+  hideWhyPaused: string
+};
+
+class WhyPaused extends PureComponent<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hideWhyPaused: "" };
+  }
+
+  componentDidUpdate() {
+    const { delay } = this.props;
+
+    if (delay) {
+      setTimeout(() => {
+        this.setState({ hideWhyPaused: "" });
+      }, delay);
+    } else {
+      this.setState({ hideWhyPaused: "pane why-paused" });
+    }
+  }
+
+  renderExceptionSummary(exception: string | Grip) {
+    if (typeof exception === "string") {
+      return exception;
+    }
+
+    const preview = exception.preview;
+    if (!preview || !preview.name || !preview.message) {
+      return;
+    }
+
+    return `${preview.name}: ${preview.message}`;
+  }
+
+  renderMessage(why: ExceptionReason) {
+    if (why.type == "exception" && why.exception) {
+      return (
+        <div className={"message warning"}>
+          {this.renderExceptionSummary(why.exception)}
+        </div>
+      );
+    }
+
+    if (typeof why.message == "string") {
+      return <div className={"message"}>{why.message}</div>;
+    }
+
+    return null;
+  }
+
+  render() {
+    const { endPanelCollapsed, why } = this.props;
+    const reason = getPauseReason(why);
+
+    if (reason) {
+      if (!endPanelCollapsed) {
+        return (
+          <div className={"pane why-paused"}>
+            <div>
+              <div className="pause reason">
+                {L10N.getStr(reason)}
+                {this.renderMessage(why)}
+              </div>
+              <div className="info icon">
+                <AccessibleImage className="info" />
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+    return <div className={this.state.hideWhyPaused} />;
+  }
+}
+
+const mapStateToProps = state => {
+  const thread = getCurrentThread(state);
+  return {
+    endPanelCollapsed: getPaneCollapse(state, "end"),
+    why: getWhy(state, thread)
+  };
+};
+
+export default connect(mapStateToProps)(WhyPaused);
