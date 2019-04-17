a2a1b4d1f56d0174180b68c8cd1e77ab61ef3085
Dave Townsend <dtownsend@oxymoronical.com>
2019-04-17 20:06:20 +0000

    Bug 1529879: Block changing the profile list when another process has changed it. r=froydnj,Gijs,flod
    On startup we record the size and modified time of the profile lists. If
    changed we refuse to flush any new changes to disk. Also adds a getter to check
    if they've changed so the UI can do something sensible.
    
    All attempts to flush are now checked for success. In some cases in early
    startup the failure mode isn't great, we just quit startup. The assumption
    though is that it's extremely unlikely that the files will have changed on disk
    in the time between when they are read and when profile selection occurs, likely
    less than a second later.
    
    The profile reset flow is changed to only delete the old profile and flush once
    all the migration has completed, so if something fails the user gets back to
    their old profile.
    
    In testing I ended up having to fix bug 1522584 so background file deletions on
    a background thread are safer.
    
    I haven't implemented any UI tests right now since making modifications to the
    profiles means modifying the actual user's profiles which I'm not keen to do.
    See bug 1539868.
    
    Differential Revision: https://phabricator.services.mozilla.com/D25278
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/profile/nsToolkitProfileService.h
+++ toolkit/profile/nsToolkitProfileService.h
@@ -102,7 +102,8 @@ class nsToolkitProfileService final : public nsIToolkitProfileService {
                                 nsACString& aDescriptor, bool* aIsRelative);
   bool IsProfileForCurrentInstall(nsIToolkitProfile* aProfile);
   void ClearProfileFromOtherInstalls(nsIToolkitProfile* aProfile);
-  bool MaybeMakeDefaultDedicatedProfile(nsIToolkitProfile* aProfile);
+  nsresult MaybeMakeDefaultDedicatedProfile(nsIToolkitProfile* aProfile,
+                                            bool* aResult);
   bool IsSnapEnvironment();
   nsresult CreateDefaultProfile(nsIToolkitProfile** aResult);
   void SetNormalDefault(nsIToolkitProfile* aProfile);
@@ -149,10 +150,17 @@ class nsToolkitProfileService final : public nsIToolkitProfileService {
   bool mCreatedAlternateProfile;
   nsString mStartupReason;
   bool mMaybeLockProfile;
-
   // Holds the current application update channel. This is only really held
   // so it can be overriden in tests.
   nsCString mUpdateChannel;
+  // Keep track of some attributes of the databases so we can tell if another
+  // process has changed them.
+  bool mProfileDBExists;
+  int64_t mProfileDBFileSize;
+  PRTime mProfileDBModifiedTime;
+  bool mInstallDBExists;
+  int64_t mInstallDBFileSize;
+  PRTime mInstallDBModifiedTime;
 
   static nsToolkitProfileService* gService;
 
