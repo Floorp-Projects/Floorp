7aa6ea07da59f29cc89d569dc4de49df09cd00d3
Daisuke Akatsuka <dakatsuka@mozilla.com>
2019-04-18 02:54:34 +0000

    Bug 1540609: Add a test whether the status of connect button will be reverted after a certain period of time. r=jdescottes
    Depends on D27645
    
    Differential Revision: https://phabricator.services.mozilla.com/D27646
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_connection_state.js
+++ devtools/client/aboutdebugging-new/test/browser/browser_aboutdebugging_sidebar_connection_state.js
@@ -8,11 +8,16 @@ const RUNTIME_NAME = "test runtime name";
 const RUNTIME_DEVICE_NAME = "test device name";
 const RUNTIME_SHORT_NAME = "test short name";
 
+const CONNECTION_TIMING_OUT_DELAY = 1000;
+const CONNECTION_CANCEL_DELAY = 2000;
+
 // Test following connection state tests.
 // * Connect button label and state will change during connecting.
 // * Show error message if connection failed.
 // * Show warninng if connection has been taken time.
 add_task(async function() {
+  await setupPreferences();
+
   const mocks = new Mocks();
 
   const { document, tab } = await openAboutDebugging();
@@ -51,11 +56,11 @@ add_task(async function() {
 
   info("Click on the connect button and wait until it disappears");
   connectButton.click();
-  info("Check whether a warning of connection not responding displays after 3sec");
+  info("Check whether a warning of connection not responding displays");
   await waitUntil(() => document.querySelector(".qa-connection-not-responding"));
   ok(document.querySelector(".qa-connection-not-responding"),
      "A warning of connection not responding displays");
-  ok(connectButton.disabled, "State of the connect button displays");
+  ok(connectButton.disabled, "Connect button is disabled");
   ok(connectButton.textContent.startsWith("Connecting"),
      "Label of the connect button changes");
   ok(!document.querySelector(".qa-connection-error"), "Error message disappears");
@@ -73,3 +78,65 @@ add_task(async function() {
 
   await removeTab(tab);
 });
+
+// Test whether the status of all will be reverted after a certain period of time during
+// waiting connection.
+add_task(async function() {
+  await setupPreferences();
+
+  const mocks = new Mocks();
+
+  const { document, tab } = await openAboutDebugging();
+
+  mocks.createUSBRuntime(RUNTIME_ID, {
+    name: RUNTIME_NAME,
+    deviceName: RUNTIME_DEVICE_NAME,
+    shortName: RUNTIME_SHORT_NAME,
+  });
+  mocks.emitUSBUpdate();
+
+  info("Wait until the USB sidebar item appears");
+  await waitUntil(() => findSidebarItemByText(RUNTIME_DEVICE_NAME, document));
+  const usbRuntimeSidebarItem = findSidebarItemByText(RUNTIME_DEVICE_NAME, document);
+  const connectButton = usbRuntimeSidebarItem.querySelector(".js-connect-button");
+
+  let resumeConnection;
+  const resumeConnectionPromise = new Promise(r => {
+    resumeConnection = r;
+  });
+  mocks.runtimeClientFactoryMock.createClientForRuntime = async (runtime) => {
+    await resumeConnectionPromise;
+    return mocks._clients[runtime.type][runtime.id];
+  };
+
+  info("Click on the connect button and wait until it disappears");
+  connectButton.click();
+  await waitUntil(() => document.querySelector(".qa-connection-not-responding"));
+  info("Check whether the all status will be reverted");
+  await waitUntil(() => !document.querySelector(".qa-connection-not-responding"));
+  ok(document.querySelector(".qa-connection-timeout"),
+     "Connection timeout message displays");
+  ok(!connectButton.disabled, "Connect button is enabled");
+  is(connectButton.textContent, "Connect", "Label of the connect button reverted");
+  ok(!document.querySelector(".qa-connection-error"), "Error message disappears");
+
+  info("Check whether the timeout message disappears");
+  resumeConnection();
+  await waitUntil(() => !document.querySelector(".qa-connection-timeout"));
+
+  info("Remove a USB runtime");
+  mocks.removeUSBRuntime(RUNTIME_ID);
+  mocks.emitUSBUpdate();
+
+  info("Wait until the USB sidebar item disappears");
+  await waitUntilUsbDeviceIsUnplugged(RUNTIME_DEVICE_NAME, document);
+
+  await removeTab(tab);
+});
+
+async function setupPreferences() {
+  await pushPref("devtools.aboutdebugging.test-connection-timing-out-delay",
+                 CONNECTION_TIMING_OUT_DELAY);
+  await pushPref("devtools.aboutdebugging.test-connection-cancel-delay",
+                 CONNECTION_CANCEL_DELAY);
+}
