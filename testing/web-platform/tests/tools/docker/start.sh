0cc9f3318c9f6a825fec0109cc3908b911773749
Sam Sneddon <me@gsnedders.com>
2019-04-18 11:56:13 +0000

    Bug 1540048 [wpt PR 16101] - Revert "Don't use HEAD or FETCH_HEAD to checkout specific revisions.", a=testonly
    Automatic update from web-platform-tests
    Revert "Don't use HEAD or FETCH_HEAD to checkout specific revisions. (#16066)" (#16101)
    
    This reverts commit 856a95f08c3674cf34edbd15cf51d4fbffe84a5c.
    --
    
    wpt-commits: fbd91b73ea03f23fffe4717556c088ae344c9501
    wpt-pr: 16101

--- testing/web-platform/tests/tools/docker/start.sh
+++ testing/web-platform/tests/tools/docker/start.sh
@@ -14,6 +14,9 @@ set -ex
 
 REMOTE=${1:-https://github.com/web-platform-tests/wpt}
 REF=${2:-master}
+REVISION=${3:-FETCH_HEAD}
+BROWSER=${4:-all}
+CHANNEL=${5:-nightly}
 
 cd ~
 
@@ -24,6 +27,15 @@ git init
 git remote add origin ${REMOTE}
 
 # Initially we just fetch 50 commits in order to save several minutes of fetching
-retry git fetch --quiet --depth=50 --tags origin ${REF}:task_head
+retry git fetch --quiet --depth=50 --tags origin ${REF}
 
-git checkout --quiet task_head
+if [[ ! `git rev-parse --verify -q ${REVISION}` ]];
+then
+    # But if for some reason the commit under test isn't in that range, we give in and
+    # fetch everything
+    retry git fetch -q --unshallow ${REMOTE}
+    git rev-parse --verify ${REVISION}
+fi
+git checkout -b build ${REVISION}
+
+source tools/ci/start.sh
