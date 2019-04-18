f4f5144e5f3386f3e4c4572bec39c5f770b0bea9
Sam Sneddon <me@gsnedders.com>
2019-04-18 11:56:01 +0000

    Bug 1539649 [wpt PR 16083] - Upload MANIFEST.json.bz2 and MANIFEST.json.zst, a=testonly
    Automatic update from web-platform-tests
    Upload MANIFEST.json.bz2 and MANIFEST.json.zst (#16083)
    
    The first half of #16081.
    
    zstandard has the best compression ratio and fast decoding. bzip2 has good compression
    and very slow decoding. gzip has poor compression and fast decoding. Overall zstandard
    is alway the best to download + decompress, but relies on a third party Python package.
    gzip is always the worst unless network is very fast and CPU very slow. So in the future
    we will move to zstandard where possible and bzip2 where not possible.
    --
    
    wpt-commits: d5921ca1198a8f3ddbecd8392c1dada9c4aa8fbc
    wpt-pr: 16083

--- testing/web-platform/tests/tools/ci/action_manifest_build.sh
+++ testing/web-platform/tests/tools/ci/action_manifest_build.sh
@@ -6,4 +6,6 @@ mkdir -p ~/meta
 WPT_MANIFEST_FILE=~/meta/MANIFEST.json
 
 ./wpt manifest -p $WPT_MANIFEST_FILE
-gzip -f --best $WPT_MANIFEST_FILE
+gzip -k -f --best $WPT_MANIFEST_FILE
+bzip2 -k -f --best $WPT_MANIFEST_FILE
+zstd -k -f --ultra -22 $WPT_MANIFEST_FILE
