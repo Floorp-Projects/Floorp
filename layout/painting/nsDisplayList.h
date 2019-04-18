8edc5f2dedaaf1d5d66dea07293bfeaa03fd00cf
Dan Glastonbury <dan.glastonbury@gmail.com>
2019-04-18 02:41:33 +0000

    Bug 1526972 - P8: Reset InInvalidSubtree when processing unrelated frames. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D26141

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -1132,6 +1132,11 @@ class nsDisplayListBuilder {
    */
   class AutoBuildingDisplayList {
    public:
+    AutoBuildingDisplayList(nsDisplayListBuilder* aBuilder, nsIFrame* aForChild)
+        : AutoBuildingDisplayList(
+              aBuilder, aForChild, aBuilder->GetVisibleRect(),
+              aBuilder->GetDirtyRect(), aForChild->IsTransformed()) {}
+
     AutoBuildingDisplayList(nsDisplayListBuilder* aBuilder, nsIFrame* aForChild,
                             const nsRect& aVisibleRect,
                             const nsRect& aDirtyRect)
