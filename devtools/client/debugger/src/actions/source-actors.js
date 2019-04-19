59376adc139a647ad2982a2a2f5e1308212dd606
Logan Smyth <loganfsmyth@gmail.com>
2019-04-19 16:07:37 +0000

    Bug 1541629 - Part 2: Add a new source-actor action/reducer root type. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D27957
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/debugger/src/actions/source-actors.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// @flow
+
+import type { ThunkArgs } from "./types";
+import type { SourceActor } from "../reducers/source-actors";
+
+export function insertSourceActor(item: SourceActor) {
+  return insertSourceActors([item]);
+}
+export function insertSourceActors(items: Array<SourceActor>) {
+  return function({ dispatch }: ThunkArgs) {
+    dispatch({
+      type: "INSERT_SOURCE_ACTORS",
+      items
+    });
+  };
+}
+
+export function removeSourceActor(item: SourceActor) {
+  return removeSourceActors([item]);
+}
+export function removeSourceActors(items: Array<SourceActor>) {
+  return function({ dispatch }: ThunkArgs) {
+    dispatch({
+      type: "REMOVE_SOURCE_ACTORS",
+      items
+    });
+  };
+}
