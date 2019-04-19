40b3679e2ae69317d5a3eadaccc2995f490f6d45
Emilio Cobos Álvarez <emilio@crisal.io>
2019-04-19 04:32:12 +0000

    Bug 1544546 - Un-fill the styleset and drop the cascade data when removing a pres shell. r=heycam
    This should keep memory usage a bit more under control when loosing a pres
    shell.
    
    Differential Revision: https://phabricator.services.mozilla.com/D27571
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -3987,8 +3987,6 @@ void Document::DeletePresShell() {
     presContext->RefreshDriver()->CancelPendingFullscreenEvents(this);
   }
 
-  mStyleSet->ShellDetachedFromDocument();
-
   // When our shell goes away, request that all our images be immediately
   // discarded, so we don't carry around decoded image data for a document we
   // no longer intend to paint.
@@ -4006,6 +4004,10 @@ void Document::DeletePresShell() {
 
   ClearStaleServoData();
   AssertNoStaleServoDataIn(*this);
+
+  mStyleSet->ShellDetachedFromDocument();
+  mStyleSetFilled = false;
+  mQuirkSheetAdded = false;
 }
 
 void Document::SetBFCacheEntry(nsIBFCacheEntry* aEntry) {
