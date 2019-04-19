7f4d653ea83d4d1b17da7ec2b3c73d0b63ba6449
Emilio Cobos Álvarez <emilio@crisal.io>
2019-04-19 19:04:35 +0200

    Bug 1440537 - Don't propagate flushes across docgroup boundaries. r=bzbarsky
    We don't need to flush layout in the parent document if the parent and child
    documents can't observe each other.
    
    This will also match our behavior in a Fission world.
    
    I'm not attached to the name of the function, better ideas welcome.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28217

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -785,7 +785,8 @@ bool nsComputedDOMStyle::NeedsToFlush() const {
   Document* doc = mElement->OwnerDoc();
   // If parent document is there, also needs to check if there is some change
   // that needs to flush this document (e.g. size change for iframe).
-  while (Document* parentDocument = doc->GetParentDocument()) {
+  while (doc->StyleOrLayoutObservablyDependsOnParentDocumentLayout()) {
+    Document* parentDocument = doc->GetParentDocument();
     Element* element = parentDocument->FindContentForSubDocument(doc);
     if (ElementNeedsRestyle(element, nullptr)) {
       return true;
