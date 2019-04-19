f2cd91cb305fc3a5f6cd6af4e01b91f5b3aab50b
Jason Laster <jlaster@mozilla.com>
2019-04-19 16:07:24 +0000

    Bug 1545103 - applyBreakpoint should no longer support fuzzy matches.
    Differential Revision: https://phabricator.services.mozilla.com/D28203
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/tests/unit/test_setBreakpoint-at-the-beginning-of-a-minified-fn.js
+++ devtools/server/tests/unit/test_setBreakpoint-at-the-beginning-of-a-minified-fn.js
@@ -9,7 +9,7 @@ add_task(threadClientTest(async ({ threadClient, debuggee, client }) => {
 
   // Pause inside of the nested function so we can make sure that we don't
   // add any other breakpoints at other places on this line.
-  const location = { sourceUrl: source.url, line: 3, column: 47 };
+  const location = { sourceUrl: source.url, line: 3, column: 56 };
   setBreakpoint(threadClient, location);
 
   const packet = await executeOnNextTickAndWaitForPause(function() {
