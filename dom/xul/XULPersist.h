174cb99f752292bb05c92058802a6c2a9319c51a
Myk Melez <myk@mykzilla.org>
2019-04-22 02:59:51 +0000

    Bug 1460811 - migrate XULStore to rkv r=bgrins,lina
    Differential Revision: https://phabricator.services.mozilla.com/D25355

--- dom/xul/XULPersist.h
+++ dom/xul/XULPersist.h
@@ -7,8 +7,6 @@
 #ifndef mozilla_dom_XULPersist_h
 #define mozilla_dom_XULPersist_h
 
-class nsIXULStore;
-
 namespace mozilla {
 namespace dom {
 
@@ -33,7 +31,6 @@ class XULPersist final : public nsStubDocumentObserver {
   nsresult ApplyPersistentAttributesToElements(const nsAString& aID,
                                                nsCOMArray<Element>& aElements);
 
-  nsCOMPtr<nsIXULStore> mLocalStore;
   // A weak pointer to our document. Nulled out by DropDocumentReference.
   Document* MOZ_NON_OWNING_REF mDocument;
 };
--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -34,6 +34,7 @@ extern crate log;
 extern crate cert_storage;
 extern crate cosec;
 extern crate rsdparsa_capi;
+extern crate xulstore;
 #[cfg(feature = "spidermonkey_rust")]
 extern crate jsrust_shared;
 #[cfg(feature = "bitsdownload")]
