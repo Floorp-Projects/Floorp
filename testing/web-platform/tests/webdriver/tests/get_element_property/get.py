55f847b9a5668ebe82d73f6133f8853f8e18962f
Andreas Tolfsen <ato@sny.no>
2019-04-23 13:38:53 +0000

    bug 1546319: webdriver: add Get Element Property tests for mutated web content; r=automatedtester
    There are approximately zero coverage for properties modified or set
    by web content and through script evaluation.  The only succcess
    path test for Get Element Property is test_element, but it checks
    an attribute already present in the prototype of the HTMLInputElement.
    
    This patch adds tests where the property is defined through web
    content.  This uncovers a defect in Firefox, where Get Element
    Property seemingly operates on the initial value of the attribute.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28463

--- testing/web-platform/tests/webdriver/tests/get_element_property/get.py
+++ testing/web-platform/tests/webdriver/tests/get_element_property/get.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tests.support.asserts import assert_error, assert_success
 from tests.support.inline import inline
 
@@ -36,6 +38,63 @@ def test_property_non_existent(session):
     response = get_element_property(session, element.id, "foo")
     assert_success(response, None)
     assert session.execute_script("return arguments[0].foo", args=(element,)) is None
+
+
+def test_content_attribute(session):
+    session.url = inline("<input value=foobar>")
+    element = session.find.css("input", all=False)
+
+    response = get_element_property(session, element.id, "value")
+    assert_success(response, "foobar")
+
+
+def test_idl_attribute(session):
+    session.url = inline("<input value=foo>")
+    element = session.find.css("input", all=False)
+    session.execute_script("""arguments[0].value = "bar";""", args=(element,))
+
+    response = get_element_property(session, element.id, "value")
+    assert_success(response, "bar")
+
+
+@pytest.mark.parametrize("js_primitive,py_primitive", [
+    ("\"foobar\"", "foobar"),
+    (42, 42),
+    ([], []),
+    ({}, {}),
+    ("null", None),
+    ("undefined", None),
+])
+def test_primitives(session, js_primitive, py_primitive):
+    session.url = inline("""
+        <input>
+
+        <script>
+        const input = document.querySelector("input");
+        input.foobar = {js_primitive};
+        </script>
+        """.format(js_primitive=js_primitive))
+    element = session.find.css("input", all=False)
+
+    response = get_element_property(session, element.id, "foobar")
+    assert_success(response, py_primitive)
+
+
+@pytest.mark.parametrize("js_primitive,py_primitive", [
+    ("\"foobar\"", "foobar"),
+    (42, 42),
+    ([], []),
+    ({}, {}),
+    ("null", None),
+    ("undefined", None),
+])
+def test_primitives_set_by_execute_script(session, js_primitive, py_primitive):
+    session.url = inline("<input>")
+    element = session.find.css("input", all=False)
+    session.execute_script("arguments[0].foobar = {}".format(js_primitive), args=(element,))
+
+    response = get_element_property(session, element.id, "foobar")
+    assert_success(response, py_primitive)
 
 
 def test_mutated_element(session):
