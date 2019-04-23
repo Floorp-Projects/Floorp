ba29bfcb3104b1434c9680ef464cce29d81d8207
Tom Prince <mozilla@hocat.ca>
2019-04-23 21:12:00 +0000

    Bug 1543872: [taskgraph] Simplify the calculated path to the root of mozilla-central; r=dustin
    On windows, the generated path will be close to the path length limits, which
    causes `mach try` to fail.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28554

--- taskcluster/taskgraph/__init__.py
+++ taskcluster/taskgraph/__init__.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 
-GECKO = os.path.realpath(os.path.join(__file__, '..', '..', '..'))
+GECKO = os.path.normpath(os.path.realpath(os.path.join(__file__, '..', '..', '..')))
 
 # Maximum number of dependencies a single task can have
 # https://docs.taskcluster.net/reference/platform/taskcluster-queue/references/api#createTask
