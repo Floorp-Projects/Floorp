afb4855533e190af1161a1c37c096113f76c78d0
Benjamin Bouvier <benj@benj.me>
2019-04-23 17:14:19 +0000

    Bug 1546439: Allow showing the optimized IR of a Cranelift-compiled function; r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D28513

--- js/src/wasm/cranelift/src/compile.rs
+++ js/src/wasm/cranelift/src/compile.rs
@@ -95,6 +95,7 @@ impl<'a, 'b> BatchCompiler<'a, 'b> {
 
     pub fn compile(&mut self) -> CodegenResult<()> {
         let size = self.context.compile(&*self.isa)?;
+        debug!("Optimized wasm function IR: {}", self);
         self.binemit(size as usize)
     }
 
@@ -120,7 +121,7 @@ impl<'a, 'b> BatchCompiler<'a, 'b> {
         )?;
 
         info!("Translated wasm function {}.", func.index);
-        debug!("Content: {}", self.context.func.display(&*self.isa));
+        debug!("Translated wasm function IR: {}", self);
         Ok(wsig)
     }
 
