87464e6dab1a26810e2efdf29042a1fa24fbbf6a
Kartikaya Gupta <kgupta@mozilla.com>
2019-04-24 17:40:28 +0000

    Bug 1544966 - Put the touch behavior flags on the TouchBlockState earlier. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D28433

--- gfx/layers/apz/src/InputQueue.cpp
+++ gfx/layers/apz/src/InputQueue.cpp
@@ -27,7 +27,8 @@ InputQueue::~InputQueue() { mQueuedInputs.Clear(); }
 nsEventStatus InputQueue::ReceiveInputEvent(
     const RefPtr<AsyncPanZoomController>& aTarget,
     TargetConfirmationFlags aFlags, const InputData& aEvent,
-    uint64_t* aOutInputBlockId) {
+    uint64_t* aOutInputBlockId,
+    const Maybe<nsTArray<TouchBehaviorFlags>>& aTouchBehaviors) {
   APZThreadUtils::AssertOnControllerThread();
 
   AutoRunImmediateTimeout timeoutRunner{this};
@@ -35,7 +36,7 @@ nsEventStatus InputQueue::ReceiveInputEvent(
   switch (aEvent.mInputType) {
     case MULTITOUCH_INPUT: {
       const MultiTouchInput& event = aEvent.AsMultiTouchInput();
-      return ReceiveTouchInput(aTarget, aFlags, event, aOutInputBlockId);
+      return ReceiveTouchInput(aTarget, aFlags, event, aOutInputBlockId, aTouchBehaviors);
     }
 
     case SCROLLWHEEL_INPUT: {
@@ -74,7 +75,8 @@ nsEventStatus InputQueue::ReceiveInputEvent(
 nsEventStatus InputQueue::ReceiveTouchInput(
     const RefPtr<AsyncPanZoomController>& aTarget,
     TargetConfirmationFlags aFlags, const MultiTouchInput& aEvent,
-    uint64_t* aOutInputBlockId) {
+    uint64_t* aOutInputBlockId,
+    const Maybe<nsTArray<TouchBehaviorFlags>>& aTouchBehaviors) {
   TouchBlockState* block = nullptr;
   if (aEvent.mType == MultiTouchInput::MULTITOUCH_START) {
     nsTArray<TouchBehaviorFlags> currentBehaviors;
@@ -115,12 +117,21 @@ nsEventStatus InputQueue::ReceiveTouchInput(
         block->SetAllowedTouchBehaviors(currentBehaviors);
       }
       INPQ_LOG("block %p tagged as fast-motion\n", block);
+    } else if (aTouchBehaviors) {
+      // If this block isn't started during a fast-fling, and APZCTM has
+      // provided touch behavior information, then put it on the block so
+      // that the ArePointerEventsConsumable call below can use it.
+      block->SetAllowedTouchBehaviors(*aTouchBehaviors);
     }
 
     CancelAnimationsForNewBlock(block);
 
     MaybeRequestContentResponse(aTarget, block);
   } else {
+    // for touch inputs that don't start a block, APZCTM shouldn't be giving
+    // us any touch behaviors.
+    MOZ_ASSERT(aTouchBehaviors.isNothing());
+
     block = mActiveTouchBlock.get();
     if (!block) {
       NS_WARNING(
