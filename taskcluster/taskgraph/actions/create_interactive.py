de6775ec21fd99b973886e949213ee236c1a285e
Nathan Froyd <froydnj@mozilla.com>
2019-04-24 14:14:59 +0000

    Bug 1546619 - whitelist internal tooltool downloads for interactive task creation; r=dustin
    These artifacts are "internal" because we can't redistribute them, but
    downloading them into an interactive task is not redistribution.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28656

--- taskcluster/taskgraph/actions/create_interactive.py
+++ taskcluster/taskgraph/actions/create_interactive.py
@@ -54,6 +54,8 @@ SCOPE_WHITELIST = [
     re.compile(r'^secrets:get:project/taskcluster/gecko/(hgfingerprint|hgmointernal)$'),
     # public downloads are OK
     re.compile(r'^docker-worker:relengapi-proxy:tooltool.download.public$'),
+    # internal downloads are OK
+    re.compile(r'^docker-worker:relengapi-proxy:tooltool.download.internal$'),
     # level-appropriate secrets are generally necessary to run a task; these
     # also are "not that secret" - most of them are built into the resulting
     # binary and could be extracted by someone with `strings`.
