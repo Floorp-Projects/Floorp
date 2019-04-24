bbea26527b3f003a9bbd020c7bbf258c44b55435
Andrew Erickson <aerickson@mozilla.com>
2019-04-24 18:14:14 +0000

    Bug 1546187: `mach android-emulator` uses in-tree tooltool.py r=gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D28397

--- testing/mozbase/mozrunner/mozrunner/devices/android_device.py
+++ testing/mozbase/mozrunner/mozrunner/devices/android_device.py
@@ -27,8 +27,7 @@ EMULATOR_HOME_DIR = os.path.join(os.path.expanduser('~'), '.mozbuild', 'android-
 
 EMULATOR_AUTH_FILE = os.path.join(os.path.expanduser('~'), '.emulator_console_auth_token')
 
-TOOLTOOL_URL = ('https://raw.githubusercontent.com/mozilla/release-services/'
-                'master/src/tooltool/client/tooltool.py')
+TOOLTOOL_PATH = 'testing/mozharness/external_tools/tooltool.py'
 
 TRY_URL = 'https://hg.mozilla.org/try/raw-file/default'
 
@@ -865,8 +864,8 @@ def _tooltool_fetch():
     def outputHandler(line):
         _log_debug(line)
 
-    _download_file(TOOLTOOL_URL, 'tooltool.py', EMULATOR_HOME_DIR)
-    command = [sys.executable, 'tooltool.py',
+    tooltool_full_path = os.path.abspath(TOOLTOOL_PATH)
+    command = [sys.executable, tooltool_full_path,
                'fetch', '-o', '-m', 'releng.manifest']
     proc = ProcessHandler(
         command, processOutputLine=outputHandler, storeOutput=False,
