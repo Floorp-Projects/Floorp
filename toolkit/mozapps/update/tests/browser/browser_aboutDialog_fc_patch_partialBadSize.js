cf12922bb0d2ce174815e7c64752fb69477caa6a
Robert Strong <robert.bugzilla@gmail.com>
2019-04-24 18:50:39 +0000

    Bug 1546265 - Fixes for the About Dialog and About Preferences BITS download failure tests. r=bytesized
    Adds a new step parameter for download information named downloadInfo.
    Adds checks for both nsIIncrementalDownload and BITS download result codes.
    Makes 8 tests run using BITS for the update download.
    Adds test tags for bits and internal browser chrome tests.
    Removes the duplicate logging from UpdateService.jsm when running tests locally.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28602

--- toolkit/mozapps/update/tests/browser/browser_aboutDialog_fc_patch_partialBadSize.js
+++ toolkit/mozapps/update/tests/browser/browser_aboutDialog_fc_patch_partialBadSize.js
@@ -6,6 +6,17 @@
 // Test for About Dialog foreground check for updates
 // with a partial bad size patch.
 add_task(async function aboutDialog_foregroundCheck_partialBadSize() {
+  let downloadInfo = [];
+  if (Services.prefs.getBoolPref(PREF_APP_UPDATE_BITS_ENABLED)) {
+    downloadInfo[0] = {patchType: "partial",
+                       bitsResult: gBadSizeResult};
+    downloadInfo[1] = {patchType: "partial",
+                       internalResult: gBadSizeResult};
+  } else {
+    downloadInfo[0] = {patchType: "partial",
+                       internalResult: gBadSizeResult};
+  }
+
   let updateParams = "&partialPatchOnly=1&invalidPartialSize=1";
   await runAboutDialogUpdateTest(updateParams, false, [
     {
@@ -17,6 +28,7 @@ add_task(async function aboutDialog_foregroundCheck_partialBadSize() {
       panelId: "downloading",
       checkActiveUpdate: {state: STATE_DOWNLOADING},
       continueFile: CONTINUE_DOWNLOAD,
+      downloadInfo,
     },
     {
       panelId: "downloadFailed",
