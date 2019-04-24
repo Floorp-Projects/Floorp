a102add5947fcdddb6b4e4e98afe97a02cb58c60
Dão Gottwald <dao@mozilla.com>
2019-04-24 12:55:42 +0000

    Bug 1546423 - Update browser_ext_themes_autocomplete_popup.js for QuantumBar. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D28621

--- toolkit/components/extensions/test/browser/browser_ext_themes_autocomplete_popup.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_autocomplete_popup.js
@@ -4,6 +4,7 @@
 // popup properties are applied correctly to the autocomplete bar.
 const POPUP_COLOR = "#85A400";
 const POPUP_BORDER_COLOR = "#220300";
+const CHROME_CONTENT_SEPARATOR_COLOR = "#220301";
 const POPUP_TEXT_COLOR_DARK = "#000000";
 const POPUP_TEXT_COLOR_BRIGHT = "#ffffff";
 const POPUP_SELECTED_COLOR = "#9400ff";
@@ -56,6 +57,7 @@ add_task(async function test_popup_url() {
           "tab_background_text": TEXT_COLOR,
           "popup": POPUP_COLOR,
           "popup_border": POPUP_BORDER_COLOR,
+          "toolbar_bottom_separator": CHROME_CONTENT_SEPARATOR_COLOR,
           "popup_text": POPUP_TEXT_COLOR_DARK,
           "popup_highlight": POPUP_SELECTED_COLOR,
           "popup_highlight_text": POPUP_SELECTED_TEXT_COLOR,
@@ -102,7 +104,13 @@ add_task(async function test_popup_url() {
                `rgb(${hexToRGB(POPUP_COLOR).join(", ")})`,
                `Popup background color should be set to ${POPUP_COLOR}`);
 
-  testBorderColor(popup, POPUP_BORDER_COLOR);
+  if (quantumbar) {
+    Assert.equal(popupCS.borderBottomColor,
+                 `rgb(${hexToRGB(CHROME_CONTENT_SEPARATOR_COLOR).join(", ")})`,
+                 `Popup bottom color should be set to ${CHROME_CONTENT_SEPARATOR_COLOR}`);
+  } else {
+    testBorderColor(popup, POPUP_BORDER_COLOR);
+  }
 
   Assert.equal(popupCS.color,
                `rgb(${hexToRGB(POPUP_TEXT_COLOR_DARK).join(", ")})`,
