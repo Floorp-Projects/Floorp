e562a7ac38aaf3217408bd50169c2790ded34084
Andrea Marchesini <amarchesini@mozilla.com>
2019-04-25 07:44:29 +0000

    Bug 1545758 - Move dom/file IPC components into mozilla::dom namespace - TemporaryIPCBlob actors, r=qdot
    Differential Revision: https://phabricator.services.mozilla.com/D28190

--- dom/file/ipc/TemporaryIPCBlobParent.h
+++ dom/file/ipc/TemporaryIPCBlobParent.h
@@ -4,20 +4,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_ipc_TemporaryIPCBlobParent_h
-#define mozilla_dom_ipc_TemporaryIPCBlobParent_h
+#ifndef mozilla_dom_TemporaryIPCBlobParent_h
+#define mozilla_dom_TemporaryIPCBlobParent_h
 
-#include "mozilla/ipc/PTemporaryIPCBlob.h"
-#include "mozilla/ipc/PTemporaryIPCBlobParent.h"
+#include "mozilla/dom/PTemporaryIPCBlob.h"
+#include "mozilla/dom/PTemporaryIPCBlobParent.h"
 
 class nsIFile;
 
 namespace mozilla {
 namespace dom {
 
-class TemporaryIPCBlobParent final
-    : public mozilla::ipc::PTemporaryIPCBlobParent {
-  friend class mozilla::ipc::PTemporaryIPCBlobParent;
+class TemporaryIPCBlobParent final : public PTemporaryIPCBlobParent {
+  friend class PTemporaryIPCBlobParent;
 
  public:
   explicit TemporaryIPCBlobParent();
@@ -43,4 +42,4 @@ class TemporaryIPCBlobParent final
 }  // namespace dom
 }  // namespace mozilla
 
-#endif  // mozilla_dom_ipc_TemporaryIPCBlobParent_h
+#endif  // mozilla_dom_TemporaryIPCBlobParent_h
