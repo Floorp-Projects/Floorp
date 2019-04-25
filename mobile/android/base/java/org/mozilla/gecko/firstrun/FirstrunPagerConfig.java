9be797dedcf3aed9c8aeec58ab8e58ef6ad3589c
Petru Lingurar <petru.lingurar@softvision.ro>
2019-04-25 18:10:59 +0000

    Bug 1545805 - Update Fennec Onboarding process; r=VladBaicu a=pascalc
    The new Onboarding process will have updated imagery and strings.
    It will also not show the "Customize" screen anymore.
    
    It will only be shown if the new Strings are localized;
    
    Differential Revision: https://phabricator.services.mozilla.com/D28863
    
    --HG--
    extra : source : 035da268a5d5585c2d7d5996ca7c73263ffdbbc8
    extra : intermediate-source : 883bb879562fc643b87dab14f3d7b7a9a711053f

--- mobile/android/base/java/org/mozilla/gecko/firstrun/FirstrunPagerConfig.java
+++ mobile/android/base/java/org/mozilla/gecko/firstrun/FirstrunPagerConfig.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import org.mozilla.gecko.util.OnboardingStringUtil;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,7 +25,9 @@ class FirstrunPagerConfig {
         final List<FirstrunPanelConfig> panels = new LinkedList<>();
         panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.WELCOME, useLocalValues));
         panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.PRIVACY, useLocalValues));
-        panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.CUSTOMIZE, useLocalValues));
+        if (!OnboardingStringUtil.getInstance(context).areStringsLocalized()) {
+            panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.CUSTOMIZE, useLocalValues));
+        }
         panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.SYNC, useLocalValues));
 
         return panels;
@@ -32,8 +36,12 @@ class FirstrunPagerConfig {
     static List<FirstrunPanelConfig> forFxAUser(Context context, final boolean useLocalValues) {
         final List<FirstrunPanelConfig> panels = new LinkedList<>();
         panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.WELCOME, useLocalValues));
-        panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.PRIVACY, useLocalValues));
-        panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.LAST_CUSTOMIZE, useLocalValues));
+        if (OnboardingStringUtil.getInstance(context).areStringsLocalized()) {
+            panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.LAST_PRIVACY, useLocalValues));
+        } else {
+            panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.PRIVACY, useLocalValues));
+            panels.add(FirstrunPanelConfig.getConfiguredPanel(context, PanelConfig.TYPE.LAST_CUSTOMIZE, useLocalValues));
+        }
 
         return panels;
     }
