043307cbad0dac87a13e33a3409cc7afc043c978
Dorel Luca <dluca@mozilla.com>
2019-04-25 21:01:40 +0300

    Backed out changeset 72ae3513844c (bug 1515204) for Mochitest failures in layout/base/tests/test_reftests_with_caret.html. CLOSED TREE

--- testing/mochitest/tests/SimpleTest/EventUtils.js
+++ testing/mochitest/tests/SimpleTest/EventUtils.js
@@ -2337,7 +2337,6 @@ function synthesizeDropAfterDragOver(aResult, aDataTransfer, aDestElement, aDest
                                   aDataTransfer, aDragEvent);
     sendDragEvent(event, aDestElement, aDestWindow);
   }
-  synthesizeMouse(aDestElement, 2, 2, { type: "mouseup" }, aDestWindow);
 
   return effect;
 }
