e084a162165fd597c172ad5863c327d4a5b762aa
Ryan Hunt <rhunt@eqrion.net>
2019-04-24 23:05:40 -0500

    Bug 1525720, part 2 - Move UITabResolutionChanged method to nsIRemoteTab interface from BrowserParent. r=smaug
    This reduces the amount of code that assumes that BrowserParent implements nsIRemoteTab.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31430
    
    --HG--
    extra : rebase_source : d639864ba86001cf90a8b3d42b6eda97a7f829ac
    extra : histedit_source : 6f6f1e0b6d3561e478391ce9e72a8a2dbc280983

--- dom/base/nsWindowRoot.h
+++ dom/base/nsWindowRoot.h
@@ -70,8 +70,8 @@ class nsWindowRoot final : public nsPIWindowRoot {
 
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(nsWindowRoot)
 
-  virtual void AddBrowser(mozilla::dom::BrowserParent* aBrowser) override;
-  virtual void RemoveBrowser(mozilla::dom::BrowserParent* aBrowser) override;
+  virtual void AddBrowser(nsIRemoteTab* aBrowser) override;
+  virtual void RemoveBrowser(nsIRemoteTab* aBrowser) override;
   virtual void EnumerateBrowsers(BrowserEnumerator aEnumFunc,
                                  void* aArg) override;
 
