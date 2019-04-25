f8b91466be42d5153c5317a0ff27efe3e260e6b0
Andrea Marchesini <amarchesini@mozilla.com>
2019-04-25 19:39:35 +0000

    Bug 1545273 - User-Interaction required for trackers only for some urls, set in privacy.restrict3rdpartystorage.userInteractionRequiredForHosts, r=Ehsan
    This patch fixes a bug about when the
    privacy.restrict3rdpartystorage.userInteractionRequiredForHosts should be
    considered.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28155
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/antitracking/test/browser/storageprincipal_head.js
+++ toolkit/components/antitracking/test/browser/storageprincipal_head.js
@@ -20,6 +20,7 @@ this.StoragePrincipalHelper = {
         ["privacy.trackingprotection.pbmode.enabled", false],
         ["privacy.trackingprotection.annotate_channels", true],
         ["privacy.storagePrincipal.enabledForTrackers", true],
+        ["privacy.restrict3rdpartystorage.userInteractionRequiredForHosts", "tracking.example.com,tracking.example.org"],
       ]});
 
       if (extraPrefs && Array.isArray(extraPrefs) && extraPrefs.length) {
