b3e208c4afce55a8408a5fd8555b12144a1f0b0f
Andrew Halberstadt <ahalberstadt@mozilla.com>
2019-04-26 12:45:38 +0000

    Bug 1546100 - [mochitest] Use kwargs from moztest.resolve in mach mochitest, r=jmaher
    These days 'moztest.resolve' is becoming the defacto place to define test harness
    related metadata. Currently |mach test| uses it to find kwargs that need to get
    set for mochitest commands, but |mach mochitest| does not. Instead, it grabs this
    metadata from 'mochitest_options.py'.
    
    This patch makes the kwargs used consistent between both |mach test| and |mach
    mochitest|.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28537
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -644,6 +644,11 @@ class MochitestArguments(ArgumentContainer):
         if options.flavor is None:
             options.flavor = 'plain'
 
+        for value in ALL_FLAVORS.values():
+            if options.flavor in value['aliases']:
+                options.flavor = value['suite']
+                break
+
         if options.gmp_path is None and options.app and build_obj:
             # Need to fix the location of gmp_fake which might not be shipped in the binary
             gmp_modules = (
