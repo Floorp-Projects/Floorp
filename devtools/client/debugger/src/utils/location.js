2a75475e548d28fff97dd022271f8049cfcfec49
Logan Smyth <loganfsmyth@gmail.com>
2019-04-26 23:00:26 +0000

    Bug 1547306 - Part 1: Split getSelectedLocation into its own file to avoid dependency cycle. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D29021

--- devtools/client/debugger/src/utils/location.js
+++ devtools/client/debugger/src/utils/location.js
@@ -4,7 +4,7 @@
 
 // @flow
 import { sortBy } from "lodash";
-import { getSelectedLocation } from "./source-maps";
+import { getSelectedLocation } from "./selected-location";
 
 import type {
   MappedLocation,
