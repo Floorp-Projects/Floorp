a3f0aa60d48381fab1a9f25e736d1beba29308c8
Tom Tung <shes050117@gmail.com>
2019-04-29 10:49:58 +0000

    Bug 1514600 - P1 - Notify LS to cleanup the gUsages while storage initialization fails; r=janv
    Differential Revision: https://phabricator.services.mozilla.com/D27502
    
    --HG--
    extra : moz-landing-system : lando

--- dom/quota/Client.h
+++ dom/quota/Client.h
@@ -164,6 +164,8 @@ class Client {
 
   virtual void ReleaseIOThreadObjects() = 0;
 
+  virtual void OnStorageInitFailed(){};
+
   // Methods which are called on the background thread.
   virtual void AbortOperations(const nsACString& aOrigin) = 0;
 
