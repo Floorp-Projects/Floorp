3bbe9476e37ccbd1c9119b0626e26552170ae7ca
shindli <shindli@mozilla.com>
2019-04-30 04:37:53 +0300

    Backed out 2 changesets (bug 1544976) for causing xpcshell failures in devtools/client/memory/test/unit/test_action_diffing_02.js
    Backed out changeset 30b551b1a212 (bug 1544976)
    Backed out changeset 1ebc89e3ad97 (bug 1544976)

--- devtools/shared/preferences/devtools-shared.js
+++ devtools/shared/preferences/devtools-shared.js
@@ -32,7 +32,6 @@ pref("devtools.debugger.remote-enabled", true, sticky);
 // Disable remote debugging protocol logging
 pref("devtools.debugger.log", false);
 pref("devtools.debugger.log.verbose", false);
-pref("devtools.debugger.quiet", false);
 
 pref("devtools.debugger.remote-port", 6000);
 pref("devtools.debugger.remote-websocket", false);
