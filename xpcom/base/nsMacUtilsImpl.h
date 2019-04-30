cfa2ec526270f68b5460808d021879ed6ebcbe32
Haik Aftandilian <haftandilian@mozilla.com>
2019-04-30 14:01:20 -0700

    Bug 1546544 - Enable TCSM. r=mccr8,asuth,spohl a=pascalc

--- xpcom/base/nsMacUtilsImpl.h
+++ xpcom/base/nsMacUtilsImpl.h
@@ -9,7 +9,10 @@
 
 #include "nsIMacUtils.h"
 #include "nsString.h"
+#include "mozilla/Atomics.h"
 #include "mozilla/Attributes.h"
+
+using mozilla::Atomic;
 
 class nsMacUtilsImpl final : public nsIMacUtils {
  public:
@@ -26,6 +29,8 @@ class nsMacUtilsImpl final : public nsIMacUtils {
 #  endif /* DEBUG */
 #endif   /* MOZ_CONTENT_SANDBOX */
 
+  static void EnableTCSMIfAvailable();
+
  private:
   ~nsMacUtilsImpl() {}
 
@@ -34,6 +39,15 @@ class nsMacUtilsImpl final : public nsIMacUtils {
   // A string containing a "-" delimited list of architectures
   // in our binary.
   nsString mBinaryArchs;
+
+  enum TCSMStatus { TCSM_Unknown = 0, TCSM_Available, TCSM_Unavailable };
+  static mozilla::Atomic<nsMacUtilsImpl::TCSMStatus> sTCSMStatus;
+
+  static bool IsTCSMAvailable();
+  static nsresult EnableTCSM();
+#if defined(DEBUG)
+  static bool IsTCSMEnabled();
+#endif
 };
 
 // Global singleton service
