aa467994aed2a39d3ebc32b22d6bce8294682862
shindli <shindli@mozilla.com>
2019-04-30 04:37:53 +0300

    Backed out 2 changesets (bug 1544976) for causing xpcshell failures in devtools/client/memory/test/unit/test_action_diffing_02.js. a=backout
    Backed out changeset 30b551b1a212 (bug 1544976)
    Backed out changeset 1ebc89e3ad97 (bug 1544976)

--- devtools/client/debugger/src/client/firefox/workers.js
+++ devtools/client/debugger/src/client/firefox/workers.js
@@ -26,32 +26,26 @@ export async function updateWorkerClients({
 
   const { workers } = await tabTarget.listWorkers();
   for (const workerTargetFront of workers) {
-    try {
-      await workerTargetFront.attach();
-      const [, workerThread] = await workerTargetFront.attachThread(options);
+    await workerTargetFront.attach();
+    const [, workerThread] = await workerTargetFront.attachThread(options);
 
-      const actor = workerThread.actor;
-      if (workerClients[actor]) {
-        if (workerClients[actor].thread != workerThread) {
-          console.error(`Multiple clients for actor ID: ${workerThread.actor}`);
-        }
-        newWorkerClients[actor] = workerClients[actor];
-      } else {
-        addThreadEventListeners(workerThread);
-        workerThread.resume();
-
-        const consoleFront = await workerTargetFront.getFront("console");
-        await consoleFront.startListeners([]);
-
-        newWorkerClients[actor] = {
-          url: workerTargetFront.url,
-          thread: workerThread,
-          console: consoleFront
-        };
+    if (workerClients[workerThread.actor]) {
+      if (workerClients[workerThread.actor].thread != workerThread) {
+        throw new Error(`Multiple clients for actor ID: ${workerThread.actor}`);
       }
-    } catch (e) {
-      // If any of the workers have terminated since the list command initiated
-      // then we will get errors. Ignore these.
+      newWorkerClients[workerThread.actor] = workerClients[workerThread.actor];
+    } else {
+      addThreadEventListeners(workerThread);
+      workerThread.resume();
+
+      const consoleFront = await workerTargetFront.getFront("console");
+      await consoleFront.startListeners([]);
+
+      newWorkerClients[workerThread.actor] = {
+        url: workerTargetFront.url,
+        thread: workerThread,
+        console: consoleFront
+      };
     }
   }
 
