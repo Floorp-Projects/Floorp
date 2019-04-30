8cbcd032126c17ac86c9c93632109481a41bf0c2
Petru Lingurar <petru.lingurar@softvision.ro>
2019-04-30 16:43:46 +0300

    Bug 1547924 - Ensure Leanplum Onboarding falls back to the new Onboarding UX; r=VladBaicu
    Summary:
    - Added the `LAST_PRIVACY` case for when the user is already signed into Firefox;
    - Use the new default onboarding strings and images. If they are localized.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29347

--- mobile/android/base/java/org/mozilla/gecko/mma/LeanplumVariables.java
+++ mobile/android/base/java/org/mozilla/gecko/mma/LeanplumVariables.java
@@ -13,6 +13,8 @@ import android.support.annotation.NonNull;
 import com.leanplum.annotations.Variable;
 
 import org.mozilla.gecko.R;
+import org.mozilla.gecko.firstrun.FirstrunPanel;
+import org.mozilla.gecko.util.OnboardingStringUtil;
 
 import java.lang.reflect.Field;
 
@@ -77,25 +79,43 @@ public class LeanplumVariables {
      */
     private LeanplumVariables(@NonNull Context context) {
         appResources = context.getResources();
+
+        // Same titles for the screens of the old / new onboarding UX.
         welcomePanelTitle = appResources.getString(R.string.firstrun_panel_title_welcome);
-        welcomePanelMessage = appResources.getString(R.string.firstrun_urlbar_message);
-        welcomePanelSubtext = appResources.getString(R.string.firstrun_urlbar_subtext);
-        welcomeDrawableId = R.drawable.firstrun_welcome;
-
         privacyPanelTitle = appResources.getString(R.string.firstrun_panel_title_privacy);
-        privacyPanelMessage = appResources.getString(R.string.firstrun_privacy_message);
-        privacyPanelSubtext = appResources.getString(R.string.firstrun_privacy_subtext);
-        privacyDrawableId = R.drawable.firstrun_private;
-
         customizePanelTitle = appResources.getString(R.string.firstrun_panel_title_customize);
-        customizePanelMessage = appResources.getString(R.string.firstrun_customize_message);
-        customizePanelSubtext = appResources.getString(R.string.firstrun_customize_subtext);
-        customizingDrawableId = R.drawable.firstrun_data;
-
         syncPanelTitle = appResources.getString(R.string.firstrun_sync_title);
-        syncPanelMessage = appResources.getString(R.string.firstrun_sync_message);
-        syncPanelSubtext = appResources.getString(R.string.firstrun_sync_subtext);
-        syncDrawableId = R.drawable.firstrun_sync;
+
+        // The new Onboarding UX uses different messages and images. Only if they are localized.
+        if (OnboardingStringUtil.getInstance(context).areStringsLocalized()) {
+            welcomePanelMessage = appResources.getString(R.string.newfirstrun_urlbar_message);
+            welcomePanelSubtext = appResources.getString(R.string.newfirstrun_privacy_subtext);
+            welcomeDrawableId = R.drawable.firstrun_welcome2;
+
+            privacyPanelMessage = FirstrunPanel.NO_MESSAGE;
+            privacyPanelSubtext = appResources.getString(R.string.newfirstrun_privacy_subtext);
+            privacyDrawableId = R.drawable.firstrun_private2;
+
+            syncPanelMessage = FirstrunPanel.NO_MESSAGE;
+            syncPanelSubtext = appResources.getString(R.string.newfirstrun_sync_subtext);
+            syncDrawableId = R.drawable.firstrun_sync2;
+        } else {
+            welcomePanelMessage = appResources.getString(R.string.firstrun_urlbar_message);
+            welcomePanelSubtext = appResources.getString(R.string.firstrun_urlbar_subtext);
+            welcomeDrawableId = R.drawable.firstrun_welcome;
+
+            privacyPanelMessage = appResources.getString(R.string.firstrun_privacy_message);
+            privacyPanelSubtext = appResources.getString(R.string.firstrun_privacy_subtext);
+            privacyDrawableId = R.drawable.firstrun_private;
+
+            customizePanelMessage = appResources.getString(R.string.firstrun_customize_message);
+            customizePanelSubtext = appResources.getString(R.string.firstrun_customize_subtext);
+            customizingDrawableId = R.drawable.firstrun_data;
+
+            syncPanelMessage = appResources.getString(R.string.firstrun_sync_message);
+            syncPanelSubtext = appResources.getString(R.string.firstrun_sync_subtext);
+            syncDrawableId = R.drawable.firstrun_sync;
+        }
     }
 
     public static int getWelcomeImage() {
