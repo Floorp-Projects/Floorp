6bb9951cf70274a0b0f6a3976e463eb44ab2dfa9
Kenny Levinsen <kl@kl.wtf>
2019-04-30 09:37:37 +0000

    Bug 1467127 - Post wl_display_dispatch_queue_pending tasks from ProcessNextNativeEvent. r=stransky
    Send tasks to dispatch our other wayland event queues from their respective threads whenever we service the GTK main event loop.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28819
    
    --HG--
    extra : moz-landing-system : lando

---
+++ widget/gtk/nsWaylandDisplayShutdown.h
@@ -0,0 +1,19 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:expandtab:shiftwidth=4:tabstop=4:
+ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __MOZ_WAYLAND_DISPLAY_SHUTDOWN_H__
+#define __MOZ_WAYLAND_DISPLAY_SHUTDOWN_H__
+
+namespace mozilla {
+namespace widget {
+
+void WaylandDisplayShutdown();
+
+}  // namespace widget
+}  // namespace mozilla
+
+#endif  // __MOZ_WAYLAND_DISPLAY_SHUTDOWN_H__
