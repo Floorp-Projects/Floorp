4462730da2e83311f0403c00964377aa2a8b6b5d
Johan Lorenzo <jlorenzo@mozilla.com>
2019-04-30 13:57:48 +0000

    Bug 1546320 - Create mozconfigs for Fennec beta builds r=RyanVM
    Create mozconfigs for Fennec beta builds.
    This also prevents mergeday scripts from modifying nightly mozconfigs on beta
    
    Differential Revision: https://phabricator.services.mozilla.com/D29350

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_beta_debug.py
@@ -0,0 +1,6 @@
+config = {
+    'stage_platform': 'android-x86_64-debug',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-x86_64/debug-beta',
+    'multi_locale_config_platform': 'android',
+    'debug_build': True,
+}
