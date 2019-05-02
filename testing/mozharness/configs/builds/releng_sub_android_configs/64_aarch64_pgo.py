3e32d74d22b29c9d2cab5d6090b48098a4198b86
Mike Shal <mshal@mozilla.com>
2019-05-02 21:36:46 +0000

    Bug 1543212 - Create a PGO-optimized android-aarch64 build; r=firefox-build-system-reviewers,chmanchester
    Differential Revision: https://phabricator.services.mozilla.com/D29588
    
    --HG--
    extra : moz-landing-system : lando

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_pgo.py
@@ -0,0 +1,5 @@
+config = {
+    'stage_platform': 'android-aarch64-pgo',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-aarch64/profile-use',
+    'multi_locale_config_platform': 'android',
+}
