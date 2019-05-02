Floorp-Projects/Floorp

commit 0e9c4444628f6024749745128e44ba232c42b248

Bug 1052579 - Allow JS AllocPolicy to be assigned to an arena r=sfink
Several areas of JS code abstract their memory allocation logic behind an
AllocPolicy. Since these allocations may now need to be in different arenas,
each AllocPolicy will need to be assigned a Mozjemalloc arena that it will
use for all its allocations.

Differential Revision: https://phabricator.services.mozilla.com/D25705

js/public/AllocPolicy.h
@@ -25,18 +25,26 @@ enum class AllocFunction { Malloc, Calloc, Realloc };
 
 /* Base class allocation policies providing allocation methods. */
 class AllocPolicyBase {
+  const arena_id_t& arenaId_;
+
+ protected:
+  arena_id_t getArenaId() { return arenaId_; }
+
  public:
+  explicit AllocPolicyBase(const arena_id_t& arenaId = js::MallocArena)
+      : arenaId_(arenaId) {}
+
   template <typename T>
   T* maybe_pod_malloc(size_t numElems) {
-    return js_pod_malloc<T>(numElems);
+    return js_pod_arena_malloc<T>(getArenaId(), numElems);
   }
   template <typename T>
   T* maybe_pod_calloc(size_t numElems) {
-    return js_pod_calloc<T>(numElems);
+    return js_pod_arena_calloc<T>(getArenaId(), numElems);
   }
   template <typename T>
   T* maybe_pod_realloc(T* p, size_t oldSize, size_t newSize) {
-    return js_pod_realloc<T>(p, oldSize, newSize);
+    return js_pod_arena_realloc<T>(getArenaId(), p, oldSize, newSize);
   }
   template <typename T>
   T* pod_malloc(size_t numElems) {
@@ -95,7 +103,9 @@ class TempAllocPolicy : public AllocPolicyBase {
   }
 
  public:
-  MOZ_IMPLICIT TempAllocPolicy(JSContext* cx) : cx_(cx) {}
+  MOZ_IMPLICIT TempAllocPolicy(JSContext* cx,
+                               const arena_id_t& arenaId = js::MallocArena)
+      : AllocPolicyBase(arenaId), cx_(cx) {}
 
   template <typename T>
   T* pod_malloc(size_t numElems) {

js/src/util/AllocPolicy.cpp
@@ -12,7 +12,7 @@ using namespace js;
 
 void* TempAllocPolicy::onOutOfMemory(AllocFunction allocFunc, size_t nbytes,
                                      void* reallocPtr) {
-  return cx_->onOutOfMemory(allocFunc, js::MallocArena, nbytes, reallocPtr);
+  return cx_->onOutOfMemory(allocFunc, this->getArenaId(), nbytes, reallocPtr);
 }
 
 void TempAllocPolicy::reportAllocOverflow() const {

commit 482055a2f539339ab4be69df90e2686919f29f03

Bug 1052579 - Remove arenaId member that was introduced in D25705 r=sfink
In D25705, I added a new arenaId member to the js::BaseAllocPolicy. This
increased the size of everything that uses a JS AllocPolicy, which is a
lot.

This change follows suit from earlier work, which is to make everything
allocation-related have an "arena" version and a "default" version that
uses the arena version with the implied default arena.

StringBuffer is then changed to use this new functionity to define its
own alloc policy that uses the new StringBufferArena.

Differential Revision: https://phabricator.services.mozilla.com/D29685

js/public/AllocPolicy.h
@@ -25,39 +25,57 @@ enum class AllocFunction { Malloc, Calloc, Realloc };
 
 /* Base class allocation policies providing allocation methods. */
 class AllocPolicyBase {
-  const arena_id_t& arenaId_;
-
- protected:
-  arena_id_t getArenaId() { return arenaId_; }
-
  public:
-  explicit AllocPolicyBase(const arena_id_t& arenaId = js::MallocArena)
-      : arenaId_(arenaId) {}
+  template <typename T>
+  T* maybe_pod_arena_malloc(arena_id_t arenaId, size_t numElems) {
+    return js_pod_arena_malloc<T>(arenaId, numElems);
+  }
+  template <typename T>
+  T* maybe_pod_arena_calloc(arena_id_t arenaId, size_t numElems) {
+    return js_pod_arena_calloc<T>(arenaId, numElems);
+  }
+  template <typename T>
+  T* maybe_pod_arena_realloc(arena_id_t arenaId, T* p, size_t oldSize, size_t newSize) {
+    return js_pod_arena_realloc<T>(arenaId, p, oldSize, newSize);
+  }
+  template <typename T>
+  T* pod_arena_malloc(arena_id_t arenaId, size_t numElems) {
+    return maybe_pod_arena_malloc<T>(arenaId, numElems);
+  }
+  template <typename T>
+  T* pod_arena_calloc(arena_id_t arenaId, size_t numElems) {
+    return maybe_pod_arena_calloc<T>(arenaId, numElems);
+  }
+  template <typename T>
+  T* pod_arena_realloc(arena_id_t arenaId, T* p, size_t oldSize, size_t newSize) {
+    return maybe_pod_arena_realloc<T>(arenaId, p, oldSize, newSize);
+  }
 
   template <typename T>
   T* maybe_pod_malloc(size_t numElems) {
-    return js_pod_arena_malloc<T>(getArenaId(), numElems);
+    return maybe_pod_arena_malloc<T>(js::MallocArena, numElems);
   }
   template <typename T>
   T* maybe_pod_calloc(size_t numElems) {
-    return js_pod_arena_calloc<T>(getArenaId(), numElems);
+    return maybe_pod_arena_calloc<T>(js::MallocArena, numElems);
   }
   template <typename T>
   T* maybe_pod_realloc(T* p, size_t oldSize, size_t newSize) {
-    return js_pod_arena_realloc<T>(getArenaId(), p, oldSize, newSize);
+    return maybe_pod_arena_realloc<T>(js::MallocArena, p, oldSize, newSize);
   }
   template <typename T>
   T* pod_malloc(size_t numElems) {
-    return maybe_pod_malloc<T>(numElems);
+    return pod_arena_malloc<T>(js::MallocArena, numElems);
   }
   template <typename T>
   T* pod_calloc(size_t numElems) {
-    return maybe_pod_calloc<T>(numElems);
+    return pod_arena_calloc<T>(js::MallocArena, numElems);
   }
   template <typename T>
   T* pod_realloc(T* p, size_t oldSize, size_t newSize) {
-    return maybe_pod_realloc<T>(p, oldSize, newSize);
+    return pod_arena_realloc<T>(js::MallocArena, p, oldSize, newSize);
   }
+
   template <typename T>
   void free_(T* p, size_t numElems = 0) {
     js_free(p);
@@ -89,49 +107,64 @@ class TempAllocPolicy : public AllocPolicyBase {
    * Non-inline helper to call JSRuntime::onOutOfMemory with minimal
    * code bloat.
    */
-  JS_FRIEND_API void* onOutOfMemory(AllocFunction allocFunc, size_t nbytes,
-                                    void* reallocPtr = nullptr);
+  JS_FRIEND_API void* onOutOfMemory(arena_id_t arenaId, AllocFunction allocFunc,
+                                    size_t nbytes, void* reallocPtr = nullptr);
 
   template <typename T>
-  T* onOutOfMemoryTyped(AllocFunction allocFunc, size_t numElems,
-                        void* reallocPtr = nullptr) {
+  T* onOutOfMemoryTyped(arena_id_t arenaId, AllocFunction allocFunc,
+                        size_t numElems, void* reallocPtr = nullptr) {
     size_t bytes;
     if (MOZ_UNLIKELY(!CalculateAllocSize<T>(numElems, &bytes))) {
       return nullptr;
     }
-    return static_cast<T*>(onOutOfMemory(allocFunc, bytes, reallocPtr));
+    return static_cast<T*>(
+        onOutOfMemory(arenaId, allocFunc, bytes, reallocPtr));
   }
 
  public:
-  MOZ_IMPLICIT TempAllocPolicy(JSContext* cx,
-                               const arena_id_t& arenaId = js::MallocArena)
-      : AllocPolicyBase(arenaId), cx_(cx) {}
+  MOZ_IMPLICIT TempAllocPolicy(JSContext* cx) : cx_(cx) {}
+
+  template <typename T>
+  T* pod_arena_malloc(arena_id_t arenaId, size_t numElems) {
+    T* p = this->maybe_pod_arena_malloc<T>(arenaId, numElems);
+    if (MOZ_UNLIKELY(!p)) {
+      p = onOutOfMemoryTyped<T>(arenaId, AllocFunction::Malloc, numElems);
+    }
+    return p;
+  }
+
+  template <typename T>
+  T* pod_arena_calloc(arena_id_t arenaId, size_t numElems) {
+    T* p = this->maybe_pod_arena_calloc<T>(arenaId, numElems);
+    if (MOZ_UNLIKELY(!p)) {
+      p = onOutOfMemoryTyped<T>(arenaId, AllocFunction::Calloc, numElems);
+    }
+    return p;
+  }
+
+  template <typename T>
+  T* pod_arena_realloc(arena_id_t arenaId, T* prior, size_t oldSize, size_t newSize) {
+    T* p2 = this->maybe_pod_arena_realloc<T>(arenaId, prior, oldSize, newSize);
+    if (MOZ_UNLIKELY(!p2)) {
+      p2 = onOutOfMemoryTyped<T>(arenaId, AllocFunction::Realloc, newSize,
+                                 prior);
+    }
+    return p2;
+  }
 
   template <typename T>
   T* pod_malloc(size_t numElems) {
-    T* p = this->maybe_pod_malloc<T>(numElems);
-    if (MOZ_UNLIKELY(!p)) {
-      p = onOutOfMemoryTyped<T>(AllocFunction::Malloc, numElems);
-    }
-    return p;
+    return pod_arena_malloc<T>(js::MallocArena, numElems);
   }
 
   template <typename T>
   T* pod_calloc(size_t numElems) {
-    T* p = this->maybe_pod_calloc<T>(numElems);
-    if (MOZ_UNLIKELY(!p)) {
-      p = onOutOfMemoryTyped<T>(AllocFunction::Calloc, numElems);
-    }
-    return p;
+    return pod_arena_calloc<T>(js::MallocArena, numElems);
   }
 
   template <typename T>
   T* pod_realloc(T* prior, size_t oldSize, size_t newSize) {
-    T* p2 = this->maybe_pod_realloc<T>(prior, oldSize, newSize);
-    if (MOZ_UNLIKELY(!p2)) {
-      p2 = onOutOfMemoryTyped<T>(AllocFunction::Realloc, newSize, prior);
-    }
-    return p2;
+    return pod_arena_realloc<T>(js::MallocArena, prior, oldSize, newSize);
   }
 
   template <typename T>

js/src/util/AllocPolicy.cpp
@@ -10,9 +10,10 @@
 
 using namespace js;
 
-void* TempAllocPolicy::onOutOfMemory(AllocFunction allocFunc, size_t nbytes,
+void* TempAllocPolicy::onOutOfMemory(arena_id_t arenaId,
+                                     AllocFunction allocFunc, size_t nbytes,
                                      void* reallocPtr) {
-  return cx_->onOutOfMemory(allocFunc, this->getArenaId(), nbytes, reallocPtr);
+  return cx_->onOutOfMemory(allocFunc, arenaId, nbytes, reallocPtr);
 }
 
 void TempAllocPolicy::reportAllocOverflow() const {

js/src/util/StringBuffer.cpp
@@ -18,7 +18,7 @@ template <typename CharT, class Buffer>
 static CharT* ExtractWellSized(Buffer& cb) {
   size_t capacity = cb.capacity();
   size_t length = cb.length();
-  TempAllocPolicy allocPolicy = cb.allocPolicy();
+  StringBufferAllocPolicy allocPolicy = cb.allocPolicy();
 
   CharT* buf = cb.extractOrCopyRawBuffer();
   if (!buf) {
@@ -50,7 +50,7 @@ char16_t* StringBuffer::stealChars() {
 bool StringBuffer::inflateChars() {
   MOZ_ASSERT(isLatin1());
 
-  TwoByteCharBuffer twoByte(TempAllocPolicy{cx_, arenaId_});
+  TwoByteCharBuffer twoByte(StringBufferAllocPolicy{cx_, arenaId_});
 
   /*
    * Note: we don't use Vector::capacity() because it always returns a

js/src/util/StringBuffer.h
@@ -16,6 +16,47 @@
 
 namespace js {
 
+class StringBufferAllocPolicy {
+  TempAllocPolicy impl_;
+
+  const arena_id_t& arenaId_;
+
+ public:
+  StringBufferAllocPolicy(JSContext* cx, const arena_id_t& arenaId)
+      : impl_(cx), arenaId_(arenaId) {}
+
+  template <typename T>
+  T* maybe_pod_malloc(size_t numElems) {
+    return impl_.maybe_pod_arena_malloc<T>(arenaId_, numElems);
+  }
+  template <typename T>
+  T* maybe_pod_calloc(size_t numElems) {
+    return impl_.maybe_pod_arena_calloc<T>(arenaId_, numElems);
+  }
+  template <typename T>
+  T* maybe_pod_realloc(T* p, size_t oldSize, size_t newSize) {
+    return impl_.maybe_pod_arena_realloc<T>(arenaId_, p, oldSize, newSize);
+  }
+  template <typename T>
+  T* pod_malloc(size_t numElems) {
+    return impl_.pod_arena_malloc<T>(arenaId_, numElems);
+  }
+  template <typename T>
+  T* pod_calloc(size_t numElems) {
+    return impl_.pod_arena_calloc<T>(arenaId_, numElems);
+  }
+  template <typename T>
+  T* pod_realloc(T* p, size_t oldSize, size_t newSize) {
+    return impl_.pod_arena_realloc<T>(arenaId_, p, oldSize, newSize);
+  }
+  template <typename T>
+  void free_(T* p, size_t numElems = 0) {
+    impl_.free_(p, numElems);
+  }
+  void reportAllocOverflow() const { impl_.reportAllocOverflow(); }
+  bool checkSimulatedOOM() const { return impl_.checkSimulatedOOM(); }
+};
+
 /*
  * String builder that eagerly checks for over-allocation past the maximum
  * string length.
@@ -30,7 +71,7 @@ namespace js {
 class StringBuffer {
  protected:
   template <typename CharT>
-  using BufferType = Vector<CharT, 64 / sizeof(CharT)>;
+  using BufferType = Vector<CharT, 64 / sizeof(CharT), StringBufferAllocPolicy>;
 
   /*
    * The Vector's buffer may be either stolen or copied, so we need to use
@@ -101,7 +142,7 @@ class StringBuffer {
   explicit StringBuffer(JSContext* cx,
                         const arena_id_t& arenaId = js::MallocArena)
       : cx_(cx), arenaId_(arenaId), reserved_(0) {
-    cb.construct<Latin1CharBuffer>(TempAllocPolicy{cx_, arenaId_});
+    cb.construct<Latin1CharBuffer>(StringBufferAllocPolicy{cx_, arenaId_});
   }
 
   void clear() {