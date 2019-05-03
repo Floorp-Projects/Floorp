7eb52975f36e0e6b59a14688b0609422bff2fb31
Michael Froman <mfroman@mozilla.com>
2019-05-03 00:36:19 +0000

    Bug 1548113 - pass mEOS from MediaRawData to remote decoder. r=jya
    Lack of mEOS for decoding Vorbis on RDD was causing a mochitest failure
    with file spacestorm-1000Hz-100ms.ogg in dom/media/test/test_playback.html.
    The symptom was an incorrect frame count here[1].
    
    [1] https://searchfox.org/mozilla-central/rev/b59a99943de4dd314bae4e44ab43ce7687ccbbec/dom/media/platforms/agnostic/VorbisDecoder.cpp#178
    
    Differential Revision: https://phabricator.services.mozilla.com/D29756

--- dom/media/ipc/RemoteDecoderChild.cpp
+++ dom/media/ipc/RemoteDecoderChild.cpp
@@ -114,6 +114,7 @@ RefPtr<MediaDataDecoder::DecodePromise> RemoteDecoderChild::Decode(
   MediaRawDataIPDL sample(
       MediaDataIPDL(aSample->mOffset, aSample->mTime, aSample->mTimecode,
                     aSample->mDuration, aSample->mKeyframe),
+      aSample->mEOS,
       std::move(buffer));
   SendInput(sample);
 
