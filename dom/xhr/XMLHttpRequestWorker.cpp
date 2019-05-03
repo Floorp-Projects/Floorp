53fa91d6ae2183c18b33e5ed61d5c383a6aab9c7
Daniel Varga <dvarga@mozilla.com>
2019-05-03 11:56:25 +0300

    Backed out 7 changesets (bug 1392408) for mochitest failures at test_xhr_withCredentials.html and xpcshell failures at test_startup_(no)session_async.js
    Backed out changeset 05656abf3e6c (bug 1392408)
    Backed out changeset ac9c08f90cd0 (bug 1392408)
    Backed out changeset 855b8dd227f9 (bug 1392408)
    Backed out changeset 9c3b9f800a0c (bug 1392408)
    Backed out changeset 2a64ecfab240 (bug 1392408)
    Backed out changeset b765eaf69bcb (bug 1392408)
    Backed out changeset 337d93c03c2e (bug 1392408)

--- dom/xhr/XMLHttpRequestWorker.cpp
+++ dom/xhr/XMLHttpRequestWorker.cpp
@@ -668,10 +668,6 @@ class OpenRunnable final : public WorkerThreadProxySyncRunnable {
   XMLHttpRequestResponseType mResponseType;
   const nsString mMimeTypeOverride;
 
-  // Remember the worker thread's stack when the XHR was opened, so that it can
-  // be passed on to the net monitor.
-  UniquePtr<SerializedStackHolder> mOriginStack;
-
  public:
   OpenRunnable(WorkerPrivate* aWorkerPrivate, Proxy* aProxy,
                const nsACString& aMethod, const nsAString& aURL,
@@ -679,8 +675,7 @@ class OpenRunnable final : public WorkerThreadProxySyncRunnable {
                const Optional<nsAString>& aPassword, bool aBackgroundRequest,
                bool aWithCredentials, uint32_t aTimeout,
                XMLHttpRequestResponseType aResponseType,
-               const nsString& aMimeTypeOverride,
-               UniquePtr<SerializedStackHolder> aOriginStack)
+               const nsString& aMimeTypeOverride)
       : WorkerThreadProxySyncRunnable(aWorkerPrivate, aProxy),
         mMethod(aMethod),
         mURL(aURL),
@@ -688,8 +683,7 @@ class OpenRunnable final : public WorkerThreadProxySyncRunnable {
         mWithCredentials(aWithCredentials),
         mTimeout(aTimeout),
         mResponseType(aResponseType),
-        mMimeTypeOverride(aMimeTypeOverride),
-        mOriginStack(std::move(aOriginStack)) {
+        mMimeTypeOverride(aMimeTypeOverride) {
     if (aUser.WasPassed()) {
       mUserStr = aUser.Value();
       mUser = &mUserStr;
@@ -1282,10 +1276,6 @@ nsresult OpenRunnable::MainThreadRunInternal() {
     NS_ENSURE_SUCCESS(rv, rv);
   }
 
-  if (mOriginStack) {
-    mProxy->mXHR->SetOriginStack(std::move(mOriginStack));
-  }
-
   ErrorResult rv;
 
   if (mWithCredentials) {
@@ -1787,17 +1777,10 @@ void XMLHttpRequestWorker::Open(const nsACString& aMethod,
 
   mProxy->mOuterEventStreamId++;
 
-  UniquePtr<SerializedStackHolder> stack;
-  if (JSContext* cx = nsContentUtils::GetCurrentJSContext()) {
-    stack = GetCurrentStackForNetMonitor(cx);
-  }
-
   RefPtr<OpenRunnable> runnable = new OpenRunnable(
       mWorkerPrivate, mProxy, aMethod, aUrl, aUser, aPassword,
       mBackgroundRequest, mWithCredentials, mTimeout, mResponseType,
-      alsoOverrideMimeType ? mMimeTypeOverride : VoidString(),
-      std::move(stack));
-
+      alsoOverrideMimeType ? mMimeTypeOverride : VoidString());
   ++mProxy->mOpenCount;
   runnable->Dispatch(Canceling, aRv);
   if (aRv.Failed()) {
