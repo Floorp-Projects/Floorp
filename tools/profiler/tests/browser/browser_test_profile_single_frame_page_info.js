414cbe1e2bc6dec40526bba19bdb5ddee8902a7b
Nazım Can Altınova <canaltinova@gmail.com>
2019-05-03 12:43:55 +0000

    Bug 1548694 - Split the profiler page information tests to prevent intermittent timeout. r=julienw, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D29812
    
    --HG--
    rename : tools/profiler/tests/browser/browser_test_profile_history_page_info.js => tools/profiler/tests/browser/browser_test_profile_pushstate_page_info.js
    rename : tools/profiler/tests/browser/browser_test_profile_page_info.js => tools/profiler/tests/browser/browser_test_profile_single_frame_page_info.js
    extra : source : 89664bfdb628a012b717ecee5427495f3fea528c

--- tools/profiler/tests/browser/browser_test_profile_single_frame_page_info.js
+++ tools/profiler/tests/browser/browser_test_profile_single_frame_page_info.js
@@ -2,42 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-add_task(async function test_profile_single_frame_page_info() {
-  if (!AppConstants.MOZ_GECKO_PROFILER) {
-    return;
-  }
-  Assert.ok(!Services.profiler.IsActive());
-  // Clear all pages in case we have some pages registered before.
-  await Services.profiler.ClearAllPages();
-  startProfiler();
-
-  const url = BASE_URL + "single_frame.html";
-  let contentPid;
-  await BrowserTestUtils.withNewTab(url, async function(contentBrowser) {
-    contentPid = await ContentTask.spawn(contentBrowser, null, () => {
-      return Services.appinfo.processID;
-    });
-  });
-
-  const profile = await Services.profiler.getProfileDataAsync();
-  Services.profiler.StopProfiler();
-
-  let pageFound = false;
-  // We need to find the correct content process for that tab.
-  let contentProcess = profile.processes.find(p => p.threads[0].pid == contentPid);
-  for (const page of contentProcess.pages) {
-    if (page.url == url) {
-      Assert.equal(page.url, url);
-      Assert.equal(typeof page.docshellId, "string");
-      Assert.equal(typeof page.historyId, "number");
-      Assert.equal(page.isSubFrame, false);
-      pageFound = true;
-      break;
-    }
-  }
-  Assert.equal(pageFound, true);
-});
-
 add_task(async function test_profile_multi_frame_page_info() {
   if (!AppConstants.MOZ_GECKO_PROFILER) {
     return;
