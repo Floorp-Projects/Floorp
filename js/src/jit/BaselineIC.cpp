4b8f561ab6d6da9bdc42bcfe679683a120cc4e69
Iain Ireland <iireland@mozilla.com>
2019-05-06 20:02:01 +0000

    Bug 1533890: Turn on CacheIR call ICs by default r=mgaudet
    This patch enables CacheIR call ICs by default, and turns off the old call ICs when CacheIR ICs are turned on.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29537
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -3730,7 +3730,7 @@ bool DoCallFallback(JSContext* cx, BaselineFrame* frame, ICCall_Fallback* stub,
 
     // Try attaching a regular call stub, but only if the CacheIR attempt didn't
     // add any stubs.
-    if (!handled && !deferred) {
+    if (!handled && !deferred && JitOptions.disableCacheIRCalls) {
       bool createSingleton =
           ObjectGroup::useSingletonForNewObject(cx, script, pc);
       if (!TryAttachCallStub(cx, stub, script, pc, op, argc, vp, constructing,
@@ -3879,7 +3879,7 @@ bool DoSpreadCallFallback(JSContext* cx, BaselineFrame* frame,
 
     // Try attaching a regular call stub, but only if the CacheIR attempt didn't
     // add any stubs.
-    if (!handled) {
+    if (!handled && JitOptions.disableCacheIRCalls) {
       if (!TryAttachCallStub(cx, stub, script, pc, op, 1, vp, constructing,
                              true, false, &handled)) {
         return false;
