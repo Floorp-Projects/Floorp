31429a113a649bec927f0c92ad8107dd7c35186e
Michael Ratcliffe <mratcliffe@mozilla.com>
2019-05-07 14:59:05 +0000

    Bug 1306054 - [inactive CSS] Fix current tests and add new test r=rcaliman
    ### Try
    
    https://treeherder.mozilla.org/#/jobs?repo=try&revision=0bad22682ebfa917a91e9b95dab5345093f68d79
    
    Differential Revision: https://phabricator.services.mozilla.com/D29025
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/test/head.js
+++ devtools/client/inspector/test/head.js
@@ -775,20 +775,20 @@ async function assertTooltipShownOnHover(tooltip, target) {
  * @return a promise that resolves with the tooltip object
  */
 async function assertShowPreviewTooltip(view, target) {
+  const name = "previewTooltip";
+
+  // Get the tooltip. If it does not exist one will be created.
+  const tooltip = view.tooltips.getTooltip(name);
+  ok(tooltip, `Tooltip '${name}' has been instantiated`);
+
+  const shown = tooltip.once("shown");
   const mouseEvent = new target.ownerDocument.defaultView.MouseEvent("mousemove", {
     bubbles: true,
   });
   target.dispatchEvent(mouseEvent);
 
-  const name = "previewTooltip";
-  ok(view.tooltips._instances.has(name),
-    `Tooltip '${name}' has been instantiated`);
-  const tooltip = view.tooltips.getTooltip(name);
-
-  if (!tooltip.isVisible()) {
-    info("Waiting for tooltip to be shown");
-    await tooltip.once("shown");
-  }
+  info("Waiting for tooltip to be shown");
+  await shown;
 
   ok(tooltip.isVisible(), `The tooltip '${name}' is visible`);
 
