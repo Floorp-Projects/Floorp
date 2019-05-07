561ce5e84d9b254a1d68872051407cd5a42d64f3
Martin Stransky <stransky@redhat.com>
2019-05-07 11:28:04 +0000

    Bug 1539471 - [wayland] Track active popup windows on Wayland and use dynamic popup hierarchy, r=ashie
    In order to show all popups on Wayland we need to set popup parent runtime for popups which don't have
    fixed parent. For instance popup menus (fired after right button mouse click) can be issued on top of another popup
    and we need to follow that connection on Wayland.
    
    We track all open (active) popups to:
    
    - close all visible tooltip windows when we're going to open another tooltip
    - close concurrent popup on the same level when a new one is about to open
    - get latest active popup as a parent for a new tooltip windows
    - get latest active popup as a parent for a new popup menu without fixed parent
    
    Differential Revision: https://phabricator.services.mozilla.com/D29348

--- widget/gtk/nsWindow.h
+++ widget/gtk/nsWindow.h
@@ -272,6 +272,7 @@ class nsWindow final : public nsBaseWidget {
   GtkWidget* GetMozContainerWidget();
   GdkWindow* GetGdkWindow() { return mGdkWindow; }
   GtkWidget* GetGtkWidget() { return mShell; }
+  nsIFrame* GetFrame();
   bool IsDestroyed() { return mIsDestroyed; }
 
   void DispatchDragEvent(mozilla::EventMessage aMsg,
@@ -609,10 +610,10 @@ class nsWindow final : public nsBaseWidget {
 
   void ForceTitlebarRedraw();
 
-  // This is used by Wayland backend to keep strict popup window hierarchy.
-  GtkWindow* GetPopupParentWindow();
-
   bool IsWaylandPopup();
+  GtkWidget* ConfigureWaylandPopupWindows();
+  void CloseWaylandTooltips();
+  void CloseWaylandWindow();
 
   /**
    * |mIMContext| takes all IME related stuff.
