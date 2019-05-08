6fd7873bb723ad6e15718c723ca5b396596db7fb
Shane Caraveo <scaraveo@mozilla.com>
2019-05-08 01:30:25 +0000

    Bug 1541317 fix extension failures when setting homepage on upgrade r=aswan a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D29912

--- browser/components/extensions/test/xpcshell/test_ext_chrome_settings_overrides_update.js
+++ browser/components/extensions/test/xpcshell/test_ext_chrome_settings_overrides_update.js
@@ -5,21 +5,21 @@
 const {AddonTestUtils} = ChromeUtils.import("resource://testing-common/AddonTestUtils.jsm");
 const {HomePage} = ChromeUtils.import("resource:///modules/HomePage.jsm");
 
-const {
-  createAppInfo,
-  promiseShutdownManager,
-  promiseStartupManager,
-} = AddonTestUtils;
-
 AddonTestUtils.init(this);
 AddonTestUtils.overrideCertDB();
 
-createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
+AddonTestUtils.createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
+
+add_task(async function setup() {
+  await AddonTestUtils.promiseStartupManager();
+});
 
 add_task(async function test_overrides_update_removal() {
   /* This tests the scenario where the manifest key for homepage and/or
    * search_provider are removed between updates and therefore the
-   * settings are expected to revert. */
+   * settings are expected to revert.  It also tests that an extension
+   * can make a builtin extension the default extension without user
+   * interaction.  */
 
   const EXTENSION_ID = "test_overrides_update@tests.mozilla.org";
   const HOMEPAGE_URI = "webext-homepage-1.html";
@@ -37,7 +37,6 @@ add_task(async function test_overrides_update_removal() {
     });
   }
 
-  await promiseStartupManager();
 
   let extensionInfo = {
     useAddonManager: "permanent",
@@ -62,6 +61,8 @@ add_task(async function test_overrides_update_removal() {
 
   let defaultHomepageURL = HomePage.get();
   let defaultEngineName = (await Services.search.getDefault()).name;
+  ok(defaultEngineName !== "DuckDuckGo",
+     "Default engine is not DuckDuckGo.");
 
   let prefPromise = promisePrefChanged(HOMEPAGE_URI);
   await extension.startup();
@@ -73,7 +74,7 @@ add_task(async function test_overrides_update_removal() {
      "Home page url is overridden by the extension.");
   equal((await Services.search.getDefault()).name,
         "DuckDuckGo",
-        "Default engine is overridden by the extension");
+        "Builtin default engine was set default by extension");
 
   extensionInfo.manifest = {
     "version": "2.0",
@@ -97,6 +98,75 @@ add_task(async function test_overrides_update_removal() {
         "Default engine reverted to the default after update.");
 
   await extension.unload();
+});
 
-  await promiseShutdownManager();
+add_task(async function test_overrides_update_adding() {
+  /* This tests the scenario where an addon adds support for
+   * a homepage or search service when upgrading. Neither
+   * should override existing entries for those when added
+   * in an upgrade. Also, a search_provider being added
+   * with is_default should not prompt the user or override
+   * the current default engine. */
+
+  const EXTENSION_ID = "test_overrides_update@tests.mozilla.org";
+  const HOMEPAGE_URI = "webext-homepage-1.html";
+
+  let extensionInfo = {
+    useAddonManager: "permanent",
+    manifest: {
+      "version": "1.0",
+      "applications": {
+        "gecko": {
+          "id": EXTENSION_ID,
+        },
+      },
+    },
+  };
+  let extension = ExtensionTestUtils.loadExtension(extensionInfo);
+
+  let defaultHomepageURL = HomePage.get();
+  let defaultEngineName = (await Services.search.getDefault()).name;
+  ok(defaultEngineName !== "DuckDuckGo",
+     "Home page url is not DuckDuckGo.");
+
+  await extension.startup();
+
+  equal(extension.version, "1.0", "The installed addon has the expected version.");
+  equal(HomePage.get(),
+        defaultHomepageURL,
+        "Home page url is the default after startup.");
+  equal((await Services.search.getDefault()).name,
+        defaultEngineName,
+        "Default engine is the default after startup.");
+
+  extensionInfo.manifest = {
+    "version": "2.0",
+    "applications": {
+      "gecko": {
+        "id": EXTENSION_ID,
+      },
+    },
+    "chrome_settings_overrides": {
+      "homepage": HOMEPAGE_URI,
+      "search_provider": {
+        "name": "DuckDuckGo",
+        "search_url": "https://example.com/?q={searchTerms}",
+        "is_default": true,
+      },
+    },
+  };
+
+  await extension.upgrade(extensionInfo);
+  await AddonTestUtils.waitForSearchProviderStartup(extension);
+
+  equal(extension.version, "2.0", "The updated addon has the expected version.");
+  equal(HomePage.get(), defaultHomepageURL,
+        "Home page url is not overridden by the extension during upgrade.");
+  // An upgraded extension adding a search engine cannot override
+  // the default engine.
+  equal((await Services.search.getDefault()).name,
+        defaultEngineName,
+        "Default engine is still the default after startup.");
+
+  await extension.unload();
 });
