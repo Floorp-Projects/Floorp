3b22a5306086097bc708c0b61cdf780f28cb8857
Boris Zbarsky <bzbarsky@mit.edu>
2019-05-09 06:55:05 +0000

    Bug 1550364 part 2.  Use dom::UDPSocketChild, not nsIUDPSocketChild, in C++ code.  r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D30445

--- media/mtransport/nr_socket_prsock.h
+++ media/mtransport/nr_socket_prsock.h
@@ -94,6 +94,10 @@ namespace net {
 union NetAddr;
 }
 
+namespace dom {
+class UDPSocketChild;
+}  // namespace dom
+
 class NrSocketBase {
  public:
   NrSocketBase() : connect_invoked_(false), poll_flags_(0) {
@@ -277,7 +281,7 @@ class NrUdpSocketIpc : public NrSocketIpc {
   void sendto_i(const net::NetAddr& addr, nsAutoPtr<MediaPacket> buf);
   void close_i();
 #if defined(MOZILLA_INTERNAL_API) && !defined(MOZILLA_XPCOMRT_API)
-  static void destroy_i(nsIUDPSocketChild* aChild,
+  static void destroy_i(dom::UDPSocketChild* aChild,
                         nsCOMPtr<nsIEventTarget>& aStsThread);
 #endif
   // STS thread executor
@@ -289,7 +293,8 @@ class NrUdpSocketIpc : public NrSocketIpc {
 
   std::queue<RefPtr<nr_udp_message>> received_msgs_;
 
-  RefPtr<nsIUDPSocketChild> socket_child_;  // only accessed from the io_thread
+  // only accessed from the io_thread
+  RefPtr<dom::UDPSocketChild> socket_child_;
 };
 
 // The socket child holds onto one of these, which just passes callbacks
