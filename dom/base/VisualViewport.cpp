c348e46c8509216696041e760fbc8f1ebbc2364d
Botond Ballo <botond@mozilla.com>
2019-05-09 03:56:41 +0000

    Bug 1477610 - Flush layout when reporting the visual viewport size via the Visual Viewport API. r=hiro
    Differential Revision: https://phabricator.services.mozilla.com/D29089

--- dom/base/VisualViewport.cpp
+++ dom/base/VisualViewport.cpp
@@ -13,6 +13,7 @@
 #include "nsIDocShell.h"
 #include "nsPresContext.h"
 #include "nsRefreshDriver.h"
+#include "DocumentInlines.h"
 
 #define VVP_LOG(...)
 // #define VVP_LOG(...) printf_stderr("VVP: " __VA_ARGS__)
@@ -60,6 +61,14 @@ void VisualViewport::GetEventTargetParent(EventChainPreVisitor& aVisitor) {
 CSSSize VisualViewport::VisualViewportSize() const {
   CSSSize size = CSSSize(0, 0);
 
+  // Flush layout, as that may affect the answer below (e.g. scrollbars
+  // may have appeared, decreasing the available viewport size).
+  RefPtr<const VisualViewport> kungFuDeathGrip(this);
+  if (Document* doc = GetDocument()) {
+    doc->FlushPendingNotifications(FlushType::Layout);
+  }
+
+  // Fetch the pres shell after the layout flush, as it might have destroyed it.
   if (PresShell* presShell = GetPresShell()) {
     if (presShell->IsVisualViewportSizeSet()) {
       size = CSSRect::FromAppUnits(presShell->GetVisualViewportSize());
@@ -121,7 +130,7 @@ double VisualViewport::OffsetTop() const {
   return PageTop() - LayoutViewportOffset().Y();
 }
 
-PresShell* VisualViewport::GetPresShell() const {
+Document* VisualViewport::GetDocument() const {
   nsCOMPtr<nsPIDOMWindowInner> window = GetOwner();
   if (!window) {
     return nullptr;
@@ -132,16 +141,17 @@ PresShell* VisualViewport::GetPresShell() const {
     return nullptr;
   }
 
-  return docShell->GetPresShell();
+  return docShell->GetDocument();
+}
+
+PresShell* VisualViewport::GetPresShell() const {
+  RefPtr<Document> document = GetDocument();
+  return document ? document->GetPresShell() : nullptr;
 }
 
 nsPresContext* VisualViewport::GetPresContext() const {
-  PresShell* presShell = GetPresShell();
-  if (!presShell) {
-    return nullptr;
-  }
-
-  return presShell->GetPresContext();
+  RefPtr<Document> document = GetDocument();
+  return document ? document->GetPresContext() : nullptr;
 }
 
 /* ================= Resize event handling ================= */
