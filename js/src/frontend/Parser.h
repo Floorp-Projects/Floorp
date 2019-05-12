8c3bcd64f242a65339851f2d108210602c341439
Jeff Walden <jwalden@mit.edu>
2019-05-12 01:38:24 +0000

    Bug 1550616 - Make FunctionBox::setEnd take the actual uint32_t end, not TokenStreamAnyChars&, and add ParserBase::setFunctionEndFromCurrentToken that calls it passing the correct value, to eliminate another SharedContext.h dependency on... r=tcampbell
    ...TokenStream.h.  r=tcampbell
    
    Depends on D30574
    
    Differential Revision: https://phabricator.services.mozilla.com/D30575
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -457,6 +457,8 @@ class MOZ_STACK_CLASS ParserBase : public ParserSharedBase,
   JSAtom* prefixAccessorName(PropertyType propType, HandleAtom propAtom);
 
   MOZ_MUST_USE bool setSourceMapInfo();
+
+  void setFunctionEndFromCurrentToken(FunctionBox* funbox) const;
 };
 
 enum FunctionCallBehavior {
@@ -824,6 +826,7 @@ class MOZ_STACK_CLASS GeneralParser : public PerHandlerParser<ParseHandler> {
   using Base::prefixAccessorName;
   using Base::processExport;
   using Base::processExportFrom;
+  using Base::setFunctionEndFromCurrentToken;
 
  private:
   inline FinalParser* asFinalParser();
