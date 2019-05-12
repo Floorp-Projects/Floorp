091723db9f1f0de7ccf4f74193d77bb73a274f20
Jeff Walden <jwalden@mit.edu>
2019-05-12 01:38:24 +0000

    Bug 1550616 - Make FunctionBox::setEnd take the actual uint32_t end, not TokenStreamAnyChars&, and add ParserBase::setFunctionEndFromCurrentToken that calls it passing the correct value, to eliminate another SharedContext.h dependency on... r=tcampbell
    ...TokenStream.h.  r=tcampbell
    
    Depends on D30574
    
    Differential Revision: https://phabricator.services.mozilla.com/D30575

--- js/src/frontend/SharedContext.h
+++ js/src/frontend/SharedContext.h
@@ -555,13 +555,11 @@ class FunctionBox : public ObjectBox, public SharedContext {
     startColumn = column;
   }
 
-  void setEnd(const TokenStreamAnyChars& anyChars) {
+  void setEnd(uint32_t end) {
     // For all functions except class constructors, the buffer and
     // toString ending positions are the same. Class constructors override
     // the toString ending position with the end of the class definition.
-    uint32_t offset = anyChars.currentToken().pos.end;
-    bufEnd = offset;
-    toStringEnd = offset;
+    bufEnd = toStringEnd = end;
   }
 
   void trace(JSTracer* trc) override;
