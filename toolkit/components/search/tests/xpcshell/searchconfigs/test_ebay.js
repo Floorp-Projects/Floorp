efa985f8f7f2ced08c3c9ecdb85f0a8fc2eaee25
Mark Banner <standard8@mozilla.com>
2019-05-13 12:22:21 +0000

    Bug 1541419 - Adds test to ensure that search engines are using the correct domain in all regions/locales. r=daleharvey
    Depends on D30398
    
    Differential Revision: https://phabricator.services.mozilla.com/D30399

--- toolkit/components/search/tests/xpcshell/searchconfigs/test_ebay.js
+++ toolkit/components/search/tests/xpcshell/searchconfigs/test_ebay.js
@@ -19,6 +19,103 @@ const test = new SearchConfigTest({
       },
     }],
   },
+  searchUrlBase: "https://rover.ebay.com/rover/1/",
+  domains: {
+    // Note: These should be based on region, but we don't currently enforce that.
+    // Note: the order here is important. A region/locale match higher up in the
+    // list will override a region/locale match lower down.
+    "befr.ebay.be": {
+      included: [{
+        regions: ["be"],
+        locales: { matches: ["br", "fr", "fy-NL", "nl", "wo"]},
+      }],
+      searchUrlEnd: "1553-53471-19255-0/1",
+    },
+    "ebay.at": {
+      included: [{
+        regions: ["at"],
+        locales: { matches: ["de", "dsb", "hsb"]},
+      }],
+      searchUrlEnd: "5221-53469-19255-0/1",
+    },
+    "ebay.ca": {
+      included: [{
+        locales: { matches: ["en-CA"] },
+      }, {
+        regions: ["ca"],
+        locales: { matches: ["br", "fr", "wo"]},
+      }],
+      searchUrlEnd: "706-53473-19255-0/1",
+    },
+    "ebay.ch": {
+      included: [{
+        locales: { matches: ["rm"] },
+      }, {
+        regions: ["ch"],
+        locales: { matches: ["br", "de", "dsb", "fr", "hsb", "wo"]},
+      }],
+      searchUrlEnd: "5222-53480-19255-0/1",
+    },
+    "ebay.com": {
+      included: [{
+        locales: { matches: ["en-US"] },
+      }],
+      searchUrlEnd: "711-53200-19255-0/1",
+    },
+    "ebay.com.au": {
+      included: [{
+        regions: ["au"],
+        locales: { matches: ["cy", "en-GB", "gd"]},
+      }],
+      searchUrlEnd: "705-53470-19255-0/1",
+    },
+    "ebay.ie": {
+      included: [{
+        locales: { matches: ["ga-IE", "ie"] },
+      }, {
+        regions: ["ie"],
+        locales: { matches: ["cy", "en-GB", "gd"]},
+      }],
+      searchUrlEnd: "5282-53468-19255-0/1",
+    },
+    "ebay.co.uk": {
+      included: [{
+        locales: { matches: ["cy", "en-GB", "gd"] },
+      }],
+      searchUrlEnd: "710-53481-19255-0/1",
+    },
+    "ebay.de": {
+      included: [{
+        locales: { matches: ["de", "dsb", "hsb"] },
+      }],
+      searchUrlEnd: "707-53477-19255-0/1",
+    },
+    "ebay.es": {
+      included: [{
+        locales: { matches: ["an", "ast", "ca", "es-ES", "eu", "gl"] },
+      }],
+      searchUrlEnd: "1185-53479-19255-0/1",
+    },
+    "ebay.fr": {
+      included: [{
+        locales: { matches: ["br", "fr", "wo"] },
+      }],
+      searchUrlEnd: "709-53476-19255-0/1",
+    },
+    "ebay.it": {
+      included: [{
+        locales: { matches: ["it", "lij"] },
+      }],
+      searchUrlEnd: "724-53478-19255-0/1",
+    },
+    "ebay.nl": {
+      included: [{
+        locales: { matches: ["fy-NL", "nl"] },
+      }],
+      searchUrlEnd: "1346-53482-19255-0/1",
+    },
+  },
+  noSuggestionsURL: true,
 });
 
 add_task(async function setup() {
