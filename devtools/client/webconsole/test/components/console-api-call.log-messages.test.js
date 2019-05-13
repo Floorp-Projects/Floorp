97142f3de4299102d6d780c7b4812e00174d7414
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-05-13 23:47:19 +0000

    Bug 1549388 - Render non-string messageText with GripMessageBody. r=bgrins.
    Sometimes the messageText can be a longString, which was crashing
    the whole console output since we were always assuming messageText
    was a plain string.
    We now render non-string messageText with reps so it handles longStrings.
    A mocha test is added to ensure this works as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30875

--- devtools/client/webconsole/test/components/console-api-call.log-messages.test.js
+++ devtools/client/webconsole/test/components/console-api-call.log-messages.test.js
@@ -8,6 +8,8 @@ const { render } = require("enzyme");
 
 // React
 const { createFactory } = require("devtools/client/shared/vendor/react");
+const { setupStore } = require("devtools/client/webconsole/test/helpers");
+const Provider = createFactory(require("react-redux").Provider);
 
 // Components under test.
 const ConsoleApiCall = createFactory(require("devtools/client/webconsole/components/message-types/ConsoleApiCall"));
@@ -36,6 +38,19 @@ describe("ConsoleAPICall component:", () => {
       // There should not be the location
       expect(wrapper.find(".message-location").text()).toBe("");
     });
+
+    it("renders longString logMessage grips", () => {
+      const message =
+        prepareMessage(logMessageLongStringStubPacket, {getNextId: () => "1"});
+
+      // We need to wrap the ConsoleApiElement in a Provider in order for the
+      // ObjectInspector to work.
+      const wrapper = render(
+        Provider({ store: setupStore() }, ConsoleApiCall({ message, serviceContainer }))
+      );
+
+      expect(wrapper.find(".message-body").text()).toInclude(initialText);
+    });
   });
 });
 
@@ -51,5 +66,22 @@ const logMessageStubPacket = {
   "from": "server1.conn0.consoleActor2",
   "type": "logMessage",
   "message": "foobar test",
+  "timeStamp": 1519052480060,
+};
+
+const multilineFullText = `a\n${Array(20000)
+  .fill("a")
+  .join("")}`;
+const fullTextLength = multilineFullText.length;
+const initialText = multilineFullText.substring(0, 10000);
+const logMessageLongStringStubPacket = {
+  "from": "server1.conn0.consoleActor2",
+  "type": "logMessage",
+  "message": {
+    type: "longString",
+    initial: initialText,
+    length: fullTextLength,
+    actor: "server1.conn1.child1/longString58",
+  },
   "timeStamp": 1519052480060,
 };
