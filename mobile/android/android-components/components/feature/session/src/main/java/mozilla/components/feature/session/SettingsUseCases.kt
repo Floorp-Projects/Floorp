4b7c55c9179d06fdcaff31976ad076d9026f1c71
Tiger Oakes <toakes@mozilla.com>
2019-05-13 16:43:14 -0400

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/2746: Add generic update settings use case

--- mobile/android/android-components/components/feature/session/src/main/java/mozilla/components/feature/session/SettingsUseCases.kt
+++ mobile/android/android-components/components/feature/session/src/main/java/mozilla/components/feature/session/SettingsUseCases.kt
@@ -12,35 +12,72 @@ import mozilla.components.concept.engine.Settings
 /**
  * Contains use cases related to user settings.
  *
- * @param settings the engine's [Settings].
+ * @param engineSettings the engine's [Settings].
  * @param sessionManager the application's [SessionManager].
  */
 class SettingsUseCases(
-        settings: Settings,
-        sessionManager: SessionManager
+    engineSettings: Settings,
+    sessionManager: SessionManager
 ) {
 
-    class UpdateTrackingProtectionUseCase internal constructor(
-            private val settings: Settings,
-            private val sessionManager: SessionManager
+    /**
+     * Use case to update a setting and then change all
+     * active browsing sessions to use the new setting.
+     *
+     * @param engineSettings the engine's [Settings]. The first settings object that is updated.
+     * @param sessionManager the application's [SessionManager]. Used to query the active sessions.
+     */
+    abstract class UpdateSettingUseCase<T> internal constructor(
+        private val engineSettings: Settings,
+        private val sessionManager: SessionManager
     ) {
 
         /**
-         * Updates the tracking protection policy to the given policy value.
-         * All active sessions are automatically updated with the new policy.
+         * Updates the engine setting and all active sessions.
          *
-         * @param policy The new policy describing what Tracking Protection to use.
+         * @param value The new setting value
          */
-        fun invoke(policy: TrackingProtectionPolicy) {
-            settings.trackingProtectionPolicy = policy
-
+        fun invoke(value: T) {
+            update(engineSettings, value)
             with(sessionManager) {
                 sessions.forEach {
-                    getEngineSession(it)?.enableTrackingProtection(policy)
+                    getEngineSession(it)?.let { session -> forEachSession(session, value) }
                 }
             }
         }
+
+        /**
+         * Called to update a [Settings] object using the value from the invoke call.
+         */
+        protected abstract fun update(settings: Settings, value: T)
+
+        /**
+         * Called to update an active session. Defaults to updating the session's [Settings] object.
+         */
+        protected open fun forEachSession(session: EngineSession, value: T) {
+            update(session.settings, value)
+        }
     }
 
-    val updateTrackingProtection: UpdateTrackingProtectionUseCase by lazy { UpdateTrackingProtectionUseCase(settings, sessionManager) }
+    /**
+     * Updates the tracking protection policy to the given policy value when invoked.
+     * All active sessions are automatically updated with the new policy.
+     */
+    class UpdateTrackingProtectionUseCase internal constructor(
+        engineSettings: Settings,
+        sessionManager: SessionManager
+    ) : UpdateSettingUseCase<TrackingProtectionPolicy>(engineSettings, sessionManager) {
+
+        override fun update(settings: Settings, value: TrackingProtectionPolicy) {
+            settings.trackingProtectionPolicy = value
+        }
+
+        override fun forEachSession(session: EngineSession, value: TrackingProtectionPolicy) {
+            session.enableTrackingProtection(value)
+        }
+    }
+
+    val updateTrackingProtection: UpdateTrackingProtectionUseCase by lazy {
+        UpdateTrackingProtectionUseCase(engineSettings, sessionManager)
+    }
 }
--- mobile/android/android-components/components/feature/session/src/test/java/mozilla/components/feature/session/SettingsUseCasesTest.kt
+++ mobile/android/android-components/components/feature/session/src/test/java/mozilla/components/feature/session/SettingsUseCasesTest.kt
@@ -5,24 +5,47 @@ import mozilla.components.browser.session.SessionManager
 import mozilla.components.concept.engine.EngineSession
 import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy
 import mozilla.components.concept.engine.Settings
+import mozilla.components.feature.session.SettingsUseCases.UpdateSettingUseCase
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
 class SettingsUseCasesTest {
     private val settings = mock(Settings::class.java)
     private val sessionManager = mock(SessionManager::class.java)
-    private val engineSessionA = mock(EngineSession::class.java)
     private val sessionA = mock(Session::class.java)
-    private val engineSessionB = mock(EngineSession::class.java)
+    private val engineSessionA = mock(EngineSession::class.java)
+    private val settingsA = mock(Settings::class.java)
     private val sessionB = mock(Session::class.java)
+    private val engineSessionB = mock(EngineSession::class.java)
+    private val settingsB = mock(Settings::class.java)
+    private val sessionC = mock(Session::class.java)
     private val useCases = SettingsUseCases(settings, sessionManager)
 
     @Before
     fun setup() {
-        `when`(sessionManager.sessions).thenReturn(listOf(sessionA, sessionB))
+        `when`(sessionManager.sessions).thenReturn(listOf(sessionA, sessionB, sessionC))
         `when`(sessionManager.getEngineSession(sessionA)).thenReturn(engineSessionA)
         `when`(sessionManager.getEngineSession(sessionB)).thenReturn(engineSessionB)
+        `when`(sessionManager.getEngineSession(sessionC)).thenReturn(null)
+        `when`(engineSessionA.settings).thenReturn(settingsA)
+        `when`(engineSessionB.settings).thenReturn(settingsB)
+    }
+
+    @Test
+    fun `UpdateSettingUseCase will update all sessions`() {
+        val autoplaySetting = object : UpdateSettingUseCase<Boolean>(settings, sessionManager) {
+            override fun update(settings: Settings, value: Boolean) {
+                settings.allowAutoplayMedia = value
+            }
+        }
+
+        autoplaySetting.invoke(true)
+        verify(settings).allowAutoplayMedia = true
+        verify(engineSessionA.settings).allowAutoplayMedia = true
+        verify(engineSessionB.settings).allowAutoplayMedia = true
     }
 
     @Test
