cb54619aa0fad84ddef5af047cbc77d14fc82c3b
Jonathan Watt <jwatt@jwatt.org>
2019-05-13 16:30:20 +0100

    Bug 1553324. Remove nsIPrintSettings.printFrameTypeUsage. r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D32068
    
    --HG--
    extra : rebase_source : 939cce38c90a39bd002f95e71e1de94a62f895e1
    extra : amend_source : 1be783028096f61145727492fa3c3f41e363c205

--- widget/nsPrintSettingsImpl.h
+++ widget/nsPrintSettingsImpl.h
@@ -55,7 +55,6 @@ class nsPrintSettings : public nsIPrintSettings {
   bool mPrintBGColors;  // print background colors
   bool mPrintBGImages;  // print background images
 
-  int16_t mPrintFrameTypeUsage;
   int16_t mPrintFrameType;
   int16_t mHowToEnableFrameUI;
   bool mIsCancelled;
