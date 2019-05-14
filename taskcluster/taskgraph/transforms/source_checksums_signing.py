51012c5ee95316a541da1d38e1b198d36fd5cf65
Chris AtLee <catlee@mozilla.com>
2019-05-14 21:36:08 +0000

    Bug 1520591: switch gpg signing to autograph r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D31135

--- taskcluster/taskgraph/transforms/source_checksums_signing.py
+++ taskcluster/taskgraph/transforms/source_checksums_signing.py
@@ -57,7 +57,7 @@ def make_checksums_signing_description(config, jobs):
             "paths": [
                 "public/target-source.checksums",
             ],
-            "formats": ["gpg"]
+            "formats": ["autograph_gpg"]
         }]
 
         signing_cert_scope = get_signing_cert_scope(config)
@@ -71,7 +71,6 @@ def make_checksums_signing_description(config, jobs):
                        'max-run-time': 3600},
             'scopes': [
                 signing_cert_scope,
-                add_scope_prefix(config, 'signing:format:gpg'),
             ],
             'dependencies': dependencies,
             'attributes': attributes,
