2bcb6b7303ee66f519987505230792224ba42d24
Jared Wein <jwein@mozilla.com>
2019-05-14 20:05:47 +0000

    Bug 1549809 - Stub out LoginFilter custom element. r=MattN,Pike
    Differential Revision: https://phabricator.services.mozilla.com/D30753

---
+++ browser/components/aboutlogins/content/components/login-filter.js
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class LoginFilter extends HTMLElement {
+  connectedCallback() {
+    if (this.children.length) {
+      return;
+    }
+
+    let loginFilterTemplate = document.querySelector("#login-filter-template");
+    this.attachShadow({mode: "open"})
+        .appendChild(loginFilterTemplate.content.cloneNode(true));
+  }
+
+  static get observedAttributes() {
+    return ["placeholder"];
+  }
+
+  /* Fluent doesn't handle localizing into Shadow DOM yet so strings
+     need to get reflected in to their targeted element. */
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (!this.shadowRoot) {
+      return;
+    }
+
+    switch (attr) {
+      case "placeholder":
+        this.shadowRoot.querySelector("input").placeholder = newValue;
+        break;
+    }
+  }
+}
+customElements.define("login-filter", LoginFilter);
