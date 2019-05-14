6d703cdf927ede661d5fce35f4311b974cdf1a8d
Dana Keeler <dkeeler@mozilla.com>
2019-05-14 20:51:10 +0000

    bug 1548040 - batch cert_storage certificate adding/removal r=jcj,myk
    Differential Revision: https://phabricator.services.mozilla.com/D30271
    
    --HG--
    extra : moz-landing-system : lando

--- security/manager/ssl/tests/unit/test_cert_storage_direct.js
+++ security/manager/ssl/tests/unit/test_cert_storage_direct.js
@@ -13,19 +13,18 @@ if (AppConstants.MOZ_NEW_CERT_STORAGE) {
   this.certStorage = Cc["@mozilla.org/security/certstorage;1"].getService(Ci.nsICertStorage);
 }
 
-async function addCertBySubject(cert, subject) {
+async function addCerts(certInfos) {
   let result = await new Promise((resolve) => {
-    certStorage.addCertBySubject(btoa(cert), btoa(subject), Ci.nsICertStorage.TRUST_INHERIT,
-                                 resolve);
+    certStorage.addCerts(certInfos, resolve);
   });
-  Assert.equal(result, Cr.NS_OK, "addCertBySubject should succeed");
+  Assert.equal(result, Cr.NS_OK, "addCerts should succeed");
 }
 
-async function removeCertByHash(hashBase64) {
+async function removeCertsByHashes(hashesBase64) {
   let result = await new Promise((resolve) => {
-    certStorage.removeCertByHash(hashBase64, resolve);
+    certStorage.removeCertsByHashes(hashesBase64, resolve);
   });
-  Assert.equal(result, Cr.NS_OK, "removeCertByHash should succeed");
+  Assert.equal(result, Cr.NS_OK, "removeCertsByHashes should succeed");
 }
 
 function stringToArray(s) {
@@ -48,19 +47,31 @@ function getLongString(uniquePart, length) {
   return String(uniquePart).padStart(length, "0");
 }
 
+class CertInfo {
+  constructor(cert, subject) {
+    this.cert = btoa(cert);
+    this.subject = btoa(subject);
+    this.trust = Ci.nsICertStorage.TRUST_INHERIT;
+  }
+}
+if (AppConstants.MOZ_NEW_CERT_STORAGE) {
+  CertInfo.prototype.QueryInterface = ChromeUtils.generateQI([Ci.nsICertInfo]);
+}
+
 add_task({
     skip_if: () => !AppConstants.MOZ_NEW_CERT_STORAGE,
   }, async function test_common_subject() {
-  await addCertBySubject("some certificate bytes 1", "some common subject");
-  await addCertBySubject("some certificate bytes 2", "some common subject");
-  await addCertBySubject("some certificate bytes 3", "some common subject");
+  let someCert1 = new CertInfo("some certificate bytes 1", "some common subject");
+  let someCert2 = new CertInfo("some certificate bytes 2", "some common subject");
+  let someCert3 = new CertInfo("some certificate bytes 3", "some common subject");
+  await addCerts([someCert1, someCert2, someCert3]);
   let storedCerts = certStorage.findCertsBySubject(stringToArray("some common subject"));
   let storedCertsAsStrings = storedCerts.map(arrayToString);
   let expectedCerts = ["some certificate bytes 1", "some certificate bytes 2",
                        "some certificate bytes 3"];
   Assert.deepEqual(storedCertsAsStrings.sort(), expectedCerts.sort(), "should find expected certs");
 
-  await addCertBySubject("some other certificate bytes", "some other subject");
+  await addCerts([new CertInfo("some other certificate bytes", "some other subject")]);
   storedCerts = certStorage.findCertsBySubject(stringToArray("some common subject"));
   storedCertsAsStrings = storedCerts.map(arrayToString);
   Assert.deepEqual(storedCertsAsStrings.sort(), expectedCerts.sort(),
@@ -78,9 +89,11 @@ add_task({
   const NUM_CERTS = 500;
   const CERT_LENGTH = 3000;
   const SUBJECT_LENGTH = 40;
+  let certs = [];
   for (let i = 0; i < NUM_CERTS; i++) {
-    await addCertBySubject(getLongString(i, CERT_LENGTH), getLongString(i, SUBJECT_LENGTH));
+    certs.push(new CertInfo(getLongString(i, CERT_LENGTH), getLongString(i, SUBJECT_LENGTH)));
   }
+  await addCerts(certs);
   for (let i = 0; i < NUM_CERTS; i++) {
     let subject = stringToArray(getLongString(i, SUBJECT_LENGTH));
     let storedCerts = certStorage.findCertsBySubject(subject);
@@ -96,11 +109,12 @@ add_task({
   }, async function test_removal() {
   // As long as cert_storage is given valid base64, attempting to delete some nonexistent
   // certificate will "succeed" (it'll do nothing).
-  await removeCertByHash(btoa("thishashisthewrongsize"));
+  await removeCertsByHashes([btoa("thishashisthewrongsize")]);
 
-  await addCertBySubject("removal certificate bytes 1", "common subject to remove");
-  await addCertBySubject("removal certificate bytes 2", "common subject to remove");
-  await addCertBySubject("removal certificate bytes 3", "common subject to remove");
+  let removalCert1 = new CertInfo("removal certificate bytes 1", "common subject to remove");
+  let removalCert2 = new CertInfo("removal certificate bytes 2", "common subject to remove");
+  let removalCert3 = new CertInfo("removal certificate bytes 3", "common subject to remove");
+  await addCerts([removalCert1, removalCert2, removalCert3]);
 
   let storedCerts = certStorage.findCertsBySubject(stringToArray("common subject to remove"));
   let storedCertsAsStrings = storedCerts.map(arrayToString);
@@ -110,7 +124,7 @@ add_task({
                    "should find expected certs before removing them");
 
   // echo -n "removal certificate bytes 2" | sha256sum | xxd -r -p | base64
-  await removeCertByHash("2nUPHwl5TVr1mAD1FU9FivLTlTb0BAdnVUhsYgBccN4=");
+  await removeCertsByHashes(["2nUPHwl5TVr1mAD1FU9FivLTlTb0BAdnVUhsYgBccN4="]);
   storedCerts = certStorage.findCertsBySubject(stringToArray("common subject to remove"));
   storedCertsAsStrings = storedCerts.map(arrayToString);
   expectedCerts = ["removal certificate bytes 1", "removal certificate bytes 3"];
@@ -118,7 +132,7 @@ add_task({
                    "should only have first and third certificates now");
 
   // echo -n "removal certificate bytes 1" | sha256sum | xxd -r -p | base64
-  await removeCertByHash("8zoRqHYrklr7Zx6UWpzrPuL+ol8KL1Ml6XHBQmXiaTY=");
+  await removeCertsByHashes(["8zoRqHYrklr7Zx6UWpzrPuL+ol8KL1Ml6XHBQmXiaTY="]);
   storedCerts = certStorage.findCertsBySubject(stringToArray("common subject to remove"));
   storedCertsAsStrings = storedCerts.map(arrayToString);
   expectedCerts = ["removal certificate bytes 3"];
@@ -126,11 +140,34 @@ add_task({
                    "should only have third certificate now");
 
   // echo -n "removal certificate bytes 3" | sha256sum | xxd -r -p | base64
-  await removeCertByHash("vZn7GwDSabB/AVo0T+N26nUsfSXIIx4NgQtSi7/0p/w=");
+  await removeCertsByHashes(["vZn7GwDSabB/AVo0T+N26nUsfSXIIx4NgQtSi7/0p/w="]);
   storedCerts = certStorage.findCertsBySubject(stringToArray("common subject to remove"));
   Assert.equal(storedCerts.length, 0, "shouldn't have any certificates now");
 
   // echo -n "removal certificate bytes 3" | sha256sum | xxd -r -p | base64
   // Again, removing a nonexistent certificate should "succeed".
-  await removeCertByHash("vZn7GwDSabB/AVo0T+N26nUsfSXIIx4NgQtSi7/0p/w=");
+  await removeCertsByHashes(["vZn7GwDSabB/AVo0T+N26nUsfSXIIx4NgQtSi7/0p/w="]);
+});
+
+add_task({
+    skip_if: () => !AppConstants.MOZ_NEW_CERT_STORAGE,
+}, async function test_batched_removal() {
+  let removalCert1 = new CertInfo("batch removal certificate bytes 1", "batch subject to remove");
+  let removalCert2 = new CertInfo("batch removal certificate bytes 2", "batch subject to remove");
+  let removalCert3 = new CertInfo("batch removal certificate bytes 3", "batch subject to remove");
+  await addCerts([removalCert1, removalCert2, removalCert3]);
+  let storedCerts = certStorage.findCertsBySubject(stringToArray("batch subject to remove"));
+  let storedCertsAsStrings = storedCerts.map(arrayToString);
+  let expectedCerts = ["batch removal certificate bytes 1", "batch removal certificate bytes 2",
+                       "batch removal certificate bytes 3"];
+  Assert.deepEqual(storedCertsAsStrings.sort(), expectedCerts.sort(),
+                   "should find expected certs before removing them");
+  // echo -n "batch removal certificate bytes 1" | sha256sum | xxd -r -p | base64
+  // echo -n "batch removal certificate bytes 2" | sha256sum | xxd -r -p | base64
+  // echo -n "batch removal certificate bytes 3" | sha256sum | xxd -r -p | base64
+  await removeCertsByHashes(["EOEEUTuanHZX9NFVCoMKVT22puIJC6g+ZuNPpJgvaa8=",
+                             "Xz6h/Kvn35cCLJEZXkjPqk1GG36b56sreLyAXpO+0zg=",
+                             "Jr7XdiTT8ZONUL+ogNNMW2oxKxanvYOLQPKBPgH/has="]);
+  storedCerts = certStorage.findCertsBySubject(stringToArray("batch subject to remove"));
+  Assert.equal(storedCerts.length, 0, "shouldn't have any certificates now");
 });
