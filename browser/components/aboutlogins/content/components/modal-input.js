cb264a4e3d815b6ace1c7364fb8182a8b9ccc710
Jared Wein <jwein@mozilla.com>
2019-05-14 20:07:00 +0000

    Bug 1550099 - Create basic custom element for a modal input. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D30957

---
+++ browser/components/aboutlogins/content/components/modal-input.js
@@ -0,0 +1,85 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class ModalInput extends HTMLElement {
+  static get LOCKED_PASSWORD_DISPLAY() {
+    return "••••••••";
+  }
+
+  connectedCallback() {
+    if (this.children.length) {
+      return;
+    }
+
+    let modalInputTemplate = document.querySelector("#modal-input-template");
+    this.attachShadow({mode: "open"})
+        .appendChild(modalInputTemplate.content.cloneNode(true));
+
+    if (this.hasAttribute("value")) {
+      this.value = this.getAttribute("value");
+    }
+
+    if (this.getAttribute("type") == "password") {
+      let unlockedValue = this.shadowRoot.querySelector(".unlocked-value");
+      unlockedValue.setAttribute("type", "password");
+    }
+  }
+
+  static get observedAttributes() {
+    return ["editing", "type", "value"];
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (!this.shadowRoot) {
+      return;
+    }
+
+    let lockedValue = this.shadowRoot.querySelector(".locked-value");
+    let unlockedValue = this.shadowRoot.querySelector(".unlocked-value");
+
+    switch (attr) {
+      case "editing": {
+        let isEditing = newValue !== null;
+        if (!isEditing) {
+          this.setAttribute("value", unlockedValue.value);
+        }
+        break;
+      }
+      case "type": {
+        if (newValue == "password") {
+          lockedValue.textContent = this.constructor.LOCKED_PASSWORD_DISPLAY;
+          unlockedValue.setAttribute("type", "password");
+        } else {
+          lockedValue.textContent = this.getAttribute("value");
+          unlockedValue.setAttribute("type", "text");
+        }
+        break;
+      }
+      case "value": {
+        this.value = newValue;
+        break;
+      }
+    }
+  }
+
+  get value() {
+    return this.hasAttribute("editing") ? this.shadowRoot.querySelector(".unlocked-value").value.trim()
+                                        : this.getAttribute("value") || "";
+  }
+
+  set value(val) {
+    if (this.getAttribute("value") != val) {
+      this.setAttribute("value", val);
+      return;
+    }
+    this.shadowRoot.querySelector(".unlocked-value").value = val;
+    let lockedValue = this.shadowRoot.querySelector(".locked-value");
+    if (this.getAttribute("type") == "password" && val && val.length) {
+      lockedValue.textContent = this.constructor.LOCKED_PASSWORD_DISPLAY;
+    } else {
+      lockedValue.textContent = val;
+    }
+  }
+}
+customElements.define("modal-input", ModalInput);
