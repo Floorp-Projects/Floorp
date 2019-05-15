ae34660ded6ae1917467333dbf0e60451f853f4e
Coroiu Cristina <ccoroiu@mozilla.com>
2019-05-15 22:26:56 +0300

    Backed out changeset 03b1762fb073 (bug 1548369) for devtools failures at devtools/client/debugger/test/mochitest/browser_dbg-inspector-integration.js on a CLOSED TREE

--- devtools/client/debugger/src/actions/preview.js
+++ devtools/client/debugger/src/actions/preview.js
@@ -70,7 +70,7 @@ export function updatePreview(
       return;
     }
 
-    dispatch(setPreview(cx, expression, location, tokenPos, cursorPos, target));
+    dispatch(setPreview(cx, expression, location, tokenPos, cursorPos));
   };
 }
 
@@ -79,18 +79,13 @@ export function setPreview(
   expression: string,
   location: AstLocation,
   tokenPos: Position,
-  cursorPos: ClientRect,
-  target: HTMLElement
+  cursorPos: ClientRect
 ) {
   return async ({ dispatch, getState, client, sourceMaps }: ThunkArgs) => {
     await dispatch({
       type: "SET_PREVIEW",
       cx,
       [PROMISE]: (async function() {
-        if (getPreview(getState())) {
-          dispatch(clearPreview(cx));
-        }
-
         const source = getSelectedSource(getState());
         if (!source) {
           return;
@@ -136,13 +131,6 @@ export function setPreview(
         };
         const properties = await client.loadObjectProperties(root);
 
-        // The first time a popup is rendered, the mouse should be hovered
-        // on the token. If it happens to be hovered on whitespace, it should
-        // not render anything
-        if (!target.matches(":hover")) {
-          return;
-        }
-
         return {
           expression,
           result,
@@ -150,8 +138,7 @@ export function setPreview(
           root,
           location,
           tokenPos,
-          cursorPos,
-          target
+          cursorPos
         };
       })()
     });
