8b4cbdf9664560e426dcf6943eec1f07ab7ae154
yulia <ystartsev@mozilla.com>
2019-05-15 08:26:31 +0000

    Bug 1493714 - move toolbox specific listeners to threadClient events to the toolbox; r=jdescottes
    This is the first part of getting rid of framework/attach-thread.js -- here we move the
    toolbox related logic back into the toolbox.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29193
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/framework/attach-thread.js
+++ devtools/client/framework/attach-thread.js
@@ -5,32 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const Services = require("Services");
-const {LocalizationHelper} = require("devtools/shared/l10n");
-const L10N = new LocalizationHelper("devtools/client/locales/toolbox.properties");
 
-function handleThreadState(toolbox, event, packet) {
-  // Suppress interrupted events by default because the thread is
-  // paused/resumed a lot for various actions.
-  if (event === "paused" && packet.why.type === "interrupted") {
-    return;
-  }
-
-  if (event === "paused") {
-    toolbox.highlightTool("jsdebugger");
-
-    if (packet.why.type === "debuggerStatement" ||
-       packet.why.type === "breakpoint" ||
-       packet.why.type === "exception") {
-      toolbox.raise();
-      toolbox.selectTool("jsdebugger", packet.why.type);
-    }
-  } else if (event === "resumed") {
-    toolbox.unhighlightTool("jsdebugger");
-  }
-}
-
-async function attachThread(toolbox) {
-  const target = toolbox.target;
+async function attachThread(target) {
   const threadOptions = {
     autoBlackBox: false,
     ignoreFrameEnvironment: true,
@@ -44,31 +20,7 @@ async function attachThread(toolbox) {
   if (!threadClient.paused) {
     throw new Error("Thread in wrong state when starting up, should be paused.");
   }
-
-  try {
-    await threadClient.resume();
-  } catch (ex) {
-    // Interpret a possible error thrown by ThreadActor.resume
-    if (ex.error === "wrongOrder") {
-      const box = toolbox.getNotificationBox();
-      box.appendNotification(
-        L10N.getStr("toolbox.resumeOrderWarning"),
-        "wrong-resume-order",
-        "",
-        box.PRIORITY_WARNING_HIGH
-      );
-    } else {
-      throw ex;
-    }
-  }
-  threadClient.addListener("paused", handleThreadState.bind(null, toolbox));
-  threadClient.addListener("resumed", handleThreadState.bind(null, toolbox));
   return threadClient;
 }
 
-function detachThread(threadClient) {
-  threadClient.removeListener("paused");
-  threadClient.removeListener("resumed");
-}
-
-module.exports = { attachThread, detachThread };
+module.exports = { attachThread };
