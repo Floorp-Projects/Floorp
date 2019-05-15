5dc30d86009b72d6aefb7bc1889e915c4125b25c
Aaron Klotz <aklotz@mozilla.com>
2019-05-15 19:38:34 +0000

    Bug 1550544: Clean up untrusted module evaluator; r=agashlin
    This patch takes care of two things:
    
    1) It changes the module evaluator such that, if a binary is signed but the
       cert is neither Microsoft's nor ours, the binary is automatically
       disqualified.
    
    2) General cleanup. Use nsIFile::Contains instead of StringBeginsWith for
       checking path containment. Better OO.
    
    Differential Revision: https://phabricator.services.mozilla.com/D30556

--- toolkit/xre/WinDllServices.cpp
+++ toolkit/xre/WinDllServices.cpp
@@ -143,6 +143,7 @@ class UntrustedModulesManager {
    */
   void ProcessQueuedEvents(bool& aHasProcessedStartupModules) {
     MOZ_ASSERT(!NS_IsMainThread());
+    MOZ_ASSERT(!!mEvaluator);
 
     // Hold a reference to DllServices to ensure the object doesn't get deleted
     // during this call.
@@ -165,6 +166,10 @@ class UntrustedModulesManager {
       mQueuedEvents.swap(queuedEvents);
     }
 
+    if (!mEvaluator) {
+      return;
+    }
+
     Vector<ModuleLoadEvent, 0, InfallibleAllocPolicy> processedEvents;
     int errorModules = 0;
 
@@ -178,6 +183,12 @@ class UntrustedModulesManager {
       ModuleLoadEvent eventCopy(
           e, ModuleLoadEvent::CopyOption::CopyWithoutModules);
       for (auto& m : e.mModules) {
+        bool ok = m.PrepForTelemetry();
+        MOZ_ASSERT(ok);
+        if (!ok) {
+          continue;
+        }
+
         Maybe<bool> maybeIsTrusted =
             mEvaluator.IsModuleTrusted(m, eventCopy, dllSvcRef.get());
 
@@ -308,9 +319,7 @@ class UntrustedModulesManager {
 
         // It's never been seen before so it must be a startup module.
         // One module = one event here.
-        ModuleLoadEvent::ModuleInfo mi;
-        mi.mBase = base;
-        mi.mLoadDurationMS = Nothing();
+        ModuleLoadEvent::ModuleInfo mi(base);
         ModuleLoadEvent e;
         e.mIsStartup = true;
         e.mProcessUptimeMS = 0;
@@ -331,8 +340,7 @@ class UntrustedModulesManager {
     for (auto& e : startupEvents) {
       MOZ_ASSERT(e.mModules.length() == 1);
       ModuleLoadEvent::ModuleInfo& mi(e.mModules[0]);
-      widget::WinUtils::GetModuleFullPath((HMODULE)mi.mBase, mi.mLdrName);
-      Unused << NS_NewLocalFile(mi.mLdrName, false, getter_AddRefs(mi.mFile));
+      mi.PopulatePathInfo();
     }
 
     // Lock mQueuedEvents to add the new items.
