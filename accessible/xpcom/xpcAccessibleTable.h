287d51401e88157c72d46c1b224b301d8c02e0fd
Boris Zbarsky <bzbarsky@mit.edu>
2019-05-15 13:44:23 +0000

    Bug 1551684 part 1.  Stop using [array] in nsIAccessibleTable.  r=surkov
    Differential Revision: https://phabricator.services.mozilla.com/D31179

--- accessible/xpcom/xpcAccessibleTable.h
+++ accessible/xpcom/xpcAccessibleTable.h
@@ -55,12 +55,9 @@ class xpcAccessibleTable : public xpcAccessibleHyperText,
   NS_IMETHOD GetSelectedColumnCount(uint32_t* aSelectedColumnCount) final;
   NS_IMETHOD GetSelectedRowCount(uint32_t* aSelectedRowCount) final;
   NS_IMETHOD GetSelectedCells(nsIArray** aSelectedCell) final;
-  NS_IMETHOD GetSelectedCellIndices(uint32_t* aCellsArraySize,
-                                    int32_t** aCellsArray) final;
-  NS_IMETHOD GetSelectedColumnIndices(uint32_t* aColsArraySize,
-                                      int32_t** aColsArray) final;
-  NS_IMETHOD GetSelectedRowIndices(uint32_t* aRowsArraySize,
-                                   int32_t** aRowsArray) final;
+  NS_IMETHOD GetSelectedCellIndices(nsTArray<uint32_t>& aCellsArray) final;
+  NS_IMETHOD GetSelectedColumnIndices(nsTArray<uint32_t>& aColsArray) final;
+  NS_IMETHOD GetSelectedRowIndices(nsTArray<uint32_t>& aRowsArray) final;
   NS_IMETHOD SelectColumn(int32_t aColIdx) final;
   NS_IMETHOD SelectRow(int32_t aRowIdx) final;
   NS_IMETHOD UnselectColumn(int32_t aColIdx) final;
