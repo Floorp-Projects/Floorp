0ed9b8bcaba39f7a6654d30823646809f50dde9a
Carolina <carolina.jimenez.g@gmail.com>
2019-05-16 18:45:48 +0000

    Bug 1541450 - Add a Certs cleaner and defines that object in FLAGS_MAP. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D27193

---
+++ toolkit/components/cleardata/tests/unit/test_certs.js
@@ -0,0 +1,49 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const certService = Cc["@mozilla.org/security/local-cert-service;1"]
+                      .getService(Ci.nsILocalCertService);
+const overrideService = Cc["@mozilla.org/security/certoverride;1"]
+                          .getService(Ci.nsICertOverrideService);
+const certDB = Cc["@mozilla.org/security/x509certdb;1"]
+                 .getService(Ci.nsIX509CertDB);
+
+const CERT_TEST = "MIHhMIGcAgEAMA0GCSqGSIb3DQEBBQUAMAwxCjAIBgNVBAMTAUEwHhcNMTEwMzIzMjMyNTE3WhcNMTEwNDIyMjMyNTE3WjAMMQowCAYDVQQDEwFBMEwwDQYJKoZIhvcNAQEBBQADOwAwOAIxANFm7ZCfYNJViaDWTFuMClX3+9u18VFGiyLfM6xJrxir4QVtQC7VUC/WUGoBUs9COQIDAQABMA0GCSqGSIb3DQEBBQUAAzEAx2+gIwmuYjJO5SyabqIm4lB1MandHH1HQc0y0tUFshBOMESTzQRPSVwPn77a6R9t";
+
+add_task(async function() {
+  Assert.ok(Services.clearData);
+
+  const TEST_URI = Services.io.newURI("http://test.com/");
+  let cert = certDB.constructX509FromBase64(CERT_TEST);
+  let flags = Ci.nsIClearDataService.CLEAR_CERT_EXCEPTIONS;
+
+  ok(cert, "Cert was created");
+
+  Assert.equal(overrideService.isCertUsedForOverrides(cert, true, true), 0,
+               "Cert should not be used for override yet");
+
+  overrideService.rememberValidityOverride(
+    TEST_URI.asciiHost, TEST_URI.port,
+    cert,
+    flags,
+    false
+  );
+
+  Assert.equal(overrideService.isCertUsedForOverrides(cert, true, true), 1,
+               "Cert should be used for override now");
+
+  await new Promise(aResolve => {
+    Services.clearData
+            .deleteDataFromHost(TEST_URI.asciiHostPort, true /* user request */,
+                                flags,
+                                value => {
+      Assert.equal(value, 0);
+      aResolve();
+    });
+  });
+
+  Assert.equal(overrideService.isCertUsedForOverrides(cert, true, true), 0,
+               "Cert should not be used for override now");
+});
