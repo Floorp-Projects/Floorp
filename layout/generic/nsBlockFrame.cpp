20f9c9335695983395912f8cb8770b1ad87ed2c7
Miko Mynttinen <mikokm@gmail.com>
2019-05-20 22:54:18 +0000

    Bug 1552789 - Check NS_FRAME_FORCE_DISPLAY_LIST_DESCEND_INTO flag for every line r=mattwoodrow a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D31865

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1180,9 +1180,7 @@ static bool ClearLineClampEllipsis(nsBlockFrame* aFrame) {
   return true;
 }
 
-void nsBlockFrame::ClearLineClampEllipsis() {
-  ::ClearLineClampEllipsis(this);
-}
+void nsBlockFrame::ClearLineClampEllipsis() { ::ClearLineClampEllipsis(this); }
 
 static bool IsLineClampItem(const ReflowInput& aReflowInput) {
   return aReflowInput.mFlags.mApplyLineClamp ||
@@ -6730,15 +6728,20 @@ void nsBlockFrame::BuildDisplayList(nsDisplayListBuilder* aBuilder,
   Maybe<TextOverflow> textOverflow =
       TextOverflow::WillProcessLines(aBuilder, this);
 
-  const bool descendAlways =
+  const bool hasDescendantPlaceHolders =
       (GetStateBits() & NS_FRAME_FORCE_DISPLAY_LIST_DESCEND_INTO) ||
-      aBuilder->GetIncludeAllOutOfFlows();
-  const bool descendIfVisible = ForceDescendIntoIfVisible();
-  const bool hasDescendantPlaceHolders = descendAlways || descendIfVisible;
+      ForceDescendIntoIfVisible() || aBuilder->GetIncludeAllOutOfFlows();
 
   const auto ShouldDescendIntoLine = [&](const nsRect& aLineArea) -> bool {
+    // TODO(miko): Unfortunately |descendAlways| cannot be cached, because with
+    // some frame trees, building display list for child lines can change it.
+    // See bug 1552789.
+    const bool descendAlways =
+        (GetStateBits() & NS_FRAME_FORCE_DISPLAY_LIST_DESCEND_INTO) ||
+        aBuilder->GetIncludeAllOutOfFlows();
+
     return descendAlways || aLineArea.Intersects(aBuilder->GetDirtyRect()) ||
-           (descendIfVisible &&
+           (ForceDescendIntoIfVisible() &&
             aLineArea.Intersects(aBuilder->GetVisibleRect()));
   };
 
