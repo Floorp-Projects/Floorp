338f5312d60d434db19954acce18f49f8f26852b
Liang-Heng Chen <xeonchen@gmail.com>
2019-05-20 08:48:02 +0000

    Bug 1552536 - add dummy page load origin; r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D31666

--- dom/base/ContentBlockingLog.cpp
+++ dom/base/ContentBlockingLog.cpp
@@ -21,6 +21,9 @@ typedef mozilla::Telemetry::OriginMetricID OriginMetricID;
 namespace mozilla {
 namespace dom {
 
+// sync with TelemetryOriginData.inc
+NS_NAMED_LITERAL_CSTRING(kDummyOriginHash, "PAGELOAD");
+
 // randomly choose 1% users included in the content blocking measurement
 // based on their client id.
 static constexpr double kRatioReportUser = 0.01;
@@ -111,6 +114,12 @@ void ContentBlockingLog::ReportLog() {
     return;
   }
   LOG("ContentBlockingLog::ReportLog [this=%p]", this);
+  const bool testMode =
+      StaticPrefs::telemetry_origin_telemetry_test_mode_enabled();
+  OriginMetricID metricId =
+      testMode ? OriginMetricID::ContentBlocking_Blocked_TestOnly
+               : OriginMetricID::ContentBlocking_Blocked;
+  ReportOriginSingleHash(metricId, kDummyOriginHash);
 
   for (const auto& originEntry : mLog) {
     if (!originEntry.mData) {
@@ -126,12 +135,9 @@ void ContentBlockingLog::ReportLog() {
 
       const bool isBlocked = logEntry.mBlocked;
       Maybe<StorageAccessGrantedReason> reason = logEntry.mReason;
-      const bool testMode =
-          StaticPrefs::telemetry_origin_telemetry_test_mode_enabled();
 
-      OriginMetricID metricId =
-          testMode ? OriginMetricID::ContentBlocking_Blocked_TestOnly
-                   : OriginMetricID::ContentBlocking_Blocked;
+      metricId = testMode ? OriginMetricID::ContentBlocking_Blocked_TestOnly
+                          : OriginMetricID::ContentBlocking_Blocked;
       if (!isBlocked) {
         MOZ_ASSERT(reason.isSome());
         switch (reason.value()) {
