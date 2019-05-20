84551661d4d0e82c96476781a4dfd94560839855
Aaron Klotz <aklotz@mozilla.com>
2019-05-20 19:01:00 +0000

    Bug 1552362: Ensure that the DLL Interceptor's ADRP decoding treats its immediate operand as signed; r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D31550

--- mozglue/misc/interceptor/Arm64.h
+++ mozglue/misc/interceptor/Arm64.h
@@ -10,6 +10,7 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/Result.h"
 #include "mozilla/Types.h"
+#include "mozilla/TypeTraits.h"
 
 namespace mozilla {
 namespace interceptor {
@@ -35,6 +36,41 @@ enum class PCRelCheckError {
 
 MFBT_API Result<LoadInfo, PCRelCheckError> CheckForPCRel(const uintptr_t aPC,
                                                          const uint32_t aInst);
+
+/**
+ * Casts |aValue| to a |ResultT| via sign extension.
+ *
+ * This function should be used when extracting signed immediate values from
+ * an instruction.
+ *
+ * @param aValue The value to be sign extended. This value should already be
+ *               isolated from the remainder of the instruction's bits and
+ *               shifted all the way to the right.
+ * @param aNumValidBits The number of bits in |aValue| that contain the
+ *                      immediate signed value, including the sign bit.
+ */
+template <typename ResultT>
+inline ResultT SignExtend(const uint32_t aValue, const uint8_t aNumValidBits) {
+  static_assert(IsIntegral<ResultT>::value && IsSigned<ResultT>::value,
+                "ResultT must be a signed integral type");
+  MOZ_ASSERT(aNumValidBits < 32U && aNumValidBits > 1);
+
+  using UnsignedResultT =
+      typename Decay<typename MakeUnsigned<ResultT>::Type>::Type;
+
+  const uint8_t kResultWidthBits = sizeof(ResultT) * 8;
+
+  // Shift left unsigned
+  const uint8_t shiftAmt = kResultWidthBits - aNumValidBits;
+  UnsignedResultT shiftedLeft = static_cast<UnsignedResultT>(aValue)
+                                << shiftAmt;
+
+  // Now shift right signed
+  auto result = static_cast<ResultT>(shiftedLeft);
+  result >>= shiftAmt;
+
+  return result;
+}
 
 inline static uint32_t BuildUnconditionalBranchToRegister(const uint32_t aReg) {
   MOZ_ASSERT(aReg < 32);
