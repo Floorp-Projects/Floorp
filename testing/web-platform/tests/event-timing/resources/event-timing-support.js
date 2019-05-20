b16801ff3c6f2c49a19be00c08c1b4ffcfb0e973
Nicolás Peña Moreno <npm@chromium.org>
2019-05-20 15:27:40 +0000

    Bug 1551428 [wpt PR 16690] - Reland "[EventTiming] Round instead of ceil the |duration|", a=testonly
    Automatic update from web-platform-tests
    Reland "[EventTiming] Round instead of ceil the |duration|"
    
    This is a reland of 9ac1e68e80f46289d00e6a1858740b89e34eb01e
    
    Original change's description:
    > [EventTiming] Round instead of ceil the |duration|
    >
    > The purpose of this change is to expose entries whose |duration| is >=
    > 100ms.
    >
    > Bug: 823744
    > Change-Id: Ibb089009ba287769b1365a938cc7564278a86c35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1594818
    > Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    > Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#656797}
    
    Bug: 823744
    Change-Id: Ia7ceffaf7dc84951b74fff507b024a3f895039db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1596849
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#656927}
    
    --
    
    wpt-commits: 98cac08c5f064da91c6be5b056c8b89d1b49a2a7
    wpt-pr: 16690

--- testing/web-platform/tests/event-timing/resources/event-timing-support.js
+++ testing/web-platform/tests/event-timing/resources/event-timing-support.js
@@ -35,7 +35,9 @@ function verifyClickEvent(entry, is_first=false) {
       "The entry's processingStart should be greater than startTime.");
   assert_greater_than_equal(entry.processingEnd, entry.processingStart,
       "The entry's processingEnd must be at least as large as processingStart.");
-  assert_greater_than_equal(entry.duration, entry.processingEnd - entry.startTime,
+  // |duration| is a number rounded to the nearest 8 ms, so add 4 to get a lower bound
+  // on the actual duration.
+  assert_greater_than_equal(entry.duration + 4, entry.processingEnd - entry.startTime,
       "The entry's duration must be at least as large as processingEnd - startTime.");
   if (is_first) {
     let firstInputs = performance.getEntriesByType('firstInput');
