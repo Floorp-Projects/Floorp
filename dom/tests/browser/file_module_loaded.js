e0a647b4044fcd86b36a7f878479ee041cf90e97
Jared Wein <jwein@mozilla.com>
2019-05-21 18:02:21 +0000

    Bug 1552441 - Test to verify that only unlinkable about pages can load script[type=module]. r=smaug,ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D31678

---
+++ dom/tests/browser/file_module_loaded.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import setBodyText from "chrome://mochitests/content/browser/dom/tests/browser/file_module_loaded2.js";
+document.addEventListener("DOMContentLoaded", () => {
+  setBodyText();
+});
