f795631269ca0c0b6b79393adc5141bd65651f7c
Barret Rennie <barret@brennie.ca>
2019-05-21 21:34:54 +0000

    Bug 1510569 - Keep track of whether we are navigating to a new URI in nsDocShell r=mconley,kmag,qdot
    Previously the `WebNavigationChild` would keep track of when triggering its
    `nsIWebNavigation`, `goForward`, `goBack`, `gotoIndex`, and `loadURI` methods.
    It's `nsIWebNavigation` instance is always an `nsIDocShell` and as part of
    porting `OnStateChange` and `OnLocationChange` events from
    `WebProgressChild`/`RemoteWebProgress` to `BrowserChild`/`BrowserParent`, this
    informations needs to be available from the `BrowserChild`. As it stands, it is
    currently an expando property on the `WebProgressChild`.
    
    Instead of introducing yet another XPCOM interface for the WebProgressChild, we
    now store this information directly on the `nsDocShell`. Furthermore, instead
    of having the `WebNavigationChild` manage this part of the `nsDocShell`'s
    state, we can have the `nsDocShell` manage this state itself so it is always
    consistent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28124
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -5048,8 +5048,8 @@ class TabProgressListener {
 
           this.mBrowser.userTypedValue = null;
 
-          let inLoadURI = this.mBrowser.inLoadURI;
-          if (this.mTab.selected && gURLBar && !inLoadURI) {
+          let isNavigating = this.mBrowser.isNavigating;
+          if (this.mTab.selected && gURLBar && !isNavigating) {
             URLBarSetURI();
           }
         } else if (isSuccessful) {
@@ -5122,7 +5122,7 @@ class TabProgressListener {
       // and the user cleared the URL manually.
       if (this.mBrowser.didStartLoadSinceLastUserTyping() ||
           (isErrorPage && aLocation.spec != "about:blank") ||
-          (isSameDocument && this.mBrowser.inLoadURI) ||
+          (isSameDocument && this.mBrowser.isNavigating) ||
           (isSameDocument && !this.mBrowser.userTypedValue)) {
         this.mBrowser.userTypedValue = null;
       }
