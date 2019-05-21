08b8f771db787e4707cb2954686e3433cf03ae39
Jared Wein <jwein@mozilla.com>
2019-05-21 18:02:21 +0000

    Bug 1552441 - Test to verify that only unlinkable about pages can load script[type=module]. r=smaug,ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D31678
    
    --HG--
    extra : moz-landing-system : lando

---
+++ dom/tests/browser/.eslintrc.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  overrides: [
+    {
+      files: [
+        "file_module_loaded.js",
+        "file_module_loaded2.js",
+      ],
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+  ],
+  "extends": [
+    "plugin:mozilla/browser-test"
+  ]
+};
