33c73bd3eb1d1bd78d40eca26ae50802675cace2
Valentin Gosu <valentin.gosu@gmail.com>
2019-05-22 13:58:55 +0000

    Bug 1529911 - Add more tests for cancelling the channel r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D20500
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/test/unit/test_httpcancel.js
+++ netwerk/test/unit/test_httpcancel.js
@@ -3,6 +3,11 @@
 //
 // I've also shoehorned in a test that ENSURE_CALLED_BEFORE_CONNECT works as
 // expected: see comments that start with ENSURE_CALLED_BEFORE_CONNECT:
+//
+// This test also checks that cancelling a channel before asyncOpen, after
+// onStopRequest, or during onDataAvailable works as expected.
+
+"use strict";
 
 const {HttpServer} = ChromeUtils.import("resource://testing-common/httpd.js");
 
@@ -32,28 +37,24 @@ var observer = {
     // ENSURE_CALLED_BEFORE_CONNECT: setting values should still work
     try {
       subject.QueryInterface(Ci.nsIHttpChannel);
-      currentReferrer = subject.getRequestHeader("Referer");
+      let currentReferrer = subject.getRequestHeader("Referer");
       Assert.equal(currentReferrer, "http://site1.com/");
       var uri = ios.newURI("http://site2.com");
       subject.referrerInfo = new ReferrerInfo(Ci.nsIHttpChannel.REFERRER_POLICY_UNSET, true, uri);
     } catch (ex) {
       do_throw("Exception: " + ex);
     }
-
-    var obs = Cc["@mozilla.org/observer-service;1"].getService();
-    obs = obs.QueryInterface(Ci.nsIObserverService);
-    obs.removeObserver(observer, "http-on-modify-request");
   },
 };
 
-var listener = {
+let cancelDuringOnStartListener = {
   onStartRequest: function test_onStartR(request) {
     Assert.equal(request.status, Cr.NS_BINDING_ABORTED);
 
     // ENSURE_CALLED_BEFORE_CONNECT: setting referrer should now fail
     try {
       request.QueryInterface(Ci.nsIHttpChannel);
-      currentReferrer = request.getRequestHeader("Referer");
+      let currentReferrer = request.getRequestHeader("Referer");
       Assert.equal(currentReferrer, "http://site2.com/");
       var uri = ios.newURI("http://site3.com/");
 
@@ -76,7 +77,32 @@ var listener = {
   },
 
   onStopRequest: function test_onStopR(request, status) {
-    httpserv.stop(do_test_finished);
+    this.resolved();
+  }
+};
+
+var cancelDuringOnDataListener = {
+  data: "",
+  channel: null,
+  receivedSomeData: null,
+  onStartRequest: function test_onStartR(request, ctx) {
+    Assert.equal(request.status, Cr.NS_OK);
+  },
+
+  onDataAvailable: function test_ODA(request, stream, offset, count) {
+    let string = NetUtil.readInputStreamToString(stream, count);
+    Assert.ok(!string.includes("b"));
+    this.data += string;
+    this.channel.cancel(Cr.NS_BINDING_ABORTED);
+    if (receivedSomeData) {
+      receivedSomeData();
+    }
+  },
+
+  onStopRequest: function test_onStopR(request, ctx, status) {
+    Assert.ok(this.data.includes("a"), `data: ${this.data}`);
+    Assert.equal(request.status, Cr.NS_BINDING_ABORTED);
+    this.resolved();
   },
 };
 
@@ -92,32 +118,128 @@ function makeChan(url) {
 
 var httpserv = null;
 
-function execute_test() {
+add_task(async function setup() {
+  httpserv = new HttpServer();
+  httpserv.registerPathHandler("/failtest", failtest);
+  httpserv.registerPathHandler("/cancel_middle", cancel_middle);
+  httpserv.registerPathHandler("/normal_response", normal_response);
+  httpserv.start(-1);
+
+  registerCleanupFunction(async () => {
+    await new Promise(resolve => httpserv.stop(resolve));
+  });
+});
+
+add_task(async function test_cancel_during_onModifyRequest() {
   var chan = makeChan("http://localhost:" +
                       httpserv.identity.primaryPort + "/failtest");
 
   if (!inChildProcess()) {
-    var obs = Cc["@mozilla.org/observer-service;1"].getService();
-    obs = obs.QueryInterface(Ci.nsIObserverService);
-    obs.addObserver(observer, "http-on-modify-request");
+    Services.obs.addObserver(observer, "http-on-modify-request");
+  } else {
+    do_send_remote_message("register-observer");
+    await do_await_remote_message("register-observer-done");
   }
 
-  chan.asyncOpen(listener);
-}
+  await new Promise(resolve => {
+    cancelDuringOnStartListener.resolved = resolve;
+    chan.asyncOpen(cancelDuringOnStartListener);
+  })
 
-function run_test() {
-  httpserv = new HttpServer();
-  httpserv.registerPathHandler("/failtest", failtest);
-  httpserv.start(-1);
+  if (!inChildProcess()) {
+    Services.obs.removeObserver(observer, "http-on-modify-request");
+  } else {
+    do_send_remote_message("unregister-observer");
+    await do_await_remote_message("unregister-observer-done");
+  }
+});
 
-  execute_test();
+add_task(async function test_cancel_before_asyncOpen() {
+  var chan = makeChan("http://localhost:" +
+                      httpserv.identity.primaryPort + "/failtest");
 
-  do_test_pending();
-}
+  chan.cancel(Cr.NS_BINDING_ABORTED);
+
+  Assert.throws(() => { chan.asyncOpen(cancelDuringOnStartListener); },
+                /NS_BINDING_ABORTED/, "cannot open if already cancelled");
+});
+
+add_task(async function test_cancel_during_onData() {
+  var chan = makeChan("http://localhost:" +
+                      httpserv.identity.primaryPort + "/cancel_middle");
+
+  await new Promise(resolve => {
+    cancelDuringOnDataListener.resolved = resolve;
+    cancelDuringOnDataListener.channel = chan;
+    chan.asyncOpen(cancelDuringOnDataListener);
+  });
+});
+
+
+var cancelAfterOnStopListener = {
+  data: "",
+  channel: null,
+  onStartRequest: function test_onStartR(request, ctx) {
+    Assert.equal(request.status, Cr.NS_OK);
+  },
+
+  onDataAvailable: function test_ODA(request, stream, offset, count) {
+    let string = NetUtil.readInputStreamToString(stream, count);
+    this.data += string;
+  },
+
+  onStopRequest: function test_onStopR(request, status) {
+    info("onStopRequest");
+    Assert.equal(request.status, Cr.NS_OK);
+    this.resolved();
+  }
+};
+
+add_task(async function test_cancel_after_onStop() {
+  var chan = makeChan("http://localhost:" +
+                      httpserv.identity.primaryPort + "/normal_response");
+
+  await new Promise(resolve => {
+    cancelAfterOnStopListener.resolved = resolve;
+    cancelAfterOnStopListener.channel = chan;
+    chan.asyncOpen(cancelAfterOnStopListener);
+  });
+  Assert.equal(chan.status, Cr.NS_OK);
+
+  // For now it's unclear if cancelling after onStop should throw,
+  // silently fail, or overwrite the channel's status as we currently do.
+  // See discussion in bug 1553083
+  chan.cancel(Cr.NS_BINDING_ABORTED);
+  Assert.equal(chan.status, Cr.NS_BINDING_ABORTED);
+});
+
 
 // PATHS
 
 // /failtest
 function failtest(metadata, response) {
   do_throw("This should not be reached");
+}
+
+function cancel_middle(metadata, response) {
+  response.processAsync();
+  response.setStatusLine(metadata.httpVersion, 200, "OK");
+  let str1 = "a".repeat(128*1024);
+  response.write(str1, str1.length);
+  response.bodyOutputStream.flush();
+
+  let p = new Promise(resolve => {
+    cancelDuringOnDataListener.receivedSomeData = resolve;
+  });
+  p.then(() => {
+    let str1 = "b".repeat(128*1024);
+    response.write(str2, str2.length);
+    response.finish();
+  });
+}
+
+function normal_response(metadata, response) {
+  response.setStatusLine(metadata.httpVersion, 200, "OK");
+  let str1 = "Is this normal?";
+  response.write(str1, str1.length);
 }
