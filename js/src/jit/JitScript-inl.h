ed52e997392ca3450706a7e8502ed1db1e77d202
Jan de Mooij <jdemooij@mozilla.com>
2019-05-22 20:04:25 +0000

    Bug 1551796 part 2 - Move JitScript from vm/TypeInference.* to jit/JitScript.*. r=tcampbell
    Some of the TypeInference-related static methods were kept in TypeInference-inl.h and
    TypeInference.cpp because of various dependencies. Also, methods like JitScript::MonitorAssign
    probably don't belong on JitScript anyway, but for now this seems reasonable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31982
    
    --HG--
    rename : js/src/vm/TypeInference-inl.h => js/src/jit/JitScript-inl.h
    rename : js/src/vm/TypeInference.cpp => js/src/jit/JitScript.cpp
    rename : js/src/vm/TypeInference.h => js/src/jit/JitScript.h
    extra : moz-landing-system : lando

---
+++ js/src/jit/JitScript-inl.h
@@ -0,0 +1,106 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_JitScript_inl_h
+#define jit_JitScript_inl_h
+
+#include "jit/JitScript.h"
+
+#include "mozilla/BinarySearch.h"
+
+#include "vm/JSScript.h"
+#include "vm/TypeInference.h"
+
+#include "vm/JSContext-inl.h"
+
+namespace js {
+
+inline StackTypeSet* JitScript::thisTypes(const AutoSweepJitScript& sweep,
+                                          JSScript* script) {
+  return typeArray(sweep) + script->numBytecodeTypeSets();
+}
+
+/*
+ * Note: for non-escaping arguments, argTypes reflect only the initial type of
+ * the variable (e.g. passed values for argTypes, or undefined for localTypes)
+ * and not types from subsequent assignments.
+ */
+
+inline StackTypeSet* JitScript::argTypes(const AutoSweepJitScript& sweep,
+                                         JSScript* script, unsigned i) {
+  MOZ_ASSERT(i < script->functionNonDelazifying()->nargs());
+  return typeArray(sweep) + script->numBytecodeTypeSets() + 1 /* this */ + i;
+}
+
+template <typename TYPESET>
+/* static */ inline TYPESET* JitScript::BytecodeTypes(JSScript* script,
+                                                      jsbytecode* pc,
+                                                      uint32_t* bytecodeMap,
+                                                      uint32_t* hint,
+                                                      TYPESET* typeArray) {
+  MOZ_ASSERT(CodeSpec[*pc].format & JOF_TYPESET);
+  uint32_t offset = script->pcToOffset(pc);
+
+  // See if this pc is the next typeset opcode after the last one looked up.
+  size_t numBytecodeTypeSets = script->numBytecodeTypeSets();
+  if ((*hint + 1) < numBytecodeTypeSets && bytecodeMap[*hint + 1] == offset) {
+    (*hint)++;
+    return typeArray + *hint;
+  }
+
+  // See if this pc is the same as the last one looked up.
+  if (bytecodeMap[*hint] == offset) {
+    return typeArray + *hint;
+  }
+
+  // Fall back to a binary search.  We'll either find the exact offset, or
+  // there are more JOF_TYPESET opcodes than nTypeSets in the script (as can
+  // happen if the script is very long) and we'll use the last location.
+  size_t loc;
+  bool found =
+      mozilla::BinarySearch(bytecodeMap, 0, numBytecodeTypeSets, offset, &loc);
+  if (found) {
+    MOZ_ASSERT(bytecodeMap[loc] == offset);
+  } else {
+    MOZ_ASSERT(numBytecodeTypeSets == JSScript::MaxBytecodeTypeSets);
+    loc = numBytecodeTypeSets - 1;
+  }
+
+  *hint = mozilla::AssertedCast<uint32_t>(loc);
+  return typeArray + *hint;
+}
+
+inline StackTypeSet* JitScript::bytecodeTypes(const AutoSweepJitScript& sweep,
+                                              JSScript* script,
+                                              jsbytecode* pc) {
+  MOZ_ASSERT(CurrentThreadCanAccessZone(script->zone()));
+  return BytecodeTypes(script, pc, bytecodeTypeMap(), bytecodeTypeMapHint(),
+                       typeArray(sweep));
+}
+
+inline AutoKeepJitScripts::AutoKeepJitScripts(JSContext* cx)
+    : zone_(cx->zone()->types), prev_(zone_.keepJitScripts) {
+  zone_.keepJitScripts = true;
+}
+
+inline AutoKeepJitScripts::~AutoKeepJitScripts() {
+  MOZ_ASSERT(zone_.keepJitScripts);
+  zone_.keepJitScripts = prev_;
+}
+
+inline bool JitScript::typesNeedsSweep(Zone* zone) const {
+  MOZ_ASSERT(!js::TlsContext.get()->inUnsafeCallWithABI);
+  return typesGeneration() != zone->types.generation;
+}
+
+}  // namespace js
+
+inline bool JSScript::ensureHasJitScript(JSContext* cx,
+                                         js::AutoKeepJitScripts&) {
+  return jitScript() || createJitScript(cx);
+}
+
+#endif /* jit_JitScript_inl_h */
