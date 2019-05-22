09d2a1f8ebc5293e24fa44b884a171980053b5d1
Olli Pettay <Olli.Pettay@helsinki.fi>
2019-05-22 15:40:10 +0000

    Bug 1553400 - XMLHttpRequestDoneNotifier should own XHR, not vice-versa. r=baku a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D32142

--- dom/xhr/XMLHttpRequestMainThread.h
+++ dom/xhr/XMLHttpRequestMainThread.h
@@ -724,7 +724,7 @@ class XMLHttpRequestMainThread final : public XMLHttpRequest,
   // Our parse-end listener, if we are parsing.
   RefPtr<nsXHRParseEndListener> mParseEndListener;
 
-  RefPtr<XMLHttpRequestDoneNotifier> mDelayedDoneNotifier;
+  XMLHttpRequestDoneNotifier* mDelayedDoneNotifier;
   void DisconnectDoneNotifier();
 
   // Any stack information for the point the XHR was opened. This is deleted
@@ -795,8 +795,9 @@ class XMLHttpRequestDoneNotifier : public Runnable {
   NS_IMETHOD Run() override {
     if (mXHR) {
       RefPtr<XMLHttpRequestMainThread> xhr = mXHR;
-      mXHR = nullptr;
+      // ChangeStateToDoneInternal ends up calling Disconnect();
       xhr->ChangeStateToDoneInternal();
+      MOZ_ASSERT(!mXHR);
     }
     return NS_OK;
   }
@@ -804,7 +805,7 @@ class XMLHttpRequestDoneNotifier : public Runnable {
   void Disconnect() { mXHR = nullptr; }
 
  private:
-  XMLHttpRequestMainThread* mXHR;
+  RefPtr<XMLHttpRequestMainThread> mXHR;
 };
 
 class nsXHRParseEndListener : public nsIDOMEventListener {
