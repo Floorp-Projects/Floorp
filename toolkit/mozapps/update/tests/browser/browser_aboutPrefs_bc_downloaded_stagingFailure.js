c54526ed90402d59dc7eadfa4b724ce1d69162ad
Robert Strong <robert.bugzilla@gmail.com>
2019-05-22 19:29:37 +0000

    Bug 1552853 - Fix the update UI so it displays restart when it should when update staging fails. r=mhowell a=jcristau
    Fixes the case reported where disabling the maintenance service caused the UI to display Applying due to the update staging failure.
    Added new error codes for the two cases where pending was written to the update.status file when staging failed.
    Added code to UpdateService.jsm to set the update.status to pending when either of the two new error codes are in the update.status file.
    Added two new tests that verify the UI and simulate the failure condition in updater.cpp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32177
    
    --HG--
    extra : source : ebeb4133719f9276cf26516cea8df8fa16127fdc

---
+++ toolkit/mozapps/update/tests/browser/browser_aboutPrefs_bc_downloaded_stagingFailure.js
@@ -0,0 +1,29 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test for about:preferences background check for updates
+// with the update downloaded and staging has failed when the about:preferences
+// is opened.
+add_task(async function aboutPrefs_backgroundCheck_downloaded_stagingFailure() {
+  gEnv.set("MOZ_TEST_STAGING_ERROR", "1");
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [PREF_APP_UPDATE_STAGING_ENABLED, true],
+    ],
+  });
+
+  // Since the partial should be successful specify an invalid size for the
+  // complete update.
+  let params = {queryString: "&completePatchOnly=1",
+                backgroundUpdate: true,
+                waitForUpdateState: STATE_PENDING};
+  await runAboutDialogUpdateTest(params, [
+    {
+      panelId: "apply",
+      checkActiveUpdate: {state: STATE_PENDING},
+      continueFile: null,
+    },
+  ]);
+});
