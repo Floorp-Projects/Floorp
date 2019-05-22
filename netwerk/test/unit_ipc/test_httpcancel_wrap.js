1f3c3a7915a34935f1d81715084df18ed1009df9
Valentin Gosu <valentin.gosu@gmail.com>
2019-05-22 13:58:55 +0000

    Bug 1529911 - Add more tests for cancelling the channel r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D20500

--- netwerk/test/unit_ipc/test_httpcancel_wrap.js
+++ netwerk/test/unit_ipc/test_httpcancel_wrap.js
@@ -7,40 +7,44 @@ const ReferrerInfo = Components.Constructor("@mozilla.org/referrer-info;1",
                                             "nsIReferrerInfo",
                                             "init");
 
-let observer = {
-  QueryInterface: function eventsink_qi(iid) {
-    if (iid.equals(Ci.nsISupports) ||
-        iid.equals(Ci.nsIObserver))
-      return this;
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  },
-
-  observe: function(subject, topic, data) {
-    subject = subject.QueryInterface(Ci.nsIRequest);
-    subject.cancel(Cr.NS_BINDING_ABORTED);
-
-    // ENSURE_CALLED_BEFORE_CONNECT: setting values should still work
-    try {
-      subject.QueryInterface(Ci.nsIHttpChannel);
-      let currentReferrer = subject.getRequestHeader("Referer");
-      Assert.equal(currentReferrer, "http://site1.com/");
-      let uri = Services.io.newURI("http://site2.com");
-      subject.referrerInfo = new ReferrerInfo(Ci.nsIHttpChannel.REFERRER_POLICY_UNSET, true, uri);
-    } catch (ex) {
-      do_throw("Exception: " + ex);
-    }
-
-    let obs = Cc["@mozilla.org/observer-service;1"].getService();
-    obs = obs.QueryInterface(Ci.nsIObserverService);
-    obs.removeObserver(observer, "http-on-modify-request");
-  }
-};
+let observer = null;
 
 function run_test() {
+  do_await_remote_message("register-observer").then(() => {
+    observer =  {
+      QueryInterface: function eventsink_qi(iid) {
+        if (iid.equals(Ci.nsISupports) ||
+            iid.equals(Ci.nsIObserver))
+          return this;
+        throw Cr.NS_ERROR_NO_INTERFACE;
+      },
 
-  let obs = Cc["@mozilla.org/observer-service;1"].getService();
-  obs = obs.QueryInterface(Ci.nsIObserverService);
-  obs.addObserver(observer, "http-on-modify-request");
+      observe: function(subject, topic, data) {
+        subject = subject.QueryInterface(Ci.nsIRequest);
+        subject.cancel(Cr.NS_BINDING_ABORTED);
+
+        // ENSURE_CALLED_BEFORE_CONNECT: setting values should still work
+        try {
+          subject.QueryInterface(Ci.nsIHttpChannel);
+          let currentReferrer = subject.getRequestHeader("Referer");
+          Assert.equal(currentReferrer, "http://site1.com/");
+          let uri = Services.io.newURI("http://site2.com");
+          subject.referrerInfo = new ReferrerInfo(Ci.nsIHttpChannel.REFERRER_POLICY_UNSET, true, uri);
+        } catch (ex) {
+          do_throw("Exception: " + ex);
+        }
+      }
+    }
+
+    Services.obs.addObserver(observer, "http-on-modify-request");
+    do_send_remote_message('register-observer-done');
+  });
+
+  do_await_remote_message("unregister-observer").then(() => {
+    Services.obs.removeObserver(observer, "http-on-modify-request");
+
+    do_send_remote_message('unregister-observer-done');
+  });
 
   run_test_in_child("../unit/test_httpcancel.js");
 }
