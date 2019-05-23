782055b72a13c8a071fe6d5f5fab0b5d1aba9d5f
Sylvestre Ledru <sledru@mozilla.com>
2019-05-23 16:44:04 +0000

    Bug 1552897 - Ride along: format with black r=ahal
    Depends on D31807
    
    Differential Revision: https://phabricator.services.mozilla.com/D31808

--- python/mozlint/mozlint/formatters/stylish.py
+++ python/mozlint/mozlint/formatters/stylish.py
@@ -13,23 +13,25 @@ from ..util.string import pluralize
 class StylishFormatter(object):
     """Formatter based on the eslint default."""
 
-    _indent_ = '  '
+    _indent_ = "  "
 
     # Colors later on in the list are fallbacks in case the terminal
     # doesn't support colors earlier in the list.
     # See http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
     _colors = {
-        'grey': [247, 8, 7],
-        'red': [1],
-        'green': [2],
-        'yellow': [3],
-        'brightred': [9, 1],
-        'brightyellow': [11, 3],
+        "grey": [247, 8, 7],
+        "red": [1],
+        "green": [2],
+        "yellow": [3],
+        "brightred": [9, 1],
+        "brightyellow": [11, 3],
     }
 
     fmt = """
   {c1}{lineno}{column}  {c2}{level}{normal}  {message}  {c1}{rule}({linter}){normal}
-{diff}""".lstrip('\n')
+{diff}""".lstrip(
+        "\n"
+    )
     fmt_summary = "{t.bold}{c}\u2716 {problem} ({error}, {warning}{failure}){t.normal}"
 
     def __init__(self, disable_colors=False):
@@ -40,7 +42,7 @@ class StylishFormatter(object):
         for num in self._colors[color]:
             if num < self.num_colors:
                 return self.term.color(num)
-        return ''
+        return ""
 
     def _reset_max(self):
         self.max_lineno = 0
@@ -60,15 +62,15 @@ class StylishFormatter(object):
         if not diff:
             return ""
 
-        new_diff = ''
-        for line in diff.split('\n'):
-            if line.startswith('+'):
-                new_diff += self.color('green')
-            elif line.startswith('-'):
-                new_diff += self.color('red')
+        new_diff = ""
+        for line in diff.split("\n"):
+            if line.startswith("+"):
+                new_diff += self.color("green")
+            elif line.startswith("-"):
+                new_diff += self.color("red")
             else:
                 new_diff += self.term.normal
-            new_diff += self._indent_ + line + '\n'
+            new_diff += self._indent_ + line + "\n"
         return new_diff
 
     def __call__(self, result):
@@ -85,48 +87,61 @@ class StylishFormatter(object):
             for err in errors:
                 assert isinstance(err, Issue)
                 self._update_max(err)
-                if err.level == 'error':
+                if err.level == "error":
                     num_errors += 1
                 else:
                     num_warnings += 1
 
-            for err in sorted(errors, key=lambda e: (int(e.lineno), int(e.column or 0))):
+            for err in sorted(
+                errors, key=lambda e: (int(e.lineno), int(e.column or 0))
+            ):
                 if err.column:
                     col = ":" + str(err.column).ljust(self.max_column)
                 else:
-                    col = "".ljust(self.max_column+1)
+                    col = "".ljust(self.max_column + 1)
 
                 args = {
-                    'normal': self.term.normal,
-                    'c1': self.color('grey'),
-                    'c2': self.color('red') if err.level == 'error' else self.color('yellow'),
-                    'lineno': str(err.lineno).rjust(self.max_lineno),
-                    'column': col,
-                    'level': err.level.ljust(self.max_level),
-                    'rule': '{} '.format(err.rule) if err.rule else '',
-                    'linter': err.linter.lower(),
-                    'message': err.message.ljust(self.max_message),
-                    'diff': self._get_colored_diff(err.diff).ljust(self.max_message)
+                    "normal": self.term.normal,
+                    "c1": self.color("grey"),
+                    "c2": self.color("red")
+                    if err.level == "error"
+                    else self.color("yellow"),
+                    "lineno": str(err.lineno).rjust(self.max_lineno),
+                    "column": col,
+                    "level": err.level.ljust(self.max_level),
+                    "rule": "{} ".format(err.rule) if err.rule else "",
+                    "linter": err.linter.lower(),
+                    "message": err.message.ljust(self.max_message),
+                    "diff": self._get_colored_diff(err.diff).ljust(self.max_message),
                 }
-                message.append(self.fmt.format(**args).rstrip().rstrip('\n'))
+                message.append(self.fmt.format(**args).rstrip().rstrip("\n"))
 
-            message.append('')  # newline
+            message.append("")  # newline
 
         # If there were failures, make it clear which linters failed
         for fail in failed:
-            message.append("{c}A failure occurred in the {name} linter.".format(
-                c=self.color('brightred'),
-                name=fail,
-            ))
+            message.append(
+                "{c}A failure occurred in the {name} linter.".format(
+                    c=self.color("brightred"), name=fail
+                )
+            )
 
         # Print a summary
-        message.append(self.fmt_summary.format(
-            t=self.term,
-            c=self.color('brightred') if num_errors or failed else self.color('brightyellow'),
-            problem=pluralize('problem', num_errors + num_warnings + len(failed)),
-            error=pluralize('error', num_errors),
-            warning=pluralize('warning', num_warnings or result.total_suppressed_warnings),
-            failure=', {}'.format(pluralize('failure', len(failed))) if failed else '',
-        ))
+        message.append(
+            self.fmt_summary.format(
+                t=self.term,
+                c=self.color("brightred")
+                if num_errors or failed
+                else self.color("brightyellow"),
+                problem=pluralize("problem", num_errors + num_warnings + len(failed)),
+                error=pluralize("error", num_errors),
+                warning=pluralize(
+                    "warning", num_warnings or result.total_suppressed_warnings
+                ),
+                failure=", {}".format(pluralize("failure", len(failed)))
+                if failed
+                else "",
+            )
+        )
 
-        return '\n'.join(message)
+        return "\n".join(message)
