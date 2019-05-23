3a7b503ec555b70f1a5b77a04f3f73cfda40695d
Ryan VanderMeulen <ryanvm@gmail.com>
2019-05-23 14:57:19 -0400

    Bug 1553912 - Update to FreeType 2.10.0. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D32398

--- modules/freetype2/builds/cmake/testbuild.sh
+++ modules/freetype2/builds/cmake/testbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# Copyright 2015-2018 by
+# Copyright (C) 2015-2019 by
 # David Turner, Robert Wilhelm, and Werner Lemberg.
 #
 # This file is part of the FreeType project, and may only be used, modified,
