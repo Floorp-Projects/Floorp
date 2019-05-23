0953b970d805ca40bd6b026333f93ee50f4a2909
James Teh <jteh@mozilla.com>
2019-05-23 19:54:37 +0000

    Bug 1543307: For out-of-process iframes on Windows, return the embedded document as a child of the OuterDocAccessible. r=eeejay
    Windows accessibility clients talk directly to the content process via COM.
    In order to expose an OOP iframe document accessible as a child of the embedder iframe accessible via COM, the embedder process needs a COM proxy for the iframe document accessible.
    This is exposed on the embedder's BrowserBridgeChild, so we can use this when a client asks for the child of the OuterDocAccessible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32281

--- accessible/generic/OuterDocAccessible.h
+++ accessible/generic/OuterDocAccessible.h
@@ -28,6 +28,9 @@ class OuterDocAccessible final : public AccessibleWrap {
   NS_INLINE_DECL_REFCOUNTING_INHERITED(OuterDocAccessible, AccessibleWrap)
 
   DocAccessibleParent* RemoteChildDoc() const;
+#if defined(XP_WIN)
+  Accessible* RemoteChildDocAccessible() const;
+#endif
 
   // Accessible
   virtual void Shutdown() override;
