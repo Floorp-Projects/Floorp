5f9423d7150745dc722bc3863723f33f6fd4c51c
Kartikaya Gupta <kgupta@mozilla.com>
2019-05-23 20:06:33 +0000

    Bug 1551648 - Record peak resident memory usage in telemetry. r=erahm
    Differential Revision: https://phabricator.services.mozilla.com/D32184

--- xpcom/base/MemoryTelemetry.cpp
+++ xpcom/base/MemoryTelemetry.cpp
@@ -306,6 +306,7 @@ nsresult MemoryTelemetry::GatherReports(
         RECORD(MEMORY_VSIZE_MAX_CONTIGUOUS, VsizeMaxContiguous, UNITS_BYTES);
 #endif
         RECORD(MEMORY_RESIDENT_FAST, ResidentFast, UNITS_BYTES);
+        RECORD(MEMORY_RESIDENT_PEAK, ResidentPeak, UNITS_BYTES);
         RECORD(MEMORY_UNIQUE, ResidentUnique, UNITS_BYTES);
         RECORD(MEMORY_HEAP_ALLOCATED, HeapAllocated, UNITS_BYTES);
         RECORD(MEMORY_HEAP_OVERHEAD_FRACTION, HeapOverheadFraction,
