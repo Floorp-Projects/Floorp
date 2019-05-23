5f13eb1e4186829ff62b772e2ff7c133e40d3509
Oana Pop Rus <opoprus@mozilla.com>
2019-05-23 17:52:21 +0300

    Backed out changeset 3304277f5bde (bug 1553252) for build bustages on Windows on a CLOSED TREE

--- servo/components/style/values/specified/text.rs
+++ servo/components/style/values/specified/text.rs
@@ -134,16 +134,14 @@ impl ToComputedValue for LineHeight {
 }
 
 /// A generic value for the `text-overflow` property.
-/// cbindgen:derive-tagged-enum-copy-constructor=true
 #[derive(Clone, Debug, Eq, MallocSizeOf, PartialEq, SpecifiedValueInfo, ToCss, ToShmem)]
-#[repr(C, u8)]
 pub enum TextOverflowSide {
     /// Clip inline content.
     Clip,
     /// Render ellipsis to represent clipped inline content.
     Ellipsis,
     /// Render a given string to represent clipped inline content.
-    String(crate::OwnedStr),
+    String(Box<str>),
 }
 
 impl Parse for TextOverflowSide {
@@ -163,7 +161,7 @@ impl Parse for TextOverflowSide {
                 }
             },
             Token::QuotedString(ref v) => Ok(TextOverflowSide::String(
-                v.as_ref().to_owned().into(),
+                v.as_ref().to_owned().into_boxed_str(),
             )),
             ref t => Err(location.new_unexpected_token_error(t.clone())),
         }
