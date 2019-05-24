f7aace2a69d4accabf5067342be675670a09d457
Boris Zbarsky <bzbarsky@mit.edu>
2019-05-24 07:19:58 +0000

    Bug 1121623 part 2.  Switch AddonManagerPermissions to being a namespace.  r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D32200

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -1809,7 +1809,9 @@ class IDLNamespace(IDLInterfaceOrNamespace):
                 if not attr.noArguments():
                     raise WebIDLError("[%s] must not have arguments" % identifier,
                                       [attr.location])
-            elif identifier == "Pref" or identifier == "Func":
+            elif (identifier == "Pref" or
+                  identifier == "HeaderFile" or
+                  identifier == "Func"):
                 # Known extended attributes that take a string value
                 if not attr.hasValue():
                     raise WebIDLError("[%s] must have a value" % identifier,
