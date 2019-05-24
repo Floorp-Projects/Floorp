1439a6e482faeccce270f872ba8483ea74829d91
Jan Odvarko <odvarko@gmail.com>
2019-05-24 13:01:01 +0000

    Bug 1552110 - Properly render values in the Security panel r=fvsch a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D32324

--- devtools/client/netmonitor/src/components/SecurityPanel.js
+++ devtools/client/netmonitor/src/components/SecurityPanel.js
@@ -20,7 +20,14 @@ const {
 const TreeViewClass = require("devtools/client/shared/components/tree/TreeView");
 const PropertiesView = createFactory(require("./PropertiesView"));
 
-const { div, input, span } = dom;
+loader.lazyGetter(this, "Rep", function() {
+  return require("devtools/client/shared/components/reps/reps").REPS.Rep;
+});
+loader.lazyGetter(this, "MODE", function() {
+  return require("devtools/client/shared/components/reps/reps").MODE;
+});
+
+const { div, span } = dom;
 const NOT_AVAILABLE = L10N.getStr("netmonitor.security.notAvailable");
 const ERROR_LABEL = L10N.getStr("netmonitor.security.error");
 const CIPHER_SUITE_LABEL = L10N.getStr("netmonitor.security.cipherSuite");
@@ -96,16 +103,15 @@ class SecurityPanel extends Component {
 
     return span({ className: "security-info-value" },
       member.name === ERROR_LABEL ?
-        // Display multiline text for security error
-        value
-        :
-        // Display one line selectable text for security details
-        input({
-          className: "textbox-input",
-          readOnly: "true",
-          value,
-        })
-      ,
+        // Display multiline text for security error for a label using a rep.
+        value : Rep(Object.assign(props, {
+          // FIXME: A workaround for the issue in StringRep
+          // Force StringRep to crop the text everytime
+          member: Object.assign({}, member, { open: false }),
+          mode: MODE.TINY,
+          cropLimit: 60,
+          noGrip: true,
+        })),
       weaknessReasons.includes("cipher") &&
       member.name === CIPHER_SUITE_LABEL ?
         // Display an extra warning icon after the cipher suite
