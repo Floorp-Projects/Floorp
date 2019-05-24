ad3208992c0127242b56a7ac7d64dae19ada7ce6
Jon Coppeard <jcoppeard@mozilla.com>
2019-05-24 15:45:33 +0100

    Bug 1395509 - Track malloc memory associated with JSScripts r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D32500

--- js/src/vm/JSScript.h
+++ js/src/vm/JSScript.h
@@ -2987,6 +2987,7 @@ class alignas(uintptr_t) LazyScriptData final {
   // Size to allocate
   static size_t AllocationSize(uint32_t numClosedOverBindings,
                                uint32_t numInnerFunctions);
+  size_t allocationSize() const;
 
   // Translate an offset into a concrete pointer.
   template <typename T>
