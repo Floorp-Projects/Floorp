cc8b296f7f8f2c203fda217c3c0999742f781081
Gabriel Luong <gabriel.luong@gmail.com>
2019-05-24 14:02:14 -0400

    Bug 1550519 - Show a translucent parent highlight when a subgrid is highlighted. r=pbro
    Differential Revision: https://phabricator.services.mozilla.com/D32507

--- devtools/client/inspector/grids/grid-inspector.js
+++ devtools/client/inspector/grids/grid-inspector.js
@@ -346,6 +346,10 @@ class GridInspector {
           return;
         }
 
+        if (!parentGridNodeFront) {
+          return;
+        }
+
         const parentIndex = grids.findIndex(g =>
           g.nodeFront.actorID === parentGridNodeFront.actorID);
         gridData.parentNodeActorID = parentGridNodeFront.actorID;
@@ -506,8 +510,10 @@ class GridInspector {
 
         // If the grid for which the color was updated currently has a highlighter, update
         // the color.
-        if (grid.highlighted) {
+        if (this.highlighters.gridHighlighters.has(node)) {
           this.highlighters.showGridHighlighter(node);
+        } else if (this.highlighters.parentGridHighlighters.has(node)) {
+          this.highlighters.showParentGridHighlighter(node);
         }
       }
     }
