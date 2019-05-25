c3f03e82ee97e83b54343b2162b8d24a97cd360c
Ehsan Akhgari <ehsan@mozilla.com>
2019-05-25 01:25:37 +0000

    Bug 1547813 - Part 5: Introduce StorageAccess::ePartitionForeignOrDeny; r=baku
    This StorageAccess code tells callers that they must partition third-party
    storage, or deny storage access if that is not possible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D29740
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsContentUtils.h
+++ dom/base/nsContentUtils.h
@@ -2892,6 +2892,9 @@ class nsContentUtils {
   // of permissions. Private Browsing is considered to be more limiting
   // then session scoping
   enum class StorageAccess {
+    // The storage should be partitioned for third-party resources. if the
+    // caller is unable to do it, deny the storage access.
+    ePartitionForeignOrDeny = -2,
     // The storage should be partitioned for third-party trackers. if the caller
     // is unable to do it, deny the storage access.
     ePartitionTrackersOrDeny = -1,
