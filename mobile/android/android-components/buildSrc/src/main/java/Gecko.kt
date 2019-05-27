5135dc42ef8cc9640ce20ed5a9a37c302a9c89f2
Sebastian Kaspari <s.kaspari@gmail.com>
2019-05-27 09:27:35 -0400

    [components] Update GeckoView Nightly to 69.0.20190527095441.

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "69.0.20190524095337"
+    const val nightly_version = "69.0.20190527095441"
 
     /**
      * GeckoView Beta Version.
