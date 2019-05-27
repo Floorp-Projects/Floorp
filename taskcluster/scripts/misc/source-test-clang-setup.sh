f4c66c5c5b8a777d589de66135cfe0694c2add1b
Andi-Bogdan Postelnicu <bpostelnicu@mozilla.com>
2019-05-27 14:07:39 +0000

    Bug 1554632 - reviewbot tasks should use a mozconfig file. r=bastien,sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D32680

--- taskcluster/scripts/misc/source-test-clang-setup.sh
+++ taskcluster/scripts/misc/source-test-clang-setup.sh
@@ -7,6 +7,13 @@ export PATH=$MOZBUILD_STATE_PATH/clang-tidy/bin:$PATH
 # Use toolchain clang
 export LD_LIBRARY_PATH=$MOZBUILD_STATE_PATH/clang/lib
 
+# Write custom mozconfig
+export MOZCONFIG=$HOME/checkouts/gecko/mozconfig
+# Enable debug mode
+echo "ac_add_options --enable-debug"  > $MOZCONFIG
+# Enable GC zeal, a testing and debugging feature that helps find GC-related bugs in JSAPI applications.
+echo "ac_add_options --enable-gczeal" > $MOZCONFIG
+
 # Mach lookup clang-tidy in clang-tools
 mkdir -p $MOZBUILD_STATE_PATH/clang-tools
 ln -s $MOZBUILD_STATE_PATH/clang-tidy $MOZBUILD_STATE_PATH/clang-tools/clang-tidy
