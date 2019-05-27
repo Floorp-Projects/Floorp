26201b29a2a9858078d636df6311864f48fd2afe
Andreas Tolfsen <ato@sny.no>
2019-05-27 11:39:16 +0000

    bug 1553317: remote: improve error message on missing method; r=remote-protocol-reviewers,ochameau
    We return with this rather omnious message when we are missing the
    implementation of a CDP method:
    
    	Error: Protocol error (Target.createBrowserContext): TypeError: inst[command] is not a function:
    
    This patch improves the error message so that debugging is not
    necessary to find out which domain or command is missing.
    
    Ideally Session.jsm and ContentProcessSession.jsm would share the
    same execute() function (there's really not reason they don't),
    but that involves more work.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32069
    
    --HG--
    extra : moz-landing-system : lando

---
+++ remote/test/browser/browser_session.js
@@ -0,0 +1,25 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+add_task(async function() {
+  await RemoteAgent.listen(Services.io.newURI("http://localhost:9222"));
+  const CDP = await getCDP();
+  const {webSocketDebuggerUrl} = await CDP.Version();
+  const client = await CDP({"target": webSocketDebuggerUrl});
+
+  try {
+    await client.send("Hoobaflooba");
+  } catch (e) {
+    ok(e.message.match(/TypeError: Invalid method format/));
+  }
+  try {
+    await client.send("Hooba.flooba");
+  } catch (e) {
+    ok(e.message.match(/UnknownMethodError/));
+  }
+
+  await client.close();
+  await RemoteAgent.close();
+});
