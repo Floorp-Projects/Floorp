8cd0fa195d7aac87a5c945faf5cf9b9f93336417
Julian Descottes <jdescottes@mozilla.com>
2019-05-27 08:04:11 +0000

    Bug 1535102 - Extract shared helper to setup remote CDP mochitests r=ato
    Depends on D32538.
    Not mandatory, but we have some duplicated code shared by several mochitests, maybe we could have a shared helper?
    
    Differential Revision: https://phabricator.services.mozilla.com/D32539

--- remote/test/browser/browser_page_bringToFront.js
+++ remote/test/browser/browser_page_bringToFront.js
@@ -56,26 +56,3 @@ add_task(async function testBringToFrontUpdatesFocusedWindow() {
 function getFocusedNavigator() {
   return Services.wm.getMostRecentWindow("navigator:browser");
 }
-
-async function setupTestForUri(uri) {
-  // Open a test page, to prevent debugging the random default page
-  const tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, uri);
-
-  // Start the CDP server
-  RemoteAgent.listen(Services.io.newURI("http://localhost:9222"));
-
-  // Retrieve the chrome-remote-interface library object
-  const CDP = await getCDP();
-
-  // Connect to the server
-  const client = await CDP({
-    target(list) {
-      // Ensure debugging the right target, i.e. the one for our test tab.
-      return list.find(target => {
-        return target.url == uri;
-      });
-    },
-  });
-  ok(true, "CDP client has been instantiated");
-  return { client, tab };
-}
