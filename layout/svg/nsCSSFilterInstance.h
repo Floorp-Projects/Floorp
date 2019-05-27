fce57b44fdaa6999f332c1c7589048b044ce510a
Emilio Cobos Álvarez <emilio@crisal.io>
2019-05-27 12:37:37 +0000

    Bug 1552878 - Use cbindgen for filters. r=jwatt
    Had to implement some OwnedSlice bits that the canvas code used.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31799

--- layout/svg/nsCSSFilterInstance.h
+++ layout/svg/nsCSSFilterInstance.h
@@ -13,9 +13,7 @@
 #include "mozilla/gfx/Point.h"
 #include "mozilla/gfx/Types.h"
 #include "nsColor.h"
-#include "nsTArrayForwardDeclare.h"
-
-struct nsStyleFilter;
+#include "mozilla/ServoStyleConsts.h"
 
 /**
  * This class helps nsFilterInstance build its filter graph. It turns a CSS
@@ -23,6 +21,7 @@ struct nsStyleFilter;
  * FilterPrimitiveDescription connected to the filter graph.
  */
 class nsCSSFilterInstance {
+  using StyleFilter = mozilla::StyleFilter;
   typedef mozilla::gfx::Color Color;
   typedef mozilla::gfx::FilterPrimitiveDescription FilterPrimitiveDescription;
   typedef mozilla::gfx::IntPoint IntPoint;
@@ -41,7 +40,7 @@ class nsCSSFilterInstance {
    *   the filtered element's frame space in CSS pixels to filter space.
    */
   nsCSSFilterInstance(
-      const nsStyleFilter& aFilter, nscolor aShadowFallbackColor,
+      const StyleFilter& aFilter, nscolor aShadowFallbackColor,
       const nsIntRect& aTargetBoundsInFilterSpace,
       const gfxMatrix& aFrameSpaceInCSSPxToFilterSpaceTransform);
 
@@ -121,7 +120,7 @@ class nsCSSFilterInstance {
   /**
    * The CSS filter originally from the style system.
    */
-  const nsStyleFilter& mFilter;
+  const StyleFilter& mFilter;
 
   /**
    * The color that should be used for drop-shadow() filters that don't
