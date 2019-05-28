cb7e7880670242450506992a99682ceeb3946c41
Nika Layzell <nika@thelayzells.com>
2019-05-28 14:38:14 +0000

    Bug 1554217 - Disable HTTPResponseProcessSelection on Beta, r=farre a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D32489

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -503,8 +503,13 @@ pref("browser.tabs.delayHidingAudioPlayingIconMS", 3000);
 pref("browser.tabs.remote.separatePrivilegedContentProcess", true);
 #endif
 
-// Turn on HTTP response process selection.
+#ifdef NIGHTLY_BUILD
 pref("browser.tabs.remote.useHTTPResponseProcessSelection", true);
+#else
+// Disabled outside of nightly due to bug 1554217
+pref("browser.tabs.remote.useHTTPResponseProcessSelection", false);
+#endif
+
 
 // Unload tabs when available memory is running low
 pref("browser.tabs.unloadOnLowMemory", true);
