fa0ba491b048d6c5df3dac3e26abcd4c42ce52ee
Nick Alexander <nalexander@mozilla.com>
2019-05-28 22:08:41 +0000

    Bug 1554256 - Read GeckoView configuration file in release builds when package is the current Android "debug-app". r=geckoview-reviewers,snorp a=jcristau
    Bug 1533385 taught GeckoView to read configuration from a file. But
    right now, release APKs don't read that configuration: see the
    documentation at See
    https://mozilla.github.io/geckoview/tutorials/automation.html#enabling-reading-configuration-from-a-file-for-release-builds.
    However, Android has a flag -- the "debug_app" flag -- that Chrome
    uses to control this configuration file behaviour, and this commit
    makes GeckoView uses that flag as well.
    
    To use this, invoke `adb shell am set-debug-app --persistent PACKAGE`
    (see https://developer.android.com/studio/command-line/adb).
    
    Differential Revision: https://phabricator.services.mozilla.com/D32511

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -15,7 +15,6 @@ import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.content.res.Configuration;
@@ -37,6 +36,7 @@ import org.mozilla.gecko.GeckoSystemStateListener;
 import org.mozilla.gecko.GeckoThread;
 import org.mozilla.gecko.PrefsHelper;
 import org.mozilla.gecko.util.BundleEventListener;
+import org.mozilla.gecko.util.ContextUtils;
 import org.mozilla.gecko.util.DebugConfig;
 import org.mozilla.gecko.util.EventCallback;
 import org.mozilla.gecko.util.GeckoBundle;
@@ -264,11 +264,10 @@ public final class GeckoRuntime implements Parcelable {
         String configFilePath = settings.getConfigFilePath();
         if (configFilePath == null) {
             // Default to /data/local/tmp/$PACKAGE-geckoview-config.yaml if android:debuggable="true"
-            // and to not read configuration from a file if android:debuggable="false".
-            final ApplicationInfo applicationInfo = context.getApplicationInfo();
-            final boolean isPackageDebuggable = (applicationInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-            if (isPackageDebuggable) {
-                configFilePath = String.format(CONFIG_FILE_PATH_TEMPLATE, applicationInfo.packageName);
+            // or if this application is the current Android "debug_app", and to not read configuration
+            // from a file otherwise.
+            if (ContextUtils.isApplicationDebuggable(context) || ContextUtils.isApplicationCurrentDebugApp(context)) {
+                configFilePath = String.format(CONFIG_FILE_PATH_TEMPLATE, context.getApplicationInfo().packageName);
             }
         }
 
