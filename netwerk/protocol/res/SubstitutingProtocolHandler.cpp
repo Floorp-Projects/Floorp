06ba14d80da44bc07be0f196c0c4326c646e6094
Valentin Gosu <valentin.gosu@gmail.com>
2019-05-28 13:46:55 +0000

    Bug 1536744 - Make SubstitutingProtocolHandler::NewURI only call ResolveJARURI if the host is android r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D30694

--- netwerk/protocol/res/SubstitutingProtocolHandler.cpp
+++ netwerk/protocol/res/SubstitutingProtocolHandler.cpp
@@ -348,22 +348,14 @@ nsresult SubstitutingProtocolHandler::NewURI(const nsACString& aSpec,
   rv = uri->GetHost(host);
   if (NS_FAILED(rv)) return rv;
 
-  uint32_t flags;
-  rv = GetSubstitutionFlags(host, &flags);
-  if (NS_FAILED(rv)) {
-    // This protocol has not been registered yet or does not exist,
-    // we can assume we don't expose nsIJARURI
-    uri.forget(aResult);
-    return NS_OK;
+  // "android" is the only root that would return the RESOLVE_JAR_URI flag
+  // see nsResProtocolHandler::GetSubstitutionInternal
+  if (host.EqualsLiteral("android")) {
+    return ResolveJARURI(uri, aResult);
   }
 
-  if (flags & nsISubstitutingProtocolHandler::RESOLVE_JAR_URI) {
-    rv = ResolveJARURI(uri, aResult);
-  } else {
-    uri.forget(aResult);
-  }
-
-  return rv;
+  uri.forget(aResult);
+  return NS_OK;
 }
 
 nsresult SubstitutingProtocolHandler::ResolveJARURI(nsIURL* aURL,
@@ -387,9 +379,9 @@ nsresult SubstitutingProtocolHandler::ResolveJARURI(nsIURL* aURL,
   if (!jarURI) {
     // This substitution does not resolve to a jar: URL, so we just
     // return the plain SubstitutionURL
-    *aResult = aURL;
-    NS_ADDREF(*aResult);
-    return rv;
+    nsCOMPtr<nsIURI> url = aURL;
+    url.forget(aResult);
+    return NS_OK;
   }
 
   nsCOMPtr<nsIJARURI> result = new SubstitutingJARURI(aURL, jarURI);
