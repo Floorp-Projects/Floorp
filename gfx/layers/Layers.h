f187c6f26bfda056b81d74562bc3dc713f860f44
Barret Rennie <barret@brennie.ca>
2019-05-28 13:42:57 +0000

    Bug 1553261 - Clean up some of the CompositionPayload infrastructure r=kats a=jcristau
    Some time ago (bug 819791), InfallibleTArray and nsTArray become equivalent, so
    continuing to use InfallibleTArray here will just lead to confusion.
    
    In addition, the overloaded `RegisterPayload` could take either a single
    payload or multiple, so I've split it into
    `RegisterPayload(CompositionPayload&)` and
    RegisterPayloads(nsTArray<CompositionPayload>&)`
    
    Differential Revision: https://phabricator.services.mozilla.com/D32227
    
    --HG--
    extra : source : a8973dc65dc4963a209a84d7650da970ad44470e

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -738,7 +738,7 @@ class LayerManager : public FrameRecorder {
     mPayload.AppendElement(aPayload);
     MOZ_ASSERT(mPayload.Length() < 10000);
   }
-  void RegisterPayload(const InfallibleTArray<CompositionPayload>& aPayload) {
+  void RegisterPayloads(const nsTArray<CompositionPayload>& aPayload) {
     mPayload.AppendElements(aPayload);
     MOZ_ASSERT(mPayload.Length() < 10000);
   }
@@ -782,7 +782,7 @@ class LayerManager : public FrameRecorder {
   // next composite.
   // IMPORTANT: Clients should take care to clear this or risk it slowly
   // growing out of control.
-  InfallibleTArray<CompositionPayload> mPayload;
+  nsTArray<CompositionPayload> mPayload;
 
  public:
   /*
