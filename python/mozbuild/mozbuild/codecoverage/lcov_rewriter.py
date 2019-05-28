a0d80576034516d37f702f5bd7c7ef236225d269
Justin Wood <Callek@gmail.com>
2019-05-28 14:22:45 +0000

    Bug 1542963 - Add absolute_imports to mozbuild and mozpack. r=glandium
    Lint python/mozbuild/{mozbuild,mozpack}. r=#build
    
    Add 'from __future__ import absolute_import' to all missing files in python/mozbuild/{mozbuild,mozpack}/**/*.py
    
    Differential Revision: https://phabricator.services.mozilla.com/D26643
    
    --HG--
    extra : moz-landing-system : lando

--- python/mozbuild/mozbuild/codecoverage/lcov_rewriter.py
+++ python/mozbuild/mozbuild/codecoverage/lcov_rewriter.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import absolute_import
+
 from argparse import ArgumentParser
 import json
 import os
@@ -15,7 +17,7 @@ from six import viewitems
 
 from mozpack.chrome.manifest import parse_manifest
 import mozpack.path as mozpath
-from manifest_handler import ChromeManifestHandler
+from .manifest_handler import ChromeManifestHandler
 
 
 class LcovRecord(object):
