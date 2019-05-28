7528309cf7a11b12697d733d86c45b9f17ebc9ce
Robert Strong <robert.bugzilla@gmail.com>
2019-05-28 17:36:16 +0000

    Bug 1503341 - Use UX provided text for displaying errors when writing the update settings json file. r=bytesized,flod
    Differential Revision: https://phabricator.services.mozilla.com/D32637

--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -1374,8 +1374,8 @@ var gMainPane = {
 
   async reportUpdatePrefWriteError(error) {
     let [title, message] = await document.l10n.formatValues([
-      {id: "update-pref-write-failure-title"},
-      {id: "update-pref-write-failure-message", args: {path: error.path}},
+      {id: "update-setting-write-failure-title"},
+      {id: "update-setting-write-failure-message", args: {path: error.path}},
     ]);
 
     // Set up the Ok Button
