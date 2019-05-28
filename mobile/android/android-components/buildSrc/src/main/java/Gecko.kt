bc3b16bc8a3ae28f52127526ff4ec0ec1a56b643
Christian Sadilek <christian.sadilek@gmail.com>
2019-05-28 14:47:42 -0400

    [components] Upgrade to GeckoView beta 68.0.20190527103257

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "68.0.20190520141152"
+    const val beta_version = "68.0.20190527103257"
 
     /**
      * GeckoView Release Version.
