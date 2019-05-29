c720f658474c4061f5129b0991507527da7a99b3
Daosheng Mu <daoshengmu@gmail.com>
2019-05-29 20:48:29 +0000

    Bug 1523351 - Part 2: Add tests for GamepadTouch and GamepadLightIndicator. r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D29289

--- dom/gamepad/ipc/GamepadTestChannelParent.cpp
+++ dom/gamepad/ipc/GamepadTestChannelParent.cpp
@@ -26,7 +26,8 @@ mozilla::ipc::IPCResult GamepadTestChannelParent::RecvGamepadTestEvent(
     LossyCopyUTF16toASCII(a.id(), gamepadID);
     uint32_t index = service->AddGamepad(
         gamepadID.get(), static_cast<GamepadMappingType>(a.mapping()), a.hand(),
-        a.num_buttons(), a.num_axes(), a.num_haptics());
+        a.num_buttons(), a.num_axes(), a.num_haptics(), a.num_lights(),
+        a.num_touches());
     if (!mShuttingdown) {
       Unused << SendReplyGamepadIndex(aID, index);
     }
@@ -50,6 +51,11 @@ mozilla::ipc::IPCResult GamepadTestChannelParent::RecvGamepadTestEvent(
   if (body.type() == GamepadChangeEventBody::TGamepadPoseInformation) {
     const GamepadPoseInformation& a = body.get_GamepadPoseInformation();
     service->NewPoseEvent(index, a.pose_state());
+    return IPC_OK();
+  }
+  if (body.type() == GamepadChangeEventBody::TGamepadTouchInformation) {
+    const GamepadTouchInformation& a = body.get_GamepadTouchInformation();
+    service->NewMultiTouchEvent(index, a.index(), a.touch_state());
     return IPC_OK();
   }
 
