913d6b76eeb484f8743c653ca7939f0fec257505
Jonathan Watt <jwatt@jwatt.org>
2019-05-29 10:42:14 +0100

    Bug 1557687. Remove nsPrintData::mDocWasToBeDestroyed. r=bobowen
    This shows that this variable has never been read going back all the way to
    the original Mercurial import in 2007:
    
      hg grep -f --diff mDocWasToBeDestroyed layout/printing/ns*
    
    Differential Revision: https://phabricator.services.mozilla.com/D34143
    
    --HG--
    extra : rebase_source : c14a51dab1825e26fb8031d291d2c69e78931c84
    extra : amend_source : 73794b07d703987db8d2eb49dd97d74b1a9be325

--- layout/printing/nsPrintData.h
+++ layout/printing/nsPrintData.h
@@ -31,13 +31,6 @@ class PrintPreviewUserEventSuppressor;
 // mPreparingForPrint - indicates that we have started Printing but
 //   have not gone to the timer to start printing the pages. It gets turned
 //   off right before we go to the timer.
-//
-// mDocWasToBeDestroyed - Gets set when "someone" tries to unload the document
-//   while we were prparing to Print. This typically happens if a user starts
-//   to print while a page is still loading. If they start printing and pause
-//   at the print dialog and then the page comes in, we then abort printing
-//   because the document is no longer stable.
-//
 //------------------------------------------------------------------------
 class nsPrintData {
   typedef mozilla::PrintPreviewUserEventSuppressor
@@ -77,9 +70,8 @@ class nsPrintData {
   bool mIsIFrameSelected;
   bool mIsParentAFrameSet;
   bool mOnStartSent;
-  bool mIsAborted;            // tells us the document is being aborted
-  bool mPreparingForPrint;    // see comments above
-  bool mDocWasToBeDestroyed;  // see comments above
+  bool mIsAborted;          // tells us the document is being aborted
+  bool mPreparingForPrint;  // see comments above
   bool mShrinkToFit;
   int32_t mNumPrintablePages;
   int32_t mNumPagesPrinted;
