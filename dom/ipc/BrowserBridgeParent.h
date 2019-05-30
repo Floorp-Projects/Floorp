4660ac813a0f3b29499c26be8680f37ff3968929
James Teh <jteh@mozilla.com>
2019-05-30 14:49:40 +0000

    Bug 1555362: Fix build bustage with --disable-accessibility (introduced in bug 1543282). r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D33119

--- dom/ipc/BrowserBridgeParent.h
+++ dom/ipc/BrowserBridgeParent.h
@@ -42,6 +42,7 @@ class BrowserBridgeParent : public PBrowserBridgeParent {
   // Get our manager actor.
   BrowserParent* Manager();
 
+#if defined(ACCESSIBILITY)
   /**
    * Get the accessible for this iframe's embedder OuterDocAccessible.
    * This returns the actor for the containing document and the unique id of
@@ -51,6 +52,7 @@ class BrowserBridgeParent : public PBrowserBridgeParent {
     return Tuple<a11y::DocAccessibleParent*, uint64_t>(mEmbedderAccessibleDoc,
                                                        mEmbedderAccessibleID);
   }
+#endif  // defined(ACCESSIBILITY)
 
   // Tear down this BrowserBridgeParent.
   void Destroy();
@@ -93,8 +95,10 @@ class BrowserBridgeParent : public PBrowserBridgeParent {
   ~BrowserBridgeParent();
 
   RefPtr<BrowserParent> mBrowserParent;
+#if defined(ACCESSIBILITY)
   RefPtr<a11y::DocAccessibleParent> mEmbedderAccessibleDoc;
   uint64_t mEmbedderAccessibleID;
+#endif  // defined(ACCESSIBILITY)
   bool mIPCOpen;
 };
 
