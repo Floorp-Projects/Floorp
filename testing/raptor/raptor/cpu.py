c232c624f1ed82d318d719b5bae2ad770e4e7499
Chris Hartjes <chartjes@mozilla.com>
2019-05-30 19:33:08 +0000

    Bug 1544470 - Added in code that can be used to take a snapshot of CPU usage on Android devices; r=rwood
    Differential Revision: https://phabricator.services.mozilla.com/D27954

---
+++ testing/raptor/raptor/cpu.py
@@ -0,0 +1,56 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import absolute_import
+
+
+def get_app_cpu_usage(raptor):
+    # If we don't find the browser running, we default to 0 usage
+    cpu_usage = 0
+    app_name = raptor.config['binary']
+    verbose = raptor.device._verbose
+    raptor.device._verbose = False
+
+    '''
+    There are two ways to get CPU usage information:
+
+    1. By using the 'top' command and parsing details
+    2. By using 'dumpsys cpuinfo' and parsing the details
+
+    'top' is our first choice if it is available but the
+    parameters we use are only available in Android 8 or
+    greater, otherwise we fall back to using dumpsys
+    '''
+    if raptor.device.version >= 8:
+        cpuinfo = raptor.device.shell_output("top -O %CPU -n 1").split("\n")
+        raptor.device._verbose = verbose
+        for line in cpuinfo:
+            # 14781 u0_a83         0 92.8  12.4  64:53.04 org.mozilla.geckoview_example
+            data = line.split()
+            if data[-1] == app_name:
+                cpu_usage = float(data[3])
+    else:
+        cpuinfo = raptor.device.shell_output("dumpsys cpuinfo | grep %s" % app_name).split("\n")
+        for line in cpuinfo:
+            # 34% 14781/org.mozilla.geckoview_example: 26% user + 7.5% kernel
+            data = line.split()
+            cpu_usage = float(data[0].strip('%'))
+
+    return cpu_usage
+
+
+def generate_android_cpu_profile(raptor, test_name):
+    if not raptor.device or not raptor.config['cpu_test']:
+        return
+
+    result = get_app_cpu_usage(raptor)
+
+    cpuinfo_data = {
+        u'type': u'cpu',
+        u'test': test_name,
+        u'unit': u'%',
+        u'values': {
+            u'browser_cpu_usage': result
+        }
+    }
+    raptor.control_server.submit_supporting_data(cpuinfo_data)
