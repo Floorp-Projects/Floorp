0af2d00190b68b46bb3e4a6bc3f04d169dd7cac5
Nico Grunbaum <na-g@nostrum.com>
2019-05-30 04:44:02 +0000

    Bug 1553011 - update import of Rust SDP parser - r=drno
    update import of Rust SDP parser
    
    Differential Revision: https://phabricator.services.mozilla.com/D31943

--- media/webrtc/signaling/src/sdp/rsdparsa/src/network.rs
+++ media/webrtc/signaling/src/sdp/rsdparsa/src/network.rs
@@ -1,19 +1,18 @@
-use std::str::FromStr;
 use std::fmt;
 use std::net::IpAddr;
+use std::str::FromStr;
 
 use error::SdpParserInternalError;
 
-#[derive(Clone,Copy,Debug,PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum SdpAddrType {
     IP4 = 4,
     IP6 = 6,
 }
 
 impl SdpAddrType {
-    pub fn same_protocol(&self, addr: &IpAddr) -> bool {
-        (addr.is_ipv6() && *self == SdpAddrType::IP6) ||
-        (addr.is_ipv4() && *self == SdpAddrType::IP4)
+    pub fn same_protocol(self, addr: &IpAddr) -> bool {
+        (addr.is_ipv6() && self == SdpAddrType::IP6) || (addr.is_ipv4() && self == SdpAddrType::IP4)
     }
 }
 
@@ -29,7 +28,9 @@ impl fmt::Display for SdpAddrType {
 
 pub fn parse_nettype(value: &str) -> Result<(), SdpParserInternalError> {
     if value.to_uppercase() != "IN" {
-        return Err(SdpParserInternalError::Generic("nettype needs to be IN".to_string()));
+        return Err(SdpParserInternalError::Generic(
+            "nettype needs to be IN".to_string(),
+        ));
     };
     Ok(())
 }
@@ -45,13 +46,14 @@ fn test_parse_nettype() {
 
 pub fn parse_addrtype(value: &str) -> Result<SdpAddrType, SdpParserInternalError> {
     Ok(match value.to_uppercase().as_ref() {
-           "IP4" => SdpAddrType::IP4,
-           "IP6" => SdpAddrType::IP6,
-           _ => {
-               return Err(SdpParserInternalError::Generic("address type needs to be IP4 or IP6"
-                                                              .to_string()))
-           }
-       })
+        "IP4" => SdpAddrType::IP4,
+        "IP6" => SdpAddrType::IP6,
+        _ => {
+            return Err(SdpParserInternalError::Generic(
+                "address type needs to be IP4 or IP6".to_string(),
+            ));
+        }
+    })
 }
 
 #[test]
