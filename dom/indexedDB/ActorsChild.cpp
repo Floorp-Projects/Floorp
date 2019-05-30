24d75f68a90f4c2b428408ffa3223bbda43877d1
Tom Tung <shes050117@gmail.com>
2019-05-30 02:53:07 +0000

    Bug 1539219 - Get RefPtr for IDBOpenDBRequest before using it; r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D31440

--- dom/indexedDB/ActorsChild.cpp
+++ dom/indexedDB/ActorsChild.cpp
@@ -2391,9 +2391,14 @@ mozilla::ipc::IPCResult BackgroundVersionChangeTransactionChild::RecvComplete(
     database->Close();
   }
 
+  RefPtr<IDBOpenDBRequest> request = mOpenDBRequest;
+  MOZ_ASSERT(request);
+
   mTransaction->FireCompleteOrAbortEvents(aResult);
 
-  mOpenDBRequest->SetTransaction(nullptr);
+  request->SetTransaction(nullptr);
+  request = nullptr;
+
   mOpenDBRequest = nullptr;
 
   NoteComplete();
