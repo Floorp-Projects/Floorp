f7e1325ca1e15eef5ce96c37a87143fe6e371f0b
Mike Shal <mshal@mozilla.com>
2019-05-31 23:56:16 +0000

    Bug 1553065 - Move profile-use mozconfig info into common mozconfigs; r=firefox-build-system-reviewers,chmanchester a=RyanVM
    The 3-tier PGO builds used a separate mozconfig called 'profile-use' for
    the final tier. This created a problem when it rode to beta, since the
    same mozconfig was used for all trees, which meant we ended up with
    nightly branding on beta builds.
    
    With the PGO-enabling logic in common mozconfigs, we can enable it by
    setting the MOZ_PGO_PROFILE_USE environment variable from the task
    definition. All of the final-tier PGO builds now use the nightly, beta,
    etc mozconfigs like before, so branding should be intact.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33172

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_pgo.py
+++
@@ -1,5 +0,0 @@
-config = {
-    'stage_platform': 'android-aarch64-pgo',
-    'src_mozconfig': 'mobile/android/config/mozconfigs/android-aarch64/profile-use',
-    'multi_locale_config_platform': 'android',
-}
