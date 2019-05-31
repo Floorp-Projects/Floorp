db0bfee98565d52cf401e2b33d3193b3718c43d8
Dan Minor <dminor@mozilla.com>
2019-05-31 20:24:35 +0000

    Bug 1552755 - Update video_capture/windows code to tip of webrtc.org; r=ng
    This updates just video_capture/windows to the tip of webrtc.org, from commit
    8581877121f58435d683c2a39a73e72e5dcd558a.
    
    This removes the locking which was leading to the deadlock in Bug 1552755. It
    also removes the reliance upon the DirectShow example code which lead to our
    local implementations of BaseFilter, BaseInputPin, BasePin, and MediaType.
    These can now be removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33336

--- media/webrtc/trunk/webrtc/modules/video_capture/windows/sink_filter_ds.h
+++ media/webrtc/trunk/webrtc/modules/video_capture/windows/sink_filter_ds.h
@@ -11,95 +11,152 @@
 #ifndef MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_SINK_FILTER_DS_H_
 #define MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_SINK_FILTER_DS_H_
 
-#include "modules/video_capture/video_capture_defines.h"
-#include "BaseInputPin.h"
-#include "BaseFilter.h"
-#include "MediaType.h"
+#include <dshow.h>
 
-namespace webrtc
-{
-namespace videocapturemodule
-{
-//forward declaration
+#include <atomic>
+#include <memory>
+#include <vector>
 
+#include "modules/video_capture/video_capture_impl.h"
+#include "modules/video_capture/windows/help_functions_ds.h"
+#include "rtc_base/thread_annotations.h"
+#include "rtc_base/thread_checker.h"
+
+namespace webrtc {
+namespace videocapturemodule {
+// forward declarations
 class CaptureSinkFilter;
-/**
- *	input pin for camera input
- *
- */
-class CaptureInputPin: public mozilla::media::BaseInputPin
-{
-public:
-    int32_t _moduleId;
 
-    VideoCaptureCapability _requestedCapability;
-    VideoCaptureCapability _resultingCapability;
-    HANDLE _threadHandle;
+// Input pin for camera input
+// Implements IMemInputPin, IPin.
+class CaptureInputPin : public IMemInputPin, public IPin {
+ public:
+  CaptureInputPin(CaptureSinkFilter* filter);
 
-    CaptureInputPin(int32_t moduleId,
-                    IN TCHAR* szName,
-                    IN CaptureSinkFilter* pFilter,
-                    IN mozilla::CriticalSection * pLock,
-                    OUT HRESULT * pHr,
-                    IN LPCWSTR pszName);
-    virtual ~CaptureInputPin();
+  HRESULT SetRequestedCapability(const VideoCaptureCapability& capability);
 
-    HRESULT GetMediaType (IN int iPos, OUT mozilla::media::MediaType * pmt);
-    HRESULT CheckMediaType (IN const mozilla::media::MediaType * pmt);
-    STDMETHODIMP Receive (IN IMediaSample *);
-    HRESULT SetMatchingMediaType(const VideoCaptureCapability& capability);
+  // Notifications from the filter.
+  void OnFilterActivated();
+  void OnFilterDeactivated();
+
+ protected:
+  virtual ~CaptureInputPin();
+
+ private:
+  CaptureSinkFilter* Filter() const;
+
+  HRESULT AttemptConnection(IPin* receive_pin, const AM_MEDIA_TYPE* media_type);
+  std::vector<AM_MEDIA_TYPE*> DetermineCandidateFormats(
+      IPin* receive_pin,
+      const AM_MEDIA_TYPE* media_type);
+  void ClearAllocator(bool decommit);
+  HRESULT CheckDirection(IPin* pin) const;
+
+  // IUnknown
+  STDMETHOD(QueryInterface)(REFIID riid, void** ppv) override;
+
+  // clang-format off
+  // clang isn't sure what to do with the longer STDMETHOD() function
+  // declarations.
+
+  // IPin
+  STDMETHOD(Connect)(IPin* receive_pin,
+                     const AM_MEDIA_TYPE* media_type) override;
+  STDMETHOD(ReceiveConnection)(IPin* connector,
+                               const AM_MEDIA_TYPE* media_type) override;
+  STDMETHOD(Disconnect)() override;
+  STDMETHOD(ConnectedTo)(IPin** pin) override;
+  STDMETHOD(ConnectionMediaType)(AM_MEDIA_TYPE* media_type) override;
+  STDMETHOD(QueryPinInfo)(PIN_INFO* info) override;
+  STDMETHOD(QueryDirection)(PIN_DIRECTION* pin_dir) override;
+  STDMETHOD(QueryId)(LPWSTR* id) override;
+  STDMETHOD(QueryAccept)(const AM_MEDIA_TYPE* media_type) override;
+  STDMETHOD(EnumMediaTypes)(IEnumMediaTypes** types) override;
+  STDMETHOD(QueryInternalConnections)(IPin** pins, ULONG* count) override;
+  STDMETHOD(EndOfStream)() override;
+  STDMETHOD(BeginFlush)() override;
+  STDMETHOD(EndFlush)() override;
+  STDMETHOD(NewSegment)(REFERENCE_TIME start, REFERENCE_TIME stop,
+                        double rate) override;
+
+  // IMemInputPin
+  STDMETHOD(GetAllocator)(IMemAllocator** allocator) override;
+  STDMETHOD(NotifyAllocator)(IMemAllocator* allocator, BOOL read_only) override;
+  STDMETHOD(GetAllocatorRequirements)(ALLOCATOR_PROPERTIES* props) override;
+  STDMETHOD(Receive)(IMediaSample* sample) override;
+  STDMETHOD(ReceiveMultiple)(IMediaSample** samples, long count,
+                             long* processed) override;
+  STDMETHOD(ReceiveCanBlock)() override;
+  // clang-format on
+
+  rtc::ThreadChecker main_checker_;
+  rtc::ThreadChecker capture_checker_;
+
+  VideoCaptureCapability requested_capability_ RTC_GUARDED_BY(main_checker_);
+  // Accessed on the main thread when Filter()->IsStopped() (capture thread not
+  // running), otherwise accessed on the capture thread.
+  VideoCaptureCapability resulting_capability_;
+  DWORD capture_thread_id_ = 0;
+  rtc::scoped_refptr<IMemAllocator> allocator_ RTC_GUARDED_BY(main_checker_);
+  rtc::scoped_refptr<IPin> receive_pin_ RTC_GUARDED_BY(main_checker_);
+  std::atomic_bool flushing_{false};
+  std::atomic_bool runtime_error_{false};
+  // Holds a referenceless pointer to the owning filter, the name and
+  // direction of the pin. The filter pointer can be considered const.
+  PIN_INFO info_ = {};
+  AM_MEDIA_TYPE media_type_ RTC_GUARDED_BY(main_checker_) = {};
 };
 
-class CaptureSinkFilter: public mozilla::media::BaseFilter
-{
+// Implement IBaseFilter (including IPersist and IMediaFilter).
+class CaptureSinkFilter : public IBaseFilter {
+ public:
+  CaptureSinkFilter(VideoCaptureImpl* capture_observer);
 
-public:
-    CaptureSinkFilter(IN TCHAR * tszName,
-                      IN LPUNKNOWN punk,
-                      OUT HRESULT * phr,
-                      VideoCaptureExternal& captureObserver,
-                      int32_t moduleId);
-    virtual ~CaptureSinkFilter();
+  HRESULT SetRequestedCapability(const VideoCaptureCapability& capability);
 
-    //  --------------------------------------------------------------------
-    //  class methods
+  // Called on the capture thread.
+  void ProcessCapturedFrame(unsigned char* buffer,
+                            size_t length,
+                            const VideoCaptureCapability& frame_info);
 
-    void ProcessCapturedFrame(unsigned char* pBuffer, int32_t length,
-                              const VideoCaptureCapability& frameInfo);
-    //  explicit receiver lock aquisition and release
-    void LockReceive()  { m_crtRecv.Enter();}
-    void UnlockReceive() {m_crtRecv.Leave();}
-    //  explicit filter lock aquisition and release
-    void LockFilter() {m_crtFilter.Enter();}
-    void UnlockFilter() { m_crtFilter.Leave(); }
-    void SetFilterGraph(IGraphBuilder* graph); // Used if EVR
+  void NotifyEvent(long code, LONG_PTR param1, LONG_PTR param2);
+  bool IsStopped() const;
 
-    //  --------------------------------------------------------------------
-    //  COM interfaces
-    STDMETHODIMP QueryInterface(REFIID aIId, void **aInterface)
-    {
-      return mozilla::media::BaseFilter::QueryInterface(aIId, aInterface);
-    }
+  //  IUnknown
+  STDMETHOD(QueryInterface)(REFIID riid, void** ppv) override;
 
-    STDMETHODIMP SetMatchingMediaType(const VideoCaptureCapability& capability);
+  // IPersist
+  STDMETHOD(GetClassID)(CLSID* clsid) override;
 
-    //  --------------------------------------------------------------------
-    //  CBaseFilter methods
-    int GetPinCount ();
-    mozilla::media::BasePin * GetPin ( IN int Index);
-    STDMETHODIMP Pause ();
-    STDMETHODIMP Stop ();
-    STDMETHODIMP GetClassID ( OUT CLSID * pCLSID);
-    //  --------------------------------------------------------------------
-    //  class factory calls this
-    static IUnknown * CreateInstance (IN LPUNKNOWN punk, OUT HRESULT * phr);
-private:
-    mozilla::CriticalSection m_crtFilter; //  filter lock
-    mozilla::CriticalSection m_crtRecv;  //  receiver lock; always acquire before filter lock
-    CaptureInputPin * m_pInput;
-    VideoCaptureExternal& _captureObserver;
-    int32_t _moduleId;
+  // IMediaFilter.
+  STDMETHOD(GetState)(DWORD msecs, FILTER_STATE* state) override;
+  STDMETHOD(SetSyncSource)(IReferenceClock* clock) override;
+  STDMETHOD(GetSyncSource)(IReferenceClock** clock) override;
+  STDMETHOD(Pause)() override;
+  STDMETHOD(Run)(REFERENCE_TIME start) override;
+  STDMETHOD(Stop)() override;
+
+  // IBaseFilter
+  STDMETHOD(EnumPins)(IEnumPins** pins) override;
+  STDMETHOD(FindPin)(LPCWSTR id, IPin** pin) override;
+  STDMETHOD(QueryFilterInfo)(FILTER_INFO* info) override;
+  STDMETHOD(JoinFilterGraph)(IFilterGraph* graph, LPCWSTR name) override;
+  STDMETHOD(QueryVendorInfo)(LPWSTR* vendor_info) override;
+
+ protected:
+  virtual ~CaptureSinkFilter();
+
+ private:
+  rtc::ThreadChecker main_checker_;
+  const rtc::scoped_refptr<ComRefCount<CaptureInputPin>> input_pin_;
+  VideoCaptureImpl* const capture_observer_;
+  FILTER_INFO info_ RTC_GUARDED_BY(main_checker_) = {};
+  // Set/cleared in JoinFilterGraph. The filter must be stopped (no capture)
+  // at that time, so no lock is required. While the state is not stopped,
+  // the sink will be used from the capture thread.
+  IMediaEventSink* sink_ = nullptr;
+  FILTER_STATE state_ RTC_GUARDED_BY(main_checker_) = State_Stopped;
 };
 }  // namespace videocapturemodule
 }  // namespace webrtc
-#endif // MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_SINK_FILTER_DS_H_
+#endif  // MODULES_VIDEO_CAPTURE_MAIN_SOURCE_WINDOWS_SINK_FILTER_DS_H_
