d461133a5f621419b9cb04b57f7b1231fd92d75a
Mark Striemer <mstriemer@mozilla.com>
2019-05-31 22:19:50 +0000

    Bug 1551213 - Update commands on show in HTML about:addons r=mixedpuppy,kmag a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D33304
    
    --HG--
    extra : rebase_source : 01aaded25f24eaae4a93be72071d3ad26500e142
    extra : source : a55afb0346770d95e0d978538ce79023597df785
    extra : histedit_source : 66dc107669e383407aadcc15cf39303fd8cb1196

--- toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
@@ -824,3 +824,42 @@ add_task(async function testPermissions() {
   info("Check permissions for add-on without permission messages");
   await runTest("addon2@mochi.test");
 });
+
+// When the back button is used, its disabled state will be updated. If it
+// isn't updated when showing a view, then it will be disabled on the next
+// use (bug 1551213) if the last use caused it to become disabled.
+add_task(async function testGoBackButton() {
+  // Make sure the list view is the first loaded view so you cannot go back.
+  Services.prefs.setCharPref(PREF_UI_LASTCATEGORY, "addons://list/extension");
+
+  let id = "addon1@mochi.test";
+  let win = await loadInitialView("extension");
+  let doc = win.document;
+  let backButton = win.managerWindow.document.getElementById("go-back");
+
+  let loadDetailView = () => {
+    let loaded = waitForViewLoad(win);
+    getAddonCard(doc, id).querySelector("[action=expand]").click();
+    return loaded;
+  };
+
+  let checkBackButtonState = () => {
+    is_element_visible(backButton, "Back button is visible on the detail page");
+    ok(!backButton.disabled, "Back button is enabled");
+  };
+
+  // Load the detail view, first time should be fine.
+  await loadDetailView();
+  checkBackButtonState();
+
+  // Use the back button directly to pop from history and trigger its disabled
+  // state to be updated.
+  let loaded = waitForViewLoad(win);
+  backButton.click();
+  await loaded;
+
+  await loadDetailView();
+  checkBackButtonState();
+
+  await closeView(win);
+});
