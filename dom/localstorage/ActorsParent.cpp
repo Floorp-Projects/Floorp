e7f226ff859a3a23b8febf6a0f11cbb029f128a5
Jan Varga <jan.varga@gmail.com>
2019-06-03 15:32:59 +0200

    Bug 1534431 - Part 2: Disallow LS requests with undefined client id; r=asuth a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D33595
    
    --HG--
    extra : rebase_source : 597fc6e39aa2e39211a5abf5b28e413546a620b9
    extra : source : 7fcdc0352853923694d7a5ae6e18e8e02e3ca571

--- dom/localstorage/ActorsParent.cpp
+++ dom/localstorage/ActorsParent.cpp
@@ -3154,28 +3154,38 @@ void RequestAllowToCloseIf(P aPredicate) {
   databases.Clear();
 }
 
-bool VerifyPrincipalInfo(const Maybe<ContentParentId>& aContentParentId,
-                         const PrincipalInfo& aPrincipalInfo,
-                         const PrincipalInfo& aStoragePrincipalInfo,
-                         const Maybe<nsID>& aClientId) {
+bool VerifyPrincipalInfo(const PrincipalInfo& aPrincipalInfo,
+                         const PrincipalInfo& aStoragePrincipalInfo) {
   AssertIsOnBackgroundThread();
 
   if (NS_WARN_IF(!QuotaManager::IsPrincipalInfoValid(aPrincipalInfo))) {
     return false;
   }
 
-  if (gClientValidation && aClientId.isSome()) {
+  if (NS_WARN_IF(!StoragePrincipalHelper::
+                     VerifyValidStoragePrincipalInfoForPrincipalInfo(
+                         aStoragePrincipalInfo, aPrincipalInfo))) {
+    return false;
+  }
+
+  return true;
+}
+
+bool VerifyClientId(const Maybe<ContentParentId>& aContentParentId,
+                    const PrincipalInfo& aPrincipalInfo,
+                    const Maybe<nsID>& aClientId) {
+  AssertIsOnBackgroundThread();
+
+  if (gClientValidation) {
+    if (NS_WARN_IF(aClientId.isNothing())) {
+      return false;
+    }
+
     RefPtr<ClientManagerService> svc = ClientManagerService::GetInstance();
     if (svc && NS_WARN_IF(!svc->HasWindow(aContentParentId, aPrincipalInfo,
                                           aClientId.ref()))) {
       return false;
     }
-  }
-
-  if (NS_WARN_IF(!StoragePrincipalHelper::
-                     VerifyValidStoragePrincipalInfoForPrincipalInfo(
-                         aStoragePrincipalInfo, aPrincipalInfo))) {
-    return false;
   }
 
   return true;
@@ -6408,9 +6418,8 @@ bool LSRequestBase::VerifyRequestParams() {
       const LSRequestCommonParams& params =
           mParams.get_LSRequestPreloadDatastoreParams().commonParams();
 
-      if (NS_WARN_IF(
-              !VerifyPrincipalInfo(mContentParentId, params.principalInfo(),
-                                   params.storagePrincipalInfo(), Nothing()))) {
+      if (NS_WARN_IF(!VerifyPrincipalInfo(params.principalInfo(),
+                                          params.storagePrincipalInfo()))) {
         return false;
       }
 
@@ -6428,9 +6437,15 @@ bool LSRequestBase::VerifyRequestParams() {
 
       const LSRequestCommonParams& commonParams = params.commonParams();
 
-      if (NS_WARN_IF(!VerifyPrincipalInfo(
-              mContentParentId, commonParams.principalInfo(),
-              commonParams.storagePrincipalInfo(), params.clientId()))) {
+      if (NS_WARN_IF(
+              !VerifyPrincipalInfo(commonParams.principalInfo(),
+                                   commonParams.storagePrincipalInfo()))) {
+        return false;
+      }
+
+      if (NS_WARN_IF(!VerifyClientId(mContentParentId,
+                                     commonParams.principalInfo(),
+                                     params.clientId()))) {
         return false;
       }
 
@@ -6446,9 +6461,13 @@ bool LSRequestBase::VerifyRequestParams() {
       const LSRequestPrepareObserverParams& params =
           mParams.get_LSRequestPrepareObserverParams();
 
-      if (NS_WARN_IF(!VerifyPrincipalInfo(
-              mContentParentId, params.principalInfo(),
-              params.storagePrincipalInfo(), params.clientId()))) {
+      if (NS_WARN_IF(!VerifyPrincipalInfo(params.principalInfo(),
+                                          params.storagePrincipalInfo()))) {
+        return false;
+      }
+
+      if (NS_WARN_IF(!VerifyClientId(mContentParentId, params.principalInfo(),
+                                     params.clientId()))) {
         return false;
       }
 
@@ -8109,9 +8128,8 @@ bool LSSimpleRequestBase::VerifyRequestParams() {
       const LSSimpleRequestPreloadedParams& params =
           mParams.get_LSSimpleRequestPreloadedParams();
 
-      if (NS_WARN_IF(
-              !VerifyPrincipalInfo(mContentParentId, params.principalInfo(),
-                                   params.storagePrincipalInfo(), Nothing()))) {
+      if (NS_WARN_IF(!VerifyPrincipalInfo(params.principalInfo(),
+                                          params.storagePrincipalInfo()))) {
         return false;
       }
 
