72f367554423736cdd4385328e5db26bfe6a10f5
Nika Layzell <nika@thelayzells.com>
2019-06-03 20:04:00 +0000

    Bug 1554785 - Part 1: Support dynamic remote types for GetMaxProcessCount, r=farre
    This allows the # of processes to be specified for remote types like
    "webIsolated", used by fission windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32762

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3309,6 +3309,9 @@ pref("dom.ipc.processCount.privilegedabout", 1);
 // to avoid multiple of these content processes
 pref("dom.ipc.processCount.privilegedmozilla", 1);
 
+// Isolated content processes are always one-per-origin.
+pref("dom.ipc.processCount.webIsolated", 1);
+
 // Keep a single privileged about process alive for performance reasons.
 // e.g. we do not want to throw content processes out every time we navigate
 // away from about:newtab.
