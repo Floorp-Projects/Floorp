8a66f6bfd35cc8ef775cbe8d2e5681511bc7f088
Tom Tung <shes050117@gmail.com>
2019-06-03 16:22:43 +0000

    Bug 944918 - Allow thumbs.db in the storage directory; r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D33453

--- dom/quota/test/unit/test_allowListFiles.js
+++ dom/quota/test/unit/test_allowListFiles.js
@@ -4,7 +4,7 @@
  */
 
 /**
- * This test is mainly to verify thoes unexpected files are in the white list of
+ * This test is mainly to verify thoes unexpected files are in the allow list of
  * QuotaManager. They aren't expected in the repository but if there are,
  * QuotaManager shouldn't fail to initialize an origin and getting usage, though
  * those files aren't managed by QuotaManager.
@@ -12,15 +12,16 @@
 
 async function testSteps()
 {
-  const whiteListFiles = [
+  const allowListFiles = [
     ".dot-file",
     "desktop.ini",
     "Desktop.ini",
-    "Thumbs.db"
+    "Thumbs.db",
+    "thumbs.db"
   ];
 
-  for (let whiteListFile of whiteListFiles) {
-    info("Testing " + whiteListFile + " in the repository");
+  for (let allowListFile of allowListFiles) {
+    info("Testing " + allowListFile + " in the repository");
 
     info("Creating unknown file");
 
@@ -28,7 +29,7 @@ async function testSteps()
       let dirPath =
         dir == "persistenceType dir" ? "storage/default/"
                                      : "storage/default/http+++example.com/";
-        let file = getRelativeFile(dirPath + whiteListFile);
+        let file = getRelativeFile(dirPath + allowListFile);
         file.create(Ci.nsIFile.NORMAL_FILE_TYPE, parseInt("0644", 8));
     }
 
