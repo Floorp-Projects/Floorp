7c048f1c6d130934dd37587f84f359bda87dbb79
Bryan Kok <bryan.wyern1@gmail.com>
2019-06-03 15:03:05 +0000

    Bug 1519314 - Enter should create a new line, Ctrl-Enter should execute the input when 'devtools.webconsole.input.editor' is true. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D32612
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/components/JSTerm.js
+++ devtools/client/webconsole/components/JSTerm.js
@@ -195,6 +195,22 @@ class JSTerm extends Component {
           return "CodeMirror.Pass";
         };
 
+        const onCtrlCmdEnter = () => {
+          const hasSuggestion = this.hasAutocompletionSuggestion();
+          if (!hasSuggestion &&
+            !Debugger.isCompilableUnit(this.getInputValueBeforeCursor())) {
+            // incomplete statement
+            return "CodeMirror.Pass";
+          }
+
+          if (hasSuggestion) {
+            return this.acceptProposedCompletion();
+          }
+
+          this.execute();
+          return null;
+        };
+
         this.editor = new Editor({
           autofocus: true,
           enableCodeFolding: false,
@@ -220,9 +236,15 @@ class JSTerm extends Component {
                 return this.acceptProposedCompletion();
               }
 
-              this.execute();
-              return null;
+              if (!this.props.editorMode) {
+                this.execute();
+                return null;
+              }
+              return "CodeMirror.Pass";
             },
+
+            "Cmd-Enter": onCtrlCmdEnter,
+            "Ctrl-Enter": onCtrlCmdEnter,
 
             "Tab": () => {
               if (this.hasEmptyInput()) {
@@ -928,7 +950,29 @@ class JSTerm extends Component {
         event.preventDefault();
       }
 
+      // control-enter should execute the current input if codeMirror
+      // is not enabled but editor mode is enabled.
+      if (event.keyCode === KeyCodes.DOM_VK_RETURN) {
+        if (this.hasAutocompletionSuggestion()) {
+          this.acceptProposedCompletion();
+        } else if (this.props.editorMode) {
+          this.execute();
+        }
+        event.preventDefault();
+      }
+
       return;
+    } else if (event.metaKey && Services.appinfo.OS === "Darwin") {
+      // cmd-enter should execute the current input if codeMirror
+      // is not enabled but editor mode is enabled.
+      if (event.keyCode === KeyCodes.DOM_VK_RETURN) {
+        if (this.hasAutocompletionSuggestion()) {
+          this.acceptProposedCompletion();
+        } else if (this.props.editorMode) {
+          this.execute();
+        }
+        event.preventDefault();
+      }
     } else if (event.keyCode == KeyCodes.DOM_VK_RETURN) {
       if (!this.autocompletePopup.isOpen && (
         event.shiftKey || !Debugger.isCompilableUnit(this._getValue())
@@ -950,8 +994,10 @@ class JSTerm extends Component {
       case KeyCodes.DOM_VK_RETURN:
         if (this.hasAutocompletionSuggestion()) {
           this.acceptProposedCompletion();
-        } else {
+        } else if (!this.props.editorMode) {
           this.execute();
+        } else {
+          this.insertStringAtCursor("\n");
         }
         event.preventDefault();
         break;
