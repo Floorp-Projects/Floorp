5caa6d2cdb17ebbf0f98865d3507f9384551a350
Micah Tigley <mtigley@mozilla.com>
2019-06-03 20:24:02 +0000

    Bug 1554695 - RDM reports incorrect angle when rotated. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D33546

--- devtools/client/responsive.html/components/App.js
+++ devtools/client/responsive.html/components/App.js
@@ -275,12 +275,7 @@ class App extends PureComponent {
     }
 
     const currentAngle = Services.prefs.getIntPref("devtools.responsive.viewport.angle");
-    // TODO: For Firefox Android, when the device is rotated clock-wise, the angle is
-    // updated to 270 degrees. This is valid since the user-agent determines whether it
-    // will be set to 90 or 270. However, we should update the angle based on the how the
-    // user-agent assigns the angle value.
-    // See https://w3c.github.io/screen-orientation/#dfn-screen-orientation-values-table
-    const angleToRotateTo = currentAngle === 270 ? 0 : 270;
+    const angleToRotateTo = currentAngle === 90 ? 0 : 90;
     const orientation = getOrientation(currentDevice, viewport, angleToRotateTo);
 
     this.onChangeViewportOrientation(id, orientation);
