fab9b786ac26115e220d371088aad7e3922ca9c3
Gerald Squelart <gsquelart@mozilla.com>
2019-06-04 06:55:46 +0000

    Bug 1492121 - Use BaseProfiler in nsBrowserApp.cpp's main() - r=njn
    Start using BaseProfiler in Firefox main(), before&after XPCOM runs.
    
    Also added a BaseProfiler label around Gecko Profiler init/shutdown (so that
    samples may be ignored if user is only interested in non-XPCOM profiling).
    
    Main process name changed to "Main Thread (Base Profiler)", so as not to confuse
    the front-end, and show where this thread comes from.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31933
    
    --HG--
    extra : moz-landing-system : lando

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -38,6 +38,7 @@
 #include "mozilla/Sprintf.h"
 #include "mozilla/StartupTimeline.h"
 #include "mozilla/WindowsDllBlocklist.h"
+#include "BaseProfiler.h"
 
 #ifdef LIBFUZZER
 #  include "FuzzerDefs.h"
@@ -238,6 +239,9 @@ uint32_t gBlocklistInitFlags = eDllBlocklistInitFlagDefault;
 
 int main(int argc, char* argv[], char* envp[]) {
   mozilla::TimeStamp start = mozilla::TimeStamp::Now();
+
+  AUTO_BASE_PROFILER_INIT;
+  AUTO_BASE_PROFILER_LABEL("nsBrowserApp main", OTHER);
 
 #ifdef MOZ_BROWSER_CAN_BE_CONTENTPROC
   // We are launching as a content process, delegate to the appropriate
