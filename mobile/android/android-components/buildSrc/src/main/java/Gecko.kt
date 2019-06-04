91cddf356cc78fb879d181ffc7d0c970152e95a9
Sebastian Kaspari <s.kaspari@gmail.com>
2019-06-04 16:45:46 +0200

    [components] Update GeckoView Beta to 68.0.20190604110028.

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "68.0.20190603093516"
+    const val beta_version = "68.0.20190604110028"
 
     /**
      * GeckoView Release Version.
