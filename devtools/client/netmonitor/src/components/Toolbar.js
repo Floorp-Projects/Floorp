858cf4d57243c500b02f426a10d189c827332a82
Tim Nguyen <ntim.bugs@gmail.com>
2019-06-04 12:53:16 +0000

    Bug 1551870 - Reintroduce netmonitor-toolbar-container in single-toolbar mode so React doesn't re-render the toolbar when switching panels. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D33616
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/netmonitor/src/components/Toolbar.js
+++ devtools/client/netmonitor/src/components/Toolbar.js
@@ -389,23 +389,22 @@ class Toolbar extends Component {
     // Render the entire toolbar.
     // dock at bottom or dock at side has different layout
     return singleRow ? (
-      span({
-        id: "netmonitor-main-toolbar",
-        className: "devtools-toolbar devtools-input-toolbar",
-      },
-        this.renderClearButton(clearRequests),
-        this.renderSeparator(),
-        this.renderFilterBox(setRequestFilterText),
-        this.renderSeparator(),
-        this.renderToggleRecordingButton(recording, toggleRecording),
-        this.renderSeparator(),
-        this.renderFilterButtons(requestFilterTypes),
-        this.renderSeparator(),
-        this.renderPersistlogCheckbox(persistentLogsEnabled, togglePersistentLogs),
-        this.renderCacheCheckbox(browserCacheDisabled, toggleBrowserCache),
-        this.renderSeparator(),
-        this.renderThrottlingMenu(),
-        this.renderHarButton(),
+      span({ id: "netmonitor-toolbar-container" },
+        span({ className: "devtools-toolbar devtools-input-toolbar" },
+          this.renderClearButton(clearRequests),
+          this.renderSeparator(),
+          this.renderFilterBox(setRequestFilterText),
+          this.renderSeparator(),
+          this.renderToggleRecordingButton(recording, toggleRecording),
+          this.renderSeparator(),
+          this.renderFilterButtons(requestFilterTypes),
+          this.renderSeparator(),
+          this.renderPersistlogCheckbox(persistentLogsEnabled, togglePersistentLogs),
+          this.renderCacheCheckbox(browserCacheDisabled, toggleBrowserCache),
+          this.renderSeparator(),
+          this.renderThrottlingMenu(),
+          this.renderHarButton(),
+        )
       )
     ) : (
       span({ id: "netmonitor-toolbar-container" },
