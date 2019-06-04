dc310fd6124c1d04f26e4f2f329a2ca7d6d156c5
Andrew Swan <aswan@mozilla.com>
2019-06-03 21:10:14 -0700

    Bug 1555060 Rewrite webextension tabs.move tests r=kmag
    These tests are full of too many problems to catalog here.
    This patch just rewrites them to make them less insane.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34647

--- browser/components/extensions/test/browser/browser_ext_tabs_move_window.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_move_window.js
@@ -3,54 +3,78 @@
 "use strict";
 
 add_task(async function() {
-  await BrowserTestUtils.openNewForegroundTab(gBrowser, "http://example.net/");
-  let window1 = await BrowserTestUtils.openNewBrowserWindow();
-  await BrowserTestUtils.openNewForegroundTab(window1.gBrowser, "http://example.com/");
-  let window2 = await BrowserTestUtils.openNewBrowserWindow({private: true});
-  await BrowserTestUtils.openNewForegroundTab(window2.gBrowser, "http://example.com/");
-
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       "permissions": ["tabs"],
     },
     incognitoOverride: "spanning",
     async background() {
-      let tabs = await browser.tabs.query({url: "<all_urls>"});
-      let destination = tabs[0];
-      let source = tabs[1]; // skip over about:blank in window1
-      let privateTab = tabs[2];
-      browser.test.assertTrue(privateTab.incognito, "have a private tab.");
+      const URL = "http://example.com/";
+      let mainWindow = await browser.windows.getCurrent();
+      let newWindow = await browser.windows.create({
+        url: [URL, URL],
+      });
+      let privateWindow = await browser.windows.create({
+        incognito: true,
+        url: [URL, URL],
+      });
 
       browser.tabs.onUpdated.addListener(() => {
         // Bug 1398272: Adding onUpdated listener broke tab IDs across windows.
       });
 
+      let tab = newWindow.tabs[0].id;
+      let privateTab = privateWindow.tabs[0].id;
+
       // Assuming that this windowId does not exist.
       await browser.test.assertRejects(
-        browser.tabs.move(source.id, {windowId: 123144576, index: 0}),
+        browser.tabs.move(tab, {windowId: 123144576, index: 0}),
         /Invalid window/,
         "Should receive invalid window error");
 
       // Test that a tab cannot be moved to a private window.
-      let moved = await browser.tabs.move(source.id, {windowId: privateTab.windowId, index: 0});
+      let moved = await browser.tabs.move(tab, {windowId: privateWindow.id, index: 0});
       browser.test.assertEq(moved.length, 0, "tab was not moved to private window");
       // Test that a private tab cannot be moved to a non-private window.
-      moved = await browser.tabs.move(privateTab.id, {windowId: source.windowId, index: 0});
+      moved = await browser.tabs.move(privateTab,
+                                      {windowId: newWindow.id, index: 0});
       browser.test.assertEq(moved.length, 0, "tab was not moved from private window");
 
       // Verify tabs did not move between windows via another query.
-      let tabs2 = await browser.tabs.query({url: "<all_urls>"});
-      for (let i = 0; i < 3; i++) {
-        browser.test.assertEq(tabs2[i].windowId, tabs[i].windowId, "tab was not moved to another window");
-        browser.test.assertEq(tabs2[i].incognito, tabs[i].incognito, "tab privateness matches.");
+      let windows = await browser.windows.getAll({populate: true});
+      let newWin2 = windows.find(w => w.id === newWindow.id);
+      browser.test.assertTrue(newWin2, "Found window");
+      browser.test.assertEq(newWin2.tabs.length, 2, "Window still has two tabs");
+      for (let origTab of newWindow.tabs) {
+        browser.test.assertTrue(newWin2.tabs.find(t => t.id === origTab.id),
+                                `Window still has tab ${origTab.id}`);
       }
 
-      browser.tabs.move(source.id, {windowId: destination.windowId, index: 0});
+      let privateWin2 = windows.find(w => w.id === privateWindow.id);
+      browser.test.assertTrue(privateWin2 !== null, "Found private window");
+      browser.test.assertEq(privateWin2.incognito, true,
+                            "Private window is still private");
+      browser.test.assertEq(privateWin2.tabs.length, 2,
+                            "Private window still has two tabs");
+      for (let origTab of privateWindow.tabs) {
+        browser.test.assertTrue(privateWin2.tabs.find(t => t.id === origTab.id),
+                                `Private window still has tab ${origTab.id}`);
+      }
 
-      tabs = await browser.tabs.query({url: "<all_urls>"});
-      browser.test.assertEq(tabs[0].url, "http://example.com/");
-      browser.test.assertEq(tabs[0].windowId, destination.windowId);
-      browser.test.assertEq(tabs[0].id, source.id);
+      // Move a tab from one non-private window to another
+      await browser.tabs.move(tab, {windowId: mainWindow.id, index: 0});
+
+      mainWindow = await browser.windows.get(mainWindow.id, {populate: true});
+      browser.test.assertTrue(mainWindow.tabs.find(t => t.id === tab),
+                              "Moved tab is in main window");
+
+      newWindow = await browser.windows.get(newWindow.id, {populate: true});
+      browser.test.assertEq(newWindow.tabs.length, 1, "New window has 1 tab left");
+      browser.test.assertTrue(newWindow.tabs[0].id != tab, "Moved tab is no longer in original window");
+
+      await browser.windows.remove(newWindow.id);
+      await browser.windows.remove(privateWindow.id);
+      await browser.tabs.remove(tab);
 
       browser.test.notifyPass("tabs.move.window");
     },
@@ -59,12 +83,6 @@ add_task(async function() {
   await extension.startup();
   await extension.awaitFinish("tabs.move.window");
   await extension.unload();
-
-  for (let tab of window.gBrowser.tabs) {
-    BrowserTestUtils.removeTab(tab);
-  }
-  await BrowserTestUtils.closeWindow(window1);
-  await BrowserTestUtils.closeWindow(window2);
 });
 
 add_task(async function test_currentWindowAfterTabMoved() {
