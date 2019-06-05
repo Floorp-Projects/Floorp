2593ff9abba98860634329e9c70d0cf7765f1145
Steve Fink <sfink@mozilla.com>
2019-06-05 18:17:58 +0000

    Bug 1556430 - Weakkeys iterator invalidation when severing a delegate in the same zone r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D33727
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/gc/WeakMap-inl.h
+++ js/src/gc/WeakMap-inl.h
@@ -251,8 +251,7 @@ bool WeakMap<K, V>::markEntries(GCMarker* marker) {
 }
 
 template <class K, class V>
-void WeakMap<K, V>::postSeverDelegate(GCMarker* marker, gc::Cell* key,
-                                      Compartment* comp) {
+void WeakMap<K, V>::postSeverDelegate(GCMarker* marker, gc::Cell* key) {
   if (IsMarked(markColor)) {
     // We only stored the delegate, not the key, and we're severing the
     // delegate from the key. So store the key.
