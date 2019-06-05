55692f7ae48bb43e333db8f1b3ad90011acfda61
Aki Sasaki <asasaki@mozilla.com>
2019-06-05 15:02:25 -0700

    Bug 1471004 - mac-signing transform logic + config. r=tomprince, a=jcristau
    This moves mac signing tasks to the notarization/iscript pool. It also
    adds support for pkg installers and notarization.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33857

--- taskcluster/taskgraph/transforms/task.py
+++ taskcluster/taskgraph/transforms/task.py
@@ -877,6 +877,11 @@ def build_generic_worker_payload(config, task, task_def):
         # Signing formats to use on each of the paths
         Required('formats'): [basestring],
     }],
+
+    # behavior for mac iscript
+    Optional('mac-behavior'): Any(
+        "mac_notarize", "mac_sign", "mac_sign_and_pkg", "mac_pkg",
+    ),
 })
 def build_scriptworker_signing_payload(config, task, task_def):
     worker = task['worker']
@@ -885,15 +890,16 @@ def build_scriptworker_signing_payload(config, task, task_def):
         'maxRunTime': worker['max-run-time'],
         'upstreamArtifacts':  worker['upstream-artifacts']
     }
-
+    if worker.get('mac-behavior'):
+        task_def['payload']['behavior'] = worker['mac-behavior']
     artifacts = set(task.get('release-artifacts', []))
     for upstream_artifact in worker['upstream-artifacts']:
         for path in upstream_artifact['paths']:
             artifacts.update(get_signed_artifacts(
                 input=path,
                 formats=upstream_artifact['formats'],
+                behavior=worker.get('mac-behavior'),
             ))
-
     task['release-artifacts'] = list(artifacts)
 
 
--- taskcluster/taskgraph/util/signed_artifacts.py
+++ taskcluster/taskgraph/util/signed_artifacts.py
@@ -99,13 +99,16 @@ def _strip_widevine_for_partners(artifacts_specifications):
     return artifacts_specifications
 
 
-def get_signed_artifacts(input, formats):
+def get_signed_artifacts(input, formats, behavior=None):
     """
     Get the list of signed artifacts for the given input and formats.
     """
     artifacts = set()
     if input.endswith('.dmg'):
-        artifacts.add(input.replace('.dmg', '.tar.gz'))
+        if behavior != "mac_pkg":
+            artifacts.add(input.replace('.dmg', '.tar.gz'))
+        if behavior and behavior != "mac_sign":
+            artifacts.add(input.replace('.dmg', '.pkg'))
     else:
         artifacts.add(input)
     if 'autograph_gpg' in formats:
