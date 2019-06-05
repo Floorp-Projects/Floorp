6adcddf12bb4172b5e19cd74be29c1d4454735ce
Edwin Gao <egao@mozilla.com>
2019-06-05 20:26:36 +0000

    Bug 1555458 - require --full for android-hw job scheduling with fuzzy r=jmaher,gbrown
    Changes:
    - require `--full` keyword for `./mach try fuzzy` in order to schedule android-hw jobs to hopefully reduce the backlog
    
    Differential Revision: https://phabricator.services.mozilla.com/D33834

--- tools/tryselect/selectors/fuzzy.py
+++ tools/tryselect/selectors/fuzzy.py
@@ -26,7 +26,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 # or uncommon enough that they should only be selectable with --full.
 TARGET_TASK_FILTERS = (
     '.*-ccov\/.*',
-    'windows10-aarch64/opt.*'
+    'windows10-aarch64/opt.*',
+    'android-hw.*'
 )
 
 
