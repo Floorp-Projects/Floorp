d4fe439b0c9d8fefdd3450b953e44b6a827c6670
Jonathan Watt <jwatt@jwatt.org>
2019-06-06 11:12:21 -0700

    Bug 1559096. Rename SVGObserverUtils::mInObserverList. r=longsonr
    Differential Revision: https://phabricator.services.mozilla.com/D34852

--- layout/svg/SVGObserverUtils.h
+++ layout/svg/SVGObserverUtils.h
@@ -103,7 +103,7 @@ class SVGRenderingObserver : public nsStubMutationObserver {
  public:
   typedef mozilla::dom::Element Element;
 
-  SVGRenderingObserver() : mInObserverList(false) {}
+  SVGRenderingObserver() : mInObserverSet(false) {}
 
   // nsIMutationObserver
   NS_DECL_NSIMUTATIONOBSERVER_ATTRIBUTECHANGED
@@ -162,8 +162,8 @@ class SVGRenderingObserver : public nsStubMutationObserver {
   void DebugObserverSet();
 #endif
 
-  // Whether we're in our referenced element's observer list at this time.
-  bool mInObserverList;
+  // Whether we're in our observed element's observer set at this time.
+  bool mInObserverSet;
 };
 
 class SVGObserverUtils {
