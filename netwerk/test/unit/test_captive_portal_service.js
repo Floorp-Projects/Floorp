965fd54792dd5eb4a4635e5a0967f71a5e6061f5
Valentin Gosu <valentin.gosu@gmail.com>
2019-06-06 12:08:35 +0000

    Bug 1556259 - Make sure CaptiveDetect.jsm interprets any redirect as a locked captive portal r=mayhemer
    The problem with CaptiveDetect was that it uses an XMLHttpRequest, and
    apparently xhr.status is 0 for failed requests, which here includes cert
    errors, redirect loops, etc.
    Getting the XHR to not follow redirects was tricky, so a hacky fix was to
    set nsIHttpChannel.redirectionLimit = 0;
    For any redirect the XHR would now fail with NS_ERROR_REDIRECT_LOOP, which
    we need to handle separately.
    
    I also included tests for:
      * redirect to https with invalid cert
      * redirect to same URL causing redirect loop
      * redirect to different URL with different content
      * redirect to different URL with canonical content
    All of these cases should be detected as locked captive portals.
    
    Differential Revision: https://phabricator.services.mozilla.com/D33706

--- netwerk/test/unit/test_captive_portal_service.js
+++ netwerk/test/unit/test_captive_portal_service.js
@@ -2,7 +2,7 @@
 
 const {HttpServer} = ChromeUtils.import("resource://testing-common/httpd.js");
 
-var httpserver = null;
+let httpserver = null;
 XPCOMUtils.defineLazyGetter(this, "cpURI", function() {
   return "http://localhost:" + httpserver.identity.primaryPort + "/captive.txt";
 });
@@ -22,13 +22,20 @@ const PREF_CAPTIVE_MINTIME = "network.captive-portal-service.minInterval";
 const PREF_CAPTIVE_MAXTIME = "network.captive-portal-service.maxInterval";
 const PREF_DNS_NATIVE_IS_LOCALHOST = "network.dns.native-is-localhost";
 
-registerCleanupFunction(() => {
+const cps = Cc["@mozilla.org/network/captive-portal-service;1"]
+              .getService(Ci.nsICaptivePortalService);
+
+registerCleanupFunction(async () => {
   Services.prefs.clearUserPref(PREF_CAPTIVE_ENABLED);
   Services.prefs.clearUserPref(PREF_CAPTIVE_TESTMODE);
   Services.prefs.clearUserPref(PREF_CAPTIVE_ENDPOINT);
   Services.prefs.clearUserPref(PREF_CAPTIVE_MINTIME);
   Services.prefs.clearUserPref(PREF_CAPTIVE_MAXTIME);
   Services.prefs.clearUserPref(PREF_DNS_NATIVE_IS_LOCALHOST);
+
+  await new Promise(resolve => {
+    httpserver.stop(resolve);
+  });
 });
 
 function observerPromise(topic) {
@@ -60,8 +67,6 @@ add_task(function setup(){
 
 add_task(async function test_simple()
 {
-  var cps = Cc["@mozilla.org/network/captive-portal-service;1"]
-              .getService(Ci.nsICaptivePortalService);
   Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
 
   equal(cps.state, Ci.nsICaptivePortalService.UNKNOWN);
@@ -88,9 +93,95 @@ add_task(async function test_simple()
   equal(cps.state, Ci.nsICaptivePortalService.UNLOCKED_PORTAL);
 });
 
-add_task(async function shutdown()
+// This test redirects to another URL which returns the same content.
+// It should still be interpreted as a captive portal.
+add_task(async function test_redirect_success()
 {
-  await new Promise(resolve => {
-    httpserver.stop(resolve);
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
+  equal(cps.state, Ci.nsICaptivePortalService.UNKNOWN);
+
+  httpserver.registerPathHandler("/succ.txt", (metadata, response) => {
+    response.setHeader("Content-Type", "text/plain");
+    response.bodyOutputStream.write(cpResponse, cpResponse.length);
   });
+  httpserver.registerPathHandler("/captive.txt", (metadata, response) => {
+    response.setStatusLine(metadata.httpVersion, 307, "Moved Temporarily");
+    response.setHeader("Location", `http://localhost:${httpserver.identity.primaryPort}/succ.txt`);
+  });
+
+  let notification = observerPromise("captive-portal-login").then(() => "login");
+  let succNotif = observerPromise("network:captive-portal-connectivity").then(() => "connectivity");
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, true);
+
+  let winner = await Promise.race([notification, succNotif]);
+  equal(winner, "login", "This should have been a login, not a success");
+  equal(cps.state, Ci.nsICaptivePortalService.LOCKED_PORTAL, "Should be locked after redirect to same text");
+});
+
+// This redirects to another URI with a different content.
+// We check that it triggers a captive portal login
+add_task(async function test_redirect_bad() {
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
+  equal(cps.state, Ci.nsICaptivePortalService.UNKNOWN);
+
+  httpserver.registerPathHandler("/bad.txt", (metadata, response) => {
+    response.setHeader("Content-Type", "text/plain");
+    response.bodyOutputStream.write("bad", "bad".length);
+  });
+
+  httpserver.registerPathHandler("/captive.txt", (metadata, response) => {
+    response.setStatusLine(metadata.httpVersion, 307, "Moved Temporarily");
+    response.setHeader("Location", `http://localhost:${httpserver.identity.primaryPort}/bad.txt`);
+  });
+
+  let notification = observerPromise("captive-portal-login");
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, true);
+
+  await notification;
+  equal(cps.state, Ci.nsICaptivePortalService.LOCKED_PORTAL, "Should be locked after redirect to bad text");
+});
+
+// This redirects to the same URI.
+// We check that it triggers a captive portal login
+add_task(async function test_redirect_loop() {
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
+  equal(cps.state, Ci.nsICaptivePortalService.UNKNOWN);
+
+  // This is actually a redirect loop
+  httpserver.registerPathHandler("/captive.txt", (metadata, response) => {
+    response.setStatusLine(metadata.httpVersion, 307, "Moved Temporarily");
+    response.setHeader("Location", cpURI);
+  });
+
+  let notification = observerPromise("captive-portal-login");
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, true);
+
+  await notification;
+  equal(cps.state, Ci.nsICaptivePortalService.LOCKED_PORTAL);
+});
+
+// This redirects to a https URI.
+// We check that it triggers a captive portal login
+add_task(async function test_redirect_https() {
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
+  equal(cps.state, Ci.nsICaptivePortalService.UNKNOWN);
+
+  let h2Port = Cc["@mozilla.org/process/environment;1"]
+                 .getService(Ci.nsIEnvironment)
+                 .get("MOZHTTP2_PORT");
+  Assert.notEqual(h2Port, null);
+  Assert.notEqual(h2Port, "");
+
+  // Any kind of redirection should trigger the captive portal login.
+  httpserver.registerPathHandler("/captive.txt", (metadata, response) => {
+    response.setStatusLine(metadata.httpVersion, 307, "Moved Temporarily");
+    response.setHeader("Location", `https://foo.example.com:${h2Port}/exit`);
+  });
+
+  let notification = observerPromise("captive-portal-login");
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, true);
+
+  await notification;
+  equal(cps.state, Ci.nsICaptivePortalService.LOCKED_PORTAL, "Should be locked after redirect to https");
+  Services.prefs.setBoolPref(PREF_CAPTIVE_ENABLED, false);
 });
