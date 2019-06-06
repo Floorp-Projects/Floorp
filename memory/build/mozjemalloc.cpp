a93df69b63aa168cab578b84c3da032ced52cc7e
Mike Hommey <mh@glandium.org>
2019-06-06 12:25:02 +0300

    Bug 997353 - Make powerpc not use static page sizes in mozjemalloc. r=njn a=jcristau
    Summary: __powerpc__ covers powerpc and powerpc64.
    
    Reviewers: njn
    
    Reviewed By: njn
    
    Bug #: 997353
    
    Differential Revision: https://phabricator.services.mozilla.com/D33750

--- memory/build/mozjemalloc.cpp
+++ memory/build/mozjemalloc.cpp
@@ -182,7 +182,7 @@ using namespace mozilla;
 // Debug builds are opted out too, for test coverage.
 #ifndef MOZ_DEBUG
 #  if !defined(__ia64__) && !defined(__sparc__) && !defined(__mips__) && \
-      !defined(__aarch64__)
+      !defined(__aarch64__) && !defined(__powerpc__)
 #    define MALLOC_STATIC_PAGESIZE 1
 #  endif
 #endif
