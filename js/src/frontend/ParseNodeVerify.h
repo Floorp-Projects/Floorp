21f38f93d16da854762420c7f87aea68a9bd654b
Jason Orendorff <jorendorff@mozilla.com>
2019-06-07 20:34:05 +0000

    Bug 1547561 - Part 3: Additional CheckParseTree pass, enabled in debug builds only. r=khyperia
    This is an experiment. We'll see if the fuzzers find anything. If nothing else,
    it's good to have the invariants of the ParseNode data structure documented in
    code, as assertions, separate from the parser and emitter code.
    
    We can add more checks to this as we go. It may not be fast enough to leave on
    in release builds, even in Nightly, and I don't want to push my luck. So
    DEBUG-only for now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32245
    
    --HG--
    extra : moz-landing-system : lando

---
+++ js/src/frontend/ParseNodeVerify.h
@@ -0,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef frontend_ParseNodeVerify_h
+#define frontend_ParseNodeVerify_h
+
+#include "jsutil.h"                       // JS_CRASH_DIAGNOSTICS
+#include "ds/LifoAlloc.h"                 // LifoAlloc
+#include "frontend/ParseNode.h"           // ParseNode
+#include "frontend/SyntaxParseHandler.h"  // SyntaxParseHandler::Node
+
+namespace js {
+namespace frontend {
+
+// In most builds, examine the given ParseNode and crash if it's not
+// well-formed. (In late beta and shipping builds of Firefox, this does
+// nothing.)
+//
+// This returns true on success, and false only if we hit the recursion limit.
+// If the ParseNode is actually bad, we crash.
+
+#ifdef DEBUG
+extern MOZ_MUST_USE bool CheckParseTree(JSContext* cx, const LifoAlloc& alloc,
+                                        ParseNode* pn);
+#else
+inline MOZ_MUST_USE bool CheckParseTree(JSContext* cx, const LifoAlloc& alloc,
+                                        ParseNode* pn) {
+  return true;
+}
+#endif
+
+inline MOZ_MUST_USE bool CheckParseTree(JSContext* cx, const LifoAlloc& alloc,
+                                        SyntaxParseHandler::Node pn) {
+  return true;
+}
+
+} /* namespace frontend */
+} /* namespace js */
+
+#endif  // frontend_ParseNodeVerify_h
