0eb0ec2b107ea5646b895de48e33db78e4609599
Mark Striemer <mstriemer@mozilla.com>
2019-06-07 23:16:57 +0000

    Bug 1525185 - Telemetry for HTML about:addons r=mixedpuppy,rpl
    Differential Revision: https://phabricator.services.mozilla.com/D31726

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -1062,6 +1062,12 @@ class AddonDetails extends HTMLElement {
     if (e.type == "view-changed" && e.target == this.deck) {
       switch (this.deck.selectedViewName) {
         case "release-notes":
+          AMTelemetry.recordActionEvent({
+            object: "aboutAddons",
+            view: getTelemetryViewName(this),
+            action: "releaseNotes",
+            addon: this.addon,
+          });
           let releaseNotes = this.querySelector("update-release-notes");
           let uri = this.releaseNotesUri;
           if (uri) {
@@ -1147,15 +1153,10 @@ class AddonDetails extends HTMLElement {
       description.appendChild(nl2br(addon.fullDescription));
     }
 
-    // Contribute.
-    if (!addon.contributionURL) {
-      this.querySelector(".addon-detail-contribute").remove();
-    }
-
-    // Auto updates setting.
-    if (!hasPermission(addon, "upgrade")) {
-      this.querySelector(".addon-detail-row-updates").remove();
-    }
+    this.querySelector(".addon-detail-contribute").hidden =
+      !addon.contributionURL;
+    this.querySelector(".addon-detail-row-updates").hidden =
+      !hasPermission(addon, "upgrade");
 
     let pbRow = this.querySelector(".addon-detail-row-private-browsing");
     if (!allowPrivateBrowsingByDefault && addon.type == "extension" &&
@@ -1167,26 +1168,25 @@ class AddonDetails extends HTMLElement {
       learnMore.href = SUPPORT_URL + "extensions-pb";
     } else {
       // Remove the help row, which is right after the settings.
-      pbRow.nextElementSibling.remove();
+      pbRow.nextElementSibling.hidden = true;
       // Then remove the actual settings.
-      pbRow.remove();
+      pbRow.hidden = true;
     }
 
     // Author.
     let creatorRow = this.querySelector(".addon-detail-row-author");
     if (addon.creator) {
-      let creator;
-      if (addon.creator.url) {
-        creator = document.createElement("a");
-        creator.href = addon.creator.url;
-        creator.target = "_blank";
-        creator.textContent = addon.creator.name;
+      let link = creatorRow.querySelector("a");
+      link.hidden = !addon.creator.url;
+      if (link.hidden) {
+        creatorRow.appendChild(new Text(addon.creator.name));
       } else {
-        creator = new Text(addon.creator.name);
+        link.href = addon.creator.url;
+        link.target = "_blank";
+        link.textContent = addon.creator.name;
       }
-      creatorRow.appendChild(creator);
     } else {
-      creatorRow.remove();
+      creatorRow.hidden = true;
     }
 
     // Version. Don't show a version for LWTs.
@@ -1194,7 +1194,7 @@ class AddonDetails extends HTMLElement {
     if (addon.version && !/@personas\.mozilla\.org/.test(addon.id)) {
       version.appendChild(new Text(addon.version));
     } else {
-      version.remove();
+      version.hidden = true;
     }
 
     // Last updated.
@@ -1207,7 +1207,7 @@ class AddonDetails extends HTMLElement {
       });
       updateDate.appendChild(new Text(lastUpdated));
     } else {
-      updateDate.remove();
+      updateDate.hidden = true;
     }
 
     // Homepage.
@@ -1217,7 +1217,7 @@ class AddonDetails extends HTMLElement {
       homepageURL.href = addon.homepageURL;
       homepageURL.textContent = addon.homepageURL;
     } else {
-      homepageRow.remove();
+      homepageRow.hidden = true;
     }
 
     // Rating.
@@ -1230,7 +1230,7 @@ class AddonDetails extends HTMLElement {
         numberOfReviews: addon.reviewCount,
       });
     } else {
-      ratingRow.remove();
+      ratingRow.hidden = true;
     }
 
     this.update();
@@ -1327,6 +1327,7 @@ class AddonCard extends HTMLElement {
     if (e.type == "click") {
       switch (action) {
         case "toggle-disabled":
+          this.recordActionEvent(addon.userDisabled ? "enable" : "disable");
           if (addon.userDisabled) {
             if (shouldShowPermissionsPrompt(addon)) {
               await showPermissionsPrompt(addon);
@@ -1347,12 +1348,15 @@ class AddonCard extends HTMLElement {
           }
           break;
         case "always-activate":
+          this.recordActionEvent("enable");
           addon.userDisabled = false;
           break;
         case "never-activate":
+          this.recordActionEvent("disable");
           addon.userDisabled = true;
           break;
         case "update-check":
+          this.recordActionEvent("checkForUpdate");
           let listener = {
             onUpdateAvailable(addon, install) {
               attachUpdateHandler(install);
@@ -1377,6 +1381,7 @@ class AddonCard extends HTMLElement {
           this.updateInstall = null;
           break;
         case "contribute":
+          this.recordActionEvent("contribute");
           windowRoot.ownerGlobal.openUILinkIn(addon.contributionURL, "tab", {
             triggeringPrincipal:
               Services.scriptSecurityManager.createNullPrincipal({}),
@@ -1384,8 +1389,10 @@ class AddonCard extends HTMLElement {
           break;
         case "preferences":
           if (getOptionsType(addon) == "tab") {
+            this.recordActionEvent("preferences", "external");
             openOptionsInTab(addon.optionsURL);
           } else if (getOptionsType(addon) == "inline") {
+            this.recordActionEvent("preferences", "inline");
             loadViewFn("detail", this.addon.id, "preferences");
           }
           break;
@@ -1395,6 +1402,8 @@ class AddonCard extends HTMLElement {
             let {
               remove, report,
             } = windowRoot.ownerGlobal.promptRemoveExtension(addon);
+            let value = remove ? "accepted" : "cancelled";
+            this.recordActionEvent("uninstall", value);
             if (remove) {
               await addon.uninstall(true);
               this.sendEvent("remove");
@@ -1425,14 +1434,28 @@ class AddonCard extends HTMLElement {
           // Handle a click on the card itself.
           if (!this.expanded) {
             loadViewFn("detail", this.addon.id);
+          } else if (e.target.localName == "a" &&
+                     e.target.getAttribute("data-telemetry-name")) {
+            let value = e.target.getAttribute("data-telemetry-name");
+            AMTelemetry.recordLinkEvent({
+              object: "aboutAddons",
+              addon,
+              value,
+              extra: {
+                view: getTelemetryViewName(this),
+              },
+            });
           }
           break;
       }
     } else if (e.type == "change") {
       let {name} = e.target;
+      let telemetryValue = e.target.getAttribute("data-telemetry-value");
       if (name == "autoupdate") {
+        this.recordActionEvent("setAddonUpdate", telemetryValue);
         addon.applyBackgroundUpdates = e.target.value;
       } else if (name == "private-browsing") {
+        this.recordActionEvent("privateBrowsingAllowed", telemetryValue);
         let policy = WebExtensionPolicy.getByID(addon.id);
         let extension = policy && policy.extension;
 
@@ -1635,6 +1658,16 @@ class AddonCard extends HTMLElement {
 
   sendEvent(name, detail) {
     this.dispatchEvent(new CustomEvent(name, {detail}));
+  }
+
+  recordActionEvent(action, value) {
+    AMTelemetry.recordActionEvent({
+      object: "aboutAddons",
+      view: getTelemetryViewName(this),
+      action,
+      addon: this.addon,
+      value,
+    });
   }
 }
 customElements.define("addon-card", AddonCard);
@@ -1977,6 +2010,12 @@ class AddonList extends HTMLElement {
     const undo = document.createElement("button");
     undo.setAttribute("action", "undo");
     undo.addEventListener("click", () => {
+      AMTelemetry.recordActionEvent({
+        object: "aboutAddons",
+        view: getTelemetryViewName(this),
+        action: "undo",
+        addon,
+      });
       addon.cancelUninstall();
     });
 
@@ -2603,7 +2642,7 @@ class DiscoveryView {
 }
 
 // Generic view management.
-let root = null;
+let mainEl = null;
 
 /**
  * The name of the view for an element, used for telemetry.
@@ -2620,15 +2659,15 @@ function getTelemetryViewName(el) {
  * Called from extensions.js once, when about:addons is loading.
  */
 function initialize(opts) {
-  root = document.getElementById("main");
+  mainEl = document.getElementById("main");
   loadViewFn = opts.loadViewFn;
   replaceWithDefaultViewFn = opts.replaceWithDefaultViewFn;
   setCategoryFn = opts.setCategoryFn;
   AddonCardListenerHandler.startup();
   window.addEventListener("unload", () => {
-    // Clear out the root node so the disconnectedCallback will trigger
+    // Clear out the main node so the disconnectedCallback will trigger
     // properly and all of the custom elements can cleanup.
-    root.textContent = "";
+    mainEl.textContent = "";
     AddonCardListenerHandler.shutdown();
   }, {once: true});
 }
@@ -2655,10 +2694,10 @@ async function show(type, param) {
   } else {
     throw new Error(`Unknown view type: ${type}`);
   }
-  root.textContent = "";
-  root.appendChild(container);
+  mainEl.textContent = "";
+  mainEl.appendChild(container);
 }
 
 function hide() {
-  root.textContent = "";
+  mainEl.textContent = "";
 }
