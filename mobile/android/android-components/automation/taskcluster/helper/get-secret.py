c4fd31b96303844b893faa34d779f1ea474e57ef
Sebastian Kaspari <s.kaspari@gmail.com>
2019-06-07 12:14:01 +0200

    [components] get-secret.py: Fix shadowing of 'json'.

--- mobile/android/android-components/automation/taskcluster/helper/get-secret.py
+++ mobile/android/android-components/automation/taskcluster/helper/get-secret.py
@@ -8,13 +8,13 @@ import os
 import json
 import taskcluster
 
-def write_secret_to_file(path, data, key, base64decode=False, json=False, append=False, prefix=''):
+def write_secret_to_file(path, data, key, base64decode=False, json_secret=False, append=False, prefix=''):
     path = os.path.join(os.path.dirname(__file__), '../../../' + path)
     with open(path, 'a' if append else 'w') as f:
         value = data['secret'][key]
         if base64decode:
             value = base64.b64decode(value)
-        if json:
+        if json_secret:
             value = json.dumps(value)
         f.write(prefix + value)
 
