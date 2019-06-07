88cb777162b37c7d0d5796d1341af6b3a6980981
Eden Chuang <echuang@mozilla.com>
2019-06-07 08:18:37 +0200

    Bug 1516333 - Part 2: Add LS unit test for uri encoding edge cases; r=janv a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D33617

---
+++ dom/localstorage/test/unit/test_uri_encoding_edge_cases.js
@@ -0,0 +1,60 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+/**
+ * This test verifies that group and origin strings for URIs with special
+ * characters are consistent between calling
+ * EnsureQuotaForOringin/EnsureOriginIsInitailized and GetQuotaObject in
+ * PrepareDatastoreOp, so writing to local storage won't cause a crash because
+ * of a null quota object. See bug 1516333.
+ */
+
+async function testSteps() {
+  /**
+   * The edge cases are specified in this array of origins. Each edge case must
+   * contain two properties uri and path (origin directory path relative to the
+   * profile directory).
+   */
+  const origins = [
+    {
+      uri: "file:///test'.html",
+      path: "storage/default/file++++test'.html",
+    },
+    {
+      uri: "file:///test>.html",
+      path: "storage/default/file++++test%3E.html",
+    },
+  ];
+
+  for (let origin of origins) {
+    const principal = getPrincipal(origin.uri);
+
+    let originDir = getRelativeFile(origin.path);
+
+    info("Checking the origin directory existence");
+
+    ok(!originDir.exists(),
+      `The origin directory ${origin.path} should not exists`);
+
+    info("Getting storage");
+
+    let storage = getLocalStorage(principal);
+
+    info("Adding item");
+
+    storage.setItem("foo", "bar");
+
+    info("Resetting origin");
+
+    // This forces any pending changes to be flushed to disk (including origin
+    // directory creation).
+    let request = resetOrigin(principal);
+    await requestFinished(request);
+
+    info("Checking the origin directory existence");
+
+    ok(originDir.exists(), `The origin directory ${origin.path} should exist`);
+  }
+}
