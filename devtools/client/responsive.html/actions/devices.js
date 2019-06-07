7b689b420538e08f96a6fa06344b554b0c797b65
Micah Tigley <mtigley@mozilla.com>
2019-06-07 00:00:55 +0000

    Bug 1556533 - Fix RDM simulating orientationchange event when the page is refreshed. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D33734

--- devtools/client/responsive.html/actions/devices.js
+++ devtools/client/responsive.html/actions/devices.js
@@ -22,7 +22,7 @@ const { post } = require("../utils/message");
 
 const { addDevice, editDevice, getDevices, removeDevice } = require("devtools/client/shared/devices");
 const { changeUserAgent, toggleTouchSimulation } = require("./ui");
-const { changeDevice, changePixelRatio, changeViewportAngle } = require("./viewports");
+const { changeDevice, changePixelRatio } = require("./viewports");
 
 const DISPLAYED_DEVICES_PREF = "devtools.responsive.html.displayedDeviceList";
 
@@ -222,7 +222,6 @@ module.exports = {
 
       dispatch(changeDevice(id, device.name, deviceType));
       dispatch(changePixelRatio(id, device.pixelRatio));
-      dispatch(changeViewportAngle(id, viewport.angle));
       dispatch(changeUserAgent(device.userAgent));
       dispatch(toggleTouchSimulation(device.touch));
     };
