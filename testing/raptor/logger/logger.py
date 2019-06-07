f3ba95f2a492295f3a15caa479e1d8f3851b37ce
Marian Raiciof <mraiciof@mozilla.com>
2019-06-07 08:33:39 +0000

    Bug 1522604 - Add custom logger class to Raptor r=perftest-reviewers,davehunt
    The new RaptorLogger class will add prefixes to raptor logging messages.
    These changes will help raptor to display the log messages with the right log_level.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32984

---
+++ testing/raptor/logger/logger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+from mozlog.proxy import ProxyLogger
+
+
+class RaptorLogger(ProxyLogger):
+
+    def __init__(self, component=None):
+        self.logger = ProxyLogger(component)
+
+    def exception(self, message):
+        self.critical(message)
+
+    def debug(self, message):
+        return self.logger.debug("Debug: {}".format(message))
+
+    def info(self, message):
+        return self.logger.info("Info: {}".format(message))
+
+    def warning(self, message):
+        return self.logger.warning("Warning: {}".format(message))
+
+    def error(self, message):
+        return self.logger.error("Error: {}".format(message))
+
+    def critical(self, message):
+        return self.logger.critical("Critical: {}".format(message))
+
+    def log_raw(self, message):
+        return self.logger.log_raw(message)
+
+    def process_output(self, *args, **kwargs):
+        return self.logger.process_output(*args, **kwargs)
