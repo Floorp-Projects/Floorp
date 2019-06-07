4d75ffa981a0546ae4f0aba713dd0819b9cc60d3
Michael Kaply <mozilla@kaply.com>
2019-06-07 20:48:11 +0000

    Bug 1553041 - Switch page load mechanism for about policy tests. r=Gijs, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D34188

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_block_about.js
@@ -0,0 +1,38 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+ "use strict";
+
+ const policiesToTest = {
+   "BlockAboutAddons": "about:addons",
+   "BlockAboutConfig": "about:config",
+   "BlockAboutProfiles": "about:profiles",
+   "BlockAboutSupport": "about:support",
+ };
+
+ add_task(async function testAboutTask() {
+   for (let policy in policiesToTest) {
+     await testPageBlockedByPolicy(policy, policiesToTest[policy]);
+   }
+ });
+
+ async function testPageBlockedByPolicy(policy, page) {
+   let policyJSON = {"policies": {}};
+   policyJSON.policies[policy] = true;
+   await setupPolicyEngineWithJson(policyJSON);
+
+   await BrowserTestUtils.withNewTab({ gBrowser, url: "about:blank" }, async (browser) => {
+     BrowserTestUtils.loadURI(browser, page);
+     await BrowserTestUtils.browserLoaded(browser, false, page, true);
+     await ContentTask.spawn(browser, page, async function(innerPage) {
+       ok(content.document.documentURI.startsWith("about:neterror?e=blockedByPolicy"),
+          content.document.documentURI + " should start with about:neterror?e=blockedByPolicy");
+
+       // There is currently a testing-specific race condition that causes this test
+       // to fail, but it is not a problem if we test after the first page load.
+       // Until the race condition is fixed, just make sure to test this *after*
+       // testing the page load.
+       is(Services.policies.isAllowed(innerPage), false,
+          `Policy Engine should report ${innerPage} as not allowed`);
+     });
+   });
+ }
