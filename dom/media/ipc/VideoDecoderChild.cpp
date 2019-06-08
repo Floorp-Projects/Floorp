e997ba00fb98188ab5de1d1aaa54b551f10b8061
Michael Froman <mfroman@mozilla.com>
2019-06-08 14:14:53 +0000

    Bug 1557827 - pass mDiscardPadding from MediaRawData to remote decoder. r=jya
    Opus decoding on RDD will fail dom/media/webaudio/test/test_decodeOpusTail.html.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34235

--- dom/media/ipc/VideoDecoderChild.cpp
+++ dom/media/ipc/VideoDecoderChild.cpp
@@ -260,7 +260,7 @@ RefPtr<MediaDataDecoder::DecodePromise> VideoDecoderChild::Decode(
   MediaRawDataIPDL sample(
       MediaDataIPDL(aSample->mOffset, aSample->mTime, aSample->mTimecode,
                     aSample->mDuration, aSample->mKeyframe),
-      aSample->mEOS, std::move(buffer));
+      aSample->mEOS, aSample->mDiscardPadding, std::move(buffer));
   SendInput(sample);
   return mDecodePromise.Ensure(__func__);
 }
