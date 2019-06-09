a08929c889af93da9e60efda6770c661a85a74dd
Jan Varga <jan.varga@gmail.com>
2019-06-09 21:46:34 +0200

    Bug 1542572 - Add more temporary logging to LocalStorageShutdownTimeout crash annotation; r=asuth dr=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D34295

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -6594,6 +6594,12 @@ class FactoryOp
   bool HasBlockedDatabases() const { return !mMaybeBlockedDatabases.IsEmpty(); }
 #endif
 
+  const nsCString& Origin() const {
+    AssertIsOnOwningThread();
+
+    return mOrigin;
+  }
+
   bool DatabaseFilePathIsKnown() const {
     AssertIsOnOwningThread();
 
@@ -6608,8 +6614,6 @@ class FactoryOp
   }
 
   void StringifyPersistenceType(nsCString& aResult) const;
-
-  void GetSanitizedOrigin(nsCString& aResult) const;
 
   void StringifyState(nsCString& aResult) const;
 
@@ -8962,23 +8966,6 @@ nsresult RemoveDatabaseFilesAndDirectory(nsIFile* aBaseDirectory,
   }
 
   return NS_OK;
-}
-
-void SanitizeString(nsACString& aOrigin) {
-  char* iter = aOrigin.BeginWriting();
-  char* end = aOrigin.EndWriting();
-
-  while (iter != end) {
-    char c = *iter;
-
-    if (IsAsciiAlpha(c)) {
-      *iter = 'a';
-    } else if (IsAsciiDigit(c)) {
-      *iter = 'D';
-    }
-
-    ++iter;
-  }
 }
 
 /*******************************************************************************
@@ -16353,8 +16340,8 @@ void QuotaClient::ShutdownTimedOut() {
       nsCString persistenceType;
       factoryOp->StringifyPersistenceType(persistenceType);
 
-      nsCString origin;
-      factoryOp->GetSanitizedOrigin(origin);
+      nsCString origin(factoryOp->Origin());
+      SanitizeOrigin(origin);
 
       nsCString state;
       factoryOp->StringifyState(state);
@@ -19158,25 +19145,6 @@ void FactoryOp::StringifyPersistenceType(nsCString& aResult) const {
   PersistenceType persistenceType = mCommonParams.metadata().persistenceType();
 
   PersistenceTypeToText(persistenceType, aResult);
-}
-
-void FactoryOp::GetSanitizedOrigin(nsCString& aResult) const {
-  AssertIsOnOwningThread();
-
-  int32_t colonPos = mOrigin.FindChar(':');
-  if (colonPos >= 0) {
-    const nsACString& prefix = Substring(mOrigin, 0, colonPos);
-
-    nsCString normSuffix(Substring(mOrigin, colonPos));
-    SanitizeString(normSuffix);
-
-    aResult = prefix + normSuffix;
-  } else {
-    nsCString origin(mOrigin);
-    SanitizeString(origin);
-
-    aResult = origin;
-  }
 }
 
 void FactoryOp::StringifyState(nsCString& aResult) const {
