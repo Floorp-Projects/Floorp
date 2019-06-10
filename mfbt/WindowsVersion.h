305b1de6ed75418952edf7be0a70c72866f31786
Aaron Klotz <aklotz@mozilla.com>
2019-06-10 18:52:38 +0000

    Bug 1558272: Add Windows 10 May 2019 Update to WindowsVersion.h; r=dmajor, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D34390

--- mfbt/WindowsVersion.h
+++ mfbt/WindowsVersion.h
@@ -158,6 +158,10 @@ MOZ_ALWAYS_INLINE bool IsWin10Sep2018UpdateOrLater() {
   return IsWindows10BuildOrLater(17763);
 }
 
+MOZ_ALWAYS_INLINE bool IsWin10May2019UpdateOrLater() {
+  return IsWindows10BuildOrLater(18362);
+}
+
 MOZ_ALWAYS_INLINE bool IsNotWin7PreRTM() {
   return IsWin7SP1OrLater() || IsWindowsBuildOrLater(7600);
 }
