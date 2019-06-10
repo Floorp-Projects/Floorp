d3a8e429e1e0d60ad24268b380fda1c696676b80
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2019-06-10 16:25:05 +0000

    Bug 1551801 - Fix the velocity direction on pan gesture events. r=botond
    The gtest in this commit fails without the fix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34245
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/apz/test/gtest/TestSnapping.cpp
+++ gfx/layers/apz/test/gtest/TestSnapping.cpp
@@ -134,3 +134,70 @@ TEST_F(APZCSnappingTester, Snap_After_Pinch) {
 
   apzc->AssertStateIsSmoothScroll();
 }
+
+TEST_F(APZCSnappingTester, Snap_On_Momentum) {
+  SCOPED_GFX_VAR(UseWebRender, bool, false);
+
+  const char* layerTreeSyntax = "c";
+  nsIntRegion layerVisibleRegion[] = {
+      nsIntRegion(IntRect(0, 0, 100, 100)),
+  };
+  root =
+      CreateLayerTree(layerTreeSyntax, layerVisibleRegion, nullptr, lm, layers);
+  SetScrollableFrameMetrics(root, ScrollableLayerGuid::START_SCROLL_ID,
+                            CSSRect(0, 0, 100, 500));
+
+  // Set up some basic scroll snapping
+  ScrollSnapInfo snap;
+  snap.mScrollSnapStrictnessY = StyleScrollSnapStrictness::Mandatory;
+
+  if (StaticPrefs::layout_css_scroll_snap_v1_enabled()) {
+    snap.mSnapPositionY.AppendElement(0 * AppUnitsPerCSSPixel());
+    snap.mSnapPositionY.AppendElement(100 * AppUnitsPerCSSPixel());
+  } else {
+    snap.mScrollSnapIntervalY = Some(100 * AppUnitsPerCSSPixel());
+  }
+
+  ScrollMetadata metadata = root->GetScrollMetadata(0);
+  metadata.SetSnapInfo(ScrollSnapInfo(snap));
+  root->SetScrollMetadata(metadata);
+
+  UniquePtr<ScopedLayerTreeRegistration> registration =
+      MakeUnique<ScopedLayerTreeRegistration>(manager, LayersId{0}, root, mcc);
+  manager->UpdateHitTestingTree(LayersId{0}, root, false, LayersId{0}, 0);
+
+  RefPtr<TestAsyncPanZoomController> apzc = ApzcOf(root);
+
+  TimeStamp now = mcc->Time();
+
+  PanGesture(PanGestureInput::PANGESTURE_START, manager, ScreenIntPoint(50, 80),
+             ScreenPoint(0, 2), now);
+  mcc->AdvanceByMillis(5);
+  apzc->AdvanceAnimations(mcc->Time());
+  PanGesture(PanGestureInput::PANGESTURE_PAN, manager, ScreenIntPoint(50, 80),
+             ScreenPoint(0, 50), mcc->Time());
+  mcc->AdvanceByMillis(5);
+  apzc->AdvanceAnimations(mcc->Time());
+  PanGesture(PanGestureInput::PANGESTURE_END, manager, ScreenIntPoint(50, 80),
+             ScreenPoint(0, 0), mcc->Time());
+  mcc->AdvanceByMillis(5);
+
+  apzc->AdvanceAnimations(mcc->Time());
+  PanGesture(PanGestureInput::PANGESTURE_MOMENTUMSTART, manager,
+             ScreenIntPoint(50, 80), ScreenPoint(0, 200), mcc->Time());
+  mcc->AdvanceByMillis(10);
+  apzc->AdvanceAnimations(mcc->Time());
+  PanGesture(PanGestureInput::PANGESTURE_MOMENTUMPAN, manager,
+             ScreenIntPoint(50, 80), ScreenPoint(0, 50), mcc->Time());
+  mcc->AdvanceByMillis(10);
+  apzc->AdvanceAnimations(mcc->Time());
+  PanGesture(PanGestureInput::PANGESTURE_MOMENTUMEND, manager,
+             ScreenIntPoint(50, 80), ScreenPoint(0, 0), mcc->Time());
+
+  apzc->AdvanceAnimationsUntilEnd();
+  EXPECT_EQ(
+      100.0f,
+      apzc->GetCurrentAsyncScrollOffset(
+              AsyncPanZoomController::AsyncTransformConsumer::eForHitTesting)
+          .y);
+}
