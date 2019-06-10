e686c309ebd2581c90d5d852a8ac196b7657ddaa
dleblanccyr <danielleleb12@gmail.com>
2019-06-10 14:21:55 +0000

    Bug 1555135 - Adds InfoItem web component. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D32894

---
+++ toolkit/components/certviewer/content/components/info-item.js
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+class InfoItem extends HTMLElement {
+  constructor(item) {
+    super();
+    this.item = item;
+  }
+
+  connectedCallback() {
+    let infoItemTemplate = document.getElementById("info-item-template");
+
+    this.attachShadow({mode: "open"})
+        .appendChild(infoItemTemplate.content.cloneNode(true));
+    this.render();
+  }
+
+  render() {
+    let label = this.shadowRoot.querySelector("label");
+    let info = this.shadowRoot.querySelector(".info");
+
+    label.textContent = this.item.label;
+    info.textContent = this.item.info;
+
+    // TODO: Use Fluent-friendly condition.
+    if (this.item.label === "Modulus") {
+      info.classList.add("long-hex");
+    }
+  }
+}
+
+customElements.define("info-item", InfoItem);
