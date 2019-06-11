3c9c195746904de82451f3e90a17ced0ec78b030
Rob Wu <rob@robwu.nl>
2019-06-11 15:30:18 +0000

    Bug 1555012 - Enable HTML about:addons by default r=rpl a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D34337
    
    --HG--
    extra : source : c6d9871972a4308931726939befc0c4fc743534b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5190,7 +5190,7 @@ pref("extensions.webextensions.enablePerformanceCounters", true);
 pref("extensions.webextensions.performanceCountersMaxAge", 5000);
 
 // The HTML about:addons page.
-pref("extensions.htmlaboutaddons.enabled", false);
+pref("extensions.htmlaboutaddons.enabled", true);
 // Whether to allow the inline options browser in HTML about:addons page.
 pref("extensions.htmlaboutaddons.inline-options.enabled", true);
 // Show recommendations on the extension and theme list views.
