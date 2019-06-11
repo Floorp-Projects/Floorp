2181b961d6f423d845697480f4bc6a61e1e0bb50
Rob Wu <rob@robwu.nl>
2019-06-11 21:02:04 +0000

    Bug 1555012 - Enable abuse reporting at about:addons by default r=rpl a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D34530
    
    --HG--
    extra : source : 60f7565db88b534b66a5eb3d7d7d16f4283bd3bd

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2738,7 +2738,7 @@ pref("services.settings.security.onecrl.collection", "onecrl");
 pref("services.settings.security.onecrl.signer", "onecrl.content-signature.mozilla.org");
 pref("services.settings.security.onecrl.checked", 0);
 
-pref("extensions.abuseReport.enabled", false);
+pref("extensions.abuseReport.enabled", true);
 pref("extensions.abuseReport.url", "https://addons.mozilla.org/api/v4/abuse/report/addon/");
 
 // Blocklist preferences
