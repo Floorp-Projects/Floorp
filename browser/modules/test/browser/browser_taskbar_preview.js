31475a361a8393930675a64bf66e7ad5dbae3768
Andrew Swan <aswan@mozilla.com>
2019-06-11 14:49:46 -0700

    Bug 1555060 Stop using dom structure for <tabs>/<tab> relationships
    A bunch of existing code assumes that <tab> elements are the immediate
    and only children of a <tabs> element, and uses dom apis to traverse
    relationships between these elements.  To simplify conversion of <tabs>
    to a custom element (and hopefully improve readability a bit at the same
    time!), introduce new apis:
    
    On <tab>
    this.parentNode -> this.container
    this.nextElementSibling -> this.container.findNextTab(...)
    this.previousElementSibiling -> this.container.findNextTab(...)
    
    On <tabs>
    this.children -> this.allTabs
    
    Differential Revision: https://phabricator.services.mozilla.com/D34648

--- browser/modules/test/browser/browser_taskbar_preview.js
+++ browser/modules/test/browser/browser_taskbar_preview.js
@@ -58,11 +58,11 @@ function test() {
   checkSelectedTab();
 
   // Remove #3 (non active)
-  gBrowser.removeTab(gBrowser.tabContainer.lastElementChild);
+  gBrowser.removeTab(gBrowser.tabs[gBrowser.tabs.length - 1]);
   checkPreviews(2, "Expected number of previews after closing unselected via browser");
 
   // Remove #1 (active)
-  gBrowser.removeTab(gBrowser.tabContainer.firstElementChild);
+  gBrowser.removeTab(gBrowser.tabs[0]);
   checkPreviews(1, "Expected number of previews after closing selected tab via browser");
 
   // Add a new tab
