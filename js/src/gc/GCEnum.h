1373f49120629e9760e6d6904381b619d5ad4dc8
Jon Coppeard <jcoppeard@mozilla.com>
2019-06-11 17:03:42 +0100

    Bug 1395509 - Track malloc memory used by RegExpStatics objects r=pbone
    I wanted to make VectorMatchPairs use ZoneAllocPolicy but this is also used in a bunch of places where it's not attached to a GC thing, so I left this as a todo.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34556

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -109,7 +109,8 @@ enum class ZealMode {
   _(IonScript)                             \
   _(ArgumentsData)                         \
   _(RareArgumentsData)                     \
-  _(FileObjectFile)
+  _(FileObjectFile)                        \
+  _(RegExpStatics)
 
 #define JS_FOR_EACH_MEMORY_USE(_)  \
   JS_FOR_EACH_PUBLIC_MEMORY_USE(_) \
--- js/src/vm/RegExpStatics.cpp
+++ js/src/vm/RegExpStatics.cpp
@@ -24,7 +24,7 @@ static void resc_finalize(FreeOp* fop, JSObject* obj) {
   MOZ_ASSERT(fop->onMainThread());
   RegExpStatics* res =
       static_cast<RegExpStatics*>(obj->as<RegExpStaticsObject>().getPrivate());
-  fop->delete_(res);
+  fop->delete_(obj, res, MemoryUse::RegExpStatics);
 }
 
 static void resc_trace(JSTracer* trc, JSObject* obj) {
@@ -36,7 +36,7 @@ static void resc_trace(JSTracer* trc, JSObject* obj) {
 
 static const ClassOps RegExpStaticsObjectClassOps = {nullptr, /* addProperty */
                                                      nullptr, /* delProperty */
-                                                     nullptr, /* enumerate */
+                                                     nullptr, /* enumreate */
                                                      nullptr, /* newEnumerate */
                                                      nullptr, /* resolve */
                                                      nullptr, /* mayResolve */
@@ -60,7 +60,9 @@ RegExpStaticsObject* RegExpStatics::create(JSContext* cx) {
   if (!res) {
     return nullptr;
   }
-  obj->setPrivate(static_cast<void*>(res));
+  // TODO: This doesn't account for match vector heap memory used if there are
+  // more 10 matches. This is likely to be rare.
+  InitObjectPrivate(obj, res, MemoryUse::RegExpStatics);
   return obj;
 }
 
