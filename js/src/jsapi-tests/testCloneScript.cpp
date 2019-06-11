7db26e7b682b1d4d67ef2bc71a4fee715f3a0d89
Jan de Mooij <jdemooij@mozilla.com>
2019-06-11 10:11:21 +0000

    Bug 1557664 - Add AutoHoldPrincipals RAII class and use it to fix a leak in the JS shell. r=jwalden
    Unfortunately JS_DropPrincipals wants the cx so it's hard to use RefPtr<>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34148
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jsapi-tests/testCloneScript.cpp
+++ js/src/jsapi-tests/testCloneScript.cpp
@@ -79,19 +79,6 @@ struct Principals final : public JSPrincipals {
   }
 };
 
-class AutoDropPrincipals {
-  JSContext* cx;
-  JSPrincipals* principals;
-
- public:
-  AutoDropPrincipals(JSContext* cx, JSPrincipals* principals)
-      : cx(cx), principals(principals) {
-    JS_HoldPrincipals(principals);
-  }
-
-  ~AutoDropPrincipals() { JS_DropPrincipals(cx, principals); }
-};
-
 static void DestroyPrincipals(JSPrincipals* principals) {
   auto p = static_cast<Principals*>(principals);
   delete p;
@@ -100,13 +87,11 @@ static void DestroyPrincipals(JSPrincipals* principals) {
 BEGIN_TEST(test_cloneScriptWithPrincipals) {
   JS_InitDestroyPrincipalsCallback(cx, DestroyPrincipals);
 
-  JSPrincipals* principalsA = new Principals();
-  AutoDropPrincipals dropA(cx, principalsA);
-  JSPrincipals* principalsB = new Principals();
-  AutoDropPrincipals dropB(cx, principalsB);
+  JS::AutoHoldPrincipals principalsA(cx, new Principals());
+  JS::AutoHoldPrincipals principalsB(cx, new Principals());
 
-  JS::RootedObject A(cx, createGlobal(principalsA));
-  JS::RootedObject B(cx, createGlobal(principalsB));
+  JS::RootedObject A(cx, createGlobal(principalsA.get()));
+  JS::RootedObject B(cx, createGlobal(principalsB.get()));
 
   CHECK(A);
   CHECK(B);
@@ -136,7 +121,7 @@ BEGIN_TEST(test_cloneScriptWithPrincipals) {
     JSScript* script;
     CHECK(script = JS_GetFunctionScript(cx, fun));
 
-    CHECK(JS_GetScriptPrincipals(script) == principalsA);
+    CHECK(JS_GetScriptPrincipals(script) == principalsA.get());
     CHECK(obj = JS_GetFunctionObject(fun));
   }
 
@@ -153,7 +138,7 @@ BEGIN_TEST(test_cloneScriptWithPrincipals) {
     JSScript* script;
     CHECK(script = JS_GetFunctionScript(cx, fun));
 
-    CHECK(JS_GetScriptPrincipals(script) == principalsB);
+    CHECK(JS_GetScriptPrincipals(script) == principalsB.get());
 
     JS::RootedValue v(cx);
     JS::RootedValue arg(cx, JS::Int32Value(1));
@@ -165,7 +150,7 @@ BEGIN_TEST(test_cloneScriptWithPrincipals) {
     CHECK(JS_ObjectIsFunction(funobj));
     CHECK(fun = JS_ValueToFunction(cx, v));
     CHECK(script = JS_GetFunctionScript(cx, fun));
-    CHECK(JS_GetScriptPrincipals(script) == principalsB);
+    CHECK(JS_GetScriptPrincipals(script) == principalsB.get());
   }
 
   return true;
