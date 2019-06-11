251b100b757a1660d5ea1860595e9e94d2351368
Will Hawkins <whawkins@mozilla.com>
2019-06-11 14:19:43 +0000

    Bug 1550437: Make sure that About dialog animates the throbber. r=aosmond r=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D34429

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -2570,32 +2570,22 @@ void nsImageFrame::IconLoad::GetPrefs() {
 
 nsresult nsImageFrame::RestartAnimation() {
   nsCOMPtr<imgIRequest> currentRequest = GetCurrentRequest();
-  /*
-   * We cannot count on mContentURLRequestRegistered to make
-   * the deregistration work. So, we are going to force it.
-   */
-  bool deregister = true;
 
-  if (currentRequest && !mContentURLRequestRegistered) {
+  if (currentRequest) {
+    bool deregister = false;
     nsLayoutUtils::RegisterImageRequestIfAnimated(PresContext(), currentRequest,
                                                   &deregister);
-    return currentRequest->StartDecoding(imgIContainer::FLAG_NONE);
   }
   return NS_OK;
 }
 
 nsresult nsImageFrame::StopAnimation() {
   nsCOMPtr<imgIRequest> currentRequest = GetCurrentRequest();
-  /*
-   * We cannot count on mContentURLRequestRegistered to make
-   * the deregistration work. So, we are going to force it.
-   */
-  bool deregister = true;
 
   if (currentRequest) {
+    bool deregister = true;
     nsLayoutUtils::DeregisterImageRequest(PresContext(), currentRequest,
                                           &deregister);
-    return currentRequest->CancelAndForgetObserver(NS_BINDING_ABORTED);
   }
   return NS_OK;
 }
