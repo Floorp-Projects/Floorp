c5f1982901564d3990c6a27b72c8053419ab31c6
Denys M <dector9@gmail.com>
2019-06-11 12:52:31 +0300

    [components] For https://github.com/mozilla-mobile/android-components/issues/1481. Use androidx runner in `lib-fetch-okhttp`.

--- mobile/android/android-components/components/lib/fetch-okhttp/src/test/java/mozilla/components/lib/fetch/okhttp/OkHttpFetchTestCases.kt
+++ mobile/android/android-components/components/lib/fetch-okhttp/src/test/java/mozilla/components/lib/fetch/okhttp/OkHttpFetchTestCases.kt
@@ -4,15 +4,16 @@
 
 package mozilla.components.lib.fetch.okhttp
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.concept.fetch.Client
 import mozilla.components.support.test.robolectric.testContext
+import mozilla.components.tooling.fetch.tests.FetchTestCases
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
-class OkHttpFetchTestCases : mozilla.components.tooling.fetch.tests.FetchTestCases() {
+@RunWith(AndroidJUnit4::class)
+class OkHttpFetchTestCases : FetchTestCases() {
 
     override fun createNewClient(): Client = OkHttpClient(okhttp3.OkHttpClient(), testContext)
 
