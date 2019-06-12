6dbc494407a1c98b4dc810300d54f8693ddc05a6
Jared Wein <jwein@mozilla.com>
2019-06-12 17:46:41 +0000

    Bug 1550137 - Change from aria-hidden to HTML5 hidden. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D34601
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutlogins/tests/browser/browser_openPreferences.js
+++ browser/components/aboutlogins/tests/browser/browser_openPreferences.js
@@ -16,9 +16,9 @@ add_task(async function test_open_preferences() {
   await ContentTask.spawn(browser, null, async () => {
     return ContentTaskUtils.waitForCondition(() => {
       let menuButton = Cu.waiveXrays(content.document.querySelector("menu-button"));
-      return menuButton.shadowRoot
-                       .querySelector(".menu")
-                       .getAttribute("aria-hidden") == "false";
+      return !menuButton.shadowRoot
+                        .querySelector(".menu")
+                        .hidden;
     }, "waiting for menu to open");
   });
 
