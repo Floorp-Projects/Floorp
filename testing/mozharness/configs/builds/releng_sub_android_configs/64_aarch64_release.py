73ab2c5d1c59d9af0d05bf5a53ec5c85e7113e1d
Johan Lorenzo <jlorenzo@mozilla.com>
2019-06-12 18:57:56 +0200

    Bug 1559115 - Build and ship Fennec Release off mozilla-esr68 r=tomprince a=release
    Differential Revision: https://phabricator.services.mozilla.com/D35671
    
    --HG--
    rename : mobile/android/config/mozconfigs/android-aarch64/debug-beta => mobile/android/config/mozconfigs/android-aarch64/debug-release
    rename : mobile/android/config/mozconfigs/android-aarch64/beta => mobile/android/config/mozconfigs/android-aarch64/release
    rename : mobile/android/config/mozconfigs/android-api-16/debug-beta => mobile/android/config/mozconfigs/android-api-16/debug-release
    rename : mobile/android/config/mozconfigs/android-api-16/beta => mobile/android/config/mozconfigs/android-api-16/release
    rename : mobile/android/config/mozconfigs/android-x86/debug-beta => mobile/android/config/mozconfigs/android-x86/debug-release
    rename : mobile/android/config/mozconfigs/android-x86/beta => mobile/android/config/mozconfigs/android-x86/release
    rename : mobile/android/config/mozconfigs/android-x86_64/debug-beta => mobile/android/config/mozconfigs/android-x86_64/debug-release
    rename : mobile/android/config/mozconfigs/android-x86_64/beta => mobile/android/config/mozconfigs/android-x86_64/release
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_beta.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_release.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_beta_debug.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_release_debug.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_beta.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_release.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_beta_debug.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_release_debug.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_beta.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_release.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_beta_debug.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_release_debug.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_beta.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_release.py
    rename : testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_beta_debug.py => testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_release_debug.py
    extra : rebase_source : 26df957e42f659c3aecbae82c1fc4fa8d156f3b6
    extra : amend_source : cba51bbff4eafd589704b5ca5d3e26511a303f29

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_release.py
@@ -0,0 +1,5 @@
+config = {
+    'stage_platform': 'android-aarch64',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-aarch64/release',
+    'multi_locale_config_platform': 'android',
+}
---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_aarch64_release_debug.py
@@ -0,0 +1,6 @@
+config = {
+    'stage_platform': 'android-aarch64-debug',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-aarch64/debug-release',
+    'multi_locale_config_platform': 'android',
+    'debug_build': True,
+}
