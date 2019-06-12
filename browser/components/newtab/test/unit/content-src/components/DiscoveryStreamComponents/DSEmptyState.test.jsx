0ea95615a2ba0b3f01019bd2ef92079770d3dfbc
Ed Lee <edilee@mozilla.com>
2019-06-12 15:16:00 +0000

    Bug 1558441 - Add scrollable scrollbar, try-again button and bug fixes to Activity Stream r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D34611
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/newtab/test/unit/content-src/components/DiscoveryStreamComponents/DSEmptyState.test.jsx
+++ browser/components/newtab/test/unit/content-src/components/DiscoveryStreamComponents/DSEmptyState.test.jsx
@@ -14,9 +14,36 @@ describe("<DSEmptyState>", () => {
     assert.ok(wrapper.find(".section-empty-state").exists());
   });
 
-  it("should render message", () => {
+  it("should render defaultempty state message", () => {
     assert.ok(wrapper.find(".empty-state-message").exists());
     assert.ok(wrapper.find("h2").exists());
     assert.ok(wrapper.find("p").exists());
+  });
+
+  it("should render failed state message", () => {
+    wrapper = shallowWithIntl(<DSEmptyState status="failed" />);
+    assert.ok(wrapper.find("button.try-again-button").exists());
+  });
+
+  it("should render waiting state message", () => {
+    wrapper = shallowWithIntl(<DSEmptyState status="waiting" />);
+    assert.ok(wrapper.find("button.try-again-button.waiting").exists());
+  });
+
+  it("should dispatch DISCOVERY_STREAM_RETRY_FEED on failed state button click", () => {
+    const dispatch = sinon.spy();
+
+    wrapper = shallowWithIntl(<DSEmptyState status="failed" dispatch={dispatch} feed={{url: "https://foo.com", data: {}}} />);
+    wrapper.find("button.try-again-button").simulate("click");
+
+    assert.calledTwice(dispatch);
+    let [action] = dispatch.firstCall.args;
+    assert.equal(action.type, "DISCOVERY_STREAM_FEED_UPDATE");
+    assert.deepEqual(action.data.feed, {url: "https://foo.com", data: {status: "waiting"}});
+
+    [action] = dispatch.secondCall.args;
+
+    assert.equal(action.type, "DISCOVERY_STREAM_RETRY_FEED");
+    assert.deepEqual(action.data.feed, {url: "https://foo.com", data: {}});
   });
 });
