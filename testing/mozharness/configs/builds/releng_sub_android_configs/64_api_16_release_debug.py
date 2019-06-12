ea30ab7c27ebe0e9390c8510d7ac8d4377522985
Johan Lorenzo <jlorenzo@mozilla.com>
2019-06-12 18:57:56 +0200

    Bug 1559115 - Build and ship Fennec Release off mozilla-esr68 r=tomprince a=release
    Differential Revision: https://phabricator.services.mozilla.com/D35671

---
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_release_debug.py
@@ -0,0 +1,6 @@
+config = {
+    'stage_platform': 'android-api-16-debug',
+    'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16/debug-release',
+    'multi_locale_config_platform': 'android',
+    'debug_build': True,
+}
