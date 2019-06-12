4d7d19eebe193188b6ca186056091510a20066ba
Kris Maglione <maglione.k@gmail.com>
2019-06-12 10:48:29 -0700

    Bug 1541557: Part 7 - Convert SpecialPowers to use JSWindowActors rather than framescripts. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D35057

--- toolkit/components/normandy/test/browser/browser_actions_BranchedAddonStudyAction.js
+++ toolkit/components/normandy/test/browser/browser_actions_BranchedAddonStudyAction.js
@@ -616,8 +616,6 @@ decorate_task(
   async function unenrollMissingAddonTest([study], sendEventStub) {
     const action = new BranchedAddonStudyAction();
 
-    SimpleTest.waitForExplicitFinish();
-    SimpleTest.monitorConsole(() => SimpleTest.finish(), [{message: /could not uninstall addon/i}]);
     await action.unenroll(study.recipeId);
 
     sendEventStub.assertEvents(
