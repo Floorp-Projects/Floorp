df64847023a7801b734ab9da1035af870c0690aa
Jon Coppeard <jcoppeard@mozilla.com>
2019-06-12 16:21:18 +0100

    Bug 1395509 - Track malloc memory used by RegExp bytecode r=jandem
    I had to store the length as the first word of the bytecode. I also shuffled RegExpShared fields around to make the class a little smaller.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34726

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -109,7 +109,8 @@ enum class ZealMode {
   _(IonScript)                             \
   _(ArgumentsData)                         \
   _(RareArgumentsData)                     \
-  _(RegExpStatics)
+  _(RegExpStatics)                         \
+  _(RegExpSharedBytecode)
 
 #define JS_FOR_EACH_MEMORY_USE(_)  \
   JS_FOR_EACH_PUBLIC_MEMORY_USE(_) \
