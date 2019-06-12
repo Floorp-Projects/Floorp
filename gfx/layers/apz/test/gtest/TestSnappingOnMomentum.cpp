75a14f624868c3a6c12fa6740421a40554b6189d
Julien Cristau <jcristau@mozilla.com>
2019-06-12 17:35:42 +0200

    Bug 1551801 - fix build bustage.  a=bustage-fix
    Account for bug 1546219 renaming mScrollSnapTypeY to mScrollSnapStrictnessY.

--- gfx/layers/apz/test/gtest/TestSnappingOnMomentum.cpp
+++ gfx/layers/apz/test/gtest/TestSnappingOnMomentum.cpp
@@ -26,7 +26,7 @@ TEST_F(APZCSnappingOnMomentumTester, Snap_On_Momentum) {
 
   // Set up some basic scroll snapping
   ScrollSnapInfo snap;
-  snap.mScrollSnapStrictnessY = StyleScrollSnapStrictness::Mandatory;
+  snap.mScrollSnapTypeY = StyleScrollSnapStrictness::Mandatory;
 
   if (StaticPrefs::layout_css_scroll_snap_v1_enabled()) {
     snap.mSnapPositionY.AppendElement(0 * AppUnitsPerCSSPixel());
