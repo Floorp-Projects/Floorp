5c5eab399b61d992c56a4a1f259559531fb662d1
Jon Coppeard <jcoppeard@mozilla.com>
2019-06-12 16:23:04 +0100

    Bug 1395509 - Track malloc memory used by TypedObject trace lists r=sfink
    This changes the format of the trace list from using -1 as a delimter to storing the list lengths up front so that we have length information.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34731

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -111,7 +111,8 @@ enum class ZealMode {
   _(RareArgumentsData)                     \
   _(RegExpStatics)                         \
   _(RegExpSharedBytecode)                  \
-  _(TypedArrayElements)
+  _(TypedArrayElements)                    \
+  _(TypeDescrTraceList)
 
 #define JS_FOR_EACH_MEMORY_USE(_)  \
   JS_FOR_EACH_PUBLIC_MEMORY_USE(_) \
