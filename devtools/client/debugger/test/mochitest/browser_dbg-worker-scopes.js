79a0d883e1472d0ffbfacba0a0cacac128cc6dfe
Jason Laster <jlaster@mozilla.com>
2019-06-12 21:37:31 +0000

    Bug 1531493 - fix intermittent browser_dbg-worker-scopes.js. r=davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D34716

--- devtools/client/debugger/test/mochitest/browser_dbg-worker-scopes.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-worker-scopes.js
@@ -1,7 +1,8 @@
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
 /* vim: set ft=javascript ts=2 et sw=2 tw=80: */
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 // This error shows up sometimes when running the test, and while this is a
 // strange problem that shouldn't be happening it doesn't prevent the test from
@@ -12,7 +13,7 @@ const { PromiseTestUtils } = ChromeUtils.import(
 PromiseTestUtils.whitelistRejectionsGlobally(/Current state is running/);
 
 function findNode(dbg, text) {
-  for (let index = 0;; index++) {
+  for (let index = 0; ; index++) {
     var elem = findElement(dbg, "scopeNode", index);
     if (elem && elem.innerText == text) {
       return elem;
@@ -25,7 +26,7 @@ function toggleNode(dbg, text) {
 }
 
 function findNodeValue(dbg, text) {
-  for (let index = 0;; index++) {
+  for (let index = 0; ; index++) {
     var elem = findElement(dbg, "scopeNode", index);
     if (elem && elem.innerText == text) {
       return findElement(dbg, "scopeValue", index).innerText;
@@ -39,6 +40,7 @@ add_task(async function() {
   const workerSource = findSource(dbg, "scopes-worker.js");
 
   await addBreakpoint(dbg, workerSource, 11);
+  await dbg.toolbox._target.waitForRequestsToSettle();
   invokeInTab("startWorker");
   await waitForPaused(dbg, "scopes-worker.js");
   await removeBreakpoint(dbg, workerSource.id, 11);
@@ -47,9 +49,9 @@ add_task(async function() {
   assertPausedAtSourceAndLine(dbg, workerSource.id, 11);
 
   await toggleNode(dbg, "var_array");
-  ok(findNodeValue(dbg, "0") == "\"mango\"", "array elem0");
-  ok(findNodeValue(dbg, "1") == "\"pamplemousse\"", "array elem1");
-  ok(findNodeValue(dbg, "2") == "\"pineapple\"", "array elem2");
+  ok(findNodeValue(dbg, "0") == '"mango"', "array elem0");
+  ok(findNodeValue(dbg, "1") == '"pamplemousse"', "array elem1");
+  ok(findNodeValue(dbg, "2") == '"pineapple"', "array elem2");
   await toggleNode(dbg, "var_array");
 
   await toggleNode(dbg, "var_tarray");
@@ -61,19 +63,19 @@ add_task(async function() {
   await toggleNode(dbg, "var_set");
   await toggleNode(dbg, "<entries>");
 
-  ok(findNodeValue(dbg, "0") == "\"papaya\"", "set elem0");
-  ok(findNodeValue(dbg, "1") == "\"banana\"", "set elem1");
+  ok(findNodeValue(dbg, "0") == '"papaya"', "set elem0");
+  ok(findNodeValue(dbg, "1") == '"banana"', "set elem1");
   await toggleNode(dbg, "var_set");
 
   await toggleNode(dbg, "var_map");
   await toggleNode(dbg, "<entries>");
   await toggleNode(dbg, "0");
   ok(findNodeValue(dbg, "<key>"), "1");
-  ok(findNodeValue(dbg, "<value>"), "\"one\"");
+  ok(findNodeValue(dbg, "<value>"), '"one"');
   await toggleNode(dbg, "0");
   await toggleNode(dbg, "1");
   ok(findNodeValue(dbg, "<key>"), "2");
-  ok(findNodeValue(dbg, "<value>"), "\"two\"");
+  ok(findNodeValue(dbg, "<value>"), '"two"');
   await toggleNode(dbg, "var_map");
 
   await toggleNode(dbg, "var_weakmap");
