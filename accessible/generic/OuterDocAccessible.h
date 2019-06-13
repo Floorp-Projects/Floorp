e2f2dd831f10253cfbe6487a8e96f30dbcabaa98
Andreea Pavel <apavel@mozilla.com>
2019-06-13 03:33:45 +0300

    Backed out changeset b5df002db72c (bug 1558390) for build bustages on a CLOSED TREE

--- accessible/generic/OuterDocAccessible.h
+++ accessible/generic/OuterDocAccessible.h
@@ -9,11 +9,6 @@
 #include "AccessibleWrap.h"
 
 namespace mozilla {
-
-namespace dom {
-class BrowserBridgeChild;
-}
-
 namespace a11y {
 class DocAccessibleParent;
 
@@ -36,16 +31,6 @@ class OuterDocAccessible final : public AccessibleWrap {
 #if defined(XP_WIN)
   Accessible* RemoteChildDocAccessible() const;
 #endif
-
-  /**
-   * For iframes in a content process which will be rendered in another content
-   * process, tell the parent process about this OuterDocAccessible
-   * so it can link the trees together when the embedded document is added.
-   * Note that an OuterDocAccessible can be created before the
-   * BrowserBridgeChild or vice versa. Therefore, this must be conditionally
-   * called when either of these is created.
-   */
-  void SendEmbedderAccessible(dom::BrowserBridgeChild* aBridge);
 
   // Accessible
   virtual void Shutdown() override;
