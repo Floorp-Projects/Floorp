7cfd9f1e20d3d04bf69da2462bc1a663cacf3a42
Marco Bonardo <mbonardo@mozilla.com>
2019-06-13 16:54:23 +0000

    Bug 1557302 - Enter on a fully typed @alias should move the caret at the end. r=adw a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D34908
    
    --HG--
    extra : source : 0cb2bef9b2518cc985ae6e0850612ba1e646fe79
    extra : histedit_source : 1971c66607b3867b020171d5cc8becf825d4e1dd

--- browser/components/urlbar/tests/browser/browser_urlbarTokenAlias.js
+++ browser/components/urlbar/tests/browser/browser_urlbarTokenAlias.js
@@ -311,29 +311,28 @@ add_task(async function enterAndFillAlias() {
 // Pressing enter on an @ alias autofill should fill it in the urlbar input
 // with a trailing space and move the caret at the end.
 add_task(async function enterAutofillsAlias() {
-  // Do a search for a partial "@" alias.
-  await UrlbarTestUtils.promiseAutocompleteResultPopup({
-    window,
-    waitForFocus,
-    value: ALIAS.substring(0, ALIAS.length - 1),
-    selectionStart: ALIAS.length - 1,
-    selectionEnd: ALIAS.length - 1,
-  });
-  let details = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
-  Assert.ok(details.autofill);
-
-  // Press Enter.
-  EventUtils.synthesizeKey("KEY_Enter");
-  await waitForAutocompleteResultAt(0);
-
-  // The urlbar input value should be the alias followed by a space so that it's
-  // ready for the user to start typing.
   let expectedString = `${ALIAS} `;
-  Assert.equal(gURLBar.textValue, expectedString);
-  Assert.equal(gURLBar.selectionStart, expectedString.length);
-  Assert.equal(gURLBar.selectionEnd, expectedString.length);
-  await assertAlias(true);
+  for (let value of [ALIAS.substring(0, ALIAS.length - 1), ALIAS]) {
+    await UrlbarTestUtils.promiseAutocompleteResultPopup({
+      window,
+      waitForFocus,
+      value,
+      selectionStart: value.length,
+      selectionEnd: value.length,
+    });
+    await waitForAutocompleteResultAt(0);
 
+    // Press Enter.
+    EventUtils.synthesizeKey("KEY_Enter");
+    await waitForAutocompleteResultAt(0);
+
+    // The urlbar input value should be the alias followed by a space so that it's
+    // ready for the user to start typing.
+    Assert.equal(gURLBar.textValue, expectedString);
+    Assert.equal(gURLBar.selectionStart, expectedString.length);
+    Assert.equal(gURLBar.selectionEnd, expectedString.length);
+    await assertAlias(true);
+  }
   await UrlbarTestUtils.promisePopupClose(window,
     () => EventUtils.synthesizeKey("KEY_Escape"));
 });
