dd7d3f370801ff5a0c135e33256c37ecc3efe44f
Greg Tatum <gtatum@mozilla.com>
2019-06-13 20:25:12 +0000

    Bug 1545582 - Add tests for new JS allocations feature; r=canaltinova
    Differential Revision: https://phabricator.services.mozilla.com/D34544
    
    --HG--
    extra : moz-landing-system : lando

--- tools/profiler/tests/browser/head.js
+++ tools/profiler/tests/browser/head.js
@@ -3,13 +3,15 @@ const { BrowserTestUtils } = ChromeUtils.import("resource://testing-common/Brows
 
 const BASE_URL = "http://example.com/browser/tools/profiler/tests/browser/";
 
-function startProfiler() {
-  const settings = {
-    entries: 1000000, // 9MB
-    interval: 1, // ms
-    features: ["threads"],
-    threads: ["GeckoMain"],
-  };
+const defaultSettings = {
+  entries: 1000000, // 9MB
+  interval: 1, // ms
+  features: ["threads"],
+  threads: ["GeckoMain"],
+};
+
+function startProfiler(callersSettings) {
+  const settings = Object.assign({}, defaultSettings, callersSettings);
   Services.profiler.StartProfiler(
     settings.entries,
     settings.interval,
@@ -17,4 +19,38 @@ function startProfiler() {
     settings.threads,
     settings.duration
   );
+}
+
+/**
+ * This is a helper function be able to run `await wait(500)`. Unfortunately this
+ * is needed as the act of collecting functions relies on the periodic sampling of
+ * the threads. See: https://bugzilla.mozilla.org/show_bug.cgi?id=1529053
+ *
+ * @param {number} time
+ * @returns {Promise}
+ */
+function wait(time) {
+  return new Promise(resolve => {
+    // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+    setTimeout(resolve, time);
+  });
+}
+
+/**
+ * Get the payloads of a type from a single thread.
+ *
+ * @param {Object} thread The thread from a profile.
+ * @param {string} type The marker payload type, e.g. "DiskIO".
+ * @return {Array} The payloads.
+ */
+function getPayloadsOfType(thread, type) {
+  const {markers} = thread;
+  const results = [];
+  for (const markerTuple of markers.data) {
+    const payload = markerTuple[markers.schema.data];
+    if (payload && payload.type === type) {
+      results.push(payload);
+    }
+  }
+  return results;
 }
