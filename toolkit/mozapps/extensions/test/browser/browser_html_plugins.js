d48b3702d3e33449a3ee4bccd45d73bce12b7968
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-06-13 08:42:47 +0000

    Bug 1519434 - remove 'always activate' and 'remember this decision' UI options, r=aswan,johannh
    Differential Revision: https://phabricator.services.mozilla.com/D34215
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/extensions/test/browser/browser_html_plugins.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_plugins.js
@@ -55,8 +55,7 @@ add_task(async function testAskToActivate() {
   let panelItems = card.querySelectorAll("panel-item:not([hidden])");
   let actions = Array.from(panelItems).map(item => item.getAttribute("action"));
   Assert.deepEqual(actions, [
-    "ask-to-activate", "always-activate", "never-activate", "preferences",
-    "expand",
+    "ask-to-activate", "never-activate", "preferences", "expand",
   ], "The panel items are for a plugin");
 
   checkItems(panelItems, "ask-to-activate");
@@ -65,18 +64,9 @@ add_task(async function testAskToActivate() {
      "Flash is ask-to-activate");
   ok(flash.isActive, "Flash is active");
 
-  // Switch the plugin to always activate.
-  let updated = BrowserTestUtils.waitForEvent(card, "update");
-  panelItems[1].click();
-  await updated;
-  checkItems(panelItems, "always-activate");
-  ok(flash.userDisabled != AddonManager.STATE_ASK_TO_ACTIVATE,
-     "Flash isn't ask-to-activate");
-  ok(flash.isActive, "Flash is still active");
-
   // Switch to never activate.
-  updated = BrowserTestUtils.waitForEvent(card, "update");
-  panelItems[2].click();
+  let updated = BrowserTestUtils.waitForEvent(card, "update");
+  card.querySelector("panel-item[action*=never]").click();
   await updated;
   checkItems(panelItems, "never-activate");
   ok(flash.userDisabled, `Flash is not userDisabled... for some reason`);
@@ -84,7 +74,7 @@ add_task(async function testAskToActivate() {
 
   // Switch it back to ask to activate.
   updated = BrowserTestUtils.waitForEvent(card, "update");
-  panelItems[0].click();
+  card.querySelector("panel-item[action*=ask]").click();
   await updated;
   checkItems(panelItems, "ask-to-activate");
   is(flash.userDisabled, AddonManager.STATE_ASK_TO_ACTIVATE,
@@ -96,28 +86,19 @@ add_task(async function testAskToActivate() {
   card.querySelector("[action=expand]").click();
   await loaded;
 
-  // Set the state to always activate.
   card = doc.querySelector("addon-card");
   panelItems = card.querySelectorAll("panel-item");
   checkItems(panelItems, "ask-to-activate");
-  updated = BrowserTestUtils.waitForEvent(card, "update");
-  panelItems[1].click();
-  await updated;
-  checkItems(panelItems, "always-activate");
 
   await closeView(win);
 
   assertAboutAddonsTelemetryEvents([
     ["addonsManager", "view", "aboutAddons", "list", {type: "plugin"}],
     ["addonsManager", "action", "aboutAddons", null,
-     {type: "plugin", addonId, view: "list", action: "enable"}],
-    ["addonsManager", "action", "aboutAddons", null,
      {type: "plugin", addonId, view: "list", action: "disable"}],
     // Ask-to-activate doesn't trigger a telemetry event.
     ["addonsManager", "view", "aboutAddons", "detail",
      {type: "plugin", addonId}],
-    ["addonsManager", "action", "aboutAddons", null,
-     {type: "plugin", addonId, view: "detail", action: "enable"}],
   ]);
 });
 
