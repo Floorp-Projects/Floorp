debd3aae57c58acd68c5b28ccedc6411fed66603
Lily Chen <chlily@chromium.org>
2019-06-13 14:05:13 +0000

    Bug 1553622 [wpt PR 16957] - Cookies WPTs: Add test coverage for SameSite=None, a=testonly
    Automatic update from web-platform-tests
    Cookies WPTs: Add test coverage for SameSite=None
    
    This CL adds test coverage for SameSite=None cookies, since the spec
    was recently updated to add `None` as a possible value for SameSite:
    https://tools.ietf.org/html/draft-ietf-httpbis-rfc6265bis-03#section-4.1.2.7
    
    This basically just renames the cookie we previously had in the tests as
    "samesite_none" to "samesite_unspecified", and adds a new "samesite_none"
    cookie that sets the new SameSite=None attribute.
    
    Bug: 961439
    Change-Id: Ic2a96cbe73a75a7cc28fa93f04917c3c61208f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1603523
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#662337}
    
    --
    
    wp5At-commits: c899c3403fe6cc92ae92f0ef321f08f6aa74adb4
    wpt-pr: 16957

--- testing/web-platform/tests/cookies/resources/cookie-helper.sub.js
+++ testing/web-platform/tests/cookies/resources/cookie-helper.sub.js
@@ -142,6 +142,7 @@ async function resetSameSiteCookies(origin, value) {
       assert_dom_cookie("samesite_strict", value, false);
       assert_dom_cookie("samesite_lax", value, false);
       assert_dom_cookie("samesite_none", value, false);
+      assert_dom_cookie("samesite_unspecified", value, false);
     }
 
     w.postMessage({type: "set", value: value, useOwnOrigin: true}, "*");
@@ -150,6 +151,7 @@ async function resetSameSiteCookies(origin, value) {
       assert_dom_cookie("samesite_strict", value, true);
       assert_dom_cookie("samesite_lax", value, true);
       assert_dom_cookie("samesite_none", value, true);
+      assert_dom_cookie("samesite_unspecified", value, true);
     }
   } finally {
     w.close();
@@ -159,7 +161,8 @@ async function resetSameSiteCookies(origin, value) {
 // Given an |expectedStatus| and |expectedValue|, assert the |cookies| contains the
 // proper set of cookie names and values.
 function verifySameSiteCookieState(expectedStatus, expectedValue, cookies) {
-    assert_equals(cookies["samesite_none"], expectedValue, "Non-SameSite cookies are always sent.");
+    assert_equals(cookies["samesite_none"], expectedValue, "SameSite=None cookies are always sent.");
+    assert_equals(cookies["samesite_unspecified"], expectedValue, "Unspecified-SameSite cookies are always sent.");
     if (expectedStatus == SameSiteStatus.CROSS_SITE) {
       assert_not_equals(cookies["samesite_strict"], expectedValue, "SameSite=Strict cookies are not sent with cross-site requests.");
       assert_not_equals(cookies["samesite_lax"], expectedValue, "SameSite=Lax cookies are not sent with cross-site requests.");
--- testing/web-platform/tests/cookies/resources/setSameSite.py
+++ testing/web-platform/tests/cookies/resources/setSameSite.py
@@ -1,10 +1,11 @@
 from helpers import makeCookieHeader, setNoCacheAndCORSHeaders
 
 def main(request, response):
-    """Respond to `/cookie/set/samesite?{value}` by setting three cookies:
+    """Respond to `/cookie/set/samesite?{value}` by setting four cookies:
     1. `samesite_strict={value};SameSite=Strict;path=/`
     2. `samesite_lax={value};SameSite=Lax;path=/`
-    3. `samesite_none={value};path=/`
+    3. `samesite_none={value};SameSite=None;path=/`
+    4. `samesite_unspecified={value};path=/`
     Then navigate to a page that will post a message back to the opener with the set cookies"""
     headers = setNoCacheAndCORSHeaders(request, response)
     value = request.url_parts.query
@@ -12,7 +13,8 @@ def main(request, response):
     headers.append(("Content-Type", "text/html; charset=utf-8"))
     headers.append(makeCookieHeader("samesite_strict", value, {"SameSite":"Strict","path":"/"}))
     headers.append(makeCookieHeader("samesite_lax", value, {"SameSite":"Lax","path":"/"}))
-    headers.append(makeCookieHeader("samesite_none", value, {"path":"/"}))
+    headers.append(makeCookieHeader("samesite_none", value, {"SameSite":"None", "path":"/"}))
+    headers.append(makeCookieHeader("samesite_unspecified", value, {"path":"/"}))
 
     document = """
 <!DOCTYPE html>
