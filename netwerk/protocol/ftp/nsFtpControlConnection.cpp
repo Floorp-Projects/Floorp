a4410ccb5178172f8f6fd3611d385812af5ec321
Boris Zbarsky <bzbarsky@mit.edu>
2019-06-13 04:21:32 +0000

    Bug 1558726.  Stop using [array] in nsISocketTransportService.  r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D34668
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/protocol/ftp/nsFtpControlConnection.cpp
+++ netwerk/protocol/ftp/nsFtpControlConnection.cpp
@@ -13,6 +13,7 @@
 #include "nsThreadUtils.h"
 #include "nsIOutputStream.h"
 #include "nsNetCID.h"
+#include "nsTArray.h"
 #include <algorithm>
 
 using namespace mozilla;
@@ -93,7 +94,7 @@ nsresult nsFtpControlConnection::Connect(nsIProxyInfo* proxyInfo,
       do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
   if (NS_FAILED(rv)) return rv;
 
-  rv = sts->CreateTransport(nullptr, 0, mHost, mPort, proxyInfo,
+  rv = sts->CreateTransport(nsTArray<nsCString>(), mHost, mPort, proxyInfo,
                             getter_AddRefs(mSocket));  // the command transport
   if (NS_FAILED(rv)) return rv;
 
