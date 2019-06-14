9ab3272468a969a78ae3844b512ea3c31c6350b1
yulia <ystartsev@mozilla.com>
2019-06-14 20:34:04 +0000

    Bug 1494796 - fix verify test r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D34981
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/shared/client/thread-client.js
+++ devtools/shared/client/thread-client.js
@@ -195,8 +195,15 @@ class ThreadClient extends FrontClassWithSpec(threadSpec) {
   /**
    * Request the loaded sources for the current thread.
    */
-  getSources() {
-    return super.sources();
+  async getSources() {
+    let sources = [];
+    try {
+      ({sources} = await super.sources());
+    } catch (e) {
+      // we may have closed the connection
+      console.log(`getSources failed. Connection may have closed: ${e}`);
+    }
+    return { sources };
   }
 
   /**
