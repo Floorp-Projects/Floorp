0d949dc846a878411c62bc7653eecc2d10a4d08a
lloan <lloanalas@outlook.com>
2019-06-14 12:12:44 +0000

    Bug 1174100 - Cache information is not exposed to HAR. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D33570

--- devtools/client/netmonitor/src/har/har-builder.js
+++ devtools/client/netmonitor/src/har/har-builder.js
@@ -112,7 +112,7 @@ HarBuilder.prototype = {
 
     entry.request = await this.buildRequest(file);
     entry.response = await this.buildResponse(file);
-    entry.cache = this.buildCache(file);
+    entry.cache = await this.buildCache(file);
     entry.timings = eventTimings ? eventTimings.timings : {};
 
     // Calculate total time by summing all timings. Note that
@@ -403,18 +403,19 @@ HarBuilder.prototype = {
     return content;
   },
 
-  buildCache: function(file) {
+  buildCache: async function(file) {
     const cache = {};
 
-    if (!file.fromCache) {
+    // if resource has changed, return early
+    if (file.status !== "304") {
       return cache;
     }
 
-    // There is no such info yet in the Net panel.
-    // cache.beforeRequest = {};
-
-    if (file.cacheEntry) {
-      cache.afterRequest = this.buildCacheEntry(file.cacheEntry);
+    if (file.responseCacheAvailable && this._options.requestData) {
+      const responseCache = await this._options.requestData(file.id, "responseCache");
+      if (responseCache.cache) {
+        cache.afterRequest = this.buildCacheEntry(responseCache.cache);
+      }
     } else {
       cache.afterRequest = null;
     }
@@ -425,10 +426,12 @@ HarBuilder.prototype = {
   buildCacheEntry: function(cacheEntry) {
     const cache = {};
 
-    cache.expires = findValue(cacheEntry, "Expires");
-    cache.lastAccess = findValue(cacheEntry, "Last Fetched");
-    cache.eTag = "";
-    cache.hitCount = findValue(cacheEntry, "Fetch Count");
+    if (typeof cacheEntry !== "undefined") {
+      cache.expires = cacheEntry.expires;
+      cache.lastAccess = cacheEntry.lastFetched;
+      cache.eTag = "";
+      cache.hitCount = cacheEntry.fetchCount;
+    }
 
     return cache;
   },
