aa0686544f335cc44ed593095cf98943bdb96971
Jonathan Kingston <jkt@mozilla.com>
2019-06-14 14:28:05 +0000

    Bug 1554294 - Adding in chrome as a supported scriptLoader IsInternalURIScheme and adding relative file paths to aboutlogins r=baku,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D34705
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutlogins/content/aboutLogins.js
+++ browser/components/aboutlogins/content/aboutLogins.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {recordTelemetryEvent} from "chrome://browser/content/aboutlogins/aboutLoginsUtils.js";
+import {recordTelemetryEvent} from "./aboutLoginsUtils.js";
 
 let gElements = {};
 
