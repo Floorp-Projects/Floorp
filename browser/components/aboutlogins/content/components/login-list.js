d79639781c310e38fb2ba7075527298a1754171c
Jonathan Kingston <jkt@mozilla.com>
2019-06-14 14:28:05 +0000

    Bug 1554294 - Adding in chrome as a supported scriptLoader IsInternalURIScheme and adding relative file paths to aboutlogins r=baku,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D34705

--- browser/components/aboutlogins/content/components/login-list.js
+++ browser/components/aboutlogins/content/components/login-list.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import LoginListItem from "chrome://browser/content/aboutlogins/components/login-list-item.js";
-import ReflectedFluentElement from "chrome://browser/content/aboutlogins/components/reflected-fluent-element.js";
+import LoginListItem from "./login-list-item.js";
+import ReflectedFluentElement from "./reflected-fluent-element.js";
 
 const collator = new Intl.Collator();
 const sortFnOptions = {
