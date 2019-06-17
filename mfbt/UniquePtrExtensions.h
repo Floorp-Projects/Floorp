19b7fc2cbd6b68af69f9e6946b03f3481ef34f8e
Jed Davis <jld@mozilla.com>
2019-06-17 02:19:35 +0000

    Bug 1534780 - Move ipc::FileDescriptor's UniquePtr instance into MFBT as UniqueFileHandle. r=froydnj
    MozReview-Commit-ID: 7bbGVIjTTaJ
    
    Differential Revision: https://phabricator.services.mozilla.com/D26737
    
    --HG--
    extra : moz-landing-system : lando

--- mfbt/UniquePtrExtensions.h
+++ mfbt/UniquePtrExtensions.h
@@ -12,6 +12,10 @@
 #include "mozilla/fallible.h"
 #include "mozilla/UniquePtr.h"
 
+#ifdef XP_WIN
+#  include <cstdint>
+#endif
+
 namespace mozilla {
 
 /**
@@ -42,10 +46,80 @@ struct FreePolicy {
   void operator()(const void* ptr) { free(const_cast<void*>(ptr)); }
 };
 
+#if defined(XP_WIN)
+// Can't include <windows.h> to get the actual definition of HANDLE
+// because of namespace pollution.
+typedef void* FileHandleType;
+#elif defined(XP_UNIX)
+typedef int FileHandleType;
+#else
+#  error "Unsupported OS?"
+#endif
+
+struct FileHandleHelper {
+  MOZ_IMPLICIT FileHandleHelper(FileHandleType aHandle) : mHandle(aHandle) {}
+
+  MOZ_IMPLICIT constexpr FileHandleHelper(std::nullptr_t)
+      : mHandle(kInvalidHandle) {}
+
+  bool operator!=(std::nullptr_t) const {
+#ifdef XP_WIN
+    // Windows uses both nullptr and INVALID_HANDLE_VALUE (-1 cast to
+    // HANDLE) in different situations, but nullptr is more reliably
+    // null while -1 is also valid input to some calls that take
+    // handles.  So class considers both to be null (since neither
+    // should be closed) but default-constructs as nullptr.
+    if (mHandle == (void*)-1) {
+      return false;
+    }
+#endif
+    return mHandle != kInvalidHandle;
+  }
+
+  operator FileHandleType() const { return mHandle; }
+
+#ifdef XP_WIN
+  // NSPR uses an integer type for PROsfd, so this conversion is
+  // provided for working with it without needing reinterpret casts
+  // everywhere.
+  operator std::intptr_t() const {
+    return reinterpret_cast<std::intptr_t>(mHandle);
+  }
+#endif
+
+  // When there's only one user-defined conversion operator, the
+  // compiler will use that to derive equality, but that doesn't work
+  // when the conversion is ambiguoug (the XP_WIN case above).
+  bool operator==(const FileHandleHelper& aOther) const {
+    return mHandle == aOther.mHandle;
+  }
+
+ private:
+  FileHandleType mHandle;
+
+#ifdef XP_WIN
+  // See above for why this is nullptr.  (Also, INVALID_HANDLE_VALUE
+  // can't be expressed as a constexpr.)
+  static constexpr FileHandleType kInvalidHandle = nullptr;
+#else
+  static constexpr FileHandleType kInvalidHandle = -1;
+#endif
+};
+
+struct FileHandleDeleter {
+  typedef FileHandleHelper pointer;
+  MFBT_API void operator()(FileHandleHelper aHelper);
+};
+
 }  // namespace detail
 
 template <typename T>
 using UniqueFreePtr = UniquePtr<T, detail::FreePolicy<T>>;
+
+// A RAII class for the OS construct used for open files and similar
+// objects: a file descriptor on Unix or a handle on Windows.
+using UniqueFileHandle =
+    UniquePtr<detail::FileHandleType, detail::FileHandleDeleter>;
 
 }  // namespace mozilla
 
