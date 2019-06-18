a6c83a6384674e5cb719e58f219f5e49f30850da
MickeyMoz <sebastian@mozilla.com>
2019-06-18 14:14:42 +0000

    [components] Update GeckoView (release) (20190618-141439)

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "67.0.20190521210220"
+    const val release_version = "67.0.20190613202504"
 }
 
 @Suppress("MaxLineLength")
