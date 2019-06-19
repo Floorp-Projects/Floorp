f03e67a9405b093c8eb53d1b26e7c11471668bdb
Mike Shal <mshal@mozilla.com>
2019-06-19 22:05:41 +0000

    Bug 1557785 - Update run-profileserver.sh for Windows compatibility; r=firefox-build-system-reviewers,chmanchester
    The run-profileserver.sh script is a bridge between the Taskcluster task
    and profileserver.py. It was originally written as a Linux-only script,
    but with a few modifications it can support Windows as well. The xvfb
    support needs to be optional, and the UPLOAD_PATH and PGO_RUNDIR
    variables must not assume a Linux filesystem.
    
    Differential Revision: https://phabricator.services.mozilla.com/D34796

--- taskcluster/scripts/misc/run-profileserver.sh
+++ taskcluster/scripts/misc/run-profileserver.sh
@@ -4,33 +4,36 @@ set -x -e
 
 echo "running as" $(id)
 
+: NEED_XVFB     ${NEED_XVFB:=false}
+: UPLOAD_PATH   ${UPLOAD_PATH:=$HOME/artifacts}
+
 ####
 # Taskcluster friendly wrapper for running the profileserver
 ####
 
-PGO_RUNDIR=/builds/worker/workspace/build/src/obj-firefox/dist
-export UPLOAD_PATH=$HOME/artifacts
+PGO_RUNDIR=obj-firefox/dist
 export JARLOG_FILE="en-US.log"
 
 set -v
 
-# run XVfb in the background
-. /builds/worker/scripts/xvfb.sh
+if $NEED_XVFB; then
+    # run XVfb in the background
+    . /builds/worker/scripts/xvfb.sh
 
-cleanup() {
-    local rv=$?
-    cleanup_xvfb
-    exit $rv
-}
-trap cleanup EXIT INT
+    cleanup() {
+        local rv=$?
+        cleanup_xvfb
+        exit $rv
+    }
+    trap cleanup EXIT INT
 
-start_xvfb '1024x768x24' 2
-
-cd /builds/worker/checkouts/gecko
+    start_xvfb '1024x768x24' 2
+fi
 
 # Move our fetched firefox into objdir/dist so the jarlog entries will match
 # the paths when the final PGO stage packages the build.
 mkdir -p $PGO_RUNDIR
+mkdir -p $UPLOAD_PATH
 mv $MOZ_FETCHES_DIR/firefox $PGO_RUNDIR
 ./mach python build/pgo/profileserver.py --binary $PGO_RUNDIR/firefox/firefox
 
