b284d5366a8006f0bc63714b942eb9f9ab21fe04
Peter Van der Beken <peterv@propagandism.org>
2019-06-19 21:08:45 +0200

    Bug 1560940 - Change tests that use session history listeners to listen in the parent process. r=nika
    Fix tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35655

--- docshell/test/browser/browser_bug670318.js
+++ docshell/test/browser/browser_bug670318.js
@@ -12,71 +12,51 @@ const URL =
   "http://mochi.test:8888/browser/docshell/test/browser/file_bug670318.html";
 
 add_task(async function test() {
-  await BrowserTestUtils.withNewTab(
-    { gBrowser, url: "about:blank" },
-    async function(browser) {
-      await ContentTask.spawn(browser, URL, async function(URL) {
-        let history = docShell.QueryInterface(Ci.nsIWebNavigation)
-          .sessionHistory;
-        let count = 0;
+  await BrowserTestUtils.withNewTab({ gBrowser, url: "about:blank" },
+                                    async function(browser) {
+    let history = browser.browsingContext.sessionHistory;
+    let count = 0;
 
-        let testDone = {};
-        testDone.promise = new Promise(resolve => {
-          testDone.resolve = resolve;
-        });
+    let testDone = {};
+    testDone.promise = new Promise(resolve => { testDone.resolve = resolve; });
 
-        let listener = {
-          OnHistoryNewEntry(aNewURI) {
-            if (aNewURI.spec == URL && 5 == ++count) {
-              addEventListener(
-                "load",
-                function onLoad() {
-                  removeEventListener("load", onLoad, true);
+    let listener = {
+      async OnHistoryNewEntry(aNewURI) {
+        if (aNewURI.spec == URL && 5 == ++count) {
+          history.removeSHistoryListener(listener);
+          await ContentTask.spawn(browser, null, async () => {
+            let promise = new Promise(resolve => {
+              addEventListener("load", evt => {
+                let history = docShell.QueryInterface(Ci.nsIWebNavigation).sessionHistory;
+                Assert.ok(history.index < history.count, "history.index is valid");
+                resolve();
+              }, { capture: true, once: true });
+            });
 
-                  Assert.ok(
-                    history.index < history.count,
-                    "history.index is valid"
-                  );
-                  testDone.resolve();
-                },
-                true
-              );
+            content.location.reload();
+            await promise;
+          });
+          testDone.resolve();
+        }
+      },
 
-              history.legacySHistory.removeSHistoryListener(listener);
-              delete content._testListener;
-              content.setTimeout(() => {
-                content.location.reload();
-              }, 0);
-            }
-          },
+      OnHistoryReload: () => true,
+      OnHistoryGotoIndex: () => {},
+      OnHistoryPurge: () => {},
+      OnHistoryReplaceEntry: () => {
+        // The initial load of about:blank causes a transient entry to be
+        // created, so our first navigation to a real page is a replace
+        // instead of a new entry.
+        ++count;
+      },
 
-          OnHistoryReload: () => true,
-          OnHistoryGotoIndex: () => {},
-          OnHistoryPurge: () => {},
-          OnHistoryReplaceEntry: () => {
-            // The initial load of about:blank causes a transient entry to be
-            // created, so our first navigation to a real page is a replace
-            // instead of a new entry.
-            ++count;
-          },
+      QueryInterface: ChromeUtils.generateQI([Ci.nsISHistoryListener,
+                                              Ci.nsISupportsWeakReference]),
+    };
 
-          QueryInterface: ChromeUtils.generateQI([
-            Ci.nsISHistoryListener,
-            Ci.nsISupportsWeakReference,
-          ]),
-        };
+    history.addSHistoryListener(listener);
+    BrowserTestUtils.loadURI(browser, URL);
 
-        history.legacySHistory.addSHistoryListener(listener);
-        // Since listener implements nsISupportsWeakReference, we are
-        // responsible for keeping it alive so that the GC doesn't clear
-        // it before the test completes. We do this by anchoring the listener
-        // to the content global window, and clearing it just before the test
-        // completes.
-        content._testListener = listener;
-        content.location = URL;
-
-        await testDone.promise;
-      });
-    }
-  );
+    await testDone.promise;
+  });
 });
