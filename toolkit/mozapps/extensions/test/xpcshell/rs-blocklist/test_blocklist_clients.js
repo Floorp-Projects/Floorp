e5beee336a6c95a370543f1f74074ea1bdc0f599
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-06-19 14:20:38 +0000

    Bug 1559353 - fix test_blocklist_clients.js to actually work, r=leplatrem
    It's unclear to me how this is passing on infra. After bug 1548542, as far as I can tell the only
    time this test passes is when it somehow finishes before it does any of the filter testing.
    
    The filter functions as-is (which predates that bug) does not allow these items to be in the
    blocklist without something to identify what they're blocking (a guid/name for addons, a
    matchXXXXX prop for plugins). Also, we no longer have a separate bucket pref for the extension
    blocklist.
    
    Before this patch, when this (rarely) passes for me on the local machine, it does so because
    we bail out immediately after the initial run_test, as if the add_task functions have somehow
    not registered. I do not understand why this would happen. In any case, after these changes,
    we definitely run the rest of the test and it passes for me locally.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35030

--- toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_blocklist_clients.js
+++ toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/test_blocklist_clients.js
@@ -17,7 +17,7 @@ async function clear_state() {
 }
 
 
-function run_test() {
+add_task(async function setup() {
   AddonTestUtils.createAppInfo("XPCShell", "xpcshell@tests.mozilla.org", "1", "");
 
   // This will initialize the remote settings clients for blocklists.
@@ -31,8 +31,8 @@ function run_test() {
     {client: BlocklistGlobal.GfxBlocklistRS._client, testData: ["g204", "g200", "g36"]},
   ];
 
-  promiseStartupManager().then(run_next_test);
-}
+  await promiseStartupManager();
+});
 
 add_task(async function test_initial_dump_is_loaded_as_synced_when_collection_is_empty() {
   for (let {client} of gBlocklistClients) {
@@ -47,6 +47,8 @@ add_task(clear_state);
 
 add_task(async function test_data_is_filtered_for_target() {
   const initial = [{
+    "guid": "foo",
+    "matchName": "foo",
     "versionRange": [{
       "targetApplication": [],
       "maxVersion": "*",
@@ -55,24 +57,30 @@ add_task(async function test_data_is_filtered_for_target() {
     }],
   }];
   const noMatchingTarget = [{
+    "guid": "foo",
+    "matchName": "foo",
     "versionRange": [{
-      "targetApplication": [],
+      "targetApplication": [{guid: "Foo"}],
       "maxVersion": "*",
       "minVersion": "0",
       "severity": "3",
     }],
   }, {
+    "guid": "foo",
+    "matchName": "foo",
     "versionRange": [{
-      "targetApplication": [],
+      "targetApplication": [{guid: "XPCShell", maxVersion: "0.1"}],
       "maxVersion": "*",
       "minVersion": "0",
       "severity": "1",
     }],
   }];
   const oneMatch = [{
+    "guid": "foo",
+    "matchName": "foo",
     "versionRange": [{
       "targetApplication": [{
-        "guid": "some-guid",
+        "guid": "XPCShell",
       }],
     }],
   }];
@@ -88,24 +96,35 @@ add_task(async function test_data_is_filtered_for_target() {
 
     const { data: internalData } = await collection.list();
     Assert.equal(internalData.length, records.length);
-    Assert.equal((await client.get()).length, 2); // only two matches.
+    let filtered = await client.get({syncIfEmpty: false});
+    Assert.equal(filtered.length, 2); // only two matches.
   }
 });
 add_task(clear_state);
 
 add_task(async function test_entries_are_filtered_when_jexl_filter_expression_is_present() {
   const records = [{
+      guid: "foo",
+      matchName: "foo",
       willMatch: true,
     }, {
+      guid: "foo",
+      matchName: "foo",
       willMatch: true,
       filter_expression: null,
     }, {
+      guid: "foo",
+      matchName: "foo",
       willMatch: true,
       filter_expression: "1 == 1",
     }, {
+      guid: "foo",
+      matchName: "foo",
       willMatch: false,
       filter_expression: "1 == 2",
     }, {
+      guid: "foo",
+      matchName: "foo",
       willMatch: true,
       filter_expression: "1 == 1",
       versionRange: [{
@@ -114,6 +133,8 @@ add_task(async function test_entries_are_filtered_when_jexl_filter_expression_is
         }],
       }],
     }, {
+      guid: "foo",
+      matchName: "foo",
       willMatch: false,  // jexl prevails over versionRange.
       filter_expression: "1 == 2",
       versionRange: [{
@@ -131,7 +152,7 @@ add_task(async function test_entries_are_filtered_when_jexl_filter_expression_is
       await collection.create(record);
     }
     await collection.db.saveLastModified(42); // Prevent from loading JSON dump.
-    const list = await client.get();
+    const list = await client.get({syncIfEmpty: false});
     equal(list.length, 4);
     ok(list.every(e => e.willMatch));
   }
@@ -142,16 +163,12 @@ add_task(async function test_bucketname_changes_when_bucket_pref_changes() {
   for (const { client } of gBlocklistClients) {
     equal(client.bucketName, "blocklists");
   }
-  equal(BlocklistGlobal.ExtensionBlocklistRS._client.bucketName, "addons");
 
   Services.prefs.setCharPref("services.blocklist.bucket", "blocklists-preview");
-  Services.prefs.setCharPref("services.blocklist.addons.bucket", "addons-preview");
 
   for (const { client } of gBlocklistClients) {
     equal(client.bucketName, "blocklists-preview", client.identifier);
   }
-  equal(BlocklistGlobal.ExtensionBlocklistRS._client.bucketName, "addons-preview");
   Services.prefs.clearUserPref("services.blocklist.bucket");
-  Services.prefs.clearUserPref("services.blocklist.addons.bucket");
 });
 add_task(clear_state);
