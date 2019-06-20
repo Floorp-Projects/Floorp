2b4a3caf61a8e6de19c788c488231428402a3740
Kartikaya Gupta <kgupta@mozilla.com>
2019-06-20 14:34:54 +0000

    Bug 1548687 - Add mochitest to exercise scrolling an oopif's ancestor. r=botond
    This scrolls the document containing the OOPIF and ensures that
    hit-testing still works. Some of the fission hit-testing machinery is
    still WR-only, so we have to restrict the subtest to only run when WR
    is enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35204

--- gfx/layers/apz/test/mochitest/browser_test_group_fission.js
+++ gfx/layers/apz/test/mochitest/browser_test_group_fission.js
@@ -16,6 +16,7 @@ add_task(async function test_main() {
   if (isWebRender) {
     test_urls = test_urls.concat([
       httpURL("helper_fission_transforms.html"),
+      httpURL("helper_fission_scroll_oopif.html"),
       // add additional WebRender-specific tests here
     ]);
   }
