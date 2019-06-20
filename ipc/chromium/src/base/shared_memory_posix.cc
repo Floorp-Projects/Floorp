d976b4cf657c31b2a2261281cb967eaf6a60634d
Jed Davis <jld@mozilla.com>
2019-06-20 22:40:32 +0000

    Bug 1479960 - Add freezing of IPC shared memory. r=froydnj,kmag
    This allows writing to shared memory and then making it read-only before
    sharing it to other processes, such that a malicious sandboxed process
    cannot regain write access.  This is currently available only in the
    low-level base::SharedMemory interface.
    
    The freeze operation exposes the common subset of read-only shared
    memory that we can implement on all supported OSes: with some APIs
    (POSIX shm_open) we can't revoke writeability from existing capabilies,
    while for others (Android ashmem) we *must* revoke it.  Thus, we require
    that the writeable capability not have been duplicated or shared to
    another process, and consume it as part of freezing.  Also, because in
    some backends need special handling at creation time, freezeability must
    be explicitly requested.
    
    In particular, this doesn't allow giving an untrusted process read-only
    access to memory that the original process can write.
    
    Note that on MacOS before 10.12 this will use temporary files in order to
    avoid an OS security bug that allows regaining write access; those OS
    versions are no longer supported by Apple (but are supported by Firefox).
    
    Depends on D26742
    
    Differential Revision: https://phabricator.services.mozilla.com/D26743

--- ipc/chromium/src/base/shared_memory_posix.cc
+++ ipc/chromium/src/base/shared_memory_posix.cc
@@ -27,9 +27,11 @@ namespace base {
 
 SharedMemory::SharedMemory()
     : mapped_file_(-1),
+      frozen_file_(-1),
       mapped_size_(0),
       memory_(nullptr),
       read_only_(false),
+      freezeable_(false),
       max_size_(0) {}
 
 SharedMemory::SharedMemory(SharedMemory&& other) {
@@ -39,12 +41,15 @@ SharedMemory::SharedMemory(SharedMemory&& other) {
 
   mapped_file_ = other.mapped_file_;
   mapped_size_ = other.mapped_size_;
+  frozen_file_ = other.frozen_file_;
   memory_ = other.memory_;
   read_only_ = other.read_only_;
+  freezeable_ = other.freezeable_;
   max_size_ = other.max_size_;
 
   other.mapped_file_ = -1;
   other.mapped_size_ = 0;
+  other.frozen_file_ = -1;
   other.memory_ = nullptr;
 }
 
@@ -52,7 +57,9 @@ SharedMemory::~SharedMemory() { Close(); }
 
 bool SharedMemory::SetHandle(SharedMemoryHandle handle, bool read_only) {
   DCHECK(mapped_file_ == -1);
+  DCHECK(frozen_file_ == -1);
 
+  freezeable_ = false;
   mapped_file_ = handle.fd;
   read_only_ = read_only;
   return true;
@@ -67,6 +74,89 @@ bool SharedMemory::IsValid() const { return mapped_file_ >= 0; }
 
 // static
 SharedMemoryHandle SharedMemory::NULLHandle() { return SharedMemoryHandle(); }
+
+// Workaround for CVE-2018-4435 (https://crbug.com/project-zero/1671);
+// can be removed when minimum OS version is at least 10.12.
+#ifdef OS_MACOSX
+static const char* GetTmpDir() {
+  static const char* const kTmpDir = [] {
+    const char* tmpdir = PR_GetEnv("TMPDIR");
+    if (tmpdir) {
+      return tmpdir;
+    }
+    return "/tmp";
+  }();
+  return kTmpDir;
+}
+
+static int FakeShmOpen(const char* name, int oflag, int mode) {
+  CHECK(name[0] == '/');
+  std::string path(GetTmpDir());
+  path += name;
+  return open(path.c_str(), oflag | O_CLOEXEC | O_NOCTTY, mode);
+}
+
+static int FakeShmUnlink(const char* name) {
+  CHECK(name[0] == '/');
+  std::string path(GetTmpDir());
+  path += name;
+  return unlink(path.c_str());
+}
+
+static bool IsShmOpenSecure() {
+  static const bool kIsSecure = [] {
+    mozilla::UniqueFileHandle rwfd, rofd;
+    std::string name;
+    CHECK(SharedMemory::AppendPosixShmPrefix(&name, getpid()));
+    name += "sectest";
+    // The prefix includes the pid and this will be called at most
+    // once per process, so no need for a counter.
+    rwfd.reset(
+        HANDLE_EINTR(shm_open(name.c_str(), O_RDWR | O_CREAT | O_EXCL, 0600)));
+    // An adversary could steal the name.  Handle this semi-gracefully.
+    DCHECK(rwfd);
+    if (!rwfd) {
+      return false;
+    }
+    rofd.reset(shm_open(name.c_str(), O_RDONLY, 0400));
+    CHECK(rofd);
+    CHECK(shm_unlink(name.c_str()) == 0);
+    CHECK(ftruncate(rwfd.get(), 1) == 0);
+    rwfd = nullptr;
+    void* map = mmap(nullptr, 1, PROT_READ, MAP_SHARED, rofd.get(), 0);
+    CHECK(map != MAP_FAILED);
+    bool ok = mprotect(map, 1, PROT_READ | PROT_WRITE) != 0;
+    munmap(map, 1);
+    return ok;
+  }();
+  return kIsSecure;
+}
+
+static int SafeShmOpen(bool freezeable, const char* name, int oflag, int mode) {
+  if (!freezeable || IsShmOpenSecure()) {
+    return shm_open(name, oflag, mode);
+  } else {
+    return FakeShmOpen(name, oflag, mode);
+  }
+}
+
+static int SafeShmUnlink(bool freezeable, const char* name) {
+  if (!freezeable || IsShmOpenSecure()) {
+    return shm_unlink(name);
+  } else {
+    return FakeShmUnlink(name);
+  }
+}
+
+#elif !defined(ANDROID)
+static int SafeShmOpen(bool freezeable, const char* name, int oflag, int mode) {
+  return shm_open(name, oflag, mode);
+}
+
+static int SafeShmUnlink(bool freezeable, const char* name) {
+  return shm_unlink(name);
+}
+#endif
 
 // static
 bool SharedMemory::AppendPosixShmPrefix(std::string* str, pid_t pid) {
@@ -99,13 +189,15 @@ bool SharedMemory::AppendPosixShmPrefix(std::string* str, pid_t pid) {
 #endif    // !ANDROID
 }
 
-bool SharedMemory::Create(size_t size) {
+bool SharedMemory::CreateInternal(size_t size, bool freezeable) {
   read_only_ = false;
 
   DCHECK(size > 0);
   DCHECK(mapped_file_ == -1);
+  DCHECK(frozen_file_ == -1);
 
   mozilla::UniqueFileHandle fd;
+  mozilla::UniqueFileHandle frozen_fd;
   bool needs_truncate = true;
 
 #ifdef ANDROID
@@ -130,10 +222,21 @@ bool SharedMemory::Create(size_t size) {
     CHECK(AppendPosixShmPrefix(&name, getpid()));
     StringAppendF(&name, "%zu", sNameCounter++);
     // O_EXCL means the names being predictable shouldn't be a problem.
-    fd.reset(
-        HANDLE_EINTR(shm_open(name.c_str(), O_RDWR | O_CREAT | O_EXCL, 0600)));
+    fd.reset(HANDLE_EINTR(SafeShmOpen(freezeable, name.c_str(),
+                                      O_RDWR | O_CREAT | O_EXCL, 0600)));
     if (fd) {
-      if (shm_unlink(name.c_str()) != 0) {
+      if (freezeable) {
+        frozen_fd.reset(HANDLE_EINTR(
+            SafeShmOpen(freezeable, name.c_str(), O_RDONLY, 0400)));
+        if (!frozen_fd) {
+          int open_err = errno;
+          SafeShmUnlink(freezeable, name.c_str());
+          DLOG(FATAL) << "failed to re-open freezeable shm: "
+                      << strerror(open_err);
+          return false;
+        }
+      }
+      if (SafeShmUnlink(freezeable, name.c_str()) != 0) {
         // This shouldn't happen, but if it does: assume the file is
         // in fact leaked, and bail out now while it's still 0-length.
         DLOG(FATAL) << "failed to unlink shm: " << strerror(errno);
@@ -156,7 +259,32 @@ bool SharedMemory::Create(size_t size) {
   }
 
   mapped_file_ = fd.release();
+  frozen_file_ = frozen_fd.release();
   max_size_ = size;
+  freezeable_ = freezeable;
+  return true;
+}
+
+bool SharedMemory::Freeze() {
+  DCHECK(!read_only_);
+  CHECK(freezeable_);
+  Unmap();
+
+#ifdef ANDROID
+  if (ioctl(mapped_file_, ASHMEM_SET_PROT_MASK, PROT_READ) != 0) {
+    CHROMIUM_LOG(WARNING) << "failed to freeze shm: " << strerror(errno);
+    return false;
+  }
+#else
+  DCHECK(frozen_file_ >= 0);
+  DCHECK(mapped_file_ >= 0);
+  close(mapped_file_);
+  mapped_file_ = frozen_file_;
+  frozen_file_ = -1;
+#endif
+
+  read_only_ = true;
+  freezeable_ = false;
   return true;
 }
 
@@ -206,6 +334,7 @@ void* SharedMemory::FindFreeAddressSpace(size_t size) {
 bool SharedMemory::ShareToProcessCommon(ProcessId processId,
                                         SharedMemoryHandle* new_handle,
                                         bool close_self) {
+  freezeable_ = false;
   const int new_fd = dup(mapped_file_);
   DCHECK(new_fd >= -1);
   new_handle->fd = new_fd;
@@ -225,12 +354,19 @@ void SharedMemory::Close(bool unmap_view) {
     close(mapped_file_);
     mapped_file_ = -1;
   }
+  if (frozen_file_ >= 0) {
+    CHROMIUM_LOG(WARNING) << "freezeable shared memory was never frozen";
+    close(frozen_file_);
+    frozen_file_ = -1;
+  }
 }
 
 mozilla::UniqueFileHandle SharedMemory::TakeHandle() {
   mozilla::UniqueFileHandle fh(mapped_file_);
   mapped_file_ = -1;
-  Unmap();
+  // Now that the main fd is removed, reset everything else: close the
+  // frozen fd if present and unmap the memory if mapped.
+  Close();
   return fh;
 }
 
