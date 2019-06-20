881c079b0be281dc5a74c3d1e0c9747f0df7c99f
Alexandre Poirot <poirot.alex@gmail.com>
2019-06-20 14:36:19 +0000

    Bug 1560278 - Prepend "Headless" in Browser.getVersion's product field when firefox runs headless. r=remote-protocol-reviewers,ato
    Differential Revision: https://phabricator.services.mozilla.com/D35416

--- remote/test/browser/browser_cdp.js
+++ remote/test/browser/browser_cdp.js
@@ -34,7 +34,8 @@ add_task(async function testCDP() {
   ok("Page" in client, "Page domain is available");
 
   const version = await Browser.getVersion();
-  is(version.product, "Firefox", "Browser.getVersion works");
+  const { isHeadless } = Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo);
+  is(version.product, isHeadless ? "Headless Firefox" : "Firefox", "Browser.getVersion works and depends on headless mode");
 
   // receive console.log messages and print them
   Log.enable();
