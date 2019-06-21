c00be0dd255ea87ae898022027a86f12cbd9f7e8
Nika Layzell <nika@thelayzells.com>
2019-06-21 16:24:55 +0000

    Bug 1560399 - Propagate remote type selection into subframes with Fission, r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D35451

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -212,8 +212,9 @@ class ContentParent final : public PContentParent,
    */
   static already_AddRefed<RemoteBrowser> CreateBrowser(
       const TabContext& aContext, Element* aFrameElement,
-      BrowsingContext* aBrowsingContext, ContentParent* aOpenerContentParent,
-      BrowserParent* aSameTabGroupAs, uint64_t aNextRemoteTabId);
+      const nsAString& aRemoteType, BrowsingContext* aBrowsingContext,
+      ContentParent* aOpenerContentParent, BrowserParent* aSameTabGroupAs,
+      uint64_t aNextRemoteTabId);
 
   static void GetAll(nsTArray<ContentParent*>& aArray);
 
