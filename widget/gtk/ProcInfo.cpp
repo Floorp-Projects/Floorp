a494b08766f53851312dc7a55e3ed1e2e600f8b8
Tarek Ziadé <tarek@mozilla.com>
2019-06-21 01:33:47 +0000

    Bug 1529022 - Add a GeckoChildProcessHosts iterator r=jld
    Adds GeckoChildProcessHost::GetAll() and use it in ChromeUtils::GetProcInfo()
    
    Differential Revision: https://phabricator.services.mozilla.com/D33920
    
    --HG--
    extra : moz-landing-system : lando

--- widget/gtk/ProcInfo.cpp
+++ widget/gtk/ProcInfo.cpp
@@ -208,8 +208,7 @@ class ThreadInfoReader final : public StatReader {
 };
 
 RefPtr<ProcInfoPromise> GetProcInfo(base::ProcessId pid, int32_t childId,
-                                    const ProcType& type,
-                                    ipc::GeckoChildProcessHost* childProcess) {
+                                    const ProcType& type) {
   auto holder = MakeUnique<MozPromiseHolder<ProcInfoPromise>>();
   RefPtr<ProcInfoPromise> promise = holder->Ensure(__func__);
   nsresult rv = NS_OK;
