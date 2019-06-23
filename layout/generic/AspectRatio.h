c17aaa433990e5383c97239c377a8fc0b334eb91
Emilio Cobos Álvarez <emilio@crisal.io>
2019-06-23 10:08:16 +0000

    Bug 1559094 - Restore old behavior for background-size: cover + zero-sized background positioning area. r=dholbert
    This restores our previous and per-spec behavior. Comparing only ratios was not
    correct in the case one of the dimensions was zero and thus not scaled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35571

--- layout/generic/AspectRatio.h
+++ layout/generic/AspectRatio.h
@@ -33,6 +33,11 @@ struct AspectRatio {
     return NSCoordSaturatingNonnegativeMultiply(aCoord, mRatio);
   }
 
+  float ApplyToFloat(float aFloat) const {
+    MOZ_DIAGNOSTIC_ASSERT(*this);
+    return mRatio * aFloat;
+  }
+
   // Inverts the ratio, in order to get the height / width ratio.
   MOZ_MUST_USE AspectRatio Inverted() const {
     return *this ? AspectRatio(1.0f / mRatio) : *this;
