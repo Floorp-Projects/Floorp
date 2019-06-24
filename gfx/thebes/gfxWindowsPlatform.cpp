166460ff5512e45804404455547ad4e43fe3552f
Nicholas Nethercote <nnethercote@mozilla.com>
2019-06-24 16:43:33 +0000

    Bug 1560833 - Remove setters for static VarCache prefs. r=froydnj
    Static VarCache prefs have setters. These are dangerous because they can
    violate the primary invariant of VarCache prefs, which is that the global
    variable always matches the pref value in the table.
    
    Fortunately they are only used in tests, and we can use vanilla pref setters
    instead, and get rid of the VarCache setters.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35632

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -1354,7 +1354,7 @@ void gfxWindowsPlatform::InitializeD3D11Config() {
       d3d11.Disable(FeatureStatus::Blacklisted, "Blacklisted, see bug 1351349",
                     NS_LITERAL_CSTRING("FEATURE_FAILURE_BUG_1351349"));
 #else
-      StaticPrefs::SetCompositorClearState(true);
+      Preferences::SetBool("gfx.compositor.clearstate", true);
 #endif
     }
   }
