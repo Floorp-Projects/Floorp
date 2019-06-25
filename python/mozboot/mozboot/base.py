2e21ab4391256f0a9ced9d4fc2a1b9523237c9c1
Mike Hommey <mh+mozilla@glandium.org>
2019-06-25 16:50:17 +0000

    Bug 1560826 - Make bootstrap upgrade mercurial when it is older than 4.8. r=nalexander
    4.3.3 is the strict minimum required for v-c-t's config wizard, but it
    is preferable people use more modern versions. We could go with 5.0, but
    it feels like people still using 4.8 and 4.9 don't really need to be
    bugged to update to a more recent version, they are kind of modern
    enough. OTOH MozillaBuild comes with 4.5.x, and this will force an
    upgrade for those.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35756

--- python/mozboot/mozboot/base.py
+++ python/mozboot/mozboot/base.py
@@ -143,9 +143,7 @@ ac_add_options --enable-artifact-builds
 '''
 
 # Upgrade Mercurial older than this.
-# This should match OLDEST_NON_LEGACY_VERSION from
-# the hg setup wizard in version-control-tools.
-MODERN_MERCURIAL_VERSION = LooseVersion('4.3.3')
+MODERN_MERCURIAL_VERSION = LooseVersion('4.8')
 
 # Upgrade Python older than this.
 MODERN_PYTHON_VERSION = LooseVersion('2.7.3')
