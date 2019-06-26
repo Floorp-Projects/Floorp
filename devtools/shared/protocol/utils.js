602a052063aa27f9002c2774e881a3a9f738c2f3
Jason Laster <jlaster@mozilla.com>
2019-06-26 15:53:20 +0000

    Bug 1560445 - Extract classes from protocol.js. r=yulia
    Differential Revision: https://phabricator.services.mozilla.com/D35502

---
+++ devtools/shared/protocol/utils.js
@@ -0,0 +1,76 @@
+"use strict";
+
+function describeTemplate(template) {
+  return JSON.parse(
+    JSON.stringify(template, (key, value) => {
+      if (value.describe) {
+        return value.describe();
+      }
+      return value;
+    })
+  );
+}
+
+exports.describeTemplate = describeTemplate;
+
+/**
+ * Find Placeholders in the template and save them along with their
+ * paths.
+ */
+function findPlaceholders(template, constructor, path = [], placeholders = []) {
+  if (!template || typeof template != "object") {
+    return placeholders;
+  }
+
+  if (template instanceof constructor) {
+    placeholders.push({ placeholder: template, path: [...path] });
+    return placeholders;
+  }
+
+  for (const name in template) {
+    path.push(name);
+    findPlaceholders(template[name], constructor, path, placeholders);
+    path.pop();
+  }
+
+  return placeholders;
+}
+
+exports.findPlaceholders = findPlaceholders;
+
+/**
+ * Get the value at a given path, or undefined if not found.
+ */
+function getPath(obj, path) {
+  for (const name of path) {
+    if (!(name in obj)) {
+      return undefined;
+    }
+    obj = obj[name];
+  }
+  return obj;
+}
+exports.getPath = getPath;
+
+/**
+ * Tags a prtotype method as an actor method implementation.
+ *
+ * @param function fn
+ *    The implementation function, will be returned.
+ * @param spec
+ *    The method specification, with the following (optional) properties:
+ *      request (object): a request template.
+ *      response (object): a response template.
+ *      oneway (bool): 'true' if no response should be sent.
+ */
+exports.method = function(fn, spec = {}) {
+  fn._methodSpec = Object.freeze(spec);
+  if (spec.request) {
+    Object.freeze(spec.request);
+  }
+  if (spec.response) {
+    Object.freeze(spec.response);
+  }
+  return fn;
+};
+
