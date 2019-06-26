3096a69833f0b6cf28b951e5372c8f3698bd0069
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-06-27 02:03:31 +0300

    Backed out 5 changesets (bug 1559982, bug 1559975) for cpp unit test failures due to unsupported operand type. CLOSED TREE Backed out changeset 2309aac6cea3 (bug 1559975) Backed out changeset bf5a6d853abd (bug 1559975) Backed out changeset 0c36f78c971a (bug 1559975) Backed out changeset 0560cf7df7dc (bug 1559975) Backed out changeset 4cb64ca524e7 (bug 1559982)

--- testing/runcppunittests.py
+++ testing/runcppunittests.py
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function, with_statement
+from __future__ import with_statement
 import sys
 import os
 from optparse import OptionParser
