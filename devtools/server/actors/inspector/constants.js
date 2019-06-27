e572b4c130c2dd1238522789e1c6638f04e374b4
Logan Smyth <loganfsmyth@gmail.com>
2019-06-27 06:23:54 +0000

    Bug 1561707 - Support excluding listeners from the event collector list. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D36076
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/server/actors/inspector/constants.js
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/**
+ * Any event listener flagged with this symbol will not be considered when
+ * the EventCollector class enumerates listeners for nodes. For example:
+ *
+ *   const someListener = () => {};
+ *   someListener[EXCLUDED_LISTENER] = true;
+ *   eventListenerService.addSystemEventListener(node, "event", someListener);
+ */
+const EXCLUDED_LISTENER = Symbol("event-collector-excluded-listener");
+
+exports.EXCLUDED_LISTENER = EXCLUDED_LISTENER;
