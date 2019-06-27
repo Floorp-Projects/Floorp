5e7040aac6ac15d1e13f1fb6af79531a6b4d61c8
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2019-06-27 01:56:22 +0000

    Bug 1488953 - Wait for a scrollend event to make sure the scrolling has finished before proceeding the test. r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D33614

--- browser/base/content/test/tabs/browser_tabCloseSpacer.js
+++ browser/base/content/test/tabs/browser_tabCloseSpacer.js
@@ -65,7 +65,11 @@ async function overflowTabs() {
       index: 0,
     });
   }
-  await window.promiseDocumentFlushed(() => {});
+
+  // Make sure scrolling finished.
+  await new Promise(resolve => {
+    arrowScrollbox.addEventListener("scrollend", resolve, { once: true });
+  });
 }
 
 function getLastCloseButton() {
