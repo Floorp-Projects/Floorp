362635a00336f1e82812f82cf8da930d74201593
Jon Coppeard <jcoppeard@mozilla.com>
2019-06-27 11:56:37 +0100

    Bug 1564072 - Allow ZoneAllocPolicy to be moved r=sfink
    Patch to allow ZoneAllocPolicy insstance to be moved after having memory accounted to it. This is useful in the situation where a data structure is populated before being attached to a GC thing by moving it. The code will currently assert (in unregisterPolicy()) if a ZoneAllocPolicy is copied after having memory accounted to it. This could be allowed but it seems better to force users to move in this situation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36177

--- js/src/gc/ZoneAllocator.h
+++ js/src/gc/ZoneAllocator.h
@@ -34,7 +34,7 @@ class ZoneAllocator : public JS::shadow::Zone,
 
  public:
   static ZoneAllocator* from(JS::Zone* zone) {
-    // This is a safe downcast, but the compiler hasn't seen the definition yet.
+    // This is a safe upcast, but the compiler hasn't seen the definition yet.
     return reinterpret_cast<ZoneAllocator*>(zone);
   }
 
@@ -104,6 +104,9 @@ class ZoneAllocator : public JS::shadow::Zone,
   }
   void unregisterPolicy(js::ZoneAllocPolicy* policy) {
     return gcMallocTracker.unregisterPolicy(policy);
+  }
+  void movePolicy(js::ZoneAllocPolicy* dst, js::ZoneAllocPolicy* src) {
+    return gcMallocTracker.movePolicy(dst, src);
   }
 #endif
 
@@ -215,13 +218,37 @@ class ZoneAllocPolicy : public MallocProvider<ZoneAllocPolicy> {
 #endif
   }
   ZoneAllocPolicy(ZoneAllocPolicy& other) : ZoneAllocPolicy(other.zone_) {}
-  ZoneAllocPolicy(ZoneAllocPolicy&& other) : ZoneAllocPolicy(other.zone_) {}
+  ZoneAllocPolicy(ZoneAllocPolicy&& other) : zone_(other.zone_) {
+#ifdef DEBUG
+    zone()->movePolicy(this, &other);
+#endif
+    other.zone_ = nullptr;
+  }
   ~ZoneAllocPolicy() {
 #ifdef DEBUG
     if (zone_) {
       zone_->unregisterPolicy(this);
     }
 #endif
+  }
+
+  ZoneAllocPolicy& operator=(const ZoneAllocPolicy& other) {
+#ifdef DEBUG
+    zone()->unregisterPolicy(this);
+#endif
+    zone_ = other.zone();
+#ifdef DEBUG
+    zone()->registerPolicy(this);
+#endif
+    return *this;
+  }
+  ZoneAllocPolicy& operator=(ZoneAllocPolicy&& other) {
+#ifdef DEBUG
+    zone()->unregisterPolicy(this);
+    zone()->movePolicy(this, &other);
+#endif
+    other.zone_ = nullptr;
+    return *this;
   }
 
   // Public methods required to fulfill the AllocPolicy interface.
