54e44e3496536e3a0f73c1bffa7cb5989cb6fed0
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-06-27 05:45:20 +0300

    Backed out 2 changesets (bug 1528031) for causing crashtests to time out. CLOSED TREE
    Backed out changeset 27ce9b212d26 (bug 1528031)
    Backed out changeset 0a18aa212ee6 (bug 1528031)

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -6358,7 +6358,7 @@ VARCACHE_PREF(
   Live,
   "media.devices.insecure.enabled",
   media_devices_insecure_enabled,
-  bool, false
+  bool, true
 )
 
 // If the above pref is also enabled, this pref enabled getUserMedia() support
