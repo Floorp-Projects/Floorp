48a4b632cbd1b4b1d48ecc6763b75aff6edc21af
Kathy Brade <brade@pearlcrescent.com>
2019-06-27 09:11:48 +0000

    Bug 1561636 - intermittent updater failures on Win64 (Error 19) r=rstrong
    Avoid MinGW's _ftelli64() and _fseeki64() implementations because they
    are unreliable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36037

--- modules/libmar/src/mar_private.h
+++ modules/libmar/src/mar_private.h
@@ -55,8 +55,15 @@ MOZ_STATIC_ASSERT(sizeof(BLOCKSIZE) <
    instead of the NSPR equivalents. */
 #ifdef XP_WIN
 #include <winsock2.h>
+#ifdef __MINGW32__
+/* Avoid MinGW's _ftelli64() and _fseeki64() implementations because they
+ * are unreliable. */
+#define ftello ftello64
+#define fseeko fseeko64
+#else
 #define ftello _ftelli64
 #define fseeko _fseeki64
+#endif
 #else
 #define _FILE_OFFSET_BITS 64
 #include <netinet/in.h>
