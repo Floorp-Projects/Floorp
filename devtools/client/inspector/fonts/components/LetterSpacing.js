Floorp-Projects/Floorp

commit 3817b026fa5ebe8ce250b0bcd1e1a060079a6990

Bug 1559589 - Ensure Font Editor supports "normal" as computed style value for line-height; r=gl
Remove conversion for computed `line-height` value to unitless value to prevent Font Editor from crashing when the patch from [Bug 1536871](https://bugzilla.mozilla.org/show_bug.cgi?id=1536871) is applied (the default computed value for `line-height` is no longer numeric).

Add support for `normal` keyword value for `line-height` as the default returned by `getComputedStyle()`, similar to existing behavior for `letter-spacing`.

Fix misnomer of the class in `LetterSpacing.js` component.

Differential Revision: https://phabricator.services.mozilla.com/D35394

devtools/client/inspector/fonts/components/LetterSpacing.js
@@ -12,7 +12,7 @@ const FontPropertyValue = createFactory(require("./FontPropertyValue"));
 const { getStr } = require("../utils/l10n");
 const { getUnitFromValue, getStepForUnit } = require("../utils/font-utils");
 
-class LineHeight extends PureComponent {
+class LetterSpacing extends PureComponent {
   static get propTypes() {
     return {
       disabled: PropTypes.bool.isRequired,
@@ -55,9 +55,8 @@ class LineHeight extends PureComponent {
     const unit = getUnitFromValue(this.props.value) || "em";
     // When the initial value of "letter-spacing" is "normal", the parsed value
     // is not a number (NaN). Guard by setting the default value to 0.
-    let value = parseFloat(this.props.value);
-    const hasKeywordValue = isNaN(value);
-    value = isNaN(value) ? 0 : value;
+    const isKeywordValue = this.props.value === "normal";
+    const value = isKeywordValue ? 1.2 : parseFloat(this.props.value);
 
     let { min, max } = this.getDefaultMinMax(unit);
     min = Math.min(min, value);
@@ -82,15 +81,15 @@ class LineHeight extends PureComponent {
       // Increase the increment granularity because letter spacing is very sensitive.
       step: getStepForUnit(unit) / 100,
       // Show the value input and unit only when the value is not a keyword.
-      showInput: !hasKeywordValue,
-      showUnit: !hasKeywordValue,
+      showInput: !isKeywordValue,
+      showUnit: !isKeywordValue,
       unit,
       unitOptions: ["em", "rem", "px"],
       value,
       // Show the value as a read-only label if it's a keyword.
-      valueLabel: hasKeywordValue ? this.props.value : null,
+      valueLabel: isKeywordValue ? this.props.value : null,
     });
   }
 }
 
-module.exports = LineHeight;
+module.exports = LetterSpacing;

devtools/client/inspector/fonts/components/LineHeight.js
@@ -27,7 +27,13 @@ class LineHeight extends PureComponent {
   }
 
   render() {
-    const value = parseFloat(this.props.value);
+    // When the initial value of "line-height" is "normal", the parsed value
+    // is not a number (NaN). Guard by setting the default value to 1.2.
+    // This will be the starting point for changing the value by dragging the slider.
+    // @see https://searchfox.org/mozilla-central/rev/1133b6716d9a8131c09754f3f29288484896b8b6/layout/generic/ReflowInput.cpp#2786
+    const isKeywordValue = this.props.value === "normal";
+    const value = isKeywordValue ? 1.2 : parseFloat(this.props.value);
+
     // When values for line-height are be unitless, getUnitFromValue() returns null.
     // In that case, set the unit to an empty string for special treatment in conversion.
     const unit = getUnitFromValue(this.props.value) || "";
@@ -70,9 +76,14 @@ class LineHeight extends PureComponent {
       name: "line-height",
       onChange: this.props.onChange,
       step: getStepForUnit(unit),
+      // Show the value input and unit only when the value is not a keyword.
+      showInput: !isKeywordValue,
+      showUnit: !isKeywordValue,
       unit,
       unitOptions: ["", "em", "%", "px"],
       value,
+      // Show the value as a read-only label if it's a keyword.
+      valueLabel: isKeywordValue ? this.props.value : null,
     });
   }
 }

devtools/client/inspector/fonts/fonts.js
@@ -61,8 +61,6 @@ class FontInspector {
     this.cssProperties = inspector.cssProperties;
     this.document = window.document;
     this.inspector = inspector;
-    // Set of unique keyword values supported by designated font properties.
-    this.keywordValues = new Set(this.getFontPropertyValueKeywords());
     // Selected node in the markup view. For text nodes, this points to their parent node
     // element. Font faces and font properties for this node will be shown in the editor.
     this.node = null;
@@ -91,6 +89,35 @@ class FontInspector {
     this.updateFontVariationSettings = this.updateFontVariationSettings.bind(this);
 
     this.init();
+  }
+
+  /**
+   * Map CSS font property names to a list of values that should be skipped when consuming
+   * font properties from CSS rules. The skipped values are mostly keyword values like
+   * `bold`, `initial`, `unset`. Computed values will be used instead of such keywords.
+   *
+   * @return {Map}
+   */
+  get skipValuesMap() {
+    if (!this._skipValuesMap) {
+      this._skipValuesMap = new Map();
+
+      for (const property of FONT_PROPERTIES) {
+        const values = this.cssProperties.getValues(property);
+
+        switch (property) {
+          case "line-height":
+          case "letter-spacing":
+            // There's special handling for "normal" so remove it from the skip list.
+            this.skipValuesMap.set(property, values.filter(value => value !== "normal"));
+            break;
+          default:
+            this.skipValuesMap.set(property, values);
+        }
+      }
+    }
+
+    return this._skipValuesMap;
   }
 
   init() {
@@ -310,14 +337,6 @@ class FontInspector {
           : "";
     }
 
-    // Convert computed value for line-height from pixels to unitless.
-    // If it is not overwritten by an explicit line-height CSS declaration,
-    // this will be the implicit value shown in the editor.
-
-    properties["line-height"] =
-      await this.convertUnits("line-height", parseFloat(properties["line-height"]),
-                              "px", "");
-
     // Then, replace with enabled font properties found on any of the rules that apply.
     for (const rule of this.ruleView.rules) {
       if (rule.inherited) {
@@ -326,7 +345,7 @@ class FontInspector {
 
       for (const textProp of rule.textProps) {
         if (FONT_PROPERTIES.includes(textProp.name) &&
-            !this.keywordValues.has(textProp.value) &&
+            !this.skipValuesMap.get(textProp.name).includes(textProp.value) &&
             !textProp.value.includes("calc(") &&
             !textProp.value.includes("var(") &&
             !textProp.overridden &&
@@ -337,31 +356,6 @@ class FontInspector {
     }
 
     return properties;
-  }
-
-  /**
-   * Get an array of keyword values supported by the following CSS properties:
-   * - font-size
-   * - font-weight
-   * - font-stretch
-   * - letter-spacing
-   * - line-height
-   *
-   * This list is used to filter out values when reading CSS font properties from rules.
-   * Computed styles will be used instead of any of these values.
-   *
-   * @return {Array}
-   */
-  getFontPropertyValueKeywords() {
-    return [
-      "font-size",
-      "font-weight",
-      "font-stretch",
-      "letter-spacing",
-      "line-height",
-    ].reduce((acc, property) => {
-      return acc.concat(this.cssProperties.getValues(property));
-    }, []);
   }
 
   async getFontsForNode(node, options) {

commit 66fd46bab621d3c0a8d006a8d0b99ecaab942e53

Bug 1561871 - Restore default numeric value for letter-spacing to 0 in the Font Editor. r=gl
Fix for accidental regression in D35394 form careless copy / paste.

Differential Revision: https://phabricator.services.mozilla.com/D36180

devtools/client/inspector/fonts/components/LetterSpacing.js
@@ -56,7 +56,7 @@ class LetterSpacing extends PureComponent {
     // When the initial value of "letter-spacing" is "normal", the parsed value
     // is not a number (NaN). Guard by setting the default value to 0.
     const isKeywordValue = this.props.value === "normal";
-    const value = isKeywordValue ? 1.2 : parseFloat(this.props.value);
+    const value = isKeywordValue ? 0 : parseFloat(this.props.value);
 
     let { min, max } = this.getDefaultMinMax(unit);
     min = Math.min(min, value);