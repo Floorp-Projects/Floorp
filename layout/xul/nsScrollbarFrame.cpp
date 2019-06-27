38b15bc9cfb63b7de43cb1a7d3504d2b8c00a368
Cameron McCormack <cam@mcc.id.au>
2019-06-27 07:22:20 +0000

    Bug 1554571 - Part 2: Remove unused sbattr="scrollbar-thumb" from XUL thumb elements. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D33122

--- layout/xul/nsScrollbarFrame.cpp
+++ layout/xul/nsScrollbarFrame.cpp
@@ -284,19 +284,6 @@ int32_t nsScrollbarFrame::MoveToNewPosition() {
 
 nsresult nsScrollbarFrame::CreateAnonymousContent(
     nsTArray<ContentInfo>& aElements) {
-  // clang-format off
-
-  // <xul:scrollbarbutton sbattr="scrollbar-up-top" type="decrement" xbl:inherits="curpos,maxpos,disabled"/>
-  // <xul:scrollbarbutton sbattr="scrollbar-down-top" type="increment" xbl:inherits="curpos,maxpos,disabled"/>
-  // <xul:slider flex="1" xbl:inherits="disabled,curpos,maxpos,pageincrement,increment,orient">
-  //   <xul:thumb sbattr="scrollbar-thumb" xbl:inherits="orient,collapsed=disabled"
-  //              align="center" pack="center"/>
-  // </xul:slider>
-  // <xul:scrollbarbutton sbattr="scrollbar-up-bottom" type="decrement" xbl:inherits="curpos,maxpos,disabled"/>
-  // <xul:scrollbarbutton sbattr="scrollbar-down-bottom" type="increment" xbl:inherits="curpos,maxpos,disabled"/>
-
-  // clang-format on
-
   nsNodeInfoManager* nodeInfoManager = mContent->NodeInfo()->NodeInfoManager();
 
   Element* el(GetContent()->AsElement());
@@ -352,8 +339,6 @@ nsresult nsScrollbarFrame::CreateAnonymousContent(
       getter_AddRefs(mThumb),
       nodeInfoManager->GetNodeInfo(nsGkAtoms::thumb, nullptr, kNameSpaceID_XUL,
                                    nsINode::ELEMENT_NODE));
-  mThumb->SetAttr(kNameSpaceID_None, nsGkAtoms::sbattr,
-                  NS_LITERAL_STRING("scrollbar-thumb"), false);
   mThumb->SetAttr(kNameSpaceID_None, nsGkAtoms::orient, orient, false);
   mThumb->SetAttr(kNameSpaceID_None, nsGkAtoms::align,
                   NS_LITERAL_STRING("center"), false);
