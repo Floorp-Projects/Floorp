5c3ad0372c60d6bab0e7a2b0c2ac98911b0979dd
Oana Pop Rus <opoprus@mozilla.com>
2019-06-27 14:29:44 +0300

    Backed out changeset 3fdff178b58d (bug 1558898) for multiple failure on browser_private_clicktoplay.js

--- browser/base/content/test/plugins/browser_private_clicktoplay.js
+++ browser/base/content/test/plugins/browser_private_clicktoplay.js
@@ -125,8 +125,8 @@ add_task(async function test2c() {
                                                    "Shown");
   popupNotification.reshow();
   await promiseShown;
-  is(gPrivateWindow.PopupNotifications.panel.firstElementChild.secondaryButton.hidden, false,
-     "Test 2c, Activated plugin in a private window should have visible 'Block' button.");
+  is(gPrivateWindow.PopupNotifications.panel.firstElementChild.secondaryButton.hidden, true,
+     "Test 2c, Activated plugin in a private window should not have visible 'Block' button.");
   is(gPrivateWindow.PopupNotifications.panel.firstElementChild.checkbox.hidden, true,
      "Test 2c, Activated plugin in a private window should not have visible 'Remember' checkbox.");
 
