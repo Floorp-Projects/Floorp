7ce0967cb9a30b23725257e4f974a1cc42dbc144
Andrew Osmond <aosmond@mozilla.com>
2019-06-28 10:44:11 -0400

    Bug 1409773 - Use WebRender instead of fallback for border raster images. r=jrmuizel a=RyanVM
    Before this patch, we would use fallback for all border images. Now for
    all but vector images we will use the WebRender border images
    primitives. Vector images are an exception because the fallback is
    clever in that it upscales the vector image and clips to only draw the
    region it requires. This avoids artifacting but to do something similar
    for WebRender as it is currently defined, we would increase our CPU and
    memory footprint as we would need to produce the entire vector image
    upscaled, not just the parts we need. In the future we should change
    WebRender to accept different image resources for each of the segments.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37093

--- layout/painting/nsCSSRenderingBorders.cpp
+++ layout/painting/nsCSSRenderingBorders.cpp
@@ -3577,7 +3577,11 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
   NS_FOR_CSS_SIDES(i) {
     slice[i] = (float)(mSlice.Side(i)) / appUnitsPerDevPixel;
     widths[i] = (float)(mWidths.Side(i)) / appUnitsPerDevPixel;
-    outset[i] = (float)(mImageOutset.Side(i)) / appUnitsPerDevPixel;
+
+    // The outset is already taken into account by the adjustments to mArea
+    // in our constructor. We use mArea as our dest rect so we can just supply
+    // zero outsets to WebRender.
+    outset[i] = 0.0f;
   }
 
   LayoutDeviceRect destRect =
@@ -3595,6 +3599,19 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
   ImgDrawResult drawResult = ImgDrawResult::SUCCESS;
   switch (mImageRenderer.GetType()) {
     case eStyleImageType_Image: {
+      RefPtr<imgIContainer> img = mImageRenderer.GetImage();
+      if (!img || img->GetType() == imgIContainer::TYPE_VECTOR) {
+        // Vector images will redraw each segment of the border up to 8 times.
+        // We draw using a restricted region derived from the segment's clip and
+        // scale the image accordingly (see ClippedImage::Draw). If we follow
+        // this convention as is for WebRender, we will need to rasterize the
+        // entire vector image scaled up without the restriction region, which
+        // means our main thread CPU and memory footprints will be much higher.
+        // Ideally we would be able to provide a raster image for each segment
+        // of the border. For now we use fallback.
+        return ImgDrawResult::NOT_SUPPORTED;
+      }
+
       uint32_t flags = imgIContainer::FLAG_ASYNC_NOTIFY;
       if (aDisplayListBuilder->IsPaintingToWindow()) {
         flags |= imgIContainer::FLAG_HIGH_QUALITY_SCALING;
@@ -3603,11 +3620,13 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
         flags |= imgIContainer::FLAG_SYNC_DECODE;
       }
 
-      RefPtr<imgIContainer> img = mImageRenderer.GetImage();
+      LayoutDeviceRect imageRect = LayoutDeviceRect::FromAppUnits(
+          nsRect(nsPoint(), mImageRenderer.GetSize()), appUnitsPerDevPixel);
+
       Maybe<SVGImageContext> svgContext;
       gfx::IntSize decodeSize =
           nsLayoutUtils::ComputeImageContainerDrawingParameters(
-              img, aForFrame, destRect, aSc, flags, svgContext);
+              img, aForFrame, imageRect, aSc, flags, svgContext);
 
       RefPtr<layers::ImageContainer> container;
       drawResult = img->GetImageContainerAtSize(aManager->LayerManager(),
@@ -3627,15 +3646,18 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
         break;
       }
 
-      aBuilder.PushBorderImage(
-          dest, clip, !aItem->BackfaceIsHidden(),
+      wr::WrBorderImage params{
           wr::ToBorderWidths(widths[0], widths[1], widths[2], widths[3]),
-          key.value(), (float)(mImageSize.width) / appUnitsPerDevPixel,
-          (float)(mImageSize.height) / appUnitsPerDevPixel,
+          key.value(),
+          mImageSize.width / appUnitsPerDevPixel,
+          mImageSize.height / appUnitsPerDevPixel,
+          mFill,
           wr::ToSideOffsets2D_i32(slice[0], slice[1], slice[2], slice[3]),
           wr::ToSideOffsets2D_f32(outset[0], outset[1], outset[2], outset[3]),
           wr::ToRepeatMode(mRepeatModeHorizontal),
-          wr::ToRepeatMode(mRepeatModeVertical));
+          wr::ToRepeatMode(mRepeatModeVertical)};
+
+      aBuilder.PushBorderImage(dest, clip, !aItem->BackfaceIsHidden(), params);
       break;
     }
     case eStyleImageType_Gradient: {
@@ -3661,7 +3683,7 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
             dest, clip, !aItem->BackfaceIsHidden(),
             wr::ToBorderWidths(widths[0], widths[1], widths[2], widths[3]),
             (float)(mImageSize.width) / appUnitsPerDevPixel,
-            (float)(mImageSize.height) / appUnitsPerDevPixel,
+            (float)(mImageSize.height) / appUnitsPerDevPixel, mFill,
             wr::ToSideOffsets2D_i32(slice[0], slice[1], slice[2], slice[3]),
             wr::ToLayoutPoint(startPoint), wr::ToLayoutPoint(endPoint), stops,
             extendMode,
@@ -3671,8 +3693,8 @@ ImgDrawResult nsCSSBorderImageRenderer::CreateWebRenderCommands(
         aBuilder.PushBorderRadialGradient(
             dest, clip, !aItem->BackfaceIsHidden(),
             wr::ToBorderWidths(widths[0], widths[1], widths[2], widths[3]),
-            wr::ToLayoutPoint(lineStart), wr::ToLayoutSize(gradientRadius),
-            stops, extendMode,
+            mFill, wr::ToLayoutPoint(lineStart),
+            wr::ToLayoutSize(gradientRadius), stops, extendMode,
             wr::ToSideOffsets2D_f32(outset[0], outset[1], outset[2],
                                     outset[3]));
       }
