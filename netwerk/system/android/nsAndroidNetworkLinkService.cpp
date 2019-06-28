3f81ce5a9d648c139e64f2056dfb50879a5f3fe0
Valentin Gosu <valentin.gosu@gmail.com>
2019-06-28 11:42:05 +0000

    Bug 1561005 - Add empty GetNetworkID impl for Android r=michal
    Differential Revision: https://phabricator.services.mozilla.com/D35883

--- netwerk/system/android/nsAndroidNetworkLinkService.cpp
+++ netwerk/system/android/nsAndroidNetworkLinkService.cpp
@@ -52,3 +52,9 @@ nsAndroidNetworkLinkService::GetLinkType(uint32_t* aLinkType) {
   *aLinkType = java::GeckoAppShell::GetNetworkLinkType();
   return NS_OK;
 }
+
+NS_IMETHODIMP
+nsAndroidNetworkLinkService::GetNetworkID(nsACString& aNetworkID) {
+  aNetworkID.Truncate();
+  return NS_OK;
+}
