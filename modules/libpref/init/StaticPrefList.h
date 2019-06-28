a13f950256b5d0a1c1d95c7159d75bdb8f6db3d2
Cameron McCormack <cam@mcc.id.au>
2019-06-28 05:00:35 +0000

    Bug 1561773 - Enable anonymous content style caching. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D36140
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -4708,7 +4708,7 @@ VARCACHE_PREF(
   Live,
   "layout.css.cached-scrollbar-styles.enabled",
   layout_css_cached_scrollbar_styles_enabled,
-  bool, false
+  bool, true
 )
 
 // Is path() supported in clip-path?
