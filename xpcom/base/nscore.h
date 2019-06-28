72cdd37986359b9456c15ac49248fa0cefc1800c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-06-28 16:38:14 +0000

    Bug 1557762 - ensure we define NS_FREE_PERMANENT_DATA for single-stage pgo builds, r=chmanchester
    Differential Revision: https://phabricator.services.mozilla.com/D35469
    
    --HG--
    extra : moz-landing-system : lando

--- xpcom/base/nscore.h
+++ xpcom/base/nscore.h
@@ -172,10 +172,12 @@
  * if we're using _exit. Bug 1555974 covers improving this.
  *
  */
+#ifndef NS_FREE_PERMANENT_DATA
 #if defined(NS_BUILD_REFCNT_LOGGING) || defined(MOZ_VALGRIND) || \
     defined(MOZ_ASAN) || defined(MOZ_CODE_COVERAGE) ||           \
     defined(MOZ_PROFILE_GENERATE)
 #  define NS_FREE_PERMANENT_DATA
+#endif
 #endif
 
 /**
