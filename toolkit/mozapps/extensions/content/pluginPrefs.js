4c6abe45cec2292cfbcb97de4102f34f8a52b44f
Rob Wu <rob@robwu.nl>
2019-06-28 23:48:06 +0000

    Bug 1561827 - Move pluginPrefs initialization to pluginPrefs.js. r=Gijs, a=jcristau
    The plugin preference rendering logic was in PluginProvider.jsm and
    was tightly coupled to the markup of the XUL about:addons page,
    and therefore does not work with the HTML about:addons page.
    
    Fix this by moving the rendering logic to pluginPrefs.js (which is
    loaded by pluginPrefs.xul), and updating the browser_pluginprefs.js unit
    test to ensure that the rendering works as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36505

--- toolkit/mozapps/extensions/content/pluginPrefs.js
+++ toolkit/mozapps/extensions/content/pluginPrefs.js
@@ -5,6 +5,7 @@
 "use strict";
 
 const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { AddonManager } = ChromeUtils.import("resource://gre/modules/AddonManager.jsm");
 
 const PREFS = {
   "pluginFlashBlockingCheckbox":
@@ -13,7 +14,43 @@ const PREFS = {
     { pref: "dom.ipc.plugins.flash.disable-protected-mode", invert: true },
 };
 
+async function renderPluginMetadata(id) {
+  let plugin = await AddonManager.getAddonByID(id);
+  if (!plugin)
+    return;
+
+  let libLabel = document.getElementById("pluginLibraries");
+  libLabel.textContent = plugin.pluginLibraries.join(", ");
+
+  let typeLabel = document.getElementById("pluginMimeTypes"), types = [];
+  for (let type of plugin.pluginMimeTypes) {
+    let extras = [type.description.trim(), type.suffixes].
+                 filter(x => x).join(": ");
+    types.push(type.type + (extras ? " (" + extras + ")" : ""));
+  }
+  typeLabel.textContent = types.join(",\n");
+  let showProtectedModePref = canDisableFlashProtectedMode(plugin);
+  document.getElementById("pluginEnableProtectedMode")
+    .setAttribute("collapsed", showProtectedModePref ? "" : "true");
+}
+
+function isFlashPlugin(aPlugin) {
+  for (let type of aPlugin.pluginMimeTypes) {
+    if (type.type == "application/x-shockwave-flash") {
+      return true;
+    }
+  }
+  return false;
+}
+// Protected mode is win32-only, not win64
+function canDisableFlashProtectedMode(aPlugin) {
+  return isFlashPlugin(aPlugin) && Services.appinfo.XPCOMABI == "x86-msvc";
+}
+
 function init() {
+  let params = new URLSearchParams(location.hash.slice(1));
+  renderPluginMetadata(params.get("id"));
+
   for (let id of Object.keys(PREFS)) {
     let checkbox = document.getElementById(id);
     var prefVal = Services.prefs.getBoolPref(PREFS[id].pref);
