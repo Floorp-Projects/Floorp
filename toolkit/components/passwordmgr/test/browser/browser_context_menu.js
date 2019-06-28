790aa6af69ffb53d55e8b6ef9c4a22015ae5d885
Sam Foster <sfoster@mozilla.com>
2019-06-28 22:03:57 +0000

    Bug 1548391 - Add a contextmenu item to fill a password field with a generated password. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D35592
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/passwordmgr/test/browser/browser_context_menu.js
+++ toolkit/components/passwordmgr/test/browser/browser_context_menu.js
@@ -289,39 +289,6 @@ add_task(async function test_context_menu_open_management() {
 });
 
 /**
- * Synthesize mouse clicks to open the password manager context menu popup
- * for a target password input element.
- *
- * assertCallback should return true if we should continue or else false.
- */
-async function openPasswordContextMenu(browser, passwordInput, assertCallback = null) {
-  let contextMenuShownPromise = BrowserTestUtils.waitForEvent(CONTEXT_MENU, "popupshown");
-
-  // Synthesize a right mouse click over the password input element, we have to trigger
-  // both events because formfill code relies on this event happening before the contextmenu
-  // (which it does for real user input) in order to not show the password autocomplete.
-  let eventDetails = {type: "mousedown", button: 2};
-  await BrowserTestUtils.synthesizeMouseAtCenter(passwordInput, eventDetails, browser);
-  // Synthesize a contextmenu event to actually open the context menu.
-  eventDetails = {type: "contextmenu", button: 2};
-  await BrowserTestUtils.synthesizeMouseAtCenter(passwordInput, eventDetails, browser);
-
-  await contextMenuShownPromise;
-
-  if (assertCallback) {
-    let shouldContinue = await assertCallback();
-    if (!shouldContinue) {
-      return;
-    }
-  }
-
-  // Synthesize a mouse click over the fill login menu header.
-  let popupShownPromise = BrowserTestUtils.waitForCondition(() => POPUP_HEADER.open);
-  EventUtils.synthesizeMouseAtCenter(POPUP_HEADER, {});
-  await popupShownPromise;
-}
-
-/**
  * Verify that only the expected form fields are filled.
  */
 async function assertContextMenuFill(browser, formId, usernameFieldId, passwordFieldId, loginIndex) {
