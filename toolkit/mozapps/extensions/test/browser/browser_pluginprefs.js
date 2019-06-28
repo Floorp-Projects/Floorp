4cc30a27d08cdebbd346e10f7254b600d7a56306
Rob Wu <rob@robwu.nl>
2019-06-28 23:48:06 +0000

    Bug 1561827 - Move pluginPrefs initialization to pluginPrefs.js r=Gijs a=jcristau
    The plugin preference rendering logic was in PluginProvider.jsm and
    was tightly coupled to the markup of the XUL about:addons page,
    and therefore does not work with the HTML about:addons page.
    
    Fix this by moving the rendering logic to pluginPrefs.js (which is
    loaded by pluginPrefs.xul), and updating the browser_pluginprefs.js unit
    test to ensure that the rendering works as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36185

--- toolkit/mozapps/extensions/test/browser/browser_pluginprefs.js
+++ toolkit/mozapps/extensions/test/browser/browser_pluginprefs.js
@@ -4,67 +4,72 @@
 
 // Tests the detail view of plugins
 
-// This test is testing plugin preferences in the XUL about:addons UI, the HTML about:addons
-// doesn't yet supported (tracked by Bug 1552537) and this test cases will be part of the
-// HTML about:addons test files.
-SpecialPowers.pushPrefEnv({
-  set: [["extensions.htmlaboutaddons.enabled", false]],
-});
-
 var gManagerWindow;
 
-async function test() {
-  waitForExplicitFinish();
-
-  let aWindow = await open_manager("addons://list/plugin");
-  gManagerWindow = aWindow;
-
-  run_next_test();
+async function getTestPluginAddon() {
+  const plugins = await AddonManager.getAddonsByTypes(["plugin"]);
+  return plugins.find(plugin => plugin.name === "Test Plug-in");
 }
 
-async function end_test() {
-  await close_manager(gManagerWindow);
-  finish();
-}
+async function test_inline_plugin_prefs() {
+  gManagerWindow = await open_manager("addons://list/plugin");
+  let testPlugin = await getTestPluginAddon();
+  ok(testPlugin, "Test Plug-in should exist");
 
-add_test(async function() {
-  let plugins = await AddonManager.getAddonsByTypes(["plugin"]);
-  let testPluginId;
-  for (let plugin of plugins) {
-    if (plugin.name == "Test Plug-in") {
-      testPluginId = plugin.id;
-      break;
-    }
+  let pluginEl = get_addon_element(gManagerWindow, testPlugin.id);
+  is(testPlugin.optionsType, AddonManager.OPTIONS_TYPE_INLINE_BROWSER, "Options should be inline type");
+
+  let optionsBrowserPromise =
+    BrowserTestUtils.waitForEvent(pluginEl.ownerDocument, "load", true, event => {
+      let {target} = event;
+      return target.currentURI && target.currentURI.spec === testPlugin.optionsURL;
+    }).then(event => event.target);
+
+  if (gManagerWindow.useHtmlViews) {
+    pluginEl.querySelector("panel-item[action='preferences']").click();
+  } else {
+    pluginEl.parentNode.ensureElementIsVisible(pluginEl);
+
+    let button = gManagerWindow.document.getAnonymousElementByAttribute(pluginEl, "anonid", "preferences-btn");
+    is_element_visible(button, "Preferences button should be visible");
+
+    EventUtils.synthesizeMouseAtCenter(pluginEl, { clickCount: 1 }, gManagerWindow);
+
+    await TestUtils.topicObserved(AddonManager.OPTIONS_NOTIFICATION_DISPLAYED);
   }
-  ok(testPluginId, "Test Plug-in should exist");
 
-  let testPlugin = await AddonManager.getAddonByID(testPluginId);
-  let pluginEl = get_addon_element(gManagerWindow, testPluginId);
-  is(pluginEl.mAddon.optionsType, AddonManager.OPTIONS_TYPE_INLINE_BROWSER, "Options should be inline type");
-  pluginEl.parentNode.ensureElementIsVisible(pluginEl);
+  info("Waiting for inline options page to be ready");
+  let doc = (await optionsBrowserPromise).contentDocument;
 
-  let button = gManagerWindow.document.getAnonymousElementByAttribute(pluginEl, "anonid", "preferences-btn");
-  is_element_visible(button, "Preferences button should be visible");
+  await BrowserTestUtils.waitForCondition(() => {
+    // Wait until pluginPrefs.js has initialized the plugin pref view.
+    return doc.getElementById("pluginLibraries").textContent;
+  }, "Waiting for pluginPrefs to finish rendering");
 
-  EventUtils.synthesizeMouseAtCenter(pluginEl, { clickCount: 1 }, gManagerWindow);
+  let pluginLibraries = doc.getElementById("pluginLibraries");
+  ok(pluginLibraries, "Plugin file name row should be displayed");
+  // the file name depends on the platform
+  is(pluginLibraries.textContent, testPlugin.pluginLibraries, "Plugin file name should be displayed");
 
-  Services.obs.addObserver(async function observer(subject, topic, data) {
-    Services.obs.removeObserver(observer, topic);
+  let pluginMimeTypes = doc.getElementById("pluginMimeTypes");
+  ok(pluginMimeTypes, "Plugin mime type row should be displayed");
+  is(pluginMimeTypes.textContent, "application/x-test (Test \u2122 mimetype: tst)", "Plugin mime type should be displayed");
 
-    // Wait for PluginProvider to do its stuff.
-    await new Promise(executeSoon);
+  await close_manager(gManagerWindow);
+}
 
-    let doc = gManagerWindow.document.getElementById("addon-options").contentDocument;
+add_task(async function test_inline_plugin_prefs_on_XUL_aboutaddons() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.htmlaboutaddons.enabled", false]],
+  });
+  await test_inline_plugin_prefs();
+  await SpecialPowers.popPrefEnv();
+});
 
-    let pluginLibraries = doc.getElementById("pluginLibraries");
-    ok(pluginLibraries, "Plugin file name row should be displayed");
-    // the file name depends on the platform
-    is(pluginLibraries.textContent, testPlugin.pluginLibraries, "Plugin file name should be displayed");
-
-    let pluginMimeTypes = doc.getElementById("pluginMimeTypes");
-    ok(pluginMimeTypes, "Plugin mime type row should be displayed");
-    is(pluginMimeTypes.textContent, "application/x-test (Test \u2122 mimetype: tst)", "Plugin mime type should be displayed");
-
-    run_next_test();
-  }, AddonManager.OPTIONS_NOTIFICATION_DISPLAYED);
+add_task(async function test_inline_plugin_prefs_on_HTML_aboutaddons() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.htmlaboutaddons.enabled", true]],
+  });
+  await test_inline_plugin_prefs();
+  await SpecialPowers.popPrefEnv();
 });
