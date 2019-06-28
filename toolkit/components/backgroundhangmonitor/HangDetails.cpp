95e2c329ebeb55bd73c8bb2f118a79c576813644
Andrew McCreight <continuation@gmail.com>
2019-06-28 14:30:18 +0000

    Bug 1561653 - Drop URIs from remote types we use in telemetry. r=nika
    With Fission enabled, remote types can have the form
    webIsolated=<URI>. This is bad, because we store them in telemetry in
    some places. This patch adds and uses a method that sanitizes remote
    types.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36068

--- toolkit/components/backgroundhangmonitor/HangDetails.cpp
+++ toolkit/components/backgroundhangmonitor/HangDetails.cpp
@@ -3,6 +3,7 @@
 #include "nsPrintfCString.h"
 #include "mozilla/gfx/GPUParent.h"
 #include "mozilla/dom/ContentChild.h"
+#include "mozilla/dom/ContentParent.h"  // For RemoteTypePrefix
 #include "mozilla/Unused.h"
 #include "mozilla/GfxMessageUtils.h"  // For ParamTraits<GeckoProcessType>
 
@@ -255,7 +256,10 @@ void nsHangDetails::Submit() {
           case GeckoProcessType_Content: {
             auto cc = dom::ContentChild::GetSingleton();
             if (cc) {
-              hangDetails->mDetails.remoteType().Assign(cc->GetRemoteType());
+              // Use the prefix so we don't get URIs from Fission isolated
+              // processes.
+              hangDetails->mDetails.remoteType().Assign(
+                  dom::RemoteTypePrefix(cc->GetRemoteType()));
               Unused << cc->SendBHRThreadHang(hangDetails->mDetails);
             }
             break;
