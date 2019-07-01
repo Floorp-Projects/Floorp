6744e4baa973dffe4a64634688fb712467c516c3
Andreas Pehrson <apehrson@mozilla.com>
2019-07-01 17:47:29 +0000

    Bug 1560215 - Properly handle errors from VP8TrackEncoder::GetEncodedPartitions. r=bryce
    In the particular case of this bug's crashtest, a dataavailable callback from
    the track encoder was raised at a point when
    VP8TrackEncoder::GetEncodedPartitions would not actually return any data.
    This would make VP8TrackEncoder::GetEncodedTrack propagate the error and set the
    recorder in an error state, and cancel any ongoing recordings.
    
    VP8TrackEncoder::GetEncodedPartitions was changed in bug 1333341 to distinguish
    end-of-stream from other, real, errors; but callsites were not updated.
    This patch fixes the callsites.
    
    Depends on D36509
    
    Differential Revision: https://phabricator.services.mozilla.com/D36510

--- dom/media/encoder/VP8TrackEncoder.cpp
+++ dom/media/encoder/VP8TrackEncoder.cpp
@@ -499,7 +499,10 @@ nsresult VP8TrackEncoder::GetEncodedTrack(EncodedFrameContainer& aData) {
       }
       // Get the encoded data from VP8 encoder.
       rv = GetEncodedPartitions(aData);
-      NS_ENSURE_SUCCESS(rv, NS_ERROR_FAILURE);
+      if (rv != NS_OK && rv != NS_ERROR_NOT_AVAILABLE) {
+        VP8LOG(LogLevel::Error, "GetEncodedPartitions failed.");
+        return NS_ERROR_FAILURE;
+      }
     } else {
       // SKIP_FRAME
       // Extend the duration of the last encoded data in aData
@@ -546,12 +549,21 @@ nsresult VP8TrackEncoder::GetEncodedTrack(EncodedFrameContainer& aData) {
     mEncodingComplete = true;
     // Bug 1243611, keep calling vpx_codec_encode and vpx_codec_get_cx_data
     // until vpx_codec_get_cx_data return null.
-    do {
+    while (true) {
       if (vpx_codec_encode(mVPXContext, nullptr, mEncodedTimestamp, 0, 0,
                            VPX_DL_REALTIME)) {
         return NS_ERROR_FAILURE;
       }
-    } while (NS_SUCCEEDED(GetEncodedPartitions(aData)));
+      nsresult rv = GetEncodedPartitions(aData);
+      if (rv == NS_ERROR_NOT_AVAILABLE) {
+        // End-of-stream
+        break;
+      }
+      if (rv != NS_OK) {
+        // Error
+        return NS_ERROR_FAILURE;
+      }
+    }
   }
 
   return NS_OK;
