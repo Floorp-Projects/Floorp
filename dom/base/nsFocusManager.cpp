6fc1e474dd2c26693f08244ac6f336f313b06207
Emilio Cobos Álvarez <emilio@crisal.io>
2019-07-01 20:04:07 +0000

    Bug 1561288 - Don't override currentContent when focusing something on a shadow root. r=smaug
    It is wrong, what we're trying to focus is frame->GetContent() still.
    
    This is not reproducible outside of Thunderbird because for it to happen the
    focus manager needs to wrap around the start content.
    
    Here's what's going on:
    
     * We start with a <tree> focused, which is a shadow host.
     * There's nothing else to focus inside the <tree>, or after the <tree> in document order.
     * So we restart the search with a different tabindex (1), but find nothing before the tree (since all focusable elements before the tree also have tabindex 0), and we return wrongly in [1], instead of searching with tabindex 0 again afterwards. (Since the child frame is not focusable, we wrongly end up at [1], and returning early, since currentContent != frame->GetContent(), instead of keeping looking for the right thing to focus.)
    
    As I said this cannot reproduce on content, as far as I can tell, so I'm not
    sure how to go about testing this, but all the shadow dom tab navigation tests
    keep passing with this change.
    
    [1]: https://searchfox.org/mozilla-central/rev/06bd14ced96f25ff1dbd5352cb985fc0fa12a64e/dom/base/nsFocusManager.cpp#3560
    
    Differential Revision: https://phabricator.services.mozilla.com/D36196

--- dom/base/nsFocusManager.cpp
+++ dom/base/nsFocusManager.cpp
@@ -3374,22 +3374,20 @@ nsresult nsFocusManager::GetNextTabbableContent(
       } else {
         currentTopLevelScopeOwner = currentContent;
       }
-      if (currentTopLevelScopeOwner) {
-        if (currentTopLevelScopeOwner == oldTopLevelScopeOwner) {
-          // We're within non-document scope, continue.
-          do {
-            if (aForward) {
-              frameTraversal->Next();
-            } else {
-              frameTraversal->Prev();
-            }
-            frame = static_cast<nsIFrame*>(frameTraversal->CurrentItem());
-            // For the usage of GetPrevContinuation, see the comment
-            // at the end of while (frame) loop.
-          } while (frame && frame->GetPrevContinuation());
-          continue;
-        }
-        currentContent = currentTopLevelScopeOwner;
+      if (currentTopLevelScopeOwner &&
+          currentTopLevelScopeOwner == oldTopLevelScopeOwner) {
+        // We're within non-document scope, continue.
+        do {
+          if (aForward) {
+            frameTraversal->Next();
+          } else {
+            frameTraversal->Prev();
+          }
+          frame = static_cast<nsIFrame*>(frameTraversal->CurrentItem());
+          // For the usage of GetPrevContinuation, see the comment
+          // at the end of while (frame) loop.
+        } while (frame && frame->GetPrevContinuation());
+        continue;
       }
 
       // For document navigation, check if this element is an open panel. Since
@@ -3441,18 +3439,18 @@ nsresult nsFocusManager::GetNextTabbableContent(
       //  append ELEMENT to NAVIGATION-ORDER."
       // and later in "For each element ELEMENT in NAVIGATION-ORDER: "
       // hosts and slots are handled before other elements.
-      if (IsHostOrSlot(currentContent)) {
+      if (IsHostOrSlot(currentTopLevelScopeOwner)) {
         bool focusableHostSlot;
-        int32_t tabIndex =
-            HostOrSlotTabIndexValue(currentContent, &focusableHostSlot);
+        int32_t tabIndex = HostOrSlotTabIndexValue(currentTopLevelScopeOwner,
+                                                   &focusableHostSlot);
         // Host or slot itself isn't focusable or going backwards, enter its
         // scope.
         if ((!aForward || !focusableHostSlot) && tabIndex >= 0 &&
             (aIgnoreTabIndex || aCurrentTabIndex == tabIndex)) {
           nsIContent* contentToFocus = GetNextTabbableContentInScope(
-              currentContent, currentContent, aOriginalStartContent, aForward,
-              aForward ? 1 : 0, aIgnoreTabIndex, aForDocumentNavigation,
-              true /* aSkipOwner */);
+              currentTopLevelScopeOwner, currentTopLevelScopeOwner,
+              aOriginalStartContent, aForward, aForward ? 1 : 0,
+              aIgnoreTabIndex, aForDocumentNavigation, true /* aSkipOwner */);
           if (contentToFocus) {
             NS_ADDREF(*aResultContent = contentToFocus);
             return NS_OK;
