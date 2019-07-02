2270bf983bfb5f7973a94c662eac034d9474d4ec
Dragana Damjanovic <dd.mozilla@gmail.com>
2019-07-02 21:26:36 +0000

    Bug 1560354 - Transform some nss types into gecko types. r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D35566

--- security/manager/ssl/nsNSSIOLayer.cpp
+++ security/manager/ssl/nsNSSIOLayer.cpp
@@ -472,8 +472,8 @@ nsNSSSocketInfo::IsAcceptableForHost(const nsACString& hostname,
   UniqueCERTCertList unusedBuiltChain;
   mozilla::pkix::Result result =
       certVerifier->VerifySSLServerCert(nssCert,
-                                        nullptr,  // stapledOCSPResponse
-                                        nullptr,  // sctsFromTLSExtension
+                                        Maybe<nsTArray<uint8_t>>(),  // stapledOCSPResponse
+                                        Maybe<nsTArray<uint8_t>>(),  // sctsFromTLSExtension
                                         mozilla::pkix::Now(),
                                         nullptr,  // pinarg
                                         hostname, unusedBuiltChain,
