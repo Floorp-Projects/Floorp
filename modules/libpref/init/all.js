1c90332e6aadf2890c69186f4de200c7e5781f23
Jan Varga <jan.varga@gmail.com>
2019-07-02 20:24:23 +0200

    Bug 1562942 - Disable LSNG in 68; r=asuth, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D36645

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1280,7 +1280,8 @@ pref("dom.storage.enabled", true);
 // See bug 1517090 for enabling this on Nightly.
 // See bug 1534736 for changing it to EARLY_BETA_OR_EARLIER.
 // See bug 1539835 for enabling this unconditionally.
-pref("dom.storage.next_gen", true);
+// See bug 1562942 for disabling this in late 68.
+pref("dom.storage.next_gen", false);
 pref("dom.storage.default_quota",      5120);
 pref("dom.storage.shadow_writes", true);
 pref("dom.storage.snapshot_prefill", 16384);
