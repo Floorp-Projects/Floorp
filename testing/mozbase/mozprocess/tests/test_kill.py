e869758aa5ec584090775850c473b885d0fa1dd9
Cosmin Sabou <csabou@mozilla.com>
2019-07-03 19:48:58 +0300

    Backed out changeset da3af6a077fc (bug 1562083) for busting gecko decision task. CLOSED TREE

--- testing/mozbase/mozprocess/tests/test_kill.py
+++ testing/mozbase/mozprocess/tests/test_kill.py
@@ -7,7 +7,6 @@ import time
 import unittest
 import proctest
 import signal
-import sys
 
 import mozunit
 
@@ -51,17 +50,9 @@ class ProcTestKill(proctest.ProcTest):
         """Process is started, we use a deep process tree, we let it spawn
            for a bit, we kill it"""
 
-        myenv = None
-        # On macosx1014, subprocess fails to find `six` when run with python3.
-        # This ensures that subprocess first looks to sys.path to find `six`.
-        # See https://bugzilla.mozilla.org/show_bug.cgi?id=1562083
-        if sys.platform == 'darwin' and sys.version_info[0] > 2:
-            myenv = os.environ.copy()
-            myenv['PYTHONPATH'] = ':'.join(sys.path)
-
         p = processhandler.ProcessHandler([self.python, self.proclaunch,
                                            "process_normal_deep.ini"],
-                                          cwd=here, env=myenv)
+                                          cwd=here)
         p.run()
         # Let the tree spawn a bit, before attempting to kill
         time.sleep(3)
@@ -84,17 +75,9 @@ class ProcTestKill(proctest.ProcTest):
         """Process is started, we use a broad process tree, we let it spawn
            for a bit, we kill it"""
 
-        myenv = None
-        # On macosx1014, subprocess fails to find `six` when run with python3.
-        # This ensures that subprocess first looks to sys.path to find `six`.
-        # See https://bugzilla.mozilla.org/show_bug.cgi?id=1562083
-        if sys.platform == 'darwin' and sys.version_info[0] > 2:
-            myenv = os.environ.copy()
-            myenv['PYTHONPATH'] = ':'.join(sys.path)
-
         p = processhandler.ProcessHandler([self.python, self.proclaunch,
                                            "process_normal_broad.ini"],
-                                          cwd=here, env=myenv)
+                                          cwd=here)
         p.run()
         # Let the tree spawn a bit, before attempting to kill
         time.sleep(3)
