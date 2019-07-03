8cd2c1b4f19f3ff6542ade95740504ddc269aae7
Edwin Gao <egao@mozilla.com>
2019-07-03 17:52:19 +0000

    Bug 1562083 - explicitly insert sys.path to env for use in subprocess for python3 mozprocess unit tests r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D36681

--- testing/mozbase/mozprocess/tests/test_wait.py
+++ testing/mozbase/mozprocess/tests/test_wait.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 
 import os
 import signal
+import sys
 
 import mozinfo
 import mozunit
@@ -42,9 +43,17 @@ class ProcTestWait(proctest.ProcTest):
         """ Process is started, runs but we time out waiting on it
             to complete
         """
+        myenv = None
+        # On macosx1014, subprocess fails to find `six` when run with python3.
+        # This ensures that subprocess first looks to sys.path to find `six`.
+        # See https://bugzilla.mozilla.org/show_bug.cgi?id=1562083
+        if sys.platform == 'darwin' and sys.version_info[0] > 2:
+            myenv = os.environ.copy()
+            myenv['PYTHONPATH'] = ':'.join(sys.path)
+
         p = processhandler.ProcessHandler([self.python, self.proclaunch,
                                            "process_waittimeout.ini"],
-                                          cwd=here)
+                                          cwd=here, env=myenv)
         p.run(timeout=10)
         p.wait()
 
