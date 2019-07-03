ca126b35cb9273d3600fd99d47318b66359ad9f1
thomasmo <thomasmo@mozilla.com>
2019-07-03 15:50:28 +0000

    Bug 1562777 - Refactor ShMem code from VR classes into separate class r=kip
    This change brings the related ShMem code from VRManager and VRService into a
    new class, VRShMem. This is to support future work where this ShMem will be used
    for other efforts. Having this code in the same class will enable it to be more
    easily shared in these efforts.
    Until the new class replaces the code in VRManager and VRService, it can be
    exercised and validated with two instances of vrtesthost, with the -testmgr and
    -testsvc parameters.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36649

---
+++ gfx/vr/VRShMem.h
@@ -0,0 +1,66 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef GFX_VR_VRSHMEM_H
+#define GFX_VR_VRSHMEM_H
+
+// This file declares VRShMem, which is used for cross-platform, cross-process
+// communication between VR components.
+// A shared memory handle is opened for the struct of type VRExternalShmem, and
+// different processes write or read members of it to share data. Note that no
+// process should be reading or writing to the same members, except for the
+// versioning members used for synchronization.
+
+#include "moz_external_vr.h"
+#include "base/process.h"  // for base::ProcessHandle
+#include <functional>
+
+namespace mozilla {
+namespace gfx {
+class VRShMem final {
+ public:
+  VRShMem(volatile VRExternalShmem* aShmem, bool aSameProcess,
+          bool aIsParentProcess);
+  ~VRShMem() = default;
+
+  void CreateShMem();
+  void CloseShMem();
+
+  bool JoinShMem();
+  void LeaveShMem();
+
+  void PushBrowserState(VRBrowserState& aBrowserState, bool aNotifyCond);
+  void PullBrowserState(mozilla::gfx::VRBrowserState& aState);
+
+  void PushSystemState(const mozilla::gfx::VRSystemState& aState);
+  void PullSystemState(
+      VRDisplayState& aDisplayState, VRHMDSensorState& aSensorState,
+      VRControllerState (&aControllerState)[kVRControllerMaxCount],
+      bool& aEnumerationCompleted,
+      const std::function<bool()>& aWaitCondition = nullptr);
+
+ private:
+  volatile VRExternalShmem* mExternalShmem = nullptr;
+  bool mSameProcess = false;
+#if defined(XP_WIN)
+  bool mIsParentProcess = false;
+#endif
+
+#if defined(XP_MACOSX)
+  int mShmemFD;
+#elif defined(XP_WIN)
+  base::ProcessHandle mShmemFile;
+  base::ProcessHandle mMutex;
+#endif
+
+#if !defined(MOZ_WIDGET_ANDROID)
+  int64_t mBrowserGeneration = 0;
+#endif
+};
+}  // namespace gfx
+}  // namespace mozilla
+
+#endif
