8dd328fee05fab5fe04dbb008ce2fe318b94060f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-07-03 02:37:11 +0000

    Bug 1366738: Implement [LegacyWindowAlias] r=bzbarsky
    Add [LegacyWindowAlias] extended attribute support for WebKitCSSMatrix and webkitURL.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35601
    
    --HG--
    extra : moz-landing-system : lando

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -3048,7 +3048,7 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
 
     properties should be a PropertyArrays instance.
     """
-    def __init__(self, descriptor, properties, haveUnscopables):
+    def __init__(self, descriptor, properties, haveUnscopables, haveLegacyWindowAliases):
         args = [Argument('JSContext*', 'aCx'),
                 Argument('JS::Handle<JSObject*>', 'aGlobal'),
                 Argument('ProtoAndIfaceCache&', 'aProtoAndIfaceCache'),
@@ -3056,6 +3056,7 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
         CGAbstractMethod.__init__(self, descriptor, 'CreateInterfaceObjects', 'void', args)
         self.properties = properties
         self.haveUnscopables = haveUnscopables
+        self.haveLegacyWindowAliases = haveLegacyWindowAliases
 
     def definition_body(self):
         (protoGetter, protoHandleGetter) = InterfacePrototypeObjectProtoGetter(self.descriptor)
@@ -3184,7 +3185,8 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
                                         ${chromeProperties},
                                         ${name}, aDefineOnGlobal,
                                         ${unscopableNames},
-                                        ${isGlobal});
+                                        ${isGlobal},
+                                        ${legacyWindowAliases});
             """,
             protoClass=protoClass,
             parentProto=parentProto,
@@ -3199,7 +3201,8 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
             chromeProperties=chromeProperties,
             name='"' + self.descriptor.interface.identifier.name + '"' if needInterfaceObject else "nullptr",
             unscopableNames="unscopableNames" if self.haveUnscopables else "nullptr",
-            isGlobal=toStringBool(isGlobal))
+            isGlobal=toStringBool(isGlobal),
+            legacyWindowAliases="legacyWindowAliases" if self.haveLegacyWindowAliases else "nullptr")
 
         # If we fail after here, we must clear interface and prototype caches
         # using this code: intermediate failure must not expose the interface in
@@ -13359,10 +13362,20 @@ class CGDescriptor(CGThing):
                                           [CGGeneric("nullptr")], ",\n")),
                         CGGeneric("};\n")], "\n"))
 
+        legacyWindowAliases = descriptor.interface.legacyWindowAliases
+        haveLegacyWindowAliases = len(legacyWindowAliases) != 0
+        if haveLegacyWindowAliases:
+            cgThings.append(
+                CGList([CGGeneric("static const char* const legacyWindowAliases[] = {"),
+                        CGIndenter(CGList([CGGeneric('"%s"' % name) for
+                                           name in legacyWindowAliases] +
+                                          [CGGeneric("nullptr")], ",\n")),
+                        CGGeneric("};\n")], "\n"))
+
         # CGCreateInterfaceObjectsMethod needs to come after our
         # CGDOMJSClass and unscopables, if any.
         cgThings.append(CGCreateInterfaceObjectsMethod(descriptor, properties,
-                                                       haveUnscopables))
+                                                       haveUnscopables, haveLegacyWindowAliases))
 
         # CGGetProtoObjectMethod and CGGetConstructorObjectMethod need
         # to come after CGCreateInterfaceObjectsMethod.
@@ -14328,6 +14341,7 @@ def getGlobalNames(config):
     for desc in config.getDescriptors(registersGlobalNamesOnWindow=True):
         names.append((desc.name, desc))
         names.extend((n.identifier.name, desc) for n in desc.interface.namedConstructors)
+        names.extend((n, desc) for n in desc.interface.legacyWindowAliases)
     return names
 
 class CGGlobalNames(CGGeneric):
