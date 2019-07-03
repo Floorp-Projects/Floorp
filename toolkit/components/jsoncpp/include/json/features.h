905cb04927e2b6cb566e4344ed9ba0a86b584880
Ryan VanderMeulen <ryanvm@gmail.com>
2019-07-03 08:03:12 +0000

    Bug 1563085 - Update jsoncpp to version 1.9.0. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D36700
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/jsoncpp/include/json/features.h
+++ toolkit/components/jsoncpp/include/json/features.h
@@ -1,4 +1,4 @@
-// Copyright 2007-2010 Baptiste Lepilleur
+// Copyright 2007-2010 Baptiste Lepilleur and The JsonCpp Authors
 // Distributed under MIT license, or public domain if desired and
 // recognized in your jurisdiction.
 // See file LICENSE for detail or copy at http://jsoncpp.sourceforge.net/LICENSE
@@ -41,17 +41,17 @@ public:
   Features();
 
   /// \c true if comments are allowed. Default: \c true.
-  bool allowComments_;
+  bool allowComments_{true};
 
   /// \c true if root must be either an array or an object value. Default: \c
   /// false.
-  bool strictRoot_;
+  bool strictRoot_{false};
 
   /// \c true if dropped null placeholders are allowed. Default: \c false.
-  bool allowDroppedNullPlaceholders_;
+  bool allowDroppedNullPlaceholders_{false};
 
   /// \c true if numeric object key are allowed. Default: \c false.
-  bool allowNumericKeys_;
+  bool allowNumericKeys_{false};
 };
 
 } // namespace Json
