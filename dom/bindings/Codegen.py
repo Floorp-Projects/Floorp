9c4d142e968711f2ecbaa1df6523e10d636cba86
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-07-03 10:21:30 +0300

    Backed out changeset 509af1e0771d (bug 1366738) for causing failures in test_WebKitCSSMatrix.html CLOSED TREE

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -3048,7 +3048,7 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
 
     properties should be a PropertyArrays instance.
     """
-    def __init__(self, descriptor, properties, haveUnscopables, haveLegacyWindowAliases):
+    def __init__(self, descriptor, properties, haveUnscopables):
         args = [Argument('JSContext*', 'aCx'),
                 Argument('JS::Handle<JSObject*>', 'aGlobal'),
                 Argument('ProtoAndIfaceCache&', 'aProtoAndIfaceCache'),
@@ -3056,7 +3056,6 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
         CGAbstractMethod.__init__(self, descriptor, 'CreateInterfaceObjects', 'void', args)
         self.properties = properties
         self.haveUnscopables = haveUnscopables
-        self.haveLegacyWindowAliases = haveLegacyWindowAliases
 
     def definition_body(self):
         (protoGetter, protoHandleGetter) = InterfacePrototypeObjectProtoGetter(self.descriptor)
@@ -3185,8 +3184,7 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
                                         ${chromeProperties},
                                         ${name}, aDefineOnGlobal,
                                         ${unscopableNames},
-                                        ${isGlobal},
-                                        ${legacyWindowAliases});
+                                        ${isGlobal});
             """,
             protoClass=protoClass,
             parentProto=parentProto,
@@ -3201,8 +3199,7 @@ class CGCreateInterfaceObjectsMethod(CGAbstractMethod):
             chromeProperties=chromeProperties,
             name='"' + self.descriptor.interface.identifier.name + '"' if needInterfaceObject else "nullptr",
             unscopableNames="unscopableNames" if self.haveUnscopables else "nullptr",
-            isGlobal=toStringBool(isGlobal),
-            legacyWindowAliases="legacyWindowAliases" if self.haveLegacyWindowAliases else "nullptr")
+            isGlobal=toStringBool(isGlobal))
 
         # If we fail after here, we must clear interface and prototype caches
         # using this code: intermediate failure must not expose the interface in
@@ -13367,20 +13364,10 @@ class CGDescriptor(CGThing):
                                           [CGGeneric("nullptr")], ",\n")),
                         CGGeneric("};\n")], "\n"))
 
-        legacyWindowAliases = descriptor.interface.legacyWindowAliases
-        haveLegacyWindowAliases = len(legacyWindowAliases) != 0
-        if haveLegacyWindowAliases:
-            cgThings.append(
-                CGList([CGGeneric("static const char* const legacyWindowAliases[] = {"),
-                        CGIndenter(CGList([CGGeneric('"%s"' % name) for
-                                           name in legacyWindowAliases] +
-                                          [CGGeneric("nullptr")], ",\n")),
-                        CGGeneric("};\n")], "\n"))
-
         # CGCreateInterfaceObjectsMethod needs to come after our
         # CGDOMJSClass and unscopables, if any.
         cgThings.append(CGCreateInterfaceObjectsMethod(descriptor, properties,
-                                                       haveUnscopables, haveLegacyWindowAliases))
+                                                       haveUnscopables))
 
         # CGGetProtoObjectMethod and CGGetConstructorObjectMethod need
         # to come after CGCreateInterfaceObjectsMethod.
@@ -14346,7 +14333,6 @@ def getGlobalNames(config):
     for desc in config.getDescriptors(registersGlobalNamesOnWindow=True):
         names.append((desc.name, desc))
         names.extend((n.identifier.name, desc) for n in desc.interface.namedConstructors)
-        names.extend((n, desc) for n in desc.interface.legacyWindowAliases)
     return names
 
 class CGGlobalNames(CGGeneric):
