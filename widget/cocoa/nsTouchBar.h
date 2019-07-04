d1e552113e68269a9b6083b8abc3b5ba592457bf
harry <htwyford@mozilla.com>
2019-07-04 03:57:47 +0000

    Bug 1522012 - Implement Touch Bar's native customization window and remove ui.touchbar.layout preference. r=spohl,mikedeboer,fluent-reviewers,Pike
    This patch also fixes the Home and Sidebar Touch Bar buttons, since using them after customizing showed that they no longer worked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35085
    
    --HG--
    extra : moz-landing-system : lando

--- widget/cocoa/nsTouchBar.h
+++ widget/cocoa/nsTouchBar.h
@@ -39,9 +39,12 @@ __attribute__((weak_import)) @interface NSCustomTouchBarItem : NSTouchBarItem
 @protocol NSTouchBarDelegate
 @end
 
+typedef NSString* NSTouchBarCustomizationIdentifier;
 __attribute__((weak_import)) @interface NSTouchBar : NSObject
 @property(strong) NSArray<NSTouchBarItemIdentifier>* defaultItemIdentifiers;
 @property(strong) id<NSTouchBarDelegate> delegate;
+@property(strong) NSTouchBarCustomizationIdentifier customizationIdentifier;
+@property(strong) NSArray<NSTouchBarItemIdentifier>* customizationAllowedItemIdentifiers;
 - (NSTouchBarItem*)itemForIdentifier:(NSTouchBarItemIdentifier)aIdentifier;
 @end
 
