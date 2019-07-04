64cafcc6dbb8de4d65e4987cfb0eae033190636e
Iain Ireland <iireland@mozilla.com>
2019-07-04 20:00:35 +0000

    Bug 1561732: Remove sincos optimization r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D36976
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jit/shared/Lowering-shared-inl.h
+++ js/src/jit/shared/Lowering-shared-inl.h
@@ -316,42 +316,6 @@ void LIRGeneratorShared::defineReturn(LInstruction* lir, MDefinition* mir) {
   add(lir);
 }
 
-template <size_t Ops, size_t Temps>
-void LIRGeneratorShared::defineSinCos(LInstructionHelper<2, Ops, Temps>* lir,
-                                      MDefinition* mir,
-                                      LDefinition::Policy policy) {
-  MOZ_ASSERT(lir->isCall());
-
-  uint32_t vreg = getVirtualRegister();
-  lir->setDef(
-      0, LDefinition(vreg, LDefinition::DOUBLE, LFloatReg(ReturnDoubleReg)));
-#if defined(JS_CODEGEN_ARM)
-  lir->setDef(1, LDefinition(vreg + VREG_INCREMENT, LDefinition::DOUBLE,
-                             LFloatReg(FloatRegister(FloatRegisters::d1,
-                                                     FloatRegister::Double))));
-#elif defined(JS_CODEGEN_ARM64)
-  lir->setDef(1, LDefinition(vreg + VREG_INCREMENT, LDefinition::DOUBLE,
-                             LFloatReg(FloatRegister(FloatRegisters::d1,
-                                                     FloatRegisters::Double))));
-#elif defined(JS_CODEGEN_MIPS32) || defined(JS_CODEGEN_MIPS64)
-  lir->setDef(1, LDefinition(vreg + VREG_INCREMENT, LDefinition::DOUBLE,
-                             LFloatReg(f2)));
-#elif defined(JS_CODEGEN_NONE)
-  MOZ_CRASH();
-#elif defined(JS_CODEGEN_X86) || defined(JS_CODEGEN_X64)
-  lir->setDef(1, LDefinition(vreg + VREG_INCREMENT, LDefinition::DOUBLE,
-                             LFloatReg(xmm1)));
-#else
-#  error "Unsupported architecture for SinCos"
-#endif
-
-  getVirtualRegister();
-
-  lir->setMir(mir);
-  mir->setVirtualRegister(vreg);
-  add(lir);
-}
-
 // In LIR, we treat booleans and integers as the same low-level type (INTEGER).
 // When snapshotting, we recover the actual JS type from MIR. This function
 // checks that when making redefinitions, we don't accidentally coerce two
@@ -365,30 +329,6 @@ static inline bool IsCompatibleLIRCoercion(MIRType to, MIRType from) {
     return true;
   }
   return false;
-}
-
-// We can redefine the sin(x) and cos(x) function to return the sincos result.
-void LIRGeneratorShared::redefine(MDefinition* def, MDefinition* as,
-                                  MMathFunction::Function func) {
-  MOZ_ASSERT(def->isMathFunction());
-  MOZ_ASSERT(def->type() == MIRType::Double &&
-             as->type() == MIRType::SinCosDouble);
-  MOZ_ASSERT(MMathFunction::Sin == func || MMathFunction::Cos == func);
-
-  ensureDefined(as);
-  MMathFunction* math = def->toMathFunction();
-
-  MOZ_ASSERT(math->function() == MMathFunction::Cos ||
-             math->function() == MMathFunction::Sin);
-
-  // The sincos returns two values:
-  // - VREG: it returns the sin's value of the sincos;
-  // - VREG + VREG_INCREMENT: it returns the cos' value of the sincos.
-  if (math->function() == MMathFunction::Sin) {
-    def->setVirtualRegister(as->virtualRegister());
-  } else {
-    def->setVirtualRegister(as->virtualRegister() + VREG_INCREMENT);
-  }
 }
 
 void LIRGeneratorShared::redefine(MDefinition* def, MDefinition* as) {
