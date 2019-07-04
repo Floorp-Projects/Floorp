32595c163faa81e7305a8de23bd9fc4ddc236afd
Iain Ireland <iireland@mozilla.com>
2019-07-04 20:00:35 +0000

    Bug 1561732: Remove sincos optimization r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D36976

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -122,14 +122,6 @@ DefaultJitOptions::DefaultJitOptions() {
   // Toggles whether CacheIR stubs are used.
   SET_DEFAULT(disableCacheIR, false);
 
-// Toggles whether sincos optimization is globally disabled.
-// See bug984018: The MacOS is the only one that has the sincos fast.
-#if defined(XP_MACOSX)
-  SET_DEFAULT(disableSincos, false);
-#else
-  SET_DEFAULT(disableSincos, true);
-#endif
-
   // Toggles whether sink code motion is globally disabled.
   SET_DEFAULT(disableSink, true);
 
