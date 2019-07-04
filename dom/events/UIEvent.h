e6036dfe739ed550f0cc621440f9546aa15db06b
Olli Pettay <Olli.Pettay@helsinki.fi>
2019-07-04 01:50:22 +0000

    Bug 1178763, move UIEvent.pageX/pageY to MouseEvent, r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D36781

--- dom/events/UIEvent.h
+++ dom/events/UIEvent.h
@@ -63,9 +63,6 @@ class UIEvent : public Event {
 
   int32_t LayerY() const { return GetLayerPoint().y; }
 
-  int32_t PageX() const;
-  int32_t PageY() const;
-
   virtual uint32_t Which(CallerType aCallerType = CallerType::System) {
     MOZ_ASSERT(mEvent->mClass != eKeyboardEventClass,
                "Key events should override Which()");
