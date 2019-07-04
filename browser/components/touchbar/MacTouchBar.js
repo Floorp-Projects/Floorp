86d1612103368f948612e98f82f36cc97677dd69
harry <htwyford@mozilla.com>
2019-07-04 03:57:47 +0000

    Bug 1522012 - Implement Touch Bar's native customization window and remove ui.touchbar.layout preference. r=spohl,mikedeboer,fluent-reviewers,Pike
    This patch also fixes the Home and Sidebar Touch Bar buttons, since using them after customizing showed that they no longer worked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35085

--- browser/components/touchbar/MacTouchBar.js
+++ browser/components/touchbar/MacTouchBar.js
@@ -73,7 +73,12 @@ const kBuiltInInputs = {
     title: "home",
     image: "home.pdf",
     type: "button",
-    callback: () => execCommand("Browser:Home", "Home"),
+    callback: () => {
+      let win = BrowserWindowTracker.getTopWindow();
+      win.BrowserHome();
+      let telemetry = Services.telemetry.getHistogramById("TOUCHBAR_BUTTON_PRESSES");
+      telemetry.add("Home");
+    },
   },
   Fullscreen: {
     title: "fullscreen",
@@ -94,10 +99,15 @@ const kBuiltInInputs = {
     callback: () => execCommand("cmd_newNavigatorTabNoEvent", "NewTab"),
   },
   Sidebar: {
-    title: "open-bookmarks-sidebar",
+    title: "open-sidebar",
     image: "sidebar-left.pdf",
     type: "button",
-    callback: () => execCommand("viewBookmarksSidebar", "Sidebar"),
+    callback: () => {
+      let win = BrowserWindowTracker.getTopWindow();
+      win.SidebarUI.toggle();
+      let telemetry = Services.telemetry.getHistogramById("TOUCHBAR_BUTTON_PRESSES");
+      telemetry.add("Sidebar");
+    },
   },
   AddBookmark: {
     title: "add-bookmark",
@@ -141,9 +151,6 @@ class TouchBarHelper {
     for (let topic of kHelperObservers) {
       Services.obs.addObserver(this, topic);
     }
-
-    XPCOMUtils.defineLazyPreferenceGetter(this, "_touchBarLayout",
-      "ui.touchbar.layout", "Back,Forward,Reload,OpenLocation,NewTab,Share");
   }
 
   destructor() {
@@ -161,11 +168,9 @@ class TouchBarHelper {
     return activeTitle;
   }
 
-  get layout() {
-    let prefArray = this.storedLayout;
+  get allItems() {
     let layoutItems = Cc["@mozilla.org/array;1"].createInstance(Ci.nsIMutableArray);
-
-    for (let inputName of prefArray) {
+    for (let inputName of Object.keys(kBuiltInInputs)) {
       if (typeof kBuiltInInputs[inputName].context == "function") {
         inputName = kBuiltInInputs[inputName].context();
       }
@@ -187,23 +192,6 @@ class TouchBarHelper {
     return this.window.BookmarkingUI;
   }
 
-  /**
-   * Returns a string array of the user's Touch Bar layout preference.
-   * Set by a pref ui.touchbar.layout and returned in an array.
-   */
-  get storedLayout() {
-    let prefArray = this._touchBarLayout.split(",");
-    prefArray = prefArray.map(str => str.trim());
-    // Remove duplicates.
-    prefArray = Array.from(new Set(prefArray));
-
-    // Remove unimplemented/mispelled inputs.
-    prefArray = prefArray.filter(input =>
-      Object.keys(kBuiltInInputs).includes(input));
-    this._storedLayout = prefArray;
-    return this._storedLayout;
-  }
-
   getTouchBarInput(inputName) {
     // inputName might be undefined if an input's context() returns undefined.
     if (!inputName || !kBuiltInInputs.hasOwnProperty(inputName)) {
@@ -222,7 +210,6 @@ class TouchBarHelper {
     let inputData = kBuiltInInputs[inputName];
 
     let item = new TouchBarInput(inputData);
-
     // Skip localization if there is already a cached localized title.
     if (kBuiltInInputs[inputName].hasOwnProperty("localTitle")) {
       return item;
@@ -258,12 +245,6 @@ class TouchBarHelper {
 
     let inputs = [];
     for (let inputName of inputNames) {
-      // Updating Touch Bar items isn't cheap in terms of performance, so
-      // only consider this input if it's actually part of the layout.
-      if (!this._storedLayout.includes(inputName)) {
-        continue;
-      }
-
       let input = this.getTouchBarInput(inputName);
       if (!input) {
         continue;
@@ -300,10 +281,10 @@ class TouchBarHelper {
         break;
       case "intl:app-locales-changed":
         // On locale change, refresh all inputs after loading new localTitle.
-        for (let inputName of this._storedLayout) {
-          delete kBuiltInInputs[inputName].localTitle;
+        for (let input in kBuiltInInputs) {
+          delete input.localTitle;
         }
-        this._updateTouchBarInputs(...this._storedLayout);
+        this._updateTouchBarInputs(...kBuiltInInputs.keys());
         break;
       case "quit-application":
         this.destructor();
