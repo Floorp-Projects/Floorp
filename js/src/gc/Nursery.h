0018731dbd5e6c72e5a7d9033e8b5c285b962e04
Paul Bone <pbone@mozilla.com>
2019-07-05 13:18:17 +0000

    Bug 1506733 - (part 4) Decommit unused portion of Nursery chunks r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D35591

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -78,12 +78,22 @@ class NurseryDecommitTask : public GCParallelTaskHelper<NurseryDecommitTask> {
   explicit NurseryDecommitTask(JSRuntime* rt) : GCParallelTaskHelper(rt) {}
 
   void queueChunk(NurseryChunk* chunk, const AutoLockHelperThreadState& lock);
+
+  // queueRange can also update the current to-decommit range of the
+  // current chunk.
+  void queueRange(size_t newCapacity, NurseryChunk& chunk,
+                  const AutoLockHelperThreadState& lock);
+
   void run();
   void decommitChunk(gc::Chunk* chunk);
+  void decommitRange(AutoLockHelperThreadState& lock);
 
  private:
   // Use the next pointers in Chunk::info to form a singly-linked list.
   MainThreadOrGCTaskData<gc::Chunk*> queue;
+
+  MainThreadOrGCTaskData<NurseryChunk*> partialChunk;
+  MainThreadOrGCTaskData<size_t> partialCapacity;
 
   gc::Chunk* popChunk(const AutoLockHelperThreadState& lock);
 };
