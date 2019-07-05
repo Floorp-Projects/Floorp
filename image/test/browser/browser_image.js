cc8e4bb6eb4dbcbeaad0bfc86c76be544a0699da
Victor Porof <vporof@mozilla.com>
2019-07-05 10:47:38 +0200

    Bug 1561435 - Format image/, a=automatic-formatting
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D35906

--- image/test/browser/browser_image.js
+++ image/test/browser/browser_image.js
@@ -11,20 +11,29 @@ function testBFCache() {
   function theTest() {
     var abort = false;
     var chances, gImage, gFrames;
-    gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, TESTROOT + "image.html");
-    gBrowser.selectedBrowser.addEventListener("pageshow", function () {
-      var window = gBrowser.contentWindow;
-      // If false, we are in an optimized build, and we abort this and
-      // all further tests
-      if (!actOnMozImage(window.document, "img1", function(image) {
-        gImage = image;
-        gFrames = gImage.framesNotified;
-      })) {
-        gBrowser.removeCurrentTab();
-        abort = true;
-      }
-      goer.next();
-    }, {capture: true, once: true});
+    gBrowser.selectedTab = BrowserTestUtils.addTab(
+      gBrowser,
+      TESTROOT + "image.html"
+    );
+    gBrowser.selectedBrowser.addEventListener(
+      "pageshow",
+      function() {
+        var window = gBrowser.contentWindow;
+        // If false, we are in an optimized build, and we abort this and
+        // all further tests
+        if (
+          !actOnMozImage(window.document, "img1", function(image) {
+            gImage = image;
+            gFrames = gImage.framesNotified;
+          })
+        ) {
+          gBrowser.removeCurrentTab();
+          abort = true;
+        }
+        goer.next();
+      },
+      { capture: true, once: true }
+    );
     yield;
     if (abort) {
       finish();
@@ -35,15 +44,19 @@ function testBFCache() {
     chances = 120;
     do {
       gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-      gTimer.initWithCallback(function() {
-        if (gImage.framesNotified >= 20) {
-          goer.send(true);
-        } else {
-          chances--;
-          goer.send(chances == 0); // maybe if we wait a bit, it will happen
-        }
-      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
-    } while (!(yield));
+      gTimer.initWithCallback(
+        function() {
+          if (gImage.framesNotified >= 20) {
+            goer.send(true);
+          } else {
+            chances--;
+            goer.send(chances == 0); // maybe if we wait a bit, it will happen
+          }
+        },
+        500,
+        Ci.nsITimer.TYPE_ONE_SHOT
+      );
+    } while (!yield);
     is(chances > 0, true, "Must have animated a few frames so far");
 
     // Browse elsewhere; push our animating page into the bfcache
@@ -52,16 +65,30 @@ function testBFCache() {
     // Wait a bit for page to fully load, then wait a while and
     // see that no animation occurs.
     gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-    gTimer.initWithCallback(function() {
-      gFrames = gImage.framesNotified;
-      gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-      gTimer.initWithCallback(function() {
-        // Might have a few stray frames, until other page totally loads
-        var additionalFrames = gImage.framesNotified  - gFrames;
-        is(additionalFrames == 0, true, "Must have not animated in bfcache! Got " + additionalFrames + " additional frames");
-        goer.next();
-      }, 4000, Ci.nsITimer.TYPE_ONE_SHOT); // 4 seconds - expect 40 frames
-    }, 0, Ci.nsITimer.TYPE_ONE_SHOT); // delay of 0 - wait for next event loop
+    gTimer.initWithCallback(
+      function() {
+        gFrames = gImage.framesNotified;
+        gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+        gTimer.initWithCallback(
+          function() {
+            // Might have a few stray frames, until other page totally loads
+            var additionalFrames = gImage.framesNotified - gFrames;
+            is(
+              additionalFrames == 0,
+              true,
+              "Must have not animated in bfcache! Got " +
+                additionalFrames +
+                " additional frames"
+            );
+            goer.next();
+          },
+          4000,
+          Ci.nsITimer.TYPE_ONE_SHOT
+        ); // 4 seconds - expect 40 frames
+      },
+      0,
+      Ci.nsITimer.TYPE_ONE_SHOT
+    ); // delay of 0 - wait for next event loop
     yield;
 
     // Go back
@@ -70,15 +97,19 @@ function testBFCache() {
     chances = 120;
     do {
       gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-      gTimer.initWithCallback(function() {
-        if (gImage.framesNotified - gFrames >= 20) {
-          goer.send(true);
-        } else {
-          chances--;
-          goer.send(chances == 0); // maybe if we wait a bit, it will happen
-        }
-      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
-    } while (!(yield));
+      gTimer.initWithCallback(
+        function() {
+          if (gImage.framesNotified - gFrames >= 20) {
+            goer.send(true);
+          } else {
+            chances--;
+            goer.send(chances == 0); // maybe if we wait a bit, it will happen
+          }
+        },
+        500,
+        Ci.nsITimer.TYPE_ONE_SHOT
+      );
+    } while (!yield);
     is(chances > 0, true, "Must have animated once out of bfcache!");
 
     // Finally, check that the css background image has essentially the same
@@ -90,9 +121,17 @@ function testBFCache() {
     var div = doc.getElementById("background_div");
     div.innerHTML += '<img src="animated2.gif" id="img3">';
     actOnMozImage(doc, "img3", function(image) {
-      is(Math.abs(image.framesNotified - gImage.framesNotified)/gImage.framesNotified < 0.5, true,
-         "Must have also animated the background image, and essentially the same # of frames. " +
-         "Regular image got " + gImage.framesNotified + " frames but background image got " + image.framesNotified);
+      is(
+        Math.abs(image.framesNotified - gImage.framesNotified) /
+          gImage.framesNotified <
+          0.5,
+        true,
+        "Must have also animated the background image, and essentially the same # of frames. " +
+          "Regular image got " +
+          gImage.framesNotified +
+          " frames but background image got " +
+          image.framesNotified
+      );
     });
 
     gBrowser.removeCurrentTab();
@@ -111,57 +150,87 @@ function testSharedContainers() {
     var gImages = [];
     var gFrames;
 
-    gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, TESTROOT + "image.html");
-    gBrowser.selectedBrowser.addEventListener("pageshow", function () {
-      actOnMozImage(gBrowser.contentDocument, "img1", function(image) {
-        gImages[0] = image;
-        gFrames = image.framesNotified; // May in theory have frames from last test
-                                        // in this counter - so subtract them out
-      });
-      goer.next();
-    }, {capture: true, once: true});
+    gBrowser.selectedTab = BrowserTestUtils.addTab(
+      gBrowser,
+      TESTROOT + "image.html"
+    );
+    gBrowser.selectedBrowser.addEventListener(
+      "pageshow",
+      function() {
+        actOnMozImage(gBrowser.contentDocument, "img1", function(image) {
+          gImages[0] = image;
+          gFrames = image.framesNotified; // May in theory have frames from last test
+          // in this counter - so subtract them out
+        });
+        goer.next();
+      },
+      { capture: true, once: true }
+    );
     yield;
 
     // Load next tab somewhat later
     gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-    gTimer.initWithCallback(function() {
-      goer.next();
-    }, 1500, Ci.nsITimer.TYPE_ONE_SHOT);
+    gTimer.initWithCallback(
+      function() {
+        goer.next();
+      },
+      1500,
+      Ci.nsITimer.TYPE_ONE_SHOT
+    );
     yield;
 
-    gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, TESTROOT + "imageX2.html");
-    gBrowser.selectedBrowser.addEventListener("pageshow", function () {
-      [1,2].forEach(function(i) {
-        actOnMozImage(gBrowser.contentDocument, "img"+i, function(image) {
-          gImages[i] = image;
+    gBrowser.selectedTab = BrowserTestUtils.addTab(
+      gBrowser,
+      TESTROOT + "imageX2.html"
+    );
+    gBrowser.selectedBrowser.addEventListener(
+      "pageshow",
+      function() {
+        [1, 2].forEach(function(i) {
+          actOnMozImage(gBrowser.contentDocument, "img" + i, function(image) {
+            gImages[i] = image;
+          });
         });
-      });
-      goer.next();
-    }, {capture: true, once: true});
+        goer.next();
+      },
+      { capture: true, once: true }
+    );
     yield;
 
     var chances = 120;
     do {
       gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-      gTimer.initWithCallback(function() {
-        if (gImages[0].framesNotified - gFrames >= 10) {
-          goer.send(true);
-        } else {
-          chances--;
-          goer.send(chances == 0); // maybe if we wait a bit, it will happen
-        }
-      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
-    } while (!(yield));
-    is(chances > 0, true, "Must have been animating while showing several images");
+      gTimer.initWithCallback(
+        function() {
+          if (gImages[0].framesNotified - gFrames >= 10) {
+            goer.send(true);
+          } else {
+            chances--;
+            goer.send(chances == 0); // maybe if we wait a bit, it will happen
+          }
+        },
+        500,
+        Ci.nsITimer.TYPE_ONE_SHOT
+      );
+    } while (!yield);
+    is(
+      chances > 0,
+      true,
+      "Must have been animating while showing several images"
+    );
 
     // Check they all have the same frame counts
     var theFrames = null;
-    [0,1,2].forEach(function(i) {
+    [0, 1, 2].forEach(function(i) {
       var frames = gImages[i].framesNotified;
       if (theFrames == null) {
         theFrames = frames;
       } else {
-        is(theFrames, frames, "Sharing the same imgContainer means *exactly* the same frame counts!");
+        is(
+          theFrames,
+          frames,
+          "Sharing the same imgContainer means *exactly* the same frame counts!"
+        );
       }
     });
 
@@ -189,4 +258,3 @@ function test() {
   ignoreAllUncaughtExceptions();
   nextTest();
 }
-
