6e9e02b8e65ec092f61eb84d2a8146f12183bbb9
Dragana Damjanovic <dd.mozilla@gmail.com>
2019-07-05 18:54:34 +0000

    Bug 1563695 - Do not close SpdyConnectTransaction in TLSFilterTransaction. r=mayhemer, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D37048
    
    --HG--
    extra : source : 7d95f38f8dada3b12f299d9fe36822e8dacb6d1e

--- netwerk/protocol/http/TunnelUtils.cpp
+++ netwerk/protocol/http/TunnelUtils.cpp
@@ -130,18 +130,6 @@ void TLSFilterTransaction::Close(nsresult aReason) {
   }
   mTransaction->Close(aReason);
   mTransaction = nullptr;
-
-  RefPtr<NullHttpTransaction> baseTrans(do_QueryReferent(mWeakTrans));
-  SpdyConnectTransaction* trans =
-      baseTrans ? baseTrans->QuerySpdyConnectTransaction() : nullptr;
-
-  LOG(("TLSFilterTransaction::Close %p aReason=%" PRIx32 " trans=%p\n", this,
-       static_cast<uint32_t>(aReason), trans));
-
-  if (trans) {
-    trans->Close(aReason);
-    trans = nullptr;
-  }
 }
 
 nsresult TLSFilterTransaction::OnReadSegment(const char* aData, uint32_t aCount,
