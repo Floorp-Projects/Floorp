5e100d882ff4c19c3660ea73c6ccf14b12487e62
Victor Porof <vporof@mozilla.com>
2019-07-05 10:47:38 +0200

    Bug 1561435 - Format image/, a=automatic-formatting
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D35906

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,6 @@ module.exports = {
   "overrides": [{
       "files": [
         "devtools/**",
-        "image/**",
         "intl/**",
         "ipc/**",
         "js/**",
--- image/test/unit/test_async_notification.js
+++ image/test/unit/test_async_notification.js
@@ -3,8 +3,11 @@
  */
 
 // A simple 3x3 png; rows go red, green, blue. Stolen from the PNG encoder test.
-var pngspec = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAIAAADZSiLoAAAAEUlEQVQImWP4z8AAQTAamQkAhpcI+DeMzFcAAAAASUVORK5CYII=";
-var ioService = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+var pngspec =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAMAAAADCAIAAADZSiLoAAAAEUlEQVQImWP4z8AAQTAamQkAhpcI+DeMzFcAAAAASUVORK5CYII=";
+var ioService = Cc["@mozilla.org/network/io-service;1"].getService(
+  Ci.nsIIOService
+);
 var uri = ioService.newURI(pngspec);
 
-load('async_load_tests.js');
+load("async_load_tests.js");
