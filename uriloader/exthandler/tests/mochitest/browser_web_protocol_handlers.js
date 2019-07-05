982d30bde216ca49232ceb4a3b55d01b0e4be52d
Victor Porof <vporof@mozilla.com>
2019-07-05 11:18:48 +0200

    Bug 1561435 - Format uriloader/, a=automatic-formatting
    # ignore-this-changeset
    
    Differential Revision: https://phabricator.services.mozilla.com/D35942

--- uriloader/exthandler/tests/mochitest/browser_web_protocol_handlers.js
+++ uriloader/exthandler/tests/mochitest/browser_web_protocol_handlers.js
@@ -1,4 +1,5 @@
-let testURL = "https://example.com/browser/" +
+let testURL =
+  "https://example.com/browser/" +
   "uriloader/exthandler/tests/mochitest/protocolHandler.html";
 
 add_task(async function() {
@@ -18,45 +19,66 @@ add_task(async function() {
   button.click();
 
   // Set the new handler as default.
-  const protoSvc = Cc["@mozilla.org/uriloader/external-protocol-service;1"].
-                     getService(Ci.nsIExternalProtocolService);
+  const protoSvc = Cc[
+    "@mozilla.org/uriloader/external-protocol-service;1"
+  ].getService(Ci.nsIExternalProtocolService);
   let protoInfo = protoSvc.getProtocolHandlerInfo("web+testprotocol");
-  is(protoInfo.preferredAction, protoInfo.useHelperApp,
-     "using a helper application is the preferred action");
+  is(
+    protoInfo.preferredAction,
+    protoInfo.useHelperApp,
+    "using a helper application is the preferred action"
+  );
   ok(!protoInfo.preferredApplicationHandler, "no preferred handler is set");
   let handlers = protoInfo.possibleApplicationHandlers;
   is(1, handlers.length, "only one handler registered for web+testprotocol");
   let handler = handlers.queryElementAt(0, Ci.nsIHandlerApp);
   ok(handler instanceof Ci.nsIWebHandlerApp, "the handler is a web handler");
-  is(handler.uriTemplate, "https://example.com/foobar?uri=%s",
-     "correct url template");
+  is(
+    handler.uriTemplate,
+    "https://example.com/foobar?uri=%s",
+    "correct url template"
+  );
   protoInfo.preferredApplicationHandler = handler;
   protoInfo.alwaysAskBeforeHandling = false;
-  const handlerSvc = Cc["@mozilla.org/uriloader/handler-service;1"].
-                       getService(Ci.nsIHandlerService);
+  const handlerSvc = Cc["@mozilla.org/uriloader/handler-service;1"].getService(
+    Ci.nsIHandlerService
+  );
   handlerSvc.store(protoInfo);
 
   // Middle-click a testprotocol link and check the new tab is correct
   let link = "#link";
-  const expectedURL = "https://example.com/foobar?uri=web%2Btestprotocol%3Atest";
+  const expectedURL =
+    "https://example.com/foobar?uri=web%2Btestprotocol%3Atest";
 
-  let promiseTabOpened =
-    BrowserTestUtils.waitForNewTab(gBrowser, expectedURL);
-  await BrowserTestUtils.synthesizeMouseAtCenter(link, {button: 1}, browser);
+  let promiseTabOpened = BrowserTestUtils.waitForNewTab(gBrowser, expectedURL);
+  await BrowserTestUtils.synthesizeMouseAtCenter(link, { button: 1 }, browser);
   let tab = await promiseTabOpened;
   gBrowser.selectedTab = tab;
-  is(gURLBar.value, expectedURL,
-     "the expected URL is displayed in the location bar");
+  is(
+    gURLBar.value,
+    expectedURL,
+    "the expected URL is displayed in the location bar"
+  );
   BrowserTestUtils.removeTab(tab);
 
   // Shift-click the testprotocol link and check the new window.
-  let newWindowPromise = BrowserTestUtils.waitForNewWindow({url: expectedURL});
-  await BrowserTestUtils.synthesizeMouseAtCenter(link, {shiftKey: true},
-                                                 browser);
+  let newWindowPromise = BrowserTestUtils.waitForNewWindow({
+    url: expectedURL,
+  });
+  await BrowserTestUtils.synthesizeMouseAtCenter(
+    link,
+    { shiftKey: true },
+    browser
+  );
   let win = await newWindowPromise;
-  await BrowserTestUtils.waitForCondition(() => win.gBrowser.currentURI.spec == expectedURL);
-  is(win.gURLBar.value, expectedURL,
-     "the expected URL is displayed in the location bar");
+  await BrowserTestUtils.waitForCondition(
+    () => win.gBrowser.currentURI.spec == expectedURL
+  );
+  is(
+    win.gURLBar.value,
+    expectedURL,
+    "the expected URL is displayed in the location bar"
+  );
   await BrowserTestUtils.closeWindow(win);
 
   // Click the testprotocol link and check the url in the current tab.
@@ -64,8 +86,11 @@ add_task(async function() {
   await BrowserTestUtils.synthesizeMouseAtCenter(link, {}, browser);
   await loadPromise;
   await BrowserTestUtils.waitForCondition(() => gURLBar.value != testURL);
-  is(gURLBar.value, expectedURL,
-     "the expected URL is displayed in the location bar");
+  is(
+    gURLBar.value,
+    expectedURL,
+    "the expected URL is displayed in the location bar"
+  );
 
   // Cleanup.
   protoInfo.preferredApplicationHandler = null;
