d45e85e785fdf8da88a65d14154292181e363e94
Jonathan Kew <jkew@mozilla.com>
2019-07-08 19:47:03 +0000

    Bug 1554208 - patch 5 - Implement shared-fontlist support in the gfxFT2FontList backend. r=jwatt
    This is the main part of the implementation, except that it doesn't handle populating the
    local names table (for @font-face src:local() lookups) with Full and PostScript names;
    that follows in the next patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36110
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/thebes/gfxFT2FontList.h
+++ gfx/thebes/gfxFT2FontList.h
@@ -84,6 +84,7 @@ class FT2FontEntry : public gfxFontEntry {
   // Check for various kinds of brokenness, and set flags on the entry
   // accordingly so that we avoid using bad font tables
   void CheckForBrokenFont(gfxFontFamily* aFamily);
+  void CheckForBrokenFont(const nsACString& aFamilyName);
 
   FT_MM_Var* GetMMVar() override;
 
@@ -122,6 +123,10 @@ class gfxFT2FontList : public gfxPlatformFontList {
   gfxFT2FontList();
   virtual ~gfxFT2FontList();
 
+  gfxFontEntry* CreateFontEntry(
+      mozilla::fontlist::Face* aFace,
+      const mozilla::fontlist::Family* aFamily) override;
+
   gfxFontEntry* LookupLocalFont(const nsACString& aFontName,
                                 WeightRange aWeightForEntry,
                                 StretchRange aStretchForEntry,
@@ -144,7 +149,7 @@ class gfxFT2FontList : public gfxPlatformFontList {
   }
 
   void GetFontFamilyList(
-      nsTArray<RefPtr<gfxFontFamily> >& aFamilyArray) override;
+      nsTArray<RefPtr<gfxFontFamily>>& aFamilyArray) override;
 
   gfxFontFamily* CreateFontFamily(const nsACString& aName) const override;
 
@@ -166,11 +171,29 @@ class gfxFT2FontList : public gfxPlatformFontList {
                                    const nsCString& aEntryName,
                                    FontNameCache* aCache, bool aJarChanged);
 
-  // Returns true if it is able to read at least one face entry; false if no
-  // usable face entry was found.
-  bool AppendFacesFromCachedFaceList(const nsCString& aFileName,
+  void InitSharedFontListForPlatform() override;
+  void CollectInitData(const nsCString& aFamilyName, const FontListEntry& aFLE,
+                       StandardFile aStdFile);
+
+  /**
+   * Callback passed to AppendFacesFromCachedFaceList to collect family/face
+   * information in either the unshared or shared list we're building.
+   */
+  typedef void (*CollectFunc)(const nsCString& aFamilyName,
+                              const FontListEntry& aFLE, StandardFile aStdFile);
+
+  /**
+   * Append faces from the face-list record for a specific file.
+   * aCollectFace is a callback that will store the face(s) in either the
+   * unshared mFontFamilies list or the mFamilyInitData/mFaceInitData tables
+   * that will be used to initialize the shared list.
+   * Returns true if it is able to read at least one face entry; false if no
+   * usable face entry was found.
+   */
+  bool AppendFacesFromCachedFaceList(CollectFunc aCollectFace,
+                                     const nsCString& aFileName,
                                      const nsCString& aFaceList,
-                                     StandardFile aStdFile = kStandard);
+                                     StandardFile aStdFile);
 
   void AddFaceToList(const nsCString& aEntryName, uint32_t aIndex,
                      StandardFile aStdFile, FT_Face aFace,
@@ -190,6 +213,11 @@ class gfxFT2FontList : public gfxPlatformFontList {
   mozilla::UniquePtr<FontNameCache> mFontNameCache;
   int64_t mJarModifiedTime;
   RefPtr<WillShutdownObserver> mObserver;
+
+  nsTArray<mozilla::fontlist::Family::InitData> mFamilyInitData;
+  nsClassHashtable<nsCStringHashKey,
+                   nsTArray<mozilla::fontlist::Face::InitData>>
+      mFaceInitData;
 };
 
 #endif /* GFX_FT2FONTLIST_H */
