519e918209c530646b88a185a661af6f1afa8f15
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-07-08 09:25:10 +0000

    Bug 1557305 - Modify autocomplete DAMP test. r=ochameau.
    We now prepend a very large string before the value we
    want to autocomplete so we can check it doesn't have too
    much impact on the overall autocomplete function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36573
    
    --HG--
    extra : moz-landing-system : lando

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/autocomplete.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/autocomplete.js
@@ -37,28 +37,43 @@ module.exports = async function() {
   const test = runTest(TEST_NAME);
   const ITERATIONS = 5;
   for (let i = 0; i < ITERATIONS; i++) {
-    await showAndHideAutoCompletePopup(jsterm);
+    await triggerAutocompletePopupAndUpdate(jsterm);
+    await hideAutocompletePopup(jsterm);
   }
-
   test.done();
+
+  const longInputTest = runTest(TEST_NAME + ".longInput");
+  for (let i = 0; i < ITERATIONS; i++) {
+    await triggerAutocompletePopup(jsterm, true);
+    await hideAutocompletePopup(jsterm);
+  }
+  longInputTest.done();
 
   await closeToolbox();
   await testTeardown();
 };
 
-async function showAndHideAutoCompletePopup(jsterm) {
+async function triggerAutocompletePopupAndUpdate(jsterm) {
   await triggerAutocompletePopup(jsterm);
-  await hideAutocompletePopup(jsterm);
-}
-
-async function triggerAutocompletePopup(jsterm) {
-  const onPopupOpened = jsterm.autocompletePopup.once("popup-opened");
-  setJsTermValueForCompletion(jsterm, "window.autocompleteTest.");
-  await onPopupOpened;
 
   const onPopupUpdated = jsterm.once("autocomplete-updated");
   setJsTermValueForCompletion(jsterm, "window.autocompleteTest.item9");
   await onPopupUpdated;
+}
+
+const LONG_INPUT_PREFIX = `var data = [ ${"{ hello : 'world', foo: [ 1, 2, 3] }, ".repeat(
+  500
+)}];`;
+
+async function triggerAutocompletePopup(jsterm, withLongPrefix = false) {
+  const onPopupOpened = jsterm.autocompletePopup.once("popup-opened");
+
+  let inputValue = "window.autocompleteTest.";
+  if (withLongPrefix) {
+    inputValue = `${LONG_INPUT_PREFIX}\n${inputValue}`;
+  }
+  setJsTermValueForCompletion(jsterm, inputValue);
+  await onPopupOpened;
 }
 
 function hideAutocompletePopup(jsterm) {
