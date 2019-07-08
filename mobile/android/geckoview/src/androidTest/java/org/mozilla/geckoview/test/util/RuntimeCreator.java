0244ae3772e58f8aa757b3286800d530ba0e827c
Agi Sferro <agi@sferro.dev>
2019-07-08 20:48:42 +0000

    Bug 1553515 - Replace loopUntilIdle -> waitForCondition. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D32583

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/util/RuntimeCreator.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/util/RuntimeCreator.java
@@ -5,11 +5,13 @@ import org.mozilla.geckoview.GeckoRuntimeSettings;
 import org.mozilla.geckoview.test.TestCrashHandler;
 
 import android.os.Process;
+import android.support.annotation.UiThread;
 import android.support.test.InstrumentationRegistry;
 
 public class RuntimeCreator {
     private static GeckoRuntime sRuntime;
 
+    @UiThread
     public static GeckoRuntime getRuntime() {
         if (sRuntime != null) {
             return sRuntime;
@@ -30,12 +32,7 @@ public class RuntimeCreator {
                 InstrumentationRegistry.getTargetContext(),
                 runtimeSettingsBuilder.build());
 
-        sRuntime.setDelegate(new GeckoRuntime.Delegate() {
-            @Override
-            public void onShutdown() {
-                Process.killProcess(Process.myPid());
-            }
-        });
+        sRuntime.setDelegate(() -> Process.killProcess(Process.myPid()));
 
         return sRuntime;
     }
