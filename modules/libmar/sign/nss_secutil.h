95a130621a58cf8d8f85c303f7e491788836b304
Sylvestre Ledru <sledru@mozilla.com>
2019-07-08 15:38:50 +0000

    Bug 1563903 - Reformat libmar to the coding style r=Ehsan
    Depends on D37152
    
    Differential Revision: https://phabricator.services.mozilla.com/D37153

--- modules/libmar/sign/nss_secutil.h
+++ modules/libmar/sign/nss_secutil.h
@@ -23,19 +23,18 @@ typedef struct {
     PW_PLAINTEXT = 2,
     PW_EXTERNAL = 3
   } source;
-  char *data;
+  char* data;
 } secuPWData;
 
-#if( defined(_WINDOWS) && !defined(_WIN32_WCE))
-#include <conio.h>
-#include <io.h>
-#define QUIET_FGETS quiet_fgets
-static char * quiet_fgets (char *buf, int length, FILE *input);
+#if (defined(_WINDOWS) && !defined(_WIN32_WCE))
+#  include <conio.h>
+#  include <io.h>
+#  define QUIET_FGETS quiet_fgets
+static char* quiet_fgets(char* buf, int length, FILE* input);
 #else
-#define QUIET_FGETS fgets
+#  define QUIET_FGETS fgets
 #endif
 
-char *
-SECU_GetModulePassword(PK11SlotInfo *slot, PRBool retry, void *arg);
+char* SECU_GetModulePassword(PK11SlotInfo* slot, PRBool retry, void* arg);
 
 #endif
