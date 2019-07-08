099ebfa9c1f6f13851e737c04b70a61d52321d8a
Tiger Oakes <toakes@mozilla.com>
2019-07-08 11:50:25 -0400

    [components] Use deprecated annotation for pxToDp

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/net/Uri.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/net/Uri.kt
@@ -6,24 +6,22 @@ package mozilla.components.support.ktx.android.net
 
 import android.net.Uri
 
+private val commonPrefixes = listOf("www.", "mobile.", "m.")
+
 /**
  * Returns the host without common prefixes like "www" or "m".
  */
-@Suppress("MagicNumber")
 val Uri.hostWithoutCommonPrefixes: String?
     get() {
         val host = host ?: return null
-
-        return when {
-            host.startsWith("www.") -> host.substring(4)
-            host.startsWith("mobile.") -> host.substring(7)
-            host.startsWith("m.") -> host.substring(2)
-            else -> host
+        for (prefix in commonPrefixes) {
+            if (host.startsWith(prefix)) return host.substring(prefix.length)
         }
+        return host
     }
 
 /**
  * Returns true if the [Uri] uses the "http" or "https" protocol scheme.
  */
 val Uri.isHttpOrHttps: Boolean
-    get() = (scheme?.equals("http") ?: false) || (scheme?.equals("https") ?: false)
+    get() = scheme == "http" || scheme == "https"
