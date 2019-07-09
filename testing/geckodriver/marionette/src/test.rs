0103a002ff5c05eaf60c681c70ad0f5c86885f3e
Nupur Baghel <nupurbaghel@gmail.com>
2019-07-09 12:10:10 +0000

    Bug 1560836 - Create marionette subcrate in geckodriver. r=ato
    Differential Revision: https://phabricator.services.mozilla.com/D37375

---
+++ testing/geckodriver/marionette/src/test.rs
@@ -0,0 +1,19 @@
+pub fn assert_ser_de<T>(data: &T, json: serde_json::Value)
+where
+    T: std::fmt::Debug,
+    T: std::cmp::PartialEq,
+    T: serde::de::DeserializeOwned,
+    T: serde::Serialize,
+{
+    assert_eq!(serde_json::to_value(data).unwrap(), json);
+    assert_eq!(data, &serde_json::from_value::<T>(json).unwrap());
+}
+
+pub fn assert_ser<T>(data: &T, json: serde_json::Value)
+where
+    T: std::fmt::Debug,
+    T: std::cmp::PartialEq,
+    T: serde::Serialize,
+{
+    assert_eq!(serde_json::to_value(data).unwrap(), json);
+}
