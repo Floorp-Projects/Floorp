356d01ef38b4d06b3bce930d5257fee068b1f461
Mirko Brodesser <mbrodesser@mozilla.com>
2019-07-09 15:29:49 +0200

    Bug 1564926: remove dead code returning pointers to local variables in `Selection::GetIndicesForInterval`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D37576

--- dom/base/Selection.h
+++ dom/base/Selection.h
@@ -744,8 +744,8 @@ class Selection final : public nsSupportsWeakReference,
   nsresult GetIndicesForInterval(const nsINode* aBeginNode,
                                  int32_t aBeginOffset, const nsINode* aEndNode,
                                  int32_t aEndOffset, bool aAllowAdjacent,
-                                 int32_t* aStartIndex,
-                                 int32_t* aEndIndex) const;
+                                 int32_t& aStartIndex,
+                                 int32_t& aEndIndex) const;
   RangeData* FindRangeData(nsRange* aRange);
 
   static void UserSelectRangesToAdd(nsRange* aItem,
