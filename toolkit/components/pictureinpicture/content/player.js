06a82260297da58d2dcff6355cf764c878080ea0
Mike Conley <mconley@mozilla.com>
2019-07-09 21:37:26 +0000

    Bug 1560590 - Add Telemetry event probes to determine if users tend to move or resize the Picture-in-Picture window after opening it. data-review=chutten, r=JSON_voorhees, a=RyanVM
    Depends on D36359
    
    Differential Revision: https://phabricator.services.mozilla.com/D36360

--- toolkit/components/pictureinpicture/content/player.js
+++ toolkit/components/pictureinpicture/content/player.js
@@ -5,11 +5,16 @@
 const { PictureInPicture } = ChromeUtils.import(
   "resource://gre/modules/PictureInPicture.jsm"
 );
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { DeferredTask } = ChromeUtils.import(
+  "resource://gre/modules/DeferredTask.jsm"
+);
 
 // Time to fade the Picture-in-Picture video controls after first opening.
-const CONTROLS_FADE_TIMEOUT = 3000;
+const CONTROLS_FADE_TIMEOUT_MS = 3000;
+const RESIZE_DEBOUNCE_RATE_MS = 500;
 
-async function setupPlayer(originatingBrowser, videoData) {
+async function setupPlayer(id, originatingBrowser, videoData) {
   let holder = document.querySelector(".player-holder");
   let browser = document.getElementById("browser");
   browser.remove();
@@ -43,10 +48,6 @@ async function setupPlayer(originatingBrowser, videoData) {
     PictureInPicture.closePipWindow({ reason: "browser-crash" });
   });
 
-  window.addEventListener("unload", () => {
-    PictureInPicture.unload(window);
-  });
-
   let close = document.getElementById("close");
   close.addEventListener("click", () => {
     PictureInPicture.closePipWindow({ reason: "close-button" });
@@ -55,5 +56,46 @@ async function setupPlayer(originatingBrowser, videoData) {
   document.getElementById("controls").setAttribute("showing", true);
   setTimeout(() => {
     document.getElementById("controls").removeAttribute("showing");
-  }, CONTROLS_FADE_TIMEOUT);
+  }, CONTROLS_FADE_TIMEOUT_MS);
+
+  Services.telemetry.setEventRecordingEnabled("pictureinpicture", true);
+
+  let resizeDebouncer = new DeferredTask(() => {
+    Services.telemetry.recordEvent("pictureinpicture", "resize", "player", id, {
+      width: window.outerWidth.toString(),
+      height: window.outerHeight.toString(),
+    });
+  }, RESIZE_DEBOUNCE_RATE_MS);
+
+  addEventListener("resize", e => {
+    resizeDebouncer.disarm();
+    resizeDebouncer.arm();
+  });
+
+  let lastScreenX = window.screenX;
+  let lastScreenY = window.screenY;
+
+  addEventListener("mouseout", e => {
+    if (window.screenX != lastScreenX || window.screenY != lastScreenY) {
+      Services.telemetry.recordEvent("pictureinpicture", "move", "player", id, {
+        screenX: window.screenX.toString(),
+        screenY: window.screenY.toString(),
+      });
+    }
+
+    lastScreenX = window.screenX;
+    lastScreenY = window.screenY;
+  });
+
+  Services.telemetry.recordEvent("pictureinpicture", "create", "player", id, {
+    width: window.outerWidth.toString(),
+    height: window.outerHeight.toString(),
+    screenX: window.screenX.toString(),
+    screenY: window.screenY.toString(),
+  });
+
+  window.addEventListener("unload", () => {
+    resizeDebouncer.disarm();
+    PictureInPicture.unload(window);
+  });
 }
