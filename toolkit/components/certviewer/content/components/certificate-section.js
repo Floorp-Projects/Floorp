d319f81ce28347d13073bb3dd4d154fb90c7b33a
dleblanccyr <danielleleb12@gmail.com>
2019-07-09 16:51:12 +0000

    Bug 1560594 - Improves accessibility of certificate-viewer tab panel. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D36981

--- toolkit/components/certviewer/content/components/certificate-section.js
+++ toolkit/components/certviewer/content/components/certificate-section.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* globals certArray, CertificateTab, InfoGroup */
+/* globals certArray, updateSelectedItem, InfoGroup */
 
 class CertificateSection extends HTMLElement {
   constructor() {
@@ -26,18 +26,83 @@ class CertificateSection extends HTMLElement {
     title.textContent = "Certificate";
     this.infoGroupContainer = this.shadowRoot.querySelector(".info-groups");
     for (let i = 0; i < certArray.length; i++) {
-      let tab = certificateTabs.appendChild(new CertificateTab(i));
+      let tab = document.createElement("button");
+      tab.textContent = "tab" + i;
+      tab.setAttribute("id", "tab" + i);
+      tab.setAttribute("aria-controls", "panel" + i);
+      tab.setAttribute("idnumber", i);
+      tab.setAttribute("role", "tab");
+      tab.classList.add("certificate-tab");
+      tab.classList.add("tab");
+      certificateTabs.appendChild(tab);
+
+      // If it is the first tab, allow it to be tabbable by the user.
+      // If it isn't the first tab, do not allow tab functionality,
+      // as arrow functionality is implemented in certviewer.js.
       if (i === 0) {
         tab.classList.add("selected");
+        tab.setAttribute("tabindex", 0);
+      } else {
+        tab.setAttribute("tabindex", -1);
       }
     }
-
+    this.setAccessibilityEventListeners();
     this.infoGroupsContainers[0].classList.add("selected");
+  }
+
+  /* Information on setAccessibilityEventListeners() can be found
+   * at https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Tab_Role */
+  setAccessibilityEventListeners() {
+    const tabs = this.shadowRoot.querySelectorAll('[role="tab"]');
+    const tabList = this.shadowRoot.querySelector('[role="tablist"]');
+
+    // Add a click event handler to each tab
+    tabs.forEach(tab => {
+      tab.addEventListener("click", e =>
+        updateSelectedItem(e.target.getAttribute("idnumber"))
+      );
+    });
+
+    // Enable arrow navigation between tabs in the tab list
+    let tabFocus = 0;
+
+    tabList.addEventListener("keydown", e => {
+      // Move right
+      if (e.keyCode === 39 || e.keyCode === 37) {
+        // After navigating away from the current tab,
+        // prevent that tab from being tabbable -
+        // so as to only allow arrow navigation within the tablist.
+        tabs[tabFocus].setAttribute("tabindex", -1);
+        if (e.keyCode === 39) {
+          tabFocus++;
+          // If we're at the end, go to the start
+          if (tabFocus > tabs.length - 1) {
+            tabFocus = 0;
+          }
+          // Move left
+        } else if (e.keyCode === 37) {
+          tabFocus--;
+          // If we're at the start, move to the end
+          if (tabFocus < 0) {
+            tabFocus = tabs.length;
+          }
+        }
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
+      }
+    });
   }
 
   createInfoGroupsContainers() {
     for (let i = 0; i < certArray.length; i++) {
       this.infoGroupsContainers[i] = document.createElement("div");
+      this.infoGroupsContainers[i].setAttribute("id", "panel" + i);
+      this.infoGroupsContainers[i].setAttribute("role", "tabpanel");
+      this.infoGroupsContainers[i].setAttribute("tabindex", 0);
+      this.infoGroupsContainers[i].setAttribute("aria-labelledby", "tab" + i);
+      if (i !== 0) {
+        this.infoGroupsContainers[i].setAttribute("hidden", true);
+      }
       this.infoGroupsContainers[i].classList.add("info-groups");
       this.shadowRoot.appendChild(this.infoGroupsContainers[i]);
       let arrayItem = certArray[i];
@@ -48,7 +113,7 @@ class CertificateSection extends HTMLElement {
   }
 
   updateSelectedTab(index) {
-    let tabs = this.shadowRoot.querySelectorAll("certificate-tab");
+    let tabs = this.shadowRoot.querySelectorAll(".certificate-tab");
 
     for (let i = 0; i < tabs.length; i++) {
       tabs[i].classList.remove("selected");
