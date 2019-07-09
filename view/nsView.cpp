cdedaf412da5a4865cf92907b5de9a9018de72b5
Nicholas Nethercote <nnethercote@mozilla.com>
2019-07-09 07:59:55 +0000

    Bug 1563996 - Make layout.show_previous_page a static pref. r=bzbarsky
    This is a weird pref.
    
    First, it is VarCached into two different global variables, one in
    nsSubDocumentFrame.cpp, and the other in nsView.cpp.
    
    Second, the pref is not defined by default. When the VarCache variables are
    initialized they are therefore set to the default value provided to the
    `AddBoolVarCache()` call, which in both cases is `true`. This semantics isn't
    possible with `StaticPrefs`, so the patch defines the pref as true by default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37204

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -12,6 +12,7 @@
 #include "mozilla/Likely.h"
 #include "mozilla/Poison.h"
 #include "mozilla/PresShell.h"
+#include "mozilla/StaticPrefs.h"
 #include "nsIWidget.h"
 #include "nsViewManager.h"
 #include "nsIFrame.h"
@@ -23,8 +24,6 @@
 #include "mozilla/CompositeTimelineMarker.h"
 
 using namespace mozilla;
-
-static bool sShowPreviousPage = true;
 
 nsView::nsView(nsViewManager* aViewManager, nsViewVisibility aVisibility)
     : mViewManager(aViewManager),
@@ -47,13 +46,6 @@ nsView::nsView(nsViewManager* aViewManager, nsViewVisibility aVisibility)
   // a promise that the view will paint all its pixels opaquely. Views
   // should make this promise explicitly by calling
   // SetViewContentTransparency.
-
-  static bool sShowPreviousPageInitialized = false;
-  if (!sShowPreviousPageInitialized) {
-    Preferences::AddBoolVarCache(&sShowPreviousPage,
-                                 "layout.show_previous_page", true);
-    sShowPreviousPageInitialized = true;
-  }
 }
 
 void nsView::DropMouseGrabbing() {
@@ -1076,6 +1068,6 @@ nsEventStatus nsView::HandleEvent(WidgetGUIEvent* aEvent,
 }
 
 bool nsView::IsPrimaryFramePaintSuppressed() {
-  return sShowPreviousPage && mFrame &&
+  return StaticPrefs::layout_show_previous_page() && mFrame &&
          mFrame->PresShell()->IsPaintingSuppressed();
 }
