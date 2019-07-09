48de6ceb3a4a21ffa0ad2cbbe5113242a96b34e7
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-07-09 19:45:10 +0000

    Bug 1562176 - fix colons in 'save file as' filenames, r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D37400

--- toolkit/components/downloads/test/unit/test_DownloadPaths.js
+++ toolkit/components/downloads/test/unit/test_DownloadPaths.js
@@ -52,11 +52,11 @@ add_task(async function test_sanitize() {
     testSanitize("Website | Page!", "Website | Page!");
     testSanitize("Directory Listing: /a/b/", "Directory Listing  _a_b_");
   } else {
-    testSanitize(kSpecialChars, kSpecialChars);
-    testSanitize(" :: Website :: ", ":: Website ::");
+    testSanitize(kSpecialChars, kSpecialChars.replace(/[:]/g, " "));
+    testSanitize(" :: Website :: ", "Website");
     testSanitize("* Website!", "* Website!");
     testSanitize("Website | Page!", "Website | Page!");
-    testSanitize("Directory Listing: /a/b/", "Directory Listing: _a_b_");
+    testSanitize("Directory Listing: /a/b/", "Directory Listing  _a_b_");
   }
 
   // Conversion of consecutive runs of slashes and backslashes to underscores.
@@ -79,6 +79,9 @@ add_task(async function test_sanitize() {
   // Stripping of BIDI formatting characters.
   testSanitize("\u200e \u202b\u202c\u202d\u202etest\x7f\u200f", "test");
   testSanitize("AB\x7f\u202a\x7f\u202a\x7fCD", "AB CD");
+
+  // Stripping of colons:
+  testSanitize("foo:bar", "foo bar");
 });
 
 add_task(async function test_splitBaseNameAndExtension() {
