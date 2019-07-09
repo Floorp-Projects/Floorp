064fd41499718eb2108454caf9b9234ca01182e6
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-07-09 12:28:38 +0000

    Bug 1534706 - Don't focus ConfirmDialog when displaying it. r=Honza
    Some people found that the ConfirmDialog was getting into their
    way when typing into the console, as it was stealing the focus.
    This patch fixes this by not focusing the ConfirmDialog when
    we show it, so the user can still type.
    This means that we now handle the dialog confirm and dismiss from
    JSTerm, when the former is displayed.
    
    Since it wasn't clear how you could close the popup, we add a close
    button that makes it very obvious.
    
    This means we can drop the key handler in the dialog as the jsterm
    is always focused.
    We also simply remove the feature to open the MDN link on `?` key
    stroke as it's not discoverable and was the only part of the
    panel where you could do such thing.
    
    Existing tests are adapted and extended to cover the new behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36174
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/test/mochitest/browser_jsterm_autocomplete_getters_cancel.js
+++ devtools/client/webconsole/test/mochitest/browser_jsterm_autocomplete_getters_cancel.js
@@ -45,9 +45,8 @@ async function performTests() {
   );
 
   info("Check that Escape closes the confirm tooltip");
-  let onConfirmTooltipClosed = waitFor(() => !isConfirmDialogOpened(toolbox));
   EventUtils.synthesizeKey("KEY_Escape");
-  await onConfirmTooltipClosed;
+  await waitFor(() => !isConfirmDialogOpened(toolbox));
 
   info("Check that typing a letter won't show the tooltip");
   const onAutocompleteUpdate = jsterm.once("autocomplete-updated");
@@ -66,8 +65,26 @@ async function performTests() {
     "Dialog has expected text content"
   );
 
-  info("Check that ArrowLeft closes the confirm tooltip");
-  onConfirmTooltipClosed = waitFor(() => !isConfirmDialogOpened(toolbox));
-  EventUtils.synthesizeKey("KEY_ArrowLeft");
-  await onConfirmTooltipClosed;
+  info("Check that clicking on the close button closes the tooltip");
+  const closeButtonEl = tooltip.querySelector(".close-confirm-dialog-button");
+  is(closeButtonEl.title, "Close (Esc)", "Close button has the expected title");
+  closeButtonEl.click();
+  await waitFor(() => !isConfirmDialogOpened(toolbox));
+  ok(true, "Clicking the close button does close the tooltip");
+
+  info(
+    "Check that the tooltip closes when there's no more reason to display it"
+  );
+  // Open the tooltip again
+  EventUtils.synthesizeKey(" ", { ctrlKey: true });
+  await waitFor(() => isConfirmDialogOpened(toolbox));
+
+  // Adding a space will make the input `foo.rab.t `, which we shouldn't try to
+  // autocomplete.
+  EventUtils.sendString(" ");
+  await waitFor(() => !isConfirmDialogOpened(toolbox));
+  ok(
+    true,
+    "The tooltip is now closed since the input doesn't match a getter name"
+  );
 }
