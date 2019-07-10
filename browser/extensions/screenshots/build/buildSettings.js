20ffcf934fb7f2188b3b60b1ea1beb574f71f91b
Sylvestre Ledru <sledru@mozilla.com>
2019-07-10 12:03:37 +0000

    Bug 1562642 - Part 3 - Add missing MPL2 headers in browser & toolkit r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D37217

--- browser/extensions/screenshots/build/buildSettings.js
+++ browser/extensions/screenshots/build/buildSettings.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 window.buildSettings = {
   defaultSentryDsn: "https://904ccdd4866247c092ae8fc1a4764a63:940d44bdc71d4daea133c19080ccd38d@sentry.prod.mozaws.net/224",
   logLevel: "" || "warn",
