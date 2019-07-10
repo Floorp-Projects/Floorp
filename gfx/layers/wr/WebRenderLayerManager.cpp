bb3e4e1357dbc244814a543d071fc35beff58e51
Kartikaya Gupta <kgupta@mozilla.com>
2019-07-10 17:26:59 +0000

    Bug 1564947 - Split the WR builder dump index into a RenderRootArray. r=Gankro
    Depends on D37589
    
    Differential Revision: https://phabricator.services.mozilla.com/D37590

--- gfx/layers/wr/WebRenderLayerManager.cpp
+++ gfx/layers/wr/WebRenderLayerManager.cpp
@@ -343,7 +343,8 @@ void WebRenderLayerManager::EndTransactionWithoutLayer(
     mWebRenderCommandBuilder.BuildWebRenderCommands(
         builder, resourceUpdates, aDisplayList, aDisplayListBuilder,
         mScrollDatas, std::move(aFilters));
-    builderDumpIndex = mWebRenderCommandBuilder.GetBuilderDumpIndex();
+    builderDumpIndex = mWebRenderCommandBuilder.GetBuilderDumpIndex(
+        builder.GetRenderRoot());
     containsSVGGroup = mWebRenderCommandBuilder.GetContainsSVGGroup();
   } else {
     // ViewToPaint does not have frame yet, then render only background clolor.
