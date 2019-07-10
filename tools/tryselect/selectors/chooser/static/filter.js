8abb9a8efd3dec62ba96c3eb56841ebd9d297510
Sylvestre Ledru <sledru@mozilla.com>
2019-07-10 19:12:07 +0000

    Bug 1562642 - Part 4 - Add missing MPL2 headers in tools r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D37579
    
    --HG--
    extra : moz-landing-system : lando

--- tools/tryselect/selectors/chooser/static/filter.js
+++ tools/tryselect/selectors/chooser/static/filter.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const selection = $("#selection")[0];
 const count = $("#selection-count")[0];
 const pluralize = (count, noun, suffix = "s") =>
