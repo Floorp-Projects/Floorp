13af736b5d9a8ee6308e1ca72f8651076538767e
Sylvestre Ledru <sledru@mozilla.com>
2019-07-10 11:15:25 +0000

    Bug 1562642 - Part 3 - Add missing MPL2 headers in browser & toolkit r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D37217

--- browser/components/newtab/webpack.aboutlibrary.config.js
+++ browser/components/newtab/webpack.aboutlibrary.config.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 const path = require("path");
 const config = require("./webpack.system-addon.config.js");
 const absolute = relPath => path.join(__dirname, relPath);
