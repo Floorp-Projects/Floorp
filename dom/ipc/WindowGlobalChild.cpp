dfe2225feecfacf1c73a87f34d1efcd2d63b9d19
Kashav Madan <kmadan@mozilla.com>
2019-07-10 21:13:44 +0000

    Bug 1543251 - Move hasBeforeUnload from PBrowser to PWindowGlobal, r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D37003

--- dom/ipc/WindowGlobalChild.cpp
+++ dom/ipc/WindowGlobalChild.cpp
@@ -46,6 +46,7 @@ WindowGlobalChild::WindowGlobalChild(nsGlobalWindowInner* aWindow,
       mBrowsingContext(aBrowsingContext),
       mInnerWindowId(aWindow->WindowID()),
       mOuterWindowId(aWindow->GetOuterWindow()->WindowID()),
+      mBeforeUnloadListeners(0),
       mIPCClosed(true) {}
 
 already_AddRefed<WindowGlobalChild> WindowGlobalChild::Create(
@@ -155,6 +156,26 @@ bool WindowGlobalChild::IsProcessRoot() {
   }
 
   return !BrowsingContext()->GetEmbedderElement();
+}
+
+void WindowGlobalChild::BeforeUnloadAdded() {
+  // Don't bother notifying the parent if we don't have an IPC link open.
+  if (mBeforeUnloadListeners == 0 && !mIPCClosed) {
+    SendSetHasBeforeUnload(true);
+  }
+
+  mBeforeUnloadListeners++;
+  MOZ_ASSERT(mBeforeUnloadListeners > 0);
+}
+
+void WindowGlobalChild::BeforeUnloadRemoved() {
+  mBeforeUnloadListeners--;
+  MOZ_ASSERT(mBeforeUnloadListeners >= 0);
+
+  // Don't bother notifying the parent if we don't have an IPC link open.
+  if (mBeforeUnloadListeners == 0 && !mIPCClosed) {
+    SendSetHasBeforeUnload(false);
+  }
 }
 
 void WindowGlobalChild::Destroy() {
