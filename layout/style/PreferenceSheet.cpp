7ec3e8fefa7ec29a2d53cfb58e114ffac59cf7bb
Eitan Isaacson <eitan@monotonous.org>
2019-07-10 21:46:21 +0000

    Bug 697836 - Use system's link color and create new visited link color in HC mode. r=heycam
    Differential Revision: https://phabricator.services.mozilla.com/D33540

--- layout/style/PreferenceSheet.cpp
+++ layout/style/PreferenceSheet.cpp
@@ -14,6 +14,8 @@
 #include "mozilla/dom/Document.h"
 #include "nsContentUtils.h"
 
+#define AVG2(a, b) (((a) + (b) + 1) >> 1)
+
 using mozilla::dom::Document;
 
 namespace mozilla {
@@ -65,19 +67,34 @@ void PreferenceSheet::Prefs::Load(bool aIsChrome) {
         LookAndFeel::ColorID::Windowtext, mDefaultColor);
     mDefaultBackgroundColor = LookAndFeel::GetColorUsingStandins(
         LookAndFeel::ColorID::Window, mDefaultBackgroundColor);
+    mLinkColor = LookAndFeel::GetColorUsingStandins(
+        LookAndFeel::ColorID::MozNativehyperlinktext, mLinkColor);
   } else if (usePrefColors) {
     GetColor("browser.display.background_color", mDefaultBackgroundColor);
     GetColor("browser.display.foreground_color", mDefaultColor);
+    GetColor("browser.anchor_color", mLinkColor);
   } else {
     mDefaultColor = LookAndFeel::GetColor(
         LookAndFeel::ColorID::WindowForeground, mDefaultColor);
     mDefaultBackgroundColor = LookAndFeel::GetColor(
         LookAndFeel::ColorID::WindowBackground, mDefaultBackgroundColor);
+    mLinkColor = LookAndFeel::GetColor(
+        LookAndFeel::ColorID::MozNativehyperlinktext, mLinkColor);
   }
 
-  GetColor("browser.anchor_color", mLinkColor);
-  GetColor("browser.active_color", mActiveLinkColor);
-  GetColor("browser.visited_color", mVisitedLinkColor);
+  if (mUseAccessibilityTheme && !nsContentUtils::UseStandinsForNativeColors()) {
+    mActiveLinkColor = mLinkColor;
+    // Visited link color is produced by preserving the foreground's green
+    // and averaging the foreground and background for the red and blue.
+    // This is how IE and Edge do it too.
+    mVisitedLinkColor = NS_RGB(
+        AVG2(NS_GET_R(mDefaultColor), NS_GET_R(mDefaultBackgroundColor)),
+        NS_GET_G(mDefaultColor),
+        AVG2(NS_GET_B(mDefaultColor), NS_GET_B(mDefaultBackgroundColor)));
+  } else {
+    GetColor("browser.active_color", mActiveLinkColor);
+    GetColor("browser.visited_color", mVisitedLinkColor);
+  }
 
   GetColor("browser.display.focus_text_color", mFocusTextColor);
   GetColor("browser.display.focus_background_color", mFocusBackgroundColor);
@@ -115,3 +132,5 @@ void PreferenceSheet::Initialize() {
 }
 
 }  // namespace mozilla
+
+#undef AVG2
