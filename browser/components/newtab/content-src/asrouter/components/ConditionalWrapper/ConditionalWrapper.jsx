fd95abf361d6adb55c1dfbcf0716e03d2b26072d
Sylvestre Ledru <sledru@mozilla.com>
2019-07-10 11:15:25 +0000

    Bug 1562642 - Part 3 - Add missing MPL2 headers in browser & toolkit r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D37217
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/newtab/content-src/asrouter/components/ConditionalWrapper/ConditionalWrapper.jsx
+++ browser/components/newtab/content-src/asrouter/components/ConditionalWrapper/ConditionalWrapper.jsx
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // lifted from https://gist.github.com/kitze/23d82bb9eb0baabfd03a6a720b1d637f
 export const ConditionalWrapper = ({ condition, wrap, children }) =>
   condition ? wrap(children) : children;
