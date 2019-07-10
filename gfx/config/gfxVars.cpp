8d8d3aeca8fd18670439df3de88748f650003417
Barret Rennie <barret@brennie.ca>
2019-07-10 05:25:54 +0000

    Bug 1289211 - Rename InfallibleTArray to nsTArray in gfx/ r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D36958

--- gfx/config/gfxVars.cpp
+++ gfx/config/gfxVars.cpp
@@ -54,7 +54,7 @@ void gfxVars::Initialize() {
                "Initial updates should be provided in content process");
     if (!gGfxVarInitUpdates) {
       // No provided initial updates, sync-request them from parent.
-      InfallibleTArray<GfxVarUpdate> initUpdates;
+      nsTArray<GfxVarUpdate> initUpdates;
       dom::ContentChild::GetSingleton()->SendGetGfxVars(&initUpdates);
       gGfxVarInitUpdates = new nsTArray<GfxVarUpdate>(std::move(initUpdates));
     }
