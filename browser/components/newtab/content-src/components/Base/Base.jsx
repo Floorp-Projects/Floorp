f4d77439444415ae7f088ff80275ba5fb62549b0
Sylvestre Ledru <sledru@mozilla.com>
2019-07-10 12:03:37 +0000

    Bug 1562642 - Part 3 - Add missing MPL2 headers in browser & toolkit r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D37217

--- browser/components/newtab/content-src/components/Base/Base.jsx
+++ browser/components/newtab/content-src/components/Base/Base.jsx
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { actionCreators as ac, actionTypes as at } from "common/Actions.jsm";
 import { ASRouterAdmin } from "content-src/components/ASRouterAdmin/ASRouterAdmin";
 import { ASRouterUISurface } from "../../asrouter/asrouter-content";
--- browser/components/newtab/content-src/components/ComponentPerfTimer/ComponentPerfTimer.jsx
+++ browser/components/newtab/content-src/components/ComponentPerfTimer/ComponentPerfTimer.jsx
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { actionCreators as ac, actionTypes as at } from "common/Actions.jsm";
 import { perfService as perfSvc } from "common/PerfService.jsm";
 import React from "react";
