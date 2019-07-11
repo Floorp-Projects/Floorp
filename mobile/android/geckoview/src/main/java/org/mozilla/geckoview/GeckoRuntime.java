f509e6386fc2bec3f33801a190bfcffe2823ce45
chrmod <chrmod@chrmod.net>
2019-07-11 11:35:25 +0000

    Bug 1539144 - GeckoView extensions support chrome.tabs.create r=snorp,agi,geckoview-reviewers,robwu,rpl
    Differential Revision: https://phabricator.services.mozilla.com/D32796

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -161,6 +161,12 @@ public final class GeckoRuntime implements Parcelable {
     private RuntimeTelemetry mTelemetry;
     private WebExtensionEventDispatcher mWebExtensionDispatcher;
     private StorageController mStorageController;
+    private WebExtensionController mWebExtensionController;
+
+    public GeckoRuntime() {
+        mWebExtensionDispatcher = new WebExtensionEventDispatcher();
+        mWebExtensionController = new WebExtensionController(this, mWebExtensionDispatcher);
+    }
 
     /**
      * Attach the runtime to the given context.
@@ -253,8 +259,6 @@ public final class GeckoRuntime implements Parcelable {
         GeckoAppShell.setCrashHandlerService(settings.getCrashHandler());
         GeckoFontScaleListener.getInstance().attachToContext(context, settings);
 
-        mWebExtensionDispatcher = new WebExtensionEventDispatcher();
-
         final GeckoThread.InitInfo info = new GeckoThread.InitInfo();
         info.args = settings.getArguments();
         info.extras = settings.getExtras();
@@ -327,6 +331,16 @@ public final class GeckoRuntime implements Parcelable {
     public static @NonNull GeckoRuntime create(final @NonNull Context context) {
         ThreadUtils.assertOnUiThread();
         return create(context, new GeckoRuntimeSettings());
+    }
+
+    /**
+     * Returns a WebExtensionController for this GeckoRuntime.
+     *
+     * @return an instance of {@link WebExtensionController}.
+     */
+    @UiThread
+    public @NonNull WebExtensionController getWebExtensionController() {
+        return mWebExtensionController;
     }
 
     /**
