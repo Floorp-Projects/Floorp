2ed6e0afdc7ff7ea0e36c6e3cd855c625eed5c10
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-07-11 18:48:39 +0300

    Backed out changeset 2c36222570a9 (bug 1552141) for causing bc failures in browser_keywordBookmarklets.js a=backout

--- browser/components/urlbar/tests/browser/browser_keywordBookmarklets.js
+++ browser/components/urlbar/tests/browser/browser_keywordBookmarklets.js
@@ -3,111 +3,53 @@
 
 "use strict";
 
-add_task(async function setup() {
+add_task(async function test_keyword_bookmarklet() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
   let bm = await PlacesUtils.bookmarks.insert({
     parentGuid: PlacesUtils.bookmarks.unfiledGuid,
     title: "bookmarklet",
-    url: "javascript:'%s'%20",
+    url: "javascript:'1';",
   });
-  await PlacesUtils.keywords.insert({ keyword: "bm", url: bm.url });
+
   registerCleanupFunction(async function() {
+    BrowserTestUtils.removeTab(tab);
     await PlacesUtils.bookmarks.remove(bm);
   });
 
-  let testFns = [
-    function() {
-      info("Type keyword and immediately press enter");
-      gURLBar.value = "bm";
-      gURLBar.focus();
-      EventUtils.synthesizeKey("KEY_Enter");
-      return "javascript:''%20";
-    },
-    function() {
-      info("Type keyword with searchstring and immediately press enter");
-      gURLBar.value = "bm a";
-      gURLBar.focus();
-      EventUtils.synthesizeKey("KEY_Enter");
-      return "javascript:'a'%20";
-    },
-    async function() {
-      info("Search keyword, then press enter");
-      await promiseAutocompleteResultPopup("bm");
-      let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
-      Assert.equal(result.title, "javascript:'' ", "Check title");
-      EventUtils.synthesizeKey("KEY_Enter");
-      return "javascript:''%20";
-    },
-    async function() {
-      info("Search keyword with searchstring, then press enter");
-      await promiseAutocompleteResultPopup("bm a");
-      let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
-      Assert.equal(result.title, "javascript:'a' ", "Check title");
-      EventUtils.synthesizeKey("KEY_Enter");
-      return "javascript:'a'%20";
-    },
-    async function() {
-      await promiseAutocompleteResultPopup("bm");
-      let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
-      Assert.equal(result.title, "javascript:'' ", "Check title");
-      let element = UrlbarTestUtils.getSelectedElement(window);
-      EventUtils.synthesizeMouseAtCenter(element, {});
-      return "javascript:''%20";
-    },
-    async function() {
-      info("Search keyword with searchstring, then click");
-      await promiseAutocompleteResultPopup("bm a");
-      let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 0);
-      Assert.equal(result.title, "javascript:'a' ", "Check title");
-      let element = UrlbarTestUtils.getSelectedElement(window);
-      EventUtils.synthesizeMouseAtCenter(element, {});
-      return "javascript:'a'%20";
-    },
-  ];
-  for (let testFn of testFns) {
-    await do_test(testFn);
+  let originalPrincipal = gBrowser.contentPrincipal;
+  let originalPrincipalURI = await getPrincipalURI(tab.linkedBrowser);
+
+  await PlacesUtils.keywords.insert({ keyword: "bm", url: "javascript:'1';" });
+
+  // Enter bookmarklet keyword in the URL bar
+  gURLBar.value = "bm";
+  gURLBar.focus();
+  EventUtils.synthesizeKey("KEY_Enter");
+
+  await BrowserTestUtils.waitForContentEvent(
+    gBrowser.selectedBrowser,
+    "pageshow"
+  );
+
+  let newPrincipalURI = await getPrincipalURI(tab.linkedBrowser);
+  is(newPrincipalURI, originalPrincipalURI, "content has the same principal");
+
+  // In e10s, null principals don't round-trip so the same null principal sent
+  // from the child will be a new null principal. Verify that this is the
+  // case.
+  if (tab.linkedBrowser.isRemoteBrowser) {
+    ok(
+      originalPrincipal.isNullPrincipal &&
+        gBrowser.contentPrincipal.isNullPrincipal,
+      "both principals should be null principals in the parent"
+    );
+  } else {
+    ok(
+      gBrowser.contentPrincipal.equals(originalPrincipal),
+      "javascript bookmarklet should inherit principal"
+    );
   }
 });
-
-async function do_test(loadFn) {
-  await BrowserTestUtils.withNewTab(
-    {
-      gBrowser,
-    },
-    async browser => {
-      let originalPrincipal = gBrowser.contentPrincipal;
-      let originalPrincipalURI = await getPrincipalURI(browser);
-
-      let promise = BrowserTestUtils.waitForContentEvent(browser, "pageshow");
-      let expectedUrl = await loadFn();
-      info("Awaiting pageshow event");
-      await promise;
-      Assert.equal(gBrowser.currentURI.spec, expectedUrl);
-
-      let newPrincipalURI = await getPrincipalURI(browser);
-      Assert.equal(
-        newPrincipalURI,
-        originalPrincipalURI,
-        "content has the same principal"
-      );
-
-      // In e10s, null principals don't round-trip so the same null principal sent
-      // from the child will be a new null principal. Verify that this is the
-      // case.
-      if (browser.isRemoteBrowser) {
-        Assert.ok(
-          originalPrincipal.isNullPrincipal &&
-            gBrowser.contentPrincipal.isNullPrincipal,
-          "both principals should be null principals in the parent"
-        );
-      } else {
-        Assert.ok(
-          gBrowser.contentPrincipal.equals(originalPrincipal),
-          "javascript bookmarklet should inherit principal"
-        );
-      }
-    }
-  );
-}
 
 function getPrincipalURI(browser) {
   return ContentTask.spawn(browser, null, function() {
