d74f5dfdea8613b1114ee698b3cd02eaca72144f
Michael Froman <mfroman@mozilla.com>
2019-07-11 19:43:56 +0000

    Bug 1560366 - add WAV decoding on RDD. r=jya
    Differential Revision: https://phabricator.services.mozilla.com/D37619

--- modules/libpref/init/StaticPrefList.h
+++ modules/libpref/init/StaticPrefList.h
@@ -5880,6 +5880,23 @@ VARCACHE_PREF(
 )
 #undef PREF_VALUE
 
+#if defined(XP_LINUX) && !defined(ANDROID)
+# define PREF_VALUE true
+#elif defined(XP_WIN) && !defined(_ARM64_)
+# define PREF_VALUE false
+#elif defined(XP_MACOSX)
+# define PREF_VALUE true
+#else
+# define PREF_VALUE false
+#endif
+VARCACHE_PREF(
+  Live,
+  "media.rdd-wav.enabled",
+   media_rdd_wav_enabled,
+  RelaxedAtomicBool, PREF_VALUE
+)
+#undef PREF_VALUE
+
 #ifdef ANDROID
 
 // Enable the MediaCodec PlatformDecoderModule by default.
