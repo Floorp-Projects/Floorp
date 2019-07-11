368f90344989e2facc46c82bb07a43e224150019
Emilio Cobos Álvarez <emilio@crisal.io>
2019-07-11 18:49:50 +0000

    Bug 1565129 - Re-introduce plain_text.wrap_long_lines. r=bzbarsky a=ryanvm
    Looks like some users use it, and it's not too much effort to support. This is
    somewhat simpler, and IMO better than what existed before bug 1514655 because:
    
     * It doesn't regress bidi rendering when the pref is disabled (before, the pref
       would prevent plaintext.css from applying altogether).
    
     * It's consistent with the way view-source docs work.
    
     * It doesn't use non-standard stylesheet APIs to toggle the stylesheet
       (bug 1260720).
    
    Differential Revision: https://phabricator.services.mozilla.com/D37742
    
    --HG--
    extra : source : 16bc2f5a8e199244beca4c28b93c0084e333d323

--- parser/html/nsHtml5PlainTextUtils.cpp
+++ parser/html/nsHtml5PlainTextUtils.cpp
@@ -3,12 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsHtml5PlainTextUtils.h"
-#include "mozilla/Preferences.h"
+#include "mozilla/StaticPrefs.h"
 #include "nsHtml5AttributeName.h"
 #include "nsHtml5Portability.h"
 #include "nsHtml5String.h"
-#include "nsIServiceManager.h"
-#include "nsIStringBundle.h"
+#include "nsGkAtoms.h"
 
 // static
 nsHtml5HtmlAttributes* nsHtml5PlainTextUtils::NewLinkAttributes() {
@@ -19,4 +18,16 @@ nsHtml5HtmlAttributes* nsHtml5PlainTextUtils::NewLinkAttributes() {
       "resource://content-accessible/plaintext.css");
   linkAttrs->addAttribute(nsHtml5AttributeName::ATTR_HREF, href, -1);
   return linkAttrs;
+}
+
+// static
+nsHtml5HtmlAttributes* nsHtml5PlainTextUtils::NewBodyAttributes() {
+  if (mozilla::StaticPrefs::plain_text_wrap_long_lines()) {
+    return nsHtml5HtmlAttributes::EMPTY_ATTRIBUTES;
+  }
+  nsHtml5HtmlAttributes* bodyAttrs = new nsHtml5HtmlAttributes(0);
+  RefPtr<nsAtom> nowrap = nsGkAtoms::nowrap;
+  bodyAttrs->addAttribute(nsHtml5AttributeName::ATTR_CLASS,
+                          nsHtml5String::FromAtom(nowrap.forget()), -1);
+  return bodyAttrs;
 }
