e4c98a5a9240fdd4bf3dd7c327847077000636aa
Jeff Muizelaar <jrmuizel@gmail.com>
2019-07-12 15:09:01 +0000

    Bug 1565580. Improve blob metadata deserialization code. r=Gankro
    This removes duplication and makes it a bit safer by using ConvertFromBytes to
    do an unaligned read of the indexOffset.
    
    Also, inner classes can't have template methods. Who knew.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37870

--- gfx/webrender_bindings/Moz2DImageRenderer.cpp
+++ gfx/webrender_bindings/Moz2DImageRenderer.cpp
@@ -305,6 +305,44 @@ static RefPtr<ScaledFont> GetScaledFont(Translator* aTranslator,
   return data.mScaledFont;
 }
 
+template<typename T>
+T ConvertFromBytes(const uint8_t *bytes) {
+  T t;
+  memcpy(&t, bytes, sizeof(T));
+  return t;
+}
+
+struct Reader {
+  const uint8_t* buf;
+  size_t len;
+  size_t pos;
+
+  Reader(const uint8_t* buf, size_t len) : buf(buf), len(len), pos(0) {}
+
+  template<typename T>
+    T Read() {
+      MOZ_RELEASE_ASSERT(pos + sizeof(T) <= len);
+      T ret = ConvertFromBytes<T>(buf + pos);
+      pos += sizeof(ret);
+      return ret;
+    }
+
+    size_t ReadSize() {
+      return Read<size_t>();
+    }
+    int ReadInt() {
+      return Read<int>();
+    }
+
+    IntRectAbsolute ReadBounds() {
+      return Read<IntRectAbsolute>();
+    }
+
+    layers::BlobFont ReadBlobFont() {
+      return Read<layers::BlobFont>();
+    }
+};
+
 static bool Moz2DRenderCallback(const Range<const uint8_t> aBlob,
                                 gfx::IntSize aSize, gfx::SurfaceFormat aFormat,
                                 const uint16_t* aTileSize,
@@ -352,52 +390,10 @@ static bool Moz2DRenderCallback(const Range<const uint8_t> aBlob,
                 aDirtyRect->size.width, aDirtyRect->size.height));
   }
 
-  struct Reader {
-    const uint8_t* buf;
-    size_t len;
-    size_t pos;
-
-    Reader(const uint8_t* buf, size_t len) : buf(buf), len(len), pos(0) {}
-
-    size_t ReadSize() {
-      size_t ret;
-      MOZ_RELEASE_ASSERT(pos + sizeof(ret) <= len);
-      memcpy(&ret, buf + pos, sizeof(ret));
-      pos += sizeof(ret);
-      return ret;
-    }
-    int ReadInt() {
-      int ret;
-      MOZ_RELEASE_ASSERT(pos + sizeof(ret) <= len);
-      memcpy(&ret, buf + pos, sizeof(ret));
-      pos += sizeof(ret);
-      return ret;
-    }
-
-    IntRectAbsolute ReadBounds() {
-      MOZ_RELEASE_ASSERT(pos + sizeof(int32_t) * 4 <= len);
-      int32_t x1, y1, x2, y2;
-      memcpy(&x1, buf + pos + 0 * sizeof(int32_t), sizeof(x1));
-      memcpy(&y1, buf + pos + 1 * sizeof(int32_t), sizeof(y1));
-      memcpy(&x2, buf + pos + 2 * sizeof(int32_t), sizeof(x2));
-      memcpy(&y2, buf + pos + 3 * sizeof(int32_t), sizeof(y2));
-      pos += sizeof(int32_t) * 4;
-      return IntRectAbsolute(x1, y1, x2, y2);
-    }
-
-    layers::BlobFont ReadBlobFont() {
-      MOZ_RELEASE_ASSERT(pos + sizeof(layers::BlobFont) <= len);
-      layers::BlobFont ret;
-      memcpy(&ret, buf + pos, sizeof(ret));
-      pos += sizeof(ret);
-      return ret;
-    }
-  };
-
   // We try hard to not have empty blobs but we can end up with
   // them because of CompositorHitTestInfo and merging.
   MOZ_RELEASE_ASSERT(aBlob.length() >= sizeof(size_t));
-  size_t indexOffset = *(size_t*)(aBlob.end().get() - sizeof(size_t));
+  size_t indexOffset = ConvertFromBytes<size_t>(aBlob.end().get() - sizeof(size_t));
   MOZ_RELEASE_ASSERT(indexOffset <= aBlob.length() - sizeof(size_t));
   Reader reader(aBlob.begin().get() + indexOffset,
                 aBlob.length() - sizeof(size_t) - indexOffset);
