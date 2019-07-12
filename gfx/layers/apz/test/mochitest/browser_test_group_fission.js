99d5d84df8184e13e78b568d3f4b47a20002e53e
Kartikaya Gupta <kgupta@mozilla.com>
2019-07-12 17:13:04 +0000

    Bug 1560312 - Run fission tests without WR as well. r=botond
    This also disables the test on Windows 7 because the newly enabled subtests
    fail intermittently there. We don't care so much about Windows 7 these days,
    and I don't have a local setup to reproduce it, so I didn't investigate the
    failure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37819
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/apz/test/mochitest/browser_test_group_fission.js
+++ gfx/layers/apz/test/mochitest/browser_test_group_fission.js
@@ -14,12 +14,12 @@ add_task(async function test_main() {
   // is fission-enabled.
   var test_urls = [
     httpURL("helper_fission_basic.html"),
+    httpURL("helper_fission_transforms.html"),
+    httpURL("helper_fission_scroll_oopif.html"),
     // add additional tests here
   ];
   if (isWebRender) {
     test_urls = test_urls.concat([
-      httpURL("helper_fission_transforms.html"),
-      httpURL("helper_fission_scroll_oopif.html"),
       // add additional WebRender-specific tests here
     ]);
   }
