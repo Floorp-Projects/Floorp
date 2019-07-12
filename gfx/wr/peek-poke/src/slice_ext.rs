22dc98ae7a0b51779d44047e932aec1fb2cae2ed
Gurzau Raul <rgurzau@mozilla.com>
2019-07-12 04:56:07 +0300

    Backed out 2 changesets (bug 1550640) for webrenderer bustages on a CLOSED TREE.
    Backed out changeset 2dcbe1372a05 (bug 1550640)
    Backed out changeset 84db3a9838a5 (bug 1550640)

--- gfx/wr/peek-poke/src/slice_ext.rs
+++
@@ -1,9 +0,0 @@
-pub trait AsEndMutPtr<T> {
-    fn as_end_mut_ptr(self) -> *mut T;
-}
-
-impl<'a> AsEndMutPtr<u8> for &'a mut [u8] {
-    fn as_end_mut_ptr(self) -> *mut u8 {
-        unsafe { self.as_mut_ptr().add(self.len()) }
-    }
-}
