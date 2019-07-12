017447b6bc1d1f95f36bc13673b3bc07ae1f3796
Jon Coppeard <jcoppeard@mozilla.com>
2019-07-12 15:47:56 +0100

    Bug 1565275 - Don't call drop() on debugger handler that has never been associated with a frame object r=jimb
    The changes made in bug changeset 03e0de747d02 assume that drop() is only called on handlers that have been succesfully set on a DebuggerFrame object, and attempt to remove the memory associated with that object. That logic fails if these are called due to an error during initialisation as is happening here. The patch changes a call to drop() to js_delete() and adds assertions that drop() is only called on handlers that are currently associated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37874

---
+++ js/src/jit-test/tests/debug/bug-1565275.js
@@ -0,0 +1,19 @@
+// |jit-test| skip-if: !('oomTest' in this)
+
+Object.defineProperty(this, "fuzzutils", {
+    value: {
+        evaluate: function() {},
+    }
+});
+var g = newGlobal({
+    newCompartment: true
+});
+g.parent = this;
+g.eval("(" + function() {
+    var dbg = Debugger(parent);
+    dbg.onEnterFrame = function(frame) {
+        frame.onStep = function() {}
+    }
+} + ")()");
+fuzzutils.evaluate();
+oomTest(new Function(), {expectExceptionOnFailure: false});
