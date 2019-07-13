60f6bed06ac33d432a35bafa1c7909ad276e9ad5
Mark Banner <standard8@mozilla.com>
2019-07-13 15:25:13 +0000

    Bug 1565315 - Fix Yandex missing from en builds when in Russian regions. r=mconnor,daleharvey
    Depends on D37869
    
    Differential Revision: https://phabricator.services.mozilla.com/D37872
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/search/tests/xpcshell/searchconfigs/test_google.js
+++ toolkit/components/search/tests/xpcshell/searchconfigs/test_google.js
@@ -18,8 +18,7 @@ const test = new SearchConfigTest({
         regions: ["ru", "tr", "by", "kz"],
         locales: {
           matches: ["ru", "tr", "be", "kk"],
-          // We don't currently enforce this.
-          // startsWith: ["en"],
+          startsWith: ["en"],
         },
       },
       {
