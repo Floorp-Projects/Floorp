79955e3472c7b002a280316b585b7370d1802b66
Maliha Islam <mislam@mozilla.com>
2019-07-15 11:01:04 +0000

    Bug 1478156 - Move shareable focus methods to be used for color picker accessibility, r=yzen,gl
    Differential Revision: https://phabricator.services.mozilla.com/D33885

--- devtools/client/shared/focus.js
+++ devtools/client/shared/focus.js
@@ -4,19 +4,70 @@
 
 "use strict";
 
+/*
+ * Simplied selector targetting elements that can receive the focus, full
+ * version at http://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
+ */
+const focusableSelector = [
+  "a[href]:not([tabindex='-1'])",
+  "button:not([disabled]):not([tabindex='-1'])",
+  "iframe:not([tabindex='-1'])",
+  "input:not([disabled]):not([tabindex='-1'])",
+  "select:not([disabled]):not([tabindex='-1'])",
+  "textarea:not([disabled]):not([tabindex='-1'])",
+  "[tabindex]:not([tabindex='-1'])",
+].join(", ");
+
+/**
+ * Wrap and move keyboard focus to first/last focusable element inside a container
+ * element to prevent the focus from escaping the container.
+ *
+ * @param  {Array} elms
+ *         focusable elements inside a container
+ * @param  {DOMNode} current
+ *         currently focused element inside containter
+ * @param  {Boolean} back
+ *         direction
+ * @return {DOMNode}
+ *         newly focused element
+ */
+function wrapMoveFocus(elms, current, back) {
+  let next;
+
+  if (elms.length === 0) {
+    return false;
+  }
+
+  if (back) {
+    if (elms.indexOf(current) === 0) {
+      next = elms[elms.length - 1];
+      next.focus();
+    }
+  } else if (elms.indexOf(current) === elms.length - 1) {
+    next = elms[0];
+    next.focus();
+  }
+
+  return next;
+}
+
+/**
+ * Get a list of all elements that are focusable with a keyboard inside the parent element
+ *
+ * @param  {DOMNode} parentEl
+ *         parent DOM element to be queried
+ * @return {Array}
+ *         array of focusable children elements inside the parent
+ */
+function getFocusableElements(parentEl) {
+  return parentEl
+    ? Array.from(parentEl.querySelectorAll(focusableSelector))
+    : [];
+}
+
 // Make this available to both AMD and CJS environments
 define(function(require, exports, module) {
-  // Simplied selector targetting elements that can receive the focus, full
-  // version at
-  // http://stackoverflow.com/questions/1599660/which-html-elements-can-receive-focus
-  // .
-  module.exports.focusableSelector = [
-    "a[href]:not([tabindex='-1'])",
-    "button:not([disabled]):not([tabindex='-1'])",
-    "iframe:not([tabindex='-1'])",
-    "input:not([disabled]):not([tabindex='-1'])",
-    "select:not([disabled]):not([tabindex='-1'])",
-    "textarea:not([disabled]):not([tabindex='-1'])",
-    "[tabindex]:not([tabindex='-1'])",
-  ].join(", ");
+  module.exports.focusableSelector = focusableSelector;
+  exports.wrapMoveFocus = wrapMoveFocus;
+  exports.getFocusableElements = getFocusableElements;
 });
