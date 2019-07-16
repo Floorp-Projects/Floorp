e370b0c73a1bae2c62ff4db8d6fb1a1822c5d671
Kershaw Chang <kershaw@mozilla.com>
2019-07-16 08:50:14 +0000

    Bug 1309358 - P1: Add wildcard to Access-Control-Expose-Headers r=baku
    For requests without credentials, add wildcard to Access-Control-Expose-Headers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36624

--- dom/fetch/FetchDriver.cpp
+++ dom/fetch/FetchDriver.cpp
@@ -865,7 +865,8 @@ FetchDriver::OnStartRequest(nsIRequest* aRequest) {
     rv = httpChannel->GetResponseStatusText(statusText);
     MOZ_ASSERT(NS_SUCCEEDED(rv));
 
-    response = new InternalResponse(responseStatus, statusText);
+    response = new InternalResponse(responseStatus, statusText,
+                                    mRequest->GetCredentialsMode());
 
     UniquePtr<mozilla::ipc::PrincipalInfo> principalInfo(
         new mozilla::ipc::PrincipalInfo());
@@ -893,7 +894,8 @@ FetchDriver::OnStartRequest(nsIRequest* aRequest) {
     }
     MOZ_ASSERT(!result.Failed());
   } else {
-    response = new InternalResponse(200, NS_LITERAL_CSTRING("OK"));
+    response = new InternalResponse(200, NS_LITERAL_CSTRING("OK"),
+                                    mRequest->GetCredentialsMode());
 
     if (!contentType.IsEmpty()) {
       nsAutoCString contentCharset;
