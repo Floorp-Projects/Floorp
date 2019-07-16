fb0e9a64d577e95d99515289559959476396b1ad
Edwin Gao <egao@mozilla.com>
2019-07-16 17:47:33 +0000

    Bug 1559975 - fix python2 linter error for tools/jprof/split-profile.py r=ahal
    Changes:
    - add __future__ import statements
    
    Differential Revision: https://phabricator.services.mozilla.com/D37766
    
    --HG--
    extra : moz-landing-system : lando

--- tools/jprof/split-profile.py
+++ tools/jprof/split-profile.py
@@ -52,6 +52,8 @@
 # functions in the input file can lead to a logical splitting of the
 # profile into segments.
 
+from __future__ import absolute_import, print_function
+
 import sys
 import subprocess
 import os.path
