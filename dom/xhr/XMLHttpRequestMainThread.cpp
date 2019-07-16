0f07bc4a3d68dc62d41e0a443a2881317a03b80e
Kershaw Chang <kershaw@mozilla.com>
2019-07-16 08:50:14 +0000

    Bug 1309358 - P1: Add wildcard to Access-Control-Expose-Headers r=baku
    For requests without credentials, add wildcard to Access-Control-Expose-Headers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36624
    
    --HG--
    extra : moz-landing-system : lando

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -1029,10 +1029,14 @@ bool XMLHttpRequestMainThread::IsSafeHeader(
     if (!NS_IsValidHTTPToken(token)) {
       return false;
     }
-    if (aHeader.Equals(token, nsCaseInsensitiveCStringComparator())) {
+
+    if (token.EqualsLiteral("*") && !mFlagACwithCredentials) {
+      isSafe = true;
+    } else if (aHeader.Equals(token, nsCaseInsensitiveCStringComparator())) {
       isSafe = true;
     }
   }
+
   return isSafe;
 }
 
