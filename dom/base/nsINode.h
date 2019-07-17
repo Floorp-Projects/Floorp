dccebf3dec51205aee5d5761f81468dd7ffaba14
Mirko Brodesser <mbrodesser@mozilla.com>
2019-07-17 11:14:35 +0200

    Bug 1566751: update description of `nsINode::ComputeIndexOf` about anonymous content. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D38288

--- dom/base/nsINode.h
+++ dom/base/nsINode.h
@@ -571,9 +571,12 @@ class nsINode : public mozilla::dom::EventTarget {
   nsIContent* GetChildAt_Deprecated(uint32_t aIndex) const;
 
   /**
-   * Get the index of a child within this content
+   * Get the index of a child within this content.
+   *
    * @param aPossibleChild the child to get the index of.
-   * @return the index of the child, or -1 if not a child
+   * @return the index of the child, or -1 if not a child. Be aware that
+   *         anonymous children (e.g. a <div> child of an <input> element) will
+   *         result in -1.
    *
    * If the return value is not -1, then calling GetChildAt_Deprecated() with
    * that value will return aPossibleChild.
