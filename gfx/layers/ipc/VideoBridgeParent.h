e6b8f44989a04e479d5acefaf9c766830b1a37ab
Matt Woodrow <mwoodrow@mozilla.com>
2019-07-17 20:17:45 +0000

    Bug 1566956 - Wait for a response from the VideoBridge to ensure textures are available before telling the content process about them. r=sotaro
    Differential Revision: https://phabricator.services.mozilla.com/D37781
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/ipc/VideoBridgeParent.h
+++ gfx/layers/ipc/VideoBridgeParent.h
@@ -33,6 +33,11 @@ class VideoBridgeParent final : public PVideoBridgeParent,
                                       const uint64_t& aSerial);
   bool DeallocPTextureParent(PTextureParent* actor);
 
+  mozilla::ipc::IPCResult RecvFlush(FlushResolver&& aResolver) {
+    aResolver(true);
+    return IPC_OK();
+  }
+
   // HostIPCAllocator
   base::ProcessId GetChildProcessId() override { return OtherPid(); }
   void NotifyNotUsed(PTextureParent* aTexture,
