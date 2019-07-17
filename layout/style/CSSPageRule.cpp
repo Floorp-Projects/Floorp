e5371027f86daeb5de57b1e8f99c783fe70c3566
Emilio Cobos Álvarez <emilio@crisal.io>
2019-07-17 15:09:58 +0000

    Bug 1566684 - Make sure to unlink rule subclass members before unlinking css::Rule. r=bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D38326

--- layout/style/CSSPageRule.cpp
+++ layout/style/CSSPageRule.cpp
@@ -103,15 +103,17 @@ NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN_INHERITED(CSSPageRule, css::Rule)
   tmp->mDecls.TraceWrapper(aCallbacks, aClosure);
 NS_IMPL_CYCLE_COLLECTION_TRACE_END
 
-NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(CSSPageRule, css::Rule)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(CSSPageRule)
   // Keep this in sync with IsCCLeaf.
 
   // Unlink the wrapper for our declaraton.  This just expands out
   // NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER which we can't use
   // directly because the wrapper is on the declaration, not on us.
+  //
+  // Note that this has to happen before unlinking css::Rule.
   tmp->mDecls.ReleaseWrapper(static_cast<nsISupports*>(p));
   tmp->mDecls.mDecls->SetOwningRule(nullptr);
-NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END_INHERITED(css::Rule)
 
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(CSSPageRule, css::Rule)
   // Keep this in sync with IsCCLeaf.
