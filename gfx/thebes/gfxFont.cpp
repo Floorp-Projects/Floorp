c3c61bb9acc1b7e2bb0a79198b99176d14e77d58
Jonathan Kew <jkew@mozilla.com>
2019-07-18 04:55:05 +0000

    Bug 1513423 - Measure text by always laying out glyphs LTR. r=heycam
    Depends on D38298
    
    Differential Revision: https://phabricator.services.mozilla.com/D38299

--- gfx/thebes/gfxFont.cpp
+++ gfx/thebes/gfxFont.cpp
@@ -2471,7 +2471,6 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
   const gfxTextRun::CompressedGlyph* charGlyphs =
       aTextRun->GetCharacterGlyphs();
   bool isRTL = aTextRun->IsRightToLeft();
-  double direction = aTextRun->GetDirection();
   bool needsGlyphExtents = NeedsGlyphExtents(this, aTextRun);
   gfxGlyphExtents* extents =
       ((aBoundingBoxType == LOOSE_INK_EXTENTS && !needsGlyphExtents &&
@@ -2482,7 +2481,7 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
           : GetOrCreateGlyphExtents(aTextRun->GetAppUnitsPerDevUnit());
   double x = 0;
   if (aSpacing) {
-    x += direction * aSpacing[0].mBefore;
+    x += aSpacing[0].mBefore;
   }
   uint32_t spaceGlyph = GetSpaceGlyph();
   bool allGlyphsInvisible = true;
@@ -2505,7 +2504,7 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
         if (extentsWidth != gfxGlyphExtents::INVALID_WIDTH &&
             aBoundingBoxType == LOOSE_INK_EXTENTS) {
           UnionRange(x, &advanceMin, &advanceMax);
-          UnionRange(x + direction * extentsWidth, &advanceMin, &advanceMax);
+          UnionRange(x + extentsWidth, &advanceMin, &advanceMax);
         } else {
           gfxRect glyphRect;
           if (!extents->GetTightGlyphExtentsAppUnits(this, aRefDrawTarget,
@@ -2514,13 +2513,15 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
                                 metrics.mBoundingBox.Height());
           }
           if (isRTL) {
-            glyphRect.MoveByX(-advance);
+            // In effect, swap left and right sidebearings of the glyph, for
+            // proper accumulation of potentially-overlapping glyph rects.
+            glyphRect.MoveToX(advance - glyphRect.XMost());
           }
           glyphRect.MoveByX(x);
           metrics.mBoundingBox = metrics.mBoundingBox.Union(glyphRect);
         }
       }
-      x += direction * advance;
+      x += advance;
     } else {
       allGlyphsInvisible = false;
       uint32_t glyphCount = glyphData->GetGlyphCount();
@@ -2543,12 +2544,12 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
                                 metrics.mAscent + metrics.mDescent);
           }
           if (isRTL) {
-            glyphRect.MoveByX(-advance);
+            glyphRect.MoveToX(advance - glyphRect.XMost());
           }
           glyphRect.MoveByX(x + details->mOffset.x);
           glyphRect.MoveByY(details->mOffset.y);
           metrics.mBoundingBox = metrics.mBoundingBox.Union(glyphRect);
-          x += direction * advance;
+          x += advance;
         }
       }
     }
@@ -2558,7 +2559,7 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
       if (i + 1 < aEnd) {
         space += aSpacing[i + 1 - aStart].mBefore;
       }
-      x += direction * space;
+      x += space;
     }
   }
 
@@ -2571,9 +2572,12 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
                       metrics.mAscent + metrics.mDescent);
       metrics.mBoundingBox = metrics.mBoundingBox.Union(fontBox);
     }
-    if (isRTL) {
-      metrics.mBoundingBox -= gfxPoint(x, 0);
-    }
+  }
+
+  if (isRTL) {
+    // Reverse the effect of having swapped each glyph's sidebearings, to get
+    // the correct sidebearings of the merged bounding box.
+    metrics.mBoundingBox.MoveToX(x - metrics.mBoundingBox.XMost());
   }
 
   // If the font may be rendered with a fake-italic effect, we need to allow
@@ -2598,7 +2602,8 @@ gfxFont::RunMetrics gfxFont::Measure(const gfxTextRun* aTextRun,
     metrics.mBoundingBox.MoveByY(baselineOffset);
   }
 
-  metrics.mAdvanceWidth = x * direction;
+  metrics.mAdvanceWidth = x;
+
   return metrics;
 }
 
