b4f4b6898e54fb368f9228bddd308b63e9ea4e9d
Jared Wein <jwein@mozilla.com>
2019-07-18 16:24:31 +0000

    Bug 1560431 - Add pref for breach alerts in about:logins and disable it by default. r=groovecoder
    Differential Revision: https://phabricator.services.mozilla.com/D38333
    
    --HG--
    extra : moz-landing-system : lando

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1753,6 +1753,7 @@ pref("signon.management.overrideURI", "about:logins?filter=%DOMAIN%");
 #else
 pref("signon.management.page.enabled", false);
 #endif
+pref("signon.management.page.breach-alerts.enabled", false);
 #ifdef NIGHTLY_BUILD
 // Bug 1563330 tracks shipping this by default.
 pref("signon.showAutoCompleteOrigins", true);
