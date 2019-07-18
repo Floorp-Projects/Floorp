69066c0a6917489457e6891c830b86a84b0c0209
Jason Laster <jlaster@mozilla.com>
2019-07-18 22:14:49 +0000

    Bug 1354679 - re-apply pause overlay. r=davidwalsh
    Differential Revision: https://phabricator.services.mozilla.com/D38572
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/highlighters/paused-debugger.js
+++ devtools/server/actors/highlighters/paused-debugger.js
@@ -9,6 +9,10 @@ const {
   createNode,
 } = require("./utils/markup");
 
+const { LocalizationHelper } = require("devtools/shared/l10n");
+const STRINGS_URI = "devtools/client/shared/locales/debugger.properties";
+const L10N = new LocalizationHelper(STRINGS_URI);
+
 /**
  * The PausedDebuggerOverlay is a class that displays a semi-transparent mask on top of
  * the whole page and a toolbar at the top of the page.
@@ -16,8 +20,12 @@ const {
  * The toolbar is used to display the reason for the pause in script execution as well as
  * buttons to resume or step through the program.
  */
-function PausedDebuggerOverlay(highlighterEnv) {
+function PausedDebuggerOverlay(highlighterEnv, options = {}) {
   this.env = highlighterEnv;
+  this.showOverlayStepButtons = options.showOverlayStepButtons;
+  this.resume = options.resume;
+  this.stepOver = options.stepOver;
+
   this.markup = new CanvasFrameAnonymousContentHelper(
     highlighterEnv,
     this._buildMarkup.bind(this)
@@ -68,6 +76,37 @@ PausedDebuggerOverlay.prototype = {
       prefix,
     });
 
+    if (this.showOverlayStepButtons) {
+      createNode(window, {
+        parent: toolbar,
+        attributes: {
+          id: "divider",
+          class: "divider",
+        },
+        prefix,
+      });
+
+      createNode(window, {
+        nodeType: "button",
+        parent: toolbar,
+        attributes: {
+          id: "step-button",
+          class: "step-button",
+        },
+        prefix,
+      });
+
+      createNode(window, {
+        nodeType: "button",
+        parent: toolbar,
+        attributes: {
+          id: "resume-button",
+          class: "resume-button",
+        },
+        prefix,
+      });
+    }
+
     return container;
   },
 
@@ -77,35 +116,53 @@ PausedDebuggerOverlay.prototype = {
     this.env = null;
   },
 
+  onClick(target) {
+    if (target.id == "paused-dbg-step-button") {
+      this.stepOver();
+    } else if (target.id == "paused-dbg-resume-button") {
+      this.resume();
+    }
+  },
+
+  handleEvent(e) {
+    switch (e.type) {
+      case "click":
+        this.onClick(e.target);
+        break;
+      case "DOMMouseScroll":
+        // Prevent scrolling. That's because we only took a screenshot of the viewport, so
+        // scrolling out of the viewport wouldn't draw the expected things. In the future
+        // we can take the screenshot again on scroll, but for now it doesn't seem
+        // important.
+        e.preventDefault();
+        break;
+      case "mouseover":
+        break;
+    }
+  },
+
   getElement(id) {
     return this.markup.getElement(this.ID_CLASS_PREFIX + id);
   },
 
   show(node, options = {}) {
-    if (this.env.isXUL) {
+    if (this.env.isXUL || !options.reason) {
       return false;
     }
 
     // Show the highlighter's root element.
     const root = this.getElement("root");
     root.removeAttribute("hidden");
-
-    // The page overlay is only shown upon request. Sometimes we just want the toolbar.
-    if (options.onlyToolbar) {
-      root.removeAttribute("overlay");
-    } else {
-      root.setAttribute("overlay", "true");
-    }
+    root.setAttribute("overlay", "true");
 
     // Set the text to appear in the toolbar.
     const toolbar = this.getElement("toolbar");
-    if (options.reason) {
-      this.getElement("reason").setTextContent(options.reason);
-      toolbar.removeAttribute("hidden");
-    } else {
-      toolbar.setAttribute("hidden", "true");
-    }
+    this.getElement("reason").setTextContent(
+      L10N.getStr(`whyPaused.${options.reason}`)
+    );
+    toolbar.removeAttribute("hidden");
 
+    this.env.window.document.setSuppressedEventListener(this);
     return true;
   },
 
