9392762fc2a75eff7404a005216c12c6117b0985
Julian Descottes <jdescottes@mozilla.com>
2019-07-18 10:33:57 +0000

    Bug 1567084 - Remove files related to running netmonitor with launchpad/chrome r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D38462

--- devtools/client/netmonitor/src/connector/chrome-connector.js
+++ devtools/client/netmonitor/src/connector/chrome-connector.js
@@ -7,6 +7,12 @@
 const { ACTIVITY_TYPE } = require("../constants");
 const { CDPConnector } = require("./chrome/events");
 
+/**
+ * DO NOT DELETE THIS FILE
+ *
+ * The ChromeConnector is currently not used, but is kept in tree to illustrate
+ * the Connector abstraction.
+ */
 class ChromeConnector {
   constructor() {
     // Internal properties
