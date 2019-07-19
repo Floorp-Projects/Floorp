901855b08303b8d65fdbd070ff663067c01dff29
Julian Descottes <jdescottes@mozilla.com>
2019-07-19 07:27:44 +0000

    Bug 1550489 - Remove react-redux.js from nsContentSecurityManager whitelist r=ckerschb
    Depends on D38513
    
    Differential Revision: https://phabricator.services.mozilla.com/D38618

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -50,9 +50,6 @@ static nsLiteralCString evalWhitelist[] = {
 
     // Bug 1498560
     NS_LITERAL_CSTRING("chrome://global/content/bindings/autocomplete.xml"),
-    // Bug 1550489
-    NS_LITERAL_CSTRING(
-        "resource://devtools/client/shared/vendor/react-redux.js"),
     // Bug 1550463
     NS_LITERAL_CSTRING("resource://devtools/client/shared/vendor/lodash.js"),
     // Bug 1550471
