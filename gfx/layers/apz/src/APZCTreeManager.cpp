ebb1d25ee57a69c30c20f20d65fe1b093962a324
Kartikaya Gupta <kgupta@mozilla.com>
2019-07-19 16:27:23 +0000

    Bug 1563622 - Miscellaneous useful logging. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D38241

--- gfx/layers/apz/src/APZCTreeManager.cpp
+++ gfx/layers/apz/src/APZCTreeManager.cpp
@@ -2607,6 +2607,8 @@ already_AddRefed<AsyncPanZoomController> APZCTreeManager::GetAPZCAtPointWR(
       hitInfo += CompositorHitTestFlags::eApzAwareListeners;
     }
   }
+  APZCTM_LOG("Successfully matched APZC %p (hit result 0x%x)\n",
+             result.get(), hitInfo.serialize());
   if (!result) {
     // It falls back to the root
     MOZ_ASSERT(scrollId == ScrollableLayerGuid::NULL_SCROLL_ID);
