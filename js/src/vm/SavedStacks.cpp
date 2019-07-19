0693d44efefdd00a3c15ffad95d1cd0b4446c0c9
Jason Laster <jlaster@mozilla.com>
2019-07-19 19:35:58 +0000

    Bug 1564168 - [jsdbg2] Debugger.prototype.enabled should be removed. r=jimb
    Differential Revision: https://phabricator.services.mozilla.com/D37756

--- js/src/vm/SavedStacks.cpp
+++ js/src/vm/SavedStacks.cpp
@@ -1832,7 +1832,7 @@ void SavedStacks::chooseSamplingProbability(Realm* realm) {
     // this is safe as long as dbgp does not escape.
     Debugger* dbgp = p->unbarrieredGet();
 
-    if (dbgp->trackingAllocationSites && dbgp->enabled) {
+    if (dbgp->trackingAllocationSites) {
       foundAnyDebuggers = true;
       probability = std::max(dbgp->allocationSamplingProbability, probability);
     }
