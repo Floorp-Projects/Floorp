4ee81b6e02f9070b17985752532b893c790906af
Julian Descottes <jdescottes@mozilla.com>
2019-07-19 07:26:32 +0000

    Bug 1550476 - Remove jsol.js from nsContentSecurityManager whitelist r=ckerschb
    Depends on D38515
    
    Differential Revision: https://phabricator.services.mozilla.com/D38621

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -50,8 +50,6 @@ static nsLiteralCString evalWhitelist[] = {
 
     // Bug 1498560
     NS_LITERAL_CSTRING("chrome://global/content/bindings/autocomplete.xml"),
-    // Bug 1550476
-    NS_LITERAL_CSTRING("resource://devtools/client/shared/vendor/jsol.js"),
 };
 
 /* static */
