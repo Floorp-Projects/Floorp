42f19c54d1829128ef9c5ab6345cf9cc9e6dbb04
tanhengyeow <E0032242@u.nus.edu>
2019-07-19 13:25:30 +0000

    Bug 1555631 - WebSocket frame payload preview. r=Honza,nchevobbe
    Support WebSocket frame payload preview.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38271

---
+++ devtools/client/netmonitor/src/components/websockets/RawData.js
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { Component } = require("devtools/client/shared/vendor/react");
+const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+
+/**
+ * Shows raw payload of a WebSocket frame.
+ */
+class RawData extends Component {
+  static get propTypes() {
+    return {
+      payload: PropTypes.string.isRequired,
+    };
+  }
+
+  render() {
+    const { payload } = this.props;
+    return dom.div(
+      {
+        className: "ws-frame-rawData-payload",
+      },
+      payload
+    );
+  }
+}
+
+module.exports = RawData;
