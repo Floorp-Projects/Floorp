4030a1c68556b6872b94f0d8ab2efaae67631a5a
Luca Greco <lgreco@mozilla.com>
2019-07-19 11:30:48 +0000

    Bug 1472740 - Remove support for deprecated lwt aliases from WebExtensions theme API. r=ntim,robwu
    Removing the lwt aliases from the theme API schema is not going to be enough, because the images and colors properties are very permissive on the unknown properties (likely to prevent a property supported on chrome but not on firefox to prevent the theme from being installed), and so removing the lwt aliases from the schema would not raise any error (the theme API implementation would just be silently ignoring the deprecated lwt aliases).
    
    For the above reason the following patch use the following approach:
    
    - kept the deprecated lwt aliases in the schema, but changes the deprecation warning message to mention that the property is now completely ignored by Firefox, and which property should be used instead
    
    - removed the deprecation warning from the toolbar_text theme colors property, as we decided that we are not going to deprecate it anymore
    
    - changed the theme API implementation to ignore the deprecated lwt alias property
    
    - repurposed browser_ext_themes_lwtsupport.js test file to verify that the lwt aliases are ignored as expected
    
    A separate addons-linter pull request is going to be created, to ensure that the addons-linter will raise linting errors (instead of linting warning) when these deprecated lwt aliases are being used in a theme (to prevent that newly submitted theme versions including the aliases will go unnoticed).
    
    Depends on D37890
    
    Differential Revision: https://phabricator.services.mozilla.com/D37891
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/extensions/test/browser/browser_ext_themes_lwtsupport.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_lwtsupport.js
@@ -1,6 +1,9 @@
 "use strict";
 
-add_task(async function test_support_LWT_properties() {
+const DEFAULT_THEME_BG_COLOR = "rgb(255, 255, 255)";
+const DEFAULT_THEME_TEXT_COLOR = "rgb(0, 0, 0)";
+
+add_task(async function test_deprecated_LWT_properties_ignored() {
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       theme: {
@@ -25,120 +28,27 @@ add_task(async function test_support_LWT_properties() {
 
   Assert.ok(docEl.hasAttribute("lwtheme"), "LWT attribute should be set");
   Assert.ok(
-    docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should be set"
+    !docEl.hasAttribute("lwtheme-image"),
+    "LWT image attribute should not be set on deprecated headerURL alias"
   );
   Assert.equal(
     docEl.getAttribute("lwthemetextcolor"),
-    "bright",
-    "LWT text color attribute should be set"
+    "dark",
+    "LWT text color attribute should not be set on deprecated textcolor alias"
   );
 
-  Assert.ok(
-    style.backgroundImage.includes("image1.png"),
-    "Expected background image"
-  );
   Assert.equal(
     style.backgroundColor,
-    "rgb(" + hexToRGB(ACCENT_COLOR).join(", ") + ")",
-    "Expected correct background color"
+    DEFAULT_THEME_BG_COLOR,
+    "Expected default theme background color"
   );
   Assert.equal(
     style.color,
-    "rgb(" + hexToRGB(TEXT_COLOR).join(", ") + ")",
-    "Expected correct text color"
+    DEFAULT_THEME_TEXT_COLOR,
+    "Expected default theme text color"
   );
 
   await extension.unload();
 
   Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
-});
-
-add_task(async function test_LWT_image_attribute() {
-  let extension = ExtensionTestUtils.loadExtension({
-    manifest: {
-      theme: {
-        colors: {
-          accentcolor: ACCENT_COLOR,
-          textcolor: TEXT_COLOR,
-        },
-      },
-    },
-  });
-
-  await extension.startup();
-
-  let docEl = window.document.documentElement;
-  Assert.ok(docEl.hasAttribute("lwtheme"), "LWT attribute should be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
-  await extension.unload();
-  Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
-});
-
-add_task(async function test_LWT_does_not_require_accentcolor_textcolor_only() {
-  let extension = ExtensionTestUtils.loadExtension({
-    manifest: {
-      theme: {
-        colors: {
-          textcolor: TEXT_COLOR,
-        },
-      },
-    },
-  });
-
-  await extension.startup();
-
-  let docEl = window.document.documentElement;
-  Assert.ok(docEl.hasAttribute("lwtheme"), "LWT attribute should be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
-  await extension.unload();
-  Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
-});
-
-add_task(async function test_LWT_does_not_require_accentcolor_image_only() {
-  let extension = ExtensionTestUtils.loadExtension({
-    manifest: {
-      theme: {
-        images: {
-          headerURL: "image1.png",
-        },
-      },
-    },
-    files: {
-      "image1.png": BACKGROUND,
-    },
-  });
-
-  await extension.startup();
-
-  let docEl = window.document.documentElement;
-  Assert.ok(docEl.hasAttribute("lwtheme"), "LWT attribute should be set");
-  Assert.ok(
-    docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should be set"
-  );
-  await extension.unload();
-  Assert.ok(!docEl.hasAttribute("lwtheme"), "LWT attribute should not be set");
-  Assert.ok(
-    !docEl.hasAttribute("lwtheme-image"),
-    "LWT image attribute should not be set"
-  );
 });
