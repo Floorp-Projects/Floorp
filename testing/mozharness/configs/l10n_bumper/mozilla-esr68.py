8bf2a89c5823db1eec8f8ebe524af7de45c9cbaa
Aki Sasaki <asasaki@mozilla.com>
2019-07-20 01:10:56 +0000

    Bug 1562902 - fennec l10n bumping on esr68. r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D38782

---
+++ testing/mozharness/configs/l10n_bumper/mozilla-esr68.py
@@ -0,0 +1,38 @@
+import sys
+
+MULTI_REPO = "releases/mozilla-esr68"
+EXES = {}
+if sys.platform.startswith("linux"):
+    EXES = {
+        # Get around the https warnings
+        "hg": ["/usr/local/bin/hg", "--config", "web.cacerts=/etc/pki/tls/certs/ca-bundle.crt"],
+        "hgtool.py": ["/usr/local/bin/hgtool.py"],
+    }
+
+config = {
+    "log_name": "l10n_bumper",
+    "log_type": "multi",
+    "exes": EXES,
+
+    "gecko_pull_url": "https://hg.mozilla.org/{}".format(MULTI_REPO),
+    "gecko_push_url": "ssh://hg.mozilla.org/{}".format(MULTI_REPO),
+
+    "hg_user": "L10n Bumper Bot <release+l10nbumper@mozilla.com>",
+    "ssh_key": "~/.ssh/ffxbld_rsa",
+    "ssh_user": "ffxbld",
+
+    "vcs_share_base": "/builds/hg-shared",
+    "version_path": "mobile/android/config/version-files/release/version.txt",
+    "status_path": ".l10n_bumper_status",
+
+    "bump_configs": [{
+        "path": "mobile/locales/l10n-changesets.json",
+        "format": "json",
+        "name": "Fennec l10n changesets",
+        "revision_url": "https://l10n.mozilla.org/shipping/l10n-changesets?av=fennec%(COMBINED_MAJOR_VERSION)s",
+        "platform_configs": [{
+            "platforms": ["android-multilocale"],
+            "path": "mobile/android/locales/maemo-locales"
+        }],
+    }],
+}
