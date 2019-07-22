7c25e3ff49669982d0095fe22d410ea6c607ee67
Alex Cooper <alcooper@chromium.org>
2019-07-22 11:06:16 +0000

    Bug 1565656 [wpt PR 17810] - Implement Input portions of webxr-test-api, a=testonly
    Automatic update from web-platform-tests
    Implement Input portions of webxr-test-api
    
    This change implements the input related portions of the webxr-test-api
    and updates internal tests to use those methods/constants/inits.
    Most of the logic is ported from xr-internal-device-mocking.js and just
    converted from attributes to a pair of set/clear properties.  No tests
    were actually using the getters, so those were just deleted.  Because
    the webxr-test-api does not yet support gamepads, all of that logic was
    left in the internal mock.
    
    It also forces/fixes a potential bug where a device with no inputs may
    opt to set the "InputStates" value of the mojom to null rather than an
    empty array. (The device code typically sets this to an empty array)
    
    Rather than risk losing/confusing the diff to the updated tests, future
    work will move these tests to the external WPTs
    
    Bug: 979318
    Change-Id: I846831a561217abd66ab841326a5e5472f38afeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699505
    Reviewed-by: Bill Orr <billorr@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#677061}
    
    --
    
    wpt-commits: 6b406c741ea44867d369c30eab1d910c5ededeab
    wpt-pr: 17810

--- testing/web-platform/tests/webxr/resources/webxr_test_constants.js
+++ testing/web-platform/tests/webxr/resources/webxr_test_constants.js
@@ -29,10 +29,26 @@ const VALID_PROJECTION_MATRIX =
     [1, 0, 0, 0, 0, 1, 0, 0, 3, 2, -1, -1, 0, 0, -0.2, 0];
 
 // A valid input grip matrix for  when we don't care about specific values
-const VALID_GRIP = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 4, 3, 2, 1];
+const VALID_GRIP = [1, 0, 0, 0,
+                    0, 1, 0, 0,
+                    0, 0, 1, 0,
+                    4, 3, 2, 1];
+
+const VALID_GRIP_TRANSFORM = {
+    position: [4, 3, 2],
+    orientation: [0, 0, 0, 1]
+};
 
 // A valid input pointer offset for  when we don't care about specific values
-const VALID_POINTER_OFFSET = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1];
+const VALID_POINTER_OFFSET = [1, 0, 0, 0,
+                              0, 1, 0, 0,
+                              0, 0, 1, 0,
+                              0, 0, 1, 1];
+
+const VALID_POINTER_TRANSFORM = {
+    position: [0, 0, 1],
+    orientation: [0, 0, 0, 1]
+};
 
 const VALID_GRIP_WITH_POINTER_OFFSET =
     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 4, 3, 3, 1];
@@ -105,3 +121,17 @@ const VALID_NON_IMMERSIVE_DEVICE = {
     views: NON_IMMERSIVE_VIEWS,
     viewerOrigin: IDENTITY_TRANSFORM
 };
+
+const VALID_CONTROLLER = {
+    handedness: "none",
+    targetRayMode: "tracked-pointer",
+    pointerOrigin: VALID_POINTER_TRANSFORM,
+    profiles: []
+};
+
+const RIGHT_CONTROLLER = {
+    handedness: "right",
+    targetRayMode: "tracked-pointer",
+    pointerOrigin: VALID_POINTER_TRANSFORM,
+    profiles: []
+}
