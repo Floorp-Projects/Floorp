9c74919340f9a9f0283bf71db279f63dff03b890
Boris Zbarsky <bzbarsky@mit.edu>
2019-07-22 14:52:04 +0000

    Bug 1566595.  Stop using [array] in nsIBinaryOutputStream.  r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D38387
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/test/unit/test_gzipped_206.js
+++ netwerk/test/unit/test_gzipped_206.js
@@ -118,7 +118,7 @@ function cachedHandler(metadata, response) {
   bos.setOutputStream(response.bodyOutputStream);
 
   response.processAsync();
-  bos.writeByteArray(body, body.length);
+  bos.writeByteArray(body);
   response.finish();
 }
 
