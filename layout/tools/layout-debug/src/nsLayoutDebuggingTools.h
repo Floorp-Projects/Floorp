75400edc999c636fd4918e13988088fdcf6f4f37
Cameron McCormack <cam@mcc.id.au>
2019-07-22 04:48:26 +0000

    Bug 1566215 - Part 1: Remove some unused nsLayoutDebuggingTools code. r=dbaron
    Differential Revision: https://phabricator.services.mozilla.com/D38411
    
    --HG--
    extra : moz-landing-system : lando

--- layout/tools/layout-debug/src/nsLayoutDebuggingTools.h
+++ layout/tools/layout-debug/src/nsLayoutDebuggingTools.h
@@ -23,12 +23,10 @@ class nsLayoutDebuggingTools : public nsILayoutDebuggingTools {
   virtual ~nsLayoutDebuggingTools();
 
   void ForceRefresh();
-  nsresult GetBoolPref(const char* aPrefName, bool* aValue);
   nsresult SetBoolPrefAndRefresh(const char* aPrefName, bool aNewValue);
 
   nsCOMPtr<nsIDocShell> mDocShell;
 
-  bool mEditorMode;
   bool mVisualDebugging;
   bool mVisualEventDebugging;
   bool mPaintFlashing;
