054c8361a6a08512bfb36bb605d10de6eda3c9f4
Matthew Gaudet <mgaudet@mozilla.com>
2019-07-23 14:33:56 +0000

    Bug 1563324 - Remove use of FunctionBox::function() in ParseContext::init() r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D36814

--- js/src/frontend/ParseContext.cpp
+++ js/src/frontend/ParseContext.cpp
@@ -261,15 +261,14 @@ bool ParseContext::init() {
     // binding is closed over when we finish parsing the function in
     // finishExtraFunctionScopes, the function box needs to be marked as
     // needing a dynamic DeclEnv object.
-    RootedFunction fun(cx, functionBox()->function());
-    if (fun->isNamedLambda()) {
+    if (functionBox()->isNamedLambda()) {
       if (!namedLambdaScope_->init(this)) {
         return false;
       }
       AddDeclaredNamePtr p =
-          namedLambdaScope_->lookupDeclaredNameForAdd(fun->explicitName());
+          namedLambdaScope_->lookupDeclaredNameForAdd(functionBox()->explicitName());
       MOZ_ASSERT(!p);
-      if (!namedLambdaScope_->addDeclaredName(this, p, fun->explicitName(),
+      if (!namedLambdaScope_->addDeclaredName(this, p, functionBox()->explicitName(),
                                               DeclarationKind::Const,
                                               DeclaredNameInfo::npos)) {
         return false;
