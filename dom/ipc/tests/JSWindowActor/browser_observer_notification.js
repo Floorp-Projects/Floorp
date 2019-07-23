2de0a249d306af65e4e7f7928e29cb79ae7d51ca
John Dai <jdai@mozilla.com>
2019-07-23 16:09:27 +0000

    Bug 1563597 - Support nsGlobalWindowOuter as observer notification's subject; r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D39029

--- dom/ipc/tests/JSWindowActor/browser_observer_notification.js
+++ dom/ipc/tests/JSWindowActor/browser_observer_notification.js
@@ -62,3 +62,26 @@ declTest("observers don't notify with wrong window", {
     });
   },
 });
+
+declTest("observers notify with audio-playback", {
+  url: "http://example.com/browser/dom/ipc/tests/JSWindowActor/file_mediaPlayback.html",
+
+  async test(browser) {
+    await ContentTask.spawn(browser, {}, async function() {
+      let audio = content.document.querySelector("audio");
+      audio.play();
+
+      let child = content.window.getWindowGlobalChild();
+      let actorChild = child.getActor("Test");
+      ok(actorChild, "JSWindowActorChild should have value.");
+
+      let observePromise = new Promise(resolve => {
+        actorChild.done = ({subject, topic, data}) => resolve({subject, topic, data});
+      });
+
+      let {subject, topic, data} = await observePromise;
+      is(topic, "audio-playback", "Topic matches");
+      is(data, "active", "Data matches");
+    });
+  },
+});
