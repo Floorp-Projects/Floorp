8dd1b602b4bbf014b6f409137571983b6088bd5b
Andrea Marchesini <amarchesini@mozilla.com>
2019-07-24 15:13:24 +0000

    Bug 1567892 - PeerConnectionMedia should use nsIProtocolProxyService on the parent process - part 1 - nsProtocolProxyService on parent process only, r=bwc,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D39009

--- netwerk/base/nsProtocolProxyService.cpp
+++ netwerk/base/nsProtocolProxyService.cpp
@@ -767,7 +767,9 @@ nsProtocolProxyService::nsProtocolProxyService()
       mSessionStart(PR_Now()),
       mFailedProxyTimeout(30 * 60)  // 30 minute default
       ,
-      mIsShutdown(false) {}
+      mIsShutdown(false) {
+  MOZ_ASSERT(XRE_IsParentProcess());
+}
 
 nsProtocolProxyService::~nsProtocolProxyService() {
   // These should have been cleaned up in our Observe method.
