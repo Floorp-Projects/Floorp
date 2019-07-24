6425e7499aac6867010ed5a45aa14a9033abe290
James Willcox <snorp@snorp.net>
2019-07-24 15:47:55 +0000

    Bug 1561079 - Add `referrerInfo` to <browser> r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D36525

--- toolkit/content/widgets/browser-custom-element.js
+++ toolkit/content/widgets/browser-custom-element.js
@@ -306,6 +306,8 @@
 
       this._csp = null;
 
+      this._referrerInfo = null;
+
       this._contentRequestContextID = null;
 
       this._fullZoom = 1;
@@ -801,6 +803,12 @@
       } else {
         this.markupDocumentViewer.fullZoom = val;
       }
+    }
+
+    get referrerInfo() {
+      return this.isRemoteBrowser
+        ? this._referrerInfo
+        : this.contentDocument.referrerInfo;
     }
 
     get fullZoom() {
@@ -1560,6 +1568,7 @@
       aContentPrincipal,
       aContentStoragePrincipal,
       aCSP,
+      aReferrerInfo,
       aIsSynthetic,
       aInnerWindowID,
       aHaveRequestContextID,
@@ -1584,6 +1593,7 @@
         this._contentPrincipal = aContentPrincipal;
         this._contentStoragePrincipal = aContentStoragePrincipal;
         this._csp = aCSP;
+        this._referrerInfo = aReferrerInfo;
         this._isSyntheticDocument = aIsSynthetic;
         this._innerWindowID = aInnerWindowID;
         this._contentRequestContextID = aHaveRequestContextID
