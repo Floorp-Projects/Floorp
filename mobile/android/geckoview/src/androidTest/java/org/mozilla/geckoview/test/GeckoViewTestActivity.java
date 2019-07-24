092a26a05d0d5ad4c9431ebd6000d3e414f1a013
James Willcox <snorp@snorp.net>
2019-07-24 14:53:19 +0000

    Bug 1510314 - Add some initial basic tests for the GeckoView class r=geckoview-reviewers,esawin
    Differential Revision: https://phabricator.services.mozilla.com/D36113

---
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/GeckoViewTestActivity.java
@@ -0,0 +1,23 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.geckoview.test;
+
+import org.mozilla.geckoview.GeckoView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GeckoViewTestActivity extends Activity {
+    public GeckoView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new GeckoView(this);
+        setContentView(view);
+    }
+}
