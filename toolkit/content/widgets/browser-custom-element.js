c230c12dae6b55c76ae076d01bb2eb0d160abc70
Bogdan Tara <btara@mozilla.com>
2019-07-24 18:31:15 +0300

    Backed out 2 changesets (bug 1561079) foe ES lint failure on browser-custom-element.js CLOSED TREE
    Backed out changeset 74cd5c0338ee (bug 1561079)
    Backed out changeset adbc4ea67da9 (bug 1561079)

--- toolkit/content/widgets/browser-custom-element.js
+++ toolkit/content/widgets/browser-custom-element.js
@@ -306,8 +306,6 @@
 
       this._csp = null;
 
-      this._referrerInfo = null;
-
       this._contentRequestContextID = null;
 
       this._fullZoom = 1;
@@ -802,22 +800,6 @@
         }
       } else {
         this.markupDocumentViewer.fullZoom = val;
-      }
-    }
-    
-    get referrerInfo() {
-      return this.isRemoteBrowser ? this._referrerInfo : this.contentDocument.referrerInfo;
-    }
-
-    get contentRequestContextID() {
-      if (this.isRemoteBrowser) {
-        return this._contentRequestContextID;
-      }
-      try {
-        return this.contentDocument.documentLoadGroup
-          .requestContextID;
-      } catch (e) {
-        return null;
       }
     }
 
@@ -1578,7 +1560,6 @@
       aContentPrincipal,
       aContentStoragePrincipal,
       aCSP,
-      aReferrerInfo,
       aIsSynthetic,
       aInnerWindowID,
       aHaveRequestContextID,
@@ -1603,7 +1584,6 @@
         this._contentPrincipal = aContentPrincipal;
         this._contentStoragePrincipal = aContentStoragePrincipal;
         this._csp = aCSP;
-        this._referrerInfo = aReferrerInfo;
         this._isSyntheticDocument = aIsSynthetic;
         this._innerWindowID = aInnerWindowID;
         this._contentRequestContextID = aHaveRequestContextID
