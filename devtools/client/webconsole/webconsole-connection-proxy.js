eda0d89ea5361459a18d6082d845fcd2f89cc06c
Alexandre Poirot <poirot.alex@gmail.com>
2019-07-25 06:43:52 +0000

    Bug 1566421 - Make WebConsoleUI.destroy synchronous. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D38460

--- devtools/client/webconsole/webconsole-connection-proxy.js
+++ devtools/client/webconsole/webconsole-connection-proxy.js
@@ -81,12 +81,6 @@ WebConsoleConnectionProxy.prototype = {
   connected: false,
 
   /**
-   * Tells if the console is attached.
-   * @type boolean
-   */
-  isAttached: null,
-
-  /**
    * Timer used for the connection.
    * @private
    * @type object
@@ -132,7 +126,7 @@ WebConsoleConnectionProxy.prototype = {
     if (this.target.isBrowsingContext) {
       this.webConsoleUI.onLocationChange(this.target.url, this.target.title);
     }
-    this.isAttached = this._attachConsole();
+    this._attachConsole();
 
     return connPromise;
   },
@@ -431,20 +425,9 @@ WebConsoleConnectionProxy.prototype = {
    * @return object
    *         A promise object that is resolved when disconnect completes.
    */
-  disconnect: async function() {
-    if (this._disconnecter) {
-      return this._disconnecter.promise;
-    }
-
-    this._disconnecter = defer();
-
-    // allow the console to finish attaching if it started.
-    if (this.isAttached) {
-      await this.isAttached;
-    }
+  disconnect: function() {
     if (!this.client) {
-      this._disconnecter.resolve(null);
-      return this._disconnecter.promise;
+      return;
     }
 
     this.webConsoleClient.off("logMessage", this._onLogMessage);
@@ -468,9 +451,6 @@ WebConsoleConnectionProxy.prototype = {
     this.target = null;
     this.connected = false;
     this.webConsoleUI = null;
-    this._disconnecter.resolve(null);
-
-    return this._disconnecter.promise;
   },
 };
 
