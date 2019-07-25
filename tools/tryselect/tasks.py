233b2d7fc7aa42b65147e360a9a6bb8db96e9de0
Edwin Gao <egao@mozilla.com>
2019-07-25 17:10:43 +0000

    Bug 1541424 - ensure path expansion in mach try respects manifest annotations r=ahal,marco
    Differential Revision: https://phabricator.services.mozilla.com/D38771
    
    --HG--
    extra : moz-landing-system : lando

--- tools/tryselect/tasks.py
+++ tools/tryselect/tasks.py
@@ -139,8 +139,15 @@ def resolve_tests_by_suite(paths):
     _, run_tests = resolver.resolve_metadata(paths)
 
     suite_to_tests = defaultdict(list)
+
     for test in run_tests:
         key, _ = get_suite_definition(test['flavor'], test.get('subsuite'), strict=True)
-        suite_to_tests[key].append(test['srcdir_relpath'])
+
+        # Passing test files will ignore mochitest manifest conditions (see bug 1541424).
+        if test.get('dir_relpath') in paths and test['dir_relpath'] not in suite_to_tests[key]:
+            suite_to_tests[key].append(test['dir_relpath'])
+        elif (test.get('srcdir_relpath') in paths and
+              test['srcdir_relpath'] not in suite_to_tests[key]):
+            suite_to_tests[key].append(test['srcdir_relpath'])
 
     return suite_to_tests
