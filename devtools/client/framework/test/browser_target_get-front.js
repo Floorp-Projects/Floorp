721438955c4d92166790f74dbfd2275dce55e5a1
Gabriel Luong <gabriel.luong@gmail.com>
2019-07-25 20:44:09 +0000

    Bug 1539764 -  Add a targetFront attribute to the Front class to retrieve the target front. r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D39130

--- devtools/client/framework/test/browser_target_get-front.js
+++ devtools/client/framework/test/browser_target_get-front.js
@@ -3,14 +3,22 @@
 
 add_task(async function() {
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser);
-  const tab = await addTab("about:blank");
 
+  const tab = await addTab("about:blank");
   const target = await TargetFactory.forTab(tab);
   await target.attach();
 
   const tab2 = await addTab("about:blank");
   const target2 = await TargetFactory.forTab(tab2);
   await target2.attach();
+
+  info("Test the targetFront attribute for the root");
+  const { client } = target;
+  is(
+    client.mainRoot.targetFront,
+    null,
+    "got null from the targetFront attribute for the root"
+  );
 
   info("Test getting a front twice");
   const getAccessibilityFront = await target.getFront("accessibility");
@@ -20,6 +28,16 @@ add_task(async function() {
     getAccessibilityFront2,
     "got the same front when calling getFront twice"
   );
+  is(
+    getAccessibilityFront.targetFront,
+    target,
+    "got the correct targetFront attribute from the front"
+  );
+  is(
+    getAccessibilityFront2.targetFront,
+    target,
+    "got the correct targetFront attribute from the front"
+  );
 
   info("Test getting a front on different targets");
   const target1Front = await target.getFront("accessibility");
@@ -28,6 +46,16 @@ add_task(async function() {
     target1Front !== target2Front,
     true,
     "got different fronts when calling getFront on different targets"
+  );
+  is(
+    target1Front.targetFront !== target2Front.targetFront,
+    true,
+    "got different targetFront from different fronts from different targets"
+  );
+  is(
+    target2Front.targetFront,
+    target2,
+    "got the correct targetFront attribute from the front"
   );
 
   info("Test async front retrieval");
