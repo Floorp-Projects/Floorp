04fcdb8729978214e0001b1b3a11a0cfbf543b58
Nupur Baghel <nupurbaghel@gmail.com>
2019-07-26 15:02:16 +0000

    Bug 1569100 - Marionette crate: move WebElement and Timeouts objects to common module. r=ato
    Both WebElement and Timeouts objects are used by the request and response stages.
    Hence moving them to the common module.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39511
    
    --HG--
    extra : moz-landing-system : lando

--- testing/geckodriver/marionette/src/common.rs
+++ testing/geckodriver/marionette/src/common.rs
@@ -10,3 +10,50 @@ impl BoolValue {
         BoolValue { value: val }
     }
 }
+
+// TODO(nupur): Bug 1567165 - Make WebElement in Marionette a unit struct
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct WebElement {
+    #[serde(rename = "element-6066-11e4-a52e-4f735466cecf")]
+    pub element: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Timeouts {
+    pub implicit: u64,
+    #[serde(rename = "pageLoad", alias = "page load")]
+    pub page_load: u64,
+    pub script: Option<u64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::{assert_de, assert_ser_de, ELEMENT_KEY};
+    use serde_json::json;
+
+    #[test]
+    fn test_web_element() {
+        let data = WebElement {
+            element: "foo".into(),
+        };
+        assert_ser_de(&data, json!({ELEMENT_KEY: "foo"}));
+    }
+
+    #[test]
+    fn test_timeouts() {
+        let data = Timeouts {
+            implicit: 1000,
+            page_load: 200000,
+            script: Some(60000),
+        };
+        assert_ser_de(
+            &data,
+            json!({"implicit":1000,"pageLoad":200000,"script":60000}),
+        );
+        assert_de(
+            &data,
+            json!({"implicit":1000,"page load":200000,"script":60000}),
+        );
+    }
+}
