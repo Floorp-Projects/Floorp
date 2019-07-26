c5f747644a9a9300c2d7186436781305f2fa864a
André Bargull <andre.bargull@gmail.com>
2019-07-26 07:44:16 +0000

    Bug 1568134: Add support for "fractionalSecondDigits" option to Intl.DateTimeFormat. r=jwalden
    Nightly-only for now until the open spec issues are addressed.
    
    Drive-by change:
    Implement the "unknown" parts change from <https://github.com/tc39/ecma402/pull/245>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38992
    
    --HG--
    extra : moz-landing-system : lando

---
+++ js/src/tests/non262/Intl/DateTimeFormat/fractional-second-digits.js
@@ -0,0 +1,106 @@
+// |reftest| skip-if(!this.hasOwnProperty("Intl")||release_or_beta)
+
+const tests = [
+  {
+      date: new Date("2019-01-01T00:00:00.123"),
+      digits: {
+          1: {
+              string: "1 (second: 0)",
+              parts: [
+                  {type: "fractionalSecondDigits", value: "1"},
+                  {type: "literal", value: " (second: "},
+                  {type: "second", value: "0"},
+                  {type: "literal", value: ")"},
+              ],
+          },
+          2: {
+              string: "0.12",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "12"},
+              ],
+          },
+          3: {
+              string: "0.123",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "123"},
+              ],
+          },
+      }
+  },
+  {
+      date: new Date("2019-01-01T00:00:00.023"),
+      digits: {
+          1: {
+              string: "0 (second: 0)",
+              parts: [
+                  {type: "fractionalSecondDigits", value: "0"},
+                  {type: "literal", value: " (second: "},
+                  {type: "second", value: "0"},
+                  {type: "literal", value: ")"},
+              ],
+          },
+          2: {
+              string: "0.02",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "02"},
+              ],
+          },
+          3: {
+              string: "0.023",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "023"},
+              ],
+          },
+      }
+  },
+  {
+      date: new Date("2019-01-01T00:00:00.003"),
+      digits: {
+          1: {
+              string: "0 (second: 0)",
+              parts: [
+                  {type: "fractionalSecondDigits", value: "0"},
+                  {type: "literal", value: " (second: "},
+                  {type: "second", value: "0"},
+                  {type: "literal", value: ")"},
+              ],
+          },
+          2: {
+              string: "0.00",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "00"},
+              ],
+          },
+          3: {
+              string: "0.003",
+              parts: [
+                  {type: "second", value: "0"},
+                  {type: "literal", value: "."},
+                  {type: "fractionalSecondDigits", value: "003"},
+              ],
+          },
+      }
+  },
+];
+
+for (let {date, digits} of tests) {
+    for (let [fractionalSecondDigits, {string, parts}] of Object.entries(digits)) {
+        let dtf = new Intl.DateTimeFormat("en", {second: "numeric", fractionalSecondDigits});
+
+        assertEq(dtf.format(date), string);
+        assertDeepEq(dtf.formatToParts(date), parts);
+    }
+}
+
+if (typeof reportCompare === "function")
+  reportCompare(0, 0, "ok");
