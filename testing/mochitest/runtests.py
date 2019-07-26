303825c94e8748b86b5dfb853e0340985b3f8ad1
Gurzau Raul <rgurzau@mozilla.com>
2019-07-26 22:35:27 +0300

    Backed out changeset 54629c24e7c3 (bug 1569235) for UnicodeEncodeError on a CLOSED TREE.

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -184,11 +184,6 @@ class MessageLogger(object):
                 'action'] in MessageLogger.VALID_ACTIONS):
             raise ValueError
 
-    def _fix_subtest_name(self, message):
-        """Make sure subtest name is a string"""
-        if 'subtest' in message:
-            message['subtest'] = str(message['subtest'])
-
     def _fix_test_name(self, message):
         """Normalize a logged test path to match the relative path from the sourcedir.
         """
@@ -237,7 +232,6 @@ class MessageLogger(object):
                         message=fragment,
                     )
 
-            self._fix_subtest_name(message)
             self._fix_test_name(message)
             self._fix_message_format(message)
             messages.append(message)
