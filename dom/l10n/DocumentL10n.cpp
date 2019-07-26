87980a08672a9d54a5b7b851331304a1abd2876c
Zibi Braniecki <zbraniecki@mozilla.com>
2019-07-26 20:17:21 +0000

    Bug 1517880 - Plug DOMLocalization and DocumentL10n into XULProtypeCache. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D38972
    
    --HG--
    extra : moz-landing-system : lando

--- dom/l10n/DocumentL10n.cpp
+++ dom/l10n/DocumentL10n.cpp
@@ -100,24 +100,119 @@ void DocumentL10n::TriggerInitialDocumentTranslation() {
 
   Element* elem = mDocument->GetDocumentElement();
   if (!elem) {
+    mReady->MaybeRejectWithUndefined();
+    InitialDocumentTranslationCompleted();
     return;
   }
 
-  Sequence<OwningNonNull<Element>> elements;
   ErrorResult rv;
 
+  // 1. Collect all localizable elements.
+  Sequence<OwningNonNull<Element>> elements;
   GetTranslatables(*elem, elements, rv);
-
-  ConnectRoot(*elem, rv);
-
-  RefPtr<Promise> promise = TranslateElements(elements, rv);
-  if (!promise) {
+  if (NS_WARN_IF(rv.Failed())) {
+    mReady->MaybeRejectWithUndefined();
+    InitialDocumentTranslationCompleted();
     return;
   }
 
-  RefPtr<PromiseNativeHandler> l10nReadyHandler =
-      new L10nReadyHandler(mReady, this);
-  promise->AppendNativeHandler(l10nReadyHandler);
+  RefPtr<nsXULPrototypeDocument> proto = mDocument->GetPrototype();
+
+  // 2. Check if the document has a prototype that may cache
+  //    translated elements.
+  if (proto) {
+    // 2.1. Handle the case when we have proto.
+
+    // 2.1.1. Move elements that are not in the proto to a separate
+    //        array.
+    Sequence<OwningNonNull<Element>> nonProtoElements;
+
+    uint32_t i = elements.Length();
+    while (i > 0) {
+      Element* elem = elements.ElementAt(i - 1);
+      MOZ_RELEASE_ASSERT(elem->HasAttr(nsGkAtoms::datal10nid));
+      if (!elem->HasElementCreatedFromPrototypeAndHasUnmodifiedL10n()) {
+        nonProtoElements.AppendElement(*elem, fallible);
+        elements.RemoveElement(elem);
+      }
+      i--;
+    }
+
+    // We populate the sequence in reverse order. Let's bring it
+    // back to top->bottom one.
+    nonProtoElements.Reverse();
+
+    nsTArray<RefPtr<Promise>> promises;
+
+    // 2.1.2. If we're not loading from cache, push the elements that
+    //        are in the prototype to be translated and cached.
+    if (!proto->WasL10nCached() && !elements.IsEmpty()) {
+      RefPtr<Promise> translatePromise = TranslateElements(elements, proto, rv);
+      if (NS_WARN_IF(!translatePromise || rv.Failed())) {
+        mReady->MaybeRejectWithUndefined();
+        InitialDocumentTranslationCompleted();
+        return;
+      }
+      promises.AppendElement(translatePromise);
+    }
+
+    // 2.1.3. If there are elements that are not in the prototype,
+    //        localize them without attempting to cache and
+    //        independently of if we're loading from cache.
+    if (!nonProtoElements.IsEmpty()) {
+      RefPtr<Promise> nonProtoTranslatePromise =
+          TranslateElements(nonProtoElements, nullptr, rv);
+      if (NS_WARN_IF(!nonProtoTranslatePromise || rv.Failed())) {
+        mReady->MaybeRejectWithUndefined();
+        InitialDocumentTranslationCompleted();
+        return;
+      }
+      promises.AppendElement(nonProtoTranslatePromise);
+    }
+
+    // 2.1.4. Check if anything has to be translated.
+    if (promises.IsEmpty()) {
+      // 2.1.4.1. If not, resolve the mReady and complete
+      //          initial translation.
+      mReady->MaybeResolveWithUndefined();
+      InitialDocumentTranslationCompleted();
+    } else {
+      // 2.1.4.2. If we have any TranslateElements promises,
+      //          collect them with Promise::All and schedule
+      //          the L10nReadyHandler.
+      AutoEntryScript aes(mGlobal,
+                          "DocumentL10n InitialDocumentTranslationCompleted");
+      RefPtr<Promise> promise = Promise::All(aes.cx(), promises, rv);
+      if (NS_WARN_IF(!promise || rv.Failed())) {
+        mReady->MaybeRejectWithUndefined();
+        InitialDocumentTranslationCompleted();
+        return;
+      }
+
+      RefPtr<PromiseNativeHandler> l10nReadyHandler =
+          new L10nReadyHandler(mReady, this);
+      promise->AppendNativeHandler(l10nReadyHandler);
+    }
+  } else {
+    // 2.2. Handle the case when we don't have proto.
+
+    // 2.2.1. Otherwise, translate all available elements,
+    //        without attempting to cache them and schedule
+    //        the L10nReadyHandler.
+    RefPtr<Promise> promise = TranslateElements(elements, nullptr, rv);
+    if (NS_WARN_IF(!promise || rv.Failed())) {
+      mReady->MaybeRejectWithUndefined();
+      InitialDocumentTranslationCompleted();
+      return;
+    }
+
+    RefPtr<PromiseNativeHandler> l10nReadyHandler =
+        new L10nReadyHandler(mReady, this);
+    promise->AppendNativeHandler(l10nReadyHandler);
+  }
+
+  // 3. Connect the root to L10nMutations observer.
+  ConnectRoot(*elem, rv);
 }
 
 void DocumentL10n::InitialDocumentTranslationCompleted() {
