80a654545afa1481ae1ff09af3ec3746b00167d3
Jason Laster <jlaster@mozilla.com>
2019-07-29 22:40:53 +0000

    Bug 1565578 - 'Step in' exits function. r=loganfsmyth
    Differential Revision: https://phabricator.services.mozilla.com/D38769

---
+++ devtools/server/tests/unit/test_stepping-11.js
@@ -0,0 +1,30 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+/* eslint-disable no-shadow */
+
+"use strict";
+
+/**
+ * Check basic stepping for console evaluations.
+ */
+
+add_task(
+  threadFrontTest(async ({ threadFront, targetFront, debuggee }) => {
+    dumpn("Evaluating test code and waiting for first debugger statement");
+
+    const consoleFront = await targetFront.getFront("console");
+    consoleFront.evaluateJSAsync(
+      `(function(){
+        debugger;
+        var a = 1;
+        var b = 2;
+      })();`
+    );
+
+    await waitForEvent(threadFront, "paused");
+    await threadFront.stepOver();
+    const packet = await waitForEvent(threadFront, "paused");
+    Assert.equal(packet.frame.where.line, 3, "step to line 3");
+    await threadFront.resume();
+  })
+);
