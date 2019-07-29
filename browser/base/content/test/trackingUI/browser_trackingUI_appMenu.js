eb47d7f225579389f9dabd1d333d497b0575e3d6
Erica Wright <ewright@mozilla.com>
2019-07-29 19:15:11 +0000

    Bug 1563547 - Update app menu to lead to the protection report. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D39549

--- browser/base/content/test/trackingUI/browser_trackingUI_appMenu.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_appMenu.js
@@ -8,31 +8,39 @@ ChromeUtils.import(
   this
 );
 
-// Test that the "Tracking Protection" button in the app menu loads about:preferences
-add_task(async function testPreferencesButton() {
+add_task(async function setup() {
+  // Hide protections cards so as not to trigger more async messaging
+  // when landing on the page.
+  SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.contentblocking.report.monitor.enabled", false],
+      ["browser.contentblocking.report.lockwise.enabled", false],
+      ["browser.contentblocking.report.proxy.enabled", false],
+    ],
+  });
+});
+
+// Test that the "Privacy Protections" button in the app menu loads about:protections
+add_task(async function testProtectionsButton() {
   let cuiTestUtils = new CustomizableUITestUtils(window);
 
   await BrowserTestUtils.withNewTab(gBrowser, async function(browser) {
     await cuiTestUtils.openMainMenu();
 
     let loaded = TestUtils.waitForCondition(
-      () => gBrowser.currentURI.spec == "about:preferences#privacy",
-      "Should open about:preferences."
+      () => gBrowser.currentURI.spec == "about:protections",
+      "Should open about:protections"
     );
-    document.getElementById("appMenu-tp-label").click();
+    document.getElementById("appMenu-protection-report-button").click();
     await loaded;
 
+    // When the graph is built it means any messaging has finished,
+    // we can close the tab.
     await ContentTask.spawn(browser, {}, async function() {
-      let doc = content.document;
-      let section = await ContentTaskUtils.waitForCondition(
-        () => doc.querySelector(".spotlight"),
-        "The spotlight should appear."
-      );
-      is(
-        section.getAttribute("data-subcategory"),
-        "trackingprotection",
-        "The trackingprotection section is spotlighted."
-      );
+      await ContentTaskUtils.waitForCondition(() => {
+        let bars = content.document.querySelectorAll(".graph-bar");
+        return bars.length;
+      }, "The graph has been built");
     });
   });
 });
