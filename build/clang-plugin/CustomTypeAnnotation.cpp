9cbf253a379ddda6985a0165e0bbc21cdc224a6f
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-07-29 22:53:13 +0300

    Backed out 3 changesets (bug 1569681) for causing build bustages. CLOSED TREE
    Backed out changeset 075b818a46fe (bug 1569681)
    Backed out changeset 2c543b239808 (bug 1569681)
    Backed out changeset 21ea6fea046e (bug 1569681)

--- build/clang-plugin/CustomTypeAnnotation.cpp
+++ build/clang-plugin/CustomTypeAnnotation.cpp
@@ -16,8 +16,6 @@ CustomTypeAnnotation NonTemporaryClass =
     CustomTypeAnnotation(moz_non_temporary_class, "non-temporary");
 CustomTypeAnnotation TemporaryClass =
     CustomTypeAnnotation(moz_temporary_class, "temporary");
-CustomTypeAnnotation StaticLocalClass =
-    CustomTypeAnnotation(moz_static_local_class, "static-local");
 
 void CustomTypeAnnotation::dumpAnnotationReason(BaseCheck &Check, QualType T,
                                                 SourceLocation Loc) {
