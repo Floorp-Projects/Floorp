2c01385ea0dc83b555f1c8d4b9a875b173a6e1b4
Junior Hsu <juhsu@mozilla.com>
2019-07-29 08:00:35 +0000

    Bug 1566295 - Do not send Origin: if we can't extract a valid origin r=ckerschb,tnguyen
    Sometimes we don't have origin like OCSP requests, or a fetch issues from web extension.
    We should not send `Origin:`
    
    Spec doesn't specify how to treat other protocol like moz-extension.
    IMO we should also prevent sending `Origin:` to keep web-compat.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39595

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -9745,10 +9745,12 @@ void nsHttpChannel::SetOriginHeader() {
   nsCOMPtr<nsIURI> referrer;
   mLoadInfo->TriggeringPrincipal()->GetURI(getter_AddRefs(referrer));
 
-  nsAutoCString origin("null");
-  if (referrer && dom::ReferrerInfo::IsReferrerSchemeAllowed(referrer)) {
-    nsContentUtils::GetASCIIOrigin(referrer, origin);
+  if (!referrer || !dom::ReferrerInfo::IsReferrerSchemeAllowed(referrer)) {
+    return;
   }
+
+  nsAutoCString origin("null");
+  nsContentUtils::GetASCIIOrigin(referrer, origin);
 
   // Restrict Origin to same-origin loads if requested by user or leaving from
   // .onion
@@ -9772,7 +9774,7 @@ void nsHttpChannel::SetOriginHeader() {
     }
   }
 
-  if (referrer && ReferrerInfo::ShouldSetNullOriginHeader(this, referrer)) {
+  if (ReferrerInfo::ShouldSetNullOriginHeader(this, referrer)) {
     origin.AssignLiteral("null");
   }
 
