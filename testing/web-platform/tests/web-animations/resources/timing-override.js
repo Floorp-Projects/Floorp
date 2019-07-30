593ac1d0e8a5416d196bb2e09565c0c853ab13f0
Tom Ritter <tom@mozilla.com>
2019-07-30 10:58:21 -0500

    Bug 1387894 - Followup: tabs to spaces for lint CLOSED TREE

--- testing/web-platform/tests/web-animations/resources/timing-override.js
+++ testing/web-platform/tests/web-animations/resources/timing-override.js
@@ -2,17 +2,17 @@
 // we hit some test failures because the Time Precision is too small. We override these functions
 // on a per-test basis for Firefox only.
 if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
-	window.assert_times_equal = (actual, expected, description) => {
-		let TIME_PRECISION = 0.02;
-		assert_approx_equals(actual, expected, TIME_PRECISION * 2, description);
-	};
+  window.assert_times_equal = (actual, expected, description) => {
+    let TIME_PRECISION = 0.02;
+    assert_approx_equals(actual, expected, TIME_PRECISION * 2, description);
+  };
 
-	window.assert_time_equals_literal = (actual, expected, description) => {
-		let TIME_PRECISION = 0.02;
-		if (Math.abs(expected) === Infinity) {
-		  assert_equals(actual, expected, description);
-		} else {
-		  assert_approx_equals(actual, expected, TIME_PRECISION, description);
-		}
-	}
+  window.assert_time_equals_literal = (actual, expected, description) => {
+    let TIME_PRECISION = 0.02;
+    if (Math.abs(expected) === Infinity) {
+      assert_equals(actual, expected, description);
+    } else {
+      assert_approx_equals(actual, expected, TIME_PRECISION, description);
+    }
+  }
 }
