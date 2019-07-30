f313e2913e301df286c42bc26da6aa6e8b5ab2f2
Jan de Mooij <jdemooij@mozilla.com>
2019-07-30 17:22:12 +0300

    Bug 1567438 part 13 - Modernize and simplify BaselineStackBuilder. r=tcampbell CLOSED TREE
    Instead of storing both buffer_ and header_ we now store just header_ (as UniquePtr).
    
    This also removes HeaderSize() and uses sizeof(BaselineBailoutInfo) directly.
    
    Differential Revision: https://phabricator.services.mozilla.com//D39873

--- js/src/jit/BaselineJIT.h
+++ js/src/jit/BaselineJIT.h
@@ -458,7 +458,7 @@ void ToggleBaselineProfiling(JSContext* cx, bool enable);
 void ToggleBaselineTraceLoggerScripts(JSRuntime* runtime, bool enable);
 void ToggleBaselineTraceLoggerEngine(JSRuntime* runtime, bool enable);
 
-struct BaselineBailoutInfo {
+struct alignas(uintptr_t) BaselineBailoutInfo {
   // Pointer into the current C stack, where overwriting will start.
   uint8_t* incomingStack = nullptr;
 
