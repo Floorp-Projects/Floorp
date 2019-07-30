86a92b3655b8f967d155d38be888e3f7ca4a3b25
Maja Frydrychowicz <mjzffr@gmail.com>
2019-07-30 16:55:19 +0000

    Bug 1569440 - [mozrunner] Empty the minidump directory instead of deleting it; r=gbrown
    Other harness code intermittently complains if the directory is absent. Maybe this will help.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39621

--- testing/mozbase/mozrunner/mozrunner/devices/base.py
+++ testing/mozbase/mozrunner/mozrunner/devices/base.py
@@ -78,6 +78,7 @@ class Device(object):
                 print("WARNING: {}".format(e))
         if os.listdir(local_dump_dir):
             self.device.rm(remote_dump_dir, recursive=True)
+            self.device.mkdir(remote_dump_dir)
         return local_dump_dir
 
     def setup_profile(self, profile):
