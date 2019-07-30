4342c142639c3c547db4684009f5b7220eef30cf
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-07-30 17:43:08 +0200

    [components] Use the GleanTestRule in bookmark-sync tests using the Glean SDK

--- mobile/android/android-components/components/browser/storage-sync/src/test/java/mozilla/components/browser/storage/sync/TestUtil.kt
+++ mobile/android/android-components/components/browser/storage-sync/src/test/java/mozilla/components/browser/storage/sync/TestUtil.kt
@@ -4,28 +4,6 @@
 
 package mozilla.components.browser.storage.sync
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
-import androidx.work.testing.WorkManagerTestInitHelper
-import mozilla.components.service.glean.Glean
 import java.util.Date
-
-/**
- * Enable testing mode, initialize [WorkManagerTestInitHelper] for testing,
- * and initialize Glean. This is used in tests that send Glean pings.
- *
- * @param context the application context to init Glean with
- */
-internal fun resetGlean(
-    context: Context = ApplicationProvider.getApplicationContext()
-) {
-    Glean.enableTestingMode()
-
-    // We're using the WorkManager in a bunch of places, and Glean will crash
-    // in tests without this line. Let's simply put it here.
-    WorkManagerTestInitHelper.initializeTestWorkManager(context)
-
-    Glean.initialize(context)
-}
 
 fun Date.asSeconds() = time / BaseGleanSyncPing.MILLIS_PER_SEC
