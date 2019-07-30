16eccedc989555634da39ac35cb420f709f8d21a
Timothy Nikkel <tnikkel@gmail.com>
2019-07-30 09:06:31 +0000

    Bug 1565039. In PicturePrimitive::take_context do calculation in float in case the unclipped rect is too big for int. r=gw a=RyanVM
    The final clipped result should hopefully not overflow.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37813

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -2250,7 +2250,7 @@ impl PicturePrimitive {
                             blur_std_deviation * scale_factors.1
                         );
                         let inflation_factor = frame_state.surfaces[raster_config.surface_index.0].inflation_factor;
-                        let inflation_factor = (inflation_factor * device_pixel_scale.0).ceil() as i32;
+                        let inflation_factor = (inflation_factor * device_pixel_scale.0).ceil();
 
                         // The clipped field is the part of the picture that is visible
                         // on screen. The unclipped field is the screen-space rect of
@@ -2260,10 +2260,20 @@ impl PicturePrimitive {
                         // blur results, inflate that clipped area by the blur range, and
                         // then intersect with the total screen rect, to minimize the
                         // allocation size.
-                        let mut device_rect = clipped
+                        // We cast clipped to f32 instead of casting unclipped to i32
+                        // because unclipped can overflow an i32.
+                        let device_rect = clipped.to_f32()
                             .inflate(inflation_factor, inflation_factor)
-                            .intersection(&unclipped.to_i32())
+                            .intersection(&unclipped)
                             .unwrap();
+
+                        let mut device_rect = match device_rect.try_cast::<i32>() {
+                            Some(rect) => rect,
+                            None => {
+                                return None
+                            }
+                        };
+
                         // Adjust the size to avoid introducing sampling errors during the down-scaling passes.
                         // what would be even better is to rasterize the picture at the down-scaled size
                         // directly.
@@ -2313,10 +2323,21 @@ impl PicturePrimitive {
                         }
 
                         max_std_deviation = max_std_deviation.round();
-                        let max_blur_range = (max_std_deviation * BLUR_SAMPLE_SCALE).ceil() as i32;
-                        let mut device_rect = clipped.inflate(max_blur_range, max_blur_range)
-                                .intersection(&unclipped.to_i32())
+                        let max_blur_range = (max_std_deviation * BLUR_SAMPLE_SCALE).ceil();
+                        // We cast clipped to f32 instead of casting unclipped to i32
+                        // because unclipped can overflow an i32.
+                        let device_rect = clipped.to_f32()
+                                .inflate(max_blur_range, max_blur_range)
+                                .intersection(&unclipped)
                                 .unwrap();
+
+                        let mut device_rect = match device_rect.try_cast::<i32>() {
+                            Some(rect) => rect,
+                            None => {
+                                return None
+                            }
+                        };
+
                         device_rect.size = RenderTask::adjusted_blur_source_size(
                             device_rect.size,
                             DeviceSize::new(max_std_deviation, max_std_deviation),
