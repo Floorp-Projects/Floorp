3bc99c2060bda267fbd73225e620198a80c58f0d
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2019-07-30 07:14:09 +0000

    Bug 1567877: Apply Meta CSP to about:devtools-toolbox. r=jdescottes,Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D39328

--- dom/xul/nsXULElement.cpp
+++ dom/xul/nsXULElement.cpp
@@ -78,6 +78,7 @@
 #include "XULPopupElement.h"
 #include "XULTreeElement.h"
 
+#include "mozilla/dom/nsCSPUtils.h"
 #include "mozilla/dom/XULElementBinding.h"
 #include "mozilla/dom/XULBroadcastManager.h"
 #include "mozilla/dom/MouseEventBinding.h"
@@ -678,6 +679,29 @@ nsresult nsXULElement::BindToTree(BindContext& aContext, nsINode& aParent) {
         "Unexpected XUL element in non-XUL doc");
   }
 #endif
+
+  // Within Bug 1492063 and its dependencies we started to apply a
+  // CSP to system privileged about pages. Since some about: pages
+  // are implemented in *.xul files we added this workaround to
+  // apply a CSP to them. To do so, we check the introduced custom
+  // attribute 'csp' on the root element.
+  if (doc.GetRootElement() == this) {
+    nsAutoString cspPolicyStr;
+    GetAttr(kNameSpaceID_None, nsGkAtoms::csp, cspPolicyStr);
+
+#ifdef DEBUG
+    {
+      nsCOMPtr<nsIContentSecurityPolicy> docCSP = doc.GetCsp();
+      uint32_t policyCount = 0;
+      if (docCSP) {
+        docCSP->GetPolicyCount(&policyCount);
+      }
+      MOZ_ASSERT(policyCount == 0, "how come we already have a policy?");
+    }
+#endif
+
+    CSP_ApplyMetaCSPToDoc(doc, cspPolicyStr);
+  }
 
   if (NodeInfo()->Equals(nsGkAtoms::keyset, kNameSpaceID_XUL)) {
     // Create our XUL key listener and hook it up.
