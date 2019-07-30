d5a633f2e92ab4a4f0479ef7e84d8482b9da8cdd
Mike Hommey <mh+mozilla@glandium.org>
2019-07-30 16:39:20 +0000

    Bug 1569887 - Use a fetch task artifact to get tup source. r=nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D39849
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/scripts/misc/build-tup-linux.sh
+++ taskcluster/scripts/misc/build-tup-linux.sh
@@ -3,8 +3,6 @@ set -e -v
 
 # This script is for building tup on Linux.
 
-TUP_REVISION=v0.7.8
-
 WORKSPACE=$HOME/workspace
 UPLOAD_DIR=$HOME/artifacts
 COMPRESS_EXT=xz
@@ -14,9 +12,24 @@ cd $WORKSPACE/build/src
 
 . taskcluster/scripts/misc/tooltool-download.sh
 
-git clone https://github.com/gittup/tup.git
-cd tup
-git checkout $TUP_REVISION
+cd $MOZ_FETCHES_DIR/tup
+
+patch -p1 <<'EOF'
+diff --git a/src/tup/link.sh b/src/tup/link.sh
+index ed9a01c6..4ecc6d7d 100755
+--- a/src/tup/link.sh
++++ b/src/tup/link.sh
+@@ -4,7 +4,7 @@
+ # linking, so that the version is updated whenever we change anything that
+ # affects the tup binary. This used to live in the Tupfile, but to support
+ # Windows local builds we need to make it an explicit shell script.
+-version=`git describe`
++version=unknown
+ CC=$1
+ CFLAGS=$2
+ LDFLAGS=$3
+EOF
+
 (echo 'CONFIG_TUP_SERVER=ldpreload'; echo 'CONFIG_TUP_USE_SYSTEM_PCRE=n') > tup.config
 ./bootstrap-ldpreload.sh
 cd ..
