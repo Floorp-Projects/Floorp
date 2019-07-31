d752441217b0406dc8eed17464ccd0d9b2f92f14
Gerald Squelart <gsquelart@mozilla.com>
2019-07-31 01:28:49 +0000

    Bug 1569506 - SamplerThread can own a Sampler instead of being one - r=canaltinova
    `SamplerThread` inheriting from `Sampler` was a bit confusing, and scary with no
    virtual destructor&functions.
    `SamplerThread` only uses `Sampler`'s `Disable()` and
    `SuspendAndSampleAndResumeThread()` functions, and `SamplerThread` is never
    accessed through a `Sampler` reference/pointer.
    So `SamplerThread` can just own a `Sampler` to make that relationship clearer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39640

--- mozglue/baseprofiler/core/platform-linux-android.cpp
+++ mozglue/baseprofiler/core/platform-linux-android.cpp
@@ -387,7 +387,7 @@ static void* ThreadEntry(void* aArg) {
 
 SamplerThread::SamplerThread(PSLockRef aLock, uint32_t aActivityGeneration,
                              double aIntervalMilliseconds)
-    : Sampler(aLock),
+    : mSampler(aLock),
       mActivityGeneration(aActivityGeneration),
       mIntervalMicroseconds(
           std::max(1, int(floor(aIntervalMilliseconds * 1000 + 0.5)))) {
@@ -451,7 +451,7 @@ void SamplerThread::Stop(PSLockRef aLock) {
   // though this SamplerThread is still alive, because the next time the main
   // loop of Run() iterates it won't get past the mActivityGeneration check,
   // and so won't send any signals.
-  Sampler::Disable(aLock);
+  mSampler.Disable(aLock);
 }
 
 // END SamplerThread target specifics
