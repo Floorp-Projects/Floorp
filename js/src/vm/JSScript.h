bfc4e3b9b54f6a99a18a4d038a7e34de517a271e
Matthew Gaudet <mgaudet@mozilla.com>
2019-07-31 19:27:57 +0000

    Bug 1567579 - Store inner FunctionBoxes instead of inner functions r=tcampbell
    This is a good preparation fo deferred function allocation (Bug 1569315),
    but also removes some awkward rooting / type issues in deferred lazyscript
    allocation
    
    Differential Revision: https://phabricator.services.mozilla.com/D39745
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/JSScript.h
+++ js/src/vm/JSScript.h
@@ -3268,14 +3268,13 @@ class LazyScript : public BaseScript {
 
   // Create a LazyScript and initialize closedOverBindings and innerFunctions
   // with the provided vectors.
-  static LazyScript* Create(JSContext* cx, HandleFunction fun,
-                            HandleScriptSourceObject sourceObject,
-                            const frontend::AtomVector& closedOverBindings,
-                            Handle<GCVector<JSFunction*, 8>> innerFunctions,
-                            uint32_t sourceStart, uint32_t sourceEnd,
-                            uint32_t toStringStart, uint32_t toStringEnd,
-                            uint32_t lineno, uint32_t column,
-                            frontend::ParseGoal parseGoal);
+  static LazyScript* Create(
+      JSContext* cx, HandleFunction fun, HandleScriptSourceObject sourceObject,
+      const frontend::AtomVector& closedOverBindings,
+      Vector<const js::frontend::FunctionBox*, 8>& innerFunctionBoxes,
+      uint32_t sourceStart, uint32_t sourceEnd, uint32_t toStringStart,
+      uint32_t toStringEnd, uint32_t lineno, uint32_t column,
+      frontend::ParseGoal parseGoal);
 
   // Create a LazyScript and initialize the closedOverBindings and the
   // innerFunctions with dummy values to be replaced in a later initialization
