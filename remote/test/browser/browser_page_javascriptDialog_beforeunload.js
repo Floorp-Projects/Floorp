e64c2ab14141899ee756da784bd29ef2e8d199c8
Julian Descottes <jdescottes@mozilla.com>
2019-08-01 12:25:57 +0000

    Bug 1545724 - Add support for javascriptDialog APIs in Page domain r=remote-protocol-reviewers,whimboo,ochameau
    Depends on D37168
    
    Differential Revision: https://phabricator.services.mozilla.com/D37368
    
    --HG--
    extra : moz-landing-system : lando

---
+++ remote/test/browser/browser_page_javascriptDialog_beforeunload.js
@@ -0,0 +1,63 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI = "data:text/html;charset=utf-8,default-test-page";
+
+// Test beforeunload dialog events.
+add_task(async function() {
+  info("Allow to trigger onbeforeunload without user interaction");
+  await new Promise(resolve => {
+    const options = {
+      set: [["dom.require_user_interaction_for_beforeunload", false]],
+    };
+    SpecialPowers.pushPrefEnv(options, resolve);
+  });
+
+  const { client, tab } = await setupTestForUri(TEST_URI);
+
+  const { Page } = client;
+
+  info("Enable the page domain");
+  await Page.enable();
+
+  info("Attach a valid onbeforeunload handler");
+  await ContentTask.spawn(gBrowser.selectedBrowser, null, () => {
+    content.onbeforeunload = () => true;
+  });
+
+  info("Trigger the beforeunload again but reject the prompt");
+  const { type } = await triggerBeforeUnload(Page, tab, false);
+  is(type, "beforeunload", "dialog event contains the correct type");
+
+  info("Trigger the beforeunload again and accept the prompt");
+  const onTabClose = BrowserTestUtils.waitForEvent(tab, "TabClose");
+  await triggerBeforeUnload(Page, tab, true);
+
+  info("Wait for the TabClose event");
+  await onTabClose;
+
+  await client.close();
+  ok(true, "The client is closed");
+
+  await RemoteAgent.close();
+});
+
+function triggerBeforeUnload(Page, tab, accept) {
+  // We use then here because after clicking on the close button, nothing
+  // in the main block of the function will be executed until the prompt
+  // is accepted or rejected. Attaching a then to this promise still works.
+
+  const onDialogOpen = Page.javascriptDialogOpening().then(
+    async dialogEvent => {
+      await Page.handleJavaScriptDialog({ accept });
+      return dialogEvent;
+    }
+  );
+
+  info("Click on the tab close icon");
+  tab.closeButton.click();
+
+  return onDialogOpen;
+}
---
+++ remote/test/browser/browser_page_javascriptDialog_confirm.js
@@ -0,0 +1,53 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI = "data:text/html;charset=utf-8,default-test-page";
+
+// Test for window.confirm(). Check that the dialog is correctly detected and that it can
+// be rejected or accepted.
+add_task(async function() {
+  const { client, tab } = await setupTestForUri(TEST_URI);
+
+  const { Page } = client;
+
+  info("Enable the page domain");
+  await Page.enable();
+
+  info("Create a confirm dialog to open");
+  const { message, type } = await createConfirmDialog(Page);
+
+  is(type, "confirm", "dialog event contains the correct type");
+  is(message, "confirm-1234?", "dialog event contains the correct text");
+
+  info("Accept the dialog");
+  await Page.handleJavaScriptDialog({ accept: true });
+  let isConfirmed = await getContentProperty("isConfirmed");
+  ok(isConfirmed, "The confirm dialog was accepted");
+
+  await createConfirmDialog(Page);
+  info("Trigger another confirm in the test page");
+
+  info("Reject the dialog");
+  await Page.handleJavaScriptDialog({ accept: false });
+  isConfirmed = await getContentProperty("isConfirmed");
+  ok(!isConfirmed, "The confirm dialog was rejected");
+
+  await client.close();
+  ok(true, "The client is closed");
+
+  BrowserTestUtils.removeTab(tab);
+  await RemoteAgent.close();
+});
+
+function createConfirmDialog(Page) {
+  const onDialogOpen = Page.javascriptDialogOpening();
+
+  info("Trigger a confirm in the test page");
+  ContentTask.spawn(gBrowser.selectedBrowser, null, () => {
+    content.isConfirmed = content.confirm("confirm-1234?");
+  });
+
+  return onDialogOpen;
+}
