c4f468de6a4cf623c2e21221df3b045178bb3a7e
Boris Chiou <boris.chiou@gmail.com>
2019-08-01 21:22:49 +0000

    Bug 1569795 - Block compositor animations of transform-like properties if offset-path is not none. r=hiro
    The animations of motion path are not running on the compositor, and the
    properties in [motion-1] is not part of transform-like properties (i.e.
    nsCSSProperties::TransformLikeProperties()) for now, so we should run
    transform animations on the main thread if offset-path is not `none`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39966
    
    --HG--
    extra : moz-landing-system : lando

--- dom/animation/KeyframeEffect.cpp
+++ dom/animation/KeyframeEffect.cpp
@@ -1881,6 +1881,13 @@ KeyframeEffect::MatchForCompositor KeyframeEffect::IsMatchForCompositor(
     }
   }
 
+  // Bug 1429305: Drop this after supporting compositor animations for motion
+  // path.
+  if (aPropertySet.Intersects(nsCSSPropertyIDSet::TransformLikeProperties()) &&
+      !aFrame->StyleDisplay()->mOffsetPath.IsNone()) {
+    return KeyframeEffect::MatchForCompositor::No;
+  }
+
   return mAnimation->IsPlaying() ? KeyframeEffect::MatchForCompositor::Yes
                                  : KeyframeEffect::MatchForCompositor::IfNeeded;
 }
