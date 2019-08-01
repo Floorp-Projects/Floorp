4aad3ed6d11c020910cede02cdbc98968b5a6108
Ehsan Akhgari <ehsan@mozilla.com>
2019-08-01 20:59:54 +0200

    Bug 1566836 - Respect the Content Blocking allow list for ETP interventions on all platforms; r=baku a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D40268
    
    --HG--
    extra : amend_source : f104f301b3f8b343a078d2ce849e058118a6773f

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1565,8 +1565,6 @@ pref("privacy.trackingprotection.fingerprinting.enabled", true);
 // Enable cryptomining blocking by default for all channels, only on desktop.
 pref("privacy.trackingprotection.cryptomining.enabled", true);
 
-pref("browser.contentblocking.allowlist.storage.enabled", true);
-
 pref("browser.contentblocking.database.enabled", true);
 
 pref("dom.storage_access.enabled", true);
