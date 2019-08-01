1f43ea0fb9ad5cf31d2cccf9e64ef57d54525ea4
Frédéric Wang <fwang@igalia.com>
2019-08-01 14:23:36 +0000

    Bug 1568941 [wpt PR 18086] - MathML: Avoid more false negative results for fraction reftests, a=testonly
    Automatic update from web-platform-tests
    MathML: Avoid more false negative results for fraction reftests (#18086)
    
    This also introduces improvements to MathMLFeatureDetection:
    * Introduce has_mfrac().
    * Split mathml source into several lines for better readability.
    * Add reference links to core spec.
    * Fix detection for lspace/rspace (measure the mrow container instead of mo).
    * Add helper function to check a feature for match reftest and otherwise
      force failure by displaying a red square at the top left corner.
    --
    
    wpt-commits: f1e6032e71849ee5ed11b5794406dd9ebd4bcdf6
    wpt-pr: 18086

--- testing/web-platform/tests/mathml/support/feature-detection.js
+++ testing/web-platform/tests/mathml/support/feature-detection.js
@@ -2,9 +2,14 @@
 // It is indented to be used to prevent false negative test results.
 
 var MathMLFeatureDetection = {
+
     has_mspace: function() {
+        // https://mathml-refresh.github.io/mathml-core/#space-mspace
         if (!this.hasOwnProperty("_has_mspace")) {
-            document.body.insertAdjacentHTML("beforeend", "<math><mspace></mspace><mspace width='20px'></mspace></math>");
+            document.body.insertAdjacentHTML("beforeend", "<math>\
+<mspace></mspace>\
+<mspace width='20px'></mspace>\
+</math>");
             var math = document.body.lastElementChild;
             // The width attribute will add 20px per MathML and none if not supported.
             this._has_mspace =
@@ -16,16 +21,62 @@ var MathMLFeatureDetection = {
     },
 
     has_operator_spacing: function() {
+        // https://mathml-refresh.github.io/mathml-core/#dfn-lspace
+        // https://mathml-refresh.github.io/mathml-core/#layout-of-mrow
         if (!this.hasOwnProperty("_has_operator_spacing")) {
-            document.body.insertAdjacentHTML("beforeend", "<math><mrow><mn>1</mn><mo lspace='0px' rspace='0px'>+</mo><mn>2</mn><mo lspace='8px' rspace='8px'>+</mo><mn>3</mn></mspace></mrow></math>");
+            document.body.insertAdjacentHTML("beforeend", "<math>\
+<mrow>\
+  <mn>1</mn><mo lspace='0px' rspace='0px'>+</mo><mn>2</mn>\
+</mrow>\
+<mrow>\
+  <mn>1</mn><mo lspace='8px' rspace='8px'>+</mo><mn>2</mn>\
+</mrow>\
+</math>");
             var math = document.body.lastElementChild;
-            var mo = math.getElementsByTagName("mo");
+            var mrow = math.getElementsByTagName("mrow");
             // lspace/rspace will add 16px per MathML and none if not supported.
             this._has_operator_spacing =
-                mo[1].getBoundingClientRect().width -
-                mo[0].getBoundingClientRect().width > 10;
+                mrow[1].getBoundingClientRect().width -
+                mrow[0].getBoundingClientRect().width > 10;
             document.body.removeChild(math);
         }
         return this._has_operator_spacing;
+    },
+
+    has_mfrac: function() {
+        if (!this.hasOwnProperty("_has_mfrac")) {
+            // Use tall enough fraction to avoid side effect of min num/denum shifts.
+            document.body.insertAdjacentHTML("beforeend", "<math>\
+<mfrac>\
+  <mspace height='50px' depth='50px'></mspace>\
+  <mspace height='50px' depth='50px'></mspace>\
+</mfrac>\
+<mfrac>\
+  <mspace height='60px' depth='60px'></mspace>\
+  <mspace height='60px' depth='60px'></mspace>\
+</mfrac>\
+</math>");
+            var math = document.body.lastElementChild;
+            var mfrac = math.getElementsByTagName("mfrac");
+            // height/depth will add 40px per MathML, 20px if mfrac does not stack its children and none if mspace is not supported.
+            this._has_mfrac =
+                mfrac[1].getBoundingClientRect().height -
+                mfrac[0].getBoundingClientRect().height > 30;
+            document.body.removeChild(math);
+        }
+        return this._has_mfrac;
+    },
+
+    ensure_for_match_reftest: function(has_function) {
+        if (!document.querySelector("link[rel='match']"))
+            throw "This function must only be used for match reftest";
+        // Add a little red square at the top left corner if the feature is not supported in order to make match reftest fail.
+        if (!this[has_function]()) {
+            document.body.insertAdjacentHTML("beforeend", "\
+<div style='width: 10px !important; height: 10px !important;\
+            position: absolute !important;\
+            left: 0 !important; top: 0 !important;\
+            background: red !important; z-index: 1000 !important;'></div>");
+        }
     }
 };
