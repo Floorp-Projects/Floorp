884fa8a6ff036b3e70ec757dae06b521675338ae
Timothy Nikkel <tnikkel@gmail.com>
2019-08-01 02:03:19 +0000

    Bug 1569215. In StackingContextHelper constructor pass parent scale to ChooseScale. r=jrmuizel a=RyanVM
    This might seem like we are including the parent scale twice because it is also included in mInheritedTransform but FrameLayerBuilder::ChooseScale only incorporates the passed in scale when combining it with a scale computed purely based on the local transform induced by this stacking context item.
    
    This also fixes bug 1564698 and doesn't regress bug 1495163 (the only testcase I can still find for the regressing bug, bug 1415987).
    
    Differential Revision: https://phabricator.services.mozilla.com/D39867
    
    --HG--
    extra : source : 9ec20458d0b29827a8715f3cbe7990ccc6516d57

--- gfx/layers/wr/StackingContextHelper.cpp
+++ gfx/layers/wr/StackingContextHelper.cpp
@@ -44,9 +44,10 @@ StackingContextHelper::StackingContextHelper(
 
     int32_t apd = aContainerFrame->PresContext()->AppUnitsPerDevPixel();
     nsRect r = LayoutDevicePixel::ToAppUnits(aBounds, apd);
-    mScale = FrameLayerBuilder::ChooseScale(aContainerFrame, aContainerItem, r,
-                                            1.f, 1.f, mInheritedTransform,
-                                            /* aCanDraw2D = */ true);
+    mScale = FrameLayerBuilder::ChooseScale(
+        aContainerFrame, aContainerItem, r, aParentSC.mScale.width,
+        aParentSC.mScale.height, mInheritedTransform,
+        /* aCanDraw2D = */ true);
 
     if (aParams.mAnimated) {
       mSnappingSurfaceTransform =
