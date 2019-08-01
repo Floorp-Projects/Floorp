74613c514b4d627039a9365edc2d74e032164322
Frédéric Wang <fwang@igalia.com>
2019-08-01 14:22:22 +0000

    Bug 1568802 [wpt PR 18081] - MathML: Add tests for embellished operators, a=testonly
    Automatic update from web-platform-tests
    MathML: Add tests for embellished operators (#18081)
    
    See https://mathml-refresh.github.io/mathml-core/#embellished-operators
    These tests assume that operator spacing is supported, so a feature detection
    is added to prevent false negative results. This new detection is also used
    for space-like tests which rely on the same assumption.
    
    Minor tweak for mspace detection: compare `<mspace width="20px">` against
    `<mspace>`, just in case the latter is nonzero in some browsers or tests.
    --
    
    wpt-commits: 7e9bfd839d6b19555d947447ae996f11ac86facc
    wpt-pr: 18081

--- testing/web-platform/tests/mathml/support/feature-detection.js
+++ testing/web-platform/tests/mathml/support/feature-detection.js
@@ -4,12 +4,28 @@
 var MathMLFeatureDetection = {
     has_mspace: function() {
         if (!this.hasOwnProperty("_has_mspace")) {
-            document.body.insertAdjacentHTML("beforeend", "<math><mspace width='20px'></mspace></math>");
+            document.body.insertAdjacentHTML("beforeend", "<math><mspace></mspace><mspace width='20px'></mspace></math>");
             var math = document.body.lastElementChild;
+            // The width attribute will add 20px per MathML and none if not supported.
             this._has_mspace =
+                math.lastChild.getBoundingClientRect().width -
                 math.firstChild.getBoundingClientRect().width > 10;
             document.body.removeChild(math);
         }
         return this._has_mspace;
+    },
+
+    has_operator_spacing: function() {
+        if (!this.hasOwnProperty("_has_operator_spacing")) {
+            document.body.insertAdjacentHTML("beforeend", "<math><mrow><mn>1</mn><mo lspace='0px' rspace='0px'>+</mo><mn>2</mn><mo lspace='8px' rspace='8px'>+</mo><mn>3</mn></mspace></mrow></math>");
+            var math = document.body.lastElementChild;
+            var mo = math.getElementsByTagName("mo");
+            // lspace/rspace will add 16px per MathML and none if not supported.
+            this._has_operator_spacing =
+                mo[1].getBoundingClientRect().width -
+                mo[0].getBoundingClientRect().width > 10;
+            document.body.removeChild(math);
+        }
+        return this._has_operator_spacing;
     }
 };
