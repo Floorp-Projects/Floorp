0ac043bdbabe1bb98ca74a066e33299e8658c7f7
Boris Chiou <boris.chiou@gmail.com>
2019-08-01 21:22:49 +0000

    Bug 1569795 - Block compositor animations of transform-like properties if offset-path is not none. r=hiro
    The animations of motion path are not running on the compositor, and the
    properties in [motion-1] is not part of transform-like properties (i.e.
    nsCSSProperties::TransformLikeProperties()) for now, so we should run
    transform animations on the main thread if offset-path is not `none`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39966

--- layout/style/test/animation_utils.js
+++ layout/style/test/animation_utils.js
@@ -506,10 +506,17 @@ const ExpectComparisonTo = {
     var compare;
     var normalizedToString = JSON.stringify;
     switch (property) {
-      case "transform":
       case "translate":
       case "rotate":
       case "scale":
+        if (runningOn == RunningOn.MainThread) {
+          normalize = value => value;
+          compare = function(a, b, error) {
+            return a == b;
+          };
+          break;
+        }
+      case "transform":
         normalize = convertTo3dMatrix;
         compare = matricesRoughlyEqual;
         normalizedToString = convert3dMatrixToString;
