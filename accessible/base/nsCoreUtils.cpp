847e9229459a3884aeb2c28128b5f03fb30cf3bd
Eitan Isaacson <eitan@monotonous.org>
2019-08-01 16:19:06 +0000

    Bug 686400 - Add function to nsCoreUtils for display: contents. r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D40131

--- accessible/base/nsCoreUtils.cpp
+++ accessible/base/nsCoreUtils.cpp
@@ -545,3 +545,8 @@ void nsCoreUtils::DispatchAccEvent(RefPtr<nsIAccessibleEvent> event) {
 
   obsService->NotifyObservers(event, NS_ACCESSIBLE_EVENT_TOPIC, nullptr);
 }
+
+bool nsCoreUtils::IsDisplayContents(nsIContent* aContent) {
+  return aContent && aContent->IsElement() &&
+         aContent->AsElement()->IsDisplayContents();
+}
