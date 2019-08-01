e120b9af2d60559bf21de35944c1cfc93be6b932
Tim Nguyen <ntim.bugs@gmail.com>
2019-08-01 03:09:00 +0000

    Bug 1569723 - Show a 'Discard' dialog when canceling or leaving the edit/create mode in about:logins. r=fluent-reviewers,jaws,flod
    Differential Revision: https://phabricator.services.mozilla.com/D39984

--- browser/components/aboutlogins/tests/chrome/aboutlogins_common.js
+++ browser/components/aboutlogins/tests/chrome/aboutlogins_common.js
@@ -59,5 +59,12 @@ Object.defineProperty(window, "AboutLoginsUtils", {
     promptForMasterPassword(resolve) {
       resolve(true);
     },
+    doLoginsMatch(login1, login2) {
+      return (
+        login1.origin == login2.origin &&
+        login1.username == login2.username &&
+        login1.password == login2.password
+      );
+    },
   },
 });
