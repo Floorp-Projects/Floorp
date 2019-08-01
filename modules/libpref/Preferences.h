cb9400e186f624b427fa840b34ba31109c7831a8
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-01 02:11:07 +0000

    Bug 1567074 - Document libpref properly. r=jya
    This is a combination of the All About Prefs google doc and kmag's description
    of the shared memory work in
    https://bugzilla.mozilla.org/show_bug.cgi?id=1471025#c95, with some updates and
    tweaks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38611
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/Preferences.h
+++ modules/libpref/Preferences.h
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Documentation for libpref is in modules/libpref/docs/index.rst.
+
 #ifndef mozilla_Preferences_h
 #define mozilla_Preferences_h
 
