461fafe92765805859e5290bdf63c7334851132e
Mike Hommey <mh+mozilla@glandium.org>
2019-08-02 10:50:48 +0900

    Bug 1570598 - Reduce the number of scripts used to build clang toolchains. r=froydnj
    They are now all very similar, and there are only a few variations.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40365
    
    MANUAL PUSH: avoid closing autoland while toolchains are rebuilt.

--- taskcluster/scripts/misc/build-clang-tidy64-windows.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-source $GECKO_PATH/taskcluster/scripts/misc/build-clang-windows-helper64.sh $1
---
+++ taskcluster/scripts/misc/build-clang.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -x -e -v
+
+# This script is for building clang.
+
+ORIGPWD="$PWD"
+JSON_CONFIG="$1"
+
+cd $GECKO_PATH
+
+. taskcluster/scripts/misc/tooltool-download.sh
+
+if [ -d "$GECKO_PATH/binutils/bin" ]; then
+  export PATH="$GECKO_PATH/binutils/bin:$PATH"
+fi
+
+case "$JSON_CONFIG" in
+*macosx64*)
+  # these variables are used in build-clang.py
+  export CROSS_CCTOOLS_PATH=$GECKO_PATH/cctools
+  export CROSS_SYSROOT=$GECKO_PATH/MacOSX10.11.sdk
+  export PATH=$PATH:$CROSS_CCTOOLS_PATH/bin
+  ;;
+*win64*)
+  UPLOAD_DIR=$ORIGPWD/public/build
+  # Set up all the Visual Studio paths.
+  . taskcluster/scripts/misc/vs-setup.sh
+
+  # LLVM_ENABLE_DIA_SDK is set if the directory "$ENV{VSINSTALLDIR}DIA SDK"
+  # exists.
+  export VSINSTALLDIR="${VSPATH}/"
+
+  # Add git.exe to the path
+  export PATH="$(pwd)/cmd:${PATH}"
+  export PATH="$(cd cmake && pwd)/bin:${PATH}"
+  export PATH="$(cd ninja && pwd)/bin:${PATH}"
+  ;;
+*linux64*|*android*)
+  ;;
+*)
+  echo Cannot figure out build configuration for $JSON_CONFIG
+  exit 1
+  ;;
+esac
+
+# gets a bit too verbose here
+set +x
+
+python3 build/build-clang/build-clang.py -c $1
+
+set -x
+
+# Put a tarball in the artifacts dir
+mkdir -p $UPLOAD_DIR
+cp clang*.tar.* $UPLOAD_DIR
