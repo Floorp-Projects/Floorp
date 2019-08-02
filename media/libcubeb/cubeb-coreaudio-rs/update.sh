9ddf32719bfa658aeda625041f11f168a3cdc4f9
Chun-Min Chang <chun.m.chang@gmail.com>
2019-08-02 18:29:25 +0000

    Bug 1570446 - P1: Update script importing cubeb-coreaudio-rs to gecko. r=padenot
    - Don't import the build settings since the settings in gecko is more
    complicated.
    - Print out the picked commits when running the scripts
    
    Differential Revision: https://phabricator.services.mozilla.com/D40097

--- media/libcubeb/cubeb-coreaudio-rs/update.sh
+++ media/libcubeb/cubeb-coreaudio-rs/update.sh
@@ -3,15 +3,20 @@ set -e
 
 cp -p $1/LICENSE .
 cp -p $1/Cargo.toml .
-cp -r $1/coreaudio-sys-utils .
 test -d src || mkdir -p src
 # Copy all the files under src folder, except tests.
 rsync -av --progress $1/src/ src/ --exclude backend/tests
+# Separate build settings of coreaudio-sys-utils in gecko for now (Bug 1569003).
+rsync -av --progress $1/coreaudio-sys-utils . --exclude Cargo.toml
 
 if [ -d $1/.git ]; then
   rev=$(cd $1 && git rev-parse --verify HEAD)
   date=$(cd $1 && git show -s --format=%ci HEAD)
   dirty=$(cd $1 && git diff-index --name-only HEAD)
+  set +e
+  pre_rev=$(grep -o '[[:xdigit:]]\{40\}' README_MOZILLA)
+  commits=$(cd $1 && git log --pretty=format:'%h - %s' $pre_rev..$rev)
+  set -e
 fi
 
 if [ -n "$rev" ]; then
@@ -23,6 +28,7 @@ if [ -n "$rev" ]; then
   echo "$version ($date)"
   sed -i.bak -e "/The git commit ID used was/ s/[0-9a-f]\{40\}\(-dirty\)\{0,1\} .\{1,100\}/$version ($date)/" README_MOZILLA
   rm README_MOZILLA.bak
+  [[ -n "$commits" ]] && echo -e "Pick commits:\n$commits"
 else
   echo "Remember to update README_MOZILLA with the version details."
 fi
