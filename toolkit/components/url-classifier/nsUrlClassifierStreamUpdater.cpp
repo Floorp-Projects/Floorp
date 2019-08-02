c16799f4af25fad5fd4e994de34d8a2f3959f9b4
Tom Schuster <evilpies@gmail.com>
2019-08-02 08:54:18 +0000

    Bug 1558915 - Use infallible nsIURI::SchemeIs in toolkit/ r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D40323
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
@@ -178,8 +178,7 @@ nsresult nsUrlClassifierStreamUpdater::FetchUpdate(
   // purposes.
   // This is only used for testing and should be deleted.
   bool match;
-  if ((NS_SUCCEEDED(aUpdateUrl->SchemeIs("file", &match)) && match) ||
-      (NS_SUCCEEDED(aUpdateUrl->SchemeIs("data", &match)) && match)) {
+  if (aUpdateUrl->SchemeIs("file") || aUpdateUrl->SchemeIs("data")) {
     mChannel->SetContentType(
         NS_LITERAL_CSTRING("application/vnd.google.safebrowsing-update"));
   } else {
--- toolkit/mozapps/extensions/AddonManagerWebAPI.cpp
+++ toolkit/mozapps/extensions/AddonManagerWebAPI.cpp
@@ -55,9 +55,7 @@ bool AddonManagerWebAPI::IsValidSite(nsIURI* uri) {
     return false;
   }
 
-  bool isSecure;
-  nsresult rv = uri->SchemeIs("https", &isSecure);
-  if (NS_FAILED(rv) || !isSecure) {
+  if (!uri->SchemeIs("https")) {
     if (!(xpc::IsInAutomation() &&
           Preferences::GetBool("extensions.webapi.testing.http", false))) {
       return false;
