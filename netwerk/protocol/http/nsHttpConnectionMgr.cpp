f7952a841d8f46a018e42ff011d180100ec964e3
Honza Bambas <honzab.moz@firemni.cz>
2019-08-02 11:04:20 +0000

    Bug 1565242 - Remove HTTP/2 connection (e2e or proxy) from the opposite (an)onymous connection entry in case it cannot be found in the entry the connection is associated with because of HTTP/2 (an)onymous connection pool sharing, r=kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D37881

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -625,6 +625,8 @@ nsresult nsHttpConnectionMgr::UpdateRequestTokenBucket(
 nsresult nsHttpConnectionMgr::ClearConnectionHistory() {
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
 
+  LOG(("nsHttpConnectionMgr::ClearConnectionHistory"));
+
   for (auto iter = mCT.Iter(); !iter.Done(); iter.Next()) {
     RefPtr<nsConnectionEntry> ent = iter.Data();
     if (ent->mIdleConns.Length() == 0 && ent->mActiveConns.Length() == 0 &&
@@ -2792,6 +2794,7 @@ void nsHttpConnectionMgr::OnMsgReclaimConnection(int32_t, ARefBase* param) {
                                ? mCT.GetWeak(conn->ConnectionInfo()->HashKey())
                                : nullptr;
 
+  DebugOnly<bool> newEntry = false;
   if (!ent) {
     // this can happen if the connection is made outside of the
     // connection manager and is being "reclaimed" for use with
@@ -2801,6 +2804,8 @@ void nsHttpConnectionMgr::OnMsgReclaimConnection(int32_t, ARefBase* param) {
         ("nsHttpConnectionMgr::OnMsgReclaimConnection conn %p "
          "forced new hash entry %s\n",
          conn, conn->ConnectionInfo()->HashKey().get()));
+
+    newEntry = true;
   }
 
   MOZ_ASSERT(ent);
@@ -2834,6 +2839,31 @@ void nsHttpConnectionMgr::OnMsgReclaimConnection(int32_t, ARefBase* param) {
   if (ent->mActiveConns.RemoveElement(conn)) {
     DecrementActiveConnCount(conn);
     ConditionallyStopTimeoutTick();
+  } else if (conn->EverUsedSpdy()) {
+    LOG(("nsHttpConnection %p not found in its connection entry, try ^anon",
+         conn));
+    // repeat for flipped anon flag as we share connection entries for spdy
+    // connections.
+    RefPtr<nsHttpConnectionInfo> anonInvertedCI(ci->Clone());
+    anonInvertedCI->SetAnonymous(!ci->GetAnonymous());
+
+    nsConnectionEntry* ent = mCT.GetWeak(anonInvertedCI->HashKey());
+    if (ent && ent->mActiveConns.RemoveElement(conn)) {
+      DecrementActiveConnCount(conn);
+      ConditionallyStopTimeoutTick();
+    } else {
+      LOG(
+          ("nsHttpConnection %p could not be removed from its entry's active "
+           "list",
+           conn));
+
+      // nsHttpConnectionMgr::AbortAndCloseAllConnections may race with this
+      // event handling.  It wipes and throws away all conn entries.  Then it's
+      // obviously fine that we can't find this connection anywhere.
+      MOZ_ASSERT(newEntry,
+                 "Active connection not found in a pre-existing entry nor "
+                 "^anonymous entry");
+    }
   }
 
   if (conn->CanReuse()) {
