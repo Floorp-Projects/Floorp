a2b73b8abc11545fdd058daa9a98e4f095dc0c31
Aaron Klotz <aklotz@mozilla.com>
2019-08-02 17:26:37 +0000

    Bug 1570395: Add StaticLocalAutoPtr and StaticLocalRefPtr to XPCOM; r=froydnj
    This patch adds smart pointers to be used for initializing objects as C++11
    "magic statics" -- that is, they are able to take advantage of C++11's
    guarantee of thread safety during initialization by atomically constructing
    both the smart pointer itself as well as the object being pointed to.
    
    Unlike Static{Auto,Ref}Ptr, they have non-trivial constructors, though they
    must still have trivial destructors to prevent emission of atexit calls.
    
    The new classes use the new `MOZ_STATIC_LOCAL_CLASS` annotation which ensures
    their instantiations are static locals and prevents non-trivial destructors.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40092
    
    --HG--
    rename : xpcom/base/StaticPtr.h => xpcom/base/StaticLocalPtr.h
    extra : moz-landing-system : lando

--- mfbt/RefPtr.h
+++ mfbt/RefPtr.h
@@ -24,6 +24,8 @@ namespace mozilla {
 template <class T>
 class OwningNonNull;
 template <class T>
+class StaticLocalRefPtr;
+template <class T>
 class StaticRefPtr;
 #if defined(XP_WIN)
 namespace mscom {
@@ -146,6 +148,10 @@ class MOZ_IS_REFPTR RefPtr {
   template <class U>
   MOZ_IMPLICIT RefPtr(const mozilla::OwningNonNull<U>& aOther);
 
+  // Defined in StaticLocalPtr.h
+  template <class U>
+  MOZ_IMPLICIT RefPtr(const mozilla::StaticLocalRefPtr<U>& aOther);
+
   // Defined in StaticPtr.h
   template <class U>
   MOZ_IMPLICIT RefPtr(const mozilla::StaticRefPtr<U>& aOther);
@@ -209,6 +215,10 @@ class MOZ_IS_REFPTR RefPtr {
   // Defined in OwningNonNull.h
   template <class U>
   RefPtr<T>& operator=(const mozilla::OwningNonNull<U>& aOther);
+
+  // Defined in StaticLocalPtr.h
+  template <class U>
+  RefPtr<T>& operator=(const mozilla::StaticLocalRefPtr<U>& aOther);
 
   // Defined in StaticPtr.h
   template <class U>
