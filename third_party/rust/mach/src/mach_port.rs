dc035152ff5e1bb99ff05c727e4dbaf99a8b27c4
Paul Adenot <paul@paul.cx>
2019-08-02 12:49:45 +0000

    Bug 1570948 - mach vendor rust. r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D40411

--- third_party/rust/mach/src/mach_port.rs
+++ third_party/rust/mach/src/mach_port.rs
@@ -1,22 +1,29 @@
 //! This module corresponds to `mach/mach_port.h`
 
-use kern_return::{kern_return_t};
-use message::{mach_msg_type_name_t};
+use kern_return::kern_return_t;
+use mach_types::ipc_space_t;
+use message::mach_msg_type_name_t;
 use port::{mach_port_name_t, mach_port_right_t, mach_port_t};
-use types::{ipc_space_t};
 
 extern "C" {
-    pub fn mach_port_allocate(task: ipc_space_t, right: mach_port_right_t,
-                              name: *mut mach_port_name_t) -> kern_return_t;
-    pub fn mach_port_destroy(task: ipc_space_t,
-                             name: mach_port_name_t) -> kern_return_t;
-    pub fn mach_port_deallocate(task: ipc_space_t,
-                                name: mach_port_name_t) -> kern_return_t;
-    pub fn mach_port_insert_right(task: ipc_space_t, name: mach_port_name_t,
-                                  poly: mach_port_t,
-                                  polyPoly: mach_msg_type_name_t) -> kern_return_t;
-    pub fn mach_port_extract_right(task: ipc_space_t, name: mach_port_name_t,
-                                   msgt_name: mach_msg_type_name_t,
-                                   poly: *mut mach_port_t,
-                                   polyPoly: *mut mach_msg_type_name_t) -> kern_return_t;
+    pub fn mach_port_allocate(
+        task: ipc_space_t,
+        right: mach_port_right_t,
+        name: *mut mach_port_name_t,
+    ) -> kern_return_t;
+    pub fn mach_port_destroy(task: ipc_space_t, name: mach_port_name_t) -> kern_return_t;
+    pub fn mach_port_deallocate(task: ipc_space_t, name: mach_port_name_t) -> kern_return_t;
+    pub fn mach_port_insert_right(
+        task: ipc_space_t,
+        name: mach_port_name_t,
+        poly: mach_port_t,
+        polyPoly: mach_msg_type_name_t,
+    ) -> kern_return_t;
+    pub fn mach_port_extract_right(
+        task: ipc_space_t,
+        name: mach_port_name_t,
+        msgt_name: mach_msg_type_name_t,
+        poly: *mut mach_port_t,
+        polyPoly: *mut mach_msg_type_name_t,
+    ) -> kern_return_t;
 }
