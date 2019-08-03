865583425882ba9715b96240e9fbcfb3f7dd8a16
Jan Andre Ikenmeyer <jan@ikenmeyer.eu>
2019-08-03 11:18:22 +0000

    Bug 1570635 - Add about:certificate to secureInternalUIWhitelist. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D40212

--- browser/base/content/browser-siteIdentity.js
+++ browser/base/content/browser-siteIdentity.js
@@ -57,7 +57,7 @@ var gIdentityHandler = {
    * RegExp used to decide if an about url should be shown as being part of
    * the browser UI.
    */
-  _secureInternalUIWhitelist: /^(?:accounts|addons|cache|config|crashes|downloads|license|logins|preferences|protections|rights|sessionrestore|support|welcomeback)(?:[?#]|$)/i,
+  _secureInternalUIWhitelist: /^(?:accounts|addons|cache|certificate|config|crashes|downloads|license|logins|preferences|protections|rights|sessionrestore|support|welcomeback)(?:[?#]|$)/i,
 
   /**
    * Whether the established HTTPS connection is considered "broken".
