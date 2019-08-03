31a2c9657a24db3ff62c458319d2180362ac0c34
Bryce Van Dyk <bvandyk@mozilla.com>
2019-08-03 17:27:16 +0000

    Bug 1014393 - Remove EncodedFrameContainer. r=pehrsons
    Remove EncodedFrameContainer and clean up areas where it was used.
    EncodedFrameContainer provided a wrapper around an
    nsTArray<RefPtr<EncodedFrame>>, but it simplifies the code to simply expose
    this array. Also clean up unused enums in EncodedFrame, and clean up some of
    the outdated comments for our encoded frame handling.
    
    Differential Revision: https://phabricator.services.mozilla.com/D35384

--- dom/media/encoder/TrackEncoder.h
+++ dom/media/encoder/TrackEncoder.h
@@ -7,7 +7,7 @@
 #define TrackEncoder_h_
 
 #include "AudioSegment.h"
-#include "EncodedFrameContainer.h"
+#include "EncodedFrame.h"
 #include "MediaStreamGraph.h"
 #include "StreamTracks.h"
 #include "TrackMetadataBase.h"
@@ -82,7 +82,7 @@ class TrackEncoder {
    * Encodes raw segments. Result data is returned in aData, and called on the
    * worker thread.
    */
-  virtual nsresult GetEncodedTrack(EncodedFrameContainer& aData) = 0;
+  virtual nsresult GetEncodedTrack(nsTArray<RefPtr<EncodedFrame>>& aData) = 0;
 
   /**
    * Returns true once this TrackEncoder is initialized.
