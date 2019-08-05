360873127918834fc6b1e97687bed0950baf6754
alwu <alwu@mozilla.com>
2019-08-05 17:32:27 +0000

    Bug 1565689 - part5 : control media from chrome process. r=farre,baku
    This patch implements how to use MediaController to control corresponding media in content processes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38145
    
    --HG--
    extra : moz-landing-system : lando

--- dom/ipc/ContentChild.h
+++ dom/ipc/ContentChild.h
@@ -73,6 +73,7 @@ class ClonedMessageData;
 class BrowserChild;
 class GetFilesHelperChild;
 class TabContext;
+enum class MediaControlActions : uint32_t;
 
 class ContentChild final : public PContentChild,
                            public nsIWindowProvider,
@@ -687,6 +688,9 @@ class ContentChild final : public PContentChild,
 
   mozilla::ipc::IPCResult RecvSetMediaMuted(BrowsingContext* aContext,
                                             bool aMuted);
+
+  mozilla::ipc::IPCResult RecvUpdateMediaAction(BrowsingContext* aContext,
+                                                MediaControlActions aAction);
 
   void HoldBrowsingContextGroup(BrowsingContextGroup* aBCG);
   void ReleaseBrowsingContextGroup(BrowsingContextGroup* aBCG);
