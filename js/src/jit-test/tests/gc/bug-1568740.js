433561b46dd897e9366c78c4629c65316a342226
Paul Bone <pbone@mozilla.com>
2019-08-05 07:02:32 +0000

    Bug 1568740 - (part 8) Add a test r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D39993

---
+++ js/src/jit-test/tests/gc/bug-1568740.js
@@ -0,0 +1,20 @@
+gczeal(0);
+
+function setAndTest(param, value) {
+  gcparam(param, value);
+  assertEq(gcparam(param), value);
+}
+
+
+// Set a large nursery size.
+setAndTest("maxNurseryBytes", 1024*1024);
+setAndTest("minNurseryBytes", 1024*1024);
+minorgc();
+assertEq(gcparam("nurseryBytes"), 1024*1024);
+
+// Force it to shrink by more then one half.
+setAndTest("minNurseryBytes", 64*1024);
+setAndTest("maxNurseryBytes", 64*1024);
+minorgc();
+assertEq(gcparam("nurseryBytes"), 64*1024);
+
