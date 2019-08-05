e663b012d73fab9ae1d9acbe501558937074ca47
Markus Stange <mstange@themasta.com>
2019-08-05 12:09:46 +0000

    Bug 1570869 - Inline FrameRenderingComplete into HandleFrameOneDoc. r=nical
    Depends on D40374
    
    Differential Revision: https://phabricator.services.mozilla.com/D40375

--- gfx/webrender_bindings/RenderThread.h
+++ gfx/webrender_bindings/RenderThread.h
@@ -225,8 +225,6 @@ class RenderThread final {
   void IncPendingFrameCount(wr::WindowId aWindowId, const VsyncId& aStartId,
                             const TimeStamp& aStartTime,
                             uint8_t aDocFrameCount);
-  /// Can be called from any thread.
-  void FrameRenderingComplete(wr::WindowId aWindowId);
 
   void NotifySlowFrame(wr::WindowId aWindowId);
 
