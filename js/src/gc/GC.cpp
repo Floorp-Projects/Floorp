87a167b222c6028dd2535546045cf34516620303
Paul Bone <pbone@mozilla.com>
2019-08-05 06:37:55 +0000

    Bug 1388701 - Remove the special nursery size of 0 r=jonco
    Rather than make re-enabling the nursery work correctly (because there's
    various ways to disable it and we'd need to make sure we're enabling it for
    the right reason).  It's simplier just to not disable it for a
    gcMaxNurseryBytes of 0, and instead return an error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39988

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -1471,18 +1471,14 @@ bool GCSchedulingTunables::setParameter(JSGCParamKey key, uint32_t value,
       gcMaxBytes_ = value;
       break;
     case JSGC_MIN_NURSERY_BYTES:
-      if ((value > gcMaxNurseryBytes_ && gcMaxNurseryBytes_ != 0) ||
-          value < ArenaSize || value >= MaxNurseryBytes) {
-        // We make an exception for gcMaxNurseryBytes_ == 0 since that special
-        // value is used to disable generational GC.
+      if (value > gcMaxNurseryBytes_ || value < ArenaSize ||
+          value >= MaxNurseryBytes) {
         return false;
       }
       gcMinNurseryBytes_ = value;
       break;
     case JSGC_MAX_NURSERY_BYTES:
-      if (((value < gcMinNurseryBytes_) && (value != 0)) ||
-          value >= MaxNurseryBytes) {
-        // Note that we make an exception for value == 0 as above.
+      if (value < gcMinNurseryBytes_ || value >= MaxNurseryBytes) {
         return false;
       }
       gcMaxNurseryBytes_ = value;
