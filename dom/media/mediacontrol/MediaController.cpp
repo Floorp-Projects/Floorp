d64b11e7080d440d8259ace9b7c7ff781db79328
alwu <alwu@mozilla.com>
2019-08-06 19:24:45 +0000

    Bug 1565689 - part1 : implement MediaController and MediaControlService. r=baku
    In order to have a centralized audio control in the parent process, we create two new classes here.
    
    * MediaController
    MediaController is a class used to control certain amount of media in the content process. Every controller corresponds to a browsing context.
    For example, TabMediaController would correspond to the top level browsing context, which mean it can control all media in the specific tab.
    
    * MediaControlService
    As there might be multiple tabs playing audio, so there would be multiple controllers. MediaControlService is a place to manage all of them, you can access specific controller through MediaControlService by providing controller ID.
    Everytime a controller becomes active, which means there is a media starts in corresponding browsing context, then controller would be added into the list of the MediaControlService. And it would be removed from the list when the media in corresponding browsering context stopped.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38141
    
    --HG--
    extra : moz-landing-system : lando

---
+++ dom/media/mediacontrol/MediaController.cpp
@@ -0,0 +1,112 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "MediaController.h"
+
+#include "MediaControlService.h"
+#include "mozilla/dom/BrowsingContext.h"
+
+extern mozilla::LazyLogModule gMediaControlLog;
+
+// avoid redefined macro in unified build
+#undef LOG
+#define LOG(msg, ...)                                                       \
+  MOZ_LOG(gMediaControlLog, LogLevel::Debug,                                \
+          ("TabMediaController=%p, Id=%" PRId64 ", " msg, this, this->Id(), \
+           ##__VA_ARGS__))
+
+namespace mozilla {
+namespace dom {
+
+already_AddRefed<BrowsingContext> MediaController::GetContext() const {
+  return BrowsingContext::Get(mBrowsingContextId);
+}
+
+TabMediaController::TabMediaController(uint64_t aContextId)
+    : MediaController(aContextId) {
+  MOZ_DIAGNOSTIC_ASSERT(XRE_IsParentProcess(),
+                        "MediaController only runs on Chrome process!");
+  LOG("Create controller %" PRId64, Id());
+}
+
+TabMediaController::~TabMediaController() {
+  LOG("Destroy controller %" PRId64, Id());
+  MOZ_DIAGNOSTIC_ASSERT(!mControlledMediaNum);
+};
+
+void TabMediaController::Play() {
+  LOG("Play");
+  mIsPlaying = true;
+}
+
+void TabMediaController::Pause() {
+  LOG("Pause");
+  mIsPlaying = false;
+}
+
+void TabMediaController::Stop() {
+  LOG("Stop");
+  mIsPlaying = false;
+}
+
+void TabMediaController::Shutdown() {
+  mIsPlaying = false;
+  mControlledMediaNum = 0;
+}
+
+bool TabMediaController::IsAudible() const { return mIsPlaying && mAudible; }
+
+void TabMediaController::NotifyMediaActiveChanged(bool aActive) {
+  if (aActive) {
+    IncreaseControlledMediaNum();
+  } else {
+    DecreaseControlledMediaNum();
+  }
+}
+
+void TabMediaController::NotifyMediaAudibleChanged(bool aAudible) {
+  mAudible = aAudible;
+}
+
+void TabMediaController::IncreaseControlledMediaNum() {
+  MOZ_DIAGNOSTIC_ASSERT(mControlledMediaNum >= 0);
+  mControlledMediaNum++;
+  LOG("Increase controlled media num to %" PRId64, mControlledMediaNum);
+  if (mControlledMediaNum == 1) {
+    Activate();
+  }
+}
+
+void TabMediaController::DecreaseControlledMediaNum() {
+  MOZ_DIAGNOSTIC_ASSERT(mControlledMediaNum >= 1);
+  mControlledMediaNum--;
+  LOG("Decrease controlled media num to %" PRId64, mControlledMediaNum);
+  if (mControlledMediaNum == 0) {
+    Deactivate();
+  }
+}
+
+// TODO : Use watchable to moniter mControlledMediaNum
+void TabMediaController::Activate() {
+  mIsPlaying = true;
+  RefPtr<MediaControlService> service = MediaControlService::GetService();
+  MOZ_ASSERT(service);
+  service->AddMediaController(this);
+}
+
+void TabMediaController::Deactivate() {
+  mIsPlaying = false;
+  RefPtr<MediaControlService> service = MediaControlService::GetService();
+  MOZ_ASSERT(service);
+  service->RemoveMediaController(this);
+}
+
+uint64_t TabMediaController::ControlledMediaNum() const {
+  return mControlledMediaNum;
+}
+
+}  // namespace dom
+}  // namespace mozilla
