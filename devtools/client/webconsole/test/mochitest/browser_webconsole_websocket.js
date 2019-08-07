644c37795fcc4f529630128be4a9714d26834dd7
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-08-07 09:47:54 +0000

    Bug 1571606 - Fix browser_webconsole_websocket intermittent. r=Honza.
    The test was navigating to a data url but it doesn't seem like it
    was needed in the first place, as we only want to check that the
    error messages are shown in the console input.
    We also bump up the interval a bit just to be sure to capture the messages.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40934

--- devtools/client/webconsole/test/mochitest/browser_webconsole_websocket.js
+++ devtools/client/webconsole/test/mochitest/browser_webconsole_websocket.js
@@ -8,18 +8,19 @@
 // Check that WebSocket connection failure messages are displayed. See Bug 603750.
 
 const TEST_URI =
-  "http://example.com/browser/devtools/client/webconsole/" +
-  "test/mochitest/test-websocket.html";
-const TEST_URI2 =
-  "data:text/html;charset=utf-8,Web Console test for " +
-  "bug 603750: Web Socket errors";
+  "http://example.com/browser/devtools/client/webconsole/test/mochitest/test-websocket.html";
 
 add_task(async function() {
-  const hud = await openNewTabAndConsole(TEST_URI2);
-
-  BrowserTestUtils.loadURI(gBrowser.selectedBrowser, TEST_URI);
-
-  await waitFor(() => findMessage(hud, "ws://0.0.0.0:81"), null, 100);
-  await waitFor(() => findMessage(hud, "ws://0.0.0.0:82"), null, 100);
+  const hud = await openNewTabAndConsole(TEST_URI);
+  await waitFor(
+    () => findMessage(hud, "ws://0.0.0.0:81", ".error"),
+    "Did not find error message for ws://0.0.0.0:81 connection",
+    500
+  );
+  await waitFor(
+    () => findMessage(hud, "ws://0.0.0.0:82", ".error"),
+    "Did not find error message for ws://0.0.0.0:82 connection",
+    500
+  );
   ok(true, "WebSocket error messages are displayed in the console");
 });
