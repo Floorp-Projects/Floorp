40714b365365d5dd39086b85dde00be3e9932466
Aaron Klotz <aklotz@mozilla.com>
2019-08-07 15:58:09 +0000

    Bug 1571875: Part 2 - Change over all existing static local uses of DynamicallyLinkedFunctionPtr to use StaticDynamicallyLinkedFunctionPtr instead; r=mhowell
    Depends on D40885
    
    Differential Revision: https://phabricator.services.mozilla.com/D40886

--- mozglue/misc/WindowsMapRemoteView.cpp
+++ mozglue/misc/WindowsMapRemoteView.cpp
@@ -38,7 +38,7 @@ NTSTATUS NTAPI NtMapViewOfSection(
 NTSTATUS NTAPI NtUnmapViewOfSection(HANDLE aProcess, PVOID aBaseAddress);
 
 static DWORD GetWin32ErrorCode(NTSTATUS aNtStatus) {
-  static const mozilla::DynamicallyLinkedFunctionPtr<decltype(
+  static const mozilla::StaticDynamicallyLinkedFunctionPtr<decltype(
       &RtlNtStatusToDosError)>
       pRtlNtStatusToDosError(L"ntdll.dll", "RtlNtStatusToDosError");
 
@@ -56,7 +56,7 @@ MFBT_API void* MapRemoteViewOfFile(HANDLE aFileMapping, HANDLE aProcess,
                                    ULONG64 aOffset, PVOID aBaseAddress,
                                    SIZE_T aViewSize, ULONG aAllocationType,
                                    ULONG aProtectionFlags) {
-  static const DynamicallyLinkedFunctionPtr<decltype(&MapViewOfFileNuma2)>
+  static const StaticDynamicallyLinkedFunctionPtr<decltype(&MapViewOfFileNuma2)>
       pMapViewOfFileNuma2(L"Api-ms-win-core-memory-l1-1-5.dll",
                           "MapViewOfFileNuma2");
 
@@ -66,7 +66,7 @@ MFBT_API void* MapRemoteViewOfFile(HANDLE aFileMapping, HANDLE aProcess,
                                NUMA_NO_PREFERRED_NODE);
   }
 
-  static const DynamicallyLinkedFunctionPtr<decltype(&NtMapViewOfSection)>
+  static const StaticDynamicallyLinkedFunctionPtr<decltype(&NtMapViewOfSection)>
       pNtMapViewOfSection(L"ntdll.dll", "NtMapViewOfSection");
 
   MOZ_ASSERT(!!pNtMapViewOfSection);
@@ -100,14 +100,15 @@ MFBT_API void* MapRemoteViewOfFile(HANDLE aFileMapping, HANDLE aProcess,
 }
 
 MFBT_API bool UnmapRemoteViewOfFile(HANDLE aProcess, PVOID aBaseAddress) {
-  static const DynamicallyLinkedFunctionPtr<decltype(&UnmapViewOfFile2)>
+  static const StaticDynamicallyLinkedFunctionPtr<decltype(&UnmapViewOfFile2)>
       pUnmapViewOfFile2(L"kernel32.dll", "UnmapViewOfFile2");
 
   if (!!pUnmapViewOfFile2) {
     return !!pUnmapViewOfFile2(aProcess, aBaseAddress, 0);
   }
 
-  static const DynamicallyLinkedFunctionPtr<decltype(&NtUnmapViewOfSection)>
+  static const StaticDynamicallyLinkedFunctionPtr<decltype(
+      &NtUnmapViewOfSection)>
       pNtUnmapViewOfSection(L"ntdll.dll", "NtUnmapViewOfSection");
 
   MOZ_ASSERT(!!pNtUnmapViewOfSection);
