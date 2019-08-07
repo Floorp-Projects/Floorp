b92c30a27aabc06a9d7cd1227b710355ca124829
Gregory Mierzwinski <gmierz2@outlook.com>
2019-08-07 17:50:26 +0000

    Bug 1567954 - Part 2: Implement IntelPowerGadget and IPGResultsHandler classes in the Mozpower module. r=perftest-reviewers,rwood
    This patch implements IntelPowerGadget and IPGResultsHandler for the Mozpower module. These classes handle data generation with Intel Power Gadget as well as cleaning the data and formatting it into the PERFHERDER_DATA format.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39192

--- testing/mozbase/mozpower/tests/conftest.py
+++ testing/mozbase/mozpower/tests/conftest.py
@@ -1,8 +1,14 @@
 from __future__ import absolute_import, print_function
 
+import os
 import pytest
+import tempfile
 
 from mozpower.powerbase import PowerBase
+from mozpower.intel_power_gadget import (
+    IntelPowerGadget,
+    IPGResultsHandler
+)
 
 
 @pytest.fixture(scope='function')
@@ -13,3 +19,35 @@ def powermeasurer():
     class PowerMeasurer(PowerBase):
         pass
     return PowerMeasurer()
+
+
+@pytest.fixture(scope='function')
+def ipg_obj():
+    """Returns an IntelPowerGadget object with the test
+    output file path.
+    """
+    return IntelPowerGadget(
+        'ipg-path',
+        output_file_path=os.path.abspath(os.path.dirname(__file__)) +
+        '/files/raptor-tp6-amazon-firefox_powerlog',
+    )
+
+
+@pytest.fixture(scope='function')
+def ipg_rh_obj():
+    """Returns an IPGResultsHandler object set up with the
+    test files and cleans up the directory after the tests
+    are complete.
+    """
+    base_path = os.path.abspath(os.path.dirname(__file__)) + '/files/'
+    tmpdir = tempfile.mkdtemp()
+
+    # Return the results handler for the test
+    yield IPGResultsHandler(
+        [
+            base_path + 'raptor-tp6-amazon-firefox_powerlog_1_.txt',
+            base_path + 'raptor-tp6-amazon-firefox_powerlog_2_.txt',
+            base_path + 'raptor-tp6-amazon-firefox_powerlog_3_.txt',
+        ],
+        tmpdir
+    )
