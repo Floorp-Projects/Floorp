501d41a22f4b91e34f5b1f5a9d79277c371da9c1
Dan Glastonbury <dan.glastonbury@gmail.com>
2019-08-07 03:51:20 +0000

    Bug 1571612 - P1: Split FlushType::Style and FlushType::Frame. r=heycam
    Differential Revision: https://phabricator.services.mozilla.com/D40755

--- dom/base/FlushType.h
+++ dom/base/FlushType.h
@@ -20,22 +20,20 @@ namespace mozilla {
  * flushTypeNames array inside PresShell::FlushPendingNotifications.
  */
 enum class FlushType : uint8_t {
-  None = 0,             /* Actually don't flush anything */
-  Event = 1,            /* Flush pending events before notify other observers */
-  Content = 2,          /* flush the content model construction */
-  ContentAndNotify = 3, /* As above, plus flush the frame model
-                           construction and other nsIMutationObserver
-                           notifications. */
-  Style = 4,            /* As above, plus flush style reresolution */
-  Frames = Style,
-  EnsurePresShellInitAndFrames =
-      5,                   /* As above, plus ensure the pres shell is alive */
-  InterruptibleLayout = 6, /* As above, plus flush reflow,
-                              but allow it to be interrupted (so
-                              an incomplete layout may result) */
-  Layout = 7,              /* As above, but layout must run to
-                              completion */
-  Display = 8,             /* As above, plus flush painting */
+  None,             /* Actually don't flush anything */
+  Event,            /* Flush pending events before notify other observers */
+  Content,          /* flush the content model construction */
+  ContentAndNotify, /* As above, plus flush the frame model
+                       construction and other nsIMutationObserver
+                       notifications. */
+  Style,            /* As above, plus flush style reresolution */
+  Frames,           /* As above, plus flush frame construction */
+  EnsurePresShellInitAndFrames, /* As above, plus ensure the pres shell is alive
+                                 */
+  InterruptibleLayout, /* As above, plus flush reflow, but allow it to be
+                          interrupted (so an incomplete layout may result) */
+  Layout,              /* As above, but layout must run to completion */
+  Display,             /* As above, plus flush painting */
   Count
 };
 
