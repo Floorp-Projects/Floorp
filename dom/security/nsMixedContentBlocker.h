cce855c685cfba38bffce5a7fcdafe979c33f1f2
Birunthan Mohanathas <birunthan@mohanathas.com>
2019-08-07 00:19:59 +0000

    Bug 1376309 - Allow localhost ws:// connections from secure origins. r=jkt
    We already allow HTTPS origins to use to plain HTTP active content when using
    loopback URLs such as http://127.0.0.1. Lets extend this to WebSocket
    connections as well to match Chrome.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38290

--- dom/security/nsMixedContentBlocker.h
+++ dom/security/nsMixedContentBlocker.h
@@ -49,6 +49,8 @@ class nsMixedContentBlocker : public nsIContentPolicy,
 
   // See:
   // https://w3c.github.io/webappsec-secure-contexts/#is-origin-trustworthy
+  static bool IsPotentiallyTrustworthyLoopbackHost(
+      const nsACString& aAsciiHost);
   static bool IsPotentiallyTrustworthyLoopbackURL(nsIURI* aURL);
   static bool IsPotentiallyTrustworthyOnion(nsIURI* aURL);
   static bool IsPotentiallyTrustworthyOrigin(nsIURI* aURI);
