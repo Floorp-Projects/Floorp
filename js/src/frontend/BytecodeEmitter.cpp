77c298a89df69bca852d8dd97aa6633ea657df54
Ashley Hauck <khyperia@mozilla.com>
2019-08-07 17:29:35 +0000

    Bug 1571289 - Emit JSOP_SETFUNNAME immediately, instead of deferring until later. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D40753

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -4094,9 +4094,7 @@ bool BytecodeEmitter::emitAssignmentRhs(ParseNode* rhs,
     if (anonFunctionName) {
       return emitAnonymousFunctionWithName(rhs, anonFunctionName);
     }
-    // If anonFunctionName is null, that means we don't have a compiletime
-    // name, and should emit JSOP_SETFUNNAME (which happens later).
-    *emitSetFunName = true;
+    return emitAnonymousFunctionWithComputedName(rhs, FunctionPrefixKind::None);
   }
   return emitTree(rhs);
 }
@@ -4407,8 +4405,6 @@ bool BytecodeEmitter::emitAssignmentOrInit(ParseNodeKind kind, ParseNode* lhs,
       // We threw above, so nothing to do here.
       break;
     case ParseNodeKind::ElemExpr: {
-      MOZ_ASSERT((!anonFunctionName && rhs && rhs->isDirectRHSAnonFunction()) ==
-                 (emitSetFunName == ElemOpEmitter::EmitSetFunctionName::Yes));
       if (!eoe->emitAssignment(emitSetFunName)) {
         //          [stack] VAL
         return false;
---
+++ js/src/jit-test/tests/fields/bug1571289.js
@@ -0,0 +1,6 @@
+class C66 {
+    0 = class {
+        static set name(x) {}
+    };
+}
+new C66();
