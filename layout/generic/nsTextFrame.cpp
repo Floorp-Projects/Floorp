f8d104910730dab6793c4d14ef6da3313dfba554
Charlie Marlow <cmarlow@mozilla.com>
2019-08-07 17:41:13 +0000

    Bug 1411922: Part 1: Add rendering support for text-decoration-skip-ink r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D39377
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -6227,6 +6227,7 @@ bool nsTextFrame::PaintTextWithSelectionColors(
   params.clipEdges = &aClipEdges;
   params.advanceWidth = &advance;
   params.callbacks = aParams.callbacks;
+  params.glyphRange = aParams.glyphRange;
 
   PaintShadowParams shadowParams(aParams);
   shadowParams.provider = aParams.provider;
@@ -6691,6 +6692,11 @@ void nsTextFrame::PaintText(const PaintTextParams& aParams,
 
   // Trim trailing whitespace, unless we're painting a selection highlight,
   // which should include trailing spaces if present (bug 1146754).
+  Range glyphRange;
+  if (aIsSelected) {
+    provider.InitializeForDisplay(true);
+    glyphRange = ComputeTransformedRange(provider);
+  }
   provider.InitializeForDisplay(!aIsSelected);
 
   const bool reversed = mTextRun->IsInlineReversed();
@@ -6747,6 +6753,7 @@ void nsTextFrame::PaintText(const PaintTextParams& aParams,
     params.provider = &provider;
     params.contentRange = contentRange;
     params.textPaintStyle = &textPaintStyle;
+    params.glyphRange = glyphRange;
     if (PaintTextWithSelection(params, clipEdges)) {
       return;
     }
@@ -6797,6 +6804,7 @@ void nsTextFrame::PaintText(const PaintTextParams& aParams,
   params.drawSoftHyphen = (GetStateBits() & TEXT_HYPHEN_BREAK) != 0;
   params.contextPaint = aParams.contextPaint;
   params.callbacks = aParams.callbacks;
+  params.glyphRange = range;
   DrawText(range, textBaselinePt, params);
 }
 
@@ -6946,6 +6954,8 @@ void nsTextFrame::DrawTextRunAndDecorations(
   params.dirtyRect = aParams.dirtyRect;
   params.overrideColor = aParams.decorationOverrideColor;
   params.callbacks = aParams.callbacks;
+  params.glyphRange = aParams.glyphRange;
+  params.provider = aParams.provider;
   // pt is the physical point where the decoration is to be drawn,
   // relative to the frame; one of its coordinates will be updated below.
   params.pt = Point(x / app, y / app);
