ae0eba57cedcaafd988e8c9b717230f82d838bee
Johann Hofmann <jhofmann@mozilla.com>
2019-08-07 09:03:13 +0000

    Bug 1570751 - Avoid identity block flickering. r=Ehsan
    The security UI receives an about:blank load when navigating to or from
    about: pages, which causes it to try and render security indicators before
    showing the "real" security indicators for the next site. It shouldn't do that.
    
    The solution is a bit cheap, we simply ignore all about:blank and other loads
    that will receive a pageproxystate="invalid" state in the URL bar and thus
    hide the identity block. This takes care of our problem and also avoids
    some work when loading the home page.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40515
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/extensions/test/browser/browser_ext_identity_indication.js
+++ browser/components/extensions/test/browser/browser_ext_identity_indication.js
@@ -12,8 +12,11 @@ function confirmDefaults() {
     "Identity icon should be the search icon"
   );
 
-  let label = document.getElementById("identity-icon-label").value;
-  is(label, "", "No label should be used before the extension is started");
+  let label = document.getElementById("identity-icon-label");
+  ok(
+    BrowserTestUtils.is_hidden(label),
+    "No label should be used before the extension is started"
+  );
 }
 
 function confirmExtensionPage() {
@@ -33,8 +36,13 @@ function confirmExtensionPage() {
     "The correct tooltip should be used"
   );
 
-  let label = document.getElementById("identity-icon-label").value;
-  is(label, "Extension (Test Extension)", "The correct label should be used");
+  let label = document.getElementById("identity-icon-label");
+  is(
+    label.value,
+    "Extension (Test Extension)",
+    "The correct label should be used"
+  );
+  ok(BrowserTestUtils.is_visible(label), "No label should be visible");
 }
 
 add_task(async function testIdentityIndication() {
