ea550ac41dca22cca2be6901aef3d7740fa77352
dlee <dlee@mozilla.com>
2019-08-07 15:17:49 +0000

    Bug 1564346 - SafeBrowsing gtest code refactoring. r=gcp
    Refactor the gtest code because it confused me while adding new tests.
    This patch focus on refining utility function but it also contains other
    minor refinements.
    
    Changes includes:
    1. Add comments to utility function
    2. Move common utility functions to Common.cpp and remove duplicates
    3. Header file removal and reorder
    4. Unify MPL commnetc
    5. Replace anonymouse namespace with static function
    
    Differential Revision: https://phabricator.services.mozilla.com/D37532

--- toolkit/components/url-classifier/tests/gtest/TestFailUpdate.cpp
+++ toolkit/components/url-classifier/tests/gtest/TestFailUpdate.cpp
@@ -1,34 +1,43 @@
-#include "HashStore.h"
-#include "nsPrintfCString.h"
-#include "string.h"
-#include "gtest/gtest.h"
-#include "mozilla/Unused.h"
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-using namespace mozilla;
-using namespace mozilla::safebrowsing;
+#include "string.h"
+
+#include "gtest/gtest.h"
+#include "HashStore.h"
+#include "mozilla/Unused.h"
+#include "nsPrintfCString.h"
 
 static const char* kFilesInV2[] = {".vlpset", ".sbstore"};
 static const char* kFilesInV4[] = {".vlpset", ".metadata"};
 
-#define V2_TABLE "gtest-malware-simple"
-#define V4_TABLE1 "goog-malware-proto"
-#define V4_TABLE2 "goog-phish-proto"
+#define GTEST_MALWARE_TABLE_V4 NS_LITERAL_CSTRING("goog-malware-proto")
+#define GTEST_PHISH_TABLE_V4 NS_LITERAL_CSTRING("goog-phish-proto")
 
 #define ROOT_DIR NS_LITERAL_STRING("safebrowsing")
 #define SB_FILE(x, y) NS_ConvertUTF8toUTF16(nsPrintfCString("%s%s", x, y))
 
 template <typename T, size_t N>
-void CheckFileExist(const char* table, const T (&files)[N], bool expectExists) {
+static void CheckFileExist(const nsCString& aTable, const T (&aFiles)[N],
+                           bool aExpectExists, const char* aMsg = nullptr) {
   for (uint32_t i = 0; i < N; i++) {
     // This is just a quick way to know if this is v4 table
-    NS_ConvertUTF8toUTF16 SUB_DIR(strstr(table, "-proto") ? "google4" : "");
-    nsCOMPtr<nsIFile> file = GetFile(
-        nsTArray<nsString>{ROOT_DIR, SUB_DIR, SB_FILE(table, files[i])});
+    NS_ConvertUTF8toUTF16 SUB_DIR(strstr(aTable.get(), "-proto") ? "google4"
+                                                                 : "");
+    nsCOMPtr<nsIFile> file = GetFile(nsTArray<nsString>{
+        ROOT_DIR, SUB_DIR, SB_FILE(aTable.get(), aFiles[i])});
 
     bool exists;
     file->Exists(&exists);
 
-    ASSERT_EQ(expectExists, exists) << file->HumanReadablePath().get();
+    if (aMsg) {
+      ASSERT_EQ(aExpectExists, exists)
+          << file->HumanReadablePath().get() << " " << aMsg;
+    } else {
+      ASSERT_EQ(aExpectExists, exists) << file->HumanReadablePath().get();
+    }
   }
 }
 
@@ -39,14 +48,14 @@ TEST(UrlClassifierFailUpdate, CheckTableReset)
 
   // Apply V2 update
   {
-    RefPtr<TableUpdateV2> update =
-        new TableUpdateV2(NS_LITERAL_CSTRING(V2_TABLE));
+    RefPtr<TableUpdateV2> update = new TableUpdateV2(GTEST_TABLE_V2);
     Unused << update->NewAddChunk(1);
 
     ApplyUpdate(update);
 
     // A successful V2 update should create .vlpset & .sbstore files
-    CheckFileExist(V2_TABLE, kFilesInV2, true);
+    CheckFileExist(GTEST_TABLE_V2, kFilesInV2, true,
+                   "V2 update doesn't create vlpset or sbstore");
   }
 
   // Helper function to generate table update data
@@ -58,41 +67,43 @@ TEST(UrlClassifierFailUpdate, CheckTableReset)
 
   // Apply V4 update for table1
   {
-    RefPtr<TableUpdateV4> update =
-        new TableUpdateV4(NS_LITERAL_CSTRING(V4_TABLE1));
+    RefPtr<TableUpdateV4> update = new TableUpdateV4(GTEST_MALWARE_TABLE_V4);
     func(update, FULL_UPDATE, "test_prefix");
 
     ApplyUpdate(update);
 
     // A successful V4 update should create .vlpset & .metadata files
-    CheckFileExist(V4_TABLE1, kFilesInV4, true);
+    CheckFileExist(GTEST_MALWARE_TABLE_V4, kFilesInV4, true,
+                   "v4 update doesn't create vlpset or metadata");
   }
 
   // Apply V4 update for table2
   {
-    RefPtr<TableUpdateV4> update =
-        new TableUpdateV4(NS_LITERAL_CSTRING(V4_TABLE2));
+    RefPtr<TableUpdateV4> update = new TableUpdateV4(GTEST_PHISH_TABLE_V4);
     func(update, FULL_UPDATE, "test_prefix");
 
     ApplyUpdate(update);
 
-    CheckFileExist(V4_TABLE2, kFilesInV4, true);
+    CheckFileExist(GTEST_PHISH_TABLE_V4, kFilesInV4, true,
+                   "v4 update doesn't create vlpset or metadata");
   }
 
   // Apply V4 update with the same prefix in previous full udpate
   // This should cause an update error.
   {
-    RefPtr<TableUpdateV4> update =
-        new TableUpdateV4(NS_LITERAL_CSTRING(V4_TABLE1));
+    RefPtr<TableUpdateV4> update = new TableUpdateV4(GTEST_MALWARE_TABLE_V4);
     func(update, PARTIAL_UPDATE, "test_prefix");
 
     ApplyUpdate(update);
 
     // A fail update should remove files for that table
-    CheckFileExist(V4_TABLE1, kFilesInV4, false);
+    CheckFileExist(GTEST_MALWARE_TABLE_V4, kFilesInV4, false,
+                   "a fail v4 update doesn't remove the tables");
 
     // A fail update should NOT remove files for the other tables
-    CheckFileExist(V2_TABLE, kFilesInV2, true);
-    CheckFileExist(V4_TABLE2, kFilesInV4, true);
+    CheckFileExist(GTEST_TABLE_V2, kFilesInV2, true,
+                   "a fail v4 update removes a v2 table");
+    CheckFileExist(GTEST_PHISH_TABLE_V4, kFilesInV4, true,
+                   "a fail v4 update removes the other v4 table");
   }
 }
