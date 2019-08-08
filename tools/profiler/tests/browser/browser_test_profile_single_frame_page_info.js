4fb6dc2751f043d1b4fd2c864dbf33fb63304b9f
Nazım Can Altınova <canaltinova@gmail.com>
2019-08-08 14:53:19 +0000

    Bug 1572456 - Switch the files of multi/single page information tests. r=julienw
    Differential Revision: https://phabricator.services.mozilla.com/D41216

--- tools/profiler/tests/browser/browser_test_profile_single_frame_page_info.js
+++ tools/profiler/tests/browser/browser_test_profile_single_frame_page_info.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-add_task(async function test_profile_multi_frame_page_info() {
+add_task(async function test_profile_single_frame_page_info() {
   if (!AppConstants.MOZ_GECKO_PROFILER) {
     return;
   }
@@ -11,8 +11,7 @@ add_task(async function test_profile_multi_frame_page_info() {
   await Services.profiler.ClearAllPages();
   startProfiler();
 
-  // multi_frame.html embeds single_frame.html inside an iframe.
-  const url = BASE_URL + "multi_frame.html";
+  const url = BASE_URL + "single_frame.html";
   let contentPid;
   await BrowserTestUtils.withNewTab(url, async function(contentBrowser) {
     contentPid = await ContentTask.spawn(contentBrowser, null, () => {
@@ -23,29 +22,20 @@ add_task(async function test_profile_multi_frame_page_info() {
   const profile = await Services.profiler.getProfileDataAsync();
   Services.profiler.StopProfiler();
 
-  let foundPage = 0;
+  let pageFound = false;
   // We need to find the correct content process for that tab.
   let contentProcess = profile.processes.find(
     p => p.threads[0].pid == contentPid
   );
   for (const page of contentProcess.pages) {
-    // Parent page
     if (page.url == url) {
       Assert.equal(page.url, url);
       Assert.equal(typeof page.docshellId, "string");
       Assert.equal(typeof page.historyId, "number");
       Assert.equal(page.isSubFrame, false);
-      foundPage++;
-    }
-
-    // Child page (iframe)
-    if (page.url == BASE_URL + "single_frame.html") {
-      Assert.equal(page.url, BASE_URL + "single_frame.html");
-      Assert.equal(typeof page.docshellId, "string");
-      Assert.equal(typeof page.historyId, "number");
-      Assert.equal(page.isSubFrame, true);
-      foundPage++;
+      pageFound = true;
+      break;
     }
   }
-  Assert.equal(foundPage, 2);
+  Assert.equal(pageFound, true);
 });
