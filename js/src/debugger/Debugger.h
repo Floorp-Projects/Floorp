301b3b222fe1502075c09a62513cc5924d968673
Jim Blandy <jimb@mozilla.com>
2019-08-08 23:20:02 +0000

    Bug 1567245: Rename DebuggerWeakMap type parameter 'UnbarrieredKey' to 'Referent'. r=jorendorff
    Technically, DebuggerWeakMap could be used for any sort of cross-compartment
    mapping. But we actually only use it for looking up Debugger reflection objects,
    so it is more informative to call the key and value types 'referent' and
    'wrapper'.
    
    No intended change in visible behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38540
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/debugger/Debugger.h
+++ js/src/debugger/Debugger.h
@@ -261,11 +261,10 @@ extern void CheckDebuggeeThing(JSObject* obj, bool invisibleOk);
  * objects are killed when debugging is disabled for their compartment, and if
  * it's re-enabled later, new Frame objects are created.)
  */
-template <class UnbarrieredKey, class Wrapper, bool InvisibleKeysOk = false>
-class DebuggerWeakMap
-    : private WeakMap<HeapPtr<UnbarrieredKey>, HeapPtr<Wrapper*>> {
+template <class Referent, class Wrapper, bool InvisibleKeysOk = false>
+class DebuggerWeakMap : private WeakMap<HeapPtr<Referent>, HeapPtr<Wrapper*>> {
  private:
-  typedef HeapPtr<UnbarrieredKey> Key;
+  typedef HeapPtr<Referent> Key;
   typedef HeapPtr<Wrapper*> Value;
 
   typedef HashMap<JS::Zone*, uintptr_t, DefaultHasher<JS::Zone*>,
