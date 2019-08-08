17c3e69cbbaac404dfbd10e6353359778e3e0e54
Nika Layzell <nika@thelayzells.com>
2019-08-08 16:07:34 +0000

    Bug 1566643 - Part 5: Remove CPM::GetBrowserParentsByProcessId, r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D39261
    
    --HG--
    extra : moz-landing-system : lando

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -757,14 +757,12 @@ void ContentParent::ReleaseCachedProcesses() {
   // future...
   nsTArray<ContentParent*>& contentParents =
       GetOrCreatePool(NS_LITERAL_STRING(DEFAULT_REMOTE_TYPE));
-  ContentProcessManager* cpm = ContentProcessManager::GetSingleton();
   nsTArray<ContentParent*> toRelease;
 
   // Shuting down these processes will change the array so let's use another
   // array for the removal.
   for (auto* cp : contentParents) {
-    nsTArray<TabId> tabIds = cpm->GetBrowserParentsByProcessId(cp->mChildID);
-    if (!tabIds.Length()) {
+    if (cp->ManagedPBrowserParent().Count() == 0) {
       toRelease.AppendElement(cp);
     }
   }
@@ -1787,8 +1785,8 @@ void ContentParent::NotifyTabDestroying(const TabId& aTabId,
       return;
     }
     ++cp->mNumDestroyingTabs;
-    nsTArray<TabId> tabIds = cpm->GetBrowserParentsByProcessId(aCpId);
-    if (static_cast<size_t>(cp->mNumDestroyingTabs) != tabIds.Length()) {
+    uint32_t tabCount = cpm->GetBrowserParentCountByProcessId(aCpId);
+    if (uint32_t(cp->mNumDestroyingTabs) != tabCount) {
       return;
     }
 
@@ -1843,10 +1841,8 @@ void ContentParent::NotifyTabDestroyed(const TabId& aTabId,
   // There can be more than one PBrowser for a given app process
   // because of popup windows.  When the last one closes, shut
   // us down.
-  ContentProcessManager* cpm = ContentProcessManager::GetSingleton();
-  nsTArray<TabId> tabIds = cpm->GetBrowserParentsByProcessId(this->ChildID());
-
-  if (tabIds.Length() == 1 && !ShouldKeepProcessAlive() && !TryToRecycle()) {
+  if (ManagedPBrowserParent().Count() == 1 && !ShouldKeepProcessAlive() &&
+      !TryToRecycle()) {
     // In the case of normal shutdown, send a shutdown message to child to
     // allow it to perform shutdown tasks.
     MessageLoop::current()->PostTask(NewRunnableMethod<ShutDownMethod>(
