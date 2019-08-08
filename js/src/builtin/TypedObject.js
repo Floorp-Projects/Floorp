a30e9c1f3cf13599b90ef2cd4d3b6c233ba266b6
Jared Wein <jwein@mozilla.com>
2019-08-08 15:18:41 +0000

    Bug 1571567 - Fix no-fallthrough errors in /js. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D40875
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/builtin/TypedObject.js
+++ js/src/builtin/TypedObject.js
@@ -497,6 +497,7 @@ function TypedObjectArrayTypeBuild(a, b, c) {
       ThrowTypeError(JSMSG_TYPEDOBJECT_BAD_ARGS);
     else
       ThrowTypeError(JSMSG_TYPEDOBJECT_BAD_ARGS);
+      break;
   default:
     ThrowTypeError(JSMSG_TYPEDOBJECT_BAD_ARGS);
   }
