271b6b135703d1acfeef780249270001f5e0f9ee
Scott <scott.downe@gmail.com>
2019-08-08 16:22:38 +0200

    Bug 1568932 - reverting PR #5111 due to perf regression (#5210). r=Mardak a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D41213

--- browser/components/newtab/content-src/components/DiscoveryStreamComponents/DSImage/DSImage.jsx
+++ browser/components/newtab/content-src/components/DiscoveryStreamComponents/DSImage/DSImage.jsx
@@ -46,13 +46,7 @@ export class DSImage extends React.PureComponent {
   }
 
   componentDidMount() {
-    this.observer = new IntersectionObserver(this.onSeen.bind(this), {
-      // Assume an image will be eventually seen if it is within 520px of the viewport
-      // This is half the average Desktop vertical screen size:
-      // http://gs.statcounter.com/screen-resolution-stats/desktop/north-america
-      rootMargin: `540px`,
-    });
-
+    this.observer = new IntersectionObserver(this.onSeen.bind(this));
     this.observer.observe(ReactDOM.findDOMNode(this));
   }
 
