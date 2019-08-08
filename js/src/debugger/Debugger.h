bc1125aacea84ce21d322a1bab2416b1ac0fc184
Jim Blandy <jimb@mozilla.com>
2019-08-08 23:37:46 +0000

    Bug 1564164: Move DebuggerSource into its own file. r=jorendorff
    This adds js/src/dbg/Source.{cpp,h}, and moves DebuggerSource's definition
    there. No intended change in implementation or visible behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38538

--- js/src/debugger/Debugger.h
+++ js/src/debugger/Debugger.h
@@ -1146,55 +1146,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger> {
   Debugger& operator=(const Debugger&) = delete;
 };
 
-class DebuggerSource : public NativeObject {
- public:
-  static const Class class_;
-
-  enum {
-    OWNER_SLOT,
-    TEXT_SLOT,
-    RESERVED_SLOTS,
-  };
-
-  static NativeObject* initClass(JSContext* cx, Handle<GlobalObject*> global,
-                                 HandleObject debugCtor);
-  static DebuggerSource* create(JSContext* cx, HandleObject proto,
-                                Handle<DebuggerSourceReferent> referent,
-                                HandleNativeObject debugger);
-
-  static void trace(JSTracer* trc, JSObject* obj);
-
-  NativeObject* getReferentRawObject() const;
-  DebuggerSourceReferent getReferent() const;
-
-  static DebuggerSource* check(JSContext* cx, HandleValue v,
-                               const char* fnname);
-  template <typename ReferentT>
-  static DebuggerSource* checkThis(JSContext* cx, const CallArgs& args,
-                                   const char* fnname, const char* refname);
-
-  // JS methods
-  static bool construct(JSContext* cx, unsigned argc, Value* vp);
-  static bool getText(JSContext* cx, unsigned argc, Value* vp);
-  static bool getBinary(JSContext* cx, unsigned argc, Value* vp);
-  static bool getURL(JSContext* cx, unsigned argc, Value* vp);
-  static bool getId(JSContext* cx, unsigned argc, Value* vp);
-  static bool getDisplayURL(JSContext* cx, unsigned argc, Value* vp);
-  static bool getElement(JSContext* cx, unsigned argc, Value* vp);
-  static bool getElementProperty(JSContext* cx, unsigned argc, Value* vp);
-  static bool getIntroductionScript(JSContext* cx, unsigned argc, Value* vp);
-  static bool getIntroductionOffset(JSContext* cx, unsigned argc, Value* vp);
-  static bool getIntroductionType(JSContext* cx, unsigned argc, Value* vp);
-  static bool setSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
-  static bool getSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
-
- private:
-  static const ClassOps classOps_;
-
-  static const JSPropertySpec properties_[];
-  static const JSFunctionSpec methods_[];
-};
-
 /*
  * A Handler represents a Debugger API reflection object's handler function,
  * like a Debugger.Frame's onStep handler. These handler functions are called by
---
+++ js/src/debugger/Source.h
@@ -0,0 +1,72 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef dbg_Source_h
+#define dbg_Source_h
+
+#include "jsapi.h"
+
+#include "debugger/Debugger.h"
+#include "js/Class.h"
+#include "js/RootingAPI.h"
+#include "js/TypeDecls.h"
+#include "vm/GlobalObject.h"
+#include "vm/NativeObject.h"
+
+namespace js {
+
+class DebuggerSource : public NativeObject {
+ public:
+  static const Class class_;
+
+  enum {
+    OWNER_SLOT,
+    TEXT_SLOT,
+    RESERVED_SLOTS,
+  };
+
+  static NativeObject* initClass(JSContext* cx, Handle<GlobalObject*> global,
+                                 HandleObject debugCtor);
+  static DebuggerSource* create(JSContext* cx, HandleObject proto,
+                                Handle<DebuggerSourceReferent> referent,
+                                HandleNativeObject debugger);
+
+  static void trace(JSTracer* trc, JSObject* obj);
+
+  NativeObject* getReferentRawObject() const;
+  DebuggerSourceReferent getReferent() const;
+
+  static DebuggerSource* check(JSContext* cx, HandleValue v,
+                               const char* fnname);
+  template <typename ReferentT>
+  static DebuggerSource* checkThis(JSContext* cx, const CallArgs& args,
+                                   const char* fnname, const char* refname);
+
+  // JS methods
+  static bool construct(JSContext* cx, unsigned argc, Value* vp);
+  static bool getText(JSContext* cx, unsigned argc, Value* vp);
+  static bool getBinary(JSContext* cx, unsigned argc, Value* vp);
+  static bool getURL(JSContext* cx, unsigned argc, Value* vp);
+  static bool getId(JSContext* cx, unsigned argc, Value* vp);
+  static bool getDisplayURL(JSContext* cx, unsigned argc, Value* vp);
+  static bool getElement(JSContext* cx, unsigned argc, Value* vp);
+  static bool getElementProperty(JSContext* cx, unsigned argc, Value* vp);
+  static bool getIntroductionScript(JSContext* cx, unsigned argc, Value* vp);
+  static bool getIntroductionOffset(JSContext* cx, unsigned argc, Value* vp);
+  static bool getIntroductionType(JSContext* cx, unsigned argc, Value* vp);
+  static bool setSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
+  static bool getSourceMapURL(JSContext* cx, unsigned argc, Value* vp);
+
+ private:
+  static const ClassOps classOps_;
+
+  static const JSPropertySpec properties_[];
+  static const JSFunctionSpec methods_[];
+};
+
+} /* namespace js */
+
+#endif /* dbg_Source_h */
