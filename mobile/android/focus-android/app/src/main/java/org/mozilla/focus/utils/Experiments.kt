dba124fe2349d218450544dfef545238bedd80f4
Mathieu Leplatre <mathieu@mozilla.com>
2019-08-08 10:37:55 +0200

    [focus] Fixes https://github.com/mozilla-mobile/focus-android/issues/4327: Fetch experiments from Remote Settings CDN

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Experiments.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/Experiments.kt
@@ -10,7 +10,7 @@ import org.mozilla.focus.FocusApplication
 import org.mozilla.focus.web.Config
 
 const val EXPERIMENTS_JSON_FILENAME = "experiments.json"
-const val EXPERIMENTS_BASE_URL = "https://settings.prod.mozaws.net/v1"
+const val EXPERIMENTS_BASE_URL = "https://firefox.settings.services.mozilla.com/v1"
 const val EXPERIMENTS_BUCKET_NAME = "main"
 const val EXPERIMENTS_COLLECTION_NAME = "focus-experiments"
 
