268981bb20fe562e7ad2fb86394cc1485d9c2e1b
André Bargull <andre.bargull@gmail.com>
2019-08-09 19:43:19 +0000

    Bug 1539780: Remove "--with-intl-api=build" build config option. r=jwalden
    There are about the same number of occurrences of "ENABLE_INTL_API" and "EXPOSE_INTL_API"
    in the tree, so preferring one over the other doesn't lead to fewer changes. Therefore
    I went with "ENABLE_INTL_API", because "ENABLE_" (resp. "MOZ_ENABLE") is already used as
    the prefix for other preprocessor ifdef's.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41188
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -698,7 +698,7 @@ struct JSRuntime {
     return defaultFreeOp_;
   }
 
-#if !EXPOSE_INTL_API
+#if !ENABLE_INTL_API
   /* Number localization, used by jsnum.cpp. */
   js::WriteOnceData<const char*> thousandsSeparator;
   js::WriteOnceData<const char*> decimalSeparator;
