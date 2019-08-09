41dabd23ea9e13458605470e4dcc7f89abdb69ff
Travis Long <tlong@mozilla.com>
2019-08-09 14:57:27 -0500

    [components] Bug 1572796 - Improve service-experiments public API docstrings

--- mobile/android/android-components/components/service/experiments/src/main/java/mozilla/components/service/experiments/Configuration.kt
+++ mobile/android/android-components/components/service/experiments/src/main/java/mozilla/components/service/experiments/Configuration.kt
@@ -11,6 +11,9 @@ import mozilla.components.lib.fetch.httpurlconnection.HttpURLConnectionClient
  * The Configuration class describes how to configure Experiments.
  *
  * @property httpClient The HTTP client implementation to use for uploading pings.
+ * @property kintoEndpoint the endpoint to fetch experiments from, must be one of:
+ * [ExperimentsUpdater.KINTO_ENDPOINT_DEV], [ExperimentsUpdater.KINTO_ENDPOINT_STAGING], or
+ * [ExperimentsUpdater.KINTO_ENDPOINT_PROD]
  */
 data class Configuration(
     val httpClient: Lazy<Client> = lazy { HttpURLConnectionClient() },
