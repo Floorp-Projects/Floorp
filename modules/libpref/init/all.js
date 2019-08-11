a8f9f4c0c2558311672f6027d791ec9fdfc42cdc
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-11 23:31:12 +0000

    Bug 1572582 - Merge datareporting-prefs.js into all.js. r=chutten
    This makes prefs definition simpler, more consistent, and less error-prone.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41298

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5654,6 +5654,17 @@ pref("fission.preserve_browsing_contexts", false);
 //  * userChrome.css
 pref("toolkit.legacyUserProfileCustomizations.stylesheets", false);
 
+#ifdef MOZ_DATA_REPORTING
+pref("datareporting.policy.dataSubmissionEnabled", true);
+pref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "0");
+pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 0);
+pref("datareporting.policy.dataSubmissionPolicyBypassNotification", false);
+pref("datareporting.policy.currentPolicyVersion", 2);
+pref("datareporting.policy.minimumPolicyVersion", 1);
+pref("datareporting.policy.minimumPolicyVersion.channel-beta", 2);
+pref("datareporting.policy.firstRunURL", "https://www.mozilla.org/privacy/firefox/");
+#endif
+
 pref("services.common.log.logger.rest.request", "Debug");
 pref("services.common.log.logger.rest.response", "Debug");
 pref("services.common.log.logger.tokenserverclient", "Debug");
