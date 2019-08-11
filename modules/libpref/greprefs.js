77c458a8eb8344e7ca4bbe3502c106dfefb8415a
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-11 23:31:30 +0000

    Bug 1572582 - Merge healthreport-prefs.js into all.js. r=chutten
    This makes prefs definition simpler, more consistent, and less error-prone.
    
    The patch also changes the form of the "not Android" condition to one used more
    widely in all.js.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41299

--- modules/libpref/greprefs.js
+++ modules/libpref/greprefs.js
@@ -1,7 +1,2 @@
 #include init/all.js
 #include ../../devtools/shared/preferences/devtools-shared.js
-#ifdef MOZ_SERVICES_HEALTHREPORT
-#if MOZ_WIDGET_TOOLKIT != android
-#include ../../toolkit/components/telemetry/healthreport-prefs.js
-#endif
-#endif
