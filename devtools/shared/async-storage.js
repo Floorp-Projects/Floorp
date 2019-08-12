69a84776d3cd758097d994d9b92c855d58415722
Sylvestre Ledru <sledru@mozilla.com>
2019-08-12 15:56:26 +0000

    Bug 1562642 - Part 5 - Add missing MPL2 headers in devtools/ r=jlast
    Depends on D41566
    
    Differential Revision: https://phabricator.services.mozilla.com/D41567

--- devtools/shared/async-storage.js
+++ devtools/shared/async-storage.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /**
  *
  * Adapted from https://github.com/mozilla-b2g/gaia/blob/f09993563fb5fec4393eb71816ce76cb00463190/shared/js/async_storage.js
