fa9fbefd3ca8b41eab59df3e04286c4cae120f70
Omkar Konaraddi <okonaraddi@mozilla.com>
2019-08-12 19:43:42 +0000

    Bug 1559501 - Implement icon support through setContextualTip r=adw,mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D39820

--- browser/components/extensions/parent/ext-urlbar.js
+++ browser/components/extensions/parent/ext-urlbar.js
@@ -15,6 +15,11 @@ var { ExtensionPreferencesManager } = ChromeUtils.import(
 );
 var { getSettingsAPI } = ExtensionPreferencesManager;
 
+var { ExtensionParent } = ChromeUtils.import(
+  "resource://gre/modules/ExtensionParent.jsm"
+);
+var { IconDetails } = ExtensionParent;
+
 ExtensionPreferencesManager.addSetting("openViewOnFocus", {
   prefNames: ["browser.urlbar.openViewOnFocus"],
   setCallback(value) {
@@ -28,6 +33,7 @@ ExtensionPreferencesManager.addSetting("engagementTelemetry", {
     return { [this.prefNames[0]]: value };
   },
 });
+
 // Keeps track of the id of the extension using the contextual tip.
 // Only one extension is allowed to use the contextual tip.
 // If another extension attempts to use the contextual tip,
@@ -42,6 +48,41 @@ this.urlbar = class extends ExtensionAPI {
       }
       idOfExtUsingContextualTip = null;
     }
+  }
+
+  /**
+   * Produces the styles to add to the icon element.
+   * These styles dictate the background images to use by default,
+   * for light/dark themes, and for low/high resolution screens.
+   *
+   * @param {object} icons
+   *   Object obtained from calling `IconDetails.normalize`.
+   *   It's a map of the icon size (e.g. 16) to the actual path of the icon.
+   *   The map can contain multiple size-to-path entries (one entry per icon).
+   * @returns {string} iconStyle
+   */
+  _getStyleFromIconsObject(icons) {
+    let getIcon = (icon, theme) => {
+      if (typeof icon === "object") {
+        return IconDetails.escapeUrl(icon[theme]);
+      }
+      return IconDetails.escapeUrl(icon);
+    };
+
+    let getStyle = (name, icon) => {
+      return `
+        --webextension-${name}: url("${getIcon(icon, "default")}");
+        --webextension-${name}-light: url("${getIcon(icon, "light")}");
+        --webextension-${name}-dark: url("${getIcon(icon, "dark")}");
+      `;
+    };
+
+    let icon16 = IconDetails.getPreferredIcon(icons, this.extension, 16).icon;
+    let icon32 = IconDetails.getPreferredIcon(icons, this.extension, 32).icon;
+    return `
+      ${getStyle("contextual-tip-icon", icon16)}
+      ${getStyle("contextual-tip-icon-2x", icon32)}
+    `;
   }
 
   /**
@@ -165,7 +206,8 @@ this.urlbar = class extends ExtensionAPI {
 
         contextualTip: {
           /**
-           * Sets the contextual tip's title, button title, and link title.
+           * Sets the contextual tip's icon,
+           * title, button title, and link title.
            *
            * @param {object} details
            *   If null, then the contextual tip will be hidden.
@@ -175,11 +217,44 @@ this.urlbar = class extends ExtensionAPI {
            *   If not given, no button is shown.
            * @param {string} [details.linkTitle]
            *   If not given, no link is shown.
+           * @param {object} [details.icon]
+           * @param {string} [details.icon.defaultIcon]
+           *   Path to an image file to be used as the default icon.
+           * @param {object} [details.icon.themeIcons]
+           * @param {string} [details.icon.themeIcons.light]
+           *   Path to an image file to be used as the icon for light themes.
+           * @param {string} [details.icon.themeIcons.dark]
+           *   Path to an image file to be used as the icon for dark themes.
+           * @param {integer} [details.icon.themeIcons.size]
+           *   Indicates the size of the icons.
            */
           set: details => {
             this._registerExtensionToUseContextualTip();
+
             const mostRecentWindow = windowTracker.getTopNormalWindow(context);
-            mostRecentWindow.gURLBar.view.setContextualTip(details);
+
+            const iconPathFromDetails = details.icon
+              ? details.icon.defaultIcon
+              : null;
+            const iconPathFromExtensionManifest =
+              context.extension.manifest.icons;
+            const icons = IconDetails.normalize(
+              {
+                path: iconPathFromDetails || iconPathFromExtensionManifest,
+                iconType: "contextualTip",
+                themeIcons: details.icon ? details.icon.themeIcons : null,
+              },
+              context.extension
+            );
+
+            const iconStyle = this._getStyleFromIconsObject(icons);
+
+            mostRecentWindow.gURLBar.view.setContextualTip({
+              iconStyle,
+              title: details.title,
+              buttonTitle: details.buttonTitle,
+              linkTitle: details.linkTitle,
+            });
           },
 
           /**
