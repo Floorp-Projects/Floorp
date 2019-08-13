2924fb2d9f2b49b623a6fba0d3fa31e4672fede0
tanhengyeow <E0032242@u.nus.edu>
2019-08-13 16:33:23 +0000

    Bug 1561631 - Limit number of displayed frames in WebSocket side panel. r=Harald,Honza
    Truncate displayed frames if they are over the specified limit. Offers a way to disable this behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39087
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/netmonitor/src/components/websockets/FrameListContent.js
+++ devtools/client/netmonitor/src/components/websockets/FrameListContent.js
@@ -9,16 +9,23 @@ const {
   createFactory,
 } = require("devtools/client/shared/vendor/react");
 const PropTypes = require("devtools/client/shared/vendor/react-prop-types");
+const Services = require("Services");
 const {
   connect,
 } = require("devtools/client/shared/redux/visibility-handler-connect");
+const { PluralForm } = require("devtools/shared/plural-form");
 const { getDisplayedFrames } = require("../../selectors/index");
-
 const dom = require("devtools/client/shared/vendor/react-dom-factories");
-const { table, tbody, div } = dom;
-
+const { table, tbody, tr, td, div, input, label } = dom;
 const { L10N } = require("../../utils/l10n");
 const FRAMES_EMPTY_TEXT = L10N.getStr("messagesEmptyText");
+const TOGGLE_MESSAGES_TRUNCATION = L10N.getStr("toggleMessagesTruncation");
+const TOGGLE_MESSAGES_TRUNCATION_TITLE = L10N.getStr(
+  "toggleMessagesTruncation.title"
+);
+const TRUNCATED_MESSAGES_WARNING = L10N.getStr(
+  "netmonitor.ws.truncated-messages.warning"
+);
 const Actions = require("../../actions/index");
 
 const { getSelectedFrame } = require("../../selectors/index");
@@ -46,6 +53,18 @@ class FrameListContent extends Component {
     };
   }
 
+  constructor(props) {
+    super(props);
+
+    this.framesLimit = Services.prefs.getIntPref(
+      "devtools.netmonitor.ws.displayed-frames.limit"
+    );
+    this.currentTruncatedNum = 0;
+    this.state = {
+      checked: false,
+    };
+  }
+
   onMouseDown(evt, item) {
     if (evt.button === LEFT_MOUSE_BUTTON) {
       this.props.selectFrame(item);
@@ -66,23 +85,96 @@ class FrameListContent extends Component {
       .filter(([name, isVisible]) => isVisible)
       .map(([name]) => name);
 
-    return table(
-      { className: "ws-frames-list-table" },
-      FrameListHeader(),
-      tbody(
-        {
-          className: "ws-frames-list-body",
-        },
-        frames.map((item, index) =>
-          FrameListItem({
-            key: "ws-frame-list-item-" + index,
-            item,
-            index,
-            isSelected: item === selectedFrame,
-            onMouseDown: evt => this.onMouseDown(evt, item),
-            connector,
-            visibleColumns,
-          })
+    let displayedFrames;
+    let MESSAGES_TRUNCATED;
+    const shouldTruncate = frames.length > this.framesLimit;
+    if (shouldTruncate) {
+      // If the checkbox is checked, we display all frames after the currentedTruncatedNum limit.
+      // If the checkbox is unchecked, we display all frames after the framesLimit.
+      this.currentTruncatedNum = this.state.checked
+        ? this.currentTruncatedNum
+        : frames.length - this.framesLimit;
+      displayedFrames = frames.slice(this.currentTruncatedNum);
+
+      MESSAGES_TRUNCATED = PluralForm.get(
+        this.currentTruncatedNum,
+        L10N.getStr("netmonitor.ws.truncated-messages.warning")
+      ).replace("#1", this.currentTruncatedNum);
+    } else {
+      displayedFrames = frames;
+    }
+
+    return div(
+      {},
+      table(
+        { className: "ws-frames-list-table" },
+        FrameListHeader(),
+        tr(
+          {
+            tabIndex: 0,
+          },
+          td(
+            {
+              className: "truncated-messages-cell",
+              colSpan: visibleColumns.length,
+            },
+            shouldTruncate &&
+              div(
+                {
+                  className: "truncated-messages-header",
+                },
+                div(
+                  {
+                    className: "truncated-messages-container",
+                  },
+                  div({
+                    className: "truncated-messages-warning-icon",
+                    title: TRUNCATED_MESSAGES_WARNING,
+                  }),
+                  div(
+                    {
+                      className: "truncated-message",
+                      title: MESSAGES_TRUNCATED,
+                    },
+                    MESSAGES_TRUNCATED
+                  )
+                ),
+                label(
+                  {
+                    className: "truncated-messages-checkbox-label",
+                    title: TOGGLE_MESSAGES_TRUNCATION_TITLE,
+                  },
+                  input({
+                    type: "checkbox",
+                    className: "truncation-checkbox",
+                    title: TOGGLE_MESSAGES_TRUNCATION_TITLE,
+                    checked: this.state.checked,
+                    onClick: () => {
+                      this.setState({
+                        checked: !this.state.checked,
+                      });
+                    },
+                  }),
+                  TOGGLE_MESSAGES_TRUNCATION
+                )
+              )
+          )
+        ),
+        tbody(
+          {
+            className: "ws-frames-list-body",
+          },
+          displayedFrames.map((item, index) =>
+            FrameListItem({
+              key: "ws-frame-list-item-" + index,
+              item,
+              index,
+              isSelected: item === selectedFrame,
+              onMouseDown: evt => this.onMouseDown(evt, item),
+              connector,
+              visibleColumns,
+            })
+          )
         )
       )
     );
