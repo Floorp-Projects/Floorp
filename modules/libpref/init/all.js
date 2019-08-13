b9759c4afa0693bbc4ec1bec819636efd236bd87
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-13 23:49:39 +0000

    Bug 1572336 - Merge marionette.js into all.js. r=ato
    Differential Revision: https://phabricator.services.mozilla.com/D41151

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5761,3 +5761,36 @@ pref("services.sync.engine.passwords.validation.maxRecords", 1000);
 // a sync.
 pref("services.sync.maxResyncs", 5);
 #endif // MOZ_SERVICES_SYNC
+
+// Marionette is the remote protocol that lets OOP programs communicate with,
+// instrument, and control Gecko.
+
+// Starts and stops the Marionette server.
+pref("marionette.enabled", false);
+
+// Delay server startup until a modal dialogue has been clicked to allow time
+// for user to set breakpoints in the Browser Toolbox.
+pref("marionette.debugging.clicktostart", false);
+
+// Verbosity of Marionette logger repository.
+//
+// Available levels are, in descending order of severity, "trace", "debug",
+// "config", "info", "warn", "error", and "fatal". The value is treated
+// case-insensitively.
+pref("marionette.log.level", "Info");
+
+// Certain log messages that are known to be long are truncated. This
+// preference causes them to not be truncated.
+pref("marionette.log.truncate", true);
+
+// Port to start Marionette server on.
+pref("marionette.port", 2828);
+
+// Sets recommended automation preferences when Marionette is started.
+pref("marionette.prefs.recommended", true);
+
+// Whether content scripts can be safely reused.
+//
+// Deprecated and scheduled for removal with
+// https://bugzil.la/marionette-window-tracking
+pref("marionette.contentListener", false);
