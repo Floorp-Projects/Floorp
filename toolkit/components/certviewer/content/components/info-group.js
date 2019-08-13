7915f94e5793419f7c8176becb0e532463ea8bf4
dleblanccyr <danielleleb12@gmail.com>
2019-08-13 17:51:13 +0000

    Bug 1572189 - Adds padding between sub-sections. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D41095

--- toolkit/components/certviewer/content/components/info-group.js
+++ toolkit/components/certviewer/content/components/info-group.js
@@ -29,6 +29,12 @@ export class InfoGroup extends HTMLElement {
       this.item.sectionTitle.replace(/\s+/g, "-").toLowerCase()
     );
 
+    // Adds a class with the section title's name, to make
+    // it easier to find when highlighting errors.
+    this.classList.add(
+      this.item.sectionTitle.replace(/\s+/g, "-").toLowerCase()
+    );
+
     for (let i = 0; i < this.item.sectionItems.length; i++) {
       this.shadowRoot.append(new InfoItem(this.item.sectionItems[i]));
     }
