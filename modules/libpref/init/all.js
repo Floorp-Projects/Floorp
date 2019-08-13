87899da116201ae103b3d297829c7d68dcca4022
kriswright <kwright@mozilla.com>
2019-08-13 22:52:38 +0000

    Bug 1573268 - Convert layout.css.ruby.intercharacter.enabled to static pref. r=njn
    Converts layout.css.ruby.intercharacter.enabled to a static pref and removes the associated function nsLayoutUtils::IsInterCharacterRubyEnabled(). Also removes the macro INTERCHARACTER_RUBY_ENABLED_PREF_NAME, since it was only being used to add the varcache pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41653

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2952,9 +2952,6 @@ pref("layout.css.scroll-behavior.spring-constant", "250.0");
 // at the greatest speed without overshooting.
 pref("layout.css.scroll-behavior.damping-ratio", "1.0");
 
-// Are inter-character ruby annotations enabled?
-pref("layout.css.ruby.intercharacter.enabled", false);
-
 // pref for which side vertical scrollbars should be on
 // 0 = end-side in UI direction
 // 1 = end-side in document/content direction
