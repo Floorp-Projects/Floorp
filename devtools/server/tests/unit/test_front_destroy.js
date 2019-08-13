b6285ab675de6e8bbe43462f489eb80bd6b208e8
Perry Jiang <perry@mozilla.com>
2019-08-13 04:04:58 +0000

    Bug 1231213 - Various changes to existing tests. r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D32078

--- devtools/server/tests/unit/test_front_destroy.js
+++ devtools/server/tests/unit/test_front_destroy.js
@@ -7,6 +7,12 @@
 
 "use strict";
 
+// HACK: ServiceWorkerManager requires the "profile-change-teardown" to cleanly
+// shutdown, and setting _profileInitialized to `true` will trigger those
+// notifications (see /testing/xpcshell/head.js).
+// eslint-disable-next-line no-undef
+_profileInitialized = true;
+
 add_task(async function test() {
   DebuggerServer.init();
   DebuggerServer.registerAllActors();
