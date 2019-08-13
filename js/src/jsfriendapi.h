1e7546a42e955260535a6e45c8eec533656835f7
Andrew McCreight <continuation@gmail.com>
2019-08-13 19:10:11 +0000

    Bug 1559489, part 1 - Add IsDOMRemoteProxyObject. r=tcampbell
    This is just a convenience method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38340
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1255,6 +1255,8 @@ inline bool DOMProxyIsShadowing(DOMProxyShadowsResult result) {
 
 const void* GetDOMRemoteProxyHandlerFamily();
 
+extern JS_FRIEND_API bool IsDOMRemoteProxyObject(JSObject* object);
+
 // Callbacks and other information for use by the JITs when optimizing accesses
 // on xray wrappers.
 struct XrayJitInfo {
