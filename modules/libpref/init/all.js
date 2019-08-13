6f7173ddbd4ad02128ad42499f3b0e0f9a0f7986
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-13 23:49:39 +0000

    Bug 1572622 - Merge remote.js into all.js. r=ato
    This makes prefs definition simpler, more consistent, and less error-prone.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41324

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5794,3 +5794,18 @@ pref("marionette.prefs.recommended", true);
 // Deprecated and scheduled for removal with
 // https://bugzil.la/marionette-window-tracking
 pref("marionette.contentListener", false);
+
+// Indicates whether the remote agent is enabled. If it is false, the remote
+// agent will not be loaded.
+pref("remote.enabled", false);
+
+// Limits remote agent to listen on loopback devices, e.g. 127.0.0.1,
+// localhost, and ::1.
+pref("remote.force-local", true);
+
+// Defines the verbosity of the internal logger.
+//
+// Available levels are, in descending order of severity, "Trace", "Debug",
+// "Config", "Info", "Warn", "Error", and "Fatal". The value is treated
+// case-sensitively.
+pref("remote.log.level", "Info");
