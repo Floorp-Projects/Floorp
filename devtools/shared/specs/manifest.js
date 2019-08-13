22058c10e7d7a17898dfa8ce245d44a9eb43f4a1
Belén Albeza <balbeza@mozilla.com>
2019-08-13 14:43:24 +0000

    Bug 1565973 - Part 1: Implement manifest actor in the server r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D40979

---
+++ devtools/shared/specs/manifest.js
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const { generateActorSpec, RetVal } = require("devtools/shared/protocol");
+
+const manifestSpec = generateActorSpec({
+  typeName: "manifest",
+  methods: {
+    fetchCanonicalManifest: {
+      request: {},
+      response: RetVal("json"),
+    },
+  },
+});
+
+exports.manifestSpec = manifestSpec;
