08d4f419c9d2847150e0b39d67063a0e5df9e405
Aaron Klotz <aklotz@mozilla.com>
2019-08-14 03:46:41 +0000

    Bug 1573273: Add ProfilerLabelBegin and ProfilerLabelEnd to mozglue; r=gerald
    While mozglue continues to be the correct location for calling the affected
    code in this patch, the calls requiring profiler labels will soon be
    originating from firefox.exe via the launcher process.
    
    mozglue will be supplying the launcher process with an interface that consists
    of what are effectively "OnBeginDllLoad" and "OnEndDllLoad" callback
    notifications; obviously an RAII class is not going to be useful for that case.
    
    We still want to keep the RAII stuff around, however, since we still need it
    for cases where we need to fall back to using the legacy DLL blocklist.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41807

--- mozglue/misc/AutoProfilerLabel.h
+++ mozglue/misc/AutoProfilerLabel.h
@@ -9,6 +9,7 @@
 
 #include "mozilla/Attributes.h"
 #include "mozilla/GuardObjects.h"
+#include "mozilla/Tuple.h"
 #include "mozilla/Types.h"
 
 // The Gecko Profiler defines AutoProfilerLabel, an RAII class for
@@ -53,6 +54,17 @@ class MOZ_RAII AutoProfilerLabel {
   // context from one generation to the next.
   uint32_t mGeneration;
 };
+
+using ProfilerLabel = Tuple<void*, uint32_t>;
+
+bool IsProfilerPresent();
+ProfilerLabel ProfilerLabelBegin(const char* aLabelName,
+                                 const char* aDynamicString, void* aSp);
+void ProfilerLabelEnd(const ProfilerLabel& aLabel);
+
+inline bool IsValidProfilerLabel(const ProfilerLabel& aLabel) {
+  return !!Get<0>(aLabel);
+}
 
 #endif
 
