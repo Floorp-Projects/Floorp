32c4c2e21067b53a88eda9ce077dcb0ea8d08740
Kris Maglione <maglione.k@gmail.com>
2019-08-14 16:13:13 -0700

    Bug 1561705: Part 1 - Add JSWindowActor.actorCreated callback. r=jdai
    The JSWindowActor constructor is called before the actor is fully initialized,
    which means it can't do things like send messages or access its content
    window. This patch adds a new callback which can do those things immediately
    after the actor is created.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42178
    
    --HG--
    extra : rebase_source : ae5b99e690f7c1ebc38726a20bf0d4cf1334cfbe

--- dom/ipc/JSWindowActor.h
+++ dom/ipc/JSWindowActor.h
@@ -39,7 +39,7 @@ class JSWindowActor : public nsISupports, public nsWrapperCache {
   JSWindowActor();
 
   enum class Type { Parent, Child };
-  enum class DestroyCallbackFunction { WillDestroy, DidDestroy };
+  enum class CallbackFunction { WillDestroy, DidDestroy, ActorCreated };
 
   const nsString& Name() const { return mName; }
 
@@ -76,7 +76,7 @@ class JSWindowActor : public nsISupports, public nsWrapperCache {
 
   void AfterDestroy();
 
-  void DestroyCallback(DestroyCallbackFunction willDestroy);
+  void InvokeCallback(CallbackFunction willDestroy);
 
  private:
   void ReceiveMessageOrQuery(JSContext* aCx,
