eab4a17151636bee67f734b18d59eff0013a5381
Sebastian Streich <sstreich@mozilla.com>
2019-08-14 13:59:06 +0000

    Bug 1571742 - Move Nosniff-Check into Sniffers r=ckerschb,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D41504

--- netwerk/base/nsNetUtil.cpp
+++ netwerk/base/nsNetUtil.cpp
@@ -2694,15 +2694,6 @@ nsresult NS_GenerateHostPort(const nsCString& host, int32_t port,
 void NS_SniffContent(const char* aSnifferType, nsIRequest* aRequest,
                      const uint8_t* aData, uint32_t aLength,
                      nsACString& aSniffedType) {
-  // In case XCTO nosniff was present, we could just skip sniffing here
-  nsCOMPtr<nsIChannel> channel = do_QueryInterface(aRequest);
-  if (channel) {
-    nsCOMPtr<nsILoadInfo> loadInfo = channel->LoadInfo();
-    if (loadInfo->GetSkipContentSniffing()) {
-      aSniffedType.Truncate();
-      return;
-    }
-  }
   typedef nsCategoryCache<nsIContentSniffer> ContentSnifferCache;
   extern ContentSnifferCache* gNetSniffers;
   extern ContentSnifferCache* gDataSniffers;
