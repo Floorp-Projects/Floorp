f98858dcc7844d44abc13128b6cf65aa620e5c54
Perry Jiang <perry@mozilla.com>
2019-08-14 16:19:40 +0000

    Bug 1231213 - Implement IPCInternal{Request,Response}. r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D26163
    
    --HG--
    extra : moz-landing-system : lando

--- dom/fetch/InternalRequest.cpp
+++ dom/fetch/InternalRequest.cpp
@@ -16,6 +16,8 @@
 
 #include "mozilla/dom/WorkerCommon.h"
 #include "mozilla/dom/WorkerPrivate.h"
+#include "mozilla/ipc/IPCStreamUtils.h"
+#include "mozilla/ipc/PBackgroundChild.h"
 
 namespace mozilla {
 namespace dom {
@@ -171,7 +173,72 @@ InternalRequest::InternalRequest(const InternalRequest& aOther)
   // NOTE: does not copy body stream... use the fallible Clone() for that
 }
 
+InternalRequest::InternalRequest(const IPCInternalRequest& aIPCRequest)
+    : mMethod(aIPCRequest.method()),
+      mURLList(aIPCRequest.urlList()),
+      mHeaders(new InternalHeaders(aIPCRequest.headers(),
+                                   aIPCRequest.headersGuard())),
+      mBodyStream(mozilla::ipc::DeserializeIPCStream(aIPCRequest.body())),
+      mBodyLength(aIPCRequest.bodySize()),
+      mPreferredAlternativeDataType(aIPCRequest.preferredAlternativeDataType()),
+      mContentPolicyType(
+          static_cast<nsContentPolicyType>(aIPCRequest.contentPolicyType())),
+      mReferrer(aIPCRequest.referrer()),
+      mReferrerPolicy(aIPCRequest.referrerPolicy()),
+      mMode(aIPCRequest.requestMode()),
+      mCredentialsMode(aIPCRequest.requestCredentials()),
+      mCacheMode(aIPCRequest.cacheMode()),
+      mRedirectMode(aIPCRequest.requestRedirect()),
+      mIntegrity(aIPCRequest.integrity()),
+      mFragment(aIPCRequest.fragment()),
+      mCreatedByFetchEvent(aIPCRequest.createdByFetchEvent()) {
+  if (aIPCRequest.principalInfo()) {
+    mPrincipalInfo = MakeUnique<mozilla::ipc::PrincipalInfo>(
+        aIPCRequest.principalInfo().ref());
+  }
+}
+
 InternalRequest::~InternalRequest() {}
+
+template void InternalRequest::ToIPC<mozilla::ipc::PBackgroundChild>(
+    IPCInternalRequest* aIPCRequest, mozilla::ipc::PBackgroundChild* aManager,
+    UniquePtr<mozilla::ipc::AutoIPCStream>& aAutoStream);
+
+template <typename M>
+void InternalRequest::ToIPC(
+    IPCInternalRequest* aIPCRequest, M* aManager,
+    UniquePtr<mozilla::ipc::AutoIPCStream>& aAutoStream) {
+  MOZ_ASSERT(aIPCRequest);
+  MOZ_ASSERT(aManager);
+  MOZ_ASSERT(!mURLList.IsEmpty());
+
+  aIPCRequest->method() = mMethod;
+  aIPCRequest->urlList() = mURLList;
+  mHeaders->ToIPC(aIPCRequest->headers(), aIPCRequest->headersGuard());
+
+  if (mBodyStream) {
+    aAutoStream.reset(new mozilla::ipc::AutoIPCStream(aIPCRequest->body()));
+    DebugOnly<bool> ok = aAutoStream->Serialize(mBodyStream, aManager);
+    MOZ_ASSERT(ok);
+  }
+
+  aIPCRequest->bodySize() = mBodyLength;
+  aIPCRequest->preferredAlternativeDataType() = mPreferredAlternativeDataType;
+  aIPCRequest->contentPolicyType() = static_cast<uint32_t>(mContentPolicyType);
+  aIPCRequest->referrer() = mReferrer;
+  aIPCRequest->referrerPolicy() = mReferrerPolicy;
+  aIPCRequest->requestMode() = mMode;
+  aIPCRequest->requestCredentials() = mCredentialsMode;
+  aIPCRequest->cacheMode() = mCacheMode;
+  aIPCRequest->requestRedirect() = mRedirectMode;
+  aIPCRequest->integrity() = mIntegrity;
+  aIPCRequest->fragment() = mFragment;
+  aIPCRequest->createdByFetchEvent() = mCreatedByFetchEvent;
+
+  if (mPrincipalInfo) {
+    aIPCRequest->principalInfo().emplace(*mPrincipalInfo);
+  }
+}
 
 void InternalRequest::SetContentPolicyType(
     nsContentPolicyType aContentPolicyType) {
