3cae0754d755ab49517ed5c3f8cb8d2cf4e2d1ce
Gabriele Svelto <gsvelto@mozilla.com>
2019-08-15 12:06:51 +0000

    Bug 1571711 - Factorize crash handling out of the various process IPC classes r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D41657

--- gfx/vr/ipc/VRChild.cpp
+++ gfx/vr/ipc/VRChild.cpp
@@ -14,7 +14,6 @@
 #include "mozilla/Telemetry.h"
 #include "mozilla/VsyncDispatcher.h"
 #include "mozilla/dom/MemoryReportRequest.h"
-#include "mozilla/ipc/CrashReporterHost.h"
 
 namespace mozilla {
 namespace gfx {
@@ -91,12 +90,7 @@ mozilla::ipc::IPCResult VRChild::RecvFinishMemoryReport(
 
 void VRChild::ActorDestroy(ActorDestroyReason aWhy) {
   if (aWhy == AbnormalShutdown) {
-    if (mCrashReporter) {
-      mCrashReporter->GenerateCrashReport(OtherPid());
-      mCrashReporter = nullptr;
-    } else {
-      CrashReporter::FinalizeOrphanedMinidump(OtherPid(), GeckoProcessType_VR);
-    }
+    GenerateCrashReport(OtherPid());
 
     Telemetry::Accumulate(
         Telemetry::SUBPROCESS_ABNORMAL_ABORT,
@@ -172,14 +166,6 @@ mozilla::ipc::IPCResult VRChild::RecvOpenVRControllerManifestPathToParent(
 mozilla::ipc::IPCResult VRChild::RecvInitComplete() {
   // We synchronously requested VR parameters before this arrived.
   mVRReady = true;
-  return IPC_OK();
-}
-
-mozilla::ipc::IPCResult VRChild::RecvInitCrashReporter(
-    Shmem&& aShmem, const NativeThreadId& aThreadId) {
-  mCrashReporter = MakeUnique<ipc::CrashReporterHost>(GeckoProcessType_VR,
-                                                      aShmem, aThreadId);
-
   return IPC_OK();
 }
 
