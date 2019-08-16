b18534fc4b2a31171ba9b34a365479f1df2f8319
Jonathan Kew <jkew@mozilla.com>
2019-08-16 07:24:46 +0000

    Bug 1568514 - Add telemetry histograms to report hyphenation memory footprint and resource load times. r=heycam
    Differential Revision: https://phabricator.services.mozilla.com/D39323

--- intl/hyphenation/glue/nsHyphenationManager.h
+++ intl/hyphenation/glue/nsHyphenationManager.h
@@ -16,8 +16,11 @@ class nsHyphenator;
 class nsAtom;
 class nsIURI;
 
-class nsHyphenationManager {
+class nsHyphenationManager : public nsIObserver {
  public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+
   nsHyphenationManager();
 
   already_AddRefed<nsHyphenator> GetHyphenator(nsAtom* aLocale);
@@ -29,17 +32,9 @@ class nsHyphenationManager {
   size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf);
 
  private:
-  ~nsHyphenationManager();
+  virtual ~nsHyphenationManager();
 
  protected:
-  class MemoryPressureObserver final : public nsIObserver {
-    ~MemoryPressureObserver() {}
-
-   public:
-    NS_DECL_ISUPPORTS
-    NS_DECL_NSIOBSERVER
-  };
-
   void LoadPatternList();
   void LoadPatternListFromOmnijar(mozilla::Omnijar::Type aType);
   void LoadPatternListFromDir(nsIFile* aDir);
