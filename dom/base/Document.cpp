60dcc3a69c02e7f48bb8eb92ad62483a0e08c9ef
Razvan Maries <rmaries@mozilla.com>
2019-08-17 00:52:46 +0300

    Backed out changeset e2097b17e465 (bug 1573842) for build bustages. a=backout

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -14469,8 +14469,7 @@ void Document::ReportUseCounters(UseCounterReportKind aKind) {
       (IsContentDocument() || IsResourceDoc())) {
     nsCOMPtr<nsIURI> uri;
     NodePrincipal()->GetURI(getter_AddRefs(uri));
-    if (!uri || uri->SchemeIs("about") || uri->SchemeIs("chrome") ||
-        uri->SchemeIs("resource")) {
+    if (!uri || MightBeAboutOrChromeScheme(uri)) {
       return;
     }
 
