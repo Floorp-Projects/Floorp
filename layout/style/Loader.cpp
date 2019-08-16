7670c060f1ddaac0fe13c16cdc40e1f11456346e
Emilio Cobos Álvarez <emilio@crisal.io>
2019-08-16 10:56:22 +0000

    Bug 1571530 - Remove sheets from the <link> cache when they have been touched by CSS OM. r=heycam
    Differential Revision: https://phabricator.services.mozilla.com/D41563

--- layout/style/Loader.cpp
+++ layout/style/Loader.cpp
@@ -535,6 +535,9 @@ auto Loader::Sheets::Lookup(SheetLoadDataHashKey& aKey, bool aSyncLoad)
       return MakeTuple(std::move(clone), SheetState::Complete);
     }
     LOG(("    Not cloning due to forced unique inner"));
+    // Remove it now that we know that we're never going to use this stylesheet
+    // again.
+    lookup.Remove();
   }
 
   if (aSyncLoad) {
