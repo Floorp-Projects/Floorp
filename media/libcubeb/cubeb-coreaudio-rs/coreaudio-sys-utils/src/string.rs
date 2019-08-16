acb13e575a3999d580439e1a0492bcb1ec2892dc
Chun-Min Chang <chun.m.chang@gmail.com>
2019-08-16 22:20:30 +0000

    Bug 1574644 - Update cubeb-coreaudio-rs to version 71faddb. r=kinetik
    Differential Revision: https://phabricator.services.mozilla.com/D42382

--- media/libcubeb/cubeb-coreaudio-rs/coreaudio-sys-utils/src/string.rs
+++ media/libcubeb/cubeb-coreaudio-rs/coreaudio-sys-utils/src/string.rs
@@ -1,7 +1,11 @@
-use core_foundation_sys::base::{kCFAllocatorDefault, kCFAllocatorNull, Boolean, CFIndex};
+use core_foundation_sys::base::{
+    kCFAllocatorDefault, kCFAllocatorNull, Boolean, CFIndex, CFRange, CFRelease,
+};
 use core_foundation_sys::string::{
     kCFStringEncodingUTF8, CFStringCreateWithBytes, CFStringCreateWithBytesNoCopy,
+    CFStringGetBytes, CFStringGetLength, CFStringRef,
 };
+use std::ffi::CString;
 
 pub fn cfstringref_from_static_string(string: &'static str) -> coreaudio_sys::CFStringRef {
     // Set deallocator to kCFAllocatorNull to prevent the the memory of the parameter `string`
@@ -32,11 +36,95 @@ pub fn cfstringref_from_string(string: &str) -> coreaudio_sys::CFStringRef {
     cfstringref as coreaudio_sys::CFStringRef
 }
 
+#[derive(Debug)]
+pub struct StringRef(CFStringRef);
+impl StringRef {
+    pub fn new(string_ref: CFStringRef) -> Self {
+        assert!(!string_ref.is_null());
+        Self(string_ref)
+    }
+
+    pub fn to_string(&self) -> String {
+        String::from_utf8(utf8_from_cfstringref(self.0)).expect("convert bytes to a String")
+    }
+
+    pub fn into_string(self) -> String {
+        self.to_string()
+    }
+
+    pub fn to_cstring(&self) -> CString {
+        unsafe {
+            // Assume that bytes doesn't contain `0` in the middle.
+            CString::from_vec_unchecked(utf8_from_cfstringref(self.0))
+        }
+    }
+
+    pub fn into_cstring(self) -> CString {
+        self.to_cstring()
+    }
+
+    pub fn get_raw(&self) -> CFStringRef {
+        self.0
+    }
+}
+
+impl Drop for StringRef {
+    fn drop(&mut self) {
+        use std::os::raw::c_void;
+        unsafe { CFRelease(self.0 as *mut c_void) };
+    }
+}
+
+fn utf8_from_cfstringref(string_ref: CFStringRef) -> Vec<u8> {
+    use std::ptr;
+
+    assert!(!string_ref.is_null());
+
+    let length: CFIndex = unsafe { CFStringGetLength(string_ref) };
+    assert!(length > 0);
+
+    // Get the buffer size of the string.
+    let range: CFRange = CFRange {
+        location: 0,
+        length,
+    };
+    let mut size: CFIndex = 0;
+    let mut converted_chars: CFIndex = unsafe {
+        CFStringGetBytes(
+            string_ref,
+            range,
+            kCFStringEncodingUTF8,
+            0,
+            false as Boolean,
+            ptr::null_mut() as *mut u8,
+            0,
+            &mut size,
+        )
+    };
+    assert!(converted_chars > 0 && size > 0);
+
+    // Then, allocate the buffer with the required size and actually copy data into it.
+    let mut buffer = vec![b'\x00'; size as usize];
+    converted_chars = unsafe {
+        CFStringGetBytes(
+            string_ref,
+            range,
+            kCFStringEncodingUTF8,
+            0,
+            false as Boolean,
+            buffer.as_mut_ptr(),
+            size,
+            ptr::null_mut() as *mut CFIndex,
+        )
+    };
+    assert!(converted_chars > 0);
+
+    buffer
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use core_foundation_sys::base::{CFRange, CFRelease};
-    use core_foundation_sys::string::{CFStringGetBytes, CFStringGetLength, CFStringRef};
 
     const STATIC_STRING: &str = "static string for testing";
 
@@ -44,7 +132,11 @@ mod test {
     fn test_create_static_cfstring_ref() {
         let stringref =
             StringRef::new(cfstringref_from_static_string(STATIC_STRING) as CFStringRef);
-        assert_eq!(STATIC_STRING, stringref.into_string());
+        assert_eq!(STATIC_STRING, stringref.to_string());
+        assert_eq!(
+            CString::new(STATIC_STRING).unwrap(),
+            stringref.into_cstring()
+        );
         // TODO: Find a way to check the string's inner pointer is same.
     }
 
@@ -52,77 +144,8 @@ mod test {
     fn test_create_cfstring_ref() {
         let expected = "Rustaceans 🦀";
         let stringref = StringRef::new(cfstringref_from_string(expected) as CFStringRef);
-        assert_eq!(expected, stringref.into_string());
+        assert_eq!(expected, stringref.to_string());
+        assert_eq!(CString::new(expected).unwrap(), stringref.into_cstring());
         // TODO: Find a way to check the string's inner pointer is different.
-    }
-
-    #[derive(Debug)]
-    struct StringRef(CFStringRef);
-    impl StringRef {
-        fn new(string_ref: CFStringRef) -> Self {
-            assert!(!string_ref.is_null());
-            Self(string_ref)
-        }
-
-        fn to_string(&self) -> String {
-            String::from_utf8(utf8_from_cfstringref(self.0)).unwrap()
-        }
-
-        fn into_string(self) -> String {
-            self.to_string()
-        }
-    }
-
-    impl Drop for StringRef {
-        fn drop(&mut self) {
-            use std::os::raw::c_void;
-            unsafe { CFRelease(self.0 as *mut c_void) };
-        }
-    }
-
-    fn utf8_from_cfstringref(string_ref: CFStringRef) -> Vec<u8> {
-        use std::ptr;
-
-        assert!(!string_ref.is_null());
-
-        let length: CFIndex = unsafe { CFStringGetLength(string_ref) };
-        assert!(length > 0);
-
-        let range: CFRange = CFRange {
-            location: 0,
-            length,
-        };
-        let mut size: CFIndex = 0;
-        let mut converted_chars: CFIndex = unsafe {
-            CFStringGetBytes(
-                string_ref,
-                range,
-                kCFStringEncodingUTF8,
-                0,
-                false as Boolean,
-                ptr::null_mut() as *mut u8,
-                0,
-                &mut size,
-            )
-        };
-        assert!(converted_chars > 0 && size > 0);
-
-        // Then, allocate the buffer with the required size and actually copy data into it.
-        let mut buffer = vec![b'\x00'; size as usize];
-        converted_chars = unsafe {
-            CFStringGetBytes(
-                string_ref,
-                range,
-                kCFStringEncodingUTF8,
-                0,
-                false as Boolean,
-                buffer.as_mut_ptr(),
-                size,
-                ptr::null_mut() as *mut CFIndex,
-            )
-        };
-        assert!(converted_chars > 0);
-
-        buffer
     }
 }
