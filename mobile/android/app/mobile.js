e056bfc7de8a87da8bbb24822d12708779ff1ff9
Robert Helmer <rhelmer@mozilla.com>
2019-08-16 13:52:14 +0000

    Bug 1573285 - set system add-on enable pref on by default for Fennec r=snorp
    This was originally added in bug 1428459 to separate system add-on updates from app updates, but the pref only made it to desktop and not mobile.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41795

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -810,6 +810,7 @@ pref("media.navigator.permission.device", true);
 
 // Allow system add-on updates
 pref("extensions.systemAddon.update.url", "https://aus5.mozilla.org/update/3/SystemAddons/%VERSION%/%BUILD_ID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/update.xml");
+pref("extensions.systemAddon.update.enabled", true);
 
 // E10s stuff. We don't support 'file' or 'priveleged' process types.
 pref("browser.tabs.remote.separateFileUriProcess", false);
