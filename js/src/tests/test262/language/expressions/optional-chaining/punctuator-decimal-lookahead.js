ebba1e8a027ae287d9d132eaf037137e22029a9d
André Bargull <andre.bargull@gmail.com>
2019-08-16 20:20:17 +0000

    Bug 1574180 - Part 6: Update test262. r=khyperia
    Update test262 to get the changes from <https://github.com/tc39/test262/pull/2299>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42158

---
+++ js/src/tests/test262/language/expressions/optional-chaining/punctuator-decimal-lookahead.js
@@ -0,0 +1,18 @@
+// |reftest| skip -- optional-chaining is not supported
+// Copyright 2019 Google, Inc.  All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: prod-OptionalExpression
+description: >
+  ternary operation with decimal does not evaluate as optional chain 
+info: |
+  Punctuators
+    OptionalChainingPunctuator::
+      ?.[lookahead ∉ DecimalDigit]
+features: [optional-chaining]
+---*/
+
+const value = true ?.30 : false;
+assert.sameValue(.30, value);
+
+reportCompare(0, 0);
---
+++ js/src/tests/test262/language/module-code/top-level-await/syntax/await-expr-func-expression.js
@@ -0,0 +1,48 @@
+// |reftest| skip module -- top-level-await is not supported
+// Copyright (C) 2019 Leo Balter. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: >
+  Valid syntax for top level await.
+  AwaitExpression StringLiteral
+info: |
+  ModuleItem:
+    StatementListItem[~Yield, +Await, ~Return]
+
+  ...
+
+  ExpressionStatement[Yield, Await]:
+    [lookahead ∉ { {, function, async [no LineTerminator here] function, class, let [ }]
+      Expression[+In, ?Yield, ?Await];
+
+  UnaryExpression[Yield, Await]
+    [+Await]AwaitExpression[?Yield]
+
+  AwaitExpression[Yield]:
+    await UnaryExpression[?Yield, +Await]
+
+  ...
+
+  PrimaryExpression[Yield, Await]:
+    this
+    IdentifierReference[?Yield, ?Await]
+    Literal
+    ArrayLiteral[?Yield, ?Await]
+    ObjectLiteral[?Yield, ?Await]
+    FunctionExpression
+    ClassExpression[?Yield, ?Await]
+    GeneratorExpression
+    AsyncFunctionExpression
+    AsyncGeneratorExpression
+    RegularExpressionLiteral
+    TemplateLiteral[?Yield, ?Await, ~Tagged]
+    CoverParenthesizedExpressionAndArrowParameterList[?Yield, ?Await]
+esid: prod-AwaitExpression
+flags: [module]
+features: [top-level-await]
+---*/
+
+await function() {};
+
+reportCompare(0, 0);
