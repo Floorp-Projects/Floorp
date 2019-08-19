bf0034023c6998b16ea4084952760bea4eabdd86
Nicolas Silva <nsilva@mozilla.com>
2019-08-19 11:16:27 +0000

    Bug 1562091 - Partial backout of bug 1521027. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D42468

--- image/VectorImage.cpp
+++ image/VectorImage.cpp
@@ -1102,8 +1102,7 @@ already_AddRefed<SourceSurface> VectorImage::CreateSurface(
   auto frame = MakeNotNull<RefPtr<imgFrame>>();
   nsresult rv = frame->InitWithDrawable(
       aSVGDrawable, aParams.size, SurfaceFormat::B8G8R8A8,
-      SamplingFilter::POINT, aParams.flags, backend,
-      aParams.context ? aParams.context->GetDrawTarget() : nullptr);
+      SamplingFilter::POINT, aParams.flags, backend);
 
   // If we couldn't create the frame, it was probably because it would end
   // up way too big. Generally it also wouldn't fit in the cache, but the prefs
