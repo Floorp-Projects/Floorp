da5524fb090cd985f0fee519ae7ff2d1a66604db
dleblanccyr <danielleleb12@gmail.com>
2019-08-19 19:44:34 +0000

    Bug 1573473 - Creates global function to make strings kebab-case. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D41818

--- toolkit/components/certviewer/content/components/certificate-section.js
+++ toolkit/components/certviewer/content/components/certificate-section.js
@@ -5,6 +5,7 @@
 import { updateSelectedItem } from "../certviewer.js";
 import { InfoGroup } from "./info-group.js";
 import { ErrorSection } from "./error-section.js";
+import { normalizeToKebabCase } from "../utils.js";
 
 class CertificateSection extends HTMLElement {
   constructor(certs, error) {
@@ -125,7 +126,7 @@ class CertificateSection extends HTMLElement {
   createTabSection(tabName, i, certificateTabs) {
     let tab = document.createElement("button");
     tab.textContent = tabName;
-    tab.setAttribute("id", "tab" + i);
+    tab.setAttribute("id", normalizeToKebabCase(tabName));
     tab.setAttribute("aria-controls", "panel" + i);
     tab.setAttribute("idnumber", i);
     tab.setAttribute("role", "tab");
