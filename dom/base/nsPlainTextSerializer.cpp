acc2d4c23b0a74ced481295bda1a8edfaaf0d821
Mirko Brodesser <mbrodesser@mozilla.com>
2019-08-19 07:55:22 +0000

    Bug 1574458: remove unused `nsIDocumentEncoder::OutputDontRemoveLineEndingSpaces` functionality. r=hsivonen
    Doesn't event seem to be used by comm-central/Thunderbird.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42299

--- dom/base/nsPlainTextSerializer.cpp
+++ dom/base/nsPlainTextSerializer.cpp
@@ -1347,7 +1347,6 @@ void nsPlainTextSerializer::EndLine(bool aSoftlinebreak, bool aBreakBySpace) {
    * signed messages according to the OpenPGP standard (RFC 2440).
    */
   if (!(mFlags & nsIDocumentEncoder::OutputPreformatted) &&
-      !(mFlags & nsIDocumentEncoder::OutputDontRemoveLineEndingSpaces) &&
       (aSoftlinebreak || !(mCurrentLine.EqualsLiteral("-- ") ||
                            mCurrentLine.EqualsLiteral("- -- ")))) {
     // Remove spaces from the end of the line.
