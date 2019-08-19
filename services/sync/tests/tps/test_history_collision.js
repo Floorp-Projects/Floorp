c2a39163961219166e2d8a582314430d304e2505
Mark Hammond <mhammond@skippinet.com.au>
2019-08-19 20:55:05 +0000

    Bug 1574382 - use yaml to parse TPS tests. r=tcsc
    Differential Revision: https://phabricator.services.mozilla.com/D42263
    
    --HG--
    extra : moz-landing-system : lando

--- services/sync/tests/tps/test_history_collision.js
+++ services/sync/tests/tps/test_history_collision.js
@@ -1,13 +1,13 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-/*
- * The list of phases mapped to their corresponding profiles.  The object
- * here must be in strict JSON format, as it will get parsed by the Python
- * testrunner (no single quotes, extra comma's, etc).
- */
 EnableEngines(["history"]);
 
+/*
+ * The list of phases mapped to their corresponding profiles.  The object
+ * here must be in JSON format as it will get parsed by the Python
+ * testrunner. It is parsed by the YAML package, so it relatively flexible.
+ */
 var phases = {
   phase1: "profile1",
   phase2: "profile2",
