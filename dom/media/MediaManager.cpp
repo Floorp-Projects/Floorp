350e037ffefc64bba16f403adbf4cea88c9dd911
Ciure Andrei <aciure@mozilla.com>
2019-08-19 19:48:25 +0300

    Backed out 2 changesets (bug 1571705) for causing liux debug gtest asertion failures CLOSED TREE
    Backed out changeset 3d33cae2d9f9 (bug 1571705)
    Backed out changeset decff7a230b7 (bug 1571705)

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -1180,7 +1180,6 @@ class GetUserMediaStreamRunnable : public Runnable {
         MOZ_ASSERT(IsOn(mConstraints.mAudio));
         RefPtr<MediaStreamTrack> track = new dom::AudioStreamTrack(
             window, stream, kAudioTrack, audioTrackSource,
-            dom::MediaStreamTrackState::Live,
             GetInvariant(mConstraints.mAudio));
         domStream->AddTrackInternal(track);
       }
@@ -1194,7 +1193,6 @@ class GetUserMediaStreamRunnable : public Runnable {
         MOZ_ASSERT(IsOn(mConstraints.mVideo));
         RefPtr<MediaStreamTrack> track = new dom::VideoStreamTrack(
             window, stream, kVideoTrack, videoTrackSource,
-            dom::MediaStreamTrackState::Live,
             GetInvariant(mConstraints.mVideo));
         domStream->AddTrackInternal(track);
         switch (mVideoDevice->GetMediaSource()) {
