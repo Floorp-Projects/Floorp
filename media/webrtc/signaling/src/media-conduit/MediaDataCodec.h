0e392d0f13af7f4b9c8e7ced127f345ce29ced42
Alastor Wu <alwu@mozilla.com>
2019-08-19 08:03:55 +0000

    Bug 1568101 - part7 : rename 'MediaDataDecoderCodec' to 'MediaDataCodec'. r=jolin
    As this class is not only used for creating decoder, also for encoder, it's more proper to rename it as `MediaDataCodec`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41443
    
    --HG--
    rename : media/webrtc/signaling/src/media-conduit/MediaDataDecoderCodec.cpp => media/webrtc/signaling/src/media-conduit/MediaDataCodec.cpp
    rename : media/webrtc/signaling/src/media-conduit/MediaDataDecoderCodec.h => media/webrtc/signaling/src/media-conduit/MediaDataCodec.h
    extra : moz-landing-system : lando

--- media/webrtc/signaling/src/media-conduit/MediaDataCodec.h
+++ media/webrtc/signaling/src/media-conduit/MediaDataCodec.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef MEDIA_DATA_DECODER_CODEC_H_
-#define MEDIA_DATA_DECODER_CODEC_H_
+#ifndef MEDIA_DATA_CODEC_H_
+#define MEDIA_DATA_CODEC_H_
 
 #include "MediaConduitInterface.h"
 #include "webrtc/common_types.h"
@@ -12,7 +12,7 @@ namespace mozilla {
 
 class WebrtcVideoDecoder;
 class WebrtcVideoEncoder;
-class MediaDataDecoderCodec {
+class MediaDataCodec {
  public:
   /**
    * Create encoder object for codec type |aCodecType|. Return |nullptr| when
@@ -28,4 +28,4 @@ class MediaDataDecoderCodec {
 };
 }  // namespace mozilla
 
-#endif  // MEDIA_DATA_DECODER_CODEC_H_
+#endif  // MEDIA_DATA_CODEC_H_
