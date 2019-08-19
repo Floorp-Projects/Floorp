9875e48074f920f57a19a4df70d8175aa6b47cf8
Dzmitry Malyshau <dmalyshau@mozilla.com>
2019-08-19 21:28:51 +0000

    Bug 1575008 - Update parking_lot dependency to 0.9.0 r=aosmond
    Updating parking_lot would allow us to avoid dependency duplication when WebGPU tree is vendored.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42557
    
    --HG--
    rename : third_party/rust/parking_lot_core/src/thread_parker/wasm.rs => third_party/rust/parking_lot_core/src/thread_parker/wasm_atomic.rs
    extra : moz-landing-system : lando

--- third_party/rust/parking_lot_core/src/lib.rs
+++ third_party/rust/parking_lot_core/src/lib.rs
@@ -56,42 +56,9 @@
     feature(thread_local, checked_duration_since)
 )]
 
-use cfg_if::cfg_if;
-
-cfg_if! {
-    if #[cfg(all(has_sized_atomics, target_os = "linux"))] {
-        #[path = "thread_parker/linux.rs"]
-        mod thread_parker;
-    } else if #[cfg(unix)] {
-        #[path = "thread_parker/unix.rs"]
-        mod thread_parker;
-    } else if #[cfg(windows)] {
-        #[path = "thread_parker/windows/mod.rs"]
-        mod thread_parker;
-    } else if #[cfg(all(has_sized_atomics, target_os = "redox"))] {
-        #[path = "thread_parker/redox.rs"]
-        mod thread_parker;
-    } else if #[cfg(all(target_env = "sgx", target_vendor = "fortanix"))] {
-        #[path = "thread_parker/sgx.rs"]
-        mod thread_parker;
-    } else if #[cfg(all(
-        feature = "nightly",
-        target_arch = "wasm32",
-        target_feature = "atomics"
-    ))] {
-        #[path = "thread_parker/wasm.rs"]
-        mod thread_parker;
-    } else if #[cfg(all(feature = "nightly", target_os = "cloudabi"))] {
-        #[path = "thread_parker/cloudabi.rs"]
-        mod thread_parker;
-    } else {
-        #[path = "thread_parker/generic.rs"]
-        mod thread_parker;
-    }
-}
-
 mod parking_lot;
 mod spinwait;
+mod thread_parker;
 mod util;
 mod word_lock;
 
--- third_party/rust/parking_lot_core/src/thread_parker/wasm.rs
+++ third_party/rust/parking_lot_core/src/thread_parker/wasm.rs
@@ -5,104 +5,49 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use core::{
-    arch::wasm32,
-    sync::atomic::{AtomicI32, Ordering},
-};
-use std::{convert::TryFrom, thread, time::Instant};
+//! The wasm platform can't park when atomic support is not available.
+//! So this ThreadParker just panics on any attempt to park.
 
-// Helper type for putting a thread to sleep until some other thread wakes it up
-pub struct ThreadParker {
-    parked: AtomicI32,
-}
+use std::{thread, time::Instant};
 
-const UNPARKED: i32 = 0;
-const PARKED: i32 = 1;
+pub struct ThreadParker(());
 
-impl ThreadParker {
-    pub const IS_CHEAP_TO_CONSTRUCT: bool = true;
+impl super::ThreadParkerT for ThreadParker {
+    type UnparkHandle = UnparkHandle;
 
-    #[inline]
-    pub fn new() -> ThreadParker {
-        ThreadParker {
-            parked: AtomicI32::new(UNPARKED),
-        }
+    const IS_CHEAP_TO_CONSTRUCT: bool = true;
+
+    fn new() -> ThreadParker {
+        ThreadParker(())
     }
 
-    // Prepares the parker. This should be called before adding it to the queue.
-    #[inline]
-    pub fn prepare_park(&self) {
-        self.parked.store(PARKED, Ordering::Relaxed);
+    unsafe fn prepare_park(&self) {
+        panic!("Parking not supported on this platform");
     }
 
-    // Checks if the park timed out. This should be called while holding the
-    // queue lock after park_until has returned false.
-    #[inline]
-    pub fn timed_out(&self) -> bool {
-        self.parked.load(Ordering::Relaxed) == PARKED
+    unsafe fn timed_out(&self) -> bool {
+        panic!("Parking not supported on this platform");
     }
 
-    // Parks the thread until it is unparked. This should be called after it has
-    // been added to the queue, after unlocking the queue.
-    #[inline]
-    pub fn park(&self) {
-        while self.parked.load(Ordering::Acquire) == PARKED {
-            let r = unsafe { wasm32::i32_atomic_wait(self.ptr(), PARKED, -1) };
-            // we should have either woken up (0) or got a not-equal due to a
-            // race (1). We should never time out (2)
-            debug_assert!(r == 0 || r == 1);
-        }
+    unsafe fn park(&self) {
+        panic!("Parking not supported on this platform");
     }
 
-    // Parks the thread until it is unparked or the timeout is reached. This
-    // should be called after it has been added to the queue, after unlocking
-    // the queue. Returns true if we were unparked and false if we timed out.
-    #[inline]
-    pub fn park_until(&self, timeout: Instant) -> bool {
-        while self.parked.load(Ordering::Acquire) == PARKED {
-            if let Some(left) = timeout.checked_duration_since(Instant::now()) {
-                let nanos_left = i64::try_from(left.as_nanos()).unwrap_or(i64::max_value());
-                let r = unsafe { wasm32::i32_atomic_wait(self.ptr(), PARKED, nanos_left) };
-                debug_assert!(r == 0 || r == 1 || r == 2);
-            } else {
-                return false;
-            }
-        }
-        true
+    unsafe fn park_until(&self, _timeout: Instant) -> bool {
+        panic!("Parking not supported on this platform");
     }
 
-    // Locks the parker to prevent the target thread from exiting. This is
-    // necessary to ensure that thread-local ThreadData objects remain valid.
-    // This should be called while holding the queue lock.
-    #[inline]
-    pub fn unpark_lock(&self) -> UnparkHandle {
-        // We don't need to lock anything, just clear the state
-        self.parked.store(UNPARKED, Ordering::Release);
-        UnparkHandle(self.ptr())
-    }
-
-    #[inline]
-    fn ptr(&self) -> *mut i32 {
-        &self.parked as *const AtomicI32 as *mut i32
+    unsafe fn unpark_lock(&self) -> UnparkHandle {
+        panic!("Parking not supported on this platform");
     }
 }
 
-// Handle for a thread that is about to be unparked. We need to mark the thread
-// as unparked while holding the queue lock, but we delay the actual unparking
-// until after the queue lock is released.
-pub struct UnparkHandle(*mut i32);
+pub struct UnparkHandle(());
 
-impl UnparkHandle {
-    // Wakes up the parked thread. This should be called after the queue lock is
-    // released to avoid blocking the queue for too long.
-    #[inline]
-    pub fn unpark(self) {
-        let num_notified = unsafe { wasm32::atomic_notify(self.0 as *mut i32, 1) };
-        debug_assert!(num_notified == 0 || num_notified == 1);
-    }
+impl super::UnparkHandleT for UnparkHandle {
+    unsafe fn unpark(self) {}
 }
 
-#[inline]
 pub fn thread_yield() {
     thread::yield_now();
 }
