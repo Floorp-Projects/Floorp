7a59b5f5dea62aec45cd8f5a90c88974deac736b
Julian Descottes <jdescottes@mozilla.com>
2019-08-20 07:28:41 +0000

    Bug 1539979 - Add delay in various DevTools mochitests after switching hosts r=ochameau
    Depends on D40994
    
    Switching hosts (especially going from docked to window) seems more async than before.
    I would like to work on fixing those in a follow up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40995
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/rules/test/browser_rules_eyedropper.js
+++ devtools/client/inspector/rules/test/browser_rules_eyedropper.js
@@ -54,6 +54,10 @@ add_task(async function() {
   info("Change toolbox host to WINDOW");
   await toolbox.switchHost("window");
 
+  // Switching hosts is not correctly waiting when DevTools run in content frame
+  // See Bug 1571421.
+  await wait(1000);
+
   await runTest(testActor, inspector, view, true);
 });
 
