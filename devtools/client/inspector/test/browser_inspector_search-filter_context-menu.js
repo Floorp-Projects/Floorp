4bdeb3a10cb2974e1efb45af01f3fcd348d907e8
Julian Descottes <jdescottes@mozilla.com>
2019-08-20 06:23:56 +0000

    Bug 1539979 - Fix browser_inspector_search-filter_context-menu.js when running in content frame r=nchevobbe
    Depends on D40992
    
    In general, using `click()` seems less reliable when using a frame with type=content. I am not entirely sure why since the context menu is supposed to be created in the top window anyway
    
    Differential Revision: https://phabricator.services.mozilla.com/D40993

--- devtools/client/inspector/test/browser_inspector_search-filter_context-menu.js
+++ devtools/client/inspector/test/browser_inspector_search-filter_context-menu.js
@@ -63,11 +63,17 @@ add_task(async function() {
   await onContextMenuOpen;
 
   searchContextMenu = toolbox.getTextBoxContextMenu();
-  cmdCopy = searchContextMenu.querySelector("#editmenu-copy");
-  await waitForClipboardPromise(() => cmdCopy.click(), TEST_INPUT);
 
+  // Simulating a click on cmdCopy will also close the context menu.
   onContextMenuClose = toolbox.once("menu-close");
-  EventUtils.sendKey("ESCAPE", toolbox.win);
+
+  cmdCopy = searchContextMenu.querySelector("#editmenu-copy");
+  await waitForClipboardPromise(
+    () => EventUtils.synthesizeMouseAtCenter(cmdCopy, {}, toolbox.topWindow),
+    TEST_INPUT
+  );
+
+  info("Wait for context menu to close");
   await onContextMenuClose;
 
   info("Reopen context menu and check command properties");
