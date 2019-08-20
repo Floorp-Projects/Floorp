32819855553b81658b4c3bbefd3a717ed3388361
Brian Hackett <bhackett1024@gmail.com>
2019-08-20 15:16:18 +0000

    Bug 1573970 - Pause on debugger statements when using web replay, r=loganfsmyth.
    Differential Revision: https://phabricator.services.mozilla.com/D42243
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/replay/control.js
+++ devtools/server/actors/replay/control.js
@@ -265,8 +265,11 @@ ChildProcess.prototype = {
   // Get an estimate of the amount of time required for this child to reach an
   // execution point.
   timeToReachPoint(point) {
-    let startDelay = 0,
-      startPoint = this.lastPausePoint;
+    let startDelay = 0;
+    let startPoint = this.lastPausePoint;
+    if (!startPoint) {
+      startPoint = checkpointExecutionPoint(FirstCheckpointId);
+    }
     if (!this.paused) {
       if (this.manifest.expectedDuration) {
         const elapsed = Date.now() - this.manifestSendTime;
@@ -493,6 +496,9 @@ function CheckpointInfo() {
 
   // If the checkpoint is saved and scanned, the duration of the scan.
   this.scanDuration = null;
+
+  // If the checkpoint is saved, any debugger statement hits in its region.
+  this.debuggerStatements = [];
 }
 
 function getCheckpointInfo(id) {
@@ -1169,6 +1175,9 @@ async function finishResume() {
       );
     }
 
+    // Always pause at debugger statements, as if they are breakpoint hits.
+    hits = hits.concat(getCheckpointInfo(checkpoint).debuggerStatements);
+
     const hit = findClosestPoint(
       hits,
       gPausePoint,
@@ -1442,6 +1451,7 @@ function handleResumeManifestResponse({
   duration,
   consoleMessages,
   scripts,
+  debuggerStatements,
 }) {
   if (!point.position) {
     addCheckpoint(point.checkpoint - 1, duration);
@@ -1461,6 +1471,11 @@ function handleResumeManifestResponse({
       queuePauseData(msg.executionPoint, /* trackCached */ true);
     }
   });
+
+  for (const point of debuggerStatements) {
+    const checkpoint = getSavedCheckpoint(point.checkpoint);
+    getCheckpointInfo(checkpoint).debuggerStatements.push(point);
+  }
 }
 
 // If necessary, continue executing in the main child.
@@ -1486,7 +1501,11 @@ function maybeResumeRecording() {
     return;
   }
   gMainChild.sendManifest({
-    contents: { kind: "resume", breakpoints: gBreakpoints },
+    contents: {
+      kind: "resume",
+      breakpoints: gBreakpoints,
+      pauseOnDebuggerStatement: true,
+    },
     onFinished(response) {
       handleResumeManifestResponse(response);
 
@@ -1750,7 +1769,11 @@ const gControl = {
         // We can only flush the recording at checkpoints, so we need to send the
         // main child forward and pause/flush ASAP.
         gMainChild.sendManifest({
-          contents: { kind: "resume", breakpoints: [] },
+          contents: {
+            kind: "resume",
+            breakpoints: [],
+            pauseOnDebuggerStatement: false,
+          },
           onFinished(response) {
             handleResumeManifestResponse(response);
           },
@@ -1871,6 +1894,16 @@ const gControl = {
         RecordReplayControl.clearGraphics();
       }
     }
+  },
+
+  isPausedAtDebuggerStatement() {
+    const point = gControl.pausePoint();
+    if (point) {
+      const checkpoint = getSavedCheckpoint(point.checkpoint);
+      const { debuggerStatements } = getCheckpointInfo(checkpoint);
+      return pointArrayIncludes(debuggerStatements, point);
+    }
+    return false;
   },
 };
 
