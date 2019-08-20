b924ffd96e37b9740519b15672be1e5d2e37cf56
tanhengyeow <E0032242@u.nus.edu>
2019-08-20 10:47:56 +0000

    Bug 1574826 - WebSocket Inspector Test: Basic functionality. r=Honza
    Setup base structure common to all WS inspector tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42441

---
+++ devtools/client/netmonitor/test/browser_net_ws-basic.js
@@ -0,0 +1,78 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Test for working WS connection made and sent/received messages are correct.
+ */
+
+add_task(async function() {
+  const { tab, monitor } = await initNetMonitor(WS_PAGE_URL);
+  info("Starting test... ");
+
+  const { document, store, windowRequire } = monitor.panelWin;
+  const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
+
+  store.dispatch(Actions.batchEnable(false));
+
+  // Wait for WS connection to be established + send messages
+  await ContentTask.spawn(tab.linkedBrowser, {}, async () => {
+    await content.wrappedJSObject.openConnection(1);
+  });
+
+  const requests = document.querySelectorAll(".request-list-item");
+  is(requests.length, 1, "There should be one request");
+
+  // Select the request to open the side panel.
+  EventUtils.sendMouseEvent({ type: "mousedown" }, requests[0]);
+
+  // Wait for all sent/received messages to be displayed in DevTools
+  wait = waitForDOM(
+    document,
+    "#messages-panel .ws-frames-list-table .ws-frame-list-item",
+    2
+  );
+
+  // Click on the "Messages" panel
+  EventUtils.sendMouseEvent(
+    { type: "click" },
+    document.querySelector("#messages-tab")
+  );
+  await wait;
+
+  // Get all messages present in the "Messages" panel
+  const frames = document.querySelectorAll(
+    "#messages-panel .ws-frames-list-table .ws-frame-list-item"
+  );
+
+  // Check expected results
+  is(frames.length, 2, "There should be two frames");
+
+  // Sent frame
+  is(
+    frames[0].children[0].textContent,
+    " Payload 0",
+    "The correct sent payload should be displayed"
+  );
+  is(frames[0].classList.contains("sent"), true, "The payload type is 'Sent'");
+
+  // Received frame
+  is(
+    frames[1].children[0].textContent,
+    " Payload 0",
+    "The correct received payload should be displayed"
+  );
+  is(
+    frames[1].classList.contains("received"),
+    true,
+    "The payload type is 'Received'"
+  );
+
+  // Close WS connection
+  await ContentTask.spawn(tab.linkedBrowser, {}, async () => {
+    await content.wrappedJSObject.closeConnection();
+  });
+
+  await teardown(monitor);
+});
