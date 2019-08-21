75218afff901555b12d59f323261933065af8d95
Sam Foster <sfoster@mozilla.com>
2019-08-21 22:07:17 +0000

    Bug 1560042 - Part 2: Add test coverage for empty-username/doorhanger case. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D41295
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/passwordmgr/test/browser/browser_notifications.js
+++ toolkit/components/passwordmgr/test/browser/browser_notifications.js
@@ -7,31 +7,77 @@ add_task(async function test_save_change() {
     {
       username: "username",
       password: "password",
+      expectOutcome: [
+        {
+          username: "username",
+          password: "password",
+        },
+      ],
     },
     {
       username: "",
       password: "password",
+      expectOutcome: [
+        {
+          username: "",
+          password: "password",
+        },
+      ],
     },
     {
       username: "username",
       oldPassword: "password",
       password: "newPassword",
+      expectOutcome: [
+        {
+          username: "username",
+          password: "newPassword",
+        },
+      ],
     },
     {
       username: "",
       oldPassword: "password",
       password: "newPassword",
+      expectOutcome: [
+        {
+          username: "",
+          password: "newPassword",
+        },
+      ],
+    },
+    {
+      oldUsername: "",
+      username: "username",
+      oldPassword: "password",
+      password: "newPassword",
+      expectOutcome: [
+        {
+          username: "",
+          password: "password",
+        },
+        {
+          username: "username",
+          password: "newPassword",
+        },
+      ],
     },
   ];
 
-  for (let { username, oldPassword, password } of testCases) {
+  for (let {
+    oldUsername,
+    username,
+    oldPassword,
+    password,
+    expectOutcome,
+  } of testCases) {
     // Add a login for the origin of the form if testing a change notification.
     if (oldPassword) {
       Services.logins.addLogin(
         LoginTestUtils.testData.formLogin({
           origin: "https://example.com",
           formActionOrigin: "https://example.com",
-          username,
+          username: typeof oldUsername !== "undefined" ? oldUsername : username,
           password: oldPassword,
         })
       );
@@ -62,6 +108,7 @@ add_task(async function test_save_change() {
           doc.getElementById("form-basic").submit();
         });
         await promiseShown;
+        let notif = PopupNotifications.getNotification("password", browser);
         let notificationElement = PopupNotifications.panel.childNodes[0];
         // Style flush to make sure binding is attached
         notificationElement.querySelector("#password-notification-password")
@@ -82,22 +129,24 @@ add_task(async function test_save_change() {
         // Simulate the action on the notification to request the login to be
         // saved, and wait for the data to be updated or saved based on the type
         // of operation we expect.
-        let expectedNotification = oldPassword ? "modifyLogin" : "addLogin";
+        let expectedNotification;
+        if (oldPassword !== undefined && oldUsername !== undefined) {
+          expectedNotification = "addLogin";
+        } else if (oldPassword !== undefined) {
+          expectedNotification = "modifyLogin";
+        } else {
+          expectedNotification = "addLogin";
+        }
         let promiseLogin = TestUtils.topicObserved(
           "passwordmgr-storage-changed",
           (_, data) => data == expectedNotification
         );
         notificationElement.button.doCommand();
-        let [result] = await promiseLogin;
+        await promiseLogin;
+        await cleanupDoorhanger(notif); // clean slate for the next test
 
         // Check that the values in the database match the expected values.
-        let login = oldPassword
-          ? result
-              .QueryInterface(Ci.nsIArray)
-              .queryElementAt(1, Ci.nsILoginInfo)
-          : result.QueryInterface(Ci.nsILoginInfo);
-        Assert.equal(login.username, username);
-        Assert.equal(login.password, password);
+        verifyLogins(expectOutcome);
       }
     );
 
