9de4de811331e444cad4d146075480fcef88ed06
Coroiu Cristina <ccoroiu@mozilla.com>
2019-08-21 15:18:49 +0300

    Backed out changeset 82634a1a2374 (bug 1566011) for devtools failures at browser/base/content/test/static/browser_parsable_css.js on a CLOSED TREE

--- devtools/client/application/src/components/manifest/ManifestPage.js
+++ devtools/client/application/src/components/manifest/ManifestPage.js
@@ -14,46 +14,9 @@ const {
 
 const ManifestLoader = createFactory(require("../manifest/ManifestLoader"));
 
-const ManifestView = createFactory(require("./ManifestView"));
-const ManifestViewEmpty = createFactory(require("./ManifestViewEmpty"));
-
-const { MANIFEST_DATA } = require("../../constants");
-
 class ManifestPage extends PureComponent {
   render() {
-    const isManifestViewEmpty = !MANIFEST_DATA;
-
-    // needs to be replaced with data from ManifestLoader
-    const data = {
-      warnings: MANIFEST_DATA.moz_validation,
-      icons: MANIFEST_DATA.icons,
-      identity: {
-        name: MANIFEST_DATA.name,
-        short_name: MANIFEST_DATA.short_name,
-      },
-      presentation: {
-        display: MANIFEST_DATA.display,
-        orientation: MANIFEST_DATA.orientation,
-        start_url: MANIFEST_DATA.start_url,
-        theme_color: MANIFEST_DATA.theme_color,
-        background_color: MANIFEST_DATA.background_color,
-      },
-    };
-
-    return section(
-      {
-        className: `app-page ${isManifestViewEmpty ? "app-page--empty" : ""}`,
-      },
-      ManifestLoader({}),
-      isManifestViewEmpty
-        ? ManifestViewEmpty({})
-        : ManifestView({
-            identity: data.identity,
-            warnings: data.warnings,
-            icons: data.icons,
-            presentation: data.presentation,
-          })
-    );
+    return section({ className: `manifest-page` }, ManifestLoader({}));
   }
 }
 
