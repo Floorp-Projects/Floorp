f1951479cada215905aae45480ef97b9c5c370ef
Nick Alexander <nalexander@mozilla.com>
2019-08-21 03:07:16 +0000

    Bug 1550146 - Part 2: Make mach "build" run "stage-package && android assemble-app" for mobile/android. r=glandium
    Android artifacts (GeckoView AARs, GeckoViewExample (and Fennec) APKs)
    require native libraries (`libxul.so`) and an omnijar (`omni.ja`).
    These are produced by `mach package` (really, the `stage-package`
    target).  Engineers essentially never want a build without a package
    for mobile/android.  This adds mobile/android-only tiers that run
    `mach package` and then `mach android assemble-app`.  The latter
    consumes `libxul.so` and `omni.ja` to produce _all the things_
    relevant to GeckoView engineers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41450

--- config/baseconfig.mk
+++ config/baseconfig.mk
@@ -43,7 +43,7 @@ ifndef INCLUDED_AUTOCONF_MK
 default::
 else
 # All possible tiers
-ALL_TIERS := artifact win32-artifact android-fat-aar-artifact pre-export export rust compile misc libs tools check
+ALL_TIERS := artifact win32-artifact android-fat-aar-artifact pre-export export rust compile misc libs android-stage-package android-archive-geckoview tools check
 
 # All tiers that may be used manually via `mach build $tier`
 RUNNABLE_TIERS := $(ALL_TIERS)
@@ -55,6 +55,10 @@ RUNNABLE_TIERS := $(filter-out win32-artifact,$(RUNNABLE_TIERS))
 endif
 ifndef MOZ_ANDROID_FAT_AAR_ARCHITECTURES
 RUNNABLE_TIERS := $(filter-out android-fat-aar-artifact,$(RUNNABLE_TIERS))
+endif
+ifneq ($(MOZ_BUILD_APP),mobile/android)
+RUNNABLE_TIERS := $(filter-out android-stage-package,$(RUNNABLE_TIERS))
+RUNNABLE_TIERS := $(filter-out android-archive-geckoview,$(RUNNABLE_TIERS))
 endif
 
 # All tiers that run automatically on `mach build`
