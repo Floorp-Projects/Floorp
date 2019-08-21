5f07c1da4b1e872dd50595d7d0388c4b0611c672
Paul Bone <pbone@mozilla.com>
2019-08-21 23:35:00 +0000

    Bug 1574047 - (part 2) Merge common code from {Posix,Windows}Thread.cpp files r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D42076

---
+++ js/src/threading/windows/ThreadPlatformData.h
@@ -0,0 +1,26 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef threading_windows_PlatformData_h
+#define threading_windows_PlatformData_h
+
+#include <assert.h>
+#include <new.h>
+#include <process.h>
+
+#include "threading/Thread.h"
+
+#include "util/Windows.h"
+
+class js::Thread::Id::PlatformData {
+  friend class js::Thread;
+  friend js::Thread::Id js::ThisThread::GetId();
+
+  HANDLE handle;
+  unsigned id;
+};
+
+#endif  // threading_windows_PlatformData_h
