3cab8e70c61dbab7261d0aaf68963cae0ec92f1a
Ted Campbell <tcampbell@mozilla.com>
2019-08-22 13:08:10 +0000

    Bug 1575055 - Privatize js::CompileOptions::strictMode. r=jandem
    We already have an accessor to make sure this is can only be set but not
    cleared so hide the underlying storage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42559
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/frontend/BytecodeCompiler.cpp
+++ js/src/frontend/BytecodeCompiler.cpp
@@ -387,7 +387,7 @@ BytecodeCompiler::BytecodeCompiler(JSContext* cx,
       cx(cx),
       options(options),
       sourceObject(cx),
-      directives(options.strictOption),
+      directives(options.forceStrictMode()),
       script(cx) {}
 
 bool BytecodeCompiler::createScriptSource(
@@ -745,7 +745,7 @@ JSScript* frontend::CompileGlobalBinASTScript(
     return nullptr;
   }
 
-  Directives directives(options.strictOption);
+  Directives directives(options.forceStrictMode());
   GlobalSharedContext globalsc(cx, ScopeKind::Global, directives,
                                options.extraWarningsOption);
 
@@ -796,8 +796,8 @@ static ModuleObject* InternalParseModule(
   AutoAssertReportedException assertException(cx);
 
   CompileOptions options(cx, optionsInput);
-  options.maybeMakeStrictMode(
-      true);  // ES6 10.2.1 Module code is always strict mode code.
+  options.setForceStrictMode();  // ES6 10.2.1 Module code is always strict mode
+                                 // code.
   options.setIsRunOnce(true);
   options.allowHTMLComments = false;
 
