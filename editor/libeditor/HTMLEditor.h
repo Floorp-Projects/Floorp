07451453fea8a8d4a091cd743e56dd0c935e9e5e
Noemi Erli <nerli@mozilla.com>
2019-08-22 11:41:29 +0300

    Backed out changeset 6456b7a6cc67 (bug 1574852) for failing in browser_html_tooltip-05.js

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -1113,13 +1113,6 @@ class HTMLEditor final : public TextEditor,
   MOZ_CAN_RUN_SCRIPT MOZ_MUST_USE nsresult
   GetInlineStyles(nsINode& aNode, AutoStyleCacheArray& aStyleCacheArray);
 
-  /**
-   * ReapplyCachedStyles() restores some styles which are disappeared during
-   * handling edit action and it should be restored.  This may cause flushing
-   * layout at retrieving computed value of CSS properties.
-   */
-  MOZ_CAN_RUN_SCRIPT MOZ_MUST_USE nsresult ReapplyCachedStyles();
-
  protected:  // Called by helper classes.
   virtual void OnStartToHandleTopLevelEditSubAction(
       EditSubAction aEditSubAction, nsIEditor::EDirection aDirection) override;
