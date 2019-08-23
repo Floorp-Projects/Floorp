9a1502decfe2809df54c9f5bb5622161d19b9364
Masayuki Nakano <masayuki@d-toybox.com>
2019-08-23 06:55:34 +0000

    Bug 1574852 - part 22: Move `HTMLEditRules::GetPromotedRanges()` to `HTMLEditor` r=m_kato
    `HTMLEditRules::GetPromotedRanges()` does 2 things.  Calling
    `CreateRangeIncludingAdjuscentWhiteSpaces()` or
    `CreateRangeExtendedToHardLineStartAndEnd()` with each range of `Selection`.
    The difference is considered with current edit sub-action.  Therefore, we cal
    split it to `GetSelectionRangesExtendedToIncludeAdjuscentWhiteSpaces()` and
    `GetSelectionRangesExtendedToHardLineStartAndEnd()`.  Then, we got clearer code
    at each caller.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43018

--- editor/libeditor/HTMLEditRules.cpp
+++ editor/libeditor/HTMLEditRules.cpp
@@ -1039,8 +1039,9 @@ nsresult HTMLEditRules::GetAlignment(bool* aMixed,
       return NS_ERROR_FAILURE;
     }
   } else {
-    nsTArray<RefPtr<nsRange>> arrayOfRanges;
-    GetPromotedRanges(arrayOfRanges, EditSubAction::eSetOrClearAlignment);
+    AutoTArray<RefPtr<nsRange>, 4> arrayOfRanges;
+    HTMLEditorRef().GetSelectionRangesExtendedToHardLineStartAndEnd(
+        arrayOfRanges, EditSubAction::eSetOrClearAlignment);
 
     // Use these ranges to construct a list of nodes to act on.
     nsTArray<OwningNonNull<nsINode>> arrayOfNodes;
@@ -4416,10 +4417,6 @@ nsresult HTMLEditRules::WillRemoveList(bool* aCancel, bool* aHandled) {
 
   AutoSelectionRestorer restoreSelectionLater(HTMLEditorRef());
 
-  nsTArray<RefPtr<nsRange>> arrayOfRanges;
-  GetPromotedRanges(arrayOfRanges, EditSubAction::eCreateOrChangeList);
-
-  // use these ranges to contruct a list of nodes to act on.
   nsTArray<OwningNonNull<nsINode>> arrayOfNodes;
   rv = GetListActionNodes(arrayOfNodes, EntireList::no, TouchContent::yes);
   if (NS_WARN_IF(NS_FAILED(rv))) {
@@ -5064,8 +5061,9 @@ nsresult HTMLEditRules::IndentAroundSelectionWithHTML() {
   // block parent, and then further expands to include any ancestors
   // whose children are all in the range
 
-  nsTArray<RefPtr<nsRange>> arrayOfRanges;
-  GetPromotedRanges(arrayOfRanges, EditSubAction::eIndent);
+  AutoTArray<RefPtr<nsRange>, 4> arrayOfRanges;
+  HTMLEditorRef().GetSelectionRangesExtendedToHardLineStartAndEnd(
+      arrayOfRanges, EditSubAction::eIndent);
 
   // use these ranges to contruct a list of nodes to act on.
   nsTArray<OwningNonNull<nsINode>> arrayOfNodes;
@@ -7139,41 +7137,46 @@ EditorDOMPoint HTMLEditor::GetCurrentHardLineEndPoint(
   return point;
 }
 
-void HTMLEditRules::GetPromotedRanges(
-    nsTArray<RefPtr<nsRange>>& outArrayOfRanges,
-    EditSubAction aEditSubAction) const {
-  MOZ_ASSERT(IsEditorDataAvailable());
+void HTMLEditor::GetSelectionRangesExtendedToIncludeAdjuscentWhiteSpaces(
+    nsTArray<RefPtr<nsRange>>& aOutArrayOfRanges) {
+  MOZ_ASSERT(IsEditActionDataAvailable());
+  MOZ_ASSERT(aOutArrayOfRanges.IsEmpty());
 
-  uint32_t rangeCount = SelectionRefPtr()->RangeCount();
-  for (uint32_t i = 0; i < rangeCount; i++) {
-    RefPtr<nsRange> selectionRange = SelectionRefPtr()->GetRangeAt(i);
+  aOutArrayOfRanges.SetCapacity(SelectionRefPtr()->RangeCount());
+  for (uint32_t i = 0; i < SelectionRefPtr()->RangeCount(); i++) {
+    nsRange* selectionRange = SelectionRefPtr()->GetRangeAt(i);
     MOZ_ASSERT(selectionRange);
 
+    RefPtr<nsRange> extendedRange =
+        CreateRangeIncludingAdjuscentWhiteSpaces(*selectionRange);
+    if (!extendedRange) {
+      extendedRange = selectionRange->CloneRange();
+    }
+
+    aOutArrayOfRanges.AppendElement(extendedRange);
+  }
+}
+void HTMLEditor::GetSelectionRangesExtendedToHardLineStartAndEnd(
+    nsTArray<RefPtr<nsRange>>& aOutArrayOfRanges,
+    EditSubAction aEditSubAction) {
+  MOZ_ASSERT(IsEditActionDataAvailable());
+  MOZ_ASSERT(aOutArrayOfRanges.IsEmpty());
+
+  aOutArrayOfRanges.SetCapacity(SelectionRefPtr()->RangeCount());
+  for (uint32_t i = 0; i < SelectionRefPtr()->RangeCount(); i++) {
     // Make a new adjusted range to represent the appropriate block content.
     // The basic idea is to push out the range endpoints to truly enclose the
     // blocks that we will affect.  This call alters opRange.
+    nsRange* selectionRange = SelectionRefPtr()->GetRangeAt(i);
+    MOZ_ASSERT(selectionRange);
 
-    RefPtr<nsRange> opRange;
-    switch (aEditSubAction) {
-      case EditSubAction::eInsertText:
-      case EditSubAction::eInsertTextComingFromIME:
-      case EditSubAction::eInsertLineBreak:
-      case EditSubAction::eInsertParagraphSeparator:
-      case EditSubAction::eDeleteText:
-        opRange = HTMLEditorRef().CreateRangeIncludingAdjuscentWhiteSpaces(
-            *selectionRange);
-        break;
-      default:
-        opRange = HTMLEditorRef().CreateRangeExtendedToHardLineStartAndEnd(
-            *selectionRange, aEditSubAction);
-        break;
-    }
-    if (!opRange) {
-      opRange = selectionRange->CloneRange();
+    RefPtr<nsRange> extendedRange = CreateRangeExtendedToHardLineStartAndEnd(
+        *selectionRange, aEditSubAction);
+    if (!extendedRange) {
+      extendedRange = selectionRange->CloneRange();
     }
 
-    // Stuff new opRange into array
-    outArrayOfRanges.AppendElement(opRange);
+    aOutArrayOfRanges.AppendElement(extendedRange);
   }
 }
 
@@ -7937,8 +7940,21 @@ nsresult HTMLEditRules::GetNodesFromSelection(
   MOZ_ASSERT(IsEditorDataAvailable());
 
   // Promote selection ranges
-  nsTArray<RefPtr<nsRange>> arrayOfRanges;
-  GetPromotedRanges(arrayOfRanges, aEditSubAction);
+  AutoTArray<RefPtr<nsRange>, 4> arrayOfRanges;
+  switch (aEditSubAction) {
+    case EditSubAction::eInsertText:
+    case EditSubAction::eInsertTextComingFromIME:
+    case EditSubAction::eInsertLineBreak:
+    case EditSubAction::eInsertParagraphSeparator:
+    case EditSubAction::eDeleteText:
+      HTMLEditorRef().GetSelectionRangesExtendedToIncludeAdjuscentWhiteSpaces(
+          arrayOfRanges);
+      break;
+    default:
+      HTMLEditorRef().GetSelectionRangesExtendedToHardLineStartAndEnd(
+          arrayOfRanges, aEditSubAction);
+      break;
+  }
 
   if (aTouchContent == TouchContent::yes) {
     nsresult rv = MOZ_KnownLive(HTMLEditorRef())
@@ -10901,13 +10917,9 @@ nsresult HTMLEditRules::PrepareToMakeElementAbsolutePosition(
 
   AutoSelectionRestorer restoreSelectionLater(HTMLEditorRef());
 
-  // Convert the selection ranges into "promoted" selection ranges: this
-  // basically just expands the range to include the immediate block parent,
-  // and then further expands to include any ancestors whose children are all
-  // in the range.
-
-  nsTArray<RefPtr<nsRange>> arrayOfRanges;
-  GetPromotedRanges(arrayOfRanges, EditSubAction::eSetPositionToAbsolute);
+  AutoTArray<RefPtr<nsRange>, 4> arrayOfRanges;
+  HTMLEditorRef().GetSelectionRangesExtendedToHardLineStartAndEnd(
+      arrayOfRanges, EditSubAction::eSetPositionToAbsolute);
 
   // Use these ranges to contruct a list of nodes to act on.
   nsTArray<OwningNonNull<nsINode>> arrayOfNodes;
