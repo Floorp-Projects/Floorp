7ca1ecc142f451e7857ace51a12118e078a1e76c
Noemi Erli <nerli@mozilla.com>
2019-08-24 02:43:14 +0300

    Backed out changeset ffda592c113d (bug 1573924) for failures in browser_sessionRestore.js

--- browser/components/aboutlogins/content/aboutLogins.js
+++ browser/components/aboutlogins/content/aboutLogins.js
@@ -2,106 +2,83 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-let aboutLogins = {
-  loaded: false,
-  handleEvent(event) {
-    // Run the initialization code for DOMContentLoaded and
-    // pageshow to handle normal loads and sessionrestore/bfcache.
-    if (this.loaded) {
-      return;
-    }
-    this.loaded = true;
-    this._onLoad(event);
-  },
-
-  _onLoad(event2) {
-    const gElements = {
-      fxAccountsButton: document.querySelector("fxaccounts-button"),
-      loginList: document.querySelector("login-list"),
-      loginIntro: document.querySelector("login-intro"),
-      loginItem: document.querySelector("login-item"),
-      loginFilter: document.querySelector("login-filter"),
-      // loginFooter is nested inside of loginItem
-      get loginFooter() {
-        return this.loginItem.shadowRoot.querySelector("login-footer");
-      },
-    };
-
-    let numberOfLogins = 0;
-
-    let { searchParams } = new URL(document.location);
-    if (searchParams.get("filter")) {
-      gElements.loginFilter.value = searchParams.get("filter");
-    }
-
-    gElements.loginFilter.focus();
-
-    function updateNoLogins() {
-      document.documentElement.classList.toggle(
-        "no-logins",
-        numberOfLogins == 0
-      );
-      gElements.loginList.classList.toggle("no-logins", numberOfLogins == 0);
-    }
-
-    window.addEventListener("AboutLoginsChromeToContent", event => {
-      switch (event.detail.messageType) {
-        case "AllLogins": {
-          gElements.loginList.setLogins(event.detail.value);
-          numberOfLogins = event.detail.value.length;
-          updateNoLogins();
-          break;
-        }
-        case "InitialInfo": {
-          gElements.loginFooter.hidden = event.detail.value.hideMobileFooter;
-          break;
-        }
-        case "LocalizeBadges": {
-          gElements.loginFooter.showStoreIconsForLocales(event.detail.value);
-          break;
-        }
-        case "LoginAdded": {
-          gElements.loginList.loginAdded(event.detail.value);
-          gElements.loginItem.loginAdded(event.detail.value);
-          numberOfLogins++;
-          updateNoLogins();
-          break;
-        }
-        case "LoginModified": {
-          gElements.loginList.loginModified(event.detail.value);
-          gElements.loginItem.loginModified(event.detail.value);
-          break;
-        }
-        case "LoginRemoved": {
-          gElements.loginList.loginRemoved(event.detail.value);
-          gElements.loginItem.loginRemoved(event.detail.value);
-          numberOfLogins--;
-          updateNoLogins();
-          break;
-        }
-        case "SendFavicons": {
-          gElements.loginList.addFavicons(event.detail.value);
-          break;
-        }
-        case "SyncState": {
-          gElements.fxAccountsButton.updateState(event.detail.value);
-          break;
-        }
-        case "UpdateBreaches": {
-          gElements.loginList.updateBreaches(event.detail.value);
-          gElements.loginItem.updateBreaches(event.detail.value);
-          break;
-        }
-      }
-    });
-
-    document.dispatchEvent(
-      new CustomEvent("AboutLoginsInit", { bubbles: true })
-    );
+// The init code isn't wrapped in a DOMContentLoaded/load event listener so the
+// page works properly when restored from session restore.
+const gElements = {
+  fxAccountsButton: document.querySelector("fxaccounts-button"),
+  loginList: document.querySelector("login-list"),
+  loginIntro: document.querySelector("login-intro"),
+  loginItem: document.querySelector("login-item"),
+  loginFilter: document.querySelector("login-filter"),
+  // loginFooter is nested inside of loginItem
+  get loginFooter() {
+    return this.loginItem.shadowRoot.querySelector("login-footer");
   },
 };
 
-// Run the initialization code for DOMContentLoaded and
-// pageshow to handle normal loads and sessionrestore/bfcache.
-window.addEventListener("DOMContentLoaded", aboutLogins, { once: true });
-window.addEventListener("pageshow", aboutLogins, { once: true });
+let numberOfLogins = 0;
+
+let { searchParams } = new URL(document.location);
+if (searchParams.get("filter")) {
+  gElements.loginFilter.value = searchParams.get("filter");
+}
+
+gElements.loginFilter.focus();
+
+function updateNoLogins() {
+  document.documentElement.classList.toggle("no-logins", numberOfLogins == 0);
+  gElements.loginList.classList.toggle("no-logins", numberOfLogins == 0);
+}
+
+window.addEventListener("AboutLoginsChromeToContent", event => {
+  switch (event.detail.messageType) {
+    case "AllLogins": {
+      gElements.loginList.setLogins(event.detail.value);
+      numberOfLogins = event.detail.value.length;
+      updateNoLogins();
+      break;
+    }
+    case "InitialInfo": {
+      gElements.loginFooter.hidden = event.detail.value.hideMobileFooter;
+      break;
+    }
+    case "LocalizeBadges": {
+      gElements.loginFooter.showStoreIconsForLocales(event.detail.value);
+      break;
+    }
+    case "LoginAdded": {
+      gElements.loginList.loginAdded(event.detail.value);
+      gElements.loginItem.loginAdded(event.detail.value);
+      numberOfLogins++;
+      updateNoLogins();
+      break;
+    }
+    case "LoginModified": {
+      gElements.loginList.loginModified(event.detail.value);
+      gElements.loginItem.loginModified(event.detail.value);
+      break;
+    }
+    case "LoginRemoved": {
+      gElements.loginList.loginRemoved(event.detail.value);
+      gElements.loginItem.loginRemoved(event.detail.value);
+      numberOfLogins--;
+      updateNoLogins();
+      break;
+    }
+    case "SendFavicons": {
+      gElements.loginList.addFavicons(event.detail.value);
+      break;
+    }
+    case "SyncState": {
+      gElements.fxAccountsButton.updateState(event.detail.value);
+      break;
+    }
+    case "UpdateBreaches": {
+      gElements.loginList.updateBreaches(event.detail.value);
+      gElements.loginItem.updateBreaches(event.detail.value);
+      break;
+    }
+  }
+});
+
+document.dispatchEvent(new CustomEvent("AboutLoginsInit", { bubbles: true }));
