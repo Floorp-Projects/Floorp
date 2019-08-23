320f8468b0f0d712c60cc339515bb50c48789f55
Kershaw Chang <kershaw@mozilla.com>
2019-08-23 21:17:15 +0000

    Bug 1570732 - Skip trr when parental control is enabled r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D42489

--- netwerk/test/unit/test_trr.js
+++ netwerk/test/unit/test_trr.js
@@ -3,10 +3,26 @@
 const dns = Cc["@mozilla.org/network/dns-service;1"].getService(
   Ci.nsIDNSService
 );
+const { MockRegistrar } = ChromeUtils.import(
+  "resource://testing-common/MockRegistrar.jsm"
+);
 const mainThread = Services.tm.currentThread;
 
 const defaultOriginAttributes = {};
 let h2Port = null;
+
+async function SetParentalControlEnabled(aEnabled) {
+  let parentalControlsService = {
+    parentalControlsEnabled: aEnabled,
+    QueryInterface: ChromeUtils.generateQI([Ci.nsIParentalControlsService]),
+  };
+  let cid = MockRegistrar.register(
+    "@mozilla.org/parental-controls-service;1",
+    parentalControlsService
+  );
+  dns.reloadParentalControlEnabled();
+  MockRegistrar.unregister(cid);
+}
 
 add_task(function setup() {
   dump("start!\n");
@@ -48,6 +64,8 @@ add_task(function setup() {
     Ci.nsIX509CertDB
   );
   addCertFromFile(certdb, "http2-ca.pem", "CTu,u,u");
+
+  SetParentalControlEnabled(false);
 });
 
 registerCleanupFunction(() => {
@@ -710,6 +728,14 @@ add_task(async function test24f() {
   await new DNSListener("test.detectportal.com", "127.0.0.1");
 });
 
+// TRR-first check that a domain is resolved via native DNS when parental control is enabled.
+add_task(async function test24g() {
+  dns.clearCache(true);
+  await SetParentalControlEnabled(true);
+  await new DNSListener("www.example.com", "127.0.0.1");
+  await SetParentalControlEnabled(false);
+});
+
 // TRR-only that resolving localhost with TRR-only mode will use the remote
 // resolver if it's not in the excluded domains
 add_task(async function test25() {
@@ -780,6 +806,15 @@ add_task(async function test25e() {
   );
 
   await new DNSListener("test.detectportal.com", "127.0.0.1");
+});
+
+// TRR-only check that a domain is resolved via native DNS when parental control is enabled.
+add_task(async function test25f() {
+  dns.clearCache(true);
+  Services.prefs.setIntPref("network.trr.mode", 3); // TRR-only
+  await SetParentalControlEnabled(true);
+  await new DNSListener("www.example.com", "127.0.0.1");
+  await SetParentalControlEnabled(false);
 });
 
 // Check that none of the requests have set any cookies.
