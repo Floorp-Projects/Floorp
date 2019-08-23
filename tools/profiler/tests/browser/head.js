103921e690651a1db63d5f03a76e343c0ae7190b
Brindusan Cristian <cbrindusan@mozilla.com>
2019-08-23 04:22:03 +0300

    Backed out 1 changesets (bug 1551313) for build bustages at ProfilerMarkerPayload.cpp. CLOSED TREE
    Backed out changeset 7fd9b797a83e (bug 1551313)

--- tools/profiler/tests/browser/head.js
+++ tools/profiler/tests/browser/head.js
@@ -21,60 +21,6 @@ function startProfiler(callersSettings) {
     settings.duration
   );
 }
-/**
- * This function spins on a while loop until at least one
- * periodic sample is taken. Use this function to ensure
- * that markers are properly collected for a test or that
- * at least one sample in which we are interested is collected.
- */
-async function doAtLeastOnePeriodicSample() {
-  async function getProfileSampleCount() {
-    const profile = await Services.profiler.getProfileDataAsync();
-    return profile.threads[0].samples.data.length;
-  }
-
-  const sampleCount = await getProfileSampleCount();
-  // Create an infinite loop until a sample has been collected.
-  while (true) {
-    if (sampleCount < (await getProfileSampleCount())) {
-      return;
-    }
-  }
-}
-
-/**
- * This is a helper function that will stop the profiler of the browser
- * running with PID contentPid. The profiler in that PID
- * will not stop until there is at least one periodic sample taken, though.
- *
- * @param {number} contentPid
- * @returns {Promise}
- */
-async function stopProfilerAndGetThreads(contentPid) {
-  await doAtLeastOnePeriodicSample();
-
-  const profile = await Services.profiler.getProfileDataAsync();
-  Services.profiler.StopProfiler();
-
-  const parentThread = profile.threads[0];
-  const contentProcess = profile.processes.find(
-    p => p.threads[0].pid == contentPid
-  );
-  if (!contentProcess) {
-    throw new Error("Could not find the content process.");
-  }
-  const contentThread = contentProcess.threads[0];
-
-  if (!parentThread) {
-    throw new Error("The parent thread was not found in the profile.");
-  }
-
-  if (!contentThread) {
-    throw new Error("The content thread was not found in the profile.");
-  }
-
-  return { parentThread, contentThread };
-}
 
 /**
  * This is a helper function be able to run `await wait(500)`. Unfortunately this
