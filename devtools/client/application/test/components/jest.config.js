b7bb675372a2518acb895e952e5700b13b939a49
Belén Albeza <balbeza@mozilla.com>
2019-08-23 09:53:19 +0000

    Bug 1575823 - Enable support for l10n.getString in tests r=Ola
    - This creates a mock for fluent-l10n module, so we can use `l10n.getString()` in our code and test for it.
    - This patch also removes unused files `test/fixtures/l10n.js` and `test/fixtures/PluralForm.js`
    
    In order to double check the mock works, these two lines can be added to any test:
    
    ```
    const { l10n } = require("devtools/client/application/src/modules/l10n");
    expect(l10n.getString("foo")).toBe("foo");
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D43050

--- devtools/client/application/test/components/jest.config.js
+++ devtools/client/application/test/components/jest.config.js
@@ -9,11 +9,10 @@ module.exports = {
   verbose: true,
   moduleNameMapper: {
     // Custom name mappers for modules that require m-c specific API.
-    "^../utils/l10n": `${__dirname}/fixtures/l10n`,
     "^devtools/client/shared/link": `${__dirname}/fixtures/stub`,
-    "^devtools/shared/plural-form": `${__dirname}/fixtures/plural-form`,
     "^chrome": `${__dirname}/fixtures/Chrome`,
     "^Services": `${__dirname}/fixtures/Services`,
+    "^devtools/client/shared/fluent-l10n/fluent-l10n$": `${__dirname}/fixtures/fluent-l10n`,
     "^devtools/client/shared/unicode-url": `${__dirname}/fixtures/unicode-url`,
     // Map all require("devtools/...") to the real devtools root.
     "^devtools\\/(.*)": `${__dirname}/../../../../$1`,
