72793931dcb2d62d4769233b888106dcfe1866ff
Drew Willcoxon <adw@mozilla.com>
2019-08-23 19:09:27 +0000

    Bug 1576246 - Set pref browser.urlbar.eventTelemetry.enabled by default. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D43286

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -330,6 +330,7 @@ pref("browser.urlbar.usepreloadedtopurls.expire_days", 14);
 pref("browser.urlbar.megabar", false);
 
 pref("browser.urlbar.openViewOnFocus", false);
+pref("browser.urlbar.eventTelemetry.enabled", false);
 
 pref("browser.altClickSave", false);
 
