507af7650e2ad36a5a483ebd311f2300170f1249
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-26 16:40:10 +0000

    Bug 1576546 - Add various explanatory comments to the top of prefs files. r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D43391

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Non-static prefs that are specific to Firefox on Android belong in this file
+// (unless there is a compelling and documented reason for them to belong in
+// another file).
+//
 // Please indent all prefs defined within #ifdef/#ifndef conditions. This
 // improves readability, particular for conditional blocks that exceed a single
 // screen.
