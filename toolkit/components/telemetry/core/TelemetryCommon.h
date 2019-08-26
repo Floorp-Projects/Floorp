605749ff64aa91c18dab4135d3b13e2972df6618
Jorg K <jorgk@jorgk.com>
2019-08-26 09:42:37 +0200

    Backed out 2 changesets (bug 1427877) for causing Thunderbird xpcshell-bustages. a=backout
    MANUAL PUSH: Approval from sheriffs via IRC. Immediate fix for massive xpcshell failures.
    
    Backed out changeset 6b90caa175cb (bug 1427877)
    Backed out changeset bf126cb4b08f (bug 1427877)
    
    --HG--
    extra : amend_source : 0ff7d8fd28b27380cb21ae41ff1df5339759863b

--- toolkit/components/telemetry/core/TelemetryCommon.h
+++ toolkit/components/telemetry/core/TelemetryCommon.h
@@ -39,7 +39,6 @@ enum class SupportedProduct : uint8_t {
   Fennec = (1 << 1),
   Geckoview = (1 << 2),
   GeckoviewStreaming = (1 << 3),
-  Thunderbird = (1 << 4),
 };
 MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(SupportedProduct);
 
