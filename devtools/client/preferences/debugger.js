dc11cb75fd988e35b87cbc0ff868c5723364b3e4
David Walsh <dwalsh@mozilla.com>
2019-08-26 20:19:39 +0000

    Bug 1576219 - Enable DOM Mutation Breakpoints r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D43273

--- devtools/client/preferences/debugger.js
+++ devtools/client/preferences/debugger.js
@@ -80,7 +80,7 @@ pref("devtools.debugger.features.map-expression-bindings", true);
 pref("devtools.debugger.features.xhr-breakpoints", true);
 pref("devtools.debugger.features.original-blackbox", true);
 pref("devtools.debugger.features.event-listeners-breakpoints", true);
-pref("devtools.debugger.features.dom-mutation-breakpoints", false);
+pref("devtools.debugger.features.dom-mutation-breakpoints", true);
 pref("devtools.debugger.features.log-points", true);
 pref("devtools.debugger.features.overlay-step-buttons", false);
 pref("devtools.debugger.features.inline-preview", false);
