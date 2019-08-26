d730875b8e9c19a778faf08cf71ae9fee8ac58e8
Nicholas Nethercote <nnethercote@mozilla.com>
2019-08-26 16:40:10 +0000

    Bug 1576546 - Add various explanatory comments to the top of prefs files. r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D43391

--- browser/branding/unofficial/pref/firefox-branding.js
+++ browser/branding/unofficial/pref/firefox-branding.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This file contains branding-specific prefs.
+
 pref("startup.homepage_override_url", "");
 pref("startup.homepage_welcome_url", "");
 pref("startup.homepage_welcome_url.additional", "");
--- modules/libpref/greprefs.js
+++ modules/libpref/greprefs.js
@@ -1,1 +1,3 @@
+// Do not add anything else to this file. Additions should go in all.js.
+
 #include init/all.js
