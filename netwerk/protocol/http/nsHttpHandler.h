d65c5e03097c8c67be0fa0bcd03e61727d85932a
Jean-Yves Avenard <jyavenard@mozilla.com>
2019-08-26 12:14:44 +1000

    Bug 1556489 - P19. Fix devtools stack traces. r=honza,ochameau
    DevTools rely on events when a new HttpChannel is opened, it needs to know when a DocumentChannel is.
    
    Differential Revision: https://phabricator.services.mozilla.com/D40975

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -361,6 +361,10 @@ class nsHttpHandler final : public nsIHttpProtocolHandler,
     NotifyObservers(chan, NS_HTTP_ON_OPENING_REQUEST_TOPIC);
   }
 
+  void OnOpeningDocumentRequest(nsIIdentChannel* chan) {
+    NotifyObservers(chan, NS_DOCUMENT_ON_OPENING_REQUEST_TOPIC);
+  }
+
   // Called by the channel before writing a request
   void OnModifyRequest(nsIHttpChannel* chan) {
     NotifyObservers(chan, NS_HTTP_ON_MODIFY_REQUEST_TOPIC);
