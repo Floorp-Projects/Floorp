252c68413f0a544d275a943c3af7e3aea077c3d8
Mirko Brodesser <mbrodesser@mozilla.com>
2019-08-27 10:41:44 +0000

    Bug 1575839: part 5) Factor out `ReplaceVisiblyTrailingNbsps` from `nsPlainTextSerializer::Write`. r=hsivonen
    Differential Revision: https://phabricator.services.mozilla.com/D43419
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsPlainTextSerializer.cpp
+++ dom/base/nsPlainTextSerializer.cpp
@@ -1450,6 +1450,25 @@ void nsPlainTextSerializer::OutputQuotesAndIndent(
   }
 }
 
+static bool IsLineFeedCarriageReturnBlankOrTab(char16_t c) {
+  return ('\n' == c || '\r' == c || ' ' == c || '\t' == c);
+}
+
+static void ReplaceVisiblyTrailingNbsps(nsAString& aString) {
+  const int32_t totLen = aString.Length();
+  for (int32_t i = totLen - 1; i >= 0; i--) {
+    char16_t c = aString[i];
+    if (IsLineFeedCarriageReturnBlankOrTab(c)) {
+      continue;
+    }
+    if (kNBSP == c) {
+      aString.Replace(i, 1, ' ');
+    } else {
+      break;
+    }
+  }
+}
+
 /**
  * Write a string. This is the highlevel function to use to get text output.
  * By using AddToLine, Output, EndLine and other functions it handles quotation,
@@ -1476,14 +1495,7 @@ void nsPlainTextSerializer::Write(const nsAString& aStr) {
   // For Flowed text change nbsp-ses to spaces at end of lines to allow them
   // to be cut off along with usual spaces if required. (bug #125928)
   if (mSettings.HasFlag(nsIDocumentEncoder::OutputFormatFlowed)) {
-    for (int32_t i = totLen - 1; i >= 0; i--) {
-      char16_t c = str[i];
-      if ('\n' == c || '\r' == c || ' ' == c || '\t' == c) continue;
-      if (kNBSP == c)
-        str.Replace(i, 1, ' ');
-      else
-        break;
-    }
+    ReplaceVisiblyTrailingNbsps(str);
   }
 
   // We have two major codepaths here. One that does preformatted text and one
@@ -1536,8 +1548,7 @@ void nsPlainTextSerializer::Write(const nsAString& aStr) {
         stringpart.Assign(Substring(str, bol, totLen - bol));
         if (!stringpart.IsEmpty()) {
           char16_t lastchar = stringpart[stringpart.Length() - 1];
-          if ((lastchar == '\t') || (lastchar == ' ') || (lastchar == '\r') ||
-              (lastchar == '\n')) {
+          if (IsLineFeedCarriageReturnBlankOrTab(lastchar)) {
             mInWhitespace = true;
           } else {
             mInWhitespace = false;
