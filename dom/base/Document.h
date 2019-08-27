ff387f6937480630a8a7f546c224c3471298602f
Edgar Chen <echen@mozilla.com>
2019-08-27 21:22:19 +0000

    Bug 1576627 - Move MaybeNotifyAutoplayBlocked from Document to HTMLMediaElement; r=alwu
    Given that it is used only in HTMLMediaElement.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43452
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -4117,10 +4117,6 @@ class Document : public nsINode,
 
   void ReportShadowDOMUsage();
 
-  // When the doc is blocked permanantly, we would dispatch event to notify
-  // front-end side to show blocking icon.
-  void MaybeNotifyAutoplayBlocked();
-
   // Sets flags for media autoplay telemetry.
   void SetDocTreeHadAudibleMedia();
   void SetDocTreeHadPlayRevoked();
