c7b01ab9f1131f7fa79c0ada306b4753289ab5d5
yulia <ystartsev@mozilla.com>
2019-08-27 09:07:45 +0000

    Bug 1574156 - implement FrameDescriptor; r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D42305

---
+++ devtools/server/actors/descriptors/frame.js
@@ -0,0 +1,91 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { ActorClassWithSpec, Actor } = require("devtools/shared/protocol");
+const {
+  frameDescriptorSpec,
+} = require("devtools/shared/specs/descriptors/frame");
+
+loader.lazyRequireGetter(
+  this,
+  "FrameConnector",
+  "devtools/server/connectors/frame-connector",
+  true
+);
+
+const FrameDescriptorActor = ActorClassWithSpec(frameDescriptorSpec, {
+  initialize(connection, browsingContext) {
+    if (typeof browsingContext.id != "number") {
+      throw Error("Frame Descriptor Connect requires a valid browsingContext.");
+    }
+    Actor.prototype.initialize.call(this, connection);
+    this.destroy = this.destroy.bind(this);
+    this.id = browsingContext.id;
+    this._browsingContext = browsingContext;
+    this._embedderElement = browsingContext.embedderElement;
+  },
+
+  /**
+   * Connect to a remote process actor, always a Frame target.
+   */
+  async _connectMessageManager() {
+    return FrameConnector.startServer(
+      this.conn,
+      this._embedderElement,
+      this.destroy
+    );
+  },
+
+  async _connectBrowsingContext() {
+    const id = this.id;
+    return {
+      error: "NoJSWindowAPISupport",
+      message: `The browsingContext '${id}' can only be accessed via JSWindowAPI`,
+    };
+  },
+
+  get _mm() {
+    // Get messageManager from XUL browser (which might be a specialized tunnel for RDM)
+    // or else fallback to asking the frameLoader itself.
+    return (
+      this._embedderElement &&
+      (this._embedderElement.messageManager ||
+        this._embedderElement.frameLoader.messageManager)
+    );
+  },
+
+  /**
+   * Connect the a frame actor.
+   */
+  async getTarget() {
+    let form;
+    if (this._mm) {
+      form = await this._connectMessageManager();
+    } else {
+      form = await this._connectBrowsingContext();
+    }
+
+    return form;
+  },
+
+  form() {
+    return {
+      actor: this.actorID,
+      id: this.id,
+      parentId: this._browsingContext.parent
+        ? this._browsingContext.parent.id
+        : null,
+    };
+  },
+
+  destroy() {
+    this._browsingContext = null;
+    this._embedderElement = null;
+    Actor.prototype.destroy.call(this);
+  },
+});
+
+exports.FrameDescriptorActor = FrameDescriptorActor;
