3e14510bdc6c3e4346e2c368e5640a2a179ed315
Gurzau Raul <rgurzau@mozilla.com>
2019-08-28 17:12:00 +0300

    Backed out changeset 9aa911cc3fcf (bug 1558417) for failing at browser_jsterm_editor_toolbar.js on a CLOSED TREE.

--- devtools/client/webconsole/actions/ui.js
+++ devtools/client/webconsole/actions/ui.js
@@ -22,7 +22,6 @@ const {
   FILTERBAR_DISPLAY_MODE_SET,
   EDITOR_TOGGLE,
   EDITOR_SET_WIDTH,
-  EDITOR_ONBOARDING_DISMISS,
 } = require("devtools/client/webconsole/constants");
 
 function persistToggle() {
@@ -98,15 +97,6 @@ function editorToggle() {
   };
 }
 
-function editorOnboardingDismiss() {
-  return ({ dispatch, prefsService }) => {
-    dispatch({
-      type: EDITOR_ONBOARDING_DISMISS,
-    });
-    prefsService.setBoolPref(PREFS.UI.EDITOR_ONBOARDING, false);
-  };
-}
-
 function setEditorWidth(width) {
   return ({ dispatch, prefsService }) => {
     dispatch({
@@ -161,7 +151,6 @@ function filterBarDisplayModeSet(displayMode) {
 
 module.exports = {
   contentMessagesToggle,
-  editorOnboardingDismiss,
   editorToggle,
   filterBarDisplayModeSet,
   initialize,
