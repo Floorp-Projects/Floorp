f04a457e6ded6dc5f7ed20c4435d467c1f15fee9
Nick Alexander <nalexander@mozilla.com>
2019-08-28 20:58:26 +0000

    Bug 1576971 - Post: Use github.com for fetching browsertime binary dependencies. r=barret
    In automation, we install `ffmpeg` as part of `mach browsertime
    --setup` in the browsertime toolchain task.  Those tasks run on Linux
    64 from within AWS, and most of the hosts we hit (intermittently) deny
    AWS traffic.  Let's just use github.com in automation (and locally),
    for all platforms, which will agree with upcoming fetch tasks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43656

--- tools/browsertime/mach_commands.py
+++ tools/browsertime/mach_commands.py
@@ -102,7 +102,7 @@ host_fetches = {
     'darwin': {
         'ffmpeg': {
             'type': 'static-url',
-            'url': 'https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-4.1.1-macos64-static.zip',  # noqa
+            'url': 'https://github.com/ncalexan/geckodriver/releases/download/v0.24.0-android/ffmpeg-4.1.1-macos64-static.zip',  # noqa
             # An extension to `fetch` syntax.
             'path': 'ffmpeg-4.1.1-macos64-static',
         },
@@ -119,9 +119,9 @@ host_fetches = {
     'linux64': {
         'ffmpeg': {
             'type': 'static-url',
-            'url': 'https://www.johnvansickle.com/ffmpeg/old-releases/ffmpeg-4.0.3-64bit-static.tar.xz',  # noqa
+            'url': 'https://github.com/ncalexan/geckodriver/releases/download/v0.24.0-android/ffmpeg-4.1.4-i686-static.tar.xz',  # noqa
             # An extension to `fetch` syntax.
-            'path': 'ffmpeg-4.0.3-64bit-static',
+            'path': 'ffmpeg-4.1.4-i686-static',
         },
         # TODO: install a static ImageMagick.  All easily available binaries are
         # not statically linked, so they will (mostly) fail at runtime due to
@@ -131,7 +131,7 @@ host_fetches = {
     'win64': {
         'ffmpeg': {
             'type': 'static-url',
-            'url': 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.1.1-win64-static.zip',  # noqa
+            'url': 'https://github.com/ncalexan/geckodriver/releases/download/v0.24.0-android/ffmpeg-4.1.1-win64-static.zip',  # noqa
             # An extension to `fetch` syntax.
             'path': 'ffmpeg-4.1.1-win64-static',
         },
