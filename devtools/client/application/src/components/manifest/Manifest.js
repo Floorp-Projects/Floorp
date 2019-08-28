ef64830e0c1b7f19ac2041801d773d0cb6fefd61
Belén Albeza <balbeza@mozilla.com>
2019-08-28 12:20:43 +0000

    Bug 1575872 - Part 1: Render a list of issues (errors and warnings) in the Manifest r=Ola,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D43637
    
    --HG--
    rename : devtools/client/application/src/components/manifest/ManifestItemWarning.css => devtools/client/application/src/components/manifest/ManifestIssue.css
    rename : devtools/client/application/src/components/manifest/ManifestItemWarning.js => devtools/client/application/src/components/manifest/ManifestIssue.js
    extra : moz-landing-system : lando

--- devtools/client/application/src/components/manifest/Manifest.js
+++ devtools/client/application/src/components/manifest/Manifest.js
@@ -12,6 +12,8 @@ const {
 const {
   article,
   h1,
+  table,
+  tbody,
 } = require("devtools/client/shared/vendor/react-dom-factories");
 
 const FluentReact = require("devtools/client/shared/vendor/fluent-react");
@@ -19,7 +21,7 @@ const Localized = createFactory(FluentReact.Localized);
 const { l10n } = require("../../modules/l10n");
 
 const ManifestItem = createFactory(require("./ManifestItem"));
-const ManifestItemWarning = createFactory(require("./ManifestItemWarning"));
+const ManifestIssueList = createFactory(require("./ManifestIssueList"));
 const ManifestSection = createFactory(require("./ManifestSection"));
 
 /**
@@ -27,16 +29,33 @@ const ManifestSection = createFactory(require("./ManifestSection"));
  */
 class Manifest extends PureComponent {
   static get propTypes() {
+    // TODO: Use well-defined types
+    //       See https://bugzilla.mozilla.org/show_bug.cgi?id=1576881
     return {
       icons: PropTypes.array.isRequired,
       identity: PropTypes.array.isRequired,
       presentation: PropTypes.array.isRequired,
-      warnings: PropTypes.array.isRequired,
+      validation: PropTypes.array.isRequired,
     };
   }
 
-  render() {
-    const { identity, warnings, icons, presentation } = this.props;
+  renderIssueSection() {
+    const { validation } = this.props;
+    const shouldRender = validation && validation.length > 0;
+
+    return shouldRender
+      ? ManifestSection(
+          {
+            key: `manifest-section-0`,
+            title: l10n.getString("manifest-item-warnings"),
+          },
+          ManifestIssueList({ issues: validation })
+        )
+      : null;
+  }
+
+  renderItemSections() {
+    const { identity, icons, presentation } = this.props;
 
     const manifestMembers = [
       { localizationId: "manifest-item-identity", items: identity },
@@ -44,6 +63,32 @@ class Manifest extends PureComponent {
       { localizationId: "manifest-item-icons", items: icons },
     ];
 
+    return manifestMembers.map(({ localizationId, items }, index) => {
+      return ManifestSection(
+        {
+          key: `manifest-section-${index + 1}`,
+          title: l10n.getString(localizationId),
+        },
+        // NOTE: this table should probably be its own component, to keep
+        //       the same level of abstraction as with the validation issues
+        // Bug https://bugzilla.mozilla.org/show_bug.cgi?id=1577138
+        table(
+          {},
+          tbody(
+            {},
+            // TODO: handle different data types for values (colors, images…)
+            //       See https://bugzilla.mozilla.org/show_bug.cgi?id=1575529
+            items.map(item => {
+              const { key, value } = item;
+              return ManifestItem({ label: key, key: key }, value);
+            })
+          )
+        )
+      );
+    });
+  }
+
+  render() {
     return article(
       {},
       Localized(
@@ -52,31 +97,8 @@ class Manifest extends PureComponent {
         },
         h1({ className: "app-page__title" })
       ),
-      // TODO: this probably should not be a table, but a list. Review markup
-      //       in https://bugzilla.mozilla.org/show_bug.cgi?id=1575872
-      ManifestSection(
-        {
-          key: `manifest-section-0`,
-          title: l10n.getString("manifest-item-warnings"),
-        },
-        warnings.map((warning, index) =>
-          ManifestItemWarning({ warning, key: `warning-${index}` })
-        )
-      ),
-      manifestMembers.map(({ localizationId, items }, index) => {
-        return ManifestSection(
-          {
-            key: `manifest-section-${index + 1}`,
-            title: l10n.getString(localizationId),
-          },
-          // TODO: handle different data types for values (colors, images…)
-          //       See https://bugzilla.mozilla.org/show_bug.cgi?id=1575529
-          items.map(item => {
-            const { key, value } = item;
-            return ManifestItem({ label: key, key: key }, value);
-          })
-        );
-      })
+      this.renderIssueSection(),
+      this.renderItemSections()
     );
   }
 }
