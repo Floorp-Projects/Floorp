22d963beb7aa8e392805e5c39a9a97e0d4f03a16
James Willcox <snorp@snorp.net>
2019-08-29 18:59:28 +0000

    Bug 1343678 - Enable web push pref for GeckoView r=geckoview-reviewers,droeh,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D41288

--- dom/workers/test/test_worker_interfaces.js
+++ dom/workers/test/test_worker_interfaces.js
@@ -213,14 +213,13 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "PromiseRejectionEvent", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "PushManager", insecureContext: true, fennecOrDesktop: true },
+  { name: "PushManager", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "PushSubscription", insecureContext: true, fennecOrDesktop: true },
+  { name: "PushSubscription", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   {
     name: "PushSubscriptionOptions",
     insecureContext: true,
-    fennecOrDesktop: true,
   },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "Request", insecureContext: true },
