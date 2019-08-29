5d26659ae39374860cad0b40b6c2cab3fabe201c
Dorel Luca <dluca@mozilla.com>
2019-08-29 05:55:23 +0300

    Backed out changeset d870fdbf200f (bug 1575240) for Devtools failures on devtools/client/webconsole/test/browser/browser_jsterm_inspect.js. CLOSED TREE

--- devtools/client/webconsole/test/browser/browser_jsterm_inspect.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_inspect.js
@@ -5,75 +5,21 @@
 
 "use strict";
 
-const TEST_URI =
-  "https://example.com/browser/devtools/client/webconsole/test/browser/" +
-  "test-simple-function.html";
+const TEST_URI = "data:text/html;charset=utf8,<p>test inspect() command";
 
 add_task(async function() {
-  let hud = await openNewTabAndConsole(TEST_URI);
-  const toolbox = gDevTools.getToolbox(hud.target);
+  const hud = await openNewTabAndConsole(TEST_URI);
 
-  await testInspectingElement(hud, toolbox);
-  await testInspectingFunction(hud, toolbox);
-
-  hud = await openNewTabAndConsole(TEST_URI);
-  await testInspectingWindow(hud);
-  await testInspectIngPrimitive(hud);
-});
-
-async function testInspectingElement(hud, toolbox) {
-  info("Test `inspect(el)`");
-  execute(hud, "inspect(document.querySelector('p'))");
-  await waitForSelectedElementInInspector(toolbox, "p");
-  ok(true, "inspected element is now selected in the inspector");
-}
-
-async function testInspectingFunction(hud, toolbox) {
-  info("Test `inspect(test)`");
-  execute(hud, "inspect(test)");
-
-  await waitFor(() => {
-    const dbg = toolbox.getPanel("jsdebugger");
-    if (!dbg) {
-      return false;
-    }
-
-    const selectedLocation = dbg._selectors.getSelectedLocation(
-      dbg._getState()
-    );
-
-    if (!selectedLocation) {
-      return false;
-    }
-
-    return (
-      selectedLocation.sourceId.includes("test-simple-function.js") &&
-      selectedLocation.line == 3
-    );
-  });
-
-  ok(true, "inspected function is now selected in the debugger");
-}
-
-async function testInspectingWindow(hud) {
   info("Test `inspect(window)`");
   // Add a global value so we can check it later.
   execute(hud, "testProp = 'testValue'");
   execute(hud, "inspect(window)");
 
-  const objectInspectors = await waitFor(() => {
-    const message = findInspectResultMessage(hud.ui.outputNode, 1);
-    if (!message) {
-      return false;
-    }
+  const inspectWindowNode = await waitFor(() =>
+    findInspectResultMessage(hud.ui.outputNode, 1)
+  );
 
-    const treeEls = message.querySelectorAll(".tree");
-    if (treeEls.length == 0) {
-      return false;
-    }
-    return [...treeEls];
-  });
-
+  const objectInspectors = [...inspectWindowNode.querySelectorAll(".tree")];
   is(
     objectInspectors.length,
     1,
@@ -109,9 +55,8 @@ async function testInspectingWindow(hud) {
     '"testValue"',
     "The testProp property value is displayed as expected"
   );
-}
 
-async function testInspectIngPrimitive(hud) {
+  /* Check that a primitive value can be inspected, too */
   info("Test `inspect(1)`");
   execute(hud, "inspect(1)");
 
@@ -123,24 +68,8 @@ async function testInspectIngPrimitive(hud) {
     1,
     "The primitive is displayed as expected"
   );
-}
+});
 
 function findInspectResultMessage(node, index) {
   return node.querySelectorAll(".message.result")[index];
-}
-
-async function waitForSelectedElementInInspector(toolbox, displayName) {
-  return waitFor(() => {
-    const inspector = toolbox.getPanel("inspector");
-    if (!inspector) {
-      return false;
-    }
-
-    const selection = inspector.selection;
-    return (
-      selection &&
-      selection.nodeFront &&
-      selection.nodeFront.displayName == displayName
-    );
-  });
 }
