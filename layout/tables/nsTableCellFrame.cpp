81134b078b35e40019c8c69ccfdb94ad36112d4b
Mats Palmgren <mats@mozilla.com>
2019-08-29 21:22:25 +0000

    Bug 1577364 - Remove the NS_TABLE_CELL_HAS_PCT_OVER_BSIZE frame bit since it's not used.  r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D43864
    
    --HG--
    extra : moz-landing-system : lando

--- layout/tables/nsTableCellFrame.cpp
+++ layout/tables/nsTableCellFrame.cpp
@@ -80,7 +80,6 @@ nsTableCellFrame::nsTableCellFrame(ComputedStyle* aStyle,
   mPriorAvailISize = 0;
 
   SetContentEmpty(false);
-  SetHasPctOverBSize(false);
 }
 
 nsTableCellFrame::~nsTableCellFrame() {}
@@ -856,8 +855,6 @@ void nsTableCellFrame::Reflow(nsPresContext* aPresContext,
           .SetComputedBSize(computedUnpaginatedBSize);
       DISPLAY_REFLOW_CHANGE();
     }
-  } else {
-    SetHasPctOverBSize(false);
   }
 
   WritingMode kidWM = firstKid->GetWritingMode();
@@ -954,15 +951,9 @@ void nsTableCellFrame::Reflow(nsPresContext* aPresContext,
 
   // the overflow area will be computed when BlockDirAlignChild() gets called
 
-  if (aReflowInput.mFlags.mSpecialBSizeReflow) {
-    if (aDesiredSize.BSize(wm) > BSize(wm)) {
-      // set a bit indicating that the pct bsize contents exceeded
-      // the height that they could honor in the pass 2 reflow
-      SetHasPctOverBSize(true);
-    }
-    if (NS_UNCONSTRAINEDSIZE == aReflowInput.AvailableBSize()) {
-      aDesiredSize.BSize(wm) = BSize(wm);
-    }
+  if (aReflowInput.mFlags.mSpecialBSizeReflow &&
+      NS_UNCONSTRAINEDSIZE == aReflowInput.AvailableBSize()) {
+    aDesiredSize.BSize(wm) = BSize(wm);
   }
 
   // If our parent is in initial reflow, it'll handle invalidating our
