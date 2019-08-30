fd7aae55653d098b02b4f56f99ce75952c46f4a6
arthur.iakab <aiakab@mozilla.com>
2019-08-30 21:37:15 +0300

    Backed out changeset 9057d052acee (bug 1559088) for causing xpcshell failures on test_redirect-caching_failure.js CLOSED TREE

--- netwerk/test/unit_ipc/test_redirect-caching_failure_wrap.js
+++ netwerk/test/unit_ipc/test_redirect-caching_failure_wrap.js
@@ -1,13 +1,7 @@
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-
 //
 // Run test script in content process instead of chrome (xpcshell's default)
 //
 //
 function run_test() {
-  do_await_remote_message("disable-ports").then(_ => {
-    Services.prefs.setCharPref("network.security.ports.banned", "65400");
-    do_send_remote_message("disable-ports-done");
-  });
   run_test_in_child("../unit/test_redirect-caching_failure.js");
 }
