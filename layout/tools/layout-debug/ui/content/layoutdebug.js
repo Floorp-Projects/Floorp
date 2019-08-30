2503e0f3c16e195a8deea88474d7c38c48ca81e4
Cameron McCormack <cam@mcc.id.au>
2019-08-30 23:12:58 +0000

    Bug 1577390 - Wait a bit before loading document in Layout Debugger when profiling. r=dbaron
    That gives the profiler a chance to attach to a newly created content
    process.  There's no API for waiting for the profiler to be ready in a
    just-created process, unfortunately.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43898
    
    --HG--
    extra : moz-landing-system : lando

--- layout/tools/layout-debug/ui/content/layoutdebug.js
+++ layout/tools/layout-debug/ui/content/layoutdebug.js
@@ -290,6 +290,13 @@ function OnLDBLoad() {
         ["default"],
         ["GeckoMain", "Compositor", "Renderer", "RenderBackend", "StyleThread"]
       );
+      if (gArgs.url) {
+        // Switch to the right kind of content process, and wait a bit so that
+        // the profiler has had a chance to attach to it.
+        updateBrowserRemotenessByURL(gArgs.url);
+        setTimeout(() => loadURI(gArgs.url), 3000);
+        return;
+      }
     } else {
       dump("Cannot profile Layout Debugger; profiler was not compiled in.\n");
     }
