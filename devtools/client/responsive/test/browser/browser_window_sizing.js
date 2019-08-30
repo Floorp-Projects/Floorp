3f0714c0cf01d66c7be2cddd50beceeeced6db2f
Brad Werth <bwerth@mozilla.com>
2019-08-30 18:45:56 +0000

    Bug 1572840 Part 5: Update test expectations. r=gl
    https://phabricator.services.mozilla.com/D42379
    
    Differential Revision: https://phabricator.services.mozilla.com/D43825
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/responsive/test/browser/browser_window_sizing.js
+++ devtools/client/responsive/test/browser/browser_window_sizing.js
@@ -12,6 +12,14 @@ const WIDTH = 375;
 const HEIGHT = 450;
 const ZOOM_LEVELS = [0.3, 0.5, 0.9, 1, 1.5, 2, 2.4];
 
+function promiseContentReflow(ui) {
+  return ContentTask.spawn(ui.getViewportBrowser(), {}, async function() {
+    return new Promise(resolve => {
+      content.window.requestAnimationFrame(resolve);
+    });
+  });
+}
+
 add_task(async function() {
   const tab = await addTab(TEST_URL);
   const browser = tab.linkedBrowser;
@@ -24,27 +32,40 @@ add_task(async function() {
     info(`Setting zoom level to ${ZOOM_LEVELS[i]}`);
     ZoomManager.setZoomForBrowser(browser, ZOOM_LEVELS[i]);
 
-    await checkWindowOuterSize(ui);
-    await checkWindowScreenSize(ui);
+    // We need to ensure that the RDM pane has had time to both change size and
+    // change the zoom level. This is currently not an atomic operation. The event
+    // timing is this:
+    // 1) Pane changes size, content reflows.
+    // 2) Pane changes zoom, content reflows.
+    // So to wait for the post-zoom reflow to be complete, we have two wait on TWO
+    // reflows.
+    await promiseContentReflow(ui);
+    await promiseContentReflow(ui);
+
+    await checkWindowOuterSize(ui, ZOOM_LEVELS[i]);
+    await checkWindowScreenSize(ui, ZOOM_LEVELS[i]);
   }
 });
 
-async function checkWindowOuterSize(ui) {
+async function checkWindowOuterSize(ui, zoom_level) {
   return ContentTask.spawn(
     ui.getViewportBrowser(),
-    { width: WIDTH, height: HEIGHT },
-    async function({ width, height }) {
+    { width: WIDTH, height: HEIGHT, zoom: zoom_level },
+    async function({ width, height, zoom }) {
       // Approximate the outer size value returned on the window content with the expected
-      // value. We should expect, at the very most, a 1px difference between the two due
+      // value. We should expect, at the very most, a 2px difference between the two due
       // to floating point rounding errors that occur when scaling from inner size CSS
       // integer values to outer size CSS integer values. See Part 1 of Bug 1107456.
+      // Some of the drift is also due to full zoom scaling effects; see Bug 1577775.
       ok(
-        Math.abs(content.outerWidth - width) <= 1,
-        `window.outerWidth should be ${width} and we got ${content.outerWidth}.`
+        Math.abs(content.outerWidth - width) <= 2,
+        `window.outerWidth zoom ${zoom} should be ${width} and we got ${
+          content.outerWidth
+        }.`
       );
       ok(
-        Math.abs(content.outerHeight - height) <= 1,
-        `window.outerHeight should be ${height} and we got ${
+        Math.abs(content.outerHeight - height) <= 2,
+        `window.outerHeight zoom ${zoom} should be ${height} and we got ${
           content.outerHeight
         }.`
       );
@@ -52,33 +73,39 @@ async function checkWindowOuterSize(ui) {
   );
 }
 
-async function checkWindowScreenSize(ui) {
+async function checkWindowScreenSize(ui, zoom_level) {
   return ContentTask.spawn(
     ui.getViewportBrowser(),
-    { width: WIDTH, height: HEIGHT },
-    async function({ width, height }) {
+    { width: WIDTH, height: HEIGHT, zoom: zoom_level },
+    async function({ width, height, zoom }) {
       const { screen } = content;
 
       ok(
-        Math.abs(screen.availWidth - width) <= 1,
-        `screen.availWidth should be ${width} and we got ${screen.availWidth}.`
+        Math.abs(screen.availWidth - width) <= 2,
+        `screen.availWidth zoom ${zoom} should be ${width} and we got ${
+          screen.availWidth
+        }.`
       );
 
       ok(
-        Math.abs(screen.availHeight - height) <= 1,
-        `screen.availHeight should be ${height} and we got ${
+        Math.abs(screen.availHeight - height) <= 2,
+        `screen.availHeight zoom ${zoom} should be ${height} and we got ${
           screen.availHeight
         }.`
       );
 
       ok(
-        Math.abs(screen.width - width) <= 1,
-        `screen.width should be ${width} and we got ${screen.width}.`
+        Math.abs(screen.width - width) <= 2,
+        `screen.width zoom " ${zoom} should be ${width} and we got ${
+          screen.width
+        }.`
       );
 
       ok(
-        Math.abs(screen.height - height) <= 1,
-        `screen.height should be ${height} and we got ${screen.height}.`
+        Math.abs(screen.height - height) <= 2,
+        `screen.height zoom " ${zoom} should be ${height} and we got ${
+          screen.height
+        }.`
       );
     }
   );
