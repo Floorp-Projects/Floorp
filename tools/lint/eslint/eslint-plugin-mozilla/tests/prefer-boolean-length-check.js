f3fbc155e40e1660ea3737fa3651e55e89e5c259
monikamaheshwari <monikamaheshwari1996@gmail.com>
2019-09-02 11:09:44 +0000

    Bug 1578190 - Add a rule to ensure 'if(foo.length)' instead of 'foo.length>0'. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D33011

---
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/prefer-boolean-length-check.js
@@ -0,0 +1,97 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require("../lib/rules/prefer-boolean-length-check");
+var RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+function invalidError() {
+  let message = "Prefer boolean length check";
+  return [{ message, type: "BinaryExpression" }];
+}
+
+ruleTester.run("check-length", rule, {
+  valid: [
+    "if (foo.length && foo.length) {}",
+    "if (!foo.length) {}",
+    "if (foo.value == 0) {}",
+    "if (foo.value > 0) {}",
+    "if (0 == foo.value) {}",
+    "if (0 < foo.value) {}",
+    "var a = !!foo.length",
+    "function bar() { return !!foo.length }",
+  ],
+  invalid: [
+    {
+      code: "if (foo.length == 0) {}",
+      output: "if (!foo.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "if (foo.length > 0) {}",
+      output: "if (foo.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "if (0 < foo.length) {}",
+      output: "if (foo.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "if (0 == foo.length) {}",
+      output: "if (!foo.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "if (foo && foo.length == 0) {}",
+      output: "if (foo && !foo.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "if (foo.bar.length == 0) {}",
+      output: "if (!foo.bar.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "var a = foo.length>0",
+      output: "var a = !!foo.length",
+      errors: invalidError(),
+    },
+    {
+      code: "function bar() { return foo.length>0 }",
+      output: "function bar() { return !!foo.length }",
+      errors: invalidError(),
+    },
+    {
+      code: "if (foo && bar.length>0) {}",
+      output: "if (foo && bar.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "while (foo && bar.length>0) {}",
+      output: "while (foo && bar.length) {}",
+      errors: invalidError(),
+    },
+    {
+      code: "x = y && bar.length > 0",
+      output: "x = y && !!bar.length",
+      errors: invalidError(),
+    },
+    {
+      code: "function bar() { return x && foo.length > 0}",
+      output: "function bar() { return x && !!foo.length}",
+      errors: invalidError(),
+    },
+  ],
+});
