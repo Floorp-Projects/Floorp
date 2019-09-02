c454b68fd26ce8c260017277b17879f04df3752e
David Teller <dteller@mozilla.com>
2019-09-02 12:34:12 +0000

    Bug 1572013 - AutoList::done();r=arai
    Depends on D43064
    
    Differential Revision: https://phabricator.services.mozilla.com/D43065

--- js/src/frontend/BinASTTokenReaderContext.cpp
+++ js/src/frontend/BinASTTokenReaderContext.cpp
@@ -1275,9 +1275,7 @@ BinASTTokenReaderContext::AutoList::AutoList(BinASTTokenReaderContext& reader)
 
 void BinASTTokenReaderContext::AutoList::init() { AutoBase::init(); }
 
-JS::Result<Ok> BinASTTokenReaderContext::AutoList::done() {
-  return reader_.raiseError("Not Yet Implemented");
-}
+JS::Result<Ok> BinASTTokenReaderContext::AutoList::done() { return Ok(); }
 
 // Internal uint32_t
 // Note that this is different than varnum in multipart.
