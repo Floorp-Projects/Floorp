fe13bb07e8925f774cdfb7203d690f04737a5f98
David Teller <dteller@mozilla.com>
2019-09-02 13:48:25 +0000

    Bug 1572013 - We may now attempt to read 0 bytes;r=arai
    Depends on D42850
    
    Differential Revision: https://phabricator.services.mozilla.com/D42851
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/frontend/BinASTTokenReaderBase.cpp
+++ js/src/frontend/BinASTTokenReaderBase.cpp
@@ -85,7 +85,6 @@ void BinASTTokenReaderBase::seek(size_t offset) {
 
 JS::Result<Ok> BinASTTokenReaderBase::readBuf(uint8_t* bytes, uint32_t len) {
   MOZ_ASSERT(!hasRaisedError());
-  MOZ_ASSERT(len > 0);
 
   if (stop_ < current_ + len) {
     return raiseError("Buffer exceeds length");
