14cf21c9645ce4d3818838349a6bb37dcadcac75
Johann Hofmann <jhofmann@mozilla.com>
2019-09-02 21:03:05 +0000

    Bug 1576960 - In pageInfo, set security information in init() instead of fetching it from the selected browser. r=carolina.jimenez.g
    This is to avoid displaying incorrect information when the user switches their selected tab but keeps the page info window open.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43736

--- browser/base/content/pageinfo/security.js
+++ browser/base/content/pageinfo/security.js
@@ -24,25 +24,41 @@ ChromeUtils.defineModuleGetter(
 );
 
 var security = {
-  init(uri, windowInfo) {
+  async init(uri, windowInfo) {
     this.uri = uri;
     this.windowInfo = windowInfo;
+    this.securityInfo = await this._getSecurityInfo();
   },
 
-  // Display the server certificate (static)
   viewCert() {
-    var cert = security._cert;
-    viewCertHelper(window, cert);
+    if (Services.prefs.getBoolPref("security.aboutcertificate.enabled")) {
+      let certChain = getCertificateChain(this.securityInfo.certChain);
+      let certs = certChain.map(elem =>
+        encodeURIComponent(elem.getBase64DERString())
+      );
+      let certsStringURL = certs.map(elem => `cert=${elem}`);
+      certsStringURL = certsStringURL.join("&");
+      let url = `about:certificate?${certsStringURL}`;
+      openTrustedLinkIn(url, "tab", {
+        triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
+      });
+    } else {
+      Services.ww.openWindow(
+        window,
+        "chrome://pippki/content/certViewer.xul",
+        "_blank",
+        "centerscreen,chrome",
+        this.securityInfo.cert
+      );
+    }
   },
 
-  _getSecurityInfo() {
+  async _getSecurityInfo() {
     // We don't have separate info for a frame, return null until further notice
     // (see bug 138479)
     if (!this.windowInfo.isTopWindow) {
       return null;
     }
-
-    var hostName = this.windowInfo.hostName;
 
     var ui = security._getSecurityUI();
     if (!ui) {
@@ -54,16 +70,18 @@ var security = {
       ui.state &
       (Ci.nsIWebProgressListener.STATE_LOADED_MIXED_ACTIVE_CONTENT |
         Ci.nsIWebProgressListener.STATE_LOADED_MIXED_DISPLAY_CONTENT);
-    var isInsecure = ui.state & Ci.nsIWebProgressListener.STATE_IS_INSECURE;
     var isEV = ui.state & Ci.nsIWebProgressListener.STATE_IDENTITY_EV_TOPLEVEL;
-    var secInfo = ui.secInfo;
 
-    if (!isInsecure && secInfo) {
-      var cert = secInfo.serverCert;
-      var issuerName = cert.issuerOrganization || cert.issuerName;
+    let secInfo = await window.opener.gBrowser.selectedBrowser.browsingContext.currentWindowGlobal.getSecurityInfo();
+    if (secInfo) {
+      secInfo.QueryInterface(Ci.nsITransportSecurityInfo);
+      let cert = secInfo.serverCert;
+      let issuerName = null;
+      if (cert) {
+        issuerName = cert.issuerOrganization || cert.issuerName;
+      }
 
       var retval = {
-        hostName,
         cAName: issuerName,
         encryptionAlgorithm: undefined,
         encryptionStrength: undefined,
@@ -72,6 +90,7 @@ var security = {
         isMixed,
         isEV,
         cert,
+        certChain: secInfo.succeededCertChain || secInfo.failedCertChain,
         certificateTransparency: undefined,
       };
 
@@ -122,7 +141,6 @@ var security = {
       return retval;
     }
     return {
-      hostName,
       cAName: "",
       encryptionAlgorithm: "",
       encryptionStrength: 0,
@@ -206,18 +224,16 @@ var security = {
    */
   viewPasswords() {
     LoginHelper.openPasswordManager(window, {
-      filterString: this._getSecurityInfo().hostName,
+      filterString: this.windowInfo.hostName,
       entryPoint: "pageinfo",
     });
   },
-
-  _cert: null,
 };
 
-function securityOnLoad(uri, windowInfo) {
-  security.init(uri, windowInfo);
+async function securityOnLoad(uri, windowInfo) {
+  await security.init(uri, windowInfo);
 
-  var info = security._getSecurityInfo();
+  let info = security.securityInfo;
   if (
     !info ||
     (uri.scheme === "about" && !uri.spec.startsWith("about:certerror"))
@@ -228,7 +244,7 @@ function securityOnLoad(uri, windowInfo) {
   document.getElementById("securityTab").hidden = false;
 
   /* Set Identity section text */
-  setText("security-identity-domain-value", info.hostName);
+  setText("security-identity-domain-value", windowInfo.hostName);
 
   var validity;
   if (info.cert && !info.isBroken) {
@@ -276,7 +292,6 @@ function securityOnLoad(uri, windowInfo) {
   /* Manage the View Cert button*/
   var viewCert = document.getElementById("security-view-cert");
   if (info.cert) {
-    security._cert = info.cert;
     viewCert.collapsed = false;
   } else {
     viewCert.collapsed = true;
@@ -306,7 +321,7 @@ function securityOnLoad(uri, windowInfo) {
   document.l10n.setAttributes(
     document.getElementById("security-privacy-history-value"),
     "security-visits-number",
-    { visits: previousVisitCount(info.hostName) }
+    { visits: previousVisitCount(windowInfo.hostName) }
   );
 
   /* Set the Technical Detail section messages */
@@ -335,12 +350,11 @@ function securityOnLoad(uri, windowInfo) {
     );
     msg1 = pkiBundle.getString("pageInfo_Privacy_Encrypted1");
     msg2 = pkiBundle.getString("pageInfo_Privacy_Encrypted2");
-    security._cert = info.cert;
   } else {
     hdr = pkiBundle.getString("pageInfo_NoEncryption");
-    if (info.hostName != null) {
+    if (windowInfo.hostName != null) {
       msg1 = pkiBundle.getFormattedString("pageInfo_Privacy_None1", [
-        info.hostName,
+        windowInfo.hostName,
       ]);
     } else {
       msg1 = pkiBundle.getString("pageInfo_Privacy_None4");
@@ -382,35 +396,6 @@ function getCertificateChain(certChain, options = {}) {
     certificates.push(cert);
   }
   return certificates;
-}
-
-function viewCertHelper(parent, cert) {
-  if (!cert) {
-    return;
-  }
-
-  if (Services.prefs.getBoolPref("security.aboutcertificate.enabled")) {
-    let ui = security._getSecurityUI();
-    let securityInfo = ui.secInfo;
-    let certChain = getCertificateChain(securityInfo.succeededCertChain);
-    let certs = certChain.map(elem =>
-      encodeURIComponent(elem.getBase64DERString())
-    );
-    let certsStringURL = certs.map(elem => `cert=${elem}`);
-    certsStringURL = certsStringURL.join("&");
-    let url = `about:certificate?${certsStringURL}`;
-    openTrustedLinkIn(url, "tab", {
-      triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
-    });
-  } else {
-    Services.ww.openWindow(
-      parent,
-      "chrome://pippki/content/certViewer.xul",
-      "_blank",
-      "centerscreen,chrome",
-      cert
-    );
-  }
 }
 
 /**
