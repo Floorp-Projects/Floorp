4a21e380a7a182b29d42510d31ed925676241b4f
Justin Wood <Callek@gmail.com>
2019-09-02 11:53:10 -0400

    Correct Typo - Bug 1578263 - Use release-type in param eval rather than release-level. r=jlorenzo a=release for CLOSED TREE

--- taskcluster/taskgraph/transforms/release_beetmover_signed_addons.py
+++ taskcluster/taskgraph/transforms/release_beetmover_signed_addons.py
@@ -66,7 +66,7 @@ def resolve_keys(config, jobs):
             resolve_keyed_by(
                 job, field, item_name=job['label'],
                 **{
-                    'release-type': config.params['release-type'],
+                    'release-type': config.params['release_type'],
                     'project': config.params['project']
                 }
             )
