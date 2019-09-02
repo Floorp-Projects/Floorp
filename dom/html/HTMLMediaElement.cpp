869a0bf5ea82a34f5c2457f2c8243d5a5dfa56dd
Csoregi Natalia <ncsoregi@mozilla.com>
2019-09-02 16:30:05 +0300

    Backed out 2 changesets (bug 1576836) for media failures on DOMMediaStream.cpp. CLOSED TREE
    Backed out changeset 6c0f218a2988 (bug 1576836)
    Backed out changeset 481ab3a1f913 (bug 1576836)

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -4720,8 +4720,10 @@ class HTMLMediaElement::MediaStreamTrackListener
     mElement->NotifyMediaStreamTrackRemoved(aTrack);
   }
 
-  void OnActive() {
-    MOZ_ASSERT(mElement);
+  void NotifyActive() override {
+    if (!mElement) {
+      return;
+    }
 
     // mediacapture-main says:
     // Note that once ended equals true the HTMLVideoElement will not play media
@@ -4754,68 +4756,18 @@ class HTMLMediaElement::MediaStreamTrackListener
     mElement->DoLoad();
   }
 
-  void NotifyActive() override {
+  void NotifyInactive() override {
     if (!mElement) {
       return;
     }
-
-    if (!mElement->IsVideo()) {
-      // Audio elements use NotifyAudible().
-      return;
-    }
-
-    OnActive();
-  }
-
-  void NotifyAudible() override {
-    if (!mElement) {
-      return;
-    }
-
-    if (mElement->IsVideo()) {
-      // Video elements use NotifyActive().
-      return;
-    }
-
-    OnActive();
-  }
-
-  void OnInactive() {
-    MOZ_ASSERT(mElement);
-
     if (mElement->IsPlaybackEnded()) {
       return;
     }
     LOG(LogLevel::Debug, ("%p, mSrcStream %p became inactive", mElement.get(),
                           mElement->mSrcStream.get()));
+    MOZ_ASSERT(!mElement->mSrcStream->Active());
     mElement->PlaybackEnded();
     mElement->UpdateReadyStateInternal();
-  }
-
-  void NotifyInactive() override {
-    if (!mElement) {
-      return;
-    }
-
-    if (!mElement->IsVideo()) {
-      // Audio elements use NotifyInaudible().
-      return;
-    }
-
-    OnInactive();
-  }
-
-  void NotifyInaudible() override {
-    if (!mElement) {
-      return;
-    }
-
-    if (mElement->IsVideo()) {
-      // Video elements use NotifyInactive().
-      return;
-    }
-
-    OnInactive();
   }
 
  protected:
