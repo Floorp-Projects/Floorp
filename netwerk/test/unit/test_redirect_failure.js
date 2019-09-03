10683d0d2a16a0800bce2037782fa43f25cd7c31
Honza Bambas <honzab.moz@firemni.cz>
2019-09-03 12:24:11 +0000

    Bug 1559088 - Fix test_redirect-caching_failure.js to again do what it has to, r=kershaw, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D43796

--- netwerk/test/unit/test_redirect_failure.js
+++ netwerk/test/unit/test_redirect_failure.js
@@ -1,5 +1,19 @@
 const { HttpServer } = ChromeUtils.import("resource://testing-common/httpd.js");
 
+/*
+ * The test is checking async redirect code path that is loading a
+ * redirect.  But creation of the target channel fails before we even try
+ * to do async open on it. We force the creation error by forbidding
+ * the port number the URI contains.
+ */
+
+function inChildProcess() {
+  return (
+    Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
+      .processType != Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT
+  );
+}
+
 XPCOMUtils.defineLazyGetter(this, "URL", function() {
   return "http://localhost:" + httpServer.identity.primaryPort;
 });
@@ -18,16 +32,12 @@ function make_channel(url, callback, ctx) {
 
 function redirectHandler(metadata, response) {
   response.setStatusLine(metadata.httpVersion, 301, "Moved");
-  response.setHeader(
-    "Location",
-    "httpx://localhost:" + httpServer.identity.primaryPort + "/content",
-    false
-  );
+  response.setHeader("Location", "http://non-existent.tld:65400", false);
   response.setHeader("Cache-Control", "no-cache", false);
 }
 
 function finish_test(request, buffer) {
-  Assert.equal(request.status, Cr.NS_ERROR_UNKNOWN_PROTOCOL);
+  Assert.equal(request.status, Cr.NS_ERROR_PORT_ACCESS_NOT_ALLOWED);
 
   Assert.equal(buffer, "");
   httpServer.stop(do_test_finished);
@@ -37,6 +47,10 @@ function run_test() {
   httpServer = new HttpServer();
   httpServer.registerPathHandler(randomPath, redirectHandler);
   httpServer.start(-1);
+
+  if (!inChildProcess()) {
+    Services.prefs.setCharPref("network.security.ports.banned", "65400");
+  }
 
   var chan = make_channel(randomURI);
   chan.asyncOpen(new ChannelListener(finish_test, null, CL_EXPECT_FAILURE));
--- netwerk/test/unit_ipc/test_redirect_failure_wrap.js
+++ netwerk/test/unit_ipc/test_redirect_failure_wrap.js
@@ -1,7 +1,10 @@
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
 //
 // Run test script in content process instead of chrome (xpcshell's default)
 //
 
 function run_test() {
+  Services.prefs.setCharPref("network.security.ports.banned", "65400");
   run_test_in_child("../unit/test_redirect_failure.js");
 }
