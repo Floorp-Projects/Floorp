89f3c31f6092e62d0a2529a8a8624ca61e11d897
Dão Gottwald <dao@mozilla.com>
2019-09-03 18:32:32 +0000

    Bug 1578375 - Stop flushing layout in SearchOneOffs::__rebuild when used with the address bar. r=adw a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D44449

--- browser/base/content/test/performance/browser_urlbar_search.js
+++ browser/base/content/test/performance/browser_urlbar_search.js
@@ -13,26 +13,7 @@
  */
 
 /* These reflows happen only the first time the panel opens. */
-const EXPECTED_REFLOWS_FIRST_OPEN = [
-  {
-    stack: [
-      "__rebuild@chrome://browser/content/search/search-one-offs.js",
-      /* This is limited to a one-line stack, because the next item is an async
-         function and as such not supported on all trees, according to bug 1501761.
-      "async*_rebuild@chrome://browser/content/search/search-one-offs.js",
-      "async*_on_popupshowing@chrome://browser/content/search/search-one-offs.js",
-      "handleEvent@chrome://browser/content/search/search-one-offs.js",
-      "_openPanel@resource:///modules/UrlbarView.jsm",
-      "onQueryResults@resource:///modules/UrlbarView.jsm",
-      "_notify@resource:///modules/UrlbarController.jsm",
-      "receiveResults@resource:///modules/UrlbarController.jsm",
-      "notifyResults@resource:///modules/UrlbarProvidersManager.jsm",
-      "add@resource:///modules/UrlbarProvidersManager.jsm",
-      "onSearchResult@resource:///modules/UrlbarProviderUnifiedComplete.jsm",
-      */
-    ],
-  },
-];
+const EXPECTED_REFLOWS_FIRST_OPEN = [];
 
 /* These reflows happen every time the panel opens. */
 const EXPECTED_REFLOWS_SECOND_OPEN = [];
