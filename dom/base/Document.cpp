74b9acaaa2a068eb8096775d6ac0bdb6374fcbd7
Emilio Cobos Álvarez <emilio@crisal.io>
2019-09-04 21:47:57 +0000

    Bug 1578700 - Rename a variable to better follow our coding style. r=smaug
    It's a constant, not a global. This matches both the Mozilla and Google coding
    styles so should hopefully be uncontroversial.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44643

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -14441,7 +14441,8 @@ bool Document::InlineScriptAllowedByCSP() {
 }
 
 void Document::ReportUseCounters() {
-  static const bool sDebugUseCounters = false;
+  static const bool kDebugUseCounters = false;
+
   if (mReportedUseCounters) {
     return;
   }
@@ -14457,7 +14458,7 @@ void Document::ReportUseCounters() {
       return;
     }
 
-    if (sDebugUseCounters) {
+    if (kDebugUseCounters) {
       nsCString spec = uri->GetSpecOrDefault();
 
       // URIs can be rather long for data documents, so truncate them to
@@ -14503,7 +14504,7 @@ void Document::ReportUseCounters() {
       bool value = GetUseCounter(uc);
 
       if (value) {
-        if (sDebugUseCounters) {
+        if (kDebugUseCounters) {
           const char* name = Telemetry::GetHistogramName(id);
           if (name) {
             printf("  %s", name);
@@ -14522,7 +14523,7 @@ void Document::ReportUseCounters() {
         value = GetUseCounter(uc) || GetChildDocumentUseCounter(uc);
 
         if (value) {
-          if (sDebugUseCounters) {
+          if (kDebugUseCounters) {
             const char* name = Telemetry::GetHistogramName(id);
             if (name) {
               printf("  %s", name);
