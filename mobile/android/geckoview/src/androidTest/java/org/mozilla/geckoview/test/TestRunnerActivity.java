0899c12837dc8759a33a1861a97ddffb0ce01bf5
James Willcox <snorp@snorp.net>
2019-09-04 21:25:44 +0000

    Bug 1343678 - Add WebPush support to GeckoView r=jcj,lina,agi,geckoview-reviewers,droeh,mt
    Differential Revision: https://phabricator.services.mozilla.com/D41290

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerActivity.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/TestRunnerActivity.java
@@ -23,6 +23,8 @@ import android.content.Intent;
 import android.graphics.SurfaceTexture;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.Surface;
 
 import java.util.HashMap;
@@ -39,6 +41,18 @@ public class TestRunnerActivity extends Activity {
     private boolean mKillProcessOnDestroy;
 
     private HashMap<GeckoSession, GeckoDisplay> mDisplays = new HashMap<>();
+
+    private GeckoSession.PermissionDelegate mPermissionDelegate = new GeckoSession.PermissionDelegate() {
+        @Override
+        public void onContentPermissionRequest(@NonNull GeckoSession session, @Nullable String uri, int type, @NonNull Callback callback) {
+            callback.grant();
+        }
+
+        @Override
+        public void onAndroidPermissionsRequest(@NonNull GeckoSession session, @Nullable String[] permissions, @NonNull Callback callback) {
+            callback.grant();
+        }
+    };
 
     private GeckoSession.NavigationDelegate mNavigationDelegate = new GeckoSession.NavigationDelegate() {
         @Override
@@ -134,6 +148,7 @@ public class TestRunnerActivity extends Activity {
         final GeckoSession session = new GeckoSession(settings);
         session.setNavigationDelegate(mNavigationDelegate);
         session.setContentDelegate(mContentDelegate);
+        session.setPermissionDelegate(mPermissionDelegate);
         return session;
     }
 
