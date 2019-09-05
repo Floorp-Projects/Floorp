05742a0dfe0dc875ea35a34b20a6d9fde41905b8
Junior Hsu <juhsu@mozilla.com>
2019-09-05 21:27:37 +0000

    Bug 1571573 - P1 use node in toolchain-artifact for xpcshell r=tomprince
    Differential Revision: https://phabricator.services.mozilla.com/D44898

--- testing/mozharness/configs/unittests/win_unittest.py
+++ testing/mozharness/configs/unittests/win_unittest.py
@@ -6,6 +6,8 @@ import sys
 ABS_WORK_DIR = os.path.join(os.getcwd(), "build")
 BINARY_PATH = os.path.join(ABS_WORK_DIR, "firefox", "firefox.exe")
 INSTALLER_PATH = os.path.join(ABS_WORK_DIR, "installer.zip")
+NODEJS_PATH = os.path.join(os.environ["MOZ_FETCHES_DIR"], "node/node.exe")
+
 XPCSHELL_NAME = 'xpcshell.exe'
 EXE_SUFFIX = '.exe'
 DISABLE_SCREEN_SAVER = False
@@ -283,7 +285,7 @@ config = {
                              },
     "minidump_stackwalk_path": "win32-minidump_stackwalk.exe",
     "minidump_tooltool_manifest_path": "config/tooltool-manifests/win32/releng.manifest",
-    "download_nodejs": True,
-    "nodejs_path": "node-win32.exe",
+    "download_nodejs": False,
+    "nodejs_path": NODEJS_PATH,
     "nodejs_tooltool_manifest_path": "config/tooltool-manifests/win32/nodejs.manifest",
 }
