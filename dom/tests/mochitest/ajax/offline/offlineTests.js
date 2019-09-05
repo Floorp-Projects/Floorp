99bfbf1328d5d3d6c0b2d3d2d59cb48d46dc818e
Paul Zuehlcke <pzuhlcke@mozilla.com>
2019-09-05 13:37:55 +0000

    Bug 1574480 - Updated browser.js and OfflineAppsChild.jsm to use PermissionManager principal methods. r=johannh,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D42620
    
    --HG--
    extra : moz-landing-system : lando

--- dom/tests/mochitest/ajax/offline/offlineTests.js
+++ dom/tests/mochitest/ajax/offline/offlineTests.js
@@ -59,8 +59,6 @@ fetch: function(callback)
 
 var OfflineTest = {
 
-_allowedByDefault: false,
-
 _hasSlave: false,
 
 // The window where test results should be sent.
@@ -74,17 +72,7 @@ _SJSsStated: [],
 
 setupChild: function()
 {
-  if (this._allowedByDefault) {
-    this._masterWindow = window;
-    return true;
-  }
-
-  if (window.parent.OfflineTest._hasSlave) {
-    return false;
-  }
-
-  this._masterWindow = window.top;
-
+  this._masterWindow = window;
   return true;
 },
 
@@ -97,41 +85,8 @@ setupChild: function()
  */
 setup: function()
 {
-  try {
-    this._allowedByDefault = SpecialPowers.getBoolPref("offline-apps.allow_by_default");
-  } catch (e) {}
-
-  if (this._allowedByDefault) {
     this._masterWindow = window;
-
     return true;
-  }
-
-  if (!window.opener || !window.opener.OfflineTest ||
-      !window.opener.OfflineTest._hasSlave) {
-    // Offline applications must be toplevel windows and have the
-    // offline-app permission.  Because we were loaded without the
-    // offline-app permission and (probably) in an iframe, we need to
-    // enable the pref and spawn a new window to perform the actual
-    // tests.  It will use this window to report successes and
-    // failures.
-
-    if (SpecialPowers.testPermission("offline-app", Ci.nsIPermissionManager.ALLOW_ACTION, document)) {
-      ok(false, "Previous test failed to clear offline-app permission!  Expect failures.");
-    }
-    SpecialPowers.addPermission("offline-app", Ci.nsIPermissionManager.ALLOW_ACTION, document);
-
-    // Tests must run as toplevel windows.  Open a slave window to run
-    // the test.
-    this._hasSlave = true;
-    window.open(window.location, "offlinetest");
-
-    return false;
-  }
-
-  this._masterWindow = window.opener;
-
-  return true;
 },
 
 teardownAndFinish: function()
@@ -160,16 +115,7 @@ teardown: function(callback)
 
 finish: function()
 {
-  if (this._allowedByDefault) {
-    SimpleTest.executeSoon(SimpleTest.finish);
-  } else if (this._masterWindow) {
-    // Slave window: pass control back to master window, close itself.
-    this._masterWindow.SimpleTest.executeSoon(this._masterWindow.OfflineTest.finish);
-    window.close();
-  } else {
-    // Master window: finish test.
-    SimpleTest.finish();
-  }
+  SimpleTest.executeSoon(SimpleTest.finish);
 },
 
 //
