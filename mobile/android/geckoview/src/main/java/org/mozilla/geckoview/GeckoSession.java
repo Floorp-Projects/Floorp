428bf5599543ef9a0ae2cdfeb45a0e4293af461b
James Willcox <snorp@snorp.net>
2019-09-05 13:59:32 +0000

    Bug 1343678 - Enable web push pref for GeckoView r=geckoview-reviewers,droeh,smaug,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D41288

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -796,6 +796,11 @@ public class GeckoSession implements Parcelable {
                         type = PermissionDelegate.PERMISSION_DESKTOP_NOTIFICATION;
                     } else if ("persistent-storage".equals(typeString)) {
                         type = PermissionDelegate.PERMISSION_PERSISTENT_STORAGE;
+                    } else if ("midi".equals(typeString)) {
+                        // We can get this from WPT and presumably other content, but Gecko
+                        // doesn't support Web MIDI.
+                        callback.sendError("Unsupported");
+                        return;
                     } else {
                         throw new IllegalArgumentException("Unknown permission request: " + typeString);
                     }
