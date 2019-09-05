1a9552b2adaa537abaa5c9ba2ac8e4e969f2ab35
Dorel Luca <dluca@mozilla.com>
2019-09-05 09:47:35 +0300

    Backed out changeset 890bcaee9b7d (bug 1529068) for causing massive tier2 failures on central in dom/security/test/csp/test_navigate_to.html. CLOSED TREE

--- dom/security/nsCSPUtils.cpp
+++ dom/security/nsCSPUtils.cpp
@@ -316,7 +316,6 @@ CSPDirective CSP_ContentTypeToDirective(nsContentPolicyType aType) {
       return nsIContentSecurityPolicy::NO_DIRECTIVE;
 
     // Fall through to error for all other directives
-    // Note that we should never end up here for navigate-to
     default:
       MOZ_ASSERT(false, "Can not map nsContentPolicyType to CSPDirective");
   }
@@ -1472,31 +1471,6 @@ bool nsCSPPolicy::hasDirective(CSPDirective aDir) const {
     }
   }
   return false;
-}
-
-bool nsCSPPolicy::allowsNavigateTo(nsIURI* aURI, bool aWasRedirected,
-                                   bool aEnforceWhitelist) const {
-  bool allowsNavigateTo = true;
-
-  for (unsigned long i = 0; i < mDirectives.Length(); i++) {
-    if (mDirectives[i]->equals(
-            nsIContentSecurityPolicy::NAVIGATE_TO_DIRECTIVE)) {
-      // Early return if we can skip the whitelist AND 'unsafe-allow-redirects'
-      // is present.
-      if (!aEnforceWhitelist &&
-          mDirectives[i]->allows(CSP_UNSAFE_ALLOW_REDIRECTS, EmptyString(),
-                                 false)) {
-        return true;
-      }
-      // Otherwise, check against the whitelist.
-      if (!mDirectives[i]->permits(aURI, EmptyString(), aWasRedirected, false,
-                                   false, false)) {
-        allowsNavigateTo = false;
-      }
-    }
-  }
-
-  return allowsNavigateTo;
 }
 
 /*
