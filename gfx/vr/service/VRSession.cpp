980e0a300d0f339d35b4c2bfe360efdaaa10a29d
thomasmo <thomasmo@mozilla.com>
2019-09-06 16:48:56 +0000

    Bug 1576975 - Compile VRSession.cpp into vrhost.dll r=kip
    Includes changes for separating mozilla-central-specific code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43859

--- gfx/vr/service/VRSession.cpp
+++ gfx/vr/service/VRSession.cpp
@@ -12,25 +12,56 @@
 #  include <d3d11.h>
 #endif  // defined(XP_WIN)
 
+#ifndef MOZILLA_INTERNAL_API
+#  define NS_WARNING(s)
+#endif
+
 using namespace mozilla::gfx;
 
-VRSession::VRSession() : mShouldQuit(false) {}
+VRSession::VRSession()
+    : mShouldQuit(false)
+#ifdef XP_WIN
+      ,
+      mDevice(nullptr),
+      mContext(nullptr),
+      mDeviceContextState(nullptr)
+#endif
+{
+}
+
+#ifdef XP_WIN
+VRSession::~VRSession() {
+  if (mDevice != nullptr) {
+    mDevice->Release();
+    mDevice = nullptr;
+  }
+
+  if (mContext != nullptr) {
+    mContext->Release();
+    mContext = nullptr;
+  }
+
+  if (mDeviceContextState != nullptr) {
+    mDeviceContextState->Release();
+    mDeviceContextState = nullptr;
+  }
+}
+#endif
 
 #if defined(XP_WIN)
-bool VRSession::CreateD3DContext(RefPtr<ID3D11Device> aDevice) {
+bool VRSession::CreateD3DContext(ID3D11Device* aDevice) {
   if (!mDevice) {
     if (!aDevice) {
       NS_WARNING("VRSession::CreateD3DObjects failed to get a D3D11Device");
       return false;
     }
-    if (FAILED(aDevice->QueryInterface(__uuidof(ID3D11Device1),
-                                       getter_AddRefs(mDevice)))) {
+    if (FAILED(aDevice->QueryInterface(IID_PPV_ARGS(&mDevice)))) {
       NS_WARNING("VRSession::CreateD3DObjects failed to get a D3D11Device1");
       return false;
     }
   }
   if (!mContext) {
-    mDevice->GetImmediateContext1(getter_AddRefs(mContext));
+    mDevice->GetImmediateContext1(&mContext);
     if (!mContext) {
       NS_WARNING(
           "VRSession::CreateD3DObjects failed to get an immediate context");
@@ -42,7 +73,7 @@ bool VRSession::CreateD3DContext(RefPtr<ID3D11Device> aDevice) {
                                       D3D_FEATURE_LEVEL_11_0};
     mDevice->CreateDeviceContextState(0, featureLevels, 2, D3D11_SDK_VERSION,
                                       __uuidof(ID3D11Device1), nullptr,
-                                      getter_AddRefs(mDeviceContextState));
+                                      &mDeviceContextState);
   }
   if (!mDeviceContextState) {
     NS_WARNING(
@@ -65,44 +96,46 @@ ID3DDeviceContextState* VRSession::GetD3DDeviceContextState() {
 bool VRSession::SubmitFrame(
     const mozilla::gfx::VRLayer_Stereo_Immersive& aLayer) {
 #if defined(XP_WIN)
-
+  bool success = false;
   if (aLayer.textureType ==
       VRLayerTextureType::LayerTextureType_D3D10SurfaceDescriptor) {
-    RefPtr<ID3D11Texture2D> dxTexture;
-    HRESULT hr = mDevice->OpenSharedResource(
-        (HANDLE)aLayer.textureHandle, __uuidof(ID3D11Texture2D),
-        (void**)(ID3D11Texture2D**)getter_AddRefs(dxTexture));
-    if (FAILED(hr) || !dxTexture) {
+    ID3D11Texture2D* dxTexture = nullptr;
+    HRESULT hr = mDevice->OpenSharedResource((HANDLE)aLayer.textureHandle,
+                                             IID_PPV_ARGS(&dxTexture));
+    if (SUCCEEDED(hr) && dxTexture != nullptr) {
+      // Similar to LockD3DTexture in TextureD3D11.cpp
+      IDXGIKeyedMutex* mutex = nullptr;
+      hr = dxTexture->QueryInterface(IID_PPV_ARGS(&mutex));
+      if (SUCCEEDED(hr)) {
+        hr = mutex->AcquireSync(0, 1000);
+#  ifdef MOZILLA_INTERNAL_API
+        if (hr == WAIT_TIMEOUT) {
+          gfxDevCrash(LogReason::D3DLockTimeout) << "D3D lock mutex timeout";
+        } else if (hr == WAIT_ABANDONED) {
+          gfxCriticalNote << "GFX: D3D11 lock mutex abandoned";
+        }
+#  endif
+        if (SUCCEEDED(hr)) {
+          success = SubmitFrame(aLayer, dxTexture);
+          hr = mutex->ReleaseSync(0);
+          if (FAILED(hr)) {
+            NS_WARNING("Failed to unlock the texture");
+          }
+        } else {
+          NS_WARNING("Failed to lock the texture");
+        }
+
+        mutex->Release();
+        mutex = nullptr;
+      }
+
+      dxTexture->Release();
+      dxTexture = nullptr;
+    } else {
       NS_WARNING("Failed to open shared texture");
-      return false;
     }
 
-    // Similar to LockD3DTexture in TextureD3D11.cpp
-    RefPtr<IDXGIKeyedMutex> mutex;
-    dxTexture->QueryInterface((IDXGIKeyedMutex**)getter_AddRefs(mutex));
-    if (mutex) {
-      HRESULT hr = mutex->AcquireSync(0, 1000);
-      if (hr == WAIT_TIMEOUT) {
-        gfxDevCrash(LogReason::D3DLockTimeout) << "D3D lock mutex timeout";
-      } else if (hr == WAIT_ABANDONED) {
-        gfxCriticalNote << "GFX: D3D11 lock mutex abandoned";
-      }
-      if (FAILED(hr)) {
-        NS_WARNING("Failed to lock the texture");
-        return false;
-      }
-    }
-    bool success = SubmitFrame(aLayer, dxTexture);
-    if (mutex) {
-      HRESULT hr = mutex->ReleaseSync(0);
-      if (FAILED(hr)) {
-        NS_WARNING("Failed to unlock the texture");
-      }
-    }
-    if (!success) {
-      return false;
-    }
-    return true;
+    return SUCCEEDED(hr) && success;
   }
 
 #elif defined(XP_MACOSX)
