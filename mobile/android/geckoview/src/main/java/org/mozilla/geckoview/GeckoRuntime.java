5ee7c5e622ae3161f45686e5e81163826799fae3
Dylan Roeh <droeh@mozilla.com>
2019-09-06 15:14:36 +0000

    Bug 1557009 - Add ContentBlockingController to GeckoSession to allow managing exceptions list for content blocking. r=snorp,geckoview-reviewers,Ehsan,agi
    Differential Revision: https://phabricator.services.mozilla.com/D43758
    
    --HG--
    extra : moz-landing-system : lando

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -166,10 +166,12 @@ public final class GeckoRuntime implements Parcelable {
     private StorageController mStorageController;
     private final WebExtensionController mWebExtensionController;
     private WebPushController mPushController;
+    private final ContentBlockingController mContentBlockingController;
 
     private GeckoRuntime() {
         mWebExtensionDispatcher = new WebExtensionEventDispatcher();
         mWebExtensionController = new WebExtensionController(this, mWebExtensionDispatcher);
+        mContentBlockingController = new ContentBlockingController();
         if (sRuntime != null) {
             throw new IllegalStateException("Only one GeckoRuntime instance is allowed");
         }
@@ -354,6 +356,16 @@ public final class GeckoRuntime implements Parcelable {
     @UiThread
     public @NonNull WebExtensionController getWebExtensionController() {
         return mWebExtensionController;
+    }
+
+    /**
+     * Returns the ContentBlockingController for this GeckoRuntime.
+     *
+     * @return An instance of {@link ContentBlockingController}.
+     */
+    @UiThread
+    public @NonNull ContentBlockingController getContentBlockingController() {
+        return mContentBlockingController;
     }
 
     /**
