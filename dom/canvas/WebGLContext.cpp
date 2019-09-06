ef96affe67f5a33bfd5d46cb5b9e155c360f4765
Imanol Fernandez <mortimergoro@gmail.com>
2019-09-06 20:55:51 +0000

    Bug 1579028 - Do not call surface->Commit() if composition failed in WebGLContext::GetVRFrame. r=jgilbert,rbarker
    PresentScreenBuffer() or screen->PublishFrame() may fail for different reasons. In that case we shouldn't call surface->Commit() because it may be in a not swaped state or not available.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44813
    
    --HG--
    extra : moz-landing-system : lando

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -2162,7 +2162,9 @@ WebGLContext::GetVRFrame() {
   if (IsContextLost()) return nullptr;
 
   RefPtr<SharedSurfaceTextureClient> sharedSurface = mVRScreen->Front();
-  if (!sharedSurface || !sharedSurface->Surf()) return nullptr;
+  if (!sharedSurface || !sharedSurface->Surf() ||
+      !sharedSurface->Surf()->IsBufferAvailable())
+    return nullptr;
 
   // Make sure that the WebGL buffer is committed to the attached SurfaceTexture
   // on Android.
