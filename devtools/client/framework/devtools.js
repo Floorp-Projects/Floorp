0517cc9eb0482df20b0621b49f5c7bbe105a1552
Daisuke Akatsuka <daisuke@birchill.co.jp>
2019-09-06 03:25:43 +0000

    Bug 1571942: Avoid to raise the toolbox whenever the shortcut key of inspector was typed. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D44772

--- devtools/client/framework/devtools.js
+++ devtools/client/framework/devtools.js
@@ -490,6 +490,8 @@ DevTools.prototype = {
    *        opening started. This is a `Cu.now()` timing.
    * @param {string} reason
    *        Reason the tool was opened
+   * @param {boolean} shouldRaiseToolbox
+   *        Whether we need to raise the toolbox or not.
    *
    * @return {Toolbox} toolbox
    *        The toolbox that was opened
@@ -500,7 +502,8 @@ DevTools.prototype = {
     hostType,
     hostOptions,
     startTime,
-    reason = "toolbox_show"
+    reason = "toolbox_show",
+    shouldRaiseToolbox = true
   ) {
     let toolbox = this._toolboxes.get(target);
 
@@ -515,7 +518,9 @@ DevTools.prototype = {
         await toolbox.selectTool(toolId, reason);
       }
 
-      toolbox.raise();
+      if (shouldRaiseToolbox) {
+        toolbox.raise();
+      }
     } else {
       // As toolbox object creation is async, we have to be careful about races
       // Check for possible already in process of loading toolboxes before
