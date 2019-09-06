d11711fe9b3e7fa07291c7a0f650b42d6e98d8ed
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2019-09-06 14:30:27 +0000

    Bug 1567848 - make XRE_IsE10sParentProcess always tell the truth on fennec, r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D38907
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -223,6 +223,7 @@
 
 #ifdef MOZ_WIDGET_ANDROID
 #  include "GeneratedJNIWrappers.h"
+#  include "mozilla/jni/Utils.h"  // for mozilla::jni::IsFennec()
 #endif
 
 #if defined(MOZ_SANDBOX)
@@ -4889,7 +4890,12 @@ GeckoProcessType XRE_GetProcessType() {
 }
 
 bool XRE_IsE10sParentProcess() {
+#ifdef MOZ_WIDGET_ANDROID
+  return XRE_IsParentProcess() && mozilla::jni::IsAvailable() &&
+         !mozilla::jni::IsFennec() && BrowserTabsRemoteAutostart();
+#else
   return XRE_IsParentProcess() && BrowserTabsRemoteAutostart();
+#endif
 }
 
 #define GECKO_PROCESS_TYPE(enum_name, string_name, xre_name, bin_type) \
