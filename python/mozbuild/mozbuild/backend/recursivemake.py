73e529409a135090bbdf52839684b7829c582fe4
Mike Hommey <mh+mozilla@glandium.org>
2019-09-07 22:45:47 +0000

    Bug 1579156 - Fix binding tests after bug 1573566. r=nalexander
    The build system has no clue that there is something to build in
    dom/bindings/test. It's currently all handled via make rules generated
    by the backend, but ideally, this would all be handled by the frontend
    emitting appropriate GeneratedFiles and Sources objects.
    
    In the meanwhile, we just force the make backend to recurse through
    dom/bindings/test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45124
    
    --HG--
    extra : moz-landing-system : lando

--- python/mozbuild/mozbuild/backend/recursivemake.py
+++ python/mozbuild/mozbuild/backend/recursivemake.py
@@ -1827,3 +1827,8 @@ class RecursiveMakeBackend(CommonBackend):
         webidls_mk = mozpath.join(bindings_dir, 'webidlsrcs.mk')
         with self._write_file(webidls_mk) as fh:
             mk.dump(fh, removal_guard=False)
+
+        # Add the test directory to the compile graph.
+        self._compile_graph[mozpath.join(
+            mozpath.relpath(bindings_dir, self.environment.topobjdir),
+            'test', 'target-objects')]
