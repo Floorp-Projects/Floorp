36a6b0e807fd2fe1d92be3d712d47d3e49f5ab8c
Brian Hackett <bhackett1024@gmail.com>
2019-09-08 01:11:43 +0000

    Bug 1577007 - Add debugger method to compare underlying natives, r=jimb.
    Differential Revision: https://phabricator.services.mozilla.com/D43680

---
+++ js/src/jit-test/tests/debug/Object-isSameNative.js
@@ -0,0 +1,46 @@
+// Test that the onNativeCall hook is called when expected.
+
+load(libdir + 'eqArrayHelper.js');
+
+var g = newGlobal({newCompartment: true});
+var dbg = Debugger(g);
+var gdbg = dbg.addDebuggee(g);
+
+assertEq(gdbg.getProperty("print").return.isSameNative(print), true);
+assertEq(gdbg.getProperty("print").return.isSameNative(newGlobal), false);
+
+g.eval(`
+const x = [];
+Object.defineProperty(x, "a", {
+  get: print,
+  set: print,
+});
+function f() {
+  x.a++;
+  x.length = 0;
+  x.push(4, 5, 6);
+  x.sort(print);
+}
+`);
+
+const comparisons = [
+  print,
+  Array.prototype.push,
+  Array.prototype.sort, // Note: self-hosted
+  newGlobal
+];
+
+const rv = [];
+dbg.onNativeCall = (callee, reason) => {
+  for (const fn of comparisons) {
+    if (callee.isSameNative(fn)) {
+      rv.push(fn.name);
+    }
+  }
+}
+
+for (let i = 0; i < 5; i++) {
+  rv.length = 0;
+  gdbg.executeInGlobal(`f()`);
+  assertEqArray(rv, ["print", "print", "push", "sort", "print", "print"]);
+}
