e0eb251ca22ead438dfcd9f3ffb05e4229f96d0d
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2019-09-09 14:15:50 +0000

    Bug 1552041 - Part 2. Don't open virtual keyboard via chrome script. r=geckoview-reviewers,snorp
    Even if `element.focus` is called from chrome, we always open virtual keyboard.
    But I would like to change this behaviour that virtual keyboard isn't opened
    via `element.focus` that is chrome script.
    
    Depends on D44104
    
    Differential Revision: https://phabricator.services.mozilla.com/D44105
    
    --HG--
    extra : moz-landing-system : lando

--- widget/android/GeckoEditableSupport.cpp
+++ widget/android/GeckoEditableSupport.cpp
@@ -1529,6 +1529,8 @@ void GeckoEditableSupport::NotifyIMEContext(const InputContext& aContext,
   // shouldn't open it.
   const bool isUserAction =
       aAction.mCause != InputContextAction::CAUSE_LONGPRESS &&
+      !(aAction.mCause == InputContextAction::CAUSE_UNKNOWN_CHROME &&
+        aContext.mIMEState.mEnabled == IMEState::ENABLED) &&
       (aAction.IsHandlingUserInput() || aContext.mHasHandledUserInput);
   const int32_t flags =
       (inPrivateBrowsing ? EditableListener::IME_FLAG_PRIVATE_BROWSING : 0) |
