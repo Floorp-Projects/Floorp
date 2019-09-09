5fbdc1ea98789b721a71b197b402e288e9d33a71
Andi-Bogdan Postelnicu <bpostelnicu@mozilla.com>
2019-09-09 13:30:16 +0000

    Bug 1579351 - Add constant complexity for MemMoveAnnotation when looking for specific `std::atomic`. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D45027
    
    --HG--
    extra : moz-landing-system : lando

--- build/clang-plugin/MemMoveAnnotation.h
+++ build/clang-plugin/MemMoveAnnotation.h
@@ -9,6 +9,8 @@
 #include "CustomTypeAnnotation.h"
 #include "Utils.h"
 
+#include <unordered_set>
+
 class MemMoveAnnotation final : public CustomTypeAnnotation {
 public:
   MemMoveAnnotation()
@@ -24,30 +26,52 @@ protected:
       // This doesn't check that it's really ::std::pair and not
       // ::std::something_else::pair, but should be good enough.
       StringRef Name = getNameChecked(D);
-      if (Name == "pair" || Name == "atomic" ||
-          // libstdc++ specific names
-          Name == "__atomic_base" || Name == "atomic_bool" ||
-          Name == "__cxx_atomic_impl" || Name == "__cxx_atomic_base_impl" ||
-          Name == "__pair_base" ||
-          // MSVCRT specific names
-          Name == "_Atomic_impl" || Name == "_Atomic_base" ||
-          Name == "_Atomic_bool" || Name == "_Atomic_char" ||
-          Name == "_Atomic_schar" || Name == "_Atomic_uchar" ||
-          Name == "_Atomic_char16_t" || Name == "_Atomic_char32_t" ||
-          Name == "_Atomic_wchar_t" || Name == "_Atomic_short" ||
-          Name == "_Atomic_ushort" || Name == "_Atomic_int" ||
-          Name == "_Atomic_uint" || Name == "_Atomic_long" ||
-          Name == "_Atomic_ulong" || Name == "_Atomic_llong" ||
-          Name == "_Atomic_ullong" || Name == "_Atomic_address" ||
-          // MSVCRT 2019
-          Name == "_Atomic_integral" || Name == "_Atomic_integral_facade" ||
-          Name == "_Atomic_padded" || Name == "_Atomic_pointer" ||
-          Name == "_Atomic_storage") {
+      if (isNameExcepted(Name.data())) {
         return "";
       }
       return "it is an stl-provided type not guaranteed to be memmove-able";
     }
     return "";
+  }
+
+private:
+  bool isNameExcepted(const char *Name) const {
+    static std::unordered_set<std::string> NamesSet = {
+        {"pair"},
+        {"atomic"},
+        // libstdc++ specific names
+        {"__atomic_base"},
+        {"atomic_bool"},
+        {"__cxx_atomic_impl"},
+        {"__cxx_atomic_base_impl"},
+        {"__pair_base"},
+        // MSVCRT specific names
+        {"_Atomic_impl"},
+        {"_Atomic_base"},
+        {"_Atomic_bool"},
+        {"_Atomic_char"},
+        {"_Atomic_schar"},
+        {"_Atomic_uchar"},
+        {"_Atomic_char16_t"},
+        {"_Atomic_char32_t"},
+        {"_Atomic_wchar_t"},
+        {"_Atomic_short"},
+        {"_Atomic_ushort"},
+        {"_Atomic_int"},
+        {"_Atomic_uint"},
+        {"_Atomic_long"},
+        {"_Atomic_ulong"},
+        {"_Atomic_llong"},
+        {"_Atomic_ullong"},
+        {"_Atomic_address"},
+        // MSVCRT 2019
+        {"_Atomic_integral"},
+        {"_Atomic_integral_facade"},
+        {"_Atomic_padded"},
+        {"_Atomic_pointer"},
+        {"_Atomic_storage"}};
+
+    return NamesSet.find(Name) != NamesSet.end();
   }
 };
 
