29379343f37d81bd44d47fee8e5af2a3baea0533
Bogdan Tara <btara@mozilla.com>
2019-09-10 01:42:51 +0300

    Backed out changeset acd271126bbc (bug 1579487) for test_tracking_db_service.js failures CLOSED TREE

--- browser/components/protections/content/protections.js
+++ browser/components/protections/content/protections.js
@@ -69,12 +69,17 @@ document.addEventListener("DOMContentLoaded", e => {
   document.sendTelemetryEvent("show", "protection_report");
 
   let createGraph = data => {
-    let earliestDate = data.earliestDate || Date.now();
+    // All of our dates are recorded as 00:00 GMT, add 12 hours to the timestamp
+    // to ensure we display the correct date no matter the user's location.
+    let hoursInMS12 = 12 * 60 * 60 * 1000;
+    let dateInMS = data.earliestDate
+      ? new Date(data.earliestDate).getTime() + hoursInMS12
+      : Date.now();
 
     let summary = document.getElementById("graph-total-summary");
     summary.setAttribute(
       "data-l10n-args",
-      JSON.stringify({ count: data.sumEvents, earliestDate })
+      JSON.stringify({ count: data.sumEvents, earliestDate: dateInMS })
     );
     summary.setAttribute("data-l10n-id", "graph-total-tracker-summary");
 
