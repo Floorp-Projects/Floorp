f90a6cf09bcb3da0613bc2cd75391fa24cd9dc10
Roger Yang <royang@mozilla.com>
2019-09-09 17:08:09 -0400

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/4298 Add Timestamp Support in lib-crash BreadCrumbs

--- mobile/android/android-components/components/lib/crash/src/test/java/mozilla/components/lib/crash/BreadcrumbTest.kt
+++ mobile/android/android-components/components/lib/crash/src/test/java/mozilla/components/lib/crash/BreadcrumbTest.kt
@@ -8,10 +8,14 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.lib.crash.service.CrashReporterService
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.spy
+import java.lang.Thread.sleep
+import java.util.Date
 
 @RunWith(AndroidJUnit4::class)
 class BreadcrumbTest {
@@ -51,6 +55,7 @@ class BreadcrumbTest {
         assertEquals(reporter.crashBreadcrumbs[0].category, testCategory)
         assertEquals(reporter.crashBreadcrumbs[0].level, testLevel)
         assertEquals(reporter.crashBreadcrumbs[0].type, testType)
+        assertNotNull(reporter.crashBreadcrumbs[0].date)
     }
 
     @Test
@@ -127,5 +132,44 @@ class BreadcrumbTest {
                 Breadcrumb(testMessage, testData, testCategory, testLevel, testType)
         )
         assertEquals(reporter.crashBreadcrumbs.size, CrashReporter.BREADCRUMB_MAX_NUM)
+    }
+
+    @Test
+    fun `RecordBreadCrumb stores correct date`() {
+        val testMessage = "test_Message"
+        val testData = hashMapOf("1" to "one", "2" to "two")
+        val testCategory = "testing_category"
+        val testLevel = Breadcrumb.Level.CRITICAL
+        val testType = Breadcrumb.Type.USER
+
+        val service = object : CrashReporterService {
+            override fun report(crash: Crash.UncaughtExceptionCrash) {
+            }
+
+            override fun report(crash: Crash.NativeCodeCrash) {
+            }
+        }
+
+        val reporter = spy(CrashReporter(
+                services = listOf(service),
+                shouldPrompt = CrashReporter.Prompt.NEVER
+        ).install(testContext))
+
+        val beginDate = Date()
+        sleep(100) /* make sure time elapsed */
+        reporter.recordCrashBreadcrumb(
+                Breadcrumb(testMessage, testData, testCategory, testLevel, testType)
+        )
+        sleep(100) /* make sure time elapsed */
+        val afterDate = Date()
+
+        assertTrue(reporter.crashBreadcrumbs[0].date.after(beginDate))
+        assertTrue(reporter.crashBreadcrumbs[0].date.before(afterDate))
+
+        val date = Date()
+        reporter.recordCrashBreadcrumb(
+                Breadcrumb(testMessage, testData, testCategory, testLevel, testType, date)
+        )
+        assertTrue(reporter.crashBreadcrumbs[1].date.compareTo(date) == 0)
     }
 }
