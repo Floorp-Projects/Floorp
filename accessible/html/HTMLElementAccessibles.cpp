107633cba40ab57ab5bf72a298d83a4d75d54917
Morgan Reschenberg <mreschenberg@mozilla.com>
2019-09-10 16:16:53 +0000

    Bug 1277201: Fire a STATE_CHANGE event when a details element is opened or closed. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D44872
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/html/HTMLElementAccessibles.cpp
+++ accessible/html/HTMLElementAccessibles.cpp
@@ -151,6 +151,30 @@ uint64_t HTMLSummaryAccessible::NativeState() const {
   return state;
 }
 
+HTMLSummaryAccessible* HTMLSummaryAccessible::FromDetails(Accessible* details) {
+  if (!dom::HTMLDetailsElement::FromNodeOrNull(details->GetContent())) {
+    return nullptr;
+  }
+
+  HTMLSummaryAccessible* summaryAccessible = nullptr;
+  for (uint32_t i = 0; i < details->ChildCount(); i++) {
+    // Iterate through the children of our details accessible to locate main
+    // summary. This iteration includes the anonymous summary if the details
+    // element was not explicitly created with one.
+    Accessible* child = details->GetChildAt(i);
+    auto* summary =
+        mozilla::dom::HTMLSummaryElement::FromNodeOrNull(child->GetContent());
+    if (summary && summary->IsMainSummary()) {
+      summaryAccessible = static_cast<HTMLSummaryAccessible*>(child);
+      break;
+    }
+  }
+
+  MOZ_ASSERT(summaryAccessible,
+             "Details objects should have at least one summary");
+  return summaryAccessible;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // HTMLSummaryAccessible: Widgets
 
