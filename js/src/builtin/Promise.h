2aa496b60cd10ad8dc39d0398ae072f59701b229
Tooru Fujisawa <arai_a@mac.com>
2019-09-10 18:14:00 +0000

    Bug 1577613 - Throw when resolving/rejecting promises returned by async generator method. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D44353
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/builtin/Promise.h
+++ js/src/builtin/Promise.h
@@ -61,7 +61,8 @@ enum PromiseSlots {
 // The PromiseSlot_RejectFunction slot is not used.
 #define PROMISE_FLAG_DEFAULT_RESOLVING_FUNCTIONS 0x08
 
-// This promise is the return value of an async function invocation.
+// This promise is either the return value of an async function invocation or
+// an async generator's method.
 #define PROMISE_FLAG_ASYNC 0x10
 
 // This promise knows how to propagate information required to keep track of
@@ -239,9 +240,9 @@ MOZ_MUST_USE PromiseObject* CreatePromiseObjectForAsync(JSContext* cx);
 
 /**
  * Returns true if the given object is a promise created by
- * CreatePromiseObjectForAsync function.
+ * either CreatePromiseObjectForAsync function or async generator's method.
  */
-MOZ_MUST_USE bool IsPromiseForAsync(JSObject* promise);
+MOZ_MUST_USE bool IsPromiseForAsyncFunctionOrGenerator(JSObject* promise);
 
 class AsyncFunctionGeneratorObject;
 
