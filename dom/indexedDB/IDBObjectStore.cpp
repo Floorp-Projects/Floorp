4bfd09c0895cddfedf928b9d5ebdf2d3657cae07
Simon Giesecke <sgiesecke@mozilla.com>
2019-09-10 15:36:15 +0000

    Bug 1168606 - Reduce code duplication for different open cursor variants. r=ttung,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D40956
    
    --HG--
    extra : moz-landing-system : lando

--- dom/indexedDB/IDBObjectStore.cpp
+++ dom/indexedDB/IDBObjectStore.cpp
@@ -2270,22 +2270,14 @@ already_AddRefed<IDBRequest> IDBObjectStore::OpenCursorInternal(
 
   IDBCursor::Direction direction = IDBCursor::ConvertDirection(aDirection);
 
-  OpenCursorParams params;
-  if (aKeysOnly) {
-    ObjectStoreOpenKeyCursorParams openParams;
-    openParams.commonParams().objectStoreId() = objectStoreId;
-    openParams.commonParams().optionalKeyRange() = std::move(optionalKeyRange);
-    openParams.commonParams().direction() = direction;
+  const CommonOpenCursorParams commonParams = {
+      objectStoreId, std::move(optionalKeyRange), direction};
 
-    params = std::move(openParams);
-  } else {
-    ObjectStoreOpenCursorParams openParams;
-    openParams.commonParams().objectStoreId() = objectStoreId;
-    openParams.commonParams().optionalKeyRange() = std::move(optionalKeyRange);
-    openParams.commonParams().direction() = direction;
-
-    params = std::move(openParams);
-  }
+  // TODO: It would be great if the IPDL generator created a constructor
+  // accepting a CommonOpenCursorParams by value or rvalue reference.
+  const auto params =
+      aKeysOnly ? OpenCursorParams{ObjectStoreOpenKeyCursorParams{commonParams}}
+                : OpenCursorParams{ObjectStoreOpenCursorParams{commonParams}};
 
   RefPtr<IDBRequest> request = GenerateRequest(aCx, this);
   MOZ_ASSERT(request);
