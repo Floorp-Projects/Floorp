475e20eff921761740d7cb9819af1e7754219900
Razvan Maries <rmaries@mozilla.com>
2019-09-11 00:41:19 +0300

    Backed out changeset 8695461482b4 (bug 1580006) for ES Lint failure. CLOSED TREE

--- devtools/client/shared/components/reps/reps.js
+++ devtools/client/shared/components/reps/reps.js
@@ -2969,7 +2969,6 @@ ErrorRep.propTypes = {
 function ErrorRep(props) {
   const object = props.object;
   const preview = object.preview;
-  const mode = props.mode;
   let name;
 
   if (preview && preview.name && preview.kind) {
@@ -2991,13 +2990,13 @@ function ErrorRep(props) {
 
   const content = [];
 
-  if (mode === MODE.TINY) {
+  if (props.mode === MODE.TINY) {
     content.push(name);
   } else {
     content.push(`${name}: "${preview.message}"`);
   }
 
-  if (preview.stack && mode !== MODE.TINY && mode !== MODE.SHORT) {
+  if (preview.stack && props.mode !== MODE.TINY) {
     const stacktrace = props.renderStacktrace ? props.renderStacktrace(parseStackString(preview.stack)) : getStacktraceElements(props, preview);
     content.push(stacktrace);
   }
