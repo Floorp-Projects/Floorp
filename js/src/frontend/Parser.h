2271b3f30e9174373c55707d23e9f0b23b295e37
Matthew Gaudet <mgaudet@mozilla.com>
2019-09-10 15:53:03 +0000

    Bug 1577857 - Hoist FunctionTree into its own header r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D45041

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -181,6 +181,7 @@
 #include "frontend/BytecodeCompiler.h"
 #include "frontend/ErrorReporter.h"
 #include "frontend/FullParseHandler.h"
+#include "frontend/FunctionTree.h"
 #include "frontend/NameAnalysisTypes.h"
 #include "frontend/NameCollections.h"
 #include "frontend/ParseContext.h"
@@ -289,97 +290,6 @@ class MOZ_STACK_CLASS ParserSharedBase : private JS::AutoGCRooter {
 
   // Create a new BigInt and store it into the trace list.
   BigIntBox* newBigIntBox(BigInt* val);
-};
-
-// A tree of function nodes pointing to a FunctionBox and all its
-// nested inner functions.
-class FunctionTree {
-  FunctionBox* funbox_;
-
-  Vector<FunctionTree> children_;
-
- public:
-  explicit FunctionTree(JSContext* cx) : funbox_(nullptr), children_(cx) {}
-
-  // Note: If we're using vector type, the pointer returned here
-  // is only valid if the tree is only added to in DFS order
-  //
-  // Open to suggestions about how to do that better.
-  FunctionTree* add(JSContext* cx) {
-    if (!children_.emplaceBack(cx)) {
-      return nullptr;
-    }
-    return &children_.back();
-  }
-
-  void reset() {
-    funbox_ = nullptr;
-    children_.clear();
-  }
-
-  FunctionBox* funbox() { return funbox_; }
-  void setFunctionBox(FunctionBox* node) { funbox_ = node; }
-
-  typedef bool (*FunctionTreeVisitorFunction)(ParserBase*, FunctionTree*);
-  bool visitRecursively(JSContext* cx, ParserBase* parser,
-                        FunctionTreeVisitorFunction func) {
-    if (!CheckRecursionLimit(cx)) {
-      return false;
-    }
-
-    for (auto& child : children_) {
-      if (!child.visitRecursively(cx, parser, func)) {
-        return false;
-      }
-    }
-
-    return func(parser, this);
-  }
-
-  void dump(JSContext* cx) { dump(cx, *this, 1); }
-
- private:
-  static void dump(JSContext* cx, FunctionTree& node, int indent);
-};
-
-// Owner of a function tree
-//
-// The holder mode can be eager or deferred:
-//
-// - In Eager mode, deferred items happens right away and the tree is not
-//   constructed.
-// - In Deferred mode, deferred items happens only when publishDeferredItems
-//   is called.
-//
-// Note: Function trees point to function boxes, which only have the lifetime of
-//       the BytecodeCompiler, so exercise caution when holding onto a
-//       holder.
-class FunctionTreeHolder {
- public:
-  enum Mode { Eager, Deferred };
-
- private:
-  FunctionTree treeRoot_;
-  FunctionTree* currentParent_;
-  Mode mode_;
-
- public:
-  explicit FunctionTreeHolder(JSContext* cx, Mode mode = Mode::Eager)
-      : treeRoot_(cx), currentParent_(&treeRoot_), mode_(mode) {}
-
-  FunctionTree* getFunctionTree() { return &treeRoot_; }
-  FunctionTree* getCurrentParent() { return currentParent_; }
-  void setCurrentParent(FunctionTree* parent) { currentParent_ = parent; }
-
-  bool isEager() { return mode_ == Mode::Eager; }
-  bool isDeferred() { return mode_ == Mode::Deferred; }
-
-  // When a parse has failed, we need to reset the root of the
-  // function tree as we don't want a reparse to have old entries.
-  void resetFunctionTree() {
-    treeRoot_.reset();
-    currentParent_ = &treeRoot_;
-  }
 };
 
 class MOZ_STACK_CLASS ParserBase : public ParserSharedBase,
