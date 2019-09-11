0effe6b5218c47761356b4b6bd4802d0fa2ae910
Timothy Nikkel <tnikkel@gmail.com>
2019-09-11 20:57:17 +0000

    Bug 1580087. Fix 32bit * 32bit integer multiplies so they don't overflow in imagelib. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D45303
    
    --HG--
    extra : moz-landing-system : lando

--- image/SurfaceFilters.h
+++ image/SurfaceFilters.h
@@ -139,14 +139,15 @@ class DeinterlacingFilter final : public SurfaceFilter {
     gfx::IntSize outputSize = mNext.InputSize();
     mProgressiveDisplay = aConfig.mProgressiveDisplay;
 
-    const uint32_t bufferSize =
-        outputSize.width * outputSize.height * sizeof(PixelType);
+    const CheckedUint32 bufferSize = CheckedUint32(outputSize.width) *
+                                     CheckedUint32(outputSize.height) *
+                                     CheckedUint32(sizeof(PixelType));
 
     // Use the size of the SurfaceCache as a heuristic to avoid gigantic
     // allocations. Even if DownscalingFilter allowed us to allocate space for
     // the output image, the deinterlacing buffer may still be too big, and
     // fallible allocation won't always save us in the presence of overcommit.
-    if (!SurfaceCache::CanHold(bufferSize)) {
+    if (!bufferSize.isValid() || !SurfaceCache::CanHold(bufferSize.value())) {
       return NS_ERROR_OUT_OF_MEMORY;
     }
 
@@ -156,13 +157,13 @@ class DeinterlacingFilter final : public SurfaceFilter {
     // pipeline may be transforming the rows it receives (for example, by
     // downscaling them), the rows may no longer exist in their original form on
     // the surface itself.
-    mBuffer.reset(new (fallible) uint8_t[bufferSize]);
+    mBuffer.reset(new (fallible) uint8_t[bufferSize.value()]);
     if (MOZ_UNLIKELY(!mBuffer)) {
       return NS_ERROR_OUT_OF_MEMORY;
     }
 
     // Clear the buffer to avoid writing uninitialized memory to the output.
-    memset(mBuffer.get(), 0, bufferSize);
+    memset(mBuffer.get(), 0, bufferSize.value());
 
     ConfigureFilter(outputSize, sizeof(PixelType));
     return NS_OK;
@@ -344,13 +345,19 @@ class DeinterlacingFilter final : public SurfaceFilter {
   }
 
   uint8_t* GetRowPointer(uint32_t aRow) const {
-    uint32_t offset = aRow * InputSize().width * sizeof(PixelType);
+#ifdef DEBUG
+    uint64_t offset64 = uint64_t(aRow) * uint64_t(InputSize().width) *
+                        uint64_t(sizeof(PixelType));
+    uint64_t bufferLength = uint64_t(InputSize().width) *
+                            uint64_t(InputSize().height) *
+                            uint64_t(sizeof(PixelType));
+    MOZ_ASSERT(offset64 < bufferLength, "Start of row is outside of image");
     MOZ_ASSERT(
-        offset < InputSize().width * InputSize().height * sizeof(PixelType),
-        "Start of row is outside of image");
-    MOZ_ASSERT(offset + InputSize().width * sizeof(PixelType) <=
-                   InputSize().width * InputSize().height * sizeof(PixelType),
-               "End of row is outside of image");
+        offset64 + uint64_t(InputSize().width) * uint64_t(sizeof(PixelType)) <=
+            bufferLength,
+        "End of row is outside of image");
+#endif
+    uint32_t offset = aRow * InputSize().width * sizeof(PixelType);
     return mBuffer.get() + offset;
   }
 
