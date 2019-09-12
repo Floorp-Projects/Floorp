2459b8fd860f349c865dc37ba447d8bdddc2abe3
Kershaw Chang <kershaw@mozilla.com>
2019-09-12 20:34:58 +0000

    Bug 1579874 - Use the correct process type for socket process r=jld
    Differential Revision: https://phabricator.services.mozilla.com/D45633

--- netwerk/ipc/SocketProcessParent.h
+++ netwerk/ipc/SocketProcessParent.h
@@ -29,7 +29,7 @@ class SocketProcessHost;
 // by SocketProcessHost.
 class SocketProcessParent final
     : public PSocketProcessParent,
-      public ipc::CrashReporterHelper<GeckoProcessType_Content> {
+      public ipc::CrashReporterHelper<GeckoProcessType_Socket> {
  public:
   friend class SocketProcessHost;
 
