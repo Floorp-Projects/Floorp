daeadbec99437b28b0fdaa77be7dea857858debd
Mark Striemer <mstriemer@mozilla.com>
2019-09-12 23:20:55 +0000

    Bug 1553624 - Select current text when editing in about:config r=paolo,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D45078
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutconfig/test/browser/browser_edit.js
+++ browser/components/aboutconfig/test/browser/browser_edit.js
@@ -1,13 +1,17 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
+const PREF_MODIFY_BOOLEAN = "test.aboutconfig.modify.boolean";
+const PREF_MODIFY_NUMBER = "test.aboutconfig.modify.number";
+const PREF_MODIFY_STRING = "test.aboutconfig.modify.string";
+
 add_task(async function setup() {
   await SpecialPowers.pushPrefEnv({
     set: [
-      ["test.aboutconfig.modify.boolean", true],
-      ["test.aboutconfig.modify.number", 1337],
+      [PREF_MODIFY_BOOLEAN, true],
+      [PREF_MODIFY_NUMBER, 1337],
       [
-        "test.aboutconfig.modify.string",
+        PREF_MODIFY_STRING,
         "the answer to the life the universe and everything",
       ],
     ],
@@ -174,7 +178,7 @@ add_task(async function test_modify() {
   await AboutConfigTest.withNewTab(async function() {
     // Test toggle for boolean prefs.
     for (let nameOfBoolPref of [
-      "test.aboutconfig.modify.boolean",
+      PREF_MODIFY_BOOLEAN,
       PREF_BOOLEAN_DEFAULT_TRUE,
     ]) {
       let row = this.getRow(nameOfBoolPref);
@@ -193,17 +197,14 @@ add_task(async function test_modify() {
     }
 
     // Test abort of edit by starting with string and continuing with editing Int pref.
-    let row = this.getRow("test.aboutconfig.modify.string");
+    let row = this.getRow(PREF_MODIFY_STRING);
     row.editColumnButton.click();
     row.valueInput.value = "test";
-    let intRow = this.getRow("test.aboutconfig.modify.number");
+    let intRow = this.getRow(PREF_MODIFY_NUMBER);
     intRow.editColumnButton.click();
-    Assert.equal(
-      intRow.valueInput.value,
-      Preferences.get("test.aboutconfig.modify.number")
-    );
+    Assert.equal(intRow.valueInput.value, Preferences.get(PREF_MODIFY_NUMBER));
     Assert.ok(!row.valueInput);
-    Assert.equal(row.value, Preferences.get("test.aboutconfig.modify.string"));
+    Assert.equal(row.value, Preferences.get(PREF_MODIFY_STRING));
 
     // Test validation of integer values.
     for (let invalidValue of [
@@ -222,8 +223,8 @@ add_task(async function test_modify() {
 
     // Test correct saving and DOM-update.
     for (let [prefName, willDelete] of [
-      ["test.aboutconfig.modify.string", true],
-      ["test.aboutconfig.modify.number", true],
+      [PREF_MODIFY_STRING, true],
+      [PREF_MODIFY_NUMBER, true],
       [PREF_NUMBER_DEFAULT_ZERO, false],
       [PREF_STRING_DEFAULT_EMPTY, false],
     ]) {
@@ -243,6 +244,29 @@ add_task(async function test_modify() {
       row.resetColumnButton.click();
       Assert.ok(!row.hasClass("has-user-value"));
       Assert.equal(row.hasClass("deleted"), willDelete);
+    }
+  });
+});
+
+add_task(async function test_edit_field_selected() {
+  let prefsToCheck = [
+    [PREF_MODIFY_STRING, "A string", "A new string"],
+    [PREF_MODIFY_NUMBER, "100", "500"],
+  ];
+  await AboutConfigTest.withNewTab(async function() {
+    for (let [prefName, startValue, endValue] of prefsToCheck) {
+      Preferences.set(prefName, startValue);
+      let row = this.getRow(prefName);
+
+      Assert.equal(row.value, startValue);
+      row.editColumnButton.click();
+      Assert.equal(row.valueInput.value, startValue);
+
+      EventUtils.sendString(endValue, this.window);
+
+      row.editColumnButton.click();
+      Assert.equal(row.value, endValue);
+      Assert.equal(Preferences.get(prefName), endValue);
     }
   });
 });
