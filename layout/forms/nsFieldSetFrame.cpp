fe168f7af073f0924a4210a1beac986b596b5d18
Andrew Osmond <aosmond@mozilla.com>
2019-09-12 12:42:41 +0000

    Bug 1574493 - Part 3. Stop rounding rects/clips during display list building. r=jrmuizel
    Rounding in layout pixels is very close to snapping in raster pixels if
    there are no transforms involved. This is why it worked most of the time
    and fell flat in many edge cases. In future parts of this series, we
    will trust scene building and frame building to do the heavy lifting for
    snapping purposes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45058

--- layout/forms/nsFieldSetFrame.cpp
+++ layout/forms/nsFieldSetFrame.cpp
@@ -185,12 +185,12 @@ bool nsDisplayFieldSetBorder::CreateWebRenderCommands(
     if (!legendRect.IsEmpty()) {
       // We need to clip out the part of the border where the legend would go
       auto appUnitsPerDevPixel = frame->PresContext()->AppUnitsPerDevPixel();
-      auto layoutRect = wr::ToRoundedLayoutRect(LayoutDeviceRect::FromAppUnits(
+      auto layoutRect = wr::ToLayoutRect(LayoutDeviceRect::FromAppUnits(
           frame->GetVisualOverflowRectRelativeToSelf() + offset,
           appUnitsPerDevPixel));
 
       wr::ComplexClipRegion region;
-      region.rect = wr::ToRoundedLayoutRect(
+      region.rect = wr::ToLayoutRect(
           LayoutDeviceRect::FromAppUnits(legendRect, appUnitsPerDevPixel));
       region.mode = wr::ClipMode::ClipOut;
       region.radii = wr::EmptyBorderRadius();
