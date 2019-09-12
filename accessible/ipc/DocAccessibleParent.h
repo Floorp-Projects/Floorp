35496390cd303248b71b3dbeadbfe5468062c9b3
Eitan Isaacson <eitan@monotonous.org>
2019-09-12 23:01:59 +0000

    Bug 1565728 - Introduce PDocAccessiblePlatformExt. r=Jamie
    This is an extension protocol that can be used for platform specific
    API (ie. AccessibleWrap methods).
    
    I'm not thrilled with the seperate constructor for the sub-protocol.
    This means that the parent won't have the actor upon DocAccessibleParent
    construction, and some timing bugs can arise because of the extra round
    trip. It would be cool if both actors could be co-created, but that
    would require ManagedEndpoint, and for PBrowser to manage them both. I
    don't want to expose this to PBrowser.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37955
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/ipc/DocAccessibleParent.h
+++ accessible/ipc/DocAccessibleParent.h
@@ -20,6 +20,10 @@ namespace a11y {
 
 class xpcAccessibleGeneric;
 
+#if !defined(XP_WIN)
+class DocAccessiblePlatformExtParent;
+#endif
+
 /*
  * These objects live in the main process and comunicate with and represent
  * an accessible document in a content process.
@@ -241,6 +245,14 @@ class DocAccessibleParent : public ProxyAccessible,
 #if !defined(XP_WIN)
   virtual mozilla::ipc::IPCResult RecvBatch(
       const uint64_t& aBatchType, nsTArray<BatchData>&& aData) override;
+
+  virtual bool DeallocPDocAccessiblePlatformExtParent(
+      PDocAccessiblePlatformExtParent* aActor) override;
+
+  virtual PDocAccessiblePlatformExtParent* AllocPDocAccessiblePlatformExtParent()
+      override;
+
+  DocAccessiblePlatformExtParent* GetPlatformExtension();
 #endif
 
   /**
