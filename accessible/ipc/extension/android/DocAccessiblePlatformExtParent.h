b82fcbd2b1d98a4f984e2764d6a8354f27847556
Eitan Isaacson <eitan@monotonous.org>
2019-09-12 20:10:55 +0000

    Bug 1565728 - Introduce PDocAccessiblePlatformExt for Android. r=Jamie
    Ultimately Batch from PDocAccessible would be moved to here as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37954

---
+++ accessible/ipc/extension/android/DocAccessiblePlatformExtParent.h
@@ -0,0 +1,12 @@
+#ifndef mozilla_a11y_DocAccessiblePlatformExtParent_h
+#define mozilla_a11y_DocAccessiblePlatformExtParent_h
+
+#include "mozilla/a11y/PDocAccessiblePlatformExtParent.h"
+
+namespace mozilla {
+namespace a11y {
+class DocAccessiblePlatformExtParent : public PDocAccessiblePlatformExtParent {};
+}  // namespace a11y
+}  // namespace mozilla
+
+#endif
