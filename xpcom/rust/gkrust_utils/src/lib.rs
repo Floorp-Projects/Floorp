857855857a0a25c2888eac0875cfd3f87ac276c4
Bastien Orivel <eijebong@bananium.fr>
2019-09-12 21:45:58 +0000

    Bug 1580908 - Part 1: Update uuid to 0.7 to dedupe it. r=froydnj
    This also removes a dependency on rand 0.4 getting it closer to being
    able to remove that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45710

--- xpcom/rust/gkrust_utils/src/lib.rs
+++ xpcom/rust/gkrust_utils/src/lib.rs
@@ -7,7 +7,6 @@ use std::fmt::Write;
 
 #[no_mangle]
 pub extern "C" fn GkRustUtils_GenerateUUID(res: &mut nsACString) {
-    // TODO once the vendored Uuid implementation is >7 this likely can use Hyphenated instead of to_string
-    let uuid = Uuid::new_v4().hyphenated().to_string();
-    write!(res, "{{{}}}", uuid).expect("Unexpected uuid generated");
+    let uuid = Uuid::new_v4();
+    write!(res, "{{{}}}", uuid.to_hyphenated_ref()).expect("Unexpected uuid generated");
 }
