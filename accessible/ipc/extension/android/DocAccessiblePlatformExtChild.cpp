00bbe5d47f60c4dc31d3530b68e30542496a663f
Eitan Isaacson <eitan@monotonous.org>
2019-09-12 20:10:55 +0000

    Bug 1565728 - Introduce PDocAccessiblePlatformExt for Android. r=Jamie
    Ultimately Batch from PDocAccessible would be moved to here as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37954

---
+++ accessible/ipc/extension/android/DocAccessiblePlatformExtChild.cpp
@@ -0,0 +1,81 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "DocAccessiblePlatformExtChild.h"
+
+#include "DocAccessibleChild.h"
+#include "AccessibleWrap.h"
+
+namespace mozilla {
+namespace a11y {
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvPivot(
+    uint64_t aID, int32_t aGranularity, bool aForward, bool aInclusive) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvNavigateText(
+    int32_t aID, int32_t aGranularity, int32_t aStartOffset, int32_t aEndOffset,
+    bool aForward, bool aSelect) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvSetSelection(
+    uint64_t aID, int32_t aStart, int32_t aEnd) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvCut(uint64_t aID) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvCopy(uint64_t aID) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvPaste(uint64_t aID) {
+  if (auto acc = IdToAccessibleWrap(aID)) {
+    // XXX: Forward to appropriate wrapper method.
+  }
+
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult DocAccessiblePlatformExtChild::RecvExploreByTouch(
+    uint64_t aID, float aX, float aY) {
+  // XXX: Forward to appropriate wrapper method.
+
+  return IPC_OK();
+}
+
+AccessibleWrap* DocAccessiblePlatformExtChild::IdToAccessibleWrap(
+    const uint64_t& aID) const {
+  return static_cast<AccessibleWrap*>(
+      static_cast<DocAccessibleChild*>(Manager())->IdToAccessible(aID));
+}
+
+}  // namespace a11y
+}  // namespace mozilla
