e33e7502c9c7f612e1d744b779672a0c100b952d
Mark Banner <standard8@mozilla.com>
2019-09-13 15:37:24 +0000

    Bug 1563017 - Use private browsing search engine in the search bar when in private browsing mode. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D45850

--- browser/components/search/content/autocomplete-popup.js
+++ browser/components/search/content/autocomplete-popup.js
@@ -212,23 +212,28 @@
       }
     }
 
-    updateHeader() {
-      Services.search.getDefault().then(currentEngine => {
-        let uri = currentEngine.iconURI;
-        if (uri) {
-          this.setAttribute("src", uri.spec);
-        } else {
-          // If the default has just been changed to a provider without icon,
-          // avoid showing the icon of the previous default provider.
-          this.removeAttribute("src");
-        }
+    async updateHeader() {
+      let currentEngine;
+      if (PrivateBrowsingUtils.isWindowPrivate(window)) {
+        currentEngine = await Services.search.getDefaultPrivate();
+      } else {
+        currentEngine = await Services.search.getDefault();
+      }
 
-        let headerText = this.bundle.formatStringFromName("searchHeader", [
-          currentEngine.name,
-        ]);
-        this.searchbarEngineName.setAttribute("value", headerText);
-        this.searchbarEngine.engine = currentEngine;
-      });
+      let uri = currentEngine.iconURI;
+      if (uri) {
+        this.setAttribute("src", uri.spec);
+      } else {
+        // If the default has just been changed to a provider without icon,
+        // avoid showing the icon of the previous default provider.
+        this.removeAttribute("src");
+      }
+
+      let headerText = this.bundle.formatStringFromName("searchHeader", [
+        currentEngine.name,
+      ]);
+      this.searchbarEngineName.setAttribute("value", headerText);
+      this.searchbarEngine.engine = currentEngine;
     }
 
     /**
