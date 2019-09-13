851c30a183fc02f199b9f4320a58f035879d6e33
Andrea Marchesini <amarchesini@mozilla.com>
2019-09-13 22:18:13 +0000

    Bug 1571037 - BodyStreamHolder must release itself when unlinked, r=smaug, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D45510

--- dom/base/BodyStream.cpp
+++ dom/base/BodyStream.cpp
@@ -26,6 +26,10 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(BodyStreamHolder)
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
 
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(BodyStreamHolder)
+  if (tmp->mBodyStream) {
+    tmp->mBodyStream->ReleaseObjects();
+    MOZ_ASSERT(!tmp->mBodyStream);
+  }
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(BodyStreamHolder)
@@ -34,6 +38,19 @@ NS_IMPL_CYCLE_COLLECTING_RELEASE(BodyStreamHolder)
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(BodyStreamHolder)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
+
+BodyStreamHolder::BodyStreamHolder() : mBodyStream(nullptr) {}
+
+void BodyStreamHolder::StoreBodyStream(BodyStream* aBodyStream) {
+  MOZ_ASSERT(aBodyStream);
+  MOZ_ASSERT(!mBodyStream);
+  mBodyStream = aBodyStream;
+}
+
+void BodyStreamHolder::ForgetBodyStream() {
+  MOZ_ASSERT(mBodyStream);
+  mBodyStream = nullptr;
+}
 
 // BodyStream
 // ---------------------------------------------------------------------------
@@ -118,6 +135,7 @@ void BodyStream::Create(JSContext* aCx, BodyStreamHolder* aStreamHolder,
   // js object is finalized.
   NS_ADDREF(stream.get());
 
+  aStreamHolder->StoreBodyStream(stream);
   aStreamHolder->SetReadableStreamBody(body);
 }
 
@@ -503,6 +521,7 @@ void BodyStream::ReleaseObjects(const MutexAutoLock& aProofOfLock) {
   mWorkerRef = nullptr;
   mGlobal = nullptr;
 
+  mStreamHolder->ForgetBodyStream();
   mStreamHolder->NullifyStream();
   mStreamHolder = nullptr;
 }
