8cabfd9f693d48ca919a700bec6345a9852e1873
Frédéric Wang <fwang@igalia.com>
2019-09-13 10:50:04 +0000

    Bug 1580444 [wpt PR 18991] - MathML: check that determination of space-like is done from in-flow c…, a=testonly
    Automatic update from web-platform-tests
    MathML: check that determination of space-like is done from in-flow children (#18991)
    
    This is a follow-up of #18990 but for space-like elements.
        It also introduces helper to navigate in-flow children and properly use
        it in the space-like and embellished-op tests.
    --
    
    wpt-commits: aa2d6a597714020478b4a09e06b23b66cb82fad5
    wpt-pr: 18991

---
+++ testing/web-platform/tests/mathml/support/box-navigation.js
@@ -0,0 +1,22 @@
+function IsInFlow(element) {
+    var style = window.getComputedStyle(child);
+    return style.getPropertyValue("display") !== "none" &&
+        style.getPropertyValue("position") !== "absolute" &&
+        style.getPropertyValue("position") !== "fixed";
+}
+
+function nextInFlowSibling(element) {
+    var child = element;
+    do {
+        child = child.nextElementSibling;
+    } while (child && !IsInFlow(child));
+    return child;
+}
+
+function previousInFlowSibling(element) {
+    var child = element;
+    do {
+        child = child.previousElementSibling;
+    } while (child && !IsInFlow(child));
+    return child;
+}
