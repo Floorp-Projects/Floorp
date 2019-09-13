a4df7349e34d35d786a04aa6a09d51134e849345
Mike Conley <mconley@mozilla.com>
2019-09-13 09:16:19 +0000

    Bug 1580848 - Clean up some review feedback on the browser_crash_oopiframe.js test. r=kmag
    * The test first asserts that we can actually load iframes in separate subprocesses
    * The test now loads in a new tab rather than a new window
    * The test no longer uses the "fission" attribute
    * The test avoids the BrowsingContext.get pattern, in favour of just passing the
      BrowsingContext from one actor to the other directly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45744
    
    --HG--
    extra : moz-landing-system : lando

--- dom/ipc/tests/browser_crash_oopiframe.js
+++ dom/ipc/tests/browser_crash_oopiframe.js
@@ -10,85 +10,76 @@
  */
 add_task(async function() {
   // Open a new window with fission enabled.
-  let win = await BrowserTestUtils.openNewBrowserWindow({
-    remote: true,
-    fission: true,
-  });
-
-  // Wait for the provided URL to load in our browser.
-  let url = "about:blank";
-  let browser = win.gBrowser.selectedBrowser;
-  BrowserTestUtils.loadURI(browser, url);
-  await BrowserTestUtils.browserLoaded(browser, false, url);
-
-  let rootBC = browser.browsingContext;
-
-  // Create an oop iframe.
-  let iframeID = await SpecialPowers.spawn(browser, [], async () => {
-    let iframe = content.document.createElement("iframe");
-    iframe.setAttribute("fission", "true");
-    iframe.setAttribute("src", "http://example.com");
-
-    const { ContentTaskUtils } = ChromeUtils.import(
-      "resource://testing-common/ContentTaskUtils.jsm"
-    );
-
-    content.document.body.appendChild(iframe);
-    await ContentTaskUtils.waitForEvent(iframe, "load");
-
-    let iframeBC = iframe.frameLoader.browsingContext;
-    return iframeBC.id;
-  });
-
-  let iframeBC = BrowsingContext.get(iframeID);
-  is(iframeBC.parent, rootBC, "oop frame has root as parent");
-
-  BrowserTestUtils.crashFrame(
-    browser,
-    true /* shouldShowTabCrashPage */,
-    true /* shouldClearMinidumps */,
-    iframeBC
-  );
-
-  let eventFiredPromise = BrowserTestUtils.waitForEvent(
-    browser,
-    "oop-browser-crashed"
-  );
-
-  info("Waiting for oop-browser-crashed event.");
-  await eventFiredPromise.then(event => {
-    ok(!event.isTopFrame, "should not be reporting top-level frame crash");
-
-    isnot(
-      event.browsingContextId,
-      rootBC,
-      "top frame browsing context id not expected."
-    );
-
-    is(
-      event.browsingContextId,
-      iframeBC.id,
-      "oop frame browsing context id expected."
-    );
-  });
-
-  // The BrowsingContext is re-used, but the currentWindowGlobal
-  // might still be getting set up at this point. We poll to wait
-  // until its created and available.
-  await BrowserTestUtils.waitForCondition(() => {
-    return iframeBC.currentWindowGlobal;
-  });
-
-  let newIframeURI = await SpecialPowers.spawn(
-    iframeBC,
-    [],
-    () => content.document.documentURI
-  );
-
   ok(
-    newIframeURI.startsWith("about:framecrashed"),
-    "The iframe is now pointing at about:framecrashed"
+    SpecialPowers.useRemoteSubframes,
+    "This test only makes sense of we can use OOP iframes."
   );
 
-  await BrowserTestUtils.closeWindow(win);
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "about:blank",
+    },
+    async browser => {
+      let rootBC = browser.browsingContext;
+
+      // If we load example.com in an injected subframe, we assume that this
+      // will load in its own subprocess, which we can then crash.
+      let iframeBC = await SpecialPowers.spawn(browser, [], async () => {
+        let iframe = content.document.createElement("iframe");
+        iframe.setAttribute("src", "http://example.com");
+
+        content.document.body.appendChild(iframe);
+        await ContentTaskUtils.waitForEvent(iframe, "load");
+        return iframe.frameLoader.browsingContext;
+      });
+
+      is(iframeBC.parent, rootBC, "oop frame has root as parent");
+
+      let eventFiredPromise = BrowserTestUtils.waitForEvent(
+        browser,
+        "oop-browser-crashed"
+      );
+
+      BrowserTestUtils.crashFrame(
+        browser,
+        true /* shouldShowTabCrashPage */,
+        true /* shouldClearMinidumps */,
+        iframeBC
+      );
+
+      info("Waiting for oop-browser-crashed event.");
+      await eventFiredPromise.then(event => {
+        ok(!event.isTopFrame, "should not be reporting top-level frame crash");
+
+        isnot(
+          event.browsingContextId,
+          rootBC,
+          "top frame browsing context id not expected."
+        );
+
+        is(
+          event.browsingContextId,
+          iframeBC.id,
+          "oop frame browsing context id expected."
+        );
+      });
+
+      // The BrowsingContext is re-used, but the currentWindowGlobal
+      // might still be getting set up at this point. We poll to wait
+      // until its created and available.
+      await BrowserTestUtils.waitForCondition(() => {
+        return iframeBC.currentWindowGlobal;
+      });
+
+      let newIframeURI = await SpecialPowers.spawn(iframeBC, [], async () => {
+        return content.document.documentURI;
+      });
+
+      ok(
+        newIframeURI.startsWith("about:framecrashed"),
+        "The iframe is now pointing at about:framecrashed"
+      );
+    }
+  );
 });
