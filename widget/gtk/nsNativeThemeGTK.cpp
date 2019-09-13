56529c6d35547db1ae223b889c119352d8785f7e
Andrew Osmond <aosmond@mozilla.com>
2019-09-13 14:03:23 +0000

    Bug 1574493 - Part 3. Stop rounding rects/clips during display list building. r=jrmuizel
    Rounding in layout pixels is very close to snapping in raster pixels if
    there are no transforms involved. This is why it worked most of the time
    and fell flat in many edge cases. In future parts of this series, we
    will trust scene building and frame building to do the heavy lifting for
    snapping purposes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45058

--- widget/gtk/nsNativeThemeGTK.cpp
+++ widget/gtk/nsNativeThemeGTK.cpp
@@ -1243,9 +1243,8 @@ bool nsNativeThemeGTK::CreateWebRenderCommandsForWidget(
     mozilla::layers::RenderRootStateManager* aManager, nsIFrame* aFrame,
     StyleAppearance aAppearance, const nsRect& aRect) {
   nsPresContext* presContext = aFrame->PresContext();
-  wr::LayoutRect bounds =
-      wr::ToRoundedLayoutRect(LayoutDeviceRect::FromAppUnits(
-          aRect, presContext->AppUnitsPerDevPixel()));
+  wr::LayoutRect bounds = wr::ToLayoutRect(LayoutDeviceRect::FromAppUnits(
+      aRect, presContext->AppUnitsPerDevPixel()));
 
   switch (aAppearance) {
     case StyleAppearance::Window:
