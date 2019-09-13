75d6adf6d69643d552582f78c0e4a318758b4e1b
Andreea Pavel <apavel@mozilla.com>
2019-09-13 19:51:01 +0300

    Backed out changeset 82faf6a9be50 (bug 1578133) for failure increase in Bug 1352715

--- layout/xul/nsImageBoxFrame.cpp
+++ layout/xul/nsImageBoxFrame.cpp
@@ -260,9 +260,9 @@ void nsImageBoxFrame::UpdateImage() {
     // Only get the list-style-image if we aren't being drawn
     // by a native theme.
     auto* display = StyleDisplay();
-    nsITheme* theme;
-    if (!(display->HasAppearance() && (theme = presContext->GetTheme()) &&
-          theme->ThemeSupportsWidget(nullptr, this, display->mAppearance))) {
+    if (!(display->HasAppearance() && nsBox::gTheme &&
+          nsBox::gTheme->ThemeSupportsWidget(nullptr, this,
+                                             display->mAppearance))) {
       // get the list-style-image
       imgRequestProxy* styleRequest = StyleList()->GetListStyleImage();
       if (styleRequest) {
@@ -624,9 +624,8 @@ void nsImageBoxFrame::DidSetComputedStyle(ComputedStyle* aOldComputedStyle) {
 
   // If we're using a native theme implementation, we shouldn't draw anything.
   const nsStyleDisplay* disp = StyleDisplay();
-  nsITheme* theme;
-  if (disp->HasAppearance() && (theme = PresContext()->GetTheme()) &&
-      theme->ThemeSupportsWidget(nullptr, this, disp->mAppearance))
+  if (disp->HasAppearance() && nsBox::gTheme &&
+      nsBox::gTheme->ThemeSupportsWidget(nullptr, this, disp->mAppearance))
     return;
 
   // If list-style-image changes, we have a new image.
