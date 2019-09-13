5f29710fd7d882503b9992bba28b38bcee433896
Dão Gottwald <dao@mozilla.com>
2019-09-13 14:52:53 +0000

    Bug 1577181 - Fix one off search button overflow layout in the urlbar. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D45826
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/search/content/search-one-offs.js
+++ browser/components/search/content/search-one-offs.js
@@ -19,7 +19,7 @@ class SearchOneOffs {
       MozXULElement.parseXULToFragment(
         `
       <hbox class="search-panel-one-offs-header search-panel-header search-panel-current-input">
-        <label class="searchbar-oneoffheader-search" value="&searchWithDesc.label;"/>
+        <label class="search-panel-one-offs-header-label" value="&searchWithDesc.label;"/>
       </hbox>
       <hbox class="search-panel-one-offs" role="group"/>
       <vbox class="search-add-engines"/>
@@ -66,6 +66,7 @@ class SearchOneOffs {
     this.spacerCompact = this.querySelector(".search-one-offs-spacer");
 
     this.contextMenuPopup = this.querySelector(".search-one-offs-context-menu");
+    document.getElementById("mainPopupSet").appendChild(this.contextMenuPopup);
 
     this._bundle = null;
 
@@ -106,6 +107,7 @@ class SearchOneOffs {
     this.addEventListener("mouseout", this);
     this.addEventListener("click", this);
     this.addEventListener("command", this);
+    this.contextMenuPopup.addEventListener("command", this);
     this.addEventListener("contextmenu", this);
 
     // Prevent popup events from the context menu from reaching the autocomplete
@@ -454,7 +456,7 @@ class SearchOneOffs {
     }
 
     let headerText = this.header.querySelector(
-      ".searchbar-oneoffheader-search"
+      ".search-panel-one-offs-header-label"
     );
     this.buttons.setAttribute("aria-label", headerText.value);
 
@@ -1194,10 +1196,9 @@ class SearchOneOffs {
       event.preventDefault();
       return;
     }
-    this.querySelector(".search-one-offs-context-set-default").setAttribute(
-      "disabled",
-      target.engine == Services.search.defaultEngine
-    );
+    this.contextMenuPopup
+      .querySelector(".search-one-offs-context-set-default")
+      .setAttribute("disabled", target.engine == Services.search.defaultEngine);
 
     this.contextMenuPopup.openPopupAtScreen(event.screenX, event.screenY, true);
     event.preventDefault();
