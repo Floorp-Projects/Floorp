3fedb0508ca2a07550d310b073469b5a30aabef8
Will Hawkins <whawkins@mozilla.com>
2019-09-14 17:05:23 +0000

    Bug 1566057: Convert Detecting function to use bytecode iterator. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D42611
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/BytecodeUtil.h
+++ js/src/vm/BytecodeUtil.h
@@ -535,6 +535,10 @@ inline bool IsCheckStrictOp(JSOp op) {
   return CodeSpec[op].format & JOF_CHECKSTRICT;
 }
 
+inline bool IsDetecting(JSOp op) { return CodeSpec[op].format & JOF_DETECTING; }
+
+inline bool IsNameOp(JSOp op) { return CodeSpec[op].format & JOF_NAME; }
+
 #ifdef DEBUG
 inline bool IsCheckSloppyOp(JSOp op) {
   return CodeSpec[op].format & JOF_CHECKSLOPPY;
