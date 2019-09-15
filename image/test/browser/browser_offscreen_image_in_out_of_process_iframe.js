daa68529bcfedeb0a8ff20f8eff5980784799f19
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2019-09-15 22:03:07 +0000

    Bug 1581137 - Allow one frameUpdate observation for the animated image which is out of the display port for now. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D45945
    
    --HG--
    extra : moz-landing-system : lando

--- image/test/browser/browser_offscreen_image_in_out_of_process_iframe.js
+++ image/test/browser/browser_offscreen_image_in_out_of_process_iframe.js
@@ -124,7 +124,11 @@ add_task(async function() {
       observeFrameUpdate
     );
     // Bug 1577084.
-    todo_is(frameCount, 0, "No frameUpdate should happen");
+    if (frameCount == 0) {
+      is(frameCount, 0, "no frameupdates");
+    } else {
+      todo_is(frameCount, 0, "no frameupdates");
+    }
 
     // Scroll the iframe into view.
     await SpecialPowers.spawn(browser, [], async () => {
