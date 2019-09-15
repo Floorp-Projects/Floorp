796df5774a3f2bcebf4eed9bb7edb94f7e38e5c8
Rob Wu <rob@robwu.nl>
2019-09-15 22:18:27 +0000

    Bug 1527029 - Split about:blank test from browser_ext_tabs_executeScript.js. r=zombie, a=RyanVM
    The browser_ext_tabs_executeScript.js test occasionally times out.
    Apparently the part of the test that opens "about:blank" and awaits the
    result sometimes times out (`tabs.executeScript` does not resolve).
    
    To avoid collateral damage by this small part of the test, the
    "about:blank" test has been moved to its own file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45955

---
+++ browser/components/extensions/test/browser/browser_ext_tabs_executeScript_about_blank.js
@@ -0,0 +1,30 @@
+/* -*- Mode: indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/* vim: set sts=2 sw=2 et tw=80: */
+"use strict";
+
+add_task(async function testExecuteScript_at_about_blank() {
+  let extension = ExtensionTestUtils.loadExtension({
+    async background() {
+      try {
+        const tab = await browser.tabs.create({ url: "about:blank" });
+        const result = await browser.tabs.executeScript(tab.id, {
+          code: "location.href",
+          matchAboutBlank: true,
+        });
+        browser.test.assertEq(
+          "about:blank",
+          result[0],
+          "Script executed correctly in new tab"
+        );
+        await browser.tabs.remove(tab.id);
+        browser.test.notifyPass("executeScript");
+      } catch (e) {
+        browser.test.fail(`Error: ${e} :: ${e.stack}`);
+        browser.test.notifyFail("executeScript");
+      }
+    },
+  });
+  await extension.startup();
+  await extension.awaitFinish("executeScript");
+  await extension.unload();
+});
