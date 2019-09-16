b1a8ab6518aa5b6da30f2eb0b0f3381bbbed48db
Rob Wu <rob@robwu.nl>
2019-09-16 16:34:19 +0000

    Bug 1581223 - Ignore extension warning by Raptor on Android r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D45919

--- testing/profiles/raptor-android/user.js
+++ testing/profiles/raptor-android/user.js
@@ -3,3 +3,7 @@
 
 // disk cache smart size is enabled in shipped apps
 user_pref("browser.cache.disk.smart_size.enabled", true);
+
+// Raptor's manifest.json contains the "geckoProfiler" permission, which is
+// not supported on Android. Ignore the warning about the unknown permission.
+user_pref("extensions.webextensions.warnings-as-errors", false);
