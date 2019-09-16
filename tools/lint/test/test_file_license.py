e1f4d923898afff0d968a2dd110b8a1a02df75ca
Sylvestre Ledru <sledru@mozilla.com>
2019-09-16 16:19:55 +0000

    Bug 1579845 - Add a test to the mozlint check: license r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D45873

---
+++ tools/lint/test/test_file_license.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, print_function
+
+import mozunit
+
+LINTER = 'license'
+
+
+def test_lint_license(lint, paths):
+    results = lint(paths())
+    print(results)
+    assert len(results) == 2
+
+    assert "No matching license strings" in results[0].message
+    assert results[0].level == "error"
+    assert "bad.c" in results[0].relpath
+
+    assert "No matching license strings" in results[1].message
+    assert results[1].level == "error"
+    assert "bad.js" in results[1].relpath
+
+
+if __name__ == '__main__':
+    mozunit.main()
