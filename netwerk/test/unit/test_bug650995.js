52feb0024aa50279ef33ac60984445e1f8284d56
Brindusan Cristian <cbrindusan@mozilla.com>
2019-09-17 01:03:46 +0300

    Backed out changeset 3af4309ffce7 (bug 1237782) for wpt assertions/leaks. CLOSED TREE

--- netwerk/test/unit/test_bug650995.js
+++ netwerk/test/unit/test_bug650995.js
@@ -167,7 +167,6 @@ function run_test() {
   httpserver.start(-1);
 
   prefService.setBoolPref("browser.cache.offline.enable", false);
-  prefService.setBoolPref("browser.cache.offline.storage.enable", false);
   prefService.setBoolPref("network.http.rcwn.enabled", false);
 
   nextTest();
