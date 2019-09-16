288d94c07a1cd9ea1547254496262b398dc690c8
Cosmin Sabou <csabou@mozilla.com>
2019-09-16 19:56:01 +0300

    Backed out changeset d8091d350d7c (bug 1579845) for linting opt failures on foobar.js. CLOSED TREE

--- tools/lint/test/files/license/good-other.h
+++
@@ -1,9 +0,0 @@
-/*
-Permission to use, copy, modify, distribute and sell this software
-and its documentation for any purpose is hereby granted without fee,
-provided that the above copyright notice appear in all copies and
-that both that copyright notice and this permission notice appear
-in supporting documentation.  Samphan Raruenrom makes no
-representations about the suitability of this software for any
-purpose.  It is provided "as is" without express or implied warranty.
-*/
