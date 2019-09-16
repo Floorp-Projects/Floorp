f1857e2d0223893ff269cce843486a25bd58f076
Johann Hofmann <jhofmann@mozilla.com>
2019-09-16 09:06:00 +0000

    Bug 1573502 - Always use system principal as triggeringPrincipal for about:certificate. r=jkt
    about:certificate is always trusted and we don't have to use the content principal in browser.js
    
    Differential Revision: https://phabricator.services.mozilla.com/D45939

--- browser/base/content/pageinfo/security.js
+++ browser/base/content/pageinfo/security.js
@@ -39,9 +39,7 @@ var security = {
       let certsStringURL = certs.map(elem => `cert=${elem}`);
       certsStringURL = certsStringURL.join("&");
       let url = `about:certificate?${certsStringURL}`;
-      openTrustedLinkIn(url, "tab", {
-        triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
-      });
+      openTrustedLinkIn(url, "tab");
     } else {
       Services.ww.openWindow(
         window,
