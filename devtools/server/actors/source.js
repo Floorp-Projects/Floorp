52bb5d1e45e2644269c345cbc18bb73095f6c72a
Brian Hackett <bhackett1024@gmail.com>
2019-09-16 03:17:23 +0000

    Bug 1579654 - Use old set of scripts if reparsing for breakpoint positions fails, r=jlast.
    Differential Revision: https://phabricator.services.mozilla.com/D45965
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/source.js
+++ devtools/server/actors/source.js
@@ -399,17 +399,21 @@ const SourceActor = ActorClassWithSpec(sourceSpec, {
     // alive.
     const isWasm = this._source.introductionType === "wasm";
     if (!isWasm && !scripts.some(script => !script.isFunction)) {
-      scripts.length = 0;
-      function addScripts(script) {
-        scripts.push(script);
-        script.getChildScripts().forEach(addScripts);
-      }
+      let newScript;
       try {
-        addScripts(this._source.reparse());
+        newScript = this._source.reparse();
       } catch (e) {
         // reparse() will throw if the source is not valid JS. This can happen
         // if this source is the resurrection of a GC'ed source and there are
         // parse errors in the refetched contents.
+      }
+      if (newScript) {
+        scripts.length = 0;
+        function addScripts(script) {
+          scripts.push(script);
+          script.getChildScripts().forEach(addScripts);
+        }
+        addScripts(newScript);
       }
     }
 
