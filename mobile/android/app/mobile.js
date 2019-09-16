24be096fd9bf985a324ff2217cca471c8dc7a1a5
Valentin Gosu <valentin.gosu@gmail.com>
2019-09-16 15:21:36 +0000

    Bug 1513574 - Remove UserAgentOverrides.jsm and UserAgentUpdates.jsm r=michal
    Also removes the UA cache attached to nsILoadGroup and nsIRequestContext and the "http-on-useragent-request" observer notification.
    If overriding the user agent is needed "http-on-modify-request" is equally usable (but should be used rarely, for performance reasons). A better way is using nsIDocShell.customUserAgent.
    
    Depends on D14750
    
    Differential Revision: https://phabricator.services.mozilla.com/D14751

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -693,12 +693,6 @@ pref("dom.phonenumber.substringmatching.VE", 7);
 
 pref("gfx.canvas.azure.backends", "skia");
 
-// See ua-update.json.in for the packaged UA override list
-pref("general.useragent.updates.enabled", true);
-pref("general.useragent.updates.url", "https://dynamicua.cdn.mozilla.net/0/%APP_ID%");
-pref("general.useragent.updates.interval", 604800); // 1 week
-pref("general.useragent.updates.retry", 86400); // 1 day
-
 // When true, phone number linkification is enabled.
 pref("browser.ui.linkify.phone", false);
 
