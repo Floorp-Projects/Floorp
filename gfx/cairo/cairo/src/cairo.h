3417aec992012de15083fa4349a45d80387ed461
Lee Salzman <lsalzman@mozilla.com>
2019-09-16 16:44:34 +0000

    Bug 1547063 - Cairo support for SharedFTFace. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D44492
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/cairo/cairo/src/cairo.h
+++ gfx/cairo/cairo/src/cairo.h
@@ -1200,6 +1200,30 @@ typedef enum _cairo_hint_metrics {
 } cairo_hint_metrics_t;
 
 /**
+ * cairo_lcd_filter_t:
+ * @CAIRO_LCD_FILTER_DEFAULT: Use the default LCD filter for
+ *   font backend and target device
+ * @CAIRO_LCD_FILTER_NONE: Do not perform LCD filtering
+ * @CAIRO_LCD_FILTER_INTRA_PIXEL: Intra-pixel filter
+ * @CAIRO_LCD_FILTER_FIR3: FIR filter with a 3x3 kernel
+ * @CAIRO_LCD_FILTER_FIR5: FIR filter with a 5x5 kernel
+ *
+ * The LCD filter specifies the low-pass filter applied to LCD-optimized
+ * bitmaps generated with an antialiasing mode of %CAIRO_ANTIALIAS_SUBPIXEL.
+ *
+ * Note: This API was temporarily made available in the public
+ * interface during the 1.7.x development series, but was made private
+ * before 1.8.
+ **/
+typedef enum _cairo_lcd_filter {
+    CAIRO_LCD_FILTER_DEFAULT,
+    CAIRO_LCD_FILTER_NONE,
+    CAIRO_LCD_FILTER_INTRA_PIXEL,
+    CAIRO_LCD_FILTER_FIR3,
+    CAIRO_LCD_FILTER_FIR5
+} cairo_lcd_filter_t;
+
+/**
  * cairo_font_options_t:
  *
  * An opaque structure holding all options that are used when
@@ -1266,6 +1290,10 @@ cairo_font_options_set_hint_metrics (cairo_font_options_t *options,
 				     cairo_hint_metrics_t  hint_metrics);
 cairo_public cairo_hint_metrics_t
 cairo_font_options_get_hint_metrics (const cairo_font_options_t *options);
+
+cairo_public void
+cairo_font_options_set_lcd_filter (cairo_font_options_t   *options,
+                    cairo_lcd_filter_t  lcd_filter);
 
 /* This interface is for dealing with text as text, not caring about the
    font object inside the the cairo_t. */
