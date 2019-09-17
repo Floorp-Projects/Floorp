c47fb85a4cfac91c7d50b995f1d9909727cebeab
Dorel Luca <dluca@mozilla.com>
2019-09-17 21:10:58 +0300

    Backed out changeset 7b042044bd2d (bug 1578772) for eslint failures. CLOSED TREE

--- devtools/client/definitions.js
+++ devtools/client/definitions.js
@@ -556,14 +556,20 @@ exports.ToolboxButtons = [
   {
     id: "command-button-replay",
     description: l10n("toolbox.buttons.replay"),
-    isTargetSupported: target => !target.canRewind && target.isLocalTab,
+    isTargetSupported: target =>
+      Services.prefs.getBoolPref("devtools.recordreplay.mvp.enabled") &&
+      !target.canRewind &&
+      target.isLocalTab,
     onClick: () => reloadAndRecordTab(),
     isChecked: () => false,
   },
   {
     id: "command-button-stop-replay",
     description: l10n("toolbox.buttons.stopReplay"),
-    isTargetSupported: target => target.canRewind && target.isLocalTab,
+    isTargetSupported: target =>
+      Services.prefs.getBoolPref("devtools.recordreplay.mvp.enabled") &&
+      target.canRewind &&
+      target.isLocalTab,
     onClick: () => reloadAndStopRecordingTab(),
     isChecked: () => true,
   },
