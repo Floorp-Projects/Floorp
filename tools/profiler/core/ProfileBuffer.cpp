29e9a394d5032b83ae0fbebb13cfa512464ac4cf
Gerald Squelart <gsquelart@mozilla.com>
2019-09-17 01:53:50 +0000

    Bug 1576555 - Remove unneeded ProfilerMarker - r=gregtatum
    Now that what was in ProfilerMarker is stored directly in `BlocksRingBuffer`,
    there is no need for this class anymore!
    This also removes all the pointer management around it (when added to a TLS
    list, moved during sampling, deleted when expired).
    
    Differential Revision: https://phabricator.services.mozilla.com/D43431

--- tools/profiler/core/ProfileBuffer.cpp
+++ tools/profiler/core/ProfileBuffer.cpp
@@ -6,8 +6,6 @@
 
 #include "ProfileBuffer.h"
 
-#include "ProfilerMarker.h"
-
 #include "BaseProfiler.h"
 #include "jsfriendapi.h"
 #include "mozilla/MathAlgorithms.h"
@@ -36,9 +34,6 @@ ProfileBuffer::ProfileBuffer(BlocksRingBuffer& aBuffer) : mEntries(aBuffer) {
 }
 
 ProfileBuffer::~ProfileBuffer() {
-  while (mStoredMarkers.peek()) {
-    delete mStoredMarkers.popHead();
-  }
   // Only ProfileBuffer controls this buffer, and it should be empty when there
   // is no ProfileBuffer using it.
   mEntries.Reset();
@@ -79,11 +74,6 @@ uint64_t ProfileBuffer::AddThreadIdEntry(int aThreadId) {
   return AddThreadIdEntry(mEntries, aThreadId).ConvertToU64();
 }
 
-void ProfileBuffer::AddMarker(ProfilerMarker* aMarker) {
-  aMarker->SetPositionInBuffer(AddEntry(ProfileBufferEntry::Marker(aMarker)));
-  mStoredMarkers.insert(aMarker);
-}
-
 void ProfileBuffer::CollectCodeLocation(
     const char* aLabel, const char* aStr, uint32_t aFrameFlags,
     const Maybe<uint32_t>& aLineNumber, const Maybe<uint32_t>& aColumnNumber,
@@ -121,22 +111,10 @@ void ProfileBuffer::CollectCodeLocation(
   }
 }
 
-void ProfileBuffer::DeleteExpiredStoredMarkers() {
-  AUTO_PROFILER_STATS(gecko_ProfileBuffer_DeleteExpiredStoredMarkers);
-
-  // Delete markers of samples that have been overwritten due to circular
-  // buffer wraparound.
-  while (mStoredMarkers.peek() &&
-         mStoredMarkers.peek()->HasExpired(BufferRangeStart())) {
-    delete mStoredMarkers.popHead();
-  }
-}
-
 size_t ProfileBuffer::SizeOfExcludingThis(MallocSizeOf aMallocSizeOf) const {
   // Measurement of the following members may be added later if DMD finds it
   // is worthwhile:
   // - memory pointed to by the elements within mEntries
-  // - mStoredMarkers
   return mEntries.SizeOfExcludingThis(aMallocSizeOf);
 }
 
