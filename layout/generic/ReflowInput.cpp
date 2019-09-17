67d2693a2ce683856a734aba2c35b73439173a5d
L. David Baron <dbaron@dbaron.org>
2019-09-17 22:44:19 +0000

    Bug 1581718 - Avoid strange results for orthogonal-flow cases in CalculateBlockSideMargins when border/padding/margin are present. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D46143

--- layout/generic/ReflowInput.cpp
+++ layout/generic/ReflowInput.cpp
@@ -2652,8 +2652,9 @@ void ReflowInput::CalculateBlockSideMargins(LayoutFrameType aFrameType) {
   nscoord computedISizeCBWM = ComputedSize(cbWM).ISize(cbWM);
   if (computedISizeCBWM == NS_UNCONSTRAINEDSIZE) {
     // For orthogonal flows, where we found a parent orthogonal-limit
-    // for AvailableISize() in Init(), we'll use the same here as well.
-    computedISizeCBWM = availISizeCBWM;
+    // for AvailableISize() in Init(), we don't have meaningful sizes to
+    // adjust.  Act like the sum is already correct (below).
+    return;
   }
 
   LAYOUT_WARN_IF_FALSE(NS_UNCONSTRAINEDSIZE != computedISizeCBWM &&
