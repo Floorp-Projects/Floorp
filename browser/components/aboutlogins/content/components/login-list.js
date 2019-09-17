369211edb0ea459aa9246eca8e229e2505e42395
Jared Wein <jwein@mozilla.com>
2019-09-17 14:03:31 +0000

    Bug 1580859 - Keyboard navigation in the login-list should cycle through only visible login-list-items. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D45894

--- browser/components/aboutlogins/content/components/login-list.js
+++ browser/components/aboutlogins/content/components/login-list.js
@@ -456,13 +456,24 @@ export default class LoginList extends HTMLElement {
 
     let isLTR = document.dir == "ltr";
     let activeDescendantId = this._list.getAttribute("aria-activedescendant");
-    let activeDescendant = activeDescendantId
-      ? this.shadowRoot.getElementById(activeDescendantId)
-      : this._list.firstElementChild;
+    let activeDescendant =
+      activeDescendantId && this.shadowRoot.getElementById(activeDescendantId);
+    if (!activeDescendant || activeDescendant.hidden) {
+      activeDescendant =
+        this._list.querySelector(".login-list-item[data-guid]:not([hidden])") ||
+        this._list.firstElementChild;
+    }
     let newlyFocusedItem = null;
+    let previousItem = activeDescendant.previousElementSibling;
+    while (previousItem && previousItem.hidden) {
+      previousItem = previousItem.previousElementSibling;
+    }
+    let nextItem = activeDescendant.nextElementSibling;
+    while (nextItem && nextItem.hidden) {
+      nextItem = nextItem.nextElementSibling;
+    }
     switch (event.key) {
       case "ArrowDown": {
-        let nextItem = activeDescendant.nextElementSibling;
         if (!nextItem) {
           return;
         }
@@ -470,9 +481,7 @@ export default class LoginList extends HTMLElement {
         break;
       }
       case "ArrowLeft": {
-        let item = isLTR
-          ? activeDescendant.previousElementSibling
-          : activeDescendant.nextElementSibling;
+        let item = isLTR ? previousItem : nextItem;
         if (!item) {
           return;
         }
@@ -480,9 +489,7 @@ export default class LoginList extends HTMLElement {
         break;
       }
       case "ArrowRight": {
-        let item = isLTR
-          ? activeDescendant.nextElementSibling
-          : activeDescendant.previousElementSibling;
+        let item = isLTR ? nextItem : previousItem;
         if (!item) {
           return;
         }
@@ -490,7 +497,6 @@ export default class LoginList extends HTMLElement {
         break;
       }
       case "ArrowUp": {
-        let previousItem = activeDescendant.previousElementSibling;
         if (!previousItem) {
           return;
         }
