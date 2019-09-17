ead77cea902e3fea6aeea93ccdedee95d5a7df95
Byron Campen [:bwc] <docfaraday@gmail.com>
2019-09-17 18:18:08 +0000

    Bug 1569183: Remove NrTcpSocketIpc. r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D45111

--- media/mtransport/nr_socket_prsock.h
+++ media/mtransport/nr_socket_prsock.h
@@ -61,7 +61,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "nsProxyRelease.h"
 #include "nsThreadUtils.h"
 
-#include "nsITCPSocketCallback.h"
 #include "mediapacket.h"
 #include "m_cpp_utils.h"
 #include "mozilla/ReentrantMonitor.h"
@@ -82,9 +81,6 @@ typedef struct nr_socket_ nr_socket;
 #if defined(MOZILLA_INTERNAL_API)
 namespace mozilla {
 class NrSocketProxyConfig;
-namespace dom {
-class TCPSocketChild;
-}
 }  // namespace mozilla
 #endif
 
@@ -317,85 +313,6 @@ class NrUdpSocketIpcProxy : public nsIUDPSocketInternal {
   RefPtr<NrUdpSocketIpc> socket_;
   nsCOMPtr<nsIEventTarget> sts_thread_;
 };
-
-struct nr_tcp_message {
-  explicit nr_tcp_message(nsAutoPtr<MediaPacket>& data)
-      : read_bytes(0), data(data) {}
-
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(nr_tcp_message);
-
-  const uint8_t* reading_pointer() const { return data->data() + read_bytes; }
-
-  size_t unread_bytes() const { return data->len() - read_bytes; }
-
-  size_t read_bytes;
-
- private:
-  ~nr_tcp_message() {}
-  DISALLOW_COPY_ASSIGN(nr_tcp_message);
-
-  nsAutoPtr<MediaPacket> data;
-};
-
-#if defined(MOZILLA_INTERNAL_API) && !defined(MOZILLA_XPCOMRT_API)
-class NrTcpSocketIpc : public NrSocketIpc, public nsITCPSocketCallback {
- public:
-  NS_DECL_THREADSAFE_ISUPPORTS
-  NS_DECL_NSITCPSOCKETCALLBACK
-
-  explicit NrTcpSocketIpc(nsIThread* aThread);
-
-  // Implementations of the NrSocketBase APIs
-  virtual int create(nr_transport_addr* addr) override;
-  virtual int sendto(const void* msg, size_t len, int flags,
-                     nr_transport_addr* to) override;
-  virtual int recvfrom(void* buf, size_t maxlen, size_t* len, int flags,
-                       nr_transport_addr* from) override;
-  virtual int getaddr(nr_transport_addr* addrp) override;
-  virtual void close() override;
-  virtual int connect(nr_transport_addr* addr) override;
-  virtual int write(const void* msg, size_t len, size_t* written) override;
-  virtual int read(void* buf, size_t maxlen, size_t* len) override;
-  virtual int listen(int backlog) override;
-  virtual int accept(nr_transport_addr* addrp, nr_socket** sockp) override;
-
-  virtual bool IsProxied() const override { return false; }
-
- private:
-  class TcpSocketReadyRunner;
-  DISALLOW_COPY_ASSIGN(NrTcpSocketIpc);
-  virtual ~NrTcpSocketIpc();
-
-  // Main thread executors of the NrSocketBase APIs
-  void connect_i(const nsACString& remote_addr, uint16_t remote_port,
-                 const nsACString& local_addr, uint16_t local_port,
-                 const nsACString& tls_host);
-  void write_i(nsAutoPtr<nsTArray<uint8_t>> buf, uint32_t tracking_number);
-  void close_i();
-
-  static void release_child_i(dom::TCPSocketChild* aChild);
-
-  // STS thread executor
-  void message_sent_s(uint32_t bufferedAmount, uint32_t tracking_number);
-  void recv_message_s(nr_tcp_message* msg);
-  void update_state_s(NrSocketIpcState next_state);
-  void maybe_post_socket_ready();
-
-  // Accessed from UpdateReadyState (not sts_thread) to avoid sending
-  // runnables when not needed
-  NrSocketIpcState mirror_state_;
-
-  // variables that can only be accessed on STS.
-  NrSocketIpcState state_;
-  std::queue<RefPtr<nr_tcp_message>> msg_queue_;
-  uint32_t buffered_bytes_;
-  uint32_t tracking_number_;
-  std::deque<size_t> writes_in_flight_;
-
-  // main thread.
-  RefPtr<dom::TCPSocketChild> socket_child_;
-};
-#endif
 
 int nr_netaddr_to_transport_addr(const net::NetAddr* netaddr,
                                  nr_transport_addr* addr, int protocol);
