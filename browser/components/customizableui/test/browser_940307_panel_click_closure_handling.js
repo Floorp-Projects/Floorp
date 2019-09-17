a664b7b2e8d7a5517a1264c66b5a371f25f26ed6
Tim Nguyen <ntim.bugs@gmail.com>
2019-09-17 01:35:25 +0000

    Bug 1534455 - Convert autocomplete binding to a customized input element. r=dao,bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D33250

--- browser/components/customizableui/test/browser_940307_panel_click_closure_handling.js
+++ browser/components/customizableui/test/browser_940307_panel_click_closure_handling.js
@@ -53,19 +53,23 @@ add_task(async function searchbar_in_panel() {
 
   searchbar.value = "foo";
   searchbar.focus();
-  // Reaching into this context menu is pretty evil, but hey... it's a test.
-  let textbox = document.getAnonymousElementByAttribute(
-    searchbar.textbox,
-    "anonid",
-    "moz-input-box"
-  );
-  let contextmenu = textbox.menupopup;
-  let contextMenuShown = promisePanelElementShown(window, contextmenu);
+
+  // Can't use promisePanelElementShown() here since the search bar
+  // creates its context menu lazily the first time it is opened.
+  let contextMenuShown = new Promise(resolve => {
+    let listener = event => {
+      if (searchbar._menupopup && event.target == searchbar._menupopup) {
+        window.removeEventListener("popupshown", listener);
+        resolve(searchbar._menupopup);
+      }
+    };
+    window.addEventListener("popupshown", listener);
+  });
   EventUtils.synthesizeMouseAtCenter(searchbar, {
     type: "contextmenu",
     button: 2,
   });
-  await contextMenuShown;
+  let contextmenu = await contextMenuShown;
 
   ok(isOverflowOpen(), "Panel should still be open");
 
--- toolkit/content/customElements.js
+++ toolkit/content/customElements.js
@@ -4,7 +4,7 @@
 
 // This file defines these globals on the window object.
 // Define them here so that ESLint can find them:
-/* globals MozXULElement, MozElements */
+/* globals MozXULElement, MozHTMLElement, MozElements */
 
 "use strict";
 
@@ -616,6 +616,7 @@
   };
 
   const MozXULElement = MozElements.MozElementMixin(XULElement);
+  const MozHTMLElement = MozElements.MozElementMixin(HTMLElement);
 
   /**
    * Given an object, add a proxy that reflects interface implementations
@@ -737,6 +738,7 @@
 
   // Attach the base class to the window so other scripts can use it:
   window.MozXULElement = MozXULElement;
+  window.MozHTMLElement = MozHTMLElement;
 
   customElements.setElementCreationCallback("browser", () => {
     Services.scriptloader.loadSubScript(
@@ -777,6 +779,10 @@
       ["findbar", "chrome://global/content/elements/findbar.js"],
       ["menulist", "chrome://global/content/elements/menulist.js"],
       ["search-textbox", "chrome://global/content/elements/search-textbox.js"],
+      [
+        "autocomplete-input",
+        "chrome://global/content/elements/autocomplete-input.js",
+      ],
       ["stringbundle", "chrome://global/content/elements/stringbundle.js"],
       [
         "printpreview-toolbar",
