55f31447a859cb0fb0b35206bfc17f5369d3f3bd
Jared Wein <jwein@mozilla.com>
2019-09-17 14:02:18 +0000

    Bug 1575516 - Show the Discard Changes dialog if a login is modified while being edited. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D44910
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutlogins/tests/browser/browser_openSite.js
+++ browser/components/aboutlogins/tests/browser/browser_openSite.js
@@ -20,22 +20,39 @@ add_task(async function test_launch_login_item() {
   );
 
   let browser = gBrowser.selectedBrowser;
-  await ContentTask.spawn(
-    browser,
-    LoginHelper.loginToVanillaObject(TEST_LOGIN1),
-    async login => {
-      let loginItem = Cu.waiveXrays(
-        content.document.querySelector("login-item")
-      );
-      loginItem.setLogin(login);
-      let originInput = loginItem.shadowRoot.querySelector(".origin-input");
-      originInput.click();
-    }
-  );
+  await ContentTask.spawn(browser, null, async () => {
+    let loginItem = Cu.waiveXrays(content.document.querySelector("login-item"));
+    loginItem._editButton.click();
+    loginItem._usernameInput.value += "-changed";
+
+    ok(
+      content.document.querySelector("confirmation-dialog").hidden,
+      "discard-changes confirmation-dialog should be hidden before opening the site"
+    );
+
+    let originInput = loginItem.shadowRoot.querySelector(".origin-input");
+    originInput.click();
+  });
 
   info("waiting for new tab to get opened");
   let newTab = await promiseNewTab;
   ok(true, "New tab opened to " + TEST_LOGIN1.origin);
 
+  let modifiedLogin = TEST_LOGIN1.clone();
+  modifiedLogin.timeLastUsed = 9000;
+  let storageChangedPromised = TestUtils.topicObserved(
+    "passwordmgr-storage-changed",
+    (_, data) => data == "modifyLogin"
+  );
+  Services.logins.modifyLogin(TEST_LOGIN1, modifiedLogin);
+  await storageChangedPromised;
+
   BrowserTestUtils.removeTab(newTab);
+
+  await ContentTask.spawn(browser, null, async () => {
+    ok(
+      !content.document.querySelector("confirmation-dialog").hidden,
+      "discard-changes confirmation-dialog should be visible after logging in to a site with a modified login present in the form"
+    );
+  });
 });
