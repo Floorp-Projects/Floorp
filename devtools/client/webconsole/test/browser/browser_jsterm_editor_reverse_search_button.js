5a33b3c101c3f1d3580fa85ebe08fe3ce811eb1c
Armando Ferreira <armando.ferreira@edu.uag.mx>
2019-09-17 21:58:17 +0000

    Bug 1567372 - Add a reverse search button to the Editor Toolbar. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D45095
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/webconsole/test/browser/browser_jsterm_editor_reverse_search_button.js
@@ -0,0 +1,54 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI =
+  "data:text/html;charset=utf-8,Web Console test for bug 1567372";
+
+add_task(async function() {
+  await pushPref("devtools.webconsole.features.editor", true);
+  await pushPref("devtools.webconsole.input.editor", true);
+
+  const hud = await openNewTabAndConsole(TEST_URI);
+
+  info("Searching for `.webconsole-editor-toolbar`");
+  const editorToolbar = hud.ui.outputNode.querySelector(
+    ".webconsole-editor-toolbar"
+  );
+
+  info("Searching for `.webconsole-editor-toolbar-reverseSearchButton`");
+  const reverseSearchButton = editorToolbar.querySelector(
+    ".webconsole-editor-toolbar-reverseSearchButton"
+  );
+
+  const onReverseSearchUiOpen = waitFor(
+    () => getReverseSearchElement(hud) != null
+  );
+
+  info("Performing click on `.webconsole-editor-toolbar-reverseSearchButton`");
+  reverseSearchButton.click();
+
+  await onReverseSearchUiOpen;
+  ok(true, "Reverse Search UI is open");
+
+  ok(
+    reverseSearchButton.classList.contains("checked"),
+    "Reverse Search Button is marked as checked"
+  );
+
+  const onReverseSearchUiClosed = waitFor(
+    () => getReverseSearchElement(hud) == null
+  );
+
+  info("Performing click on `.webconsole-editor-toolbar-reverseSearchButton`");
+  reverseSearchButton.click();
+
+  await onReverseSearchUiClosed;
+  ok(true, "Reverse Search UI is closed");
+
+  ok(
+    !reverseSearchButton.classList.contains("checked"),
+    "Reverse Search Button is NOT marked as checked"
+  );
+});
