52029af28887a790de7a5decae72cde3ce7642bb
Jan-Erik Rediger <jrediger@mozilla.com>
2019-09-18 09:02:41 +0000

    Bug 1581179 - Remove Fennec-specific canary client id fix r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D46027
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/telemetry/tests/unit/test_TelemetryClientID_reset.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryClientID_reset.js
@@ -58,13 +58,6 @@ add_task(async function test_setup() {
  * with and without the optout ping reset included.
  */
 add_task(async function test_clientid_reset_after_reenabling() {
-  const isUnified = Preferences.get(TelemetryUtils.Preferences.Unified, false);
-  if (!isUnified) {
-    // Skipping the test if unified telemetry is off, as no optout ping will
-    // be generated.
-    return;
-  }
-
   await sendPing();
   let ping = await PingServer.promiseNextPing();
   Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
@@ -125,13 +118,6 @@ add_task(async function test_clientid_reset_after_reenabling() {
  * with and without the optout ping reset included.
  */
 add_task(async function test_clientid_canary_after_disabling() {
-  const isUnified = Preferences.get(TelemetryUtils.Preferences.Unified, false);
-  if (!isUnified) {
-    // Skipping the test if unified telemetry is off, as no optout ping will
-    // be generated.
-    return;
-  }
-
   await sendPing();
   let ping = await PingServer.promiseNextPing();
   Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
@@ -178,158 +164,6 @@ add_task(async function test_clientid_canary_after_disabling() {
     TelemetryUtils.knownClientID,
     newClientId,
     "Client ID should be a canary when upload disabled"
-  );
-});
-
-/**
- * On Android Telemetry is not unified.
- * This tests that we reset the client ID if it was previously reset to a canary client ID by accident.
- */
-add_task(async function test_clientid_canary_reset_canary_on_nonunified() {
-  const isUnified = Preferences.get(TelemetryUtils.Preferences.Unified, false);
-  if (isUnified) {
-    // Skipping the test if unified telemetry is on.
-    return;
-  }
-
-  await sendPing();
-  let ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
-  Assert.ok("clientId" in ping);
-
-  let firstClientId = ping.clientId;
-  Assert.notEqual(
-    TelemetryUtils.knownClientID,
-    firstClientId,
-    "Client ID should be valid and random"
-  );
-
-  // Force a canary client ID
-  let clientId = await ClientID.setClientID(TelemetryUtils.knownClientID);
-  Assert.equal(TelemetryUtils.knownClientID, clientId);
-
-  // Now shutdown the instance
-  await TelemetryController.testShutdown();
-  await TelemetryStorage.testClearPendingPings();
-
-  // Start the instance
-  await TelemetryController.testReset();
-
-  let newClientId = await ClientID.getClientID();
-  Assert.notEqual(
-    TelemetryUtils.knownClientID,
-    newClientId,
-    "Client ID should be valid and random"
-  );
-  Assert.notEqual(
-    firstClientId,
-    newClientId,
-    "Client ID should be valid and random"
-  );
-});
-
-/**
- * On Android Telemetry is not unified.
- * This tests that we don't touch the client ID if the pref is toggled for some reason.
- */
-add_task(async function test_clientid_canary_nonunified_no_pref_trigger() {
-  const isUnified = Preferences.get(TelemetryUtils.Preferences.Unified, false);
-  if (isUnified) {
-    // Skipping the test if unified telemetry is on.
-    return;
-  }
-
-  await sendPing();
-  let ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
-  Assert.ok("clientId" in ping);
-
-  let firstClientId = ping.clientId;
-  Assert.notEqual(
-    TelemetryUtils.knownClientID,
-    firstClientId,
-    "Client ID should be valid and random"
-  );
-
-  // Flip the pref again
-  Preferences.set(TelemetryUtils.Preferences.FhrUploadEnabled, true);
-
-  // Restart the instance
-  await TelemetryController.testShutdown();
-  await TelemetryStorage.testClearPendingPings();
-  await TelemetryController.testReset();
-
-  let newClientId = await ClientID.getClientID();
-  Assert.equal(firstClientId, newClientId, "Client ID should be unmodified");
-
-  // Flip the pref again
-  Preferences.set(TelemetryUtils.Preferences.FhrUploadEnabled, false);
-
-  // Restart the instance
-  await TelemetryController.testShutdown();
-  await TelemetryStorage.testClearPendingPings();
-  await TelemetryController.testReset();
-
-  newClientId = await ClientID.getClientID();
-  Assert.equal(firstClientId, newClientId, "Client ID should be unmodified");
-});
-
-/**
- * On Android Telemetry is not unified.
- * Test that we record the canary flag after detecting a canary client ID and resetting to a new ID.
- */
-add_task(async function test_clientid_canary_nonunified_canary_detected() {
-  const isUnified = Preferences.get(TelemetryUtils.Preferences.Unified, false);
-  if (isUnified) {
-    // Skipping the test if unified telemetry is on.
-    return;
-  }
-
-  let firstClientId = await ClientID.resetClientID();
-  await TelemetryController.testReset();
-  await sendPing(/* environment */ true);
-  let ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
-  Assert.equal(
-    firstClientId,
-    ping.clientId,
-    "Client ID should be from the reset"
-  );
-  Assert.ok(!("wasCanary" in ping.environment.profile));
-
-  // Setting canary
-  await ClientID.setClientID(TelemetryUtils.knownClientID);
-
-  // Reset the controller to reset the client ID.
-  await TelemetryController.testReset();
-  await sendPing(/* environment */ true);
-  ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
-  let clientId = ping.clientId;
-  Assert.notEqual(
-    TelemetryUtils.knownClientID,
-    clientId,
-    "Client ID should have been reset to a valid one."
-  );
-  Assert.notEqual(
-    firstClientId,
-    clientId,
-    "Client ID should be a new one after reset."
-  );
-  Assert.ok(
-    ping.environment.profile.wasCanary,
-    "Previous canary client ID should have been detected after reset."
-  );
-
-  // Reset the controller again.
-  await TelemetryController.testReset();
-  await sendPing(/* environment */ true);
-  ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, TEST_PING_TYPE, "The ping must be a test ping");
-  Assert.equal(clientId, ping.clientId, "Client ID should be unmodified now.");
-  Assert.ok(
-    ping.environment.profile.wasCanary,
-    "Canary client ID flag should be persisted."
   );
 });
 
