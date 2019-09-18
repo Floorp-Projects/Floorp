dc8ac83ba626fe1a011802c76a3b77d2fe1377f0
Gerald Squelart <gsquelart@mozilla.com>
2019-09-18 01:22:12 +0000

    Bug 1578329 - Use profiler_can_accept_markers() where appropriate - r=gregtatum
    In places where profiler_is_active() was used around a profiler_add_marker() (or
    similar) call, replace it with profiler_can_accept_markers().
    
    Differential Revision: https://phabricator.services.mozilla.com/D44435
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -1114,7 +1114,7 @@ void HttpChannelChild::OnStopRequest(
   mCacheReadEnd = timing.cacheReadEnd;
 
 #ifdef MOZ_GECKO_PROFILER
-  if (profiler_is_active()) {
+  if (profiler_can_accept_markers()) {
     int32_t priority = PRIORITY_NORMAL;
     GetPriority(&priority);
     profiler_add_network_marker(
