514078f12f89e0edf28fbcba42839cb8cbdaeb68
Alex Chronopoulos <achronop@gmail.com>
2019-09-18 13:16:57 +0000

    Bug 1572281 - Remove audio device change notifications from video capture in Linux. r=dminor
    Video capture used to provide device change notifications for audio and video devices. From now on, CubebDeviceEnumerator will provide audio device change notifications thus video capture is updated to notify only changes of the video device. This is the Linux part.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46272

--- media/webrtc/trunk/webrtc/modules/video_capture/linux/device_info_linux.h
+++ media/webrtc/trunk/webrtc/modules/video_capture/linux/device_info_linux.h
@@ -64,7 +64,7 @@ private:
     std::unique_ptr<rtc::PlatformThread> _inotifyEventThread;
     static bool InotifyEventThread(void*);
     bool InotifyProcess();
-    int _fd_v4l, _fd_snd, _fd_dev, _wd_v4l, _wd_snd, _wd_dev; /* accessed on InotifyEventThread thread */
+    int _fd_v4l, _fd_dev, _wd_v4l, _wd_dev; /* accessed on InotifyEventThread thread */
     Atomic32 _isShutdown;
 #endif
 };
