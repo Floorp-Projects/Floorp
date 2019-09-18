24d44bff6946c9b48e03ec3ce579c9d53a5a6c81
Zibi Braniecki <zbraniecki@mozilla.com>
2019-09-18 16:25:17 +0000

    Bug 1581185 - Fix relative paths in migration script generation. r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D45880
    
    --HG--
    extra : moz-landing-system : lando

--- python/l10n/convert_xul_to_fluent/lib/utils.py
+++ python/l10n/convert_xul_to_fluent/lib/utils.py
@@ -10,9 +10,10 @@ def read_file(path, base_path=None):
         return fptr.read()
 
 
-def write_file(path, text, base_path=None):
+def write_file(path, text, base_path=None, append=False):
     if base_path is not None:
         path = os.path.join(base_path, path)
         path = path.replace('\\', '/')
-    with open(path, "w") as text_file:
+    mode = "a" if append and os.path.exists(path) else "w"
+    with open(path, mode) as text_file:
         text_file.write(text)
