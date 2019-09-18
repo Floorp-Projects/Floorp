2978a72248a0585b3c6b62aa154bec985106e35d
Cosmin Sabou <csabou@mozilla.com>
2019-09-19 01:50:03 +0300

    Backed out 10 changesets (bug 1578661) for linting failures on layout/style/ServoCSSPropList.py. CLOSED TREE
    Backed out changeset b1b5393b1099 (bug 1578661)
    Backed out changeset b87eebb0cf6e (bug 1578661)
    Backed out changeset 6ba2ee45f26e (bug 1578661)
    Backed out changeset 4a1d45160d60 (bug 1578661)
    Backed out changeset 7a5687bebc02 (bug 1578661)
    Backed out changeset 33c306c9020a (bug 1578661)
    Backed out changeset 89d7e6fb24b0 (bug 1578661)
    Backed out changeset 42c4943e569b (bug 1578661)
    Backed out changeset 88d988a80de1 (bug 1578661)
    Backed out changeset 78c90f9e9eee (bug 1578661)

--- dom/base/UseCounter.h
+++ dom/base/UseCounter.h
@@ -17,43 +17,17 @@ enum UseCounter : int16_t {
 #define USE_COUNTER_DOM_ATTRIBUTE(interface_, name_) \
   eUseCounter_##interface_##_##name_##_getter,       \
       eUseCounter_##interface_##_##name_##_setter,
+#define USE_COUNTER_CSS_PROPERTY(name_, id_) eUseCounter_property_##id_,
 #define USE_COUNTER_CUSTOM(name_, desc_) eUseCounter_custom_##name_,
 #include "mozilla/dom/UseCounterList.h"
 #undef USE_COUNTER_DOM_METHOD
 #undef USE_COUNTER_DOM_ATTRIBUTE
+#undef USE_COUNTER_CSS_PROPERTY
 #undef USE_COUNTER_CUSTOM
 
 #define DEPRECATED_OPERATION(op_) eUseCounter_##op_,
 #include "nsDeprecatedOperationList.h"
 #undef DEPRECATED_OPERATION
-
-  eUseCounter_FirstCSSProperty,
-  __reset_hack = eUseCounter_FirstCSSProperty - 1,
-
-// Need an extra level of macro nesting to force expansion of method_
-// params before they get pasted.
-#define CSS_PROP_USE_COUNTER(method_) eUseCounter_property_##method_,
-#define CSS_PROP_PUBLIC_OR_PRIVATE(publicname_, privatename_) privatename_
-#define CSS_PROP_LONGHAND(name_, id_, method_, ...) \
-  CSS_PROP_USE_COUNTER(method_)
-#define CSS_PROP_SHORTHAND(name_, id_, method_, ...) \
-  CSS_PROP_USE_COUNTER(method_)
-#define CSS_PROP_ALIAS(name_, aliasid_, id_, method_, ...) \
-  CSS_PROP_USE_COUNTER(method_)
-#include "mozilla/ServoCSSPropList.h"
-#undef CSS_PROP_ALIAS
-#undef CSS_PROP_SHORTHAND
-#undef CSS_PROP_LONGHAND
-#undef CSS_PROP_PUBLIC_OR_PRIVATE
-#undef CSS_PROP_USE_COUNTER
-
-  eUseCounter_EndCSSProperties,
-  eUseCounter_FirstCountedUnknownProperty = eUseCounter_EndCSSProperties,
-  __reset_hack_2 = eUseCounter_FirstCountedUnknownProperty - 1,
-
-#define COUNTED_UNKNOWN_PROPERTY(name_, method_) eUseCounter_unknown_property_##method_,
-#include "mozilla/CountedUnknownProperties.h"
-#undef COUNTED_UNKNOWN_PROPERTY
 
   eUseCounter_Count
 };
