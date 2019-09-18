6127ad8cb199c3ea51eae382a52013ec8907e447
Ted Campbell <tcampbell@mozilla.com>
2019-09-18 15:05:53 +0000

    Bug 1575055 - Avoid duplicating CompileOptions constructor code. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D42562

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -3543,9 +3543,8 @@ void JS::TransitiveCompileOptions::copyPODTransitiveOptions(
   fieldsEnabledOption = rhs.fieldsEnabledOption;
 };
 
-void JS::ReadOnlyCompileOptions::copyPODOptions(
+void JS::ReadOnlyCompileOptions::copyPODNonTransitiveOptions(
     const ReadOnlyCompileOptions& rhs) {
-  copyPODTransitiveOptions(rhs);
   lineno = rhs.lineno;
   column = rhs.column;
   scriptSourceOffset = rhs.scriptSourceOffset;
@@ -3585,7 +3584,8 @@ bool JS::OwningCompileOptions::copy(JSContext* cx,
   // Release existing string allocations.
   release();
 
-  copyPODOptions(rhs);
+  copyPODTransitiveOptions(rhs);
+  copyPODNonTransitiveOptions(rhs);
 
   elementRoot = rhs.element();
   elementAttributeNameRoot = rhs.elementAttributeName();
