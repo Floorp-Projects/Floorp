f02043749bf48cab32510039d9cb768553761460
Alessio Placitelli <alessio.placitelli@gmail.com>
2019-09-18 12:32:36 +0200

    [components] Enable calling the Glean SDK General API from Java
    This changes the API in a few places:
    
    - adds `@JvmOverloads` to public methods in Glean that
      have default arguments;
    - adds `@JvmOverloads` to the Configuration object's
      public constructor.
    - moves the `channel` parameter of the configuration object
      earlier in the function signature, to make it more convenient
      to call.

--- mobile/android/android-components/components/service/glean/src/main/java/mozilla/components/service/glean/config/Configuration.kt
+++ mobile/android/android-components/components/service/glean/src/main/java/mozilla/components/service/glean/config/Configuration.kt
@@ -14,6 +14,8 @@ import mozilla.components.service.glean.net.PingUploader
  *
  * @property serverEndpoint the server pings are sent to. Please note that this is
  *           is only meant to be changed for tests.
+ * @property channel the release channel the application is on, if known. This will be
+ *           sent along with all the pings, in the `client_info` section.
  * @property userAgent the user agent used when sending pings, only to be used internally.
  * @property maxEvents the number of events to store before the events ping is sent
  * @property logPings whether to log ping contents to the console. This is only meant to be used
@@ -21,10 +23,10 @@ import mozilla.components.service.glean.net.PingUploader
  * @property httpClient The HTTP client implementation to use for uploading pings.
  * @property pingTag String tag to be applied to headers when uploading pings for debug view.
  *           This is only meant to be used internally by the `GleanDebugActivity`.
- * @property channel the release channel the application is on, if known. This will be
- *           sent along with all the pings, in the `client_info` section. */
+ */
 data class Configuration internal constructor(
     val serverEndpoint: String,
+    val channel: String? = null,
     val userAgent: String = DEFAULT_USER_AGENT,
     val maxEvents: Int = DEFAULT_MAX_EVENTS,
     val logPings: Boolean = DEFAULT_LOG_PINGS,
@@ -32,27 +34,27 @@ data class Configuration internal constructor(
     // default values for the lines below are ever changed, they are required
     // to change in the public constructor below.
     val httpClient: PingUploader = ConceptFetchHttpUploader(lazy { HttpURLConnectionClient() }),
-    val pingTag: String? = null,
-    val channel: String? = null
+    val pingTag: String? = null
 ) {
     // This is the only public constructor this class should have. It should only
     // expose things we want to allow external applications to change. Every test
     // only or internal configuration option should be added to the above primary internal
     // constructor and only initialized with a proper default when calling the primary
     // constructor from the secondary, public one, below.
+    @JvmOverloads
     constructor(
         serverEndpoint: String = DEFAULT_TELEMETRY_ENDPOINT,
+        channel: String? = null,
         maxEvents: Int = DEFAULT_MAX_EVENTS,
-        httpClient: PingUploader = ConceptFetchHttpUploader(lazy { HttpURLConnectionClient() }),
-        channel: String? = null
+        httpClient: PingUploader = ConceptFetchHttpUploader(lazy { HttpURLConnectionClient() })
     ) : this (
         serverEndpoint = serverEndpoint,
+        channel = channel,
         userAgent = DEFAULT_USER_AGENT,
         maxEvents = maxEvents,
         logPings = DEFAULT_LOG_PINGS,
         httpClient = httpClient,
-        pingTag = null,
-        channel = channel
+        pingTag = null
     )
 
     companion object {
