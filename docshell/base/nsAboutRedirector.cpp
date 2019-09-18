2f69fc40c32c98b483ec22f51920e8134a15c353
James Willcox <snorp@snorp.net>
2019-09-18 15:25:35 +0000

    Bug 1540065 - Guard about:config access with 'general.aboutConfig.enable' pref r=Ehsan
    Differential Revision: https://phabricator.services.mozilla.com/D36608

--- docshell/base/nsAboutRedirector.cpp
+++ docshell/base/nsAboutRedirector.cpp
@@ -10,14 +10,9 @@
 #include "nsBaseChannel.h"
 #include "mozilla/ArrayUtils.h"
 #include "nsIProtocolHandler.h"
+#include "mozilla/Preferences.h"
 
-#if defined(MOZ_WIDGET_ANDROID) && defined(RELEASE_OR_BETA)
-#  define ABOUT_CONFIG_BLOCKED_GV
-#endif
-
-#ifdef ABOUT_CONFIG_BLOCKED_GV
-#  include "mozilla/jni/Utils.h"  // for mozilla::jni::IsFennec()
-#endif
+#define ABOUT_CONFIG_ENABLED_PREF "general.aboutConfig.enable"
 
 NS_IMPL_ISUPPORTS(nsAboutRedirector, nsIAboutModule)
 
@@ -163,13 +158,10 @@ nsAboutRedirector::NewChannel(nsIURI* aURI, nsILoadInfo* aLoadInfo,
     return NS_OK;
   }
 
-#ifdef ABOUT_CONFIG_BLOCKED_GV
-  // We don't want to allow access to about:config from
-  // GeckoView on release or beta, but it's fine for Fennec.
-  if (path.EqualsASCII("config") && !mozilla::jni::IsFennec()) {
+  if (path.EqualsASCII("config") &&
+      !mozilla::Preferences::GetBool(ABOUT_CONFIG_ENABLED_PREF, true)) {
     return NS_ERROR_NOT_AVAILABLE;
   }
-#endif
 
   for (int i = 0; i < kRedirTotal; i++) {
     if (!strcmp(path.get(), kRedirMap[i].id)) {
