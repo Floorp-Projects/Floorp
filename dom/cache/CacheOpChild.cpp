b92f406f4d8824b653a4924daed25c765fc68a74
Boris Zbarsky <bzbarsky@mit.edu>
2019-09-19 13:02:39 +0000

    Bug 1581278 part 2.  Add convenience methods for rejecting promises with various types of spec exceptions.  r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D46383

--- dom/cache/CacheOpChild.cpp
+++ dom/cache/CacheOpChild.cpp
@@ -140,10 +140,8 @@ mozilla::ipc::IPCResult CacheOpChild::Recv__delete__(
       // reject instead of crashing, though, if we get a nullptr here.
       MOZ_DIAGNOSTIC_ASSERT(actor);
       if (!actor) {
-        ErrorResult status;
-        status.ThrowTypeError(
+        mPromise->MaybeRejectWithTypeError(
             u"CacheStorage.open() failed to access the storage system.");
-        mPromise->MaybeReject(status);
         break;
       }
 
