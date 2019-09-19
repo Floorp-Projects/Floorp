eee83864c0714bc4ff36bf9b04edc90b6da6f439
Brian Hackett <bhackett1024@gmail.com>
2019-09-19 17:32:18 +0000

    Bug 1582057 - Whitelist exceptions from canceled operations in debugger tests, r=jlast.
    Differential Revision: https://phabricator.services.mozilla.com/D46345

--- devtools/client/debugger/test/mochitest/browser_dbg-inline-script-offset.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-inline-script-offset.js
@@ -6,12 +6,6 @@
 
 // Test that breakpoints work when set in inline scripts that do not start at column 0.
 
-// Debugger operations may still be in progress when we navigate.
-const { PromiseTestUtils } = ChromeUtils.import(
-  "resource://testing-common/PromiseTestUtils.jsm"
-);
-PromiseTestUtils.whitelistRejectionsGlobally(/Page has navigated/);
-
 add_task(async function() {
   const dbg = await initDebugger("doc-inline-script-offset.html");
   await addBreakpoint(dbg, "doc-inline-script-offset.html", 6, 66);
