7605ca80a3c7ee9b1864a11a40d07797d8400ae9
Nihanth Subramanya <nhnt11@gmail.com>
2019-09-20 16:12:46 +0000

    Bug 1582763 - [Protections Panel] Remove blue badge from shield and toggle when TP is disabled. r=johannh a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D46618

--- browser/base/content/browser-siteProtections.js
+++ browser/base/content/browser-siteProtections.js
@@ -1900,14 +1900,6 @@ var gProtectionsHandler = {
       !currentlyEnabled
     );
 
-    // Show the blue dot indicator if the protection is disabled. We need this
-    // in addition to the 'enabled' attribute of the TP switch section due to
-    // the blue dot won't be shown in the case that TP switch to off from on.
-    this._protectionsPopupTPSwitch.toggleAttribute(
-      "showdotindicator",
-      !currentlyEnabled
-    );
-
     // Give the button an accessible label for screen readers.
     if (currentlyEnabled) {
       this._protectionsPopupTPSwitch.setAttribute(
