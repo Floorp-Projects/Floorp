bc0fa52bec1ee3a10087e30aeb5457ab8db5dc95
Aaron Klotz <aklotz@mozilla.com>
2019-09-20 16:59:57 +0000

    Bug 1542830: Part 3 - Add ntdll_freestanding.lib to freestanding; r=mhowell,froydnj
    The `freestanding` library is built with specific compiler flags to signify
    that it is indeed freestanding code. That is, it does not depend on a
    standard library.
    
    One of the requirements of freestanding code is that the toolchain still
    expects implementations of `memcpy`, `memmove`, `memcmp`, and `memset`.
    
    I did briefly implement my own naive versions of these functions, but that
    solution is less than ideal since the implementations must be `extern` and are
    thus picked up by the entire `firefox.exe` binary. This denies the rest of
    `firefox.exe` the benefit of optimized implementations. On Windows, the
    sandbox is linked into `firefox.exe`, so we cannot just shrug and
    assume that naive implementations will not have any effect on anything.
    
    There are, however, optimized implementations of these functions that are
    exported by `ntdll.dll`. They are not included in the `ntdll.lib` that is
    included in the Windows SDK. Using `llvm-dlltool`, we can build an import
    library containing the missing entries and then add that library to `OS_LIBS`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D43157

---
+++ browser/app/winlauncher/freestanding/gen_ntdll_freestanding_lib.py
@@ -0,0 +1,30 @@
+# -*- Mode: python; indent-tabs-mode: nil; tab-width: 40 -*-
+# vim: set filetype=python:
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+import os
+import subprocess
+import tempfile
+
+
+def main(output_fd, def_file, llvm_dlltool, *llvm_dlltool_args):
+    # llvm-dlltool can't output to stdout, so we create a temp file, use that
+    # to write out the lib, and then copy it over to output_fd
+    (tmp_fd, tmp_output) = tempfile.mkstemp()
+    os.close(tmp_fd)
+
+    try:
+        cmd = [llvm_dlltool]
+        cmd.extend(llvm_dlltool_args)
+        cmd += ['-d', def_file, '-l', tmp_output]
+
+        subprocess.check_call(cmd)
+
+        with open(tmp_output, 'rb') as tmplib:
+            output_fd.write(tmplib.read())
+    finally:
+        os.remove(tmp_output)
