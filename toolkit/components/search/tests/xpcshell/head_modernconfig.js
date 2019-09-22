c5eae4f8515e7493c9edc5acfe7962ae039c9c3d
Dale Harvey <dale@arandomurl.com>
2019-09-22 20:21:32 +0000

    Bug 1577452 - Run search tests with modernConfig enabled r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D45192

---
+++ toolkit/components/search/tests/xpcshell/head_modernconfig.js
@@ -0,0 +1,8 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/* import-globals-from head_search.js */
+
+"use strict";
+
+Services.prefs.setBoolPref("browser.search.modernConfig", true);
