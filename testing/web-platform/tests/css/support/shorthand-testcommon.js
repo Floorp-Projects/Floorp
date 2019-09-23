774cab2de8c47d9e6ed52cec75ed220233fb8e87
Eric Willigers <ewilligers@users.noreply.github.com>
2019-09-23 12:26:46 +0000

    Bug 1582904 [wpt PR 19194] - Expansion of shorthands, a=testonly
    Automatic update from web-platform-tests
    Expansion of shorthands (#19194)
    
    We check the values assigned to longhand properties
    by the following shorthand properties:
    - flex
    - flex-flow
    - inset
    - list-style
    - margin
    - outline
    - padding
    - text-decoration
    
    --
    
    wpt-commits: cd5c82839b6bb825f636744b35d623886dd4c180
    wpt-pr: 19194

--- testing/web-platform/tests/css/support/shorthand-testcommon.js
+++ testing/web-platform/tests/css/support/shorthand-testcommon.js
@@ -6,15 +6,19 @@ function test_shorthand_value(property, value, longhands) {
     test(function(){
         var div = document.getElementById('target') || document.createElement('div');
         div.style[property] = "";
-        div.style[property] = value;
+        try {
+            div.style[property] = value;
 
-        for (let longhand of Object.keys(longhands).sort()) {
-            const readValue = div.style[longhand];
-            assert_equals(readValue, longhands[longhand], longhand + " should be canonical");
+            for (let longhand of Object.keys(longhands).sort()) {
+                const readValue = div.style[longhand];
+                assert_equals(readValue, longhands[longhand], longhand + " should be canonical");
 
-            div.style[longhand] = "";
-            div.style[longhand] = readValue;
-            assert_equals(div.style[longhand], readValue, "serialization should round-trip");
+                div.style[longhand] = "";
+                div.style[longhand] = readValue;
+                assert_equals(div.style[longhand], readValue, "serialization should round-trip");
+            }
+        } finally {
+            div.style[property] = "";
         }
     }, "e.style['" + property + "'] = " + stringifiedValue + " should set the longhand values");
 }
