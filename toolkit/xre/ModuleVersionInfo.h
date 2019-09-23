a66f8249945bfdba8eb8d0c33d7e46c5e910c127
Aaron Klotz <aklotz@mozilla.com>
2019-09-23 20:19:17 +0000

    Bug 1542830: Part 6 - Rewrite the untrusted modules processor in toolkit/xre; r=mhowell
    * Significant cleanup to `ModuleEvaluator`
    * `UntrustedModuleData` holds all of the accumulated untrusted module info for
      a single process.
    * `ProcessedModuleLoadEvent` holds information about an individual untrusted
      module load in a Gecko-friendly, sanitized, format.
    * Since multiple `ProcessModuleLoadEvent` objects may reference the same
      module, we store module metadata in a shared `ModuleInfo` structure.
    * The `UntrustedModulesProcessor` receives the events from `mozglue` and
      processes them on a background thread:
    ** It does not start background processing until the main thread has gone idle.
       The idea here is that we do not want to add any more background work until
       we are reasonably confident that Gecko is no longer starting up or doing
       other intense activity.
    ** Background processing runs at a background priority level, *except* when
       results are requested by telemetry itself.
    ** Telemetry requests the data via `UntrustedModulesProcessor::GetProcessedData`
       which runs at normal priority and returns a promise to the caller.
    
    Depends on D43159
    
    Differential Revision: https://phabricator.services.mozilla.com/D43160
    
    --HG--
    rename : toolkit/xre/ModuleEvaluator_windows.cpp => toolkit/xre/ModuleEvaluator.cpp
    rename : toolkit/xre/ModuleEvaluator_windows.cpp => toolkit/xre/ModuleEvaluator.h
    rename : toolkit/xre/ModuleVersionInfo_windows.cpp => toolkit/xre/ModuleVersionInfo.cpp
    rename : toolkit/xre/ModuleVersionInfo_windows.h => toolkit/xre/ModuleVersionInfo.h
    rename : toolkit/xre/ModuleEvaluator_windows.cpp => toolkit/xre/UntrustedModulesData.cpp
    rename : toolkit/xre/ModuleEvaluator_windows.h => toolkit/xre/UntrustedModulesData.h
    rename : toolkit/xre/ModuleEvaluator_windows.cpp => toolkit/xre/UntrustedModulesProcessor.cpp
    rename : toolkit/xre/ModuleEvaluator_windows.h => toolkit/xre/UntrustedModulesProcessor.h
    extra : moz-landing-system : lando

--- toolkit/xre/ModuleVersionInfo.h
+++ toolkit/xre/ModuleVersionInfo.h
@@ -4,8 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_ModuleVersionInfo_windows_h
-#define mozilla_ModuleVersionInfo_windows_h
+#ifndef mozilla_ModuleVersionInfo_h
+#define mozilla_ModuleVersionInfo_h
 
 #include <windows.h>
 #include "nsString.h"
@@ -68,4 +68,4 @@ class ModuleVersionInfo {
 
 }  // namespace mozilla
 
-#endif  // mozilla_ModuleVersionInfo_windows_h
+#endif  // mozilla_ModuleVersionInfo_h
