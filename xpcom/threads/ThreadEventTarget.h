ecdf4c1ca3fc6bce783c8537cbf4efee0061f90f
Doug Thayer <dothayer@mozilla.com>
2019-09-23 21:07:04 +0000

    Bug 1510226 - Do not block main thread in nsThread::Init r=froydnj
    To remove the blocking inside nsThread::Init, two things needed
    to happen:
    
    - Switch the ThreadInitData value passed as the argument for
      ThreadFunc to a heap allocation, so that it can outlive the call
      to nsThread::Init.
    - Initialize mThread and mEventTarget->mThread to the return
      value of PR_CreateThread, so that to the callers, checks which
      depend on these values being set can continue to function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41248
    
    --HG--
    extra : moz-landing-system : lando

--- xpcom/threads/ThreadEventTarget.h
+++ xpcom/threads/ThreadEventTarget.h
@@ -30,7 +30,7 @@ class ThreadEventTarget final : public nsISerialEventTarget {
 
   // Sets the thread for which IsOnCurrentThread returns true to the current
   // thread.
-  void SetCurrentThread();
+  void SetCurrentThread(PRThread* aThread);
 
   size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf) const {
     size_t n = 0;
