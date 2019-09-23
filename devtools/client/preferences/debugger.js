9ebb8c9ae6a63467a88dd3b8fb2c07609bee42f7
Andreea Pavel <apavel@mozilla.com>
2019-09-23 10:00:30 +0300

    Bug 1581645 - Limit inline preview to nightly and early beta. r=davidwalsh a=lizzard

--- devtools/client/preferences/debugger.js
+++ devtools/client/preferences/debugger.js
@@ -83,4 +83,9 @@ pref("devtools.debugger.features.event-listeners-breakpoints", true);
 pref("devtools.debugger.features.dom-mutation-breakpoints", true);
 pref("devtools.debugger.features.log-points", true);
 pref("devtools.debugger.features.overlay-step-buttons", false);
-pref("devtools.debugger.features.inline-preview", true);
+
+#ifdef EARLY_BETA_OR_EARLIER
+  pref("devtools.debugger.features.inline-preview", true);
+#else
+  pref("devtools.debugger.features.inline-preview", false);
+#endif
