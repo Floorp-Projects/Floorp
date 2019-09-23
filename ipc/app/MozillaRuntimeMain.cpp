fccd6eb9bae6dc6c476485a08f6044f3471e36df
Aaron Klotz <aklotz@mozilla.com>
2019-09-23 20:18:41 +0000

    Bug 1542830: Part 4 - Modify mozglue to use new untrusted modules interfaces; r=mhowell
    * At this point our DLL blocking infra is complicated enough that I decided to
      bite the bullet and move all of this code out of `mozglue/build` and into its
      own subdirectory, `mozglue/dllservices`.
    * We delete the original `UntrustedDllsHandler` code which is now obsolete.
    * We implement mozglue's `LoaderObserver`:
    ** When this observer registers itself with the launcher process API, it
       receives a vector containing all saved records of loaded DLLs that happened
       until that moment.
    ** This code handles profiler labels and stackwalking suppression.
    ** Once a load has completed, we either pass the load on to XUL for further
       processing, or save it for later if XUL is not initialized yet.
    * mozglue has its own `ModuleLoadFrame` implementation for the legacy blocklist.
    * `DllServicesBase` is updated to support the new interfaces.
    * We implement `FallbackLoaderAPI` for `plugin-container`, `xpcshell`, and
      any other non-`firefox` processes that do not have a launcher process
      providing a loader API.
    * We add some wide to UTF8 conversion functions.
    
    Depends on D43157
    
    Differential Revision: https://phabricator.services.mozilla.com/D43158
    
    --HG--
    rename : mozglue/build/Authenticode.cpp => mozglue/dllservices/Authenticode.cpp
    rename : mozglue/build/Authenticode.h => mozglue/dllservices/Authenticode.h
    rename : browser/app/winlauncher/freestanding/LoaderAPIInterfaces.h => mozglue/dllservices/LoaderAPIInterfaces.h
    rename : browser/app/winlauncher/freestanding/ModuleLoadInfo.h => mozglue/dllservices/ModuleLoadInfo.h
    rename : browser/app/winlauncher/NtLoaderAPI.h => mozglue/dllservices/NtLoaderAPI.h
    rename : mozglue/build/WindowsDllBlocklist.cpp => mozglue/dllservices/WindowsDllBlocklist.cpp
    rename : mozglue/build/WindowsDllBlocklist.h => mozglue/dllservices/WindowsDllBlocklist.h
    rename : mozglue/build/WindowsDllBlocklistCommon.h => mozglue/dllservices/WindowsDllBlocklistCommon.h
    rename : mozglue/build/WindowsDllBlocklistDefs.in => mozglue/dllservices/WindowsDllBlocklistDefs.in
    rename : mozglue/build/WindowsDllServices.h => mozglue/dllservices/WindowsDllServices.h
    rename : mozglue/build/gen_dll_blocklist_defs.py => mozglue/dllservices/gen_dll_blocklist_defs.py
    rename : mozglue/build/moz.build => mozglue/dllservices/moz.build
    rename : mozglue/build/MozglueUtils.h => mozglue/misc/WinUtils.h
    extra : moz-landing-system : lando

--- ipc/app/MozillaRuntimeMain.cpp
+++ ipc/app/MozillaRuntimeMain.cpp
@@ -7,7 +7,9 @@
 #include "../contentproc/plugin-container.cpp"
 
 #include "mozilla/Bootstrap.h"
-#include "mozilla/WindowsDllBlocklist.h"
+#if defined(XP_WIN)
+#  include "mozilla/WindowsDllBlocklist.h"
+#endif  // defined(XP_WIN)
 
 using namespace mozilla;
 
