3e7a3387186ba05c668a2dae85ce40c4b2070f1d
Andrew Osmond <aosmond@mozilla.com>
2019-09-24 10:34:00 +0000

    Bug 1581934 - Restore the calculation for a more precise picture local rect. r=kvark,gw
    As it turns out, the difference between the snapped local rect and the
    unsnapped local rect was not just that the former contained snapped
    primitives and the latter contained unsnapped primitives, but also that
    the former took into account surface inflation for primitives, the
    entire clip chain instead of just the primitive's local clip, and
    removal of culled primitives. As such, the picture's rects can be wildly
    different, even if snapping has been taken care of earlier, and parts of
    WebRender have come to rely upon this more accurate representation of a
    picture.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46605

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -1075,7 +1075,7 @@ impl BatchBuilder {
                 let prim_cache_address = gpu_cache.get_address(&ctx.globals.default_image_handle);
 
                 let prim_header = PrimitiveHeader {
-                    local_rect: picture.local_rect,
+                    local_rect: picture.precise_local_rect,
                     local_clip_rect: prim_info.combined_local_clip_rect,
                     specific_prim_address: prim_cache_address,
                     transform_id,
@@ -1102,7 +1102,7 @@ impl BatchBuilder {
                             );
 
                             let prim_header = PrimitiveHeader {
-                                local_rect: pic.local_rect,
+                                local_rect: pic.precise_local_rect,
                                 local_clip_rect: child_prim_info.combined_local_clip_rect,
                                 specific_prim_address: GpuCacheAddress::INVALID,
                                 transform_id: transforms
@@ -1699,7 +1699,7 @@ impl BatchBuilder {
                                 };
 
                                 let prim_header = PrimitiveHeader {
-                                    local_rect: picture.local_rect,
+                                    local_rect: picture.precise_local_rect,
                                     local_clip_rect: prim_info.combined_local_clip_rect,
                                     specific_prim_address: prim_cache_address,
                                     transform_id,
@@ -2430,7 +2430,7 @@ impl BatchBuilder {
                 let prim_cache_address = gpu_cache.get_address(&ctx.globals.default_image_handle);
                 let backdrop_picture = &ctx.prim_store.pictures[backdrop_pic_index.0];
                 let prim_header = PrimitiveHeader {
-                    local_rect: backdrop_picture.local_rect,
+                    local_rect: backdrop_picture.precise_local_rect,
                     local_clip_rect: prim_info.combined_local_clip_rect,
                     transform_id,
                     specific_prim_address: prim_cache_address,
