c929efd7dcefda52e108f6db1429fdcba6d137a3
Rishi Gupta <rishigpt2009@gmail.com>
2019-09-24 13:50:10 +0000

    Bug 1572706 - Stop handling "extensions.shield-recipe-client.*" preferences in Marionette and geckodriver r=whimboo
    Those preferences got updated to "app.normandy.*" with Firefox 60, and are no longer necessary to be set.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45576
    
    --HG--
    extra : moz-landing-system : lando

--- testing/marionette/harness/marionette_harness/tests/unit/test_prefs.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_prefs.py
@@ -30,16 +30,8 @@ class TestPreferences(MarionetteTestCase):
         required_prefs = geckoinstance.GeckoInstance.required_prefs
 
         for key, value in required_prefs.iteritems():
-            # The extensions.shield-recipe-client.* prefs branch
-            # is as of Firefox 60 migrated to app.normandy.*.
-            #
-            # This is a workaround that can be removed together
-            # with extensions.shield-recipe-client.api_url.
-            if key == "extensions.shield-recipe-client.api_url":
-                self.assertEqual(self.marionette.get_pref("app.normandy.api_url"), value)
-            else:
-                self.assertEqual(self.marionette.get_pref(key), value,
-                                 "Preference {} hasn't been set to {}".format(key, repr(value)))
+            self.assertEqual(self.marionette.get_pref(key), value,
+                             "Preference {} hasn't been set to {}".format(key, repr(value)))
 
     def test_desktop_instance_preferences(self):
         required_prefs = geckoinstance.DesktopInstance.desktop_prefs
