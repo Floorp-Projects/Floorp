021b9b908783da3deed53fef34575b49d26fcf59
Ehsan Akhgari <ehsan@mozilla.com>
2019-09-24 12:39:24 +0000

    Bug 1552800 - Instead of assuming success, do proper error checking; r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D46600
    
    --HG--
    extra : moz-landing-system : lando

--- dom/indexedDB/ScriptErrorHelper.cpp
+++ dom/indexedDB/ScriptErrorHelper.cpp
@@ -95,7 +95,6 @@ class ScriptErrorRunnable final : public mozilla::Runnable {
 
     nsCOMPtr<nsIConsoleService> consoleService =
         do_GetService(NS_CONSOLESERVICE_CONTRACTID);
-    MOZ_ASSERT(consoleService);
 
     nsCOMPtr<nsIScriptError> scriptError =
         do_CreateInstance(NS_SCRIPTERROR_CONTRACTID);
@@ -115,7 +114,11 @@ class ScriptErrorRunnable final : public mozilla::Runnable {
           /* from chrome context */ aIsChrome));
     }
 
-    MOZ_ALWAYS_SUCCEEDS(consoleService->LogMessage(scriptError));
+    // We may not be able to obtain the console service when we're shutting
+    // down.
+    if (consoleService) {
+      MOZ_ALWAYS_SUCCEEDS(consoleService->LogMessage(scriptError));
+    }
   }
 
   NS_IMETHOD
