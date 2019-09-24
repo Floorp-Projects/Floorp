b5a80177f7bfc37a47ee42fc9c312e2bfa42985c
Boris Zbarsky <bzbarsky@mit.edu>
2019-09-24 16:53:44 +0000

    Bug 927610 part 1.  Stop using NS_ERROR_TYPE_ERR in UDPSocket.  r=smaug
    The only ways DispatchReceivedData can fail are if our scope is torn down, we
    OOM when creating a typed array, or we fail to queue a runnable.  These all
    seem like pretty unexpected situations, so NS_ERROR_UNEXPECTED seems
    appropriate if we don't want to just propagate through the error from
    DispatchReceivedData.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46483

--- dom/network/UDPSocket.cpp
+++ dom/network/UDPSocket.cpp
@@ -572,7 +572,7 @@ void UDPSocket::HandleReceivedData(const nsACString& aRemoteAddress,
   }
 
   if (NS_FAILED(DispatchReceivedData(aRemoteAddress, aRemotePort, aData))) {
-    CloseWithReason(NS_ERROR_TYPE_ERR);
+    CloseWithReason(NS_ERROR_UNEXPECTED);
   }
 }
 
