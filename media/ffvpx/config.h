36adeaea97df5c09a1dccd70158f5ff189269250
Michael Froman <mfroman@mozilla.com>
2019-09-24 21:02:07 +0000

    Bug 1582271 - pt1 - prep ffvpx files for adding mp3 support. r=jya
    - Update README_MOZILLA for adding mp3 support, and add some clarifying text.
     - Clang-format config.h for easier reading since it is our file, not ffmpeg's.
     - Use sort -d -u to produce defaults_disabled.* files so linux and macOS
       produce same files.
     - Change MOZ_FFVPX_FLACONLY to MOZ_FFVPX_AUDIOONLY since it indicates flac
       and mp3 decoders.
     - Rename config_flac.h to config_audio.h
    
    Differential Revision: https://phabricator.services.mozilla.com/D46423
    
    --HG--
    rename : media/ffvpx/config_flac.h => media/ffvpx/config_audio.h
    extra : moz-landing-system : lando

--- media/ffvpx/config.h
+++ media/ffvpx/config.h
@@ -6,46 +6,49 @@
 
 #ifndef MOZ_FFVPX_CONFIG_H
 #define MOZ_FFVPX_CONFIG_H
-#if defined(MOZ_FFVPX_FLACONLY)
-#if defined(MOZ_WIDGET_ANDROID) && defined(__arm__)
-#include "config_android32.h"
-#else
-#include "config_flac.h"
-#endif
-#else // MOZ_FFVPX_FLACONLY
-#if defined(XP_WIN)
+
+#if defined(MOZ_FFVPX_AUDIOONLY)
+#  if defined(MOZ_WIDGET_ANDROID) && defined(__arm__)
+#    include "config_android32.h"
+#  else
+#    include "config_audio.h"
+#  endif
+#else  // MOZ_FFVPX_AUDIOONLY
+#  if defined(XP_WIN)
 // Avoid conflicts with mozilla-config.h
-#if !defined(_MSC_VER)
-#undef HAVE_DIRENT_H
-#undef HAVE_UNISTD_H
-#endif
-#if defined(_ARM64_)
-#include "config_aarch64_win64.h"
-#else
-#if defined(HAVE_64BIT_BUILD)
-#include "config_win64.h"
-#else
-#include "config_win32.h"
-#endif
-#endif
+#    if !defined(_MSC_VER)
+#      undef HAVE_DIRENT_H
+#      undef HAVE_UNISTD_H
+#    endif
+#    if defined(_ARM64_)
+#      include "config_aarch64_win64.h"
+#    else
+#      if defined(HAVE_64BIT_BUILD)
+#        include "config_win64.h"
+#      else
+#        include "config_win32.h"
+#      endif
+#    endif
 // Adjust configure defines for GCC
-#if !defined(_MSC_VER)
-#if !defined(HAVE_64BIT_BUILD)
-#undef HAVE_MM_EMPTY
-#define HAVE_MM_EMPTY 0
-#endif
-#undef HAVE_LIBC_MSVCRT
-#define HAVE_LIBC_MSVCRT 0
-#endif
-#elif defined(XP_DARWIN)
-#include "config_darwin64.h"
-#elif defined(XP_UNIX)
-#if defined(HAVE_64BIT_BUILD)
-#include "config_unix64.h"
-#else
-#include "config_unix32.h"
-#endif
-#endif
-#endif // else MOZ_FFVPX_FLACONLY
+#    if !defined(_MSC_VER)
+#      if !defined(HAVE_64BIT_BUILD)
+#        undef HAVE_MM_EMPTY
+#        define HAVE_MM_EMPTY 0
+#      endif
+#      undef HAVE_LIBC_MSVCRT
+#      define HAVE_LIBC_MSVCRT 0
+#    endif
+#  elif defined(XP_DARWIN)
+#    include "config_darwin64.h"
+#  elif defined(XP_UNIX)
+#    if defined(HAVE_64BIT_BUILD)
+#      include "config_unix64.h"
+#    else
+#      include "config_unix32.h"
+#    endif
+#  endif
+#endif  // else MOZ_FFVPX_AUDIOONLY
+
 #include "config_common.h"
-#endif // MOZ_FFVPX_CONFIG_H
+
+#endif  // MOZ_FFVPX_CONFIG_H
