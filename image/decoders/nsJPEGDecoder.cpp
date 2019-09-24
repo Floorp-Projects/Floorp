4bf69333a239cb6f5e53ed2593de8c14f4ffa78d
Andrew Osmond <aosmond@mozilla.com>
2019-09-24 20:42:47 +0000

    Bug 1551088 - Part 6. Make image decoders use accelerated methods for swizzling and premultiplication. r=tnikkel
    The PNG decoder lacks fast implementations for swizzling/unpacking
    inside the library, and both PNG and WebP may need to perform
    premultiplication due to the alpha channel. This patch adds a new filter
    allowing us to take advantage of our accelerated implementations to
    perform these transformations on their behalf.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46449
    
    --HG--
    extra : moz-landing-system : lando

--- image/decoders/nsJPEGDecoder.cpp
+++ image/decoders/nsJPEGDecoder.cpp
@@ -357,7 +357,8 @@ LexerTransition<nsJPEGDecoder::State> nsJPEGDecoder::ReadJPEGData(
 
       Maybe<SurfacePipe> pipe = SurfacePipeFactory::CreateSurfacePipe(
           this, Size(), OutputSize(), FullFrame(), SurfaceFormat::B8G8R8X8,
-          Nothing(), pipeTransform, SurfacePipeFlags());
+          SurfaceFormat::B8G8R8X8, Nothing(), pipeTransform,
+          SurfacePipeFlags());
       if (!pipe) {
         mState = JPEG_ERROR;
         MOZ_LOG(sJPEGDecoderAccountingLog, LogLevel::Debug,
