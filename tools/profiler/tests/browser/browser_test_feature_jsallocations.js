7e6bf6932cf84822ad7f3810bf00e362bd0dfee2
Olli Pettay <Olli.Pettay@helsinki.fi>
2019-09-24 09:23:35 +0000

    Bug 1582655, try to make browser_test_feature_preferencereads.js less racy and let browser_test_feature_jsallocations.js to run a bit longer, r=whawkins
    Differential Revision: https://phabricator.services.mozilla.com/D46865

--- tools/profiler/tests/browser/browser_test_feature_jsallocations.js
+++ tools/profiler/tests/browser/browser_test_feature_jsallocations.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+requestLongerTimeout(10);
+
 /**
  * Test the JS Allocations feature. This is done as a browser test to ensure that
  * we realistically try out how the JS allocations are running. This ensures that
