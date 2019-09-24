74c90812324ffe351f8fa12bc6d7dd6a2a74b9e5
Nihanth Subramanya <nhnt11@gmail.com>
2019-09-24 16:45:55 +0000

    Bug 1571210 - Wait for content blocking events in trackingUI tests before interacting with the page. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D46792
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/trackingUI/browser_trackingUI_trackers_subview.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_trackers_subview.js
@@ -22,81 +22,81 @@ add_task(async function setup() {
 });
 
 async function assertSitesListed(blocked) {
-  await BrowserTestUtils.withNewTab(TRACKING_PAGE, async function(browser) {
-    await openProtectionsPopup();
-
-    let categoryItem = document.getElementById(
-      "protections-popup-category-tracking-protection"
-    );
-    ok(
-      BrowserTestUtils.is_visible(categoryItem),
-      "TP category item is visible"
-    );
-    let trackersView = document.getElementById(
-      "protections-popup-trackersView"
-    );
-    let viewShown = BrowserTestUtils.waitForEvent(trackersView, "ViewShown");
-    categoryItem.click();
-    await viewShown;
-
-    ok(true, "Trackers view was shown");
-
-    let listItems = trackersView.querySelectorAll(
-      ".protections-popup-list-item"
-    );
-    is(listItems.length, 1, "We have 1 tracker in the list");
-
-    let mainView = document.getElementById("protections-popup-mainView");
-    viewShown = BrowserTestUtils.waitForEvent(mainView, "ViewShown");
-    let backButton = trackersView.querySelector(".subviewbutton-back");
-    backButton.click();
-    await viewShown;
-
-    ok(true, "Main view was shown");
-
-    let change = waitForSecurityChange(1);
-    let timeoutPromise = new Promise(resolve => setTimeout(resolve, 1000));
-
-    await ContentTask.spawn(browser, {}, function() {
-      content.postMessage("more-tracking", "*");
-    });
-
-    let result = await Promise.race([change, timeoutPromise]);
-    is(result, undefined, "No securityChange events should be received");
-
-    viewShown = BrowserTestUtils.waitForEvent(trackersView, "ViewShown");
-    categoryItem.click();
-    await viewShown;
-
-    ok(true, "Trackers view was shown");
-
-    listItems = Array.from(
-      trackersView.querySelectorAll(".protections-popup-list-item")
-    );
-    is(listItems.length, 2, "We have 2 trackers in the list");
-
-    let listItem = listItems.find(
-      item => item.querySelector("label").value == "trackertest.org"
-    );
-    ok(listItem, "Has an item for trackertest.org");
-    ok(BrowserTestUtils.is_visible(listItem), "List item is visible");
-    is(
-      listItem.classList.contains("allowed"),
-      !blocked,
-      "Indicates whether the tracker was blocked or allowed"
-    );
-
-    listItem = listItems.find(
-      item => item.querySelector("label").value == "itisatracker.org"
-    );
-    ok(listItem, "Has an item for itisatracker.org");
-    ok(BrowserTestUtils.is_visible(listItem), "List item is visible");
-    is(
-      listItem.classList.contains("allowed"),
-      !blocked,
-      "Indicates whether the tracker was blocked or allowed"
-    );
+  let promise = BrowserTestUtils.openNewForegroundTab({
+    url: TRACKING_PAGE,
+    gBrowser,
   });
+
+  // Wait for 2 content blocking events - one for the load and one for the tracker.
+  let [tab] = await Promise.all([promise, waitForContentBlockingEvent(2)]);
+
+  await openProtectionsPopup();
+
+  let categoryItem = document.getElementById(
+    "protections-popup-category-tracking-protection"
+  );
+  ok(BrowserTestUtils.is_visible(categoryItem), "TP category item is visible");
+  let trackersView = document.getElementById("protections-popup-trackersView");
+  let viewShown = BrowserTestUtils.waitForEvent(trackersView, "ViewShown");
+  categoryItem.click();
+  await viewShown;
+
+  ok(true, "Trackers view was shown");
+
+  let listItems = trackersView.querySelectorAll(".protections-popup-list-item");
+  is(listItems.length, 1, "We have 1 tracker in the list");
+
+  let mainView = document.getElementById("protections-popup-mainView");
+  viewShown = BrowserTestUtils.waitForEvent(mainView, "ViewShown");
+  let backButton = trackersView.querySelector(".subviewbutton-back");
+  backButton.click();
+  await viewShown;
+
+  ok(true, "Main view was shown");
+
+  let change = waitForSecurityChange(1);
+  let timeoutPromise = new Promise(resolve => setTimeout(resolve, 1000));
+
+  await ContentTask.spawn(tab.linkedBrowser, {}, function() {
+    content.postMessage("more-tracking", "*");
+  });
+
+  let result = await Promise.race([change, timeoutPromise]);
+  is(result, undefined, "No securityChange events should be received");
+
+  viewShown = BrowserTestUtils.waitForEvent(trackersView, "ViewShown");
+  categoryItem.click();
+  await viewShown;
+
+  ok(true, "Trackers view was shown");
+
+  listItems = Array.from(
+    trackersView.querySelectorAll(".protections-popup-list-item")
+  );
+  is(listItems.length, 2, "We have 2 trackers in the list");
+
+  let listItem = listItems.find(
+    item => item.querySelector("label").value == "trackertest.org"
+  );
+  ok(listItem, "Has an item for trackertest.org");
+  ok(BrowserTestUtils.is_visible(listItem), "List item is visible");
+  is(
+    listItem.classList.contains("allowed"),
+    !blocked,
+    "Indicates whether the tracker was blocked or allowed"
+  );
+
+  listItem = listItems.find(
+    item => item.querySelector("label").value == "itisatracker.org"
+  );
+  ok(listItem, "Has an item for itisatracker.org");
+  ok(BrowserTestUtils.is_visible(listItem), "List item is visible");
+  is(
+    listItem.classList.contains("allowed"),
+    !blocked,
+    "Indicates whether the tracker was blocked or allowed"
+  );
+  BrowserTestUtils.removeTab(tab);
 }
 
 add_task(async function testTrackersSubView() {
