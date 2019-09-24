220372483a1d7f81bea372875f6c9ba38a66f453
Matt Woodrow <mwoodrow@mozilla.com>
2019-09-24 12:13:49 +0000

    Bug 1583076 - Pass LoadInfo back for the current channel when confirming redirects. r=ckerschb
    We previously used the initial LoadInfo from when the DocumentChannel was created, but need the one from the most recent channel in the parent.
    
    Depends on D46740
    
    Differential Revision: https://phabricator.services.mozilla.com/D46741

--- netwerk/ipc/DocumentChannelChild.cpp
+++ netwerk/ipc/DocumentChannelChild.cpp
@@ -255,12 +255,8 @@ IPCResult DocumentChannelChild::RecvRedirectToRealChannel(
     const uint32_t& aRedirectFlags, const Maybe<uint32_t>& aContentDisposition,
     const Maybe<nsString>& aContentDispositionFilename,
     RedirectToRealChannelResolver&& aResolve) {
-  nsCOMPtr<nsILoadInfo> originalLoadInfo;
   RefPtr<dom::Document> loadingDocument;
-  GetLoadInfo(getter_AddRefs(originalLoadInfo));
-  if (originalLoadInfo) {
-    originalLoadInfo->GetLoadingDocument(getter_AddRefs(loadingDocument));
-  }
+  mLoadInfo->GetLoadingDocument(getter_AddRefs(loadingDocument));
 
   nsCOMPtr<nsILoadInfo> loadInfo;
   nsresult rv = LoadInfoArgsToLoadInfo(aLoadInfo, loadingDocument,
@@ -404,11 +400,18 @@ DocumentChannelChild::OnRedirectVerifyCallback(nsresult aStatusCode) {
 }
 
 IPCResult DocumentChannelChild::RecvConfirmRedirect(
-    nsIURI* aNewUri, ConfirmRedirectResolver&& aResolve) {
+    const LoadInfoArgs& aLoadInfo, nsIURI* aNewUri,
+    ConfirmRedirectResolver&& aResolve) {
   // This is effectively the same as AsyncOnChannelRedirect, except since we're
   // not propagating the redirect into this process, we don't have an nsIChannel
   // for the redirection and we have to do the checks manually.
   // This just checks CSP thus far, hopefully there's not much else needed.
+  RefPtr<dom::Document> loadingDocument;
+  mLoadInfo->GetLoadingDocument(getter_AddRefs(loadingDocument));
+  nsCOMPtr<nsILoadInfo> loadInfo;
+  MOZ_ALWAYS_SUCCEEDS(LoadInfoArgsToLoadInfo(Some(aLoadInfo), loadingDocument,
+                                             getter_AddRefs(loadInfo)));
+
   nsCOMPtr<nsIURI> originalUri;
   nsresult rv = GetOriginalURI(getter_AddRefs(originalUri));
   if (NS_FAILED(rv)) {
@@ -418,7 +421,7 @@ IPCResult DocumentChannelChild::RecvConfirmRedirect(
   }
 
   Maybe<nsresult> cancelCode;
-  rv = CSPService::ConsultCSPForRedirect(originalUri, aNewUri, mLoadInfo,
+  rv = CSPService::ConsultCSPForRedirect(originalUri, aNewUri, loadInfo,
                                          cancelCode);
   aResolve(Tuple<const nsresult&, const Maybe<nsresult>&>(rv, cancelCode));
   return IPC_OK();
