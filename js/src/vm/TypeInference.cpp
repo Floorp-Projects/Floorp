f79eef9814ae5bff3f49e6ce55ac60187653df23
Ted Campbell <tcampbell@mozilla.com>
2019-09-24 19:55:27 +0000

    Bug 1548044 - Handle unknownProperties as result of AutoSweepObjectGroup r=jandem,iain
    Differential Revision: https://phabricator.services.mozilla.com/D29575

--- js/src/vm/TypeInference.cpp
+++ js/src/vm/TypeInference.cpp
@@ -1307,6 +1307,9 @@ bool CompilerConstraintInstance<T>::generateTypeConstraint(
   }
 
   AutoSweepObjectGroup sweep(property.object()->maybeGroup());
+  if (property.object()->maybeGroup()->unknownProperties(sweep)) {
+    return false;
+  }
   if (!data.constraintHolds(sweep, cx, property, expected)) {
     return false;
   }
@@ -1363,7 +1366,9 @@ HeapTypeSetKey TypeSet::ObjectKey::property(jsid id) {
   property.id_ = id;
   if (ObjectGroup* group = maybeGroup()) {
     AutoSweepObjectGroup sweep(group);
-    property.maybeTypes_ = group->maybeGetProperty(sweep, id);
+    if (!group->unknownProperties(sweep)) {
+      property.maybeTypes_ = group->maybeGetProperty(sweep, id);
+    }
   }
 
   return property;
