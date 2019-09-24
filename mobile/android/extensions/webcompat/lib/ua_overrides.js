5085abf569e28c855a69ba99e60c6d258a016f00
Thomas Wisniewski <twisniewski@mozilla.com>
2019-09-24 13:08:42 +0000

    Bug 1583225 - Import WebCompat GoFaster 6.1.0 sources; r=miketaylr, a=RyanVM
    Import WebCompat GoFaster 6.1.0 sources
    
    Differential Revision: https://phabricator.services.mozilla.com/D46814
    
    --HG--
    extra : source : 07c68c882f140fe3d75e2b435c2a7edf3f9ae8c7
    extra : intermediate-source : 8d4dfb36052db73c031be45d89405b560d382229

--- mobile/android/extensions/webcompat/lib/ua_overrides.js
+++ mobile/android/extensions/webcompat/lib/ua_overrides.js
@@ -52,11 +52,26 @@ class UAOverrides {
       return;
     }
 
-    const { matches, uaTransformer } = override.config;
+    const { blocks, matches, telemetryKey, uaTransformer } = override.config;
     const listener = details => {
-      for (const header of details.requestHeaders) {
-        if (header.name.toLowerCase() === "user-agent") {
-          header.value = uaTransformer(header.value);
+      // Don't actually override the UA for an experiment if the user is not
+      // part of the experiment (unless they force-enabed the override).
+      if (
+        !override.config.experiment ||
+        override.experimentActive ||
+        override.permanentPrefEnabled === true
+      ) {
+        if (telemetryKey && !details.frameId) {
+          // For now, we only care about Telemetry on Fennec, where telemetry
+          // is sent in Java code (as part of the core ping). That code must
+          // be aware of each key we send, which we send as a SharedPreference.
+          browser.sharedPreferences.setBoolPref(`${telemetryKey}Used`, true);
+        }
+
+        for (const header of details.requestHeaders) {
+          if (header.name.toLowerCase() === "user-agent") {
+            header.value = uaTransformer(header.value);
+          }
         }
       }
       return { requestHeaders: details.requestHeaders };
@@ -68,8 +83,76 @@ class UAOverrides {
       ["blocking", "requestHeaders"]
     );
 
-    this._activeListeners.set(override, listener);
+    const listeners = { onBeforeSendHeaders: listener };
+    if (blocks) {
+      const blistener = details => {
+        return { cancel: true };
+      };
+
+      browser.webRequest.onBeforeRequest.addListener(
+        blistener,
+        { urls: blocks },
+        ["blocking"]
+      );
+
+      listeners.onBeforeRequest = blistener;
+    }
+    this._activeListeners.set(override, listeners);
     override.active = true;
+
+    // If collecting telemetry on the override, note that it was activated.
+    if (telemetryKey) {
+      browser.sharedPreferences.setBoolPref(`${telemetryKey}Ready`, true);
+    }
+  }
+
+  onOverrideConfigChanged(override) {
+    // Check whether the override should be hidden from about:compat.
+    override.hidden = override.config.hidden;
+
+    // Also hide if the override is in an experiment the user is not part of.
+    if (override.config.experiment && !override.experimentActive) {
+      override.hidden = true;
+    }
+
+    // Setting the override's permanent pref overrules whether it is hidden.
+    if (override.permanentPrefEnabled !== undefined) {
+      override.hidden = !override.permanentPrefEnabled;
+    }
+
+    // Also check whether the override should be active.
+    let shouldBeActive = true;
+
+    // Overrides can be force-deactivated by their permanent preference.
+    if (override.permanentPrefEnabled === false) {
+      shouldBeActive = false;
+    }
+
+    // Overrides gated behind an experiment the user is not part of do not
+    // have to be activated, unless they are gathering telemetry, or the
+    // user has force-enabled them with their permanent pref.
+    if (
+      override.config.experiment &&
+      !override.experimentActive &&
+      !override.config.telemetryKey &&
+      override.permanentPrefEnabled !== true
+    ) {
+      shouldBeActive = false;
+    }
+
+    if (shouldBeActive) {
+      this.enableOverride(override);
+    } else {
+      this.disableOverride(override);
+    }
+
+    if (this._overridesEnabled) {
+      this._aboutCompatBroker.portsToAboutCompatTabs.broadcast({
+        overridesChanged: this._aboutCompatBroker.filterOverrides(
+          this._availableOverrides
+        ),
+      });
+    }
   }
 
   async registerUAOverrides() {
@@ -80,7 +163,32 @@ class UAOverrides {
     for (const override of this._availableOverrides) {
       if (platformMatches.includes(override.platform)) {
         override.availableOnPlatform = true;
-        this.enableOverride(override);
+
+        // If there is a specific experiment running for
+        // this override, get its state.
+        const experiment = override.config.experiment;
+        override.experimentActive =
+          experiment && (await browser.experiments.isActive(experiment));
+
+        // If there is a specific about:config preference governing
+        // this override, monitor its state.
+        const pref = override.config.permanentPref;
+        override.permanentPrefEnabled =
+          pref && (await browser.aboutConfigPrefs.getPref(pref));
+        if (pref) {
+          const checkOverridePref = () => {
+            browser.aboutConfigPrefs.getPref(pref).then(value => {
+              override.permanentPrefEnabled = value;
+              this.onOverrideConfigChanged(override);
+            });
+          };
+          browser.aboutConfigPrefs.onPrefChange.addListener(
+            checkOverridePref,
+            pref
+          );
+        }
+
+        this.onOverrideConfigChanged(override);
       }
     }
 
@@ -108,9 +216,10 @@ class UAOverrides {
       return;
     }
 
-    browser.webRequest.onBeforeSendHeaders.removeListener(
-      this._activeListeners.get(override)
-    );
+    const listeners = this._activeListeners.get(override);
+    for (const [name, listener] of Object.entries(listeners)) {
+      browser.webRequest[name].removeListener(listener);
+    }
     override.active = false;
     this._activeListeners.delete(override);
   }
