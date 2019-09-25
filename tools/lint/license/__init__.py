d8f681f85f1ee194ea0f6177c2b9527e72ea5c22
Andrew Halberstadt <ahalberstadt@mozilla.com>
2019-09-25 20:03:14 +0000

    Bug 1580280 - [mozlint] Run |mach lint| with Python 3 and drop support for Python 2 r=mars
    Differential Revision: https://phabricator.services.mozilla.com/D45441

--- tools/lint/license/__init__.py
+++ tools/lint/license/__init__.py
@@ -2,13 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import absolute_import, print_function
-
 import os
 
 from mozlint import result
 from mozlint.pathutils import expand_exclusions
-from six import PY2
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -46,8 +43,7 @@ def is_valid_license(licenses, filename):
     From a given file, check if we can find the license patterns
     in the X first lines of the file
     """
-    kwargs = {} if PY2 else {'errors': 'replace'}
-    with open(filename, 'r', **kwargs) as myfile:
+    with open(filename, 'r', errors='replace') as myfile:
         contents = myfile.read()
         # Empty files don't need a license.
         if not contents:
