43676bbdc4fce2c051d1ea600e108140b4d9f4e7
Timothy Nikkel <tnikkel@gmail.com>
2019-09-25 19:24:07 +0000

    Bug 1580317. Fix calculation of buffer size. r=aosmond a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D47123
    
    --HG--
    extra : source : 4975f8c1003bf6a71844573954fde121abdd8b8b

--- image/imgFrame.cpp
+++ image/imgFrame.cpp
@@ -87,7 +87,7 @@ static bool ShouldUseHeap(const IntSize& aSize, int32_t aStride,
 
   // Lets us avoid too many small images consuming all of the handles. The
   // actual allocation checks for overflow.
-  int32_t bufferSize = (aStride * aSize.width) / 1024;
+  int32_t bufferSize = (aStride * aSize.height) / 1024;
   if (bufferSize < StaticPrefs::image_mem_volatile_min_threshold_kb()) {
     return true;
   }
