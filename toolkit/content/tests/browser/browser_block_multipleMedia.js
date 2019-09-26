e6f3f3d1f959a2e20615c629e023b5a508ec3999
alwu <alwu@mozilla.com>
2019-09-26 02:53:52 +0000

    Bug 1578615 - part6 : modify tests. r=chunmin
    We could simply check `TabBlockEvent` to know whether we starts delaying media playback or not. We don't need to check media element's `computedSuspended`. Because `computedSuspended` is something, which was designed to get the internal suspended state of `AudioChannelWrapper`, in our case, as the delaying autoplay is not related to `AudioChannelWrapper` anymore, so the state we got actually couldn't reflect anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D44920

--- toolkit/content/tests/browser/browser_block_multipleMedia.js
+++ toolkit/content/tests/browser/browser_block_multipleMedia.js
@@ -2,45 +2,6 @@
 const PAGE =
   "https://example.com/browser/toolkit/content/tests/browser/file_multipleAudio.html";
 
-var SuspendedType = {
-  NONE_SUSPENDED: 0,
-  SUSPENDED_PAUSE: 1,
-  SUSPENDED_BLOCK: 2,
-  SUSPENDED_PAUSE_DISPOSABLE: 3,
-};
-
-function check_all_audio_suspended(suspendedType) {
-  let autoPlay = content.document.getElementById("autoplay");
-  let nonAutoPlay = content.document.getElementById("nonautoplay");
-  if (!autoPlay || !nonAutoPlay) {
-    ok(false, "Can't get the audio element!");
-  }
-
-  is(
-    autoPlay.computedSuspended,
-    suspendedType,
-    "The suspeded state of autoplay audio is correct."
-  );
-  is(
-    nonAutoPlay.computedSuspended,
-    suspendedType,
-    "The suspeded state of non-autoplay audio is correct."
-  );
-}
-
-function check_autoplay_audio_suspended(suspendedType) {
-  let autoPlay = content.document.getElementById("autoplay");
-  if (!autoPlay) {
-    ok(false, "Can't get the audio element!");
-  }
-
-  is(
-    autoPlay.computedSuspended,
-    suspendedType,
-    "The suspeded state of autoplay audio is correct."
-  );
-}
-
 function check_autoplay_audio_onplay() {
   let autoPlay = content.document.getElementById("autoplay");
   if (!autoPlay) {
@@ -63,19 +24,6 @@ function check_autoplay_audio_onplay() {
       resolve();
     }, 1000);
   });
-}
-
-function check_nonautoplay_audio_suspended(suspendedType) {
-  let nonAutoPlay = content.document.getElementById("nonautoplay");
-  if (!nonAutoPlay) {
-    ok(false, "Can't get the audio element!");
-  }
-
-  is(
-    nonAutoPlay.computedSuspended,
-    suspendedType,
-    "The suspeded state of non-autoplay audio is correct."
-  );
 }
 
 function play_nonautoplay_audio_should_be_blocked() {
@@ -108,21 +56,7 @@ add_task(async function block_multiple_media() {
   await waitForTabBlockEvent(tab, true);
 
   info("- autoplay media should be blocked -");
-  await ContentTask.spawn(
-    browser,
-    SuspendedType.SUSPENDED_BLOCK,
-    check_autoplay_audio_suspended
-  );
   await ContentTask.spawn(browser, null, check_autoplay_audio_onplay);
-
-  info(
-    "- non-autoplay media won't be blocked, because it doesn't start playing -"
-  );
-  await ContentTask.spawn(
-    browser,
-    SuspendedType.NONE_SUSPENDED,
-    check_nonautoplay_audio_suspended
-  );
 
   info("- non-autoplay can't start playback when the tab is blocked -");
   await ContentTask.spawn(
@@ -130,22 +64,13 @@ add_task(async function block_multiple_media() {
     null,
     play_nonautoplay_audio_should_be_blocked
   );
-  await ContentTask.spawn(
-    browser,
-    SuspendedType.SUSPENDED_BLOCK,
-    check_nonautoplay_audio_suspended
-  );
 
   info("- select tab as foreground tab -");
   await BrowserTestUtils.switchTab(window.gBrowser, tab);
 
   info("- tab should be resumed -");
   await waitForTabPlayingEvent(tab, true);
-  await ContentTask.spawn(
-    browser,
-    SuspendedType.NONE_SUSPENDED,
-    check_all_audio_suspended
-  );
+  await waitForTabBlockEvent(tab, false);
 
   info("- remove tab -");
   BrowserTestUtils.removeTab(tab);
