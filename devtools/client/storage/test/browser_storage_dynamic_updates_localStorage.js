9323417073d615ed3c8cd0044608dce0179bad87
Luca Greco <lgreco@mozilla.com>
2019-09-26 12:28:06 +0000

    Bug 1578447 - Fix storage panel ui not refreshing on changes to items with string keys parsable as JSON. r=miker a=lizzard
    In /devtools/client/storage/ui.js, handleChangedItems and handleDeletedItems should only JSON.parse string keys related to the indexedDB and Cache actors.
    
    The string keys used by all the other storage actors are expected to be a simple string keys, and their valid values include strings that can be successfully parsed as JSON (e.g. "123" or "null" are both valid keys for the storage items related to the localStorage, extensionStorage etc.).
    
    Differential Revision: https://phabricator.services.mozilla.com/D45197
    
    --HG--
    extra : source : 36bfbbce7ee2f6205e292436432a70ae40bd5bdd

--- devtools/client/storage/test/browser_storage_dynamic_updates_localStorage.js
+++ devtools/client/storage/test/browser_storage_dynamic_updates_localStorage.js
@@ -7,46 +7,22 @@
 // Test dynamic updates in the storage inspector for localStorage.
 
 add_task(async function() {
+  const TEST_HOST = "http://test1.example.org";
+
   await openTabAndSetupStorage(MAIN_DOMAIN + "storage-updates.html");
 
   gUI.tree.expandAll();
 
   ok(gUI.sidebar.hidden, "Sidebar is initially hidden");
 
-  await checkState([
-    [
-      ["localStorage", "http://test1.example.org"],
-      ["ls1", "ls2", "ls3", "ls4", "ls5", "ls6", "ls7"],
-    ],
-  ]);
+  const expectedKeys = ["1", "2", "3", "4", "5", "null", "non-json-parsable"];
 
-  await removeLocalStorageItem("ls4");
-
-  await gUI.once("store-objects-edit");
-
-  await checkState([
-    [
-      ["localStorage", "http://test1.example.org"],
-      ["ls1", "ls2", "ls3", "ls5", "ls6", "ls7"],
-    ],
-  ]);
-
-  await setLocalStorageItem("ls4", "again");
-
-  await gUI.once("store-objects-edit");
-
-  await checkState([
-    [
-      ["localStorage", "http://test1.example.org"],
-      ["ls1", "ls2", "ls3", "ls4", "ls5", "ls6", "ls7"],
-    ],
-  ]);
-  // Updating a row
-  await setLocalStorageItem("ls2", "ls2-changed");
-
-  await gUI.once("store-objects-edit");
-
-  checkCell("ls2", "value", "ls2-changed");
+  // Test on string keys that JSON.parse can parse without throwing
+  // (to verify the issue fixed by Bug 1578447 doesn't regress).
+  await testRemoveAndChange("null", expectedKeys, TEST_HOST);
+  await testRemoveAndChange("4", expectedKeys, TEST_HOST);
+  // Test on a string that makes JSON.parse to throw.
+  await testRemoveAndChange("non-json-parsable", expectedKeys, TEST_HOST);
 
   // Clearing items.
   await ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
@@ -55,10 +31,29 @@ add_task(async function() {
 
   await gUI.once("store-objects-cleared");
 
-  await checkState([[["localStorage", "http://test1.example.org"], []]]);
+  await checkState([[["localStorage", TEST_HOST], []]]);
 
   await finishTests();
 });
+
+async function testRemoveAndChange(targetKey, expectedKeys, host) {
+  await checkState([[["localStorage", host], expectedKeys]]);
+
+  await removeLocalStorageItem(targetKey);
+  await gUI.once("store-objects-edit");
+  await checkState([
+    [["localStorage", host], expectedKeys.filter(key => key !== targetKey)],
+  ]);
+
+  await setLocalStorageItem(targetKey, "again");
+  await gUI.once("store-objects-edit");
+  await checkState([[["localStorage", host], expectedKeys]]);
+
+  // Updating a row set to the string "null"
+  await setLocalStorageItem(targetKey, `key-${targetKey}-changed`);
+  await gUI.once("store-objects-edit");
+  checkCell(targetKey, "value", `key-${targetKey}-changed`);
+}
 
 async function setLocalStorageItem(key, value) {
   await ContentTask.spawn(
