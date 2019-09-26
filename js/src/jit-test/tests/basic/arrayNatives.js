1f920838b5744a2ea6e76dd01f904693e6d009b4
Brindusan Cristian <cbrindusan@mozilla.com>
2019-09-27 01:33:44 +0300

    Backed out changeset 311d8c50d88b (bug 1222547) for spidermonkey bustages on JSFunction.h:568. CLOSED TREE

---
+++ js/src/jit-test/tests/basic/arrayNatives.js
@@ -0,0 +1,18 @@
+// |jit-test| skip-if: !(getBuildConfiguration().release_or_beta)
+/* Array natives applied to non-arrays. */
+
+var oa = {};
+Array.pop(oa);
+assertEq(oa.length, 0);
+
+var ob = {};
+Array.push(ob, "twelve");
+assertEq(ob.length, 1);
+
+var oc = {};
+Array.shift(oc);
+assertEq(oc.length, 0);
+
+var od = {};
+Array.unshift(od, "eight");
+assertEq(od.length, 1);
