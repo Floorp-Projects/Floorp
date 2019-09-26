88670a47e97dc1289c304386537b55ddb99a84c4
Sebastian Streich <sstreich@mozilla.com>
2019-09-26 10:47:16 +0000

    Bug 1580782 - Change Callsites to use nsIPrincipal->SchemeIs r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D45654
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/antitracking/StorageAccess.cpp
+++ toolkit/components/antitracking/StorageAccess.cpp
@@ -134,11 +134,7 @@ static StorageAccess InternalStorageAllowedCheck(
   // be affected, which is desireable due to the lack of automated testing for
   // about: URIs with these preferences set, and the importance of the correct
   // functioning of these URIs even with custom preferences.
-  nsCOMPtr<nsIURI> uri = aURI;
-  if (!uri) {
-    Unused << aPrincipal->GetURI(getter_AddRefs(uri));
-  }
-  if (uri && uri->SchemeIs("about")) {
+  if ((aURI && aURI->SchemeIs("about")) || aPrincipal->SchemeIs("about")) {
     return access;
   }
 
