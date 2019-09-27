209f6e49426de9436719388a5f2875a0e4e6b043
Emilio Cobos Álvarez <emilio@crisal.io>
2019-09-27 12:31:23 +0000

    Bug 1583439 - followup: Update lmdb-rkv-sys to avoid pulling a bunch of slow-to-compile crates. r=vporof
    Patch created with:
    
    $ cargo update -p lmdb-rkv-sys
    Updating crates.io index
    Updating lmdb-rkv-sys v0.9.3 -> v0.9.5
    Removing which v3.0.0
    
    $ cargo update -p bindgen --precise 0.51.1-oldsyn
    Updating crates.io index
    Updating bindgen v0.51.1 -> v0.51.1-oldsyn
    Removing proc-macro2 v1.0.4
    Removing quote v1.0.2
    Removing unicode-xid v0.2.0
    
    $ ./mach vendor rust
    
    Differential Revision: https://phabricator.services.mozilla.com/D47284
    
    --HG--
    rename : third_party/rust/quote-0.6.11/tests/conditional/integer128.rs => third_party/rust/quote/tests/conditional/integer128.rs
    rename : third_party/rust/unicode-xid-0.1.0/scripts/unicode.py => third_party/rust/unicode-xid/scripts/unicode.py
    extra : moz-landing-system : lando

--- third_party/rust/lmdb-rkv-sys/src/lib.rs
+++ third_party/rust/lmdb-rkv-sys/src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![allow(clippy::all)]
-#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.9.3")]
+#![doc(html_root_url = "https://docs.rs/lmdb-rkv-sys/0.9.5")]
 
 extern crate libc;
 
