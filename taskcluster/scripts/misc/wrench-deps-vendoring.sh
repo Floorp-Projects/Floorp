7edeff79c0d84b4ab4987b81df5119379d706f78
Jamie Nicol <jnicol@mozilla.com>
2019-09-27 13:50:43 +0000

    Bug 1581628 - Update wrench jobs to work with NDK r20. r=jrmuizel
    To support NDK r20, wrench needs to be built with a more recent, upstream
    (though still unpublished) version of cargo-apk. This has some consequences
    which have been adjusted for:
     * Gradle is no longer required to build wrench.
     * The output apk file paths have changed.
     * The apks are now signed automatically.
     * The default activity name has changed.
     * Android permissions must be explicitly requested.
     * We must ensure winit is built with a matching version of android_glue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47129

--- taskcluster/scripts/misc/wrench-deps-vendoring.sh
+++ taskcluster/scripts/misc/wrench-deps-vendoring.sh
@@ -18,10 +18,7 @@ mkdir wrench-deps
 mv vendor .cargo wrench-deps/
 mkdir wrench-deps/cargo-apk
 # Until there's a version of cargo-apk published on crates.io that has
-# https://github.com/tomaka/android-rs-glue/pull/205 and
-# https://github.com/tomaka/android-rs-glue/pull/207 and
-# https://github.com/tomaka/android-rs-glue/pull/171 (see also
-# https://github.com/tomaka/android-rs-glue/issues/204), we need to use
+# https://github.com/rust-windowing/android-rs-glue/pull/223, we need to use
 # an unpublished version.
 cargo install --path $MOZ_FETCHES_DIR/android-rs-glue/cargo-apk --root wrench-deps/cargo-apk cargo-apk
 tar caf wrench-deps.tar.bz2 wrench-deps
