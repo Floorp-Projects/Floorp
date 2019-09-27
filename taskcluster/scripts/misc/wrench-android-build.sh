6bd07ccf22ad83ff0fd0fc3da90534d0be7907b7
Jamie Nicol <jnicol@mozilla.com>
2019-09-27 13:50:43 +0000

    Bug 1581628 - Update wrench jobs to work with NDK r20. r=jrmuizel
    To support NDK r20, wrench needs to be built with a more recent, upstream
    (though still unpublished) version of cargo-apk. This has some consequences
    which have been adjusted for:
     * Gradle is no longer required to build wrench.
     * The output apk file paths have changed.
     * The apks are now signed automatically.
     * The default activity name has changed.
     * Android permissions must be explicitly requested.
     * We must ensure winit is built with a matching version of android_glue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47129

--- taskcluster/scripts/misc/wrench-android-build.sh
+++ taskcluster/scripts/misc/wrench-android-build.sh
@@ -8,39 +8,20 @@ mv wrench-deps/{vendor,.cargo,cargo-apk} ${GECKO_PATH}/gfx/wr
 popd
 
 pushd "${GECKO_PATH}/gfx/wr/wrench"
-# The following maven links are equivalent to GRADLE_MAVEN_REPOSITORIES, try
-# and keep in sync
-cat > build.gradle.inc <<END
-buildscript {
-    repositories {
-        maven{ url uri('file:${MOZ_FETCHES_DIR}/android-gradle-dependencies/google') }
-        maven{ url uri('file:${MOZ_FETCHES_DIR}/android-gradle-dependencies/jcenter') }
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:3.4.2'
-    }
-}
-allprojects {
-    repositories {
-        maven{ url uri('file:${MOZ_FETCHES_DIR}/android-gradle-dependencies/google') }
-        maven{ url uri('file:${MOZ_FETCHES_DIR}/android-gradle-dependencies/jcenter') }
-    }
-}
-END
 # These things come from the toolchain dependencies of the job that invokes
 # this script (webrender-wrench-android-build).
 export PATH="${PATH}:${MOZ_FETCHES_DIR}/rustc/bin"
 export ANDROID_HOME="${MOZ_FETCHES_DIR}/android-sdk-linux"
 export NDK_HOME="${MOZ_FETCHES_DIR}/android-ndk"
-export CARGO_APK_GRADLE_COMMAND="${MOZ_FETCHES_DIR}/android-gradle-dependencies/gradle-dist/bin/gradle"
-export CARGO_APK_BUILD_GRADLE_INC="${PWD}/build.gradle.inc"
 
+# `cargo apk build` fails whilst attempting to sign the output apk,
+# unless the `.android` directory exists.
+# See https://github.com/rust-windowing/android-rs-glue/issues/252
+mkdir /builds/worker/.android
 if [ "$MODE" == "debug" ]; then
     ../cargo-apk/bin/cargo-apk build --frozen --verbose
 elif [ "$MODE" == "release" ]; then
     ../cargo-apk/bin/cargo-apk build --frozen --verbose --release
-    ${GECKO_PATH}/mobile/android/debug_sign_tool.py --verbose \
-        ../target/android-artifacts/app/build/outputs/apk/release/app-release-unsigned.apk
 else
     echo "Unknown mode '${MODE}'; must be 'debug' or 'release'"
     exit 1
