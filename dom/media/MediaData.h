c18b81f79ff50d487566c3066a029e761d795d12
Dorel Luca <dluca@mozilla.com>
2019-09-27 23:31:51 +0300

    Backed out 3 changesets (bug 1509316) for MDA failures on dom/media/tests/mochitest/test_peerConnection_captureStream_canvas_2d.html
    Backed out changeset d64c35b9c211 (bug 1509316)
    Backed out changeset 817f14af91f0 (bug 1509316)
    Backed out changeset b4bee18bb499 (bug 1509316)

--- dom/media/MediaData.h
+++ dom/media/MediaData.h
@@ -451,6 +451,12 @@ class VideoData : public MediaData {
     ColorRange mColorRange = ColorRange::LIMITED;
   };
 
+  class Listener {
+   public:
+    virtual void OnSentToCompositor() = 0;
+    virtual ~Listener() {}
+  };
+
   // Constructs a VideoData object. If aImage is nullptr, creates a new Image
   // holding a copy of the YCbCr data passed in aBuffer. If aImage is not
   // nullptr, it's stored as the underlying video image and aBuffer is assumed
@@ -505,7 +511,8 @@ class VideoData : public MediaData {
             const media::TimeUnit& aTimecode, IntSize aDisplay,
             uint32_t aFrameID);
 
-  void MarkSentToCompositor() { mSentToCompositor = true; }
+  void SetListener(UniquePtr<Listener> aListener);
+  void MarkSentToCompositor();
   bool IsSentToCompositor() { return mSentToCompositor; }
 
   void UpdateDuration(const media::TimeUnit& aDuration);
@@ -521,6 +528,7 @@ class VideoData : public MediaData {
   ~VideoData();
 
   bool mSentToCompositor;
+  UniquePtr<Listener> mListener;
   media::TimeUnit mNextKeyFrameTime;
 };
 
