4892b1dbbbf9f09760bebd50dcbe336935706fcc
Nathan Froyd <froydnj@mozilla.com>
2019-09-27 13:50:43 +0000

    Bug 1577220 - update to the r20 Android NDK; r=snorp,nalexander
    This version has a newer libc++, brings along some linker fixes, etc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D45574

--- python/mozboot/mozboot/android.py
+++ python/mozboot/mozboot/android.py
@@ -13,7 +13,7 @@ import sys
 # We need the NDK version in multiple different places, and it's inconvenient
 # to pass down the NDK version to all relevant places, so we have this global
 # variable.
-NDK_VERSION = 'r17b'
+NDK_VERSION = 'r20'
 
 ANDROID_NDK_EXISTS = '''
 Looks like you have the Android NDK installed at:
