45889c23c5eb2ca471e5207713be2753e109d719
Ted Campbell <tcampbell@mozilla.com>
2019-09-30 12:57:43 +0000

    Bug 1583816 - Store associated JSFunction directly in JSScript r=jandem
    For function JSScripts, store the JSFunction in the BaseScript
    functionOrGlobal field. This makes JSScript more consistent with the
    LazyScript behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47070

--- js/src/frontend/BytecodeCompilation.h
+++ js/src/frontend/BytecodeCompilation.h
@@ -93,7 +93,8 @@ class MOZ_STACK_CLASS BytecodeCompiler {
 
   // Create a script for source of the given length, using the explicitly-
   // provided toString offsets as the created script's offsets in the source.
-  MOZ_MUST_USE bool internalCreateScript(uint32_t toStringStart,
+  MOZ_MUST_USE bool internalCreateScript(HandleObject functionOrGlobal,
+                                         uint32_t toStringStart,
                                          uint32_t toStringEnd,
                                          uint32_t sourceBufferLength);
 
