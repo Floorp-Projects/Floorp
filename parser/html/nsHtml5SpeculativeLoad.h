0c088dfc5acf0e95608d681096cf134fd85f7f1e
Emilio Cobos Álvarez <emilio@crisal.io>
2019-09-30 13:00:22 +0000

    Bug 1546783 - Speculatively load @import rules inside <style>. r=bzbarsky,hsivonen
    We don't bother handling the nested element case amazingly. We'd instead stop
    at the inner <style> element and drop the URLs from the outer.
    
    But I think that's ok. Any good way to test this? I've verified it does the
    right thing looking at the CSS loader logs, but... :)
    
    Differential Revision: https://phabricator.services.mozilla.com/D47471

--- parser/html/nsHtml5SpeculativeLoad.h
+++ parser/html/nsHtml5SpeculativeLoad.h
@@ -154,6 +154,13 @@ class nsHtml5SpeculativeLoad {
     mIsDefer = aDefer;
   }
 
+  inline void InitImportStyle(nsString&& aUrl) {
+    MOZ_ASSERT(mOpCode == eSpeculativeLoadUninitialized,
+               "Trying to reinitialize a speculative load!");
+    mOpCode = eSpeculativeLoadStyle;
+    mUrlOrSizes = std::move(aUrl);
+  }
+
   inline void InitStyle(nsHtml5String aUrl, nsHtml5String aCharset,
                         nsHtml5String aCrossOrigin,
                         nsHtml5String aReferrerPolicy,
