10ea46fdbb1329f3b4ab8407a5ebcd7872546768
Tim Huang <tihuang@mozilla.com>
2019-09-30 12:36:05 +0000

    Bug 1578334 - Update the tooltip of the tracking protection icon when toggling the TP switch. r=nhnt11
    In order to show a correct tooltip after toggling the TP switch. We
    update the tooltip of the tracking protection icon when toggling the TP
    switch right before the 500 ms waiting.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47587
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/browser-siteProtections.js
+++ browser/base/content/browser-siteProtections.js
@@ -1289,6 +1289,14 @@ var gProtectionsHandler = {
         "trackingProtection.icon.disabledTooltip2"
       ));
     },
+
+    get noTrackerTooltipText() {
+      delete this.noTrackerTooltipText;
+      return (this.noTrackerTooltipText = gNavigatorBundle.getFormattedString(
+        "trackingProtection.icon.noTrackersDetectedTooltip",
+        [gBrandBundle.GetStringFromName("brandShortName")]
+      ));
+    },
   },
 
   // A list of blockers that will be displayed in the categories list
@@ -1625,35 +1633,19 @@ var gProtectionsHandler = {
     this.iconBox.toggleAttribute("hasException", hasException);
 
     if (hasException) {
-      this._trackingProtectionIconTooltipLabel.textContent = this.strings.disabledTooltipText;
-      gIdentityHandler._trackingProtectionIconContainer.setAttribute(
-        "aria-label",
-        this.strings.disabledTooltipText
-      );
+      this.showDisabledTooltipForTPIcon();
       if (!this.hadShieldState && !isSimulated) {
         this.hadShieldState = true;
         this.shieldHistogramAdd(1);
       }
     } else if (anyBlocking) {
-      this._trackingProtectionIconTooltipLabel.textContent = this.strings.activeTooltipText;
-      gIdentityHandler._trackingProtectionIconContainer.setAttribute(
-        "aria-label",
-        this.strings.activeTooltipText
-      );
+      this.showActiveTooltipForTPIcon();
       if (!this.hadShieldState && !isSimulated) {
         this.hadShieldState = true;
         this.shieldHistogramAdd(2);
       }
     } else {
-      let noTrackerTooltipStr = gNavigatorBundle.getFormattedString(
-        "trackingProtection.icon.noTrackersDetectedTooltip",
-        [gBrandBundle.GetStringFromName("brandShortName")]
-      );
-      this._trackingProtectionIconTooltipLabel.textContent = noTrackerTooltipStr;
-      gIdentityHandler._trackingProtectionIconContainer.setAttribute(
-        "aria-label",
-        noTrackerTooltipStr
-      );
+      this.showNoTrackerTooltipForTPIcon();
     }
 
     if (
@@ -1889,6 +1881,13 @@ var gProtectionsHandler = {
     // Toggle the breakage link if needed.
     this.toggleBreakageLink();
 
+    // Change the tooltip of the tracking protection icon.
+    if (newExceptionState) {
+      this.showDisabledTooltipForTPIcon();
+    } else {
+      this.showNoTrackerTooltipForTPIcon();
+    }
+
     // Change the state of the tracking protection icon.
     this.iconBox.toggleAttribute("hasException", newExceptionState);
 
@@ -1941,6 +1940,30 @@ var gProtectionsHandler = {
     this._protectionsPopupTrackersCounterBox.toggleAttribute(
       "showing",
       trackerCount != 0
+    );
+  },
+
+  showDisabledTooltipForTPIcon() {
+    this._trackingProtectionIconTooltipLabel.textContent = this.strings.disabledTooltipText;
+    gIdentityHandler._trackingProtectionIconContainer.setAttribute(
+      "aria-label",
+      this.strings.disabledTooltipText
+    );
+  },
+
+  showActiveTooltipForTPIcon() {
+    this._trackingProtectionIconTooltipLabel.textContent = this.strings.activeTooltipText;
+    gIdentityHandler._trackingProtectionIconContainer.setAttribute(
+      "aria-label",
+      this.strings.activeTooltipText
+    );
+  },
+
+  showNoTrackerTooltipForTPIcon() {
+    this._trackingProtectionIconTooltipLabel.textContent = this.strings.noTrackerTooltipText;
+    gIdentityHandler._trackingProtectionIconContainer.setAttribute(
+      "aria-label",
+      this.strings.noTrackerTooltipText
     );
   },
 
