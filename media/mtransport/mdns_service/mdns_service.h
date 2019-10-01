340d659b0c9b82ca1c1f9c99ba566b2010856dde
Dan Minor <dminor@mozilla.com>
2019-10-01 12:58:18 +0000

    Bug 1567201 - Add query support to mdns_service; r=mjf
    This adds basic query support to the mdns_service. Support for limiting
    the number of pending queries, timeouts and retries is added in
    another commit in this series.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46975
    
    --HG--
    extra : moz-landing-system : lando

--- media/mtransport/mdns_service/mdns_service.h
+++ media/mtransport/mdns_service/mdns_service.h
@@ -14,6 +14,12 @@ MDNSService* mdns_service_start(const char* ifaddr);
 
 void mdns_service_stop(MDNSService* serv);
 
+void mdns_service_query_hostname(MDNSService* serv, void* data,
+                                 void (*resolved)(void* data,
+                                                  const char* hostname,
+                                                  const char* address),
+                                 const char* hostname);
+
 void mdns_service_unregister_hostname(MDNSService* serv, const char* hostname);
 
 const char* mdns_service_generate_uuid();
