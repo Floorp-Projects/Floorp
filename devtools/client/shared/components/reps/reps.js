fc33edc41db706376c109ab71125ed5a415cd4a2
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-10-01 13:17:22 +0000

    Bug 1584244 - Fix invoke getter action on quoted property. r=Honza.
    When clicking the invoke button on a property that
    needs to be quoted, e.g. hello-world, the result
    was undefined.
    That's because we were sending the property name to
    the server with the surrounding quotes.
    This patch fixes the issue and adds a couple of test
    case to make sure we don't regress.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47399

--- devtools/client/shared/components/reps/reps.js
+++ devtools/client/shared/components/reps/reps.js
@@ -8241,8 +8241,17 @@ class ObjectInspectorItem extends Component {
         const receiverGrip = getNonPrototypeParentGripValue(item);
 
         if (targetGrip && receiverGrip) {
+          let propertyName = item.name; // If we're dealing with a property that can't be accessed
+          // with the dot notation, for example: x["hello-world"]
+
+          if (propertyName.startsWith(`"`) && propertyName.endsWith(`"`)) {
+            // We remove the quotes wrapping the property name, and we replace any
+            // "double" escaped quotes (\\\") by simple escaped ones (\").
+            propertyName = propertyName.substring(1, propertyName.length - 1).replace(/\\\"/g, `\"`);
+          }
+
           Object.assign(repProps, {
-            onInvokeGetterButtonClick: () => this.props.invokeGetter(item, targetGrip, receiverGrip.actor, item.name)
+            onInvokeGetterButtonClick: () => this.props.invokeGetter(item, targetGrip, receiverGrip.actor, propertyName)
           });
         }
       }
