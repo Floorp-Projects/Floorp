79266a67afcc7fe35e3d0e25b7b4d7f9007cf228
Kershaw Chang <kershaw@mozilla.com>
2019-10-01 12:10:58 +0000

    Bug 1560353 - Extend SSLTokensCache to store the result of VerifySSLServerCert r=dragana,keeler
    Differential Revision: https://phabricator.services.mozilla.com/D46159

--- netwerk/base/SSLTokensCache.h
+++ netwerk/base/SSLTokensCache.h
@@ -8,12 +8,22 @@
 #include "nsIMemoryReporter.h"
 #include "nsClassHashtable.h"
 #include "nsTArray.h"
+#include "mozilla/Maybe.h"
 #include "mozilla/StaticMutex.h"
 #include "mozilla/StaticPtr.h"
 #include "nsXULAppAPI.h"
+#include "TransportSecurityInfo.h"  // For EVStatus
 
 namespace mozilla {
 namespace net {
+
+struct SessionCacheInfo {
+  psm::EVStatus mEVStatus = psm::EVStatus::NotEV;
+  uint16_t mCertificateTransparencyStatus =
+      nsITransportSecurityInfo::CERTIFICATE_TRANSPARENCY_NOT_APPLICABLE;
+  nsTArray<uint8_t> mServerCertBytes;
+  Maybe<nsTArray<nsTArray<uint8_t>>> mSucceededCertChainBytes;
+};
 
 class SSLTokensCache : public nsIMemoryReporter {
  public:
@@ -28,8 +38,10 @@ class SSLTokensCache : public nsIMemoryReporter {
   static bool IsEnabled() { return sEnabled; }
 
   static nsresult Put(const nsACString& aKey, const uint8_t* aToken,
-                      uint32_t aTokenLen);
+                      uint32_t aTokenLen, nsITransportSecurityInfo* aSecInfo);
   static nsresult Get(const nsACString& aKey, nsTArray<uint8_t>& aToken);
+  static bool GetSessionCacheInfo(const nsACString& aKey,
+                                  SessionCacheInfo& aResult);
   static nsresult Remove(const nsACString& aKey);
 
  private:
@@ -55,9 +67,13 @@ class SSLTokensCache : public nsIMemoryReporter {
 
   class TokenCacheRecord {
    public:
+    uint32_t Size() const;
+    void Reset();
+
     nsCString mKey;
     PRUint32 mExpirationTime;
     nsTArray<uint8_t> mToken;
+    SessionCacheInfo mSessionCacheInfo;
   };
 
   nsClassHashtable<nsCStringHashKey, TokenCacheRecord> mTokenCacheRecords;
