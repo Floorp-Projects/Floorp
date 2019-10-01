fc49ddb34e4397ba6fb5dcae82f675358d6191e7
Ciure Andrei <aciure@mozilla.com>
2019-10-01 07:39:54 +0300

    Backed out changeset 4f126cfd8012 (bug 1584439) for causing wrench bustage CLOSED TREE

--- gfx/wr/webrender/src/renderer.rs
+++ gfx/wr/webrender/src/renderer.rs
@@ -68,7 +68,7 @@ use crate::internal_types::{CacheTextureId, DebugOutput, FastHashMap, FastHashSe
 use crate::internal_types::{TextureCacheAllocationKind, TextureCacheUpdate, TextureUpdateList, TextureUpdateSource};
 use crate::internal_types::{RenderTargetInfo, SavedTargetIndex, Swizzle};
 use malloc_size_of::MallocSizeOfOps;
-use crate::picture::{RecordedDirtyRegion, TILE_SIZE_LARGE, TILE_SIZE_SMALL};
+use crate::picture::{RecordedDirtyRegion, TILE_SIZE_WIDTH, TILE_SIZE_HEIGHT};
 use crate::prim_store::DeferredResolve;
 use crate::profiler::{BackendProfileCounters, FrameProfileCounters, TimeProfileCounter,
                GpuProfileTag, RendererProfileCounters, RendererProfileTimers};
@@ -2150,8 +2150,7 @@ impl Renderer {
             }
 
             let picture_tile_sizes = &[
-                TILE_SIZE_LARGE,
-                TILE_SIZE_SMALL,
+                DeviceIntSize::new(TILE_SIZE_WIDTH, TILE_SIZE_HEIGHT),
             ];
 
             let texture_cache = TextureCache::new(
