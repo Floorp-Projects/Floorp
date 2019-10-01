4345e5aa63c65a047e2de40e5c06c5a80703649e
Nico Grunbaum <na-g@nostrum.com>
2019-10-01 01:01:56 +0000

    Bug 1560370 - move webrtc-sdp out of tree; r=drno
    Differential Revision: https://phabricator.services.mozilla.com/D45930

--- media/webrtc/signaling/src/sdp/rsdparsa/fuzz/fuzz_targets/fuzz_target_parse_sdp.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate webrtc_sdp;
-
-fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = std::str::from_utf8(data) {
-        let _ = webrtc_sdp::parse_sdp(s, true);
-    }
-});
