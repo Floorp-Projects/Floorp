4bb15c0af48a3678e4b38898282dd1349ec462de
Andrew Osmond <aosmond@mozilla.com>
2019-10-02 13:37:26 +0000

    Bug 1551088 - Part 4. Add gtests for PremultiplyRow and SwizzleRow. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D46447

--- gfx/tests/gtest/TestSwizzle.cpp
+++ gfx/tests/gtest/TestSwizzle.cpp
@@ -45,6 +45,34 @@ TEST(Moz2D, PremultiplyData)
   EXPECT_TRUE(ArrayEqual(out, check_argb));
 }
 
+TEST(Moz2D, PremultiplyRow)
+{
+  const uint8_t in_bgra[5 * 4] = {
+      255, 255, 0,   255,  // verify 255 alpha leaves RGB unchanged
+      0,   0,   255, 255,
+      0,   255, 255, 0,  // verify 0 alpha zeroes out RGB
+      0,   0,   0,   0,
+      255, 0,   0,   128,  // verify that 255 RGB maps to alpha
+  };
+  uint8_t out[5 * 4];
+  const uint8_t check_bgra[5 * 4] = {
+      255, 255, 0, 255, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0, 128, 0, 0, 128,
+  };
+  // check swizzled output
+  const uint8_t check_rgba[5 * 4] = {
+      0, 255, 255, 255, 255, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 128, 128,
+  };
+
+  SwizzleRowFn func =
+    PremultiplyRow(SurfaceFormat::B8G8R8A8, SurfaceFormat::B8G8R8A8);
+  func(in_bgra, out, 5);
+  EXPECT_TRUE(ArrayEqual(out, check_bgra));
+
+  func = PremultiplyRow(SurfaceFormat::B8G8R8A8, SurfaceFormat::R8G8B8A8);
+  func(in_bgra, out, 5);
+  EXPECT_TRUE(ArrayEqual(out, check_rgba));
+}
+
 TEST(Moz2D, UnpremultiplyData)
 {
   const uint8_t in_bgra[5 * 4] = {
@@ -154,4 +182,69 @@ TEST(Moz2D, SwizzleData)
               reinterpret_cast<uint8_t*>(out16), sizeof(out16),
               SurfaceFormat::R5G6B5_UINT16, IntSize(5, 1));
   EXPECT_TRUE(ArrayEqual(out16, check_16));
+}
+
+TEST(Moz2D, SwizzleRow)
+{
+  const uint8_t in_bgra[5 * 4] = {
+      253, 254, 0, 255, 0, 0, 255, 255, 0, 0, 0, 0, 1, 2, 3, 64, 127, 0, 9, 128,
+
+  };
+  uint8_t out[5 * 4];
+  // check swaps
+  const uint8_t check_rgba[5 * 4] = {
+      0, 254, 253, 255, 255, 0, 0, 255, 0, 0, 0, 0, 3, 2, 1, 64, 9, 0, 127, 128,
+  };
+  // check opaquifying
+  const uint8_t check_rgbx[5 * 4] = {
+      0, 254, 253, 255, 255, 0,   0, 255, 0,   0,
+      0, 255, 3,   2,   1,   255, 9, 0,   127, 255,
+  };
+  // check unpacking
+  uint8_t out_unpack[16 * 4];
+  const uint8_t in_rgb[16 * 3] = {
+      0, 254, 253, 255, 0, 0, 0, 0, 0, 3, 2, 1,
+      9, 0, 127, 4, 5, 6, 9, 8, 7, 10, 11, 12,
+      13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+      25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+  };
+  const uint8_t check_unpack_rgbx[16 * 4] = {
+      0, 254, 253, 255, 255, 0, 0, 255, 0, 0, 0, 255, 3, 2, 1, 255,
+      9, 0, 127, 255, 4, 5, 6, 255, 9, 8, 7, 255, 10, 11, 12, 255,
+      13, 14, 15, 255, 16, 17, 18, 255, 19, 20, 21, 255, 22, 23, 24, 255,
+      25, 26, 27, 255, 28, 29, 30, 255, 31, 32, 33, 255, 34, 35, 36, 255,
+  };
+  const uint8_t check_unpack_bgrx[16 * 4] = {
+      253, 254, 0, 255, 0, 0, 255, 255, 0, 0, 0, 255, 1, 2, 3, 255,
+      127, 0, 9, 255, 6, 5, 4, 255, 7, 8, 9, 255, 12, 11, 10, 255,
+      15, 14, 13, 255, 18, 17, 16, 255, 21, 20, 19, 255, 24, 23, 22, 255,
+      27, 26, 25, 255, 30, 29, 28, 255, 33, 32, 31, 255, 36, 35, 34, 255,
+  };
+
+  SwizzleRowFn func =
+    SwizzleRow(SurfaceFormat::B8G8R8A8, SurfaceFormat::R8G8B8A8);
+  func(in_bgra, out, 5);
+  EXPECT_TRUE(ArrayEqual(out, check_rgba));
+
+  func = SwizzleRow(SurfaceFormat::B8G8R8A8, SurfaceFormat::R8G8B8X8);
+  func(in_bgra, out, 5);
+  EXPECT_TRUE(ArrayEqual(out, check_rgbx));
+
+  func = SwizzleRow(SurfaceFormat::R8G8B8, SurfaceFormat::B8G8R8X8);
+  func(in_rgb, out_unpack, 16);
+  EXPECT_TRUE(ArrayEqual(out_unpack, check_unpack_bgrx));
+
+  memset(out_unpack, 0xE5, sizeof(out_unpack));
+  memcpy(out_unpack, in_rgb, sizeof(in_rgb));
+  func(out_unpack, out_unpack, 16);
+  EXPECT_TRUE(ArrayEqual(out_unpack, check_unpack_bgrx));
+
+  func = SwizzleRow(SurfaceFormat::R8G8B8, SurfaceFormat::R8G8B8X8);
+  func(in_rgb, out_unpack, 16);
+  EXPECT_TRUE(ArrayEqual(out_unpack, check_unpack_rgbx));
+
+  memset(out_unpack, 0xE5, sizeof(out_unpack));
+  memcpy(out_unpack, in_rgb, sizeof(in_rgb));
+  func(out_unpack, out_unpack, 16);
+  EXPECT_TRUE(ArrayEqual(out_unpack, check_unpack_rgbx));
 }
