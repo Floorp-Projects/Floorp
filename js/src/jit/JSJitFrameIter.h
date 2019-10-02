380ff7ebdeac23a04d91a3ebc122f28c0ebcdff3
Jan de Mooij <jdemooij@mozilla.com>
2019-10-02 01:22:12 +0000

    Bug 1583172 part 3 - Make JSJitFrameIter compute the Baseline frame size. r=tcampbell
    The computed value is equivalent to BaselineFrame::frameSize_, but later in the stack
    the BaselineFrame::frameSize_ field will be debug-only.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46913

--- js/src/jit/JSJitFrameIter.h
+++ js/src/jit/JSJitFrameIter.h
@@ -7,6 +7,8 @@
 #ifndef jit_JSJitFrameIter_h
 #define jit_JSJitFrameIter_h
 
+#include "mozilla/Maybe.h"
+
 #include "jstypes.h"
 
 #include "jit/IonCode.h"
@@ -108,6 +110,10 @@ class JSJitFrameIter {
   FrameType type_;
   uint8_t* resumePCinCurrentFrame_;
   size_t frameSize_;
+
+  // Size of the current Baseline frame. Equivalent to
+  // BaselineFrame::debugFrameSize_ in debug builds.
+  mozilla::Maybe<uint32_t> baselineFrameSize_;
 
  private:
   mutable const SafepointIndex* cachedSafepointIndex_;
