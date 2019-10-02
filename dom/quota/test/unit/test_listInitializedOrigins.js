3c7ec0657bf70140330c744a058cf252e7d0b4b6
Tom Tung <ttung@mozilla.com>
2019-10-02 07:57:57 +0000

    Bug 1584986 - P1 - Remove the early return for listInitializedOrigins; r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D47740

--- dom/quota/test/unit/test_listInitializedOrigins.js
+++ dom/quota/test/unit/test_listInitializedOrigins.js
@@ -64,20 +64,6 @@ async function testSteps() {
 
   await CreateTestEnvironment(origins);
 
-  info("Getting origins before initializing the storage");
-
-  await new Promise(resolve => {
-    listInitializedOrigins(request => {
-      verifyResult(request.result, []);
-      resolve();
-    });
-  });
-
-  info("Verifying result");
-
-  let request = initTemporaryStorage();
-  request = await requestFinished(request);
-
   info("Getting origins after initializing the storage");
 
   await new Promise(resolve => {
