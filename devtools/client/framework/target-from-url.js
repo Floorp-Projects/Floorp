adebb9b7900f187a47585cd8df8adffa0fb6abaa
Alexandre Poirot <poirot.alex@gmail.com>
2019-10-02 16:24:27 +0000

    Bug 1571703 - Close DebuggerClient's opened by about:debugging toolboxes. r=jdescottes
    When debugging firefox instance, we open about:devtools toolboxes,
    without any remoteId attribute. So that we create a brand new DebuggerClient
    instance. For now, this instance is only ever closed if the debugged target
    is a local tab. We should try to close the clients for all cases.
    Remote debugging clients are ignored and I think are still left opened.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47112

--- devtools/client/framework/target-from-url.js
+++ devtools/client/framework/target-from-url.js
@@ -54,8 +54,9 @@ exports.targetFromURL = async function targetFromURL(url) {
   const type = params.get("type");
   const chrome = params.has("chrome");
 
+  let target;
   try {
-    return await _targetFromURL(client, id, type, chrome);
+    target = await _targetFromURL(client, id, type, chrome);
   } catch (e) {
     if (!isCachedClient) {
       // If the client was not cached, then the client was created here. If the target
@@ -64,6 +65,15 @@ exports.targetFromURL = async function targetFromURL(url) {
     }
     throw e;
   }
+
+  // If this isn't a cached client, it means that we just created a new client
+  // in `clientFromURL` and we have to destroy it at some point.
+  // In such case, force the Target to destroy the client as soon as it gets
+  // destroyed. This typically happens only for about:debugging toolboxes
+  // opened for local Firefox's targets.
+  target.shouldCloseClient = !isCachedClient;
+
+  return target;
 };
 
 async function _targetFromURL(client, id, type, chrome) {
