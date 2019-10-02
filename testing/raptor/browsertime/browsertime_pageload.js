eb64c8ffb81415000002d74419b8c2f6259f6982
Rob Wood <rwood@mozilla.com>
2019-10-02 19:26:38 +0000

    Bug 1581990 - Align page-load test delays and timeouts under --browsertime with Raptor r=nalexander,tarek
    Differential Revision: https://phabricator.services.mozilla.com/D47833

--- testing/raptor/browsertime/browsertime_pageload.js
+++ testing/raptor/browsertime/browsertime_pageload.js
@@ -6,6 +6,7 @@ module.exports = async function(context, commands) {
   let page_cycles = context.options.browsertime.page_cycles;
   let page_cycle_delay = context.options.browsertime.page_cycle_delay;
   let foreground_delay = context.options.browsertime.foreground_delay;
+  let post_startup_delay = context.options.browsertime.post_startup_delay;
 
   context.log.info("Waiting for %d ms (foreground_delay)", foreground_delay);
   await commands.wait.byTime(foreground_delay);
@@ -13,9 +14,16 @@ module.exports = async function(context, commands) {
   context.log.info("Navigating to about:blank");
   await commands.navigate("about:blank");
 
+  context.log.info(
+    "Waiting for %d ms (post_startup_delay)",
+    post_startup_delay
+  );
+  await commands.wait.byTime(post_startup_delay);
+
   for (let count = 0; count < page_cycles; count++) {
     context.log.info("Cycle %d, waiting for %d ms", count, page_cycle_delay);
     await commands.wait.byTime(page_cycle_delay);
+
     context.log.info("Cycle %d, starting the measure", count);
     await commands.measure.start(url);
   }
