bfb5373a5d9d4cfffd3b9623597bc0620fd1f752
Brindusan Cristian <cbrindusan@mozilla.com>
2019-10-02 04:14:53 +0300

    Backed out 6 changesets (bug 1576714) for build bustages at nsFrameLoaderOwner.cpp. CLOSED TREE
    Backed out changeset 083967e704d2 (bug 1576714)
    Backed out changeset b3467f1bdde7 (bug 1576714)
    Backed out changeset 88e3b4b7fbaf (bug 1576714)
    Backed out changeset b91221da32c7 (bug 1576714)
    Backed out changeset 6996b7705f06 (bug 1576714)
    Backed out changeset a303fc193f60 (bug 1576714)

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -2122,11 +2122,30 @@ already_AddRefed<RemoteBrowser> ContentChild::CreateBrowser(
   RefPtr<BrowserBridgeChild> browserBridge =
       new BrowserBridgeChild(aFrameLoader, aBrowsingContext, tabId);
 
+  nsDocShell::Cast(docShell)->OOPChildLoadStarted(browserBridge);
+
   browserChild->SendPBrowserBridgeConstructor(
       browserBridge, PromiseFlatString(aContext.PresentationURL()), aRemoteType,
       aBrowsingContext, chromeFlags, tabId);
+  browserBridge->mIPCOpen = true;
 
-  return browserBridge->FinishInit();
+#if defined(ACCESSIBILITY)
+  a11y::DocAccessible* docAcc =
+      a11y::GetExistingDocAccessible(owner->OwnerDoc());
+  if (docAcc) {
+    a11y::Accessible* ownerAcc = docAcc->GetAccessible(owner);
+    if (ownerAcc) {
+      a11y::OuterDocAccessible* outerAcc = ownerAcc->AsOuterDoc();
+      if (outerAcc) {
+        outerAcc->SendEmbedderAccessible(browserBridge);
+      }
+    }
+  }
+#endif  // defined(ACCESSIBILITY)
+
+  RefPtr<BrowserBridgeHost> browserBridgeHost =
+      new BrowserBridgeHost(browserBridge);
+  return browserBridgeHost.forget();
 }
 
 PScriptCacheChild* ContentChild::AllocPScriptCacheChild(
