a020dc3af382c5512e806c2cf43be892a761da29
Andreas Pehrson <apehrson@mozilla.com>
2019-10-03 19:50:31 +0000

    Bug 1583463 - Don't allocate and append frames when the tab source's stream has been destroyed. r=jib
    Differential Revision: https://phabricator.services.mozilla.com/D47684

--- dom/media/webrtc/MediaEngineTabVideoSource.cpp
+++ dom/media/webrtc/MediaEngineTabVideoSource.cpp
@@ -257,6 +257,12 @@ void MediaEngineTabVideoSource::Draw() {
     return;
   }
 
+  if (mTrackMain->IsDestroyed()) {
+    // The track was already destroyed by MediaManager. This can happen because
+    // stopping the draw timer is async.
+    return;
+  }
+
   if (mWindow) {
     if (mScrollWithPage || mViewportWidth == INT32_MAX) {
       mWindow->GetInnerWidth(&mViewportWidth);
