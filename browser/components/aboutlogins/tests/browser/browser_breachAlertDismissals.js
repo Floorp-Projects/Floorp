c439f9c236582396f6c25e76d94e7636c6f9f782
Jared Wein <jwein@mozilla.com>
2019-10-03 15:32:47 +0000

    Bug 1583398 - Store breaches in the remote store for tests when expected, and otherwise prevent all of the fxmonitor logspam when running aboutlogins mochitest-browser tests. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D47529
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutlogins/tests/browser/browser_breachAlertDismissals.js
+++ browser/components/aboutlogins/tests/browser/browser_breachAlertDismissals.js
@@ -1,26 +1,21 @@
 /* Any copyright is dedicated to the Public Domain.
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
-let { LoginBreaches } = ChromeUtils.import(
-  "resource:///modules/LoginBreaches.jsm"
-);
-
-const TEST_BREACHES = [
-  {
-    AddedDate: "2019-12-20T23:56:26Z",
-    BreachDate: "2018-12-16",
-    Domain: "breached.example.com",
-    Name: "Breached",
-    PwnCount: 1643100,
-    DataClasses: ["Email addresses", "Usernames", "Passwords", "IP addresses"],
-    _status: "synced",
-    id: "047940fe-d2fd-4314-b636-b4a952ee0043",
-    last_modified: "1541615610052",
-    schema: "1541615609018",
-  },
-];
+EXPECTED_BREACH = {
+  AddedDate: "2018-12-20T23:56:26Z",
+  BreachDate: "2018-12-16",
+  Domain: "breached.example.com",
+  Name: "Breached",
+  PwnCount: 1643100,
+  DataClasses: ["Email addresses", "Usernames", "Passwords", "IP addresses"],
+  _status: "synced",
+  id: "047940fe-d2fd-4314-b636-b4a952ee0043",
+  last_modified: "1541615610052",
+  schema: "1541615609018",
+};
 
 add_task(async function setup() {
+  TEST_LOGIN3.QueryInterface(Ci.nsILoginMetaInfo).timePasswordChanged = 123456;
   TEST_LOGIN3 = await addLogin(TEST_LOGIN3);
   await BrowserTestUtils.openNewForegroundTab({
     gBrowser,
@@ -34,15 +29,6 @@ add_task(async function setup() {
 
 add_task(async function test_show_login() {
   let browser = gBrowser.selectedBrowser;
-  TEST_LOGIN3.timePasswordChanged = 12345;
-  let testBreaches = await LoginBreaches.getPotentialBreachesByLoginGUID(
-    [TEST_LOGIN3],
-    TEST_BREACHES
-  );
-  browser.messageManager.sendAsyncMessage(
-    "AboutLogins:SetBreaches",
-    testBreaches
-  );
   await ContentTask.spawn(browser, null, async () => {
     let loginItem = Cu.waiveXrays(content.document.querySelector("login-item"));
     let breachAlert = loginItem.shadowRoot.querySelector(".breach-alert");
