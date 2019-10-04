82ee4e20aaa549aff10c10925555a4fa6c157782
Edouard Oger <eoger@fastmail.com>
2019-10-04 13:57:37 +0000

    Bug 1586075 - Add login failed avatar icon. r=vbudhram
    Differential Revision: https://phabricator.services.mozilla.com/D48117

--- browser/base/content/browser-sync.js
+++ browser/base/content/browser-sync.js
@@ -402,8 +402,9 @@ var gSync = {
 
     // The Firefox Account toolbar currently handles 3 different states for
     // users. The default `not_configured` state shows an empty avatar, `unverified`
-    // state shows an avatar with an email icon and the `verified` state will show
-    // the users custom profile image or a filled avatar.
+    // state shows an avatar with an email icon, `login-failed` state shows an avatar
+    // with a danger icon and the `verified` state will show the users
+    // custom profile image or a filled avatar.
     let stateValue = "not_configured";
 
     const menuHeaderTitleEl = document.getElementById("fxa-menu-header-title");
@@ -452,7 +453,7 @@ var gSync = {
     if (state.status === UIState.STATUS_NOT_CONFIGURED) {
       mainWindowEl.style.removeProperty("--avatar-image-url");
     } else if (state.status === UIState.STATUS_LOGIN_FAILED) {
-      stateValue = "unverified";
+      stateValue = "login-failed";
       headerTitle = this.fxaStrings.GetStringFromName("account.reconnectToFxA");
       headerDescription = state.email;
       mainWindowEl.style.removeProperty("--avatar-image-url");
