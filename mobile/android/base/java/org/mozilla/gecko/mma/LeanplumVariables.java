0e1885d86188f181eb02a34ee61d6c5d087dac9d
Vlad Baicu <vlad.baicu@softvision.ro>
2019-10-04 14:32:40 +0000

    Bug 1586205 - Change LP variables to fallback to same nonLP defaults.r=petru
    Differential Revision: https://phabricator.services.mozilla.com/D48184
    
    --HG--
    extra : moz-landing-system : lando

--- mobile/android/base/java/org/mozilla/gecko/mma/LeanplumVariables.java
+++ mobile/android/base/java/org/mozilla/gecko/mma/LeanplumVariables.java
@@ -33,19 +33,14 @@ public class LeanplumVariables {
     private static Resources appResources;
 
     private static final String FIRSTRUN_WELCOME_PANEL_GROUP_NAME = "FirstRun Welcome Panel";
-    private static final String FIRSTRUN_PRIVACY_PANEL_GROUP_NAME = "FirstRun Privacy Panel";
     private static final String FIRSTRUN_CUSTOMIZE_PANEL_GROUP_NAME = "FirstRun Customize Panel";
     private static final String FIRSTRUN_SYNC_PANEL_GROUP_NAME = "FirstRun Sync Panel";
+    private static final String FIRSTRUN_SEND_TAB_PANEL_GROUP_NAME = "FirstRun Send Tab Panel";
 
     @Variable(group = FIRSTRUN_WELCOME_PANEL_GROUP_NAME) public static String welcomePanelTitle;
     @Variable(group = FIRSTRUN_WELCOME_PANEL_GROUP_NAME) public static String welcomePanelMessage;
     @Variable(group = FIRSTRUN_WELCOME_PANEL_GROUP_NAME) public static String welcomePanelSubtext;
     @DrawableRes private static int welcomeDrawableId;
-
-    @Variable(group = FIRSTRUN_PRIVACY_PANEL_GROUP_NAME) public static String privacyPanelTitle;
-    @Variable(group = FIRSTRUN_PRIVACY_PANEL_GROUP_NAME) public static String privacyPanelMessage;
-    @Variable(group = FIRSTRUN_PRIVACY_PANEL_GROUP_NAME) public static String privacyPanelSubtext;
-    @DrawableRes private static int privacyDrawableId;
 
     @Variable(group = FIRSTRUN_CUSTOMIZE_PANEL_GROUP_NAME) public static String customizePanelTitle;
     @Variable(group = FIRSTRUN_CUSTOMIZE_PANEL_GROUP_NAME) public static String customizePanelMessage;
@@ -56,6 +51,11 @@ public class LeanplumVariables {
     @Variable(group = FIRSTRUN_SYNC_PANEL_GROUP_NAME) public static String syncPanelMessage;
     @Variable(group = FIRSTRUN_SYNC_PANEL_GROUP_NAME) public static String syncPanelSubtext;
     @DrawableRes private static int syncDrawableId;
+
+    @Variable(group = FIRSTRUN_SEND_TAB_PANEL_GROUP_NAME) public static String sendTabPanelTitle;
+    @Variable(group = FIRSTRUN_SEND_TAB_PANEL_GROUP_NAME) public static String sendTabPanelMessage;
+    @Variable(group = FIRSTRUN_SEND_TAB_PANEL_GROUP_NAME) public static String sendTabPanelSubtext;
+    @DrawableRes private static int sendTabDrawableId;
 
     /**
      * Allows constructing and/or returning an already constructed instance of this class
@@ -82,33 +82,31 @@ public class LeanplumVariables {
 
         // Same titles for the screens of the old / new onboarding UX.
         welcomePanelTitle = appResources.getString(R.string.firstrun_panel_title_welcome);
-        privacyPanelTitle = appResources.getString(R.string.firstrun_panel_title_privacy);
         customizePanelTitle = appResources.getString(R.string.firstrun_panel_title_customize);
 
         // The new Onboarding UX uses different messages and images. Only if they are localized.
         OnboardingResources onboardingUtil = OnboardingResources.getInstance(context);
         syncPanelTitle = onboardingUtil.getSyncTitle();
 
+        sendTabPanelTitle = appResources.getString(R.string.firstrun_sendtab_title);
+
         if (onboardingUtil.useNewOnboarding()) {
             welcomePanelMessage = onboardingUtil.getWelcomeMessage();
             welcomePanelSubtext = onboardingUtil.getWelcomeSubtext();
             welcomeDrawableId = R.drawable.firstrun_welcome2;
 
-            privacyPanelMessage = FirstrunPanel.NO_MESSAGE;
-            privacyPanelSubtext = onboardingUtil.getPrivacySubtext();
-            privacyDrawableId = R.drawable.firstrun_private2;
-
             syncPanelMessage = FirstrunPanel.NO_MESSAGE;
             syncPanelSubtext = onboardingUtil.getSyncSubtext();
             syncDrawableId = onboardingUtil.getSyncImageResId();
+
+            sendTabPanelMessage = FirstrunPanel.NO_MESSAGE;
+            sendTabPanelSubtext = appResources.getString(R.string.firstrun_sendtab_message);
+            sendTabDrawableId = R.drawable.firstrun_sendtab;
+
         } else {
             welcomePanelMessage = appResources.getString(R.string.firstrun_urlbar_message);
             welcomePanelSubtext = appResources.getString(R.string.firstrun_urlbar_subtext);
             welcomeDrawableId = R.drawable.firstrun_welcome;
-
-            privacyPanelMessage = appResources.getString(R.string.firstrun_privacy_message);
-            privacyPanelSubtext = appResources.getString(R.string.firstrun_privacy_subtext);
-            privacyDrawableId = R.drawable.firstrun_private;
 
             customizePanelMessage = appResources.getString(R.string.firstrun_customize_message);
             customizePanelSubtext = appResources.getString(R.string.firstrun_customize_subtext);
@@ -124,16 +122,16 @@ public class LeanplumVariables {
         return welcomeDrawableId;
     }
 
-    public static int getPrivacyImage() {
-        return privacyDrawableId;
-    }
-
     public static int getCustomizingImage() {
         return customizingDrawableId;
     }
 
     public static int getSyncImage() {
         return syncDrawableId;
+    }
+
+    public static int getSendTabImage() {
+        return sendTabDrawableId;
     }
 
 }
