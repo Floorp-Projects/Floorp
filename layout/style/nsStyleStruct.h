4cafd390e15b1784241303ed0d367117beab4c0d
Savo <savo@jarsl.co.uk>
2019-10-04 12:34:28 +0200

    Bug 1586068 - Replace #define NS_STYLE_FLEX_WRAP with enum class. r=emilio
    MANUAL PUSH: Rebasing contributor patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48166

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -1033,7 +1033,7 @@ struct MOZ_NEEDS_MEMMOVABLE_MEMBERS nsStylePosition {
   uint8_t mJustifyItems;
   uint8_t mJustifySelf;
   mozilla::StyleFlexDirection mFlexDirection;
-  uint8_t mFlexWrap;   // NS_STYLE_FLEX_WRAP_*
+  mozilla::StyleFlexWrap mFlexWrap;
   uint8_t mObjectFit;  // NS_STYLE_OBJECT_FIT_*
   int32_t mOrder;
   float mFlexGrow;
