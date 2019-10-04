89cbd6bb5e677d47eaf96196a3d2023e2780b906
Nick Alexander <nalexander@mozilla.com>
2019-10-04 20:56:27 +0000

    Bug 1580356 - Pre: Don't collect package metrics. r=mshal
    Differential Revision: https://phabricator.services.mozilla.com/D47696

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_gradle_dependencies.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_gradle_dependencies.py
@@ -3,7 +3,6 @@ config = {
     'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16-gradle-dependencies/nightly',
     'multi_locale_config_platform': 'android',
      # gradle-dependencies doesn't produce a package. So don't collect package metrics.
-    'disable_package_metrics': True,
     'postflight_build_mach_commands': [
         ['android',
          'gradle-dependencies',
