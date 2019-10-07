f371ad39d480c8fb5dd636385b98eff5bff463d4
Greg Tatum <gtatum@mozilla.com>
2019-10-07 13:44:12 +0000

    Bug 1581569 - Add type definitions for performance-new client; r=julienw
    Everything should be typed in the JS Docs. I added a bunch of TODOs for features
    the could be added in the future, but weren't valuable for this initial
    prototype. Unfortunately I couldn't get the @types folder working, and had to
    co-locate the redux.d.ts next to the vender file. TypeScript was attempting
    to parse the file, and was failing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46042

--- devtools/client/performance-new/store/selectors.js
+++ devtools/client/performance-new/store/selectors.js
@@ -3,17 +3,54 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
+/**
+ * @typedef {import("../types").RecordingState} RecordingState
+ * @typedef {import("../types").RecordingStateFromPreferences} RecordingStateFromPreferences
+ * @typedef {import("../types").InitializedValues} InitializedValues
+ * @typedef {import("../types").PerfFront} PerfFront
+ * @typedef {import("../types").ReceiveProfile} ReceiveProfile
+ * @typedef {import("../types").SetRecordingPreferences} SetRecordingPreferences
+ * @typedef {import("../types").GetSymbolTableCallback} GetSymbolTableCallback
+ */
+/**
+ * @template S
+ * @typedef {import("../types").Selector<S>} Selector<S>
+ */
+
+/** @type {Selector<RecordingState>} */
 const getRecordingState = state => state.recordingState;
+
+/** @type {Selector<boolean>} */
 const getRecordingUnexpectedlyStopped = state =>
   state.recordingUnexpectedlyStopped;
+
+/** @type {Selector<boolean>} */
 const getIsSupportedPlatform = state => state.isSupportedPlatform;
+
+/** @type {Selector<number>} */
 const getInterval = state => state.interval;
+
+/** @type {Selector<number>} */
 const getEntries = state => state.entries;
+
+/** @type {Selector<string[]>} */
 const getFeatures = state => state.features;
+
+/** @type {Selector<string[]>} */
 const getThreads = state => state.threads;
+
+/** @type {Selector<string>} */
 const getThreadsString = state => getThreads(state).join(",");
+
+/** @type {Selector<string[]>} */
 const getObjdirs = state => state.objdirs;
 
+/**
+ * Warning! This function returns a new object on every run, and so should not
+ * be used directly as a React prop.
+ *
+ * @type {Selector<RecordingStateFromPreferences>}
+ */
 const getRecordingSettings = state => {
   return {
     entries: getEntries(state),
@@ -24,6 +61,7 @@ const getRecordingSettings = state => {
   };
 };
 
+/** @type {Selector<InitializedValues>} */
 const getInitializedValues = state => {
   const values = state.initializedValues;
   if (!values) {
@@ -32,11 +70,20 @@ const getInitializedValues = state => {
   return values;
 };
 
+/** @type {Selector<PerfFront>} */
 const getPerfFront = state => getInitializedValues(state).perfFront;
+
+/** @type {Selector<ReceiveProfile>} */
 const getReceiveProfileFn = state => getInitializedValues(state).receiveProfile;
+
+/** @type {Selector<SetRecordingPreferences>} */
 const getSetRecordingPreferencesFn = state =>
   getInitializedValues(state).setRecordingPreferences;
+
+/** @type {Selector<boolean>} */
 const getIsPopup = state => getInitializedValues(state).isPopup;
+
+/** @type {Selector<(profile: Object) => GetSymbolTableCallback>} */
 const getSymbolTableGetter = state =>
   getInitializedValues(state).getSymbolTableGetter;
 
