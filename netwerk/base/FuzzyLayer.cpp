31859940670e0fda4c4a1a45a67b907072906e1b
Christian Holler <choller@mozilla.com>
2019-10-07 10:18:37 +0000

    Bug 1584537 - Support multiple connections in the Necko fuzzing layer. r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D47448

--- netwerk/base/FuzzyLayer.cpp
+++ netwerk/base/FuzzyLayer.cpp
@@ -5,6 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "FuzzyLayer.h"
+#include "nsDataHashtable.h"
+#include "nsDeque.h"
 #include "nsIRunnable.h"
 #include "nsSocketTransportService2.h"
 #include "nsThreadUtils.h"
@@ -12,6 +14,7 @@
 #include "prmem.h"
 #include "prio.h"
 #include "mozilla/Logging.h"
+#include "mozilla/Mutex.h"
 
 namespace mozilla {
 namespace net {
@@ -21,36 +24,89 @@ LazyLogModule gFuzzingLog("nsFuzzingNecko");
 #define FUZZING_LOG(args) \
   MOZ_LOG(mozilla::net::gFuzzingLog, mozilla::LogLevel::Verbose, args)
 
-// These will be set by setNetworkFuzzingBuffer below
-static Atomic<const uint8_t*> gFuzzingBuf(NULL);
-static Atomic<size_t> gFuzzingSize(0);
+// Mutex for modifying our hash tables
+Mutex gConnRecvMutex("ConnectRecvMutex");
+
+// This structure will be created by addNetworkFuzzingBuffer below
+// and then added to the gNetworkFuzzingBuffers structure.
+//
+// It is assigned on connect and associated with the socket it belongs to.
+typedef struct {
+  const uint8_t* buf;
+  size_t size;
+  bool allowRead;
+  bool allowUnused;
+} NetworkFuzzingBuffer;
+
+// This holds all connections we have currently open.
+static nsDataHashtable<nsPtrHashKey<PRFileDesc>, NetworkFuzzingBuffer*>
+    gConnectedNetworkFuzzingBuffers;
+
+// This holds all buffers for connections we can still open.
+static nsDeque gNetworkFuzzingBuffers;
+
+// This is `true` once all connections are closed and either there are
+// no buffers left to be used or all remaining buffers are marked optional.
+// Used by `signalNetworkFuzzingDone` to tell the main thread if it needs
+// to spin-wait for `gFuzzingConnClosed`.
+static Atomic<bool> fuzzingNoWaitRequired(false);
+
+// Used to memorize if the main thread has indicated that it is done with
+// its iteration and we don't expect more connections now.
+static Atomic<bool> fuzzingMainSignaledDone(false);
 
 /*
- * These globals control the behavior of our virtual socket:
- *
- *   If `gFuzzingAllowRead` is false, any reading on the socket will be denied.
- *   This is to ensure that a write has to happen first on a new connection.
- *
- *   If `gFuzzingAllowNewConn` is false, any calls to `FuzzyConnect` will fail.
- *   We use this to control that only a single connection is opened per buffer.
- *
- *   The flag `gFuzzingConnClosed` is set by `FuzzyClose` when the connection
- *   is closed. The main thread spins until this becomes true to synchronize
- *   the fuzzing iteration between the main thread and the socket thread.
- *
+ * The flag `gFuzzingConnClosed` is set by `FuzzyClose` when all connections
+ * are closed *and* there are no more buffers in `gNetworkFuzzingBuffers` that
+ * must be used. The main thread spins until this becomes true to synchronize
+ * the fuzzing iteration between the main thread and the socket thread, if
+ * the prior call to `signalNetworkFuzzingDone` returned `false`.
  */
 Atomic<bool> gFuzzingConnClosed(true);
-Atomic<bool> gFuzzingAllowNewConn(false);
-Atomic<bool> gFuzzingAllowRead(false);
 
-void setNetworkFuzzingBuffer(const uint8_t* data, size_t size) {
+void addNetworkFuzzingBuffer(const uint8_t* data, size_t size, bool readFirst,
+                             bool useIsOptional) {
   if (size > INT32_MAX) {
     MOZ_CRASH("Unsupported buffer size");
   }
-  gFuzzingBuf = data;
-  gFuzzingSize = size;
-  gFuzzingAllowNewConn = true;
-  gFuzzingAllowRead = false;
+
+  NetworkFuzzingBuffer* buf = new NetworkFuzzingBuffer();
+  buf->buf = data;
+  buf->size = size;
+  buf->allowRead = readFirst;
+  buf->allowUnused = useIsOptional;
+
+  gNetworkFuzzingBuffers.Push(buf);
+
+  fuzzingMainSignaledDone = false;
+  fuzzingNoWaitRequired = false;
+}
+
+/*
+ * This method should be called by fuzzing from the main thread to signal to
+ * the layer code that a fuzzing iteration is done. As a result, we can throw
+ * away any optional buffers and signal back once all connections have been
+ * closed. The main thread should synchronize on all connections being closed
+ * after the actual request/action is complete.
+ */
+bool signalNetworkFuzzingDone() {
+  FUZZING_LOG(("[signalNetworkFuzzingDone] Called."));
+  MutexAutoLock lock(gConnRecvMutex);
+  bool rv = false;
+
+  if (fuzzingNoWaitRequired) {
+    FUZZING_LOG(("[signalNetworkFuzzingDone] Purging remaining buffers."));
+    // Easy case, we already have no connections and non-optional buffers left.
+    gNetworkFuzzingBuffers.Erase();
+    gFuzzingConnClosed = true;
+    rv = true;
+  } else {
+    // We still have either connections left open or non-optional buffers left.
+    // In this case, FuzzyClose will handle the tear-down and signaling.
+    fuzzingMainSignaledDone = true;
+  }
+
+  return rv;
 }
 
 static PRDescIdentity sFuzzyLayerIdentity;
@@ -61,7 +117,11 @@ static PRInt16 PR_CALLBACK FuzzyPoll(PRFileDesc* fd, PRInt16 in_flags,
                                      PRInt16* out_flags) {
   *out_flags = 0;
 
-  if (in_flags & PR_POLL_READ && gFuzzingAllowRead) {
+  FUZZING_LOG(("[FuzzyPoll] Called with in_flags=%X.", in_flags));
+
+  NetworkFuzzingBuffer* fuzzBuf = gConnectedNetworkFuzzingBuffers.Get(fd);
+
+  if (in_flags & PR_POLL_READ && fuzzBuf && fuzzBuf->allowRead) {
     *out_flags = PR_POLL_READ;
     return PR_POLL_READ;
   }
@@ -79,14 +139,20 @@ static PRStatus FuzzyConnect(PRFileDesc* fd, const PRNetAddr* addr,
   MOZ_RELEASE_ASSERT(fd->identity == sFuzzyLayerIdentity);
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
 
-  if (!gFuzzingAllowNewConn) {
+  MutexAutoLock lock(gConnRecvMutex);
+
+  NetworkFuzzingBuffer* buf =
+      (NetworkFuzzingBuffer*)gNetworkFuzzingBuffers.PopFront();
+  if (!buf) {
     FUZZING_LOG(("[FuzzyConnect] Denying additional connection."));
     return PR_FAILURE;
   }
 
-  FUZZING_LOG(("[FuzzyConnect] Successfully opened connection."));
+  gConnectedNetworkFuzzingBuffers.Put(fd, buf);
+  fuzzingNoWaitRequired = false;
 
-  gFuzzingAllowNewConn = false;
+  FUZZING_LOG(("[FuzzyConnect] Successfully opened connection: %p", fd));
+
   gFuzzingConnClosed = false;
 
   return PR_SUCCESS;
@@ -97,11 +163,21 @@ static PRInt32 FuzzySend(PRFileDesc* fd, const void* buf, PRInt32 amount,
   MOZ_RELEASE_ASSERT(fd->identity == sFuzzyLayerIdentity);
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
 
-  // Allow reading once the implementation has written at least some data
-  if (!gFuzzingAllowRead) {
-    FUZZING_LOG(("[FuzzySend] Write received, allowing further reads."));
-    gFuzzingAllowRead = true;
+  MutexAutoLock lock(gConnRecvMutex);
+
+  NetworkFuzzingBuffer* fuzzBuf = gConnectedNetworkFuzzingBuffers.Get(fd);
+  if (!fuzzBuf) {
+    FUZZING_LOG(("[FuzzySend] Write on socket that is not connected."));
+    amount = 0;
   }
+
+  // Allow reading once the implementation has written at least some data
+  if (fuzzBuf && !fuzzBuf->allowRead) {
+    FUZZING_LOG(("[FuzzySend] Write received, allowing further reads."));
+    fuzzBuf->allowRead = true;
+  }
+
+  FUZZING_LOG(("[FuzzySend] Sent %" PRIx32 " bytes of data.", amount));
 
   return amount;
 }
@@ -114,8 +190,16 @@ static PRInt32 FuzzyRecv(PRFileDesc* fd, void* buf, PRInt32 amount,
                          PRIntn flags, PRIntervalTime timeout) {
   MOZ_RELEASE_ASSERT(fd->identity == sFuzzyLayerIdentity);
 
+  MutexAutoLock lock(gConnRecvMutex);
+
+  NetworkFuzzingBuffer* fuzzBuf = gConnectedNetworkFuzzingBuffers.Get(fd);
+  if (!fuzzBuf) {
+    FUZZING_LOG(("[FuzzyRecv] Denying read, connection is closed."));
+    return 0;
+  }
+
   // As long as we haven't written anything, act as if no data was there yet
-  if (!gFuzzingAllowRead) {
+  if (!fuzzBuf->allowRead) {
     FUZZING_LOG(("[FuzzyRecv] Denying read, nothing written before."));
     PR_SetError(PR_WOULD_BLOCK_ERROR, 0);
     return -1;
@@ -127,18 +211,23 @@ static PRInt32 FuzzyRecv(PRFileDesc* fd, void* buf, PRInt32 amount,
   }
 
   // No data left, act as if the connection was closed.
-  if (!gFuzzingSize) return 0;
+  if (!fuzzBuf->size) {
+    FUZZING_LOG(("[FuzzyRecv] Read failed, no data left."));
+    return 0;
+  }
 
   // Use the remains of fuzzing buffer, if too little is left
-  if (gFuzzingSize < (PRUint32)amount) amount = gFuzzingSize;
+  if (fuzzBuf->size < (PRUint32)amount) amount = fuzzBuf->size;
 
   // Consume buffer, copy data
-  memcpy(buf, gFuzzingBuf, amount);
+  memcpy(buf, fuzzBuf->buf, amount);
 
   if (!(flags & PR_MSG_PEEK)) {
-    gFuzzingBuf += amount;
-    gFuzzingSize -= amount;
+    fuzzBuf->buf += amount;
+    fuzzBuf->size -= amount;
   }
+
+  FUZZING_LOG(("[FuzzyRecv] Read %" PRIx32 " bytes of data.", amount));
 
   return amount;
 }
@@ -157,16 +246,59 @@ static PRStatus FuzzyClose(PRFileDesc* fd) {
 
   layer->dtor(layer);
 
+  MutexAutoLock lock(gConnRecvMutex);
+
+  NetworkFuzzingBuffer* fuzzBuf = nullptr;
+  if (gConnectedNetworkFuzzingBuffers.Remove(fd, &fuzzBuf)) {
+    FUZZING_LOG(("[FuzzyClose] Received close on socket %p", fd));
+    delete fuzzBuf;
+  } else {
+    /* Happens when close is called on a non-connected socket */
+    FUZZING_LOG(("[FuzzyClose] Received close on unknown socket %p.", fd));
+  }
+
   PRStatus ret = fd->methods->close(fd);
 
-  // Mark connection as closed, but still don't allow new connections
-  gFuzzingConnClosed = true;
+  if (!gConnectedNetworkFuzzingBuffers.Count()) {
+    // At this point, all connections are closed, but we might still have
+    // unused network buffers that were not marked as optional.
+    bool haveRemainingUnusedBuffers = false;
+    for (size_t i = 0; i < gNetworkFuzzingBuffers.GetSize(); ++i) {
+      NetworkFuzzingBuffer* buf = static_cast<NetworkFuzzingBuffer*>(
+          gNetworkFuzzingBuffers.ObjectAt(i));
 
-  FUZZING_LOG(("[FuzzyClose] Connection closed."));
+      if (!buf->allowUnused) {
+        haveRemainingUnusedBuffers = true;
+        break;
+      }
+    }
 
-  // We need to dispatch this so the main thread is guaranteed to wake up
-  nsCOMPtr<nsIRunnable> r(new mozilla::Runnable("Dummy"));
-  NS_DispatchToMainThread(r.forget());
+    if (haveRemainingUnusedBuffers) {
+      FUZZING_LOG(
+          ("[FuzzyClose] All connections closed, waiting for remaining "
+           "connections."));
+    } else if (!fuzzingMainSignaledDone) {
+      // We have no connections left, but the main thread hasn't signaled us
+      // yet. For now, that means once the main thread signals us, we can tell
+      // it immediately that it won't have to wait for closing connections.
+      FUZZING_LOG(
+          ("[FuzzyClose] All connections closed, waiting for main thread."));
+      fuzzingNoWaitRequired = true;
+    } else {
+      // No connections left and main thread is already done. Perform cleanup
+      // and then signal the main thread to continue.
+      FUZZING_LOG(("[FuzzyClose] All connections closed, cleaning up."));
+
+      gNetworkFuzzingBuffers.Erase();
+      gFuzzingConnClosed = true;
+
+      // We need to dispatch this so the main thread is guaranteed to wake up
+      nsCOMPtr<nsIRunnable> r(new mozilla::Runnable("Dummy"));
+      NS_DispatchToMainThread(r.forget());
+    }
+  } else {
+    FUZZING_LOG(("[FuzzyClose] Connection closed."));
+  }
 
   return ret;
 }
