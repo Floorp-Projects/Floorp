fa8f10db01afb497423c1b6e44a7d10adff772dc
André Bargull <andre.bargull@gmail.com>
2019-10-07 13:43:24 +0000

    Bug 1429776: Use recommended ICU build options flags. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D41043

--- intl/icu_sources_data.py
+++ intl/icu_sources_data.py
@@ -213,6 +213,7 @@ def update_data_file(topsrcdir):
     # bug 1262101 - these should be shared with the moz.build files
     env.update({
         'CPPFLAGS': ('-DU_NO_DEFAULT_INCLUDE_UTF_HEADERS=1 ' +
+                     '-DU_HIDE_OBSOLETE_UTF_OLD_H=1' +
                      '-DUCONFIG_NO_LEGACY_CONVERSION ' +
                      '-DUCONFIG_NO_TRANSLITERATION ' +
                      '-DUCONFIG_NO_REGULAR_EXPRESSIONS ' +
