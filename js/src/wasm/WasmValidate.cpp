238bafe0be94be87d0dd454414de309aa891163c
Gurzau Raul <rgurzau@mozilla.com>
2019-10-07 10:48:35 +0300

    Backed out changeset b868b0719ca6 (bug 1577508) as requested by lth. a=backout

--- js/src/wasm/WasmValidate.cpp
+++ js/src/wasm/WasmValidate.cpp
@@ -444,24 +444,15 @@ bool wasm::DecodeValidatedLocalEntries(Decoder& d, ValTypeVector* locals) {
 
 // Function body validation.
 
-class NothingVector {
-  Nothing unused_;
-
- public:
-  bool resize(size_t length) { return true; }
-  Nothing& operator[](size_t) { return unused_; }
-  Nothing& back() { return unused_; }
-};
-
 struct ValidatingPolicy {
   typedef Nothing Value;
-  typedef NothingVector ValueVector;
   typedef Nothing ControlItem;
 };
 
 typedef OpIter<ValidatingPolicy> ValidatingOpIter;
 
 static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
+                                    // FIXME(1401675): Replace with BlockType.
                                     uint32_t funcIndex,
                                     const ValTypeVector& locals,
                                     const uint8_t* bodyEnd, Decoder* d) {
@@ -482,13 +473,12 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
     }
 
     Nothing nothing;
-    NothingVector nothings;
-    ResultType unusedType;
 
     switch (op.b0) {
       case uint16_t(Op::End): {
         LabelKind unusedKind;
-        if (!iter.readEnd(&unusedKind, &unusedType, &nothings)) {
+        ExprType unusedType;
+        if (!iter.readEnd(&unusedKind, &unusedType, &nothing)) {
           return false;
         }
         iter.popEnd();
@@ -503,12 +493,12 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
         CHECK(iter.readDrop());
       case uint16_t(Op::Call): {
         uint32_t unusedIndex;
-        NothingVector unusedArgs;
+        ValidatingOpIter::ValueVector unusedArgs;
         CHECK(iter.readCall(&unusedIndex, &unusedArgs));
       }
       case uint16_t(Op::CallIndirect): {
         uint32_t unusedIndex, unusedIndex2;
-        NothingVector unusedArgs;
+        ValidatingOpIter::ValueVector unusedArgs;
         CHECK(iter.readCallIndirect(&unusedIndex, &unusedIndex2, &nothing,
                                     &unusedArgs));
       }
@@ -583,8 +573,10 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
         CHECK(iter.readLoop());
       case uint16_t(Op::If):
         CHECK(iter.readIf(&nothing));
-      case uint16_t(Op::Else):
-        CHECK(iter.readElse(&unusedType, &nothings));
+      case uint16_t(Op::Else): {
+        ExprType type;
+        CHECK(iter.readElse(&type, &nothing));
+      }
       case uint16_t(Op::I32Clz):
       case uint16_t(Op::I32Ctz):
       case uint16_t(Op::I32Popcnt):
@@ -823,20 +815,23 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
         CHECK(iter.readMemorySize());
       case uint16_t(Op::Br): {
         uint32_t unusedDepth;
-        CHECK(iter.readBr(&unusedDepth, &unusedType, &nothings));
+        ExprType unusedType;
+        CHECK(iter.readBr(&unusedDepth, &unusedType, &nothing));
       }
       case uint16_t(Op::BrIf): {
         uint32_t unusedDepth;
-        CHECK(iter.readBrIf(&unusedDepth, &unusedType, &nothings, &nothing));
+        ExprType unusedType;
+        CHECK(iter.readBrIf(&unusedDepth, &unusedType, &nothing, &nothing));
       }
       case uint16_t(Op::BrTable): {
         Uint32Vector unusedDepths;
         uint32_t unusedDefault;
+        ExprType unusedType;
         CHECK(iter.readBrTable(&unusedDepths, &unusedDefault, &unusedType,
-                               &nothings, &nothing));
+                               &nothing, &nothing));
       }
       case uint16_t(Op::Return):
-        CHECK(iter.readReturn(&nothings));
+        CHECK(iter.readReturn(&nothing));
       case uint16_t(Op::Unreachable):
         CHECK(iter.readUnreachable());
       case uint16_t(Op::MiscPrefix): {
@@ -963,7 +958,7 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
               return iter.unrecognizedOpcode(&op);
             }
             uint32_t unusedUint;
-            NothingVector unusedArgs;
+            ValidatingOpIter::ValueVector unusedArgs;
             CHECK(iter.readStructNew(&unusedUint, &unusedArgs));
           }
           case uint32_t(MiscOp::StructGet): {
@@ -1215,8 +1210,10 @@ static bool DecodeFunctionBodyExprs(const ModuleEnvironment& env,
 bool wasm::ValidateFunctionBody(const ModuleEnvironment& env,
                                 uint32_t funcIndex, uint32_t bodySize,
                                 Decoder& d) {
+  const FuncType& funcType = *env.funcTypes[funcIndex];
+
   ValTypeVector locals;
-  if (!locals.appendAll(env.funcTypes[funcIndex]->args())) {
+  if (!locals.appendAll(funcType.args())) {
     return false;
   }
 
