df1b172c11db24b5f19a731f2cd656336635ae53
Bob Owen <bobowencode@gmail.com>
2019-10-07 14:38:06 +0000

    Bug 1580511: Check size is allowed in DrawTargetRecording::CreateSourceSurfaceFromData. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D48317

--- gfx/2d/DrawTargetRecording.cpp
+++ gfx/2d/DrawTargetRecording.cpp
@@ -106,6 +106,10 @@ class DataSourceSurfaceRecording : public DataSourceSurface {
   static already_AddRefed<DataSourceSurface> Init(
       uint8_t* aData, IntSize aSize, int32_t aStride, SurfaceFormat aFormat,
       DrawEventRecorderPrivate* aRecorder) {
+    if (!Factory::AllowedSurfaceSize(aSize)) {
+      return nullptr;
+    }
+
     // XXX: do we need to ensure any alignment here?
     auto data = MakeUnique<uint8_t[]>(aStride * aSize.height);
     if (data) {
