7edc4fbc79eb50523e88ddb663f772226cb02d62
Gavin Lazar Suntop <gavin@gsuntop.com>
2019-10-07 21:46:29 +0300

    Bug 1584642 - Control engagment labels with a pref r=Mardak a=lizzard
    Control visibility of engagement labels with a pref
    
    Differential Revision: https://phabricator.services.mozilla.com/D48408

--- browser/components/newtab/content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
+++ browser/components/newtab/content-src/components/DiscoveryStreamBase/DiscoveryStreamBase.jsx
@@ -111,6 +111,10 @@ export class _DiscoveryStreamBase extends React.PureComponent {
   }
 
   renderComponent(component, embedWidth) {
+    const ENGAGEMENT_LABEL_ENABLED = this.props.Prefs.values[
+      `discoverystream.engagementLabelEnabled`
+    ];
+
     switch (component.type) {
       case "Highlights":
         return <Highlights />;
@@ -212,6 +216,7 @@ export class _DiscoveryStreamBase extends React.PureComponent {
             dispatch={this.props.dispatch}
             items={component.properties.items}
             cta_variant={component.cta_variant}
+            display_engagement_labels={ENGAGEMENT_LABEL_ENABLED}
           />
         );
       case "Hero":
