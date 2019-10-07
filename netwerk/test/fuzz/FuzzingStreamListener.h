74ce3baa8c16bc48f6136d04256c22e91a368a6b
Christian Holler <choller@mozilla.com>
2019-10-07 10:18:50 +0000

    Bug 1562283 - FTP fuzzing target for the Necko fuzzing layer. r=michal
    Depends on D47448
    
    Differential Revision: https://phabricator.services.mozilla.com/D47449

---
+++ netwerk/test/fuzz/FuzzingStreamListener.h
@@ -0,0 +1,38 @@
+#ifndef FuzzingStreamListener_h__
+#define FuzzingStreamListener_h__
+
+#include "nsCOMPtr.h"
+#include "nsNetCID.h"
+#include "nsString.h"
+#include "nsNetUtil.h"
+#include "nsIStreamListener.h"
+#include "nsThreadUtils.h"
+
+namespace mozilla {
+namespace net {
+
+class FuzzingStreamListener final : public nsIStreamListener {
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
+
+  FuzzingStreamListener() = default;
+
+  void waitUntilDone() {
+    SpinEventLoopUntil([&]() { return mChannelDone; });
+  }
+
+  bool isDone() {
+    return mChannelDone;
+  }
+
+ private:
+  ~FuzzingStreamListener() = default;
+  bool mChannelDone = false;
+};
+
+}  // namespace net
+}  // namespace mozilla
+
+#endif
