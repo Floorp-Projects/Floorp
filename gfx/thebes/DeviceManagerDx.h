3546b3c6226f8f2fce7658119217a976ffdd0f8f
sotaro <sotaro.ikeda.g@gmail.com>
2019-10-07 20:09:17 +0000

    Bug 1585278 - Add option of enabling performance debugging counters of DirectComposition r=nical,jrmuizel
    IDCompositionDevice is replaced by IDCompositionDevice2. It is necessary for IDCompositionDeviceDebug usage. And for using IDCompositionDevice2, _WIN32_WINNT and NTDDI_VERSION is updated from Windows 8 to Windows 8.1.
    
    Workaround MinGW build failure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D47742
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/thebes/DeviceManagerDx.h
+++ gfx/thebes/DeviceManagerDx.h
@@ -33,7 +33,7 @@
 #endif
 
 struct ID3D11Device;
-struct IDCompositionDevice;
+struct IDCompositionDevice2;
 struct IDirectDraw7;
 
 namespace mozilla {
@@ -59,7 +59,10 @@ class DeviceManagerDx final {
   RefPtr<ID3D11Device> GetContentDevice();
   RefPtr<ID3D11Device> GetCanvasDevice();
   RefPtr<ID3D11Device> GetImageDevice();
-  RefPtr<IDCompositionDevice> GetDirectCompositionDevice();
+// Currently, MinGW build environment does not handle IDCompositionDevice2
+#if !defined(__MINGW32__)
+  RefPtr<IDCompositionDevice2> GetDirectCompositionDevice();
+#endif
   RefPtr<ID3D11Device> GetVRDevice();
   RefPtr<ID3D11Device> CreateDecoderDevice();
   RefPtr<layers::MLGDevice> GetMLGDevice();
@@ -172,8 +175,9 @@ class DeviceManagerDx final {
   RefPtr<ID3D11Device> mImageDevice;
   RefPtr<ID3D11Device> mVRDevice;
   RefPtr<ID3D11Device> mDecoderDevice;
-  RefPtr<IDCompositionDevice> mDirectCompositionDevice;
-
+#if !defined(__MINGW32__)
+  RefPtr<IDCompositionDevice2> mDirectCompositionDevice;
+#endif
   RefPtr<layers::DeviceAttachmentsD3D11> mCompositorAttachments;
   RefPtr<layers::MLGDevice> mMLGDevice;
   bool mCompositorDeviceSupportsVideo;
