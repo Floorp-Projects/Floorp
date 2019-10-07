1273d512eb6d35b404822d5de31a3c330ab0d7e1
Botond Ballo <botond@mozilla.com>
2019-10-07 20:37:09 +0000

    Bug 1552608 - Define AsyncCompositionManager::mFixedLayerMargins and its getter/setter on all platforms. r=tnikkel
    It's still only used on Android, but keeping it and all code that interacts
    with it inside an #ifdef is too much trouble for a tiny amount of space
    saving.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48369
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/layers/composite/AsyncCompositionManager.h
+++ gfx/layers/composite/AsyncCompositionManager.h
@@ -234,11 +234,14 @@ class AsyncCompositionManager final {
 
   MOZ_NON_OWNING_REF CompositorBridgeParent* mCompositorBridge;
 
-#ifdef MOZ_WIDGET_ANDROID
  public:
   void SetFixedLayerMargins(ScreenIntCoord aTop, ScreenIntCoord aBottom);
   ScreenMargin GetFixedLayerMargins() const;
 
+ private:
+  ScreenMargin mFixedLayerMargins;
+
+#ifdef MOZ_WIDGET_ANDROID
  private:
   // This calculates whether frame metrics should be sent to Java.
   bool FrameMetricsHaveUpdated(const FrameMetrics& aMetrics);
@@ -249,7 +252,6 @@ class AsyncCompositionManager final {
   // then we need to reposition the gecko scrollbar to deal with the
   // dynamic toolbar shifting content around.
   ScrollableLayerGuid::ViewID mRootScrollableId;
-  ScreenMargin mFixedLayerMargins;
 #endif
 };
 
