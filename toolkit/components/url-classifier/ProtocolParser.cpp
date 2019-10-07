cce02fa9dd527ab1a06b6196c0d412af7c59f960
DimiDL <dlee@mozilla.com>
2019-10-07 15:02:39 +0000

    Bug 1576374 - Use fallible append in ProtocolParser::AppendStream. r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D48354

--- toolkit/components/url-classifier/ProtocolParser.cpp
+++ toolkit/components/url-classifier/ProtocolParser.cpp
@@ -124,7 +124,9 @@ nsresult ProtocolParserV2::AppendStream(const nsACString& aData) {
   if (NS_FAILED(mUpdateStatus)) return mUpdateStatus;
 
   nsresult rv;
-  mPending.Append(aData);
+  if (!mPending.Append(aData, mozilla::fallible)) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
 #ifdef MOZ_SAFEBROWSING_DUMP_FAILED_UPDATES
   mRawUpdate.Append(aData);
 #endif
@@ -722,7 +724,9 @@ RefPtr<TableUpdate> ProtocolParserProtobuf::CreateTableUpdate(
 
 nsresult ProtocolParserProtobuf::AppendStream(const nsACString& aData) {
   // Protobuf data cannot be parsed progressively. Just save the incoming data.
-  mPending.Append(aData);
+  if (!mPending.Append(aData, mozilla::fallible)) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
   return NS_OK;
 }
 
