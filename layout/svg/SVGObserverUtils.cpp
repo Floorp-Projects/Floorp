6292c6695fc04e8ebe052d1828966b9c27887e3c
Emilio Cobos Álvarez <emilio@crisal.io>
2019-10-08 21:52:47 +0000

    Bug 1585863 - Remove nsChangeHint_UpdateTextPath. r=longsonr
    The only thing it does is asserting a bit and posting more async work to the
    text frame. It seems we can just post all the async work early instead, and
    remove the change hint.
    
    This was only introduced to fix bug 779971, where a <textPath> element
    references its parent SVG, which is obviously unsound if we allowed to render
    it.
    
    What we're doing right now is a bit silly... We're observing the <svg>, so when
    we finish reflowing it and store its overflow, we invalidate its rendering
    observers, but that invalidates a _descendant_, which makes no sense.
    
    Fortunately we don't let the element affect its rendering, as it fails this
    check:
    
     * https://searchfox.org/mozilla-central/rev/35cc00a25c4471993fdaa5761952bd3afd4f1731/layout/svg/SVGObserverUtils.cpp#1390
    
    But we still request reflow of the outer <text>, which is not amazing. We
    shouldn't invalidate anything if the textpath doesn't reference a valid element
    and that didn't change. This is roughly what the code tried to do when checking
    mValid, except we always initialize mValid to true and thus always trigger at
    least one bogus reflow call.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48008

--- layout/svg/SVGObserverUtils.cpp
+++ layout/svg/SVGObserverUtils.cpp
@@ -28,6 +28,7 @@
 #include "nsTHashtable.h"
 #include "nsURIHashKey.h"
 #include "SVGGeometryElement.h"
+#include "SVGTextFrame.h"
 #include "SVGTextPathElement.h"
 #include "SVGUseElement.h"
 
@@ -386,7 +387,7 @@ class SVGTextPathObserver final : public nsSVGRenderingObserverProperty {
   SVGTextPathObserver(URLAndReferrerInfo* aURI, nsIFrame* aFrame,
                       bool aReferenceImage)
       : nsSVGRenderingObserverProperty(aURI, aFrame, aReferenceImage),
-        mValid(true) {}
+        mValid(TargetIsValid()) {}
 
  protected:
   void OnRenderingChange() override;
@@ -433,11 +434,21 @@ void SVGTextPathObserver::OnRenderingChange() {
   }
   mValid = nowValid;
 
-  // Repaint asynchronously in case the path frame is being torn down
-  nsChangeHint changeHint =
-      nsChangeHint(nsChangeHint_RepaintFrame | nsChangeHint_UpdateTextPath);
+  MOZ_ASSERT(frame->GetContent()->IsSVGElement(nsGkAtoms::textPath),
+             "expected frame for a <textPath> element");
+
+  // Repaint asynchronously text in case the path frame is being torn down
   frame->PresContext()->RestyleManager()->PostRestyleEvent(
-      frame->GetContent()->AsElement(), RestyleHint{0}, changeHint);
+      frame->GetContent()->AsElement(), RestyleHint{0},
+      nsChangeHint_RepaintFrame);
+
+  nsIFrame* text =
+      nsLayoutUtils::GetClosestFrameOfType(frame, LayoutFrameType::SVGText);
+  MOZ_ASSERT(text, "expected to find an ancestor SVGTextFrame");
+  if (text) {
+    // This also does async work.
+    static_cast<SVGTextFrame*>(text)->NotifyGlyphMetricsChange();
+  }
 }
 
 class SVGMarkerObserver final : public nsSVGRenderingObserverProperty {
