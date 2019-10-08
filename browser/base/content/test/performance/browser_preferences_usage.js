695191a7dd27edb57439f1eadbba38a62e725e86
Emma Malysz <emalysz@mozilla.com>
2019-10-08 14:54:53 +0000

    Bug 1585727, ensure browser_preferences_usage.js is fission compatible r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D47945
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -132,6 +132,11 @@ add_task(async function startup() {
     },
   };
 
+  if (SpecialPowers.useRemoteSubframes) {
+    // Bug 1585732 - Number of accesses with Fission enabled is higher than without.
+    max = 50;
+  }
+
   let startupRecorder = Cc["@mozilla.org/test/startuprecorder;1"].getService()
     .wrappedJSObject;
   await startupRecorder.done;
@@ -209,6 +214,29 @@ add_task(async function navigate_around() {
       max: 55,
     },
   };
+
+  if (SpecialPowers.useRemoteSubframes) {
+    // Bug 1585732 - Number of accesses with Fission enabled is higher than without.
+    max = 50;
+
+    whitelist = Object.assign(
+      {
+        "fission.rebuild_frameloaders_on_remoteness_change": {
+          min: 1,
+          max: 100,
+        },
+        "media.cubeb.sandbox": {
+          min: 1,
+          max: 200,
+        },
+        "security.sandbox.content.level": {
+          min: 1,
+          max: 200,
+        },
+      },
+      whitelist
+    );
+  }
 
   Services.prefs.resetStats();
 
