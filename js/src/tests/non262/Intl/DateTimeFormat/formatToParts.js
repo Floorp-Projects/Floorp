b2cd75addee76c840dc4c67ca1bbb19b12b9a327
André Bargull <andre.bargull@gmail.com>
2019-10-08 07:34:47 +0000

    Bug 1573439 - Part 2: Update DateTimeFormat formatToParts tests to use common test helpers. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D41741
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/tests/non262/Intl/DateTimeFormat/formatToParts.js
+++ js/src/tests/non262/Intl/DateTimeFormat/formatToParts.js
@@ -5,57 +5,18 @@
 // Tests the format function with a diverse set of locales and options.
 // Always use UTC to avoid dependencies on test environment.
 
-/*
- * Return true if A is equal to B, where equality on arrays and objects
- * means that they have the same set of enumerable properties, the values
- * of each property are deep_equal, and their 'length' properties are
- * equal. Equality on other types is ==.
- */
-function deepEqual(a, b) {
-    if (typeof a !== typeof b)
-        return false;
-
-    if (a === null)
-        return b === null;
-
-    if (typeof a === 'object') {
-        // For every property of a, does b have that property with an equal value?
-        var props = {};
-        for (var prop in a) {
-            if (!deepEqual(a[prop], b[prop]))
-                return false;
-            props[prop] = true;
-        }
-
-        // Are all of b's properties present on a?
-        for (var prop in b)
-            if (!props[prop])
-                return false;
-
-        // length isn't enumerable, but we want to check it, too.
-        return a.length === b.length;
-    }
-
-    return Object.is(a, b);
-}
-
-function composeDate(parts) {
-  return parts.map(({value}) => value)
-              .reduce((string, part) => string + part);
-}
+const {
+  Era, Year, Month, Weekday, Day, DayPeriod, Hour, Minute, Second, Literal
+} = DateTimeFormatParts
 
 var format;
 var date = Date.UTC(2012, 11, 17, 3, 0, 42);
 
 // Locale en-US; default options.
 format = new Intl.DateTimeFormat("en-us", {timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'month', value: '12' },
-  { type: 'literal', value: '/' },
-  { type: 'day', value: '17' },
-  { type: 'literal', value: '/' },
-  { type: 'year', value: '2012' }
-]), true);
+assertParts(format, date, [
+  Month("12"), Literal("/"), Day("17"), Literal("/"), Year("2012"),
+]);
 
 // Just date
 format = new Intl.DateTimeFormat("en-us", {
@@ -63,14 +24,9 @@ format = new Intl.DateTimeFormat("en-us", {
   month: 'numeric',
   day: 'numeric',
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'month', value: '12' },
-  { type: 'literal', value: '/' },
-  { type: 'day', value: '17' },
-  { type: 'literal', value: '/' },
-  { type: 'year', value: '2012' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Month("12"), Literal("/"), Day("17"), Literal("/"), Year("2012"),
+]);
 
 // Just time in hour24
 format = new Intl.DateTimeFormat("en-us", {
@@ -79,14 +35,9 @@ format = new Intl.DateTimeFormat("en-us", {
   second: 'numeric',
   hour12: false,
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'hour', value: '03' },
-  { type: 'literal', value: ':' },
-  { type: 'minute', value: '00' },
-  { type: 'literal', value: ':' },
-  { type: 'second', value: '42' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Hour("03"), Literal(":"), Minute("00"), Literal(":"), Second("42"),
+]);
 
 // Just time in hour12
 format = new Intl.DateTimeFormat("en-us", {
@@ -95,46 +46,34 @@ format = new Intl.DateTimeFormat("en-us", {
   second: 'numeric',
   hour12: true,
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'hour', value: '3' },
-  { type: 'literal', value: ':' },
-  { type: 'minute', value: '00' },
-  { type: 'literal', value: ':' },
-  { type: 'second', value: '42' },
-  { type: 'literal', value: ' ' },
-  { type: 'dayPeriod', value: 'AM' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Hour("3"), Literal(":"), Minute("00"), Literal(":"), Second("42"), Literal(" "), DayPeriod("AM"),
+]);
 
 // Just month.
 format = new Intl.DateTimeFormat("en-us", {
   month: "narrow",
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'month', value: 'D' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Month("D"),
+]);
 
 // Just weekday.
 format = new Intl.DateTimeFormat("en-us", {
   weekday: "narrow",
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'weekday', value: 'M' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Weekday("M"),
+]);
 
 // Year and era.
 format = new Intl.DateTimeFormat("en-us", {
   year: "numeric",
   era: "short",
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'year', value: '2012' },
-  { type: 'literal', value: ' ' },
-  { type: 'era', value: 'AD' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Year("2012"), Literal(" "), Era("AD"),
+]);
 
 // Time and date
 format = new Intl.DateTimeFormat("en-us", {
@@ -147,24 +86,11 @@ format = new Intl.DateTimeFormat("en-us", {
   second: 'numeric',
   hour12: true,
   timeZone: "UTC"});
-assertEq(deepEqual(format.formatToParts(date), [
-  { type: 'weekday', value: 'Monday' },
-  { type: 'literal', value: ', ' },
-  { type: 'month', value: '12' },
-  { type: 'literal', value: '/' },
-  { type: 'day', value: '17' },
-  { type: 'literal', value: '/' },
-  { type: 'year', value: '2012' },
-  { type: 'literal', value: ', ' },
-  { type: 'hour', value: '3' },
-  { type: 'literal', value: ':' },
-  { type: 'minute', value: '00' },
-  { type: 'literal', value: ':' },
-  { type: 'second', value: '42' },
-  { type: 'literal', value: ' ' },
-  { type: 'dayPeriod', value: 'AM' }
-]), true);
-assertEq(composeDate(format.formatToParts(date)), format.format(date));
+assertParts(format, date, [
+  Weekday("Monday"), Literal(", "), Month("12"), Literal("/"), Day("17"), Literal("/"), Year("2012"),
+  Literal(", "),
+  Hour("3"), Literal(":"), Minute("00"), Literal(":"), Second("42"), Literal(" "), DayPeriod("AM"),
+]);
 
 if (typeof reportCompare === "function")
     reportCompare(0, 0, 'ok');
