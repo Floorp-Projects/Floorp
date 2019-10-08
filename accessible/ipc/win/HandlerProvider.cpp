25f6b663b5c25bf58b1c08ac86fb16d25448adfc
Aaron Klotz <aklotz@mozilla.com>
2019-10-08 20:43:38 +0000

    Bug 1586436: Ensure that mscom methods that accept variadic arguments use rvalue references in their arguments; r=Jamie
    Differential Revision: https://phabricator.services.mozilla.com/D48435

--- accessible/ipc/win/HandlerProvider.cpp
+++ accessible/ipc/win/HandlerProvider.cpp
@@ -105,8 +105,9 @@ void HandlerProvider::GetAndSerializePayload(
 
   if (!mscom::InvokeOnMainThread("HandlerProvider::BuildInitialIA2Data", this,
                                  &HandlerProvider::BuildInitialIA2Data,
-                                 aInterceptor, &payload.mStaticData,
-                                 &payload.mDynamicData) ||
+                                 std::forward<NotNull<mscom::IInterceptor*>>(aInterceptor),
+                                 std::forward<StaticIA2Data*>(&payload.mStaticData),
+                                 std::forward<DynamicIA2Data*>(&payload.mDynamicData)) ||
       !payload.mDynamicData.mUniqueId) {
     return;
   }
@@ -503,7 +504,7 @@ HandlerProvider::Refresh(DynamicIA2Data* aOutData) {
 
   if (!mscom::InvokeOnMainThread("HandlerProvider::BuildDynamicIA2Data", this,
                                  &HandlerProvider::BuildDynamicIA2Data,
-                                 aOutData)) {
+                                 std::forward<DynamicIA2Data*>(aOutData))) {
     return E_FAIL;
   }
 
@@ -628,8 +629,13 @@ HandlerProvider::get_AllTextInfo(BSTR* aText,
   HRESULT hr;
   if (!mscom::InvokeOnMainThread(
           "HandlerProvider::GetAllTextInfoMainThread", this,
-          &HandlerProvider::GetAllTextInfoMainThread, aText, aHyperlinks,
-          aNHyperlinks, aAttribRuns, aNAttribRuns, &hr)) {
+          &HandlerProvider::GetAllTextInfoMainThread,
+          std::forward<BSTR*>(aText),
+          std::forward<IAccessibleHyperlink***>(aHyperlinks),
+          std::forward<long*>(aNHyperlinks),
+          std::forward<IA2TextSegment**>(aAttribRuns),
+          std::forward<long*>(aNAttribRuns),
+          std::forward<HRESULT*>(&hr))) {
     return E_FAIL;
   }
 
@@ -697,7 +703,9 @@ HandlerProvider::get_RelationsInfo(IARelationData** aRelations,
   if (!mscom::InvokeOnMainThread("HandlerProvider::GetRelationsInfoMainThread",
                                  this,
                                  &HandlerProvider::GetRelationsInfoMainThread,
-                                 aRelations, aNRelations, &hr)) {
+                                 std::forward<IARelationData**>(aRelations),
+                                 std::forward<long*>(aNRelations),
+                                 std::forward<HRESULT*>(&hr))) {
     return E_FAIL;
   }
 
@@ -838,7 +846,9 @@ HandlerProvider::get_AllChildren(AccChildData** aChildren, ULONG* aNChildren) {
   if (!mscom::InvokeOnMainThread("HandlerProvider::GetAllChildrenMainThread",
                                  this,
                                  &HandlerProvider::GetAllChildrenMainThread,
-                                 aChildren, aNChildren, &hr)) {
+                                 std::forward<AccChildData**>(aChildren),
+                                 std::forward<ULONG*>(aNChildren),
+                                 std::forward<HRESULT*>(&hr))) {
     return E_FAIL;
   }
 
