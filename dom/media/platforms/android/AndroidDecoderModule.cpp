848b82fa19a0115f6ed488d492af59b166bd41a0
Michael Froman <mfroman@mozilla.com>
2019-10-08 22:44:07 +0000

    Bug 1583997 - prefer ffvpx mp3 decoding on android. r=jya
    Differential Revision: https://phabricator.services.mozilla.com/D47674
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/platforms/android/AndroidDecoderModule.cpp
+++ dom/media/platforms/android/AndroidDecoderModule.cpp
@@ -104,6 +104,12 @@ bool AndroidDecoderModule::SupportsMimeType(const nsACString& aMimeType) {
     return false;
   }
 
+  if (aMimeType.EqualsLiteral("audio/mpeg") &&
+      StaticPrefs::media_ffvpx_mp3_enabled()) {
+    // Prefer the ffvpx mp3 software decoder if available.
+    return false;
+  }
+
   return java::HardwareCodecCapabilityUtils::FindDecoderCodecInfoForMimeType(
       TranslateMimeType(aMimeType));
 }
