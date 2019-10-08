142d8f2dcac2d6ca53a27601901b5d269ce0e0da
John Lin <jolin@mozilla.com>
2019-10-08 17:16:01 +0000

    Bug 1581902 - p6: introduce a pref to enable/disable H.264 for WebRTC. r=pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D46368

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
@@ -620,7 +620,7 @@ class CompareCodecPriority {
 class ConfigureCodec {
  public:
   explicit ConfigureCodec(nsCOMPtr<nsIPrefBranch>& branch)
-      : mHardwareH264Supported(false),
+      : mHardwareH264Enabled(false),
         mSoftwareH264Enabled(false),
         mH264Enabled(false),
         mVP9Enabled(true),
@@ -637,9 +637,12 @@ class ConfigureCodec {
         mDtmfEnabled(false) {
     mSoftwareH264Enabled = PeerConnectionCtx::GetInstance()->gmpHasH264();
 
-    mHardwareH264Supported = WebrtcVideoConduit::HasH264Hardware();
+    if (WebrtcVideoConduit::HasH264Hardware()) {
+      branch->GetBoolPref("media.webrtc.hw.h264.enabled",
+                          &mHardwareH264Enabled);
+    }
 
-    mH264Enabled = mHardwareH264Supported || mSoftwareH264Enabled;
+    mH264Enabled = mHardwareH264Enabled || mSoftwareH264Enabled;
 
     branch->GetIntPref("media.navigator.video.h264.level", &mH264Level);
     mH264Level &= 0xFF;
@@ -712,7 +715,7 @@ class ConfigureCodec {
             videoCodec.mEnabled = false;
           }
 
-          if (mHardwareH264Supported) {
+          if (mHardwareH264Enabled) {
             videoCodec.mStronglyPreferred = true;
           }
         } else if (videoCodec.mName == "red") {
@@ -748,7 +751,7 @@ class ConfigureCodec {
   }
 
  private:
-  bool mHardwareH264Supported;
+  bool mHardwareH264Enabled;
   bool mSoftwareH264Enabled;
   bool mH264Enabled;
   bool mVP9Enabled;
