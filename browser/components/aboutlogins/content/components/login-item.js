afdeed7927684f5dc2eda3faebf3a6dbb4c8e317
Michael Kaply <mozilla@kaply.com>
2019-10-09 02:46:16 +0000

    Bug 1586913 - Add policy for DisablePasswordReveal. r=jaws,flod,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D48592
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/aboutlogins/content/components/login-item.js
+++ browser/components/aboutlogins/content/components/login-item.js
@@ -727,6 +727,14 @@ export default class LoginItem extends HTMLElement {
   }
 
   _updatePasswordRevealState() {
+    if (
+      window.AboutLoginsUtils &&
+      !window.AboutLoginsUtils.passwordRevealVisible
+    ) {
+      this._revealCheckbox.hidden = true;
+      return;
+    }
+
     let titleId = this._revealCheckbox.checked
       ? "login-item-password-reveal-checkbox-hide"
       : "login-item-password-reveal-checkbox-show";
