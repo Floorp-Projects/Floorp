41081dd8fe50993d471d1702478e9910fbf3f06d
Daisuke Akatsuka <daisuke@birchill.co.jp>
2019-10-09 00:35:40 +0000

    Bug 1586634: Add a test for the pref which enables the compatibility tool. r=pbro,rcaliman
    Differential Revision: https://phabricator.services.mozilla.com/D48324

---
+++ devtools/client/inspector/compatibility/test/head.js
@@ -0,0 +1,12 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* import-globals-from ../../test/head.js */
+
+// Import the inspector's head.js first (which itself imports shared-head.js).
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/inspector/test/head.js",
+  this
+);
