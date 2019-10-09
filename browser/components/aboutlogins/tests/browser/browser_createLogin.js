690380c06358a130d95d3bdf745a6bfb7a18c0ce
Jared Wein <jwein@mozilla.com>
2019-10-09 01:21:40 +0000

    Bug 1576501 - Ignore expected error messages for duplicate logins. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D48400

--- browser/components/aboutlogins/tests/browser/browser_createLogin.js
+++ browser/components/aboutlogins/tests/browser/browser_createLogin.js
@@ -397,6 +397,7 @@ add_task(async function test_cancel_create_login_with_logins_filtered_out() {
 
 add_task(async function test_create_duplicate_login() {
   let browser = gBrowser.selectedBrowser;
+  EXPECTED_ERROR_MESSAGE = "This login already exists.";
   await ContentTask.spawn(browser, null, async () => {
     let loginList = Cu.waiveXrays(content.document.querySelector("login-list"));
     let createButton = loginList._createLoginButton;
@@ -467,4 +468,5 @@ add_task(async function test_create_duplicate_login() {
       "the duplicated login should be selected in the list"
     );
   });
+  EXPECTED_ERROR_MESSAGE = null;
 });
