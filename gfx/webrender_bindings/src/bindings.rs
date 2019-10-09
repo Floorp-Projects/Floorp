f1b2af1f17cce7b8a084b968e0e99ade77176ee9
Glenn Watson <git@intuitionlibrary.com>
2019-10-09 08:07:36 +0000

    Bug 1587084 - Restore the picture caching enabled preference. r=nical
    The previous patch fixed the bug in the non-picture caching code
    path, so we can re-enable the preference now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48639

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -1203,10 +1203,6 @@ pub extern "C" fn wr_window_new(window_id: WrWindowId,
         ColorF::new(0.0, 0.0, 0.0, 0.0)
     };
 
-    if !enable_picture_caching {
-        info!("WebRender - picture-caching cannot be disabled until bug 1587084 is resolved.");
-    }
-
     let opts = RendererOptions {
         enable_aa: true,
         enable_subpixel_aa: cfg!(not(target_os = "android")),
@@ -1238,7 +1234,7 @@ pub extern "C" fn wr_window_new(window_id: WrWindowId,
         clear_color: Some(color),
         precache_flags,
         namespace_alloc_by_client: true,
-        enable_picture_caching: true,
+        enable_picture_caching,
         allow_pixel_local_storage_support: false,
         start_debug_server,
         ..Default::default()
