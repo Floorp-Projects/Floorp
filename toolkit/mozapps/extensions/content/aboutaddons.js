aa5a36985baca3d3f9a2bb28a302acee7c0135a9
Mark Striemer <mstriemer@mozilla.com>
2019-10-09 18:46:23 +0000

    Bug 1580962 - Show a SUMO link when an add-on can't be removed r=rpl,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D48128
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -715,25 +715,21 @@ class PanelList extends HTMLElement {
           // If the menu is opened with the mouse, the active element might be
           // somewhere else in the document. In that case we should ignore it
           // to avoid walking unrelated DOM nodes.
-          this.walker.currentNode = this.contains(document.activeElement)
+          this.focusWalker.currentNode = this.contains(document.activeElement)
             ? document.activeElement
             : this;
           let nextItem = moveForward
-            ? this.walker.nextNode()
-            : this.walker.previousNode();
+            ? this.focusWalker.nextNode()
+            : this.focusWalker.previousNode();
 
           // If the next item wasn't found, try looping to the top/bottom.
           if (!nextItem) {
-            this.walker.currentNode = this;
+            this.focusWalker.currentNode = this;
             if (moveForward) {
-              nextItem = this.walker.firstChild();
+              nextItem = this.focusWalker.firstChild();
             } else {
-              nextItem = this.walker.lastChild();
+              nextItem = this.focusWalker.lastChild();
             }
-          }
-
-          if (nextItem) {
-            nextItem.focus();
           }
           break;
         } else if (e.key === "Escape") {
@@ -768,19 +764,44 @@ class PanelList extends HTMLElement {
     }
   }
 
-  get walker() {
-    if (!this._walker) {
-      this._walker = document.createTreeWalker(this, NodeFilter.SHOW_ELEMENT, {
-        acceptNode: node => {
-          if (node.disabled || node.hidden || node.localName !== "panel-item") {
-            return NodeFilter.FILTER_REJECT;
-          }
+  /**
+   * A TreeWalker that can be used to focus elements. The returned element will
+   * be the element that has gained focus based on the requested movement
+   * through the tree.
+   *
+   * Example:
+   *
+   *   this.focusWalker.currentNode = this;
+   *   // Focus and get the first focusable child.
+   *   let focused = this.focusWalker.nextNode();
+   *   // Focus the second focusable child.
+   *   this.focusWalker.nextNode();
+   */
+  get focusWalker() {
+    if (!this._focusWalker) {
+      this._focusWalker = document.createTreeWalker(
+        this,
+        NodeFilter.SHOW_ELEMENT,
+        {
+          acceptNode: node => {
+            // No need to look at hidden nodes.
+            if (node.hidden) {
+              return NodeFilter.FILTER_REJECT;
+            }
 
-          return NodeFilter.FILTER_ACCEPT;
-        },
-      });
+            // Focus the node, if it worked then this is the node we want.
+            node.focus();
+            if (node === document.activeElement) {
+              return NodeFilter.FILTER_ACCEPT;
+            }
+
+            // Continue into child nodes if the parent couldn't be focused.
+            return NodeFilter.FILTER_SKIP;
+          },
+        }
+      );
     }
-    return this._walker;
+    return this._focusWalker;
   }
 
   async onShow() {
@@ -797,11 +818,8 @@ class PanelList extends HTMLElement {
         triggeringEvent &&
         triggeringEvent.mozInputSource === MouseEvent.MOZ_SOURCE_KEYBOARD
       ) {
-        this.walker.currentNode = this;
-        let firstItem = this.walker.nextNode();
-        if (firstItem) {
-          firstItem.focus();
-        }
+        this.focusWalker.currentNode = this;
+        this.focusWalker.nextNode();
       }
 
       this.sendEvent("shown");
@@ -895,7 +913,27 @@ class AddonOptions extends HTMLElement {
   setElementState(el, card, addon, updateInstall) {
     switch (el.getAttribute("action")) {
       case "remove":
-        el.hidden = !hasPermission(addon, "uninstall");
+        if (hasPermission(addon, "uninstall")) {
+          // Regular add-on that can be uninstalled.
+          el.disabled = false;
+          el.hidden = false;
+          document.l10n.setAttributes(el, "remove-addon-button");
+        } else if (addon.isBuiltin) {
+          // Likely the built-in themes, can't be removed, that's fine.
+          el.hidden = true;
+        } else {
+          // Likely sideloaded, mention that it can't be removed with a link.
+          el.hidden = false;
+          el.disabled = true;
+          if (!el.querySelector('[slot="support-link"]')) {
+            let link = document.createElement("a", { is: "support-link" });
+            link.setAttribute("data-l10n-name", "link");
+            link.setAttribute("support-page", "cant-remove-addon");
+            link.setAttribute("slot", "support-link");
+            el.appendChild(link);
+            document.l10n.setAttributes(el, "remove-addon-disabled-button");
+          }
+        }
         break;
       case "report":
         el.hidden = !isAbuseReportSupported(addon);
