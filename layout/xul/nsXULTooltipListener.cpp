4640818cebbcec6238072f99e63a89b3d26c2de6
Alexander J. Vincent <ajvincent@gmail.com>
2019-10-08 21:28:16 -0700

    Bug 1508165, build bustage fix for a no-longer-used variable.

--- layout/xul/nsXULTooltipListener.cpp
+++ layout/xul/nsXULTooltipListener.cpp
@@ -348,10 +348,6 @@ void nsXULTooltipListener::CheckTreeBodyMove(MouseEvent* aMouseEvent) {
     // determine if we are going to need a titletip
     // XXX check the disabletitletips attribute on the tree content
     mNeedTitletip = false;
-    int16_t colType = -1;
-    if (col) {
-      colType = col->Type();
-    }
     if (row >= 0 && cellInfo.mChildElt.EqualsLiteral("text")) {
       mNeedTitletip = tree->IsCellCropped(row, col, rv);
     }
