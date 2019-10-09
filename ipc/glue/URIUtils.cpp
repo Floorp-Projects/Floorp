4ae52d1f45267ed227c9286512160b9558cebb55
Matt Woodrow <mwoodrow@mozilla.com>
2019-10-09 04:52:34 +0000

    Bug 1578624 - P1: Serialize SubstitutingURL fully. r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D44757
    
    --HG--
    extra : moz-landing-system : lando

--- ipc/glue/URIUtils.cpp
+++ ipc/glue/URIUtils.cpp
@@ -9,6 +9,7 @@
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/dom/BlobURL.h"
+#include "mozilla/net/SubstitutingURL.h"
 #include "mozilla/NullPrincipalURI.h"
 #include "nsComponentManagerUtils.h"
 #include "nsDebug.h"
@@ -69,7 +70,11 @@ already_AddRefed<nsIURI> DeserializeURI(const URIParams& aParams) {
       break;
 
     case URIParams::TStandardURLParams:
-      mutator = do_CreateInstance(kStandardURLMutatorCID);
+      if (aParams.get_StandardURLParams().isSubstituting()) {
+        mutator = new net::SubstitutingURL::Mutator();
+      } else {
+        mutator = do_CreateInstance(kStandardURLMutatorCID);
+      }
       break;
 
     case URIParams::TJARURIParams:
