b7d78b54e2cdd25d021ff41ee75a985a31496343
Oana Pop Rus <opoprus@mozilla.com>
2019-10-10 22:14:48 +0300

    Backed out 3 changesets (bug 1580602) for eslint in test_setactionhandler.html on a CLOSED TREE
    Backed out changeset b4a5a6bc1d56 (bug 1580602)
    Backed out changeset e70e3c090791 (bug 1580602)
    Backed out changeset 2c90724f42fc (bug 1580602)

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -80,7 +80,6 @@ class VRDisplay;
 class VRServiceTest;
 class StorageManager;
 class MediaCapabilities;
-class MediaSession;
 struct ShareData;
 class WindowGlobalChild;
 
@@ -218,7 +217,6 @@ class Navigator final : public nsISupports, public nsWrapperCache {
   static void GetAcceptLanguages(nsTArray<nsString>& aLanguages);
 
   dom::MediaCapabilities* MediaCapabilities();
-  dom::MediaSession* MediaSession();
 
   AddonManager* GetMozAddonManager(ErrorResult& aRv);
 
@@ -280,7 +278,6 @@ class Navigator final : public nsISupports, public nsWrapperCache {
   nsTArray<uint32_t> mRequestedVibrationPattern;
   RefPtr<StorageManager> mStorageManager;
   RefPtr<dom::MediaCapabilities> mMediaCapabilities;
-  RefPtr<dom::MediaSession> mMediaSession;
   RefPtr<AddonManager> mAddonManager;
   RefPtr<webgpu::Instance> mWebGpu;
   RefPtr<Promise> mSharePromise;  // Web Share API related
