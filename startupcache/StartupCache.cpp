27626483e153f44fb8ad382d5588cc2f8bbcfa28
Doug Thayer <dothayer@mozilla.com>
2019-10-10 05:39:15 +0000

    Bug 1587107 - Increase StartupCache bg thread stack sizes r=njn
    In bug 1550108 we added a thread to prefetch the contents of the
    startup cache file, and as part of that we reduced its stack size
    and the stack size of the thread which writes the file. However
    it seems we set the write thread's stack size too low.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48570

--- startupcache/StartupCache.cpp
+++ startupcache/StartupCache.cpp
@@ -694,7 +694,7 @@ void StartupCache::WriteTimeout(nsITimer* aTimer, void* aClosure) {
   startupCacheObj->mCacheData.reset();
   startupCacheObj->mWriteThread = PR_CreateThread(
       PR_USER_THREAD, StartupCache::ThreadedWrite, startupCacheObj,
-      PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD, PR_JOINABLE_THREAD, 256 * 1024);
+      PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD, PR_JOINABLE_THREAD, 512 * 1024);
 }
 
 // We don't want to refcount StartupCache, so we'll just
