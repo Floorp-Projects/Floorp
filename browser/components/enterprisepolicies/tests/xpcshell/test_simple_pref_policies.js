412f8ef0e3e09a1ebd35190e0db69b6903158aa7
Michael Kaply <mozilla@kaply.com>
2019-10-10 08:13:39 -0500

    Bug 1577149 - Additional preferences managed by policy requested by users. r=mconley, a=lizzard

--- browser/components/enterprisepolicies/tests/xpcshell/test_simple_pref_policies.js
+++ browser/components/enterprisepolicies/tests/xpcshell/test_simple_pref_policies.js
@@ -121,6 +121,10 @@ const POLICIES_TESTS = [
           SPNEGO: true,
           NTLM: true,
         },
+        AllowProxies: {
+          SPNEGO: false,
+          NTLM: false,
+        },
       },
     },
     lockedPrefs: {
@@ -129,6 +133,8 @@ const POLICIES_TESTS = [
       "network.automatic-ntlm-auth.trusted-uris": "a.com, b.com",
       "network.automatic-ntlm-auth.allow-non-fqdn": true,
       "network.negotiate-auth.allow-non-fqdn": true,
+      "network.automatic-ntlm-auth.allow-proxies": false,
+      "network.negotiate-auth.allow-proxies": false,
     },
   },
 
