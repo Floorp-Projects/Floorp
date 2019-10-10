69a6cb1ef23d1b853076b65083b0f779a515b84c
Erica Wright <ewright@mozilla.com>
2019-10-10 08:06:58 +0000

    Bug 1586882 - Add params to lockwise and proxy links from protection report. r=johannh, a=lizzard
    Differential Revision: https://phabricator.services.mozilla.com/D48582
    
    --HG--
    extra : source : bc6e2d9f6b7449d94ee4bd25cab7f4da351c994e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1629,9 +1629,9 @@ pref("browser.contentblocking.report.proxy.enabled", false);
 
 pref("browser.contentblocking.report.monitor.url", "https://monitor.firefox.com/?entrypoint=protection_report_monitor&utm_source=about-protections");
 pref("browser.contentblocking.report.monitor.sign_in_url", "https://monitor.firefox.com/oauth/init?entrypoint=protection_report_monitor&utm_source=about-protections&email=");
-pref("browser.contentblocking.report.lockwise.url", "https://lockwise.firefox.com/");
+pref("browser.contentblocking.report.lockwise.url", "https://lockwise.firefox.com/?utm_source=firefox-desktop&utm_medium=referral&utm_campaign=about-protections&utm_content=about-protections");
 pref("browser.contentblocking.report.manage_devices.url", "https://accounts.firefox.com/settings/clients");
-pref("browser.contentblocking.report.proxy_extension.url", "https://fpn.firefox.com/browser");
+pref("browser.contentblocking.report.proxy_extension.url", "https://fpn.firefox.com/browser?utm_source=firefox-desktop&utm_medium=referral&utm_campaign=about-protections&utm_content=about-protections");
 
 // Protection Report's SUMO urls
 pref("browser.contentblocking.report.monitor.how_it_works.url", "https://support.mozilla.org/1/firefox/%VERSION%/%OS%/%LOCALE%/monitor-faq");
