1632828fd7d713d97e8a7283e0be5103ad21f9c9
Kris Maglione <maglione.k@gmail.com>
2019-10-11 19:32:02 +0000

    Bug 1586911: Silence SHEntry assertion after local->remote->local iframe navigation. r=peterv
    When we have a parser-created iframe which starts out in-process, transitions
    to remote, and then transitions back to in-process, we create separate
    DocShells for the first and last in-process loads. Since both are
    network-created, and have the same child index, they both try to add
    themselves as children to their parent's SHistory at the same index. And since
    the entry for the first DocShell already exists at that index when we try to
    add the second, that triggers an assertion.
    
    This isn't really ideal, but it is expected given the current state of session
    history under Fission. It should hopefully be solved more gracefully when the
    Fission-aware session history rewrite is done, but in the mean time, I think
    we should just ignore the conflict, since it's expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48437

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -3460,7 +3460,7 @@ nsDocShell::AddChildSHEntry(nsISHEntry* aCloneRef, nsISHEntry* aNewEntry,
   } else if (!aCloneRef) {
     /* This is an initial load in some subframe.  Just append it if we can */
     if (mOSHE) {
-      rv = mOSHE->AddChild(aNewEntry, aChildOffset);
+      rv = mOSHE->AddChild(aNewEntry, aChildOffset, UseRemoteSubframes());
     }
   } else {
     rv = AddChildSHEntryInternal(aCloneRef, aNewEntry, aChildOffset, aLoadType,
