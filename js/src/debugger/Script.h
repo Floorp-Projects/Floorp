bcb4aa29fd23718fbef198297102aed2a1395cc3
Jim Blandy <jimb@mozilla.com>
2019-10-15 19:29:29 +0000

    Bug 1564170: Clean up some DebuggerScript referent matchers. r=jorendorff
    As suggested in patch review, some of the mozilla::Variant matcher classes in
    Script.cpp could be written more clearly using lambda-based matching. This patch
    applies that technique in the implementations of DebuggerScript's create,
    getStartLine, and getFormat methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38559

--- js/src/debugger/Script.h
+++ js/src/debugger/Script.h
@@ -76,12 +76,8 @@ class DebuggerScript : public NativeObject {
   static const JSPropertySpec properties_[];
   static const JSFunctionSpec methods_[];
 
-  class SetPrivateMatcher;
-  struct GetStartLineMatcher;
-  struct GetStartColumnMatcher;
   struct GetLineCountMatcher;
   class GetSourceMatcher;
-  class GetFormatMatcher;
   template <bool OnlyOffsets>
   class GetPossibleBreakpointsMatcher;
   class GetOffsetMetadataMatcher;
