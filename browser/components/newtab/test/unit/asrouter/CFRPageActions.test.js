679469d477297dc9d0418b1fc2998af76834b1e4
Nan Jiang <njiang028@gmail.com>
2019-10-15 23:12:49 +0000

    Bug 1588199 - Load local fluent file for ASRouter as fallback r=Pike,Mardak
    This changes the either-or semantics to one-after between the local and remote fluent files for ASRouter.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49068

--- browser/components/newtab/test/unit/asrouter/CFRPageActions.test.js
+++ browser/components/newtab/test/unit/asrouter/CFRPageActions.test.js
@@ -770,18 +770,22 @@ describe("CFRPageActions", () => {
           "browser/branding/sync-brand.ftl",
           "branding/brand.ftl",
         ]);
+        assert.isFunction(args[1]);
       });
       it("should load the local Fluent file if USE_REMOTE_L10N_PREF is false", () => {
         sandbox.stub(global.Services.prefs, "getBoolPref").returns(false);
         pageAction._createDOML10n();
 
-        assert.calledOnce(domL10nStub);
-        assert.calledWith(domL10nStub, [
+        const { args } = domL10nStub.firstCall;
+        // The first arg is the resource array, and the second one should be null.
+        assert.equal(args.length, 2);
+        assert.deepEqual(args[0], [
           "browser/newtab/asrouter.ftl",
           "browser/branding/brandings.ftl",
           "browser/branding/sync-brand.ftl",
           "branding/brand.ftl",
         ]);
+        assert.isUndefined(args[1]);
       });
     });
   });
