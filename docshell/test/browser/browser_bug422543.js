20333f071510b2462ad5bcb8ffc0f451ebf18d67
Peter Van der Beken <peterv@propagandism.org>
2019-10-15 09:25:03 +0200

    Bug 1591899 - Make various docshell tests work with session history in the parent turned off. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D50791
    
    --HG--
    extra : rebase_source : e8441d50b7bdc59933e5adc6430b01039e775515
    extra : source : c24e9ca249b1d21e20f3c99cb8ae752c441e4b86
    extra : histedit_source : e4071f2be4c58baef0f26303d5a16f985fea8506

--- docshell/test/browser/browser_bug422543.js
+++ docshell/test/browser/browser_bug422543.js
@@ -2,6 +2,112 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 add_task(async function runTests() {
+  if (!SpecialPowers.getBoolPref("fission.sessionHistoryInParent")) {
+    await setupAsync();
+    let browser = gBrowser.selectedBrowser;
+    // Now that we're set up, initialize our frame script.
+    await checkListenersAsync("initial", "listeners initialized");
+
+    // Check if all history listeners are always notified.
+    info("# part 1");
+    await whenPageShown(browser, () =>
+      BrowserTestUtils.loadURI(browser, "http://www.example.com/")
+    );
+    await checkListenersAsync("newentry", "shistory has a new entry");
+    ok(browser.canGoBack, "we can go back");
+
+    await whenPageShown(browser, () => browser.goBack());
+    await checkListenersAsync("gotoindex", "back to the first shentry");
+    ok(browser.canGoForward, "we can go forward");
+
+    await whenPageShown(browser, () => browser.goForward());
+    await checkListenersAsync("gotoindex", "forward to the second shentry");
+
+    await whenPageShown(browser, () => browser.reload());
+    await checkListenersAsync("reload", "current shentry reloaded");
+
+    await whenPageShown(browser, () => browser.gotoIndex(0));
+    await checkListenersAsync("gotoindex", "back to the first index");
+
+    // Check nsISHistory.notifyOnHistoryReload
+    info("# part 2");
+    ok(await notifyReloadAsync(), "reloading has not been canceled");
+    await checkListenersAsync("reload", "saw the reload notification");
+
+    // Let the first listener cancel the reload action.
+    info("# part 3");
+    await resetListenersAsync();
+    await setListenerRetvalAsync(0, false);
+    ok(!(await notifyReloadAsync()), "reloading has been canceled");
+    await checkListenersAsync("reload", "saw the reload notification");
+
+    // Let both listeners cancel the reload action.
+    info("# part 4");
+    await resetListenersAsync();
+    await setListenerRetvalAsync(1, false);
+    ok(!(await notifyReloadAsync()), "reloading has been canceled");
+    await checkListenersAsync("reload", "saw the reload notification");
+
+    // Let the second listener cancel the reload action.
+    info("# part 5");
+    await resetListenersAsync();
+    await setListenerRetvalAsync(0, true);
+    ok(!(await notifyReloadAsync()), "reloading has been canceled");
+    await checkListenersAsync("reload", "saw the reload notification");
+
+    function listenOnce(message, arg = {}) {
+      return new Promise(resolve => {
+        let mm = gBrowser.selectedBrowser.messageManager;
+        mm.addMessageListener(message + ":return", function listener(msg) {
+          mm.removeMessageListener(message + ":return", listener);
+          resolve(msg.data);
+        });
+
+        mm.sendAsyncMessage(message, arg);
+      });
+    }
+
+    function checkListenersAsync(aLast, aMessage) {
+      return listenOnce("bug422543:getListenerStatus").then(listenerStatuses => {
+        is(listenerStatuses[0], aLast, aMessage);
+        is(listenerStatuses[1], aLast, aMessage);
+      });
+    }
+
+    function resetListenersAsync() {
+      return listenOnce("bug422543:resetListeners");
+    }
+
+    function notifyReloadAsync() {
+      return listenOnce("bug422543:notifyReload").then(({ rval }) => {
+        return rval;
+      });
+    }
+
+    function setListenerRetvalAsync(num, val) {
+      return listenOnce("bug422543:setRetval", { num, val });
+    }
+
+    function setupAsync() {
+      return BrowserTestUtils.openNewForegroundTab(
+        gBrowser,
+        "http://mochi.test:8888"
+      ).then(function(tab) {
+        let browser = tab.linkedBrowser;
+        registerCleanupFunction(async function() {
+          await listenOnce("bug422543:cleanup");
+          gBrowser.removeTab(tab);
+        });
+
+        browser.messageManager.loadFrameScript(
+          getRootDirectory(gTestPath) + "file_bug422543_script.js",
+          false
+        );
+      });
+    }
+    return;
+  }
+
   await setup();
   let browser = gBrowser.selectedBrowser;
   // Now that we're set up, initialize our frame script.
