8b4a760b76001acbdc1d2f1836e6e8740fbe22d2
Edwin Takahashi <egao@mozilla.com>
2019-10-15 20:58:35 +0000

    Bug 1559977 - Remove tools/rb from py3/py2 linter exclusion list r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D35238

--- tools/rb/fix_macosx_stack.py
+++ tools/rb/fix_macosx_stack.py
@@ -8,7 +8,7 @@
 # NS_FormatCodeAddress(), which on Mac often lack a file name and a line
 # number.
 
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import json
 import os
