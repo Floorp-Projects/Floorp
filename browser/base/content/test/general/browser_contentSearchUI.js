4d8032ae0b2cd5e6c217edb680fe37118ea132ba
Nihanth Subramanya <nhnt11@gmail.com>
2019-10-16 00:42:14 +0000

    Bug 1191130 - [Content Search UI tests] Fix TypeError and add some diagnostic logging. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D47400
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/general/browser_contentSearchUI.js
+++ browser/base/content/test/general/browser_contentSearchUI.js
@@ -707,9 +707,11 @@ function msg(type, data = null) {
       if (msgObj.data.type != type) {
         return;
       }
+      info(`Received response: ${type}`);
       gMsgMan.removeMessageListener(TEST_MSG, onMsg);
       resolve(msgObj.data.data);
     });
+    info(`Sending message: ${type}`);
     gMsgMan.sendAsyncMessage(TEST_MSG, {
       type,
       data,
