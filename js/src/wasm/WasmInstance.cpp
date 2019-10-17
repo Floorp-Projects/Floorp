6e597fbe291de1bc65f338430b389ff7ed0ab097
Lars T Hansen <lhansen@mozilla.com>
2019-10-17 13:25:24 +0000

    Bug 1546592 - Allow element segments to be 'anyref', and generalize. r=rhunt
    The main changes here are:
    
    * Element segments that carry an elementexpr can carry a type T that
      is other than 'funcref', though we require T <: anyref
    
    * We generalize type handling around table initialization so that a
      table-of-T can be initialized from segment-of-U if U <: T.
    
    
    Also:
    
    * A declared element segment needs to have type funcref.  The spec is
      silent on this, but it's a conservative choice.
    
    * A drive-by fix in serialize/deserialize, the 'kind' field was not
      being handled properly.  This doesn't affect any shipping product as
      the serialize/deserialize code is currently unused.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48690

--- js/src/wasm/WasmInstance.cpp
+++ js/src/wasm/WasmInstance.cpp
@@ -753,7 +753,7 @@ static int32_t PerformWait(Instance* instance, uint32_t byteOffset, T value,
   return 0;
 }
 
-void Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
+bool Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
                          uint32_t dstOffset, uint32_t srcOffset, uint32_t len) {
   Table& table = *tables_[tableIndex];
   MOZ_ASSERT(dstOffset <= table.length());
@@ -773,6 +773,13 @@ void Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
     uint32_t funcIndex = elemFuncIndices[srcOffset + i];
     if (funcIndex == NullFuncIndex) {
       table.setNull(dstOffset + i);
+    } else if (!table.isFunction()) {
+      // Note, fnref must be rooted if we do anything more than just store it.
+      void* fnref = Instance::funcRef(this, funcIndex);
+      if (fnref == AnyRef::invalid().forCompiledCode()) {
+        return false;  // OOM, which has already been reported.
+      }
+      table.fillAnyRef(dstOffset + i, 1, AnyRef::fromCompiledCode(fnref));
     } else {
       if (funcIndex < funcImports.length()) {
         FuncImportTls& import = funcImportTls(funcImports[funcIndex]);
@@ -801,6 +808,7 @@ void Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
       table.setFuncRef(dstOffset + i, code, this);
     }
   }
+  return true;
 }
 
 /* static */ int32_t Instance::tableInit(Instance* instance, uint32_t dstOffset,
@@ -824,10 +832,6 @@ void Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
 
   const Table& table = *instance->tables()[tableIndex];
   const uint32_t tableLen = table.length();
-
-  // Element segments cannot currently contain arbitrary values, and anyref
-  // tables cannot be initialized from segments.
-  MOZ_ASSERT(table.kind() == TableKind::FuncRef);
 
   // We are proposing to copy
   //
@@ -860,7 +864,9 @@ void Instance::initElems(uint32_t tableIndex, const ElemSegment& seg,
   }
 
   if (len > 0) {
-    instance->initElems(tableIndex, seg, dstOffset, srcOffset, len);
+    if (!instance->initElems(tableIndex, seg, dstOffset, srcOffset, len)) {
+      return -1;  // OOM, which has already been reported.
+    }
   }
 
   if (!mustTrap) {
