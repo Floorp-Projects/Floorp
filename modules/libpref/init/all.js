ba9c1f273eb4260817a3c5d698412e414969517b
Mark Banner <standard8@mozilla.com>
2019-10-18 18:53:36 +0000

    Bug 1588785 - Make the search service also detect when the UI is not enabled. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D49652

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4449,6 +4449,7 @@ pref("browser.search.geoSpecificDefaults", false);
 pref("browser.search.geoip.url", "https://location.services.mozilla.com/v1/country?key=%MOZILLA_API_KEY%");
 pref("browser.search.geoip.timeout", 3000);
 pref("browser.search.separatePrivateDefault", false);
+pref("browser.search.separatePrivateDefault.ui.enabled", false);
 
 #ifdef MOZ_OFFICIAL_BRANDING
   // {moz:official} expands to "official"
