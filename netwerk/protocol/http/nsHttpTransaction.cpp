b93b717b8caf3e2a5385833504bdb0692a287358
Junior Hsu <juhsu@mozilla.com>
2019-10-18 09:44:51 +0000

    Bug 1580288, r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D47831

--- netwerk/protocol/http/nsHttpTransaction.cpp
+++ netwerk/protocol/http/nsHttpTransaction.cpp
@@ -2360,6 +2360,8 @@ void nsHttpTransaction::Refused0RTT() {
 void nsHttpTransaction::SetHttpTrailers(nsCString& aTrailers) {
   LOG(("nsHttpTransaction::SetHttpTrailers %p", this));
   LOG(("[\n    %s\n]", aTrailers.BeginReading()));
+
+  MutexAutoLock lock(*nsHttp::GetLock());
   if (!mForTakeResponseTrailers) {
     mForTakeResponseTrailers = new nsHttpHeaderArray();
   }
