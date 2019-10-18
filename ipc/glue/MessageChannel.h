99f0b672a6454b143b8da1a66157dd0e0387de8e
Jim Porter <jporter@mozilla.com>
2019-10-18 02:56:02 +0000

    Bug 1557447 - Profiler support for IPC information; r=nika
    This adds the ability to add profile markers for both the sender and recipient
    sides of IPC messages. These can then be correlated with one another in the
    profile visualization. For the UI component of this patch, see
    <https://github.com/firefox-devtools/profiler/pull/2172>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D42226

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -50,6 +50,11 @@ class RefCountedMonitor : public Monitor {
   ~RefCountedMonitor() {}
 };
 
+enum class MessageDirection {
+  eSending,
+  eReceiving,
+};
+
 enum class SyncSendError {
   SendSuccess,
   PreviousTimeout,
@@ -462,6 +467,9 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
   // This method is only safe to call on the worker thread, or in a
   // debugger with all threads paused.
   void DumpInterruptStack(const char* const pfx = "") const;
+
+  void AddProfilerMarker(const IPC::Message* aMessage,
+                         MessageDirection aDirection);
 
  private:
   // Called from both threads
