3bcc69f9838a10c723e151206d013fac7a7adeed
Mirko Brodesser <mbrodesser@mozilla.com>
2019-10-21 09:06:48 +0000

    Bug 1584170: part 2) Replace array with `AutoTArray`. r=hsivonen, a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D48321
    
    --HG--
    extra : source : be0a2d1c25c15069d14f9fc5a4ac0c4571c287cf

--- dom/base/nsPlainTextSerializer.h
+++ dom/base/nsPlainTextSerializer.h
@@ -366,8 +366,7 @@ class nsPlainTextSerializer final : public nsIContentSerializer {
   uint32_t mIgnoreAboveIndex;
 
   // The stack for ordered lists
-  int32_t* mOLStack;
-  uint32_t mOLStackIndex;
+  AutoTArray<int32_t, 100> mOLStack;
 
   uint32_t mULCount;
 
