ebcb4e6fbfed5dab23205383b437708247224538
shindli <shindli@mozilla.com>
2019-10-21 21:09:24 +0300

    Backed out changeset 5686e9cc48b2 (bug 1579992) for causing bc failures in browser/components/aboutlogins/tests/browser/browser_dismissFooter.js CLOSED TREE

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -4463,7 +4463,7 @@ nsILoadInfo::CrossOriginOpenerPolicy CreateCrossOriginOpenerPolicy(
 // Obtain a cross-origin opener-policy from a response response and a
 // cross-origin opener policy initiator.
 // https://gist.github.com/annevk/6f2dd8c79c77123f39797f6bdac43f3e
-NS_IMETHODIMP HttpBaseChannel::ComputeCrossOriginOpenerPolicy(
+NS_IMETHODIMP HttpBaseChannel::GetCrossOriginOpenerPolicy(
     nsILoadInfo::CrossOriginOpenerPolicy aInitiatorPolicy,
     nsILoadInfo::CrossOriginOpenerPolicy* aOutPolicy) {
   MOZ_ASSERT(aOutPolicy);
