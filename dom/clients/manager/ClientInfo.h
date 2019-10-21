ac69d4fff8b392ac64b4bbc30f9d36cad0992359
Matt Woodrow <mwoodrow@mozilla.com>
2019-10-21 21:47:02 +0000

    Bug 1583700 - Create a new ClientSource from a parent-allocated ClientInfo even for same-origin redirects, since there might have been a prior cross-origin redirect. r=perry,asuth
    We fail navigation-redirect.https.html?client without this (with the subtest to redirects to a cross-origin page and then redirects back again to a same-origin page). In this case the ClientChannelHelper running in the child only sees a same-origin redirect (the first URL to the final one), but we've still allocated a new ClientInfo in the parent and we want to create the corresponding ClientSource.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49870
    
    --HG--
    extra : moz-landing-system : lando

--- dom/clients/manager/ClientInfo.h
+++ dom/clients/manager/ClientInfo.h
@@ -46,6 +46,7 @@ class ClientInfo final {
   ~ClientInfo();
 
   bool operator==(const ClientInfo& aRight) const;
+  bool operator!=(const ClientInfo& aRight) const;
 
   // Get the unique identifier chosen at the time of the global's creation.
   const nsID& Id() const;
