7f73b2f797b0483ebea656a681b1295677e87799
Benjamin Bouvier <benj@benj.me>
2019-10-22 07:11:05 +0000

    Bug 1590083: Disable wasm multi value with Cranelift; r=lth
    Since wasm multi-value is enabled by default (without a shell switch), there
    was no way to signal it was disabled in certain configurations, namely
    Cranelift.
    
    Instead, assume multi-value is enabled by default and disable it with
    Cranelift, and store this information in the CompilerEnvironment so we can use
    it when iterating on the wasm binary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49925
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/jit-test/tests/wasm/binary.js
+++ js/src/jit-test/tests/wasm/binary.js
@@ -234,13 +234,12 @@ assertErrorMessage(() => wasmEval(moduleWithSections([
 for (var bad of [0xff, 1, 0x3f])
     assertErrorMessage(() => wasmEval(moduleWithSections([sigSection([v2vSig]), declSection([0]), bodySection([funcBody({locals:[], body:[BlockCode, bad, EndCode]})])])), CompileError, /invalid .*block type/);
 
+const multiValueModule = moduleWithSections([sigSection([v2vSig]), declSection([0]), bodySection([funcBody({locals:[], body:[BlockCode, 0, EndCode]})])]);
 if (wasmMultiValueEnabled()) {
     // In this test module, 0 denotes a void-to-void block type.
-    let binary = moduleWithSections([sigSection([v2vSig]), declSection([0]), bodySection([funcBody({locals:[], body:[BlockCode, 0, EndCode]})])]);
-    assertEq(WebAssembly.validate(binary), true);
+    assertEq(WebAssembly.validate(multiValueModule), true);
 } else {
-    const bad = 0;
-    assertErrorMessage(() => wasmEval(moduleWithSections([sigSection([v2vSig]), declSection([0]), bodySection([funcBody({locals:[], body:[BlockCode, bad, EndCode]})])])), CompileError, /invalid .*block type/);
+    assertErrorMessage(() => wasmEval(multiValueModule), CompileError, /invalid .*block type/);
 }
 
 // Ensure all invalid opcodes rejected
