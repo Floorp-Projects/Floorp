290145cc7446c518c4cac6327390c869ee379714
Matthew Gregan <kinetik@flim.org>
2019-10-22 00:01:07 +0000

    Bug 1590246 - Update audioipc to 8f2a9e0a.  r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D50000

--- media/audioipc/client/src/lib.rs
+++ media/audioipc/client/src/lib.rs
@@ -16,14 +16,12 @@ mod stream;
 
 use crate::context::ClientContext;
 use crate::stream::ClientStream;
-use audioipc::{PlatformHandle, PlatformHandleType};
+use audioipc::PlatformHandleType;
 use cubeb_backend::{capi, ffi};
 use std::os::raw::{c_char, c_int};
 
-type InitParamsTls = std::cell::RefCell<Option<CpuPoolInitParams>>;
-
 thread_local!(static IN_CALLBACK: std::cell::RefCell<bool> = std::cell::RefCell::new(false));
-thread_local!(static CPUPOOL_INIT_PARAMS: InitParamsTls = std::cell::RefCell::new(None));
+thread_local!(static AUDIOIPC_INIT_PARAMS: std::cell::RefCell<Option<AudioIpcInitParams>> = std::cell::RefCell::new(None));
 
 // This must match the definition of AudioIpcInitParams in
 // dom/media/CubebUtils.cpp in Gecko.
@@ -37,23 +35,6 @@ pub struct AudioIpcInitParams {
     pub thread_create_callback: Option<extern "C" fn(*const ::std::os::raw::c_char)>,
 }
 
-#[derive(Clone, Copy, Debug)]
-struct CpuPoolInitParams {
-    pool_size: usize,
-    stack_size: usize,
-    thread_create_callback: Option<extern "C" fn(*const ::std::os::raw::c_char)>,
-}
-
-impl CpuPoolInitParams {
-    fn init_with(params: &AudioIpcInitParams) -> Self {
-        CpuPoolInitParams {
-            pool_size: params.pool_size,
-            stack_size: params.stack_size,
-            thread_create_callback: params.thread_create_callback,
-        }
-    }
-}
-
 fn set_in_callback(in_callback: bool) {
     IN_CALLBACK.with(|b| {
         assert_eq!(*b.borrow(), !in_callback);
@@ -63,7 +44,7 @@ fn set_in_callback(in_callback: bool) {
 
 fn run_in_callback<F, R>(f: F) -> R
 where
-    F: FnOnce() -> R
+    F: FnOnce() -> R,
 {
     set_in_callback(true);
 
@@ -80,17 +61,6 @@ fn assert_not_in_callback() {
     });
 }
 
-fn set_cpupool_init_params<P>(params: P)
-where
-    P: Into<Option<CpuPoolInitParams>>,
-{
-    CPUPOOL_INIT_PARAMS.with(|p| {
-        *p.borrow_mut() = params.into();
-    });
-}
-
-static mut G_SERVER_FD: Option<PlatformHandle> = None;
-
 #[no_mangle]
 /// Entry point from C code.
 pub unsafe extern "C" fn audioipc_client_init(
@@ -104,16 +74,8 @@ pub unsafe extern "C" fn audioipc_client_init(
 
     let init_params = &*init_params;
 
-    // TODO: Better way to pass extra parameters to Context impl.
-    if G_SERVER_FD.is_some() {
-        return cubeb_backend::ffi::CUBEB_ERROR;
-    }
-    G_SERVER_FD = PlatformHandle::try_new(init_params.server_connection);
-    if G_SERVER_FD.is_none() {
-        return cubeb_backend::ffi::CUBEB_ERROR;
-    }
-
-    let cpupool_init_params = CpuPoolInitParams::init_with(&init_params);
-    set_cpupool_init_params(cpupool_init_params);
+    AUDIOIPC_INIT_PARAMS.with(|p| {
+        *p.borrow_mut() = Some(*init_params);
+    });
     capi::capi_init::<ClientContext>(c, context_name)
 }
