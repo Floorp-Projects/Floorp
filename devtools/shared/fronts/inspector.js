b463cdc1ba7bc12f1390cb862d461369336b6336
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-10-22 06:14:36 +0000

    Bug 1589301 - Migrate WalkerFront methods to async/await syntax. r=gl.
    Differential Revision: https://phabricator.services.mozilla.com/D49720

--- devtools/shared/fronts/inspector.js
+++ devtools/shared/fronts/inspector.js
@@ -44,10 +44,9 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
    * This is kept for backward-compatibility reasons with older remote target.
    * Targets previous to bug 916443
    */
-  pick() {
-    return super.pick().then(response => {
-      return response.node;
-    });
+  async pick() {
+    const response = await super.pick();
+    return response.node;
   }
 
   constructor(client, targetFront, parentFront) {
@@ -85,11 +84,10 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
    * Create the root node promise, triggering the "new-root" notification
    * on resolution.
    */
-  _createRootNodePromise() {
+  async _createRootNodePromise() {
     this._rootNodeDeferred = defer();
-    this._rootNodeDeferred.promise.then(() => {
-      this.emit("new-root");
-    });
+    await this._rootNodeDeferred.promise;
+    this.emit("new-root");
   }
 
   /**
@@ -129,20 +127,18 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
    * semantics by setting our local retained flag on the node only AFTER
    * a SUCCESSFUL retainNode call.
    */
-  retainNode(node) {
-    return super.retainNode(node).then(() => {
-      node.retained = true;
-    });
+  async retainNode(node) {
+    await super.retainNode(node);
+    node.retained = true;
   }
 
-  unretainNode(node) {
-    return super.unretainNode(node).then(() => {
-      node.retained = false;
-      if (this._retainedOrphans.has(node)) {
-        this._retainedOrphans.delete(node);
-        this._releaseFront(node);
-      }
-    });
+  async unretainNode(node) {
+    await super.unretainNode(node);
+    node.retained = false;
+    if (this._retainedOrphans.has(node)) {
+      this._retainedOrphans.delete(node);
+      this._releaseFront(node);
+    }
   }
 
   releaseNode(node, options = {}) {
@@ -153,16 +149,14 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
     return super.releaseNode({ actorID: actorID });
   }
 
-  findInspectingNode() {
-    return super.findInspectingNode().then(response => {
-      return response.node;
-    });
+  async findInspectingNode() {
+    const response = await super.findInspectingNode();
+    return response.node;
   }
 
-  querySelector(queryNode, selector) {
-    return super.querySelector(queryNode, selector).then(response => {
-      return response.node;
-    });
+  async querySelector(queryNode, selector) {
+    const response = await super.querySelector(queryNode, selector);
+    return response.node;
   }
 
   async gripToNodeFront(grip) {
@@ -176,37 +170,33 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
     return nodeFront;
   }
 
-  getNodeActorFromWindowID(windowID) {
-    return super.getNodeActorFromWindowID(windowID).then(response => {
-      return response ? response.node : null;
-    });
+  async getNodeActorFromWindowID(windowID) {
+    const response = await super.getNodeActorFromWindowID(windowID);
+    return response ? response.node : null;
   }
 
-  getNodeActorFromContentDomReference(contentDomReference) {
+  async getNodeActorFromContentDomReference(contentDomReference) {
     if (!this.traits.retrieveNodeFromContentDomReference) {
       console.error(
         "The server is too old to retrieve a node from a contentDomReference"
       );
-      return Promise.resolve(null);
+      return null;
     }
 
-    return super
-      .getNodeActorFromContentDomReference(contentDomReference)
-      .then(response => {
-        return response ? response.node : null;
-      });
+    const response = await super.getNodeActorFromContentDomReference(
+      contentDomReference
+    );
+    return response ? response.node : null;
   }
 
-  getStyleSheetOwnerNode(styleSheetActorID) {
-    return super.getStyleSheetOwnerNode(styleSheetActorID).then(response => {
-      return response ? response.node : null;
-    });
+  async getStyleSheetOwnerNode(styleSheetActorID) {
+    const response = await super.getStyleSheetOwnerNode(styleSheetActorID);
+    return response ? response.node : null;
   }
 
-  getNodeFromActor(actorID, path) {
-    return super.getNodeFromActor(actorID, path).then(response => {
-      return response ? response.node : null;
-    });
+  async getNodeFromActor(actorID, path) {
+    const response = await super.getNodeFromActor(actorID, path);
+    return response ? response.node : null;
   }
 
   /*
@@ -281,168 +271,164 @@ class WalkerFront extends FrontClassWithSpec(walkerSpec) {
   /**
    * Get any unprocessed mutation records and process them.
    */
-  getMutations(options = {}) {
-    /* eslint-disable complexity */
-    return super.getMutations(options).then(mutations => {
-      const emitMutations = [];
-      for (const change of mutations) {
-        // The target is only an actorID, get the associated front.
-        let targetID;
-        let targetFront;
+  /* eslint-disable complexity */
+  async getMutations(options = {}) {
+    const mutations = await super.getMutations(options);
+    const emitMutations = [];
+    for (const change of mutations) {
+      // The target is only an actorID, get the associated front.
+      let targetID;
+      let targetFront;
 
-        if (change.type === "newRoot") {
-          // We may receive a new root without receiving any documentUnload
-          // beforehand. Like when opening tools in middle of a document load.
-          if (this.rootNode) {
-            this._createRootNodePromise();
-          }
-          this.rootNode = types.getType("domnode").read(change.target, this);
-          this._rootNodeDeferred.resolve(this.rootNode);
-          targetID = this.rootNode.actorID;
-          targetFront = this.rootNode;
-        } else {
-          targetID = change.target;
-          targetFront = this.get(targetID);
+      if (change.type === "newRoot") {
+        // We may receive a new root without receiving any documentUnload
+        // beforehand. Like when opening tools in middle of a document load.
+        if (this.rootNode) {
+          this._createRootNodePromise();
         }
-
-        if (!targetFront) {
-          console.warn(
-            "Got a mutation for an unexpected actor: " +
-              targetID +
-              ", please file a bug on bugzilla.mozilla.org!"
-          );
-          console.trace();
-          continue;
-        }
-
-        const emittedMutation = Object.assign(change, { target: targetFront });
-
-        if (
-          change.type === "childList" ||
-          change.type === "nativeAnonymousChildList"
-        ) {
-          // Update the ownership tree according to the mutation record.
-          const addedFronts = [];
-          const removedFronts = [];
-          for (const removed of change.removed) {
-            const removedFront = this.get(removed);
-            if (!removedFront) {
-              console.error(
-                "Got a removal of an actor we didn't know about: " + removed
-              );
-              continue;
-            }
-            // Remove from the ownership tree
-            removedFront.reparent(null);
-
-            // This node is orphaned unless we get it in the 'added' list
-            // eventually.
-            this._orphaned.add(removedFront);
-            removedFronts.push(removedFront);
-          }
-          for (const added of change.added) {
-            const addedFront = this.get(added);
-            if (!addedFront) {
-              console.error(
-                "Got an addition of an actor we didn't know " +
-                  "about: " +
-                  added
-              );
-              continue;
-            }
-            addedFront.reparent(targetFront);
-
-            // The actor is reconnected to the ownership tree, unorphan
-            // it.
-            this._orphaned.delete(addedFront);
-            addedFronts.push(addedFront);
-          }
-
-          // Before passing to users, replace the added and removed actor
-          // ids with front in the mutation record.
-          emittedMutation.added = addedFronts;
-          emittedMutation.removed = removedFronts;
-
-          // If this is coming from a DOM mutation, the actor's numChildren
-          // was passed in. Otherwise, it is simulated from a frame load or
-          // unload, so don't change the front's form.
-          if ("numChildren" in change) {
-            targetFront._form.numChildren = change.numChildren;
-          }
-        } else if (change.type === "frameLoad") {
-          // Nothing we need to do here, except verify that we don't have any
-          // document children, because we should have gotten a documentUnload
-          // first.
-          for (const child of targetFront.treeChildren()) {
-            if (child.nodeType === nodeConstants.DOCUMENT_NODE) {
-              console.warn(
-                "Got an unexpected frameLoad in the inspector, " +
-                  "please file a bug on bugzilla.mozilla.org!"
-              );
-              console.trace();
-            }
-          }
-        } else if (change.type === "documentUnload") {
-          if (targetFront === this.rootNode) {
-            this._createRootNodePromise();
-          }
-
-          // We try to give fronts instead of actorIDs, but these fronts need
-          // to be destroyed now.
-          emittedMutation.target = targetFront.actorID;
-          emittedMutation.targetParent = targetFront.parentNode();
-
-          // Release the document node and all of its children, even retained.
-          this._releaseFront(targetFront, true);
-        } else if (change.type === "shadowRootAttached") {
-          targetFront._form.isShadowHost = true;
-        } else if (change.type === "customElementDefined") {
-          targetFront._form.customElementLocation =
-            change.customElementLocation;
-        } else if (change.type === "unretained") {
-          // Retained orphans were force-released without the intervention of
-          // client (probably a navigated frame).
-          for (const released of change.nodes) {
-            const releasedFront = this.get(released);
-            this._retainedOrphans.delete(released);
-            this._releaseFront(releasedFront, true);
-          }
-        } else {
-          targetFront.updateMutation(change);
-        }
-
-        // Update the inlineTextChild property of the target for a selected list of
-        // mutation types.
-        if (
-          change.type === "inlineTextChild" ||
-          change.type === "childList" ||
-          change.type === "shadowRootAttached" ||
-          change.type === "nativeAnonymousChildList"
-        ) {
-          if (change.inlineTextChild) {
-            targetFront.inlineTextChild = types
-              .getType("domnode")
-              .read(change.inlineTextChild, this);
-          } else {
-            targetFront.inlineTextChild = undefined;
-          }
-        }
-
-        emitMutations.push(emittedMutation);
+        this.rootNode = types.getType("domnode").read(change.target, this);
+        this._rootNodeDeferred.resolve(this.rootNode);
+        targetID = this.rootNode.actorID;
+        targetFront = this.rootNode;
+      } else {
+        targetID = change.target;
+        targetFront = this.get(targetID);
       }
 
-      if (options.cleanup) {
-        for (const node of this._orphaned) {
-          // This will move retained nodes to this._retainedOrphans.
-          this._releaseFront(node);
-        }
-        this._orphaned = new Set();
+      if (!targetFront) {
+        console.warn(
+          "Got a mutation for an unexpected actor: " +
+            targetID +
+            ", please file a bug on bugzilla.mozilla.org!"
+        );
+        console.trace();
+        continue;
       }
 
-      this.emit("mutations", emitMutations);
-    });
-    /* eslint-enable complexity */
+      const emittedMutation = Object.assign(change, { target: targetFront });
+
+      if (
+        change.type === "childList" ||
+        change.type === "nativeAnonymousChildList"
+      ) {
+        // Update the ownership tree according to the mutation record.
+        const addedFronts = [];
+        const removedFronts = [];
+        for (const removed of change.removed) {
+          const removedFront = this.get(removed);
+          if (!removedFront) {
+            console.error(
+              "Got a removal of an actor we didn't know about: " + removed
+            );
+            continue;
+          }
+          // Remove from the ownership tree
+          removedFront.reparent(null);
+
+          // This node is orphaned unless we get it in the 'added' list
+          // eventually.
+          this._orphaned.add(removedFront);
+          removedFronts.push(removedFront);
+        }
+        for (const added of change.added) {
+          const addedFront = this.get(added);
+          if (!addedFront) {
+            console.error(
+              "Got an addition of an actor we didn't know " + "about: " + added
+            );
+            continue;
+          }
+          addedFront.reparent(targetFront);
+
+          // The actor is reconnected to the ownership tree, unorphan
+          // it.
+          this._orphaned.delete(addedFront);
+          addedFronts.push(addedFront);
+        }
+
+        // Before passing to users, replace the added and removed actor
+        // ids with front in the mutation record.
+        emittedMutation.added = addedFronts;
+        emittedMutation.removed = removedFronts;
+
+        // If this is coming from a DOM mutation, the actor's numChildren
+        // was passed in. Otherwise, it is simulated from a frame load or
+        // unload, so don't change the front's form.
+        if ("numChildren" in change) {
+          targetFront._form.numChildren = change.numChildren;
+        }
+      } else if (change.type === "frameLoad") {
+        // Nothing we need to do here, except verify that we don't have any
+        // document children, because we should have gotten a documentUnload
+        // first.
+        for (const child of targetFront.treeChildren()) {
+          if (child.nodeType === nodeConstants.DOCUMENT_NODE) {
+            console.warn(
+              "Got an unexpected frameLoad in the inspector, " +
+                "please file a bug on bugzilla.mozilla.org!"
+            );
+            console.trace();
+          }
+        }
+      } else if (change.type === "documentUnload") {
+        if (targetFront === this.rootNode) {
+          this._createRootNodePromise();
+        }
+
+        // We try to give fronts instead of actorIDs, but these fronts need
+        // to be destroyed now.
+        emittedMutation.target = targetFront.actorID;
+        emittedMutation.targetParent = targetFront.parentNode();
+
+        // Release the document node and all of its children, even retained.
+        this._releaseFront(targetFront, true);
+      } else if (change.type === "shadowRootAttached") {
+        targetFront._form.isShadowHost = true;
+      } else if (change.type === "customElementDefined") {
+        targetFront._form.customElementLocation = change.customElementLocation;
+      } else if (change.type === "unretained") {
+        // Retained orphans were force-released without the intervention of
+        // client (probably a navigated frame).
+        for (const released of change.nodes) {
+          const releasedFront = this.get(released);
+          this._retainedOrphans.delete(released);
+          this._releaseFront(releasedFront, true);
+        }
+      } else {
+        targetFront.updateMutation(change);
+      }
+
+      // Update the inlineTextChild property of the target for a selected list of
+      // mutation types.
+      if (
+        change.type === "inlineTextChild" ||
+        change.type === "childList" ||
+        change.type === "shadowRootAttached" ||
+        change.type === "nativeAnonymousChildList"
+      ) {
+        if (change.inlineTextChild) {
+          targetFront.inlineTextChild = types
+            .getType("domnode")
+            .read(change.inlineTextChild, this);
+        } else {
+          targetFront.inlineTextChild = undefined;
+        }
+      }
+
+      emitMutations.push(emittedMutation);
+    }
+
+    if (options.cleanup) {
+      for (const node of this._orphaned) {
+        // This will move retained nodes to this._retainedOrphans.
+        this._releaseFront(node);
+      }
+      this._orphaned = new Set();
+    }
+
+    this.emit("mutations", emitMutations);
   }
+  /* eslint-enable complexity */
 
   /**
    * Handle the `new-mutations` notification by fetching the
