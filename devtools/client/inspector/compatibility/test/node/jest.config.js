3d16270968f61df1d5b9a2d5fdec938cf4e55ba3
Daisuke Akatsuka <daisuke@birchill.co.jp>
2019-10-23 09:08:17 +0000

    Bug 1588964: Add tests for the unsupported browsers. r=rcaliman,ladybenko
    Depends on D49518
    
    Differential Revision: https://phabricator.services.mozilla.com/D49519
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/compatibility/test/node/jest.config.js
+++ devtools/client/inspector/compatibility/test/node/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
   moduleNameMapper: {
     // Map all require("devtools/...") to the real devtools root.
     "^devtools\\/(.*)": `${__dirname}/../../../../../$1`,
+    "^Services": `${__dirname}/fixtures/Services`,
   },
   setupFiles: ["<rootDir>setup.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"],
