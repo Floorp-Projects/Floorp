ca3d4dcc87437416dc3f3781ee058637abbd200b
Daniel Holbert <dholbert@cs.stanford.edu>
2019-10-23 22:05:22 +0000

    Bug 1590639 part 1: Fix non-unified build issues in layout/generic. r=TYLin
    The issues fall into these categories:
     - Files that used StaticPrefs::layout_XYZ() API or gfxVars::XYZ that needed an
       include. (Addressed by adding the missing include.)
     - Files that use mozilla::dom::XYZ or mozilla::gfx::XYZ without qualifying the
       namespace & without a 'using' decl. (Addressed by adding "using".)
     - A few other includes for types/inlines that were used without their header.
    
    Depends on D50162
    
    Differential Revision: https://phabricator.services.mozilla.com/D50163

--- layout/generic/nsSubDocumentFrame.cpp
+++ layout/generic/nsSubDocumentFrame.cpp
@@ -31,6 +31,8 @@
 #include "nsViewManager.h"
 #include "nsGkAtoms.h"
 #include "nsStyleConsts.h"
+#include "nsStyleStruct.h"
+#include "nsStyleStructInlines.h"
 #include "nsFrameSetFrame.h"
 #include "nsIScrollable.h"
 #include "nsNameSpaceManager.h"
@@ -53,8 +55,9 @@
 #include "mozilla/layers/RenderRootStateManager.h"
 
 using namespace mozilla;
+using namespace mozilla::dom;
+using namespace mozilla::gfx;
 using namespace mozilla::layers;
-using mozilla::dom::Document;
 
 static Document* GetDocumentFromView(nsView* aView) {
   MOZ_ASSERT(aView, "null view");
