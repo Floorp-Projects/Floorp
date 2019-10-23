fe38024c4813babf9405e6de7a7f3fb0627d7f19
Mike de Boer <mdeboer@mozilla.com>
2019-10-23 14:59:19 +0000

    Bug 1590775 - Ensure to enable the default private search engine feature for Nightly users only. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D50235

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -412,7 +412,7 @@ pref("permissions.default.geo", 0);
 pref("permissions.default.desktop-notification", 0);
 pref("permissions.default.shortcuts", 0);
 
-#ifdef EARLY_BETA_OR_EARLIER
+#ifdef NIGHTLY_BUILD
   pref("permissions.desktop-notification.postPrompt.enabled", true);
 #else
   pref("permissions.desktop-notification.postPrompt.enabled", false);
