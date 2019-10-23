13e1425f8bb1331d10f1a77b8d92d15896434e60
Mark Banner <standard8@mozilla.com>
2019-10-23 08:48:10 +0000

    Bug 1590404 - Remove unnecessary search order check with SearchService._getEngineInfo and add tests for _getEngineInfo. r=mikedeboer
    The check for an engine being with `this._searchOrder` effectively duplicates that of `engine._isDefault`. We don't need to do both.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50052

--- toolkit/components/search/tests/xpcshell/test_sendSubmissionURL.js
+++ toolkit/components/search/tests/xpcshell/test_sendSubmissionURL.js
@@ -28,6 +28,9 @@ const SUBMISSION_NO = new Map([
 ]);
 
 add_task(async function setup() {
+  useTestEngineConfig("resource://test/data1/");
+  installDistributionEngine();
+
   await AddonTestUtils.promiseStartupManager();
 });
 
@@ -41,14 +44,14 @@ async function addAndMakeDefault(name, searchURL) {
   return engine;
 }
 
-add_task(async function test() {
+add_task(async function test_submission_url_matching() {
   Assert.ok(!Services.search.isInitialized);
   let engineInfo;
   let engine;
 
   for (let [name, searchURL] of SUBMISSION_YES) {
     engine = await addAndMakeDefault(name, searchURL);
-    engineInfo = await Services.search.getDefaultEngineInfo(false);
+    engineInfo = await Services.search.getDefaultEngineInfo();
     Assert.equal(
       engineInfo.defaultSearchEngineData.submissionURL,
       searchURL.replace("{searchTerms}", "")
@@ -58,8 +61,78 @@ add_task(async function test() {
 
   for (let [name, searchURL] of SUBMISSION_NO) {
     engine = await addAndMakeDefault(name, searchURL);
-    engineInfo = await Services.search.getDefaultEngineInfo(false);
+    engineInfo = await Services.search.getDefaultEngineInfo();
     Assert.equal(engineInfo.defaultSearchEngineData.submissionURL, null);
     await Services.search.removeEngine(engine);
   }
+});
+
+add_task(async function test_submission_url_built_in() {
+  const engine = await Services.search.getEngineByName("engine1");
+  await Services.search.setDefault(engine);
+
+  const engineInfo = await Services.search.getDefaultEngineInfo();
+  Assert.equal(
+    engineInfo.defaultSearchEngineData.submissionURL,
+    "https://1.example.com/search?q=",
+    "Should have given the submission url for a built-in engine."
+  );
+});
+
+add_task(async function test_submission_url_distribution() {
+  const engine = Services.search.getEngineByName("bug645970");
+  await Services.search.setDefault(engine);
+
+  const engineInfo = await Services.search.getDefaultEngineInfo();
+  Assert.equal(
+    engineInfo.defaultSearchEngineData.submissionURL,
+    "http://searchtest.local/?search=",
+    "Should have given the submission url for a distribution engine."
+  );
+});
+
+add_task(async function test_submission_url_promoted_by_preference() {
+  const engine = await Services.search.addEngineWithDetails("fakeEngine", {
+    method: "GET",
+    template: "https://fake.example.com/?q={searchTerms}",
+  });
+
+  await Services.search.setDefault(engine);
+
+  let engineInfo = await Services.search.getDefaultEngineInfo();
+  Assert.equal(
+    engineInfo.defaultSearchEngineData.submissionURL,
+    undefined,
+    "Should not have a submission url for a user-based engine"
+  );
+
+  Services.prefs.setCharPref(
+    SearchUtils.BROWSER_SEARCH_PREF + "order.extra.1",
+    "fakeEngine"
+  );
+
+  engineInfo = await Services.search.getDefaultEngineInfo();
+  Assert.equal(
+    engineInfo.defaultSearchEngineData.submissionURL,
+    "https://fake.example.com/?q=",
+    "Should have a submission URL for a pref-promoted user-based engine (extra)"
+  );
+
+  Services.prefs.clearUserPref(
+    SearchUtils.BROWSER_SEARCH_PREF + "order.extra.1"
+  );
+  let localizedStr = Cc["@mozilla.org/pref-localizedstring;1"].createInstance(
+    Ci.nsIPrefLocalizedString
+  );
+  localizedStr.data = "fakeEngine";
+  Services.prefs.setComplexValue(
+    SearchUtils.BROWSER_SEARCH_PREF + "order.1",
+    Ci.nsIPrefLocalizedString,
+    localizedStr
+  );
+  Assert.equal(
+    engineInfo.defaultSearchEngineData.submissionURL,
+    "https://fake.example.com/?q=",
+    "Should have a submission URL for a pref-promoted user-based engine"
+  );
 });
