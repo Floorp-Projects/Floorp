ae93b679de6d6cc2df21aec62a606405b8328407
DimiDL <dlee@mozilla.com>
2019-10-24 14:03:25 +0000

    Bug 1553855 - P2. Fix Safe Browsing testcase errors after introducing LazyIdle thread. r=gcp
    A LazyIdle thread should be created and removed by the same thread. This
    patch fixes testcases that trigger the assertion.
    
    Depends on D49874
    
    Differential Revision: https://phabricator.services.mozilla.com/D49875

--- toolkit/components/url-classifier/tests/gtest/TestUrlClassifierTableUpdateV4.cpp
+++ toolkit/components/url-classifier/tests/gtest/TestUrlClassifierTableUpdateV4.cpp
@@ -169,30 +169,18 @@ static void Clear() {
 }
 
 static void testUpdateFail(TableUpdateArray& tableUpdates) {
-  nsCOMPtr<nsIFile> file;
-  NS_GetSpecialDirectory(NS_APP_USER_PROFILE_50_DIR, getter_AddRefs(file));
-
-  RefPtr<Classifier> classifier = new Classifier();
-  classifier->Open(*file);
-
-  nsresult rv = SyncApplyUpdates(classifier, tableUpdates);
+  nsresult rv = SyncApplyUpdates(tableUpdates);
   ASSERT_TRUE(NS_FAILED(rv));
 }
 
 static void testUpdate(TableUpdateArray& tableUpdates,
                        PrefixStringMap& expected) {
-  nsCOMPtr<nsIFile> file;
-  NS_GetSpecialDirectory(NS_APP_USER_PROFILE_50_DIR, getter_AddRefs(file));
-
   // Force nsUrlClassifierUtils loading on main thread
   // because nsIUrlClassifierDBService will not run in advance
   // in gtest.
   nsUrlClassifierUtils::GetInstance();
 
-  RefPtr<Classifier> classifier = new Classifier();
-  classifier->Open(*file);
-
-  nsresult rv = SyncApplyUpdates(classifier, tableUpdates);
+  nsresult rv = SyncApplyUpdates(tableUpdates);
   ASSERT_TRUE(rv == NS_OK);
   VerifyPrefixSet(expected);
 }
