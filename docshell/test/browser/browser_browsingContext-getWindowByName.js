ef36d5b8514872e90e2d18d90002765bd60f1ef6
Andreas Farre <farre@mozilla.com>
2019-10-24 14:53:07 +0000

    Bug 1575051 - Part 3: Test that nsWindowWatcher can find windows. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D48977

---
+++ docshell/test/browser/browser_browsingContext-getWindowByName.js
@@ -0,0 +1,35 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+function addWindow(name) {
+  var blank = Cc["@mozilla.org/supports-string;1"].createInstance(
+    Ci.nsISupportsString
+  );
+  blank.data = "about:blank";
+  let promise = BrowserTestUtils.waitForNewWindow({
+    anyWindow: true,
+    url: "about:blank",
+  });
+  Services.ww.openWindow(
+    null,
+    AppConstants.BROWSER_CHROME_URL,
+    name,
+    "chrome,dialog=no",
+    blank
+  );
+  return promise;
+}
+
+add_task(async function() {
+  let windows = [await addWindow("first"), await addWindow("second")];
+
+  for (let w of windows) {
+    isnot(w, null);
+    is(Services.ww.getWindowByName(w.name, null), w, `Found ${w.name}`);
+  }
+
+  await Promise.all(windows.map(BrowserTestUtils.closeWindow));
+});
