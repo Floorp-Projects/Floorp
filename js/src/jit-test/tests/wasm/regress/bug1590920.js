533641d81b21686dff44c3baa6cecf082a872c6a
Andy Wingo <wingo@igalia.com>
2019-10-24 09:26:17 +0000

    Bug 1590920 - Fix popBlockResults(Empty) for jump to outer block r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D50448

---
+++ js/src/jit-test/tests/wasm/regress/bug1590920.js
@@ -0,0 +1,11 @@
+let { exports } = new WebAssembly.Instance(new WebAssembly.Module(
+    wasmTextToBinary(`
+      (module
+        (func (export "g")
+          (drop
+            (block (result i32)
+              (i32.clz (i32.const 1))
+              (if
+                (i32.const 1)
+                (return))))))`)));
+exports.g();
