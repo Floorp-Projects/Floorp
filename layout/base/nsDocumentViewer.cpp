5338c8acbbe3986ace4d00d3693d356488451b9b
Brad Werth <bwerth@mozilla.com>
2019-10-24 16:48:14 +0000

    Bug 1561227 Part 1: Make nsDocumentViewer send a new event before setting full zoom levels on the pres contexts. r=emilio
    The PreFullZoomChange event gives listeners an opportunity to capture
    state before the viewport full zoom changes. Responsive Design Mode uses
    this to save and later restore the resolution of the RDM pane.
    
    Differential Revision: https://phabricator.services.mozilla.com/D48621
    
    --HG--
    extra : moz-landing-system : lando

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -2826,6 +2826,14 @@ nsDocumentViewer::SetFullZoom(float aFullZoom) {
   }
 
   bool fullZoomChange = (mPageZoom != aFullZoom);
+
+  // Dispatch PreFullZoomChange event only if fullzoom value really has changed.
+  if (fullZoomChange) {
+    nsContentUtils::DispatchChromeEvent(mDocument, ToSupports(mDocument),
+                                        NS_LITERAL_STRING("PreFullZoomChange"),
+                                        CanBubble::eYes, Cancelable::eYes);
+  }
+
   mPageZoom = aFullZoom;
 
   PropagateToPresContextsHelper(
