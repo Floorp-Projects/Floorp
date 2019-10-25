4bfabde79e8e30e37e18075f339cf81f72207a53
Thom Chiovoloni <tchiovoloni@mozilla.com>
2019-10-25 21:38:01 +0000

    Bug 1238810 - Replace {FXA,WEAVE}_CONFIGURED with entries in the telemetry environment. r=chutten,markh,loines
    Replace {FXA,WEAVE}_CONFIGURED with entries in the telemetry environment
    
    Differential Revision: https://phabricator.services.mozilla.com/D49166

--- services/fxaccounts/tests/xpcshell/test_accounts.js
+++ services/fxaccounts/tests/xpcshell/test_accounts.js
@@ -263,9 +263,6 @@ add_task(async function test_get_signed_in_user_initially_unset() {
   Assert.equal(result, null);
 
   await account._internal.setSignedInUser(credentials);
-  let histogram = Services.telemetry.getHistogramById("FXA_CONFIGURED");
-  Assert.equal(histogram.snapshot().sum, 1);
-  histogram.clear();
 
   // getSignedInUser only returns a subset.
   result = await account.getSignedInUser();
