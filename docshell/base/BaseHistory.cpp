7c0ed40aed0539c76a485fb78039f5c861f45958
Emilio Cobos Álvarez <emilio@crisal.io>
2019-10-25 13:02:16 +0000

    Bug 1590816 - Move History::DispatchNotifyVisited and related code to BaseHistory. r=mak,lina
    Interestingly the android implementation had a potentially serious bug (was
    missing a script blocker).
    
    Differential Revision: https://phabricator.services.mozilla.com/D50263
    
    --HG--
    extra : moz-landing-system : lando

--- docshell/base/BaseHistory.cpp
+++ docshell/base/BaseHistory.cpp
@@ -11,10 +11,59 @@
 
 namespace mozilla {
 
-dom::Document* BaseHistory::GetLinkDocument(dom::Link& aLink) {
+using mozilla::dom::Document;
+using mozilla::dom::Link;
+
+Document* BaseHistory::GetLinkDocument(Link& aLink) {
   Element* element = aLink.GetElement();
   // Element can only be null for mock_Link.
   return element ? element->OwnerDoc() : nullptr;
 }
+
+void BaseHistory::DispatchNotifyVisited(nsIURI* aURI, dom::Document* aDoc) {
+  nsCOMPtr<nsIRunnable> runnable =
+      NewRunnableMethod<nsCOMPtr<nsIURI>, RefPtr<dom::Document>>(
+          "BaseHistory::DispatchNotifyVisited", this,
+          &BaseHistory::NotifyVisitedForDocument, aURI, aDoc);
+  if (aDoc) {
+    aDoc->Dispatch(TaskCategory::Other, runnable.forget());
+  } else {
+    NS_DispatchToMainThread(runnable.forget());
+  }
+}
+
+void BaseHistory::NotifyVisitedForDocument(nsIURI* aURI, dom::Document* aDoc) {
+  MOZ_ASSERT(NS_IsMainThread());
+  // Make sure that nothing invalidates our observer array while we're walking
+  // over it.
+  nsAutoScriptBlocker scriptBlocker;
+
+  // If we have no observers for this URI, we have nothing to notify about.
+  auto entry = mTrackedURIs.Lookup(aURI);
+  if (!entry) {
+    return;
+  }
+
+  TrackedURI& trackedURI = entry.Data();
+
+  {
+    // Update status of each Link node. We iterate over the array backwards so
+    // we can remove the items as we encounter them.
+    ObserverArray::BackwardIterator iter(trackedURI.mLinks);
+    while (iter.HasMore()) {
+      Link* link = iter.GetNext();
+      if (GetLinkDocument(*link) == aDoc) {
+        link->SetLinkState(eLinkState_Visited);
+        iter.Remove();
+      }
+    }
+  }
+
+  // If we don't have any links left, we can remove the array.
+  if (trackedURI.mLinks.IsEmpty()) {
+    entry.Remove();
+  }
+}
+
 
 } // namespace mozilla
