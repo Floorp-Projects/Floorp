da4b2ff371d2205e61654017d3ab85ba4f9b3a28
Andrew McCreight <continuation@gmail.com>
2019-10-28 16:58:17 +0000

    Bug 1591447, part 1 - Copy over and use BrowserTestUtils.addContentEventListener() in browser_bug1058164. r=mconley
    browser_bug1058164 wants to test the behavior of event handling for
    frame scripts using BrowserTestUtils.addContentEventListener(). In bug
    1586393, I'm changing addContentEventListener() to not use frame
    scripts, so this test will no longer be testing that. To work around
    that, I'm making a copy of that method specific to the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50613

--- dom/base/test/browser_bug1058164.js
+++ dom/base/test/browser_bug1058164.js
@@ -11,6 +11,123 @@ SpecialPowers.pushPrefEnv({
 const PAGE =
   "data:text/html,<html><body>A%20regular,%20everyday,%20normal%20page.";
 
+let gListenerId = 0;
+
+/**
+ * Adds a content event listener on the given browser element. NOTE: this test
+ * is checking the behavior of pageshow and pagehide as seen by frame scripts,
+ * so it is specifically implemented using the message message manager.
+ * Similar to BrowserTestUtils.waitForContentEvent, but the listener will fire
+ * until it is removed. A callable object is returned that, when called, removes
+ * the event listener. Note that this function works even if the browser's
+ * frameloader is swapped.
+ *
+ * @param {xul:browser} browser
+ *        The browser element to listen for events in.
+ * @param {string} eventName
+ *        Name of the event to listen to.
+ * @param {function} listener
+ *        Function to call in parent process when event fires.
+ *        Not passed any arguments.
+ * @param {object} listenerOptions [optional]
+ *        Options to pass to the event listener.
+ * @param {function} checkFn [optional]
+ *        Called with the Event object as argument, should return true if the
+ *        event is the expected one, or false if it should be ignored and
+ *        listening should continue. If not specified, the first event with
+ *        the specified name resolves the returned promise. This is called
+ *        within the content process and can have no closure environment.
+ * @param {bool} autoremove [optional]
+ *        Whether the listener should be removed when |browser| is removed
+ *        from the DOM. Note that, if this flag is true, it won't be possible
+ *        to listen for events after a frameloader swap.
+ *
+ * @returns function
+ *        If called, the return value will remove the event listener.
+ */
+function addContentEventListenerWithMessageManager(
+  browser,
+  eventName,
+  listener,
+  listenerOptions = {},
+  checkFn,
+  autoremove = true
+) {
+  let id = gListenerId++;
+  let checkFnSource = checkFn
+    ? encodeURIComponent(escape(checkFn.toSource()))
+    : "";
+
+  // To correctly handle frameloader swaps, we load a frame script
+  // into all tabs but ignore messages from the ones not related to
+  // |browser|.
+
+  /* eslint-disable no-eval */
+  function frameScript(id, eventName, listenerOptions, checkFnSource) {
+    let checkFn;
+    if (checkFnSource) {
+      checkFn = eval(`(() => (${unescape(checkFnSource)}))()`);
+    }
+
+    function listener(event) {
+      if (checkFn && !checkFn(event)) {
+        return;
+      }
+      sendAsyncMessage("ContentEventListener:Run", id);
+    }
+    function removeListener(msg) {
+      if (msg.data == id) {
+        removeMessageListener("ContentEventListener:Remove", removeListener);
+        removeEventListener(eventName, listener, listenerOptions);
+      }
+    }
+    addMessageListener("ContentEventListener:Remove", removeListener);
+    addEventListener(eventName, listener, listenerOptions);
+  }
+  /* eslint-enable no-eval */
+
+  let frameScriptSource = `data:,(${frameScript.toString()})(${id}, "${eventName}", ${uneval(
+    listenerOptions
+  )}, "${checkFnSource}")`;
+
+  let mm = Services.mm;
+
+  function runListener(msg) {
+    if (msg.data == id && msg.target == browser) {
+      listener();
+    }
+  }
+  mm.addMessageListener("ContentEventListener:Run", runListener);
+
+  let needCleanup = true;
+
+  let unregisterFunction = function() {
+    if (!needCleanup) {
+      return;
+    }
+    needCleanup = false;
+    mm.removeMessageListener("ContentEventListener:Run", runListener);
+    mm.broadcastAsyncMessage("ContentEventListener:Remove", id);
+    mm.removeDelayedFrameScript(frameScriptSource);
+    if (autoremove) {
+      Services.obs.removeObserver(cleanupObserver, "message-manager-close");
+    }
+  };
+
+  function cleanupObserver(subject, topic, data) {
+    if (subject == browser.messageManager) {
+      unregisterFunction();
+    }
+  }
+  if (autoremove) {
+    Services.obs.addObserver(cleanupObserver, "message-manager-close");
+  }
+
+  mm.loadFrameScript(frameScriptSource, true);
+
+  return unregisterFunction;
+}
+
 /**
  * Returns a Promise that resolves when it sees a pageshow and
  * pagehide events in a particular order, where each event must
@@ -54,7 +171,7 @@ function prepareForVisibilityEvents(browser, expectedOrder) {
 
     let checkFn = e => e.persisted;
 
-    rmvHide = BrowserTestUtils.addContentEventListener(
+    rmvHide = addContentEventListenerWithMessageManager(
       browser,
       "pagehide",
       () => eventListener("pagehide"),
@@ -62,7 +179,7 @@ function prepareForVisibilityEvents(browser, expectedOrder) {
       checkFn,
       /* autoremove = */ false
     );
-    rmvShow = BrowserTestUtils.addContentEventListener(
+    rmvShow = addContentEventListenerWithMessageManager(
       browser,
       "pageshow",
       () => eventListener("pageshow"),
