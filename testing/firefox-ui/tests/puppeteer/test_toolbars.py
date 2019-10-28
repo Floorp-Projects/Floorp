c25887752473832d4607551721ccf975ffaec598
Henrik Skupin <mail@hskupin.info>
2019-10-28 18:24:57 +0000

    Bug 1587627 - [fxui] Update firefox-puppeteer for removal of anonymous nodes. r=webdriver-reviewers,maja_zf
    The patch removes the usage of elements based on anonymous nodes,
    which are soon to be completely removed. Also code which is no longer
    in use by Firefox-UI tests can simply be removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50803

--- testing/firefox-ui/tests/puppeteer/test_toolbars.py
+++ testing/firefox-ui/tests/puppeteer/test_toolbars.py
@@ -87,12 +87,6 @@ class TestLocationBar(PuppeteerMixin, MarionetteTestCase):
     def test_elements(self):
         self.assertEqual(self.locationbar.urlbar_input.get_property('id'), 'urlbar-input')
 
-        self.assertEqual(self.locationbar.identity_box.get_property('localName'), 'box')
-        self.assertEqual(self.locationbar.identity_country_label.get_property('localName'),
-                         'label')
-        self.assertEqual(self.locationbar.identity_organization_label.get_property('localName'),
-                         'label')
-        self.assertEqual(self.locationbar.identity_icon.get_property('localName'), 'image')
         self.assertEqual(self.locationbar.reload_button.get_property('localName'),
                          'toolbarbutton')
         self.assertEqual(self.locationbar.stop_button.get_property('localName'),
@@ -123,82 +117,3 @@ class TestLocationBar(PuppeteerMixin, MarionetteTestCase):
 
         with self.marionette.using_context('content'):
             Wait(self.marionette).until(lambda mn: mn.get_url() == data_uri)
-
-
-class TestIdentityPopup(PuppeteerMixin, MarionetteTestCase):
-    def setUp(self):
-        super(TestIdentityPopup, self).setUp()
-
-        self.locationbar = self.browser.navbar.locationbar
-        self.identity_popup = self.locationbar.identity_popup
-
-        self.url = 'https://extended-validation.badssl.com'
-
-        with self.marionette.using_context('content'):
-            self.marionette.navigate(self.url)
-
-    def tearDown(self):
-        try:
-            self.identity_popup.close(force=True)
-        finally:
-            super(TestIdentityPopup, self).tearDown()
-
-    def test_elements(self):
-        self.locationbar.open_identity_popup()
-
-        # Test main view elements
-        main = self.identity_popup.view.main
-        self.assertEqual(main.element.get_property('localName'), 'panelview')
-
-        self.assertEqual(main.expander.get_property('localName'), 'button')
-        self.assertEqual(main.insecure_connection_label.get_property('localName'),
-                         'description')
-        self.assertEqual(main.internal_connection_label.get_property('localName'),
-                         'description')
-        self.assertEqual(main.secure_connection_label.get_property('localName'),
-                         'description')
-
-        self.assertEqual(main.permissions.get_property('localName'), 'vbox')
-
-        # Test security view elements
-        security = self.identity_popup.view.security
-        self.assertEqual(security.element.get_property('localName'), 'panelview')
-
-        self.assertEqual(security.host.get_property('localName'), 'label')
-        self.assertEqual(security.insecure_connection_label.get_property('localName'),
-                         'description')
-        self.assertEqual(security.secure_connection_label.get_property('localName'),
-                         'description')
-
-        self.assertEqual(security.owner.get_property('localName'), 'description')
-        self.assertEqual(security.owner_location.get_property('localName'), 'description')
-        self.assertEqual(security.verifier.get_property('localName'), 'description')
-
-        self.assertEqual(security.disable_mixed_content_blocking_button.get_property('localName'),
-                         'button')
-        self.assertEqual(security.enable_mixed_content_blocking_button.get_property('localName'),
-                         'button')
-
-        self.assertEqual(security.more_info_button.get_property('localName'), 'button')
-
-    def test_open_close(self):
-        with self.marionette.using_context('content'):
-            self.marionette.navigate(self.url)
-
-        self.assertFalse(self.identity_popup.is_open)
-
-        self.locationbar.open_identity_popup()
-
-        self.identity_popup.close()
-        self.assertFalse(self.identity_popup.is_open)
-
-    def test_force_close(self):
-        with self.marionette.using_context('content'):
-            self.marionette.navigate(self.url)
-
-        self.assertFalse(self.identity_popup.is_open)
-
-        self.locationbar.open_identity_popup()
-
-        self.identity_popup.close(force=True)
-        self.assertFalse(self.identity_popup.is_open)
