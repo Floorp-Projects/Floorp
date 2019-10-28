63c8c75af49944652ab0d435e3ed7e52c2e119a7
Olli Pettay <Olli.Pettay@helsinki.fi>
2019-10-28 20:50:59 +0000

    Bug 1591334, use faster access to the existing doc in antitracking reporting,r=baku a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D50758

--- toolkit/components/antitracking/AntiTrackingCommon.cpp
+++ toolkit/components/antitracking/AntiTrackingCommon.cpp
@@ -398,12 +398,7 @@ void ReportBlockingToConsole(nsPIDOMWindowOuter* aWindow, nsIURI* aURI,
       aRejectedReason == nsIWebProgressListener::STATE_COOKIES_BLOCKED_ALL ||
       aRejectedReason == nsIWebProgressListener::STATE_COOKIES_BLOCKED_FOREIGN);
 
-  nsCOMPtr<nsIDocShell> docShell = aWindow->GetDocShell();
-  if (NS_WARN_IF(!docShell)) {
-    return;
-  }
-
-  RefPtr<Document> doc = docShell->GetDocument();
+  RefPtr<Document> doc = aWindow->GetExtantDoc();
   if (NS_WARN_IF(!doc)) {
     return;
   }
@@ -483,12 +478,7 @@ void ReportUnblockingToConsole(
     return;
   }
 
-  nsCOMPtr<nsIDocShell> docShell = aWindow->GetDocShell();
-  if (NS_WARN_IF(!docShell)) {
-    return;
-  }
-
-  RefPtr<Document> doc = docShell->GetDocument();
+  RefPtr<Document> doc = aWindow->GetExtantDoc();
   if (NS_WARN_IF(!doc)) {
     return;
   }
