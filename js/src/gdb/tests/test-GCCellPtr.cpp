11fb9268f651efbe0ba4e71716d152fcc977f2a2
Ted Campbell <tcampbell@mozilla.com>
2019-10-28 18:22:42 +0000

    Bug 1592051 - Mark GCCellPtr as JS_HAZ_GC_POINTER. r=sfink
    This also fixes two inconsequential hazards.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50836

--- js/src/gdb/tests/test-GCCellPtr.cpp
+++ js/src/gdb/tests/test-GCCellPtr.cpp
@@ -5,12 +5,11 @@
 #include "js/Symbol.h"
 
 FRAGMENT(GCCellPtr, simple) {
-  JS::GCCellPtr nulll(nullptr);
-
   JS::Rooted<JSObject*> glob(cx, JS::CurrentGlobalOrNull(cx));
   JS::Rooted<JSString*> empty(cx, JS_NewStringCopyN(cx, nullptr, 0));
   JS::Rooted<JS::Symbol*> unique(cx, JS::NewSymbol(cx, nullptr));
 
+  JS::GCCellPtr nulll(nullptr);
   JS::GCCellPtr object(glob.get());
   JS::GCCellPtr string(empty.get());
   JS::GCCellPtr symbol(unique.get());
