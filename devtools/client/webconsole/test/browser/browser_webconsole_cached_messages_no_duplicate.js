2d01b6e914cc4bfdd62229f5144dba31210ebddb
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-10-28 13:08:19 +0000

    Bug 1578138 - Retrieve cached messages before setting new messages event listeners. r=Honza.
    This patch ensures we get the cached messages
    before setting the event listeners so we don't
    risk having duplicated messages (coming from both
    cache and event).
    
    Differential Revision: https://phabricator.services.mozilla.com/D50602
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/webconsole/test/browser/browser_webconsole_cached_messages_no_duplicate.js
@@ -0,0 +1,37 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test to see if we don't get duplicated messages (cached and "live").
+// See Bug 1578138 for more information.
+
+"use strict";
+
+// Log 1 message every 50ms, until we reach 50 messages.
+const TEST_URI = `data:text/html,<meta charset=utf8><script>
+    var i = 0;
+    var intervalId = setInterval(() => {
+      if (i >= 50) {
+        clearInterval(intervalId);
+        intervalId = null;
+        return;
+      }
+      console.log("startup message " + (++i));
+    }, 50);
+  </script>`;
+
+add_task(async function() {
+  info("Add a tab and open the console");
+  const tab = await addTab(TEST_URI, { waitForLoad: false });
+  const hud = await openConsole(tab);
+
+  info("wait until all the messages are displayed");
+  await waitFor(
+    () => findMessage(hud, "message 1") && findMessage(hud, "message 50")
+  );
+
+  is(
+    findMessages(hud, "startup message").length,
+    50,
+    "We have the expected number of messages"
+  );
+});
