f7eef9ebc5ecdd4697ef371bb5d7ee1a5251fbcf
Ehsan Akhgari <ehsan@mozilla.com>
2019-10-28 13:55:56 +0000

    Bug 1591803 - Part 2: Rename nsIPermissionManager.enumerator to all and turn it into an Array; r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D50753
    
    --HG--
    extra : moz-landing-system : lando

--- extensions/permissions/test/unit/test_permmanager_migrate_4-7_no_history.js
+++ extensions/permissions/test/unit/test_permmanager_migrate_4-7_no_history.js
@@ -212,7 +212,7 @@ add_task(function test() {
   Services.obs.notifyObservers(null, "testonly-reload-permissions-from-disk");
 
   // Force initialization of the nsPermissionManager
-  for (let permission of Services.perms.enumerator) {
+  for (let permission of Services.perms.all) {
     let isExpected = false;
 
     expected.forEach((it, i) => {
