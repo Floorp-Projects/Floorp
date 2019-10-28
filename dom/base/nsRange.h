ad3b8519869556a33ea7f48f0485bc286f568180
Mirko Brodesser <mbrodesser@mozilla.com>
2019-10-28 08:44:29 +0000

    Bug 1587433: part 1) Slightly refactor `nsRange::CharacterDataChanged` in order to make it understanable. r=smaug
    The method performed too many tasks. Now, some of the tasks are
    abstracted away, simplifying reasoning about it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50606

--- dom/base/nsRange.h
+++ dom/base/nsRange.h
@@ -306,6 +306,21 @@ class nsRange final : public mozilla::dom::AbstractRange,
    */
   bool CanAccess(const nsINode&) const;
 
+  void AdjustNextRefsOnCharacterDataSplit(const nsIContent& aContent,
+                                          const CharacterDataChangeInfo& aInfo);
+
+  struct RangeBoundariesAndRoot {
+    RawRangeBoundary mStart;
+    RawRangeBoundary mEnd;
+    nsINode* mRoot = nullptr;
+  };
+
+  /**
+   * @param aContent Must be non-nullptr.
+   */
+  RangeBoundariesAndRoot DetermineNewRangeBoundariesAndRootOnCharacterDataMerge(
+      nsIContent* aContent, const CharacterDataChangeInfo& aInfo) const;
+
  public:
   /**
    * Return true if any part of (aNode, aStartOffset) .. (aNode, aEndOffset)
