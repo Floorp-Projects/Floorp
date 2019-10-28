26f6995f1dfde91852fbaf0fe8732fc2fc98709a
Julian Descottes <jdescottes@mozilla.com>
2019-10-28 09:10:29 +0000

    Bug 1588773 - Use ContentDOMReference for context menu Inspect Element r=mconley,pbro
    Depends on D49941
    
    Using ContentDOMReference instead of creating an array of selectors makes inspect element more stable in case the page is modified between after the contextmenu opens.
    It will also make the feature easier to make fission compatible
    
    Differential Revision: https://phabricator.services.mozilla.com/D49303

---
+++ devtools/client/inspector/test/browser_inspector_inspect_mutated_node.js
@@ -0,0 +1,75 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+/* globals getTestActorWithoutToolbox */
+
+"use strict";
+
+// Check that Inspect Element works even if the selector of the inspected
+// element changes after opening the context menu.
+// For this test, we explicitly move the element when opening the context menu.
+
+const TEST_URL =
+  `data:text/html;charset=utf-8,` +
+  encodeURIComponent(`
+
+  <div id="parent-1">
+    <span>Inspect me</span>
+  </div>
+  <div id="parent-2"></div>
+  <div id="changing-id">My ID will change</div>
+
+  <script type="text/javascript">
+    document.querySelector("span").addEventListener("contextmenu", () => {
+      // Right after opening the context menu, move the target element in the
+      // tree to change its selector.
+      window.setTimeout(() => {
+        const span = document.querySelector("span");
+        document.getElementById("parent-2").appendChild(span);
+      }, 0)
+    });
+
+    document.querySelector("#changing-id").addEventListener("contextmenu", () => {
+      // Right after opening the context menu, update the id of #changing-id
+      // to make sure we are not relying on outdated selectors to find the node
+      window.setTimeout(() => {
+        document.querySelector("#changing-id").id= "new-id";
+      }, 0)
+    });
+  </script>
+`);
+
+add_task(async function() {
+  const tab = await addTab(TEST_URL);
+  const testActor = await getTestActorWithoutToolbox(tab);
+
+  await testNodeWithChangingPath(testActor);
+  await testNodeWithChangingId(testActor);
+});
+
+async function testNodeWithChangingPath(testActor) {
+  info("Test that inspect element works if the CSS path changes");
+  const inspector = await clickOnInspectMenuItem(testActor, "span");
+
+  const selectedNode = inspector.selection.nodeFront;
+  ok(selectedNode, "A node is selected in the inspector");
+  is(
+    selectedNode.tagName.toLowerCase(),
+    "span",
+    "The selected node is correct"
+  );
+  const parentNode = await selectedNode.parentNode();
+  is(
+    parentNode.id,
+    "parent-2",
+    "The selected node is under the expected parent node"
+  );
+}
+
+async function testNodeWithChangingId(testActor) {
+  info("Test that inspect element works if the id changes");
+  const inspector = await clickOnInspectMenuItem(testActor, "#changing-id");
+
+  const selectedNode = inspector.selection.nodeFront;
+  ok(selectedNode, "A node is selected in the inspector");
+  is(selectedNode.id.toLowerCase(), "new-id", "The selected node is correct");
+}
