c262b1fd119253c7842858fe606a32860dd4a9e9
André Bargull <andre.bargull@gmail.com>
2019-10-29 09:31:45 +0000

    Bug 1129202 - Part 2: Move IsInteger to jsnum.h to allow reusing it. r=jandem
    Part 5 will call this function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D39036

--- js/src/vm/BigIntType.cpp
+++ js/src/vm/BigIntType.cpp
@@ -1559,29 +1559,8 @@ bool BigInt::literalIsZero(const Range<const CharT> chars) {
 
 template bool BigInt::literalIsZero(const Range<const char16_t> chars);
 
-// BigInt proposal section 5.1.1
-static bool IsInteger(double d) {
-  // Step 1 is an assertion checked by the caller.
-  // Step 2.
-  if (!mozilla::IsFinite(d)) {
-    return false;
-  }
-
-  // Step 3.
-  double i = JS::ToInteger(d);
-
-  // Step 4.
-  if (i != d) {
-    return false;
-  }
-
-  // Step 5.
-  return true;
-}
-
 BigInt* BigInt::createFromDouble(JSContext* cx, double d) {
-  MOZ_ASSERT(::IsInteger(d),
-             "Only integer-valued doubles can convert to BigInt");
+  MOZ_ASSERT(IsInteger(d), "Only integer-valued doubles can convert to BigInt");
 
   if (d == 0) {
     return zero(cx);
@@ -1701,7 +1680,7 @@ BigInt* BigInt::createFromInt64(JSContext* cx, int64_t n) {
 BigInt* js::NumberToBigInt(JSContext* cx, double d) {
   // Step 1 is an assertion checked by the caller.
   // Step 2.
-  if (!::IsInteger(d)) {
+  if (!IsInteger(d)) {
     JS_ReportErrorNumberASCII(cx, GetErrorMessage, nullptr,
                               JSMSG_NUMBER_TO_BIGINT);
     return nullptr;
