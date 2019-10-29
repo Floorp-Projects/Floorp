bf5c481f4357ed5b74b84d78e4830101f2973a2a
André Bargull <andre.bargull@gmail.com>
2019-10-29 09:32:27 +0000

    Bug 1129202 - Part 4: Allow numbers with fractional parts or exponents for canonical numeric indices.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D39038

--- js/src/jit-test/tests/basic/typed-array-index-out-of-range.js
+++ js/src/jit-test/tests/basic/typed-array-index-out-of-range.js
@@ -3,49 +3,167 @@
 
 var x = new Int32Array(10);
 
+// 7.1.16 CanonicalNumericIndexString ( argument )
+function CanonicalNumericIndexString(argument) {
+    // Step 1.
+    assertEq(typeof argument, "string");
+
+    // Step 2.
+    if (argument === "-0") {
+        return -0;
+    }
+
+    // Step 3.
+    var n = Number(argument);
+
+    // Step 4.
+    if (String(n) !== argument) {
+        return undefined;
+    }
+
+    // Step 5.
+    return n;
+}
+
+function CanonicalIndexInRange(argument) {
+    return Number.isInteger(argument) && !Object.is(argument, -0) &&
+           argument >= 0 && argument < x.length;
+}
+
+function assertCanonicalNumericIndexString(i) {
+    var canonical = CanonicalNumericIndexString(i);
+    assertEq(canonical !== undefined, true,
+            `Argument ${i} is not a canonical numeric index string`);
+
+    x[i] = 0; // Clear any previous value.
+    x[i] = 1;
+
+    var expected = CanonicalIndexInRange(canonical) ? 1 : undefined;
+    assertEq(x[i], expected, `${i} is a canonical index string`);
+
+    // Also test when accessing with a number instead of a string.
+
+    x[canonical] = 0; // Clear any previous value.
+    x[canonical] = 2;
+
+    // Add +0 to convert -0 to +0.
+    var expected = CanonicalIndexInRange(canonical + 0) ? 2 : undefined;
+    assertEq(x[canonical], expected, `${i} is a canonical index string`);
+}
+
+function assertNotCanonicalNumericIndexString(i) {
+    assertEq(CanonicalNumericIndexString(i) === undefined, true,
+             `Argument ${i} is a canonical numeric index string`);
+
+    x[i] = ""; // Clear any previous value.
+    x[i] = "test";
+
+    assertEq(x[i], "test", `${i} isn't a canonical index string`);
+}
+
 function f() {
     for (var i = -100; i < 100; i++) {
-	x[i] = i + 1;
-	if (i >= 0 && i < 10)
-	    assertEq(x[i], i + 1);
-	else
-	    assertEq(x[i], undefined);
+        assertCanonicalNumericIndexString(String(i));
     }
 }
 f();
 
-// Really big integers not representable with a double or uint64 are still integers.
+// INT32_MAX and UINT32_MAX are canonical numeric indices.
 
-var bigint = "" + Math.pow(2, 53);
-x[bigint] = "twelve";
-assertEq(x[bigint], undefined);
+assertCanonicalNumericIndexString("2147483647");
+assertCanonicalNumericIndexString("4294967295");
 
-x["9999999999999999999999"] = "twelve";
-assertEq(x["9999999999999999999999"], undefined);
+// Neither INT64_MAX nor UINT64_MAX are canonical numeric indices.
 
-// Except when their toString() makes them not look like integers!
-x[9999999999999999999999] = "twelve";
-assertEq(x[9999999999999999999999], "twelve");
+assertNotCanonicalNumericIndexString("9223372036854775807");
+assertNotCanonicalNumericIndexString("18446744073709551615");
+
+// "18446744073709552000" and "18446744073709556000" are canonical numeric indices.
+
+assertEq(String(18446744073709551615), "18446744073709552000");
+
+assertCanonicalNumericIndexString("18446744073709552000");
+assertCanonicalNumericIndexString("18446744073709556000");
+
+// Number.MAX_SAFE_INTEGER and Number.MAX_SAFE_INTEGER + 1 are canonical numeric
+// indices, but Number.MAX_SAFE_INTEGER + 2 is not.
+
+assertEq(String(Number.MAX_SAFE_INTEGER), "9007199254740991");
+
+assertCanonicalNumericIndexString("9007199254740991");
+assertCanonicalNumericIndexString("9007199254740992");
+assertNotCanonicalNumericIndexString("9007199254740993");
+
+// Number.MIN_SAFE_INTEGER and Number.MIN_SAFE_INTEGER - 1 are canonical numeric
+// indices, but Number.MIN_SAFE_INTEGER - 2 is not.
+
+assertEq(String(Number.MIN_SAFE_INTEGER), "-9007199254740991");
+
+assertCanonicalNumericIndexString("-9007199254740991");
+assertCanonicalNumericIndexString("-9007199254740992");
+assertNotCanonicalNumericIndexString("-9007199254740993");
+
+// -0 is a canonical numeric index.
+
+assertCanonicalNumericIndexString("-0");
+
+// 0.1 and -0.1 are canonical numeric indices.
+
+assertCanonicalNumericIndexString("0.1");
+assertCanonicalNumericIndexString("-0.1");
+
+// 0.10 and -0.10 aren't canonical numeric indices.
+
+assertNotCanonicalNumericIndexString("0.10");
+assertNotCanonicalNumericIndexString("-0.10");
+
+// Number.MIN_VALUE and -Number.MIN_VALUE are canonical numeric indices.
+
+assertEq(String(Number.MIN_VALUE), "5e-324");
+assertEq(String(-Number.MIN_VALUE), "-5e-324");
+
+assertCanonicalNumericIndexString("5e-324");
+assertCanonicalNumericIndexString("-5e-324");
+
+assertNotCanonicalNumericIndexString("5E-324");
+assertNotCanonicalNumericIndexString("-5E-324");
+
+// 1e2, -1e2, 1e-2, and -1e-2 aren't canonical numeric indices.
+
+assertNotCanonicalNumericIndexString("1e2");
+assertNotCanonicalNumericIndexString("-1e2");
+assertNotCanonicalNumericIndexString("1e+2");
+assertNotCanonicalNumericIndexString("-1e+2");
+assertNotCanonicalNumericIndexString("1e-2");
+assertNotCanonicalNumericIndexString("-1e-2");
+
+assertNotCanonicalNumericIndexString("1E2");
+assertNotCanonicalNumericIndexString("-1E2");
+assertNotCanonicalNumericIndexString("1E+2");
+assertNotCanonicalNumericIndexString("-1E+2");
+assertNotCanonicalNumericIndexString("1E-2");
+assertNotCanonicalNumericIndexString("-1E-2");
+
+// Number.MAX_VALUE and -Number.MAX_VALUE are canonical numeric indices.
+
+assertEq(String(Number.MAX_VALUE), "1.7976931348623157e+308");
+assertEq(String(-Number.MAX_VALUE), "-1.7976931348623157e+308");
+
+assertCanonicalNumericIndexString("1.7976931348623157e+308");
+assertCanonicalNumericIndexString("-1.7976931348623157e+308");
+
+assertNotCanonicalNumericIndexString("1.7976931348623157E+308");
+assertNotCanonicalNumericIndexString("-1.7976931348623157E+308");
 
 // Infinity, -Infinity, and NaN are canonical numeric indices and are handled
 // the same way as other out-of-bounds accesses.
 
-x["Infinity"] = "twelve";
-assertEq(x["Infinity"], undefined);
+assertCanonicalNumericIndexString("Infinity");
+assertCanonicalNumericIndexString("-Infinity");
+assertCanonicalNumericIndexString("NaN");
 
-x["-Infinity"] = "twelve";
-assertEq(x["-Infinity"], undefined);
+// +Infinity, +NaN, or -NaN are not canonical numeric indices.
 
-x["NaN"] = "twelve";
-assertEq(x["NaN"], undefined);
-
-// +Infinity, +NaN, or -NaN are not integers.
-
-x["+Infinity"] = "twelve";
-assertEq(x["+Infinity"], "twelve");
-
-x["+NaN"] = "twelve";
-assertEq(x["+NaN"], "twelve");
-
-x["-NaN"] = "twelve";
-assertEq(x["-NaN"], "twelve");
+assertNotCanonicalNumericIndexString("+Infinity");
+assertNotCanonicalNumericIndexString("+NaN");
+assertNotCanonicalNumericIndexString("-NaN");
