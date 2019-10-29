bcb5611ee6c93d41c8ffdf261c198c5f6c3af658
alwu <alwu@mozilla.com>
2019-10-29 23:08:05 +0000

    Bug 1591608 - part3 : follow the new naming rule for define guards. r=chunmin
    Per [1], we should follow our coding style.
    
    [1] https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Coding_Style#CC_practices
    
    Differential Revision: https://phabricator.services.mozilla.com/D50683

--- dom/media/mediacontrol/MediaHardwareKeysEventSourceMac.h
+++ dom/media/mediacontrol/MediaHardwareKeysEventSourceMac.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_mediahardwarekeyseventmac_h__
-#define mozilla_dom_mediahardwarekeyseventmac_h__
+#ifndef DOM_MEDIA_MEDIACONTROL_MEDIAHARDWAREKEYSEVENTSOURCEMAC_H_
+#define DOM_MEDIA_MEDIACONTROL_MEDIAHARDWAREKEYSEVENTSOURCEMAC_H_
 
 #import <ApplicationServices/ApplicationServices.h>
 #import <CoreFoundation/CoreFoundation.h>
