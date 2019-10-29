29cef09b45191d2ef73231ed0efd18ceefe411a1
Gurzau Raul <rgurzau@mozilla.com>
2019-10-30 00:56:04 +0200

    Backed out 2 changesets (bug 1591013) for eslint failure at inspector/node.js on a CLOSED TREE.
    Backed out changeset 5d53ab2f3152 (bug 1591013)
    Backed out changeset d888aded0e70 (bug 1591013)

--- tools/lint/eslint/eslint-plugin-mozilla/lib/rules/reject-some-requires.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/rules/reject-some-requires.js
@@ -40,8 +40,7 @@ module.exports = function(context) {
       } else if (
         node.callee.type == "MemberExpression" &&
         node.callee.property.type == "Identifier" &&
-        (node.callee.property.name == "lazyRequireGetter" ||
-          node.callee.property.name == "lazyImporter") &&
+        node.callee.property.name == "lazyRequireGetter" &&
         node.arguments.length >= 3 &&
         node.arguments[2].type == "Literal"
       ) {
