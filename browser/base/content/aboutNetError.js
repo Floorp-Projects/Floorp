a11c674cb4a0fbf84725eb483033511fee260ee8
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2019-10-30 17:54:36 +0000

    Bug 1584998: Make x-frame-options work with fission enabled. r=jkt,farre,johannh
    Differential Revision: https://phabricator.services.mozilla.com/D50588

--- browser/base/content/aboutNetError.js
+++ browser/base/content/aboutNetError.js
@@ -259,9 +259,9 @@ function initPage() {
     document.getElementById("netErrorButtonContainer").style.display = "none";
   }
 
-  if (err == "cspBlocked") {
-    // Remove the "Try again" button for CSP violations, since it's
-    // almost certainly useless. (Bug 553180)
+  if (err == "cspBlocked" || err == "xfoBlocked") {
+    // Remove the "Try again" button for XFO and CSP violations,
+    // since it's almost certainly useless. (Bug 553180)
     document.getElementById("netErrorButtonContainer").style.display = "none";
   }
 
