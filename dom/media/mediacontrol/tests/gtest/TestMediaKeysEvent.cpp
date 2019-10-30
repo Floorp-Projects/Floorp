4091db653e1f611669704f12652c63b4368880a8
alwu <alwu@mozilla.com>
2019-10-30 09:52:04 +0000

    Bug 1591620 - part1 : make 'MediaControlKeysEventListener' as a pure interface. r=chunmin
    If the web extention also needs to implement its listener, they would definitely not need the current implementation in `KeyPressed()` for controlling media controller. Therefore, we should make `MediaControlKeysEventListener` as a pure interface and let inherited class to define their own `KeyPressed()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50686

--- dom/media/mediacontrol/tests/gtest/TestMediaKeysEvent.cpp
+++ dom/media/mediacontrol/tests/gtest/TestMediaKeysEvent.cpp
@@ -14,7 +14,7 @@ TEST(MediaControlKeysEvent, TestAddOrRemoveListener)
   ASSERT_TRUE(source->GetListenersNum() == 0);
 
   RefPtr<MediaControlKeysEventListener> listener =
-      new MediaControlKeysEventListener();
+      new MediaControlKeysHandler();
 
   source->AddListener(listener);
   ASSERT_TRUE(source->GetListenersNum() == 1);
