5870176253b6bc9a1dae42d78abf3c34e68942d3
Gregory Mierzwinski <gmierz2@outlook.com>
2019-10-30 20:32:19 +0000

    Bug 1592401 - Prevent verbose output when unpacking browsertime. r=perftest-reviewers,rwood
    This patch changes the browsertime archive built as a toolchain from a .zip archive to a .tar.bz2 archive to prevent the large amount of verbose unpacking output we currently see.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50995
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/scripts/misc/browsertime.sh
+++ taskcluster/scripts/misc/browsertime.sh
@@ -13,8 +13,7 @@ export PATH=$PATH:$MOZ_FETCHES_DIR/node/bin
 ./mach browsertime --setup
 
 # We have tools/browsertime/{package.json,node_modules,...} and want
-# browsertime/{package.json,node_modules}.  ZIP because generic-worker
-# doesn't support .tar.xz.
+# browsertime/{package.json,node_modules}.
 mkdir -p  /builds/worker/artifacts
 cd tools
-zip -r /builds/worker/artifacts/browsertime.zip browsertime
+tar cjf /builds/worker/artifacts/browsertime.tar.bz2 browsertime
