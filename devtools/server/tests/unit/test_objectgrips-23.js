b4c496de6bc24840295b955b9820c43b6a0cc482
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-10-30 10:03:01 +0000

    Bug 1351635 - Return isClassConstructor in class grip. r=loganfsmyth.
    A test is added to ensure isClassConstructor has the expected
    value for ES6 class grips.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50962

---
+++ devtools/server/tests/unit/test_objectgrips-23.js
@@ -0,0 +1,43 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that ES6 classes grip have the expected properties.
+
+"use strict";
+
+Services.prefs.setBoolPref("security.allow_eval_with_system_principal", true);
+registerCleanupFunction(() => {
+  Services.prefs.clearUserPref("security.allow_eval_with_system_principal");
+});
+
+add_task(
+  threadFrontTest(async ({ threadFront, debuggee }) => {
+    await new Promise(function(resolve) {
+      threadFront.once("paused", async function(packet) {
+        const [grip] = packet.frame.arguments;
+        strictEqual(
+          grip.class,
+          "Function",
+          `Grip has expected value for "class" property`
+        );
+        strictEqual(
+          grip.isClassConstructor,
+          true,
+          `Grip has expected value for "isClassConstructor" property`
+        );
+
+        await threadFront.resume();
+        resolve();
+      });
+
+      debuggee.eval(`
+        class MyClass {};
+        stopMe(MyClass);
+
+        function stopMe(arg1) {
+          debugger;
+        }
+      `);
+    });
+  })
+);
