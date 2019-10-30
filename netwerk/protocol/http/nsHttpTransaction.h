cb40e1ed6f2bb07d55e5be8037877228bfedc6ee
Dragana Damjanovic <dd.mozilla@gmail.com>
2019-10-30 20:55:38 +0000

    Bug 1581637 - Part 10 - Implement a posibility to use alt-svc on transaction restart in some cases. r=mayhemer mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D46654

--- netwerk/protocol/http/nsHttpTransaction.h
+++ netwerk/protocol/http/nsHttpTransaction.h
@@ -142,6 +142,8 @@ class nsHttpTransaction final : public nsAHttpTransaction,
 
   void DisableSpdy() override;
 
+  void DoNotRemoveAltSvc() override { mDoNotRemoveAltSvc = true; }
+
   nsHttpTransaction* QueryHttpTransaction() override { return this; }
 
   already_AddRefed<Http2PushedStreamWrapper> GetPushedStream() {
@@ -378,6 +380,8 @@ class nsHttpTransaction final : public nsAHttpTransaction,
   bool mContentDecodingCheck;
   bool mDeferredSendProgress;
   bool mWaitingOnPipeOut;
+
+  bool mDoNotRemoveAltSvc;
 
   // mClosed           := transaction has been explicitly closed
   // mTransactionDone  := transaction ran to completion or was interrupted
