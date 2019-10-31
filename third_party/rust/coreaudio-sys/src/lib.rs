74519ef1740fc9a82e6c4224952534345571b2ac
Chun-Min Chang <chun.m.chang@gmail.com>
2019-10-31 20:14:00 +0000

    Bug 1591249 - Bump coreaudio-sys to 0.2.3. r=glandium
    The current coreaudio-sys in gecko is a custom 0.2.2 version that used
    to avoid the cross-compiling issue mentioned in bug 1569003. The issue
    has been fixed in the coreaudio-sys 0.2.3, so we should follow the
    upstream instead of using a custom version. As a result, the
    coreaudio-sys would generate API bindings based on the MacOS SDK defined
    in the build settings.
    
    Differential Revision: https://phabricator.services.mozilla.com/D50531

--- third_party/rust/coreaudio-sys/src/lib.rs
+++ third_party/rust/coreaudio-sys/src/lib.rs
@@ -1,9 +1,6 @@
+#![cfg(any(target_os = "macos", target_os = "ios"))]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
-#[cfg(fullback_bindings)]
-include!("fullback_coreaudio.rs");
-
-#[cfg(not(fullback_bindings))]
 include!(concat!(env!("OUT_DIR"), "/coreaudio.rs"));
