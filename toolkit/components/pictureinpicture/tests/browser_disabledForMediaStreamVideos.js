9f9b5ce908d7e3d5cb4c033c9672b698937204ab
Mike Conley <mconley@mozilla.com>
2019-10-31 15:51:27 +0000

    Bug 1592729 - Add tests to ensure that the Picture-in-Picture toggle and context menus do not display for MediaStream videos. r=Gijs a=pascalc
    This also updates the canToggle=false code paths in the Picture-in-Picture
    mochitest head.js file, which were obviously never really exercised until
    now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51160

---
+++ toolkit/components/pictureinpicture/tests/browser_disabledForMediaStreamVideos.js
@@ -0,0 +1,49 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Due to bug 1592539, we've disabled the Picture-in-Picture toggle and
+ * context menu item for <video> elements with srcObject set to anything
+ * other than null. We will re-enable Picture-in-Picture support for these
+ * these types of video elements when the underlying bug is fixed.
+ */
+add_task(async function test_disabledForMediaStreamVideos() {
+  // Disabling of the toggle on MediaStream videos is overridden by the always-show
+  // pref.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      [
+        "media.videocontrols.picture-in-picture.video-toggle.always-show",
+        false,
+      ],
+    ],
+  });
+
+  await testToggle(
+    TEST_PAGE,
+    {
+      "with-controls": { canToggle: false },
+      "no-controls": { canToggle: false },
+    },
+    async browser => {
+      await ContentTask.spawn(browser, null, async () => {
+        // Construct a new video element, and capture a stream from it
+        // to redirect to both testing videos
+        let newVideo = content.document.createElement("video");
+        newVideo.src = "test-video.mp4";
+        content.document.body.appendChild(newVideo);
+        newVideo.loop = true;
+        await newVideo.play();
+
+        for (let videoID of ["with-controls", "no-controls"]) {
+          let testedVideo = content.document.getElementById(videoID);
+          testedVideo.srcObject = newVideo.mozCaptureStream();
+          await testedVideo.play();
+          await testedVideo.pause();
+        }
+      });
+    }
+  );
+});
