4404c0826ab76f9c990c0cd5d41b966084613e77
sotaro <sotaro.ikeda.g@gmail.com>
2019-10-31 11:59:17 +0000

    Bug 1591004 - First pass of OS compositor implementation on Windows r=gw
    This implementation is off by default (behind gfx.webrender.compositor) until various bugs and inefficiencies are fixed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51034
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/webrender_bindings/DCLayerTree.h
+++ gfx/webrender_bindings/DCLayerTree.h
@@ -7,40 +7,81 @@
 #ifndef MOZILLA_GFX_DCLAYER_TREE_H
 #define MOZILLA_GFX_DCLAYER_TREE_H
 
+#include <unordered_map>
 #include <windows.h>
 
+#include "GLTypes.h"
+#include "mozilla/Maybe.h"
 #include "mozilla/RefPtr.h"
 #include "mozilla/UniquePtr.h"
+#include "mozilla/webrender/WebRenderTypes.h"
 
+struct ID3D11Device;
+struct ID3D11DeviceContext;
 struct IDCompositionDevice2;
+struct IDCompositionSurface;
 struct IDCompositionTarget;
 struct IDCompositionVisual2;
 struct IDXGISwapChain1;
 
 namespace mozilla {
 
+namespace gl {
+class GLContext;
+}
+
 namespace wr {
 
 // Currently, MinGW build environment does not handle IDCompositionDesktopDevice
 // and IDCompositionDevice2
 #if !defined(__MINGW32__)
+
+class DCLayer;
+
 /**
  * DCLayerTree manages direct composition layers.
  * It does not manage gecko's layers::Layer.
  */
 class DCLayerTree {
  public:
-  static UniquePtr<DCLayerTree> Create(HWND aHwnd);
-  explicit DCLayerTree(IDCompositionDevice2* aCompositionDevice);
+  static UniquePtr<DCLayerTree> Create(gl::GLContext* aGL, EGLConfig aEGLConfig,
+                                       ID3D11Device* aDevice, HWND aHwnd);
+  explicit DCLayerTree(gl::GLContext* aGL, EGLConfig aEGLConfig,
+                       ID3D11Device* aDevice,
+                       IDCompositionDevice2* aCompositionDevice);
   ~DCLayerTree();
 
   void SetDefaultSwapChain(IDXGISwapChain1* aSwapChain);
   void MaybeUpdateDebug();
 
+  // Interface for wr::Compositor
+  void CompositorBeginFrame();
+  void CompositorEndFrame();
+  void Bind(wr::NativeSurfaceId aId, wr::DeviceIntPoint* aOffset,
+            uint32_t* aFboId, wr::DeviceIntRect aDirtyRect);
+  void Unbind();
+  void CreateSurface(wr::NativeSurfaceId aId, wr::DeviceIntSize aSize,
+                     bool aIsOpaque);
+  void DestroySurface(NativeSurfaceId aId);
+  void AddSurface(wr::NativeSurfaceId aId, wr::DeviceIntPoint aPosition,
+                  wr::DeviceIntRect aClipRect);
+
+  gl::GLContext* GetGLContext() const { return mGL; }
+  EGLConfig GetEGLConfig() const { return mEGLConfig; }
+  ID3D11Device* GetDevice() const { return mDevice; }
+  IDCompositionDevice2* GetCompositionDevice() const {
+    return mCompositionDevice;
+  }
+
  protected:
   bool Initialize(HWND aHwnd);
   bool MaybeUpdateDebugCounter();
   bool MaybeUpdateDebugVisualRedrawRegions();
+
+  RefPtr<gl::GLContext> mGL;
+  EGLConfig mEGLConfig;
+
+  RefPtr<ID3D11Device> mDevice;
 
   RefPtr<IDCompositionDevice2> mCompositionDevice;
   RefPtr<IDCompositionTarget> mCompositionTarget;
@@ -49,13 +90,63 @@ class DCLayerTree {
 
   bool mDebugCounter;
   bool mDebugVisualRedrawRegions;
+
+  Maybe<wr::NativeSurfaceId> mCurrentId;
+
+  std::unordered_map<uint64_t, UniquePtr<DCLayer>> mDCLayers;
 };
+
+class DCLayer {
+ public:
+  explicit DCLayer(DCLayerTree* aDCLayerTree);
+  ~DCLayer();
+  bool Initialize(wr::DeviceIntSize aSize);
+  bool CreateEGLSurfaceForCompositionSurface(wr::DeviceIntPoint* aOffset);
+  void EndDraw();
+
+  IDCompositionSurface* GetCompositionSurface() const {
+    return mCompositionSurface;
+  }
+  EGLSurface GetEGLSurface() const { return mEGLSurface; }
+  IDCompositionVisual2* GetVisual() const { return mVisual; }
+
+ protected:
+  RefPtr<IDCompositionSurface> CreateCompositionSurface(wr::DeviceIntSize aSize,
+                                                        bool aUseAlpha);
+  void DestroyEGLSurface();
+
+ protected:
+  DCLayerTree* mDCLayerTree;
+
+  RefPtr<IDCompositionSurface> mCompositionSurface;
+
+  LayoutDeviceIntSize mBufferSize;
+  EGLSurface mEGLSurface;
+
+  RefPtr<IDCompositionVisual2> mVisual;
+};
+
 #else
 class DCLayerTree {
  public:
-  static UniquePtr<DCLayerTree> Create(HWND aHwnd) { return nullptr; }
+  static UniquePtr<DCLayerTree> Create(gl::GLContext* aGL, EGLConfig aEGLConfig,
+                                       ID3D11Device* aDevice, HWND aHwnd) {
+    return nullptr;
+  }
   void SetDefaultSwapChain(IDXGISwapChain1* aSwapChain) {}
   void MaybeUpdateDebug() {}
+
+  // Interface for wr::Compositor
+  void CompositorBeginFrame() {}
+  void CompositorEndFrame() {}
+  void Bind(wr::NativeSurfaceId aId, wr::DeviceIntPoint* aOffset,
+            uint32_t* aFboId, wr::DeviceIntRect aDirtyRect) {}
+  void Unbind() {}
+  void CreateSurface(wr::NativeSurfaceId aId, wr::DeviceIntSize aSize,
+                     bool aIsOpaque) {}
+  void DestroySurface(NativeSurfaceId aId) {}
+  void AddSurface(wr::NativeSurfaceId aId, wr::DeviceIntPoint aPosition,
+                  wr::DeviceIntRect aClipRect) {}
 };
 #endif
 
