ca5176f7cc8a7142b37e3ba8be7fce0261fe24e9
Alexander J. Vincent <ajvincent@gmail.com>
2019-10-31 14:05:46 +0000

    Bug 1094939 - Preferences actor should throw with the name for an unknown preference.  r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D50728
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/preference.js
+++ devtools/server/actors/preference.js
@@ -4,10 +4,18 @@
 
 "use strict";
 
-const { Ci } = require("chrome");
 const protocol = require("devtools/shared/protocol");
 const Services = require("Services");
 const { preferenceSpec } = require("devtools/shared/specs/preference");
+
+const { PREF_STRING, PREF_INT, PREF_BOOL } = Services.prefs;
+
+function ensurePrefType(name, expectedType) {
+  const type = Services.prefs.getPrefType(name);
+  if (type !== expectedType) {
+    throw new Error(`preference is not of the right type: ${name}`);
+  }
+}
 
 /**
  * Normally the preferences are set using Services.prefs, but this actor allows
@@ -23,14 +31,17 @@ var PreferenceActor = protocol.ActorClassWithSpec(preferenceSpec, {
   typeName: "preference",
 
   getBoolPref: function(name) {
+    ensurePrefType(name, PREF_BOOL);
     return Services.prefs.getBoolPref(name);
   },
 
   getCharPref: function(name) {
+    ensurePrefType(name, PREF_STRING);
     return Services.prefs.getCharPref(name);
   },
 
   getIntPref: function(name) {
+    ensurePrefType(name, PREF_INT);
     return Services.prefs.getIntPref(name);
   },
 
@@ -41,13 +52,13 @@ var PreferenceActor = protocol.ActorClassWithSpec(preferenceSpec, {
       try {
         let value;
         switch (Services.prefs.getPrefType(name)) {
-          case Ci.nsIPrefBranch.PREF_STRING:
+          case PREF_STRING:
             value = Services.prefs.getCharPref(name);
             break;
-          case Ci.nsIPrefBranch.PREF_INT:
+          case PREF_INT:
             value = Services.prefs.getIntPref(name);
             break;
-          case Ci.nsIPrefBranch.PREF_BOOL:
+          case PREF_BOOL:
             value = Services.prefs.getBoolPref(name);
             break;
           default:
