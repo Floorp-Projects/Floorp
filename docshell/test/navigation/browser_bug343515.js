cd89f55ee3fd8752ea15db00dfacd9433ab0d05c
Andrew McCreight <continuation@gmail.com>
2019-10-31 02:47:08 +0000

    Bug 1592465 - Modernize browser_bug343515.js. r=bzbarsky
    I rewrote BrowserUtils.addContentEventListener(), and that caused an
    intermittent failure in browser_bug343515.js. The use of
    addContentEventListener in the test is questionable, and the test as a
    whole is old, so I freshened it up.
    
    Instead of nShotsListener (and oneShotListener, which is a special
    case of that) which waits for a hard coded number of load events to
    fire to ensure that a page and its subframes are all loaded, I use
    BrowserTestUtils.browserLoaded().
    
    Instead of waitForPageshow, which runs a script in the content process
    that waits for an event, it uses BrowserTestUtils.waitForContentEvent,
    which does the same thing in a hopefully more resilient fashion.
    
    The largest change in terms of number of characters changed is that I
    rewrote the test so that it is written inside of a single add_task
    async function, instead of being split into a chain of async
    functions. I also used await to flatten a lot of .then() chained
    things.
    
    I fixed up some variable shadowing. There was some trickiness around
    shadowed variable names being used as field names, so I introduced an
    intermediate 'data' variable to hold the structure.
    
    Finally, I call BrowserTestUtils.removeTab() instead of calling
    removeTab() directly on the browser. I figured that was probably
    better, though maybe it does not matter.
    
    I added a check after each step that the URI is correct, where
    missing, which would have caught a bug in an earlier version of this
    patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51183

--- docshell/test/navigation/browser_bug343515.js
+++ docshell/test/navigation/browser_bug343515.js
@@ -4,50 +4,9 @@
 var testPath = "http://mochi.test:8888/browser/docshell/test/navigation/";
 var ctx = {};
 
-// We need to wait until the page from each testcase is fully loaded,
-// including all of its descendant iframes. To do that we manually count
-// how many load events should happen on that page (one for the toplevel doc
-// and one for each subframe) and wait until we receive the expected number
-// of events.
-function nShotsListener(aBrowser, aType, aCallback, aCount) {
-  let count = aCount;
-  let removeFunc;
-  removeFunc = BrowserTestUtils.addContentEventListener(
-    aBrowser,
-    aType,
-    function listenerCallback() {
-      if (--count == 0) {
-        removeFunc();
+add_task(async function() {
+  // Step 1.
 
-        // aCallback is executed asynchronously, which is handy because load
-        // events fire before mIsDocumentLoaded is actually set to true. :(
-        executeSoon(aCallback);
-      }
-    },
-    { capture: true }
-  );
-}
-
-function oneShotListener(aBrowser, aType, aCallback) {
-  nShotsListener(aBrowser, aType, aCallback, 1);
-}
-
-function waitForPageshow(aBrowser, callback) {
-  return ContentTask.spawn(aBrowser, null, async function() {
-    await ContentTaskUtils.waitForEvent(this, "pageshow");
-  }).then(callback);
-}
-
-// Entry point from Mochikit
-function test() {
-  // Lots of callbacks going on here
-  waitForExplicitFinish();
-
-  // Begin the test
-  step1();
-}
-
-async function step1() {
   // Get a handle on the initial tab
   ctx.tab0 = gBrowser.selectedTab;
   ctx.tab0Browser = gBrowser.getBrowserForTab(ctx.tab0);
@@ -60,10 +19,9 @@ async function step1() {
   // Open a New Tab
   ctx.tab1 = BrowserTestUtils.addTab(gBrowser, testPath + "bug343515_pg1.html");
   ctx.tab1Browser = gBrowser.getBrowserForTab(ctx.tab1);
-  oneShotListener(ctx.tab1Browser, "load", step2);
-}
+  await BrowserTestUtils.browserLoaded(ctx.tab1Browser);
 
-function step2() {
+  // Step 2.
   is(
     testPath + "bug343515_pg1.html",
     ctx.tab1Browser.currentURI.spec,
@@ -75,25 +33,19 @@ function step2() {
   ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should not be active");
 
   // Switch to tab 1
-  BrowserTestUtils.switchTab(gBrowser, ctx.tab1).then(() => {
-    // Tab 1 should now be active
-    ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
-    ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
+  await BrowserTestUtils.switchTab(gBrowser, ctx.tab1);
 
-    // Open another tab
-    ctx.tab2 = BrowserTestUtils.addTab(
-      gBrowser,
-      testPath + "bug343515_pg2.html"
-    );
-    ctx.tab2Browser = gBrowser.getBrowserForTab(ctx.tab2);
+  // Tab 1 should now be active
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
 
-    // bug343515_pg2.html consists of a page with two iframes,
-    // which will therefore generate 3 load events.
-    nShotsListener(ctx.tab2Browser, "load", step3, 3);
-  });
-}
+  // Open another tab
+  ctx.tab2 = BrowserTestUtils.addTab(gBrowser, testPath + "bug343515_pg2.html");
+  ctx.tab2Browser = gBrowser.getBrowserForTab(ctx.tab2);
 
-function step3() {
+  await BrowserTestUtils.browserLoaded(ctx.tab2Browser);
+
+  // Step 3.
   is(
     testPath + "bug343515_pg2.html",
     ctx.tab2Browser.currentURI.spec,
@@ -106,27 +58,28 @@ function step3() {
 
   // Tab 2's window _and_ its iframes should be inactive
   ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
-  ContentTask.spawn(ctx.tab2Browser, null, async function() {
+
+  await ContentTask.spawn(ctx.tab2Browser, null, async function() {
     Assert.equal(content.frames.length, 2, "Tab 2 should have 2 iframes");
     for (var i = 0; i < content.frames.length; i++) {
       info("step 3, frame " + i + " info: " + content.frames[i].location);
       let docShell = content.frames[i].docShell;
       Assert.ok(!docShell.isActive, `Tab2 iframe ${i} should be inactive`);
     }
-  }).then(() => {
-    // Navigate tab 2 to a different page
-    BrowserTestUtils.loadURI(ctx.tab2Browser, testPath + "bug343515_pg3.html");
-
-    // bug343515_pg3.html consists of a page with two iframes, one of which
-    // contains another iframe, so there'll be a total of 4 load events
-    nShotsListener(ctx.tab2Browser, "load", step4, 4);
   });
-}
 
-function step4() {
-  /* eslint-disable no-shadow */
-  function checkTab2Active(expected) {
-    return ContentTask.spawn(ctx.tab2Browser, expected, async function(
+  // Navigate tab 2 to a different page
+  await BrowserTestUtils.loadURI(
+    ctx.tab2Browser,
+    testPath + "bug343515_pg3.html"
+  );
+
+  await BrowserTestUtils.browserLoaded(ctx.tab2Browser);
+
+  // Step 4.
+
+  async function checkTab2Active(outerExpected) {
+    await ContentTask.spawn(ctx.tab2Browser, outerExpected, async function(
       expected
     ) {
       function isActive(aWindow) {
@@ -161,7 +114,7 @@ function step4() {
       );
     });
   }
-  /* eslint-enable no-shadow */
+
   is(
     testPath + "bug343515_pg3.html",
     ctx.tab2Browser.currentURI.spec,
@@ -173,59 +126,68 @@ function step4() {
   ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
 
   // Tab2 and all descendants should be inactive
-  checkTab2Active(false)
-    .then(() => {
-      // Switch to Tab 2
-      return BrowserTestUtils.switchTab(gBrowser, ctx.tab2);
-    })
-    .then(() => {
-      // Check everything
-      ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
-      ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should be inactive");
-      ok(ctx.tab2Browser.docShellIsActive, "Tab 2 should be active");
+  await checkTab2Active(false);
 
-      return checkTab2Active(true);
-    })
-    .then(() => {
-      // Go back
-      waitForPageshow(ctx.tab2Browser, step5);
-      ctx.tab2Browser.goBack();
-    });
-}
+  // Switch to Tab 2
+  await BrowserTestUtils.switchTab(gBrowser, ctx.tab2);
 
-function step5() {
   // Check everything
   ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
   ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should be inactive");
   ok(ctx.tab2Browser.docShellIsActive, "Tab 2 should be active");
-  ContentTask.spawn(ctx.tab2Browser, null, async function() {
+
+  await checkTab2Active(true);
+
+  // Go back
+  let backDone = BrowserTestUtils.waitForContentEvent(
+    ctx.tab2Browser,
+    "pageshow"
+  );
+  ctx.tab2Browser.goBack();
+  await backDone;
+
+  // Step 5.
+
+  // Check everything
+  ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
+  ok(!ctx.tab1Browser.docShellIsActive, "Tab 1 should be inactive");
+  ok(ctx.tab2Browser.docShellIsActive, "Tab 2 should be active");
+  is(
+    testPath + "bug343515_pg2.html",
+    ctx.tab2Browser.currentURI.spec,
+    "Got expected tab 2 url in step 5"
+  );
+
+  await ContentTask.spawn(ctx.tab2Browser, null, async function() {
     for (var i = 0; i < content.frames.length; i++) {
       let docShell = content.frames[i].docShell;
       Assert.ok(docShell.isActive, `Tab2 iframe ${i} should be active`);
     }
-  })
-    .then(() => {
-      // Switch to tab 1
-      return BrowserTestUtils.switchTab(gBrowser, ctx.tab1);
-    })
-    .then(() => {
-      // Navigate to page 3
-      BrowserTestUtils.loadURI(
-        ctx.tab1Browser,
-        testPath + "bug343515_pg3.html"
-      );
+  });
 
-      // bug343515_pg3.html consists of a page with two iframes, one of which
-      // contains another iframe, so there'll be a total of 4 load events
-      nShotsListener(ctx.tab1Browser, "load", step6, 4);
-    });
-}
+  // Switch to tab 1
+  await BrowserTestUtils.switchTab(gBrowser, ctx.tab1);
 
-function step6() {
+  // Navigate to page 3
+  await BrowserTestUtils.loadURI(
+    ctx.tab1Browser,
+    testPath + "bug343515_pg3.html"
+  );
+
+  await BrowserTestUtils.browserLoaded(ctx.tab1Browser);
+
+  // Step 6.
+
   // Check everything
   ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
   ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
-  ContentTask.spawn(ctx.tab1Browser, null, async function() {
+  is(
+    testPath + "bug343515_pg3.html",
+    ctx.tab1Browser.currentURI.spec,
+    "Got expected tab 1 url in step 6"
+  );
+
+  await ContentTask.spawn(ctx.tab1Browser, null, async function() {
     function isActive(aWindow) {
       var docshell = aWindow.docShell;
       return docshell.isActive;
@@ -237,30 +199,30 @@ function step6() {
       "Tab1 iframe 0 subiframe 0 should be active"
     );
     Assert.ok(isActive(content.frames[1]), "Tab1 iframe 1 should be active");
-  })
-    .then(() => {
-      ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
-      return ContentTask.spawn(ctx.tab2Browser, null, async function() {
-        for (var i = 0; i < content.frames.length; i++) {
-          let docShell = content.frames[i].docShell;
-          Assert.ok(!docShell.isActive, `Tab2 iframe ${i} should be inactive`);
-        }
-      });
-    })
-    .then(() => {
-      // Go forward on tab 2
-      waitForPageshow(ctx.tab2Browser, step7);
-      ctx.tab2Browser.goForward();
-    });
-}
+  });
 
-function step7() {
-  /* eslint-disable no-shadow */
-  function checkBrowser(browser, tabNum, active) {
-    return ContentTask.spawn(browser, { tabNum, active }, async function({
-      tabNum,
-      active,
-    }) {
+  ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
+
+  await ContentTask.spawn(ctx.tab2Browser, null, async function() {
+    for (var i = 0; i < content.frames.length; i++) {
+      let docShell = content.frames[i].docShell;
+      Assert.ok(!docShell.isActive, `Tab2 iframe ${i} should be inactive`);
+    }
+  });
+
+  // Go forward on tab 2
+  let forwardDone = BrowserTestUtils.waitForContentEvent(
+    ctx.tab2Browser,
+    "pageshow"
+  );
+  ctx.tab2Browser.goForward();
+  await forwardDone;
+
+  // Step 7.
+
+  async function checkBrowser(browser, outerTabNum, outerActive) {
+    let data = { tabNum: outerTabNum, active: outerActive };
+    await ContentTask.spawn(browser, data, async function({ tabNum, active }) {
       function isActive(aWindow) {
         var docshell = aWindow.docShell;
         return docshell.isActive;
@@ -284,26 +246,22 @@ function step7() {
       );
     });
   }
-  /* eslint-enable no-shadow */
+
   // Check everything
   ok(!ctx.tab0Browser.docShellIsActive, "Tab 0 should be inactive");
   ok(ctx.tab1Browser.docShellIsActive, "Tab 1 should be active");
-  checkBrowser(ctx.tab1Browser, 1, true)
-    .then(() => {
-      ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
-      return checkBrowser(ctx.tab2Browser, 2, false);
-    })
-    .then(() => {
-      // That's probably enough
-      allDone();
-    });
-}
+  is(
+    testPath + "bug343515_pg3.html",
+    ctx.tab2Browser.currentURI.spec,
+    "Got expected tab 2 url in step 7"
+  );
 
-function allDone() {
+  await checkBrowser(ctx.tab1Browser, 1, true);
+
+  ok(!ctx.tab2Browser.docShellIsActive, "Tab 2 should be inactive");
+  await checkBrowser(ctx.tab2Browser, 2, false);
+
   // Close the tabs we made
-  gBrowser.removeTab(ctx.tab1);
-  gBrowser.removeTab(ctx.tab2);
-
-  // Tell the framework we're done
-  finish();
-}
+  BrowserTestUtils.removeTab(ctx.tab1);
+  BrowserTestUtils.removeTab(ctx.tab2);
+});
