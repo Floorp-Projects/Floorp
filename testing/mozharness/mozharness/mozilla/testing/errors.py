3dc3a1599d836e4d3f7f93d1495e566e4f4dfaff
Sebastian Hengst <archaeopteryx@coole-files.de>
2019-11-01 00:29:23 +0000

    Bug 1591678 - add process name to regexps for harness errors to let their occurrences trigger the task to fail. r=jlund
    Differential Revision: https://phabricator.services.mozilla.com/D50700
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozharness/mozharness/mozilla/testing/errors.py
+++ testing/mozharness/mozharness/mozilla/testing/errors.py
@@ -110,7 +110,7 @@ TinderBoxPrintRe = {
     },
 
     "harness_error": {
-        'full_regex': re.compile(r"(?:TEST-UNEXPECTED-FAIL|PROCESS-CRASH) \| .* \| (application crashed|missing output line for total leaks!|negative leaks caught!|\d+ bytes leaked)"),  # NOQA: E501
+        'full_regex': re.compile(r"(?:TEST-UNEXPECTED-FAIL|PROCESS-CRASH) \| .* \|[^\|]* (application crashed|missing output line for total leaks!|negative leaks caught!|\d+ bytes leaked)"),  # NOQA: E501
         'minimum_regex': re.compile(r'''(TEST-UNEXPECTED|PROCESS-CRASH)'''),
         'retry_regex': re.compile(r'''(FAIL-SHOULD-RETRY|No space left on device|ADBError|ADBProcessError|ADBTimeoutError|program finished with exit code 80|INFRA-ERROR)''')  # NOQA: E501
     },
