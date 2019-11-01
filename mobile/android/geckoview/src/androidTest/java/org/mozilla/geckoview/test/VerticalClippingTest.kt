9e72c49d497b7cb26a9643dec2411193097a7899
Cosmin Sabou <csabou@mozilla.com>
2019-11-01 23:27:41 +0200

    Backed out changeset bb34b6826ada (bug 1577192) for geckoview failures on VerticalClippingTest.

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/VerticalClippingTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/VerticalClippingTest.kt
@@ -13,14 +13,8 @@ import org.junit.runner.RunWith
 import org.mozilla.geckoview.GeckoResult
 import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.WithDisplay
 import android.graphics.Bitmap
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.equalTo
-import org.mozilla.geckoview.GeckoSession
-import org.mozilla.geckoview.test.rule.GeckoSessionTestRule
-import org.mozilla.geckoview.test.util.Callbacks
 import java.nio.ByteBuffer
-import kotlin.math.absoluteValue
-import kotlin.math.max
 
 
 private const val SCREEN_HEIGHT = 100
@@ -55,10 +49,11 @@ class VerticalClippingTest : BaseSessionTest() {
             assertThat("Heights are the same", comparisonImage.height, equalTo(it.height))
             assertThat("Byte counts are the same", comparisonImage.byteCount, equalTo(it.byteCount))
             assertThat("Configs are the same", comparisonImage.config, equalTo(it.config))
-            assertThat("Images are almost identical",
-                    ScreenshotTest.Companion.imageElementDifference(comparisonImage, it),
-                    Matchers.lessThanOrEqualTo(1))
-        }
+            val comparisonPixels: ByteBuffer = ByteBuffer.allocate(comparisonImage.byteCount)
+            comparisonImage.copyPixelsToBuffer(comparisonPixels)
+            val itPixels: ByteBuffer = ByteBuffer.allocate(it.byteCount)
+            it.copyPixelsToBuffer(itPixels)
+            assertThat("Bytes are the same", comparisonPixels, equalTo(itPixels))        }
     }
 
 
@@ -66,11 +61,7 @@ class VerticalClippingTest : BaseSessionTest() {
     @Test
     fun verticalClippingSucceeds() {
         sessionRule.session.loadTestPath(BaseSessionTest.FIXED_BOTTOM)
-        sessionRule.waitUntilCalled(object : Callbacks.ContentDelegate {
-            @GeckoSessionTestRule.AssertCalled(count = 1)
-            override fun onFirstContentfulPaint(session: GeckoSession) {
-            }
-        })
+        sessionRule.waitForPageStop()
 
         sessionRule.display?.let {
             assertScreenshotResult(it.capturePixels(), getComparisonScreenshot(0))
