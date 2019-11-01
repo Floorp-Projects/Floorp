b08053e9dbe0fe8b3ff0279275b0b36ea52b6156
Andrew Halberstadt <ahalberstadt@mozilla.com>
2019-11-01 20:13:35 +0000

    Bug 1583353 - [moztest] Use a fixture to generate the test data in test_resolve.py, r=egao
    I'm making this refactor now because a future commit is going to completely re-write the
    test data (so that it matches the data that a future TestManifestLoader is going to use).
    
    With this method, updating the data will be a lot less tedious.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51278

--- testing/mozbase/moztest/tests/test_resolve.py
+++ testing/mozbase/moztest/tests/test_resolve.py
@@ -6,10 +6,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 import cPickle as pickle
+import json
 import os
 import re
 import shutil
 import tempfile
+from collections import defaultdict
 
 import pytest
 import mozpack.path as mozpath
@@ -23,188 +25,124 @@ from moztest.resolve import (
 )
 
 
-ALL_TESTS = {
-    "accessible/tests/mochitest/actions/test_anchors.html": [
-        {
-            "dir_relpath": "accessible/tests/mochitest/actions",
-            "expected": "pass",
-            "file_relpath": "accessible/tests/mochitest/actions/test_anchors.html",
-            "flavor": "a11y",
-            "here": "/firefox/accessible/tests/mochitest/actions",
-            "manifest": "/firefox/accessible/tests/mochitest/actions/a11y.ini",
-            "name": "test_anchors.html",
-            "path": "/firefox/accessible/tests/mochitest/actions/test_anchors.html",
-            "relpath": "test_anchors.html"
-        }
-    ],
-    "services/common/tests/unit/test_async_chain.js": [
-        {
-            "dir_relpath": "services/common/tests/unit",
-            "file_relpath": "services/common/tests/unit/test_async_chain.js",
+@pytest.fixture(scope='module')
+def create_tests():
+    sourcedir = '/firefox'
+
+    def inner(*paths, **defaults):
+        tests = defaultdict(list)
+        for path in paths:
+            if isinstance(path, tuple):
+                path, kwargs = path
+            else:
+                kwargs = {}
+
+            path = mozpath.normpath(path)
+            manifest_name = kwargs.get('flavor', defaults.get('flavor', 'manifest'))
+            manifest = kwargs.pop('manifest', defaults.pop('manifest',
+                                  mozpath.join(mozpath.dirname(path), manifest_name + '.ini')))
+
+            manifest_abspath = mozpath.join(sourcedir, manifest)
+            relpath = mozpath.relpath(path, mozpath.dirname(manifest))
+            test = {
+                'name': relpath,
+                'path': mozpath.join(sourcedir, path),
+                'relpath': relpath,
+                'file_relpath': path,
+                'dir_relpath': mozpath.dirname(path),
+                'here': mozpath.dirname(manifest_abspath),
+                'manifest': manifest_abspath,
+                'manifest_relpath': manifest,
+            }
+            test.update(**defaults)
+            test.update(**kwargs)
+            tests[path].append(test)
+
+        # dump tests to stdout for easier debugging on failure
+        print("The 'create_tests' fixture returned:")
+        print(json.dumps(dict(tests), indent=2, sort_keys=True))
+        return tests
+
+    return inner
+
+
+@pytest.fixture(scope='module')
+def all_tests(create_tests):
+    return create_tests(*[
+        ("accessible/tests/mochitest/actions/test_anchors.html", {
+             "expected": "pass",
+             "flavor": "a11y",
+         }),
+        ("services/common/tests/unit/test_async_chain.js", {
             "firefox-appdir": "browser",
             "flavor": "xpcshell",
             "head": "head_global.js head_helpers.js head_http.js",
-            "here": "/firefox/services/common/tests/unit",
-            "manifest": "/firefox/services/common/tests/unit/xpcshell.ini",
-            "name": "test_async_chain.js",
-            "path": "/firefox/services/common/tests/unit/test_async_chain.js",
-            "relpath": "test_async_chain.js",
-        }
-    ],
-    "services/common/tests/unit/test_async_querySpinningly.js": [
-        {
-            "dir_relpath": "services/common/tests/unit",
-            "file_relpath": "services/common/tests/unit/test_async_querySpinningly.js",
+         }),
+        ("services/common/tests/unit/test_async_querySpinningly.js", {
             "firefox-appdir": "browser",
             "flavor": "xpcshell",
             "head": "head_global.js head_helpers.js head_http.js",
-            "here": "/firefox/services/common/tests/unit",
-            "manifest": "/firefox/services/common/tests/unit/xpcshell.ini",
-            "name": "test_async_querySpinningly.js",
-            "path": "/firefox/services/common/tests/unit/test_async_querySpinningly.js",
-            "relpath": "test_async_querySpinningly.js",
-        }
-    ],
-    "toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js": [
-        {
-            "dir_relpath": "toolkit/mozapps/update/test/unit",
-            "file_relpath": "toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js",
+         }),
+        ("toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js", {
             "flavor": "xpcshell",
             "generated-files": "head_update.js",
             "head": "head_update.js",
-            "here": "/firefox/toolkit/mozapps/update/test/unit",
-            "manifest": "/firefox/toolkit/mozapps/update/test/unit/xpcshell_updater.ini",
-            "name": "test_0201_app_launch_apply_update.js",
-            "path": "/firefox/toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js",
+            "manifest": "toolkit/mozapps/update/test/unit/xpcshell_updater.ini",
             "reason": "bug 820380",
-            "relpath": "test_0201_app_launch_apply_update.js",
             "run-sequentially": "Launches application.",
             "skip-if": "os == 'android'",
-        },
-        {
-            "dir_relpath": "toolkit/mozapps/update/test/unit",
-            "file_relpath": "toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js",
+         }),
+        ("toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js", {
             "flavor": "xpcshell",
             "generated-files": "head_update.js",
             "head": "head_update.js head2.js",
-            "here": "/firefox/toolkit/mozapps/update/test/unit",
-            "manifest": "/firefox/toolkit/mozapps/update/test/unit/xpcshell_updater.ini",
-            "name": "test_0201_app_launch_apply_update.js",
-            "path": "/firefox/toolkit/mozapps/update/test/unit/test_0201_app_launch_apply_update.js",
+            "manifest": "toolkit/mozapps/update/test/unit/xpcshell_updater.ini",
             "reason": "bug 820380",
-            "relpath": "test_0201_app_launch_apply_update.js",
             "run-sequentially": "Launches application.",
             "skip-if": "os == 'android'",
-        }
-    ],
-    "mobile/android/tests/background/junit3/src/common/TestAndroidLogWriters.java": [
-        {
-            "dir_relpath": "mobile/android/tests/background/junit3/src/common",
-            "file_relpath": "mobile/android/tests/background/junit3/src/common/TestAndroidLogWriters.java",
+         }),
+        ("mobile/android/tests/background/junit3/src/common/TestAndroidLogWriters.java", {
             "flavor": "instrumentation",
-            "here": "/firefox/mobile/android/tests/background/junit3",
-            "manifest": "/firefox/mobile/android/tests/background/junit3/instrumentation.ini",
-            "name": "src/common/TestAndroidLogWriters.java",
-            "path": "/firefox/mobile/android/tests/background/junit3/src/common/TestAndroidLogWriters.java",
-            "relpath": "src/common/TestAndroidLogWriters.java",
-            "subsuite": "background"
-        }
-    ],
-    "mobile/android/tests/browser/junit3/src/TestDistribution.java": [
-        {
-            "dir_relpath": "mobile/android/tests/browser/junit3/src",
-            "file_relpath": "mobile/android/tests/browser/junit3/src/TestDistribution.java",
+            "manifest": "mobile/android/tests/background/junit3/instrumentation.ini",
+            "subsuite": "background",
+         }),
+        ("mobile/android/tests/browser/junit3/src/TestDistribution.java", {
             "flavor": "instrumentation",
-            "here": "/firefox/mobile/android/tests/browser/junit3",
-            "manifest": "/firefox/mobile/android/tests/browser/junit3/instrumentation.ini",
-            "name": "src/TestDistribution.java",
-            "path": "/firefox/mobile/android/tests/browser/junit3/src/TestDistribution.java",
-            "relpath": "src/TestDistribution.java",
-            "subsuite": "browser"
-        }
-    ],
-    "image/test/browser/browser_bug666317.js": [
-        {
-            "dir_relpath": "image/test/browser",
-            "file_relpath": "image/test/browser/browser_bug666317.js",
+            "manifest": "mobile/android/tests/browser/junit3/instrumentation.ini",
+            "subsuite": "browser",
+         }),
+        ("image/test/browser/browser_bug666317.js", {
             "flavor": "browser-chrome",
-            "here": "/firefox/testing/mochitest/browser/image/test/browser",
-            "manifest": "/firefox/image/test/browser/browser.ini",
-            "name": "browser_bug666317.js",
-            "path": "/firefox/testing/mochitest/browser/image/test/browser/browser_bug666317.js",
-            "relpath": "image/test/browser/browser_bug666317.js",
+            "manifest": "image/test/browser/browser.ini",
             "skip-if": "e10s # Bug 948194 - Decoded Images seem to not be discarded on memory-pressure notification with e10s enabled",
-            "subsuite": ""
-        }
-   ],
-   "devtools/client/markupview/test/browser_markupview_copy_image_data.js": [
-        {
-            "dir_relpath": "devtools/client/markupview/test",
-            "file_relpath": "devtools/client/markupview/test/browser_markupview_copy_image_data.js",
+            "subsuite": "",
+         }),
+        ("devtools/client/markupview/test/browser_markupview_copy_image_data.js", {
             "flavor": "browser-chrome",
-            "here": "/firefox/testing/mochitest/browser/devtools/client/markupview/test",
-            "manifest": "/firefox/devtools/client/markupview/test/browser.ini",
-            "name": "browser_markupview_copy_image_data.js",
-            "path": "/firefox/testing/mochitest/browser/devtools/client/markupview/test/browser_markupview_copy_image_data.js",
-            "relpath": "devtools/client/markupview/test/browser_markupview_copy_image_data.js",
+            "manifest": "devtools/client/markupview/test/browser.ini",
             "subsuite": "devtools",
-            "tags": "devtools"
+            "tags": "devtools",
+         }),
+    ])
+
+
+@pytest.fixture(scope='module')
+def defaults():
+    return {
+        "/firefox/toolkit/mozapps/update/test/unit/xpcshell_updater.ini": {
+            "support-files": "\ndata/**\nxpcshell_updater.ini"
         }
-   ]
-}
-
-TEST_DEFAULTS = {
-    "/firefox/toolkit/mozapps/update/test/unit/xpcshell_updater.ini": {"support-files": "\ndata/**\nxpcshell_updater.ini"}
-}
-
-TASK_LABELS = [
-    'test-linux64/opt-browser-screenshots-1',
-    'test-linux64/opt-browser-screenshots-e10s-1',
-    'test-linux64/opt-marionette',
-    'test-linux64/opt-mochitest',
-    'test-linux64/debug-mochitest-e10s',
-    'test-linux64/opt-mochitest-a11y',
-    'test-linux64/opt-mochitest-browser',
-    'test-linux64/opt-mochitest-browser-chrome',
-    'test-linux64/opt-mochitest-browser-chrome-e10s',
-    'test-linux64/opt-mochitest-browser-chrome-e10s-11',
-    'test-linux64/opt-mochitest-chrome',
-    'test-linux64/opt-mochitest-devtools',
-    'test-linux64/opt-mochitest-devtools-chrome',
-    'test-linux64/opt-mochitest-gpu',
-    'test-linux64/opt-mochitest-gpu-e10s',
-    'test-linux64/opt-mochitest-media-e10s-1',
-    'test-linux64/opt-mochitest-media-e10s-11',
-    'test-linux64/opt-mochitest-plain',
-    'test-linux64/opt-mochitest-screenshots-1',
-    'test-linux64/opt-reftest',
-    'test-linux64/debug-reftest-e10s-1',
-    'test-linux64/debug-reftest-e10s-11',
-    'test-linux64/opt-robocop',
-    'test-linux64/opt-robocop-1',
-    'test-linux64/opt-robocop-e10s',
-    'test-linux64/opt-robocop-e10s-1',
-    'test-linux64/opt-robocop-e10s-11',
-    'test-linux64/opt-web-platform-tests-e10s-1',
-    'test-linux64/opt-web-platform-tests-reftests-e10s-1',
-    'test-linux64/opt-web-platform-tests-reftest-e10s-1',
-    'test-linux64/opt-web-platform-tests-wdspec-e10s-1',
-    'test-linux64/opt-web-platform-tests-1',
-    'test-linux64/opt-web-platform-test-e10s-1',
-    'test-linux64/opt-xpcshell',
-    'test-linux64/opt-xpcshell-1',
-    'test-linux64/opt-xpcshell-2',
-]
+    }
 
 
 @pytest.fixture
-def resolver(tmpdir):
+def resolver(tmpdir, all_tests, defaults):
     topobjdir = tmpdir.mkdir("objdir").strpath
 
     with open(os.path.join(topobjdir, 'all-tests.pkl'), 'wb') as fh:
-        pickle.dump(ALL_TESTS, fh)
+        pickle.dump(all_tests, fh)
     with open(os.path.join(topobjdir, 'test-defaults.pkl'), 'wb') as fh:
-        pickle.dump(TEST_DEFAULTS, fh)
+        pickle.dump(defaults, fh)
 
     o = MozbuildObject('/firefox', None, None, topobjdir=topobjdir)
 
@@ -331,6 +269,44 @@ def test_resolve_metadata(resolver):
 
 def test_task_regexes():
     """Test the task_regexes defined in TEST_SUITES."""
+    task_labels = [
+        'test-linux64/opt-browser-screenshots-1',
+        'test-linux64/opt-browser-screenshots-e10s-1',
+        'test-linux64/opt-marionette',
+        'test-linux64/opt-mochitest',
+        'test-linux64/debug-mochitest-e10s',
+        'test-linux64/opt-mochitest-a11y',
+        'test-linux64/opt-mochitest-browser',
+        'test-linux64/opt-mochitest-browser-chrome',
+        'test-linux64/opt-mochitest-browser-chrome-e10s',
+        'test-linux64/opt-mochitest-browser-chrome-e10s-11',
+        'test-linux64/opt-mochitest-chrome',
+        'test-linux64/opt-mochitest-devtools',
+        'test-linux64/opt-mochitest-devtools-chrome',
+        'test-linux64/opt-mochitest-gpu',
+        'test-linux64/opt-mochitest-gpu-e10s',
+        'test-linux64/opt-mochitest-media-e10s-1',
+        'test-linux64/opt-mochitest-media-e10s-11',
+        'test-linux64/opt-mochitest-plain',
+        'test-linux64/opt-mochitest-screenshots-1',
+        'test-linux64/opt-reftest',
+        'test-linux64/debug-reftest-e10s-1',
+        'test-linux64/debug-reftest-e10s-11',
+        'test-linux64/opt-robocop',
+        'test-linux64/opt-robocop-1',
+        'test-linux64/opt-robocop-e10s',
+        'test-linux64/opt-robocop-e10s-1',
+        'test-linux64/opt-robocop-e10s-11',
+        'test-linux64/opt-web-platform-tests-e10s-1',
+        'test-linux64/opt-web-platform-tests-reftests-e10s-1',
+        'test-linux64/opt-web-platform-tests-reftest-e10s-1',
+        'test-linux64/opt-web-platform-tests-wdspec-e10s-1',
+        'test-linux64/opt-web-platform-tests-1',
+        'test-linux64/opt-web-platform-test-e10s-1',
+        'test-linux64/opt-xpcshell',
+        'test-linux64/opt-xpcshell-1',
+        'test-linux64/opt-xpcshell-2',
+    ]
 
     test_cases = {
         'mochitest-browser-chrome': [
@@ -401,7 +377,7 @@ def test_task_regexes():
     for suite, expected in sorted(test_cases.items()):
         print(suite)
         regexes = TEST_SUITES[suite]['task_regex']
-        assert set(filter(match_task, TASK_LABELS)) == set(expected)
+        assert set(filter(match_task, task_labels)) == set(expected)
 
 
 if __name__ == '__main__':
