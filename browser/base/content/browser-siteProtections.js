c949419c3171306b9190d61510f7f7c4ca09bacb
Johann Hofmann <jhofmann@mozilla.com>
2019-11-01 10:50:22 +0000

    Bug 1592616 - Don't show breakage report link when we're not blocking anything. r=nhnt11
    Differential Revision: https://phabricator.services.mozilla.com/D51381

--- browser/base/content/browser-siteProtections.js
+++ browser/base/content/browser-siteProtections.js
@@ -2177,6 +2177,7 @@ var gProtectionsHandler = {
     // ContentBlockingAllowList here.
     this._protectionsPopupTPSwitchBreakageLink.hidden =
       ContentBlockingAllowList.includes(gBrowser.selectedBrowser) ||
+      !this._protectionsPopup.hasAttribute("blocking") ||
       !this._protectionsPopupTPSwitch.hasAttribute("enabled");
   },
 
