e4a1dba7fde35fc3dcfa7aa2f8fbd5fcd207a382
Jonathan Almeida <jalmeida@mozilla.com>
2019-11-02 01:33:16 -0400

    [components] Replace WebPushDelegate API returns with lambdas

--- mobile/android/android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/webpush/WebPushDelegate.kt
+++ mobile/android/android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/webpush/WebPushDelegate.kt
@@ -12,17 +12,17 @@ interface WebPushDelegate {
     /**
      * Requests a WebPush subscription for the given Service Worker scope.
      */
-    fun onGetSubscription(scope: String): WebPushSubscription
+    fun onGetSubscription(scope: String, onSubscription: (WebPushSubscription) -> Unit) = Unit
 
     /**
      * Create a WebPush subscription for the given Service Worker scope.
      */
-    fun onSubscribe(scope: String, serverKey: ByteArray?): WebPushSubscription
+    fun onSubscribe(scope: String, serverKey: ByteArray?, onSubscribe: (WebPushSubscription) -> Unit) = Unit
 
     /**
      * Remove a subscription for the given Service Worker scope.
      *
      * @return whether the unsubscribe was successful or not.
      */
-    fun onUnsubscribe(scope: String): Boolean
+    fun onUnsubscribe(scope: String, onUnsubscribe: (Boolean) -> Unit) = Unit
 }
