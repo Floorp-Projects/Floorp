2d62ad526f9165793f87876d024b35d0f7038842
Nihanth Subramanya <nhnt11@gmail.com>
2019-11-04 09:55:02 +0000

    Bug 1593509 - Improvements to social tracking frontend test. r=xeonchen
    Differential Revision: https://phabricator.services.mozilla.com/D51544

--- browser/base/content/test/trackingUI/browser_trackingUI_socialtracking.js
+++ browser/base/content/test/trackingUI/browser_trackingUI_socialtracking.js
@@ -4,7 +4,7 @@
 "use strict";
 
 const TRACKING_PAGE =
-  "http://example.org/browser/browser/base/content/test/trackingUI/trackingPage.html";
+  "http://example.com/browser/browser/base/content/test/trackingUI/trackingPage.html";
 
 const ST_PROTECTION_PREF = "privacy.trackingprotection.socialtracking.enabled";
 const ST_BLOCK_COOKIES_PREF = "privacy.socialtracking.block_cookies.enabled";
@@ -12,24 +12,17 @@ const ST_BLOCK_COOKIES_PREF = "privacy.socialtracking.block_cookies.enabled";
 add_task(async function setup() {
   await SpecialPowers.pushPrefEnv({
     set: [
-      [ST_PROTECTION_PREF, true],
       [ST_BLOCK_COOKIES_PREF, true],
       [
         "urlclassifier.features.socialtracking.blacklistHosts",
-        "socialtracking.example.com",
+        "social-tracking.example.org",
       ],
       [
         "urlclassifier.features.socialtracking.annotate.blacklistHosts",
-        "socialtracking.example.com",
+        "social-tracking.example.org",
       ],
       ["privacy.trackingprotection.enabled", false],
-      ["privacy.trackingprotection.annotate_channels", false],
-      ["privacy.trackingprotection.cryptomining.enabled", false],
-      ["urlclassifier.features.cryptomining.annotate.blacklistHosts", ""],
-      ["urlclassifier.features.cryptomining.annotate.blacklistTables", ""],
-      ["privacy.trackingprotection.fingerprinting.enabled", false],
-      ["urlclassifier.features.fingerprinting.annotate.blacklistHosts", ""],
-      ["urlclassifier.features.fingerprinting.annotate.blacklistTables", ""],
+      ["privacy.trackingprotection.annotate_channels", true],
     ],
   });
 });
@@ -51,8 +44,12 @@ async function testIdentityState(hasException) {
     await loaded;
   }
 
+  let categoryItem = document.getElementById(
+    "protections-popup-category-socialblock"
+  );
+
   ok(
-    !gProtectionsHandler._protectionsPopup.hasAttribute("detected"),
+    categoryItem.classList.contains("notFound"),
     "socialtrackings are not detected"
   );
 
@@ -61,17 +58,21 @@ async function testIdentityState(hasException) {
     "icon box is visible regardless the exception"
   );
 
-  promise = waitForContentBlockingEvent();
-
   await SpecialPowers.spawn(tab.linkedBrowser, [], function() {
     content.postMessage("socialtracking", "*");
   });
 
-  await promise;
+  await TestUtils.waitForCondition(() => {
+    return !categoryItem.classList.contains("notFound");
+  });
 
   ok(
     gProtectionsHandler._protectionsPopup.hasAttribute("detected"),
     "trackers are detected"
+  );
+  ok(
+    !categoryItem.classList.contains("notFound"),
+    "social trackers are detected"
   );
   ok(
     BrowserTestUtils.is_visible(gProtectionsHandler.iconBox),
@@ -140,7 +141,7 @@ async function testSubview(hasException) {
   ok(BrowserTestUtils.is_visible(listItem), "List item is visible");
   is(
     listItem.querySelector("label").value,
-    "socialtracking.example.com",
+    "social-tracking.example.org",
     "Has the correct host"
   );
 
@@ -165,8 +166,11 @@ async function testSubview(hasException) {
   BrowserTestUtils.removeTab(tab);
 }
 
-async function testCategoryItem() {
-  Services.prefs.setBoolPref(ST_PROTECTION_PREF, false);
+async function testCategoryItem(blockLoads) {
+  if (blockLoads) {
+    Services.prefs.setBoolPref(ST_PROTECTION_PREF, true);
+  }
+
   Services.prefs.setBoolPref(ST_BLOCK_COOKIES_PREF, false);
 
   let promise = BrowserTestUtils.openNewForegroundTab({
@@ -188,7 +192,6 @@ async function testCategoryItem() {
     "Category marked as not found"
   );
 
-  Services.prefs.setBoolPref(ST_PROTECTION_PREF, true);
   Services.prefs.setBoolPref(ST_BLOCK_COOKIES_PREF, true);
 
   ok(categoryItem.classList.contains("blocked"), "Category marked as blocked");
@@ -197,25 +200,24 @@ async function testCategoryItem() {
     "Category marked as not found"
   );
 
-  promise = waitForContentBlockingEvent();
-
   await SpecialPowers.spawn(tab.linkedBrowser, [], function() {
     content.postMessage("socialtracking", "*");
   });
 
-  await promise;
-
-  ok(categoryItem.classList.contains("blocked"), "Category marked as blocked");
-
   await TestUtils.waitForCondition(() => {
     return !categoryItem.classList.contains("notFound");
   });
+
+  ok(categoryItem.classList.contains("blocked"), "Category marked as blocked");
+
   ok(
     !categoryItem.classList.contains("notFound"),
     "Category not marked as not found"
   );
 
   BrowserTestUtils.removeTab(tab);
+
+  Services.prefs.clearUserPref(ST_PROTECTION_PREF);
 }
 
 add_task(async function testIdentityUI() {
@@ -227,5 +229,6 @@ add_task(async function testIdentityUI() {
   await testSubview(false);
   await testSubview(true);
 
-  await testCategoryItem();
+  await testCategoryItem(false);
+  await testCategoryItem(true);
 });
