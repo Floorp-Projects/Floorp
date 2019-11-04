7f28f7c6a7ebd2cb676a20d4cdd9e31d9435446b
Simon Sapin <simon.sapin@exyr.org>
2019-11-04 12:52:57 +0000

    Bug 1593642 - More compact debug output for CSS values.
    ```
    Rect {
        start_corner: Vec2 { i: 0.0 px, b: 0.0 px },
        size: Vec2 { i: 1024.0 px, b: 20.0 px },
    }
    ```
    
    … instead of:
    
    ```
    Rect {
        start_corner: Vec2 {
            inline: CSSPixelLength(
                0.0,
            ),
            block: CSSPixelLength(
                0.0,
            ),
        },
        size: Vec2 {
            inline: CSSPixelLength(
                1024.0,
            ),
            block: CSSPixelLength(
                0.0,
            ),
        },
    }
    ```
    
    Differential Revision: https://phabricator.services.mozilla.com/D51590
    
    --HG--
    extra : moz-landing-system : lando

--- servo/components/style/values/computed/length.rs
+++ servo/components/style/values/computed/length.rs
@@ -613,7 +613,6 @@ impl Size {
     Clone,
     ComputeSquaredDistance,
     Copy,
-    Debug,
     Deserialize,
     MallocSizeOf,
     PartialEq,
@@ -626,6 +625,13 @@ impl Size {
 )]
 #[repr(C)]
 pub struct CSSPixelLength(CSSFloat);
+
+impl fmt::Debug for CSSPixelLength {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)?;
+        f.write_str(" px")
+    }
+}
 
 impl CSSPixelLength {
     /// Return a new CSSPixelLength.
