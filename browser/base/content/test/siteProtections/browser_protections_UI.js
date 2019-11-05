620a1c44cd29de284e3b45084e263de3ebe55a52
Nihanth Subramanya <nhnt11@gmail.com>
2019-11-05 21:33:06 +0000

    Bug 1592616 - Ensure we wait for site not working link to be updated before checking visibility. r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D51920

--- browser/base/content/test/siteProtections/browser_protections_UI.js
+++ browser/base/content/test/siteProtections/browser_protections_UI.js
@@ -55,6 +55,11 @@ add_task(async function testToggleSwitch() {
   is(buttonEvents.length, 1, "recorded telemetry for opening the popup");
 
   // Check the visibility of the "Site not working?" link.
+  await TestUtils.waitForCondition(() => {
+    return BrowserTestUtils.is_visible(
+      gProtectionsHandler._protectionsPopupTPSwitchBreakageLink
+    );
+  });
   ok(
     BrowserTestUtils.is_visible(
       gProtectionsHandler._protectionsPopupTPSwitchBreakageLink
