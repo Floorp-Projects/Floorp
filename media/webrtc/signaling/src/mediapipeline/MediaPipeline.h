18c4e9ab29dcd1ba9a9055299063ad66671edbd7
Andreas Pehrson <apehrson@mozilla.com>
2019-11-05 20:04:45 +0000

    Bug 1212237 - Use a content principal by default for received tracks. r=bwc
    This swaps things around so that received tracks in peer connections have a
    content principal instead of a null principal initially.
    
    This puts an extra requirement on us to not output any frames under the content
    principal after ALPN has been negotiated with requested privacy, but before this
    private principal has been signaled to the MediaPipelines. The private principal
    is signaled from the STS thread, via the main thread, whereas media is consumed
    directly by MediaPipelines after being received on the STS thread.
    
    This patch adds an extra signaling path directly from the STS thread that tells
    MediaPipelines to make their PrincipalHandle private, and to ignore any data
    until the private PrincipalHandle is set. It also moves the responsibility of
    updating the principal of the received MediaStreamTracks from TransceiverImpl
    to MediaPipeline, so it's all in the same path.
    
    This lets all MediaStream and MediaStreamTrack APIs consume received tracks
    directly after getting exposed to JS without errors. In case privacy is later
    requested, consumers that have already been set up must handle this on the fly.
    (They do, in specs and impls)
    
    Differential Revision: https://phabricator.services.mozilla.com/D48947
    
    --HG--
    extra : moz-landing-system : lando

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.h
@@ -205,6 +205,7 @@ class MediaPipeline : public sigslot::has_slots<> {
   void RtcpStateChange(const std::string& aTransportId, TransportLayer::State);
   virtual void CheckTransportStates();
   void PacketReceived(const std::string& aTransportId, MediaPacket& packet);
+  void AlpnNegotiated(const std::string& aAlpn, bool aPrivacyRequested);
 
   void RtpPacketReceived(MediaPacket& packet);
   void RtcpPacketReceived(MediaPacket& packet);
@@ -213,6 +214,10 @@ class MediaPipeline : public sigslot::has_slots<> {
                               MediaPacket& aPacket);
 
   void SetDescription_s(const std::string& description);
+
+  // Called when ALPN is negotiated and is requesting privacy, so receive
+  // pipelines do not enter data into the graph under a content principal.
+  virtual void MakePrincipalPrivate_s() {}
 
   const DirectionType mDirection;
   size_t mLevel;
@@ -331,11 +336,6 @@ class MediaPipelineReceive : public MediaPipeline {
                        nsCOMPtr<nsISerialEventTarget> aStsThread,
                        RefPtr<MediaSessionConduit> aConduit);
 
-  // Sets the PrincipalHandle we set on the media chunks produced by this
-  // pipeline. Must be called on the main thread.
-  virtual void SetPrincipalHandle_m(
-      const PrincipalHandle& aPrincipalHandle) = 0;
-
  protected:
   ~MediaPipelineReceive();
 };
@@ -356,7 +356,7 @@ class MediaPipelineReceiveAudio : public MediaPipelineReceive {
 
   bool IsVideo() const override { return false; }
 
-  void SetPrincipalHandle_m(const PrincipalHandle& aPrincipalHandle) override;
+  void MakePrincipalPrivate_s() override;
 
   void Start() override;
   void Stop() override;
@@ -387,7 +387,7 @@ class MediaPipelineReceiveVideo : public MediaPipelineReceive {
 
   bool IsVideo() const override { return true; }
 
-  void SetPrincipalHandle_m(const PrincipalHandle& aPrincipalHandle) override;
+  void MakePrincipalPrivate_s() override;
 
   void Start() override;
   void Stop() override;
