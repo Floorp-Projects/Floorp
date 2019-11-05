cff7d7fb1665b9219257873e16c5ba0c3c48be21
Brian Hackett <bhackett1024@gmail.com>
2019-11-05 01:28:08 +0000

    Bug 1592854 - Workaround rendering problems with conditional panel during test, r=loganfsmyth.
    Differential Revision: https://phabricator.services.mozilla.com/D51438
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/test/browser/head.js
+++ devtools/client/webconsole/test/browser/head.js
@@ -477,6 +477,11 @@ async function checkClickOnNode(
 ) {
   info("checking click on node location");
 
+  // If the debugger hasn't fully loaded yet and breakpoints are still being
+  // added when we click on the logpoint link, the logpoint panel might not
+  // render. Work around this for now, see bug 1592854.
+  await waitForTime(1000);
+
   const onSourceInDebuggerOpened = once(hud, "source-in-debugger-opened");
 
   EventUtils.sendMouseEvent(
