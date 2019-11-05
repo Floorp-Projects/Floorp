a5af98738c738b513a1814f20f3fa9c6e2758ee6
Nicolas Silva <nsilva@mozilla.com>
2019-11-05 19:05:25 +0000

    Bug 1594091 - Move glyph_index to the second half of the glyph instance z field. r=gw
    Depends on D51880
    
    Differential Revision: https://phabricator.services.mozilla.com/D51882

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -979,10 +979,10 @@ impl BatchBuilder {
 
                                 for glyph in glyphs {
                                     batch.push(base_instance.build(
-                                        glyph.index_in_text_run | ((render_task_address.0 as i32) << 16),
+                                        (rasterization_space as i32) | ((render_task_address.0 as i32) << 16),
                                         (subpx_dir as u32 as i32) << 24
                                         | (color_mode as u32 as i32) << 16
-                                        | (rasterization_space as i32),
+                                        | glyph.index_in_text_run,
                                         glyph.uv_rect_address.as_int(),
                                     ));
                                 }
