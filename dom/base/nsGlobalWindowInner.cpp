9a4043957e534a51acca61554c8a2ea27951c624
Brindusan Cristian <cbrindusan@mozilla.com>
2019-11-06 02:42:43 +0200

    Backed out changeset c3813936e074 (bug 1589913) for mochitest assertion failures at nsGlobalWindowInner.cpp. CLOSED TREE

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -4092,7 +4092,6 @@ void nsGlobalWindowInner::DisableGamepadUpdates() {
 }
 
 void nsGlobalWindowInner::EnableVRUpdates() {
-  MOZ_ASSERT(!IsDying());
   if (mHasVREvents && !mVREventObserver) {
     mVREventObserver = new VREventObserver(this);
     nsPIDOMWindowOuter* outer = GetOuterWindow();
