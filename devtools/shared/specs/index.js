5a8330dd2fd86371263ae462c592f075ee32aba7
Jason Laster <jlaster@mozilla.com>
2019-11-06 20:07:35 +0000

    Bug 1571704 - Make ThreadActor.frames return Frame fronts instead of JSON objects. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D51004

--- devtools/shared/specs/index.js
+++ devtools/shared/specs/index.js
@@ -82,11 +82,10 @@ const Types = (exports.__TypesForTests = [
     spec: "devtools/shared/specs/environment",
     front: "devtools/shared/fronts/environment",
   },
-  /* frame has old fashion client and no front */
   {
     types: ["frame"],
     spec: "devtools/shared/specs/frame",
-    front: null,
+    front: "devtools/shared/fronts/frame",
   },
   {
     types: ["framerate"],
