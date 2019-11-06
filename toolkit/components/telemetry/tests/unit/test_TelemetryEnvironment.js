70c7a633fbcbb57b664aaaf212f46c70ec53e41d
Moritz Birghan <mbirghan@mozilla.com>
2019-11-06 18:07:54 +0000

    Bug 1571656 - Use Assert.jsm numeric comparison functions in tests r=mixedpuppy,MattN
    Differential Revision: https://phabricator.services.mozilla.com/D40614
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/telemetry/tests/unit/test_TelemetryEnvironment.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryEnvironment.js
@@ -768,7 +768,11 @@ function checkSystemSection(data, assertProcessData) {
 
   Assert.ok(Array.isArray(gfxData.monitors));
   if (gIsWindows || gIsMac || gIsLinux) {
-    Assert.ok(gfxData.monitors.length >= 1, "There is at least one monitor.");
+    Assert.greaterEqual(
+      gfxData.monitors.length,
+      1,
+      "There is at least one monitor."
+    );
     Assert.equal(typeof gfxData.monitors[0].screenWidth, "number");
     Assert.equal(typeof gfxData.monitors[0].screenHeight, "number");
     if (gIsWindows) {
