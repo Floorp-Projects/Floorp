46f931aa7a35f19fb3011b2fd3c3a664dced4e40
Matt Woodrow <mwoodrow@mozilla.com>
2019-11-06 18:22:00 +0000

    Bug 1593560 - Allow data urls with DocumentChannel. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D51547

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -9547,8 +9547,8 @@ static bool IsConsideredSameOriginForUIR(nsIPrincipal* aTriggeringPrincipal,
 }
 
 static bool SchemeUsesDocChannel(nsIURI* aURI) {
-  return aURI && (aURI->SchemeIs("http") || aURI->SchemeIs("https") ||
-                  aURI->SchemeIs("moz"));
+  return SchemeIsHTTP(aURI) || SchemeIsHTTPS(aURI) || aURI->SchemeIs("moz") ||
+         SchemeIsData(aURI);
 }
 
 /* static */ bool nsDocShell::CreateChannelForLoadState(
