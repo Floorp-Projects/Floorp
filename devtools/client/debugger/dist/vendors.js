609127e0c121778152c418358da078c0098995c6
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-11-06 17:48:48 +0000

    Bug 1594379 - Update devtools-launchpad to 0.0.160. r=jlast.
    Differential Revision: https://phabricator.services.mozilla.com/D51987

--- devtools/client/debugger/dist/vendors.js
+++ devtools/client/debugger/dist/vendors.js
@@ -3625,17 +3625,6 @@ function getSourceNames(source) {
       gSourceNamesStore.set(source, result);
       return result;
     }
-  } // If Scratchpad URI, like "Scratchpad/1"; no modifications,
-  // and short/long are the same.
-
-
-  if (isScratchpadScheme(sourceStr)) {
-    let result = {
-      short: sourceStr,
-      long: sourceStr
-    };
-    gSourceNamesStore.set(source, result);
-    return result;
   }
 
   const parsedUrl = parseURL(sourceStr);
@@ -3689,14 +3678,6 @@ function getSourceNames(source) {
 
 function isColonSlashSlash(location, i = 0) {
   return location.charCodeAt(++i) === CHAR_CODE_COLON && location.charCodeAt(++i) === CHAR_CODE_SLASH && location.charCodeAt(++i) === CHAR_CODE_SLASH;
-}
-/**
-* Checks for a Scratchpad URI, like "Scratchpad/1"
-*/
-
-
-function isScratchpadScheme(location, i = 0) {
-  return location.charCodeAt(i) === CHAR_CODE_CAP_S && location.charCodeAt(++i) === CHAR_CODE_C && location.charCodeAt(++i) === CHAR_CODE_R && location.charCodeAt(++i) === CHAR_CODE_A && location.charCodeAt(++i) === CHAR_CODE_T && location.charCodeAt(++i) === CHAR_CODE_C && location.charCodeAt(++i) === CHAR_CODE_H && location.charCodeAt(++i) === CHAR_CODE_P && location.charCodeAt(++i) === CHAR_CODE_A && location.charCodeAt(++i) === CHAR_CODE_D && location.charCodeAt(++i) === CHAR_CODE_SLASH;
 }
 
 function isDataScheme(location, i = 0) {
@@ -3812,7 +3793,6 @@ function getSourceMappedFile(source) {
 module.exports = {
   parseURL,
   getSourceNames,
-  isScratchpadScheme,
   isChromeScheme,
   isContentScheme,
   isWASM,
@@ -4194,7 +4174,7 @@ module.exports = Telemetry;
 // devtools/client/shared/unicode-url.js in the mozilla-central repository.
 
 
-const punycode = __webpack_require__(62);
+const punycode = __webpack_require__(521);
 /**
  * Gets a readble Unicode hostname from a hostname.
  *
@@ -6334,14 +6314,7 @@ module.exports = () => {};
 
 /***/ }),
 
-/***/ 6:
-/***/ (function(module, exports) {
-
-module.exports = __WEBPACK_EXTERNAL_MODULE_6__;
-
-/***/ }),
-
-/***/ 62:
+/***/ 521:
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(module, global) {var __WEBPACK_AMD_DEFINE_RESULT__;/*! https://mths.be/punycode v1.4.1 by @mathias */
@@ -6878,6 +6851,13 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_6__;
 }(this));
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(22)(module), __webpack_require__(15)))
+
+/***/ }),
+
+/***/ 6:
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_6__;
 
 /***/ }),
 
