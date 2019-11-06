064c012be6d89061fd4bd49f07a34b7dec2264cd
Bob Owen <bobowencode@gmail.com>
2019-11-06 20:25:59 +0000

    Bug 1580742: Allow sandboxed x86 GMP process to duplicate crashreporter handle to the arm64 main process. r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D51985

--- security/sandbox/win/src/remotesandboxbroker/RemoteSandboxBrokerChild.cpp
+++ security/sandbox/win/src/remotesandboxbroker/RemoteSandboxBrokerChild.cpp
@@ -50,8 +50,19 @@ mozilla::ipc::IPCResult RemoteSandboxBrokerChild::AnswerLaunchApp(
     envmap[towstring(env.name())] = towstring(env.value());
   }
 
+  // We need to add our parent as a target peer, so that the sandboxed child can
+  // duplicate handles to it for crash reporting. AddTargetPeer duplicates the
+  // handle, so we use a ScopedProcessHandle to automatically close ours.
+  ipc::ScopedProcessHandle parentProcHandle;
+  if (!base::OpenProcessHandle(OtherPid(), &parentProcHandle.rwget())) {
+    *aOutOk = false;
+    return IPC_OK();
+  }
+  mSandboxBroker.AddTargetPeer(parentProcHandle);
+
   if (!mSandboxBroker.SetSecurityLevelForGMPlugin(
-          AbstractSandboxBroker::SandboxLevel(aParams.sandboxLevel()))) {
+          AbstractSandboxBroker::SandboxLevel(aParams.sandboxLevel()),
+          /* aIsRemoteLaunch */ true)) {
     *aOutOk = false;
     return IPC_OK();
   }
