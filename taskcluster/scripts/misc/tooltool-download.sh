6b4475cbdcfd4f86759c4553e5818a1f2f616a8e
Dorel Luca <dluca@mozilla.com>
2019-11-06 23:20:13 +0200

    Backed out 2 changesets (bug 1594530) for Toolchain failure. a=backout
    Backed out changeset c1e66d6fc61e (bug 1594530)
    Backed out changeset 9c751b07dda1 (bug 1594530)

--- taskcluster/scripts/misc/tooltool-download.sh
+++ taskcluster/scripts/misc/tooltool-download.sh
@@ -19,13 +19,8 @@ if [ -e "$TOOLTOOL_AUTH_FILE" ]; then
 fi
 
 if [ -n "$TASKCLUSTER_PROXY_URL" ]; then
-    TOOLTOOL_HOST="tooltool.mozilla-releng.net"
-    LEGACY_TC_ROOT_URL="https://taskcluster.net"
-    if [ ${TASKCLUSTER_ROOT_URL:-${LEGACY_TC_ROOT_URL}} != ${LEGACY_TC_ROOT_URL} ]; then
-        TOOLTOOL_HOST="tooltool.staging.mozilla-releng.net/"
-    fi
     # When the worker has the relengapi proxy setup, use it.
-    TOOLTOOL_DL_FLAGS="${TOOLTOOL_DL_FLAGS=} --tooltool-url=${TASKCLUSTER_PROXY_URL}/${TOOLTOOL_URL}/"
+    TOOLTOOL_DL_FLAGS="${TOOLTOOL_DL_FLAGS=} --tooltool-url=${TASKCLUSTER_PROXY_URL}/tooltool.mozilla-releng.net/"
 fi
 
 if [ -n "$UPLOAD_DIR" ]; then
