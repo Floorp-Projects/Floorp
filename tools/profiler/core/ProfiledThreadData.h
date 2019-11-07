4513746ecbd9233a92715ab2e13df642df3834cd
Ciure Andrei <aciure@mozilla.com>
2019-11-07 16:37:00 +0200

    Backed out 3 changesets (bug 1572337, bug 1594015) for causing linting failures and build bustages CLOSED TREE
    Backed out changeset 25f074eb5074 (bug 1594015)
    Backed out changeset fc8a37c2c22c (bug 1572337)
    Backed out changeset 913d6bd82284 (bug 1572337)

--- tools/profiler/core/ProfiledThreadData.h
+++ tools/profiler/core/ProfiledThreadData.h
@@ -10,6 +10,7 @@
 #include "platform.h"
 #include "ProfileBufferEntry.h"
 #include "ThreadInfo.h"
+#include "ThreadResponsiveness.h"
 
 #include "js/ProfilingStack.h"
 #include "mozilla/TimeStamp.h"
@@ -42,10 +43,12 @@ class ProfileBuffer;
 // when the profiler is stopped.
 class ProfiledThreadData final {
  public:
-  ProfiledThreadData(ThreadInfo* aThreadInfo, nsIEventTarget* aEventTarget);
+  ProfiledThreadData(ThreadInfo* aThreadInfo, nsIEventTarget* aEventTarget,
+                     bool aIncludeResponsiveness);
   ~ProfiledThreadData();
 
   void NotifyUnregistered(uint64_t aBufferPosition) {
+    mResponsiveness.reset();
     mLastSample = mozilla::Nothing();
     MOZ_ASSERT(!mBufferPositionWhenReceivedJSContext,
                "JSContext should have been cleared before the thread was "
@@ -67,6 +70,13 @@ class ProfiledThreadData final {
 
   void StreamTraceLoggerJSON(JSContext* aCx, SpliceableJSONWriter& aWriter,
                              const mozilla::TimeStamp& aProcessStartTime);
+
+  // Returns nullptr if this is not the main thread, the responsiveness
+  // feature is not turned on, or if this thread is not being profiled.
+  ThreadResponsiveness* GetThreadResponsiveness() {
+    ThreadResponsiveness* responsiveness = mResponsiveness.ptrOr(nullptr);
+    return responsiveness;
+  }
 
   const RefPtr<ThreadInfo> Info() const { return mThreadInfo; }
 
@@ -98,6 +108,10 @@ class ProfiledThreadData final {
   // Group B:
   // The following fields are only used while this thread is alive and
   // registered. They become Nothing() once the thread is unregistered.
+
+  // A helper object that instruments nsIThreads to obtain responsiveness
+  // information about their event loop.
+  mozilla::Maybe<ThreadResponsiveness> mResponsiveness;
 
   // When sampling, this holds the position in ActivePS::mBuffer of the most
   // recent sample for this thread, or Nothing() if there is no sample for this
---
+++ tools/profiler/gecko/ThreadResponsiveness.h
@@ -0,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef ThreadResponsiveness_h
+#define ThreadResponsiveness_h
+
+#include "mozilla/Maybe.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/TimeStamp.h"
+#include "nsISupports.h"
+
+class CheckResponsivenessTask;
+class nsIEventTarget;
+
+// This class should only be used for the main thread.
+class ThreadResponsiveness {
+ public:
+  explicit ThreadResponsiveness(nsIEventTarget* aThread, bool aIsMainThread);
+
+  ~ThreadResponsiveness();
+
+  void Update();
+
+  // The number of milliseconds that elapsed since the last
+  // CheckResponsivenessTask was processed.
+  double GetUnresponsiveDuration(double aStartToNow_ms) const {
+    return aStartToNow_ms - *mStartToPrevTracer_ms;
+  }
+
+  bool HasData() const { return mStartToPrevTracer_ms.isSome(); }
+
+ private:
+  RefPtr<CheckResponsivenessTask> mActiveTracerEvent;
+  // The time at which the last CheckResponsivenessTask was processed, in
+  // milliseconds since process start (i.e. what you get from profiler_time()).
+  mozilla::Maybe<double> mStartToPrevTracer_ms;
+};
+
+#endif
