5abf1831a192a3c1497dc882c84cd27395855605
Edwin Takahashi <egao@mozilla.com>
2019-11-07 22:04:45 +0000

    Bug 1508320 - upgrade vendored psutil package to 5.6.3 r=ahal,gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D51764

--- third_party/python/psutil/psutil/arch/windows/wmi.h
+++ third_party/python/psutil/psutil/arch/windows/wmi.h
@@ -4,7 +4,9 @@
  * found in the LICENSE file.
  */
 
-#include <Python.h>
-#include <windows.h>
 
-PyObject* psutil_get_open_files(long pid, HANDLE processHandle);
+
+#include <Python.h>
+
+PyObject* psutil_init_loadavg_counter();
+PyObject* psutil_get_loadavg();
