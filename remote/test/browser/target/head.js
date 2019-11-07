15c37c5653d0bbd9f1595e3e34be2aa2621e6465
Henrik Skupin <mail@hskupin.info>
2019-11-07 14:46:42 +0000

    Bug 1592643 - [remote] Refactor and improve browser chrome tests for Target domain. r=remote-protocol-reviewers,ato
    Differential Revision: https://phabricator.services.mozilla.com/D51998
    
    --HG--
    rename : remote/test/browser/target/browser_target.js => remote/test/browser/target/browser_sendMessageToTarget.js
    extra : moz-landing-system : lando

--- remote/test/browser/target/head.js
+++ remote/test/browser/target/head.js
@@ -9,3 +9,35 @@ Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/remote/test/browser/head.js",
   this
 );
+
+// Wait for all Target.targetCreated events. One for each tab, plus the one
+// for the main process target.
+async function getDiscoveredTargets(Target) {
+  return new Promise(resolve => {
+    const targets = [];
+
+    const unsubscribe = Target.targetCreated(target => {
+      targets.push(target);
+
+      if (targets.length >= gBrowser.tabs.length + 1) {
+        unsubscribe();
+        resolve(targets);
+      }
+    });
+
+    Target.setDiscoverTargets({ discover: true });
+  });
+}
+
+async function openTab(Target) {
+  info("Create a new tab and wait for the target to be created");
+
+  const targetCreated = Target.targetCreated();
+  const newTab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
+  const { targetInfo } = await targetCreated;
+
+  is(targetInfo.type, "page");
+  info(`New tab with target id ${targetInfo.targetId} created`);
+
+  return { targetInfo, newTab };
+}
