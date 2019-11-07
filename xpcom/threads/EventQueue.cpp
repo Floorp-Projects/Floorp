a511b769d3046004b37a12ae1d4eef8bef6261d7
Randell Jesup <rjesup@wgate.com>
2019-11-07 12:53:28 +0000

    Bug 1572337: Monitor running event delays and start times r=froydnj
    This lets us determine the time that an event has been running, and the time
    that the event spent queued - which can be used to figure out 'jank' at the
    time the event was queued. For PrioritizedEventQueues, only if such queuing
    would delay an input event then the queuing delay is reported.
    
    Differential Revision: https://phabricator.services.mozilla.com/D41279

--- xpcom/threads/EventQueue.cpp
+++ xpcom/threads/EventQueue.cpp
@@ -13,20 +13,58 @@ EventQueue::EventQueue(EventQueuePriority aPriority) {}
 
 void EventQueue::PutEvent(already_AddRefed<nsIRunnable>&& aEvent,
                           EventQueuePriority aPriority,
-                          const MutexAutoLock& aProofOfLock) {
+                          const MutexAutoLock& aProofOfLock,
+                          mozilla::TimeDuration* aDelay) {
+#ifdef MOZ_GECKO_PROFILER
+  // Sigh, this doesn't check if this thread is being profiled
+  if (profiler_is_active()) {
+    // check to see if the profiler has been enabled since the last PutEvent
+    while (mDispatchTimes.Count() < mQueue.Count()) {
+      mDispatchTimes.Push(TimeStamp());
+    }
+    mDispatchTimes.Push(aDelay ? TimeStamp::Now() - *aDelay : TimeStamp::Now());
+  } else {
+    // XXX clear queues when profiler is turned off without adding (much)
+    // overhead instead of doing this
+    mDispatchTimes.Push(TimeStamp());
+  }
+#endif
+
   nsCOMPtr<nsIRunnable> event(aEvent);
   mQueue.Push(std::move(event));
 }
 
 already_AddRefed<nsIRunnable> EventQueue::GetEvent(
-    EventQueuePriority* aPriority, const MutexAutoLock& aProofOfLock) {
+    EventQueuePriority* aPriority, const MutexAutoLock& aProofOfLock,
+    mozilla::TimeDuration* aLastEventDelay) {
   if (mQueue.IsEmpty()) {
+    if (aLastEventDelay) {
+      *aLastEventDelay = TimeDuration();
+    }
     return nullptr;
   }
 
   if (aPriority) {
     *aPriority = EventQueuePriority::Normal;
   }
+
+#ifdef MOZ_GECKO_PROFILER
+  if (profiler_is_active()) {
+    // check to see if the profiler has been enabled since the last PutEvent
+    while (mDispatchTimes.Count() < mQueue.Count()) {
+      mDispatchTimes.Push(TimeStamp());
+    }
+    TimeStamp dispatch_time = mDispatchTimes.Pop();
+    if (!dispatch_time.IsNull()) {
+      if (aLastEventDelay) {
+        *aLastEventDelay = TimeStamp::Now() - dispatch_time;
+      }
+    }
+  } else {
+    (void)mDispatchTimes.Pop();
+  }
+  // XXX clear queues when profiler is turned off without adding (much) overhead
+#endif
 
   nsCOMPtr<nsIRunnable> result = mQueue.Pop();
   return result.forget();
