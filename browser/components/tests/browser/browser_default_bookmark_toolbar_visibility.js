f6890b0c4c201d495782f6066f58095584189d09
Mike Conley <mconley@mozilla.com>
2019-11-07 21:41:04 +0000

    Bug 1553510 - Don't compute whether or not to show the Bookmarks Toolbar for new profiles until Places has finished initting. r=MattN
    This causes BrowserGlue to wait until Places has notified that it's initted before checking
    to see whether or not the Bookmarks Toolbar should be shown.
    
    Also, if it turns out that the Bookmarks Toolbar is shown, we now use CustomizableUI to do
    this, which means that the Bookmarks Toolbar will be made visible on all windows after the
    check is run - not just new windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51701

--- browser/components/tests/browser/browser_default_bookmark_toolbar_visibility.js
+++ browser/components/tests/browser/browser_default_bookmark_toolbar_visibility.js
@@ -8,6 +8,26 @@
  * in NUM_TOOLBAR_BOOKMARKS_TO_UNHIDE may need to be adjusted there.
  */
 add_task(async function test_default_bookmark_toolbar_visibility() {
+  // The Bookmarks Toolbar visibility state should be set only after
+  // Places has notified that it's done initializing.
+  const browserGlue = Cc["@mozilla.org/browser/browserglue;1"].getService(
+    Ci.nsIObserver
+  );
+
+  let placesInitCompleteObserved = TestUtils.topicObserved(
+    "places-browser-init-complete"
+  );
+
+  // If places-browser-init-complete has already notified, this will cause it
+  // to notify again. Otherwise, we wait until the notify is done.
+  browserGlue.observe(
+    null,
+    "browser-glue-test",
+    "places-browser-init-complete"
+  );
+
+  await placesInitCompleteObserved;
+
   const BROWSER_DOCURL = AppConstants.BROWSER_CHROME_URL;
   let xulStore = Services.xulStore;
 
