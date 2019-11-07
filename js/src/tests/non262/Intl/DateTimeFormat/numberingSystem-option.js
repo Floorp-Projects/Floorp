763838f4f8c9c0b6326b5d0cf5802d4fb6f2218b
André Bargull <andre.bargull@gmail.com>
2019-11-07 16:31:12 +0000

    Bug 1560355 - Part 3: Add support for "numberingSystem" and "calendar" options. r=jwalden
    Currently restricted to Nightly-only, because the spec PR still contains bugs
    and there are no test262 tests for this feature (except for tests to ensure the
    properties are retrieved from the options object).
    
    Differential Revision: https://phabricator.services.mozilla.com/D51855

---
+++ js/src/tests/non262/Intl/DateTimeFormat/numberingSystem-option.js
@@ -0,0 +1,64 @@
+// |reftest| skip-if(release_or_beta)
+
+const defaultLocale = "en";
+const defaultNumberingSystem = new Intl.DateTimeFormat(defaultLocale).resolvedOptions().numberingSystem;
+
+function createWithLocale(locale, numberingSystem) {
+  return new Intl.DateTimeFormat(locale, {numberingSystem});
+}
+
+function create(numberingSystem) {
+  return createWithLocale(defaultLocale, numberingSystem);
+}
+
+// Empty string should throw.
+assertThrowsInstanceOf(() => create(""), RangeError);
+
+// Trailing \0 should throw.
+assertThrowsInstanceOf(() => create("latn\0"), RangeError);
+
+// Too short or too long strings should throw.
+assertThrowsInstanceOf(() => create("a"), RangeError);
+assertThrowsInstanceOf(() => create("toolongstring"), RangeError);
+
+// Throw even when prefix is valid.
+assertThrowsInstanceOf(() => create("latn-toolongstring"), RangeError);
+
+// |numberingSystem| can be set to |undefined|.
+let dtf = create(undefined);
+assertEq(dtf.resolvedOptions().numberingSystem, defaultNumberingSystem);
+
+// Unsupported numbering systems are ignored.
+dtf = create("xxxxxxxx");
+assertEq(dtf.resolvedOptions().numberingSystem, defaultNumberingSystem);
+
+// Numbering system in options overwrite Unicode extension keyword.
+dtf = createWithLocale(`${defaultLocale}-u-nu-thai`, "arab");
+assertEq(dtf.resolvedOptions().locale, defaultLocale);
+assertEq(dtf.resolvedOptions().numberingSystem, "arab");
+
+// |numberingSystem| option ignores case.
+dtf = create("ARAB");
+assertEq(dtf.resolvedOptions().locale, defaultLocale);
+assertEq(dtf.resolvedOptions().numberingSystem, "arab");
+
+const numberingSystems = [
+  "arab", "arabext", "bali", "beng", "deva",
+  "fullwide", "gujr", "guru", "hanidec", "khmr",
+  "knda", "laoo", "latn", "limb", "mlym",
+  "mong", "mymr", "orya", "tamldec", "telu",
+  "thai", "tibt",
+];
+
+for (let numberingSystem of numberingSystems) {
+  let dtf1 = new Intl.DateTimeFormat(`${defaultLocale}-u-nu-${numberingSystem}`);
+  let dtf2 = new Intl.DateTimeFormat(defaultLocale, {numberingSystem});
+
+  assertEq(dtf1.resolvedOptions().numberingSystem, numberingSystem);
+  assertEq(dtf2.resolvedOptions().numberingSystem, numberingSystem);
+
+  assertEq(dtf2.format(0), dtf1.format(0));
+}
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
