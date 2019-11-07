fff62b5bd8aa7e054b7bf5bf19ed1c14c1c05a87
Byron Campen [:bwc] <docfaraday@gmail.com>
2019-11-07 23:07:09 +0000

    Bug 1588588: Get MediaPipeline to use the new timestamp implementation, and fix a typo in how we calculate expiries for CSRC stats. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D50113

--- media/webrtc/signaling/src/mediapipeline/MediaPipeline.cpp
+++ media/webrtc/signaling/src/mediapipeline/MediaPipeline.cpp
@@ -1664,14 +1664,14 @@ void MediaPipelineReceiveVideo::OnRtpPacketReceived() {
   }
 }
 
-DOMHighResTimeStamp MediaPipeline::GetNow() {
-  return webrtc::Clock::GetRealTimeClock()->TimeInMilliseconds();
+DOMHighResTimeStamp MediaPipeline::GetNow() const {
+  return Conduit()->GetNow();
 }
 
 DOMHighResTimeStamp MediaPipeline::RtpCSRCStats::GetExpiryFromTime(
     const DOMHighResTimeStamp aTime) {
   // DOMHighResTimeStamp is a unit measured in ms
-  return aTime - EXPIRY_TIME_MILLISECONDS;
+  return aTime + EXPIRY_TIME_MILLISECONDS;
 }
 
 MediaPipeline::RtpCSRCStats::RtpCSRCStats(const uint32_t aCsrc,
