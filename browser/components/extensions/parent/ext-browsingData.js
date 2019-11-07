47987c557246ffc0539de4b31ffe21c7b59060ac
Luca Greco <lgreco@mozilla.com>
2019-11-07 17:00:42 +0000

    Bug 1575891 - browsingData API should not clear nextGen localStorage data stored by browser components and extensions. r=mixedpuppy,asuth,johannh a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D48535
    
    --HG--
    extra : source : 4b4c2fa6e6fa6bac0227b1ebe5f7e185de7116a0

--- browser/components/extensions/parent/ext-browsingData.js
+++ browser/components/extensions/parent/ext-browsingData.js
@@ -191,24 +191,34 @@ const clearLocalStorage = async function(options) {
           let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
             item.origin
           );
-          let host = principal.URI.hostPort;
-          if (!options.hostnames || options.hostnames.includes(host)) {
-            promises.push(
-              new Promise((resolve, reject) => {
-                let clearRequest = quotaManagerService.clearStoragesForPrincipal(
-                  principal,
-                  "default",
-                  "ls"
-                );
-                clearRequest.callback = () => {
-                  if (clearRequest.resultCode == Cr.NS_OK) {
-                    resolve();
-                  } else {
-                    reject({ message: "Clear localStorage failed" });
-                  }
-                };
-              })
-            );
+          // Consistently to removeIndexedDB and the API documentation for
+          // removeLocalStorage, we should only clear the data stored by
+          // regular websites, on the contrary we shouldn't clear data stored
+          // by browser components (like about:newtab) or other extensions.
+          if (
+            principal.schemeIs("http") ||
+            principal.schemeIs("https") ||
+            principal.schemeIs("file")
+          ) {
+            let host = principal.URI.hostPort;
+            if (!options.hostnames || options.hostnames.includes(host)) {
+              promises.push(
+                new Promise((resolve, reject) => {
+                  let clearRequest = quotaManagerService.clearStoragesForPrincipal(
+                    principal,
+                    "default",
+                    "ls"
+                  );
+                  clearRequest.callback = () => {
+                    if (clearRequest.resultCode == Cr.NS_OK) {
+                      resolve();
+                    } else {
+                      reject({ message: "Clear localStorage failed" });
+                    }
+                  };
+                })
+              );
+            }
           }
         }
 
