e0922a5784287b8f9123f5f582bfe2e9e026a48d
Tom Prince <mozilla@hocat.ca>
2019-11-07 16:07:45 -0700

    Bug 1594530: [firefox-ci] Fix staging tooltool URL; r=Callek a=tomrpince CLOSED TREE
    MANUAL PUSH: Firefox-CI migration testing
    
    Differential Revision: https://phabricator.services.mozilla.com/D52125

--- testing/mozharness/configs/repackage/linux64_signed.py
+++ testing/mozharness/configs/repackage/linux64_signed.py
@@ -5,7 +5,7 @@ platform = "linux64"
 tooltool_url = 'http://taskcluster/tooltool.mozilla-releng.net/'
 if os.environ.get('TASKCLUSTER_ROOT_URL', 'https://taskcluster.net') != 'https://taskcluster.net':
     # Pre-point tooltool at staging cluster so we can run in parallel with legacy cluster
-    tooltool_url = 'http://taskcluster/tooltool.staging.mozilla-releng.net/'
+    tooltool_url = 'http://taskcluster/stage.tooltool.mozilla-releng.net/'
 
 config = {
     "locale": os.environ.get("LOCALE"),
