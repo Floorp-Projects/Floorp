85504dd5147357be1e440ac6a7755b6c2ec45e36
Henrik Skupin <mail@hskupin.info>
2019-11-07 19:01:38 +0000

    Bug 1587846 - [remote] Add "format" option to Page.captureScreenshot. r=remote-protocol-reviewers,ato
    Differential Revision: https://phabricator.services.mozilla.com/D52145

--- remote/test/browser/page/browser_captureScreenshot.js
+++ remote/test/browser/page/browser_captureScreenshot.js
@@ -3,47 +3,6 @@
 
 "use strict";
 
-async function getDevicePixelRatio() {
-  return ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
-    return content.devicePixelRatio;
-  });
-}
-
-async function getImageDetails(format, data) {
-  return ContentTask.spawn(
-    gBrowser.selectedBrowser,
-    { format, data },
-    async function({ format, data }) {
-      return new Promise(resolve => {
-        const img = new content.Image();
-        img.addEventListener(
-          "load",
-          () => {
-            resolve({
-              width: img.width,
-              height: img.height,
-            });
-          },
-          { once: true }
-        );
-
-        img.src = `data:image/${format};base64,${data}`;
-      });
-    }
-  );
-}
-
-async function getViewportRect() {
-  return ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
-    return {
-      left: content.pageXOffset,
-      top: content.pageYOffset,
-      width: content.innerWidth,
-      height: content.innerHeight,
-    };
-  });
-}
-
 add_task(async function documentSmallerThanViewport({ Page }) {
   loadURL(toDataURL("<div>Hello world"));
 
@@ -53,7 +12,9 @@ add_task(async function documentSmallerThanViewport({ Page }) {
 
   const scale = await getDevicePixelRatio();
   const viewportRect = await getViewportRect();
-  const { width, height } = await getImageDetails("png", data);
+  const { mimeType, width, height } = await getImageDetails(data);
+
+  is(mimeType, "image/png", "Screenshot has correct MIME type");
   is(width, (viewportRect.width - viewportRect.left) * scale);
   is(height, (viewportRect.height - viewportRect.top) * scale);
 });
@@ -67,8 +28,106 @@ add_task(async function documentLargerThanViewport({ Page }) {
 
   const scale = await getDevicePixelRatio();
   const viewportRect = await getViewportRect();
-  const { width, height } = await getImageDetails("png", data);
+  const { mimeType, width, height } = await getImageDetails(data);
 
+  is(mimeType, "image/png", "Screenshot has correct MIME type");
   is(width, (viewportRect.width - viewportRect.left) * scale);
   is(height, (viewportRect.height - viewportRect.top) * scale);
 });
+
+add_task(async function invalidFormat({ Page }) {
+  loadURL(toDataURL("<div>Hello world"));
+
+  let exceptionThrown = false;
+  try {
+    await Page.captureScreenshot({ format: "foo" });
+  } catch (e) {
+    exceptionThrown = true;
+  }
+  ok(
+    exceptionThrown,
+    "captureScreenshot raised error for invalid image format"
+  );
+});
+
+add_task(async function asJPEGFormat({ Page }) {
+  loadURL(toDataURL("<div>Hello world"));
+
+  info("Check that captureScreenshot() captures as JPEG format");
+  const { data } = await Page.captureScreenshot({ format: "jpeg" });
+  ok(!!data, "Screenshot data is not empty");
+
+  const scale = await getDevicePixelRatio();
+  const viewportRect = await getViewportRect();
+  const { mimeType, height, width } = await getImageDetails(data);
+
+  is(mimeType, "image/jpeg", "Screenshot has correct MIME type");
+  is(width, (viewportRect.width - viewportRect.left) * scale);
+  is(height, (viewportRect.height - viewportRect.top) * scale);
+});
+
+async function getDevicePixelRatio() {
+  return ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+    return content.devicePixelRatio;
+  });
+}
+
+async function getImageDetails(image) {
+  const mimeType = getMimeType(image);
+
+  return ContentTask.spawn(
+    gBrowser.selectedBrowser,
+    { mimeType, image },
+    async function({ mimeType, image }) {
+      return new Promise(resolve => {
+        const img = new content.Image();
+        img.addEventListener(
+          "load",
+          () => {
+            resolve({
+              mimeType,
+              width: img.width,
+              height: img.height,
+            });
+          },
+          { once: true }
+        );
+
+        img.src = `data:${mimeType};base64,${image}`;
+      });
+    }
+  );
+}
+
+function getMimeType(image) {
+  // Decode from base64 and convert the first 4 bytes to hex
+  const raw = atob(image).slice(0, 4);
+  let magicBytes = "";
+  for (let i = 0; i < raw.length; i++) {
+    magicBytes += raw
+      .charCodeAt(i)
+      .toString(16)
+      .toUpperCase();
+  }
+
+  switch (magicBytes) {
+    case "89504E47":
+      return "image/png";
+    case "FFD8FFDB":
+    case "FFD8FFE0":
+      return "image/jpeg";
+    default:
+      throw new Error("Unknown MIME type");
+  }
+}
+
+async function getViewportRect() {
+  return ContentTask.spawn(gBrowser.selectedBrowser, null, function() {
+    return {
+      left: content.pageXOffset,
+      top: content.pageYOffset,
+      width: content.innerWidth,
+      height: content.innerHeight,
+    };
+  });
+}
