56d362ecfcb9ad88683ef86b733c53d97fb1dd59
Jan Odvarko <odvarko@gmail.com>
2019-11-08 18:40:04 +0000

    Bug 1592692 - Enable Resource blocking in all channels r=Harald a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D52335

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2127,11 +2127,7 @@ pref("devtools.netmonitor.enabled", true);
   pref("devtools.netmonitor.features.search", false);
 #endif
 
-#if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
-  pref("devtools.netmonitor.features.requestBlocking", true);
-#else
-  pref("devtools.netmonitor.features.requestBlocking", false);
-#endif
+pref("devtools.netmonitor.features.requestBlocking", true);
 
 // Enable the Application panel
 pref("devtools.application.enabled", false);
