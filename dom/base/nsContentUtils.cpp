f52491766c9b841c0a71948b7f59160e9bc2a962
Emma Malysz <emalysz@mozilla.com>
2019-11-08 00:10:59 +0000

    Bug 1594874, remove xbl.properties and instances r=zbraniecki
    Differential Revision: https://phabricator.services.mozilla.com/D52281
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -3550,7 +3550,6 @@ void nsContentUtils::GetEventArgNames(int32_t aNameSpaceID, nsAtom* aEventName,
 static const char* gPropertiesFiles[nsContentUtils::PropertiesFile_COUNT] = {
     // Must line up with the enum values in |PropertiesFile| enum.
     "chrome://global/locale/css.properties",
-    "chrome://global/locale/xbl.properties",
     "chrome://global/locale/xul.properties",
     "chrome://global/locale/layout_errors.properties",
     "chrome://global/locale/layout/HtmlForm.properties",
