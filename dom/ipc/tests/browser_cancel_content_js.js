fc952ea9fa1b44056953474bbf314d68002ad607
Jim Porter <jporter@mozilla.com>
2019-11-08 02:43:00 +0000

    Bug 1592470 - Don't cancel content JS when "navigating" to javascript: URLs; r=smaug
    Prior to this patch, users entering javascript: URLs in the location bar
    (e.g. via a bookmark) would trigger the cancel content JS path for the page
    they were on, potentially breaking the site. Since javascript: URLs
    (generally) don't navigate to a new page, we should avoid canceling the
    current page's JS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51951

--- dom/ipc/tests/browser_cancel_content_js.js
+++ dom/ipc/tests/browser_cancel_content_js.js
@@ -1,47 +1,67 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 "use strict";
+
+requestLongerTimeout(10);
 
 const TEST_PAGE =
   "http://mochi.test:8888/browser/dom/ipc/tests/file_cancel_content_js.html";
 const NEXT_PAGE = "https://example.org/";
+const JS_URI = "javascript:void(document.title = 'foo')";
 
 function sleep(ms) {
   // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function test_navigation(cancelContentJS) {
+async function test_navigation(nextPage, cancelContentJSPref, shouldCancel) {
   await SpecialPowers.pushPrefEnv({
-    set: [["dom.ipc.cancel_content_js_when_navigating", cancelContentJS]],
+    set: [["dom.ipc.cancel_content_js_when_navigating", cancelContentJSPref]],
   });
   let tab = await BrowserTestUtils.openNewForegroundTab({
     gBrowser,
     opening: TEST_PAGE,
   });
 
+  const loopEnded = ContentTask.spawn(tab.linkedBrowser, {}, async function() {
+    return new Promise(resolve => {
+      content.window.addEventListener("LongLoopEnded", resolve);
+    });
+  });
+
+  // Wait for the test page's long-running JS loop to start; it happens ~500ms
+  // after load.
   await sleep(1000);
-  BrowserTestUtils.loadURI(gBrowser, NEXT_PAGE);
-  const nextPageLoaded = BrowserTestUtils.browserLoaded(
-    tab.linkedBrowser,
-    false,
-    NEXT_PAGE
+
+  info(
+    `navigating to ${nextPage} with cancel content JS ${
+      cancelContentJSPref ? "enabled" : "disabled"
+    }`
   );
+  const nextPageLoaded = BrowserTestUtils.waitForContentEvent(
+    tab.linkedBrowser,
+    "DOMTitleChanged"
+  );
+  BrowserTestUtils.loadURI(gBrowser, nextPage);
+
   const result = await Promise.race([
     nextPageLoaded,
-    sleep(1000).then(() => "timeout"),
+    loopEnded.then(() => "timeout"),
   ]);
 
   const timedOut = result === "timeout";
-  if (cancelContentJS) {
+  if (shouldCancel) {
     ok(timedOut === false, "expected next page to be loaded");
   } else {
     ok(timedOut === true, "expected timeout");
-    // Give the page time to finish its long-running JS before we close the
-    // tab...
-    await nextPageLoaded;
   }
 
   BrowserTestUtils.removeTab(tab);
 }
 
-add_task(async () => test_navigation(true));
-add_task(async () => test_navigation(false));
+add_task(async () => test_navigation(NEXT_PAGE, true, true));
+add_task(async () => test_navigation(NEXT_PAGE, false, false));
+add_task(async () => test_navigation(JS_URI, true, false));
+add_task(async () => test_navigation(JS_URI, false, false));
