f135df206ca7e5f09284dbffb4f5210568cda1c3
Andy Wingo <wingo@igalia.com>
2019-11-08 14:17:20 +0000

    Bug 1593027 - Add WebAssembly multi-value codegen and run-time tests r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D51287

---
+++ js/src/jit-test/tests/wasm/multi-value/block-run.js
@@ -0,0 +1,228 @@
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.add
+        (block (result i32 i32)
+          (i32.const 32)
+          (i32.const 10)))))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32) (i32.const 10)
+      (block (param i32 i32) (result i32 i32))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (block (param i32) (result i32 i32)
+        (i32.const 10))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.add
+        (loop (result i32 i32)
+          (i32.const 32)
+          (i32.const 10)))))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32) (i32.const 10)
+      (loop (param i32 i32) (result i32 i32))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (loop (param i32) (result i32 i32)
+        (i32.const 10))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.add
+        (block (result i32 i32)
+          (i32.const 32)
+          (i32.const 10)
+          (br 0)))))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (block (param i32 i32) (result i32 i32)
+        (br 0))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (block (param i32) (result i32 i32)
+        (i32.const 10)
+        (br 0))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.add
+        (block (result i32 i32)
+          (i32.const 32)
+          (i32.const 10)
+          (block (param i32 i32) (result i32 i32)
+            (br 1))))))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (block (param i32 i32) (result i32 i32)
+        (block (param i32 i32) (result i32 i32)
+          (br 1)))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (block (param i32 i32) (result i32 i32)
+        (block (param i32 i32) (result i32 i32)
+          (br 0)))
+      (i32.add)))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (block (param i32) (result i32 i32)
+        (i32.const 10)
+        (block (param i32 i32) (result i32 i32)
+          (br 1)))
+      (i32.add)))`,
+            42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 1)
+      (if (param i32 i32) (result i32)
+          (then (i32.add))
+          (else (i32.sub)))))`,
+             42);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 1)
+      (if (param i32 i32) (result i32 i32)
+          (then
+            (drop)
+            (drop)
+            (i32.const 10)
+            (i32.const 32)))
+      (i32.sub)))`,
+             -22);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 0)
+      (if (param i32 i32) (result i32 i32)
+          (then
+            (drop)
+            (drop)
+            (i32.const 10)
+            (i32.const 32)))
+      (i32.sub)))`,
+             22);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 1)
+      (if (param i32 i32) (result i32 i32)
+          (then (return)))
+      (i32.sub)))`,
+             10);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 0)
+      (if (param i32 i32) (result i32 i32)
+          (then (return)))
+      (i32.sub)))`,
+             22);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 1)
+      (if (param i32 i32) (result i32 i32)
+          (then)
+          (else (return)))
+      (i32.sub)))`,
+             22);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (i32.const 32)
+      (i32.const 10)
+      (i32.const 0)
+      (if (param i32 i32) (result i32 i32)
+          (then)
+          (else (return)))
+      (i32.sub)))`,
+             10);
+
+wasmFullPass(`
+  (module
+    (func (export "run") (result i32)
+      (local $i i32)
+      (i32.const 0) ;; sum
+      (i32.const 10) ;; i
+      (loop $loop (param i32 i32) (result i32)
+        (local.tee $i)
+        (i32.add) ;; sum = i + sum
+        (i32.sub (local.get $i) (i32.const 1))
+        (i32.eqz (local.tee $i))
+        (if (param i32) (result i32)
+            (then)
+            (else (local.get $i) (br $loop))))))`,
+            55);
