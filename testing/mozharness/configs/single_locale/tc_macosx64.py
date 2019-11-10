ae1fff72804ad4db0915e84368174a3c13d531d9
Andreea Pavel <apavel@mozilla.com>
2019-11-10 05:33:35 +0200

    Backed out changeset 69049d8a2c4b (bug 1594530) on tomprince's request. CLOSED TREE

--- testing/mozharness/configs/single_locale/tc_macosx64.py
+++ testing/mozharness/configs/single_locale/tc_macosx64.py
@@ -1,10 +1,5 @@
 import os
 
-tooltool_url = 'http://taskcluster/tooltool.mozilla-releng.net/'
-if os.environ.get('TASKCLUSTER_ROOT_URL', 'https://taskcluster.net') != 'https://taskcluster.net':
-    # Pre-point tooltool at staging cluster so we can run in parallel with legacy cluster
-    tooltool_url = 'http://taskcluster/tooltool.staging.mozilla-releng.net/'
-
 config = {
     "bootstrap_env": {
         "NO_MERCURIAL_SETUP_CHECK": "1",
@@ -18,7 +13,7 @@ config = {
         'UPLOAD_PATH': '/builds/worker/artifacts/',
     },
 
-    "tooltool_url": tooltool_url,
+    "tooltool_url": 'http://taskcluster/tooltool.mozilla-releng.net/',
 
     "vcs_share_base": "/builds/hg-shared",
 }
