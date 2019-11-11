e968fd472c897b1bdb2b46a727563a96b2b26878
Brian Hackett <bhackett1024@gmail.com>
2019-11-11 12:30:31 +0000

    Bug 1594975 - Fix ThreadActor.frames argument, r=jlast.
    Differential Revision: https://phabricator.services.mozilla.com/D52477

--- devtools/client/webreplay/mochitest/head.js
+++ devtools/client/webreplay/mochitest/head.js
@@ -104,7 +104,7 @@ async function checkEvaluateInTopFrameThrows(dbg, text) {
   const consoleFront = await dbg.toolbox.target.getFront("console");
   const { frames } = await threadFront.getFrames(0, 1);
   ok(frames.length == 1, "Got one frame");
-  const options = { thread: threadFront.actor, frameActor: frames[0].actor };
+  const options = { thread: threadFront.actor, frameActor: frames[0].actorID };
   const response = await consoleFront.evaluateJSAsync(text, options);
   ok(response.exception, "Eval threw an exception");
 }
