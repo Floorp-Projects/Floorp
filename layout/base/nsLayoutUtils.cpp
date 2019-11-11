27acc9722e13dafcf17d8b77f24b14022d0bf940
Botond Ballo <botond@mozilla.com>
2019-11-11 09:23:45 +0000

    Bug 1589022 - Ignore displayport properties in GetDisplayPortImpl() if APZ is disabled. r=tnikkel
    This facilitates disabling APZ "live", such as when moving a tab from an APZ
    window into a non-APZ window.
    
    Depends on D51468
    
    Differential Revision: https://phabricator.services.mozilla.com/D51469
    
    --HG--
    extra : moz-landing-system : lando

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -1068,6 +1068,11 @@ static bool GetDisplayPortImpl(
     return false;
   }
 
+  nsIFrame* frame = aContent->GetPrimaryFrame();
+  if (frame && !frame->PresShell()->AsyncPanZoomEnabled()) {
+    return false;
+  }
+
   if (!aResult) {
     // We have displayport data, but the caller doesn't want the actual
     // rect, so we don't need to actually compute it.
@@ -1076,7 +1081,6 @@ static bool GetDisplayPortImpl(
 
   bool isDisplayportSuppressed = false;
 
-  nsIFrame* frame = aContent->GetPrimaryFrame();
   if (frame) {
     nsPresContext* presContext = frame->PresContext();
     MOZ_ASSERT(presContext);
