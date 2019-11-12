537c377bbe9cbbfaca00d2a88104a782272c91ad
Greg Tatum <gtatum@mozilla.com>
2019-11-12 16:55:46 +0000

    Bug 1592625 - All threads' allocation markers get stored on the main thread; r=gerald
    This change makes it so that all allocations (from any thread in a
    process) are stored on the main thread in the profile. This way it's
    easy to balance allocations with frees. Memory happens more in a
    per-process model, than a per-thread model. The front-end can then
    process the information and display it in more interesting ways.
    
    Before allocations and deallocations were only stored on the
    thread where they were being generated. It was easy to miss
    deallocations with the old model.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51937
    
    --HG--
    extra : moz-landing-system : lando

--- tools/profiler/core/memory_hooks.cpp
+++ tools/profiler/core/memory_hooks.cpp
@@ -270,6 +270,12 @@ class ThreadIntercept {
                          mozilla::recordreplay::Behavior::DontPreserve>
       sAllocationsFeatureEnabled;
 
+  // The markers will be stored on the main thread. Retain the id to the main
+  // thread of this process here.
+  static mozilla::Atomic<int, mozilla::Relaxed,
+                         mozilla::recordreplay::Behavior::DontPreserve>
+      sMainThreadId;
+
   ThreadIntercept() = default;
 
   // Only allow consumers to access this information if they run
@@ -316,15 +322,25 @@ class ThreadIntercept {
 
   bool IsBlocked() const { return ThreadIntercept::IsBlocked_(); }
 
-  static void EnableAllocationFeature() { sAllocationsFeatureEnabled = true; }
+  static void EnableAllocationFeature(int aMainThreadId) {
+    sAllocationsFeatureEnabled = true;
+    sMainThreadId = aMainThreadId;
+  }
 
   static void DisableAllocationFeature() { sAllocationsFeatureEnabled = false; }
+
+  static int MainThreadId() { return sMainThreadId; }
 };
 
 PROFILER_THREAD_LOCAL(bool) ThreadIntercept::tlsIsBlocked;
+
 mozilla::Atomic<bool, mozilla::Relaxed,
                 mozilla::recordreplay::Behavior::DontPreserve>
     ThreadIntercept::sAllocationsFeatureEnabled(false);
+
+mozilla::Atomic<int, mozilla::Relaxed,
+                mozilla::recordreplay::Behavior::DontPreserve>
+    ThreadIntercept::sMainThreadId(0);
 
 // An object of this class must be created (on the stack) before running any
 // code that might allocate.
@@ -382,7 +398,8 @@ static void AllocCallback(void* aPtr, size_t aReqSize) {
       // First perform the Bernoulli trial.
       gBernoulli->trial(actualSize) &&
       // Second, attempt to add a marker if the Bernoulli trial passed.
-      profiler_add_native_allocation_marker(static_cast<int64_t>(actualSize))) {
+      profiler_add_native_allocation_marker(ThreadIntercept::MainThreadId(),
+                                            static_cast<int64_t>(actualSize))) {
     MOZ_ASSERT(gAllocationTracker,
                "gAllocationTracker must be properly installed for the memory "
                "hooks.");
@@ -424,7 +441,8 @@ static void FreeCallback(void* aPtr) {
       "gAllocationTracker must be properly installed for the memory hooks.");
   if (gAllocationTracker->RemoveMemoryAddressIfFound(aPtr)) {
     // This size here is negative, indicating a deallocation.
-    profiler_add_native_allocation_marker(signedSize);
+    profiler_add_native_allocation_marker(ThreadIntercept::MainThreadId(),
+                                          signedSize);
   }
 }
 
@@ -559,7 +577,7 @@ void install_memory_hooks() {
 // leak these values.
 void remove_memory_hooks() { jemalloc_replace_dynamic(nullptr); }
 
-void enable_native_allocations() {
+void enable_native_allocations(int aMainThreadId) {
   // The bloat log tracks allocations and deallocations. This can conflict
   // with the memory hook machinery, as the bloat log creates its own
   // allocations. This means we can re-enter inside the bloat log machinery. At
@@ -585,7 +603,7 @@ void enable_native_allocations() {
   if (!PR_GetEnv("XPCOM_MEM_BLOAT_LOG")) {
     EnsureBernoulliIsInstalled();
     EnsureAllocationTrackerIsInstalled();
-    ThreadIntercept::EnableAllocationFeature();
+    ThreadIntercept::EnableAllocationFeature(aMainThreadId);
   }
 }
 
