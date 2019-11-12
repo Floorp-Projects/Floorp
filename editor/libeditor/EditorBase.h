e62fc1b57381fa5ed59fcbca7fd4fdbcecd0a462
Mirko Brodesser <mbrodesser@mozilla.com>
2019-11-12 15:20:20 +0000

    Bug 1591417: part 2) Rename `HasMutationEventListeners` to `MaybeHasMutationEventListeners`. r=masayuki
    After removing all mutation event listeners, the corresponding method
    still returns `true`, so the new name is more accurate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52342

--- editor/libeditor/EditorBase.h
+++ editor/libeditor/EditorBase.h
@@ -211,7 +211,9 @@ class EditorBase : public nsIEditor,
   nsPIDOMWindowInner* GetInnerWindow() const {
     return mDocument ? mDocument->GetInnerWindow() : nullptr;
   }
-  bool HasMutationEventListeners(
+  // @param aMutationEventType One or multiple of NS_EVENT_BITS_MUTATION_*.
+  // @return true, iff at least one of NS_EVENT_BITS_MUTATION_* is set.
+  bool MaybeHasMutationEventListeners(
       uint32_t aMutationEventType = 0xFFFFFFFF) const {
     if (!mIsHTMLEditorClass) {
       // DOM mutation event listeners cannot catch the changes of
