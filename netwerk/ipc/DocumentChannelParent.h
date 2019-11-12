771adf8873031ea8167072d86ca21ec3bccded14
Jean-Yves Avenard <jyavenard@mozilla.com>
2019-11-12 23:35:20 +0000

    Bug 1588241 - P4. Remove nsIClassifiedChannel implementation from DocumentChannelChild. r=mattwoodrow
    The information is now passed to the parent channel once replacement is completed.
    
    Depends on D52262
    
    Differential Revision: https://phabricator.services.mozilla.com/D52263

--- netwerk/ipc/DocumentChannelParent.h
+++ netwerk/ipc/DocumentChannelParent.h
@@ -130,9 +130,22 @@ class DocumentChannelParent : public nsIInterfaceRequestor,
     uint32_t mClassificationFlags;
     bool mIsThirdParty;
   };
+
+  struct NotifyChannelClassifierProtectionDisabledParams {
+    uint32_t mAcceptedReason;
+  };
+
+  struct NotifyCookieAllowedParams {};
+
+  struct NotifyCookieBlockedParams {
+    uint32_t mRejectedReason;
+  };
+
   typedef mozilla::Variant<
       nsIHttpChannel::FlashPluginState, ClassifierMatchedInfoParams,
-      ClassifierMatchedTrackingInfoParams, ClassificationFlagsParams>
+      ClassifierMatchedTrackingInfoParams, ClassificationFlagsParams,
+      NotifyChannelClassifierProtectionDisabledParams,
+      NotifyCookieAllowedParams, NotifyCookieBlockedParams>
       IParentChannelFunction;
 
   // Store a list of all the attribute setters that have been called on this
