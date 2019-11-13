20c3b5adb53c639a682f7d30c49752250c3c8dbe
Sylvestre Ledru <sledru@mozilla.com>
2019-11-13 09:27:25 +0000

    Bug 1562642 - add missing license in layout/style/tools/ r=emilio
    Depends on D52592
    
    Differential Revision: https://phabricator.services.mozilla.com/D52593

--- layout/style/tools/cleanup_keywords.py
+++ layout/style/tools/cleanup_keywords.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 """
 Script to remove unused keywords from nsCSSKeywordList.
