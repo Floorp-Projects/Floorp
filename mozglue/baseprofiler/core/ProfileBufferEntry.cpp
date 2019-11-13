f542396ff9934cae479724ecdc416797b3e3e427
Randell Jesup <rjesup@wgate.com>
2019-11-13 14:21:52 +0000

    Bug 1595482: change "responsiveness" field to "eventDelay" in profiler r=canaltinova
    We want the profiler UI to be able to know if the data can be used for
    reconstructing the event delays, since it measures something different
    from the old 16ms event injection.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52534
    
    --HG--
    extra : moz-landing-system : lando

--- mozglue/baseprofiler/core/ProfileBufferEntry.cpp
+++ mozglue/baseprofiler/core/ProfileBufferEntry.cpp
@@ -347,7 +347,7 @@ static void WriteSample(SpliceableJSONWriter& aWriter,
   enum Schema : uint32_t {
     STACK = 0,
     TIME = 1,
-    RESPONSIVENESS = 2,
+    EVENT_DELAY = 2,
   };
 
   AutoArraySchemaWriter writer(aWriter, aUniqueStrings);
@@ -357,7 +357,7 @@ static void WriteSample(SpliceableJSONWriter& aWriter,
   writer.DoubleElement(TIME, aSample.mTime);
 
   if (aSample.mResponsiveness.isSome()) {
-    writer.DoubleElement(RESPONSIVENESS, *aSample.mResponsiveness);
+    writer.DoubleElement(EVENT_DELAY, *aSample.mResponsiveness);
   }
 }
 
