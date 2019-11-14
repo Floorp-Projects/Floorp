56b47c39ee87845e1d89aeff3aff060b4a27cb5a
alwu <alwu@mozilla.com>
2019-11-14 20:59:05 +0000

    Bug 1593131 - part3 : use nsISupport for ref counting r=chunmin
    Use` nsISupport` for ref counting, instead of using `NS_INLINE_DECL_THREADSAFE_REFCOUNTING`, then we can allow an usage such like inheritting from both `MediaControlKeysEventListener` and `MediaControlKeysEventSource` at the same time, which doesn't be allowed in old implementation because we can only have one parent class owning `mRefCnt`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52703
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/mediacontrol/MediaControlKeysEvent.h
+++ dom/media/mediacontrol/MediaControlKeysEvent.h
@@ -19,9 +19,8 @@ enum class MediaControlKeysEvent { ePlayPause, ePrev, eNext, eNone };
  * and then everytime when the media key events occur, `OnKeyPressed` will be
  * called so that we can do related handling.
  */
-class MediaControlKeysEventListener {
+class MediaControlKeysEventListener : public nsISupports {
  public:
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaControlKeysEventListener);
   MediaControlKeysEventListener() = default;
 
   virtual void OnKeyPressed(MediaControlKeysEvent aKeyEvent) = 0;
@@ -36,7 +35,12 @@ class MediaControlKeysEventListener {
  */
 class MediaControlKeysHandler final : public MediaControlKeysEventListener {
  public:
+  NS_DECL_ISUPPORTS
+
   void OnKeyPressed(MediaControlKeysEvent aKeyEvent) override;
+
+ private:
+  virtual ~MediaControlKeysHandler() = default;
 };
 
 /**
@@ -44,9 +48,10 @@ class MediaControlKeysHandler final : public MediaControlKeysEventListener {
  * transporting media control keys event to all its listeners when media keys
  * event happens.
  */
-class MediaControlKeysEventSource {
+class MediaControlKeysEventSource : public nsISupports {
  public:
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaControlKeysEventSource);
+  NS_DECL_ISUPPORTS
+
   MediaControlKeysEventSource() = default;
 
   virtual void AddListener(MediaControlKeysEventListener* aListener);
