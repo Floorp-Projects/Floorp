18e58a1d4988d287941ee9785c0e129160906b44
Tarek Ziadé <tarek@mozilla.com>
2019-11-14 06:38:56 +0000

    Bug 1596074 - Type mismatch between g5/pixel platform name and "android"  - r=rwood
    Fixes the platform for mobile for the changelog API
    
    Differential Revision: https://phabricator.services.mozilla.com/D52849
    
    --HG--
    extra : moz-landing-system : lando

--- testing/condprofile/condprof/runner.py
+++ testing/condprofile/condprof/runner.py
@@ -117,7 +117,10 @@ def main(args=sys.argv[1:]):
         raise IOError("Cannot find %s" % args.geckodriver)
 
     try:
-        plat = args.android and "android" or get_current_platform()
+        if args.android:
+            plat = "%s-%s" % (args.device_name, args.firefox.split("org.mozilla.")[-1])
+        else:
+            plat = get_current_platform()
         changelog = read_changelog(plat)
         LOG("Got the changelog from TaskCluster")
     except ProfileNotFoundError:
