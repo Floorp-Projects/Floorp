be1d1fb817960c3a1c0c9d5ba222052d44389190
Gurzau Raul <rgurzau@mozilla.com>
2019-11-14 03:57:50 +0200

    Bug 1533943 - Fixed eslint failure at browser/web-navigation.js on a CLOSED TREE.

--- devtools/client/responsive/browser/web-navigation.js
+++ devtools/client/responsive/browser/web-navigation.js
@@ -136,8 +136,7 @@ BrowserElementWebNavigation.prototype = {
   _sendMessage(message, data) {
     try {
       if (this._browser.frameLoader) {
-        const windowGlobal = this._browser.browsingContext
-          .currentWindowGlobal;
+        const windowGlobal = this._browser.browsingContext.currentWindowGlobal;
         if (windowGlobal) {
           windowGlobal
             .getActor("WebNavigation")
