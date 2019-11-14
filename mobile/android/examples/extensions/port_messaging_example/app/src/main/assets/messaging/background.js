e0f667fec0153fa2e53d633aae9ed3dd73d8b25b
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-11-14 15:35:02 +0200

    Backed out 2 changesets (bug 1585661) for causing lint failures.
    Backed out changeset c1e90bf96e14 (bug 1585661)
    Backed out changeset 1f2c6dc5a14e (bug 1585661)

--- mobile/android/examples/extensions/port_messaging_example/app/src/main/assets/messaging/background.js
+++
@@ -1,7 +0,0 @@
-// Establish connection with app
-let port = browser.runtime.connectNative("browser");
-port.onMessage.addListener(response => {
-    // Let's just echo the message back
-    port.postMessage(`Received: ${JSON.stringify(response)}`);
-});
-port.postMessage("Hello from WebExtension!");
