fe4b2a91ef1414b2a1c4f28b1075a50b231de253
Mihai Alexandru Michis <malexandru@mozilla.com>
2019-11-14 15:35:02 +0200

    Backed out 2 changesets (bug 1585661) for causing lint failures.
    Backed out changeset c1e90bf96e14 (bug 1585661)
    Backed out changeset 1f2c6dc5a14e (bug 1585661)

--- mobile/android/examples/extensions/messaging_example/app/src/main/assets/messaging/messaging.js
+++
@@ -1,9 +0,0 @@
-let manifest = document.querySelector("head > link[rel=manifest]");
-if (manifest) {
-  fetch(manifest.href)
-    .then(response => response.json())
-    .then(json => {
-      let message = { type: "WPAManifest", manifest: json };
-      browser.runtime.sendNativeMessage("browser", message);
-    });
-}
