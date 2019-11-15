63489b549158888ecd8c3a615aa51ef88e07f572
Mike Hommey <mh+mozilla@glandium.org>
2019-11-15 00:21:32 +0000

    Bug 1594998 - Make standalone SpiderMonkey use libxul-like panic. r=froydnj
    This moves the parts of toolkit/library/rust/shared/lib.rs related to
    panic hooking to a new mozglue subdirectory, which will be used for
    things that can be statically linked to e.g. libxul, rather than in
    a "shared library".
    
    The panic hook is disabled when building spidermonkey via the mozjs_sys
    crate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52793
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/library/rust/shared/lib.rs
+++ toolkit/library/rust/shared/lib.rs
@@ -47,8 +47,6 @@ extern crate storage;
 extern crate bookmark_sync;
 extern crate shift_or_euc_c;
 
-extern crate arrayvec;
-
 extern crate audio_thread_priority;
 
 #[cfg(feature = "webrtc")]
@@ -64,15 +62,12 @@ use std::boxed::Box;
 use std::env;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
+#[cfg(target_os = "android")]
 use std::os::raw::c_int;
 #[cfg(target_os = "android")]
 use log::Level;
 #[cfg(not(target_os = "android"))]
 use log::Log;
-use std::cmp;
-use std::panic;
-use std::ops::Deref;
-use arrayvec::{Array, ArrayString};
 
 extern "C" {
     fn gfx_critical_note(msg: *const c_char);
@@ -175,93 +170,6 @@ pub extern "C" fn GkRust_Shutdown() {
 #[no_mangle]
 pub extern "C" fn intentional_panic(message: *const c_char) {
     panic!("{}", unsafe { CStr::from_ptr(message) }.to_string_lossy());
-}
-
-extern "C" {
-    // We can't use MOZ_Crash directly because it may be weakly linked
-    // to libxul, and rust can't handle that.
-    fn GeckoCrash(filename: *const c_char, line: c_int, reason: *const c_char) -> !;
-}
-
-/// Truncate a string at the closest unicode character boundary
-/// ```
-/// assert_eq!(str_truncate_valid("éà", 3), "é");
-/// assert_eq!(str_truncate_valid("éà", 4), "éè");
-/// ```
-fn str_truncate_valid(s: &str, mut mid: usize) -> &str {
-    loop {
-        if let Some(res) = s.get(..mid) {
-            return res;
-        }
-        mid -= 1;
-    }
-}
-
-/// Similar to ArrayString, but with terminating nul character.
-#[derive(Debug, PartialEq)]
-struct ArrayCString<A: Array<Item = u8> + Copy> {
-    inner: ArrayString<A>,
-}
-
-impl<S: AsRef<str>, A: Array<Item = u8> + Copy> From<S> for ArrayCString<A> {
-    /// Contrary to ArrayString::from, truncates at the closest unicode
-    /// character boundary.
-    /// ```
-    /// assert_eq!(ArrayCString::<[_; 4]>::from("éà"),
-    ///            ArrayCString::<[_; 4]>::from("é"));
-    /// assert_eq!(&*ArrayCString::<[_; 4]>::from("éà"), "é\0");
-    /// ```
-    fn from(s: S) -> Self {
-        let s = s.as_ref();
-        let len = cmp::min(s.len(), A::CAPACITY - 1);
-        let mut result = Self {
-            inner: ArrayString::from(str_truncate_valid(s, len)).unwrap(),
-        };
-        result.inner.push('\0');
-        result
-    }
-}
-
-impl<A: Array<Item = u8> + Copy> Deref for ArrayCString<A> {
-    type Target = str;
-
-    fn deref(&self) -> &str {
-        self.inner.as_str()
-    }
-}
-
-fn panic_hook(info: &panic::PanicInfo) {
-    // Try to handle &str/String payloads, which should handle 99% of cases.
-    let payload = info.payload();
-    let message = if let Some(s) = payload.downcast_ref::<&str>() {
-        s
-    } else if let Some(s) = payload.downcast_ref::<String>() {
-        s.as_str()
-    } else {
-        // Not the most helpful thing, but seems unlikely to happen
-        // in practice.
-        "Unhandled rust panic payload!"
-    };
-    let (filename, line) = if let Some(loc) = info.location() {
-        (loc.file(), loc.line())
-    } else {
-        ("unknown.rs", 0)
-    };
-    // Copy the message and filename to the stack in order to safely add
-    // a terminating nul character (since rust strings don't come with one
-    // and GeckoCrash wants one).
-    let message = ArrayCString::<[_; 512]>::from(message);
-    let filename = ArrayCString::<[_; 512]>::from(filename);
-    unsafe {
-        GeckoCrash(filename.as_ptr() as *const c_char, line as c_int,
-                   message.as_ptr() as *const c_char);
-    }
-}
-
-/// Configure a panic hook to redirect rust panics to Gecko's MOZ_Crash.
-#[no_mangle]
-pub extern "C" fn install_rust_panic_hook() {
-    panic::set_hook(Box::new(panic_hook));
 }
 
 #[cfg(feature = "oom_with_hook")]
