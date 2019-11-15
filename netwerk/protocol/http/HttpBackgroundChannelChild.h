06768416627023272659effc1ba84489300af5df
Jean-Yves Avenard <jyavenard@mozilla.com>
2019-11-15 02:42:03 +0000

    Bug 1588241 - P3. Move classification methods to PHttpChannel. r=mayhemer
    We can guarantee that those callbacks will occur prior the call to OnStartRequest.
    But running them all on the PHttpChannel we remove the potential race between the PHttpChannel which runs on the main thread and PHttpBackroundChannel which runs on the necko thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52262
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/protocol/http/HttpBackgroundChannelChild.h
+++ netwerk/protocol/http/HttpBackgroundChannelChild.h
@@ -12,7 +12,6 @@
 #include "nsIRunnable.h"
 #include "nsTArray.h"
 
-using mozilla::dom::ClassifierInfo;
 using mozilla::ipc::IPCResult;
 
 namespace mozilla {
@@ -58,23 +57,6 @@ class HttpBackgroundChannelChild final : public PHttpBackgroundChannelChild {
   IPCResult RecvDivertMessages();
 
   IPCResult RecvOnStartRequestSent();
-
-  IPCResult RecvNotifyChannelClassifierProtectionDisabled(
-      const uint32_t& aAcceptedReason);
-
-  IPCResult RecvNotifyCookieAllowed();
-
-  IPCResult RecvNotifyCookieBlocked(const uint32_t& aRejectedReason);
-
-  IPCResult RecvNotifyClassificationFlags(const uint32_t& aClassificationFlags,
-                                          const bool& aIsThirdParty);
-
-  IPCResult RecvNotifyFlashPluginStateChanged(
-      const nsIHttpChannel::FlashPluginState& aState);
-
-  IPCResult RecvSetClassifierMatchedInfo(const ClassifierInfo& info);
-
-  IPCResult RecvSetClassifierMatchedTrackingInfo(const ClassifierInfo& info);
 
   void ActorDestroy(ActorDestroyReason aWhy) override;
 
