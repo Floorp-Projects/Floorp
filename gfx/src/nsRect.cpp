a5fc21bc91d0381b21ce8bbcd6f76b2b0ff682a5
Ting-Yu Lin <tlin@mozilla.com>
2019-11-15 03:52:21 +0000

    Bug 1596339 Part 7 - Delete unused operator<< for nsRect. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D53118

--- gfx/src/nsRect.cpp
+++ gfx/src/nsRect.cpp
@@ -32,26 +32,3 @@ bool nsRect::Overflows() const {
   return !xMost.isValid() || !yMost.isValid();
 #endif
 }
-
-#ifdef DEBUG
-// Diagnostics
-
-FILE* operator<<(FILE* out, const nsRect& rect) {
-  nsAutoString tmp;
-
-  // Output the coordinates in fractional pixels so they're easier to read
-  tmp.Append('{');
-  tmp.AppendFloat(NSAppUnitsToFloatPixels(rect.X(), AppUnitsPerCSSPixel()));
-  tmp.AppendLiteral(", ");
-  tmp.AppendFloat(NSAppUnitsToFloatPixels(rect.Y(), AppUnitsPerCSSPixel()));
-  tmp.AppendLiteral(", ");
-  tmp.AppendFloat(NSAppUnitsToFloatPixels(rect.Width(), AppUnitsPerCSSPixel()));
-  tmp.AppendLiteral(", ");
-  tmp.AppendFloat(
-      NSAppUnitsToFloatPixels(rect.Height(), AppUnitsPerCSSPixel()));
-  tmp.Append('}');
-  fputs(NS_LossyConvertUTF16toASCII(tmp).get(), out);
-  return out;
-}
-
-#endif  // DEBUG
