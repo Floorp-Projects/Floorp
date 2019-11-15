691d1901bc1b075bd8a8dd119949550903fae85e
Edwin Takahashi <egao@mozilla.com>
2019-11-15 22:45:12 +0000

    Bug 1428711 - add python3 compatibility to moznetwork r=ahal
    Changes:
    - rename `test.py` to a more descriptive `test_moznetwork.py` and change associated names in the manifest
    - added `r` specifier to strings as per PEP warning
    - bump version to 1.0.0 pending release to pypi
    
    Differential Revision: https://phabricator.services.mozilla.com/D52107
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozbase/moznetwork/setup.py
+++ testing/mozbase/moznetwork/setup.py
@@ -6,28 +6,37 @@ from __future__ import absolute_import
 
 from setuptools import setup
 
-PACKAGE_VERSION = '0.27'
+PACKAGE_VERSION = '1.0.0'
 
-deps = ['mozinfo',
-        'mozlog >= 5.0',
+deps = [
+    'mozinfo',
+    'mozlog >= 5.0',
+]
+
+setup(
+    name='moznetwork',
+    version=PACKAGE_VERSION,
+    description="Library of network utilities for use in Mozilla testing",
+    long_description="see https://firefox-source-docs.mozilla.org/mozbase/index.html",
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='mozilla',
+    author='Mozilla Automation and Tools team',
+    author_email='tools@lists.mozilla.org',
+    url='https://wiki.mozilla.org/Auto-tools/Projects/Mozbase',
+    license='MPL',
+    packages=['moznetwork'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=deps,
+    entry_points={
+        'console_scripts': [
+            'moznetwork = moznetwork:cli'
         ]
-
-setup(name='moznetwork',
-      version=PACKAGE_VERSION,
-      description="Library of network utilities for use in Mozilla testing",
-      long_description="see https://firefox-source-docs.mozilla.org/mozbase/index.html",
-      classifiers=['Programming Language :: Python :: 2.7',
-                   'Programming Language :: Python :: 2 :: Only'],
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='mozilla',
-      author='Mozilla Automation and Tools team',
-      author_email='tools@lists.mozilla.org',
-      url='https://wiki.mozilla.org/Auto-tools/Projects/Mozbase',
-      license='MPL',
-      packages=['moznetwork'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=deps,
-      entry_points={'console_scripts': [
-          'moznetwork = moznetwork:cli']},
-      )
+    },
+)
