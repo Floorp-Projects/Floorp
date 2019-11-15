239ef6b22a46be85e5797a224f7a1ae02a7ffee0
Emilio Cobos Álvarez <emilio@crisal.io>
2019-11-15 18:19:37 +0000

    Bug 1596209 - Remove nsIContent::GetBindingParent. r=smaug
    It's unused after all the previous patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53217

--- dom/base/ShadowRoot.cpp
+++ dom/base/ShadowRoot.cpp
@@ -62,7 +62,6 @@ ShadowRoot::ShadowRoot(Element* aElement, ShadowRootMode aMode,
   SetFlags(NODE_IS_IN_SHADOW_TREE);
   Bind();
 
-  ExtendedDOMSlots()->mBindingParent = aElement;
   ExtendedDOMSlots()->mContainingShadow = this;
 }
 
