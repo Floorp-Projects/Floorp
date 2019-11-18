06d4249d443d89fe1081b6adede63cd293b685c8
Emilio Cobos Álvarez <emilio@crisal.io>
2019-11-18 19:06:51 +0000

    Bug 1596800 - Remove document.getBindingParent. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D53342
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -7801,15 +7801,6 @@ already_AddRefed<nsINode> Document::ImportNode(nsINode& aNode, bool aDeep,
   return nullptr;
 }
 
-// FIXME(bug 1596800): This should be removed, only has a couple of callers.
-Element* Document::GetBindingParent(nsINode& aNode) {
-  if (aNode.IsInNativeAnonymousSubtree()) {
-    return Element::FromNodeOrNull(
-        aNode.GetClosestNativeAnonymousSubtreeRootParent());
-  }
-  return Element::FromNodeOrNull(aNode.GetContainingShadowHost());
-}
-
 nsINodeList* Document::GetAnonymousNodes(Element& aElement) { return nullptr; }
 
 already_AddRefed<nsRange> Document::CreateRange(ErrorResult& rv) {
