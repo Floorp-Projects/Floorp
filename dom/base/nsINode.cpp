7816f654e9ff8aa8235f58a917cc9de633c1f22a
Emilio Cobos Álvarez <emilio@crisal.io>
2019-11-18 08:49:27 +0000

    Bug 1597123 - Remove NODE_IS_ANONYMOUS_ROOT. r=smaug
    We no longer have multiple kinds of anonymous subtrees, so we can get back one
    node bit.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53344

--- dom/base/nsINode.cpp
+++ dom/base/nsINode.cpp
@@ -1162,12 +1162,7 @@ nsIGlobalObject* nsINode::GetOwnerGlobal() const {
 }
 
 bool nsINode::UnoptimizableCCNode() const {
-  const uintptr_t problematicFlags =
-      (NODE_IS_ANONYMOUS_ROOT | NODE_IS_IN_NATIVE_ANONYMOUS_SUBTREE |
-       NODE_IS_NATIVE_ANONYMOUS_ROOT);
-  return HasFlag(problematicFlags) || NodeType() == ATTRIBUTE_NODE ||
-         // For strange cases like xbl:content/xbl:children
-         (IsElement() && AsElement()->IsInNamespace(kNameSpaceID_XBL));
+  return IsInNativeAnonymousSubtree() || IsAttr();
 }
 
 /* static */
