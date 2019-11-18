ee80df40a2231af4330d45ab550885473b813f1a
Mark Striemer <mstriemer@mozilla.com>
2019-11-18 20:03:14 +0000

    Bug 1594014 - Remove unused options UI helpers from extensions.xul r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D53104

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -9,9 +9,6 @@
 /* globals ProcessingInstruction */
 /* exported loadView */
 
-const { DeferredTask } = ChromeUtils.import(
-  "resource://gre/modules/DeferredTask.jsm"
-);
 const { AddonManager } = ChromeUtils.import(
   "resource://gre/modules/AddonManager.jsm"
 );
@@ -101,14 +98,6 @@ function initialize(event) {
     return;
   }
   document.removeEventListener("load", initialize, true);
-
-  let contentAreaContextMenu = document.getElementById(
-    "contentAreaContextMenu"
-  );
-  contentAreaContextMenu.addEventListener("popupshowing", function(event) {
-    Cu.reportError("This dummy menupopup is not supposed to be shown");
-    return false;
-  });
 
   let addonPage = document.getElementById("addons-page");
   addonPage.addEventListener("dragenter", function(event) {
@@ -1540,28 +1529,6 @@ var gDragDrop = {
     }
   },
 };
-
-// Force the options_ui remote browser to recompute window.mozInnerScreenX and
-// window.mozInnerScreenY when the "addon details" page has been scrolled
-// (See Bug 1390445 for rationale).
-{
-  const UPDATE_POSITION_DELAY = 100;
-
-  const updatePositionTask = new DeferredTask(() => {
-    const browser = document.getElementById("addon-options");
-    if (browser && browser.isRemoteBrowser) {
-      browser.frameLoader.requestUpdatePosition();
-    }
-  }, UPDATE_POSITION_DELAY);
-
-  window.addEventListener(
-    "scroll",
-    () => {
-      updatePositionTask.arm();
-    },
-    true
-  );
-}
 
 const addonTypes = new Set([
   "extension",
