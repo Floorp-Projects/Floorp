ec24a723ef6b8a221e52816c9c66e6e3a646158a
Jan de Mooij <jdemooij@mozilla.com>
2019-11-19 17:00:20 +0000

    Bug 1595476 part 4 - Remove now-almost-empty IonControlFlow.{h,cpp} files. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D52636

--- js/src/jit/BaselineJIT.cpp
+++ js/src/jit/BaselineJIT.cpp
@@ -17,7 +17,6 @@
 #include "jit/BaselineCodeGen.h"
 #include "jit/BaselineIC.h"
 #include "jit/CompileInfo.h"
-#include "jit/IonControlFlow.h"
 #include "jit/JitCommon.h"
 #include "jit/JitSpewer.h"
 #include "util/Memory.h"
