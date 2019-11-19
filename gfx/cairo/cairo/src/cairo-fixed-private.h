cff70f9d670804e2fb09b8602ee5b4ab9ba6796f
Lee Salzman <lsalzman@mozilla.com>
2019-11-19 11:49:35 +0000

    Bug 1584639 - silence Cairo shift warnings. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D53720

--- gfx/cairo/cairo/src/cairo-fixed-private.h
+++ gfx/cairo/cairo/src/cairo-fixed-private.h
@@ -59,7 +59,7 @@
 static inline cairo_fixed_t
 _cairo_fixed_from_int (int i)
 {
-    return i << CAIRO_FIXED_FRAC_BITS;
+    return (cairo_fixed_t)((cairo_fixed_unsigned_t)i << CAIRO_FIXED_FRAC_BITS);
 }
 
 /* This is the "magic number" approach to converting a double into fixed
