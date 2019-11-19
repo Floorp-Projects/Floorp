af99b520afe802ca1c8e42b1d13d016bf40eb7e6
Chris H-C <chutten@mozilla.com>
2019-11-19 14:28:40 +0000

    Bug 1585410 - Implement and document 'deletion-request' ping r=janerik,Dexter
    The 'deletion-request' ping, which supercedes the 'optout' ping, notifies the
    pipeline when a profile opts out of FHR upload. (IOW, when a user on a specific
    profile unchecks the box in about:preferences#privacy about sharing
    technical and interaction data with Mozilla).
    
    This ping tries its best to reach the pipeline to let them know that we need
    to delete data associated with the provided clientId. This means it will remain
    pending on the client even after opt out and it will try to resend if upload is
    ever re-enabled.
    
    Depends on D51709
    
    Differential Revision: https://phabricator.services.mozilla.com/D51710

--- toolkit/components/telemetry/tests/unit/test_TelemetryClientID_reset.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryClientID_reset.js
@@ -13,7 +13,7 @@ const { Preferences } = ChromeUtils.import(
 );
 
 const PING_FORMAT_VERSION = 4;
-const OPTOUT_PING_TYPE = "optout";
+const DELETION_REQUEST_PING_TYPE = "deletion-request";
 const TEST_PING_TYPE = "test-ping-type";
 
 function sendPing(addEnvironment = false) {
@@ -55,7 +55,7 @@ add_task(async function test_setup() {
  * 6. Detect that upload is enabled and reset client ID
  *
  * This scenario e.g. happens when switching between channels
- * with and without the optout ping reset included.
+ * with and without the deletion-request ping reset included.
  */
 add_task(async function test_clientid_reset_after_reenabling() {
   await sendPing();
@@ -70,12 +70,16 @@ add_task(async function test_clientid_reset_after_reenabling() {
     "Client ID should be valid and random"
   );
 
-  // Disable FHR upload: this should trigger a optout ping.
+  // Disable FHR upload: this should trigger a deletion-request ping.
   Preferences.set(TelemetryUtils.Preferences.FhrUploadEnabled, false);
 
   ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, OPTOUT_PING_TYPE, "The ping must be an optout ping");
-  Assert.ok(!("clientId" in ping));
+  Assert.equal(
+    ping.type,
+    DELETION_REQUEST_PING_TYPE,
+    "The ping must be a deletion-request ping"
+  );
+  Assert.equal(ping.clientId, firstClientId);
   let clientId = await ClientID.getClientID();
   Assert.equal(TelemetryUtils.knownClientID, clientId);
 
@@ -115,7 +119,7 @@ add_task(async function test_clientid_reset_after_reenabling() {
  * 6. Detect that upload is disabled and sets canary client ID
  *
  * This scenario e.g. happens when switching between channels
- * with and without the optout ping reset included.
+ * with and without the deletion-request ping reset included.
  */
 add_task(async function test_clientid_canary_after_disabling() {
   await sendPing();
@@ -130,12 +134,16 @@ add_task(async function test_clientid_canary_after_disabling() {
     "Client ID should be valid and random"
   );
 
-  // Disable FHR upload: this should trigger a optout ping.
+  // Disable FHR upload: this should trigger a deletion-request ping.
   Preferences.set(TelemetryUtils.Preferences.FhrUploadEnabled, false);
 
   ping = await PingServer.promiseNextPing();
-  Assert.equal(ping.type, OPTOUT_PING_TYPE, "The ping must be an optout ping");
-  Assert.ok(!("clientId" in ping));
+  Assert.equal(
+    ping.type,
+    DELETION_REQUEST_PING_TYPE,
+    "The ping must be a deletion-request ping"
+  );
+  Assert.equal(ping.clientId, firstClientId);
   let clientId = await ClientID.getClientID();
   Assert.equal(TelemetryUtils.knownClientID, clientId);
 
