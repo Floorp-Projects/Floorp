eeeba68d88854ebebf0ab2f1fa02021022c47c39
Andreas Pehrson <apehrson@mozilla.com>
2019-11-20 16:32:52 +0000

    Bug 1596777 - Hook up the SecondaryVideoContainer with state mirroring. r=alwu
    This better follows how MediaDecoder and MediaDecoderStateMachine was
    architected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53710

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -269,9 +269,6 @@ class MediaDecoderStateMachine
 
   RefPtr<GenericPromise> InvokeSetSink(RefPtr<AudioDeviceInfo> aSink);
 
-  void SetSecondaryVideoContainer(
-      const RefPtr<VideoFrameContainer>& aSecondary);
-
  private:
   class StateObject;
   class DecodeMetadataState;
@@ -375,6 +372,7 @@ class MediaDecoderStateMachine
   void SetPlaybackRate(double aPlaybackRate);
   void PreservesPitchChanged();
   void LoopingChanged();
+  void UpdateSecondaryVideoContainer();
   void UpdateOutputCaptured();
   void OutputTracksChanged();
   void OutputPrincipalChanged();
@@ -707,6 +705,10 @@ class MediaDecoderStateMachine
   // The device used with SetSink, or nullptr if no explicit device has been
   // set.
   Mirror<RefPtr<AudioDeviceInfo>> mSinkDevice;
+
+  // Set if the decoder is sending video to a secondary container. While set we
+  // should not suspend the decoder.
+  Mirror<RefPtr<VideoFrameContainer>> mSecondaryVideoContainer;
 
   // Whether all output should be captured into mOutputTracks. While true, the
   // media sink will only play if there are output tracks.
