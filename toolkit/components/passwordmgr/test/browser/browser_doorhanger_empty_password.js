88728db5a3b1d0ce6d3242fe0a0263817f9cc86c
Sam Foster <sfoster@mozilla.com>
2019-11-20 06:18:26 +0000

    Bug 1388674 - Update tests to use setUserInput, ensuring we get an input event for field modifications. r=MattN
    Depends on D53055
    
    Differential Revision: https://phabricator.services.mozilla.com/D51718

--- toolkit/components/passwordmgr/test/browser/browser_doorhanger_empty_password.js
+++ toolkit/components/passwordmgr/test/browser/browser_doorhanger_empty_password.js
@@ -22,8 +22,8 @@ add_task(async function test_empty_password() {
       // case. This will cause the doorhanger notification to be displayed.
       await ContentTask.spawn(browser, null, async function() {
         let doc = content.document;
-        doc.getElementById("form-basic-username").value = "username";
-        doc.getElementById("form-basic-password").value = "pw";
+        doc.getElementById("form-basic-username").setUserInput("username");
+        doc.getElementById("form-basic-password").setUserInput("pw");
         doc.getElementById("form-basic").submit();
       });
 
