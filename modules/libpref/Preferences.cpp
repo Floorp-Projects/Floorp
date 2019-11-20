2046efb7a3727011e1763a087ec23bd9ed0ee9f7
Geoff Brown <gbrown@mozilla.com>
2019-11-20 17:13:34 +0000

    Bug 1596962 - Initialize static pointers in Preferences.cpp; r=njn
    Explicit initialization of these static pointers appears to resolve these intermittent failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53842

--- modules/libpref/Preferences.cpp
+++ modules/libpref/Preferences.cpp
@@ -1199,7 +1199,7 @@ class CallbackNode {
 
 using PrefsHashTable = HashSet<UniquePtr<Pref>, PrefHasher>;
 
-static PrefsHashTable* gHashTable;
+static PrefsHashTable* gHashTable = nullptr;
 
 #ifdef DEBUG
 // This defines the type used to store our `once` mirrors checker. We can't use
@@ -1226,7 +1226,7 @@ static CallbackNode* gLastPriorityNode = nullptr;
 
 #ifdef ACCESS_COUNTS
 using AccessCountsHashTable = nsDataHashtable<nsCStringHashKey, uint32_t>;
-static AccessCountsHashTable* gAccessCounts;
+static AccessCountsHashTable* gAccessCounts = nullptr;
 
 static void AddAccessCount(const nsACString& aPrefName) {
   // FIXME: Servo reads preferences from background threads in unsafe ways (bug
