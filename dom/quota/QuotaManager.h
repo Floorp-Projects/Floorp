d6a0611dfa11de23cf70243de934fe487d1f567f
Tom Tung <ttung@mozilla.com>
2019-11-20 10:34:34 +0000

    Bug 1592934 - Add a telemetry probe to track storage initialization success rate; r=janv a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D51845

--- dom/quota/QuotaManager.h
+++ dom/quota/QuotaManager.h
@@ -595,6 +595,7 @@ class QuotaManager final : public BackgroundThreadObject {
   uint64_t mTemporaryStorageLimit;
   uint64_t mTemporaryStorageUsage;
   int64_t mNextDirectoryLockId;
+  bool mStorageInitializationAttempted;
   bool mTemporaryStorageInitializationAttempted;
   bool mTemporaryStorageInitialized;
   bool mCacheUsable;
