f87343b267f27084a0d6f8aaf97aa6d44a8c1129
Brindusan Cristian <cbrindusan@mozilla.com>
2019-11-21 05:37:04 +0200

    Backed out 2 changesets (bug 1549394) for MinGW bustages at TSFTextStore.cpp. CLOSED TREE
    Backed out changeset 5da3dcd12b69 (bug 1549394)
    Backed out changeset fb2f4ca36af4 (bug 1549394)

--- widget/windows/TSFTextStore.h
+++ widget/windows/TSFTextStore.h
@@ -348,8 +348,7 @@ class TSFTextStore final : public ITextStoreACP,
   HRESULT HandleRequestAttrs(DWORD aFlags, ULONG aFilterCount,
                              const TS_ATTRID* aFilterAttrs);
   void SetInputScope(const nsString& aHTMLInputType,
-                     const nsString& aHTMLInputInputmode,
-                     bool aInPrivateBrowsing);
+                     const nsString& aHTMLInputInputmode);
 
   // Creates native caret over our caret.  This method only works on desktop
   // application.  Otherwise, this does nothing.
