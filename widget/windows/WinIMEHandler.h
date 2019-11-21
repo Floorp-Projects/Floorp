13e852fd98761a6924bd6924c268a301776dbd61
Brindusan Cristian <cbrindusan@mozilla.com>
2019-11-21 05:37:04 +0200

    Backed out 2 changesets (bug 1549394) for MinGW bustages at TSFTextStore.cpp. CLOSED TREE
    Backed out changeset 5da3dcd12b69 (bug 1549394)
    Backed out changeset fb2f4ca36af4 (bug 1549394)

--- widget/windows/WinIMEHandler.h
+++ widget/windows/WinIMEHandler.h
@@ -199,8 +199,7 @@ class IMEHandler final {
   static decltype(SetInputScopes)* sSetInputScopes;
   static void SetInputScopeForIMM32(nsWindow* aWindow,
                                     const nsAString& aHTMLInputType,
-                                    const nsAString& aHTMLInputInputmode,
-                                    bool aInPrivateBrowsing);
+                                    const nsAString& aHTMLInputInputmode);
   static bool sIsInTSFMode;
   // If sIMMEnabled is false, any IME messages are not handled in TSF mode.
   // Additionally, IME context is always disassociated from focused window.
