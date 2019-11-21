e433d5cfce5dcc739c37442511bcfe176ff0a9fb
Brendan Dahl <bdahl@mozilla.com>
2019-11-21 21:25:55 +0000

    Bug 1596709 - Move special chrome root element handling to an observer. r=smaug
    Enables any type of document to use the special root element attributes
    currently used by XUL <window>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D54016
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -166,6 +166,7 @@ class AnonymousContent;
 class Attr;
 class XULBroadcastManager;
 class XULPersist;
+class ChromeObserver;
 class ClientInfo;
 class ClientState;
 class CDATASection;
@@ -5337,6 +5338,7 @@ class Document : public nsINode,
 
   RefPtr<XULBroadcastManager> mXULBroadcastManager;
   RefPtr<XULPersist> mXULPersist;
+  RefPtr<ChromeObserver> mChromeObserver;
 
   RefPtr<mozilla::dom::HTMLAllCollection> mAll;
 
