ba7e841092aead4cd25a7682a34519a89390a3c4
Belén Albeza <balbeza@mozilla.com>
2019-11-21 13:25:48 +0000

    Bug 1596148 - Part 2: Re-enable start SW mochitest r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D53991
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/application/test/browser/browser_application_panel_start-service-worker.js
+++ devtools/client/application/test/browser/browser_application_panel_start-service-worker.js
@@ -48,34 +48,3 @@ add_task(async function() {
 
   await unregisterAllWorkers(target.client);
 });
-
-/**
- * Tests that Start button is disabled for service workers, when they cannot be debugged
- */
-add_task(async function() {
-  await enableApplicationPanel();
-
-  // disable sw debugging by increasing the # of processes and thus multi-e10s kicking in
-  info("Disable service worker debugging");
-  await pushPref("dom.ipc.processCount", 8);
-
-  const { panel, tab, target } = await openNewTabAndApplicationPanel(TAB_URL);
-  const doc = panel.panelWin.document;
-
-  selectPage(panel, "service-workers");
-
-  await waitForWorkerRegistration(tab);
-
-  info("Wait until the service worker appears in the application panel");
-  await waitUntil(() => getWorkerContainers(doc).length === 1);
-
-  info("Wait until the start button is displayed");
-  const container = getWorkerContainers(doc)[0];
-  await waitUntil(() => container.querySelector(".js-start-button"));
-  ok(
-    container.querySelector(".js-start-button").disabled,
-    "Start button is disabled"
-  );
-
-  await unregisterAllWorkers(target.client);
-});
