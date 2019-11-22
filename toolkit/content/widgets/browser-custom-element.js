c466b251727653b0ad5a380d5b8bbf995c4b5dbd
Tim Nguyen <ntim.bugs@gmail.com>
2019-11-22 00:13:15 +0000

    Bug 1597844 - Fix positioning of form validation and date/time input popups. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D53902
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/content/widgets/browser-custom-element.js
+++ toolkit/content/widgets/browser-custom-element.js
@@ -504,7 +504,7 @@
       }
 
       // Create an anchor for the popup
-      popupAnchor = document.createXULElement("hbox");
+      popupAnchor = document.createElement("div");
       popupAnchor.className = "popup-anchor";
       popupAnchor.hidden = true;
       stack.appendChild(popupAnchor);
