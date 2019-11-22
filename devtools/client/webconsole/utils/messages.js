921b28d89bbf8401c08ade69c016cdd9c40f8911
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-11-22 09:48:58 +0000

    Bug 1326182 - Send all the console.table items in the console message. r=Honza.
    This allows us to not have to reach for the server
    again from the client to get all the data we need
    to render the table.
    
    We still need to keep the old function to handle
    connections to older servers. We'll use Bug 1597955
    in Firefox 74 to clean the bits we don't need anymore.
    
    A test is added to ensure the bug is fixed and we don't
    regress; stubs are updated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53962

--- devtools/client/webconsole/utils/messages.js
+++ devtools/client/webconsole/utils/messages.js
@@ -689,9 +689,25 @@ function getArrayTypeNames() {
   ];
 }
 
+function getDescriptorValue(descriptor) {
+  if (!descriptor) {
+    return descriptor;
+  }
+
+  if (Object.prototype.hasOwnProperty.call(descriptor, "safeGetterValues")) {
+    return descriptor.safeGetterValues;
+  }
+
+  if (Object.prototype.hasOwnProperty.call(descriptor, "value")) {
+    return descriptor.value;
+  }
+  return descriptor;
+}
+
 module.exports = {
   createWarningGroupMessage,
   getArrayTypeNames,
+  getDescriptorValue,
   getInitialMessageCountForViewport,
   getParentWarningGroupMessageId,
   getWarningGroupType,
