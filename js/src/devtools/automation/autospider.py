b2b8d5278556f9bb9a6b1310e7a36fca674414b2
Steve Fink <sfink@mozilla.com>
2019-11-22 21:51:59 +0000

    Bug 1597005 - Switch autospider.py to be run with python3. r=jimb
    Differential Revision: https://phabricator.services.mozilla.com/D54226

--- js/src/devtools/automation/autospider.py
+++ js/src/devtools/automation/autospider.py
@@ -345,7 +345,7 @@ REPLACEMENTS = {
 # Add in environment variable settings for this variant. Normally used to
 # modify the flags passed to the shell or to set the GC zeal mode.
 for k, v in variant.get('env', {}).items():
-    env[k.encode('ascii')] = v.encode('ascii').format(**REPLACEMENTS)
+    env[k] = v.format(**REPLACEMENTS)
 
 if AUTOMATION:
     # Currently only supported on linux64.
