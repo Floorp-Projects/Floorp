19e8a51a77171db2cfdb90c74101b07a266eef50
Daniel Varga <dvarga@mozilla.com>
2019-11-22 03:45:45 +0200

    Backed out changeset 59e0b3d74d19 (bug 1598448) for webrender-lint-tidy fail. On a CLOSED TREE

--- gfx/wr/example-compositor/compositor/src/main.rs
+++ gfx/wr/example-compositor/compositor/src/main.rs
@@ -65,7 +65,7 @@ impl webrender::Compositor for DirectCompositeInterface {
         id: webrender::NativeSurfaceId,
         dirty_rect: DeviceIntRect,
     ) -> webrender::NativeSurfaceInfo {
-        let (fbo_id, x, y) = compositor::bind_surface(
+        let (x, y) = compositor::bind_surface(
             self.window,
             id.0,
             dirty_rect.origin.x,
@@ -76,7 +76,7 @@ impl webrender::Compositor for DirectCompositeInterface {
 
         webrender::NativeSurfaceInfo {
             origin: DeviceIntPoint::new(x, y),
-            fbo_id,
+            fbo_id: 0,
         }
     }
 
