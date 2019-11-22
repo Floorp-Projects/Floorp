fe0fb5049230263c5358fd76a0af68ebba39b3b8
alwu <alwu@mozilla.com>
2019-11-22 09:17:35 +0000

    Bug 1593843 - part2 : fix build problem under unified build. r=bryce
    As we added new files, which affects the build bundle created by unified build. Therefore, add missing scope definition and included header to pass the build.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52430

--- dom/media/MediaDecoderStateMachine.h
+++ dom/media/MediaDecoderStateMachine.h
@@ -82,6 +82,7 @@ hardware (via AudioStream).
 #if !defined(MediaDecoderStateMachine_h__)
 #  define MediaDecoderStateMachine_h__
 
+#  include "AudioDeviceInfo.h"
 #  include "ImageContainer.h"
 #  include "MediaDecoder.h"
 #  include "MediaDecoderOwner.h"
