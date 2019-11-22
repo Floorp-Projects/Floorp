d59fc60ab8d44f95440624c2c72c1220336dd065
Tooru Fujisawa <arai_a@mac.com>
2019-11-22 14:33:52 +0000

    Bug 1594322 - Part 3: Use HuffmanDictionary::TableIdentity in HuffmanPreludeReader::readTable. r=Yoric
    Another preparation for Part 4.
    
    Depends on D52031
    
    Differential Revision: https://phabricator.services.mozilla.com/D52032

--- js/src/frontend/BinASTTokenReaderContext.cpp
+++ js/src/frontend/BinASTTokenReaderContext.cpp
@@ -476,10 +476,8 @@ class HuffmanPreludeReader {
       // 2. Add the field to the set of visited fields
       status = HuffmanDictionary::TableStatus::Initializing;
 
-      auto& table = dictionary_.table(tableId);
-
       // Read the lengths immediately.
-      MOZ_TRY((readTable<List>(table, status, list)));
+      MOZ_TRY((readTable<List>(tableId, list)));
     }
 
     // Spec:
@@ -833,34 +831,40 @@ class HuffmanPreludeReader {
       return raiseDuplicateTableError(entry.identity_);
     }
 
-    auto& table = dictionary_.table(tableId);
-    return readTable<Entry>(table, status, entry);
+    return readTable<Entry>(tableId, entry);
   }
 
-  // Three-arguments version: pass table explicitly. Generally called from
-  // single-argument version, but may be called manually, e.g. for list
+  // Two-arguments version: pass table identity explicitly. Generally called
+  // from single-argument version, but may be called manually, e.g. for list
   // lengths.
   template <typename Entry>
-  MOZ_MUST_USE JS::Result<Ok> readTable(GenericHuffmanTable& table,
-                                        HuffmanDictionary::TableStatus& status,
-                                        Entry entry) {
+  MOZ_MUST_USE JS::Result<Ok> readTable(
+      HuffmanDictionary::TableIdentity tableId, Entry entry) {
     uint8_t headerByte;
     MOZ_TRY_VAR(headerByte, reader_.readByte<Compression::No>());
     switch (headerByte) {
-      case TableHeader::SingleValue:
+      case TableHeader::SingleValue: {
+        auto& table = dictionary_.table(tableId);
+        auto& status = dictionary_.status(tableId);
+
         new (mozilla::KnownNotNull, &table) GenericHuffmanTable();
         status = HuffmanDictionary::TableStatus::Ready;
 
         // The table contains a single value.
         MOZ_TRY((readSingleValueTable<Entry>(table, entry)));
         return Ok();
-      case TableHeader::MultipleValues:
+      }
+      case TableHeader::MultipleValues: {
+        auto& table = dictionary_.table(tableId);
+        auto& status = dictionary_.status(tableId);
+
         new (mozilla::KnownNotNull, &table) GenericHuffmanTable();
         status = HuffmanDictionary::TableStatus::Ready;
 
         // Table contains multiple values.
         MOZ_TRY((readMultipleValuesTable<Entry>(table, entry)));
         return Ok();
+      }
       case TableHeader::Unreachable:
         // Table is unreachable, nothing to do.
         return Ok();
