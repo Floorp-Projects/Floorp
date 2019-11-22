395f68222ac081d54e0e126e0ff0134acbcc94b1
James Teh <jteh@mozilla.com>
2019-11-22 09:37:39 +0000

    Bug 1594608: Use the focused remote browser for context menu key events. r=hsivonen
    Even though context menu key events are caused by a key press, they get handled as mouse events.
    Because the parent process FocusManager only knows about top level remote browsers, the event can only be targeted to a top level remote browser, even if the focus is actually in an OOP iframe.
    To fix this, when dispatching cross-process events, handle context menu key events the same way we handle normal keyboard events; i.e. use the focused remote browser.
    
    Differential Revision: https://phabricator.services.mozilla.com/D54245

--- dom/events/EventStateManager.cpp
+++ dom/events/EventStateManager.cpp
@@ -1275,16 +1275,9 @@ void EventStateManager::DispatchCrossProcessEvent(WidgetEvent* aEvent,
     return;
   }
 
-  if (aEvent->mLayersId.IsValid()) {
-    BrowserParent* preciseRemote =
-        BrowserParent::GetBrowserParentFromLayersId(aEvent->mLayersId);
-    if (preciseRemote) {
-      remote = preciseRemote;
-    }
-    // else there is a race between APZ and the LayersId to BrowserParent
-    // mapping, so fall back to delivering the event to the topmost child
-    // process.
-  } else if (aEvent->mClass == eKeyboardEventClass) {
+  WidgetMouseEvent* mouseEvent = aEvent->AsMouseEvent();
+  bool isContextMenuKey = mouseEvent && mouseEvent->IsContextMenuKeyEvent();
+  if (aEvent->mClass == eKeyboardEventClass || isContextMenuKey) {
     // APZ attaches a LayersId to hit-testable events, for keyboard events,
     // we use focus.
     BrowserParent* preciseRemote = BrowserParent::GetFocused();
@@ -1293,11 +1286,20 @@ void EventStateManager::DispatchCrossProcessEvent(WidgetEvent* aEvent,
     }
     // else there is a race between layout and focus tracking,
     // so fall back to delivering the event to the topmost child process.
+  } else if (aEvent->mLayersId.IsValid()) {
+    BrowserParent* preciseRemote =
+        BrowserParent::GetBrowserParentFromLayersId(aEvent->mLayersId);
+    if (preciseRemote) {
+      remote = preciseRemote;
+    }
+    // else there is a race between APZ and the LayersId to BrowserParent
+    // mapping, so fall back to delivering the event to the topmost child
+    // process.
   }
 
   switch (aEvent->mClass) {
     case eMouseEventClass: {
-      remote->SendRealMouseEvent(*aEvent->AsMouseEvent());
+      remote->SendRealMouseEvent(*mouseEvent);
       return;
     }
     case eKeyboardEventClass: {
