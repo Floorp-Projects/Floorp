eb86c9cd9af5c8aeec57c79138e96adbe0272078
alwu <alwu@mozilla.com>
2019-11-25 11:00:13 +0000

    Bug 1593843 - part4 : implement 'GVAutoplayPermissionRequestor' to manage the request. r=bryce
    `GVAutoplayPermissionRequestor` provides a method to request autoplay permission for a page, which would be used to decide if media is allowed to autoplay or not on GeckoView.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52434
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/GVAutoplayPermissionRequest.cpp
+++ dom/media/GVAutoplayPermissionRequest.cpp
@@ -4,11 +4,21 @@
 
 #include "GVAutoplayPermissionRequest.h"
 
+#include "mozilla/dom/HTMLMediaElement.h"
+
 namespace mozilla {
 namespace dom {
 
 using RType = GVAutoplayRequestType;
 using RStatus = GVAutoplayRequestStatus;
+
+static RStatus GetRequestStatus(BrowsingContext* aContext, RType aType) {
+  MOZ_ASSERT(aContext);
+  AssertIsOnMainThread();
+  return aType == RType::eAUDIBLE
+             ? aContext->GetGVAudibleAutoplayRequestStatus()
+             : aContext->GetGVInaudibleAutoplayRequestStatus();
+}
 
 NS_IMPL_CYCLE_COLLECTION_INHERITED(GVAutoplayPermissionRequest,
                                    ContentPermissionRequestBase)
@@ -17,44 +27,120 @@ NS_IMPL_ISUPPORTS_CYCLE_COLLECTION_INHERITED_0(GVAutoplayPermissionRequest,
                                                ContentPermissionRequestBase)
 
 /* static */
-already_AddRefed<GVAutoplayPermissionRequest>
-GVAutoplayPermissionRequest::CreateRequest(nsGlobalWindowInner* aWindow,
-                                           RType aType) {
-  if (!aWindow || !aWindow->GetPrincipal()) {
-    return nullptr;
-  }
+void GVAutoplayPermissionRequest::CreateRequest(nsGlobalWindowInner* aWindow,
+                                                BrowsingContext* aContext,
+                                                GVAutoplayRequestType aType) {
   RefPtr<GVAutoplayPermissionRequest> request =
-      new GVAutoplayPermissionRequest(aWindow, aType);
-  return request.forget();
+      new GVAutoplayPermissionRequest(aWindow, aContext, aType);
+  request->SetRequestStatus(RStatus::ePENDING);
+  request->RequestDelayedTask(
+      aWindow->EventTargetFor(TaskCategory::Other),
+      GVAutoplayPermissionRequest::DelayedTaskType::Request);
 }
 
 GVAutoplayPermissionRequest::GVAutoplayPermissionRequest(
-    nsGlobalWindowInner* aWindow, RType aType)
+    nsGlobalWindowInner* aWindow, BrowsingContext* aContext, RType aType)
     : ContentPermissionRequestBase(
           aWindow->GetPrincipal(), aWindow,
           NS_LITERAL_CSTRING(""),  // No testing pref used in this class
           aType == RType::eAUDIBLE
               ? NS_LITERAL_CSTRING("autoplay-media-audible")
               : NS_LITERAL_CSTRING("autoplay-media-inaudible")),
-      mType(aType) {}
+      mType(aType),
+      mContext(aContext) {
+  MOZ_ASSERT(mContext);
+}
 
 GVAutoplayPermissionRequest::~GVAutoplayPermissionRequest() {
   // If user doesn't response to the request before it gets destroyed (ex.
   // request dismissed, tab closed, naviagation to a new page), then we should
   // treat it as a denial.
-  Cancel();
+  if (mContext) {
+    Cancel();
+  }
+}
+
+void GVAutoplayPermissionRequest::SetRequestStatus(RStatus aStatus) {
+  MOZ_ASSERT(mContext);
+  AssertIsOnMainThread();
+  if (mType == RType::eAUDIBLE) {
+    mContext->SetGVAudibleAutoplayRequestStatus(aStatus);
+  } else {
+    mContext->SetGVInaudibleAutoplayRequestStatus(aStatus);
+  }
 }
 
 NS_IMETHODIMP
 GVAutoplayPermissionRequest::Cancel() {
-  // TODO : It will be implemented in the following patch.
+  MOZ_ASSERT(mContext, "Do not call 'Cancel()' twice!");
+  // As the process of replying of the request is an async task, the status
+  // might have be reset at the time we get the result from parent process.
+  // Ex. if the page got closed or naviagated immediately after user replied to
+  // the request. Therefore, the status should be either `pending` or `unknown`.
+  const RStatus status = GetRequestStatus(mContext, mType);
+  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eUNKNOWN);
+  if (status == RStatus::ePENDING) {
+    SetRequestStatus(RStatus::eDENIED);
+  }
+  mContext = nullptr;
   return NS_OK;
 }
 
 NS_IMETHODIMP
 GVAutoplayPermissionRequest::Allow(JS::HandleValue aChoices) {
-  // TODO : It will be implemented in the following patch.
+  MOZ_ASSERT(mContext, "Do not call 'Allow()' twice!");
+  // As the process of replying of the request is an async task, the status
+  // might have be reset at the time we get the result from parent process.
+  // Ex. if the page got closed or naviagated immediately after user replied to
+  // the request. Therefore, the status should be either `pending` or `unknown`.
+  const RStatus status = GetRequestStatus(mContext, mType);
+  MOZ_ASSERT(status == RStatus::ePENDING || status == RStatus::eUNKNOWN);
+  if (status == RStatus::ePENDING) {
+    SetRequestStatus(RStatus::eALLOWED);
+  }
+  mContext = nullptr;
   return NS_OK;
+}
+
+/* static */
+void GVAutoplayPermissionRequestor::AskForPermissionIfNeeded(
+    nsPIDOMWindowInner* aWindow) {
+  if (!aWindow) {
+    return;
+  }
+
+  // The request is used for content permission, so it's no need to create a
+  // content request in parent process.
+  if (XRE_IsParentProcess()) {
+    return;
+  }
+
+  // The request status is stored in top-level browsing context only.
+  RefPtr<BrowsingContext> context = aWindow->GetBrowsingContext()->Top();
+  if (!HasEverAskForRequest(context, RType::eAUDIBLE)) {
+    CreateAsyncRequest(aWindow, context, RType::eAUDIBLE);
+  }
+  if (!HasEverAskForRequest(context, RType::eINAUDIBLE)) {
+    CreateAsyncRequest(aWindow, context, RType::eINAUDIBLE);
+  }
+}
+
+/* static */
+bool GVAutoplayPermissionRequestor::HasEverAskForRequest(
+    BrowsingContext* aContext, RType aType) {
+  return GetRequestStatus(aContext, aType) != RStatus::eUNKNOWN;
+}
+
+/* static */
+void GVAutoplayPermissionRequestor::CreateAsyncRequest(
+    nsPIDOMWindowInner* aWindow, BrowsingContext* aContext,
+    GVAutoplayRequestType aType) {
+  nsGlobalWindowInner* innerWindow = nsGlobalWindowInner::Cast(aWindow);
+  if (!innerWindow || !innerWindow->GetPrincipal()) {
+    return;
+  }
+
+  GVAutoplayPermissionRequest::CreateRequest(innerWindow, aContext, aType);
 }
 
 }  // namespace dom
--- dom/media/GVAutoplayPermissionRequest.h
+++ dom/media/GVAutoplayPermissionRequest.h
@@ -25,19 +25,59 @@ class GVAutoplayPermissionRequest : public ContentPermissionRequestBase {
   NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(GVAutoplayPermissionRequest,
                                            ContentPermissionRequestBase)
 
-  static already_AddRefed<GVAutoplayPermissionRequest> CreateRequest(
-      nsGlobalWindowInner* aWindow, GVAutoplayRequestType aType);
-
   // nsIContentPermissionRequest methods
   NS_IMETHOD Cancel(void) override;
   NS_IMETHOD Allow(JS::HandleValue choices) override;
 
  private:
+  // Only allow to create this request from the requestor.
+  friend class GVAutoplayPermissionRequestor;
+  static void CreateRequest(nsGlobalWindowInner* aWindow,
+                            BrowsingContext* aContext,
+                            GVAutoplayRequestType aType);
+
   GVAutoplayPermissionRequest(nsGlobalWindowInner* aWindow,
+                              BrowsingContext* aContext,
                               GVAutoplayRequestType aType);
   ~GVAutoplayPermissionRequest();
 
+  void SetRequestStatus(GVAutoplayRequestStatus aStatus);
+
   GVAutoplayRequestType mType;
+  RefPtr<BrowsingContext> mContext;
+};
+
+/**
+ * This class provides a method to request autoplay permission for a page, which
+ * would be used to be a factor to determine if media is allowed to autoplay or
+ * not on GeckoView.
+ *
+ * A page could only have at most one audible request and one inaudible request,
+ * and once a page has been closed or reloaded, those requests would be dropped.
+ * In order to achieve that all media existing in the same page can share the
+ * result of those requests, the request status would only be stored in the
+ * top-level browsing context, which allows them to be synchronized among
+ * different processes when Fission is enabled.
+ *
+ * The current way we choose is to request for a permission when creating media
+ * element, in order to get the response from the embedding app before media
+ * starts playing if the app can response the request quickly enough. However,
+ * the request might be pending if the app doesn't response to it, we might
+ * never get the response. As that is just one factor of determining the
+ * autoplay result, even if we don't get the response for the request, we still
+ * have a chance to play media. Check AutoplayPolicy to see more details about
+ * how we decide the final autoplay decision.
+ */
+class GVAutoplayPermissionRequestor final {
+ public:
+  static void AskForPermissionIfNeeded(nsPIDOMWindowInner* aWindow);
+
+ private:
+  static bool HasEverAskForRequest(BrowsingContext* aContext,
+                                   GVAutoplayRequestType aType);
+  static void CreateAsyncRequest(nsPIDOMWindowInner* aWindow,
+                                 BrowsingContext* aContext,
+                                 GVAutoplayRequestType aType);
 };
 
 }  // namespace dom
