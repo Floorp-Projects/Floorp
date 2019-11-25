5f14b88249cc48f999e4a1381045d2a028c18edf
Philip Jägenstedt <philip@foolip.org>
2019-11-25 16:23:21 +0000

    Bug 1594415 [wpt PR 20123] - [testharness.js] extract the stack for unhandled rejection, a=testonly
    Automatic update from web-platform-tests
    [testharness.js] extract the stack for unhandled rejection (#20123)
    
    Contrary to the earlier comment, a stack may exist if the rejection
    value was an error object with a stack...
    --
    
    wpt-commits: cbeb6f7c0d963489ec456e48ee544d17cfd7efba
    wpt-pr: 20123

--- testing/web-platform/tests/resources/testharness.js
+++ testing/web-platform/tests/resources/testharness.js
@@ -3706,15 +3706,17 @@ policies and contribution forms [3].
         }, false);
 
         addEventListener("unhandledrejection", function(e) {
-            var reason;
-            if (e.reason) {
-                reason  = e.reason.message ? e.reason.message : e.reason;
+            var message;
+            if (e.reason && e.reason.message) {
+                message = "Unhandled rejection: " + e.reason.message;
             } else {
-                reason = e;
+                message = "Unhandled rejection";
             }
-            var message = "Unhandled rejection: " + reason;
-            // There's no stack for unhandled rejections.
-            error_handler(message);
+            var stack;
+            if (e.reason && e.reason.stack) {
+                stack = e.reason.stack;
+            }
+            error_handler(message, stack);
         }, false);
     }
 
