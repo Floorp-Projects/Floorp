5691ae89bcce960f0a91ad24d7b3d03e016223ce
Luca Greco <lgreco@mozilla.com>
2019-11-25 17:35:48 +0000

    Bug 1598079 - Turn abuseReport.openDialog on by default on all channels. r=mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D54026

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2312,7 +2312,7 @@ pref("extensions.abuseReport.enabled", true);
 // Allow AMO to handoff reports to the Firefox integrated dialog.
 pref("extensions.abuseReport.amWebAPI.enabled", true);
 // Opened as a sub-frame of the about:addons page when set to false.
-pref("extensions.abuseReport.openDialog", false);
+pref("extensions.abuseReport.openDialog", true);
 pref("extensions.abuseReport.url", "https://services.addons.mozilla.org/api/v4/abuse/report/addon/");
 pref("extensions.abuseReport.amoDetailsURL", "https://services.addons.mozilla.org/api/v4/addons/addon/");
 
