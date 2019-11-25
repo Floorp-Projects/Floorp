942a5d61757f9419cf786436b6f30195f79e8c1a
jeffin143 <jeffinsam@karunya.edu.in>
2019-11-25 15:41:18 +0000

    Bug 1598539 : convert  NS_STYLE_IME_MODE_* to an enum class in nsStyleConsts.h r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D54255

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -3703,7 +3703,7 @@ nsStyleUIReset::nsStyleUIReset(const Document& aDocument)
     : mUserSelect(StyleUserSelect::Auto),
       mScrollbarWidth(StyleScrollbarWidth::Auto),
       mForceBrokenImageIcon(0),
-      mIMEMode(NS_STYLE_IME_MODE_AUTO),
+      mIMEMode(StyleImeMode::Auto),
       mWindowDragging(StyleWindowDragging::Default),
       mWindowShadow(NS_STYLE_WINDOW_SHADOW_DEFAULT),
       mWindowOpacity(1.0),
