de35b308ec4b67fee80d144900b3a6512aa4815e
Michael Cooper <mcooper@mozilla.com>
2019-11-25 21:56:47 +0000

    Bug 1594035 p2 - Add filler enrollment IDs to old Normandy studies and rollouts. r=Gijs
    Having null or undefined values for enrollment IDs causes trouble with
    Telemetry. The easiest approach is to assign all objects that could have an
    enrollment ID but don't a filler value. This value will be ignored by tooling
    and should be fairly obvious to humans reading the code.
    
    Any newly created studies and rollouts will have enrollment IDs set. The
    migrations introduced here will ensure that any old studies and rollouts get a
    value. Therefor all of them will have an enrollment ID that is non-null. This
    simplifies the code that deals with enrollment IDs, instead of scattering null
    checks all around the code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52085
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/normandy/test/browser/browser_PreferenceExperiments.js
+++ toolkit/components/normandy/test/browser/browser_PreferenceExperiments.js
@@ -48,35 +48,14 @@ function experimentFactory(attrs) {
 
 const NOW = new Date();
 
-const mockV1Data = {
-  hypothetical_experiment: {
-    name: "hypothetical_experiment",
-    branch: "hypo_1",
-    expired: false,
-    lastSeen: NOW.toJSON(),
-    preferenceName: "some.pref",
-    preferenceValue: 2,
-    preferenceType: "integer",
-    previousPreferenceValue: 1,
-    preferenceBranchType: "user",
-    experimentType: "exp",
-  },
-  another_experiment: {
-    name: "another_experiment",
-    branch: "another_4",
-    expired: true,
-    lastSeen: NOW.toJSON(),
-    preferenceName: "another.pref",
-    preferenceValue: true,
-    preferenceType: "boolean",
-    previousPreferenceValue: false,
-    preferenceBranchType: "default",
-    experimentType: "exp",
-  },
-};
-
-const mockV2Data = {
-  experiments: {
+// Note that the below data is not entirely representative of what we see in the
+// real world. Specifically, even the older format includes one experiment with
+// an enrollmentId. There isn't any way this could have happened in the real
+// world. However, including it here lets us use a consistent set of data for
+// all the tests. The simplification of the tests seems worth it.
+const mockDataVersions = [
+  // before any migrations
+  {
     hypothetical_experiment: {
       name: "hypothetical_experiment",
       branch: "hypo_1",
@@ -88,6 +67,7 @@ const mockV2Data = {
       previousPreferenceValue: 1,
       preferenceBranchType: "user",
       experimentType: "exp",
+      enrollmentId: "existing-enrollment-id",
     },
     another_experiment: {
       name: "another_experiment",
@@ -102,116 +82,193 @@ const mockV2Data = {
       experimentType: "exp",
     },
   },
-};
 
-const mockV3Data = {
-  experiments: {
-    hypothetical_experiment: {
-      name: "hypothetical_experiment",
-      branch: "hypo_1",
-      expired: false,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "some.pref": {
-          preferenceValue: 2,
-          preferenceType: "integer",
-          previousPreferenceValue: 1,
-          preferenceBranchType: "user",
-        },
+  // after migration 01
+  {
+    experiments: {
+      hypothetical_experiment: {
+        name: "hypothetical_experiment",
+        branch: "hypo_1",
+        expired: false,
+        lastSeen: NOW.toJSON(),
+        preferenceName: "some.pref",
+        preferenceValue: 2,
+        preferenceType: "integer",
+        previousPreferenceValue: 1,
+        preferenceBranchType: "user",
+        experimentType: "exp",
+        enrollmentId: "existing-enrollment-id",
       },
-      experimentType: "exp",
-    },
-    another_experiment: {
-      name: "another_experiment",
-      branch: "another_4",
-      expired: true,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "another.pref": {
-          preferenceValue: true,
-          preferenceType: "boolean",
-          previousPreferenceValue: false,
-          preferenceBranchType: "default",
-        },
+      another_experiment: {
+        name: "another_experiment",
+        branch: "another_4",
+        expired: true,
+        lastSeen: NOW.toJSON(),
+        preferenceName: "another.pref",
+        preferenceValue: true,
+        preferenceType: "boolean",
+        previousPreferenceValue: false,
+        preferenceBranchType: "default",
+        experimentType: "exp",
       },
-      experimentType: "exp",
     },
   },
-};
 
-const mockV4Data = {
-  experiments: {
-    hypothetical_experiment: {
-      name: "hypothetical_experiment",
-      branch: "hypo_1",
-      actionName: "SinglePreferenceExperimentAction",
-      expired: false,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "some.pref": {
-          preferenceValue: 2,
-          preferenceType: "integer",
-          previousPreferenceValue: 1,
-          preferenceBranchType: "user",
+  // after migration 02
+  {
+    experiments: {
+      hypothetical_experiment: {
+        name: "hypothetical_experiment",
+        branch: "hypo_1",
+        expired: false,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "some.pref": {
+            preferenceValue: 2,
+            preferenceType: "integer",
+            previousPreferenceValue: 1,
+            preferenceBranchType: "user",
+          },
         },
+        experimentType: "exp",
+        enrollmentId: "existing-enrollment-id",
       },
-      experimentType: "exp",
-    },
-    another_experiment: {
-      name: "another_experiment",
-      branch: "another_4",
-      actionName: "SinglePreferenceExperimentAction",
-      expired: true,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "another.pref": {
-          preferenceValue: true,
-          preferenceType: "boolean",
-          previousPreferenceValue: false,
-          preferenceBranchType: "default",
+      another_experiment: {
+        name: "another_experiment",
+        branch: "another_4",
+        expired: true,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "another.pref": {
+            preferenceValue: true,
+            preferenceType: "boolean",
+            previousPreferenceValue: false,
+            preferenceBranchType: "default",
+          },
         },
+        experimentType: "exp",
       },
-      experimentType: "exp",
     },
   },
-};
 
-const mockV5Data = {
-  experiments: {
-    hypothetical_experiment: {
-      slug: "hypothetical_experiment",
-      branch: "hypo_1",
-      actionName: "SinglePreferenceExperimentAction",
-      expired: false,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "some.pref": {
-          preferenceValue: 2,
-          preferenceType: "integer",
-          previousPreferenceValue: 1,
-          preferenceBranchType: "user",
+  // after migration 03
+  {
+    experiments: {
+      hypothetical_experiment: {
+        name: "hypothetical_experiment",
+        branch: "hypo_1",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: false,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "some.pref": {
+            preferenceValue: 2,
+            preferenceType: "integer",
+            previousPreferenceValue: 1,
+            preferenceBranchType: "user",
+          },
         },
+        experimentType: "exp",
+        enrollmentId: "existing-enrollment-id",
       },
-      experimentType: "exp",
-    },
-    another_experiment: {
-      slug: "another_experiment",
-      branch: "another_4",
-      actionName: "SinglePreferenceExperimentAction",
-      expired: true,
-      lastSeen: NOW.toJSON(),
-      preferences: {
-        "another.pref": {
-          preferenceValue: true,
-          preferenceType: "boolean",
-          previousPreferenceValue: false,
-          preferenceBranchType: "default",
+      another_experiment: {
+        name: "another_experiment",
+        branch: "another_4",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: true,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "another.pref": {
+            preferenceValue: true,
+            preferenceType: "boolean",
+            previousPreferenceValue: false,
+            preferenceBranchType: "default",
+          },
         },
+        experimentType: "exp",
       },
-      experimentType: "exp",
     },
   },
-};
+
+  // after migration 04
+  {
+    experiments: {
+      hypothetical_experiment: {
+        slug: "hypothetical_experiment",
+        branch: "hypo_1",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: false,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "some.pref": {
+            preferenceValue: 2,
+            preferenceType: "integer",
+            previousPreferenceValue: 1,
+            preferenceBranchType: "user",
+          },
+        },
+        experimentType: "exp",
+        enrollmentId: "existing-enrollment-id",
+      },
+      another_experiment: {
+        slug: "another_experiment",
+        branch: "another_4",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: true,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "another.pref": {
+            preferenceValue: true,
+            preferenceType: "boolean",
+            previousPreferenceValue: false,
+            preferenceBranchType: "default",
+          },
+        },
+        experimentType: "exp",
+      },
+    },
+  },
+
+  // after migration 05
+  {
+    experiments: {
+      hypothetical_experiment: {
+        slug: "hypothetical_experiment",
+        branch: "hypo_1",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: false,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "some.pref": {
+            preferenceValue: 2,
+            preferenceType: "integer",
+            previousPreferenceValue: 1,
+            preferenceBranchType: "user",
+          },
+        },
+        experimentType: "exp",
+        enrollmentId: "existing-enrollment-id",
+      },
+      another_experiment: {
+        slug: "another_experiment",
+        branch: "another_4",
+        actionName: "SinglePreferenceExperimentAction",
+        expired: true,
+        lastSeen: NOW.toJSON(),
+        preferences: {
+          "another.pref": {
+            preferenceValue: true,
+            preferenceType: "boolean",
+            previousPreferenceValue: false,
+            preferenceBranchType: "default",
+          },
+        },
+        experimentType: "exp",
+        enrollmentId: "__NO_ENROLLMENT_ID__",
+      },
+    },
+  },
+];
 
 /**
  * Make a mock `JsonFile` object with a no-op `saveSoon` method and a deep copy
@@ -226,37 +283,33 @@ function makeMockJsonFile(data = {}) {
   };
 }
 
+let migrationsList = [
+  PreferenceExperiments.migrations.migration01MoveExperiments,
+  PreferenceExperiments.migrations.migration02MultiPreference,
+  PreferenceExperiments.migrations.migration03AddActionName,
+  PreferenceExperiments.migrations.migration04RenameNameToSlug,
+  PreferenceExperiments.migrations.migration05AddFillerEnrollmentId,
+];
+
 /** Test that each migration results in the expected data */
 add_task(async function test_migrations() {
-  let mockJsonFile = makeMockJsonFile(mockV1Data);
-  await PreferenceExperiments.migrations.migration01MoveExperiments(
-    mockJsonFile
-  );
-  Assert.deepEqual(mockJsonFile.data, mockV2Data);
-
-  mockJsonFile = makeMockJsonFile(mockV2Data);
-  await PreferenceExperiments.migrations.migration02MultiPreference(
-    mockJsonFile
-  );
-  Assert.deepEqual(mockJsonFile.data, mockV3Data);
-
-  mockJsonFile = makeMockJsonFile(mockV3Data);
-  await PreferenceExperiments.migrations.migration03AddActionName(mockJsonFile);
-  Assert.deepEqual(mockJsonFile.data, mockV4Data);
-
-  mockJsonFile = makeMockJsonFile(mockV4Data);
-  await PreferenceExperiments.migrations.migration04RenameNameToSlug(
-    mockJsonFile
-  );
-  Assert.deepEqual(mockJsonFile.data, mockV5Data);
+  let mockJsonFile = makeMockJsonFile(mockDataVersions[0]);
+  for (let i = 0; i < migrationsList.length; i++) {
+    await migrationsList[i](mockJsonFile);
+    Assert.deepEqual(
+      mockJsonFile.data,
+      mockDataVersions[i + 1],
+      `Migration ${i + 1} should produce the expected results`
+    );
+  }
 });
 
 add_task(async function migration03KeepsActionName() {
-  let mockData = JSON.parse(JSON.stringify(mockV3Data));
+  let mockData = JSON.parse(JSON.stringify(mockDataVersions[2]));
   mockData.experiments.another_experiment.actionName = "SomeOldAction";
   const mockJsonFile = makeMockJsonFile(mockData);
-  // Output should be the same as mockV4Data, but preserving the action.
-  const migratedData = JSON.parse(JSON.stringify(mockV4Data));
+  // Output should be the same as mockDataVersions[3], but preserving the action.
+  const migratedData = JSON.parse(JSON.stringify(mockDataVersions[3]));
   migratedData.experiments.another_experiment.actionName = "SomeOldAction";
 
   await PreferenceExperiments.migrations.migration03AddActionName(mockJsonFile);
@@ -264,13 +317,9 @@ add_task(async function migration03KeepsActionName() {
 });
 
 add_task(async function migrations_are_idempotent() {
-  let dataVersions = [
-    [PreferenceExperiments.migrations.migration01MoveExperiments, mockV1Data],
-    [PreferenceExperiments.migrations.migration02MultiPreference, mockV2Data],
-    [PreferenceExperiments.migrations.migration03AddActionName, mockV3Data],
-    [PreferenceExperiments.migrations.migration04RenameNameToSlug, mockV4Data],
-  ];
-  for (const [migration, mockOldData] of dataVersions) {
+  for (let i = 0; i < migrationsList.length; i++) {
+    const migration = migrationsList[i];
+    const mockOldData = mockDataVersions[i];
     const mockJsonFileOnce = makeMockJsonFile(mockOldData);
     const mockJsonFileTwice = makeMockJsonFile(mockOldData);
     await migration(mockJsonFileOnce);
