4568986f3567a0827dc597d8da57e577fe57964d
Nick Thomas <nthomas@mozilla.com>
2019-11-26 04:01:49 +0000

    Bug 1598436 - add nightly bouncer checks to cron, r=aki
    Depends on D54657
    
    Differential Revision: https://phabricator.services.mozilla.com/D54658

---
+++ testing/mozharness/configs/releases/bouncer_firefox_nightly.py
@@ -0,0 +1,75 @@
+# lint_ignore=E501
+config = {
+    "products": {
+        "installer-latest": {
+            "product-name": "Firefox-nightly-latest",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "installer-latest-ssl": {
+            "product-name": "Firefox-nightly-latest-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "installer-latest-l10n-ssl": {
+            "product-name": "Firefox-nightly-latest-l10n-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "msi-latest": {
+            "product-name": "Firefox-nightly-msi-latest-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+            ],
+        },
+        "msi-latest-l10n": {
+            "product-name": "Firefox-nightly-msi-latest-l10n-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+            ],
+        },
+        "stub-installer": {
+            "product-name": "Firefox-nightly-stub",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "stub-installer-l10n": {
+            "product-name": "Firefox-nightly-stub-l10n",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+    },
+}
