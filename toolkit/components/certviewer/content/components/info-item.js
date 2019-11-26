cca656eccaf6189512f8bb000e9cbc383fee8126
daniellelc <danielleleb12@gmail.com>
2019-11-26 11:00:17 +0000

    Bug 1588776 - Makes long-hex fields selectable without causing them to change state. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D51165
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/certviewer/content/components/info-item.js
+++ toolkit/components/certviewer/content/components/info-item.js
@@ -64,8 +64,12 @@ export class InfoItem extends HTMLElement {
 
     if (labelId === "modulus" || labelId === "public-value") {
       info.classList.add("long-hex");
-      this.addEventListener("click", () => {
-        info.classList.toggle("long-hex-open");
+      this.addEventListener("mouseup", () => {
+        // If a range of text is selected, don't toggle the class that
+        // hides/shows additional text.
+        if (window.getSelection().type !== "Range") {
+          info.classList.toggle("long-hex-open");
+        }
       });
     }
 
