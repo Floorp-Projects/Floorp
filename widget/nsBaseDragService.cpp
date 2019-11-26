3fb5164722a534e0d6ee81229dad969088ddf9b7
Andreas Farre <farre@mozilla.com>
2019-11-26 23:28:37 +0000

    Bug 1595399 - Prevent EndDragSession from being called recursively. r=smaug
    nsGlobalWindowOuter::EnterModalState will try to end the current drag
    session, so we need to keep track if we're entering modal state from
    an event firing from ending the drag session.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53934

--- widget/nsBaseDragService.cpp
+++ widget/nsBaseDragService.cpp
@@ -62,6 +62,7 @@ nsBaseDragService::nsBaseDragService()
     : mCanDrop(false),
       mOnlyChromeDrop(false),
       mDoingDrag(false),
+      mEndingSession(false),
       mHasImage(false),
       mUserCancelled(false),
       mDragEventDispatchedToChildProcess(false),
@@ -453,9 +454,11 @@ int32_t nsBaseDragService::TakeChildProcessDragAction() {
 //-------------------------------------------------------------------------
 NS_IMETHODIMP
 nsBaseDragService::EndDragSession(bool aDoneDrag, uint32_t aKeyModifiers) {
-  if (!mDoingDrag) {
+  if (!mDoingDrag || mEndingSession) {
     return NS_ERROR_FAILURE;
   }
+
+  mEndingSession = true;
 
   if (aDoneDrag && !mSuppressLevel) {
     FireDragEventAtSource(eDragEnd, aKeyModifiers);
@@ -485,6 +488,7 @@ nsBaseDragService::EndDragSession(bool aDoneDrag, uint32_t aKeyModifiers) {
   }
 
   mDoingDrag = false;
+  mEndingSession = false;
   mCanDrop = false;
 
   // release the source we've been holding on to.
