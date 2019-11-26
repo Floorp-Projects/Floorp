73517788f6f3dd3262fbbce44b52135efc361a0c
Leon Han <leon.han@intel.com>
2019-11-26 11:21:31 +0000

    Bug 1595451 [wpt PR 20193] - [webnfc] Make NDEFReader#scan() return a Promise, a=testonly
    Automatic update from web-platform-tests
    [webnfc] Make NDEFReader#scan() return a Promise
    
    Previously NDEFReader#scan() returns void and an NDEFErrorEvent will be
    dispatched in case that the scan operation cannot be started
    successfully.
    
    This is not a well accepted pattern, and also to align with
    NDEFWriter#push(), this CL makes NDEFReader#scan() return a Promise
    instead.
    
    Note that now NDEFErrorEvent is only used to notify Mojo disconnection,
    a follow-up CL will remove it completely by using ErrorEvent instead.
    
    The spec change:
    https://github.com/w3c/web-nfc/pull/398
    https://github.com/w3c/web-nfc/pull/432
    
    BUG=520391
    
    Change-Id: I1477258ab70f7e40da31ea8795d63125b6a13af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899586
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716904}
    
    --
    
    wpt-commits: 581a056bf7a743722a2e83cb80e1a8ce250e4f9d
    wpt-pr: 20193

--- testing/web-platform/tests/web-nfc/resources/nfc-helpers.js
+++ testing/web-platform/tests/web-nfc/resources/nfc-helpers.js
@@ -174,22 +174,20 @@ function testNDEFScanOptions(message, scanOptions, unmatchedScanOptions, desc) {
     const reader2 = new NDEFReader();
     const controller = new AbortController();
 
-    mockNFC.setReadingMessage(message);
-
     // Reading from unmatched reader will not be triggered
     reader1.onreading = t.unreached_func("reading event should not be fired.");
     unmatchedScanOptions.signal = controller.signal;
-    reader1.scan(unmatchedScanOptions);
+    await reader1.scan(unmatchedScanOptions);
 
     const readerWatcher = new EventWatcher(t, reader2, ["reading", "error"]);
-
     const promise = readerWatcher.wait_for("reading").then(event => {
       controller.abort();
       assertWebNDEFMessagesEqual(event.message, new NDEFMessage(message));
     });
-    // NDEFReader#scan() asynchronously dispatches the onreading event.
     scanOptions.signal = controller.signal;
-    reader2.scan(scanOptions);
+    await reader2.scan(scanOptions);
+
+    mockNFC.setReadingMessage(message);
     await promise;
   }, desc);
 }
@@ -200,19 +198,16 @@ function testReadingMultiMessages(
     const reader = new NDEFReader();
     const controller = new AbortController();
     const readerWatcher = new EventWatcher(t, reader, ["reading", "error"]);
-
     const promise = readerWatcher.wait_for("reading").then(event => {
       controller.abort();
       assertWebNDEFMessagesEqual(event.message, new NDEFMessage(message));
     });
-    // NDEFReader#scan() asynchronously dispatches the onreading event.
     scanOptions.signal = controller.signal;
-    reader.scan(scanOptions);
+    await reader.scan(scanOptions);
 
     // Unmatched message will not be read
     mockNFC.setReadingMessage(unmatchedMessage);
     mockNFC.setReadingMessage(message);
-
     await promise;
   }, desc);
 }
