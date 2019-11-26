9954883286443dd03d153a08c4dfca69b83928e2
Coroiu Cristina <ccoroiu@mozilla.com>
2019-11-26 04:40:35 +0200

    Backed out 6 changesets (bug 1598516) for Mn and pgo failures on a CLOSED TREE
    Backed out changeset 63a84cdfd937 (bug 1598516)
    Backed out changeset 894aaa4bc62e (bug 1598516)
    Backed out changeset e19ada9e8b30 (bug 1598516)
    Backed out changeset 72f2256ae171 (bug 1598516)
    Backed out changeset 6ba74ff735ba (bug 1598516)
    Backed out changeset b235cf22b0e4 (bug 1598516)

--- netwerk/ipc/DocumentChannelChild.cpp
+++ netwerk/ipc/DocumentChannelChild.cpp
@@ -31,9 +31,6 @@
 using namespace mozilla::dom;
 using namespace mozilla::ipc;
 
-extern mozilla::LazyLogModule gDocumentChannelLog;
-#define LOG(fmt) MOZ_LOG(gDocumentChannelLog, mozilla::LogLevel::Verbose, fmt)
-
 namespace mozilla {
 namespace net {
 
@@ -77,16 +74,10 @@ DocumentChannelChild::DocumentChannelChild(
       mLoadFlags(aLoadFlags),
       mURI(aLoadState->URI()),
       mLoadInfo(aLoadInfo) {
-  LOG(("DocumentChannelChild ctor [this=%p, uri=%s]", this,
-       aLoadState->URI()->GetSpecOrDefault().get()));
   RefPtr<nsHttpHandler> handler = nsHttpHandler::GetInstance();
   uint64_t channelId;
   Unused << handler->NewChannelId(channelId);
   mChannelId = channelId;
-}
-
-DocumentChannelChild::~DocumentChannelChild() {
-  LOG(("DocumentChannelChild dtor [this=%p]", this));
 }
 
 NS_IMETHODIMP
@@ -213,8 +204,6 @@ IPCResult DocumentChannelChild::RecvFailedAsyncOpen(
 }
 
 void DocumentChannelChild::ShutdownListeners(nsresult aStatusCode) {
-  LOG(("DocumentChannelChild ShutdownListeners [this=%p, status=%" PRIx32 "]",
-       this, static_cast<uint32_t>(aStatusCode)));
   mStatus = aStatusCode;
 
   nsCOMPtr<nsIStreamListener> l = mListener;
@@ -266,9 +255,6 @@ IPCResult DocumentChannelChild::RecvDeleteSelf() {
 IPCResult DocumentChannelChild::RecvRedirectToRealChannel(
     const RedirectToRealChannelArgs& aArgs,
     RedirectToRealChannelResolver&& aResolve) {
-  LOG(("DocumentChannelChild RecvRedirectToRealChannel [this=%p, uri=%s]", this,
-       aArgs.uri()->GetSpecOrDefault().get()));
-
   RefPtr<dom::Document> loadingDocument;
   mLoadInfo->GetLoadingDocument(getter_AddRefs(loadingDocument));
 
@@ -295,6 +281,13 @@ IPCResult DocumentChannelChild::RecvRedirectToRealChannel(
                             nullptr,     // aCallbacks
                             aArgs.newLoadFlags());
 
+  RefPtr<HttpChannelChild> httpChild = do_QueryObject(newChannel);
+  RefPtr<nsIChildChannel> childChannel = do_QueryObject(newChannel);
+  if (NS_FAILED(rv)) {
+    MOZ_DIAGNOSTIC_ASSERT(false, "NS_NewChannelInternal failed");
+    return IPC_OK();
+  }
+
   // This is used to report any errors back to the parent by calling
   // CrossProcessRedirectFinished.
   auto scopeExit = MakeScopeExit([&]() {
@@ -304,11 +297,6 @@ IPCResult DocumentChannelChild::RecvRedirectToRealChannel(
     mRedirectResolver = nullptr;
   });
 
-  if (NS_FAILED(rv)) {
-    return IPC_OK();
-  }
-
-  RefPtr<HttpChannelChild> httpChild = do_QueryObject(newChannel);
   if (httpChild) {
     rv = httpChild->SetChannelId(aArgs.channelId());
   }
@@ -362,7 +350,6 @@ IPCResult DocumentChannelChild::RecvRedirectToRealChannel(
   }
 
   // connect parent.
-  nsCOMPtr<nsIChildChannel> childChannel = do_QueryInterface(newChannel);
   if (childChannel) {
     rv = childChannel->ConnectParent(
         aArgs.registrarId());  // creates parent channel
@@ -706,5 +693,3 @@ DocumentChannelChild::SetChannelId(uint64_t aChannelId) {
 
 }  // namespace net
 }  // namespace mozilla
-
-#undef LOG
