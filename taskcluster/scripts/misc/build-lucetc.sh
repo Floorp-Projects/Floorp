93d3e01a912fb7b28ad8bbb88a8a177b22cef7de
Ricky Stewart <rstewart@mozilla.com>
2019-11-27 20:02:55 +0000

    Bug 1599607 - lucetc toolchain artifact should unpack to a directory called "lucetc" r=firefox-build-system-reviewers,mshal
    Differential Revision: https://phabricator.services.mozilla.com/D54823
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/scripts/misc/build-lucetc.sh
+++ taskcluster/scripts/misc/build-lucetc.sh
@@ -10,7 +10,11 @@ export CXXFLAGS=$CFLAGS
 export CXX=$MOZ_FETCHES_DIR/clang/bin/clang++
 export AR=$MOZ_FETCHES_DIR/clang/bin/llvm-ar
 export RUSTFLAGS="-C linker=$CXX -C link-arg=$CXXFLAGS"
-
 make build
-mkdir -p $UPLOAD_DIR
-tar --xz -cf $UPLOAD_DIR/lucetc.tar.xz target/release/lucetc
+
+# Put the lucetc binary in a directory called "lucetc", which is where consumers
+# will expect it.
+mkdir -p $UPLOAD_DIR/lucetc
+mv target/release/lucetc $UPLOAD_DIR/lucetc
+tar -C $UPLOAD_DIR --xz -cf $UPLOAD_DIR/lucetc.tar.xz lucetc
+rm -rf $UPLOAD_DIR/lucetc
