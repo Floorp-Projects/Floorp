718534d4bfd9dd4deb7fb55bd40f53c008218a4d
Emilio Cobos Álvarez <emilio@crisal.io>
2019-11-27 18:45:52 +0000

    Bug 1599843 - FetchDriver::FinishOnStopRequest is infallible. r=baku
    Depends on D54992
    
    Differential Revision: https://phabricator.services.mozilla.com/D54993
    
    --HG--
    extra : moz-landing-system : lando

--- dom/fetch/FetchDriver.cpp
+++ dom/fetch/FetchDriver.cpp
@@ -311,7 +311,8 @@ AlternativeDataStreamListener::OnStopRequest(nsIRequest* aRequest,
   // continue the final step for the case FetchDriver::OnStopRequest is called
   // earlier than AlternativeDataStreamListener::OnStopRequest
   MOZ_ASSERT(fetchDriver);
-  return fetchDriver->FinishOnStopRequest(this);
+  fetchDriver->FinishOnStopRequest(this);
+  return NS_OK;
 }
 
 NS_IMETHODIMP
@@ -1262,16 +1263,17 @@ FetchDriver::OnStopRequest(nsIRequest* aRequest, nsresult aStatusCode) {
     }
   }
 
-  return FinishOnStopRequest(altDataListener);
+  FinishOnStopRequest(altDataListener);
+  return NS_OK;
 }
 
-nsresult FetchDriver::FinishOnStopRequest(
+void FetchDriver::FinishOnStopRequest(
     AlternativeDataStreamListener* aAltDataListener) {
   AssertIsOnMainThread();
   // OnStopRequest is not called from channel, that means the main data loading
   // does not finish yet. Reaching here since alternative data loading finishes.
   if (!mOnStopRequestCalled) {
-    return NS_OK;
+    return;
   }
 
   MOZ_DIAGNOSTIC_ASSERT(!mAltDataListener);
@@ -1280,7 +1282,7 @@ nsresult FetchDriver::FinishOnStopRequest(
       aAltDataListener->Status() == AlternativeDataStreamListener::LOADING) {
     // For LOADING case, channel holds the reference of altDataListener, no need
     // to restore it to mAltDataListener.
-    return NS_OK;
+    return;
   }
 
   if (mObserver) {
@@ -1298,7 +1300,6 @@ nsresult FetchDriver::FinishOnStopRequest(
   }
 
   mChannel = nullptr;
-  return NS_OK;
 }
 
 NS_IMETHODIMP
