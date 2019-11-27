fadd311079446734e9fbc726a464b04986f72fc0
Tim Nguyen <ntim.bugs@gmail.com>
2019-11-27 17:36:13 +0000

    Bug 1599070 - Replace references to textbox.reset(). r=Standard8 a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D54742
    
    --HG--
    extra : source : 080af3128eb1fe9f6d2349f7e466815716806cd8

--- toolkit/content/widgets/search-textbox.js
+++ toolkit/content/widgets/search-textbox.js
@@ -201,16 +201,6 @@
       return this.inputField.disabled;
     }
 
-    reset() {
-      this.value = this.defaultValue;
-      // XXX: Is this still needed ?
-      try {
-        this.editor.transactionManager.clear();
-        return true;
-      } catch (e) {}
-      return false;
-    }
-
     _fireCommand(me) {
       if (me._timer) {
         clearTimeout(me._timer);
