8c66e9177e93ac60b5409bd8c7512bc5608e2007
Nick Thomas <nthomas@mozilla.com>
2019-11-27 02:53:21 +0000

    Bug 1598436 - add ESR bouncer checks to cron, r=aki
    Fixes up ESR bouncer aliases now that ESR60 is EOL.
    Adds check for latest products used by www.mozilla.org.
    Enables the cron checks.
    Fixes up a script error which meant we weren't checking all partial platforms in release automation.
    
    Depends on D54656
    
    Differential Revision: https://phabricator.services.mozilla.com/D54657

--- testing/mozharness/configs/releases/bouncer_firefox_esr.py
+++ testing/mozharness/configs/releases/bouncer_firefox_esr.py
@@ -1,32 +1,99 @@
 # lint_ignore=E501
 config = {
     "products": {
+        # for installers, stubs, msi (ie not updates) ...
+        # products containing "latest" are for www.mozilla.org via cron-bouncer-check
+        # products using versions are for release automation via release-bouncer-check-firefox
         "installer": {
             "product-name": "Firefox-%(version)s",
             "check_uptake": True,
-            # convert to firefox-esr-latest when ESR52 stops
             "platforms": [
                 "linux",
                 "linux64",
                 "osx",
                 "win",
                 "win64",
+                "win64-aarch64",
+            ],
+        },
+        "installer-latest": {
+            "product-name": "Firefox-esr-latest",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "installer-next-latest": {
+            "product-name": "Firefox-esr-next-latest",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
             ],
         },
         "installer-ssl": {
             "product-name": "Firefox-%(version)s-SSL",
             "check_uptake": True,
-            # convert to firefox-esr-latest-ssl when ESR52 stops
             "platforms": [
                 "linux",
                 "linux64",
                 "osx",
                 "win",
                 "win64",
+               "win64-aarch64",
+            ],
+        },
+        "installer-latest-ssl": {
+            "product-name": "Firefox-esr-latest-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
+            ],
+        },
+        "installer-next-latest-ssl": {
+            "product-name": "Firefox-esr-next-latest-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "linux",
+                "linux64",
+                "osx",
+                "win",
+                "win64",
+                "win64-aarch64",
             ],
         },
         "msi": {
             "product-name": "Firefox-%(version)s-msi-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+            ],
+        },
+        "msi-latest": {
+            "product-name": "Firefox-esr-msi-latest-SSL",
+            "check_uptake": True,
+            "platforms": [
+                "win",
+                "win64",
+            ],
+        },
+        "msi-next-latest": {
+            "product-name": "Firefox-esr-next-msi-latest-SSL",
             "check_uptake": True,
             "platforms": [
                 "win",
@@ -42,18 +109,7 @@ config = {
                 "osx",
                 "win",
                 "win64",
-            ],
-        },
-        "complete-mar-bz2": {
-            "product-name": "Firefox-%(version)s-Complete-bz2",
-            "check_uptake": True,
-            "platforms": [
-                "linux",
-                "linux64",
-                "osx",
-                "win",
-                "win64",
-                "win64-aarch64",
+                # add "win64-aarch64" when ESR60 dropped
             ],
         },
     },
@@ -67,6 +123,7 @@ config = {
                 "osx",
                 "win",
                 "win64",
+                # add "win64-aarch64" when ESR60 dropped
             ],
         },
     },
