210cc7dc4670d5437326ab0e9d73acf0b5217b68
Mark Banner <standard8@mozilla.com>
2019-11-28 11:30:57 +0000

    Bug 1600000 - Remove unused promiseTabLoadEvent from search's mochitest head.js. r=mikedeboer
    Differential Revision: https://phabricator.services.mozilla.com/D55077

--- browser/components/search/test/browser/head.js
+++ browser/components/search/test/browser/head.js
@@ -176,40 +176,6 @@ function promiseStateChangeURI() {
   });
 }
 
-/**
- * Waits for a load (or custom) event to finish in a given tab. If provided
- * load an uri into the tab.
- *
- * @param {object} tab
- *        The tab to load into.
- * @param {string} [url]
- *        The url to load, or the current url.
- * @returns {Promise} resolved when the event is handled.
- * @resolves to the received event
- * @rejects if a valid load event is not received within a meaningful interval
- */
-function promiseTabLoadEvent(tab, url) {
-  info("Wait tab event: load");
-
-  function handle(loadedUrl) {
-    if (loadedUrl === "about:blank" || (url && loadedUrl !== url)) {
-      info(`Skipping spurious load event for ${loadedUrl}`);
-      return false;
-    }
-
-    info("Tab event received: load");
-    return true;
-  }
-
-  let loaded = BrowserTestUtils.browserLoaded(tab.linkedBrowser, false, handle);
-
-  if (url) {
-    BrowserTestUtils.loadURI(tab.linkedBrowser, url);
-  }
-
-  return loaded;
-}
-
 // Get an array of the one-off buttons.
 function getOneOffs() {
   let oneOffs = [];
