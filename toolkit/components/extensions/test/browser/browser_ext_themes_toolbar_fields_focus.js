a4783d2b3419ed4cd5c0629aca77238d4be8a942
Dão Gottwald <dao@mozilla.com>
2019-11-28 15:11:14 +0000

    Bug 1590759 - Ensure webextension theme's toolbar_field_text_focus is visible when overriding toolbar_field_focus' alpha value. r=mikedeboer
    Differential Revision: https://phabricator.services.mozilla.com/D55093

--- toolkit/components/extensions/test/browser/browser_ext_themes_toolbar_fields_focus.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_toolbar_fields_focus.js
@@ -1,6 +1,14 @@
 "use strict";
 
-add_task(async function test_toolbar_field_hover() {
+add_task(async function setup() {
+  // Remove the `remotecontrol` attribute since it interferes with the urlbar styling.
+  document.documentElement.removeAttribute("remotecontrol");
+  registerCleanupFunction(() => {
+    document.documentElement.setAttribute("remotecontrol", "true");
+  });
+});
+
+add_task(async function test_toolbar_field_focus() {
   const TOOLBAR_FIELD_BACKGROUND = "#FF00FF";
   const TOOLBAR_FIELD_COLOR = "#00FF00";
   const TOOLBAR_FOCUS_BACKGROUND = "#FF0000";
@@ -25,23 +33,18 @@ add_task(async function test_toolbar_field_hover() {
 
   await extension.startup();
 
-  // Remove the `remotecontrol` attribute since it interferes with the urlbar styling.
-  document.documentElement.removeAttribute("remotecontrol");
-  registerCleanupFunction(() => {
-    document.documentElement.setAttribute("remotecontrol", "true");
-  });
   info("Checking toolbar field's focus color");
 
   let urlBar = gURLBar.megabar
     ? document.querySelector("#urlbar-background")
     : gURLBar.textbox;
   gURLBar.textbox.setAttribute("focused", "true");
+
   Assert.equal(
     window.getComputedStyle(urlBar).backgroundColor,
     `rgb(${hexToRGB(TOOLBAR_FOCUS_BACKGROUND).join(", ")})`,
     "Background Color is changed"
   );
-
   Assert.equal(
     window.getComputedStyle(urlBar).color,
     `rgb(${hexToRGB(TOOLBAR_FOCUS_TEXT).join(", ")})`,
@@ -61,5 +64,43 @@ add_task(async function test_toolbar_field_hover() {
     `rgb(${hexToRGB(TOOLBAR_FIELD_COLOR).join(", ")})`,
     "Text Color is set back to initial"
   );
+  await extension.unload();
+});
+
+add_task(async function test_toolbar_field_focus_low_alpha() {
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      theme: {
+        colors: {
+          frame: "#FF0000",
+          tab_background_color: "#ffffff",
+          toolbar_field: "#FF00FF",
+          toolbar_field_text: "#00FF00",
+          toolbar_field_focus: "rgba(0, 0, 255, 0.4)",
+          toolbar_field_text_focus: "red",
+          toolbar_field_border_focus: "#FFFFFF",
+        },
+      },
+    },
+  });
+
+  await extension.startup();
+  gURLBar.textbox.setAttribute("focused", "true");
+
+  let urlBar = gURLBar.megabar
+    ? document.querySelector("#urlbar-background")
+    : gURLBar.textbox;
+  Assert.equal(
+    window.getComputedStyle(urlBar).backgroundColor,
+    `rgba(0, 0, 255, 0.7)`,
+    "Background color has minimum opacity enforced"
+  );
+  Assert.equal(
+    window.getComputedStyle(urlBar).color,
+    `rgb(255, 255, 255)`,
+    "Text color has been overridden to match background"
+  );
+
+  gURLBar.textbox.removeAttribute("focused");
   await extension.unload();
 });
