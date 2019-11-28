62c1123521a452e9c8831fcc2490f72c7777ced9
Nihanth Subramanya <nhnt11@gmail.com>
2019-11-28 16:21:52 +0000

    Bug 1598223 - Import doh-rollout src from github into browser/extensions. r=mixedpuppy
    Based on upstream https://github.com/mozilla/doh-rollout/commit/0bb6e992f70fa90c7c70b0529038a34d099a286d
    
    Differential Revision: https://phabricator.services.mozilla.com/D54617

---
+++ browser/extensions/doh-rollout/experiments/doorhanger/api.js
@@ -0,0 +1,134 @@
+"use strict";
+/* global Services, ChromeUtils, BrowserWindowTracker,
+   ExtensionCommon, ExtensionAPI */
+/* exported doorhanger */
+
+ChromeUtils.import("resource://gre/modules/Console.jsm");
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+ChromeUtils.import("resource://gre/modules/ExtensionUtils.jsm");
+
+var { EventManager, EventEmitter } = ExtensionCommon;
+const {
+  Management: {
+    global: { tabTracker },
+  },
+} = ChromeUtils.import("resource://gre/modules/Extension.jsm", null);
+
+ChromeUtils.defineModuleGetter(
+  this,
+  "BrowserWindowTracker",
+  "resource:///modules/BrowserWindowTracker.jsm"
+);
+
+/** Return most recent NON-PRIVATE browser window, so that we can
+ * manipulate chrome elements on it.
+ */
+function getMostRecentBrowserWindow() {
+  return BrowserWindowTracker.getTopWindow({
+    private: false,
+    allowPopups: false,
+  });
+}
+
+class DoorhangerEventEmitter extends EventEmitter {
+  async emitShow({
+    name,
+    text,
+    okLabel,
+    okAccessKey,
+    cancelLabel,
+    cancelAccessKey,
+  }) {
+    const self = this;
+    const recentWindow = getMostRecentBrowserWindow();
+    const browser = recentWindow.gBrowser.selectedBrowser;
+    const tabId = tabTracker.getBrowserTabId(browser);
+
+    const primaryAction = {
+      disableHighlight: false,
+      label: okLabel,
+      accessKey: okAccessKey,
+      callback: () => {
+        self.emit("doorhanger-accept", tabId);
+      },
+    };
+    const secondaryActions = [
+      {
+        label: cancelLabel,
+        accessKey: cancelAccessKey,
+        callback: () => {
+          self.emit("doorhanger-decline", tabId);
+        },
+      },
+    ];
+
+    let learnMoreURL = Services.urlFormatter.formatURL(
+      "https://support.mozilla.org/%LOCALE%/kb/firefox-dns-over-https"
+    );
+
+    const options = {
+      hideClose: true,
+      persistWhileVisible: true,
+      persistent: true,
+      autofocus: true,
+      name,
+      popupIconURL: "chrome://browser/skin/connection-secure.svg",
+      learnMoreURL,
+      escAction: "buttoncommand",
+      removeOnDismissal: false,
+    };
+
+    recentWindow.PopupNotifications.show(
+      browser,
+      "doh-first-time",
+      text,
+      null,
+      primaryAction,
+      secondaryActions,
+      options
+    );
+  }
+}
+
+var doorhanger = class doorhanger extends ExtensionAPI {
+  getAPI(context) {
+    const doorhangerEventEmitter = new DoorhangerEventEmitter();
+    return {
+      experiments: {
+        doorhanger: {
+          async show(properties) {
+            await doorhangerEventEmitter.emitShow(properties);
+          },
+          onDoorhangerAccept: new EventManager({
+            context,
+            name: "doorhanger.onDoorhangerAccept",
+            register: fire => {
+              let listener = (value, tabId) => {
+                fire.async(tabId);
+              };
+              doorhangerEventEmitter.on("doorhanger-accept", listener);
+              return () => {
+                doorhangerEventEmitter.off("doorhanger-accept", listener);
+              };
+            },
+          }).api(),
+          onDoorhangerDecline: new EventManager({
+            context,
+            name: "doorhanger.onDoorhangerDecline",
+            register: fire => {
+              let listener = (value, tabId) => {
+                fire.async(tabId);
+              };
+              doorhangerEventEmitter.on("doorhanger-decline", listener);
+              return () => {
+                doorhangerEventEmitter.off("doorhanger-decline", listener);
+              };
+            },
+          }).api(),
+        },
+      },
+    };
+  }
+};
---
+++ browser/extensions/doh-rollout/experiments/preferences/api.js
@@ -0,0 +1,100 @@
+"use strict";
+
+/* exported preferences */
+/* global Components, ExtensionAPI, ExtensionCommon, Services */
+let Cu2 = Components.utils;
+Cu2.import("resource://gre/modules/Services.jsm");
+Cu2.import("resource://gre/modules/ExtensionSettingsStore.jsm");
+Cu2.import("resource://gre/modules/AddonManager.jsm");
+Cu2.import("resource://gre/modules/NetUtil.jsm");
+Cu2.import("resource://gre/modules/ExtensionPreferencesManager.jsm");
+/* global ExtensionPreferencesManager */
+// TODO file scope issue on experiments that join extension contexts causing redeclaration issues.
+
+const TRR_URI_PREF = "network.trr.uri";
+const TRR_DISABLE_ECS_PREF = "network.trr.disable-ECS";
+
+ExtensionPreferencesManager.addSetting("dohRollout.state", {
+  prefNames: [TRR_URI_PREF, TRR_DISABLE_ECS_PREF],
+
+  setCallback() {
+    let prefs = {};
+    prefs[TRR_URI_PREF] = "https://mozilla.cloudflare-dns.com/dns-query";
+    prefs[TRR_DISABLE_ECS_PREF] = true;
+    return prefs;
+  },
+});
+
+var preferences = class preferences extends ExtensionAPI {
+  getAPI(context) {
+    const EventManager = ExtensionCommon.EventManager;
+    return {
+      experiments: {
+        preferences: {
+          async getIntPref(name, defaultValue) {
+            return Services.prefs.getIntPref(name, defaultValue);
+          },
+          async setIntPref(name, defaultValue) {
+            return Services.prefs.setIntPref(name, defaultValue);
+          },
+          async getBoolPref(name, defaultValue) {
+            return Services.prefs.getBoolPref(name, defaultValue);
+          },
+          async setBoolPref(name, defaultValue) {
+            return Services.prefs.setBoolPref(name, defaultValue);
+          },
+          async getCharPref(name, defaultValue) {
+            return Services.prefs.getCharPref(name, defaultValue);
+          },
+          async setCharPref(name, defaultValue) {
+            return Services.prefs.setCharPref(name, defaultValue);
+          },
+          async clearUserPref(name) {
+            return Services.prefs.clearUserPref(name);
+          },
+          async prefHasUserValue(name) {
+            return Services.prefs.prefHasUserValue(name);
+          },
+
+          onPrefChanged: new EventManager({
+            context,
+            name: "preferences.onPrefChanged",
+            register: fire => {
+              let observer = () => {
+                fire.async();
+              };
+              Services.prefs.addObserver("doh-rollout.enabled", observer);
+              Services.prefs.addObserver("doh-rollout.debug", observer);
+              return () => {
+                Services.prefs.removeObserver("doh-rollout.enabled", observer);
+                Services.prefs.removeObserver("doh-rollout.debug", observer);
+              };
+            },
+          }).api(),
+
+          state: Object.assign(
+            ExtensionPreferencesManager.getSettingsAPI(
+              context.extension.id,
+              "dohRollout.state",
+              () => {
+                throw new Error("Not supported");
+              },
+              undefined,
+              false,
+              () => {}
+            ),
+            {
+              set: details => {
+                return ExtensionPreferencesManager.setSetting(
+                  context.extension.id,
+                  "dohRollout.state",
+                  details.value
+                );
+              },
+            }
+          ),
+        },
+      },
+    };
+  }
+};
