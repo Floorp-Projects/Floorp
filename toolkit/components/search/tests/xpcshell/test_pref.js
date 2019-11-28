b0b4fa5fee198c940b1450ae7d571c0701ecf43d
Coroiu Cristina <ccoroiu@mozilla.com>
2019-11-28 17:26:07 +0200

    Backed out 3 changesets (bug 1598571) for browser-chrome failures at browser/components/search/test/browser/browser_amazon.js on a CLOSED TREE
    Backed out changeset 6426d4607db0 (bug 1598571)
    Backed out changeset ff2c15cb11b4 (bug 1598571)
    Backed out changeset 290a5cd61260 (bug 1598571)

--- toolkit/components/search/tests/xpcshell/test_pref.js
+++ toolkit/components/search/tests/xpcshell/test_pref.js
@@ -6,12 +6,7 @@
 
 "use strict";
 
-const defaultBranch = Services.prefs.getDefaultBranch(
-  SearchUtils.BROWSER_SEARCH_PREF
-);
-const baseURL = "https://www.google.com/search?q=foo";
-
-add_task(async function setup() {
+function run_test() {
   // The test engines used in this test need to be recognized as 'default'
   // engines, or their MozParams will be ignored.
   let url = "resource://test/data/";
@@ -19,9 +14,14 @@ add_task(async function setup() {
     .getProtocolHandler("resource")
     .QueryInterface(Ci.nsIResProtocolHandler);
   resProt.setSubstitution("search-extensions", Services.io.newURI(url));
-});
 
-add_task(async function test_pref_initial_value() {
+  run_next_test();
+}
+
+add_task(async function test_pref() {
+  let defaultBranch = Services.prefs.getDefaultBranch(
+    SearchUtils.BROWSER_SEARCH_PREF
+  );
   defaultBranch.setCharPref("param.code", "good&id=unique");
   Services.prefs.setCharPref(
     SearchUtils.BROWSER_SEARCH_PREF + "param.code",
@@ -31,44 +31,12 @@ add_task(async function test_pref_initial_value() {
   await AddonTestUtils.promiseStartupManager();
   await Services.search.init();
 
-  const engine = Services.search.getEngineByName("engine-pref");
-  const base = baseURL + "&code=";
-  Assert.equal(
-    engine.getSubmission("foo").uri.spec,
-    base + "good%26id%3Dunique",
-    "Should have got the submission URL with the correct code"
-  );
-
-  // Now clear the user-set preference. Having a user set preference means
-  // we don't get updates from the pref service of changes on the default
-  // branch. Normally, this won't be an issue, since we don't expect users
-  // to be playing with these prefs, and worst-case, they'll just get the
-  // actual change on restart.
-  Services.prefs.clearUserPref(SearchUtils.BROWSER_SEARCH_PREF + "param.code");
-});
-
-add_task(async function test_pref_updated() {
-  // Update the pref without re-init nor restart.
-  defaultBranch.setCharPref("param.code", "supergood&id=unique123456");
-
-  const engine = Services.search.getEngineByName("engine-pref");
-  const base = baseURL + "&code=";
-  Assert.equal(
-    engine.getSubmission("foo").uri.spec,
-    base + "supergood%26id%3Dunique123456",
-    "Should have got the submission URL with the updated code"
-  );
-});
-
-add_task(async function test_pref_cleared() {
-  // Update the pref without re-init nor restart.
-  // Note you can't delete a preference from the default branch.
-  defaultBranch.setCharPref("param.code", "");
-
   let engine = Services.search.getEngineByName("engine-pref");
+  let base = "https://www.google.com/search?q=foo&code=";
   Assert.equal(
     engine.getSubmission("foo").uri.spec,
-    baseURL,
-    "Should have just the base URL after the pref was cleared"
+    base + "good%26id%3Dunique"
   );
+
+  do_test_finished();
 });
