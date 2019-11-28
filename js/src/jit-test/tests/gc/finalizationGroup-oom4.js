2b32c3283058ccd9fadecf1afc0f61b10f0e99d6
André Bargull <andre.bargull@gmail.com>
2019-11-28 16:56:36 +0000

    Bug 1600019 - Part 1: Handle OOM in registerWithFinalizationGroup. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D55086

--- js/src/jit-test/tests/gc/finalizationGroup-oom4.js
+++ js/src/jit-test/tests/gc/finalizationGroup-oom4.js
@@ -1,5 +1,5 @@
 // |jit-test| --enable-weak-refs; skip-if: !('oomTest' in this)
 let group = new FinalizationGroup(x => 0);
-group.register({}, 1, {});
+let target = {};
 let token = {};
-oomTest(() => group.unregister(token));
+oomTest(() => group.register(target, 1, token));
