7cf0c27e14c5b09f348f3c0ff822dd81a002f486
Nihanth Subramanya <nhnt11@gmail.com>
2019-11-28 16:21:52 +0000

    Bug 1598223 - Import doh-rollout src from github into browser/extensions. r=mixedpuppy
    Based on upstream https://github.com/mozilla/doh-rollout/commit/0bb6e992f70fa90c7c70b0529038a34d099a286d
    
    Differential Revision: https://phabricator.services.mozilla.com/D54617

---
+++ browser/extensions/doh-rollout/experiments/heuristics/api.js
@@ -0,0 +1,166 @@
+"use strict";
+/* exported heuristics */
+/* global Cc, Ci, Components, ExtensionAPI, Services  */
+let Cu3 = Components.utils;
+Cu3.import("resource://gre/modules/Services.jsm");
+
+function log() {
+  // eslint-disable-next-line no-constant-condition
+  if (false) {
+    // eslint-disable-next-line no-console
+    console.log(...arguments);
+  }
+}
+
+let pcs = Cc["@mozilla.org/parental-controls-service;1"].getService(
+  Ci.nsIParentalControlsService
+);
+
+const TELEMETRY_CATEGORY = "doh";
+
+const TELEMETRY_EVENTS = {
+  evaluate: {
+    methods: ["evaluate"],
+    objects: ["heuristics"],
+    extra_keys: [
+      "google",
+      "youtube",
+      "zscalerCanary",
+      "canary",
+      "modifiedRoots",
+      "browserParent",
+      "thirdPartyRoots",
+      "policy",
+      "evaluateReason",
+    ],
+    record_on_release: true,
+  },
+  state: {
+    methods: ["state"],
+    objects: [
+      "loaded",
+      "enabled",
+      "disabled",
+      "manuallyDisabled",
+      "uninstalled",
+      "UIOk",
+      "UIDisabled",
+    ],
+    extra_keys: [],
+    record_on_release: true,
+  },
+};
+
+const heuristicsManager = {
+  setupTelemetry() {
+    // Set up the Telemetry for the heuristics  and addon state
+    Services.telemetry.registerEvents(TELEMETRY_CATEGORY, TELEMETRY_EVENTS);
+  },
+
+  sendHeuristicsPing(decision, results) {
+    log("Sending a heuristics ping", decision, results);
+    Services.telemetry.recordEvent(
+      TELEMETRY_CATEGORY,
+      "evaluate",
+      "heuristics",
+      decision,
+      results
+    );
+  },
+
+  sendStatePing(state) {
+    log("Sending an addon state ping", state);
+    Services.telemetry.recordEvent(TELEMETRY_CATEGORY, "state", state, "null");
+  },
+
+  async checkEnterprisePolicies() {
+    if (Services.policies.status === Services.policies.ACTIVE) {
+      let policies = Services.policies.getActivePolicies();
+      if (!("DNSOverHTTPS" in policies)) {
+        // If DoH isn't in the policy, return that there is a policy (but no DoH specifics)
+        return "policy_without_doh";
+      }
+      let dohPolicy = policies.DNSOverHTTPS;
+      if (dohPolicy.Enabled === true) {
+        // If DoH is enabled in the policy, enable it
+        return "enable_doh";
+      }
+      // If DoH is disabled in the policy, disable it
+      return "disable_doh";
+    }
+
+    // Default return, meaning no policy related to DNSOverHTTPS
+    return "no_policy_set";
+  },
+
+  async checkParentalControls() {
+    let enabled = pcs.parentalControlsEnabled;
+    if (enabled) {
+      return "disable_doh";
+    }
+    return "enable_doh";
+  },
+
+  async checkThirdPartyRoots() {
+    let certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(
+      Ci.nsIX509CertDB
+    );
+    let allCerts = certdb.getCerts();
+    // Pre 71 code (Bug 1577836)
+    if (allCerts.getEnumerator) {
+      allCerts = allCerts.getEnumerator();
+    }
+    for (let cert of allCerts) {
+      if (
+        certdb.isCertTrusted(
+          cert,
+          Ci.nsIX509Cert.CA_CERT,
+          Ci.nsIX509CertDB.TRUSTED_SSL
+        )
+      ) {
+        if (!cert.isBuiltInRoot) {
+          // this cert is a trust anchor that wasn't shipped with the browser
+          return "disable_doh";
+        }
+      }
+    }
+    return "enable_doh";
+  },
+};
+
+var heuristics = class heuristics extends ExtensionAPI {
+  getAPI() {
+    return {
+      experiments: {
+        heuristics: {
+          setupTelemetry() {
+            heuristicsManager.setupTelemetry();
+          },
+
+          sendHeuristicsPing(decision, results) {
+            heuristicsManager.sendHeuristicsPing(decision, results);
+          },
+
+          sendStatePing(state) {
+            heuristicsManager.sendStatePing(state);
+          },
+
+          async checkEnterprisePolicies() {
+            let result = await heuristicsManager.checkEnterprisePolicies();
+            return result;
+          },
+
+          async checkParentalControls() {
+            let result = await heuristicsManager.checkParentalControls();
+            return result;
+          },
+
+          async checkThirdPartyRoots() {
+            let result = await heuristicsManager.checkThirdPartyRoots();
+            return result;
+          },
+        },
+      },
+    };
+  }
+};
---
+++ browser/extensions/doh-rollout/heuristics.js
@@ -0,0 +1,147 @@
+"use strict";
+/* global browser */
+/* exported runHeuristics */
+
+const GLOBAL_CANARY = "use-application-dns.net";
+
+// TODO: Confirm that this error message corresponds to NXDOMAIN
+const NXDOMAIN_ERR = "NS_ERROR_UNKNOWN_HOST";
+
+async function dnsLookup(hostname) {
+  let flags = ["disable_trr", "disable_ipv6", "bypass_cache"];
+  let addresses, err;
+
+  try {
+    let response = await browser.dns.resolve(hostname, flags);
+    addresses = response.addresses;
+  } catch (e) {
+    addresses = [null];
+    err = e.message;
+  }
+  return { addresses, err };
+}
+
+async function dnsListLookup(domainList) {
+  let results = [];
+  for (let i = 0; i < domainList.length; i++) {
+    let domain = domainList[i];
+    let { addresses } = await dnsLookup(domain);
+    results = results.concat(addresses);
+  }
+  return results;
+}
+
+async function safeSearch() {
+  const providerList = [
+    {
+      name: "google",
+      unfiltered: ["www.google.com", "google.com"],
+      safeSearch: ["forcesafesearch.google.com"],
+    },
+    {
+      name: "youtube",
+      unfiltered: [
+        "www.youtube.com",
+        "m.youtube.com",
+        "youtubei.googleapis.com",
+        "youtube.googleapis.com",
+        "www.youtube-nocookie.com",
+      ],
+      safeSearch: ["restrict.youtube.com", "restrictmoderate.youtube.com"],
+    },
+  ];
+
+  // Compare strict domain lookups to non-strict domain lookups
+  let safeSearchChecks = {};
+  for (let i = 0; i < providerList.length; i++) {
+    let providerObj = providerList[i];
+    let providerName = providerObj.name;
+    safeSearchChecks[providerName] = "enable_doh";
+
+    let results = {};
+    results.unfilteredAnswers = await dnsListLookup(providerObj.unfiltered);
+    results.safeSearchAnswers = await dnsListLookup(providerObj.safeSearch);
+
+    // Given a provider, check if the answer for any safe search domain
+    // matches the answer for any default domain
+    for (let j = 0; j < results.safeSearchAnswers.length; j++) {
+      let answer = results.safeSearchAnswers[j];
+      if (answer === null) {
+        continue;
+      }
+
+      let safeSearchEnabled = results.unfilteredAnswers.includes(answer);
+      if (safeSearchEnabled) {
+        safeSearchChecks[providerName] = "disable_doh";
+      }
+    }
+  }
+  return safeSearchChecks;
+}
+
+async function zscalerCanary() {
+  const ZSCALER_CANARY = "sitereview.zscaler.com";
+  let { addresses } = await dnsLookup(ZSCALER_CANARY);
+  for (let j = 0; j < addresses.length; j++) {
+    let answer = addresses[j];
+    if (
+      answer == "213.152.228.242" ||
+      answer == "199.168.151.251" ||
+      answer == "8.25.203.30"
+    ) {
+      // if sitereview.zscaler.com resolves to either one of the 3 IPs above,
+      // Zscaler Shift service is in use, don't enable DoH
+      return "disable_doh";
+    }
+  }
+  return "enable_doh";
+}
+
+// TODO: Confirm the expected behavior when filtering is on
+async function globalCanary() {
+  let { addresses, err } = await dnsLookup(GLOBAL_CANARY);
+  if (err === NXDOMAIN_ERR) {
+    return "disable_doh";
+  }
+  if (addresses.length === 0) {
+    return "disable_doh";
+  }
+  return "enable_doh";
+}
+
+async function modifiedRoots() {
+  // Check for presence of enterprise_roots cert pref. If enabled, disable DoH
+  let rootsEnabled = await browser.experiments.preferences.getBoolPref(
+    "security.enterprise_roots.enabled",
+    false
+  );
+  if (rootsEnabled) {
+    return "disable_doh";
+  }
+  return "enable_doh";
+}
+
+async function runHeuristics() {
+  let safeSearchChecks = await safeSearch();
+  let zscalerCheck = await zscalerCanary();
+  let canaryCheck = await globalCanary();
+  let modifiedRootsCheck = await modifiedRoots();
+
+  // Check other heuristics through privileged code
+  let browserParentCheck = await browser.experiments.heuristics.checkParentalControls();
+  let enterpriseCheck = await browser.experiments.heuristics.checkEnterprisePolicies();
+  let thirdPartyRootsCheck = await browser.experiments.heuristics.checkThirdPartyRoots();
+
+  // Return result of each heuristic
+  let heuristics = {
+    google: safeSearchChecks.google,
+    youtube: safeSearchChecks.youtube,
+    zscalerCanary: zscalerCheck,
+    canary: canaryCheck,
+    modifiedRoots: modifiedRootsCheck,
+    browserParent: browserParentCheck,
+    thirdPartyRoots: thirdPartyRootsCheck,
+    policy: enterpriseCheck,
+  };
+  return heuristics;
+}
