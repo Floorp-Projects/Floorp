9ebdbcec7dd0f70a1c7259a01daf101c271aea05
Brendan Dahl <bdahl@mozilla.com>
2019-11-28 21:46:33 +0000

    Bug 1596709 - Move special chrome root element handling to an observer. r=smaug
    Enables any type of document to use the special root element attributes
    currently used by XUL <window>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D54016

--- dom/xul/nsXULElement.h
+++ dom/xul/nsXULElement.h
@@ -512,6 +512,8 @@ class nsXULElement : public nsStyledElement {
 
   bool IsInteractiveHTMLContent(bool aIgnoreTabindex) const override;
 
+  void MaybeUpdatePrivateLifetime();
+
  protected:
   ~nsXULElement();
 
@@ -553,17 +555,6 @@ class nsXULElement : public nsStyledElement {
   void AddListenerForAttributeIfNeeded(const nsAttrName& aName);
   void AddListenerForAttributeIfNeeded(nsAtom* aLocalName);
 
-  nsIWidget* GetWindowWidget();
-
-  // attribute setters for widget
-  nsresult HideWindowChrome(bool aShouldHide);
-  void SetChromeMargins(const nsAttrValue* aValue);
-  void ResetChromeMargins();
-
-  void SetDrawsInTitlebar(bool aState);
-  void SetDrawsTitle(bool aState);
-  void UpdateBrightTitlebarForeground(Document* aDocument);
-
  protected:
   void AddTooltipSupport();
   void RemoveTooltipSupport();
@@ -594,8 +585,6 @@ class nsXULElement : public nsStyledElement {
 
   virtual JSObject* WrapNode(JSContext* aCx,
                              JS::Handle<JSObject*> aGivenProto) override;
-
-  void MaybeUpdatePrivateLifetime();
 
   bool IsEventStoppedFromAnonymousScrollbar(mozilla::EventMessage aMessage);
 
