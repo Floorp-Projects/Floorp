24de23a2bed560554a622ca85fc118c7e63977ca
Belén Albeza <balbeza@mozilla.com>
2019-11-29 14:39:49 +0000

    Bug 1598629 - Part 2: Re-enable disable start button mochitest r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D54964

--- devtools/client/application/test/browser/browser_application_panel_disable-start-service-worker.js
+++ devtools/client/application/test/browser/browser_application_panel_disable-start-service-worker.js
@@ -11,9 +11,19 @@ const TAB_URL = URL_ROOT + "resources/service-workers/simple.html";
 add_task(async function() {
   await enableApplicationPanel();
 
-  // disable sw debugging by increasing the # of processes and thus multi-e10s kicking in
+  // Setting a low idle_timeout and idle_extended_timeout will allow the service worker
+  // to reach the STOPPED state quickly, which will allow us to test the start button.
+  // The default value is 30000 milliseconds.
+  info("Set a low service worker idle timeout");
+  await pushPref("dom.serviceWorkers.idle_timeout", 1000);
+  await pushPref("dom.serviceWorkers.idle_extended_timeout", 1000);
+
+  // disable sw debugging
   info("Disable service worker debugging");
-  await pushPref("dom.ipc.processCount", 8);
+  await pushPref(
+    "devtools.debugger.features.windowless-service-workers",
+    false
+  );
 
   const { panel, tab, target } = await openNewTabAndApplicationPanel(TAB_URL);
   const doc = panel.panelWin.document;
