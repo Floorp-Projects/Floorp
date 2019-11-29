c758c3bd80ecd15d2600b913bdb403b13b65d8d9
Simon Giesecke <sgiesecke@mozilla.com>
2019-11-29 12:23:42 +0000

    Bug 1596129 - Make use of std::find_if in FileManagerInfo::InvalidateAndRemoveFileManager. r=dom-workers-and-storage-reviewers,ttung,janv
    Differential Revision: https://phabricator.services.mozilla.com/D52874
    
    --HG--
    extra : moz-landing-system : lando

--- dom/indexedDB/IndexedDatabaseManager.cpp
+++ dom/indexedDB/IndexedDatabaseManager.cpp
@@ -916,15 +916,16 @@ void FileManagerInfo::InvalidateAndRemoveFileManager(
     PersistenceType aPersistenceType, const nsAString& aName) {
   AssertIsOnIOThread();
 
-  nsTArray<RefPtr<FileManager> >& managers = GetArray(aPersistenceType);
+  auto& managers = GetArray(aPersistenceType);
+  const auto end = managers.cend();
+  const auto foundIt =
+      std::find_if(managers.cbegin(), end, [&aName](const auto& fileManager) {
+        return fileManager->DatabaseName() == aName;
+      });
 
-  for (uint32_t i = 0; i < managers.Length(); i++) {
-    RefPtr<FileManager>& fileManager = managers[i];
-    if (fileManager->DatabaseName() == aName) {
-      fileManager->Invalidate();
-      managers.RemoveElementAt(i);
-      return;
-    }
+  if (foundIt != end) {
+    (*foundIt)->Invalidate();
+    managers.RemoveElementAt(foundIt.GetIndex());
   }
 }
 
