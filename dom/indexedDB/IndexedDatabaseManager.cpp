116e7587e35444e89708a05f5fe2c20e4f103a02
Simon Giesecke <sgiesecke@mozilla.com>
2019-11-29 09:22:03 +0000

    Bug 1596129 - Make use of std::find_if in FileManagerInfo::GetFileManager. r=dom-workers-and-storage-reviewers,ttung
    Depends on D52872
    
    Differential Revision: https://phabricator.services.mozilla.com/D52873

--- dom/indexedDB/IndexedDatabaseManager.cpp
+++ dom/indexedDB/IndexedDatabaseManager.cpp
@@ -860,19 +860,15 @@ already_AddRefed<FileManager> FileManagerInfo::GetFileManager(
     PersistenceType aPersistenceType, const nsAString& aName) const {
   AssertIsOnIOThread();
 
-  const nsTArray<RefPtr<FileManager> >& managers =
-      GetImmutableArray(aPersistenceType);
+  const auto& managers = GetImmutableArray(aPersistenceType);
 
-  for (uint32_t i = 0; i < managers.Length(); i++) {
-    const RefPtr<FileManager>& fileManager = managers[i];
+  const auto end = managers.cend();
+  const auto foundIt =
+      std::find_if(managers.cbegin(), end, [&aName](const auto& fileManager) {
+        return fileManager->DatabaseName() == aName;
+      });
 
-    if (fileManager->DatabaseName() == aName) {
-      RefPtr<FileManager> result = fileManager;
-      return result.forget();
-    }
-  }
-
-  return nullptr;
+  return foundIt != end ? RefPtr<FileManager>{*foundIt}.forget() : nullptr;
 }
 
 void FileManagerInfo::AddFileManager(FileManager* aFileManager) {
