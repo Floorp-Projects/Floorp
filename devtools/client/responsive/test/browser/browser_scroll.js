d653f8051c63b5b1770230d08b62cd74b687fc05
Brad Werth <bwerth@mozilla.com>
2019-11-29 13:51:48 +0000

    Bug 1521934 Part 2: Update a test to specify a meta viewport size to ensure scrolling is still possible. r=pbro
    Depends on D53005
    
    Differential Revision: https://phabricator.services.mozilla.com/D54630

--- devtools/client/responsive/test/browser/browser_scroll.js
+++ devtools/client/responsive/test/browser/browser_scroll.js
@@ -8,35 +8,48 @@ http://creativecommons.org/publicdomain/zero/1.0/ */
  * behaves correctly, both with and without touch simulation enabled.
  */
 
+const Types = require("devtools/client/responsive/types");
+
 const TEST_URL =
   "data:text/html;charset=utf-8," +
+  '<head><meta name="viewport" content="width=100, height=100"/></head>' +
   '<div style="background:blue; width:200px; height:200px"></div>';
 
 addRDMTask(TEST_URL, async function({ ui, manager }) {
-  await setViewportSize(ui, manager, 100, 100);
+  await SpecialPowers.pushPrefEnv({
+    set: [["devtools.responsive.metaViewport.enabled", true]],
+  });
+
+  // Wait until the viewport has been added and the device list has been loaded
+  const { store } = ui.toolWindow;
+  await waitUntilState(
+    store,
+    state =>
+      state.viewports.length == 1 &&
+      state.devices.listState == Types.loadableState.LOADED
+  );
+
+  await setViewportSize(ui, manager, 50, 50);
   const browser = ui.getViewportBrowser();
 
-  info("Setting focus on the browser.");
-  browser.focus();
-
-  info("Testing scroll behavior with touch simulation OFF.");
-  await testScrollingOfContent(ui);
-
-  // Run the tests again with touch simulation on.
-  const reloadNeeded = await ui.updateTouchSimulation(true);
-  if (reloadNeeded) {
-    info("Reload is needed -- waiting for it.");
-    const reload = waitForViewportLoad(ui);
-    browser.reload();
-    await reload;
+  for (const mv in [true, false]) {
+    const reloadNeeded = await ui.updateTouchSimulation(mv);
+    if (reloadNeeded) {
+      info("Reload is needed -- waiting for it.");
+      const reload = waitForViewportLoad(ui);
+      browser.reload();
+      await reload;
+    }
+    info("Setting focus on the browser.");
+    browser.focus();
 
     await ContentTask.spawn(browser, null, () => {
       content.scrollTo(0, 0);
     });
-  }
 
-  info("Testing scroll behavior with touch simulation ON.");
-  await testScrollingOfContent(ui);
+    info("Testing scroll behavior with touch simulation " + mv + ".");
+    await testScrollingOfContent(ui);
+  }
 });
 
 async function testScrollingOfContent(ui) {
@@ -60,11 +73,13 @@ async function testScrollingOfContent(ui) {
    * scrolling, then no event will be fired and the test will time out.
    */
   scroll = waitForViewportScroll(ui);
+  info("Synthesizing an arrow key down.");
   EventUtils.synthesizeKey("KEY_ArrowDown");
   await scroll;
   info("Scroll event was fired after arrow key down.");
 
   scroll = waitForViewportScroll(ui);
+  info("Synthesizing an arrow key right.");
   EventUtils.synthesizeKey("KEY_ArrowRight");
   await scroll;
   info("Scroll event was fired after arrow key right.");
