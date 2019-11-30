28618c9230857737a0bf51251dc6623fd37e873b
Jeff Muizelaar <jrmuizel@gmail.com>
2019-11-30 00:28:35 +0000

    Bug 1595388. Use mDrawTarget->Rect() instead of Size(). r=mstange
    Using Rect() will work properly when mDrawTarget does not have 0,0
    origin. It also makes the code's intention more clear.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55337
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/2d/DrawTargetOffset.cpp
+++ gfx/2d/DrawTargetOffset.cpp
@@ -79,8 +79,7 @@ void DrawTargetOffset::DrawFilter(FilterNode* aNode, const Rect& aSourceRect,
     // Try to reduce the source rect so that it's not much bigger
     // than the draw target. The result is not minimal. Examples
     // are left as an exercise for the reader.
-    auto destRect = Rect(mOrigin.x, mOrigin.y, mDrawTarget->GetSize().width,
-                         mDrawTarget->GetSize().height);
+    auto destRect = Rect(mDrawTarget->GetRect() + mOrigin);
     Rect userSpaceBounds = clone.TransformBounds(destRect);
     userSpaceSource = userSpaceSource.Intersect(userSpaceBounds);
   }
