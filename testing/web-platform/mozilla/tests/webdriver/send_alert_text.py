618d10f5875fe23951d5663bb9928182a9ff2f9c
Andreas Tolfsen <ato@sny.no>
2019-12-01 14:35:14 +0000

    bug 1600078: webdriver: make test_alert_unsupported_operation Gecko specific; r=webdriver-reviewers,jgraham
    Basic auth dialogues are not included in HTML's definition of
    'user prompts': those are limited to the 'simple dialogues' such
    as window.alert(), window.prompt() et al. and the print dialogue.
    
    Since we don't want to regress this behaviour in geckodriver, this
    patch removes test_alert_unsupported_operation from WPT and makes
    it Mozilla-specific.
    
    The patch doesn't remove the basic auth framework code from WPT
    because it's assumed we will need that in WPT in the very near future.
    
    Thanks-to: Carlos Garcia Campos <cgarcia@igalia.com>
    
    Differential Revision: https://phabricator.services.mozilla.com/D55134

---
+++ testing/web-platform/mozilla/tests/webdriver/send_alert_text.py
@@ -0,0 +1,20 @@
+from tests.support.asserts import assert_error
+from tests.support.authentication import basic_authentication
+
+
+def send_alert_text(session, text=None):
+    return session.transport.send(
+        "POST", "session/{session_id}/alert/text".format(**vars(session)),
+        {"text": text})
+
+
+def test_basic_auth_unsupported_operation(session):
+    """
+    Basic auth dialogues are not included in HTML's definition of
+    'user prompts': those are limited to the 'simple dialogues'
+    such as window.alert(), window.prompt() et al. and the print
+    dialogue.
+    """
+    session.url = basic_authentication()
+    response = send_alert_text(session, "Federer")
+    assert_error(response, "unsupported operation")
