7c743c71787964877a447fc42e71589ea3f2bbd3
Noemi Erli <nerli@mozilla.com>
2019-12-02 20:13:45 +0200

    Backed out changeset f7fb3affa4fb (bug 1598377) for causing failures in wasm-testharness.js CLOSED TREE

--- js/src/wasm/WasmValidate.h
+++ js/src/wasm/WasmValidate.h
@@ -224,9 +224,6 @@ struct ModuleEnvironment {
   bool hugeMemoryEnabled() const {
     return !isAsmJS() && compilerEnv->hugeMemory();
   }
-  uint32_t funcMaxResults() const {
-    return multiValuesEnabled() ? MaxResults : 1;
-  }
   bool funcIsImport(uint32_t funcIndex) const {
     return funcIndex < funcImportGlobalDataOffsets.length();
   }
