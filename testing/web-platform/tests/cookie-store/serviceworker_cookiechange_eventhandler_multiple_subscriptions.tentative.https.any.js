b6fabffafae7c84451d68a3009274bf6dc9bc2e5
Staphany Park <staphany@chromium.org>
2019-12-02 13:38:35 +0000

    Bug 1599674 [wpt PR 20468] - Cookie Store: Refactor tests to also work in window context., a=testonly
    Automatic update from web-platform-tests
    Cookie Store: Refactor tests to also work in window context.
    
    This refactor condenses service worker test files by converting the
    service worker scripts into .any.js WPT files that test both window and
    worker contexts.
    
    Change-Id: Iee64f5cc68b939ab02b06910444b8ebffdf359a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851232
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720381}
    
    --
    
    wpt-commits: ff11f93316527e37ec580adca01a9cf0a58a297e
    wpt-pr: 20468

--- testing/web-platform/tests/cookie-store/serviceworker_cookiechange_eventhandler_multiple_subscriptions.tentative.https.any.js
+++ testing/web-platform/tests/cookie-store/serviceworker_cookiechange_eventhandler_multiple_subscriptions.tentative.https.any.js
@@ -1,56 +1,14 @@
-self.GLOBAL = {
-  isWindow: function() { return false; },
-  isWorker: function() { return true; },
-};
-importScripts("/resources/testharness.js");
+// META: title=Cookie Store API: cookiechange event in ServiceWorker with multiple subscriptions
+// META: global=!default,serviceworker
+
+'use strict';
+
+const kScope = '/cookie-store/does/not/exist';
 
 // Resolves when the service worker receives the 'activate' event.
-const kServiceWorkerActivatedPromise = new Promise(resolve => {
+const kServiceWorkerActivatedPromise = new Promise((resolve) => {
   self.addEventListener('activate', event => { resolve(); });
 });
-
-// sort() comparator that uses the < operator.
-//
-// This is intended to be used for sorting strings. Using < is preferred to
-// localeCompare() because the latter has some implementation-dependent
-// behavior.
-function CompareStrings(a, b) {
-  return a < b ? -1 : (b < a ? 1 : 0);
-}
-
-promise_test(async testCase => {
-  await kServiceWorkerActivatedPromise;
-
-  {
-    const subscriptions = [
-      { name: 'cookie-name1', matchType: 'equals', url: '/cookie-store/path1' },
-    ];
-    await registration.cookies.subscribe(subscriptions);
-    testCase.add_cleanup(() => registration.cookies.unsubscribe(subscriptions));
-  }
-  {
-    const subscriptions = [
-      { },  // Test the default values for subscription properties.
-      { name: 'cookie-prefix', matchType: 'starts-with' },
-    ];
-    await registration.cookies.subscribe(subscriptions);
-    testCase.add_cleanup(() => registration.cookies.unsubscribe(subscriptions));
-  }
-
-  const subscriptions = await registration.cookies.getSubscriptions();
-  assert_equals(subscriptions.length, 3);
-
-  subscriptions.sort((a, b) => CompareStrings(`${a.name}`, `${b.name}`));
-
-  assert_equals(subscriptions[0].name, 'cookie-name1');
-  assert_equals('equals', subscriptions[0].matchType);
-
-  assert_equals(subscriptions[1].name, 'cookie-prefix');
-  assert_equals('starts-with', subscriptions[1].matchType);
-
-  assert_false('name' in subscriptions[2]);
-  assert_equals('starts-with', subscriptions[2].matchType);
-}, 'getSubscriptions returns subscriptions passed to subscribe');
 
 // Accumulates cookiechange events dispatched to the service worker.
 let g_cookie_changes = [];
@@ -61,8 +19,9 @@ let g_cookie_change_received_promise = null;
 let g_cookie_change_received_promise_resolver = null;
 self.addEventListener('cookiechange', (event) => {
   g_cookie_changes.push(event);
-  if (g_cookie_change_received_promise_resolver)
+  if (g_cookie_change_received_promise_resolver) {
     g_cookie_change_received_promise_resolver();
+  }
 });
 function RearmCookieChangeReceivedPromise() {
   g_cookie_change_received_promise = new Promise((resolve) => {
@@ -76,7 +35,7 @@ promise_test(async testCase => {
 
   {
     const subscriptions = [
-      { name: 'cookie-name1', matchType: 'equals', url: '/cookie-store/path1' },
+      { name: 'cookie-name1', matchType: 'equals', url: `${kScope}/path1` },
     ];
     await registration.cookies.subscribe(subscriptions);
     testCase.add_cleanup(() => registration.cookies.unsubscribe(subscriptions));
@@ -100,7 +59,7 @@ promise_test(async testCase => {
   testCase.add_cleanup(() => RearmCookieChangeReceivedPromise());
 
   assert_equals(g_cookie_changes.length, 1);
-  const event = g_cookie_changes[0]
+  const event = g_cookie_changes[0];
   assert_equals(event.type, 'cookiechange');
   assert_equals(event.changed.length, 1);
   assert_equals(event.changed[0].name, 'cookie-name');
@@ -109,5 +68,3 @@ promise_test(async testCase => {
   assert_true(event instanceof ExtendableCookieChangeEvent);
   assert_true(event instanceof ExtendableEvent);
 }, 'cookiechange dispatched with cookie change that matches subscription');
-
-done();
--- testing/web-platform/tests/cookie-store/serviceworker_cookiechange_eventhandler_single_subscription.tentative.https.any.js
+++ testing/web-platform/tests/cookie-store/serviceworker_cookiechange_eventhandler_single_subscription.tentative.https.any.js
@@ -1,33 +1,29 @@
-self.GLOBAL = {
-  isWindow: function() { return false; },
-  isWorker: function() { return true; },
-};
-importScripts("/resources/testharness.js");
+// META: title=Cookie Store API: cookiechange event in ServiceWorker with single subscription
+// META: global=!default,serviceworker
+
+'use strict';
+
+const kScope = '/cookie-store/does/not/exist';
 
 // Resolves when the service worker receives the 'activate' event.
 const kServiceWorkerActivatedPromise = new Promise((resolve) => {
   self.addEventListener('activate', event => { resolve(); });
 });
 
-const kCookieChangeReceivedPromise = new Promise((resolve) => {
-  self.addEventListener('cookiechange', (event) => {
-    resolve(event);
-  });
+// Resolves when a cookiechange event is received.
+const kCookieChangeReceivedPromise = new Promise(resolve => {
+  self.addEventListener('cookiechange', event => { resolve(event); });
 });
 
 promise_test(async testCase => {
   await kServiceWorkerActivatedPromise;
 
   const subscriptions = [
-    { name: 'cookie-name', matchType: 'equals',
-      url: '/cookie-store/scope/path' }];
+    { name: 'cookie-name', matchType: 'equals', url: `${kScope}/path` },
+  ];
   await registration.cookies.subscribe(subscriptions);
   testCase.add_cleanup(() => registration.cookies.unsubscribe(subscriptions));
 
-  await cookieStore.set('another-cookie-name', 'cookie-value');
-  testCase.add_cleanup(async () => {
-    await cookieStore.delete('another-cookie-name');
-  });
   await cookieStore.set('cookie-name', 'cookie-value');
   testCase.add_cleanup(async () => {
     await cookieStore.delete('cookie-name');
@@ -38,6 +34,8 @@ promise_test(async testCase => {
   assert_equals(event.changed.length, 1);
   assert_equals(event.changed[0].name, 'cookie-name');
   assert_equals(event.changed[0].value, 'cookie-value');
-}, 'cookiechange not dispatched for change that does not match subscription');
-
-done();
+  assert_equals(event.deleted.length, 0);
+  assert_true(event instanceof ExtendableCookieChangeEvent);
+  assert_true(event instanceof ExtendableEvent);
+}, 'cookiechange dispatched with cookie change that matches subscription ' +
+   'to cookiechange event handler registered with addEventListener');
