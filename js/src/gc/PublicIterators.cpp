a628912161396d65e8fc3ba5b58ec434aeec0653
Ted Campbell <tcampbell@mozilla.com>
2019-12-02 20:18:04 +0000

    Bug 1600439 - Replace LazyScriptData with PrivateScriptData. r=jandem
    It it straight-foward for users of LazyScriptData to support a single array
    for closedOverBindings and innerFunctions. As a result, we can use
    PrivateScriptData as the implementation and eliminate the LazyScriptData type
    altogether.
    
    Depends on D55035
    
    Differential Revision: https://phabricator.services.mozilla.com/D55360

--- js/src/gc/PublicIterators.cpp
+++ js/src/gc/PublicIterators.cpp
@@ -86,11 +86,12 @@ static void TraverseInnerLazyScriptsForLazyScript(
     JSContext* cx, void* data, LazyScript* enclosingLazyScript,
     IterateLazyScriptCallback lazyScriptCallback,
     const JS::AutoRequireNoGC& nogc) {
-  for (JSFunction* fun : enclosingLazyScript->innerFunctions()) {
-    // LazyScript::CreateForXDR temporarily initializes innerFunctions with
-    // its own function, but it should be overwritten with correct
-    // inner functions before getting inserted into parent's innerFunctions.
-    MOZ_ASSERT(fun != enclosingLazyScript->function());
+  for (JS::GCCellPtr gcThing : enclosingLazyScript->gcthings()) {
+    if (!gcThing.is<JSObject>()) {
+      continue;
+    }
+
+    JSFunction* fun = &gcThing.as<JSObject>().as<JSFunction>();
 
     if (!fun->isInterpretedLazy()) {
       return;
