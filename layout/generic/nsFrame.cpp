537fd457fe31b8dee494ffd7a9a543e19f5ebe8e
Coroiu Cristina <ccoroiu@mozilla.com>
2019-12-02 21:29:26 +0200

    Backed out changeset 6c09d8b07585 (bug 1596050) for reftest failures at layout/reftests/high-contrast/selection-001.html on a CLOSED TREE

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -2373,18 +2373,13 @@ already_AddRefed<ComputedStyle> nsIFrame::ComputeSelectionStyle(
       aSelectionStatus != nsISelectionController::SELECTION_DISABLED) {
     return nullptr;
   }
-  // When in high-contrast mode, the style system ends up ignoring the color
-  // declarations, which means that the ::selection style becomes the inherited
-  // color, and default background. That's no good.
-  if (!PresContext()->PrefSheetPrefs().mUseDocumentColors) {
-    return nullptr;
-  }
   Element* element = FindElementAncestorForMozSelection(GetContent());
   if (!element) {
     return nullptr;
   }
-  return PresContext()->StyleSet()->ProbePseudoElementStyle(
+  RefPtr<ComputedStyle> sc = PresContext()->StyleSet()->ProbePseudoElementStyle(
       *element, PseudoStyleType::selection, Style());
+  return sc.forget();
 }
 
 /********************************************************
