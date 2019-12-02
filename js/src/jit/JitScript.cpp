7c1693b591619127a69569c90502eb671192f989
Coroiu Cristina <ccoroiu@mozilla.com>
2019-12-02 21:27:55 +0200

    Backed out 7 changesets (bug 1600439, bug 1566466, bug 1591598) for raptor crashes on a CLOSED TREE
    Backed out changeset 14f33b78f0eb (bug 1566466)
    Backed out changeset 617ce539db70 (bug 1600439)
    Backed out changeset 47f84263339e (bug 1600439)
    Backed out changeset 07471e96b2d8 (bug 1600439)
    Backed out changeset 5163670bfede (bug 1591598)
    Backed out changeset 5ea098cc9ef0 (bug 1591598)
    Backed out changeset 42ca239ee6a6 (bug 1591598)

--- js/src/jit/JitScript.cpp
+++ js/src/jit/JitScript.cpp
@@ -149,8 +149,9 @@ bool JSScript::createJitScript(JSContext* cx) {
     return false;
   }
 
+  MOZ_ASSERT(!hasJitScript());
   prepareForDestruction.release();
-  warmUpData_.initJitScript(jitScript.release());
+  warmUpData_.setJitScript(jitScript.release());
   AddCellMemory(this, allocSize.value(), MemoryUse::JitScript);
 
   // We have a JitScript so we can set the script's jitCodeRaw_ pointer to the
