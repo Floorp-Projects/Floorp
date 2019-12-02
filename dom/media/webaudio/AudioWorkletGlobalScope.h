5bcd174a4680d1da0faa8b7119dcdb65f58df7cc
Karl Tomlinson <karlt+@karlt.net>
2019-12-02 11:30:39 +0000

    Bug 1599952 pass MessagePort to AudioWorkletGlobalScope r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D53134

--- dom/media/webaudio/AudioWorkletGlobalScope.h
+++ dom/media/webaudio/AudioWorkletGlobalScope.h
@@ -19,7 +19,9 @@ class AudioWorkletImpl;
 namespace dom {
 
 class AudioWorkletProcessorConstructor;
+class MessagePort;
 class StructuredCloneHolder;
+class UniqueMessagePortId;
 
 class AudioWorkletGlobalScope final : public WorkletGlobalScope {
  public:
@@ -49,6 +51,7 @@ class AudioWorkletGlobalScope final : public WorkletGlobalScope {
   MOZ_CAN_RUN_SCRIPT
   bool ConstructProcessor(const nsAString& aName,
                           NotNull<StructuredCloneHolder*> aSerializedOptions,
+                          UniqueMessagePortId& aPortIdentifier,
                           JS::MutableHandle<JSObject*> aRetProcessor);
 
  private:
@@ -66,6 +69,10 @@ class AudioWorkletGlobalScope final : public WorkletGlobalScope {
   typedef nsRefPtrHashtable<nsStringHashKey, AudioWorkletProcessorConstructor>
       NodeNameToProcessorDefinitionMap;
   NodeNameToProcessorDefinitionMap mNameToProcessorMap;
+  // https://webaudio.github.io/web-audio-api/#pending-processor-construction-data-transferred-port
+  // This does not need to be traversed during cycle-collection because it is
+  // only set while this AudioWorkletGlobalScope is on the stack.
+  RefPtr<MessagePort> mPortForProcessor;
 };
 
 }  // namespace dom
