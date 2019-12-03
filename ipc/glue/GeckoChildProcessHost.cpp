c149e05d276d2c68da46be0cc28aa67574441398
Thinker Li <thinker.li@gmail.com>
2019-12-03 19:27:32 +0000

    Bug 1470591 - Part 5: ForkServer to create new processes. r=gsvelto
    Class ForkServer and class ForkServiceChild are implemented.  The
    chrome process can ask the fork server process to create content
    processes.  The requests are sent by MiniTransceiver over a socket.
    The fork server replys with the process IDs/handles of created
    processes.
    
    LaunchOptions::use_forkserver is a boolean.  With use_forkserver being
    true, the chrome process sends a request to the fork server instead of
    forking directly.
    
    Depends on D46881
    
    Differential Revision: https://phabricator.services.mozilla.com/D46883

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -102,6 +102,10 @@ using mozilla::ScopedPRFileDesc;
 #  include "mozilla/jni/Utils.h"
 #endif
 
+#ifdef MOZ_ENABLE_FORKSERVER
+#  include "mozilla/ipc/ForkServiceChild.h"
+#endif
+
 static bool ShouldHaveDirectoryService() {
   return GeckoProcessType_Default == XRE_GetProcessType();
 }
@@ -355,6 +359,11 @@ GeckoChildProcessHost::GeckoChildProcessHost(GeckoProcessType aProcessType,
     if (NS_SUCCEEDED(rv)) {
       contentTempDir->GetNativePath(mTmpDirName);
     }
+  }
+#endif
+#if defined(MOZ_ENABLE_FORKSERVER)
+  if (aProcessType == GeckoProcessType_Content) {
+    mLaunchOptions->use_forkserver = true;
   }
 #endif
 }
@@ -819,7 +828,10 @@ void BaseProcessLauncher::GetChildLogName(const char* origLogName,
 //
 // Android also needs a single dedicated thread to simplify thread
 // safety in java.
-#if defined(XP_WIN) || defined(MOZ_WIDGET_ANDROID)
+//
+// Fork server needs a dedicated thread for accessing
+// |ForkServiceChild|.
+#if defined(XP_WIN) || defined(MOZ_WIDGET_ANDROID) || defined(MOZ_ENABLE_FORKSERVER)
 
 static mozilla::StaticMutex gIPCLaunchThreadMutex;
 static mozilla::StaticRefPtr<nsIThread> gIPCLaunchThread;
@@ -872,7 +884,7 @@ nsCOMPtr<nsIEventTarget> GetIPCLauncher() {
   return thread;
 }
 
-#else  // defined(XP_WIN) || defined(MOZ_WIDGET_ANDROID)
+#else  // defined(XP_WIN) || defined(MOZ_WIDGET_ANDROID) || defined(MOZ_ENABLE_FORKSERVER)
 
 // Other platforms use an on-demand thread pool.
 
@@ -883,7 +895,7 @@ nsCOMPtr<nsIEventTarget> GetIPCLauncher() {
   return pool;
 }
 
-#endif  // XP_WIN
+#endif  // XP_WIN || MOZ_WIDGET_ANDROID || MOZ_ENABLE_FORKSERVER
 
 void
 #if defined(XP_WIN)
