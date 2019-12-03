b496f9b400bdc0b85e0b400937d3bd3ee7002877
Thinker Li <thinker.li@gmail.com>
2019-12-03 18:53:53 +0000

    Bug 1470591 - Part 1: Add a new process type for ForkServer. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D46878

--- ipc/glue/CrashReporterHost.cpp
+++ ipc/glue/CrashReporterHost.cpp
@@ -47,9 +47,12 @@ static_assert(nsICrashService::PROCESS_TYPE_SOCKET ==
 static_assert(nsICrashService::PROCESS_TYPE_SANDBOX_BROKER ==
                   (int)GeckoProcessType_RemoteSandboxBroker,
               "GeckoProcessType enum is out of sync with nsICrashService!");
+static_assert(nsICrashService::PROCESS_TYPE_FORKSERVER ==
+                  (int)GeckoProcessType_ForkServer,
+              "GeckoProcessType enum is out of sync with nsICrashService!");
 // Add new static asserts here if you add more process types.
 // Update this static assert as well.
-static_assert(nsICrashService::PROCESS_TYPE_SANDBOX_BROKER + 1 ==
+static_assert(nsICrashService::PROCESS_TYPE_FORKSERVER + 1 ==
                   (int)GeckoProcessType_End,
               "GeckoProcessType enum is out of sync with nsICrashService!");
 
