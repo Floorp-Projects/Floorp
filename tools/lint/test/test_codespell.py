81496f0d3a78e175459dcd8dbd5007d532e7f924
Sylvestre Ledru <sledru@mozilla.com>
2019-12-03 14:00:49 +0000

    Bug 1579845 - Add a test to the mozlint check: codespell r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D45446

---
+++ tools/lint/test/test_codespell.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, print_function
+
+import mozunit
+
+LINTER = 'codespell'
+
+
+def test_lint_codespell(lint, paths):
+    results = lint(paths())
+    assert len(results) == 2
+
+    assert results[0].message == 'informations ==> information'
+    assert results[0].level == "error"
+    assert results[0].lineno == 1
+    assert results[0].relpath == "ignore.rst"
+
+    assert results[1].message == "mozila ==> mozilla"
+    assert results[1].level == "error"
+    assert results[1].lineno == 5
+    assert results[1].relpath == "ignore.rst"
+
+
+if __name__ == '__main__':
+    mozunit.main()
