aa4ef3548b7ad8814b18dfaec5a4646ce2bc38fa
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-12-04 09:06:00 +0000

    Bug 1567849 - Use ad-hoc console front for calls to evaluateJSAsync with selectedObjectActor. r=jdescottes.
    When the selectedObjectActor options is passed to the evaluateJSAsync
    command in the console, we now try to retrieve the corresponding console
    front to send the request to the correct target.
    This makes the "Copy Object" context menu entry works in the Browser Console
    on content objects.
    A test is added to ensure this works as expected.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55258

--- devtools/client/webconsole/commands.js
+++ devtools/client/webconsole/commands.js
@@ -12,8 +12,8 @@ class ConsoleCommands {
     this.currentTarget = currentTarget;
   }
 
-  evaluateJSAsync(expression, options = {}) {
-    const { selectedNodeFront, webConsoleFront } = options;
+  async evaluateJSAsync(expression, options = {}) {
+    const { selectedNodeFront, webConsoleFront, selectedObjectActor } = options;
     let front = this.proxy.webConsoleFront;
 
     // Defer to the selected paused thread front
@@ -21,9 +21,17 @@ class ConsoleCommands {
       front = webConsoleFront;
     }
 
-    // Defer to the selected node's thread console front
-    if (selectedNodeFront) {
-      front = selectedNodeFront.targetFront.activeConsole;
+    // If there's a selectedObjectActor option, this means the user intend to do a
+    // given action on a specific object, so it should take precedence over selected
+    // node front.
+    if (selectedObjectActor) {
+      const objectFront = this.debuggerClient.getFrontByID(selectedObjectActor);
+      if (objectFront) {
+        front = await objectFront.targetFront.getFront("console");
+      }
+    } else if (selectedNodeFront) {
+      // Defer to the selected node's thread console front
+      front = await selectedNodeFront.targetFront.getFront("console");
       options.selectedNodeActor = selectedNodeFront.actorID;
     }
 
