7cc8dee6ea619dc9393e79c116aa8b69799a5d8a
Marco Bonardo <mbonardo@mozilla.com>
2019-12-04 18:21:09 +0000

    Bug 1600244 - Don't store favicons added after the initial page load. r=mossop
    Icons added after the initial parsing are likely randomly generated to show badges,
    thus they are not good for permanent storage, because they are transient and can
    potentially flood the store.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55310
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/favicons/browser_favicon_nostore.js
+++ browser/base/content/test/favicons/browser_favicon_nostore.js
@@ -2,6 +2,7 @@
  * https://creativecommons.org/publicdomain/zero/1.0/ */
 
 // Test that a favicon with Cache-Control: no-store is not stored in Places.
+// Also tests that favicons added after pageshow are not stored.
 
 const TEST_SITE = "http://example.net";
 const ICON_URL =
@@ -21,7 +22,6 @@ add_task(async function browser_loader() {
   let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PAGE_URL);
   registerCleanupFunction(async () => {
     await cleanup();
-    BrowserTestUtils.removeTab(tab);
   });
 
   let { iconURL } = await iconPromise;
@@ -41,6 +41,7 @@ add_task(async function browser_loader() {
       }
     );
   });
+  BrowserTestUtils.removeTab(tab);
 });
 
 add_task(async function places_loader() {
@@ -67,4 +68,43 @@ add_task(async function places_loader() {
       }
     );
   });
+});
+
+add_task(async function later_addition() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, PAGE_URL);
+  registerCleanupFunction(async () => {
+    await cleanup();
+    BrowserTestUtils.removeTab(tab);
+  });
+
+  const LATE_ICON_URL =
+    TEST_SITE + "/browser/browser/base/content/test/favicons/moz.png";
+  let iconPromise = waitForFaviconMessage(true, LATE_ICON_URL);
+  await ContentTask.spawn(gBrowser.selectedBrowser, LATE_ICON_URL, href => {
+    let doc = content.document;
+    let head = doc.head;
+    let link = doc.createElement("link");
+    link.rel = "icon";
+    link.href = href;
+    link.type = "image/png";
+    head.appendChild(link);
+  });
+  let { iconURL } = await iconPromise;
+  is(iconURL, LATE_ICON_URL, "Should have seen the expected icon.");
+
+  // Ensure the favicon has not been stored.
+  /* eslint-disable mozilla/no-arbitrary-setTimeout */
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve, reject) => {
+    PlacesUtils.favicons.getFaviconURLForPage(
+      Services.io.newURI(PAGE_URL),
+      foundIconURI => {
+        if (foundIconURI) {
+          reject(new Error("An icon has been stored " + foundIconURI.spec));
+        }
+        resolve();
+      }
+    );
+  });
+  BrowserTestUtils.removeTab(tab);
 });
