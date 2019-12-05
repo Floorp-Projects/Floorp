7cd7ff8dfd3cd92c071d7f92a30b93f62f2af54f
Thinker Li <thinker.li@gmail.com>
2019-12-05 00:05:21 +0000

    Bug 1470591 - Part 5: ForkServer to create new processes. r=gsvelto
    Class ForkServer and class ForkServiceChild are implemented.  The
    chrome process can ask the fork server process to create content
    processes.  The requests are sent by MiniTransceiver over a socket.
    The fork server replys with the process IDs/handles of created
    processes.
    
    LaunchOptions::use_forkserver is a boolean.  With use_forkserver being
    true, the chrome process sends a request to the fork server instead of
    forking directly.
    
    Depends on D46881
    
    Differential Revision: https://phabricator.services.mozilla.com/D46883

--- ipc/glue/FileDescriptor.cpp
+++ ipc/glue/FileDescriptor.cpp
@@ -129,8 +129,17 @@ FileDescriptor::UniquePlatformHandle FileDescriptor::Clone(
 void IPDLParamTraits<FileDescriptor>::Write(IPC::Message* aMsg,
                                             IProtocol* aActor,
                                             const FileDescriptor& aParam) {
+#ifdef XP_WIN
   FileDescriptor::PickleType pfd =
       aParam.ShareTo(FileDescriptor::IPDLPrivate(), aActor->OtherPid());
+#else
+  // The pid returned by OtherPID() is only required for Windows to
+  // send file descriptors.  For the use case of the fork server,
+  // aActor is always null.  Since it is only for the special case of
+  // Windows, here we skip it for other platforms.
+  FileDescriptor::PickleType pfd =
+      aParam.ShareTo(FileDescriptor::IPDLPrivate(), 0);
+#endif
   WriteIPDLParam(aMsg, aActor, pfd);
 }
 
