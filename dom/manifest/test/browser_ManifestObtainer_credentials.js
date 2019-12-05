dacceabefa76068daea86db4986c7ff1b4d0c58a
Marcos Cáceres <marcos@marcosc.com>
2019-12-05 13:19:00 +0000

    Bug 1548424 - Check that Web App Manifest does not fetch using credentials by default r=baku
    tests that confirm we are not sending credentials by default
    
    Differential Revision: https://phabricator.services.mozilla.com/D49580

---
+++ dom/manifest/test/browser_ManifestObtainer_credentials.js
@@ -0,0 +1,44 @@
+// Used by JSHint:
+/* global ok, is, Cu, BrowserTestUtils, add_task, gBrowser, makeTestURL, requestLongerTimeout*/
+"use strict";
+const { ManifestObtainer } = ChromeUtils.import(
+  "resource://gre/modules/ManifestObtainer.jsm"
+);
+
+// Don't send cookies
+add_task(async function() {
+  const testPath = "/browser/dom/manifest/test/cookie_setter.html";
+  const tabURL = `http://example.com:80${testPath}`;
+  const browser = BrowserTestUtils.addTab(gBrowser, tabURL).linkedBrowser;
+  await BrowserTestUtils.browserLoaded(browser);
+  const { short_name } = await ManifestObtainer.browserObtainManifest(browser);
+  is(short_name, "no cookie");
+  const tab = gBrowser.getTabForBrowser(browser);
+  gBrowser.removeTab(tab);
+});
+
+// Send cookies
+add_task(async function() {
+  const testPath =
+    "/browser/dom/manifest/test/cookie_setter_with_credentials.html";
+  const tabURL = `http://example.com:80${testPath}`;
+  const browser = BrowserTestUtils.addTab(gBrowser, tabURL).linkedBrowser;
+  await BrowserTestUtils.browserLoaded(browser);
+  const { short_name } = await ManifestObtainer.browserObtainManifest(browser);
+  is(short_name, "🍪");
+  const tab = gBrowser.getTabForBrowser(browser);
+  gBrowser.removeTab(tab);
+});
+
+// Cross origin - we go from example.com to example.org
+add_task(async function() {
+  const testPath =
+    "/browser/dom/manifest/test/cookie_setter_with_credentials_cross_origin.html";
+  const tabURL = `http://example.com:80${testPath}`;
+  const browser = BrowserTestUtils.addTab(gBrowser, tabURL).linkedBrowser;
+  await BrowserTestUtils.browserLoaded(browser);
+  const { short_name } = await ManifestObtainer.browserObtainManifest(browser);
+  is(short_name, "no cookie");
+  const tab = gBrowser.getTabForBrowser(browser);
+  gBrowser.removeTab(tab);
+});
