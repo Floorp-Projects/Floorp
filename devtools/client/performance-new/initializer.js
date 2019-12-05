4e1a0ec073b8d9b92435852efc7ce384a45e10bd
Greg Tatum <gtatum@mozilla.com>
2019-12-05 17:38:52 +0000

    Bug 1597376 - Break out profiler event handling into a separate component; r=julienw
    Differential Revision: https://phabricator.services.mozilla.com/D55007

--- devtools/client/performance-new/initializer.js
+++ devtools/client/performance-new/initializer.js
@@ -33,9 +33,14 @@
   scope.loader = browserLoader.loader;
 }
 
-const Perf = require("devtools/client/performance-new/components/Perf");
 const ReactDOM = require("devtools/client/shared/vendor/react-dom");
 const React = require("devtools/client/shared/vendor/react");
+const DevToolsAndPopup = React.createFactory(
+  require("devtools/client/performance-new/components/DevToolsAndPopup")
+);
+const ProfilerEventHandling = React.createFactory(
+  require("devtools/client/performance-new/components/ProfilerEventHandling")
+);
 const createStore = require("devtools/client/shared/redux/create-store");
 const selectors = require("devtools/client/performance-new/store/selectors");
 const reducers = require("devtools/client/performance-new/store/reducers");
@@ -113,7 +118,16 @@ async function gInit(perfFront, preferenceFront) {
   );
 
   ReactDOM.render(
-    React.createElement(Provider, { store }, React.createElement(Perf)),
+    React.createElement(
+      Provider,
+      { store },
+      React.createElement(
+        React.Fragment,
+        null,
+        ProfilerEventHandling(),
+        DevToolsAndPopup()
+      )
+    ),
     document.querySelector("#root")
   );
 }
