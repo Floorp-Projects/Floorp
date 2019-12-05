b37bad6c758baae6d3e4eae1612a4b2ec3d1c6c0
Jan de Mooij <jdemooij@mozilla.com>
2019-12-05 12:53:25 +0000

    Bug 1598548 part 5 - Remove loopEntry_ and loopHead_ fields from LoopState. r=tcampbell
    In visitTestBackedge we can just get the backedge target from the
    bytecode instruction.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55629

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -1811,8 +1811,7 @@ AbortReasonOr<Ok> IonBuilder::startLoop(jsbytecode* loopEntry,
                                            stackPhiCount));
   current->end(MGoto::New(alloc(), header));
 
-  if (!loopStack_.emplaceBack(header, loopEntry, loopHead,
-                              GetNextPc(backjump))) {
+  if (!loopStack_.emplaceBack(header, GetNextPc(backjump))) {
     return abort(AbortReason::Alloc);
   }
 
@@ -3140,12 +3139,16 @@ AbortReasonOr<Ok> IonBuilder::visitTestBackedge(JSOp op, bool* restarted) {
 
   MDefinition* ins = current->pop();
 
-  jsbytecode* successorPC = GetNextPc(pc);
-  MOZ_ASSERT(loopStack_.back().successorStart() == successorPC);
+  jsbytecode* loopHead = pc + GET_JUMP_OFFSET(pc);
+  MOZ_ASSERT(JSOp(*loopHead) == JSOP_LOOPHEAD);
 
-  // We can finish the loop now.
+  jsbytecode* successorPC = GetNextPc(pc);
+
+  // We can finish the loop now. Use the loophead pc instead of the current pc
+  // because the stack depth at the start of that op matches the current stack
+  // depth (after popping our operand).
   MBasicBlock* backedge;
-  MOZ_TRY_VAR(backedge, newBlock(current, loopStack_.back().loopEntry()));
+  MOZ_TRY_VAR(backedge, newBlock(current, loopHead));
 
   if (op == JSOP_IFNE) {
     current->end(newTest(ins, backedge, nullptr));
