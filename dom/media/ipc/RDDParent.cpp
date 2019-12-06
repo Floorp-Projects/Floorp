9635a14382f68208ac62c52debf9dfe6c8076879
Aaron Klotz <aklotz@mozilla.com>
2019-12-06 22:12:22 +0000

    Bug 1522830: Part 10 - Add IPC for untrusted modules to RDD; r=mjf
    * The parent process needs to be able to request the child process to provide
      its untrusted modules telemetry. This is done via `GetUntrustedModulesData`.
    * The child process needs to be able to determine which of its module loads are
      trusted, and which are not. Since the child process is sandboxed, it must
      delegate that work to the parent process. This is done via `GetModulesTrust`.
    * The handlers for these functions just pass the requests on to DLL Services
      to do the actual processing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53682
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/ipc/RDDParent.cpp
+++ dom/media/ipc/RDDParent.cpp
@@ -8,6 +8,7 @@
 #if defined(XP_WIN)
 #  include <process.h>
 #  include <dwrite.h>
+#  include "mozilla/WinDllServices.h"
 #endif
 
 #include "mozilla/Assertions.h"
@@ -88,6 +89,11 @@ bool RDDParent::Init(base::ProcessId aParentPid, const char* aParentBuildID,
   gfxVars::Initialize();
 
   mozilla::ipc::SetThisProcessName("RDD Process");
+
+#if defined(XP_WIN)
+  RefPtr<DllServices> dllSvc(DllServices::Get());
+  dllSvc->StartUntrustedModulesProcessor();
+#endif  // defined(XP_WIN)
   return true;
 }
 
@@ -197,6 +203,22 @@ mozilla::ipc::IPCResult RDDParent::RecvRequestMemoryReport(
   return IPC_OK();
 }
 
+mozilla::ipc::IPCResult RDDParent::RecvGetUntrustedModulesData(
+    GetUntrustedModulesDataResolver&& aResolver) {
+#if defined(XP_WIN)
+  RefPtr<DllServices> dllSvc(DllServices::Get());
+  dllSvc->GetUntrustedModulesData()->Then(
+      GetMainThreadSerialEventTarget(), __func__,
+      [aResolver](Maybe<UntrustedModulesData>&& aData) {
+        aResolver(std::move(aData));
+      },
+      [aResolver](nsresult aReason) { aResolver(Nothing()); });
+  return IPC_OK();
+#else
+  return IPC_FAIL(this, "Unsupported on this platform");
+#endif  // defined(XP_WIN)
+}
+
 mozilla::ipc::IPCResult RDDParent::RecvPreferenceUpdate(const Pref& aPref) {
   Preferences::SetPreference(aPref);
   return IPC_OK();
@@ -213,6 +235,11 @@ void RDDParent::ActorDestroy(ActorDestroyReason aWhy) {
   // state.
   ProcessChild::QuickExit();
 #endif
+
+#if defined(XP_WIN)
+  RefPtr<DllServices> dllSvc(DllServices::Get());
+  dllSvc->DisableFull();
+#endif  // defined(XP_WIN)
 
 #ifdef MOZ_GECKO_PROFILER
   if (mProfilerController) {
