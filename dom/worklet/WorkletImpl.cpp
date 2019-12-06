0fc4fc52d1ea586613b34622774b2d024e7575c7
Karl Tomlinson <karlt+@karlt.net>
2019-12-06 01:40:49 +0000

    Bug 1594632 acquire ownership of already_AddRefed runnable parameter with RefPtr r=baku
    which will release the runnable on failure.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52614

--- dom/worklet/WorkletImpl.cpp
+++ dom/worklet/WorkletImpl.cpp
@@ -104,6 +104,7 @@ void WorkletImpl::NotifyWorkletFinished() {
 nsresult WorkletImpl::SendControlMessage(
     already_AddRefed<nsIRunnable> aRunnable) {
   MOZ_ASSERT(NS_IsMainThread());
+  RefPtr<nsIRunnable> runnable = std::move(aRunnable);
 
   // TODO: bug 1492011 re ConsoleWorkletRunnable.
   if (mTerminated) {
@@ -114,11 +115,11 @@ nsresult WorkletImpl::SendControlMessage(
     // Thread creation. FIXME: this will change.
     mWorkletThread = dom::WorkletThread::Create(this);
     if (!mWorkletThread) {
-      return NS_ERROR_UNEXPECTED;
+      return NS_ERROR_ILLEGAL_DURING_SHUTDOWN;
     }
   }
 
-  return mWorkletThread->DispatchRunnable(std::move(aRunnable));
+  return mWorkletThread->DispatchRunnable(runnable.forget());
 }
 
 }  // namespace mozilla
