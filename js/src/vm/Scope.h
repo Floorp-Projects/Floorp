7d0938a826ec6af980496c6cf5ab8707d3fe087b
Matthew Gaudet <mgaudet@mozilla.com>
2019-12-06 16:25:25 +0000

    Bug 1592102 - Hoist ScopeKind into own header r=tcampbell
    Avoids future circular include issues.
    
    Differential Revision: https://phabricator.services.mozilla.com/D51909

--- js/src/vm/Scope.h
+++ js/src/vm/Scope.h
@@ -21,6 +21,7 @@
 #include "util/Poison.h"
 #include "vm/BytecodeUtil.h"
 #include "vm/JSObject.h"
+#include "vm/ScopeKind.h"
 #include "vm/Xdr.h"
 
 namespace js {
@@ -44,43 +45,6 @@ enum class BindingKind : uint8_t {
 static inline bool BindingKindIsLexical(BindingKind kind) {
   return kind == BindingKind::Let || kind == BindingKind::Const;
 }
-
-enum class ScopeKind : uint8_t {
-  // FunctionScope
-  Function,
-
-  // VarScope
-  FunctionBodyVar,
-  ParameterExpressionVar,
-
-  // LexicalScope
-  Lexical,
-  SimpleCatch,
-  Catch,
-  NamedLambda,
-  StrictNamedLambda,
-  FunctionLexical,
-
-  // WithScope
-  With,
-
-  // EvalScope
-  Eval,
-  StrictEval,
-
-  // GlobalScope
-  Global,
-  NonSyntactic,
-
-  // ModuleScope
-  Module,
-
-  // WasmInstanceScope
-  WasmInstance,
-
-  // WasmFunctionScope
-  WasmFunction
-};
 
 enum class IsFieldInitializer : bool { No, Yes };
 
