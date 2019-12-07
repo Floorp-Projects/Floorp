8f4a55899ea8ae8c44d414feba8078d5d2f1b9f9
Brian Hackett <bhackett1024@gmail.com>
2019-12-07 16:13:07 +0000

    Bug 1595964 Part 2 - Expose evaluating worker in service worker registration front, r=jdescottes.
    Depends on D54286
    
    Differential Revision: https://phabricator.services.mozilla.com/D54288
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/worker/service-worker.js
+++ devtools/server/actors/worker/service-worker.js
@@ -4,6 +4,7 @@
 
 "use strict";
 
+const { Ci } = require("chrome");
 const protocol = require("devtools/shared/protocol");
 const {
   serviceWorkerSpec,
@@ -20,11 +21,17 @@ const ServiceWorkerActor = protocol.ActorClassWithSpec(serviceWorkerSpec, {
       return null;
     }
 
+    // handlesFetchEvents is not available if the worker's main script is in the
+    // evaluating state.
+    const isEvaluating =
+      this._worker.state == Ci.nsIServiceWorkerInfo.STATE_PARSED;
+    const fetch = isEvaluating ? undefined : this._worker.handlesFetchEvents;
+
     return {
       actor: this.actorID,
       url: this._worker.scriptSpec,
       state: this._worker.state,
-      fetch: this._worker.handlesFetchEvents,
+      fetch,
       id: this._worker.id,
     };
   },
