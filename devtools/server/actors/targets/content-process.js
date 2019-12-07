ad3914884b3c93dfd85aaa5ccbe31006f551e83a
Brian Hackett <bhackett1024@gmail.com>
2019-12-07 18:18:03 +0000

    Bug 1596939 Part 2 - Support pausing new service workers for a specific origin, r=jdescottes.
    Depends on D54292
    
    Differential Revision: https://phabricator.services.mozilla.com/D54293
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/actors/targets/content-process.js
+++ devtools/server/actors/targets/content-process.js
@@ -139,30 +139,36 @@ const ContentProcessTargetActor = ActorClassWithSpec(contentProcessTargetSpec, {
     };
   },
 
-  listWorkers: function() {
+  ensureWorkerList() {
     if (!this._workerList) {
       this._workerList = new WorkerTargetActorList(this.conn, {});
     }
-    return this._workerList.getList().then(actors => {
-      const pool = new Pool(this.conn);
-      for (const actor of actors) {
-        pool.manage(actor);
-      }
+    return this._workerList;
+  },
 
-      // Do not destroy the pool before transfering ownership to the newly created
-      // pool, so that we do not accidentally destroy actors that are still in use.
-      if (this._workerTargetActorPool) {
-        this._workerTargetActorPool.destroy();
-      }
+  listWorkers: function() {
+    return this.ensureWorkerList()
+      .getList()
+      .then(actors => {
+        const pool = new Pool(this.conn);
+        for (const actor of actors) {
+          pool.manage(actor);
+        }
 
-      this._workerTargetActorPool = pool;
-      this._workerList.onListChanged = this._onWorkerListChanged;
+        // Do not destroy the pool before transfering ownership to the newly created
+        // pool, so that we do not accidentally destroy actors that are still in use.
+        if (this._workerTargetActorPool) {
+          this._workerTargetActorPool.destroy();
+        }
 
-      return {
-        from: this.actorID,
-        workers: actors,
-      };
-    });
+        this._workerTargetActorPool = pool;
+        this._workerList.onListChanged = this._onWorkerListChanged;
+
+        return {
+          from: this.actorID,
+          workers: actors,
+        };
+      });
   },
 
   _onWorkerListChanged: function() {
@@ -170,12 +176,17 @@ const ContentProcessTargetActor = ActorClassWithSpec(contentProcessTargetSpec, {
     this._workerList.onListChanged = null;
   },
 
+  pauseMatchingServiceWorkers(request) {
+    this.ensureWorkerList().workerPauser.setPauseServiceWorkers(request.origin);
+  },
+
   destroy: function() {
     Actor.prototype.destroy.call(this);
 
     // Tell the live lists we aren't watching any more.
     if (this._workerList) {
-      this._workerList.onListChanged = null;
+      this._workerList.destroy();
+      this._workerList = null;
     }
 
     if (this._sources) {
