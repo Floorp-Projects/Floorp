099acd0134d738ffad397828c03fc5520b965afa
Kris Maglione <maglione.k@gmail.com>
2019-12-07 18:44:34 +0000

    Bug 1596918: Part 4a - Re-add eslint-disable comments removed by rewrite. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D53742

--- browser/components/enterprisepolicies/tests/browser/browser_policy_disable_popup_blocker.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_disable_popup_blocker.js
@@ -36,6 +36,7 @@ async function test_popup_blocker_disabled({ disabled, locked }) {
   await SpecialPowers.spawn(
     tab.linkedBrowser,
     [{ disabled, locked }],
+    // eslint-disable-next-line no-shadow
     async function({ disabled, locked }) {
       let checkbox = content.document.getElementById("popupPolicy");
       is(
