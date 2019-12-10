3c65829d4b13dcdfbd76b1f2528ed69a232a8939
caroline <cullen.caroline@gmail.com>
2019-12-10 01:46:29 +0000

    Bug 1588861 - Part 4: Add test harness for xdr modules. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D55596

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -142,7 +142,8 @@ enum class ZealMode {
   _(FinalizationGroupRegistrations)        \
   _(FinalizationRecordVector)              \
   _(ZoneAllocPolicy)                       \
-  _(SharedArrayRawBuffer)
+  _(SharedArrayRawBuffer)                  \
+  _(XDRBufferElements)
 
 #define JS_FOR_EACH_MEMORY_USE(_)  \
   JS_FOR_EACH_PUBLIC_MEMORY_USE(_) \
