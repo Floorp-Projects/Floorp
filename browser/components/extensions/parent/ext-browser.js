2e128ef3b0dcfec4ed1eb64c374616b0b6478e7e
Ciure Andrei <aciure@mozilla.com>
2019-12-11 00:57:08 +0200

    Backed out 3 changesets (bug 1591328) for causing Bug 1602808 a=backout l10n=flod
    Backed out changeset 475b4e9b25fc (bug 1591328)
    Backed out changeset bff9eef8191f (bug 1591328)
    Backed out changeset 0bcc6c8444db (bug 1591328)
    
    --HG--
    extra : histedit_source : f67e5979aa2bf7af03ec020d964927cdae8f5830

--- browser/components/extensions/parent/ext-browser.js
+++ browser/components/extensions/parent/ext-browser.js
@@ -978,14 +978,10 @@ class Window extends WindowBase {
   }
 
   setTitlePreface(titlePreface) {
-    if (!titlePreface) {
-      this.window.document.documentElement.removeAttribute("titlepreface");
-    } else {
-      this.window.document.documentElement.setAttribute(
-        "titlepreface",
-        titlePreface
-      );
-    }
+    this.window.document.documentElement.setAttribute(
+      "titlepreface",
+      titlePreface
+    );
   }
 
   get focused() {
