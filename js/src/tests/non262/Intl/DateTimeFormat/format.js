52c2846503b8c88e12f5bd1812ac7b957a0252f4
André Bargull <andre.bargull@gmail.com>
2019-12-10 12:25:55 +0000

    Bug 1591664: Remove Nightly-only restriction for DateTimeFormat's relatedYear output. r=jwalden
    This includes adjusting the date-time format pattern selection to take the
    calendar type into account.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55799

--- js/src/tests/non262/Intl/DateTimeFormat/format.js
+++ js/src/tests/non262/Intl/DateTimeFormat/format.js
@@ -6,12 +6,6 @@
 // Tests the format function with a diverse set of locales and options.
 // Always use UTC to avoid dependencies on test environment.
 
-if (typeof getBuildConfiguration === "undefined") {
-    var getBuildConfiguration = SpecialPowers.Cu.getJSTestingFunctions().getBuildConfiguration;
-}
-
-const isNightly = !getBuildConfiguration().release_or_beta;
-
 var format;
 var date = Date.UTC(2012, 11, 12, 3, 0, 0);
 var longFormatOptions = {timeZone: "UTC",
@@ -37,11 +31,7 @@ assertEq(format.format(date), "2012年12月12日 3:00:00");
 
 // Locale ar-MA; long format, Islamic civilian calendar.
 format = new Intl.DateTimeFormat("ar-ma-u-ca-islamicc", longFormatOptions);
-if (isNightly) {
-    assertEq(format.format(date), "28 محرم 1434 هـ 03:00:00");
-} else {
-    assertEq(format.format(date), "28 محرم 1434 03:00:00");
-}
+assertEq(format.format(date), "28 محرم 1434 هـ 03:00:00");
 
 // Locale en-IE: timeZoneName for crash test
 format = new Intl.DateTimeFormat("en-IE", {timeZone: "UTC", timeZoneName: "short"});
