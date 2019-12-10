85117004f284133099b938effbd3f86d0e40b3de
Emilio Cobos Álvarez <emilio@crisal.io>
2019-12-10 16:59:34 +0000

    Bug 1526725 - Make wheel events passive by default on Nightly. r=smaug
    This turned out to not be the root cause for that broken site, but I wrote the
    code so... I enabled it on Nightly only to see if there's fallout, I don't think
    we need to ship this in the near term, your call.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56556
    
    --HG--
    extra : moz-landing-system : lando

--- dom/events/EventListenerManager.cpp
+++ dom/events/EventListenerManager.cpp
@@ -661,20 +661,41 @@ bool EventListenerManager::ListenerCanHandle(const Listener* aListener,
   return aListener->mEventMessage == aEventMessage;
 }
 
+static bool IsDefaultPassiveWhenOnRoot(EventMessage aMessage) {
+  if (aMessage == eTouchStart || aMessage == eTouchMove) {
+    return StaticPrefs::dom_event_default_to_passive_touch_listeners();
+  }
+  if (aMessage == eWheel || aMessage == eLegacyMouseLineOrPageScroll ||
+      aMessage == eLegacyMousePixelScroll) {
+    return StaticPrefs::dom_event_default_to_passive_wheel_listeners();
+  }
+  return false;
+}
+
+static bool IsRootEventTaget(EventTarget* aTarget) {
+  if (nsCOMPtr<nsPIDOMWindowInner> win = do_QueryInterface(aTarget)) {
+    return true;
+  }
+  nsCOMPtr<nsINode> node = do_QueryInterface(aTarget);
+  if (!node) {
+    return false;
+  }
+  Document* doc = node->OwnerDoc();
+  return node == doc || node == doc->GetRootElement() || node == doc->GetBody();
+}
+
 void EventListenerManager::MaybeMarkPassive(EventMessage aMessage,
                                             EventListenerFlags& aFlags) {
-  if ((aMessage == eTouchStart || aMessage == eTouchMove) && mIsMainThreadELM &&
-      StaticPrefs::dom_event_default_to_passive_touch_listeners()) {
-    nsCOMPtr<nsINode> node;
-    nsCOMPtr<nsPIDOMWindowInner> win;
-    if ((win = GetTargetAsInnerWindow()) ||
-        ((node = do_QueryInterface(mTarget)) &&
-         (node == node->OwnerDoc() ||
-          node == node->OwnerDoc()->GetRootElement() ||
-          node == node->OwnerDoc()->GetBody()))) {
-      aFlags.mPassive = true;
-    }
+  if (!mIsMainThreadELM) {
+    return;
   }
+  if (!IsDefaultPassiveWhenOnRoot(aMessage)) {
+    return;
+  }
+  if (!IsRootEventTaget(mTarget)) {
+    return;
+  }
+  aFlags.mPassive = true;
 }
 
 void EventListenerManager::AddEventListenerByType(
