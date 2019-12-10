aac9e83281920345ce14738ac91c868ea069dc40
Johann Hofmann <jhofmann@mozilla.com>
2019-12-10 14:39:49 +0000

    Bug 1600942 - Remove event telemetry for permission prompt studies. r=Ehsan
    Depends on D56540
    
    Differential Revision: https://phabricator.services.mozilla.com/D56541

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -412,13 +412,6 @@ pref("permissions.fullscreen.allowed", false);
 
 pref("permissions.postPrompt.animate", true);
 
-// This is primarily meant to be enabled for studies.
-#ifdef NIGHTLY_BUILD
-  pref("permissions.eventTelemetry.enabled", true);
-#else
-  pref("permissions.eventTelemetry.enabled", false);
-#endif
-
 #ifdef NIGHTLY_BUILD
   pref("permissions.delegation.enabled", true);
 #else
