68f4dae4d67bc16de009b7de830b10f8f0c7fc71
Iain Ireland <iireland@mozilla.com>
2019-12-10 16:32:33 +0000

    Bug 1592307: Part 1: Define empty stub files r=mgaudet
    Creating shim files, to be filled in over the course of many patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56484
    
    --HG--
    extra : moz-landing-system : lando

---
+++ js/src/new-regexp/regexp-macro-assembler-arch.h
@@ -0,0 +1,16 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This file implements the NativeRegExpMacroAssembler interface for
+// SpiderMonkey. It provides the same interface as each of V8's
+// architecture-specific implementations.
+
+#ifndef RegexpMacroAssemblerArch_h
+#define RegexpMacroAssemblerArch_h
+
+#include "new-regexp/regexp-shim.h"
+
+#endif // RegexpMacroAssemblerArch_h
