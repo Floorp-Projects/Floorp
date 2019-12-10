9f6b4ddb80e248911da024ce1a29d8f3f10c0dbc
alexandru.ionescu <alexandru.ionescu@softvision.ro>
2019-12-10 17:04:35 +0000

    Bug 1587080 - Part 1: Add performance documentation verification code. r=ahal,perftest-reviewers,alexandru.irimovici,octavian_negru
    This patch adds the performance documentation (perfdocs) verification code under `tools/lint/perfdocs`. This tool currently validates `perfdocs` folders found within the `testing` folder to ensure all performance tests have documentation (it only does this for raptor at the moment). See `tools/lint/docs/perfdocs.rst` for more details.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53647
    
    --HG--
    extra : moz-landing-system : lando

---
+++ tools/lint/perfdocs/logger.py
@@ -0,0 +1,73 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import absolute_import
+
+import re
+
+
+class PerfDocLogger(object):
+    '''
+    Logger for the PerfDoc tooling. Handles the warnings by outputting
+    them into through the StructuredLogger provided by lint.
+    '''
+    PATHS = []
+    LOGGER = None
+
+    def __init__(self):
+        '''Initializes the PerfDocLogger.'''
+
+        # Set up class attributes for all logger instances
+        if not PerfDocLogger.LOGGER:
+            raise Exception(
+                "Missing linting LOGGER instance for PerfDocLogger initialization"
+            )
+        if not PerfDocLogger.PATHS:
+            raise Exception(
+                "Missing PATHS for PerfDocLogger initialization"
+            )
+        self.logger = PerfDocLogger.LOGGER
+
+    def log(self, msg):
+        '''
+        Log a message.
+
+        :param str msg: Message to log.
+        '''
+        self.logger.info(msg)
+
+    def warning(self, msg, files):
+        '''
+        Logs a validation warning message. The warning message is
+        used as the error message that is output in the reviewbot.
+
+        :param str msg: Message to log, it's also used as the error message
+            for the issue that is output by the reviewbot.
+        :param list/str files: The file(s) that this warning is about.
+        '''
+        if type(files) != list:
+            files = [files]
+
+        # Add a reviewbot error for each file that is given
+        for file in files:
+            # Get a relative path (reviewbot can't handle absolute paths)
+            # TODO: Expand to outside of the testing directory
+            fpath = re.sub(".*testing", "testing", file)
+
+            # Filter out any issues that do not relate to the paths
+            # that are being linted
+            for path in PerfDocLogger.PATHS:
+                if path not in file:
+                    continue
+
+                # Output error entry
+                self.logger.lint_error(
+                    message=msg,
+                    lineno=0,
+                    column=None,
+                    path=fpath,
+                    linter='perfdocs',
+                    rule="Flawless performance docs."
+                )
+
+                break
