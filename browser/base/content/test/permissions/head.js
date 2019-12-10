2bcb58b665a39d966a85ade895427e973e9781c1
Kris Maglione <maglione.k@gmail.com>
2019-12-10 23:07:28 +0000

    Bug 1596918: Part 4e - Fix callers which rely on frame message manager globals. r=mccr8
    Differential Revision: https://phabricator.services.mozilla.com/D53746

--- browser/base/content/test/permissions/head.js
+++ browser/base/content/test/permissions/head.js
@@ -2,3 +2,8 @@ ChromeUtils.import("resource:///modules/SitePermissions.jsm", this);
 const { PermissionTestUtils } = ChromeUtils.import(
   "resource://testing-common/PermissionTestUtils.jsm"
 );
+
+SpecialPowers.addTaskImport(
+  "E10SUtils",
+  "resource://gre/modules/E10SUtils.jsm"
+);
