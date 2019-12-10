84bd388bf6c0f802e4a8e67c9334aaf1764a6fa0
Jon Coppeard <jcoppeard@mozilla.com>
2019-12-10 20:26:20 +0000

    Bug 1602741 - Ensure shell drainJobQueue() function always drains the job queue r=sfink
    Previously this wouldn't drain any jobs queued by FinalizationGroup callbacks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56538

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -1030,12 +1030,15 @@ static void ShellCleanupFinalizationGroupCallback(JSObject* group, void* data) {
   }
 }
 
-static void MaybeRunFinalizationGroupCleanupTasks(JSContext* cx) {
+// Run any FinalizationGroup cleanup tasks and return whether any ran.
+static bool MaybeRunFinalizationGroupCleanupTasks(JSContext* cx) {
   ShellContext* sc = GetShellContext(cx);
   MOZ_ASSERT(!sc->quitting);
 
   Rooted<ShellContext::ObjectVector> groups(cx);
   std::swap(groups.get(), sc->finalizationGroupsToCleanUp.get());
+
+  bool ranTasks = false;
 
   RootedObject group(cx);
   for (const auto& g : groups) {
@@ -1048,10 +1051,14 @@ static void MaybeRunFinalizationGroupCleanupTasks(JSContext* cx) {
       mozilla::Unused << JS::CleanupQueuedFinalizationGroup(cx, group);
     }
 
+    ranTasks = true;
+
     if (sc->quitting) {
       break;
     }
   }
+
+  return ranTasks;
 }
 
 static bool EnqueueJob(JSContext* cx, unsigned argc, Value* vp) {
@@ -1074,14 +1081,19 @@ static void RunShellJobs(JSContext* cx) {
     return;
   }
 
-  // Run microtasks.
-  js::RunJobs(cx);
-  if (sc->quitting) {
-    return;
-  }
+  while (true) {
+    // Run microtasks.
+    js::RunJobs(cx);
+    if (sc->quitting) {
+      return;
+    }
 
-  // Run tasks (only finalization group clean tasks are possible).
-  MaybeRunFinalizationGroupCleanupTasks(cx);
+    // Run tasks (only finalization group clean tasks are possible).
+    bool ranTasks = MaybeRunFinalizationGroupCleanupTasks(cx);
+    if (!ranTasks) {
+      break;
+    }
+  }
 }
 
 static bool DrainJobQueue(JSContext* cx, unsigned argc, Value* vp) {
