88cf3fe71928bf7b33bec20a19aa1b7e9d0197e3
Tom Prince <mozilla@hocat.ca>
2019-12-11 10:14:29 +0000

    Bug 1596775: Run black on imported code; r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D55324

--- tools/crashreporter/system-symbols/mac/list-packages.py
+++ tools/crashreporter/system-symbols/mac/list-packages.py
@@ -6,22 +6,23 @@ from __future__ import print_function
 import os
 import sys
 from reposadolib import reposadocommon
-reposadocommon.get_main_dir = lambda: '/usr/local/bin/'
+
+reposadocommon.get_main_dir = lambda: "/usr/local/bin/"
 
 products = reposadocommon.getProductInfo()
 args = []
 for product_id, p in products.iteritems():
-  try:
-    t = p['title']
-  except KeyError:
-    print("Missing title in {}, skipping".format(p), file=sys.stderr)
-    continue
-  #p['CatalogEntry']['Packages']
-  if t.startswith('OS X') or t.startswith('Mac OS X') or t.startswith('macOS'):
-    args.append('--product-id=' + product_id)
-  else:
-    print('Skipping %r for repo_sync' % t, file=sys.stderr)
-if 'JUST_ONE_PACKAGE' in os.environ:
-  args = args[:1]
+    try:
+        t = p["title"]
+    except KeyError:
+        print("Missing title in {}, skipping".format(p), file=sys.stderr)
+        continue
+    # p['CatalogEntry']['Packages']
+    if t.startswith("OS X") or t.startswith("Mac OS X") or t.startswith("macOS"):
+        args.append("--product-id=" + product_id)
+    else:
+        print("Skipping %r for repo_sync" % t, file=sys.stderr)
+if "JUST_ONE_PACKAGE" in os.environ:
+    args = args[:1]
 
-print(' '.join(args))
+print(" ".join(args))
