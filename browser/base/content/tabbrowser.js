fb32b4fb7156074cde678566e0a30b7ddfe822af
Brindusan Cristian <cbrindusan@mozilla.com>
2019-12-11 04:44:43 +0200

    Backed out 2 changesets (bug 1602168, bug 1602123) for turning bug 1336075 into perma. CLOSED TREE
    Backed out changeset b892c240d938 (bug 1602168)
    Backed out changeset f47c59a4e3d3 (bug 1602123)

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -3156,10 +3156,7 @@
       this.removeTab(this.selectedTab, aParams);
     },
 
-    removeTab(
-      aTab,
-      { animate, byMouse, skipPermitUnload, closeWindowWithLastTab } = {}
-    ) {
+    removeTab(aTab, { animate, byMouse, skipPermitUnload } = {}) {
       // Telemetry stopwatches may already be running if removeTab gets
       // called again for an already closing tab.
       if (
@@ -3191,7 +3188,6 @@
         !this._beginRemoveTab(aTab, {
           closeWindowFastpath: true,
           skipPermitUnload,
-          closeWindowWithLastTab,
         })
       ) {
         TelemetryStopwatch.cancel("FX_TAB_CLOSE_TIME_ANIM_MS", aTab);
