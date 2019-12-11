049487ef5eb07b661bf0f267567df4d051aefa29
Brindusan Cristian <cbrindusan@mozilla.com>
2019-12-11 05:31:08 +0200

    Backed out 2 changesets (bug 1580567, bug 1599927) for mochitest failures at test_interfaces.html. CLOSED TREE
    Backed out changeset 0e0d88942d33 (bug 1599927)
    Backed out changeset 522c8d183bd5 (bug 1580567)

--- browser/modules/test/unit/test_SitePermissions.js
+++ browser/modules/test/unit/test_SitePermissions.js
@@ -28,7 +28,6 @@ add_task(async function testPermissionsListing() {
     "shortcuts",
     "persistent-storage",
     "storage-access",
-    "xr",
   ];
   if (RESIST_FINGERPRINTING_ENABLED) {
     // Canvas permission should be hidden unless privacy.resistFingerprinting
@@ -185,7 +184,6 @@ add_task(async function testExactHostMatch() {
     "microphone",
     "screen",
     "geo",
-    "xr",
     "persistent-storage",
   ];
   if (RESIST_FINGERPRINTING_ENABLED) {
