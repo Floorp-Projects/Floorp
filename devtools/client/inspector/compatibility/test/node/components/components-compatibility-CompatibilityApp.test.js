369ca073d4ffaa6196af986d88658edd19c2ecfb
Daisuke Akatsuka <daisuke@birchill.co.jp>
2019-12-11 12:23:55 +0000

    Bug 1588963: Add a jest test for CompatibilityApp component. r=ladybenko
    Depends on D55431
    
    Differential Revision: https://phabricator.services.mozilla.com/D55432
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/inspector/compatibility/test/node/components/components-compatibility-CompatibilityApp.test.js
@@ -0,0 +1,30 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Unit tests for the CompatibilityApp component.
+ */
+
+const { shallow } = require("enzyme");
+const { createFactory } = require("devtools/client/shared/vendor/react");
+const { thunk } = require("devtools/client/shared/redux/middleware/thunk.js");
+const configureStore = require("redux-mock-store").default;
+
+const CompatibilityApp = createFactory(
+  require("devtools/client/inspector/compatibility/components/CompatibilityApp")
+);
+
+describe("CompatibilityApp component", () => {
+  it("renders zero issues", () => {
+    const mockStore = configureStore([thunk]);
+    const store = mockStore({
+      compatibility: {
+        selectedNodeIssues: [],
+      },
+    });
+    const wrapper = shallow(CompatibilityApp({ store })).dive();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
