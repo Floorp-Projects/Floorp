63559fdcd5f1645b5a64ecac10899733b70ba909
Jason Laster <jlaster@mozilla.com>
2019-12-11 16:13:00 +0000

    Bug 1583550 - Highlight toolbox paused state. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D55848

--- devtools/client/framework/toolbox.js
+++ devtools/client/framework/toolbox.js
@@ -237,6 +237,9 @@ function Toolbox(
   this.frameMap = new Map();
   this.selectedFrameId = null;
 
+  // Set of paused threads to determine whether the toolbox is paused
+  this._pausedThreads = new Set();
+
   /**
    * KeyShortcuts instance specific to WINDOW host type.
    * This is the key shortcuts that are only register when the toolbox
@@ -589,7 +592,7 @@ Toolbox.prototype = {
     );
   },
 
-  _onPausedState: function(packet) {
+  _onPausedState: function(packet, threadFront) {
     // Suppress interrupted events by default because the thread is
     // paused/resumed a lot for various actions.
     if (packet.why.type === "interrupted") {
@@ -607,11 +610,18 @@ Toolbox.prototype = {
     ) {
       this.raise();
       this.selectTool("jsdebugger", packet.why.type);
+      this._pausedThreads.add(threadFront);
+      this.emit("toolbox-paused");
     }
   },
 
-  _onResumedState: function() {
-    this.unhighlightTool("jsdebugger");
+  _onResumedState: function(threadFront) {
+    this._pausedThreads.delete(threadFront);
+
+    if (this._pausedThreads.size == 0) {
+      this.emit("toolbox-resumed");
+      this.unhighlightTool("jsdebugger");
+    }
   },
 
   /**
@@ -668,19 +678,11 @@ Toolbox.prototype = {
   },
 
   _startThreadFrontListeners: function(threadFront) {
-    threadFront.on("paused", this._onPausedState);
-    threadFront.on("resumed", this._onResumedState);
-  },
-
-  _stopThreadFrontListeners: function(threadFront) {
-    // Only cleanup thread listeners if it has been attached.
-    // We may have closed the toolbox before it attached, or
-    // we may not have attached to the target at all.
-    if (!threadFront) {
-      return;
-    }
-    threadFront.off("paused", this._onPausedState);
-    threadFront.off("resumed", this._onResumedState);
+    // threadFront listeners are removed when the thread is destroyed
+    threadFront.on("paused", packet =>
+      this._onPausedState(packet, threadFront)
+    );
+    threadFront.on("resumed", () => this._onResumedState(threadFront));
   },
 
   _attachAndResumeThread: async function(target) {
@@ -902,7 +904,6 @@ Toolbox.prototype = {
     this.target.off("frame-update", this._updateFrames);
 
     // Detach the thread
-    this._stopThreadFrontListeners(this._threadFront);
     this._threadFront = null;
   },
 
@@ -2942,9 +2943,7 @@ Toolbox.prototype = {
    * Raise the toolbox host.
    */
   raise: function() {
-    this.postMessage({
-      name: "raise-host",
-    });
+    this.postMessage({ name: "raise-host" });
   },
 
   /**
@@ -3586,6 +3585,7 @@ Toolbox.prototype = {
     this.telemetry.toolClosed(this.currentToolId, this.sessionId, this);
 
     this._lastFocusedElement = null;
+    this._pausedThreads = null;
 
     if (this._sourceMapURLService) {
       this._sourceMapURLService.destroy();
