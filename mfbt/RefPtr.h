2e57c42685b5442ef0622540052973901f000782
Simon Giesecke <sgiesecke@mozilla.com>
2019-12-11 10:11:45 +0000

    Bug 1600096 - Disable RefPtr conversion constructors when underlying pointer types are not convertible. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D55141

--- mfbt/RefPtr.h
+++ mfbt/RefPtr.h
@@ -12,6 +12,8 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/DbgMacro.h"
 
+#include <type_traits>
+
 /*****************************************************************************/
 
 // template <class T> class RefPtrGetterAddRefs;
@@ -110,19 +112,22 @@ class MOZ_IS_REFPTR RefPtr {
 
   MOZ_IMPLICIT RefPtr(decltype(nullptr)) : mRawPtr(nullptr) {}
 
-  template <typename I>
+  template <typename I,
+            typename = std::enable_if_t<std::is_convertible_v<I*, T*>>>
   MOZ_IMPLICIT RefPtr(already_AddRefed<I>& aSmartPtr)
       : mRawPtr(aSmartPtr.take())
   // construct from |already_AddRefed|
   {}
 
-  template <typename I>
+  template <typename I,
+            typename = std::enable_if_t<std::is_convertible_v<I*, T*>>>
   MOZ_IMPLICIT RefPtr(already_AddRefed<I>&& aSmartPtr)
       : mRawPtr(aSmartPtr.take())
   // construct from |otherRefPtr.forget()|
   {}
 
-  template <typename I>
+  template <typename I,
+            typename = std::enable_if_t<std::is_convertible_v<I*, T*>>>
   MOZ_IMPLICIT RefPtr(const RefPtr<I>& aSmartPtr)
       : mRawPtr(aSmartPtr.get())
   // copy-construct from a smart pointer with a related pointer type
@@ -132,7 +137,8 @@ class MOZ_IS_REFPTR RefPtr {
     }
   }
 
-  template <typename I>
+  template <typename I,
+            typename = std::enable_if_t<std::is_convertible_v<I*, T*>>>
   MOZ_IMPLICIT RefPtr(RefPtr<I>&& aSmartPtr)
       : mRawPtr(aSmartPtr.forget().take())
   // construct from |Move(RefPtr<SomeSubclassOfT>)|.
