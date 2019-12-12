ac589f56c92b9beb88e12892e1dd050c39456cfa
Matt Woodrow <mwoodrow@mozilla.com>
2019-12-12 02:32:00 +0000

    Bug 1589123 - Do BrowsingContext::LoadURI messages via PContent, not PWindowGlobal since we might not have a current global. r=kmag
    Depends on D56818
    
    Differential Revision: https://phabricator.services.mozilla.com/D56820

--- docshell/base/BrowsingContext.cpp
+++ docshell/base/BrowsingContext.cpp
@@ -891,8 +891,9 @@ nsresult BrowsingContext::LoadURI(BrowsingContext* aAccessor,
   }
 
   if (!aAccessor && XRE_IsParentProcess()) {
-    Unused << Canonical()->GetCurrentWindowGlobal()->SendLoadURIInChild(
-        aLoadState, aSetNavigating);
+    if (ContentParent* cp = Canonical()->GetContentParent()) {
+      Unused << cp->SendLoadURI(this, aLoadState, aSetNavigating);
+    }
   } else {
     MOZ_DIAGNOSTIC_ASSERT(aAccessor);
     MOZ_DIAGNOSTIC_ASSERT(aAccessor->Group() == Group());
@@ -920,7 +921,7 @@ nsresult BrowsingContext::InternalLoad(BrowsingContext* aAccessor,
   }
 
   bool isActive =
-      aAccessor->GetIsActive() && !mIsActive &&
+      aAccessor && aAccessor->GetIsActive() && !mIsActive &&
       !Preferences::GetBool("browser.tabs.loadDivertedInBackground", false);
   if (mDocShell) {
     nsresult rv = nsDocShell::Cast(mDocShell)->InternalLoad(
@@ -941,9 +942,10 @@ nsresult BrowsingContext::InternalLoad(BrowsingContext* aAccessor,
     return rv;
   }
 
-  if (!aAccessor && XRE_IsParentProcess()) {
-    Unused << Canonical()->GetCurrentWindowGlobal()->SendInternalLoadInChild(
-        aLoadState, isActive);
+  if (XRE_IsParentProcess()) {
+    if (ContentParent* cp = Canonical()->GetContentParent()) {
+      Unused << cp->SendInternalLoad(this, aLoadState, isActive);
+    }
   } else {
     MOZ_DIAGNOSTIC_ASSERT(aAccessor);
     MOZ_DIAGNOSTIC_ASSERT(aAccessor->Group() == Group());
@@ -973,8 +975,9 @@ void BrowsingContext::DisplayLoadError(const nsAString& aURI) {
                                 PromiseFlatString(aURI).get(), nullptr,
                                 &didDisplayLoadError);
   } else {
-    Unused << Canonical()->GetCurrentWindowGlobal()->SendDisplayLoadError(
-        PromiseFlatString(aURI));
+    if (ContentParent* cp = Canonical()->GetContentParent()) {
+      Unused << cp->SendDisplayLoadError(this, PromiseFlatString(aURI));
+    }
   }
 }
 
