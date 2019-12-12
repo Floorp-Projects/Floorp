79468834d89750e813f880f60235bc85678223ab
Tom Prince <mozilla@hocat.ca>
2019-12-12 02:05:21 +0000

    Bug 1596775: Import scripts for downloading macOS system symbols; r=marco
    Import the code from
    https://github.com/marco-c/breakpad-mac-update-symbols/
    and
    https://github.com/marco-c/breakpad-scrape-system-symbols
    
    Differential Revision: https://phabricator.services.mozilla.com/D55321

---
+++ tools/crashreporter/system-symbols/mac/run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -v -e -x
+
+base="$(realpath $(dirname $0))"
+export PATH="$PATH:/home/worker/bin:$base"
+
+cd /home/worker
+
+if test "$PROCESSED_PACKAGES"; then
+  curl -L "$PROCESSED_PACKAGES" | gzip -dc > processed-packages
+  # Prevent reposado from downloading packages that have previously been
+  # dumped.
+  for f in `cat processed-packages`; do
+      mkdir -p `dirname $f`
+      touch $f
+  done
+fi
+
+mkdir -p /opt/data-reposado/html /opt/data-reposado/metadata
+
+# First, just fetch all the update info.
+repo_sync --no-download
+
+# Next, fetch just the update packages we're interested in.
+packages=$(python "${base}/list-packages.py")
+repo_sync $packages
+
+du -sh /opt/data-reposado
+
+# Now scrape symbols out of anything that was downloaded.
+mkdir -p symbols artifacts
+python "${base}/PackageSymbolDumper.py" --tracking-file=/home/worker/processed-packages --dump_syms=/home/worker/bin/dump_syms_mac /opt/data-reposado/html/content/downloads /home/worker/symbols
+
+# Hand out artifacts
+gzip -c processed-packages > artifacts/processed-packages.gz
+
+cd symbols
+zip -r9 /home/worker/artifacts/target.crashreporter-symbols.zip * || echo "No symbols dumped"
