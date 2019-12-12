e38327a944ab488c8903d8699eec4fe09baa4bf0
Kris Maglione <maglione.k@gmail.com>
2019-12-12 23:11:01 +0000

    Bug 1588220: Keep track of last active inner window when BrowsingContext is discarded. r=bzbarsky
    Any number of outer windows may be attached to a BrowsingContext over its
    lifetime. While the BrowsingContext is alive, it's easy to keep track of which
    of these is active, and therefore which of its inner windows is active. After
    it has been discarded, though, it discards its docShell reference, so all we
    can tell about an inner window is whether it is active for its own outer
    window, but not whether it should be considered active for its
    BrowsingContext.
    
    This patch updates the BrowsingContext detach logic to store a flag on the
    current inner window recording that it was active when its BrowsingContext was
    detached, and then later checks that flag to determine if it is the current
    window for a detached BrowsingContext.
    
    Differential Revision: https://phabricator.services.mozilla.com/D49032
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -842,6 +842,7 @@ nsGlobalWindowInner::nsGlobalWindowInner(nsGlobalWindowOuter* aOuterWindow,
       mHasVRDisplayActivateEvents(false),
       mXRPermissionRequestInFlight(false),
       mXRPermissionGranted(false),
+      mWasCurrentInnerWindow(false),
       mHasSeenGamepadInput(false),
       mSuspendDepth(0),
       mFreezeDepth(0),
@@ -2527,19 +2528,13 @@ bool nsPIDOMWindowInner::IsCurrentInnerWindow() const {
   auto* bc = GetBrowsingContext();
   MOZ_ASSERT(bc);
 
-  nsPIDOMWindowOuter* outer;
-  // When a BC is discarded, it stops returning outer windows altogether. That
-  // doesn't work for this check, since we still want current inner window to be
-  // treated as current after that point. Simply falling back to `mOuterWindow`
-  // here isn't ideal, since it will start returning true for inner windows
-  // which were current before a remoteness switch once a BrowsingContext has
-  // been discarded, but it's not incorrect in a way which should cause
-  // significant issues.
-  if (!bc->IsDiscarded()) {
-    outer = bc->GetDOMWindow();
-  } else {
-    outer = mOuterWindow;
+  if (bc->IsDiscarded()) {
+    // If our BrowsingContext has been discarded, we consider ourselves
+    // still-current if we were current at the time it was discarded.
+    return mOuterWindow && WasCurrentInnerWindow();
   }
+
+  nsPIDOMWindowOuter* outer = bc->GetDOMWindow();
   return outer && outer->GetCurrentInnerWindow() == this;
 }
 
