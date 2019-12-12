34aa6f275adefdcc5cfecd78fd1f43f738eeeb4d
Nazım Can Altınova <canaltinova@gmail.com>
2019-12-12 13:52:34 +0000

    Bug 1602800 - Move profiler recording preferences from firefox.js to all.js r=gregtatum a=jcristau
    We had to do it because firefox android doesn't use that file. There is a
    mobile.js file for that purpose. We had to move all recording preferences to
    all.js and add the android only preference to mobile.js to be able to handle
    that better.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56719
    
    --HG--
    extra : source : 16642f81cdc42ad89ee4a1cc252c558a270143ce

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -345,6 +345,13 @@ pref("devtools.debugger.unix-domain-socket", "@ANDROID_PACKAGE_NAME@/firefox-deb
 pref("devtools.remote.usb.enabled", false);
 pref("devtools.remote.wifi.enabled", false);
 
+// Preference for the new performance panel.
+// Profiler feature set. See tools/profiler/core/platform.cpp for features and
+// explanations. Android additionally has "java" feature but other features must
+// be the same. If you intend to change the default value of this pref, please
+// don't forget to change the all.js accordingly.
+pref("devtools.performance.recording.features", "[\"js\",\"leaf\",\"stackwalk\",\"java\"]");
+
 // With the typical screen sizes on mobile devices, we want to wrap page sources by default.
 pref("view_source.wrap_long_lines", true);
 
