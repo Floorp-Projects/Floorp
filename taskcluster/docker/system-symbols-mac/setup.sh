9e2fc7ebbcc56e625216b329c9e6a8cd1283cad7
Tom Prince <mozilla@hocat.ca>
2019-12-12 02:05:21 +0000

    Bug 1596775: Import scripts for downloading macOS system symbols; r=marco
    Import the code from
    https://github.com/marco-c/breakpad-mac-update-symbols/
    and
    https://github.com/marco-c/breakpad-scrape-system-symbols
    
    Differential Revision: https://phabricator.services.mozilla.com/D55321
    
    --HG--
    extra : moz-landing-system : lando

---
+++ taskcluster/docker/system-symbols-mac/setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -v -e -x
+
+ncpu=-j`grep -c ^processor /proc/cpuinfo`
+
+WORK=/setup/
+cd $WORK
+git clone https://github.com/mackyle/xar xar
+cd xar/xar
+./autogen.sh --prefix=/home/worker
+make $ncpu && make install
+
+cd $WORK
+git clone -b from_zarvox https://github.com/andreas56/libdmg-hfsplus.git
+cd libdmg-hfsplus
+cmake .
+make $ncpu dmg-bin hfsplus
+# `make install` installs way too much stuff
+cp dmg/dmg hfs/hfsplus /home/worker/bin
+strip /home/worker/bin/dmg /home/worker/bin/hfsplus
+
+cd $WORK
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:$PWD/depot_tools
+mkdir breakpad
+cd breakpad
+fetch breakpad
+cd src
+touch README
+./configure
+make $ncpu src/tools/mac/dump_syms/dump_syms_mac
+# `make install` is broken because there are two dump_syms binaries.
+cp src/tools/mac/dump_syms/dump_syms_mac /home/worker/bin
+strip /home/worker/bin/dump_syms_mac
+
+pip install git+https://github.com/wdas/reposado
+# Patch repo_sync from reposado for
+# https://github.com/wdas/reposado/pull/34
+cp /setup/repo_sync /usr/local/bin
+
+repoutil --configure <<EOF
+/opt/data-reposado/html/
+/opt/data-reposado/metadata/
+http://example.com/
+EOF
+
+pip install -r /setup/requirements.txt
+
+cd /
+rm -rf /setup
---
+++ tools/crashreporter/system-symbols/mac/list-packages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Copyright 2015 Ted Mielczarek. See the LICENSE
+# file at the top-level directory of this distribution.
+from __future__ import print_function
+
+import os
+import sys
+from reposadolib import reposadocommon
+reposadocommon.get_main_dir = lambda: '/usr/local/bin/'
+
+products = reposadocommon.getProductInfo()
+args = []
+for product_id, p in products.iteritems():
+  try:
+    t = p['title']
+  except KeyError:
+    print("Missing title in {}, skipping".format(p), file=sys.stderr)
+    continue
+  #p['CatalogEntry']['Packages']
+  if t.startswith('OS X') or t.startswith('Mac OS X') or t.startswith('macOS'):
+    args.append('--product-id=' + product_id)
+  else:
+    print('Skipping %r for repo_sync' % t, file=sys.stderr)
+if 'JUST_ONE_PACKAGE' in os.environ:
+  args = args[:1]
+
+print(' '.join(args))
