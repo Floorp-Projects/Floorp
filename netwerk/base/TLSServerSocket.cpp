b1f2d3fe60f8f21e5ebab3109b08b3e5c0c86c2e
Kershaw Chang <kershaw@mozilla.com>
2019-12-12 13:42:43 +0000

    Bug 1602009 - Make sure mSecurityObserver can be only accessed within a lock r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D56904

--- netwerk/base/TLSServerSocket.cpp
+++ netwerk/base/TLSServerSocket.cpp
@@ -279,10 +279,6 @@ TLSServerConnectionInfo::TLSServerConnectionInfo()
       mSecurityObserver(nullptr) {}
 
 TLSServerConnectionInfo::~TLSServerConnectionInfo() {
-  if (!mSecurityObserver) {
-    return;
-  }
-
   RefPtr<nsITLSServerSecurityObserver> observer;
   {
     MutexAutoLock lock(mLock);
@@ -421,14 +417,13 @@ nsresult TLSServerConnectionInfo::HandshakeCallback(PRFileDesc* aFD) {
   mKeyLength = cipherInfo.effectiveKeyBits;
   mMacLength = cipherInfo.macBits;
 
-  if (!mSecurityObserver) {
-    return NS_OK;
-  }
-
   // Notify consumer code that handshake is complete
   nsCOMPtr<nsITLSServerSecurityObserver> observer;
   {
     MutexAutoLock lock(mLock);
+    if (!mSecurityObserver) {
+      return NS_OK;
+    }
     mSecurityObserver.swap(observer);
   }
   nsCOMPtr<nsITLSServerSocket> serverSocket;
