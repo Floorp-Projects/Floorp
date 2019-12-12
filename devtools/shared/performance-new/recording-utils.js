5a7c30d79f8a2d3579ddb0f2b0e7f1d1d4512ec9
Nazım Can Altınova <canaltinova@gmail.com>
2019-12-12 13:53:31 +0000

    Bug 1602800 - Move the function that gets BrowsingContext ID to a new file inside shared folder r=gregtatum
    Differential Revision: https://phabricator.services.mozilla.com/D56720
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/shared/performance-new/recording-utils.js
@@ -0,0 +1,69 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+// @ts-check
+"use strict";
+
+/**
+ * This file is for the new performance panel that targets profiler.firefox.com,
+ * not the default-enabled DevTools performance panel.
+ */
+
+/**
+ * @typedef {import("../../client/performance-new/@types/perf").GetActiveBrowsingContextID} GetActiveBrowsingContextID
+ */
+
+/**
+ * TS-TODO
+ *
+ * This function replaces lazyRequireGetter, and TypeScript can understand it. It's
+ * currently duplicated until we have consensus that TypeScript is a good idea.
+ *
+ * @template T
+ * @type {(callback: () => T) => () => T}
+ */
+function requireLazy(callback) {
+  /** @type {T | undefined} */
+  let cache;
+  return () => {
+    if (cache === undefined) {
+      cache = callback();
+    }
+    return cache;
+  };
+}
+
+const lazyServices = requireLazy(() =>
+  require("resource://gre/modules/Services.jsm")
+);
+
+/**
+ * Gets the ID of active BrowsingContext from the browser.
+ *
+ * @type {GetActiveBrowsingContextID}
+ */
+function getActiveBrowsingContextID() {
+  const { Services } = lazyServices();
+  const win = Services.wm.getMostRecentWindow("navigator:browser");
+
+  if (
+    win &&
+    win.gBrowser &&
+    win.gBrowser.selectedBrowser &&
+    win.gBrowser.selectedBrowser.browsingContext &&
+    win.gBrowser.selectedBrowser.browsingContext.id
+  ) {
+    return win.gBrowser.selectedBrowser.browsingContext.id;
+  }
+
+  console.error(
+    "Failed to get the active BrowsingContext ID while starting the profiler."
+  );
+  // `0` mean that we failed to ge the active BrowsingContext ID, and it's
+  // treated as null value in the platform.
+  return 0;
+}
+
+module.exports = {
+  getActiveBrowsingContextID,
+};
