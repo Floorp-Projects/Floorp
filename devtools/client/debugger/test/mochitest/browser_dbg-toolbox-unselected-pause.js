55cb60a6e3e853d8b70beadbd18d8168c6792703
Julian Descottes <jdescottes@mozilla.com>
2019-12-12 15:00:20 +0000

    Bug 1603366 - Rename Omniscient browser toolbox to Multiprocess Browser Toolbox and support localization r=pbro,flod
    Differential Revision: https://phabricator.services.mozilla.com/D56877
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/debugger/test/mochitest/browser_dbg-toolbox-unselected-pause.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-toolbox-unselected-pause.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Test that the debugger pauses in the omniscient browser toolbox even when it
-// hasn't been opened.
+// Test that the debugger pauses in the multiprocess browser toolbox even when
+// it hasn't been opened.
 
 "use strict";
 
--- devtools/client/debugger/test/mochitest/browser_dbg-toolbox-workers.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-toolbox-workers.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Test that all kinds of workers show up properly in the omniscient browser
+// Test that all kinds of workers show up properly in the multiprocess browser
 // toolbox.
 
 "use strict";
