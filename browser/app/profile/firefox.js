0ae5a64d9b8528dbe6c54486ab50938db147f86f
Harry Twyford <htwyford@mozilla.com>
2019-12-12 20:23:46 +0000

    Bug 1601339 - Disable expandTextOnFocus. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D56985
    
    --HG--
    extra : moz-landing-system : lando

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -320,13 +320,12 @@ pref("browser.urlbar.usepreloadedtopurls.expire_days", 14);
 #ifdef NIGHTLY_BUILD
   // Whether the quantum bar displays design update 1.
   pref("browser.urlbar.update1", true);
-  // Whether we expand the font size when when the urlbar is
-  // focused in design update 1.
-  pref("browser.urlbar.update1.expandTextOnFocus", true);
   // Whether the urlbar should strip https from urls in the view.
   pref("browser.urlbar.update1.view.stripHttps", true);
 #else
   pref("browser.urlbar.update1", false);
+  // Whether we expand the font size when when the urlbar is
+  // focused in design update 1.
   pref("browser.urlbar.update1.expandTextOnFocus", false);
   pref("browser.urlbar.update1.view.stripHttps", false);
 #endif
