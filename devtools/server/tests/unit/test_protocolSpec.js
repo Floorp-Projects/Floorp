0ad204a9f2e63bb4c313ca192a0f7d19dbbf9767
Nicolas Chevobbe <nchevobbe@mozilla.com>
2019-12-13 13:11:45 +0000

    Bug 1602709 - Remove unused DebuggerClient.close onClosed parameter. r=ladybenko.
    Differential Revision: https://phabricator.services.mozilla.com/D56562

--- devtools/server/tests/unit/test_protocolSpec.js
+++ devtools/server/tests/unit/test_protocolSpec.js
@@ -3,14 +3,17 @@
 const run_test = Test(async function() {
   initTestDebuggerServer();
   const connection = DebuggerServer.connectPipe();
-  const client = Async(new DebuggerClient(connection));
+  const client = new DebuggerClient(connection);
 
   await client.connect();
 
   const response = await client.mainRoot.protocolDescription();
 
-  assert(response.from == "root");
-  assert(typeof response.types === "object");
+  assert(response.from == "root", "response.from has expected value");
+  assert(
+    typeof response.types === "object",
+    "response.types has expected type"
+  );
 
   await client.close();
 });
