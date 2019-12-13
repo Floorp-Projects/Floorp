b08a12d8e70a32d46a27fb7854909a7492f6dadb
Dave Townsend <dtownsend@oxymoronical.com>
2019-12-13 00:52:27 +0000

    Bug 1602173: Create a SiteSpecificBrowser class and expose data about it in content processes. r=Gijs
    Needed for providing synchronous responses to load requests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56285

--- browser/components/ssb/content/ssb.js
+++ browser/components/ssb/content/ssb.js
@@ -2,15 +2,24 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+ChromeUtils.defineModuleGetter(
+  this,
+  "SiteSpecificBrowser",
+  "resource:///modules/SiteSpecificBrowserService.jsm"
+);
+
 let gSSBBrowser = null;
+let gSSB = null;
 
 function init() {
+  gSSB = SiteSpecificBrowser.get(window.arguments[0]);
+
   gSSBBrowser = document.createXULElement("browser");
   gSSBBrowser.setAttribute("id", "browser");
   gSSBBrowser.setAttribute("type", "content");
   gSSBBrowser.setAttribute("remote", "true");
   document.getElementById("browser-container").appendChild(gSSBBrowser);
-  gSSBBrowser.src = window.arguments[0];
+  gSSBBrowser.src = gSSB.startURI.spec;
 }
 
 window.addEventListener("load", init, true);
