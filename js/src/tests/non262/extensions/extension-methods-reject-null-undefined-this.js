33445c52300a6718d3881436aa727081122c56c1
André Bargull <andre.bargull@gmail.com>
2019-12-13 19:13:27 +0000

    Bug 1565001 - Part 17: Move tests which are no longer our extensions into the main test file. r=evilpie
    And additionally remove tests for deleted extensions like `Object.prototype.watch`.
    
    Depends on D56932
    
    Differential Revision: https://phabricator.services.mozilla.com/D56933

--- js/src/tests/non262/extensions/extension-methods-reject-null-undefined-this.js
+++ js/src/tests/non262/extensions/extension-methods-reject-null-undefined-this.js
@@ -20,21 +20,13 @@ print(BUGNUMBER + ": " + summary);
 
 var ClassToMethodMap =
   {
-    Object:   [/*
-                * Don't box this just yet for these methods -- they're used too
-                * much without qualification to do that.  :-(
-                */
-               /* "__defineGetter__", "__defineSetter__", */
-               "__lookupGetter__", "__lookupSetter__", "watch", "unwatch",
-               "toSource"],
+    Object:   ["toSource"],
     Function: ["toSource"],
     Array:    ["toSource"],
-    String:   ["toSource", "quote", "bold", "italics", "fixed", "fontsize",
-               "fontcolor", "link", "anchor", "strike", "small", "big", "blink",
-               "sup", "sub", "substr", "trimLeft", "trimRight", "toJSON"],
-    Boolean:  ["toSource", "toJSON"],
-    Number:   ["toSource", "toJSON"],
-    Date:     ["toSource", "getYear", "setYear",  "toGMTString"],
+    String:   ["toSource"],
+    Boolean:  ["toSource"],
+    Number:   ["toSource"],
+    Date:     ["toSource"],
     RegExp:   ["toSource"],
     Error:    ["toSource"],
   };
