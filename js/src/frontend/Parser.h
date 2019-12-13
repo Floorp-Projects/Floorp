48400c98546703b222b6b6b6be7a29b1064dd265
Matthew Gaudet <mgaudet@mozilla.com>
2019-12-13 22:52:33 +0000

    Bug 1601698 - Rename publish functions to reflect current state r=caroline
    Depends on D57159
    
    Differential Revision: https://phabricator.services.mozilla.com/D57160

--- js/src/frontend/Parser.h
+++ js/src/frontend/Parser.h
@@ -327,22 +327,13 @@ class MOZ_STACK_CLASS ParserBase : public ParserSharedBase,
 
   FunctionTreeHolder& treeHolder_;
 
-  MOZ_MUST_USE bool publishDeferredItems(FunctionTree* root) {
-    // Publish deferred functions before LazyScripts, as the
-    // LazyScripts need the functions.
-    if (!publishDeferredFunctions(root)) {
-      return false;
-    }
-    return true;
-  }
-
-  bool publishDeferredFunctions(FunctionTree* root);
+  MOZ_MUST_USE bool publishDeferredFunctions(FunctionTree* root);
 
  public:
   FunctionTreeHolder& getTreeHolder() { return treeHolder_; }
 
-  MOZ_MUST_USE bool publishDeferredItems() {
-    return publishDeferredItems(getTreeHolder().getFunctionTree());
+  MOZ_MUST_USE bool publishDeferredFunctions() {
+    return publishDeferredFunctions(getTreeHolder().getFunctionTree());
   }
 
   bool awaitIsKeyword() const { return awaitHandling_ != AwaitIsName; }
