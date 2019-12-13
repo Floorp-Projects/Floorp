8596d8cc755a49e88f2a6c06b51540211ac97909
Eric Rahm <erahm@mozilla.com>
2019-12-13 10:26:49 +0000

    Bug 1594344 - Part 1: Add dump_syms toolchain. r=froydnj
    This defines a new `dump_syms` toolchain that uses the rust implementation of `dump_syms`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53318

---
+++ taskcluster/scripts/misc/build-dump-syms.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -x -e -v
+
+PROJECT=dump_syms
+
+# This script is for building dump_syms
+case "$(uname -s)" in
+Linux)
+    COMPRESS_EXT=xz
+    ;;
+MINGW*)
+    UPLOAD_DIR=$PWD/public/build
+    COMPRESS_EXT=bz2
+
+    . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
+    ;;
+esac
+
+cd $GECKO_PATH
+
+if [ -n "$TOOLTOOL_MANIFEST" ]; then
+  . taskcluster/scripts/misc/tooltool-download.sh
+fi
+
+# cargo gets mad if the parent directory has a Cargo.toml file in it
+if [ -e Cargo.toml ]; then
+  mv Cargo.toml Cargo.toml.back
+fi
+
+PATH="$(cd $MOZ_FETCHES_DIR && pwd)/rustc/bin:$PATH"
+
+cd $MOZ_FETCHES_DIR/$PROJECT
+
+cargo build --verbose --release
+
+mkdir $PROJECT
+cp target/release/${PROJECT}* ${PROJECT}/
+tar -acf ${PROJECT}.tar.$COMPRESS_EXT $PROJECT
+mkdir -p $UPLOAD_DIR
+cp ${PROJECT}.tar.$COMPRESS_EXT $UPLOAD_DIR
+
+cd ..
+if [ -e Cargo.toml.back ]; then
+  mv Cargo.toml.back Cargo.toml
+fi
+
+. $GECKO_PATH/taskcluster/scripts/misc/vs-cleanup.sh
