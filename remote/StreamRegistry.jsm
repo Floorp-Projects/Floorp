c5f22e7a2d9ab3d9c535c809c2f2ddb547b3e622
Henrik Skupin <mail@hskupin.info>
2019-12-16 16:17:47 +0000

    Bug 1600053 - [remote] Add registry for managing references to streams. r=remote-protocol-reviewers,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D57304

---
+++ remote/StreamRegistry.jsm
@@ -0,0 +1,125 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+var EXPORTED_SYMBOLS = ["StreamRegistry"];
+
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  AsyncShutdown: "resource://gre/modules/AsyncShutdown.jsm",
+  OS: "resource://gre/modules/osfile.jsm",
+});
+
+XPCOMUtils.defineLazyServiceGetters(this, {
+  uuidGen: ["@mozilla.org/uuid-generator;1", "nsIUUIDGenerator"],
+});
+
+const { UnsupportedError } = ChromeUtils.import(
+  "chrome://remote/content/Error.jsm"
+);
+
+class StreamRegistry {
+  constructor() {
+    // handle => stream
+    this.streams = new Map();
+
+    // Register an async shutdown blocker to ensure all open IO streams are
+    // closed, and remaining temporary files removed. Needs to happen before
+    // OS.File has been shutdown.
+    AsyncShutdown.profileBeforeChange.addBlocker(
+      "Remote Agent: Clean-up of open streams",
+      async () => {
+        await this.destructor();
+      }
+    );
+  }
+
+  async destructor() {
+    for (const stream of this.streams.values()) {
+      await this._discard(stream);
+    }
+
+    this.streams.clear();
+  }
+
+  async _discard(stream) {
+    if (stream instanceof OS.File) {
+      const fileInfo = await stream.stat();
+      stream.close();
+
+      // Also remove the temporary file
+      try {
+        await OS.File.remove(fileInfo.path, { ignoreAbsent: true });
+      } catch (e) {
+        console.error(`Failed to remove ${fileInfo.path}: ${e.message}`);
+      }
+    }
+  }
+
+  /**
+   * Add a new stream to the registry.
+   *
+   * @param {OS.File} stream
+   *      Instance of the stream to add.
+   *
+   * @return {string}
+   *     Stream handle (uuid)
+   */
+  add(stream) {
+    let handle;
+
+    if (stream instanceof OS.File) {
+      handle = uuidGen
+        .generateUUID()
+        .toString()
+        .slice(1, -1);
+    } else {
+      // Bug 1602731 - Implement support for blob
+      throw new UnsupportedError(`Unknown stream type for ${stream}`);
+    }
+
+    this.streams.set(handle, stream);
+
+    return handle;
+  }
+
+  /**
+   * Get a stream from the registry.
+   *
+   * @param {string} handle
+   *      Handle of the stream to retrieve.
+   *
+   * @return {OS.File}
+   *     Requested stream
+   */
+  get(handle) {
+    const stream = this.streams.get(handle);
+
+    if (!stream) {
+      throw new TypeError(`Invalid stream handle`);
+    }
+
+    return stream;
+  }
+
+  /**
+   * Remove a stream from the registry.
+   *
+   * @param {string} handle
+   *      Handle of the stream to remove.
+   *
+   * @return {boolean}
+   *     true if successfully removed
+   */
+  async remove(handle) {
+    const stream = this.get(handle);
+    await this._discard(stream);
+
+    return this.streams.delete(handle);
+  }
+}
---
+++ remote/test/unit/test_StreamRegistry.js
@@ -0,0 +1,172 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const { AsyncShutdown } = ChromeUtils.import(
+  "resource://gre/modules/AsyncShutdown.jsm"
+);
+const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+const { StreamRegistry } = ChromeUtils.import(
+  "chrome://remote/content/StreamRegistry.jsm"
+);
+
+add_test(function test_constructor() {
+  const registry = new StreamRegistry();
+  equal(registry.streams.size, 0);
+
+  run_next_test();
+});
+
+add_test(async function test_destructor() {
+  const registry = new StreamRegistry();
+  const { file: file1, path: path1 } = await createFile("foo bar");
+  const { file: file2, path: path2 } = await createFile("foo bar");
+
+  registry.add(file1);
+  registry.add(file2);
+
+  equal(registry.streams.size, 2);
+
+  await registry.destructor();
+  equal(registry.streams.size, 0);
+  ok(!(await OS.File.exists(path1)), "temporary file has been removed");
+  ok(!(await OS.File.exists(path2)), "temporary file has been removed");
+
+  run_next_test();
+});
+
+add_test(async function test_addValidStreamType() {
+  const registry = new StreamRegistry();
+  const { file } = await createFile("foo bar");
+
+  const handle = registry.add(file);
+  equal(registry.streams.size, 1, "A single stream has been added");
+  equal(typeof handle, "string", "Handle is of type string");
+  ok(registry.streams.has(handle), "Handle has been found");
+  equal(registry.streams.get(handle), file, "Expected OS.File stream found");
+
+  run_next_test();
+});
+
+add_test(async function test_addCreatesDifferentHandles() {
+  const registry = new StreamRegistry();
+  const { file } = await createFile("foo bar");
+
+  const handle1 = registry.add(file);
+  equal(registry.streams.size, 1, "A single stream has been added");
+  equal(typeof handle1, "string", "Handle is of type string");
+  ok(registry.streams.has(handle1), "Handle has been found");
+  equal(registry.streams.get(handle1), file, "Expected OS.File stream found");
+
+  const handle2 = registry.add(file);
+  equal(registry.streams.size, 2, "A single stream has been added");
+  equal(typeof handle2, "string", "Handle is of type string");
+  ok(registry.streams.has(handle2), "Handle has been found");
+  equal(registry.streams.get(handle2), file, "Expected OS.File stream found");
+
+  notEqual(handle1, handle2, "Different handles have been generated");
+
+  run_next_test();
+});
+
+add_test(async function test_addInvalidStreamType() {
+  const registry = new StreamRegistry();
+  Assert.throws(() => registry.add(new Blob([])), /UnsupportedError/);
+
+  run_next_test();
+});
+
+add_test(async function test_getForValidHandle() {
+  const registry = new StreamRegistry();
+  const { file } = await createFile("foo bar");
+  const handle = registry.add(file);
+
+  equal(registry.streams.size, 1, "A single stream has been added");
+  equal(registry.get(handle), file, "Expected OS.File stream found");
+
+  run_next_test();
+});
+
+add_test(async function test_getForInvalidHandle() {
+  const registry = new StreamRegistry();
+  const { file } = await createFile("foo bar");
+  registry.add(file);
+
+  equal(registry.streams.size, 1, "A single stream has been added");
+  Assert.throws(() => registry.get("foo"), /TypeError/);
+
+  run_next_test();
+});
+
+add_test(async function test_removeForValidHandle() {
+  const registry = new StreamRegistry();
+  const { file: file1, path: path1 } = await createFile("foo bar");
+  const { file: file2, path: path2 } = await createFile("foo bar");
+
+  const handle1 = registry.add(file1);
+  const handle2 = registry.add(file2);
+
+  equal(registry.streams.size, 2);
+
+  await registry.remove(handle1);
+  equal(registry.streams.size, 1);
+  ok(
+    !(await OS.File.exists(path1)),
+    "temporary file for first stream has been removed"
+  );
+  equal(registry.get(handle2), file2, "Second stream has not been closed");
+  ok(
+    await OS.File.exists(path2),
+    "temporary file for second stream hasn't been removed"
+  );
+
+  run_next_test();
+});
+
+add_test(async function test_removeForInvalidHandle() {
+  const registry = new StreamRegistry();
+  const { file } = await createFile("foo bar");
+  registry.add(file);
+
+  equal(registry.streams.size, 1, "A single stream has been added");
+  await Assert.rejects(registry.remove("foo"), /TypeError/);
+
+  run_next_test();
+});
+
+async function createFile(contents, options = {}) {
+  let { path = null, remove = true } = options;
+
+  if (!path) {
+    const basePath = OS.Path.join(OS.Constants.Path.tmpDir, "remote-agent.txt");
+    const { file, path: tmpPath } = await OS.File.openUnique(basePath, {
+      humanReadable: true,
+    });
+    await file.close();
+    path = tmpPath;
+  }
+
+  let encoder = new TextEncoder();
+  let array = encoder.encode(contents);
+
+  const count = await OS.File.writeAtomic(path, array, {
+    encoding: "utf-8",
+    tmpPath: path + ".tmp",
+  });
+  equal(count, contents.length, "All data has been written to file");
+
+  const file = await OS.File.open(path);
+
+  // Automatically remove the file once the test has finished
+  if (remove) {
+    registerCleanupFunction(async () => {
+      await file.close();
+      await OS.File.remove(path, { ignoreAbsent: true });
+    });
+  }
+
+  return { file, path };
+}
