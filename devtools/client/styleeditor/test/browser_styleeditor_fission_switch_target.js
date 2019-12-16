6b4e2ff792698eae8f357c5c4a60a4cca0bea08e
Daisuke Akatsuka <daisuke@birchill.co.jp>
2019-12-16 10:18:07 +0000

    Bug 1578753: Add a test for top-level target switing. r=jdescottes,ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D56705

---
+++ devtools/client/styleeditor/test/browser_styleeditor_fission_switch_target.js
@@ -0,0 +1,28 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+// Test switching for the top-level target.
+
+const PARENT_PROCESS_URI = "about:robots";
+const CONTENT_PROCESS_URI = TEST_BASE_HTTPS + "simple.html";
+
+add_task(async function() {
+  await pushPref("devtools.target-switching.enabled", true);
+
+  // We use about:robots, because this page will run in the parent process.
+  // Navigating from about:robots to a regular content page will always trigger a target
+  // switch, with or without fission.
+
+  info("Open a page that runs in the parent process");
+  const { toolbox, ui } = await openStyleEditorForURL(PARENT_PROCESS_URI);
+  is(ui.editors.length, 3, `Three style sheets for ${PARENT_PROCESS_URI}`);
+
+  info("Navigate to a page that runs in the child process");
+  await navigateToAndWaitForStyleSheets(CONTENT_PROCESS_URI, ui);
+  is(ui.editors.length, 2, `Two sheets present for ${CONTENT_PROCESS_URI}`);
+
+  const onToolboxDestroyed = gDevTools.once("toolbox-destroyed");
+  toolbox.closeToolbox();
+  await onToolboxDestroyed;
+});
