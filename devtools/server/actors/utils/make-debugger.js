d52bd986843f8e0e0ad31a6a524cbafa1248a9b4
Logan Smyth <loganfsmyth@gmail.com>
2019-12-16 15:41:31 +0000

    Bug 1592644 - Ensure that we are always passing a global to listen for events. r=jlast
    We are being inconsistent here and sometimes calling `.connect` with a
    Debugger.Object that points at a global instead of the global itself.
    Passing the Debugger.Object fails since it isn't a global.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57270

--- devtools/server/actors/utils/make-debugger.js
+++ devtools/server/actors/utils/make-debugger.js
@@ -65,16 +65,9 @@ module.exports = function makeDebugger({
   dbg.allowUnobservedAsmJS = true;
   dbg.uncaughtExceptionHook = reportDebuggerHookException;
 
-  function onNewDebuggee(global) {
-    if (dbg.onNewDebuggee) {
-      dbg.onNewDebuggee(global);
-    }
-  }
-
   const onNewGlobalObject = function(global) {
     if (shouldAddNewGlobalAsDebuggee(global)) {
       safeAddDebuggee(this, global);
-      onNewDebuggee(global);
     }
   };
 
@@ -82,7 +75,6 @@ module.exports = function makeDebugger({
   dbg.addDebuggees = function() {
     for (const global of findDebuggees(this)) {
       safeAddDebuggee(this, global);
-      onNewDebuggee(global);
     }
   };
 
@@ -105,9 +97,15 @@ const reportDebuggerHookException = e => reportException("DBG-SERVER", e);
  * Add |global| as a debuggee to |dbg|, handling error cases.
  */
 function safeAddDebuggee(dbg, global) {
+  let globalDO;
   try {
-    dbg.addDebuggee(global);
+    globalDO = dbg.addDebuggee(global);
   } catch (e) {
     // Ignoring attempt to add the debugger's compartment as a debuggee.
+    return;
+  }
+
+  if (dbg.onNewDebuggee) {
+    dbg.onNewDebuggee(globalDO);
   }
 }
