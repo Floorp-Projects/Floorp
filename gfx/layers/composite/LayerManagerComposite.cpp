8bf1fb6a515d4fbbe94548344b41522f09e50ad8
Markus Stange <mstange@themasta.com>
2019-12-17 17:48:58 -0500

    Back out bug 1592739 on 72 due to multiple regressions (bug 1599366, bug 1601183, bug 1602193). a=jcristau

--- gfx/layers/composite/LayerManagerComposite.cpp
+++ gfx/layers/composite/LayerManagerComposite.cpp
@@ -1208,7 +1208,11 @@ bool LayerManagerComposite::Render(const nsIntRegion& aInvalidRegion,
       // opaque parts of the window are covered by different layers and we can
       // update those parts separately.
       IntRegion opaqueRegion;
-      opaqueRegion.And(aOpaqueRegion, mRenderBounds);
+#ifdef XP_MACOSX
+      opaqueRegion =
+          mCompositor->GetWidget()->GetOpaqueWidgetRegion().ToUnknownRegion();
+#endif
+      opaqueRegion.AndWith(mRenderBounds);
 
       // Limit the complexity of these regions. Usually, opaqueRegion should be
       // only one or two rects, so this SimplifyInward call will not change the
