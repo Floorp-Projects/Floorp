038bd866b5a75a892fd1bd1ada1eabd761241545
Sylvestre Ledru <sledru@mozilla.com>
2019-12-17 19:17:43 +0000

    Bug 1601612 - Fix some py2/py3 lints with a newer version of Python r=marco
    Differential Revision: https://phabricator.services.mozilla.com/D57509
    
    --HG--
    extra : moz-landing-system : lando

--- tools/crashreporter/system-symbols/mac/parse_pbzx.py
+++ tools/crashreporter/system-symbols/mac/parse_pbzx.py
@@ -28,6 +28,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import
+
 import struct
 import sys
 
