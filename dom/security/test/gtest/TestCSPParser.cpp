e6e2b93b2f554fa7ddb5972c64595c45ae388409
Jonathan Kingston <jkt@mozilla.com>
2019-12-17 17:39:37 +0000

    Bug 1603709 - Add 'report-sample' to CSP exemptions to invalidation when using 'strict-dynamic' r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D57491

--- dom/security/test/gtest/TestCSPParser.cpp
+++ dom/security/test/gtest/TestCSPParser.cpp
@@ -219,6 +219,8 @@ TEST(CSPParser, Directives)
       "script-src 'nonce-foo' 'unsafe-inline'" },
     { "script-src 'nonce-foo' 'strict-dynamic' 'unsafe-inline' https:  ",
       "script-src 'nonce-foo' 'strict-dynamic' 'unsafe-inline' https:" },
+    { "script-src 'nonce-foo' 'strict-dynamic' 'unsafe-inline' 'report-sample' https:  ",
+      "script-src 'nonce-foo' 'strict-dynamic' 'unsafe-inline' 'report-sample' https:" },
     { "default-src 'sha256-siVR8' 'strict-dynamic' 'unsafe-inline' https:  ",
       "default-src 'sha256-siVR8' 'unsafe-inline' https:" },
     { "worker-src https://example.com",
