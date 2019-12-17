ad6e96dab1000f39c87916db704c63e973021b88
James Willcox <snorp@snorp.net>
2019-12-17 20:59:31 +0000

    Bug 1603114 - Ensure Gecko channel is closed with `WebResponse` body stream. r=geckoview-reviewers,agi
    This also fixes a lifetime issue where we may have been leaking the
    native instance of `WebResponse.Support`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56994

--- widget/android/WebExecutorSupport.cpp
+++ widget/android/WebExecutorSupport.cpp
@@ -143,14 +143,24 @@ class StreamSupport final
  public:
   typedef java::GeckoInputStream::Support::Natives<StreamSupport> Base;
   using Base::AttachNative;
-  using Base::DisposeNative;
   using Base::GetNative;
 
-  explicit StreamSupport(nsIRequest* aRequest) : mRequest(aRequest) {}
+  explicit StreamSupport(java::GeckoInputStream::Support::Param aInstance,
+                         nsIRequest* aRequest)
+      : mInstance(aInstance), mRequest(aRequest) {}
+
+  void Close() {
+    mRequest->Cancel(NS_ERROR_ABORT);
+    mRequest->Resume();
+
+    // This is basically `delete this`, so don't run anything else!
+    Base::DisposeNative(mInstance);
+  }
 
   void Resume() { mRequest->Resume(); }
 
  private:
+  java::GeckoInputStream::Support::GlobalRef mInstance;
   nsCOMPtr<nsIRequest> mRequest;
 };
 
@@ -183,8 +193,8 @@ class LoaderListener final : public nsIStreamListener,
 
     // We're expecting data later via OnDataAvailable, so create the stream now.
     mSupport = java::GeckoInputStream::Support::New();
-    StreamSupport::AttachNative(mSupport,
-                                mozilla::MakeUnique<StreamSupport>(aRequest));
+    StreamSupport::AttachNative(
+        mSupport, mozilla::MakeUnique<StreamSupport>(mSupport, aRequest));
 
     mStream = java::GeckoInputStream::New(mSupport);
 
