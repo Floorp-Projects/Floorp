ba7381bafc14593c8956202473ceae01af557fa7
Kirk Steuber <ksteuber@mozilla.com>
2019-12-17 10:50:28 +0000

    Bug 1601708 - Return focus to the previously active element when a subdialog closes r=Gijs,dao
    Differential Revision: https://phabricator.services.mozilla.com/D56976

--- browser/components/preferences/in-content/subdialogs.js
+++ browser/components/preferences/in-content/subdialogs.js
@@ -610,6 +610,7 @@ var gSubDialog = {
   _dialogTemplate: null,
   _nextDialogID: 0,
   _preloadDialog: null,
+  _topLevelPrevActiveElement: null,
   get _topDialog() {
     return this._dialogs.length
       ? this._dialogs[this._dialogs.length - 1]
@@ -632,10 +633,13 @@ var gSubDialog = {
       return;
     }
 
-    if (!this._dialogs.length) {
+    if (this._dialogs.length) {
+      this._topDialog._prevActiveElement = document.activeElement;
+    } else {
       // When opening the first dialog, show the dialog stack to make sure
       // the browser binding can be constructed.
       this._dialogStack.hidden = false;
+      this._topLevelPrevActiveElement = document.activeElement;
     }
 
     this._preloadDialog.open(aURL, aFeatures, aParams, aClosingCallback);
@@ -693,16 +697,11 @@ var gSubDialog = {
     }
 
     if (this._topDialog) {
-      fm.moveFocus(
-        this._topDialog._frame.contentWindow,
-        null,
-        fm.MOVEFOCUS_FIRST,
-        fm.FLAG_BYKEY
-      );
+      this._topDialog._prevActiveElement.focus();
       this._topDialog._overlay.setAttribute("topmost", true);
       this._topDialog._addDialogEventListeners();
     } else {
-      fm.moveFocus(window, null, fm.MOVEFOCUS_ROOT, fm.FLAG_BYKEY);
+      this._topLevelPrevActiveElement.focus();
       this._dialogStack.hidden = true;
       this._removeStackEventListeners();
     }
