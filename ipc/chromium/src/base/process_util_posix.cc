db978567e25c098dd2ec6547acbb60030002118c
Thinker Li <thinker.li@gmail.com>
2019-12-17 21:04:39 +0000

    Bug 1604002 - Adapt the way of testing process status with the fork server. r=gsvelto
    Differential Revision: https://phabricator.services.mozilla.com/D57217

--- ipc/chromium/src/base/process_util_posix.cc
+++ ipc/chromium/src/base/process_util_posix.cc
@@ -205,6 +205,21 @@ void CloseSuperfluousFds(void* aCtx, bool (*aShouldPreserve)(void*, int)) {
 }
 
 bool DidProcessCrash(bool* child_exited, ProcessHandle handle) {
+#ifdef MOZ_ENABLE_FORKSERVER
+  // We only know if a process exists, but not if it has crashed.
+  //
+  // Since content processes are not direct children of the chrome
+  // process any more, it is impossible to use |waitpid()| to wait for
+  // them.
+  const int r = kill(handle, 0);
+  if (r < 0 && errno == ESRCH) {
+      if (child_exited) *child_exited = true;
+  } else {
+    if (child_exited) *child_exited = false;
+  }
+
+  return false;
+#else
   int status;
   const int result = HANDLE_EINTR(waitpid(handle, &status, WNOHANG));
   if (result == -1) {
@@ -246,6 +261,7 @@ bool DidProcessCrash(bool* child_exited, ProcessHandle handle) {
   if (WIFEXITED(status)) return WEXITSTATUS(status) != 0;
 
   return false;
+#endif // MOZ_ENABLE_FORKSERVER
 }
 
 void FreeEnvVarsArray::operator()(char** array) {
