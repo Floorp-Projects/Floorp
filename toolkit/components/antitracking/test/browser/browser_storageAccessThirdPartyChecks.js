9507740d26fe0c01358f49524c523f817bb1481a
Ehsan Akhgari <ehsan@mozilla.com>
2019-12-18 19:15:03 +0000

    Bug 1603969 - Part 3: Add an automated test to ensure that after storage access has been granted, the third-party is allowed to load images from tracking origins; r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D57495

---
+++ toolkit/components/antitracking/test/browser/browser_storageAccessThirdPartyChecks.js
@@ -0,0 +1,63 @@
+/* import-globals-from antitracking_head.js */
+
+AntiTracking._createTask({
+  name:
+    "Test that after a storage access grant we have full first-party access",
+  cookieBehavior: BEHAVIOR_REJECT_TRACKER,
+  blockingByContentBlockingRTUI: true,
+  allowList: false,
+  callback: async _ => {
+    /* import-globals-from storageAccessAPIHelpers.js */
+    await noStorageAccessInitially();
+
+    await callRequestStorageAccess();
+
+    const TRACKING_PAGE =
+      "http://another-tracking.example.net/browser/browser/base/content/test/trackingUI/trackingPage.html";
+    async function runChecks(name) {
+      let iframe = document.createElement("iframe");
+      iframe.src = TRACKING_PAGE;
+      document.body.appendChild(iframe);
+      await new Promise(resolve => {
+        iframe.onload = resolve;
+      });
+
+      await SpecialPowers.spawn(iframe, [name], name => {
+        content.postMessage(name, "*");
+      });
+
+      await new Promise(resolve => {
+        onmessage = e => {
+          if (e.data == "done") {
+            resolve();
+          }
+        };
+      });
+    }
+
+    await runChecks("image");
+  },
+  extraPrefs: null,
+  expectedBlockingNotifications:
+    Ci.nsIWebProgressListener.STATE_COOKIES_BLOCKED_TRACKER,
+  runInPrivateWindow: false,
+  iframeSandbox: null,
+  accessRemoval: null,
+  callbackAfterRemoval: null,
+  thirdPartyPage: TEST_3RD_PARTY_PAGE_HTTP,
+  errorMessageDomains: [
+    "http://tracking.example.org",
+    "http://tracking.example.org",
+    "http://tracking.example.org",
+    "http://tracking.example.org",
+    "http://tracking.example.org",
+  ],
+});
+
+add_task(async _ => {
+  await new Promise(resolve => {
+    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value =>
+      resolve()
+    );
+  });
+});
