f42eefffd413e8ccbabfb0d6dfa3977dbee18fcb
Mats Palmgren <mats@mozilla.com>
2019-12-18 01:41:20 +0000

    Bug 1604668 - Remove the eExcludesIgnorableWhitespace IsFrameOfType bit.  r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D57569

--- layout/xul/nsBoxFrame.h
+++ layout/xul/nsBoxFrame.h
@@ -117,7 +117,7 @@ class nsBoxFrame : public nsContainerFrame {
     // NS_UNCONSTRAINEDSIZE wide.)
     return nsContainerFrame::IsFrameOfType(
         aFlags & ~(nsIFrame::eReplaced | nsIFrame::eReplacedContainsBlock |
-                   eXULBox | nsIFrame::eExcludesIgnorableWhitespace));
+                   eXULBox));
   }
 
 #ifdef DEBUG_FRAME_DUMP
