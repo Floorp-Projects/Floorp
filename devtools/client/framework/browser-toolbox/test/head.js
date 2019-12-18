4275691a74e19e939ed6b5b44d61bb0ef35819b8
Julian Descottes <jdescottes@mozilla.com>
2019-12-18 18:28:09 +0000

    Bug 1603209 - Add a test to expand remote frames in the markup view from the Omniscient Browser Toolbox r=ochameau
    Depends on D56347
    
    Differential Revision: https://phabricator.services.mozilla.com/D56866
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/framework/browser-toolbox/test/head.js
+++ devtools/client/framework/browser-toolbox/test/head.js
@@ -15,3 +15,12 @@ Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/devtools/client/framework/browser-toolbox/test/helpers-browser-toolbox.js",
   this
 );
+
+async function selectNodeFront(inspector, walker, selector, rootNode) {
+  rootNode = rootNode || walker.rootNode;
+  const nodeFront = await walker.querySelector(rootNode, selector);
+  const updated = inspector.once("inspector-updated");
+  inspector.selection.setNodeFront(nodeFront);
+  await updated;
+  return nodeFront;
+}
