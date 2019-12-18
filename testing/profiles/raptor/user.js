e3afcba11eda826eddfa883b6cfcf3c21b4fb097
Rob Wood <rwood@mozilla.com>
2019-12-18 07:09:13 +0000

    Bug 1603222 - Enable geckodriver debug logging by default in CI for raptor-browsertime tests r=tarek,stephendonner
    Differential Revision: https://phabricator.services.mozilla.com/D57559

--- testing/profiles/raptor/user.js
+++ testing/profiles/raptor/user.js
@@ -25,3 +25,6 @@ user_pref("toolkit.content-background-hang-monitor.disabled", true);
 // disable async stacks to match release builds
 // https://developer.mozilla.org/en-US/docs/Mozilla/Benchmarking#Async_Stacks
 user_pref('javascript.options.asyncstack', false);
+
+// enable marionette trace logging
+user_pref('marionette.log.level', 'Trace');
