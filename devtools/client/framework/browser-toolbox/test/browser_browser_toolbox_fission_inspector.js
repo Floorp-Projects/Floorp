b165e88aa468a5e0f774b9ca0887d4b3d5073042
Narcis Beleuzu <nbeleuzu@mozilla.com>
2019-12-18 18:12:47 +0200

    Backed out 3 changesets (bug 1603209) for dt leaks at mozilla::net::AddrInfo::AddrInfo . CLOSED TREE
    Backed out changeset ef521f6b57ea (bug 1603209)
    Backed out changeset 56392b65466b (bug 1603209)
    Backed out changeset 10769c72ba39 (bug 1603209)

--- devtools/client/framework/browser-toolbox/test/browser_browser_toolbox_fission_inspector.js
+++ devtools/client/framework/browser-toolbox/test/browser_browser_toolbox_fission_inspector.js
@@ -18,9 +18,7 @@ add_task(async function() {
   const ToolboxTask = await initBrowserToolboxTask({
     enableBrowserToolboxFission: true,
   });
-  await ToolboxTask.importFunctions({
-    selectNodeFront,
-  });
+  await ToolboxTask.importFunctions({});
 
   const tab = await addTab(
     `data:text/html,<div id="my-div" style="color: red">Foo</div>`
@@ -36,14 +34,20 @@ add_task(async function() {
     inspector.sidebar.select("computedview");
     await onSidebarSelect;
 
-    const browser = await selectNodeFront(
-      inspector,
+    async function select(walker, selector) {
+      const nodeFront = await walker.querySelector(walker.rootNode, selector);
+      const updated = inspector.once("inspector-updated");
+      inspector.selection.setNodeFront(nodeFront);
+      await updated;
+      return nodeFront;
+    }
+    const browser = await select(
       inspector.walker,
       'browser[remote="true"][test-tab]'
     );
     const browserTarget = await browser.connectToRemoteFrame();
     const walker = (await browserTarget.getFront("inspector")).walker;
-    await selectNodeFront(inspector, walker, "#my-div");
+    await select(walker, "#my-div");
 
     const view = inspector.getPanel("computedview").computedView;
     function getProperty(name) {
