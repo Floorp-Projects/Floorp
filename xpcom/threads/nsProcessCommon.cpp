a0c0d3821453291f23e3e259cd29c4e548307d8d
Toshihito Kikuchi <tkikuchi@mozilla.com>
2019-12-19 15:39:02 +0000

    Bug 1601905 - Add quotes for a path including whitespaces in ShellExecuteWithIFile.  r=aklotz
    A fix for Bug 1588975 replaced the call to `LaunchWithIProcess` in
    `nsMIMEInfoWin::LaunchWithFile` with the call to `ShellExecuteWithIFile` to use
    `mozilla::ShellExecuteByExplorer`.  This caused a regression that if a filepath
    contains whitespaces, we pass it to a target application without quoting it
    while the old codepath `ShellExecuteWithIFile` quoted a path accordingly in
    `assembleCmdLine`.
    
    A proposed fix is to quote a path in the same way as `assembleCmdLine` does.
    This patch also moves `assembleCmdLine` to an exported header so that we can add
    a unittest to cover both of `assembleCmdLine` and a new function
    `assembleSingleArgument.  It would be better to refactor these functions in the
    future because many lines are duplicated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56646

--- xpcom/threads/nsProcessCommon.cpp
+++ xpcom/threads/nsProcessCommon.cpp
@@ -33,6 +33,7 @@
 #  include "nsString.h"
 #  include "nsLiteralString.h"
 #  include "nsReadableUtils.h"
+#  include "mozilla/AssembleCmdLine.h"
 #  include "mozilla/UniquePtrExtensions.h"
 #else
 #  ifdef XP_MACOSX
@@ -108,117 +109,6 @@ nsProcess::Init(nsIFile* aExecutable) {
 
   return rv;
 }
-
-#if defined(XP_WIN)
-// Out param `aWideCmdLine` must be free()d by the caller.
-static int assembleCmdLine(char* const* aArgv, wchar_t** aWideCmdLine,
-                           UINT aCodePage) {
-  char* const* arg;
-  char* p;
-  char* q;
-  char* cmdLine;
-  int cmdLineSize;
-  int numBackslashes;
-  int i;
-  int argNeedQuotes;
-
-  /*
-   * Find out how large the command line buffer should be.
-   */
-  cmdLineSize = 0;
-  for (arg = aArgv; *arg; ++arg) {
-    /*
-     * \ and " need to be escaped by a \.  In the worst case,
-     * every character is a \ or ", so the string of length
-     * may double.  If we quote an argument, that needs two ".
-     * Finally, we need a space between arguments, and
-     * a null byte at the end of command line.
-     */
-    cmdLineSize += 2 * strlen(*arg) /* \ and " need to be escaped */
-                   + 2              /* we quote every argument */
-                   + 1;             /* space in between, or final null */
-  }
-  p = cmdLine = (char*)malloc(cmdLineSize * sizeof(char));
-  if (!p) {
-    return -1;
-  }
-
-  for (arg = aArgv; *arg; ++arg) {
-    /* Add a space to separates the arguments */
-    if (arg != aArgv) {
-      *p++ = ' ';
-    }
-    q = *arg;
-    numBackslashes = 0;
-    argNeedQuotes = 0;
-
-    /* If the argument contains white space, it needs to be quoted. */
-    if (strpbrk(*arg, " \f\n\r\t\v")) {
-      argNeedQuotes = 1;
-    }
-
-    if (argNeedQuotes) {
-      *p++ = '"';
-    }
-    while (*q) {
-      if (*q == '\\') {
-        numBackslashes++;
-        q++;
-      } else if (*q == '"') {
-        if (numBackslashes) {
-          /*
-           * Double the backslashes since they are followed
-           * by a quote
-           */
-          for (i = 0; i < 2 * numBackslashes; i++) {
-            *p++ = '\\';
-          }
-          numBackslashes = 0;
-        }
-        /* To escape the quote */
-        *p++ = '\\';
-        *p++ = *q++;
-      } else {
-        if (numBackslashes) {
-          /*
-           * Backslashes are not followed by a quote, so
-           * don't need to double the backslashes.
-           */
-          for (i = 0; i < numBackslashes; i++) {
-            *p++ = '\\';
-          }
-          numBackslashes = 0;
-        }
-        *p++ = *q++;
-      }
-    }
-
-    /* Now we are at the end of this argument */
-    if (numBackslashes) {
-      /*
-       * Double the backslashes if we have a quote string
-       * delimiter at the end.
-       */
-      if (argNeedQuotes) {
-        numBackslashes *= 2;
-      }
-      for (i = 0; i < numBackslashes; i++) {
-        *p++ = '\\';
-      }
-    }
-    if (argNeedQuotes) {
-      *p++ = '"';
-    }
-  }
-
-  *p = '\0';
-  int32_t numChars = MultiByteToWideChar(aCodePage, 0, cmdLine, -1, nullptr, 0);
-  *aWideCmdLine = (wchar_t*)malloc(numChars * sizeof(wchar_t));
-  MultiByteToWideChar(aCodePage, 0, cmdLine, -1, *aWideCmdLine, numChars);
-  free(cmdLine);
-  return 0;
-}
-#endif
 
 void nsProcess::Monitor(void* aArg) {
   RefPtr<nsProcess> process = dont_AddRef(static_cast<nsProcess*>(aArg));
