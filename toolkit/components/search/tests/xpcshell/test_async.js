ca00535cfbed947a2d048baa669f45f8578d2148
Mark Banner <standard8@mozilla.com>
2019-12-19 21:57:36 +0000

    Bug 1604929 - Move search's data/search-extensions to a slightly more descriptive and generic name. r=mikedeboer
    Also drops the bug645970 naming as we don't really need to reference that bug number any more.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57680
    
    --HG--
    rename : toolkit/components/search/tests/xpcshell/data/search-extensions/bug645970/manifest.json => toolkit/components/search/tests/xpcshell/simple-engines/basic/manifest.json
    rename : toolkit/components/search/tests/xpcshell/data/search-extensions/engines.json => toolkit/components/search/tests/xpcshell/simple-engines/engines.json
    rename : toolkit/components/search/tests/xpcshell/data/search-extensions/hidden/manifest.json => toolkit/components/search/tests/xpcshell/simple-engines/hidden/manifest.json
    rename : toolkit/components/search/tests/xpcshell/data/search-extensions/list.json => toolkit/components/search/tests/xpcshell/simple-engines/list.json
    extra : moz-landing-system : lando

--- toolkit/components/search/tests/xpcshell/test_async.js
+++ toolkit/components/search/tests/xpcshell/test_async.js
@@ -3,7 +3,7 @@
 
 add_task(async function setup() {
   await AddonTestUtils.promiseStartupManager();
-  await useTestEngines("data", "search-extensions");
+  await useTestEngines("simple-engines");
 });
 
 add_task(async function test_async() {
@@ -18,7 +18,7 @@ add_task(async function test_async() {
   Assert.equal(engines.length, 1);
 
   // test jar engine is loaded ok.
-  let engine = Services.search.getEngineByName("bug645970");
+  let engine = Services.search.getEngineByName("basic");
   Assert.notEqual(engine, null);
 
   // Check the hidden engine is not loaded.
