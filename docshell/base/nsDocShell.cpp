8d6ea4625d3a550ea12268b7df13177affa0b984
Kris Maglione <maglione.k@gmail.com>
2019-12-19 22:05:19 +0000

    Bug 1588259: Part 2 - Prevent URI loads in partially-initialized BrowsingContexts. r=smaug
    When we open a new window from a content process, we create a nested event
    loop to wait for it to be initialized by the parent. The problem with this is
    that the OpenWindow code which calls the window provider expects the window to
    be in-process and uninitialized, so that it can load its own initial URI into
    it, and correctly fulfil the spec-codified contract of window.open(). If
    another caller initiates a load in the new window during the nested event
    loop, those invariants are broken, and any manner of undefined behavior can
    occur.
    
    This patch adds a new flag to the BrowsingContext, marking it as uninitialized
    until the end of the nested event loop, and blocking any attempts to load a
    new URI into it in the meantime.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57667

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -8785,6 +8785,9 @@ nsresult nsDocShell::InternalLoad(nsDocShellLoadState* aLoadState,
   if (mUseStrictSecurityChecks && !aLoadState->TriggeringPrincipal()) {
     return NS_ERROR_FAILURE;
   }
+  if (mBrowsingContext->PendingInitialization()) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
 
   mOriginalUriString.Truncate();
 
