0162ffca4b9548929fb9d7555cefcb001a74795b
Andrew McCreight <continuation@gmail.com>
2019-12-19 21:17:11 +0000

    Bug 1604609 - Don't send large window actor messages. r=jdai
    IPC crashes if you try to send large messages. The message manager
    already drops messages that are too big (and used to collect telemetry
    for which messages were being dropped).
    
    I think that some message manager messages have been ported over to JS
    window actors and started crashing on beta, so let's just give the
    same behavior for JS window actors as we do for the message manager.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57583
    
    --HG--
    extra : moz-landing-system : lando

--- dom/ipc/JSWindowActor.h
+++ dom/ipc/JSWindowActor.h
@@ -71,6 +71,11 @@ class JSWindowActor : public nsISupports, public nsWrapperCache {
                               ipc::StructuredCloneData&& aStack,
                               ErrorResult& aRv) = 0;
 
+  // Check if a message is so large that IPC will probably crash if we try to
+  // send it. If it is too large, record telemetry about the message.
+  static bool AllowMessage(const JSWindowActorMessageMeta& aMetadata,
+                           size_t aDataLength);
+
   virtual ~JSWindowActor() = default;
 
   void SetName(const nsAString& aName);
