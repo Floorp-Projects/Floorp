c30f9e541c0bec702a8f4cf83f73e6abc9817834
Alexander Surkov <surkov.alexander@gmail.com>
2019-12-19 02:35:57 +0000

    Bug 1597765 - support auto scroll in out of process frames r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D54596

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -6,34 +6,11 @@
 /* eslint-env mozilla/frame-script */
 /* eslint no-unused-vars: ["error", {args: "none"}] */
 
+/* exported Services */
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
 var { ActorManagerChild } = ChromeUtils.import(
   "resource://gre/modules/ActorManagerChild.jsm"
 );
 
 ActorManagerChild.attach(this);
-
-ChromeUtils.defineModuleGetter(
-  this,
-  "AutoScrollController",
-  "resource://gre/modules/AutoScrollController.jsm"
-);
-
-var global = this;
-
-var AutoScrollListener = {
-  handleEvent(event) {
-    if (event.isTrusted & !event.defaultPrevented && event.button == 1) {
-      if (!this._controller) {
-        this._controller = new AutoScrollController(global);
-      }
-      this._controller.handleEvent(event);
-    }
-  },
-};
-Services.els.addSystemEventListener(
-  global,
-  "mousedown",
-  AutoScrollListener,
-  true
-);
