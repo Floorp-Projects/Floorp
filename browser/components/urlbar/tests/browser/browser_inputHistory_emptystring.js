57f883308c1ce441648a12bdac2d96d2eb6df9e3
Dão Gottwald <dao@mozilla.com>
2019-12-19 14:13:22 +0000

    Bug 1605094 - Prepare urlbar tests for browser.urlbar.openViewOnFocus=true. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D57788

--- browser/components/urlbar/tests/browser/browser_inputHistory_emptystring.js
+++ browser/components/urlbar/tests/browser/browser_inputHistory_emptystring.js
@@ -78,16 +78,22 @@ add_task(async function test_history_no_search_terms() {
         return true;
       },
       () => {
+        if (gURLBar.dropmarker.hidden) {
+          return false;
+        }
         info("Test opening panel with history dropmarker");
-        UrlbarTestUtils.getDropMarker(window).click();
+        gURLBar.dropmarker.click();
         return true;
       },
       async () => {
+        if (gURLBar.dropmarker.hidden) {
+          return false;
+        }
         info("Test opening panel with history dropmarker on a page");
         let selectedBrowser = gBrowser.selectedBrowser;
         await BrowserTestUtils.loadURI(selectedBrowser, TEST_URL);
         await BrowserTestUtils.browserLoaded(selectedBrowser);
-        UrlbarTestUtils.getDropMarker(window).click();
+        gURLBar.dropmarker.click();
         return true;
       },
       // The following tests must be the last to run because of the pref change.
