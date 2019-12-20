a48ae07d4e1a58e27f06bbbfe6325bc94e4fa258
Matthew Gaudet <mgaudet@mozilla.com>
2019-12-20 16:26:42 +0000

    Bug 1605254 - Move BigIntCreationData to Stencil.h r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D57936

--- js/src/frontend/ParseNode.h
+++ js/src/frontend/ParseNode.h
@@ -13,7 +13,6 @@
 #include "frontend/Stencil.h"
 #include "frontend/Token.h"
 #include "util/Text.h"
-#include "vm/BigIntType.h"
 #include "vm/BytecodeUtil.h"
 #include "vm/JSContext.h"
 #include "vm/Printer.h"
@@ -1524,41 +1523,6 @@ class NumericLiteral : public ParseNode {
   void setValue(double v) { value_ = v; }
 
   void setDecimalPoint(DecimalPoint d) { decimalPoint_ = d; }
-};
-
-// This owns a set of characters guaranteed to parse into a BigInt via
-// ParseBigIntLiteral. Used to avoid allocating the BigInt on the
-// GC heap during parsing.
-class BigIntCreationData {
-  UniqueTwoByteChars buf_;
-  size_t length_ = 0;
-
- public:
-  BigIntCreationData() = default;
-
-  MOZ_MUST_USE bool init(JSContext* cx, const Vector<char16_t, 32>& buf) {
-#ifdef DEBUG
-    // Assert we have no separators; if we have a separator then the algorithm
-    // used in BigInt::literalIsZero will be incorrect.
-    for (char16_t c : buf) {
-      MOZ_ASSERT(c != '_');
-    }
-#endif
-    length_ = buf.length();
-    buf_ = js::DuplicateString(cx, buf.begin(), buf.length());
-    return buf_ != nullptr;
-  }
-
-  BigInt* createBigInt(JSContext* cx) {
-    mozilla::Range<const char16_t> source(buf_.get(), length_);
-
-    return js::ParseBigIntLiteral(cx, source);
-  }
-
-  bool isZero() {
-    mozilla::Range<const char16_t> source(buf_.get(), length_);
-    return js::BigIntLiteralIsZero(source);
-  }
 };
 
 class BigIntLiteral : public ParseNode {
