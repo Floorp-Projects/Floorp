0ed2c652dda207a19707709cb8a318e62e9e8b68
James Graham <james@hoppipolla.co.uk>
2019-12-20 18:35:37 +0000

    Bug 1569559 - Don't remove subtests where a condition is outside the updatable set, r=maja_zf
    Sometimes we see subtests that only appear in certain conditions e.g. if we
    get an early error on nightly but the tests run as expected in stable. In this
    case the wpt sync bot will update the metadata with --full and remove the
    missing subtests. To work around this, check if subtests contain any
    conditions that aren't part of the update set, and if so never
    remove them
    
    Differential Revision: https://phabricator.services.mozilla.com/D57983
    
    --HG--
    extra : moz-landing-system : lando

--- testing/web-platform/tests/tools/wptrunner/wptrunner/wptmanifest/backends/conditional.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/wptmanifest/backends/conditional.py
@@ -382,6 +382,10 @@ class ManifestItem(object):
         for item in self._flatten().iterkeys():
             yield item
 
+    def iter_properties(self):
+        for item in self._data:
+            yield item, self._data[item]
+
     def remove_value(self, key, value):
         if key not in self._data:
             return
