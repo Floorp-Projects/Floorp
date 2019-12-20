e6993ad39faab435b0e7f6e8463b222d6318d541
Steve Fink <sfink@mozilla.com>
2019-12-20 03:35:50 +0000

    Bug 1597005 - Prevent -Werror failure for bogus printf overflow warning in ZydisAPI.cpp with gcc -O0 r=glandium,lth
    Differential Revision: https://phabricator.services.mozilla.com/D55050

--- js/src/zydis/ZydisAPI.cpp
+++ js/src/zydis/ZydisAPI.cpp
@@ -70,7 +70,10 @@ void zydisDisassemble(const uint8_t* code, size_t codeLen,
 
     // Pad out to the limit if necessary
     if (strlen(buffer) < LIMIT) {
-      sprintf(buffer+strlen(buffer), "%*s", int(LIMIT-strlen(buffer)), "");
+      char* cur_end = buffer + strlen(buffer);
+      size_t spaces = LIMIT - strlen(buffer);
+      memset(cur_end, ' ', spaces);
+      cur_end[spaces] = '\0';
     }
 
     // If too long then flush and provide an appropriate indent
@@ -92,4 +95,3 @@ void zydisDisassemble(const uint8_t* code, size_t codeLen,
 #  undef LIMSTR
   }
 }
-
