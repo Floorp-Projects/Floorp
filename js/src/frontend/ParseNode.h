45cf5b11341a5b14c28fe373bfb5bf10e952e6b9
Matthew Gaudet <mgaudet@mozilla.com>
2019-12-20 16:25:16 +0000

    Bug 1604952 - Move RegExpCreationData to Stencil.h r=tcampbell
    As a side effect of removing a now-cylic-dependency, this rehomes
    FunctionSytaxKind.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57903
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/frontend/ParseNode.h
+++ js/src/frontend/ParseNode.h
@@ -10,6 +10,7 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/Variant.h"
 
+#include "frontend/Stencil.h"
 #include "frontend/Token.h"
 #include "util/Text.h"
 #include "vm/BigIntType.h"
@@ -586,7 +587,7 @@ inline bool IsTypeofKind(ParseNodeKind kind) {
 FOR_EACH_PARSENODE_SUBCLASS(DECLARE_CLASS)
 #undef DECLARE_CLASS
 
-enum class FunctionSyntaxKind {
+enum class FunctionSyntaxKind : uint8_t {
   // A non-arrow function expression.
   Expression,
 
@@ -1890,30 +1891,6 @@ class BooleanLiteral : public NullaryNode {
     MOZ_ASSERT_IF(match, node.is<NullaryNode>());
     return match;
   }
-};
-
-// This owns a set of characters, previously syntax checked as a RegExp. Used
-// to avoid allocating the RegExp on the GC heap during parsing.
-class RegExpCreationData {
-  UniquePtr<char16_t[], JS::FreePolicy> buf_;
-  size_t length_ = 0;
-  JS::RegExpFlags flags_;
-
- public:
-  RegExpCreationData() = default;
-
-  MOZ_MUST_USE bool init(JSContext* cx, mozilla::Range<const char16_t> range,
-                         JS::RegExpFlags flags) {
-    length_ = range.length();
-    buf_ = js::DuplicateString(cx, range.begin().get(), range.length());
-    if (!buf_) {
-      return false;
-    }
-    flags_ = flags;
-    return true;
-  }
-
-  RegExpObject* createRegExp(JSContext* cx) const;
 };
 
 class RegExpLiteral : public ParseNode {
