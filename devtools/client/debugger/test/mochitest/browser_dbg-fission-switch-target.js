59577859b05427e9b59208830b83c2736f54f088
Julian Descottes <jdescottes@mozilla.com>
2019-12-23 16:51:26 +0000

    Bug 1572409 - Add a test for Debugger target-switching support r=jlast,ochameau
    Depends on D55671
    
    Differential Revision: https://phabricator.services.mozilla.com/D57806
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/client/debugger/test/mochitest/browser_dbg-fission-switch-target.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+// Test switching for the top-level target.
+
+const PARENT_PROCESS_URI = "about:robots";
+
+add_task(async function() {
+  await pushPref("devtools.target-switching.enabled", true);
+
+  // Start the debugger on a parent process URL
+  const dbg = await initDebuggerWithAbsoluteURL(
+    PARENT_PROCESS_URI,
+    "aboutRobots.js"
+  );
+
+  // Navigate to a content process URL and check that the sources tree updates
+  await navigate(dbg, EXAMPLE_URL + "doc-scripts.html", "simple1.js");
+  is(dbg.selectors.getSourceCount(), 5, "5 sources are loaded.");
+
+  // Check that you can still break after target switching.
+  await selectSource(dbg, "simple1");
+  await addBreakpoint(dbg, "simple1.js", 4);
+  invokeInTab("main");
+  await waitForPaused(dbg);
+  await waitForLoadedSource(dbg, "simple1");
+
+  await dbg.toolbox.closeToolbox();
+});
