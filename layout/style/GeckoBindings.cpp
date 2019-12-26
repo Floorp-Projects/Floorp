4d1924c9fbee5fef1977f6bc089a42373762ef0b
Emilio Cobos Álvarez <emilio@crisal.io>
2019-12-26 22:17:35 +0000

    Bug 1606019 - Remove unused FFI function to refcount nsIReferrerInfo. r=xidorn
    We don't use RefPtr<nsIReferrerInfo> in rust.
    
    Differential Revision: https://phabricator.services.mozilla.com/D58274
    
    --HG--
    extra : moz-landing-system : lando

--- layout/style/GeckoBindings.cpp
+++ layout/style/GeckoBindings.cpp
@@ -1443,12 +1443,6 @@ void Gecko_nsIURI_Debug(nsIURI* aURI, nsCString* aOut) {
 // subclasses have the HasThreadSafeRefCnt bits.
 void Gecko_AddRefnsIURIArbitraryThread(nsIURI* aPtr) { NS_ADDREF(aPtr); }
 void Gecko_ReleasensIURIArbitraryThread(nsIURI* aPtr) { NS_RELEASE(aPtr); }
-void Gecko_AddRefnsIReferrerInfoArbitraryThread(nsIReferrerInfo* aPtr) {
-  NS_ADDREF(aPtr);
-}
-void Gecko_ReleasensIReferrerInfoArbitraryThread(nsIReferrerInfo* aPtr) {
-  NS_RELEASE(aPtr);
-}
 
 void Gecko_nsIReferrerInfo_Debug(nsIReferrerInfo* aReferrerInfo,
                                  nsCString* aOut) {
