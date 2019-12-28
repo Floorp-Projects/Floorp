378a026b323798c0c4c70df260c9a2d1153ba7a7
Dzmitry Malyshau <dmalyshau@mozilla.com>
2019-12-28 02:44:59 +0000

    Bug 1596126 - Github synchronization for gfx/wgpu r=Callek,tomprince
    This change introduces a "github-sync" component into tools,
    which aims to support synchronizing both wgpu and WebRender with github.
    ~~It also features a "cargo test" job for standalone wgpu (bug  1596127)~~
    
    The code is ported from "gfx/wr/scripts/wrupdater" folder. Changes are:
      1. remove explicit WR parts and make them configurable by command line params
      2. detect "mozilla-xxx" tags and use them in addition to the commits
    
    As a follow up, wrupdater will be removed in favor of github-sync.
    
    Status:
      - [x] get the CI test job working
      - [x] get @kats to fork "wgpu" github for "moz-gfx" bot
      - [x] remove the wgpu testing CI job (into separate PR)
      - [x] create new secret and reference it
    
    Differential Revision: https://phabricator.services.mozilla.com/D57057

---
+++ taskcluster/taskgraph/transforms/github_sync.py
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import absolute_import, print_function, unicode_literals
+
+from taskgraph.transforms.base import TransformSequence
+
+
+transforms = TransformSequence()
+
+
+@transforms.add
+def sync_github(config, tasks):
+    """Do transforms specific to github-sync tasks.
+    """
+    for task in tasks:
+        # Add the secret to the scopes, only in m-c.
+        # Doing this on any other tree will result in decision task failure
+        # because m-c is the only one allowed to have that scope.
+        secret = task['secret']
+        if config.params['project'] == 'mozilla-central':
+            task.setdefault('scopes', [])
+            task['scopes'].append('secrets:get:' + secret)
+            task['worker'].setdefault('env', {})['GITHUB_SECRET'] = secret
+        del task['secret']
+        yield task
