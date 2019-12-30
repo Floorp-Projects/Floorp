05ab98736e6fe54efcd7463991e1d4d26d4a4238
Ricky Stewart <rstewart@mozilla.com>
2019-12-30 19:24:01 +0000

    Bug 1605708 - compiler-rt wasm have a different version as clang compiler r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D58166

--- taskcluster/scripts/misc/build-wasi-sysroot.sh
+++ taskcluster/scripts/misc/build-wasi-sysroot.sh
@@ -5,7 +5,8 @@ export PATH="$MOZ_FETCHES_DIR/gcc/bin:$MOZ_FETCHES_DIR/binutils/bin:$PATH"
 export LD_LIBRARY_PATH="$MOZ_FETCHES_DIR/gcc/lib64/:$LD_LIBRARY_PATH"
 cd $MOZ_FETCHES_DIR/wasi-sdk
 PREFIX=$MOZ_FETCHES_DIR/wasi-sdk/wasi-sysroot
-make PREFIX=$PREFIX
+LLVM_PROJ_DIR=$MOZ_FETCHES_DIR/llvm-project
+make LLVM_PROJ_DIR=$LLVM_PROJ_DIR PREFIX=$PREFIX
 # Put a tarball in the artifacts dir
 mkdir -p $UPLOAD_DIR
 tar -C $MOZ_FETCHES_DIR/wasi-sdk/ -caf $UPLOAD_DIR/wasi-sysroot.tar.xz wasi-sysroot
