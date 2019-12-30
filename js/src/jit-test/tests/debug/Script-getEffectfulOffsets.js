274486cc3fcc1c2f36438a818522b22b50552fc1
Brian Hackett <bhackett1024@gmail.com>
2019-12-30 22:49:35 +0000

    Bug 1604435 - Include effectful offsets preceding the script's main offset, r=jimb.
    Differential Revision: https://phabricator.services.mozilla.com/D57975

--- js/src/jit-test/tests/debug/Script-getEffectfulOffsets.js
+++ js/src/jit-test/tests/debug/Script-getEffectfulOffsets.js
@@ -20,13 +20,14 @@ ${code}
   assertEq(numEffectfulOperations, expected);
 }
 
-test("a.f = 0", 1);
-test("a.f++", 1);
-test("return a.f", 0);
-test("a[b] = 0", 1);
-test("a[b]++", 1);
-test("return a[b]", 0);
-test("a = 0", 0);
-test("d = 0", 1);
-test("with (a) { b = 0; }", 1);
-test("let o = {}; ({x: o.x} = { x: 10 })", 1);
+test("a.f = 0", 2);
+test("a.f++", 2);
+test("return a.f", 1);
+test("a[b] = 0", 2);
+test("a[b]++", 2);
+test("return a[b]", 1);
+test("a = 0", 1);
+test("d = 0", 2);
+test("with (a) { b = 0; }", 4);
+test("let o = {}; ({x: o.x} = { x: 10 })", 2);
+test("var x", 1);
