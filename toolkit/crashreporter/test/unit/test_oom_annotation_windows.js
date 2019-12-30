4a8884f4ac1a1d68d56df9dd8c11842d11413ccd
Arthur Iakab <aiakab@mozilla.com>
2019-12-30 17:28:31 +0200

    Backed out 3 changesets (bug 1587722) for causing xpcshell failures on test_crashreporter_crash.js  CLOSED TREE
    Backed out changeset 6aad53d9e447 (bug 1587722)
    Backed out changeset 7d664635c1dc (bug 1587722)
    Backed out changeset c9dbcfa6a938 (bug 1587722)

---
+++ toolkit/crashreporter/test/unit/test_oom_annotation_windows.js
@@ -0,0 +1,28 @@
+add_task(async function run_test() {
+  if (!("@mozilla.org/toolkit/crash-reporter;1" in Cc)) {
+    dump(
+      "INFO | test_crash_oom.js | Can't test crashreporter in a non-libxul build.\n"
+    );
+    return;
+  }
+
+  await do_crash(
+    function() {
+      crashType = CrashTestUtils.CRASH_OOM;
+      crashReporter.annotateCrashReport("TestKey", "Yes");
+    },
+    function(mdump, extra) {
+      Assert.equal(extra.TestKey, "Yes");
+      Assert.ok("OOMAllocationSize" in extra);
+      Assert.ok(Number(extra.OOMAllocationSize) > 0);
+      Assert.ok("SystemMemoryUsePercentage" in extra);
+      Assert.ok("TotalVirtualMemory" in extra);
+      Assert.ok("AvailableVirtualMemory" in extra);
+      Assert.ok("TotalPageFile" in extra);
+      Assert.ok("AvailablePageFile" in extra);
+      Assert.ok("TotalPhysicalMemory" in extra);
+      Assert.ok("AvailablePhysicalMemory" in extra);
+    },
+    true
+  );
+});
