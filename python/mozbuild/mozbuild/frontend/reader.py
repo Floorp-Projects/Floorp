42edd7f6805f10e96ee11df324a7631720bde685
Ricky Stewart <rstewart@mozilla.com>
2019-12-30 18:38:46 +0000

    Bug 1606008 - mach build-backend takes a long time to recursively list all files called "config.status" r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D58314

--- python/mozbuild/mozbuild/frontend/reader.py
+++ python/mozbuild/mozbuild/frontend/reader.py
@@ -852,7 +852,7 @@ class BuildReader(object):
 
         # Also ignore any other directories that could be objdirs, they don't
         # necessarily start with the string 'obj'.
-        for path, f in self._relevant_mozbuild_finder.find('**/config.status'):
+        for path, f in self._relevant_mozbuild_finder.find('*/config.status'):
             self._relevant_mozbuild_finder.ignore.add(os.path.dirname(path))
 
         max_workers = cpu_count()
