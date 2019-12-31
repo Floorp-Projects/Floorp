5e3f90efe3a66b514dcde040eb57849d64cf49e4
Tom Schuster <evilpies@gmail.com>
2019-12-31 21:12:52 +0000

    Bug 1605854 - Remove uneval/toSource from WebExtension tests. r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D58315
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/extensions/test/xpcshell/test_ext_contentscript_triggeringPrincipal.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_contentscript_triggeringPrincipal.js
@@ -685,10 +685,13 @@ function injectElements(tests, baseOpts) {
 
         for (let test of tests) {
           let { elem, srcElem, src } = createElement(test, opts);
+
           document.body.appendChild(elem);
           window.wrappedJSObject.elem = srcElem;
           window.wrappedJSObject.eval(
-            `elem.setAttribute(${uneval(test.srcAttr)}, ${uneval(src)})`
+            `elem.setAttribute(${JSON.stringify(
+              test.srcAttr
+            )}, ${JSON.stringify(src)})`
           );
         }
 
@@ -714,7 +717,9 @@ function injectElements(tests, baseOpts) {
           let { elem, srcElem, src } = createElement(test, opts);
           window.wrappedJSObject.elem = srcElem;
           window.wrappedJSObject.eval(
-            `elem.setAttribute(${uneval(test.srcAttr)}, ${uneval(src)})`
+            `elem.setAttribute(${JSON.stringify(
+              test.srcAttr
+            )}, ${JSON.stringify(src)})`
           );
           document.body.appendChild(elem);
         }
@@ -732,7 +737,9 @@ function injectElements(tests, baseOpts) {
           document.body.appendChild(elem);
           window.wrappedJSObject.elem = srcElem;
           window.wrappedJSObject.eval(
-            `elem.setAttribute(${uneval(test.srcAttr)}, ${uneval(src)})`
+            `elem.setAttribute(${JSON.stringify(
+              test.srcAttr
+            )}, ${JSON.stringify(src)})`
           );
         }
       }
