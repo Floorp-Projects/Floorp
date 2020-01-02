2dae2fb839bcbcc874bb159a88bf80979ebbbf59
Gerald Squelart <gsquelart@mozilla.com>
2020-01-02 12:21:58 +0000

    Bug 1600644 - Don't count ActivePS::mProfileBuffer's own size twice - r=canaltinova
    In bug 1587332, ActivePS::mProfileBuffer was changed from
    `const UniquePtr<ProfileBuffer>` to just `ProfileBuffer`, so its own object size
    is now counted as part of `sizeof(ActivePS)`, and we should only count its
    dependencies with `SizeOfExcludingThis`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D58288

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -780,7 +780,7 @@ class ActivePS {
 
     size_t n = aMallocSizeOf(sInstance);
 
-    n += sInstance->mProfileBuffer.SizeOfIncludingThis(aMallocSizeOf);
+    n += sInstance->mProfileBuffer.SizeOfExcludingThis(aMallocSizeOf);
 
     // Measurement of the following members may be added later if DMD finds it
     // is worthwhile:
