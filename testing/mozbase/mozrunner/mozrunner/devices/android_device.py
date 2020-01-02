15828c8a0b62b2fbda0909d297d7f6b724a1c071
Geoff Brown <gbrown@mozilla.com>
2020-01-02 18:52:53 +0000

    Bug 1598920 - Enable android_device.py to find file paths regardless of cwd; r=jgraham
    Join <topsrcdir> with MANIFEST_PATH in android_device.py. I was concerned about TOOLTOOL_PATH
    also, but that is already consistently joined with <topsrcdir> before use. I think these
    are the only cases that needed attention.
    
    Differential Revision: https://phabricator.services.mozilla.com/D58548

--- testing/mozbase/mozrunner/mozrunner/devices/android_device.py
+++ testing/mozbase/mozrunner/mozrunner/devices/android_device.py
@@ -102,7 +102,8 @@ def _install_host_utils(build_obj):
     installed = False
     host_platform = _get_host_platform()
     if host_platform:
-        path = os.path.join(MANIFEST_PATH, host_platform, 'hostutils.manifest')
+        path = os.path.join(build_obj.topsrcdir, MANIFEST_PATH)
+        path = os.path.join(path, host_platform, 'hostutils.manifest')
         _get_tooltool_manifest(build_obj.substs, path, EMULATOR_HOME_DIR,
                                'releng.manifest')
         _tooltool_fetch(build_obj.substs)
@@ -143,7 +144,8 @@ def _maybe_update_host_utils(build_obj):
     if host_platform:
         # Extract tooltool file name from manifest, something like:
         #     "filename": "host-utils-58.0a1.en-US-linux-x86_64.tar.gz",
-        manifest_path = os.path.join(MANIFEST_PATH, host_platform, 'hostutils.manifest')
+        path = os.path.join(build_obj.topsrcdir, MANIFEST_PATH)
+        manifest_path = os.path.join(path, host_platform, 'hostutils.manifest')
         with open(manifest_path, 'r') as f:
             for line in f.readlines():
                 m = re.search('.*\"(host-utils-.*)\"', line)
