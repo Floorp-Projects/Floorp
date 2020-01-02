22251f3cbf722530c3c9177edf89e57fdd7a2dd4
chujun <chujunlu@hotmail.com>
2020-01-02 16:09:49 +0000

    Bug 1596114 - Refactor server unit tests part3 r=jlast,bhackett
    Remove `threadFrontTestFinished`
    
    Differential Revision: https://phabricator.services.mozilla.com/D58345

--- devtools/server/tests/unit/test_blackboxing-05.js
+++ devtools/server/tests/unit/test_blackboxing-05.js
@@ -8,84 +8,66 @@
  * Test exceptions inside black boxed sources.
  */
 
-var gDebuggee;
-var gThreadFront;
-
 add_task(
-  threadFrontTest(
-    async ({ threadFront, debuggee }) => {
-      gThreadFront = threadFront;
-      gDebuggee = debuggee;
-      test_black_box();
-    },
-    { waitForFinish: true }
-  )
+  threadFrontTest(async ({ threadFront, debuggee }) => {
+    await executeOnNextTickAndWaitForPause(
+      () => evalCode(debuggee),
+      threadFront
+    );
+
+    const { error } = await threadFront.getSources();
+    Assert.ok(!error, "Should not get an error: " + error);
+
+    const sourceFront = await getSource(threadFront, BLACK_BOXED_URL);
+    await blackBox(sourceFront);
+    threadFront.pauseOnExceptions(true, false);
+
+    threadFront.resume();
+    const packet = await waitForPause(threadFront);
+    const source = await getSourceById(threadFront, packet.frame.where.actor);
+
+    Assert.equal(
+      source.url,
+      SOURCE_URL,
+      "We shouldn't pause while in the black boxed source."
+    );
+    await threadFront.resume();
+  })
 );
 
 const BLACK_BOXED_URL = "http://example.com/blackboxme.js";
 const SOURCE_URL = "http://example.com/source.js";
 
-function test_black_box() {
-  gThreadFront.once("paused", test_black_box_exception);
-
+function evalCode(debuggee) {
   /* eslint-disable no-multi-spaces, no-unreachable, no-undef */
+  // prettier-ignore
   Cu.evalInSandbox(
     "" +
-      function doStuff(k) {
-        // line 1
+      function doStuff(k) {           // line 1
         throw new Error("error msg"); // line 2
-        k(100); // line 3
-      }, // line 4
-    gDebuggee,
+        k(100);                       // line 3
+      },                              // line 4
+    debuggee,
     "1.8",
     BLACK_BOXED_URL,
     1
   );
-
+  // prettier-ignore
   Cu.evalInSandbox(
     "" +
-    function runTest() {
-      // line 1
-      doStuff(
-        // line 2
-        function(n) {
-          // line 3
-          debugger; // line 4
-        } // line 5
-      ); // line 6
-    } + // line 7
-    "\ndebugger;\n" + // line 8
+    function runTest() { // line 1
+      doStuff(           // line 2
+        function(n) {    // line 3
+          debugger;      // line 4
+        }                // line 5
+      );                 // line 6
+    } +                  // line 7
+    "\ndebugger;\n" +    // line 8
       "try { runTest() } catch (ex) { }", // line 9
-    gDebuggee,
+    debuggee,
     "1.8",
     SOURCE_URL,
     1
   );
   /* eslint-enable no-multi-spaces, no-unreachable, no-undef */
-}
-
-function test_black_box_exception() {
-  gThreadFront.getSources().then(async function({ error, sources }) {
-    Assert.ok(!error, "Should not get an error: " + error);
-    const sourceFront = await getSource(gThreadFront, BLACK_BOXED_URL);
-    await blackBox(sourceFront);
-    gThreadFront.pauseOnExceptions(true, false);
-
-    gThreadFront.once("paused", async function(packet) {
-      const source = await getSourceById(
-        gThreadFront,
-        packet.frame.where.actor
-      );
-
-      Assert.equal(
-        source.url,
-        SOURCE_URL,
-        "We shouldn't pause while in the black boxed source."
-      );
-      await gThreadFront.resume();
-      threadFrontTestFinished();
-    });
-
-    gThreadFront.resume();
-  });
 }
--- devtools/server/tests/unit/test_conditional_breakpoint-02.js
+++ devtools/server/tests/unit/test_conditional_breakpoint-02.js
@@ -8,53 +8,47 @@
  * Check conditional breakpoint when condition evaluates to false.
  */
 
-var gDebuggee;
-var gThreadFront;
-
 add_task(
-  threadFrontTest(
-    async ({ threadFront, debuggee }) => {
-      gThreadFront = threadFront;
-      gDebuggee = debuggee;
-      test_simple_breakpoint();
-    },
-    { waitForFinish: true }
-  )
-);
+  threadFrontTest(async ({ threadFront, debuggee }) => {
+    const packet1 = await executeOnNextTickAndWaitForPause(
+      () => evalCode(debuggee),
+      threadFront
+    );
 
-function test_simple_breakpoint() {
-  gThreadFront.once("paused", async function(packet) {
-    const source = await getSourceById(gThreadFront, packet.frame.where.actor);
+    const source = await getSourceById(threadFront, packet1.frame.where.actor);
     const location1 = { sourceUrl: source.url, line: 3 };
-    gThreadFront.setBreakpoint(location1, { condition: "a === 2" });
+    threadFront.setBreakpoint(location1, { condition: "a === 2" });
+
     const location2 = { sourceUrl: source.url, line: 4 };
-    gThreadFront.setBreakpoint(location2, { condition: "a === 1" });
-    gThreadFront.once("paused", function(packet) {
-      // Check the return value.
-      Assert.equal(packet.why.type, "breakpoint");
-      Assert.equal(packet.frame.where.line, 4);
-
-      // Remove the breakpoint.
-      gThreadFront.removeBreakpoint(location2);
-
-      gThreadFront.resume().then(function() {
-        threadFrontTestFinished();
-      });
-    });
+    threadFront.setBreakpoint(location2, { condition: "a === 1" });
 
     // Continue until the breakpoint is hit.
-    gThreadFront.resume();
-  });
+    threadFront.resume();
+    const packet2 = await waitForPause(threadFront);
 
+    // Check the return value.
+    Assert.equal(packet2.why.type, "breakpoint");
+    Assert.equal(packet2.frame.where.line, 4);
+
+    // Remove the breakpoint.
+    await threadFront.removeBreakpoint(location2);
+
+    threadFront.resume();
+  })
+);
+
+function evalCode(debuggee) {
   /* eslint-disable */
-  Cu.evalInSandbox("debugger;\n" +   // 1
-                   "var a = 1;\n" +  // 2
-                   "var b = 2;\n" +  // 3
-                   "b++;" +          // 4
-                   "debugger;",      // 5
-                   gDebuggee,
-                   "1.8",
-                   "test.js",
-                   1);
+  Cu.evalInSandbox(
+    "debugger;\n" + // line 1
+    "var a = 1;\n" + // line 2
+    "var b = 2;\n" + // line 3
+    "b++;" + // line 4
+      "debugger;", // line 5
+    debuggee,
+    "1.8",
+    "test.js",
+    1
+  );
   /* eslint-enable */
 }
