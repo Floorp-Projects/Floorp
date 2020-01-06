9b042f35ee5fda7d634598fd428e3901f4e803a7
Jan Odvarko <odvarko@gmail.com>
2020-01-06 08:13:46 +0000

    Bug 1605576 - Set javascript.options.asyncstack in browser_webconsole_stubs_page_error.js and browser_webconsole_worker_error.js r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D58607
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/test/browser/browser_webconsole_stubs_page_error.js
+++ devtools/client/webconsole/test/browser/browser_webconsole_stubs_page_error.js
@@ -16,6 +16,8 @@ const TEST_URI =
 const STUB_FILE = "pageError.js";
 
 add_task(async function() {
+  await pushPref("javascript.options.asyncstack", true);
+
   const isStubsUpdate = env.get(STUBS_UPDATE_ENV) == "true";
   info(`${isStubsUpdate ? "Update" : "Check"} ${STUB_FILE}`);
 
