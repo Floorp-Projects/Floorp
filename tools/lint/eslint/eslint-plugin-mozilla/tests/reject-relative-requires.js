3b345a689e6a720f0a5dd5057863278ab2020690
Julian Descottes <jdescottes@mozilla.com>
2020-01-06 12:54:39 +0000

    Bug 1605995 - Add linting rule against relative paths in DevTools require r=Standard8
    Depends on D58243
    
    Differential Revision: https://phabricator.services.mozilla.com/D58253
    
    --HG--
    extra : moz-landing-system : lando

---
+++ tools/lint/eslint/eslint-plugin-mozilla/tests/reject-relative-requires.js
@@ -0,0 +1,58 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+var rule = require("../lib/rules/reject-relative-requires");
+var RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 8 } });
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+function invalidError() {
+  let message = "relative paths are not allowed with require()";
+  return [{ message, type: "CallExpression" }];
+}
+
+ruleTester.run("reject-relative-requires", rule, {
+  valid: [
+    'require("devtools/absolute/path")',
+    'require("resource://gre/modules/SomeModule.jsm")',
+    'loader.lazyRequireGetter(this, "path", "devtools/absolute/path", true)',
+    'loader.lazyRequireGetter(this, "Path", "devtools/absolute/path")',
+  ],
+  invalid: [
+    {
+      code: 'require("./relative/path")',
+      errors: invalidError(),
+    },
+    {
+      code: 'require("../parent/folder/path")',
+      errors: invalidError(),
+    },
+    {
+      code: 'loader.lazyRequireGetter(this, "path", "./relative/path", true)',
+      errors: invalidError(),
+    },
+    {
+      code:
+        'loader.lazyRequireGetter(this, "path", "../parent/folder/path", true)',
+      errors: invalidError(),
+    },
+    {
+      code: 'loader.lazyRequireGetter(this, "path", "./relative/path")',
+      errors: invalidError(),
+    },
+    {
+      code: 'loader.lazyRequireGetter(this, "path", "../parent/folder/path")',
+      errors: invalidError(),
+    },
+  ],
+});
