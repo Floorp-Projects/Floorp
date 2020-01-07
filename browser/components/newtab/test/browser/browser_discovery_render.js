f1d765c7de9a565149d42bc4abf65dfb21ed5e46
Scott <scott.downe@gmail.com>
2020-01-07 19:35:20 +0000

    Bug 1598876 - Intermittent test failure. r=gvn, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D58696
    
    --HG--
    extra : source : 9831a9845eaed2c41823fed1992c185c221e1df4

--- browser/components/newtab/test/browser/browser_discovery_render.js
+++ browser/components/newtab/test/browser/browser_discovery_render.js
@@ -13,19 +13,20 @@ async function before({ pushPrefs }) {
 
 test_newtab({
   before,
-  test: async function test_render_hardcoded() {
+  test: async function test_render_hardcoded_topsites() {
     const topSites = await ContentTaskUtils.waitForCondition(() =>
       content.document.querySelector(".ds-top-sites")
     );
     ok(topSites, "Got the discovery stream top sites section");
+  },
+});
 
-    const learnMore = content.document.querySelector(
-      ".ds-layout a[href$=new_tab_learn_more]"
+test_newtab({
+  before,
+  test: async function test_render_hardcoded_learnmore() {
+    const learnMoreLink = await ContentTaskUtils.waitForCondition(() =>
+      content.document.querySelector(".ds-layout .learn-more-link > a")
     );
-    is(
-      learnMore.textContent,
-      "What’s Pocket?",
-      "Got the rendered Message with link text and url within discovery stream"
-    );
+    ok(learnMoreLink, "Got the discovery stream learn more link");
   },
 });
