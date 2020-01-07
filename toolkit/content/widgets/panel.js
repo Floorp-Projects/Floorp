544bd79ea59c53d3c1b984ed82f62dc8ae642d61
Kirk Steuber <ksteuber@mozilla.com>
2020-01-07 17:16:01 +0000

    Bug 1603827 - Replace XULElement.orient with getAttribute/setAttribute r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D57665
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/content/widgets/panel.js
+++ toolkit/content/widgets/panel.js
@@ -123,8 +123,8 @@
       this.setAttribute("arrowposition", position);
 
       if (position.indexOf("start_") == 0 || position.indexOf("end_") == 0) {
-        container.orient = "horizontal";
-        arrowbox.orient = "vertical";
+        container.setAttribute("orient", "horizontal");
+        arrowbox.setAttribute("orient", "vertical");
         if (position.indexOf("_after") > 0) {
           arrowbox.setAttribute("pack", "end");
         } else {
@@ -146,8 +146,8 @@
         position.indexOf("before_") == 0 ||
         position.indexOf("after_") == 0
       ) {
-        container.orient = "";
-        arrowbox.orient = "";
+        container.removeAttribute("orient");
+        arrowbox.removeAttribute("orient");
         if (position.indexOf("_end") > 0) {
           arrowbox.setAttribute("pack", "end");
         } else {
