90d7a797da050545227f44ed4d77730d5763ccb2
Tom Prince <mozilla@hocat.ca>
2020-01-07 18:08:42 +0000

    Bug 1605275: [taskgraph] Provide a function to get all tasks from a previous set of graphs; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D58450

--- taskcluster/taskgraph/util/taskgraph.py
+++ taskcluster/taskgraph/util/taskgraph.py
@@ -25,16 +25,28 @@ def find_decision_task(parameters, graph_config):
         parameters['pushlog_id']))
 
 
-def find_existing_tasks_from_previous_kinds(full_task_graph, previous_graph_ids,
-                                            rebuild_kinds):
-    """Given a list of previous decision/action taskIds and kinds to ignore
-    from the previous graphs, return a dictionary of labels-to-taskids to use
-    as ``existing_tasks`` in the optimization step."""
+def find_existing_tasks(previous_graph_ids):
     existing_tasks = {}
     for previous_graph_id in previous_graph_ids:
         label_to_taskid = get_artifact(previous_graph_id, "public/label-to-taskid.json")
-        kind_labels = set(t.label for t in full_task_graph.tasks.itervalues()
-                          if t.attributes['kind'] not in rebuild_kinds)
-        for label in set(label_to_taskid.keys()).intersection(kind_labels):
-            existing_tasks[label] = label_to_taskid[label]
+        existing_tasks.update(label_to_taskid)
     return existing_tasks
+
+
+def find_existing_tasks_from_previous_kinds(
+    full_task_graph, previous_graph_ids, rebuild_kinds
+):
+    """Given a list of previous decision/action taskIds and kinds to ignore
+    from the previous graphs, return a dictionary of labels-to-taskids to use
+    as ``existing_tasks`` in the optimization step."""
+    existing_tasks = find_existing_tasks(previous_graph_ids)
+    kind_labels = {
+        t.label
+        for t in full_task_graph.tasks.itervalues()
+        if t.attributes["kind"] not in rebuild_kinds
+    }
+    return {
+        label: taskid
+        for (label, taskid) in existing_tasks.items()
+        if label in kind_labels
+    }
