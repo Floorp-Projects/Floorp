659919a4a79ef97b5a560e045acb24580573437f
Henrik Skupin <mail@hskupin.info>
2020-01-08 13:26:42 +0000

    Bug 1596136 - [remote] Remove openTab() and openWindow() helpers from target tests head.js. r=remote-protocol-reviewers,ato
    Bug 1605722 already added those helpers to the global head.js file.
    As such they can be removed from the target's head.js file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D58163

--- remote/test/browser/target/head.js
+++ remote/test/browser/target/head.js
@@ -9,28 +9,3 @@ Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/remote/test/browser/head.js",
   this
 );
-
-async function openTab(Target) {
-  info("Create a new tab and wait for the target to be created");
-
-  const targetCreated = Target.targetCreated();
-  const newTab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
-  const { targetInfo } = await targetCreated;
-
-  is(targetInfo.type, "page");
-  info(`New tab with target id ${targetInfo.targetId} created`);
-
-  return { targetInfo, newTab };
-}
-
-async function openWindow(Target) {
-  info("Create a new window and wait for the target to be created");
-
-  const targetCreated = Target.targetCreated();
-  const newWindow = await BrowserTestUtils.openNewBrowserWindow();
-  const newTab = newWindow.gBrowser.selectedTab;
-  const { targetInfo } = await targetCreated;
-  is(targetInfo.type, "page");
-
-  return { targetInfo, newWindow, newTab };
-}
