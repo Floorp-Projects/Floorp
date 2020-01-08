0591796a42a31895efedd56e6460badb5819e12a
Carolina <carolina.jimenez.g@gmail.com>
2020-01-08 11:36:09 +0000

    Bug 1595133 - Moved repetead functions to head.js.r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D57663

--- toolkit/components/certviewer/tests/browser/head.js
+++ toolkit/components/certviewer/tests/browser/head.js
@@ -70,19 +70,56 @@ function promiseLoadSubDialog(aURL) {
   });
 }
 
-async function openErrorPage() {
-  let src = "https://expired.example.com/";
-  let certErrorLoaded;
-  let tab = await BrowserTestUtils.openNewForegroundTab(
-    gBrowser,
-    () => {
-      gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, src);
-      let browser = gBrowser.selectedBrowser;
-      certErrorLoaded = BrowserTestUtils.waitForErrorPage(browser);
-    },
-    false
+function injectErrorPageFrame(tab, src) {
+  return SpecialPowers.spawn(
+    tab.linkedBrowser,
+    [{ frameSrc: src }],
+    async function({ frameSrc }) {
+      let loaded = ContentTaskUtils.waitForEvent(
+        content.wrappedJSObject,
+        "DOMFrameContentLoaded"
+      );
+      let iframe = content.document.createElement("iframe");
+      iframe.src = frameSrc;
+      content.document.body.appendChild(iframe);
+      await loaded;
+      // We will have race conditions when accessing the frame content after setting a src,
+      // so we can't wait for AboutNetErrorLoad. Let's wait for the certerror class to
+      // appear instead (which should happen at the same time as AboutNetErrorLoad).
+      await ContentTaskUtils.waitForCondition(() =>
+        iframe.contentDocument.body.classList.contains("certerror")
+      );
+    }
   );
-  info("Loading and waiting for the cert error");
-  await certErrorLoaded;
+}
+
+async function openErrorPage(useFrame = false) {
+  let src = "https://expired.example.com/";
+  let dummyPage =
+    getRootDirectory(gTestPath).replace(
+      "chrome://mochitests/content",
+      "https://example.com"
+    ) + "dummy_page.html";
+
+  let tab;
+  if (useFrame) {
+    info("Loading cert error page in an iframe");
+    tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, dummyPage);
+    await injectErrorPageFrame(tab, src);
+  } else {
+    let certErrorLoaded;
+    tab = await BrowserTestUtils.openNewForegroundTab(
+      gBrowser,
+      () => {
+        gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, src);
+        let browser = gBrowser.selectedBrowser;
+        certErrorLoaded = BrowserTestUtils.waitForErrorPage(browser);
+      },
+      false
+    );
+    info("Loading and waiting for the cert error");
+    await certErrorLoaded;
+  }
+
   return tab;
 }
