704d83336d96b9db828466423b274ecc7eedeb0d
Razvan Maries <rmaries@mozilla.com>
2020-01-08 23:37:53 +0200

    Backed out changeset 47d6ea69c274 (bug 1607846) for perma failures on nsDisplayList.h. CLOSED TREE

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -4647,6 +4647,7 @@ bool nsDisplayBackgroundImage::CreateWebRenderCommands(
     mozilla::wr::IpcResourceUpdateQueue& aResources,
     const StackingContextHelper& aSc, RenderRootStateManager* aManager,
     nsDisplayListBuilder* aDisplayListBuilder) {
+  ContainerLayerParameters parameter;
   if (!CanBuildWebRenderDisplayItems(aManager->LayerManager(),
                                      aDisplayListBuilder)) {
     return false;
@@ -5422,27 +5423,22 @@ void nsDisplayOutline::Paint(nsDisplayListBuilder* aBuilder, gfxContext* aCtx) {
       nsRect(offset, mFrame->GetSize()), mFrame->Style());
 }
 
-bool nsDisplayOutline::IsThemedOutline() const {
-  const auto& outlineStyle = mFrame->StyleOutline()->mOutlineStyle;
-  if (!outlineStyle.IsAuto() ||
-      !StaticPrefs::layout_css_outline_style_auto_enabled()) {
-    return false;
-  }
-
-  nsPresContext* pc = mFrame->PresContext();
-  nsITheme* theme = pc->GetTheme();
-  return theme && theme->ThemeSupportsWidget(pc, mFrame,
-                                             StyleAppearance::FocusOutline);
-}
-
 bool nsDisplayOutline::CreateWebRenderCommands(
     mozilla::wr::DisplayListBuilder& aBuilder,
     mozilla::wr::IpcResourceUpdateQueue& aResources,
     const StackingContextHelper& aSc,
     mozilla::layers::RenderRootStateManager* aManager,
     nsDisplayListBuilder* aDisplayListBuilder) {
-  if (IsThemedOutline()) {
-    return false;
+  ContainerLayerParameters parameter;
+
+  const auto& outlineStyle = mFrame->StyleOutline()->mOutlineStyle;
+  if (outlineStyle.IsAuto() &&
+      StaticPrefs::layout_css_outline_style_auto_enabled()) {
+    nsITheme* theme = mFrame->PresContext()->GetTheme();
+    if (theme && theme->ThemeSupportsWidget(mFrame->PresContext(), mFrame,
+                                            StyleAppearance::FocusOutline)) {
+      return false;
+    }
   }
 
   nsPoint offset = ToReferenceFrame();
