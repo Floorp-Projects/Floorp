ea4c73f19d282e5fe266b42f722a3e7e3cc50cb8
Florin Strugariu <fstrugariu@mozilla.com>
2020-01-09 13:52:29 +0000

    Bug 1608038 - Split signal_handler from raptor.py r=AlexandruIonescu,perftest-reviewers,octavian_negru,rwood
    Differential Revision: https://phabricator.services.mozilla.com/D59287
    
    --HG--
    extra : moz-landing-system : lando

---
+++ testing/raptor/raptor/signal_handler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import
+
+import signal
+
+
+class SignalHandler:
+
+    def __init__(self):
+        signal.signal(signal.SIGINT, self.handle_signal)
+        signal.signal(signal.SIGTERM, self.handle_signal)
+
+    def handle_signal(self, signum, frame):
+        raise SignalHandlerException("Program aborted due to signal %s" % signum)
+
+
+class SignalHandlerException(Exception):
+    pass
