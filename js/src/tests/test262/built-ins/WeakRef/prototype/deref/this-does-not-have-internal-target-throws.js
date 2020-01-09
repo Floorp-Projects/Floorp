71433d41f60f1e829c0902ac6977c48634eed89b
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2020-01-09 15:06:38 +0000

    Bug 1603701 - Part 2: update WeakRef tests in test262. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D59303

--- js/src/tests/test262/built-ins/WeakRef/prototype/deref/this-does-not-have-internal-target-throws.js
+++ js/src/tests/test262/built-ins/WeakRef/prototype/deref/this-does-not-have-internal-target-throws.js
@@ -1,4 +1,4 @@
-// |reftest| skip -- WeakRef is not supported
+// |reftest| skip-if(!this.hasOwnProperty('WeakRef')||!this.hasOwnProperty('FinalizationGroup')) -- WeakRef,FinalizationGroup is not enabled unconditionally
 // Copyright (C) 2019 Leo Balter. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
