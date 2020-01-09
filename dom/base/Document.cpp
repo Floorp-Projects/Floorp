1003d38e109ba84fa4bcb55b5af3005fd04713ec
Johann Hofmann <jhofmann@mozilla.com>
2020-01-09 14:55:02 +0000

    Bug 1597029 - Set securityInfo for all document types. r=baku a=RyanVM
    In StartDocumentLoad we copy the securityInfo reference from the load channel into a document
    member variable. This used to happen only for HTML documents, but other documents (e.g. media)
    can be loaded via secure channels, too and thus should have securityInfos. We're using the
    securityInfo object to display information in the UI, which would previously fail for images and videos.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59327

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -3094,6 +3094,9 @@ nsresult Document::StartDocumentLoad(const char* aCommand, nsIChannel* aChannel,
         IsSynthesized() && XRE_IsContentProcess()) {
       ContentChild::UpdateCookieStatus(mChannel);
     }
+
+    // Store the security info for future use.
+    mChannel->GetSecurityInfo(getter_AddRefs(mSecurityInfo));
   }
 
   // If this document is being loaded by a docshell, copy its sandbox flags
