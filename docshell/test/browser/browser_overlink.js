51df30ea82c326381179a5f464b6130160673baa
Johann Hofmann <jhofmann@mozilla.com>
2020-01-10 12:45:38 +0000

    Bug 353933 - Make OnOverlink use CreateExposableURI. r=bzbarsky
    Differential Revision: https://phabricator.services.mozilla.com/D59428

---
+++ docshell/test/browser/browser_overlink.js
@@ -0,0 +1,27 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_PATH = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "https://example.com"
+);
+
+add_task(async function test_stripAuthCredentials() {
+  await BrowserTestUtils.withNewTab(
+    TEST_PATH + "overlink_test.html",
+    async function(browser) {
+      await SpecialPowers.spawn(browser, [], function() {
+        content.document.getElementById("link").focus();
+      });
+
+      await TestUtils.waitForCondition(
+        () => XULBrowserWindow.overLink == "https://example.com",
+        "Overlink should be missing auth credentials"
+      );
+
+      ok(true, "Test successful");
+    }
+  );
+});
