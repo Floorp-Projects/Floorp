444fb855c64683ed9eb076bf45487bb963edae32
Bogdan Tara <btara@mozilla.com>
2020-01-10 12:16:01 +0200

    Backed out changeset 82396fd59d58 (bug 1566755) for ES lint failure on wamp/serializers.js CLOSED TREE

--- devtools/client/netmonitor/src/utils/request-utils.js
+++ devtools/client/netmonitor/src/utils/request-utils.js
@@ -506,24 +506,6 @@ function getResponseHeader(item, header) {
 }
 
 /**
- * Get the value of a particular request header, or null if not
- * present.
- */
-function getRequestHeader(item, header) {
-  const { requestHeaders } = item;
-  if (!requestHeaders || !requestHeaders.headers.length) {
-    return null;
-  }
-  header = header.toLowerCase();
-  for (const requestHeader of requestHeaders.headers) {
-    if (requestHeader.name.toLowerCase() == header) {
-      return requestHeader.value;
-    }
-  }
-  return null;
-}
-
-/**
  * Extracts any urlencoded form data sections from a POST request.
  */
 async function updateFormDataSections(props) {
@@ -665,7 +647,6 @@ module.exports = {
   getEndTime,
   getFormattedProtocol,
   getFramePayload,
-  getRequestHeader,
   getResponseHeader,
   getResponseTime,
   getStartTime,
