e42c854c2746ecdafb97cd97938c40a8aebc6f98
Boris Zbarsky <bzbarsky@mit.edu>
2020-01-13 22:31:05 +0000

    Bug 1607953.  Refactor our runnable metrics and long task code to make it more robust and correct.  r=jesup,tarek,froydnj
    The idea is to use the slice-accounting setup from the performance counters for
    idle tasks as well, and fix the various bugs we have when nested event loops
    are involved.
    
    The bit in nsThread::SizeOfEventQueues that counted memory for
    mCurrentPerformanceCounter was wrong all along, I think: the docgroup that owns
    the performance counter should account for it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59596
    
    --HG--
    extra : moz-landing-system : lando

--- xpcom/tests/gtest/TestThreadMetrics.cpp
+++ xpcom/tests/gtest/TestThreadMetrics.cpp
@@ -13,43 +13,83 @@
 #include "mozilla/SchedulerGroup.h"
 #include "mozilla/TaskCategory.h"
 #include "mozilla/PerformanceCounter.h"
+#include "mozilla/Unused.h"
 #include "nsThreadUtils.h"
 #include "nsServiceManagerUtils.h"
+#include "nsTArray.h"
 #include "nsThread.h"
 
 using namespace mozilla;
 using mozilla::Runnable;
+using mozilla::dom::DocGroup;
+
+/* A struct that describes a runnable to run and, optionally, a
+ * docgroup to dispatch it to.
+ */
+struct RunnableDescriptor {
+  MOZ_IMPLICIT RunnableDescriptor(nsIRunnable* aRunnable,
+                                  DocGroup* aDocGroup = nullptr)
+      : mRunnable(aRunnable), mDocGroup(aDocGroup) {}
+
+  RunnableDescriptor(RunnableDescriptor&& aDescriptor)
+      : mRunnable(std::move(aDescriptor.mRunnable)),
+        mDocGroup(std::move(aDescriptor.mDocGroup)) {}
+
+  nsCOMPtr<nsIRunnable> mRunnable;
+  RefPtr<DocGroup> mDocGroup;
+};
 
 /* Timed runnable which simulates some execution time
- * and can run a nested runnable.
+ * and can run some nested runnables.
  */
 class TimedRunnable final : public Runnable {
  public:
-  explicit TimedRunnable(uint32_t aExecutionTime1, uint32_t aExecutionTime2,
-                         uint32_t aSubExecutionTime)
+  explicit TimedRunnable(uint32_t aExecutionTime1, uint32_t aExecutionTime2)
       : Runnable("TimedRunnable"),
         mExecutionTime1(aExecutionTime1),
-        mExecutionTime2(aExecutionTime2),
-        mSubExecutionTime(aSubExecutionTime) {}
+        mExecutionTime2(aExecutionTime2) {}
+
   NS_IMETHODIMP Run() {
     PR_Sleep(PR_MillisecondsToInterval(mExecutionTime1 + 5));
-    if (mSubExecutionTime > 0) {
-      // Dispatch another runnable so nsThread::ProcessNextEvent is called
-      // recursively
-      nsCOMPtr<nsIThread> thread = do_GetMainThread();
-      nsCOMPtr<nsIRunnable> runnable =
-          new TimedRunnable(mSubExecutionTime, 0, 0);
-      thread->Dispatch(runnable, NS_DISPATCH_NORMAL);
-      (void)NS_ProcessNextEvent(thread, false);
+    for (uint32_t index = 0; index < mNestedRunnables.Length(); ++index) {
+      if (index != 0) {
+        PR_Sleep(PR_MillisecondsToInterval(mExecutionTime1 + 5));
+      }
+      (void)DispatchNestedRunnable(mNestedRunnables[index].mRunnable,
+                                   mNestedRunnables[index].mDocGroup);
     }
     PR_Sleep(PR_MillisecondsToInterval(mExecutionTime2 + 5));
     return NS_OK;
   }
 
+  void AddNestedRunnable(RunnableDescriptor aDescriptor) {
+    mNestedRunnables.AppendElement(std::move(aDescriptor));
+  }
+
+  static void DispatchNestedRunnable(nsIRunnable* aRunnable,
+                                     DocGroup* aDocGroup) {
+    // Dispatch another runnable so nsThread::ProcessNextEvent is called
+    // recursively
+    nsCOMPtr<nsIThread> thread = do_GetMainThread();
+    if (aDocGroup) {
+      (void)DispatchWithDocgroup(aRunnable, aDocGroup);
+    } else {
+      thread->Dispatch(aRunnable, NS_DISPATCH_NORMAL);
+    }
+    (void)NS_ProcessNextEvent(thread, false);
+  }
+
+  static nsresult DispatchWithDocgroup(nsIRunnable* aRunnable,
+                                       DocGroup* aDocGroup) {
+    nsCOMPtr<nsIRunnable> runnable = aRunnable;
+    runnable = new SchedulerGroup::Runnable(runnable.forget(), aDocGroup);
+    return aDocGroup->Dispatch(TaskCategory::Other, runnable.forget());
+  }
+
  private:
   uint32_t mExecutionTime1;
   uint32_t mExecutionTime2;
-  uint32_t mSubExecutionTime;
+  nsTArray<RunnableDescriptor> mNestedRunnables;
 };
 
 /* test class used for all metrics tests
@@ -65,11 +105,11 @@ class ThreadMetrics : public ::testing::Test {
  protected:
   virtual void SetUp() {
     // building the TabGroup/DocGroup structure
-    nsCString key = NS_LITERAL_CSTRING("key");
-    RefPtr<dom::Document> doc;
     RefPtr<dom::TabGroup> tabGroup = new dom::TabGroup(false);
-    mDocGroup = tabGroup->AddDocument(key, doc);
+    mDocGroup = tabGroup->AddDocument(NS_LITERAL_CSTRING("key"), nullptr);
+    mDocGroup2 = tabGroup->AddDocument(NS_LITERAL_CSTRING("key2"), nullptr);
     mCounter = mDocGroup->GetPerformanceCounter();
+    mCounter2 = mDocGroup2->GetPerformanceCounter();
     mThreadMgr = do_GetService("@mozilla.org/thread-manager;1");
     mOther = DispatchCategory(TaskCategory::Other).GetValue();
     mDispatchCount = (uint32_t)TaskCategory::Other + 1;
@@ -78,28 +118,28 @@ class ThreadMetrics : public ::testing::Test {
   virtual void TearDown() {
     // and remove the document from the doc group (actually, a nullptr)
     mDocGroup->RemoveDocument(nullptr);
+    mDocGroup2->RemoveDocument(nullptr);
     mDocGroup = nullptr;
+    mDocGroup2 = nullptr;
     ProcessAllEvents();
   }
 
   // this is used to get rid of transient events
   void initScheduler() { ProcessAllEvents(); }
 
-  nsresult Dispatch(uint32_t aExecutionTime1, uint32_t aExecutionTime2,
-                    uint32_t aSubExecutionTime) {
+  nsresult Dispatch(nsIRunnable* aRunnable) {
     ProcessAllEvents();
-    nsCOMPtr<nsIRunnable> runnable =
-        new TimedRunnable(aExecutionTime1, aExecutionTime2, aSubExecutionTime);
-    runnable = new SchedulerGroup::Runnable(runnable.forget(), mDocGroup);
-    return mDocGroup->Dispatch(TaskCategory::Other, runnable.forget());
+    return TimedRunnable::DispatchWithDocgroup(aRunnable, mDocGroup);
   }
 
   void ProcessAllEvents() { mThreadMgr->SpinEventLoopUntilEmpty(); }
 
   uint32_t mOther;
   bool mOldPref;
-  RefPtr<mozilla::dom::DocGroup> mDocGroup;
-  RefPtr<mozilla::PerformanceCounter> mCounter;
+  RefPtr<DocGroup> mDocGroup;
+  RefPtr<DocGroup> mDocGroup2;
+  RefPtr<PerformanceCounter> mCounter;
+  RefPtr<PerformanceCounter> mCounter2;
   nsCOMPtr<nsIThreadManager> mThreadMgr;
   uint32_t mDispatchCount;
 };
@@ -109,7 +149,8 @@ TEST_F(ThreadMetrics, CollectMetrics) {
   initScheduler();
 
   // Dispatching a runnable that will last for +50ms
-  rv = Dispatch(25, 25, 0);
+  nsCOMPtr<nsIRunnable> runnable = new TimedRunnable(25, 25);
+  rv = Dispatch(runnable);
   ASSERT_TRUE(NS_SUCCEEDED(rv));
 
   // Flush the queue
@@ -137,7 +178,10 @@ TEST_F(ThreadMetrics, CollectRecursiveMetrics) {
 
   // Dispatching a runnable that will last for +50ms
   // and run another one recursively that lasts for 400ms
-  rv = Dispatch(25, 25, 400);
+  RefPtr<TimedRunnable> runnable = new TimedRunnable(25, 25);
+  nsCOMPtr<nsIRunnable> nested = new TimedRunnable(400, 0);
+  runnable->AddNestedRunnable({nested});
+  rv = Dispatch(runnable);
   ASSERT_TRUE(NS_SUCCEEDED(rv));
 
   // Flush the queue
@@ -156,6 +200,90 @@ TEST_F(ThreadMetrics, CollectRecursiveMetrics) {
   // did we get incremented in the docgroup ?
   uint64_t duration = mCounter->GetExecutionDuration();
   ASSERT_GE(duration, 50000u);
+
+  // let's make sure we don't count the time spent in recursive calls
+  ASSERT_LT(duration, 300000u);
+}
+
+TEST_F(ThreadMetrics, CollectMultipleRecursiveMetrics) {
+  nsresult rv;
+
+  initScheduler();
+
+  // Dispatching a runnable that will last for +75ms
+  // and run another two recursively that last for 400ms each.
+  RefPtr<TimedRunnable> runnable = new TimedRunnable(25, 25);
+  for (auto i : {1, 2}) {
+    Unused << i;
+    nsCOMPtr<nsIRunnable> nested = new TimedRunnable(400, 0);
+    runnable->AddNestedRunnable({nested});
+  }
+
+  rv = Dispatch(runnable);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
+
+  // Flush the queue
+  ProcessAllEvents();
+
+  // let's look at the counters
+  ASSERT_EQ(mCounter->GetDispatchCounter()[mOther], 1u);
+
+  // other counters should stay empty
+  for (uint32_t i = 0; i < mDispatchCount; i++) {
+    if (i != mOther) {
+      ASSERT_EQ(mCounter->GetDispatchCounter()[i], 0u);
+    }
+  }
+
+  // did we get incremented in the docgroup ?
+  uint64_t duration = mCounter->GetExecutionDuration();
+  ASSERT_GE(duration, 75000u);
+
+  // let's make sure we don't count the time spent in recursive calls
+  ASSERT_LT(duration, 300000u);
+}
+
+TEST_F(ThreadMetrics, CollectMultipleRecursiveMetricsWithTwoDocgroups) {
+  nsresult rv;
+
+  initScheduler();
+
+  // Dispatching a runnable that will last for +75ms
+  // and run another two recursively that last for 400ms each.  The
+  // first nested runnable will have a docgroup, but the second will
+  // not, to test that the time for first nested event is accounted
+  // correctly.
+  RefPtr<TimedRunnable> runnable = new TimedRunnable(25, 25);
+  nsCOMPtr<nsIRunnable> nested = new TimedRunnable(400, 0);
+  runnable->AddNestedRunnable({nested, mDocGroup2});
+  nested = new TimedRunnable(400, 0);
+  runnable->AddNestedRunnable({nested});
+
+  rv = Dispatch(runnable);
+  ASSERT_TRUE(NS_SUCCEEDED(rv));
+
+  // Flush the queue
+  ProcessAllEvents();
+
+  // let's look at the counters
+  ASSERT_EQ(mCounter->GetDispatchCounter()[mOther], 1u);
+
+  // other counters should stay empty
+  for (uint32_t i = 0; i < mDispatchCount; i++) {
+    if (i != mOther) {
+      ASSERT_EQ(mCounter->GetDispatchCounter()[i], 0u);
+    }
+  }
+
+  uint64_t duration = mCounter2->GetExecutionDuration();
+  // Make sure this we incremented the timings for the first nested
+  // runnable correctly.
+  ASSERT_GE(duration, 400000u);
+  ASSERT_LT(duration, 420000u);
+
+  // And now for the outer runnable.
+  duration = mCounter->GetExecutionDuration();
+  ASSERT_GE(duration, 75000u);
 
   // let's make sure we don't count the time spent in recursive calls
   ASSERT_LT(duration, 300000u);
