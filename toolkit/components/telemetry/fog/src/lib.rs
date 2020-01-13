e370be055daf4b8cfcdb97d65550b924ecb128bb
Chris H-C <chutten@mozilla.com>
2020-01-13 18:51:51 +0000

    Bug 1591564 - Initialize the FOGotype when Telemetry inits r=janerik,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D57105

--- toolkit/components/telemetry/fog/src/lib.rs
+++ toolkit/components/telemetry/fog/src/lib.rs
@@ -2,10 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use glean_preview::Configuration;
+use nserror::{nsresult, NS_ERROR_FAILURE, NS_OK};
+use nsstring::nsAString;
+
+#[no_mangle]
+pub unsafe extern "C" fn fog_init(upload_enabled: bool, data_dir: &nsAString) -> nsresult {
+  let cfg = Configuration {
+    data_path: data_dir.to_string(),
+    application_id: "org.mozilla.fogotype".into(),
+    upload_enabled: upload_enabled,
+    max_events: None,
+    delay_ping_lifetime_io: false, // We will want this eventually.
+  };
+
+  if glean_preview::initialize(cfg).is_err() {
+    return NS_ERROR_FAILURE
+  }
+
+  NS_OK
 }
