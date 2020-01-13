c6f59c79e30dc83270885f0abcb4f6eddc1db4b2
Ciure Andrei <aciure@mozilla.com>
2020-01-13 04:10:57 +0200

    Backed out changeset ca6f43c06131 (bug 1607987) for causing test_bug369370.html to permafail

--- netwerk/ipc/DocumentChannelChild.h
+++ netwerk/ipc/DocumentChannelChild.h
@@ -9,19 +9,25 @@
 #define mozilla_net_DocumentChannelChild_h
 
 #include "mozilla/net/PDocumentChannelChild.h"
-#include "mozilla/net/DocumentChannel.h"
+#include "nsDOMNavigationTiming.h"
 #include "nsIAsyncVerifyRedirectCallback.h"
+#include "nsIChannel.h"
+#include "nsIChildChannel.h"
+#include "nsITraceableChannel.h"
+
+#define DOCUMENT_CHANNEL_CHILD_IID                   \
+  {                                                  \
+    0x6977bc44, 0xb1db, 0x41b7, {                    \
+      0xb5, 0xc5, 0xe2, 0x13, 0x68, 0x22, 0xc9, 0x8f \
+    }                                                \
+  }
 
 namespace mozilla {
 namespace net {
 
-/**
- * DocumentChannelChild is an implementation of DocumentChannel for nsDocShells
- * in the content process, that uses PDocumentChannel to serialize everything
- * across IPDL to the parent process.
- */
-class DocumentChannelChild final : public DocumentChannel,
+class DocumentChannelChild final : public nsIIdentChannel,
                                    public nsIAsyncVerifyRedirectCallback,
+                                   public nsITraceableChannel,
                                    public PDocumentChannelChild {
  public:
   DocumentChannelChild(nsDocShellLoadState* aLoadState,
@@ -30,11 +36,14 @@ class DocumentChannelChild final : public DocumentChannel,
                        uint32_t aLoadType, uint32_t aCacheKey, bool aIsActive,
                        bool aIsTopLevelDoc, bool aHasNonEmptySandboxingFlags);
 
-  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIREQUEST
+  NS_DECL_NSICHANNEL
+  NS_DECL_NSIIDENTCHANNEL
   NS_DECL_NSIASYNCVERIFYREDIRECTCALLBACK
+  NS_DECL_NSITRACEABLECHANNEL
 
-  NS_IMETHOD AsyncOpen(nsIStreamListener* aListener) override;
-  NS_IMETHOD Cancel(nsresult aStatusCode) override;
+  NS_DECLARE_STATIC_IID_ACCESSOR(DOCUMENT_CHANNEL_CHILD_IID)
 
   mozilla::ipc::IPCResult RecvFailedAsyncOpen(const nsresult& aStatusCode);
 
@@ -54,15 +63,63 @@ class DocumentChannelChild final : public DocumentChannel,
       LoadInfoArgs&& aLoadInfo, nsIURI* aNewUri,
       ConfirmRedirectResolver&& aResolve);
 
+  const nsTArray<DocumentChannelRedirect>& GetRedirectChain() const {
+    return mRedirects;
+  }
+
+  void GetLastVisit(nsIURI** aURI, uint32_t* aChannelRedirectFlags) const {
+    *aURI = do_AddRef(mLastVisitInfo.uri()).take();
+    *aChannelRedirectFlags = mLastVisitInfo.previousFlags();
+  }
+
+  void SetDocumentOpenFlags(uint32_t aFlags, bool aPluginsAllowed) {
+    mDocumentOpenFlags = Some(aFlags);
+    mPluginsAllowed = aPluginsAllowed;
+  }
+
+  void SetNavigationTiming(nsDOMNavigationTiming* aTiming) {
+    mTiming = aTiming;
+  }
+
  private:
   void ShutdownListeners(nsresult aStatusCode);
+  nsDocShell* GetDocShell();
 
   ~DocumentChannelChild();
 
+  LastVisitInfo mLastVisitInfo;
   nsCOMPtr<nsIChannel> mRedirectChannel;
+  nsTArray<DocumentChannelRedirect> mRedirects;
 
   RedirectToRealChannelResolver mRedirectResolver;
+
+  const TimeStamp mAsyncOpenTime;
+  const RefPtr<nsDocShellLoadState> mLoadState;
+  const Maybe<nsString> mInitiatorType;
+  const uint32_t mLoadType;
+  const uint32_t mCacheKey;
+  const bool mIsActive;
+  const bool mIsTopLevelDoc;
+  const bool mHasNonEmptySandboxingFlags;
+
+  nsresult mStatus = NS_OK;
+  bool mCanceled = false;
+  Maybe<uint32_t> mDocumentOpenFlags;
+  bool mIsPending = false;
+  bool mWasOpened = false;
+  uint64_t mChannelId;
+  uint32_t mLoadFlags = LOAD_NORMAL;
+  const nsCOMPtr<nsIURI> mURI;
+  nsCOMPtr<nsILoadGroup> mLoadGroup;
+  nsCOMPtr<nsILoadInfo> mLoadInfo;
+  nsCOMPtr<nsIInterfaceRequestor> mCallbacks;
+  nsCOMPtr<nsIStreamListener> mListener;
+  nsCOMPtr<nsISupports> mOwner;
+  bool mPluginsAllowed = false;
+  RefPtr<nsDOMNavigationTiming> mTiming;
 };
+
+NS_DEFINE_STATIC_IID_ACCESSOR(DocumentChannelChild, DOCUMENT_CHANNEL_CHILD_IID)
 
 }  // namespace net
 }  // namespace mozilla
