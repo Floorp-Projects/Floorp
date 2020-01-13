73c92a684cfdcffb4a55c9a79510214b0e721b22
Rob Wood <rwood@mozilla.com>
2020-01-13 20:08:07 +0000

    Bug 1607891 - Add fennec (68) support to raptor-browsertime r=tarek
    Differential Revision: https://phabricator.services.mozilla.com/D59737
    
    --HG--
    extra : moz-landing-system : lando

--- testing/raptor/raptor/manifest.py
+++ testing/raptor/raptor/manifest.py
@@ -453,6 +453,13 @@ def get_raptor_test_list(args, oskey):
                 next_test['page_timeout']) * LIVE_SITE_TIMEOUT_MULTIPLIER
             LOG.info("using live sites so using page timeout of %dms" % next_test['page_timeout'])
 
+        # browsertime doesn't use the 'measure' test ini setting; however just for the sake
+        # of supporting both webext and browsertime, just provide a dummy 'measure' setting
+        # here to prevent having to check in multiple places; it has no effect on what
+        # browsertime actually measures; remove this when eventually we remove webext support
+        if args.browsertime is not None and next_test.get('measure') is None:
+            next_test['measure'] = "fnbpaint, fcp, dcf, loadtime"
+
         # convert 'measure =' test INI line to list
         if next_test.get('measure') is not None:
             _measures = []
