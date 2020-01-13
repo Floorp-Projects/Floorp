d7a2eff4e63ca69fde60ca15fe4509163ae05146
Dorel Luca <dluca@mozilla.com>
2020-01-13 13:29:55 +0200

    Backed out changeset 47f8d76aa4fc (bug 1603190) for Devtools failures in devtools/client/framework/test/browser_toolbox_view_source_02.js. CLOSED TREE

--- devtools/client/preferences/debugger.js
+++ devtools/client/preferences/debugger.js
@@ -86,4 +86,4 @@ pref("devtools.debugger.features.dom-mutation-breakpoints", true);
 pref("devtools.debugger.features.log-points", true);
 pref("devtools.debugger.features.overlay", true);
 pref("devtools.debugger.features.inline-preview", true);
-
+pref("devtools.debugger.features.windowless-service-workers", false);
