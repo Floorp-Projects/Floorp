1676b6ba3d3e67f3d414d738a1a8e785aaf4b10e
Jason Orendorff <jorendorff@mozilla.com>
2020-01-14 22:49:22 +0000

    Bug 1608509 - Part 8: Remove JSOP_* constants. r=jandem.
    This also removes the `name` field from FOR_EACH_OPCODE. Disassembly now uses
    the UpperCamelCase opcode name.
    
    A few tests look at disassembler output.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59809
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/BytecodeUtil.cpp
+++ js/src/vm/BytecodeUtil.cpp
@@ -67,8 +67,7 @@ using js::frontend::IsIdentifier;
 JS_STATIC_ASSERT(sizeof(uint32_t) * CHAR_BIT >= INDEX_LIMIT_LOG2 + 1);
 
 const JSCodeSpec js::CodeSpecTable[] = {
-#define MAKE_CODESPEC(op, op_camel, op_snake, name, token, length, nuses, \
-                      ndefs, format)                                      \
+#define MAKE_CODESPEC(op, op_snake, token, length, nuses, ndefs, format) \
   {length, nuses, ndefs, format},
     FOR_EACH_OPCODE(MAKE_CODESPEC)
 #undef MAKE_CODESPEC
@@ -79,7 +78,7 @@ const JSCodeSpec js::CodeSpecTable[] = {
  * bytecode or null.
  */
 static const char* const CodeToken[] = {
-#define TOKEN(op, op_camel, op_snake, name, token, ...) token,
+#define TOKEN(op, op_snake, token, ...) token,
     FOR_EACH_OPCODE(TOKEN)
 #undef TOKEN
 };
@@ -89,7 +88,7 @@ static const char* const CodeToken[] = {
  * and JIT debug spew.
  */
 const char* const js::CodeNameTable[] = {
-#define OPNAME(op, op_camel, op_snake, name, ...) name,
+#define OPNAME(op, ...) #op,
     FOR_EACH_OPCODE(OPNAME)
 #undef OPNAME
 };
