99e14167be8444d89ef22bd6735115899040571d
Samael Wang <freesamael@gmail.com>
2020-01-14 10:43:56 +0000

    Bug 836567 - Part 2: Replace inapplicable tests with a web-platform-test for reloading after setting javascript: URI, and fix other tests relying on javascript: URI. r=Gijs,miker
    Some notes about the changes:
    
    Both test_bug384014.html and test_bug123696.html were testing reloading of
    javascript: URI. The expected result of the iframes after reloading would
    become about:blank. I deleted both file and instead wrote with
    web-platform-test to cover reloading of javascript: URI since wpt is more
    preferable.
    
    storage-cache-error.html was utilizing javascript: URI to test bug 1262766.
    javascript: URI would cause CacheStorage::Keys to throw a dom security
    exception because the URI's principal (which did not match the principal of the
    document in this case!) was a nullpricipal.  With my patches the iframe's URL
    would no longer be the javascript: URI, so it's no longer applicable for the
    test case. Instead we can test what bug 1262766 was originally about - that
    CacheStorage::Keys would throw a dom security exception if it's in a private
    browsing window.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59465

--- devtools/client/storage/test/browser_storage_cache_error.js
+++ devtools/client/storage/test/browser_storage_cache_error.js
@@ -7,9 +7,22 @@
 // Test handling errors in CacheStorage
 
 add_task(async function() {
-  await openTabAndSetupStorage(MAIN_DOMAIN + "storage-cache-error.html");
+  // Open the URL in a private browsing window.
+  const win = await BrowserTestUtils.openNewBrowserWindow({ private: true });
+  const tab = win.gBrowser.selectedBrowser;
+  const systemPrincipal = Services.scriptSecurityManager.getSystemPrincipal();
+  tab.loadURI(MAIN_DOMAIN + "storage-cache-error.html", {
+    triggeringPrincipal: systemPrincipal,
+  });
+  await BrowserTestUtils.browserLoaded(tab);
 
-  const cacheItemId = ["Cache", "javascript:parent.frameContent"];
+  // On enumerating cache storages, CacheStorage::Keys would throw a
+  // DOM security exception. We'd like to verify storage panel still work in
+  // this case.
+  const target = await TargetFactory.forTab(win.gBrowser.selectedTab);
+  await openStoragePanel(null, target);
+
+  const cacheItemId = ["Cache", "http://test2.example.org"];
 
   await selectTreeItem(cacheItemId);
   ok(
@@ -17,5 +30,6 @@ add_task(async function() {
     `The item ${cacheItemId.join(" > ")} is present in the tree`
   );
 
+  await BrowserTestUtils.closeWindow(win);
   await finishTests();
 });
