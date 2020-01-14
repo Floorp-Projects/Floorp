d7a0a95c68d8c459ddcf5829d14928420710168a
Emilio Cobos Álvarez <emilio@crisal.io>
2020-01-14 16:29:50 +0000

    Bug 1609167 - Don't use NS_IMETHOD for nsITextControlFrame::GetTextEditor. r=smaug
    As it seems it causes MinGW 32-bit-only build issues.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59889
    
    --HG--
    extra : moz-landing-system : lando

--- layout/forms/nsITextControlFrame.h
+++ layout/forms/nsITextControlFrame.h
@@ -8,6 +8,7 @@
 #define nsITextControlFrame_h___
 
 #include "nsIFormControlFrame.h"
+#include "mozilla/AlreadyAddRefed.h"
 
 class nsISelectionController;
 class nsFrameSelection;
@@ -16,13 +17,14 @@ namespace mozilla {
 class TextEditor;
 }  // namespace mozilla
 
+// FIXME(emilio): This has only one implementation, seems it could be removed...
 class nsITextControlFrame : public nsIFormControlFrame {
  public:
   NS_DECL_QUERYFRAME_TARGET(nsITextControlFrame)
 
   enum SelectionDirection { eNone, eForward, eBackward };
 
-  NS_IMETHOD_(already_AddRefed<mozilla::TextEditor>) GetTextEditor() = 0;
+  virtual already_AddRefed<mozilla::TextEditor> GetTextEditor() = 0;
 
   MOZ_CAN_RUN_SCRIPT NS_IMETHOD
   SetSelectionRange(uint32_t aSelectionStart, uint32_t aSelectionEnd,
