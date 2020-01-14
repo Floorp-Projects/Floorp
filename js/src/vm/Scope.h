6e7ec440a71c6728754c4f4c33e1246fda514e04
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-01-14 21:58:10 +0200

    Backed out changeset 5259dd26dcc7 (bug 1608183) for bustages on Stencil.h. CLOSED TREE

--- js/src/vm/Scope.h
+++ js/src/vm/Scope.h
@@ -25,10 +25,6 @@
 
 namespace js {
 
-namespace frontend {
-class ScopeCreationData;
-};
-
 class BaseScopeData;
 class ModuleObject;
 class AbstractScope;
@@ -243,7 +239,7 @@ class WrappedPtrOperations<Scope*, Wrapper> {
 //
 class Scope : public js::gc::TenuredCell {
   friend class GCMarker;
-  friend class frontend::ScopeCreationData;
+  friend class ScopeCreationData;
 
   // The enclosing scope or nullptr.
   const GCPtrScope enclosing_;
@@ -397,7 +393,7 @@ class LexicalScope : public Scope {
   friend class Scope;
   friend class BindingIter;
   friend class GCMarker;
-  friend class frontend::ScopeCreationData;
+  friend class ScopeCreationData;
 
  public:
   // Data is public because it is created by the frontend. See
@@ -625,7 +621,7 @@ class VarScope : public Scope {
   friend class GCMarker;
   friend class BindingIter;
   friend class Scope;
-  friend class frontend::ScopeCreationData;
+  friend class ScopeCreationData;
 
  public:
   // Data is public because it is created by the
@@ -800,7 +796,7 @@ class EvalScope : public Scope {
   friend class Scope;
   friend class BindingIter;
   friend class GCMarker;
-  friend class frontend::ScopeCreationData;
+  friend class ScopeCreationData;
 
  public:
   // Data is public because it is created by the frontend. See
@@ -887,7 +883,7 @@ class ModuleScope : public Scope {
   friend class BindingIter;
   friend class Scope;
   friend class AbstractScope;
-  friend class frontend::ScopeCreationData;
+  friend class ScopeCreationData;
   static const ScopeKind classScopeKind_ = ScopeKind::Module;
 
  public:
