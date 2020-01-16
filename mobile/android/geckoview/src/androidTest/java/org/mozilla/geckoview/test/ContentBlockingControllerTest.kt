3821b4074c036a8bd1dda6d73a234cc91011cccf
Dylan Roeh <droeh@mozilla.com>
2020-01-16 18:09:45 +0000

    Bug 1587552 - Update GeckoView content blocking exception API and add better support for removing exceptions. r=esawin,Ehsan,geckoview-reviewers,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D58828

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentBlockingControllerTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentBlockingControllerTest.kt
@@ -13,6 +13,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mozilla.geckoview.ContentBlocking
 import org.mozilla.geckoview.ContentBlockingController
+import org.mozilla.geckoview.ContentBlockingController.ContentBlockingException
 import org.mozilla.geckoview.GeckoSession
 import org.mozilla.geckoview.test.rule.GeckoSessionTestRule
 import org.mozilla.geckoview.test.rule.GeckoSessionTestRule.AssertCalled
@@ -23,9 +24,9 @@ import org.junit.Assume.assumeThat
 @MediumTest
 class ContentBlockingControllerTest : BaseSessionTest() {
     @GeckoSessionTestRule.Setting(key = GeckoSessionTestRule.Setting.Key.USE_TRACKING_PROTECTION, value = "true")
-// disable test on debug for frequently failing #Bug 1580223
     @Test
     fun trackingProtectionException() {
+        // disable test on debug for frequently failing #Bug 1580223
         assumeThat(sessionRule.env.isDebugBuild, equalTo(false))
         val category = ContentBlocking.AntiTracking.TEST
         sessionRule.runtime.settings.contentBlocking.setAntiTracking(category)
@@ -44,6 +45,7 @@ class ContentBlockingControllerTest : BaseSessionTest() {
                     }
                 })
 
+        // Add exception for this site.
         sessionRule.runtime.contentBlockingController.addException(sessionRule.session)
 
         sessionRule.runtime.contentBlockingController.checkException(sessionRule.session).accept {
@@ -52,7 +54,7 @@ class ContentBlockingControllerTest : BaseSessionTest() {
 
         var list = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController.saveExceptionList())
         assertThat("Exceptions list should not be null", list, Matchers.notNullValue())
-        assertThat("Exceptions list should have one entry", list.uris.size, Matchers.equalTo(1))
+        assertThat("Exceptions list should have one entry", list.size, Matchers.equalTo(1))
 
         sessionRule.session.reload()
         sessionRule.waitForPageStop()
@@ -65,11 +67,79 @@ class ContentBlockingControllerTest : BaseSessionTest() {
                     }
                 })
 
+        // Remove exception for this site by passing GeckoSession.
         sessionRule.runtime.contentBlockingController.removeException(sessionRule.session)
 
         list = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController.saveExceptionList())
         assertThat("Exceptions list should not be null", list, Matchers.notNullValue())
-        assertThat("Exceptions list should have one entry", list.uris.size, Matchers.equalTo(0))
+        assertThat("Exceptions list should have one entry", list.size, Matchers.equalTo(0))
+
+        sessionRule.session.reload()
+
+        sessionRule.waitUntilCalled(
+                object : Callbacks.ContentBlockingDelegate {
+                    @GeckoSessionTestRule.AssertCalled(count=3)
+                    override fun onContentBlocked(session: GeckoSession,
+                                                  event: ContentBlocking.BlockEvent) {
+                        assertThat("Category should be set",
+                                event.antiTrackingCategory,
+                                Matchers.equalTo(category))
+                        assertThat("URI should not be null", event.uri, Matchers.notNullValue())
+                        assertThat("URI should match", event.uri, Matchers.endsWith("tracker.js"))
+                    }
+                })
+    }
+
+    @GeckoSessionTestRule.Setting(key = GeckoSessionTestRule.Setting.Key.USE_TRACKING_PROTECTION, value = "true")
+    @Test
+    fun trackingProtectionExceptionRemoveByException() {
+        // disable test on debug for frequently failing #Bug 1580223
+        assumeThat(sessionRule.env.isDebugBuild, equalTo(false))
+        val category = ContentBlocking.AntiTracking.TEST
+        sessionRule.runtime.settings.contentBlocking.setAntiTracking(category)
+        sessionRule.session.loadTestPath(TRACKERS_PATH)
+
+        sessionRule.waitUntilCalled(
+                object : Callbacks.ContentBlockingDelegate {
+                    @GeckoSessionTestRule.AssertCalled(count=3)
+                    override fun onContentBlocked(session: GeckoSession,
+                                                  event: ContentBlocking.BlockEvent) {
+                        assertThat("Category should be set",
+                                event.antiTrackingCategory,
+                                Matchers.equalTo(category))
+                        assertThat("URI should not be null", event.uri, Matchers.notNullValue())
+                        assertThat("URI should match", event.uri, Matchers.endsWith("tracker.js"))
+                    }
+                })
+
+        // Add exception for this site.
+        sessionRule.runtime.contentBlockingController.addException(sessionRule.session)
+
+        sessionRule.runtime.contentBlockingController.checkException(sessionRule.session).accept {
+            assertThat("Site should be on exceptions list", it, Matchers.equalTo(true))
+        }
+
+        var list = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController.saveExceptionList())
+        assertThat("Exceptions list should not be null", list, Matchers.notNullValue())
+        assertThat("Exceptions list should have one entry", list.size, Matchers.equalTo(1))
+
+        sessionRule.session.reload()
+        sessionRule.waitForPageStop()
+
+        sessionRule.forCallbacksDuringWait(
+                object : Callbacks.ContentBlockingDelegate {
+                    @GeckoSessionTestRule.AssertCalled(false)
+                    override fun onContentBlocked(session: GeckoSession,
+                                                  event: ContentBlocking.BlockEvent) {
+                    }
+                })
+
+        // Remove exception for this site by passing ContentBlockingException.
+        sessionRule.runtime.contentBlockingController.removeException(list.get(0))
+
+        list = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController.saveExceptionList())
+        assertThat("Exceptions list should not be null", list, Matchers.notNullValue())
+        assertThat("Exceptions list should have one entry", list.size, Matchers.equalTo(0))
 
         sessionRule.session.reload()
 
@@ -103,12 +173,9 @@ class ContentBlockingControllerTest : BaseSessionTest() {
         var export = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController
                 .saveExceptionList())
         assertThat("Exported list must not be null", export, Matchers.notNullValue())
-        assertThat("Exported list must contain one entry", export.uris.size, Matchers.equalTo(1))
+        assertThat("Exported list must contain one entry", export.size, Matchers.equalTo(1))
 
-        val exportStr = export.toString()
-        assertThat("Exported string must not be null", exportStr, Matchers.notNullValue())
-
-        val exportJson = export.toJson()
+        val exportJson = export.get(0).toJson()
         assertThat("Exported JSON must not be null", exportJson, Matchers.notNullValue())
 
         // Wipe
@@ -116,32 +183,16 @@ class ContentBlockingControllerTest : BaseSessionTest() {
         export = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController
                 .saveExceptionList())
         assertThat("Exported list must not be null", export, Matchers.notNullValue())
-        assertThat("Exported list must contain zero entries", export.uris.size, Matchers.equalTo(0))
-
-        // Restore from String
-        val importStr = sessionRule.runtime.contentBlockingController.ExceptionList(exportStr)
-        sessionRule.runtime.contentBlockingController.restoreExceptionList(importStr)
-
-        export = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController
-                .saveExceptionList())
-        assertThat("Exported list must not be null", export, Matchers.notNullValue())
-        assertThat("Exported list must contain one entry", export.uris.size, Matchers.equalTo(1))
-
-        // Wipe
-        sessionRule.runtime.contentBlockingController.clearExceptionList()
-        export = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController
-                .saveExceptionList())
-        assertThat("Exported list must not be null", export, Matchers.notNullValue())
-        assertThat("Exported list must contain zero entries", export.uris.size, Matchers.equalTo(0))
+        assertThat("Exported list must contain zero entries", export.size, Matchers.equalTo(0))
 
         // Restore from JSON
-        val importJson = sessionRule.runtime.contentBlockingController.ExceptionList(exportJson)
+        val importJson = listOf(ContentBlockingException.fromJson(exportJson))
         sessionRule.runtime.contentBlockingController.restoreExceptionList(importJson)
 
         export = sessionRule.waitForResult(sessionRule.runtime.contentBlockingController
                 .saveExceptionList())
         assertThat("Exported list must not be null", export, Matchers.notNullValue())
-        assertThat("Exported list must contain one entry", export.uris.size, Matchers.equalTo(1))
+        assertThat("Exported list must contain one entry", export.size, Matchers.equalTo(1))
 
         // Wipe so as not to break other tests.
         sessionRule.runtime.contentBlockingController.clearExceptionList()
