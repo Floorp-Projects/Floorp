a034f080abd56f6420cae40263730b5a1d479bcf
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-01-16 15:32:20 +0000

    Bug 1609630 - Fix eager evaluation when paused in debugger. r=jlast.
    We weren't await-ing the result of webConsoleUI.getFrameActor,
    which means we wouldn't get the correct front for evaluating
    the input.
    This patch fixes this and adds a patch to ensure we don't regress.
    
    Depends on D60123
    
    Differential Revision: https://phabricator.services.mozilla.com/D60124

---
+++ devtools/client/webconsole/test/browser/browser_jsterm_eager_evaluation_in_debugger_stackframe.js
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Test that eager evaluation works as expected when paused in the debugger.
+
+const TEST_URI = `data:text/html;charset=utf-8,
+<script>
+var x = "global";
+
+function pauseInDebugger(param) {
+  let x = "local";
+  debugger;
+}
+
+</script>
+`;
+
+const EAGER_EVALUATION_PREF = "devtools.webconsole.input.eagerEvaluation";
+
+add_task(async function() {
+  await pushPref(EAGER_EVALUATION_PREF, true);
+  const hud = await openNewTabAndConsole(TEST_URI);
+
+  const target = await TargetFactory.forTab(gBrowser.selectedTab);
+  const toolbox = gDevTools.getToolbox(target);
+
+  setInputValue(hud, "x");
+  await waitForEagerEvaluationResult(hud, `"global"`);
+
+  info("Open Debugger");
+  await openDebugger();
+  const dbg = createDebuggerContext(toolbox);
+
+  info("Pause in Debugger");
+  SpecialPowers.spawn(gBrowser.selectedBrowser, [], function() {
+    content.wrappedJSObject.pauseInDebugger("myParam");
+  });
+  await pauseDebugger(dbg);
+
+  info("Opening Console");
+  await toolbox.selectTool("webconsole");
+
+  info("Check that the parameter is eagerly evaluated as expected");
+  setInputValue(hud, "param");
+  await waitForEagerEvaluationResult(hud, `"myParam"`);
+
+  setInputValue(hud, "x");
+  await waitForEagerEvaluationResult(hud, `"local"`);
+
+  await resume(dbg);
+});
