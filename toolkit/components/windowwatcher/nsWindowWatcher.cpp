c649a533c49834be1dd152e972e19dd81e4fa300
pbz <pbz@mozilla.com>
2020-01-16 14:38:08 +0000

    Bug 1432856 - Leave DOM fullscreen on window.open(). r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D55809

--- toolkit/components/windowwatcher/nsWindowWatcher.cpp
+++ toolkit/components/windowwatcher/nsWindowWatcher.cpp
@@ -1308,6 +1308,11 @@ nsresult nsWindowWatcher::OpenWindowInternal(
       newChrome->ShowAsModal();
     }
   }
+  // If a website opens a popup exit DOM fullscreen
+  if (windowIsNew && aCalledFromJS && !hasChromeParent && !isCallerChrome &&
+      parentWindow) {
+    Document::AsyncExitFullscreen(parentWindow->GetDoc());
+  }
 
   if (aForceNoOpener && windowIsNew) {
     NS_RELEASE(*aResult);
