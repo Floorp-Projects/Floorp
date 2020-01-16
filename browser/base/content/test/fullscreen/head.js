899c4467e44b0d46465087a08bd4cbbe182d3836
pbz <pbz@mozilla.com>
2020-01-16 14:39:11 +0000

    Bug 1432856 - Added tests for window open and focus in fullscreen. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D56391
    
    --HG--
    extra : moz-landing-system : lando

--- browser/base/content/test/fullscreen/head.js
+++ browser/base/content/test/fullscreen/head.js
@@ -2,7 +2,6 @@ const { ContentTaskUtils } = ChromeUtils.import(
   "resource://testing-common/ContentTaskUtils.jsm"
 );
 function waitForFullScreenState(browser, state) {
-  info("inside waitforfullscreenstate");
   return new Promise(resolve => {
     let eventReceived = false;
 
@@ -15,7 +14,7 @@ function waitForFullScreenState(browser, state) {
     };
     Services.obs.addObserver(observe, "fullscreen-painted");
 
-    window.addEventListener(
+    browser.ownerGlobal.addEventListener(
       `MozDOMFullscreen:${state ? "Entered" : "Exited"}`,
       () => {
         eventReceived = true;
@@ -49,4 +48,79 @@ async function changeFullscreen(browser, fullScreenState) {
     }
   });
   return fullScreenChange;
+}
+
+async function testExpectFullScreenExit(browser, leaveFS, action) {
+  let fsPromise = waitForFullScreenState(browser, !leaveFS);
+  if (leaveFS) {
+    if (action) {
+      await action();
+    }
+    await fsPromise;
+    ok(true, "Should leave full-screen");
+  } else {
+    if (action) {
+      await action();
+    }
+    let result = await Promise.race([
+      fsPromise,
+      new Promise(resolve => {
+        SimpleTest.requestFlakyTimeout("Wait for failure condition");
+        // eslint-disable-next-line mozilla/no-arbitrary-setTimeout
+        setTimeout(() => resolve(true), 2500);
+      }),
+    ]);
+    ok(result, "Should not leave full-screen");
+  }
+}
+
+function jsWindowFocus(browser, iframeId) {
+  return ContentTask.spawn(browser, { iframeId }, async args => {
+    let destWin = content;
+    if (args.iframeId) {
+      let iframe = content.document.getElementById(args.iframeId);
+      if (!iframe) {
+        throw new Error("iframe not set");
+      }
+      destWin = iframe.contentWindow;
+    }
+    await content.wrappedJSObject.sendMessage(destWin, "focus");
+  });
+}
+
+async function jsWindowOpen(browser, iframeId) {
+  let windowOpened = BrowserTestUtils.waitForNewWindow();
+  ContentTask.spawn(browser, { iframeId }, async args => {
+    let destWin = content;
+    if (args.iframeId) {
+      // Create a cross origin iframe
+      destWin = (await content.wrappedJSObject.createIframe(
+        args.iframeId,
+        true
+      )).contentWindow;
+    }
+    // Send message to either the iframe or the current page to open a popup
+    await content.wrappedJSObject.sendMessage(destWin, "open");
+  });
+  return windowOpened;
+}
+
+function waitForFocus(...args) {
+  return new Promise(resolve => SimpleTest.waitForFocus(resolve, ...args));
+}
+
+function waitForBrowserWindowActive(win) {
+  return new Promise(resolve => {
+    if (Services.focus.activeWindow == win) {
+      resolve();
+    } else {
+      win.addEventListener(
+        "activate",
+        () => {
+          resolve();
+        },
+        { once: true }
+      );
+    }
+  });
 }
