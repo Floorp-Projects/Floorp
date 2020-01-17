23a209efaa1db066e368010edd38504f315db83d
Mike Hommey <mh+mozilla@glandium.org>
2020-01-17 00:23:11 +0000

    Bug 1588538 - Add missing file.
    Differential Revision: https://phabricator.services.mozilla.com/D60196

---
+++ python/mozboot/mozboot/dump_syms.py
@@ -0,0 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+WIN64_DUMP_SYMS = 'win64-dump-syms'
