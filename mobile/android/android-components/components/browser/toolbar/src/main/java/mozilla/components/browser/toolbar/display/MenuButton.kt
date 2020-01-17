ee5372f81bb83816699a8db2821cfc02a3f40d41
Tiger Oakes <toakes@mozilla.com>
2020-01-16 17:42:06 -0800

    [components] For https://github.com/mozilla-mobile/fenix/issues/7506 - Expose MenuButton

--- mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/MenuButton.kt
+++ mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/MenuButton.kt
@@ -4,78 +4,30 @@
 
 package mozilla.components.browser.toolbar.display
 
-import android.content.Context
-import android.content.res.ColorStateList
-import android.util.AttributeSet
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageView
 import androidx.annotation.ColorInt
-import androidx.annotation.DrawableRes
 import androidx.annotation.VisibleForTesting
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.content.ContextCompat
-import mozilla.components.browser.menu.BrowserMenu
+import androidx.core.view.isVisible
 import mozilla.components.browser.menu.BrowserMenuBuilder
-import mozilla.components.browser.menu.BrowserMenuHighlight
-import mozilla.components.browser.menu.HighlightableMenuItem
-import mozilla.components.browser.toolbar.R
+import mozilla.components.browser.menu.ext.getHighlight
 import mozilla.components.browser.toolbar.facts.emitOpenMenuFact
-import mozilla.components.support.ktx.android.content.res.resolveAttribute
 
-internal class MenuButton @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
-
-    @VisibleForTesting internal var menu: BrowserMenu? = null
-
-    private val menuIcon =
-        createImageView(R.drawable.mozac_ic_menu, context.getString(R.string.mozac_browser_toolbar_menu_button))
-    private val highlightView =
-        createImageView(R.drawable.mozac_menu_indicator)
-    private val notificationIconView =
-        createImageView(R.drawable.mozac_menu_notification)
+internal class MenuButton(
+    @VisibleForTesting
+    internal val impl: mozilla.components.browser.menu.view.MenuButton
+) {
 
     init {
-        setBackgroundResource(context.theme.resolveAttribute(android.R.attr.selectableItemBackgroundBorderless))
-
-        visibility = View.GONE
-        isClickable = true
-        isFocusable = true
-
-        setOnClickListener {
-            if (menu == null) {
-                menu = menuBuilder?.build(context)
-                val endAlwaysVisible = menuBuilder?.endOfMenuAlwaysVisible ?: false
-                menu?.show(
-                    anchor = this,
-                    orientation = BrowserMenu.determineMenuOrientation(parent as View?),
-                    endOfMenuAlwaysVisible = endAlwaysVisible
-                ) { menu = null }
-
-                emitOpenMenuFact(menuBuilder?.extras)
-            } else {
-                menu?.dismiss()
-            }
+        impl.isVisible = false
+        impl.onShow = {
+            emitOpenMenuFact(impl.menuBuilder?.extras)
         }
-
-        addView(highlightView)
-        addView(menuIcon)
-        addView(notificationIconView)
     }
 
-    var menuBuilder: BrowserMenuBuilder? = null
+    var menuBuilder: BrowserMenuBuilder?
+        get() = impl.menuBuilder
         set(value) {
-            field = value
-            menu?.dismiss()
-            if (value != null) {
-                visibility = View.VISIBLE
-            } else {
-                visibility = View.GONE
-                menu = null
-            }
+            impl.menuBuilder = value
+            impl.isVisible = value != null
         }
 
     /**
@@ -83,65 +35,11 @@ internal class MenuButton @JvmOverloads constructor(
      */
     @Suppress("Deprecation")
     fun invalidateMenu() {
-        menu?.invalidate()
-
-        val highlight = menuBuilder?.items.orEmpty()
-            .asSequence()
-            .filter { it.visible() }
-            .mapNotNull { it as? HighlightableMenuItem }
-            .filter { it.isHighlighted() }
-            .map { it.highlight }
-            .maxBy {
-                // Select the highlight with the highest priority
-                when (it) {
-                    is BrowserMenuHighlight.HighPriority -> 2
-                    is BrowserMenuHighlight.LowPriority -> 1
-                    is BrowserMenuHighlight.ClassicHighlight -> 0
-                }
-            }
-
-        // If a highlighted item is found, show the indicator
-        when (highlight) {
-            is BrowserMenuHighlight.HighPriority -> {
-                highlightView.imageTintList = ColorStateList.valueOf(highlight.backgroundTint)
-                highlightView.visibility = View.VISIBLE
-                notificationIconView.visibility = View.GONE
-            }
-            is BrowserMenuHighlight.LowPriority -> {
-                notificationIconView.setColorFilter(highlight.notificationTint)
-                highlightView.visibility = View.GONE
-                notificationIconView.visibility = View.VISIBLE
-            }
-            is BrowserMenuHighlight.ClassicHighlight -> {
-                val color = ContextCompat.getColor(context, highlight.colorResource)
-                highlightView.imageTintList = ColorStateList.valueOf(color)
-                highlightView.visibility = View.VISIBLE
-                notificationIconView.visibility = View.GONE
-            }
-            null -> {
-                highlightView.visibility = View.GONE
-                notificationIconView.visibility = View.GONE
-            }
-        }
+        impl.invalidateBrowserMenu()
+        impl.setHighlight(menuBuilder?.items?.getHighlight())
     }
 
-    fun dismissMenu() {
-        menu?.dismiss()
-    }
+    fun dismissMenu() = impl.dismissMenu()
 
-    fun setColorFilter(@ColorInt color: Int) {
-        menuIcon.setColorFilter(color)
-    }
-
-    private fun createImageView(@DrawableRes image: Int, description: String? = null) =
-        AppCompatImageView(context).apply {
-            setImageResource(image)
-            scaleType = ImageView.ScaleType.CENTER
-            if (description != null) {
-                contentDescription = description
-            } else {
-                importantForAccessibility = View.IMPORTANT_FOR_ACCESSIBILITY_NO
-                visibility = View.GONE
-            }
-        }
+    fun setColorFilter(@ColorInt color: Int) = impl.setColorFilter(color)
 }
--- mobile/android/android-components/components/browser/toolbar/src/test/java/mozilla/components/browser/toolbar/display/MenuButtonTest.kt
+++ mobile/android/android-components/components/browser/toolbar/src/test/java/mozilla/components/browser/toolbar/display/MenuButtonTest.kt
@@ -4,37 +4,21 @@
 
 package mozilla.components.browser.toolbar.display
 
-import android.content.Context
-import android.content.res.Resources
 import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
-import android.graphics.drawable.Drawable
 import android.view.View
-import android.widget.ImageView
-import androidx.annotation.ColorInt
-import androidx.annotation.ColorRes
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.core.view.children
-import androidx.core.view.isVisible
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.browser.menu.BrowserMenu
 import mozilla.components.browser.menu.BrowserMenuBuilder
 import mozilla.components.browser.menu.BrowserMenuHighlight
+import mozilla.components.browser.menu.ext.getHighlight
 import mozilla.components.browser.menu.item.BrowserMenuHighlightableItem
-import mozilla.components.browser.toolbar.R
 import mozilla.components.support.test.any
-import mozilla.components.support.test.eq
 import mozilla.components.support.test.mock
 import mozilla.components.support.test.robolectric.testContext
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyBoolean
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.never
@@ -46,121 +30,37 @@ import org.mockito.Mockito.verify
 class MenuButtonTest {
     private lateinit var menuBuilder: BrowserMenuBuilder
     private lateinit var menu: BrowserMenu
+    private lateinit var menuButtonInternal: mozilla.components.browser.menu.view.MenuButton
     private lateinit var menuButton: MenuButton
-    private lateinit var menuIcon: ImageView
-    private lateinit var highlightView: ImageView
-    private lateinit var notificationIconView: ImageView
 
     @Before
     fun setup() {
-        val context = spy(testContext)
-        doReturn("Menu").`when`(context).getString(R.string.mozac_browser_toolbar_menu_button)
-
         menu = mock()
-
         menuBuilder = mock()
-        `when`(menuBuilder.build(context)).thenReturn(menu)
+        doReturn(menu).`when`(menuBuilder).build(testContext)
 
-        menuButton = MenuButton(context)
-        val images = menuButton.children.mapNotNull { it as? AppCompatImageView }.toList()
-        highlightView = images[0]
-        menuIcon = images[1]
-        notificationIconView = images[2]
+        menuButtonInternal = mock()
+        menuButton = MenuButton(menuButtonInternal)
     }
 
     @Test
     fun `menu button is visible only if menu builder attached`() {
-        assertEquals(View.GONE, menuButton.visibility)
+        verify(menuButtonInternal).visibility = View.GONE
 
         menuButton.menuBuilder = mock()
-        assertEquals(View.VISIBLE, menuButton.visibility)
+        verify(menuButtonInternal).visibility = View.VISIBLE
 
         menuButton.menuBuilder = null
-        assertEquals(View.GONE, menuButton.visibility)
-    }
-
-    @Test
-    fun `changing menu builder dismisses old menu`() {
-        menuButton.menuBuilder = menuBuilder
-        menuButton.performClick()
-
-        verify(menu).show(eq(menuButton), any(), anyBoolean(), any())
-
-        menuButton.menuBuilder = mock()
-        verify(menu).dismiss()
-    }
-
-    @Test
-    fun `trying to open a new menu when we already have one will dismiss the current`() {
-        menuButton.menuBuilder = menuBuilder
-
-        menuButton.performClick()
-        menuButton.performClick()
-
-        verify(menu, times(1)).show(eq(menuButton), any(), anyBoolean(), any())
-        verify(menu, times(1)).dismiss()
-    }
-
-    @Test
-    fun `icon has content description`() {
-        assertEquals("Menu", menuIcon.contentDescription)
-        assertNotNull(menuIcon.drawable)
-    }
-
-    @Test
-    fun `icon color filter can be changed`() {
-        assertNull(menuIcon.colorFilter)
-
-        menuButton.setColorFilter(0xffffff)
-        assertEquals(PorterDuffColorFilter(0xffffff, PorterDuff.Mode.SRC_ATOP), menuIcon.colorFilter)
-    }
-
-    @Test
-    fun `icon displays highlight if highlighted item is present in menu`() {
-        val colorResource = 100
-        val context = mockContextWithColorResource(colorResource, 0xffffff)
-
-        val menuButton = MenuButton(context)
-        assertFalse(highlightView.isVisible)
-        assertFalse(notificationIconView.isVisible)
-
-        var isHighlighted = false
-        val highlight = spy(BrowserMenuHighlight.HighPriority(colorResource))
-        val highlightMenuBuilder = spy(BrowserMenuBuilder(listOf(
-            BrowserMenuHighlightableItem(
-                label = "Test",
-                startImageResource = 0,
-                highlight = highlight,
-                isHighlighted = { isHighlighted }
-            )
-        )))
-        `when`(highlightMenuBuilder.build(context)).thenReturn(menu)
-
-        menuButton.menuBuilder = highlightMenuBuilder
-        menuButton.invalidateMenu()
-
-        verify(menu, never()).invalidate()
-        assertFalse(highlightView.isVisible)
-        assertFalse(notificationIconView.isVisible)
-
-        isHighlighted = true
-        menuButton.invalidateMenu()
-
-        verify(highlight).backgroundTint
-        verify(context).getDrawable(R.drawable.mozac_menu_indicator)
+        verify(menuButtonInternal, times(2)).visibility = View.GONE
     }
 
     @Test
     fun `icon displays dot if low highlighted item is present in menu`() {
-        val colorResource = 200
-        val context = mockContextWithColorResource(colorResource, Color.BLUE)
-
-        val menuButton = MenuButton(context)
-        assertFalse(highlightView.isVisible)
-        assertFalse(notificationIconView.isVisible)
+        verify(menuButtonInternal, never()).invalidateBrowserMenu()
+        verify(menuButtonInternal, never()).setHighlight(any())
 
         var isHighlighted = false
-        val highlight = spy(BrowserMenuHighlight.LowPriority(colorResource))
+        val highlight = BrowserMenuHighlight.LowPriority(Color.YELLOW)
         val highlightMenuBuilder = spy(BrowserMenuBuilder(listOf(
             BrowserMenuHighlightableItem(
                 label = "Test",
@@ -169,45 +69,18 @@ class MenuButtonTest {
                 isHighlighted = { isHighlighted }
             )
         )))
-        `when`(highlightMenuBuilder.build(context)).thenReturn(menu)
+        `when`(highlightMenuBuilder.build(testContext)).thenReturn(menu)
 
         menuButton.menuBuilder = highlightMenuBuilder
+        doReturn(highlightMenuBuilder).`when`(menuButtonInternal).menuBuilder
         menuButton.invalidateMenu()
 
-        verify(menu, never()).invalidate()
-        assertFalse(highlightView.isVisible)
-        assertFalse(notificationIconView.isVisible)
+        verify(menuButtonInternal).setHighlight(null)
 
         isHighlighted = true
         menuButton.invalidateMenu()
 
-        verify(highlight).notificationTint
-        verify(context).getDrawable(R.drawable.mozac_menu_notification)
-    }
-
-    @Test
-    fun `menu can be dismissed`() {
-        menuButton.menu = menu
-
-        menuButton.dismissMenu()
-
-        verify(menuButton.menu)?.dismiss()
-    }
-
-    private fun mockContextWithColorResource(@ColorRes resId: Int, @ColorInt color: Int): Context {
-        val context: Context = spy(testContext)
-
-        val drawable: Drawable = mock()
-        doReturn(drawable).`when`(drawable).mutate()
-        doReturn(drawable).`when`(context).getDrawable(R.drawable.mozac_menu_indicator)
-
-        val res: Resources = spy(testContext.resources)
-        doReturn(res).`when`(context).resources
-
-        doReturn(color).`when`(context).getColor(resId)
-        @Suppress("Deprecation")
-        doReturn(color).`when`(res).getColor(resId)
-
-        return context
+        assertEquals(highlight, highlightMenuBuilder.items.getHighlight())
+        verify(menuButtonInternal).setHighlight(highlight)
     }
 }
