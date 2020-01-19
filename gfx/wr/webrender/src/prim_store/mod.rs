b44b6af7d40547415fa7fb85b9552c12ff154cad
Nicolas Silva <nsilva@mozilla.com>
2020-01-19 19:32:11 +0000

    Bug 1607836 - Preallocate picture task dependency vectors. r=gw
    Unlike other types of render tasks, pictures can have hundreds of dependencies. The dependency vector is re-built every frame, leading to a lot of vector re-allocations in some pages.
    
    Depends on D60151
    
    Differential Revision: https://phabricator.services.mozilla.com/D60182
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/wr/webrender/src/prim_store/mod.rs
+++ gfx/wr/webrender/src/prim_store/mod.rs
@@ -2732,6 +2732,7 @@ impl PrimitiveStore {
                 // Restore the dependencies (borrow check dance)
                 self.pictures[pic_context_for_children.pic_index.0]
                     .restore_context(
+                        pic_context.surface_index,
                         prim_list,
                         pic_context_for_children,
                         pic_state_for_children,
