8f2a59dcb56f9f8e8ca0171041885f4832c9233d
Emilio Cobos Álvarez <emilio@crisal.io>
2020-01-20 16:18:28 +0000

    Bug 1609996 - Fix some things that the automated replacement didn't catch. r=froydnj
    This makes Gecko build again. I'll squash this into the previous commit anyhow.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60324

--- dom/media/webm/WebMDemuxer.h
+++ dom/media/webm/WebMDemuxer.h
@@ -3,17 +3,17 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#if !defined(WebMDemuxer_h_)
-#  define WebMDemuxer_h_
+#ifndef WebMDemuxer_h_
+#define WebMDemuxer_h_
 
-#  include "nsTArray.h"
-#  include "MediaDataDemuxer.h"
-#  include "MediaResource.h"
-#  include "NesteggPacketHolder.h"
-#  include "mozilla/Move.h"
+#include "nsTArray.h"
+#include "MediaDataDemuxer.h"
+#include "MediaResource.h"
+#include "NesteggPacketHolder.h"
 
-#  include <deque>
-#  include <stdint.h>
+#include <deque>
+#include <stdint.h>
+#include <utility>
 
 typedef struct nestegg nestegg;
 
