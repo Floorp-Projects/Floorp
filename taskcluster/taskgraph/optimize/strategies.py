0eb4d8b37dcbc84bec16f66a7102aa12f2b87676
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-01-21 17:12:56 +0000

    Bug 1609967 - [taskgraph] Implement a relevant tests optimizer, r=jmaher
    An experimental optimizer that schedules all test tasks that run tests that
    are in the same directory (or subdir) of a modified file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60527

--- taskcluster/taskgraph/optimize/strategies.py
+++ taskcluster/taskgraph/optimize/strategies.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import logging
 import os
 
+import mozpack.path as mozpath
 from mozbuild.base import MozbuildObject
 from mozbuild.util import memoize
 
@@ -89,4 +90,30 @@ class SkipUnlessSchedules(OptimizationStrategy):
         if conditions & scheduled:
             return False
 
+        return True
+
+
+@register_strategy("skip-unless-has-relevant-tests")
+class SkipUnlessHasRelevantTests(OptimizationStrategy):
+    """Optimizes tasks that don't run any tests that were
+    in child directories of a modified file.
+    """
+
+    @memoize
+    def get_changed_dirs(self, repo, rev):
+        changed = map(mozpath.dirname, files_changed.get_changed_files(repo, rev))
+        # Filter out empty directories (from files modified in the root).
+        # Otherwise all tasks would be scheduled.
+        return {d for d in changed if d}
+
+    def should_remove_task(self, task, params, _):
+        if not task.attributes.get('test_manifests'):
+            return True
+
+        for d in self.get_changed_dirs(params['head_repository'], params['head_rev']):
+            for t in task.attributes['test_manifests']:
+                if t.startswith(d):
+                    logger.debug('{} runs a test path ({}) contained by a modified file ({})'
+                                 .format(task.label, t, d))
+                    return False
         return True
