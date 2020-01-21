bcf590f0ac7f376c5841865f3fcd761774826945
Kris Maglione <maglione.k@gmail.com>
2020-01-21 21:32:28 +0000

    Bug 1602898: Rename Window.getWindowGlobalChild() to .windowGlobalChild. r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D56609

--- browser/components/syncedtabs/sidebar.js
+++ browser/components/syncedtabs/sidebar.js
@@ -35,10 +35,7 @@ let onLoaded = () => {
   window.addEventListener(
     "pageshow",
     e => {
-      window
-        .getWindowGlobalChild()
-        .getActor("LightweightTheme")
-        .handleEvent(e);
+      window.windowGlobalChild.getActor("LightweightTheme").handleEvent(e);
     },
     { once: true }
   );
