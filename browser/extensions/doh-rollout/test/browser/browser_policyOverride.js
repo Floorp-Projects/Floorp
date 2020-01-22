54efda898a89eb295310032dd6473f0aaa7d76dd
Nihanth Subramanya <nhnt11@gmail.com>
2020-01-22 22:16:46 +0000

    Bug 1600109 - Fix network change handling and test telemetry. r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D58196

--- browser/extensions/doh-rollout/test/browser/browser_policyOverride.js
+++ browser/extensions/doh-rollout/test/browser/browser_policyOverride.js
@@ -38,25 +38,28 @@ add_task(async function testPolicyOverride() {
     "Breadcrumb not saved."
   );
   await ensureNoTRRModeChange(0);
+  await checkHeuristicsTelemetry("policy_without_doh", "first_run");
 
   // Simulate a network change.
   simulateNetworkChange();
   await ensureNoTRRModeChange(0);
+  ensureNoHeuristicsTelemetry();
 
   // Restart for good measure.
   await restartAddon();
   await ensureNoTRRModeChange(0);
+  await checkHeuristicsTelemetry("policy_without_doh", "startup");
 
   // Simulate a network change.
   simulateNetworkChange();
   await ensureNoTRRModeChange(0);
+  ensureNoHeuristicsTelemetry();
 
   // Clean up.
   await EnterprisePolicyTesting.setupPolicyEngineWithJson({
     policies: {},
   });
   EnterprisePolicyTesting.resetRunOnceState();
-  await resetPrefsAndRestartAddon();
 
   is(
     Services.policies.status,
