786c21f1f0ee9102523ad4bfd956bfa1726f4310
Dan Minor <dminor@mozilla.com>
2020-01-22 16:06:28 +0000

    Bug 1607205 - Reset RtcpEventObserver when deleting receive stream; r=ng
    Holding a RefPtr to this in OnRtcpBye should keep the VideoConduit alive until
    the callback completes. If this is not happening, it is likely that we're
    racing with the destructor on the other thread. Clearing the callback early
    will prevent this. This should not affect the VideoConduit because in that case
    the callback is directly part of the receive stream, not attached to a channel.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60658

--- media/webrtc/signaling/src/media-conduit/AudioConduit.cpp
+++ media/webrtc/signaling/src/media-conduit/AudioConduit.cpp
@@ -1014,6 +1014,8 @@ void WebrtcAudioConduit::DeleteRecvStream() {
     mCall->Call()->DestroyAudioReceiveStream(mRecvStream);
     mRecvStream = nullptr;
   }
+  mRecvChannelProxy->SetRtcpEventObserver(nullptr);
+
   // Destroying the stream unregisters the transport
   mRecvChannelProxy->RegisterTransport(nullptr);
 }
