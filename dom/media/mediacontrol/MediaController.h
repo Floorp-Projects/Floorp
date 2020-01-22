20023a67251ec8ba3b50f887fed6b0f1b1066dbe
alwu <alwu@mozilla.com>
2020-01-22 21:21:16 +0000

    Bug 1610723 - implement controller's track selection and seek methods. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D60625

--- dom/media/mediacontrol/MediaController.h
+++ dom/media/mediacontrol/MediaController.h
@@ -61,6 +61,10 @@ class MediaController final {
   void Play();
   void Pause();
   void Stop();
+  void PrevTrack();
+  void NextTrack();
+  void SeekBackward();
+  void SeekForward();
 
   // Calling this method explicitly would mark this controller as deprecated,
   // then calling any its method won't take any effect.
