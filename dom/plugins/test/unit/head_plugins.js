4372aa03c1d9d9d493ee141b20606939425f76ab
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-01-22 10:18:22 +0000

    Bug 1609257 - only read flash only pref in automation, r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D60335

--- dom/plugins/test/unit/head_plugins.js
+++ dom/plugins/test/unit/head_plugins.js
@@ -12,6 +12,13 @@ const gDirSvc = Services.dirsvc;
 
 function allow_all_plugins() {
   Services.prefs.setBoolPref("plugin.load_flash_only", false);
+  // plugin.load_flash_only is only respected if xpc::IsInAutomation is true.
+  // This is not the case by default in xpcshell tests, unless the following
+  // pref is also set. Fixing this generically is bug 1598804
+  Services.prefs.setBoolPref(
+    "security.turn_off_all_security_so_that_viruses_can_take_over_this_computer",
+    true
+  );
 }
 
 // Finds the test plugin library
