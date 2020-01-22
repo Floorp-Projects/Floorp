c61d70889d7e8c3e61307f8bfa78968513a1f6fc
Agi Sferro <agi@sferro.dev>
2020-01-22 15:19:49 +0000

    Bug 1610641 - Remove all remaining Fennec modules. r=esawin
    Differential Revision: https://phabricator.services.mozilla.com/D60586

--- mobile/android/.eslintrc.js
+++ mobile/android/.eslintrc.js
@@ -25,8 +25,6 @@ module.exports = {
       "chrome/**",
       // Bug 1425048.
       "components/extensions/**",
-      // Bug 1425034.
-      "modules/WebsiteMetadata.jsm",
     ],
     rules: {
       "no-unused-vars": "off",
