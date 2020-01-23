ce2204248f6c9b226d8a4d8cb636c38638cdde40
Aaron Klotz <aklotz@mozilla.com>
2020-01-23 18:12:51 +0000

    Bug 1595834: Make GeckoProcessManager interface e10s-multi aware; r=snorp
    This patch does not change any of the *internals* of process management in
    `GeckoProcessManager`, however it upgrades the interface in preparation for
    such changes.
    
    * We add a java equivalent to Gecko's `GeckoProcessType` enumeration;
    * We add the `GeckoProcessType.Selector` class which will be used for
      identifying running content processes;
    * We update `ContentParent::MarkAsDead` to use `GeckoProcessType.Selector` for
      indicating which process should be marked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60539
    
    --HG--
    extra : moz-landing-system : lando

---
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoProcessType.java
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gecko.process;
+
+import org.mozilla.gecko.annotation.WrapForJNI;
+
+@WrapForJNI
+public enum GeckoProcessType {
+    // These need to match the stringified names from the GeckoProcessType enum
+    PARENT ("default"),
+    PLUGIN ("plugin"),
+    CONTENT ("tab"),
+    IPDLUNITTEST ("ipdlunittest"),
+    GMPLUGIN ("gmplugin"),
+    GPU ("gpu"),
+    VR ("vr"),
+    RDD ("rdd"),
+    SOCKET ("socket"),
+    REMOTESANDBOXBROKER ("sandboxbroker"),
+    FORKSERVER ("forkserver");
+
+    private final String mGeckoName;
+
+    private GeckoProcessType(final String geckoName) {
+        mGeckoName = geckoName;
+    }
+
+    @Override
+    public String toString() {
+        return mGeckoName;
+    }
+
+    private static final GeckoProcessType fromInt(final int type) {
+        return values()[type];
+    }
+}
+
