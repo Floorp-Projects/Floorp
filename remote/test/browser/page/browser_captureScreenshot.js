a75b9401b13e06be90ca50ac0ebad53caee10a6e
Henrik Skupin <mail@hskupin.info>
2020-01-23 21:31:30 +0000

    Bug 1587845 - [remote] Add "clip" argument to Page.captureScreenshot. r=remote-protocol-reviewers,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D57793

--- remote/test/browser/page/browser_captureScreenshot.js
+++ remote/test/browser/page/browser_captureScreenshot.js
@@ -5,7 +5,8 @@
 
 add_task(async function documentSmallerThanViewport({ client }) {
   const { Page } = client;
-  await loadURL(toDataURL("<div>Hello world"));
+
+  await loadURLWithElement();
 
   info("Check that captureScreenshot() captures the viewport by default");
   const { data } = await Page.captureScreenshot();
@@ -26,6 +27,7 @@ add_task(async function documentSmallerThanViewport({ client }) {
 
 add_task(async function documentLargerThanViewport({ client }) {
   const { Page } = client;
+
   await loadURL(toDataURL("<div style='margin: 100vh 100vw'>Hello world"));
 
   info("Check that captureScreenshot() captures the viewport by default");
@@ -145,9 +147,360 @@ add_task(async function asJPEGFormatAndQuality({ client }) {
   );
 });
 
+add_task(async function clipMissingProperties({ client }) {
+  const { Page } = client;
+  const contentSize = await getContentSize();
+
+  for (const prop of ["x", "y", "width", "height", "scale"]) {
+    console.info(`Check for missing ${prop}`);
+
+    const clip = {
+      x: 0,
+      y: 0,
+      width: contentSize.width,
+      height: contentSize.height,
+    };
+    clip[prop] = undefined;
+
+    let errorThrown = false;
+    try {
+      await Page.captureScreenshot({ clip });
+    } catch (e) {
+      errorThrown = true;
+    }
+    ok(errorThrown, `raised error for missing clip.${prop} property`);
+  }
+});
+
+add_task(async function clipOutOfBoundsXAndY({ client }) {
+  const { Page } = client;
+
+  const ratio = await getDevicePixelRatio();
+  const size = 50;
+
+  await loadURLWithElement();
+  const contentSize = await getContentSize();
+
+  var { data: refData } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: size,
+      height: size,
+      scale: 1,
+    },
+  });
+
+  for (const x of [-1, contentSize.width]) {
+    console.info(`Check out-of-bounds x for ${x}`);
+    const { data } = await Page.captureScreenshot({
+      clip: {
+        x,
+        y: 0,
+        width: size,
+        height: size,
+        scale: 1,
+      },
+    });
+    const { width, height } = await getImageDetails(data);
+
+    is(width, size * ratio, "Image has expected width");
+    is(height, size * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+
+  for (const y of [-1, contentSize.height]) {
+    console.info(`Check out-of-bounds y for ${y}`);
+    const { data } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y,
+        width: size,
+        height: size,
+        scale: 1,
+      },
+    });
+    const { width, height } = await getImageDetails(data);
+
+    is(width, size * ratio, "Image has expected width");
+    is(height, size * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+add_task(async function clipOutOfBoundsWidthAndHeight({ client }) {
+  const { Page } = client;
+  const ratio = await getDevicePixelRatio();
+
+  await loadURL(toDataURL("<div style='margin: 100vh 100vw'>Hello world"));
+  const contentSize = await getContentSize();
+
+  var { data: refData } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: contentSize.width,
+      height: contentSize.height,
+      scale: 1,
+    },
+  });
+
+  for (const value of [-1, 0]) {
+    console.info(`Check out-of-bounds width for ${value}`);
+    const clip = {
+      x: 0,
+      y: 0,
+      width: value,
+      height: contentSize.height,
+      scale: 1,
+    };
+
+    const { data } = await Page.captureScreenshot({ clip });
+    const { width, height } = await getImageDetails(data);
+    is(width, contentSize.width * ratio, "Image has expected width");
+    is(height, contentSize.height * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+
+  for (const value of [-1, 0]) {
+    console.info(`Check out-of-bounds height for ${value}`);
+    const clip = {
+      x: 0,
+      y: 0,
+      width: contentSize.width,
+      height: value,
+      scale: 1,
+    };
+
+    const { data } = await Page.captureScreenshot({ clip });
+    const { width, height } = await getImageDetails(data);
+    is(width, contentSize.width * ratio, "Image has expected width");
+    is(height, contentSize.height * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+add_task(async function clipOutOfBoundsScale({ client }) {
+  const { Page } = client;
+  const ratio = await getDevicePixelRatio();
+
+  await loadURLWithElement();
+  const contentSize = await getContentSize();
+
+  var { data: refData } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: contentSize.width,
+      height: contentSize.height,
+      scale: 1,
+    },
+  });
+
+  for (const value of [-1, 0]) {
+    console.info(`Check out-of-bounds scale for ${value}`);
+    var { data } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width: 50,
+        height: 50,
+        scale: value,
+      },
+    });
+
+    const { width, height } = await getImageDetails(data);
+    is(width, contentSize.width * ratio, "Image has expected width");
+    is(height, contentSize.height * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+add_task(async function clipScale({ client }) {
+  const { Page } = client;
+  const ratio = await getDevicePixelRatio();
+
+  for (const scale of [1.5, 2]) {
+    console.info(`Check scale for ${scale}`);
+    await loadURLWithElement({ width: 100 * scale, height: 100 * scale });
+    var { data: refData } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width: 100 * scale,
+        height: 100 * scale,
+        scale: 1,
+      },
+    });
+
+    await loadURLWithElement({ width: 100, height: 100 });
+    var { data } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100,
+        scale,
+      },
+    });
+
+    const { width, height } = await getImageDetails(data);
+    is(width, 100 * ratio * scale, "Image has expected width");
+    is(height, 100 * ratio * scale, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+add_task(async function clipScaleAndDevicePixelRatio({ client }) {
+  const { Page } = client;
+
+  const originalRatio = await getDevicePixelRatio();
+
+  const ratio = 2;
+  const scale = 1.5;
+  const size = 100;
+
+  const expectedSize = size * ratio * scale;
+
+  console.info(`Create reference screenshot: ${expectedSize} x ${expectedSize}`);
+  await loadURLWithElement({
+    width: expectedSize,
+    height: expectedSize,
+  });
+  var { data: refData } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: expectedSize,
+      height: expectedSize,
+      scale: 1,
+    },
+  });
+
+  await setDevicePixelRatio(originalRatio * ratio);
+
+  await loadURLWithElement({ width: size, height: size });
+  var { data } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: size,
+      height: size,
+      scale,
+    },
+  });
+
+  const { width, height } = await getImageDetails(data);
+  is(width, expectedSize * originalRatio, "Image has expected width");
+  is(height, expectedSize * originalRatio, "Image has expected height");
+  is(data, refData, "Image is equal");
+});
+
+add_task(async function clipPosition({ client }) {
+  const { Page } = client;
+  const ratio = await getDevicePixelRatio();
+
+  await loadURLWithElement();
+  var { data: refData } = await Page.captureScreenshot({
+    clip: {
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      scale: 1,
+    },
+  });
+
+  for (const [x, y] of [[10, 20], [20, 10], [20, 20]]) {
+    console.info(`Check postion for ${x} and ${y}`);
+    await loadURLWithElement({ x, y });
+    var { data } = await Page.captureScreenshot({
+      clip: {
+        x,
+        y,
+        width: 100,
+        height: 100,
+        scale: 1,
+      },
+    });
+
+    const { width, height } = await getImageDetails(data);
+    is(width, 100 * ratio, "Image has expected width");
+    is(height, 100 * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+add_task(async function clipDimension({ client }) {
+  const { Page } = client;
+  const ratio = await getDevicePixelRatio();
+
+  for (const [width, height] of [[10, 20], [20, 10], [20, 20]]) {
+    console.info(`Check width and height for ${width} and ${height}`);
+
+    // Get reference image as section from a larger image
+    await loadURLWithElement({ width: 50, height: 50 });
+    var { data: refData } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width,
+        height,
+        scale: 1,
+      },
+    });
+
+    await loadURLWithElement({ width, height });
+    var { data } = await Page.captureScreenshot({
+      clip: {
+        x: 0,
+        y: 0,
+        width,
+        height,
+        scale: 1,
+      },
+    });
+
+    const dimension = await getImageDetails(data);
+    is(dimension.width, width * ratio, "Image has expected width");
+    is(dimension.height, height * ratio, "Image has expected height");
+    is(data, refData, "Image is equal");
+  }
+});
+
+async function loadURLWithElement(options = {}) {
+  const { x = 0, y = 0, width = 100, height = 100 } = options;
+
+  const doc = `
+    <style>
+      body {
+        margin: 0;
+      }
+      div {
+        margin-left: ${x}px;
+        margin-top: ${y}px;
+        width: ${width}px;
+        height: ${height}px;
+        background: green;
+      }
+    </style>
+    <body>
+    <div></div>
+  `;
+
+  await loadURL(toDataURL(doc));
+}
+
 async function getDevicePixelRatio() {
   return SpecialPowers.spawn(gBrowser.selectedBrowser, [], function() {
     return content.devicePixelRatio;
+  });
+}
+
+async function setDevicePixelRatio(dppx) {
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [dppx], function(dppx) {
+    content.docShell.contentViewer.overrideDPPX = dppx;
+    is(content.devicePixelRatio, dppx, "devicePixelRatio override set");
   });
 }
 
