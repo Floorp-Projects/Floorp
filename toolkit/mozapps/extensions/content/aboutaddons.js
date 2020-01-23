93adc16d01ae250d08f848edb9bc7d537bcb4f82
Mark Striemer <mstriemer@mozilla.com>
2020-01-23 11:31:13 +0000

    Bug 1610227 - Remove page height hack from about:addons inline options r=rpl
    Depends on D60555
    
    Differential Revision: https://phabricator.services.mozilla.com/D60558

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -1936,8 +1936,7 @@ class InlineOptionsBrowser extends HTMLElement {
       let messageListener = {
         receiveMessage({ name, data }) {
           if (name === "Extension:BrowserResized") {
-            // Add a pixel to work around a scrolling issue, bug 1548687.
-            browser.style.height = `${data.height + 1}px`;
+            browser.style.height = `${data.height}px`;
           } else if (name === "Extension:BrowserContentLoaded") {
             resolve();
           }
