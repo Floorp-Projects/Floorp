acdd4926d9d7b2ccd6e83c37425fa44a0b1d8f36
Ehsan Akhgari <ehsan@mozilla.com>
2020-01-23 20:06:38 +0000

    Bug 1560947 - Avoid minting a new principal and just use the document principal, to ensure we have the right principal in private windows; r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D60598
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/antitracking/test/browser/browser_permissionInPrivateWindows.js
+++ toolkit/components/antitracking/test/browser/browser_permissionInPrivateWindows.js
@@ -13,9 +13,7 @@ AntiTracking.runTest(
       let Services = SpecialPowers.Services;
       // We would use TEST_3RD_PARTY_DOMAIN here, except that the variable isn't
       // accessible in the context of the web page...
-      let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
-        "https://tracking.example.org/"
-      );
+      let principal = SpecialPowers.wrap(document).nodePrincipal;
       for (let perm of Services.perms.getAllForPrincipal(principal)) {
         // Ignore permissions other than storage access
         if (!perm.type.startsWith("3rdPartyStorage^")) {
