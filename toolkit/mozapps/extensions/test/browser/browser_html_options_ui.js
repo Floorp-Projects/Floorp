5a20bc7d9ab0d03bd85a4e40a825c0959dfebe2a
Mark Striemer <mstriemer@mozilla.com>
2020-01-23 11:31:13 +0000

    Bug 1610227 - Remove page height hack from about:addons inline options r=rpl
    Depends on D60555
    
    Differential Revision: https://phabricator.services.mozilla.com/D60558

--- toolkit/mozapps/extensions/test/browser/browser_html_options_ui.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_options_ui.js
@@ -134,32 +134,28 @@ add_task(async function testInlineOptions() {
   let waitForHeightChange = expectedHeight =>
     TestUtils.waitForCondition(() => browser.clientHeight === expectedHeight);
 
-  // The expected heights are 1px taller, to work around bug 1548687.
-  const EXPECTED_HEIGHT_SHORT = HEIGHT_SHORT + 1;
-  const EXPECTED_HEIGHT_TALL = HEIGHT_TALL + 1;
-
-  await waitForHeightChange(EXPECTED_HEIGHT_SHORT);
+  await waitForHeightChange(HEIGHT_SHORT);
 
   // Check resizing the browser through extension CSS.
   await extension.sendMessage("get-height");
   let height = await extension.awaitMessage("height");
-  is(height, EXPECTED_HEIGHT_SHORT, "The height is smaller to start");
+  is(height, HEIGHT_SHORT, "The height is smaller to start");
   is(height, browser.clientHeight, "The browser is the same size");
 
   info("Resize the browser to be taller");
   await extension.sendMessage("toggle-class");
-  await waitForHeightChange(EXPECTED_HEIGHT_TALL);
+  await waitForHeightChange(HEIGHT_TALL);
   await extension.sendMessage("get-height");
   height = await extension.awaitMessage("height");
-  is(height, EXPECTED_HEIGHT_TALL, "The height is bigger now");
+  is(height, HEIGHT_TALL, "The height is bigger now");
   is(height, browser.clientHeight, "The browser is the same size");
 
   info("Shrink the browser again");
   await extension.sendMessage("toggle-class");
-  await waitForHeightChange(EXPECTED_HEIGHT_SHORT);
+  await waitForHeightChange(HEIGHT_SHORT);
   await extension.sendMessage("get-height");
   height = await extension.awaitMessage("height");
-  is(height, EXPECTED_HEIGHT_SHORT, "The browser shrunk back");
+  is(height, HEIGHT_SHORT, "The browser shrunk back");
   is(height, browser.clientHeight, "The browser is the same size");
 
   info("Switching to details view");
