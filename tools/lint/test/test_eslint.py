5191fb9eb6de1938662d33033c67a1f79fb6265f
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-01-23 13:30:27 +0000

    Bug 1608535 - [lint] Forward new --no-error-on-unmatched-pattern to eslint, r=Standard8
    This prevents eslint from dumping an error and bailing when a path was specified that
    doesn't contain any lintable files.
    
    This patch fixes a bad interaction with 'mozlint' where passing a directory containing
    failures alongside a directory that doesn't have lintable files results in the errors
    being hidden and mozlint reporting that everying is OK.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60686

--- tools/lint/test/test_eslint.py
+++ tools/lint/test/test_eslint.py
@@ -12,8 +12,13 @@ def test_lint_with_global_exclude(lint, config, paths):
 
 
 def test_no_files_to_lint(lint, config, paths):
-    ret = lint(paths('nolint'), root=build.topsrcdir)
-    assert ret == []
+    # A directory with no files to lint.
+    results = lint(paths('nolint'), root=build.topsrcdir)
+    assert results == []
+
+    # Errors still show up even when a directory with no files is passed in.
+    results = lint(paths('nolint', 'subdir/bad.js'), root=build.topsrcdir)
+    assert len(results) == 1
 
 
 if __name__ == '__main__':
