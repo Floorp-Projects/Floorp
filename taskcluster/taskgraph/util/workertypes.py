1e3e99e73b26a587cca2eb32eee8c0a93ad6a57c
Tom Prince <mozilla@hocat.ca>
2020-01-23 16:53:58 +0000

    Bug 1605275: [taskgraph] Use worker alias for treescript workers; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D60779

--- taskcluster/taskgraph/util/workertypes.py
+++ taskcluster/taskgraph/util/workertypes.py
@@ -23,8 +23,6 @@ WORKER_TYPES = {
     "scriptworker-prov-v1/signing-linux-v1": ('scriptworker-signing', None),
     "scriptworker-k8s/gecko-3-shipit": ('shipit', None),
     "scriptworker-k8s/gecko-1-shipit": ('shipit', None),
-    "scriptworker-k8s/gecko-3-tree": ('treescript', None),
-    "scriptworker-k8s/gecko-1-tree": ('treescript', None),
     'terraform-packet/gecko-t-linux': ('docker-worker', 'linux'),
     'releng-hardware/gecko-t-osx-1014': ('generic-worker', 'macosx'),
     'releng-hardware/gecko-t-osx-1014-power': ('generic-worker', 'macosx'),
