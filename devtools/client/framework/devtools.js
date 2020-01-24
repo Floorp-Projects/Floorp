f9e883f143b69ee07cc207020ad3e09cfe3564a6
Oana Pop Rus <opoprus@mozilla.com>
2020-01-24 22:14:29 +0200

    Backed out changeset d3dc60cc5291 (bug 1610562) for devtools failures in browser_inspector_inspect_node_contextmenu.js on a CLOSED TREE

--- devtools/client/framework/devtools.js
+++ devtools/client/framework/devtools.js
@@ -730,7 +730,8 @@ DevTools.prototype = {
     // new-node-front tells us when the node has been selected, whether the
     // browser is remote or not.
     const onNewNode = inspector.selection.once("new-node-front");
-    const nodeFront = await inspector.inspectorFront.getNodeActorFromContentDomReference(
+
+    const nodeFront = await inspector.walker.getNodeActorFromContentDomReference(
       domReference
     );
 
@@ -770,7 +771,7 @@ DevTools.prototype = {
       startTime
     );
     const inspectorFront = await toolbox.target.getFront("inspector");
-    const nodeFront = await inspectorFront.getNodeActorFromContentDomReference(
+    const nodeFront = await inspectorFront.walker.getNodeActorFromContentDomReference(
       domReference
     );
     // Select the accessible object in the panel and wait for the event that
