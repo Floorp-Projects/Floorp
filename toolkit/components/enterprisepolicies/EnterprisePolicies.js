0b3dc8bc2297b0aed04ee5f5f66b7b61178df7e9
Michael Kaply <mozilla@kaply.com>
2020-01-24 14:48:23 +0000

    Bug 1552600 - Allow policies.json to augment platform policy. r=Felipe
    Differential Revision: https://phabricator.services.mozilla.com/D32362

--- toolkit/components/enterprisepolicies/EnterprisePolicies.js
+++ toolkit/components/enterprisepolicies/EnterprisePolicies.js
@@ -113,19 +113,21 @@ EnterprisePoliciesManager.prototype = {
   },
 
   _chooseProvider() {
-    let provider = null;
+    let platformProvider = null;
     if (AppConstants.platform == "win") {
-      provider = new WindowsGPOPoliciesProvider();
+      platformProvider = new WindowsGPOPoliciesProvider();
     } else if (AppConstants.platform == "macosx") {
-      provider = new macOSPoliciesProvider();
+      platformProvider = new macOSPoliciesProvider();
     }
-    if (provider && provider.hasPolicies) {
-      return provider;
+    let jsonProvider = new JSONPoliciesProvider();
+    if (platformProvider && platformProvider.hasPolicies) {
+      if (jsonProvider.hasPolicies) {
+        return new CombinedProvider(platformProvider, jsonProvider);
+      }
+      return platformProvider;
     }
-
-    provider = new JSONPoliciesProvider();
-    if (provider.hasPolicies) {
-      return provider;
+    if (jsonProvider.hasPolicies) {
+      return jsonProvider;
     }
 
     return null;
@@ -614,6 +616,33 @@ class macOSPoliciesProvider {
 
   get failed() {
     return this._failed;
+  }
+}
+
+class CombinedProvider {
+  constructor(primaryProvider, secondaryProvider) {
+    // Combine policies with primaryProvider taking precedence.
+    // We only do this for top level policies.
+    this._policies = primaryProvider._policies;
+    for (let policyName of Object.keys(secondaryProvider.policies)) {
+      if (!(policyName in this._policies)) {
+        this._policies[policyName] = secondaryProvider.policies[policyName];
+      }
+    }
+  }
+
+  get hasPolicies() {
+    // Combined provider always has policies.
+    return true;
+  }
+
+  get policies() {
+    return this._policies;
+  }
+
+  get failed() {
+    // Combined provider never fails.
+    return false;
   }
 }
 
