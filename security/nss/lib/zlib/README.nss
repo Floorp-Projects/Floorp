799f0cd87c7b553f4b0fad85533103a3a09a12a5
J.C. Jones <jc@mozilla.com>
2020-01-28 06:50:08 +0000

    Bug 1606927 - land NSS 4bf79c4d2954 UPGRADE_NSS_RELEASE, r=kjacobs
    2020-01-27  J.C. Jones  <jjones@mozilla.com>
    
    	* lib/freebl/blinit.c:
    	Bug 1602386 - clang-format r=bustage
    	[4bf79c4d2954] [tip]
    
    2020-01-27  Piotr Kubaj  <pkubaj@FreeBSD.org>
    
    	* lib/freebl/Makefile, lib/freebl/blinit.c:
    	Bug 1602386 - Fix build on FreeBSD/powerpc platforms. r=jcj
    
    	FreeBSD has elf_aux_info instead of getauxval, but only since
    	FreeBSD 12. Previous versions (11 is still supported) don't have any
    	equivalent and users need to query sysctl manually.
    
    	[f2ac5e318886]
    
    2020-01-27  Jan Beich  <jbeich@FreeBSD.org>
    
    	* lib/freebl/blinit.c:
    	Bug 1609181 - Detect ARM CPU features on FreeBSD. r=jcj
    
    	Implement `getauxval` via `elf_aux_info` to avoid code duplication.
    	`AT_HWCAP*` can be used on powerpc* and riscv64 as well.
    	[edb60bae9219]
    
    2020-01-22  Martin Thomson  <mt@lowentropy.net>
    
    	* lib/zlib/README, lib/zlib/README.nss, lib/zlib/adler32.c,
    	lib/zlib/compress.c, lib/zlib/crc32.c, lib/zlib/crc32.h,
    	lib/zlib/deflate.c, lib/zlib/deflate.h, lib/zlib/gzguts.h,
    	lib/zlib/gzlib.c, lib/zlib/gzread.c, lib/zlib/gzwrite.c,
    	lib/zlib/infback.c, lib/zlib/inffast.c, lib/zlib/inffixed.h,
    	lib/zlib/inflate.c, lib/zlib/inflate.h, lib/zlib/inftrees.c,
    	lib/zlib/trees.c, lib/zlib/trees.h, lib/zlib/uncompr.c,
    	lib/zlib/zconf.h, lib/zlib/zlib.h, lib/zlib/zutil.c,
    	lib/zlib/zutil.h:
    	Bug 1547639 - Update zlib to 1.2.11, r=jcj
    
    	[91f3f0749d0b]
    
    	* lib/zlib/README.nss, lib/zlib/config.mk, lib/zlib/example.c,
    	lib/zlib/manifest.mn, lib/zlib/minigzip.c, lib/zlib/vendor.sh,
    	lib/zlib/zlib.gyp:
    	Bug 1547639 - Automatic vendoring of zlib, r=jcj
    
    	[fc128963a9aa]
    
    Differential Revision: https://phabricator.services.mozilla.com/D61126
    
    --HG--
    extra : moz-landing-system : lando

--- security/nss/lib/zlib/README.nss
+++ security/nss/lib/zlib/README.nss
@@ -1,7 +1,7 @@
 zlib data compression library
 
 URL: http://zlib.net/
-Version: 1.2.5
+Version: 1.2.11 (cacf7f1d4e3d44d871b605da3b647f07d718623f)
 License: zlib License
 License File: http://zlib.net/zlib_license.html
 
@@ -9,10 +9,3 @@ Description:
 
 NSS uses zlib in libSSL (for the DEFLATE compression method), modutil, and
 signtool.
-
-Local Modifications:
-
-- patches/prune-zlib.sh: run this shell script to remove unneeded files
-  from the zlib distribution.
-- patches/msvc-vsnprintf.patch: define HAVE_VSNPRINTF for Visual C++ 2008
-  (9.0) and later.
--- security/nss/lib/zlib/inffixed.h
+++ security/nss/lib/zlib/inffixed.h
@@ -1,14 +1,10 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
     /* inffixed.h -- table for decoding fixed codes
      * Generated automatically by makefixed().
      */
 
-    /* WARNING: this file should *not* be used by applications. It
-       is part of the implementation of the compression library and
-       is subject to change. Applications should only use zlib.h.
+    /* WARNING: this file should *not* be used by applications.
+       It is part of the implementation of this library and is
+       subject to change. Applications should only use zlib.h.
      */
 
     static const code lenfix[512] = {
