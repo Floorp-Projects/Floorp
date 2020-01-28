e269b988486be4d39d3d17d8084dc6c0b444c11f
André Bargull <andre.bargull@gmail.com>
2020-01-28 09:00:04 +0000

    Bug 1608808 - Part 4: Remove DestructuringFlavor::FormalParameterInVarScope. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D60503

--- js/src/frontend/DestructuringFlavor.h
+++ js/src/frontend/DestructuringFlavor.h
@@ -14,13 +14,6 @@ enum class DestructuringFlavor {
   // Destructuring into a declaration.
   Declaration,
 
-  // Destructuring into a formal parameter, when the formal parameters
-  // contain an expression that might be evaluated, and thus require
-  // this destructuring to assign not into the innermost scope that
-  // contains the function body's vars, but into its enclosing scope for
-  // parameter expressions.
-  FormalParameterInVarScope,
-
   // Destructuring as part of an AssignmentExpression.
   Assignment
 };
