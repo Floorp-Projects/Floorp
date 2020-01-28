9745b30124d50c5d2f7a88c30068aaf5c75d489d
Mike Conley <mconley@mozilla.com>
2020-01-28 08:50:02 +0000

    Bug 1608425 - Adjust Picture-in-Picture toggle to be at 25% from the top for Twitch videos. r=denschub
    Differential Revision: https://phabricator.services.mozilla.com/D61009
    
    --HG--
    extra : moz-landing-system : lando

--- browser/extensions/webcompat/data/picture_in_picture_overrides.js
+++ browser/extensions/webcompat/data/picture_in_picture_overrides.js
@@ -21,5 +21,10 @@ let AVAILABLE_PIP_OVERRIDES;
     //
     // "https://*.youtube.com/*": TOGGLE_POLICIES.THREE_QUARTERS,
     // "https://*.twitch.tv/mikeconley_dot_ca/*": TOGGLE_POLICIES.TOP,
+
+    // Twitch
+    "https://*.twitch.tv/*": TOGGLE_POLICIES.ONE_QUARTER,
+    "https://*.twitch.tech/*": TOGGLE_POLICIES.ONE_QUARTER,
+    "https://*.twitch.a2z.com/*": TOGGLE_POLICIES.ONE_QUARTER,
   };
 }
