562cfbe4da18223016e6395b9842ff5ff0b47b1e
Dale Harvey <dale@arandomurl.com>
2020-01-29 21:01:50 +0000

    Bug 1591079 - Add application name and version restrictions. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D61174

---
+++ toolkit/components/search/tests/xpcshell/test_engine_selector_application_name.js
@@ -0,0 +1,136 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+XPCOMUtils.defineLazyModuleGetters(this, {
+  SearchEngineSelector: "resource://gre/modules/SearchEngineSelector.jsm",
+});
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+const CONFIG = {
+  data: [
+    {
+      webExtension: {
+        id: "aol@example.com",
+      },
+      appliesTo: [
+        {
+          included: { everywhere: true },
+        },
+      ],
+      default: "yes-if-no-other",
+    },
+    {
+      webExtension: {
+        id: "lycos@example.com",
+      },
+      appliesTo: [
+        {
+          included: { everywhere: true },
+          application: {
+            name: ["firefox"],
+          },
+        },
+      ],
+      default: "yes",
+    },
+    {
+      webExtension: {
+        id: "altavista@example.com",
+      },
+      appliesTo: [
+        {
+          included: { everywhere: true },
+          application: {
+            name: ["fenix"],
+          },
+        },
+      ],
+    },
+    {
+      webExtension: {
+        id: "excite@example.com",
+      },
+      appliesTo: [
+        {
+          included: { everywhere: true },
+          application: {
+            name: ["firefox"],
+            minVersion: "10",
+            maxVersion: "30",
+          },
+          default: "yes",
+        },
+      ],
+    },
+  ],
+};
+
+function fetchWithConfig(name, version) {
+  Services.appinfo = { name, version };
+  return engineSelector.fetchEngineConfiguration("default", "default");
+}
+
+const engineSelector = new SearchEngineSelector();
+
+const tests = [
+  {
+    name: "Firefox",
+    version: "1",
+    expected: ["lycos@example.com", "aol@example.com"],
+  },
+  {
+    name: "Firefox",
+    version: "20",
+    expected: ["lycos@example.com", "aol@example.com", "excite@example.com"],
+  },
+  {
+    name: "Fenix",
+    version: "20",
+    expected: ["aol@example.com", "altavista@example.com"],
+  },
+  {
+    name: "Firefox",
+    version: "31",
+    expected: ["lycos@example.com", "aol@example.com"],
+  },
+  {
+    name: "Firefox",
+    version: "30",
+    expected: ["lycos@example.com", "aol@example.com", "excite@example.com"],
+  },
+  {
+    name: "Firefox",
+    version: "10",
+    expected: ["lycos@example.com", "aol@example.com", "excite@example.com"],
+  },
+];
+
+add_task(async function setup() {
+  await useTestEngines();
+  await AddonTestUtils.promiseStartupManager();
+
+  let confUrl = `data:application/json,${JSON.stringify(CONFIG)}`;
+  Services.prefs.setStringPref("search.config.url", confUrl);
+
+  await engineSelector.init();
+});
+
+add_task(async function test_application_name() {
+  for (const { name, version, expected } of tests) {
+    Services.appinfo = { name, version };
+    let { engines } = engineSelector.fetchEngineConfiguration(
+      "default",
+      "default"
+    );
+    const engineIds = engines.map(obj => obj.webExtension.id);
+    Assert.deepEqual(
+      engineIds,
+      expected,
+      `Should have the expected engines for app: "${name}"
+      and version: "${version}"`
+    );
+  }
+});
