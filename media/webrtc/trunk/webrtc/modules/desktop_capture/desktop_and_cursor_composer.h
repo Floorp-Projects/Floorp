b5abb9a3cbb348b14a13bcf3e61aaa71c4559dca
Nico Grunbaum <na-g@nostrum.com>
2020-01-30 05:47:41 +0000

    Bug 1611486 - P1 - Cherry-pick webrct desktop sharing X hang fixes;r=dminor
    https://chromium.googlesource.com/external/webrtc/+/4b47dd39a79d62acaaeb68bedd147d748320ce4d
    
    Differential Revision: https://phabricator.services.mozilla.com/D61000

--- media/webrtc/trunk/webrtc/modules/desktop_capture/desktop_and_cursor_composer.h
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/desktop_and_cursor_composer.h
@@ -70,8 +70,6 @@ class DesktopAndCursorComposer : public DesktopCapturer,
 
   // MouseCursorMonitor::Callback interface.
   void OnMouseCursor(MouseCursor* cursor) override;
-  void OnMouseCursorPosition(MouseCursorMonitor::CursorState state,
-                             const DesktopVector& position) override;
   void OnMouseCursorPosition(const DesktopVector& position) override;
 
   const std::unique_ptr<DesktopCapturer> desktop_capturer_;
