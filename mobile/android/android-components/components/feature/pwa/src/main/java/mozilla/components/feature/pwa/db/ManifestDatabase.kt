efb92b091840d86a5e0a6d78fe0e34362ef323ad
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2020-01-31 01:10:51 +0100

    [components] fix feature-pwa manifests db migration
    The version 1 schema of the manifests db was modified without increasing
    the schema version. This causes existing databases to not get upgraded
    and room to throw an exception. The version number has to be increased
    and migrations must be provided. Since the modified schema version 1 was
    already released, we have to verify both schema variants can be upgraded
    to version 2.

--- mobile/android/android-components/components/feature/pwa/src/main/java/mozilla/components/feature/pwa/db/ManifestDatabase.kt
+++ mobile/android/android-components/components/feature/pwa/src/main/java/mozilla/components/feature/pwa/db/ManifestDatabase.kt
@@ -5,21 +5,42 @@
 package mozilla.components.feature.pwa.db
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 /**
  * Internal database for storing web app manifests and metadata.
  */
-@Database(entities = [ManifestEntity::class], version = 1)
+@Database(entities = [ManifestEntity::class], version = 2)
 @TypeConverters(ManifestConverter::class)
 internal abstract class ManifestDatabase : RoomDatabase() {
     abstract fun manifestDao(): ManifestDao
 
     companion object {
         @Volatile private var instance: ManifestDatabase? = null
+
+        @VisibleForTesting
+        internal val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                val cursor = database.query("SELECT * FROM manifests LIMIT 0,1")
+
+                if (cursor.getColumnIndex("used_at") < 0) {
+                    database.execSQL("ALTER TABLE manifests ADD COLUMN used_at INTEGER NOT NULL DEFAULT 0")
+                }
+
+                if (cursor.getColumnIndex("scope") < 0) {
+                    database.execSQL("ALTER TABLE manifests ADD COLUMN scope TEXT")
+                }
+
+                database.execSQL("CREATE INDEX IF NOT EXISTS index_manifests_scope ON manifests (scope)")
+                database.execSQL("UPDATE manifests SET used_at = updated_at WHERE used_at = 0")
+            }
+        }
 
         @Synchronized
         fun get(context: Context): ManifestDatabase {
@@ -29,7 +50,7 @@ internal abstract class ManifestDatabase : RoomDatabase() {
                 context,
                 ManifestDatabase::class.java,
                 "manifests"
-            ).build().also {
+            ).addMigrations(MIGRATION_1_2).build().also {
                 instance = it
             }
         }
