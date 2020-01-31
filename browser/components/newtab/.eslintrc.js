bdb588de4f5a34407cfc7e3153d88c6fce463e07
Ed Lee <edilee@mozilla.com>
2020-01-31 07:33:54 +0000

    Bug 1610923 - Point npm run testmc to use mach to avoid error with linting r=thecount
    Add various linters supported by mach and remove eslint dependency.
    
    Differential Revision: https://phabricator.services.mozilla.com/D60895

--- browser/components/newtab/.eslintrc.js
+++ browser/components/newtab/.eslintrc.js
@@ -15,8 +15,6 @@ module.exports = {
     "import", // require("eslint-plugin-import")
     "react", // require("eslint-plugin-react")
     "jsx-a11y", // require("eslint-plugin-jsx-a11y")
-    // Temporarily disabled since they aren't vendored into in mozilla central yet
-    // "react-hooks", // require("react-hooks")
   ],
   "settings": {
     "react": {
@@ -33,11 +31,6 @@ module.exports = {
     "plugin:prettier/recommended", // require("eslint-plugin-prettier")
     "prettier/react", // require("eslint-config-prettier")
   ],
-  "globals": {
-    // Remove this when m-c updates their eslint: See https://github.com/mozilla/activity-stream/pull/4219
-    "RPMSendAsyncMessage": true,
-    "NewTabPagePreloading": true,
-  },
   "overrides": [
     {
       // These files use fluent-dom to insert content
@@ -73,8 +66,6 @@ module.exports = {
     }
   ],
   "rules": {
-    // "react-hooks/rules-of-hooks": 2,
-
     "fetch-options/no-fetch-credentials": 2,
 
     "react/jsx-boolean-value": [2, "always"],
