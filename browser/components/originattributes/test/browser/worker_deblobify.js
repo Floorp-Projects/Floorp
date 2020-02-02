1b1308abac96f019fd8090797b50663e9824bd33
Coroiu Cristina <ccoroiu@mozilla.com>
2020-02-02 10:55:25 +0200

    Backed out changeset a422cac07967 (bug 1612519) for ESlint failure at browser/components/originattributes/test/browser/worker_blobify.js

---
+++ browser/components/originattributes/test/browser/worker_deblobify.js
@@ -0,0 +1,33 @@
+// Wait for a blob URL to be posted to this worker.
+// Obtain the blob, and read the string contained in it.
+// Post back the string.
+
+/* eslint-env worker */
+
+var postStringInBlob = function(blobObject) {
+  var fileReader = new FileReaderSync();
+  var result = fileReader.readAsText(blobObject);
+  postMessage(result);
+};
+
+self.addEventListener("message", function(message) {
+  if ("error" in message.data) {
+    postMessage(message.data);
+    return;
+  }
+  var blobURL = message.data.blobURL,
+    xhr = new XMLHttpRequest();
+  try {
+    xhr.open("GET", blobURL, true);
+    xhr.onload = function() {
+      postStringInBlob(xhr.response);
+    };
+    xhr.onerror = function() {
+      postMessage({ error: "xhr error" });
+    };
+    xhr.responseType = "blob";
+    xhr.send();
+  } catch (e) {
+    postMessage({ error: e.message });
+  }
+});
