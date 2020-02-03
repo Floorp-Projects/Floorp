23e7aaa178f1d07b7b3d44d426b23251640bf4d8
Marco Vega <marco@proweb.co>
2020-02-03 11:31:33 +0000

    Bug 1252803 - Enable ESLint on the rest of devtools (manual changes). r=jdescottes
    Depends on D57223
    
    Differential Revision: https://phabricator.services.mozilla.com/D57265
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/markup/test/browser_markup_events_02.js
+++ devtools/client/inspector/markup/test/browser_markup_events_02.js
@@ -11,25 +11,25 @@ const TEST_URL = URL_ROOT + "doc_markup_events_02.html";
 
 loadHelperScript("helper_events_test_runner.js");
 
-const TEST_DATA = [ // eslint-disable-line
+const TEST_DATA = [
   {
     selector: "#fatarrow",
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":39:43",
+        filename: TEST_URL + ":42:43",
         attributes: ["Bubbling", "DOM2"],
         handler: "() => {\n" + '  alert("Fat arrow without params!");\n' + "}",
       },
       {
         type: "click",
-        filename: TEST_URL + ":43:43",
+        filename: TEST_URL + ":46:43",
         attributes: ["Bubbling", "DOM2"],
         handler: "event => {\n" + '  alert("Fat arrow with 1 param!");\n' + "}",
       },
       {
         type: "click",
-        filename: TEST_URL + ":47:43",
+        filename: TEST_URL + ":50:43",
         attributes: ["Bubbling", "DOM2"],
         handler:
           "(event, foo, bar) => {\n" +
@@ -38,7 +38,7 @@ const TEST_DATA = [ // eslint-disable-line
       },
       {
         type: "click",
-        filename: TEST_URL + ":51:43",
+        filename: TEST_URL + ":54:43",
         attributes: ["Bubbling", "DOM2"],
         handler: "b => b",
       },
@@ -49,7 +49,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":62:32",
+        filename: TEST_URL + ":65:32",
         attributes: ["Bubbling", "DOM2"],
         handler: "function(event) {\n" + '  alert("Bound event");\n' + "}",
       },
@@ -60,7 +60,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":85:29",
+        filename: TEST_URL + ":88:29",
         attributes: ["Bubbling", "DOM2"],
         handler: "function() {\n" + '  alert("boundHandleEvent");\n' + "}",
       },
@@ -71,7 +71,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":91:47",
+        filename: TEST_URL + ":94:47",
         attributes: ["Bubbling", "DOM2"],
         handler:
           "function functionProceededByInlineComment() {\n" +
@@ -85,7 +85,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":96:50",
+        filename: TEST_URL + ":99:50",
         attributes: ["Bubbling", "DOM2"],
         handler:
           "function functionProceededByStreamingComment() {\n" +
@@ -99,7 +99,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":71:34",
+        filename: TEST_URL + ":74:34",
         attributes: ["Bubbling", "DOM2"],
         handler: "function() {\n" + '  alert("obj.anonObjectMethod");\n' + "}",
       },
@@ -110,7 +110,7 @@ const TEST_DATA = [ // eslint-disable-line
     expected: [
       {
         type: "click",
-        filename: TEST_URL + ":75:34",
+        filename: TEST_URL + ":78:34",
         attributes: ["Bubbling", "DOM2"],
         handler: "function kay() {\n" + '  alert("obj.objectMethod");\n' + "}",
       },
