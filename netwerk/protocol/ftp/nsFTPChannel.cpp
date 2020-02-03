147111c0c0b165d43890dc792d58630a2c905be7
Michal Novotny <michal.novotny@gmail.com>
2020-02-03 19:22:37 +0000

    Bug 1610810 - FTP download prompt appears while authentication dialog is displayed, r=valentin
    Set nsFtpChannel's content-type to application/octet-stream only when downloading a file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61494

--- netwerk/protocol/ftp/nsFTPChannel.cpp
+++ netwerk/protocol/ftp/nsFTPChannel.cpp
@@ -6,7 +6,6 @@
 
 #include "nsFTPChannel.h"
 #include "nsFtpConnectionThread.h"  // defines nsFtpState
-#include "nsMimeTypes.h"
 
 #include "nsThreadUtils.h"
 #include "mozilla/Attributes.h"
@@ -92,7 +91,6 @@ nsresult nsFtpChannel::OpenContentStream(bool async, nsIInputStream** result,
                                          nsIChannel** channel) {
   if (!async) return NS_ERROR_NOT_IMPLEMENTED;
 
-  SetContentType(NS_LITERAL_CSTRING(APPLICATION_OCTET_STREAM));
   RefPtr<nsFtpState> state = new nsFtpState();
 
   nsresult rv = state->Init(this);
