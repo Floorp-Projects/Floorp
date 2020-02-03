be8fbc64ecc6b0b05308192c5627f42f4d451fad
Drew Willcoxon <adw@mozilla.com>
2020-02-03 12:46:22 +0000

    Bug 1606918 - Port the Update Intervention from the experiment, part 2: Tests. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D60786
    
    --HG--
    extra : moz-landing-system : lando

---
+++ browser/components/urlbar/tests/browser/interventions_update/browser_updateRestart.js
@@ -0,0 +1,45 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Checks the UPDATE_RESTART tip.
+//
+// The update parts of this test are adapted from:
+// https://searchfox.org/mozilla-central/source/toolkit/mozapps/update/tests/browser/browser_aboutDialog_bc_downloaded_staged.js
+
+"use strict";
+
+let params = {
+  queryString: "&invalidCompleteSize=1",
+  backgroundUpdate: true,
+  continueFile: CONTINUE_STAGING,
+  waitForUpdateState: STATE_APPLIED,
+};
+
+let preSteps = [
+  {
+    panelId: "apply",
+    checkActiveUpdate: { state: STATE_APPLIED },
+    continueFile: null,
+  },
+];
+
+add_task(async function test() {
+  // Enable the pref that automatically downloads and installs updates.
+  await SpecialPowers.pushPrefEnv({
+    set: [[PREF_APP_UPDATE_STAGING_ENABLED, true]],
+  });
+
+  // Set up the "apply" update state.
+  await initUpdate(params);
+  UrlbarProviderInterventions.checkForBrowserUpdate(true);
+  await processUpdateSteps(preSteps);
+
+  // Picking the tip should attempt to restart the browser.
+  await doUpdateTest({
+    searchString: SEARCH_STRINGS.UPDATE,
+    tip: TIPS.UPDATE_RESTART,
+    title: /^The latest .+ is downloaded and ready to install\.$/,
+    button: "Restart to Update",
+    awaitCallback: awaitAppRestartRequest,
+  });
+});
