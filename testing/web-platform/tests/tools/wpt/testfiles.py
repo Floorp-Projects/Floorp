eb731b66a29f50798623d68a92e76149f21cac2f
Stephen McGruer <smcgruer@chromium.org>
2020-02-04 13:19:39 +0000

    Bug 1612576 [wpt PR 21531] - Support crashtests in testfiles.affected_testfiles, a=testonly
    Automatic update from web-platform-tests
    Support crashtests in testfiles.affected_testfiles (#21531)
    
    Co-Authored-By: Robert Ma <robertma@chromium.org>
    
    --
    
    wpt-commits: c7227ea61e08f972023767b22cbea7ab23209426
    wpt-pr: 21531

--- testing/web-platform/tests/tools/wpt/testfiles.py
+++ testing/web-platform/tests/tools/wpt/testfiles.py
@@ -247,7 +247,7 @@ def affected_testfiles(files_changed,  # type: Iterable[Text]
     nontests_changed = set(files_changed)
     wpt_manifest = load_manifest(manifest_path, manifest_update)
 
-    test_types = ["testharness", "reftest", "wdspec"]
+    test_types = ["crashtest", "testharness", "reftest", "wdspec"]
     support_files = {os.path.join(wpt_root, path)
                      for _, path, _ in wpt_manifest.itertypes("support")}
     wdspec_test_files = {os.path.join(wpt_root, path)
