a5649a3ba841ff7701f5684e0b0ca720241ac6e7
Tom Ritter <tom@mozilla.com>
2020-02-04 07:07:12 +0000

    Bug 1599226 - Refactor Ion.h/cpp into JitContext.h/cpp r=jandem
    We need to move IsBaseLineJitEnabled into BaselineJit.h.
    Then we need to include BaselineJit.h from Ion.h.
    
    HOWEVER, lots of things include Ion.h and we create an include
    loop with BaselineJit.h. Fortunately; however, the reason lots
    of things include Ion.h is to get at the JitContext that's
    defined in Ion.h - and it doesn't need to be.
    
    So we move JitContext and a few other things into a separate
    header and include that instead of Ion.h
    
    Depends on D61076
    
    Differential Revision: https://phabricator.services.mozilla.com/D61271

---
+++ js/src/jit/JitContext.cpp
@@ -0,0 +1,171 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "jit/JitContext.h"
+
+#include "mozilla/DebugOnly.h"
+#include "mozilla/IntegerPrintfMacros.h"
+#include "mozilla/MemoryReporting.h"
+#include "mozilla/ThreadLocal.h"
+#include "mozilla/Unused.h"
+
+#include "gc/FreeOp.h"
+#include "gc/Marking.h"
+#include "gc/PublicIterators.h"
+#include "jit/AliasAnalysis.h"
+#include "jit/AlignmentMaskAnalysis.h"
+#include "jit/BacktrackingAllocator.h"
+#include "jit/BaselineFrame.h"
+#include "jit/BaselineInspector.h"
+#include "jit/BaselineJIT.h"
+#include "jit/CacheIRSpewer.h"
+#include "jit/CodeGenerator.h"
+#include "jit/EdgeCaseAnalysis.h"
+#include "jit/EffectiveAddressAnalysis.h"
+#include "jit/FoldLinearArithConstants.h"
+#include "jit/InstructionReordering.h"
+#include "jit/IonAnalysis.h"
+#include "jit/IonBuilder.h"
+#include "jit/IonIC.h"
+#include "jit/IonOptimizationLevels.h"
+#include "jit/JitcodeMap.h"
+#include "jit/JitCommon.h"
+#include "jit/JitRealm.h"
+#include "jit/JitSpewer.h"
+#include "jit/LICM.h"
+#include "jit/Linker.h"
+#include "jit/LIR.h"
+#include "jit/Lowering.h"
+#include "jit/PerfSpewer.h"
+#include "jit/RangeAnalysis.h"
+#include "jit/ScalarReplacement.h"
+#include "jit/Sink.h"
+#include "jit/StupidAllocator.h"
+#include "jit/ValueNumbering.h"
+#include "jit/WasmBCE.h"
+#include "js/Printf.h"
+#include "js/UniquePtr.h"
+#include "util/Memory.h"
+#include "util/Windows.h"
+#include "vm/HelperThreads.h"
+#include "vm/Realm.h"
+#include "vm/TraceLogging.h"
+#ifdef MOZ_VTUNE
+#  include "vtune/VTuneWrapper.h"
+#endif
+
+#include "debugger/DebugAPI-inl.h"
+#include "gc/GC-inl.h"
+#include "jit/JitFrames-inl.h"
+#include "jit/MacroAssembler-inl.h"
+#include "jit/shared/Lowering-shared-inl.h"
+#include "vm/EnvironmentObject-inl.h"
+#include "vm/GeckoProfiler-inl.h"
+#include "vm/JSObject-inl.h"
+#include "vm/JSScript-inl.h"
+#include "vm/Realm-inl.h"
+#include "vm/Stack-inl.h"
+
+#if defined(ANDROID)
+#  include <sys/system_properties.h>
+#endif
+
+using mozilla::DebugOnly;
+
+using namespace js;
+using namespace js::jit;
+
+// Assert that JitCode is gc::Cell aligned.
+JS_STATIC_ASSERT(sizeof(JitCode) % gc::CellAlignBytes == 0);
+
+static MOZ_THREAD_LOCAL(JitContext*) TlsJitContext;
+
+static JitContext* CurrentJitContext() {
+  if (!TlsJitContext.init()) {
+    return nullptr;
+  }
+  return TlsJitContext.get();
+}
+
+void jit::SetJitContext(JitContext* ctx) { TlsJitContext.set(ctx); }
+
+JitContext* jit::GetJitContext() {
+  MOZ_ASSERT(CurrentJitContext());
+  return CurrentJitContext();
+}
+
+JitContext* jit::MaybeGetJitContext() { return CurrentJitContext(); }
+
+JitContext::JitContext(CompileRuntime* rt, CompileRealm* realm,
+                       TempAllocator* temp)
+    : prev_(CurrentJitContext()), realm_(realm), temp(temp), runtime(rt) {
+  MOZ_ASSERT(rt);
+  MOZ_ASSERT(realm);
+  MOZ_ASSERT(temp);
+  SetJitContext(this);
+}
+
+JitContext::JitContext(JSContext* cx, TempAllocator* temp)
+    : prev_(CurrentJitContext()),
+      realm_(CompileRealm::get(cx->realm())),
+      cx(cx),
+      temp(temp),
+      runtime(CompileRuntime::get(cx->runtime())) {
+  SetJitContext(this);
+}
+
+JitContext::JitContext(TempAllocator* temp)
+    : prev_(CurrentJitContext()), temp(temp) {
+#ifdef DEBUG
+  isCompilingWasm_ = true;
+#endif
+  SetJitContext(this);
+}
+
+JitContext::JitContext() : JitContext(nullptr) {}
+
+JitContext::~JitContext() { SetJitContext(prev_); }
+
+bool jit::InitializeJit() {
+  if (!TlsJitContext.init()) {
+    return false;
+  }
+
+  CheckLogging();
+
+#ifdef JS_CACHEIR_SPEW
+  const char* env = getenv("CACHEIR_LOGS");
+  if (env && env[0] && env[0] != '0') {
+    CacheIRSpewer::singleton().init(env);
+  }
+#endif
+
+#if defined(JS_CODEGEN_ARM)
+  InitARMFlags();
+#endif
+
+  // Note: these flags need to be initialized after the InitARMFlags call above.
+  JitOptions.supportsFloatingPoint = MacroAssembler::SupportsFloatingPoint();
+  JitOptions.supportsUnalignedAccesses =
+      MacroAssembler::SupportsUnalignedAccesses();
+
+  CheckPerf();
+  return true;
+}
+
+bool jit::JitSupportsSimd() { return js::jit::MacroAssembler::SupportsSimd(); }
+
+bool jit::JitSupportsAtomics() {
+#if defined(JS_CODEGEN_ARM)
+  // Bug 1146902, bug 1077318: Enable Ion inlining of Atomics
+  // operations on ARM only when the CPU has byte, halfword, and
+  // doubleword load-exclusive and store-exclusive instructions,
+  // until we can add support for systems that don't have those.
+  return js::jit::HasLDSTREXBHD();
+#else
+  return true;
+#endif
+}
---
+++ js/src/jit/JitContext.h
@@ -0,0 +1,162 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef jit_JitContext_h
+#define jit_JitContext_h
+#include "mozilla/MemoryReporting.h"
+#include "mozilla/Result.h"
+
+#include "jit/CompileWrappers.h"
+#include "jit/JitOptions.h"
+#include "vm/JSContext.h"
+#include "vm/Realm.h"
+#include "vm/TypeInference.h"
+
+namespace js {
+namespace jit {
+
+class TempAllocator;
+
+enum MethodStatus {
+  Method_Error,
+  Method_CantCompile,
+  Method_Skipped,
+  Method_Compiled
+};
+
+enum class AbortReason : uint8_t {
+  Alloc,
+  Inlining,
+  PreliminaryObjects,
+  Disable,
+  Error,
+  NoAbort
+};
+
+template <typename V>
+using AbortReasonOr = mozilla::Result<V, AbortReason>;
+using mozilla::Err;
+using mozilla::Ok;
+
+static_assert(sizeof(AbortReasonOr<Ok>) <= sizeof(uintptr_t),
+              "Unexpected size of AbortReasonOr<Ok>");
+static_assert(sizeof(AbortReasonOr<bool>) <= sizeof(uintptr_t),
+              "Unexpected size of AbortReasonOr<bool>");
+
+// A JIT context is needed to enter into either an JIT method or an instance
+// of a JIT compiler. It points to a temporary allocator and the active
+// JSContext, either of which may be nullptr, and the active realm, which
+// will not be nullptr.
+
+class JitContext {
+  JitContext* prev_ = nullptr;
+  CompileRealm* realm_ = nullptr;
+  int assemblerCount_ = 0;
+
+#ifdef DEBUG
+  // Whether this thread is actively Ion compiling (does not include Wasm or
+  // IonBuilder).
+  bool inIonBackend_ = false;
+
+  // Whether this thread is actively Ion compiling in a context where a minor
+  // GC could happen simultaneously. If this is true, this thread cannot use
+  // any pointers into the nursery.
+  bool inIonBackendSafeForMinorGC_ = false;
+
+  bool isCompilingWasm_ = false;
+  bool oom_ = false;
+#endif
+
+ public:
+  // Running context when executing on the main thread. Not available during
+  // compilation.
+  JSContext* cx = nullptr;
+
+  // Allocator for temporary memory during compilation.
+  TempAllocator* temp = nullptr;
+
+  // Wrappers with information about the current runtime/realm for use
+  // during compilation.
+  CompileRuntime* runtime = nullptr;
+
+  // Constructor for compilations happening on the main thread.
+  JitContext(JSContext* cx, TempAllocator* temp);
+
+  // Constructor for off-thread Ion compilations.
+  JitContext(CompileRuntime* rt, CompileRealm* realm, TempAllocator* temp);
+
+  // Constructors for Wasm compilation.
+  explicit JitContext(TempAllocator* temp);
+  JitContext();
+
+  ~JitContext();
+
+  int getNextAssemblerId() { return assemblerCount_++; }
+
+  CompileRealm* maybeRealm() const { return realm_; }
+  CompileRealm* realm() const {
+    MOZ_ASSERT(maybeRealm());
+    return maybeRealm();
+  }
+
+#ifdef DEBUG
+  bool isCompilingWasm() { return isCompilingWasm_; }
+  bool hasOOM() { return oom_; }
+  void setOOM() { oom_ = true; }
+
+  bool inIonBackend() const { return inIonBackend_; }
+
+  bool inIonBackendSafeForMinorGC() const {
+    return inIonBackendSafeForMinorGC_;
+  }
+
+  void enterIonBackend(bool safeForMinorGC) {
+    MOZ_ASSERT(!inIonBackend_);
+    MOZ_ASSERT(!inIonBackendSafeForMinorGC_);
+    inIonBackend_ = true;
+    inIonBackendSafeForMinorGC_ = safeForMinorGC;
+  }
+  void leaveIonBackend() {
+    MOZ_ASSERT(inIonBackend_);
+    inIonBackend_ = false;
+    inIonBackendSafeForMinorGC_ = false;
+  }
+#endif
+};
+
+// Process-wide initialization of JIT data structures.
+MOZ_MUST_USE bool InitializeJit();
+
+// Get and set the current JIT context.
+JitContext* GetJitContext();
+JitContext* MaybeGetJitContext();
+
+void SetJitContext(JitContext* ctx);
+
+enum JitExecStatus {
+  // The method call had to be aborted due to a stack limit check. This
+  // error indicates that Ion never attempted to clean up frames.
+  JitExec_Aborted,
+
+  // The method call resulted in an error, and IonMonkey has cleaned up
+  // frames.
+  JitExec_Error,
+
+  // The method call succeeded and returned a value.
+  JitExec_Ok
+};
+
+static inline bool IsErrorStatus(JitExecStatus status) {
+  return status == JitExec_Error || status == JitExec_Aborted;
+}
+
+bool JitSupportsSimd();
+bool JitSupportsAtomics();
+
+}  // namespace jit
+}  // namespace js
+
+#endif /* jit_JitContext_h */
