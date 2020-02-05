227468453f002211d5a57e8fe06c06384b054c6b
Paul Adenot <paul@paul.cx>
2020-02-05 19:21:15 +0000

    Bug 1569809 - Remove AudioNodeOptions from the ctor of AudioWorkletProcessor.  r=baku
    This was changed in https://github.com/WebAudio/web-audio-api/issues/2044
    
    Differential Revision: https://phabricator.services.mozilla.com/D61714
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/webaudio/AudioWorkletProcessor.h
+++ dom/media/webaudio/AudioWorkletProcessor.h
@@ -28,8 +28,7 @@ class AudioWorkletProcessor final : public nsWrapperCache {
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_NATIVE_CLASS(AudioWorkletProcessor)
 
   static already_AddRefed<AudioWorkletProcessor> Constructor(
-      const GlobalObject& aGlobal, const AudioWorkletNodeOptions& aOptions,
-      ErrorResult& aRv);
+      const GlobalObject& aGlobal, ErrorResult& aRv);
 
   nsIGlobalObject* GetParentObject() const { return mParent; }
 
