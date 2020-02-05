3f74e6d79417611e0b9bef4b260cea1ad8488077
Florin Strugariu <fstrugariu@mozilla.com>
2020-02-05 14:01:50 +0000

    Bug 1613332 - Add log messages around raptor conditioned profiles r=tarek
    Differential Revision: https://phabricator.services.mozilla.com/D61663
    
    --HG--
    extra : moz-landing-system : lando

--- testing/condprofile/condprof/client.py
+++ testing/condprofile/condprof/client.py
@@ -58,12 +58,14 @@ def get_profile(
         "task_id": task_id,
         "repo": repo,
     }
+    LOG("Getting conditioned profile with arguments: %s" % params)
     filename = ARTIFACT_NAME % params
     if task_id is None:
         url = TC_LINK % params + filename
     else:
         url = DIRECT_LINK % params + filename
 
+    LOG("preparing download dir")
     if not download_cache:
         download_dir = tempfile.mkdtemp()
     else:
@@ -73,6 +75,7 @@ def get_profile(
             os.makedirs(download_dir)
 
     downloaded_archive = os.path.join(download_dir, filename)
+    LOG("Downloaded archive path: %s" % downloaded_archive)
     retries = 0
 
     while retries < RETRIES:
