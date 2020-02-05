79c2974d43c0a9c4821363f2f9e00871d3efa9e3
Morgan Reschenberg <mreschenberg@mozilla.com>
2020-02-05 10:26:32 +0000

    Bug 1611903: Change 'Text zoom only' checkbox behaviour to call ZoomManager.toggleZoom instead of changing preference directly. r=Gijs, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D61587

--- browser/components/preferences/in-content/main.js
+++ browser/components/preferences/in-content/main.js
@@ -145,9 +145,6 @@ Preferences.addAll([
   // Fonts
   { id: "font.language.group", type: "wstring" },
 
-  // Zoom
-  { id: "browser.zoom.full", type: "bool", inverted: true },
-
   // Languages
   { id: "browser.translation.detectLanguage", type: "bool" },
 
@@ -949,6 +946,11 @@ var gMainPane = {
       let parsedZoom = parseFloat((event.target.value / 100).toFixed(2));
       gMainPane.handleDefaultZoomChange(parsedZoom);
     });
+
+    setEventListener("zoomText", "command", function() {
+      win.ZoomManager.toggleZoom();
+    });
+
     let zoomValues = win.ZoomManager.zoomValues.map(a => {
       return Math.round(a * 100);
     });
@@ -966,6 +968,9 @@ var gMainPane = {
     let menupopup = menulist.querySelector("menupopup");
     menupopup.appendChild(fragment);
     menulist.value = Math.round(selected * 100);
+
+    let checkbox = document.getElementById("zoomText");
+    checkbox.checked = !win.ZoomManager.useFullZoom;
 
     document.getElementById("zoomBox").hidden = false;
   },
