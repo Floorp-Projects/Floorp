47a81f54120576056b7e3bde0bfc599103f43baf
Mirko Brodesser <mbrodesser@mozilla.com>
2020-02-05 12:22:08 +0000

    Bug 1612828: part 3) Remove `nsresult` return type from `Selection::FindInsertionPoint`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D61533

--- dom/base/Selection.h
+++ dom/base/Selection.h
@@ -734,18 +734,18 @@ class Selection final : public nsSupportsWeakReference,
   /**
    * Binary searches the given sorted array of ranges for the insertion point
    * for the given node/offset. The given comparator is used, and the index
-   * where the point should appear in the array is placed in *aInsertionPoint.
+   * where the point should appear in the array is returned.
 
    * If there is an item in the array equal to the input point (aPointNode,
    * aPointOffset), we will return the index of this item.
    *
-   * @param aInsertionPoint can be in [0, `aElementArray->Length()`].
+   * @return the index where the point should appear in the array. In
+   *         [0, `aElementArray->Length()`].
    */
-  static nsresult FindInsertionPoint(
+  static int32_t FindInsertionPoint(
       const nsTArray<StyledRange>* aElementArray, const nsINode& aPointNode,
       int32_t aPointOffset,
-      int32_t (*aComparator)(const nsINode&, int32_t, const nsRange&),
-      int32_t* aInsertionPoint);
+      int32_t (*aComparator)(const nsINode&, int32_t, const nsRange&));
 
   bool HasEqualRangeBoundariesAt(const nsRange& aRange,
                                  int32_t aRangeIndex) const;
