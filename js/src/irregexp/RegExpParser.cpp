e6272c01b482b9a975242a5c18290cb2338f1908
Razvan Maries <rmaries@mozilla.com>
2020-02-05 09:51:47 +0200

    Backed out changeset 50d6945fd47b (bug 1613270) for build bustages on ErrorReporting.cpp. CLOSED TREE

--- js/src/irregexp/RegExpParser.cpp
+++ js/src/irregexp/RegExpParser.cpp
@@ -335,8 +335,7 @@ RegExpParser<CharT>::SyntaxError(unsigned errorNumber, ...)
     va_list args;
     va_start(args, errorNumber);
 
-    ReportCompileErrorLatin1(ts.context(), std::move(err), nullptr,
-                             JSREPORT_ERROR, errorNumber, &args);
+    ReportCompileError(ts.context(), std::move(err), nullptr, JSREPORT_ERROR, errorNumber, &args);
 
     va_end(args);
 }
--- js/src/vm/ErrorReporting.cpp
+++ js/src/vm/ErrorReporting.cpp
@@ -94,11 +94,9 @@ bool js::ReportCompileWarning(JSContext* cx, ErrorMetadata&& metadata,
   return true;
 }
 
-static void ReportCompileErrorImpl(JSContext* cx, ErrorMetadata&& metadata,
-                                   UniquePtr<JSErrorNotes> notes,
-                                   unsigned flags, unsigned errorNumber,
-                                   va_list* args,
-                                   ErrorArgumentsType argumentsType) {
+void js::ReportCompileError(JSContext* cx, ErrorMetadata&& metadata,
+                            UniquePtr<JSErrorNotes> notes, unsigned flags,
+                            unsigned errorNumber, va_list* args) {
   // On the main thread, report the error immediately. When compiling off
   // thread, save the error so that the thread finishing the parse can report
   // it later.
@@ -123,27 +121,13 @@ static void ReportCompileErrorImpl(JSContext* cx, ErrorMetadata&& metadata,
   }
 
   if (!ExpandErrorArgumentsVA(cx, GetErrorMessage, nullptr, errorNumber,
-                              nullptr, argumentsType, err, *args)) {
+                              nullptr, ArgumentsAreLatin1, err, *args)) {
     return;
   }
 
   if (!cx->isHelperThreadContext()) {
     err->throwError(cx);
   }
-}
-
-void js::ReportCompileErrorLatin1(JSContext* cx, ErrorMetadata&& metadata,
-                                  UniquePtr<JSErrorNotes> notes, unsigned flags,
-                                  unsigned errorNumber, va_list* args) {
-  ReportCompileErrorImpl(cx, std::move(metadata), std::move(notes), flags,
-                         errorNumber, args, ArgumentsAreLatin1);
-}
-
-void js::ReportCompileErrorUTF8(JSContext* cx, ErrorMetadata&& metadata,
-                                UniquePtr<JSErrorNotes> notes, unsigned flags,
-                                unsigned errorNumber, va_list* args) {
-  ReportCompileErrorImpl(cx, std::move(metadata), std::move(notes), flags,
-                         errorNumber, args, ArgumentsAreUTF8);
 }
 
 void js::ReportErrorToGlobal(JSContext* cx, Handle<GlobalObject*> global,
