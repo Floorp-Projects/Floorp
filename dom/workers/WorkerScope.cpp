db7085724d077d571dadef5c54464c985607358d
Andrea Marchesini <amarchesini@mozilla.com>
2020-02-05 22:09:44 +0000

    Bug 1611855 - Worklet must be part of the same parent's agentCluster - part 7 - nsIGlobalObject::IsSharedMemoryAllowed(), r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D61277
    
    --HG--
    extra : moz-landing-system : lando

--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -536,6 +536,10 @@ WorkerGlobalScope::GetExistingDebuggerNotificationManager() {
   return mDebuggerNotificationManager;
 }
 
+bool WorkerGlobalScope::IsSharedMemoryAllowed() const {
+  return mWorkerPrivate->IsSharedMemoryAllowed();
+}
+
 Maybe<ClientInfo> WorkerGlobalScope::GetClientInfo() const {
   return mWorkerPrivate->GetClientInfo();
 }
