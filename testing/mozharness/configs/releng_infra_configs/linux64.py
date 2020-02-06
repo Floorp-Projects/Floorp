d0a791c619d957482682c9bc77840806c8238068
Tom Prince <mozilla@hocat.ca>
2020-02-06 22:51:50 +0000

    No bug: [mozharness] Remove unreferenced `releng_infra_configs` config files; r=nthomas
    Differential Revision: https://phabricator.services.mozilla.com/D61610

--- testing/mozharness/configs/releng_infra_configs/linux64.py
+++
@@ -1,5 +0,0 @@
-config = {
-    'env': {
-        'MINIDUMP_STACKWALK': '%(abs_tools_dir)s/breakpad/linux64/minidump_stackwalk',
-    }
-}
--- testing/mozharness/configs/releng_infra_configs/win64.py
+++
@@ -1,5 +0,0 @@
-config = {
-    'env': {
-        'MINIDUMP_STACKWALK': '%(abs_tools_dir)s/breakpad/win64/minidump_stackwalk',
-    }
-}
