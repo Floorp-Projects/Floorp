b13b3b72d290d9199e6a009f6be25ec815f8644e
Andrea Marchesini <amarchesini@mozilla.com>
2020-02-06 13:46:25 +0000

    Bug 1611855 - Worklet must be part of the same parent's agentCluster - part 2 - CloneDataPolicy in writing, r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D61182
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/StructuredCloneHolder.h
+++ dom/base/StructuredCloneHolder.h
@@ -102,7 +102,7 @@ class StructuredCloneHolderBase {
   // of cloning policy.
   bool Write(JSContext* aCx, JS::Handle<JS::Value> aValue,
              JS::Handle<JS::Value> aTransfer,
-             JS::CloneDataPolicy aCloneDataPolicy);
+             const JS::CloneDataPolicy& aCloneDataPolicy);
 
   // If Write() has been called, this method retrieves data and stores it into
   // aValue.
@@ -110,7 +110,7 @@ class StructuredCloneHolderBase {
 
   // Like Read() but it supports handling of clone policy.
   bool Read(JSContext* aCx, JS::MutableHandle<JS::Value> aValue,
-            JS::CloneDataPolicy aCloneDataPolicy);
+            const JS::CloneDataPolicy& aCloneDataPolicy);
 
   bool HasData() const { return !!mBuffer; }
 
@@ -164,18 +164,20 @@ class StructuredCloneHolder : public StructuredCloneHolderBase {
 
   // Normally you should just use Write() and Read().
 
-  void Write(JSContext* aCx, JS::Handle<JS::Value> aValue, ErrorResult& aRv);
+  virtual void Write(JSContext* aCx, JS::Handle<JS::Value> aValue,
+                     ErrorResult& aRv);
 
-  void Write(JSContext* aCx, JS::Handle<JS::Value> aValue,
-             JS::Handle<JS::Value> aTransfer,
-             JS::CloneDataPolicy aCloneDataPolicy, ErrorResult& aRv);
+  virtual void Write(JSContext* aCx, JS::Handle<JS::Value> aValue,
+                     JS::Handle<JS::Value> aTransfer,
+                     const JS::CloneDataPolicy& aCloneDataPolicy,
+                     ErrorResult& aRv);
 
   void Read(nsIGlobalObject* aGlobal, JSContext* aCx,
             JS::MutableHandle<JS::Value> aValue, ErrorResult& aRv);
 
   void Read(nsIGlobalObject* aGlobal, JSContext* aCx,
             JS::MutableHandle<JS::Value> aValue,
-            JS::CloneDataPolicy aCloneDataPolicy, ErrorResult& aRv);
+            const JS::CloneDataPolicy& aCloneDataPolicy, ErrorResult& aRv);
 
   // Call this method to know if this object is keeping some DOM object alive.
   bool HasClonedDOMObjects() const {
@@ -295,13 +297,15 @@ class StructuredCloneHolder : public StructuredCloneHolderBase {
   void ReadFromBuffer(nsIGlobalObject* aGlobal, JSContext* aCx,
                       JSStructuredCloneData& aBuffer,
                       JS::MutableHandle<JS::Value> aValue,
-                      JS::CloneDataPolicy aCloneDataPolicy, ErrorResult& aRv);
+                      const JS::CloneDataPolicy& aCloneDataPolicy,
+                      ErrorResult& aRv);
 
   void ReadFromBuffer(nsIGlobalObject* aGlobal, JSContext* aCx,
                       JSStructuredCloneData& aBuffer,
                       uint32_t aAlgorithmVersion,
                       JS::MutableHandle<JS::Value> aValue,
-                      JS::CloneDataPolicy aCloneDataPolicy, ErrorResult& aRv);
+                      const JS::CloneDataPolicy& aCloneDataPolicy,
+                      ErrorResult& aRv);
 
   void SameProcessScopeRequired(bool* aSameProcessScopeRequired);
 
