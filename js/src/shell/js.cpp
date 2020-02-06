96d2a3958e6f34f3efe8a1895deb2bec5a5e028b
Asumu Takikawa <asumu@igalia.com>
2020-02-06 15:47:34 +0000

    Bug 1608772 - Enable BigInt <-> i64 interconversion by default in JS shell and Nightly r=lth
    This revision enables the Wasm BigInt<->I64 conversion proposal by default in the JS shell and Nightly.
    
    Some tests are moved around to match the defaults and metadata for WPT wasm tests are set.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61762
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -489,7 +489,7 @@ bool shell::enableWasmGc = false;
 bool shell::enableWasmVerbose = false;
 bool shell::enableTestWasmAwaitTier2 = false;
 #ifdef ENABLE_WASM_BIGINT
-bool shell::enableWasmBigInt = false;
+bool shell::enableWasmBigInt = true;
 #endif
 bool shell::enableAsyncStacks = false;
 bool shell::enableStreams = false;
@@ -10411,7 +10411,7 @@ static bool SetContextOptions(JSContext* cx, const OptionParser& op) {
   enableWritableStreams = op.getBoolOption("enable-writable-streams");
   enableReadableStreamPipeTo = op.getBoolOption("enable-readablestream-pipeto");
 #ifdef ENABLE_WASM_BIGINT
-  enableWasmBigInt = op.getBoolOption("wasm-bigint");
+  enableWasmBigInt = !op.getBoolOption("no-wasm-bigint");
 #endif
   enableFields = !op.getBoolOption("disable-experimental-fields");
   enableAwaitFix = op.getBoolOption("enable-experimental-await-fix");
@@ -11180,10 +11180,10 @@ int main(int argc, char** argv, char** envp) {
       !op.addBoolOption('\0', "disable-wasm-huge-memory",
                         "Disable WebAssembly huge memory") ||
 #ifdef ENABLE_WASM_BIGINT
-      !op.addBoolOption('\0', "wasm-bigint",
-                        "Enable WebAssembly BigInt conversions") ||
+      !op.addBoolOption('\0', "no-wasm-bigint",
+                        "Disable WebAssembly BigInt conversions") ||
 #else
-      !op.addBoolOption('\0', "wasm-bigint", "No-op") ||
+      !op.addBoolOption('\0', "no-wasm-bigint", "No-op") ||
 #endif
       !op.addBoolOption('\0', "test-wasm-await-tier2",
                         "Forcibly activate tiering and block "
