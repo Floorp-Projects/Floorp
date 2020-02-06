a9a09fab93837aa601f06701daa9470a5757ec20
Ted Campbell <tcampbell@mozilla.com>
2020-02-06 15:23:38 +0000

    Bug 1612863 - Use OOM crash when InitSelfHostedCode fails. r=mccr8
    The browser is unable to start if this operation fails, but we should track
    the difference between OOM and other forms of failure. This should now
    classify OOM errors correctly in crash stats.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61779

--- js/xpconnect/src/XPCJSContext.cpp
+++ js/xpconnect/src/XPCJSContext.cpp
@@ -1268,7 +1268,16 @@ nsresult XPCJSContext::Initialize() {
   Preferences::RegisterCallback(ReloadPrefsCallback, "fuzzing.enabled", this);
 #endif
 
-  MOZ_RELEASE_ASSERT(JS::InitSelfHostedCode(cx), "InitSelfHostedCode failed");
+  if (!JS::InitSelfHostedCode(cx)) {
+    // Note: If no exception is pending, failure is due to OOM.
+    if (!JS_IsExceptionPending(cx) || JS_IsThrowingOutOfMemory(cx)) {
+      NS_ABORT_OOM(0);  // Size is unknown.
+    }
+
+    // Failed to execute self-hosted JavaScript! Uh oh.
+    MOZ_CRASH("InitSelfHostedCode failed");
+  }
+
   MOZ_RELEASE_ASSERT(Runtime()->InitializeStrings(cx),
                      "InitializeStrings failed");
 
