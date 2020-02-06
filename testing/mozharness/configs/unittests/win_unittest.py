623de665034eee43a54ff02939b61385ffd5990d
Mike Hommey <mh+mozilla@glandium.org>
2020-02-06 08:39:54 +0000

    Bug 1525218 - Use minidump_stackwalk toolchain artifacts instead of tooltool packages. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D59936
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozharness/configs/unittests/win_unittest.py
+++ testing/mozharness/configs/unittests/win_unittest.py
@@ -274,7 +274,5 @@ config = {
                              "cppunittest": [],
                              "jittest": [],
                              },
-    "minidump_stackwalk_path": "win32-minidump_stackwalk.exe",
-    "minidump_tooltool_manifest_path": "config/tooltool-manifests/win32/releng.manifest",
     "nodejs_path": NODEJS_PATH,
 }
