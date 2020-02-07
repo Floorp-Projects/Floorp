877ec9a17adbb21f63dba36ea473b031e572d5b4
Dana Keeler <dkeeler@mozilla.com>
2020-02-07 21:25:03 +0000

    bug 1586915 - build osclientcerts in-tree for macOS r=jcj,mstange
    This patch implements osclientcerts for macOS.
    Because the SDK we build with isn't recent enough, some of the functions we
    need aren't guaranteed to be available. To handle this, we load the Security
    framework at runtime and attempt to locate the symbols we need. If this
    succeeds, then operation proceeds as normal. Otherwise, the module will report
    that there are no certificates/keys available.
    
    Differential Revision: https://phabricator.services.mozilla.com/D59957
    
    --HG--
    extra : moz-landing-system : lando

--- security/manager/ssl/osclientcerts/build.rs
+++ security/manager/ssl/osclientcerts/build.rs
@@ -3,19 +3,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[cfg(target_os = "windows")]
 use bindgen;
 
+#[cfg(target_os = "windows")]
 use std::env;
+#[cfg(target_os = "windows")]
 use std::path::PathBuf;
 
 fn main() {
-    let bindings = bindgen::Builder::default()
-        .header("src/wrapper-windows.h")
-        .whitelist_function("NCryptSignHash")
-        .generate()
-        .expect("Unable to generate bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR unset?"));
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings");
+    #[cfg(target_os = "windows")]
+    {
+        let bindings = bindgen::Builder::default()
+            .header("src/wrapper-windows.h")
+            .whitelist_function("NCryptSignHash")
+            .generate()
+            .expect("Unable to generate bindings");
+        let out_path = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR unset?"));
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings");
+    }
 }
