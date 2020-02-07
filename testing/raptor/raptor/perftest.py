449ae902f97201849a1349f2da52c8521a1b2444
Florin Strugariu <fstrugariu@mozilla.com>
2020-02-07 16:38:05 +0000

    Bug 1613938 - Raptor tests hang when downloading conditioned profiles r=tarek
    Differential Revision: https://phabricator.services.mozilla.com/D62053

--- testing/raptor/raptor/perftest.py
+++ testing/raptor/raptor/perftest.py
@@ -13,6 +13,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import traceback
 from abc import ABCMeta, abstractmethod
 
 import mozinfo
@@ -205,6 +206,12 @@ either Raptor or browsertime."""
             cond_prof_target_dir = get_profile(
                 temp_download_dir, platform, "settled", repo="try"
             )
+        except Exception:
+            # any other error is a showstopper
+            LOG.critical("Could not get the conditioned profile")
+            traceback.print_exc()
+            raise
+
         # now get the full directory path to our fetched conditioned profile
         self.conditioned_profile_dir = os.path.join(
             temp_download_dir, cond_prof_target_dir
