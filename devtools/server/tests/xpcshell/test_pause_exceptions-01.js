958a11f1c5fc5757619b90a94ff89054c4431954
chujun <chujunlu@hotmail.com>
2020-02-07 00:47:49 +0000

    Bug 1596114 - Refactor server unit tests part6 r=jlast
    Remove `threadFrontTestFinished`
    
    Differential Revision: https://phabricator.services.mozilla.com/D60384
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/server/tests/xpcshell/test_pause_exceptions-01.js
+++ devtools/server/tests/xpcshell/test_pause_exceptions-01.js
@@ -6,37 +6,30 @@
 
 /**
  * Test that setting pauseOnExceptions to true will cause the debuggee to pause
- * when an exceptions is thrown.
+ * when an exception is thrown.
  */
 
-var gDebuggee;
-var gThreadFront;
-
 add_task(
-  threadFrontTest(
-    async ({ threadFront, debuggee }) => {
-      gThreadFront = threadFront;
-      gDebuggee = debuggee;
-      test_pause_frame();
-    },
-    { waitForFinish: true }
-  )
+  threadFrontTest(async ({ threadFront, debuggee }) => {
+    await executeOnNextTickAndWaitForPause(
+      () => evaluateTestCode(debuggee),
+      threadFront
+    );
+
+    threadFront.pauseOnExceptions(true, false);
+    threadFront.resume();
+
+    const packet = await waitForPause(threadFront);
+    Assert.equal(packet.why.type, "exception");
+    Assert.equal(packet.why.exception, 42);
+
+    threadFront.resume();
+  })
 );
 
-function test_pause_frame() {
-  gThreadFront.once("paused", function(packet) {
-    gThreadFront.once("paused", function(packet) {
-      Assert.equal(packet.why.type, "exception");
-      Assert.equal(packet.why.exception, 42);
-      gThreadFront.resume().then(() => threadFrontTestFinished());
-    });
-
-    gThreadFront.pauseOnExceptions(true, false);
-    gThreadFront.resume();
-  });
-
+function evaluateTestCode(debuggee) {
   /* eslint-disable */
-  gDebuggee.eval("(" + function () {
+  debuggee.eval("(" + function () {
     function stopMe() {
       debugger;
       throw 42;
