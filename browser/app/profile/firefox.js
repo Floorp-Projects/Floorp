32b94488450c973d026fc48ad53b7f85510fe72a
Jason Laster <jlaster@mozilla.com>
2020-02-07 18:43:32 +0000

    Bug 1613538 - Enable eager evaluation on DevEdition by default. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D61929
    
    --HG--
    extra : moz-landing-system : lando

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2148,7 +2148,7 @@ pref("devtools.webconsole.input.autocomplete",true);
 
 // Set to true to eagerly show the results of webconsole terminal evaluations
 // when they don't have side effects.
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
   pref("devtools.webconsole.input.eagerEvaluation", true);
 #else
   pref("devtools.webconsole.input.eagerEvaluation", false);
