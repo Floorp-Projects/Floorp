174580611b17ddb7ff90a19cb6b053749a6c2f86
Jared Wein <jwein@mozilla.com>
2020-02-07 21:02:09 +0000

    Bug 1613731 - Move the 'fill login' menuitem up to the top of the context menu. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D61919

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -984,10 +984,12 @@ class nsContextMenu {
       let insertBeforeElement = document.getElementById("fill-login-no-logins");
       popup.insertBefore(fragment, insertBeforeElement);
     } finally {
-      this.showItem("fill-login-separator", showFill);
       this.showItem("fill-login", showFill);
       this.showItem("fill-login-generated-password", showGenerate);
-      this.showItem("generated-password-separator", showGenerate);
+      this.showItem(
+        "fill-login-and-generated-password-separator",
+        showFill || showGenerate
+      );
     }
   }
 
