e8ef45785533a26c112a3fc87f7dcb18fd191409
Boris Zbarsky <bzbarsky@mit.edu>
2020-02-09 14:51:57 +0000

    Bug 1614173.  Stop using NS_ERROR_DOM_TYPE_ERR in ChromeNodeList. r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D62156

--- dom/base/ChromeNodeList.cpp
+++ dom/base/ChromeNodeList.cpp
@@ -27,7 +27,7 @@ void ChromeNodeList::Append(nsINode& aNode, ErrorResult& aError) {
   if (!aNode.IsContent()) {
     // nsINodeList deals with nsIContent objects only, so need to
     // filter out other nodes for now.
-    aError.Throw(NS_ERROR_DOM_TYPE_ERR);
+    aError.ThrowTypeError(u"The node passed in is not a ChildNode");
     return;
   }
 
@@ -36,7 +36,7 @@ void ChromeNodeList::Append(nsINode& aNode, ErrorResult& aError) {
 
 void ChromeNodeList::Remove(nsINode& aNode, ErrorResult& aError) {
   if (!aNode.IsContent()) {
-    aError.Throw(NS_ERROR_DOM_TYPE_ERR);
+    aError.ThrowTypeError(u"The node passed in is not a ChildNode");
     return;
   }
 
