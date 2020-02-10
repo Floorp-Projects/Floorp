d0e1f6aa131cf1ed3a7d33970ea0308a01b57bd8
Greg Tatum <gtatum@mozilla.com>
2020-02-10 15:53:23 +0000

    Bug 1602079 - Add a mochitest to test the profiler.firefox.com WebChannel; r=julienw
    This test covers the basic enabling of the profiler popup menu button.
    
    Differential Revision: https://phabricator.services.mozilla.com/D57123

---
+++ devtools/client/performance-new/test/browser/browser_webchannel-enable-menu-button.js
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+add_task(async function test() {
+  info("Test the WebChannel mechanism works for turning on the menu button.");
+  await makeSureProfilerPopupIsDisabled();
+
+  await withWebChannelTestDocument(async browser => {
+    await waitForTabTitle("WebChannel Page Ready");
+    await waitForProfilerMenuButton();
+    ok(true, "The profiler menu button was enabled by the WebChannel.");
+  });
+});
