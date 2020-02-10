a2b4601a7b57b3036b2b1b3c76a77939ce49df05
Gerald Squelart <gsquelart@mozilla.com>
2020-02-10 20:44:33 +0000

    Bug 1614013 - Fix RAII Profiler object scopes - r=gregtatum
    In bug 1578329 I introduced two scoping mistakes:
    - A marker was made to have a shorter duration.
    - A label was scoped too short and so would most likely be missed during
      sampling.
    This patch reverts to the original wider scope.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62274

--- gfx/thebes/gfxFontEntry.cpp
+++ gfx/thebes/gfxFontEntry.cpp
@@ -1741,12 +1741,12 @@ void gfxFontFamily::FindFontForChar(GlobalFontMatch* aMatchData) {
   }
 
 #ifdef MOZ_GECKO_PROFILER
+  nsCString charAndName;
   if (profiler_can_accept_markers()) {
-    nsCString charAndName =
-        nsPrintfCString("\\u%x %s", aMatchData->mCh, mName.get());
-    AUTO_PROFILER_LABEL_DYNAMIC_NSCSTRING("gfxFontFamily::FindFontForChar",
-                                          LAYOUT, charAndName);
+    charAndName = nsPrintfCString("\\u%x %s", aMatchData->mCh, mName.get());
   }
+  AUTO_PROFILER_LABEL_DYNAMIC_NSCSTRING("gfxFontFamily::FindFontForChar",
+                                        LAYOUT, charAndName);
 #endif
 
   AutoTArray<gfxFontEntry*, 4> entries;
