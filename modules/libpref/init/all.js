db44316423dadb43f03c4a7bdf7340bacbe4dd21
Gabriele Svelto <gsvelto@mozilla.com>
2020-02-10 22:36:37 +0000

    Bug 1614337 - Turn dom.ipc.tabs.createKillHardCrashReports into a static preference r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D62268
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2669,14 +2669,6 @@ pref("plugins.favorfallback.rules", "");
   #endif
 #endif
 
-// Whether or not to collect a paired minidump when force-killing a
-// content process.
-#ifdef RELEASE_OR_BETA
-  pref("dom.ipc.tabs.createKillHardCrashReports", false);
-#else
-  pref("dom.ipc.tabs.createKillHardCrashReports", true);
-#endif
-
 pref("dom.ipc.plugins.flash.disable-protected-mode", false);
 
 pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", true);
