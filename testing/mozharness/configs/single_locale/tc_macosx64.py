6971fe2010cf9efdbadcdd02a18dcb786bb371a9
Tom Prince <mozilla@hocat.ca>
2020-02-11 19:47:06 +0000

    Bug 1527313: [mozharness] Don't hardcode `MOZ_OBJDIR` in mozharness configs; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D62345

--- testing/mozharness/configs/single_locale/tc_macosx64.py
+++ testing/mozharness/configs/single_locale/tc_macosx64.py
@@ -3,7 +3,7 @@ import os
 config = {
     "bootstrap_env": {
         "NO_MERCURIAL_SETUP_CHECK": "1",
-        "MOZ_OBJDIR": "obj-firefox",
+        "MOZ_OBJDIR": "%(abs_obj_dir)s",
         "EN_US_BINARY_URL": os.environ["EN_US_BINARY_URL"],
         "DIST": "%(abs_obj_dir)s",
         "L10NBASEDIR": "../../l10n",
