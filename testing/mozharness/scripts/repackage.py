fe4e7e3a75acd380b293eea322c1b36640cdaedc
Tom Prince <mozilla@hocat.ca>
2020-02-11 19:47:02 +0000

    No bug: [mozhanress] Remove unused abs_tools_dir; r=Callek
    The tools repo is no longer used, so remove the references to the path where it
    was checked out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62404
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozharness/scripts/repackage.py
+++ testing/mozharness/scripts/repackage.py
@@ -43,7 +43,6 @@ class Repackage(BaseScript):
         config = self.config
 
         dirs = {}
-        dirs['abs_tools_dir'] = os.path.join(abs_dirs['abs_work_dir'], 'tools')
         dirs['abs_src_dir'] = os.environ['GECKO_PATH']
         dirs['abs_input_dir'] = os.path.join(abs_dirs['base_work_dir'], 'fetches')
         output_dir_suffix = []
