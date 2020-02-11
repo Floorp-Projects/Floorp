a68356b907fe18b1fa0a5caf7c1de19204fc7db1
Brindusan Cristian <cbrindusan@mozilla.com>
2020-02-11 12:27:49 +0200

    Backed out 5 changesets (bug 1613378) for mochitest failures at test_general.html. CLOSED TREE
    Backed out changeset f38b806ee78b (bug 1613378)
    Backed out changeset cee4bbabc4b5 (bug 1613378)
    Backed out changeset e3a2ab6d7267 (bug 1613378)
    Backed out changeset 6fe76439f1d3 (bug 1613378)
    Backed out changeset f3baf3407cbe (bug 1613378)

--- dom/base/nsDocumentEncoder.cpp
+++ dom/base/nsDocumentEncoder.cpp
@@ -1378,7 +1378,7 @@ nsHTMLCopyEncoder::SetSelection(Selection* aSelection) {
   // there's no Clone() for selection! fix...
   // nsresult rv = aSelection->Clone(getter_AddRefs(mSelection);
   // NS_ENSURE_SUCCESS(rv, rv);
-  mEncodingScope.mSelection = new Selection(nullptr);
+  mEncodingScope.mSelection = new Selection();
 
   // loop thru the ranges in the selection
   for (uint32_t rangeIdx = 0; rangeIdx < rangeCount; ++rangeIdx) {
