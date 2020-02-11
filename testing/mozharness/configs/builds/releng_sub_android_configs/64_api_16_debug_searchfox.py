5f05d0993475f429e07959c95951356ab4f44bfd
Tom Prince <mozilla@hocat.ca>
2020-02-11 19:46:47 +0000

    Bug 1527313: Consolidate multi-locale config, since it does not vary by branch; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D62342
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_debug_searchfox.py
+++ testing/mozharness/configs/builds/releng_sub_android_configs/64_api_16_debug_searchfox.py
@@ -4,6 +4,5 @@ config = {
         'SCCACHE_DISABLE': '1',
     },
     'src_mozconfig': 'mobile/android/config/mozconfigs/android-api-16/debug-searchfox',
-    'multi_locale_config_platform': 'android',
     'debug_build': True,
 }
