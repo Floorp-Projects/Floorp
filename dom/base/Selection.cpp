36437a9d2dbc69b1143cde02197d088ce5a0ffdb
Mirko Brodesser <mbrodesser@mozilla.com>
2020-02-12 12:27:32 +0000

    Bug 1613378: part 9) Clarify dependency to `mFrameSelection` in `Selection` when adding/removing ranges. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D62198
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Selection.cpp
+++ dom/base/Selection.cpp
@@ -1370,7 +1370,7 @@ void Selection::SelectFramesInAllRanges(nsPresContext* aPresContext) {
  * traversing through the frames
  */
 nsresult Selection::SelectFrames(nsPresContext* aPresContext, nsRange* aRange,
-                                 bool aSelect) {
+                                 bool aSelect) const {
   if (!mFrameSelection || !aPresContext || !aPresContext->GetPresShell()) {
     // nothing to do
     return NS_OK;
@@ -1878,10 +1878,12 @@ void Selection::AddRangeAndSelectFramesAndNotifyListeners(nsRange& aRange,
     SetInterlinePosition(true, IgnoreErrors());
   }
 
+  if (!mFrameSelection) {
+    return;  // nothing to do
+  }
+
   RefPtr<nsPresContext> presContext = GetPresContext();
   SelectFrames(presContext, range, true);
-
-  if (!mFrameSelection) return;  // nothing to do
 
   // Be aware, this instance may be destroyed after this call.
   // XXX Why doesn't this call Selection::NotifySelectionListener() directly?
