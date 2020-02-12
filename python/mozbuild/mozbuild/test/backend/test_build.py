3314dda5939252c04a7f5c88e1fb6fb5621d30f4
Ricky Stewart <rstewart@mozilla.com>
2020-02-12 23:19:37 +0000

    Bug 1613263 - Use io.open() rather than open() in mozbuild/preprocessor.py r=firefox-build-system-reviewers,mshal
    Differential Revision: https://phabricator.services.mozilla.com/D61647
    
    --HG--
    extra : moz-landing-system : lando

--- python/mozbuild/mozbuild/test/backend/test_build.py
+++ python/mozbuild/mozbuild/test/backend/test_build.py
@@ -151,11 +151,8 @@ class TestBuild(unittest.TestCase):
         test_path = os.sep.join(('$SRCDIR', 'python', 'mozbuild', 'mozbuild',
                                  'test', 'backend', 'data', 'build')) + os.sep
 
-        # We want unicode instances out of the files, because having plain str
-        # makes assertEqual diff output in case of error extra verbose because
-        # of the difference in type.
         result = {
-            p: f.open().read().decode('utf-8')
+            p: f.open(mode='r').read()
             for p, f in FileFinder(mozpath.join(config.topobjdir, 'dist'))
         }
         self.assertTrue(len(result))
