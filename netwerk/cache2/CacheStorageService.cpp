31990b30b72fca95fda2231a387872f0546d2820
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-02-12 17:04:50 +0200

    Backed out changeset 4758bb49e763 (bug 1614637) for causing multiple failures.
    CLOSED TREE

--- netwerk/cache2/CacheStorageService.cpp
+++ netwerk/cache2/CacheStorageService.cpp
@@ -1273,15 +1273,9 @@ void CacheStorageService::OnMemoryConsumptionChange(
 
   if (!overLimit) return;
 
-  {
-    mozilla::MutexAutoLock lock(mLock);
-
-    // It's likely the timer has already been set when we get here,
-    // check outside the lock to save resources.
-    if (mPurgeTimer) {
-      return;
-    }
-  }
+  // It's likely the timer has already been set when we get here,
+  // check outside the lock to save resources.
+  if (mPurgeTimer) return;
 
   // We don't know if this is called under the service lock or not,
   // hence rather dispatch.
