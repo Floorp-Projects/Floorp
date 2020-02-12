c798ed8038ce85d0550b7e3c36d2e89adc72d699
Boris Zbarsky <bzbarsky@mit.edu>
2020-02-12 15:38:02 +0000

    Bug 1614038 part 1.  Fix ChannelSplitterNode to follow the spec for its setters. r=padenot
    This is based on looking at what the actual spec constraints around the
    channelCount, channelCountMode, and channelInterpretation setters are and on
    testing Chrome's behavior: the setters _can_ be called as long as you do it with
    the current value.  That said, the spec sure could use being clearer here; the
    style it's using is pretty hard to follow, unfortunately.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62140
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/webaudio/ChannelSplitterNode.cpp
+++ dom/media/webaudio/ChannelSplitterNode.cpp
@@ -71,22 +71,25 @@ already_AddRefed<ChannelSplitterNode> ChannelSplitterNode::Create(
   // Manually check that the other options are valid, this node has
   // channelCount, channelCountMode and channelInterpretation constraints: they
   // cannot be changed from the default.
-  if (aOptions.mChannelCount.WasPassed() &&
-      aOptions.mChannelCount.Value() != audioNode->ChannelCount()) {
-    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-    return nullptr;
+  if (aOptions.mChannelCount.WasPassed()) {
+    audioNode->SetChannelCount(aOptions.mChannelCount.Value(), aRv);
+    if (aRv.Failed()) {
+      return nullptr;
+    }
   }
-  if (aOptions.mChannelInterpretation.WasPassed() &&
-      aOptions.mChannelInterpretation.Value() !=
-          audioNode->ChannelInterpretationValue()) {
-    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-    return nullptr;
+  if (aOptions.mChannelInterpretation.WasPassed()) {
+    audioNode->SetChannelInterpretationValue(
+        aOptions.mChannelInterpretation.Value(), aRv);
+    if (aRv.Failed()) {
+      return nullptr;
+    }
   }
-  if (aOptions.mChannelCountMode.WasPassed() &&
-      aOptions.mChannelCountMode.Value() !=
-          audioNode->ChannelCountModeValue()) {
-    aRv.Throw(NS_ERROR_DOM_INVALID_STATE_ERR);
-    return nullptr;
+  if (aOptions.mChannelCountMode.WasPassed()) {
+    audioNode->SetChannelCountModeValue(aOptions.mChannelCountMode.Value(),
+                                        aRv);
+    if (aRv.Failed()) {
+      return nullptr;
+    }
   }
 
   return audioNode.forget();
