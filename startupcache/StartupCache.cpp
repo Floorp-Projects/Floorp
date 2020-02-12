cdd059e2c223632fbf151d65786a337303e5bdf2
shindli <shindli@mozilla.com>
2020-02-12 22:37:08 +0200

    Backed out 2 changesets (bug 1607505) for causing xpcshell permafailures in netwerk/test/unit/test_alt-data_simple.js CLOSED TREE
    Backed out changeset d79f1d283acd (bug 1607505)
    Backed out changeset 7b7b147b6955 (bug 1607505)

--- startupcache/StartupCache.cpp
+++ startupcache/StartupCache.cpp
@@ -147,7 +147,26 @@ StartupCache::StartupCache()
       mPrefetchThread(nullptr) {}
 
 StartupCache::~StartupCache() {
+  if (mTimer) {
+    mTimer->Cancel();
+  }
+
+  // Generally, the in-memory table should be empty here,
+  // but an early shutdown means either mTimer didn't run
+  // or the write thread is still running.
   WaitOnWriteThread();
+  WaitOnPrefetchThread();
+
+  // If we shutdown quickly timer wont have fired. Instead of writing
+  // it on the main thread and block the shutdown we simply wont update
+  // the startup cache. Always do this if the file doesn't exist since
+  // we use it part of the package step.
+  if (!mCacheData.initialized() || ShouldCompactCache()) {
+    mDirty = true;
+    auto result = WriteToDisk();
+    Unused << NS_WARN_IF(result.isErr());
+  }
+
   UnregisterWeakMemoryReporter(this);
 }
 
@@ -594,25 +613,6 @@ void StartupCache::InvalidateCache(bool memoryOnly) {
   }
 }
 
-void StartupCache::MaybeInitShutdownWrite() {
-  if (mTimer) {
-    mTimer->Cancel();
-  }
-  gShutdownInitiated = true;
-
-  MaybeSpawnWriteThread();
-
-  // If we shutdown quickly timer wont have fired. Instead of writing
-  // it on the main thread and block the shutdown we simply wont update
-  // the startup cache. Always do this if the file doesn't exist since
-  // we use it part of the package step.
-  if (!mCacheData.initialized() || ShouldCompactCache()) {
-    mDirty = true;
-    auto result = WriteToDisk();
-    Unused << NS_WARN_IF(result.isErr());
-  }
-}
-
 void StartupCache::IgnoreDiskCache() {
   gIgnoreDiskCache = true;
   if (gStartupCache) gStartupCache->InvalidateCache();
@@ -690,28 +690,22 @@ void StartupCache::WriteTimeout(nsITimer* aTimer, void* aClosure) {
    * if the StartupCache object is valid.
    */
   StartupCache* startupCacheObj = static_cast<StartupCache*>(aClosure);
-  startupCacheObj->MaybeSpawnWriteThread();
-}
-
-/*
- * See StartupCache::WriteTimeout above - this is just the non-static body.
- */
-void StartupCache::MaybeSpawnWriteThread() {
-  if (mWrittenOnce || mWriteThread) {
+  if (startupCacheObj->mWrittenOnce) {
     return;
   }
 
-  if (mCacheData.initialized() && !ShouldCompactCache()) {
+  if (startupCacheObj->mCacheData.initialized() &&
+      !startupCacheObj->ShouldCompactCache()) {
     return;
   }
 
-  WaitOnPrefetchThread();
-  mStartupWriteInitiated = false;
-  mDirty = true;
-  mCacheData.reset();
-  mWriteThread = PR_CreateThread(PR_USER_THREAD, StartupCache::ThreadedWrite,
-                                 this, PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD,
-                                 PR_JOINABLE_THREAD, 512 * 1024);
+  startupCacheObj->WaitOnPrefetchThread();
+  startupCacheObj->mStartupWriteInitiated = false;
+  startupCacheObj->mDirty = true;
+  startupCacheObj->mCacheData.reset();
+  startupCacheObj->mWriteThread = PR_CreateThread(
+      PR_USER_THREAD, StartupCache::ThreadedWrite, startupCacheObj,
+      PR_PRIORITY_NORMAL, PR_GLOBAL_THREAD, PR_JOINABLE_THREAD, 512 * 1024);
 }
 
 // We don't want to refcount StartupCache, so we'll just
@@ -726,7 +720,6 @@ nsresult StartupCacheListener::Observe(nsISupports* subject, const char* topic,
   if (strcmp(topic, NS_XPCOM_SHUTDOWN_OBSERVER_ID) == 0) {
     // Do not leave the thread running past xpcom shutdown
     sc->WaitOnWriteThread();
-    sc->WaitOnPrefetchThread();
     StartupCache::gShutdownInitiated = true;
   } else if (strcmp(topic, "startupcache-invalidate") == 0) {
     sc->InvalidateCache(data && nsCRT::strcmp(data, u"memoryOnly") == 0);
