b0bbc80821c8429ba36a17197f2f58440acb8fe0
Coroiu Cristina <ccoroiu@mozilla.com>
2020-02-12 12:13:03 +0200

    Backed out changeset b4dc366c9161 (bug 1353652) for build bustage on linux32 at build/src/widget/gtk/MPRISServiceHandler.cpp a=backout

--- widget/gtk/MediaKeysEventSourceFactory.cpp
+++ widget/gtk/MediaKeysEventSourceFactory.cpp
@@ -3,12 +3,12 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MediaKeysEventSourceFactory.h"
-#include "MPRISServiceHandler.h"
 
 namespace mozilla::widget {
 
 mozilla::dom::MediaControlKeysEventSource* CreateMediaControlKeysEventSource() {
-  return new MPRISServiceHandler();
+  // TODO : will implement this in bug 1353652.
+  return nullptr;
 }
 
 }  // namespace mozilla::widget
