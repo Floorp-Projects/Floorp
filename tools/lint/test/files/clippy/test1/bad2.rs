c967ba8643b6161a337d9253161e1617b3bac4d9
Sylvestre Ledru <sledru@mozilla.com>
2020-02-13 23:22:56 +0000

    Bug 1361341 - Add rust clippy to mozlint r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D58250

---
+++ tools/lint/test/files/clippy/test1/bad2.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    let mut a;
+    let mut b=1;
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+
+
+    for x in 5..10 - 5 {
+        a = x;
+    }
+
+    }
