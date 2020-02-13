5e1007953cadb94e22ce20b0306549863d92c773
Sylvestre Ledru <sledru@mozilla.com>
2020-02-13 21:39:51 +0000

    Bug 1361341 - Add rust clippy to mozlint r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D58250

---
+++ tools/lint/test/test_clippy.py
@@ -0,0 +1,89 @@
+import mozunit
+
+
+LINTER = 'clippy'
+
+
+def test_basic(lint, config, paths):
+    results = lint(paths("test1/"))
+    print(results)
+    assert len(results) > 7
+
+    assert "is never read" in results[0].message or 'but never used' in results[0].message
+    assert results[0].level == "warning"
+    assert results[0].lineno == 7
+    assert results[0].column == 13
+    assert results[0].rule == "unused_assignments"
+    assert results[0].relpath == "test1/bad.rs"
+    assert "tools/lint/test/files/clippy/test1/bad.rs" in results[0].path
+
+    assert "value assigned to `b` is never read" in results[1].message
+    assert results[1].level == "warning"
+    assert results[1].relpath == "test1/bad.rs"
+
+    assert "this looks like you are trying to swap `a` and `b`" in results[2].message
+    assert results[2].level == "error"
+    assert results[2].relpath == "test1/bad.rs"
+    assert results[2].rule == "clippy::almost_swapped"
+    assert "or maybe you should use `std::mem::replace`" in results[2].hint
+
+    assert "variable does not need to be mutable" in results[6].message
+    assert results[6].relpath == "test1/bad2.rs"
+    assert results[6].rule == "unused_mut"
+
+    assert "this range is empty so this for loop will never run" in results[7].message
+    assert results[7].level == "error"
+    assert results[7].relpath == "test1/bad2.rs"
+    assert results[7].rule == "clippy::reverse_range_loop"
+
+
+def test_error(lint, config, paths):
+    results = lint(paths("test1/Cargo.toml"))
+    # Should fail. We don't accept Cargo.toml as input
+    assert results == 1
+
+
+def test_file_and_path_provided(lint, config, paths):
+    results = lint(paths("./test2/src/bad_1.rs", "test1/"))
+    # even if clippy analyzed it
+    # we should not have anything from bad_2.rs
+    # as mozlint is filtering out the file
+    print(results)
+    assert len(results) > 16
+    assert "value assigned to `a` is never read" in results[0].message
+    assert results[0].level == "warning"
+    assert results[0].lineno == 7
+    assert results[0].column == 13
+    assert results[0].rule == "unused_assignments"
+    assert results[0].relpath == "test1/bad.rs"
+    assert "tools/lint/test/files/clippy/test1/bad.rs" in results[0].path
+    assert "value assigned to `a` is never read" in results[0].message
+    assert results[8].level == "warning"
+    assert results[8].lineno == 9
+    assert results[8].column == 13
+    assert results[8].rule == "unused_assignments"
+    assert results[8].relpath == "test2/src/bad_1.rs"
+    assert "tools/lint/test/files/clippy/test2/src/bad_1.rs" in results[8].path
+    for r in results:
+        assert "bad_2.rs" not in r.relpath
+
+
+def test_file_provided(lint, config, paths):
+    results = lint(paths("./test2/src/bad_1.rs"))
+    # even if clippy analyzed it
+    # we should not have anything from bad_2.rs
+    # as mozlint is filtering out the file
+    print(results)
+    assert len(results) > 8
+    assert results[0].level == "warning"
+    assert results[0].lineno == 9
+    assert results[0].column == 13
+    assert results[0].rule == "unused_assignments"
+    assert results[0].relpath == "test2/src/bad_1.rs"
+    assert "tools/lint/test/files/clippy/test2/src/bad_1.rs" in results[0].path
+    for r in results:
+        assert "bad_2.rs" not in r.relpath
+
+
+if __name__ == '__main__':
+    mozunit.main()
