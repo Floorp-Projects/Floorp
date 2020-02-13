ccd73dffd31d127dd0323201ee5fac86c934aad1
Sam Foster <sfoster@mozilla.com>
2020-02-13 22:11:01 +0000

    Bug 1536728 - Show dismissed save/update doorhanger when password value is changed. r=MattN
    * Behavior is behind a false-by-default pref signon.passwordEditCapture.enabled
    * Responds to change events on the field (not input)
    * New browser-chrome tests for doorhanger behavior when editing (non-generated) password field values
    
    Differential Revision: https://phabricator.services.mozilla.com/D54264

--- toolkit/components/passwordmgr/test/browser/browser_context_menu_generated_password.js
+++ toolkit/components/passwordmgr/test/browser/browser_context_menu_generated_password.js
@@ -192,12 +192,14 @@ add_task(async function fill_generated_password_nonempty_field() {
     },
     async function(browser) {
       await SimpleTest.promiseFocus(browser.ownerGlobal);
+      await changeContentFormValues(browser, {
+        [passwordInputSelector]: "aa",
+      });
       await SpecialPowers.spawn(
         browser,
         [[passwordInputSelector]],
         function checkInitialFieldValue(inputSelector) {
           const input = content.document.querySelector(inputSelector);
-          input.setUserInput("aa");
           is(
             content.getComputedStyle(input).filter,
             "none",
@@ -233,6 +235,8 @@ add_task(async function fill_generated_password_nonempty_field() {
       );
     }
   );
+  LoginTestUtils.clearData();
+  LoginTestUtils.resetGeneratedPasswordsCache();
 });
 
 add_task(async function fill_generated_password_with_matching_logins() {
@@ -467,7 +471,7 @@ add_task(async function test_edited_generated_password_in_new_tab() {
     }
   );
 
-  Services.logins.removeAllLogins();
+  LoginTestUtils.clearData();
   LoginTestUtils.resetGeneratedPasswordsCache();
   await SpecialPowers.popPrefEnv();
 });
