0e8445443e0170d4f6556179a8f39cb3bb1162b8
Sam Foster <sfoster@mozilla.com>
2020-02-13 00:19:12 +0000

    Bug 1536728 - test-only changes to ensure we get typed input into forms. r=MattN
    Differential Revision: https://phabricator.services.mozilla.com/D60117

--- toolkit/components/passwordmgr/test/browser/browser_doorhanger_save_password.js
+++ toolkit/components/passwordmgr/test/browser/browser_doorhanger_save_password.js
@@ -2,149 +2,159 @@
  * Test that the doorhanger notification for password saving is populated with
  * the correct values in various password capture cases.
  */
-add_task(async function test_save_change() {
-  let testCases = [
-    {
-      username: "username",
-      password: "password",
-      expectOutcome: [
-        {
-          username: "username",
-          password: "password",
-        },
-      ],
-    },
-    {
-      username: "",
-      password: "password",
-      expectOutcome: [
-        {
-          username: "",
-          password: "password",
-        },
-      ],
-    },
-    {
-      username: "username",
-      oldPassword: "password",
-      password: "newPassword",
-      expectOutcome: [
-        {
-          username: "username",
-          password: "newPassword",
-        },
-      ],
-    },
-    {
-      username: "",
-      oldPassword: "password",
-      password: "newPassword",
-      expectOutcome: [
-        {
-          username: "",
-          password: "newPassword",
-        },
-      ],
-    },
-    {
-      oldUsername: "",
-      username: "username",
-      oldPassword: "password",
-      password: "newPassword",
-      expectOutcome: [
-        {
-          username: "",
-          password: "password",
-        },
-        {
-          username: "username",
-          password: "newPassword",
-        },
-      ],
-    },
-  ];
 
-  for (let {
+const testCases = [
+  {
+    name: "No saved logins, username and password",
+    username: "username",
+    password: "password",
+    expectOutcome: [
+      {
+        username: "username",
+        password: "password",
+      },
+    ],
+  },
+  {
+    name: "No saved logins, password with empty username",
+    username: "",
+    password: "password",
+    expectOutcome: [
+      {
+        username: "",
+        password: "password",
+      },
+    ],
+  },
+  {
+    name: "Saved login with username, update password",
+    username: "username",
+    oldPassword: "password",
+    password: "newPassword",
+    expectOutcome: [
+      {
+        username: "username",
+        password: "newPassword",
+      },
+    ],
+  },
+  {
+    name: "Saved login with no username, update password",
+    username: "",
+    oldPassword: "password",
+    password: "newPassword",
+    expectOutcome: [
+      {
+        username: "",
+        password: "newPassword",
+      },
+    ],
+  },
+  {
+    name: "Saved login with no username, add username and different password",
+    oldUsername: "",
+    username: "username",
+    oldPassword: "password",
+    password: "newPassword",
+    expectOutcome: [
+      {
+        username: "",
+        password: "password",
+      },
+      {
+        username: "username",
+        password: "newPassword",
+      },
+    ],
+  },
+];
+
+for (let testData of testCases) {
+  let tmp = {
+    async [testData.name]() {
+      info("testing with: " + JSON.stringify(testData));
+      await test_save_change(testData);
+    },
+  };
+  add_task(tmp[testData.name]);
+}
+
+async function test_save_change(testData) {
+  let {
     oldUsername,
     username,
     oldPassword,
     password,
     expectOutcome,
-  } of testCases) {
-    // Add a login for the origin of the form if testing a change notification.
-    if (oldPassword) {
-      Services.logins.addLogin(
-        LoginTestUtils.testData.formLogin({
-          origin: "https://example.com",
-          formActionOrigin: "https://example.com",
-          username: typeof oldUsername !== "undefined" ? oldUsername : username,
-          password: oldPassword,
-        })
-      );
-    }
-
-    await BrowserTestUtils.withNewTab(
-      {
-        gBrowser,
-        url:
-          "https://example.com/browser/toolkit/components/" +
-          "passwordmgr/test/browser/form_basic.html",
-      },
-      async function(browser) {
-        // Submit the form in the content page with the credentials from the test
-        // case. This will cause the doorhanger notification to be displayed.
-        await SpecialPowers.spawn(
-          browser,
-          [[username, password]],
-          async function([contentUsername, contentPassword]) {
-            let doc = content.document;
-            doc
-              .getElementById("form-basic-username")
-              .setUserInput(contentUsername);
-            doc
-              .getElementById("form-basic-password")
-              .setUserInput(contentPassword);
-            doc.getElementById("form-basic").submit();
-          }
-        );
-
-        // Simulate the action on the notification to request the login to be
-        // saved, and wait for the data to be updated or saved based on the type
-        // of operation we expect.
-        let expectedNotification, expectedDoorhanger;
-        if (oldPassword !== undefined && oldUsername !== undefined) {
-          expectedNotification = "addLogin";
-          expectedDoorhanger = "password-save";
-        } else if (oldPassword !== undefined) {
-          expectedNotification = "modifyLogin";
-          expectedDoorhanger = "password-change";
-        } else {
-          expectedNotification = "addLogin";
-          expectedDoorhanger = "password-save";
-        }
-
-        let notif = getCaptureDoorhanger(
-          expectedDoorhanger,
-          PopupNotifications,
-          browser
-        );
-        // Check the actual content of the popup notification.
-        await checkDoorhangerUsernamePassword(username, password);
-
-        let promiseLogin = TestUtils.topicObserved(
-          "passwordmgr-storage-changed",
-          (_, data) => data == expectedNotification
-        );
-        await clickDoorhangerButton(notif, REMEMBER_BUTTON);
-        await promiseLogin;
-        await cleanupDoorhanger(notif); // clean slate for the next test
-
-        // Check that the values in the database match the expected values.
-        verifyLogins(expectOutcome);
-      }
+  } = testData;
+  // Add a login for the origin of the form if testing a change notification.
+  if (oldPassword) {
+    Services.logins.addLogin(
+      LoginTestUtils.testData.formLogin({
+        origin: "https://example.com",
+        formActionOrigin: "https://example.com",
+        username: typeof oldUsername !== "undefined" ? oldUsername : username,
+        password: oldPassword,
+      })
     );
-
-    // Clean up the database before the next test case is executed.
-    Services.logins.removeAllLogins();
   }
-});
+
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url:
+        "https://example.com/browser/toolkit/components/" +
+        "passwordmgr/test/browser/form_basic.html",
+    },
+    async function(browser) {
+      await SimpleTest.promiseFocus(browser.ownerGlobal);
+
+      // Submit the form in the content page with the credentials from the test
+      // case. This will cause the doorhanger notification to be displayed.
+      info(`update form with username: ${username}, password: ${password}`);
+      await changeContentFormValues(browser, {
+        "#form-basic-username": username,
+        "#form-basic-password": password,
+      });
+      await SpecialPowers.spawn(browser, [], async function() {
+        let doc = this.content.document;
+        doc.getElementById("form-basic").submit();
+      });
+
+      // Simulate the action on the notification to request the login to be
+      // saved, and wait for the data to be updated or saved based on the type
+      // of operation we expect.
+      let expectedNotification, expectedDoorhanger;
+      if (oldPassword !== undefined && oldUsername !== undefined) {
+        expectedNotification = "addLogin";
+        expectedDoorhanger = "password-save";
+      } else if (oldPassword !== undefined) {
+        expectedNotification = "modifyLogin";
+        expectedDoorhanger = "password-change";
+      } else {
+        expectedNotification = "addLogin";
+        expectedDoorhanger = "password-save";
+      }
+
+      info("Waiting for doorhanger of type: " + expectedDoorhanger);
+      let notif = await waitForDoorhanger(browser, expectedDoorhanger);
+
+      // Check the actual content of the popup notification.
+      await checkDoorhangerUsernamePassword(username, password);
+
+      let promiseLogin = TestUtils.topicObserved(
+        "passwordmgr-storage-changed",
+        (_, data) => data == expectedNotification
+      );
+      await clickDoorhangerButton(notif, REMEMBER_BUTTON);
+      await promiseLogin;
+      await cleanupDoorhanger(notif); // clean slate for the next test
+
+      // Check that the values in the database match the expected values.
+      verifyLogins(expectOutcome);
+    }
+  );
+
+  // Clean up the database before the next test case is executed.
+  Services.logins.removeAllLogins();
+}
