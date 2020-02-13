641647b05d58e815726e377ef8c59d1bb1169e26
Lina Cambridge <lina@yakshaving.ninja>
2020-02-13 18:54:23 +0000

    Bug 1607341 - Ensure the bookmarks mirror passes millisecond timestamps for `bookmark-added`. r=LougeniaBailey
    The legacy `nsINavBookmarkObserver` notifications use microsecond
    timestamps that are rounded to the nearest millisecond, while the
    new `bookmark-added` Places event uses milliseconds directly. This
    commit fixes that, and also changes the store to use the given
    `localTimeSeconds` as the current time, instead of querying the
    current system time. This, in turn, lets tests set deterministic
    last modified times, which is useful for comparing `lastModified`
    timestamps.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62675

--- toolkit/components/places/bookmark_sync/src/store.rs
+++ toolkit/components/places/bookmark_sync/src/store.rs
@@ -429,7 +429,13 @@ impl<'s> dogear::Store for Store<'s> {
         }
 
         debug!(self.driver, "Updating local items in Places");
-        update_local_items_in_places(&tx, &self.driver, &self.controller, &ops)?;
+        update_local_items_in_places(
+            &tx,
+            &self.driver,
+            &self.controller,
+            self.local_time_millis,
+            &ops,
+        )?;
 
         debug!(self.driver, "Staging items to upload");
         stage_items_to_upload(
@@ -464,6 +470,7 @@ fn update_local_items_in_places<'t>(
     db: &Conn,
     driver: &Driver,
     controller: &AbortController,
+    local_time_millis: i64,
     ops: &CompletionOps<'t>,
 ) -> Result<()> {
     debug!(
@@ -479,7 +486,10 @@ fn update_local_items_in_places<'t>(
          DELETE FROM itemsMoved;",
     )?;
 
-    let now = rounded_now();
+    // Places uses microsecond timestamps for dates added and last modified
+    // times, rounded to the nearest millisecond. Using `now` for the local
+    // time lets us set modified times deterministically for tests.
+    let now = local_time_millis * 1000;
 
     // Insert URLs for new remote items into the `moz_places` table. We need to
     // do this before inserting new remote items, since we need Place IDs for
@@ -1255,15 +1265,6 @@ impl fmt::Display for UploadItemsFragment {
             self.0
         )
     }
-}
-
-/// Returns the current time, in microseconds, rounded to the nearest
-/// millisecond.
-fn rounded_now() -> u64 {
-    SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .map(|d| (d.as_secs() as u64) * 1_000_000 + u64::from(d.subsec_millis()) * 1000)
-        .unwrap_or(0)
 }
 
 pub enum ApplyStatus {
