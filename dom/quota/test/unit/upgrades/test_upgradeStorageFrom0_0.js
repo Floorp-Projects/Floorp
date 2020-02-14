30e2651bceabc531c5ddf19f551ffc612c642aea
Jan Varga <jan.varga@gmail.com>
2020-02-14 09:23:24 +0000

    Bug 1608759 - Add "beforeInstall" key to package definitions; r=ttung,dom-workers-and-storage-reviewers
    This is a preparation for "afterInitTemporaryStorage" key which requires initialized storage.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62055

--- dom/quota/test/unit/upgrades/test_upgradeStorageFrom0_0.js
+++ dom/quota/test/unit/upgrades/test_upgradeStorageFrom0_0.js
@@ -45,6 +45,10 @@ function* testSteps() {
   clear(continueToNextStepSync);
   yield undefined;
 
+  info("Verifying storage");
+
+  verifyStorage(packages, "beforeInstall");
+
   info("Installing packages");
 
   installPackages(packages);
--- dom/quota/test/unit/upgrades/test_upgradeStorageFrom2_0.js
+++ dom/quota/test/unit/upgrades/test_upgradeStorageFrom2_0.js
@@ -41,6 +41,10 @@ function* testSteps() {
   clear(continueToNextStepSync);
   yield undefined;
 
+  info("Verifying storage");
+
+  verifyStorage(packages, "beforeInstall");
+
   info("Installing packages");
 
   installPackages(packages);
