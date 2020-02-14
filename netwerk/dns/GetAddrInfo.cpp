54459c444c74ab7ce8eeea0f01630e0578c95de7
Valentin Gosu <valentin.gosu@gmail.com>
2020-02-14 11:40:57 +0000

    Bug 1615058 - Do not load library for DnsQuery_A at runtime r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D62652

--- netwerk/dns/GetAddrInfo.cpp
+++ netwerk/dns/GetAddrInfo.cpp
@@ -9,10 +9,6 @@
 #include "prnetdb.h"
 #include "nsHostResolver.h"
 #include "nsError.h"
-#include "mozilla/Mutex.h"
-#include "mozilla/StaticPtr.h"
-#include "MainThreadUtils.h"
-#include "mozilla/DebugOnly.h"
 #include "mozilla/net/DNS.h"
 #include <algorithm>
 #include "prerror.h"
@@ -50,109 +46,17 @@ static_assert(PR_AF_INET == AF_INET && PR_AF_INET6 == AF_INET6 &&
                   PR_AF_UNSPEC == AF_UNSPEC,
               "PR_AF_* must match AF_*");
 
-// We intentionally leak this mutex. This is because we can run into a
-// situation where the worker threads are still running until the process
-// is actually fully shut down, and at any time one of those worker
-// threads can access gDnsapiInfoLock.
-static OffTheBooksMutex* gDnsapiInfoLock = nullptr;
-
-struct DnsapiInfo {
- public:
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(DnsapiInfo);
-
-  HMODULE mLibrary;
-  decltype(&DnsQuery_A) mDnsQueryFunc;
-  decltype(&DnsFree) mDnsFreeFunc;
-
- private:
-  // This will either be called during shutdown of the GetAddrInfo module, or
-  // when a worker thread is done doing a lookup (ie: within
-  // _GetAddrInfo_Windows). Note that the lock must be held when this is
-  // called.
-  ~DnsapiInfo() {
-    if (gDnsapiInfoLock) {
-      gDnsapiInfoLock->AssertCurrentThreadOwns();
-    } else {
-      MOZ_ASSERT_UNREACHABLE(
-          "No mutex available during GetAddrInfo "
-          "shutdown.");
-      return;
-    }
-
-    LOG("Freeing Dnsapi.dll");
-    MOZ_ASSERT(mLibrary);
-    DebugOnly<BOOL> rv = FreeLibrary(mLibrary);
-    NS_WARNING_ASSERTION(rv, "Failed to free Dnsapi.dll.");
-  }
-};
-
-static StaticRefPtr<DnsapiInfo> gDnsapiInfo;
-
-static MOZ_ALWAYS_INLINE nsresult _GetAddrInfoInit_Windows() {
-  // This is necessary to ensure strict thread safety because if two threads
-  // run this function at the same time they can potentially create two
-  // mutexes.
-  MOZ_ASSERT(NS_IsMainThread(),
-             "Do not initialize GetAddrInfo off main thread!");
-
-  if (!gDnsapiInfoLock) {
-    gDnsapiInfoLock = new OffTheBooksMutex("GetAddrInfo.cpp::gDnsapiInfoLock");
-  }
-  OffTheBooksMutexAutoLock lock(*gDnsapiInfoLock);
-
-  if (gDnsapiInfo) {
-    MOZ_ASSERT_UNREACHABLE("GetAddrInfo is being initialized multiple times!");
-    return NS_ERROR_ALREADY_INITIALIZED;
-  }
-
-  HMODULE library = LoadLibraryA("Dnsapi.dll");
-  if (NS_WARN_IF(!library)) {
-    return NS_ERROR_FAILURE;
-  }
-
-  FARPROC dnsQueryFunc = GetProcAddress(library, "DnsQuery_A");
-  FARPROC dnsFreeFunc = GetProcAddress(library, "DnsFree");
-  if (NS_WARN_IF(!dnsQueryFunc) || NS_WARN_IF(!dnsFreeFunc)) {
-    DebugOnly<BOOL> rv = FreeLibrary(library);
-    NS_WARNING_ASSERTION(rv, "Failed to free Dnsapi.dll.");
-    return NS_ERROR_FAILURE;
-  }
-
-  DnsapiInfo* info = new DnsapiInfo;
-  info->mLibrary = library;
-  info->mDnsQueryFunc = (decltype(info->mDnsQueryFunc))dnsQueryFunc;
-  info->mDnsFreeFunc = (decltype(info->mDnsFreeFunc))dnsFreeFunc;
-  gDnsapiInfo = info;
-
-  return NS_OK;
-}
-
-static MOZ_ALWAYS_INLINE nsresult _GetAddrInfoShutdown_Windows() {
-  OffTheBooksMutexAutoLock lock(*gDnsapiInfoLock);
-
-  if (NS_WARN_IF(!gDnsapiInfo) || NS_WARN_IF(!gDnsapiInfoLock)) {
-    MOZ_ASSERT_UNREACHABLE("GetAddrInfo not initialized!");
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
-  gDnsapiInfo = nullptr;
-
-  return NS_OK;
-}
-
 // If successful, returns in aResult a TTL value that is smaller or
 // equal with the one already there. Gets the TTL value by calling
-// to dnsapi->mDnsQueryFunc and iterating through the returned
+// to DnsQuery_A and iterating through the returned
 // records to find the one with the smallest TTL value.
-static MOZ_ALWAYS_INLINE nsresult
-_GetMinTTLForRequestType_Windows(DnsapiInfo* dnsapi, const char* aHost,
-                                 uint16_t aRequestType, unsigned int* aResult) {
-  MOZ_ASSERT(dnsapi);
+static MOZ_ALWAYS_INLINE nsresult _GetMinTTLForRequestType_Windows(
+    const char* aHost, uint16_t aRequestType, unsigned int* aResult) {
   MOZ_ASSERT(aHost);
   MOZ_ASSERT(aResult);
 
   PDNS_RECORDA dnsData = nullptr;
-  DNS_STATUS status = dnsapi->mDnsQueryFunc(
+  DNS_STATUS status = DnsQuery_A(
       aHost, aRequestType,
       (DNS_QUERY_STANDARD | DNS_QUERY_NO_NETBT | DNS_QUERY_NO_HOSTS_FILE |
        DNS_QUERY_NO_MULTICAST | DNS_QUERY_ACCEPT_TRUNCATED_RESPONSE |
@@ -183,7 +87,7 @@ _GetMinTTLForRequestType_Windows(DnsapiInfo* dnsapi, const char* aHost,
     }
   }
 
-  dnsapi->mDnsFreeFunc(dnsData, DNS_FREE_TYPE::DnsFreeRecordList);
+  DnsFree(dnsData, DNS_FREE_TYPE::DnsFreeRecordList);
   return NS_OK;
 }
 
@@ -197,34 +101,15 @@ static MOZ_ALWAYS_INLINE nsresult _GetTTLData_Windows(const nsACString& aHost,
     return NS_ERROR_UNEXPECTED;
   }
 
-  RefPtr<DnsapiInfo> dnsapi = nullptr;
-  {
-    OffTheBooksMutexAutoLock lock(*gDnsapiInfoLock);
-    dnsapi = gDnsapiInfo;
-  }
-
-  if (!dnsapi) {
-    LOG_WARNING("GetAddrInfo has been shutdown or has not been initialized.");
-    return NS_ERROR_NOT_INITIALIZED;
-  }
-
   // In order to avoid using ANY records which are not always implemented as a
   // "Gimme what you have" request in hostname resolvers, we should send A
   // and/or AAAA requests, based on the address family requested.
   unsigned int ttl = (unsigned int)-1;
   if (aAddressFamily == PR_AF_UNSPEC || aAddressFamily == PR_AF_INET) {
-    _GetMinTTLForRequestType_Windows(dnsapi, aHost.BeginReading(), DNS_TYPE_A,
-                                     &ttl);
+    _GetMinTTLForRequestType_Windows(aHost.BeginReading(), DNS_TYPE_A, &ttl);
   }
   if (aAddressFamily == PR_AF_UNSPEC || aAddressFamily == PR_AF_INET6) {
-    _GetMinTTLForRequestType_Windows(dnsapi, aHost.BeginReading(),
-                                     DNS_TYPE_AAAA, &ttl);
-  }
-
-  {
-    // dnsapi's destructor is not thread-safe, so we release explicitly here
-    OffTheBooksMutexAutoLock lock(*gDnsapiInfoLock);
-    dnsapi = nullptr;
+    _GetMinTTLForRequestType_Windows(aHost.BeginReading(), DNS_TYPE_AAAA, &ttl);
   }
 
   if (ttl == (unsigned int)-1) {
@@ -293,22 +178,12 @@ _GetAddrInfo_Portable(const nsACString& aCanonHost, uint16_t aAddressFamily,
 //////////////////////////////////////
 nsresult GetAddrInfoInit() {
   LOG("Initializing GetAddrInfo.\n");
-
-#ifdef DNSQUERY_AVAILABLE
-  return _GetAddrInfoInit_Windows();
-#else
   return NS_OK;
-#endif
 }
 
 nsresult GetAddrInfoShutdown() {
   LOG("Shutting down GetAddrInfo.\n");
-
-#ifdef DNSQUERY_AVAILABLE
-  return _GetAddrInfoShutdown_Windows();
-#else
   return NS_OK;
-#endif
 }
 
 nsresult GetAddrInfo(const nsACString& aHost, uint16_t aAddressFamily,
