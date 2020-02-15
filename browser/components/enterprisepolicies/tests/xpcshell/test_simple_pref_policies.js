0e6e704cc362943b1420499e6ef52efdbf0d17e8
Michael Kaply <mozilla@kaply.com>
2020-02-15 18:49:49 +0000

    Bug 1612352 - Allow domains to be excluded from DOH via policy. r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D62996

--- browser/components/enterprisepolicies/tests/xpcshell/test_simple_pref_policies.js
+++ browser/components/enterprisepolicies/tests/xpcshell/test_simple_pref_policies.js
@@ -427,12 +427,14 @@ const POLICIES_TESTS = [
       DNSOverHTTPS: {
         Enabled: true,
         ProviderURL: "http://example.com/provider",
+        ExcludedDomains: ["example.com", "example.org"],
         Locked: true,
       },
     },
     lockedPrefs: {
       "network.trr.mode": 2,
       "network.trr.uri": "http://example.com/provider",
+      "network.trr.excluded-domains": "example.com,example.org",
     },
   },
 
@@ -442,11 +444,13 @@ const POLICIES_TESTS = [
       DNSOverHTTPS: {
         Enabled: false,
         ProviderURL: "http://example.com/provider",
+        ExcludedDomains: ["example.com", "example.org"],
       },
     },
     unlockedPrefs: {
       "network.trr.mode": 5,
       "network.trr.uri": "http://example.com/provider",
+      "network.trr.excluded-domains": "example.com,example.org",
     },
   },
 
