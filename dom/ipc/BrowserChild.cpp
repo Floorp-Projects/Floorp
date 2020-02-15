7a1225282574f33f0157b66dd22b42c0745e164b
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-02-15 03:17:43 +0200

    Backed out changeset 93c88e36d8ea (bug 1615609) for causing bustages in DOMIntersectionObserver.cpp
    CLOSED TREE

--- dom/ipc/BrowserChild.cpp
+++ dom/ipc/BrowserChild.cpp
@@ -1300,18 +1300,17 @@ mozilla::ipc::IPCResult BrowserChild::RecvSizeModeChanged(
 }
 
 mozilla::ipc::IPCResult BrowserChild::RecvChildToParentMatrix(
-    const Maybe<gfx::Matrix4x4>& aMatrix,
-    const ScreenRect& aTopLevelViewportVisibleRectInBrowserCoords) {
+    const mozilla::Maybe<mozilla::gfx::Matrix4x4>& aMatrix,
+    const mozilla::ScreenRect& aTopLevelViewportVisibleRectInBrowserCoords) {
   mChildToParentConversionMatrix =
       LayoutDeviceToLayoutDeviceMatrix4x4::FromUnknownMatrix(aMatrix);
   mTopLevelViewportVisibleRectInBrowserCoords =
       aTopLevelViewportVisibleRectInBrowserCoords;
 
-  // Trigger an intersection observation update since ancestor viewports
-  // changed.
+  // Triger an intersection observation since ancestor viewports changed.
   if (RefPtr<Document> toplevelDoc = GetTopLevelDocument()) {
-    if (nsPresContext* pc = toplevelDoc->GetPresContext()) {
-      pc->RefreshDriver()->EnsureIntersectionObservationsUpdateHappens();
+    if (nsPresContext* presContext = toplevelDoc->GetPresContext()) {
+      presContext->RefreshDriver()->IntersectionObservationAdded();
     }
   }
 
