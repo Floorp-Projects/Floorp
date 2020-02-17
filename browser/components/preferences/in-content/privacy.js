f51e7fd16b59bc469d4a57b0c5deca415f80c02a
Christoph Walcher <christoph-wa@gmx.de>
2020-02-17 12:05:35 +0000

    Bug 1596486 - Remove XBL workaround for setting checkbox disabled state in privacy.js r=bgrins
    Differential Revision: https://phabricator.services.mozilla.com/D61085

--- browser/components/preferences/in-content/privacy.js
+++ browser/components/preferences/in-content/privacy.js
@@ -222,17 +222,8 @@ function dataCollectionCheckboxHandler({
       checkbox.removeAttribute("checked");
     }
 
-    // We can't use checkbox.disabled here because the XBL binding may not be present,
-    // in which case setting the property won't work properly.
-    if (
-      !collectionEnabled ||
-      Services.prefs.prefIsLocked(pref) ||
-      isDisabled()
-    ) {
-      checkbox.setAttribute("disabled", "true");
-    } else {
-      checkbox.removeAttribute("disabled");
-    }
+    checkbox.disabled =
+      !collectionEnabled || Services.prefs.prefIsLocked(pref) || isDisabled();
   }
 
   Preferences.get(PREF_UPLOAD_ENABLED).on("change", updateCheckbox);
