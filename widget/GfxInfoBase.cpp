d893aa1dd5c634caa469827117446c4ec9fa1e71
Andrew Osmond <aosmond@mozilla.com>
2020-02-18 13:56:10 +0000

    Bug 1614377 - Switch the WebRender rollout to use allowlist rules. r=jrmuizel
    Now that GfxInfo supports allowlisting, we can port our existing
    configuration in gfxPlatform to using allowlist rules. This will
    greatly increase maintainability and certainty that the expected
    devices are getting WebRender.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62325

--- widget/GfxInfoBase.cpp
+++ widget/GfxInfoBase.cpp
@@ -1122,7 +1122,9 @@ bool GfxInfoBase::DoesDriverVendorMatch(const nsAString& aBlocklistVendor,
              nsCaseInsensitiveStringComparator());
 }
 
-bool GfxInfoBase::IsFeatureAllowlisted(int32_t aFeature) const { return false; }
+bool GfxInfoBase::IsFeatureAllowlisted(int32_t aFeature) const {
+  return aFeature == nsIGfxInfo::FEATURE_WEBRENDER;
+}
 
 nsresult GfxInfoBase::GetFeatureStatusImpl(
     int32_t aFeature, int32_t* aStatus, nsAString& aSuggestedVersion,
