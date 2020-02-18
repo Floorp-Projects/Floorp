206d3d85d10fe24e84a9b38096e7061d41328727
Brian Grinstead <bgrinstead@mozilla.com>
2020-02-18 23:27:30 +0000

    Bug 1616179 - Fix `JavaScript error: chrome://browser/content/sanitizeDialog.js, line 70: TypeError: right-hand side of 'in' should be an object, got null` error when sanitize dialog is opened outside of preferences;r=Gijs
    This error has been present since the initial landing in Bug 1523696.
    
    This is unrelated to the fix in the next patch, just something I noticed when debugging it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D63233

--- browser/base/content/sanitizeDialog.js
+++ browser/base/content/sanitizeDialog.js
@@ -67,7 +67,7 @@ var gSanitizePromptDialog = {
     }
 
     // Only apply the following if the dialog is opened outside of the Preferences.
-    if (!("gSubDialog" in window.opener)) {
+    if (!window.opener || !("gSubDialog" in window.opener)) {
       // The style attribute on the dialog may get set after the dialog has been sized.
       // Force the dialog to size again after the style attribute has been applied.
       document.l10n.translateElements([document.documentElement]).then(() => {
