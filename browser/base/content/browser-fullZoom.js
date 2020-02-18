44a391230161f46e51fac9a8c5b1202cf7d91215
Morgan Reschenberg <mreschenberg@mozilla.com>
2020-02-18 16:16:02 +0000

    Bug 1603494: Allow global zoom values to be read from CPS when site specific zoom is disabled. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D62830

--- browser/base/content/browser-fullZoom.js
+++ browser/base/content/browser-fullZoom.js
@@ -417,7 +417,7 @@ var FullZoom = {
     let token = this._getBrowserToken(browser);
     let result = ZoomUI.getGlobalValue().then(value => {
       if (token.isCurrent) {
-        ZoomManager.setZoomForBrowser(browser, value === undefined ? 1 : value);
+        ZoomManager.setZoomForBrowser(browser, value);
         this._ignorePendingZoomAccesses(browser);
       }
     });
@@ -453,7 +453,7 @@ var FullZoom = {
     aBrowser,
     aCallback
   ) {
-    if (!this.siteSpecific || gInPrintPreviewMode) {
+    if (gInPrintPreviewMode) {
       this._executeSoon(aCallback);
       return;
     }
@@ -476,10 +476,7 @@ var FullZoom = {
     let token = this._getBrowserToken(aBrowser);
     ZoomUI.getGlobalValue().then(value => {
       if (token.isCurrent) {
-        ZoomManager.setZoomForBrowser(
-          aBrowser,
-          value === undefined ? 1 : value
-        );
+        ZoomManager.setZoomForBrowser(aBrowser, value);
         this._ignorePendingZoomAccesses(aBrowser);
       }
       this._executeSoon(aCallback);
