330b580f32d07bc8d36f2a2fc77908fd7682bccf
Bob Clary <bclary@bclary.com>
2020-02-18 14:12:31 +0000

    Bug 1615280  - [mozdevice] ADBDevice.get_top_activity() should use dumpsys window on Android 10, r=gbrown,snorp.
    Differential Revision: https://phabricator.services.mozilla.com/D62975
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozbase/mozdevice/mozdevice/adb.py
+++ testing/mozbase/mozdevice/mozdevice/adb.py
@@ -2990,13 +2990,21 @@ class ADBDevice(ADBCommand):
         :raises: * ADBTimeoutError
                  * ADBError
         """
+        if self.version < version_codes.Q:
+            return self._get_top_activity_P(timeout=timeout)
+        return self._get_top_activity_Q(timeout=timeout)
+
+    def _get_top_activity_P(self, timeout=None):
+        """Returns the name of the top activity (focused app) reported by dumpsys
+        for Android 9 and earlier.
+        """
         package = None
         data = None
         cmd = "dumpsys window windows"
         try:
             data = self.shell_output(cmd, timeout=timeout)
         except Exception:
-            # dumpsys intermittently fails on some platforms (4.3 arm emulator)
+            # dumpsys intermittently fails on some platforms.
             return package
         m = re.search('mFocusedApp(.+)/', data)
         if not m:
@@ -3006,6 +3014,30 @@ class ADBDevice(ADBCommand):
             line = m.group(0)
             # Extract package name: string of non-whitespace ending in forward slash
             m = re.search('(\S+)/$', line)
+            if m:
+                package = m.group(1)
+        if self._verbose:
+            self._logger.debug('get_top_activity: %s' % str(package))
+        return package
+
+    def _get_top_activity_Q(self, timeout=None):
+        """Returns the name of the top activity (focused app) reported by dumpsys
+        for Android 10 and later.
+        """
+        package = None
+        data = None
+        cmd = "dumpsys window"
+        try:
+            data = self.shell_output(cmd, timeout=timeout)
+        except Exception:
+            # dumpsys intermittently fails on some platforms (4.3 arm emulator)
+            return package
+        m = re.search('mFocusedApp=AppWindowToken{\w+ token=Token{'
+                      '\w+ ActivityRecord{\w+ w+ (\w+)/w+ \w+}}}', data)
+        if m:
+            line = m.group(1)
+            # Extract package name: string of non-whitespace ending in forward slash
+            m = re.search('(\S+)/', line)
             if m:
                 package = m.group(1)
         if self._verbose:
