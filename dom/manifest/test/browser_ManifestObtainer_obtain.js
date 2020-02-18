986567db565583f725f33a2848b79be99f9dc3df
shindli <shindli@mozilla.com>
2020-02-18 05:39:05 +0200

    Backed out changeset 061d47c2c5c7 (bug 1603673) for mozlint failure in /builds/worker/checkouts/gecko/mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java CLOSED TREE

--- dom/manifest/test/browser_ManifestObtainer_obtain.js
+++ dom/manifest/test/browser_ManifestObtainer_obtain.js
@@ -2,8 +2,6 @@
 /* global ok, is, Cu, BrowserTestUtils, add_task, gBrowser, requestLongerTimeout*/
 "use strict";
 
-Services.prefs.setBoolPref("dom.manifest.enabled", true);
-
 const { ManifestObtainer } = ChromeUtils.import(
   "resource://gre/modules/ManifestObtainer.jsm"
 );
