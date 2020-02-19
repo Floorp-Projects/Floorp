c1f5e4a94ae6d9f82cab840b5c3f2f09a2421088
Neil Deakin <neil@mozilla.com>
2020-02-19 22:28:36 +0000

    Bug 1598305, replace frame script in test browser_TelemetryGC.js with SpecialPowers.spawn, r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D61910

--- toolkit/components/telemetry/tests/browser/browser_TelemetryGC.js
+++ toolkit/components/telemetry/tests/browser/browser_TelemetryGC.js
@@ -129,50 +129,23 @@ add_task(async function test() {
     set: [["javascript.options.mem.notify", true]],
   });
 
-  function runRemote(f) {
-    gBrowser.selectedBrowser.messageManager.loadFrameScript(
-      `data:,(${f})()`,
-      false
-    );
-  }
-
-  // These are available to frame scripts.
-  /* global addMessageListener:false, removeMessageListener: false */
-  function initScript() {
-    const { GCTelemetry } = ChromeUtils.import(
-      "resource://gre/modules/GCTelemetry.jsm"
-    );
-
-    /*
-     * Don't shut down GC telemetry if it was already running before the test!
-     * Note: We need to use a multiline comment here since this code is turned into a data: URI.
-     */
-    let shutdown = GCTelemetry.init();
-
-    function listener() {
-      removeMessageListener("GCTelemTest:Shutdown", listener);
-      if (shutdown) {
-        GCTelemetry.shutdown();
-      }
-    }
-    addMessageListener("GCTelemTest:Shutdown", listener);
-  }
-
   if (multiprocess) {
-    runRemote(initScript);
+    await SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
+      const { GCTelemetry } = ChromeUtils.import(
+        "resource://gre/modules/GCTelemetry.jsm"
+      );
+
+      /*
+       * Don't shut down GC telemetry if it was already running before the test!
+       * Note: We need to use a multiline comment here since this code is turned into a data: URI.
+       */
+      content.shutdown = GCTelemetry.init();
+      content.GCTelemetry = GCTelemetry;
+    });
   }
 
   // Don't shut down GC telemetry if it was already running before the test!
   let shutdown = GCTelemetry.init();
-  registerCleanupFunction(() => {
-    if (shutdown) {
-      GCTelemetry.shutdown();
-    }
-
-    gBrowser.selectedBrowser.messageManager.sendAsyncMessage(
-      "GCTelemTest:Shutdown"
-    );
-  });
 
   let localPromise = new Promise(resolve => {
     function obs() {
@@ -198,7 +171,9 @@ add_task(async function test() {
   // Make sure we have a GC to work with in both processes.
   Cu.forceGC();
   if (multiprocess) {
-    runRemote(() => Cu.forceGC());
+    await SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
+      Cu.forceGC();
+    });
   }
 
   info("Waiting for GCs");
@@ -223,4 +198,16 @@ add_task(async function test() {
 
   is(remoteEntries.random.length, 0, "no random GCs after reset");
   is(remoteEntries.worst.length, 0, "no worst GCs after reset");
+
+  if (shutdown) {
+    GCTelemetry.shutdown();
+  }
+
+  if (multiprocess) {
+    await SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
+      if (content.shutdown) {
+        content.GCTelemetry.shutdown();
+      }
+    });
+  }
 });
