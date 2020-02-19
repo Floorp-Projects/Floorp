468082fdd6251c4e7688b315198f258607d2990f
Boris Chiou <boris.chiou@gmail.com>
2020-02-19 22:01:13 +0000

    Bug 1616544 - Mark CSSPseudoElement as a disabled interface. r=smaug
    We test this only in wpt, and test_interfaces.js should treat
    it as a disabled interface because its preference is default off on all
    channels.
    
    Differential Revision: https://phabricator.services.mozilla.com/D63364

--- dom/tests/mochitest/general/test_interfaces.js
+++ dom/tests/mochitest/general/test_interfaces.js
@@ -269,7 +269,7 @@ var interfaceNamesInGlobalScope = [
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSPageRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
-  { name: "CSSPseudoElement", insecureContext: true, release: false },
+  { name: "CSSPseudoElement", insecureContext: true, disabled: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
   { name: "CSSRule", insecureContext: true },
   // IMPORTANT: Do not change this list without review from a DOM peer!
