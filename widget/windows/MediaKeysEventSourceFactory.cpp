ec427fb6cca942669e479830e95e927f80c83b41
Marc Streckfuss <marc.streckfuss@gmail.com>
2020-02-19 22:13:28 +0000

    Bug 1584501: Draft Implementation of the Windows SystemMediaTransportControl WinRT Interface r=alwu
    Differential Revision: https://phabricator.services.mozilla.com/D47546

--- widget/windows/MediaKeysEventSourceFactory.cpp
+++ widget/windows/MediaKeysEventSourceFactory.cpp
@@ -3,13 +3,13 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "MediaKeysEventSourceFactory.h"
+#include "WindowsSMTCProvider.h"
 
 namespace mozilla {
 namespace widget {
 
 mozilla::dom::MediaControlKeysEventSource* CreateMediaControlKeysEventSource() {
-  // TODO : will implement this in bug 1584501.
-  return nullptr;
+  return new WindowsSMTCProvider();
 }
 
 }  // namespace widget
