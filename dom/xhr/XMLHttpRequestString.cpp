3f36ef12b2b93508a07ce1d7c14a23b26761425b
Simon Giesecke <sgiesecke@mozilla.com>
2020-02-20 15:59:01 +0000

    Bug 1613985 - Use default for equivalent-to-default constructors/destructors in dom/xhr. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D63174
    
    --HG--
    extra : moz-landing-system : lando

--- dom/xhr/XMLHttpRequestString.cpp
+++ dom/xhr/XMLHttpRequestString.cpp
@@ -77,7 +77,7 @@ class XMLHttpRequestStringBuffer final {
   }
 
  private:
-  ~XMLHttpRequestStringBuffer() {}
+  ~XMLHttpRequestStringBuffer() = default;
 
   nsString& UnsafeData() { return mData; }
 
@@ -93,7 +93,7 @@ class XMLHttpRequestStringBuffer final {
 XMLHttpRequestString::XMLHttpRequestString()
     : mBuffer(new XMLHttpRequestStringBuffer()) {}
 
-XMLHttpRequestString::~XMLHttpRequestString() {}
+XMLHttpRequestString::~XMLHttpRequestString() = default;
 
 void XMLHttpRequestString::Truncate() {
   mBuffer = new XMLHttpRequestStringBuffer();
@@ -126,15 +126,10 @@ void XMLHttpRequestString::CreateSnapshot(
 XMLHttpRequestStringSnapshot::XMLHttpRequestStringSnapshot()
     : mLength(0), mVoid(false) {}
 
-XMLHttpRequestStringSnapshot::~XMLHttpRequestStringSnapshot() {}
+XMLHttpRequestStringSnapshot::~XMLHttpRequestStringSnapshot() = default;
 
 XMLHttpRequestStringSnapshot& XMLHttpRequestStringSnapshot::operator=(
-    const XMLHttpRequestStringSnapshot& aOther) {
-  mBuffer = aOther.mBuffer;
-  mLength = aOther.mLength;
-  mVoid = aOther.mVoid;
-  return *this;
-}
+    const XMLHttpRequestStringSnapshot& aOther) = default;
 
 void XMLHttpRequestStringSnapshot::ResetInternal(bool aIsVoid) {
   mBuffer = nullptr;
