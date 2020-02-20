0d486cce16e997d802e1f284b79eb75bb4c8c103
Nicholas Nethercote <nnethercote@mozilla.com>
2020-02-20 20:57:32 +0000

    Bug 1604964 - Add fix-stacks toolchain. r=erahm
    Differential Revision: https://phabricator.services.mozilla.com/D57747
    
    --HG--
    extra : moz-landing-system : lando

---
+++ taskcluster/scripts/misc/build-fix-stacks.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -x -e -v
+
+# This script is for building fix-stacks
+PROJECT=fix-stacks
+
+case "$(uname -s)" in
+Linux)
+    COMPRESS_EXT=xz
+    export CC=$MOZ_FETCHES_DIR/clang/bin/clang
+    export CXX=$MOZ_FETCHES_DIR/clang/bin/clang++
+    export PATH="$MOZ_FETCHES_DIR/binutils/bin:$PATH"
+    export RUSTFLAGS="-C linker=$CXX"
+    ;;
+MINGW*)
+    UPLOAD_DIR=$PWD/public/build
+    COMPRESS_EXT=bz2
+
+    . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
+    ;;
+esac
+
+cd $GECKO_PATH
+
+if [ -n "$TOOLTOOL_MANIFEST" ]; then
+  . taskcluster/scripts/misc/tooltool-download.sh
+fi
+
+# cargo gets mad if the parent directory has a Cargo.toml file in it
+if [ -e Cargo.toml ]; then
+  mv Cargo.toml Cargo.toml.back
+fi
+
+PATH="$(cd $MOZ_FETCHES_DIR && pwd)/rustc/bin:$PATH"
+
+cd $MOZ_FETCHES_DIR/$PROJECT
+
+cargo build --verbose --release
+
+mkdir $PROJECT
+cp target/release/${PROJECT}* ${PROJECT}/
+tar -acf ${PROJECT}.tar.$COMPRESS_EXT $PROJECT
+mkdir -p $UPLOAD_DIR
+cp ${PROJECT}.tar.$COMPRESS_EXT $UPLOAD_DIR
+
+cd ..
+if [ -e Cargo.toml.back ]; then
+  mv Cargo.toml.back Cargo.toml
+fi
+
+. $GECKO_PATH/taskcluster/scripts/misc/vs-cleanup.sh
