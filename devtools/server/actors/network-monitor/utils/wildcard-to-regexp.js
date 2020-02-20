2db22c93fd9581d883d24a62d41617bf3bed901d
Duncan Dean <duncangleeddean@gmail.com>
2020-02-20 09:26:56 +0000

    Bug 1613988 - Allow wildcards for blocking URLs. r=Honza,Harald
    Differential Revision: https://phabricator.services.mozilla.com/D63020
    
    --HG--
    extra : moz-landing-system : lando

---
+++ devtools/server/actors/network-monitor/utils/wildcard-to-regexp.js
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/**
+ * Create a RegExp from a given wildcard string.
+ */
+function wildcardToRegExp(s) {
+  return new RegExp(
+    s
+      .split("*")
+      .map(regExpEscape)
+      .join(".*"),
+    "i"
+  );
+}
+exports.wildcardToRegExp = wildcardToRegExp;
+
+/**
+ * Escapes all special RegExp characters in the given string.
+ */
+const regExpEscape = s => {
+  return s.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
+};
