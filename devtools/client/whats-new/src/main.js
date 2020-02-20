04cb9a86bf739f308c485fff381845ffbd694367
Harald Kirschner <hkirschner@mozilla.com>
2020-02-20 19:00:17 +0000

    Bug 1615030 - Update DevTools What's New content for 74/73 r=jlast,victoria
    Differential Revision: https://phabricator.services.mozilla.com/D63420
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/whats-new/src/main.js
+++ devtools/client/whats-new/src/main.js
@@ -29,39 +29,44 @@ const aside = {
 };
 
 const release = {
-  title: "What’s New in DevTools (Firefox 72 & 71)",
+  title: "What’s New in DevTools (Firefox 74 & 73)",
   linkText: "Read more",
-  linkUrl: `https://developer.mozilla.org/docs/Mozilla/Firefox/Releases/72?${utmParams}`,
+  linkUrl: `https://developer.mozilla.org/docs/Mozilla/Firefox/Releases/74?${utmParams}`,
   features: [
     {
-      header: `Debug Variables with Watchpoints`,
-      description: `Debugger’s new Watchpoint feature lets you pause when properties get read or write. Right-click object properties in the Scopes pane when paused to use the new “Break on…” menu.`,
-      href: `https://wiki.developer.mozilla.org/docs/Tools/Debugger/How_to/Set_a_watchpoint_on_a_property?${utmParams}`,
+      header: `Debugging Async/Await Stacks`,
+      description: `Awaiting call stacks in the Debugger allow you to examine the promise-driven function calls at previous points in time.`,
+      href: `https://developer.mozilla.org/en-US/docs/Tools/Debugger/UI_Tour?${utmParams}`,
     },
     {
-      header: `Improvements to VS Code’s Debugger for Firefox`,
-      description: `A new source map integration makes debugging faster and also integrates with VSCode’s new column breakpoints UI for fine-grained control. The new Watchpoints can be used via VSCode’s Data Points.`,
-      href: `https://marketplace.visualstudio.com/items?itemName=firefox-devtools.vscode-firefox-debug`,
+      header: `Optional chaining in Console`,
+      description: `Freshly landed support for the optional chaining operator "?." which can also be used with Console's autocomplete.`,
+      href: `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining?${utmParams}`,
     },
     {
-      header: `Async Stacks for Console traces`,
-      description: `Stack Traces in the Console include the full chain of asynchronous promises and callbacks over time, including timeouts and events.`,
-      href: `https://developer.mozilla.org/en-US/docs/Tools/Web_Console/Console_messages?${utmParams}`,
+      header: `New in 73: More WebSocket protocols`,
+      description: `WAMP-encoded data is now decoded into a readable payload (on top of SignalR, Socket.IO, etc) to make it easier to debug various WebSocket message formats.`,
+      href: `https://developer.mozilla.org/en-US/docs/Tools/Network_Monitor/Inspecting_web_sockets?${utmParams}#Supported_WS_protocols`,
     },
     {
-      header: `New in 71: New WebSocket Messages Inspector`,
-      description: `The Network panel has a new Messages tab for inspecting WebSocket frames sent and received through the selected connection.`,
-      href: `https://hacks.mozilla.org/2019/10/firefoxs-new-websocket-inspector/?${utmParams}`,
+      header: `New in 73: CORS Network Errors`,
+      description: `To make CORS network errors harder to miss, Console now shows them as errors and not warnings.`,
+      href: `https://wiki.developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors?${utmParams}`,
     },
     {
-      header: `New in 71: Block URLs in the Network panel`,
-      description: `Test how a page loads without specific files, like CSS or JavaScript. Right-click network requests and select “Block URL” or use the new Request Blocking pane.`,
-      href: `https://developer.mozilla.org/docs/Tools/Network_Monitor/request_list?${utmParams}#Block_a_specific_URL`,
+      header: `New in 73: Performance Highlights`,
+      description: `We made fast-firing requests in Network panel and large JavaScript source maps in Debugger a lot snappier.`,
+      href: `https://hacks.mozilla.org/2020/02/firefox-73-is-upon-us/?${utmParams}`,
     },
     {
-      header: `New in 71: Log on Events`,
-      description: `Enabling logging for Event Listener Breakpoints in the Debugger lets you observe which event handlers are being executed without the overhead of pausing.`,
-      href: `https://developer.mozilla.org/docs/Tools/Debugger/Set_event_listener_breakpoints?${utmParams}`,
+      header: `New in 73: CSP Style Directives No Longer Prevent CSS Edits`,
+      description: `Editing an element’s inline style from Inspector’s rule-view now works even if style-src is blocked by CSP.`,
+      href: `https://developer.mozilla.org/en-US/docs/Tools/Page_Inspector/How_to/Examine_and_edit_CSS?${utmParams}`,
+    },
+    {
+      header: `New in 72: Correct Meta Viewport in Responsive Design Mode`,
+      description: `RDM with touch simulation enabled will now correctly simulate the meta viewport rendering of a mobile device.`,
+      href: `https://developer.mozilla.org/docs/Tools/Responsive_Design_Mode?${utmParams}`,
     },
   ],
 };
@@ -72,19 +77,19 @@ const dev = {
   linkText: "Get DevEdition",
   features: [
     {
-      header: `Asynchronous Stacks in Debugger`,
-      description: `Asynchronous call stacks in the Debugger let you examine the event-driven function calls at previous points in time.`,
+      header: `Full Asynchronous Stacks in Debugger`,
+      description: `Step through event, timeout and promise-based function calls over time with the full-featured async stacks in Debugger.`,
       href: `https://developer.mozilla.org/en-US/docs/Tools/Debugger/UI_Tour?${utmParams}`,
     },
     {
-      header: `Correct Meta Viewport in Responsive Design Mode`,
-      description: `RDM with touch simulation enabled will now correctly simulate the meta viewport rendering of a mobile device.`,
-      href: `https://developer.mozilla.org/docs/Tools/Responsive_Design_Mode?${utmParams}`,
+      header: `Inspect & Debug Service Workers`,
+      description: `Start, pause and debug your Service Workers and inspect your Web App Manifests within the long-awaited Application panel.`,
+      href: `https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers?${utmParams}`,
     },
     {
-      header: `CSP Style Directives No Longer Prevent CSS Edits`,
-      description: `Editing an element’s inline style from Inspector’s rule-view now works even if style-src is blocked by CSP.`,
-      href: `https://developer.mozilla.org/en-US/docs/Tools/Page_Inspector/How_to/Examine_and_edit_CSS?${utmParams}`,
+      header: `Instantly evaluated Console previews`,
+      description: `Peek into the results of your current Console expression and autocomplete values as you type.`,
+      href: `https://developer.mozilla.org/en-US/docs/Tools/Web_Console/The_command_line_interpreter?${utmParams}`,
     },
   ],
 };
