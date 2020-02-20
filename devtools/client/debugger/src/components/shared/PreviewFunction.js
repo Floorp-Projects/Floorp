98c2481096351cf897cdadf4cd6e69b37bed0e5e
Stepan Stava <obdelnik@gmail.com>
2020-02-20 23:32:32 +0000

    Bug 1601533 - Indicate that function references in tooltips can be clicked to jump r=jlast
    Added 'jumpToDefinitionButton' to the function preview with a title containing target source + location.
    The button is not rendered for ignored source urls similarly like in the console.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62591

--- devtools/client/debugger/src/components/shared/PreviewFunction.js
+++ devtools/client/debugger/src/components/shared/PreviewFunction.js
@@ -18,9 +18,16 @@ type FunctionType = {
   displayName?: string,
   userDisplayName?: string,
   parameterNames?: string[],
+  location?: {
+    url: string,
+    line: number,
+    column: number,
+  },
 };
 
 type Props = { func: FunctionType };
+
+const IGNORED_SOURCE_URLS = ["debugger eval code"];
 
 export default class PreviewFunction extends Component<Props> {
   renderFunctionName(func: FunctionType) {
@@ -49,6 +56,26 @@ export default class PreviewFunction extends Component<Props> {
     return flatten(zip(params, commas));
   }
 
+  jumpToDefinitionButton(func: FunctionType) {
+    const { location } = func;
+
+    if (
+      location &&
+      location.url &&
+      !IGNORED_SOURCE_URLS.includes(location.url)
+    ) {
+      const lastIndex = location.url.lastIndexOf("/");
+
+      return (
+        <button
+          className="jump-definition"
+          draggable="false"
+          title={`${location.url.slice(lastIndex + 1)}:${location.line}`}
+        />
+      );
+    }
+  }
+
   render() {
     const { func } = this.props;
     return (
@@ -57,6 +84,7 @@ export default class PreviewFunction extends Component<Props> {
         <span className="paren">(</span>
         {this.renderParams(func)}
         <span className="paren">)</span>
+        {this.jumpToDefinitionButton(func)}
       </span>
     );
   }
