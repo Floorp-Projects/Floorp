8486885f18b0c50000c49fe77d79523432bfd9e3
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-02-21 19:36:30 +0200

    Backed out changeset 1494b28cea80 (bug 1581886) for causing failures in browser_ext_themes_getCurrent_differentExt.js
    CLOSED TREE

--- toolkit/components/extensions/parent/ext-theme.js
+++ toolkit/components/extensions/parent/ext-theme.js
@@ -22,7 +22,7 @@ const onUpdatedEmitter = new EventEmitter();
 
 // Represents an empty theme for convenience of use
 const emptyTheme = {
-  details: { colors: null, images: null, properties: null },
+  details: {},
 };
 
 let defaultTheme = emptyTheme;
