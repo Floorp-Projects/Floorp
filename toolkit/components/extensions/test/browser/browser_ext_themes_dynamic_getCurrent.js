2641e31eb299043f6292792c369f0a2812c30afb
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-02-21 19:36:30 +0200

    Backed out changeset 1494b28cea80 (bug 1581886) for causing failures in browser_ext_themes_getCurrent_differentExt.js
    CLOSED TREE

--- toolkit/components/extensions/test/browser/browser_ext_themes_dynamic_getCurrent.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_dynamic_getCurrent.js
@@ -98,8 +98,8 @@ add_task(async function test_get_current() {
 
       function testEmptyTheme(returnedTheme) {
         browser.test.assertEq(
-          JSON.stringify({ colors: null, images: null, properties: null }),
-          JSON.stringify(returnedTheme),
+          0,
+          Object.keys(returnedTheme).length,
           JSON.stringify(returnedTheme, null, 2)
         );
       }
