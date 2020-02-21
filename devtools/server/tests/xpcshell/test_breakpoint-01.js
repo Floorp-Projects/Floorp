beae9d3f95ed587909802bcdc76f1bce59fdaecb
chujun <chujunlu@hotmail.com>
2020-02-21 12:06:12 +0000

    Bug 1613636 - Edit test_breakpoint-01.js and disable test_breakpoint-02.js r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D61987

--- devtools/server/tests/xpcshell/test_breakpoint-01.js
+++ devtools/server/tests/xpcshell/test_breakpoint-01.js
@@ -10,46 +10,47 @@
 
 add_task(
   threadFrontTest(async ({ threadFront, debuggee }) => {
-    (async () => {
-      info("Wait for the debugger statement to be hit");
-      let packet = await waitForPause(threadFront);
-      const source = await getSourceById(threadFront, packet.frame.where.actor);
+    info("Wait for the debugger statement to be hit");
+    const packet1 = await executeOnNextTickAndWaitForPause(
+      () => evaluateTestCode(debuggee),
+      threadFront
+    );
+    const source = await getSourceById(threadFront, packet1.frame.where.actor);
+    const location = { sourceUrl: source.url, line: debuggee.line0 + 3 };
 
-      const location = { sourceUrl: source.url, line: debuggee.line0 + 3 };
+    threadFront.setBreakpoint(location, {});
 
-      threadFront.setBreakpoint(location, {});
+    await threadFront.resume();
+    const packet2 = await waitForPause(threadFront);
 
-      await threadFront.resume();
-      packet = await waitForPause(threadFront);
+    info("Paused at the breakpoint");
+    Assert.equal(packet2.frame.where.actor, source.actor);
+    Assert.equal(packet2.frame.where.line, location.line);
+    Assert.equal(packet2.why.type, "breakpoint");
 
-      info("Paused at the breakpoint");
-      Assert.equal(packet.type, "paused");
-      Assert.equal(packet.frame.where.actor, source.actor);
-      Assert.equal(packet.frame.where.line, location.line);
-      Assert.equal(packet.why.type, "breakpoint");
+    info("Check that the breakpoint worked.");
+    Assert.equal(debuggee.a, 1);
+    Assert.equal(debuggee.b, undefined);
 
-      info("Check that the breakpoint worked.");
-      Assert.equal(debuggee.a, 1);
-      Assert.equal(debuggee.b, undefined);
-
-      await threadFront.resume();
-    })();
-
-    /*
-     * Be sure to run debuggee code in its own HTML 'task', so that when we call
-     * the onDebuggerStatement hook, the test's own microtasks don't get suspended
-     * along with the debuggee's.
-     */
-    do_timeout(0, () => {
-      /* eslint-disable */
-      Cu.evalInSandbox(
-        "var line0 = Error().lineNumber;\n" +
-          "debugger;\n" +   // line0 + 1
-          "var a = 1;\n" +  // line0 + 2
-          "var b = 2;\n",   // line0 + 3
-          debuggee
-      );
-      /* eslint-enable */
-    });
+    await threadFront.resume();
   })
 );
+
+function evaluateTestCode(debuggee) {
+  /*
+   * Be sure to run debuggee code in its own HTML 'task', so that when we call
+   * the onDebuggerStatement hook, the test's own microtasks don't get suspended
+   * along with the debuggee's.
+   */
+  do_timeout(0, () => {
+    /* eslint-disable */
+    Cu.evalInSandbox(
+      "var line0 = Error().lineNumber;\n" +
+      "debugger;\n" + // line0 + 1
+      "var a = 1;\n" + // line0 + 2
+      "var b = 2;\n", // line0 + 3
+      debuggee
+    );
+    /* eslint-enable */
+  });
+}
