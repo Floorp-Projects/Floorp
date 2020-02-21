e7d890391425078c1ccd0f152d6772f4bc11c738
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-02-21 23:23:35 +0000

    Bug 1614958 - Disable VP9 codec in MSE on Android device with no VP9 hardware decoder. r=jolin
    We remove the old behaviour that if webm was disabled it would be overridden under some circumstances.
    Instead we replace if with a new specialised preference (media.mediasource.vp9.enabled) that is only disabled on Android.
    
    If this pref is disabled, vp9 will only be enabled under some conditions:
     - h264 HW decoding is not supported
     - mp4 is not enabled
     - Device was deemed fast enough to decode VP9 via the P9Benchmark utility
     - On Android, A VP9 HW decoder is present.
    
    The primary observable result is that YouTube will serve H264 content on devices with no hardware VP9 decoder
    
    Differential Revision: https://phabricator.services.mozilla.com/D63042

--- dom/media/mediasource/MediaSource.cpp
+++ dom/media/mediasource/MediaSource.cpp
@@ -7,9 +7,9 @@
 #include "MediaSource.h"
 
 #include "AsyncEventRunner.h"
-#include "DecoderTraits.h"
 #include "Benchmark.h"
 #include "DecoderDoctorDiagnostics.h"
+#include "DecoderTraits.h"
 #include "MediaContainerType.h"
 #include "MediaResult.h"
 #include "MediaSourceDemuxer.h"
@@ -18,22 +18,22 @@
 #include "SourceBufferList.h"
 #include "mozilla/ErrorResult.h"
 #include "mozilla/FloatingPoint.h"
+#include "mozilla/Logging.h"
+#include "mozilla/Sprintf.h"
 #include "mozilla/StaticPrefs_media.h"
 #include "mozilla/dom/BindingDeclarations.h"
 #include "mozilla/dom/HTMLMediaElement.h"
+#include "mozilla/gfx/gfxVars.h"
 #include "mozilla/mozalloc.h"
 #include "nsDebug.h"
 #include "nsError.h"
 #include "nsIRunnable.h"
 #include "nsIScriptObjectPrincipal.h"
-#include "nsPIDOMWindow.h"
 #include "nsMimeTypes.h"
+#include "nsPIDOMWindow.h"
+#include "nsServiceManagerUtils.h"
 #include "nsString.h"
 #include "nsThreadUtils.h"
-#include "mozilla/Logging.h"
-#include "nsServiceManagerUtils.h"
-#include "mozilla/gfx/gfxVars.h"
-#include "mozilla/Sprintf.h"
 
 #ifdef MOZ_WIDGET_ANDROID
 #  include "AndroidBridge.h"
@@ -73,7 +73,7 @@ namespace mozilla {
 //     optional "Windows Media Feature Pack"
 // 2. If H264 hardware acceleration is not available.
 // 3. The CPU is considered to be fast enough
-static bool IsWebMForced(DecoderDoctorDiagnostics* aDiagnostics) {
+static bool IsVP9Forced(DecoderDoctorDiagnostics* aDiagnostics) {
   bool mp4supported = DecoderTraits::IsMP4SupportedType(
       MediaContainerType(MEDIAMIMETYPE(VIDEO_MP4)), aDiagnostics);
   bool hwsupported = gfx::gfxVars::CanUseHardwareVideoDecoding();
@@ -105,6 +105,15 @@ void MediaSource::IsTypeSupported(const nsAString& aType,
     return aRv.ThrowNotSupportedError("Can't play type");
   }
 
+  bool hasVP9 = false;
+  const MediaCodecs& codecs = containerType->ExtendedType().Codecs();
+  for (const auto& codec : codecs.Range()) {
+    if (IsVP9CodecString(codec)) {
+      hasVP9 = true;
+      break;
+    }
+  }
+
   // Now we know that this media type could be played.
   // MediaSource imposes extra restrictions, and some prefs.
   const MediaMIMEType& mimeType = containerType->Type();
@@ -115,19 +124,23 @@ void MediaSource::IsTypeSupported(const nsAString& aType,
       // like we can't play it.  Or should this throw "Unknown type"?
       return aRv.ThrowNotSupportedError("Can't play type");
     }
+    if (!StaticPrefs::media_mediasource_vp9_enabled() && hasVP9 &&
+        !IsVP9Forced(aDiagnostics)) {
+      // Don't leak information about the fact that it's pref-disabled; just act
+      // like we can't play it.  Or should this throw "Unknown type"?
+      return aRv.ThrowNotSupportedError("Can't play type");
+    }
+
     return;
   }
   if (mimeType == MEDIAMIMETYPE("video/webm")) {
-    if (!(StaticPrefs::media_mediasource_webm_enabled() ||
-          StaticPrefs::media_media_capabilities_enabled() ||
-          containerType->ExtendedType().Codecs().Contains(
-              NS_LITERAL_STRING("vp8")) ||
-#ifdef MOZ_AV1
-          (StaticPrefs::media_av1_enabled() &&
-           IsAV1CodecString(
-               containerType->ExtendedType().Codecs().AsString())) ||
-#endif
-          IsWebMForced(aDiagnostics))) {
+    if (!StaticPrefs::media_mediasource_webm_enabled()) {
+      // Don't leak information about the fact that it's pref-disabled; just act
+      // like we can't play it.  Or should this throw "Unknown type"?
+      return aRv.ThrowNotSupportedError("Can't play type");
+    }
+    if (!StaticPrefs::media_mediasource_vp9_enabled() && hasVP9 &&
+        !IsVP9Forced(aDiagnostics)) {
       // Don't leak information about the fact that it's pref-disabled; just act
       // like we can't play it.  Or should this throw "Unknown type"?
       return aRv.ThrowNotSupportedError("Can't play type");
