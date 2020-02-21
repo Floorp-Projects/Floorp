2b8efe7270e2bea2e0d85a3578742df8c0c26721
Andrew Osmond <aosmond@mozilla.com>
2020-02-21 13:16:35 +0000

    Bug 1617148 - Re-enable WebRender for Intel Haswell on Linux nightly. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D63635

--- widget/GfxDriverInfo.cpp
+++ widget/GfxDriverInfo.cpp
@@ -526,7 +526,7 @@ const GfxDeviceFamily* GfxDriverInfo::GetDeviceFamily(DeviceFamily id) {
       APPEND_DEVICE(0x163d);
       APPEND_DEVICE(0x163e);
 
-#if 0
+#ifdef MOZ_WIDGET_GTK
       // Gen7.5 not allowed until bug 1576637 is resolved.
       APPEND_DEVICE(0x0412);
       APPEND_DEVICE(0x0416);
