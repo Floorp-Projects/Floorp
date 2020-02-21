712321729ef120a06da316d656f4dee9caf32a4e
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-02-21 16:45:33 +0000

    Bug 1616902 - [try] Move tools/tryselect/preview.py to the 'selectors' directory, r=sfraser
    Differential Revision: https://phabricator.services.mozilla.com/D63676

--- tools/tryselect/selectors/fuzzy.py
+++ tools/tryselect/selectors/fuzzy.py
@@ -26,7 +26,7 @@ terminal = Terminal()
 here = os.path.abspath(os.path.dirname(__file__))
 build = MozbuildObject.from_environment(cwd=here)
 
-PREVIEW_SCRIPT = os.path.join(build.topsrcdir, 'tools/tryselect/preview.py')
+PREVIEW_SCRIPT = os.path.join(build.topsrcdir, 'tools/tryselect/selectors/preview.py')
 
 # Some tasks show up in the target task set, but are either special cases
 # or uncommon enough that they should only be selectable with --full.
