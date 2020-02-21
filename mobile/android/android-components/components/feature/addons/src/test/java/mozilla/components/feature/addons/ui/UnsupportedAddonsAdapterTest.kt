edc26c403a544ebda8777bf2c81b3289a48be4ab
Simon Chae <chaesmn@gmail.com>
2020-02-21 13:56:26 -0500

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/6025: Disable remove actions while uninstalling in UnsupportedAddonsAdapter

--- mobile/android/android-components/components/feature/addons/src/test/java/mozilla/components/feature/addons/ui/UnsupportedAddonsAdapterTest.kt
+++ mobile/android/android-components/components/feature/addons/src/test/java/mozilla/components/feature/addons/ui/UnsupportedAddonsAdapterTest.kt
@@ -4,6 +4,7 @@
 
 package mozilla.components.feature.addons.amo.mozilla.components.feature.addons.ui
 
+import android.widget.ImageButton
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -11,9 +12,15 @@ import mozilla.components.feature.addons.Addon
 import mozilla.components.feature.addons.AddonManager
 import mozilla.components.feature.addons.ui.UnsupportedAddonsAdapter
 import mozilla.components.feature.addons.ui.UnsupportedAddonsAdapterDelegate
+import mozilla.components.support.test.any
+import mozilla.components.support.test.argumentCaptor
 import mozilla.components.support.test.mock
+import mozilla.components.support.test.robolectric.testContext
 import mozilla.components.support.test.rule.MainCoroutineRule
+import mozilla.components.support.test.whenever
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,7 +41,9 @@ class UnsupportedAddonsAdapterTest {
     fun `removing successfully notifies the adapter item changed`() {
         val addonManager: AddonManager = mock()
         val unsupportedAddonsAdapterDelegate: UnsupportedAddonsAdapterDelegate = mock()
-        val unsupportedAddons = listOf(Addon("id1"), Addon("id2"))
+        val addonOne = Addon("id1")
+        val addonTwo = Addon("id2")
+        val unsupportedAddons = listOf(addonOne, addonTwo)
 
         val adapter = spy(
             UnsupportedAddonsAdapter(
@@ -44,21 +53,80 @@ class UnsupportedAddonsAdapterTest {
             )
         )
 
-        adapter.removeUninstalledAddonAtPosition(0)
+        adapter.removeUninstalledAddon(addonOne)
         testDispatcher.advanceUntilIdle()
         verify(unsupportedAddonsAdapterDelegate, times(1)).onUninstallSuccess()
-        verify(adapter, times(1)).notifyItemRemoved(0)
+        verify(adapter, times(1)).notifyDataSetChanged()
         assertEquals(1, adapter.itemCount)
 
-        adapter.removeUninstalledAddonAtPosition(0)
+        adapter.removeUninstalledAddon(addonTwo)
         testDispatcher.advanceUntilIdle()
         verify(unsupportedAddonsAdapterDelegate, times(2)).onUninstallSuccess()
-        verify(adapter, times(2)).notifyItemRemoved(0)
+        verify(adapter, times(2)).notifyDataSetChanged()
         assertEquals(0, adapter.itemCount)
 
-        adapter.removeUninstalledAddonAtPosition(0)
+        adapter.removeUninstalledAddon(addonTwo)
         testDispatcher.advanceUntilIdle()
         verify(unsupportedAddonsAdapterDelegate, times(2)).onUninstallSuccess()
-        verify(adapter, times(2)).notifyItemRemoved(0)
+        verify(adapter, times(2)).notifyDataSetChanged()
+    }
+
+    @Test
+    fun `uninstalling action disables all remove buttons`() {
+        val removeButtonOne = ImageButton(testContext)
+        val unsupportedViewHolderOne = UnsupportedAddonsAdapter.UnsupportedAddonViewHolder(
+            view = mock(),
+            iconView = mock(),
+            titleView = mock(),
+            removeButton = removeButtonOne
+        )
+        val removeButtonTwo = ImageButton(testContext)
+        val unsupportedViewHolderTwo = UnsupportedAddonsAdapter.UnsupportedAddonViewHolder(
+                view = mock(),
+                iconView = mock(),
+                titleView = mock(),
+                removeButton = removeButtonTwo
+        )
+        val addonManager: AddonManager = mock()
+        val addonOne = Addon("id1")
+        val addonTwo = Addon("id2")
+        val unsupportedAddons = mapOf(
+            unsupportedViewHolderOne to addonOne,
+            unsupportedViewHolderTwo to addonTwo
+        )
+        val adapter = spy(
+            UnsupportedAddonsAdapter(
+                addonManager,
+                mock(),
+                unsupportedAddons.values.toList()
+            )
+        )
+
+        // mock the adapter.notifyDataSetChanged() behavior
+        whenever(adapter.notifyDataSetChanged()).thenAnswer {
+            unsupportedAddons.forEach { addonEntry ->
+                val addonPair = addonEntry.toPair()
+                adapter.bindRemoveButton(addonPair.first, addonPair.second)
+            }
+        }
+
+        val onSuccessCaptor = argumentCaptor<(() -> Unit)>()
+        adapter.bindRemoveButton(unsupportedViewHolderOne, addonOne)
+        assertFalse(adapter.pendingUninstall)
+        removeButtonOne.performClick()
+        assertTrue(adapter.pendingUninstall)
+        verify(adapter, times(1)).notifyDataSetChanged()
+        // All the visible remove buttons in the adapter should be disabled
+        assertFalse(removeButtonOne.isEnabled)
+        assertFalse(removeButtonTwo.isEnabled)
+
+        testDispatcher.advanceUntilIdle()
+        verify(addonManager).uninstallAddon(any(), onSuccessCaptor.capture(), any())
+        onSuccessCaptor.value.invoke()
+        assertFalse(adapter.pendingUninstall)
+        verify(adapter, times(2)).notifyDataSetChanged()
+        // All the visible remove buttons in the adapter should be enabled after uninstall complete
+        assertTrue(removeButtonOne.isEnabled)
+        assertTrue(removeButtonTwo.isEnabled)
     }
 }
