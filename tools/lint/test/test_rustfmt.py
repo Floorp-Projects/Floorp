cc5611e5778c5a9b22705eeb5d71eb02cc4c0461
Sylvestre Ledru <sledru@mozilla.com>
2020-02-21 18:28:49 +0000

    Bug 1615903 - Add unit tests for rustfmt r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D63028

---
+++ tools/lint/test/test_rustfmt.py
@@ -0,0 +1,44 @@
+import mozunit
+
+
+LINTER = 'rustfmt'
+
+
+def test_good(lint, config, paths):
+    results = lint(paths("subdir/good.rs"))
+    print(results)
+    assert len(results) == 0
+
+
+def test_basic(lint, config, paths):
+    results = lint(paths("subdir/bad.rs"))
+    print(results)
+    assert len(results) >= 1
+
+    assert "Reformat rust" in results[0].message
+    assert results[0].level == "warning"
+    assert results[0].lineno == 4
+    assert "bad.rs" in results[0].path
+    assert "Print text to the console" in results[0].diff
+
+
+def test_dir(lint, config, paths):
+    results = lint(paths("subdir/"))
+    print(results)
+    assert len(results) >= 4
+
+    assert "Reformat rust" in results[0].message
+    assert results[0].level == "warning"
+    assert results[0].lineno == 4
+    assert "bad.rs" in results[0].path
+    assert "Print text to the console" in results[0].diff
+
+    assert "Reformat rust" in results[1].message
+    assert results[1].level == "warning"
+    assert results[1].lineno == 4
+    assert "bad2.rs" in results[1].path
+    assert "Print text to the console" in results[1].diff
+
+
+if __name__ == '__main__':
+    mozunit.main()
