1ece026f406bc1db9ef4ebb8e7dc34c38acd5235
Csoregi Natalia <ncsoregi@mozilla.com>
2020-02-23 12:26:50 +0200

    Backed out changeset aa296c08a667 (bug 1597336) for browser-chrome failures on browser_all_files_referenced.js. CLOSED TREE

--- testing/condprofile/condprof/customization/__init__.py
+++ testing/condprofile/condprof/customization/__init__.py
@@ -12,21 +12,14 @@ def get_customizations():
         yield os.path.join(HERE, f)
 
 
-def find_customization(path_or_name):
-    if not path_or_name.endswith(".json"):
-        path_or_name += ".json"
-    if not os.path.exists(path_or_name):
+def get_customization(path):
+    if not path.endswith(".json"):
+        path += ".json"
+    if not os.path.exists(path):
         # trying relative
-        rpath = os.path.join(HERE, path_or_name)
+        rpath = os.path.join(HERE, path)
         if not os.path.exists(rpath):
-            return None
-        path_or_name = rpath
-    return path_or_name
-
-
-def get_customization(path_or_name):
-    path = find_customization(path_or_name)
-    if path is None:
-        raise IOError("Can't find the customization file %r" % path_or_name)
+            raise IOError("Can't find the customization file %r" % path)
+        path = rpath
     with open(path) as f:
         return json.loads(f.read())
