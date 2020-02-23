736fe0c60cd8a7d94d4a3289ef5ee64651ac401c
Tarek Ziadé <tarek@mozilla.com>
2020-02-23 07:48:19 +0000

    Bug 1597336 - add a fxa+sync scenario r=lina,markh
    This patch adds a scenario that ends up syncing the generated profile into FxSync.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53789

---
+++ services/automation/.eslintrc.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "no-unused-vars": ["error", {"args": "none", "varsIgnorePattern": "^EXPORTED_SYMBOLS|^Sync"}],
+  }
+};
--- testing/condprofile/condprof/scenarii/__init__.py
+++ testing/condprofile/condprof/scenarii/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from condprof.scenarii.heavy import heavy
 from condprof.scenarii.settled import settled
+from condprof.scenarii.synced import synced
 
 
-scenarii = {"heavy": heavy, "settled": settled}
+scenarii = {"heavy": heavy, "settled": settled, "synced": synced}
