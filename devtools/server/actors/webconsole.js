39193cdb5c519e55995bf1bd94bdaa376c65a567
Surya Shankar <Surya Shankar>
2020-02-24 10:43:50 +0000

    Bug 1584087 - Rename console-progress to mention it is about file activity. r=nchevobbe
    renamed the console-progress.js/ConsoleProgressListener file and class to
    console-file-activity.js/ConsoleFileActivityListener.
    
    Differential Revision: https://phabricator.services.mozilla.com/D63672

--- devtools/server/actors/webconsole.js
+++ devtools/server/actors/webconsole.js
@@ -37,8 +37,8 @@ loader.lazyRequireGetter(
 );
 loader.lazyRequireGetter(
   this,
-  "ConsoleProgressListener",
-  "devtools/server/actors/webconsole/listeners/console-progress",
+  "ConsoleFileActivityListener",
+  "devtools/server/actors/webconsole/listeners/console-file-activity",
   true
 );
 loader.lazyRequireGetter(
@@ -387,9 +387,9 @@ const WebConsoleActor = ActorClassWithSpec(webconsoleSpec, {
   consoleAPIListener: null,
 
   /**
-   * The ConsoleProgressListener instance.
+   * The ConsoleFileActivityListener instance.
    */
-  consoleProgressListener: null,
+  consoleFileActivityListener: null,
 
   /**
    * The ConsoleReflowListener instance.
@@ -796,13 +796,13 @@ const WebConsoleActor = ActorClassWithSpec(webconsoleSpec, {
             break;
           }
           if (this.window instanceof Ci.nsIDOMWindow) {
-            if (!this.consoleProgressListener) {
-              this.consoleProgressListener = new ConsoleProgressListener(
+            if (!this.consoleFileActivityListener) {
+              this.consoleFileActivityListener = new ConsoleFileActivityListener(
                 this.window,
                 this
               );
             }
-            this.consoleProgressListener.startMonitor();
+            this.consoleFileActivityListener.startMonitor();
             startedListeners.push(event);
           }
           break;
@@ -908,9 +908,9 @@ const WebConsoleActor = ActorClassWithSpec(webconsoleSpec, {
           stoppedListeners.push(event);
           break;
         case "FileActivity":
-          if (this.consoleProgressListener) {
-            this.consoleProgressListener.stopMonitor();
-            this.consoleProgressListener = null;
+          if (this.consoleFileActivityListener) {
+            this.consoleFileActivityListener.stopMonitor();
+            this.consoleFileActivityListener = null;
           }
           stoppedListeners.push(event);
           break;
@@ -2028,7 +2028,7 @@ const WebConsoleActor = ActorClassWithSpec(webconsoleSpec, {
    * Handler for file activity. This method sends the file request information
    * to the remote Web Console client.
    *
-   * @see ConsoleProgressListener
+   * @see ConsoleFileActivityListener
    * @param string fileURI
    *        The requested file URI.
    */
