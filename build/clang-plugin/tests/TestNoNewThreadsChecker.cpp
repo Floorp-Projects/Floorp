54e998ac668939a837d66f0bab20c36046b69098
Dorel Luca <dluca@mozilla.com>
2020-02-24 21:56:51 +0200

    Backed out changeset 459a77fff516 (bug 1613440) for Clang bustage on tools/extra/clang-tidy/mozilla/Utils.h. CLOSED TREE

--- build/clang-plugin/tests/TestNoNewThreadsChecker.cpp
+++
@@ -1,9 +0,0 @@
-// Dummy NS_NewNamedThread.
-void NS_NewNamedThread(const char *aName) {}
-
-void func_threads() {
-  // Test to see if the checker recognizes a bad name, and if it recognizes a
-  // name from the ThreadAllows.txt.
-  NS_NewNamedThread("A bad name"); // expected-error {{Thread name not recognized. Please use the background thread pool.}} expected-note {{NS_NewNamedThread has been deprecated in favor of background task dispatch via NS_DispatchBackgroundTask and NS_CreateBackgroundTaskQueue. If you must create a new ad-hoc thread, have your thread name added to ThreadAllows.txt.}}
-  NS_NewNamedThread("Checker Test");
-}
