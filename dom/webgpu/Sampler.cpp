1e1226ada3b2a8bf025bb102f7d6f8f5ea3c27a8
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-02-24 20:12:16 +0000

    Bug 1614702 - WebGPU textures, texture views, and samplers r=jgilbert,webidl,smaug
    this change adds an ability to create WebGPU textures, views, and samplers
    
    Differential Revision: https://phabricator.services.mozilla.com/D63595

--- dom/webgpu/Sampler.cpp
+++ dom/webgpu/Sampler.cpp
@@ -14,7 +14,20 @@ namespace webgpu {
 GPU_IMPL_CYCLE_COLLECTION(Sampler, mParent)
 GPU_IMPL_JS_WRAP(Sampler)
 
-Sampler::~Sampler() = default;
+Sampler::Sampler(Device* const aParent, RawId aId)
+    : ChildOf(aParent), mId(aId) {}
+
+Sampler::~Sampler() { Cleanup(); }
+
+void Sampler::Cleanup() {
+  if (mValid && mParent) {
+    mValid = false;
+    WebGPUChild* bridge = mParent->mBridge;
+    if (bridge && bridge->IsOpen()) {
+      bridge->DestroySampler(mId);
+    }
+  }
+}
 
 }  // namespace webgpu
 }  // namespace mozilla
