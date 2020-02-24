cc1cc528c85358cf1e4555c123723852eb8ca8f2
Greg V <greg@unrelenting.technology>
2020-02-24 20:18:01 +0000

    Bug 1594027 - add more STL wrappers to fix build with libc++ 9 r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D62847

--- config/gcc-stl-wrapper.template.h
+++ config/gcc-stl-wrapper.template.h
@@ -32,6 +32,14 @@
 #  define moz_dont_include_mozalloc_for_cstdlib
 #endif
 
+#ifndef moz_dont_include_mozalloc_for_type_traits
+#  define moz_dont_include_mozalloc_for_type_traits
+#endif
+
+#ifndef moz_dont_include_mozalloc_for_limits
+#  define moz_dont_include_mozalloc_for_limits
+#endif
+
 // Include mozalloc after the STL header and all other headers it includes
 // have been preprocessed.
 #if !defined(MOZ_INCLUDE_MOZALLOC_H) && \
