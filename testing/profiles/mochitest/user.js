5217248cff5de388df91d41e587b65f9f4aa773f
Andrea Marchesini <amarchesini@mozilla.com>
2020-02-25 18:32:08 +0000

    Bug 1604212 - Enable sameSite=lax by default, r=Ehsan,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D63081

---
+++ testing/profiles/mochitest/user.js
@@ -0,0 +1,6 @@
+// Base preferences file used by the mochitest
+/* globals user_pref */
+/* eslint quotes: 0 */
+
+// XXX: Bug 1617611 - Fix all the tests broken by "cookies sameSite=lax by default"
+user_pref("network.cookie.sameSite.laxByDefault", false);
