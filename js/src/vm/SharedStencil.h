12132100cdfc40bd8409fdf2cc5c383e02236e97
Matthew Gaudet <mgaudet@mozilla.com>
2020-02-25 13:19:10 +0000

    Bug 1617306 - Change implementation of SharedStencil ImmutableFlags to prepare for sharing r=caroline
    Differential Revision: https://phabricator.services.mozilla.com/D63941

--- js/src/vm/SharedStencil.h
+++ js/src/vm/SharedStencil.h
@@ -17,127 +17,142 @@
  */
 
 namespace js {
-namespace frontend {
+class BaseScript;
 
-class ImmutableScriptFlags {
- public:
-  enum ScriptFlags : uint32_t {
-    // Input Flags
-    //
-    // Flags that come from CompileOptions.
-    // ----
+template <typename EnumType>
+class ScriptFlagBase {
+  // To allow cross-checking offsetof assert.
+  friend class js::BaseScript;
 
-    // No need for result value of last expression statement.
-    NoScriptRval = 1 << 0,
-
-    // See Parser::selfHostingMode.
-    SelfHosted = 1 << 1,
-
-    // Script is a lambda to treat as running once or a global or eval script
-    // that will only run once.  Which one it is can be disambiguated by
-    // checking whether function() is null.
-    TreatAsRunOnce = 1 << 2,
-    // ----
-
-    // Code is in strict mode.
-    Strict = 1 << 3,
-
-    // True if the script has a non-syntactic scope on its dynamic scope chain.
-    // That is, there are objects about which we know nothing between the
-    // outermost syntactic scope and the global.
-    HasNonSyntacticScope = 1 << 4,
-
-    // See FunctionBox.
-    BindingsAccessedDynamically = 1 << 5,
-    FunHasExtensibleScope = 1 << 6,
-
-    // Bytecode contains JSOp::CallSiteObj
-    // (We don't relazify functions with template strings, due to observability)
-    HasCallSiteObj = 1 << 7,
-
-    // Script is parsed with a top-level goal of Module. This may be a top-level
-    // or an inner-function script.
-    HasModuleGoal = 1 << 8,
-
-    FunctionHasThisBinding = 1 << 9,
-    FunctionHasExtraBodyVarScope = 1 << 10,
-
-    // Whether the arguments object for this script, if it needs one, should be
-    // mapped (alias formal parameters).
-    HasMappedArgsObj = 1 << 11,
-
-    // Script contains inner functions. Used to check if we can relazify the
-    // script.
-    HasInnerFunctions = 1 << 12,
-
-    NeedsHomeObject = 1 << 13,
-
-    IsDerivedClassConstructor = 1 << 14,
-    IsDefaultClassConstructor = 1 << 15,
-
-    // 'this', 'arguments' and f.apply() are used. This is likely to be a
-    // wrapper.
-    IsLikelyConstructorWrapper = 1 << 16,
-
-    // Set if this function is a generator function or async generator.
-    IsGenerator = 1 << 17,
-
-    // Set if this function is an async function or async generator.
-    IsAsync = 1 << 18,
-
-    // Set if this function has a rest parameter.
-    HasRest = 1 << 19,
-
-    // See comments below.
-    ArgumentsHasVarBinding = 1 << 20,
-
-    // Script came from eval().
-    IsForEval = 1 << 21,
-
-    // Whether this is a top-level module script.
-    IsModule = 1 << 22,
-
-    // Whether this function needs a call object or named lambda environment.
-    NeedsFunctionEnvironmentObjects = 1 << 23,
-
-    // Whether the Parser declared 'arguments'.
-    ShouldDeclareArguments = 1 << 24,
-
-    // Script is for function.
-    IsFunction = 1 << 25,
-
-    // Whether this script contains a direct eval statement.
-    HasDirectEval = 1 << 26,
-
-    // Whether this BaseScript is a LazyScript. This flag will be removed after
-    // LazyScript and JSScript are merged in Bug 1529456.
-    IsLazyScript = 1 << 27,
-  };
-
-  // This is stored as uint32_t instead of bitfields to make it more
-  // predictable to access from JIT code.
-  uint32_t scriptFlags_ = 0;
+ protected:
+  // Stored as a uint32_t to make access more predictable from
+  // JIT code.
+  uint32_t flags_ = 0;
 
  public:
-  ImmutableScriptFlags() : scriptFlags_() {
-    static_assert(sizeof(ImmutableScriptFlags) == sizeof(scriptFlags_),
+  MOZ_MUST_USE bool hasFlag(EnumType flag) const {
+    return flags_ & static_cast<uint32_t>(flag);
+  }
+  void setFlag(EnumType flag) { flags_ |= static_cast<uint32_t>(flag); }
+  void clearFlag(EnumType flag) { flags_ &= ~static_cast<uint32_t>(flag); }
+
+  operator uint32_t() const { return flags_; }
+};
+
+enum class ImmutableScriptFlagsEnum : uint32_t {
+  // Input Flags
+  //
+  // Flags that come from CompileOptions.
+  // ----
+
+  // No need for result value of last expression statement.
+  NoScriptRval = 1 << 0,
+
+  // See Parser::selfHostingMode.
+  SelfHosted = 1 << 1,
+
+  // Script is a lambda to treat as running once or a global or eval script
+  // that will only run once.  Which one it is can be disambiguated by
+  // checking whether function() is null.
+  TreatAsRunOnce = 1 << 2,
+  // ----
+
+  // Code is in strict mode.
+  Strict = 1 << 3,
+
+  // True if the script has a non-syntactic scope on its dynamic scope chain.
+  // That is, there are objects about which we know nothing between the
+  // outermost syntactic scope and the global.
+  HasNonSyntacticScope = 1 << 4,
+
+  // See FunctionBox.
+  BindingsAccessedDynamically = 1 << 5,
+  FunHasExtensibleScope = 1 << 6,
+
+  // Bytecode contains JSOp::CallSiteObj
+  // (We don't relazify functions with template strings, due to observability)
+  HasCallSiteObj = 1 << 7,
+
+  // Script is parsed with a top-level goal of Module. This may be a top-level
+  // or an inner-function script.
+  HasModuleGoal = 1 << 8,
+
+  FunctionHasThisBinding = 1 << 9,
+  FunctionHasExtraBodyVarScope = 1 << 10,
+
+  // Whether the arguments object for this script, if it needs one, should be
+  // mapped (alias formal parameters).
+  HasMappedArgsObj = 1 << 11,
+
+  // Script contains inner functions. Used to check if we can relazify the
+  // script.
+  HasInnerFunctions = 1 << 12,
+
+  NeedsHomeObject = 1 << 13,
+
+  IsDerivedClassConstructor = 1 << 14,
+  IsDefaultClassConstructor = 1 << 15,
+
+  // 'this', 'arguments' and f.apply() are used. This is likely to be a
+  // wrapper.
+  IsLikelyConstructorWrapper = 1 << 16,
+
+  // Set if this function is a generator function or async generator.
+  IsGenerator = 1 << 17,
+
+  // Set if this function is an async function or async generator.
+  IsAsync = 1 << 18,
+
+  // Set if this function has a rest parameter.
+  HasRest = 1 << 19,
+
+  // See comments below.
+  ArgumentsHasVarBinding = 1 << 20,
+
+  // Script came from eval().
+  IsForEval = 1 << 21,
+
+  // Whether this is a top-level module script.
+  IsModule = 1 << 22,
+
+  // Whether this function needs a call object or named lambda environment.
+  NeedsFunctionEnvironmentObjects = 1 << 23,
+
+  // Whether the Parser declared 'arguments'.
+  ShouldDeclareArguments = 1 << 24,
+
+  // Script is for function.
+  IsFunction = 1 << 25,
+
+  // Whether this script contains a direct eval statement.
+  HasDirectEval = 1 << 26,
+
+  // Whether this BaseScript is a LazyScript. This flag will be removed after
+  // LazyScript and JSScript are merged in Bug 1529456.
+  IsLazyScript = 1 << 27,
+};
+
+class ImmutableScriptFlags : public ScriptFlagBase<ImmutableScriptFlagsEnum> {
+  // Immutable flags should not be modified after the JSScript that contains
+  // them has been initialized. These flags should likely be preserved when
+  // serializing (XDR) or copying (CopyScript) the script. This is only public
+  // for the JITs.
+  //
+  // Specific accessors for flag values are defined with
+  // IMMUTABLE_FLAG_* macros below.
+ public:
+  ImmutableScriptFlags() = default;
+
+  void static_asserts() {
+    static_assert(sizeof(ImmutableScriptFlags) == sizeof(flags_),
                   "No extra members allowed");
-    static_assert(offsetof(ImmutableScriptFlags, scriptFlags_) == 0,
+    static_assert(offsetof(ImmutableScriptFlags, flags_) == 0,
                   "Required for JIT flag access");
   }
 
-  MOZ_IMPLICIT ImmutableScriptFlags(ScriptFlags sf) : scriptFlags_(sf) {}
-  MOZ_IMPLICIT ImmutableScriptFlags(int32_t flags)
-      : scriptFlags_(ScriptFlags(flags)) {}
-
-  operator uint32_t() const { return scriptFlags_; }
-
-  void operator=(uint32_t flag) { scriptFlags_ = flag; }
-
-  uint32_t toRaw() const { return scriptFlags_; }
+  void operator=(uint32_t flag) { flags_ = flag; }
 };
 
-}  // namespace frontend
 }  // namespace js
 
 #endif /* vm_SharedStencil_h */
