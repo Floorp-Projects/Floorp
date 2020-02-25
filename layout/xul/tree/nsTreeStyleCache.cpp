ec812b96bea479aa61819162e11d3ab96f3fba0c
Simon Giesecke <sgiesecke@mozilla.com>
2020-02-25 09:36:12 +0000

    Bug 1617542 - Remove nsRefPtrHashtable::Put overload accepting already_AddRefed and adapt all uses. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D63865

--- layout/xul/tree/nsTreeStyleCache.cpp
+++ layout/xul/tree/nsTreeStyleCache.cpp
@@ -95,7 +95,7 @@ ComputedStyle* nsTreeStyleCache::GetComputedStyle(
       mCache = MakeUnique<ComputedStyleCache>();
     }
     result = newResult.get();
-    mCache->Put(currState, newResult.forget());
+    mCache->Put(currState, std::move(newResult));
   }
 
   return result;
