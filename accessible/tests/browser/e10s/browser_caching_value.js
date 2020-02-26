9212362ff5bc85c048f16f10b21fbe71cf817dde
James Teh <jteh@mozilla.com>
2020-02-26 00:36:02 +0000

    Bug 1602692: Enable a11y tests for Fission which depend on TakeFocus. r=yzen
    nsFocusManager::SetFocus (and thus Accessible::TakeFocus) was fixed for Fission in bug 1556627.
    Most of these tests now pass without modification.
    browser_caching_value.js needed to be tweaked slightly to synthesize a key in the OOP iframe instead of the top level document.
    
    Differential Revision: https://phabricator.services.mozilla.com/D64254

--- accessible/tests/browser/e10s/browser_caching_value.js
+++ accessible/tests/browser/e10s/browser_caching_value.js
@@ -136,7 +136,13 @@ const valueTests = [
     id: "range",
     async action(browser) {
       await invokeFocus(browser, "range");
-      await BrowserTestUtils.synthesizeKey("VK_LEFT", {}, browser);
+      await invokeContentTask(browser, [], () => {
+        const { ContentTaskUtils } = ChromeUtils.import(
+          "resource://testing-common/ContentTaskUtils.jsm"
+        );
+        const EventUtils = ContentTaskUtils.getEventUtils(content);
+        EventUtils.synthesizeKey("VK_LEFT", {}, content);
+      });
     },
     waitFor: EVENT_VALUE_CHANGE,
     expected: "5",
