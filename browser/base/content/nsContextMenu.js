5340a20c4db990e85febf0de50633056f688caa2
Bogdan Tara <btara@mozilla.com>
2020-02-26 23:19:40 +0200

    Backed out changeset fbc90d35117e (bug 1615220) for browser_autocomplete_generated_password_private_window.js failures CLOSED TREE

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -918,7 +918,6 @@ class nsContextMenu {
   initPasswordManagerItems() {
     let showFill = false;
     let showGenerate = false;
-    let enableGeneration = Services.logins.isLoggedIn;
     try {
       let loginFillInfo = this.contentData && this.contentData.loginFillInfo;
       let documentURI = this.contentData.documentURIObject;
@@ -994,11 +993,6 @@ class nsContextMenu {
     } finally {
       this.showItem("fill-login", showFill);
       this.showItem("fill-login-generated-password", showGenerate);
-      this.setItemAttr(
-        "fill-login-generated-password",
-        "disabled",
-        !enableGeneration
-      );
       this.showItem(
         "fill-login-and-generated-password-separator",
         showFill || showGenerate
