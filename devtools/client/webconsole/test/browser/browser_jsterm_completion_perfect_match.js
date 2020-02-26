53f7daba4b391a51c57de0c180639272917cafed
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-02-26 14:46:45 +0000

    Bug 1609942 - Display autocomplete popup at the top of the input. r=jdescottes,Honza.
    This prevents the popup to cover the eager evaluation result.
    In order for the popup to be able to appear outside of the
    toolbox, we pass the useXulWrapper option to the HTMLTooltip.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62431
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/webconsole/test/browser/browser_jsterm_completion_perfect_match.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_completion_perfect_match.js
@@ -21,9 +21,7 @@ add_task(async function() {
   const { autocompletePopup } = jsterm;
 
   info("Check that filtering the cache works like on the server");
-  const onPopUpOpen = autocompletePopup.once("popup-opened");
-  EventUtils.sendString("x.");
-  await onPopUpOpen;
+  await setInputValueForAutocompletion(hud, "x.");
   is(
     getAutocompletePopupLabels(autocompletePopup).join(" - "),
     "foo - foO - fOo - fOO",
