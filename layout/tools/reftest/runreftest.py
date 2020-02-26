492865afbc5226b166a203faf8f3ea036f297672
Geoff Brown <gbrown@mozilla.com>
2020-02-26 15:45:33 +0000

    Bug 1617637 - Use string rather than tuple for reftest test ids in suite_start; r=ahal
    reftest-plain test identifiers may be tuples like ['this', '==', 'that'], which are
    undesirable to ActiveData. This patch converts such identifiers to simple strings,
    'this == that' in the suite_start message (string format is already used in other
    messages, like test_start).
    
    Differential Revision: https://phabricator.services.mozilla.com/D63976

--- layout/tools/reftest/runreftest.py
+++ layout/tools/reftest/runreftest.py
@@ -957,7 +957,10 @@ class RefTest(object):
         ids_by_manifest = defaultdict(list)
         for t in tests:
             tests_by_manifest[t['manifest']].append(t)
-            ids_by_manifest[t['manifestID']].append(t['identifier'])
+            test_id = t['identifier']
+            if not isinstance(test_id, string_types):
+                test_id = ' '.join(test_id)
+            ids_by_manifest[t['manifestID']].append(test_id)
 
         self.log.suite_start(ids_by_manifest, name=options.suite)
 
