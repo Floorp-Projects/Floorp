f401ce2145e0c282a04b09800ffc54eeff33ea0e
Alphan Chen <alchen@mozilla.com>
2020-02-26 14:15:57 +0000

    Bug 1589299 - Force scrollbars flag to be always enabled for new windows. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D64126

--- toolkit/components/windowwatcher/test/browser_new_content_window_chromeflags.js
+++ toolkit/components/windowwatcher/test/browser_new_content_window_chromeflags.js
@@ -323,10 +323,11 @@ add_task(async function test_new_remote_window_flags() {
 
 /**
  * Opens a window with some chrome flags specified, which should not affect
- * scrollbars flag which defaults to true when not disabled explicitly.
+ * scrollbars flag which always be true.
  */
 add_task(async function test_scrollbars_flag() {
-  const SCRIPT = 'window.open("about:blank", "_blank", "toolbar=0");';
+  const SCRIPT =
+    'window.open("about:blank", "_blank", "toolbar=0,scrollbars=0");';
   const SCRIPT_PAGE = `data:text/html,<script>${SCRIPT}</script>`;
 
   let newWinPromise = BrowserTestUtils.waitForNewWindow();
@@ -341,13 +342,13 @@ add_task(async function test_scrollbars_flag() {
       let parentChromeFlags = getParentChromeFlags(win);
       Assert.ok(
         parentChromeFlags & Ci.nsIWebBrowserChrome.CHROME_SCROLLBARS,
-        "Should have scrollbars when not disabled explicitly"
+        "Always have scrollbars even when disabled explicitly"
       );
 
       let contentChromeFlags = await getContentChromeFlags(win);
       Assert.ok(
         contentChromeFlags & Ci.nsIWebBrowserChrome.CHROME_SCROLLBARS,
-        "Should have scrollbars when not disabled explicitly"
+        "Always have scrollbars even when disabled explicitly"
       );
 
       await BrowserTestUtils.closeWindow(win);
