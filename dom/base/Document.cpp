e1ae464b647a08db82c5e1ff9a8019110f4af24d
Cosmin Sabou <csabou@mozilla.com>
2020-02-26 21:25:09 +0200

    Backed out changeset 8c1c55d16e1a (bug 1346893) for bc failures on browser_all_files_referenced.js.
    --HG--
    rename : layout/style/res/geckoview.css => mobile/android/themes/geckoview/content.css
    extra : histedit_source : e5159e67631172a933b02ac22c838991da589170

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -2857,10 +2857,6 @@ void Document::FillStyleSetUserAndUASheets() {
   mStyleSet->AppendStyleSheet(StyleOrigin::UserAgent,
                               cache->PluginProblemSheet());
 
-  if (StyleSheet* sheet = cache->GetGeckoViewSheet()) {
-    mStyleSet->AppendStyleSheet(StyleOrigin::UserAgent, sheet);
-  }
-
   for (StyleSheet* sheet : *sheetService->AgentStyleSheets()) {
     mStyleSet->AppendStyleSheet(StyleOrigin::UserAgent, sheet);
   }
