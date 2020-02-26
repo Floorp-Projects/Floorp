e781db7c143c45c738d773c5482e6b0c5be902ae
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-02-26 14:15:14 +0000

    Bug 1616879 - [reftest] Add some selftests for invalid manifests, r=dbaron
    Adds some tests to cover error cases in reftest manifests. This is far from
    comprehensive, but gives us an easy place to add more in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D63718

--- layout/tools/reftest/selftest/test_reftest_manifest_parser.py
+++ layout/tools/reftest/selftest/test_reftest_manifest_parser.py
@@ -38,5 +38,20 @@ def test_parse_test_types(parse):
     assert tests[4]['type'] == 'print'
 
 
+def test_parse_invalid_manifests(parse):
+    # XXX We should assert that the output contains the appropriate error
+    # message, but we seem to be hitting an issue in pytest that is preventing
+    # us from capturing the Gecko output with the capfd fixture. See:
+    # https://github.com/pytest-dev/pytest/issues/5997
+    with pytest.raises(SystemExit):
+        parse('invalid-defaults.list')
+
+    with pytest.raises(SystemExit):
+        parse('invalid-defaults-include.list')
+
+    with pytest.raises(SystemExit):
+        parse('invalid-include.list')
+
+
 if __name__ == '__main__':
     mozunit.main()
