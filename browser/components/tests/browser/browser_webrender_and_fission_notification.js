eebb2cfe8b046c97375b41e03439c14e957dd17d
David Teller <dteller@mozilla.com>
2020-02-27 13:59:23 +0000

    Bug 1607366 - Display a warning when we're running Fission without WebRender;r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D61827

---
+++ browser/components/tests/browser/browser_webrender_and_fission_notification.js
@@ -0,0 +1,24 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Makes sure that we report correctly the combo fission + non-webrender
+
+"use strict";
+
+add_task(async function() {
+  const isWebRenderEnabled = Services.prefs.getBoolPref("gfx.webrender.all");
+  const isFissionEnabled = Services.prefs.getBoolPref("fission.autostart");
+  const isWarningExpected = isFissionEnabled && !isWebRenderEnabled;
+
+  // Wait until the browser has had a chance to display the warning.
+  await gBrowserInit.idleTasksFinishedPromise;
+
+  let isWarningFound = !!gNotificationBox.getNotificationWithValue(
+    "warning-fission-without-webrender-notification"
+  );
+  is(
+    isWarningFound,
+    isWarningExpected,
+    "Did we get the Fission/WebRender warning right?"
+  );
+});
