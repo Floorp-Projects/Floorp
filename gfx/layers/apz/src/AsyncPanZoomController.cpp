a7fb587a496abadba503d9b120c86294f2e7f8bc
Dorel Luca <dluca@mozilla.com>
2020-02-27 14:13:09 +0200

    Backed out 4 changesets (bug 1583956) for Build bustage in gfx/layers/apz/util/ChromeProcessController.cpp. CLOSED TREE
    Backed out changeset f787e3e4a31c (bug 1583956)
    Backed out changeset c8eadb4b6301 (bug 1583956)
    Backed out changeset 78dfbab51db6 (bug 1583956)
    Backed out changeset 7d0e2d6cfaeb (bug 1583956)

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -99,19 +99,13 @@
 
 static mozilla::LazyLogModule sApzCtlLog("apz.controller");
 #define APZC_LOG(...) MOZ_LOG(sApzCtlLog, LogLevel::Debug, (__VA_ARGS__))
-#define APZC_LOGV(...) MOZ_LOG(sApzCtlLog, LogLevel::Verbose, (__VA_ARGS__))
-
-#define APZC_LOG_FM_COMMON(fm, prefix, level, ...)          \
-  if (MOZ_LOG_TEST(sApzCtlLog, level)) {                    \
-    std::stringstream ss;                                   \
-    ss << nsPrintfCString(prefix, __VA_ARGS__).get();       \
-    AppendToString(ss, fm, ":", "", true);                  \
-    MOZ_LOG(sApzCtlLog, level, ("%s\n", ss.str().c_str())); \
+#define APZC_LOG_FM(fm, prefix, ...)                  \
+  if (MOZ_LOG_TEST(sApzCtlLog, LogLevel::Debug)) {    \
+    std::stringstream ss;                             \
+    ss << nsPrintfCString(prefix, __VA_ARGS__).get(); \
+    AppendToString(ss, fm, ":", "", true);            \
+    APZC_LOG("%s\n", ss.str().c_str());               \
   }
-#define APZC_LOG_FM(fm, prefix, ...) \
-  APZC_LOG_FM_COMMON(fm, prefix, LogLevel::Debug, __VA_ARGS__)
-#define APZC_LOGV_FM(fm, prefix, ...) \
-  APZC_LOG_FM_COMMON(fm, prefix, LogLevel::Verbose, __VA_ARGS__)
 
 namespace mozilla {
 namespace layers {
@@ -2786,7 +2780,6 @@ nsEventStatus AsyncPanZoomController::GenerateSingleTap(
       // the corresponding touch-up. To avoid that we schedule the singletap
       // message to run on the next spin of the event loop. See bug 965381 for
       // the issue this was causing.
-      APZC_LOG("posting runnable for HandleTap from GenerateSingleTap");
       RefPtr<Runnable> runnable =
           NewRunnableMethod<TapType, LayoutDevicePoint, mozilla::Modifiers,
                             ScrollableLayerGuid, uint64_t>(
@@ -3832,7 +3825,7 @@ const ScreenMargin AsyncPanZoomController::CalculatePendingDisplayPort(
   float paintFactor = kDefaultEstimatedPaintDurationMs;
   displayPort.MoveBy(velocity * paintFactor * StaticPrefs::apz_velocity_bias());
 
-  APZC_LOGV_FM(
+  APZC_LOG_FM(
       aFrameMetrics,
       "Calculated displayport as %s from velocity %s paint time %f metrics",
       Stringify(displayPort).c_str(), ToString(aVelocity).c_str(), paintFactor);
@@ -3987,7 +3980,7 @@ void AsyncPanZoomController::RequestContentRepaint(
     return;
   }
 
-  APZC_LOGV_FM(aFrameMetrics, "%p requesting content repaint", this);
+  APZC_LOG_FM(aFrameMetrics, "%p requesting content repaint", this);
   {  // scope lock
     MutexAutoLock lock(mCheckerboardEventLock);
     if (mCheckerboardEvent && mCheckerboardEvent->IsRecordingTrace()) {
@@ -4427,7 +4420,7 @@ void AsyncPanZoomController::NotifyLayersUpdated(
 
   if ((aScrollMetadata == mLastContentPaintMetadata) && !isDefault) {
     // No new information here, skip it.
-    APZC_LOGV("%p NotifyLayersUpdated short-circuit\n", this);
+    APZC_LOG("%p NotifyLayersUpdated short-circuit\n", this);
     return;
   }
 
@@ -4451,10 +4444,10 @@ void AsyncPanZoomController::NotifyLayersUpdated(
   }
 
   mScrollMetadata.SetScrollParentId(aScrollMetadata.GetScrollParentId());
-  APZC_LOGV_FM(aLayerMetrics,
-               "%p got a NotifyLayersUpdated with aIsFirstPaint=%d, "
-               "aThisLayerTreeUpdated=%d",
-               this, aIsFirstPaint, aThisLayerTreeUpdated);
+  APZC_LOG_FM(aLayerMetrics,
+              "%p got a NotifyLayersUpdated with aIsFirstPaint=%d, "
+              "aThisLayerTreeUpdated=%d",
+              this, aIsFirstPaint, aThisLayerTreeUpdated);
 
   {  // scope lock
     MutexAutoLock lock(mCheckerboardEventLock);
