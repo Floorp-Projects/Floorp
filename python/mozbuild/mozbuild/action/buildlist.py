e4b69ea6b5b43cc796d7456aec77e2801f1b08c2
Ricky Stewart <rstewart@mozilla.com>
2020-02-27 00:25:03 +0000

    Bug 1617313 - Remaining tests in mozbuild/backend support Python 3 r=firefox-build-system-reviewers,mshal
    Differential Revision: https://phabricator.services.mozilla.com/D63908
    
    --HG--
    extra : moz-landing-system : lando

--- python/mozbuild/mozbuild/action/buildlist.py
+++ python/mozbuild/mozbuild/action/buildlist.py
@@ -7,8 +7,9 @@ if the entry does not already exist.
 
 Usage: buildlist.py <filename> <entry> [<entry> ...]
 '''
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
+import io
 import sys
 import os
 
@@ -23,10 +24,10 @@ def addEntriesToListFile(listFile, entries):
     add each entry in |entries| to the file, unless it is already
     present."""
     ensureParentDir(listFile)
-    lock = lock_file(listFile + ".lck")
+    lock = lock_file(listFile + '.lck')
     try:
         if os.path.exists(listFile):
-            f = open(listFile)
+            f = io.open(listFile)
             existing = set(x.strip() for x in f.readlines())
             f.close()
         else:
@@ -34,8 +35,8 @@ def addEntriesToListFile(listFile, entries):
         for e in entries:
             if e not in existing:
                 existing.add(e)
-        with open(listFile, 'wb') as f:
-            f.write("\n".join(sorted(existing))+"\n")
+        with io.open(listFile, 'w', newline='\n') as f:
+            f.write('\n'.join(sorted(existing)) + '\n')
     finally:
         del lock  # Explicitly release the lock_file to free it
 
