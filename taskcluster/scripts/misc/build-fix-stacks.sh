08ca7c7fd08a9ace200d85d13783073c566524e7
Nicholas Nethercote <nnethercote@mozilla.com>
2020-02-27 20:07:10 +0000

    Bug 1604095 - Fix up `fix-stacks` toolchain on Mac. r=erahm
    It currently is missing some cross-compilation stuff and so actually builds a
    Linux binary.
    
    The commit also avoids the unintentional installation of the `fix-stacks.d`
    file, by introducing the `EXE` variable.
    
    Differential Revision: https://phabricator.services.mozilla.com/D64528
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/scripts/misc/build-fix-stacks.sh
+++ taskcluster/scripts/misc/build-fix-stacks.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 set -x -e -v
 
-# This script is for building fix-stacks
+# This script is for building fix-stacks.
 PROJECT=fix-stacks
+
+# Needed by osx-cross-linker; unset when building for non-osx.
+export TARGET="$1"
 
 case "$(uname -s)" in
 Linux)
+    EXE=
     COMPRESS_EXT=xz
+    # {CC,CXX} and TARGET_{CC,CXX} must be set because a build.rs file builds
+    # some C and C++ code.
     export CC=$MOZ_FETCHES_DIR/clang/bin/clang
     export CXX=$MOZ_FETCHES_DIR/clang/bin/clang++
     export PATH="$MOZ_FETCHES_DIR/binutils/bin:$PATH"
-    export RUSTFLAGS="-C linker=$CXX"
+    if [ "$TARGET" == "x86_64-apple-darwin" ] ; then
+        # Cross-compiling for Mac on Linux.
+        export PATH="$MOZ_FETCHES_DIR/cctools/bin:$PATH"
+        export RUSTFLAGS="-C linker=$GECKO_PATH/taskcluster/scripts/misc/osx-cross-linker"
+        export TARGET_CC="$CC -isysroot $MOZ_FETCHES_DIR/MacOSX10.11.sdk"
+        export TARGET_CXX="$CXX -isysroot $MOZ_FETCHES_DIR/MacOSX10.11.sdk"
+    else
+        export RUSTFLAGS="-C linker=$CXX"
+    fi
     ;;
 MINGW*)
     UPLOAD_DIR=$PWD/public/build
+    EXE=.exe
     COMPRESS_EXT=bz2
 
     . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
@@ -35,10 +50,10 @@ PATH="$(cd $MOZ_FETCHES_DIR && pwd)/rustc/bin:$PATH"
 
 cd $MOZ_FETCHES_DIR/$PROJECT
 
-cargo build --verbose --release
+cargo build --verbose --release --target "$TARGET"
 
 mkdir $PROJECT
-cp target/release/${PROJECT}* ${PROJECT}/
+cp target/$TARGET/release/${PROJECT}${EXE} ${PROJECT}/
 tar -acf ${PROJECT}.tar.$COMPRESS_EXT $PROJECT
 mkdir -p $UPLOAD_DIR
 cp ${PROJECT}.tar.$COMPRESS_EXT $UPLOAD_DIR
