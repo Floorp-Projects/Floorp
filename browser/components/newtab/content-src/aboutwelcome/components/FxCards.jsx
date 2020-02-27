782942ae9510fb8bc98a3c176deae6a0f03d1d3f
Punam Dahiya <punamdahiya@yahoo.com>
2020-02-27 18:52:46 +0000

    Bug 1616370 - Simplified welcome page r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D63406

---
+++ browser/components/newtab/content-src/aboutwelcome/components/FxCards.jsx
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from "react";
+import { addUtmParams } from "../../asrouter/templates/FirstRun/addUtmParams";
+import { OnboardingCard } from "../../asrouter/templates/OnboardingMessage/OnboardingMessage";
+import { AboutWelcomeUtils } from "../../lib/aboutwelcome-utils";
+
+export class FxCards extends React.PureComponent {
+  onCardAction(action) {
+    let actionUpdates = {};
+    let UTMTerm = "utm_term_separate_welcome";
+
+    if (action.type === "OPEN_URL") {
+      let url = new URL(action.data.args);
+      addUtmParams(url, UTMTerm);
+      actionUpdates = { data: { ...action.data, args: url.toString() } };
+    }
+
+    AboutWelcomeUtils.handleUserAction({
+      data: { ...action, ...actionUpdates },
+    });
+  }
+
+  render() {
+    const { props } = this;
+    return (
+      <React.Fragment>
+        <div className={`trailheadCardGrid show`}>
+          {props.cards.map(card => (
+            <OnboardingCard
+              key={card.id}
+              message={card}
+              className="trailheadCard"
+              sendUserActionTelemetry={props.sendTelemetry}
+              onAction={this.onCardAction}
+              UISurface="ABOUT_WELCOME"
+              {...card}
+            />
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
