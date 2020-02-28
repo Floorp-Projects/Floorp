5672e124d1a517cd0bdbd4bc4901ef8279e62637
Julian Descottes <jdescottes@mozilla.com>
2020-02-28 11:45:32 +0000

    Bug 1579042 - Move tab call to getTarget from the server to the client r=ochameau
    Depends on D63501
    
    Differential Revision: https://phabricator.services.mozilla.com/D63502
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/shared/specs/descriptors/tab.js
+++ devtools/shared/specs/descriptors/tab.js
@@ -3,10 +3,19 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { generateActorSpec } = require("devtools/shared/protocol");
+const { generateActorSpec, RetVal } = require("devtools/shared/protocol");
 
 const tabDescriptorSpec = generateActorSpec({
   typeName: "tabDescriptor",
+
+  methods: {
+    getTarget: {
+      request: {},
+      response: {
+        frame: RetVal("json"),
+      },
+    },
+  },
 });
 
 exports.tabDescriptorSpec = tabDescriptorSpec;
