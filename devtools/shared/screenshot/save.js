c3e62ab62446fc4c5bdd589193ccd4bf8dab8d7f
Csoregi Natalia <ncsoregi@mozilla.com>
2020-02-28 18:11:05 +0200

    Backed out changeset f216619d394a (bug 766661) for failures on browser_jsterm_screenshot_command_warnings.js. CLOSED TREE

--- devtools/shared/screenshot/save.js
+++ devtools/shared/screenshot/save.js
@@ -116,11 +116,6 @@ function getFormattedHelpData() {
  *         Response messages from processing the screenshot
  */
 function saveScreenshot(window, args = {}, value) {
-  // Guard against missing image data.
-  if (!value.data) {
-    return [];
-  }
-
   if (args.help) {
     const message = getFormattedHelpData();
     // Wrap message in an array so that the return value is consistant with save
@@ -229,11 +224,6 @@ function saveToClipboard(base64URI) {
  */
 async function saveToFile(image) {
   let filename = image.filename;
-
-  // Guard against missing image data.
-  if (!image.data) {
-    return "";
-  }
 
   // Check there is a .png extension to filename
   if (!filename.match(/.png$/i)) {
