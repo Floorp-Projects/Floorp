acfda99d8b28481af36115d668938e2d809f9897
Mark Banner <standard8@mozilla.com>
2020-02-28 12:13:50 +0000

    Bug 1613348 - Turn on the modern Search Service configuration for nightly builds. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D63162

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -403,6 +403,10 @@ pref("browser.search.widget.inNavBar", false);
 // The maximum amount of times the private default banner is shown.
 pref("browser.search.separatePrivateDefault.ui.banner.max", 0);
 
+#ifdef NIGHTLY_BUILD
+  pref("browser.search.modernConfig", true);
+#endif
+
 pref("browser.sessionhistory.max_entries", 50);
 
 // Built-in default permissions.
