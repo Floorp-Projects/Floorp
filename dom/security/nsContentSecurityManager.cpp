fb639185eaeb63dabc63f7f13794326622b64b22
Frederik Braun <fbraun@mozilla.com>
2020-02-28 12:35:34 +0000

    Bug 1561318 - rm exception that fennec Settings may load accounts.firefox.com using the system principal r=Gijs
    This breaks fennec builds from mozilla-central, but fennec will only be built from
    esr branches, so this code-exception that adds complexity can be removed
    
    Differential Revision: https://phabricator.services.mozilla.com/D64384

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -820,11 +820,7 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
 
   // FIXME The discovery feature in about:addons uses the SystemPrincpal.
   // We should remove the exception for AMO with bug 1544011.
-  // We should remove the exception for Firefox Accounts with bug 1561318.
   static nsAutoCString sDiscoveryPrePath;
-#ifdef ANDROID
-  static nsAutoCString sFxaSPrePath;
-#endif
   static bool recvdPrefValues = false;
   if (!recvdPrefValues) {
     nsAutoCString discoveryURLString;
@@ -837,16 +833,6 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
     if (discoveryURL) {
       discoveryURL->GetPrePath(sDiscoveryPrePath);
     }
-#ifdef ANDROID
-    nsAutoCString fxaURLString;
-    Preferences::GetCString("identity.fxaccounts.remote.webchannel.uri",
-                            fxaURLString);
-    nsCOMPtr<nsIURI> fxaURL;
-    NS_NewURI(getter_AddRefs(fxaURL), fxaURLString);
-    if (fxaURL) {
-      fxaURL->GetPrePath(sFxaSPrePath);
-    }
-#endif
     recvdPrefValues = true;
   }
   nsAutoCString requestedPrePath;
@@ -855,11 +841,6 @@ nsresult nsContentSecurityManager::CheckAllowLoadInSystemPrivilegedContext(
   if (requestedPrePath.Equals(sDiscoveryPrePath)) {
     return NS_OK;
   }
-#ifdef ANDROID
-  if (requestedPrePath.Equals(sFxaSPrePath)) {
-    return NS_OK;
-  }
-#endif
   nsAutoCString requestedURL;
   finalURI->GetAsciiSpec(requestedURL);
   if (xpc::AreNonLocalConnectionsDisabled()) {
