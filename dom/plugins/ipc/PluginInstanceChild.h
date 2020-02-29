05033fda2a6752699b1160a5ca4b97fa61c3b6cf
Eric Rahm <erahm@mozilla.com>
2020-02-29 02:10:12 +0000

    Bug 1618735 - Remove nsAutoPtr usage from dom/plugins. r=handyman
    Differential Revision: https://phabricator.services.mozilla.com/D64693
    
    --HG--
    extra : moz-landing-system : lando

--- dom/plugins/ipc/PluginInstanceChild.h
+++ dom/plugins/ipc/PluginInstanceChild.h
@@ -24,7 +24,7 @@
 #endif
 
 #include "npfunctions.h"
-#include "nsAutoPtr.h"
+#include "mozilla/UniquePtr.h"
 #include "nsTArray.h"
 #include "ChildTimer.h"
 #include "nsRect.h"
@@ -387,14 +387,14 @@ class PluginInstanceChild : public PPluginInstanceChild {
 #if defined(OS_WIN)
   nsTArray<FlashThrottleMsg*> mPendingFlashThrottleMsgs;
 #endif
-  nsTArray<nsAutoPtr<ChildTimer> > mTimers;
+  nsTArray<UniquePtr<ChildTimer> > mTimers;
 
   /**
    * During destruction we enumerate all remaining scriptable objects and
    * invalidate/delete them. Enumeration can re-enter, so maintain a
    * hash separate from PluginModuleChild.mObjectMap.
    */
-  nsAutoPtr<nsTHashtable<DeletingObjectEntry> > mDeletingHash;
+  UniquePtr<nsTHashtable<DeletingObjectEntry> > mDeletingHash;
 
 #if defined(MOZ_WIDGET_COCOA)
  private:
