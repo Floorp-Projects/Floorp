ae73f1a2cb0d20e2f52a3c7de6f101d65a7ca2c2
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2020-02-29 12:35:46 +0000

    Bug 1374612 - CSP: Hide nonce values from the DOM. r=smaug,annevk
    Differential Revision: https://phabricator.services.mozilla.com/D62811

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -1118,6 +1118,14 @@ class Document : public nsINode,
   }
 
   /**
+   * Returns true if the document holds a CSP
+   * delivered through an HTTP Header.
+   */
+  bool GetHasCSPDeliveredThroughHeader() {
+    return mHasCSPDeliveredThroughHeader;
+  }
+
+  /**
    * Return a promise which resolves to the content blocking events.
    */
   typedef MozPromise<uint32_t, bool, true> GetContentBlockingEventsPromise;
@@ -4420,6 +4428,9 @@ class Document : public nsINode,
 
   // True if a document load has a CSP with unsafe-inline attached.
   bool mHasUnsafeInlineCSP : 1;
+
+  // True if the document has a CSP delivered throuh a header
+  bool mHasCSPDeliveredThroughHeader : 1;
 
   // True if DisallowBFCaching has been called on this document.
   bool mBFCacheDisallowed : 1;
