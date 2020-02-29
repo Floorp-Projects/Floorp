431e5f33f46b4bad6e46842784d3d67a557c4d7c
Tarek Ziadé <tarek@mozilla.com>
2020-02-29 14:06:28 +0000

    Bug 1618464 - merge heavy & async into full r=Bebe
    One single scenario with options
    
    Differential Revision: https://phabricator.services.mozilla.com/D64540

--- testing/condprofile/condprof/scenarii/__init__.py
+++ testing/condprofile/condprof/scenarii/__init__.py
@@ -1,9 +1,8 @@
 from __future__ import absolute_import
 
-from condprof.scenarii.heavy import heavy
+from condprof.scenarii.full import full
 from condprof.scenarii.settled import settled
-from condprof.scenarii.synced import synced
 from condprof.scenarii.settled2 import settled2
 
 
-scenarii = {"heavy": heavy, "settled": settled, "synced": synced, "settled2": settled2}
+scenarii = {"full": full, "settled": settled, "settled2": settled2}
