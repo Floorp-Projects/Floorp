c3dc0b0eb03854e5f2da61436c7f44c0710e4dea
Marco Zehe <mzehe@mozilla.com>
2020-03-01 19:35:06 +0000

    Bug 1608969 - Add new ARIA 1.3 comment role, r=Jamie
    This adds both the role as well as calculation of the posinset, setsize, and level calculation if not provided by the author.
    
    Differential Revision: https://phabricator.services.mozilla.com/D64590

--- accessible/generic/Accessible.cpp
+++ accessible/generic/Accessible.cpp
@@ -2648,6 +2648,18 @@ int32_t Accessible::GetLevelInternal() {
     } else {
       ++level;  // level is 1-index based
     }
+  } else if (role == roles::COMMENT) {
+    // For comments, count the ancestor elements with the same role to get the
+    // level.
+    level = 1;
+
+    Accessible* parent = this;
+    while ((parent = parent->Parent())) {
+      roles::Role parentRole = parent->Role();
+      if (parentRole == roles::COMMENT) {
+        ++level;
+      }
+    }
   }
 
   return level;
