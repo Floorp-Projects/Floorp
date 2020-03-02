27ce2465f1b2f422b0b0127c1a723b01420e0e1e
Cosmin Sabou <csabou@mozilla.com>
2020-03-02 22:46:06 +0200

    Backed out changeset 4c6c850d0fa1 (bug 1619245) for assertion failures on nsDisplayList.cpp.
    CLOSED TREE

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -4668,7 +4668,6 @@ class nsDisplayBackgroundImage : public nsDisplayImageContainer {
   nsRect GetBoundsInternal(nsDisplayListBuilder* aBuilder,
                            nsIFrame* aFrameForBounds = nullptr);
 
-  void AssociateImageIfNeeded();
   void DisassociateImage();
 
   void PaintInternal(nsDisplayListBuilder* aBuilder, gfxContext* aCtx,
@@ -4700,7 +4699,6 @@ class nsDisplayBackgroundImage : public nsDisplayImageContainer {
   bool mShouldFixToViewport;
   uint32_t mImageFlags;
   bool mAssociatedImage;
-  bool mTriedToAssociateImage;
 };
 
 enum class TableType : uint8_t {
