249b1bda1df4acabaefe10935777b0d0955918a2
Eric Rahm <erahm@mozilla.com>
2020-03-02 18:52:07 +0000

    Bug 1618753 Remove nsAutoPtr usage from modules. r=KrisWright
    Differential Revision: https://phabricator.services.mozilla.com/D64704
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libjar/nsZipArchive.h
+++ modules/libjar/nsZipArchive.h
@@ -14,7 +14,6 @@
 
 #include "zlib.h"
 #include "zipstruct.h"
-#include "nsAutoPtr.h"
 #include "nsIFile.h"
 #include "nsISupportsImpl.h"  // For mozilla::ThreadSafeAutoRefCnt
 #include "mozilla/ArenaAllocator.h"
@@ -403,7 +402,7 @@ class nsZipHandle final {
 
   PRFileMap* mMap; /* nspr datastructure for mmap */
   mozilla::AutoFDClose mNSPRFileDesc;
-  nsAutoPtr<nsZipItemPtr<uint8_t> > mBuf;
+  mozilla::UniquePtr<nsZipItemPtr<uint8_t> > mBuf;
   mozilla::ThreadSafeAutoRefCnt mRefCnt; /* ref count */
   NS_DECL_OWNINGTHREAD
 
