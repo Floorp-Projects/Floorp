cef73bf3e2f19a0c3e29f96403b044daa934e1ca
Pranav Pandey <pranavpandey1998official@gmail.com>
2020-03-02 12:51:22 +0000

    Bug 1619139 - fixed reach button toggle issue. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D64935

--- devtools/client/netmonitor/src/components/Toolbar.js
+++ devtools/client/netmonitor/src/components/Toolbar.js
@@ -619,7 +619,7 @@ module.exports = connect(
     recording: getRecordingState(state),
     requestFilterTypes: state.filters.requestFilterTypes,
     networkThrottling: state.networkThrottling,
-    networkActionBarOpen: state.search.panelOpen,
+    networkActionBarOpen: state.ui.networkActionOpen,
     networkActionBarSelectedPanel: state.ui.selectedActionBarTabId || "",
     selectedRequest: getSelectedRequest(state),
   }),
