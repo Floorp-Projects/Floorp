aeff352aca0267fe64d7ebd629453802b78ef189
Lina Cambridge <lina@yakshaving.ninja>
2020-03-03 22:57:39 +0000

    Bug 1613835 - Use a SQLite API call to check for in-progress transactions. r=mak
    Previously, `mozIStorageConnection#transactionInProgress` returned true
    only if a transaction was started via `beginTransaction()`. This meant
    that manually executing `BEGIN`, as `Sqlite.jsm` and the Rust bindings
    do, wouldn't accurately report if a transaction was in progress.
    Similarly, the flag wasn't accurate in cases where SQLite automatically
    rolled back a transaction.
    
    Fortunately, SQLite provides the `sqlite3_get_autocommit()` function,
    which we can use to determine if a transaction is open or not. This
    commit refactors the `transactionInProgress` getter, along with all
    `Connection` methods that depend on it, to use the SQLite API instead
    of managing that state on the connection. `mozStorageTransaction` and
    `Sqlite.jsm` still use their own flags to decide whether to commit
    their transactions, for reasons explained in the IDL comment.
    
    This commit also moves `transactionInProgress` to
    `mozIStorageAsyncConnection`, so that `Sqlite.jsm` can use it, and
    exposes it to Rust.
    
    Differential Revision: https://phabricator.services.mozilla.com/D63732

--- storage/mozStorageAsyncStatementExecution.cpp
+++ storage/mozStorageAsyncStatementExecution.cpp
@@ -319,16 +319,20 @@ nsresult AsyncExecuteStatements::notifyComplete() {
 
   // Handle our transaction, if we have one
   if (mHasTransaction) {
+    SQLiteMutexAutoLock lockedScope(mDBMutex);
     if (mState == COMPLETED) {
-      nsresult rv = mConnection->commitTransactionInternal(mNativeConnection);
+      nsresult rv = mConnection->commitTransactionInternal(lockedScope,
+                                                           mNativeConnection);
       if (NS_FAILED(rv)) {
         mState = ERROR;
+        // We cannot hold the DB mutex while calling notifyError.
+        SQLiteMutexAutoUnlock unlockedScope(mDBMutex);
         (void)notifyError(mozIStorageError::ERROR,
                           "Transaction failed to commit");
       }
     } else {
-      DebugOnly<nsresult> rv =
-          mConnection->rollbackTransactionInternal(mNativeConnection);
+      DebugOnly<nsresult> rv = mConnection->rollbackTransactionInternal(
+          lockedScope, mNativeConnection);
       NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Transaction failed to rollback");
     }
     mHasTransaction = false;
@@ -486,16 +490,20 @@ AsyncExecuteStatements::Run() {
   }
   if (mState == CANCELED) return notifyComplete();
 
-  if (statementsNeedTransaction() && mConnection->getAutocommit()) {
-    if (NS_SUCCEEDED(mConnection->beginTransactionInternal(
-            mNativeConnection, mozIStorageConnection::TRANSACTION_IMMEDIATE))) {
-      mHasTransaction = true;
-    }
+  if (statementsNeedTransaction()) {
+    SQLiteMutexAutoLock lockedScope(mDBMutex);
+    if (!mConnection->transactionInProgress(lockedScope)) {
+      if (NS_SUCCEEDED(mConnection->beginTransactionInternal(
+              lockedScope, mNativeConnection,
+              mozIStorageConnection::TRANSACTION_IMMEDIATE))) {
+        mHasTransaction = true;
+      }
 #ifdef DEBUG
-    else {
-      NS_WARNING("Unable to create a transaction for async execution.");
-    }
+      else {
+        NS_WARNING("Unable to create a transaction for async execution.");
+      }
 #endif
+    }
   }
 
   // Execute each statement, giving the callback results if it returns any.
