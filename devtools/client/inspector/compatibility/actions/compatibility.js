15debc84bf08afd9652d4a37be7c5d11d67ea5f3
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-03-04 06:31:01 +0000

    Bug 1615509: Move place where calculate the issues to the action. r=rcaliman
    To avoid storing declarationBlocks in reducer.
    The reason why I held the information is because had thought that want to update
    as soon as possible when change the target browser.
    However, as it seems that to update the target browser is not so frequently,
    remove the information to reduce the memory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D64305

--- devtools/client/inspector/compatibility/actions/compatibility.js
+++ devtools/client/inspector/compatibility/actions/compatibility.js
@@ -4,33 +4,41 @@
 
 "use strict";
 
+loader.lazyGetter(this, "mdnCompatibility", () => {
+  const MDNCompatibility = require("devtools/client/inspector/compatibility/lib/MDNCompatibility");
+  const cssPropertiesCompatData = require("devtools/client/inspector/compatibility/lib/dataset/css-properties.json");
+  return new MDNCompatibility(cssPropertiesCompatData);
+});
+
 const {
   COMPATIBILITY_UPDATE_SELECTED_NODE_START,
   COMPATIBILITY_UPDATE_SELECTED_NODE_SUCCESS,
   COMPATIBILITY_UPDATE_SELECTED_NODE_FAILURE,
   COMPATIBILITY_UPDATE_SELECTED_NODE_COMPLETE,
-  COMPATIBILITY_UPDATE_TARGET_BROWSERS,
+  COMPATIBILITY_UPDATE_SELECTED_NODE_ISSUES,
+  COMPATIBILITY_UPDATE_TARGET_BROWSERS_START,
+  COMPATIBILITY_UPDATE_TARGET_BROWSERS_SUCCESS,
+  COMPATIBILITY_UPDATE_TARGET_BROWSERS_FAILURE,
+  COMPATIBILITY_UPDATE_TARGET_BROWSERS_COMPLETE,
 } = require("devtools/client/inspector/compatibility/actions/index");
 
-function updateSelectedNode(nodeFront) {
-  return async ({ dispatch }) => {
+function updateSelectedNode(node) {
+  return async ({ dispatch, getState }) => {
     dispatch({ type: COMPATIBILITY_UPDATE_SELECTED_NODE_START });
 
     try {
-      const pageStyle = nodeFront.inspectorFront.pageStyle;
-      const styles = await pageStyle.getApplied(nodeFront, {
-        skipPseudo: false,
-      });
-      const declarationBlocks = styles
-        .map(({ rule }) => rule.declarations.filter(d => !d.commentOffsets))
-        .filter(declarations => declarations.length);
+      const { targetBrowsers } = getState().compatibility;
+      await _updateSelectedNodeIssues(node, targetBrowsers, dispatch);
 
       dispatch({
         type: COMPATIBILITY_UPDATE_SELECTED_NODE_SUCCESS,
-        declarationBlocks,
+        node,
       });
     } catch (error) {
-      dispatch({ type: COMPATIBILITY_UPDATE_SELECTED_NODE_FAILURE, error });
+      dispatch({
+        type: COMPATIBILITY_UPDATE_SELECTED_NODE_FAILURE,
+        error,
+      });
     }
 
     dispatch({ type: COMPATIBILITY_UPDATE_SELECTED_NODE_COMPLETE });
@@ -38,10 +46,63 @@ function updateSelectedNode(nodeFront) {
 }
 
 function updateTargetBrowsers(targetBrowsers) {
-  return {
-    type: COMPATIBILITY_UPDATE_TARGET_BROWSERS,
-    targetBrowsers,
+  return async ({ dispatch, getState }) => {
+    dispatch({ type: COMPATIBILITY_UPDATE_TARGET_BROWSERS_START });
+
+    try {
+      const { selectedNode } = getState().compatibility;
+
+      if (selectedNode) {
+        await _updateSelectedNodeIssues(selectedNode, targetBrowsers, dispatch);
+      }
+
+      dispatch({
+        type: COMPATIBILITY_UPDATE_TARGET_BROWSERS_SUCCESS,
+        targetBrowsers,
+      });
+    } catch (error) {
+      dispatch({ type: COMPATIBILITY_UPDATE_TARGET_BROWSERS_FAILURE, error });
+    }
+
+    dispatch({ type: COMPATIBILITY_UPDATE_TARGET_BROWSERS_COMPLETE });
   };
+}
+
+async function _getNodeIssues(node, targetBrowsers) {
+  const pageStyle = node.inspectorFront.pageStyle;
+  const styles = await pageStyle.getApplied(node, {
+    skipPseudo: false,
+  });
+
+  const declarationBlocks = styles
+    .map(({ rule }) => rule.declarations.filter(d => !d.commentOffsets))
+    .filter(declarations => declarations.length);
+
+  return declarationBlocks
+    .map(declarationBlock =>
+      mdnCompatibility.getCSSDeclarationBlockIssues(
+        declarationBlock,
+        targetBrowsers
+      )
+    )
+    .flat()
+    .reduce((issues, issue) => {
+      // Get rid of duplicate issue
+      return issues.find(
+        i => i.type === issue.type && i.property === issue.property
+      )
+        ? issues
+        : [...issues, issue];
+    }, []);
+}
+
+async function _updateSelectedNodeIssues(node, targetBrowsers, dispatch) {
+  const issues = await _getNodeIssues(node, targetBrowsers);
+
+  dispatch({
+    type: COMPATIBILITY_UPDATE_SELECTED_NODE_ISSUES,
+    issues,
+  });
 }
 
 module.exports = {
