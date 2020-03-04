41e6cbaf3de11e6c60c320d96e9fac8386b5c181
Micah Tigley <mtigley@mozilla.com>
2020-03-04 16:29:48 +0000

    Bug 1619478 - Ensure that actor methods are not being called before a remoteness change r=bradwerth
    Target switching support involves actors being destroyed during process flips. However, when RDM is closed due to a remoteness change, we are still trying to call methods on an actor that has already been destroyed. We need to ensure this doesn't happen by only calling these methods in `destroy` if RDM is being closed manually by the user.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65349

--- devtools/client/responsive/test/browser/browser_tab_remoteness_change.js
+++ devtools/client/responsive/test/browser/browser_tab_remoteness_change.js
@@ -16,53 +16,61 @@ const Types = require("devtools/client/responsive/types");
 
 const TEST_URL = "http://example.com/";
 
-add_task(async function() {
-  const tab = await addTab(TEST_URL);
+addRDMTask(
+  null,
+  async function() {
+    const tab = await addTab(TEST_URL);
 
-  const { ui } = await openRDM(tab);
-  const { store } = ui.toolWindow;
-  await waitUntilState(
-    store,
-    state =>
-      state.viewports.length == 1 &&
-      state.devices.listState == Types.loadableState.LOADED
-  );
-  const clientClosed = waitForClientClose(ui);
+    const { ui } = await openRDM(tab);
+    const { store } = ui.toolWindow;
+    await waitUntilState(
+      store,
+      state =>
+        state.viewports.length == 1 &&
+        state.devices.listState == Types.loadableState.LOADED
+    );
+    const clientClosed = waitForClientClose(ui);
 
-  closeRDM(tab, {
-    reason: "BeforeTabRemotenessChange",
-  });
+    closeRDM(tab, {
+      reason: "BeforeTabRemotenessChange",
+    });
 
-  // This flag is set at the end of `ResponsiveUI.destroy`.  If it is true
-  // without waiting for `closeRDM` above, then we must have closed
-  // synchronously.
-  is(ui.destroyed, true, "RDM closed synchronously");
+    // This flag is set at the end of `ResponsiveUI.destroy`.  If it is true
+    // without waiting for `closeRDM` above, then we must have closed
+    // synchronously.
+    is(ui.destroyed, true, "RDM closed synchronously");
 
-  await clientClosed;
-  await removeTab(tab);
-});
+    await clientClosed;
+    await removeTab(tab);
+  },
+  { usingBrowserUI: true, onlyPrefAndTask: true }
+);
 
-add_task(async function() {
-  const tab = await addTab(TEST_URL);
+addRDMTask(
+  null,
+  async function() {
+    const tab = await addTab(TEST_URL);
 
-  const { ui } = await openRDM(tab);
-  const { store } = ui.toolWindow;
-  await waitUntilState(
-    store,
-    state =>
-      state.viewports.length == 1 &&
-      state.devices.listState == Types.loadableState.LOADED
-  );
-  const clientClosed = waitForClientClose(ui);
+    const { ui } = await openRDM(tab);
+    const { store } = ui.toolWindow;
+    await waitUntilState(
+      store,
+      state =>
+        state.viewports.length == 1 &&
+        state.devices.listState == Types.loadableState.LOADED
+    );
+    const clientClosed = waitForClientClose(ui);
 
-  // Load URL that requires the main process, forcing a remoteness flip
-  await load(tab.linkedBrowser, "about:robots");
+    // Load URL that requires the main process, forcing a remoteness flip
+    await load(tab.linkedBrowser, "about:robots");
 
-  // This flag is set at the end of `ResponsiveUI.destroy`.  If it is true without
-  // waiting for `closeRDM` itself and only removing the tab, then we must have closed
-  // synchronously in response to tab closing.
-  is(ui.destroyed, true, "RDM closed synchronously");
+    // This flag is set at the end of `ResponsiveUI.destroy`.  If it is true without
+    // waiting for `closeRDM` itself and only removing the tab, then we must have closed
+    // synchronously in response to tab closing.
+    is(ui.destroyed, true, "RDM closed synchronously");
 
-  await clientClosed;
-  await removeTab(tab);
-});
+    await clientClosed;
+    await removeTab(tab);
+  },
+  { usingBrowserUI: true, onlyPrefAndTask: true }
+);
