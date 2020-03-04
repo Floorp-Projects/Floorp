3c31e7fef7cbe32460ba805351b10815eb06870c
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2020-03-04 07:26:42 +0000

    Bug 1503656 - Part 5. Get safe area insets from Gecko. r=emilio
    Add binding to get safe area insets from Gecko.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52509

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -494,6 +494,13 @@ class nsPresContext : public nsISupports,
     UpdateEffectiveTextZoom();
   }
 
+  /**
+   * Notify the pres context that the safe area insets have changed.
+   */
+  void SetSafeAreaInsets(const mozilla::ScreenIntMargin& aInsets);
+
+  mozilla::ScreenIntMargin GetSafeAreaInsets() const { return mSafeAreaInsets; }
+
  protected:
   void UpdateEffectiveTextZoom();
 
@@ -596,7 +603,7 @@ class nsPresContext : public nsISupports,
     return AppUnitsToIntCSSPixels(DevPixelsToAppUnits(aPixels));
   }
 
-  float DevPixelsToFloatCSSPixels(int32_t aPixels) {
+  float DevPixelsToFloatCSSPixels(int32_t aPixels) const {
     return AppUnitsToFloatCSSPixels(DevPixelsToAppUnits(aPixels));
   }
 
@@ -1210,6 +1217,8 @@ class nsPresContext : public nsISupports,
   // The maximum height of the dynamic toolbar on mobile.
   mozilla::ScreenIntCoord mDynamicToolbarMaxHeight;
   mozilla::ScreenIntCoord mDynamicToolbarHeight;
+  // Safe area insets support
+  mozilla::ScreenIntMargin mSafeAreaInsets;
   nsSize mPageSize;
   float mPageScale;
   float mPPScale;
