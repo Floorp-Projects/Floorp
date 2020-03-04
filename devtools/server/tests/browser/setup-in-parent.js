8ff8d9cd3659faec71030a717b863baa49d71541
Alexandre Poirot <poirot.alex@gmail.com>
2020-03-04 16:32:12 +0000

    Bug 1614494 - Refactor all DevTools test using mozbrowser iframes. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D65229

--- devtools/server/tests/browser/setup-in-parent.js
+++ devtools/server/tests/browser/setup-in-parent.js
@@ -6,4 +6,5 @@ var ChromeUtils = require("ChromeUtils");
 exports.setupParent = function({ mm, prefix }) {
   const args = [ChromeUtils.getClassName(mm) == "ChromeMessageSender", prefix];
   Services.obs.notifyObservers(null, "test:setupParent", JSON.stringify(args));
+  return {};
 };
