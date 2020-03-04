22b36f3377d06a44cdb1fa6cf6ec9134b7e9c1a8
Brad Werth <bwerth@mozilla.com>
2020-03-04 00:45:38 +0000

    Bug 1593756 Part 1: Add a new method GetBoxQuadsFromWindowOrigin. r=emilio,hiro
    Window space is not a sensible space for CSS points, as returned by
    getBoxQuads, since the CSS points are relative to window origin and not the
    layout origin. This new method is useful in paired calling patterns, where
    the returned quads are further translated by the position of the document
    node relative to the window origin. Later parts of this patch demonstrate
    this calling pattern in a test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52788

--- layout/base/GeometryUtils.cpp
+++ layout/base/GeometryUtils.cpp
@@ -15,6 +15,7 @@
 #include "mozilla/dom/DOMPoint.h"
 #include "mozilla/dom/DOMQuad.h"
 #include "mozilla/dom/DOMRect.h"
+#include "mozilla/dom/BrowserChild.h"
 #include "nsIFrame.h"
 #include "nsCSSFrameConstructor.h"
 #include "nsLayoutUtils.h"
@@ -299,6 +300,85 @@ void GetBoxQuads(nsINode* aNode, const dom::BoxQuadOptions& aOptions,
   AccumulateQuadCallback callback(ownerDoc, aResult, relativeToFrame,
                                   relativeToTopLeft, aOptions.mBox);
   nsLayoutUtils::GetAllInFlowBoxes(frame, &callback);
+}
+
+void GetBoxQuadsFromWindowOrigin(nsINode* aNode,
+                                 const dom::BoxQuadOptions& aOptions,
+                                 nsTArray<RefPtr<DOMQuad> >& aResult,
+                                 ErrorResult& aRv) {
+  // We want the quads relative to the window. To do this, we ignore the
+  // provided aOptions.mRelativeTo and instead use the document node of
+  // the top-most in-process document. Later, we'll check if there is a
+  // browserChild associated with that document, and if so, transform the
+  // calculated quads with the browserChild's to-parent matrix, which
+  // will get us to top-level coordinates.
+  if (aOptions.mRelativeTo.WasPassed()) {
+    return aRv.ThrowNotSupportedError(
+        "Can't request quads in window origin space relative to another "
+        "node.");
+  }
+
+  // We're going to call GetBoxQuads with our parameters, but we supply
+  // a new BoxQuadOptions object that uses the top in-process document
+  // as the relativeTo target.
+  BoxQuadOptions bqo(aOptions);
+
+  RefPtr<Document> topInProcessDoc =
+      nsContentUtils::GetRootDocument(aNode->OwnerDoc());
+
+  OwningGeometryNode ogn;
+  ogn.SetAsDocument() = topInProcessDoc;
+  bqo.mRelativeTo.Construct(ogn);
+
+  GetBoxQuads(aNode, bqo, aResult, CallerType::System, aRv);
+  if (aRv.Failed()) {
+    return;
+  }
+
+  // Now we have aResult filled with DOMQuads with values relative to the
+  // top in-process document. See if topInProcessDoc is associated with a
+  // BrowserChild, and if it is, get its transformation matrix and use that
+  // to transform the DOMQuads in place to make them relative to the window
+  // origin.
+  nsIDocShell* docShell = topInProcessDoc->GetDocShell();
+  if (!docShell) {
+    return aRv.ThrowInvalidStateError(
+        "Returning untranslated quads because top in process document has "
+        "no docshell.");
+  }
+
+  BrowserChild* browserChild = BrowserChild::GetFrom(docShell);
+  if (!browserChild) {
+    return;
+  }
+
+  nsPresContext* presContext = docShell->GetPresContext();
+  if (!presContext) {
+    return;
+  }
+  int32_t appUnitsPerDevPixel = presContext->AppUnitsPerDevPixel();
+
+  LayoutDeviceToLayoutDeviceMatrix4x4 matrix =
+      browserChild->GetChildToParentConversionMatrix();
+
+  // For each DOMQuad in aResult, change the css units into layer pixels,
+  // then transform them by matrix, then change them back into css units
+  // and overwrite the original points.
+  LayoutDeviceToCSSScale ld2cScale(appUnitsPerDevPixel);
+  CSSToLayoutDeviceScale c2ldScale = ld2cScale.Inverse();
+
+  for (auto& quad : aResult) {
+    for (uint32_t i = 0; i < 4; i++) {
+      DOMPoint* p = quad->Point(i);
+      CSSPoint cp(p->X(), p->Y());
+
+      LayoutDevicePoint windowLdp = matrix.TransformPoint(cp * c2ldScale);
+
+      CSSPoint windowCp = windowLdp * ld2cScale;
+      p->SetX(windowCp.x);
+      p->SetY(windowCp.y);
+    }
+  }
 }
 
 static void TransformPoints(nsINode* aTo, const GeometryNode& aFrom,
