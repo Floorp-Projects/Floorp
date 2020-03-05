2a31a3f4c812691229aea00efb386ec3cda2f31f
Masayuki Nakano <masayuki@d-toybox.com>
2020-03-05 04:39:35 +0000

    Bug 1618906 - Make `HTMLEditor` stop handling anything if container of a range in `Selection` is not a content node r=m_kato
    It's valid thing that a container of a `Range` of `Selection` is not a content
    node.  Actually, it can be a `Document` node.  But it's illegal case for
    editor.  So, if `HTMLEditor` meets such case, it does not need to do anything.
    
    This patch makes that if `HTMLEditor` meets the situation at very first time
    of public edit action method, it returns "OK" for avoiding new exception case.
    Otherwise, i.e., it's an XPCOM API or meeting such situation after a DOM
    mutation, returns error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65278

--- editor/libeditor/HTMLEditor.cpp
+++ editor/libeditor/HTMLEditor.cpp
@@ -985,13 +985,19 @@ nsresult HTMLEditor::InsertLineBreakAsAction(nsIPrincipal* aPrincipal) {
     return EditorBase::ToGenericNSResult(rv);
   }
 
+  if (IsSelectionRangeContainerNotContent()) {
+    return NS_SUCCESS_DOM_NO_OPERATION;
+  }
+
   // XXX This method may be called by "insertLineBreak" command.  So, using
   //     TypingTxnName here is odd in such case.
   AutoPlaceholderBatch treatAsOneTransaction(*this, *nsGkAtoms::TypingTxnName);
   rv = InsertBrElementAtSelectionWithTransaction();
   NS_WARNING_ASSERTION(NS_SUCCEEDED(rv),
                        "InsertBrElementAtSelectionWithTransaction() failed");
-  return rv;
+  // Don't return NS_SUCCESS_DOM_NO_OPERATION for compatibility of `execCommand`
+  // result of Chrome.
+  return NS_FAILED(rv) ? rv : NS_OK;
 }
 
 nsresult HTMLEditor::InsertParagraphSeparatorAsAction(
@@ -1110,6 +1116,7 @@ EditActionResult HTMLEditor::HandleTabKeyPressInTable(
 
 nsresult HTMLEditor::InsertBrElementAtSelectionWithTransaction() {
   MOZ_ASSERT(IsEditActionDataAvailable());
+  MOZ_ASSERT(!IsSelectionRangeContainerNotContent());
 
   // calling it text insertion to trigger moz br treatment by rules
   // XXX Why do we use EditSubAction::eInsertText here?  Looks like
@@ -1847,6 +1854,10 @@ HTMLEditor::GetParagraphState(bool* aMixed, nsAString& aFirstParagraphState) {
     return NS_ERROR_NOT_INITIALIZED;
   }
 
+  if (NS_WARN_IF(IsSelectionRangeContainerNotContent())) {
+    return NS_ERROR_FAILURE;
+  }
+
   ErrorResult error;
   ParagraphStateAtSelection state(*this, error);
   if (NS_WARN_IF(error.Failed())) {
@@ -2215,6 +2226,10 @@ nsresult HTMLEditor::FormatBlockContainerAsSubAction(nsAtom& aTagName) {
     return result.Rv();
   }
 
+  if (IsSelectionRangeContainerNotContent()) {
+    return NS_SUCCESS_DOM_NO_OPERATION;
+  }
+
   nsresult rv = EnsureNoPaddingBRElementForEmptyEditor();
   if (NS_WARN_IF(rv == NS_ERROR_EDITOR_DESTROYED)) {
     return NS_ERROR_EDITOR_DESTROYED;
@@ -2321,12 +2336,18 @@ Element* HTMLEditor::GetElementOrParentByTagName(const nsAtom& aTagName,
   if (aNode) {
     return GetElementOrParentByTagNameInternal(aTagName, *aNode);
   }
+
+  if (IsSelectionRangeContainerNotContent()) {
+    return nullptr;
+  }
+
   return GetElementOrParentByTagNameAtSelection(aTagName);
 }
 
 Element* HTMLEditor::GetElementOrParentByTagNameAtSelection(
     const nsAtom& aTagName) const {
   MOZ_ASSERT(IsEditActionDataAvailable());
+  MOZ_ASSERT(!IsSelectionRangeContainerNotContent());
 
   MOZ_ASSERT(&aTagName != nsGkAtoms::_empty);
 
