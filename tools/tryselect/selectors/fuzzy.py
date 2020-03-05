5f78eacf9b3ec149762d17cf4cee6d86112b4b51
Simon Fraser <sfraser@mozilla.com>
2020-03-05 19:58:43 +0000

    Bug 1619554 Improve performance of |mach try fuzzy| preview r=ahal
    Remove use of requests module in preview pane
    
    Reformat task duration data to avoid reprocessing in preview pane
    
    Avoid loading task durations json more than once.
    
    Increase required fzf version, use temporary file instead of arglist
    
    Differential Revision: https://phabricator.services.mozilla.com/D65094

--- tools/tryselect/selectors/fuzzy.py
+++ tools/tryselect/selectors/fuzzy.py
@@ -20,7 +20,7 @@ from mozterm import Terminal
 from ..cli import BaseTryParser
 from ..tasks import generate_tasks, filter_tasks_by_paths
 from ..push import check_working_directory, push_to_try, generate_try_task_config
-from ..util.estimates import download_task_history_data, make_trimmed_taskgraph_cache
+from ..util.manage_estimates import download_task_history_data, make_trimmed_taskgraph_cache
 
 terminal = Terminal()
 
@@ -61,7 +61,7 @@ editor integrations, download the appropriate binary and put it on your $PATH:
 FZF_VERSION_FAILED = """
 Could not obtain the 'fzf' version.
 
-The 'mach try fuzzy' command depends on fzf, and requires version > 0.18.0
+The 'mach try fuzzy' command depends on fzf, and requires version > 0.20.0
 for some of the features. Please install it following the appropriate
 instructions for your platform:
 
@@ -202,9 +202,9 @@ def should_force_fzf_update(fzf_bin):
     # Some fzf versions have extra, e.g 0.18.0 (ff95134)
     fzf_version = fzf_version.split()[0]
 
-    # 0.18.0 introduced FZF_PREVIEW_COLUMNS as an env variable
-    # in preview subprocesses, which is a feature we use.
-    if StrictVersion(fzf_version) < StrictVersion('0.18.0'):
+    # 0.20.0 introduced passing selections through a temporary file,
+    # which is good for large ctrl-a actions.
+    if StrictVersion(fzf_version) < StrictVersion('0.20.0'):
         print("fzf version is old, forcing update.")
         return True
     return False
@@ -341,12 +341,12 @@ def run(update=False, query=None, intersect_query=None, try_config=None, full=Fa
 
     if show_estimates:
         base_cmd.extend([
-            '--preview', 'python {} -g {} -s -c {} "{{+}}"'.format(
+            '--preview', 'python {} -g {} -s -c {} -t "{{+f}}"'.format(
                 PREVIEW_SCRIPT, dep_cache, cache_dir),
         ])
     else:
         base_cmd.extend([
-            '--preview', 'python {} "{{+}}"'.format(PREVIEW_SCRIPT),
+            '--preview', 'python {} -t "{{+f}}"'.format(PREVIEW_SCRIPT),
         ])
 
     if exact:
