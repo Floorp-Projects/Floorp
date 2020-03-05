ca5053868b4725b97844a16aa819ec67bfb1eb6d
Ciure Andrei <aciure@mozilla.com>
2020-03-05 13:08:02 +0200

    Backed out 3 changesets (bug 1620087) for causing browser/browser_console_eager_eval.js failures CLOSED TREE
    Backed out changeset 9e09d30270d6 (bug 1620087)
    Backed out changeset a6efc9fb5e12 (bug 1620087)
    Backed out changeset 62d4008a089a (bug 1620087)

--- devtools/server/actors/webconsole/eval-with-debugger.js
+++ devtools/server/actors/webconsole/eval-with-debugger.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/* global BigInt */
 
 "use strict";
 
@@ -37,11 +38,6 @@ loader.lazyRequireGetter(
   "LongStringActor",
   "devtools/server/actors/string",
   true
-);
-loader.lazyRequireGetter(
-  this,
-  "eagerEcmaWhitelist",
-  "devtools/server/actors/webconsole/eager-ecma-whitelist"
 );
 loader.lazyRequireGetter(
   this,
@@ -276,22 +272,17 @@ function preventSideEffects(dbg) {
     throw new Error("Debugger has hook installed");
   }
 
-  // We ensure that the metadata for native functions is loaded before we
-  // initialize sideeffect-prevention because the data is lazy-loaded, and this
-  // logic can run inside of debuggee compartments because the
-  // "addAllGlobalsAsDebuggees" considers the vast majority of realms
-  // valid debuggees. Without this, eager-eval runs the risk of failing
-  // because building the list of valid native functions is itself a
-  // side-effectful operation because it needs to populate a
-  // module cache, among any number of other things.
-  ensureSideEffectFreeNatives();
-
   // Note: It is critical for debuggee performance that we implement all of
   // this debuggee tracking logic with a separate Debugger instance.
   // Bug 1617666 arises otherwise if we set an onEnterFrame hook on the
   // existing debugger object and then later clear it.
   const newDbg = new Debugger();
-  newDbg.addAllGlobalsAsDebuggees();
+  for (const debuggee of dbg.getDebuggees()) {
+    newDbg.addDebuggee(debuggee.unsafeDereference());
+  }
+
+  // TODO: re-enable addAllGlobalsAsDebuggees(bug #1610532)
+  // newDbg.addAllGlobalsAsDebuggees();
 
   const timeoutDuration = 100;
   const endTime = Date.now() + timeoutDuration;
@@ -338,21 +329,14 @@ function preventSideEffects(dbg) {
   // explicitly calling eval, so we need to add this hook on "dbg" even though
   // the rest of our hooks work via "newDbg".
   dbg.onNativeCall = (callee, reason) => {
-    try {
-      // Getters are never considered effectful, and setters are always effectful.
-      // Natives called normally are handled with a whitelist.
-      if (
-        reason == "get" ||
-        (reason == "call" && nativeHasNoSideEffects(callee))
-      ) {
-        // Returning undefined causes execution to continue normally.
-        return undefined;
-      }
-    } catch (err) {
-      DevToolsUtils.reportException(
-        "evalWithDebugger onNativeCall",
-        new Error("Unable to validate native function against whitelist")
-      );
+    // Getters are never considered effectful, and setters are always effectful.
+    // Natives called normally are handled with a whitelist.
+    if (
+      reason == "get" ||
+      (reason == "call" && nativeHasNoSideEffects(callee))
+    ) {
+      // Returning undefined causes execution to continue normally.
+      return undefined;
     }
     // Returning null terminates the current evaluation.
     return null;
@@ -378,7 +362,141 @@ function ensureSideEffectFreeNatives() {
   }
 
   const natives = [
-    ...eagerEcmaWhitelist,
+    Array,
+    Array.from,
+    Array.isArray,
+    Array.of,
+    Array.prototype.concat,
+    Array.prototype.entries,
+    Array.prototype.every,
+    Array.prototype.fill,
+    Array.prototype.filter,
+    Array.prototype.find,
+    Array.prototype.findIndex,
+    Array.prototype.flat,
+    Array.prototype.flatMap,
+    Array.prototype.forEach,
+    Array.prototype.includes,
+    Array.prototype.indexOf,
+    Array.prototype.join,
+    Array.prototype.keys,
+    Array.prototype.lastIndexOf,
+    Array.prototype.map,
+    Array.prototype.reduce,
+    Array.prototype.reduceRight,
+    Array.prototype.slice,
+    Array.prototype.some,
+    Array.prototype.values,
+    ArrayBuffer,
+    ArrayBuffer.isView,
+    ArrayBuffer.prototype.slice,
+    BigInt,
+    ...allProperties(BigInt),
+    Boolean,
+    DataView,
+    Date,
+    Date.now,
+    Date.parse,
+    Date.UTC,
+    ...matchingProperties(Date.prototype, /^get/),
+    ...matchingProperties(Date.prototype, /^to.*?String$/),
+    Error,
+    Function,
+    Function.prototype.apply,
+    Function.prototype.bind,
+    Function.prototype.call,
+    Int8Array,
+    Uint8Array,
+    Uint8ClampedArray,
+    Int16Array,
+    Uint16Array,
+    Int32Array,
+    Uint32Array,
+    Float32Array,
+    Float64Array,
+    // These will apply to other typed array prototypes.
+    Int8Array.prototype.entries,
+    Int8Array.prototype.every,
+    Int8Array.prototype.filter,
+    Int8Array.prototype.find,
+    Int8Array.prototype.findIndex,
+    Int8Array.prototype.forEach,
+    Int8Array.prototype.indexOf,
+    Int8Array.prototype.includes,
+    Int8Array.prototype.join,
+    Int8Array.prototype.keys,
+    Int8Array.prototype.lastIndexOf,
+    Int8Array.prototype.map,
+    Int8Array.prototype.reduce,
+    Int8Array.prototype.reduceRight,
+    Int8Array.prototype.slice,
+    Int8Array.prototype.some,
+    Int8Array.prototype.subarray,
+    Int8Array.prototype.values,
+    ...allProperties(JSON),
+    Map,
+    Map.prototype.forEach,
+    Map.prototype.get,
+    Map.prototype.has,
+    Map.prototype.entries,
+    Map.prototype.keys,
+    Map.prototype.values,
+    ...allProperties(Math),
+    Number,
+    ...allProperties(Number),
+    ...allProperties(Number.prototype),
+    Object,
+    Object.create,
+    Object.keys,
+    Object.entries,
+    Object.getOwnPropertyDescriptor,
+    Object.getOwnPropertyDescriptors,
+    Object.getOwnPropertyNames,
+    Object.getOwnPropertySymbols,
+    Object.getPrototypeOf,
+    Object.is,
+    Object.isExtensible,
+    Object.isFrozen,
+    Object.isSealed,
+    Object.values,
+    Object.prototype.hasOwnProperty,
+    Object.prototype.isPrototypeOf,
+    Proxy,
+    Proxy.revocable,
+    Reflect.apply,
+    Reflect.construct,
+    Reflect.get,
+    Reflect.getOwnPropertyDescriptor,
+    Reflect.getPrototypeOf,
+    Reflect.has,
+    Reflect.isExtensible,
+    Reflect.ownKeys,
+    RegExp,
+    RegExp.prototype.exec,
+    RegExp.prototype.test,
+    Set,
+    Set.prototype.entries,
+    Set.prototype.forEach,
+    Set.prototype.has,
+    Set.prototype.values,
+    String,
+    ...allProperties(String),
+    ...allProperties(String.prototype),
+    Symbol,
+    Symbol.keyFor,
+    WeakMap,
+    WeakMap.prototype.get,
+    WeakMap.prototype.has,
+    WeakSet,
+    WeakSet.prototype.has,
+    decodeURI,
+    decodeURIComponent,
+    encodeURI,
+    encodeURIComponent,
+    escape,
+    isFinite,
+    isNaN,
+    unescape,
 
     // Pull in all of the non-ECMAScript native functions that we want to
     // whitelist as well.
@@ -394,6 +512,17 @@ function ensureSideEffectFreeNatives() {
   }
 
   gSideEffectFreeNatives = map;
+
+  function matchingProperties(obj, regexp) {
+    return Object.getOwnPropertyNames(obj)
+      .filter(n => regexp.test(n))
+      .map(n => obj[n])
+      .filter(v => typeof v == "function");
+  }
+
+  function allProperties(obj) {
+    return matchingProperties(obj, /./);
+  }
 }
 
 function nativeHasNoSideEffects(fn) {
@@ -408,6 +537,8 @@ function nativeHasNoSideEffects(fn) {
     case "valueOf":
       return true;
   }
+
+  ensureSideEffectFreeNatives();
 
   const natives = gSideEffectFreeNatives.get(fn.name);
   return natives && natives.some(n => fn.isSameNative(n));
