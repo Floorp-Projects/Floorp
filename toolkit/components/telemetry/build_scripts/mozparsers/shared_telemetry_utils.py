0db2ec47fd5bdb9065bd35d464a113325949dae6
Mike Shal <mshal@mozilla.com>
2020-03-06 16:02:50 +0000

    Bug 1620140 - Convert gen_histogram_data.py to py3; r=firefox-build-system-reviewers,rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D65457

--- toolkit/components/telemetry/build_scripts/mozparsers/shared_telemetry_utils.py
+++ toolkit/components/telemetry/build_scripts/mozparsers/shared_telemetry_utils.py
@@ -84,13 +84,13 @@ class ParserError(Exception):
 
     def handle_now(self):
         ParserError.print_eventuals()
-        print(self.message, file=sys.stderr)
+        print(str(self), file=sys.stderr)
         sys.exit(1)
 
     @classmethod
     def print_eventuals(cls):
         while cls.eventual_errors:
-            print(cls.eventual_errors.pop(0).message, file=sys.stderr)
+            print(str(cls.eventual_errors.pop(0)), file=sys.stderr)
 
     @classmethod
     def exit_func(cls):
@@ -175,7 +175,6 @@ class StringTable:
         :param name: the name of the output array.
         """
         entries = self.table.items()
-        entries.sort(key=lambda x: x[1])
 
         # Avoid null-in-string warnings with GCC and potentially
         # overlong string constants; write everything out the long way.
@@ -192,7 +191,7 @@ class StringTable:
         f.write("#else\n")
         f.write("constexpr char %s[] = {\n" % name)
         f.write("#endif\n")
-        for (string, offset) in entries:
+        for (string, offset) in sorted(entries, key=lambda x: x[1]):
             if "*/" in string:
                 raise ValueError("String in string table contains unexpected sequence '*/': %s" %
                                  string)
