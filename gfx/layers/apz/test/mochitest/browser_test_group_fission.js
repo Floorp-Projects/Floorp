d6a09a89b6a678a6618c5b69c65f7a1c3e4c1918
Kartikaya Gupta <kgupta@mozilla.com>
2020-03-06 17:28:23 +0000

    Bug 1613514 - Increase the timeout for the APZ fission tests. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D65558

--- gfx/layers/apz/test/mochitest/browser_test_group_fission.js
+++ gfx/layers/apz/test/mochitest/browser_test_group_fission.js
@@ -58,6 +58,10 @@ add_task(async function test_main() {
     ]);
   }
 
+  // ccov builds run slower and need longer, so let's scale up the timeout
+  // by the number of tests we're running.
+  requestLongerTimeout(subtests.length);
+
   let fissionWindow = await BrowserTestUtils.openNewBrowserWindow({
     fission: true,
   });
