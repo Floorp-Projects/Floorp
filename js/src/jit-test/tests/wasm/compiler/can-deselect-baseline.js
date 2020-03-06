0990678aede1c3c8c5c9f8ea2bcbc3cf748079fa
shindli <shindli@mozilla.com>
2020-03-06 14:54:43 +0200

    Backed out changeset 59b6e23c9926 (bug 1566427) for spidermonkey bustage CLOSED TREE

--- js/src/jit-test/tests/wasm/compiler/can-deselect-baseline.js
+++
@@ -1,8 +0,0 @@
-// |jit-test| --wasm-compiler=ion
-
-if (wasmCompilersPresent().match("ion")) {
-    assertEq(wasmCompileMode(), "ion");
-} else {
-    // No ion on arm64, so asking for ion-only disables everything.
-    assertEq(wasmCompileMode(), "none");
-}
