5c05d331c327d4aec120f8c14f33d9e0df94ffcc
Aaron Klotz <aklotz@mozilla.com>
2020-03-09 19:22:40 +0000

    Bug 1608301: Part 1 - Add an overload to ContextUtils.getCurrentPackageInfo that accepts flags; r=geckoview-reviewers,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D65634

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/ContextUtils.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/ContextUtils.java
@@ -20,15 +20,16 @@ public class ContextUtils {
 
     private ContextUtils() {}
 
-    /**
-     * @return {@link android.content.pm.PackageInfo#firstInstallTime} for the context's package.
-     */
-    public static PackageInfo getCurrentPackageInfo(final Context context) {
+    public static PackageInfo getCurrentPackageInfo(final Context context, final int flags) {
         try {
-            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), flags);
         } catch (PackageManager.NameNotFoundException e) {
             throw new AssertionError("Should not happen: Can't get package info of own package");
         }
+    }
+
+    public static PackageInfo getCurrentPackageInfo(final Context context) {
+        return getCurrentPackageInfo(context, 0);
     }
 
     public static boolean isPackageInstalled(final Context context, final String packageName) {
