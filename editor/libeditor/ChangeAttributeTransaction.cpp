c94ebc35d318870e54b4bf9ef1efbc2aaf336a32
Masayuki Nakano <masayuki@d-toybox.com>
2020-03-09 14:57:23 +0000

    Bug 1620504 - part 5: Clean up warnings in ChangeAttributeTransaction r=m_kato
    Depends on D65870
    
    Differential Revision: https://phabricator.services.mozilla.com/D65871

--- editor/libeditor/ChangeAttributeTransaction.cpp
+++ editor/libeditor/ChangeAttributeTransaction.cpp
@@ -49,8 +49,7 @@ NS_IMPL_RELEASE_INHERITED(ChangeAttributeTransaction, EditTransactionBase)
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ChangeAttributeTransaction)
 NS_INTERFACE_MAP_END_INHERITING(EditTransactionBase)
 
-NS_IMETHODIMP
-ChangeAttributeTransaction::DoTransaction() {
+NS_IMETHODIMP ChangeAttributeTransaction::DoTransaction() {
   // Need to get the current value of the attribute and save it, and set
   // mAttributeWasSet
   mAttributeWasSet =
@@ -64,27 +63,38 @@ ChangeAttributeTransaction::DoTransaction() {
 
   // Now set the attribute to the new value
   if (mRemoveAttribute) {
-    return mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+    nsresult rv = mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+    NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::UnsetAttr() failed");
+    return rv;
   }
 
-  return mElement->SetAttr(kNameSpaceID_None, mAttribute, mValue, true);
+  nsresult rv = mElement->SetAttr(kNameSpaceID_None, mAttribute, mValue, true);
+  NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::SetAttr() failed");
+  return rv;
 }
 
-NS_IMETHODIMP
-ChangeAttributeTransaction::UndoTransaction() {
+NS_IMETHODIMP ChangeAttributeTransaction::UndoTransaction() {
   if (mAttributeWasSet) {
-    return mElement->SetAttr(kNameSpaceID_None, mAttribute, mUndoValue, true);
+    nsresult rv =
+        mElement->SetAttr(kNameSpaceID_None, mAttribute, mUndoValue, true);
+    NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::SetAttr() failed");
+    return rv;
   }
-  return mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+  nsresult rv = mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+  NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::UnsetAttr() failed");
+  return rv;
 }
 
-NS_IMETHODIMP
-ChangeAttributeTransaction::RedoTransaction() {
+NS_IMETHODIMP ChangeAttributeTransaction::RedoTransaction() {
   if (mRemoveAttribute) {
-    return mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+    nsresult rv = mElement->UnsetAttr(kNameSpaceID_None, mAttribute, true);
+    NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::UnsetAttr() failed");
+    return rv;
   }
 
-  return mElement->SetAttr(kNameSpaceID_None, mAttribute, mValue, true);
+  nsresult rv = mElement->SetAttr(kNameSpaceID_None, mAttribute, mValue, true);
+  NS_WARNING_ASSERTION(NS_SUCCEEDED(rv), "Element::SetAttr() failed");
+  return rv;
 }
 
 }  // namespace mozilla
