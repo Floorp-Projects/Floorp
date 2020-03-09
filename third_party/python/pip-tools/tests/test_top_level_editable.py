e291c90c546ba5202c860dfea58d6fa53e04150c
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-03-09 13:23:08 +0000

    Bug 1602271 - Update vendored copy of 'pip-tools' to 4.5.1, r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D64755

--- third_party/python/pip-tools/tests/test_top_level_editable.py
+++ third_party/python/pip-tools/tests/test_top_level_editable.py
@@ -2,7 +2,8 @@ import os
 
 import pytest
 
-from piptools.pip import get_pip_command
+from .constants import PACKAGES_PATH
+
 from piptools.repositories import PyPIRepository
 
 
@@ -16,42 +17,22 @@ class MockedPyPIRepository(PyPIRepository):
         return super(MockedPyPIRepository, self).get_dependencies(ireq)
 
 
-def _get_repository():
-    pip_command = get_pip_command()
-    pip_args = []
-    pip_options, _ = pip_command.parse_args(pip_args)
-    session = pip_command._build_session(pip_options)
-    repository = MockedPyPIRepository(pip_options, session)
-    return repository
+@pytest.fixture
+def mocked_repository(tmpdir):
+    return MockedPyPIRepository(["--no-index"], cache_dir=str(tmpdir / "pypi-repo"))
 
 
-@pytest.mark.parametrize(
-    ("input", "expected"),
-    (
-        (tup)
-        for tup in [
-            (
-                [
-                    os.path.join(
-                        os.path.dirname(__file__), "test_data", "small_fake_package"
-                    )
-                ],
-                ["six"],
-            )
-        ]
-    ),
-)
 def test_editable_top_level_deps_preserved(
-    base_resolver, repository, from_editable, input, expected
+    base_resolver, mocked_repository, from_editable
 ):
-    input = [from_editable(line) for line in input]
-    repository = _get_repository()
-    output = base_resolver(input, prereleases=False, repository=repository).resolve()
+    package_path = os.path.join(PACKAGES_PATH, "small_fake_with_deps")
+    ireqs = [from_editable(package_path)]
+    output = base_resolver(
+        ireqs, prereleases=False, repository=mocked_repository
+    ).resolve()
 
     output = {p.name for p in output}
 
     # sanity check that we're expecting something
     assert output != set()
-
-    for package_name in expected:
-        assert package_name in output
+    assert "small-fake-a" in output
