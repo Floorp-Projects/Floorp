16119e010d81ce69f80cb32cb0a232f5ab967b16
Tom Schuster <evilpies@gmail.com>
2020-03-10 22:49:47 +0000

    Bug 1619177 - Do not use extra warnings for f.arguments or f.caller. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D65651
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/JSFunction.cpp
+++ js/src/vm/JSFunction.cpp
@@ -169,14 +169,6 @@ static bool ArgumentsRestrictions(JSContext* cx, HandleFunction fun) {
     return false;
   }
 
-  // Otherwise emit a strict warning about |f.arguments| to discourage use of
-  // this non-standard, performance-harmful feature.
-  if (!JS_ReportErrorFlagsAndNumberASCII(
-          cx, JSREPORT_WARNING | JSREPORT_STRICT, GetErrorMessage, nullptr,
-          JSMSG_DEPRECATED_USAGE, js_arguments_str)) {
-    return false;
-  }
-
   return true;
 }
 
@@ -248,14 +240,6 @@ static bool CallerRestrictions(JSContext* cx, HandleFunction fun) {
   // pairings of callee/caller.
   if (!IsSloppyNormalFunction(fun)) {
     ThrowTypeErrorBehavior(cx);
-    return false;
-  }
-
-  // Otherwise emit a strict warning about |f.caller| to discourage use of
-  // this non-standard, performance-harmful feature.
-  if (!JS_ReportErrorFlagsAndNumberASCII(
-          cx, JSREPORT_WARNING | JSREPORT_STRICT, GetErrorMessage, nullptr,
-          JSMSG_DEPRECATED_USAGE, js_caller_str)) {
     return false;
   }
 
