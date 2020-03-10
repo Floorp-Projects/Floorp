1c5c5caea7932852e9f3f283adcc24d43703c450
Sylvestre Ledru <sledru@mozilla.com>
2020-03-10 09:27:56 +0000

    Bug 1617369 - Reformat a rust test using rustfmt r=marco
    Depends on D65902
    
    Differential Revision: https://phabricator.services.mozilla.com/D65903
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/gtest/hello.rs
+++ dom/media/gtest/hello.rs
@@ -1,5 +1,5 @@
 #[no_mangle]
-pub extern fn test_rust() -> *const u8 {
+pub extern "C" fn test_rust() -> *const u8 {
     // NB: rust &str aren't null terminated.
     let greeting = "hello from rust.\0";
     greeting.as_ptr()
