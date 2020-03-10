24f65774be0bd8092a92eea80999b0c4494d7577
Valentin Gosu <valentin.gosu@gmail.com>
2020-03-10 19:04:43 +0000

    Bug 1611651 - Add pref for stale-while-revalidate r=dragana a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D65813

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -10571,6 +10571,10 @@ NS_IMPL_ISUPPORTS(BackgroundRevalidatingListener, nsIStreamListener,
 }  // namespace
 
 void nsHttpChannel::PerformBackgroundCacheRevalidation() {
+  if (!StaticPrefs::network_http_stale_while_revalidate_enabled()) {
+    return;
+  }
+
   LOG(("nsHttpChannel::PerformBackgroundCacheRevalidation %p", this));
 
   Unused << NS_DispatchToMainThreadQueue(
