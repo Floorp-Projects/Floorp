d2990408514f4f7020ff289759edae876c6c1d30
Cosmin Sabou <csabou@mozilla.com>
2020-03-10 18:24:56 +0200

    Backed out 6 changesets (bug 1608302, bug 1608301) for linting failure on GeckoProcessManager.java.
    CLOSED TREE
    
    Backed out changeset 5a78c81ddaa7 (bug 1608302)
    Backed out changeset 81b75065d43d (bug 1608302)
    Backed out changeset e75565cd4103 (bug 1608302)
    Backed out changeset dba5330a46dc (bug 1608301)
    Backed out changeset 7b9e2ba30ff2 (bug 1608301)
    Backed out changeset 3a2342e9440f (bug 1608301)

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/XPCOMEventTarget.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/XPCOMEventTarget.java
@@ -39,13 +39,7 @@ public final class XPCOMEventTarget extends JNIObject implements IXPCOMEventTarg
 
     public static void assertOnLauncherThread() {
         if (BuildConfig.DEBUG && !launcherThread().isOnCurrentThread()) {
-            throw new AssertionError("Expected to be running on XPCOM launcher thread");
-        }
-    }
-
-    public static void assertNotOnLauncherThread() {
-        if (BuildConfig.DEBUG && launcherThread().isOnCurrentThread()) {
-            throw new AssertionError("Expected to not be running on XPCOM launcher thread");
+            throw new IllegalThreadStateException("Expected to be running on XPCOM launcher thread");
         }
     }
 
