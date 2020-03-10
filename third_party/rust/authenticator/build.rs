b434ac8c64a1536b552f776abf0e01c4aec78c33
J.C. Jones <jjones@mozilla.com>
2020-03-10 19:28:27 +0000

    Bug 1621346 - Update Authenticator-rs to 0.2.10 to add MIPS64 and S390X support r=kjacobs
    Differential Revision: https://phabricator.services.mozilla.com/D66267

--- third_party/rust/authenticator/build.rs
+++ third_party/rust/authenticator/build.rs
@@ -25,6 +25,8 @@ fn main() {
         "ioctl_mipsbe.rs"
     } else if cfg!(all(target_arch = "mips", target_endian = "little")) {
         "ioctl_mipsle.rs"
+    } else if cfg!(all(target_arch = "mips64", target_endian = "little")) {
+        "ioctl_mips64le.rs"
     } else if cfg!(all(target_arch = "powerpc", target_endian = "little")) {
         "ioctl_powerpcle.rs"
     } else if cfg!(all(target_arch = "powerpc", target_endian = "big")) {
@@ -41,6 +43,8 @@ fn main() {
         "ioctl_aarch64le.rs"
     } else if cfg!(all(target_arch = "aarch64", target_endian = "big")) {
         "ioctl_aarch64be.rs"
+    } else if cfg!(all(target_arch = "s390x", target_endian = "big")) {
+        "ioctl_s390xbe.rs"
     } else {
         panic!("architecture not supported");
     };
