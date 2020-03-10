b9f445db8673a6e6a105aa3a85df6bbae0128ae1
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-03-10 01:20:45 +0000

    Bug 1615509: Update test that changes style on rule view. r=mtigley
    Differential Revision: https://phabricator.services.mozilla.com/D64533
    
    --HG--
    extra : moz-landing-system : lando

--- devtools/client/inspector/compatibility/test/browser/browser_compatibility_event_rule-change.js
+++ devtools/client/inspector/compatibility/test/browser/browser_compatibility_event_rule-change.js
@@ -6,7 +6,92 @@
 // Test whether the content of the issue list will be changed when the rules are changed
 // on the rule view.
 
-const TEST_URI = `<div style="border-block-color: lime;"></div>`;
+const TEST_URI = `
+  <style>
+  .test-class {
+    border-block-color: lime;
+  }
+  div {
+    margin-inline-start: 5px;
+  }
+  </style>
+  <div class="test-class">test class</div>
+  <div>test</div>
+`;
+
+const TEST_DATA_SELECTED = {
+  fullRule: {
+    expectedProperties: [
+      { property: "border-block-color" },
+      { property: "margin-inline-start" },
+    ],
+    expectedNodes: [
+      {
+        property: "border-block-color",
+        nodes: [],
+      },
+      {
+        property: "margin-inline-start",
+        nodes: [],
+      },
+    ],
+  },
+  classRule: {
+    expectedProperties: [{ property: "border-block-color" }],
+    expectedNodes: [
+      {
+        property: "border-block-color",
+        nodes: [],
+      },
+    ],
+  },
+  elementRule: {
+    expectedProperties: [{ property: "margin-inline-start" }],
+    expectedNodes: [
+      {
+        property: "margin-inline-start",
+        nodes: [],
+      },
+    ],
+  },
+};
+
+const TEST_DATA_ALL = {
+  fullRule: {
+    expectedProperties: [
+      { property: "border-block-color" },
+      { property: "margin-inline-start" },
+    ],
+    expectedNodes: [
+      {
+        property: "border-block-color",
+        nodes: ["div.test-class"],
+      },
+      {
+        property: "margin-inline-start",
+        nodes: ["div.test-class", "div"],
+      },
+    ],
+  },
+  classRule: {
+    expectedProperties: [{ property: "border-block-color" }],
+    expectedNodes: [
+      {
+        property: "border-block-color",
+        nodes: ["div.test-class"],
+      },
+    ],
+  },
+  elementRule: {
+    expectedProperties: [{ property: "margin-inline-start" }],
+    expectedNodes: [
+      {
+        property: "margin-inline-start",
+        nodes: ["div.test-class", "div"],
+      },
+    ],
+  },
+};
 
 const {
   COMPATIBILITY_UPDATE_NODES_COMPLETE,
@@ -18,24 +103,42 @@ add_task(async function() {
 
   await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
 
-  const { inspector, selectedElementPane } = await openCompatibilityView();
-  await selectNode("div", inspector);
+  const {
+    allElementsPane,
+    inspector,
+    selectedElementPane,
+  } = await openCompatibilityView();
+  await selectNode(".test-class", inspector);
 
   info("Check the initial issue");
-  await assertIssueList(selectedElementPane, [
-    { property: "border-block-color" },
-  ]);
+  await assertAll(selectedElementPane, TEST_DATA_SELECTED.fullRule);
+  await assertAll(allElementsPane, TEST_DATA_ALL.fullRule);
 
-  info("Check the issue after toggling the property");
-  await _togglePropRule(inspector, 0, 0);
+  info("Check the issue after unchecking class rule");
+  await _togglePropRule(inspector, 1, 0);
+  await assertAll(selectedElementPane, TEST_DATA_SELECTED.elementRule);
+  await assertAll(allElementsPane, TEST_DATA_ALL.elementRule);
+
+  info("Check the issue after unchecking div rule");
+  await _togglePropRule(inspector, 2, 0);
   await assertIssueList(selectedElementPane, []);
+  await assertIssueList(allElementsPane, []);
 
-  info("Check the issue after toggling the property again");
-  await _togglePropRule(inspector, 0, 0);
-  await assertIssueList(selectedElementPane, [
-    { property: "border-block-color" },
-  ]);
+  info("Check the issue after reverting class rule");
+  await _togglePropRule(inspector, 1, 0);
+  await assertAll(selectedElementPane, TEST_DATA_SELECTED.classRule);
+  await assertAll(allElementsPane, TEST_DATA_ALL.classRule);
+
+  info("Check the issue after reverting div rule");
+  await _togglePropRule(inspector, 2, 0);
+  await assertAll(selectedElementPane, TEST_DATA_SELECTED.fullRule);
+  await assertAll(allElementsPane, TEST_DATA_ALL.fullRule);
 });
+
+async function assertAll(pane, { expectedProperties, expectedNodes }) {
+  await assertIssueList(pane, expectedProperties);
+  await assertNodeList(pane, expectedNodes);
+}
 
 async function _togglePropRule(inspector, ruleIndex, propIndex) {
   const ruleView = inspector.getPanel("ruleview").view;
