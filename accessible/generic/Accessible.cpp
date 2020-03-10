febc66584c9364b409e24b6cac65e9e25edddadb
Eitan Isaacson <eitan@monotonous.org>
2020-03-10 22:35:51 +0000

    Bug 1620508 - Revert change where opacity:0 makes subtrees invisible. r=Jamie a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D66296

--- accessible/generic/Accessible.cpp
+++ accessible/generic/Accessible.cpp
@@ -301,10 +301,6 @@ uint64_t Accessible::VisibilityState() const {
     return states::INVISIBLE;
   }
 
-  if (frame->Style()->IsInOpacityZeroSubtree()) {
-    return states::INVISIBLE;
-  }
-
   if (!frame->StyleVisibility()->IsVisible()) return states::INVISIBLE;
 
   // It's invisible if the presshell is hidden by a visibility:hidden element in
