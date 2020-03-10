a1c387b7754a87c45d7b9b025c3f7e110eb5a001
Jan de Mooij <jdemooij@mozilla.com>
2020-03-10 10:42:43 +0000

    Bug 1618198 part 12 - Implement JSOp::Arguments, add WarpOpSnapshot mechanism. r=iain
    For JSOp::Arguments we need to look up the realm-specific template object on the
    main thread. To support that, this patch adds a mechanism for snapshotting
    op-specific data in WarpOracle. WarpScriptSnapshot contains a linked list of
    WarpOpSnapshots that WarpBuilder can then iterate over.
    
    JSOp::RegExp now uses a snapshot for the hasShared flag. Later we could use the
    same mechanism for capturing Baseline IC data.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65942

--- js/src/jit/WarpOracle.h
+++ js/src/jit/WarpOracle.h
@@ -7,6 +7,9 @@
 #ifndef jit_WarpOracle_h
 #define jit_WarpOracle_h
 
+#include "mozilla/LinkedList.h"
+
+#include "jit/JitAllocPolicy.h"
 #include "jit/JitContext.h"
 
 namespace js {
@@ -14,21 +17,94 @@ namespace jit {
 
 class MIRGenerator;
 
-// Snapshot data for a single JSScript.
-class WarpScriptSnapshot {
-  JSScript* script_;
+#define WARP_OP_SNAPSHOT_LIST(_) \
+  _(WarpArguments)               \
+  _(WarpRegExp)
+
+// WarpOpSnapshot is the base class for data attached to a single bytecode op by
+// WarpOracle. This is typically data that WarpBuilder can't read off-thread
+// without racing.
+class WarpOpSnapshot : public TempObject,
+                       public mozilla::LinkedListElement<WarpOpSnapshot> {
+ public:
+  enum class Kind : uint16_t {
+#define DEF_KIND(KIND) KIND,
+    WARP_OP_SNAPSHOT_LIST(DEF_KIND)
+#undef DEF_KIND
+  };
+
+ private:
+  // Bytecode offset.
+  uint32_t offset_ = 0;
+
+#ifdef DEBUG
+  Kind kind_;
+#endif
+
+ protected:
+  WarpOpSnapshot(Kind kind, uint32_t offset) : offset_(offset) {
+#ifdef DEBUG
+    kind_ = kind;
+#endif
+  }
 
  public:
-  explicit WarpScriptSnapshot(JSScript* script) : script_(script) {}
+  uint32_t offset() const { return offset_; }
+
+  template <typename T>
+  const T* as() const {
+    MOZ_ASSERT(kind_ == T::ThisKind);
+    return static_cast<const T*>(this);
+  }
+};
+
+using WarpOpSnapshotList = mozilla::LinkedList<WarpOpSnapshot>;
+
+// Template object for JSOp::Arguments.
+class WarpArguments : public WarpOpSnapshot {
+  // TODO: trace this when we can trace snapshots.
+  ArgumentsObject* templateObj_;
+
+ public:
+  static constexpr Kind ThisKind = Kind::WarpArguments;
+
+  WarpArguments(uint32_t offset, ArgumentsObject* templateObj)
+      : WarpOpSnapshot(ThisKind, offset), templateObj_(templateObj) {}
+  ArgumentsObject* templateObj() const { return templateObj_; }
+};
+
+// The "has RegExpShared" state for JSOp::RegExp's template object.
+class WarpRegExp : public WarpOpSnapshot {
+  bool hasShared_;
+
+ public:
+  static constexpr Kind ThisKind = Kind::WarpRegExp;
+
+  WarpRegExp(uint32_t offset, bool hasShared)
+      : WarpOpSnapshot(ThisKind, offset), hasShared_(hasShared) {}
+  bool hasShared() const { return hasShared_; }
+};
+
+// Snapshot data for a single JSScript.
+class WarpScriptSnapshot : public TempObject {
+  JSScript* script_;
+  WarpOpSnapshotList opSnapshots_;
+
+ public:
+  WarpScriptSnapshot(JSScript* script, WarpOpSnapshotList&& opSnapshots)
+      : script_(script), opSnapshots_(std::move(opSnapshots)) {}
 
   JSScript* script() const { return script_; }
+  const WarpOpSnapshotList& opSnapshots() const { return opSnapshots_; }
 };
 
 // Data allocated by WarpOracle on the main thread that's used off-thread by
 // WarpBuilder to build the MIR graph.
 //
-// TODO: trace IC data in IonCompileTask::trace (like MRootList for non-Warp).
-class WarpSnapshot {
+// TODO: trace op snapshots in IonCompileTask::trace (like MRootList for
+// non-Warp).
+// TODO: add logging to dump the full WarpSnapshot.
+class WarpSnapshot : public TempObject {
   // The script to compile.
   WarpScriptSnapshot* script_;
 
