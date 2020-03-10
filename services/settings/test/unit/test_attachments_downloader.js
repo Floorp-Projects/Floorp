d5ab0b5df2e9891d973c082f08259ab9b88b279a
Daniel Varga <dvarga@mozilla.com>
2020-03-10 21:08:48 +0200

    Backed out changeset 6aaa835ae0eb (bug 1620630) for causing xpcshell failures at osfile/tests/xpcshell/test_osfile_closed.js
    CLOSED TREE

--- services/settings/test/unit/test_attachments_downloader.js
+++ services/settings/test/unit/test_attachments_downloader.js
@@ -192,11 +192,7 @@ add_task(async function test_delete_removes_local_file() {
   downloader.delete(RECORD);
 
   Assert.ok(!(await OS.File.exists(localFilePath)));
-  Assert.ok(
-    !(await OS.File.exists(
-      OS.Path.join(OS.Constants.Path.localProfileDir, ...downloader.folders)
-    ))
-  );
+  Assert.ok(!(await OS.File.exists(downloader.baseFolder)));
 });
 add_task(clear_state);
 
