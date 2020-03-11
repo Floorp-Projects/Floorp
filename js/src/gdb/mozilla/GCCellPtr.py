c17ab7391d5c2701aeb91179a324048146e0c064
André Bargull <andre.bargull@gmail.com>
2020-03-11 09:55:02 +0000

    Bug 1621256: Update GCCellPtr gdb pretty printer and add support for out-of-line types. r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D66216

--- js/src/gdb/mozilla/GCCellPtr.py
+++ js/src/gdb/mozilla/GCCellPtr.py
@@ -12,31 +12,64 @@ from mozilla.prettyprinters import pretty_printer
 # Forget any printers from previous loads of this module.
 mozilla.prettyprinters.clear_module_printers(__name__)
 
-# Cache information about the JS::TraceKind type for this objfile.
+# Cache information about the types for this objfile.
 
 
 class GCCellPtrTypeCache(object):
     def __init__(self, cache):
         self.TraceKind_t = gdb.lookup_type('JS::TraceKind')
+        self.AllocKind_t = gdb.lookup_type('js::gc::AllocKind')
+        self.Arena_t = gdb.lookup_type('js::gc::Arena')
+        self.Cell_t = gdb.lookup_type('js::gc::Cell')
+        self.TenuredCell_t = gdb.lookup_type('js::gc::TenuredCell')
+
+        trace_kinds = gdb.types.make_enum_dict(self.TraceKind_t)
+        alloc_kinds = gdb.types.make_enum_dict(self.AllocKind_t)
+
+        def trace_kind(k):
+            return trace_kinds['JS::TraceKind::' + k]
+
+        def alloc_kind(k):
+            return alloc_kinds['js::gc::AllocKind::' + k]
 
         # Build a mapping from TraceKind enum values to the types they denote.
-        e = gdb.types.make_enum_dict(self.TraceKind_t)
-        kind_to_type = {}
+        trace_map = {
+            # Inline types.
+            'Object':       'JSObject',
+            'BigInt':       'JS::BigInt',
+            'String':       'JSString',
+            'Symbol':       'JS::Symbol',
+            'Shape':        'js::Shape',
+            'ObjectGroup':  'js::ObjectGroup',
+            'Null':         'std::nullptr_t',
 
-        def kind(k, t):
-            kind_to_type[e['JS::TraceKind::' + k]] = gdb.lookup_type(t)
-        kind('Object',      'JSObject')
-        kind('String',      'JSString')
-        kind('Symbol',      'JS::Symbol')
-        kind('Script',      'js::BaseScript')
-        kind('Shape',       'js::Shape')
-        kind('ObjectGroup', 'js::ObjectGroup')
-        kind('BaseShape',   'js::BaseShape')
-        kind('JitCode',     'js::jit::JitCode')
-        self.kind_to_type = kind_to_type
+            # Out-of-line types.
+            'BaseShape':    'js::BaseShape',
+            'JitCode':      'js::jit::JitCode',
+            'Script':       'js::BaseScript',
+            'Scope':        'js::Scope',
+            'RegExpShared': 'js::RegExpShared',
+        }
 
-        self.Null = e['JS::TraceKind::Null']
-        self.mask = gdb.parse_and_eval('JS::OutOfLineTraceKindMask')
+        # Map from AllocKind to TraceKind for out-of-line types.
+        alloc_map = {
+            'BASE_SHAPE':   'BaseShape',
+            'JITCODE':      'JitCode',
+            'SCRIPT':       'Script',
+            'SCOPE':        'Scope',
+            'REGEXP_SHARED': 'RegExpShared',
+        }
+
+        self.trace_kind_to_type = {
+            trace_kind(k): gdb.lookup_type(v) for k, v in trace_map.items()
+        }
+        self.alloc_kind_to_trace_kind = {
+            alloc_kind(k): trace_kind(v) for k, v in alloc_map.items()
+        }
+
+        self.Null = trace_kind('Null')
+        self.tracekind_mask = gdb.parse_and_eval('JS::OutOfLineTraceKindMask')
+        self.arena_mask = gdb.parse_and_eval('js::gc::ArenaMask')
 
 
 @pretty_printer('JS::GCCellPtr')
@@ -49,8 +82,35 @@ class GCCellPtr(object):
 
     def to_string(self):
         ptr = self.value['ptr']
-        kind = ptr & self.cache.mod_GCCellPtr.mask
+        kind = ptr & self.cache.mod_GCCellPtr.tracekind_mask
         if kind == self.cache.mod_GCCellPtr.Null:
             return "JS::GCCellPtr(nullptr)"
-        tipe = self.cache.mod_GCCellPtr.kind_to_type[int(kind)]
-        return "JS::GCCellPtr(({}*) {})".format(tipe, ptr.cast(self.cache.void_ptr_t))
+        if kind == self.cache.mod_GCCellPtr.tracekind_mask:
+            # Out-of-line trace kinds.
+            #
+            # Compute the underlying type for out-of-line kinds by
+            # reimplementing the GCCellPtr::outOfLineKind() method.
+            #
+            # The extra casts below are only present to make it easier to
+            # compare this code against the C++ implementation.
+
+            # GCCellPtr::asCell()
+            cell_ptr = ptr & ~self.cache.mod_GCCellPtr.tracekind_mask
+            cell = cell_ptr.reinterpret_cast(self.cache.mod_GCCellPtr.Cell_t.pointer())
+
+            # Cell::asTenured()
+            tenured = cell.cast(self.cache.mod_GCCellPtr.TenuredCell_t.pointer())
+
+            # TenuredCell::arena()
+            addr = int(tenured)
+            arena_ptr = addr & ~self.cache.mod_GCCellPtr.arena_mask
+            arena = arena_ptr.reinterpret_cast(self.cache.mod_GCCellPtr.Arena_t.pointer())
+
+            # Arena::getAllocKind()
+            alloc_kind = arena['allocKind'].cast(self.cache.mod_GCCellPtr.AllocKind_t)
+            alloc_idx = int(alloc_kind.cast(self.cache.mod_GCCellPtr.AllocKind_t.target()))
+
+            # Map the AllocKind to a TraceKind.
+            kind = self.cache.mod_GCCellPtr.alloc_kind_to_trace_kind[alloc_idx]
+        type_name = self.cache.mod_GCCellPtr.trace_kind_to_type[int(kind)]
+        return "JS::GCCellPtr(({}*) {})".format(type_name, ptr.cast(self.cache.void_ptr_t))
