8529240cf49426337cabe0f0b608306120ad7903
Harry Twyford <htwyford@mozilla.com>
2020-03-12 02:35:24 +0000

    Bug 1620274 - Move tests in urlbar/tests/browser/tips to urlbar/tests/browser-tips. r=mak,Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D65577

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ function removeOverrides(config) {
 
 const xpcshellTestPaths = ["**/test*/unit*/", "**/test*/xpcshell/"];
 
-const browserTestPaths = ["**/test*/**/browser/"];
+const browserTestPaths = ["**/test*/**/browser*/"];
 
 const mochitestTestPaths = [
   // Note: we do not want to match testing/mochitest as that would apply
--- browser/components/urlbar/tests/browser-tips/head.js
+++ browser/components/urlbar/tests/browser-tips/head.js
@@ -5,18 +5,10 @@
 // particular the update-related interventions.
 // We mock updates by using the test helpers in
 // toolkit/mozapps/update/tests/browser.
-//
-// If you're running these tests and you get an error like this:
-//
-// FAIL head.js import threw an exception - Error opening input stream (invalid filename?): chrome://mochitests/content/browser/toolkit/mozapps/update/tests/browser/head.js
-//
-// Then run `mach test toolkit/mozapps/update/tests/browser` first.  You can
-// stop mach as soon as it starts the first test, but this is necessary so that
-// mach builds the update tests in your objdir.
 
 "use strict";
 
-/* import-globals-from ../../../../../../toolkit/mozapps/update/tests/browser/head.js */
+/* import-globals-from ../../../../../toolkit/mozapps/update/tests/browser/head.js */
 Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/toolkit/mozapps/update/tests/browser/head.js",
   this
