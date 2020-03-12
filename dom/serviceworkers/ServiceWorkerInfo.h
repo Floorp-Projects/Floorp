93a0109c43be0b8f39e0d162b64fa7f894e82efe
Eden Chuang <echuang@mozilla.com>
2020-03-12 20:53:28 +0000

    Bug 1407276 - Avoid creating InterceptedHttpChannel if the service worker has no fetch event handler r=dom-workers-and-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D64092

--- dom/serviceworkers/ServiceWorkerInfo.h
+++ dom/serviceworkers/ServiceWorkerInfo.h
@@ -126,6 +126,7 @@ class ServiceWorkerInfo final : public nsIServiceWorkerInfo {
     MOZ_ASSERT(NS_IsMainThread());
     MOZ_DIAGNOSTIC_ASSERT(mHandlesFetch == Unknown);
     mHandlesFetch = aHandlesFetch ? Enabled : Disabled;
+    mDescriptor.SetHandlesFetch(aHandlesFetch);
   }
 
   void SetRegistrationVersion(uint64_t aVersion);
