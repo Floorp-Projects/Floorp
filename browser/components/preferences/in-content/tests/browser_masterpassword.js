baeb1b90c4de3d58b759a0800741a9176ba61748
Jared Wein <jwein@mozilla.com>
2020-03-12 14:47:43 +0000

    Bug 1194529 - Require OS auth to set a Master Password. r=fluent-reviewers,MattN
    We require OS auth before setting a Master Password since Master Password supersedes the OS authentication.
    
    Differential Revision: https://phabricator.services.mozilla.com/D61749

--- browser/components/preferences/in-content/tests/browser_masterpassword.js
+++ browser/components/preferences/in-content/tests/browser_masterpassword.js
@@ -1,3 +1,5 @@
+ChromeUtils.import("resource://testing-common/OSKeyStoreTestUtils.jsm", this);
+
 add_task(async function() {
   let prefs = await openPreferencesViaOpenPreferencesAPI("panePrivacy", {
     leaveOpen: true,
@@ -8,8 +10,10 @@ add_task(async function() {
   // Fake the subdialog and LoginHelper
   let win = doc.defaultView;
   let dialogURL = "";
+  let dialogOpened = false;
   win.gSubDialog = {
     open(aDialogURL, unused, unused2, aCallback) {
+      dialogOpened = true;
       dialogURL = aDialogURL;
       masterPasswordSet = masterPasswordNextState;
       aCallback();
@@ -23,6 +27,7 @@ add_task(async function() {
   };
 
   let checkbox = doc.querySelector("#useMasterPassword");
+  checkbox.scrollIntoView();
   ok(
     !checkbox.checked,
     "master password checkbox should be unchecked by default"
@@ -30,18 +35,61 @@ add_task(async function() {
   let button = doc.getElementById("changeMasterPassword");
   ok(button.disabled, "master password button should be disabled by default");
 
-  let masterPasswordNextState = true;
-  checkbox.click();
-  is(
-    dialogURL,
-    "chrome://mozapps/content/preferences/changemp.xhtml",
-    "clicking on the checkbox should open the masterpassword dialog"
-  );
+  let masterPasswordNextState = false;
+  if (OSKeyStoreTestUtils.canTestOSKeyStoreLogin()) {
+    let osAuthDialogShown = OSKeyStoreTestUtils.waitForOSKeyStoreLogin(false);
+    checkbox.click();
+    info("waiting for os auth dialog to appear and get canceled");
+    await osAuthDialogShown;
+    await TestUtils.waitForCondition(
+      () => !checkbox.checked,
+      "wait for checkbox to get unchecked"
+    );
+    ok(!dialogOpened, "the dialog should not have opened");
+    ok(
+      !dialogURL,
+      "the changemp dialog should not have been opened when the os auth dialog is canceled"
+    );
+    ok(
+      !checkbox.checked,
+      "master password checkbox should be unchecked after canceling os auth dialog"
+    );
+    ok(button.disabled, "button should be disabled after canceling os auth");
+  }
+
+  masterPasswordNextState = true;
+  if (OSKeyStoreTestUtils.canTestOSKeyStoreLogin()) {
+    let osAuthDialogShown = OSKeyStoreTestUtils.waitForOSKeyStoreLogin(true);
+    checkbox.click();
+    info("waiting for os auth dialog to appear");
+    await osAuthDialogShown;
+    info("waiting for dialogURL to get set");
+    await TestUtils.waitForCondition(
+      () => dialogURL,
+      "wait for open to get called asynchronously"
+    );
+    is(
+      dialogURL,
+      "chrome://mozapps/content/preferences/changemp.xhtml",
+      "clicking on the checkbox should open the masterpassword dialog"
+    );
+  } else {
+    masterPasswordSet = true;
+    doc.defaultView.gPrivacyPane._initMasterPasswordUI();
+    await TestUtils.waitForCondition(
+      () => !button.disabled,
+      "waiting for master password button to get enabled"
+    );
+  }
   ok(!button.disabled, "master password button should now be enabled");
   ok(checkbox.checked, "master password checkbox should be checked now");
 
   dialogURL = "";
   button.doCommand();
+  await TestUtils.waitForCondition(
+    () => dialogURL,
+    "wait for open to get called asynchronously"
+  );
   is(
     dialogURL,
     "chrome://mozapps/content/preferences/changemp.xhtml",
@@ -49,6 +97,17 @@ add_task(async function() {
   );
   ok(!button.disabled, "master password button should still be enabled");
   ok(checkbox.checked, "master password checkbox should be checked still");
+
+  // Confirm that we won't automatically respond to the dialog,
+  // since we don't expect a dialog here, we want the test to fail if one appears.
+  is(
+    Services.prefs.getStringPref(
+      "browser.osKeyStore.unofficialBuildOnlyLogin",
+      ""
+    ),
+    "",
+    "Pref should be set to an empty string"
+  );
 
   masterPasswordNextState = false;
   dialogURL = "";
