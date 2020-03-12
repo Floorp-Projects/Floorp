55e45044a8fecdcc93f334952c877c2ac08b0aed
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-03-12 14:32:40 +0000

    Bug 1615509: Show nodes that caused the issue. r=rcaliman,ladybenko
    Differential Revision: https://phabricator.services.mozilla.com/D64308

---
+++ devtools/client/inspector/compatibility/components/NodeItem.js
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { PureComponent } = require("devtools/client/shared/vendor/react");
+const dom = require("devtools/client/shared/vendor/react-dom-factories");
+
+const {
+  translateNodeFrontToGrip,
+} = require("devtools/client/inspector/shared/utils");
+const { REPS, MODE } = require("devtools/client/shared/components/reps/reps");
+const { Rep } = REPS;
+const ElementNode = REPS.ElementNode;
+
+const Types = require("devtools/client/inspector/compatibility/types");
+
+class NodeItem extends PureComponent {
+  static get propTypes() {
+    return {
+      node: Types.node.isRequired,
+    };
+  }
+
+  render() {
+    const { node } = this.props;
+
+    return dom.li(
+      {},
+      Rep({
+        defaultRep: ElementNode,
+        mode: MODE.TINY,
+        object: translateNodeFrontToGrip(node),
+      })
+    );
+  }
+}
+
+module.exports = NodeItem;
