d86a6177935932b626c759a5d30214aedb1bfa16
Marc Streckfuss <marc.streckfuss@gmail.com>
2020-03-12 21:36:04 +0000

    Bug 1620340 - part3: Cosmetic Changes to MediaControlKeysManager, r=alwu
    rearranged the code, so that the comment is directly above the relevant statement (see https://phabricator.services.mozilla.com/D63459#inline-381952)
    
    Differential Revision: https://phabricator.services.mozilla.com/D66391
    
    --HG--
    extra : moz-landing-system : lando

--- dom/media/mediacontrol/MediaControlKeysManager.cpp
+++ dom/media/mediacontrol/MediaControlKeysManager.cpp
@@ -55,13 +55,15 @@ void MediaControlKeysManager::StartMonitoringControlKeys() {
 
   // When cross-compiling with MinGW, we cannot use the related WinAPI, thus
   // mEventSource might be null there.
-  if (mEventSource && !mEventSource->IsOpened()) {
-    LOG("StartMonitoringControlKeys");
-    if (mEventSource->Open()) {
-      mEventSource->SetPlaybackState(mPlaybackState);
-      mEventSource->SetMediaMetadata(mMetadata);
-      mEventSource->AddListener(this);
-    }
+  if (!mEventSource) {
+    return;
+  }
+
+  LOG("StartMonitoringControlKeys");
+  if (!mEventSource->IsOpened() && mEventSource->Open()) {
+    mEventSource->SetPlaybackState(mPlaybackState);
+    mEventSource->SetMediaMetadata(mMetadata);
+    mEventSource->AddListener(this);
   }
 }
 
