86185c0ade1faf859b5d68131799e81d97496042
Tom Schuster <evilpies@gmail.com>
2020-03-12 00:50:53 +0000

    Bug 1621341 - Remove flags from ReportCompileWarning. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D66509

--- js/src/vm/ErrorReporting.h
+++ js/src/vm/ErrorReporting.h
@@ -98,9 +98,11 @@ extern void ReportCompileErrorUTF8(JSContext* cx, ErrorMetadata&& metadata,
  * script.  Returns true if the warning was successfully reported, false if an
  * error occurred.
  */
-extern MOZ_MUST_USE bool ReportCompileWarning(
-    JSContext* cx, ErrorMetadata&& metadata, UniquePtr<JSErrorNotes> notes,
-    unsigned flags, unsigned errorNumber, va_list* args);
+extern MOZ_MUST_USE bool ReportCompileWarning(JSContext* cx,
+                                              ErrorMetadata&& metadata,
+                                              UniquePtr<JSErrorNotes> notes,
+                                              unsigned errorNumber,
+                                              va_list* args);
 
 class GlobalObject;
 
