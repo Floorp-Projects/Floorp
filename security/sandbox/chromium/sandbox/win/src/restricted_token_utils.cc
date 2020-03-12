841b34f1db8919bbf4adf3db39f4d3884131c454
Bob Owen <bobowencode@gmail.com>
2020-03-12 20:39:40 +0000

    Bug 1557282 Part 1: Take chromium commit c1ce57ea5d31208af589b4839390a44ab20b0c8f. r=handyman,gcp
    This adds AddRestrictingRandomSid feature, which fixes our issues with
    SetLockdownDefaultDacl, apart from when we are running from a network drive.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66610

--- security/sandbox/chromium/sandbox/win/src/restricted_token_utils.cc
+++ security/sandbox/chromium/sandbox/win/src/restricted_token_utils.cc
@@ -56,12 +56,19 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
                             IntegrityLevel integrity_level,
                             TokenType token_type,
                             bool lockdown_default_dacl,
+                            PSID unique_restricted_sid,
                             bool use_restricting_sids,
                             base::win::ScopedHandle* token) {
   RestrictedToken restricted_token;
   restricted_token.Init(effective_token);
   if (lockdown_default_dacl)
     restricted_token.SetLockdownDefaultDacl();
+  if (unique_restricted_sid) {
+    restricted_token.AddDefaultDaclSid(Sid(unique_restricted_sid), GRANT_ACCESS,
+                                       GENERIC_ALL);
+    restricted_token.AddDefaultDaclSid(Sid(WinCreatorOwnerRightsSid),
+                                       GRANT_ACCESS, READ_CONTROL);
+  }
 
   std::vector<base::string16> privilege_exceptions;
   std::vector<Sid> sid_exceptions;
@@ -108,6 +115,8 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
         restricted_token.AddRestrictingSid(WinRestrictedCodeSid);
         restricted_token.AddRestrictingSidCurrentUser();
         restricted_token.AddRestrictingSidLogonSession();
+        if (unique_restricted_sid)
+          restricted_token.AddRestrictingSid(Sid(unique_restricted_sid));
       }
       break;
     }
@@ -120,6 +129,8 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
         restricted_token.AddRestrictingSid(WinBuiltinUsersSid);
         restricted_token.AddRestrictingSid(WinWorldSid);
         restricted_token.AddRestrictingSid(WinRestrictedCodeSid);
+        if (unique_restricted_sid)
+          restricted_token.AddRestrictingSid(Sid(unique_restricted_sid));
 
         // This token has to be able to create objects in BNO.
         // Unfortunately, on Vista+, it needs the current logon sid
@@ -127,6 +138,8 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
         // low integrity level so it can't access object created by other
         // processes.
         restricted_token.AddRestrictingSidLogonSession();
+      } else {
+        restricted_token.AddUserSidForDenyOnly();
       }
       break;
     }
@@ -135,6 +148,8 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
       restricted_token.AddUserSidForDenyOnly();
       if (use_restricting_sids) {
         restricted_token.AddRestrictingSid(WinRestrictedCodeSid);
+        if (unique_restricted_sid)
+          restricted_token.AddRestrictingSid(Sid(unique_restricted_sid));
       }
       break;
     }
@@ -142,6 +157,8 @@ DWORD CreateRestrictedToken(HANDLE effective_token,
       restricted_token.AddUserSidForDenyOnly();
       if (use_restricting_sids) {
         restricted_token.AddRestrictingSid(WinNullSid);
+        if (unique_restricted_sid)
+          restricted_token.AddRestrictingSid(Sid(unique_restricted_sid));
       }
       break;
     }
