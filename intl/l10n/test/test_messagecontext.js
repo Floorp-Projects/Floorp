a74e8445fbc457800473e141aeed449118b5e8e7
Razvan Maries <rmaries@mozilla.com>
2020-03-14 00:43:41 +0200

    Backed out 2 changesets (bug 1560038) for perma failures on browser_application_panel_list-single-worker.js. CLOSED TREE
    Backed out changeset 7cd88f40fc85 (bug 1560038)
    Backed out changeset 54dbe282c9ac (bug 1560038)

--- intl/l10n/test/test_messagecontext.js
+++ intl/l10n/test/test_messagecontext.js
@@ -2,6 +2,9 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
 function run_test() {
+  const { FluentBundle, FluentResource } =
+    ChromeUtils.import("resource://gre/modules/Fluent.jsm");
+
   test_methods_presence(FluentBundle);
   test_methods_calling(FluentBundle, FluentResource);
 
