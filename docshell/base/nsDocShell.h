998f915888b65a072440dcdb4fa51375cbb2c6a5
Agi Sferro <agi@sferro.dev>
2020-03-13 18:22:16 +0000

    Bug 1619796 - Don't fire onLoadRequest for embedder-initiated loads. r=smaug,droeh
    Differential Revision: https://phabricator.services.mozilla.com/D66695

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -151,6 +151,9 @@ class nsDocShell final : public nsDocLoader,
 
     // Whether the load was triggered by user interaction.
     INTERNAL_LOAD_FLAGS_IS_USER_TRIGGERED = 0x1000,
+
+    // Whether the load should go through LoadURIDelegate.
+    INTERNAL_LOAD_FLAGS_BYPASS_LOAD_URI_DELEGATE = 0x2000,
   };
 
   // Event type dispatched by RestorePresentation
