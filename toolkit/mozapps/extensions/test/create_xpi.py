1f6bb7fb2148344e4bb0ec858360b4b9e58bf840
Brendan Dahl <brendan.dahl@gmail.com>
2020-03-13 22:22:15 +0000

    Bug 1620083 - Make web extensions re-bundle when dependent files change. r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D65416
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/extensions/test/create_xpi.py
+++ toolkit/mozapps/extensions/test/create_xpi.py
@@ -4,11 +4,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from os.path import abspath
+from os.path import abspath, relpath
 
 from mozbuild.action.zip import main as create_zip
 
-def main(output, input_dir):
+def main(output, input_dir, *files):
     output.close()
 
-    return create_zip(['-C', input_dir, abspath(output.name), '**'])
+    if files:
+      # The zip builder doesn't handle the files being an absolute path.
+      in_files = [relpath(file, input_dir) for file in files]
+
+      return create_zip(['-C', input_dir, abspath(output.name)] + in_files)
+    else:
+      return create_zip(['-C', input_dir, abspath(output.name), '**'])
