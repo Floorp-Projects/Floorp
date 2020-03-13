b32519a073abb0c54647ac93ccad1a7a75b1b65e
Jan de Mooij <jdemooij@mozilla.com>
2020-03-13 07:58:30 +0000

    Bug 1618198 part 14 - Implement environment chain initialization. r=iain
    The previous patch introduced more jit-test failures because we didn't
    initialize the environment chain yet (a good thing because it means we're
    beginning to support non-trivial tests!).
    
    WarpEnvironment is added to store information WarpBuilder needs to build the
    environment chain. The goal is to make most of the decisions in WarpOracle so
    that the WarpBuilder code can be as simple as possible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66387

--- js/src/jit/WarpOracle.cpp
+++ js/src/jit/WarpOracle.cpp
@@ -16,6 +16,7 @@
 
 #include "vm/BytecodeIterator-inl.h"
 #include "vm/BytecodeLocation-inl.h"
+#include "vm/EnvironmentObject-inl.h"
 
 using namespace js;
 using namespace js::jit;
@@ -65,6 +66,59 @@ static MOZ_MUST_USE bool AddOpSnapshot(TempAllocator& alloc,
   return true;
 }
 
+AbortReasonOr<WarpEnvironment> WarpOracle::createEnvironment(
+    HandleScript script) {
+  WarpEnvironment env;
+
+  // Don't do anything if the script doesn't use the environment chain.
+  // Always make an environment chain if the script needs an arguments object
+  // because ArgumentsObject construction requires the environment chain to be
+  // passed in.
+  if (!script->jitScript()->usesEnvironmentChain() && !script->needsArgsObj()) {
+    MOZ_ASSERT(env.kind() == WarpEnvironment::Kind::None);
+    return env;
+  }
+
+  if (ModuleObject* module = script->module()) {
+    env.initConstantObject(&module->initialEnvironment());
+    return env;
+  }
+
+  JSFunction* fun = script->function();
+  if (!fun) {
+    // For global scripts without a non-syntactic global scope, the environment
+    // chain is the global lexical environment.
+    MOZ_ASSERT(!script->isForEval());
+    MOZ_ASSERT(!script->hasNonSyntacticScope());
+    env.initConstantObject(&script->global().lexicalEnvironment());
+    return env;
+  }
+
+  // TODO: Parameter expression-induced extra var environment not
+  // yet handled.
+  if (fun->needsExtraBodyVarEnvironment()) {
+    return abort(AbortReason::Disable, "Extra var environment unsupported");
+  }
+
+  JSObject* templateEnv = script->jitScript()->templateEnvironment();
+
+  CallObject* callObjectTemplate = nullptr;
+  if (fun->needsCallObject()) {
+    callObjectTemplate = &templateEnv->as<CallObject>();
+  }
+
+  LexicalEnvironmentObject* namedLambdaTemplate = nullptr;
+  if (fun->needsNamedLambdaEnvironment()) {
+    if (callObjectTemplate) {
+      templateEnv = templateEnv->enclosingEnvironment();
+    }
+    namedLambdaTemplate = &templateEnv->as<LexicalEnvironmentObject>();
+  }
+
+  env.initFunction(callObjectTemplate, namedLambdaTemplate);
+  return env;
+}
+
 AbortReasonOr<WarpScriptSnapshot*> WarpOracle::createScriptSnapshot(
     HandleScript script) {
   MOZ_ASSERT(script->hasJitScript());
@@ -72,6 +126,9 @@ AbortReasonOr<WarpScriptSnapshot*> WarpOracle::createScriptSnapshot(
   if (!script->jitScript()->ensureHasCachedIonData(cx_, script)) {
     return abort(AbortReason::Error);
   }
+
+  WarpEnvironment environment;
+  MOZ_TRY_VAR(environment, createEnvironment(script));
 
   // Unfortunately LinkedList<> asserts the list is empty in its destructor.
   // Clear the list if we abort compilation.
@@ -132,7 +189,7 @@ AbortReasonOr<WarpScriptSnapshot*> WarpOracle::createScriptSnapshot(
   }
 
   auto* scriptSnapshot = new (alloc_.fallible())
-      WarpScriptSnapshot(script, std::move(opSnapshots));
+      WarpScriptSnapshot(script, environment, std::move(opSnapshots));
   if (!scriptSnapshot) {
     return abort(AbortReason::Alloc);
   }
--- js/src/jit/WarpOracle.h
+++ js/src/jit/WarpOracle.h
@@ -85,16 +85,82 @@ class WarpRegExp : public WarpOpSnapshot {
   bool hasShared() const { return hasShared_; }
 };
 
+// Snapshot data for the environment object(s) created in the script's prologue.
+// TODO: trace object pointers.
+class WarpEnvironment {
+ public:
+  enum class Kind {
+    // No environment object should be set. Leave the slot initialized to
+    // |undefined|.
+    None,
+
+    // Use constantObject_ as the environment object.
+    ConstantObject,
+
+    // Use the callee's environment chain. Optionally allocate a new
+    // NamedLambdaObject and/or CallObject based on namedLambdaTemplate_ and
+    // callObjectTemplate_.
+    Function,
+  };
+
+ private:
+  Kind kind_ = Kind::None;
+
+  union {
+    // For Kind::ConstantObject, the object.
+    JSObject* constantObject_;
+    // For Kind::Function, the template objects for CallObject and NamedLambda
+    // if needed. nullptr if the environment object is not needed.
+    struct {
+      CallObject* callObjectTemplate_;
+      LexicalEnvironmentObject* namedLambdaTemplate_;
+    } fun;
+  };
+
+ public:
+  void initConstantObject(JSObject* obj) {
+    kind_ = Kind::ConstantObject;
+    MOZ_ASSERT(obj);
+    constantObject_ = obj;
+  }
+  void initFunction(CallObject* callObjectTemplate,
+                    LexicalEnvironmentObject* namedLambdaTemplate) {
+    kind_ = Kind::Function;
+    fun.callObjectTemplate_ = callObjectTemplate;
+    fun.namedLambdaTemplate_ = namedLambdaTemplate;
+  }
+
+  Kind kind() const { return kind_; }
+
+  JSObject* constantObject() const {
+    MOZ_ASSERT(kind_ == Kind::ConstantObject);
+    return constantObject_;
+  }
+  CallObject* maybeCallObjectTemplate() const {
+    MOZ_ASSERT(kind_ == Kind::Function);
+    return fun.callObjectTemplate_;
+  }
+  LexicalEnvironmentObject* maybeNamedLambdaTemplate() const {
+    MOZ_ASSERT(kind_ == Kind::Function);
+    return fun.namedLambdaTemplate_;
+  }
+};
+
 // Snapshot data for a single JSScript.
 class WarpScriptSnapshot : public TempObject {
   JSScript* script_;
+  WarpEnvironment environment_;
   WarpOpSnapshotList opSnapshots_;
 
  public:
-  WarpScriptSnapshot(JSScript* script, WarpOpSnapshotList&& opSnapshots)
-      : script_(script), opSnapshots_(std::move(opSnapshots)) {}
+  WarpScriptSnapshot(JSScript* script, const WarpEnvironment& env,
+                     WarpOpSnapshotList&& opSnapshots)
+      : script_(script),
+        environment_(env),
+        opSnapshots_(std::move(opSnapshots)) {}
 
   JSScript* script() const { return script_; }
+  const WarpEnvironment& environment() const { return environment_; }
   const WarpOpSnapshotList& opSnapshots() const { return opSnapshots_; }
 };
 
@@ -126,6 +192,7 @@ class MOZ_STACK_CLASS WarpOracle {
   mozilla::GenericErrorResult<AbortReason> abort(AbortReason r,
                                                  const char* message, ...);
 
+  AbortReasonOr<WarpEnvironment> createEnvironment(HandleScript script);
   AbortReasonOr<WarpScriptSnapshot*> createScriptSnapshot(HandleScript script);
 
  public:
