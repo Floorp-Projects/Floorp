5c64096511e1d1b36a6984aca3851c7630fdeebf
Erik Nordin <nordzilla@mozilla.com>
2020-03-13 21:20:22 +0000

    Bug 1621415 - Ensure adopted styles are applied when printing r=emilio
    - Add functionality to clone adopted style sheets for printing.
    - Add reftest to ensure that the document's adopted styles show in print.
    - Add reftest to ensure that a shadow root's adopted styles show in print.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66517

--- dom/base/DocumentOrShadowRoot.cpp
+++ dom/base/DocumentOrShadowRoot.cpp
@@ -198,6 +198,27 @@ void DocumentOrShadowRoot::ClearAdoptedStyleSheets() {
   mAdoptedStyleSheets.Clear();
 }
 
+void DocumentOrShadowRoot::CloneAdoptedSheetsFrom(
+    DocumentOrShadowRoot& aSource) {
+  Document& ownerDoc = *AsNode().OwnerDoc();
+  Sequence<OwningNonNull<StyleSheet>> list;
+  if (!list.SetCapacity(mAdoptedStyleSheets.Length(), fallible)) {
+    return;
+  }
+
+  // TODO(nordzilla) We can improve the performance here for some cases.
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1622322
+  for (auto& sheet : aSource.mAdoptedStyleSheets) {
+    DebugOnly<bool> succeeded =
+        list.AppendElement(sheet->CloneAdoptedSheet(ownerDoc), fallible);
+    MOZ_ASSERT(succeeded);
+  }
+
+  ErrorResult rv;
+  SetAdoptedStyleSheets(list, rv);
+  MOZ_ASSERT(!rv.Failed());
+}
+
 Element* DocumentOrShadowRoot::GetElementById(const nsAString& aElementId) {
   if (MOZ_UNLIKELY(aElementId.IsEmpty())) {
     nsContentUtils::ReportEmptyGetElementByIdArg(AsNode().OwnerDoc());
