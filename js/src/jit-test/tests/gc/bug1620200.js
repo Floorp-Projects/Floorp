296156586a41afae6f7f506ef7ef5a8a3aab2a10
Steve Fink <sfink@mozilla.com>
2020-03-14 17:32:56 +0000

    Bug 1620200 - test r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D65827

---
+++ js/src/jit-test/tests/gc/bug1620200.js
@@ -0,0 +1,13 @@
+gczeal(14, 8);
+try {
+  badReference();
+} catch (exc) {}
+
+var ObjectStruct = new TypedObject.StructType({});
+var ValueStruct = new TypedObject.StructType({
+    field: TypedObject.Any
+});
+
+ObjectStruct.field2 = new ValueStruct();
+
+undefined.toString();
