9d313b39ce0314a6dc93dfe100669106ecc05c26
Dana Keeler <dkeeler@mozilla.com>
2020-03-16 22:27:03 +0000

    bug 1620110 - osclientcerts: properly store serial numbers as DER-encoded integers r=kjacobs
    PKCS#11 requires that serial numbers be DER-encoded (essentially, the bytes of
    the serialNumber component of TBSCertificate). On macOS,
    SecCertificateCopySerialNumberData gives the contents of this component (so it
    lacks the tag and length fields, and may or may not have leading 00 or FF bytes
    to indicate sign). On Windows, CERT_INFO.SerialNumber is the value of the
    integer with the least significant byte first, which is the opposite of DER
    (which has the most significant byte first). It also lacks any leading 00 or FF
    sign bytes. Since the OS APIs can't be used here, this patch introduces a
    utility function to grab the value of the serialNumber component of a
    DER-encoded certificate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66327

--- security/manager/ssl/osclientcerts/src/util.rs
+++ security/manager/ssl/osclientcerts/src/util.rs
@@ -73,6 +73,27 @@ pub fn read_ec_sig_point<'a>(signature: &'a [u8]) -> Result<(&'a [u8], &'a [u8])
     Ok((r, s))
 }
 
+/// Given a slice of DER bytes representing an X.509 certificate, extracts the encoded serial
+/// number. Does not verify that the remainder of the certificate is in any way well-formed.
+///   Certificate  ::=  SEQUENCE  {
+///           tbsCertificate       TBSCertificate,
+///           signatureAlgorithm   AlgorithmIdentifier,
+///           signatureValue       BIT STRING  }
+///
+///   TBSCertificate  ::=  SEQUENCE  {
+///           version         [0]  EXPLICIT Version DEFAULT v1,
+///           serialNumber         CertificateSerialNumber,
+///           ...
+///
+///   CertificateSerialNumber  ::=  INTEGER
+pub fn read_encoded_serial_number(certificate: &[u8]) -> Result<Vec<u8>, ()> {
+    let mut certificate_sequence = Sequence::new(certificate)?;
+    let mut tbs_certificate_sequence = certificate_sequence.read_sequence()?;
+    let _version = tbs_certificate_sequence.read_tagged_value(0)?;
+    let serial_number = tbs_certificate_sequence.read_encoded_sequence_component(INTEGER)?;
+    Ok(serial_number)
+}
+
 /// Helper macro for reading some bytes from a slice while checking the slice is long enough.
 /// Returns a pair consisting of a slice of the bytes read and a slice of the rest of the bytes
 /// from the original slice.
@@ -91,6 +112,8 @@ const INTEGER: u8 = 0x02;
 const SEQUENCE: u8 = 0x10;
 /// ASN.1 tag modifier identifying an item as constructed.
 const CONSTRUCTED: u8 = 0x20;
+/// ASN.1 tag modifier identifying an item as context-specific.
+const CONTEXT_SPECIFIC: u8 = 0x80;
 
 /// A helper struct for reading items from a DER SEQUENCE (in this case, all sequences are
 /// assumed to be CONSTRUCTED).
@@ -102,7 +125,7 @@ struct Sequence<'a> {
 impl<'a> Sequence<'a> {
     fn new(input: &'a [u8]) -> Result<Sequence<'a>, ()> {
         let mut der = Der::new(input);
-        let sequence_bytes = der.read(SEQUENCE | CONSTRUCTED)?;
+        let (_, _, sequence_bytes) = der.read_tlv(SEQUENCE | CONSTRUCTED)?;
         // We're assuming we want to consume the entire input for now.
         if !der.at_end() {
             return Err(());
@@ -114,7 +137,7 @@ impl<'a> Sequence<'a> {
 
     // TODO: we're not exhaustively validating this integer
     fn read_unsigned_integer(&mut self) -> Result<&'a [u8], ()> {
-        let bytes = self.contents.read(INTEGER)?;
+        let (_, _, bytes) = self.contents.read_tlv(INTEGER)?;
         if bytes.is_empty() {
             return Err(());
         }
@@ -126,6 +149,28 @@ impl<'a> Sequence<'a> {
         } else {
             Ok(bytes)
         }
+    }
+
+    fn read_sequence(&mut self) -> Result<Sequence<'a>, ()> {
+        let (_, _, sequence_bytes) = self.contents.read_tlv(SEQUENCE | CONSTRUCTED)?;
+        Ok(Sequence {
+            contents: Der::new(sequence_bytes),
+        })
+    }
+
+    fn read_tagged_value(&mut self, tag: u8) -> Result<&'a [u8], ()> {
+        let (_, _, tagged_value_bytes) = self
+            .contents
+            .read_tlv(CONTEXT_SPECIFIC | CONSTRUCTED | tag)?;
+        Ok(tagged_value_bytes)
+    }
+
+    fn read_encoded_sequence_component(&mut self, tag: u8) -> Result<Vec<u8>, ()> {
+        let (tag, length, value) = self.contents.read_tlv(tag)?;
+        let mut encoded_component_bytes = length;
+        encoded_component_bytes.insert(0, tag);
+        encoded_component_bytes.extend_from_slice(value);
+        Ok(encoded_component_bytes)
     }
 
     fn at_end(&self) -> bool {
@@ -148,23 +193,30 @@ impl<'a> Der<'a> {
     // be in an inconsistent state. As long as this implementation isn't exposed to code that would
     // use it incorrectly (i.e. it stays in this module and we only expose a stateless API), it
     // should be safe.
-    fn read(&mut self, tag: u8) -> Result<&'a [u8], ()> {
+    /// Given an expected tag, reads the next (tag, lengh, value) from the contents. Most
+    /// consumers will only be interested in the value, but some may want the entire encoded
+    /// contents, in which case the returned tuple can be concatenated.
+    fn read_tlv(&mut self, tag: u8) -> Result<(u8, Vec<u8>, &'a [u8]), ()> {
         let contents = self.contents;
         let (tag_read, rest) = try_read_bytes!(contents, 1);
         if tag_read[0] != tag {
             return Err(());
         }
+        let mut accumulated_length_bytes = Vec::with_capacity(4);
         let (length1, rest) = try_read_bytes!(rest, 1);
+        accumulated_length_bytes.extend_from_slice(length1);
         let (length, to_read_from) = if length1[0] < 0x80 {
             (length1[0] as usize, rest)
         } else if length1[0] == 0x81 {
             let (length, rest) = try_read_bytes!(rest, 1);
+            accumulated_length_bytes.extend_from_slice(length);
             if length[0] < 0x80 {
                 return Err(());
             }
             (length[0] as usize, rest)
         } else if length1[0] == 0x82 {
             let (lengths, rest) = try_read_bytes!(rest, 2);
+            accumulated_length_bytes.extend_from_slice(lengths);
             let length = (&mut &lengths[..])
                 .read_u16::<BigEndian>()
                 .map_err(|_| ())?;
@@ -177,7 +229,7 @@ impl<'a> Der<'a> {
         };
         let (contents, rest) = try_read_bytes!(to_read_from, length);
         self.contents = rest;
-        Ok(contents)
+        Ok((tag, accumulated_length_bytes, contents))
     }
 
     fn at_end(&self) -> bool {
@@ -193,23 +245,25 @@ mod tests {
     fn der_test_empty_input() {
         let input = Vec::new();
         let mut der = Der::new(&input);
-        assert!(der.read(INTEGER).is_err());
+        assert!(der.read_tlv(INTEGER).is_err());
     }
 
     #[test]
     fn der_test_no_length() {
         let input = vec![INTEGER];
         let mut der = Der::new(&input);
-        assert!(der.read(INTEGER).is_err());
+        assert!(der.read_tlv(INTEGER).is_err());
     }
 
     #[test]
     fn der_test_empty_sequence() {
         let input = vec![SEQUENCE, 0];
         let mut der = Der::new(&input);
-        let read_result = der.read(SEQUENCE);
+        let read_result = der.read_tlv(SEQUENCE);
         assert!(read_result.is_ok());
-        let sequence_bytes = read_result.unwrap();
+        let (tag, length, sequence_bytes) = read_result.unwrap();
+        assert_eq!(tag, SEQUENCE);
+        assert_eq!(length, vec![0]);
         assert_eq!(sequence_bytes.len(), 0);
         assert!(der.at_end());
     }
@@ -218,9 +272,11 @@ mod tests {
     fn der_test_not_at_end() {
         let input = vec![SEQUENCE, 0, 1];
         let mut der = Der::new(&input);
-        let read_result = der.read(SEQUENCE);
+        let read_result = der.read_tlv(SEQUENCE);
         assert!(read_result.is_ok());
-        let sequence_bytes = read_result.unwrap();
+        let (tag, length, sequence_bytes) = read_result.unwrap();
+        assert_eq!(tag, SEQUENCE);
+        assert_eq!(length, vec![0]);
         assert_eq!(sequence_bytes.len(), 0);
         assert!(!der.at_end());
     }
@@ -229,28 +285,28 @@ mod tests {
     fn der_test_wrong_tag() {
         let input = vec![SEQUENCE, 0];
         let mut der = Der::new(&input);
-        assert!(der.read(INTEGER).is_err());
+        assert!(der.read_tlv(INTEGER).is_err());
     }
 
     #[test]
     fn der_test_truncated_two_byte_length() {
         let input = vec![SEQUENCE, 0x81];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
     fn der_test_truncated_three_byte_length() {
         let input = vec![SEQUENCE, 0x82, 1];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
     fn der_test_truncated_data() {
         let input = vec![SEQUENCE, 20, 1];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
@@ -259,10 +315,13 @@ mod tests {
             SEQUENCE, 20, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0, 0,
         ];
         let mut der = Der::new(&input);
-        let result = der.read(SEQUENCE);
+        let result = der.read_tlv(SEQUENCE);
         assert!(result.is_ok());
+        let (tag, length, value) = result.unwrap();
+        assert_eq!(tag, SEQUENCE);
+        assert_eq!(length, vec![20]);
         assert_eq!(
-            result.unwrap(),
+            value,
             [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 0, 0]
         );
         assert!(der.at_end());
@@ -272,21 +331,21 @@ mod tests {
     fn der_test_not_shortest_two_byte_length_encoding() {
         let input = vec![SEQUENCE, 0x81, 1, 1];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
     fn der_test_not_shortest_three_byte_length_encoding() {
         let input = vec![SEQUENCE, 0x82, 0, 1, 1];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
     fn der_test_indefinite_length_unsupported() {
         let input = vec![SEQUENCE, 0x80, 1, 2, 3, 0x00, 0x00];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
@@ -296,7 +355,7 @@ mod tests {
         // support lengths 2^16 and up).
         let input = vec![SEQUENCE, 0x83, 0x01, 0x00, 0x01, 1, 1, 1, 1];
         let mut der = Der::new(&input);
-        assert!(der.read(SEQUENCE).is_err());
+        assert!(der.read_tlv(SEQUENCE).is_err());
     }
 
     #[test]
@@ -304,6 +363,7 @@ mod tests {
         let empty = Vec::new();
         assert!(read_rsa_modulus(&empty).is_err());
         assert!(read_ec_sig_point(&empty).is_err());
+        assert!(read_encoded_serial_number(&empty).is_err());
     }
 
     #[test]
@@ -311,6 +371,7 @@ mod tests {
         let empty = vec![SEQUENCE | CONSTRUCTED];
         assert!(read_rsa_modulus(&empty).is_err());
         assert!(read_ec_sig_point(&empty).is_err());
+        assert!(read_encoded_serial_number(&empty).is_err());
     }
 
     #[test]
@@ -320,5 +381,20 @@ mod tests {
         assert!(result.is_ok());
         let modulus = result.unwrap();
         assert_eq!(modulus, include_bytes!("../test/modulus.bin").to_vec());
+    }
+
+    #[test]
+    fn test_read_serial_number() {
+        let certificate = include_bytes!("../test/certificate.bin");
+        let result = read_encoded_serial_number(certificate);
+        assert!(result.is_ok());
+        let serial_number = result.unwrap();
+        assert_eq!(
+            serial_number,
+            &[
+                0x02, 0x14, 0x3f, 0xed, 0x7b, 0x43, 0x47, 0x8a, 0x53, 0x42, 0x5b, 0x0d, 0x50, 0xe1,
+                0x37, 0x88, 0x2a, 0x20, 0x3f, 0x31, 0x17, 0x20
+            ]
+        );
     }
 }
