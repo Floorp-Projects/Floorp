6e2a2c05cbeb0cab3dddd8d8a30d4c9f8c2c6449
Molly Howell <mhowell@mozilla.com>
2020-03-16 20:16:25 +0000

    Bug 1602463 Part 3 - Windows default browser agent. r=agashlin,bytesized,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D61889

---
+++ toolkit/mozapps/defaultagent/ScheduledTask.h
@@ -0,0 +1,19 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __DEFAULT_BROWSER_AGENT_SCHEDULED_TASK_H__
+#define __DEFAULT_BROWSER_AGENT_SCHEDULED_TASK_H__
+
+#include <windows.h>
+
+// uniqueToken should be a string unique to the installation, so that a
+// separate task can be created for each installation. Typically this will be
+// the install hash string.
+HRESULT RegisterTask(const wchar_t* uniqueToken, BSTR startTime = nullptr);
+HRESULT UpdateTask(const wchar_t* uniqueToken);
+HRESULT RemoveTask(const wchar_t* uniqueToken);
+
+#endif  // __DEFAULT_BROWSER_AGENT_SCHEDULED_TASK_H__
