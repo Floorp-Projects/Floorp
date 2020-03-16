9f28f89085c9c1e829b2784c1d5768509aac2bc4
Emilio Cobos Álvarez <emilio@crisal.io>
2020-03-16 17:41:07 +0000

    Bug 253870 - Make disabled form controls selectable. r=masayuki,MarcoZ
    This rejiggers a bit the way selection focus is handled so that focusing a
    disabled form control with the mouse handles selection properly, and hides the
    document selection and so on.
    
    This matches the behavior of other browsers as far as I can tell.
    
    Given now readonly and disabled editors behave the same, we can simplify a bit
    the surrounding editor code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66464
    
    --HG--
    extra : moz-landing-system : lando

--- editor/libeditor/EditorBase.h
+++ editor/libeditor/EditorBase.h
@@ -491,10 +491,6 @@ class EditorBase : public nsIEditor,
     return (mFlags & nsIEditor::eEditorReadonlyMask) != 0;
   }
 
-  bool IsDisabled() const {
-    return (mFlags & nsIEditor::eEditorDisabledMask) != 0;
-  }
-
   bool IsInputFiltered() const {
     return (mFlags & nsIEditor::eEditorFilterInputMask) != 0;
   }
@@ -2554,8 +2550,7 @@ class EditorBase : public nsIEditor,
     // Check for password/readonly/disabled, which are not spellchecked
     // regardless of DOM. Also, check to see if spell check should be skipped
     // or not.
-    return !IsPasswordEditor() && !IsReadonly() && !IsDisabled() &&
-           !ShouldSkipSpellCheck();
+    return !IsPasswordEditor() && !IsReadonly() && !ShouldSkipSpellCheck();
   }
 
   /**
