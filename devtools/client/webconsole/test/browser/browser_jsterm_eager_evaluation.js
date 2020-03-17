e81bf37db1ae0bfcff316ce5a9fa3bd23c50206a
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-03-17 11:58:06 +0000

    Bug 1603358 - Enable Eager evaluation in all channels. r=Honza. a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D67116
    
    --HG--
    extra : source : af76a7b080958cd13e90efe4d86e01f55995bf26

--- devtools/client/webconsole/test/browser/browser_jsterm_eager_evaluation.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_eager_evaluation.js
@@ -23,7 +23,6 @@ const EAGER_EVALUATION_PREF = "devtools.webconsole.input.eagerEvaluation";
 // eagerly evaluated should show their results, and expressions with side
 // effects should not perform those side effects.
 add_task(async function() {
-  await pushPref(EAGER_EVALUATION_PREF, true);
   const hud = await openNewTabAndConsole(TEST_URI);
 
   // Do an evaluation to populate $_
@@ -228,7 +227,6 @@ add_task(async function() {
 
 // Test that the currently selected autocomplete result is eagerly evaluated.
 add_task(async function() {
-  await pushPref(EAGER_EVALUATION_PREF, true);
   const hud = await openNewTabAndConsole(TEST_URI);
   const { jsterm } = hud;
 
@@ -281,12 +279,6 @@ add_task(async function() {
 
 // Test that the setting works as expected.
 add_task(async function() {
-  // Settings is only enabled on Nightly at the moment.
-  if (!AppConstants.NIGHTLY_BUILD) {
-    ok(true);
-    return;
-  }
-
   // start with the pref off.
   await pushPref(EAGER_EVALUATION_PREF, false);
   const hud = await openNewTabAndConsole(TEST_URI);
