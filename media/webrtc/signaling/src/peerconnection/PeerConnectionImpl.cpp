1ab6ddb396a3b8e7c8a43c5e3f01a63c0a531c3e
Dan Minor <dminor@mozilla.com>
2020-03-17 14:25:38 +0000

    Bug 1622923 - Remove expiring WebRTC hostname obfuscation telemetry; r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D67038

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
@@ -2502,51 +2502,6 @@ void PeerConnectionImpl::IceConnectionStateChange(
       MOZ_ASSERT_UNREACHABLE("Unexpected mIceConnectionState!");
   }
 
-  // Collect telemetry for situations where hostname obfuscation would be used
-  uint64_t winId = GetWindow()->WindowID();
-  bool iceJustFailed =
-      mIceConnectionState == dom::RTCIceConnectionState::Failed;
-  bool iceJustSucceeded =
-      mIceConnectionState == dom::RTCIceConnectionState::Connected;
-  if (!MediaManager::Get()->IsActivelyCapturingOrHasAPermission(winId)) {
-    bool enabled = Preferences::GetBool(
-        "media.peerconnection.ice.obfuscate_host_addresses", false);
-    if (enabled) {
-      if (!mIceFinished && !mIceStartTime.IsNull() &&
-          (iceJustFailed || iceJustSucceeded)) {
-        Telemetry::AccumulateTimeDelta(
-            Telemetry::WEBRTC_HOSTNAME_OBFUSCATION_ENABLED_ICE_DURATION_MS,
-            mIceStartTime, TimeStamp::Now());
-      }
-      if (iceJustSucceeded) {
-        Telemetry::ScalarAdd(
-            Telemetry::ScalarID::WEBRTC_HOSTNAMEOBFUSCATION_ENABLED_SUCCEEDED,
-            1);
-      } else if (iceJustFailed) {
-        Telemetry::ScalarAdd(
-            Telemetry::ScalarID::WEBRTC_HOSTNAMEOBFUSCATION_ENABLED_FAILED, 1);
-      }
-    } else {
-      if (!mIceFinished && !mIceStartTime.IsNull() &&
-          (iceJustFailed || iceJustSucceeded)) {
-        Telemetry::AccumulateTimeDelta(
-            Telemetry::WEBRTC_HOSTNAME_OBFUSCATION_DISABLED_ICE_DURATION_MS,
-            mIceStartTime, TimeStamp::Now());
-      }
-      if (iceJustSucceeded) {
-        Telemetry::ScalarAdd(
-            Telemetry::ScalarID::WEBRTC_HOSTNAMEOBFUSCATION_DISABLED_SUCCEEDED,
-            1);
-      } else if (iceJustFailed) {
-        Telemetry::ScalarAdd(
-            Telemetry::ScalarID::WEBRTC_HOSTNAMEOBFUSCATION_DISABLED_FAILED, 1);
-      }
-    }
-    if (iceJustFailed || iceJustSucceeded) {
-      mIceFinished = true;
-    }
-  }
-
   WrappableJSErrorResult rv;
   mPCObserver->OnStateChange(PCObserverStateType::IceConnectionState, rv);
 }
