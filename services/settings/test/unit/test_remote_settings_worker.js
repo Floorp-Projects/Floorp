991a8093f03535bbc2bc940bf4fdf44b725a1a01
Mathieu Leplatre <mathieu@mozilla.com>
2020-03-17 20:22:32 +0000

    Bug 1620185 - Simplify CanonicalJSON of records r=glasserc
    Differential Revision: https://phabricator.services.mozilla.com/D66750

--- services/settings/test/unit/test_remote_settings_worker.js
+++ services/settings/test/unit/test_remote_settings_worker.js
@@ -19,27 +19,21 @@ XPCOMUtils.defineLazyGlobalGetters(this, ["indexedDB"]);
 
 const IS_ANDROID = AppConstants.platform == "android";
 
-add_task(async function test_canonicaljson_merges_remote_into_local() {
-  const localRecords = [
+add_task(async function test_canonicaljson() {
+  const records = [
     { id: "1", title: "title 1" },
     { id: "2", title: "title 2" },
-    { id: "3", title: "title 3" },
-  ];
-  const remoteRecords = [
-    { id: "2", title: "title b" },
-    { id: "3", deleted: true },
   ];
   const timestamp = 42;
 
   const serialized = await RemoteSettingsWorker.canonicalStringify(
-    localRecords,
-    remoteRecords,
+    records,
     timestamp
   );
 
   Assert.equal(
     serialized,
-    '{"data":[{"id":"1","title":"title 1"},{"id":"2","title":"title b"}],"last_modified":"42"}'
+    '{"data":[{"id":"1","title":"title 1"},{"id":"2","title":"title 2"}],"last_modified":"42"}'
   );
 });
 
@@ -67,11 +61,11 @@ add_task(async function test_import_json_dump_into_idb() {
 add_task(async function test_throws_error_if_worker_fails() {
   let error;
   try {
-    await RemoteSettingsWorker.canonicalStringify(null, [], 42);
+    await RemoteSettingsWorker.canonicalStringify(null, 42);
   } catch (e) {
     error = e;
   }
-  Assert.equal(error.message.endsWith("localRecords is null"), true);
+  Assert.equal(error.message.endsWith("records is null"), true);
 });
 
 add_task(async function test_throws_error_if_worker_fails_async() {
@@ -111,7 +105,7 @@ add_task(async function test_stops_worker_after_timeout() {
     1
   );
   // Run a task:
-  let serialized = await RemoteSettingsWorker.canonicalStringify([], [], 42);
+  let serialized = await RemoteSettingsWorker.canonicalStringify([], 42);
   Assert.equal(serialized, '{"data":[],"last_modified":"42"}', "API works.");
   // Check that the worker gets stopped now the task is done:
   await TestUtils.waitForCondition(() => !RemoteSettingsWorker.worker);
@@ -121,7 +115,7 @@ add_task(async function test_stops_worker_after_timeout() {
     600000
   );
   // Run another task:
-  serialized = await RemoteSettingsWorker.canonicalStringify([], [], 42);
+  serialized = await RemoteSettingsWorker.canonicalStringify([], 42);
   Assert.equal(
     serialized,
     '{"data":[],"last_modified":"42"}',
