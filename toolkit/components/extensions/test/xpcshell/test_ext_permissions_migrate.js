1f2a1b7d797f62dd7351290b6bf467323cd6eb54
Shane Caraveo <scaraveo@mozilla.com>
2020-03-18 18:22:56 +0000

    Bug 1622117 load old addon manifest during update if necessary, to migrate permissions r=aswan a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D66690
    
    --HG--
    extra : source : 9ca2af89c949f76646604514c7f9d22c1bfbbf7d

--- toolkit/components/extensions/test/xpcshell/test_ext_permissions_migrate.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_permissions_migrate.js
@@ -68,6 +68,9 @@ add_task(async function test_migrated_permission_to_optional() {
     "http://other.com/*",
   ];
 
+  // Restart the addon manager to flush the AddonInternal instance created
+  // when installing the addon above.  See bug 1622117.
+  await AddonTestUtils.promiseRestartManager();
   await extension.upgrade(extensionData);
 
   equal(extension.version, "2.0", "Expected extension version");
