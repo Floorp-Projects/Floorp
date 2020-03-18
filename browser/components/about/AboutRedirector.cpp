4f48d0ec5b685bf49f2f629eba44faf9ecab2b07
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-03-18 18:05:02 +0000

    Bug 1623332 - remove the non-functional about:library stub, r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D67323
    
    --HG--
    extra : moz-landing-system : lando

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -89,9 +89,6 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::ALLOW_SCRIPT},
-    {"library", "chrome://browser/content/aboutLibrary.xhtml",
-     nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
-         nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT},
     {"pocket-saved", "chrome://pocket/content/panels/saved.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
