71659472e774c7538d66d168638d6d4aa5d8e3ae
Ciure Andrei <aciure@mozilla.com>
2020-03-18 05:32:00 +0200

    Backed out 2 changesets (bug 1618781) for toolchain bustages CLOSED TREE
    Backed out changeset 83026834b242 (bug 1618781)
    Backed out changeset ac168e956e3a (bug 1618781)

--- taskcluster/scripts/misc/exfiltrate-pdbstr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x -e -v
-
-# Copy the file that already exists on Windows workers.
-mkdir pdbstr
-cp "c:/Program Files (x86)/Windows Kits/10/Debuggers/$1/srcsrv/pdbstr.exe" pdbstr
-
-tar -jcvf pdbstr.tar.bz2 pdbstr
