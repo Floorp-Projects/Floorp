766875d32eb4e14ec9f93b60d8609815812e4fbd
Mike Hommey <mh+mozilla@glandium.org>
2020-03-18 04:21:18 +0000

    Bug 1618781 - Use the taskcluster artifact for pdbstr on both Windows native and cross builds. r=froydnj
    And make it an explicit dependency of the build system.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67086

--- testing/mozharness/configs/builds/taskcluster_base_win32.py
+++ testing/mozharness/configs/builds/taskcluster_base_win32.py
@@ -1,7 +1,4 @@
 config = {
     'platform': 'win32',
-    'env': {
-        'PDBSTR_PATH': 'C:/Program Files (x86)/Windows Kits/10/Debuggers/x86/srcsrv/pdbstr.exe',
-    },
     'mozconfig_platform': 'win32',
 }
