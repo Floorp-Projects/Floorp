815e0bb3af56934641d820da5434d0afe260c3b4
Ryan Hunt <rhunt@eqrion.net>
2020-03-18 20:45:15 +0000

    Bug 1612534 - Manually fix 'debug/' jit-tests. r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D67261

--- js/src/jit-test/tests/debug/wasm-step.js
+++ js/src/jit-test/tests/debug/wasm-step.js
@@ -7,7 +7,7 @@ load(libdir + "wasm.js");
 // Checking if we stop at every wasm instruction during step.
 var onEnterFrameCalled, onLeaveFrameCalled, onStepCalled;
 wasmRunWithDebugger(
-    '(module (func (nop) (nop)) (export "test" 0))',
+    '(module (func (nop) (nop)) (export "test" (func 0)))',
     undefined,
     function ({dbg}) {
         onEnterFrameCalled = 0;
@@ -38,7 +38,7 @@ wasmRunWithDebugger(
 
 // Checking if step mode was disabled after debugger has been detached.
 wasmRunWithDebugger(
-    '(module (func (nop) (nop)) (export "test" 0))',
+    '(module (func (nop) (nop)) (export "test" (func 0)))',
     undefined,
     function ({dbg, g}) {
         onEnterFrameCalled = 0;
