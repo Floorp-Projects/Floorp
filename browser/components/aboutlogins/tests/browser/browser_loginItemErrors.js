26d215103fc62f24789fdf02b261b95970274f6e
Daniel Varga <dvarga@mozilla.com>
2020-03-18 19:37:55 +0200

    Backed out 2 changesets (bug 1611914, bug 1605494) for causing browser chrome failures at browser/components/aboutlogins/tests/browser/browser_osAuthDialog.js
    CLOSED TREE
    
    Backed out changeset b0ef46ae3853 (bug 1611914)
    Backed out changeset f93685e544c5 (bug 1605494)

--- browser/components/aboutlogins/tests/browser/browser_loginItemErrors.js
+++ browser/components/aboutlogins/tests/browser/browser_loginItemErrors.js
@@ -25,16 +25,16 @@ add_task(async function test_showLoginItemErrors() {
   );
   LOGIN_TO_UPDATE = Services.logins.addLogin(LOGIN_TO_UPDATE);
   EXPECTED_ERROR_MESSAGE = "This login already exists.";
-  const LOGIN_UPDATES = {
-    origin: "https://example.com",
-    password: "my1GoodPassword",
-    username: "user1",
-  };
 
   await SpecialPowers.spawn(
     browser,
-    [[LoginHelper.loginToVanillaObject(LOGIN_TO_UPDATE), LOGIN_UPDATES]],
-    async ([loginToUpdate, loginUpdates]) => {
+    [
+      [
+        LoginHelper.loginToVanillaObject(LOGIN_TO_UPDATE),
+        OSKeyStoreTestUtils.canTestOSKeyStoreLogin(),
+      ],
+    ],
+    async ([loginToUpdate, canTestOSKeyStoreLogin]) => {
       const loginItem = Cu.waiveXrays(
         content.document.querySelector("login-item")
       );
@@ -47,6 +47,12 @@ add_task(async function test_showLoginItemErrors() {
 
       const createButton = loginList._createLoginButton;
       createButton.click();
+
+      const loginUpdates = {
+        origin: "https://example.com",
+        password: "my1GoodPassword",
+        username: "user1",
+      };
 
       const event = Cu.cloneInto(
         {
@@ -102,20 +108,12 @@ add_task(async function test_showLoginItemErrors() {
         loginItemErrorMessage.hidden,
         "The error message should no longer be visible."
       );
-    }
-  );
-  if (!OSKeyStoreTestUtils.canTestOSKeyStoreLogin()) {
-    // The rest of the test uses Edit mode which causes an OS prompt in official builds.
-    return;
-  }
-  let reauthObserved = OSKeyStoreTestUtils.waitForOSKeyStoreLogin(true);
-  await SpecialPowers.spawn(
-    browser,
-    [[LoginHelper.loginToVanillaObject(LOGIN_TO_UPDATE), LOGIN_UPDATES]],
-    async ([loginToUpdate, loginUpdates]) => {
-      const loginItem = Cu.waiveXrays(
-        content.document.querySelector("login-item")
-      );
+
+      if (!canTestOSKeyStoreLogin) {
+        // The rest of the test uses Edit mode which causes an OS prompt in opt builds.
+        return;
+      }
+
       const editButton = loginItem.shadowRoot.querySelector(".edit-button");
       editButton.click();
 
@@ -132,9 +130,6 @@ add_task(async function test_showLoginItemErrors() {
         new content.CustomEvent("AboutLoginsUpdateLogin", updateEvent)
       );
 
-      const loginItemErrorMessage = Cu.waiveXrays(
-        loginItem.shadowRoot.querySelector(".error-message")
-      );
       const loginAlreadyExistsErrorShownAfterUpdate = await ContentTaskUtils.waitForCondition(
         () => {
           return !loginItemErrorMessage.hidden;
@@ -147,8 +142,5 @@ add_task(async function test_showLoginItemErrors() {
       );
     }
   );
-  info("making sure os auth dialog is shown");
-  await reauthObserved;
-  info("saw os auth dialog");
   EXPECTED_ERROR_MESSAGE = null;
 });
