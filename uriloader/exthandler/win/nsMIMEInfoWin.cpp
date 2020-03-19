c1e5d642df9c8c129ff2f00d8ca8e7684f7255dd
Toshihito Kikuchi <tkikuchi@mozilla.com>
2020-03-19 16:09:18 +0000

    Bug 1609451 - Skip ShellExecuteByExplorer to open a uri in Thunderbird.  r=froydnj
    We use `ShellExecuteByExplorer` to open a uri because applications may not
    support the mitigation policies inherited from our process.
    
    Unlike Firefox, Thunderbird runs `ShellExecuteByExplorer` to launch a browser
    to open an HTTP/HTTPS uri.  If a uri includes credentials like
    http://user:password@domain.com/, `ShellExecuteByExplorer` succceds, but
    explorer.exe refuses to open the uri by design.  In this case the fallback to
    `ShellExecuteExW` does not happen because the API is async and no error is
    returned
    
    The proposed fix is to skip `ShellExecuteByExplorer` if it's Thunderbird.
    Because Thunderbird does not apply additional mitigation policies which may
    cause compat issues, we don't need to run `ShellExecuteByExplorer` in the
    first place.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67339
    
    --HG--
    extra : moz-landing-system : lando

--- uriloader/exthandler/win/nsMIMEInfoWin.cpp
+++ uriloader/exthandler/win/nsMIMEInfoWin.cpp
@@ -280,22 +280,38 @@ nsresult nsMIMEInfoWin::LoadUriInternal(nsIURI* aURL) {
     _variant_t workingDir;
     _variant_t showCmd(SW_SHOWNORMAL);
 
-    // Ask Explorer to ShellExecute on our behalf, as some URL handlers do not
-    // start correctly when inheriting our process's process migitations.
+    // To open a uri, we first try ShellExecuteByExplorer, which starts a new
+    // process as a child process of explorer.exe, because applications may not
+    // support the mitigation policies inherited from our process.  If it fails,
+    // we fall back to ShellExecuteExW.
+    //
+    // For Thunderbird, however, there is a known issue that
+    // ShellExecuteByExplorer succeeds but explorer.exe shows an error popup
+    // if a uri to open includes credentials.  This does not happen in Firefox
+    // because Firefox does not have to launch a process to open a uri.
+    //
+    // Since Thunderbird does not use mitigation policies which could cause
+    // compatibility issues, we get no benefit from using
+    // ShellExecuteByExplorer.  Thus we skip it and go straight to
+    // ShellExecuteExW for Thunderbird.
+#ifndef MOZ_THUNDERBIRD
     mozilla::LauncherVoidResult shellExecuteOk =
         mozilla::ShellExecuteByExplorer(validatedUri.inspect(), args, verb,
                                         workingDir, showCmd);
-    if (shellExecuteOk.isErr()) {
-      SHELLEXECUTEINFOW sinfo = {sizeof(sinfo)};
-      sinfo.fMask = SEE_MASK_NOASYNC;
-      sinfo.lpVerb = V_BSTR(&verb);
-      sinfo.nShow = showCmd;
-      sinfo.lpFile = validatedUri.inspect();
+    if (shellExecuteOk.isOk()) {
+      return NS_OK;
+    }
+#endif  // MOZ_THUNDERBIRD
 
-      BOOL result = ShellExecuteExW(&sinfo);
-      if (!result || reinterpret_cast<LONG_PTR>(sinfo.hInstApp) < 32) {
-        rv = NS_ERROR_FAILURE;
-      }
+    SHELLEXECUTEINFOW sinfo = {sizeof(sinfo)};
+    sinfo.fMask = SEE_MASK_NOASYNC;
+    sinfo.lpVerb = V_BSTR(&verb);
+    sinfo.nShow = showCmd;
+    sinfo.lpFile = validatedUri.inspect();
+
+    BOOL result = ShellExecuteExW(&sinfo);
+    if (!result || reinterpret_cast<LONG_PTR>(sinfo.hInstApp) < 32) {
+      rv = NS_ERROR_FAILURE;
     }
   }
 
