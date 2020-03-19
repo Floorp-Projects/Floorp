0da2ea0e8b8fad32d75fca0c69ebc3bc904eed59
Simon Giesecke <sgiesecke@mozilla.com>
2020-03-19 09:33:31 +0000

    Bug 1623278 - Use SafeRefPtr for FileInfo. r=dom-workers-and-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D65550

--- dom/indexedDB/IndexedDatabase.h
+++ dom/indexedDB/IndexedDatabase.h
@@ -13,6 +13,7 @@
 #include "nsTArray.h"
 #include "InitializedOnce.h"
 #include "FileInfoFwd.h"
+#include "SafeRefPtr.h"
 
 namespace mozilla {
 namespace dom {
@@ -110,7 +111,7 @@ struct StructuredCloneFileParent : StructuredCloneFileBase {
   StructuredCloneFileParent& operator=(StructuredCloneFileParent&&) = delete;
 
   // In IndexedDatabaseInlines.h
-  StructuredCloneFileParent(FileType aType, RefPtr<FileInfo> aFileInfo);
+  StructuredCloneFileParent(FileType aType, SafeRefPtr<FileInfo> aFileInfo);
 
   // In IndexedDatabaseInlines.h
   ~StructuredCloneFileParent();
@@ -123,10 +124,10 @@ struct StructuredCloneFileParent : StructuredCloneFileBase {
   const indexedDB::FileInfo& FileInfo() const { return ***mContents; }
 
   // In IndexedDatabaseInlines.h
-  RefPtr<indexedDB::FileInfo> FileInfoPtr() const;
+  SafeRefPtr<indexedDB::FileInfo> FileInfoPtr() const;
 
  private:
-  InitializedOnce<const Maybe<RefPtr<indexedDB::FileInfo>>> mContents;
+  InitializedOnce<const Maybe<SafeRefPtr<indexedDB::FileInfo>>> mContents;
 };
 
 struct StructuredCloneReadInfoBase {
