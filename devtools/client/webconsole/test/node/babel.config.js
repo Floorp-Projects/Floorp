c3343e8d21effc2917a35f853e2982569132ce37
Jason Laster <jlaster@mozilla.com>
2020-03-19 21:11:12 +0000

    Bug 1622996 - Refactor devtools codebase to use optional chaining. r=jdescottes
    Depends on D67540
    
    Differential Revision: https://phabricator.services.mozilla.com/D67084

--- devtools/client/webconsole/test/node/babel.config.js
+++ devtools/client/webconsole/test/node/babel.config.js
@@ -6,7 +6,6 @@
 
 module.exports = {
   plugins: [
-    "@babel/plugin-proposal-async-generator-functions",
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-nullish-coalescing-operator",
   ],
