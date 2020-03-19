ae0844e0275fc845cab98e12a6dd36d43e32083c
Kristen Wright <kwright@mozilla.com>
2020-03-19 00:54:13 +0000

    bug 1622111 - Convert intl.ime.hack.on_ime_unaware_apps.fire_key_events_for_composition to static pref r=njn
    Converts `intl.ime.hack.on_ime_unaware_apps.fire_key_events_for_composition` to a static pref.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67203

--- dom/events/IMEStateManager.cpp
+++ dom/events/IMEStateManager.cpp
@@ -13,9 +13,9 @@
 #include "mozilla/EventListenerManager.h"
 #include "mozilla/EventStates.h"
 #include "mozilla/MouseEvents.h"
-#include "mozilla/Preferences.h"
 #include "mozilla/PresShell.h"
 #include "mozilla/StaticPrefs_dom.h"
+#include "mozilla/StaticPrefs_intl.h"
 #include "mozilla/TextComposition.h"
 #include "mozilla/TextEvents.h"
 #include "mozilla/ToString.h"
@@ -76,15 +76,9 @@ InputContext::Origin IMEStateManager::sOrigin = InputContext::ORIGIN_MAIN;
 InputContext IMEStateManager::sActiveChildInputContext;
 bool IMEStateManager::sInstalledMenuKeyboardListener = false;
 bool IMEStateManager::sIsGettingNewIMEState = false;
-bool IMEStateManager::sCheckForIMEUnawareWebApps = false;
 
 // static
 void IMEStateManager::Init() {
-  Preferences::AddBoolVarCache(
-      &sCheckForIMEUnawareWebApps,
-      "intl.ime.hack.on_ime_unaware_apps.fire_key_events_for_composition",
-      false);
-
   sOrigin = XRE_IsParentProcess() ? InputContext::ORIGIN_MAIN
                                   : InputContext::ORIGIN_CONTENT;
   ResetActiveChildInputContext();
@@ -1263,9 +1257,11 @@ void IMEStateManager::SetIMEState(const IMEState& aState,
   InputContext context;
   context.mIMEState = aState;
   context.mOrigin = aOrigin;
-  context.mMayBeIMEUnaware = context.mIMEState.IsEditable() &&
-                             sCheckForIMEUnawareWebApps &&
-                             MayBeIMEUnawareWebApp(aContent);
+  context.mMayBeIMEUnaware =
+      context.mIMEState.IsEditable() &&
+      StaticPrefs::
+          intl_ime_hack_on_ime_unaware_apps_fire_key_events_for_composition() &&
+      MayBeIMEUnawareWebApp(aContent);
 
   context.mHasHandledUserInput =
       aPresContext && aPresContext->PresShell()->HasHandledUserInput();
