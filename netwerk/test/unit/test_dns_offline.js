ad8d78570f291fb11d913d1b7c3083cddee38d8d
Andrea Marchesini <amarchesini@mozilla.com>
2020-03-19 13:06:48 +0000

    Bug 1220810 - Hardcode localhost to loopback, r=ckerschb,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D64586

--- netwerk/test/unit/test_dns_offline.js
+++ netwerk/test/unit/test_dns_offline.js
@@ -43,6 +43,8 @@ const defaultOriginAttributes = {};
 function run_test() {
   do_test_pending();
   prefs.setBoolPref("network.dns.offline-localhost", false);
+  // We always resolve localhost as it's hardcoded without the following pref:
+  prefs.setBoolPref("network.proxy.allow_hijacking_localhost", true);
   ioService.offline = true;
   try {
     dns.asyncResolve(
@@ -97,4 +99,5 @@ function test3Continued() {
 
 function cleanup() {
   prefs.clearUserPref("network.dns.offline-localhost");
+  prefs.clearUserPref("network.proxy.allow_hijacking_localhost");
 }
