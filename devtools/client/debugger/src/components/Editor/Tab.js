3a645039c765aaadd823718211616539fa8ae801
David Walsh <dwalsh@mozilla.com>
2020-03-19 18:18:22 +0000

    Bug 1617781 - Show icon for web extension JavaScript files r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D67417

--- devtools/client/debugger/src/components/Editor/Tab.js
+++ devtools/client/debugger/src/components/Editor/Tab.js
@@ -242,7 +242,9 @@ class Tab extends PureComponent<Props> {
       >
         <SourceIcon
           source={source}
-          shouldHide={icon => ["file", "javascript"].includes(icon)}
+          modifier={icon =>
+            ["file", "javascript"].includes(icon) ? null : icon
+          }
         />
         <div className="filename">
           {getTruncatedFileName(source, query)}
