271d69406b36dad4c0c507c1a2a130d7eaeb85fb
anjali1903 <anjali.jha1903@gmail.com>
2020-03-20 18:58:10 +0000

    Bug 1622797 Replace URIParams with nsIURI in PFTPChannel.ipdl r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D67684
    
    --HG--
    extra : moz-landing-system : lando

--- netwerk/protocol/ftp/FTPChannelChild.h
+++ netwerk/protocol/ftp/FTPChannelChild.h
@@ -83,7 +83,7 @@ class FTPChannelChild final : public PFTPChannelChild,
                                              const nsCString& aContentType,
                                              const PRTime& aLastModified,
                                              const nsCString& aEntityID,
-                                             const URIParams& aURI) override;
+                                             nsIURI* aURI) override;
   mozilla::ipc::IPCResult RecvOnDataAvailable(const nsresult& aChannelStatus,
                                               const nsCString& aData,
                                               const uint64_t& aOffset,
@@ -101,7 +101,7 @@ class FTPChannelChild final : public PFTPChannelChild,
                         const int64_t& aContentLength,
                         const nsCString& aContentType,
                         const PRTime& aLastModified, const nsCString& aEntityID,
-                        const URIParams& aURI);
+                        nsIURI* aURI);
   void DoOnDataAvailable(const nsresult& aChannelStatus, const nsCString& aData,
                          const uint64_t& aOffset, const uint32_t& aCount);
   void DoOnStopRequest(const nsresult& StatusCode, const nsCString& aErrorMsg,
