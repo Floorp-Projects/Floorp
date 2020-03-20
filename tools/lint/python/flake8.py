b18f7cf35ecccd080e771d51d27c00ee36f3ced2
Sylvestre Ledru <sledru@mozilla.com>
2020-03-20 18:49:09 +0000

    no bug - mozlint/flake8 - Show the flake8 version in debug mode r=linter-reviewers,ahal
    Differential Revision: https://phabricator.services.mozilla.com/D67322

--- tools/lint/python/flake8.py
+++ tools/lint/python/flake8.py
@@ -95,6 +95,7 @@ def lint(paths, config, **lintargs):
 
     # Run flake8.
     app = Application()
+    log.debug("flake8 version={}".format(app.version))
 
     output_file = mozfile.NamedTemporaryFile(mode='r')
     flake8_cmd = [
