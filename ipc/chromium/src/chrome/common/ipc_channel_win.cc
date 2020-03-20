15c6a8cb0f5914b81a04bc0e01c6aaa0e170f29a
Doug Thayer <dothayer@mozilla.com>
2020-03-20 20:09:20 +0000

    Bug 1623668 - Suspend late writes for Windows IPC writes r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D67542

--- ipc/chromium/src/chrome/common/ipc_channel_win.cc
+++ ipc/chromium/src/chrome/common/ipc_channel_win.cc
@@ -17,6 +17,7 @@
 #include "base/win_util.h"
 #include "chrome/common/ipc_message_utils.h"
 #include "mozilla/ipc/ProtocolUtils.h"
+#include "mozilla/LateWriteChecks.h"
 
 #ifdef FUZZING
 #  include "mozilla/ipc/Faulty.h"
@@ -496,8 +497,15 @@ bool Channel::ChannelImpl::ProcessOutgoingMessages(
   }
 
   Pickle::BufferList::IterImpl& iter = partial_write_iter_.ref();
+
+  // Don't count this write for the purposes of late write checking. If this
+  // message results in a legitimate file write, that will show up when it
+  // happens.
+  mozilla::PushSuspendLateWriteChecks();
   BOOL ok = WriteFile(pipe_, iter.Data(), iter.RemainingInSegment(),
                       &bytes_written, &output_state_.context.overlapped);
+  mozilla::PopSuspendLateWriteChecks();
+
   if (!ok) {
     DWORD err = GetLastError();
     if (err == ERROR_IO_PENDING) {
