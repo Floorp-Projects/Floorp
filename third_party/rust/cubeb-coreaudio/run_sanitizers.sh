bd48f5eb61b6775aeaad238be74d7a4d590b16ae
Chun-Min Chang <chun.m.chang@gmail.com>
2020-03-20 16:41:27 +0000

    Bug 1619005 - Update cubeb-coreaudio to 799518a. r=padenot
    Pick commits:
    799518a - Rework threading model (#55)
    6e3e8e8 - Fix memory leak (#54)
    996fcfa - Revise messages in test_switch_device
    6fac4a6 - Isolate device tests (#53)
    b78e817 - Save the duplicate tests and compilings when running sanitizers (#52)
    
    Differential Revision: https://phabricator.services.mozilla.com/D67536

--- third_party/rust/cubeb-coreaudio/run_sanitizers.sh
+++ third_party/rust/cubeb-coreaudio/run_sanitizers.sh
@@ -6,42 +6,15 @@
 toolchain=$(rustup default)
 echo "\nUse Rust toolchain: $toolchain"
 
-if [[ $toolchain == nightly-* ]]
-then
-   echo "Run sanitizers!"
-else
+if [[ $toolchain != nightly-* ]]; then
    echo "The sanitizer is only available on Rust Nightly only. Skip."
    exit
 fi
 
-# Run tests in the sub crate
-# -------------------------------------------------------------------------------------------------
-cd coreaudio-sys-utils
-
-echo "\n\nRun ASan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=address" cargo test
-
-echo "\n\nRun LSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=leak" cargo test
-
-echo "\n\nRun MSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=memory" cargo test
-
-echo "\n\nRun TSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=thread" cargo test
-
-cd ..
-
-# Run tests in the main crate
-# -------------------------------------------------------------------------------------------------
-echo "\n\nRun ASan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=address" sh run_tests.sh
-
-echo "\n\nRun LSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=leak" sh run_tests.sh
-
-echo "\n\nRun MSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=memory" sh run_tests.sh
-
-echo "\n\nRun TSan\n-----------\n"
-RUSTFLAGS="-Z sanitizer=thread" sh run_tests.sh
+sanitizers=("address" "leak" "memory" "thread")
+for san in "${sanitizers[@]}"
+do
+    San="$(tr '[:lower:]' '[:upper:]' <<< ${san:0:1})${san:1}"
+    echo "\n\nRun ${San}Sanitizer\n------------------------------"
+    RUSTFLAGS="-Z sanitizer=${san}" sh run_tests.sh
+done
