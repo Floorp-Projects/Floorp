dd8f683562dea35b035a022fbe2824f645da9dcc
alwu <alwu@mozilla.com>
2020-03-20 00:10:31 +0000

    Bug 1621166 - part4 : add test. r=MeFisto94
    Differential Revision: https://phabricator.services.mozilla.com/D67385

---
+++ dom/media/mediacontrol/tests/browser_media_control_stop_timer.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-undef */
+
+// Import this in order to use `triggerPictureInPicture()`.
+/* import-globals-from ../../../../../toolkit/components/pictureinpicture/tests/head.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/toolkit/components/pictureinpicture/tests/head.js",
+  this
+);
+
+const PAGE_NON_AUTOPLAY =
+  "https://example.com/browser/dom/media/mediacontrol/tests/file_non_autoplay.html";
+
+const testVideoId = "video";
+
+add_task(async function setupTestingPref() {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["media.mediacontrol.testingevents.enabled", true],
+// As `main-media-controller-playback-changed` is dispatched asynchronously, if
+// we want to receive it after pausing media, we should postpone the time of
+// triggering the stop timer. That value should be changed to 0 after landing
+// bug1620113.
+      ["media.mediacontrol.stopcontrol.timer.ms", 2000],
+    ],
+  });
+});
+
+/**
+ * This test is used to check the stop timer for media element, which would stop
+ * media control for the specific element when the element has been paused over
+ * certain length of time. (That is controlled by the value of the pref
+ * `media.mediacontrol.stopcontrol.timer.ms`) In this test, we set the pref to 0
+ * which means the stop timer would be triggered after the media is paused.
+ * However, if the media is being used in PIP mode, we won't start the stop
+ * timer for it.
+ */
+add_task(async function testStopMediaControlAfterPausingMedia() {
+  info(`open media page`);
+  const tab = await createTabAndLoad(PAGE_NON_AUTOPLAY);
+
+  info(`start media`);
+  await playMedia(tab, testVideoId);
+
+  info(`pause media and the stop timer would stop media control`);
+  await pauseMediaAndMediaControlShouldBeStopped(tab, testVideoId);
+
+  info(`remove tab`);
+  await BrowserTestUtils.removeTab(tab);
+});
+
+add_task(async function testNotToStopMediaControlForPIPVideo() {
+  info(`open media page`);
+  const tab = await createTabAndLoad(PAGE_NON_AUTOPLAY);
+
+  info(`start media`);
+  await playMedia(tab, testVideoId);
+
+  info(`trigger PIP mode`);
+  let pipWin = await triggerPictureInPicture(tab.linkedBrowser, testVideoId);
+
+  info(`pause media and the stop timer would not stop media control`);
+  await pauseMedia(tab, testVideoId);
+
+  info(`pressing 'play' key should start PIP video again`);
+  await generateMediaControlKeyEvent("play");
+  await checkOrWaitUntilMediaStartedPlaying(tab, testVideoId);
+
+  info(`remove tab`);
+  await BrowserTestUtils.removeTab(tab);
+});
+
+/**
+ * The following is helper function.
+ */
+function pauseMediaAndMediaControlShouldBeStopped(tab, testVideoId) {
+  // After pausing media, the stop timer would be triggered and stop the media
+  // control, which would reset the current main media controller.
+  const controllerChangedPromise = waitUntilMainMediaControllerChanged();
+  return Promise.all([pauseMedia(tab, testVideoId), controllerChangedPromise]);
+}
