8c66344a6ddfa4017fdf33e50de73409ce1ebf84
Mirko Brodesser <mbrodesser@mozilla.com>
2020-03-20 14:27:39 +0000

    Bug 1623858: part 1) Rename `nsINode::IsSelectionDescendant` to `nsINode::IsMaybeSelected`. r=smaug
    More accurate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67599
    
    --HG--
    extra : moz-landing-system : lando

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -3369,9 +3369,8 @@ class nsIFrame : public nsQueryFrame {
    * @returns true if this frame is selected.
    */
   bool IsSelected() const {
-    return (GetContent() && GetContent()->IsSelectionDescendant())
-               ? IsFrameSelected()
-               : false;
+    return (GetContent() && GetContent()->IsMaybeSelected()) ? IsFrameSelected()
+                                                             : false;
   }
 
   /**
