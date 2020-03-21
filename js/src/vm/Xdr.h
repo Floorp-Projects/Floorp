08ca80f03599831ab56437b622e7fca2b1a9d81b
André Bargull <andre.bargull@gmail.com>
2020-03-21 14:25:24 +0000

    Bug 1623957 - Part 11: Replace mozilla::IsEnum with std::is_enum. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D67659
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -9,7 +9,6 @@
 
 #include "mozilla/EndianUtils.h"
 #include "mozilla/MaybeOneOf.h"
-#include "mozilla/TypeTraits.h"
 #include "mozilla/Utf8.h"
 
 #include <type_traits>
@@ -346,9 +345,7 @@ class XDRState : public XDRCoderBase {
    * as C++ will extract the parameterized from the argument list.
    */
   template <typename T>
-  XDRResult codeEnum32(
-      T* val,
-      std::enable_if_t<mozilla::IsEnum<T>::value>* = nullptr) {
+  XDRResult codeEnum32(T* val, std::enable_if_t<std::is_enum_v<T>>* = nullptr) {
     // Mix the enumeration value with a random magic number, such that a
     // corruption with a low-ranged value (like 0) is less likely to cause a
     // miss-interpretation of the XDR content and instead cause a failure.
