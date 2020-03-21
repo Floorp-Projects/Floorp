084bc6d6c529986c90fd5741c2cfb19ca62f82fe
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2020-03-21 03:39:20 +0000

    Bug 1623511: turn DTLS 1.0 for WebRTC back on. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D67407
    
    --HG--
    extra : moz-landing-system : lando

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -506,10 +506,11 @@ pref("media.videocontrols.picture-in-picture.video-toggle.always-show", false);
   pref("media.peerconnection.mute_on_bye_or_timeout", false);
 
   // 770 = DTLS 1.0, 771 = DTLS 1.2, 772 = DTLS 1.3
-  pref("media.peerconnection.dtls.version.min", 771);
 #if defined(NIGHTLY_BUILD)
+  pref("media.peerconnection.dtls.version.min", 771);
   pref("media.peerconnection.dtls.version.max", 772);
 #else
+  pref("media.peerconnection.dtls.version.min", 770);
   pref("media.peerconnection.dtls.version.max", 771);
 #endif
 
