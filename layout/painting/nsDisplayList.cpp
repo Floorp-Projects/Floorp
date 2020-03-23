e29451c49041a7edace4c9f7bc57bfe7bae70911
Botond Ballo <botond@mozilla.com>
2020-03-23 22:20:11 +0000

    Bug 1623476 - Rename enum RelativeTo to DisplayportRelativeTo. r=tnikkel
    This is in anticipation of introducing a struct named RelativeTo
    in bug 1556556.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67515

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -7174,8 +7174,9 @@ bool nsDisplaySubDocument::ComputeVisibility(nsDisplayListBuilder* aBuilder,
   nsRect displayport;
   nsIFrame* rootScrollFrame = mFrame->PresShell()->GetRootScrollFrame();
   MOZ_ASSERT(rootScrollFrame);
-  Unused << nsLayoutUtils::GetDisplayPort(
-      rootScrollFrame->GetContent(), &displayport, RelativeTo::ScrollFrame);
+  Unused << nsLayoutUtils::GetDisplayPort(rootScrollFrame->GetContent(),
+                                          &displayport,
+                                          DisplayportRelativeTo::ScrollFrame);
 
   nsRegion childVisibleRegion;
   // The visible region for the children may be much bigger than the hole we
