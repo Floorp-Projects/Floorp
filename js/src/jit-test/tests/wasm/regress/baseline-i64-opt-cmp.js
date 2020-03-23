0e890254ffb3fcbede7727f6009f1e69f18ae1cf
Ryan Hunt <rhunt@eqrion.net>
2020-03-23 16:17:58 +0000

    Bug 1612534 - Switch to (result) type parameter for block. r=lth
    This commit uses a regex to automatically replace uses of
      block valtype
    with
      block (result valtype)
    
    Differential Revision: https://phabricator.services.mozilla.com/D67247

--- js/src/jit-test/tests/wasm/regress/baseline-i64-opt-cmp.js
+++ js/src/jit-test/tests/wasm/regress/baseline-i64-opt-cmp.js
@@ -5,7 +5,7 @@
 wasmEvalText(
     `(module
       (func $run (param i64) (param i64) (result i64)
-        block i64
+        block (result i64)
 	  i64.const 1
           (i64.lt_s (local.get 0) (local.get 1))
 	  br_if 0
