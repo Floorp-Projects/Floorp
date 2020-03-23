17362d09d0814cc07031b5498bc6e2bc6f3df59b
Dorel Luca <dluca@mozilla.com>
2020-03-23 12:47:26 +0200

    Backed out changeset ff16837de269 (bug 1622757) by dev's request

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -2367,10 +2367,12 @@ void GCRuntime::updateCellPointers(Zone* zone, AllocKinds kinds) {
                               gcstats::PhaseKind::COMPACT_UPDATE_CELLS,
                               bgArenas, SliceBudget::unlimited(), lock);
 
-  AutoUnlockHelperThreadState unlock(lock);
+  ParallelWorker fgTask(this, UpdateArenaListSegmentPointers, fgArenas,
+                        SliceBudget::unlimited(), lock);
 
-  for (; !fgArenas.done(); fgArenas.next()) {
-    UpdateArenaListSegmentPointers(this, fgArenas.get());
+  {
+    AutoUnlockHelperThreadState unlock(lock);
+    fgTask.runFromMainThread();
   }
 }
 
