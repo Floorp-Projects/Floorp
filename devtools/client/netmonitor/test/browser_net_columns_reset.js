e1f12abfda97d4cbcb00270ae006eadbd520fb21
Alexandre Poirot <poirot.alex@gmail.com>
2020-03-23 14:53:51 +0000

    Bug 1622043 - Remove unecessary wait. r=Honza,bomsy
    This test was wrong. The event timings were fire before, when performing the request.
    Not when opening the context menu. So, with the previous changeset, this test started to fail.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66659

--- devtools/client/netmonitor/test/browser_net_columns_reset.js
+++ devtools/client/netmonitor/test/browser_net_columns_reset.js
@@ -27,61 +27,17 @@ add_task(async function() {
   await hideColumn(monitor, "status");
   await hideColumn(monitor, "waterfall");
 
-  const onRequestsFinished = waitForRequestsFinished(monitor);
   EventUtils.sendMouseEvent(
     { type: "contextmenu" },
     document.querySelector("#requests-list-contentSize-button")
   );
 
   getContextMenuItem(monitor, "request-list-header-reset-columns").click();
-  await onRequestsFinished;
 
   ok(
     JSON.stringify(prefBefore) === JSON.stringify(Prefs.visibleColumns),
     "Reset columns item should reset columns pref"
   );
+
+  return teardown(monitor);
 });
-
-/**
- * Helper function for waiting for all events to fire before resolving a promise.
- *
- * @param monitor subject
- *        The event emitter object that is being listened to.
- * @return {Promise}
- *        Returns a promise that resolves upon the last event being fired.
- */
-function waitForRequestsFinished(monitor, event) {
-  const window = monitor.panelWin;
-
-  return new Promise(resolve => {
-    // Key is the request id, value is a boolean - is request finished or not?
-    const requests = new Map();
-
-    function onRequest(id) {
-      info(`Request ${id} not yet done, keep waiting...`);
-      requests.set(id, false);
-    }
-
-    function onEventRequest(id) {
-      info(`Request ${id} `);
-      requests.set(id, true);
-      maybeResolve();
-    }
-
-    function maybeResolve() {
-      // Have all the requests in the map finished yet?
-      if ([...requests.values()].some(finished => !finished)) {
-        return;
-      }
-
-      // All requests are done - unsubscribe from events and resolve!
-      window.api.off(TEST_EVENTS.NETWORK_EVENT, onRequest);
-      window.api.off(EVENTS.RECEIVED_EVENT_TIMINGS, onEventRequest);
-      info("All requests finished");
-      resolve();
-    }
-
-    window.api.on(TEST_EVENTS.NETWORK_EVENT, onRequest);
-    window.api.on(EVENTS.RECEIVED_EVENT_TIMINGS, onEventRequest);
-  });
-}
