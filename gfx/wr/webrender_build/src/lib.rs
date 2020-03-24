9cb73ab9c6baa4670030c2e5a2463f9cffe770b0
Lee Salzman <lsalzman@mozilla.com>
2020-03-24 22:21:25 +0000

    Bug 1624396 - implement centralized shader features list management for WebRender. r=gw,jrmuizel,jnicol
    Differential Revision: https://phabricator.services.mozilla.com/D67958
    
    --HG--
    extra : moz-landing-system : lando

--- gfx/wr/webrender_build/src/lib.rs
+++ gfx/wr/webrender_build/src/lib.rs
@@ -2,8 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#[macro_use]
+extern crate bitflags;
+
 #[cfg(any(feature = "serde"))]
 #[macro_use]
 extern crate serde;
 
 pub mod shader;
+mod shader_features;
