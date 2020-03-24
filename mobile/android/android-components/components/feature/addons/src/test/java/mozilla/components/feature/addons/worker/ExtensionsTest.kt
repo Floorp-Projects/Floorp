e7a50f53ba4361b53fda4573015b5e625665980d
Arturo Mejia <arturomejiamarmol@gmail.com>
2020-03-24 17:08:04 -0400

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/6372: Ignore JobCancellationException for add-on workers

---
+++ mobile/android/android-components/components/feature/addons/src/test/java/mozilla/components/feature/addons/worker/ExtensionsTest.kt
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package mozilla.components.feature.addons.worker
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase.assertTrue
+import java.io.IOException
+import kotlinx.coroutines.CancellationException
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExtensionsTest {
+
+    @Test
+    fun `shouldReport - when cause is an IOException must be reported`() {
+        assertTrue(Exception(IOException()).shouldReport())
+    }
+
+    @Test
+    fun `shouldReport - a CancellationException must be reported`() {
+        assertTrue(CancellationException().shouldReport())
+    }
+}
