b5d243959c53ccb4930f595e77aa36e7bd126d57
Andrea Marchesini <amarchesini@mozilla.com>
2020-03-24 16:30:03 +0000

    Bug 1623593 - Get rid of NECKO_COOKIES compilation flag, r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D67451

--- netwerk/ipc/NeckoParent.cpp
+++ netwerk/ipc/NeckoParent.cpp
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "necko-config.h"
 #include "nsHttp.h"
 #include "mozilla/BasePrincipal.h"
 #include "mozilla/ContentPrincipal.h"
