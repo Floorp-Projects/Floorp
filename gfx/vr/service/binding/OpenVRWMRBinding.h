ed9ea5073e29d8e8c2b0a9ae2380118ec999e715
Daosheng Mu <daoshengmu@gmail.com>
2020-03-24 17:45:53 +0000

    Bug 1624645 - Make adjustment for undefined OpenVR controllers. r=kip
    MozReview-Commit-ID: L3kx4Cqw8hg
    
    Differential Revision: https://phabricator.services.mozilla.com/D68057

--- gfx/vr/service/binding/OpenVRWMRBinding.h
+++ gfx/vr/service/binding/OpenVRWMRBinding.h
@@ -17,7 +17,7 @@ struct OpenVRWMRBinding {
   "  \"version\" : \"0.1\", \n"
   "  \"controller_type\" : \"holographic_controller\", \n"
   "  \"description\" : \"Bindings for Firefox OpenVR for the Windows Mixed Reality controller\", \n"
-  "  \"name\" : \"Firefox bindings for Mixed Reality Controller\", \n"
+  "  \"name\" : \"Firefox bindings for Windows Mixed Reality Controller\", \n"
   "  \"bindings\" : { \n"
   "     \"/actions/firefox\" : { \n"
   "         \"poses\" : [        \n"
