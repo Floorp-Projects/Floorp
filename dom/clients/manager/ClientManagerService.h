3cfd41028c0d69e24e8a18b8930517d3bcd4bdea
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-03-24 11:29:22 +0200

    Backed out 5 changesets (bug 1584007) for causing crashes in ClientManagerService.
    CLOSED TREE
    
    Backed out changeset e8e6a6bb50ff (bug 1584007)
    Backed out changeset 9468d92f2a55 (bug 1584007)
    Backed out changeset 19ed74a7ea6f (bug 1584007)
    Backed out changeset 6c35d3d0f747 (bug 1584007)
    Backed out changeset ea67977a5cdd (bug 1584007)

--- dom/clients/manager/ClientManagerService.h
+++ dom/clients/manager/ClientManagerService.h
@@ -6,12 +6,7 @@
 #ifndef _mozilla_dom_ClientManagerService_h
 #define _mozilla_dom_ClientManagerService_h
 
-#include "ClientHandleParent.h"
 #include "ClientOpPromise.h"
-#include "mozilla/Assertions.h"
-#include "mozilla/HashTable.h"
-#include "mozilla/MozPromise.h"
-#include "mozilla/Variant.h"
 #include "nsDataHashtable.h"
 
 namespace mozilla {
@@ -33,58 +28,13 @@ class ContentParent;
 // browser.  This service runs on the PBackground thread.  To interact
 // it with it please use the ClientManager and ClientHandle classes.
 class ClientManagerService final {
-  // Placeholder type that represents a ClientSourceParent that may be created
-  // in the future (e.g. while a redirect chain is being resolved).
-  //
-  // Each FutureClientSourceParent has a promise that callbacks may be chained
-  // to; the promise will be resolved when the associated ClientSourceParent is
-  // created or rejected when it's known that it'll never be created.
-  class FutureClientSourceParent {
-   public:
-    explicit FutureClientSourceParent(const IPCClientInfo& aClientInfo);
-
-    const mozilla::ipc::PrincipalInfo& PrincipalInfo() const {
-      return mPrincipalInfo;
-    }
-
-    already_AddRefed<SourcePromise> Promise() {
-      return mPromiseHolder.Ensure(__func__);
-    }
-
-    void ResolvePromiseIfExists(ClientSourceParent* aSource) {
-      MOZ_ASSERT(aSource);
-      mPromiseHolder.ResolveIfExists(aSource, __func__);
-    }
-
-    void RejectPromiseIfExists(const CopyableErrorResult& aRv) {
-      MOZ_ASSERT(aRv.Failed());
-      mPromiseHolder.RejectIfExists(aRv, __func__);
-    }
-
-   private:
-    const mozilla::ipc::PrincipalInfo mPrincipalInfo;
-    MozPromiseHolder<SourcePromise> mPromiseHolder;
-  };
-
-  using SourceTableEntry =
-      Variant<FutureClientSourceParent, ClientSourceParent*>;
-
-  struct nsIDHasher {
-    using Key = nsID;
-    using Lookup = Key;
-
-    static HashNumber hash(const Lookup& aLookup) {
-      return HashBytes(&aLookup, sizeof(Lookup));
-    }
-
-    static bool match(const Key& aKey, const Lookup& aLookup) {
-      return aKey.Equals(aLookup);
-    }
-  };
-
-  // Store the possible ClientSourceParent objects in a hash table.  We want to
+  // Store the ClientSourceParent objects in a hash table.  We want to
   // optimize for insertion, removal, and lookup by UUID.
-  HashMap<nsID, SourceTableEntry, nsIDHasher> mSourceTable;
+  nsDataHashtable<nsIDHashKey, ClientSourceParent*> mSourceTable;
+
+  // The set of handles waiting for their corresponding ClientSourceParent
+  // to be created.
+  nsDataHashtable<nsIDHashKey, nsTArray<ClientHandleParent*>> mPendingHandles;
 
   nsTArray<ClientManagerParent*> mManagerList;
 
@@ -94,16 +44,6 @@ class ClientManagerService final {
   ~ClientManagerService();
 
   void Shutdown();
-
-  // Returns nullptr if aEntry isn't a ClientSourceParent (i.e. it's a
-  // FutureClientSourceParent).
-  ClientSourceParent* MaybeUnwrapAsExistingSource(
-      const SourceTableEntry& aEntry) const;
-
-  // Returns nullptr if the ClientSourceParent doesn't exist yet (i.e. it's a
-  // FutureClientSourceParent or has already been destroyed) or is frozen.
-  ClientSourceParent* FindExistingSource(
-      const nsID& aID, const mozilla::ipc::PrincipalInfo& aPrincipalInfo) const;
 
  public:
   static already_AddRefed<ClientManagerService> GetOrCreateInstance();
@@ -115,15 +55,14 @@ class ClientManagerService final {
 
   bool RemoveSource(ClientSourceParent* aSource);
 
-  // Returns true when a FutureClientSourceParent is successfully added.
-  bool ExpectFutureSource(const IPCClientInfo& aClientInfo);
-
-  // May still be called if it's possible that the FutureClientSourceParent
-  // no longer exists.
-  void ForgetFutureSource(const IPCClientInfo& aClientInfo);
-
-  RefPtr<SourcePromise> FindSource(
+  ClientSourceParent* FindSource(
       const nsID& aID, const mozilla::ipc::PrincipalInfo& aPrincipalInfo);
+
+  // Called when a ClientHandle is created before the corresponding
+  // ClientSource. Will call FoundSource on the ClientHandleParent when it
+  // becomes available.
+  void WaitForSource(ClientHandleParent* aHandle, const nsID& aID);
+  void StopWaitingForSource(ClientHandleParent* aHandle, const nsID& aID);
 
   void AddManager(ClientManagerParent* aManager);
 
