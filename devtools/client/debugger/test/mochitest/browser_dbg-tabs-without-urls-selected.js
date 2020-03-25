275378810d9c4cb5770a5aaf65ab5938c0bc7e07
David Walsh <dwalsh@mozilla.com>
2020-03-25 17:03:47 +0000

    Bug 1623607 - Prevent multiple tabs from displaying for a single source r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D67888

---
+++ devtools/client/debugger/test/mochitest/browser_dbg-tabs-without-urls-selected.js
@@ -0,0 +1,23 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+// Test that URL-less sources have tabs and selecting that location does not
+// create a new tab for the same URL-less source
+add_task(async function() {
+  const dbg = await initDebugger("doc-scripts.html");
+
+  // Create a URL-less source 
+  const { hud } = await dbg.toolbox.selectTool("webconsole");
+  await evaluateExpressionInConsole(hud, `
+    (() => {
+        setTimeout(() => { debugger; }, 100);
+    })();
+  `);
+  await waitForPaused(dbg);
+
+  // Click a frame which shouldn't open a new source tab
+  await clickElement(dbg, "frame", 2);
+
+  // Click the frame to select the same location and ensure there's only 1 tab
+  is(countTabs(dbg), 1);
+});
