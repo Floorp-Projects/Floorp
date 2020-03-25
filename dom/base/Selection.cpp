5b963b488102a68f7c95a3dff55c8be54905a95a
Mirko Brodesser <mbrodesser@mozilla.com>
2020-03-25 15:36:54 +0000

    Bug 1623858: part 11) Split `Selection::AddRangesForSelectableNodes`. r=smaug
    Depends on D67848
    
    Differential Revision: https://phabricator.services.mozilla.com/D68008
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/Selection.cpp
+++ dom/base/Selection.cpp
@@ -806,6 +806,106 @@ static void UserSelectRangesToAdd(nsRange* aItem,
   }
 }
 
+nsresult Selection::AddRangesForUserSelectableNodes(
+    nsRange* aRange, int32_t* aOutIndex,
+    const DispatchSelectstartEvent aDispatchSelectstartEvent) {
+  MOZ_ASSERT(mUserInitiated);
+  MOZ_ASSERT(aOutIndex);
+
+  if (!aRange) {
+    return NS_ERROR_NULL_POINTER;
+  }
+
+  if (!aRange->IsPositioned()) {
+    return NS_ERROR_UNEXPECTED;
+  }
+
+  AutoTArray<RefPtr<nsRange>, 4> rangesToAdd;
+  *aOutIndex = int32_t(mRanges.Length()) - 1;
+
+  Document* doc = GetDocument();
+  bool selectEventsEnabled = StaticPrefs::dom_select_events_enabled() ||
+                             (doc && doc->NodePrincipal()->IsSystemPrincipal());
+
+  if (aDispatchSelectstartEvent == DispatchSelectstartEvent::Maybe &&
+      mSelectionType == SelectionType::eNormal && selectEventsEnabled &&
+      IsCollapsed() && !IsBlockingSelectionChangeEvents()) {
+    // First, we generate the ranges to add with a scratch range, which is a
+    // clone of the original range passed in. We do this seperately, because
+    // the selectstart event could have caused the world to change, and
+    // required ranges to be re-generated
+    RefPtr<nsRange> scratchRange = aRange->CloneRange();
+    UserSelectRangesToAdd(scratchRange, rangesToAdd);
+    bool newRangesNonEmpty =
+        rangesToAdd.Length() > 1 ||
+        (rangesToAdd.Length() == 1 && !rangesToAdd[0]->Collapsed());
+
+    MOZ_ASSERT(!newRangesNonEmpty || nsContentUtils::IsSafeToRunScript());
+    if (newRangesNonEmpty && nsContentUtils::IsSafeToRunScript()) {
+      // We consider a selection to be starting if we are currently collapsed,
+      // and the selection is becoming uncollapsed, and this is caused by a
+      // user initiated event.
+      bool defaultAction = true;
+
+      // The spec currently doesn't say that we should dispatch this event
+      // on text controls, so for now we only support doing that under a
+      // pref, disabled by default.
+      // See https://github.com/w3c/selection-api/issues/53.
+      bool dispatchEvent = true;
+      nsCOMPtr<nsINode> target = aRange->GetStartContainer();
+      if (nsFrameSelection::sSelectionEventsOnTextControlsEnabled) {
+        // Get the first element which isn't in a native anonymous subtree
+        while (target && target->IsInNativeAnonymousSubtree()) {
+          target = target->GetParent();
+        }
+      } else {
+        if (target->IsInNativeAnonymousSubtree()) {
+          // This is a selection under a text control, so don't dispatch the
+          // event.
+          dispatchEvent = false;
+        }
+      }
+
+      if (dispatchEvent) {
+        nsContentUtils::DispatchTrustedEvent(
+            GetDocument(), target, NS_LITERAL_STRING("selectstart"),
+            CanBubble::eYes, Cancelable::eYes, &defaultAction);
+
+        if (!defaultAction) {
+          return NS_OK;
+        }
+
+        // As we just dispatched an event to the DOM, something could have
+        // changed under our feet. Re-generate the rangesToAdd array, and
+        // ensure that the range we are about to add is still valid.
+        if (!aRange->IsPositioned()) {
+          return NS_ERROR_UNEXPECTED;
+        }
+      }
+    }
+
+    // The scratch ranges we generated may be invalid now, throw them out
+    rangesToAdd.ClearAndRetainStorage();
+  }
+
+  // Generate the ranges to add
+  UserSelectRangesToAdd(aRange, rangesToAdd);
+  size_t newAnchorFocusIndex =
+      GetDirection() == eDirPrevious ? 0 : rangesToAdd.Length() - 1;
+  for (size_t i = 0; i < rangesToAdd.Length(); ++i) {
+    int32_t index;
+    nsresult rv = MaybeAddRangeAndTruncateOverlaps(rangesToAdd[i], &index);
+    NS_ENSURE_SUCCESS(rv, rv);
+    if (i == newAnchorFocusIndex) {
+      *aOutIndex = index;
+      rangesToAdd[i]->SetIsGenerated(false);
+    } else {
+      rangesToAdd[i]->SetIsGenerated(true);
+    }
+  }
+  return NS_OK;
+}
+
 nsresult Selection::AddRangesForSelectableNodes(
     nsRange* aRange, int32_t* aOutIndex,
     const DispatchSelectstartEvent aDispatchSelectstartEvent) {
@@ -820,92 +920,10 @@ nsresult Selection::AddRangesForSelectableNodes(
   NS_ASSERTION(aOutIndex, "aOutIndex can't be null");
 
   if (mUserInitiated) {
-    AutoTArray<RefPtr<nsRange>, 4> rangesToAdd;
-    *aOutIndex = int32_t(mRanges.Length()) - 1;
-
-    Document* doc = GetDocument();
-    bool selectEventsEnabled =
-        StaticPrefs::dom_select_events_enabled() ||
-        (doc && doc->NodePrincipal()->IsSystemPrincipal());
-
-    if (aDispatchSelectstartEvent == DispatchSelectstartEvent::Maybe &&
-        mSelectionType == SelectionType::eNormal && selectEventsEnabled &&
-        IsCollapsed() && !IsBlockingSelectionChangeEvents()) {
-      // First, we generate the ranges to add with a scratch range, which is a
-      // clone of the original range passed in. We do this seperately, because
-      // the selectstart event could have caused the world to change, and
-      // required ranges to be re-generated
-      RefPtr<nsRange> scratchRange = aRange->CloneRange();
-      UserSelectRangesToAdd(scratchRange, rangesToAdd);
-      bool newRangesNonEmpty =
-          rangesToAdd.Length() > 1 ||
-          (rangesToAdd.Length() == 1 && !rangesToAdd[0]->Collapsed());
-
-      MOZ_ASSERT(!newRangesNonEmpty || nsContentUtils::IsSafeToRunScript());
-      if (newRangesNonEmpty && nsContentUtils::IsSafeToRunScript()) {
-        // We consider a selection to be starting if we are currently collapsed,
-        // and the selection is becoming uncollapsed, and this is caused by a
-        // user initiated event.
-        bool defaultAction = true;
-
-        // The spec currently doesn't say that we should dispatch this event
-        // on text controls, so for now we only support doing that under a
-        // pref, disabled by default.
-        // See https://github.com/w3c/selection-api/issues/53.
-        bool dispatchEvent = true;
-        nsCOMPtr<nsINode> target = aRange->GetStartContainer();
-        if (nsFrameSelection::sSelectionEventsOnTextControlsEnabled) {
-          // Get the first element which isn't in a native anonymous subtree
-          while (target && target->IsInNativeAnonymousSubtree()) {
-            target = target->GetParent();
-          }
-        } else {
-          if (target->IsInNativeAnonymousSubtree()) {
-            // This is a selection under a text control, so don't dispatch the
-            // event.
-            dispatchEvent = false;
-          }
-        }
-
-        if (dispatchEvent) {
-          nsContentUtils::DispatchTrustedEvent(
-              GetDocument(), target, NS_LITERAL_STRING("selectstart"),
-              CanBubble::eYes, Cancelable::eYes, &defaultAction);
-
-          if (!defaultAction) {
-            return NS_OK;
-          }
-
-          // As we just dispatched an event to the DOM, something could have
-          // changed under our feet. Re-generate the rangesToAdd array, and
-          // ensure that the range we are about to add is still valid.
-          if (!aRange->IsPositioned()) {
-            return NS_ERROR_UNEXPECTED;
-          }
-        }
-      }
-
-      // The scratch ranges we generated may be invalid now, throw them out
-      rangesToAdd.ClearAndRetainStorage();
-    }
-
-    // Generate the ranges to add
-    UserSelectRangesToAdd(aRange, rangesToAdd);
-    size_t newAnchorFocusIndex =
-        GetDirection() == eDirPrevious ? 0 : rangesToAdd.Length() - 1;
-    for (size_t i = 0; i < rangesToAdd.Length(); ++i) {
-      int32_t index;
-      nsresult rv = MaybeAddRangeAndTruncateOverlaps(rangesToAdd[i], &index);
-      NS_ENSURE_SUCCESS(rv, rv);
-      if (i == newAnchorFocusIndex) {
-        *aOutIndex = index;
-        rangesToAdd[i]->SetIsGenerated(false);
-      } else {
-        rangesToAdd[i]->SetIsGenerated(true);
-      }
-    }
-    return NS_OK;
+    return AddRangesForUserSelectableNodes(aRange, aOutIndex,
+                                           aDispatchSelectstartEvent);
   }
+
   return MaybeAddRangeAndTruncateOverlaps(aRange, aOutIndex);
 }
 
