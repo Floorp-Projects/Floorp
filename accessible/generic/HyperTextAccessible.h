d5bfd8943699f7605d3b7eb6b67e3b662822747d
Mirko Brodesser <mbrodesser@mozilla.com>
2020-03-25 15:39:37 +0000

    Bug 1623858: part 13) Declare some methods in `HyperTextAccessible` `const`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D68039
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/generic/HyperTextAccessible.h
+++ accessible/generic/HyperTextAccessible.h
@@ -143,7 +143,7 @@ class HyperTextAccessible : public AccessibleWrap {
    * @return true   if conversion was successful
    */
   bool OffsetsToDOMRange(int32_t aStartOffset, int32_t aEndOffset,
-                         nsRange* aRange);
+                         nsRange* aRange) const;
 
   /**
    * Convert the given offset into DOM point.
@@ -152,7 +152,7 @@ class HyperTextAccessible : public AccessibleWrap {
    * if before embedded object then (parent node, indexInParent), if after then
    * (parent node, indexInParent + 1).
    */
-  DOMPoint OffsetToDOMPoint(int32_t aOffset);
+  DOMPoint OffsetToDOMPoint(int32_t aOffset) const;
 
   /**
    * Return true if the used ARIA role (if any) allows the hypertext accessible
@@ -530,7 +530,7 @@ class HyperTextAccessible : public AccessibleWrap {
    *                        contents.
    */
   DOMPoint ClosestNotGeneratedDOMPoint(const DOMPoint& aDOMPoint,
-                                       nsIContent* aElementContent);
+                                       nsIContent* aElementContent) const;
 
   // Helpers
   nsresult GetDOMPointByFrameOffset(nsIFrame* aFrame, int32_t aOffset,
