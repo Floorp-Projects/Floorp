41eeb0aaac821d6b75c02c5f774bce6e9f13ff1d
Tim Huang <tihuang@mozilla.com>
2020-03-25 13:20:44 +0000

    Bug 1612378 - Part 4: Remove the ContentBlockingAllowListCache. r=dimi,baku
    Due to the fact that we compute the IsOnContentBlockingAllowList in the
    parent process and pass it as a flag to the content processes, we don't
    need to do the cache anymore. Because the check would be just to read a
    bool from the WindowContext. The cost should be low enough that we don't
    need a cache anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66211

--- toolkit/components/antitracking/ContentBlockingAllowList.h
+++ toolkit/components/antitracking/ContentBlockingAllowList.h
@@ -7,6 +7,7 @@
 #ifndef mozilla_contentblockingallowlist_h
 #define mozilla_contentblockingallowlist_h
 
+class nsICookieJarSettings;
 class nsIHttpChannel;
 class nsIPrincipal;
 class nsIURI;
@@ -39,13 +40,10 @@ class ContentBlockingAllowList final {
                                  nsIPrincipal** aPrincipal);
 
  private:
-  // Get the cache of allow list lookups for recent window/channels passed to
-  // Check().
-  static ContentBlockingAllowListCache& Cache();
-
   // Utility APIs for ContentBlocking.
   static bool Check(nsIPrincipal* aTopWinPrincipal, bool aIsPrivateBrowsing);
   static bool Check(nsPIDOMWindowInner* aWindow);
+  static bool Check(nsICookieJarSettings* aCookieJarSettings);
 
   friend class ContentBlocking;
 };
