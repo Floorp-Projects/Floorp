69ad64382b9dc570984d57eaf2a05c702a650494
Subhamoy Sengupta <ssengupta@mozilla.com>
2020-03-25 12:45:59 +0000

    Bug 1624588 - dom/file/ipc sources now also build when not unified r=baku
    Missing includes, namespace-related issues, and missing template instantiations added to make sources compile without unifying.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68142

--- dom/file/ipc/TemporaryIPCBlobParent.cpp
+++ dom/file/ipc/TemporaryIPCBlobParent.cpp
@@ -4,11 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "private/pprio.h"
 #include "TemporaryIPCBlobParent.h"
-
 #include "mozilla/dom/FileBlobImpl.h"
 #include "nsAnonymousTemporaryFile.h"
 #include "TemporaryFileBlobImpl.h"
+#include "mozilla/dom/IPCBlobUtils.h"
 
 namespace mozilla {
 namespace dom {
