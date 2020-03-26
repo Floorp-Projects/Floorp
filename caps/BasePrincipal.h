d3c62706af98f09935d4d5cae3c69f156c88c359
Sebastian Streich <sstreich@mozilla.com>
2020-03-26 13:18:40 +0000

    Bug 1622042 - Refactor NsContentUtils:Allowsl10n r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D66633

--- caps/BasePrincipal.h
+++ caps/BasePrincipal.h
@@ -122,6 +122,7 @@ class BasePrincipal : public nsJSPrincipals {
   NS_IMETHOD GetIsSystemPrincipal(bool* aResult) override;
   NS_IMETHOD SchemeIs(const char* aScheme, bool* aResult) override;
   NS_IMETHOD IsURIInPrefList(const char* aPref, bool* aResult) override;
+  NS_IMETHOD IsL10nAllowed(nsIURI* aURI, bool* aResult) override;
   NS_IMETHOD GetAboutModuleFlags(uint32_t* flags) override;
   NS_IMETHOD GetIsAddonOrExpandedAddonPrincipal(bool* aResult) override;
   NS_IMETHOD GetOriginAttributes(JSContext* aCx,
