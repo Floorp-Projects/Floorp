a3a5a8ee351e9a2233fdc357c49fc8a4d61bc916
James Teh <jteh@mozilla.com>
2020-03-26 20:54:02 +0000

    Bug 1616829: Don't remove ATK_STATE_EDITABLE from list items in editors. r=MarcoZ
    Differential Revision: https://phabricator.services.mozilla.com/D68327
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/atk/AccessibleWrap.cpp
+++ accessible/atk/AccessibleWrap.cpp
@@ -818,10 +818,14 @@ gint getIndexInParentCB(AtkObject* aAtkObj) {
   return parent->GetIndexOfEmbeddedChild(accWrap);
 }
 
-static void TranslateStates(uint64_t aState, AtkStateSet* aStateSet) {
+static void TranslateStates(uint64_t aState, roles::Role aRole,
+                            AtkStateSet* aStateSet) {
   // atk doesn't have a read only state so read only things shouldn't be
-  // editable.
-  if (aState & states::READONLY) aState &= ~states::EDITABLE;
+  // editable. However, we don't do this for list items because Gecko always
+  // exposes those as read only.
+  if ((aState & states::READONLY) && aRole != roles::LISTITEM) {
+    aState &= ~states::EDITABLE;
+  }
 
   // Convert every state to an entry in AtkStateMap
   uint64_t bitMask = 1;
@@ -845,12 +849,13 @@ AtkStateSet* refStateSetCB(AtkObject* aAtkObj) {
   state_set = ATK_OBJECT_CLASS(parent_class)->ref_state_set(aAtkObj);
 
   AccessibleWrap* accWrap = GetAccessibleWrap(aAtkObj);
-  if (accWrap)
-    TranslateStates(accWrap->State(), state_set);
-  else if (ProxyAccessible* proxy = GetProxy(aAtkObj))
-    TranslateStates(proxy->State(), state_set);
-  else
-    TranslateStates(states::DEFUNCT, state_set);
+  if (accWrap) {
+    TranslateStates(accWrap->State(), accWrap->Role(), state_set);
+  } else if (ProxyAccessible* proxy = GetProxy(aAtkObj)) {
+    TranslateStates(proxy->State(), proxy->Role(), state_set);
+  } else {
+    TranslateStates(states::DEFUNCT, roles::NOTHING, state_set);
+  }
 
   return state_set;
 }
