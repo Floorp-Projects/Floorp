d38ec8bca4c2b1355bcb6754805b31852a62c092
David Walsh <dwalsh@mozilla.com>
2020-03-26 15:16:50 +0000

    Bug 1625137 - Fix perma-failing browser_dbg-tabs-without-urls-selected test r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D68382

--- devtools/client/debugger/test/mochitest/browser_dbg-tabs-without-urls-selected.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-tabs-without-urls-selected.js
@@ -16,8 +16,10 @@ add_task(async function() {
   await waitForPaused(dbg);
 
   // Click a frame which shouldn't open a new source tab
-  await clickElement(dbg, "frame", 2);
+  await clickElement(dbg, "frame", 3);
 
   // Click the frame to select the same location and ensure there's only 1 tab
   is(countTabs(dbg), 1);
+
+  await resume(dbg);
 });
