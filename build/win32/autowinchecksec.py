d8753f59bbe6b45bb7d29e557afd4d220170602d
Mike Hommey <mh+mozilla@glandium.org>
2020-03-26 22:13:14 +0000

    Bug 1450088 - Use Winchecksec instead of Binscope. r=dmajor
    While almost rewriting autobinscope.py entirely (and renaming it),
    switch to python 3.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68156

---
+++ build/win32/autowinchecksec.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# run the Winchecksec tool (https://github.com/trailofbits/winchecksec)
+# against a given Windows binary.
+
+import buildconfig
+import json
+import subprocess
+import sys
+
+# usage
+if len(sys.argv) != 2:
+    print("""usage : autowinchecksec.by path_to_binary""")
+    sys.exit(0)
+
+binary_path = sys.argv[1]
+
+# execute winchecksec against the binary, using the WINCHECKSEC environment
+# variable as the path to winchecksec.exe
+try:
+    winchecksec_path = buildconfig.substs['WINCHECKSEC']
+except KeyError:
+    print("TEST-UNEXPECTED-FAIL | autowinchecksec.py | WINCHECKSEC environment variable is "
+          "not set, can't check DEP/ASLR etc. status.")
+    sys.exit(1)
+
+try:
+    result = subprocess.check_output([winchecksec_path, '-j', binary_path],
+                                     universal_newlines=True)
+
+except subprocess.CalledProcessError as e:
+    print("TEST-UNEXPECTED-FAIL | autowinchecksec.py | Winchecksec returned error code %d:\n%s" % (
+          e.returncode, e.output))
+    sys.exit(1)
+
+
+result = json.loads(result)
+
+checks = [
+    'aslr',
+    'cfg',
+    'dynamicBase',
+    'gs',
+    'isolation',
+    'nx',
+    'seh',
+]
+
+if buildconfig.substs['CPU_ARCH'] == 'x86':
+    checks += [
+        'safeSEH',
+    ]
+else:
+    checks += [
+        'highEntropyVA',
+    ]
+
+failed = [c for c in checks if result.get(c) is False]
+
+if failed:
+    print("TEST-UNEXPECTED-FAIL | autowinchecksec.py | Winchecksec reported %d error(s) for %s" %
+          (len(failed), binary_path))
+    print("TEST-UNEXPECTED-FAIL | autowinchecksec.py | The following check(s) failed: %s" %
+          (', '.join(failed)))
+    sys.exit(1)
+else:
+    print("TEST-PASS | autowinchecksec.py | %s succeeded" % binary_path)
