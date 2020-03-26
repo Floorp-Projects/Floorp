749c3380290e62d0eea675b25e179795289645fc
Emily McMinn <emcminn@mozilla.com>
2020-03-26 22:13:42 +0000

    Bug 1624509 - Dark mode support in simplified about:welcome r=pdahiya
    Differential Revision: https://phabricator.services.mozilla.com/D68137

--- toolkit/components/extensions/test/browser/browser_ext_themes_ntp_colors_perwindow.js
+++ toolkit/components/extensions/test/browser/browser_ext_themes_ntp_colors_perwindow.js
@@ -183,7 +183,7 @@ add_task(async function test_per_window_ntp_theme() {
       win.NewTabPagePreloading.removePreloadedBrowser(win);
       // These pages were initially chosen because LightweightThemeChild.jsm
       // treats them specially.
-      for (let url of ["about:newtab", "about:home"]) {
+      for (let url of ["about:newtab", "about:home", "about:welcome"]) {
         info("Opening url: " + url);
         await BrowserTestUtils.withNewTab(
           { gBrowser: win.gBrowser, url },
