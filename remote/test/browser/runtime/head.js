fbfa99d7a786841c899fe98d1fc44be29db20f3f
Henrik Skupin <mail@hskupin.info>
2020-03-27 14:12:55 +0000

    Bug 1624785 - [remote] Split-up long running test file browser_callFunctionOn.js. r=remote-protocol-reviewers,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D68269

--- remote/test/browser/runtime/head.js
+++ remote/test/browser/runtime/head.js
@@ -9,3 +9,19 @@ Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/remote/test/browser/head.js",
   this
 );
+
+async function enableRuntime(client) {
+  const { Runtime } = client;
+
+  // Enable watching for new execution context
+  await Runtime.enable();
+  info("Runtime domain has been enabled");
+
+  // Calling Runtime.enable will emit executionContextCreated for the existing contexts
+  const { context } = await Runtime.executionContextCreated();
+  ok(!!context.id, "The execution context has an id");
+  ok(context.auxData.isDefault, "The execution context is the default one");
+  ok(!!context.auxData.frameId, "The execution context has a frame id set");
+
+  return context.id;
+}
