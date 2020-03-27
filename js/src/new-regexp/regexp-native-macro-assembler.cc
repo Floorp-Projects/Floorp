ccefad19b8ab048bd66ffac72ab8b4aa364089b5
Iain Ireland <iireland@mozilla.com>
2020-03-27 07:16:43 +0000

    Bug 1594545: Part 2: Push, Pop, register allocation r=jandem
    This patch defines our register usage, and implements a few simple ops that don't rely on anything else.
    
    Depends on D68412
    
    Differential Revision: https://phabricator.services.mozilla.com/D68414

--- js/src/new-regexp/regexp-native-macro-assembler.cc
+++ js/src/new-regexp/regexp-native-macro-assembler.cc
@@ -16,6 +16,12 @@
 namespace v8 {
 namespace internal {
 
+using js::jit::Address;
+using js::jit::AllocatableGeneralRegisterSet;
+using js::jit::GeneralRegisterSet;
+using js::jit::Imm32;
+using js::jit::ImmWord;
+using js::jit::Register;
 using js::jit::StackMacroAssembler;
 
 SMRegExpMacroAssembler::SMRegExpMacroAssembler(JSContext* cx, Isolate* isolate,
@@ -30,10 +36,60 @@ SMRegExpMacroAssembler::SMRegExpMacroAssembler(JSContext* cx, Isolate* isolate,
       num_capture_registers_(num_capture_registers) {
   // Each capture has a start and an end register
   MOZ_ASSERT(num_capture_registers_ % 2 == 0);
+
+  AllocatableGeneralRegisterSet regs(GeneralRegisterSet::All());
+
+  temp0_ = regs.takeAny();
+  temp1_ = regs.takeAny();
+  temp2_ = regs.takeAny();
+  input_end_pointer_ = regs.takeAny();
+  current_character_ = regs.takeAny();
+  current_position_ = regs.takeAny();
+  backtrack_stack_pointer_ = regs.takeAny();
+  savedRegisters_ = js::jit::SavedNonVolatileRegisters(regs);
+
+  masm_.jump(&entry_label_);  // We'll generate the entry code later
+  masm_.bind(&start_label_);  // and continue from here.
 }
 
 int SMRegExpMacroAssembler::stack_limit_slack() {
   return RegExpStack::kStackLimitSlack;
+}
+
+void SMRegExpMacroAssembler::AdvanceCurrentPosition(int by) {
+  if (by != 0) {
+    masm_.addPtr(Imm32(by * char_size()), current_position_);
+  }
+}
+
+void SMRegExpMacroAssembler::Fail() {
+  masm_.movePtr(ImmWord(js::RegExpRunStatus_Success_NotFound), temp0_);
+  masm_.jump(&exit_label_);
+}
+
+void SMRegExpMacroAssembler::PopCurrentPosition() { Pop(current_position_); }
+
+void SMRegExpMacroAssembler::PushCurrentPosition() { Push(current_position_); }
+
+// Returns true if a regexp match can be restarted (aka the regexp is global).
+// The return value is not used anywhere, but we implement it to be safe.
+bool SMRegExpMacroAssembler::Succeed() {
+  masm_.jump(&success_label_);
+  return global();
+}
+
+void SMRegExpMacroAssembler::Push(Register source) {
+  MOZ_ASSERT(source != backtrack_stack_pointer_);
+
+  masm_.subPtr(Imm32(sizeof(void*)), backtrack_stack_pointer_);
+  masm_.storePtr(source, Address(backtrack_stack_pointer_, 0));
+}
+
+void SMRegExpMacroAssembler::Pop(Register target) {
+  MOZ_ASSERT(target != backtrack_stack_pointer_);
+
+  masm_.loadPtr(Address(backtrack_stack_pointer_, 0), target);
+  masm_.addPtr(Imm32(sizeof(void*)), backtrack_stack_pointer_);
 }
 
 // This is only used by tracing code.
