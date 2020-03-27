325f958e21697130d7b5734a16875b7b53817ba1
Eitan Isaacson <eitan@monotonous.org>
2020-03-27 00:53:31 +0000

    Bug 1624729 - Add a nativeInterface attribute to nsIAccessible. r=yzen
    Differential Revision: https://phabricator.services.mozilla.com/D68122
    
    --HG--
    extra : moz-landing-system : lando

--- accessible/xpcom/xpcAccessible.h
+++ accessible/xpcom/xpcAccessible.h
@@ -52,6 +52,9 @@ class xpcAccessible : public nsIAccessible {
   NS_IMETHOD GetKeyboardShortcut(nsAString& aKeyBinding) final;
 
   NS_IMETHOD GetAttributes(nsIPersistentProperties** aAttributes) final;
+
+  NS_IMETHOD GetNativeInterface(nsISupports** aNativeInterface) final;
+
   NS_IMETHOD GetBounds(int32_t* aX, int32_t* aY, int32_t* aWidth,
                        int32_t* aHeight) final;
   NS_IMETHOD GetBoundsInCSSPixels(int32_t* aX, int32_t* aY, int32_t* aWidth,
