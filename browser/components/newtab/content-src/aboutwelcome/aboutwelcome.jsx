8e85ed5e2e5bde34815c7c048b471f803ec1653c
Csoregi Natalia <ncsoregi@mozilla.com>
2020-03-27 02:26:03 +0200

    Backed out changeset 1e5b4839d371 (bug 1624509) for failures on browser_ext_themes_ntp_colors_perwindow.js. CLOSED TREE

--- browser/components/newtab/content-src/aboutwelcome/aboutwelcome.jsx
+++ browser/components/newtab/content-src/aboutwelcome/aboutwelcome.jsx
@@ -49,7 +49,7 @@ class AboutWelcome extends React.PureComponent {
   render() {
     const { props } = this;
     return (
-      <div className="outer-wrapper welcomeContainer">
+      <div className="welcomeContainer">
         <div className="welcomeContainerInner">
           <main>
             <HeroText title={props.title} subtitle={props.subtitle} />
