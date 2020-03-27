144dea28098847f487473f614dbbb305419b47b3
Iain Ireland <iireland@mozilla.com>
2020-03-27 18:00:28 +0000

    Bug 1594545: Part 1: Add SMRegExpMacroAssembler r=jandem
    Irregexp defines a RegExpMacroAssembler API (with methods like "CheckCharacter", "SetRegister", and "Backtrack"). This API is used as a backend for the RegexpCompiler. In V8, the API is implemented by the bytecode generator for the interpreter, and by a native code generator for each of their supported architecture.
    
    We are reusing V8's bytecode generator, but we need our own native codegen. This patch stack implements the RegExpMacroAssembler API using masm, folowing Brian Hackett's approach from the previous import of irregexp (and copying a lot of his code). Where possible, I based my implementation off Brian's implementation (in js/src/irregexp/NativeRegExpMacroAssembler.cpp). I also relied heavily on V8's x86 implementation (https://github.com/v8/v8/blob/master/src/regexp/ia32/regexp-macro-assembler-ia32.cc).
    
    This patch gets the files set up and implements a couple of trivial methods. Our old import kept the V8 license, despite being heavily rewritten. These files are dual-licensed with both the Mozilla license and the V8 license, which is our current preference.
    
    Future patches in this stack add extra functionality in roughly increasing order of complexity. I've made sure that each patch compiles, but in a few places there are comments referring to things that don't exist yet. If anything is unclear without context, let me know.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68412
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/new-regexp/regexp-macro-assembler-arch.h
+++ js/src/new-regexp/regexp-macro-assembler-arch.h
@@ -4,6 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Copyright 2020 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // This file implements the NativeRegExpMacroAssembler interface for
 // SpiderMonkey. It provides the same interface as each of V8's
 // architecture-specific implementations.
@@ -11,6 +15,33 @@
 #ifndef RegexpMacroAssemblerArch_h
 #define RegexpMacroAssemblerArch_h
 
-#include "new-regexp/regexp-shim.h"
+#include "jit/MacroAssembler.h"
+#include "new-regexp/regexp-macro-assembler.h"
 
-#endif // RegexpMacroAssemblerArch_h
+namespace v8 {
+namespace internal {
+
+class SMRegExpMacroAssembler final : public NativeRegExpMacroAssembler {
+ public:
+  SMRegExpMacroAssembler(JSContext* cx, Isolate* isolate,
+                         js::jit::StackMacroAssembler& masm, Zone* zone,
+                         Mode mode, uint32_t num_capture_registers);
+  virtual ~SMRegExpMacroAssembler() {} // Nothing to do here
+
+  virtual int stack_limit_slack();
+  virtual IrregexpImplementation Implementation();
+
+
+private:
+  JSContext* cx_;
+  js::jit::StackMacroAssembler& masm_;
+
+  Mode mode_;
+  int num_registers_;
+  int num_capture_registers_;
+};
+
+}  // namespace internal
+}  // namespace v8
+
+#endif  // RegexpMacroAssemblerArch_h
