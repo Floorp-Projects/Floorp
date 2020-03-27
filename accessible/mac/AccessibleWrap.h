f77993d800909eac1ee535c19308fc79bc7aaf6a
Eitan Isaacson <eitan@monotonous.org>
2020-03-27 17:15:31 +0000

    Bug 1624729 - Introduce nsIAccessibleMacInterface as mac native xpc interface. r=yzen,morgan
    Differential Revision: https://phabricator.services.mozilla.com/D68123

--- accessible/mac/AccessibleWrap.h
+++ accessible/mac/AccessibleWrap.h
@@ -53,6 +53,8 @@ class AccessibleWrap : public Accessible {
   virtual nsresult HandleAccEvent(AccEvent* aEvent) override;
 
  protected:
+  friend class xpcAccessibleMacInterface;
+
   /**
    * Return true if the parent doesn't have children to expose to AT.
    */
