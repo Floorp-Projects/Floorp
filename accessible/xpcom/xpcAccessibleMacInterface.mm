fc69fc7536cf34aa46b62d0d902b7fb7c8199821
Eitan Isaacson <eitan@monotonous.org>
2020-03-27 17:15:18 +0000

    Bug 1624729 - Implement attributes and actions getters in xpcAccessibleMacInterface. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D68124

--- accessible/xpcom/xpcAccessibleMacInterface.mm
+++ accessible/xpcom/xpcAccessibleMacInterface.mm
@@ -26,16 +26,133 @@ xpcAccessibleMacInterface::xpcAccessibleMacInterface(id aNativeObj) : mNativeObj
 xpcAccessibleMacInterface::~xpcAccessibleMacInterface() { [mNativeObject release]; }
 
 NS_IMETHODIMP
-xpcAccessibleMacInterface::GetAttributeNames(nsTArray<nsString>& aAttributeNames) { return NS_OK; }
+xpcAccessibleMacInterface::GetAttributeNames(nsTArray<nsString>& aAttributeNames) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT
+
+  if (!mNativeObject || [mNativeObject isExpired]) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  NSMutableSet* attributeNames =
+      [NSMutableSet setWithArray:[mNativeObject accessibilityAttributeNames]];
+  for (NSString* name in attributeNames) {
+    nsAutoString attribName;
+    nsCocoaUtils::GetStringForNSString(name, attribName);
+    aAttributeNames.AppendElement(attribName);
+  }
+
+  return NS_OK;
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT
+}
 
 NS_IMETHODIMP
-xpcAccessibleMacInterface::GetActionNames(nsTArray<nsString>& aActionNames) { return NS_OK; }
+xpcAccessibleMacInterface::GetActionNames(nsTArray<nsString>& aActionNames) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT
+
+  if (!mNativeObject || [mNativeObject isExpired]) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  NSMutableSet* actionNames = [NSMutableSet setWithArray:[mNativeObject accessibilityActionNames]];
+  for (NSString* name in actionNames) {
+    nsAutoString actionName;
+    nsCocoaUtils::GetStringForNSString(name, actionName);
+    aActionNames.AppendElement(actionName);
+  }
+
+  return NS_OK;
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT
+}
 
 NS_IMETHODIMP
-xpcAccessibleMacInterface::PerformAction(const nsAString& aActionName) { return NS_OK; }
+xpcAccessibleMacInterface::PerformAction(const nsAString& aActionName) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT
+
+  if (!mNativeObject || [mNativeObject isExpired]) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  [mNativeObject accessibilityPerformAction:actionName];
+
+  return NS_OK;
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT
+}
 
 NS_IMETHODIMP
 xpcAccessibleMacInterface::GetAttributeValue(const nsAString& aAttributeName, JSContext* aCx,
                                              JS::MutableHandleValue aResult) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT
+
+  if (!mNativeObject || [mNativeObject isExpired]) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
+
+  NSString* attribName = nsCocoaUtils::ToNSString(aAttributeName);
+  return NSObjectToJsValue([mNativeObject accessibilityAttributeValue:attribName], aCx, aResult);
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT
+}
+
+nsresult xpcAccessibleMacInterface::NSObjectToJsValue(id aObj, JSContext* aCx,
+                                                      JS::MutableHandleValue aResult) {
+  if (!aObj) {
+    aResult.set(JS::NullValue());
+  } else if ([aObj isKindOfClass:[NSString class]]) {
+    nsAutoString strVal;
+    nsCocoaUtils::GetStringForNSString((NSString*)aObj, strVal);
+    if (!mozilla::dom::ToJSValue(aCx, strVal, aResult)) {
+      return NS_ERROR_FAILURE;
+    }
+  } else if ([aObj isKindOfClass:[NSNumber class]]) {
+    // If the type being held by the NSNumber is a BOOL set js value
+    // to boolean. Otherwise use a double value.
+    if (strcmp([(NSNumber*)aObj objCType], @encode(BOOL)) == 0) {
+      if (!mozilla::dom::ToJSValue(aCx, [(NSNumber*)aObj boolValue], aResult)) {
+        return NS_ERROR_FAILURE;
+      }
+    } else {
+      if (!mozilla::dom::ToJSValue(aCx, [(NSNumber*)aObj doubleValue], aResult)) {
+        return NS_ERROR_FAILURE;
+      }
+    }
+  } else if ([aObj isKindOfClass:[NSValue class]] &&
+             strcmp([(NSValue*)aObj objCType], @encode(NSPoint)) == 0) {
+    NSPoint point = [(NSValue*)aObj pointValue];
+    return NSObjectToJsValue(
+        @[ [NSNumber numberWithDouble:point.x], [NSNumber numberWithDouble:point.y] ], aCx,
+        aResult);
+  } else if ([aObj isKindOfClass:[NSValue class]] &&
+             strcmp([(NSValue*)aObj objCType], @encode(NSSize)) == 0) {
+    NSSize size = [(NSValue*)aObj sizeValue];
+    return NSObjectToJsValue(
+        @[ [NSNumber numberWithDouble:size.width], [NSNumber numberWithDouble:size.height] ], aCx,
+        aResult);
+  } else if ([aObj respondsToSelector:@selector(accessibilityAttributeNames)]) {
+    nsCOMPtr<nsIAccessibleMacInterface> obj = new xpcAccessibleMacInterface(aObj);
+    return nsContentUtils::WrapNative(aCx, obj, &NS_GET_IID(nsIAccessibleMacInterface), aResult);
+  } else if ([aObj isKindOfClass:[NSArray class]]) {
+    NSArray* objArr = (NSArray*)aObj;
+
+    JS::RootedVector<JS::Value> v(aCx);
+    if (!v.resize([objArr count])) {
+      return NS_ERROR_FAILURE;
+    }
+    for (size_t i = 0; i < [objArr count]; ++i) {
+      nsresult rv = NSObjectToJsValue(objArr[i], aCx, v[i]);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    JSObject* arrayObj = JS::NewArrayObject(aCx, v);
+    if (!arrayObj) {
+      return NS_ERROR_FAILURE;
+    }
+    aResult.setObject(*arrayObj);
+  } else {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+
   return NS_OK;
 }
