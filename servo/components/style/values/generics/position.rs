b18d5514e73d416f33e9e309c64a5c382991c5c2
Ciure Andrei <aciure@mozilla.com>
2020-03-28 00:14:32 +0200

    Backed out changeset 9bbf5c41c5ce (bug 1618997) for causing for causing mochitest and wpt failures CLOSED TREE

--- servo/components/style/values/generics/position.rs
+++ servo/components/style/values/generics/position.rs
@@ -31,17 +31,6 @@ pub struct GenericPosition<H, V> {
     pub vertical: V,
 }
 
-impl<H, V> PositionComponent for Position<H, V>
-where
-    H: PositionComponent,
-    V: PositionComponent,
-{
-    #[inline]
-    fn is_center(&self) -> bool {
-        self.horizontal.is_center() && self.vertical.is_center()
-    }
-}
-
 pub use self::GenericPosition as Position;
 
 impl<H, V> Position<H, V> {
@@ -52,13 +41,6 @@ impl<H, V> Position<H, V> {
             vertical,
         }
     }
-}
-
-/// Implements a method that checks if the position is centered.
-pub trait PositionComponent {
-    /// Returns if the position component is 50% or center.
-    /// For pixel lengths, it always returns false.
-    fn is_center(&self) -> bool;
 }
 
 /// A generic type for representing an `Auto | <position>`.
