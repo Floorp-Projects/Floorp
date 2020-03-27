6b940f42a0e60a400def7908cc77ee659f88b818
Ciure Andrei <aciure@mozilla.com>
2020-03-27 21:50:25 +0200

    Backed out changeset 507a811671f8 (bug 1618997) for causing bustages CLOSED TREE

--- servo/components/style/values/generics/image.rs
+++ servo/components/style/values/generics/image.rs
@@ -13,7 +13,6 @@ use crate::Zero;
 use servo_arc::Arc;
 use std::fmt::{self, Write};
 use style_traits::{CssWriter, ToCss};
-use values::generics::position::PositionComponent;
 
 /// An `<image> | none` value.
 ///
@@ -331,7 +330,7 @@ where
     LP: ToCss,
     NL: ToCss,
     NLP: ToCss,
-    P: PositionComponent + ToCss,
+    P: ToCss,
     A: ToCss,
     AoP: ToCss,
     C: ToCss,
@@ -380,59 +379,36 @@ where
                     EndingShape::Ellipse(Ellipse::Extent(ShapeExtent::FarthestCorner)) => true,
                     _ => false,
                 };
-                let omit_position = position.is_center();
                 if compat_mode == GradientCompatMode::Modern {
                     if !omit_shape {
                         shape.to_css(dest)?;
-                        if !omit_position {
-                            dest.write_str(" ")?;
-                        }
+                        dest.write_str(" ")?;
                     }
-                    if !omit_position {
-                        dest.write_str("at ")?;
-                        position.to_css(dest)?;
-                    }
+                    dest.write_str("at ")?;
+                    position.to_css(dest)?;
                 } else {
-                    if !omit_position {
-                        position.to_css(dest)?;
-                        if !omit_shape {
-                            dest.write_str(", ")?;
-                        }
-                    }
+                    position.to_css(dest)?;
                     if !omit_shape {
+                        dest.write_str(", ")?;
                         shape.to_css(dest)?;
                     }
                 }
-                let mut skip_comma = omit_shape && omit_position;
                 for item in &**items {
-                    if !skip_comma {
-                        dest.write_str(", ")?;
-                    }
-                    skip_comma = false;
+                    dest.write_str(", ")?;
                     item.to_css(dest)?;
                 }
             },
             Gradient::Conic { ref angle, ref position, ref items, .. } => {
                 dest.write_str("conic-gradient(")?;
-                let omit_angle = angle.is_zero();
-                let omit_position = position.is_center();
-                if !omit_angle {
+                if !angle.is_zero() {
                     dest.write_str("from ")?;
                     angle.to_css(dest)?;
-                    if !omit_position {
-                        dest.write_str(" ")?;
-                    }
+                    dest.write_str(" ")?;
                 }
-                if !omit_position {
-                    dest.write_str("at ")?;
-                    position.to_css(dest)?;
-                }
-                let mut skip_comma = omit_angle && omit_position;
+                dest.write_str("at ")?;
+                position.to_css(dest)?;
                 for item in &**items {
-                    if !skip_comma {
-                        dest.write_str(", ")?;
-                    }
-                    skip_comma = false;
+                    dest.write_str(", ")?;
                     item.to_css(dest)?;
                 }
             },
