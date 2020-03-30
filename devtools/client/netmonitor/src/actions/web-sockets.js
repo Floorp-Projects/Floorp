70a5e5649d8e59f6798e1147a3c00f64d200a024
Kishlaya Jaiswal <kishlaya.j@gmail.com>
2020-03-30 10:18:11 +0000

    Bug 1566780 - Added another filter frame type for viewing control frames separately r=Honza
    Correcting linter errors
    
    Added new logic for toggling control frames by adding a new control frame check variable and toggle function
    
    Preserve controlFrames option when clearing frames. Changed Control Frames to Control in locale
    
    Correct linter errors
    
    Added a separator between received and control. Changed the dropdown title to reflect control frame state
    
    Changed controlFramesEnabled to showControlFrames
    
    Added a comment about showControlFrames and toggleControlFrames
    
    Differential Revision: https://phabricator.services.mozilla.com/D67849

--- devtools/client/netmonitor/src/actions/web-sockets.js
+++ devtools/client/netmonitor/src/actions/web-sockets.js
@@ -10,6 +10,7 @@ const {
   WS_OPEN_FRAME_DETAILS,
   WS_CLEAR_FRAMES,
   WS_TOGGLE_FRAME_FILTER_TYPE,
+  WS_TOGGLE_CONTROL_FRAMES,
   WS_SET_REQUEST_FILTER_TEXT,
   WS_TOGGLE_COLUMN,
   WS_RESET_COLUMNS,
@@ -74,6 +75,16 @@ function toggleFrameFilterType(filter) {
   return {
     type: WS_TOGGLE_FRAME_FILTER_TYPE,
     filter,
+  };
+}
+
+/**
+ * Show control frames from the FrameListContent
+ * component belonging to the current channelId
+ */
+function toggleControlFrames() {
+  return {
+    type: WS_TOGGLE_CONTROL_FRAMES,
   };
 }
 
@@ -165,6 +176,7 @@ module.exports = {
   openFrameDetails,
   clearFrames,
   toggleFrameFilterType,
+  toggleControlFrames,
   setFrameFilterText,
   resetWebSocketsColumns,
   closeConnection,
