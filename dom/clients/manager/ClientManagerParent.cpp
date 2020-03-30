659c5f87b8d6ebb1c19788a05b5f661c36fdf593
Ryan VanderMeulen <ryanvm@gmail.com>
2020-03-30 14:53:46 -0400

    Backed out 6 changesets (bug 1584007) for causing bug 1625227.
    Backed out changeset c626a363823e (bug 1584007)
    Backed out changeset 0d130a64b0f5 (bug 1584007)
    Backed out changeset 8ca43f80bdd2 (bug 1584007)
    Backed out changeset 00eda7b39a13 (bug 1584007)
    Backed out changeset ac2765b4b4ec (bug 1584007)
    Backed out changeset 24deb3d46783 (bug 1584007)

--- dom/clients/manager/ClientManagerParent.cpp
+++ dom/clients/manager/ClientManagerParent.cpp
@@ -10,7 +10,6 @@
 #include "ClientManagerOpParent.h"
 #include "ClientManagerService.h"
 #include "ClientSourceParent.h"
-#include "ClientValidation.h"
 #include "mozilla/dom/PClientNavigateOpParent.h"
 #include "mozilla/Unused.h"
 
@@ -106,29 +105,6 @@ ClientManagerParent::ClientManagerParent()
 ClientManagerParent::~ClientManagerParent() { mService->RemoveManager(this); }
 
 void ClientManagerParent::Init() { mService->AddManager(this); }
-
-IPCResult ClientManagerParent::RecvExpectFutureClientSource(
-    const IPCClientInfo& aClientInfo) {
-  if (NS_WARN_IF(!ClientIsValidPrincipalInfo(aClientInfo.principalInfo()))) {
-    return IPC_FAIL(this, "Invalid PrincipalInfo.");
-  }
-
-  RefPtr<ClientManagerService> cms =
-      ClientManagerService::GetOrCreateInstance();
-  Unused << NS_WARN_IF(cms->ExpectFutureSource(aClientInfo));
-  return IPC_OK();
-}
-
-IPCResult ClientManagerParent::RecvForgetFutureClientSource(
-    const IPCClientInfo& aClientInfo) {
-  if (NS_WARN_IF(!ClientIsValidPrincipalInfo(aClientInfo.principalInfo()))) {
-    return IPC_FAIL(this, "Invalid PrincipalInfo.");
-  }
-
-  RefPtr<ClientManagerService> cms = ClientManagerService::GetInstance();
-  cms->ForgetFutureSource(aClientInfo);
-  return IPC_OK();
-}
 
 }  // namespace dom
 }  // namespace mozilla
