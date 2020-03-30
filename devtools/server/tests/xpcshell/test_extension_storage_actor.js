12b4c2f8cbbf6c20b3f4043d92edb2790a2f7f4d
Michael Ratcliffe <mratcliffe@mozilla.com>
2020-03-30 15:42:39 +0000

    Bug 1604528 - IndexedDB inspector no longer shows main process data r=jdescottes,johannh
    This reverts https://hg.mozilla.org/mozilla-central/rev/c7a6f863a0cc6d74357dc53933b89a0e168c1958 so the patches are exact opposites... not much else to say ;)
    
    Differential Revision: https://phabricator.services.mozilla.com/D59510

--- devtools/server/tests/xpcshell/test_extension_storage_actor.js
+++ devtools/server/tests/xpcshell/test_extension_storage_actor.js
@@ -5,6 +5,10 @@
 
 "use strict";
 
+const { FileUtils } = ChromeUtils.import(
+  "resource://gre/modules/FileUtils.jsm"
+);
+
 const { ExtensionTestUtils } = ChromeUtils.import(
   "resource://testing-common/ExtensionXPCShellUtils.jsm"
 );
@@ -200,8 +204,30 @@ async function shutdown(extension, target) {
   await extension.unload();
 }
 
+/**
+ * Mocks the missing 'storage/permanent' directory needed by the "indexedDB"
+ * storage actor's 'preListStores' method (called when 'listStores' is called). This
+ * directory exists in a full browser i.e. mochitest.
+ */
+function createMissingIndexedDBDirs() {
+  const dir = Services.dirsvc.get("ProfD", Ci.nsIFile).clone();
+  dir.append("storage");
+  if (!dir.exists()) {
+    dir.create(dir.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  }
+  dir.append("permanent");
+  if (!dir.exists()) {
+    dir.create(dir.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+  }
+  Assert.ok(
+    dir.exists(),
+    "Should have a 'storage/permanent' dir in the profile dir"
+  );
+}
+
 add_task(async function setup() {
   await promiseStartupManager();
+  createMissingIndexedDBDirs();
 });
 
 add_task(async function test_extension_store_exists() {
