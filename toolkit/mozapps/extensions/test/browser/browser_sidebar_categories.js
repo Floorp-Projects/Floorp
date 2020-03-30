8b5461f86a1427981fb587db6a483ab9dc2a9b53
Mark Striemer <mstriemer@mozilla.com>
2020-03-30 19:45:09 +0000

    Bug 1525178 - Part 6: Change about:addons category on keyboard nav r=rpl
    Depends on D60426
    
    Differential Revision: https://phabricator.services.mozilla.com/D65834
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/mozapps/extensions/test/browser/browser_sidebar_categories.js
+++ toolkit/mozapps/extensions/test/browser/browser_sidebar_categories.js
@@ -5,22 +5,26 @@
 
 const THEME_ID = "default-theme@mozilla.org";
 
+function assertViewHas(win, selector, msg) {
+  ok(win.document.querySelector(selector), msg);
+}
+function assertListView(win, type) {
+  assertViewHas(win, `addon-list[type="${type}"]`, `On ${type} list`);
+}
+
 add_task(async function testClickingSidebarEntriesChangesView() {
   let win = await loadInitialView("extension");
   let doc = win.document;
   let themeCategory = doc.querySelector("#categories > [name=theme]");
+  let extensionCategory = doc.querySelector("#categories > [name=extension]");
 
-  let assertViewHas = (selector, msg) => ok(doc.querySelector(selector), msg);
-  let assertListView = type =>
-    assertViewHas(`addon-list[type="${type}"]`, `On ${type} list`);
-
-  assertListView("extension");
+  assertListView(win, "extension");
 
   let loaded = waitForViewLoad(win);
   themeCategory.click();
   await loaded;
 
-  assertListView("theme");
+  assertListView(win, "theme");
 
   loaded = waitForViewLoad(win);
   getAddonCard(win, THEME_ID).click();
@@ -28,6 +32,7 @@ add_task(async function testClickingSidebarEntriesChangesView() {
 
   ok(!doc.querySelector("addon-list"), "No more addon-list");
   assertViewHas(
+    win,
     `addon-card[addon-id="${THEME_ID}"][expanded]`,
     "Detail view now"
   );
@@ -36,21 +41,13 @@ add_task(async function testClickingSidebarEntriesChangesView() {
   EventUtils.synthesizeMouseAtCenter(themeCategory, {}, win);
   await loaded;
 
-  assertListView("theme");
+  assertListView(win, "theme");
 
   loaded = waitForViewLoad(win);
-  doc.getElementById("preferencesButton").focus();
-  EventUtils.synthesizeKey("VK_TAB", { shiftKey: true }, win);
-  is(doc.activeElement, themeCategory, "The theme category is focused");
-  EventUtils.synthesizeKey("VK_UP", {}, win);
-  ok(
-    !win.managerWindow.gViewController.isLoading,
-    "The new view isn't loading yet"
-  );
-  EventUtils.synthesizeKey(" ", {}, win);
+  EventUtils.synthesizeMouseAtCenter(extensionCategory, {}, win);
   await loaded;
 
-  assertListView("extension");
+  assertListView(win, "extension");
 
   await closeView(win);
 });
@@ -88,6 +85,79 @@ add_task(async function testClickingSidebarPaddingNoChange() {
   await loadDetailView();
   EventUtils.synthesizeMouse(themeCategory, -5, -5, {}, win);
   ok(!managerWindow.gViewController.isLoading, "No view is loading");
+
+  await closeView(win);
+});
+
+add_task(async function testKeyboardUsage() {
+  let win = await loadInitialView("extension");
+  let categories = win.document.getElementById("categories");
+  let extensionCategory = categories.getButtonByName("extension");
+  let themeCategory = categories.getButtonByName("theme");
+  let pluginCategory = categories.getButtonByName("plugin");
+
+  let waitForAnimationFrame = () =>
+    new Promise(resolve => win.requestAnimationFrame(resolve));
+  let sendKey = (key, e = {}) => {
+    EventUtils.synthesizeKey(key, e, win);
+    return waitForAnimationFrame();
+  };
+  let sendTabKey = e => sendKey("VK_TAB", e);
+  let isFocusInCategories = () =>
+    categories.contains(win.document.activeElement);
+
+  ok(!isFocusInCategories(), "Focus is not in the category list");
+
+  // Tab into the HTML browser.
+  await sendTabKey();
+  // Tab to the first focusable element.
+  await sendTabKey();
+
+  ok(isFocusInCategories(), "Focus is in the category list");
+  is(
+    win.document.activeElement,
+    extensionCategory,
+    "The extension button is focused"
+  );
+
+  // Tab out of the categories list.
+  await sendTabKey();
+  ok(!isFocusInCategories(), "Focus is out of the category list");
+
+  // Tab back into the list.
+  await sendTabKey({ shiftKey: true });
+  is(win.document.activeElement, extensionCategory, "Back on Extensions");
+
+  // We're on the extension list.
+  assertListView(win, "extension");
+
+  // Switch to theme list.
+  let loaded = waitForViewLoad(win);
+  await sendKey("VK_DOWN");
+  is(win.document.activeElement, themeCategory, "Themes is focused");
+  await loaded;
+
+  assertListView(win, "theme");
+
+  loaded = waitForViewLoad(win);
+  await sendKey("VK_DOWN");
+  is(win.document.activeElement, pluginCategory, "Plugins is focused");
+  await loaded;
+
+  assertListView(win, "plugin");
+
+  await sendKey("VK_DOWN");
+  is(win.document.activeElement, pluginCategory, "Plugins is still focused");
+  ok(!win.managerWindow.gViewController.isLoading, "No view is loading");
+
+  loaded = waitForViewLoad(win);
+  await sendKey("VK_UP");
+  await loaded;
+  loaded = waitForViewLoad(win);
+  await sendKey("VK_UP");
+  await loaded;
+  is(win.document.activeElement, extensionCategory, "Extensions is focused");
+  assertListView(win, "extension");
 
   await closeView(win);
 });
