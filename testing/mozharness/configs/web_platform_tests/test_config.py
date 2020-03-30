4d6e03604ee360e126afd296df620532bbc5cb0c
James Graham <james@hoppipolla.co.uk>
2020-03-30 23:03:12 +0000

    Bug 1626089 - Run two wpt processes in CI, r=jmaher
    This runs wpt with --processes=2 in CI, excluding wdspec tests where this
    causes obvious test failures and android where we can't have more than a
    single instance. This should provide some speedup at the cost of possibly
    increased instability. So following landing this it will be necessary to
    look out for an increased rate of intermittents.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68885
    
    --HG--
    extra : moz-landing-system : lando

--- testing/mozharness/configs/web_platform_tests/test_config.py
+++ testing/mozharness/configs/web_platform_tests/test_config.py
@@ -7,7 +7,6 @@
 config = {
     "options": [
         "--prefs-root=%(test_path)s/prefs",
-        "--processes=1",
         "--config=%(test_path)s/wptrunner.ini",
         "--ca-cert-path=%(test_path)s/tests/tools/certs/cacert.pem",
         "--host-key-path=%(test_path)s/tests/tools/certs/web-platform.test.key",
