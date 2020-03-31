6dfbcf60460cdb70733a36bcad43ce0d9a36ad97
Edwin Takahashi <egao@mozilla.com>
2020-03-31 20:41:17 +0000

    Bug 1608837 - fix incorrect names of reftest/crashtest variants of web-platform-tests r=ahal
    The definition for reftest/crashtest variants of web-platform-tests in web-platform.yml has a trailing `s` which is technically incorrect.
    
    This causes problems when querying ActiveData for the runtimes for these subsuites since the suite name recorded in ActiveData has the trailing s.
    
    Changes:
    
      - remove the trailing `s` from the definitions
    
    Differential Revision: https://phabricator.services.mozilla.com/D68069

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -1290,8 +1290,8 @@ CHUNK_SUITES_BLACKLIST = (
     'test-verify-gpu',
     'test-verify-wpt',
     'web-platform-tests',
-    'web-platform-tests-crashtests',
-    'web-platform-tests-reftests',
+    'web-platform-tests-crashtest',
+    'web-platform-tests-reftest',
     'web-platform-tests-wdspec',
 )
 """These suites will be chunked at test runtime rather than here in the taskgraph."""
