5e8dc639fe6063ea388fe0d934cd1381e2bf9bb5
Jeff Walden <jwalden@mit.edu>
2020-03-31 01:31:11 +0000

    Bug 1626105 - Convert |JS::EvaluateUtf8Path| to |JS::EvaluateUtf8PathDontInflate| semantics, and remove |JS::EvaluateUtf8PathDontInflate|.  r=evilpie
    Differential Revision: https://phabricator.services.mozilla.com/D68910
    
    --HG--
    extra : moz-landing-system : lando

--- js/public/CompilationAndEvaluation.h
+++ js/public/CompilationAndEvaluation.h
@@ -166,25 +166,6 @@ extern JS_PUBLIC_API bool EvaluateUtf8Path(
     MutableHandle<Value> rval);
 
 /**
- * Evaluate the UTF-8 contents of the file at the given path, and return the
- * completion value in |rval|.  (The path itself is in the system encoding, not
- * [necessarily] UTF-8.)  If the contents contain any malformed UTF-8, an error
- * is reported.
- *
- * The "DontInflate" suffix and (semantically unobservable) don't-inflate
- * characteristic are temporary while bugs in UTF-8 compilation are ironed out.
- * In the long term |JS::EvaluateUtf8Path| will just never inflate, and this
- * separate function will die.
- *
- * NOTE: UTF-8 compilation is currently experimental, and it's possible it has
- *       as-yet-undiscovered bugs that the UTF-16 compilation functions do not
- *       have.  Use only if you're willing to take a risk!
- */
-extern JS_PUBLIC_API bool EvaluateUtf8PathDontInflate(
-    JSContext* cx, const ReadOnlyCompileOptions& options, const char* filename,
-    MutableHandle<Value> rval);
-
-/**
  * Compile the provided script using the given options.  Return the script on
  * success, or return null on failure (usually with an error reported).
  */
