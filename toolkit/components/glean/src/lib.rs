f428e066bdaefdf514291202b9a23caed8e1e5e2
Jan-Erik Rediger <jrediger@mozilla.com>
2020-03-31 08:24:51 +0000

    Bug 1623300 - Add configuration for Glean. r=chutten
    We can re-use the glean_core configuration now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68541
    
    --HG--
    extra : moz-landing-system : lando

--- toolkit/components/glean/src/lib.rs
+++ toolkit/components/glean/src/lib.rs
@@ -2,9 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::ffi::CStr;
 use std::os::raw::c_char;
 
 use nserror::{nsresult, NS_OK};
+
+use glean_core::Configuration;
 
 #[no_mangle]
 pub unsafe extern "C" fn fog_init(
@@ -13,6 +16,18 @@ pub unsafe extern "C" fn fog_init(
     channel: *const c_char,
 ) -> nsresult {
     log::debug!("Initializing FOG.");
+
+    let upload_enabled = static_prefs::pref!("datareporting.healthreport.uploadEnabled");
+    let data_path = "/tmp".to_string(); // need to pass in something
+    let configuration = Configuration {
+        upload_enabled,
+        data_path,
+        application_id: "org-mozilla-firefox".to_string(),
+        max_events: None,
+        delay_ping_lifetime_io: false,
+    };
+
+    log::debug!("Configuration: {:#?}", configuration);
 
     NS_OK
 }
