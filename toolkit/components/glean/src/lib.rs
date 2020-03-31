150d8bda67095c9fc29ca675d7408582c9e30ee5
Jan-Erik Rediger <jrediger@mozilla.com>
2020-03-31 08:24:49 +0000

    Bug 1623300 - Init Glean from old Telemetry. r=chutten
    This passes on dummy values for now.
    Still no Glean is initialized, and thus no data placed on disk (and no
    temporary or additional profile directories created).
    It's done that way to ensure it's actually compiled and called (and make
    local development easier).
    
    Differential Revision: https://phabricator.services.mozilla.com/D68540

--- toolkit/components/glean/src/lib.rs
+++ toolkit/components/glean/src/lib.rs
@@ -2,10 +2,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::os::raw::c_char;
+
 use nserror::{nsresult, NS_OK};
-use nsstring::nsAString;
 
 #[no_mangle]
-pub unsafe extern "C" fn fog_init(_data_dir: &nsAString, _pingsender_path: &nsAString) -> nsresult {
+pub unsafe extern "C" fn fog_init(
+    app_build: *const c_char,
+    app_display_version: *const c_char,
+    channel: *const c_char,
+) -> nsresult {
+    log::debug!("Initializing FOG.");
+
     NS_OK
 }
