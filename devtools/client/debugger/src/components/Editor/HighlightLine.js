d0f8ac1c5c7711578ed92d385e771795e51ac020
David Walsh <dwalsh@mozilla.com>
2020-03-31 15:44:05 +0000

    Bug 1625814 - Update CodeMirror line class for new API r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D68741

--- devtools/client/debugger/src/components/Editor/HighlightLine.js
+++ devtools/client/debugger/src/components/Editor/HighlightLine.js
@@ -134,7 +134,7 @@ export class HighlightLine extends Component<Props> {
     }
 
     const doc = getDocument(sourceId);
-    doc.addLineClass(editorLine, "line", "highlight-line");
+    doc.addLineClass(editorLine, "wrapClass", "highlight-line");
     this.resetHighlightLine(doc, editorLine);
   }
 
@@ -155,7 +155,8 @@ export class HighlightLine extends Component<Props> {
     );
 
     setTimeout(
-      () => doc && doc.removeLineClass(editorLine, "line", "highlight-line"),
+      () =>
+        doc && doc.removeLineClass(editorLine, "wrapClass", "highlight-line"),
       duration
     );
   }
@@ -171,7 +172,7 @@ export class HighlightLine extends Component<Props> {
     const { line, sourceId } = selectedLocation;
     const editorLine = toEditorLine(sourceId, line);
     const doc = getDocument(sourceId);
-    doc.removeLineClass(editorLine, "line", "highlight-line");
+    doc.removeLineClass(editorLine, "wrapClass", "highlight-line");
   }
 
   render() {
