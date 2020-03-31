f2ba85fcf2c47607d46530c099aab083a4f864ee
Stefan Hindli <shindli@mozilla.com>
2020-03-31 15:22:42 +0300

    Backed out changeset ea3d100c8428 (bug 1624488) as per emilio's request on Riot for being related to Bug 1626198 CLOSED TREE

--- toolkit/content/widgets/general.js
+++ toolkit/content/widgets/general.js
@@ -47,13 +47,9 @@
     constructor() {
       super();
       let shadowRoot = this.attachShadow({ mode: "open" });
-      let stylesheet = document.createElement("link");
-      stylesheet.rel = "stylesheet";
-      stylesheet.href = "chrome://global/skin/dropmarker.css";
-
       let image = document.createXULElement("image");
-      image.part = "icon";
-      shadowRoot.append(stylesheet, image);
+      image.setAttribute("part", "icon");
+      shadowRoot.appendChild(image);
     }
   }
 
