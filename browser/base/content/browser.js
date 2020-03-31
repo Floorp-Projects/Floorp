4f25feb99fa1ed11426dd5e8ad9cf3f7d27a59cb
Zibi Braniecki <zbraniecki@mozilla.com>
2020-03-31 23:52:03 +0000

    Bug 1602808 - Revert Window Title to use DTD. r=fluent-reviewers,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D68661

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -8369,6 +8369,13 @@ var gPrivateBrowsingUI = {
 
     // Adjust the window's title
     let docElement = document.documentElement;
+    if (!PrivateBrowsingUtils.permanentPrivateBrowsing) {
+      docElement.title = docElement.getAttribute("title_privatebrowsing");
+      docElement.setAttribute(
+        "titlemodifier",
+        docElement.getAttribute("titlemodifier_privatebrowsing")
+      );
+    }
     docElement.setAttribute(
       "privatebrowsingmode",
       PrivateBrowsingUtils.permanentPrivateBrowsing ? "permanent" : "temporary"
