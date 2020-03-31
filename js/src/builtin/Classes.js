d60143d537a7f83989d31ba843419f9a5561e650
Ted Campbell <tcampbell@mozilla.com>
2020-03-31 18:48:49 +0000

    Bug 1597795 - Remove MakeDefaultConstructor intrinsic. r=mgaudet
    Now that we directly construct the JSFunction of a default constructor, we
    set the name and flags correctly. As a result the MakeDefaultConstructor
    intrinsic doesn't do anything of consequence and we can remove it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69020
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/builtin/Classes.js
+++ js/src/builtin/Classes.js
@@ -6,14 +6,8 @@
 // it to our newly made class, we will be sure to set it up with the correct name
 // and .prototype, so that everything works properly.
 
-// The template classes should have all their code on a single line, so that
-// bytecodes' line offsets within the script don't contribute spurious offsets
-// when we transplant them into the real class definition. Punting on column numbers.
-
 var DefaultDerivedClassConstructor =
     class extends null { constructor(...args) { super(...allowContentIter(args)); } };
-MakeDefaultConstructor(DefaultDerivedClassConstructor);
 
 var DefaultBaseClassConstructor =
     class { constructor() { } };
-MakeDefaultConstructor(DefaultBaseClassConstructor);
