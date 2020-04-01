ef1e279b76de3296a8b08e725a1acfd679bf9724
Simon Giesecke <sgiesecke@mozilla.com>
2020-04-01 13:45:40 +0000

    Bug 1626563 - Remove redundant user-provided copy constructors in gfx/thebes. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D69177

--- gfx/thebes/gfxFontUtils.h
+++ gfx/thebes/gfxFontUtils.h
@@ -44,7 +44,6 @@ class gfxSparseBitSet {
   enum { NO_BLOCK = 0xffff };  // index value indicating missing (empty) block
 
   struct Block {
-    Block(const Block& aBlock) { memcpy(mBits, aBlock.mBits, sizeof(mBits)); }
     explicit Block(unsigned char memsetValue = 0) {
       memset(mBits, memsetValue, BLOCK_SIZE);
     }
@@ -53,10 +52,6 @@ class gfxSparseBitSet {
 
  public:
   gfxSparseBitSet() = default;
-  gfxSparseBitSet(const gfxSparseBitSet& aBitset) {
-    mBlockIndex.AppendElements(aBitset.mBlockIndex);
-    mBlocks.AppendElements(aBitset.mBlocks);
-  }
 
   bool Equals(const gfxSparseBitSet* aOther) const {
     if (mBlockIndex.Length() != aOther->mBlockIndex.Length()) {
