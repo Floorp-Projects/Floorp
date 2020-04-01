1e5b31f2bfa488b87952c499dd9c575b072095f2
Tom Schuster <evilpies@gmail.com>
2020-04-01 17:28:36 +0000

    Bug 1607986 - Test. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D68883

---
+++ js/src/jit-test/tests/cacheir/string-int32-arith.js
@@ -0,0 +1,23 @@
+function test(zero, one) {
+    assertEq(10 - zero, 10);
+    assertEq(10 - one, 9);
+    assertEq(zero - 0, 0);
+    assertEq(one - 1, 0);
+
+    assertEq(10 * zero, 0);
+    assertEq(zero * 10, 0);
+    assertEq(10 * one, 10);
+    assertEq(one * 10, 10);
+
+    assertEq(10 / one, 10);
+    assertEq(one / 1, 1);
+    assertEq(10 % one, 0);
+    assertEq(one % 1, 0);
+}
+
+for (var i = 0; i < 10; i++) {
+    test(0, 1);
+    test('0', '1');
+    test('0x0', '0x1');
+    test('0.0', '1.0');
+}
