452c92d58ba44c87fed8ea12544b58a03923cde6
Bogdan Tara <btara@mozilla.com>
2020-04-01 22:27:41 +0300

    Backed out changeset e348ec7e3a2b (bug 1597268) for causing mass bustages CLOSED TREE

--- uriloader/exthandler/nsMIMEInfoImpl.cpp
+++ uriloader/exthandler/nsMIMEInfoImpl.cpp
@@ -309,7 +309,9 @@ nsMIMEInfoBase::LaunchWithFile(nsIFile* aFile) {
 NS_IMETHODIMP
 nsMIMEInfoBase::LaunchWithURI(nsIURI* aURI,
                               nsIInterfaceRequestor* aWindowContext) {
-  // This is only being called with protocol handlers
+  // for now, this is only being called with protocol handlers; that
+  // will change once we get to more general registerContentHandler
+  // support
   NS_ASSERTION(mClass == eProtocolInfo,
                "nsMIMEInfoBase should be a protocol handler");
 
