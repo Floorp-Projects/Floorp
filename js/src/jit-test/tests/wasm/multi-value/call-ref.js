85e7888f562ab54b5ddba0f1c78484ccd6cc1eb3
Andy Wingo <wingo@igalia.com>
2020-04-01 08:57:58 +0000

    Bug 1622828 - Implement wasm->js multi-value calls r=lth
    This patch implements calls from WebAssembly to JS, where the JS
    function returns multiple values.  The multiple values are returned as
    an iterable.
    
    Spec here:
    https://webassembly.github.io/multi-value/js-api/index.html#exported-function-exotic-objects
    
    Differential Revision: https://phabricator.services.mozilla.com/D66988

--- js/src/jit-test/tests/wasm/multi-value/call-ref.js
+++ js/src/jit-test/tests/wasm/multi-value/call-ref.js
@@ -1,31 +1,64 @@
 // |jit-test| skip-if: !wasmReftypesEnabled()
 
-let counter = 0;
-let i = new WebAssembly.Instance(
-    new WebAssembly.Module(wasmTextToBinary(`
+let counter;
+function resetCounter() { counter = 0; }
+
+function boxNextInt() { return {val: counter++}; }
+function unboxInt(box) { return box.val; }
+function boxNextThreeInts() {
+    return [boxNextInt(), boxNextInt(), boxNextInt()];
+}
+function unboxThreeInts(x, y, z) {
+    return [unboxInt(x), unboxInt(y), unboxInt(z)];
+}
+
+function testAddNextThreeInts(text, imports) {
+    let i = new WebAssembly.Instance(
+        new WebAssembly.Module(wasmTextToBinary(text)), { imports });
+
+    resetCounter();
+    for (let n = 0; n < 100000; n += 3) {
+        assertEq(i.exports.addNextThreeInts(), n * 3 + 3);
+    }
+}
+
+testAddNextThreeInts(`
       (module
         (func $boxNextInt (import "imports" "boxNextInt")
           (result anyref))
         (func $unboxInt (import "imports" "unboxInt")
           (param anyref) (result i32))
 
-        (func $boxNextTwoInts (result anyref anyref)
+        (func $boxNextThreeInts (result anyref anyref anyref)
+          call $boxNextInt
           call $boxNextInt
           call $boxNextInt)
 
-        (func $unboxTwoInts (param anyref anyref) (result i32 i32)
+        (func $unboxThreeInts (param anyref anyref anyref) (result i32 i32 i32)
           local.get 0
           call $unboxInt
           local.get 1
+          call $unboxInt
+          local.get 2
           call $unboxInt)
 
-        (func $addNextTwoInts (export "addNextTwoInts") (result i32)
-          call $boxNextTwoInts
-          call $unboxTwoInts
-          i32.add))`)),
-    {imports: {boxNextInt: () => ({val: counter++}),
-               unboxInt: box => box.val}});
+        (func $addNextThreeInts (export "addNextThreeInts") (result i32)
+          call $boxNextThreeInts
+          call $unboxThreeInts
+          i32.add
+          i32.add))`,
+                   {boxNextInt, unboxInt});
 
-for (let n = 0; n < 200000; n += 2) {
-    assertEq(i.exports.addNextTwoInts(), n * 2 + 1);
-}
+testAddNextThreeInts(`
+      (module
+        (func $boxNextThreeInts (import "imports" "boxNextThreeInts")
+          (result anyref anyref anyref))
+        (func $unboxThreeInts (import "imports" "unboxThreeInts")
+          (param anyref anyref anyref) (result i32 i32 i32))
+
+        (func $addNextThreeInts (export "addNextThreeInts") (result i32)
+          call $boxNextThreeInts
+          call $unboxThreeInts
+          i32.add
+          i32.add))`,
+                   {boxNextThreeInts, unboxThreeInts});
--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -1933,7 +1933,7 @@ static bool GenerateImportInterpExit(MacroAssembler& masm, const FuncImport& fi,
   i++;
 
   // argument 2: argc
-  unsigned argc = fi.funcType().args().length();
+  unsigned argc = abiArgCount;
   if (i->kind() == ABIArg::GPR) {
     masm.mov(ImmWord(argc), i->gpr());
   } else {
