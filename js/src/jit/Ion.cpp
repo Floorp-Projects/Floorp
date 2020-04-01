a85c9feda2a56e629cb01b066bd4b942febe107e
Jon Coppeard <jcoppeard@mozilla.com>
2020-04-01 09:43:18 +0000

    Bug 1625212 - Give JitCode a CellHeader r=jandem,tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D68832

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -545,18 +545,18 @@ template JitCode* JitCode::New<NoGC>(JSContext* cx, uint8_t* code,
 void JitCode::copyFrom(MacroAssembler& masm) {
   // Store the JitCode pointer in the JitCodeHeader so we can recover the
   // gcthing from relocation tables.
-  JitCodeHeader::FromExecutable(code_)->init(this);
+  JitCodeHeader::FromExecutable(raw())->init(this);
 
   insnSize_ = masm.instructionsSize();
-  masm.executableCopy(code_);
+  masm.executableCopy(raw());
 
   jumpRelocTableBytes_ = masm.jumpRelocationTableBytes();
-  masm.copyJumpRelocationTable(code_ + jumpRelocTableOffset());
+  masm.copyJumpRelocationTable(raw() + jumpRelocTableOffset());
 
   dataRelocTableBytes_ = masm.dataRelocationTableBytes();
-  masm.copyDataRelocationTable(code_ + dataRelocTableOffset());
+  masm.copyDataRelocationTable(raw() + dataRelocTableOffset());
 
-  masm.processCodeLabels(code_);
+  masm.processCodeLabels(raw());
 }
 
 void JitCode::traceChildren(JSTracer* trc) {
@@ -567,12 +567,12 @@ void JitCode::traceChildren(JSTracer* trc) {
   }
 
   if (jumpRelocTableBytes_) {
-    uint8_t* start = code_ + jumpRelocTableOffset();
+    uint8_t* start = raw() + jumpRelocTableOffset();
     CompactBufferReader reader(start, start + jumpRelocTableBytes_);
     MacroAssembler::TraceJumpRelocations(trc, this, reader);
   }
   if (dataRelocTableBytes_) {
-    uint8_t* start = code_ + dataRelocTableOffset();
+    uint8_t* start = raw() + dataRelocTableOffset();
     CompactBufferReader reader(start, start + dataRelocTableBytes_);
     MacroAssembler::TraceDataRelocations(trc, this, reader);
   }
@@ -597,11 +597,11 @@ void JitCode::finalize(JSFreeOp* fop) {
   // With W^X JIT code, reprotecting memory for each JitCode instance is
   // slow, so we record the ranges and poison them later all at once. It's
   // safe to ignore OOM here, it just means we won't poison the code.
-  if (fop->appendJitPoisonRange(JitPoisonRange(pool_, code_ - headerSize_,
+  if (fop->appendJitPoisonRange(JitPoisonRange(pool_, raw() - headerSize_,
                                                headerSize_ + bufferSize_))) {
     pool_->addRef();
   }
-  code_ = nullptr;
+  cellHeaderAndCode_.setPtr(nullptr);
 
   // Code buffers are stored inside ExecutablePools. Pools are refcounted.
   // Releasing the pool may free it. Horrible hack: if we are using perf
