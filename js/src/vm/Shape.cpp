ecbd72e069453e5c1b0c4ac34cbc2917c3ad7587
Jon Coppeard <jcoppeard@mozilla.com>
2020-04-01 09:43:06 +0000

    Bug 1625212 - Add CellHeaderWithTenuredGCPointer and use it for Shape r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D68831
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/Shape.cpp
+++ js/src/vm/Shape.cpp
@@ -136,7 +136,7 @@ bool Shape::makeOwnBaseShape(JSContext* cx) {
   new (nbase) BaseShape(StackBaseShape(this));
   nbase->setOwned(base()->toUnowned());
 
-  this->base_ = nbase;
+  setBase(nbase);
 
   return true;
 }
@@ -155,10 +155,10 @@ void Shape::handoffTableTo(Shape* shape) {
   MOZ_ASSERT_IF(!shape->isEmptyShape() && shape->isDataProperty(),
                 nbase->slotSpan() > shape->slot());
 
-  this->base_ = nbase->baseUnowned();
+  setBase(nbase->baseUnowned());
   nbase->adoptUnowned(shape->base()->toUnowned());
 
-  shape->base_ = nbase;
+  shape->setBase(nbase);
 }
 
 /* static */
@@ -1090,7 +1090,7 @@ Shape* NativeObject::putDataProperty(JSContext* cx, HandleNativeObject obj,
     if (updateLast) {
       shape->base()->adoptUnowned(nbase);
     } else {
-      shape->base_ = nbase;
+      shape->setBase(nbase);
     }
 
     shape->setSlot(slot);
@@ -1196,7 +1196,7 @@ Shape* NativeObject::putAccessorProperty(JSContext* cx, HandleNativeObject obj,
     if (updateLast) {
       shape->base()->adoptUnowned(nbase);
     } else {
-      shape->base_ = nbase;
+      shape->setBase(nbase);
     }
 
     shape->setSlot(SHAPE_INVALID_SLOT);
@@ -1324,7 +1324,7 @@ bool NativeObject::removeProperty(JSContext* cx, HandleNativeObject obj,
       if (!nbase) {
         return false;
       }
-      previous->base_ = nbase;
+      previous->setBase(nbase);
     }
   }
 
