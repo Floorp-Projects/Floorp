3546511a11659bc7adaecee546f6bab810755d46
Mark Striemer <mstriemer@mozilla.com>
2020-04-02 02:06:25 +0000

    Bug 1559392 - Support drag+drop of add-ons into HTML about:addons r=rpl
    Depends on D65834
    
    Differential Revision: https://phabricator.services.mozilla.com/D65835

--- toolkit/mozapps/extensions/test/browser/browser_dragdrop.js
+++ toolkit/mozapps/extensions/test/browser/browser_dragdrop.js
@@ -2,20 +2,16 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-// This tests simulated drag and drop of files into the add-ons manager.
-// We test with the add-ons manager in its own tab if in Firefox otherwise
-// in its own window.
-// Tests are only simulations of the drag and drop events, we cannot really do
-// this automatically.
+const ABOUT_ADDONS_URL = "chrome://mozapps/content/extensions/aboutaddons.html";
 
-// Instead of loading EventUtils.js into the test scope in browser-test.js for all tests,
-// we only need EventUtils.js for a few files which is why we are using loadSubScript.
-var gManagerWindow;
-var EventUtils = {};
-Services.scriptloader.loadSubScript(
-  "chrome://mochikit/content/tests/SimpleTest/EventUtils.js",
-  EventUtils
+const dragService = Cc["@mozilla.org/widget/dragservice;1"].getService(
+  Ci.nsIDragService
 );
+
+// Test that the drag-drop-addon-installer component installs add-ons and is
+// included in about:addons. There is an issue with EventUtils.synthesizeDrop
+// where it throws an exception when you give it an subbrowser so we test
+// the component directly.
 
 async function checkInstallConfirmation(...names) {
   let notificationCount = 0;
@@ -74,145 +70,201 @@ async function checkInstallConfirmation(...names) {
   Services.obs.removeObserver(observer, "addon-install-started");
 }
 
-function getViewContainer(gManagerWindow) {
-  return gManagerWindow.document.getElementById("category-box");
+function getDragOverTarget(win) {
+  return win.document.querySelector("categories-box");
+}
+
+function getDropTarget(win) {
+  return win.document.querySelector("drag-drop-addon-installer");
+}
+
+function withTestPage(fn) {
+  return BrowserTestUtils.withNewTab(
+    { url: ABOUT_ADDONS_URL, gBrowser },
+    async browser => {
+      let win = browser.contentWindow;
+      await win.customElements.whenDefined("drag-drop-addon-installer");
+      await fn(browser);
+    }
+  );
+}
+
+function initDragSession({ dragData, dropEffect }) {
+  let dropAction;
+  switch (dropEffect) {
+    case null:
+    case undefined:
+    case "move":
+      dropAction = _EU_Ci.nsIDragService.DRAGDROP_ACTION_MOVE;
+      break;
+    case "copy":
+      dropAction = _EU_Ci.nsIDragService.DRAGDROP_ACTION_COPY;
+      break;
+    case "link":
+      dropAction = _EU_Ci.nsIDragService.DRAGDROP_ACTION_LINK;
+      break;
+    default:
+      throw new Error(`${dropEffect} is an invalid drop effect value`);
+  }
+
+  const dataTransfer = new DataTransfer();
+  dataTransfer.dropEffect = dropEffect;
+
+  for (let i = 0; i < dragData.length; i++) {
+    const item = dragData[i];
+    for (let j = 0; j < item.length; j++) {
+      dataTransfer.mozSetDataAt(item[j].type, item[j].data, i);
+    }
+  }
+
+  dragService.startDragSessionForTests(dropAction);
+  const session = dragService.getCurrentSession();
+  session.dataTransfer = dataTransfer;
+
+  return session;
+}
+
+async function simulateDragAndDrop(win, dragData) {
+  const dropTarget = getDropTarget(win);
+  const dragOverTarget = getDragOverTarget(win);
+  const dropEffect = "move";
+
+  const session = initDragSession({ dragData, dropEffect });
+
+  info("Simulate drag over and wait for the drop target to be visible");
+
+  EventUtils.synthesizeDragOver(
+    dragOverTarget,
+    dragOverTarget,
+    dragData,
+    dropEffect,
+    win
+  );
+
+  // This make sure that the fake dataTransfer has still
+  // the expected drop effect after the synthesizeDragOver call.
+  session.dataTransfer.dropEffect = "move";
+
+  await BrowserTestUtils.waitForCondition(
+    () => !dropTarget.hidden,
+    "Wait for the drop target element to be visible"
+  );
+
+  info("Simulate drop dragData on drop target");
+
+  EventUtils.synthesizeDropAfterDragOver(
+    null,
+    session.dataTransfer,
+    dropTarget,
+    win,
+    { _domDispatchOnly: true }
+  );
+
+  dragService.endDragSession(true);
 }
 
 // Simulates dropping a URL onto the manager
 add_task(async function test_drop_url() {
-  let url = TESTROOT + "addons/browser_dragdrop1.xpi";
-  gManagerWindow = await open_manager("addons://list/extension");
-  let promise = checkInstallConfirmation("Drag Drop test 1");
-  let viewContainer = getViewContainer(gManagerWindow);
-  let effect = EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [[{ type: "text/x-moz-url", data: url }]],
-    "copy",
-    gManagerWindow
-  );
-  is(effect, "copy", "Drag should be accepted");
-  await promise;
-  await close_manager(gManagerWindow);
+  for (let fileType of ["xpi", "zip"]) {
+    await withTestPage(async browser => {
+      const url = TESTROOT + `addons/browser_dragdrop1.${fileType}`;
+      const promise = checkInstallConfirmation("Drag Drop test 1");
+
+      await simulateDragAndDrop(browser.contentWindow, [
+        [{ type: "text/x-moz-url", data: url }],
+      ]);
+
+      await promise;
+    });
+  }
 });
 
 // Simulates dropping a file onto the manager
 add_task(async function test_drop_file() {
-  let fileurl = get_addon_file_url("browser_dragdrop1.xpi");
-  gManagerWindow = await open_manager("addons://list/extension");
-  await wait_for_view_load(gManagerWindow);
-  let promise = checkInstallConfirmation("Drag Drop test 1");
-  let viewContainer = getViewContainer(gManagerWindow);
-  let effect = EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [[{ type: "application/x-moz-file", data: fileurl.file }]],
-    "copy",
-    gManagerWindow
-  );
-  is(effect, "copy", "Drag should be accepted");
-  await promise;
-  await close_manager(gManagerWindow);
+  for (let fileType of ["xpi", "zip"]) {
+    await withTestPage(async browser => {
+      let fileurl = get_addon_file_url(`browser_dragdrop1.${fileType}`);
+      let promise = checkInstallConfirmation("Drag Drop test 1");
+
+      await simulateDragAndDrop(browser.contentWindow, [
+        [{ type: "application/x-moz-file", data: fileurl.file }],
+      ]);
+
+      await promise;
+    });
+  }
 });
 
 // Simulates dropping two urls onto the manager
 add_task(async function test_drop_multiple_urls() {
-  let url1 = TESTROOT + "addons/browser_dragdrop1.xpi";
-  let url2 = TESTROOT2 + "addons/browser_dragdrop2.xpi";
-  gManagerWindow = await open_manager("addons://list/extension");
-  await wait_for_view_load(gManagerWindow);
-  let promise = checkInstallConfirmation(
-    "Drag Drop test 1",
-    "Drag Drop test 2"
-  );
-  let viewContainer = getViewContainer(gManagerWindow);
-  let effect = EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [
+  await withTestPage(async browser => {
+    let url1 = TESTROOT + "addons/browser_dragdrop1.xpi";
+    let url2 = TESTROOT2 + "addons/browser_dragdrop2.zip";
+    let promise = checkInstallConfirmation(
+      "Drag Drop test 1",
+      "Drag Drop test 2"
+    );
+
+    await simulateDragAndDrop(browser.contentWindow, [
       [{ type: "text/x-moz-url", data: url1 }],
       [{ type: "text/x-moz-url", data: url2 }],
-    ],
-    "copy",
-    gManagerWindow
-  );
-  is(effect, "copy", "Drag should be accepted");
-  await promise;
-  await close_manager(gManagerWindow);
-});
+    ]);
+
+    await promise;
+  });
+}).skip(); // TODO(rpl): this fails because mozSetDataAt throws IndexSizeError.
 
 // Simulates dropping two files onto the manager
 add_task(async function test_drop_multiple_files() {
-  let fileurl1 = get_addon_file_url("browser_dragdrop1.xpi");
-  let fileurl2 = get_addon_file_url("browser_dragdrop2.xpi");
-  gManagerWindow = await open_manager("addons://list/extension");
-  await wait_for_view_load(gManagerWindow);
-  let promise = checkInstallConfirmation(
-    "Drag Drop test 1",
-    "Drag Drop test 2"
-  );
-  let viewContainer = getViewContainer(gManagerWindow);
-  let effect = EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [
+  await withTestPage(async browser => {
+    let fileurl1 = get_addon_file_url("browser_dragdrop1.zip");
+    let fileurl2 = get_addon_file_url("browser_dragdrop2.xpi");
+    let promise = checkInstallConfirmation(
+      "Drag Drop test 1",
+      "Drag Drop test 2"
+    );
+
+    await simulateDragAndDrop(browser.contentWindow, [
       [{ type: "application/x-moz-file", data: fileurl1.file }],
       [{ type: "application/x-moz-file", data: fileurl2.file }],
-    ],
-    "copy",
-    gManagerWindow
-  );
-  is(effect, "copy", "Drag should be accepted");
-  await promise;
-  await close_manager(gManagerWindow);
-});
+    ]);
+
+    await promise;
+  });
+}).skip(); // TODO(rpl): this fails because mozSetDataAt throws IndexSizeError.
 
 // Simulates dropping a file and a url onto the manager (weird, but should still work)
 add_task(async function test_drop_file_and_url() {
-  let url = TESTROOT + "addons/browser_dragdrop1.xpi";
-  let fileurl = get_addon_file_url("browser_dragdrop2.xpi");
-  gManagerWindow = await open_manager("addons://list/extension");
-  await wait_for_view_load(gManagerWindow);
-  let promise = checkInstallConfirmation(
-    "Drag Drop test 1",
-    "Drag Drop test 2"
-  );
-  let viewContainer = getViewContainer(gManagerWindow);
-  let effect = EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [
+  await withTestPage(async browser => {
+    let url = TESTROOT + "addons/browser_dragdrop1.xpi";
+    let fileurl = get_addon_file_url("browser_dragdrop2.zip");
+    let promise = checkInstallConfirmation(
+      "Drag Drop test 1",
+      "Drag Drop test 2"
+    );
+
+    await simulateDragAndDrop(browser.contentWindow, [
       [{ type: "text/x-moz-url", data: url }],
       [{ type: "application/x-moz-file", data: fileurl.file }],
-    ],
-    "copy",
-    gManagerWindow
-  );
-  is(effect, "copy", "Drag should be accepted");
-  await promise;
-  await close_manager(gManagerWindow);
-});
+    ]);
+
+    await promise;
+  });
+}).skip(); // TODO(rpl): this fails because mozSetDataAt throws IndexSizeError.
 
 // Test that drag-and-drop of an incompatible addon generates
 // an error.
 add_task(async function test_drop_incompat_file() {
-  let gManagerWindow = await open_manager("addons://list/extension");
-  await wait_for_view_load(gManagerWindow);
+  await withTestPage(async browser => {
+    let url = `${TESTROOT}/addons/browser_dragdrop_incompat.xpi`;
 
-  let errorPromise = TestUtils.topicObserved("addon-install-failed");
+    let panelPromise = promisePopupNotificationShown("addon-install-failed");
+    await simulateDragAndDrop(browser.contentWindow, [
+      [{ type: "text/x-moz-url", data: url }],
+    ]);
 
-  let url = `${TESTROOT}/addons/browser_dragdrop_incompat.xpi`;
-  let viewContainer = getViewContainer(gManagerWindow);
-  EventUtils.synthesizeDrop(
-    viewContainer,
-    viewContainer,
-    [[{ type: "text/x-moz-url", data: url }]],
-    "copy",
-    gManagerWindow
-  );
-
-  await errorPromise;
-  ok(true, "Got addon-install-failed event");
-
-  await close_manager(gManagerWindow);
+    let panel = await panelPromise;
+    ok(panel, "Got addon-install-failed popup");
+    panel.button.click();
+  });
 });
