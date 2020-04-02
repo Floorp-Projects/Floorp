d10d16156d0c533631d37efbbd763cc4f8dccf99
yulia <ystartsev@mozilla.com>
2020-04-02 20:08:15 +0000

    Bug 1625116 - add templateStringListExpr to list of optionalexprs; r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D69078

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -7722,6 +7722,7 @@ bool BytecodeEmitter::emitOptionalTree(
           kind == ParseNodeKind::Function || kind == ParseNodeKind::ClassDecl ||
           kind == ParseNodeKind::RegExpExpr ||
           kind == ParseNodeKind::TemplateStringExpr ||
+          kind == ParseNodeKind::TemplateStringListExpr ||
           kind == ParseNodeKind::RawUndefinedExpr || pn->isInParens();
 
       // https://tc39.es/ecma262/#sec-left-hand-side-expressions
