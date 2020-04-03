262b6ab5cdae8363baa9858b52d90a7228efe26c
Jan de Mooij <jdemooij@mozilla.com>
2020-04-03 07:58:32 +0000

    Bug 1626859 - Port the loop iterator mechanism from IonBuilder to WarpBuilder. r=iain
    This unfortunate mechanism is needed for certain iterator loops. This fixes the
    ion/bug1197769.js jit-test to not crash in the exception handler.
    
    A few weeks ago bug 1620960 refactored and cleaned up the IonBuilder code so this
    is just a port of that to WarpBuilder with some minor cleanups.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69321

--- js/src/jit/WarpBuilder.h
+++ js/src/jit/WarpBuilder.h
@@ -93,6 +93,11 @@ class MOZ_STACK_CLASS WarpBuilder {
   LoopStateStack loopStack_;
   PendingEdgesMap pendingEdges_;
 
+  // Loop phis for iterators that need to be kept alive.
+  // TODO: once we support inlining, this needs to be stored once per
+  // compilation instead of builder.
+  PhiVector iterators_;
+
   TempAllocator& alloc() { return alloc_; }
   MIRGraph& graph() { return graph_; }
   const CompileInfo& info() const { return info_; }
@@ -125,6 +130,8 @@ class MOZ_STACK_CLASS WarpBuilder {
   MOZ_MUST_USE bool buildTestBackedge(BytecodeLocation loc);
 
   MOZ_MUST_USE bool resumeAfter(MInstruction* ins, BytecodeLocation loc);
+
+  MOZ_MUST_USE bool addIteratorLoopPhis(BytecodeLocation loopHead);
 
   MConstant* constant(const Value& v);
   void pushConstant(const Value& v);
