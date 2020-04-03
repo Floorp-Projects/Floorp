ba250530c29e56e154dca5af4b2df924a8b606ac
Mark Banner <standard8@mozilla.com>
2020-04-03 11:36:27 +0000

    Bug 1590803 - Replace SearchEngine._isBuiltIn with a public nsISearchEngine.isAppProvided function. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D69233

--- toolkit/components/search/tests/xpcshell/test_async.js
+++ toolkit/components/search/tests/xpcshell/test_async.js
@@ -20,9 +20,11 @@ add_task(async function test_async() {
   // test jar engine is loaded ok.
   let engine = Services.search.getEngineByName("basic");
   Assert.notEqual(engine, null);
+  Assert.ok(engine.isAppProvided, "Should be shown as an app-provided engine");
 
   engine = Services.search.getEngineByName("Simple Engine");
   Assert.notEqual(engine, null);
+  Assert.ok(engine.isAppProvided, "Should be shown as an app-provided engine");
 
   // Check the hidden engine is not loaded.
   engine = Services.search.getEngineByName("hidden");
