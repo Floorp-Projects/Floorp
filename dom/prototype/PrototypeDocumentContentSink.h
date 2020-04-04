fd8d1a348fcc461f4f091b034c25c7c6e2061e8f
Coroiu Cristina <ccoroiu@mozilla.com>
2020-04-05 01:53:18 +0300

    Backed out changeset 14eb5adc6cd9 (bug 1624657) for browser-chrome failures at browser/modules/test/browser/browser_UsageTelemetry_searchbar.js

--- dom/prototype/PrototypeDocumentContentSink.h
+++ dom/prototype/PrototypeDocumentContentSink.h
@@ -105,6 +105,7 @@ class PrototypeDocumentContentSink final : public nsIStreamLoaderObserver,
   RefPtr<nsParserBase> mParser;
   nsCOMPtr<nsIURI> mDocumentURI;
   RefPtr<Document> mDocument;
+  RefPtr<nsNodeInfoManager> mNodeInfoManager;
   RefPtr<ScriptLoader> mScriptLoader;
 
   PrototypeDocumentContentSink* mNextSrcLoadWaiter;  // [OWNER] but not COMPtr
@@ -214,7 +215,7 @@ class PrototypeDocumentContentSink final : public nsIStreamLoaderObserver,
    * Note that the resulting content node is not bound to any tree
    */
   nsresult CreateElementFromPrototype(nsXULPrototypeElement* aPrototype,
-                                      Element** aResult, nsIContent* aParent);
+                                      Element** aResult, bool aIsRoot);
   /**
    * Prepare to walk the current prototype.
    */
