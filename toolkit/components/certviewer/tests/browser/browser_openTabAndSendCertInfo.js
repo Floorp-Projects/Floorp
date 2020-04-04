b6e82a6adae286c9f3018b81e26150a95e3ea25b
jayati <gaurijove@gmail.com>
2020-04-04 06:37:44 +0000

    Bug 1599985 - Ensure that new cert viewer shows the full cert chain when viewing an intermmediate authority cert.r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D68527

--- toolkit/components/certviewer/tests/browser/browser_openTabAndSendCertInfo.js
+++ toolkit/components/certviewer/tests/browser/browser_openTabAndSendCertInfo.js
@@ -32,6 +32,20 @@ function checksCertTab(tabsCount) {
   checkSpec(spec);
 }
 
+async function checkCertChain() {
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function() {
+    let certificateTabs;
+    await ContentTaskUtils.waitForCondition(() => {
+      certificateTabs = content.document
+        .querySelector("certificate-section")
+        .shadowRoot.querySelectorAll(".certificate-tab");
+      return certificateTabs.length;
+    }, "Found certificate tabs.");
+
+    Assert.greater(certificateTabs.length, 1, "Certificate chain tabs shown");
+  });
+}
+
 // taken from https://searchfox.org/mozilla-central/rev/7ed8e2d3d1d7a1464ba42763a33fd2e60efcaedc/security/manager/ssl/tests/mochitest/browser/browser_downloadCert_ui.js#47
 function openCertDownloadDialog(cert) {
   let returnVals = Cc["@mozilla.org/hash-property-bag;1"].createInstance(
@@ -236,22 +250,22 @@ add_task(async function testPreferencesCert() {
     Assert.ok(doc, "doc loaded");
 
     doc.getElementById("certmanagertabs").selectedTab = doc.getElementById(
-      "ca_tab"
+      "mine_tab"
     );
-    let treeView = doc.getElementById("ca-tree").view;
+    let treeView = doc.getElementById("user-tree").view;
     let selectedCert;
     // See https://searchfox.org/mozilla-central/rev/40ef22080910c2e2c27d9e2120642376b1d8b8b2/browser/components/preferences/in-content/tests/browser_cert_export.js#41
     for (let i = 0; i < treeView.rowCount; i++) {
       treeView.selection.select(i);
       dialogWin.getSelectedCerts();
       let certs = dialogWin.selected_certs;
-      if (certs && certs.length == 1 && certs[0]) {
+      if (certs && certs[0]) {
         selectedCert = certs[0];
         break;
       }
     }
     Assert.ok(selectedCert, "A cert should be selected");
-    let viewButton = doc.getElementById("ca_viewButton");
+    let viewButton = doc.getElementById("mine_viewButton");
     Assert.equal(viewButton.disabled, false, "Should enable view button");
 
     let loaded = BrowserTestUtils.waitForNewTab(gBrowser, null, true);
@@ -259,6 +273,7 @@ add_task(async function testPreferencesCert() {
     await loaded;
 
     checksCertTab(tabsCount);
+    await checkCertChain();
   });
   gBrowser.removeCurrentTab(); // closes about:certificate
 });
