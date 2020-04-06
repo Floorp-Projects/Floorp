fc4d0d052e0c3593f6bad22839511a08a2defe35
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2020-04-06 18:47:04 +0000

    Bug 1627749: remove webrtc_version.h and webrtc_update.sh. r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D69851
    
    --HG--
    extra : moz-landing-system : lando

--- media/webrtc/webrtc_version.h
+++
@@ -1,8 +0,0 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set ts=8 sts=2 et sw=2 tw=80: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-#define WEBRTC_SVNVERSION 7864
-#define WEBRTC_PULL_DATE "Wed Dec 10 12:23:33 EST 2014"
