66238a9156bb0e8b22eadebc03ee55d74ce95998
Guido Urdaneta <guidou@chromium.org>
2020-04-06 21:58:21 +0000

    Bug 1627032 [wpt PR 22661] - [RTCInsertableStreams] Add web tests for audio insertable streams, a=testonly
    Automatic update from web-platform-tests
    [RTCInsertableStreams] Add web tests for audio insertable streams
    
    Drive-by: update video tests to return correct promise and improve
    style.
    
    Bug: 1052765
    Change-Id: Id1c31b577f0431b2073f0ae486926283aca41759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132200
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Marina Ciocea <marinaciocea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755919}
    
    --
    
    wpt-commits: b3c42806623c70da7750344b16f3b6d796c9fe17
    wpt-pr: 22661

--- testing/web-platform/tests/webrtc-insertable-streams/resources/serviceworker-failure.js
+++ testing/web-platform/tests/webrtc-insertable-streams/resources/serviceworker-failure.js
@@ -8,14 +8,14 @@ self.onmessage = e => {
   if (e.data === "start in window") {
     assert_equals(state, "start in worker");
     e.source.postMessage(state);
-    state = "we are expecting a messageerror due to the window sending us an RTCEncodedVideoFrame";
+    state = "we are expecting a messageerror due to the window sending us an RTCEncodedVideoFrame or RTCEncodedAudioFrame";
   } else {
     e.source.postMessage(`worker onmessage was reached when in state "${state}" and data ${e.data}`);
   }
 };
 
 self.onmessageerror = e => {
-  if (state === "we are expecting a messageerror due to the window sending us an RTCEncodedVideoFrame") {
+  if (state === "we are expecting a messageerror due to the window sending us an RTCEncodedVideoFrame or RTCEncodedAudioFrame") {
     assert_equals(e.constructor.name, "ExtendableMessageEvent", "type");
     assert_equals(e.data, null, "data");
     assert_equals(e.origin, self.origin, "origin");
