3e62ad73c451c1688dab8dac87a6c11086835e50
Nika Layzell <nika@thelayzells.com>
2020-04-06 14:29:59 +0000

    Bug 1616353 - Part 7.1: Introduce a new nsOpenWindowInfo type, r=Gijs,kmag
    This type replaces the individual properties, like nextRemoteTabId and
    presetOpenerWindow, which were passed around by frontend code for window opening
    requests performed by Gecko. This new type is intentionally native-only to make
    it easier to pass more information down to newly created windows in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D67050

---
+++ toolkit/components/windowwatcher/nsOpenWindowInfo.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsOpenWindowInfo_h
+#define nsOpenWindowInfo_h
+
+#include "nsIOpenWindowInfo.h"
+#include "nsISupportsImpl.h"
+#include "mozilla/OriginAttributes.h"
+#include "mozilla/RefPtr.h"
+
+class nsOpenWindowInfo : public nsIOpenWindowInfo {
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOPENWINDOWINFO
+
+  bool mForceNoOpener = false;
+  bool mIsRemote = false;
+  uint64_t mNextRemoteBrowserId = 0;
+  OriginAttributes mOriginAttributes;
+  RefPtr<BrowsingContext> mParent;
+
+ private:
+  virtual ~nsOpenWindowInfo() = default;
+};
+
+#endif  // nsOpenWindowInfo_h
