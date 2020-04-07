a39a136567d96e69b7e6d994509a4f2aaa24bf4b
Harry Twyford <htwyford@mozilla.com>
2020-04-07 20:30:25 +0000

    Bug 1623637 - Part 5 - Port test_search_suggestions.js. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D69700
    
    --HG--
    rename : toolkit/components/places/tests/unifiedcomplete/test_search_suggestions.js => browser/components/urlbar/tests/unit/test_search_suggestions.js
    extra : moz-landing-system : lando

--- browser/components/urlbar/tests/unit/head.js
+++ browser/components/urlbar/tests/unit/head.js
@@ -46,6 +46,7 @@ AddonTestUtils.createAppInfo(
  *          required options.
  */
 function createContext(searchString = "foo", properties = {}) {
+  info(`Creating new queryContext with searchString: ${searchString}`);
   return new UrlbarQueryContext(
     Object.assign(
       {
@@ -215,4 +216,205 @@ async function addTestSuggestionsEngine(suggestionsFn = null) {
     resp.write(JSON.stringify(data));
   });
   return addTestEngine("engine-suggestions.xml", server);
+}
+
+/**
+ * Creates a UrlbarResult for a search result.
+ * @param {UrlbarQueryContext} queryContext
+ *   The context that this result will be displayed in.
+ * @param {string} [options.suggestion]
+ *   The suggestion offered by the search engine.
+ * @param {string} [options.engineName]
+ *   The name of the engine providing the suggestion. Leave blank if there
+ *   is no suggestion.
+ * @param {string} [options.query]
+ *   The query that started the search. This overrides
+ *   `queryContext.searchString`. This is useful when the query that will show
+ *   up in the result object will be different from what was typed. For example,
+ *   if a leading restriction token will be used.
+ * @param {string} [options.alias]
+ *   The alias for the search engine, if the search is an alias search.
+ * @param {string} [options.engineIconUri]
+ *   A URI for the engine's icon.
+ * @param {boolean} [options.heuristic]
+ *   True if this is a heuristic result. Defaults to false.
+ * @param {number} [options.keywordOffer]
+ *   A value from UrlbarUtils.KEYWORD_OFFER.
+ * @returns {UrlbarResult}
+ */
+function makeSearchResult(
+  queryContext,
+  {
+    suggestion,
+    engineName,
+    alias,
+    query,
+    engineIconUri,
+    heuristic = false,
+    keywordOffer,
+  }
+) {
+  UrlbarTokenizer.tokenize(queryContext);
+
+  if (!keywordOffer) {
+    keywordOffer = UrlbarUtils.KEYWORD_OFFER.NONE;
+    if (alias && !query.trim() && alias.startsWith("@")) {
+      keywordOffer = heuristic
+        ? UrlbarUtils.KEYWORD_OFFER.HIDE
+        : UrlbarUtils.KEYWORD_OFFER.SHOW;
+    }
+  }
+
+  let result = new UrlbarResult(
+    UrlbarUtils.RESULT_TYPE.SEARCH,
+    UrlbarUtils.RESULT_SOURCE.SEARCH,
+    ...UrlbarResult.payloadAndSimpleHighlights(queryContext.tokens, {
+      engine: [engineName, UrlbarUtils.HIGHLIGHT.TYPED],
+      suggestion: [suggestion, UrlbarUtils.HIGHLIGHT.SUGGESTED],
+      keyword: [alias, UrlbarUtils.HIGHLIGHT.TYPED],
+      // Check against undefined so consumers can pass in the empty string.
+      query: [
+        typeof query != "undefined" ? query : queryContext.searchString.trim(),
+        UrlbarUtils.HIGHLIGHT.TYPED,
+      ],
+      icon: [engineIconUri ? engineIconUri : ""],
+      keywordOffer,
+    })
+  );
+
+  result.heuristic = heuristic;
+  return result;
+}
+
+/**
+ * Creates a UrlbarResult for a history result.
+ * @param {UrlbarQueryContext} queryContext
+ *   The context that this result will be displayed in.
+ * @param {string} options.title
+ *   The page title.
+ * @param {string} options.uri
+ *   The page URI.
+ * @param {array} [options.tags]
+ *   An array of string tags. Defaults to an empty array.
+ * @param {string} [options.iconUri]
+ *   A URI for the page's icon.
+ * @param {boolean} [options.heuristic]
+ *   True if this is a heuristic result. Defaults to false.
+ * @returns {UrlbarResult}
+ */
+function makeVisitResult(
+  queryContext,
+  { title, uri, iconUri, tags = [], heuristic = false }
+) {
+  UrlbarTokenizer.tokenize(queryContext);
+
+  let payload = {
+    url: [uri, UrlbarUtils.HIGHLIGHT.TYPED],
+    // Check against undefined so consumers can pass in the empty string.
+    icon: [typeof iconUri != "undefined" ? iconUri : `page-icon:${uri}`],
+    title: [title, UrlbarUtils.HIGHLIGHT.TYPED],
+  };
+
+  if (!heuristic) {
+    payload.tags = [tags, UrlbarUtils.HIGHLIGHT.TYPED];
+  }
+
+  let result = new UrlbarResult(
+    UrlbarUtils.RESULT_TYPE.URL,
+    UrlbarUtils.RESULT_SOURCE.OTHER_LOCAL,
+    ...UrlbarResult.payloadAndSimpleHighlights(queryContext.tokens, payload)
+  );
+
+  result.heuristic = heuristic;
+  return result;
+}
+
+/**
+ * Creates a UrlbarResult for a bookmark result.
+ * @param {UrlbarQueryContext} queryContext
+ *   The context that this result will be displayed in.
+ * @param {string} options.title
+ *   The page title.
+ * @param {string} options.uri
+ *   The page URI.
+ * @param {string} [options.iconUri]
+ *   A URI for the page's icon.
+ * @param {array} [options.tags]
+ *   An array of string tags. Defaults to an empty array.
+ * @param {boolean} [options.heuristic]
+ *   True if this is a heuristic result. Defaults to false.
+ * @returns {UrlbarResult}
+ */
+function makeBookmarkResult(
+  queryContext,
+  { title, uri, iconUri, tags = [], heuristic = false }
+) {
+  UrlbarTokenizer.tokenize(queryContext);
+
+  let result = new UrlbarResult(
+    UrlbarUtils.RESULT_TYPE.URL,
+    UrlbarUtils.RESULT_SOURCE.BOOKMARKS,
+    ...UrlbarResult.payloadAndSimpleHighlights(queryContext.tokens, {
+      url: [uri, UrlbarUtils.HIGHLIGHT.TYPED],
+      // Check against undefined so consumers can pass in the empty string.
+      icon: [typeof iconUri != "undefined" ? iconUri : `page-icon:${uri}`],
+      title: [title, UrlbarUtils.HIGHLIGHT.TYPED],
+      tags: [tags, UrlbarUtils.HIGHLIGHT.TYPED],
+    })
+  );
+
+  result.heuristic = heuristic;
+  return result;
+}
+
+/**
+ * Checks that the results returned by a UrlbarController match those in
+ * the param `matches`.
+ * @param {UrlbarQueryContext} context
+ *   The context for this query.
+ * @param {array} matches
+ *   An array of UrlbarResults.
+ * @param {boolean} [isPrivate]
+ *   Set this to `true` to simulate a search in a private window.
+ */
+async function check_results({ context, matches = [] } = {}) {
+  if (!context) {
+    return;
+  }
+
+  // At this point frecency could still be updating due to latest pages
+  // updates.
+  // This is not a problem in real life, but autocomplete tests should
+  // return reliable resultsets, thus we have to wait.
+  await PlacesTestUtils.promiseAsyncUpdates();
+
+  let controller = UrlbarTestUtils.newMockController({
+    input: {
+      isPrivate: context.isPrivate,
+      window: {
+        location: {
+          href: AppConstants.BROWSER_CHROME_URL,
+        },
+      },
+    },
+  });
+  await controller.startQuery(context);
+
+  Assert.equal(
+    context.results.length,
+    matches.length,
+    "Found the expected number of results."
+  );
+
+  Assert.deepEqual(
+    matches.map(m => m.payload),
+    context.results.map(m => m.payload),
+    "Payloads are the same."
+  );
+
+  Assert.deepEqual(
+    matches.map(m => m.heuristic),
+    context.results.map(m => m.heuristic),
+    "Heuristic results are correctly flagged."
+  );
 }
