1a4bd13b33684a03d098882904fead4c98bc0543
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-04-07 12:15:59 +0000

    Bug 1627687 - Make console complicated reload test more reliable. r=jdescottes,perftest-reviewers,sparky.
    This patch modifies the reloadConsoleAndLog helper so it can
    consumes an array of expected messages, and not only a number
    of expected messages.
    This should prevent the performance variations caused by new
    warning messages being added.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69816
    
    --HG--
    extra : moz-landing-system : lando

--- testing/talos/talos/tests/devtools/addon/content/tests/webconsole/complicated.js
+++ testing/talos/talos/tests/devtools/addon/content/tests/webconsole/complicated.js
@@ -14,14 +14,44 @@ const {
 } = require("../head");
 const { reloadConsoleAndLog } = require("./webconsole-helpers");
 
-const EXPECTED_MESSAGES = isFissionEnabled() ? 3 : 7;
+const EXPECTED_MESSAGES = [
+  {
+    text: `This page uses the non standard property “zoom”`,
+    count: isFissionEnabled() ? 2 : 4,
+    visibleWhenFissionEnabled: true,
+  },
+  {
+    text: `Layout was forced before the page was fully loaded.`,
+    visibleWhenFissionEnabled: true,
+  },
+  {
+    text: `Some cookies are misusing the “sameSite“ attribute, so it won’t work as expected`,
+    visibleWhenFissionEnabled: true,
+  },
+  {
+    text: `InvalidStateError: XMLHttpRequest state must be OPENED.`,
+    visibleWhenFissionEnabled: true,
+  },
+  {
+    text: `SyntaxError: missing ) after argument list`,
+    count: 2,
+    visibleWhenFissionEnabled: false,
+  },
+  {
+    text: `ReferenceError: Bootloaddisableder is not defined`,
+    count: 4,
+    visibleWhenFissionEnabled: false,
+  },
+].filter(
+  ({ visibleWhenFissionEnabled }) =>
+    !isFissionEnabled() || visibleWhenFissionEnabled
+);
 
 module.exports = async function() {
   await testSetup(COMPLICATED_URL);
 
   let toolbox = await openToolboxAndLog("complicated.webconsole", "webconsole");
   await reloadConsoleAndLog("complicated", toolbox, EXPECTED_MESSAGES);
-
   await closeToolboxAndLog("complicated.webconsole", toolbox);
 
   await testTeardown();
