715e096b9af8adfa7e11850f8afad29bf98fba17
Jonathan Kew <jkew@mozilla.com>
2020-04-07 12:30:17 +0000

    Bug 1550037 - patch 1 - Migrate shared font-list code from mozilla::ipc::SharedMemoryBasic to base::SharedMemory APIs. r=jwatt
    The base::SharedMemory class provides APIs to create a "read-only" copy of a shared memory block,
    which means it can be shared to a child process without the risk that the child might map it as
    writable and corrupt the contents. We want to use this facility for the font list, hence switching
    the shared-memory APIs used.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68778
    
    --HG--
    extra : moz-landing-system : lando

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -4977,7 +4977,7 @@ mozilla::ipc::IPCResult ContentParent::RecvGetOutputColorProfileData(
 
 mozilla::ipc::IPCResult ContentParent::RecvGetFontListShmBlock(
     const uint32_t& aGeneration, const uint32_t& aIndex,
-    mozilla::ipc::SharedMemoryBasic::Handle* aOut) {
+    base::SharedMemoryHandle* aOut) {
   auto fontList = gfxPlatformFontList::PlatformFontList();
   MOZ_RELEASE_ASSERT(fontList, "gfxPlatformFontList not initialized?");
   fontList->ShareFontListShmBlockToProcess(aGeneration, aIndex, Pid(), aOut);
