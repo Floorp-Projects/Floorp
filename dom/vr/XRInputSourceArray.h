e2b211270f7d820d4e095d074139d085b529e7e1
Daosheng Mu <daoshengmu@gmail.com>
2020-04-07 21:26:01 +0000

    Bug 1611310 - Implement XRInputSource module. r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D67430
    
    --HG--
    extra : moz-landing-system : lando

--- dom/vr/XRInputSourceArray.h
+++ dom/vr/XRInputSourceArray.h
@@ -13,6 +13,9 @@
 #include "gfxVR.h"
 
 namespace mozilla {
+namespace gfx {
+struct VRControllerState;
+}
 namespace dom {
 class XRInputSource;
 
@@ -28,9 +31,12 @@ class XRInputSourceArray final : public nsISupports, public nsWrapperCache {
   JSObject* WrapObject(JSContext* aCx,
                        JS::Handle<JSObject*> aGivenProto) override;
 
-  // WebIDL Memberss
+  // WebIDL Members
   XRInputSource* IndexedGetter(uint32_t aIndex, bool& aFound);
   uint32_t Length();
+  void Setup(XRSession* aSession, RefPtr<gfx::VRDisplayClient> aDisplayClient);
+  void Update(XRSession* aSession);
+  void Clear();
 
  protected:
   virtual ~XRInputSourceArray() = default;
