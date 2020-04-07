65efdc405d7387e93a0c73871dafaac0c104185b
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-04-07 00:09:38 +0000

    Bug 1603182: Add a test for target-switching of performance panel. r=julienw
    Depends on D68537
    
    Differential Revision: https://phabricator.services.mozilla.com/D69488

--- devtools/client/performance/test/helpers/urls.js
+++ devtools/client/performance/test/helpers/urls.js
@@ -5,3 +5,5 @@
 exports.EXAMPLE_URL =
   "http://example.com/browser/devtools/client/performance/test";
 exports.SIMPLE_URL = `${exports.EXAMPLE_URL}/doc_simple-test.html`;
+// Used to test a page running on main process.
+exports.MAIN_PROCESS_URL = "about:robots";
