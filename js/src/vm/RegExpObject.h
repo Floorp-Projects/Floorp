083dec2cfdc41deff7173ed66644d635763a50e9
Iain Ireland <iireland@mozilla.com>
2020-04-07 21:06:48 +0000

    Bug 1627356: Remove MatchOnly mode r=tcampbell
    MatchOnly mode is a hack. Instead of rewriting it for our new irregexp import, I'm just removing it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69629

--- js/src/vm/RegExpObject.h
+++ js/src/vm/RegExpObject.h
@@ -206,7 +206,6 @@ class RegExpObject : public NativeObject {
 #ifdef DEBUG
   static MOZ_MUST_USE bool dumpBytecode(JSContext* cx,
                                         Handle<RegExpObject*> regexp,
-                                        bool match_only,
                                         HandleLinearString input);
 #endif
 
