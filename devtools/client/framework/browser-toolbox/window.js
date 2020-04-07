efd9f16f51a4b3861bb43cc3a5662ae2bfb0b7ef
Julian Descottes <jdescottes@mozilla.com>
2020-04-07 13:18:55 +0000

    Bug 1627933 - Fix preference name to disable new perf panel for the Browser Toolbox r=julienw
    This line was introduced in https://bugzilla.mozilla.org/show_bug.cgi?id=1408124
    
    However the preference is incorrectly named. It should be devtools.performance.new-panel-enabled instead of devtools.preference.new-panel-enabled (which doesn't exist)
    
    Differential Revision: https://phabricator.services.mozilla.com/D69985

--- devtools/client/framework/browser-toolbox/window.js
+++ devtools/client/framework/browser-toolbox/window.js
@@ -137,7 +137,7 @@ function setPrefDefaults() {
   );
   // Bug 1225160 - Using source maps with browser debugging can lead to a crash
   Services.prefs.setBoolPref("devtools.debugger.source-maps-enabled", false);
-  Services.prefs.setBoolPref("devtools.preference.new-panel-enabled", false);
+  Services.prefs.setBoolPref("devtools.performance.new-panel-enabled", false);
   Services.prefs.setBoolPref("layout.css.emulate-moz-box-with-flex", false);
 
   Services.prefs.setBoolPref("devtools.performance.enabled", false);
