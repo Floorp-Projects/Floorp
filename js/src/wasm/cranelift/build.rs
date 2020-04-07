98ce8548f3b7137188165c0fbc77e337fc5db4a1
Ryan Hunt <rhunt@eqrion.net>
2020-04-07 07:54:36 +0000

    Bug 1627835 - Emit build system dependencies for Cranelift build.rs. r=bbouvier
    bindgen::CargoCallbacks will emit rustc-rerun-if-changed for included headers
    so that the build system can re-run build.rs if any header used is changed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69882

--- js/src/wasm/cranelift/build.rs
+++ js/src/wasm/cranelift/build.rs
@@ -31,6 +31,7 @@ fn main() {
     println!("cargo:rerun-if-changed=baldrapi.h");
 
     let mut generator = bindgen::builder()
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .disable_name_namespacing()
         .size_t_is_usize(true)
         // We whitelist the Baldr C functions and get the associated types for free.
