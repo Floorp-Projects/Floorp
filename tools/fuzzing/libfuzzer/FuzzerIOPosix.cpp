f6918c72706865c36c8f09520c4b86f65cf622bd
Jesse Schwartzentruber <truber@mozilla.com>
2020-04-07 15:53:03 +0000

    Bug 1608849 - Switch Linux Fuzzing Debug build to use clang. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D59782

--- tools/fuzzing/libfuzzer/FuzzerIOPosix.cpp
+++ tools/fuzzing/libfuzzer/FuzzerIOPosix.cpp
@@ -7,6 +7,7 @@
 //===----------------------------------------------------------------------===//
 // IO functions implementation using Posix API.
 //===----------------------------------------------------------------------===//
+#include "mozilla/Unused.h"
 #include "FuzzerDefs.h"
 #if LIBFUZZER_POSIX || LIBFUZZER_FUCHSIA
 
@@ -159,7 +160,7 @@ bool IsInterestingCoverageFile(const std::string &FileName) {
 }
 
 void RawPrint(const char *Str) {
-  write(2, Str, strlen(Str));
+  mozilla::Unused << write(2, Str, strlen(Str));
 }
 
 void MkDir(const std::string &Path) {
