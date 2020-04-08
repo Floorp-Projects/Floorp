1604123014aaa1a9c868d429443cee464a58f473
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-04-08 13:52:33 +0000

    Bug 1627167- Re-enable Resources API to fetch console messages. r=Honza.
    Differential Revision: https://phabricator.services.mozilla.com/D69968

--- devtools/client/webconsole/webconsole-ui.js
+++ devtools/client/webconsole/webconsole-ui.js
@@ -330,12 +330,12 @@ class WebConsoleUI {
       this._onTargetAvailable,
       this._onTargetDestroy
     );
-    // TODO: Re-enable as part of Bug 1627167.
-    // const resourceWatcher = this.hud.resourceWatcher;
-    // await resourceWatcher.watch(
-    //   [resourceWatcher.TYPES.CONSOLE_MESSAGES],
-    //   this._onResourceAvailable
-    // );
+
+    const resourceWatcher = this.hud.resourceWatcher;
+    await resourceWatcher.watch(
+      [resourceWatcher.TYPES.CONSOLE_MESSAGES],
+      this._onResourceAvailable
+    );
   }
 
   _onResourceAvailable({ resourceType, targetFront, resource }) {
