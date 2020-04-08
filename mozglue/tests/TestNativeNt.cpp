a77f2601204f72e83d8cde1586cdecfe9a862e35
Toshihito Kikuchi <tkikuchi@mozilla.com>
2020-04-08 14:27:02 +0000

    Bug 1603974 - Part 6: Introduce Kernel32ExportsSolver.  r=mhowell
    This patch introduces `Kernel32ExportsSolver` which calculates RVAs of
    kernel32's functions and transfers them to a target process, where the
    transferred RVAs are resolved into function addresses.
    
    Depends on D68346
    
    Differential Revision: https://phabricator.services.mozilla.com/D68347
    
    --HG--
    extra : moz-landing-system : lando

--- mozglue/tests/TestNativeNt.cpp
+++ mozglue/tests/TestNativeNt.cpp
@@ -64,6 +64,12 @@ bool TestVirtualQuery(HANDLE aProcess, LPCVOID aAddress) {
   return true;
 }
 
+LauncherResult<HMODULE> GetModuleHandleFromLeafName(const wchar_t* aName) {
+  UNICODE_STRING name;
+  ::RtlInitUnicodeString(&name, aName);
+  return nt::GetModuleHandleFromLeafName(name);
+}
+
 // Need a non-inline function to bypass compiler optimization that the thread
 // local storage pointer is cached in a register before accessing a thread-local
 // variable.
@@ -263,6 +269,23 @@ int wmain(int argc, wchar_t* argv[]) {
   if (!TestVirtualQuery(process, nullptr) || !TestVirtualQuery(process, argv) ||
       !TestVirtualQuery(process, kNormal) ||
       !TestVirtualQuery(nullptr, kNormal)) {
+    return 1;
+  }
+
+  auto moduleResult = GetModuleHandleFromLeafName(kKernel32);
+  if (moduleResult.isErr() ||
+      moduleResult.inspect() != k32headers.template RVAToPtr<HMODULE>(0)) {
+    printf(
+        "TEST-FAILED | NativeNt | "
+        "GetModuleHandleFromLeafName returns a wrong value.\n");
+    return 1;
+  }
+
+  moduleResult = GetModuleHandleFromLeafName(L"invalid");
+  if (moduleResult.isOk()) {
+    printf(
+        "TEST-FAILED | NativeNt | "
+        "GetModuleHandleFromLeafName unexpectedly returns a value.\n");
     return 1;
   }
 
