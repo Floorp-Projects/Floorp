b1fdbf26acc29aceaa8eb83ef8082a6a47dafd39
Jim Blandy <jimb@mozilla.com>
2020-04-08 23:31:36 +0000

    Bug 1628508: Add 'IMPLICIT' comment for webrender_api::display_item::ScrollFrameDisplayItem. DONTBUILD r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D70269

--- gfx/wr/webrender_api/src/display_item.rs
+++ gfx/wr/webrender_api/src/display_item.rs
@@ -289,7 +289,7 @@ pub struct ScrollFrameDisplayItem {
     /// should be added to those display item coordinates in order to get a
     /// normalized value that is consistent across display lists.
     pub external_scroll_offset: LayoutVector2D,
-}
+} // IMPLICIT: complex_clips: Vec<ComplexClipRegion>
 
 /// A solid or an animating color to draw (may not actually be a rectangle due to complex clips)
 #[derive(Clone, Copy, Debug, Default, Deserialize, PartialEq, Serialize, PeekPoke)]
