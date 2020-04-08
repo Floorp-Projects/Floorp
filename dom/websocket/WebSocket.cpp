2b6cb398200f50300fc3716d965d62d9be078d62
sonakshi <sonakshisaxena1@gmail.com>
2020-04-08 19:44:43 +0000

    Bug 1621345 - Support for nsIWebSocketEventListener.send() API r=michal,baku
    Differential Revision: https://phabricator.services.mozilla.com/D68068

--- dom/websocket/WebSocket.cpp
+++ dom/websocket/WebSocket.cpp
@@ -66,6 +66,7 @@
 #include "nsIWebSocketListener.h"
 #include "nsProxyRelease.h"
 #include "nsWeakReference.h"
+#include "nsIWebSocketImpl.h"
 
 #define OPEN_EVENT_STRING NS_LITERAL_STRING("open")
 #define MESSAGE_EVENT_STRING NS_LITERAL_STRING("message")
@@ -82,7 +83,8 @@ class WebSocketImpl final : public nsIInterfaceRequestor,
                             public nsIObserver,
                             public nsSupportsWeakReference,
                             public nsIRequest,
-                            public nsIEventTarget {
+                            public nsIEventTarget,
+                            public nsIWebSocketImpl {
  public:
   NS_DECL_NSIINTERFACEREQUESTOR
   NS_DECL_NSIWEBSOCKETLISTENER
@@ -90,6 +92,7 @@ class WebSocketImpl final : public nsIInterfaceRequestor,
   NS_DECL_NSIREQUEST
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIEVENTTARGET_FULL
+  NS_DECL_NSIWEBSOCKETIMPL
 
   explicit WebSocketImpl(WebSocket* aWebSocket)
       : mWebSocket(aWebSocket),
@@ -237,7 +240,7 @@ class WebSocketImpl final : public nsIInterfaceRequestor,
 
 NS_IMPL_ISUPPORTS(WebSocketImpl, nsIInterfaceRequestor, nsIWebSocketListener,
                   nsIObserver, nsISupportsWeakReference, nsIRequest,
-                  nsIEventTarget)
+                  nsIEventTarget, nsIWebSocketImpl)
 
 class CallDispatchConnectionCloseEvents final : public CancelableRunnable {
  public:
@@ -611,6 +614,22 @@ void WebSocketImpl::DisconnectInternal() {
       os->RemoveObserver(this, DOM_WINDOW_FROZEN_TOPIC);
     }
   }
+}
+
+//-----------------------------------------------------------------------------
+// WebSocketImpl::nsIWebSocketImpl
+//-----------------------------------------------------------------------------
+
+NS_IMETHODIMP
+WebSocketImpl::SendMessage(const nsAString& aMessage) {
+  nsString message(aMessage);
+  nsCOMPtr<nsIRunnable> runnable = NS_NewRunnableFunction(
+      "WebSocketImpl::SendMessage",
+      [self = RefPtr<WebSocketImpl>(this), message = std::move(message)]() {
+        ErrorResult IgnoredErrorResult;
+        self->mWebSocket->Send(message, IgnoredErrorResult);
+      });
+  return Dispatch(runnable.forget(), NS_DISPATCH_NORMAL);
 }
 
 //-----------------------------------------------------------------------------
@@ -1778,6 +1797,11 @@ nsresult WebSocketImpl::InitializeConnection(
   NS_ENSURE_SUCCESS(rv, rv);
 
   mChannel = wsChannel;
+
+  nsWeakPtr mWeakWebSocketImpl = do_GetWeakReference(this);
+
+  mService->AssociateWebSocketImplWithSerialID(mWeakWebSocketImpl,
+                                               mChannel->Serial());
 
   if (mIsMainThread && doc) {
     mMainThreadEventTarget = doc->EventTargetFor(TaskCategory::Other);
