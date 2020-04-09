74169022fe3c6d84fe95b8cebfc09354c326a751
André Bargull <andre.bargull@gmail.com>
2020-04-09 14:58:51 +0000

    Bug 1628309 - Part 1: Move always-inlined bitwise operations into Interpreter.cpp. r=jandem
    Moves the always-inline functions into Interpreter.cpp and appends "Operation"
    to the function name, similar to how the inlined arithmetic functions are named.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70184

--- js/src/vm/Interpreter.h
+++ js/src/vm/Interpreter.h
@@ -547,6 +547,23 @@ bool ModValues(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
 bool PowValues(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
                MutableHandleValue res);
 
+bool BitNot(JSContext* cx, MutableHandleValue in, MutableHandleValue res);
+
+bool BitXor(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
+            MutableHandleValue res);
+
+bool BitOr(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
+           MutableHandleValue res);
+
+bool BitAnd(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
+            MutableHandleValue res);
+
+bool BitLsh(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
+            MutableHandleValue res);
+
+bool BitRsh(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
+            MutableHandleValue res);
+
 bool UrshValues(JSContext* cx, MutableHandleValue lhs, MutableHandleValue rhs,
                 MutableHandleValue res);
 
