18d0a14fbd031136269539476f8ff218d981c9bf
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2020-04-10 23:59:58 +0000

    Bug 1629174 - Don't cache input values from the login doorhanger upon dismissal. r=sfoster
    We already cache the values from `input` event listeners. The "dismissed" listener can happen before "shown" and cause old values to be used.
    
    * Also clear the fields now in the "dismissed" listener so that automated tests don't have stale values and therefore false positives. This has a nice side-effect of removing the credentials from memory sooner if the user doesn't save.
    * Added some logging for doorhanger opening/closing as it seems like it is low volume and could be useful in the future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70566
    
    --HG--
    rename : toolkit/components/passwordmgr/test/browser/subtst_notifications_11_popup.html => toolkit/components/passwordmgr/test/browser/subtst_notifications_12_target_blank.html
    extra : moz-landing-system : lando

---
+++ toolkit/components/passwordmgr/test/browser/browser_doorhanger_target_blank.js
@@ -0,0 +1,86 @@
+/**
+ * Test capture popup notifications when the login form uses target="_blank"
+ */
+
+add_task(async function setup() {
+  await SimpleTest.promiseFocus(window);
+});
+
+add_task(async function test_saveTargetBlank() {
+  // This test submits the form to a new tab using target="_blank".
+  let url = "subtst_notifications_12_target_blank.html?notifyu3|notifyp3||";
+  let notifShownPromise = BrowserTestUtils.waitForEvent(
+    PopupNotifications.panel,
+    "popupshown"
+  );
+
+  let submissionTabPromise = BrowserTestUtils.waitForNewTab(
+    gBrowser,
+    url => {
+      info(url);
+      return url.includes("formsubmit.sjs");
+    },
+    false,
+    true
+  );
+
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "http://mochi.test:8888" + DIRECTORY_PATH + url,
+    },
+    async function() {
+      // For now the doorhanger appears in the previous tab but it should maybe
+      // appear in the new tab from target="_blank"?
+      BrowserTestUtils.removeTab(await submissionTabPromise);
+
+      let notif = await TestUtils.waitForCondition(
+        () =>
+          getCaptureDoorhangerThatMayOpen(
+            "password-save",
+            PopupNotifications,
+            gBrowser.selectedBrowser
+          ),
+        "Waiting for doorhanger"
+      );
+      ok(notif, "got notification popup");
+
+      EventUtils.synthesizeMouseAtCenter(notif.anchorElement, {});
+      await notifShownPromise;
+      await checkDoorhangerUsernamePassword("notifyu3", "notifyp3");
+      let storageChangedPromised = TestUtils.topicObserved(
+        "passwordmgr-storage-changed",
+        (subject, data) => data != "removeLogin"
+      );
+
+      clickDoorhangerButton(notif, REMEMBER_BUTTON);
+      await storageChangedPromised;
+      BrowserTestUtils.removeTab(gBrowser.selectedTab);
+    }
+  );
+
+  // Check result of clicking Remember
+  let logins = Services.logins.getAllLogins();
+  is(logins.length, 1, "Should only have 1 login now");
+  let login = logins[0].QueryInterface(Ci.nsILoginMetaInfo);
+  is(login.username, "notifyu3", "Check the username used on the new entry");
+  is(login.password, "notifyp3", "Check the password used on the new entry");
+  is(login.timesUsed, 1, "Check times used on new entry");
+
+  // Check for stales values in the doorhanger <input> after closing.
+  let usernameField = document.getElementById("password-notification-username");
+  is(
+    usernameField.value,
+    "",
+    "Check the username field doesn't have a stale value"
+  );
+  let passwordField = document.getElementById("password-notification-password");
+  is(
+    passwordField.value,
+    "",
+    "Check the password field doesn't have a stale value"
+  );
+
+  // Cleanup
+  Services.logins.removeLogin(login);
+});
