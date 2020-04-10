a33697f793fcd87d0b3a96c72fbccc857995b93d
Kate Hudson <k88hudson@gmail.com>
2020-04-10 17:44:53 +0000

    Bug 1620021 - Add ExperimentsManager and ExperimentAPI r=mythmon
    Differential Revision: https://phabricator.services.mozilla.com/D65365

---
+++ toolkit/components/messaging-system/test/unit/head.js
@@ -0,0 +1,5 @@
+"use strict";
+// Globals
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
