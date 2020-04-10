ffce946e8f872e1469cfb5cc4b16cd8510907664
Zibi Braniecki <zbraniecki@mozilla.com>
2020-04-10 23:35:03 +0000

    Bug 1621674 - Unify Localization.jsm, mozILocalization and Localization WebIDL. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D66531

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -3865,7 +3865,7 @@ bool Document::GetAllowPlugins() {
   return true;
 }
 
-void Document::InitializeLocalization(nsTArray<nsString>& aResourceIds) {
+void Document::InitializeLocalization(Sequence<nsString>& aResourceIds) {
   MOZ_ASSERT(!mDocumentL10n, "mDocumentL10n should not be initialized yet");
 
   RefPtr<DocumentL10n> l10n = new DocumentL10n(this);
@@ -3899,14 +3899,18 @@ void Document::LocalizationLinkAdded(Element* aLinkElement) {
   // If the link is added after the DocumentL10n instance
   // has been initialized, just pass the resource ID to it.
   if (mDocumentL10n) {
-    AutoTArray<nsString, 1> resourceIds;
-    resourceIds.AppendElement(href);
+    Sequence<nsString> resourceIds;
+    if (NS_WARN_IF(!resourceIds.AppendElement(href, fallible))) {
+      return;
+    }
     mDocumentL10n->AddResourceIds(resourceIds, false);
   } else if (mReadyState >= READYSTATE_INTERACTIVE) {
     // Otherwise, if the document has already been parsed
     // we need to lazily initialize the localization.
-    AutoTArray<nsString, 1> resourceIds;
-    resourceIds.AppendElement(href);
+    Sequence<nsString> resourceIds;
+    if (NS_WARN_IF(!resourceIds.AppendElement(href, fallible))) {
+      return;
+    }
     InitializeLocalization(resourceIds);
     mDocumentL10n->TriggerInitialDocumentTranslation();
   } else {
@@ -3914,7 +3918,9 @@ void Document::LocalizationLinkAdded(Element* aLinkElement) {
     // In that case, add it to the pending list. This list
     // will be resolved once the end of l10n resource
     // container is reached.
-    mL10nResources.AppendElement(href);
+    if (NS_WARN_IF(!mL10nResources.AppendElement(href, fallible))) {
+      return;
+    }
 
     if (!mPendingInitialTranslation) {
       // Our initial translation is going to block layout start.  Make sure we
