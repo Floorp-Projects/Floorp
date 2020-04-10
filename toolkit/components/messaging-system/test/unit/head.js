ce2cd8194f11efc71c4d9df84390b657d78599f5
Noemi Erli <nerli@mozilla.com>
2020-04-11 01:38:33 +0300

    Backed out 2 changesets (bug 1620021) for failures in test_ExperimentManager_lifecycle.js
    Backed out changeset 600778a596aa (bug 1620021)
    Backed out changeset 50bc2329abae (bug 1620021)

--- toolkit/components/messaging-system/test/unit/head.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Globals
-
-const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
