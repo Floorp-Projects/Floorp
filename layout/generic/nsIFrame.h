9fa21b8d44ef9482e4bf92ab1263a7554d33d28d
Ting-Yu Lin <tlin@mozilla.com>
2020-04-10 22:51:13 +0000

    Bug 1628804 - Ensure nsIFrame::ContentSize() returning non-negative sizes. r=dholbert,AlaskanEmily
    Differential Revision: https://phabricator.services.mozilla.com/D70558

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -1023,7 +1023,11 @@ class nsIFrame : public nsQueryFrame {
   mozilla::LogicalSize ContentSize(mozilla::WritingMode aWritingMode) const {
     const auto bp = GetLogicalUsedBorderAndPadding(aWritingMode)
                         .ApplySkipSides(GetLogicalSkipSides());
-    return GetLogicalSize(aWritingMode) - bp.Size(aWritingMode);
+    const auto size = GetLogicalSize(aWritingMode);
+    return mozilla::LogicalSize(
+        aWritingMode,
+        std::max(0, size.ISize(aWritingMode) - bp.IStartEnd(aWritingMode)),
+        std::max(0, size.BSize(aWritingMode) - bp.BStartEnd(aWritingMode)));
   }
 
   /**
