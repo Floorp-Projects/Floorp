ef8ad7844000687b9c8a0c1e299be62e640006b0
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-04-10 13:51:51 +0000

    Bug 1628346 - Add a dedicated context selector pref for content toolbox. r=ochameau.
    Differential Revision: https://phabricator.services.mozilla.com/D70356

--- devtools/client/webconsole/test/node/mocha-test-setup.js
+++ devtools/client/webconsole/test/node/mocha-test-setup.js
@@ -37,6 +37,7 @@ pref("devtools.browserconsole.contentMessages", true);
 pref("devtools.webconsole.input.editorWidth", 800);
 pref("devtools.webconsole.input.editorOnboarding", true);
 pref("devtools.webconsole.input.context", false);
+pref("devtools.contenttoolbox.webconsole.input.context", false);
 
 global.loader = {
   lazyServiceGetter: () => {},
