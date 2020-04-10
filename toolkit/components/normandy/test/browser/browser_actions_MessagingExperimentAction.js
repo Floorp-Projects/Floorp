c6a66a1b7b4306c97e8f5117676a74feb7fdb7c6
Kate Hudson <k88hudson@gmail.com>
2020-04-10 13:25:23 +0000

    Bug 1620021 - Integrate ExperimentManager with Normandy r=mythmon
    Differential Revision: https://phabricator.services.mozilla.com/D65366

---
+++ toolkit/components/normandy/test/browser/browser_actions_MessagingExperimentAction.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const { BaseAction } = ChromeUtils.import(
+  "resource://normandy/actions/BaseAction.jsm"
+);
+const { Uptake } = ChromeUtils.import("resource://normandy/lib/Uptake.jsm");
+const { MessagingExperimentAction } = ChromeUtils.import(
+  "resource://normandy/actions/MessagingExperimentAction.jsm"
+);
+
+const { _ExperimentManager, ExperimentManager } = ChromeUtils.import(
+  "resource://messaging-system/experiments/ExperimentManager.jsm"
+);
+
+decorate_task(
+  withStudiesEnabled,
+  withStub(Uptake, "reportRecipe"),
+  async function arguments_are_validated(reportRecipe) {
+    const action = new MessagingExperimentAction();
+
+    is(
+      action.manager,
+      ExperimentManager,
+      "should set .manager to ExperimentManager singleton"
+    );
+    // Override this for the purposes of the test
+    action.manager = new _ExperimentManager();
+    const onRecipeStub = sinon.spy(action.manager, "onRecipe");
+
+    const recipe = {
+      id: 1,
+      arguments: {
+        slug: "foo",
+        branches: [
+          {
+            slug: "control",
+            ratio: 1,
+            groups: ["green"],
+            value: { title: "hello" },
+          },
+          {
+            slug: "variant",
+            ratio: 1,
+            groups: ["green"],
+            value: { title: "world" },
+          },
+        ],
+      },
+    };
+
+    ok(action.validateArguments(recipe.arguments), "should validate arguments");
+
+    await action.processRecipe(recipe, BaseAction.suitability.FILTER_MATCH);
+    await action.finalize();
+
+    Assert.deepEqual(reportRecipe.args, [[recipe, Uptake.RECIPE_SUCCESS]]);
+    Assert.deepEqual(
+      onRecipeStub.args,
+      [[recipe.arguments]],
+      "should call onRecipe with recipe args"
+    );
+  }
+);
