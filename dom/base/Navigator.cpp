4113cf17777f06cb5eba574eaa0d655459281422
Andrea Marchesini <amarchesini@mozilla.com>
2020-04-10 08:36:46 +0000

    Bug 1363541 - Modernize the PermissionManager - part 2 - PermissionDelegateHandler to mozilla::PermissionDelegateHandler, r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D69964

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -73,7 +73,7 @@
 #include "WidgetUtils.h"
 #include "nsIScriptError.h"
 #include "ReferrerInfo.h"
-#include "PermissionDelegateHandler.h"
+#include "mozilla/PermissionDelegateHandler.h"
 
 #include "nsIExternalProtocolHandler.h"
 #include "BrowserChild.h"
