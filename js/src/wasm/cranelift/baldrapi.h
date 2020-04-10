413305dda43daba45c8f1f10d1bb85b12dc55b89
Benjamin Bouvier <benj@benj.me>
2020-04-10 09:53:15 +0000

    Bug 1626967: Use snake_case in shared Cranelift data structures; r=rhunt
    This favors Rust, but there's no way to generate different names in the
    bindings, as far as I know.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69395
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/wasm/cranelift/baldrapi.h
+++ js/src/wasm/cranelift/baldrapi.h
@@ -57,26 +57,26 @@ struct ModuleEnvironment;
 // to, but which can't be automatically provided to Rust.
 
 struct CraneliftStaticEnvironment {
-  bool hasSse2;
-  bool hasSse3;
-  bool hasSse41;
-  bool hasSse42;
-  bool hasPopcnt;
-  bool hasAvx;
-  bool hasBmi1;
-  bool hasBmi2;
-  bool hasLzcnt;
-  bool platformIsWindows;
-  bool refTypesEnabled;
-  size_t staticMemoryBound;
-  size_t memoryGuardSize;
-  size_t memoryBaseTlsOffset;
-  size_t instanceTlsOffset;
-  size_t interruptTlsOffset;
-  size_t cxTlsOffset;
-  size_t realmCxOffset;
-  size_t realmTlsOffset;
-  size_t realmFuncImportTlsOffset;
+  bool has_sse2;
+  bool has_sse3;
+  bool has_sse41;
+  bool has_sse42;
+  bool has_popcnt;
+  bool has_avx;
+  bool has_bmi1;
+  bool has_bmi2;
+  bool has_lzcnt;
+  bool platform_is_windows;
+  bool ref_types_enabled;
+  size_t static_memory_bound;
+  size_t memory_guard_size;
+  size_t memory_base_tls_offset;
+  size_t instance_tls_offset;
+  size_t interrupt_tls_offset;
+  size_t cx_tls_offset;
+  size_t realm_cx_offset;
+  size_t realm_tls_offset;
+  size_t realm_func_import_tls_offset;
 
   // Not bindgen'd because it's inlined.
   inline CraneliftStaticEnvironment();
@@ -103,7 +103,7 @@ struct BD_Stackmaps;
 
 struct CraneliftFuncCompileInput {
   const uint8_t* bytecode;
-  size_t bytecodeSize;
+  size_t bytecode_size;
   uint32_t index;
   uint32_t offset_in_module;
 
@@ -123,8 +123,8 @@ struct CraneliftFuncCompileInput {
 
 struct CraneliftMetadataEntry {
   enum Which { DirectCall, IndirectCall, Trap, SymbolicAccess } which;
-  uint32_t codeOffset;
-  uint32_t moduleBytecodeOffset;
+  uint32_t code_offset;
+  uint32_t module_bytecode_offset;
   size_t extra;
 };
 
@@ -133,27 +133,27 @@ struct CraneliftMetadataEntry {
 // prologue/epilogue etc.
 
 struct CraneliftCompiledFunc {
-  size_t numMetadata;
+  size_t num_metadata;
   const CraneliftMetadataEntry* metadatas;
 
-  size_t framePushed;
-  bool containsCalls;
+  size_t frame_pushed;
+  bool contains_calls;
 
   // The compiled code comprises machine code, relocatable jump tables, and
   // copyable read-only data, concatenated without padding.  The "...Size"
   // members give the sizes of the individual sections.  The code starts at
   // offsets 0; the other offsets can be derived from the sizes.
   const uint8_t* code;
-  size_t codeSize;
-  size_t jumptablesSize;
-  size_t rodataSize;
-  size_t totalSize;
+  size_t code_size;
+  size_t jumptables_size;
+  size_t rodata_size;
+  size_t total_size;
 
   // Relocation information for instructions that reference into the jump tables
   // and read-only data segments.  The relocation information is
   // machine-specific.
-  size_t numRodataRelocs;
-  const uint32_t* rodataRelocs;
+  size_t num_rodata_relocs;
+  const uint32_t* rodata_relocs;
 };
 
 // Possible constant values for initializing globals.
