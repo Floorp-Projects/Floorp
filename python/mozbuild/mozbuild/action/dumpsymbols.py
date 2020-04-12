877064f1df62e6658638b366627e0f544ed52dd9
Mike Hommey <mh+mozilla@glandium.org>
2020-04-12 00:03:54 +0000

    Bug 1628927 - Convert symbolstore.py to python 3. r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D70467

--- python/mozbuild/mozbuild/action/dumpsymbols.py
+++ python/mozbuild/mozbuild/action/dumpsymbols.py
@@ -69,7 +69,7 @@ def dump_symbols(target, tracking_file, count_ctors=False):
     if objcopy:
         os.environ['OBJCOPY'] = objcopy
 
-    args = ([buildconfig.substs['PYTHON'],
+    args = ([sys.executable,
              os.path.join(buildconfig.topsrcdir, 'toolkit',
                           'crashreporter', 'tools', 'symbolstore.py')] +
             sym_store_args +
