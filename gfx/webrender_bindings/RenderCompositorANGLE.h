203b09d48bf8bf87fd70d67215267efbc6a30038
sotaro <sotaro.ikeda.g@gmail.com>
2020-04-13 23:55:08 +0000

    Bug 1626822 - Add a way to enable WebRender without GPU process on Windows r=jrmuizel
    WebRender could be used when WebRender does not use ANGLE. And there is a case that we want to use WebRender with ANGLE for testing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69771

--- gfx/webrender_bindings/RenderCompositorANGLE.h
+++ gfx/webrender_bindings/RenderCompositorANGLE.h
@@ -112,6 +112,7 @@ class RenderCompositorANGLE : public RenderCompositor {
   bool SutdownEGLLibraryIfNecessary();
   RefPtr<ID3D11Query> GetD3D11Query();
   void ReleaseNativeCompositorResources();
+  HWND GetCompositorHwnd();
 
   EGLConfig mEGLConfig;
   EGLSurface mEGLSurface;
