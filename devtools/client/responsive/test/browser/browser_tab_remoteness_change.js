f20a46e88435feecf3b9c84fd17e1550a46c0d58
Micah Tigley <mtigley@mozilla.com>
2020-04-13 16:45:41 +0000

    Bug 1625501 - Enable target-switching for RDM r=daisuke,jdescottes
    Target-switching in RDM will always occur on tab remoteness change with new-RDM enabled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69389

--- devtools/client/responsive/test/browser/browser_tab_remoteness_change.js
+++ devtools/client/responsive/test/browser/browser_tab_remoteness_change.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-// Verify RDM closes synchronously when tabs change remoteness.
+// Verify Fission-enabled RDM remains open when tab changes remoteness.
 
 const { PromiseTestUtils } = ChromeUtils.import(
   "resource://testing-common/PromiseTestUtils.jsm"
@@ -59,18 +59,47 @@ addRDMTask(
         state.viewports.length == 1 &&
         state.devices.listState == Types.loadableState.LOADED
     );
+
+    // Load URL that requires the main process, forcing a remoteness flip
+    await navigateToNewDomain("about:robots", ui);
+
+    // Bug 1625501: RDM will remain open when the embedded browser UI is enabled.
+    is(ui.destroyed, false, "RDM is still open.");
+
+    info("Close RDM");
+    await closeRDM(tab);
+
+    await removeTab(tab);
+  },
+  { usingBrowserUI: true, onlyPrefAndTask: true }
+);
+
+// Remove this test when the new RDM is enabled in Firefox stable release. In the
+// meantime, we should still test that old-RDM will close if needed when there is
+// remoteness change in the tab.
+addRDMTask(
+  null,
+  async function() {
+    const tab = await addTab(TEST_URL);
+
+    const { ui } = await openRDM(tab);
+    const { store } = ui.toolWindow;
+    await waitUntilState(
+      store,
+      state =>
+        state.viewports.length == 1 &&
+        state.devices.listState == Types.loadableState.LOADED
+    );
     const clientClosed = waitForClientClose(ui);
 
     // Load URL that requires the main process, forcing a remoteness flip
     await load(tab.linkedBrowser, "about:robots");
 
-    // This flag is set at the end of `ResponsiveUI.destroy`.  If it is true without
-    // waiting for `closeRDM` itself and only removing the tab, then we must have closed
-    // synchronously in response to tab closing.
+    // RDM will close if using the old version.
     is(ui.destroyed, true, "RDM closed synchronously");
 
     await clientClosed;
     await removeTab(tab);
   },
-  { usingBrowserUI: true, onlyPrefAndTask: true }
+  { usingBrowserUI: false, onlyPrefAndTask: true }
 );
