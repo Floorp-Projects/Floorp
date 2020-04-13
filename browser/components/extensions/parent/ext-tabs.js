adf8f819678d999ccb255eca50badd0f8d0d3a9e
Brindusan Cristian <cbrindusan@mozilla.com>
2020-04-13 20:36:19 +0300

    Backed out changeset 4fd31338db03 (bug 1603796) for mochitest failures at test_ext_all_apis.html. CLOSED TREE

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -1634,16 +1634,6 @@ this.tabs = class extends ExtensionAPI {
           });
           return windowManager.convert(window, { populate });
         },
-
-        goForward(tabId) {
-          let nativeTab = getTabOrActive(tabId);
-          nativeTab.linkedBrowser.goForward();
-        },
-
-        goBack(tabId) {
-          let nativeTab = getTabOrActive(tabId);
-          nativeTab.linkedBrowser.goBack();
-        },
       },
     };
     return self;
