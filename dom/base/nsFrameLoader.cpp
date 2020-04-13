c2aef158031edf09ddf8332b93a79005b1eb77ab
Kris Maglione <maglione.k@gmail.com>
2020-04-13 22:41:37 +0000

    Bug 1594529: Infer nsDocShell::IsFrame from BrowsingContext. r=nika
    This fixes the content policy type for document loads in these frames, where
    the explicit mIsFrame flag was not set, due to DocShell creation taking a
    different code path in remote frames than in in-process frames.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52093
    
    --HG--
    extra : moz-landing-system : lando

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -2147,8 +2147,6 @@ nsresult nsFrameLoader::MaybeCreateDocShell() {
 
   if (OwnerIsMozBrowserFrame()) {
     docShell->SetFrameType(nsIDocShell::FRAME_TYPE_BROWSER);
-  } else if (mPendingBrowsingContext->GetParent()) {
-    docShell->SetIsFrame();
   }
 
   // Apply sandbox flags even if our owner is not an iframe, as this copies
