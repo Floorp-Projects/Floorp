c9b065e2ffc933fef8e9a77ba6e3d3971f7ff877
Jeff Walden <jwalden@mit.edu>
2020-04-14 16:57:12 +0000

    Bug 949220 - Make |NewDenseFullyAllocatedArrayWithTemplate|'s template object argument have type |ArrayObject*|.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D70490
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/builtin/String.cpp
+++ js/src/builtin/String.cpp
@@ -4455,7 +4455,7 @@ static bool BuildFlatMatchArray(JSContext* cx, HandleString str,
 
   // Get the templateObject that defines the shape and type of the output
   // object.
-  JSObject* templateObject =
+  ArrayObject* templateObject =
       cx->realm()->regExps.getOrCreateMatchResultTemplateObject(cx);
   if (!templateObject) {
     return false;
