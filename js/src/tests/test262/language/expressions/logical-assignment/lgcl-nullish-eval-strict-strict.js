60d121260bc6ed6975406f8ba8f5f19ce032e5e7
André Bargull <andre.bargull@gmail.com>
2020-04-14 14:39:04 +0000

    Bug 1629792 - Part 2: Update test262 April 14 2020. r=yulia
    Depends on D70811
    
    Differential Revision: https://phabricator.services.mozilla.com/D70812

---
+++ js/src/tests/test262/language/expressions/logical-assignment/lgcl-nullish-eval-strict-strict.js
@@ -0,0 +1,19 @@
+// |reftest| skip error:SyntaxError -- logical-assignment-operators is not supported
+'use strict';
+// Copyright (C) 2020 Rick Waldron. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: sec-assignment-operators
+description: >
+    Strict Mode - SyntaxError is thrown if the identifier eval appear
+    as the LeftHandSideExpression of a Logical Assignment operator(??=)
+flags: [onlyStrict]
+negative:
+  phase: parse
+  type: SyntaxError
+features: [logical-assignment-operators]
+---*/
+$DONOTEVALUATE();
+
+eval ??= 20;
