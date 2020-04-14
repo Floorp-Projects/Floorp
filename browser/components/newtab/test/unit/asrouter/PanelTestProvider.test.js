f8539ee1e46793b6c09147dc5d5b59d9e7a98db9
Andrei Oprea <andrei.br92@gmail.com>
2020-04-14 16:51:35 +0000

    Bug 1628366 - Land strings for What's New Panel content in Firefox 76 r=fluent-reviewers,flod,nanj
    Differential Revision: https://phabricator.services.mozilla.com/D70827

--- browser/components/newtab/test/unit/asrouter/PanelTestProvider.test.js
+++ browser/components/newtab/test/unit/asrouter/PanelTestProvider.test.js
@@ -8,7 +8,7 @@ describe("PanelTestProvider", () => {
   it("should have a message", () => {
     // Careful: when changing this number make sure that new messages also go
     // through schema verifications.
-    assert.lengthOf(messages, 17);
+    assert.lengthOf(messages, 18);
   });
   it("should be a valid message", () => {
     const fxaMessages = messages.filter(
