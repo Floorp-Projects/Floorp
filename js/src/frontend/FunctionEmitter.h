fde814f2bfb036aeff0da38bccdcc0c46ea613c2
Ted Campbell <tcampbell@mozilla.com>
2020-04-14 13:36:56 +0000

    Bug 1628828 - Use fieldInitializers from FunctionBox during BCE. r=mgaudet
    This extends the lifetime from BCE to SharedContext which fits in a lot
    better with our deferred allocation story. Due to previous patch, we init the
    FunctionBox copy of FieldInitializers before the BCE instance runs which
    simplifies things here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70724

--- js/src/frontend/FunctionEmitter.h
+++ js/src/frontend/FunctionEmitter.h
@@ -255,7 +255,8 @@ class MOZ_STACK_CLASS FunctionScriptEmitter {
   //          compilation after `initScript` call.
   //          See the comment inside JSScript::fullyInitFromEmitter for
   //          more details.
-  MOZ_MUST_USE bool initScript(const FieldInitializers& fieldInitializers);
+  MOZ_MUST_USE bool initScript(
+      const mozilla::Maybe<FieldInitializers>& fieldInitializers);
 
  private:
   MOZ_MUST_USE bool emitExtraBodyVarScope();
