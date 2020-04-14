96b176d4f69be7d99079089a62aeb434653a7725
Jon Coppeard <jcoppeard@mozilla.com>
2020-04-14 21:02:43 +0000

    Bug 1455563 - Remove some dead code from the GC r=sfink
    Differential Revision: https://phabricator.services.mozilla.com/D70920
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/gc/StoreBuffer-inl.h
+++ js/src/gc/StoreBuffer-inl.h
@@ -59,6 +59,9 @@ inline void ArenaCellSet::check() const {
 inline void StoreBuffer::WholeCellBuffer::put(const Cell* cell) {
   MOZ_ASSERT(cell->isTenured());
 
+  // BigInts don't have any children, so shouldn't show up here.
+  MOZ_ASSERT(cell->getTraceKind() != JS::TraceKind::BigInt);
+
   Arena* arena = cell->asTenured().arena();
   ArenaCellSet* cells = arena->bufferedCells();
   if (cells->isEmpty()) {
