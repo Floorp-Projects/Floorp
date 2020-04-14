3238c19a2346697530ae2e1892f676f4ed3693e3
Jeff Walden <jwalden@mit.edu>
2020-04-14 08:30:41 +0000

    Bug 949220 - Split |NewTenuredDenseEmptyArray| out of |NewDenseEmptyArray|, and make the latter function invoke TenuredObject behavior.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D70491

--- js/src/builtin/Array.h
+++ js/src/builtin/Array.h
@@ -47,10 +47,15 @@ MOZ_ALWAYS_INLINE bool IdIsIndex(jsid id, uint32_t* indexp) {
 
 // The methods below only create dense boxed arrays.
 
-// Create a dense array with no capacity allocated, length set to 0.
+// Create a dense array with no capacity allocated, length set to 0, in the
+// normal (i.e. non-tenured) heap.
 extern ArrayObject* JS_FASTCALL
-NewDenseEmptyArray(JSContext* cx, HandleObject proto = nullptr,
-                   NewObjectKind newKind = GenericObject);
+NewDenseEmptyArray(JSContext* cx, HandleObject proto = nullptr);
+
+// Create a dense array with no capacity allocated, length set to 0, in the
+// tenured heap.
+extern ArrayObject* JS_FASTCALL
+NewTenuredDenseEmptyArray(JSContext* cx, HandleObject proto = nullptr);
 
 // Create a dense array with a set length, but without allocating space for the
 // contents. This is useful, e.g., when accepting length from the user.
