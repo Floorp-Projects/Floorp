5ecefaf95f4215c724bd76b91f8eafd9f4ad2dc7
Jeff Walden <jwalden@mit.edu>
2020-04-14 08:38:22 +0000

    Bug 949220 - Remove the |NewObjectKind| argument from |DeepCloneObjectLiteral|.  r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D70504
    
    --HG--
    extra : moz-landing-system : lando

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -4794,7 +4794,7 @@ static JSObject* CloneScriptObject(JSContext* cx, PrivateScriptData* srcData,
                                          sourceObject);
   }
 
-  return DeepCloneObjectLiteral(cx, obj, TenuredObject);
+  return DeepCloneObjectLiteral(cx, obj);
 }
 
 /* static */
