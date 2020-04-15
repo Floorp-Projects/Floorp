56c41f18f41aa954cbf9747c4bddb4fef5be7328
Eitan Isaacson <eitan@monotonous.org>
2020-04-15 22:33:55 +0000

    Bug 1629162 - Scrollability changed should fire accessible FOCUSABLE changes. r=morgan,Jamie
    When an element with overflow:auto becomes scrollable, either by a style change or growing children, it should become focusable and fire a state change. Same in the inverse.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70561

--- accessible/generic/RootAccessible.cpp
+++ accessible/generic/RootAccessible.cpp
@@ -144,7 +144,7 @@ const char* const kEventTypes[] = {
     // HTMLInputElement.cpp & radio.js)
     "RadioStateChange", "popupshown", "popuphiding", "DOMMenuInactive",
     "DOMMenuItemActive", "DOMMenuItemInactive", "DOMMenuBarActive",
-    "DOMMenuBarInactive", "scroll"};
+    "DOMMenuBarInactive", "scroll", "overflow", "underflow"};
 
 nsresult RootAccessible::AddEventListeners() {
   // EventTarget interface allows to register event listeners to
@@ -445,6 +445,19 @@ void RootAccessible::ProcessDOMEvent(Event* aDOMEvent, nsINode* aTarget) {
                             accessible);
   }
 #endif
+  else if (eventType.EqualsLiteral("overflow") ||
+           eventType.EqualsLiteral("underflow")) {
+    // An overflow or underflow may mean the element's focusable state has
+    // changed.
+    if (accessible->IsContent() && !accessible->GetContent()->IsFocusable()) {
+      // If the element is not focusable for other DOM reasons (input, tabindex,
+      // etc.), dispatch a focusable state change event.
+      RefPtr<AccEvent> event = new AccStateChangeEvent(
+          accessible, states::FOCUSABLE,
+          (accessible->NativeInteractiveState() & states::FOCUSABLE) != 0);
+      targetDocument->FireDelayedEvent(event);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
