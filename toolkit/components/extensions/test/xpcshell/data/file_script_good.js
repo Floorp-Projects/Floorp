c26509d4d6fd18ae11a5dd69772cd11ea384f85e
Daniel Varga <dvarga@mozilla.com>
2020-04-15 03:26:57 +0300

    Backed out 2 changesets (bug 1462989) for causing xpcshell failures at components/extensions/test/xpcshell/test_ext_webRequest_cached.js
    CLOSED TREE
    
    Backed out changeset 1487aaf8ccd2 (bug 1462989)
    Backed out changeset 60f81cfd0d4f (bug 1462989)

--- toolkit/components/extensions/test/xpcshell/data/file_script_good.js
+++ toolkit/components/extensions/test/xpcshell/data/file_script_good.js
@@ -1,12 +1,3 @@
 "use strict";
 
 window.success = window.success ? window.success + 1 : 1;
-window.addEventListener(
-  "load",
-  () => {
-    let el = document.createElement("div");
-    el.setAttribute("id", "good");
-    document.body.appendChild(el);
-  },
-  { once: true }
-);
