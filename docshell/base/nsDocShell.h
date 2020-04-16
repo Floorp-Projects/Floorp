590e7c6a528be48fb7edcadb40d5c090c5dd0a63
Kris Maglione <maglione.k@gmail.com>
2020-04-16 17:25:06 +0000

    Bug 1614462: Part 3b - Remove moribund DocShell FrameType/IsMozBrowser/IsInMozBrowser fields. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D70749

--- docshell/base/nsDocShell.h
+++ docshell/base/nsDocShell.h
@@ -569,18 +569,6 @@ class nsDocShell final : public nsDocLoader,
     return uint32_t(aTimeUsec / PR_USEC_PER_SEC);
   }
 
-  static const nsCString FrameTypeToString(uint32_t aFrameType) {
-    switch (aFrameType) {
-      case FRAME_TYPE_BROWSER:
-        return NS_LITERAL_CSTRING("browser");
-      case FRAME_TYPE_REGULAR:
-        return NS_LITERAL_CSTRING("regular");
-      default:
-        NS_ERROR("Unknown frame type");
-        return EmptyCString();
-    }
-  }
-
   virtual ~nsDocShell();
 
   //
@@ -1009,7 +997,6 @@ class nsDocShell final : public nsDocLoader,
   bool IsPrintingOrPP(bool aDisplayErrorDialog = true);
   bool IsNavigationAllowed(bool aDisplayPrintErrorDialog = true,
                            bool aCheckIfUnloadFired = true);
-  uint32_t GetInheritedFrameType();
   nsIScrollableFrame* GetRootScrollFrame();
   nsIChannel* GetCurrentDocChannel();
   nsresult EnsureScriptEnvironment();
@@ -1228,9 +1215,6 @@ class nsDocShell final : public nsDocLoader,
   uint32_t mLoadType;
   uint32_t mDefaultLoadFlags;
   uint32_t mFailedLoadType;
-
-  // Are we a regular frame, a browser frame, or an app frame?
-  FrameType mFrameType;
 
   // This represents the CSS display-mode we are currently using. This is mostly
   // used for media queries.
