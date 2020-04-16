6b31e7a0b1f43aa567d926b87fa8f19334383dd8
Kartikaya Gupta <kgupta@mozilla.com>
2020-04-16 03:21:24 +0000

    Bug 1627326 - Add some plumbing to provide the "stuck sides" flags. r=botond,hiro
    This is just plumbing, no functional change. The current usage of eBottom is
    parameterized and the eBottom hard-coded value is pushed out to the callers.
    
    Depends on D70912
    
    Differential Revision: https://phabricator.services.mozilla.com/D71083

--- gfx/layers/composite/AsyncCompositionManager.h
+++ gfx/layers/composite/AsyncCompositionManager.h
@@ -173,7 +173,7 @@ class AsyncCompositionManager final {
    */
   void AlignFixedAndStickyLayers(
       Layer* aTransformedSubtreeRoot, Layer* aStartTraversalAt,
-      ScrollableLayerGuid::ViewID aTransformScrollId,
+      SideBits aStuckSides, ScrollableLayerGuid::ViewID aTransformScrollId,
       const LayerToParentLayerMatrix4x4& aPreviousTransformForRoot,
       const LayerToParentLayerMatrix4x4& aCurrentTransformForRoot,
       const ScreenMargin& aCompositorFixedLayerMargins,
@@ -187,7 +187,7 @@ class AsyncCompositionManager final {
    */
   void AdjustFixedOrStickyLayer(
       Layer* aTransformedSubtreeRoot, Layer* aFixedOrSticky,
-      ScrollableLayerGuid::ViewID aTransformScrollId,
+      SideBits aStuckSides, ScrollableLayerGuid::ViewID aTransformScrollId,
       const LayerToParentLayerMatrix4x4& aPreviousTransformForRoot,
       const LayerToParentLayerMatrix4x4& aCurrentTransformForRoot,
       const ScreenMargin& aCompositorFixedLayerMargins,
