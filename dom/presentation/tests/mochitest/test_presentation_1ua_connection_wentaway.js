fcba52596048f93fcbc779b31a8f2dd98aa12aea
Kris Maglione <maglione.k@gmail.com>
2020-04-16 22:20:05 +0000

    Bug 1614462: Part 2c - Remove content mozbrowser tests. r=mccr8
    <iframe mozbrowser> is no longer supported in content processes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70744

--- dom/presentation/tests/mochitest/test_presentation_1ua_connection_wentaway.js
+++ dom/presentation/tests/mochitest/test_presentation_1ua_connection_wentaway.js
@@ -231,7 +231,6 @@ SpecialPowers.pushPermissions(
           ["dom.presentation.controller.enabled", true],
           ["dom.presentation.receiver.enabled", true],
           ["dom.presentation.test.enabled", true],
-          ["dom.mozBrowserFramesEnabled", true],
           ["dom.ipc.tabs.disabled", false],
           ["network.disable.ipc.security", true],
           ["dom.presentation.test.stage", 0],
