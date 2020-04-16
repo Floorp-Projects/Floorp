05354fe81331cb17ad089a39b684912be5619ff4
Nico Grunbaum <na-g@nostrum.com>
2020-04-16 09:17:46 +0000

    Bug 1628630 - update WEBRTC-SDP to 0.3.5;r=dminor
    Differential Revision: https://phabricator.services.mozilla.com/D70339

--- third_party/rust/webrtc-sdp/src/media_type.rs
+++ third_party/rust/webrtc-sdp/src/media_type.rs
@@ -542,8 +542,7 @@ mod tests {
                     maxplaybackrate: 48000,
                     encodings: Vec::new(),
                     dtmf_tones: "".to_string(),
-                    apt: 0,
-                    rtx_time: 0,
+                    rtx: None,
                     unknown_tokens: Vec::new()
                 }
             },))
