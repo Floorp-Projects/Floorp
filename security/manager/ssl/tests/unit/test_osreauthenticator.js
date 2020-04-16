1f86101b55b8210a9fd859570e6049af0c67abc3
Jared Wein <jwein@mozilla.com>
2020-04-16 21:33:24 +0000

    Bug 1623745 - Add a value to the pwmgr.reauthenticated telemetry event to specify if the user was able to authenticate without a password. r=MattN,spohl a=RyanVm
    Differential Revision: https://phabricator.services.mozilla.com/D70136

--- security/manager/ssl/tests/unit/test_osreauthenticator.js
+++ security/manager/ssl/tests/unit/test_osreauthenticator.js
@@ -4,20 +4,24 @@
 "use strict";
 
 // Tests nsIOSReauthenticator.asyncReauthenticateUser().
-// Currently this always resolves to false on all platforms. As this gets implemented on various
-// platforms, running this rest will result in a prompt from the OS. Consequently, we won't be able
+// As this gets implemented on various platforms, running this test
+// will result in a prompt from the OS. Consequently, we won't be able
 // to run this in automation, but it will help in testing locally.
 add_task(async function test_asyncReauthenticateUser() {
   const reauthenticator = Cc[
     "@mozilla.org/security/osreauthenticator;1"
   ].getService(Ci.nsIOSReauthenticator);
   ok(reauthenticator, "nsIOSReauthenticator should be available");
+  const EXPECTED = true; // Change this variable to suit your needs while testing.
   ok(
-    !(await reauthenticator.asyncReauthenticateUser(
-      "this is the prompt string",
-      "this is the caption string",
-      null
-    )),
-    "nsIOSReauthenticator.asyncReauthenticateUser always resolves to false for now"
+    (
+      await reauthenticator.asyncReauthenticateUser(
+        "this is the prompt string",
+        "this is the caption string",
+        null
+      )
+    )[0] == EXPECTED,
+    "nsIOSReauthenticator.asyncReauthenticateUser should return a boolean array with the first item being the authentication result of: " +
+      EXPECTED
   );
 });
