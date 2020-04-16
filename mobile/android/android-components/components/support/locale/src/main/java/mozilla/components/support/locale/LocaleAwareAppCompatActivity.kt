bab38ad63ff29c89d099ec6db934b71ccea6856c
Jeff Boek <jeff@jeffboek.com>
2020-04-16 11:27:51 -0700

    [components] For https://github.com/mozilla-mobile/fenix/issues/9413 - Updates changelog.md

--- mobile/android/android-components/components/support/locale/src/main/java/mozilla/components/support/locale/LocaleAwareAppCompatActivity.kt
+++ mobile/android/android-components/components/support/locale/src/main/java/mozilla/components/support/locale/LocaleAwareAppCompatActivity.kt
@@ -7,6 +7,7 @@ package mozilla.components.support.locale
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -20,10 +21,16 @@ open class LocaleAwareAppCompatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setLayoutDirectionIfNeeded()
+    }
 
-        // Android 8 has a bug which doesn't change the layoutDirection on activity recreation.
-        // https://github.com/mozilla-mobile/fenix/issues/9413
-        // https://stackoverflow.com/questions/46296202/rtl-layout-bug-in-android-oreo#comment98890942_46298101
+    /**
+     * Compensates for a bug in Android 8 which doesn't change the layoutDirection on activity recreation
+     * https://github.com/mozilla-mobile/fenix/issues/9413
+     * https://stackoverflow.com/questions/46296202/rtl-layout-bug-in-android-oreo#comment98890942_46298101
+     */
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    fun setLayoutDirectionIfNeeded() {
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O) {
             window.decorView.layoutDirection = resources.configuration.layoutDirection
         }
