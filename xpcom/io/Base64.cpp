399dc3d35f0d7217852f0ba1ef66def3634b1c34
Konstantin Morozov <scfocus@yandex.ru>
2020-04-16 14:14:08 +0000

    Bug 1629432 - converting integer literal to bool. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D71181

--- xpcom/io/Base64.cpp
+++ xpcom/io/Base64.cpp
@@ -184,7 +184,7 @@ nsresult EncodeInputStream(nsIInputStream* aInputStream, T& aDest,
   state.c[2] = '\0';
   state.buffer = aOffset + aDest.BeginWriting();
 
-  while (1) {
+  while (true) {
     uint32_t read = 0;
 
     rv = aInputStream->ReadSegments(&EncodeInputStream_Encoder<T>,
