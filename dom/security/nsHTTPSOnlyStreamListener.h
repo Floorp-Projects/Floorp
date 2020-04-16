e7cf745cb8fe37d1f62acebc3982761e7fd59ac7
Oana Pop Rus <opoprus@mozilla.com>
2020-04-16 13:28:49 +0300

    Backed out 1 changesets (bug 1627206) for build bustages in nsHTTPSOnlyStreamListener.cpp on a CLOSED TREE
    Backed out changeset 6f302c058d54 (bug 1627206)

--- dom/security/nsHTTPSOnlyStreamListener.h
+++ dom/security/nsHTTPSOnlyStreamListener.h
@@ -7,9 +7,8 @@
 #ifndef nsHTTPSOnlyStreamListener_h___
 #define nsHTTPSOnlyStreamListener_h___
 
-#include "mozilla/TimeStamp.h"
-#include "nsCOMPtr.h"
 #include "nsIStreamListener.h"
+#include "nsCOMPtr.h"
 
 /**
  * This event listener gets registered for requests that have been upgraded
@@ -27,21 +26,7 @@ class nsHTTPSOnlyStreamListener : public nsIStreamListener {
  private:
   virtual ~nsHTTPSOnlyStreamListener() = default;
 
-  /**
-   * Records telemetry about the upgraded request.
-   * @param aStatus Request object
-   */
-  void RecordUpgradeTelemetry(nsIRequest* request, nsresult aStatus);
-
-  /**
-   * Logs information to the console if the request failed.
-   * @param request Request object
-   * @param aStatus Status of request
-   */
-  void LogUpgradeFailure(nsIRequest* request, nsresult aStatus);
-
   nsCOMPtr<nsIStreamListener> mListener;
-  mozilla::TimeStamp mCreationStart;
 };
 
 #endif /* nsHTTPSOnlyStreamListener_h___ */
