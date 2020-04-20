aa1ec95d3e72af1c34648abff37b33849f494046
Molly Howell <mhowell@mozilla.com>
2020-04-20 03:06:07 +0000

    Bug 1596812 Part 2 - NSIS WebBrowser plugin. r=agashlin,nalexander
    This is all the code and build files for an NSIS plugin that enables
    rendering a web page as the content of an NSIS dialog.
    
    Documentation and the compiled binary are in later commits in this series.
    
    Depends on D56576
    
    Differential Revision: https://phabricator.services.mozilla.com/D56577

---
+++ other-licenses/nsis/Contrib/WebBrowser/CustomFunctions.cpp
@@ -0,0 +1,75 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0.If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "WebBrowser.h"
+#include "exdll.h"
+
+extern WebBrowser* gBrowser;
+void Init(HWND hWndParent, int string_size, TCHAR* variables,
+          stack_t** stacktop, extra_parameters* extra);
+
+static void CustomFunctionWrapper(void* NSISFunctionAddr, VARIANT jsArg,
+                                  VARIANT* retVal) {
+  // Marshal the argument passed to the JavaScript function onto the NSIS stack.
+  switch (jsArg.vt) {
+    case VT_BSTR:
+      pushstring(jsArg.bstrVal);
+      break;
+    case VT_I4: {
+      TCHAR intArgStr[32] = _T("");
+      _itot_s(jsArg.intVal, intArgStr, 10);
+      pushstring(intArgStr);
+      break;
+    }
+    case VT_BOOL:
+      pushstring(jsArg.boolVal == VARIANT_TRUE ? _T("1") : _T("0"));
+      break;
+    default:
+      // No other argument types supported.
+      pushstring(_T(""));
+      break;
+  }
+
+  // Call the NSIS function.
+  int rv = g_executeCodeSegment((int)NSISFunctionAddr, g_hwndParent);
+
+  // Retrieve the return value from the NSIS stack.
+  TCHAR* nsisRetval =
+      (TCHAR*)HeapAlloc(GetProcessHeap(), 0, g_stringsize * sizeof(TCHAR));
+  popstring(nsisRetval);
+
+  // Pass the return value back to JavaScript, if it asked for one.
+  if (retVal) {
+    VariantInit(retVal);
+    retVal->vt = VT_BSTR;
+    retVal->bstrVal = SysAllocString(nsisRetval);
+  }
+
+  HeapFree(GetProcessHeap(), 0, nsisRetval);
+}
+
+PLUGINFUNCTION(RegisterCustomFunction) {
+  if (!gBrowser) {
+    Init(hWndParent, string_size, variables, stacktop, extra);
+  }
+
+  TCHAR* funcAddrStr =
+      (TCHAR*)HeapAlloc(GetProcessHeap(), 0, g_stringsize * sizeof(TCHAR));
+  popstring(funcAddrStr);
+
+  TCHAR* funcName =
+      (TCHAR*)HeapAlloc(GetProcessHeap(), 0, g_stringsize * sizeof(TCHAR));
+  popstring(funcName);
+
+  if (gBrowser && funcAddrStr && funcName) {
+    // Apparently GetFunctionAddress returnes a 1-indexed offset, but
+    // ExecuteCodeSegment expects a 0-indexed one. Or something.
+    uintptr_t funcAddr = static_cast<uintptr_t>(_ttoi64(funcAddrStr)) - 1;
+    gBrowser->AddCustomFunction(funcName, CustomFunctionWrapper,
+                                (void*)funcAddr);
+  }
+
+  HeapFree(GetProcessHeap(), 0, funcName);
+  HeapFree(GetProcessHeap(), 0, funcAddrStr);
+}
---
+++ other-licenses/nsis/Contrib/WebBrowser/exdll.h
@@ -0,0 +1,73 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0.If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef _EXDLL_H_
+#define _EXDLL_H_
+
+#include <windows.h>
+#include <tchar.h>
+
+#define PLUGINFUNCTION(name)                                   \
+  extern "C" void __declspec(dllexport)                        \
+      name(HWND hWndParent, int string_size, TCHAR* variables, \
+           stack_t** stacktop, extra_parameters* extra)
+
+#define EXDLL_INIT()                                  \
+  {                                                   \
+    g_stringsize = string_size;                       \
+    g_stacktop = stacktop;                            \
+    g_executeCodeSegment = extra->ExecuteCodeSegment; \
+    g_hwndParent = hWndParent;                        \
+  }
+
+#define WM_NOTIFY_OUTER_NEXT (WM_USER + 0x8)
+#define WM_NOTIFY_CUSTOM_READY (WM_USER + 0xd)
+
+typedef struct _stack_t {
+  struct _stack_t* next;
+  TCHAR text[1];  // the real length of this buffer should be string_size
+} stack_t;
+
+extern unsigned int g_stringsize;
+extern stack_t** g_stacktop;
+extern int(__stdcall* g_executeCodeSegment)(int, HWND);
+extern HWND g_hwndParent;
+
+typedef struct {
+  int autoclose;
+  int all_user_var;
+  int exec_error;
+  int abort;
+  int exec_reboot;
+  int reboot_called;
+  int XXX_cur_insttype;      // deprecated
+  int XXX_insttype_changed;  // deprecated
+  int silent;
+  int instdir_error;
+  int rtl;
+  int errlvl;
+} exec_flags;
+
+// NSIS Plug-In Callback Messages
+enum NSPIM {
+  NSPIM_UNLOAD,     // This is the last message a plugin gets, do final cleanup
+  NSPIM_GUIUNLOAD,  // Called after .onGUIEnd
+};
+
+typedef UINT_PTR (*NSISPLUGINCALLBACK)(enum NSPIM);
+
+typedef struct {
+  exec_flags* exec_flags;
+  int(__stdcall* ExecuteCodeSegment)(int, HWND);
+  void(__stdcall* validate_filename)(LPWSTR);
+  int(__stdcall* RegisterPluginCallback)(
+      HMODULE,
+      NSISPLUGINCALLBACK);  // returns 0 on success, 1 if already
+                            // registered and < 0 on errors
+} extra_parameters;
+
+int popstring(TCHAR* str);
+void pushstring(const TCHAR* str);
+
+#endif  //_EXDLL_H_
