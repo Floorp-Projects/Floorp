3d9b00e6532da4ba0e2b8b1a946b9c2a1d872c1b
Eden Chuang <echuang@mozilla.com>
2020-04-20 17:21:15 +0000

    Bug 1575090 - set COEP for all workers and enforce it when loading Dedicated Workers r=asuth
    Differential Revision: https://phabricator.services.mozilla.com/D46177

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -17,6 +17,7 @@
 #include "MessageEventRunnable.h"
 #include "mozilla/Result.h"
 #include "mozilla/ScopeExit.h"
+#include "mozilla/StaticPrefs_browser.h"
 #include "mozilla/StaticPrefs_dom.h"
 #include "mozilla/dom/BlobURLProtocolHandler.h"
 #include "mozilla/dom/CallbackDebuggerNotification.h"
@@ -5263,6 +5264,92 @@ bool WorkerPrivate::CrossOriginIsolated() const {
 
   return mAgentClusterOpenerPolicy ==
          nsILoadInfo::OPENER_POLICY_SAME_ORIGIN_EMBEDDER_POLICY_REQUIRE_CORP;
+}
+
+Maybe<nsILoadInfo::CrossOriginEmbedderPolicy> WorkerPrivate::GetEmbedderPolicy()
+    const {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!StaticPrefs::browser_tabs_remote_useCrossOriginEmbedderPolicy()) {
+    return Some(nsILoadInfo::EMBEDDER_POLICY_NULL);
+  }
+
+  return mEmbedderPolicy;
+}
+
+Result<Ok, nsresult> WorkerPrivate::SetEmbedderPolicy(
+    nsILoadInfo::CrossOriginEmbedderPolicy aPolicy) {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!StaticPrefs::browser_tabs_remote_useCrossOriginEmbedderPolicy()) {
+    return Ok();
+  }
+
+  // If owner's emebedder policy is corp_reqired, aPolicy must also be
+  // corp_reqired. But if owner's embedder policy is null, aPolicy needs not
+  // match owner's value.
+  // https://wicg.github.io/cross-origin-embedder-policy/#cascade-vs-require
+  auto ownerEmbedderPolicy = GetOwnerEmbedderPolicy();
+  if (ownerEmbedderPolicy.valueOr(nsILoadInfo::EMBEDDER_POLICY_NULL) !=
+      nsILoadInfo::EMBEDDER_POLICY_NULL) {
+    if (ownerEmbedderPolicy.valueOr(aPolicy) != aPolicy) {
+      return Err(NS_ERROR_BLOCKED_BY_POLICY);
+    }
+  }
+
+  mEmbedderPolicy.emplace(aPolicy);
+
+  return Ok();
+}
+
+void WorkerPrivate::InheritOwnerEmbedderPolicyOrNull(nsIRequest* aRequest) {
+  MOZ_ASSERT(NS_IsMainThread());
+  MOZ_ASSERT(aRequest);
+
+  auto coep = GetOwnerEmbedderPolicy();
+
+  if (coep.isSome()) {
+    nsCOMPtr<nsIChannel> channel = do_QueryInterface(aRequest);
+    MOZ_ASSERT(channel);
+
+    nsCOMPtr<nsIURI> scriptURI;
+    MOZ_ALWAYS_SUCCEEDS(channel->GetURI(getter_AddRefs(scriptURI)));
+
+    bool isLocalScriptURI = false;
+    MOZ_ALWAYS_SUCCEEDS(NS_URIChainHasFlags(
+        scriptURI, nsIProtocolHandler::URI_IS_LOCAL_RESOURCE,
+        &isLocalScriptURI));
+
+    MOZ_RELEASE_ASSERT(isLocalScriptURI);
+  }
+
+  mEmbedderPolicy.emplace(coep.valueOr(nsILoadInfo::EMBEDDER_POLICY_NULL));
+}
+
+bool WorkerPrivate::MatchEmbedderPolicy(
+    nsILoadInfo::CrossOriginEmbedderPolicy aPolicy) const {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!StaticPrefs::browser_tabs_remote_useCrossOriginEmbedderPolicy()) {
+    return true;
+  }
+
+  return mEmbedderPolicy.value() == aPolicy;
+}
+
+Maybe<nsILoadInfo::CrossOriginEmbedderPolicy>
+WorkerPrivate::GetOwnerEmbedderPolicy() const {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (GetParent()) {
+    return GetParent()->GetEmbedderPolicy();
+  }
+
+  if (GetWindow() && GetWindow()->GetBrowsingContext()) {
+    return Some(GetWindow()->GetBrowsingContext()->GetEmbedderPolicy());
+  }
+
+  return Nothing();
 }
 
 NS_IMPL_ADDREF(WorkerPrivate::EventTarget)
