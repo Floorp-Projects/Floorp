a4a77fb6336dd4686549e5d92f60bdbf31da2517
alwu <alwu@mozilla.com>
2020-04-20 21:19:57 +0000

    Bug 1625615 - part3 : prevent media starting playing when inactive docshell wants to suspend any media. r=bryce
    When the docShell's `SuspendMediaWhenInactive` flag is true, no media should be allowed to start playing. Therefore, we add a check in `Play()`, `CanActivateAutoplay()` to prevent media from playing. In addition, we should also prevent the audio channel agant from starting.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69672

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -1535,6 +1535,11 @@ class HTMLMediaElement::AudioChannelAgentCallback final
       return false;
     }
 
+    // Media is suspended by the docshell.
+    if (mOwner->ShouldBeSuspendedByInactiveDocShell()) {
+      return false;
+    }
+
     // Are we paused
     if (mOwner->mPaused) {
       return false;
@@ -4214,6 +4219,12 @@ already_AddRefed<Promise> HTMLMediaElement::Play(ErrorResult& aRv) {
   // play promises.
   // Note: Promise appended to list of pending promises as needed below.
 
+  if (ShouldBeSuspendedByInactiveDocShell()) {
+    LOG(LogLevel::Debug, ("%p no allow to play by the docShell for now", this));
+    mPendingPlayPromises.AppendElement(promise);
+    return promise.forget();
+  }
+
   // We may delay starting playback of a media resource for an unvisited tab
   // until it's going to foreground or being resumed by the play tab icon.
   if (MediaPlaybackDelayPolicy::ShouldDelayPlayback(this)) {
@@ -6026,6 +6037,11 @@ bool HTMLMediaElement::CanActivateAutoplay() {
   // Static document is used for print preview and printing, should not be
   // autoplay
   if (OwnerDoc()->IsStaticDocument()) {
+    return false;
+  }
+
+  if (ShouldBeSuspendedByInactiveDocShell()) {
+    LOG(LogLevel::Debug, ("%p prohibiting autoplay by the docShell", this));
     return false;
   }
 
