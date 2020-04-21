22db1a98dd7fb87a7bbe316181a083154110101f
Andrea Marchesini <amarchesini@mozilla.com>
2020-04-21 10:41:40 +0000

    Bug 1631565 - getCookieStringFromHttp requires the nsIChannel argument, r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D71642

--- netwerk/cookie/test/unit/test_bug643051.js
+++ netwerk/cookie/test/unit/test_bug643051.js
@@ -4,16 +4,25 @@ const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 function run_test() {
   // Allow all cookies.
   Services.prefs.setIntPref("network.cookie.cookieBehavior", 0);
+  Services.prefs.setBoolPref(
+    "network.cookieJarSettings.unblocked_for_testing",
+    true
+  );
 
   let cs = Cc["@mozilla.org/cookieService;1"].getService(Ci.nsICookieService);
 
   let uri = NetUtil.newURI("http://example.org/");
+  let channel = NetUtil.newChannel({
+    uri,
+    loadUsingSystemPrincipal: true,
+    contentPolicyType: Ci.nsIContentPolicy.TYPE_DOCUMENT,
+  });
 
   let set = "foo=bar\nbaz=foo";
   let expected = "foo=bar; baz=foo";
   cs.setCookieStringFromHttp(uri, set, null);
 
-  let actual = cs.getCookieStringFromHttp(uri, null);
+  let actual = cs.getCookieStringFromHttp(uri, channel);
   Assert.equal(actual, expected);
 
   uri = NetUtil.newURI("http://example.com/");
