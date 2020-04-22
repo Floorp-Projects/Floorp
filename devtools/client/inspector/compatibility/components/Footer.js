fd3b574e9fc20e177b56fb62f58390e45a476df0
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-04-22 00:53:33 +0000

    Bug 1590981: Append settings button. r=mtigley
    Depends on D70286
    
    Differential Revision: https://phabricator.services.mozilla.com/D70317

--- devtools/client/inspector/compatibility/components/Footer.js
+++ devtools/client/inspector/compatibility/components/Footer.js
@@ -17,28 +17,38 @@ loader.lazyRequireGetter(
 const FEEDBACK_LINK =
   "https://docs.google.com/forms/d/e/1FAIpQLSeevOHveQ1tDuKYY5Fxyb3vvbKKumdLWUT5-RuwJWoAAOST5g/viewform";
 
+const REPORT_ICON = "chrome://devtools/skin/images/report.svg";
+const SETTINGS_ICON = "chrome://devtools/skin/images/settings.svg";
+
 class Footer extends PureComponent {
+  _renderButton(icon, label, onClick) {
+    return dom.button(
+      {
+        className: "compatibility-footer__button",
+        title: label,
+        onClick,
+      },
+      dom.img({
+        className: "compatibility-footer__icon",
+        src: icon,
+      }),
+      dom.label(
+        {
+          className: "compatibility-footer__label",
+        },
+        label
+      )
+    );
+  }
+
   render() {
     return dom.footer(
       {
         className: "compatibility-footer",
       },
-      dom.button(
-        {
-          className: "compatibility-footer__feedback-button",
-          title: "Feedback",
-          onClick: () => openDocLink(FEEDBACK_LINK),
-        },
-        dom.img({
-          className: "compatibility-footer__feedback-icon",
-          src: "chrome://devtools/skin/images/report.svg",
-        }),
-        dom.label(
-          {
-            className: "compatibility-footer__feedback-label",
-          },
-          "Feedback"
-        )
+      this._renderButton(SETTINGS_ICON, "Settings"),
+      this._renderButton(REPORT_ICON, "Feedback", () =>
+        openDocLink(FEEDBACK_LINK)
       )
     );
   }
