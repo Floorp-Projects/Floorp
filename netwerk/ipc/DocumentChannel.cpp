e5601108adb34a2b8f4a879b51f16782cc36bf7b
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-04-22 23:49:38 +0000

    Bug 1607984 - P17. Put ParentProcessDocumentChannel behind a pref. r=necko-reviewers,mattwoodrow
    We disable it on Android for now due to unexplained reftest start failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71875

--- netwerk/ipc/DocumentChannel.cpp
+++ netwerk/ipc/DocumentChannel.cpp
@@ -170,6 +170,12 @@ already_AddRefed<DocumentChannel> DocumentChannel::CreateDocumentChannel(
 
 bool DocumentChannel::CanUseDocumentChannel(nsDocShellLoadState* aLoadState) {
   MOZ_ASSERT(aLoadState);
+
+  if (XRE_IsParentProcess() &&
+      !StaticPrefs::browser_tabs_documentchannel_ppdc()) {
+    return false;
+  }
+
   // We want to use DocumentChannel if we're using a supported scheme. Sandboxed
   // srcdoc loads break due to failing assertions after changing processes, and
   // non-sandboxed srcdoc loads need to share the same principal object as their
