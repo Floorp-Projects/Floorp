b416de9c4363633c76454dc10fd150078cdc9815
Botond Ballo <botond@mozilla.com>
2020-04-22 00:10:55 +0000

    Bug 1629732 - Fix non-unified build errors in layout/tools. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D71846

--- layout/tools/layout-debug/src/nsLayoutDebugCLH.cpp
+++ layout/tools/layout-debug/src/nsLayoutDebugCLH.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "nsLayoutDebugCLH.h"
+#include "mozIDOMWindow.h"
 #include "nsArray.h"
 #include "nsString.h"
 #include "plstr.h"
