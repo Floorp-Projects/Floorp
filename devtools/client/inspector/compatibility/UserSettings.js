2575ca8e6e736468712709476f93b7bc6a72a69d
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-04-22 01:17:45 +0000

    Bug 1590981: Introduce UserSettings object. r=mtigley
    Depends on D70318
    
    Differential Revision: https://phabricator.services.mozilla.com/D70319

---
+++ devtools/client/inspector/compatibility/UserSettings.js
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+loader.lazyRequireGetter(
+  this,
+  "browsersDataset",
+  "devtools/client/inspector/compatibility/lib/dataset/browsers.json"
+);
+
+const TARGET_BROWSER_ID = [
+  "firefox",
+  "firefox_android",
+  "chrome",
+  "chrome_android",
+  "safari",
+  "safari_ios",
+  "edge",
+];
+const TARGET_BROWSER_STATUS = ["esr", "current", "beta", "nightly"];
+
+function getDefaultTargetBrowsers() {
+  // Retrieve the information that matches to the browser id and the status
+  // from the browsersDataset.
+  // For the structure of then browsersDataset,
+  // see https://github.com/mdn/browser-compat-data/blob/master/browsers/firefox.json
+  const targets = [];
+
+  for (const id of TARGET_BROWSER_ID) {
+    const { name, releases } = browsersDataset[id];
+
+    for (const version in releases) {
+      const { status } = releases[version];
+
+      if (TARGET_BROWSER_STATUS.includes(status)) {
+        targets.push({ id, name, version, status });
+      }
+    }
+  }
+
+  return targets;
+}
+
+module.exports = { getDefaultTargetBrowsers };
