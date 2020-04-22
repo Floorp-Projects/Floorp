b085a7e8c9750b00067860a6b0c394a2b36cfd1b
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-04-22 13:44:23 +0000

    Bug 1607984 - P16. Always whitelist file. r=markh
    This file will be removed in bug 1628752 anyway.
    
    Differential Revision: https://phabricator.services.mozilla.com/D70801

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -228,6 +228,9 @@ var whitelist = [
 
   // Referenced from the screenshots webextension
   { file: "resource://app/localization/en-US/browser/screenshots.ftl" },
+
+  // services/sync/modules/bridged_engine.js
+  { file: "resource://services-sync/bridged_engine.js" },
 ];
 
 if (AppConstants.NIGHTLY_BUILD && AppConstants.platform != "win") {
@@ -269,8 +272,6 @@ for (let entry of ignorableWhitelist) {
 if (!isDevtools) {
   // services/sync/modules/main.js
   whitelist.add("resource://services-sync/service.js");
-  // services/sync/modules/bridged_engine.js
-  whitelist.add("resource://services-sync/bridged_engine.js");
   // services/sync/modules/service.js
   for (let module of [
     "addons.js",
