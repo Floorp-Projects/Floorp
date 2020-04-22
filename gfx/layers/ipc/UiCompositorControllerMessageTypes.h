745271c6c84e52c9b3ccfe12314894bd93886f99
Kartikaya Gupta <kgupta@mozilla.com>
2020-04-22 11:50:41 +0000

    Bug 1627716 - Remove unused constants. r=geckoview-reviewers,agi
    Depends on D71779
    
    Differential Revision: https://phabricator.services.mozilla.com/D71780

--- gfx/layers/ipc/UiCompositorControllerMessageTypes.h
+++ gfx/layers/ipc/UiCompositorControllerMessageTypes.h
@@ -12,27 +12,17 @@ namespace layers {
 
 //
 // NOTE: These values are also defined in
-// mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/LayerView.java
+// mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
 // and must be kept in sync. Any new message added here must also be added
-// there to the AnimatorMessageType enum.
+// there.
 //
 
 // clang-format off
 enum UiCompositorControllerMessageTypes {
-  STATIC_TOOLBAR_NEEDS_UPDATE      = 0,  // Sent from compositor when the static toolbar wants to hide.
-  STATIC_TOOLBAR_READY             = 1,  // Sent from compositor when the static toolbar image has been updated and is ready to animate.
-  TOOLBAR_HIDDEN                   = 2,  // Sent to compositor when the real toolbar has been hidden.
-  TOOLBAR_VISIBLE                  = 3,  // Sent to compositor when the real toolbar has been made  visible
-  TOOLBAR_SHOW                     = 4,  // Sent from compositor when the real toolbar should be shown
-  FIRST_PAINT                      = 5,  // Sent from compositor after first paint
-  REQUEST_SHOW_TOOLBAR_IMMEDIATELY = 6,  // Sent to the compositor when the snapshot should be shown immediately
-  REQUEST_SHOW_TOOLBAR_ANIMATED    = 7,  // Sent to the compositor when the snapshot should be shown with an animation
-  REQUEST_HIDE_TOOLBAR_IMMEDIATELY = 8,  // Sent to the compositor when the snapshot should be hidden immediately
-  REQUEST_HIDE_TOOLBAR_ANIMATED    = 9,  // Sent to the compositor when the snapshot should be hidden with an animation
-  LAYERS_UPDATED                   = 10, // Sent from the compositor when any layer has been updated
-  TOOLBAR_SNAPSHOT_FAILED          = 11, // Sent to compositor when the toolbar snapshot fails.
-  COMPOSITOR_CONTROLLER_OPEN       = 20, // Compositor controller IPC is open
-  IS_COMPOSITOR_CONTROLLER_OPEN    = 21  // Special message sent from controller to query if the compositor controller is open
+  FIRST_PAINT                      = 0, // Sent from compositor after first paint
+  LAYERS_UPDATED                   = 1, // Sent from the compositor when any layer has been updated
+  COMPOSITOR_CONTROLLER_OPEN       = 2, // Compositor controller IPC is open
+  IS_COMPOSITOR_CONTROLLER_OPEN    = 3  // Special message sent from controller to query if the compositor controller is open
 };
 // clang-format on
 
