8445ae253231a89b671eea4faa4e5c1581582709
Botond Ballo <botond@mozilla.com>
2020-04-22 08:16:15 +0000

    Bug 1632042 - Fix inclusion of APZUtils.h in APZInputBridge.h. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D71880

--- gfx/layers/apz/public/APZInputBridge.h
+++ gfx/layers/apz/public/APZInputBridge.h
@@ -7,9 +7,9 @@
 #ifndef mozilla_layers_APZInputBridge_h
 #define mozilla_layers_APZInputBridge_h
 
-#include "APZUtils.h"               // for APZWheelAction
-#include "mozilla/EventForwards.h"  // for WidgetInputEvent, nsEventStatus
-#include "Units.h"                  // for LayoutDeviceIntPoint
+#include "mozilla/EventForwards.h"    // for WidgetInputEvent, nsEventStatus
+#include "mozilla/layers/APZUtils.h"  // for APZWheelAction
+#include "Units.h"                    // for LayoutDeviceIntPoint
 
 namespace mozilla {
 
