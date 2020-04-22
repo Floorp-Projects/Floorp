4e07d3dcc0c9206f65b0ccde678c7e9df609b000
Christian Sadilek <christian.sadilek@gmail.com>
2020-04-22 10:08:55 -0400

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/6702: Upgrade to GV Nightly 77.0.20200422093542

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "77.0.20200421094220"
+    const val nightly_version = "77.0.20200422093542"
 
     /**
      * GeckoView Beta Version.
