031bd9a59c6ed5a84eeb2fd4f7dac105322431d7
Tim Nguyen <ntim.bugs@gmail.com>
2020-04-23 19:29:48 +0000

    Bug 1631289 - Stop including preferences.css twice in preferences dialogs. r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D72180

--- browser/components/preferences/subdialogs.js
+++ browser/components/preferences/subdialogs.js
@@ -62,14 +62,15 @@ SubDialog.prototype = {
   },
 
   injectXMLStylesheet(aStylesheetURL) {
-    let contentStylesheet = this._frame.contentDocument.createProcessingInstruction(
+    const doc = this._frame.contentDocument;
+    if ([...doc.styleSheets].find(s => s.href === aStylesheetURL)) {
+      return;
+    }
+    let contentStylesheet = doc.createProcessingInstruction(
       "xml-stylesheet",
       'href="' + aStylesheetURL + '" type="text/css"'
     );
-    this._frame.contentDocument.insertBefore(
-      contentStylesheet,
-      this._frame.contentDocument.documentElement
-    );
+    doc.insertBefore(contentStylesheet, doc.documentElement);
   },
 
   async open(aURL, aFeatures = null, aParams = null, aClosingCallback = null) {
