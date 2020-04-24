84c1d3b28bce100785426dc9a7fadb871a1de165
Eitan Isaacson <eitan@monotonous.org>
2020-04-24 14:36:36 +0000

    Bug 1625864 - Expose unlinked a tags as AXGroup. r=morgan
    This is the recommended role in the HTML Accessibility API Mappings draft[1].
    
    Although Chrome and Safari just expose unlinked a tags as AXStaticText.
    They also don't update the accessble if it gets an href. So while we
    are doing this a bit differently, I think it is probably better. And VO is happy.
    
    1. https://www.w3.org/TR/html-aam-1.0/#html-element-role-mappings
    
    Differential Revision: https://phabricator.services.mozilla.com/D71260

--- accessible/tests/browser/mac/browser_link.js
+++ accessible/tests/browser/mac/browser_link.js
@@ -39,3 +39,111 @@ addAccessibleTask(
     await PlacesUtils.history.clear();
   }
 );
+
+/**
+ * Test linked vs unlinked anchor tags
+ */
+addAccessibleTask(
+  `
+  <a id="link1" href="#">I am a link link</a>
+  <a id="link2" onclick="console.log('hi')">I am a link-ish link</a>
+  <a id="link3">I am a non-link link</a>
+  `,
+  async (browser, accDoc) => {
+    let link1 = getNativeInterface(accDoc, "link1");
+    is(
+      link1.getAttributeValue("AXRole"),
+      "AXLink",
+      "a[href] gets correct link role"
+    );
+    ok(
+      link1.attributeNames.includes("AXVisited"),
+      "Link has visited attribute"
+    );
+    ok(link1.attributeNames.includes("AXURL"), "Link has URL attribute");
+
+    let link2 = getNativeInterface(accDoc, "link2");
+    is(
+      link2.getAttributeValue("AXRole"),
+      "AXLink",
+      "a[onclick] gets correct link role"
+    );
+    ok(
+      link2.attributeNames.includes("AXVisited"),
+      "Link has visited attribute"
+    );
+    ok(link2.attributeNames.includes("AXURL"), "Link has URL attribute");
+
+    let link3 = getNativeInterface(accDoc, "link3");
+    is(
+      link3.getAttributeValue("AXRole"),
+      "AXGroup",
+      "bare <a> gets correct group role"
+    );
+    ok(
+      !link3.attributeNames.includes("AXVisited"),
+      "Non-link should not have visited attribute"
+    );
+    ok(
+      !link3.attributeNames.includes("AXURL"),
+      "Non-link should not have URL attribute"
+    );
+
+    let stateChanged = waitForEvent(EVENT_STATE_CHANGE, "link1");
+    await SpecialPowers.spawn(browser, [], () => {
+      content.document.getElementById("link1").removeAttribute("href");
+    });
+    await stateChanged;
+    is(
+      link1.getAttributeValue("AXRole"),
+      "AXGroup",
+      "<a> stripped from href gets group role"
+    );
+    ok(
+      !link1.attributeNames.includes("AXVisited"),
+      "Non-link should not have visited attribute"
+    );
+    ok(
+      !link1.attributeNames.includes("AXURL"),
+      "Non-link should not have URL attribute"
+    );
+
+    stateChanged = waitForEvent(EVENT_STATE_CHANGE, "link2");
+    await SpecialPowers.spawn(browser, [], () => {
+      content.document.getElementById("link2").removeAttribute("onclick");
+    });
+    await stateChanged;
+    is(
+      link2.getAttributeValue("AXRole"),
+      "AXGroup",
+      "<a> stripped from onclick gets group role"
+    );
+    ok(
+      !link2.attributeNames.includes("AXVisited"),
+      "Non-link should not have visited attribute"
+    );
+    ok(
+      !link2.attributeNames.includes("AXURL"),
+      "Non-link should not have URL attribute"
+    );
+
+    stateChanged = waitForEvent(EVENT_STATE_CHANGE, "link3");
+    await SpecialPowers.spawn(browser, [], () => {
+      content.document
+        .getElementById("link3")
+        .setAttribute("href", "http://example.com");
+    });
+    await stateChanged;
+    is(
+      link3.getAttributeValue("AXRole"),
+      "AXLink",
+      "href added to bare a gets link role"
+    );
+
+    ok(
+      link3.attributeNames.includes("AXVisited"),
+      "Link has visited attribute"
+    );
+    ok(link3.attributeNames.includes("AXURL"), "Link has URL attribute");
+  }
+);
