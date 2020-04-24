017b985fe2e3a896a63ee3ce15f84c96e94af4cc
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-04-24 01:46:25 +0000

    Bug 1607984 - P17. Put ParentProcessDocumentChannel behind a pref. r=necko-reviewers,mattwoodrow
    We disable it for now, until some remaining failures are sorted out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71875

--- netwerk/ipc/DocumentChannel.cpp
+++ netwerk/ipc/DocumentChannel.cpp
@@ -153,6 +153,12 @@ static bool URIUsesDocChannel(nsIURI* aURI) {
 
 bool DocumentChannel::CanUseDocumentChannel(nsDocShellLoadState* aLoadState) {
   MOZ_ASSERT(aLoadState);
+
+  if (XRE_IsParentProcess() &&
+      !StaticPrefs::browser_tabs_documentchannel_ppdc()) {
+    return false;
+  }
+
   // We want to use DocumentChannel if we're using a supported scheme. Sandboxed
   // srcdoc loads break due to failing assertions after changing processes, and
   // non-sandboxed srcdoc loads need to share the same principal object as their
