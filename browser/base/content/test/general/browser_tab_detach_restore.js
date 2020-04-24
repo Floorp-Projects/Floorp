9587fac16b2e963a804733e85b15cd9d1c4a63d4
Matt Woodrow <mwoodrow@mozilla.com>
2020-04-24 06:58:31 +0000

    Bug 1602318 - Make some browser tests wait on the test uri to load instead of any uri. r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D70624

--- browser/base/content/test/general/browser_tab_detach_restore.js
+++ browser/base/content/test/general/browser_tab_detach_restore.js
@@ -15,7 +15,7 @@ add_task(async function() {
 
   let tab = BrowserTestUtils.addTab(gBrowser);
   BrowserTestUtils.loadURI(tab.linkedBrowser, uri);
-  await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+  await BrowserTestUtils.browserLoaded(tab.linkedBrowser, false, uri);
   await TabStateFlusher.flush(tab.linkedBrowser);
 
   let key = tab.linkedBrowser.permanentKey;
