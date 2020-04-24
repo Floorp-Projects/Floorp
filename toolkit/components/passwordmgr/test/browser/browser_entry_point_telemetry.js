c84a1b3dba87459b85450e626d8dbe58367343bb
Tim Nguyen <ntim.bugs@gmail.com>
2020-04-24 13:43:59 +0000

    Bug 1632097 - Clean up pageInfo.js. r=florian
    Differential Revision: https://phabricator.services.mozilla.com/D71928

--- toolkit/components/passwordmgr/test/browser/browser_entry_point_telemetry.js
+++ toolkit/components/passwordmgr/test/browser/browser_entry_point_telemetry.js
@@ -67,7 +67,7 @@ add_task(async function pageInfo_entryPoint() {
     async function(browser) {
       info("pageInfo_entryPoint, opening pageinfo");
       let pageInfo = BrowserPageInfo(TEST_ORIGIN, "securityTab", {});
-      await BrowserTestUtils.waitForEvent(pageInfo, "load");
+      await BrowserTestUtils.waitForEvent(pageInfo, "page-info-init");
       info(
         "pageInfo_entryPoint, got pageinfo, wait until password button is visible"
       );
