634e68a795ad0f8e4a64540d35984265cae5a3bc
Jan-Erik Rediger <jrediger@mozilla.com>
2020-04-24 15:22:44 +0000

    Bug 1632131 - Simple smoke test for FOG's string metric type. r=chutten
    A first Rust test, which should run without any interaction with
    Firefox.
    This will eventually break down when we get IPC in there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72385

---
+++ toolkit/components/glean/api/tests/string.rs
@@ -0,0 +1,27 @@
+mod common;
+use common::*;
+
+use glean::metrics::{CommonMetricData, Lifetime, StringMetric};
+
+#[test]
+fn sets_string_value() {
+    let _lock = lock_test();
+    let _t = setup_glean(None);
+    let store_names: Vec<String> = vec!["store1".into()];
+
+    let metric = StringMetric::new(CommonMetricData {
+        name: "string_metric".into(),
+        category: "telemetry".into(),
+        send_in_pings: store_names.clone(),
+        disabled: false,
+        lifetime: Lifetime::Ping,
+        ..Default::default()
+    });
+
+    metric.set("test_string_value");
+
+    assert_eq!(
+        "test_string_value",
+        metric.test_get_value("store1").unwrap()
+    );
+}
