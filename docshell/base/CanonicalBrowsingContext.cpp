601dd2cf04b3cf0a869b19d7529c542c6e8c6055
Matt Woodrow <mwoodrow@mozilla.com>
2020-04-24 06:55:20 +0000

    Bug 1602318 - Associate a current DocumentLoadListener with CanonicalBrowsingContext. r=nika,necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D67094

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -15,6 +15,7 @@
 #include "mozilla/dom/PlaybackController.h"
 #include "mozilla/ipc/ProtocolUtils.h"
 #include "mozilla/NullPrincipal.h"
+#include "mozilla/net/DocumentLoadListener.h"
 
 #include "nsGlobalWindowOuter.h"
 
@@ -522,6 +523,17 @@ MediaController* CanonicalBrowsingContext::GetMediaController() {
     mTabMediaController = new MediaController(Id());
   }
   return mTabMediaController;
+}
+
+void CanonicalBrowsingContext::StartDocumentLoad(
+    net::DocumentLoadListener* aLoad) {
+  mCurrentLoad = aLoad;
+}
+void CanonicalBrowsingContext::EndDocumentLoad(
+    net::DocumentLoadListener* aLoad) {
+  if (mCurrentLoad == aLoad) {
+    mCurrentLoad = nullptr;
+  }
 }
 
 NS_IMPL_CYCLE_COLLECTION_INHERITED(CanonicalBrowsingContext, BrowsingContext,
