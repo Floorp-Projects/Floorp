63892870eb93d8d639e2b357bce8fb7928ca3cae
Glenn Watson <gw@intuitionlibrary.com>
2020-04-24 18:29:38 +0000

    Bug 1632705 - Part 1 - Remove complex clips from scroll frame definitions. r=kats,Bert
    They aren't used, so can easily be removed as the first part of
    this seried of patches.
    
    If this functionality is ever required, it can be handled by the
    caller defining complex clip nodes explicitly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72261

--- gfx/wr/examples/scrolling.rs
+++ gfx/wr/examples/scrolling.rs
@@ -54,7 +54,6 @@ impl Example for App {
                 None,
                 (0, 0).by(1000, 1000),
                 scrollbox,
-                vec![],
                 ScrollSensitivity::ScriptAndInputEvents,
                 LayoutVector2D::zero(),
             );
@@ -87,7 +86,6 @@ impl Example for App {
                 None,
                 (0, 100).to(300, 1000),
                 (0, 100).to(200, 300),
-                vec![],
                 ScrollSensitivity::ScriptAndInputEvents,
                 LayoutVector2D::zero(),
             );
