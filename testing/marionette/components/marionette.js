24166c88af03f19fdf5132de94a689c599ad1a33
Henrik Skupin <mail@hskupin.info>
2020-04-24 17:25:33 +0000

    Bug 1632556 - [marionette] "navigator.webdriver" has to reflect the enabled state of Marionette. r=marionette-reviewers,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D72387

--- testing/marionette/components/marionette.js
+++ testing/marionette/components/marionette.js
@@ -304,6 +304,7 @@ class MarionetteParentProcess {
     if (env.exists(ENV_ENABLED)) {
       this.enabled = true;
     } else {
+      // TODO: Don't read the preference anymore (bug 1632821)
       this.enabled = MarionettePrefs.enabled;
     }
 
@@ -312,6 +313,19 @@ class MarionetteParentProcess {
     }
 
     Services.ppmm.addMessageListener("Marionette:IsRunning", this);
+  }
+
+  get enabled() {
+    return !!this._enabled;
+  }
+
+  set enabled(value) {
+    if (value) {
+      // Only update the preference when Marionette is going to be enabled
+      MarionettePrefs.enabled = value;
+    }
+
+    this._enabled = value;
   }
 
   get running() {
