9e4df2672469c925db3b8232525186f48e4e7b65
Matthew Noorenberghe <mozilla@noorenberghe.ca>
2020-04-25 08:27:06 +0000

    Bug 1633102 - Fix linting from moving mozscreenshots b-c tests. a=bustage
    Differential Revision: https://phabricator.services.mozilla.com/D72518

--- browser/tools/mozscreenshots/tests/browser/browser_boundingbox.js
+++ browser/tools/mozscreenshots/tests/browser/browser_boundingbox.js
@@ -4,6 +4,8 @@
 
 "use strict";
 
+/* import-globals-from ../../head.js */
+
 add_task(async function() {
   const scale = window.docShell.QueryInterface(Ci.nsIBaseWindow)
     .devicePixelsPerDesktopPixel;
