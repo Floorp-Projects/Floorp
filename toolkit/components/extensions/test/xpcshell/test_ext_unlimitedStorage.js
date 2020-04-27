3dcf5e37f400eef4ec63b7aa1c1571a22312050c
Luca Greco <lgreco@mozilla.com>
2020-04-27 18:35:05 +0000

    Bug 1631434 - Add test case to ensure storage.local data not cleared as a side-effect of removing unlimitedStorage permission. r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D72339

--- toolkit/components/extensions/test/xpcshell/test_ext_unlimitedStorage.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_unlimitedStorage.js
@@ -88,10 +88,38 @@ add_task(async function test_unlimitedStorage_restored_on_app_startup() {
 add_task(async function test_unlimitedStorage_removed_on_update() {
   const id = "test-unlimitedStorage-removed-on-update@mozilla";
 
+  function background() {
+    browser.test.onMessage.addListener(async msg => {
+      switch (msg) {
+        case "set-storage":
+          browser.test.log(`storing data in storage.local`);
+          await browser.storage.local.set({ akey: "somevalue" });
+          browser.test.log(`data stored in storage.local successfully`);
+          break;
+        case "has-storage": {
+          browser.test.log(`checking data stored in storage.local`);
+          const data = await browser.storage.local.get(["akey"]);
+          browser.test.assertEq(
+            data.akey,
+            "somevalue",
+            "Got storage.local data"
+          );
+          break;
+        }
+        default:
+          browser.test.fail(`Unexpected test message: ${msg}`);
+      }
+
+      browser.test.sendMessage(`${msg}:done`);
+    });
+  }
+
   let extension = ExtensionTestUtils.loadExtension({
+    background,
     manifest: {
-      permissions: ["unlimitedStorage"],
+      permissions: ["unlimitedStorage", "storage"],
       applications: { gecko: { id } },
+      version: "1",
     },
     useAddonManager: "permanent",
   });
@@ -107,11 +135,20 @@ add_task(async function test_unlimitedStorage_removed_on_update() {
     "has been allowed"
   );
 
+  extension.sendMessage("set-storage");
+  await extension.awaitMessage("set-storage:done");
+  extension.sendMessage("has-storage");
+  await extension.awaitMessage("has-storage:done");
+
   // Simulate an update which do not require the unlimitedStorage permission.
   await extension.upgrade({
+    background,
     manifest: {
+      permissions: ["storage"],
       applications: { gecko: { id } },
+      version: "2",
     },
+    useAddonManager: "permanent",
   });
 
   const newPolicy = WebExtensionPolicy.getByID(extension.id);
@@ -128,6 +165,12 @@ add_task(async function test_unlimitedStorage_removed_on_update() {
     Services.perms.UNKNOWN_ACTION,
     "has been cleared"
   );
+
+  // Verify that the previously stored data has not been
+  // removed as a side effect of removing the unlimitedStorage
+  // permission.
+  extension.sendMessage("has-storage");
+  await extension.awaitMessage("has-storage:done");
 
   await extension.unload();
 });
