8657f0c3ac1432805ef7caa05a5ded6856e255cd
Yura Zenevich <yura.zenevich@gmail.com>
2020-04-27 14:16:03 +0000

    Bug 1602075 - display enabled Inspect Accessibility Properties menu item in inspector if devtools.accessibility.auto-init.enabled pref is set to true. r=gl
    Differential Revision: https://phabricator.services.mozilla.com/D71587

--- devtools/client/accessibility/test/browser/browser_accessibility_context_menu_inspector.js
+++ devtools/client/accessibility/test/browser/browser_accessibility_context_menu_inspector.js
@@ -2,6 +2,11 @@
 http://creativecommons.org/publicdomain/zero/1.0/ */
 "use strict";
 
+const a11yAutoInitEnabled = Services.prefs.getBoolPref(
+  "devtools.accessibility.auto-init.enabled",
+  false
+);
+
 const TEST_URI = `
   <h1 id="h1">header</h1>
   <p id="p">paragraph</p>
@@ -24,7 +29,9 @@ async function openContextMenuForNode({ toolbox }, selector) {
     await updated;
   }
 
-  const menuUpdated = inspector.once("node-menu-updated");
+  const menuUpdated = a11yAutoInitEnabled
+    ? Promise.resolve()
+    : inspector.once("node-menu-updated");
   const allMenuItems = openContextMenuAndGetAllItems(inspector);
   await menuUpdated;
   return allMenuItems;
@@ -41,7 +48,7 @@ function checkShowA11YPropertiesNode(allMenuItems, disabled) {
   is(
     showA11YPropertiesNode.disabled,
     disabled,
-    "Show accessibility properties item has correct state"
+    `Show accessibility properties item has correct disabled state: ${disabled}`
   );
   return showA11YPropertiesNode;
 }
@@ -89,7 +96,7 @@ addA11YPanelTask(
     let allMenuItems = await openContextMenuForNode(env);
     let showA11YPropertiesNode = checkShowA11YPropertiesNode(
       allMenuItems,
-      true
+      !a11yAutoInitEnabled
     );
 
     allMenuItems = await openContextMenuForNode(env, "#h1");
@@ -101,7 +108,10 @@ addA11YPanelTask(
     await checkAccessibleObjectSelection(env, showA11YPropertiesNode, true);
 
     allMenuItems = await openContextMenuForNode(env, "#span-2");
-    showA11YPropertiesNode = checkShowA11YPropertiesNode(allMenuItems, true);
+    showA11YPropertiesNode = checkShowA11YPropertiesNode(
+      allMenuItems,
+      !a11yAutoInitEnabled
+    );
 
     const inspector = env.toolbox.getPanel("inspector");
     const span2 = await getNodeFront("#span-2", inspector);
