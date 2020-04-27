759f0f2255290d76887e62ce19f8180780aa9b5d
Razvan Maries <rmaries@mozilla.com>
2020-04-28 00:26:55 +0300

    Backed out 2 changesets (bug 1633527) for perma failures on test_ext_geckoProfiler_control.js. CLOSED TREE
    Backed out changeset 6781a7371f6e (bug 1633527)
    Backed out changeset e63cd75d1774 (bug 1633527)

--- js/src/jit/JitRealm.h
+++ js/src/jit/JitRealm.h
@@ -129,24 +129,24 @@ class JitRuntime {
  private:
   friend class JitRealm;
 
-  MainThreadData<uint64_t> nextCompilationId_{0};
+  MainThreadData<uint64_t> nextCompilationId_;
 
   // Buffer for OSR from baseline to Ion. To avoid holding on to this for too
   // long it's also freed in EnterBaseline and EnterJit (after returning from
   // JIT code).
-  MainThreadData<js::UniquePtr<uint8_t>> ionOsrTempData_{nullptr};
+  MainThreadData<js::UniquePtr<uint8_t>> ionOsrTempData_;
 
   // Shared exception-handler tail.
-  WriteOnceData<uint32_t> exceptionTailOffset_{0};
+  WriteOnceData<uint32_t> exceptionTailOffset_;
 
   // Shared post-bailout-handler tail.
-  WriteOnceData<uint32_t> bailoutTailOffset_{0};
+  WriteOnceData<uint32_t> bailoutTailOffset_;
 
   // Shared profiler exit frame tail.
-  WriteOnceData<uint32_t> profilerExitFrameTailOffset_{0};
+  WriteOnceData<uint32_t> profilerExitFrameTailOffset_;
 
   // Trampoline for entering JIT code.
-  WriteOnceData<uint32_t> enterJITOffset_{0};
+  WriteOnceData<uint32_t> enterJITOffset_;
 
   // Vector mapping frame class sizes to bailout tables.
   struct BailoutTable {
@@ -155,61 +155,58 @@ class JitRuntime {
     BailoutTable(uint32_t startOffset, uint32_t size)
         : startOffset(startOffset), size(size) {}
   };
-  using BailoutTableVector = Vector<BailoutTable, 4, SystemAllocPolicy>;
+  typedef Vector<BailoutTable, 4, SystemAllocPolicy> BailoutTableVector;
   WriteOnceData<BailoutTableVector> bailoutTables_;
 
   // Generic bailout table; used if the bailout table overflows.
-  WriteOnceData<uint32_t> bailoutHandlerOffset_{0};
+  WriteOnceData<uint32_t> bailoutHandlerOffset_;
 
   // Argument-rectifying thunk, in the case of insufficient arguments passed
   // to a function call site.
-  WriteOnceData<uint32_t> argumentsRectifierOffset_{0};
-  WriteOnceData<uint32_t> argumentsRectifierReturnOffset_{0};
+  WriteOnceData<uint32_t> argumentsRectifierOffset_;
+  WriteOnceData<uint32_t> argumentsRectifierReturnOffset_;
 
   // Thunk that invalides an (Ion compiled) caller on the Ion stack.
-  WriteOnceData<uint32_t> invalidatorOffset_{0};
+  WriteOnceData<uint32_t> invalidatorOffset_;
 
   // Thunk that calls the GC pre barrier.
-  WriteOnceData<uint32_t> valuePreBarrierOffset_{0};
-  WriteOnceData<uint32_t> stringPreBarrierOffset_{0};
-  WriteOnceData<uint32_t> objectPreBarrierOffset_{0};
-  WriteOnceData<uint32_t> shapePreBarrierOffset_{0};
-  WriteOnceData<uint32_t> objectGroupPreBarrierOffset_{0};
+  WriteOnceData<uint32_t> valuePreBarrierOffset_;
+  WriteOnceData<uint32_t> stringPreBarrierOffset_;
+  WriteOnceData<uint32_t> objectPreBarrierOffset_;
+  WriteOnceData<uint32_t> shapePreBarrierOffset_;
+  WriteOnceData<uint32_t> objectGroupPreBarrierOffset_;
 
   // Thunk to call malloc/free.
-  WriteOnceData<uint32_t> freeStubOffset_{0};
+  WriteOnceData<uint32_t> freeStubOffset_;
 
   // Thunk called to finish compilation of an IonScript.
-  WriteOnceData<uint32_t> lazyLinkStubOffset_{0};
+  WriteOnceData<uint32_t> lazyLinkStubOffset_;
 
   // Thunk to enter the interpreter from JIT code.
-  WriteOnceData<uint32_t> interpreterStubOffset_{0};
+  WriteOnceData<uint32_t> interpreterStubOffset_;
 
   // Thunk to convert the value in R0 to int32 if it's a double.
   // Note: this stub treats -0 as +0 and may clobber R1.scratchReg().
-  WriteOnceData<uint32_t> doubleToInt32ValueStubOffset_{0};
+  WriteOnceData<uint32_t> doubleToInt32ValueStubOffset_;
 
   // Thunk used by the debugger for breakpoint and step mode.
-  using DebugHandlerArray =
-      mozilla::EnumeratedArray<DebugTrapHandlerKind,
-                               DebugTrapHandlerKind::Count,
-                               WriteOnceData<JitCode*>>;
-  DebugHandlerArray debugTrapHandlers_;
+  mozilla::EnumeratedArray<DebugTrapHandlerKind, DebugTrapHandlerKind::Count,
+                           WriteOnceData<JitCode*>>
+      debugTrapHandlers_;
 
   // BaselineInterpreter state.
-  MainThreadData<BaselineInterpreter> baselineInterpreter_;
+  BaselineInterpreter baselineInterpreter_;
 
   // Code for trampolines and VMFunction wrappers.
-  WriteOnceData<JitCode*> trampolineCode_{nullptr};
+  WriteOnceData<JitCode*> trampolineCode_;
 
   // Map VMFunction addresses to the offset of the wrapper in
   // trampolineCode_.
   using VMWrapperMap = HashMap<const VMFunction*, uint32_t, VMFunction>;
-  WriteOnceData<VMWrapperMap*> functionWrappers_{nullptr};
+  WriteOnceData<VMWrapperMap*> functionWrappers_;
 
   // Maps VMFunctionId to the offset of the wrapper code in trampolineCode_.
-  using VMWrapperOffsets =
-      Vector<WriteOnceData<uint32_t>, 0, SystemAllocPolicy>;
+  using VMWrapperOffsets = Vector<uint32_t, 0, SystemAllocPolicy>;
   VMWrapperOffsets functionWrapperOffsets_;
 
   // Maps TailCallVMFunctionId to the offset of the wrapper code in
@@ -219,28 +216,28 @@ class JitRuntime {
   MainThreadData<BaselineICFallbackCode> baselineICFallbackCode_;
 
   // Global table of jitcode native address => bytecode address mappings.
-  UnprotectedData<JitcodeGlobalTable*> jitcodeGlobalTable_{nullptr};
+  UnprotectedData<JitcodeGlobalTable*> jitcodeGlobalTable_;
 
 #ifdef DEBUG
   // The number of possible bailing places encounters before forcefully bailing
   // in that place. Zero means inactive.
-  MainThreadData<uint32_t> ionBailAfter_{false};
+  MainThreadData<uint32_t> ionBailAfter_;
 #endif
 
   // Number of Ion compilations which were finished off thread and are
   // waiting to be lazily linked. This is only set while holding the helper
   // thread state lock, but may be read from at other times.
-  using NumFinishedOffThreadTasksType =
-      mozilla::Atomic<size_t, mozilla::SequentiallyConsistent>;
-  NumFinishedOffThreadTasksType numFinishedOffThreadTasks_{0};
+  typedef mozilla::Atomic<size_t, mozilla::SequentiallyConsistent>
+      NumFinishedOffThreadTasksType;
+  NumFinishedOffThreadTasksType numFinishedOffThreadTasks_;
 
   // List of Ion compilation waiting to get linked.
   using IonCompileTaskList = mozilla::LinkedList<js::jit::IonCompileTask>;
   MainThreadData<IonCompileTaskList> ionLazyLinkList_;
-  MainThreadData<size_t> ionLazyLinkListSize_{0};
+  MainThreadData<size_t> ionLazyLinkListSize_;
 
   // Counter used to help dismbiguate stubs in CacheIR
-  MainThreadData<uint64_t> disambiguationId_{0};
+  MainThreadData<uint64_t> disambiguationId_;
 
 #ifdef DEBUG
   // Flag that can be set from JIT code to indicate it's invalid to call
@@ -298,7 +295,7 @@ class JitRuntime {
   }
 
  public:
-  JitRuntime() = default;
+  JitRuntime();
   ~JitRuntime();
   MOZ_MUST_USE bool initialize(JSContext* cx);
 
@@ -338,9 +335,7 @@ class JitRuntime {
 
   JitCode* debugTrapHandler(JSContext* cx, DebugTrapHandlerKind kind);
 
-  BaselineInterpreter& baselineInterpreter() {
-    return baselineInterpreter_.ref();
-  }
+  BaselineInterpreter& baselineInterpreter() { return baselineInterpreter_; }
 
   TrampolinePtr getGenericBailoutHandler() const {
     return trampolineCode(bailoutHandlerOffset_);
