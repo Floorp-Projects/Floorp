ccbf88b7b0c263bb1f5811478f150719c9e59a5a
André Bargull <andre.bargull@gmail.com>
2020-04-27 12:00:48 +0000

    Bug 1626587 - Part 4: Remove mozilla::IsPod for HashMapEntry. r=jwalden
    The `mozilla::IsPod<HashMapEntry` specialisation was only needed for the static
    assertions in "js/src/frontend/NameCollections.h". Part 3 removed those, so we
    can now also remove `mozilla::IsPod<HashMapEntry`.
    
    Drive-by change:
    - Remove no longer needed "mozilla/PodOperations.h" include in mfbt/HashTable.h.
    - And then fix up previously transitively included files for RootingAPI.h,
      jsfriendapi.h, and Bench.cpp.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69201

--- xpcom/rust/gtest/bench-collections/Bench.cpp
+++ xpcom/rust/gtest/bench-collections/Bench.cpp
@@ -48,6 +48,7 @@
 #include "gtest/gtest.h"
 #include "gtest/MozGTestBench.h"  // For MOZ_GTEST_BENCH
 #include "mozilla/AllocPolicy.h"
+#include "mozilla/ArrayUtils.h"
 #include "mozilla/HashFunctions.h"
 #include "mozilla/HashTable.h"
 #include "mozilla/StaticMutex.h"
