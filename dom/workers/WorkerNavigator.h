c1b1fb409418970ee007a85731b4fb61e00e8d44
Michael Kaply <mozilla@kaply.com>
2020-04-27 20:16:04 +0000

    Bug 1415146 - Use getExposedWebLocales for navigator.language(s) if there are no accept languages. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D59565

--- dom/workers/WorkerNavigator.h
+++ dom/workers/WorkerNavigator.h
@@ -68,11 +68,8 @@ class WorkerNavigator final : public nsWrapperCache {
   bool TaintEnabled() const { return false; }
 
   void GetLanguage(nsString& aLanguage) const {
-    if (mProperties.mLanguages.Length() >= 1) {
-      aLanguage.Assign(mProperties.mLanguages[0]);
-    } else {
-      aLanguage.Truncate();
-    }
+    MOZ_ASSERT(mProperties.mLanguages.Length() >= 1);
+    aLanguage.Assign(mProperties.mLanguages[0]);
   }
 
   void GetLanguages(nsTArray<nsString>& aLanguages) const {
