df46a402d42637bf88e23071520a8ffa238abe26
Lina Cambridge <lina@yakshaving.ninja>
2020-04-27 05:40:52 +0000

    Bug 1626506 - Vendor the `webext_storage` component. r=markh
    Hooray, our first Application Services Rust component! This is a
    mechanical run of `mach vendor rust`, split out into its own commit
    to make reviewing the Firefox bindings easier.
    
    Differential Revision: https://phabricator.services.mozilla.com/D71895

---
+++ third_party/rust/webext-storage/src/lib.rs
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#![allow(unknown_lints)]
+#![warn(rust_2018_idioms)]
+
+mod api;
+pub mod db;
+pub mod error;
+mod schema;
+pub mod store;
+
+// This is what we roughly expect the "bridge" used by desktop to do.
+// It's primarily here to avoid dead-code warnings (but I don't want to disable
+// those warning, as stuff that remains after this is suspect!)
+pub fn delme_demo_usage() -> error::Result<()> {
+    use serde_json::json;
+
+    let store = store::Store::new("webext-storage.db")?;
+    store.set("ext-id", json!({}))?;
+    store.get("ext-id", json!({}))?;
+    store.remove("ext-id", json!({}))?;
+    store.clear("ext-id")?;
+    Ok(())
+}
