c98c1674f6752584fe04c41cf23a57d0ae10bf8d
Byron Campen [:bwc] <docfaraday@gmail.com>
2020-04-27 14:36:39 +0000

    Bug 1629565: Do not transition back to "gathering" if no gathering is needed. r=mjf
    Differential Revision: https://phabricator.services.mozilla.com/D72238

--- media/mtransport/nricectx.cpp
+++ media/mtransport/nricectx.cpp
@@ -866,8 +866,6 @@ nsresult NrIceCtx::StartGathering(bool default_route_only,
 
   obfuscate_host_addresses_ = obfuscate_host_addresses;
 
-  SetGatheringState(ICE_CTX_GATHER_STARTED);
-
   SetCtxFlags(default_route_only);
 
   // This might start gathering for the first time, or again after
@@ -877,7 +875,10 @@ nsresult NrIceCtx::StartGathering(bool default_route_only,
 
   if (!r) {
     SetGatheringState(ICE_CTX_GATHER_COMPLETE);
-  } else if (r != R_WOULDBLOCK) {
+  } else if (r == R_WOULDBLOCK) {
+    SetGatheringState(ICE_CTX_GATHER_STARTED);
+  } else {
+    SetGatheringState(ICE_CTX_GATHER_COMPLETE);
     MOZ_MTLOG(ML_ERROR, "ICE FAILED: Couldn't gather ICE candidates for '"
                             << name_ << "', error=" << r);
     SetConnectionState(ICE_CTX_FAILED);
