a44ab6adc9fb2d872856b13f4522308784cc82f3
André Bargull <andre.bargull@gmail.com>
2020-04-28 16:16:26 +0000

    Bug 1608168 - Part 2: Reimport test262 to reflect changed feature flags. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D70196

--- js/src/tests/test262/built-ins/String/prototype/replaceAll/replaceValue-tostring-abrupt.js
+++ js/src/tests/test262/built-ins/String/prototype/replaceAll/replaceValue-tostring-abrupt.js
@@ -1,4 +1,3 @@
-// |reftest| skip-if(release_or_beta) -- String.prototype.replaceAll is not released yet
 // Copyright (C) 2019 Leo Balter. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
