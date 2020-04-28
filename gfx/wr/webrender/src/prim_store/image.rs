f3a199ac9824a0e0bf235edc9a893b60dbfe0815
Brindusan Cristian <cbrindusan@mozilla.com>
2020-04-28 21:33:46 +0300

    Backed out changeset 8433832c8f09 (bug 1628175) for reftest failures at 817019-1.html. CLOSED TREE

--- gfx/wr/webrender/src/prim_store/image.rs
+++ gfx/wr/webrender/src/prim_store/image.rs
@@ -332,7 +332,6 @@ impl InternablePrimitive for Image {
         PrimitiveInstanceKind::Image {
             data_handle,
             image_instance_index,
-            is_compositor_surface: false,
         }
     }
 }
