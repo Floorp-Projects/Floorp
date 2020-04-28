78efddebd2d124e6d52eb437ec59db71c4f4fcff
Botond Ballo <botond@mozilla.com>
2020-04-28 01:38:08 +0000

    Bug 1556556 - Introduce ViewportType and RelativeTo. r=kats,mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D68916

--- layout/base/nsLayoutUtils.h
+++ layout/base/nsLayoutUtils.h
@@ -132,6 +132,39 @@ enum class DrawStringFlags {
 };
 MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(DrawStringFlags)
 
+// A flag that can be used to annotate a frame to distinguish coordinates
+// relative to the viewport frame as being in layout or visual coordinates.
+enum class ViewportType { Layout, Visual };
+
+// A struct that combines a frame with a ViewportType annotation. The
+// combination completely describes what a set of coordinates is "relative to".
+// Notes on expected usage:
+//  - The boundary between visual and layout coordinates is approximately
+//    at the root content document (RCD)'s ViewportFrame, which we'll
+//    call "RCD-VF".
+//  - Coordinates relative to the RCD-VF's descendants (other than the
+//    RCD's viewport scrollbar frames) should be in layout coordinates.
+//  - Coordinates relative to the RCD-VF's ancestors should be in visual
+//    coordinates (note that in an e10s setup, the RCD-VF doesn't
+//    typically have in-process ancestors).
+//  - Coordinates relative to the RCD-VF itself can be in either layout
+//    or visual coordinates.
+struct RelativeTo {
+  const nsIFrame* mFrame = nullptr;
+  // Choose ViewportType::Layout as the default as this is what the vast
+  // majority of layout code deals with.
+  ViewportType mViewportType = ViewportType::Layout;
+  bool operator==(const RelativeTo& aOther) const {
+    return mFrame == aOther.mFrame && mViewportType == aOther.mViewportType;
+  }
+  friend std::ostream& operator<<(std::ostream& aOs, const RelativeTo& aR) {
+    return aOs << "{" << aR.mFrame << ", "
+               << (aR.mViewportType == ViewportType::Visual ? "visual"
+                                                            : "layout")
+               << "}";
+  }
+};
+
 /**
  * nsLayoutUtils is a namespace class used for various helper
  * functions that are useful in multiple places in layout.  The goal
