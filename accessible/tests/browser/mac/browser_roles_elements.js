9a5610ca7b09ffa358aa2bc84cfd61dbe609016f
Eitan Isaacson <eitan@monotonous.org>
2020-04-28 18:04:00 +0000

    Bug 1625870 - Support figure and figcaption is OSX. r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D72509

--- accessible/tests/browser/mac/browser_roles_elements.js
+++ accessible/tests/browser/mac/browser_roles_elements.js
@@ -27,3 +27,57 @@ addAccessibleTask(`<hr id="hr" />`, (browser, accDoc) => {
     "Subrole for hr is AXContentSeparator"
   );
 });
+
+addAccessibleTask(
+  `
+  <figure id="figure">
+    <img id="img" src="http://example.com/a11y/accessible/tests/mochitest/moz.png" alt="Logo">
+    <p>Non-image figure content</p>
+    <figcaption id="figcaption">Old Mozilla logo</figcaption>
+  </figure>`,
+  (browser, accDoc) => {
+    let figure = getNativeInterface(accDoc, "figure");
+    ok(!figure.getAttributeValue("AXTitle"), "Figure should not have a title");
+    is(
+      figure.getAttributeValue("AXDescription"),
+      "Old Mozilla logo",
+      "Correct figure label"
+    );
+    is(figure.getAttributeValue("AXRole"), "AXGroup", "Correct figure role");
+    is(
+      figure.getAttributeValue("AXRoleDescription"),
+      "figure",
+      "Correct figure role description"
+    );
+
+    let img = getNativeInterface(accDoc, "img");
+    ok(!img.getAttributeValue("AXTitle"), "img should not have a title");
+    is(img.getAttributeValue("AXDescription"), "Logo", "Correct img label");
+    is(img.getAttributeValue("AXRole"), "AXImage", "Correct img role");
+    is(
+      img.getAttributeValue("AXRoleDescription"),
+      "image",
+      "Correct img role description"
+    );
+
+    let figcaption = getNativeInterface(accDoc, "figcaption");
+    ok(
+      !figcaption.getAttributeValue("AXTitle"),
+      "figcaption should not have a title"
+    );
+    ok(
+      !figcaption.getAttributeValue("AXDescription"),
+      "figcaption should not have a label"
+    );
+    is(
+      figcaption.getAttributeValue("AXRole"),
+      "AXGroup",
+      "Correct figcaption role"
+    );
+    is(
+      figcaption.getAttributeValue("AXRoleDescription"),
+      "group",
+      "Correct figcaption role description"
+    );
+  }
+);
