ae12f8e2074e569adb12672983705fa0f36ebdc4
Brindusan Cristian <cbrindusan@mozilla.com>
2020-04-28 21:54:31 +0300

    Backed out changeset b9710246d416 (bug 1625870) for mochitest failures at test_figure.html. CLOSED TREE

--- accessible/html/HTMLFormControlAccessible.cpp
+++ accessible/html/HTMLFormControlAccessible.cpp
@@ -638,7 +638,6 @@ ENameValueFlag HTMLFigureAccessible::NativeName(nsString& aName) const {
   if (captionContent)
     nsTextEquivUtils::AppendTextEquivFromContent(this, captionContent, &aName);
 
-  aName.CompressWhitespace();
   return eNameOK;
 }
 
