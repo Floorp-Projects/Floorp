de11c8a24d85c644848b0e0363a0ffcd49784931
Noemi Erli <nerli@mozilla.com>
2020-04-28 19:55:08 +0300

    .Backed out 3 changesets (bug 1628240) for causing geckoview failures CLOSED TREE
    Backed out changeset 6fc159a53fab (bug 1628240)
    Backed out changeset 6f0ed8b711fd (bug 1628240)
    Backed out changeset 4c685e21c33d (bug 1628240)

--- toolkit/components/cleardata/tests/browser/browser_quota.js
+++ toolkit/components/cleardata/tests/browser/browser_quota.js
@@ -79,36 +79,6 @@ function hasQuotaStorage(origin, attr) {
   return file.exists();
 }
 
-async function runTest(sites, deleteDataFunc) {
-  info(`Adding quota storage`);
-  for (let site of sites) {
-    const principal = getPrincipal(site.origin, site.originAttributes);
-    await addQuotaStorage(principal);
-  }
-
-  info(`Verifying ${deleteDataFunc.name}`);
-  let site;
-  while ((site = sites.shift())) {
-    await new Promise(aResolve => {
-      deleteDataFunc(...site.args, value => {
-        Assert.equal(value, 0);
-        aResolve();
-      });
-    });
-
-    ok(
-      !hasQuotaStorage(site.origin, site.originAttributes),
-      `${site.origin} has no quota storage`
-    );
-    sites.forEach(remainSite =>
-      ok(
-        hasQuotaStorage(remainSite.origin, remainSite.originAttributes),
-        `${remainSite.origin} has quota storage`
-      )
-    );
-  }
-}
-
 add_task(async function setup() {
   await SpecialPowers.pushPrefEnv({
     set: [
@@ -120,101 +90,15 @@ add_task(async function setup() {
 
 const ORG_DOMAIN = "example.com";
 const ORG_ORIGIN = `https://${ORG_DOMAIN}`;
+const ORG_PRINCIPAL = getPrincipal(ORG_ORIGIN);
 const COM_DOMAIN = "example.org";
 const COM_ORIGIN = `https://${COM_DOMAIN}`;
-const LH_DOMAIN = "localhost";
+const COM_PRINCIPAL = getPrincipal(COM_ORIGIN);
 
 add_task(async function test_deleteFromHost() {
-  const sites = [
-    {
-      args: [ORG_DOMAIN, true, Ci.nsIClearDataService.CLEAR_DOM_QUOTA],
-      origin: ORG_ORIGIN,
-    },
-    {
-      args: [COM_DOMAIN, true, Ci.nsIClearDataService.CLEAR_DOM_QUOTA],
-      origin: COM_ORIGIN,
-    },
-    {
-      args: [LH_DOMAIN, true, Ci.nsIClearDataService.CLEAR_DOM_QUOTA],
-      origin: `http://${LH_DOMAIN}:8000`,
-    },
-  ];
+  await addQuotaStorage(ORG_PRINCIPAL);
+  await addQuotaStorage(COM_PRINCIPAL);
 
-  await runTest(sites, Services.clearData.deleteDataFromHost);
-});
-
-add_task(async function test_deleteFromPrincipal() {
-  const sites = [
-    {
-      args: [
-        getPrincipal(ORG_ORIGIN),
-        true,
-        Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
-      ],
-      origin: ORG_ORIGIN,
-    },
-    {
-      args: [
-        getPrincipal(COM_ORIGIN),
-        true,
-        Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
-      ],
-      origin: COM_ORIGIN,
-    },
-  ];
-
-  await runTest(sites, Services.clearData.deleteDataFromPrincipal);
-});
-
-add_task(async function test_deleteFromOriginAttributes() {
-  const ORG_OA = { userContextId: 1 };
-  const COM_OA = { userContextId: 2 };
-  const sites = [
-    {
-      args: [ORG_OA],
-      origin: ORG_ORIGIN,
-      originAttributes: ORG_OA,
-    },
-    {
-      args: [COM_OA],
-      origin: COM_ORIGIN,
-      originAttributes: COM_OA,
-    },
-  ];
-
-  await runTest(
-    sites,
-    Services.clearData.deleteDataFromOriginAttributesPattern
-  );
-});
-
-add_task(async function test_deleteAll() {
-  info(`Adding quota storage`);
-  await addQuotaStorage(getPrincipal(ORG_ORIGIN));
-  await addQuotaStorage(getPrincipal(COM_ORIGIN));
-
-  info(`Verifying deleteData`);
-  await new Promise(aResolve => {
-    Services.clearData.deleteData(
-      Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
-      value => {
-        Assert.equal(value, 0);
-        aResolve();
-      }
-    );
-  });
-
-  ok(!hasQuotaStorage(ORG_ORIGIN), `${ORG_ORIGIN} has no quota storage`);
-  ok(!hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has no quota storage`);
-});
-
-add_task(async function test_deleteSubdomain() {
-  const ANOTHER_ORIGIN = `https://wwww.${ORG_DOMAIN}`;
-  info(`Adding quota storage`);
-  await addQuotaStorage(getPrincipal(ORG_ORIGIN));
-  await addQuotaStorage(getPrincipal(ANOTHER_ORIGIN));
-
-  info(`Verifying deleteDataFromHost for subdomain`);
   await new Promise(aResolve => {
     Services.clearData.deleteDataFromHost(
       ORG_DOMAIN,
@@ -228,5 +112,110 @@ add_task(async function test_deleteSubdomain() {
   });
 
   ok(!hasQuotaStorage(ORG_ORIGIN), `${ORG_ORIGIN} has no quota storage`);
-  ok(!hasQuotaStorage(COM_ORIGIN), `${ANOTHER_ORIGIN} has no quota storage`);
+  ok(hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has quota storage`);
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteDataFromHost(
+      COM_DOMAIN,
+      true,
+      Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(!hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has no quota storage`);
+});
+
+add_task(async function test_deleteFromPrincipal() {
+  await addQuotaStorage(ORG_PRINCIPAL);
+  await addQuotaStorage(COM_PRINCIPAL);
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteDataFromPrincipal(
+      ORG_PRINCIPAL,
+      true,
+      Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(!hasQuotaStorage(ORG_ORIGIN), `${ORG_ORIGIN} has no quota storage`);
+  ok(hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has quota storage`);
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteDataFromPrincipal(
+      COM_PRINCIPAL,
+      true,
+      Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(!hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has no quota storage`);
+});
+
+add_task(async function test_deleteFromOriginAttributes() {
+  await addQuotaStorage(getPrincipal(ORG_ORIGIN, { userContextId: 1 }));
+  await addQuotaStorage(getPrincipal(COM_ORIGIN, { userContextId: 2 }));
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteDataFromOriginAttributesPattern(
+      { userContextId: 1 },
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(
+    !hasQuotaStorage(ORG_ORIGIN, { userContextId: 1 }),
+    `${ORG_ORIGIN} has no quota storage`
+  );
+  ok(
+    hasQuotaStorage(COM_ORIGIN, { userContextId: 2 }),
+    `${COM_ORIGIN} has quota storage`
+  );
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteDataFromOriginAttributesPattern(
+      { userContextId: 2 },
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(
+    !hasQuotaStorage(COM_ORIGIN, { userContextId: 2 }),
+    `${COM_ORIGIN} has no quota storage`
+  );
+});
+
+add_task(async function test_deleteAll() {
+  await addQuotaStorage(ORG_PRINCIPAL);
+  await addQuotaStorage(COM_PRINCIPAL);
+
+  await new Promise(aResolve => {
+    Services.clearData.deleteData(
+      Ci.nsIClearDataService.CLEAR_DOM_QUOTA,
+      value => {
+        Assert.equal(value, 0);
+        aResolve();
+      }
+    );
+  });
+
+  ok(!hasQuotaStorage(ORG_ORIGIN), `${ORG_ORIGIN} has no quota storage`);
+  ok(!hasQuotaStorage(COM_ORIGIN), `${COM_ORIGIN} has no quota storage`);
 });
