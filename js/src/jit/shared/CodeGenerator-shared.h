3dfaa930df4b643fd5a835583460ad0785820e43
André Bargull <andre.bargull@gmail.com>
2020-04-29 15:40:29 +0000

    Bug 1633997 - Part 3: Remove offsetAdjustement from MLoadElement, MStoreElement, and MStoreUnboxedScalar. r=jandem
    Depends on D73019
    
    Differential Revision: https://phabricator.services.mozilla.com/D73020

--- js/src/jit/shared/CodeGenerator-shared.h
+++ js/src/jit/shared/CodeGenerator-shared.h
@@ -271,8 +271,7 @@ class CodeGeneratorShared : public LElementVisitor {
   void emitTruncateFloat32(FloatRegister src, Register dest,
                            MTruncateToInt32* mir);
 
-  void emitPreBarrier(Register elements, const LAllocation* index,
-                      int32_t offsetAdjustment);
+  void emitPreBarrier(Register elements, const LAllocation* index);
   void emitPreBarrier(Address address);
 
   // We don't emit code for trivial blocks, so if we want to branch to the
