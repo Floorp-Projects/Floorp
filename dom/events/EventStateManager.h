292672b3ac7d5095c928f80d5db717ef0b292356
Edgar Chen <echen@mozilla.com>
2020-04-29 11:58:14 +0000

    Bug 1634017 - Pass BrowserParent, instead of nsFrameLoader, to EventStateManager::DispatchCrossProcessEvent; r=smaug
    We currently pass nsFrameLoader to DispatchCrossProcessEvent and then try to get
    BrowserParent from nsFrameLoader, but the call site actually ensures the
    passed nsFrameLoader is a remote target that could get BrowserParent on it.
    So we could simplify this by passing BrowserParent to DispatchCrossProcessEvent
    and also could get rid of unnecessary conversion code in
    EventStateManager::HandleCrossProcessEvent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73037

--- dom/events/EventStateManager.h
+++ dom/events/EventStateManager.h
@@ -1104,7 +1104,8 @@ class EventStateManager : public nsSupportsWeakReference, public nsIObserver {
    * If you need to check if the event is posted to a remote process, you
    * can use aEvent->HasBeenPostedToRemoteProcess().
    */
-  void DispatchCrossProcessEvent(WidgetEvent* aEvent, nsFrameLoader* aRemote,
+  void DispatchCrossProcessEvent(WidgetEvent* aEvent,
+                                 dom::BrowserParent* aRemoteTarget,
                                  nsEventStatus* aStatus);
   /**
    * HandleCrossProcessEvent() may post aEvent to target remote processes.
