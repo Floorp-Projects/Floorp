392d6d89800b5fc7e82d23c2321e187bbab2ce93
André Bargull <andre.bargull@gmail.com>
2020-04-29 10:35:29 +0000

    Bug 1633386: Simplify flexible shift instructions. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D72646

--- js/src/jsapi-tests/testJitMacroAssembler.cpp
+++ js/src/jsapi-tests/testJitMacroAssembler.cpp
@@ -241,7 +241,7 @@ bool shiftTest(JSContext* cx, const char* name,
       Register rhs = rightHandSides.takeAny();
 
       // You can only use shift as the same reg if the values are the same
-      if (lhsOutput == rhs && lhsInput != rhsInput) {
+      if (lhsOutput == rhs && *lhsInput != *rhsInput) {
         continue;
       }
 
@@ -256,9 +256,11 @@ bool shiftTest(JSContext* cx, const char* name,
       masm.branch32(Assembler::NotEqual, AbsoluteAddress(result), lhsOutput,
                     &outputFail);
 
-      // Ensure RHS was not clobbered
-      masm.branch32(Assembler::NotEqual, AbsoluteAddress(rhsInput), rhs,
-                    &clobberRhs);
+      // Ensure RHS was not clobbered, unless it's also the output register.
+      if (lhsOutput != rhs) {
+        masm.branch32(Assembler::NotEqual, AbsoluteAddress(rhsInput), rhs,
+                      &clobberRhs);
+      }
 
       if (lhsOutput != ecx && rhs != ecx) {
         // If neither lhsOutput nor rhs is ecx, make sure ecx has been
