0ed1e619fd7f433ccf0d94720770b84ef56dade8
Mike Hommey <mh+mozilla@glandium.org>
2020-04-29 23:05:29 +0000

    Bug 1634187 - Turn xpcom/idl-parser/xpidl into a proper python 3-ready module. r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D73151

--- xpcom/idl-parser/xpidl/runtests.py
+++ xpcom/idl-parser/xpidl/runtests.py
@@ -5,10 +5,12 @@
 #
 # Unit tests for xpidl.py
 
+from __future__ import absolute_import
+
 import mozunit
 import unittest
-import xpidl
-import header
+from xpidl import xpidl
+from xpidl import header
 
 
 class TestParser(unittest.TestCase):
