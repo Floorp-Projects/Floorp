156e87aca88f028d5868aeda5a5d7b7c0034b5e0
Rob Wu <rob@robwu.nl>
2020-04-29 23:08:18 +0000

    Bug 1620621 - Unit tests for MLBF-based blocklist r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D72631

--- toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/head.js
+++ toolkit/mozapps/extensions/test/xpcshell/rs-blocklist/head.js
@@ -1,2 +1,14 @@
 // Appease eslint.
 /* import-globals-from ../head_addons.js */
+
+const MLBF_RECORD = {
+  id: "A blocklist entry that refers to a MLBF file",
+  last_modified: 1,
+  attachment: {
+    size: 32,
+    hash: "6af648a5d6ce6dbee99b0aab1780d24d204977a6606ad670d5372ef22fac1052",
+    filename: "does-not-matter.bin",
+  },
+  attachment_type: "bloomfilter-full",
+  generation_time: 1577833200000,
+};
