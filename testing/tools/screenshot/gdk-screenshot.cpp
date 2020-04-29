74254b71ffd6dd7e26902bb33461bd07dd3c9b03
Martin Stransky <stransky@redhat.com>
2020-04-29 10:50:32 +0000

    Bug 1621911 [Wayland] provide gdk-screenshot.cpp for Wayland desktops, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D73014

--- testing/tools/screenshot/gdk-screenshot.cpp
+++ testing/tools/screenshot/gdk-screenshot.cpp
@@ -39,6 +39,13 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#if defined(MOZ_ENABLE_DBUS)
+#  include <glib.h>
+#  include <glib/gstdio.h>
+#  include <dlfcn.h>
+#endif
 
 gboolean save_to_stdout(const gchar* buf, gsize count, GError** error,
                         gpointer data) {
@@ -52,14 +59,70 @@ gboolean save_to_stdout(const gchar* buf, gsize count, GError** error,
   return TRUE;
 }
 
+#if defined(MOZ_ENABLE_DBUS)
+static GdkPixbuf* get_screenshot_dbus() {
+  char *path = nullptr, *filename = nullptr, *tmpname = nullptr;
+  GdkPixbuf* screenshot = nullptr;
+  const char* method_name;
+  GVariant* method_params;
+  GDBusConnection* connection;
+
+  auto sGApplicationGetDbusConnection = (GDBusConnection * (*)(GApplication*))
+      dlsym(RTLD_DEFAULT, "g_application_get_dbus_connection");
+  if (!sGApplicationGetDbusConnection) {
+    return nullptr;
+  }
+
+  path =
+      g_build_filename(g_get_user_cache_dir(), "mozilla-screenshot", nullptr);
+  g_mkdir_with_parents(path, 0700);
+
+  tmpname = g_strdup_printf("mozilla-screen-%d.png", g_random_int());
+  filename = g_build_filename(path, tmpname, nullptr);
+
+  method_name = "Screenshot";
+  method_params = g_variant_new("(bbs)", FALSE, /* include pointer */
+                                FALSE,          /* flash */
+                                filename);
+
+  GApplication* app = g_application_new(nullptr, G_APPLICATION_NON_UNIQUE);
+  g_application_register(app, nullptr, nullptr);
+  connection = sGApplicationGetDbusConnection(app);
+  if (g_dbus_connection_call_sync(
+          connection, "org.gnome.Shell.Screenshot",
+          "/org/gnome/Shell/Screenshot", "org.gnome.Shell.Screenshot",
+          method_name, method_params, nullptr, G_DBUS_CALL_FLAGS_NONE, -1,
+          nullptr, nullptr)) {
+    screenshot = gdk_pixbuf_new_from_file(filename, nullptr);
+    unlink(filename);
+  }
+
+  g_free(path);
+  g_free(tmpname);
+  g_free(filename);
+
+  return screenshot;
+}
+#endif
+
 int main(int argc, char** argv) {
   gdk_init(&argc, &argv);
 
   GdkPixbuf* screenshot = nullptr;
-  GdkWindow* window = gdk_get_default_root_window();
-  screenshot =
-      gdk_pixbuf_get_from_window(window, 0, 0, gdk_window_get_width(window),
-                                 gdk_window_get_height(window));
+
+#if defined(MOZ_ENABLE_DBUS)
+  GdkDisplay* display = gdk_display_get_default();
+  if (display && !GDK_IS_X11_DISPLAY(display)) {
+    screenshot = get_screenshot_dbus();
+  }
+#endif
+  if (!screenshot) {
+    GdkWindow* window = gdk_get_default_root_window();
+    screenshot =
+        gdk_pixbuf_get_from_window(window, 0, 0, gdk_window_get_width(window),
+                                   gdk_window_get_height(window));
+  }
+
   if (!screenshot) {
     fprintf(stderr, "%s: failed to create screenshot GdkPixbuf\n", argv[0]);
     return 1;
