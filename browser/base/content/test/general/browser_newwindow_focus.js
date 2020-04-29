a50346320bf3c864710ef69c002fe3ed394fec37
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-04-29 11:12:53 +0000

    Bug 1630211 - P3. Use new domWindowOpenedAndLoaded where we can. r=Gijs
    Remove duplicated code; and properly wait for the window to be loaded where needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72879

--- browser/base/content/test/general/browser_newwindow_focus.js
+++ browser/base/content/test/general/browser_newwindow_focus.js
@@ -8,35 +8,6 @@
 const PAGE = `data:text/html,<a id="target" href="%23" onclick="window.open('http://www.example.com', '_blank', 'width=100,height=100');">Click me</a>`;
 
 /**
- * Returns a Promise that resolves when a new window has
- * opened, and the "load" event has fired in that window.
- * We can't use BrowserTestUtils.domWindowOpened directly,
- * because by the time the "then" on the Promise runs,
- * DOMContentLoaded and load may have already run in the new
- * window. However, we want to be very explicit about what
- * events we're waiting for, and not rely on a quirk of our
- * Promises infrastructure.
- */
-function promiseNewWindow() {
-  return new Promise(resolve => {
-    let observer = (win, topic, data) => {
-      if (topic == "domwindowopened") {
-        Services.ww.unregisterNotification(observer);
-        win.addEventListener(
-          "load",
-          function() {
-            resolve(win);
-          },
-          { once: true }
-        );
-      }
-    };
-
-    Services.ww.registerNotification(observer);
-  });
-}
-
-/**
  * Test that when a new window is opened from content, focus moves
  * to the initial browser in that window once the window has finished
  * painting.
@@ -48,7 +19,7 @@ add_task(async function test_focus_browser() {
       gBrowser,
     },
     async function(browser) {
-      let newWinPromise = promiseNewWindow();
+      let newWinPromise = BrowserTestUtils.domWindowOpenedAndLoaded(null);
       let delayedStartupPromise = BrowserTestUtils.waitForNewWindow();
 
       await BrowserTestUtils.synthesizeMouseAtCenter("#target", {}, browser);
@@ -88,7 +59,7 @@ add_task(async function test_no_steal_focus() {
       gBrowser,
     },
     async function(browser) {
-      let newWinPromise = promiseNewWindow();
+      let newWinPromise = BrowserTestUtils.domWindowOpenedAndLoaded(null);
       let delayedStartupPromise = BrowserTestUtils.waitForNewWindow();
 
       await BrowserTestUtils.synthesizeMouseAtCenter("#target", {}, browser);
