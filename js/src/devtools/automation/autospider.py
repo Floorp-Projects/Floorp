243437c8db8e566c3d996aafe4d37ef6a5c554ee
Ricky Stewart <rstewart@mozilla.com>
2020-04-30 15:23:51 +0000

    Bug 1633039 - Don't check for Python 2 in configure r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D72895

--- js/src/devtools/automation/autospider.py
+++ js/src/devtools/automation/autospider.py
@@ -580,7 +580,7 @@ if args.variant == 'msan':
 
 # Generate stacks from minidumps.
 if use_minidump:
-    venv_python = os.path.join(OBJDIR, "_virtualenvs", "init", "bin", "python")
+    venv_python = os.path.join(OBJDIR, "_virtualenvs", "init_py3", "bin", "python3")
     run_command([
         venv_python,
         os.path.join(DIR.source, "testing/mozbase/mozcrash/mozcrash/mozcrash.py"),
