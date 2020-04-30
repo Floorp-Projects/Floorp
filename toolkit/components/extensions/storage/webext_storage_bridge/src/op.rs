78a3555485da8e1e07796246506cccc7808344ac
Mark Hammond <mhammond@skippinet.com.au>
2020-04-30 08:16:26 +0000

    Bug 1634257 - expose wipe_all to mozIExtensionStorageArea. r=lina
    Differential Revision: https://phabricator.services.mozilla.com/D73191

--- toolkit/components/extensions/storage/webext_storage_bridge/src/op.rs
+++ toolkit/components/extensions/storage/webext_storage_bridge/src/op.rs
@@ -34,6 +34,8 @@ pub enum StorageOp {
     Remove { ext_id: String, keys: JsonValue },
     /// Clear all keys and values for an extension.
     Clear { ext_id: String },
+    /// Clear all keys and values for all extensions.
+    WipeAll,
 }
 
 impl StorageOp {
@@ -45,6 +47,7 @@ impl StorageOp {
             StorageOp::Set { .. } => "webext_storage::set",
             StorageOp::Remove { .. } => "webext_storage::remove",
             StorageOp::Clear { .. } => "webext_storage::clear",
+            StorageOp::WipeAll => "webext_storage::wipe_all",
         }
     }
 }
@@ -142,6 +145,10 @@ impl StorageTask {
             }
             StorageOp::Clear { ext_id } => {
                 StorageResult::with_changes(self.store()?.get()?.clear(&ext_id)?)
+            }
+            StorageOp::WipeAll => {
+                self.store()?.get()?.wipe_all()?;
+                Ok(StorageResult::default())
             }
         }?)
     }
