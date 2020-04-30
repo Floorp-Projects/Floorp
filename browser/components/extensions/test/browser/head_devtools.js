257084d3fa47d286dbd9c0026ea15cc4584ed552
Shane Caraveo <scaraveo@mozilla.com>
2020-04-30 01:50:34 +0000

    Bug 1606862 support devtools as an optional extension permission r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D71829

--- browser/components/extensions/test/browser/head_devtools.js
+++ browser/components/extensions/test/browser/head_devtools.js
@@ -3,7 +3,7 @@
 "use strict";
 
 /* exported openToolboxForTab, closeToolboxForTab, getToolboxTargetForTab,
-            registerBlankToolboxPanel, TOOLBOX_BLANK_PANEL_ID */
+            registerBlankToolboxPanel, TOOLBOX_BLANK_PANEL_ID, assertDevToolsExtensionEnabled */
 
 ChromeUtils.defineModuleGetter(
   this,
@@ -18,6 +18,12 @@ XPCOMUtils.defineLazyGetter(this, "TargetFactory", () => {
   const { TargetFactory } = loader.require("devtools/client/framework/target");
   return TargetFactory;
 });
+
+ChromeUtils.defineModuleGetter(
+  this,
+  "DevToolsShim",
+  "chrome://devtools-startup/content/DevToolsShim.jsm"
+);
 
 const TOOLBOX_BLANK_PANEL_ID = "testBlankPanel";
 
@@ -85,4 +91,14 @@ async function closeToolboxForTab(tab) {
       outerWindowID,
     })}`
   );
+}
+
+function assertDevToolsExtensionEnabled(uuid, enabled) {
+  for (let toolbox of DevToolsShim.getToolboxes()) {
+    is(
+      enabled,
+      !!toolbox.isWebExtensionEnabled(uuid),
+      `extension is ${enabled ? "enabled" : "disabled"} on toolbox`
+    );
+  }
 }
