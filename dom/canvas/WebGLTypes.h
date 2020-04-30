85dde34bb1f5d3cec0791c7f9f3cd596365f2257
David Parks <daparks@mozilla.com>
2020-04-30 01:19:36 +0000

    Bug 1621762 - Part 2: Refactor WebGL MethodDispatcher code used in remoting r=jgilbert
    * Remove CommandSink from WebGLMethodDispatcher.  The parameter is unneeded.
    * Make MethodDispatcher code simpler.  This removes a layer of complexity (CommandDispatchDriver) involved in finding the correct method for a given ID number.
    * Require WebGL methods whose signatures don't allow them to be remoted to directly call on the host object.  They are: ReadPixels, GetVRFrame, GetInternalFormatParameter, GetBufferSubData, GetLinkResult and TexImage.
    * Fix a bunch of compilation issues with busted PcqParamTraits.
    * implement PcqParamTraits for some missing types
    
    Differential Revision: https://phabricator.services.mozilla.com/D68257

--- dom/canvas/WebGLTypes.h
+++ dom/canvas/WebGLTypes.h
@@ -94,12 +94,9 @@ inline auto AutoAssertCast(const From val) {
   return detail::AutoAssertCastT<From>(val);
 }
 
-namespace ipc {
+namespace webgl {
 template <typename T>
 struct QueueParamTraits;
-}
-
-namespace webgl {
 class TexUnpackBytes;
 class TexUnpackImage;
 class TexUnpackSurface;
@@ -733,7 +730,7 @@ class RawBuffer {
   // true if we should delete[] the mData on destruction
   bool mOwnsData = false;
 
-  friend mozilla::ipc::QueueParamTraits<RawBuffer>;
+  friend struct mozilla::webgl::QueueParamTraits<RawBuffer<T>>;
 
  public:
   using ElementType = T;
