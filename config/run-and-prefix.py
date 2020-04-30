bb4e86d85ad765b5ff96138653e4b3c3f8bf10cd
Ricky Stewart <rstewart@mozilla.com>
2020-04-30 15:25:22 +0000

    Bug 1633016 - Remove a bunch of references to PYTHON(2) in Makefiles r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D72479

--- config/run-and-prefix.py
+++ config/run-and-prefix.py
@@ -6,23 +6,22 @@
 # This script runs a process and prefixes its output with.
 # Usage: run-and-prefix.py prefix command arg0 argv1...
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, print_function, unicode_literals
 
 import os
 import subprocess
 import sys
 
-sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
+sys.stderr = os.fdopen(sys.stderr.fileno(), 'wb', 0)
 
-prefix = sys.argv[1]
+prefix = sys.argv[1].encode('utf-8')
 args = sys.argv[2:]
 
 p = subprocess.Popen(args, bufsize=0,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.STDOUT,
-                     stdin=sys.stdin.fileno(),
-                     universal_newlines=True)
+                     stdin=sys.stdin.fileno())
 
 while True:
     data = p.stdout.readline()
@@ -30,6 +29,6 @@ while True:
     if data == b'':
         break
 
-    print('%s> %s' % (prefix, data), end=b'')
+    sys.stdout.write(b'%s> %s' % (prefix, data))
 
 sys.exit(p.wait())
