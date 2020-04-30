362595155365524bb65c8ce00c23b359ec370e09
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-04-30 13:53:01 +0000

    Bug 1633866 - [taskgraph.optimize] Move logic to query 'bugbug' service to a utility file, r=marco
    With dynamic-test-selection, we'll also need to query the bugbug service from
    the transforms. Let's move the querying logic to a utility file to share it
    more easily.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73088

---
+++ taskcluster/taskgraph/util/bugbug.py
@@ -0,0 +1,63 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import json
+import logging
+import time
+
+import requests
+from mozbuild.util import memoize
+
+from taskgraph.util.taskcluster import requests_retry_session
+
+logger = logging.getLogger(__name__)
+
+BUGBUG_BASE_URL = "https://bugbug.herokuapp.com"
+RETRY_TIMEOUT = 4 * 60  # seconds
+RETRY_INTERVAL = 5      # seconds
+
+# Preset confidence thresholds.
+CT_LOW = 0.5
+CT_MEDIUM = 0.7
+CT_HIGH = 0.9
+
+
+class BugbugTimeoutException(Exception):
+    pass
+
+
+@memoize
+def get_session():
+    s = requests.Session()
+    s.headers.update({'X-API-KEY': 'gecko-taskgraph'})
+    return requests_retry_session(retries=5, session=s)
+
+
+@memoize
+def push_schedules(branch, rev):
+    url = BUGBUG_BASE_URL + '/push/{branch}/{rev}/schedules'.format(branch=branch, rev=rev)
+
+    session = get_session()
+    attempts = RETRY_TIMEOUT / RETRY_INTERVAL
+    i = 0
+    while i < attempts:
+        r = session.get(url)
+        r.raise_for_status()
+
+        if r.status_code != 202:
+            break
+
+        time.sleep(RETRY_INTERVAL)
+        i += 1
+
+    data = r.json()
+    logger.debug("Bugbug scheduler service returns:\n{}".format(
+                 json.dumps(data, indent=2)))
+
+    if r.status_code == 202:
+        raise BugbugTimeoutException("Timed out waiting for result from '{}'".format(url))
+
+    return data
