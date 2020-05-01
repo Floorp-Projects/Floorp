6f36a3fdb879f0eff60d5ce716ae655835c9b3d2
Gerald Squelart <gsquelart@mozilla.com>
2020-05-01 22:12:22 +0000

    Bug 1609907 - Remove AUTO_PROFILER_THREAD_SLEEP before mozilla::CondVar waits - r=mstange
    CondVar already calls `AUTO_PROFILER_THREAD_SLEEP`, which shouldn't be called recursively.
    
    mozilla::Monitor also uses CondVar, so it shouldn't be surrounded by `AUTO_PROFILER_THREAD_SLEEP` either.
    
    Depends on D72850
    
    Differential Revision: https://phabricator.services.mozilla.com/D72851

--- dom/storage/StorageDBThread.cpp
+++ dom/storage/StorageDBThread.cpp
@@ -480,7 +480,6 @@ void StorageDBThread::ThreadFunc() {
       }
     } else if (MOZ_UNLIKELY(!mStopIOThread)) {
       AUTO_PROFILER_LABEL("StorageDBThread::ThreadFunc::Wait", IDLE);
-      AUTO_PROFILER_THREAD_SLEEP;
       lockMonitor.Wait(timeUntilFlush);
     }
   }  // thread loop
