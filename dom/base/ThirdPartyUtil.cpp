c0a2e3e93adcb9e1c602d05b4170d76018aa2e1a
Ciure Andrei <aciure@mozilla.com>
2020-05-01 23:43:02 +0300

    Backed out 2 changesets (bug 1629436) for causing test_chrome_ext_trackingprotection.html failures CLOSED TREE
    Backed out changeset 18d21d5f63dd (bug 1629436)
    Backed out changeset d14b4bb75db7 (bug 1629436)

--- dom/base/ThirdPartyUtil.cpp
+++ dom/base/ThirdPartyUtil.cpp
@@ -351,22 +351,23 @@ ThirdPartyUtil::IsThirdPartyChannel(nsIChannel* aChannel, nsIURI* aURI,
   if (NS_FAILED(rv)) return rv;
 
   if (!doForce) {
-    nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
-    parentIsThird = loadInfo->GetIsInThirdPartyContext();
-    BasePrincipal* loadingPrincipal =
-        BasePrincipal::Cast(loadInfo->GetLoadingPrincipal());
-    if (!parentIsThird &&
-        loadInfo->GetExternalContentPolicyType() !=
-            nsIContentPolicy::TYPE_DOCUMENT &&
-        (!loadingPrincipal->AddonPolicy() ||
-         !loadingPrincipal->AddonAllowsLoad(channelURI))) {
-      // Check if the channel itself is third-party to its own requestor.
-      // Unforunately, we have to go through the loading principal.
+    if (nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo()) {
+      parentIsThird = loadInfo->GetIsInThirdPartyContext();
+      if (!parentIsThird && loadInfo->GetExternalContentPolicyType() !=
+                                nsIContentPolicy::TYPE_DOCUMENT) {
+        // Check if the channel itself is third-party to its own requestor.
+        // Unforunately, we have to go through the loading principal.
 
-      rv = loadingPrincipal->IsThirdPartyURI(channelURI, &parentIsThird);
-      if (NS_FAILED(rv)) {
-        return rv;
+        rv = loadInfo->GetLoadingPrincipal()->IsThirdPartyURI(channelURI,
+                                                              &parentIsThird);
+        if (NS_FAILED(rv)) {
+          return rv;
+        }
       }
+    } else {
+      NS_WARNING(
+          "Found channel with no loadinfo, assuming third-party request");
+      parentIsThird = true;
     }
   }
 
