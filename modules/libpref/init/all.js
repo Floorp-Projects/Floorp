d8c8b2edc96ceb724d9ca6ba97c6228c13460cdc
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-05-04 00:25:15 +0300

    Backed out changeset 48e305833f6a (bug 1625803) for mda failures on test_peerConnection_basicAudioVideoVerifyExtmap.html . CLOSED TREE

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -421,11 +421,7 @@ pref("media.videocontrols.picture-in-picture.video-toggle.min-video-secs", 45);
   pref("media.navigator.video.enabled", true);
   pref("media.navigator.video.default_fps",30);
   pref("media.navigator.video.use_remb", true);
-  #ifdef EARLY_BETA_OR_EARLIER
-    pref("media.navigator.video.use_transport_cc", true);
-  #else
-    pref("media.navigator.video.use_transport_cc", false);
-  #endif
+  pref("media.navigator.video.use_transport_cc", false);
   pref("media.navigator.video.use_tmmbr", false);
   pref("media.navigator.audio.use_fec", true);
   pref("media.navigator.video.red_ulpfec_enabled", false);
