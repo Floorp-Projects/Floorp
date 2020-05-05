d713c54e7d44fa6ba8ef8f3d4b7d7996af8eba07
Agi Sferro <agi@sferro.dev>
2020-05-05 22:19:30 +0000

    Bug 1601067 - Make the test-support extension installable. r=owlish,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D72980

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/rule/GeckoSessionTestRule.java
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/rule/GeckoSessionTestRule.java
@@ -1098,6 +1098,9 @@ public class GeckoSessionTestRule implements TestRule {
     }
 
     protected void prepareSession(final GeckoSession session) {
+        UiThreadUtils.waitForCondition(() ->
+                        RuntimeCreator.sTestSupport.get() != RuntimeCreator.TEST_SUPPORT_INITIAL,
+                env.getDefaultTimeoutMillis());
         session.getWebExtensionController()
                 .setMessageDelegate(RuntimeCreator.sTestSupportExtension,
                                     mMessageDelegate,
@@ -1202,14 +1205,20 @@ public class GeckoSessionTestRule implements TestRule {
         WebExtensionController controller = getRuntime().getWebExtensionController();
         List<WebExtension> list = waitForResult(controller.list());
 
+        boolean hasTestSupport = false;
         // Uninstall any left-over extensions
         for (WebExtension extension : list) {
-            waitForResult(controller.uninstall(extension));
+            if (!extension.id.equals(RuntimeCreator.TEST_SUPPORT_EXTENSION_ID)) {
+                waitForResult(controller.uninstall(extension));
+            } else {
+                hasTestSupport = true;
+            }
         }
 
-        // If an extension was still installed, this test should fail
+        // If an extension was still installed, this test should fail.
+        // Note the test support extension is always kept for speed.
         assertThat("A WebExtension was left installed during this test.",
-                list.size(), equalTo(0));
+                list.size(), equalTo(hasTestSupport ? 1 : 0));
     }
 
     protected void cleanupStatement() throws Throwable {
