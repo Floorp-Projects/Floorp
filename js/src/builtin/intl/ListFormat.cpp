d71e7542f479c1c35676383414b22275ec0a95a3
André Bargull <andre.bargull@gmail.com>
2020-05-05 09:21:48 +0000

    Bug 1589095 - Part 1: Call new ICU API for list formatters to set style/width options. r=jwalden
    `ulistfmt_openForType` is a draft API, so it may not be available when compiling
    against a system ICU. Instead of disabling `Intl.ListFormat` completely, this
    patch adds a boolean parameter to `InitializeListFormat` to conditionally enable
    the "type" and "style" options when ICU draft APIs are available.
    
    Differential Revision: https://phabricator.services.mozilla.com/D72916

--- js/src/builtin/intl/ListFormat.cpp
+++ js/src/builtin/intl/ListFormat.cpp
@@ -29,6 +29,7 @@
 #include "vm/PlainObject.h"  // js::PlainObject
 #include "vm/Runtime.h"      // js::ReportAllocationOverflow
 #include "vm/SelfHosting.h"
+#include "vm/Stack.h"
 #include "vm/StringType.h"
 
 #include "vm/JSObject-inl.h"
@@ -99,6 +100,36 @@ const ClassSpec ListFormatObject::classSpec_ = {
     nullptr,
     ClassSpec::DontDefineConstructor};
 
+enum class ListFormatOptions {
+  SupportsTypeAndStyle,
+  NoTypeAndStyle,
+};
+
+/**
+ * Initialize a new Intl.ListFormat object using the named self-hosted function.
+ */
+static bool InitializeListFormatObject(JSContext* cx, HandleObject obj,
+                                       HandlePropertyName initializer,
+                                       HandleValue locales, HandleValue options,
+                                       ListFormatOptions lfoptions) {
+  FixedInvokeArgs<4> args(cx);
+
+  args[0].setObject(*obj);
+  args[1].set(locales);
+  args[2].set(options);
+  args[3].setBoolean(lfoptions == ListFormatOptions::SupportsTypeAndStyle);
+
+  RootedValue ignored(cx);
+  if (!CallSelfHostedFunction(cx, initializer, JS::NullHandleValue, args,
+                              &ignored)) {
+    return false;
+  }
+
+  MOZ_ASSERT(ignored.isUndefined(),
+             "Unexpected return value from non-legacy Intl object initializer");
+  return true;
+}
+
 /**
  * Intl.ListFormat([ locales [, options]])
  */
@@ -126,9 +157,18 @@ static bool ListFormat(JSContext* cx, unsigned argc, Value* vp) {
   HandleValue locales = args.get(0);
   HandleValue options = args.get(1);
 
+  constexpr ListFormatOptions lfoptions =
+#ifndef U_HIDE_DRAFT_API
+      ListFormatOptions::SupportsTypeAndStyle
+#else
+      ListFormatOptions::NoTypeAndStyle
+#endif
+      ;
+
   // Step 3.
-  if (!intl::InitializeObject(cx, listFormat, cx->names().InitializeListFormat,
-                              locales, options)) {
+  if (!InitializeListFormatObject(cx, listFormat,
+                                  cx->names().InitializeListFormat, locales,
+                                  options, lfoptions)) {
     return false;
   }
 
@@ -221,16 +261,47 @@ static UListFormatter* NewUListFormatter(JSContext* cx,
     }
   }
 
-  // We're currently only supporting "conjunctive-long" list formatters due to
-  // missing ICU APIs: https://unicode-org.atlassian.net/browse/ICU-12863
+  UErrorCode status = U_ZERO_ERROR;
+  UListFormatter* lf;
+
+#ifndef U_HIDE_DRAFT_API
+  UListFormatterType utype;
+  switch (type) {
+    case ListFormatType::Conjunction:
+      utype = ULISTFMT_TYPE_AND;
+      break;
+    case ListFormatType::Disjunction:
+      utype = ULISTFMT_TYPE_OR;
+      break;
+    case ListFormatType::Unit:
+      utype = ULISTFMT_TYPE_UNITS;
+      break;
+  }
+
+  UListFormatterWidth uwidth;
+  switch (style) {
+    case ListFormatStyle::Long:
+      uwidth = ULISTFMT_WIDTH_WIDE;
+      break;
+    case ListFormatStyle::Short:
+      uwidth = ULISTFMT_WIDTH_SHORT;
+      break;
+    case ListFormatStyle::Narrow:
+      uwidth = ULISTFMT_WIDTH_NARROW;
+      break;
+  }
+
+  lf = ulistfmt_openForType(IcuLocale(locale.get()), utype, uwidth, &status);
+#else
   MOZ_ASSERT(type == ListFormatType::Conjunction);
   MOZ_ASSERT(style == ListFormatStyle::Long);
 
   mozilla::Unused << type;
   mozilla::Unused << style;
 
-  UErrorCode status = U_ZERO_ERROR;
-  UListFormatter* lf = ulistfmt_open(IcuLocale(locale.get()), &status);
+  lf = ulistfmt_open(IcuLocale(locale.get()), &status);
+#endif
+
   if (U_FAILURE(status)) {
     intl::ReportInternalError(cx);
     return nullptr;
--- js/src/builtin/intl/ListFormat.js
+++ js/src/builtin/intl/ListFormat.js
@@ -86,7 +86,7 @@ function getListFormatInternals(obj) {
  * This later work occurs in |resolveListFormatInternals|; steps not noted
  * here occur there.
  */
-function InitializeListFormat(listFormat, locales, options) {
+function InitializeListFormat(listFormat, locales, options, supportsTypeAndStyle) {
     assert(IsObject(listFormat), "InitializeListFormat called with non-object");
     assert(GuardToListFormat(listFormat) !== null, "InitializeListFormat called with non-ListFormat");
 
@@ -128,15 +128,19 @@ function InitializeListFormat(listFormat, locales, options) {
 
     // Compute formatting options.
 
-    // We're currently only supporting "conjunctive-long" list formatters due to
-    // missing ICU APIs: https://unicode-org.atlassian.net/browse/ICU-12863
+    // Supporting all "type" and "style" options requires draft APIs in ICU 67,
+    // which may not be available when compiling against a system ICU.
 
     // Steps 12-13.
-    var type = GetOption(options, "type", "string", ["conjunction"], "conjunction");
+    var type = GetOption(options, "type", "string",
+                         supportsTypeAndStyle ? ["conjunction", "disjunction", "unit"] : ["conjunction"],
+                         "conjunction");
     lazyListFormatData.type = type;
 
     // Steps 14-15.
-    var style = GetOption(options, "style", "string", ["long"], "long");
+    var style = GetOption(options, "style", "string",
+                          supportsTypeAndStyle ? ["long", "short", "narrow"] : ["long"],
+                          "long");
     lazyListFormatData.style = style;
 
     // We've done everything that must be done now: mark the lazy data as fully
