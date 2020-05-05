85f9392bc8372af77928214c1e0513058306e130
Ian Moody <moz-ian@perix.co.uk>
2020-05-05 15:00:50 +0000

    Bug 1536556 - Add custom no-throw-cr-literal ESLint rule, and enable it by default. r=Standard8
    This rule is based on the ESLint built-in no-throw-literal. Cr.ERRORs are also
    literals since they are just integers and so have all the same disadvantages of
    no stack info.
    
    TestInterfaceJS.js is explicitly testing handling of throwing raw Cr.ERRORs and
    thus needs to stay.
    
    Differential Revision: https://phabricator.services.mozilla.com/D28072

--- tools/lint/eslint/eslint-plugin-mozilla/lib/index.js
+++ tools/lint/eslint/eslint-plugin-mozilla/lib/index.js
@@ -48,6 +48,7 @@ module.exports = {
     "no-compare-against-boolean-literals": require("../lib/rules/no-compare-against-boolean-literals"),
     "no-define-cc-etc": require("../lib/rules/no-define-cc-etc"),
     "no-task": require("../lib/rules/no-task"),
+    "no-throw-cr-literal": require("../lib/rules/no-throw-cr-literal"),
     "no-useless-parameters": require("../lib/rules/no-useless-parameters"),
     "no-useless-removeEventListener": require("../lib/rules/no-useless-removeEventListener"),
     "no-useless-run-test": require("../lib/rules/no-useless-run-test"),
