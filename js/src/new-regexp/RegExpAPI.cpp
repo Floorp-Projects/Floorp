25d9aed8ef44776eb402f2a740b806b19f0034c6
Iain Ireland <iireland@mozilla.com>
2020-05-05 12:49:54 +0000

    Bug 1634135: Disable named capture parsing until fully supported r=mgaudet
    The engine supports parsing named captures, but we don't have the code in place to expose the captured groups. Until we do, this code will make sure that we get a syntax error when parsing them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73115

--- js/src/new-regexp/RegExpAPI.cpp
+++ js/src/new-regexp/RegExpAPI.cpp
@@ -243,6 +243,14 @@ bool CheckPatternSyntax(JSContext* cx, TokenStreamAnyChars& ts,
     ReportSyntaxError(ts, result, chars.begin().get(), chars.length());
     return false;
   }
+  if (!result.capture_name_map.is_null()) {
+    // We can parse named captures, but we don't support them yet.
+    // Until we do, this will report a syntax error:
+    //  "invalid capture group name in regular expression"
+    result.error = RegExpError::kInvalidCaptureGroupName;
+    ReportSyntaxError(ts, result, chars.begin().get(), chars.length());
+    return false;
+  }
   return true;
 }
 
@@ -251,6 +259,14 @@ bool CheckPatternSyntax(JSContext* cx, TokenStreamAnyChars& ts,
   FlatStringReader reader(pattern);
   RegExpCompileData result;
   if (!CheckPatternSyntaxImpl(cx, &reader, flags, &result)) {
+    ReportSyntaxError(ts, result, pattern);
+    return false;
+  }
+  if (!result.capture_name_map.is_null()) {
+    // We can parse named captures, but we don't support them yet.
+    // Until we do, this will report a syntax error:
+    //  "invalid capture group name in regular expression"
+    result.error = RegExpError::kInvalidCaptureGroupName;
     ReportSyntaxError(ts, result, pattern);
     return false;
   }
