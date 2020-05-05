9e2b73cc436f0e69b17537927a344e10f2f657a0
Ricky Stewart <rstewart@mozilla.com>
2020-05-05 16:02:02 +0000

    Bug 1633039 - Don't check for Python 2 in configure r=glandium
    Differential Revision: https://phabricator.services.mozilla.com/D72895

--- python/mozbuild/mozbuild/backend/fastermake.py
+++ python/mozbuild/mozbuild/backend/fastermake.py
@@ -171,7 +171,6 @@ class FasterMakeBackend(MakeBackend, PartialBackend):
 
         # Add a few necessary variables inherited from configure
         for var in (
-            'PYTHON',
             'PYTHON3',
             'ACDEFINES',
             'MOZ_BUILD_APP',
