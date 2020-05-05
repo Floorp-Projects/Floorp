7bd957d86cfef564767f54e28d8d5c3b423d33ba
Agi Sferro <agi@sferro.dev>
2020-05-05 22:19:28 +0000

    Bug 1601067 - Use installBuiltIn in tests instead of registerWebExtension. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D72979

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ExtensionActionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ExtensionActionTest.kt
@@ -39,9 +39,12 @@ class ExtensionActionTest : BaseSessionTest() {
 
     @field:Parameterized.Parameter(0) @JvmField var id: String = ""
 
+    private val controller
+        get() = sessionRule.runtime.webExtensionController
+
     @Before
     fun setup() {
-        sessionRule.runtime.webExtensionController.setTabActive(mainSession, true)
+        controller.setTabActive(mainSession, true)
 
         // This method installs the extension, opens up ports with the background script and the
         // content script and captures the default action definition from the manifest
@@ -51,9 +54,8 @@ class ExtensionActionTest : BaseSessionTest() {
         val windowPortResult = GeckoResult<WebExtension.Port>()
         val backgroundPortResult = GeckoResult<WebExtension.Port>()
 
-        extension = WebExtension("resource://android/assets/web_extensions/actions/",
-                "actions", WebExtension.Flags.ALLOW_CONTENT_MESSAGING,
-                sessionRule.runtime.webExtensionController)
+        extension = sessionRule.waitForResult(
+                controller.installBuiltIn("resource://android/assets/web_extensions/actions/"));
 
         sessionRule.session.webExtensionController.setMessageDelegate(
                 extension!!,
@@ -83,14 +85,15 @@ class ExtensionActionTest : BaseSessionTest() {
             }
         })
 
-        sessionRule.waitForResult(sessionRule.runtime.registerWebExtension(extension!!))
-
         sessionRule.session.loadUri("http://example.com")
         sessionRule.waitForPageStop()
 
+        val pageAction = sessionRule.waitForResult(pageActionDefaultResult)
+        val browserAction = sessionRule.waitForResult(browserActionDefaultResult)
+
         default = when (id) {
-            "#pageAction" -> sessionRule.waitForResult(pageActionDefaultResult)
-            "#browserAction" -> sessionRule.waitForResult(browserActionDefaultResult)
+            "#pageAction" -> pageAction
+            "#browserAction" -> browserAction
             else -> throw IllegalArgumentException()
         }
 
@@ -114,7 +117,11 @@ class ExtensionActionTest : BaseSessionTest() {
 
     @After
     fun tearDown() {
-        sessionRule.waitForResult(sessionRule.runtime.unregisterWebExtension(extension!!))
+        if (extension != null) {
+            extension!!.setMessageDelegate(null, "browser")
+            extension!!.setActionDelegate(null)
+            sessionRule.waitForResult(controller.uninstall(extension!!))
+        }
     }
 
     private fun testBackgroundActionApi(message: String, tester: (WebExtension.Action) -> Unit) {
