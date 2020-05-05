f7f636f9c4dbbd7c7236a9bf359a420de216357e
Lars T Hansen <lhansen@mozilla.com>
2020-05-05 08:24:34 +0000

    Bug 1478632 - wasm simd, part 5b: new porting interfaces and x86/x64 assembler work.  r=bbouvier
    Clean up porting APIs and implement many new ones.  Also implement
    many new instructions required by the new porting APIs.
    
    We only care about x64, sse4.1, non-avx, and only register operands,
    and this simplifies things a lot for the new instructions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D69381

--- js/src/jit/x64/BaseAssembler-x64.h
+++ js/src/jit/x64/BaseAssembler-x64.h
@@ -846,6 +846,20 @@ class BaseAssemblerX64 : public BaseAssembler {
                        (RegisterID)src);
   }
 
+  void vpextrq_irr(unsigned lane, XMMRegisterID src, RegisterID dst) {
+    MOZ_ASSERT(lane < 2);
+    threeByteOpImmSimdInt64("vpextrq", VEX_PD, OP3_PEXTRQ_EvVdqIb, ESCAPE_3A,
+                            lane, (XMMRegisterID)dst, invalid_xmm,
+                            (RegisterID)src);
+  }
+
+  void vpinsrq_irr(unsigned lane, RegisterID src1, XMMRegisterID src0,
+                   XMMRegisterID dst) {
+    MOZ_ASSERT(lane < 2);
+    threeByteOpImmInt64Simd("vpinsrq", VEX_PD, OP3_PINSRQ_VdqEvIb, ESCAPE_3A,
+                            lane, src1, src0, dst);
+  }
+
   void vmovq_rr(RegisterID src, XMMRegisterID dst) {
     // While this is called "vmovq", it actually uses the vmovd encoding
     // with a REX prefix modifying it to be 64-bit.
@@ -954,7 +968,7 @@ class BaseAssemblerX64 : public BaseAssembler {
   void twoByteOpSimdInt64(const char* name, VexOperandType ty,
                           TwoByteOpcodeID opcode, XMMRegisterID rm,
                           RegisterID dst) {
-    if (useLegacySSEEncodingForOtherOutput()) {
+    if (useLegacySSEEncodingAlways()) {
       if (IsXMMReversedOperands(opcode)) {
         spew("%-11s%s, %s", legacySSEOpName(name), GPRegName(dst),
              XMMRegName(rm));
@@ -980,6 +994,38 @@ class BaseAssemblerX64 : public BaseAssembler {
     }
     m_formatter.twoByteOpVex64(ty, opcode, (RegisterID)rm, invalid_xmm,
                                (XMMRegisterID)dst);
+  }
+
+  void threeByteOpImmSimdInt64(const char* name, VexOperandType ty,
+                               ThreeByteOpcodeID opcode, ThreeByteEscape escape,
+                               uint32_t imm, XMMRegisterID src1,
+                               XMMRegisterID src0, RegisterID dst) {
+    if (useLegacySSEEncodingAlways()) {
+      spew("%-11s$0x%x, %s, %s", legacySSEOpName(name), imm, XMMRegName(src1),
+           GPReg64Name(dst));
+      m_formatter.legacySSEPrefix(ty);
+      m_formatter.threeByteOp64(opcode, escape, (RegisterID)src1, dst);
+      m_formatter.immediate8u(imm);
+      return;
+    }
+
+    MOZ_CRASH("AVX NYI");
+  }
+
+  void threeByteOpImmInt64Simd(const char* name, VexOperandType ty,
+                               ThreeByteOpcodeID opcode, ThreeByteEscape escape,
+                               uint32_t imm, RegisterID src1,
+                               XMMRegisterID src0, XMMRegisterID dst) {
+    if (useLegacySSEEncodingAlways()) {
+      spew("%-11s$0x%x, %s, %s", legacySSEOpName(name), imm, GPReg64Name(src1),
+           XMMRegName(dst));
+      m_formatter.legacySSEPrefix(ty);
+      m_formatter.threeByteOp64(opcode, escape, src1, (RegisterID)dst);
+      m_formatter.immediate8u(imm);
+      return;
+    }
+
+    MOZ_CRASH("AVX NYI");
   }
 };
 
