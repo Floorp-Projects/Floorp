d0b2cc67af40f8ab424ddbd41adc689a5a31332b
Mike Hommey <mh+mozilla@glandium.org>
2020-05-05 12:13:27 +0000

    Bug 1634204 - Upgrade Linux build docker images to Debian 8. r=froydnj
    This gets rid for the need of a number of local packages (mostly related
    to Gtk+3). One exception is that we now need a 32-bits version of the
    xz-utils package, some -dev package depends on it, and that dependency
    can't be fulfilled in the 32-bits image because we already have the
    64-bits backport installed, which conflicts with it (we need both
    32-bits and 64-bits package to be at the same version when installed).
    
    The system binutils fails to link clang-7 for some reason, so we now use
    our toolchain binutils instead, like we already do for newer versions of
    clang.
    
    The debian-packages docker image now needs an explicit installation of
    git, because it's not pulled in via the recommends of some other
    package.
    
    For some reason, snapshot.debian.org doesn't contain the jessie-backports
    archive at the same location as others, and only has a few snapshots of
    the archive.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73784

--- taskcluster/taskgraph/transforms/job/mozharness.py
+++ taskcluster/taskgraph/transforms/job/mozharness.py
@@ -148,9 +148,9 @@ def mozharness_on_docker_worker_setup(config, job, taskdesc):
                                   "'use-magic-mh-args' on docker-workers")
 
     # Running via mozharness assumes an image that contains build.sh:
-    # by default, debian7-amd64-build, but it could be another image (like
+    # by default, debian8-amd64-build, but it could be another image (like
     # android-build).
-    worker.setdefault('docker-image', {'in-tree': 'debian7-amd64-build'})
+    worker.setdefault('docker-image', {'in-tree': 'debian8-amd64-build'})
 
     worker.setdefault('artifacts', []).append({
         'name': 'public/logs',
