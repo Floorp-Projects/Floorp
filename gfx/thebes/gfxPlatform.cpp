e1d8d6d59a1e8498acbc13f7ea39e8a432131b14
Erik Nordin <nordzilla@mozilla.com>
2020-05-06 23:47:06 +0000

    Bug 1635584 - Make BackdropFilter's Availability Depend on WebRender r=emilio
    - Enable BackdropFilter pref by default
      - Add function IsBackdropFilterAavailable()
      - Use IsBackdropFilterAvailable for relevant WebIDL instead of pref
      - Add test for BackdropFilter availability
    
    Differential Revision: https://phabricator.services.mozilla.com/D73967

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -807,6 +807,12 @@ static void FrameRatePrefChanged(const char* aPref, void*) {
   }
 }
 
+static void RecomputeBackdropFilterEnabledState() {
+  NS_DispatchToMainThread(NS_NewRunnableFunction("RecomputeEnabledState", [] {
+    nsCSSProps::RecomputeEnabledState("layout.css.backdrop-filter.enabled");
+  }));
+}
+
 void gfxPlatform::Init() {
   MOZ_RELEASE_ASSERT(!XRE_IsGPUProcess(), "GFX: Not allowed in GPU process.");
   MOZ_RELEASE_ASSERT(!XRE_IsRDDProcess(), "GFX: Not allowed in RDD process.");
@@ -1078,6 +1084,8 @@ void gfxPlatform::Init() {
   if (obs) {
     obs->NotifyObservers(nullptr, "gfx-features-ready", nullptr);
   }
+
+  RecomputeBackdropFilterEnabledState();
 }
 
 void gfxPlatform::ReportTelemetry() {
@@ -3278,8 +3286,9 @@ void gfxPlatform::NotifyGPUProcessDisabled() {
             FeatureStatus::Unavailable, "GPU Process is disabled",
             NS_LITERAL_CSTRING("FEATURE_FAILURE_GPU_PROCESS_DISABLED"));
     gfxVars::SetUseWebRender(false);
-  }
 
+    RecomputeBackdropFilterEnabledState();
+  }
   gfxVars::SetRemoteCanvasEnabled(false);
 }
 
