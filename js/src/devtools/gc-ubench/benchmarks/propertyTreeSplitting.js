02db0cf1b84acc1dcecfaaaff063e9aa1b67df43
Steve Fink <sfink@mozilla.com>
2020-05-06 02:43:34 +0000

    Bug 1633625 - Load gc-ubench test loads from a file listing r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D72788

--- js/src/devtools/gc-ubench/benchmarks/propertyTreeSplitting.js
+++ js/src/devtools/gc-ubench/benchmarks/propertyTreeSplitting.js
@@ -2,36 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-window.tests.set(
+tests.set(
   "propertyTreeSplitting",
   (function() {
     var garbage = [];
     var garbageIndex = 0;
+    var obj = {};
     return {
-      description: "use delete to generate Shape garbage",
-      load: N => {
-        garbage = new Array(N);
-      },
-      unload: () => {
-        garbage = [];
-        garbageIndex = 0;
-      },
+      description: "use delete to generate Shape garbage (piles are unused)",
+      load: N => {},
+      unload: () => {},
       makeGarbage: N => {
-        function f() {
-          var a1 = eval;
-          delete eval;
-          eval = a1;
-          var a3 = toString;
-          delete toString;
-          toString = a3;
-        }
         for (var a = 0; a < N; ++a) {
-          garbage[garbageIndex++] = new f();
-          if (garbageIndex == garbage.length) {
-            garbageIndex = 0;
-          }
+          obj.x = 1;
+          obj.y = 2;
+          delete obj.x;
         }
-      },
+      }
     };
   })()
 );
