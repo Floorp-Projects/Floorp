a525597853b4af4181814c6bdd82223602d633a9
Kirk Steuber <ksteuber@mozilla.com>
2020-05-06 16:30:15 +0000

    Bug 1619145 - Add locale to app.update.url.manual and app.update.url.details in firefox-branding.js r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D74074

--- browser/branding/aurora/pref/firefox-branding.js
+++ browser/branding/aurora/pref/firefox-branding.js
@@ -12,10 +12,10 @@ pref("app.update.interval", 28800); // 8 hours
 pref("app.update.promptWaitTime", 691200);
 // URL user can browse to manually if for some reason all update installation
 // attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/firefox/aurora/");
+pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/aurora/");
 // A default value for the "More information about this update" link
 // supplied in the "An update is available" page of the update wizard.
-pref("app.update.url.details", "https://www.mozilla.org/firefox/aurora/");
+pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/aurora/");
 
 pref("app.releaseNotesURL", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%beta/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-browser&utm_campaign=whatsnew");
 
