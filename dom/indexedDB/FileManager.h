1a0758159bea05c04a9cfba320acae9e5cb077c7
Simon Giesecke <sgiesecke@mozilla.com>
2020-05-06 11:35:20 +0000

    Bug 1623278 - Use SafeRefCounted in ActorsParent. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D69351

--- dom/indexedDB/FileManager.h
+++ dom/indexedDB/FileManager.h
@@ -19,7 +19,8 @@ namespace dom {
 namespace indexedDB {
 
 // Implemented in ActorsParent.cpp.
-class FileManager final : public FileManagerBase<FileManager> {
+class FileManager final : public FileManagerBase<FileManager>,
+                          public AtomicSafeRefCounted<FileManager> {
   using PersistenceType = mozilla::dom::quota::PersistenceType;
   using FileManagerBase<FileManager>::MutexType;
 
@@ -85,11 +86,10 @@ class FileManager final : public FileManagerBase<FileManager> {
 
   [[nodiscard]] nsresult AsyncDeleteFile(int64_t aFileId);
 
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FileManager)
+  MOZ_DECLARE_REFCOUNTED_TYPENAME(FileManager)
 
   static StaticMutex& Mutex() { return sMutex; }
 
- private:
   ~FileManager() = default;
 };
 
