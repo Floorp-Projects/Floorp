735ea750b77aa406608733b7156729c6b8faab10
Bogdan Tara <btara@mozilla.com>
2020-05-06 22:39:46 +0300

    Backed out 5 changesets (bug 1633820) for bustages complaining about TabContext CLOSED TREE
    Backed out changeset 4982d41d5e6b (bug 1633820)
    Backed out changeset 9803c41e42f9 (bug 1633820)
    Backed out changeset 2475bbab03a8 (bug 1633820)
    Backed out changeset 762f0b2c154c (bug 1633820)
    Backed out changeset f9ea871a0227 (bug 1633820)

--- dom/ipc/TabContext.cpp
+++ dom/ipc/TabContext.cpp
@@ -41,15 +41,31 @@ bool TabContext::SetTabContext(const TabContext& aContext) {
   return true;
 }
 
+void TabContext::SetPrivateBrowsingAttributes(bool aIsPrivateBrowsing) {
+  mOriginAttributes.SyncAttributesWithPrivateBrowsing(aIsPrivateBrowsing);
+}
+
+void TabContext::SetFirstPartyDomainAttributes(
+    const nsAString& aFirstPartyDomain) {
+  mOriginAttributes.SetFirstPartyDomain(true, aFirstPartyDomain);
+}
+
 bool TabContext::UpdateTabContextAfterSwap(const TabContext& aContext) {
   // This is only used after already initialized.
   MOZ_ASSERT(mInitialized);
 
   // The only permissable changes are to mChromeOuterWindowID.  All other fields
   // must match for the change to be accepted.
+  if (aContext.mOriginAttributes != mOriginAttributes) {
+    return false;
+  }
 
   mChromeOuterWindowID = aContext.mChromeOuterWindowID;
   return true;
+}
+
+const OriginAttributes& TabContext::OriginAttributesRef() const {
+  return mOriginAttributes;
 }
 
 const nsAString& TabContext::PresentationURL() const {
@@ -60,12 +76,14 @@ UIStateChangeType TabContext::ShowFocusRings() const { return mShowFocusRings; }
 
 bool TabContext::SetTabContext(uint64_t aChromeOuterWindowID,
                                UIStateChangeType aShowFocusRings,
+                               const OriginAttributes& aOriginAttributes,
                                const nsAString& aPresentationURL,
                                uint32_t aMaxTouchPoints) {
   NS_ENSURE_FALSE(mInitialized, false);
 
   mInitialized = true;
   mChromeOuterWindowID = aChromeOuterWindowID;
+  mOriginAttributes = aOriginAttributes;
   mPresentationURL = aPresentationURL;
   mShowFocusRings = aShowFocusRings;
   mMaxTouchPoints = aMaxTouchPoints;
@@ -85,15 +103,16 @@ IPCTabContext TabContext::AsIPCTabContext() const {
     return IPCTabContext(JSPluginFrameIPCTabContext(mJSPluginID));
   }
 
-  return IPCTabContext(FrameIPCTabContext(mChromeOuterWindowID,
-                                          mPresentationURL, mShowFocusRings,
-                                          mMaxTouchPoints));
+  return IPCTabContext(
+      FrameIPCTabContext(mOriginAttributes, mChromeOuterWindowID,
+                         mPresentationURL, mShowFocusRings, mMaxTouchPoints));
 }
 
 MaybeInvalidTabContext::MaybeInvalidTabContext(const IPCTabContext& aParams)
     : mInvalidReason(nullptr) {
   uint64_t chromeOuterWindowID = 0;
   int32_t jsPluginId = -1;
+  OriginAttributes originAttributes;
   nsAutoString presentationURL;
   UIStateChangeType showFocusRings = UIStateChangeType_NoChange;
   uint32_t maxTouchPoints = 0;
@@ -103,17 +122,33 @@ MaybeInvalidTabContext::MaybeInvalidTabContext(const IPCTabContext& aParams)
       const PopupIPCTabContext& ipcContext = aParams.get_PopupIPCTabContext();
 
       TabContext* context;
-      if (ipcContext.openerParent()) {
-        context = BrowserParent::GetFrom(ipcContext.openerParent());
-      } else if (ipcContext.openerChild()) {
-        context = static_cast<BrowserChild*>(ipcContext.openerChild());
+      if (ipcContext.opener().type() == PBrowserOrId::TPBrowserParent) {
+        context =
+            BrowserParent::GetFrom(ipcContext.opener().get_PBrowserParent());
+        if (!context) {
+          mInvalidReason =
+              "Child is-browser process tried to "
+              "open a null tab.";
+          return;
+        }
+      } else if (ipcContext.opener().type() == PBrowserOrId::TPBrowserChild) {
+        context =
+            static_cast<BrowserChild*>(ipcContext.opener().get_PBrowserChild());
+      } else if (ipcContext.opener().type() == PBrowserOrId::TTabId) {
+        // We should never get here because this PopupIPCTabContext is only
+        // used for allocating a new tab id, not for allocating a PBrowser.
+        mInvalidReason =
+            "Child process tried to open an tab without the opener "
+            "information.";
+        return;
       } else {
         // This should be unreachable because PopupIPCTabContext::opener is not
         // a nullable field.
-        mInvalidReason = "PopupIPCTabContext::opener was null.";
+        mInvalidReason = "PopupIPCTabContext::opener was null (?!).";
         return;
       }
 
+      originAttributes = context->mOriginAttributes;
       chromeOuterWindowID = ipcContext.chromeOuterWindowID();
       break;
     }
@@ -130,7 +165,20 @@ MaybeInvalidTabContext::MaybeInvalidTabContext(const IPCTabContext& aParams)
       chromeOuterWindowID = ipcContext.chromeOuterWindowID();
       presentationURL = ipcContext.presentationURL();
       showFocusRings = ipcContext.showFocusRings();
+      originAttributes = ipcContext.originAttributes();
       maxTouchPoints = ipcContext.maxTouchPoints();
+      break;
+    }
+    case IPCTabContext::TUnsafeIPCTabContext: {
+      // XXXcatalinb: This used *only* by ServiceWorkerClients::OpenWindow.
+      // It is meant as a temporary solution until service workers can
+      // provide a BrowserChild equivalent. Don't allow this on b2g since
+      // it might be used to escalate privileges.
+      if (!StaticPrefs::dom_serviceWorkers_enabled()) {
+        mInvalidReason = "ServiceWorkers should be enabled.";
+        return;
+      }
+
       break;
     }
     default: {
@@ -143,7 +191,8 @@ MaybeInvalidTabContext::MaybeInvalidTabContext(const IPCTabContext& aParams)
     rv = mTabContext.SetTabContextForJSPluginFrame(jsPluginId);
   } else {
     rv = mTabContext.SetTabContext(chromeOuterWindowID, showFocusRings,
-                                   presentationURL, maxTouchPoints);
+                                   originAttributes, presentationURL,
+                                   maxTouchPoints);
   }
   if (!rv) {
     mInvalidReason = "Couldn't initialize TabContext.";
