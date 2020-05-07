4020944b675c5e7ee606dc17cbc64d036e57237f
Asumu Takikawa <asumu@igalia.com>
2020-05-07 21:10:44 +0000

    Bug 1608771 - Part 3, BigInt<>I64 conversion for inlined calls r=lth,wingo
    This is part 3 of implementing the Wasm BigInt<>I64 conversion proposal for inlined Ion to Wasm calls.
    
    This part adds changes needed in code generation and Wasm stubs for supporting the I64 arguments for
    inlined Ion to Wasm calls.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65236

--- js/src/wasm/WasmStubs.cpp
+++ js/src/wasm/WasmStubs.cpp
@@ -1416,6 +1416,13 @@ void wasm::GenerateDirectCallFromJit(MacroAssembler& masm, const FuncExport& fe,
         case MIRType::Int32:
           GenPrintIsize(DebugChannel::Function, masm, iter->gpr());
           break;
+        case MIRType::Int64:
+#ifdef ENABLE_WASM_BIGINT
+          GenPrintI64(DebugChannel::Function, masm, iter->gpr64());
+          break;
+#else
+          MOZ_CRASH("ion to wasm fast path can only handle i32/f32/f64");
+#endif
         case MIRType::Float32:
           GenPrintF32(DebugChannel::Function, masm, iter->fpu());
           break;
@@ -1548,6 +1555,14 @@ void wasm::GenerateDirectCallFromJit(MacroAssembler& masm, const FuncExport& fe,
         // The return value is in ReturnReg, which is what Ion expects.
         GenPrintIsize(DebugChannel::Function, masm, ReturnReg);
         break;
+      case wasm::ValType::I64:
+#ifdef ENABLE_WASM_BIGINT
+        // The return value is in ReturnReg64, which is what Ion expects.
+        GenPrintI64(DebugChannel::Function, masm, ReturnReg64);
+        break;
+#else
+        MOZ_CRASH("unexpected return type when calling from ion to wasm");
+#endif
       case wasm::ValType::F32:
         masm.canonicalizeFloat(ReturnFloat32Reg);
         GenPrintF32(DebugChannel::Function, masm, ReturnFloat32Reg);
@@ -1572,7 +1587,6 @@ void wasm::GenerateDirectCallFromJit(MacroAssembler& masm, const FuncExport& fe,
             MOZ_CRASH("unexpected return type when calling from ion to wasm");
         }
         break;
-      case wasm::ValType::I64:
       case wasm::ValType::V128:
         MOZ_CRASH("unexpected return type when calling from ion to wasm");
     }
