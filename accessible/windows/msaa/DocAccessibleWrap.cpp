62253d92c35df759a640df22667d6ad3cefba80b
James Teh <jteh@mozilla.com>
2020-05-07 04:53:39 +0000

    Bug 1636002: Don't use the parent IAccessible from DocAccessibleChild unless it is a top level document. r=MarcoZ
    In-process iframes get a DocAccessibleChild, even though they're not top level in their process.
    However, the parent IAccessible on DocAccessibleChild (sent from the parent process) is only relevant for the top level.
    Previously, we used the parent IAccessible on the DocAccessibleChild for *all* documents, resulting in accParent returning null for in-process iframe documents.
    Instead, we need to use the base implementation in this case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74180

--- accessible/windows/msaa/DocAccessibleWrap.cpp
+++ accessible/windows/msaa/DocAccessibleWrap.cpp
@@ -46,7 +46,8 @@ DocAccessibleWrap::get_accParent(
     /* [retval][out] */ IDispatch __RPC_FAR* __RPC_FAR* ppdispParent) {
   // We might be a top-level document in a content process.
   DocAccessibleChild* ipcDoc = IPCDoc();
-  if (!ipcDoc) {
+  if (!ipcDoc || static_cast<dom::BrowserChild*>(ipcDoc->Manager())
+                         ->GetTopLevelDocAccessibleChild() != ipcDoc) {
     return DocAccessible::get_accParent(ppdispParent);
   }
 
