baa36424095a7440582091d05d441f31ad5a56fc
Matthew Gaudet <mgaudet@mozilla.com>
2020-05-07 07:18:40 +0000

    Bug 1635898 - Remove remains of removed approach to initializers r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D74127

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -2027,10 +2027,6 @@ GeneralParser<ParseHandler, Unit>::functionBody(InHandling inHandling,
   uint32_t startYieldOffset = pc_->lastYieldOffset;
 #endif
 
-  // One might expect noteUsedName(".initializers") here when parsing a
-  // constructor. See GeneralParser<ParseHandler, Unit>::classDefinition on why
-  // it's not here.
-
   Node body;
   if (type == StatementListBody) {
     bool inheritedStrict = pc_->sc()->strict();
