a21b07639da9548edbbfce9aa78a134416170003
alwu <alwu@mozilla.com>
2020-05-07 02:32:56 +0000

    Bug 1633830 - part4 : update comment to describe the reason of having this class and remove out-of-date comment. r=bryce
    This patch will do :
    - update the comment to describe its usage and where it should be used
    - remove out-of-date comment
    
    The advantage of doing so :
    - help future reader to easily understand its usage
    
    Differential Revision: https://phabricator.services.mozilla.com/D72940

--- dom/media/mediacontrol/ContentPlaybackController.h
+++ dom/media/mediacontrol/ContentPlaybackController.h
@@ -15,23 +15,24 @@ namespace dom {
 class MediaSession;
 
 /**
- * This interface is used to handle different playback control actions. The
- * methods are designed based on the MediaSessionAction values, which are
- * defined in the media session spec [1].
+ * This interface is used to handle different playback control actions in the
+ * content process. Most of the methods are designed based on the
+ * MediaSessionAction values, which are defined in the media session spec [1].
+ *
+ * The reason we need that is to explicitly separate the implementation from all
+ * different defined methods. If we want to add a new method in the future, we
+ * can do that without modifying other methods.
  *
  * If the active media session has a corresponding MediaSessionActionHandler,
  * then we would invoke it, or we would do nothing. However, for certain
  * actions, such as `play`, `pause` and `stop`, we have default action handling
  * in order to control playback correctly even if the website doesn't use media
- * session at all.
- *
- * The default aciton handling we perform is to change the outer window's media
- * suspended state, in order to control playback. Therefore, this class should
- * always be used in the same process where the outer window is. And we use
- * browsing context to get the outer window. (However, because of bug 1593826,
- * now we are not able to make sure that it actually has a window.)
+ * session at all or the media session doesn't have correspending action handler
+ * [2].
  *
  * [1] https://w3c.github.io/mediasession/#enumdef-mediasessionaction
+ * [2]
+ * https://w3c.github.io/mediasession/#ref-for-active-media-session%E2%91%A1%E2%93%AA
  */
 class MOZ_STACK_CLASS ContentPlaybackController {
  public:
