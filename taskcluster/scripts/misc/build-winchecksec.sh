fcbf16265756aef4746ba78da23fe2d3e6befb73
Mike Hommey <mh+mozilla@glandium.org>
2020-05-07 00:34:36 +0000

    Bug 1625696 - Update winchecksec and build/use it natively for Linux. r=dmajor
    Now that upstream winchecksec builds and works natively on Linux, use
    that. That should solve the random crashes under Wine. If random crashes
    still happen, it will be easier to debug anyways.
    We bump to the last version that doesn't use vcpkg because vcpkg makes
    things more difficult.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73405

--- taskcluster/scripts/misc/build-winchecksec.sh
+++ taskcluster/scripts/misc/build-winchecksec.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e -v -x
 
+mkdir -p $UPLOAD_DIR
+
 cd $MOZ_FETCHES_DIR/winchecksec
 
-export PATH="$MOZ_FETCHES_DIR/clang/bin:$PATH"
+if test -n "$TOOLTOOL_MANIFEST"; then
+    export PATH="$MOZ_FETCHES_DIR/clang/bin:$PATH"
 
-export LD_PRELOAD=$MOZ_FETCHES_DIR/liblowercase/liblowercase.so
-export LOWERCASE_DIRS=$MOZ_FETCHES_DIR/vs2017_15.8.4
+    export LD_PRELOAD=$MOZ_FETCHES_DIR/liblowercase/liblowercase.so
+    export LOWERCASE_DIRS=$MOZ_FETCHES_DIR/vs2017_15.8.4
 
-. $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
-. $GECKO_PATH/taskcluster/scripts/misc/tooltool-download.sh
+    . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
+    . $GECKO_PATH/taskcluster/scripts/misc/tooltool-download.sh
 
-# Patch pe-parse because clang-cl doesn't support /analyze.
-patch -p1 <<'EOF'
+    # Patch pe-parse because clang-cl doesn't support /analyze.
+    patch -p1 <<'EOF'
 --- a/pe-parse/cmake/compilation_flags.cmake
 +++ b/pe-parse/cmake/compilation_flags.cmake
 @@ -1,5 +1,5 @@
@@ -24,22 +27,27 @@ patch -p1 <<'EOF'
      list(APPEND DEFAULT_CXX_FLAGS /Zi)
 EOF
 
-cmake \
+    CMAKE_FLAGS='
+      -DCMAKE_CXX_COMPILER=clang-cl
+      -DCMAKE_C_COMPILER=clang-cl
+      -DCMAKE_LINKER=lld-link
+      -DCMAKE_C_FLAGS=-fuse-ld=lld
+      -DCMAKE_CXX_FLAGS="-fuse-ld=lld -EHsc"
+      -DCMAKE_RC_COMPILER=llvm-rc
+      -DCMAKE_MT=llvm-mt
+      -DCMAKE_SYSTEM_NAME=Windows
+      -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
+    '
+fi
+
+eval cmake \
   -GNinja \
-  -DCMAKE_CXX_COMPILER=clang-cl \
-  -DCMAKE_C_COMPILER=clang-cl \
-  -DCMAKE_LINKER=lld-link \
-  -DCMAKE_C_FLAGS=-fuse-ld=lld \
-  -DCMAKE_CXX_FLAGS="-fuse-ld=lld \
-  -EHsc" \
-  -DCMAKE_RC_COMPILER=llvm-rc \
-  -DCMAKE_MT=llvm-mt \
-  -DCMAKE_SYSTEM_NAME=Windows \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
+  -DBUILD_SHARED_LIBS=Off \
+  $CMAKE_FLAGS
 
 ninja -v
 
 cd ..
-tar -caf winchecksec.tar.bz2 winchecksec/winchecksec.{dll,exe}
+tar -caf winchecksec.tar.bz2 winchecksec/winchecksec${TOOLTOOL_MANIFEST:+.exe}
 cp winchecksec.tar.bz2 $UPLOAD_DIR/
