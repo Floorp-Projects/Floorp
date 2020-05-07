5a05e4bb304257d382b1cfe99f02f558cac2a4a3
Florian Quèze <florian@queze.net>
2020-05-07 19:05:18 +0000

    Bug 1630982 - terminate the process quickly for mochitest-plain tests, r=gbrown.
    Depends on D71336
    
    Differential Revision: https://phabricator.services.mozilla.com/D74266

--- testing/profiles/mochitest/user.js
+++ testing/profiles/mochitest/user.js
@@ -8,3 +8,7 @@ user_pref("network.cookie.sameSite.laxByDefault", false);
 // Enable blocking access to storage from tracking resources by default.
 // We don't want to run mochitest using BEHAVIOR_REJECT_TRACKER_AND_PARTITION_FOREIGN (5 - aka Dynamic First Party Isolation) yet.
 user_pref("network.cookie.cookieBehavior", 4);
+
+// Don't restore the last open set of tabs if the browser has crashed
+// or if the profile folder is re-used after an exit(0) fast shutdown.
+user_pref("browser.sessionstore.resume_from_crash", false);
