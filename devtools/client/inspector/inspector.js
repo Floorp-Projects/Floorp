865d5dd06794f3f268a9a70516a030460eecf12f
Julian Descottes <jdescottes@mozilla.com>
2020-05-08 21:58:14 +0000

    Bug 1625961 - Use the ResourceWatcher API to fetch Root NodeFront r=ochameau,nchevobbe
    Depends on D62625
    
    Differential Revision: https://phabricator.services.mozilla.com/D72668

--- devtools/client/inspector/inspector.js
+++ devtools/client/inspector/inspector.js
@@ -166,6 +166,7 @@ function Inspector(toolbox) {
   this.onHostChanged = this.onHostChanged.bind(this);
   this.onMarkupLoaded = this.onMarkupLoaded.bind(this);
   this.onNewSelection = this.onNewSelection.bind(this);
+  this.onResourceAvailable = this.onResourceAvailable.bind(this);
   this.onRootNodeAvailable = this.onRootNodeAvailable.bind(this);
   this.onPanelWindowResize = this.onPanelWindowResize.bind(this);
   this.onShowBoxModelHighlighterForNode = this.onShowBoxModelHighlighterForNode.bind(
@@ -200,6 +201,10 @@ Inspector.prototype = {
       this._onTargetDestroyed
     );
 
+    await this.toolbox.resourceWatcher.watch(
+      [this.toolbox.resourceWatcher.TYPES.ROOT_NODE],
+      this.onResourceAvailable
+    );
     // Store the URL of the target page prior to navigation in order to ensure
     // telemetry counts in the Grid Inspector are not double counted on reload.
     this.previousURL = this.currentTarget.url;
@@ -222,8 +227,6 @@ Inspector.prototype = {
       this._getCssProperties(),
       this._getAccessibilityFront(),
     ]);
-
-    this.walker.watchRootNode(this.onRootNodeAvailable);
   },
 
   _onTargetDestroyed({ type, targetFront, isTopLevel }) {
@@ -1276,6 +1279,16 @@ Inspector.prototype = {
         this.onEyeDropperButtonClicked
       );
       this.eyeDropperButton = null;
+    }
+  },
+
+  onResourceAvailable: function({ resourceType, targetFront, resource }) {
+    if (resourceType === this.toolbox.resourceWatcher.TYPES.ROOT_NODE) {
+      // Note: the resource (ie the root node here) will be fetched from the
+      // walker later on in _getDefaultNodeForSelection.
+      // We should update the inspector to directly use the node front
+      // provided here. Bug 1635461.
+      this.onRootNodeAvailable();
     }
   },
 
