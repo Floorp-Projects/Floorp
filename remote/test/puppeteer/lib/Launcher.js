c44c095dd09e8743bcebe6c5823fc470420cfd70
Henrik Skupin <mail@hskupin.info>
2020-05-08 09:31:36 +0000

    Bug 1631089 - [remote] Force sRGB tag for generated screenshots. r=remote-protocol-reviewers,jgraham
    With full color management enabled in bug 455077, generated
    screenshots will not be saved as sRGB when the display has
    a different ICC profile. As long as bug 1615395 hasn't been
    fixed, force screenshots to be saved as sRGB.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74007

--- remote/test/puppeteer/lib/Launcher.js
+++ remote/test/puppeteer/lib/Launcher.js
@@ -642,13 +642,21 @@ class FirefoxLauncher {
 
       // Allow the application to have focus even it runs in the background
       'focusmanager.testmode': true,
+
       // Disable useragent updates
       'general.useragent.updates.enabled': false,
+
       // Always use network provider for geolocation tests so we bypass the
       // macOS dialog raised by the corelocation provider
       'geo.provider.testing': true,
+
       // Do not scan Wifi
       'geo.wifi.scan': false,
+
+      // Force sRGB tagged screenshots for image encoder
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1615395
+      'gfx.color_management.force_srgb': true,
+
       // No hang monitor
       'hangmonitor.timeout': 0,
       // Show chrome errors and warnings in the error console
