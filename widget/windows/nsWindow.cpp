1bca0138f57550913838f9f1712ed5eef594a0f1
Timothy Nikkel <tnikkel@gmail.com>
2020-05-08 23:20:31 +0000

    Bug 1630912. Handle sending Direct Manipulation input events properly. r=kats
    The old code didn't handle content prevent defaulting the input.
    
    The pinch gesture code doesn't seem to fully work properly, it will allow a little pinch zooming before halting it if content is prevent defaulting it. Not sure what is up yet.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73220

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -717,13 +717,28 @@ static bool ShouldCacheTitleBarInfo(nsWindowType aWindowType,
 }
 
 void nsWindow::SendAnAPZEvent(InputData& aEvent) {
+  APZEventResult result;
   if (mAPZC) {
-    APZEventResult es = mAPZC->InputBridge()->ReceiveInputEvent(aEvent);
-    // xxx look at es
+    result = mAPZC->InputBridge()->ReceiveInputEvent(aEvent);
   }
-  // xxx dispatch regularly if we don't have an apzc or if apzc doesn't consume
-  // it, or don't create direct manip objects in the first place if we don't
-  // have an apzc?
+  if (result.mStatus == nsEventStatus_eConsumeNoDefault) {
+    return;
+  }
+
+  MOZ_ASSERT(aEvent.mInputType == PANGESTURE_INPUT ||
+             aEvent.mInputType == PINCHGESTURE_INPUT);
+
+  if (aEvent.mInputType == PANGESTURE_INPUT) {
+    PanGestureInput& panInput = aEvent.AsPanGestureInput();
+    WidgetWheelEvent event = panInput.ToWidgetWheelEvent(this);
+    ProcessUntransformedAPZEvent(&event, result);
+
+    return;
+  }
+
+  PinchGestureInput& pinchInput = aEvent.AsPinchGestureInput();
+  WidgetWheelEvent event = pinchInput.ToWidgetWheelEvent(this);
+  ProcessUntransformedAPZEvent(&event, result);
 }
 
 void nsWindow::RecreateDirectManipulationIfNeeded() {
