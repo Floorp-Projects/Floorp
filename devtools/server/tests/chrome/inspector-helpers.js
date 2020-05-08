998f2c067550f2bb984dda8a2dc863260cb234a7
Julian Descottes <jdescottes@mozilla.com>
2020-05-08 21:53:31 +0000

    Bug 1611096 - Fix tests relying on newRoot mutations r=rcaliman
    Depends on D62623
    
    `new-root` is no longer a mutation, but an event emitted by the `walker` actor.
    Tests watching for mutations should be updated accordingly. They also need to call watchRootNode explicitly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D62624

--- devtools/server/tests/chrome/inspector-helpers.js
+++ devtools/server/tests/chrome/inspector-helpers.js
@@ -1,6 +1,6 @@
 /* exported attachURL, promiseDone,
-   promiseOnce, isNewRoot,
-   waitForMutation, addTest, addAsyncTest,
+   promiseOnce,
+   addTest, addAsyncTest,
    runNextTest, _documentWalker */
 "use strict";
 
@@ -104,32 +104,6 @@ function promiseDone(currentPromise) {
       dump(err.stack);
     }
     SimpleTest.finish();
-  });
-}
-
-// Mutation list testing
-
-function isNewRoot(change) {
-  return change.type === "newRoot";
-}
-
-// Load mutations aren't predictable, so keep accumulating mutations until
-// the one we're looking for shows up.
-function waitForMutation(walker, test, mutations = []) {
-  return new Promise(resolve => {
-    for (const change of mutations) {
-      if (test(change)) {
-        resolve(mutations);
-      }
-    }
-
-    walker.once("mutations", newMutations => {
-      waitForMutation(walker, test, mutations.concat(newMutations)).then(
-        finalMutations => {
-          resolve(finalMutations);
-        }
-      );
-    });
   });
 }
 
