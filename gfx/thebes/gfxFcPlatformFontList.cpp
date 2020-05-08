b2f4664fb5c789e1a79b9fa6711a9d6b4176b6f8
Jonathan Kew <jkew@mozilla.com>
2020-05-08 09:25:37 +0000

    Bug 1634670 - Fonts bundled with the application should be treated as part of the base font set, not as user-installed fonts. r=jwatt
    Differential Revision: https://phabricator.services.mozilla.com/D73437

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -1356,7 +1356,8 @@ void gfxFcPlatformFontList::AddPatternToFontList(
     aFontFamily =
         static_cast<gfxFontconfigFontFamily*>(mFontFamilies.GetWeak(keyName));
     if (!aFontFamily) {
-      FontVisibility visibility = GetVisibilityForFamily(keyName);
+      FontVisibility visibility =
+          aAppFonts ? FontVisibility::Base : GetVisibilityForFamily(keyName);
       aFontFamily = new gfxFontconfigFontFamily(aFamilyName, visibility);
       mFontFamilies.Put(keyName, RefPtr{aFontFamily});
     }
@@ -1609,7 +1610,8 @@ void gfxFcPlatformFontList::InitSharedFontListForPlatform() {
         }
         */
 
-        FontVisibility visibility = GetVisibilityForFamily(keyName);
+        FontVisibility visibility =
+            aAppFont ? FontVisibility::Base : GetVisibilityForFamily(keyName);
         families.AppendElement(fontlist::Family::InitData(
             keyName, aFamilyName, /*index*/ 0, visibility,
             /*bundled*/ aAppFont, /*badUnderline*/ false));
