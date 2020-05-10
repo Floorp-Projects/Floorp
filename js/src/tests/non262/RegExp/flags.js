ea3297a5fda809d4e06143e1de6304fd874c555c
Iain Ireland <iireland@mozilla.com>
2020-05-10 16:00:26 +0000

    Bug 1634135: Update tests to expect RegExp.prototype.dotAll r=mgaudet
    These two tests need to be updated to be aware of the new dotAll flag.
    
    If we ever have to turn off the new engine, this patch should also be temporarily reverted.
    
    Depends on D73118
    
    Differential Revision: https://phabricator.services.mozilla.com/D73119

--- js/src/tests/non262/RegExp/flags.js
+++ js/src/tests/non262/RegExp/flags.js
@@ -12,7 +12,7 @@ assertEq(genericFlags({}), "");
 assertEq(genericFlags({ignoreCase: true}), "i");
 assertEq(genericFlags({sticky:1, unicode:1, global: 0}), "uy");
 assertEq(genericFlags({__proto__: {multiline: true}}), "m");
-assertEq(genericFlags(new Proxy({}, {get(){return true}})), "gimuy");
+assertEq(genericFlags(new Proxy({}, {get(){return true}})), "gimsuy");
 
 assertThrowsInstanceOf(() => genericFlags(), TypeError);
 assertThrowsInstanceOf(() => genericFlags(1), TypeError);
