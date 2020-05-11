89c6fbb7739c0e95069b66171130e220dd19d964
Micah Tigley <mtigley@mozilla.com>
2020-05-11 20:24:21 +0000

    Bug 1629729 - Use BrowserTestUtils.loadURI to navigate to a new URL. r=bradwerth
    Differential Revision: https://phabricator.services.mozilla.com/D74721

--- devtools/client/responsive/test/browser/head.js
+++ devtools/client/responsive/test/browser/head.js
@@ -610,10 +610,10 @@ function waitForViewportScroll(ui) {
   );
 }
 
-function load(browser, url) {
-  const loaded = BrowserTestUtils.browserLoaded(browser, false, url);
-  BrowserTestUtils.loadURI(browser, url);
-  return loaded;
+async function load(browser, url) {
+  const loaded = BrowserTestUtils.browserLoaded(browser, false, null, false);
+  await BrowserTestUtils.loadURI(browser, url);
+  await loaded;
 }
 
 function back(browser) {
