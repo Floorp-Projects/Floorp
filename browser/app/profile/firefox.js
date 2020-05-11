7b334b0cdb5e5594a196731035940629673fd073
Dão Gottwald <dao@mozilla.com>
2020-05-11 12:06:50 +0000

    Bug 1636112 - Make post-prompt animation obey prefers-reduced-motion. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D74231

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -404,8 +404,6 @@ pref("permissions.desktop-notification.notNow.enabled", false);
 
 pref("permissions.fullscreen.allowed", false);
 
-pref("permissions.postPrompt.animate", true);
-
 // handle links targeting new windows
 // 1=current window/tab, 2=new window, 3=new tab in most recent window
 pref("browser.link.open_newwindow", 3);
