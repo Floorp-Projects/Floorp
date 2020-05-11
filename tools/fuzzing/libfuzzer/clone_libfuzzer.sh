a493330658ab3e3d834347c114e33501a944ac7c
Christian Holler <choller@mozilla.com>
2020-05-11 12:45:07 +0000

    Bug 1635762 - Switch libFuzzer upstream to git and add local patches. r=truber
    Differential Revision: https://phabricator.services.mozilla.com/D74040

--- tools/fuzzing/libfuzzer/clone_libfuzzer.sh
+++ tools/fuzzing/libfuzzer/clone_libfuzzer.sh
@@ -1,31 +1,35 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Optionally get revision from cmd line
-# Changelog: https://reviews.llvm.org/source/compiler-rt/history/compiler-rt/trunk/lib/fuzzer/
-[ $1 ] && REVISION=$1 || REVISION=356365
+[ $1 ] && REVISION=$1 || REVISION=c815210013f27cfac07d6b53b47e8ac53e86afa3
 
 mkdir tmp
-svn co -qr $REVISION https://llvm.org/svn/llvm-project/compiler-rt/trunk tmp || exit
+git clone --single-branch --no-checkout --shallow-since "2019-03-01" https://github.com/llvm/llvm-project tmp
 
-if [ $1 ]; then
-  # libFuzzer source files
-  CPPS=($(ls -rv tmp/lib/fuzzer/*.cpp))
-  CPPS=(${CPPS[@]##*/})
-  CPPS=(${CPPS[@]##FuzzerMain*}) # ignored
+(cd tmp && git reset --hard $REVISION)
 
-  # Update SOURCES entries
-  sed -e "/^SOURCES/,/^]/ {/'/d}" -i moz.build
-  for CPP in ${CPPS[@]}; do sed -e "/^SOURCES/ a \\\t'${CPP}'," -i moz.build; done
+# libFuzzer source files
+CPPS=($(ls -rv tmp/compiler-rt/lib/fuzzer/*.cpp))
+CPPS=(${CPPS[@]##*/})
+CPPS=(${CPPS[@]##FuzzerMain*}) # ignored
 
-  # Remove previous files
-  rm *.{cpp,h,def}
-fi
+# Update SOURCES entries
+sed -e "/^SOURCES/,/^]/ {/'/d}" -i moz.build
+for CPP in ${CPPS[@]}; do sed -e "/^SOURCES/ a \\    '${CPP}'," -i moz.build; done
+
+# Remove previous files
+rm *.{cpp,h,def}
 
 # Copy files
-cp tmp/lib/fuzzer/*.{cpp,h,def} .
+cp tmp/compiler-rt/lib/fuzzer/*.{cpp,h,def} .
+
+# Apply local patches
+for patch in patches/*.patch
+do
+  patch -p4 < $patch
+done
 
 # Remove the temporary directory
 rm -Rf tmp/
 
-[ $1 ] && echo "Updated libFuzzer to ${REVISION}"
-
+echo "Updated libFuzzer to ${REVISION}"
