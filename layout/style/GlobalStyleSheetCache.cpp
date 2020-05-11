92a4ee7e549d4be46b93070bf573049af23c3bdd
Cameron McCormack <cam@mcc.id.au>
2020-05-11 00:11:45 +0000

    Bug 1621773 - Gracefully handle errors creating shared memory UA style sheets. r=emilio
    We still panic in a debug build, so that developers can notice when they
    need to add a new static atom after modifying UA sheets.
    
    We also add telemetry to note when this happens, add an app note to a
    crash report, in case any crash later on occurs, and re-up the existing,
    expired shared memory sheet telemetry probes so we can look at them
    again.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73188

--- layout/style/GlobalStyleSheetCache.cpp
+++ layout/style/GlobalStyleSheetCache.cpp
@@ -386,16 +386,32 @@ void GlobalStyleSheetCache::InitSharedSheetsInParent() {
       Servo_SharedMemoryBuilder_Create(
           header->mBuffer, kSharedMemorySize - offsetof(Header, mBuffer)));
 
+  nsCString message;
+
   // Copy each one into the shared memory, and record its pointer.
-#define STYLE_SHEET(identifier_, url_, shared_)            \
-  if (shared_) {                                           \
-    StyleSheet* sheet = identifier_##Sheet();              \
-    size_t i = size_t(UserAgentStyleSheetID::identifier_); \
-    URLExtraData::sShared[i] = sheet->URLData();           \
-    header->mSheets[i] = sheet->ToShared(builder.get());   \
+  //
+  // Normally calling ToShared on UA sheets should not fail.  It happens
+  // in practice in odd cases that seem like corrupted installations; see bug
+  // 1621773.  On failure, return early and fall back to non-shared sheets.
+#define STYLE_SHEET(identifier_, url_, shared_)                             \
+  if (shared_) {                                                            \
+    StyleSheet* sheet = identifier_##Sheet();                               \
+    size_t i = size_t(UserAgentStyleSheetID::identifier_);                  \
+    URLExtraData::sShared[i] = sheet->URLData();                            \
+    header->mSheets[i] = sheet->ToShared(builder.get(), message);           \
+    if (!header->mSheets[i]) {                                              \
+      CrashReporter::AppendAppNotesToCrashReport(NS_LITERAL_CSTRING("\n") + \
+                                                 message);                  \
+      Telemetry::Accumulate(                                                \
+          Telemetry::SHARED_MEMORY_UA_SHEETS_TOSHMEM_SUCCEEDED, false);     \
+      return;                                                               \
+    }                                                                       \
   }
 #include "mozilla/UserAgentStyleSheetList.h"
 #undef STYLE_SHEET
+
+  Telemetry::Accumulate(Telemetry::SHARED_MEMORY_UA_SHEETS_TOSHMEM_SUCCEEDED,
+                        true);
 
   // Finished writing into the shared memory.  Freeze it, so that a process
   // can't confuse other processes by changing the UA style sheet contents.
