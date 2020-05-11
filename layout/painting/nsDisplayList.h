f4b4fd82c5aa16d7ee14dc2268ae6862a05ac070
Miko Mynttinen <mikokm@gmail.com>
2020-05-11 16:59:26 +0000

    Bug 1554499 - Part 16: Move table per frame index helpers to nsDisplayList.cpp r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D74095

--- layout/painting/nsDisplayList.h
+++ layout/painting/nsDisplayList.h
@@ -4710,32 +4710,6 @@ class nsDisplayBackgroundImage : public nsDisplayImageContainer {
   uint32_t mImageFlags;
 };
 
-enum class TableType : uint8_t {
-  Table,
-  TableCol,
-  TableColGroup,
-  TableRow,
-  TableRowGroup,
-  TableCell,
-
-  MAX,
-};
-
-enum class TableTypeBits : uint8_t { Count = 3 };
-
-static_assert(static_cast<uint8_t>(TableType::MAX) <
-                  (1 << (static_cast<uint8_t>(TableTypeBits::Count) + 1)),
-              "TableType cannot fit with TableTypeBits::Count");
-TableType GetTableTypeFromFrame(nsIFrame* aFrame);
-
-static uint16_t CalculateTablePerFrameKey(const uint16_t aIndex,
-                                          const TableType aType) {
-  const uint32_t key = (aIndex << static_cast<uint8_t>(TableTypeBits::Count)) |
-                       static_cast<uint8_t>(aType);
-
-  return static_cast<uint16_t>(key);
-}
-
 /**
  * A display item to paint background image for table. For table parts, such
  * as row, row group, col, col group, when drawing its background, we'll
