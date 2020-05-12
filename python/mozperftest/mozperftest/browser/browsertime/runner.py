fc47a2e28e31fe96a91b737a2dcf6bb4b4192201
Tarek Ziadé <tarek@mozilla.com>
2020-05-12 16:57:27 +0000

    Bug 1637315 - Plug flake8 r=sparky
    Differential Revision: https://phabricator.services.mozilla.com/D74886

--- python/mozperftest/mozperftest/browser/browsertime/runner.py
+++ python/mozperftest/mozperftest/browser/browsertime/runner.py
@@ -200,13 +200,16 @@ class BrowsertimeRunner(NodeRunner):
             "Extracts the browser name if any"
             # These are BT arguments, it's BT job to check them
             # here we just want to extract the browser name
-            res = re.findall("(--browser|-b)[= ]([\w]+)", " ".join(args))
+            res = re.findall(r"(--browser|-b)[= ]([\w]+)", " ".join(args))
             if res == []:
                 return None
             return res[0][-1]
 
         def matches(args, *flags):
-            "Return True if any argument matches any of the given flags (maybe with an argument)."
+            """Returns True if any argument matches any of the given flags
+
+            Maybe with an argument.
+            """
             for flag in flags:
                 if flag in args or any(arg.startswith(flag + "=") for arg in args):
                     return True
