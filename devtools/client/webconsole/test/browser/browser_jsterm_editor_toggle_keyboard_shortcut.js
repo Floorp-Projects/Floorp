0443c8078e03737b50b94497467dd942a205e0f3
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-05-12 17:18:46 +0000

    Bug 1631529 - Fix intermittent in browser_jsterm_editor_toggle_keyboard_shortcut.js . r=davidwalsh.
    The test was failing on ccov machines because it was
    timing out. The interesting part is that in the logs
    we could see that a huge error was logged, because
    we were hitting the IGNORED REDUX ACTION middleware,
    which was about a SET_TERMINAL_EAGER_RESULT action
    being blocked.
    I think that logging this object was taking so much
    time that it made the test timeout on slow machines.
    
    So here we're simply waiting for the eager evaluation
    result to be displayed, so we know that the action won't
    be ignored.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74879

--- devtools/client/webconsole/test/browser/browser_jsterm_editor_toggle_keyboard_shortcut.js
+++ devtools/client/webconsole/test/browser/browser_jsterm_editor_toggle_keyboard_shortcut.js
@@ -10,12 +10,15 @@ const TEST_URI =
   "data:text/html;charset=utf-8,Test editor mode toggle keyboard shortcut";
 const EDITOR_PREF = "devtools.webconsole.input.editor";
 
+// See Bug 1631529
+requestLongerTimeout(2);
+
 add_task(async function() {
   // Start with the editor turned off
   await pushPref(EDITOR_PREF, false);
   let hud = await openNewTabAndConsole(TEST_URI);
 
-  const INPUT_VALUE = "hello";
+  const INPUT_VALUE = "`hello`";
   setInputValue(hud, INPUT_VALUE);
 
   is(isEditorModeEnabled(hud), false, "The console isn't in editor mode");
@@ -26,6 +29,8 @@ add_task(async function() {
   is(getInputValue(hud), INPUT_VALUE, "The input value wasn't cleared");
 
   info("Close the console and reopen it");
+  // Wait for eager evaluation result so we don't have a pending call to the server.
+  await waitForEagerEvaluationResult(hud, `"hello"`);
   await closeConsole();
   hud = await openConsole();
   is(isEditorModeEnabled(hud), true, "Editor mode is still enabled");
@@ -49,6 +54,9 @@ add_task(async function() {
   await toggleLayout(hud);
   await onPopupClosed;
   ok(!popup.isOpen, "Auto complete popup is hidden on switching editor modes.");
+
+  // Wait for eager evaluation result so we don't have a pending call to the server.
+  await waitForEagerEvaluationResult(hud, /ReferenceError/);
 
   Services.prefs.clearUserPref(EDITOR_PREF);
 });
