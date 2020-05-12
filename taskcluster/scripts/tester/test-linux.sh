51c82ed11095d05a2f8ddf51a400f38bc8eafe20
Edwin Takahashi <egao@mozilla.com>
2020-05-12 12:20:45 +0000

    Bug 1633101 - remove redundant pulseaudio initializations for linux tests r=jmaher
    Changes:
    
    While the ubuntu1804 migration work was taking place, `pulseaudio` related pieces of code were sprinkled across a wide variety of modules.
    
    Now that things have settled down, remove and streamline `pulseaudio` initialization so that there are less places for this to go wrong.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74809

--- taskcluster/scripts/tester/test-linux.sh
+++ taskcluster/scripts/tester/test-linux.sh
@@ -58,10 +58,7 @@ fail() {
 # start pulseaudio
 maybe_start_pulse() {
     if $NEED_PULSEAUDIO; then
-        # call pulseaudio for Ubuntu only
-        if [ $DISTRIBUTION == "Ubuntu" ]; then
-            pulseaudio --fail --daemonize --start
-        fi
+        pulseaudio --fail --daemonize --start -vvvvv --exit-idle-time=-1
     fi
 }
 
