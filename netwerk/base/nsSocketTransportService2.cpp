bbe6a0bfb5e2423256e413d5a4a5b1c40d37de3e
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-05-12 10:43:35 +0000

    Bug 1592488 - P8. Ensure AbstractThread exists with STS thread. r=valentin,necko-reviewers
    This is necessary to ensure we can use IPC MozPromise with this target thread that requires direct tasks dispatch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74595

--- netwerk/base/nsSocketTransportService2.cpp
+++ netwerk/base/nsSocketTransportService2.cpp
@@ -15,6 +15,7 @@
 #include "prerror.h"
 #include "nsServiceManagerUtils.h"
 #include "nsIObserverService.h"
+#include "mozilla/AbstractThread.h"
 #include "mozilla/Atomics.h"
 #include "mozilla/Services.h"
 #include "mozilla/Likely.h"
@@ -793,6 +794,7 @@ nsresult nsSocketTransportService::ShutdownThread() {
     MutexAutoLock lock(mLock);
     // Drop our reference to mThread and make sure that any concurrent
     // readers are excluded
+    mAbstractThread = nullptr;
     mThread = nullptr;
   }
 
@@ -1042,6 +1044,8 @@ nsSocketTransportService::Run() {
   }
 
   mRawThread = NS_GetCurrentThread();
+  mAbstractThread = AbstractThread::CreateXPCOMThreadWrapper(
+      mRawThread, false /* require taildispatch */);
 
   // hook ourselves up to observe event processing for this thread
   nsCOMPtr<nsIThreadInternal> threadInt = do_QueryInterface(mRawThread);
