aa00e4bd4c9a9b0f25f1ea78540a8cbeaa9a8f44
David Teller <dteller@mozilla.com>
2020-05-12 07:55:39 +0000

    Bug 1593186 - Invoke JS{Parent, Window}Actor.actorCreated;r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D74639

--- dom/ipc/JSWindowActorParent.cpp
+++ dom/ipc/JSWindowActorParent.cpp
@@ -31,6 +31,8 @@ void JSWindowActorParent::Init(const nsACString& aName,
   MOZ_ASSERT(!mManager, "Cannot Init() a JSWindowActorParent twice!");
   SetName(aName);
   mManager = aManager;
+
+  InvokeCallback(CallbackFunction::ActorCreated);
 }
 
 namespace {
