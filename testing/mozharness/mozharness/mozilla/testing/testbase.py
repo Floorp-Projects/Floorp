bac8effea96ba2c90339beb549cc82cedd156059
Chris AtLee <catlee@mozilla.com>
2020-05-13 21:13:51 +0000

    Bug 1557741: Use fetches for tests that need geckodriver. r=jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D74645

--- testing/mozharness/mozharness/mozilla/testing/testbase.py
+++ testing/mozharness/mozharness/mozilla/testing/testbase.py
@@ -648,3 +648,9 @@ Did you run with --create-virtualenv? Is mozinstall in virtualenv_modules?""")
         c = self.config
         if c.get('run_cmd_checks_enabled'):
             self._run_cmd_checks(c.get('postflight_run_cmd_suites', []))
+
+    def query_abs_dirs(self):
+        abs_dirs = super(TestingMixin, self).query_abs_dirs()
+        if 'MOZ_FETCHES_DIR' in os.environ:
+            abs_dirs['abs_fetches_dir'] = os.environ['MOZ_FETCHES_DIR']
+        return abs_dirs
