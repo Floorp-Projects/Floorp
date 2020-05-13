cea3ec8c3bd9c8cd2f94eb0da53a7784ca6d6b6c
Philip Chimento <philip.chimento@gmail.com>
2020-05-13 16:10:25 +0000

    Bug 1506323 - Add JS::PrintError to public API. r=evilpie
    Differential Revision: https://phabricator.services.mozilla.com/D73519

---
+++ js/src/jsapi-tests/testPrintError.cpp
@@ -0,0 +1,100 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include <cstdio>  // fclose, fflush, open_memstream
+
+#include "jsapi.h"           // JS_{Clear,Get}PendingException
+#include "jsfriendapi.h"     // js::ErrorReport
+#include "js/ErrorReport.h"  // JS::PrintError
+#include "js/Warnings.h"     // JS::SetWarningReporter, JS::WarnUTF8
+
+#include "jsapi-tests/tests.h"
+
+class AutoStreamBuffer {
+  char* buffer;
+  size_t size;
+  FILE* fp;
+
+ public:
+  AutoStreamBuffer() { fp = open_memstream(&buffer, &size); }
+
+  ~AutoStreamBuffer() {
+    fclose(fp);
+    free(buffer);
+  }
+
+  FILE* stream() { return fp; }
+
+  bool contains(const char* str) {
+    if (fflush(fp) != 0) {
+      fprintf(stderr, "Error flushing stream\n");
+      return false;
+    }
+    if (strcmp(buffer, str) != 0) {
+      fprintf(stderr, "Expected |%s|, got |%s|\n", str, buffer);
+      return false;
+    }
+    return true;
+  }
+};
+
+BEGIN_TEST(testPrintError_Works) {
+  AutoStreamBuffer buf;
+
+  CHECK(!execDontReport("throw null;", "testPrintError_Works.js", 3));
+
+  JS::RootedValue exception(cx);
+  CHECK(JS_GetPendingException(cx, &exception));
+  JS_ClearPendingException(cx);
+
+  js::ErrorReport report(cx);
+  CHECK(report.init(cx, exception,
+                    js::ErrorReport::SniffingBehavior::NoSideEffects));
+  JS::PrintError(cx, buf.stream(), report.toStringResult(), report.report(),
+                 false);
+
+  CHECK(buf.contains("testPrintError_Works.js:3:1 uncaught exception: null\n"));
+
+  return true;
+}
+END_TEST(testPrintError_Works)
+
+BEGIN_TEST(testPrintError_SkipWarning) {
+  JS::SetWarningReporter(cx, warningReporter);
+  CHECK(JS::WarnUTF8(cx, "warning message"));
+  CHECK(warningSuccess);
+  return true;
+}
+
+static bool warningSuccess;
+
+static void warningReporter(JSContext* cx, JSErrorReport* report) {
+  AutoStreamBuffer buf;
+  JS::PrintError(cx, buf.stream(), JS::ConstUTF8CharsZ(), report, false);
+  warningSuccess = buf.contains("");
+}
+END_TEST(testPrintError_SkipWarning)
+
+bool cls_testPrintError_SkipWarning::warningSuccess = false;
+
+BEGIN_TEST(testPrintError_PrintWarning) {
+  JS::SetWarningReporter(cx, warningReporter);
+  CHECK(JS::WarnUTF8(cx, "warning message"));
+  CHECK(warningSuccess);
+  return true;
+}
+
+static bool warningSuccess;
+
+static void warningReporter(JSContext* cx, JSErrorReport* report) {
+  AutoStreamBuffer buf;
+  JS::PrintError(cx, buf.stream(), JS::ConstUTF8CharsZ(), report, true);
+  warningSuccess = buf.contains("warning: warning message\n");
+}
+END_TEST(testPrintError_PrintWarning)
+
+bool cls_testPrintError_PrintWarning::warningSuccess = false;
