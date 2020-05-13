61021a142702c4c0c9d7dbba13f966a4e91e8ec2
Brindusan Cristian <cbrindusan@mozilla.com>
2020-05-14 00:14:16 +0300

    Backed out changeset 1c4b489b6aa0 (bug 1309824) for dt failures at browser_tableWidget_basic.js. CLOSED TREE

--- devtools/client/storage/test/head.js
+++ devtools/client/storage/test/head.js
@@ -545,16 +545,9 @@ async function selectTreeItem(ids) {
 
   // The item exists but is not selected... select it.
   info(`Selecting "${ids}".`);
-  if (ids.length > 1) {
-    const updated = gUI.once("store-objects-updated");
-    gUI.tree.selectedItem = ids;
-    await updated;
-  } else {
-    // If the length of the IDs array is 1, a storage type
-    // gets selected and no 'store-objects-updated' event
-    // will be fired in that case.
-    gUI.tree.selectedItem = ids;
-  }
+  const updated = gUI.once("store-objects-updated");
+  gUI.tree.selectedItem = ids;
+  await updated;
 }
 
 /**
