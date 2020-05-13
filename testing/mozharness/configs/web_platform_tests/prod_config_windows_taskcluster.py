51984d5c6c7359ab8e55c841a10aa49da5a2bf64
Chris AtLee <catlee@mozilla.com>
2020-05-13 21:13:51 +0000

    Bug 1557741: Use fetches for tests that need geckodriver. r=jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D74645

--- testing/mozharness/configs/web_platform_tests/prod_config_windows_taskcluster.py
+++ testing/mozharness/configs/web_platform_tests/prod_config_windows_taskcluster.py
@@ -105,8 +105,7 @@ config = {
         },
     ],
 
-    # this would normally be in "exes", but "exes" is clobbered by remove_executables
-    "geckodriver": os.path.join("%(abs_test_bin_dir)s", "geckodriver.exe"),
+    "geckodriver": os.path.join("%(abs_fetches_dir)s", "geckodriver.exe"),
 
     "per_test_category": "web-platform",
 }
