f6e0d86cb533da70866895b65119590c73f19ada
Brindusan Cristian <cbrindusan@mozilla.com>
2020-05-14 00:14:16 +0300

    Backed out changeset 1c4b489b6aa0 (bug 1309824) for dt failures at browser_tableWidget_basic.js. CLOSED TREE

--- devtools/client/shared/widgets/TableWidget.js
+++ devtools/client/shared/widgets/TableWidget.js
@@ -104,11 +104,10 @@ function TableWidget(node, options = {}) {
   this.afterScroll = this.afterScroll.bind(this);
   this.tbody.addEventListener("scroll", this.onScroll.bind(this));
 
-  // Prepare placeholder
-  this.placeholder = this.document.createElement("div");
+  this.placeholder = this.document.createXULElement("label");
   this.placeholder.className = "plain table-widget-empty-text";
+  this.placeholder.setAttribute("flex", "1");
   this._parent.appendChild(this.placeholder);
-  this.setPlaceholder(this.emptyText);
 
   this.items = new Map();
   this.columns = new Map();
@@ -122,6 +121,8 @@ function TableWidget(node, options = {}) {
 
   if (initialColumns) {
     this.setColumns(initialColumns, uniqueId);
+  } else if (this.emptyText) {
+    this.setPlaceholderText(this.emptyText);
   }
 
   this.bindSelectedRow = id => {
@@ -655,30 +656,10 @@ TableWidget.prototype = {
   },
 
   /**
-   * Sets the localization ID of the description to be shown when the table is empty.
-   *
-   * @param {String} l10nID
-   *        The ID of the localization string.
-   * @param {String} learnMoreURL
-   *        A URL referring to a website with further information related to
-   *        the data shown in the table widget.
+   * Sets the text to be shown when the table is empty.
    */
-  setPlaceholder: function(l10nID, learnMoreURL) {
-    if (learnMoreURL) {
-      let placeholderLink = this.placeholder.firstElementChild;
-      if (!placeholderLink) {
-        placeholderLink = this.document.createElement("a");
-        placeholderLink.setAttribute("target", "_blank");
-        placeholderLink.setAttribute("data-l10n-name", "learn-more-link");
-        this.placeholder.appendChild(placeholderLink);
-      }
-      placeholderLink.setAttribute("href", learnMoreURL);
-    } else {
-      // Remove link element if no learn more URL is given
-      this.placeholder.firstElementChild?.remove();
-    }
-
-    this.document.l10n.setAttributes(this.placeholder, l10nID);
+  setPlaceholderText: function(text) {
+    this.placeholder.setAttribute("value", text);
   },
 
   /**
@@ -974,7 +955,7 @@ TableWidget.prototype = {
       column.clear();
     }
     this.tbody.setAttribute("empty", "empty");
-    this.setPlaceholder(this.emptyText);
+    this.setPlaceholderText(this.emptyText);
 
     this.selectedRow = null;
 
