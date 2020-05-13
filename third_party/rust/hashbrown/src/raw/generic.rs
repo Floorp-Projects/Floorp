1c4075755ad23ef2e3d174fdee0e95fcaa71dadc
Mike Hommey <mh+mozilla@glandium.org>
2020-05-13 15:41:13 +0000

    Bug 1637140 - Upgrade hashbrown to 0.7.2. r=emilio
    The upgrade of mp4parse pulled some code that previously was eliminated as
    dead code in libxul. That code was unfortunately unreproducible as it
    was generating a random seed at compile time. This was due to hashbrown
    enabling the ahash feature that does that, but that was changed in 0.7.2.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75033

--- third_party/rust/hashbrown/src/raw/generic.rs
+++ third_party/rust/hashbrown/src/raw/generic.rs
@@ -27,11 +27,7 @@ pub const BITMASK_MASK: BitMaskWord = 0x8080_8080_8080_8080_u64 as GroupWord;
 /// Helper function to replicate a byte across a `GroupWord`.
 #[inline]
 fn repeat(byte: u8) -> GroupWord {
-    let repeat = GroupWord::from(byte);
-    let repeat = repeat | repeat.wrapping_shl(8);
-    let repeat = repeat | repeat.wrapping_shl(16);
-    // This last line is a no-op with a 32-bit GroupWord
-    repeat | repeat.wrapping_shl(32)
+    GroupWord::from_ne_bytes([byte; Group::WIDTH])
 }
 
 /// Abstraction over a group of control bytes which can be scanned in
