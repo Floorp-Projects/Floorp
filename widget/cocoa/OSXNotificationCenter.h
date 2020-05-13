97d76faca65f59a2d2b8ccc2e37784f230cfbc6a
Mike Conley <mconley@mozilla.com>
2020-05-13 21:10:12 +0000

    Bug 1635257 - Implement some of nsIAlertsDoNotDisturb for the macOS NotificationCenter backend. r=mstange
    This doesn't implement manualDoNotDisturb, since we don't want to show the
    about:preferences option for suppressing notifications on macOS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74243

--- widget/cocoa/OSXNotificationCenter.h
+++ widget/cocoa/OSXNotificationCenter.h
@@ -28,11 +28,13 @@ class OSXNotificationInfo;
 
 class OSXNotificationCenter : public nsIAlertsService,
                               public nsIAlertsIconData,
+                              public nsIAlertsDoNotDisturb,
                               public nsIAlertNotificationImageListener {
  public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIALERTSSERVICE
   NS_DECL_NSIALERTSICONDATA
+  NS_DECL_NSIALERTSDONOTDISTURB
   NS_DECL_NSIALERTNOTIFICATIONIMAGELISTENER
 
   OSXNotificationCenter();
@@ -50,6 +52,7 @@ class OSXNotificationCenter : public nsIAlertsService,
   mozNotificationCenterDelegate* mDelegate;
   nsTArray<RefPtr<OSXNotificationInfo> > mActiveAlerts;
   nsTArray<RefPtr<OSXNotificationInfo> > mPendingAlerts;
+  bool mSuppressForScreenSharing;
 };
 
 }  // namespace mozilla
