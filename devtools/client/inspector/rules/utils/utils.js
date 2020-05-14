05f5e664d651ee29a9f7966b656145c5b367fa44
Razvan Caliman <rcaliman@mozilla.com>
2020-05-14 18:20:00 +0000

    Bug 1623988 - Extract CSS variables used in a CSS declaration for quick reference r=jdescottes
    Inspecting pages with many CSS variables makes DevTools Inspector very slow.
    For example, try to inspect an element on [youtube.com](https://www.youtube.com/).
    
    One of the stylesheets loaded by youtube.com contains more than 7800 CSS rules (it's 55K lines long). Some of them specify a very large number of CSS variable delcarations on `:root` which will be inherited by all nodes on the page. This is a perf bottleneck for DevTools.
    {F2150398}
    
    Whenever an element is selected, its matching CSS declarations show in the Rules view. If the node inherits CSS variables, for **each declaration**, the Rules view checks if the [variables apply to the current declaration](https://searchfox.org/mozilla-central/rev/2bfe3415fb3a2fba9b1c694bc0b376365e086927/devtools/client/inspector/rules/models/element-style.js#403-408,417,424).
    
    This among other expensive operations which are repeated for each declaration. But for CSS variables, it's particularly slow given the frequent [checks with a regular expression](https://searchfox.org/mozilla-central/rev/2bfe3415fb3a2fba9b1c694bc0b376365e086927/devtools/client/inspector/rules/utils/utils.js#292-295) to verify if a CSS declaration's value includes any references to a given CSS variable name.
    
    This patch introduces new functionality. When a `TextProperty` (aka CSS declaration model) is created on the client, the names of CSS variables found in its value are extracted ahead of time into a `Set`. Whenever we want to check if the declaration uses a certain CSS variable name, we check against this pre-compiled list instead of verifying the string value every time. For high volume checks, like the ones done by the `ElementStyle`, this approach proves significantly faster.
    
    Profile without this patch: https://bit.ly/2Yd7khD
    Profile with this patch: https://bit.ly/3aMhJTZ
    
    There are still many optimizations that can be done, but the `hasCSSVariable()` method is no longer a performance bottleneck.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73062

--- devtools/client/inspector/rules/utils/utils.js
+++ devtools/client/inspector/rules/utils/utils.js
@@ -149,7 +149,7 @@ function getNodeInfo(node, elementStyle) {
     type = VIEW_NODE_VARIABLE_TYPE;
     value = {
       property: getPropertyNameAndValue(node).name,
-      value: node.textContent,
+      value: node.textContent.trim(),
       enabled: declaration.enabled,
       overridden: declaration.overridden,
       pseudoElement: rule.pseudoElement,
@@ -280,8 +280,41 @@ function getShapePoint(node) {
 }
 
 /**
- * Returns true if the given property value is a CSS variables and contains the given
- * variable name, and false otherwise.
+ * Returns an array of CSS variables used in a CSS property value.
+ * If no CSS variables are used, returns an empty array.
+ *
+ * @param {String} propertyValue
+ *        CSS property value (e.g. "1px solid var(--color, blue)")
+ * @return {Array}
+ *         List of variable names (e.g. ["--color"])
+ *
+ */
+function getCSSVariables(propertyValue = "") {
+  const variables = [];
+  const parts = propertyValue.split(/var\(\s*--/);
+
+  if (parts.length) {
+    // Skip first part. It is the substring before the first occurence of "var(--"
+    for (let i = 1; i < parts.length; i++) {
+      // Split the part by any of the following characters expected after a variable name:
+      // comma, closing parenthesis or whitespace.
+      // Take just the first match. Anything else is either:
+      // - the fallback value, ex: ", blue" from "var(--color, blue)"
+      // - the closing parenthesis, ex: ")" from "var(--color)"
+      const variable = parts[i].split(/[,)\s+]/).shift();
+
+      if (variable) {
+        // Add back the double-dash. The initial string was split by "var(--"
+        variables.push(`--${variable}`);
+      }
+    }
+  }
+
+  return variables;
+}
+
+/**
+ * Returns true if the given CSS property value contains the given variable name.
  *
  * @param {String} propertyValue
  *        CSS property value (e.g. "var(--color)")
@@ -290,11 +323,11 @@ function getShapePoint(node) {
  * @return {Boolean}
  */
 function hasCSSVariable(propertyValue, variableName) {
-  const regex = new RegExp(`(^|\\W)var\\(${variableName}\\s*[,)]`);
-  return regex.test(propertyValue);
+  return getCSSVariables(propertyValue).includes(variableName);
 }
 
 module.exports = {
+  getCSSVariables,
   getNodeInfo,
   getRuleFromNode,
   hasCSSVariable,
