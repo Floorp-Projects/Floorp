236a9cb549be8ec3785664cbd9b04ceb0d5a731e
alwu <alwu@mozilla.com>
2020-05-14 09:33:32 +0000

    Bug 1627999 - part7 : add test. r=bryce
    This patch will do :
    - add test cases
    - introduce the test-only notification `media-displayed-metadata-changed` when the event source updates its metadata
    
    The advantage of doing so :
    - increase test coverage
    
    Differential Revision: https://phabricator.services.mozilla.com/D72501

--- dom/media/mediacontrol/tests/head.js
+++ dom/media/mediacontrol/tests/head.js
@@ -215,6 +215,35 @@ function isCurrentMetadataEqualTo(metadata) {
 }
 
 /**
+ * Check if the given tab is using the default metadata. If the tab is being
+ * used in the private browsing mode, `isPrivateBrowsing` should be definded in
+ * the `options`.
+ */
+async function isUsingDefaultMetadata(tab, options = {}) {
+  let metadata = ChromeUtils.getCurrentActiveMediaMetadata();
+  if (options.isPrivateBrowsing) {
+    is(
+      metadata.title,
+      "Firefox is playing media",
+      "Using generic title to not expose sensitive information"
+    );
+  } else {
+    await SpecialPowers.spawn(tab.linkedBrowser, [metadata.title], title => {
+      is(
+        title,
+        content.document.title,
+        "Using website title as a default title"
+      );
+    });
+  }
+  is(metadata.artwork.length, 1, "Default metada contains one artwork");
+  ok(
+    metadata.artwork[0].src.includes("defaultFavicon.svg"),
+    "Using default favicon as a default art work"
+  );
+}
+
+/**
  * Wait until the main media controller changes its playback state, we would
  * observe that by listening for `media-displayed-playback-changed`
  * notification.
@@ -224,6 +253,18 @@ function isCurrentMetadataEqualTo(metadata) {
  */
 function waitUntilDisplayedPlaybackChanged() {
   return BrowserUtils.promiseObserved("media-displayed-playback-changed");
+}
+
+/**
+ * Wait until the metadata that would be displayed on the virtual control
+ * interface changes. we would observe that by listening for
+ * `media-displayed-metadata-changed` notification.
+ *
+ * @return {Promise}
+ *         Resolve when observing `media-displayed-metadata-changed`
+ */
+function waitUntilDisplayedMetadataChanged() {
+  return BrowserUtils.promiseObserved("media-displayed-metadata-changed");
 }
 
 /**
@@ -238,9 +279,12 @@ function waitUntilMainMediaControllerChanged() {
 }
 
 /**
- * Wait until the main session controller changes its metadata, we would observe
- * that by listening for `media-session-controller-metadata-changed`
- * notification.
+ * Wait until any media controller updates its metadata even if it's not the
+ * main controller. The difference between this function and
+ * `waitUntilDisplayedMetadataChanged()` is that the changed metadata might come
+ * from non-main controller so it won't be show on the virtual control
+ * interface. we would observe that by listening for
+ * `media-session-controller-metadata-changed` notification.
  *
  * @return {Promise}
  *         Resolve when observing `media-session-controller-metadata-changed`
