cc10ff94b63afc808c8d7c135b52e044cc3bf01d
Karl Tomlinson <karlt+@karlt.net>
2020-05-14 19:51:55 +0000

    Bug 1637159 rename BindToOwnerInternal() to BindToOwner() r=smaug
    This method can be and is used by derived classes as well as DOMEventTargetHelper
    itself, so it does not need to be "Internal".
    
    Differential Revision: https://phabricator.services.mozilla.com/D75246

--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -156,7 +156,7 @@ WorkerGlobalScopeBase::WorkerGlobalScopeBase(
 
   // In workers, each DETH must have an owner. Because the global scope doesn't
   // have one, let's set it as owner of itself.
-  BindToOwnerInternal(static_cast<nsIGlobalObject*>(this));
+  BindToOwner(static_cast<nsIGlobalObject*>(this));
 }
 
 JSObject* WorkerGlobalScopeBase::GetGlobalJSObject() {
