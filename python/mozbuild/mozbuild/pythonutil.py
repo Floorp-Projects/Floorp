d845cf5481629e98c42027a51b065395b243033c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-05-14 18:28:55 +0000

    Bug 1637911 - Skip modules with a None __file__ attribute. r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D75267

--- python/mozbuild/mozbuild/pythonutil.py
+++ python/mozbuild/mozbuild/pythonutil.py
@@ -18,7 +18,7 @@ def iter_modules_in_path(*paths):
     paths = [os.path.abspath(os.path.normcase(p)) + os.sep
              for p in paths]
     for name, module in sys.modules.items():
-        if not hasattr(module, '__file__'):
+        if getattr(module, '__file__', None) is None:
             continue
 
         path = module.__file__
