c72c52bc383e984a6bc1a519cd7b429eea804a46
Jon Coppeard <jcoppeard@mozilla.com>
2020-05-15 07:14:00 +0000

    Bug 1637663 - Remove SCC telemetry r=sfink
    This was added for incremental sweeping of groups of zones (or compartments as they were then).  This has been working fine for a long time and I don't think we need to continue collecting telemetry about it.
    
    This leaves the data collection in place as it's still reported to the profile (which makes more sense I think).
    
    Differential Revision: https://phabricator.services.mozilla.com/D75147

--- js/xpconnect/src/XPCJSRuntime.cpp
+++ js/xpconnect/src/XPCJSRuntime.cpp
@@ -2642,12 +2642,6 @@ static void AccumulateTelemetryCallback(int id, uint32_t sample,
     case JS_TELEMETRY_GC_NON_INCREMENTAL_REASON:
       Telemetry::Accumulate(Telemetry::GC_NON_INCREMENTAL_REASON, sample);
       break;
-    case JS_TELEMETRY_GC_SCC_SWEEP_TOTAL_MS:
-      Telemetry::Accumulate(Telemetry::GC_SCC_SWEEP_TOTAL_MS, sample);
-      break;
-    case JS_TELEMETRY_GC_SCC_SWEEP_MAX_PAUSE_MS:
-      Telemetry::Accumulate(Telemetry::GC_SCC_SWEEP_MAX_PAUSE_MS, sample);
-      break;
     case JS_TELEMETRY_GC_MINOR_REASON:
       Telemetry::Accumulate(Telemetry::GC_MINOR_REASON, sample);
       break;
