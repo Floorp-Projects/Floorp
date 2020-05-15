a73ea439b28a02265ae27f3ffa62b0a4ab60fee6
Aaron Klotz <aklotz@mozilla.com>
2020-05-15 19:47:56 +0000

    Bug 1630229: Part 5 - GeckoProcessManager: When start fails, chain resolution of the result promise to the bind shutdown promise; r=geckoview-reviewers,agi
    Currently `unbind()` is more or less synchronous, but since those semantics may
    change in the future, for the sake of cleanliness we should ensure that our
    reaction is linked to `unbindResult`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75414

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoProcessManager.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/process/GeckoProcessManager.java
@@ -521,6 +521,32 @@ public final class GeckoProcessManager extends IProcessManager.Stub {
             });
     }
 
+    private void acceptUnbindFailure(@NonNull final GeckoResult<Void> unbindResult,
+                                     @NonNull final GeckoResult<Integer> finalResult,
+                                     final RemoteException exception,
+                                     @NonNull final GeckoProcessType type,
+                                     final boolean isRetry) {
+        unbindResult.accept(null, error -> {
+            final StringBuilder builder = new StringBuilder("Failed to unbind");
+            if (isRetry) {
+                builder.append(": ");
+            } else {
+                builder.append(" before child restart: ");
+            }
+
+            builder.append(error.toString());
+            if (exception != null) {
+                builder.append("; In response to RemoteException: ");
+                builder.append(exception.toString());
+            }
+
+            builder.append("; Type = ");
+            builder.append(type.toString());
+
+            finalResult.completeExceptionally(new RuntimeException(builder.toString()));
+        });
+    }
+
     private void start(final GeckoResult<Integer> result,
                        final ChildConnection connection,
                        final IChildProcess child,
@@ -575,7 +601,11 @@ public final class GeckoProcessManager extends IProcessManager.Stub {
         // Whether retrying or not, we should always unbind connection so that it gets cleaned up.
         final GeckoResult<Void> unbindResult = connection.unbind();
 
+        // We always complete result exceptionally if the unbind fails
+        acceptUnbindFailure(unbindResult, result, exception, type, isRetry);
+
         if (isRetry) {
+            // If we've already retried, just assemble an error message and completeExceptionally.
             Log.e(LOGTAG, "Cannot restart child " + type.toString());
             final StringBuilder builder = new StringBuilder("Cannot restart child.");
             if (prevException != null) {
@@ -590,28 +620,21 @@ public final class GeckoProcessManager extends IProcessManager.Stub {
                 builder.append(" No exceptions thrown; type = ");
                 builder.append(type.toString());
             }
-            result.completeExceptionally(new RuntimeException(builder.toString()));
+
+            final RuntimeException completionException = new RuntimeException(builder.toString());
+            unbindResult.accept(v -> {
+                result.completeExceptionally(completionException);
+            });
             return;
         }
 
-        final RemoteException captureException = exception;
+        // Attempt to retry the connection once we've finished unbinding.
         Log.w(LOGTAG, "Attempting to kill running child " + type.toString());
+        final RemoteException captureException = exception;
         unbindResult.accept(v -> {
             start(result, type, args, extras, flags, prefsFd, prefMapFd, ipcFd,
                   crashFd, crashAnnotationFd, /* isRetry */ true, captureException);
-        }, error -> {
-                final StringBuilder builder = new StringBuilder("Failed to unbind before child restart: ");
-                builder.append(error.toString());
-                if (captureException != null) {
-                    builder.append("; In response to RemoteException: ");
-                    builder.append(captureException.toString());
-                }
-
-                builder.append("; Type = ");
-                builder.append(type.toString());
-
-                result.completeExceptionally(new RuntimeException(builder.toString()));
-            });
+        });
     }
 
 } // GeckoProcessManager
