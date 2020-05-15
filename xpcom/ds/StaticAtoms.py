0525fdf4d5fa4b0d022de80522f23ec138a56119
Sebastian Streich <sstreich@mozilla.com>
2020-05-15 14:03:43 +0000

    Bug 1558394 - Block downloads in sandboxed iframes r=ckerschb,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D67351

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -76,6 +76,7 @@ STATIC_ATOMS = [
     Atom("all", "all"),
     Atom("allow", "allow"),
     Atom("allowdirs", "allowdirs"),
+    Atom("allowdownloads", "allow-downloads"),
     Atom("allowevents", "allowevents"),
     Atom("allowforms", "allow-forms"),
     Atom("allowfullscreen", "allowfullscreen"),
