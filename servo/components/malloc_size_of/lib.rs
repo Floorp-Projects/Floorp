0be4d1e51cd283b6b57680628bf6c52661570570
Simon Sapin <simon.sapin@exyr.org>
2020-05-15 09:16:50 +0000

    Bug 1633410 - Revert "Bug 1631721 - Remove hashglobe". r=manishearth a=pascalc
    This reverts commit 62cfd95c4f71cecfff0dbfef31ecdec599095549.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75486

--- servo/components/malloc_size_of/lib.rs
+++ servo/components/malloc_size_of/lib.rs
@@ -56,6 +56,7 @@ extern crate crossbeam_channel;
 extern crate cssparser;
 extern crate euclid;
 extern crate hashbrown;
+extern crate hashglobe;
 #[cfg(feature = "servo")]
 extern crate hyper;
 #[cfg(feature = "servo")]
@@ -488,6 +489,8 @@ macro_rules! malloc_size_of_hash_set {
 
 malloc_size_of_hash_set!(std::collections::HashSet<T, S>);
 malloc_size_of_hash_set!(hashbrown::HashSet<T, S>);
+malloc_size_of_hash_set!(hashglobe::hash_set::HashSet<T, S>);
+malloc_size_of_hash_set!(hashglobe::fake::HashSet<T, S>);
 
 macro_rules! malloc_size_of_hash_map {
     ($ty:ty) => {
@@ -528,6 +531,8 @@ macro_rules! malloc_size_of_hash_map {
 
 malloc_size_of_hash_map!(std::collections::HashMap<K, V, S>);
 malloc_size_of_hash_map!(hashbrown::HashMap<K, V, S>);
+malloc_size_of_hash_map!(hashglobe::hash_map::HashMap<K, V, S>);
+malloc_size_of_hash_map!(hashglobe::fake::HashMap<K, V, S>);
 
 impl<K, V> MallocShallowSizeOf for std::collections::BTreeMap<K, V>
 where
