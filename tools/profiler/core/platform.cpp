7b6222fa7d9e1fd4562d9f801288c5cd3c965533
Aaron Klotz <aklotz@mozilla.com>
2020-05-15 17:04:19 +0000

    Bug 1637452: Part 5 - Fix JNI includes in tools/profiler; r=mstange
    In this bug we're moving away from monolithic JNI headers to class-specific
    headers so that we don't have to rebuild the world every time we make a change
    to a JNI interface.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75368

--- tools/profiler/core/platform.cpp
+++ tools/profiler/core/platform.cpp
@@ -94,8 +94,7 @@
 #endif
 
 #if defined(GP_OS_android)
-#  include "GeneratedJNINatives.h"
-#  include "GeneratedJNIWrappers.h"
+#  include "mozilla/java/GeckoJavaSamplerNatives.h"
 #endif
 
 // Win32 builds always have frame pointers, so FramePointerStackWalk() always
