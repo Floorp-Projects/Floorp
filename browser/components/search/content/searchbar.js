beb1e353967e2b3a4e7702b8bd009165ee9fee82
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-05-15 13:47:41 +0000

    Bug 1638264 - fix search bar width getting lost due to it starting out in the palette, r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D75536

--- browser/components/search/content/searchbar.js
+++ browser/components/search/content/searchbar.js
@@ -81,6 +81,16 @@
         return;
       }
 
+      // Ensure we get persisted widths back, if we've been in the palette:
+      let storedWidth = Services.xulStore.getValue(
+        document.documentURI,
+        this.parentNode.id,
+        "width"
+      );
+      if (storedWidth) {
+        this.parentNode.setAttribute("width", storedWidth);
+      }
+
       this._stringBundle = this.querySelector("stringbundle");
       this._textbox = this.querySelector(".searchbar-textbox");
 
---
+++ browser/components/search/test/browser/browser_searchbar_widths.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Check that when the searchbar has a specific width, opening a new window
+// honours that specific width.
+add_task(async function test_searchbar_width_persistence() {
+  await gCUITestUtils.addSearchBar();
+  registerCleanupFunction(async function() {
+    gCUITestUtils.removeSearchBar();
+  });
+
+  // Really, we should use the splitter, but drag/drop is hard and fragile in
+  // tests, so let's just fake it real quick:
+  let container = BrowserSearch.searchBar.parentNode;
+  // There's no width attribute set initially, just grab the info from layout:
+  let oldWidth = container.getBoundingClientRect().width;
+  let newWidth = "" + Math.round(oldWidth * 2);
+  container.setAttribute("width", newWidth);
+
+  let win = await BrowserTestUtils.openNewBrowserWindow();
+  let otherBar = win.BrowserSearch.searchBar;
+  ok(otherBar, "Should have a search bar in the other window");
+  if (otherBar) {
+    is(
+      otherBar.parentNode.getAttribute("width"),
+      newWidth,
+      "Should have matching width"
+    );
+  }
+  await BrowserTestUtils.closeWindow(win);
+});
