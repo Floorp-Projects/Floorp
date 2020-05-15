2dc3527f1b680781b8a4cf0edf33d75c30c1af85
Tom Schuster <evilpies@gmail.com>
2020-05-15 20:11:30 +0000

    Bug 1636590 - Provide an exception stack for uncaught promises rejections. r=baku
    This will improve error message information when the rejection value is not an erorr object.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74479

--- dom/promise/Promise.cpp
+++ dom/promise/Promise.cpp
@@ -513,10 +513,6 @@ void Promise::ReportRejectedPromise(JSContext* aCx, JS::HandleObject aPromise) {
 
   MOZ_ASSERT(JS::GetPromiseState(aPromise) == JS::PromiseState::Rejected);
 
-  JS::Rooted<JS::Value> result(aCx, JS::GetPromiseResult(aPromise));
-
-  RefPtr<xpc::ErrorReport> xpcReport = new xpc::ErrorReport();
-
   bool isChrome = false;
   nsGlobalWindowInner* win = nullptr;
   uint64_t innerWindowID = 0;
@@ -537,11 +533,17 @@ void Promise::ReportRejectedPromise(JSContext* aCx, JS::HandleObject aPromise) {
     }
   }
 
+  JS::Rooted<JS::Value> result(aCx, JS::GetPromiseResult(aPromise));
+  // resolutionSite can be null if async stacks are disabled.
+  JS::Rooted<JSObject*> resolutionSite(aCx,
+                                       JS::GetPromiseResolutionSite(aPromise));
+
   // We're inspecting the rejection value only to report it to the console, and
   // we do so without side-effects, so we can safely unwrap it without regard to
   // the privileges of the Promise object that holds it. If we don't unwrap
   // before trying to create the error report, we wind up reporting any
   // cross-origin objects as "uncaught exception: Object".
+  RefPtr<xpc::ErrorReport> xpcReport = new xpc::ErrorReport();
   {
     Maybe<JSAutoRealm> ar;
     JS::Rooted<JS::Value> unwrapped(aCx, result);
@@ -557,7 +559,8 @@ void Promise::ReportRejectedPromise(JSContext* aCx, JS::HandleObject aPromise) {
          NS_SUCCEEDED(UNWRAP_OBJECT(Exception, &unwrapped, exn)))) {
       xpcReport->Init(aCx, exn, isChrome, innerWindowID);
     } else {
-      JS::ExceptionStack exnStack(aCx, unwrapped, nullptr);
+      // Use the resolution site as the exception stack
+      JS::ExceptionStack exnStack(aCx, unwrapped, resolutionSite);
       if (!report.init(aCx, exnStack, JS::ErrorReportBuilder::NoSideEffects)) {
         JS_ClearPendingException(aCx);
         return;
@@ -574,6 +577,9 @@ void Promise::ReportRejectedPromise(JSContext* aCx, JS::HandleObject aPromise) {
     if (!win->IsDying()) {
       // Exceptions from a dying window will cause the window to leak.
       event->SetException(aCx, result);
+      if (resolutionSite) {
+        event->SerializeStack(aCx, resolutionSite);
+      }
     }
     win->Dispatch(mozilla::TaskCategory::Other, event.forget());
   } else {
