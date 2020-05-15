1e0f09911c3bdd6d0f1292d1d50f65c84973b4d9
Andrew Osmond <aosmond@mozilla.com>
2020-05-15 08:39:43 +0000

    Bug 1638011 - Block WebRender in release for high refresh rate monitors. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D75450

--- widget/GfxInfoBase.h
+++ widget/GfxInfoBase.h
@@ -88,6 +88,7 @@ class GfxInfoBase : public nsIGfxInfo,
   virtual nsresult Init();
 
   NS_IMETHOD_(void) GetData() override;
+  NS_IMETHOD_(int32_t) GetMaxRefreshRate() override { return -1; }
 
   static void AddCollector(GfxInfoCollectorBase* collector);
   static void RemoveCollector(GfxInfoCollectorBase* collector);
