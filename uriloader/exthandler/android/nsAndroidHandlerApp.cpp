b6a9c67e2af1c6b80242fc1562d4127b34241714
Aaron Klotz <aklotz@mozilla.com>
2020-05-15 17:04:17 +0000

    Bug 1637452: Part 4 - Fix JNI includes in uriloader; r=nika
    In this bug we're moving away from monolithic JNI headers to class-specific
    headers so that we don't have to rebuild the world every time we make a change
    to a JNI interface.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75367

--- uriloader/exthandler/android/nsAndroidHandlerApp.cpp
+++ uriloader/exthandler/android/nsAndroidHandlerApp.cpp
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/java/GeckoAppShellWrappers.h"
 #include "nsAndroidHandlerApp.h"
-#include "GeneratedJNIWrappers.h"
 
 using namespace mozilla;
 
