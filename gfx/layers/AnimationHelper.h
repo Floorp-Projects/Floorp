b2a0fb70039059b26bcc34b651fe4f1839b770c0
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-05-15 21:30:25 +0000

    Bug 1638201 - Reuse AnimationStorageData in AnimationInfo. r=boris
    Differential Revision: https://phabricator.services.mozilla.com/D75453

--- gfx/layers/AnimationHelper.h
+++ gfx/layers/AnimationHelper.h
@@ -8,7 +8,8 @@
 #define mozilla_layers_AnimationHelper_h
 
 #include "mozilla/dom/Nullable.h"
-#include "mozilla/ComputedTimingFunction.h"    // for ComputedTimingFunction
+#include "mozilla/ComputedTimingFunction.h"  // for ComputedTimingFunction
+#include "mozilla/layers/AnimationStorageData.h"
 #include "mozilla/layers/LayersMessages.h"     // for TransformData, etc
 #include "mozilla/webrender/WebRenderTypes.h"  // for RenderRoot
 #include "mozilla/TimeStamp.h"                 // for TimeStamp
@@ -28,50 +29,6 @@ namespace layers {
 class Animation;
 
 typedef nsTArray<layers::Animation> AnimationArray;
-
-struct PropertyAnimation {
-  struct SegmentData {
-    RefPtr<RawServoAnimationValue> mStartValue;
-    RefPtr<RawServoAnimationValue> mEndValue;
-    Maybe<mozilla::ComputedTimingFunction> mFunction;
-    float mStartPortion;
-    float mEndPortion;
-    dom::CompositeOperation mStartComposite;
-    dom::CompositeOperation mEndComposite;
-  };
-  nsTArray<SegmentData> mSegments;
-  TimingParams mTiming;
-
-  // These two variables correspond to the variables of the same name in
-  // KeyframeEffectReadOnly and are used for the same purpose: to skip composing
-  // animations whose progress has not changed.
-  dom::Nullable<double> mProgressOnLastCompose;
-  uint64_t mCurrentIterationOnLastCompose = 0;
-  // These two variables are used for a similar optimization above but are
-  // applied to the timing function in each keyframe.
-  uint32_t mSegmentIndexOnLastCompose = 0;
-  dom::Nullable<double> mPortionInSegmentOnLastCompose;
-
-  TimeStamp mOriginTime;
-  Maybe<TimeDuration> mStartTime;
-  TimeDuration mHoldTime;
-  float mPlaybackRate;
-  dom::IterationCompositeOperation mIterationComposite;
-  bool mIsNotPlaying;
-};
-
-struct PropertyAnimationGroup {
-  nsCSSPropertyID mProperty;
-
-  nsTArray<PropertyAnimation> mAnimations;
-  RefPtr<RawServoAnimationValue> mBaseStyle;
-
-  bool IsEmpty() const { return mAnimations.IsEmpty(); }
-  void Clear() {
-    mAnimations.Clear();
-    mBaseStyle = nullptr;
-  }
-};
 
 struct AnimationTransform {
   /*
@@ -113,20 +70,6 @@ struct AnimatedValue final {
 
  private:
   AnimatedValueType mValue;
-};
-
-struct AnimationStorageData {
-  nsTArray<PropertyAnimationGroup> mAnimation;
-  Maybe<TransformData> mTransformData;
-  RefPtr<gfx::Path> mCachedMotionPath;
-
-  AnimationStorageData() = default;
-  AnimationStorageData(AnimationStorageData&& aOther) = default;
-  AnimationStorageData& operator=(AnimationStorageData&& aOther) = default;
-
-  // Avoid any copy because mAnimation could be a large array.
-  AnimationStorageData(const AnimationStorageData& aOther) = delete;
-  AnimationStorageData& operator=(const AnimationStorageData& aOther) = delete;
 };
 
 // CompositorAnimationStorage stores the animations and animated values
