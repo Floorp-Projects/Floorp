f370299b89233203ec2829a2d4ef74462fc315b4
Jared Wein <jwein@mozilla.com>
2020-05-16 06:00:03 +0000

    Bug 773942 - Add tests that cover the generic PDF description and the 'open with (internal handler)' option of the Unknown Content Type dialog. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D73537

---
+++ uriloader/exthandler/tests/unit/test_getMIMEInfo_pdf.js
@@ -0,0 +1,27 @@
+/* Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+XPCOMUtils.defineLazyServiceGetter(
+  this,
+  "gMIMEService",
+  "@mozilla.org/mime;1",
+  "nsIMIMEService"
+);
+
+// PDF files should always have a generic description instead
+// of relying on what is registered with the Operating System.
+add_task(async function test_check_unknown_mime_type() {
+  const mimeService = Cc["@mozilla.org/mime;1"].getService(Ci.nsIMIMEService);
+  let pdfType = mimeService.getTypeFromExtension("pdf");
+  Assert.equal(pdfType, "application/pdf");
+  let extension = mimeService.getPrimaryExtension("application/pdf", "");
+  Assert.equal(extension, "pdf", "Expect pdf extension when given mime");
+  let mimeInfo = gMIMEService.getFromTypeAndExtension("", "pdf");
+  Assert.equal(
+    mimeInfo.description,
+    "Portable Document Format",
+    "PDF has generic description"
+  );
+});
