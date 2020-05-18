1c716d47e10b1ff7a0d2b18593867aa5de1e484c
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-05-18 16:38:58 +0300

    Backed out 2 changesets (bug 1613609) for causing bustages.
    CLOSED TREE
    
    Backed out changeset dd6e395dc342 (bug 1613609)
    Backed out changeset 5bcb7b13a4ad (bug 1613609)

--- toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
+++ toolkit/components/url-classifier/nsUrlClassifierStreamUpdater.cpp
@@ -148,8 +148,6 @@ nsresult nsUrlClassifierStreamUpdater::FetchUpdate(
   mozilla::OriginAttributes attrs;
   attrs.mFirstPartyDomain.AssignLiteral(NECKO_SAFEBROWSING_FIRST_PARTY_DOMAIN);
   loadInfo->SetOriginAttributes(attrs);
-  // allow deprecated HTTP request from SystemPrincipal
-  loadInfo->SetAllowDeprecatedSystemRequests(true);
 
   mBeganStream = false;
 
