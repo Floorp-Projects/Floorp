f6ee95862eb5c8e74e1370d732b02ca60dba1628
Csoregi Natalia <ncsoregi@mozilla.com>
2020-05-18 13:31:56 +0300

    Backed out changeset 62ad26fbfaf8 (bug 1636998) for reftest failures on 1174332-1.html. CLOSED TREE

--- layout/forms/nsRangeFrame.h
+++ layout/forms/nsRangeFrame.h
@@ -88,6 +88,10 @@ class nsRangeFrame final : public nsContainerFrame,
         aFlags & ~(nsIFrame::eReplaced | nsIFrame::eReplacedContainsBlock));
   }
 
+  ComputedStyle* GetAdditionalComputedStyle(int32_t aIndex) const override;
+  void SetAdditionalComputedStyle(int32_t aIndex,
+                                  ComputedStyle* aComputedStyle) override;
+
   /**
    * Returns true if the slider's thumb moves horizontally, or else false if it
    * moves vertically.
@@ -175,6 +179,11 @@ class nsRangeFrame final : public nsContainerFrame,
    * @see nsRangeFrame::CreateAnonymousContent
    */
   nsCOMPtr<Element> mThumbDiv;
+
+  /**
+   * Cached ComputedStyle for -moz-focus-outer CSS pseudo-element style.
+   */
+  RefPtr<ComputedStyle> mOuterFocusStyle;
 
   class DummyTouchListener final : public nsIDOMEventListener {
    private:
