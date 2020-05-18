dab5a9792bc255bd378a9f3c7ed64ca289a16e38
Mike Conley <mconley@mozilla.com>
2020-05-18 18:55:28 +0000

    Bug 1636207 - Fork the WebRTC global indicator for a refresh. r=florian a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D74332

--- browser/base/content/test/webrtc/head.js
+++ browser/base/content/test/webrtc/head.js
@@ -53,7 +53,7 @@ function promiseIndicatorWindow() {
         function() {
           if (
             win.location.href !==
-            "chrome://browser/content/webrtcIndicator.xhtml"
+            "chrome://browser/content/webrtcLegacyIndicator.xhtml"
           ) {
             info("ignoring a window with this url: " + win.location.href);
             return;
