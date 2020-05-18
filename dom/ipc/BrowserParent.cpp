7b91b67d72b90cbb123ec7242683b588ac14d1c5
Botond Ballo <botond@mozilla.com>
2020-05-18 03:05:32 +0000

    Bug 1638458 - Remove the ignoreRootScrollFrame parameter of FrameLoader.sendCrossProcessMouseEvent(). r=tnikkel
    No one is setting this parameter to true any more.
    
    Depends on D75734
    
    Differential Revision: https://phabricator.services.mozilla.com/D75735

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -1356,12 +1356,10 @@ IPCResult BrowserParent::RecvNewWindowGlobal(
 
 void BrowserParent::SendMouseEvent(const nsAString& aType, float aX, float aY,
                                    int32_t aButton, int32_t aClickCount,
-                                   int32_t aModifiers,
-                                   bool aIgnoreRootScrollFrame) {
+                                   int32_t aModifiers) {
   if (!mIsDestroyed) {
     Unused << PBrowserParent::SendMouseEvent(nsString(aType), aX, aY, aButton,
-                                             aClickCount, aModifiers,
-                                             aIgnoreRootScrollFrame);
+                                             aClickCount, aModifiers);
   }
 }
 
--- dom/ipc/BrowserParent.h
+++ dom/ipc/BrowserParent.h
@@ -570,8 +570,7 @@ class BrowserParent final : public PBrowserParent,
   mozilla::ipc::IPCResult RecvResetPrefersReducedMotionOverrideForTest();
 
   void SendMouseEvent(const nsAString& aType, float aX, float aY,
-                      int32_t aButton, int32_t aClickCount, int32_t aModifiers,
-                      bool aIgnoreRootScrollFrame);
+                      int32_t aButton, int32_t aClickCount, int32_t aModifiers);
 
   /**
    * The following Send*Event() marks aEvent as posted to remote process if
