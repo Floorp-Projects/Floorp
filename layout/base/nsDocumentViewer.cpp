2f95a6ac8063ba93eacef6984ddb6e6913fcd487
Matt Woodrow <mwoodrow@mozilla.com>
2020-05-18 07:30:14 +0000

    Bug 1620679 - Don't fire load event from within Stop(). r=smaug
    This matches what the spec says, and what blink does.
    
    Differential Revision: https://phabricator.services.mozilla.com/D73994

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -1161,6 +1161,13 @@ nsDocumentViewer::LoadComplete(nsresult aStatus) {
     }
   } else {
     // XXX: Should fire error event to the document...
+
+    // If our load was explicitly aborted, then we want to set our
+    // readyState to COMPLETE, and fire a readystatechange event.
+    if (aStatus == NS_BINDING_ABORTED &&
+        mDocument->GetReadyStateEnum() == Document::READYSTATE_INTERACTIVE) {
+      mDocument->SetReadyStateInternal(Document::READYSTATE_COMPLETE);
+    }
   }
 
   // Notify the document that it has been shown (regardless of whether
@@ -1172,7 +1179,10 @@ nsDocumentViewer::LoadComplete(nsresult aStatus) {
   // pageshow, and that's pretty broken... Fortunately, this should be rare.
   // (It requires us to spin the event loop in onload handler, e.g. via sync
   // XHR, in order for the navigation-away to happen before onload completes.)
-  if (mDocument && mDocument->IsCurrentActiveDocument()) {
+  // We skip firing pageshow if we're currently handling unload, or if loading
+  // was explicitly aborted.
+  if (mDocument && mDocument->IsCurrentActiveDocument() &&
+      aStatus != NS_BINDING_ABORTED) {
     // Re-get window, since it might have changed during above firing of onload
     window = mDocument->GetWindow();
     if (window) {
