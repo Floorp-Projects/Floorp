4c54634d8a7a6589a9ec12df7971ba3c19da651f
Kershaw Chang <kershaw@mozilla.com>
2020-05-18 20:18:08 +0000

    Bug 1539819 - P3: Some adjustments to make TRRServiceChannel work on socket process r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D68399

--- netwerk/protocol/http/TRRServiceChannel.h
+++ netwerk/protocol/http/TRRServiceChannel.h
@@ -133,6 +133,8 @@ class TRRServiceChannel : public HttpBaseChannel,
       nsIURI* aNewURI, nsIChannel* aNewChannel, bool aPreserveMethod,
       uint32_t aRedirectFlags) override;
 
+  virtual bool SameOriginWithOriginalUri(nsIURI* aURI) override;
+
   // True only when we have computed the value of the top window origin.
   bool mTopWindowOriginComputed;
 
