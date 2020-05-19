da0bbcb9e9a9b71cb6e4c0fd8b5480b709897daf
Steve Fink <sfink@mozilla.com>
2020-05-19 21:55:26 +0000

    Bug 1633625 - Split out host-provided functionality (currently SpiderMonkey and Firefox) r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D75010

--- js/src/devtools/gc-ubench/spidermonkey.js
+++ js/src/devtools/gc-ubench/spidermonkey.js
@@ -1,93 +1,47 @@
-var FPS = 60;
-var gNumSamples = 500;
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var waited = 0;
+// SpiderMonkey JS shell benchmark script
+//
+// Usage: run $JS spidermonkey.js --help
 
-////////////// Host functionality /////////////////
+loadRelativeToScript("shell-bench.js");
 
-function start_turn() {}
+var SpiderMonkey = class extends Host {
+  start_turn() {}
 
-function end_turn() {
-  clearKeptObjects();
-}
-
-function suspend(duration) {
-  sleep(duration);
-}
-
-///////////////// Main /////////////////
-
-loadRelativeToScript("harness.js");
-loadRelativeToScript("perf.js");
-loadRelativeToScript("test_list.js");
-
-var tests = new Map();
-foreach_test_file(f => loadRelativeToScript(f));
-for (const [name, info] of tests.entries()) {
-  if ("enabled" in info && !info.enabled) {
-    tests.delete(name);
+  end_turn() {
+    drainJobQueue();
+    clearKeptObjects();
   }
-}
 
-function tick(loadMgr, timestamp) {
-  start_turn();
-  const events = loadMgr.tick(timestamp);
-  end_turn();
-  return events;
-}
-
-function now() {
-  return performance.now();
-}
-
-function wait_for_next_frame(t0, t1) {
-  const elapsed = (t1 - t0) / 1000;
-  const period = 1 / FPS;
-  const used = elapsed % period;
-  const delay = period - used;
-  waited += delay;
-  suspend(delay);
-}
-
-function report_events(events, loadMgr) {
-  let ended = false;
-  if (events & loadMgr.LOAD_ENDED) {
-    print(`${loadMgr.lastActive.name} ended`);
-    ended = true;
+  suspend(duration) {
+    sleep(duration);
   }
-  if (events & loadMgr.LOAD_STARTED) {
-    print(`${loadMgr.activeLoad().name} starting`);
+
+  get minorGCCount() {
+    return performance.mozMemory.gc.minorGCCount;
   }
-  return ended;
-}
-
-function run(loads) {
-  const loadMgr = new AllocationLoadManager(tests);
-  const perf = new FrameHistory(gNumSamples);
-
-  loads = loads.length ? loads : tests.keys();
-  loadMgr.startCycle(loads);
-  perf.start();
-
-  const t0 = now();
-
-  let possible = 0;
-  let frames = 0;
-  while (loadMgr.load_running()) {
-    const timestamp = now();
-    perf.on_frame(timestamp);
-    const events = tick(loadMgr, timestamp);
-    frames++;
-    if (report_events(events, loadMgr)) {
-      possible += (loadMgr.testDurationMS / 1000) * FPS;
-      print(
-        `  observed ${frames} / ${possible} frames in ${(now() - t0) /
-          1000} seconds`
-      );
-    }
-    wait_for_next_frame(t0, now());
+  get majorGCCount() {
+    return performance.mozMemory.gc.majorGCCount;
   }
-}
+  get GCSliceCount() {
+    return performance.mozMemory.gc.sliceCount;
+  }
+  get gcBytes() {
+    return performance.mozMemory.gc.zone.gcBytes;
+  }
+  get gcAllocTrigger() {
+    return performance.mozMemory.gc.zone.gcAllocTrigger;
+  }
+
+  features = {
+    haveMemorySizes: true,
+    haveGCCounts: true,
+  };
+};
+var gHost = new SpiderMonkey();
 
 run(scriptArgs);
 print(`Waited total of ${waited} seconds`);
