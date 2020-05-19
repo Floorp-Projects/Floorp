fbcafaf7aec3238e609180fbe7eefc6a7bd07b1f
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2020-05-19 17:27:42 +0000

    Bug 1543813: Minor refactor of macOS color picker. r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D71043

--- widget/cocoa/nsColorPicker.h
+++ widget/cocoa/nsColorPicker.h
@@ -26,11 +26,6 @@ class nsColorPicker final : public nsIColorPicker {
 
   // For NSColorPanelWrapper.
   void Update(NSColor* aColor);
-  // Call this method if you are done with this input, but the color picker needs to
-  // stay open as it will be associated to another input
-  void DoneWithRetarget();
-  // Same as DoneWithRetarget + clean the static instance of sColorPanelWrapper,
-  // as it is not needed anymore for now
   void Done();
 
  private:
@@ -39,7 +34,7 @@ class nsColorPicker final : public nsIColorPicker {
   static NSColor* GetNSColorFromHexString(const nsAString& aColor);
   static void GetHexStringFromNSColor(NSColor* aColor, nsAString& aResult);
 
-  static NSColorPanelWrapper* sColorPanelWrapper;
+  NSColorPanelWrapper* mColorPanelWrapper;
 
   nsString mTitle;
   nsString mColor;
