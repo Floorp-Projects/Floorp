27d44a1eb3f656e71fbc42cc287742b8af16435f
Tom Prince <mozilla@hocat.ca>
2020-05-19 22:44:45 +0000

    Bug 1577110: Don't add default toolchain artifact directory if already specified; r=glandium
    If a task has explicitly specified artifact paths, don't additionally specify
    the default paths. If the task has private artifacts, having a directory
    that uploads public artifacts seems like an attractive nuissance.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74200

--- taskcluster/taskgraph/transforms/job/toolchain.py
+++ taskcluster/taskgraph/transforms/job/toolchain.py
@@ -122,14 +122,14 @@ def docker_worker_toolchain(config, job, taskdesc):
     # Allow the job to specify where artifacts come from, but add
     # public/build if it's not there already.
     artifacts = worker.setdefault('artifacts', [])
-    if not any(artifact.get('name') == 'public/build' for artifact in artifacts):
+    if not artifacts:
         docker_worker_add_artifacts(config, job, taskdesc)
 
     # Toolchain checkouts don't live under {workdir}/checkouts
     workspace = '{workdir}/workspace/build'.format(**run)
     gecko_path = '{}/src'.format(workspace)
 
-    env = worker['env']
+    env = worker.setdefault('env', {})
     env.update({
         'MOZ_BUILD_DATE': config.params['moz_build_date'],
         'MOZ_SCM_LEVEL': config.params['level'],
