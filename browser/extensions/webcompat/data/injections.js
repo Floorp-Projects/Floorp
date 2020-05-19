f7f776477888fe42df15038978192ff6142f770e
Mike Taylor <miket@mozilla.com>
2020-05-19 16:37:00 +0000

    Bug 1631858 - Import webcompat addon v11.0.0 sources r=webcompat-reviewers,twisniewski
    Differential Revision: https://phabricator.services.mozilla.com/D75996

--- browser/extensions/webcompat/data/injections.js
+++ browser/extensions/webcompat/data/injections.js
@@ -205,20 +205,6 @@ const AVAILABLE_INJECTIONS = [
     },
   },
   {
-    id: "bug1568256",
-    platform: "android",
-    domain: "zertifikate.commerzbank.de",
-    bug: "1568256",
-    contentScripts: {
-      matches: ["*://*.zertifikate.commerzbank.de/webforms/mobile/*"],
-      css: [
-        {
-          file: "injections/css/bug1568256-zertifikate.commerzbank.de-flex.css",
-        },
-      ],
-    },
-  },
-  {
     id: "bug1568908",
     platform: "desktop",
     domain: "console.cloud.google.com",
@@ -435,6 +421,20 @@ const AVAILABLE_INJECTIONS = [
       css: [
         {
           file: "injections/css/bug1632019-everyman.co-gallery-width-fix.css",
+        },
+      ],
+    },
+  },
+  {
+    id: "bug1633796",
+    platform: "android",
+    domain: "www.laposte.fr",
+    bug: "1633796",
+    contentScripts: {
+      matches: ["https://www.laposte.fr/*"],
+      css: [
+        {
+          file: "injections/css/bug1633796-www-la-poste-fr-placeholder-fix.css",
         },
       ],
     },
