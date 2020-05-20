5b6558b1303ae9b976521d47d2b2b33f6ed7909c
Dan Glastonbury <dan.glastonbury@gmail.com>
2020-05-20 00:19:59 +0000

    Bug 1633459 - Marshal customUserAgent setting via nsDocShell. r=geckoview-reviewers,esawin,nika
    Reinstate customUserAgent interface for nsIDocShell. This is so it can be used
    as a choke-point to catch setting values on docshells which are in the process
    of changing process. We don't want to send changes which will be rejected on the
    parent side.
    
    This code should be removed once callers setting customUserAgent are refactored
    to only occur from parent process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75006

--- devtools/server/actors/emulation/responsive.js
+++ devtools/server/actors/emulation/responsive.js
@@ -316,7 +316,9 @@ const ResponsiveActor = protocol.ActorClassWithSpec(responsiveSpec, {
     if (this._previousUserAgentOverride === undefined) {
       this._previousUserAgentOverride = this.getUserAgentOverride();
     }
-    this.docShell.browsingContext.customUserAgent = userAgent;
+    // Bug 1637494: TODO - customUserAgent should only be set from parent
+    // process.
+    this.docShell.customUserAgent = userAgent;
     return true;
   },
 
