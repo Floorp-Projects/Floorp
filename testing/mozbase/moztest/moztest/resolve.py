cf9f527a4f5a5fe3f52af471cc889f51f12652a7
egao <egao@mozilla.com>
2020-05-20 22:01:47 +0000

    Bug 1637463 - writeruntimes script to output runtime information into nested dictionary r=ahal
    Changes:
    
    Update `writeruntimes` script so that each suite is in its own nested dictionary and add the ability to query `web-platform-tests-reftest`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D75229

--- testing/mozbase/moztest/moztest/resolve.py
+++ testing/mozbase/moztest/moztest/resolve.py
@@ -235,7 +235,8 @@ TEST_SUITES = {
     'web-platform-tests-reftest': {
         'aliases': ('wpt',),
         'mach_command': 'web-platform-tests',
-        'kwargs': {'include': []},
+        'build_flavor': 'web-platform-tests',
+        'kwargs': {'subsuite': 'reftest'},
         'task_regex': ['web-platform-tests-reftest($|.*(-1|[^0-9])$)',
                        'test-verify-wpt'],
     },
