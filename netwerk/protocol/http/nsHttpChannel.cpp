2944b6ba170a363c9ca5471b9a2c4be06a3defc1
Andrea Marchesini <amarchesini@mozilla.com>
2020-05-20 15:25:30 +0000

    Bug 1639247 - Isolate DNS cache per first-party when privacy.partition.network_state is set to true - part 1 - check the return value of GetUsername(), r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D75984

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -6740,11 +6740,13 @@ nsresult nsHttpChannel::BeginConnect() {
   rv = mURI->GetScheme(scheme);
   if (NS_SUCCEEDED(rv)) rv = mURI->GetAsciiHost(host);
   if (NS_SUCCEEDED(rv)) rv = mURI->GetPort(&port);
-  if (NS_SUCCEEDED(rv)) mURI->GetUsername(mUsername);
   if (NS_SUCCEEDED(rv)) rv = mURI->GetAsciiSpec(mSpec);
   if (NS_FAILED(rv)) {
     return rv;
   }
+
+  // Just a warning here because some nsIURIs do not implement this method.
+  Unused << NS_WARN_IF(NS_FAILED(mURI->GetUsername(mUsername)));
 
   // Reject the URL if it doesn't specify a host
   if (host.IsEmpty()) {
