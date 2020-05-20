d0374a8611f7fc7c4b1f7aa49d81030b1ac338ca
Florian Quèze <florian@queze.net>
2020-05-20 13:12:02 +0000

    Bug 1639242 - tweak browser_preferences_usage.js to make it pass on Windows with Fission enabled, r=Gijs.
    Depends on D75553
    
    Differential Revision: https://phabricator.services.mozilla.com/D76108

--- browser/base/content/test/performance/browser_preferences_usage.js
+++ browser/base/content/test/performance/browser_preferences_usage.js
@@ -227,6 +227,27 @@ add_task(async function navigate_around() {
         min: 50,
         max: 51,
       };
+    } else if (AppConstants.platform == "win") {
+      // The following 2 graphics prefs are covered by
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1639497
+      whitelist["gfx.canvas.azure.backends"] = {
+        min: 100,
+        max: 101,
+      };
+      whitelist["gfx.content.azure.backends"] = {
+        min: 100,
+        max: 101,
+      };
+      // The following 2 sandbox prefs are covered by
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1639494
+      whitelist["security.sandbox.content.read_path_whitelist"] = {
+        min: 50,
+        max: 51,
+      };
+      whitelist["security.sandbox.logging.enabled"] = {
+        min: 50,
+        max: 51,
+      };
     }
   }
 
