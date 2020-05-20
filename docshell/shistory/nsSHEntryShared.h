1bee4cc5e9af980efc8432ff1bcc3ac04f57436d
Peter Van der Beken <peterv@propagandism.org>
2020-05-20 09:09:06 +0000

    Bug 1570255 - Remove sync session history implementation. r=smaug,nika
    Differential Revision: https://phabricator.services.mozilla.com/D65326

--- docshell/shistory/nsSHEntryShared.h
+++ docshell/shistory/nsSHEntryShared.h
@@ -54,10 +54,10 @@ class SHEntrySharedParentState {
   void NotifyListenersContentViewerEvicted();
 
  protected:
-  SHEntrySharedParentState(nsISHistory* aSHistory, uint64_t aID);
-  SHEntrySharedParentState(SHEntrySharedParentState* aDuplicate, uint64_t aID)
-      : SHEntrySharedParentState(aDuplicate->mSHistory, aID) {}
-  SHEntrySharedParentState(nsIWeakReference* aSHistory, uint64_t aID);
+  explicit SHEntrySharedParentState(nsISHistory* aSHistory);
+  explicit SHEntrySharedParentState(SHEntrySharedParentState* aDuplicate)
+      : SHEntrySharedParentState(aDuplicate->mSHistory) {}
+  explicit SHEntrySharedParentState(nsIWeakReference* aSHistory);
   virtual ~SHEntrySharedParentState();
   NS_INLINE_DECL_VIRTUAL_REFCOUNTING_WITH_DESTROY(SHEntrySharedParentState,
                                                   Destroy())
@@ -104,8 +104,6 @@ class SHEntrySharedParentState {
  */
 class SHEntrySharedChildState {
  protected:
-  SHEntrySharedChildState();
-
   void CopyFrom(SHEntrySharedChildState* aSource);
 
  public:
@@ -117,16 +115,11 @@ class SHEntrySharedChildState {
   // they're specific to a particular content viewer.
   nsCOMPtr<nsIContentViewer> mContentViewer;
   RefPtr<mozilla::dom::Document> mDocument;
-  // FIXME Move to parent?
-  nsCOMPtr<nsILayoutHistoryState> mLayoutHistoryState;
   nsCOMPtr<nsISupports> mWindowState;
   // FIXME Move to parent?
   nsCOMPtr<nsIMutableArray> mRefreshURIList;
   nsExpirationState mExpirationState;
   UniquePtr<nsDocShellEditorData> mEditorData;
-
-  // FIXME Move to parent?
-  bool mSaveLayoutState;
 };
 
 }  // namespace dom
@@ -149,7 +142,7 @@ class nsSHEntryShared final : public nsIBFCacheEntry,
 
   using SHEntrySharedParentState::SHEntrySharedParentState;
 
-  already_AddRefed<nsSHEntryShared> Duplicate(uint64_t aNewSharedID);
+  already_AddRefed<nsSHEntryShared> Duplicate();
 
   NS_DECL_ISUPPORTS_INHERITED
   NS_DECL_NSIBFCACHEENTRY
@@ -166,7 +159,7 @@ class nsSHEntryShared final : public nsIBFCacheEntry,
  private:
   ~nsSHEntryShared();
 
-  friend class nsLegacySHEntry;
+  friend class nsSHEntry;
 
   void RemoveFromExpirationTracker();
   void SyncPresentationState();
