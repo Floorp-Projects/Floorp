3d9a7b03b99f09fb605e336604e7178c20273006
Tim Huang <tihuang@mozilla.com>
2020-05-20 13:50:03 +0000

    Bug 1639149 - Fix and make the test 'browser_permissionInNormalWindows.js' Fission-compatible. r=baku
    In the patch, we first fix the test for using the correct principal to
    do the test. And we make this test Fission-Compatible by checking the
    storage permission in the parent process for Fission.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76128

--- toolkit/components/antitracking/test/browser/browser_permissionInNormalWindows.js
+++ toolkit/components/antitracking/test/browser/browser_permissionInNormalWindows.js
@@ -10,23 +10,81 @@ AntiTracking.runTest(
   // Non blocking callback
   async _ => {
     try {
-      let Services = SpecialPowers.Services;
-      // We would use TEST_3RD_PARTY_DOMAIN here, except that the variable isn't
-      // accessible in the context of the web page...
-      let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
-        "https://tracking.example.org/"
-      );
-      for (let perm of Services.perms.getAllForPrincipal(principal)) {
-        // Ignore permissions other than storage access
-        if (!perm.type.startsWith("3rdPartyStorage^")) {
-          continue;
-        }
-        is(
-          perm.expireType,
-          Services.perms.EXPIRE_TIME,
-          "Permission must expire at a specific time"
+      // We load the test script in the parent process to check permissions.
+      let chromeScript = SpecialPowers.loadChromeScript(_ => {
+        // eslint-disable-next-line no-undef
+        addMessageListener("go", _ => {
+          const { Services } = ChromeUtils.import(
+            "resource://gre/modules/Services.jsm"
+          );
+
+          function ok(what, msg) {
+            // eslint-disable-next-line no-undef
+            sendAsyncMessage("ok", { what: !!what, msg });
+          }
+
+          function is(a, b, msg) {
+            ok(a === b, msg);
+          }
+
+          // We should use the principal of the TEST_DOMAIN since the storage
+          // permission is saved under it.
+          let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
+            "http://example.net/"
+          );
+
+          for (let perm of Services.perms.getAllForPrincipal(principal)) {
+            // Ignore permissions other than storage access
+            if (!perm.type.startsWith("3rdPartyStorage^")) {
+              continue;
+            }
+            is(
+              perm.expireType,
+              Services.perms.EXPIRE_TIME,
+              "Permission must expire at a specific time"
+            );
+            ok(perm.expireTime > 0, "Permission must have a expiry time");
+          }
+
+          // eslint-disable-next-line no-undef
+          sendAsyncMessage("done");
+        });
+      });
+
+      chromeScript.addMessageListener("ok", obj => {
+        ok(obj.what, obj.msg);
+      });
+
+      await new Promise(resolve => {
+        chromeScript.addMessageListener("done", _ => {
+          chromeScript.destroy();
+          resolve();
+        });
+
+        chromeScript.sendAsyncMessage("go");
+      });
+
+      // We check the permission in tracking processes for non-Fission mode. In
+      // Fission mode, the permission won't be synced to the tracking process,
+      // so we don't check it.
+      if (!SpecialPowers.useRemoteSubframes) {
+        let Services = SpecialPowers.Services;
+        let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
+          "http://example.net/"
         );
-        ok(perm.expireTime > 0, "Permission must have a expiry time");
+
+        for (let perm of Services.perms.getAllForPrincipal(principal)) {
+          // Ignore permissions other than storage access
+          if (!perm.type.startsWith("3rdPartyStorage^")) {
+            continue;
+          }
+          is(
+            perm.expireType,
+            Services.perms.EXPIRE_TIME,
+            "Permission must expire at a specific time"
+          );
+          ok(perm.expireTime > 0, "Permission must have a expiry time");
+        }
       }
     } catch (e) {
       alert(e);
