23e905269986a71f247040228bc1b7961dce6abe
Tiger Oakes <toakes@mozilla.com>
2020-05-20 13:37:32 -0700

    [components] For https://github.com/mozilla-mobile/android-components/issues/7046 - Rewrite room queries

--- mobile/android/android-components/components/feature/share/src/main/java/mozilla/components/feature/share/db/RecentAppsDao.kt
+++ mobile/android/android-components/components/feature/share/src/main/java/mozilla/components/feature/share/db/RecentAppsDao.kt
@@ -6,38 +6,60 @@ package mozilla.components.feature.share.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Update
+import androidx.room.Transaction
+
+private const val DECAY_MULTIPLIER = 0.95
 
 @Dao
-internal interface RecentAppsDao {
+internal abstract class RecentAppsDao {
 
-    @Insert
-    fun insertRecentApp(recentApp: RecentAppEntity)
-
-    @Query("""
-        SELECT * FROM recent_apps_table 
-        WHERE activityName = :activityName""")
-    fun getRecentApp(activityName: String): RecentAppEntity?
-
-    @Update
-    fun updateRecentApp(recentApp: RecentAppEntity)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract fun insertRecentApps(recentApps: List<RecentAppEntity>)
 
     @Query("""
         DELETE FROM recent_apps_table
-        WHERE activityName = :activityName""")
-    fun deleteRecentApp(activityName: String)
-
-    @Update
-    fun updateAllRecentApp(recentApps: List<RecentAppEntity>)
+        WHERE activityName = :activityName
+    """)
+    abstract fun deleteRecentApp(activityName: String)
 
     @Query("""
         SELECT * FROM recent_apps_table
-        WHERE score > 0 
         ORDER BY score DESC
-        LIMIT :limit""")
-    fun getRecentAppsUpTo(limit: Int): List<RecentAppEntity>
+        LIMIT :limit
+    """)
+    abstract fun getRecentAppsUpTo(limit: Int): List<RecentAppEntity>
 
-    @Query("""SELECT * FROM recent_apps_table""")
-    fun getAllRecentApps(): List<RecentAppEntity>
+    /**
+     * Increments the score of a recent app.
+     * @param activityName - Name of the recent app to update.
+     */
+    @Query("""
+        UPDATE recent_apps_table
+        SET score = score + 1
+        WHERE activityName = :activityName
+    """)
+    abstract fun updateRecentAppScore(activityName: String)
+
+    /**
+     * Decreases the score of all but one app (exponential decay).
+     * @param exceptActivity - ID of recent app to leave alone
+     * @param decay - Amount to decay by. Should be between 0 and 1.
+     */
+    @Query("""
+        UPDATE recent_apps_table
+        SET score = score * :decay
+        WHERE activityName != :exceptActivity
+    """)
+    abstract fun decayAllRecentApps(
+        exceptActivity: String,
+        decay: Double = DECAY_MULTIPLIER
+    )
+
+    @Transaction
+    open fun updateRecentAppAndDecayRest(activityName: String) {
+        updateRecentAppScore(activityName)
+        decayAllRecentApps(exceptActivity = activityName)
+    }
 }
--- mobile/android/android-components/components/feature/share/src/test/java/mozilla/components/feature/share/RecentAppStorageTest.kt
+++ mobile/android/android-components/components/feature/share/src/test/java/mozilla/components/feature/share/RecentAppStorageTest.kt
@@ -7,7 +7,6 @@ package mozilla.components.feature.share
 import android.content.Context
 import mozilla.components.feature.share.db.RecentAppEntity
 import mozilla.components.feature.share.db.RecentAppsDao
-import mozilla.components.feature.share.db.RecentAppsDatabase
 import mozilla.components.support.test.mock
 import mozilla.components.support.test.whenever
 import org.junit.Assert.assertEquals
@@ -17,90 +16,52 @@ import org.mockito.Mockito.verify
 
 class RecentAppStorageTest {
 
-    private lateinit var recentAppsDatabase: RecentAppsDatabase
     private lateinit var context: Context
     private lateinit var recentAppsDao: RecentAppsDao
-
     private lateinit var recentAppsStorage: RecentAppsStorage
 
     @Before
     fun setup() {
-        recentAppsDatabase = mock()
         context = mock()
         recentAppsDao = mock()
-        whenever(recentAppsDatabase.recentAppsDao()).thenReturn(recentAppsDao)
 
         recentAppsStorage = RecentAppsStorage(context)
-        recentAppsStorage.database = lazyOf(recentAppsDatabase)
+        recentAppsStorage.recentAppsDao = lazyOf(recentAppsDao)
     }
 
     @Test
     fun `get the two most recent apps`() {
-        val result = ArrayList<RecentAppEntity>()
-        whenever(recentAppsDao.getRecentAppsUpTo(2)).thenReturn(result)
+        whenever(recentAppsDao.getRecentAppsUpTo(2)).thenReturn(emptyList())
 
-        assertEquals(result, recentAppsStorage.getRecentAppsUpTo(2))
+        assertEquals(emptyList<RecentApp>(), recentAppsStorage.getRecentAppsUpTo(2))
     }
 
     @Test
     fun `increment selected app count and decay all others`() {
         val activityName = "activityName"
-        val selectedAppEntity =
-                RecentAppEntity(score = 1.0, activityName = activityName)
-        val otherAppEntity =
-                RecentAppEntity(score = 100.0, activityName = "other")
-        val allRecentApps = ArrayList<RecentAppEntity>()
-        allRecentApps.add(selectedAppEntity)
-        allRecentApps.add(otherAppEntity)
-
-        whenever(recentAppsDao.getRecentApp(activityName)).thenReturn(selectedAppEntity)
-        whenever(recentAppsDao.getAllRecentApps()).thenReturn(allRecentApps)
 
         recentAppsStorage.updateRecentApp(activityName)
 
-        verify(recentAppsDao).updateRecentApp(selectedAppEntity)
-        assertEquals(95.0, otherAppEntity.score, 0.0001)
-        assertEquals(2.0, selectedAppEntity.score, 0.0001)
-        verify(recentAppsDao).updateAllRecentApp(allRecentApps)
+        verify(recentAppsDao).updateRecentAppAndDecayRest(activityName)
     }
 
     @Test
     fun `add newly installed apps to our database`() {
-        val appsInDatabase = ArrayList<RecentAppEntity>()
         val firstActivityName = "first"
         val secondActivityName = "second"
         val thirdActivityName = "third"
         val fourthActivityName = "fourth"
-        appsInDatabase.add(
-                RecentAppEntity(
-                        score = 100.0,
-                        activityName = firstActivityName
-                )
-        )
-        appsInDatabase.add(
-                RecentAppEntity(
-                        score = 20.0,
-                        activityName = secondActivityName
-                )
-        )
-        appsInDatabase.add(
-                RecentAppEntity(
-                        score = 50.0,
-                        activityName = thirdActivityName
-                )
-        )
-        val result = RecentAppEntity(
-                activityName = fourthActivityName,
-                score = RecentAppsStorage.DEFAULT_COUNT
-        )
-
         val currentApps = listOf(firstActivityName, secondActivityName, thirdActivityName, fourthActivityName)
-
-        whenever(recentAppsDao.getRecentApp(fourthActivityName)).thenReturn(null)
+        val appsInDatabase = listOf(
+            RecentAppEntity(firstActivityName),
+            RecentAppEntity(secondActivityName),
+            RecentAppEntity(thirdActivityName),
+            RecentAppEntity(fourthActivityName)
+        )
 
         recentAppsStorage.updateDatabaseWithNewApps(currentApps)
 
-        verify(recentAppsDao).insertRecentApp(result)
+        verify(recentAppsDao).insertRecentApps(appsInDatabase)
     }
 
     @Test
