4d876c380d9d9e5a485f19e906530bd6cbeb2c03
Sid Stamm <mozilla@sidstamm.com>
2020-05-21 17:43:15 +0000

    Bug 1002724 - use resolvable URL in extension html_detail_view test. r=mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D75088

--- toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
+++ toolkit/mozapps/extensions/test/browser/browser_html_detail_view.js
@@ -144,7 +144,7 @@ add_task(async function enableHtmlViews() {
       description: "Short description",
       fullDescription: "Longer description\nWith brs!",
       type: "extension",
-      contributionURL: "http://localhost/contribute",
+      contributionURL: "http://example.com/contribute",
       averageRating: 4.279,
       userPermissions: {
         origins: ["<all_urls>", "file://*/*"],
@@ -481,7 +481,7 @@ add_task(async function testFullDetails() {
 
   let waitForTab = BrowserTestUtils.waitForNewTab(
     gBrowser,
-    "http://localhost/contribute"
+    "http://example.com/contribute"
   );
   contrib.querySelector("button").click();
   BrowserTestUtils.removeTab(await waitForTab);
