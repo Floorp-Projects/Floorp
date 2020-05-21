a39a3a38078b45e813f1a12ef7c10f57e2bcf652
Emilio Cobos Álvarez <emilio@crisal.io>
2020-05-21 12:27:54 +0000

    Bug 1635675 - Implement the ::file-chooser-button pseudo-element. r=jwatt
    As per https://github.com/w3c/csswg-drafts/issues/5049.
    
    Don't enable it unconditionally just yet, as the name may change.
    
    I had to move some rules in forms.css because otherwise you get
    specificity conflicts.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76214

--- layout/style/nsCSSPseudoElementList.h
+++ layout/style/nsCSSPseudoElementList.h
@@ -97,3 +97,7 @@ CSS_PSEUDO_ELEMENT(mozTextControlEditingRoot, ":-moz-text-control-editing-root",
 // The element that shows the autofill value.
 CSS_PSEUDO_ELEMENT(mozTextControlPreview, ":-moz-text-control-preview",
                    CSS_PSEUDO_ELEMENT_ENABLED_IN_UA_SHEETS)
+
+CSS_PSEUDO_ELEMENT(fileChooserButton, ":file-chooser-button",
+                   CSS_PSEUDO_ELEMENT_ENABLED_IN_UA_SHEETS |
+                   CSS_PSEUDO_ELEMENT_SUPPORTS_USER_ACTION_STATE)
