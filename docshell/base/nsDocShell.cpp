5e3d4df098126e660e884a1e866a4473faf90da9
Noemi Erli <nerli@mozilla.com>
2020-05-21 04:16:21 +0300

    Backed out 3 changesets (bug 1627533) for causing assertions in RequestContextService.cpp CLOSED TREE
    Backed out changeset 3b35a1852a60 (bug 1627533)
    Backed out changeset 70fa5e075269 (bug 1627533)
    Backed out changeset 0d1046670534 (bug 1627533)

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -464,7 +464,7 @@ already_AddRefed<nsDocShell> nsDocShell::Create(
   RefPtr<nsDocShell> ds = new nsDocShell(aBrowsingContext, aContentWindowID);
 
   // Initialize the underlying nsDocLoader.
-  rv = ds->nsDocLoader::InitWithBrowsingContext(aBrowsingContext);
+  rv = ds->nsDocLoader::Init();
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return nullptr;
   }
