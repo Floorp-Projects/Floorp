593ab0604fa5ded2437c3e34b74006696ab8e7df
Greg Tatum <gtatum@mozilla.com>
2020-05-22 13:18:44 +0000

    Bug 1628073 - Move gecko profiler testing files to mozgeckoprofiler; r=sparky,perftest-reviewers,gbrown
    I need to add symbolication support for the mochitest Gecko Profiler command line
    option. These profiles also need to be symbolicated. Unfortunately, there is not
    a common place where I could use these files. Talos and Raptor each had their
    own copy of the snappy symbolication server.
    
    This commit consolidates these packages into a re-usable mozbase package that can
    be used in mochitests, and eventually in other places like xpcshell tests.
    
    I stubbed out a test file, but it doesn't do anything quite yet. This commit makes
    it so that the tests still work in Raptor and Talos, but doesn't add any features.
    It also doesn't try too hard to make the files look like a mozbase package.
    
    Differential Revision: https://phabricator.services.mozilla.com/D74289

---
+++ testing/mozbase/mozgeckoprofiler/mozgeckoprofiler/__init__.py
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+mozgeckoprofiler has utilities to symbolicate and load gecko profiles.
+"""
+from __future__ import absolute_import
+
+from .profiling import save_gecko_profile
+from .symbolication import ProfileSymbolicator
+
+__all__ = [
+  'save_gecko_profile',
+  'ProfileSymbolicator',
+]
