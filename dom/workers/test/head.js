af815b333b64aa8c97ef77e74c86e08988f996c5
Neil Deakin <neil@mozilla.com>
2020-05-23 03:20:07 +0000

    Bug 1640245, remove frame script and message manager from workers tests, r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D76518

--- dom/workers/test/head.js
+++ dom/workers/test/head.js
@@ -4,18 +4,13 @@
 "use strict";
 
 const EXAMPLE_URL = "http://example.com/browser/dom/workers/test/";
-const FRAME_SCRIPT_URL = getRootDirectory(gTestPath) + "frame_script.js";
 
 /**
- * Add a tab with given `url`, and load a frame script in it. Returns a promise
+ * Add a tab with given `url`. Returns a promise
  * that will be resolved when the tab finished loading.
  */
 function addTab(url) {
-  let tab = BrowserTestUtils.addTab(gBrowser, TAB_URL);
-  gBrowser.selectedTab = tab;
-  let linkedBrowser = tab.linkedBrowser;
-  linkedBrowser.messageManager.loadFrameScript(FRAME_SCRIPT_URL, false);
-  return BrowserTestUtils.browserLoaded(linkedBrowser).then(() => tab);
+  return BrowserTestUtils.openNewForegroundTab(gBrowser, TAB_URL);
 }
 
 /**
@@ -25,49 +20,28 @@ function removeTab(tab) {
   gBrowser.removeTab(tab);
 }
 
-let nextId = 0;
-
-/**
- * Send a JSON RPC request to the frame script in the given `tab`, invoking the
- * given `method` with the given `params`. Returns a promise that will be
- * resolved with the result of the invocation.
- */
-function jsonrpc(tab, method, params) {
-  let currentId = nextId++;
-  let messageManager = tab.linkedBrowser.messageManager;
-  messageManager.sendAsyncMessage("jsonrpc", {
-    id: currentId,
-    method,
-    params,
-  });
-  return new Promise(function(resolve, reject) {
-    messageManager.addMessageListener("jsonrpc", function listener(event) {
-      let { id, result, error } = event.data;
-      if (id !== currentId) {
-        return;
-      }
-      messageManager.removeMessageListener("jsonrpc", listener);
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve(result);
-    });
-  });
-}
-
 /**
  * Create a worker with the given `url` in the given `tab`.
  */
 function createWorkerInTab(tab, url) {
-  return jsonrpc(tab, "createWorker", [url]);
+  info("Creating worker with url '" + url + "'\n");
+  return SpecialPowers.spawn(tab.linkedBrowser, [url], urlChild => {
+    if (!content._workers) {
+      content._workers = {};
+    }
+    content._workers[urlChild] = new content.Worker(urlChild);
+  });
 }
 
 /**
  * Terminate the worker with the given `url` in the given `tab`.
  */
 function terminateWorkerInTab(tab, url) {
-  return jsonrpc(tab, "terminateWorker", [url]);
+  info("Terminating worker with url '" + url + "'\n");
+  return SpecialPowers.spawn(tab.linkedBrowser, [url], urlChild => {
+    content._workers[urlChild].terminate();
+    delete content._workers[urlChild];
+  });
 }
 
 /**
@@ -75,12 +49,29 @@ function terminateWorkerInTab(tab, url) {
  * `tab`.
  */
 function postMessageToWorkerInTab(tab, url, message) {
-  return jsonrpc(tab, "postMessageToWorker", [url, message]);
+  info("Posting message to worker with url '" + url + "'\n");
+  return SpecialPowers.spawn(
+    tab.linkedBrowser,
+    [url, message],
+    (urlChild, messageChild) => {
+      let worker = content._workers[urlChild];
+      worker.postMessage(messageChild);
+      return new Promise(function(resolve) {
+        worker.onmessage = function(event) {
+          worker.onmessage = null;
+          resolve(event.data);
+        };
+      });
+    }
+  );
 }
 
 /**
  * Disable the cache in the given `tab`.
  */
 function disableCacheInTab(tab) {
-  return jsonrpc(tab, "disableCache", []);
+  return SpecialPowers.spawn(tab.linkedBrowser, [], () => {
+    content.docShell.defaultLoadFlags =
+      Ci.nsIRequest.LOAD_BYPASS_CACHE | Ci.nsIRequest.INHIBIT_CACHING;
+  });
 }
