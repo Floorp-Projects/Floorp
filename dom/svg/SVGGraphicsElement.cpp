a94d31585cadf3f4fe0b4fa2f35d760f0b84e203
Dorel Luca <dluca@mozilla.com>
2020-05-25 01:31:54 +0300

    Backed out changeset 1334652d0ec4 (bug 1640280) for build bustage in workspace/obj-build/dist/include/mozilla/dom/Document.h. CLOSED TREE

--- dom/svg/SVGGraphicsElement.cpp
+++ dom/svg/SVGGraphicsElement.cpp
@@ -6,8 +6,6 @@
 
 #include "mozilla/dom/SVGGraphicsElement.h"
 
-#include "mozilla/dom/BindContext.h"
-
 namespace mozilla {
 namespace dom {
 
@@ -62,19 +60,6 @@ bool SVGGraphicsElement::IsFocusableInternal(int32_t* aTabIndex,
   bool isFocusable = false;
   IsSVGFocusable(&isFocusable, aTabIndex);
   return isFocusable;
-}
-
-nsresult SVGGraphicsElement::BindToTree(BindContext& aContext,
-                                        nsINode& aParent) {
-  nsresult rv = SVGGraphicsElementBase::BindToTree(aContext, aParent);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  if (IsFocusable() && HasAttr(nsGkAtoms::autofocus) &&
-      aContext.AllowsAutoFocus()) {
-    aContext.OwnerDoc().SetAutoFocusElement(this);
-  }
-
-  return NS_OK;
 }
 
 }  // namespace dom
