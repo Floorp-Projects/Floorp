d734939d6b33fa68a7784f24f4911b06452da859
Lee Salzman <lsalzman@mozilla.com>
2020-05-24 17:10:14 +0000

    Bug 1640401 - only enable WR font sanitization in nightly. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D76628

--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -449,6 +449,7 @@ struct WROTSAlloc {
 
 static bool ReadRawFont(const OpAddRawFont& aOp, wr::ShmSegmentsReader& aReader,
                         wr::TransactionBuilder& aUpdates) {
+#ifdef NIGHTLY_BUILD
   wr::Vec<uint8_t> sourceBytes;
   Maybe<Range<uint8_t>> ptr =
       aReader.GetReadPointerOrCopy(aOp.bytes(), sourceBytes);
@@ -473,6 +474,13 @@ static bool ReadRawFont(const OpAddRawFont& aOp, wr::ShmSegmentsReader& aReader,
     return false;
   }
   wr::Vec<uint8_t> bytes = output.forget();
+#else
+  wr::Vec<uint8_t> bytes;
+  if (!aReader.Read(aOp.bytes(), bytes)) {
+    return false;
+  }
+#endif
+
   aUpdates.AddRawFont(aOp.key(), bytes, aOp.fontIndex());
   return true;
 }
