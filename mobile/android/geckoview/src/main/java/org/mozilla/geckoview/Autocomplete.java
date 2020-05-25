f77d0c6b2c4dbe4b22db4817dbada10961d9341a
Eugen Sawin <esawin@mozilla.com>
2020-05-25 18:43:15 +0000

    Bug 1618058 - [1.4] Add Autocomplete API for login autocomplete. r=geckoview-reviewers,snorp,agi
    Differential Revision: https://phabricator.services.mozilla.com/D73055

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/Autocomplete.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/Autocomplete.java
@@ -22,13 +22,33 @@ import org.mozilla.gecko.util.EventCallback;
 import org.mozilla.gecko.util.GeckoBundle;
 
 /**
- * The Login Storage API provides a storage-level delegate to leverage Gecko's
- * complete range of heuristics for login forms, autofill and autocomplete
- * scenarios.
+ * The Autocomplete API provides a way to leverage Gecko's input form handling
+ * for autocompletion.
  *
+ * <p>
+ * The API is split into two parts:
+ * 1. Storage-level delegates.
+ * 2. User-prompt delegates.
+ * </p>
+ * <p>
+ * The storage-level delegates connect Gecko mechanics to the app's storage,
+ * e.g., retrieving and storing of login entries.
+ * </p>
+ * <p>
+ * The user-prompt delegates propagate decisions to the app that could require
+ * user choice, e.g., saving or updating of login entries or the selection of
+ * a login entry out of multiple options.
+ * </p>
+ * <p>
+ * Throughout the documentation, we will refer to the filling out of input forms
+ * using two terms:
+ * 1. Autofill: automatic filling without user interaction.
+ * 2. Autocomplete: semi-automatic filling that requires user prompting for the
+ *    selection.
+ * </p>
  * <h2>Examples</h2>
  *
- * <h3>Autofill/Fetch API</h3>
+ * <h3>Autocomplete/Fetch API</h3>
  * <p>
  * GeckoView loads <code>https://example.com</code> which contains (for the
  * purpose of this example) elements resembling a login form, e.g.,
@@ -42,26 +62,34 @@ import org.mozilla.gecko.util.GeckoBundle;
  * <p>
  * With the document parsed and the login input fields identified, GeckoView
  * dispatches a
- * <code>LoginStorage.Delegate.onLoginFetch(&quot;example.com&quot;)</code>
+ * <code>LoginStorageDelegate.onLoginFetch(&quot;example.com&quot;)</code>
  * request to fetch logins for the given domain.
  * </p>
- *
  * <p>
  * Based on the provided login entries, GeckoView will attempt to autofill the
- * login input fields.
+ * login input fields, if there is only one suitable login entry option.
+ * </p>
+ * <p>
+ * In the case of multiple valid login entry options, GeckoView dispatches a
+ * <code>GeckoSession.PromptDelegate.onLoginSelect</code> request, which allows
+ * for user-choice delegation.
+ * </p>
+ * <p>
+ * Based on the returned login entries, GeckoView will attempt to
+ * autofill/autocomplete the login input fields.
  * </p>
  *
  * <h3>Update API</h3>
  * <p>
  * When the user submits some login input fields, GeckoView dispatches another
- * <code>LoginStorage.Delegate.onLoginFetch(&quot;example.com&quot;)</code>
+ * <code>LoginStorageDelegate.onLoginFetch(&quot;example.com&quot;)</code>
  * request to check whether the submitted login exists or whether it's a new or
  * updated login entry.
  * </p>
  * <p>
  * If the submitted login is already contained as-is in the collection returned
  * by <code>onLoginFetch</code>, then GeckoView dispatches
- * <code>LoginStorage.Delegate.onLoginUsed</code> with the submitted login
+ * <code>LoginStorageDelegate.onLoginUsed</code> with the submitted login
  * entry.
  * </p>
  * <p>
@@ -76,10 +104,8 @@ import org.mozilla.gecko.util.GeckoBundle;
  * The user enters new or updated (password) login credentials in some login
  * input fields and submits explicitely (submit action) or by navigation.
  * GeckoView identifies the entered credentials and dispatches a
- * <code>GeckoSession.PromptDelegate.onLoginStoragePrompt(session, prompt)</code>
- * request with the <code>prompt</code> being of type
- * <code>LoginStoragePrompt.Type.SAVE</code> and containing the entered
- * credentials.
+ * <code>GeckoSession.PromptDelegate.onLoginSave(session, request)</code>
+ * with the provided credentials.
  * </p>
  *
  * <p>
@@ -91,22 +117,24 @@ import org.mozilla.gecko.util.GeckoBundle;
  * the prompt request (<code>prompt.logins[0]</code>) or a new or modified login
  * entry.
  * </p>
- *
  * <p>
  * The login entry returned in a confirmed save prompt is used to request for
  * saving in the runtime delegate via
- * <code>LoginStorage.Delegate.onLoginSave(login)</code>.
+ * <code>LoginStorageDelegate.onLoginSave(login)</code>.
  * If the app has already stored the entry during the prompt request handling,
  * it may ignore this storage saving request.
  * </p>
  *
  * <br>@see GeckoRuntime#setLoginStorageDelegate
  * <br>@see GeckoSession#setPromptDelegate
- * <br>@see GeckoSession.PromptDelegate#onLoginStoragePrompt
+ * <br>@see GeckoSession.PromptDelegate#onLoginSave
+ * <br>@see GeckoSession.PromptDelegate#onLoginSelect
  */
-public class LoginStorage {
-    private static final String LOGTAG = "LoginStorage";
+public class Autocomplete {
+    private static final String LOGTAG = "Autocomplete";
     private static final boolean DEBUG = false;
+
+    protected Autocomplete() {}
 
     /**
      * Holds login information for a specific entry.
@@ -171,8 +199,8 @@ public class LoginStorage {
             origin = bundle.getString(ORIGIN_KEY);
             formActionOrigin = bundle.getString(FORM_ACTION_ORIGIN_KEY);
             httpRealm = bundle.getString(HTTP_REALM_KEY);
-            username = bundle.getString(USERNAME_KEY);
-            password = bundle.getString(PASSWORD_KEY);
+            username = bundle.getString(USERNAME_KEY, "");
+            password = bundle.getString(PASSWORD_KEY, "");
         }
 
         @Override
@@ -306,15 +334,17 @@ public class LoginStorage {
             value = { UsedField.PASSWORD })
     /* package */ @interface LSUsedField {}
 
-    // Sync with UsedField in GeckoViewLoginStorage.jsm.
+    // Sync with UsedField in GeckoViewAutocomplete.jsm.
     /**
-     * Possible login entry field types for {@link Delegate#onLoginUsed}.
+     * Possible login entry field types for {@link LoginStorageDelegate#onLoginUsed}.
      */
     public static class UsedField {
         /**
          * The password field of a login entry.
          */
         public static final int PASSWORD = 1;
+
+        protected UsedField() {}
     }
 
     /**
@@ -324,7 +354,7 @@ public class LoginStorage {
      * This delegate is attached to the runtime via
      * {@link GeckoRuntime#setLoginStorageDelegate}.
      */
-    public interface Delegate {
+    public interface LoginStorageDelegate {
         /**
          * Request login entries for a given domain.
          * While processing the web document, we have identified elements
@@ -346,9 +376,7 @@ public class LoginStorage {
         /**
          * Request saving or updating of the given login entry.
          * This is triggered by confirming a
-         * {@link GeckoSession.PromptDelegate#onLoginStoragePrompt onLoginStoragePrompt}
-         * request of type
-         * {@link GeckoSession.PromptDelegate.LoginStoragePrompt.Type#SAVE Type.SAVE}.
+         * {@link GeckoSession.PromptDelegate#onLoginSave onLoginSave} request.
          *
          * @param login The {@link LoginEntry} as confirmed by the prompt
          *              request.
@@ -372,34 +400,240 @@ public class LoginStorage {
                 @LSUsedField int usedFields) {}
     }
 
-    /* package */ final static class Proxy implements BundleEventListener {
+    /**
+     * Abstract base class for Autocomplete options.
+     * Extended by {@link Autocomplete.SaveOption} and
+     * {@link Autocomplete.SelectOption}.
+     */
+    public abstract static class Option<T> {
+        /* package */ static final String VALUE_KEY = "value";
+        /* package */ static final String HINT_KEY = "hint";
+
+        public final @NonNull T value;
+        public final int hint;
+
+        public Option(final @NonNull T value, final int hint) {
+            this.value = value;
+            this.hint = hint;
+        }
+
+        @AnyThread
+        /* package */ abstract @NonNull GeckoBundle toBundle();
+    }
+
+    /**
+     * Abstract base class for saving options.
+     * Extended by {@link Autocomplete.LoginSaveOption}.
+     */
+    public abstract static class SaveOption<T> extends Option<T> {
+
+        public SaveOption(final @NonNull T value, final int hint) {
+            super(value, hint);
+        }
+    }
+
+    /**
+     * Abstract base class for saving options.
+     * Extended by {@link Autocomplete.LoginSelectOption}.
+     */
+    public abstract static class SelectOption<T> extends Option<T> {
+        public SelectOption(
+                final @NonNull T value,
+                final int hint) {
+            super(value, hint);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder builder = new StringBuilder("SelectOption {");
+            builder
+                .append("value=").append(value).append(", ")
+                .append("hint=").append(hint)
+                .append("}");
+            return builder.toString();
+        }
+    }
+
+    /**
+     * Holds information required to process login saving requests.
+     */
+    public static class LoginSaveOption extends SaveOption<LoginEntry> {
+        @Retention(RetentionPolicy.SOURCE)
+        @IntDef(flag = true,
+                value = { Hint.NONE, Hint.GENERATED, Hint.LOW_CONFIDENCE })
+        /* package */ @interface LoginSaveHint {}
+
+        /**
+         * Hint types for login saving requests.
+         */
+        public static class Hint {
+            public static final int NONE = 0;
+
+            /**
+             * Auto-generated password.
+             * Notify but do not prompt the user for saving.
+             */
+            public static final int GENERATED = 1 << 0;
+
+            /**
+             * Potentially non-login data.
+             * The form data entered may be not login credentials but other
+             * forms of input like credit card numbers.
+             * Note that this could be valid login data in same cases, e.g.,
+             * some banks may expect credit card numbers in the username field.
+             */
+            public static final int LOW_CONFIDENCE = 1 << 1;
+
+            protected Hint() {}
+        }
+
+        /**
+         * Construct a login save option.
+         *
+         * @param value The {@link LoginEntry} login entry to be saved.
+         * @param hint The {@link Hint} detailing the type of the option.
+         */
+        /* package */ LoginSaveOption(
+                final @NonNull LoginEntry value,
+                final @LoginSaveHint int hint) {
+            super(value, hint);
+        }
+
+        /**
+         * Construct a login save option.
+         *
+         * @param value The {@link LoginEntry} login entry to be saved.
+         */
+        public LoginSaveOption(final @NonNull LoginEntry value) {
+            this(value, Hint.NONE);
+        }
+
+        @Override
+        /* package */ @NonNull GeckoBundle toBundle() {
+            final GeckoBundle bundle = new GeckoBundle(2);
+            bundle.putBundle(VALUE_KEY, value.toBundle());
+            bundle.putInt(HINT_KEY, hint);
+            return bundle;
+        }
+    }
+
+    /**
+     * Holds information required to process login selection requests.
+     */
+    public static class LoginSelectOption extends SelectOption<LoginEntry> {
+        @Retention(RetentionPolicy.SOURCE)
+        @IntDef(flag = true,
+                value = { Hint.NONE, Hint.GENERATED, Hint.INSECURE_FORM,
+                          Hint.DUPLICATE_USERNAME, Hint.MATCHING_ORIGIN })
+        /* package */ @interface LoginSelectHint {}
+
+        /**
+         * Hint types for login selection requests.
+         */
+        public static class Hint {
+            public static final int NONE = 0;
+
+            /**
+             * Auto-generated password.
+             * A new password-only login entry containing a secure generated
+             * password.
+             */
+            public static final int GENERATED = 1 << 0;
+
+            /**
+             * Insecure login.
+             * The login form or transmission mechanics are considered insecure.
+             * This is the case when the form is served via http or submitted
+             * insecurely.
+             */
+            public static final int INSECURE_FORM = 1 << 1;
+
+            /**
+             * The username is shared with another login entry.
+             * There are multiple login entries in the options that share the
+             * same username. You may have to disambiguate the login entry,
+             * e.g., using the last date of modification and its origin.
+             */
+            public static final int DUPLICATE_USERNAME = 1 << 2;
+
+            /**
+             * The login entry's origin matches the login form origin.
+             * The login was saved from the same origin it is being requested
+             * for, rather than for a subdomain.
+             */
+            public static final int MATCHING_ORIGIN = 1 << 3;
+        }
+
+        /**
+         * Construct a login select option.
+         *
+         * @param value The {@link LoginEntry} login entry selection option.
+         * @param hint The {@link Hint} detailing the type of the option.
+         */
+        /* package */ LoginSelectOption(
+                final @NonNull LoginEntry value,
+                final @LoginSelectHint int hint) {
+            super(value, hint);
+        }
+
+        /**
+         * Construct a login select option.
+         *
+         * @param value The {@link LoginEntry} login entry selection option.
+         */
+        public LoginSelectOption(final @NonNull LoginEntry value) {
+            this(value, Hint.NONE);
+        }
+
+        /* package */ static @NonNull LoginSelectOption fromBundle(
+                final @NonNull GeckoBundle bundle) {
+            final int hint = bundle.getInt("hint");
+            final LoginEntry value = new LoginEntry(bundle.getBundle("value"));
+
+            return new LoginSelectOption(value, hint);
+        }
+
+        @Override
+        /* package */ @NonNull GeckoBundle toBundle() {
+            final GeckoBundle bundle = new GeckoBundle(2);
+            bundle.putBundle(VALUE_KEY, value.toBundle());
+            bundle.putInt(HINT_KEY, hint);
+            return bundle;
+        }
+    }
+
+    /* package */ final static class LoginStorageProxy implements BundleEventListener {
         private static final String LOGTAG = "LoginStorageProxy";
 
-        private static final String FETCH_EVENT = "GeckoView:LoginStorage:Fetch";
-        private static final String SAVE_EVENT = "GeckoView:LoginStorage:Save";
-        private static final String USED_EVENT = "GeckoView:LoginStorage:Used";
+        private static final String FETCH_LOGIN_EVENT =
+            "GeckoView:Autocomplete:Fetch:Login";
+        private static final String SAVE_LOGIN_EVENT =
+            "GeckoView:Autocomplete:Save:Login";
+        private static final String USED_LOGIN_EVENT =
+            "GeckoView:Autocomplete:Used:Login";
 
-        private @Nullable Delegate mDelegate;
+        private @Nullable LoginStorageDelegate mDelegate;
 
-        public Proxy() {}
+        public LoginStorageProxy() {}
 
         private void registerListener() {
             EventDispatcher.getInstance().registerUiThreadListener(
                     this,
-                    FETCH_EVENT,
-                    SAVE_EVENT,
-                    USED_EVENT);
+                    FETCH_LOGIN_EVENT,
+                    SAVE_LOGIN_EVENT,
+                    USED_LOGIN_EVENT);
         }
 
         private void unregisterListener() {
             EventDispatcher.getInstance().unregisterUiThreadListener(
                     this,
-                    FETCH_EVENT,
-                    SAVE_EVENT,
-                    USED_EVENT);
+                    FETCH_LOGIN_EVENT,
+                    SAVE_LOGIN_EVENT,
+                    USED_LOGIN_EVENT);
         }
 
-        public synchronized void setDelegate(final @Nullable Delegate delegate) {
+        public synchronized void setDelegate(
+                final @Nullable LoginStorageDelegate delegate) {
             if (mDelegate == null && delegate != null) {
                 registerListener();
             } else if (mDelegate != null && delegate == null) {
@@ -409,7 +643,7 @@ public class LoginStorage {
             mDelegate = delegate;
         }
 
-        public synchronized @Nullable Delegate getDelegate() {
+        public synchronized @Nullable LoginStorageDelegate getDelegate() {
             return mDelegate;
         }
 
@@ -424,14 +658,14 @@ public class LoginStorage {
 
             if (mDelegate == null) {
                 if (callback != null) {
-                    callback.sendError("No LoginStorage delegate attached");
+                    callback.sendError("No LoginStorageDelegate attached");
                 }
                 return;
             }
 
-            if (FETCH_EVENT.equals(event)) {
+            if (FETCH_LOGIN_EVENT.equals(event)) {
                 final String domain = message.getString("domain");
-                final GeckoResult<LoginStorage.LoginEntry[]> result =
+                final GeckoResult<Autocomplete.LoginEntry[]> result =
                     mDelegate.onLoginFetch(domain);
 
                 if (result == null) {
@@ -443,6 +677,7 @@ public class LoginStorage {
                     logins -> {
                         if (logins == null) {
                             callback.sendSuccess(new GeckoBundle[0]);
+                            return;
                         }
 
                         // This is a one-liner with streams (API level 24).
@@ -455,12 +690,12 @@ public class LoginStorage {
                         callback.sendSuccess(loginBundles);
                     },
                     exception -> callback.sendError(exception.getMessage()));
-            } else if (SAVE_EVENT.equals(event)) {
+            } else if (SAVE_LOGIN_EVENT.equals(event)) {
                 final GeckoBundle loginBundle = message.getBundle("login");
                 final LoginEntry login = new LoginEntry(loginBundle);
 
                 mDelegate.onLoginSave(login);
-            } else if (USED_EVENT.equals(event)) {
+            } else if (USED_LOGIN_EVENT.equals(event)) {
                 final GeckoBundle loginBundle = message.getBundle("login");
                 final LoginEntry login = new LoginEntry(loginBundle);
                 final int fields = message.getInt("usedFields");
