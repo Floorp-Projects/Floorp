5c31755a29d266f1847a30f2925b5e7d9a8ab85a
Julian Descottes <jdescottes@mozilla.com>
2020-05-25 12:49:10 +0000

    Bug 1639111 - Show the filename, line and column for DevTools actor errors r=davidwalsh,ochameau
    Depends on D75902
    
    Differential Revision: https://phabricator.services.mozilla.com/D75904

--- devtools/server/tests/browser/browser_actor_error.js
+++ devtools/server/tests/browser/browser_actor_error.js
@@ -4,13 +4,13 @@
 "use strict";
 
 /**
- * Test that clients can catch errors in old style actors.
+ * Test that clients can catch errors in actors.
  */
 
 const ACTORS_URL =
   "chrome://mochitests/content/browser/devtools/server/tests/browser/error-actor.js";
 
-async function test() {
+add_task(async function test_old_actor() {
   DevToolsServer.init();
   DevToolsServer.registerAllActors();
 
@@ -36,6 +36,53 @@ async function test() {
   );
 
   await gClient.close();
+});
 
-  finish();
-}
+const TEST_ERRORS_ACTOR_URL =
+  "chrome://mochitests/content/browser/devtools/server/tests/browser/test-errors-actor.js";
+add_task(async function test_protocoljs_actor() {
+  DevToolsServer.init();
+  DevToolsServer.registerAllActors();
+
+  info("Register the new TestErrorsActor");
+  require(TEST_ERRORS_ACTOR_URL);
+  ActorRegistry.registerModule(TEST_ERRORS_ACTOR_URL, {
+    prefix: "testErrors",
+    constructor: "TestErrorsActor",
+    type: { global: true },
+  });
+
+  info("Create a DevTools client/server pair");
+  const transport = DevToolsServer.connectPipe();
+  const gClient = new DevToolsClient(transport);
+  await gClient.connect();
+
+  info("Retrieve a TestErrorsFront instance");
+  const testErrorsFront = await gClient.mainRoot.getFront("testErrors");
+  ok(testErrorsFront, "has a TestErrorsFront instance");
+
+  await Assert.rejects(testErrorsFront.throwsComponentsException(), e => {
+    return (
+      e.message ===
+      "NS_ERROR_NOT_IMPLEMENTED from: server0.conn1.testErrorsActor8 " +
+        "(chrome://mochitests/content/browser/devtools/server/tests/browser/test-errors-actor.js:41:0)"
+    );
+  });
+  await Assert.rejects(testErrorsFront.throwsException(), e => {
+    return (
+      e.message ===
+      'Protocol error (TypeError): can\'t access property "b",' +
+        " this.a is undefined from: server0.conn1.testErrorsActor8 " +
+        "(chrome://mochitests/content/browser/devtools/server/tests/browser/test-errors-actor.js:46:5)"
+    );
+  });
+  await Assert.rejects(testErrorsFront.throwsJSError(), e => {
+    return (
+      e.message ===
+      "Protocol error (Error): JSError from: server0.conn1.testErrorsActor8 " +
+        "(chrome://mochitests/content/browser/devtools/server/tests/browser/test-errors-actor.js:51:11)"
+    );
+  });
+
+  await gClient.close();
+});
