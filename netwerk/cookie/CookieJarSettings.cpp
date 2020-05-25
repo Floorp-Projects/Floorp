66458ba2e32ccb7ffed77b7b1e0aa6d9d452cde8
Dimi Lee <dlee@mozilla.com>
2020-05-25 18:30:43 +0000

    Bug 1637634 - Update IsOnContentBlockingAllowList in nsHttpChannel::AsyncOpen r=timhuang,necko-reviewers
    This patch removes TYPE_DOCUMENT test in test_shouldclassify because the
    testcase creates a non-top level channel with TYPE_DOCUMENT flag (this is wong!),
    which triggers the assertion in UpdateIsOnContentBlockingAllowList.
    
    File a follow-up bug 1640715 to add TYPE_DOCUMENT test back.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76152

--- netwerk/cookie/CookieJarSettings.cpp
+++ netwerk/cookie/CookieJarSettings.cpp
@@ -431,19 +431,22 @@ void CookieJarSettings::UpdateIsOnContentBlockingAllowList(
   MOZ_ASSERT(bc->IsTop());
 #endif
 
-  nsCOMPtr<nsIURI> uriBeingLoaded =
-      AntiTrackingUtils::MaybeGetDocumentURIBeingLoaded(aChannel);
-  nsCOMPtr<nsIPrincipal> contentBlockingAllowListPrincipal;
+  nsCOMPtr<nsIURI> uri;
+  nsresult rv = aChannel->GetURI(getter_AddRefs(uri));
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return;
+  }
 
   // We need to recompute the ContentBlockingAllowListPrincipal here for the
   // top level channel because we might navigate from the the initial
   // about:blank page or the existing page which may have a different origin
   // than the URI we are going to load here. Thus, we need to recompute the
   // prinicpal in order to get the correct ContentBlockingAllowListPrincipal.
+  nsCOMPtr<nsIPrincipal> contentBlockingAllowListPrincipal;
   OriginAttributes attrs;
   loadInfo->GetOriginAttributes(&attrs);
   ContentBlockingAllowList::RecomputePrincipal(
-      uriBeingLoaded, attrs, getter_AddRefs(contentBlockingAllowListPrincipal));
+      uri, attrs, getter_AddRefs(contentBlockingAllowListPrincipal));
 
   if (!contentBlockingAllowListPrincipal ||
       !contentBlockingAllowListPrincipal->GetIsContentPrincipal()) {
