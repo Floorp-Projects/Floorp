6f4fa0a5605837d81810808cae8b197347150de5
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-05-25 16:34:55 +0300

    Backed out 2 changesets (bug 1639111) for causing failures in browser_actor_error.js
    CLOSED TREE
    
    Backed out changeset 2964f445c2f5 (bug 1639111)
    Backed out changeset 84be772b9ac4 (bug 1639111)

--- devtools/shared/protocol/Actor.js
+++ devtools/shared/protocol/Actor.js
@@ -95,18 +95,10 @@ class Actor extends Pool {
     if (error.stack) {
       console.error(error.stack);
     }
-
     this.conn.send({
       from: this.actorID,
-      // error.error -> errors created using the throwError() helper
-      // error.name -> errors created using `new Error` or Components.exception
-      error: error.error || error.name || "unknownError",
+      error: error.error || "unknownError",
       message: error.message,
-      // error.fileName -> regular Error instances
-      // error.filename -> errors created using Components.exception
-      fileName: error.fileName || error.filename,
-      lineNumber: error.lineNumber,
-      columnNumber: error.columnNumber,
     });
   }
 
