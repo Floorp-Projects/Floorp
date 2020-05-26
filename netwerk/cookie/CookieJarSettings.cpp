afce63f3571b7863385a337469cf2455de546bde
Dimi Lee <dlee@mozilla.com>
2020-05-26 06:52:39 +0000

    Bug 1637634 - Update IsOnContentBlockingAllowList in nsHttpChannel::AsyncOpen r=timhuang,necko-reviewers
    This patch removes the assertion that ensures the channel is
    top-level via BrowsingContext.
    This is because more than one testcases creates a TYPE_DOCUMENT channel
    to test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76152

--- netwerk/cookie/CookieJarSettings.cpp
+++ netwerk/cookie/CookieJarSettings.cpp
@@ -425,25 +425,22 @@ void CookieJarSettings::UpdateIsOnContentBlockingAllowList(
   MOZ_ASSERT(aChannel);
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
 
-#ifdef DEBUG
-  RefPtr<dom::BrowsingContext> bc;
-  MOZ_ALWAYS_SUCCEEDS(loadInfo->GetTargetBrowsingContext(getter_AddRefs(bc)));
-  MOZ_ASSERT(bc->IsTop());
-#endif
-
-  nsCOMPtr<nsIURI> uriBeingLoaded =
-      AntiTrackingUtils::MaybeGetDocumentURIBeingLoaded(aChannel);
-  nsCOMPtr<nsIPrincipal> contentBlockingAllowListPrincipal;
+  nsCOMPtr<nsIURI> uri;
+  nsresult rv = aChannel->GetURI(getter_AddRefs(uri));
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return;
+  }
 
   // We need to recompute the ContentBlockingAllowListPrincipal here for the
   // top level channel because we might navigate from the the initial
   // about:blank page or the existing page which may have a different origin
   // than the URI we are going to load here. Thus, we need to recompute the
   // prinicpal in order to get the correct ContentBlockingAllowListPrincipal.
+  nsCOMPtr<nsIPrincipal> contentBlockingAllowListPrincipal;
   OriginAttributes attrs;
   loadInfo->GetOriginAttributes(&attrs);
   ContentBlockingAllowList::RecomputePrincipal(
-      uriBeingLoaded, attrs, getter_AddRefs(contentBlockingAllowListPrincipal));
+      uri, attrs, getter_AddRefs(contentBlockingAllowListPrincipal));
 
   if (!contentBlockingAllowListPrincipal ||
       !contentBlockingAllowListPrincipal->GetIsContentPrincipal()) {
