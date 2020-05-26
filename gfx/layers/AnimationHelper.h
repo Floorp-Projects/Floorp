4331b35ab5bd2047daa98d83e80fcb92ea03a542
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-05-26 21:33:25 +0000

    Bug 1640524 - Drop a variant of CompositorAnimationStorage::SetAnimatedValue. r=boris
    It's not used at all.
    
    Depends on D76631
    
    Differential Revision: https://phabricator.services.mozilla.com/D76632

--- gfx/layers/AnimationHelper.h
+++ gfx/layers/AnimationHelper.h
@@ -102,11 +102,6 @@ class CompositorAnimationStorage final {
                         const TransformData& aData);
 
   /**
-   * Set the animation transform in device pixel based on the unique id
-   */
-  void SetAnimatedValue(uint64_t aId, gfx::Matrix4x4&& aTransformInDevSpace);
-
-  /**
    * Set the animation opacity based on the unique id
    */
   void SetAnimatedValue(uint64_t aId, const float& aOpacity);
