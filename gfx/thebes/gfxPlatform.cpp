f4260a0133ebc8b8ab4b4e83bb7c641491133c6c
Andrew Osmond <aosmond@mozilla.com>
2020-05-26 21:20:18 +0000

    Bug 1639574 - Disable color management for CSS due to issues with canvas. r=jrmuizel, a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D76859

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -2087,6 +2087,11 @@ eCMSMode gfxPlatform::GetCMSMode() {
   return gCMSMode;
 }
 
+void gfxPlatform::SetCMSModeOverride(eCMSMode aMode) {
+  MOZ_ASSERT(gCMSInitialized);
+  gCMSMode = aMode;
+}
+
 int gfxPlatform::GetRenderingIntent() {
   // StaticPrefList.yaml is using 0 as the default for the rendering
   // intent preference, based on that being the value for
