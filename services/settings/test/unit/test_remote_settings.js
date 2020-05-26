da149113ba1f0ea283110d896315dbe6719c0c3a
Mathieu Leplatre <mathieu@mozilla.com>
2020-05-26 11:20:03 +0000

    Bug 1637178 - Distinguish local DB hijacks from sign errors in Uptake Telemetry r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D75529

--- services/settings/test/unit/test_remote_settings.js
+++ services/settings/test/unit/test_remote_settings.js
@@ -6,6 +6,9 @@ const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 const { AppConstants } = ChromeUtils.import(
   "resource://gre/modules/AppConstants.jsm"
 );
+const { ObjectUtils } = ChromeUtils.import(
+  "resource://gre/modules/ObjectUtils.jsm"
+);
 
 const IS_ANDROID = AppConstants.platform == "android";
 
@@ -66,6 +69,10 @@ function run_test() {
   server.registerPathHandler("/v1/", handleResponse);
   server.registerPathHandler(
     "/v1/buckets/monitor/collections/changes/records",
+    handleResponse
+  );
+  server.registerPathHandler(
+    "/v1/buckets/main/collections/password-fields",
     handleResponse
   );
   server.registerPathHandler(
@@ -304,8 +311,8 @@ add_task(async function test_get_ignores_synchronization_errors() {
 });
 add_task(clear_state);
 
-add_task(async function test_get_can_verify_signature() {
-  // No signature in metadata.
+add_task(async function test_get_verify_signature_no_sync() {
+  // No signature in metadata, and no sync if empty.
   let error;
   try {
     await client.get({ verifySignature: true, syncIfEmpty: false });
@@ -313,7 +320,35 @@ add_task(async function test_get_can_verify_signature() {
     error = e;
   }
   equal(error.message, "Missing signature (main/password-fields)");
+});
+add_task(clear_state);
 
+add_task(async function test_get_can_verify_signature_pulled() {
+  // Populate the local DB (only records, eg. loaded from dump previously)
+  await client._importJSONDump();
+
+  let calledSignature;
+  client._verifier = {
+    async asyncVerifyContentSignature(serialized, signature) {
+      calledSignature = signature;
+      return true;
+    },
+  };
+
+  // No metadata in local DB, but gets pulled and then verifies.
+  ok(ObjectUtils.isEmpty(await client.db.getMetadata()), "Metadata is empty");
+
+  await client.get({ verifySignature: true });
+
+  ok(
+    !ObjectUtils.isEmpty(await client.db.getMetadata()),
+    "Metadata was pulled"
+  );
+  ok(calledSignature.endsWith("some-sig"), "Signature was verified");
+});
+add_task(clear_state);
+
+add_task(async function test_get_can_verify_signature() {
   // Populate the local DB (record and metadata)
   await client.maybeSync(2000);
 
@@ -325,12 +360,14 @@ add_task(async function test_get_can_verify_signature() {
       return JSON.parse(serialized).data.length == 1;
     },
   };
+  ok(await Utils.hasLocalData(client), "Local data was populated");
   await client.get({ verifySignature: true });
-  ok(calledSignature.endsWith("abcdef"));
+
+  ok(calledSignature.endsWith("abcdef"), "Signature was verified");
 
   // It throws when signature does not verify.
   await client.db.delete("9d500963-d80e-3a91-6e74-66f3811b99cc");
-  error = null;
+  let error = null;
   try {
     await client.get({ verifySignature: true });
   } catch (e) {
@@ -967,7 +1004,9 @@ wNuvFqc=
       ],
       status: { status: 200, statusText: "OK" },
       responseBody: {
-        metadata: {},
+        metadata: {
+          signature: {},
+        },
         timestamp: 4000,
         changes: [
           {
@@ -995,7 +1034,9 @@ wNuvFqc=
       ],
       status: { status: 200, statusText: "OK" },
       responseBody: {
-        metadata: {},
+        metadata: {
+          signature: {},
+        },
         timestamp: 5000,
         changes: [
           {
@@ -1084,6 +1125,24 @@ wNuvFqc=
         ],
       },
     },
+    "GET:/v1/buckets/main/collections/password-fields": {
+      sampleHeaders: [
+        "Access-Control-Allow-Origin: *",
+        "Access-Control-Expose-Headers: Retry-After, Content-Length, Alert, Backoff",
+        "Content-Type: application/json; charset=UTF-8",
+        "Server: waitress",
+        'Etag: "3000"',
+      ],
+      status: { status: 200, statusText: "OK" },
+      responseBody: {
+        data: {
+          signature: {
+            signature: "some-sig",
+            x5u: `http://localhost:${port}/fake-x5u`,
+          },
+        },
+      },
+    },
     "GET:/v1/buckets/main/collections/password-fields/changeset?_expected=1337": {
       sampleHeaders: [
         "Access-Control-Allow-Origin: *",
@@ -1094,7 +1153,9 @@ wNuvFqc=
       ],
       status: { status: 200, statusText: "OK" },
       responseBody: {
-        metadata: {},
+        metadata: {
+          signature: {},
+        },
         timestamp: 3000,
         changes: [
           {
@@ -1202,7 +1263,9 @@ wNuvFqc=
       status: { status: 200, statusText: "OK" },
       responseBody: {
         timestamp: 3000,
-        metadata: {},
+        metadata: {
+          signature: {},
+        },
         changes: [
           {
             id: "1f5c98b9-6d93-4c13-aa26-978b38695096",
