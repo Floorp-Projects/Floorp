0766f22b8df51752e7c905d37d22d14283272ab3
Razvan Maries <rmaries@mozilla.com>
2020-05-28 01:35:06 +0300

    Backed out changeset 5daeee90db7e (bug 1641297) for assertion failures. CLOSED TREE

--- js/src/jit/IonCacheIRCompiler.cpp
+++ js/src/jit/IonCacheIRCompiler.cpp
@@ -2418,7 +2418,3 @@ bool IonCacheIRCompiler::emitGuardFunApply(Int32OperandId argcId,
                                            CallFlags flags) {
   MOZ_CRASH("Call ICs not used in ion");
 }
-
-bool IonCacheIRCompiler::emitIsArrayResult(ValOperandId inputId) {
-  MOZ_CRASH("Call ICs not used in ion");
-}
