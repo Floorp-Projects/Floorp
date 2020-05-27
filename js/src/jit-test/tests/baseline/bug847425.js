e993887f7175b6b63351124f1e316734b5d83a7b
Jan de Mooij <jdemooij@mozilla.com>
2020-05-27 12:26:32 +0000

    Bug 1640298 - Disable bug847425.js jit-test on Android due to harness problems. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D77017

--- js/src/jit-test/tests/baseline/bug847425.js
+++ js/src/jit-test/tests/baseline/bug847425.js
@@ -1,4 +1,6 @@
-// |jit-test| allow-oom; allow-unhandlable-oom
+// |jit-test| allow-oom; allow-unhandlable-oom; skip-if: getBuildConfiguration()['android']
+// Disabled on Android due to harness problems (Bug 1532654)
+
 gcparam("maxBytes", gcparam("gcBytes") + 4*1024);
 var max = 400;
 function f(b) {
