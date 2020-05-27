acc3f3c95458e12d6090f9dc2e711d4ff596b9a0
Jan Varga <jan.varga@gmail.com>
2020-05-27 15:52:49 +0000

    Bug 1621916 - Remove unused argument in Client::InitOrigin; r=dom-workers-and-storage-reviewers,sg
    Depends on D77070
    
    Differential Revision: https://phabricator.services.mozilla.com/D77072

--- dom/cache/QuotaClientImpl.h
+++ dom/cache/QuotaClientImpl.h
@@ -30,8 +30,7 @@ class CacheQuotaClient final : public quota::Client {
                               const nsACString& aGroup,
                               const nsACString& aOrigin,
                               const AtomicBool& aCanceled,
-                              UsageInfo* aUsageInfo,
-                              bool aForGetUsage) override;
+                              UsageInfo* aUsageInfo) override;
 
   virtual nsresult GetUsageForOrigin(PersistenceType aPersistenceType,
                                      const nsACString& aGroup,
