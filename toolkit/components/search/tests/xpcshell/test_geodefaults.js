d62a4998a30b30ab319b1152154641d5fb73fc06
Dale Harvey <dale@arandomurl.com>
2020-05-27 20:16:19 +0000

    Bug 1627541 - Move pref handling into Region.jsm r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D75719

--- toolkit/components/search/tests/xpcshell/test_geodefaults.js
+++ toolkit/components/search/tests/xpcshell/test_geodefaults.js
@@ -18,8 +18,6 @@ add_task(async function no_request_if_prefed_off() {
     await Services.search.init(true);
     await promiseAfterCache();
 
-    checkNoRequest(requests);
-
     // Install kTestEngineName and wait for it to reach the disk.
     await Promise.all([installTestEngine(), promiseAfterCache()]);
 
@@ -43,12 +41,11 @@ add_task(async function should_get_geo_defaults_only_once() {
   await withGeoServer(async function cont(requests) {
     // (Re)initializing the search service should trigger a request,
     // and set the default engine based on it.
-    // Due to the previous initialization, we expect the region to already be set.
-    await Promise.all([
-      asyncReInit({ awaitRegionFetch: true }),
-      promiseAfterCache(),
-    ]);
-    checkRequest(requests);
+    Region._setRegion("FR", false);
+    let enginesReloaded = SearchTestUtils.promiseSearchNotification(
+      "engines-reloaded"
+    );
+    await Promise.all([asyncReInit(), enginesReloaded, promiseAfterCache()]);
     Assert.equal((await Services.search.getDefault()).name, kTestEngineName);
 
     // Verify the metadata was written correctly.
@@ -61,20 +58,8 @@ add_task(async function should_get_geo_defaults_only_once() {
     Assert.equal(metadata.searchDefaultHash.length, 44);
 
     // The next restart shouldn't trigger a request.
-    await asyncReInit({ awaitRegionFetch: true });
-    checkNoRequest(requests);
+    await asyncReInit();
     Assert.equal((await Services.search.getDefault()).name, kTestEngineName);
-  });
-});
-
-add_task(async function should_request_when_region_not_set() {
-  await withGeoServer(async function cont(requests) {
-    Services.prefs.clearUserPref("browser.search.region");
-    await Promise.all([
-      asyncReInit({ awaitRegionFetch: true }),
-      promiseAfterCache(),
-    ]);
-    checkRequest(requests);
   });
 });
 
@@ -83,11 +68,7 @@ add_task(async function should_recheck_if_interval_expired() {
     await forceExpiration();
 
     let date = Date.now();
-    await Promise.all([
-      asyncReInit({ awaitRegionFetch: true }),
-      promiseAfterCache(),
-    ]);
-    checkRequest(requests);
+    await Promise.all([asyncReInit(), promiseAfterCache()]);
 
     // Check that the expiration timestamp has been updated.
     let metadata = await promiseGlobalMetadata();
@@ -109,10 +90,7 @@ add_task(async function should_recheck_if_appversion_changed() {
     data.appVersion = "1";
     await promiseSaveCacheData(data);
 
-    await Promise.all([
-      asyncReInit({ awaitRegionFetch: true }),
-      promiseAfterCache(),
-    ]);
+    await Promise.all([asyncReInit(), promiseAfterCache()]);
     checkRequest(requests);
 
     // Check that the appVersion has been updated
@@ -138,7 +116,7 @@ add_task(async function should_recheck_when_broken_hash() {
     let unInitPromise = SearchTestUtils.promiseSearchNotification(
       "uninit-complete"
     );
-    let reInitPromise = asyncReInit({ awaitRegionFetch: true });
+    let reInitPromise = asyncReInit();
     await unInitPromise;
 
     await reInitPromise;
@@ -182,7 +160,7 @@ add_task(async function should_remember_cohort_id() {
       // Trigger a new request.
       await forceExpiration();
       let commitPromise = promiseAfterCache();
-      await asyncReInit({ awaitRegionFetch: true });
+      await asyncReInit();
       checkRequest(requests);
       await commitPromise;
 
@@ -202,7 +180,7 @@ add_task(async function should_remember_cohort_id() {
     // will remove it from the prefs due to the server no longer sending it.
     await forceExpiration();
     let commitPromise = promiseAfterCache();
-    await asyncReInit({ awaitRegionFetch: true });
+    await asyncReInit();
     checkRequest(requests, cohort);
     await commitPromise;
     Assert.equal(
@@ -217,41 +195,14 @@ add_task(async function should_retry_after_failure() {
     async function cont(requests) {
       // Trigger a new request.
       await forceExpiration();
-      await asyncReInit({ awaitRegionFetch: true });
+      await asyncReInit();
       checkRequest(requests);
 
       // After another restart, a new request should be triggered automatically without
       // the test having to call forceExpiration again.
-      await asyncReInit({ awaitRegionFetch: true });
+      await asyncReInit();
       checkRequest(requests);
     },
     { path: "lookup_fail" }
-  );
-});
-
-add_task(async function should_honor_retry_after_header() {
-  await withGeoServer(
-    async function cont(requests) {
-      // Trigger a new request.
-      await forceExpiration();
-      let date = Date.now();
-      let commitPromise = promiseAfterCache();
-      await asyncReInit({ awaitRegionFetch: true });
-      await commitPromise;
-      checkRequest(requests);
-
-      // Check that the expiration timestamp has been updated.
-      let metadata = await promiseGlobalMetadata();
-      Assert.equal(typeof metadata.searchDefaultExpir, "number");
-      Assert.ok(metadata.searchDefaultExpir >= date + kDayInSeconds * 1000);
-      Assert.ok(
-        metadata.searchDefaultExpir < date + (kDayInSeconds + 3600) * 1000
-      );
-
-      // After another restart, a new request should not be triggered.
-      await asyncReInit({ awaitRegionFetch: true });
-      checkNoRequest(requests);
-    },
-    { path: "lookup_unavailable" }
   );
 });
