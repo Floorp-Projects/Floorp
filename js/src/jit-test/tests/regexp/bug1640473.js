e605fb6998ea105dbb939fff7066a5d43f8c8089
Iain Ireland <iireland@mozilla.com>
2020-05-27 07:09:03 +0000

    Bug 1640473: Add testcase r=tcampbell
    Depends on D76751
    
    Differential Revision: https://phabricator.services.mozilla.com/D76889

---
+++ js/src/jit-test/tests/regexp/bug1640473.js
@@ -0,0 +1,15 @@
+var s = "";
+var input = "";
+for (var i = 0; i < 500; ++i) {
+    s += "(?<a" + i + ">a)";
+    s += "(?<b" + i + ">b)?";
+    input += "a";
+}
+
+var r = RegExp(s);
+var e = r.exec(input);
+
+for (var i = 0; i < 500; i++) {
+    assertEq(e.groups["a" + i], "a");
+    assertEq(e.groups["b" + i], undefined);
+}
