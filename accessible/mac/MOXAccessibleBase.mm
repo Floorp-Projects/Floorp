576a980f195699097df59f18e8f5aaafe509b4bb
Eitan Isaacson <eitan@monotonous.org>
2020-05-27 17:26:38 +0000

    Bug 1641002 - Map NSAccessibility API to MOXAccessible protocol. r=morgan
    Add most attributes getters, setters and actions to prottocol,
    autogenerate objective c dictionaries, and implement forwarding
    logic from NSAccessibility methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D76921

--- accessible/mac/MOXAccessibleBase.mm
+++ accessible/mac/MOXAccessibleBase.mm
@@ -12,6 +12,39 @@
 
 using namespace mozilla::a11y;
 
+@interface NSObject (MOXAccessible)
+
+// This NSObject conforms to mozAccessible.
+// This is needed to we know to mutate the value
+// (get represented view, check isAccessibilityElement)
+// before forwarding it to NSAccessibility.
+- (BOOL)isMozAccessible;
+
+// Same as above, but this checks if the NSObject is an array with
+// mozAccessible conforming objects.
+- (BOOL)hasMozAccessibles;
+
+@end
+
+@implementation NSObject (MOXAccessible)
+
+- (BOOL)isMozAccessible {
+  return [self conformsToProtocol:@protocol(mozAccessible)];
+}
+
+- (BOOL)hasMozAccessibles {
+  return [self isKindOfClass:[NSArray class]] && [[(NSArray*)self firstObject] isMozAccessible];
+}
+
+@end
+
+// Private methods
+@interface MOXAccessibleBase ()
+
+- (BOOL)isSelectorSupported:(SEL)selector;
+
+@end
+
 @implementation MOXAccessibleBase
 
 #pragma mark - mozAccessible/widget
@@ -32,35 +65,156 @@ using namespace mozilla::a11y;
 
 - (NSArray*)accessibilityAttributeNames {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NIL;
-  return @[];
+
+  if ([self isExpired]) {
+    return nil;
+  }
+
+  static NSMutableDictionary* attributesForEachClass = nil;
+
+  if (!attributesForEachClass) {
+    attributesForEachClass = [[NSMutableDictionary alloc] init];
+  }
+
+  NSMutableArray* attributes =
+      attributesForEachClass [[self class]] ?: [[NSMutableArray alloc] init];
+
+  NSDictionary* getters = mac::AttributeGetters();
+  if (![attributes count]) {
+    // Go through all our attribute getters, if they are supported by this class
+    // advertise the attribute name.
+    for (NSString* attribute in getters) {
+      SEL selector = NSSelectorFromString(getters[attribute]);
+      if ([self isSelectorSupported:selector]) {
+        [attributes addObject:attribute];
+      }
+    }
+
+    // We store a hash table with types as keys, and atttribute lists as values.
+    // This lets us cache the atttribute list of each subclass so we only
+    // need to gather its MOXAccessible methods once.
+    // XXX: Uncomment when accessibilityAttributeNames is removed from all subclasses.
+    // attributesForEachClass[[self class]] = attributes;
+  }
+
+  return attributes;
+
   NS_OBJC_END_TRY_ABORT_BLOCK_NIL;
 }
 
 - (id)accessibilityAttributeValue:(NSString*)attribute {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NIL;
-  return nil;
+  if ([self isExpired]) {
+    return nil;
+  }
+
+  id value = nil;
+  NSDictionary* getters = mac::AttributeGetters();
+  if (getters[attribute]) {
+    SEL selector = NSSelectorFromString(getters[attribute]);
+    if ([self isSelectorSupported:selector]) {
+      value = [self performSelector:selector];
+    }
+  }
+
+  if ([value isMozAccessible]) {
+    // If this is a mozAccessible, get its represented view and filter it if
+    // it should be ignored.
+    value = GetObjectOrRepresentedView(value);
+    return [value isAccessibilityElement] ? value : nil;
+  }
+
+  if ([value hasMozAccessibles]) {
+    // If this is an array of mozAccessibles, get each element's represented view
+    // and remove it from the returned array if it should be ignored.
+    NSUInteger arrSize = [value count];
+    NSMutableArray* arr = [[NSMutableArray alloc] initWithCapacity:arrSize];
+    for (NSUInteger i = 0; i < arrSize; i++) {
+      id<mozAccessible> mozAcc = GetObjectOrRepresentedView(value[i]);
+      if ([mozAcc isAccessibilityElement]) {
+        [arr addObject:mozAcc];
+      }
+    }
+
+    return arr;
+  }
+
+  return value;
+
   NS_OBJC_END_TRY_ABORT_BLOCK_NIL;
 }
 
 - (BOOL)accessibilityIsAttributeSettable:(NSString*)attribute {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_RETURN;
-  return NO;
+
+  if ([self isExpired]) {
+    return NO;
+  }
+
+  NSDictionary* setters = mac::AttributeSetters();
+  if (setters[attribute]) {
+    SEL selector = NSSelectorFromString(setters[attribute]);
+    return ([self isSelectorSupported:selector]);
+  }
+
   NS_OBJC_END_TRY_ABORT_BLOCK_RETURN(NO);
 }
 
 - (void)accessibilitySetValue:(id)value forAttribute:(NSString*)attribute {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
+
+  if ([self isExpired]) {
+    return;
+  }
+
+  NSDictionary* setters = mac::AttributeSetters();
+  if (setters[attribute]) {
+    SEL selector = NSSelectorFromString(setters[attribute]);
+    if ([self isSelectorSupported:selector]) {
+      [self performSelector:selector withObject:value];
+    }
+  }
+
   NS_OBJC_END_TRY_ABORT_BLOCK;
 }
 
 - (NSArray*)accessibilityActionNames {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NIL;
-  return @[];
+
+  if ([self isExpired]) {
+    return nil;
+  }
+
+  NSMutableArray* actionNames = [[NSMutableArray alloc] init];
+
+  NSDictionary* actions = mac::Actions();
+  for (NSString* action in actions) {
+    SEL selector = NSSelectorFromString(actions[action]);
+    if ([self isSelectorSupported:selector]) {
+      [actionNames addObject:action];
+    }
+  }
+
+  return actionNames;
+
   NS_OBJC_END_TRY_ABORT_BLOCK_NIL;
 }
 
 - (void)accessibilityPerformAction:(NSString*)action {
   NS_OBJC_BEGIN_TRY_ABORT_BLOCK;
+
+  if ([self isExpired]) {
+    return;
+  }
+
+  NSDictionary* actions = mac::Actions();
+  if (actions[action]) {
+    SEL selector = NSSelectorFromString(actions[action]);
+    if ([self isSelectorSupported:selector]) {
+      [self performSelector:selector];
+    }
+  }
+
   NS_OBJC_END_TRY_ABORT_BLOCK;
 }
 
@@ -132,6 +286,10 @@ using namespace mozilla::a11y;
   NSAccessibilityPostNotification(GetObjectOrRepresentedView(self), notification);
 }
 
+- (BOOL)moxBlockSelector:(SEL)selector {
+  return NO;
+}
+
 #pragma mark -
 
 - (BOOL)isExpired {
@@ -144,6 +302,12 @@ using namespace mozilla::a11y;
   mIsExpired = YES;
 
   [self moxPostNotification:NSAccessibilityUIElementDestroyedNotification];
+}
+
+#pragma mark - Private
+
+- (BOOL)isSelectorSupported:(SEL)selector {
+  return [self respondsToSelector:selector] && ![self moxBlockSelector:selector];
 }
 
 @end
--- accessible/mac/MOXAccessibleProtocol.h
+++ accessible/mac/MOXAccessibleProtocol.h
@@ -3,6 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This protocol's primary use is for abstracting the NSAccessibility informal protocol
+// into a formal internal API. Conforming classes get to choose a subset of the optional
+// methods to implement. Those methods will be mapped to NSAccessibility attributes or actions.
+// A conforming class can implement moxBlockSelector to control which of its implemented
+// methods should be exposed to NSAccessibility.
+
 @protocol MOXAccessible
 
 // The deepest descendant of the accessible subtree that contains the specified point.
@@ -15,5 +21,204 @@
 
 // Sends a notification to any observing assistive applications.
 - (void)moxPostNotification:(NSString* _Nonnull)notification;
+
+// Return YES if selector should be considered not supported.
+// Used in implementations such as:
+// - accessibilityAttributeNames
+// - accessibilityActionNames
+// - accessibilityIsAttributeSettable
+- (BOOL)moxBlockSelector:(SEL _Nonnull)selector;
+
+@optional
+
+#pragma mark - AttributeGetters
+
+// AXChildren
+- (NSArray* _Nullable)moxChildren;
+
+// AXParent
+- (id _Nullable)moxParent;
+
+// AXRole
+- (NSString* _Nullable)moxRole;
+
+// AXRoleDescription
+- (NSString* _Nullable)moxRoleDescription;
+
+// AXSubrole
+- (NSString* _Nullable)moxSubrole;
+
+// AXTitle
+- (NSString* _Nullable)moxTitle;
+
+// AXDescription
+- (NSString* _Nullable)moxLabel;
+
+// AXHelp
+- (NSString* _Nullable)moxHelp;
+
+// AXValue
+- (id _Nullable)moxValue;
+
+// AXSize
+- (NSValue* _Nullable)moxSize;
+
+// AXPosition
+- (NSValue* _Nullable)moxPosition;
+
+// AXEnabled
+- (NSNumber* _Nullable)moxEnabled;
+
+// AXFocused
+- (NSNumber* _Nullable)moxFocused;
+
+// AXWindow
+- (id _Nullable)moxWindow;
+
+// AXTitleUIElement
+- (id _Nullable)moxTitleUIElement;
+
+// AXTopLevelUIElement
+- (id _Nullable)moxTopLevelUIElement;
+
+// AXHasPopup
+- (NSNumber* _Nullable)moxHasPopup;
+
+// AXARIACurrent
+- (NSString* _Nullable)moxAriaCurrent;
+
+// AXSelected
+- (NSNumber* _Nullable)moxSelected;
+
+// AXRequired
+- (NSNumber* _Nullable)moxRequired;
+
+// AXDOMIdentifier
+- (NSString* _Nullable)moxDomIdentifier;
+
+// AXURL
+- (NSURL* _Nullable)moxUrl;
+
+// AXPopupValue
+- (NSString* _Nullable)moxPopupValue;
+
+// AXVisited
+- (NSNumber* _Nullable)moxVisited;
+
+// AXExpanded
+- (NSNumber* _Nullable)moxExpanded;
+
+// AXMain
+- (NSNumber* _Nullable)moxMain;
+
+// AXMinimized
+- (NSNumber* _Nullable)moxMinimized;
+
+// AXSelectedChildren
+- (NSArray* _Nullable)moxSelectedChildren;
+
+// AXTabs
+- (NSArray* _Nullable)moxTabs;
+
+// AXContents
+- (NSArray* _Nullable)moxContents;
+
+// AXOrientation
+- (NSString* _Nullable)moxOrientation;
+
+// AXMenuItemMarkChar
+- (NSString* _Nullable)moxMenuItemMarkChar;
+
+// Table Attributes
+
+// AXRowCount
+- (NSNumber* _Nullable)moxRowCount;
+
+// AXColumnCount
+- (NSNumber* _Nullable)moxColumnCount;
+
+// AXRows
+- (NSArray* _Nullable)moxRows;
+
+// AXColumns
+- (NSArray* _Nullable)moxColumns;
+
+// AXIndex
+- (NSNumber* _Nullable)moxIndex;
+
+// AXRowIndexRange
+- (NSValue* _Nullable)moxRowIndexRange;
+
+// AXColumnIndexRange
+- (NSValue* _Nullable)moxColumnIndexRange;
+
+// AXRowHeaderUIElements
+- (NSArray* _Nullable)moxRowHeaderUIElements;
+
+// AXColumnHeaderUIElements
+- (NSArray* _Nullable)moxColumnHeaderUIElements;
+
+// Math Attributes
+
+// AXMathRootRadicand
+- (id _Nullable)moxMathRootRadicand;
+
+// AXMathRootIndex
+- (id _Nullable)moxMathRootIndex;
+
+// AXMathFractionNumerator
+- (id _Nullable)moxMathFractionNumerator;
+
+// AXMathFractionDenominator
+- (id _Nullable)moxMathFractionDenominator;
+
+// AXMathLineThickness
+- (NSNumber* _Nullable)moxMathLineThickness;
+
+// AXMathBase
+- (id _Nullable)moxMathBase;
+
+// AXMathSubscript
+- (id _Nullable)moxMathSubscript;
+
+// AXMathSuperscript
+- (id _Nullable)moxMathSuperscript;
+
+// AXMathUnder
+- (id _Nullable)moxMathUnder;
+
+// AXMathOver
+- (id _Nullable)moxMathOver;
+
+#pragma mark - AttributeSetters
+
+// AXValue
+- (void)moxSetValue:(id _Nullable)value;
+
+// AXFocused
+- (void)moxSetFocused:(NSNumber* _Nullable)focused;
+
+// AXSelected
+- (void)moxSetSelected:(NSNumber* _Nullable)selected;
+
+// AXSelectedChildren
+- (void)moxSetSelectedChildren:(NSArray* _Nullable)selectedChildren;
+
+#pragma mark - Actions
+
+// AXPress
+- (void)moxPerformPress;
+
+// AXShowMenu
+- (void)moxPerformShowMenu;
+
+// AXScrollToVisible
+- (void)moxPerformScrollToVisible;
+
+// AXIncrement
+- (void)moxPerformIncrement;
+
+// AXDecrement
+- (void)moxPerformDecrement;
 
 @end
