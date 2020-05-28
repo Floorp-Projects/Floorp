42f5cca14e6d63a78711aa6178f60bf8a42302ea
Jan Varga <jan.varga@gmail.com>
2020-05-28 11:14:52 +0000

    Bug 1621916 - Fix IndexedDB to ignore unknown files in the client directory during initialization and usage calculation; r=dom-workers-and-storage-reviewers,sg,ttung
    Differential Revision: https://phabricator.services.mozilla.com/D77073

--- dom/cache/QuotaClientImpl.h
+++ dom/cache/QuotaClientImpl.h
@@ -134,8 +134,7 @@ class CacheQuotaClient final : public quota::Client {
                                      const nsACString& aGroup,
                                      const nsACString& aOrigin,
                                      const AtomicBool& aCanceled,
-                                     UsageInfo* aUsageInfo,
-                                     const bool aInitializing);
+                                     bool aInitializing, UsageInfo* aUsageInfo);
 
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CacheQuotaClient, override)
 
