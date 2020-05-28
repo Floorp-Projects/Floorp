0bc81ca44a6b5721610a15fd27765d75271c2a46
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-05-29 00:06:56 +0300

    Backed out changeset 082fb07b2a02 (bug 1253129) for bc failures on browser_ext_windows_create_params.js . CLOSED TREE

--- browser/components/extensions/test/browser/browser_ext_windows_create_params.js
+++ browser/components/extensions/test/browser/browser_ext_windows_create_params.js
@@ -2,12 +2,6 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-const { AddonTestUtils } = ChromeUtils.import(
-  "resource://testing-common/AddonTestUtils.jsm"
-);
-
-AddonTestUtils.initMochitest(this);
-
 // Tests that incompatible parameters can't be used together.
 add_task(async function testWindowCreateParams() {
   let extension = ExtensionTestUtils.loadExtension({
@@ -36,60 +30,4 @@ add_task(async function testWindowCreateParams() {
   await extension.startup();
   await extension.awaitFinish("window-create-params");
   await extension.unload();
-});
-
-// We do not support the focused param, however we do not want
-// to fail despite an error when it is passed.  This provides
-// better code level compatibility with chrome.
-add_task(async function testWindowCreateFocused() {
-  let extension = ExtensionTestUtils.loadExtension({
-    async background() {
-      try {
-        let window = await browser.windows.create({});
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused without focused param"
-        );
-        await browser.windows.remove(window.id);
-        window = await browser.windows.create({ focused: true });
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused with focused: true"
-        );
-        await browser.windows.remove(window.id);
-        window = await browser.windows.create({ focused: false });
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused with focused: false"
-        );
-        await browser.windows.remove(window.id);
-        browser.test.notifyPass("window-create-params");
-      } catch (e) {
-        browser.test.fail(`${e} :: ${e.stack}`);
-        browser.test.notifyFail("window-create-params");
-      }
-    },
-  });
-
-  ExtensionTestUtils.failOnSchemaWarnings(false);
-  let { messages } = await AddonTestUtils.promiseConsoleOutput(async () => {
-    await extension.startup();
-    await extension.awaitFinish("window-create-params");
-    await extension.unload();
-  });
-
-  AddonTestUtils.checkMessages(
-    messages,
-    {
-      expected: [
-        /Warning processing focused: Opening inactive windows is not supported./,
-      ],
-    },
-    "Expected warning processing focused"
-  );
-
-  ExtensionTestUtils.failOnSchemaWarnings(true);
 });
