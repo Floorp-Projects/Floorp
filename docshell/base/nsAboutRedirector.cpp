48529c562fc16d93d9e11e687ffbc2f5eee40f70
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-05-28 00:07:46 +0000

    Bug 1637869 - P3. Force about:credits to load in the content process. r=nika
    It was apparently overlooked.
    
    Yours truly doesn't even appear on that list :(
    
    Differential Revision: https://phabricator.services.mozilla.com/D76316

--- docshell/base/nsAboutRedirector.cpp
+++ docshell/base/nsAboutRedirector.cpp
@@ -75,7 +75,8 @@ static const RedirEntry kRedirMap[] = {
     {"crashes", "chrome://global/content/crashes.html", 0},
 #endif
     {"credits", "https://www.mozilla.org/credits/",
-     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT},
+     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
+         nsIAboutModule::URI_MUST_LOAD_IN_CHILD},
     {"httpsonlyerror", "chrome://global/content/httpsonlyerror/errorpage.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
