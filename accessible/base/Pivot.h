d013e0b775d8125fe9423ba3d71012148d4fce5d
Morgan Reschenberg <mreschenberg@mozilla.com>
2020-05-28 15:53:01 +0000

    Bug 1624909: Create and maintain radio siblings array for position information. r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D72751

--- accessible/base/Pivot.h
+++ accessible/base/Pivot.h
@@ -83,6 +83,19 @@ class Pivot final {
   Accessible* mRoot;
 };
 
+/**
+ * This rule matches accessibles on a given role.
+ */
+class PivotRoleRule final : public PivotRule {
+ public:
+  explicit PivotRoleRule(role aRole);
+
+  virtual uint16_t Match(Accessible* aAccessible) override;
+
+ private:
+  role mRole;
+};
+
 }  // namespace a11y
 }  // namespace mozilla
 
