c733b9ce16932adfebb51a3b4eec474c45ac4d58
Razvan Maries <rmaries@mozilla.com>
2020-05-28 05:33:20 +0300

    Backed out changeset 0dba60796753 (bug 1611204) for perma failures on isIntersecting-threshold.html. CLOSED TREE

--- dom/base/DOMIntersectionObserver.h
+++ dom/base/DOMIntersectionObserver.h
@@ -31,9 +31,9 @@ class DOMIntersectionObserverEntry final : public nsISupports,
                                double aIntersectionRatio)
       : mOwner(aOwner),
         mTime(aTime),
-        mRootBounds(std::move(aRootBounds)),
-        mBoundingClientRect(std::move(aBoundingClientRect)),
-        mIntersectionRect(std::move(aIntersectionRect)),
+        mRootBounds(aRootBounds),
+        mBoundingClientRect(aBoundingClientRect),
+        mIntersectionRect(aIntersectionRect),
         mIsIntersecting(aIsIntersecting),
         mTarget(aTarget),
         mIntersectionRatio(aIntersectionRatio) {}
@@ -138,7 +138,6 @@ class DOMIntersectionObserver final : public nsISupports,
                                       const Maybe<nsRect>& aRootRect,
                                       const nsRect& aTargetRect,
                                       const Maybe<nsRect>& aIntersectionRect,
-                                      bool aIsIntersecting,
                                       double aIntersectionRatio);
 
   nsCOMPtr<nsPIDOMWindowInner> mOwner;
