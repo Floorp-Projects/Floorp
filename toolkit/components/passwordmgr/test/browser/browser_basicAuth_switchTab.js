9ade45c244d9c5e59215a410675fb5a13645ff95
pbz <pbz@mozilla.com>
2020-05-28 11:11:52 +0000

    Bug 613785 - Updated http auth prompt tests to support tab prompts. r=johannh
    Differential Revision: https://phabricator.services.mozilla.com/D75567

--- toolkit/components/passwordmgr/test/browser/browser_basicAuth_switchTab.js
+++ toolkit/components/passwordmgr/test/browser/browser_basicAuth_switchTab.js
@@ -2,40 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const PROMPT_URL = "chrome://global/content/commonDialog.xhtml";
+let modalType = Services.prefs.getIntPref("prompts.modalType.httpAuth");
 
 add_task(async function test() {
   await new Promise(resolve => {
     let tab = BrowserTestUtils.addTab(gBrowser);
     isnot(tab, gBrowser.selectedTab, "New tab shouldn't be selected");
 
-    let listener = {
-      onOpenWindow(xulWin) {
-        var domwindow = xulWin.docShell.domWindow;
-        waitForFocus(() => {
-          is(
-            domwindow.document.location.href,
-            PROMPT_URL,
-            "Should have seen a prompt window"
-          );
-          is(
-            domwindow.args.promptType,
-            "promptUserAndPass",
-            "Should be an authenticate prompt"
-          );
-
-          is(gBrowser.selectedTab, tab, "Should have selected the new tab");
-
-          domwindow.document.getElementById("commonDialog").cancelDialog();
-        }, domwindow);
-      },
-
-      onCloseWindow() {},
-    };
-
-    Services.wm.addListener(listener);
+    let authPromptShown = PromptTestUtils.waitForPrompt(tab.linkedBrowser, {
+      modalType,
+      promptType: "promptUserAndPass",
+    });
     registerCleanupFunction(() => {
-      Services.wm.removeListener(listener);
       gBrowser.removeTab(tab);
     });
 
@@ -44,5 +22,10 @@ add_task(async function test() {
       tab.linkedBrowser,
       "http://example.com/browser/toolkit/components/passwordmgr/test/browser/authenticate.sjs"
     );
+
+    authPromptShown.then(dialog => {
+      is(gBrowser.selectedTab, tab, "Should have selected the new tab");
+      PromptTestUtils.handlePrompt(dialog, { buttonNumClick: 1 }); // Cancel
+    });
   });
 });
