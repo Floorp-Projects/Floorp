0d860681b79bda73691f8e3178d6ac0209b23203
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-05-28 21:29:25 +0300

    Backed out 6 changesets (bug 1602757, bug 1612063, bug 1569928) for wpt failure on operator-dictionary-spacing-001.html . CLOSED TREE
    Backed out changeset 0f9c7960f36e (bug 1569928)
    Backed out changeset 9456c9f960c9 (bug 1602757)
    Backed out changeset 8dc7e11dbb9c (bug 1602757)
    Backed out changeset 8bacf5ad656e (bug 1602757)
    Backed out changeset b15fc1090ac7 (bug 1612063)
    Backed out changeset bd19c329c6b7 (bug 1602757)

--- dom/ipc/ProcessPriorityManager.cpp
+++ dom/ipc/ProcessPriorityManager.cpp
@@ -808,8 +808,6 @@ void ParticularProcessPriorityManager::TabActivityChanged(
 void ParticularProcessPriorityManager::ShutDown() {
   MOZ_ASSERT(mContentParent);
 
-  LOGP("shutdown for %p (mContentParent %p)", this, mContentParent);
-
   UnregisterWakeLockObserver(this);
 
   if (mResetPriorityTimer) {
@@ -944,7 +942,6 @@ void ProcessPriorityManager::Init() {
 void ProcessPriorityManager::SetProcessPriority(ContentParent* aContentParent,
                                                 ProcessPriority aPriority) {
   MOZ_ASSERT(aContentParent);
-  MOZ_ASSERT(aContentParent->Pid() != -1);
 
   ProcessPriorityManagerImpl* singleton =
       ProcessPriorityManagerImpl::GetSingleton();
