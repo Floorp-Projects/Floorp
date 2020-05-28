5b8bcdf1004eb2b4497c04e32c4b376b34b0d237
Jan Varga <jan.varga@gmail.com>
2020-05-28 07:58:56 +0000

    Bug 1621916 - Convert test_unknownFiles.js to use quasi subtests; r=dom-workers-and-storage-reviewers,sg
    Depends on D75950
    
    Differential Revision: https://phabricator.services.mozilla.com/D75951

--- dom/quota/test/xpcshell/test_unknownFiles.js
+++ dom/quota/test/xpcshell/test_unknownFiles.js
@@ -8,77 +8,101 @@
  * and clearStoragesForPrincipal are able to ignore unknown files and
  * directories in the storage/default directory and its subdirectories.
  */
-
 async function testSteps() {
-  const exampleUrl = "http://example.com";
-  const unknownRepoFile = {
-    path: "storage/default/foo.bar",
-    dir: false,
-  };
+  const principal = getPrincipal("http://example.com");
 
-  const unknownOriginFile = {
-    path: "storage/default/http+++foobar.com/foo.bar",
-    dir: false,
-  };
+  async function testFunctionality(testFunction) {
+    const originDirectory = getRelativeFile(
+      "storage/default/http+++example.com"
+    );
 
-  const unknownOriginDirectory = {
-    path: "storage/default/http+++foobar.com/foo",
-    dir: true,
-  };
+    const unknownFiles = [
+      {
+        path: "storage/default/foo.bar",
+        dir: false,
+      },
 
-  function createFile(unknownFile) {
-    let file = getRelativeFile(unknownFile.path);
+      {
+        path: "storage/default/http+++foobar.com/foo.bar",
+        dir: false,
+      },
 
-    if (unknownFile.dir) {
-      file.create(Ci.nsIFile.DIRECTORY_TYPE, parseInt("0755", 8));
-    } else {
-      file.create(Ci.nsIFile.NORMAL_FILE_TYPE, parseInt("0644", 8));
+      {
+        path: "storage/default/http+++foobar.com/foo",
+        dir: true,
+      },
+    ];
+
+    for (const createOriginDirectory of [false, true]) {
+      info("Initializing storage");
+
+      let request = init();
+      await requestFinished(request);
+
+      if (createOriginDirectory) {
+        info("Creating origin directory");
+
+        originDirectory.create(Ci.nsIFile.DIRECTORY_TYPE, parseInt("0755", 8));
+      }
+
+      info("Creating unknown files");
+
+      for (const unknownFile of unknownFiles) {
+        const file = getRelativeFile(unknownFile.path);
+
+        if (unknownFile.dir) {
+          file.create(Ci.nsIFile.DIRECTORY_TYPE, parseInt("0755", 8));
+        } else {
+          file.create(Ci.nsIFile.NORMAL_FILE_TYPE, parseInt("0644", 8));
+        }
+      }
+
+      await testFunction(createOriginDirectory);
+
+      info("Clearing");
+
+      request = clear();
+      await requestFinished(request);
     }
-
-    return file;
   }
 
-  let request;
+  info("Testing initStorageAndOrigin functionality");
 
-  info("Initializing storage");
-
-  request = init();
-  await requestFinished(request);
-
-  for (let unknownFile of [
-    unknownRepoFile,
-    unknownOriginFile,
-    unknownOriginDirectory,
-  ]) {
-    info("Creating unknown file");
-
-    createFile(unknownFile);
-
+  await testFunctionality(async function(createdOriginDirectory) {
     info("Initializing origin");
 
-    const principal = getPrincipal(exampleUrl);
     request = initStorageAndOrigin(principal, "default");
     await requestFinished(request);
 
-    ok(request.result === true, "The origin directory was created");
+    if (createdOriginDirectory) {
+      ok(request.result === false, "The origin directory was not created");
+    } else {
+      ok(request.result === true, "The origin directory was created");
+    }
+  });
 
+  info("Testing getUsageForPrincipal functionality");
+
+  await testFunctionality(async function() {
     info("Getting origin usage");
 
     request = getOriginUsage(principal);
     await requestFinished(request);
 
-    ok(request.result, "The request result is not null");
-    ok(request.result.usage === 0, "The usage was 0");
-    ok(request.result.fileUsage === 0, "The fileUsage was 0");
+    ok(
+      request.result instanceof Ci.nsIQuotaOriginUsageResult,
+      "The result is nsIQuotaOriginUsageResult instance"
+    );
+    ok(request.result.usage === 0, "The usage is 0");
+    ok(request.result.fileUsage === 0, "The fileUsage is 0");
+  });
 
+  info("Testing clearStoragesForPrincipal functionality");
+
+  await testFunctionality(async function() {
     info("Clearing origin");
 
     request = clearOrigin(principal, "default");
     await requestFinished(request);
-  }
-
-  info("Clearing");
-
-  request = clear();
-  await requestFinished(request);
+  });
 }
