d3e63c27ff1c23b67d5c3e1815bf8d6d1f3f0ded
Kris Maglione <maglione.k@gmail.com>
2020-05-28 19:34:33 +0000

    Bug 1638153: Part 1 - Add topChromeWindow getter to CanonicalBrowsingContext. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D75431

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -94,6 +94,8 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   already_AddRefed<CanonicalBrowsingContext> GetParentCrossChromeBoundary();
 
+  Nullable<WindowProxyHolder> GetTopChromeWindow();
+
   nsISHistory* GetSessionHistory();
   UniquePtr<SessionHistoryInfoAndId> CreateSessionHistoryEntryForLoad(
       nsDocShellLoadState* aLoadState, nsIChannel* aChannel);
