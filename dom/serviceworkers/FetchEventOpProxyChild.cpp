f1cfc2ec8f829f4dec95d4b366efeb168309081d
Brindusan Cristian <cbrindusan@mozilla.com>
2020-05-28 23:31:26 +0300

    Backed out 1 changesets (bug 1622451) for assertion failures on UniquePtr.h. CLOSED TREE
    Backed out changeset fb8e753340e4 (bug 1622451)

--- dom/serviceworkers/FetchEventOpProxyChild.cpp
+++ dom/serviceworkers/FetchEventOpProxyChild.cpp
@@ -120,14 +120,8 @@ void FetchEventOpProxyChild::Initialize(
                  }
 
                  Unused << self->SendRespondWith(ipcArgs);
-
-                 if (ipcArgs.internalResponse().body()) {
-                   autoBodyStream->TakeValue();
-                 }
-
-                 if (ipcArgs.internalResponse().alternativeBody()) {
-                   autoAlternativeBodyStream->TakeValue();
-                 }
+                 autoBodyStream->TakeOptionalValue();
+                 autoAlternativeBodyStream->TakeOptionalValue();
                } else if (result.is<ResetInterceptionArgs>()) {
                  Unused << self->SendRespondWith(
                      result.extract<ResetInterceptionArgs>());
--- dom/serviceworkers/FetchEventOpProxyParent.cpp
+++ dom/serviceworkers/FetchEventOpProxyParent.cpp
@@ -9,18 +9,13 @@
 #include <utility>
 
 #include "nsCOMPtr.h"
-#include "nsContentUtils.h"
 #include "nsIInputStream.h"
 
 #include "mozilla/Assertions.h"
 #include "mozilla/DebugOnly.h"
-#include "mozilla/Result.h"
-#include "mozilla/ResultExtensions.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Unused.h"
 #include "mozilla/dom/FetchEventOpParent.h"
-#include "mozilla/dom/IPCBlobInputStreamStorage.h"
-#include "mozilla/dom/IPCBlobUtils.h"
 #include "mozilla/ipc/BackgroundParent.h"
 #include "mozilla/ipc/IPCStreamUtils.h"
 
@@ -32,25 +27,20 @@ namespace dom {
 
 namespace {
 
-nsresult MaybeDeserializeAndWrapForMainThread(
-    const Maybe<BodyStreamVariant>& aSource, int64_t aBodyStreamSize,
-    Maybe<BodyStreamVariant>& aSink, PBackgroundParent* aManager) {
-  if (aSource.isNothing()) {
-    return NS_OK;
+void MaybeDeserializeAndReserialize(const Maybe<IPCStream>& aDeserialize,
+                                    Maybe<IPCStream>& aReserialize,
+                                    UniquePtr<AutoIPCStream>& aAutoStream,
+                                    PBackgroundParent* aManager) {
+  nsCOMPtr<nsIInputStream> maybeDeserialized =
+      DeserializeIPCStream(aDeserialize);
+
+  if (!maybeDeserialized) {
+    return;
   }
 
-  nsCOMPtr<nsIInputStream> deserialized =
-      DeserializeIPCStream(aSource->get_ChildToParentStream().stream());
-
-  aSink = Some(ParentToParentStream());
-  auto& uuid = aSink->get_ParentToParentStream().uuid();
-
-  MOZ_TRY(nsContentUtils::GenerateUUIDInPlace(uuid));
-
-  IPCBlobInputStreamStorage::Get()->AddStream(deserialized, uuid,
-                                              aBodyStreamSize, 0);
-
-  return NS_OK;
+  aAutoStream.reset(new AutoIPCStream(aReserialize));
+  DebugOnly<bool> ok = aAutoStream->Serialize(maybeDeserialized, aManager);
+  MOZ_ASSERT(ok);
 }
 
 }  // anonymous namespace
@@ -74,30 +64,15 @@ nsresult MaybeDeserializeAndWrapForMainThread(
   ServiceWorkerFetchEventOpArgs copyArgs = aArgs;
   IPCInternalRequest& copyRequest = copyArgs.internalRequest();
 
-  if (copyRequest.body().ref().type() ==
-      BodyStreamVariant::TParentToParentStream) {
-    nsCOMPtr<nsIInputStream> stream;
-    auto streamLength = copyRequest.bodySize();
-    const auto& uuid =
-        copyRequest.body().ref().get_ParentToParentStream().uuid();
-    IPCBlobInputStreamStorage* storage = IPCBlobInputStreamStorage::Get();
+  PBackgroundParent* bgParent = aManager->Manager();
+  MOZ_ASSERT(bgParent);
 
-    storage->GetStream(uuid, 0, streamLength, getter_AddRefs(stream));
-    storage->ForgetStream(uuid);
-
-    MOZ_DIAGNOSTIC_ASSERT(stream);
-
-    PBackgroundParent* bgParent = aManager->Manager();
-    MOZ_ASSERT(bgParent);
-
-    copyRequest.body() = Some(ParentToChildStream());
-    MOZ_ALWAYS_SUCCEEDS(IPCBlobUtils::SerializeInputStream(
-        stream, streamLength,
-        copyRequest.body().ref().get_ParentToChildStream().actorParent(),
-        bgParent));
-  }
+  UniquePtr<AutoIPCStream> autoBodyStream = MakeUnique<AutoIPCStream>();
+  MaybeDeserializeAndReserialize(aArgs.internalRequest().body(),
+                                 copyRequest.body(), autoBodyStream, bgParent);
 
   Unused << aManager->SendPFetchEventOpProxyConstructor(actor, copyArgs);
+  autoBodyStream->TakeOptionalValue();
 }
 
 FetchEventOpProxyParent::~FetchEventOpProxyParent() {
@@ -150,14 +125,20 @@ mozilla::ipc::IPCResult FetchEventOpProxyParent::RecvRespondWith(
     PBackgroundParent* bgParent = manager->Manager();
     MOZ_ASSERT(bgParent);
 
-    MOZ_ALWAYS_SUCCEEDS(MaybeDeserializeAndWrapForMainThread(
-        originalResponse.body(), copyResponse.bodySize(), copyResponse.body(),
-        bgParent));
-    MOZ_ALWAYS_SUCCEEDS(MaybeDeserializeAndWrapForMainThread(
-        originalResponse.alternativeBody(), InternalResponse::UNKNOWN_BODY_SIZE,
-        copyResponse.alternativeBody(), bgParent));
+    UniquePtr<AutoIPCStream> autoBodyStream = MakeUnique<AutoIPCStream>();
+    UniquePtr<AutoIPCStream> autoAlternativeBodyStream =
+        MakeUnique<AutoIPCStream>();
+
+    MaybeDeserializeAndReserialize(originalResponse.body(), copyResponse.body(),
+                                   autoBodyStream, bgParent);
+    MaybeDeserializeAndReserialize(originalResponse.alternativeBody(),
+                                   copyResponse.alternativeBody(),
+                                   autoAlternativeBodyStream, bgParent);
 
     Unused << mReal->SendRespondWith(copyArgs);
+
+    autoBodyStream->TakeOptionalValue();
+    autoAlternativeBodyStream->TakeOptionalValue();
   } else {
     Unused << mReal->SendRespondWith(aResult);
   }
