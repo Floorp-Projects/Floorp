9bc323b746f11f280f1c9be2ea0a258013edc1df
Erik Nordin <nordzilla@mozilla.com>
2020-05-28 15:36:44 +0000

    Bug 117233 - Implement nsIPrinterEnumeratorX r=jwatt
    - Implement the nsPrinterEnumeratorX
    - Enable the contract @mozilla.org/gfx/printerenumerator;1 for macOS
    - Add test for default printer name.
    - Remove restrictions preventing some tests from running on macOS
    
    Differential Revision: https://phabricator.services.mozilla.com/D76356

--- widget/cocoa/nsDeviceContextSpecX.mm
+++ widget/cocoa/nsDeviceContextSpecX.mm
@@ -5,19 +5,26 @@
 
 #include "nsDeviceContextSpecX.h"
 
-#include "mozilla/gfx/PrintTargetCG.h"
+#import <Cocoa/Cocoa.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <unistd.h>
+
 #ifdef MOZ_ENABLE_SKIA_PDF
 #  include "mozilla/gfx/PrintTargetSkPDF.h"
 #endif
+#include "mozilla/gfx/PrintTargetCG.h"
+#include "mozilla/Logging.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/RefPtr.h"
+
+#include "nsCocoaUtils.h"
 #include "nsCRT.h"
 #include "nsDirectoryServiceDefs.h"
 #include "nsILocalFileMac.h"
-#include <unistd.h>
-
-#include "nsQueryObject.h"
 #include "nsPrintSettingsX.h"
+#include "nsQueryObject.h"
+#include "nsStringEnumerator.h"
+#include "prenv.h"
 
 // This must be the last include:
 #include "nsObjCExceptions.h"
@@ -30,6 +37,97 @@ using mozilla::gfx::PrintTargetCG;
 using mozilla::gfx::PrintTargetSkPDF;
 #endif
 using mozilla::gfx::SurfaceFormat;
+
+static LazyLogModule sDeviceContextSpecXLog("DeviceContextSpecX");
+// Macro to make lines shorter
+#define DO_PR_DEBUG_LOG(x) MOZ_LOG(sDeviceContextSpecXLog, mozilla::LogLevel::Debug, x)
+
+//----------------------------------------------------------------------
+// nsPrinterErnumeratorX
+
+NS_IMPL_ISUPPORTS(nsPrinterEnumeratorX, nsIPrinterEnumerator);
+
+NS_IMETHODIMP nsPrinterEnumeratorX::GetPrinterNameList(nsIStringEnumerator** aPrinterNameList) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
+
+  NS_ENSURE_ARG_POINTER(aPrinterNameList);
+  *aPrinterNameList = nullptr;
+
+  NSArray<NSString*>* printerNames = [NSPrinter printerNames];
+  size_t nameCount = [printerNames count];
+  nsTArray<nsString>* printerNameList = new nsTArray<nsString>(nameCount);
+
+  for (size_t i = 0; i < nameCount; ++i) {
+    NSString* name = [printerNames objectAtIndex:i];
+    nsAutoString nsName;
+    nsCocoaUtils::GetStringForNSString(name, nsName);
+    printerNameList->AppendElement(nsName);
+  }
+
+  // If there are no printers available after all checks, return an error
+  if (printerNameList->IsEmpty()) {
+    delete printerNameList;
+    return NS_ERROR_GFX_PRINTER_NO_PRINTER_AVAILABLE;
+  }
+
+  return NS_NewAdoptingStringEnumerator(aPrinterNameList, printerNameList);
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT;
+}
+
+NS_IMETHODIMP nsPrinterEnumeratorX::GetDefaultPrinterName(nsAString& aDefaultPrinterName) {
+  NS_OBJC_BEGIN_TRY_ABORT_BLOCK_NSRESULT;
+
+  DO_PR_DEBUG_LOG(("nsPrinterEnumeratorX::GetDefaultPrinterName()\n"));
+
+  aDefaultPrinterName.Truncate();
+  NSArray<NSString*>* printerNames = [NSPrinter printerNames];
+  if ([printerNames count] > 0) {
+    NSString* name = [printerNames objectAtIndex:0];
+    nsCocoaUtils::GetStringForNSString(name, aDefaultPrinterName);
+  }
+
+  DO_PR_DEBUG_LOG(("GetDefaultPrinterName(): default printer='%s'.\n",
+                   NS_ConvertUTF16toUTF8(aDefaultPrinterName).get()));
+  return NS_OK;
+
+  NS_OBJC_END_TRY_ABORT_BLOCK_NSRESULT;
+}
+
+NS_IMETHODIMP
+nsPrinterEnumeratorX::InitPrintSettingsFromPrinter(const nsAString& aPrinterName,
+                                                   nsIPrintSettings* aPrintSettings) {
+  DO_PR_DEBUG_LOG(("nsPrinterEnumeratorX::InitPrintSettingsFromPrinter()"));
+
+  NS_ENSURE_ARG_POINTER(aPrintSettings);
+
+  // Set a default file name.
+  nsAutoString filename;
+  nsresult rv = aPrintSettings->GetToFileName(filename);
+  if (NS_FAILED(rv) || filename.IsEmpty()) {
+    const char* path = PR_GetEnv("PWD");
+    if (!path) {
+      path = PR_GetEnv("HOME");
+    }
+
+    if (path) {
+      CopyUTF8toUTF16(MakeStringSpan(path), filename);
+      filename.AppendLiteral("/mozilla.pdf");
+    } else {
+      filename.AssignLiteral("mozilla.pdf");
+    }
+
+    DO_PR_DEBUG_LOG(("Setting default filename to '%s'\n", NS_ConvertUTF16toUTF8(filename).get()));
+    aPrintSettings->SetToFileName(filename);
+  }
+
+  aPrintSettings->SetIsInitializedFromPrinter(true);
+
+  return NS_OK;
+}
+
+//----------------------------------------------------------------------
+// nsDeviceContentSpecX
 
 nsDeviceContextSpecX::nsDeviceContextSpecX()
     : mPrintSession(NULL),
