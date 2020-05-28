4f44b0353af63bedc66212028f485de500f7afb5
Ricky Stewart <rstewart@mozilla.com>
2020-05-28 20:33:03 +0000

    Bug 1641242 - Handle difference in API of ImportError between py2 and py3 in artifact_commands.py r=catlee
    Differential Revision: https://phabricator.services.mozilla.com/D77337

--- python/mozbuild/mozbuild/artifact_commands.py
+++ python/mozbuild/mozbuild/artifact_commands.py
@@ -443,7 +443,11 @@ class PackageFrontend(MachCommandBase):
                 try:
                     unpack_file(local)
                 except ImportError as e:
-                    if e.name != "zstandard":
+                    # Need to do this branch while this code is still exercised
+                    # by Python 2.
+                    if six.PY3 and e.name != "zstandard":
+                        raise
+                    elif six.PY2 and e.message != 'No module named zstandard':
                         raise
                     self._ensure_zstd()
                     unpack_file(local)
