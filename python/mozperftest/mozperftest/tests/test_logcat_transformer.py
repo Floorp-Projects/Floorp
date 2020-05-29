bc39b608cd8c849185636b35f7b2f2b944d71d61
Gregory Mierzwinski <gmierz2@outlook.com>
2020-05-29 12:40:40 +0000

    Bug 1637994 - Add a transformer for extracting times/durations from logcat logs. r=acreskey,mozperftest-reviewers,tarek
    This patch adds a transformer that is capable of extracting times/entries or durations for logcat logs. Here, we also implement the ability for users to pass options to the transformers so that they can be more generic (through transformer-options). The logcat transformer makes use of this, and also shows how a custom function can be used within transformers. A new test file is added for the transformer, as well as a test in test_perfherder.py so we can test the full pipeline.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77094

---
+++ python/mozperftest/mozperftest/tests/test_logcat_transformer.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+import mozunit
+import pytest
+
+from mozperftest.metrics.exceptions import (
+    NotebookTransformOptionsError,
+    NotebookTransformError,
+)
+from mozperftest.metrics.notebook.transforms.logcattime import LogCatTimeTransformer
+from mozperftest.tests.support import HERE
+
+
+@pytest.fixture(scope="session", autouse=True)
+def tfm():
+    yield LogCatTimeTransformer()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def logcat_data(tfm):
+    data = tfm.open_data(str(HERE / "data" / "home_activity.txt"))
+    assert data
+    yield data
+
+
+def test_logcat_transform_two_regex(tfm, logcat_data):
+    restart = r".*Activity.*Manager.*START.*org\.mozilla\.fennec_aurora/org\.mozilla\.fenix\.HomeActivity.*"  # noqa
+    reend = r".*Displayed.*org\.mozilla\.fennec_aurora.*"
+    opts = {
+        "first-timestamp": restart,
+        "second-timestamp": reend,
+        "transform-subtest-name": "HANOOBish",
+    }
+
+    actual_result = tfm.transform(logcat_data, **opts)
+    expected_result = [
+        {
+            "data": [
+                {"value": 1782.0, "xaxis": 0},
+                {"value": 1375.0, "xaxis": 1},
+                {"value": 1497.0, "xaxis": 2},
+            ],
+            "subtest": "HANOOBish",
+        }
+    ]
+    assert actual_result == expected_result
+
+    # We should get the same results back from merge
+    # since we are working with only one file
+    merged = tfm.merge(actual_result)
+    assert merged == expected_result
+
+
+def test_logcat_transform_one_regex(tfm, logcat_data):
+    def processor(groups):
+        """Parses the time from a displayed time string into milliseconds."""
+        return (float(groups[0]) * 1000) + float(groups[1])
+
+    re_w_group = r".*Displayed.*org\.mozilla\.fennec_aurora.*\+([\d]+)s([\d]+)ms.*"
+    opts = {
+        "first-timestamp": re_w_group,
+        "processor": processor,
+        "transform-subtest-name": "TimeToDisplayed",
+    }
+
+    actual_result = tfm.transform(logcat_data, **opts)
+    expected_result = [
+        {
+            "data": [
+                {"value": 1743.0, "xaxis": 0},
+                {"value": 1325.0, "xaxis": 1},
+                {"value": 1462.0, "xaxis": 2},
+            ],
+            "subtest": "TimeToDisplayed",
+        }
+    ]
+    assert actual_result == expected_result
+
+
+def test_logcat_transform_no_processor(tfm, logcat_data):
+    re_w_group = r".*Displayed.*org\.mozilla\.fennec_aurora.*\+([\d]+)s([\d]+)ms.*"
+    opts = {
+        "first-timestamp": re_w_group,
+        "transform-subtest-name": "TimeToDisplayed",
+    }
+
+    actual_result = tfm.transform(logcat_data, **opts)
+    expected_result = [
+        {
+            "data": [
+                {"value": 1.0, "xaxis": 0},
+                {"value": 1.0, "xaxis": 1},
+                {"value": 1.0, "xaxis": 2},
+            ],
+            "subtest": "TimeToDisplayed",
+        }
+    ]
+    assert actual_result == expected_result
+
+
+def test_logcat_transform_no_groups(tfm, logcat_data):
+    re_w_group = r".*Displayed.*org\.mozilla\.fennec_aurora.*"
+    opts = {
+        "first-timestamp": re_w_group,
+        "transform-subtest-name": "TimeToDisplayed",
+    }
+
+    with pytest.raises(NotebookTransformOptionsError):
+        tfm.transform(logcat_data, **opts)
+
+
+def test_logcat_transform_too_many_groups(tfm, logcat_data):
+    restart = r".*Activity.*Manager.*START.*org\.mozilla\.fennec_aurora/org\.mozilla\.fenix\.HomeActivity.*"  # noqa
+    reend = r".*Displayed.*org\.mozilla\.fennec_aurora.*\+([\d]+)s([\d]+)ms.*"
+    opts = {
+        "first-timestamp": restart,
+        "second-timestamp": reend,
+        "transform-subtest-name": "HANOOBish",
+    }
+
+    with pytest.raises(NotebookTransformError):
+        tfm.transform(logcat_data, **opts)
+
+
+if __name__ == "__main__":
+    mozunit.main()
