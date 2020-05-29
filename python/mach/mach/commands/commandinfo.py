7f3fa0551ba928af41eed19a7163bba97f0694f1
Ricky Stewart <rstewart@mozilla.com>
2020-05-29 20:43:05 +0000

    Bug 1641991 - Remove deprecated mochitest mach commands r=ahal
    These commands were removed 5 years ago (with the exception of robocop, which was removed about a year ago). We're way past the point where anyone would glean useful info from keeping the stubs here with an error message.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77536

--- python/mach/mach/commands/commandinfo.py
+++ python/mach/mach/commands/commandinfo.py
@@ -21,10 +21,8 @@ class BuiltinCommands(object):
 
     @property
     def command_keys(self):
-        # NOTE 'REMOVED' is a function in testing/mochitest/mach_commands.py
         return (k for k, v in self.context.commands.command_handlers.items()
-                if not v.conditions
-                or getattr(v.conditions[0], '__name__', None) != 'REMOVED')
+                if not v.conditions)
 
     @Command('mach-commands', category='misc',
              description='List all mach commands.')
