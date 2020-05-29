081897162d9c0eb278baebdc30c9df108e8cc303
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-05-29 07:05:21 +0300

    Backed out changeset d2b10235d8ac (bug 1624909) for bustages on Pivot.h . CLOSED TREE

--- accessible/html/HTMLFormControlAccessible.cpp
+++ accessible/html/HTMLFormControlAccessible.cpp
@@ -70,12 +70,6 @@ uint64_t HTMLRadioButtonAccessible::NativeState() const {
 
 void HTMLRadioButtonAccessible::GetPositionAndSizeInternal(int32_t* aPosInSet,
                                                            int32_t* aSetSize) {
-  Unused << ComputeGroupAttributes(aPosInSet, aSetSize);
-}
-
-Relation HTMLRadioButtonAccessible::ComputeGroupAttributes(
-    int32_t* aPosInSet, int32_t* aSetSize) const {
-  Relation rel = Relation();
   int32_t namespaceId = mContent->NodeInfo()->NamespaceID();
   nsAutoString tagName;
   mContent->NodeInfo()->GetName(tagName);
@@ -93,7 +87,7 @@ Relation HTMLRadioButtonAccessible::ComputeGroupAttributes(
     inputElms = NS_GetContentList(formElm, namespaceId, tagName);
   else
     inputElms = NS_GetContentList(mContent->OwnerDoc(), namespaceId, tagName);
-  NS_ENSURE_TRUE(inputElms, rel);
+  NS_ENSURE_TRUE_VOID(inputElms);
 
   uint32_t inputCount = inputElms->Length(false);
 
@@ -109,23 +103,12 @@ Relation HTMLRadioButtonAccessible::ComputeGroupAttributes(
                                            name, eCaseMatters) &&
         mDoc->HasAccessible(inputElm)) {
       count++;
-      rel.AppendTarget(mDoc->GetAccessible(inputElm));
       if (inputElm == mContent) indexOf = count;
     }
   }
 
   *aPosInSet = indexOf;
   *aSetSize = count;
-  return rel;
-}
-
-Relation HTMLRadioButtonAccessible::RelationByType(RelationType aType) const {
-  if (aType == RelationType::MEMBER_OF) {
-    int32_t unusedPos, unusedSetSize;
-    return ComputeGroupAttributes(&unusedPos, &unusedSetSize);
-  }
-
-  return Accessible::RelationByType(aType);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
