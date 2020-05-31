2f5607b8d052b0bbefdf8cbd625c296720816b01
Emilio Cobos Álvarez <emilio@crisal.io>
2020-05-31 17:11:57 +0000

    Bug 1642221 - Rename SheetLoadData::mLoaderPrincipal to mTriggeringPrincipal. r=jwatt
    I'm about to introduce the concept of "Loader principal" (as in "the
    principal of the CSS loader"), and SheetLoadData already has an
    mLoaderPrincipal.
    
    However SheetLoadData's principal is just the triggering principal (the
    principal that initiated the load). So name it that with consistency
    with SheetInfo::mTriggeringPrincipal etc.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77613

--- uriloader/preload/PreloadHashKey.cpp
+++ uriloader/preload/PreloadHashKey.cpp
@@ -5,6 +5,7 @@
 #include "PreloadHashKey.h"
 
 #include "mozilla/dom/Element.h"  // StringToCORSMode
+#include "mozilla/css/SheetLoadData.h"
 #include "nsIPrincipal.h"
 #include "nsIReferrerInfo.h"
 
@@ -117,7 +118,7 @@ PreloadHashKey PreloadHashKey::CreateAsStyle(
 // static
 PreloadHashKey PreloadHashKey::CreateAsStyle(
     css::SheetLoadData& aSheetLoadData) {
-  return CreateAsStyle(aSheetLoadData.mURI, aSheetLoadData.mLoaderPrincipal,
+  return CreateAsStyle(aSheetLoadData.mURI, aSheetLoadData.mTriggeringPrincipal,
                        aSheetLoadData.ReferrerInfo(),
                        aSheetLoadData.mSheet->GetCORSMode(),
                        aSheetLoadData.mSheet->ParsingMode());
