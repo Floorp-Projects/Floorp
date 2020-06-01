bdb214c5afae3821ff7707e83e1d8e98820f0bf0
Cameron McCormack <cam@mcc.id.au>
2020-06-01 20:38:08 +0000

    Bug 1642262 - Report a one-based line number in UseCounters.conf error messages. r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D77626

--- dom/base/usecounters.py
+++ dom/base/usecounters.py
@@ -39,7 +39,7 @@ def read_conf(conf_filename):
                         'name': name,
                         'desc': desc }
                 continue
-            raise ValueError('error parsing %s at line %d' % (conf_filename, line_num))
+            raise ValueError('error parsing %s at line %d' % (conf_filename, line_num + 1))
 
     return parse_counters(stream)
 
