84342f69b4c79b335b9864a4f3a1323b0512f431
Emily McDonough <emcdonough@mozilla.com>
2020-06-01 19:37:06 +0000

    Bug 1642040 - Add VoidLinux support to mach bootstrap r=rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D77571

---
+++ python/mozboot/mozboot/void.py
@@ -0,0 +1,128 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import os
+import subprocess
+import sys
+
+from mozboot.base import BaseBootstrapper
+from mozboot.linux_common import LinuxBootstrapper
+
+
+class VoidBootstrapper(
+        LinuxBootstrapper,
+        BaseBootstrapper):
+
+    PACKAGES = [
+        'autoconf213',
+        'clang',
+        'make',
+        'mercurial',
+        'nodejs',
+        'unzip',
+        'zip',
+    ]
+
+    BROWSER_PACKAGES = [
+        'dbus-devel',
+        'dbus-glib-devel',
+        'gtk+3-devel',
+        'pulseaudio',
+        'pulseaudio-devel',
+        'libcurl-devel',
+        'libxcb-devel',
+        'libXt-devel',
+        'yasm',
+    ]
+
+    MOBILE_ANDROID_PACKAGES = [
+        'openjdk8',  # Android's `sdkmanager` requires Java 1.8 exactly.
+        'wget',  # For downloading the Android SDK and NDK.
+    ]
+
+    def __init__(self, version, dist_id, **kwargs):
+        BaseBootstrapper.__init__(self, **kwargs)
+
+        self.distro = "void"
+        self.version = version
+        self.dist_id = dist_id
+
+        self.packages = self.PACKAGES
+        self.browser_packages = self.BROWSER_PACKAGES
+        self.mobile_android_packages = self.MOBILE_ANDROID_PACKAGES
+
+        # Check if we need Python2 or Python3 pip.
+        if sys.version_info[0] == 3:
+            self.packages.append('python3-pip')
+        else:
+            self.packages.append('python-pip')
+
+    def run_as_root(self, command):
+        # VoidLinux doesn't support users sudo'ing most commands by default because of the group
+        # configuration.
+        if os.geteuid() != 0:
+            command = ['su', 'root', '-c', ' '.join(command)]
+
+        print('Executing as root:', subprocess.list2cmdline(command))
+
+        subprocess.check_call(command, stdin=sys.stdin)
+
+    def xbps_install(self, *packages):
+        command = ['xbps-install']
+        if self.no_interactive:
+            command.append('-y')
+        command.extend(packages)
+
+        self.run_as_root(command)
+
+    def xbps_update(self):
+        command = ['xbps-install', '-Su']
+        if self.no_interactive:
+            command.append('-y')
+
+        self.run_as_root(command)
+
+    def install_system_packages(self):
+        # Python 3 may not be present on all distros. Search for it and
+        # install if found.
+        self.xbps_install(*self.packages)
+
+    def install_browser_packages(self):
+        self.ensure_browser_packages()
+
+    def install_browser_artifact_mode_packages(self):
+        self.ensure_browser_packages(artifact_mode=True)
+
+    def install_mobile_android_packages(self):
+        self.ensure_mobile_android_packages()
+
+    def install_mobile_android_artifact_mode_packages(self):
+        self.ensure_mobile_android_packages(artifact_mode=True)
+
+    def ensure_browser_packages(self, artifact_mode=False):
+        self.xbps_install(*self.browser_packages)
+
+    def ensure_mobile_android_packages(self, artifact_mode=False):
+        # Multi-part process:
+        # 1. System packages.
+        # 2. Android SDK. Android NDK only if we are not in artifact mode. Android packages.
+        self.xbps_install(*self.mobile_android_packages)
+
+        # 2. Android pieces.
+        self.ensure_java()
+        from mozboot import android
+        android.ensure_android('linux', artifact_mode=artifact_mode,
+                               no_interactive=self.no_interactive)
+
+    def suggest_mobile_android_mozconfig(self, artifact_mode=False):
+        from mozboot import android
+        android.suggest_mozconfig('linux', artifact_mode=artifact_mode)
+
+    def suggest_mobile_android_artifact_mode_mozconfig(self):
+        self.suggest_mobile_android_mozconfig(artifact_mode=True)
+
+    def _update_package_manager(self):
+        self.xbps_update()
