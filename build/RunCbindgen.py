f886ef583b6a3c6550c098deb7e19bec0b2046e7
Ricky Stewart <rstewart@mozilla.com>
2020-06-01 15:50:29 +0000

    Bug 1642015 - Don't dump a raw bytestring to the terminal in RunCbindgen.py r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D77556

--- build/RunCbindgen.py
+++ build/RunCbindgen.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import buildconfig
 import mozpack.path as mozpath
 import os
+import six
 import subprocess
 import pytoml
 
@@ -39,6 +40,8 @@ def _generate(output, cbindgen_crate_path, metadata_crate_path,
     ], env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     stdout, stderr = p.communicate()
+    stdout = six.ensure_text(stdout)
+    stderr = six.ensure_text(stderr)
     if p.returncode != 0:
         print(stdout)
         print(stderr)
