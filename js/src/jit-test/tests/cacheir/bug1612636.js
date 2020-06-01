cd0b709444641c8e91bae21ac1691483b64fc356
Iain Ireland <iireland@mozilla.com>
2020-06-01 23:46:46 +0000

    Bug 1612636: Add test r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D61795

---
+++ js/src/jit-test/tests/cacheir/bug1612636.js
@@ -0,0 +1,10 @@
+var concat = String.prototype.concat;
+function f18(b83) {
+    var a11 = arguments;
+    if (b83)
+        f18(false);
+    g22 = {};
+    g22.apply = concat;
+    g22.apply(null, a11);
+}
+f18(true);
