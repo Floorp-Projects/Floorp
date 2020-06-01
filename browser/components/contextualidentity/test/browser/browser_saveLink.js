e2b687e0ce5cbeb00b00bd0a6acd38a84d182ebb
Andrea Marchesini <amarchesini@mozilla.com>
2020-06-01 10:29:34 +0000

    Bug 1638358 - Cookie Schemeful Same-Site - part 5 - schemeful comparison, r=mayhemer
    Differential Revision: https://phabricator.services.mozilla.com/D75629

--- browser/components/contextualidentity/test/browser/browser_saveLink.js
+++ browser/components/contextualidentity/test/browser/browser_saveLink.js
@@ -13,7 +13,13 @@ add_task(async function setup() {
 
   // make sure userContext is enabled.
   await SpecialPowers.pushPrefEnv({
-    set: [["privacy.userContext.enabled", true]],
+    set: [
+      ["privacy.userContext.enabled", true],
+      // This test does a redirect from https to http and it checks the
+      // cookies. This is incompatible with the cookie sameSite schemeful
+      // feature and we need to disable it.
+      ["network.cookie.sameSite.schemeful", false],
+    ],
   });
 });
 
