b71c369bee8ae25b44de467fad4ea6615098a450
Mirko Brodesser <mbrodesser@mozilla.com>
2020-06-02 09:05:57 +0000

    Bug 1635709: part 24.1) Factor `HandleDragSelecting` and `HandleMouseUpOrDown` out from `TableSelection::HandleSelection`. r=masayuki
    `HandleSelection` was too complex.
    
    The arguments of the new methods will be renamed in a separate commit to
    simplify reviewing and avoid mistakes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77253

--- layout/generic/nsFrameSelection.cpp
+++ layout/generic/nsFrameSelection.cpp
@@ -2256,9 +2256,7 @@ nsresult nsFrameSelection::TableSelection::HandleSelection(
     return NS_OK;
   }
 
-  nsresult result = NS_OK;
-
-  nsIContent* childContent =
+  RefPtr<nsIContent> childContent =
       aParentContent->GetChildAt_Deprecated(aContentOffset);
 
   // When doing table selection, always set the direction to next so
@@ -2270,14 +2268,27 @@ nsresult nsFrameSelection::TableSelection::HandleSelection(
   //  BeginBatchChanges() / EndBatchChanges()
   SelectionBatcher selectionBatcher(&aNormalSelection);
 
-  int32_t startRowIndex, startColIndex, curRowIndex, curColIndex;
   if (aDragState && mDragSelectingCells) {
-    // We are drag-selecting
-    if (aTarget != TableSelectionMode::Table) {
-      // If dragging in the same cell as last event, do nothing
-      if (mEndSelectedCell == childContent) {
-        return NS_OK;
-      }
+    return HandleDragSelecting(aTarget, childContent, aMouseEvent,
+                               aNormalSelection);
+  }
+
+  return HandleMouseUpOrDown(aTarget, aDragState, childContent, aParentContent,
+                             aContentOffset, aMouseEvent, aNormalSelection);
+}
+
+nsresult nsFrameSelection::TableSelection::HandleDragSelecting(
+    TableSelectionMode aTarget, nsIContent* childContent,
+    const WidgetMouseEvent* aMouseEvent, Selection& aNormalSelection) {
+  nsresult result = NS_OK;
+
+  int32_t startRowIndex, startColIndex, curRowIndex, curColIndex;
+  // We are drag-selecting
+  if (aTarget != TableSelectionMode::Table) {
+    // If dragging in the same cell as last event, do nothing
+    if (mEndSelectedCell == childContent) {
+      return NS_OK;
+    }
 
 #ifdef DEBUG_TABLE_SELECTION
       printf(
@@ -2364,12 +2375,18 @@ nsresult nsFrameSelection::TableSelection::HandleSelection(
         return SelectBlockOfCells(mStartSelectedCell, childContent,
                                   aNormalSelection);
       }
-    }
+  }
     // Do nothing if dragging in table, but outside a cell
     return NS_OK;
-  } else {
-    // Not dragging  -- mouse event is down or up
-    if (aDragState) {
+}
+
+nsresult nsFrameSelection::TableSelection::HandleMouseUpOrDown(
+    TableSelectionMode aTarget, bool aDragState, nsIContent* childContent,
+    nsINode* aParentContent, int32_t aContentOffset,
+    const WidgetMouseEvent* aMouseEvent, Selection& aNormalSelection) {
+  nsresult result = NS_OK;
+  // Not dragging  -- mouse event is down or up
+  if (aDragState) {
 #ifdef DEBUG_TABLE_SELECTION
       printf("HandleTableSelection: Mouse down event\n");
 #endif
@@ -2457,7 +2474,7 @@ nsresult nsFrameSelection::TableSelection::HandleSelection(
 
         return SelectRowOrColumn(childContent, aNormalSelection);
       }
-    } else {
+  } else {
 #ifdef DEBUG_TABLE_SELECTION
       printf(
           "HandleTableSelection: Mouse UP event. "
@@ -2582,7 +2599,6 @@ nsresult nsFrameSelection::TableSelection::HandleSelection(
         }
         mUnselectCellOnMouseUp = nullptr;
       }
-    }
   }
   return result;
 }
