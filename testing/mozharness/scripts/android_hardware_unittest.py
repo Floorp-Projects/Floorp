9d3645ef54e3cd878dd3c2f694dea54afc4e600e
Geoff Brown <gbrown@mozilla.com>
2020-06-02 22:56:14 +0000

    Bug 1640758 - Use multiple errorsummary and raw logs for test-verify; r=jmaher
    In all mozharness scripts running test-verify (or per-test coverage), use distinct
    file names for errorsummary and raw logs. This usually means that the command needs
    to be built inside the per-test loop.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77746

--- testing/mozharness/scripts/android_hardware_unittest.py
+++ testing/mozharness/scripts/android_hardware_unittest.py
@@ -193,11 +193,9 @@ class AndroidHardwareTest(TestingMixin, BaseScript, MozbaseMixin,
             ),
         ]
 
-        raw_log_file = os.path.join(dirs['abs_blob_upload_dir'],
-                                    '%s_raw.log' % self.test_suite)
+        raw_log_file, error_summary_file = self.get_indexed_logs(dirs['abs_blob_upload_dir'],
+                                                                 self.test_suite)
 
-        error_summary_file = os.path.join(dirs['abs_blob_upload_dir'],
-                                          '%s_errorsummary.log' % self.test_suite)
         str_format_values = {
             'device_serial': self.device_serial,
             'remote_webserver': c['remote_webserver'],
@@ -363,8 +361,6 @@ class AndroidHardwareTest(TestingMixin, BaseScript, MozbaseMixin,
         for (per_test_suite, suite) in suites:
             self.test_suite = suite
 
-            cmd = self._build_command()
-
             try:
                 cwd = self._query_tests_dir()
             except Exception:
@@ -388,6 +384,7 @@ class AndroidHardwareTest(TestingMixin, BaseScript, MozbaseMixin,
                     # suite categories loops also.
                     return
 
+                cmd = self._build_command()
                 final_cmd = copy.copy(cmd)
                 if len(per_test_args) > 0:
                     # in per-test mode, remove any chunk arguments from command
