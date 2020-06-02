f83013ca9be4285ec6dd09d97597ab4f5d3a9b23
Ingrid Buh twigle_ingrid <Ingrid Buh twigle_ingrid@yahoo.com>
2020-06-02 12:15:14 +0000

    Bug 1640877 - Fix intermittent failures for browser/base/content/test/about/browser_aboutNetError_csp_iframe.js. r=johannh,prathiksha, a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D77059

--- browser/base/content/test/about/browser_aboutNetError_xfo_iframe.js
+++ browser/base/content/test/about/browser_aboutNetError_xfo_iframe.js
@@ -62,13 +62,21 @@ async function setupPage(htmlPageName, blockedPage) {
 
   is(strictCookie.sameSite, 2, "The cookie is a same site strict cookie");
 
-  // Opening the page containing the iframe
-  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, iFramePage);
+  // Opening the page that contains the iframe
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser);
   let browser = tab.linkedBrowser;
+  let browserLoaded = BrowserTestUtils.browserLoaded(
+    browser,
+    true,
+    blockedPage,
+    true
+  );
 
-  await SpecialPowers.spawn(browser, [blockedPage], async function(
-    xfoBlockedPage
-  ) {
+  BrowserTestUtils.loadURI(browser, iFramePage);
+  await browserLoaded;
+  info("The error page has loaded!");
+
+  await SpecialPowers.spawn(browser, [], async function() {
     let iframe = content.document.getElementById("theIframe");
 
     await ContentTaskUtils.waitForCondition(() =>
@@ -77,7 +85,7 @@ async function setupPage(htmlPageName, blockedPage) {
   });
 
   let frameContext = browser.browsingContext.children[0];
-  let loaded = BrowserTestUtils.waitForNewTab(gBrowser, null, true);
+  let newTabLoaded = BrowserTestUtils.waitForNewTab(gBrowser, null, true);
 
   // In the iframe, we see the correct error page and click on the button
   // to open the blocked page in a new window/tab
@@ -99,8 +107,11 @@ async function setupPage(htmlPageName, blockedPage) {
     // We click on the button
     await EventUtils.synthesizeMouseAtCenter(button, {}, content);
   });
+  info("Button was clicked!");
+
   // We wait for the new tab to load
-  await loaded;
+  await newTabLoaded;
+  info("The new tab has loaded!");
 
   let iframePageTab = tab;
   return {
