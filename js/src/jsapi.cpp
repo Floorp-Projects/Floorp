d5aba8feeb0c7fd23d56fff2ff676b469a6280f3
André Bargull <andre.bargull@gmail.com>
2020-06-02 12:29:59 +0000

    Bug 1635839 - Part 4: Add an option for private class fields, disabled by default r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D74105

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -3473,6 +3473,7 @@ void JS::TransitiveCompileOptions::copyPODTransitiveOptions(
   hasIntroductionInfo = rhs.hasIntroductionInfo;
   hideScriptFromDebugger = rhs.hideScriptFromDebugger;
   nonSyntacticScope = rhs.nonSyntacticScope;
+  privateClassFields = rhs.privateClassFields;
 };
 
 void JS::ReadOnlyCompileOptions::copyPODNonTransitiveOptions(
@@ -3564,6 +3565,8 @@ JS::CompileOptions::CompileOptions(JSContext* cx)
   }
   throwOnAsmJSValidationFailureOption =
       cx->options().throwOnAsmJSValidationFailure();
+  privateClassFields =
+      cx->realm()->creationOptions().getPrivateClassFieldsEnabled();
 
   sourcePragmas_ = cx->options().sourcePragmas();
 
