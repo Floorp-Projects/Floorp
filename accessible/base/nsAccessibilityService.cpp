df599db51df6fb94d130ba4a3ac729de37c19c5d
James Teh <jteh@mozilla.com>
2020-06-02 06:59:31 +0000

    Bug 1151648: Construct accessible objects for SVG elements which have title or desc. r=MarcoZ
    According to SVG Accessibility API Mappings, an SVG element which has title or desc element must be exposed.
    https://w3c.github.io/svg-aam/#include_elements
    Previously, we didn't expose <g> elements at all even if they had a title/desc, and we unconditionally exposed some other SVG elements even when they didn't.
    
    Original patch by Takeshi Kurosawa.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77763

--- accessible/base/nsAccessibilityService.cpp
+++ accessible/base/nsAccessibilityService.cpp
@@ -148,6 +148,20 @@ static bool MustBeAccessible(nsIContent* aContent, DocAccessible* aDocument) {
 }
 
 /**
+ * Return true if the SVG element should be accessible
+ */
+static bool MustSVGElementBeAccessible(nsIContent* aContent) {
+  // https://w3c.github.io/svg-aam/#include_elements
+  for (nsIContent* childElm = aContent->GetFirstChild(); childElm;
+       childElm = childElm->GetNextSibling()) {
+    if (childElm->IsAnyOfSVGElements(nsGkAtoms::title, nsGkAtoms::desc)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/**
  * Used by XULMap.h to map both menupopup and popup elements
  */
 #ifdef MOZ_XUL
@@ -1140,7 +1154,7 @@ Accessible* nsAccessibilityService::CreateAccessible(nsINode* aNode,
   if (!newAcc) {
     if (content->IsSVGElement()) {
       SVGGeometryFrame* geometryFrame = do_QueryFrame(frame);
-      if (geometryFrame) {
+      if (geometryFrame && MustSVGElementBeAccessible(content)) {
         // A graphic elements: rect, circle, ellipse, line, path, polygon,
         // polyline and image. A 'use' and 'text' graphic elements require
         // special support.
@@ -1149,6 +1163,9 @@ Accessible* nsAccessibilityService::CreateAccessible(nsINode* aNode,
         newAcc = new HyperTextAccessibleWrap(content->AsElement(), document);
       } else if (content->IsSVGElement(nsGkAtoms::svg)) {
         newAcc = new EnumRoleAccessible<roles::DIAGRAM>(content, document);
+      } else if (content->IsSVGElement(nsGkAtoms::g) &&
+                 MustSVGElementBeAccessible(content)) {
+        newAcc = new EnumRoleAccessible<roles::GROUPING>(content, document);
       }
 
     } else if (content->IsMathMLElement()) {
