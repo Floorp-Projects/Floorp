eba56c6de99fd34de12695d1053d40372c2f0f53
Jonathan Watt <jwatt@jwatt.org>
2020-06-02 18:42:26 +0000

    Bug 1642328. Marionette's print.printToFile takes a MozBrowser, not an nsIFrameLoader. r=jgraham,marionette-reviewers,whimboo
    Differential Revision: https://phabricator.services.mozilla.com/D77666

--- testing/marionette/print.js
+++ testing/marionette/print.js
@@ -96,7 +96,7 @@ function getPrintSettings(settings, filePath) {
   return printSettings;
 }
 
-print.printToFile = async function(frameLoader, outerWindowID, settings) {
+print.printToFile = async function(browser, outerWindowID, settings) {
   // Create a unique filename for the temporary PDF file
   const basePath = OS.Path.join(OS.Constants.Path.tmpDir, "marionette.pdf");
   const { file, path: filePath } = await OS.File.openUnique(basePath);
@@ -133,7 +133,7 @@ print.printToFile = async function(frameLoader, outerWindowID, settings) {
       QueryInterface: ChromeUtils.generateQI([Ci.nsIWebProgressListener]),
     };
 
-    frameLoader.print(outerWindowID, printSettings, printProgressListener);
+    browser.print(outerWindowID, printSettings, printProgressListener);
   });
 
   logger.debug(`PDF output written to ${filePath}`);
