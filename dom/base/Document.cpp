e34c0ee64c3de30a79dd3aa03d73f19dfb0523ae
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-03 09:44:41 +0000

    Bug 1634602 - Minor follow-up cleanups. a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D78020

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -16321,8 +16321,9 @@ void Document::RemoveToplevelLoadingDocument(Document* aDoc) {
 }
 
 StylePrefersColorScheme Document::PrefersColorScheme(
-    Document::IgnoreRFP aIgnoreRFP /* = IgnoreRFP::No */) const {
-  if (aIgnoreRFP == IgnoreRFP::No && nsContentUtils::ShouldResistFingerprinting(this)) {
+    IgnoreRFP aIgnoreRFP) const {
+  if (aIgnoreRFP == IgnoreRFP::No &&
+      nsContentUtils::ShouldResistFingerprinting(this)) {
     return StylePrefersColorScheme::Light;
   }
 
