5e9f32e76acf110d8c5232a5cd309871dd907f38
Tom Prince <mozilla@hocat.ca>
2020-06-03 02:35:14 +0000

    Bug 1641971: Allow making references to a task itself in the task definition; r=Callek
    This is to support linking to the a specific task in taskcluster.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77529

--- taskcluster/taskgraph/util/schema.py
+++ taskcluster/taskgraph/util/schema.py
@@ -145,6 +145,8 @@ def check_schema(schema):
         def check_identifier(path, k):
             if k in (text_type, text_type, voluptuous.Extra):
                 pass
+            elif isinstance(k, voluptuous.NotIn):
+                pass
             elif isinstance(k, text_type):
                 if not identifier_re.match(k) and not whitelisted(path):
                     raise RuntimeError(
@@ -152,7 +154,7 @@ def check_schema(schema):
                         'not {!r} @ {}'.format(k, path))
             elif isinstance(k, (voluptuous.Optional, voluptuous.Required)):
                 check_identifier(path, k.schema)
-            elif isinstance(k, voluptuous.Any):
+            elif isinstance(k, (voluptuous.Any, voluptuous.All)):
                 for v in k.validators:
                     check_identifier(path, v)
             elif not whitelisted(path):
