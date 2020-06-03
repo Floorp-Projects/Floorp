9f8d53a50f3930c710d90a19eefd9d1c9ad62afe
André Bargull <andre.bargull@gmail.com>
2020-06-03 09:00:34 +0000

    Bug 1642683: Retrieve 'resolve' property before the iterable in the Promise combinators. r=arai
    Apply the updates from <https://github.com/tc39/ecma262/pull/1912>.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77838

--- js/src/tests/non262/Promise/iterator-close.js
+++ js/src/tests/non262/Promise/iterator-close.js
@@ -64,7 +64,7 @@ class MyPromiseStaticResolveGetterThrows extends Promise {
 test(MyPromiseStaticResolveGetterThrows, ["all", "race"], {
     nextVal: { value: Promise.resolve(1), done: false },
     rejectReason: "static resolve getter throws",
-    closed: true,
+    closed: false,
 });
 
 class MyPromiseStaticResolveThrows extends Promise {
