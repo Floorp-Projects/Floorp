1127f9a3984e788da70258b3d57691632a47c522
Edwin Takahashi <egao@mozilla.com>
2020-06-03 21:59:46 +0000

    Bug 1632429 - bump mozhttpd version to 0.7.1 r=gbrown
    Due to minor changes in D78159 and D77965 I figured it would be a good idea to release the bugfix version bump to pypi, despite our intention to discontinue using mozhttpd (Bug 1428709).
    
    Differential Revision: https://phabricator.services.mozilla.com/D78163

--- testing/mozbase/mozhttpd/setup.py
+++ testing/mozbase/mozhttpd/setup.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 
 from setuptools import setup
 
-PACKAGE_VERSION = '0.7'
+PACKAGE_VERSION = '0.7.1'
 deps = ['moznetwork >= 0.24', 'mozinfo >= 1.0.0', 'six >= 1.10.0']
 
 setup(name='mozhttpd',
