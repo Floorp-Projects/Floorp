e8755cb9a67184bf7fb573419e62352db73831a4
André Bargull <andre.bargull@gmail.com>
2020-06-03 09:01:32 +0000

    Bug 1639591 - Part 3: Re-import test262. r=yulia
    Depends on D77823
    
    Differential Revision: https://phabricator.services.mozilla.com/D77824

--- js/src/tests/test262/language/expressions/logical-assignment/lgcl-nullish-assignment-operator-non-simple-lhs.js
+++ js/src/tests/test262/language/expressions/logical-assignment/lgcl-nullish-assignment-operator-non-simple-lhs.js
@@ -1,4 +1,4 @@
-// |reftest| skip-if(release_or_beta) error:SyntaxError -- logical-assignment-operators is not released yet
+// |reftest| error:SyntaxError
 // Copyright (c) 2020 Ecma International.  All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
