3016bebb341ef57ee42c8329a081705a373fce14
Majid Valipour <majidvp@chromium.org>
2020-06-03 04:25:58 +0000

    Bug 1642327 [wpt PR 23889] - [scroll-timeline] Correctly handle unresolvable cases, a=testonly
    Automatic update from web-platform-tests
    [scroll-timeline] Correctly handle unresolvable cases
    
    For these cases we cannot calculate a meaningful scroll offset:
    
    1. When target is not a descendant of timeline's source.
    2. When target has no layout box.
    
    The current draft spec [1] asks for these situations to result into
    unresolved scroll offset which keeps timeline inactive.
    
    [1] https://github.com/w3c/csswg-drafts/pull/5124
    
    TEST: wpt/scroll-animations/element-based-offset-unresolved.html
    BUG: 1023375
    
    Change-Id: Iec616444dda8dcdd6649e250aa993b439c00885e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222884
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774144}
    
    --
    
    wpt-commits: 52575c9d7c3139cab33780e114b5cf9237a47157
    wpt-pr: 23889

--- testing/web-platform/tests/scroll-animations/testcommon.js
+++ testing/web-platform/tests/scroll-animations/testcommon.js
@@ -7,6 +7,19 @@ function createScroller(test) {
   return scroller;
 }
 
+function createScrollerWithStartAndEnd(test, orientationClass = 'vertical') {
+  var scroller = createDiv(test);
+  scroller.innerHTML =
+    `<div class='contents'>
+        <div id='start'></div>
+        <div id='end'></div>
+      </div>`;
+  scroller.classList.add('scroller');
+  scroller.classList.add(orientationClass);
+
+  return scroller;
+}
+
 function createScrollTimeline(test, options) {
   options = options || {
     scrollSource: createScroller(test),
@@ -33,3 +46,4 @@ function createScrollLinkedAnimation(test, timeline) {
   return new Animation(
     new KeyframeEffect(createDiv(test), KEYFRAMES, DURATION), timeline);
 }
+
