6e12cd5073597a9c659de095feb3216d593d72b8
Logan Smyth <loganfsmyth@gmail.com>
2020-06-03 17:21:11 +0000

    Bug 1642371 - Part 3: Remove the sourceMapURLService dependency from function reps. r=jlast
    Differential Revision: https://phabricator.services.mozilla.com/D77739

--- devtools/client/debugger/packages/devtools-reps/src/reps/function.js
+++ devtools/client/debugger/packages/devtools-reps/src/reps/function.js
@@ -21,16 +21,10 @@ const IGNORED_SOURCE_URLS = ["debugger eval code"];
 FunctionRep.propTypes = {
   object: PropTypes.object.isRequired,
   onViewSourceInDebugger: PropTypes.func,
-  sourceMapURLService: PropTypes.object,
 };
 
 function FunctionRep(props) {
-  const {
-    object: grip,
-    onViewSourceInDebugger,
-    recordTelemetryEvent,
-    sourceMapURLService,
-  } = props;
+  const { object: grip, onViewSourceInDebugger, recordTelemetryEvent } = props;
 
   let jumpToDefinitionButton;
   if (
@@ -51,11 +45,7 @@ function FunctionRep(props) {
           recordTelemetryEvent("jump_to_definition");
         }
 
-        const sourceLocation = await getSourceLocation(
-          grip.location,
-          sourceMapURLService
-        );
-        onViewSourceInDebugger(sourceLocation);
+        onViewSourceInDebugger(grip.location);
       },
     });
   }
@@ -228,26 +218,7 @@ function supportsObject(grip, noGrip = false) {
   return type == "Function";
 }
 
-async function getSourceLocation(location, sourceMapURLService) {
-  if (!sourceMapURLService) {
-    return location;
-  }
-  try {
-    const originalLocation = await sourceMapURLService.originalPositionFor(
-      location.url,
-      location.line,
-      location.column
-    );
-    if (originalLocation) {
-      const { sourceUrl, line, column } = originalLocation;
-      return { url: sourceUrl, line, column };
-    }
-  } catch (e) {}
-  return location;
-}
-
 // Exports from this module
-
 module.exports = {
   rep: wrapRender(FunctionRep),
   supportsObject,
