f34e0ecafaf682818408b9bf027cf3c7c891d0ba
Iain Ireland <iireland@mozilla.com>
2020-06-04 14:35:04 +0000

    Bug 1642493: Clean up remaining references to ENABLE_NEW_REGEXP and old irregexp r=mgaudet
    I kept a running list of code that would be dead once we removed the old engine. This is what was on that list.
    
    Note: The regexp_parse tests used a testing function that parsed a regexp and returned an AST to compare against the expected AST. They are being deleted because the tests are fragile in the face of upstream changes, not particularly useful, and a lot of work to convert.
    
    Depends on D77727
    
    Differential Revision: https://phabricator.services.mozilla.com/D77728

--- js/src/jit-test/tests/regexp_parse/Empty.js
+++
@@ -1,4 +0,0 @@
-load(libdir + "regexp_parse.js");
-
-test("", all_flags,
-     Empty());
--- js/src/vm/RegExpObject.h
+++ js/src/vm/RegExpObject.h
@@ -45,23 +45,6 @@ namespace frontend {
 class TokenStreamAnyChars;
 }
 
-// Temporary definitions until irregexp is updated from upstream.
-namespace irregexp {
-constexpr size_t JS_HOWMANY(size_t x, size_t y) { return (x + y - 1) / y; }
-
-constexpr size_t JS_ROUNDUP(size_t x, size_t y) { return JS_HOWMANY(x, y) * y; }
-
-template <class T>
-static constexpr inline T Min(T t1, T t2) {
-  return t1 < t2 ? t1 : t2;
-}
-
-template <class T>
-static constexpr inline T Max(T t1, T t2) {
-  return t1 > t2 ? t1 : t2;
-}
-}  // namespace irregexp
-
 extern RegExpObject* RegExpAlloc(JSContext* cx, NewObjectKind newKind,
                                  HandleObject proto = nullptr);
 
