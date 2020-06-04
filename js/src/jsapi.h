5e8e6a00f22bff2638575290b0327532e448d2bb
Ted Campbell <tcampbell@mozilla.com>
2020-06-04 15:36:11 +0000

    Bug 1599634 - Move the JS::WasmModule definition to own header r=bbouvier
    Also add `JS::WasmModule::createObjectForAsmJS` which is similar to
    `createObject` but does not set the WasmModule prototype. Also mark these
    methods as const so they work with the `SharedModule` definition.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78088

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -2871,30 +2871,6 @@ extern JS_PUBLIC_API StackFormat GetStackFormat(JSContext* cx);
 namespace JS {
 
 /**
- * The WasmModule interface allows the embedding to hold a reference to the
- * underying C++ implementation of a JS WebAssembly.Module object for purposes
- * of efficient postMessage() and (de)serialization from a random thread.
- *
- * In particular, this allows postMessage() of a WebAssembly.Module:
- * GetWasmModule() is called when making a structured clone of a payload
- * containing a WebAssembly.Module object. The structured clone buffer holds a
- * refcount of the JS::WasmModule until createObject() is called in the target
- * agent's JSContext. The new WebAssembly.Module object continues to hold the
- * JS::WasmModule and thus the final reference of a JS::WasmModule may be
- * dropped from any thread and so the virtual destructor (and all internal
- * methods of the C++ module) must be thread-safe.
- */
-
-struct WasmModule : js::AtomicRefCounted<WasmModule> {
-  virtual ~WasmModule() = default;
-  virtual JSObject* createObject(JSContext* cx) = 0;
-};
-
-extern JS_PUBLIC_API bool IsWasmModuleObject(HandleObject obj);
-
-extern JS_PUBLIC_API RefPtr<WasmModule> GetWasmModule(HandleObject obj);
-
-/**
  * Attempt to disable Wasm's usage of reserving a large virtual memory
  * allocation to avoid bounds checking overhead. This must be called before any
  * Wasm module or memory is created in this process, or else this function will
