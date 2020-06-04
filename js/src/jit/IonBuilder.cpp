837b01cad43e958400cb23e2d191e0ac92e55bb4
André Bargull <andre.bargull@gmail.com>
2020-06-04 13:28:45 +0000

    Bug 1642067: Use Baseline inspector to decide if strings can occur in binary arithmetic expressions. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D78018

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -3692,11 +3692,6 @@ AbortReasonOr<Ok> IonBuilder::binaryArithTrySpecializedOnBaselineInspector(
   // Try to emit a specialized binary instruction speculating the
   // type using the baseline caches.
 
-  // Anything complex - strings, symbols, and objects - are not specialized
-  if (!SimpleArithOperand(left) || !SimpleArithOperand(right)) {
-    return Ok();
-  }
-
   MIRType specialization = inspector->expectedBinaryArithSpecialization(pc);
   if (specialization == MIRType::None) {
     return Ok();
@@ -3929,11 +3924,6 @@ AbortReasonOr<Ok> IonBuilder::unaryArithTrySpecializedOnBaselineInspector(
 
   // Try to emit a specialized binary instruction speculating the
   // type using the baseline caches.
-
-  // Anything complex - strings, symbols, and objects - are not specialized
-  if (!SimpleArithOperand(value)) {
-    return Ok();
-  }
 
   MIRType specialization = inspector->expectedBinaryArithSpecialization(pc);
   if (specialization == MIRType::None) {
