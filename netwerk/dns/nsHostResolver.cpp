11f2b7bec56404aade6118f039332ca5296fd3ae
Valentin Gosu <valentin.gosu@gmail.com>
2020-06-04 11:12:19 +0000

    Bug 1637512 - IPv6 TRR requests don't fallback to DNS properly r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D78237

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -1559,7 +1559,7 @@ nsresult nsHostResolver::NameLookup(nsHostRecord* rec) {
 
   if (rec->mEffectiveTRRMode == nsIRequest::TRR_DISABLED_MODE ||
       (rec->mEffectiveTRRMode == nsIRequest::TRR_FIRST_MODE &&
-       (rec->flags & RES_DISABLE_TRR || serviceNotReady) && NS_FAILED(rv))) {
+       (rec->flags & RES_DISABLE_TRR || serviceNotReady || NS_FAILED(rv)))) {
     if (!rec->IsAddrRecord()) {
       return rv;
     }
