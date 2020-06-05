8230af21cedb7769b1bd58698ca41ee19814e411
Kershaw Chang <kershaw@mozilla.com>
2020-06-05 13:27:35 +0000

    Bug 1641496 - P8: Introduce PNativeDNSResolverOverride.ipdl r=valentin
    A straightforward patch. Since DNS resolution is moved to socket process, we have to add another ipdl to make `nsINativeDNSResolverOverride` work.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78483

---
+++ netwerk/dns/NativeDNSResolverOverrideChild.cpp
@@ -0,0 +1,41 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 ts=8 et tw=80 : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "NativeDNSResolverOverrideChild.h"
+#include "GetAddrInfo.h"
+
+namespace mozilla {
+namespace net {
+
+NativeDNSResolverOverrideChild::NativeDNSResolverOverrideChild() {
+  mOverrideService = NativeDNSResolverOverride::GetSingleton();
+}
+
+mozilla::ipc::IPCResult NativeDNSResolverOverrideChild::RecvAddIPOverride(
+    const nsCString& aHost, const nsCString& aIPLiteral) {
+  Unused << mOverrideService->AddIPOverride(aHost, aIPLiteral);
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult NativeDNSResolverOverrideChild::RecvSetCnameOverride(
+    const nsCString& aHost, const nsCString& aCNAME) {
+  Unused << mOverrideService->SetCnameOverride(aHost, aCNAME);
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult NativeDNSResolverOverrideChild::RecvClearHostOverride(
+    const nsCString& aHost) {
+  Unused << mOverrideService->ClearHostOverride(aHost);
+  return IPC_OK();
+}
+
+mozilla::ipc::IPCResult NativeDNSResolverOverrideChild::RecvClearOverrides() {
+  Unused << mOverrideService->ClearOverrides();
+  return IPC_OK();
+}
+
+}  // namespace net
+}  // namespace mozilla
---
+++ netwerk/dns/NativeDNSResolverOverrideChild.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set sw=2 ts=8 et tw=80 : */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_net_NativeDNSResolverOverrideChild_h
+#define mozilla_net_NativeDNSResolverOverrideChild_h
+
+#include "mozilla/net/PNativeDNSResolverOverrideChild.h"
+#include "nsINativeDNSResolverOverride.h"
+
+namespace mozilla {
+namespace net {
+
+class NativeDNSResolverOverrideChild : public PNativeDNSResolverOverrideChild {
+ public:
+  NS_INLINE_DECL_REFCOUNTING(NativeDNSResolverOverrideChild, override)
+
+  NativeDNSResolverOverrideChild();
+
+  mozilla::ipc::IPCResult RecvAddIPOverride(const nsCString& aHost,
+                                            const nsCString& aIPLiteral);
+  mozilla::ipc::IPCResult RecvSetCnameOverride(const nsCString& aHost,
+                                               const nsCString& aCNAME);
+  mozilla::ipc::IPCResult RecvClearHostOverride(const nsCString& aHost);
+  mozilla::ipc::IPCResult RecvClearOverrides();
+
+ private:
+  virtual ~NativeDNSResolverOverrideChild() = default;
+
+  nsCOMPtr<nsINativeDNSResolverOverride> mOverrideService;
+};
+
+}  // namespace net
+}  // namespace mozilla
+
+#endif  // mozilla_net_NativeDNSResolverOverrideChild_h
