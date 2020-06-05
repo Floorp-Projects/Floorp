3d0ccb2000a32ad04f7e9e9ad351675792cfb1df
nchevobbe <nchevobbe@mozilla.com>
2020-06-05 14:53:33 +0000

    Bug 1639165 - Don't log messages forwarded from content processes when multiprocess browser toolbox is enabled. r=Honza.
    We were getting duplicated nsIScriptError in the Browser Toolbox console and the Browser Console
    when the multiprocess pref is enabled, because error from content processes are forwarded to
    the main process.
    The previous patch of this queue added a isForwardedFromContentProcess flag to nsIScriptError,
    that we use in this patch to _not_ handle forwarded messages in multiprocess browser console
    and browser toolbox console.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78231

--- devtools/client/webconsole/test/node/fixtures/stubs/cssMessage.js
+++ devtools/client/webconsole/test/node/fixtures/stubs/cssMessage.js
@@ -38,7 +38,8 @@ rawPackets.set(`Unknown property ‘such-unknown-property’.  Declaration dropp
     "notes": null,
     "chromeContext": false,
     "cssSelectors": "p",
-    "isPromiseRejection": false
+    "isPromiseRejection": false,
+    "isForwardedFromContentProcess": false
   },
   "resourceType": "error-message"
 });
@@ -63,7 +64,8 @@ rawPackets.set(`Error in parsing value for ‘padding-top’.  Declaration dropp
     "notes": null,
     "chromeContext": false,
     "cssSelectors": "p",
-    "isPromiseRejection": false
+    "isPromiseRejection": false,
+    "isForwardedFromContentProcess": false
   },
   "resourceType": "error-message"
 });
