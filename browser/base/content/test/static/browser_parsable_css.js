5e27c1bd6b914e0b7c315923c2557518ad1c6b27
Cameron McCormack <cam@mcc.id.au>
2020-06-05 00:39:50 +0000

    Bug 1642261 - Part 2: Add telemetry for -moz-appearance usage. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D77637

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -35,7 +35,7 @@ let whitelist = [
     isFromDevTools: false,
   },
   {
-    sourceName: /\b(html|mathml|ua)\.css$/i,
+    sourceName: /\b(html|mathml|ua|forms|svg)\.css$/i,
     errorMessage: /Unknown property.*-moz-/i,
     isFromDevTools: false,
   },
