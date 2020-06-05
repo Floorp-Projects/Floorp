d9df44ffe5e77c07562c90a99a04babffdf73df3
Yoshi Cheng-Hao Huang <allstars.chh@gmail.com>
2020-06-05 19:08:49 +0000

    Bug 1642154 : Add JSLinearString to MapTypeToTraceKind. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D78063

--- js/public/GCTypeMacros.h
+++ js/public/GCTypeMacros.h
@@ -28,10 +28,11 @@ class JS_PUBLIC_API Value;
 
 // Expand the given macro D for each public GC pointer.
 #define JS_FOR_EACH_PUBLIC_GC_POINTER_TYPE(D) \
-  D(JS::Symbol*)                              \
   D(JS::BigInt*)                              \
+  D(JS::Symbol*)                              \
   D(JSAtom*)                                  \
   D(JSFunction*)                              \
+  D(JSLinearString*)                          \
   D(JSObject*)                                \
   D(JSScript*)                                \
   D(JSString*)
