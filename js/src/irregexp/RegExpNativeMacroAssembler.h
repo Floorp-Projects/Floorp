222f1b72671c7720e0fec8e0da3f4c148f6e949c
Iain Ireland <iireland@mozilla.com>
2020-06-06 00:57:42 +0000

    Bug 1643137: Use unicode flag in CheckNotBackReference r=mgaudet
    This patch uses the unicode flag to decide which helper function to call for two-byte ignoreCase non-unicode backreferences.
    
    It also fixes the (previously unused) helper to do the right thing. Compare RegExpMacroAssembler::CaseInsensitiveCompareNonUnicode in `irregexp/imported/regexp-macro-assembler.cc`, from the previous patch.
    
    RegExpCaseFolding is only defined when we build with ICU. The differences here only occur in corner cases, so in non-intl builds, CaseInsensitiveCompareNonUnicode can do the same thing as CaseInsensitiveCompareUnicode.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78559

--- js/src/irregexp/RegExpNativeMacroAssembler.h
+++ js/src/irregexp/RegExpNativeMacroAssembler.h
@@ -79,7 +79,7 @@ class SMRegExpMacroAssembler final : public NativeRegExpMacroAssembler {
   virtual void CheckNotBackReference(int start_reg, bool read_backward,
                                      Label* on_no_match);
   virtual void CheckNotBackReferenceIgnoreCase(int start_reg,
-                                               bool read_backward,
+                                               bool read_backward, bool unicode,
                                                Label* on_no_match);
 
   virtual void LoadCurrentCharacterImpl(int cp_offset, Label* on_end_of_input,
@@ -129,7 +129,8 @@ class SMRegExpMacroAssembler final : public NativeRegExpMacroAssembler {
   void CheckCharacterInRangeImpl(uc16 from, uc16 to, Label* on_cond,
                                  js::jit::Assembler::Condition cond);
   void CheckNotBackReferenceImpl(int start_reg, bool read_backward,
-                                 Label* on_no_match, bool ignore_case);
+                                 bool unicode, Label* on_no_match,
+                                 bool ignore_case);
 
   void LoadCurrentCharacterUnchecked(int cp_offset, int characters);
 
@@ -147,12 +148,12 @@ class SMRegExpMacroAssembler final : public NativeRegExpMacroAssembler {
 
   static bool GrowBacktrackStack(RegExpStack* regexp_stack);
 
-  static uint32_t CaseInsensitiveCompareStrings(const char16_t* substring1,
+  static uint32_t CaseInsensitiveCompareNonUnicode(const char16_t* substring1,
+                                                   const char16_t* substring2,
+                                                   size_t byteLength);
+  static uint32_t CaseInsensitiveCompareUnicode(const char16_t* substring1,
                                                 const char16_t* substring2,
                                                 size_t byteLength);
-  static uint32_t CaseInsensitiveCompareUCStrings(const char16_t* substring1,
-                                                  const char16_t* substring2,
-                                                  size_t byteLength);
 
   inline int char_size() { return static_cast<int>(mode_); }
   inline js::jit::Scale factor() {
