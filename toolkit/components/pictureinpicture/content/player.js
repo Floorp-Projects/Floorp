5622ff32da56188f05460dec4d30361e64b566ce
Coroiu Cristina <ccoroiu@mozilla.com>
2020-06-06 19:13:06 +0300

    Backed out 6 changesets (bug 1641546) for permafailing browser-chrome at browser_devices_get_user_media_unprompted_access_tear_off_tab.js
    Backed out changeset 2652e1c084a7 (bug 1641546)
    Backed out changeset 72a62a455c47 (bug 1641546)
    Backed out changeset 4746c37be3e6 (bug 1641546)
    Backed out changeset a2b64d290c0b (bug 1641546)
    Backed out changeset 2dc2a430c2e7 (bug 1641546)
    Backed out changeset 041cd14e3893 (bug 1641546)

--- toolkit/components/pictureinpicture/content/player.js
+++ toolkit/components/pictureinpicture/content/player.js
@@ -154,13 +154,6 @@ let Player = {
     });
 
     this.computeAndSetMinimumSize(window.outerWidth, window.outerHeight);
-
-    // alwaysontop windows are not focused by default, so we have to do it
-    // ourselves. We use requestAnimationFrame since we have to wait until the
-    // window is visible before it can focus.
-    window.requestAnimationFrame(() => {
-      window.focus();
-    });
   },
 
   uninit() {
