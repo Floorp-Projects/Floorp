c4c6cd3a304bf5caaa35bb2bd9a96fe438c5a05a
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-06-08 02:01:41 +0300

    Backed out changeset c324ff9bf17e (bug 1643504) for breaking Docker Images. CLOSED TREE

--- taskcluster/docker/recipes/prepare_openjdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x
-
-# Debian 10 doesn't have openjdk-8, so add the Debian 9 repository, which contains it.
-if grep -q ^10\\. /etc/debian_version; then
-	sed s/buster/stretch/ /etc/apt/sources.list | tee /etc/apt/sources.list.d/stretch.list
-	apt-get update
-fi
