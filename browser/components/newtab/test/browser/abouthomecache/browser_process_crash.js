cbf3bf7d43731786534035adfc853cb0f1a8dc96
Mike Conley <mconley@mozilla.com>
2020-06-08 15:56:25 +0000

    Bug 1622088 - Part 2: Add some mochitest-browser tests for the about:home startup cache. r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D71793

---
+++ browser/components/newtab/test/browser/abouthomecache/browser_process_crash.js
@@ -0,0 +1,41 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Test that if the "privileged about content process" crashes, that it
+ * drops its internal reference to the "privileged about content process"
+ * process manager, and that a subsequent restart of that process type
+ * results in a new cached document load. Also tests that crashing of
+ * any other content process type doesn't clear the process manager
+ * reference.
+ */
+add_task(async function test_bump_version() {
+  await BrowserTestUtils.withNewTab("about:home", async browser => {
+    await simulateRestart(browser);
+    let origProcManager = AboutHomeStartupCache._procManager;
+
+    await BrowserTestUtils.crashFrame(browser);
+    Assert.notEqual(
+      origProcManager,
+      AboutHomeStartupCache._procManager,
+      "Should have dropped the reference to the crashed process"
+    );
+  });
+
+  let latestProcManager = AboutHomeStartupCache._procManager;
+
+  await BrowserTestUtils.withNewTab("about:home", async browser => {
+    await ensureCachedAboutHome(browser);
+  });
+
+  await BrowserTestUtils.withNewTab("http://example.com", async browser => {
+    await BrowserTestUtils.crashFrame(browser);
+    Assert.equal(
+      latestProcManager,
+      AboutHomeStartupCache._procManager,
+      "Should still have the reference to the privileged about process"
+    );
+  });
+});
