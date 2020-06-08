daa377d0ba7867831871938566335ea2076f7a0e
alwu <alwu@mozilla.com>
2020-06-08 18:50:45 +0000

    Bug 1640998 - part2 : use `ContentMediaAgent` to update media session's related information. r=chunmin
    This patch will
    - `ContentMediaAgent` as the only class to propagate the information to chrome process
    
    The advantage of doing so are
    - to group all methods involving IPC in one place, that would be easier to see what information would be propagated to chrome process
    
    Differential Revision: https://phabricator.services.mozilla.com/D77197

--- dom/media/mediasession/MediaSession.cpp
+++ dom/media/mediasession/MediaSession.cpp
@@ -4,10 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "mozilla/dom/BrowsingContext.h"
+#include "mozilla/dom/ContentMediaController.h"
 #include "mozilla/dom/MediaSession.h"
 #include "mozilla/EnumeratedArrayCycleCollection.h"
-
-#include "MediaSessionUtils.h"
 
 namespace mozilla {
 namespace dom {
@@ -51,15 +51,7 @@ void MediaSession::SetPlaybackState(
   RefPtr<BrowsingContext> currentBC = GetParentObject()->GetBrowsingContext();
   MOZ_ASSERT(currentBC,
              "Update session playback state after context destroyed!");
-  if (XRE_IsContentProcess()) {
-    ContentChild* contentChild = ContentChild::GetSingleton();
-    Unused << contentChild->SendNotifyMediaSessionPlaybackStateChanged(
-        currentBC, mDeclaredPlaybackState);
-    return;
-  }
-  // This would only happen when we disable e10s.
-  if (RefPtr<IMediaInfoUpdater> updater =
-          currentBC->Canonical()->GetMediaController()) {
+  if (RefPtr<IMediaInfoUpdater> updater = ContentMediaAgent::Get(currentBC)) {
     updater->SetDeclaredPlaybackState(currentBC->Id(), mDeclaredPlaybackState);
   }
 }
@@ -178,26 +170,27 @@ void MediaSession::Shutdown() {
 void MediaSession::NotifyMediaSessionStatus(SessionStatus aState) {
   RefPtr<BrowsingContext> currentBC = GetParentObject()->GetBrowsingContext();
   MOZ_ASSERT(currentBC, "Update session status after context destroyed!");
-  NotfiyMediaSessionCreationOrDeconstruction(currentBC,
-                                             aState == SessionStatus::eCreated);
+
+  RefPtr<IMediaInfoUpdater> updater = ContentMediaAgent::Get(currentBC);
+  if (!updater) {
+    return;
+  }
+  if (aState == SessionStatus::eCreated) {
+    updater->NotifySessionCreated(currentBC->Id());
+  } else {
+    updater->NotifySessionDestroyed(currentBC->Id());
+  }
 }
 
 void MediaSession::NotifyMetadataUpdated() {
   RefPtr<BrowsingContext> currentBC = GetParentObject()->GetBrowsingContext();
   MOZ_ASSERT(currentBC, "Update session metadata after context destroyed!");
+
   Maybe<MediaMetadataBase> metadata;
   if (GetMetadata()) {
     metadata.emplace(*(GetMetadata()->AsMetadataBase()));
   }
-
-  if (XRE_IsContentProcess()) {
-    ContentChild* contentChild = ContentChild::GetSingleton();
-    Unused << contentChild->SendNotifyUpdateMediaMetadata(currentBC, metadata);
-    return;
-  }
-  // This would only happen when we disable e10s.
-  if (RefPtr<IMediaInfoUpdater> updater =
-          currentBC->Canonical()->GetMediaController()) {
+  if (RefPtr<IMediaInfoUpdater> updater = ContentMediaAgent::Get(currentBC)) {
     updater->UpdateMetadata(currentBC->Id(), metadata);
   }
 }
