0f08cf6f68a83b93c1b866d4ea4decf1301fd77a
Noemi Erli <nerli@mozilla.com>
2020-06-09 20:55:58 +0300

    Backed out changeset 49f533a95f64 (bug 1644458) for causing failures in registration-updateviacache.https.html

--- dom/serviceworkers/ServiceWorkerPrivateImpl.cpp
+++ dom/serviceworkers/ServiceWorkerPrivateImpl.cpp
@@ -33,7 +33,6 @@
 #include "mozilla/ScopeExit.h"
 #include "mozilla/Services.h"
 #include "mozilla/StaticPrefs_dom.h"
-#include "mozilla/StoragePrincipalHelper.h"
 #include "mozilla/Unused.h"
 #include "mozilla/dom/ClientIPCTypes.h"
 #include "mozilla/dom/DOMTypes.h"
@@ -46,7 +45,6 @@
 #include "mozilla/dom/ServiceWorkerBinding.h"
 #include "mozilla/ipc/BackgroundChild.h"
 #include "mozilla/ipc/IPCStreamUtils.h"
-#include "mozilla/net/CookieJarSettings.h"
 
 namespace mozilla {
 
@@ -130,6 +128,7 @@ nsresult ServiceWorkerPrivateImpl::Initialize() {
 
   PrincipalInfo principalInfo;
   rv = PrincipalToPrincipalInfo(principal, &principalInfo);
+
   if (NS_WARN_IF(NS_FAILED(rv))) {
     return rv;
   }
@@ -148,27 +147,8 @@ nsresult ServiceWorkerPrivateImpl::Initialize() {
   }
 
   nsCOMPtr<nsICookieJarSettings> cookieJarSettings =
-      net::CookieJarSettings::Create();
+      mozilla::net::CookieJarSettings::Create();
   MOZ_ASSERT(cookieJarSettings);
-
-  net::CookieJarSettings::Cast(cookieJarSettings)->SetPartitionKey(uri);
-
-  net::CookieJarSettingsArgs cjsData;
-  net::CookieJarSettings::Cast(cookieJarSettings)->Serialize(cjsData);
-
-  nsCOMPtr<nsIPrincipal> partitionedPrincipal;
-  rv = StoragePrincipalHelper::CreatePartitionedPrincipalForServiceWorker(
-      principal, cookieJarSettings, getter_AddRefs(partitionedPrincipal));
-  if (NS_WARN_IF(NS_FAILED(rv))) {
-    return rv;
-  }
-
-  PrincipalInfo partitionedPrincipalInfo;
-  rv =
-      PrincipalToPrincipalInfo(partitionedPrincipal, &partitionedPrincipalInfo);
-  if (NS_WARN_IF(NS_FAILED(rv))) {
-    return rv;
-  }
 
   StorageAccess storageAccess =
       StorageAllowedForServiceWorker(principal, cookieJarSettings);
@@ -190,13 +170,17 @@ nsresult ServiceWorkerPrivateImpl::Initialize() {
       NS_ConvertUTF8toUTF16(mOuter->mInfo->ScriptSpec()), baseScriptURL,
       baseScriptURL, /* name */ VoidString(),
       /* loading principal */ principalInfo, principalInfo,
-      partitionedPrincipalInfo,
+
+      // partitionedPrincipalInfo for ServiceWorkers is equal to principalInfo
+      // because, at the moment, ServiceWorkers are not exposed in partitioned
+      // contexts.
+      principalInfo,
       /* useRegularPrincipal */ true,
 
       // ServiceWorkers run as first-party, no storage-access permission needed.
       /* hasStorageAccessPermissionGranted */ false,
 
-      cjsData, domain,
+      domain,
       /* isSecureContext */ true,
       /* clientInfo*/ Nothing(),
 
