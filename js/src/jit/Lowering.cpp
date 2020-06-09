c8aa884bcff2fc8c0f2afb205935d3ffff0b051b
André Bargull <andre.bargull@gmail.com>
2020-06-09 14:08:17 +0000

    Bug 1564942 - Part 4: Lower MPow to a series of shift-instructions when the base operand is a power of two. r=jandem
    In-tree users of `Math.pow` show that the function is often called with the base operand equal to
    two. This case can easily be optimised to a series of shift-instructions for any power of two. For
    now this optimisation is only taken for 2^i with i in {1..8} to avoid generating too many
    consecutive shift-instructions. 2^8 = 256 was chosen as the limit, because it is the maximum power
    of two base operand for `Math.pow` used in-tree.
    
    Differential Revision: https://phabricator.services.mozilla.com/D37587

--- js/src/jit/Lowering.cpp
+++ js/src/jit/Lowering.cpp
@@ -8,6 +8,7 @@
 
 #include "mozilla/DebugOnly.h"
 #include "mozilla/EndianUtils.h"
+#include "mozilla/MathAlgorithms.h"
 
 #include <type_traits>
 
@@ -1531,6 +1532,16 @@ void LIRGenerator::visitPow(MPow* ins) {
   if (ins->type() == MIRType::Int32) {
     MOZ_ASSERT(input->type() == MIRType::Int32);
     MOZ_ASSERT(power->type() == MIRType::Int32);
+
+    if (input->isConstant()) {
+      // Restrict this optimization to |base <= 256| to avoid generating too
+      // many consecutive shift instructions.
+      int32_t base = input->toConstant()->toInt32();
+      if (2 <= base && base <= 256 && mozilla::IsPowerOfTwo(uint32_t(base))) {
+        lowerPowOfTwoI(ins);
+        return;
+      }
+    }
 
     auto* lir = new (alloc())
         LPowII(useRegister(input), useRegister(power), temp(), temp());
