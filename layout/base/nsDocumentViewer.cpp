6b86e22f72cb419c67529a314f77b56bd442bb9b
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-09 13:21:54 +0000

    Bug 1643656 - Remove prefers-color-scheme: no-preference. r=gl,remote-protocol-reviewers,hiro,whimboo
    It was removed from the spec. Bug 1643934 updates the WPT tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78834

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -2739,8 +2739,6 @@ nsDocumentViewer::EmulatePrefersColorScheme(PrefersColorScheme aScheme) {
         return Some(StylePrefersColorScheme::Light);
       case PREFERS_COLOR_SCHEME_DARK:
         return Some(StylePrefersColorScheme::Dark);
-      case PREFERS_COLOR_SCHEME_NO_PREFERENCE:
-        return Some(StylePrefersColorScheme::NoPreference);
       case PREFERS_COLOR_SCHEME_NONE:
         return Nothing();
       default:
