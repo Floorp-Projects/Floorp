50edeaa4658afd78face5032e86de929cd862019
Rail Aliiev <rail@mozilla.com>
2020-06-09 03:31:56 +0000

    Bug 1642868 - Update verify tests check ja-JP-mac on all platforms now.  a=jcristau
    Beta78 only variant of D78844
    
    Differential Revision: https://phabricator.services.mozilla.com/D78915

--- python/mozrelease/mozrelease/l10n.py
+++ python/mozrelease/mozrelease/l10n.py
@@ -4,18 +4,16 @@
 
 from __future__ import absolute_import
 
-from .platforms import shippedLocales2ftp
-
 
 def getPlatformLocales(shipped_locales, platform):
     platform_locales = []
     for line in shipped_locales.splitlines():
-        entry = line.strip().split()
-        locale = entry[0]
-        if len(entry) > 1:
-            for sl_platform in entry[1:]:
-                if platform in shippedLocales2ftp(sl_platform):
-                    platform_locales.append(locale)
-        else:
-            platform_locales.append(locale)
+        locale = line.strip().split()[0]
+        # ja-JP-mac locale is a MacOS only locale
+        if locale == "ja-JP-mac" and not platform.startswith("mac"):
+            continue
+        # Skip the "ja" locale on MacOS
+        if locale == "ja" and platform.startswith("mac"):
+            continue
+        platform_locales.append(locale)
     return platform_locales
