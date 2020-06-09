7efc7c78cb41e1b69f3552b16257c9e4140b79d0
Rail Aliiev <rail@mozilla.com>
2020-06-09 03:31:56 +0000

    Bug 1642868 - Update verify tests check ja-JP-mac on all platforms now.  a=jcristau
    Beta78 only variant of D78844
    
    Differential Revision: https://phabricator.services.mozilla.com/D78915

--- python/mozrelease/mozrelease/platforms.py
+++ python/mozrelease/mozrelease/platforms.py
@@ -4,7 +4,6 @@
 
 from __future__ import absolute_import
 
-# ftp -> update platform map
 update_platform_map = {
     "android": ["Android_arm-eabi-gcc3"],
     "android-api-11": ["Android_arm-eabi-gcc3"],
@@ -52,16 +51,6 @@ def ftp2updatePlatforms(platform):
 
 def ftp2shippedLocales(platform):
     return sl_platform_map.get(platform, platform)
-
-
-def shippedLocales2ftp(platform):
-    matches = []
-    try:
-        [matches.append(
-            k) for k, v in sl_platform_map.iteritems() if v == platform][0]
-        return matches
-    except IndexError:
-        return [platform]
 
 
 def ftp2infoFile(platform):
