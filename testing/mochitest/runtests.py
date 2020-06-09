6861e58e106f99a5f9b473d34e0cd7a8829f7803
Greg Tatum <gtatum@mozilla.com>
2020-06-09 04:32:19 +0000

    Bug 1644114 - Only symbolicate mochitests profiles when using --profiler; r=gbrown
    This commit fixes an issue where the test harness can't find the profile to symbolicate.
    MOZ_PROFILER_SHUTDOWN can take a relative address, which is relative to the CWD of the
    Firefox binary. Rather than try to make this work, this patch instead outputs a message
    suggesting that the user use the official flags. I did this because it was an easier
    patch, but also I'm not sure what the user-intent is behind doing shutdown profiling
    on their own.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78805

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -2744,8 +2744,17 @@ toolbar#nav-bar {
             profiler_logger = get_proxy_logger("profiler")
             profiler_logger.info("Shutdown performance profiling was enabled")
             profiler_logger.info("Profile saved locally to: %s" % profile_path)
-            symbolicate_profile_json(profile_path, options.topobjdir)
-            view_gecko_profile_from_mochitest(profile_path, options, profiler_logger)
+
+            if options.profilerSaveOnly or options.profiler:
+                # Only do the extra work of symbolicating and viewing the profile if
+                # officially requested through a command line flag. The MOZ_PROFILER_*
+                # flags can be set by a user.
+                symbolicate_profile_json(profile_path, options.topobjdir)
+                view_gecko_profile_from_mochitest(profile_path, options, profiler_logger)
+            else:
+                profiler_logger.info("The profiler was enabled outside of the mochitests. "
+                                     "Use --profiler instead of MOZ_PROFILER_SHUTDOWN to "
+                                     "symbolicate and open the profile automatically.")
 
             # Clean up the temporary file if it exists.
             if self.profiler_tempdir:
