0a007c403de719c1d0cfa17e23a7003d2d9cae4e
Noemi Erli <nerli@mozilla.com>
2020-06-09 20:59:26 +0300

    Backed out 4 changesets (bug 1621960) for causing Android bustages CLOSED TREE
    Backed out changeset f36a95234a5f (bug 1621960)
    Backed out changeset 0e4661c9061d (bug 1621960)
    Backed out changeset 3e2cbe4dd777 (bug 1621960)
    Backed out changeset d7eb101e5df7 (bug 1621960)

--- taskcluster/scripts/tester/test-linux.sh
+++ taskcluster/scripts/tester/test-linux.sh
@@ -207,7 +207,7 @@ fi
 
 # Use |mach python| if a source checkout exists so in-tree packages are
 # available.
-[[ -x "${GECKO_PATH}/mach" ]] && python="python2.7 ${GECKO_PATH}/mach python" || python="python2.7"
+[[ -x "${GECKO_PATH}/mach" ]] && python="${GECKO_PATH}/mach python" || python="python2.7"
 
 # Save the computed mozharness command to a binary which is useful for
 # interactive mode.
