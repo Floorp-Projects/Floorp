c2cd38443cc816482a75bdf7bc5e43ed63bead8a
Pavel Yatsuk <pavely@chromium.org>
2020-06-10 11:36:21 +0000

    Bug 1644570 [wpt PR 24071] - Revert "webauthn: implement getPublicKey() and friends.", a=testonly
    Automatic update from web-platform-tests
    Revert "webauthn: implement getPublicKey() and friends."
    
    This reverts commit c5d236776cd30849a65ca626e51cd19e651da7c6.
    
    Reason for revert: I suspect that this CL causes Fido2CredentialRequestTest failures on android-pie-x86-rel:
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel
    
    Here is a snippet of callstack:
    java.lang.AssertionError: expected:<11> but was:<0>
    	at org.junit.Assert.fail(Assert.java:88)
    	at org.junit.Assert.failNotEquals(Assert.java:834)
    	at org.junit.Assert.assertEquals(Assert.java:118)
    	at org.junit.Assert.assertEquals(Assert.java:144)
    	at org.chromium.chrome.browser.webauth.Fido2CredentialRequestTest.testMakeCredential_attestationIndirect(Fido2CredentialRequestTest.java:921)
    
    Here is sample failure:
    https://00e9e64bac4dbda9c3b8a07931c85e48f4420a03929ffbcaf5-apidata.googleusercontent.com/download/storage/v1/b/chromium-result-details/o/html%2Fchrome_public_test_apk_android-pie-x86-rel_990_2020_06_09_T16_33_45-UTC?qk=AD5uMEvQvMK8K2ZyV-tHIDwdTtLEnQK-X1Mw0ohWLQeOpgogj0sVgm_7y9e5CdF-FMV6ObBmwjpVxD6Jh8_7VTv4z2FpOutuS8Gd6qQH5OxikfytS8cKFb_SRCCQXIgWq45BwD7DediOcKTF-UZea_JAoFuHO0ljO0PRqnkFYSoIHcPONuqaszXChp63b5o4VAKYsofAHDhpHru_hiAf6xUyfK91I8cr-2sdHjQU6Ks4XQIIWVnq2PSAiysd6hNeq51qWPEaX7HS8hCwWIpe55Xq9hIWLOtab3MY1WLLxM3UuWMY8IDCKfcvnrTiQMcRyeD7Iii33jHhC8-rDGttfDEl39kpFnWynDC-C0TmPFoCJVkl7mHxz0uCNlQJ3I0Rt_F2DTqA96qIKrpwEltNs96-0j1VgSOJCjU3s4od6h7juoIxpH5HThyg7G-fB1-MWzv7mQfAE5ZBilodU0CKMsFrnN_5Kf4thAbDj-RJUiUNpy0A9swLOhMLjEsfxQeDRldVvy9OVUNvcakhJziml5iA7LWoDB58E3eGTyUi-dA5aOLN3KWLR7T452j4MwnCHC5vTRNxxGxCkemgQQDkl1fHJas1u6GUcY4_ncvhIuGvLsfg8uqWpzFuau4CFamGC2USpyVpNIeNhpcf2w-lYzG1rkHjHWYgUKSvhoaItY3oeRFjnkYMgZrDsy8QmneuVVGTPd4nzoWWaCZbaG9ZaF80xRu9YhYnTUXI7GqQF1rlst09Mqu8IvDXo9dgYzodInIA6p3H7azZgnvCfzYUxL4QVrvskyjTQCITmz-o2W6CPUzBqWJhWLRTpZYcVSV7z8zjj1cFwDQPBKpJzQDRrtC1sZCM0WrmQ7oSNeU-AH3uBaNWeGBF2rkY8-9gx8dlr38DGQvXoKZVWHWi61jVnQhac27ctKBLKQ&isca=1
    
    Original change's description:
    > webauthn: implement getPublicKey() and friends.
    >
    > This change implements the getPublicKey(), getPublicKeyAlgorithm(), and
    > getAuthenticatorData() functions described in the editor's draft of
    > WebAuthn level two[1].
    >
    > [1] https://w3c.github.io/webauthn/#sctn-public-key-easy
    >
    > BUG=1083301
    >
    > Change-Id: I3a03279d5239a9f8df50a78f2166f30d01d38d3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204087
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Alex Russell <slightlyoff@chromium.org>
    > Reviewed-by: Jared Saul <jsaul@google.com>
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    > Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    > Commit-Queue: Adam Langley <agl@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776517}
    
    TBR=kenrb@chromium.org,agl@chromium.org,slightlyoff@chromium.org,nsatragno@chromium.org,agrieve@chromium.org,jsaul@google.com,martinkr@google.com
    
    Change-Id: I7429b5b7ecd7fa38c4e1ec2905b80a15e7c64216
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1083301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238749
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776637}
    
    --
    
    wpt-commits: cc7ec638fd43e6839dda3ea175b66b6d071918fb
    wpt-pr: 24071

--- testing/web-platform/tests/webauthn/resources/utils.js
+++ testing/web-platform/tests/webauthn/resources/utils.js
@@ -17,17 +17,6 @@ function uint8ArrayToBase64url(array) {
   return base64urlEncode(String.fromCharCode.apply(null, array));
 }
 
-// Encodes a Uint8Array to lowercase hex.
-function uint8ArrayToHex(array) {
-  const hexTable = '0123456789abcdef';
-  let s = '';
-  for (let i = 0; i < array.length; i++) {
-    s += hexTable.charAt(array[i] >> 4);
-    s += hexTable.charAt(array[i] & 15);
-  }
-  return s;
-}
-
 // Convert a EC signature from DER to a concatenation of the r and s parameters,
 // as expected by the subtle crypto API.
 function convertDERSignatureToSubtle(der) {
@@ -169,7 +158,13 @@ class Cbor {
     return this.slice.length == 0;
   }
   get hex() {
-    return uint8ArrayToHex(this.data);
+    const hexTable = '0123456789abcdef';
+    let s = '';
+    for (let i = 0; i < this.data.length; i++) {
+      s += hexTable.charAt(this.data[i] >> 4);
+      s += hexTable.charAt(this.data[i] & 15);
+    }
+    return s;
   }
   compare(other) {
     if (this.length < other.length) {
