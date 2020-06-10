15e942848262ea9f187fa73cf78a6408edbd6241
Michael Kaply <mozilla@kaply.com>
2020-06-10 05:31:45 +0000

    Bug 1644184 - allow hiding distro info for Mozilla distros. r=mhowell
    Differential Revision: https://phabricator.services.mozilla.com/D78823

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -19,22 +19,27 @@ async function init(aEvent) {
 
   var distroId = Services.prefs.getCharPref("distribution.id", "");
   if (distroId) {
-    var distroString = distroId;
-
-    var distroVersion = Services.prefs.getCharPref("distribution.version", "");
-    if (distroVersion) {
-      distroString += " - " + distroVersion;
-    }
-
-    var distroIdField = document.getElementById("distributionId");
-    distroIdField.value = distroString;
-    distroIdField.style.display = "block";
-
     var distroAbout = Services.prefs.getStringPref("distribution.about", "");
+    // If there is about text, we always show it.
     if (distroAbout) {
       var distroField = document.getElementById("distribution");
       distroField.value = distroAbout;
       distroField.style.display = "block";
+    }
+    // If it's not a mozilla distribution, show the rest,
+    // unless about text exists, then we always show.
+    if (!distroId.startsWith("mozilla-") || distroAbout) {
+      var distroVersion = Services.prefs.getCharPref(
+        "distribution.version",
+        ""
+      );
+      if (distroVersion) {
+        distroId += " - " + distroVersion;
+      }
+
+      var distroIdField = document.getElementById("distributionId");
+      distroIdField.value = distroId;
+      distroIdField.style.display = "block";
     }
   }
 
