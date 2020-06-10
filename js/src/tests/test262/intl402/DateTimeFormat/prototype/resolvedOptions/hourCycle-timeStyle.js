2d2d50bc71616eaa7a6b9b5cafe4c0a98a8a2144
André Bargull <andre.bargull@gmail.com>
2020-06-10 19:31:14 +0000

    Bug 1557718 - Part 5: Enable test262 tests for date-/timeStyle. r=jwalden
    Differential Revision: https://phabricator.services.mozilla.com/D73899

--- js/src/tests/test262/intl402/DateTimeFormat/prototype/resolvedOptions/hourCycle-timeStyle.js
+++ js/src/tests/test262/intl402/DateTimeFormat/prototype/resolvedOptions/hourCycle-timeStyle.js
@@ -1,4 +1,3 @@
-// |reftest| skip -- Intl.DateTimeFormat-datetimestyle is not supported
 // Copyright 2019 Mozilla Corporation, Igalia S.L. All rights reserved.
 // This code is governed by the license found in the LICENSE file.
 
