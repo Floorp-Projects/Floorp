21fd95808fbdb474fc754c86f952c77ff83bebfb
Matthew Gaudet <mgaudet@mozilla.com>
2020-06-10 21:04:54 +0000

    Bug 1640294 - Clarify UsedNameTracker comment and add worked example r=jorendorff DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D76554

--- js/src/frontend/UsedNameTracker.h
+++ js/src/frontend/UsedNameTracker.h
@@ -20,36 +20,79 @@ namespace frontend {
 
 // A data structure for tracking used names per parsing session in order to
 // compute which bindings are closed over. Scripts and scopes are numbered
-// monotonically in textual order and name uses are tracked by lists of
-// (script id, scope id) pairs of their use sites.
+// monotonically in textual order and unresolved uses of a name are tracked by
+// lists of identifier uses, which are a pair of (ScriptId,ScopeId).
 //
-// Intuitively, in a pair (P,S), P tracks the most nested function that has a
-// use of u, and S tracks the most nested scope that is still being parsed.
+// For an identifier `i` with a use (ScriptId,ScopeId) in the Used list,
+// ScriptId tracks the most nested script that has a use of u, and ScopeId
+// tracks the most nested scope that is still being parsed (as the lists will be
+// filtered as we finish processing a particular scope).
 //
-// P is used to answer the question "is u used by a nested function?"
-// S is used to answer the question "is u used in any scopes currently being
-//                                   parsed?"
+// ScriptId is used to answer the question "is `i` used by a nested function?"
+// ScopeId is used to answer the question "is `i` used in any scopes currently
+//                                         being parsed?"
 //
 // The algorithm:
 //
-// Let Used by a map of names to lists.
+// Let Used be a map of names to lists.
+// Let Declared(ScopeId) be a list of declarations for a scope numbered with
+// ScopeId
 //
 // 1. Number all scopes in monotonic increasing order in textual order.
 // 2. Number all scripts in monotonic increasing order in textual order.
-// 3. When an identifier u is used in scope numbered S in script numbered P,
-//    and u is found in Used,
-//   a. Append (P,S) to Used[u].
-//   b. Otherwise, assign the the list [(P,S)] to Used[u].
-// 4. When we finish parsing a scope S in script P, for each declared name d in
-//    Declared(S):
+// 3. When an identifier `i` is used in (ScriptId,ScopeId), append that use to
+//    the list Used[i] (creating the list and table entry if necessary).
+// 4. When an identifier `i` is declared in a scope numbered ScopeId, append `i`
+//    to Declared(ScopeId).
+// 5. When we finish parsing a scope numbered with ScopeId, in script numbered
+//    ScriptId, for each declared name d in Declared(ScopeId):
 //   a. If d is found in Used, mark d as closed over if there is a value
-//     (P_d, S_d) in Used[d] such that P_d > P and S_d > S.
-//   b. Remove all values (P_d, S_d) in Used[d] such that S_d are >= S.
+//      (UsedScriptId, UsedScopeId) in Used[d] such that UsedScriptId > ScriptId
+//      and UsedScopeId > ScopeId.
+//   b. Remove all values uses in Used[d] where UsedScopeId > ScopeId.
 //
 // Steps 1 and 2 are implemented by UsedNameTracker::next{Script,Scope}Id.
 // Step 3 is implemented by UsedNameTracker::noteUsedInScope.
-// Step 4 is implemented by UsedNameTracker::noteBoundInScope and
-// Parser::propagateFreeNamesAndMarkClosedOverBindings.
+// Step 4 is implemented by ParseContext::Scope::addDeclaredName.
+// Step 5 is implemented by UsedNameTracker::noteBoundInScope and
+// Parser::propagateFreeNamesAndMarkClosedOverBindings
+//
+// The following is a worked example to show how the algorithm works on a
+// relatively simple piece of code. (clang-format is disabled due to the width
+// of the example).
+
+// clang-format off
+//
+// // Script 1, Scope 1
+// var x = 1;                              // Declared(1) = [x];
+// function f() {// Script 2, Scope 2
+//     if (x > 10) { //Scope 3             // Used[x] = [(2,2)];
+//         var x = 12;                     // Declared(3) = [x];
+//         function g() // Script 3
+//         { // Scope 4
+//             return x;                   // Used[x] = [(2,2), (3,4)]
+//         }                               // Leaving Script 3, Scope 4: No declared variables.
+//     }                                   // Leaving Script 2, Scope 3: Declared(3) = [x];
+//                                         // - Used[x][1] = (2,2) is not > (2,3)
+//                                         // - Used[x][2] = (3,4) is > (2,3), so mark x as closed over.
+//                                         // - Update Used[x]: [] -- Makes sense, as at this point we have
+//                                         //                         bound all the unbound x to a particlar
+//                                         //                         declaration..
+//
+//     else { // Scope 5
+//         var x = 14;                     // Declared(5) = [x]
+//         function g() // Script 4
+//         { // Scope 6
+//             return y;                   // Used[y] = [(4,6)]
+//         }                               // Leaving Script 4, Scope 6: No declarations.
+//     }                                   // Leaving Script 2, Scope 5: Declared(5) = [x]
+//                                         // - Used[x] = [], so don't mark x as closed over.
+//     var y = 12;                         // Declared(2) = [y]
+// }                                       // Leaving Script 2, Scope 2: Declared(2) = [y]
+//                                         // - Used[y][1] = (4,6) is > (2,2), so mark y as closed over.
+//                                         // - Update Used[y]: [].
+
+// clang-format on
 class UsedNameTracker {
  public:
   struct Use {
