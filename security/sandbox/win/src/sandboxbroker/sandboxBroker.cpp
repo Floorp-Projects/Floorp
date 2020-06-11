83994a45b86032329f43219dfca2f1460ad59bcd
Alexis Beingessner <a.beingessner@gmail.com>
2020-06-11 12:34:10 +0000

    Bug 1642721 - convert the last two securit.sandbox.*.win32k VarCache prefs. r=bobowen
    converts:
      * security.sandbox.rdd.win32k-disable
      * security.sandbox.gmp.win32k-disable
    
    I'm assuming the pattern established by the other, newer, win32k StaticPrefs can
    be followed here, and the xpcom checks aren't needed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78933

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -58,10 +58,6 @@ static UniquePtr<nsString> sUserExtensionsDevDir;
 static UniquePtr<nsString> sUserExtensionsDir;
 #endif
 
-// Cached prefs which are needed off main thread.
-static bool sRddWin32kDisable = false;
-static bool sGmpWin32kDisable = false;
-
 static LazyLogModule sSandboxBrokerLog("SandboxBroker");
 
 #define LOG_E(...) MOZ_LOG(sSandboxBrokerLog, LogLevel::Error, (__VA_ARGS__))
@@ -141,17 +137,6 @@ void SandboxBroker::GeckoDependentInitialize() {
   // main thread.
   sLaunchErrors = MakeUnique<nsTHashtable<nsCStringHashKey>>();
   ClearOnShutdown(&sLaunchErrors);
-
-  if (haveXPCOM) {
-    // Cache prefs that are needed off main thread.
-    Preferences::AddBoolVarCache(&sRddWin32kDisable,
-                                 "security.sandbox.rdd.win32k-disable");
-    Preferences::AddBoolVarCache(&sGmpWin32kDisable,
-                                 "security.sandbox.gmp.win32k-disable");
-  } else {
-    sRddWin32kDisable = false;
-    sGmpWin32kDisable = false;
-  }
 }
 
 SandboxBroker::SandboxBroker() {
@@ -908,7 +893,7 @@ bool SandboxBroker::SetSecurityLevelForRDDProcess() {
 
   // On Windows 7, where Win32k lockdown is not supported, the Chromium
   // sandbox does something weird that breaks COM instantiation.
-  if (sRddWin32kDisable && IsWin8OrLater()) {
+  if (StaticPrefs::security_sandbox_rdd_win32k_disable() && IsWin8OrLater()) {
     mitigations |= sandbox::MITIGATION_WIN32K_DISABLE;
     result =
         mPolicy->AddRule(sandbox::TargetPolicy::SUBSYS_WIN32K_LOCKDOWN,
@@ -1279,7 +1264,7 @@ bool SandboxBroker::SetSecurityLevelForGMPlugin(SandboxLevel aLevel,
 
   // Chromium only implements win32k disable for PPAPI on Win10 or later,
   // believed to be due to the interceptions required for OPM.
-  if (sGmpWin32kDisable && IsWin10OrLater()) {
+  if (StaticPrefs::security_sandbox_gmp_win32k_disable() && IsWin10OrLater()) {
     mitigations |= sandbox::MITIGATION_WIN32K_DISABLE;
     result =
         mPolicy->AddRule(sandbox::TargetPolicy::SUBSYS_WIN32K_LOCKDOWN,
