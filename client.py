e1aae2a5fc20841652d4ae0f808eb60310ee7f75
Sylvestre Ledru <sledru@mozilla.com>
2020-06-11 18:03:17 +0000

    Bug 1644665 - client.py: clean up flake8/pylint warnings r=egao
    Differential Revision: https://phabricator.services.mozilla.com/D79045

--- client.py
+++ client.py
@@ -22,9 +22,11 @@ topsrcdir = os.path.dirname(__file__)
 if topsrcdir == '':
     topsrcdir = '.'
 
+
 def check_call_noisy(cmd, *args, **kwargs):
     print("Executing command:", cmd)
     check_call(cmd, *args, **kwargs)
+
 
 def do_hg_pull(dir, repository, hg):
     fulldir = os.path.join(topsrcdir, dir)
@@ -38,6 +40,7 @@ def do_hg_pull(dir, repository, hg):
         check_call_noisy(cmd)
     check_call([hg, 'parent', '-R', fulldir,
                 '--template=Updated to revision {node}.\n'])
+
 
 def do_hg_replace(dir, repository, tag, exclusions, hg):
     """
@@ -57,6 +60,7 @@ def do_hg_replace(dir, repository, tag, exclusions, hg):
                 shutil.rmtree(excluded)
             else:
                 os.remove(excluded)
+
 
 def do_cvs_export(modules, tag, cvsroot, cvs):
     """Check out a CVS directory without CVS metadata, using "export"
@@ -78,75 +82,81 @@ def do_cvs_export(modules, tag, cvsroot, cvs):
                          cwd=os.path.join(topsrcdir, parent))
         print("CVS export end: " + datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"))
 
-def toggle_trailing_blank_line(depname):
-  """If the trailing line is empty, then we'll delete it.
-  Otherwise we'll add a blank line."""
-  lines = open(depname, "r").readlines()
-  if not lines:
-      print("unexpected short file", file=sys.stderr)
-      return
 
-  if not lines[-1].strip():
-      # trailing line is blank, removing it
-      open(depname, "wb").writelines(lines[:-1])
-  else:
-      # adding blank line
-      open(depname, "ab").write("\n")
+def toggle_trailing_blank_line(depname):
+    """If the trailing line is empty, then we'll delete it.
+    Otherwise we'll add a blank line."""
+    lines = open(depname, "r").readlines()
+    if not lines:
+        print("unexpected short file", file=sys.stderr)
+        return
+
+    if not lines[-1].strip():
+        # trailing line is blank, removing it
+        open(depname, "wb").writelines(lines[:-1])
+    else:
+        # adding blank line
+        open(depname, "ab").write("\n")
+
 
 def get_trailing_blank_line_state(depname):
-  lines = open(depname, "r").readlines()
-  if not lines:
-      print("unexpected short file", file=sys.stderr)
-      return "no blank line"
+    lines = open(depname, "r").readlines()
+    if not lines:
+        print("unexpected short file", file=sys.stderr)
+        return "no blank line"
 
-  if not lines[-1].strip():
-      return "has blank line"
-  else:
-      return "no blank line"
+    if not lines[-1].strip():
+        return "has blank line"
+    else:
+        return "no blank line"
+
 
 def update_nspr_or_nss(tag, depfile, destination, hgpath):
-  destination = destination.rstrip('/')
-  permanent_patch_dir = destination + '/patches'
-  temporary_patch_dir = destination + '.patches'
-  if os.path.exists(temporary_patch_dir):
-    print("please clean up leftover directory " + temporary_patch_dir)
-    sys.exit(2)
-  warn_if_patch_exists(permanent_patch_dir)
-  # protect patch directory from being removed by do_hg_replace
-  if os.path.exists(permanent_patch_dir):
-    shutil.move(permanent_patch_dir, temporary_patch_dir)
-  # now update the destination
-  print("reverting to HG version of %s to get its blank line state" % depfile)
-  check_call_noisy([options.hg, 'revert', depfile])
-  old_state = get_trailing_blank_line_state(depfile)
-  print("old state of %s is: %s" % (depfile, old_state))
-  do_hg_replace(destination, hgpath, tag, HG_EXCLUSIONS, options.hg)
-  new_state = get_trailing_blank_line_state(depfile)
-  print("new state of %s is: %s" % (depfile, new_state))
-  if old_state == new_state:
-    print("toggling blank line in: ", depfile)
-    toggle_trailing_blank_line(depfile)
-  tag_file = destination + "/TAG-INFO"
-  with open(tag_file, 'w') as f:
-    f.write(tag)
-  # move patch directory back to a subdirectory
-  if os.path.exists(temporary_patch_dir):
-    shutil.move(temporary_patch_dir, permanent_patch_dir)
+    destination = destination.rstrip('/')
+    permanent_patch_dir = destination + '/patches'
+    temporary_patch_dir = destination + '.patches'
+    if os.path.exists(temporary_patch_dir):
+        print("please clean up leftover directory " + temporary_patch_dir)
+        sys.exit(2)
+    warn_if_patch_exists(permanent_patch_dir)
+    # protect patch directory from being removed by do_hg_replace
+    if os.path.exists(permanent_patch_dir):
+        shutil.move(permanent_patch_dir, temporary_patch_dir)
+    # now update the destination
+    print("reverting to HG version of %s to get its blank line state" % depfile)
+    check_call_noisy([options.hg, 'revert', depfile])
+    old_state = get_trailing_blank_line_state(depfile)
+    print("old state of %s is: %s" % (depfile, old_state))
+    do_hg_replace(destination, hgpath, tag, HG_EXCLUSIONS, options.hg)
+    new_state = get_trailing_blank_line_state(depfile)
+    print("new state of %s is: %s" % (depfile, new_state))
+    if old_state == new_state:
+        print("toggling blank line in: ", depfile)
+        toggle_trailing_blank_line(depfile)
+    tag_file = destination + "/TAG-INFO"
+    with open(tag_file, 'w') as f:
+        f.write(tag)
+    # move patch directory back to a subdirectory
+    if os.path.exists(temporary_patch_dir):
+        shutil.move(temporary_patch_dir, permanent_patch_dir)
+
 
 def warn_if_patch_exists(path):
-  # If the given patch directory exists and contains at least one file,
-  # then print warning and wait for the user to acknowledge.
-  if os.path.isdir(path) and os.listdir(path):
-    print("========================================")
-    print("WARNING: At least one patch file exists")
-    print("in directory: " + path)
-    print("You must manually re-apply all patches")
-    print("after this script has completed!")
-    print("========================================")
-    raw_input("Press Enter to continue...")
-    return
+    # If the given patch directory exists and contains at least one file,
+    # then print warning and wait for the user to acknowledge.
+    if os.path.isdir(path) and os.listdir(path):
+        print("========================================")
+        print("WARNING: At least one patch file exists")
+        print("in directory: " + path)
+        print("You must manually re-apply all patches")
+        print("after this script has completed!")
+        print("========================================")
+        raw_input("Press Enter to continue...")
+        return
 
-o = OptionParser(usage="client.py [options] update_nspr tagname | update_nss tagname | update_libffi tagname")
+
+o = OptionParser(
+    usage="client.py [options] update_nspr tagname | update_nss tagname | update_libffi tagname")
 o.add_option("--skip-mozilla", dest="skip_mozilla",
              action="store_true", default=False,
              help="Obsolete")
@@ -180,7 +190,7 @@ elif action in ('update_nss'):
     tag, = args[1:]
     depfile = "security/nss/coreconf/coreconf.dep"
     if not options.repo:
-	    options.repo = 'https://hg.mozilla.org/projects/nss'
+        options.repo = 'https://hg.mozilla.org/projects/nss'
     update_nspr_or_nss(tag, depfile, 'security/nss', options.repo)
 elif action in ('update_libffi'):
     tag, = args[1:]
