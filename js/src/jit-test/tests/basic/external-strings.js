894355e652d495abbaf4262691675c332665e030
Steve Fink <sfink@mozilla.com>
2020-06-11 18:15:25 +0000

    Bug 1644243 - Add a newString testing function for creating various types of JSString r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D78817

--- js/src/jit-test/tests/basic/external-strings.js
+++ js/src/jit-test/tests/basic/external-strings.js
@@ -1,17 +1,17 @@
-assertEq(newExternalString(""), "");
-assertEq(newExternalString("abc"), "abc");
-assertEq(newExternalString("abc\0def\u1234"), "abc\0def\u1234");
+assertEq(newString("", {external: true}), "");
+assertEq(newString("abc", {external: true}), "abc");
+assertEq(newString("abc\0def\u1234", {external: true}), "abc\0def\u1234");
 
 var o = {foo: 2, "foo\0": 4};
-var ext = newExternalString("foo");
+var ext = newString("foo", {external: true});
 assertEq(o[ext], 2);
-var ext2 = newExternalString("foo\0");
+var ext2 = newString("foo\0", {external: true});
 assertEq(o[ext2], 4);
 
-eval(newExternalString("assertEq(1, 1)"));
+eval(newString("assertEq(1, 1)", {external: true}));
 
 // Make sure ensureLinearString does the right thing for external strings.
-ext = newExternalString("abc\0defg\0");
+ext = newString("abc\0defg\0", {external: true});
 assertEq(ensureLinearString(ext), "abc\0defg\0");
 assertEq(ensureLinearString(ext), "abc\0defg\0");
 
@@ -19,7 +19,7 @@ for (var s of representativeStringArray())
     assertEq(ensureLinearString(s), s);
 
 for (var s of representativeStringArray())
-    assertEq(newExternalString(s), s);
+    assertEq(newString(s, {external: true}), s);
 
 function testQuote() {
     for (var data of [["abc", "abc"],
@@ -27,7 +27,7 @@ function testQuote() {
 		      ["abc\t\t\0", "abc\\t\\t\\x00"],
 		      ["abc\\def", "abc\\\\def"]]) {
 	try {
-	    assertEq(newExternalString(data[0]), false);
+	    assertEq(newString(data[0], {external: true}), false);
 	} catch(e) {
 	    assertEq(e.toString().includes('got "' + data[1] + '",'), true)
 	}
@@ -38,9 +38,9 @@ testQuote();
 function testMaybeExternal() {
     for (var i=0; i<10; i++) {
         var s = "abcdef4321" + i;
-        assertEq(newMaybeExternalString(s), s);
+        assertEq(newString(s, {maybeExternal: true}), s);
         if ((i % 2) === 0)
-            assertEq(ensureLinearString(newMaybeExternalString(s)), s);
+            assertEq(ensureLinearString(newString(s, {maybeExternal: true})), s);
     }
 }
 testMaybeExternal();
