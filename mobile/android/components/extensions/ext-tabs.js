af1ea3d76ddd6f141dd13a73d214e16e8df83944
dpk <deeps.karanji2@gmail.com>
2020-06-11 02:33:11 +0000

    Bug 1637431 - Removed a comment line, rev approved. r=robwu,geckoview-reviewers,agi
    ***
    Bug 1637431 - Gives meaningful tabs.query error message. r=robwu
    
    Differential Revision: https://phabricator.services.mozilla.com/D78466

--- mobile/android/components/extensions/ext-tabs.js
+++ mobile/android/components/extensions/ext-tabs.js
@@ -463,18 +463,6 @@ this.tabs = class extends ExtensionAPI {
               });
             }
           }
-
-          queryInfo = Object.assign({}, queryInfo);
-
-          if (queryInfo.url !== null) {
-            queryInfo.url = new MatchPatternSet([].concat(queryInfo.url), {
-              restrictSchemes: false,
-            });
-          }
-          if (queryInfo.title !== null) {
-            queryInfo.title = new MatchGlob(queryInfo.title);
-          }
-
           return Array.from(tabManager.query(queryInfo, context), tab =>
             tab.convert()
           );
