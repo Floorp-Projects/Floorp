578d25ba11baecd20a8d23e6e310ad1a8601ba03
Logan Smyth <loganfsmyth@gmail.com>
2020-06-11 21:24:16 +0000

    Bug 1601179 - Enable async stacks but limit captured async stacks to debuggees. r=jorendorff,smaug
    The 'asyncStack' flag on JS execution contexts is used as a general switch
    to enable async stack capture across all locations in SpiderMonkey, but
    this causes problems because it can at times be too much of a performance
    burden to general and track all of these stacks.
    
    Since the introduction of this option, we have only enabled it on Nightly
    and DevEdition for non-mobile builds, which has left a lot of users unable
    to take advantage of this data while debugging.
    
    This patch enables async stack traces across all of Firefox, but introduces
    a new pref to toggle the scope of the actual expensive part of async stacks,
    which is _capturing_ them and keeping them alive in memory. The new pref
    limits the capturing of async stack traces to only debuggees, unless an
    explicit pref is flipped to capture async traces for all cases.
    
    This means that while async stacks are technically enabled, and code could
    manually capture a stack and pass it back to SpiderMonkey and see that stack
    reflected in later captured stacks, SpiderMonkey itself and related async
    DOM APIs, among others, will not capture stacks or pass them to SpiderMonkey,
    so there should be no general change in performance by enabling the broader
    feature itself, unless the user is actively debugging the page.
    
    One affect of this patch is that if you have the debugger open and then close
    it, objects that have async stacks associated with them will retain those
    stacks and they will continue to show up in stack traces, no _new_ stacks
    will be captured. jorendorff and I have decided that this is okay because
    the expectation that the debugger fully revert every possible effect that it
    could have on a page is a nice goal but not a strict requirement.
    
    Differential Revision: https://phabricator.services.mozilla.com/D68503

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1115,17 +1115,20 @@ pref("javascript.options.wasm_reftypes",          true);
 pref("javascript.options.native_regexp",    true);
 pref("javascript.options.parallel_parsing", true);
 pref("javascript.options.source_pragmas",    true);
-// Async stacks instrumentation adds overhead that is only
-// advisable for developers, so we limit it to Nightly and DevEdition
+
+pref("javascript.options.asyncstack", true);
+// Broadly capturing async stack data adds overhead that is only advisable for
+// developers, so we limit it to Nightly and DevEdition, unless debugging.
 #if defined(ANDROID) || defined(XP_IOS)
-  pref("javascript.options.asyncstack",       false);
+  pref("javascript.options.asyncstack_capture_debuggee_only", true);
 #else
   #if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
-    pref("javascript.options.asyncstack",       true);
+    pref("javascript.options.asyncstack_capture_debuggee_only", false);
   #else
-    pref("javascript.options.asyncstack",       false);
+    pref("javascript.options.asyncstack_capture_debuggee_only", true);
   #endif
 #endif
+
 pref("javascript.options.throw_on_asmjs_validation_failure", false);
 pref("javascript.options.ion.offthread_compilation", true);
 #ifdef DEBUG
