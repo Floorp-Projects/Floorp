0d2aa310b5338a941226d264faf0102aaba0fc00
Ted Campbell <tcampbell@mozilla.com>
2020-06-12 14:25:50 +0000

    Bug 1642708 - Remove code guarded by JS_BUILD_BINAST r=arai
    Also remove js/src/frontend/BinAST* sources.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77945

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -12,7 +12,6 @@
 
 #include "builtin/MapObject.h"
 #include "debugger/DebugAPI.h"
-#include "frontend/BinASTParserBase.h"
 #include "frontend/BytecodeCompiler.h"
 #include "frontend/Parser.h"
 #include "gc/ClearEdgesTracer.h"
