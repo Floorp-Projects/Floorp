75389f67a3920ab382b11cd11f9a7d9563e8b0b1
Kartikaya Gupta <kgupta@mozilla.com>
2020-06-12 01:15:03 +0000

    Bug 1644271 - Move the innerWidth/Height overriding behavior to be conditional on a more appropriate check. r=tnikkel
    This code really cares about the case with a CSS viewport, which is only
    activated with mobile viewport sizing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79228

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -3607,7 +3607,7 @@ void nsGlobalWindowOuter::SetInnerWidthOuter(int32_t aInnerWidth,
 
   // Setting inner width should set the CSS viewport. If the CSS viewport
   // has been overridden, change the override.
-  if (presShell && presShell->GetIsViewportOverridden()) {
+  if (presShell && presShell->UsesMobileViewportSizing()) {
     nscoord height = 0;
 
     RefPtr<nsPresContext> presContext;
@@ -3653,7 +3653,7 @@ void nsGlobalWindowOuter::SetInnerHeightOuter(int32_t aInnerHeight,
 
   // Setting inner height should set the CSS viewport. If the CSS viewport
   // has been overridden, change the override.
-  if (presShell && presShell->GetIsViewportOverridden()) {
+  if (presShell && presShell->UsesMobileViewportSizing()) {
     nscoord width = 0;
 
     RefPtr<nsPresContext> presContext;
