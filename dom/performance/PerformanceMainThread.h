10eb040bdae5dee904d12031e97e505ae61d13b7
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-12 19:05:56 +0000

    Bug 1599160 - Add a performance resource entry when hitting the cache for the first time in a document. r=mayhemer,valentin
    This matches other browsers, our current behavior, and the test
    expectations from various WPTs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77842

--- dom/performance/PerformanceMainThread.h
+++ dom/performance/PerformanceMainThread.h
@@ -35,6 +35,10 @@ class PerformanceMainThread final : public Performance,
   virtual void AddEntry(nsIHttpChannel* channel,
                         nsITimedChannel* timedChannel) override;
 
+  void AddRawEntry(UniquePtr<PerformanceTimingData>,
+                   const nsAString& aInitiatorType,
+                   const nsAString& aEntryName);
+
   TimeStamp CreationTimeStamp() const override;
 
   DOMHighResTimeStamp CreationTime() const override;
