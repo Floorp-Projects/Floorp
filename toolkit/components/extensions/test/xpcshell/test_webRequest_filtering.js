e36bc4e9ea89dbd03a310f0b800b0112f96720ee
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-12 19:06:15 +0000

    Bug 1599160 - Clear the shared stylesheet cache in some mochitests. r=mayhemer,robwu
    So that they keep testing the network cache / url classifier / etc...
    
    Differential Revision: https://phabricator.services.mozilla.com/D77843

--- toolkit/components/extensions/test/xpcshell/test_webRequest_filtering.js
+++ toolkit/components/extensions/test/xpcshell/test_webRequest_filtering.js
@@ -73,6 +73,18 @@ function compareLists(list1, list2, kind) {
   equal(String(list1), String(list2), `${kind} URLs correct`);
 }
 
+async function openAndCloseContentPage(url) {
+  let contentPage = await ExtensionTestUtils.loadContentPage(URL);
+  // Clear the sheet cache so that it doesn't interact with following tests: A
+  // stylesheet with the same URI loaded from the same origin doesn't otherwise
+  // guarantee that onBeforeRequest and so on happen, because it may not need
+  // to go through necko at all.
+  await contentPage.spawn(null, () =>
+    content.windowUtils.clearSharedStyleSheetCache()
+  );
+  await contentPage.close();
+}
+
 add_task(async function setup() {
   // Disable rcwn to make cache behavior deterministic.
   Services.prefs.setBoolPref("network.http.rcwn.enabled", false);
@@ -91,8 +103,7 @@ add_task(async function filter_urls() {
   ]);
   WebRequest.onResponseStarted.addListener(onResponseStarted, filter);
 
-  let contentPage = await ExtensionTestUtils.loadContentPage(URL);
-  await contentPage.close();
+  await openAndCloseContentPage(URL);
 
   compareLists(requested, expected_urls, "requested");
   compareLists(sendHeaders, expected_urls, "sendHeaders");
@@ -113,8 +124,7 @@ add_task(async function filter_types() {
   ]);
   WebRequest.onResponseStarted.addListener(onResponseStarted, filter);
 
-  let contentPage = await ExtensionTestUtils.loadContentPage(URL);
-  await contentPage.close();
+  await openAndCloseContentPage(URL);
 
   compareLists(requested, expected_urls, "requested");
   compareLists(sendHeaders, expected_urls, "sendHeaders");
@@ -137,8 +147,7 @@ add_task(async function filter_windowId() {
   ]);
   WebRequest.onResponseStarted.addListener(onResponseStarted, filter);
 
-  let contentPage = await ExtensionTestUtils.loadContentPage(URL);
-  await contentPage.close();
+  await openAndCloseContentPage(URL);
 
   compareLists(requested, [], "requested");
   compareLists(sendHeaders, [], "sendHeaders");
@@ -161,8 +170,7 @@ add_task(async function filter_tabId() {
   ]);
   WebRequest.onResponseStarted.addListener(onResponseStarted, filter);
 
-  let contentPage = await ExtensionTestUtils.loadContentPage(URL);
-  await contentPage.close();
+  await openAndCloseContentPage(URL);
 
   compareLists(requested, [], "requested");
   compareLists(sendHeaders, [], "sendHeaders");
