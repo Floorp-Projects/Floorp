48c828e69ab27c019bc4c8063e48be19f805e3d7
Nika Layzell <nika@thelayzells.com>
2020-06-12 16:52:09 +0000

    Bug 1640019 - Part 7: Remove the unused 'navigateAndRestoreByIndex' method, r=Gijs
    This method was used by GroupedSHistory for prerendering, and was missed when we
    ripped out the incomplete code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78975

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5501,22 +5501,6 @@ var XULBrowserWindow = {
     }
     this.onStatusChange(gBrowser.webProgress, null, 0, aMessage);
   },
-
-  navigateAndRestoreByIndex: function XWB_navigateAndRestoreByIndex(
-    aBrowser,
-    aIndex
-  ) {
-    let tab = gBrowser.getTabForBrowser(aBrowser);
-    if (tab) {
-      SessionStore.navigateAndRestore(tab, {}, aIndex);
-      return;
-    }
-
-    throw new Error(
-      "Trying to navigateAndRestore a browser which was " +
-        "not attached to this tabbrowser is unsupported"
-    );
-  },
 };
 
 var LinkTargetDisplay = {
