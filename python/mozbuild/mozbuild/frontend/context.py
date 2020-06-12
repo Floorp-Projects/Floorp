5e1e99baa54978f8feb19bd1b9d74820b5578d9e
Geoff Brown <gbrown@mozilla.com>
2020-06-12 08:04:08 +0000

    Bug 1644228 - Remove support for IMPACTED_TESTS; r=remote-protocol-reviewers,firefox-build-system-reviewers,rstewart,whimboo
    IMPACTED_TESTS was added in bug 1184405, but is under-utilized in current moz.build
    annotations; also bug 1516847 removed use of IMPACTED_TESTS in 'mach test'. This
    patch removes remaining support for IMPACTED_TESTS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79141

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -41,7 +41,6 @@ from mozbuild.util import (
 from .. import schedules
 
 from ..testing import (
-    all_test_flavors,
     read_manifestparser_manifest,
     read_reftest_manifest,
 )
@@ -1023,13 +1022,6 @@ def OrderedPathListWithAction(action):
 ManifestparserManifestList = OrderedPathListWithAction(read_manifestparser_manifest)
 ReftestManifestList = OrderedPathListWithAction(read_reftest_manifest)
 
-OrderedSourceList = ContextDerivedTypedList(SourcePath, StrictOrderingOnAppendList)
-OrderedTestFlavorList = TypedList(Enum(*all_test_flavors()),
-                                  StrictOrderingOnAppendList)
-OrderedStringList = TypedList(six.text_type, StrictOrderingOnAppendList)
-DependentTestsEntry = ContextDerivedTypedRecord(('files', OrderedSourceList),
-                                                ('tags', OrderedStringList),
-                                                ('flavors', OrderedTestFlavorList))
 BugzillaComponent = TypedNamedTuple('BugzillaComponent',
                                     [('product', six.text_type), ('component', six.text_type)])
 SchedulingComponents = ContextDerivedTypedRecord(
@@ -1112,54 +1104,6 @@ class Files(SubContext):
 
             See :ref:`mozbuild_files_metadata_finalizing` for more info.
             """),
-        'IMPACTED_TESTS': (DependentTestsEntry, list,
-                           """File patterns, tags, and flavors for tests relevant to these files.
-
-            Maps source files to the tests potentially impacted by those files.
-            Tests can be specified by file pattern, tag, or flavor.
-
-            For example:
-
-            with Files('runtests.py'):
-               IMPACTED_TESTS.files += [
-                   '**',
-               ]
-
-            in testing/mochitest/moz.build will suggest that any of the tests
-            under testing/mochitest may be impacted by a change to runtests.py.
-
-            File patterns may be made relative to the topsrcdir with a leading
-            '/', so
-
-            with Files('httpd.js'):
-               IMPACTED_TESTS.files += [
-                   '/testing/mochitest/tests/Harness_sanity/**',
-               ]
-
-            in netwerk/test/httpserver/moz.build will suggest that any change to httpd.js
-            will be relevant to the mochitest sanity tests.
-
-            Tags and flavors are sorted string lists (flavors are limited to valid
-            values).
-
-            For example:
-
-            with Files('toolkit/devtools/*'):
-                IMPACTED_TESTS.tags += [
-                    'devtools',
-                ]
-
-            in the root moz.build would suggest that any test tagged 'devtools' would
-            potentially be impacted by a change to a file under toolkit/devtools, and
-
-            with Files('dom/base/nsGlobalWindow.cpp'):
-                IMPACTED_TESTS.flavors += [
-                    'mochitest',
-                ]
-
-            Would suggest that nsGlobalWindow.cpp is potentially relevant to
-            any plain mochitest.
-            """),
         'SCHEDULES': (Schedules, list,
                       """Maps source files to the CI tasks that should be scheduled when
             they change.  The tasks are grouped by named components, and those
@@ -1207,13 +1151,6 @@ class Files(SubContext):
         self.test_flavors |= other.test_flavors
 
         for k, v in other.items():
-            if k == 'IMPACTED_TESTS':
-                self.test_files |= set(mozpath.relpath(e.full_path, e.context.config.topsrcdir)
-                                       for e in v.files)
-                self.test_tags |= set(v.tags)
-                self.test_flavors |= set(v.flavors)
-                continue
-
             if k == 'SCHEDULES' and 'SCHEDULES' in self:
                 self['SCHEDULES'] = self['SCHEDULES'] | v
                 continue
