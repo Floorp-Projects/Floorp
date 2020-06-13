fa92ff33dfacbed8bc8389df53081bf49aabef99
Kartikaya Gupta <kgupta@mozilla.com>
2020-06-13 11:41:40 +0000

    Bug 1645520 - Ensure an MVM operating in VisualViewportOnly mode knows about display resizes. r=tnikkel
    This is important because if the apz.allow_zooming pref is enabled (and
    eventually even without that pref), VisualViewportOnly-type MVMs do set
    the visual viewport size based on the display size and zoom, and so need
    to be updated when those change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79594

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -1955,6 +1955,9 @@ nsresult PresShell::ResizeReflow(nscoord aWidth, nscoord aHeight,
     mMobileViewportManager->RequestReflow(false);
     return NS_OK;
   }
+  if (mMobileViewportManager) {
+    mMobileViewportManager->NotifyResizeReflow();
+  }
 
   return ResizeReflowIgnoreOverride(aWidth, aHeight, aOptions);
 }
