58bb0b25d9b68b81e77c2b255e3d290a8535b95f
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-14 11:45:06 +0200

    Backed out 2 changesets (bug 477157) for border rendering regressions.
    Backed out changeset 04543b8ded50 (bug 477157)
    Backed out changeset 5df17ecbcaa1 (bug 477157)
    
    MANUAL PUSH: backout

--- layout/painting/nsCSSRenderingBorders.cpp
+++ layout/painting/nsCSSRenderingBorders.cpp
@@ -3829,13 +3829,14 @@ nsCSSBorderImageRenderer::nsCSSBorderImageRenderer(
 
   // Compute the used values of 'border-image-slice' and 'border-image-width';
   // we do them together because the latter can depend on the former.
-  nscoord oneDevPixel = aForFrame->PresContext()->DevPixelsToAppUnits(1);
+  nsMargin slice;
+  nsMargin border;
   for (const auto s : mozilla::AllPhysicalSides()) {
     const auto& slice = aStyleBorder.mBorderImageSlice.offsets.Get(s);
     int32_t imgDimension =
         SideIsVertical(s) ? mImageSize.width : mImageSize.height;
     nscoord borderDimension = SideIsVertical(s) ? mArea.width : mArea.height;
-    nscoord value;
+    double value;
     if (slice.IsNumber()) {
       value = nsPresContext::CSSPixelsToAppUnits(NS_lround(slice.AsNumber()));
     } else {
@@ -3856,23 +3857,22 @@ nsCSSBorderImageRenderer::nsCSSBorderImageRenderer(
         value =
             std::max(0, width.AsLengthPercentage().Resolve(borderDimension));
         break;
-      case StyleBorderImageSideWidth::Tag::Number: {
-        nscoord snappedWidth = borderWidths.Side(s) / oneDevPixel * oneDevPixel;
-        // NSToCoordRoundWithClamp rounds towards infinity, but that's OK
-        // because we expect value to be non-negative.
-        value = NSToCoordRoundWithClamp(width.AsNumber() * snappedWidth);
+      case StyleBorderImageSideWidth::Tag::Number:
+        value = width.AsNumber() * borderWidths.Side(s);
         break;
-      }
       case StyleBorderImageSideWidth::Tag::Auto:
-        // Re-use `value` from the slice.
+        value = mSlice.Side(s);
         break;
       default:
         MOZ_ASSERT_UNREACHABLE("unexpected CSS unit for border image area");
         value = 0;
         break;
     }
+    // NSToCoordRoundWithClamp rounds towards infinity, but that's OK
+    // because we expect value to be non-negative.
     MOZ_ASSERT(value >= 0);
-    mWidths.Side(s) = value;
+    mWidths.Side(s) = NSToCoordRoundWithClamp(value);
+    MOZ_ASSERT(mWidths.Side(s) >= 0);
   }
 
   // "If two opposite border-image-width offsets are large enough that they
