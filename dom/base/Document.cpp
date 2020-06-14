918f5020bda2b1cef3e20fcec132c115fb529565
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-14 23:26:04 +0000

    Bug 1643656 - Remove prefers-color-scheme: no-preference. r=gl,remote-protocol-reviewers,hiro,whimboo
    It was removed from the spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78834

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -16432,20 +16432,9 @@ StylePrefersColorScheme Document::PrefersColorScheme(
     }
   }
 
-  // If LookAndFeel::IntID::SystemUsesDarkTheme fails then return 2
-  // (no-preference)
-  switch (LookAndFeel::GetInt(LookAndFeel::IntID::SystemUsesDarkTheme, 2)) {
-    case 0:
-      return StylePrefersColorScheme::Light;
-    case 1:
-      return StylePrefersColorScheme::Dark;
-    case 2:
-      return StylePrefersColorScheme::NoPreference;
-    default:
-      // This only occurs if the user has set the ui.systemUsesDarkTheme pref to
-      // an invalid value.
-      return StylePrefersColorScheme::Light;
-  }
+  const bool dark =
+      !!LookAndFeel::GetInt(LookAndFeel::IntID::SystemUsesDarkTheme, 0);
+  return dark ? StylePrefersColorScheme::Dark : StylePrefersColorScheme::Light;
 }
 
 // static
