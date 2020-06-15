212943c862c5bd89cbe228d0d156339b11ce99dc
Nika Layzell <nika@thelayzells.com>
2020-06-15 23:23:43 +0000

    Bug 1640019 - Part 1: Support toplevel process switches outside of tabbrowser, r=mattwoodrow
    This new process switching behavior is only enabled for some browser elements,
    which have specified a specific attribute. Turning this on for all browsers with
    a `remote` attribute causes breakage in reftests.
    
    The initial version does not handle switching from remote to parent or
    vice-versa, that is covered in a later part.
    
    Differential Revision: https://phabricator.services.mozilla.com/D78969

--- dom/base/nsFrameLoader.h
+++ dom/base/nsFrameLoader.h
@@ -110,7 +110,7 @@ class nsFrameLoader final : public nsStubMutationObserver,
   // FrameLoaders.
   static already_AddRefed<nsFrameLoader> Recreate(Element* aOwner,
                                                   BrowsingContext* aContext,
-                                                  const nsAString& aRemoteType,
+                                                  bool aIsRemote,
                                                   bool aNetworkCreated,
                                                   bool aPreserveContext);
 
@@ -391,6 +391,14 @@ class nsFrameLoader final : public nsStubMutationObserver,
 
   void SetWillChangeProcess();
 
+  // Configure which remote process should be used to host the remote browser
+  // created in `TryRemoteBrowser`. This method _must_ be called before
+  // `TryRemoteBrowser`, and a script blocker must be on the stack.
+  //
+  // |aContentParent|, if set, must have the remote type |aRemoteType|.
+  void ConfigRemoteProcess(const nsAString& aRemoteType,
+                           mozilla::dom::ContentParent* aContentParent);
+
   void MaybeNotifyCrashed(mozilla::dom::BrowsingContext* aBrowsingContext,
                           mozilla::ipc::MessageChannel* aChannel);
 
@@ -398,8 +406,8 @@ class nsFrameLoader final : public nsStubMutationObserver,
 
  private:
   nsFrameLoader(mozilla::dom::Element* aOwner,
-                mozilla::dom::BrowsingContext* aBrowsingContext,
-                const nsAString& aRemoteType, bool aNetworkCreated);
+                mozilla::dom::BrowsingContext* aBrowsingContext, bool aIsRemote,
+                bool aNetworkCreated);
   ~nsFrameLoader();
 
   void SetOwnerContent(mozilla::dom::Element* aContent);
