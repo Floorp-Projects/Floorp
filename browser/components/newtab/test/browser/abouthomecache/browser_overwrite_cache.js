182cdd7608c9bff5e5d271c6488b11511d3e9414
Mike Conley <mconley@mozilla.com>
2020-06-16 17:18:14 +0000

    Bug 1622088 - Part 2: Add some mochitest-browser tests for the about:home startup cache. r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D71793

---
+++ browser/components/newtab/test/browser/abouthomecache/browser_overwrite_cache.js
@@ -0,0 +1,37 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Tests that if a pre-existing about:home cache exists, that it can
+ * be overwritten with new information.
+ */
+add_task(async function test_overwrite_cache() {
+  await BrowserTestUtils.withNewTab("about:home", async browser => {
+    await simulateRestart(browser);
+    const TEST_ID = "test_overwrite_cache_h1";
+
+    // We need the CSP meta tag in about: pages, otherwise we hit assertions in
+    // debug builds.
+    await injectIntoCache(
+      `
+      <html>
+        <head>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; object-src 'none'; script-src resource: chrome:; connect-src https:; img-src https: data: blob:; style-src 'unsafe-inline';">
+        </head>
+        <body>
+          <h1 id="${TEST_ID}">Something new</h1>
+        </body>
+        <script src="about:home?jscache"></script>
+      </html>`,
+      "window.__FROM_STARTUP_CACHE__ = true;"
+    );
+    await simulateRestart(browser, false /* withAutoShutdownWrite */);
+
+    await SpecialPowers.spawn(browser, [TEST_ID], async testID => {
+      let target = content.document.getElementById(testID);
+      Assert.ok(target, "Found the target element");
+    });
+  });
+});
