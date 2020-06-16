4879bf3303ddde297923bc878f321692bab1b5ba
Razvan Maries <rmaries@mozilla.com>
2020-06-17 01:49:36 +0300

    Backed out changeset 722854464579 (bug 1623520) for multiple failures on Win10 x64 asan. CLOSED TREE

--- taskcluster/scripts/misc/build-clang-linux-win-cross.sh
+++ taskcluster/scripts/misc/build-clang-linux-win-cross.sh
@@ -9,7 +9,7 @@ cd $MOZ_FETCHES_DIR
 # We have a non-extracted clang-cl/clang.tar.zst for Windows clang-cl that we need to extract
 # files from.
 
-$GECKO_PATH/taskcluster/scripts/misc/zstdpy -d clang-cl/clang.tar.zst | tar -x --wildcards clang/lib/clang/*/lib/windows clang/bin/clang-cl.exe clang/bin/llvm-symbolizer.exe
+$GECKO_PATH/taskcluster/scripts/misc/zstdpy -d clang-cl/clang.tar.zst | tar -x --wildcards clang/lib/clang/*/lib/windows clang/bin/clang-cl.exe
 chmod +x clang/bin/clang-cl.exe
 tar -c clang | $GECKO_PATH/taskcluster/scripts/misc/zstdpy > clang.tar.zst
 
