021981d809ee796acd1387cd089e3aa8c0b59ea4
Razvan Maries <rmaries@mozilla.com>
2020-06-16 20:19:19 +0300

    Backed out changeset 13d8ddd71873 (bug 1645845) for bustages on ParserAtom.h. CLOSED TREE

--- js/src/util/Text.h
+++ js/src/util/Text.h
@@ -108,10 +108,6 @@ extern UniqueChars DuplicateStringToArena(arena_id_t destArenaId, JSContext* cx,
 extern UniqueChars DuplicateStringToArena(arena_id_t destArenaId, JSContext* cx,
                                           const char* s, size_t n);
 
-extern UniqueLatin1Chars DuplicateStringToArena(arena_id_t destArenaId,
-                                                JSContext* cx,
-                                                const Latin1Char* s, size_t n);
-
 extern UniqueTwoByteChars DuplicateStringToArena(arena_id_t destArenaId,
                                                  JSContext* cx,
                                                  const char16_t* s);
@@ -130,10 +126,6 @@ extern UniqueChars DuplicateStringToArena(arena_id_t destArenaId,
 extern UniqueChars DuplicateStringToArena(arena_id_t destArenaId, const char* s,
                                           size_t n);
 
-extern UniqueLatin1Chars DuplicateStringToArena(arena_id_t destArenaId,
-                                                const JS::Latin1Char* s,
-                                                size_t n);
-
 extern UniqueTwoByteChars DuplicateStringToArena(arena_id_t destArenaId,
                                                  const char16_t* s);
 
@@ -143,9 +135,6 @@ extern UniqueTwoByteChars DuplicateStringToArena(arena_id_t destArenaId,
 extern UniqueChars DuplicateString(JSContext* cx, const char* s);
 
 extern UniqueChars DuplicateString(JSContext* cx, const char* s, size_t n);
-
-extern UniqueLatin1Chars DuplicateString(JSContext* cx, const JS::Latin1Char* s,
-                                         size_t n);
 
 extern UniqueTwoByteChars DuplicateString(JSContext* cx, const char16_t* s);
 
@@ -159,8 +148,6 @@ extern UniqueTwoByteChars DuplicateString(JSContext* cx, const char16_t* s,
 extern UniqueChars DuplicateString(const char* s);
 
 extern UniqueChars DuplicateString(const char* s, size_t n);
-
-extern UniqueLatin1Chars DuplicateString(const JS::Latin1Char* s, size_t n);
 
 extern UniqueTwoByteChars DuplicateString(const char16_t* s);
 
