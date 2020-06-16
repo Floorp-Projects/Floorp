f1292da2326fd5250c7e292b7e189ea2a95bf3f5
Doug Thayer <dothayer@mozilla.com>
2020-06-16 18:29:35 +0000

    Bug 1644265 - Reject new DataStorages if we are shutting down r=keeler
    Differential Revision: https://phabricator.services.mozilla.com/D79760

--- security/manager/ssl/DataStorage.cpp
+++ security/manager/ssl/DataStorage.cpp
@@ -7,6 +7,7 @@
 #include "DataStorage.h"
 
 #include "mozilla/Assertions.h"
+#include "mozilla/AppShutdown.h"
 #include "mozilla/ClearOnShutdown.h"
 #include "mozilla/dom/PContent.h"
 #include "mozilla/dom/ContentParent.h"
@@ -311,6 +312,14 @@ nsresult DataStorage::Init(const nsTArray<DataStorageItem>* aItems,
   if (!NS_IsMainThread()) {
     MOZ_ASSERT_UNREACHABLE("DataStorage::Init called off main thread");
     return NS_ERROR_NOT_SAME_THREAD;
+  }
+
+  if (AppShutdown::IsShuttingDown()) {
+    // Reject new DataStorage instances if the browser is shutting down. There
+    // is no guarantee that DataStorage writes will be able to be persisted if
+    // we init during shutdown, so we return an error here to hopefully make
+    // this more explicit and consistent.
+    return NS_ERROR_NOT_AVAILABLE;
   }
 
   MutexAutoLock lock(mMutex);
