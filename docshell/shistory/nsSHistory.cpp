c3d543181b37b04162d5a96d20a3d30c952a900d
Simon Giesecke <sgiesecke@mozilla.com>
2020-06-16 10:14:24 +0000

    Bug 1645339 - Use range-based for with nsTObserverArray in docshell. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D79493

--- docshell/shistory/nsSHistory.cpp
+++ docshell/shistory/nsSHistory.cpp
@@ -105,17 +105,15 @@ extern mozilla::LazyLogModule gPageCacheLog;
   PR_END_MACRO
 
 // Iterates over all registered session history listeners.
-#define ITERATE_LISTENERS(body)                                              \
-  PR_BEGIN_MACRO {                                                           \
-    nsAutoTObserverArray<nsWeakPtr, 2>::EndLimitedIterator iter(mListeners); \
-    while (iter.HasMore()) {                                                 \
-      nsCOMPtr<nsISHistoryListener> listener =                               \
-          do_QueryReferent(iter.GetNext());                                  \
-      if (listener) {                                                        \
-        body                                                                 \
-      }                                                                      \
-    }                                                                        \
-  }                                                                          \
+#define ITERATE_LISTENERS(body)                                           \
+  PR_BEGIN_MACRO {                                                        \
+    for (const nsWeakPtr& weakPtr : mListeners.EndLimitedRange()) {       \
+      nsCOMPtr<nsISHistoryListener> listener = do_QueryReferent(weakPtr); \
+      if (listener) {                                                     \
+        body                                                              \
+      }                                                                   \
+    }                                                                     \
+  }                                                                       \
   PR_END_MACRO
 
 // Calls a given method on all registered session history listeners.
