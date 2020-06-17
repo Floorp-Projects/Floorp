d03530f8b3f7a1a0b6bc66bcb3cb090bfe8daae6
Razvan Maries <rmaries@mozilla.com>
2020-06-17 05:49:26 +0300

    Backed out changeset 647adc688cea (bug 1645845) for build bustages on ParserAtom.cpp.

--- js/src/jsapi.cpp
+++ js/src/jsapi.cpp
@@ -464,10 +464,6 @@ JS_PUBLIC_API bool JS::InitSelfHostedCode(JSContext* cx) {
     return false;
   }
 
-  if (!rt->initializeParserAtoms(cx)) {
-    return false;
-  }
-
 #ifndef JS_CODEGEN_NONE
   if (!rt->createJitRuntime(cx)) {
     return false;
