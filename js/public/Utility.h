ff6696efc0b0da9e58f80ad0238297b36e75331a
Razvan Maries <rmaries@mozilla.com>
2020-06-17 05:49:26 +0300

    Backed out changeset 647adc688cea (bug 1645845) for build bustages on ParserAtom.cpp.

--- js/public/Utility.h
+++ js/public/Utility.h
@@ -21,7 +21,6 @@
 
 #include "jstypes.h"
 #include "mozmemory.h"
-#include "js/TypeDecls.h"
 
 /* The public JS engine namespace. */
 namespace JS {}
@@ -651,7 +650,6 @@ struct FreePolicy {
 
 typedef mozilla::UniquePtr<char[], JS::FreePolicy> UniqueChars;
 typedef mozilla::UniquePtr<char16_t[], JS::FreePolicy> UniqueTwoByteChars;
-typedef mozilla::UniquePtr<JS::Latin1Char[], JS::FreePolicy> UniqueLatin1Chars;
 
 }  // namespace JS
 
