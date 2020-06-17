8867c1d7259de5778d110b47a9cfbdff8ebacaa5
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-06-17 22:38:30 +0000

    Bug 1646263 - Separate CompositorAnimationStorage from AnimationHelper.[cpp,h]. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D79944

--- gfx/layers/AnimationHelper.cpp
+++ gfx/layers/AnimationHelper.cpp
@@ -11,6 +11,7 @@
 #include "mozilla/dom/KeyframeEffect.h"       // for dom::KeyFrameEffectReadOnly
 #include "mozilla/dom/Nullable.h"             // for dom::Nullable
 #include "mozilla/layers/CompositorThread.h"  // for CompositorThreadHolder
+#include "mozilla/layers/CompositorAnimationStorage.h"  // for CompositorAnimationStorage
 #include "mozilla/layers/LayerAnimationUtils.h"  // for TimingFunctionToComputedTimingFunction
 #include "mozilla/LayerAnimationInfo.h"  // for GetCSSPropertiesFor()
 #include "mozilla/MotionPathUtils.h"     // for ResolveMotionPath()
@@ -21,114 +22,6 @@
 
 namespace mozilla {
 namespace layers {
-
-void CompositorAnimationStorage::Clear() {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-
-  mAnimatedValues.Clear();
-  mAnimations.clear();
-}
-
-void CompositorAnimationStorage::ClearById(const uint64_t& aId) {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-
-  mAnimatedValues.Remove(aId);
-  mAnimations.erase(aId);
-}
-
-AnimatedValue* CompositorAnimationStorage::GetAnimatedValue(
-    const uint64_t& aId) const {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  return mAnimatedValues.Get(aId);
-}
-
-OMTAValue CompositorAnimationStorage::GetOMTAValue(const uint64_t& aId) const {
-  OMTAValue omtaValue = mozilla::null_t();
-  auto animatedValue = GetAnimatedValue(aId);
-  if (!animatedValue) {
-    return omtaValue;
-  }
-
-  animatedValue->Value().match(
-      [&](const AnimationTransform& aTransform) {
-        gfx::Matrix4x4 transform = aTransform.mFrameTransform;
-        const TransformData& data = aTransform.mData;
-        float scale = data.appUnitsPerDevPixel();
-        gfx::Point3D transformOrigin = data.transformOrigin();
-
-        // Undo the rebasing applied by
-        // nsDisplayTransform::GetResultingTransformMatrixInternal
-        transform.ChangeBasis(-transformOrigin);
-
-        // Convert to CSS pixels (this undoes the operations performed by
-        // nsStyleTransformMatrix::ProcessTranslatePart which is called from
-        // nsDisplayTransform::GetResultingTransformMatrix)
-        double devPerCss = double(scale) / double(AppUnitsPerCSSPixel());
-        transform._41 *= devPerCss;
-        transform._42 *= devPerCss;
-        transform._43 *= devPerCss;
-        omtaValue = transform;
-      },
-      [&](const float& aOpacity) { omtaValue = aOpacity; },
-      [&](const nscolor& aColor) { omtaValue = aColor; });
-  return omtaValue;
-}
-
-void CompositorAnimationStorage::SetAnimatedValue(
-    uint64_t aId, AnimatedValue* aPreviousValue,
-    gfx::Matrix4x4&& aTransformInDevSpace, gfx::Matrix4x4&& aFrameTransform,
-    const TransformData& aData) {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  if (!aPreviousValue) {
-    MOZ_ASSERT(!mAnimatedValues.Contains(aId));
-    mAnimatedValues.Put(
-        aId, MakeUnique<AnimatedValue>(std::move(aTransformInDevSpace),
-                                       std::move(aFrameTransform), aData));
-    return;
-  }
-  MOZ_ASSERT(aPreviousValue->Is<AnimationTransform>());
-  MOZ_ASSERT(aPreviousValue == GetAnimatedValue(aId));
-
-  aPreviousValue->SetTransform(std::move(aTransformInDevSpace),
-                               std::move(aFrameTransform), aData);
-}
-
-void CompositorAnimationStorage::SetAnimatedValue(uint64_t aId,
-                                                  AnimatedValue* aPreviousValue,
-                                                  nscolor aColor) {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  if (!aPreviousValue) {
-    MOZ_ASSERT(!mAnimatedValues.Contains(aId));
-    mAnimatedValues.Put(aId, MakeUnique<AnimatedValue>(aColor));
-    return;
-  }
-
-  MOZ_ASSERT(aPreviousValue->Is<nscolor>());
-  MOZ_ASSERT(aPreviousValue == GetAnimatedValue(aId));
-  aPreviousValue->SetColor(aColor);
-}
-
-void CompositorAnimationStorage::SetAnimatedValue(uint64_t aId,
-                                                  AnimatedValue* aPreviousValue,
-                                                  float aOpacity) {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  if (!aPreviousValue) {
-    MOZ_ASSERT(!mAnimatedValues.Contains(aId));
-    mAnimatedValues.Put(aId, MakeUnique<AnimatedValue>(aOpacity));
-    return;
-  }
-
-  MOZ_ASSERT(aPreviousValue->Is<float>());
-  MOZ_ASSERT(aPreviousValue == GetAnimatedValue(aId));
-  aPreviousValue->SetOpacity(aOpacity);
-}
-
-void CompositorAnimationStorage::SetAnimations(uint64_t aId,
-                                               const AnimationArray& aValue) {
-  MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
-  mAnimations[aId] = std::make_unique<AnimationStorageData>(
-      AnimationHelper::ExtractAnimations(aValue));
-}
 
 enum class CanSkipCompose {
   IfPossible,
