9f73e2fb3ba54fdff8173646476d788841aefb98
Ted Campbell <tcampbell@mozilla.com>
2020-06-17 13:56:58 +0000

    Bug 1642708 - Remove more references to BinAST code r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D77946

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -1905,11 +1905,8 @@ static bool InstantiateScriptStencils(JSContext* cx,
     } else if (funbox->isAsmJSModule()) {
       MOZ_ASSERT(funbox->function()->isAsmJSNative());
     } else if (funbox->function()->isIncomplete()) {
-      // Lazy functions are generally only allocated in the initial parse. The
-      // exception to this is BinAST which does not allocate lazy functions
-      // inside lazy functions until delazification occurs.
-      MOZ_ASSERT(compilationInfo.lazy == nullptr ||
-                 compilationInfo.lazy->isBinAST());
+      // Lazy functions are generally only allocated in the initial parse.
+      MOZ_ASSERT(compilationInfo.lazy == nullptr);
 
       if (!CreateLazyScript(cx, compilationInfo, funbox)) {
         return false;
