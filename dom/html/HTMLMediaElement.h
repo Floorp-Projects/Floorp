4e982cf82c20dc5a7af79da9d25ba19812e481a1
Alex Chronopoulos <achronop@gmail.com>
2020-06-18 14:05:28 +0000

    Bug 1493990 - Enable sink change in the srcObject of HTMLMediaElement. r=padenot
    The SetSinkId method of HTMLMediaElement has been enhanced to handle the sink change when the element is sourced with a MediaObject through the srcObject attribute. It makes use in the corresponding method of AudioStreamTrack taking into account the AudioOutput and Volume that has to be removed and re-added to the right track.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77809

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -1831,6 +1831,8 @@ class HTMLMediaElement : public nsGenericHTMLElement,
    */
   void AfterMaybeChangeAttr(int32_t aNamespaceID, nsAtom* aName, bool aNotify);
 
+  RefPtr<GenericPromise> SetSrcMediaStreamSink(AudioDeviceInfo* aSink);
+
   // Total time a video has spent playing.
   TimeDurationAccumulator mPlayTime;
 
