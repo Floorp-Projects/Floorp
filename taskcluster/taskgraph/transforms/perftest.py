91291fe122921f057ea643a626a69be1df0ae48a
Gregory Mierzwinski <gmierz2@outlook.com>
2020-06-18 22:37:35 +0000

    Bug 1646697 - Modify how the multi-commit tasks work. r=mozperftest-reviewers,tarek
    This patch adds a `--test-date` option to mozperftest that lets us specify the date that we are testing on. This lets us trigger multi-commit tests on past dates, and also ensures that retriggers of the multi-commit tests use the same day that they were pushed on (giving us reproducibility).
    
    Differential Revision: https://phabricator.services.mozilla.com/D80173

--- taskcluster/taskgraph/transforms/perftest.py
+++ taskcluster/taskgraph/transforms/perftest.py
@@ -6,6 +6,7 @@ This transform passes options from `mach perftest` to the corresponding task.
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+from datetime import date, timedelta
 import json
 
 import six
@@ -22,4 +23,13 @@ def pass_perftest_options(config, jobs):
         env['PERFTEST_OPTIONS'] = six.ensure_text(
             json.dumps(config.params["try_task_config"].get('perftest-options'))
         )
+        yield job
+
+
+@transforms.add
+def setup_perftest_test_date(config, jobs):
+    for job in jobs:
+        if "--test-date" not in job["run"]["command"]:
+            job["run"]["command"] += " --test-date %s" % \
+                (date.today() - timedelta(1)).strftime("%Y.%m.%d")
         yield job
