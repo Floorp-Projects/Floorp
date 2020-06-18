8f097fcddef85ec39d4bc48ae8fec0f70f3a25da
Jim Porter <jporter@mozilla.com>
2020-06-18 03:59:54 +0000

    Bug 1631806 - [part 1] Update perftests to use new API and fix `--proxy` option; r=tarek
    This patch fixes the tests in testing/performance/ that hadn't been updated to
    the new API, as well as fixing a couple issues with the `--proxy` option:
    
     * `policy.json` wasn't being logged correctly, since mozlog was treating the
       data as a format string
    
     * Proxy prefs weren't being set correctly for the profile
    
    Differential Revision: https://phabricator.services.mozilla.com/D79937

--- testing/performance/perftest_politico_link.js
+++ testing/performance/perftest_politico_link.js
@@ -1,4 +1,4 @@
-module.exports = async function(context, commands) {
+async function test(context, commands) {
   let rootUrl = "https://www.politico.com/";
 
   await commands.navigate(rootUrl);
@@ -14,4 +14,11 @@ module.exports = async function(context, commands) {
 
   // Stop and collect the measurement
   await commands.measure.stop();
+}
+
+module.exports = {
+  test,
+  owner: "Performance Team",
+  name: "Politico Link",
+  description: "Measures time to load Politico homepage",
 };
