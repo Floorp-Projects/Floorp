3cdcbca508bc9a1d87f62a244a003a961e9c97cc
Kartikaya Gupta <kgupta@mozilla.com>
2020-06-18 11:54:01 +0000

    Bug 1646385 - Move SetVisualViewport goop from nsLayoutUtils to PresShell. r=tnikkel
    There's no need to put this in nsLayoutUtils since it's so closely bound
    to presShell anyway. So we move it from being a static nsLayoutUtils function
    that takes a PresShell pointer, to being a method on the PresShell itself.
    
    Two functional changes here:
    1. Don't run the MaybeReflowForInflationScreenSizeChange code unless the
       visual viewport actually changes
    2. Run the MaybeReflowForInflationScreenSizeChange if the visual viewport
       is reset.
    
    These functional changes can be logically thought of as "group the font-
    inflation reflow stuff together with the other visual-viewport-triggered
    reflow stuff".
    
    Depends on D80040
    
    Differential Revision: https://phabricator.services.mozilla.com/D80041

--- layout/base/PresShell.h
+++ layout/base/PresShell.h
@@ -1461,6 +1461,8 @@ class PresShell final : public nsStubDocumentObserver,
    */
   void MarkFixedFramesForReflow(IntrinsicDirty aIntrinsicDirty);
 
+  void MaybeReflowForInflationScreenSizeChange();
+
   // This function handles all the work after VisualViewportSize is set
   // or reset.
   void CompleteChangeToVisualViewportSize();
