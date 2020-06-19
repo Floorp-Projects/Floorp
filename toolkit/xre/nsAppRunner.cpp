80819515453597fcb120d7453117f99347c061ab
Aaron Klotz <aklotz@mozilla.com>
2020-06-19 00:40:18 +0000

    Bug 1646453: Add MSCOM channel hook to enable profiler markers for COM IPC; r=Jamie,gerald
    To intercept COM IPC, we provide an `IChannelHook` interface to
    `CoRegisterChannelHook`, which gives us notifications about COM IPC that we can
    use to insert profiler markers. Note that `IChannelHook` is not documented on
    MSDN, however it is defined in the SDK header files.
    
    When the profiler is available, once XPCOM is up:
    
    * If the profiler is active, we immediately register the channel hook;
    * Otherwise we register an observer and hold off on registering the hook until
      the profiler is started, at which point we register the hook and remove the
      observer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80053

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -105,6 +105,9 @@
 #  include "mozilla/WindowsProcessMitigations.h"
 #  include "mozilla/WinHeaderOnlyUtils.h"
 #  include "mozilla/mscom/ProcessRuntime.h"
+#  if defined(MOZ_GECKO_PROFILER)
+#    include "mozilla/mscom/ProfilerMarkers.h"
+#  endif  // defined(MOZ_GECKO_PROFILER)
 #  include "mozilla/widget/AudioSession.h"
 #  include "WinTokenUtils.h"
 
@@ -4256,7 +4259,11 @@ nsresult XREMain::XRE_mainRun() {
   dllServices->StartUntrustedModulesProcessor();
   auto dllServicesDisable =
       MakeScopeExit([&dllServices]() { dllServices->DisableFull(); });
-#endif  // defined(XP_WIN)
+
+#  if defined(MOZ_GECKO_PROFILER)
+  mozilla::mscom::InitProfilerMarkers();
+#  endif  // defined(MOZ_GECKO_PROFILER)
+#endif    // defined(XP_WIN)
 
 #ifdef NS_FUNCTION_TIMER
   // initialize some common services, so we don't pay the cost for these at odd
