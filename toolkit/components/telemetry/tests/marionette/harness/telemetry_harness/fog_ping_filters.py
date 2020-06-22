292becaeadc7d9880065847f728d72e2d6d28895
Raphael Pierzina <rpierzina@mozilla.com>
2020-06-22 14:18:57 +0000

    Bug 1632790 - Develop new tt(c) test for the FOG deletion-request ping; r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D80426

---
+++ toolkit/components/telemetry/tests/marionette/harness/telemetry_harness/fog_ping_filters.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+class FOGPingFilter(object):
+    """Ping filter that accepts any FOG pings."""
+
+    def __call__(self, ping):
+        return True
+
+
+class FOGDocTypePingFilter(FOGPingFilter):
+    """Ping filter that accepts FOG pings that match the doc-type."""
+
+    def __init__(self, doc_type):
+        super(FOGDocTypePingFilter, self).__init__()
+        self.doc_type = doc_type
+
+    def __call__(self, ping):
+        if not super(FOGDocTypePingFilter, self).__call__(ping):
+            return False
+
+        # Verify that the given ping was submitted to the URL for the doc_type
+        return ping["request_url"]["doc_type"] == self.doc_type
+
+
+FOG_DELETION_REQUEST_PING = FOGDocTypePingFilter("deletion-request")
