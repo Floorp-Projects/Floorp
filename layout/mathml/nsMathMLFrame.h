b9b9e38dd9e5bdd28718b8a946b1cceb20478b75
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-22 09:45:40 +0000

    Bug 1646224 - Use mozilla::Length rather than nscoord to store font sizes. r=heycam,jfkthame
    This avoids arbitrary precision loss when computing REM units and so on,
    which is particularly important if we ever change the base of our app
    units (but useful regardless).
    
    Differential Revision: https://phabricator.services.mozilla.com/D79928

--- layout/mathml/nsMathMLFrame.h
+++ layout/mathml/nsMathMLFrame.h
@@ -238,7 +238,7 @@ class nsMathMLFrame : public nsIMathMLFrame {
     // should switch to this API in order to scale with changes of TextZoom
     emHeight = fm->EmHeight();
 #else
-    emHeight = NSToCoordRound(float(fm->Font().size));
+    emHeight = fm->Font().size.ToAppUnits();
 #endif
   }
 
