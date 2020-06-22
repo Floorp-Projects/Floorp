e20012c014b0376d1b20e9eb50c0c5d42bafb30f
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-06-22 21:21:18 +0000

    Bug 1641370 - Add optional SPIR-V shader validation to WebGPU, via naga r=jgilbert
    This change attempts to parse the incoming SPIR-V shader modules with
    Naga SPIR-V front-end. It's not complete, but it returns an Error if it's unable to parse,
    in which case we just continue without the validation (for now).
    If it succeeds, we extract the reflection information from it, and use it for the pipeline.
    
    This is just a start. More states would need to be validated, and SPIR-V front-end needs more work.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77170

---
+++ third_party/rust/spirv_headers/lib.rs
@@ -0,0 +1,16 @@
+#![doc(html_root_url = "https://docs.rs/spirv_headers/1.4/")]
+
+//! The SPIR-V header.
+//!
+//! This crate contains Rust definitions of all SPIR-V structs, enums,
+//! and constants.
+//!
+//! The version of this crate is the version of SPIR-V it contains.
+
+#![allow(non_camel_case_types)]
+#![cfg_attr(rustfmt, rustfmt_skip)]
+
+use bitflags::bitflags;
+use num_traits;
+
+include!("autogen_spirv.rs");
