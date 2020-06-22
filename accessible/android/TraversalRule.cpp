3267d4f31fd094d44d26342f43114544e2e1c8b0
Eitan Isaacson <eitan@monotonous.org>
2020-06-22 22:03:58 +0000

    Bug 1639048 - Don't traverse into labels. r=Jamie
    Labels are often used as primary click targets for related inputs. If
    they are styled to certain bounds, but contain overflowing text for a11y
    with a color of "transparent", the synthesized click will happen outside
    the bounds of the label.
    
    If we skip text leafs with "color: transparent", we will skip labels
    altogether. Instead, lets land on labels when traversing, and ignore
    its subtree.
    
    Differential Revision: https://phabricator.services.mozilla.com/D77898

--- accessible/android/TraversalRule.cpp
+++ accessible/android/TraversalRule.cpp
@@ -250,6 +250,13 @@ uint16_t TraversalRule::DefaultMatch(Accessible* aAccessible) {
                nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
       }
       break;
+    case roles::LABEL:
+      if (IsFlatSubtree(aAccessible)) {
+        // Match if this is a label with text but no nested controls.
+        return nsIAccessibleTraversalRule::FILTER_MATCH |
+               nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
+      }
+      break;
     case roles::MENUITEM:
     case roles::LINK:
     case roles::PAGETAB:
