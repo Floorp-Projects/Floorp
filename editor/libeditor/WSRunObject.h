c499af6b6e33a39f21e17ac50f5ce46dea027bd3
Masayuki Nakano <masayuki@d-toybox.com>
2020-06-22 14:48:15 +0000

    Bug 1646296 - part 8: Make stack only classes to group start/end boundary information of `WSRunScanner` r=m_kato
    Differential Revision: https://phabricator.services.mozilla.com/D79973

--- editor/libeditor/WSRunObject.h
+++ editor/libeditor/WSRunObject.h
@@ -379,62 +379,42 @@ class MOZ_STACK_CLASS WSRunScanner {
    * which is editable, but also may be non-element and/or non-editable.  See
    * MOZ_ASSERT_IF()s in WSScanResult::AssertIfInvalidData() for the detail.
    */
-  nsIContent* GetStartReasonContent() const { return mStartReasonContent; }
-  nsIContent* GetEndReasonContent() const { return mEndReasonContent; }
+  nsIContent* GetStartReasonContent() const {
+    return mStart.GetReasonContent();
+  }
+  nsIContent* GetEndReasonContent() const { return mEnd.GetReasonContent(); }
 
-  bool StartsFromNormalText() const {
-    return mStartReason == WSType::NormalText;
-  }
-  bool StartsFromSpecialContent() const {
-    return mStartReason == WSType::SpecialContent;
-  }
-  bool StartsFromBRElement() const { return mStartReason == WSType::BRElement; }
+  bool StartsFromNormalText() const { return mStart.IsNormalText(); }
+  bool StartsFromSpecialContent() const { return mStart.IsSpecialContent(); }
+  bool StartsFromBRElement() const { return mStart.IsBRElement(); }
   bool StartsFromCurrentBlockBoundary() const {
-    return mStartReason == WSType::CurrentBlockBoundary;
+    return mStart.IsCurrentBlockBoundary();
   }
   bool StartsFromOtherBlockElement() const {
-    return mStartReason == WSType::OtherBlockBoundary;
+    return mStart.IsOtherBlockBoundary();
   }
-  bool StartsFromBlockBoundary() const {
-    return mStartReason == WSType::CurrentBlockBoundary ||
-           mStartReason == WSType::OtherBlockBoundary;
-  }
-  bool StartsFromHardLineBreak() const {
-    return mStartReason == WSType::CurrentBlockBoundary ||
-           mStartReason == WSType::OtherBlockBoundary ||
-           mStartReason == WSType::BRElement;
-  }
-  bool EndsByNormalText() const { return mEndReason == WSType::NormalText; }
-  bool EndsBySpecialContent() const {
-    return mEndReason == WSType::SpecialContent;
-  }
-  bool EndsByBRElement() const { return mEndReason == WSType::BRElement; }
+  bool StartsFromBlockBoundary() const { return mStart.IsBlockBoundary(); }
+  bool StartsFromHardLineBreak() const { return mStart.IsHardLineBreak(); }
+  bool EndsByNormalText() const { return mEnd.IsNormalText(); }
+  bool EndsBySpecialContent() const { return mEnd.IsSpecialContent(); }
+  bool EndsByBRElement() const { return mEnd.IsBRElement(); }
   bool EndsByCurrentBlockBoundary() const {
-    return mEndReason == WSType::CurrentBlockBoundary;
+    return mEnd.IsCurrentBlockBoundary();
   }
-  bool EndsByOtherBlockElement() const {
-    return mEndReason == WSType::OtherBlockBoundary;
-  }
-  bool EndsByBlockBoundary() const {
-    return mEndReason == WSType::CurrentBlockBoundary ||
-           mEndReason == WSType::OtherBlockBoundary;
-  }
+  bool EndsByOtherBlockElement() const { return mEnd.IsOtherBlockBoundary(); }
+  bool EndsByBlockBoundary() const { return mEnd.IsBlockBoundary(); }
 
   MOZ_NEVER_INLINE_DEBUG dom::Element* StartReasonOtherBlockElementPtr() const {
-    MOZ_DIAGNOSTIC_ASSERT(mStartReasonContent->IsElement());
-    return mStartReasonContent->AsElement();
+    return mStart.OtherBlockElementPtr();
   }
   MOZ_NEVER_INLINE_DEBUG dom::HTMLBRElement* StartReasonBRElementPtr() const {
-    MOZ_DIAGNOSTIC_ASSERT(mStartReasonContent->IsHTMLElement(nsGkAtoms::br));
-    return static_cast<dom::HTMLBRElement*>(mStartReasonContent.get());
+    return mStart.BRElementPtr();
   }
   MOZ_NEVER_INLINE_DEBUG dom::Element* EndReasonOtherBlockElementPtr() const {
-    MOZ_DIAGNOSTIC_ASSERT(mEndReasonContent->IsElement());
-    return mEndReasonContent->AsElement();
+    return mEnd.OtherBlockElementPtr();
   }
   MOZ_NEVER_INLINE_DEBUG dom::HTMLBRElement* EndReasonBRElementPtr() const {
-    MOZ_DIAGNOSTIC_ASSERT(mEndReasonContent->IsHTMLElement(nsGkAtoms::br));
-    return static_cast<dom::HTMLBRElement*>(mEndReasonContent.get());
+    return mEnd.BRElementPtr();
   }
 
   /**
@@ -580,7 +560,7 @@ class MOZ_STACK_CLASS WSRunScanner {
    * GetInclusiveNextEditableCharPoint() returns aPoint if it points a character
    * in an editable text node, or start of next editable text node otherwise.
    * FYI: For the performance, this does not check whether given container
-   *      is not after mStartReasonContent or not.
+   *      is not after mStart.mReasonContent or not.
    */
   template <typename PT, typename CT>
   EditorDOMPointInText GetInclusiveNextEditableCharPoint(
@@ -591,7 +571,7 @@ class MOZ_STACK_CLASS WSRunScanner {
    * text node.  Note that this returns last character point when it meets
    * non-empty text node, otherwise, returns a point in an empty text node.
    * FYI: For the performance, this does not check whether given container
-   *      is not before mEndReasonContent or not.
+   *      is not before mEnd.mReasonContent or not.
    */
   template <typename PT, typename CT>
   EditorDOMPointInText GetPreviousEditableCharPoint(
@@ -632,6 +612,83 @@ class MOZ_STACK_CLASS WSRunScanner {
 
   char16_t GetCharAt(dom::Text* aTextNode, int32_t aOffset) const;
 
+  class MOZ_STACK_CLASS BoundaryData final {
+   public:
+    BoundaryData() : mReason(WSType::NotInitialized) {}
+    template <typename EditorDOMPointType>
+    BoundaryData(const EditorDOMPointType& aPoint, nsIContent& aReasonContent,
+                 WSType aReason)
+        : mReasonContent(&aReasonContent), mPoint(aPoint), mReason(aReason) {}
+    bool Initialized() const { return mReasonContent && mPoint.IsSet(); }
+
+    nsIContent* GetReasonContent() const { return mReasonContent; }
+    const EditorDOMPoint& PointRef() const { return mPoint; }
+    WSType RawReason() const { return mReason; }
+
+    bool IsNormalText() const { return mReason == WSType::NormalText; }
+    bool IsSpecialContent() const { return mReason == WSType::SpecialContent; }
+    bool IsBRElement() const { return mReason == WSType::BRElement; }
+    bool IsCurrentBlockBoundary() const {
+      return mReason == WSType::CurrentBlockBoundary;
+    }
+    bool IsOtherBlockBoundary() const {
+      return mReason == WSType::OtherBlockBoundary;
+    }
+    bool IsBlockBoundary() const {
+      return mReason == WSType::CurrentBlockBoundary ||
+             mReason == WSType::OtherBlockBoundary;
+    }
+    bool IsHardLineBreak() const {
+      return mReason == WSType::CurrentBlockBoundary ||
+             mReason == WSType::OtherBlockBoundary ||
+             mReason == WSType::BRElement;
+    }
+    MOZ_NEVER_INLINE_DEBUG dom::Element* OtherBlockElementPtr() const {
+      MOZ_DIAGNOSTIC_ASSERT(mReasonContent->IsElement());
+      return mReasonContent->AsElement();
+    }
+    MOZ_NEVER_INLINE_DEBUG dom::HTMLBRElement* BRElementPtr() const {
+      MOZ_DIAGNOSTIC_ASSERT(mReasonContent->IsHTMLElement(nsGkAtoms::br));
+      return static_cast<dom::HTMLBRElement*>(mReasonContent.get());
+    }
+
+   private:
+    nsCOMPtr<nsIContent> mReasonContent;
+    EditorDOMPoint mPoint;
+    // Must be one of WSType::NotInitialized, WSType::NormalText,
+    // WSType::SpecialContent, WSType::BRElement, WSType::CurrentBlockBoundary
+    // or WSType::OtherBlockBoundary.
+    WSType mReason;
+  };
+
+  class MOZ_STACK_CLASS NoBreakingSpaceData final {
+   public:
+    enum class Scanning { Forward, Backward };
+    void NotifyNBSP(const EditorDOMPointInText& aPoint,
+                    Scanning aScanningDirection) {
+      MOZ_ASSERT(aPoint.IsSetAndValid());
+      MOZ_ASSERT(aPoint.IsCharNBSP());
+      if (!mFirst.IsSet() || aScanningDirection == Scanning::Backward) {
+        mFirst = aPoint;
+      }
+      if (!mLast.IsSet() || aScanningDirection == Scanning::Forward) {
+        mLast = aPoint;
+      }
+    }
+
+    const EditorDOMPointInText& FirstPointRef() const { return mFirst; }
+    const EditorDOMPointInText& LastPointRef() const { return mLast; }
+
+    bool FoundNBSP() const {
+      MOZ_ASSERT(mFirst.IsSet() == mLast.IsSet());
+      return mFirst.IsSet();
+    }
+
+   private:
+    EditorDOMPointInText mFirst;
+    EditorDOMPointInText mLast;
+  };
+
   void GetRuns();
   void ClearRuns();
   void InitializeWithSingleFragment(
@@ -663,42 +720,19 @@ class MOZ_STACK_CLASS WSRunScanner {
   // true if we are in preformatted white-space context.
   bool mPRE;
 
-  // Node/offset where ws starts and ends.
-  nsCOMPtr<nsINode> mStartNode;
-  int32_t mStartOffset;
-
-  // Node/offset where ws ends.
-  nsCOMPtr<nsINode> mEndNode;
-  int32_t mEndOffset;
-
-  // Location of first nbsp in ws run, if any.
-  RefPtr<dom::Text> mFirstNBSPNode;
-  int32_t mFirstNBSPOffset;
-
-  // Location of last nbsp in ws run, if any.
-  RefPtr<dom::Text> mLastNBSPNode;
-  int32_t mLastNBSPOffset;
-
   // The first WSFragment in the run.
   WSFragment* mStartRun;
 
   // The last WSFragment in the run, may be same as first.
   WSFragment* mEndRun;
 
-  // See above comment for GetStartReasonContent() and GetEndReasonContent().
-  nsCOMPtr<nsIContent> mStartReasonContent;
-  nsCOMPtr<nsIContent> mEndReasonContent;
-
   // Non-owning.
   const HTMLEditor* mHTMLEditor;
 
- private:
-  // Must be one of WSType::NotInitialized, WSType::NormalText,
-  // WSType::SpecialContent, WSType::BRElement, WSType::CurrentBlockBoundary or
-  // WSType::OtherBlockBoundary.  Access these values with StartsFrom*() and
-  // EndsBy*() accessors.
-  WSType mStartReason;
-  WSType mEndReason;
+ protected:
+  BoundaryData mStart;
+  BoundaryData mEnd;
+  NoBreakingSpaceData mNBSPData;
 };
 
 class MOZ_STACK_CLASS WSRunObject final : public WSRunScanner {
@@ -895,13 +929,6 @@ class MOZ_STACK_CLASS WSRunObject final : public WSRunScanner {
       const WSFragment& aRun, const EditorDOMPoint& aPoint);
 
   MOZ_CAN_RUN_SCRIPT nsresult Scrub();
-
-  EditorDOMPoint StartPoint() const {
-    return EditorDOMPoint(mStartNode, mStartOffset);
-  }
-  EditorDOMPoint EndPoint() const {
-    return EditorDOMPoint(mEndNode, mEndOffset);
-  }
 
   // Because of MOZ_CAN_RUN_SCRIPT constructors, each instanciater of this class
   // guarantees the lifetime of the HTMLEditor.
