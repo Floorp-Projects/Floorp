057854d986033800dcc9af9a66eea46810ccebb2
Tom Tung <ttung@mozilla.com>
2020-06-22 16:17:35 +0000

    Bug 1645943 - A testcase to ensure temporary storage initialization is not blocked by a cache directory in an origin directory that ends with a period; r=janv,dom-workers-and-storage-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D80001

--- dom/quota/test/xpcshell/test_initTemporaryStorage.js
+++ dom/quota/test/xpcshell/test_initTemporaryStorage.js
@@ -6,12 +6,21 @@
 /**
  * This test is mainly to verify initTemporaryStorage() does call
  * QuotaManager::EnsureTemporaryStorageIsInitialized() which does various
- * things, for example, it restores the directory metadata if it's broken or
- * missing.
+ * things, for example,
+ *  - it restores the directory metadata if it's broken or missing.
+ *  - it isn't blocked by a cache directory in an origin that ends with period.
  */
 
 async function testSteps() {
-  const originDirPath = "storage/default/https+++foo.example.com";
+  const defaultRepositoryPath = "storage/default";
+  const originDirPaths = [
+    `${defaultRepositoryPath}/https+++foo.example.com`,
+    // Bug 1647316: We should have a test that checks all possible combinations
+    // that we can get during cache origin initialization. Once we have a test
+    // for that, we can revert this change.
+    `${defaultRepositoryPath}/https+++example.com.`,
+  ];
+
   const metadataFileName = ".metadata-v2";
 
   info("Initializing");
@@ -25,8 +34,20 @@ async function testSteps() {
 
   info("Creating an empty directory");
 
-  let originDir = getRelativeFile(originDirPath);
-  originDir.create(Ci.nsIFile.DIRECTORY_TYPE, parseInt("0755", 8));
+  let originDirs = [];
+  for (let originDirPath of originDirPaths) {
+    let originDir = getRelativeFile(originDirPath);
+    originDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o755);
+    originDirs.push(originDir);
+  }
+
+  info("Creating an empty cache directory for origin that ends with period");
+
+  let originDirPathEndsWithPeriod = originDirPaths.find(path =>
+    path.endsWith(".")
+  );
+  let cacheDir = getRelativeFile(`${originDirPathEndsWithPeriod}/cache`);
+  cacheDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0o755);
 
   info("Initializing the temporary storage");
 
@@ -38,10 +59,12 @@ async function testSteps() {
       "initTemporaryStorage()"
   );
 
-  let metadataFile = originDir.clone();
-  metadataFile.append(metadataFileName);
+  for (let originDir of originDirs) {
+    let metadataFile = originDir.clone();
+    metadataFile.append(metadataFileName);
 
-  ok(metadataFile.exists(), "Directory metadata file does exist");
+    ok(metadataFile.exists(), "Directory metadata file does exist");
+  }
 
   info("Verifying initialization status");
 
