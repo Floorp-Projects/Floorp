07d0e691d60bfac8fb5a2092da868c02d6912d9a
Tom Tung <ttung@mozilla.com>
2020-06-23 06:38:25 +0000

    Bug 1645943 - Check results from nsIFile's functions rather than mozStorage's/SQLite's in LockedGetPaddingSizeFromDB; r=janv,dom-workers-and-storage-reviewers,sg
    Differential Revision: https://phabricator.services.mozilla.com/D79959

--- dom/cache/QuotaClient.h
+++ dom/cache/QuotaClient.h
@@ -41,6 +41,8 @@ nsresult RestorePaddingFile(nsIFile* aBaseDir, mozIStorageConnection* aConn);
 
 nsresult WipePaddingFile(const QuotaInfo& aQuotaInfo, nsIFile* aBaseDir);
 
+extern const nsLiteralString kCachesSQLiteFilename;
+
 }  // namespace cache
 }  // namespace dom
 }  // namespace mozilla
