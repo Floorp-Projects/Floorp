0c310a904cae53db309914113619e8955fadbb95
Jon Coppeard <jcoppeard@mozilla.com>
2020-06-23 08:31:28 +0000

    Bug 1647319 - GCZones constructor doesn't need to take ZoneSelector parameter r=sfink
    There's only one place this is called with SkipAtoms and it's fine to include the atoms zone there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80485

--- js/src/gc/PrivateIterators-inl.h
+++ js/src/gc/PrivateIterators-inl.h
@@ -47,11 +47,10 @@ class GrayObjectIter : public ZoneAllCellIter<js::gc::TenuredCell> {
 };
 
 class GCZonesIter {
-  ZonesIter zone;
+  AllZonesIter zone;
 
  public:
-  explicit GCZonesIter(GCRuntime* gc, ZoneSelector selector = WithAtoms)
-      : zone(gc, selector) {
+  explicit GCZonesIter(GCRuntime* gc) : zone(gc) {
     MOZ_ASSERT(JS::RuntimeHeapIsBusy());
     MOZ_ASSERT_IF(gc->atomsZone->wasGCStarted(),
                   !gc->rt->hasHelperThreadZones());
@@ -60,8 +59,7 @@ class GCZonesIter {
       next();
     }
   }
-  explicit GCZonesIter(JSRuntime* rt, ZoneSelector selector = WithAtoms)
-      : GCZonesIter(&rt->gc, selector) {}
+  explicit GCZonesIter(JSRuntime* rt) : GCZonesIter(&rt->gc) {}
 
   bool done() const { return zone.done(); }
 
@@ -88,31 +86,20 @@ using GCRealmsIter = CompartmentsOrRealmsIterT<GCZonesIter, RealmsInZoneIter>;
 /* Iterates over all zones in the current sweep group. */
 class SweepGroupZonesIter {
   JS::Zone* current;
-  ZoneSelector selector;
 
  public:
-  explicit SweepGroupZonesIter(GCRuntime* gc, ZoneSelector selector = WithAtoms)
-      : selector(selector) {
+  explicit SweepGroupZonesIter(GCRuntime* gc)
+      : current(gc->getCurrentSweepGroup()) {
     MOZ_ASSERT(CurrentThreadIsPerformingGC());
-    current = gc->getCurrentSweepGroup();
-    maybeSkipAtomsZone();
   }
-  explicit SweepGroupZonesIter(JSRuntime* rt, ZoneSelector selector = WithAtoms)
-      : SweepGroupZonesIter(&rt->gc, selector) {}
-
-  void maybeSkipAtomsZone() {
-    if (selector == SkipAtoms && current && current->isAtomsZone()) {
-      current = current->nextNodeInGroup();
-      MOZ_ASSERT_IF(current, !current->isAtomsZone());
-    }
-  }
+  explicit SweepGroupZonesIter(JSRuntime* rt)
+      : SweepGroupZonesIter(&rt->gc) {}
 
   bool done() const { return !current; }
 
   void next() {
     MOZ_ASSERT(!done());
     current = current->nextNodeInGroup();
-    maybeSkipAtomsZone();
   }
 
   JS::Zone* get() const {
