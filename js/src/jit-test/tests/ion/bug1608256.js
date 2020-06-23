5ae67aafa6e93e79e35c082a32a650bf0b677459
Jan de Mooij <jdemooij@mozilla.com>
2020-06-23 08:16:57 +0000

    Bug 1608256 - Add test. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D59996

---
+++ js/src/jit-test/tests/ion/bug1608256.js
@@ -0,0 +1,13 @@
+// |jit-test| --no-threads; --baseline-warmup-threshold=1; --ion-full-warmup-threshold=1
+function g(obj, v) {
+    obj.prop = v;
+}
+function f() {
+    var obj = {prop: 2};
+    for (var j = 0; j < 20; j++) {}
+    for (var i = 0; i < 100; i++) {
+        g(/x/, 1);
+        g(obj, false);
+    }
+}
+f();
