c98df93b3ef9d3cced831ff3f7b6981506a05f98
Iain Ireland <iireland@mozilla.com>
2020-06-23 06:26:39 +0000

    Bug 1646378: Factor out ICScript from JitScript r=jandem
    When we clone a JitScript for inlining, there is some data that we want to share and some data that needs to be separate. This patch moves the separate data (warmup count, ICEntry array) into ICScript and embeds an ICScript into JitScript. The layout of a JitScript in memory is mostly unchanged by this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80196

--- js/src/jit/JitScript.cpp
+++ js/src/jit/JitScript.cpp
@@ -61,15 +61,14 @@ JitScript::JitScript(JSScript* script, Offset typeSetOffset,
     : profileString_(profileString),
       typeSetOffset_(typeSetOffset),
       bytecodeTypeMapOffset_(bytecodeTypeMapOffset),
-      endOffset_(endOffset) {
+      endOffset_(endOffset),
+      icScript_(this, script->getWarmUpCount(),
+                typeSetOffset - offsetOfICScript()) {
   setTypesGeneration(script->zone()->types.generation);
 
   if (IsTypeInferenceEnabled()) {
     initElements<StackTypeSet>(typeSetOffset, numTypeSets());
   }
-
-  // Initialize the warm-up count from the count stored in the script.
-  warmUpCount_ = script->getWarmUpCount();
 
   // Ensure the baselineScript_ and ionScript_ fields match the BaselineDisabled
   // and IonDisabled script flags.
@@ -121,6 +120,10 @@ bool JSScript::createJitScript(JSContext* cx) {
   static_assert(sizeof(StackTypeSet) % sizeof(uintptr_t) == 0,
                 "Trailing arrays must be aligned properly");
 
+  static_assert(
+      sizeof(JitScript) == offsetof(JitScript, icScript_) + sizeof(ICScript),
+      "icScript_ must be the last field");
+
   // Calculate allocation size.
   CheckedInt<uint32_t> allocSize = sizeof(JitScript);
   allocSize += CheckedInt<uint32_t>(numICEntries()) * sizeof(ICEntry);
@@ -155,8 +158,12 @@ bool JSScript::createJitScript(JSContext* cx) {
   auto prepareForDestruction = mozilla::MakeScopeExit(
       [&] { jitScript->prepareForDestruction(cx->zone()); });
 
-  if (!jitScript->initICEntriesAndBytecodeTypeMap(cx, this)) {
+  if (!jitScript->icScript()->initICEntries(cx, this)) {
     return false;
+  }
+
+  if (IsTypeInferenceEnabled()) {
+    jitScript->initBytecodeTypeMap(this);
   }
 
   prepareForDestruction.release();
@@ -235,6 +242,8 @@ void JitScript::CachedIonData::trace(JSTracer* trc) {
 }
 
 void JitScript::trace(JSTracer* trc) {
+  icScript_.trace(trc);
+
   if (hasBaselineScript()) {
     baselineScript()->trace(trc);
   }
@@ -246,7 +255,9 @@ void JitScript::trace(JSTracer* trc) {
   if (hasCachedIonData()) {
     cachedIonData().trace(trc);
   }
+}
 
+void ICScript::trace(JSTracer* trc) {
   // Mark all IC stub codes hanging off the IC stub entries.
   for (size_t i = 0; i < numICEntries(); i++) {
     ICEntry& ent = icEntry(i);
@@ -335,12 +346,12 @@ void JitScript::Destroy(Zone* zone, JitScript* script) {
 }
 
 struct ICEntries {
-  JitScript* const jitScript_;
+  ICScript* const icScript_;
 
-  explicit ICEntries(JitScript* jitScript) : jitScript_(jitScript) {}
+  explicit ICEntries(ICScript* icScript) : icScript_(icScript) {}
 
-  size_t numEntries() const { return jitScript_->numICEntries(); }
-  ICEntry& operator[](size_t index) const { return jitScript_->icEntry(index); }
+  size_t numEntries() const { return icScript_->numICEntries(); }
+  ICEntry& operator[](size_t index) const { return icScript_->icEntry(index); }
 };
 
 static bool ComputeBinarySearchMid(ICEntries entries, uint32_t pcOffset,
@@ -367,7 +378,7 @@ static bool ComputeBinarySearchMid(ICEntries entries, uint32_t pcOffset,
       loc);
 }
 
-ICEntry* JitScript::maybeICEntryFromPCOffset(uint32_t pcOffset) {
+ICEntry* ICScript::maybeICEntryFromPCOffset(uint32_t pcOffset) {
   // This method ignores prologue IC entries. There can be at most one
   // non-prologue IC per bytecode op.
 
@@ -384,14 +395,14 @@ ICEntry* JitScript::maybeICEntryFromPCOffset(uint32_t pcOffset) {
   return &entry;
 }
 
-ICEntry& JitScript::icEntryFromPCOffset(uint32_t pcOffset) {
+ICEntry& ICScript::icEntryFromPCOffset(uint32_t pcOffset) {
   ICEntry* entry = maybeICEntryFromPCOffset(pcOffset);
   MOZ_RELEASE_ASSERT(entry);
   return *entry;
 }
 
-ICEntry* JitScript::maybeICEntryFromPCOffset(uint32_t pcOffset,
-                                             ICEntry* prevLookedUpEntry) {
+ICEntry* ICScript::maybeICEntryFromPCOffset(uint32_t pcOffset,
+                                            ICEntry* prevLookedUpEntry) {
   // Do a linear forward search from the last queried PC offset, or fallback to
   // a binary search if the last offset is too far away.
   if (prevLookedUpEntry && pcOffset >= prevLookedUpEntry->pcOffset() &&
@@ -411,14 +422,14 @@ ICEntry* JitScript::maybeICEntryFromPCOffset(uint32_t pcOffset,
   return maybeICEntryFromPCOffset(pcOffset);
 }
 
-ICEntry& JitScript::icEntryFromPCOffset(uint32_t pcOffset,
-                                        ICEntry* prevLookedUpEntry) {
+ICEntry& ICScript::icEntryFromPCOffset(uint32_t pcOffset,
+                                       ICEntry* prevLookedUpEntry) {
   ICEntry* entry = maybeICEntryFromPCOffset(pcOffset, prevLookedUpEntry);
   MOZ_RELEASE_ASSERT(entry);
   return *entry;
 }
 
-ICEntry* JitScript::interpreterICEntryFromPCOffset(uint32_t pcOffset) {
+ICEntry* ICScript::interpreterICEntryFromPCOffset(uint32_t pcOffset) {
   // We have to return the entry to store in BaselineFrame::interpreterICEntry
   // when resuming in the Baseline Interpreter at pcOffset. The bytecode op at
   // pcOffset does not necessarily have an ICEntry, so we want to return the
@@ -775,4 +786,31 @@ void jit::MarkActiveJitScripts(Zone* zone) {
       MarkActiveJitScripts(cx, iter);
     }
   }
+}
+
+void JitScript::initBytecodeTypeMap(JSScript* script) {
+  MOZ_ASSERT(IsTypeInferenceEnabled());
+  MOZ_ASSERT(jit::IsBaselineInterpreterEnabled());
+  MOZ_ASSERT(numICEntries() == script->numICEntries());
+
+  // Index of the next bytecode type map entry to initialize.
+  uint32_t typeMapIndex = 0;
+  uint32_t* const typeMap = bytecodeTypeMap();
+
+  // For JOF_TYPESET ops: initialize bytecode type map entries.
+  for (BytecodeLocation loc : js::AllBytecodesIterable(script)) {
+    JSOp op = loc.getOp();
+    // Note: if the script is very large there will be more JOF_TYPESET ops
+    // than bytecode type sets. See JSScript::MaxBytecodeTypeSets.
+    if (BytecodeOpHasTypeSet(op) &&
+        typeMapIndex < JSScript::MaxBytecodeTypeSets) {
+      typeMap[typeMapIndex] = loc.bytecodeToOffset(script);
+      typeMapIndex++;
+    }
+  }
+  MOZ_ASSERT(typeMapIndex == script->numBytecodeTypeSets());
+}
+
+FallbackICStubSpace* ICScript::fallbackStubSpace() {
+  return jitScript_->fallbackStubSpace();
 }
