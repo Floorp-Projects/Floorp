c814d9098f8c668637bf1feec941e19c536b9df2
Jan de Mooij <jdemooij@mozilla.com>
2020-06-23 08:16:57 +0000

    Bug 1647054 - Fix a bogus assert in MDefinition::maybeMostRecentDefUse. r=iain
    Also rename to maybeMostRecentlyAddedDefUse to make things a bit clearer.
    The loop phi case here has no effect on the transpiler's use of this method.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80418

--- js/src/jit/MIR.h
+++ js/src/jit/MIR.h
@@ -784,11 +784,11 @@ class MDefinition : public MNode {
   // use's definition. Else returns nullptr.
   MDefinition* maybeSingleDefUse() const;
 
-  // Returns the most recent use (ignoring MResumePoints) for this MDefinition.
-  // Returns nullptr if there are no uses. Note that this relies on addUse
-  // adding new uses to the front of the list, and should only be called during
-  // MIR building (before optimization passes make changes to the uses).
-  MDefinition* maybeMostRecentDefUse() const;
+  // Returns the most recently added use (ignoring MResumePoints) for this
+  // MDefinition. Returns nullptr if there are no uses. Note that this relies on
+  // addUse adding new uses to the front of the list, and should only be called
+  // during MIR building (before optimization passes make changes to the uses).
+  MDefinition* maybeMostRecentlyAddedDefUse() const;
 
   void addUse(MUse* use) {
     MOZ_ASSERT(use->producer() == this);
