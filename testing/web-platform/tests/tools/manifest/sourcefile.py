b6f0855081ce31e33da740c4ef0f7b2ffc7e9822
James Graham <james@hoppipolla.co.uk>
2020-06-23 10:00:26 +0000

    Bug 1644395 - Marionette support for wpt print reftests, r=marionette-reviewers,hiro,maja_zf
    Render print reftests to a PDF using the printing machinary, and use
    pdf.js from gecko itself to convert the PDF to an image for pixel
    comparisons.
    
    For the surrounding reftest machinery, the main change here is that we
    convert reftests to work in terms of lists of images rather than a
    single image. For normal reftests we only have a single image in the
    list; for print reftests we can have several (one per page). This is
    implemented in terms of iterators to avoid unnecessary renders when
    the test fails on an earlier page.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79081

--- testing/web-platform/tests/tools/manifest/sourcefile.py
+++ testing/web-platform/tests/tools/manifest/sourcefile.py
@@ -585,6 +585,7 @@ class SourceFile(object):
         assert self.root is not None
         return self.root.findall(".//{http://www.w3.org/1999/xhtml}meta[@name='fuzzy']")
 
+
     @cached_property
     def fuzzy(self):
         # type: () -> Dict[Optional[Tuple[Text, Text, Text]], List[List[int]]]
