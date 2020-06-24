6c815b0bcf7ae7c3ec423463ee640afe4afc44cb
Shane Caraveo <scaraveo@mozilla.com>
2020-06-24 18:22:43 +0000

    Bug 1647935 cleanup test_updatecheck.js r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D80772

--- toolkit/mozapps/extensions/test/xpcshell/test_updatecheck.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_updatecheck.js
@@ -8,15 +8,17 @@ const { AddonUpdateChecker } = ChromeUtils.import(
   "resource://gre/modules/addons/AddonUpdateChecker.jsm"
 );
 
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1");
+
 var testserver = AddonTestUtils.createHttpServer({ hosts: ["example.com"] });
 
 testserver.registerDirectory("/data/", do_get_file("data"));
 
-function checkUpdates(aId, aUpdateFile) {
+function checkUpdates(aId) {
   return new Promise((resolve, reject) => {
     AddonUpdateChecker.checkForUpdates(
       aId,
-      `http://example.com/data/${aUpdateFile}`,
+      `http://example.com/data/test_updatecheck.json`,
       {
         onUpdateCheckComplete: resolve,
 
@@ -30,20 +32,9 @@ function checkUpdates(aId, aUpdateFile) {
   });
 }
 
-function run_test() {
-  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1");
-
-  run_next_test();
-}
-
-const UPDATE_FILE = "test_updatecheck.json";
-
 // Test that a basic update check returns the expected available updates
-add_task(async function() {
-  let updates = await checkUpdates(
-    "updatecheck1@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_basic_update() {
+  let updates = await checkUpdates("updatecheck1@tests.mozilla.org");
 
   equal(updates.length, 5);
   let update = await AddonUpdateChecker.getNewestCompatibleUpdate(updates);
@@ -59,127 +50,77 @@ add_task(async function() {
 /*
  * Tests that the security checks are applied correctly
  *
- * Test     signature   updateHash  updateLink   expected
- *--------------------------------------------------------
- * 2        absent      absent      http         fail
- * 3        broken      absent      http         fail
- * 4        correct     absent      http         no update
- * 5        correct     sha1        http         update
- * 6        corrent     absent      https        update
- * 7        corrent     sha1        https        update
- * 8        corrent     md2         http         no update
- * 9        corrent     md2         https        update
+ * Test     updateHash  updateLink   expected
+ *--------------------------------------------
+ * 4        absent      http         no update
+ * 5        sha1        http         update
+ * 6        absent      https        update
+ * 7        sha1        https        update
+ * 8        md2         http         no update
+ * 9        md2         https        update
  */
 
-add_task(async function() {
-  try {
-    await checkUpdates("test_bug378216_5@tests.mozilla.org", UPDATE_FILE);
-    throw new Error("Expected the update check to fail");
-  } catch (e) {}
-});
-
-add_task(async function() {
-  try {
-    await checkUpdates("test_bug378216_7@tests.mozilla.org", UPDATE_FILE);
-
-    throw new Error("Expected the update check to fail");
-  } catch (e) {}
-});
-
-add_task(async function() {
-  // Make sure that the JSON manifest is rejected when an update key is
-  // required, but perform the remaining tests which aren't expected to fail
-  // because of the update key, without requiring one for the JSON variant.
-
-  try {
-    await checkUpdates(
-      "test_bug378216_8@tests.mozilla.org",
-      "test_updatecheck.json"
-    );
-
-    throw new Error("Expected the update check to fail");
-  } catch (e) {}
-
-  let updates = await checkUpdates(
-    "test_bug378216_8@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_4() {
+  let updates = await checkUpdates("test_bug378216_8@tests.mozilla.org");
   equal(updates.length, 1);
   ok(!("updateURL" in updates[0]));
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_9@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_5() {
+  let updates = await checkUpdates("test_bug378216_9@tests.mozilla.org");
   equal(updates.length, 1);
   equal(updates[0].version, "2.0");
   ok("updateURL" in updates[0]);
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_10@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_6() {
+  let updates = await checkUpdates("test_bug378216_10@tests.mozilla.org");
   equal(updates.length, 1);
   equal(updates[0].version, "2.0");
   ok("updateURL" in updates[0]);
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_11@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_7() {
+  let updates = await checkUpdates("test_bug378216_11@tests.mozilla.org");
   equal(updates.length, 1);
   equal(updates[0].version, "2.0");
   ok("updateURL" in updates[0]);
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_12@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_8() {
+  let updates = await checkUpdates("test_bug378216_12@tests.mozilla.org");
   equal(updates.length, 1);
-  Assert.equal(false, "updateURL" in updates[0]);
+  ok(!("updateURL" in updates[0]));
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_13@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_9() {
+  let updates = await checkUpdates("test_bug378216_13@tests.mozilla.org");
   equal(updates.length, 1);
   equal(updates[0].version, "2.0");
   ok("updateURL" in updates[0]);
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "test_bug378216_14@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_no_update_data() {
+  let updates = await checkUpdates("test_bug378216_14@tests.mozilla.org");
   equal(updates.length, 0);
 });
 
-add_task(async function() {
-  try {
-    await checkUpdates("test_bug378216_15@tests.mozilla.org", UPDATE_FILE);
-
-    throw new Error("Update check should have failed");
-  } catch (e) {
-    equal(e.status, AddonManager.ERROR_PARSE_ERROR);
-  }
+add_task(async function test_invalid_json() {
+  await checkUpdates("test_bug378216_15@tests.mozilla.org")
+    .then(() => {
+      ok(false, "Expected the update check to fail");
+    })
+    .catch(e => {
+      equal(
+        e.status,
+        AddonManager.ERROR_PARSE_ERROR,
+        "expected AddonManager.ERROR_PARSE_ERROR"
+      );
+    });
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "ignore-compat@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_ignore_compat() {
+  let updates = await checkUpdates("ignore-compat@tests.mozilla.org");
   equal(updates.length, 3);
   let update = await AddonUpdateChecker.getNewestCompatibleUpdate(
     updates,
@@ -191,11 +132,8 @@ add_task(async function() {
   equal(update.version, 2);
 });
 
-add_task(async function() {
-  let updates = await checkUpdates(
-    "compat-strict-optin@tests.mozilla.org",
-    UPDATE_FILE
-  );
+add_task(async function test_strict_compat() {
+  let updates = await checkUpdates("compat-strict-optin@tests.mozilla.org");
   equal(updates.length, 1);
   let update = await AddonUpdateChecker.getNewestCompatibleUpdate(
     updates,
