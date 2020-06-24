9e81fc1287a064a647fa8de616f9dc80e4c29501
Chris H-C <chutten@mozilla.com>
2020-06-24 16:16:10 +0000

    Bug 1635255 - Add skeleton FOG IPC GTests r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D79743

--- toolkit/components/glean/gtest/TestFogIPC.cpp
+++ toolkit/components/glean/gtest/TestFogIPC.cpp
@@ -5,4 +5,43 @@
 #include "gtest/gtest.h"
 
 // NOTE: No ContentChild/Parent tests because it includes headers that aren't
-// present in GTest builds.
+// present in GTest builds (or something).
+
+#include "mozilla/FOGIPC.h"
+#include <functional>
+#include "mozilla/ipc/ByteBuf.h"
+#include "nsTArray.h"
+#include "nsXULAppAPI.h"
+
+using mozilla::ipc::ByteBuf;
+
+TEST(FOG, TestFlushFOGData)
+{
+  // A "It doesn't explode" test.
+  std::function<void(ByteBuf &&)> resolver;
+  mozilla::glean::FlushFOGData(std::move(resolver));
+}
+
+TEST(FOG, TestFlushAllChildData)
+{
+  std::function<void(const nsTArray<ByteBuf>&&)> resolver =
+      [](const nsTArray<ByteBuf>&& bufs) {
+        ASSERT_TRUE(bufs.Length() == 0)
+        << "Not expecting any bufs yet.";
+      };
+  mozilla::glean::FlushAllChildData(std::move(resolver));
+}
+
+TEST(FOG, FOGData)
+{
+  // Another "It doesn't explode" test.
+  ByteBuf buf;
+  mozilla::glean::FOGData(std::move(buf));
+}
+
+TEST(FOG, SendFOGData)
+{
+  ASSERT_EQ(XRE_GetProcessType(), GeckoProcessType_Default)
+      << "If we can run a test as a different process type, we can write a "
+         "test for this function.";
+}
