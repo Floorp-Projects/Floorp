4603176f101badf003e2ab0e7e3ea663f79cc741
Michael Kaply <mozilla@kaply.com>
2020-06-24 23:52:38 +0000

    Bug 1592658 - Policy should not override user's locale choice. r=jaws,zbraniecki
    Differential Revision: https://phabricator.services.mozilla.com/D79913

--- browser/components/enterprisepolicies/tests/xpcshell/test_requestedlocales.js
+++ browser/components/enterprisepolicies/tests/xpcshell/test_requestedlocales.js
@@ -3,6 +3,8 @@
 
 "use strict";
 
+var { setTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm");
+
 const REQ_LOC_CHANGE_EVENT = "intl:requested-locales-changed";
 
 function promiseLocaleChanged(requestedLocale) {
@@ -19,6 +21,27 @@ function promiseLocaleChanged(requestedLocale) {
       },
     };
     Services.obs.addObserver(localeObserver, REQ_LOC_CHANGE_EVENT);
+  });
+}
+
+function promiseLocaleNotChanged(requestedLocale) {
+  return new Promise(resolve => {
+    let localeObserver = {
+      observe(aSubject, aTopic, aData) {
+        switch (aTopic) {
+          case REQ_LOC_CHANGE_EVENT:
+            ok(false, "Locale should not change.");
+            Services.obs.removeObserver(localeObserver, REQ_LOC_CHANGE_EVENT);
+            resolve();
+        }
+      },
+    };
+    Services.obs.addObserver(localeObserver, REQ_LOC_CHANGE_EVENT);
+    /* eslint-disable mozilla/no-arbitrary-setTimeout */
+    setTimeout(function() {
+      Services.obs.removeObserver(localeObserver, REQ_LOC_CHANGE_EVENT);
+      resolve();
+    }, 100);
   });
 }
 
@@ -64,5 +87,31 @@ add_task(async function test_system_locale_string() {
     },
   });
   await localePromise;
+  Services.locale.requestedLocales = originalLocales;
+});
+
+add_task(async function test_user_requested_locale_change() {
+  let originalLocales = Services.locale.requestedLocales;
+  let localePromise = promiseLocaleChanged("fr");
+  await setupPolicyEngineWithJson({
+    policies: {
+      RequestedLocales: "fr",
+    },
+  });
+  await localePromise;
+
+  // Simulate user change of locale
+  localePromise = promiseLocaleChanged("de");
+  Services.locale.requestedLocales = ["de"];
+  await localePromise;
+
+  localePromise = promiseLocaleNotChanged("fr");
+  await setupPolicyEngineWithJson({
+    policies: {
+      RequestedLocales: "fr",
+    },
+  });
+  await localePromise;
+
   Services.locale.requestedLocales = originalLocales;
 });
