8c5e9f40b463249a1a8b9f83f930e09399dcf52f
Chris H-C <chutten@mozilla.com>
2020-06-24 16:16:17 +0000

    Bug 1635255 - Build glean crate conditionally on Gecko symbols r=janerik
    Introduces a gkrust Cargo feature `glean_with_gecko` and `with_gecko` on fog
    and glean. This feature signifies the presence of gecko symbols in the build.
    
    Use this feature to implement needs_ipc() which asks Gecko which process type
    we're running as.
    
    Differential Revision: https://phabricator.services.mozilla.com/D79744

--- toolkit/components/glean/api/src/ipc.rs
+++ toolkit/components/glean/api/src/ipc.rs
@@ -8,6 +8,11 @@ use crate::metrics::CommonMetricData;
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use std::sync::Mutex;
+#[cfg(feature = "with_gecko")]
+use {
+    std::sync::atomic::{AtomicU32, Ordering},
+    xpcom::interfaces::nsIXULRuntime,
+};
 
 /// Contains all the information necessary to update the metrics on the main
 /// process.
@@ -44,6 +49,24 @@ where
 /// Do we need IPC?
 ///
 /// Thread-safe.
+#[cfg(feature = "with_gecko")]
+static PROCESS_TYPE: Lazy<AtomicU32> = Lazy::new(|| {
+    if let Some(appinfo) = xpcom::services::get_AppInfoService() {
+        let mut process_type = nsIXULRuntime::PROCESS_TYPE_DEFAULT as u32;
+        let rv = unsafe { appinfo.GetProcessType(&mut process_type) };
+        if rv.succeeded() {
+            return AtomicU32::new(process_type);
+        }
+    }
+    AtomicU32::new(nsIXULRuntime::PROCESS_TYPE_DEFAULT as u32)
+});
+
+#[cfg(feature = "with_gecko")]
+pub fn need_ipc() -> bool {
+    PROCESS_TYPE.load(Ordering::Relaxed) != nsIXULRuntime::PROCESS_TYPE_DEFAULT as u32
+}
+
+#[cfg(not(feature = "with_gecko"))]
 pub fn need_ipc() -> bool {
     false
 }
