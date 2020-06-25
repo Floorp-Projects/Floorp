5dc443249c14eecdfd76b4ad92ac25ff9f7a49f0
Emilio Cobos Álvarez <emilio@crisal.io>
2020-06-25 15:23:16 +0000

    Bug 1648334 - Use less unsafe in gecko_logger. r=froydnj,valentin
    We were storing LogModule::mName as an &'static str in the map... That's
    not fine.
    
    The Arc shenanigans were also more complicated than they need to be IMO.
    
    This uses a plain atomic bool to keep the fast path snappy.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81007

--- xpcom/rust/gecko_logger/src/lib.rs
+++ xpcom/rust/gecko_logger/src/lib.rs
@@ -4,21 +4,20 @@
 
 //! This provides a way to direct rust logging into the gecko logger.
 
-extern crate env_logger;
-extern crate log;
+#[macro_use]
+extern crate lazy_static;
+
 #[cfg(not(target_os = "android"))]
 use log::Log;
 use log::{Level, LevelFilter};
 use std::boxed::Box;
-use std::cmp;
 use std::collections::HashMap;
-use std::env;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::os::raw::c_int;
-use std::ptr;
-use std::sync::atomic::{AtomicPtr, Ordering};
-use std::sync::{Arc, RwLock};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::RwLock;
+use std::{cmp, env};
 
 extern "C" {
     fn ExternMozLog(tag: *const c_char, prio: c_int, text: *const c_char);
@@ -27,15 +26,16 @@ extern "C" {
     fn __android_log_write(prio: c_int, tag: *const c_char, text: *const c_char) -> c_int;
 }
 
-/// This is a static AtomicPtr that is normally null. When Gecko code
-/// sets the log level for a module that uses a rust logger, we will
-/// allocate this pointer and never change it.
-/// This optimizes the fast path when there are no active logging modules.
-/// That becomes only a null check. When the pointer is not null, we then
-/// proceed to read the hashmap and see if the module has an appropriate
-/// logging level.
-static LOG_MODULE_MAP: AtomicPtr<Arc<RwLock<HashMap<&str, (LevelFilter, bool)>>>> =
-    AtomicPtr::new(ptr::null_mut());
+lazy_static! {
+    // This could be a proper static once [1] is fixed or parking_lot's const fn
+    // support is not nightly-only.
+    //
+    // [1]: https://github.com/rust-lang/rust/issues/73714
+    static ref LOG_MODULE_MAP: RwLock<HashMap<String, (LevelFilter, bool)>> = RwLock::new(HashMap::new());
+}
+
+/// This tells us whether LOG_MODULE_MAP is possibly non-empty.
+static LOGGING_ACTIVE: AtomicBool = AtomicBool::new(false);
 
 /// This function searches for the module's name in the hashmap. If that is not
 /// found, it proceeds to search for the parent modules.
@@ -43,7 +43,7 @@ static LOG_MODULE_MAP: AtomicPtr<Arc<RwLock<HashMap<&str, (LevelFilter, bool)>>>
 /// was a pattern match, and the level we found in the hashmap.
 /// If none is found, it will return the module's name and LevelFilter::Off
 fn get_level_for_module<'a>(
-    map: &HashMap<&str, (LevelFilter, bool)>,
+    map: &HashMap<String, (LevelFilter, bool)>,
     key: &'a str,
 ) -> (&'a str, bool, LevelFilter) {
     if let Some((level, is_pattern_match)) = map.get(key) {
@@ -65,10 +65,7 @@ fn get_level_for_module<'a>(
 /// It returns true if the record was handled by Gecko's logging, and false
 /// otherwise.
 pub fn log_to_gecko(record: &log::Record) -> bool {
-    let module_map = LOG_MODULE_MAP.load(Ordering::Relaxed);
-
-    // No logging modules have been set.
-    if module_map.is_null() {
+    if !LOGGING_ACTIVE.load(Ordering::Relaxed) {
         return false;
     }
 
@@ -76,9 +73,9 @@ pub fn log_to_gecko(record: &log::Record) -> bool {
         Some(key) => key,
         None => return false,
     };
+
     let (mod_name, is_pattern_match, level) = {
-        let arc = Arc::clone(unsafe { &*module_map });
-        let map = arc.read().unwrap();
+        let map = LOG_MODULE_MAP.read().unwrap();
         get_level_for_module(&map, &key)
     };
 
@@ -132,50 +129,21 @@ pub extern "C" fn set_rust_log_level(module: *const c_char, level: u8) {
     };
 
     // This is the name of the rust module that we're trying to log in Gecko.
-    let mod_name_str = unsafe { CStr::from_ptr(module) }.to_str().unwrap();
+    let mut mod_name = unsafe { CStr::from_ptr(module) }
+        .to_string_lossy()
+        .into_owned();
 
-    // If this is a pattern, remove the last "::*" from it so we can search
-    // it in the hashmap
-    let (mod_name, is_pattern_match) = if mod_name_str.ends_with("::*") {
-        (&mod_name_str[..mod_name_str.len() - 3], true)
-    } else {
-        (&mod_name_str[..], false)
-    };
-    // When LOG_MODULE_MAP is null we will replace its value with a new map.
-    // If the AtomicPtr has already been initialized, we just drop `new_map`
-    // at the end of this function.
-    let mut map = HashMap::new();
-    // We insert to the map here, in case this is the first module and we can
-    // just replace the map pointer and exit without acquiring the write lock.
-    map.insert(mod_name, (rust_level, is_pattern_match));
-    let mut new_map = Box::new(Arc::new(RwLock::new(map)));
+    let is_pattern_match = mod_name.ends_with("::*");
 
-    // We only use the map when the first module is actually logging
-    let myptr = if rust_level == LevelFilter::Off {
-        ptr::null_mut()
-    } else {
-        new_map.as_mut()
-    };
-
-    // This will compare and swap the pointer. If the pointer is null, we will
-    // replace it with a pointer to `new_map`. After this the value of the
-    // pointer will never change. When we do replace the null pointer, we must
-    // then pass the ownership of `new_map` to LOG_MODULE_MAP.
-    let old_ptr = LOG_MODULE_MAP.compare_and_swap(ptr::null_mut(), myptr, Ordering::SeqCst);
-    if old_ptr.is_null() {
-        if !myptr.is_null() {
-            log::set_max_level(rust_level);
-
-            // Consume new_map so it doesn't get freed. LOG_MODULE_MAP is now
-            // the owner of that memory.
-            let _ = Box::into_raw(new_map);
-        }
-        return;
+    // If this is a pattern, remove the last "::*" from it so we can search it
+    // in the map.
+    if is_pattern_match {
+        let len = mod_name.len() - 3;
+        mod_name.truncate(len);
     }
 
-    // Insert the module into the hashmap.
-    let arc = Arc::clone(unsafe { &*old_ptr });
-    let mut map = arc.write().unwrap();
+    LOGGING_ACTIVE.store(true, Ordering::Relaxed);
+    let mut map = LOG_MODULE_MAP.write().unwrap();
     map.insert(mod_name, (rust_level, is_pattern_match));
 
     // Figure out the max level of all the modules.
@@ -218,11 +186,7 @@ impl GeckoLogger {
     }
 
     fn should_log_to_gfx_critical_note(record: &log::Record) -> bool {
-        if record.level() == log::Level::Error && record.target().contains("webrender") {
-            true
-        } else {
-            false
-        }
+        record.level() == log::Level::Error && record.target().contains("webrender")
     }
 
     fn maybe_log_to_gfx_critical_note(&self, record: &log::Record) {
