191b613fa59505f6b84abbb5024898c81b165f92
Ryan VanderMeulen <ryanvm@gmail.com>
2020-06-25 16:23:37 +0000

    Bug 1647861 - Upgrade to SQLite 3.32.3. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D80945

--- third_party/sqlite3/src/sqlite3.h
+++ third_party/sqlite3/src/sqlite3.h
@@ -123,9 +123,9 @@ extern "C" {
 ** [sqlite3_libversion_number()], [sqlite3_sourceid()],
 ** [sqlite_version()] and [sqlite_source_id()].
 */
-#define SQLITE_VERSION        "3.32.1"
-#define SQLITE_VERSION_NUMBER 3032001
-#define SQLITE_SOURCE_ID      "2020-05-25 16:19:56 0c1fcf4711a2e66c813aed38cf41cd3e2123ee8eb6db98118086764c4ba83350"
+#define SQLITE_VERSION        "3.32.3"
+#define SQLITE_VERSION_NUMBER 3032003
+#define SQLITE_SOURCE_ID      "2020-06-18 14:00:33 7ebdfa80be8e8e73324b8d66b3460222eb74c7e9dfd655b48d6ca7e1933cc8fd"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
