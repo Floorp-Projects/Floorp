900f5a17cfd970f9988bad7e1cf49f2ccd29fb50
Jan de Mooij <jdemooij@mozilla.com>
2020-06-25 15:00:46 +0000

    Bug 1648005 part 5 - Fix a test. r=iain
    The first try-catch in the test now terminates control flow after the previous patch
    and we then ilooped because inIon() never returned true in the loops after that
    (these loops are only reachable through the catch-block).
    
    The patch splits the test in a few different functions to fix this.
    
    Depends on D81029
    
    Differential Revision: https://phabricator.services.mozilla.com/D81030

--- js/src/jit-test/tests/basic/bug1292858.js
+++ js/src/jit-test/tests/basic/bug1292858.js
@@ -2,50 +2,59 @@ setJitCompilerOption("ion.warmup.trigger", 50);
 setJitCompilerOption("offthread-compilation.enable", 0);
 gcPreserveCode();
 
-var caughtInvalidArguments = false;
-var a = -1
-try {
-    var buf = new Uint8ClampedArray(a);
-    throw new Error("didn't throw");
-} catch (e) {
-    assertEq(e instanceof RangeError, true,
-             "expected RangeError, instead threw: " + e);
-    caughtInvalidArguments = true;
-}
-assertEq(caughtInvalidArguments, true);
-
-var caughtInvalidArguments = false;
-var i = 0;
-while (true) {
-    i = (i + 1) | 0;
-    var a = inIon() ? -1 : 300;
+function test1() {
+    var caughtInvalidArguments = false;
+    var a = -1
     try {
         var buf = new Uint8ClampedArray(a);
-        assertEq(buf.length, 300);
+        throw new Error("didn't throw");
     } catch (e) {
-        assertEq(a, -1);
         assertEq(e instanceof RangeError, true,
                 "expected RangeError, instead threw: " + e);
         caughtInvalidArguments = true;
-        break;
     }
+    assertEq(caughtInvalidArguments, true);
 }
-assertEq(caughtInvalidArguments, true);
+test1();
 
-var caughtInvalidArguments = false;
-var i = 0;
-while (true) {
-    i = (i + 1) | 0;
-    var a = inIon() ? -1 : 0;
-    try {
-        var buf = new Uint8ClampedArray(a);
-        assertEq(buf.length, 0);
-    } catch (e) {
-        assertEq(a, -1);
-        assertEq(e instanceof RangeError, true,
-                "expected RangeError, instead threw: " + e);
-        caughtInvalidArguments = true;
-        break;
+function test2() {
+    var caughtInvalidArguments = false;
+    var i = 0;
+    while (true) {
+        i = (i + 1) | 0;
+        var a = inIon() ? -1 : 300;
+        try {
+            var buf = new Uint8ClampedArray(a);
+            assertEq(buf.length, 300);
+        } catch (e) {
+            assertEq(a, -1);
+            assertEq(e instanceof RangeError, true,
+                    "expected RangeError, instead threw: " + e);
+            caughtInvalidArguments = true;
+            break;
+        }
     }
+    assertEq(caughtInvalidArguments, true);
 }
-assertEq(caughtInvalidArguments, true);
+test2();
+
+function test3() {
+    var caughtInvalidArguments = false;
+    var i = 0;
+    while (true) {
+        i = (i + 1) | 0;
+        var a = inIon() ? -1 : 0;
+        try {
+            var buf = new Uint8ClampedArray(a);
+            assertEq(buf.length, 0);
+        } catch (e) {
+            assertEq(a, -1);
+            assertEq(e instanceof RangeError, true,
+                    "expected RangeError, instead threw: " + e);
+            caughtInvalidArguments = true;
+            break;
+        }
+    }
+    assertEq(caughtInvalidArguments, true);
+}
+test3();
