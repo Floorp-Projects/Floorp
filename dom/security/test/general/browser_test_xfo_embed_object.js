5d8bd8816e9e14194562ea45b5980f901b3f59b3
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2020-06-26 14:28:43 +0000

    Bug 1644076: Test XFO for type embed and object. r=smaug, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D81161

---
+++ dom/security/test/general/browser_test_xfo_embed_object.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const kTestPath = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "http://example.com"
+);
+const kTestXFOEmbedURI = kTestPath + "file_framing_xfo_embed.html";
+const kTestXFOObjectURI = kTestPath + "file_framing_xfo_object.html";
+
+const errorMessage = `The loading of “http://example.com/browser/dom/security/test/general/file_framing_xfo_embed_object.sjs” in a frame is denied by “X-Frame-Options“ directive set to “deny“`;
+
+let xfoBlocked = false;
+
+function onXFOMessage(msgObj) {
+  const message = msgObj.message;
+  const logLevel = msgObj.logLevel;
+
+  if (message.includes(errorMessage)) {
+    ok(true, "XFO error message logged");
+    xfoBlocked = true;
+  }
+}
+
+add_task(async function open_test_xfo_embed_blocked() {
+  xfoBlocked = false;
+  await BrowserTestUtils.withNewTab("about:blank", async function(browser) {
+    Services.console.registerListener(onXFOMessage);
+    BrowserTestUtils.loadURI(browser, kTestXFOEmbedURI);
+    await BrowserTestUtils.waitForCondition(() => xfoBlocked == true);
+    Services.console.unregisterListener(onXFOMessage);
+  });
+});
+
+add_task(async function open_test_xfo_object_blocked() {
+  xfoBlocked = false;
+  await BrowserTestUtils.withNewTab("about:blank", async function(browser) {
+    Services.console.registerListener(onXFOMessage);
+    BrowserTestUtils.loadURI(browser, kTestXFOObjectURI);
+    await BrowserTestUtils.waitForCondition(() => xfoBlocked == true);
+    Services.console.unregisterListener(onXFOMessage);
+  });
+});
