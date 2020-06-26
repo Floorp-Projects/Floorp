d3745901a1f78304a24f3986356f0ec7b76cfe22
Bob Owen <bobowencode@gmail.com>
2020-06-26 08:22:12 +0000

    Bug 1641722: Deactivate remote canvas 2D when device creation or stream read failure occurs. r=jrmuizel,chutten
    This also adds telemetry probes to track:
    * number of times remote canvas 2D is activated
    * number of times remote canvas 2D is deactivated due to canvas device creation failure
    * number of times remote canvas 2D is deactivated due to a stream read error.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81032

--- gfx/layers/ipc/CanvasChild.h
+++ gfx/layers/ipc/CanvasChild.h
@@ -29,7 +29,14 @@ class CanvasChild final : public PCanvasChild {
 
   explicit CanvasChild(Endpoint<PCanvasChild>&& aEndpoint);
 
+  /**
+   * @returns true if remote canvas has been deactivated due to failure.
+   */
+  static bool Deactivated() { return mDeactivated; }
+
   ipc::IPCResult RecvNotifyDeviceChanged();
+
+  ipc::IPCResult RecvDeactivate();
 
   /**
    * Ensures that the DrawEventRecorder has been created.
@@ -126,6 +133,8 @@ class CanvasChild final : public PCanvasChild {
   ~CanvasChild() final;
 
   static const uint32_t kCacheDataSurfaceThreshold = 10;
+
+  static bool mDeactivated;
 
   RefPtr<CanvasDrawEventRecorder> mRecorder;
   TextureType mTextureType = TextureType::Unknown;
