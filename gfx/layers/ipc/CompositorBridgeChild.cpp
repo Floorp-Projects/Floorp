1afe78d29d454e3cd69931170379f7f2db27c8e8
Dorel Luca <dluca@mozilla.com>
2020-06-26 13:26:54 +0300

    Backed out changeset 8570bd4a1f0b (bug 1641722) for Build bustage in telemetry/.deps/TelemetryScalarData.h. CLOSED TREE

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -949,11 +949,6 @@ PTextureChild* CompositorBridgeChild::CreateTexture(
 
 already_AddRefed<CanvasChild> CompositorBridgeChild::GetCanvasChild() {
   MOZ_ASSERT(gfx::gfxVars::RemoteCanvasEnabled());
-
-  if (CanvasChild::Deactivated()) {
-    return nullptr;
-  }
-
   if (!mCanvasChild) {
     ipc::Endpoint<PCanvasParent> parentEndpoint;
     ipc::Endpoint<PCanvasChild> childEndpoint;
