a20f1c126d2e75d7ed8ab793da595af3ca06687a
Marcos Cáceres <mcaceres@mozilla.com>
2020-06-26 20:01:28 +0000

    Bug 1591329 - Require secure context for getGamePads() and events r=kip
    Differential Revision: https://phabricator.services.mozilla.com/D80444

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -293,6 +293,8 @@ class Navigator final : public nsISupports, public nsWrapperCache {
   RefPtr<AddonManager> mAddonManager;
   RefPtr<webgpu::Instance> mWebGpu;
   RefPtr<Promise> mSharePromise;  // Web Share API related
+  // Gamepad moving to secure contexts
+  bool mGamepadSecureContextWarningShown = false;
 };
 
 }  // namespace dom
