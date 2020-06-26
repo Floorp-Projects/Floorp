c781c952ee22cdab487c8ddfcf0a378e1f1882bf
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-06-26 09:34:47 +0000

    Bug 1634846 - P2. Make ipc's MessageChannel works with TaskQueue, r=nika
    We no longer rely of having a message loop for the worker thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80655

--- tools/fuzzing/ipc/ProtocolFuzzer.cpp
+++ tools/fuzzing/ipc/ProtocolFuzzer.cpp
@@ -24,7 +24,7 @@ mozilla::dom::ContentParent* ProtocolFuzzerHelper::CreateContentParent(
     const nsAString& aRemoteType) {
   auto* cp = new mozilla::dom::ContentParent(aRemoteType);
   // TODO: this duplicates MessageChannel::Open
-  cp->GetIPCChannel()->mWorkerThread = PR_GetCurrentThread();
+  cp->GetIPCChannel()->mWorkerThread = GetCurrentSerialEventTarget();
   cp->GetIPCChannel()->mMonitor = new RefCountedMonitor();
   return cp;
 }
