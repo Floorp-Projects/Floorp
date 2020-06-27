da2ad890dd6d5e9706090d18e84d3a96406ca008
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-06-27 14:26:27 +0000

    Bug 1647525 - Use HasAnyStateBits() in nsBox r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D81226

--- layout/xul/nsBox.cpp
+++ layout/xul/nsBox.cpp
@@ -32,7 +32,7 @@ nsresult nsIFrame::BeginXULLayout(nsBoxLayoutState& aState) {
   // XXXldb Is this still needed?
   AddStateBits(NS_FRAME_HAS_DIRTY_CHILDREN);
 
-  if (GetStateBits() & NS_FRAME_IS_DIRTY) {
+  if (HasAnyStateBits(NS_FRAME_IS_DIRTY)) {
     // If the parent is dirty, all the children are dirty (ReflowInput
     // does this too).
     nsIFrame* box;
@@ -272,7 +272,7 @@ nsresult nsIFrame::SyncXULLayout(nsBoxLayoutState& aBoxLayoutState) {
   }
   */
 
-  if (GetStateBits() & NS_FRAME_IS_DIRTY) {
+  if (HasAnyStateBits(NS_FRAME_IS_DIRTY)) {
     XULRedraw(aBoxLayoutState);
   }
 
