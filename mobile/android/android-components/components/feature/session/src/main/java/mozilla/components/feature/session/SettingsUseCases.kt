bb05d034cddd15417368e067f022855111a14a82
Sebastian Kaspari <s.kaspari@gmail.com>
2020-06-29 13:22:56 +0200

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/7480: Migrate SettingsUseCases to use BrowserStore.

--- mobile/android/android-components/components/feature/session/src/main/java/mozilla/components/feature/session/SettingsUseCases.kt
+++ mobile/android/android-components/components/feature/session/src/main/java/mozilla/components/feature/session/SettingsUseCases.kt
@@ -4,7 +4,8 @@
 
 package mozilla.components.feature.session
 
-import mozilla.components.browser.session.SessionManager
+import mozilla.components.browser.state.state.BrowserState
+import mozilla.components.browser.state.store.BrowserStore
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.engine.EngineSession
 import mozilla.components.concept.engine.EngineSession.TrackingProtectionPolicy
@@ -14,71 +15,42 @@ import mozilla.components.concept.engine.Settings
  * Contains use cases related to engine [Settings].
  *
  * @param engine reference to the application's browser [Engine].
- * @param sessionManager the application's [SessionManager].*
+ * @param store the application's [BrowserStore].
  */
 class SettingsUseCases(
     engine: Engine,
-    sessionManager: SessionManager
+    store: BrowserStore
 ) {
-
-    /**
-     * Use case to update a setting and then change all
-     * active browsing sessions to use the new setting.
-     * @property engine reference to the application's browser [Engine].
-     * @property sessionManager the application's [SessionManager]. Used to query the active sessions.
-     */
-    abstract class UpdateSettingUseCase<T> internal constructor(
-        private val engine: Engine,
-        private val sessionManager: SessionManager
-    ) {
-
-        /**
-         * Updates the engine setting and all active sessions.
-         *
-         * @param value The new setting value
-         */
-        operator fun invoke(value: T) {
-            update(engine.settings, value)
-            with(sessionManager) {
-                sessions.forEach {
-                    getEngineSession(it)?.let { session -> forEachSession(session, value) }
-                }
-            }
-            engine.clearSpeculativeSession()
-        }
-
-        /**
-         * Called to update a [Settings] object using the value from the invoke call.
-         */
-        protected abstract fun update(settings: Settings, value: T)
-
-        /**
-         * Called to update an active session. Defaults to updating the session's [Settings] object.
-         */
-        protected open fun forEachSession(session: EngineSession, value: T) {
-            update(session.settings, value)
-        }
-    }
-
     /**
      * Updates the tracking protection policy to the given policy value when invoked.
      * All active sessions are automatically updated with the new policy.
      */
     class UpdateTrackingProtectionUseCase internal constructor(
-        engine: Engine,
-        sessionManager: SessionManager
-    ) : UpdateSettingUseCase<TrackingProtectionPolicy>(engine, sessionManager) {
+        private val engine: Engine,
+        private val store: BrowserStore
+    ) {
+        /**
+         * Updates the tracking protection policy for all current and future [EngineSession]
+         * instances.
+         */
+        operator fun invoke(policy: TrackingProtectionPolicy) {
+            engine.settings.trackingProtectionPolicy = policy
 
-        override fun update(settings: Settings, value: TrackingProtectionPolicy) {
-            settings.trackingProtectionPolicy = value
-        }
+            store.state.forEachEngineSession { engineSession ->
+                engineSession.enableTrackingProtection(policy)
+            }
 
-        override fun forEachSession(session: EngineSession, value: TrackingProtectionPolicy) {
-            session.enableTrackingProtection(value)
+            engine.clearSpeculativeSession()
         }
     }
 
     val updateTrackingProtection: UpdateTrackingProtectionUseCase by lazy {
-        UpdateTrackingProtectionUseCase(engine, sessionManager)
+        UpdateTrackingProtectionUseCase(engine, store)
     }
+}
+
+private fun BrowserState.forEachEngineSession(block: (EngineSession) -> Unit) {
+    (tabs + customTabs)
+        .mapNotNull { it.engineState.engineSession }
+        .map { block(it) }
 }
