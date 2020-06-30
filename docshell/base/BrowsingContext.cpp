a3712ffe832f174c18806131e067638ab0e3af56
Kris Maglione <maglione.k@gmail.com>
2020-06-30 22:19:56 +0000

    Bug 1646609: Don't assert when an inactive/OOP inner window tries to navigate an OOP BrowsingContext. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D81731

--- docshell/base/BrowsingContext.cpp
+++ docshell/base/BrowsingContext.cpp
@@ -1578,12 +1578,7 @@ nsresult BrowsingContext::LoadURI(nsDocShellLoadState* aLoadState,
             win->GetCurrentInnerWindow()->GetWindowGlobalChild()) {
       wgc->SendLoadURI(this, aLoadState, aSetNavigating);
     }
-  } else {
-    MOZ_DIAGNOSTIC_ASSERT(XRE_IsParentProcess());
-    if (!XRE_IsParentProcess()) {
-      return NS_ERROR_UNEXPECTED;
-    }
-
+  } else if (XRE_IsParentProcess()) {
     if (Canonical()->LoadInParent(aLoadState, aSetNavigating)) {
       return NS_OK;
     }
@@ -1616,6 +1611,13 @@ nsresult BrowsingContext::LoadURI(nsDocShellLoadState* aLoadState,
                    }
                  });
     }
+  } else {
+    MOZ_DIAGNOSTIC_ASSERT(sourceBC);
+    if (!sourceBC) {
+      return NS_ERROR_UNEXPECTED;
+    }
+    // If we're in a content process and the source BC is no longer in-process,
+    // just fail silently.
   }
   return NS_OK;
 }
