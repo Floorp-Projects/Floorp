bfec48442916d10eb3b52e8c55f3ab41ca9fea3a
Valentin Gosu <valentin.gosu@gmail.com>
2020-06-30 12:27:09 +0000

    Bug 1641222 - Follow CNAME/AliasForm chains r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D79264

---
+++ netwerk/test/unit_ipc/head_trr_clone.js
@@ -0,0 +1,8 @@
+/* import-globals-from ../unit/head_trr.js */
+
+var { NetUtil } = ChromeUtils.import("resource://gre/modules/NetUtil.jsm");
+var { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
+
+load("../unit/head_trr.js");
