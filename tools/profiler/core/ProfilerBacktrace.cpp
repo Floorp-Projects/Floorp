f7e415091074f889155178aef0d398f5702fdf0a
Gerald Squelart <gsquelart@mozilla.com>
2020-06-30 01:43:24 +0000

    Bug 1648972 - Fix profiler non-unified build - r=canaltinova
    Mostly missing includes, and missing namespace qualifiers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81451

--- tools/profiler/core/ProfilerBacktrace.cpp
+++ tools/profiler/core/ProfilerBacktrace.cpp
@@ -13,7 +13,7 @@
 
 ProfilerBacktrace::ProfilerBacktrace(
     const char* aName, int aThreadId,
-    UniquePtr<mozilla::ProfileChunkedBuffer> aProfileChunkedBuffer,
+    mozilla::UniquePtr<mozilla::ProfileChunkedBuffer> aProfileChunkedBuffer,
     mozilla::UniquePtr<ProfileBuffer> aProfileBuffer)
     : mName(strdup(aName)),
       mThreadId(aThreadId),
