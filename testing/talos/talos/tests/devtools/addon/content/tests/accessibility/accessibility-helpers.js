232c1169bec9097ed96df777f0aed4b0ee32004f
Yura Zenevich <yura.zenevich@gmail.com>
2020-06-30 10:26:19 +0000

    Bug 1602054 - add cold open DAMP test for the accessibility panel. r=perftest-reviewers,jdescottes,Bebe
    This is a brand new (and first) DAMP talos test for accessibility panel. "accessibility.cold-open" is similar to other cold open tests such as inspector one. It opens accessibility panel, waits for its UI to render and then closes the toolbox.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80512

---
+++ testing/talos/talos/tests/devtools/addon/content/tests/accessibility/accessibility-helpers.js
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const Services = require("Services");
+const { openToolboxAndLog } = require("../head");
+
+const INITIALIZED_EVENT = "Accessibility:Initialized";
+
+const PREF_ACCESSIBILITY_FORCE_DISABLED = "accessibility.force_disabled";
+
+exports.shutdownAccessibilityService = function() {
+  // Set PREF_ACCESSIBILITY_FORCE_DISABLED to 1 to force disable
+  // accessibility service. This is the only way to guarantee an immediate
+  // accessibility service shutdown in all processes. This also prevents
+  // accessibility service from starting up in the future.
+  Services.prefs.setIntPref(PREF_ACCESSIBILITY_FORCE_DISABLED, 1);
+  // Set PREF_ACCESSIBILITY_FORCE_DISABLED back to default value. This will
+  // not start accessibility service until the user activates it again. It
+  // simply ensures that accessibility service can start again (when value is
+  // below 1).
+  Services.prefs.clearUserPref(PREF_ACCESSIBILITY_FORCE_DISABLED);
+};
+
+exports.openAccessibilityAndLog = async function(label) {
+  await openToolboxAndLog(
+    `${label}.accessibility`,
+    "accessibility",
+    (toolbox, panel) => panel.panelWin.once(INITIALIZED_EVENT)
+  );
+};
