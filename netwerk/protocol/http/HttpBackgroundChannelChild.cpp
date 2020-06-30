f0b8d33e0b774f3750e4f899f4b69268247a792a
Junior Hsu <juhsu@mozilla.com>
2020-06-30 00:32:53 +0000

    Bug 1633935 - P3 send OS*R and ODA via pHttpChannel for multipart channel, r=mayhemer,necko-reviewers
    Depends on D76969
    
    Differential Revision: https://phabricator.services.mozilla.com/D76970

--- netwerk/protocol/http/HttpBackgroundChannelChild.cpp
+++ netwerk/protocol/http/HttpBackgroundChannelChild.cpp
@@ -91,11 +91,12 @@ bool HttpBackgroundChannelChild::ChannelClosed() {
   return !mChannelChild;
 }
 
-void HttpBackgroundChannelChild::OnStartRequestReceived() {
+void HttpBackgroundChannelChild::OnStartRequestReceived(
+    Maybe<uint32_t> aMultiPartID) {
   LOG(("HttpBackgroundChannelChild::OnStartRequestReceived [this=%p]\n", this));
   MOZ_ASSERT(OnSocketThread());
   MOZ_ASSERT(mChannelChild);
-  MOZ_ASSERT(!mStartReceived);  // Should only be called once.
+  MOZ_ASSERT(!mStartReceived || *aMultiPartID > 0);
 
   mStartReceived = true;
 
@@ -157,7 +158,7 @@ IPCResult HttpBackgroundChannelChild::RecvOnStartRequest(
                                        aRequestHeaders, aArgs);
   // Allow to queue other runnable since OnStartRequest Event already hits the
   // child's mEventQ.
-  OnStartRequestReceived();
+  OnStartRequestReceived(aArgs.multiPartID());
 
   return IPC_OK();
 }
