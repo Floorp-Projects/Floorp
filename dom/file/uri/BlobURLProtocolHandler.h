3b2ddeb1c30091c3bd3b0b713f71576f968f7b99
ssengupta <ssengupta@mozilla.com>
2020-07-02 13:38:26 +0000

    Bug 1619953 - P5 - BlobURLChannel allows loading blob data that is very recently revoked r=baku
    Previously similar logic existed in BlobURLProtocolHandler, which has now been removed, since such checks are now for parent process only and should be abstracted from BlobURLProtocolHandler.
    
    Depends on D75293
    
    Differential Revision: https://phabricator.services.mozilla.com/D81126

--- dom/file/uri/BlobURLProtocolHandler.h
+++ dom/file/uri/BlobURLProtocolHandler.h
@@ -59,7 +59,8 @@ class BlobURLProtocolHandler final : public nsIProtocolHandler,
 
   static bool HasDataEntry(const nsACString& aUri);
 
-  static nsIPrincipal* GetDataEntryPrincipal(const nsACString& aUri);
+  static nsIPrincipal* GetDataEntryPrincipal(const nsACString& aUri,
+                                             bool aAlsoIfRevoked = false);
   static void Traverse(const nsACString& aUri,
                        nsCycleCollectionTraversalCallback& aCallback);
 
@@ -105,7 +106,8 @@ extern nsresult NS_GetBlobForBlobURI(nsIURI* aURI,
                                      mozilla::dom::BlobImpl** aBlob);
 
 extern nsresult NS_GetBlobForBlobURISpec(const nsACString& aSpec,
-                                         mozilla::dom::BlobImpl** aBlob);
+                                         mozilla::dom::BlobImpl** aBlob,
+                                         bool aAlsoIfRevoked = false);
 
 extern nsresult NS_GetSourceForMediaSourceURI(
     nsIURI* aURI, mozilla::dom::MediaSource** aSource);
