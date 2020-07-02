c18b83fca47d5b6601e41dd88227047fd922b07e
Julian Descottes <jdescottes@mozilla.com>
2020-07-02 15:56:00 +0000

    Bug 1618059 - Extract the storage extension live reload test r=ochameau,davidwalsh
    The failure only occurs locally when I use an attached target for the test_panel_live_reload mochitest.
    And it only happens if we perform the `extension.upgrade` call during the test.
    Most of the time it seems linked to a "frameUpdated" event fired when the webextension is being updated.
    But even after commenting out the event, the test remains intermittent (albeit with a much lower frequency)
    
    A first option would be to expose a new API on the webextension descriptor front in order to create
    detached targets.
    
    But it seems that isolating the live_reload test in a dedicated file also fixes the intermittent.
    It makes the fix a bit obscure, and it probably means we won't look into the issue much furhter but
    I would prefer to avoid test-only APIs in the codebase.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81322

---
+++ devtools/server/tests/xpcshell/test_extension_storage_actor_upgrade.js
@@ -0,0 +1,124 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/**
+ * Note: this test used to be in test_extension_storage_actor.js, but seems to
+ * fail frequently as soon as we start auto-attaching targets.
+ * See Bug 1618059.
+ */
+
+const { ExtensionTestUtils } = ChromeUtils.import(
+  "resource://testing-common/ExtensionXPCShellUtils.jsm"
+);
+
+const {
+  createMissingIndexedDBDirs,
+  extensionScriptWithMessageListener,
+  getExtensionConfig,
+  openAddonStoragePanel,
+  shutdown,
+  startupExtension,
+} = require("resource://test/helper_test_extension_storage_actor.js");
+
+// Ignore rejection related to the storage.onChanged listener being removed while the extension context is being closed.
+const { PromiseTestUtils } = ChromeUtils.import(
+  "resource://testing-common/PromiseTestUtils.jsm"
+);
+PromiseTestUtils.whitelistRejectionsGlobally(/Message manager disconnected/);
+
+const { createAppInfo, promiseStartupManager } = AddonTestUtils;
+
+const EXTENSION_STORAGE_ENABLED_PREF =
+  "devtools.storage.extensionStorage.enabled";
+
+AddonTestUtils.init(this);
+createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "42");
+
+ExtensionTestUtils.init(this);
+
+// This storage actor is gated behind a pref, so make sure it is enabled first
+Services.prefs.setBoolPref(EXTENSION_STORAGE_ENABLED_PREF, true);
+registerCleanupFunction(() => {
+  Services.prefs.clearUserPref(EXTENSION_STORAGE_ENABLED_PREF);
+});
+
+add_task(async function setup() {
+  await promiseStartupManager();
+  const dir = createMissingIndexedDBDirs();
+
+  Assert.ok(
+    dir.exists(),
+    "Should have a 'storage/permanent' dir in the profile dir"
+  );
+});
+
+/**
+ * Test case: Bg page adds an item to storage. With storage panel open, reload extension.
+ * - Load extension with background page that adds a storage item on message.
+ * - Open the add-on storage panel.
+ * - With the storage panel still open, reload the extension.
+ * - The data in the storage panel should match the item added prior to reloading.
+ */
+add_task(async function test_panel_live_reload() {
+  const EXTENSION_ID = "test_panel_live_reload@xpcshell.mozilla.org";
+  let manifest = {
+    version: "1.0",
+    applications: {
+      gecko: {
+        id: EXTENSION_ID,
+      },
+    },
+  };
+
+  info("Loading extension version 1.0");
+  const extension = await startupExtension(
+    getExtensionConfig({
+      manifest,
+      background: extensionScriptWithMessageListener,
+    })
+  );
+
+  info("Waiting for message from test extension");
+  const host = await extension.awaitMessage("extension-origin");
+
+  info("Adding storage item");
+  extension.sendMessage("storage-local-set", { a: 123 });
+  await extension.awaitMessage("storage-local-set:done");
+
+  const { target, extensionStorage } = await openAddonStoragePanel(
+    extension.id
+  );
+
+  manifest = {
+    ...manifest,
+    version: "2.0",
+  };
+  // "Reload" is most similar to an upgrade, as e.g. storage data is preserved
+  info("Update to version 2.0");
+  await extension.upgrade(
+    getExtensionConfig({
+      manifest,
+      background: extensionScriptWithMessageListener,
+    })
+  );
+
+  await extension.awaitMessage("extension-origin");
+
+  const { data } = await extensionStorage.getStoreObjects(host);
+  Assert.deepEqual(
+    data,
+    [
+      {
+        area: "local",
+        name: "a",
+        value: { str: "123" },
+        isValueEditable: true,
+      },
+    ],
+    "Got the expected results on populated storage.local"
+  );
+
+  await shutdown(extension, target);
+});
