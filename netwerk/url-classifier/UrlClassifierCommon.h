f7f366a6d2c497f43371ee1bd54577a99744ac64
Dimi Lee <dlee@mozilla.com>
2020-07-02 07:20:42 +0000

    Bug 1437626 - P1. Use TYPE_DOCUMENT to determin whether a channel is a top-level load. r=baku
    We use whether channel->GetTopWindowURI returns a failure to determin
    if the channel is a top-level load. However, channels loaded by service
    worker or appcache don't have top-level window.
    
    This patch use TYPE_DOCUMENT to determin whether it is a top-level load.
    This patch also adds UrlClassifierCommon::GetTopWindowURI to print debug message
    because nsIChannel::GetTopWindowURI is not fission-compatible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80182

--- netwerk/url-classifier/UrlClassifierCommon.h
+++ netwerk/url-classifier/UrlClassifierCommon.h
@@ -86,6 +86,8 @@ class UrlClassifierCommon final {
  private:
   static uint32_t TableToClassificationFlag(
       const nsACString& aTable, const std::vector<ClassificationData>& aData);
+
+  static nsresult GetTopWindowURI(nsIChannel* aChannel, nsIURI** aURI);
 };
 
 }  // namespace net
