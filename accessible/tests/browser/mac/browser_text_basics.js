7b241c8028a5ce1b8df77395df91c81de303c180
Eitan Isaacson <eitan@monotonous.org>
2020-07-02 21:24:58 +0000

    Bug 1649575 - Part 6: Introduce browser test for basic text API. r=morgan.
    Differential Revision: https://phabricator.services.mozilla.com/D81762

---
+++ accessible/tests/browser/mac/browser_text_basics.js
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/* import-globals-from ../../mochitest/role.js */
+/* import-globals-from ../../mochitest/states.js */
+loadScripts(
+  { name: "role.js", dir: MOCHITESTS_DIR },
+  { name: "states.js", dir: MOCHITESTS_DIR }
+);
+
+// Read-only tests
+addAccessibleTask(`<p id="p">Hello World</p>`, async (browser, accDoc) => {
+  let macDoc = accDoc.nativeInterface.QueryInterface(
+    Ci.nsIAccessibleMacInterface
+  );
+
+  function stringForRange(r) {
+    return macDoc.getParameterizedAttributeValue(
+      "AXStringForTextMarkerRange",
+      r
+    );
+  }
+
+  let startMarker = macDoc.getAttributeValue("AXStartTextMarker");
+  let endMarker = macDoc.getAttributeValue("AXEndTextMarker");
+  let range = macDoc.getParameterizedAttributeValue(
+    "AXTextMarkerRangeForUnorderedTextMarkers",
+    [startMarker, endMarker]
+  );
+  is(stringForRange(range), "Hello World");
+});
