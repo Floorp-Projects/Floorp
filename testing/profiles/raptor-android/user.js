fe45d1955aeb874200dd255889d8e64224a1776c
alexandru.ionescu <alexandru.ionescu@softvision.ro>
2020-07-02 12:37:30 +0000

    Bug 1639148 Disable telemetry on weekly Fennec tests r=chutten,Dexter
    Differential Revision: https://phabricator.services.mozilla.com/D81307

--- testing/profiles/raptor-android/user.js
+++ testing/profiles/raptor-android/user.js
@@ -7,3 +7,5 @@ user_pref("browser.cache.disk.smart_size.enabled", true);
 // Raptor's manifest.json contains the "geckoProfiler" permission, which is
 // not supported on Android. Ignore the warning about the unknown permission.
 user_pref("extensions.webextensions.warnings-as-errors", false);
+// disable telemetry bug 1639148
+user_pref("toolkit.telemetry.server", "");
