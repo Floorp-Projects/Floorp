d4666b431ca4bd8f0238dd4c52487acd2cc35d3f
Keefer Rourke <krourke@mozilla.com>
2020-07-02 20:16:07 +0000

    Bug 1642454: Move PR_CloseDelete to dom/system r=barret,smaug
    The class PR_CloseDelete is a useful deleter for automatically closing NSPR File
    Descriptors when used with UniquePtr. This patch extracts this class from
    nsTerminator.cpp to dom/system so it may easily be used by new Gecko code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80719

--- toolkit/components/terminator/nsTerminator.cpp
+++ toolkit/components/terminator/nsTerminator.cpp
@@ -51,6 +51,7 @@
 #include "mozilla/Unused.h"
 #include "mozilla/Telemetry.h"
 
+#include "mozilla/dom/IOUtils.h"
 #include "mozilla/dom/workerinternals/RuntimeService.h"
 
 // Normally, the number of milliseconds that AsyncShutdown waits until
@@ -226,16 +227,6 @@ void RunWatchdog(void* arg) {
 // thread rather than usual XPCOM I/O simply because we outlive XPCOM and its
 // threads.
 //
-
-// Utility class, used by UniquePtr<> to close nspr files.
-class PR_CloseDelete {
- public:
-  constexpr PR_CloseDelete() = default;
-
-  PR_CloseDelete(const PR_CloseDelete& aOther) = default;
-
-  void operator()(PRFileDesc* aPtr) const { PR_Close(aPtr); }
-};
 
 //
 // Communication between the main thread and the writer thread.
