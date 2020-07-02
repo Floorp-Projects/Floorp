2c0ce40290052f86a0cff584c97196cb20f59787
Keefer Rourke <krourke@mozilla.com>
2020-07-02 20:16:07 +0000

    Bug 1642454: Move PR_CloseDelete to dom/system r=barret,smaug
    The class PR_CloseDelete is a useful deleter for automatically closing NSPR File
    Descriptors when used with UniquePtr. This patch extracts this class from
    nsTerminator.cpp to dom/system so it may easily be used by new Gecko code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80719

---
+++ dom/system/IOUtils.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_IOUtils__
+#define mozilla_dom_IOUtils__
+
+#include <prio.h>
+
+namespace mozilla {
+
+/**
+ * Utility class to be used with |UniquePtr| to automatically close NSPR file
+ * descriptors when they go out of scope.
+ *
+ * Example:
+ *
+ *   UniquePtr<PRFileDesc, PR_CloseDelete> fd = PR_Open(path, flags, mode);
+ */
+class PR_CloseDelete {
+ public:
+  constexpr PR_CloseDelete() = default;
+  PR_CloseDelete(const PR_CloseDelete& aOther) = default;
+
+  void operator()(PRFileDesc* aPtr) const { PR_Close(aPtr); }
+};
+
+}  // namespace mozilla
+
+#endif
