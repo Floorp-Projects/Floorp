181bd8139b232dcd440ae66c3d78f2034a9d6c44
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-07-03 01:16:36 +0300

    Backed out 4 changesets (bug 1642454) for failures on dom/system/tests/test_ioutils_worker.xhtml . CLOSED TREE
    Backed out changeset 13553531fc62 (bug 1642454)
    Backed out changeset 7896b4315920 (bug 1642454)
    Backed out changeset 1e68090eed0b (bug 1642454)
    Backed out changeset dfb201663480 (bug 1642454)

--- toolkit/components/osfile/tests/mochi/worker_test_osfile_comms.js
+++ toolkit/components/osfile/tests/mochi/worker_test_osfile_comms.js
@@ -2,11 +2,10 @@
  * http://creativecommons.org/publicdomain/zero/1.0/ */
 
 /* eslint-env mozilla/chrome-worker, node */
-/* global finish, log */
 
 "use strict";
 
-importScripts("chrome://mochikit/content/tests/SimpleTest/WorkerSimpleTest.js");
+importScripts("worker_test_osfile_shared.js");
 
 // The set of samples for communications test. Declare as a global
 // variable to prevent this from being garbage-collected too early.
