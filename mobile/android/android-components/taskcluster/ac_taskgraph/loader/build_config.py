ec8fef7e142c24bcf64663f7614f681c8acae0ce
Ben Hearsum <bhearsum@mozilla.com>
2020-07-02 15:47:25 -0400

    [components] prune unnecessary tasks from ci tasks

--- mobile/android/android-components/taskcluster/ac_taskgraph/loader/build_config.py
+++ mobile/android/android-components/taskcluster/ac_taskgraph/loader/build_config.py
@@ -4,16 +4,105 @@
 
 from __future__ import print_function, unicode_literals
 
+import logging
 import os
+import re
+import subprocess
 
 from copy import deepcopy
 from taskgraph.loader.transform import loader as base_loader
+from taskgraph.util.memoize import memoize
+from taskgraph.util.taskcluster import get_session
 from taskgraph.util.templates import merge
 
+from ..files_changes import get_files_changed_pr, get_files_changed_push
 from ..build_config import get_components
 
 
+logger = logging.getLogger(__name__)
+
+ALL_COMPONENTS = object()
+
+def get_components_changed(files_changed):
+    """Translate a list of files changed into a list of components. Eg:
+        [
+            "components/service/sync-logins/src/main/java/mozilla/components/service/sync/logins/GeckoLoginStorageDelegate.kt",
+            "components/service/sync-logins/src/test/java/mozilla/components/service/sync/logins/GeckoLoginStorageDelegateTest.kt",
+        ]
+        ->
+        {service-sync-logins}
+    """
+    return set(["-".join(f.split("/")[1:3]) for f in files_changed if f.startswith("components")])
+
+
+cached_deps = {}
+
+# Gradle is expensive to run. Cache the results for each component.
+@memoize
+def get_deps_for_component(component):
+    """Return the full list of local upstream dependencies of a component."""
+    deps = set()
+    # Parsing output like this is not ideal but bhearsum couldn't find a way
+    # to get the dependencies printed in a better format. If we could convince
+    # gradle to spit out JSON that would be much better.
+    # This is filed as https://github.com/mozilla-mobile/android-components/issues/7814
+    for line in subprocess.check_output(['./gradlew', '%s:dependencies' % component, '--configuration', 'implementation']).splitlines():
+        if line.startswith("+--- project"):
+            deps.add(line.split(" ")[2])
+
+    cached_deps[component] = deps
+    # So far, we only have the direct local upstream dependencies. We need to look at
+    # each of those to find _their_ upstreams as well.
+    for d in deps.copy():
+        deps.update(get_deps_for_component(d))
+
+    return deps
+
+def get_affected_components(files_changed, files_affecting_components):
+    affected_components = set()
+
+    # First, find the list of changed components
+    for c in get_components_changed(files_changed):
+        affected_components.add(c)
+
+    # Then, look for any other affected components based on the files changed.
+    for pattern, components in files_affecting_components.items():
+        if any([re.match(pattern, f) for f in files_changed]):
+            # Some file changes may necessitate rebuilding _all_ components.
+            # In this we can just return immediately.
+            if components == "all-components":
+                return ALL_COMPONENTS
+
+            affected_components.update(components)
+
+    # Finally, go through all of the affected components and recursively
+    # find their dependencies.
+    for c in affected_components.copy():
+        affected_components.update(get_deps_for_component(c))
+
+    return affected_components
+
+
 def loader(kind, path, config, params, loaded_tasks):
+    if params["tasks_for"] == "github-pull-request":
+        logger.info("Processing pull request %s" % params["pull_request_number"])
+        files_changed = get_files_changed_pr(params["base_repository"], params["pull_request_number"])
+        affected_components = get_affected_components(files_changed, config.get("files-affecting-components"))
+    elif params["tasks_for"] == "github-push":
+        logger.info("Processing push for commit range %s -> %s" % (params["base_rev"], params["head_rev"]))
+        files_changed = get_files_changed_push(params["base_repository"], params["base_rev"], params["head_rev"])
+        affected_components = get_affected_components(files_changed, config.get("files-affecting-components"))
+    # Unknown tasks_for, can't safely prune tasks
+    else:
+        files_changed = []
+        affected_components = ALL_COMPONENTS
+
+    logger.info("Files changed: %s" % " ".join(files_changed))
+    if affected_components is ALL_COMPONENTS:
+        logger.info("Affected components: ALL")
+    else:
+        logger.info("Affected components: %s" % " ".join(affected_components))
+
     not_for_components = config.get("not-for-components", [])
     jobs = {
         '{}{}'.format(
@@ -28,10 +117,14 @@ def loader(kind, path, config, params, loaded_tasks):
         for component in get_components()
         for build_type in ('regular', 'nightly', 'release')
         if (
-            component['name'] not in not_for_components
+            (affected_components is ALL_COMPONENTS or component['name'] in affected_components)
+            and component['name'] not in not_for_components
             and (component['shouldPublish'] or build_type == 'regular')
         )
     }
+    # Filter away overridden jobs that we wouldn't build anyways to avoid ending up with
+    # partial job entries.
+    overridden_jobs = {k: v for k, v in config.pop('overriden-jobs', {}).items() if affected_components is ALL_COMPONENTS or k in affected_components}
     jobs = merge(jobs, config.pop('overriden-jobs', {}))
 
     config['jobs'] = jobs
