4c4c78442d1b12f818ba35ec8438321e495ce072
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-07-02 00:26:41 +0000

    Bug 1634846 - P2. Make ipc's MessageChannel works with TaskQueue, r=nika
    We no longer rely of having a message loop for the worker thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D80655

--- ipc/glue/MessageChannel.h
+++ ipc/glue/MessageChannel.h
@@ -10,7 +10,6 @@
 
 #include "base/basictypes.h"
 #include "base/message_loop.h"
-
 #include "mozilla/Atomics.h"
 #include "mozilla/DebugOnly.h"
 #include "mozilla/Monitor.h"
@@ -19,16 +18,16 @@
 #if defined(OS_WIN)
 #  include "mozilla/ipc/Neutering.h"
 #endif  // defined(OS_WIN)
-#include "mozilla/ipc/Transport.h"
-#include "MessageLink.h"
-#include "nsThreadUtils.h"
+#include <math.h>
 
 #include <deque>
 #include <functional>
 #include <map>
-#include <math.h>
 #include <stack>
 #include <vector>
+
+#include "MessageLink.h"
+#include "mozilla/ipc/Transport.h"
 
 class nsIEventTarget;
 
@@ -97,7 +96,7 @@ enum ChannelState {
 
 class AutoEnterTransaction;
 
-class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
+class MessageChannel : HasResultCodes {
   friend class ProcessLink;
   friend class ThreadLink;
 #ifdef FUZZING
@@ -171,7 +170,7 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
   // For more details on the process of opening a channel between
   // threads, see the extended comment on this function
   // in MessageChannel.cpp.
-  bool Open(MessageChannel* aTargetChan, nsIEventTarget* aEventTarget,
+  bool Open(MessageChannel* aTargetChan, nsISerialEventTarget* aEventTarget,
             Side aSide);
 
   // "Open" a connection to an actor on the current thread.
@@ -376,8 +375,10 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
 #endif    // defined(OS_WIN)
 
  private:
-  void CommonThreadOpenInit(MessageChannel* aTargetChan, Side aSide);
-  void OpenAsOtherThread(MessageChannel* aTargetChan, Side aSide);
+  void CommonThreadOpenInit(MessageChannel* aTargetChan,
+                            nsISerialEventTarget* aThread, Side aSide);
+  void OpenAsOtherThread(MessageChannel* aTargetChan,
+                         nsISerialEventTarget* aThread, Side aSide);
 
   void PostErrorNotifyTask();
   void OnNotifyMaybeChannelError();
@@ -552,10 +553,9 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
   void NotifyMaybeChannelError();
 
  private:
-  // Can be run on either thread
   void AssertWorkerThread() const {
     MOZ_ASSERT(mWorkerThread, "Channel hasn't been opened yet");
-    MOZ_RELEASE_ASSERT(mWorkerThread == PR_GetCurrentThread(),
+    MOZ_RELEASE_ASSERT(mWorkerThread && mWorkerThread->IsOnCurrentThread(),
                        "not on worker thread!");
   }
 
@@ -573,7 +573,7 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
     // If we aren't a same-thread channel, our "link" thread is _not_ our
     // worker thread!
     MOZ_ASSERT(mWorkerThread, "Channel hasn't been opened yet");
-    MOZ_RELEASE_ASSERT(mWorkerThread != PR_GetCurrentThread(),
+    MOZ_RELEASE_ASSERT(mWorkerThread && !mWorkerThread->IsOnCurrentThread(),
                        "on worker thread but should not be!");
   }
 
@@ -617,8 +617,6 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
   typedef std::map<size_t, UniquePtr<UntypedCallbackHolder>> CallbackMap;
   typedef IPC::Message::msgid_t msgid_t;
 
-  void WillDestroyCurrentMessageLoop() override;
-
  private:
   // This will be a string literal, so lifetime is not an issue.
   const char* mName;
@@ -631,13 +629,11 @@ class MessageChannel : HasResultCodes, MessageLoop::DestructionObserver {
   Side mSide;
   bool mIsCrossProcess;
   UniquePtr<MessageLink> mLink;
-  MessageLoop* mWorkerLoop;  // thread where work is done
   RefPtr<CancelableRunnable>
       mChannelErrorTask;  // NotifyMaybeChannelError runnable
 
-  // Thread we are allowed to send and receive on. This persists even after
-  // mWorkerLoop is cleared during channel shutdown.
-  PRThread* mWorkerThread;
+  // Thread we are allowed to send and receive on.
+  nsCOMPtr<nsISerialEventTarget> mWorkerThread;
 
   // Timeout periods are broken up in two to prevent system suspension from
   // triggering an abort. This method (called by WaitForEvent with a 'did
