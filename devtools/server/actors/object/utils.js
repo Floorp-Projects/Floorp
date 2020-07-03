694873c8c34f03d0bd2d831a8fd6a56749ff3358
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-07-03 12:11:36 +0000

    Bug 1644186 - Move helper functions to helper files. r=ochameau.
    Functions to create object actors are put in helper files so they
    can be used by other consumers than the console messages watcher.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81490

--- devtools/server/actors/object/utils.js
+++ devtools/server/actors/object/utils.js
@@ -22,6 +22,20 @@ loader.lazyRequireGetter(
   true
 );
 
+loader.lazyRequireGetter(
+  this,
+  "ObjectActor",
+  "devtools/server/actors/object",
+  true
+);
+
+loader.lazyRequireGetter(
+  this,
+  "EnvironmentActor",
+  "devtools/server/actors/environment",
+  true
+);
+
 /**
  * Get thisDebugger.Object referent's `promiseState`.
  *
@@ -375,6 +389,139 @@ function getModifiersForEvent(rawObj) {
   return modifiers;
 }
 
+/**
+ * Make a debuggee value for the given value.
+ *
+ * @param TargetActor targetActor
+ *        The Target Actor from which this object originates.
+ * @param mixed value
+ *        The value you want to get a debuggee value for.
+ * @return object
+ *         Debuggee value for |value|.
+ */
+function makeDebuggeeValue(targetActor, value) {
+  if (isObject(value)) {
+    try {
+      const global = Cu.getGlobalForObject(value);
+      const dbgGlobal = targetActor.dbg.makeGlobalObjectReference(global);
+      return dbgGlobal.makeDebuggeeValue(value);
+    } catch (ex) {
+      // The above can throw an exception if value is not an actual object
+      // or 'Object in compartment marked as invisible to Debugger'
+    }
+  }
+  const dbgGlobal = targetActor.dbg.makeGlobalObjectReference(
+    targetActor.window
+  );
+  return dbgGlobal.makeDebuggeeValue(value);
+}
+
+function isObject(value) {
+  return Object(value) === value;
+}
+
+/**
+ * Create a grip for the given string.
+ *
+ * @param TargetActor targetActor
+ *        The Target Actor from which this object originates.
+ */
+function createStringGrip(targetActor, string) {
+  if (string && stringIsLong(string)) {
+    const actor = new LongStringActor(targetActor.conn, string);
+    targetActor.manage(actor);
+    return actor.form();
+  }
+  return string;
+}
+
+/**
+ * Create a grip for the given value.
+ *
+ * @param TargetActor targetActor
+ *        The Target Actor from which this object originates.
+ * @param mixed value
+ *        The value you want to get a debuggee value for.
+ * @param Number depth
+ *        Depth of the object compared to the top level object,
+ *        when we are inspecting nested attributes.
+ * @return object
+ */
+function createValueGripForTarget(targetActor, value, depth = 0) {
+  return createValueGrip(
+    value,
+    targetActor,
+    createObjectGrip.bind(null, targetActor, depth)
+  );
+}
+
+/**
+ * Create and return an environment actor that corresponds to the provided
+ * Debugger.Environment. This is a straightforward clone of the ThreadActor's
+ * method except that it stores the environment actor in the web console
+ * actor's pool.
+ *
+ * @param Debugger.Environment environment
+ *        The lexical environment we want to extract.
+ * @param TargetActor targetActor
+ *        The Target Actor to use as parent actor.
+ * @return The EnvironmentActor for |environment| or |undefined| for host
+ *         functions or functions scoped to a non-debuggee global.
+ */
+function createEnvironmentActor(environment, targetActor) {
+  if (!environment) {
+    return undefined;
+  }
+
+  if (environment.actor) {
+    return environment.actor;
+  }
+
+  const actor = new EnvironmentActor(environment, targetActor);
+  targetActor.manage(actor);
+  environment.actor = actor;
+
+  return actor;
+}
+
+/**
+ * Create a grip for the given object.
+ *
+ * @param TargetActor targetActor
+ *        The Target Actor from which this object originates.
+ * @param Number depth
+ *        Depth of the object compared to the top level object,
+ *        when we are inspecting nested attributes.
+ * @param object object
+ *        The object you want.
+ * @param object pool
+ *        A Pool where the new actor instance is added.
+ * @param object
+ *        The object grip.
+ */
+function createObjectGrip(targetActor, depth, object, pool) {
+  let gripDepth = depth;
+  const actor = new ObjectActor(
+    object,
+    {
+      thread: targetActor.threadActor,
+      getGripDepth: () => gripDepth,
+      incrementGripDepth: () => gripDepth++,
+      decrementGripDepth: () => gripDepth--,
+      createValueGrip: v => createValueGripForTarget(targetActor, v, gripDepth),
+      createEnvironmentActor: env => createEnvironmentActor(env, targetActor),
+      sources: () =>
+        DevToolsUtils.reportException(
+          "WebConsoleActor",
+          Error("sources not yet implemented")
+        ),
+    },
+    targetActor.conn
+  );
+  pool.manage(actor);
+  return actor.form();
+}
+
 module.exports = {
   getPromiseState,
   makeDebuggeeValueIfNeeded,
@@ -392,4 +539,7 @@ module.exports = {
   getSafeOwnPropertySymbols,
   getModifiersForEvent,
   isObjectOrFunction,
+  createStringGrip,
+  makeDebuggeeValue,
+  createValueGripForTarget,
 };
