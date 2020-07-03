c148adab440f3a0ec35a2477f30463c2809db7d2
Luca Greco <lgreco@mozilla.com>
2020-07-03 10:14:24 +0000

    Bug 1609920 - part 1: Allow the WebExtension Framework to register a moz-extension service worker. r=dom-workers-and-storage-reviewers,asuth,mixedpuppy
    - Adds the new about:config pref "extensions.backgroundServiceWorker.enabled" (currently defaults to false).
    - Adds the background.service_worker property to the manifest JSON schema definition
    - Locks background.service_worker manifest property behind the new preference
    - Adds a new BackgroundWorker class to ext-backgroundPage.js (responsible for managing the background
      service worker for the extension, e.g. make sure that the expected worker script is registered
      as expected when the extension is starting up)
    - Adds to the ServiceWorkerManager a new method to allow the WebExtension Framework to register the
      background service worker without an existing extension page
    - Allows the "moz-extension" schema in the dom/serviceworkers and dom/cache internals
    
    Depends on D63697
    
    Differential Revision: https://phabricator.services.mozilla.com/D60244

--- dom/serviceworkers/ServiceWorkerUtils.cpp
+++ dom/serviceworkers/ServiceWorkerUtils.cpp
@@ -79,9 +79,24 @@ void ServiceWorkerScopeAndScriptAreValid(const ClientInfo& aClientInfo,
     return;
   }
 
+  auto hasHTTPScheme = [](nsIURI* aURI) -> bool {
+    return aURI->SchemeIs("http") || aURI->SchemeIs("https");
+  };
+  auto hasMozExtScheme = [](nsIURI* aURI) -> bool {
+    return aURI->SchemeIs("moz-extension");
+  };
+
+  nsCOMPtr<nsIPrincipal> principal = principalOrErr.unwrap();
+
+  auto isExtension = !!BasePrincipal::Cast(principal)->AddonPolicy();
+  auto hasValidURISchemes = !isExtension ? hasHTTPScheme : hasMozExtScheme;
+
   // https://w3c.github.io/ServiceWorker/#start-register-algorithm step 3.
-  if (!aScriptURI->SchemeIs("http") && !aScriptURI->SchemeIs("https")) {
-    aRv.ThrowTypeError("Script URL's scheme is not 'http' or 'https'");
+  if (!hasValidURISchemes(aScriptURI)) {
+    auto message = !isExtension
+                       ? "Script URL's scheme is not 'http' or 'https'"_ns
+                       : "Script URL's scheme is not 'moz-extension'"_ns;
+    aRv.ThrowTypeError(message);
     return;
   }
 
@@ -92,8 +107,11 @@ void ServiceWorkerScopeAndScriptAreValid(const ClientInfo& aClientInfo,
   }
 
   // https://w3c.github.io/ServiceWorker/#start-register-algorithm step 8.
-  if (!aScopeURI->SchemeIs("http") && !aScopeURI->SchemeIs("https")) {
-    aRv.ThrowTypeError("Scope URL's scheme is not 'http' or 'https'");
+  if (!hasValidURISchemes(aScopeURI)) {
+    auto message = !isExtension
+                       ? "Scope URL's scheme is not 'http' or 'https'"_ns
+                       : "Scope URL's scheme is not 'moz-extension'"_ns;
+    aRv.ThrowTypeError(message);
     return;
   }
 
@@ -117,8 +135,6 @@ void ServiceWorkerScopeAndScriptAreValid(const ClientInfo& aClientInfo,
     aRv.ThrowSecurityError("Non-empty fragment on script URL");
     return;
   }
-
-  nsCOMPtr<nsIPrincipal> principal = principalOrErr.unwrap();
 
   // Unfortunately we don't seem to have an obvious window id here; in
   // particular ClientInfo does not have one.
