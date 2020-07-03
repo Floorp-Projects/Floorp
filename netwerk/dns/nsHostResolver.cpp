bcd41dfc7b7c35545930d18978a9f734172b365a
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-07-04 00:01:32 +0300

    Backed out changeset 16ff0a677ab2 (bug 1649127) for causing failures in nsHostResolver.cpp
    CLOSED TREE

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -1309,6 +1309,7 @@ nsresult nsHostResolver::TrrLookup(nsHostRecord* aRec, TRR* pushedTRR) {
   if (addrRec) {
     addrRec->mTRRSuccess = 0;  // bump for each successful TRR response
     addrRec->mTrrStart = TimeStamp::Now();
+    addrRec->mTRRUsed = true;  // this record gets TRR treatment
     addrRec->mTrrAUsed = AddrHostRecord::INIT;
     addrRec->mTrrAAAAUsed = AddrHostRecord::INIT;
 
@@ -1567,15 +1568,6 @@ nsresult nsHostResolver::NameLookup(nsHostRecord* rec) {
     if (!rec->IsAddrRecord()) {
       return rv;
     }
-
-#ifdef DEBUG
-    // If we use this branch then the mTRRUsed flag should not be set
-    // Even if we did call TrrLookup above, the fact that it failed sync-ly
-    // means that we didn't actually succeed in opening the channel.
-    RefPtr<AddrHostRecord> addrRec = do_QueryObject(rec);
-    MOZ_ASSERT(addrRec && !addrRec->mTRRUsed);
-#endif
-
     rv = NativeLookup(rec);
   }
 
