88cf9e7707725e6e3c3fda8f5de5ad162f1ed4fd
Valentin Gosu <valentin.gosu@gmail.com>
2020-07-04 15:39:45 +0000

    Bug 1649127 - Only trigger confirmation in TRR-first and TRR-only modes. r=dragana, a=RyanVM
    Due to a change in timing in this patch, when we reset the confirmation pref
    at the end of the test, a TRR request would happen after we changed the prefs
    leading to accessing a non-local IP in testing and causing a crash.
    This should be gated on being in the correct mode anyway
    
    Depends on D81517
    
    Differential Revision: https://phabricator.services.mozilla.com/D82222

--- netwerk/dns/TRRService.cpp
+++ netwerk/dns/TRRService.cpp
@@ -277,7 +277,8 @@ nsresult TRRService::ReadPrefs(const char* name) {
     nsAutoCString old(mConfirmationNS);
     Preferences::GetCString(TRR_PREF("confirmationNS"), mConfirmationNS);
     if (name && !old.IsEmpty() && !mConfirmationNS.Equals(old) &&
-        (mConfirmationState > CONFIRM_TRYING)) {
+        (mConfirmationState > CONFIRM_TRYING) &&
+        (mMode == MODE_TRRFIRST || mMode == MODE_TRRONLY)) {
       LOG(("TRR::ReadPrefs: restart confirmationNS state\n"));
       mConfirmationState = CONFIRM_TRYING;
       MaybeConfirm_locked();
