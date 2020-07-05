c87b19143b9c50f63d3776a3c467d5a760a01597
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-07-05 11:43:45 +0000

    Bug 1324591 - Add APZSampler::GetCompositionBounds to get the composition bounds for a given pair of LayersId and scrollId. r=botond
    Depends on D75730
    
    Differential Revision: https://phabricator.services.mozilla.com/D81481

--- gfx/layers/apz/public/APZSampler.h
+++ gfx/layers/apz/public/APZSampler.h
@@ -98,6 +98,14 @@ class APZSampler {
   void MarkAsyncTransformAppliedToContent(const LayerMetricsWrapper& aLayer);
   bool HasUnusedAsyncTransform(const LayerMetricsWrapper& aLayer);
 
+  /**
+   * Returns the composition bounds of the APZC correspoinding to the pair of
+   * |aLayersId| and |aScrollId|.
+   */
+  ParentLayerRect GetCompositionBounds(
+      const LayersId& aLayersId,
+      const ScrollableLayerGuid::ViewID& aScrollId) const;
+
   ScrollableLayerGuid GetGuid(const LayerMetricsWrapper& aLayer);
 
   ScreenMargin GetGeckoFixedLayerMargins() const;
