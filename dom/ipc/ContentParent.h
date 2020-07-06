970c9c00b808f77375ebf57c671b9370642398bb
Matt Woodrow <mwoodrow@mozilla.com>
2020-07-06 04:29:06 +0000

    Bug 1649879 - Don't create nsIURIFixupInfo in content process nsDocShellLoadState construction. r=kmag
    Rather than constructing an nsIURIFixupInfo from the IPC call return valuess, and then immediately querying the same data, this just use the results directly.
    
    It also moves the firing of "keyword-uri-fixup" observers to the parent process side. As far as I can tell, the only consumer was URIFixupChild, which was also forwarding them to the parent process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81944

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -1103,11 +1103,11 @@ class ContentParent final
                                            RefPtr<nsIInputStream>* aPostData,
                                            RefPtr<nsIURI>* aURI);
 
-  mozilla::ipc::IPCResult RecvGetFixupURIInfo(const nsCString& aURIString,
+  mozilla::ipc::IPCResult RecvGetFixupURIInfo(const nsString& aURIString,
                                               const uint32_t& aFixupFlags,
+                                              bool aAllowThirdPartyFixup,
                                               nsString* aProviderName,
                                               RefPtr<nsIInputStream>* aPostData,
-                                              RefPtr<nsIURI>* aFixedURI,
                                               RefPtr<nsIURI>* aPreferredURI);
 
   mozilla::ipc::IPCResult RecvNotifyKeywordSearchLoading(
