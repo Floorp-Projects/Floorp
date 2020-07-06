f79442a9a36365ef85d9667b3c3e7e83ef1372f9
Dana Keeler <dkeeler@mozilla.com>
2020-07-06 19:29:17 +0000

    Bug 1649518 - 1/3: revert a34e77d6c3bf in preparation of enabling osclientcerts by default r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D81888

--- browser/components/extensions/test/xpcshell/test_ext_pkcs11_management.js
+++ browser/components/extensions/test/xpcshell/test_ext_pkcs11_management.js
@@ -2,14 +2,25 @@
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   ctypes: "resource://gre/modules/ctypes.jsm",
+  MockRegistry: "resource://testing-common/MockRegistry.jsm",
+  OS: "resource://gre/modules/osfile.jsm",
 });
 
 do_get_profile();
 let tmpDir = FileUtils.getDir("TmpD", ["PKCS11"]);
+let slug =
+  AppConstants.platform === "linux" ? "pkcs11-modules" : "PKCS11Modules";
+tmpDir.createUnique(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+let baseDir = OS.Path.join(tmpDir.path, slug);
+OS.File.makeDir(baseDir);
 
 registerCleanupFunction(() => {
   tmpDir.remove(true);
 });
+
+function getPath(filename) {
+  return OS.Path.join(baseDir, filename);
+}
 
 const testmodule =
   "../../../../../security/manager/ssl/tests/unit/pkcs11testmodule/" +
@@ -17,6 +28,77 @@ const testmodule =
 
 // This function was inspired by the native messaging test under
 // toolkit/components/extensions
+
+async function setupManifests(modules) {
+  async function writeManifest(module) {
+    let manifest = {
+      name: module.name,
+      description: module.description,
+      path: module.path,
+      type: "pkcs11",
+      allowed_extensions: [module.id],
+    };
+
+    let manifestPath = getPath(`${module.name}.json`);
+    await OS.File.writeAtomic(manifestPath, JSON.stringify(manifest));
+
+    return manifestPath;
+  }
+
+  switch (AppConstants.platform) {
+    case "macosx":
+    case "linux":
+      let dirProvider = {
+        getFile(property) {
+          if (property == "XREUserNativeManifests") {
+            return tmpDir.clone();
+          } else if (property == "XRESysNativeManifests") {
+            return tmpDir.clone();
+          }
+          return null;
+        },
+      };
+
+      Services.dirsvc.registerProvider(dirProvider);
+      registerCleanupFunction(() => {
+        Services.dirsvc.unregisterProvider(dirProvider);
+      });
+
+      for (let module of modules) {
+        await writeManifest(module);
+      }
+      break;
+
+    case "win":
+      const REGKEY = String.raw`Software\Mozilla\PKCS11Modules`;
+
+      let registry = new MockRegistry();
+      registerCleanupFunction(() => {
+        registry.shutdown();
+      });
+
+      for (let module of modules) {
+        if (!OS.Path.winIsAbsolute(module.path)) {
+          let cwd = await OS.File.getCurrentDirectory();
+          module.path = OS.Path.join(cwd, module.path);
+        }
+        let manifestPath = await writeManifest(module);
+        registry.setValue(
+          Ci.nsIWindowsRegKey.ROOT_KEY_CURRENT_USER,
+          `${REGKEY}\\${module.name}`,
+          "",
+          manifestPath
+        );
+      }
+      break;
+
+    default:
+      ok(
+        false,
+        `Loading of PKCS#11 modules is not supported on ${AppConstants.platform}`
+      );
+  }
+}
 
 add_task(async function test_pkcs11() {
   async function background() {
@@ -109,7 +191,7 @@ add_task(async function test_pkcs11() {
     }
   }
 
-  await setupPKCS11Manifests(tmpDir, [
+  await setupManifests([
     {
       name: "testmodule",
       description: "PKCS#11 Test Module",
