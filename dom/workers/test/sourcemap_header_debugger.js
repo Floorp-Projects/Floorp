af5b174fa2812ef7e31ccc143b8dc7bc8597f78c
Logan Smyth <loganfsmyth@gmail.com>
2020-07-07 21:19:58 +0000

    Bug 780269 - Pass (X-)SourceMap header to SpiderMonkey for workers. r=perry
    Differential Revision: https://phabricator.services.mozilla.com/D70293

---
+++ dom/workers/test/sourcemap_header_debugger.js
@@ -0,0 +1,29 @@
+/**
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+"use strict";
+
+addEventListener("message", function(event) {
+  let data;
+  try {
+    data = JSON.parse(event.data);
+  } catch {}
+
+  switch (data.type) {
+    case "request-sourceMapURL":
+      const dbg = new Debugger(global);
+      const sourceMapURLs = dbg
+        .findSources()
+        .filter(source => source.url === data.url)
+        .map(source => source.sourceMapURL);
+
+      postMessage(
+        JSON.stringify({
+          type: "response-sourceMapURL",
+          value: sourceMapURLs,
+        })
+      );
+      break;
+  }
+});
