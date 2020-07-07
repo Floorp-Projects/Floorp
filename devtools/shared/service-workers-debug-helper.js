17460eb5c7dad2b8e6c0300d35333cf63fec941e
Julian Descottes <jdescottes@mozilla.com>
2020-07-07 15:20:10 +0000

    Bug 1619221 - Remove can-debug-sw-updated event r=ladybenko,daisuke,devtools-backward-compat-reviewers
    Depends on D81343
    
    All the client side consumers for the event have been removed.
    Since we don't support forward compatibility, we can simply remove the event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81344

--- devtools/shared/service-workers-debug-helper.js
+++ devtools/shared/service-workers-debug-helper.js
@@ -19,35 +19,7 @@ XPCOMUtils.defineLazyServiceGetter(
  * This depends both on the current multiprocess (e10s) configuration and on the
  * usage of the new service worker implementation
  * (dom.serviceWorkers.parent_intercept = true).
- *
- * Since this can be changed on the fly, there are subscribe/unsubscribe functions
- * to get notified of this.
  */
-
-const PROCESS_COUNT_PREF = "dom.ipc.processCount";
-const MULTI_OPTOUT_PREF = "dom.ipc.multiOptOut";
-
-function addDebugServiceWorkersListener(listener) {
-  // Some notes about these observers:
-  // - nsIPrefBranch.addObserver observes prefixes. In reality, watching
-  //   PROCESS_COUNT_PREF watches two separate prefs:
-  //   dom.ipc.processCount *and* dom.ipc.processCount.web. Because these
-  //   are the two ways that we control the number of content processes,
-  //   that works perfectly fine.
-  // - The user might opt in or out of multi by setting the multi opt out
-  //   pref. That affects whether we need to show our warning, so we need to
-  //   update our state when that pref changes.
-  // - In all cases, we don't have to manually check which pref changed to
-  //   what. The platform code in nsIXULRuntime.maxWebProcessCount does all
-  //   of that for us.
-  Services.prefs.addObserver(PROCESS_COUNT_PREF, listener);
-  Services.prefs.addObserver(MULTI_OPTOUT_PREF, listener);
-}
-
-function removeDebugServiceWorkersListener(listener) {
-  Services.prefs.removeObserver(PROCESS_COUNT_PREF, listener);
-  Services.prefs.removeObserver(MULTI_OPTOUT_PREF, listener);
-}
 
 function canDebugServiceWorkers() {
   const isE10s = Services.appinfo.browserTabsRemoteAutostart;
@@ -65,8 +37,6 @@ function isParentInterceptEnabled() {
 }
 
 module.exports = {
-  addDebugServiceWorkersListener,
   canDebugServiceWorkers,
   isParentInterceptEnabled,
-  removeDebugServiceWorkersListener,
 };
