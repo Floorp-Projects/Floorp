3a33785d382be38c50b8e94b0e0cb3e5463b252e
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-07-07 11:49:36 +0300

    Backed out changeset 9b15a67a50c3 (bug 1650834) as requested by Sylvestre.
    CLOSED TREE

--- tools/lint/python/pylint.py
+++ tools/lint/python/pylint.py
@@ -15,24 +15,20 @@ from mozlint.pathutils import expand_exclusions
 from mozlint.util import pip
 
 here = os.path.abspath(os.path.dirname(__file__))
-PYLINT_REQUIREMENTS_PATH = os.path.join(here, "pylint_requirements.txt")
+PYLINT_REQUIREMENTS_PATH = os.path.join(here, 'pylint_requirements.txt')
 
 PYLINT_NOT_FOUND = """
 Could not find pylint! Install pylint and try again.
 
     $ pip install -U --require-hashes -r {}
-""".strip().format(
-    PYLINT_REQUIREMENTS_PATH
-)
+""".strip().format(PYLINT_REQUIREMENTS_PATH)
 
 
 PYLINT_INSTALL_ERROR = """
 Unable to install correct version of pylint
 Try to install it manually with:
     $ pip install -U --require-hashes -r {}
-""".strip().format(
-    PYLINT_REQUIREMENTS_PATH
-)
+""".strip().format(PYLINT_REQUIREMENTS_PATH)
 
 
 class PylintProcess(ProcessHandler):
@@ -69,6 +65,9 @@ def run_process(config, cmd):
     return proc.output
 
 
+PYLINT_FORMAT_REGEX = re.compile(r'(.*):(.*): [(.*)] (.*)$')
+
+
 def parse_issues(log, config, issues_json, path):
     results = []
 
@@ -93,27 +92,25 @@ def parse_issues(log, config, issues_json, path):
 
 
 def lint(paths, config, **lintargs):
-    log = lintargs["log"]
+    log = lintargs['log']
 
     binary = get_pylint_binary()
 
-    log = lintargs["log"]
-    paths = list(expand_exclusions(paths, config, lintargs["root"]))
+    log = lintargs['log']
+    paths = list(expand_exclusions(paths, config, lintargs['root']))
 
     cmd_args = [binary]
     results = []
 
     # list from https://code.visualstudio.com/docs/python/linting#_pylint
     # And ignore a bit more elements
-    cmd_args += [
-        "-fjson",
-        "--disable=all",
-        "--enable=F,E,unreachable,duplicate-key,unnecessary-semicolon,global-variable-not-assigned,unused-variable,binary-op-exception,bad-format-string,anomalous-backslash-in-string,bad-open-mode",  # NOQA: E501
-        "--disable=import-error,no-member",
-    ]
+    cmd_args += ["-fjson",
+                 "--disable=all",
+                 "--enable=F,E,unreachable,duplicate-key,unnecessary-semicolon,global-variable-not-assigned,unused-variable,binary-op-exception,bad-format-string,anomalous-backslash-in-string,bad-open-mode",  # NOQA: E501
+                 "--disable=import-error,no-member"]
 
     base_command = cmd_args + paths
-    log.debug("Command: {}".format(" ".join(cmd_args)))
+    log.debug("Command: {}".format(' '.join(cmd_args)))
     output = " ".join(run_process(config, base_command))
     results = parse_issues(log, config, str(output), [])
 
