6d59d2fe60fd7d1b7c76500998497e217cb7a3bf
Matthew Gaudet <mgaudet@mozilla.com>
2020-07-07 18:53:04 +0000

    Bug 1642476 - Correctly name anonymous private functions bound to private fields. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D77930

---
+++ js/src/tests/non262/PrivateName/names.js
@@ -0,0 +1,31 @@
+// |reftest| skip-if(!xulRuntime.shell) shell-option(--enable-private-fields)
+
+var C = class {
+  static #field = () => 'Test262';
+  static field = () => 'Test262';
+  #instance = () => 'Test262';
+  instance = () => 'Test262';
+
+  static accessPrivateField() {
+    return this.#field;
+  }
+
+  accessPrivateInstanceField() {
+    return this.#instance;
+  }
+
+  static accessField() {
+    return this.field;
+  }
+
+  accessInstanceField() {
+    return this.instance;
+  }
+}
+assertEq(C.accessPrivateField().name, '#field')
+assertEq(C.accessField().name, 'field');
+var c = new C;
+assertEq(c.accessPrivateInstanceField().name, '#instance');
+assertEq(c.accessInstanceField().name, 'instance');
+
+if (typeof reportCompare === 'function') reportCompare(0, 0);
