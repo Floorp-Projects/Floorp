c7ca38a604e8bd035061ec5917c034282489ee91
julianwels <julianwels@mozilla.com>
2020-07-08 12:06:02 +0000

    Bug 1644456 - Manage HTTPS-Only Mode permission with SitePermissions interface. r=necko-reviewers,pbz,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D79427

--- browser/modules/test/unit/test_SitePermissions.js
+++ browser/modules/test/unit/test_SitePermissions.js
@@ -11,6 +11,9 @@ const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 const RESIST_FINGERPRINTING_ENABLED = Services.prefs.getBoolPref(
   "privacy.resistFingerprinting"
 );
+const HTTPS_ONLY_MODE_ENABLED = Services.prefs.getBoolPref(
+  "dom.security.https_only_mode"
+);
 const MIDI_ENABLED = Services.prefs.getBoolPref("dom.webmidi.enabled");
 
 add_task(async function testPermissionsListing() {
@@ -34,6 +37,10 @@ add_task(async function testPermissionsListing() {
     // Canvas permission should be hidden unless privacy.resistFingerprinting
     // is true.
     expectedPermissions.push("canvas");
+  }
+  if (HTTPS_ONLY_MODE_ENABLED) {
+    // Exception permission should be hidden unless HTTPS-Only Mode is enabled
+    expectedPermissions.push("https-only-load-insecure");
   }
   if (MIDI_ENABLED) {
     // Should remove this checking and add it as default after it is fully pref'd-on.
@@ -192,6 +199,10 @@ add_task(async function testExactHostMatch() {
     // Canvas permission should be hidden unless privacy.resistFingerprinting
     // is true.
     exactHostMatched.push("canvas");
+  }
+  if (HTTPS_ONLY_MODE_ENABLED) {
+    // Exception permission should be hidden unless HTTPS-Only Mode is enabled
+    exactHostMatched.push("https-only-load-insecure");
   }
   if (MIDI_ENABLED) {
     // WebMIDI is only pref'd on in nightly.
@@ -352,6 +363,52 @@ add_task(async function testCanvasPermission() {
     "privacy.resistFingerprinting",
     resistFingerprinting
   );
+});
+
+add_task(async function testHttpsOnlyPermission() {
+  let originalValue = Services.prefs.getBoolPref(
+    "dom.security.https_only_mode",
+    false
+  );
+  let principal = Services.scriptSecurityManager.createContentPrincipalFromOrigin(
+    "https://example.com"
+  );
+
+  SitePermissions.setForPrincipal(
+    principal,
+    "https-only-load-insecure",
+    SitePermissions.ALLOW
+  );
+
+  // Exception permission is hidden when HTTPS-Only Mode is disabled
+  Services.prefs.setBoolPref("dom.security.https_only_mode", false);
+  Assert.equal(
+    SitePermissions.listPermissions().indexOf("https-only-load-insecure"),
+    -1
+  );
+  Assert.equal(
+    SitePermissions.getAllByPrincipal(principal).filter(
+      permission => permission.id === "https-only-load-insecure"
+    ).length,
+    0
+  );
+
+  // Exception permission should show up when HTTPS-Only Mode is enabled
+  Services.prefs.setBoolPref("dom.security.https_only_mode", true);
+  Assert.notEqual(
+    SitePermissions.listPermissions().indexOf("https-only-load-insecure"),
+    -1
+  );
+  Assert.notEqual(
+    SitePermissions.getAllByPrincipal(principal).filter(
+      permission => permission.id === "https-only-load-insecure"
+    ).length,
+    0
+  );
+
+  // Reset everything
+  SitePermissions.removeFromPrincipal(principal, "https-only-load-insecure");
+  Services.prefs.setBoolPref("dom.security.https_only_mode", originalValue);
 });
 
 add_task(async function testFilePermissions() {
