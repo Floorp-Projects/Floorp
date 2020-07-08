5c759e35424094f518398c71da8d536664768b5e
Jim Porter <jporter@mozilla.com>
2020-07-08 13:41:24 +0000

    Bug 1631806 - [mozproxy] Add a command line interface r=tarek,Bebe
    Differential Revision: https://phabricator.services.mozilla.com/D76017

--- python/mozperftest/mozperftest/tests/test_proxy.py
+++ python/mozperftest/mozperftest/tests/test_proxy.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import mozunit
+import os
+import pytest
 
+from mozbuild.base import MozbuildObject
 from mozperftest.tests.support import get_running_env
 from mozperftest.environment import SYSTEM
-from mozperftest.utils import silence
+from mozperftest.utils import install_package, silence
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-def test_proxy():
+@pytest.fixture(scope="module")
+def install_mozproxy():
+    build = MozbuildObject.from_environment(cwd=here)
+    build.virtualenv_manager.activate()
+
+    mozbase = os.path.join(build.topsrcdir, "testing", "mozbase")
+    mozproxy_deps = ["mozinfo", "mozlog", "mozproxy"]
+    for i in mozproxy_deps:
+        install_package(build.virtualenv_manager, os.path.join(mozbase, i))
+    return build
+
+
+def test_proxy(install_mozproxy):
     mach_cmd, metadata, env = get_running_env(proxy=True)
     system = env.layers[SYSTEM]
 
