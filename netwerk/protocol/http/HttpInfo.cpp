697bfed788b431c43fab79b2a2547b8dabaa82a9
Kershaw Chang <kershaw@mozilla.com>
2020-07-08 10:29:01 +0000

    Bug 1527256 - P3: Get HttpData from socket process r=dragana
    Depends on D20355
    
    Differential Revision: https://phabricator.services.mozilla.com/D82320

--- netwerk/protocol/http/HttpInfo.cpp
+++ netwerk/protocol/http/HttpInfo.cpp
@@ -10,5 +10,7 @@
 
 void mozilla::net::HttpInfo::GetHttpConnectionData(
     nsTArray<HttpRetParams>* args) {
-  if (gHttpHandler) gHttpHandler->ConnMgr()->GetConnectionData(args);
+  if (gHttpHandler && gHttpHandler->ConnMgr()) {
+    gHttpHandler->ConnMgr()->GetConnectionData(args);
+  }
 }
