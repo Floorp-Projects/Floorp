ef6f52b8e92f0b81e9de33116947c04b863bf25a
Jeff Muizelaar <jmuizelaar@mozilla.com>
2020-07-08 19:13:37 +0000

    Bug 1651063 - Lower minimum Intel driver to 24.20.100.6286 for WebRender. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D82526

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -1806,8 +1806,8 @@ const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
         BatteryStatus::Present, DesktopEnvironment::All, WindowProtocol::All,
         DriverVendor::All, DeviceFamily::IntelModernRolloutWebRender,
         nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
-        DRIVER_GREATER_THAN_OR_EQUAL, V(25, 20, 100, 6373),
-        "FEATURE_ROLLOUT_BATTERY_INTEL_S_SCRN");
+        DRIVER_GREATER_THAN_OR_EQUAL, V(24, 20, 100, 6286),
+        "FEATURE_ROLLOUT_BATTERY_INTEL");
 
     APPEND_TO_DRIVER_BLOCKLIST2_EXT(
         OperatingSystem::Windows, ScreenSizeStatus::All, BatteryStatus::All,
