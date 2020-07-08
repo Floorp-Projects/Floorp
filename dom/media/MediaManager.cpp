55a82d9ff36bc45986efa882279ffd45e17a86f4
Paul Adenot <paul@paul.cx>
2020-07-08 09:07:48 +0000

    Bug 1650181 - Null-check when getting default output device info, because there can be no output device. r=achronop, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D82560

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -4450,7 +4450,8 @@ void SourceListener::SetEnabledFor(MediaTrack* aTrack, bool aEnable) {
                     RefPtr<AudioDeviceInfo> outputDevice =
                         enumerator->DefaultDevice(
                             CubebDeviceEnumerator::Side::OUTPUT);
-                    if (outputDevice->GroupID().Equals(inputDeviceGroupId)) {
+                    if (outputDevice &&
+                        outputDevice->GroupID().Equals(inputDeviceGroupId)) {
                       LOG("Device group id match when %s, "
                           "not turning the input device off (%s)",
                           aEnable ? "unmuting" : "muting",
