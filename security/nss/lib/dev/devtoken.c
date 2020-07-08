011c83f9c1b2fc691919b63f9682a651e67a7bf0
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-07-08 03:48:22 +0300

    Backed out changeset 68fb048ce9ee (bug 1650654) for bustages on devtoken.c . UPGRADE_NSS_RELEASE  CLOSED TREE

--- security/nss/lib/dev/devtoken.c
+++ security/nss/lib/dev/devtoken.c
@@ -1424,13 +1424,6 @@ NSS_IMPLEMENT PRBool
 nssToken_IsPresent(
     NSSToken *token)
 {
-#if defined(_MSC_VER)
-    if (!token) {
-        __debugbreak();
-        *((volatile int*)NULL) = __LINE__;
-        TerminateProcess(GetCurrentProcess(), 3);
-    }
-#endif
     return nssSlot_IsTokenPresent(token->slot);
 }
 
