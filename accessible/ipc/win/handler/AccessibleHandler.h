1129e41b5b7078102a55211d3fb0b0b8a5c20991
James Teh <jteh@mozilla.com>
2020-07-09 06:56:24 +0000

    Bug 1650590: A11y COM handler: Use CoTaskMemAlloc/Free for row/column header ids arrays when returned by IGeckoBackChannel::Refresh. r=MarcoZ, a=RyanVM
    DynamicIA2Data can be built to be transmitted in two different ways:
    
    1. As part of the payload included in the stream when an accessible is marshaled; or
    2. As an out parameter returned by IGeckoBackChannel::Refresh().
    
    DynamicIA2Data includes arrays for row/column header ids.
    Normally, such arrays would be allocated by CoTaskMemAlloc and freed by CoTaskMemFree.
    However, in the first case, the struct is actually marshaled by RPC encoding functions, not by COM itself.
    This means we must use midl_user_allocate/free, lest we crash.
    We previously used midl_user_allocate/free for the second case as well.
    Unfortunately, it turns out that this too causes crashes.
    
    To fix this, we now use different memory allocation functions depending on how the struct is transmitted.
    
    This patch also cleans up the old DynamicIA2Data in the client before calling IGeckoBackChannel::Refresh.
    Previously, we didn't do this, which would have resulted in a leak.
    
    Differential Revision: https://phabricator.services.mozilla.com/D82823

--- accessible/ipc/win/handler/AccessibleHandler.h
+++ accessible/ipc/win/handler/AccessibleHandler.h
@@ -280,6 +280,7 @@ class AccessibleHandler final : public mscom::Handler,
   IAccessibleTableCell* mIATableCellPassThru;   // weak
   IAccessibleHypertext2* mIAHypertextPassThru;  // weak
   IA2Payload mCachedData;
+  bool mCachedDynamicDataMarshaledByCom;
   UniquePtr<mscom::StructToStream> mSerializer;
   uint32_t mCacheGen;
   IAccessibleHyperlink** mCachedHyperlinks;
