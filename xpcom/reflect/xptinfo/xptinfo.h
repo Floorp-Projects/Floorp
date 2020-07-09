55b68bde088e419dfac3d3722c869e661b2290c8
Kris Maglione <maglione.k@gmail.com>
2020-07-09 17:59:09 +0000

    Bug 1464542: Part 1 - Generate symbolic names for XPT interfaces and add lookup function. r=nika,froydnj
    This makes it much simpler for the for the static JS services cache to store
    and lookup interface IDs for components.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81416

--- xpcom/reflect/xptinfo/xptinfo.h
+++ xpcom/reflect/xptinfo/xptinfo.h
@@ -21,6 +21,7 @@
 #include "js/Value.h"
 #include "nsString.h"
 #include "nsTArray.h"
+#include "xptdata.h"
 
 // Forward Declarations
 namespace mozilla {
@@ -67,6 +68,10 @@ struct nsXPTInterfaceInfo {
   }
   static const nsXPTInterfaceInfo* ByName(const char* aName) {
     return xpt::detail::InterfaceByName(aName);
+  }
+
+  static const nsXPTInterfaceInfo* Get(nsXPTInterface aID) {
+    return ByIndex(uint16_t(aID));
   }
 
   // These are only needed for Components_interfaces's enumerator.
