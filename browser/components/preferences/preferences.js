2022dab14afd20efd071a4e58e0923c9bab93ea5
Razvan Maries <rmaries@mozilla.com>
2020-07-09 15:04:47 +0300

    Backed out 2 changesets (bug 1650794) for perma failures on browser_masterpassword.js .CLOSED TREE
    Backed out changeset 5ffeb4bf7fa5 (bug 1650794)
    Backed out changeset 45b3d9917fb3 (bug 1650794)

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -3,6 +3,7 @@
    - You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // Import globals from the files imported by the .xul files.
+/* import-globals-from subdialogs.js */
 /* import-globals-from main.js */
 /* import-globals-from home.js */
 /* import-globals-from search.js */
@@ -28,39 +29,6 @@ ChromeUtils.defineModuleGetter(
   "formAutofillParent",
   "resource://formautofill/FormAutofillParent.jsm"
 );
-
-XPCOMUtils.defineLazyGetter(this, "gSubDialog", function() {
-  const { SubDialogManager } = ChromeUtils.import(
-    "resource://gre/modules/SubDialog.jsm"
-  );
-  return new SubDialogManager({
-    dialogStack: document.getElementById("dialogStack"),
-    dialogTemplate: document.getElementById("dialogTemplate"),
-    dialogOptions: {
-      styleSheets: ["chrome://browser/skin/preferences/preferences.css"],
-      resizeCallback: ({ title, frame }) => {
-        // Search within main document and highlight matched keyword.
-        gSearchResultsPane.searchWithinNode(title, gSearchResultsPane.query);
-
-        // Search within sub-dialog document and highlight matched keyword.
-        gSearchResultsPane.searchWithinNode(
-          frame.contentDocument.firstElementChild,
-          gSearchResultsPane.query
-        );
-
-        // Creating tooltips for all the instances found
-        for (let node of gSearchResultsPane.listSearchTooltips) {
-          if (!node.tooltipNode) {
-            gSearchResultsPane.createSearchTooltip(
-              node,
-              gSearchResultsPane.query
-            );
-          }
-        }
-      },
-    },
-  });
-});
 
 var gLastCategory = { category: undefined, subcategory: undefined };
 const gXULDOMParser = new DOMParser();
@@ -110,6 +78,7 @@ document.addEventListener("DOMContentLoaded", init_all, { once: true });
 function init_all() {
   Preferences.forceEnableInstantApply();
 
+  gSubDialog.init();
   register_module("paneGeneral", gMainPane);
   register_module("paneHome", gHomePane);
   register_module("paneSearch", gSearchPane);
