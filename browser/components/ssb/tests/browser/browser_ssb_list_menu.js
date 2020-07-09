cb57528ee23dea3ff52845f0685af3a82bfd4207
Emma Malysz <emalysz@mozilla.com>
2020-07-09 00:35:30 +0000

    Bug 1648640, remove character encoding panel view, panic view, SSB view, more view, and what's new panel view from main panel and place in template r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D81621

--- browser/components/ssb/tests/browser/browser_ssb_list_menu.js
+++ browser/components/ssb/tests/browser/browser_ssb_list_menu.js
@@ -6,26 +6,35 @@ add_task(async () => {
   let win = await BrowserTestUtils.openNewBrowserWindow();
 
   let button = win.document.getElementById("appMenu-ssb-button");
-  let panel = win.document.querySelector(
-    "#appMenu-SSBView .panel-subview-body"
-  );
 
   Assert.ok(button.hidden, "Button should be hidden.");
-  Assert.equal(panel.firstElementChild, null, "Should be nothing in the list.");
+  Assert.equal(
+    win.document.querySelector("#appMenu-SSBView .panel-subview-body"),
+    null,
+    "Panel should not be available"
+  );
 
   let ssb = await SiteSpecificBrowser.createFromURI(
     Services.io.newURI(gHttpsTestRoot)
   );
 
   Assert.ok(button.hidden, "Button should be hidden.");
-  Assert.equal(panel.firstElementChild, null, "Should be nothing in the list.");
+  Assert.equal(
+    win.document.querySelector("#appMenu-SSBView .panel-subview-body"),
+    null,
+    "Panel should not be available"
+  );
 
   await ssb.install();
 
   // Button should still be hidden, we don't populate the list until it is
   // first opened.
   Assert.ok(button.hidden, "Button should be hidden.");
-  Assert.equal(panel.firstElementChild, null, "Should be nothing in the list.");
+  Assert.equal(
+    win.document.querySelector("#appMenu-SSBView .panel-subview-body"),
+    null,
+    "Panel should not be available"
+  );
 
   let appMenuOpened = BrowserTestUtils.waitForEvent(
     win.document.getElementById("appMenu-popup"),
@@ -42,6 +51,22 @@ add_task(async () => {
   await Promise.all([appMenuOpened, buttonShown]);
 
   Assert.ok(!button.hidden, "Button should be visible.");
+
+  EventUtils.synthesizeMouseAtCenter(
+    win.document.getElementById("appMenu-ssb-button"),
+    {},
+    win
+  );
+
+  let panelShown = BrowserTestUtils.waitForEvent(
+    win.document.getElementById("appMenu-SSBView"),
+    "ViewShown"
+  );
+  let panel = win.document.querySelector(
+    "#appMenu-SSBView .panel-subview-body"
+  );
+  await panelShown;
+
   Assert.notEqual(
     panel.firstElementChild,
     null,
@@ -52,18 +77,6 @@ add_task(async () => {
     "ssb-button-" + ssb.id,
     "Should have the right ID."
   );
-
-  let panelShown = BrowserTestUtils.waitForEvent(
-    win.document.getElementById("appMenu-SSBView"),
-    "ViewShown"
-  );
-
-  EventUtils.synthesizeMouseAtCenter(
-    win.document.getElementById("appMenu-ssb-button"),
-    {},
-    win
-  );
-  await panelShown;
 
   let ssbOpened = waitForSSB();
   EventUtils.synthesizeMouseAtCenter(panel.firstElementChild, {}, win);
