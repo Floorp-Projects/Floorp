61f378799e87136ffe37554d84c1dfe5a3c83a2d
jgraham <james@hoppipolla.co.uk>
2020-07-09 14:15:46 +0000

    Bug 1651041 [wpt PR 24484] - Move six import after path is set, a=testonly
    Automatic update from web-platform-tests
    Move six import after path is set (#24484)
    
    Co-authored-by: Stephen McGruer <smcgruer@chromium.org>
    --
    
    wpt-commits: 47821bc96914cfb6a42832490c987e16408e58be
    wpt-pr: 24484

--- testing/web-platform/tests/tools/localpaths.py
+++ testing/web-platform/tests/tools/localpaths.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from six import ensure_text
 
 here = os.path.abspath(os.path.split(__file__)[0])
 
@@ -28,4 +27,6 @@ sys.path.insert(0, os.path.join(here, "wptrunner"))
 if sys.version_info[0] == 2:
     sys.path.insert(0, os.path.join(here, "third_party", "enum"))
 
+# We can't import six until we've set the path above.
+from six import ensure_text
 repo_root = ensure_text(os.path.abspath(os.path.join(here, os.pardir)))
