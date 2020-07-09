22d58e99df3575f80295144ea7fd6f758fbe246e
Stephen McGruer <smcgruer@chromium.org>
2020-07-09 14:17:20 +0000

    Bug 1651212 [wpt PR 24497] - Manifest action: Adjust logging and try stripping the commit, a=testonly
    Automatic update from web-platform-tests
    Adjust logging and try stripping the commit (#24497)
    
    --
    
    wpt-commits: d7423b4ef835ac579b2aa6cbd0e88e47ed1c82cd
    wpt-pr: 24497

--- testing/web-platform/tests/tools/ci/manifest_build.py
+++ testing/web-platform/tests/tools/ci/manifest_build.py
@@ -107,13 +107,16 @@ def get_pr(owner, repo, sha):
 
 
 def create_release(manifest_path, owner, repo, sha, tag, body):
+    logger.info("Creating a release for tag='%s', target_commitish='%s'" % (tag, sha))
+    tag = tag.strip()
+    sha = sha.strip()
+
     create_url = "https://api.github.com/repos/%s/%s/releases" % (owner, repo)
     create_data = {"tag_name": tag,
                    "target_commitish": sha,
                    "name": tag,
                    "body": body,
                    "draft": True}
-    logger.info("Creating a release for tag=%s, target_commitish=%s" % (tag, sha))
     create_resp = request(create_url, "Release creation", json_data=create_data)
     if not create_resp:
         return False
