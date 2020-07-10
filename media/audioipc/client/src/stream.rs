573b28be3c4b3077155f0bc3e5b97f8c8f292480
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-07-10 15:56:59 +0300

    Backed out 2 changesets (bug 1561207) for bustages on lib.rs. CLOSED TREE
    Backed out changeset c0d2201bd2fa (bug 1561207)
    Backed out changeset 7931e2be169c (bug 1561207)

--- media/audioipc/client/src/stream.rs
+++ media/audioipc/client/src/stream.rs
@@ -62,8 +62,7 @@ impl rpc::Server for CallbackServer {
     type Request = CallbackReq;
     type Response = CallbackResp;
     type Future = CpuFuture<Self::Response, ()>;
-    type Transport =
-        Framed<audioipc::AsyncMessageStream, LengthDelimitedCodec<Self::Response, Self::Request>>;
+    type Transport = Framed<audioipc::AsyncMessageStream, LengthDelimitedCodec<Self::Response, Self::Request>>;
 
     fn process(&mut self, req: Self::Request) -> Self::Future {
         match req {
@@ -94,10 +93,7 @@ impl rpc::Server for CallbackServer {
                 self.cpu_pool.spawn_fn(move || {
                     // TODO: This is proof-of-concept. Make it better.
                     let input_ptr: *const u8 = match input_shm {
-                        Some(shm) => shm
-                            .get_slice(nframes as usize * input_frame_size)
-                            .unwrap()
-                            .as_ptr(),
+                        Some(shm) => shm.get_slice(nframes as usize * input_frame_size).unwrap().as_ptr(),
                         None => ptr::null(),
                     };
                     let output_ptr: *mut u8 = match output_shm {
@@ -173,13 +169,9 @@ impl<'ctx> ClientStream<'ctx> {
         let rpc = ctx.rpc();
         let data = send_recv!(rpc, StreamInit(init_params) => StreamCreated())?;
 
-        debug!(
-            "token = {}, handles = {:?}",
-            data.token, data.platform_handles
-        );
+        debug!("token = {}, handles = {:?}", data.token, data.platform_handles);
 
-        let stream =
-            unsafe { audioipc::MessageStream::from_raw_fd(data.platform_handles[0].into_raw()) };
+        let stream = unsafe { audioipc::MessageStream::from_raw_fd(data.platform_handles[0].into_raw()) };
 
         let input_file = unsafe { data.platform_handles[1].into_file() };
         let input_shm = if has_input {
