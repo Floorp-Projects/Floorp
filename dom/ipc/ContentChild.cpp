07f9227d63f197f677d0a3a9c24293d482d1f602
Butkovits Atila <abutkovits@mozilla.com>
2020-07-10 03:03:30 +0300

    Backed out changeset 4c0909f105b2 (bug 1648590) for build bustage at ContentChild.cpp. CLOSED TREE

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1650,7 +1650,11 @@ mozilla::ipc::IPCResult ContentChild::RecvSetProcessSandbox(
       CrashReporter::Annotation::ContentSandboxCapabilities,
       static_cast<int>(SandboxInfo::Get().AsInteger()));
 #  endif /* XP_LINUX && !OS_ANDROID */
-#endif   /* MOZ_SANDBOX */
+  // Use the prefix to avoid URIs from Fission isolated processes.
+  auto remoteTypePrefix = RemoteTypePrefix(GetRemoteType());
+  CrashReporter::AnnotateCrashReport(CrashReporter::Annotation::RemoteType,
+                                     remoteTypePrefix);
+#endif /* MOZ_SANDBOX */
 
   return IPC_OK();
 }
@@ -2584,8 +2588,6 @@ mozilla::ipc::IPCResult ContentChild::RecvRemoteType(
              aRemoteType.get()));
   }
 
-  auto remoteTypePrefix = RemoteTypePrefix(aRemoteType);
-
   // Update the process name so about:memory's process names are more obvious.
   if (aRemoteType == FILE_REMOTE_TYPE) {
     SetProcessName(u"file:// Content"_ns);
@@ -2595,16 +2597,12 @@ mozilla::ipc::IPCResult ContentChild::RecvRemoteType(
     SetProcessName(u"Privileged Content"_ns);
   } else if (aRemoteType == LARGE_ALLOCATION_REMOTE_TYPE) {
     SetProcessName(u"Large Allocation Web Content"_ns);
-  } else if (remoteTypePrefix.EqualsLiteral(FISSION_WEB_REMOTE_TYPE)) {
+  } else if (RemoteTypePrefix(aRemoteType) == FISSION_WEB_REMOTE_TYPE) {
     SetProcessName(u"Isolated Web Content"_ns);
   }
   // else "prealloc", "web" or "webCOOP+COEP" type -> "Web Content" already set
 
   mRemoteType.Assign(aRemoteType);
-
-  // Use the prefix to avoid URIs from Fission isolated processes.
-  CrashReporter::AnnotateCrashReport(CrashReporter::Annotation::RemoteType,
-                                     remoteTypePrefix);
 
   return IPC_OK();
 }
