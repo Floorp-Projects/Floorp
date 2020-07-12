af5cff556a9482d7aa2267a82f2ccfaf18e797e9
Csoregi Natalia <ncsoregi@mozilla.com>
2020-07-12 09:03:53 +0300

    Backed out 6 changesets (bug 1648468) for failures on test_keywords.js. CLOSED TREE
    Backed out changeset 918db5010acb (bug 1648468)
    Backed out changeset 91a4b5b593bf (bug 1648468)
    Backed out changeset 3bded27be98e (bug 1648468)
    Backed out changeset 60a36d4424df (bug 1648468)
    Backed out changeset 1edf02398318 (bug 1648468)
    Backed out changeset ebac9fb9da0d (bug 1648468)

---
+++ toolkit/components/places/tests/unifiedcomplete/test_avoid_stripping_to_empty_tokens.js
@@ -0,0 +1,81 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+add_task(async function test_protocol_trimming() {
+  for (let prot of ["http", "https", "ftp"]) {
+    let visit = {
+      // Include the protocol in the query string to ensure we get matches (see bug 1059395)
+      uri: NetUtil.newURI(
+        prot +
+          "://www.mozilla.org/test/?q=" +
+          prot +
+          encodeURIComponent("://") +
+          "www.foo"
+      ),
+      title: "Test title",
+      transition: TRANSITION_TYPED,
+    };
+    await PlacesTestUtils.addVisits(visit);
+
+    let input = prot + "://www.";
+    info("Searching for: " + input);
+    await check_autocomplete({
+      search: input,
+      matches: [
+        {
+          value: prot + "://www.mozilla.org/",
+          comment:
+            prot == "http" ? "www.mozilla.org" : prot + "://www.mozilla.org",
+          style: ["autofill", "heuristic"],
+        },
+        {
+          value: visit.uri.spec,
+          comment: visit.title,
+          style: ["favicon"],
+        },
+      ],
+    });
+
+    input = "www.";
+    info("Searching for: " + input);
+    await check_autocomplete({
+      search: input,
+      matches: [
+        {
+          value: "www.mozilla.org/",
+          comment:
+            prot == "http" ? "www.mozilla.org" : prot + "://www.mozilla.org",
+          style: ["autofill", "heuristic"],
+        },
+        {
+          value: visit.uri.spec,
+          comment: visit.title,
+          style: ["favicon"],
+        },
+      ],
+    });
+
+    let inputs = [
+      prot + "://",
+      prot + ":// ",
+      prot + ":// mo",
+      prot + "://mo te",
+      prot + "://www. ",
+      prot + "://www. mo",
+      prot + "://www.mo te",
+      "www. ",
+      "www. mo",
+      "www.mo te",
+    ];
+    for (input of inputs) {
+      info("Searching for: " + input);
+      await check_autocomplete({
+        search: input,
+        matches: [{ uri: visit.uri, title: visit.title }],
+      });
+    }
+
+    await cleanup();
+  }
+});
