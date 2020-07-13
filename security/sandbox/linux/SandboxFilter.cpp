0820e489abd47ed627b21da4f984dd728895166e
Jed Davis <jld@mozilla.com>
2020-07-13 12:18:00 +0000

    Bug 1650751 - Add FMODE_NONOTIFY to ignored file flags in Linux sandbox. r=gcp
    As of kernel 5.8 (commit [e9c15badb][]), Linux will set the internal
    `FMODE_NONOTIFY` flag on files that don't exist in the filesystem,
    including (unnamed) pipes and sockets.  Although this flag isn't
    properly part of the userspace API, it will be returned by F_GETFL, so
    userspace code that tries to change file flags will pass it to F_SETFL.
    
    The implementation of `F_SETFL` has an allow list of flags userspace can
    change (`SETFL_MASK`) and ignores all others, but our sandbox has a list
    of flags *known* to be ignored, because currently unknown flags could
    potentially be accepted by the kernel in the future.
    
    This patch adds `FMODE_NONOTIFY` as an ignored flag.
    
    [e9c15badb]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e9c15badbb7b20ccdbadf5da14e0a68fbad51015
    
    Differential Revision: https://phabricator.services.mozilla.com/D83205

--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -70,6 +70,9 @@ using namespace sandbox::bpf_dsl;
 // actual value because it shows up in file flags.
 #define O_LARGEFILE_REAL 00100000
 
+// Not part of UAPI, but userspace sees it in F_GETFL; see bug 1650751.
+#define FMODE_NONOTIFY 0x4000000
+
 #ifndef F_LINUX_SPECIFIC_BASE
 #  define F_LINUX_SPECIFIC_BASE 1024
 #else
@@ -541,7 +544,7 @@ class SandboxPolicyCommon : public SandboxPolicyBase {
         // new SETFL-able flags are added.  (In particular we want to
         // forbid O_ASYNC; see bug 1328896, but also see bug 1408438.)
         static const int ignored_flags =
-            O_ACCMODE | O_LARGEFILE_REAL | O_CLOEXEC;
+            O_ACCMODE | O_LARGEFILE_REAL | O_CLOEXEC | FMODE_NONOTIFY;
         static const int allowed_flags = ignored_flags | O_APPEND | O_NONBLOCK;
         return Switch(cmd)
             // Close-on-exec is meaningless when execve isn't allowed, but
