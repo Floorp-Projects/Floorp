8e8300c1991e3044a06dc956dbd3fa4af5884235
Dorel Luca <dluca@mozilla.com>
2020-07-14 02:04:07 +0300

    Backed out changeset cddd6ccb366e (bug 1646006) for Mochitest failures on dom/indexedDB/test/test_file_delete.html. CLOSED TREE

--- dom/indexedDB/FileInfoT.h
+++ dom/indexedDB/FileInfoT.h
@@ -38,7 +38,6 @@ class FileInfoT final {
 
   nsCOMPtr<nsIFile> GetFileForFileInfo() const;
 
-  void LockedAddRef();
   bool LockedClearDBRefs(const typename FileManager::FileManagerGuard& aGuard);
 
  private:
