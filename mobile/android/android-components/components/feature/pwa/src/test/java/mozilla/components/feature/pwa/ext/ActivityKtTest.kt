ec275caced50143bada40a356d634248db8ccdc5
Michael Comella <michael.l.comella@gmail.com>
2020-07-14 11:42:51 -0700

    [components] No issue: add Activity.reportFullyDrawnSafe.
    The production method is imported from Fenix:
        https://github.com/mozilla-mobile/fenix/blob/69020a1f269c57e86fb4eb1dfd25d3ea168b5a29/app/src/main/java/org/mozilla/fenix/ext/Activity.kt#L28
    
    And was verified to address the issue it was added for.

--- mobile/android/android-components/components/feature/pwa/src/test/java/mozilla/components/feature/pwa/ext/ActivityKtTest.kt
+++ mobile/android/android-components/components/feature/pwa/src/test/java/mozilla/components/feature/pwa/ext/ActivityKtTest.kt
@@ -7,8 +7,11 @@ package mozilla.components.feature.pwa.ext
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import mozilla.components.concept.engine.manifest.WebAppManifest
+import mozilla.components.support.ktx.android.view.reportFullyDrawnSafe
 import mozilla.components.support.test.mock
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.anyInt
 import org.mockito.Mockito.verify
 
@@ -17,6 +20,13 @@ class ActivityKtTest {
         name = "Test Manifest",
         startUrl = "/"
     )
+
+    private lateinit var activity: Activity
+
+    @Before
+    fun setUp() {
+        activity = mock()
+    }
 
     @Test
     fun `applyOrientation calls setRequestedOrientation for every value`() {
@@ -52,5 +62,17 @@ class ActivityKtTest {
             activity.applyOrientation(null)
             verify(activity).requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER
         }
+    }
+
+    @Test
+    fun `WHEN reportFullyDrawnSafe is called THEN reportFullyDrawn is called`() {
+        activity.reportFullyDrawnSafe()
+        verify(activity).reportFullyDrawn()
+    }
+
+    @Test
+    fun `GIVEN reportFullyDrawn throws a SecurityException wHEN reportFullyDrawnSafe is called THEN the exception is caught`() {
+        `when`(activity.reportFullyDrawn()).thenThrow(SecurityException())
+        activity.reportFullyDrawnSafe() // If an exception is thrown, this test will fail.
     }
 }
