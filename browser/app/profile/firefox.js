bbed65f942074eb46abc13e3c4b1ef63409e4c2f
Kriyszig <lelouch.cpp@gmail.com>
2020-07-14 21:07:02 +0000

    Bug 1649020 - Add pref for inline CSS incompatibility warning in inspector ruleview r=mtigley DONTBUILD
    This patch adds
    `devtools.inspector.ruleview.inline-compatibility-warning.enabled`
    that covers the development of the inline CSS compatiblity warning
    tooltip
    
    Differential Revision: https://phabricator.services.mozilla.com/D81473

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2041,6 +2041,8 @@ pref("devtools.inspector.showUserAgentStyles", false);
 pref("devtools.inspector.showAllAnonymousContent", false);
 // Enable the new Rules View
 pref("devtools.inspector.new-rulesview.enabled", false);
+// Enable the inline CSS compatiblity warning in inspector rule view
+pref("devtools.inspector.ruleview.inline-compatibility-warning.enabled", false);
 // Enable the compatibility tool in the inspector.
 #if defined(NIGHTLY_BUILD) || defined(MOZ_DEV_EDITION)
 pref("devtools.inspector.compatibility.enabled", true);
