4681060e62330fcacdb2d713005a3570ce4d42b4
Dorel Luca <dluca@mozilla.com>
2020-07-15 16:37:32 +0300

    Backed out 3 changesets (bug 1367251) for Mda failures in /test_background_video_tainted_by_createimagebitmap.html. CLOSED TREE
    Backed out changeset 871379f9b194 (bug 1367251)
    Backed out changeset a76c2c98b33c (bug 1367251)
    Backed out changeset a55cda2431fc (bug 1367251)

--- dom/workers/WorkerScope.cpp
+++ dom/workers/WorkerScope.cpp
@@ -562,16 +562,15 @@ already_AddRefed<IDBFactory> WorkerGlobalScope::GetIndexedDB(
 }
 
 already_AddRefed<Promise> WorkerGlobalScope::CreateImageBitmap(
-    const ImageBitmapSource& aImage, const ImageBitmapOptions& aOptions,
-    ErrorResult& aRv) {
-  return ImageBitmap::Create(this, aImage, Nothing(), aOptions, aRv);
+    const ImageBitmapSource& aImage, ErrorResult& aRv) {
+  return ImageBitmap::Create(this, aImage, Nothing(), aRv);
 }
 
 already_AddRefed<Promise> WorkerGlobalScope::CreateImageBitmap(
     const ImageBitmapSource& aImage, int32_t aSx, int32_t aSy, int32_t aSw,
-    int32_t aSh, const ImageBitmapOptions& aOptions, ErrorResult& aRv) {
-  return ImageBitmap::Create(
-      this, aImage, Some(gfx::IntRect(aSx, aSy, aSw, aSh)), aOptions, aRv);
+    int32_t aSh, ErrorResult& aRv) {
+  return ImageBitmap::Create(this, aImage,
+                             Some(gfx::IntRect(aSx, aSy, aSw, aSh)), aRv);
 }
 
 mozilla::dom::DebuggerNotificationManager*
