44b5663541654f144fde9e81acd5033153d03b5a
Tooru Fujisawa <arai_a@mac.com>
2020-07-15 04:40:08 +0000

    Bug 1623826 - Do not expose outerWidth/outerHeight features of window.open to web content. r=smaug
    Depends on D81969
    
    Differential Revision: https://phabricator.services.mozilla.com/D81970

--- toolkit/components/windowwatcher/nsWindowWatcher.h
+++ toolkit/components/windowwatcher/nsWindowWatcher.h
@@ -99,7 +99,7 @@ class nsWindowWatcher : public nsIWindowWatcher,
 
   /* Compute the right SizeSpec based on aFeatures */
   static void CalcSizeSpec(const mozilla::dom::WindowFeatures& aFeatures,
-                           SizeSpec& aResult);
+                           bool aHasChromeParent, SizeSpec& aResult);
   static void SizeOpenedWindow(
       nsIDocShellTreeOwner* aTreeOwner, mozIDOMWindowProxy* aParent,
       bool aIsCallerChrome, const SizeSpec& aSizeSpec,
