e300b5a2e6480e5baac661f2d2392387925cf752
Jamie Nicol <jnicol@mozilla.com>
2020-07-15 14:29:35 +0000

    Bug 1609191 - Ensure shader sources are always unique to workaround adreno crash. r=gw
    On some Adreno 505 and 506 devices we are encountering driver crashes during
    glLinkProgram(). The only circumstance in which we have been able to reproduce
    locally is when the show-overdraw debug option is enabled. The reason appears to
    be that, due to shader optimisation, the debug overdraw variants of many shaders
    have identical source code. The crash seems to occur when linking a shader which
    has identical source code to a previously linked shader.
    
    This does not, however, explain the non-insignificant numbers of crashes in the
    wild because a) it's unlikely many users are enabling overdraw debugging, and b)
    some crash reports predate the commit which enabled shader
    optimisation. However, it is possible that for a different reason we are
    compiling multiple shaders with identical source code.
    
    To attempt to work around this crash this change adds a random comment to the
    end of each shader source string, on the affected devices.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83571

--- gfx/wr/webrender/src/device/gl.rs
+++ gfx/wr/webrender/src/device/gl.rs
@@ -18,6 +18,7 @@ use std::{
     cell::{Cell, RefCell},
     cmp,
     collections::hash_map::Entry,
+    fmt::Write,
     marker::PhantomData,
     mem,
     num::NonZeroUsize,
@@ -31,6 +32,7 @@ use std::{
     thread,
     time::Duration,
 };
+use uuid::Uuid;
 use webrender_build::shader::{
     ProgramSourceDigest, ShaderKind, ShaderVersion, build_shader_main_string,
     build_shader_prefix_string, do_build_shader_string, shader_source_from_file,
@@ -1082,6 +1084,10 @@ pub struct Device {
     /// are null-terminated, to work around driver bugs.
     requires_null_terminated_shader_source: bool,
 
+    /// Whether we must ensure the source strings passed to glShaderSource()
+    /// are unique, to work around driver bugs.
+    requires_unique_shader_source: bool,
+
     /// Whether we must unbind any texture from GL_TEXTURE_EXTERNAL_OES before
     /// binding to GL_TEXTURE_2D, to work around an android emulator bug.
     requires_texture_external_unbind: bool,
@@ -1521,6 +1527,12 @@ impl Device {
         // strings are not null-terminated. See bug 1591945.
         let requires_null_terminated_shader_source = is_emulator;
 
+        // On Adreno 505 and 506 we encounter crashes during glLinkProgram(), which
+        // appear to be caused by some driver-internal caching of shader strings.
+        // We attempt to workaround this by appending unique comments to each source.
+        // See bug 1609191.
+        let requires_unique_shader_source = renderer_name == "Adreno (TM) 505" || renderer_name == "Adreno (TM) 506";
+
         // The android emulator gets confused if you don't explicitly unbind any texture
         // from GL_TEXTURE_EXTERNAL_OES before binding another to GL_TEXTURE_2D. See bug 1636085.
         let requires_texture_external_unbind = is_emulator;
@@ -1599,6 +1611,7 @@ impl Device {
             extensions,
             texture_storage_usage,
             requires_null_terminated_shader_source,
+            requires_unique_shader_source,
             requires_texture_external_unbind,
             optimal_pbo_stride,
             dump_shader_source,
@@ -1717,28 +1730,34 @@ impl Device {
     }
 
     pub fn compile_shader(
-        gl: &dyn gl::Gl,
+        &self,
         name: &str,
         shader_type: gl::GLenum,
         source: &String,
-        requires_null_terminated_shader_source: bool,
     ) -> Result<gl::GLuint, ShaderError> {
         debug!("compile {}", name);
-        let id = gl.create_shader(shader_type);
-        if requires_null_terminated_shader_source {
-            // Ensure the source strings we pass to glShaderSource are
-            // null-terminated on buggy platforms.
-            use std::ffi::CString;
-            let terminated_source = CString::new(source.as_bytes()).unwrap();
-            gl.shader_source(id, &[terminated_source.as_bytes_with_nul()]);
-        } else {
-            gl.shader_source(id, &[source.as_bytes()]);
+        let id = self.gl.create_shader(shader_type);
+
+        let mut new_source = Cow::from(source.as_str());
+        // On some drivers we encounter crashes during glLinkProgram(), which
+        // appear to be caused by some driver-internal caching of shader strings.
+        // We attempt to workaround this by appending unique comments to each source.
+        if self.requires_unique_shader_source {
+            let uuid = Uuid::new_v4().to_hyphenated();
+            write!(new_source.to_mut(), "\n//{}\n", uuid).unwrap();
         }
-        gl.compile_shader(id);
-        let log = gl.get_shader_info_log(id);
+        // Ensure the source strings we pass to glShaderSource are
+        // null-terminated on buggy platforms.
+        if self.requires_null_terminated_shader_source {
+            new_source.to_mut().push('\0');
+        }
+
+        self.gl.shader_source(id, &[new_source.as_bytes()]);
+        self.gl.compile_shader(id);
+        let log = self.gl.get_shader_info_log(id);
         let mut status = [0];
         unsafe {
-            gl.get_shader_iv(id, gl::COMPILE_STATUS, &mut status);
+            self.gl.get_shader_iv(id, gl::COMPILE_STATUS, &mut status);
         }
         if status[0] == 0 {
             error!("Failed to compile shader: {}\n{}", name, log);
@@ -2034,7 +2053,7 @@ impl Device {
         if build_program {
             // Compile the vertex shader
             let vs_source = info.compute_source(self, ShaderKind::Vertex);
-            let vs_id = match Device::compile_shader(&*self.gl, &info.base_filename, gl::VERTEX_SHADER, &vs_source, self.requires_null_terminated_shader_source) {
+            let vs_id = match self.compile_shader(&info.base_filename, gl::VERTEX_SHADER, &vs_source) {
                     Ok(vs_id) => vs_id,
                     Err(err) => return Err(err),
                 };
@@ -2042,7 +2061,7 @@ impl Device {
             // Compile the fragment shader
             let fs_source = info.compute_source(self, ShaderKind::Fragment);
             let fs_id =
-                match Device::compile_shader(&*self.gl, &info.base_filename, gl::FRAGMENT_SHADER, &fs_source, self.requires_null_terminated_shader_source) {
+                match self.compile_shader(&info.base_filename, gl::FRAGMENT_SHADER, &fs_source) {
                     Ok(fs_id) => fs_id,
                     Err(err) => {
                         self.gl.delete_shader(vs_id);
