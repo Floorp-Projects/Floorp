f1e9d8ded2de11201d6c94ad8e024e1985ac6ed4
Ting-Yu Lin <tlin@mozilla.com>
2020-07-15 21:53:26 +0000

    Bug 1652207 - Make IntCoordTyped and CoordTyped derive from units. r=botond
    The GCC versions we needed to work around are no longer supported. Our
    current minimum requirement for GCC is 7.1 per
    https://groups.google.com/forum/#!msg/mozilla.dev.platform/ygj-1MDVB4M/CLxE_7OcBAAJ
    
    Differential Revision: https://phabricator.services.mozilla.com/D83232

--- gfx/2d/Coord.h
+++ gfx/2d/Coord.h
@@ -85,12 +85,10 @@ struct CoordOperatorsHelper<true, coord, primitive> {
   // 'scale / coord' is intentionally omitted because it doesn't make sense.
 };
 
-// Note: 'IntCoordTyped<units>' and 'CoordTyped<units>' do not derive from
-// 'units' to work around https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61959.
-
 template <class units>
 struct IntCoordTyped
     : public BaseCoord<int32_t, IntCoordTyped<units> >,
+      public units,
       public CoordOperatorsHelper<true, IntCoordTyped<units>, float>,
       public CoordOperatorsHelper<true, IntCoordTyped<units>, double> {
   static_assert(IsPixel<units>::value,
@@ -104,6 +102,7 @@ struct IntCoordTyped
 
 template <class units, class F>
 struct CoordTyped : public BaseCoord<F, CoordTyped<units, F> >,
+                    public units,
                     public CoordOperatorsHelper<!std::is_same_v<F, int32_t>,
                                                 CoordTyped<units, F>, int32_t>,
                     public CoordOperatorsHelper<!std::is_same_v<F, uint32_t>,
