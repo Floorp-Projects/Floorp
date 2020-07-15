ca56a9bad459716577a4e6a631b864d52783a381
Simon Giesecke <sgiesecke@mozilla.com>
2020-07-15 14:09:01 +0000

    Bug 1652942 - Remove unnecessary includes from Element.h. r=smaug
    Depends on D83114
    
    Differential Revision: https://phabricator.services.mozilla.com/D83632

--- dom/html/HTMLLegendElement.cpp
+++ dom/html/HTMLLegendElement.cpp
@@ -5,6 +5,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/HTMLLegendElement.h"
+#include "mozilla/dom/ElementBinding.h"
 #include "mozilla/dom/HTMLLegendElementBinding.h"
 #include "nsFocusManager.h"
 #include "nsIFrame.h"
