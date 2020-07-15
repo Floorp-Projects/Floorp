46e356d055e355f5fa7e6b158379426fb75e42ff
ssengupta <ssengupta@mozilla.com>
2020-07-15 08:33:12 +0000

    Bug 1633880 - P1 - gStorage in IPCBlobInputStreamStorage is protected by gMutex. r=baku, a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D83617

--- dom/file/ipc/IPCBlobInputStreamStorage.cpp
+++ dom/file/ipc/IPCBlobInputStreamStorage.cpp
@@ -32,12 +32,15 @@ NS_IMPL_ADDREF(IPCBlobInputStreamStorage)
 NS_IMPL_RELEASE(IPCBlobInputStreamStorage)
 
 /* static */
-IPCBlobInputStreamStorage* IPCBlobInputStreamStorage::Get() { return gStorage; }
+IPCBlobInputStreamStorage* IPCBlobInputStreamStorage::Get() {
+  mozilla::StaticMutexAutoLock lock(gMutex);
+  return gStorage;
+}
 
 /* static */
 void IPCBlobInputStreamStorage::Initialize() {
+  mozilla::StaticMutexAutoLock lock(gMutex);
   MOZ_ASSERT(!gStorage);
-
   gStorage = new IPCBlobInputStreamStorage();
 
   nsCOMPtr<nsIObserverService> obs = mozilla::services::GetObserverService();
@@ -57,6 +60,7 @@ IPCBlobInputStreamStorage::Observe(nsISupports* aSubject, const char* aTopic,
       obs->RemoveObserver(this, "ipc:content-shutdown");
     }
 
+    mozilla::StaticMutexAutoLock lock(gMutex);
     gStorage = nullptr;
     return NS_OK;
   }
