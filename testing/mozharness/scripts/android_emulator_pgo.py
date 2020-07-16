a1d49fb98d1ef188512b8d7f8596bede833161bc
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-07-16 15:29:29 +0300

    Backed out 17 changesets (bug 1486004) for causing bustages in AccessCheck.h
    CLOSED TREE
    
    Backed out changeset 86a0855e1e45 (bug 1486004)
    Backed out changeset 4fc98067edc5 (bug 1486004)
    Backed out changeset 8ab3f1597fde (bug 1486004)
    Backed out changeset b487e98320fb (bug 1486004)
    Backed out changeset 4cba8e6bc515 (bug 1486004)
    Backed out changeset d0db5eef2343 (bug 1486004)
    Backed out changeset 50285a87de6b (bug 1486004)
    Backed out changeset 9730c0303810 (bug 1486004)
    Backed out changeset 140a9d2362f3 (bug 1486004)
    Backed out changeset 9d1ce06f6039 (bug 1486004)
    Backed out changeset 88a5a4ab39fa (bug 1486004)
    Backed out changeset eb186c92232d (bug 1486004)
    Backed out changeset 677f2129750e (bug 1486004)
    Backed out changeset 81427a5f2103 (bug 1486004)
    Backed out changeset 38d7def3d425 (bug 1486004)
    Backed out changeset a9e73221c471 (bug 1486004)
    Backed out changeset ebe8c2d5432e (bug 1486004)

--- testing/mozharness/scripts/android_emulator_pgo.py
+++ testing/mozharness/scripts/android_emulator_pgo.py
@@ -150,7 +150,7 @@ class AndroidProfileRun(TestingMixin, BaseScript, MozbaseMixin,
         """
         from mozhttpd import MozHttpd
         from mozprofile import Preferences
-        from mozdevice import ADBDeviceFactory, ADBTimeoutError
+        from mozdevice import ADBDevice, ADBTimeoutError
         from six import string_types
         from marionette_driver.marionette import Marionette
 
@@ -215,26 +215,26 @@ class AndroidProfileRun(TestingMixin, BaseScript, MozbaseMixin,
 
         # Force test_root to be on the sdcard for android pgo
         # builds which fail for Android 4.3 when profiles are located
-        # in /data/local/tmp/test_root with
+        # in /data/local/tmp/tests with
         # E AndroidRuntime: FATAL EXCEPTION: Gecko
         # E AndroidRuntime: java.lang.IllegalArgumentException: \
-        #    Profile directory must be writable if specified: /data/local/tmp/test_root/profile
+        #    Profile directory must be writable if specified: /data/local/tmp/tests/profile
         # This occurs when .can-write-sentinel is written to
         # the profile in
         # mobile/android/geckoview/src/main/java/org/mozilla/gecko/GeckoProfile.java.
         # This is not a problem on later versions of Android. This
         # over-ride of test_root should be removed when Android 4.3 is no
         # longer supported.
-        sdcard_test_root = '/sdcard/test_root'
-        adbdevice = ADBDeviceFactory(adb=adb,
-                                     device='emulator-5554',
-                                     test_root=sdcard_test_root)
+        sdcard_test_root = '/sdcard/tests'
+        adbdevice = ADBDevice(adb=adb,
+                              device='emulator-5554',
+                              test_root=sdcard_test_root)
         if adbdevice.test_root != sdcard_test_root:
             # If the test_root was previously set and shared
             # the initializer will not have updated the shared
             # value. Force it to match the sdcard_test_root.
             adbdevice.test_root = sdcard_test_root
-        adbdevice.mkdir(outputdir, parents=True)
+        adbdevice.mkdir(outputdir)
 
         try:
             # Run Fennec a first time to initialize its profile
