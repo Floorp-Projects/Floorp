2af0f9a132df507f9771cfec1ebd74a14ececfca
Razvan Maries <rmaries@mozilla.com>
2020-07-17 02:19:47 +0300

    Backed out 4 changesets (bug 1648851) for perma failues on test_2_conformance2__glsl3__matrix-row-major-dynamic-indexing.html and test_2_conformance__state__gl-get-calls.html. CLOSED TREE
    Backed out changeset 2d2686a389dc (bug 1648851)
    Backed out changeset e05b2f2fdbb2 (bug 1648851)
    Backed out changeset b576e680a9fa (bug 1648851)
    Backed out changeset 901f16e24a60 (bug 1648851)

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -602,11 +602,6 @@ test_description_schema = Schema({
     Optional('fetches'): {
         text_type: optionally_keyed_by('test-platform', [text_type])
     },
-
-    # Adds ability to specify what bootstrap script should be used to
-    # initiate the environment setup. See Bug 1648851 for what prompted this.
-    Optional('job-script'):
-        optionally_keyed_by('test-platform', Any(text_type, None)),
 })
 
 
@@ -670,12 +665,7 @@ def set_defaults(config, tasks):
         task.setdefault('loopback-audio', False)
         task.setdefault('loopback-video', False)
         task.setdefault('limit-platforms', [])
-        if build_platform.startswith('android'):
-            task.setdefault('docker-image', {'in-tree': 'android-test'})
-            task.setdefault('job-script', 'test-android.sh')
-        else:
-            task.setdefault('docker-image', {'in-tree': 'ubuntu1804-test'})
-            task.setdefault('job-script', 'test-linux.sh')
+        task.setdefault('docker-image', {'in-tree': 'ubuntu1804-test'})
         task.setdefault('checkout', False)
         task.setdefault('require-signed-extensions', False)
         task.setdefault('variants', [])
