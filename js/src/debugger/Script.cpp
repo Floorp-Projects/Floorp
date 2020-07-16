10f4503b383e03d54b3df863d7a3b658ca1fbe89
Logan Smyth <loganfsmyth@gmail.com>
2020-07-16 22:31:29 +0000

    Bug 1652625 - Drop predecessor/successor Debugger.Script functions. r=arai
    These 2 functions were added for WebReply and are thus unused now that it has
    been removed from the codebase.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83444

--- js/src/debugger/Script.cpp
+++ js/src/debugger/Script.cpp
@@ -233,8 +233,6 @@ struct MOZ_STACK_CLASS DebuggerScript::CallData {
   bool getPossibleBreakpointOffsets();
   bool getOffsetMetadata();
   bool getOffsetLocation();
-  template <bool Successor>
-  bool getSuccessorOrPredecessorOffsets();
   bool getEffectfulOffsets();
   bool getAllOffsets();
   bool getAllColumnOffsets();
@@ -1325,84 +1323,6 @@ bool DebuggerScript::CallData::getOffsetLocation() {
   return true;
 }
 
-class DebuggerScript::GetSuccessorOrPredecessorOffsetsMatcher {
-  JSContext* cx_;
-  size_t offset_;
-  bool successor_;
-  MutableHandleObject result_;
-
- public:
-  GetSuccessorOrPredecessorOffsetsMatcher(JSContext* cx, size_t offset,
-                                          bool successor,
-                                          MutableHandleObject result)
-      : cx_(cx), offset_(offset), successor_(successor), result_(result) {}
-
-  using ReturnType = bool;
-
-  ReturnType match(Handle<BaseScript*> base) {
-    RootedScript script(cx_, DelazifyScript(cx_, base));
-    if (!script) {
-      return false;
-    }
-
-    if (!EnsureScriptOffsetIsValid(cx_, script, offset_)) {
-      return false;
-    }
-
-    PcVector adjacent;
-    if (successor_) {
-      if (!GetSuccessorBytecodes(script, script->code() + offset_, adjacent)) {
-        ReportOutOfMemory(cx_);
-        return false;
-      }
-    } else {
-      if (!GetPredecessorBytecodes(script, script->code() + offset_,
-                                   adjacent)) {
-        ReportOutOfMemory(cx_);
-        return false;
-      }
-    }
-
-    result_.set(NewDenseEmptyArray(cx_));
-    if (!result_) {
-      return false;
-    }
-
-    for (jsbytecode* pc : adjacent) {
-      if (!NewbornArrayPush(cx_, result_, NumberValue(pc - script->code()))) {
-        return false;
-      }
-    }
-    return true;
-  }
-  ReturnType match(Handle<WasmInstanceObject*> instance) {
-    JS_ReportErrorASCII(
-        cx_, "getSuccessorOrPredecessorOffsets NYI on wasm instances");
-    return false;
-  }
-};
-
-template <bool Successor>
-bool DebuggerScript::CallData::getSuccessorOrPredecessorOffsets() {
-  if (!args.requireAtLeast(cx, "successorOrPredecessorOffsets", 1)) {
-    return false;
-  }
-  size_t offset;
-  if (!ScriptOffset(cx, args[0], &offset)) {
-    return false;
-  }
-
-  RootedObject result(cx);
-  GetSuccessorOrPredecessorOffsetsMatcher matcher(cx, offset, Successor,
-                                                  &result);
-  if (!referent.match(matcher)) {
-    return false;
-  }
-
-  args.rval().setObject(*result);
-  return true;
-}
-
 // Return whether an opcode is considered effectful: it can have direct side
 // effects that can be observed outside of the current frame. Opcodes are not
 // effectful if they only modify the current frame's state, modify objects
@@ -2422,10 +2342,6 @@ const JSFunctionSpec DebuggerScript::methods_[] = {
     JS_DEBUG_FN("isInCatchScope", isInCatchScope, 1),
     JS_DEBUG_FN("getOffsetMetadata", getOffsetMetadata, 1),
     JS_DEBUG_FN("getOffsetsCoverage", getOffsetsCoverage, 0),
-    JS_DEBUG_FN("getSuccessorOffsets", getSuccessorOrPredecessorOffsets<true>,
-                1),
-    JS_DEBUG_FN("getPredecessorOffsets",
-                getSuccessorOrPredecessorOffsets<false>, 1),
     JS_DEBUG_FN("getEffectfulOffsets", getEffectfulOffsets, 1),
     JS_DEBUG_FN("setInstrumentationId", setInstrumentationId, 1),
 
