c5ff2dd3e0348f2938194c2a05787c00a3d5ce6b
Gabriele Svelto <gsvelto@mozilla.com>
2020-07-16 21:48:17 +0000

    Bug 1633052 - Generate minidumps when encountering heap corruption exceptions on Windows r=dmajor
    This patch adds a vectored exception handler to catch STATUS_HEAP_CORRUPTION
    exceptions on Windows. Previously these crashes would terimnate the affected
    process without generating a minidump. For child process crashes the user would
    be notified of the crash but could not report them. Main process crashes on the
    other hand would close Firefox abruptly without any sort of feedback to the
    user.
    
    Note that this type of exceptions are originated from Windows default memory
    allocator which we replace with jemalloc. As such they're unlikely to be thrown
    from our code, but are frequently encountered when dealing with injected DLLs
    and other external components.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83753

---
+++ toolkit/crashreporter/test/unit/test_crash_heap_corruption.js
@@ -0,0 +1,32 @@
+add_task(async function run_test() {
+  if (!("@mozilla.org/toolkit/crash-reporter;1" in Cc)) {
+    dump(
+      "INFO | test_crash_heap_corruption.js | Can't test crashreporter in a non-libxul build.\n"
+    );
+    return;
+  }
+
+  // Try crashing with a STATUS_HEAP_CORRUPTION exception
+  await do_crash(
+    function() {
+      crashType = CrashTestUtils.CRASH_HEAP_CORRUPTION;
+      crashReporter.annotateCrashReport("TestKey", "TestValue");
+    },
+    async function(mdump, extra, extraFile) {
+      runMinidumpAnalyzer(mdump);
+
+      // Refresh updated extra data
+      let data = await OS.File.read(extraFile.path);
+      let decoder = new TextDecoder();
+      extra = JSON.parse(decoder.decode(data));
+
+      Assert.equal(
+        extra.StackTraces.crash_info.type,
+        "EXCEPTION_HEAP_CORRUPTION"
+      );
+      Assert.equal(extra.TestKey, "TestValue");
+    },
+    // process will exit with a zero exit status
+    true
+  );
+});
