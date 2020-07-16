2fcc6ff6caa9c544e1d9b426789b6bcad7cb7d32
André Bargull <andre.bargull@gmail.com>
2020-07-16 21:19:50 +0000

    Bug 1651668 - Part 2: Automatically generate shell-options flags for private-field features. r=mgaudet
    Also change the update script to no longer generate an external reftest entry
    when shell-options are needed, now that shell-options can be defined in an
    inline reftest entry.
    
    Depends on D83821
    
    Differential Revision: https://phabricator.services.mozilla.com/D83833

--- js/src/tests/test262/language/statements/class/elements/private-derived-cls-indirect-eval-err-contains-supercall.js
+++ js/src/tests/test262/language/statements/class/elements/private-derived-cls-indirect-eval-err-contains-supercall.js
@@ -1,3 +1,4 @@
+// |reftest| shell-option(--enable-private-fields) skip-if(!xulRuntime.shell) -- requires shell-options
 // This file was procedurally generated from the following sources:
 // - src/class-elements/eval-err-contains-supercall.case
 // - src/class-elements/initializer-eval-super-call/cls-decl-private-fields-indirect-eval.template
--- js/src/tests/test262/language/statements/class/elements/syntax/early-errors/private-member-exp-cannot-escape-token.js
+++ js/src/tests/test262/language/statements/class/elements/syntax/early-errors/private-member-exp-cannot-escape-token.js
@@ -1,4 +1,4 @@
-// |reftest| error:SyntaxError
+// |reftest| shell-option(--enable-private-fields) skip-if(!xulRuntime.shell) error:SyntaxError -- requires shell-options
 // This file was procedurally generated from the following sources:
 // - src/class-elements/private-member-exp-cannot-escape-token.case
 // - src/class-elements/syntax/invalid/cls-decl-elements-invalid-syntax.template
