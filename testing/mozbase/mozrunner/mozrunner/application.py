ffdda81f8e35a659742f21323468fe1a221844c6
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-07-16 17:58:09 +0300

    Backed out 20 changesets (bug 1486004, bug 1653094) for wrench bustages. CLOSED TREE
    Backed out changeset 120c64ebdea3 (bug 1486004)
    Backed out changeset b10f105b1c36 (bug 1486004)
    Backed out changeset 7f186e6bcccc (bug 1486004)
    Backed out changeset cdda7b349cd8 (bug 1486004)
    Backed out changeset 650c47e81402 (bug 1486004)
    Backed out changeset 2bcca7cf3f1b (bug 1486004)
    Backed out changeset e1df276f8a8d (bug 1486004)
    Backed out changeset f6f5e7f2bd0a (bug 1486004)
    Backed out changeset 1c56cb03ce53 (bug 1486004)
    Backed out changeset 582cd190f174 (bug 1486004)
    Backed out changeset 7c162e1b3a79 (bug 1486004)
    Backed out changeset d1088e178216 (bug 1486004)
    Backed out changeset 40763f4afd26 (bug 1486004)
    Backed out changeset f20eb5b5a670 (bug 1486004)
    Backed out changeset 6349ee0bd63f (bug 1486004)
    Backed out changeset 7c23729053f6 (bug 1486004)
    Backed out changeset 49586014bd29 (bug 1486004)
    Backed out changeset 07971e82ae85 (bug 1653094)
    Backed out changeset 47e7295e9c19 (bug 1653094)
    Backed out changeset 57a25bbeec37 (bug 1653094)

--- testing/mozbase/mozrunner/mozrunner/application.py
+++ testing/mozbase/mozrunner/mozrunner/application.py
@@ -9,7 +9,7 @@ from distutils.spawn import find_executable
 import os
 import posixpath
 
-from mozdevice import ADBDeviceFactory
+from mozdevice import ADBDevice
 from mozprofile import (
     Profile,
     ChromeProfile,
@@ -117,7 +117,7 @@ class FennecContext(RemoteContext):
         if device_serial in devices:
             device = devices[device_serial]
         else:
-            device = ADBDeviceFactory(adb=adb_path, device=device_serial)
+            device = ADBDevice(adb=adb_path, device=device_serial)
             devices[device_serial] = device
         return device
 
