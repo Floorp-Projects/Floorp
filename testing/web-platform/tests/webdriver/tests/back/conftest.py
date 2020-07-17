f95d57c09025a920d3ae303fe9a106710e3e583d
Henrik Skupin <mail@hskupin.info>
2020-07-17 16:32:15 +0000

    Bug 1652973 - [wdspec] Replace create_window fixture with new window command. r=jgraham
    Now that each driver supports the new window command, we can make use of it
    as replacement for the create_window fixture.
    
    Given that there is no need to check the type of the new window,
    this implementation doesn't add a check for that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83800

--- testing/web-platform/tests/webdriver/tests/back/conftest.py
+++ testing/web-platform/tests/webdriver/tests/back/conftest.py
@@ -4,10 +4,10 @@ from webdriver.error import NoSuchWindowException
 
 
 @pytest.fixture(name="session")
-def fixture_session(capabilities, session, create_window):
+def fixture_session(capabilities, session):
     """Prevent re-using existent history by running the test in a new window."""
     original_handle = session.window_handle
-    session.window_handle = create_window()
+    session.window_handle = session.new_window()
 
     yield session
 
--- testing/web-platform/tests/webdriver/tests/support/fixtures.py
+++ testing/web-platform/tests/webdriver/tests/support/fixtures.py
@@ -88,18 +88,6 @@ def create_frame(session):
 
 
 @pytest.fixture
-def create_window(session):
-    """Open new window and return the window handle."""
-    def create_window():
-        windows_before = session.handles
-        name = session.execute_script("window.open()")
-        assert len(session.handles) == len(windows_before) + 1
-        new_windows = list(set(session.handles) - set(windows_before))
-        return new_windows.pop()
-    return create_window
-
-
-@pytest.fixture
 def http(configuration):
     return HTTPRequest(configuration["host"], configuration["port"])
 
@@ -224,10 +212,10 @@ def create_dialog(session):
 
 
 @pytest.fixture
-def closed_window(session, create_window):
+def closed_window(session):
     original_handle = session.window_handle
+    new_handle = session.new_window()
 
-    new_handle = create_window()
     session.window_handle = new_handle
 
     session.window.close()
