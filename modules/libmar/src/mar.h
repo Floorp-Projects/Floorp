20d234916d84b3f378743930b54b5497a46013ef
Chris Peterson <cpeterson@mozilla.com>
2020-07-17 02:48:19 +0000

    Bug 1652914 - Part 1: Replace MOZ_STATIC_ASSERT with static_assert in C code. r=jwalden
    static_assert is available in both C++11 and C11. While C++11's static_assert is a keyword, C11's static_assert is a macro defined in assert.h (that expands to the C11 keywoard _Static_assert). libar's header files are included in both C and C++ code, so we must #include assert.h to use static_assert.
    
    https://en.cppreference.com/w/c/language/_Static_assert
    
    Differential Revision: https://phabricator.services.mozilla.com/D83736

--- modules/libmar/src/mar.h
+++ modules/libmar/src/mar.h
@@ -7,7 +7,7 @@
 #ifndef MAR_H__
 #define MAR_H__
 
-#include "mozilla/Assertions.h"
+#include <assert.h>  // for C11 static_assert
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -21,11 +21,7 @@ extern "C" {
  * code in tool/mar.c.
  */
 #define MAX_SIGNATURES 8
-#ifdef __cplusplus
 static_assert(MAX_SIGNATURES <= 9, "too many signatures");
-#else
-MOZ_STATIC_ASSERT(MAX_SIGNATURES <= 9, "too many signatures");
-#endif
 
 struct ProductInformationBlock {
   const char* MARChannelID;
