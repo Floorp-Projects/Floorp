c0bcef021fd5040edb068d5c29c925b2ae5870aa
Ryan VanderMeulen <ryanvm@gmail.com>
2020-07-17 21:05:26 +0000

    Bug 1653573 - Update pdf.js to version 2.6.123. r=bdahl
    Differential Revision: https://phabricator.services.mozilla.com/D83949

--- toolkit/components/pdfjs/content/build/pdf.js
+++ toolkit/components/pdfjs/content/build/pdf.js
@@ -335,8 +335,8 @@ var _text_layer = __w_pdfjs_require__(18);
 
 var _svg = __w_pdfjs_require__(19);
 
-const pdfjsVersion = '2.6.103';
-const pdfjsBuild = '72d71ba6a';
+const pdfjsVersion = '2.6.123';
+const pdfjsBuild = 'a604973cc';
 ;
 
 /***/ }),
@@ -1670,7 +1670,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.isNodeJS = void 0;
-const isNodeJS = typeof process === "object" && process + "" === "[object process]" && !process.versions.nw && !process.versions.electron;
+const isNodeJS = typeof process === "object" && process + "" === "[object process]" && !process.versions.nw && !(process.versions.electron && process.type && process.type !== "browser");
 exports.isNodeJS = isNodeJS;
 
 /***/ }),
@@ -1884,7 +1884,7 @@ function _fetchDocument(worker, source, pdfDataRangeTransport, docId) {
 
   return worker.messageHandler.sendWithPromise("GetDocRequest", {
     docId,
-    apiVersion: '2.6.103',
+    apiVersion: '2.6.123',
     source: {
       data: source.data,
       url: source.url,
@@ -3743,9 +3743,9 @@ const InternalRenderTask = function InternalRenderTaskClosure() {
   return InternalRenderTask;
 }();
 
-const version = '2.6.103';
+const version = '2.6.123';
 exports.version = version;
-const build = '72d71ba6a';
+const build = 'a604973cc';
 exports.build = build;
 
 /***/ }),
@@ -6048,9 +6048,6 @@ var CanvasGraphics = function CanvasGraphicsClosure() {
     },
     paintSolidColorImageMask: function CanvasGraphics_paintSolidColorImageMask() {
       this.ctx.fillRect(0, 0, 1, 1);
-    },
-    paintXObject: function CanvasGraphics_paintXObject() {
-      (0, _util.warn)("Unsupported 'paintXObject' command.");
     },
     markPoint: function CanvasGraphics_markPoint(tag) {},
     markPointProps: function CanvasGraphics_markPointProps(tag, properties) {},
