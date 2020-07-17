3b6b858b1e9956dd2c9fc247e3ff6e046919f441
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-07-17 14:11:47 +0000

    Bug 1480201 - Fix intermittent on webconsole autocomplete damp test. r=bomsy,perftest-reviewers,sparky.
    We used to clear a timeout in HTMLTooltip which is supposed to resolve a Promise
    when it's executed, meaning that we could end up with Promises that would never
    resolve.
    In the test itself, we wait for a tick between each loop iteration and after hiding
    the popup to make sure everything has the time to be painted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83912

--- testing/talos/talos/tests/devtools/addon/content/damp-tests.js
+++ testing/talos/talos/tests/devtools/addon/content/damp-tests.js
@@ -144,12 +144,11 @@ module.exports = [
     description:
       "Measure time for a bunch of sync console.log statements to appear",
   },
-  // Bug 1480201 - disable test due to frequent failures
-  //  {
-  //    name: "console.autocomplete",
-  //    path: "webconsole/autocomplete.js",
-  //    description: "Measure time for autocomplete popup to appear",
-  //  },
+  {
+    name: "console.autocomplete",
+    path: "webconsole/autocomplete.js",
+    description: "Measure time for autocomplete popup to appear",
+  },
   {
     name: "console.streamlog",
     path: "webconsole/streamlog.js",
