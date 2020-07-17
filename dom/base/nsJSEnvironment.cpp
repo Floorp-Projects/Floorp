7578383b8ef1e7b97c7a0197ec714c3f6f0de8c9
Jon Coppeard <jcoppeard@mozilla.com>
2020-07-17 17:36:04 +0000

    Bug 1648453 - Fix build errors caused by MacOS headers defining check macro r=smaug
    These patches have triggered a case where a MacOS header defines 'check' as a macro which then conflicts with use of 'check' as a method name in AccessCheck.h, probably due to unified builds.
    
    This was fixed independently in a couple of places before, but I think it makes sense to move the fix to AccessCheck.h itself.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83796

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -64,13 +64,7 @@
 #include "mozilla/CycleCollectedJSRuntime.h"
 #include "nsRefreshDriver.h"
 #include "nsJSPrincipals.h"
-
-#ifdef XP_MACOSX
-// AssertMacros.h defines 'check' and conflicts with AccessCheck.h
-#  undef check
-#endif
 #include "AccessCheck.h"
-
 #include "mozilla/Logging.h"
 #include "prthread.h"
 
