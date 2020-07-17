82b32f112ec0d24fa7afab1e33ec38cfce4f0e9c
Cameron McCormack <cam@mcc.id.au>
2020-07-17 09:56:53 +0000

    Bug 1643246 - Don't use attribute selectors for determining if a select is a drop down or a list box. r=emilio
    Instead add a pseudo-class that does the expected size="" attribute parsing.
    
    Removing the Gtk-specific rule setting the text color since it doesn't
    seem to have any effect currently.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83448

--- layout/style/ServoElementSnapshot.h
+++ layout/style/ServoElementSnapshot.h
@@ -142,6 +142,11 @@ class ServoElementSnapshot {
     return mIsMozBrowserFrame;
   }
 
+  bool IsSelectListBox() const {
+    MOZ_ASSERT(HasOtherPseudoClassState());
+    return mIsSelectListBox;
+  }
+
  private:
   // TODO: Profile, a 1 or 2 element AutoTArray could be worth it, given we know
   // we're dealing with attribute changes when we take snapshots of attributes,
@@ -156,6 +161,7 @@ class ServoElementSnapshot {
   bool mSupportsLangAttr : 1;
   bool mIsTableBorderNonzero : 1;
   bool mIsMozBrowserFrame : 1;
+  bool mIsSelectListBox : 1;
   bool mClassAttributeChanged : 1;
   bool mIdAttributeChanged : 1;
 };
