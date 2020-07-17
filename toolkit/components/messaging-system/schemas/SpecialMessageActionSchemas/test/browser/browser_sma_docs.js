e8058e000774b037befd54afe5662622081d83ba
Andrei Oprea <andrei.br92@gmail.com>
2020-07-17 20:23:48 +0000

    Bug 1652811 - Use mozilla/nimbus-shared repo for source of schema validation r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D83789

--- toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_docs.js
+++ toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_docs.js
@@ -1,10 +1,6 @@
 const TEST_URL =
   "https://example.com/browser/toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/SpecialMessageActionSchemas.md";
 
-const { SpecialMessageActionSchemas } = ChromeUtils.import(
-  "resource://testing-common/SpecialMessageActionSchemas.js"
-);
-
 function getHeadingsFromDocs(docs) {
   const re = /### `(\w+)`/g;
   const found = [];
@@ -22,10 +18,13 @@ add_task(async function test_sma_docs() {
   let request = await fetch(TEST_URL);
   let docs = await request.text();
   let headings = getHeadingsFromDocs(docs);
-  for (let action_name of Object.keys(SpecialMessageActionSchemas)) {
+  const schemaTypes = (await fetchSMASchema).anyOf.map(
+    s => s.properties.type.enum[0]
+  );
+  for (let schemaType of schemaTypes) {
     Assert.ok(
-      headings.includes(action_name),
-      `${action_name} not found in SpecialMessageActionSchemas.md`
+      headings.includes(schemaType),
+      `${schemaType} not found in SpecialMessageActionSchemas.md`
     );
   }
 });
