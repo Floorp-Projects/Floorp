09621c985d6ada2d6e076dcb3ddcc7677d6e3085
Butkovits Atila <abutkovits@mozilla.com>
2020-07-17 21:21:51 +0300

    Backed out changeset f5d8cb66e31b (bug 1652811) for causing failure at browser_asrouter_trigger_listeners.js. CLOSED TREE

--- toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_open_about_page.js
+++ toolkit/components/messaging-system/schemas/SpecialMessageActionSchemas/test/browser/browser_sma_open_about_page.js
@@ -10,7 +10,7 @@ add_task(async function test_OPEN_ABOUT_PAGE() {
   );
   await SMATestUtils.executeAndValidateAction({
     type: "OPEN_ABOUT_PAGE",
-    data: { args: "logins", entrypoint: "foo=bar", where: "tabshifted" },
+    data: { args: "logins", entrypoint: "foo=bar" },
   });
 
   const tab = await tabPromise;
