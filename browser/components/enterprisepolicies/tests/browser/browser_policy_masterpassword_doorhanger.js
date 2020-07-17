a804bfb1839d4b54d7ddf91092ae13e5393272ff
Michael Kaply <mozilla@kaply.com>
2020-07-17 14:44:54 +0000

    Bug 1653584 - Rename MasterPassword policy to PrimaryPassword. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D83957

--- browser/components/enterprisepolicies/tests/browser/browser_policy_masterpassword_doorhanger.js
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_masterpassword_doorhanger.js
@@ -13,7 +13,7 @@ Services.scriptloader.loadSubScript(
 add_task(async function test_policy_masterpassword_doorhanger() {
   await setupPolicyEngineWithJson({
     policies: {
-      MasterPassword: true,
+      PrimaryPassword: true,
     },
   });
 
