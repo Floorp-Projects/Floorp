c2ca6c734881b947e8e291ceb042aa1cedacdf31
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-07-18 04:51:06 +0000

    No bug: [taskgraph] Simplify bugbug strategies; r=ahal
    This change the strategy aliases that have bugbug, so that they handle the extra
    arguments that are needed by bugbug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83438

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -1807,11 +1807,7 @@ def make_job_description(config, tasks):
         elif set(schedules) & set(INCLUSIVE_COMPONENTS):
             jobdesc['optimization'] = {'test-inclusive': schedules}
         else:
-            # First arg goes to 'skip-unless-schedules', second goes to the
-            # main test strategy. Using an empty dict allows earlier
-            # substrategies (of a CompositeStrategy) to pass values by reference
-            # to later substrategies.
-            jobdesc['optimization'] = {'test': (schedules, {})}
+            jobdesc['optimization'] = {'test': schedules}
 
         run = jobdesc['run'] = {}
         run['using'] = 'mozharness-test'
