8af08e8f2f15046a72027e4cc785672f86c91378
Thomas Wisniewski <twisniewski@mozilla.com>
2020-07-18 11:12:13 +0000

    Bug 1637329 - add support for shimming resources blocking by tracking protection to webcompat built-in addon; r=dimi,robwu,webcompat-reviewers,denschub,miketaylr
    Differential Revision: https://phabricator.services.mozilla.com/D77724

---
+++ browser/extensions/webcompat/shims/rich-relevance.js
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+/**
+ * Bug 1449347 - Rich Relevance
+ */
+
+"use strict";
+
+if (!window.r3_common) {
+  const noopfn = () => {};
+
+  window.rr_flush_onload = noopfn;
+  window.r3 = noopfn;
+  window.r3_home = noopfn;
+  window.RR = noopfn;
+  window.r3_common = function() {};
+  window.r3_common.prototype = {
+    addContext: noopfn,
+    addPlacementType: noopfn,
+    setUserId: noopfn,
+    setSessionId: noopfn,
+    setClickthruServer: noopfn,
+    setBaseUrl: noopfn,
+    setApiKey: noopfn,
+  };
+}
