09c4f9d7e82993b695b10ee55c9b249388b14981
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-07-20 17:22:20 +0300

    Backed out changeset 703fa1371dd4 (bug 1652641) for bc failures on browser_multiselect_tabs_close.js . CLOSED TREE

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -6500,9 +6500,6 @@ var TabContextMenu = {
 
     let disabled = gBrowser.tabs.length == 1;
     let multiselectionContext = this.contextTab.multiselected;
-    let tabCountInfo = JSON.stringify({
-      tabCount: (multiselectionContext && gBrowser.multiSelectedTabsCount) || 1,
-    });
 
     var menuItems = aPopupMenu.getElementsByAttribute(
       "tbattr",
@@ -6554,8 +6551,11 @@ var TabContextMenu = {
     let contextMoveTabOptions = document.getElementById(
       "context_moveTabOptions"
     );
-    contextMoveTabOptions.setAttribute("data-l10n-args", tabCountInfo);
     contextMoveTabOptions.disabled = gBrowser.allTabsSelected();
+    document.l10n.setAttributes(
+      contextMoveTabOptions,
+      multiselectionContext ? "move-tabs" : "move-tab"
+    );
     let selectedTabs = gBrowser.selectedTabs;
     let contextMoveTabToEnd = document.getElementById("context_moveToEnd");
     let allSelectedTabsAdjacent = selectedTabs.every(
@@ -6607,10 +6607,11 @@ var TabContextMenu = {
     document.getElementById("context_closeOtherTabs").disabled =
       unpinnedTabsToClose < 1;
 
-    // Update the close item with how many tabs will close.
-    document
-      .getElementById("context_closeTab")
-      .setAttribute("data-l10n-args", tabCountInfo);
+    // Only one of close_tab/close_selected_tabs should be visible
+    document.getElementById("context_closeTab").hidden = multiselectionContext;
+    document.getElementById(
+      "context_closeSelectedTabs"
+    ).hidden = !multiselectionContext;
 
     // Hide "Bookmark Tab" for multiselection.
     // Update its state if visible.
@@ -6773,14 +6774,6 @@ var TabContextMenu = {
       if (tab.muted && !newTab.muted) {
         newTab.toggleMuteAudio(tab.muteReason);
       }
-    }
-  },
-
-  closeContextTabs(event) {
-    if (this.contextTab.multiselected) {
-      gBrowser.removeMultiSelectedTabs();
-    } else {
-      gBrowser.removeTab(this.contextTab, { animate: true });
     }
   },
 };
