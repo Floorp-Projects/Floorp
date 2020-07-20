b7ff0287e68515c525feac5a661ad354877e6527
Nathan Froyd <froydnj@mozilla.com>
2020-07-20 21:46:52 +0000

    Bug 1652782 - add a taskcluster task for building dump_syms for Mac; r=firefox-build-system-reviewers,rstewart
    We don't need this for our (current) builds, which are cross-builds, but we
    would need this at some future date if we ditched the breakpad `dump_syms`.
    
    Depends on D83528
    
    Differential Revision: https://phabricator.services.mozilla.com/D83529

--- taskcluster/scripts/misc/build-dump-syms.sh
+++ taskcluster/scripts/misc/build-dump-syms.sh
@@ -2,20 +2,17 @@
 set -x -e -v
 
 PROJECT=dump_syms
+# Needed by osx-cross-linker.
+export TARGET=$1
 
 # This script is for building dump_syms
 case "$(uname -s)" in
 Linux)
     COMPRESS_EXT=xz
-    export RUSTFLAGS=-Clinker=clang++
-    export CXX=clang++
-    export PATH=$MOZ_FETCHES_DIR/clang/bin:$MOZ_FETCHES_DIR/binutils/bin:$PATH
-    FEATURES=vendored-openssl
     ;;
 MINGW*)
     UPLOAD_DIR=$PWD/public/build
     COMPRESS_EXT=bz2
-    FEATURES=
 
     . $GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh
     ;;
@@ -31,10 +28,35 @@ PATH="$(cd $MOZ_FETCHES_DIR && pwd)/rustc/bin:$PATH"
 
 cd $MOZ_FETCHES_DIR/$PROJECT
 
-cargo build --verbose --release ${FEATURES:+--features "$FEATURES"}
+case "$(uname -s)" in
+Linux)
+    if [ "$TARGET" == "x86_64-apple-darwin" ]; then
+        export PATH="$MOZ_FETCHES_DIR/llvm-dsymutil/bin:$PATH"
+        export PATH="$MOZ_FETCHES_DIR/cctools/bin:$PATH"
+        export RUSTFLAGS="-C linker=$GECKO_PATH/taskcluster/scripts/misc/osx-cross-linker"
+        export CC="$MOZ_FETCHES_DIR/clang/bin/clang"
+        export TARGET_CC="$MOZ_FETCHES_DIR/clang/bin/clang -isysroot $MOZ_FETCHES_DIR/MacOSX10.11.sdk"
+        export TARGET_CXX="$MOZ_FETCHES_DIR/clang/bin/clang++ -isysroot $MOZ_FETCHES_DIR/MacOSX10.11.sdk -stdlib=libc++"
+        cargo build --verbose --release --target $TARGET
+    else
+        export RUSTFLAGS=-Clinker=clang++
+        export CC=clang
+        export CXX=clang++
+        export PATH="$MOZ_FETCHES_DIR/clang/bin:$MOZ_FETCHES_DIR/binutils/bin:$PATH"
+        cargo build --verbose --release --features "vendored-openssl"
+    fi
+    ;;
+MINGW*)
+    cargo build --verbose --release
+    ;;
+esac
 
 mkdir $PROJECT
-cp target/release/${PROJECT}* ${PROJECT}/
+PROJECT_OUT=target/release/${PROJECT}*
+if [ -n "$TARGET" ]; then
+    PROJECT_OUT=target/${TARGET}/release/${PROJECT}*
+fi
+cp ${PROJECT_OUT} ${PROJECT}/
 tar -acf ${PROJECT}.tar.$COMPRESS_EXT $PROJECT
 mkdir -p $UPLOAD_DIR
 cp ${PROJECT}.tar.$COMPRESS_EXT $UPLOAD_DIR
