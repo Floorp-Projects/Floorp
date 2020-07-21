87e3c0e605f7e238e35c2931acbc4ef1def7a46e
longsonr <longsonr@gmail.com>
2020-07-21 14:17:44 +0000

    Bug 1201214 - Deal with AppendElement failing if it's called in a fallible way r=jwatt
    Fortunately bug 1631371 has marked the methods and SVGPathData::GetSegmentLengths is dead code
    
    Differential Revision: https://phabricator.services.mozilla.com/D84092

--- dom/svg/SVGPathData.cpp
+++ dom/svg/SVGPathData.cpp
@@ -135,25 +135,6 @@ uint32_t SVGPathData::CountItems() const {
 }
 #endif
 
-bool SVGPathData::GetSegmentLengths(nsTArray<double>* aLengths) const {
-  aLengths->Clear();
-  SVGPathTraversalState state;
-
-  uint32_t i = 0;
-  while (i < mData.Length()) {
-    state.length = 0.0;
-    SVGPathSegUtils::TraversePathSegment(&mData[i], state);
-    // XXX(Bug 1631371) Check if this should use a fallible operation as it
-    // pretended earlier.
-    aLengths->AppendElement(state.length);
-    i += 1 + SVGPathSegUtils::ArgCountForType(mData[i]);
-  }
-
-  MOZ_ASSERT(i == mData.Length(), "Very, very bad - mData corrupt");
-
-  return true;
-}
-
 bool SVGPathData::GetDistancesFromOriginToEndsOfVisibleSegments(
     FallibleTArray<double>* aOutput) const {
   SVGPathTraversalState state;
