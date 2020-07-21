2c1c9b5c934b23d567fd21724926c16c67aa0f76
Emily McDonough <emcdonough@mozilla.com>
2020-07-21 23:39:45 +0000

    Bug 1653433 Part 2 - Use CUPS header in the CUPSShim to get struct declarations and function signatures. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D84033

--- widget/nsCUPSShim.h
+++ widget/nsCUPSShim.h
@@ -7,36 +7,7 @@
 #ifndef nsCUPSShim_h___
 #define nsCUPSShim_h___
 
-/* Various CUPS data types. We don't #include cups headers to avoid
- * requiring CUPS to be installed on the build host (and to avoid having
- * to test for CUPS in configure).
- */
-typedef struct /**** Printer Options ****/
-{
-  char* name;  /* Name of option */
-  char* value; /* Value of option */
-} cups_option_t;
-
-typedef struct /**** Destination ****/
-{
-  char *name,             /* Printer or class name */
-      *instance;          /* Local instance name or nullptr */
-  int is_default;         /* Is this printer the default? */
-  int num_options;        /* Number of options */
-  cups_option_t* options; /* Options */
-} cups_dest_t;
-
-typedef cups_dest_t* (*CupsGetDestType)(const char* printer,
-                                        const char* instance, int num_dests,
-                                        cups_dest_t* dests);
-typedef int (*CupsGetDestsType)(cups_dest_t** dests);
-typedef int (*CupsFreeDestsType)(int num_dests, cups_dest_t* dests);
-typedef int (*CupsPrintFileType)(const char* printer, const char* filename,
-                                 const char* title, int num_options,
-                                 cups_option_t* options);
-typedef int (*CupsTempFdType)(char* filename, int length);
-typedef int (*CupsAddOptionType)(const char* name, const char* value,
-                                 int num_options, cups_option_t** options);
+#include <cups/cups.h>
 
 struct PRLibrary;
 
@@ -62,12 +33,12 @@ class nsCUPSShim {
   /* Function pointers for supported functions. These are only
    * valid after successful initialization.
    */
-  CupsAddOptionType mCupsAddOption;
-  CupsFreeDestsType mCupsFreeDests;
-  CupsGetDestType mCupsGetDest;
-  CupsGetDestsType mCupsGetDests;
-  CupsPrintFileType mCupsPrintFile;
-  CupsTempFdType mCupsTempFd;
+  decltype(cupsAddOption)* mCupsAddOption;
+  decltype(cupsFreeDests)* mCupsFreeDests;
+  decltype(cupsGetDest)* mCupsGetDest;
+  decltype(cupsGetDests)* mCupsGetDests;
+  decltype(cupsPrintFile)* mCupsPrintFile;
+  decltype(cupsTempFd)* mCupsTempFd;
 
  private:
   bool mInited = false;
