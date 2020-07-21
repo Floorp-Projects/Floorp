ebc4f4e1ab130841b4b6bedd22b2c5585c3b030c
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-07-21 18:03:13 +0000

    Bug 1492797 - Autocomplete for classes panel. r=rcaliman,jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D71160

--- devtools/client/inspector/rules/views/class-list-previewer.js
+++ devtools/client/inspector/rules/views/class-list-previewer.js
@@ -5,11 +5,13 @@
 "use strict";
 
 const ClassList = require("devtools/client/inspector/rules/models/class-list");
-const { LocalizationHelper } = require("devtools/shared/l10n");
 
+const { LocalizationHelper } = require("devtools/shared/l10n");
 const L10N = new LocalizationHelper(
   "devtools/client/locales/inspector.properties"
 );
+const AutocompletePopup = require("devtools/client/shared/autocomplete-popup");
+const { debounce } = require("devtools/shared/debounce");
 
 /**
  * This UI widget shows a textfield and a series of checkboxes in the rule-view. It is
@@ -30,6 +32,11 @@ class ClassListPreviewer {
     this.onNewSelection = this.onNewSelection.bind(this);
     this.onCheckBoxChanged = this.onCheckBoxChanged.bind(this);
     this.onKeyPress = this.onKeyPress.bind(this);
+    this.onAddElementInputModified = debounce(
+      this.onAddElementInputModified,
+      75,
+      this
+    );
     this.onCurrentNodeClassChanged = this.onCurrentNodeClassChanged.bind(this);
 
     // Create the add class text field.
@@ -41,12 +48,29 @@ class ClassListPreviewer {
       L10N.getStr("inspector.classPanel.newClass.placeholder")
     );
     this.addEl.addEventListener("keypress", this.onKeyPress);
+    this.addEl.addEventListener("input", this.onAddElementInputModified);
     this.containerEl.appendChild(this.addEl);
 
     // Create the class checkboxes container.
     this.classesEl = this.doc.createElement("div");
     this.classesEl.classList.add("classes");
     this.containerEl.appendChild(this.classesEl);
+
+    // Create the autocomplete popup
+    this.autocompletePopup = new AutocompletePopup(this.inspector.toolbox.doc, {
+      listId: "inspector_classListPreviewer_autocompletePopupListBox",
+      position: "bottom",
+      autoSelect: false,
+      useXulWrapper: true,
+      input: this.addEl,
+      onClick: (e, item) => {
+        if (item) {
+          this.addEl.value = item.label;
+          this.autocompletePopup.hidePopup();
+          this.autocompletePopup.clearItems();
+        }
+      },
+    });
 
     // Start listening for interesting events.
     this.inspector.selection.on("new-node-front", this.onNewSelection);
@@ -59,7 +83,10 @@ class ClassListPreviewer {
   destroy() {
     this.inspector.selection.off("new-node-front", this.onNewSelection);
     this.addEl.removeEventListener("keypress", this.onKeyPress);
+    this.addEl.removeEventListener("input", this.onAddElementInputModified);
     this.containerEl.removeEventListener("input", this.onCheckBoxChanged);
+
+    this.autocompletePopup.destroy();
 
     this.containerEl.innerHTML = "";
 
@@ -155,22 +182,77 @@ class ClassListPreviewer {
   }
 
   onKeyPress(event) {
-    if (event.key !== "Enter" || this.addEl.value === "") {
+    // If the popup is already open, all the keyboard interaction are handled
+    // directly by the popup component.
+    if (this.autocompletePopup.isOpen) {
       return;
     }
 
-    this.model
-      .addClassName(this.addEl.value)
-      .then(() => {
-        this.render();
-        this.addEl.value = "";
-      })
-      .catch(e => {
-        // Only log the error if the panel wasn't destroyed in the meantime.
-        if (this.containerEl) {
-          console.error(e);
-        }
+    // Open the autocomplete popup on Ctrl+Space / ArrowDown (when the input isn't empty)
+    if (
+      (this.addEl.value && event.key === " " && event.ctrlKey) ||
+      event.key === "ArrowDown"
+    ) {
+      this.onAddElementInputModified();
+      return;
+    }
+
+    if (this.addEl.value !== "" && event.key === "Enter") {
+      this.addClassName(this.addEl.value);
+    }
+  }
+
+  async onAddElementInputModified() {
+    const newValue = this.addEl.value;
+
+    // if the input is empty, let's close the popup, if it was open.
+    if (newValue === "") {
+      if (this.autocompletePopup.isOpen) {
+        this.autocompletePopup.hidePopup();
+        this.autocompletePopup.clearItems();
+      }
+      return;
+    }
+
+    // Otherwise, we need to update the popup items to match the new input.
+    let items = [];
+    try {
+      const classNames = await this.model.getClassNames(newValue);
+      items = classNames.map(className => {
+        return {
+          preLabel: className.substring(0, newValue.length),
+          label: className,
+        };
       });
+    } catch (e) {
+      // If there was an error while retrieving the classNames, we'll simply NOT show the
+      // popup, which is okay.
+      console.warn("Error when calling getClassNames", e);
+    }
+
+    if (
+      items.length == 0 ||
+      (items.length == 1 && items[0].label === newValue)
+    ) {
+      this.autocompletePopup.clearItems();
+      this.autocompletePopup.hidePopup();
+    } else {
+      this.autocompletePopup.setItems(items);
+      this.autocompletePopup.openPopup();
+    }
+  }
+
+  async addClassName(className) {
+    try {
+      await this.model.addClassName(className);
+      this.render();
+      this.addEl.value = "";
+    } catch (e) {
+      // Only log the error if the panel wasn't destroyed in the meantime.
+      if (this.containerEl) {
+        console.error(e);
+      }
+    }
   }
 
   onNewSelection() {
