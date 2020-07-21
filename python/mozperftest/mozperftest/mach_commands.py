cb88926dd6209369d266e3232b175773bafa34b4
Tarek Ziadé <tarek@mozilla.com>
2020-07-21 12:51:58 +0000

    Bug 1651624 - Add a macOS layer r=sparky
    Adds a macOS layer that provides the ability to
    mount DMGs on the fly. For instance Firefox's distribution.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84162

--- python/mozperftest/mozperftest/mach_commands.py
+++ python/mozperftest/mozperftest/mach_commands.py
@@ -82,21 +82,25 @@ class PerftestTests(MachCommandBase):
         reimporting modules and produce wrong coverage info.
         """
         display = kw.pop("display", False)
+        verbose = kw.pop("verbose", False)
         args = [self.virtualenv_manager.python_path, "-m", module] + list(args)
         sys.stdout.write("=> %s " % kw.pop("label", module))
         sys.stdout.flush()
         try:
+            if verbose:
+                sys.stdout.write("\nRunning %s\n" % " ".join(args))
+                sys.stdout.flush()
             output = subprocess.check_output(args, stderr=subprocess.STDOUT)
             if display:
-                print()
+                sys.stdout.write("\n")
                 for line in output.split(b"\n"):
-                    print(line.decode("utf8"))
+                    sys.stdout.write(line.decode("utf8") + "\n")
             sys.stdout.write("[OK]\n")
             sys.stdout.flush()
             return True
         except subprocess.CalledProcessError as e:
             for line in e.output.split(b"\n"):
-                print(line.decode("utf8"))
+                sys.stdout.write(line.decode("utf8") + "\n")
             sys.stdout.write("[FAILED]\n")
             sys.stdout.flush()
             return False
@@ -125,6 +129,7 @@ class PerftestTests(MachCommandBase):
         from mozperftest.utils import install_package, temporary_env
 
         skip_linters = kwargs.get("skip_linters", False)
+        verbose = kwargs.get("verbose", False)
 
         # include in sys.path all deps
         _setup_path()
@@ -198,7 +203,9 @@ class PerftestTests(MachCommandBase):
                 options,
                 tests,
             ]
-            assert self._run_python_script("coverage", *args, label="running tests")
+            assert self._run_python_script(
+                "coverage", *args, label="running tests", verbose=verbose
+            )
             if run_coverage_check and not self._run_python_script(
                 "coverage", "report", display=True
             ):
---
+++ python/mozperftest/mozperftest/tests/test_macos.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+import mozunit
+from unittest import mock
+import platform
+import subprocess
+import pytest
+from pathlib import Path
+import os
+
+from mozperftest.tests.support import get_running_env, DMG
+from mozperftest.system.macos import MacosDevice
+
+
+def run_proc(*args, **kw):
+    if args[0][1] == "attach":
+        where = args[0][4]
+        bindir = Path(where, "firefox.app", "Contents", "MacOS")
+        os.makedirs(str(bindir))
+        firefox_bin = bindir / "firefox"
+        with firefox_bin.open("w") as f:
+            f.write("OK")
+
+
+def mock_calls(test):
+    # on macOS we don't mock the system calls
+    # so we're mounting for real using hdiutil
+    if platform.system() == "Darwin":
+        return test
+
+    # on other platforms, we're unsing run_proc
+    @mock.patch("mozperftest.system.macos.MacosDevice._run_process", new=run_proc)
+    def wrapped(*args, **kw):
+        return test(*args, **kw)
+
+
+@mock_calls
+def test_mount_dmg():
+    mach_cmd, metadata, env = get_running_env(browsertime_binary=str(DMG))
+    device = MacosDevice(env, mach_cmd)
+    try:
+        device.run(metadata)
+    finally:
+        device.teardown()
+
+    target = Path(DMG.parent, "firefox", "Contents", "MacOS", "firefox")
+    assert env.get_arg("browsertime-binary") == str(target)
+
+
+def run_fail(cmd):
+    def _run_fail(self, args):
+        run_cmd = " ".join(args)
+        if cmd not in run_cmd:
+            run_proc(args)
+            return
+        raise subprocess.CalledProcessError(returncode=2, cmd=" ".join(args))
+
+    return _run_fail
+
+
+@mock.patch("mozperftest.system.macos.MacosDevice._run_process", new=run_fail("attach"))
+def test_attach_fails():
+    mach_cmd, metadata, env = get_running_env(browsertime_binary=str(DMG))
+    device = MacosDevice(env, mach_cmd)
+
+    with pytest.raises(subprocess.CalledProcessError):
+        try:
+            device.run(metadata)
+        finally:
+            device.teardown()
+
+
+@mock.patch("mozperftest.system.macos.MacosDevice._run_process", new=run_fail("detach"))
+def test_detach_fails():
+    mach_cmd, metadata, env = get_running_env(browsertime_binary=str(DMG))
+    device = MacosDevice(env, mach_cmd)
+    # detaching will be swallowed
+    try:
+        device.run(metadata)
+    finally:
+        device.teardown()
+
+    target = Path(DMG.parent, "firefox", "Contents", "MacOS", "firefox")
+    assert env.get_arg("browsertime-binary") == str(target)
+
+
+def test_no_op():
+    mach_cmd, metadata, env = get_running_env(browsertime_binary="notadmg")
+    device = MacosDevice(env, mach_cmd)
+    device.run(metadata)
+
+
+if __name__ == "__main__":
+    mozunit.main()
