1fcee565184bc83144b63f04c7e3b1dd33749a86
Kershaw Chang <kershaw@mozilla.com>
2020-07-21 08:35:41 +0000

    Bug 1653642 - Don't release WebSocketChannel in nsWebSocketConnection::Close, r=michal,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D84127

--- netwerk/protocol/websocket/nsWebSocketConnection.cpp
+++ netwerk/protocol/websocket/nsWebSocketConnection.cpp
@@ -20,7 +20,13 @@ nsWebSocketConnection::nsWebSocketConnection(
       mSocketIn(aInputStream),
       mSocketOut(aOutputStream),
       mWriteOffset(0),
-      mStartReadingCalled(false) {}
+      mStartReadingCalled(false) {
+  LOG(("nsWebSocketConnection ctor %p\n", this));
+}
+
+nsWebSocketConnection::~nsWebSocketConnection() {
+  LOG(("nsWebSocketConnection dtor %p\n", this));
+}
 
 NS_IMETHODIMP
 nsWebSocketConnection::Init(nsIWebSocketConnectionListener* aListener,
@@ -47,6 +53,7 @@ nsWebSocketConnection::Init(nsIWebSocketConnectionListener* aListener,
 
 NS_IMETHODIMP
 nsWebSocketConnection::Close() {
+  LOG(("nsWebSocketConnection::Close %p\n", this));
   if (mTransport) {
     mTransport->SetSecurityCallbacks(nullptr);
     mTransport->SetEventSink(nullptr, nullptr);
@@ -66,7 +73,6 @@ nsWebSocketConnection::Close() {
     mSocketOut = nullptr;
   }
 
-  mListener = nullptr;
   return NS_OK;
 }
 
@@ -153,11 +159,12 @@ NS_IMETHODIMP
 nsWebSocketConnection::OnInputStreamReady(nsIAsyncInputStream* aStream) {
   LOG(("nsWebSocketConnection::OnInputStreamReady() %p\n", this));
   MOZ_ASSERT(mEventTarget->IsOnCurrentThread());
+  MOZ_ASSERT(mListener);
 
-  if (!mSocketIn)  // did we we clean up the socket after scheduling InputReady?
+  // did we we clean up the socket after scheduling InputReady?
+  if (!mSocketIn) {
     return NS_OK;
-
-  if (!mListener) return NS_OK;
+  }
 
   // this is after the http upgrade - so we are speaking websockets
   uint8_t buffer[2048];
@@ -198,8 +205,11 @@ NS_IMETHODIMP
 nsWebSocketConnection::OnOutputStreamReady(nsIAsyncOutputStream* aStream) {
   LOG(("nsWebSocketConnection::OnOutputStreamReady() %p\n", this));
   MOZ_ASSERT(mEventTarget->IsOnCurrentThread());
+  MOZ_ASSERT(mListener);
 
-  if (!mListener) return NS_OK;
+  if (!mSocketOut) {
+    return NS_OK;
+  }
 
   while (!mOutputQueue.empty()) {
     const OutputData& data = mOutputQueue.front();
