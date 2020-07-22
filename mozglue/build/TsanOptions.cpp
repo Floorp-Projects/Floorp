4f8381bd1fc7c6c123c3abb4a3d4c05a2aeda986
Byron Campen [:bwc] <docfaraday@gmail.com>
2020-07-22 15:21:41 +0000

    Bug 1648606: Suppress flag-checking race in libusrsctp's sctp_close. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D84366

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -305,6 +305,10 @@ extern "C" const char* __tsan_default_suppressions() {
          // See bug 1648604
          "race:system_base_info\n"
 
+         // See bug 1648606
+         "race:sctp_close\n"
+         "race:sctp_iterator_work\n"
+
       // End of suppressions.
       ;  // Please keep this semicolon.
 }
