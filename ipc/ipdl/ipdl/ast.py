007462d7756543fe5f8173953c74d0f45518f181
Simon Giesecke <sgiesecke@mozilla.com>
2020-07-22 09:11:52 +0000

    Bug 1654181 - Add uncomparable keyword for structs and unions. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D84298

--- ipc/ipdl/ipdl/ast.py
+++ ipc/ipdl/ipdl/ast.py
@@ -268,9 +268,10 @@ class StructField(Node):
 
 
 class StructDecl(NamespacedNode):
-    def __init__(self, loc, name, fields):
+    def __init__(self, loc, name, fields, comparable):
         NamespacedNode.__init__(self, loc, name)
         self.fields = fields
+        self.comparable = comparable
         # A list of indices into `fields` for determining the order in
         # which fields are laid out in memory.  We don't just reorder
         # `fields` itself so as to keep the ordering reasonably stable
@@ -279,9 +280,10 @@ class StructDecl(NamespacedNode):
 
 
 class UnionDecl(NamespacedNode):
-    def __init__(self, loc, name, components):
+    def __init__(self, loc, name, components, comparable):
         NamespacedNode.__init__(self, loc, name)
         self.components = components
+        self.comparable = comparable
 
 
 class Manager(Node):
