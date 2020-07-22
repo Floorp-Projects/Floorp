6620e9e829cc5eb3cba34f024696a0096308da53
Bryce Seager van Dyk <bvandyk@mozilla.com>
2020-07-22 17:13:23 +0000

    Bug 1654383 - Replace GMPEncryptionType with cdm::EncryptionScheme. r=alwu,dminor
    This removes the need to do some conversions and simplifies the code a little.
    
    Drive by remove an assert which is already covered by a switch statement
    containing a MOZ_ASSERT_UNREACHABLE in ChromiumCDMParent.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84432

--- dom/media/gmp/ChromiumCDMChild.cpp
+++ dom/media/gmp/ChromiumCDMChild.cpp
@@ -521,32 +521,15 @@ mozilla::ipc::IPCResult ChromiumCDMChild::RecvGetStatusForPolicy(
   return IPC_OK();
 }
 
-static cdm::EncryptionScheme ConvertToCdmEncryptionScheme(
-    const GMPEncryptionScheme& aEncryptionScheme) {
-  switch (aEncryptionScheme) {
-    case GMPEncryptionScheme::kGMPEncryptionNone:
-      return cdm::EncryptionScheme::kUnencrypted;
-    case GMPEncryptionScheme::kGMPEncryptionCenc:
-      return cdm::EncryptionScheme::kCenc;
-    case GMPEncryptionScheme::kGMPEncryptionCbcs:
-      return cdm::EncryptionScheme::kCbcs;
-    default:
-      MOZ_ASSERT_UNREACHABLE("Cannot convert invalid encryption scheme!");
-      return cdm::EncryptionScheme::kUnencrypted;
-  }
-}
-
 static void InitInputBuffer(const CDMInputBuffer& aBuffer,
                             nsTArray<cdm::SubsampleEntry>& aSubSamples,
                             cdm::InputBuffer_2& aInputBuffer) {
   aInputBuffer.data = aBuffer.mData().get<uint8_t>();
   aInputBuffer.data_size = aBuffer.mData().Size<uint8_t>();
 
-  if (aBuffer.mEncryptionScheme() > GMPEncryptionScheme::kGMPEncryptionNone) {
-    MOZ_ASSERT(aBuffer.mEncryptionScheme() ==
-                   GMPEncryptionScheme::kGMPEncryptionCenc ||
-               aBuffer.mEncryptionScheme() ==
-                   GMPEncryptionScheme::kGMPEncryptionCbcs);
+  if (aBuffer.mEncryptionScheme() != cdm::EncryptionScheme::kUnencrypted) {
+    MOZ_ASSERT(aBuffer.mEncryptionScheme() == cdm::EncryptionScheme::kCenc ||
+               aBuffer.mEncryptionScheme() == cdm::EncryptionScheme::kCbcs);
     aInputBuffer.key_id = aBuffer.mKeyId().Elements();
     aInputBuffer.key_id_size = aBuffer.mKeyId().Length();
 
@@ -560,8 +543,7 @@ static void InitInputBuffer(const CDMInputBuffer& aBuffer,
     }
     aInputBuffer.subsamples = aSubSamples.Elements();
     aInputBuffer.num_subsamples = aSubSamples.Length();
-    aInputBuffer.encryption_scheme =
-        ConvertToCdmEncryptionScheme(aBuffer.mEncryptionScheme());
+    aInputBuffer.encryption_scheme = aBuffer.mEncryptionScheme();
   }
   aInputBuffer.pattern.crypt_byte_block = aBuffer.mCryptByteBlock();
   aInputBuffer.pattern.skip_byte_block = aBuffer.mSkipByteBlock();
@@ -666,8 +648,7 @@ mozilla::ipc::IPCResult ChromiumCDMChild::RecvInitializeVideoDecoder(
   nsTArray<uint8_t> extraData(aConfig.mExtraData().Clone());
   config.extra_data = extraData.Elements();
   config.extra_data_size = extraData.Length();
-  config.encryption_scheme =
-      ConvertToCdmEncryptionScheme(aConfig.mEncryptionScheme());
+  config.encryption_scheme = aConfig.mEncryptionScheme();
   cdm::Status status = mCDM->InitializeVideoDecoder(config);
   GMP_LOG_DEBUG("ChromiumCDMChild::RecvInitializeVideoDecoder() status=%u",
                 status);
