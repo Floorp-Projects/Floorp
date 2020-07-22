8d40cc05d48d60613e2c76ef097e4fd7b9d39b56
Andrew Halberstadt <ahalberstadt@mozilla.com>
2020-07-22 15:36:55 +0000

    Bug 1636271 - [taskgraph] Move 'optimize-strategies' from try_task_config.json to a parameter, r=tomprince
    Differential Revision: https://phabricator.services.mozilla.com/D79704

--- taskcluster/taskgraph/generator.py
+++ taskcluster/taskgraph/generator.py
@@ -346,7 +346,7 @@ class TaskGraphGenerator(object):
 
         # this is used for testing experimental optimization strategies
         strategies = os.environ.get('TASKGRAPH_OPTIMIZE_STRATEGIES',
-                                    parameters['try_task_config'].get('optimize-strategies'))
+                                    parameters.get('optimize_strategies'))
         if strategies:
             strategies = find_object(strategies)
 
