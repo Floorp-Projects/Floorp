d29664c32699906683efe197aba9b44beb5c3663
Paul Bone <pbone@mozilla.com>
2020-07-22 22:33:54 +0000

    Bug 1643578 - Test that navigating to a http home page doesn't focus the address bar r=kashav
    Differential Revision: https://phabricator.services.mozilla.com/D83754

--- browser/base/content/test/tabs/browser_navigate_home_focuses_addressbar.js
+++ browser/base/content/test/tabs/browser_navigate_home_focuses_addressbar.js
@@ -3,14 +3,34 @@
 
 const TEST_HTTP = httpURL("dummy_page.html");
 
-// Test for Bug 1634272
-add_task(async function() {
+async function doNavigateHome(isHome, expectedURL, expectFocused) {
   await BrowserTestUtils.withNewTab(TEST_HTTP, async function(browser) {
     info("Tab ready");
 
     document.getElementById("home-button").click();
-    await BrowserTestUtils.browserLoaded(browser, false, HomePage.get());
-    is(gURLBar.value, "", "URL bar should be empty");
-    ok(gURLBar.focused, "URL bar should be focused");
+    await BrowserTestUtils.browserLoaded(browser, false, isHome);
+    is(gURLBar.focused, expectFocused, "URL bar should" 
+      + (expectFocused ? "" : " not") + " be focused");
+    is(gURLBar.value, expectedURL, "URL bar set correctly");
   });
+}
+
+// Test for Bug 1634272
+add_task(async function testNavHomeDefault() {
+  await doNavigateHome((url) => url === HomePage.get(), "",
+    true);
 });
+
+
+add_task(async function testNavHomePage() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.startup.homepage", "http://example.com"]]
+  });
+
+  // We don't focus the URL bar when the home page is a website.
+  await doNavigateHome((url) => url === "http://example.com/",
+      "example.com", false);
+
+  await SpecialPowers.popPrefEnv();
+});
+
