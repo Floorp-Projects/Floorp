7e78172c7fb2d5225c8abdb5db10e4586ea786d0
Emilio Cobos Álvarez <emilio@crisal.io>
2020-07-22 15:32:44 +0000

    Bug 1654270 - When an ongoing match crosses an anonymous boundary, ignore it instead of forcing the end of the match. r=jfkthame
    This restores our previous behavior when matching across anonymous
    boundaries, as that's not something we currently can support because DOM
    ranges can't represent that.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84397

--- toolkit/components/find/nsFind.cpp
+++ toolkit/components/find/nsFind.cpp
@@ -226,12 +226,6 @@ static bool NodeForcesBreak(const nsIContent& aContent) {
 }
 
 static bool ForceBreakBetweenText(const Text& aPrevious, const Text& aNext) {
-  if (!nsContentUtils::IsInSameAnonymousTree(&aPrevious, &aNext)) {
-    // TODO(emilio): This should ideally work, probably at least for finding
-    // across Shadow DOM?
-    return true;
-  }
-
   return GetBlockParent(aPrevious) != GetBlockParent(aNext);
 }
 
@@ -257,11 +251,12 @@ struct nsFind::State final {
     return node ? node->GetAsText() : nullptr;
   }
 
-  Text* GetNextNode() {
+  Text* GetNextNode(bool aAlreadyMatching) {
     if (MOZ_UNLIKELY(!mInitialized)) {
+      MOZ_ASSERT(!aAlreadyMatching);
       Initialize();
     } else {
-      Advance(Initializing::No);
+      Advance(Initializing::No, aAlreadyMatching);
       mIterOffset = -1;  // mIterOffset only really applies to the first node.
     }
     return GetCurrentNode();
@@ -271,12 +266,26 @@ struct nsFind::State final {
   enum class Initializing { No, Yes };
 
   // Advance to the next visible text-node.
-  void Advance(Initializing);
+  void Advance(Initializing, bool aAlreadyMatching);
   // Sets up the first node position and offset.
   void Initialize();
 
-  static bool ValidTextNode(const nsINode& aNode) {
-    return aNode.IsText() && !SkipNode(aNode.AsText());
+  static bool ValidTextNode(const nsINode& aNode, const Text* aPrev,
+                            bool aAlreadyMatching) {
+    if (!aNode.IsText()) {
+      return false;
+    }
+    if (SkipNode(aNode.AsText())) {
+      return false;
+    }
+    // TODO(emilio): We can't represent ranges that span native anonymous /
+    // shadow tree boundaries, but if we did the following check could / should
+    // be removed.
+    if (aAlreadyMatching && aPrev &&
+        !nsContentUtils::IsInSameAnonymousTree(&aNode, aPrev)) {
+      return false;
+    }
+    return true;
   }
 
   const bool mFindBackward;
@@ -295,7 +304,7 @@ struct nsFind::State final {
   const nsRange& mStartPoint;
 };
 
-void nsFind::State::Advance(Initializing aInitializing) {
+void nsFind::State::Advance(Initializing aInitializing, bool aAlreadyMatching) {
   MOZ_ASSERT(mInitialized);
 
   // The Advance() call during Initialize() calls us in a partial state, where
@@ -311,7 +320,7 @@ void nsFind::State::Advance(Initializing aInitializing) {
     if (!current) {
       return;
     }
-    if (ValidTextNode(*current)) {
+    if (ValidTextNode(*current, prev, aAlreadyMatching)) {
       mFoundBreak = mFoundBreak ||
                     (prev && ForceBreakBetweenText(*prev, *current->AsText()));
       return;
@@ -349,8 +358,9 @@ void nsFind::State::Initialize() {
     return;
   }
 
-  if (!ValidTextNode(*current)) {
-    Advance(Initializing::Yes);
+  const bool kAlreadyMatching = false;
+  if (!ValidTextNode(*current, nullptr, kAlreadyMatching)) {
+    Advance(Initializing::Yes, kAlreadyMatching);
     current = mIterator.GetCurrent();
     if (!current) {
       return;
@@ -502,12 +512,12 @@ bool nsFind::BreakInBetween(char32_t x, char32_t y) const {
   return mWordBreaker->BreakInBetween(x16, x16len, y16, y16len);
 }
 
-char32_t nsFind::PeekNextChar(State& aState) const {
+char32_t nsFind::PeekNextChar(State& aState, bool aAlreadyMatching) const {
   // We need to restore the necessary state before this function returns.
   StateRestorer restorer(aState);
 
   while (true) {
-    const Text* text = aState.GetNextNode();
+    const Text* text = aState.GetNextNode(aAlreadyMatching);
     if (!text || aState.ForcedBreak()) {
       return L'\0';
     }
@@ -626,7 +636,7 @@ nsFind::Find(const nsAString& aPatText, nsRange* aSearchRange,
 
     // If this is our first time on a new node, reset the pointers:
     if (!frag) {
-      current = state.GetNextNode();
+      current = state.GetNextNode(!!matchAnchorNode);
       if (!current) {
         return NS_OK;
       }
@@ -848,7 +858,7 @@ nsFind::Find(const nsAString& aPatText, nsRange* aSearchRange,
               nextChar = CHAR_TO_UNICHAR(t1b[nextfindex]);
           } else {
             // Get next character from the next node.
-            nextChar = PeekNextChar(state);
+            nextChar = PeekNextChar(state, !!matchAnchorNode);
           }
 
           if (nextChar == NBSP_CHARCODE) nextChar = CHAR_TO_UNICHAR(' ');
