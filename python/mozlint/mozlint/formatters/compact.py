97a298b1350dac4a9974c25e01d46b34d711ab05
Tom Prince <mozilla@hocat.ca>
2020-07-23 14:55:45 +0000

    Bug 1652503: [mozlint] Use `attrs` for `mozlint.result.Issue`; r=ahal
    This gives sorting on `Issue` for free, which makes it easier to write tests
    for linters.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84643

--- python/mozlint/mozlint/formatters/compact.py
+++ python/mozlint/mozlint/formatters/compact.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import attr
+
 from ..result import Issue
 
 
@@ -25,7 +27,7 @@ class CompactFormatter(object):
             for err in errors:
                 assert isinstance(err, Issue)
 
-                d = {s: getattr(err, s) for s in err.__slots__}
+                d = attr.asdict(err)
                 d["column"] = ", col %s" % d["column"] if d["column"] else ""
                 d['level'] = d['level'].capitalize()
                 d['rule'] = d['rule'] or d['linter']
--- python/mozlint/mozlint/formatters/unix.py
+++ python/mozlint/mozlint/formatters/unix.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import attr
+
 from ..result import Issue
 
 
@@ -20,7 +22,7 @@ class UnixFormatter(object):
             for err in errors:
                 assert isinstance(err, Issue)
 
-                slots = {s: getattr(err, s) for s in err.__slots__}
+                slots = attr.asdict(err)
                 slots["path"] = slots['relpath']
                 slots["column"] = "%d:" % slots["column"] if slots["column"] else ""
                 slots["rule"] = slots["rule"] or slots["linter"]
