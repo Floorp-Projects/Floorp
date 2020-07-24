15fb12a40997e60666e20b085a5c96a81e4a9f04
Mitchell Hentges <mhentges@mozilla.com>
2020-07-24 16:05:49 +0000

    Bug 1654663: Removes glean_parser from virtualenv_packages.txt r=firefox-build-system-reviewers,rstewart
    In preparation for Glean telemetry, we scope the availability of the out-of-date vendored
    "glean_parser" library to its one usage: "run_glean_parser.py".
    
    This allows Glean telemetry to load its modern "glean_parser" dependency from the
    "--user" package environment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84610

--- toolkit/components/telemetry/build_scripts/run_glean_parser.py
+++ toolkit/components/telemetry/build_scripts/run_glean_parser.py
@@ -3,12 +3,30 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import sys
-
-from glean_parser import lint
 from pathlib import Path
 
 
 def main(output, *filenames):
+    # Unlike most vendored packages, we don't want "glean_parser" in
+    # "build/virtualenv_packages.txt" because it interferes with mach's usage of Glean.
+    # This is because Glean ("glean_sdk"):
+    # * Has native code (so, can't be vendored)
+    # * Also depends on "glean_parser"
+    # Since this file has different version constraints than mach on "glean_parser",
+    # we want the two different consumers to own their own separate "glean_parser"
+    # packages.
+    #
+    # This is solved by:
+    # * Having mach import Glean (and transitively "glean_parser") via the "--user"
+    #   package environment. To accomplish this, the vendored "glean_parser" is removed
+    #   from "virtualenv_packages.txt".
+    # * Having this script import "glean_parser" from the vendored location. This is
+    #   done by manually adding it to the pythonpath.
+
+    srcdir = Path(__file__).joinpath('../../../../../')
+    glean_parser_path = srcdir.joinpath('third_party/python/glean_parser')
+    sys.path.insert(0, str(glean_parser_path.resolve()))
+    from glean_parser import lint
     if lint.glinter([Path(x) for x in filenames], {"allow_reserved": False}):
         sys.exit(1)
 
