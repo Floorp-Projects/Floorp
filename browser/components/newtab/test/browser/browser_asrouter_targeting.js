6accbc1fde44fba1c6b7185db9921e580e343e5b
Coroiu Cristina <ccoroiu@mozilla.com>
2020-07-24 17:42:56 +0300

    Backed out changeset 47420937d6b1 (bug 1644743) for browser-chrome failures at browser/base/content/test/static/browser_all_files_referenced.js on a CLOSED TREE

--- browser/components/newtab/test/browser/browser_asrouter_targeting.js
+++ browser/components/newtab/test/browser/browser_asrouter_targeting.js
@@ -48,6 +48,33 @@ ChromeUtils.defineModuleGetter(
   "resource://gre/modules/Region.jsm"
 );
 
+// ASRouterTargeting.isMatch
+add_task(async function should_do_correct_targeting() {
+  is(
+    await ASRouterTargeting.isMatch("FOO", { FOO: true }),
+    true,
+    "should return true for a matching value"
+  );
+  is(
+    await ASRouterTargeting.isMatch("!FOO", { FOO: true }),
+    false,
+    "should return false for a non-matching value"
+  );
+});
+
+add_task(async function should_handle_async_getters() {
+  const context = {
+    get FOO() {
+      return Promise.resolve(true);
+    },
+  };
+  is(
+    await ASRouterTargeting.isMatch("FOO", context),
+    true,
+    "should return true for a matching async value"
+  );
+});
+
 // ASRouterTargeting.findMatchingMessage
 add_task(async function find_matching_message() {
   const messages = [
@@ -80,10 +107,38 @@ add_task(async function return_nothing_for_no_matching_message() {
   );
 });
 
-add_task(async function check_other_error_handling() {
-  let called = false;
+add_task(async function check_syntax_error_handling() {
+  let result;
   function onError(...args) {
-    called = true;
+    result = args;
+  }
+
+  const messages = [{ id: "foo", targeting: "foo === 0" }];
+  const match = await ASRouterTargeting.findMatchingMessage({
+    messages,
+    onError,
+  });
+
+  is(
+    match,
+    undefined,
+    "should return nothing since no valid matching message exists"
+  );
+  // Note that in order for the following test to pass, we are expecting a particular filepath for mozjexl.
+  // If the location of this file has changed, the MOZ_JEXL_FILEPATH constant should be updated om ASRouterTargeting.jsm
+  is(
+    result[0],
+    ASRouterTargeting.ERROR_TYPES.MALFORMED_EXPRESSION,
+    "should recognize the error as coming from mozjexl and call onError with the MALFORMED_EXPRESSION error type"
+  );
+  ok(result[1].message, "should call onError with the error from mozjexl");
+  is(result[2], messages[0], "should call onError with the invalid message");
+});
+
+add_task(async function check_other_error_handling() {
+  let result;
+  function onError(...args) {
+    result = args;
   }
 
   const messages = [{ id: "foo", targeting: "foo" }];
@@ -103,8 +158,19 @@ add_task(async function check_other_error_handling() {
     undefined,
     "should return nothing since no valid matching message exists"
   );
-
-  Assert.ok(called, "Attribute error caught");
+  // Note that in order for the following test to pass, we are expecting a particular filepath for mozjexl.
+  // If the location of this file has changed, the MOZ_JEXL_FILEPATH constant should be updated om ASRouterTargeting.jsm
+  is(
+    result[0],
+    ASRouterTargeting.ERROR_TYPES.ATTRIBUTE_ERROR,
+    "should not recognize the error as being an attribute error."
+  );
+  is(
+    result[1].message,
+    "test error",
+    "should call onError with the error thrown in the context"
+  );
+  is(result[2], "foo", "should call onError with the invalid attribute");
 });
 
 // ASRouterTargeting.Environment
