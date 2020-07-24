7318cda052396bba2b52b9f850b943774e145fe4
Jan Varga <jvarga@mozilla.com>
2020-07-24 11:47:48 +0000

    Bug 1651016 - Introduce SDB_TRY and SDB_TRY_VAR macros; r=ttung
    Differential Revision: https://phabricator.services.mozilla.com/D84663

--- dom/simpledb/SimpleDBCommon.cpp
+++ dom/simpledb/SimpleDBCommon.cpp
@@ -11,5 +11,19 @@ namespace dom {
 
 const char* kPrefSimpleDBEnabled = "dom.simpleDB.enabled";
 
+namespace simpledb {
+
+void HandleError(const nsLiteralCString& aExpr,
+                 const nsLiteralCString& aSourceFile, int32_t aSourceLine) {
+#ifdef DEBUG
+  NS_DebugBreak(NS_DEBUG_WARNING, "Error", aExpr.get(), aSourceFile.get(),
+                aSourceLine);
+#endif
+
+  // TODO: Report to browser console
+}
+
+}  // namespace simpledb
+
 }  // namespace dom
 }  // namespace mozilla
