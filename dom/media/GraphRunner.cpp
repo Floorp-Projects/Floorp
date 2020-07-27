fb0207f3d8c93afe267209399db45085976cde9d
Karl Tomlinson <karlt+@karlt.net>
2020-07-27 14:47:51 +0000

    Bug 1653667 don't attempt to promote GraphRunner thread on Linux r=padenot
    This usually fails due to sandbox policy.
    
    When the sandbox is disabled, the realtime promotion can result in SIGKILL
    if/when script runs too long.  This can be re-introduced when thread demotion
    is moved to a thread that is not blocked on script, which may be part of
    the solution to https://bugzilla.mozilla.org/show_bug.cgi?id=1628198.
    
    Depends on D84941
    
    Differential Revision: https://phabricator.services.mozilla.com/D84942

--- dom/media/GraphRunner.cpp
+++ dom/media/GraphRunner.cpp
@@ -95,8 +95,10 @@ auto GraphRunner::OneIteration(GraphTime aStateEnd, GraphTime aIterationEnd,
 }
 
 NS_IMETHODIMP GraphRunner::Run() {
+#ifndef XP_LINUX
   atp_handle* handle =
       atp_promote_current_thread_to_real_time(0, mGraph->GraphRate());
+#endif
 
   nsCOMPtr<nsIThreadInternal> threadInternal = do_QueryInterface(mThread);
   threadInternal->SetObserver(mGraph);
@@ -119,9 +121,11 @@ NS_IMETHODIMP GraphRunner::Run() {
     mMonitor.Notify();
   }
 
+#ifndef XP_LINUX
   if (handle) {
     atp_demote_current_thread_from_real_time(handle);
   }
+#endif
 
   return NS_OK;
 }
