5d54400cac078b459b6e63250a0ce85a030ba2fe
Tom Prince <mozilla@hocat.ca>
2020-07-27 21:24:43 +0000

    Bug 1652503: [lint] Remove old requirements for perfdocs; r=ahal
    These packages are all vendored in-tree, so no need to install them again.
    This also fixes the issue that these packages are older than the vendored
    versions, which causes issues with the use of attrs in `mozlint` now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85037

--- tools/lint/perfdocs/__init__.py
+++ tools/lint/perfdocs/__init__.py
@@ -4,16 +4,9 @@
 import os
 
 from perfdocs import perfdocs
-from mozlint.util import pip
 
 here = os.path.abspath(os.path.dirname(__file__))
 PERFDOCS_REQUIREMENTS_PATH = os.path.join(here, "requirements.txt")
-
-
-def setup(root, **lintargs):
-    if not pip.reinstall_program(PERFDOCS_REQUIREMENTS_PATH):
-        print("Cannot install requirements.")
-        return 1
 
 
 def lint(paths, config, logger, fix=False, **lintargs):
