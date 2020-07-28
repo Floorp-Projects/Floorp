85a9d2487888c7b97b2f97accd172781a59b0680
Paul Adenot <paul@paul.cx>
2020-07-28 12:42:45 +0000

    Bug 1655699 - mach vendor rust. r=cubeb-reviewers,achronop
    Depends on D85143
    
    Differential Revision: https://phabricator.services.mozilla.com/D85144

--- third_party/rust/cubeb-coreaudio/src/backend/tests/tone.rs
+++ third_party/rust/cubeb-coreaudio/src/backend/tests/tone.rs
@@ -1,5 +1,6 @@
 use super::utils::{test_get_default_device, test_ops_stream_operation, Scope};
 use super::*;
+use std::sync::atomic::{AtomicI64, Ordering};
 
 #[test]
 fn test_dial_tone() {
@@ -24,8 +25,15 @@ fn test_dial_tone() {
     output_params.layout = ffi::CUBEB_LAYOUT_MONO;
     output_params.prefs = ffi::CUBEB_STREAM_PREF_NONE;
 
-    // Used to calculate the tone's wave.
-    let mut position: i64 = 0; // TODO: Use Atomic instead.
+    struct Closure {
+        buffer_size: AtomicI64,
+        phase: i64,
+    };
+    let mut closure = Closure {
+        buffer_size: AtomicI64::new(0),
+        phase: 0,
+    };
+    let closure_ptr = &mut closure as *mut Closure as *mut c_void;
 
     test_ops_stream_operation(
         "stream: North American dial tone",
@@ -36,10 +44,125 @@ fn test_dial_tone() {
         4096, // TODO: Get latency by get_min_latency instead ?
         Some(data_callback),
         Some(state_callback),
-        &mut position as *mut i64 as *mut c_void,
+        closure_ptr,
         |stream| {
             assert_eq!(unsafe { OPS.stream_start.unwrap()(stream) }, ffi::CUBEB_OK);
-            thread::sleep(Duration::from_millis(500));
+
+            #[derive(Debug)]
+            enum State {
+                WaitingForStart,
+                PositionIncreasing,
+                Paused,
+                Resumed,
+                End,
+            };
+            let mut state = State::WaitingForStart;
+            let mut position: u64 = 0;
+            let mut prev_position: u64 = 0;
+            let mut count = 0;
+            const CHECK_COUNT: i32 = 10;
+            loop {
+                thread::sleep(Duration::from_millis(50));
+                assert_eq!(
+                    unsafe { OPS.stream_get_position.unwrap()(stream, &mut position) },
+                    ffi::CUBEB_OK
+                );
+                println!(
+                    "State: {:?}, position: {}, previous position: {}",
+                    state, position, prev_position
+                );
+                match &mut state {
+                    State::WaitingForStart => {
+                        // It's expected to have 0 for a few iterations here: the stream can take
+                        // some time to start.
+                        if position != prev_position {
+                            assert!(position > prev_position);
+                            prev_position = position;
+                            state = State::PositionIncreasing;
+                        }
+                    }
+                    State::PositionIncreasing => {
+                        // wait a few iterations, check monotony
+                        if position != prev_position {
+                            assert!(position > prev_position);
+                            prev_position = position;
+                            count += 1;
+                            if count > CHECK_COUNT {
+                                state = State::Paused;
+                                count = 0;
+                                assert_eq!(
+                                    unsafe { OPS.stream_stop.unwrap()(stream) },
+                                    ffi::CUBEB_OK
+                                );
+                                // Update the position once paused.
+                                assert_eq!(
+                                    unsafe {
+                                        OPS.stream_get_position.unwrap()(stream, &mut position)
+                                    },
+                                    ffi::CUBEB_OK
+                                );
+                                prev_position = position;
+                            }
+                        }
+                    }
+                    State::Paused => {
+                        // The cubeb_stream_stop call above should synchrously stop the callbacks,
+                        // hence the clock, the assert below must always holds, modulo the client
+                        // side interpolation.
+                        assert!(
+                            position == prev_position
+                                || position - prev_position
+                                    <= closure.buffer_size.load(Ordering::SeqCst) as u64
+                        );
+                        count += 1;
+                        prev_position = position;
+                        if count > CHECK_COUNT {
+                            state = State::Resumed;
+                            count = 0;
+                            assert_eq!(unsafe { OPS.stream_start.unwrap()(stream) }, ffi::CUBEB_OK);
+                        }
+                    }
+                    State::Resumed => {
+                        // wait a few iterations, this can take some time to start
+                        if position != prev_position {
+                            assert!(position > prev_position);
+                            prev_position = position;
+                            count += 1;
+                            if count > CHECK_COUNT {
+                                state = State::End;
+                                count = 0;
+                                assert_eq!(
+                                    unsafe { OPS.stream_stop.unwrap()(stream) },
+                                    ffi::CUBEB_OK
+                                );
+                                assert_eq!(
+                                    unsafe {
+                                        OPS.stream_get_position.unwrap()(stream, &mut position)
+                                    },
+                                    ffi::CUBEB_OK
+                                );
+                                prev_position = position;
+                            }
+                        }
+                    }
+                    State::End => {
+                        // The cubeb_stream_stop call above should synchrously stop the callbacks,
+                        // hence the clock, the assert below must always holds, modulo the client
+                        // side interpolation.
+                        assert!(
+                            position == prev_position
+                                || position - prev_position
+                                    <= closure.buffer_size.load(Ordering::SeqCst) as u64
+                        );
+                        if position == prev_position {
+                            count += 1;
+                            if count > CHECK_COUNT {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
             assert_eq!(unsafe { OPS.stream_stop.unwrap()(stream) }, ffi::CUBEB_OK);
         },
     );
@@ -71,14 +194,16 @@ fn test_dial_tone() {
             slice::from_raw_parts_mut(ptr, len)
         };
 
-        let position = unsafe { &mut *(user_ptr as *mut i64) };
+        let closure = unsafe { &mut *(user_ptr as *mut Closure) };
+
+        closure.buffer_size.store(nframes, Ordering::SeqCst);
 
         // Generate tone on the fly.
         for data in buffer.iter_mut() {
-            let t1 = (2.0 * PI * 350.0 * (*position) as f32 / SAMPLE_FREQUENCY as f32).sin();
-            let t2 = (2.0 * PI * 440.0 * (*position) as f32 / SAMPLE_FREQUENCY as f32).sin();
+            let t1 = (2.0 * PI * 350.0 * (closure.phase) as f32 / SAMPLE_FREQUENCY as f32).sin();
+            let t2 = (2.0 * PI * 440.0 * (closure.phase) as f32 / SAMPLE_FREQUENCY as f32).sin();
             *data = f32_to_i16_sample(0.5 * (t1 + t2));
-            *position += 1;
+            closure.phase += 1;
         }
 
         nframes
