ec7f2250d4f828bc71c9c0e88d235ab02e020590
Andrew Sutherland <asutherland@asutherland.org>
2020-07-28 09:59:02 +0000

    Bug 1653986 - Add new test metadata taskcluster searchfox task deps. r=kats
    The enhancements in Bug 1653986 to display information about tests derives its
    data from these 2 jobs and so it's appropriate to explicitly depend on them.
    
    The current status of these jobs in the tree as far as I can tell is that:
    - "source-test-file-metadata-test-info-all": Seems to get automatically
      triggered by someone's regularly, so it's always likely to be there for
      our searchfox cron jobs, but it's nice to not depend on that.
    - "source-test-wpt-metadata-summary" : Seems to get automatically run, but
      only on commits that touch meta files change, per
      https://searchfox.org/mozilla-central/rev/cf561cece0ca9aeaf0202e68699836d957d0c670/taskcluster/ci/source-test/wpt-metadata.yml#31
      and indeed it wasn't there on today's searchfox jobs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84933

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -902,7 +902,9 @@ def target_tasks_searchfox(full_task_graph, parameters, graph_config):
             'searchfox-macosx64-searchfox/debug',
             'searchfox-win64-searchfox/debug',
             'searchfox-android-armv7-searchfox/debug',
-            'source-test-file-metadata-bugzilla-components']
+            'source-test-file-metadata-bugzilla-components',
+            'source-test-file-metadata-test-info-all',
+            'source-test-wpt-metadata-summary']
 
 
 # Run Coverity Static Analysis once daily.
