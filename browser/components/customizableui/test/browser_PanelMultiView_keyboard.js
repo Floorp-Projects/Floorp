2a6d9c5bb43872d332e7e5a4e8bf7075aa51b8f5
James Teh <jteh@mozilla.com>
2020-07-29 12:10:35 +0000

    Bug 1572608: browser_PanelMultiView_keyboard.js: If embedded documents aren't loaded yet, wait for them to load before trying to interact with them. r=mconley a=test-only
    Differential Revision: https://phabricator.services.mozilla.com/D84937

--- browser/components/customizableui/test/browser_PanelMultiView_keyboard.js
+++ browser/components/customizableui/test/browser_PanelMultiView_keyboard.js
@@ -456,10 +456,19 @@ add_task(async function testActivationMousedown() {
 async function testTabArrowsEmbeddedDoc(aView, aEmbedder) {
   await openPopup();
   await showSubView(aView);
+  let doc = aEmbedder.contentDocument;
+  if (doc.readyState != "complete") {
+    info(`Embedded doc readyState ${doc.readyState}, location ${doc.location}`);
+    info("Waiting for load on embedder");
+    await BrowserTestUtils.waitForEvent(aEmbedder, "load");
+    // The original doc might have been a temporary about:blank, so fetch it
+    // again.
+    doc = aEmbedder.contentDocument;
+  }
+  is(doc.location.href, kEmbeddedDocUrl, "Embedded doc has correct URl");
   let backButton = aView.querySelector(".subviewbutton-back");
   backButton.id = "docBack";
   await expectFocusAfterKey("Tab", backButton);
-  let doc = aEmbedder.contentDocument;
   // Documents don't have an id property, but expectFocusAfterKey wants one.
   doc.id = "doc";
   await expectFocusAfterKey("Tab", doc);
