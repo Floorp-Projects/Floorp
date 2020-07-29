3cd76392f1baaf75a72d42b6d5054cacd2f38203
Christian Holler <choller@mozilla.com>
2020-07-29 09:56:47 +0000

    Bug 1655910 - Disable MOZ_CHAOSMODE by default in ASan Nightly. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D85246

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3101,16 +3101,6 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
     ChaosMode::SetChaosFeature(feature);
   }
 
-#ifdef MOZ_ASAN_REPORTER
-  // In ASan Reporter builds, we enable certain chaos features by default unless
-  // the user explicitly requests a particular set of features.
-  if (!PR_GetEnv("MOZ_CHAOSMODE")) {
-    ChaosMode::SetChaosFeature(static_cast<ChaosFeature>(
-        ChaosFeature::ThreadScheduling | ChaosFeature::NetworkScheduling |
-        ChaosFeature::TimerScheduling));
-  }
-#endif
-
   if (CheckArgExists("fxr")) {
     gFxREmbedded = true;
   }
