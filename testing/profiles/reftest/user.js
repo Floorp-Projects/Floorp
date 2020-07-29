beba0d23dddb4fec35e38c73eba0c6b7fa90f01f
Zeke Medley <zekemedley@gmail.com>
2020-07-29 20:27:42 +0000

    Bug 1654918 - Improve prefers-contrast reftests with the `ui.useAccessibilityTheme` preference r=morgan
    Differential Revision: https://phabricator.services.mozilla.com/D84765

--- testing/profiles/reftest/user.js
+++ testing/profiles/reftest/user.js
@@ -97,6 +97,9 @@ user_pref("ui.caretBlinkTime", -1);
 user_pref("ui.caretWidth", 1);
 user_pref("ui.prefersReducedMotion", 0);
 user_pref("ui.systemUsesDarkTheme", 0);
+user_pref("ui.useAccessibilityTheme", 0);
+user_pref("ui.windowForeground", "");
+user_pref("ui.windowBackground", "");
 // Turn off the Push service.
 user_pref("dom.push.serverURL", "");
 // Disable intermittent telemetry collection
