332ffeb96f814c1f2092a9e8aaf916b23399c420
Sebastian Kaspari <s.kaspari@gmail.com>
2020-07-29 16:34:54 +0200

    [components] (Merge day) browser-engine-gecko-nightly -> 81

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "80.0.20200727095125"
+    const val nightly_version = "81.0.20200729094932"
 
     /**
      * GeckoView Beta Version.
