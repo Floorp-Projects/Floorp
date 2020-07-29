9d9960f1ad024ad2bc201d73556f85852bc7c439
Dave Townsend <dtownsend@oxymoronical.com>
2020-07-29 17:20:42 +0000

    Bug 1656018: Switch the update service tests to use AppInfo.jsm. r=mhowell
    This uses AppInfo to generate the mock appinfo objects rather than rolling our
    own so we can share improvements to that with the rest of the tree.
    
    The mock appinfo used for setUpdateURL also doesn't expose various properties
    that may be expected to exist. We can't use AppInfo here as we dynamically
    replace Services.appinfo in browser-chrome tests. Instead this makes the mock
    appinfo object use the real object as a prototype replacing only the updateURL
    property.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85305

--- toolkit/mozapps/update/tests/data/shared.js
+++ toolkit/mozapps/update/tests/data/shared.js
@@ -261,49 +261,16 @@ function setUpdateURL(aURL) {
     Services.appinfo = origAppInfo;
   });
 
-  let mockAppInfo = {
-    // nsIXULAppInfo
-    vendor: origAppInfo.vendor,
-    name: origAppInfo.name,
-    ID: origAppInfo.ID,
-    version: origAppInfo.version,
-    appBuildID: origAppInfo.appBuildID,
-    updateURL: url,
-
-    // nsIPlatformInfo
-    platformVersion: origAppInfo.platformVersion,
-    platformBuildID: origAppInfo.platformBuildID,
-
-    // nsIXULRuntime
-    inSafeMode: origAppInfo.inSafeMode,
-    logConsoleErrors: origAppInfo.logConsoleErrors,
-    OS: origAppInfo.OS,
-    XPCOMABI: origAppInfo.XPCOMABI,
-    invalidateCachesOnRestart() {},
-    shouldBlockIncompatJaws: origAppInfo.shouldBlockIncompatJaws,
-    processType: origAppInfo.processType,
-    processID: origAppInfo.processID,
-    uniqueProcessID: origAppInfo.uniqueProcessID,
-
-    // nsIWinAppHelper
-    get userCanElevate() {
-      return origAppInfo.userCanElevate;
+  // Override the appinfo object with an object that exposes all of the same
+  // properties overriding just the updateURL.
+  let mockAppInfo = Object.create(origAppInfo, {
+    updateURL: {
+      configurable: true,
+      enumerable: true,
+      writable: false,
+      value: url,
     },
-  };
-  let interfaces = [Ci.nsIXULAppInfo, Ci.nsIPlatformInfo, Ci.nsIXULRuntime];
-  if ("nsIWinAppHelper" in Ci) {
-    interfaces.push(Ci.nsIWinAppHelper);
-  }
-  if ("crashReporter" in origAppInfo && origAppInfo.crashReporter) {
-    // nsICrashReporter
-    mockAppInfo.crashReporter = {};
-    mockAppInfo.annotations = {};
-    mockAppInfo.annotateCrashReport = function(key, data) {
-      this.annotations[key] = data;
-    };
-    interfaces.push(Ci.nsICrashReporter);
-  }
-  mockAppInfo.QueryInterface = ChromeUtils.generateQI(interfaces);
+  });
 
   Services.appinfo = mockAppInfo;
 }
