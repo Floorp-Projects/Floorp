4f19daac47485dedc04344b3450de3cdb5d20201
Yura Zenevich <yura.zenevich@gmail.com>
2020-07-30 14:00:03 +0000

    Bug 1650094 - making the box model highlighter work in the browser toolbox. r=rcaliman
    Differential Revision: https://phabricator.services.mozilla.com/D81476

--- devtools/client/inspector/animation/test/browser_animation_inspector_exists.js
+++ devtools/client/inspector/animation/test/browser_animation_inspector_exists.js
@@ -6,6 +6,7 @@
 // Test whether AnimationInspector and base pane exists.
 
 add_task(async function() {
+  await addTab(URL_ROOT + "doc_simple_animation.html");
   const { animationInspector, panel } = await openAnimationInspector();
   ok(animationInspector, "AnimationInspector should exist");
   ok(panel, "Main animation-inspector panel should exist");
