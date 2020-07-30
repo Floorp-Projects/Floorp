b00293dd89a2a9538136abfbdfd5a9e52ce7f610
Tim Giles <tgiles@mozilla.com>
2020-07-30 21:00:58 +0000

    Bug 1618899 - Prevent Enter/Return key from activating hidden buttons in exceptions dialog. r=preferences-reviewers,ntim,MattN,bdanforth
    Differential Revision: https://phabricator.services.mozilla.com/D84994

--- toolkit/components/passwordmgr/test/browser/browser_exceptions_dialog.js
+++ toolkit/components/passwordmgr/test/browser/browser_exceptions_dialog.js
@@ -60,3 +60,52 @@ add_task(async function test_enable() {
   is(countDisabledHosts(dialog), 0, "Verify disabled host removed");
   await BrowserTestUtils.closeWindow(dialog);
 });
+
+add_task(async function test_block_button_with_enter_key() {
+  // Test ensures that the Enter/Return key does not activate the "Allow" button
+  // in the "Saved Logins" exceptions dialog
+
+  let dialog = openExceptionsDialog();
+
+  await BrowserTestUtils.waitForEvent(dialog, "load");
+  await new Promise(resolve => waitForFocus(resolve, dialog));
+  let btnBlock = dialog.document.getElementById("btnBlock");
+  let btnSession = dialog.document.getElementById("btnSession");
+  let btnAllow = dialog.document.getElementById("btnAllow");
+
+  ok(!btnBlock.hidden, "Block button is visible");
+  ok(btnSession.hidden, "Session button is not visible");
+  ok(btnAllow.hidden, "Allow button is not visible");
+  ok(btnBlock.disabled, "Block button is initially disabled");
+  ok(btnSession.disabled, "Session button is initially disabled");
+  ok(btnAllow.disabled, "Allow button is initially disabled");
+
+  EventUtils.sendString(LOGIN_HOST, dialog);
+
+  ok(
+    !btnBlock.disabled,
+    "Block button is enabled after entering text in the URL input"
+  );
+  ok(
+    btnSession.disabled,
+    "Session button is still disabled after entering text in the URL input"
+  );
+  ok(
+    btnAllow.disabled,
+    "Allow button is still disabled after entering text in the URL input"
+  );
+
+  is(
+    countDisabledHosts(dialog),
+    0,
+    "No blocked hosts should be present before hitting the Enter/Return key"
+  );
+  EventUtils.sendKey("return", dialog);
+
+  is(countDisabledHosts(dialog), 1, "Verify the blocked host was added");
+  ok(
+    btnBlock.disabled,
+    "Block button is disabled after submitting to the list"
+  );
+  await BrowserTestUtils.closeWindow(dialog);
+});
