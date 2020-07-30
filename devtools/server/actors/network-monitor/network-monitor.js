678fa178dacd3e67025af9588e555fb6db034804
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-07-30 13:43:31 +0000

    Bug 1478688: Remove no longer used code from network monitor actor. r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D85370

--- devtools/server/actors/network-monitor/network-monitor.js
+++ devtools/server/actors/network-monitor/network-monitor.js
@@ -61,7 +61,6 @@ const NetworkMonitorActor = ActorClassWithSpec(networkMonitorSpec, {
     this.lastFrames = new Map();
 
     this.onStackTraceAvailable = this.onStackTraceAvailable.bind(this);
-    this.onRequestContent = this.onRequestContent.bind(this);
     this.onSetPreference = this.onSetPreference.bind(this);
     this.onBlockRequest = this.onBlockRequest.bind(this);
     this.onUnblockRequest = this.onUnblockRequest.bind(this);
@@ -83,10 +82,6 @@ const NetworkMonitorActor = ActorClassWithSpec(networkMonitorSpec, {
     this.messageManager.addMessageListener(
       "debug:request-stack-available",
       this.onStackTraceAvailable
-    );
-    this.messageManager.addMessageListener(
-      "debug:request-content:request",
-      this.onRequestContent
     );
     this.messageManager.addMessageListener(
       "debug:netmonitor-preference",
@@ -122,10 +117,6 @@ const NetworkMonitorActor = ActorClassWithSpec(networkMonitorSpec, {
     this.messageManager.removeMessageListener(
       "debug:request-stack-available",
       this.onStackTraceAvailable
-    );
-    this.messageManager.removeMessageListener(
-      "debug:request-content:request",
-      this.onRequestContent
     );
     this.messageManager.removeMessageListener(
       "debug:netmonitor-preference",
@@ -196,49 +187,6 @@ const NetworkMonitorActor = ActorClassWithSpec(networkMonitorSpec, {
       }
       this.stackTraces.add(channelId);
     }
-  },
-
-  getRequestContentForActor(actor) {
-    const content = actor._response.content;
-    if (
-      actor._discardResponseBody ||
-      actor._truncated ||
-      !content ||
-      !content.size
-    ) {
-      // Do not return the stylesheet text if there is no meaningful content or if it's
-      // still loading. Let the caller handle it by doing its own separate request.
-      return null;
-    }
-
-    if (content.text.type != "longString") {
-      // For short strings, the text is available directly.
-      return {
-        content: content.text,
-        contentType: content.mimeType,
-      };
-    }
-    // For long strings, look up the actor that holds the full text.
-    const longStringActor = this.conn._getOrCreateActor(content.text.actor);
-    if (!longStringActor) {
-      return null;
-    }
-    return {
-      content: longStringActor.str,
-      contentType: content.mimeType,
-    };
-  },
-
-  onRequestContent(msg) {
-    const { url } = msg.data;
-    const actor = this._networkEventActorsByURL.get(url);
-    // Always reply with a message, but with a null `content` if this instance
-    // did not processed this request
-    const content = actor ? this.getRequestContentForActor(actor) : null;
-    this.messageManager.sendAsyncMessage("debug:request-content:response", {
-      url,
-      content,
-    });
   },
 
   onSetPreference({ data }) {
