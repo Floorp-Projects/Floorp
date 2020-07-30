95fde7019c52411f8538def112f06ad1499d750b
Lars T Hansen <lhansen@mozilla.com>
2020-07-30 08:02:36 +0000

    Bug 1645310 - Make presence of the WebAssembly object not depend on the debugger having been activated. r=bbouvier
    This changes the meaning of wasm::HasSupport() to depend only on
    settings and context (eg, whether in a secure context or not), not to
    depend on whether circumstances have made wasm compilation possible or
    not.
    
    In particular, if the baseline compiler is not enabled and the
    debugger is then activated, then compilation will become impossible.
    Up till now HasSupport would go from returning true to returning false
    when that happens.  But HasSupport is used to determine whether the
    global WebAssembly object should be (lazily) created.  Thus the
    presence of that object in the JS environment would depend on whether
    a debugger was instantiated before the object needed to be lazily
    created.
    
    Instead, HasSupport can be invariant of that, we just need to test for
    compiler support in a couple of other places instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85134

---
+++ js/src/jit-test/tests/wasm/bug1645310/bug1645310.js
@@ -0,0 +1,54 @@
+// |jit-test| --wasm-compiler=ion; skip-if: !wasmIsSupported()
+
+// In this case we're setting things up so that wasm can't be compiled, as the
+// only available compiler is Ion, and enabling the Debugger will disable Ion.
+//
+// The test tests that the lazy creation of the WebAssembly object is not
+// dependent on whether WebAssembly can be compiled or not: if wasm is supported
+// then the WebAssembly object should always be created.  The fact that wasm
+// can't be compiled is a separate matter.
+//
+// IT'S IMPORTANT NOT TO MENTION THE WEBASSEMBLY OBJECT UNTIL AFTER THE DEBUGGER
+// HAS BEEN CREATED, AND NOT TO LOAD lib/wasm.js OR OTHER WASM CODE HERE.
+
+var g7 = newGlobal({newCompartment: true});
+g7.parent = this;
+g7.eval("Debugger(parent)");
+assertEq(typeof WebAssembly, "object");
+
+// Test that validation works even if compilers are not available.
+
+WebAssembly.validate(wasmTextToBinary('(module (func))'));
+
+// Test that compilation fails with a sensible error.
+
+var bits = wasmTextToBinary('(module (func))');
+var msg = /no WebAssembly compiler available/
+var exn;
+
+exn = null;
+try { new WebAssembly.Module(bits); } catch (e) { exn = e; }
+assertEq(Boolean(exn), true);
+assertEq(Boolean(String(exn).match(msg)), true);
+
+exn = null;
+try { WebAssembly.compile(bits); } catch (e) { exn = e; }
+assertEq(Boolean(exn), true);
+assertEq(Boolean(String(exn).match(msg)), true);
+
+exn = null;
+try { WebAssembly.instantiate(bits); } catch (e) { exn = e; }
+assertEq(Boolean(exn), true);
+assertEq(Boolean(String(exn).match(msg)), true);
+
+exn = null;
+WebAssembly.compileStreaming(bits).catch(e => { exn = e; });
+drainJobQueue();
+assertEq(Boolean(exn), true);
+assertEq(Boolean(String(exn).match(msg)), true);
+
+exn = null;
+WebAssembly.instantiateStreaming(bits).catch(e => { exn = e; });
+drainJobQueue();
+assertEq(Boolean(exn), true);
+assertEq(Boolean(String(exn).match(msg)), true);
