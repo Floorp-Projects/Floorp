547e8208553011ce4cac0c8d61d4e4e4340eb3c7
Jed Davis <jld@mozilla.com>
2020-07-30 22:08:15 +0000

    Bug 1654957 - Enable the GfxInfo component in the GPU process on Linux. r=jgilbert
    Out-of-process WebGL needs GfxInfo to exist in the composition
    process (which is the GPU process if it exists and the parent process
    otherwise).  This patch enables the Linux version of that component in
    the GPU process; the IPC currently used to give content processes copies
    of the parent's GPU info is extended to also send it to the GPU process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85443

--- widget/GfxInfoBase.h
+++ widget/GfxInfoBase.h
@@ -16,7 +16,6 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/Maybe.h"
 #include "mozilla/Mutex.h"
-#include "mozilla/dom/PContentParent.h"
 #include "mozilla/gfx/GraphicsMessages.h"
 #include "nsCOMPtr.h"
 #include "nsIGfxInfo.h"
@@ -77,7 +76,7 @@ class GfxInfoBase : public nsIGfxInfo,
   NS_IMETHOD GetTargetFrameRate(uint32_t* aTargetFrameRate) override;
 
   // Non-XPCOM method to get IPC data:
-  void GetAllFeatures(dom::XPCOMInitData& xpcomInit);
+  nsTArray<mozilla::gfx::GfxInfoFeatureStatus> GetAllFeatures();
 
   // Initialization function. If you override this, you must call this class's
   // version of Init first.
@@ -112,7 +111,7 @@ class GfxInfoBase : public nsIGfxInfo,
   virtual nsresult FindMonitors(JSContext* cx, JS::HandleObject array);
 
   static void SetFeatureStatus(
-      const nsTArray<mozilla::gfx::GfxInfoFeatureStatus>& aFS);
+      nsTArray<mozilla::gfx::GfxInfoFeatureStatus>&& aFS);
 
  protected:
   virtual ~GfxInfoBase();
