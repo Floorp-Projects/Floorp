143348fef59e163b84fb94f3c2b3ae6aefa13b58
Nathan Froyd <froydnj@mozilla.com>
2020-07-30 16:54:43 +0000

    Bug 1223932 - delete `GuardObjectNotifier` usages from the JS rust bindings;
    Automated removals don't catch uses in Rust bindings!
    
    Differential Revision: https://phabricator.services.mozilla.com/D85424

--- js/rust/src/ar.rs
+++ js/rust/src/ar.rs
@@ -3,37 +3,15 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 use jsapi::root::*;
-#[cfg(feature = "debugmozjs")]
-use std::ptr;
 
 #[derive(Debug)]
 pub struct AutoRealm(JSAutoRealm);
 
 impl AutoRealm {
-    #[cfg(feature = "debugmozjs")]
-    pub unsafe fn with_obj(cx: *mut JSContext, target: *mut JSObject) -> AutoRealm {
-        let mut notifier = mozilla::detail::GuardObjectNotifier {
-            mStatementDone: ptr::null_mut(),
-        };
-
-        AutoRealm(JSAutoRealm::new(cx, target, &mut notifier as *mut _))
-    }
-
-    #[cfg(not(feature = "debugmozjs"))]
     pub unsafe fn with_obj(cx: *mut JSContext, target: *mut JSObject) -> AutoRealm {
         AutoRealm(JSAutoRealm::new(cx, target))
     }
 
-    #[cfg(feature = "debugmozjs")]
-    pub unsafe fn with_script(cx: *mut JSContext, target: *mut JSScript) -> AutoRealm {
-        let mut notifier = mozilla::detail::GuardObjectNotifier {
-            mStatementDone: ptr::null_mut(),
-        };
-
-        AutoRealm(JSAutoRealm::new1(cx, target, &mut notifier as *mut _))
-    }
-
-    #[cfg(not(feature = "debugmozjs"))]
     pub unsafe fn with_script(cx: *mut JSContext, target: *mut JSScript) -> AutoRealm {
         AutoRealm(JSAutoRealm::new1(cx, target))
     }
