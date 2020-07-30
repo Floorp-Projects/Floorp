f9dc7ab5e8c45c24f4caa7ec3f5ab325d2d37dd3
Chris H-C <chutten@mozilla.com>
2020-07-30 16:35:32 +0000

    Bug 1635253 - Test that debug tagging works via about:glean r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D83981

--- toolkit/components/telemetry/tests/marionette/tests/client/test_fog_deletion_request_ping.py
+++ toolkit/components/telemetry/tests/marionette/tests/client/test_fog_deletion_request_ping.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import textwrap
+
 from telemetry_harness.fog_ping_filters import FOG_DELETION_REQUEST_PING
 from telemetry_harness.fog_testcase import FOGTestCase
 
@@ -34,6 +36,14 @@ class TestDeletionRequestPing(FOGTestCase):
         self.enable_telemetry()
         self.restart_browser()
 
+        debug_tag = "my-test-tag"
+        tagging_script = """\
+        let AboutGlean = Components.classes["@mozilla.org/toolkit/glean;1"]
+            .createInstance(Components.interfaces.nsIAboutGlean);
+        AboutGlean.setTagPings("{}");
+        """.format(debug_tag)
+        with self.marionette.using_context(self.marionette.CONTEXT_CHROME):
+            self.marionette.execute_script(textwrap.dedent(tagging_script))
         self.search_in_new_tab("python unittest")
 
         ping2 = self.wait_for_ping(
@@ -41,6 +51,8 @@ class TestDeletionRequestPing(FOGTestCase):
             FOG_DELETION_REQUEST_PING,
             ping_server=self.fog_ping_server,
         )
+
+        self.assertEqual(ping2["debug_tag"], debug_tag)
 
         self.assertIn("client_id", ping2["payload"]["client_info"])
         client_id2 = ping2["payload"]["client_info"]["client_id"]
