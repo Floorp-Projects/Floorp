c40785c3bfb6accb128ac0b0403f8e034a0a8d9a
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2020-07-30 17:02:02 +0000

    Bug 1640195: Address UX feedback for non-native widget styling. r=geckoview-reviewers,emilio,agi
    Differential Revision: https://phabricator.services.mozilla.com/D76509

---
+++ widget/gtk/nsNativeBasicThemeGTK.h
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsNativeBasicThemeGTK_h
+#define nsNativeBasicThemeGTK_h
+
+#include "nsNativeBasicTheme.h"
+
+class nsNativeBasicThemeGTK : public nsNativeBasicTheme {
+ public:
+  nsNativeBasicThemeGTK() = default;
+
+ protected:
+  virtual ~nsNativeBasicThemeGTK() = default;
+};
+
+#endif
