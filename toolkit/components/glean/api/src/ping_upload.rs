cd157528785a874406c984255bf6f1c1a964564c
Chris H-C <chutten@mozilla.com>
2020-07-30 16:35:27 +0000

    Bug 1635253 - Tweak FOG to be able to compile against updated Glean SDK r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D83979

--- toolkit/components/glean/api/src/ping_upload.rs
+++ toolkit/components/glean/api/src/ping_upload.rs
@@ -41,7 +41,7 @@ pub fn check_for_uploads() {
         loop {
             if let Some(mutex) = UPLOADER.get() {
                 let uploader = &mut *mutex.lock().unwrap();
-                if let Upload(request) = crate::with_glean(|glean| glean.get_upload_task(false)) {
+                if let Upload(request) = crate::with_glean(|glean| glean.get_upload_task()) {
                     let response = (*uploader)(&request);
                     crate::with_glean(|glean| {
                         glean.process_ping_upload_response(&request.document_id, response)
