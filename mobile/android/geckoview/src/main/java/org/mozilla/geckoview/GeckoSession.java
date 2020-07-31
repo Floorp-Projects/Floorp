60d55ce83177d3648431254dda60831c47ace625
Markus Stange <mstange.moz@gmail.com>
2020-07-31 21:28:27 +0000

    Bug 1655580 - Stop leaking surfaces that were captured with RequestScreenPixels. r=geckoview-reviewers,agi
    FlipScreenPixels was heap-allocating and leaking a ScopedMap object, which kept
    the surface alive.
    
    Instead, we want to keep the surface alive just long enough to copy its data
    into a Java Bitmap object. This copy needs to be done on the C++ side so that
    we're sure that it happens while the surface data exists. After that, the Java
    code can hold on to the resulting Bitmap object as long as it wants, since the
    Bitmap then owns its own data.
    
    The ScreenshotBuilder GeckoView API allows users to specify the bitmap object
    that will be used. So rather than creating a new Bitmap object in the C++ code,
    we pass the target bitmap into the C++ code that performs the copy.
    
    As a drive-by fix, this patch eliminates another copy at the start of
    FlipScreenPixels by replacing a call to `gfx::CreateDataSourceSurfaceFromData`
    (which copies) with a call to `gfx::Factory::CreateWrappingDataSourceSurface`
    (which doesn't copy).
    It would be nice if we could eliminate another copy here, by performing the flip
    during the copy into the Bitmap object. But I don't think there's a way to do
    that. Also, for optimal performance, we probably want to get an AHardwareBuffer
    object from the compositor and wrap that into a Bitmap.
    
    Furthermore, the patch adds a check that makes sure the draw target was created
    successfully before accessing it (throwing an exception if not).
    
    Differential Revision: https://phabricator.services.mozilla.com/D85490

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -44,6 +44,7 @@ import org.mozilla.gecko.util.ThreadUtils;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -219,7 +220,8 @@ public class GeckoSession implements Parcelable {
         public native void setDefaultClearColor(int color);
 
         @WrapForJNI(calledFrom = "ui", dispatchTo = "current")
-        /* package */ native void requestScreenPixels(final GeckoResult<ByteBuffer> result,
+        /* package */ native void requestScreenPixels(final GeckoResult<Bitmap> result,
+                                                      final Bitmap target,
                                                       final int x, final int y,
                                                       final int srcWidth, final int srcHeight,
                                                       final int outWidth, final int outHeight);
