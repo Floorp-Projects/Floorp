54511455dbaf0697cab167e0562af91f94fef3b5
Kershaw Chang <kershaw@mozilla.com>
2020-07-31 18:05:04 +0000

    Bug 1650773 - P2: Make sure HttpBackgroundChannelChild will use the ODA message from the correct source, r=dragana
    Differential Revision: https://phabricator.services.mozilla.com/D83339

--- netwerk/protocol/http/HttpChannelParent.cpp
+++ netwerk/protocol/http/HttpChannelParent.cpp
@@ -1438,6 +1438,15 @@ HttpChannelParent::OnStartRequest(nsIRequest* aRequest) {
     httpChannelImpl->GetCacheTokenCachedCharset(args.cachedCharset());
 
     mDataSentToChildProcess = httpChannelImpl->DataSentToChildProcess();
+
+    // If RCWN is enabled and cache wins, we can't use the ODA from socket
+    // process.
+    if (args.isRacing()) {
+      mDataSentToChildProcess =
+          httpChannelImpl->DataSentToChildProcess() && !args.isFromCache();
+    }
+    args.dataFromSocketProcess() = mDataSentToChildProcess;
+
     bool loadedFromApplicationCache = false;
     httpChannelImpl->GetLoadedFromApplicationCache(&loadedFromApplicationCache);
     if (loadedFromApplicationCache) {
