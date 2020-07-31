74aa0a78ecec38d1585a8b465d91e197fecc9813
Andreea Pavel <apavel@mozilla.com>
2020-08-01 02:25:16 +0300

    Backed out 6 changesets (bug 1653392, bug 1648868, bug 1649202) for breaking bc at browser_modal_print.js on a CLOSED TREE
    Backed out changeset e6f8429bae89 (bug 1649202)
    Backed out changeset 5360afed1eba (bug 1649202)
    Backed out changeset d3b3387338f8 (bug 1649202)
    Backed out changeset 98b1c160e065 (bug 1653392)
    Backed out changeset 5c984392950a (bug 1648868)
    Backed out changeset fa8732ebff70 (bug 1648868)

--- toolkit/components/printing/content/printUtils.js
+++ toolkit/components/printing/content/printUtils.js
@@ -114,131 +114,21 @@ var PrintUtils = {
   },
 
   /**
-   * Retrieve the template contents for the tab modal print UI.
-   */
-  _tabModalTemplate() {
-    return document.importNode(
-      document.getElementById("printTabModalTemplate").content,
-      true
-    ).firstElementChild;
-  },
-
-  /**
    * Opens the tab modal version of the print UI for the current tab.
    *
    * @param aBrowsingContext
    *        The BrowsingContext of the window to print.
    */
-  async _openTabModalPrint(aBrowsingContext) {
-    const { SubDialog } = ChromeUtils.import(
-      "resource://gre/modules/SubDialog.jsm"
+  _openTabModalPrint(aBrowsingContext) {
+    let printPath = "chrome://global/content/print.html";
+    gBrowser.loadOneTab(
+      `${printPath}?browsingContextId=${aBrowsingContext.id}`,
+      {
+        inBackground: false,
+        relatedToCurrent: true,
+        triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
+      }
     );
-
-    let container = gBrowser.getBrowserContainer(
-      aBrowsingContext.embedderElement
-    );
-    if (container.querySelector(".printDialogContainer")) {
-      // Don't open another dialog if we're already printing.
-      return;
-    }
-
-    let dialog = new SubDialog({
-      id: `printModal${aBrowsingContext.id}`,
-      template: this._tabModalTemplate(),
-      parentElement: container,
-      dialogOptions: {
-        consumeOutsideClicks: false,
-        reuseDialog: false,
-      },
-    });
-
-    // Move the overlay so it overlaps the chrome and content.
-    container.prepend(dialog._overlay);
-
-    // Store the dialog on the overlay for access in the tests.
-    dialog._overlay._dialog = dialog;
-    let sourceBrowser = aBrowsingContext.embedderElement;
-    let printPreviewBrowser = gBrowser.createBrowser({
-      remoteType: sourceBrowser.remoteType,
-      sameProcessAsFrameLoader: sourceBrowser.frameLoader,
-      skipLoad: false,
-    });
-    printPreviewBrowser.classList.add("printPreviewBrowser");
-    container.querySelector(".printTabModalView").prepend(printPreviewBrowser);
-    printPreviewBrowser.loadURI("about:printpreview", {
-      triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
-    });
-
-    await dialog.open(
-      `chrome://global/content/print.html?browsingContextId=${aBrowsingContext.id}`
-    );
-  },
-
-  /**
-   * Update the preview browser for the provided tab browser and print settings.
-   *
-   * @param sourceBrowser
-   *        The source Browser (the one associated with a tab) that should be updated.
-   * @param printSettings
-   *        The nsIPrintSettings object to be used to render the preview.
-   *        Note: Only printerName is currently used.
-   *
-   * @return {Promise<Integer>} The number of pages that were rendered in the preview.
-   */
-  async updatePrintPreview(sourceBrowser, printSettings) {
-    let container = gBrowser.getBrowserContainer(sourceBrowser);
-    let printPreviewBrowser = container.querySelector(".printPreviewBrowser");
-
-    if (!printPreviewBrowser) {
-      // TODO: This should probably throw and close the print dialog.
-      return 0;
-    }
-
-    let numPages = await this._updatePrintPreview(
-      sourceBrowser,
-      printPreviewBrowser,
-      printSettings
-    );
-    return numPages;
-  },
-
-  /**
-   * Update a print preview for the provided source Browser, print preview Browser
-   * and nsIPrintSettings. This should probably only be called from updatePrintPreview.
-   *
-   * @param sourceBrowser
-   *        The source Browser (the one associated with a tab) that should be updated.
-   * @param printPreviewBrowser
-   *        The Browser that contains the print preview.
-   * @param printSettings
-   *        The nsIPrintSettings object to be used to render the preview.
-   *        Note: Only printerName is currently used.
-   *
-   * @return {Promise<Integer>} The number of pages that were rendered in the preview.
-   */
-  _updatePrintPreview(sourceBrowser, printPreviewBrowser, printSettings) {
-    return new Promise(resolve => {
-      printPreviewBrowser.messageManager.addMessageListener(
-        "Printing:Preview:UpdatePageCount",
-        function done(message) {
-          printPreviewBrowser.messageManager.removeMessageListener(
-            "Printing:Preview:UpdatePageCount",
-            done
-          );
-          resolve(message.data.numPages);
-        }
-      );
-
-      printPreviewBrowser.messageManager.sendAsyncMessage(
-        "Printing:Preview:Enter",
-        {
-          changingBrowsers: false,
-          lastUsedPrinterName: printSettings.printerName,
-          simplifiedMode: false,
-          windowID: sourceBrowser.outerWindowID,
-        }
-      );
-    });
   },
 
   /**
@@ -348,7 +238,7 @@ var PrintUtils = {
    */
   printPreview(aListenerObj) {
     if (PRINT_TAB_MODAL) {
-      this._openTabModalPrint(gBrowser.selectedBrowser.browsingContext);
+      this._openTabModalPrint(aListenerObj.getSourceBrowser().browsingContext);
       return;
     }
 
