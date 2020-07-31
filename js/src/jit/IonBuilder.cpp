0c7da9cb898d9142d707e402ce2afcfa6ed5905c
André Bargull <andre.bargull@gmail.com>
2020-07-31 14:44:18 +0000

    Bug 1655465 - Part 11: Change JSOp::FunctionProto to JSOp::BuiltinObject. r=jandem
    Callers to `GetBuiltinPrototype()` rely on inlining the function itself plus
    optimising the object access, so that the property value is directly seen as a
    constant in the compiler. By changing `GetBuiltinPrototype()` and
    `GetBuiltinConstructor()` to be directly translated into a JSOp, we can avoid
    heavily relying on the compiler to optimise these two functions.
    
    The new opcode replaces the existing JSOp::FunctionProto opcode. It doesn't
    use JSProtoKey directly in order to help jsparagus (bug 1622530), but instead
    uses its own set of mapping from enum values to built-in prototypes and
    constructors.
    
    This change also resolves bug 1377264.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84991

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -25,6 +25,7 @@
 #include "js/ScalarType.h"  // js::Scalar::Type
 #include "util/CheckedArithmetic.h"
 #include "vm/ArgumentsObject.h"
+#include "vm/BuiltinObjectKind.h"
 #include "vm/BytecodeIterator.h"
 #include "vm/BytecodeLocation.h"
 #include "vm/BytecodeUtil.h"
@@ -2383,8 +2384,8 @@ AbortReasonOr<Ok> IonBuilder::inspectOpcode(JSOp op, bool* restarted) {
     case JSOp::ObjWithProto:
       return jsop_objwithproto();
 
-    case JSOp::FunctionProto:
-      return jsop_functionproto();
+    case JSOp::BuiltinObject:
+      return jsop_builtinobject();
 
     case JSOp::CheckReturn:
       return jsop_checkreturn();
@@ -12689,17 +12690,17 @@ AbortReasonOr<Ok> IonBuilder::jsop_objwithproto() {
   return resumeAfter(ins);
 }
 
-AbortReasonOr<Ok> IonBuilder::jsop_functionproto() {
-  JSProtoKey key = JSProto_Function;
+AbortReasonOr<Ok> IonBuilder::jsop_builtinobject() {
+  auto kind = BuiltinObjectKind(GET_UINT8(pc));
 
-  // Bake in the prototype if it exists.
-  if (JSObject* proto = script()->global().maybeGetPrototype(key)) {
-    pushConstant(ObjectValue(*proto));
+  // Bake in the built-in if it exists.
+  if (JSObject* builtin = MaybeGetBuiltinObject(&script()->global(), kind)) {
+    pushConstant(ObjectValue(*builtin));
     return Ok();
   }
 
   // Otherwise emit code to generate it.
-  auto* ins = MFunctionProto::New(alloc());
+  auto* ins = MBuiltinObject::New(alloc(), kind);
   current->add(ins);
   current->push(ins);
   return resumeAfter(ins);
