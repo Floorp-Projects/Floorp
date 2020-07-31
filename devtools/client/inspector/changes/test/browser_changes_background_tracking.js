d35551010a834264fd4a342eab792a595b88dfa0
Razvan Caliman <rcaliman@mozilla.com>
2020-07-31 17:06:33 +0000

    Bug 1656477 - Do not react to cached DOCUMENT_EVENT resources in Changes panel. r=daisuke, a=jcristau
    This patch is borrowing a change that's coming with D85239 to fix the issue that the Changes panel shows as empty when it should show changes tracked in the background (before the Changes panel is opened).
    
    The reason, as identified in D85239, is that the Changes panel reacts to `DOCUMENT_EVENT` resources which are cached in the resource watcher. The Changes panel thinks that the document is reloading so it clears all the changes that were collected. The result in an empty Changes panel.
    
    The screenshot below shows the resource types and the order in which they arrive:
    {F2382929}
    
    Differential Revision: https://phabricator.services.mozilla.com/D85603

---
+++ devtools/client/inspector/changes/test/browser_changes_background_tracking.js
@@ -0,0 +1,46 @@
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test that CSS changes are collected in the background without the Changes panel visible
+
+const TEST_URI = `
+  <style type='text/css'>
+    div {
+      color: red;
+    }
+  </style>
+  <div></div>
+`;
+
+add_task(async function() {
+  info("Ensure Changes panel is NOT the default panel; use Computed panel");
+  await pushPref("devtools.inspector.activeSidebar", "computedview");
+
+  await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
+  const { inspector, view: ruleView } = await openRuleView();
+
+  await selectNode("div", inspector);
+  const prop = getTextProperty(ruleView, 1, { color: "red" });
+
+  info("Disable the first CSS declaration");
+  await togglePropStatus(ruleView, prop);
+
+  info("Select the Changes panel");
+  const { document: doc, store } = selectChangesView(inspector);
+  const onTrackChange = waitUntilAction(store, "TRACK_CHANGE");
+  const onResetChanges = waitUntilAction(store, "RESET_CHANGES");
+
+  info("Wait for change to be tracked");
+  await onTrackChange;
+  const removedDeclarations = getRemovedDeclarations(doc);
+  is(removedDeclarations.length, 1, "One declaration was tracked as removed");
+
+  // Test for Bug 1656477. Check that changes are not cleared immediately afterwards.
+  info("Wait to see if the RESET_CHANGES action is dispatched unexpecteldy");
+  const sym = Symbol();
+  const onTimeout = wait(500).then(() => sym);
+  const raceResult = await Promise.any([onResetChanges, onTimeout]);
+  ok(raceResult === sym, "RESET_CHANGES has not been dispatched");
+});
