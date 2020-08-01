5f16fbcc7b0bd46d1e46ec7fdd973210fba6d222
Jon Bauman <jbauman@mozilla.com>
2020-08-01 02:15:17 +0000

    Bug 1656099 - AVIF (AV1 Image File Format): sniff MIME type from content. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D85503

--- toolkit/components/mediasniffer/nsMediaSniffer.cpp
+++ toolkit/components/mediasniffer/nsMediaSniffer.cpp
@@ -57,6 +57,7 @@ nsMediaSnifferEntry sFtypEntries[] = {
     PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "qt  ", VIDEO_QUICKTIME),
     PATTERN_ENTRY("\xFF\xFF\xFF", "iso", VIDEO_MP4),  // Could be isom or iso2.
     PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "mmp4", VIDEO_MP4),
+    PATTERN_ENTRY("\xFF\xFF\xFF\xFF", "avif", IMAGE_AVIF),
 };
 
 static bool MatchesBrands(const uint8_t aData[4], nsACString& aSniffedType) {
@@ -84,8 +85,8 @@ static bool MatchesBrands(const uint8_t aData[4], nsACString& aSniffedType) {
 // including MP4 (described at
 // http://mimesniff.spec.whatwg.org/#signature-for-mp4), M4A (Apple iTunes
 // audio), and 3GPP.
-static bool MatchesMP4(const uint8_t* aData, const uint32_t aLength,
-                       nsACString& aSniffedType) {
+bool MatchesMP4(const uint8_t* aData, const uint32_t aLength,
+                nsACString& aSniffedType) {
   if (aLength <= MP4_MIN_BYTES_COUNT) {
     return false;
   }
