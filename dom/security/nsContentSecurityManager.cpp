4b0aa44482ff70a186e29c23dbc2227ba0300459
Frederik Braun <fbraun@mozilla.com>
2020-08-03 07:38:47 +0000

    Bug 1656647 - typo in ContentSecurityManager logging r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D85725

--- dom/security/nsContentSecurityManager.cpp
+++ dom/security/nsContentSecurityManager.cpp
@@ -746,7 +746,7 @@ static void DebugDoContentSecurityCheck(nsIChannel* aChannel,
             ("  - upgradeInsecureRequests: %s\n",
              aLoadInfo->GetUpgradeInsecureRequests() ? "true" : "false"));
     MOZ_LOG(sCSMLog, LogLevel::Verbose,
-            ("  - initalSecurityChecksDone: %s\n",
+            ("  - initialSecurityChecksDone: %s\n",
              aLoadInfo->GetInitialSecurityCheckDone() ? "true" : "false"));
     MOZ_LOG(sCSMLog, LogLevel::Verbose,
             ("  - allowDeprecatedSystemRequests: %s\n",
@@ -1069,7 +1069,7 @@ nsresult nsContentSecurityManager::doContentSecurityCheck(
   rv = CheckFTPSubresourceLoad(aChannel);
   NS_ENSURE_SUCCESS(rv, rv);
 
-  // now lets set the initalSecurityFlag for subsequent calls
+  // now lets set the initialSecurityFlag for subsequent calls
   loadInfo->SetInitialSecurityCheckDone(true);
 
   // all security checks passed - lets allow the load
