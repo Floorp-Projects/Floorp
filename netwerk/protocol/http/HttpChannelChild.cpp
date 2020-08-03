6cac9d56a892af6df69cfb2835a7748c13676fc8
Junior Hsu <juhsu@mozilla.com>
2020-08-03 16:49:13 +0000

    Bug 1647950 - stop redirect setup if the channel is canceled in child, r=mayhemer,necko-reviewers
    In child process, if the channel starts to setupRedirect after cancelled, we should
    fail the redirection setup and fail the redirect check.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84881

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -1665,6 +1665,10 @@ nsresult HttpChannelChild::SetupRedirect(nsIURI* uri,
                                          nsIChannel** outChannel) {
   LOG(("HttpChannelChild::SetupRedirect [this=%p]\n", this));
 
+  if (mCanceled) {
+    return NS_ERROR_ABORT;
+  }
+
   nsresult rv;
   nsCOMPtr<nsIIOService> ioService;
   rv = gHttpHandler->GetIOService(getter_AddRefs(ioService));
