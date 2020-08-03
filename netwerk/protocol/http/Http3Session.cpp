1c2de695320828dc0ce038e219a55fcfe3327bb5
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-08-03 14:13:33 +0000

    Bug 1655843 - Call ProcessPendingQ wheen a Http3 connection has been connected. r=michal,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D85201

--- netwerk/protocol/http/Http3Session.cpp
+++ netwerk/protocol/http/Http3Session.cpp
@@ -396,6 +396,7 @@ nsresult Http3Session::ProcessEvents(uint32_t count) {
         mState = CONNECTED;
         SetSecInfo();
         mSocketControl->HandshakeCompleted();
+        gHttpHandler->ConnMgr()->ReportHttp3Connection(mSegmentReaderWriter);
         MaybeResumeSend();
         break;
       case Http3Event::Tag::GoawayReceived:
