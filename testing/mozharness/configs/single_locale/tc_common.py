cc5bd5722422ee5a6caf04764a0c923de83deee2
Tom Prince <mozilla@hocat.ca>
2020-08-04 18:47:55 +0000

    Bug 1653171: Run windows L10n repacks on linux; r=Callek
    Differential Revision: https://phabricator.services.mozilla.com/D85894

--- testing/mozharness/configs/single_locale/tc_common.py
+++ testing/mozharness/configs/single_locale/tc_common.py
@@ -1,3 +1,7 @@
 config = {
     "simple_name_move": True,
+    "vcs_share_base": "/builds/hg-shared",
+    "upload_env": {
+        'UPLOAD_PATH': '/builds/worker/artifacts/',
+    },
 }
--- testing/mozharness/configs/single_locale/tc_win64.py
+++ testing/mozharness/configs/single_locale/tc_win64.py
@@ -1,24 +1,15 @@
 import os
 
 config = {
-    'vcs_share_base': os.path.join('y:', os.sep, 'hg-shared'),
     "bootstrap_env": {
         "NO_MERCURIAL_SETUP_CHECK": "1",
         "MOZ_OBJDIR": "%(abs_obj_dir)s",
         "EN_US_BINARY_URL": os.environ["EN_US_BINARY_URL"],
         "DIST": "%(abs_obj_dir)s",
         "L10NBASEDIR": "../../l10n",
-        'TOOLTOOL_CACHE': os.environ.get('TOOLTOOL_CACHE', 'c:/builds/tooltool_cache'),
+        'TOOLTOOL_CACHE': os.environ.get('TOOLTOOL_CACHE'),
         'EN_US_PACKAGE_NAME': 'target.zip',
-    },
-    "upload_env": {
-        'UPLOAD_PATH': os.path.join(os.getcwd(), 'public', 'build'),
     },
 
     'tooltool_manifest_src': "browser/config/tooltool-manifests/win64/releng.manifest",
-
-    # use mozmake?
-    "enable_mozmake": True,
-    'exes': {},
 }
-
