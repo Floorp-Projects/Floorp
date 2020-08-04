e2bbe3022ca98d063ff72a78185e964701dda2df
Erik Nordin <nordzilla@mozilla.com>
2020-08-04 04:43:47 +0000

    Bug 1655558 - Part 1 Retrieve Paper Information from CUPS on macOS r=jwatt
    Retrieve paper size information directy from CUPS.
    
    - Add unwriteable margins tests.
    - Rename nsPaper to nsPaperCUPS.
    - Add new functionality to nsCUPSShim.
    - Retrieve paper info from CUPS
    
    Differential Revision: https://phabricator.services.mozilla.com/D85211

--- widget/nsPrinterCUPS.cpp
+++ widget/nsPrinterCUPS.cpp
@@ -5,9 +5,11 @@
 
 #include "nsPrinterCUPS.h"
 
-nsPrinterCUPS::nsPrinterCUPS(const nsCUPSShim& aShim, cups_dest_t* aPrinter,
-                             nsTArray<RefPtr<nsIPaper>>&& aPaperList)
-    : mShim(aShim), mPaperList(std::move(aPaperList)) {
+#include "mozilla/ResultExtensions.h"
+#include "nsPaperCUPS.h"
+
+nsPrinterCUPS::nsPrinterCUPS(const nsCUPSShim& aShim, cups_dest_t* aPrinter)
+    : mShim(aShim) {
   MOZ_ASSERT(aPrinter);
   MOZ_ASSERT(mShim.IsInitialized());
   DebugOnly<const int> numCopied = aShim.mCupsCopyDest(aPrinter, 0, &mPrinter);
@@ -18,9 +20,11 @@ nsPrinterCUPS::nsPrinterCUPS(const nsCUPSShim& aShim, cups_dest_t* aPrinter,
 nsPrinterCUPS::~nsPrinterCUPS() {
   if (mPrinterInfo) {
     mShim.mCupsFreeDestInfo(mPrinterInfo);
+    mPrinterInfo = nullptr;
   }
   if (mPrinter) {
     mShim.mCupsFreeDests(1, mPrinter);
+    mPrinter = nullptr;
   }
 }
 
@@ -34,7 +38,10 @@ nsPrinterCUPS::GetName(nsAString& aName) {
 
 NS_IMETHODIMP
 nsPrinterCUPS::GetPaperList(nsTArray<RefPtr<nsIPaper>>& aPaperList) {
-  aPaperList.Assign(mPaperList);
+  if (!mPaperList) {
+    MOZ_TRY(InitPaperList());
+  }
+  aPaperList.Assign(*mPaperList);
   return NS_OK;
 }
 
@@ -60,4 +67,55 @@ bool nsPrinterCUPS::Supports(const char* option, const char* value) const {
   MOZ_ASSERT(mPrinterInfo);
   return mShim.mCupsCheckDestSupported(CUPS_HTTP_DEFAULT, mPrinter,
                                        mPrinterInfo, option, value);
+}
+
+nsresult nsPrinterCUPS::InitPaperList() {
+  if (mPaperList) {
+    return NS_OK;
+  }
+
+  NS_ENSURE_TRUE(mPrinterInfo, NS_ERROR_FAILURE);
+
+  const int paperCount = mShim.mCupsGetDestMediaCount(
+      CUPS_HTTP_DEFAULT, mPrinter, mPrinterInfo, CUPS_MEDIA_FLAGS_DEFAULT);
+
+  // blocking call
+  http_t* connection = mShim.mCupsConnectDest(mPrinter, CUPS_DEST_FLAGS_NONE,
+                                              /* timeout(ms) */ 5000,
+                                              /* cancel */ nullptr,
+                                              /* resource */ nullptr,
+                                              /* resourcesize */ 0,
+                                              /* callback */ nullptr,
+                                              /* user_data */ nullptr);
+
+  NS_ENSURE_TRUE(connection, NS_ERROR_FAILURE);
+
+  mPaperList = MakeUnique<nsTArray<RefPtr<nsIPaper>>>(paperCount);
+  for (int i = 0; i < paperCount; ++i) {
+    cups_size_t paperInfo;
+    int getInfoSucceded = mShim.mCupsGetDestMediaByIndex(
+        CUPS_HTTP_DEFAULT, mPrinter, mPrinterInfo, i, CUPS_MEDIA_FLAGS_DEFAULT,
+        &paperInfo);
+
+    if (!getInfoSucceded) {
+      continue;
+    }
+
+    // localizedName is owned by mPrinterInfo.
+    // https://www.cups.org/doc/cupspm.html#cupsLocalizeDestMedia
+    const char* localizedName =
+        mShim.mCupsLocalizeDestMedia(connection, mPrinter, mPrinterInfo,
+                                     CUPS_MEDIA_FLAGS_DEFAULT, &paperInfo);
+
+    if (!localizedName) {
+      continue;
+    }
+
+    mPaperList->AppendElement(new nsPaperCUPS(paperInfo, localizedName));
+  }
+
+  mShim.mHttpClose(connection);
+  connection = nullptr;
+
+  return NS_OK;
 }
