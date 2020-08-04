fa549f22ae96460abd282cddedd150b003b61da3
Emilio Cobos Álvarez <emilio@crisal.io>
2020-08-04 12:06:37 +0000

    Bug 1656146 - Introduce nsPrinterBase, and add facilities for async attributes queried in a background thread. r=heycam
    While at it, properly CC the printer classes, as they're going to hold
    promises and it's very easy to introduce leaks otherwise.
    
    Co-Authored-By: Jonathan Watt <jwatt@jwatt.org>
    
    Differential Revision: https://phabricator.services.mozilla.com/D85791

---
+++ widget/nsPrinterBase.h
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef nsPrinterBase_h__
+#define nsPrinterBase_h__
+
+#include "nsIPrinter.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsISupportsImpl.h"
+#include "mozilla/EnumeratedArray.h"
+#include "mozilla/Result.h"
+
+namespace mozilla::dom {
+class Promise;
+}
+
+class nsPrinterBase : public nsIPrinter {
+ public:
+  using Promise = mozilla::dom::Promise;
+
+  NS_IMETHOD GetSupportsDuplex(JSContext*, Promise**) final;
+  NS_IMETHOD GetSupportsColor(JSContext*, Promise**) final;
+
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_CLASS(nsPrinterBase)
+
+  // No copy or move, we're an identity.
+  nsPrinterBase(const nsPrinterBase&) = delete;
+  nsPrinterBase(nsPrinterBase&&) = delete;
+
+ private:
+  enum class AsyncAttribute {
+    SupportsDuplex = 0,
+    SupportsColor,
+    // Just a guard.
+    Last,
+  };
+
+  template <typename T>
+  using AsyncAttributeBackgroundTask = T (nsPrinterBase::*)() const;
+
+  // Resolves an async attribute via a background task.
+  template <typename T>
+  nsresult AsyncPromiseAttributeGetter(JSContext* aCx, Promise** aResultPromise,
+                                       AsyncAttribute,
+                                       AsyncAttributeBackgroundTask<T>);
+
+ protected:
+  nsPrinterBase();
+  virtual ~nsPrinterBase();
+
+  // Implementation-specific methods. These must not make assumptions about
+  // which thread they run on.
+  virtual bool SupportsDuplex() const = 0;
+  virtual bool SupportsColor() const = 0;
+
+  // FIXME: Temporary, should probably be moved to the async attribute stuff.
+  nsTArray<RefPtr<nsIPaper>> mPaperList;
+
+ private:
+  mozilla::EnumeratedArray<AsyncAttribute, AsyncAttribute::Last,
+                           RefPtr<Promise>>
+      mAsyncAttributePromises;
+};
+
+#endif
