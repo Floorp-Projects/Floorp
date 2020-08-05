d1f5c30f1225a98572d4f7a819b4b3c61993d0ae
Daniel Holbert <dholbert@cs.stanford.edu>
2020-08-05 03:28:32 +0000

    Bug 1652278 part 2: Flesh out PrintedSheetFrame implementation so that it serves as the container for nsPageFrame. r=TYLin
    This patch:
    - Creates an anon-box pseudo-style for PrintedSheetFrame, in part so that it
      can co-opt the styles that we formerly gave to page-frames in ua.css, to draw
      the sheet of paper and the shadow in Print Preview.
    - Adjusts nsCSSFrameConstructor to create a PrintedSheetFrame as the parent of
      nsPageFrame (inserting between it and its nsPageSequenceFrame container, in
      the frame tree).
    - Fleshes out out a simple BuildDisplayList() implementation for
      PrintedSheetFrame (taking the responsibility for "paper"-drawing from
      nsPageFrame).
    - Fleshes out a simple Reflow implementation for PrintedSheetFrame, just
      placing the child page (assuming there's only one for now) at the origin.
    - Adjusts nsPageFrame and nsPageSequenceFrame to account for the fact that
      there's another layer between them now.
    
    Note that PrintedSheetFrame needs to implement AppendDirectlyOwnedAnonBoxes()
    (just as nsSimplePageSequence and nsPageFrame do), since it owns anonymous
    nsPageFrame instances. This implementation only needs to append the first
    child, as explained in the code-comment and in
    https://bugzilla.mozilla.org/show_bug.cgi?id=1374761#c9 (and of course, for
    now, PrintedSheetFrame only has one child at a time anyway.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D83457

--- layout/generic/PrintedSheetFrame.cpp
+++ layout/generic/PrintedSheetFrame.cpp
@@ -8,6 +8,9 @@
 
 #include "PrintedSheetFrame.h"
 
+#include "nsCSSFrameConstructor.h"
+#include "nsPageFrame.h"
+
 using namespace mozilla;
 
 PrintedSheetFrame* NS_NewPrintedSheetFrame(PresShell* aPresShell,
@@ -26,14 +29,116 @@ NS_IMPL_FRAMEARENA_HELPERS(PrintedSheetFrame)
 
 void PrintedSheetFrame::BuildDisplayList(nsDisplayListBuilder* aBuilder,
                                          const nsDisplayListSet& aLists) {
-  // TODO: Implement me
+  if (PresContext()->IsScreen()) {
+    // Draw the background/shadow/etc. of a blank sheet of paper, for
+    // print-preview.
+    DisplayBorderBackgroundOutline(aBuilder, aLists);
+  }
+
+  // Let each of our children (pages) draw itself:
+  for (auto* frame : mFrames) {
+    BuildDisplayListForChild(aBuilder, frame, aLists);
+  }
 }
 
 void PrintedSheetFrame::Reflow(nsPresContext* aPresContext,
                                ReflowOutput& aReflowOutput,
                                const ReflowInput& aReflowInput,
                                nsReflowStatus& aStatus) {
-  // TODO: Implement me
+  MarkInReflow();
+  DO_GLOBAL_REFLOW_COUNT("PrintedSheetFrame");
+  DISPLAY_REFLOW(aPresContext, this, aReflowInput, aReflowOutput, aStatus);
+  MOZ_ASSERT(aStatus.IsEmpty(), "Caller should pass a fresh reflow status!");
+
+  // If we have a prev-in-flow, take its overflowing content:
+  MoveOverflowToChildList();
+
+  const WritingMode wm = aReflowInput.GetWritingMode();
+
+  // Both the sheet and the pages will use this size:
+  const nsSize physPageSize = aPresContext->GetPageSize();
+  const LogicalSize pageSize(wm, physPageSize);
+
+  // Until bug 1631452, we should only have one child (which we might've just
+  // pulled from our prev-in-flow's overflow list). That means the loop below
+  // is trivial & will will run exactly once, for now.
+  MOZ_ASSERT(mFrames.GetLength() == 1,
+             "how did we get more than one page per sheet");
+  for (auto* pageFrame : mFrames) {
+    MOZ_ASSERT(pageFrame->IsPageFrame(),
+               "we're only expecting page frames as children");
+    // Be sure our child has a pointer to the nsSharedPageData:
+    static_cast<nsPageFrame*>(pageFrame)->SetSharedPageData(mPD);
+
+    ReflowInput pageReflowInput(aPresContext, aReflowInput, pageFrame,
+                                pageSize);
+    // For now, we'll always position the pageFrame at our origin.  (This will
+    // change in bug 1631452 when we support more than 1 page per sheet.)
+    LogicalPoint pagePos(wm);
+
+    // Outparams for reflow:
+    ReflowOutput pageReflowOutput(pageReflowInput);
+    nsReflowStatus status;
+
+    ReflowChild(pageFrame, aPresContext, pageReflowOutput, pageReflowInput, wm,
+                pagePos, physPageSize, ReflowChildFlags::Default, status);
+
+    FinishReflowChild(pageFrame, aPresContext, pageReflowOutput,
+                      &pageReflowInput, wm, pagePos, physPageSize,
+                      ReflowChildFlags::Default);
+
+    // Did this page complete the document, or do we need to generate
+    // another page frame?
+    nsIFrame* pageNextInFlow = pageFrame->GetNextInFlow();
+    if (status.IsFullyComplete()) {
+      // Normally, we (the parent frame) would be responsible for deleting the
+      // next-in-flow of our fully-complete children. But since we don't
+      // support dynamic changes / incremental reflow for printing (and since
+      // the reflow operation is single-pass at this level of granularity), our
+      // child *shouldn't have had any opportunities* to end up with a
+      // next-in-flow that would need cleaning up here.
+      NS_ASSERTION(!pageNextInFlow, "bad child flow list");
+    } else {
+      // Our page frame child needs a continuation. For now, that makes us need
+      // one as well. (This will change in bug 1631452 when we support more
+      // than 1 page per sheet; at that point, we'll only want to do this when
+      // we max out the number of pages that are allowed on our sheet.)
+      aStatus.SetIncomplete();
+
+      if (!pageNextInFlow) {
+        // We need to create a continuation for our page frame. We add the
+        // continuation to our child list, and then push it to our overflow
+        // list so that our own (perhaps yet-to-be-created) continuation can
+        // pull it forward.
+        nsIFrame* continuingPage =
+            PresShell()->FrameConstructor()->CreateContinuingFrame(pageFrame,
+                                                                   this);
+        mFrames.InsertFrame(nullptr, pageFrame, continuingPage);
+        PushChildrenToOverflow(continuingPage, pageFrame);
+      }
+    }
+  }
+
+  // Populate our ReflowOutput outparam -- just use up all the
+  // available space, for both our desired size & overflow areas.
+  aReflowOutput.ISize(wm) = aReflowInput.AvailableISize();
+  if (aReflowInput.AvailableBSize() != NS_UNCONSTRAINEDSIZE) {
+    aReflowOutput.BSize(wm) = aReflowInput.AvailableBSize();
+  }
+  aReflowOutput.SetOverflowAreasToDesiredBounds();
+
+  FinishAndStoreOverflow(&aReflowOutput);
+  NS_FRAME_SET_TRUNCATION(aStatus, aReflowInput, aReflowOutput);
+}
+
+void PrintedSheetFrame::AppendDirectlyOwnedAnonBoxes(
+    nsTArray<OwnedAnonBox>& aResult) {
+  MOZ_ASSERT(mFrames.FirstChild() && mFrames.FirstChild()->IsPageFrame(),
+             "PrintedSheetFrame must have a nsPageFrame child");
+  // Only append the first child; all our children are expected to be
+  // continuations of each other, and our anon box handling always walks
+  // continuations.
+  aResult.AppendElement(mFrames.FirstChild());
 }
 
 #ifdef DEBUG_FRAME_DUMP
