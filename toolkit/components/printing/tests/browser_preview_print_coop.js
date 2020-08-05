6a1958c9d8268950a17c201836baac5c42485561
Tom Tung <shes050117@gmail.com>
2020-08-05 17:14:38 +0000

    Bug 1654935 - Copy over COOP header from new document to the docshell in nsPrintObject; r=nika
    The COOP header is set to BrowsingContext only in Document::StartDocumentLoad.
    If the replaced document has a different COOP header from one in its docshell.
    Then, we can crash while creating a window global child for a new inner window.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85127

---
+++ toolkit/components/printing/tests/browser_preview_print_coop.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_PATH = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "https://example.com"
+);
+
+/**
+ * Verify if the page with a COOP header can be used for printing preview.
+ */
+add_task(async function test() {
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    `${TEST_PATH}file_coop_header.html`
+  );
+
+  // Enter print preview
+  let ppBrowser = PrintPreviewListener.getPrintPreviewBrowser();
+  let printPreviewEntered = BrowserTestUtils.waitForMessage(
+    ppBrowser.messageManager,
+    "Printing:Preview:Entered"
+  );
+  document.getElementById("cmd_printPreview").doCommand();
+  await printPreviewEntered;
+
+  await BrowserTestUtils.waitForCondition(
+    () => gInPrintPreviewMode,
+    "Should be in print preview mode now."
+  );
+
+  ok(true, "We did not crash.");
+
+  PrintUtils.exitPrintPreview();
+
+  BrowserTestUtils.removeTab(tab);
+});
