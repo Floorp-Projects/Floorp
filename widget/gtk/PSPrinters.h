51464aefa0a39f67c1e4c70a385d29588d8f8e2e
Emily McDonough <emcdonough@mozilla.com>
2020-08-05 22:15:18 +0000

    Bug 1654678 Part 4 - Make GTK printer enumeration use the unified CUPS code r=jwatt,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D85502

--- widget/gtk/PSPrinters.h
+++ widget/gtk/PSPrinters.h
@@ -4,22 +4,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef nsPSPrinters_h___
-#define nsPSPrinters_h___
+#ifndef PSPrinters_h___
+#define PSPrinters_h___
 
 #include "nsString.h"
 #include "nsTArray.h"
 
-class nsPSPrinterList {
+namespace mozilla {
+
+class PSPrinters {
+  PSPrinters() = delete;
+
  public:
-  nsPSPrinterList();
 
   /**
    * Is the PostScript module enabled or disabled?
    * @return true if enabled,
    *         false if not.
    */
-  bool Enabled();
+  static bool Enabled();
 
   /**
    * Obtain a list of printers (print destinations) supported by the
@@ -33,21 +36,17 @@ class nsPSPrinterList {
    *              There should always be at least one entry. The
    *              first entry is the default print destination.
    */
-  void GetPrinterList(nsTArray<nsCString>& aList);
-
-  enum PrinterType {
-    kTypeUnknown,  // Not actually handled by the PS module
-    kTypePS,       // Generic postscript module printer
-    kTypeCUPS      // CUPS printer
-  };
+  static void GetPrinterList(nsTArray<nsCString>& aList);
 
   /**
-   * Identify a printer's type from its name.
+   * Identify if a printer is a PS printer from its name.
    * @param aName The printer's full name as a UTF8 string, including
    *              the <type> portion as described for GetPrinterList().
    * @return The PrinterType value for this name.
    */
-  static PrinterType GetPrinterType(const nsACString& aName);
+  static bool IsPSPrinter(const nsACString& aName);
 };
 
-#endif /* nsPSPrinters_h___ */
+}  // namespace mozilla
+
+#endif /* PSPrinters_h___ */
