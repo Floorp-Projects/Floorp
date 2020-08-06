b111833c4ecf42969e12a65ef11bee19780619db
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-08-06 20:57:12 +0300

    Bug 1589517 - Remove unnecessary aSameProcessAsFrameLoader declaration from tabbrowser.js for causing lint failures. a=lint-fix
    CLOSED TREE

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -1551,7 +1551,6 @@
       var aForceNotRemote;
       var aPreferredRemoteType;
       var aUserContextId;
-      var aSameProcessAsFrameLoader;
       var aInitialBrowsingContextGroupId;
       var aOriginPrincipal;
       var aOriginStoragePrincipal;
