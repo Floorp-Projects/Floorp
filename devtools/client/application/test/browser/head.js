09e066039757e8a75b75636ed318a48c558da7e1
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-08-06 14:09:20 +0000

    Bug 1657386 - Use helpers from shared-head to unregister all workers in application panel tests. r=ladybenko.
    Differential Revision: https://phabricator.services.mozilla.com/D86024

--- devtools/client/application/test/browser/head.js
+++ devtools/client/application/test/browser/head.js
@@ -103,22 +103,10 @@ async function openNewTabAndApplicationPanel(url) {
 }
 
 async function unregisterAllWorkers(client, doc) {
-  info("Wait until all workers have a valid registrationFront");
-  let workers;
-  await asyncWaitUntil(async function() {
-    workers = await client.mainRoot.listAllWorkers();
-    const allWorkersRegistered = workers.service.every(
-      worker => !!worker.registrationFront
-    );
-    return allWorkersRegistered;
-  });
+  // This method is declared in shared-head.js
+  await unregisterAllServiceWorkers(client);
 
-  info("Unregister all service workers");
-  for (const worker of workers.service) {
-    await worker.registrationFront.unregister();
-  }
-
-  // wait for service workers to disappear from the UI
+  info("Wait for service workers to disappear from the UI");
   waitUntil(() => getWorkerContainers(doc).length === 0);
 }
 
