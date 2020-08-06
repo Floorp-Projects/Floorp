85e0be181d9369e21b6944d7c98b840fc8721720
Andrew McCreight <continuation@gmail.com>
2020-08-06 17:36:45 +0000

    Bug 1657504, part 2 - Fix deprecation warning in ipdl.py. r=nika
    This is unrelated to the main issue in the bug, but I noticed
    a deprecation warning in the log spew.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86229

--- ipc/ipdl/ipdl.py
+++ ipc/ipdl/ipdl.py
@@ -72,7 +72,7 @@ def normalizedFilename(f):
 
 log(2, 'Reading sync message list')
 parser = RawConfigParser()
-parser.readfp(open(options.syncMsgList))
+parser.read_file(open(options.syncMsgList))
 syncMsgList = parser.sections()
 
 for section in syncMsgList:
@@ -84,7 +84,7 @@ for section in syncMsgList:
 # for the standard segment size used for serialization.
 log(2, 'Reading message metadata...')
 msgMetadataConfig = RawConfigParser()
-msgMetadataConfig.readfp(open(options.msgMetadata))
+msgMetadataConfig.read_file(open(options.msgMetadata))
 
 segmentCapacityDict = {}
 for msgName in msgMetadataConfig.sections():
