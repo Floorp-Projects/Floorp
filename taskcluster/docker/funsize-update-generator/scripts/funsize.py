defdb50b06bd2a544eefdd56f121d1883d6ed199
Simon Fraser <sfraser@mozilla.com>
2020-08-06 21:41:46 +0000

    Bug 1657409 Remove obsolete S3 bucket r=nthomas,Callek
    Differential Revision: https://phabricator.services.mozilla.com/D86168

--- taskcluster/docker/funsize-update-generator/scripts/funsize.py
+++ taskcluster/docker/funsize-update-generator/scripts/funsize.py
@@ -33,7 +33,6 @@ QUEUE_PREFIX = f"{ROOT_URL}/api/queue/"
 ALLOWED_URL_PREFIXES = (
     "http://download.cdn.mozilla.net/pub/mozilla.org/firefox/nightly/",
     "http://download.cdn.mozilla.net/pub/firefox/nightly/",
-    "https://mozilla-nightly-updates.s3.amazonaws.com",
     "http://ftp.mozilla.org/",
     "http://download.mozilla.org/",
     "https://archive.mozilla.org/",
