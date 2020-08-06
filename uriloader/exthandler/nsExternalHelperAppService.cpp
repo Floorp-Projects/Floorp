35b9fd1d77a6e853f49229659d6e35e1f3a59358
Tom Schuster <evilpies@gmail.com>
2020-08-06 11:41:18 +0000

    Bug 1657311 - Hard-code MIME-extension mappings for FLAC and AAC. r=jya,Gijs
    Drive-by: Remove the obviously unused macro MAC_TYPE.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85989

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -455,12 +455,6 @@ struct nsExtraMimeTypeEntry {
   const char* mDescription;
 };
 
-#ifdef XP_MACOSX
-#  define MAC_TYPE(x) x
-#else
-#  define MAC_TYPE(x) 0
-#endif
-
 /**
  * This table lists all of the 'extra' content types that we can deduce from
  * particular file extensions.  These entries also ensure that we provide a good
@@ -529,10 +523,10 @@ static const nsExtraMimeTypeEntry extraMimeEntries[] = {
     {AUDIO_WAV, "wav", "Waveform Audio"},
     {VIDEO_3GPP, "3gpp,3gp", "3GPP Video"},
     {VIDEO_3GPP2, "3g2", "3GPP2 Video"},
+    {AUDIO_AAC, "aac", "AAC Audio"},
+    {AUDIO_FLAC, "flac", "FLAC Audio"},
     {AUDIO_MIDI, "mid", "Standard MIDI Audio"},
     {APPLICATION_WASM, "wasm", "WebAssembly Module"}};
-
-#undef MAC_TYPE
 
 /**
  * File extensions for which decoding should be disabled.
