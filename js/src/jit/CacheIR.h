17346573ef8732a528011d39aca947dda973578d
André Bargull <andre.bargull@gmail.com>
2020-08-07 12:22:48 +0000

    Bug 1655465 - Part 8: Support StringSplitString in CacheIR and Warp. r=jandem
    Drive-by change:
    - Make `intrinsic_StringSplitString` a static function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84989

--- js/src/jit/CacheIR.h
+++ js/src/jit/CacheIR.h
@@ -1651,6 +1651,7 @@ class MOZ_RAII CallIRGenerator : public IRGenerator {
   AttachDecision tryAttachStringToLowerCase(HandleFunction callee);
   AttachDecision tryAttachStringToUpperCase(HandleFunction callee);
   AttachDecision tryAttachStringReplaceString(HandleFunction callee);
+  AttachDecision tryAttachStringSplitString(HandleFunction callee);
   AttachDecision tryAttachMathRandom(HandleFunction callee);
   AttachDecision tryAttachMathAbs(HandleFunction callee);
   AttachDecision tryAttachMathClz32(HandleFunction callee);
