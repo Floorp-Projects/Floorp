277ac10ba0484ca14941f34e2c082f6d73b0205d
Nils Ohlmeier [:drno] <drno@ohlmeier.org>
2020-08-07 13:53:13 +0000

    Bug 1657808: Keep DTLS 1.0 in Release and restrict DTLS 1.3 to Nightly. r=dminor, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D86295

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -506,8 +506,16 @@ pref("media.videocontrols.picture-in-picture.video-toggle.has-used", false);
   pref("media.peerconnection.mute_on_bye_or_timeout", false);
 
   // 770 = DTLS 1.0, 771 = DTLS 1.2, 772 = DTLS 1.3
+#ifdef EARLY_BETA_OR_EARLIER
   pref("media.peerconnection.dtls.version.min", 771);
+#else
+  pref("media.peerconnection.dtls.version.min", 770);
+#endif
+#ifdef NIGHTLY_BUILD
   pref("media.peerconnection.dtls.version.max", 772);
+#else
+  pref("media.peerconnection.dtls.version.max", 771);
+#endif
 
   // These values (aec, agc, and noise) are from:
   // media/webrtc/trunk/webrtc/modules/audio_processing/include/audio_processing.h
