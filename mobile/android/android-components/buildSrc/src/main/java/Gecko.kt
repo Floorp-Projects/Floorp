5628bc3dfaf66a91344fde4ec884989b3c6cdcc7
MickeyMoz <sebastian@mozilla.com>
2020-08-07 14:09:21 +0000

    [components] Update GeckoView (beta) (20200807-140921)

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "80.0.20200804180257"
+    const val beta_version = "80.0.20200806203447"
 
     /**
      * GeckoView Release Version.
