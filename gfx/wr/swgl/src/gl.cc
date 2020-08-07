9567f43ed965ac6365d1c672cd98884a868d5ce9
Lee Salzman <lsalzman@mozilla.com>
2020-08-07 19:36:01 +0000

    Bug 1652894 - fix SWGL code formatting. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D86339

--- gfx/wr/swgl/src/gl.cc
+++ gfx/wr/swgl/src/gl.cc
@@ -413,7 +413,8 @@ struct Program {
   macro(GL_ZERO, GL_ONE_MINUS_SRC_COLOR, 0, 0)                                 \
   macro(GL_ZERO, GL_ONE_MINUS_SRC_COLOR, GL_ZERO, GL_ONE)                      \
   macro(GL_ZERO, GL_ONE_MINUS_SRC_ALPHA, 0, 0)                                 \
-  macro(GL_ZERO, GL_SRC_COLOR, 0, 0) macro(GL_ONE, GL_ONE, 0, 0)               \
+  macro(GL_ZERO, GL_SRC_COLOR, 0, 0)                                           \
+  macro(GL_ONE, GL_ONE, 0, 0)                                                  \
   macro(GL_ONE, GL_ONE, GL_ONE, GL_ONE_MINUS_SRC_ALPHA)                        \
   macro(GL_ONE, GL_ZERO, 0, 0)                                                 \
   macro(GL_ONE_MINUS_DST_ALPHA, GL_ONE, GL_ZERO, GL_ONE)                       \
