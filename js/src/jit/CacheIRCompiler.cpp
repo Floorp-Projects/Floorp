b3a222fe2af210972138f08e14015c0f12ca56b0
André Bargull <andre.bargull@gmail.com>
2020-08-07 12:22:48 +0000

    Bug 1655465 - Part 8: Support StringSplitString in CacheIR and Warp. r=jandem
    Drive-by change:
    - Make `intrinsic_StringSplitString` a static function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D84989

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -7385,6 +7385,32 @@ bool CacheIRCompiler::emitStringReplaceStringResult(
   return true;
 }
 
+bool CacheIRCompiler::emitStringSplitStringResult(StringOperandId strId,
+                                                  StringOperandId separatorId,
+                                                  uint32_t groupOffset) {
+  JitSpew(JitSpew_Codegen, "%s", __FUNCTION__);
+
+  AutoCallVM callvm(masm, this, allocator);
+
+  Register str = allocator.useRegister(masm, strId);
+  Register separator = allocator.useRegister(masm, separatorId);
+  AutoScratchRegister scratch(allocator, masm);
+
+  StubFieldOffset group(groupOffset, StubField::Type::ObjectGroup);
+  emitLoadStubField(group, scratch);
+
+  callvm.prepare();
+  masm.Push(Imm32(INT32_MAX));
+  masm.Push(separator);
+  masm.Push(str);
+  masm.Push(scratch);
+
+  using Fn = ArrayObject* (*)(JSContext*, HandleObjectGroup, HandleString,
+                              HandleString, uint32_t);
+  callvm.call<Fn, js::StringSplitString>();
+  return true;
+}
+
 bool CacheIRCompiler::emitRegExpPrototypeOptimizableResult(
     ObjOperandId protoId) {
   JitSpew(JitSpew_Codegen, "%s", __FUNCTION__);
