d93891a6ae15f213df6e5e6272c9609ca6141c35
Bogdan Tara <btara@mozilla.com>
2020-08-07 18:00:31 +0300

    Backed out changeset b0ea8d83462f (bug 1657616) for es lint failure on blockedSite.js CLOSED TREE

--- browser/base/content/blockedSite.js
+++ browser/base/content/blockedSite.js
@@ -120,16 +120,10 @@ function initPage() {
   });
 
   // Add the learn more content:
-  // Map specific elements to a different message ID, to allow updates to
-  // existing labels
-  let stringMapping = {
-    "malware": "safeb-blocked-malware-page-learn-more-sumo"
-  }
-
   let learnMore = document.getElementById("learn_more");
   document.l10n.setAttributes(
     learnMore,
-    stringMapping[error] || `safeb-blocked-${error}-page-learn-more`
+    "safeb-blocked-" + error + "-page-learn-more"
   );
 
   // Set sitename to bold by adding class
