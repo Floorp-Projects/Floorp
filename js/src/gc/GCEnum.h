44d7729376e0f89908b21d61911255b128550ff4
daemon1024 <barun1024@gmail.com>
2020-08-08 13:54:52 +0000

    Bug 1483269 - Rename gc::AbortReason to GCAbortReason; r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D86431

--- js/src/gc/GCEnum.h
+++ js/src/gc/GCEnum.h
@@ -38,26 +38,6 @@ enum class State {
 #undef MAKE_STATE
 };
 
-// Reasons we reset an ongoing incremental GC or perform a non-incremental GC.
-#define GC_ABORT_REASONS(D)      \
-  D(None, 0)                     \
-  D(NonIncrementalRequested, 1)  \
-  D(AbortRequested, 2)           \
-  D(Unused1, 3)                  \
-  D(IncrementalDisabled, 4)      \
-  D(ModeChange, 5)               \
-  D(MallocBytesTrigger, 6)       \
-  D(GCBytesTrigger, 7)           \
-  D(ZoneChange, 8)               \
-  D(CompartmentRevived, 9)       \
-  D(GrayRootBufferingFailed, 10) \
-  D(JitCodeBytesTrigger, 11)
-enum class AbortReason {
-#define MAKE_REASON(name, num) name = num,
-  GC_ABORT_REASONS(MAKE_REASON)
-#undef MAKE_REASON
-};
-
 #define JS_FOR_EACH_ZEAL_MODE(D)       \
   D(RootsChange, 1)                    \
   D(Alloc, 2)                          \
@@ -91,6 +71,26 @@ enum class ZealMode {
 };
 
 } /* namespace gc */
+
+// Reasons we reset an ongoing incremental GC or perform a non-incremental GC.
+#define GC_ABORT_REASONS(D)      \
+  D(None, 0)                     \
+  D(NonIncrementalRequested, 1)  \
+  D(AbortRequested, 2)           \
+  D(Unused1, 3)                  \
+  D(IncrementalDisabled, 4)      \
+  D(ModeChange, 5)               \
+  D(MallocBytesTrigger, 6)       \
+  D(GCBytesTrigger, 7)           \
+  D(ZoneChange, 8)               \
+  D(CompartmentRevived, 9)       \
+  D(GrayRootBufferingFailed, 10) \
+  D(JitCodeBytesTrigger, 11)
+enum class GCAbortReason {
+#define MAKE_REASON(name, num) name = num,
+  GC_ABORT_REASONS(MAKE_REASON)
+#undef MAKE_REASON
+};
 
 #define JS_FOR_EACH_INTERNAL_MEMORY_USE(_) \
   _(ArrayBufferContents)                   \
