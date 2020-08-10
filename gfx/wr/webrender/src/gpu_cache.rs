b292992aa9c6908bd40fa888dfd5fb7a0f22916d
Razvan Maries <rmaries@mozilla.com>
2020-08-11 02:30:00 +0300

    Backed out changeset a8385068f5dd (bug 1657791) for Reftests perma failures. CLOSED TREE

--- gfx/wr/webrender/src/gpu_cache.rs
+++ gfx/wr/webrender/src/gpu_cache.rs
@@ -139,6 +139,13 @@ impl From<TexelRect> for GpuBlockData {
 }
 
 
+// Any data type that can be stored in the GPU cache should
+// implement this trait.
+pub trait ToGpuBlocks {
+    // Request an arbitrary number of GPU data blocks.
+    fn write_gpu_blocks(&self, _: GpuDataRequest);
+}
+
 // A handle to a GPU resource.
 #[derive(Debug, Copy, Clone, MallocSizeOf)]
 #[cfg_attr(feature = "capture", derive(Serialize))]
