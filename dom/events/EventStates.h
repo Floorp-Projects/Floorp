f6c41cd617f346fe3caabbd1166597d8876c61ec
Sean Feng <sefeng@mozilla.com>
2020-08-10 18:20:51 +0000

    Bug 1200896 - Make the document blocked by the topmost element in the top layer r=emilio
    Spec: https://html.spec.whatwg.org/multipage/#blocked-by-a-modal-dialog
    
    Differential Revision: https://phabricator.services.mozilla.com/D86227

--- dom/events/EventStates.h
+++ dom/events/EventStates.h
@@ -296,7 +296,8 @@ class EventStates {
 #define NS_EVENT_STATE_MODAL_DIALOG NS_DEFINE_EVENT_STATE_MACRO(53)
 // Inert subtrees
 #define NS_EVENT_STATE_MOZINERT NS_DEFINE_EVENT_STATE_MACRO(54)
-
+// Topmost Modal <dialog> element in top layer
+#define NS_EVENT_STATE_TOPMOST_MODAL_DIALOG NS_DEFINE_EVENT_STATE_MACRO(55)
 /**
  * NOTE: do not go over 63 without updating EventStates::InternalType!
  */
@@ -332,7 +333,7 @@ class EventStates {
    NS_EVENT_STATE_FOCUS_WITHIN | NS_EVENT_STATE_FULLSCREEN |                   \
    NS_EVENT_STATE_HOVER | NS_EVENT_STATE_URLTARGET |                           \
    NS_EVENT_STATE_FOCUS_VISIBLE | NS_EVENT_STATE_MODAL_DIALOG |                \
-   NS_EVENT_STATE_MOZINERT)
+   NS_EVENT_STATE_MOZINERT | NS_EVENT_STATE_TOPMOST_MODAL_DIALOG)
 
 #define INTRINSIC_STATES (~EXTERNALLY_MANAGED_STATES)
 
