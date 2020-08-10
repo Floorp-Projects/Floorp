0beed5742d7ff5a4fd93c04f478475bf5bf3e680
Emilio Cobos Álvarez <emilio@crisal.io>
2020-08-10 14:50:27 +0000

    Bug 1639328 - Make sure BrowserChilds for OOP iframes start in a consistent state. r=smaug
    Right now they start with a FullyHidden() effect info, but with a
    "visible" widget, and thus active docshell and so on.
    
    That's no good :)
    
    Differential Revision: https://phabricator.services.mozilla.com/D86364

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -1014,7 +1014,8 @@ nsresult ContentChild::ProvideWindowCommon(
 
   // Now that |newChild| has had its IPC link established, call |Init| to set it
   // up.
-  nsPIDOMWindowOuter* parentWindow = parent ? parent->GetDOMWindow() : nullptr;
+  RefPtr<nsPIDOMWindowOuter> parentWindow =
+      parent ? parent->GetDOMWindow() : nullptr;
   if (NS_FAILED(newChild->Init(parentWindow, windowChild))) {
     return NS_ERROR_ABORT;
   }
