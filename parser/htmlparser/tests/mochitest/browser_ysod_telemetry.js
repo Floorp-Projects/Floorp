e0dc9c982300d8c228d429393db0a55ce15dfd69
Zibi Braniecki <zbraniecki@mozilla.com>
2020-08-10 17:38:00 +0000

    Bug 1657242 - Report telemetry for Yellow Screen of Death. r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D85951

---
+++ parser/htmlparser/tests/mochitest/browser_ysod_telemetry.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { TelemetryTestUtils } = ChromeUtils.import(
+  "resource://testing-common/TelemetryTestUtils.jsm"
+);
+
+add_task(async function test_popup_opened() {
+  Services.telemetry.clearEvents();
+  Services.telemetry.setEventRecordingEnabled("ysod", true);
+
+  const PAGE_URL = getRootDirectory(gTestPath) + "broken_xml.xhtml";
+  let viewSourceTab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    PAGE_URL
+  );
+
+  let content = await SpecialPowers.spawn(
+    viewSourceTab.linkedBrowser,
+    [],
+    async function() {
+      return content.document.documentElement.innerHTML;
+    }
+  );
+
+  ok(content.includes("XML"), "Document shows XML error");
+
+  TelemetryTestUtils.assertEvents(
+    [
+      {
+        method: "shown",
+        object: "ysod",
+        value: PAGE_URL.substr(0, 80),
+      },
+    ],
+    {
+      category: "ysod",
+    },
+    { process: "parent" }
+  );
+  BrowserTestUtils.removeTab(viewSourceTab);
+});
