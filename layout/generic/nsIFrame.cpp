c06aa9beb0a87d29fb3a5ab856ab99073a06c49f
Dorel Luca <dluca@mozilla.com>
2020-08-10 09:07:48 +0300

    Backed out 5 changesets (bug 1623764) for WPT failures in /editing/run/forwarddelete.html. CLOSED TREE
    Backed out changeset 838b1a256f25 (bug 1623764)
    Backed out changeset 00911e3c92c3 (bug 1623764)
    Backed out changeset 5f7d278953d7 (bug 1623764)
    Backed out changeset 0059379a3c29 (bug 1623764)
    Backed out changeset 6e3a9276ec41 (bug 1623764)

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -8342,9 +8342,6 @@ nsresult nsIFrame::PeekOffsetForCharacter(nsPeekOffsetStruct* aPos,
 nsresult nsIFrame::PeekOffsetForWord(nsPeekOffsetStruct* aPos,
                                      int32_t aOffset) {
   SelectablePeekReport current{this, aOffset};
-  bool shouldStopAtHardBreak =
-      aPos->mWordMovementType == eDefaultBehavior &&
-      StaticPrefs::layout_word_select_eat_space_to_next_word();
   bool wordSelectEatSpace = ShouldWordSelectionEatSpace(*aPos);
 
   PeekWordState state;
@@ -8379,35 +8376,11 @@ nsresult nsIFrame::PeekOffsetForWord(nsPeekOffsetStruct* aPos,
       break;
     }
 
-    if (shouldStopAtHardBreak && next.mJumpedHardBreak) {
-      /**
-       * Prev, always: Jump and stop right there
-       * Next, saw inline: just stop
-       * Next, no inline: Jump and consume whitespaces
-       */
-      if (aPos->mDirection == eDirPrevious) {
-        // Try moving to the previous line if exists
-        current.TransferTo(*aPos);
-        current.mFrame->PeekOffsetForCharacter(aPos, current.mOffset);
-        return NS_OK;
-      }
-      if (state.mSawInlineCharacter || current.mJumpedHardBreak) {
-        if (current.mFrame->HasSignificantTerminalNewline()) {
-          current.mOffset -= 1;
-        }
-        current.TransferTo(*aPos);
-        return NS_OK;
-      }
-      // Mark the state as whitespace and continue
-      state.Update(false, true);
-    }
-
     if (next.mJumpedLine) {
       state.mContext.Truncate();
     }
     current = next;
     // Jumping a line is equivalent to encountering whitespace
-    // This affects only when it already met an actual character
     if (wordSelectEatSpace && next.mJumpedLine) {
       state.SetSawBeforeType();
     }
@@ -8769,11 +8742,11 @@ Result<bool, nsresult> nsIFrame::IsVisuallyAtLineEdge(
   nsIFrame* firstFrame;
   nsIFrame* lastFrame;
 
-  bool lineIsRTL = aLineIterator->GetDirection();
+  nsAutoLineIterator it = aLineIterator;
+  bool lineIsRTL = it->GetDirection();
   bool isReordered;
 
-  MOZ_TRY(aLineIterator->CheckLineOrder(aLine, &isReordered, &firstFrame,
-                                        &lastFrame));
+  MOZ_TRY(it->CheckLineOrder(aLine, &isReordered, &firstFrame, &lastFrame));
 
   nsIFrame** framePtr = aDirection == eDirPrevious ? &firstFrame : &lastFrame;
   if (!*framePtr) {
@@ -8791,7 +8764,8 @@ Result<bool, nsresult> nsIFrame::IsVisuallyAtLineEdge(
 
 Result<bool, nsresult> nsIFrame::IsLogicallyAtLineEdge(
     nsILineIterator* aLineIterator, int32_t aLine, nsDirection aDirection) {
-  auto line = aLineIterator->GetLine(aLine).unwrap();
+  nsAutoLineIterator it = aLineIterator;
+  auto line = it->GetLine(aLine).unwrap();
 
   if (aDirection == eDirPrevious) {
     nsIFrame* firstFrame = line.mFirstFrameOnLine;
@@ -8803,7 +8777,7 @@ Result<bool, nsresult> nsIFrame::IsLogicallyAtLineEdge(
   nsIFrame* lastFrame = line.mFirstFrameOnLine;
   for (int32_t lineFrameCount = line.mNumFramesOnLine; lineFrameCount > 1;
        lineFrameCount--) {
-    MOZ_TRY(aLineIterator->GetNextSiblingOnLine(lastFrame, aLine));
+    MOZ_TRY(it->GetNextSiblingOnLine(lastFrame, aLine));
     if (!lastFrame) {
       NS_ERROR("should not be reached nsIFrame");
       return Err(NS_ERROR_FAILURE);
@@ -8838,7 +8812,7 @@ nsIFrame::SelectablePeekReport nsIFrame::GetFrameFromDirection(
     MOZ_TRY_VAR(thisLine,
                 traversedFrame->GetLineNumber(aScrollViewStop, &blockFrame));
 
-    nsAutoLineIterator it = blockFrame->GetLineIterator();
+    nsILineIterator* it = blockFrame->GetLineIterator();
 
     bool atLineEdge;
     MOZ_TRY_VAR(
@@ -8850,12 +8824,6 @@ nsIFrame::SelectablePeekReport nsIFrame::GetFrameFromDirection(
       result.mJumpedLine = true;
       if (!aJumpLines) {
         return result;  // we are done. cannot jump lines
-      }
-      int32_t lineToCheckWrap =
-          aDirection == eDirPrevious ? thisLine - 1 : thisLine;
-      if (lineToCheckWrap < 0 ||
-          !it->GetLine(lineToCheckWrap).unwrap().mIsWrapped) {
-        result.mJumpedHardBreak = true;
       }
     }
 
@@ -8879,9 +8847,7 @@ nsIFrame::SelectablePeekReport nsIFrame::GetFrameFromDirection(
       for (nsIFrame* current = traversedFrame->GetPrevSibling(); current;
            current = current->GetPrevSibling()) {
         if (!current->IsBlockOutside() && IsSelectable(current)) {
-          if (!current->IsBrFrame()) {
-            canSkipBr = true;
-          }
+          canSkipBr = true;
           break;
         }
       }
