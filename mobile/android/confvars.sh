cd9a620edec3cf3141542a7dd19069ab11f1ebc1
Ricky Stewart <rstewart@mozilla.com>
2020-08-11 15:58:52 +0000

    Bug 1657954 - Move various branding options from `old-configure` r=geckoview-reviewers,mhentges,nalexander,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D86391

--- mobile/android/confvars.sh
+++ mobile/android/confvars.sh
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Fennec
 MOZ_APP_VENDOR=Mozilla
 
 MOZ_APP_UA_NAME=Firefox
