59250d1b733fba6493af3fd4184567b2f28ad75a
Ricky Stewart <rstewart@mozilla.com>
2020-08-11 15:58:52 +0000

    Bug 1657954 - Move various branding options from `old-configure` r=geckoview-reviewers,mhentges,nalexander,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D86391

--- tools/update-programs/confvars.sh
+++ tools/update-programs/confvars.sh
@@ -3,7 +3,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Firefox
 MOZ_APP_VENDOR=Mozilla
 
 MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
