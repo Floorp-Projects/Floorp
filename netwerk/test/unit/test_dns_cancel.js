daacd0f88d7c2ddf5bec115eac721f54e894f876
Valentin Gosu <valentin.gosu@gmail.com>
2020-08-12 01:00:39 +0000

    Bug 1657582 - Add nsIDNSResolverInfo interface r=necko-reviewers,geckoview-reviewers,snorp,mixedpuppy,extension-reviewers,dragana
    This patch adds the nsIDNSResolverInfo interface which is used to hold
    information about the resolver to be used in a DNS resolution.
    
    We use this to merge all of the *WithTRRServer resolve functions into one.
    Passing a resolver info will use that object when appropriate. No resolver
    info means that we default to using the system resolver, or the default TRR
    resolver.
    
    This patch also converts the RESOLVE_TYPE_* flags into a cenum and adds
    the resolveType as a parameter to asyncResolve thus removing the need
    to have asyncResolveByType methods.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86176

--- netwerk/test/unit/test_dns_cancel.js
+++ netwerk/test/unit/test_dns_cancel.js
@@ -57,14 +57,18 @@ function run_test() {
   // This one will be canceled with cancelAsyncResolve.
   requestList1Canceled1 = dns.asyncResolve(
     hostname2,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener1,
     mainThread,
     defaultOriginAttributes
   );
   dns.cancelAsyncResolve(
     hostname2,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener1,
     Cr.NS_ERROR_ABORT,
     defaultOriginAttributes
@@ -73,7 +77,9 @@ function run_test() {
   // This one will not be canceled.
   requestList1NotCanceled = dns.asyncResolve(
     hostname1,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener1,
     mainThread,
     defaultOriginAttributes
@@ -82,7 +88,9 @@ function run_test() {
   // This one will be canceled with cancel(Cr.NS_ERROR_ABORT).
   requestList1Canceled2 = dns.asyncResolve(
     hostname1,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener1,
     mainThread,
     defaultOriginAttributes
@@ -92,7 +100,9 @@ function run_test() {
   // This one will not be canceled.
   requestList2NotCanceled = dns.asyncResolve(
     hostname1,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener2,
     mainThread,
     defaultOriginAttributes
@@ -101,7 +111,9 @@ function run_test() {
   // This one will be canceled with cancel(Cr.NS_ERROR_ABORT).
   requestList2Canceled = dns.asyncResolve(
     hostname2,
+    Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
     flags,
+    null, // resolverInfo
     listener2,
     mainThread,
     defaultOriginAttributes
