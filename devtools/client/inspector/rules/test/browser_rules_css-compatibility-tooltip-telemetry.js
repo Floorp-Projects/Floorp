e026267efe579aaf800ead1cd4bab370097dc781
Kriyszig <lelouch.cpp@gmail.com>
2020-08-12 02:41:48 +0000

    Bug 1657984 - Part 2 - Test the telemetry for CSS Compatibility Tooltip r=daisuke,mtigley
    We check if telemetry "devtools.tooltip.shown" is set for label
    "css-compatibility" once the tooltip is toggled visible during
    the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86403

---
+++ devtools/client/inspector/rules/test/browser_rules_css-compatibility-tooltip-telemetry.js
@@ -0,0 +1,58 @@
+/* vim: set ft=javascript ts=2 et sw=2 tw=80: */
+/* Any copyright is dedicated to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Test - Toggling rules linked to the element and the class
+// Checking whether the compatibility warning icon is displayed
+// correctly.
+// If a rule is disabled, it is marked compatible to keep
+// consistency with compatibility panel.
+// We test both the compatible and incompatible rules here
+
+const TEST_URI = `
+<style>
+  div {
+    cursor: pointer;
+  }
+</style>
+<div></div>`;
+
+const TEST_DATA = [
+  {
+    selector: "div",
+    rules: [
+      {},
+      {
+        cursor: {
+          value: "pointer",
+          expected: COMPATIBILITY_TOOLTIP_MESSAGE.default,
+        },
+      },
+    ],
+  },
+];
+
+add_task(async function() {
+  startTelemetry();
+
+  await pushPref(
+    "devtools.inspector.ruleview.inline-compatibility-warning.enabled",
+    true
+  );
+  await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
+  const { inspector, view } = await openRuleView();
+
+  info("Check correctness of data by toggling tooltip open");
+  await runCSSCompatibilityTests(view, inspector, TEST_DATA);
+
+  checkResults();
+});
+
+function checkResults() {
+  info(
+    'Check the telemetry against "devtools.tooltip.shown" for label "css-compatibility" and ensure it is set'
+  );
+  checkTelemetry("devtools.tooltip.shown", "", 1, "css-compatibility");
+}
