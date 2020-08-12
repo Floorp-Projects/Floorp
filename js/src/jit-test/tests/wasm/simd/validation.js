b351289a3b72ae7c43b00a10bd6011afc9807252
Lars T Hansen <lhansen@mozilla.com>
2020-08-12 07:49:12 +0000

    Bug 1656216 - Improve SIMD test cases.  r=jseward
    This fleshes out the test cases to cover some corner cases that were
    left uncovered before.  No errors were found.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85981

--- js/src/jit-test/tests/wasm/simd/validation.js
+++ js/src/jit-test/tests/wasm/simd/validation.js
@@ -208,7 +208,10 @@ for (let op of [
     'i16x8.any_true',
     'i16x8.all_true',
     'i32x4.any_true',
-    'i32x4.all_true'])
+    'i32x4.all_true',
+    'i8x16.bitmask',
+    'i16x8.bitmask',
+    'i32x4.bitmask'])
 {
     testValid(`(module
                  (func (param v128) (result i32)
