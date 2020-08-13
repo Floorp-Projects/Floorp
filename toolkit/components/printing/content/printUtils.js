952e7dd5d76658316cb8533dc476724abd26e7b2
Emma Malysz <emalysz@mozilla.com>
2020-08-13 18:12:04 +0000

    Bug 1653323: show print preview progress in the print preview browser when preview is being rendered. r=fluent-reviewers,flod,jaws
    Differential Revision: https://phabricator.services.mozilla.com/D86116

--- toolkit/components/printing/content/printUtils.js
+++ toolkit/components/printing/content/printUtils.js
@@ -153,6 +153,7 @@ var PrintUtils = {
 
     // Move the overlay so it overlaps the chrome and content.
     container.prepend(dialog._overlay);
+    container.querySelector(".printSettingsBrowser").hidden = false;
 
     // Store the dialog on the overlay for access in the tests.
     dialog._overlay._dialog = dialog;
@@ -163,7 +164,8 @@ var PrintUtils = {
       skipLoad: false,
     });
     printPreviewBrowser.classList.add("printPreviewBrowser");
-    container.querySelector(".printTabModalView").prepend(printPreviewBrowser);
+    printPreviewBrowser.setAttribute("isRendering", "true");
+    container.querySelector(".previewStack").append(printPreviewBrowser);
     printPreviewBrowser.loadURI("about:printpreview", {
       triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
     });
@@ -195,6 +197,9 @@ var PrintUtils = {
    * @return {Promise<Integer>} The number of pages that were rendered in the preview.
    */
   updatePrintPreview(sourceBrowser, printPreviewBrowser, printSettings) {
+    let previewContainer = printPreviewBrowser.parentElement;
+    previewContainer.classList.add("previewRendering");
+
     return new Promise(resolve => {
       printPreviewBrowser.messageManager.addMessageListener(
         "Printing:Preview:UpdatePageCount",
@@ -203,6 +208,10 @@ var PrintUtils = {
             "Printing:Preview:UpdatePageCount",
             done
           );
+
+          previewContainer.classList.remove("previewRendering");
+          printPreviewBrowser.removeAttribute("isRendering");
+
           resolve(message.data.numPages);
         }
       );
