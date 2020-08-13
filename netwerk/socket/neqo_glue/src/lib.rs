3274d6bc820273346bf813abdad527bd25a0ee75
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-08-13 19:21:53 +0000

    Bug 1653179 - Handle STOPSENDING on a http3 stream. r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D86813

--- netwerk/socket/neqo_glue/src/lib.rs
+++ netwerk/socket/neqo_glue/src/lib.rs
@@ -402,6 +402,7 @@ pub enum Http3Event {
     /// A server has send STOP_SENDING frame.
     StopSending {
         stream_id: u64,
+        error: u64,
     },
     HeaderReady {
         stream_id: u64,
@@ -484,8 +485,8 @@ pub extern "C" fn neqo_http3conn_event(
     while let Some(evt) = conn.conn.next_event() {
         let fe = match evt {
             Http3ClientEvent::DataWritable { stream_id } => Http3Event::DataWritable { stream_id },
-            Http3ClientEvent::StopSending { stream_id, .. } => {
-                Http3Event::StopSending { stream_id }
+            Http3ClientEvent::StopSending { stream_id, error } => {
+                Http3Event::StopSending { stream_id, error }
             }
             Http3ClientEvent::HeaderReady {
                 stream_id,
