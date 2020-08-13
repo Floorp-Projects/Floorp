5ec0527f8fceb23fb5d54db5b024a13bb99128b4
Iain Ireland <iireland@mozilla.com>
2020-08-13 13:48:31 +0000

    Bug 1658811: Transpile GuardNotClassConstructor r=jandem
    Depends on D86856
    
    Differential Revision: https://phabricator.services.mozilla.com/D86857

--- js/src/jit/IonTypes.h
+++ js/src/jit/IonTypes.h
@@ -203,6 +203,9 @@ enum class BailoutKind : uint8_t {
   // Bailout triggered by MGuardTagNotEqual.
   TagNotEqualGuard,
 
+  // Bailout triggered by MGuardFunctionKind.
+  FunctionKindGuard,
+
   // When we're trying to use an uninitialized lexical.
   UninitializedLexical,
 
@@ -305,6 +308,8 @@ inline const char* BailoutKindString(BailoutKind kind) {
       return "BailoutKind::NullOrUndefinedGuard";
     case BailoutKind::TagNotEqualGuard:
       return "BailoutKind::TagNotEqualGuard";
+    case BailoutKind::FunctionKindGuard:
+      return "BailoutKind::FunctionKindGuard";
     case BailoutKind::UninitializedLexical:
       return "BailoutKind::UninitializedLexical";
     case BailoutKind::IonExceptionDebugMode:
