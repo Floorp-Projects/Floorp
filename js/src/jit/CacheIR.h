2c2cea4efb4763d66ebb402b94d9edbc648b7f6c
Razvan Maries <rmaries@mozilla.com>
2020-08-13 16:16:03 +0300

    Backed out 3 changesets (bug 1658279) for perma failures on browser_topsites_section.js. CLOSED TREE
    Backed out changeset 728d98643858 (bug 1658279)
    Backed out changeset 761efef13026 (bug 1658279)
    Backed out changeset 25869d8b6148 (bug 1658279)

--- js/src/jit/CacheIR.h
+++ js/src/jit/CacheIR.h
@@ -1636,7 +1636,6 @@ class MOZ_RAII CallIRGenerator : public IRGenerator {
   AttachDecision tryAttachArrayPopShift(HandleFunction callee,
                                         InlinableNative native);
   AttachDecision tryAttachArrayJoin(HandleFunction callee);
-  AttachDecision tryAttachArraySlice(HandleFunction callee);
   AttachDecision tryAttachArrayIsArray(HandleFunction callee);
   AttachDecision tryAttachDataViewGet(HandleFunction callee, Scalar::Type type);
   AttachDecision tryAttachDataViewSet(HandleFunction callee, Scalar::Type type);
