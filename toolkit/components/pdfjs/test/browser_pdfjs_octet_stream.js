d9f89804853791e75d14ae33dfbf8dd2e038773a
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-08-13 22:35:10 +0000

    Bug 1653255 - avoid using PDF.js for octet-stream content in frames, r=mattwoodrow CLOSED TREE
    Differential Revision: https://phabricator.services.mozilla.com/D86990

--- toolkit/components/pdfjs/test/browser_pdfjs_octet_stream.js
+++ toolkit/components/pdfjs/test/browser_pdfjs_octet_stream.js
@@ -8,17 +8,18 @@ const TESTROOT = getRootDirectory(gTestPath).replace(
   "http://mochi.test:8888/"
 );
 
+// Get a ref to the pdf we want to open.
+const PDF_URL = TESTROOT + "file_pdfjs_object_stream.pdf";
+
+var gMIMEService = Cc["@mozilla.org/mime;1"].getService(Ci.nsIMIMEService);
+
 /**
  * Check that if we open a PDF with octet-stream mimetype, it can load
  * PDF.js .
  */
 add_task(async function test_octet_stream_opens_pdfjs() {
   await SpecialPowers.pushPrefEnv({ set: [["pdfjs.handleOctetStream", true]] });
-  // Get a ref to the pdf we want to open.
-  let pdfURL = TESTROOT + "file_pdfjs_object_stream.pdf";
-
-  let mimeService = Cc["@mozilla.org/mime;1"].getService(Ci.nsIMIMEService);
-  let handlerInfo = mimeService.getFromTypeAndExtension(
+  let handlerInfo = gMIMEService.getFromTypeAndExtension(
     "application/pdf",
     "pdf"
   );
@@ -38,8 +39,43 @@ add_task(async function test_octet_stream_opens_pdfjs() {
   await BrowserTestUtils.withNewTab(
     { gBrowser, url: "about:blank" },
     async function(newTabBrowser) {
-      await waitForPdfJS(newTabBrowser, pdfURL);
-      is(newTabBrowser.currentURI.spec, pdfURL, "Should load pdfjs");
+      await waitForPdfJS(newTabBrowser, PDF_URL);
+      is(newTabBrowser.currentURI.spec, PDF_URL, "Should load pdfjs");
+    }
+  );
+});
+
+/**
+ * Check that if the octet-stream thing is in a frame, we don't load it inside
+ * PDF.js
+ */
+add_task(async function test_octet_stream_in_frame_downloads() {
+  await SpecialPowers.pushPrefEnv({ set: [["pdfjs.handleOctetStream", true]] });
+
+  let handlerInfo = gMIMEService.getFromTypeAndExtension(
+    "application/pdf",
+    "pdf"
+  );
+
+  // Make sure pdf.js is the default handler.
+  is(
+    handlerInfo.alwaysAskBeforeHandling,
+    false,
+    "pdf handler defaults to always-ask is false"
+  );
+  is(
+    handlerInfo.preferredAction,
+    Ci.nsIHandlerInfo.handleInternally,
+    "pdf handler defaults to internal"
+  );
+
+  let dialogPromise = BrowserTestUtils.domWindowOpenedAndLoaded();
+  await BrowserTestUtils.withNewTab(
+    { gBrowser, url: `data:text/html,<iframe src='${PDF_URL}'>` },
+    async function(newTabBrowser) {
+      let dialogWin = await dialogPromise;
+      ok(dialogWin, "Should have a dialog asking what to do.");
+      dialogWin.close(); // This is going to cancel the dialog.
     }
   );
 });
