60ee52c3f90a98cb8d46affe24826626c455f6c2
Matt Woodrow <mwoodrow@mozilla.com>
2020-08-14 00:56:22 +0000

    Bug 1658858 - Implement ImageRendering filtering parameter for SwCompositor. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D86890

--- gfx/wr/swgl/src/swgl_fns.rs
+++ gfx/wr/swgl/src/swgl_fns.rs
@@ -299,6 +299,7 @@ extern "C" {
         dst_height: GLsizei,
         opaque: GLboolean,
         flip: GLboolean,
+        filter: GLenum,
     );
     fn CreateContext() -> *mut c_void;
     fn ReferenceContext(ctx: *mut c_void);
@@ -2301,6 +2302,7 @@ impl LockedResource {
         dst_height: GLsizei,
         opaque: bool,
         flip: bool,
+        filter: GLenum,
     ) {
         unsafe {
             Composite(
@@ -2316,6 +2318,7 @@ impl LockedResource {
                 dst_height,
                 opaque as GLboolean,
                 flip as GLboolean,
+                filter,
             );
         }
     }
