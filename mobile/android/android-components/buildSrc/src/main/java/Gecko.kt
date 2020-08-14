08b66009b670785b70c59c3714c50cdd7155e178
Sebastian Kaspari <s.kaspari@gmail.com>
2020-08-14 13:30:23 +0200

    [components] Update GeckoView (release) to 79.0.20200813192915.

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -16,7 +16,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Release Version.
      */
-    const val release_version = "79.0.20200720193547"
+    const val release_version = "79.0.20200813192915"
 }
 
 @Suppress("Unused", "MaxLineLength")
