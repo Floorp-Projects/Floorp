4b61ccb0217628abfc8d9ecda7bd197b3a09018b
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-08-14 08:36:35 +0300

    Backed out changeset 8b917683232d (bug 1656277) for causing xpcshell failures in test_IE_bookmarks.js
    CLOSED TREE

--- browser/components/migration/tests/unit/test_Safari_bookmarks.js
+++ browser/components/migration/tests/unit/test_Safari_bookmarks.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const { CustomizableUI } = ChromeUtils.import(
-  "resource:///modules/CustomizableUI.jsm"
-);
-
 add_task(async function() {
   registerFakePath("ULibDir", do_get_file("Library/"));
 
@@ -42,21 +38,6 @@ add_task(async function() {
     }
   };
   PlacesUtils.observers.addListener(["bookmark-added"], listener);
-  let observerNotified = false;
-  Services.obs.addObserver((aSubject, aTopic, aData) => {
-    let [toolbar, visibility] = JSON.parse(aData);
-    Assert.equal(
-      toolbar,
-      CustomizableUI.AREA_BOOKMARKS,
-      "Notification should be received for bookmarks toolbar"
-    );
-    Assert.equal(
-      visibility,
-      "true",
-      "Notification should say to reveal the bookmarks toolbar"
-    );
-    observerNotified = true;
-  }, "browser-set-toolbar-visibility");
 
   await promiseMigration(migrator, MigrationUtils.resourceTypes.BOOKMARKS);
   PlacesUtils.observers.removeListener(["bookmark-added"], listener);
@@ -71,5 +52,4 @@ add_task(async function() {
     itemCount,
     "Telemetry reporting correct."
   );
-  Assert.ok(observerNotified, "The observer should be notified upon migration");
 });
