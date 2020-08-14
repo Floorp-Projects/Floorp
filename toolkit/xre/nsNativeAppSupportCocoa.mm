c43efad9507fb240263545c3b899ea4c2d1fff27
Mike Hommey <mh+mozilla@glandium.org>
2020-08-14 14:20:37 +0000

    Bug 1659047 - Don't fail to start on macOS 11 when building with SDK 11.0. r=froydnj
    At the same time, bump the test to reflect our actual current minimum
    supported version.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87026

--- toolkit/xre/nsNativeAppSupportCocoa.mm
+++ toolkit/xre/nsNativeAppSupportCocoa.mm
@@ -71,7 +71,7 @@ NS_IMETHODIMP nsNativeAppSupportCocoa::Start(bool* _retval) {
   // alert here.  But the alert's message and buttons would require custom
   // localization.  So (for now at least) we just log an English message
   // to the console before quitting.
-  if (major < 10 || minor < 6) {
+  if (major < 10 || (major == 10 && minor < 12)) {
     NSLog(@"Minimum OS version requirement not met!");
     return NS_OK;
   }
