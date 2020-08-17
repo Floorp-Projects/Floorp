c410920ad123cecbad66805df68f10f839877a2d
Adam Gashlin <agashlin@mozilla.com>
2020-08-17 20:13:27 +0000

    Bug 1639067 part 4 - Generalize internal handlers in the Applications list. r=Gijs,preferences-reviewers,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D86653

--- browser/components/preferences/tests/browser_filetype_dialog.js
+++ browser/components/preferences/tests/browser_filetype_dialog.js
@@ -170,9 +170,12 @@ add_task(async function dialogShowsCorrectContent() {
   let desc = dialogWin.document.getElementById("appDescription");
   let descL10n = dialogWin.document.l10n.getAttributes(desc);
   is(descL10n.id, "app-manager-handle-file", "Should have right string");
+  let stringBundle = Services.strings.createBundle(
+    "chrome://mozapps/locale/downloads/unknownContentType.properties"
+  );
   is(
     descL10n.args.type,
-    await dialogWin.document.l10n.formatValue("applications-type-pdf"),
+    stringBundle.GetStringFromName("pdfExtHandlerDescription"),
     "Should have PDF string bits."
   );
 
