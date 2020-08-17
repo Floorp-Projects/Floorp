909288b9a62c74a188c2964387b5db7d37f82452
Masayuki Nakano <masayuki@d-toybox.com>
2020-08-17 10:51:44 +0000

    Bug 1658472 - part 5: Split `AutoInclusiveAncestorBlockElementsJoiner::Run()` to computation part and touching the DOM tree part r=m_kato
    Depends on D86884
    
    Differential Revision: https://phabricator.services.mozilla.com/D86885

--- editor/libeditor/HTMLEditor.h
+++ editor/libeditor/HTMLEditor.h
@@ -2948,21 +2948,21 @@ class HTMLEditor final : public TextEditor,
                 aInclusiveDescendantOfRightBlockElement) {}
 
       /**
-       * Tries to join two block elements.  The right element is always joined
-       * to the left element.  If the elements are the same type and not nested
-       * within each other, JoinEditableNodesWithTransaction() is called
-       * (example, joining two list items together into one).  If the elements
-       * are not the same type, or one is a descendant of the other, we instead
-       * destroy the right block placing its children into leftblock.
-       *
-       * @return          Sets canceled to true if the operation should do
-       *                  nothing anymore even if this doesn't join the blocks.
-       *                  Sets handled to true if this actually handles the
-       *                  request.  Note that this may set it to true even if
-       *                  this does not join the block.  E.g., if the blocks
-       *                  shouldn't be joined or it's impossible to join them
-       *                  but it's not unexpected case, this return true with
-       *                  this.
+       * Prepare for joining inclusive ancestor block elements.  When this
+       * returns error or "canceled" or "handled" state, don't call Run().
+       */
+      EditActionResult Prepare();
+
+      /**
+       * Join inclusive ancestor block elements which are found by preceding
+       * Preare() call.
+       * The right element is always joined to the left element.
+       * If the elements are the same type and not nested within each other,
+       * JoinEditableNodesWithTransaction() is called (example, joining two
+       * list items together into one).
+       * If the elements are not the same type, or one is a descendant of the
+       * other, we instead destroy the right block placing its children into
+       * left block.
        */
       [[nodiscard]] MOZ_CAN_RUN_SCRIPT EditActionResult
       Run(HTMLEditor& aHTMLEditor);
