e523351f8cdbc334db0efa070011c8d4d6f09f04
Boris Chiou <boris.chiou@gmail.com>
2020-08-17 06:26:19 +0000

    Bug 1639963 - Calculate inline size and block size based on aspect-ratio. r=emilio
    We calculate the size of ratio-dependent axis by aspect-ratio while
    initializing its ReflowInput, for most of the basic block cases.
    
    This patch doesn't include "Automatic content-based minimum sizes",
    which will be handled later. Besides, replaced elements will be handled
    later as well.
    
    We don't pass abspos-004.tentative.html because there is still a
    spec issue: https://github.com/w3c/csswg-drafts/issues/5151.
    For other tests we didn't pass (e.g. block-aspect-ratio-009.tentative.html,
    or replaced-element-00x.tentative.html), we fix them later.
    
    Besides, in this patch, we don't handle the case if the size of
    ratio-determining axis uses intrinsic size keywords (which needs to be
    calculated by its content size). We will fix this in the following bug
    (Bug 1646100).
    
    Differential Revision: https://phabricator.services.mozilla.com/D78964

--- layout/generic/AspectRatio.h
+++ layout/generic/AspectRatio.h
@@ -16,6 +16,8 @@
 
 namespace mozilla {
 
+class WritingMode;
+
 struct AspectRatio {
   AspectRatio() : mRatio(0.0f) {}
   explicit AspectRatio(float aRatio) : mRatio(std::max(aRatio, 0.0f)) {}
@@ -50,6 +52,9 @@ struct AspectRatio {
     return AspectRatio(
         std::max(std::numeric_limits<float>::epsilon(), 1.0f / mRatio));
   }
+
+  [[nodiscard]] inline AspectRatio ConvertToWritingMode(
+      const WritingMode& aWM) const;
 
   bool operator==(const AspectRatio& aOther) const {
     return mRatio == aOther.mRatio;
