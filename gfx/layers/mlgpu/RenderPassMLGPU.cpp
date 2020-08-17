41764e29097efc8d6a85a0645575b87defbfb85d
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-08-17 16:04:50 +0000

    Bug 1659015 - P2. Add YUV identity support (rgb) to Advanced Layers. r=bas
    It's destined to go, but for now it's required.
    
    Depends on D87167
    
    Differential Revision: https://phabricator.services.mozilla.com/D87168

--- gfx/layers/mlgpu/RenderPassMLGPU.cpp
+++ gfx/layers/mlgpu/RenderPassMLGPU.cpp
@@ -738,10 +738,17 @@ void VideoRenderPass::SetupPipeline() {
 
   switch (mHost->GetReadFormat()) {
     case SurfaceFormat::YUV: {
-      if (mGeometry == GeometryMode::UnitQuad)
-        mDevice->SetPixelShader(PixelShaderID::TexturedQuadIMC4);
-      else
-        mDevice->SetPixelShader(PixelShaderID::TexturedVertexIMC4);
+      if (colorSpace == YUVColorSpace::Identity) {
+        if (mGeometry == GeometryMode::UnitQuad)
+          mDevice->SetPixelShader(PixelShaderID::TexturedQuadIdentityIMC4);
+        else
+          mDevice->SetPixelShader(PixelShaderID::TexturedVertexIdentityIMC4);
+      } else {
+        if (mGeometry == GeometryMode::UnitQuad)
+          mDevice->SetPixelShader(PixelShaderID::TexturedQuadIMC4);
+        else
+          mDevice->SetPixelShader(PixelShaderID::TexturedVertexIMC4);
+      }
       mDevice->SetPSTexturesYUV(0, mTexture);
       break;
     }
