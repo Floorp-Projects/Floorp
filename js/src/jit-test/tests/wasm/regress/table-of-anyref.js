d2a03707b784ceac9f642762cb8ff6ab0b9cca74
Ryan Hunt <rhunt@eqrion.net>
2020-08-17 23:59:40 +0000

    Bug 1561521 - Tests: Automatically replace all uses of anyref with externref. r=lth
    When we removed subtyping, we kept the anyref text syntax as an alias of
    externref. This commit completes the migration over to externref. A future
    commit will switch uses of externref which should be anyref back.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85053

--- js/src/jit-test/tests/wasm/regress/table-of-anyref.js
+++ js/src/jit-test/tests/wasm/regress/table-of-anyref.js
@@ -4,8 +4,8 @@
 gczeal(4, 8);
 let ins = wasmEvalText(
     `(module
-       (table (export "t") 10 anyref)
-       (func (export "set_anyref") (param i32) (param anyref)
+       (table (export "t") 10 externref)
+       (func (export "set_externref") (param i32) (param externref)
          (table.set (get_local 0) (get_local 1))))`);
-ins.exports.set_anyref(3, {});
-ins.exports.set_anyref(3, null);
+ins.exports.set_externref(3, {});
+ins.exports.set_externref(3, null);
