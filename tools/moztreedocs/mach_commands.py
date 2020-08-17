3be98c5ea3f72b8354ae0fa685ccf2d3d84b8652
Bogdan Tara <btara@mozilla.com>
2020-08-17 17:32:40 +0300

    Backed out changeset 6124d9a5422b (bug 1655107) for pip related failures CLOSED TREE

--- tools/moztreedocs/mach_commands.py
+++ tools/moztreedocs/mach_commands.py
@@ -50,7 +50,7 @@ class Documentation(MachCommandBase):
         self._project = None
         self._version = None
 
-    @Command('doc', category='devenv', virtualenv_name="docs",
+    @Command('doc', category='devenv',
              description='Generate and serve documentation from the tree.')
     @CommandArgument('path', default=None, metavar='DIRECTORY', nargs='?',
                      help='Path to documentation to build and display.')
@@ -80,8 +80,9 @@ class Documentation(MachCommandBase):
         if self.check_jsdoc():
             return die(JSDOC_NOT_FOUND)
 
-        self.activate_virtualenv()
-        self.virtualenv_manager.install_pip_requirements(os.path.join(here, 'requirements.txt'))
+        self.activate_pipenv(
+            os.path.dirname(self.virtualenv_manager.virtualenv_root),
+            pipfile=os.path.join(here, 'Pipfile'))
 
         import webbrowser
         from livereload import Server
