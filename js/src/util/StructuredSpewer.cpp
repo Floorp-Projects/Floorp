557f2f28e2cb0ebb608723be21f4f024845f5385
caroline <ccullen@mozilla.com>
2020-08-17 21:23:02 +0000

    Bug 1659007 - Properly begin JSONPrinter list for the StructuredSpewer. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D87343

--- js/src/util/StructuredSpewer.cpp
+++ js/src/util/StructuredSpewer.cpp
@@ -72,8 +72,8 @@ void StructuredSpewer::tryToInitializeOutput(const char* path) {
   }
 
   // These logs are structured as a JSON array.
-  output_.put("[");
   json_.emplace(output_);
+  json_->beginList();
 }
 
 // Treat pattern like a glob, and return true if pattern exists
