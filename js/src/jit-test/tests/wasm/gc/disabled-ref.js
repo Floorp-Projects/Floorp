3b63230f7fb642b52af4d8cf361ca939e15077ef
Ryan Hunt <rhunt@eqrion.net>
2020-08-18 16:54:16 +0000

    Bug 1561521 - Tests: Clean up disabled tests for gc/ref-types. r=lth
    This commit finishes the split of wasm/gc and wasm/ref-types tests by splitting
    the disabled-ref.js test into a GC test and a ref-type test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D85061

--- js/src/jit-test/tests/wasm/gc/disabled-ref.js
+++
@@ -1,5 +0,0 @@
-// |jit-test| skip-if: wasmGcEnabled()
-
-wasmCompilationShouldFail(
-    wasmTextToBinary(`(module (func (param (ref 0)) (unreachable)))`),
-    /\(ref T\) types not enabled|bad type/);
