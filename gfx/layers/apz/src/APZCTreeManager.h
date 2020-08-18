6a80e2975c81d28b6ce729b4311d98a60dc7fa63
Botond Ballo <botond@mozilla.com>
2020-08-18 21:14:32 +0000

    Bug 1633322 - Turn APZEventResult::mHandledByRootApzc into a Maybe. r=kats,geckoview-reviewers,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D79930

--- gfx/layers/apz/src/APZCTreeManager.h
+++ gfx/layers/apz/src/APZCTreeManager.h
@@ -557,7 +557,7 @@ class APZCTreeManager : public IAPZCTreeManager, public APZInputBridge {
     // Make it move-only.
     HitTestResult(HitTestResult&&) = default;
     HitTestResult& operator=(HitTestResult&&) = default;
-    bool TargetIsConfirmedRoot() const;
+    Maybe<bool> HandledByRoot() const;
   };
 
   /* Some helper functions to find an APZC given some identifying input. These
