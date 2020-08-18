3ed185f5670c491ef8d75fed9d0bf314e766e9b8
Christian Holler <choller@mozilla.com>
2020-08-18 13:32:15 +0000

    Bug 1659392 - Only enable rust fuzzing code with libFuzzer. r=truber
    Differential Revision: https://phabricator.services.mozilla.com/D87228

--- toolkit/library/gtest/rust/lib.rs
+++ toolkit/library/gtest/rust/lib.rs
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 extern crate bench_collections_gtest;
-#[cfg(feature = "fuzzing_interfaces")]
+#[cfg(feature = "libfuzzer")]
 extern crate gecko_fuzz_targets;
 extern crate gkrust_shared;
 extern crate mp4parse_gtest;
