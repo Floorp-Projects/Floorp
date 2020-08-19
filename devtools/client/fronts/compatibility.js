2cbe4613cd55b7e6304a65cf8d2692f4475a6741
Julian Descottes <jdescottes@mozilla.com>
2020-08-19 22:35:46 +0000

    Bug 1659866 - Set a default value for target-browsers from the client r=daisuke
    Depends on D87611
    
    Differential Revision: https://phabricator.services.mozilla.com/D87621

--- devtools/client/fronts/compatibility.js
+++ devtools/client/fronts/compatibility.js
@@ -10,9 +10,26 @@ const {
 } = require("devtools/shared/protocol");
 const { compatibilitySpec } = require("devtools/shared/specs/compatibility");
 
+const PREF_TARGET_BROWSERS = "devtools.inspector.compatibility.target-browsers";
+
 class CompatibilityFront extends FrontClassWithSpec(compatibilitySpec) {
   constructor(client, targetFront, parentFront) {
     super(client, targetFront, parentFront);
+    this.client = client;
+  }
+
+  async initialize() {
+    // Backward-compatibility: can be removed when FF81 is on the release
+    // channel. See 1659866.
+    const preferenceFront = await this.client.mainRoot.getFront("preference");
+    try {
+      await preferenceFront.getCharPref(PREF_TARGET_BROWSERS);
+    } catch (e) {
+      // If `getCharPref` failed, the preference is not set on the server (most
+      // likely Fenix). Set a default value, otherwise compatibility actor
+      // methods might throw.
+      await preferenceFront.setCharPref(PREF_TARGET_BROWSERS, "");
+    }
   }
 
   // Update the object given a form representation off the wire.
