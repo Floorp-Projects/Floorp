2d8e08e6205609c6284b065482ecdd6a9d943194
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-08-19 12:46:52 +0000

    Bug 1659643 - Refactor browser_dbg-chrome-debugging.js. r=bomsy.
    Differential Revision: https://phabricator.services.mozilla.com/D87408

--- devtools/client/debugger/test/mochitest/browser_dbg-chrome-debugging.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-chrome-debugging.js
@@ -6,71 +6,60 @@
  * Tests that chrome debugging works.
  */
 
-let gClient, gThreadFront;
-let gNewChromeSource = promise.defer();
-
-let { DevToolsLoader } = ChromeUtils.import("resource://devtools/shared/Loader.jsm");
-let customLoader = new DevToolsLoader({
-  invisibleToDebugger: true,
-});
-let { DevToolsServer } = customLoader.require("devtools/server/devtools-server");
-let { DevToolsClient } = require("devtools/client/devtools-client");
-
 add_task(async function() {
-  gClient = initDevToolsClient();
+  const client = initDevToolsClient();
 
-  const [type] = await gClient.connect();
+  const [type] = await client.connect();
   is(type, "browser", "Root actor should identify itself as a browser.");
 
-  const descriptorFront = await gClient.mainRoot.getMainProcess();
+  const descriptorFront = await client.mainRoot.getMainProcess();
   const front = await descriptorFront.getTarget();
   await front.attach();
   const threadFront = await front.attachThread();
-  gThreadFront = threadFront;
+
   gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser, "about:mozilla");
 
   // listen for a new source and global
-  gThreadFront.on("newSource", onNewSource);
+  const onFooDotComNewSource = new Promise(resolve => {
+    function onNewSource(packet) {
+      if (packet.source.url == "http://foo.com/") {
+        threadFront.off("newSource", onNewSource);
+        resolve(packet);
+      }
+    }
+    threadFront.on("newSource", onNewSource);
+  });
 
   // Force the creation of a new privileged source
   const systemPrincipal = Cc["@mozilla.org/systemprincipal;1"].createInstance(
-      Ci.nsIPrincipal
-    );
+    Ci.nsIPrincipal
+  );
   const sandbox = Cu.Sandbox(systemPrincipal);
   Cu.evalInSandbox("function foo() {}", sandbox, null, "http://foo.com");
 
-  await gNewChromeSource.promise;
+  const packet = await onFooDotComNewSource;
+  ok(true, "Received the custom script source: " + packet.source.url);
 
-  await resumeAndCloseConnection();
+  await threadFront.resume();
+  await client.close();
 });
 
 function initDevToolsClient() {
+  const { DevToolsLoader } = ChromeUtils.import(
+    "resource://devtools/shared/Loader.jsm"
+  );
+  const customLoader = new DevToolsLoader({
+    invisibleToDebugger: true,
+  });
+  const { DevToolsServer } = customLoader.require(
+    "devtools/server/devtools-server"
+  );
+  const { DevToolsClient } = require("devtools/client/devtools-client");
+
   DevToolsServer.init();
   DevToolsServer.registerAllActors();
   DevToolsServer.allowChromeProcess = true;
 
-  let transport = DevToolsServer.connectPipe();
+  const transport = DevToolsServer.connectPipe();
   return new DevToolsClient(transport);
 }
-
-function onNewSource(packet) {
-  if (packet.source.url == "http://foo.com/") {
-    ok(true, "Received the custom script source: " + packet.source.url);
-    gThreadFront.off("newSource", onNewSource);
-    gNewChromeSource.resolve();
-  }
-}
-
-async function resumeAndCloseConnection() {
-  await gThreadFront.resume();
-  return gClient.close();
-}
-
-registerCleanupFunction(function() {
-  gClient = null;
-  gThreadFront = null;
-  gNewChromeSource = null;
-
-  customLoader = null;
-  DevToolsServer = null;
-});
