aa3d2538396a31bccc4defb2993f5007d7435213
Michael Cooper <mcooper@mozilla.com>
2020-08-19 10:31:51 +0000

    Bug 1659842 - Make normandy.liveTelemetry accessible from consolidated targeting r=andreio
    Differential Revision: https://phabricator.services.mozilla.com/D87521

--- toolkit/components/messaging-system/targeting/test/unit/test_targeting.js
+++ toolkit/components/messaging-system/targeting/test/unit/test_targeting.js
@@ -1,3 +1,6 @@
+const { ClientEnvironment } = ChromeUtils.import(
+  "resource://normandy/lib/ClientEnvironment.jsm"
+);
 const { TargetingContext } = ChromeUtils.import(
   "resource://messaging-system/targeting/Targeting.jsm"
 );
@@ -203,4 +206,14 @@ add_task(async function test_telemetry_event_error() {
 
   TelemetryTestUtils.assertEvents(expectedEvents);
   Services.telemetry.clearEvents();
+});
+
+// Make sure that when using the Normandy-style ClientEnvironment context,
+// `liveTelemetry` works. `liveTelemetry` is a particularly tricky object to
+// proxy, so it's useful to check specifically.
+add_task(async function test_live_telemetry() {
+  let ctx = { env: ClientEnvironment };
+  let targeting = new TargetingContext();
+  // This shouldn't throw.
+  await targeting.eval("env.liveTelemetry.main", ctx);
 });
