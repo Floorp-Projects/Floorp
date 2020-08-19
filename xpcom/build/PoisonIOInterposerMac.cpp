9eb27119bb7ac11d4934e7edbde080f44c3a98ca
Mike Hommey <mh+mozilla@glandium.org>
2020-08-19 04:22:15 +0000

    Bug 1658385 - Exclude mach_override code on aarch64 builds for now. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D87385

--- xpcom/build/PoisonIOInterposerMac.cpp
+++ xpcom/build/PoisonIOInterposerMac.cpp
@@ -5,7 +5,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "PoisonIOInterposer.h"
+// Disabled until bug 1658385 is fixed.
+#ifndef __aarch64__
 #include "mach_override.h"
+#endif
 
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/Assertions.h"
@@ -326,9 +329,11 @@ void InitPoisonIOInterposer() {
     if (!d->Function) {
       continue;
     }
+#ifndef __aarch64__
     DebugOnly<mach_error_t> t =
         mach_override_ptr(d->Function, d->Wrapper, &d->Buffer);
     MOZ_ASSERT(t == err_none);
+#endif
   }
 }
 
