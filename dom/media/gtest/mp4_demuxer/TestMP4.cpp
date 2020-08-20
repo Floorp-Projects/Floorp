1da1c7e14c0acfd5af2459e422337067fc0ab5d5
John Lin <jolin@mozilla.com>
2020-08-20 19:31:38 +0000

    Bug 1604331 - read source buffer only when necessary. r=jbauman
    Differential Revision: https://phabricator.services.mozilla.com/D59966

--- dom/media/gtest/mp4_demuxer/TestMP4.cpp
+++ dom/media/gtest/mp4_demuxer/TestMP4.cpp
@@ -44,10 +44,13 @@ static intptr_t vector_reader(uint8_t* buffer, uintptr_t size, void* userdata) {
   if (source->location > source->buffer.size()) {
     return -1;
   }
-  uintptr_t available = source->buffer.size() - source->location;
+  uintptr_t available =
+      source->buffer.data() ? source->buffer.size() - source->location : 0;
   uintptr_t length = std::min(available, size);
-  memcpy(buffer, source->buffer.data() + source->location, length);
-  source->location += length;
+  if (length) {
+    memcpy(buffer, source->buffer.data() + source->location, length);
+    source->location += length;
+  }
   return length;
 }
 
