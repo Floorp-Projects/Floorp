c24d9009fda1419495cd0147a8f556da2d6c1669
Jonathan Almeida <jalmeida@mozilla.com>
2020-08-20 19:34:49 -0400

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/4510: Telemetry should be disabled for Klar

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/AppConstants.kt
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/utils/AppConstants.kt
@@ -13,7 +13,7 @@ object AppConstants {
     private const val PRODUCT_FLAVOR_KLAR = "klar"
 
     val isKlarBuild: Boolean
-        get() = PRODUCT_FLAVOR_KLAR == BuildConfig.FLAVOR
+        get() = PRODUCT_FLAVOR_KLAR == BuildConfig.FLAVOR_product
 
     val isReleaseBuild: Boolean
         get() = BUILD_TYPE_RELEASE == BuildConfig.BUILD_TYPE
