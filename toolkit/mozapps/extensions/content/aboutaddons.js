a33a0603609cc3e2e851395bd3862c1e7053c3fc
Dan Mosedale <dmose@mozilla.org>
2020-08-20 21:04:57 +0000

    Bug 1643776 - Add Alpenglow theme r=dmose,rpl,flod
    Differential Revision: https://phabricator.services.mozilla.com/D86058

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -97,6 +97,10 @@ const BUILTIN_THEME_PREVIEWS = new Map([
     "firefox-compact-dark@mozilla.org",
     "chrome://mozapps/content/extensions/firefox-compact-dark.svg",
   ],
+  [
+    "firefox-alpenglow@mozilla.org",
+    "chrome://mozapps/content/extensions/firefox-alpenglow.svg",
+  ],
 ]);
 
 const PERMISSION_MASKS = {
