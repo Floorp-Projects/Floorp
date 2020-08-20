6d1e14246390514397b5df0813f6c63a46bf52db
David Teller <dteller@mozilla.com>
2020-08-20 13:32:11 +0000

    Bug 1652813 - Extract resident unique size in GetProcInfo;r=froydnj
    Somewhere along the way, we fix the tests that are too fragile with respect to race conditions between processes to make them pass chaos mode testing.
    
    Depends on D83623
    
    Differential Revision: https://phabricator.services.mozilla.com/D83971

--- widget/gtk/ProcInfo.cpp
+++ widget/gtk/ProcInfo.cpp
@@ -10,6 +10,7 @@
 #include "mozilla/ipc/GeckoChildProcessHost.h"
 #include "nsAutoRef.h"
 #include "nsLocalFile.h"
+#include "nsMemoryReporterManager.h"
 #include "nsNetCID.h"
 #include "nsWhitespaceTokenizer.h"
 
@@ -240,6 +241,13 @@ RefPtr<ProcInfoPromise> GetProcInfo(nsTArray<ProcInfoRequest>&& aRequests) {
             // the process has been just been killed. Regardless, skip process.
             continue;
           }
+          // Computing the resident unique size is somewhat tricky,
+          // so we use about:memory's implementation. This implementation
+          // reopens `/proc/[pid]`, so there is the risk of an additional
+          // race condition. In that case, the result is `0`.
+          info.residentUniqueSize =
+              nsMemoryReporterManager::ResidentUnique(request.pid);
+
           // Extra info
           info.pid = request.pid;
           info.childId = request.childId;
--- widget/tests/browser/browser_test_procinfo.js
+++ widget/tests/browser/browser_test_procinfo.js
@@ -13,6 +13,8 @@ const HAS_THREAD_NAMES =
   AppConstants.isPlatformAndVersionAtLeast("win", 10);
 const isFissionEnabled = Services.prefs.getBoolPref("fission.autostart");
 
+const SAMPLE_SIZE = 10;
+
 add_task(async function test_proc_info() {
   waitForExplicitFinish();
   await BrowserTestUtils.withNewTab(
@@ -20,7 +22,9 @@ add_task(async function test_proc_info() {
     async function(browser) {
       let cpuThreads = 0;
       let cpuUser = 0;
-      for (let z = 0; z < 10; z++) {
+
+      // We test `SAMPLE_SIZE` times to increase a tad the chance of encountering race conditions.
+      for (let z = 0; z < SAMPLE_SIZE; z++) {
         let parentProc = await ChromeUtils.requestProcInfo();
         cpuUser += parentProc.cpuUser;
 
@@ -42,6 +46,31 @@ add_task(async function test_proc_info() {
           );
         }
 
+        Assert.ok(
+          parentProc.residentUniqueSize > 0,
+          "Resident-unique-size was set"
+        );
+        Assert.ok(
+          parentProc.residentUniqueSize <= parentProc.residentSetSize,
+          `Resident-unique-size should be bounded by resident-set-size ${parentProc.residentUniqueSize} <= ${parentProc.residentSetSize}`
+        );
+
+        if (AppConstants.platform != "win") {
+          // The definition of `virtualMemorySize` seens to differ between Windows < 7 and >= 7.
+          // Also, under Windows 7+, `virtualMemorySize` is defined as the number of private bytes,
+          // which is entirely unrelated to virtual memory.
+          Assert.ok(
+            parentProc.residentUniqueSize <= parentProc.virtualMemorySize,
+            `Resident-unique-size should be bounded by virtual ${parentProc.residentUniqueSize} <= ${parentProc.virtualMemorySize}`
+          );
+        }
+
+        // While it's very unlikely that the parent will disappear while we're running
+        // tests, some children can easily vanish. So we go twice through the list of
+        // children. Once to test stuff that all process data respects the invariants
+        // that don't care whether we have a race condition and once to test that at
+        // least one well-known process that should not be able to vanish during
+        // the test respects all the invariants.
         for (var i = 0; i < parentProc.children.length; i++) {
           let childProc = parentProc.children[i];
           Assert.notEqual(
@@ -77,6 +106,42 @@ add_task(async function test_proc_info() {
           }
           cpuUser += childProc.cpuUser;
         }
+
+        // We only check other properties on the `privilegedabout` subprocess, which
+        // as of this writing is hosting the page we test, so should be active and
+        // available. If we ever move `about:processes` to another process type, we'll
+        // need to update this test.
+        var hasSocketProcess = false;
+        for (i = 0; i < parentProc.children.length; i++) {
+          let childProc = parentProc.children[i];
+          if (childProc.type != "privilegedabout") {
+            continue;
+          }
+          hasSocketProcess = true;
+          Assert.ok(
+            childProc.residentUniqueSize > 0,
+            "Resident-unique-size was set"
+          );
+          Assert.ok(
+            childProc.residentUniqueSize <= childProc.residentSetSize,
+            `Resident-unique-size should be bounded by resident-set-size ${childProc.residentUniqueSize} <= ${childProc.residentSetSize}`
+          );
+
+          if (AppConstants.platform != "win") {
+            // The definition of `virtualMemorySize` seens to differ between Windows < 7 and >= 7.
+            // Also, under Windows 7+, `virtualMemorySize` is defined as the number of private bytes,
+            // which is entirely unrelated to virtual memory.
+            Assert.ok(
+              childProc.residentUniqueSize <= childProc.virtualMemorySize,
+              `Resident-unique-size should be bounded by virtual memory size ${childProc.residentUniqueSize} <= ${childProc.virtualMemorySize}`
+            );
+          }
+
+          // Once we have found the socket process, bailout.
+          break;
+        }
+
+        Assert.ok(hasSocketProcess, "We have found the socket process");
       }
       // see https://bugzilla.mozilla.org/show_bug.cgi?id=1529023
       if (!MAC) {
