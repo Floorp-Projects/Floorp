d99484e5ff1ce367a6dca61f997dbac33564ced0
Duncan Dean <duncangleeddean@gmail.com>
2020-08-20 08:52:42 +0000

    Bug 1620162 - Upgrade eslint-plugin-jest and fix new lint errors. r=bomsy,Standard8
    Currently the `jest/no-standalone-expect` rule needs to be disabled for `jest-in-case` cases blocks as it is not compatible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87184

--- devtools/client/debugger/.eslintrc.js
+++ devtools/client/debugger/.eslintrc.js
@@ -96,6 +96,10 @@ module.exports = {
     "flowtype/define-flow-type": 1,
     "flowtype/use-flow-type": 1,
 
+    // Incompatible with jest-in-case cases. See related GitHub issue
+    // https://github.com/jest-community/eslint-plugin-jest/issues/534
+    "jest/no-standalone-expect": "off",
+
     // Disallow flow control that escapes from "finally".
     "no-unsafe-finally": "error",
 
