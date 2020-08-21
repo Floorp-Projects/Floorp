ef7a4ef624eee4f70e90fbe246db0ee26c4dd23a
Kershaw Chang <kershaw@mozilla.com>
2020-08-21 18:59:03 +0000

    Bug 1652655 - P1: Sort HTTPSSVC records, r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D85121

--- netwerk/dns/TRR.cpp
+++ netwerk/dns/TRR.cpp
@@ -1197,6 +1197,16 @@ nsresult TRR::DohDecode(nsCString& aHost) {
     LOG(("TRR: No entries were stored!\n"));
     return NS_ERROR_FAILURE;
   }
+
+  // https://tools.ietf.org/html/draft-ietf-dnsop-svcb-httpssvc-03#page-14
+  // If one or more SVCB records of ServiceForm SvcRecordType are returned for
+  // HOST, clients should select the highest-priority option with acceptable
+  // parameters.
+  if (mResult.is<TypeRecordHTTPSSVC>()) {
+    auto& results = mResult.as<TypeRecordHTTPSSVC>();
+    results.Sort();
+  }
+
   return NS_OK;
 }
 
