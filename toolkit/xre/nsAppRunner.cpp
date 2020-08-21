57d5c10e8e5eb146f2a6b1674b79d3b55ad49d77
Andrew Osmond <aosmond@mozilla.com>
2020-08-21 15:53:58 +0000

    Bug 1659965 - Initialize the profiler before CodeCoverageHandler. r=mstange
    CodeCoverageHandler relies upon CrossProcessMutex. On Linux this is
    implemented using shared memory. In bug 1658847, we used to the
    profiler's "thread sleep" mechanism to resolve the signal interrupts in
    posix_fallocate, which requires the profiler to be initialized. As such,
    CodeCoverageHandler now needs to be initialized after.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87747

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -4817,15 +4817,15 @@ int XREMain::XRE_main(int argc, char* argv[], const BootstrapConfig& aConfig) {
 
   mozilla::LogModule::Init(gArgc, gArgv);
 
-#ifdef MOZ_CODE_COVERAGE
-  CodeCoverageHandler::Init();
-#endif
-
   NS_SetCurrentThreadName("MainThread");
 
   AUTO_BASE_PROFILER_LABEL("XREMain::XRE_main (around Gecko Profiler)", OTHER);
   AUTO_PROFILER_INIT;
   AUTO_PROFILER_LABEL("XREMain::XRE_main", OTHER);
+
+#ifdef MOZ_CODE_COVERAGE
+  CodeCoverageHandler::Init();
+#endif
 
   nsresult rv = NS_OK;
 
