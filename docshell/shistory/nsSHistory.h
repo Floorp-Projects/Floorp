13ebfdf4410660d61808f1f0193d3ddbf68da752
Olli Pettay <Olli.Pettay@helsinki.fi>
2020-08-21 20:59:52 +0000

    Bug 1660526 - Tweak AddToRootSessionHistory and AddChildSHEntryHelper to make them easier to follow, r=annyG
    Differential Revision: https://phabricator.services.mozilla.com/D87895

--- docshell/shistory/nsSHistory.h
+++ docshell/shistory/nsSHistory.h
@@ -127,10 +127,6 @@ class nsSHistory : public mozilla::LinkedListElement<nsSHistory>,
                                      WalkHistoryEntriesFunc aCallback,
                                      void* aData);
 
-  nsresult AddChildSHEntryHelper(nsISHEntry* aCloneRef, nsISHEntry* aNewEntry,
-                                 mozilla::dom::BrowsingContext* aBC,
-                                 bool aCloneChildren, nsISHEntry** aNextEntry);
-
   nsTArray<nsCOMPtr<nsISHEntry>>& Entries() { return mEntries; }
 
   void RemoveEntries(nsTArray<nsID>& aIDs, int32_t aStartIndex,
