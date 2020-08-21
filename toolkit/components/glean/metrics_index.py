638228539bec1ea72281347bd56e6ab7df512960
Chris H-C <chutten@mozilla.com>
2020-08-21 16:30:45 +0000

    Bug 1654339 - Add Glean metrics autodocs to moztreedocs r=janerik,ahal
    Also define a scheme for storing the index of Glean definitions files in a file
    separate from the build system for consumption by
    * mach build
    * mach doc
    * (future) mozilla/probe-scraper
    
    Differential Revision: https://phabricator.services.mozilla.com/D87600

---
+++ toolkit/components/glean/metrics_index.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# The list of all Glean metrics.yaml files, relative to the top src dir.
+# New additions should be added to the bottom of the list.
+METRICS = [
+    'toolkit/components/glean/metrics.yaml',
+]
+
+# The list of all Glean pings.yaml files, relative to the top src dir.
+# New additions should be added to the bottom of the list.
+PINGS = [
+    'toolkit/components/glean/pings.yaml',
+]
