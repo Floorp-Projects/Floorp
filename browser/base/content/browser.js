6c03625bd4734193a9df8ea4fc4f6b874a787b9c
Jared Wein <jwein@mozilla.com>
2020-08-23 22:24:05 +0000

    Bug 1656494 - Show the default browser notification on the first New Tab Page of a session. r=Gijs,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D87490

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -158,6 +158,11 @@ XPCOMUtils.defineLazyScriptGetter(
 );
 XPCOMUtils.defineLazyScriptGetter(
   this,
+  "DefaultBrowserNotificationOnNewTabPage",
+  "chrome://browser/content/browser-defaultBrowserNotificationOnNewTabPage.js"
+);
+XPCOMUtils.defineLazyScriptGetter(
+  this,
   ["PointerLock", "FullScreen"],
   "chrome://browser/content/browser-fullScreenAndPointerLock.js"
 );
