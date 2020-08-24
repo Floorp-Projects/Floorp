00950e9a6bdb5aa8e726a0d543d9125d106903a2
Gerald Squelart <gsquelart@mozilla.com>
2020-08-24 08:39:22 +0000

    Bug 1659382 - Remove dependency on BaseProfiler.h from BaseProfilingCategory.h - r=gregtatum
    It is just not needed here.
    
    Differential Revision: https://phabricator.services.mozilla.com/D87239

--- mozglue/baseprofiler/public/BaseProfilingCategory.h
+++ mozglue/baseprofiler/public/BaseProfilingCategory.h
@@ -7,8 +7,6 @@
 #ifndef BaseProfilingCategory_h
 #define BaseProfilingCategory_h
 
-#include "BaseProfiler.h"
-
 #ifndef MOZ_GECKO_PROFILER
 #  error Do not #include this header when MOZ_GECKO_PROFILER is not #defined.
 #endif
