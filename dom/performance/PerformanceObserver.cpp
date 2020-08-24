b28d74eccaae5975c58e501753dde111377fac75
Sean Feng <sefeng@mozilla.com>
2020-08-24 16:03:31 +0000

    Bug 1518999 - Implement PerformancePaintTiming for FirstContentfulPaint r=smaug,mstange
    Spec: https://w3c.github.io/paint-timing/#sec-PerformancePaintTiming
    We only support FirstContentfulPaint at the moment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66463

--- dom/performance/PerformanceObserver.cpp
+++ dom/performance/PerformanceObserver.cpp
@@ -132,11 +132,8 @@ void PerformanceObserver::QueueEntry(PerformanceEntry* aEntry) {
  * Keep this list in alphabetical order.
  * https://w3c.github.io/performance-timeline/#supportedentrytypes-attribute
  */
-static const char16_t* const sValidTypeNames[4] = {
-    u"mark",
-    u"measure",
-    u"navigation",
-    u"resource",
+static const char16_t* const sValidTypeNames[5] = {
+    u"mark", u"measure", u"navigation", u"paint", u"resource",
 };
 
 void PerformanceObserver::ReportUnsupportedTypesErrorToConsole(
