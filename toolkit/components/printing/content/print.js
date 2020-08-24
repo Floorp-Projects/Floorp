756d1eff7d38ff16b764aef9f52e5fce11d8d6e2
Mark Striemer <mstriemer@mozilla.com>
2020-08-24 19:17:08 +0000

    Bug 1660026 - Move print preview updating code into print.js. r=sfoster,emalysz a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D87366

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -286,19 +286,53 @@ var PrintEventHandler = {
     }
   },
 
+  /**
+   * Create a print preview for the provided source browsingContext, or refresh
+   * the preview with new settings when omitted.
+   *
+   * @param browsingContext {BrowsingContext} [optional]
+   *        The source BrowsingContext (the one associated with a tab or
+   *        subdocument) that should be previewed.
+   *
+   * @return {Promise} Resolves when the preview has been updated.
+   */
   async _updatePrintPreview(browsingContext) {
-    let totalPages = await PrintUtils.updatePrintPreview(
-      browsingContext || this.previewBrowser.browsingContext,
-      this.previewBrowser,
-      this.settings
-    );
+    let { previewBrowser, settings } = this;
+    let stack = previewBrowser.parentElement;
+    stack.setAttribute("rendering", true);
+
+    let totalPages = await new Promise(resolve => {
+      previewBrowser.messageManager.addMessageListener(
+        "Printing:Preview:UpdatePageCount",
+        function done(message) {
+          previewBrowser.messageManager.removeMessageListener(
+            "Printing:Preview:UpdatePageCount",
+            done
+          );
+
+          resolve(message.data.numPages);
+        }
+      );
+
+      previewBrowser.messageManager.sendAsyncMessage("Printing:Preview:Enter", {
+        changingBrowsers: false,
+        lastUsedPrinterName: settings.printerName,
+        simplifiedMode: false,
+        browsingContextId:
+          browsingContext?.id || previewBrowser.browsingContext.id,
+        outputFormat: settings.outputFormat,
+        startPageRange: settings.startPageRange,
+        endPageRange: settings.endPageRange,
+        printRange: settings.printRange,
+      });
+    });
+
+    stack.removeAttribute("rendering");
 
     let numPages = totalPages;
     // Adjust number of pages if the user specifies the pages they want printed
-    if (
-      this.settings.printRange == Ci.nsIPrintSettings.kRangeSpecifiedPageRange
-    ) {
-      numPages = this.settings.endPageRange - this.settings.startPageRange + 1;
+    if (settings.printRange == Ci.nsIPrintSettings.kRangeSpecifiedPageRange) {
+      numPages = settings.endPageRange - settings.startPageRange + 1;
     }
     document.dispatchEvent(
       new CustomEvent("page-count", { detail: { numPages, totalPages } })
