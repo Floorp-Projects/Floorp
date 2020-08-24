a13d636bfd61c144753da1fd3b86df426024a617
Jon Coppeard <jcoppeard@mozilla.com>
2020-08-24 19:33:53 +0000

    Bug 1656248 - Add testcase that scripts loaded from the bytecode cache are correctly associated with a base URL r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D86803

---
+++ dom/tests/mochitest/script/subdir/bug1656248_script.js
@@ -0,0 +1,18 @@
+// Import a module which should be resolved relative to this script's URL.
+import("./bug1656248_import.js")
+  .then(ns => window.parent.checkResult(ns.default))
+  .catch(e => window.parent.checkResult(`error: ${e}`));
+
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
+// To trigger the bytecode cache, this script needs to be at least 1KB.
