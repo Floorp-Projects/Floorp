49a80e092209916f501c75aa023f344346d4c682
emcminn <emcminn@mozilla.com>
2020-08-25 15:16:32 +0000

    Bug 1653899 - New long zap .svg to allow for longer spans; plus dynamic measuring r=pdahiya
    Differential Revision: https://phabricator.services.mozilla.com/D85162

--- browser/components/newtab/content-src/aboutwelcome/components/Zap.jsx
+++ browser/components/newtab/content-src/aboutwelcome/components/Zap.jsx
@@ -2,11 +2,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Localized } from "./MSLocalized";
 const MS_STRING_PROP = "string_id";
+const ZAP_SIZE_THRESHOLD = 160;
+
+function calculateZapLength() {
+  let span = document.querySelector(".zap");
+  if (!span) {
+    return;
+  }
+  let rect = span.getBoundingClientRect();
+  if (rect && rect.width > ZAP_SIZE_THRESHOLD) {
+    span.classList.add("long");
+  } else {
+    span.classList.add("short");
+  }
+}
 
 export const Zap = props => {
+  useEffect(() => {
+    requestAnimationFrame(() => calculateZapLength());
+  });
+
   if (!props.text) {
     return null;
   }
@@ -16,7 +34,7 @@ export const Zap = props => {
       return (
         <Localized text={props.text}>
           <h1 className="welcomeZap">
-            <span data-l10n-name="zap" />
+            <span data-l10n-name="zap" className="zap" />
           </h1>
         </Localized>
       );
@@ -27,7 +45,7 @@ export const Zap = props => {
       return (
         <h1 className="welcomeZap">
           {titleArray.join(" ").concat(" ")}
-          <span>{lastWord}</span>
+          <span className="zap">{lastWord}</span>
         </h1>
       );
     }
