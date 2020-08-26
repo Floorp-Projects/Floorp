1cd438384f24280f3925e54a77af0e514976f7e9
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2020-08-26 11:45:35 +0000

    Bug 1658690 - Replace :-moz-native-anonymous-no-specificity with :where(:-moz-native-anonymous). r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D88255

--- servo/components/style/servo/selector_parser.rs
+++ servo/components/style/servo/selector_parser.rs
@@ -311,11 +311,6 @@ impl ::selectors::parser::NonTSPseudoClass for NonTSPseudoClass {
         )
     }
 
-    #[inline]
-    fn has_zero_specificity(&self) -> bool {
-        false
-    }
-
     fn visit<V>(&self, _: &mut V) -> bool
     where
         V: SelectorVisitor<Impl = Self::Impl>,
