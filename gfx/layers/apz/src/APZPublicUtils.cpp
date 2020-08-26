d046d2b406f0e35d655ee7d5411da291c88d8d0e
Kartikaya Gupta <kgupta@mozilla.com>
2020-08-26 20:31:20 +0000

    Bug 1660933 - Respect wheel migration value. r=botond
    This introduces a linear interpolation when computing the wheel animation
    durations, based on the migration percentage of the user. Initially the
    migration percentage defaults to 100 (fully migrated) for new users and for
    existing users who have customized these prefs. The migration percentage
    defaults to zero for existing users without customizations, so they will
    still get the old default values of min=200,max=400. Over time we can adjust
    the migration pref clamping to move that percentage up and migrate those
    users to get the new values.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88163

--- gfx/layers/apz/src/APZPublicUtils.cpp
+++ gfx/layers/apz/src/APZPublicUtils.cpp
@@ -7,6 +7,7 @@
 #include "mozilla/layers/APZPublicUtils.h"
 
 #include "AsyncPanZoomController.h"
+#include "mozilla/StaticPrefs_general.h"
 
 namespace mozilla {
 namespace layers {
@@ -22,6 +23,33 @@ namespace apz {
     const FrameMetrics& aFrameMetrics, const ParentLayerPoint& aVelocity) {
   return AsyncPanZoomController::CalculatePendingDisplayPort(aFrameMetrics,
                                                              aVelocity);
+}
+
+/*static*/ std::pair<int32_t, int32_t> GetMouseWheelAnimationDurations() {
+  // Special code for migrating users from old values to new values over
+  // a period of time. The old values are defaults prior to bug 1660933, which
+  // we hard-code here. The user's migration percentage is stored in the
+  // migration pref. If the migration percentage is zero, the user gets the old
+  // values, and at a 100 percentage the user gets the new values. Linear
+  // interpolation in between. We can control the speed of migration by
+  // increasing the percentage value over time (e.g. by increasing the min
+  // bound on the clamped migration value). Once it reaches 100 the migration
+  // code can be removed.
+
+  int32_t minMS = StaticPrefs::general_smoothScroll_mouseWheel_durationMinMS();
+  int32_t maxMS = StaticPrefs::general_smoothScroll_mouseWheel_durationMaxMS();
+
+  const int32_t oldMin = 200;
+  const int32_t oldMax = 400;
+
+  int32_t migration =
+      StaticPrefs::general_smoothScroll_mouseWheel_migrationPercent();
+  migration = clamped(migration, 0, 100);
+
+  minMS = ((oldMin * (100 - migration)) + (minMS * migration)) / 100;
+  maxMS = ((oldMax * (100 - migration)) + (maxMS * migration)) / 100;
+
+  return std::make_pair(minMS, maxMS);
 }
 
 }  // namespace apz
