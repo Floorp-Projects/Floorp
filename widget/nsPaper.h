f225ac911bed6c863511f8ba44cb77c247c51fdd
Jonathan Kew <jkew@mozilla.com>
2020-08-26 11:10:32 +0000

    Bug 1659781 - Create a list of common standard paper sizes for Save to PDF output, and expose them via nsIPrinterList.fallbackPaperList. r=jwatt, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D88027

--- widget/nsPaper.h
+++ widget/nsPaper.h
@@ -58,11 +58,15 @@ class nsPaper final : public nsIPaper {
   NS_DECL_NSIPAPER
 
   nsPaper() = delete;
+  explicit nsPaper(const mozilla::PaperInfo&);
   nsPaper(nsPrinterBase&, const mozilla::PaperInfo&);
 
  private:
   ~nsPaper();
+
+  // null if not associated with a printer (for "Save-to-PDF" paper sizes)
   RefPtr<nsPrinterBase> mPrinter;
+
   RefPtr<Promise> mMarginPromise;
   const mozilla::PaperInfo mInfo;
 };
