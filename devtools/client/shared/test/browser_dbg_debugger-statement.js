e370fbbf3b169f1180abea7481d6b11a328a0ecd
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-08-26 15:39:48 +0300

    Backed out changeset 9d95edbd8386 (bug 1660892) for causing devtool timeouts.
    CLOSED TREE

--- devtools/client/shared/test/browser_dbg_debugger-statement.js
+++ devtools/client/shared/test/browser_dbg_debugger-statement.js
@@ -7,80 +7,28 @@
  * Tests the behavior of the debugger statement.
  */
 
-// Use distinct origins in order to use distinct processes when fission is enabled
-const TAB_URL = URL_ROOT_COM + "doc_inline-debugger-statement.html";
-const IFRAME_URL = URL_ROOT_ORG + "doc_inline-debugger-statement.html";
+// Import helpers for the workers
+/* import-globals-from helper_workers.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/shared/test/helper_workers.js",
+  this
+);
+
+const TAB_URL = TEST_URI_ROOT + "doc_inline-debugger-statement.html";
 
 add_task(async () => {
   const tab = await addTab(TAB_URL);
-  const tabBrowsingContext = tab.linkedBrowser.browsingContext;
-
-  const iframeBrowsingContext = await SpecialPowers.spawn(
-    tabBrowsingContext,
-    [IFRAME_URL],
-    async function(url) {
-      const iframe = content.document.createElement("iframe");
-      const onLoad = new Promise(r =>
-        iframe.addEventListener("load", r, { once: true })
-      );
-      iframe.src = url;
-      content.document.body.appendChild(iframe);
-      await onLoad;
-      return iframe.browsingContext;
-    }
-  );
-
   const target = await TargetFactory.forTab(tab);
   await target.attach();
   const { client } = target;
 
-  info("## Test debugger statement against the top level tab document");
-  // This function, by calling the debugger statement function, will bump the increment
-  const threadFront = await testEarlyDebuggerStatement(
-    client,
-    tabBrowsingContext,
-    target
-  );
-  await testDebuggerStatement(client, tabBrowsingContext, threadFront, 1);
-
-  info("## Test debugger statement againt a distinct origin iframe");
-  if (isFissionEnabled()) {
-    // We have to use the watcher in order to create the frame target
-    // and also have to attach to it in order to later be able to
-    // create the thread front
-    const watcher = await target.getWatcher();
-    await watcher.watchTargets("frame");
-    const iframeTarget = await target.getBrowsingContextTarget(
-      iframeBrowsingContext.id
-    );
-    await iframeTarget.attach();
-
-    // This function, by calling the debugger statement function, will bump the increment
-    const iframeThreadFront = await testEarlyDebuggerStatement(
-      client,
-      iframeBrowsingContext,
-      iframeTarget
-    );
-    await testDebuggerStatement(
-      client,
-      iframeBrowsingContext,
-      iframeThreadFront,
-      1
-    );
-  } else {
-    // But in this case, the increment will be 0 as the previous call to `testEarlyDebuggerStatement`
-    // bumped the tab's document increment and not the iframe's one.
-    await testDebuggerStatement(client, iframeBrowsingContext, threadFront, 0);
-  }
+  const threadFront = await testEarlyDebuggerStatement(client, tab, target);
+  await testDebuggerStatement(client, tab, threadFront);
 
   await target.destroy();
 });
 
-async function testEarlyDebuggerStatement(
-  client,
-  browsingContext,
-  targetFront
-) {
+async function testEarlyDebuggerStatement(client, tab, targetFront) {
   const onPaused = function(packet) {
     ok(false, "Pause shouldn't be called before we've attached!");
   };
@@ -91,17 +39,7 @@ async function testEarlyDebuggerStatement(
 
   // This should continue without nesting an event loop and calling
   // the onPaused hook, because we haven't attached yet.
-  const increment = await SpecialPowers.spawn(
-    browsingContext,
-    [],
-    async function() {
-      content.wrappedJSObject.runDebuggerStatement();
-      // Pile up another setTimeout in order to guarantee that the other one ran
-      await new Promise(r => content.setTimeout(r));
-      return content.wrappedJSObject.increment;
-    }
-  );
-  is(increment, 1, "As the thread wasn't paused, setTimeout worked");
+  callInTab(tab, "runDebuggerStatement");
 
   client.off("paused", onPaused);
 
@@ -113,53 +51,17 @@ async function testEarlyDebuggerStatement(
   return threadFront;
 }
 
-async function testDebuggerStatement(
-  client,
-  browsingContext,
-  threadFront,
-  incrementOriginalValue
-) {
-  const onPaused = threadFront.once("paused");
+async function testDebuggerStatement(client, tab, threadFront) {
+  const onPaused = new Promise(resolve => {
+    threadFront.on("paused", async packet => {
+      await threadFront.resume();
+      ok(true, "The pause handler was triggered on a debugger statement.");
+      resolve();
+    });
+  });
 
   // Reach around the debugging protocol and execute the debugger statement.
-  // Not that this will be paused and spawn will only resolve once
-  // the thread will be resumed
-  const onResumed = SpecialPowers.spawn(browsingContext, [], function() {
-    content.wrappedJSObject.runDebuggerStatement();
-  });
+  callInTab(tab, "runDebuggerStatement");
 
-  info("Waiting for paused event");
-  await onPaused;
-  ok(true, "The pause handler was triggered on a debugger statement.");
-
-  // Pile up another setTimeout in order to guarantee that the other did not run
-  /* eslint-disable-next-line mozilla/no-arbitrary-setTimeout */
-  await new Promise(r => setTimeout(r, 1000));
-
-  let increment = await SpecialPowers.spawn(
-    browsingContext,
-    [],
-    async function() {
-      return content.wrappedJSObject.increment;
-    }
-  );
-  is(
-    increment,
-    incrementOriginalValue,
-    "setTimeout are frozen while the thread is paused"
-  );
-
-  await threadFront.resume();
-  await onResumed;
-
-  increment = await SpecialPowers.spawn(browsingContext, [], async function() {
-    // Pile up another setTimeout in order to guarantee that the other did run
-    await new Promise(r => content.setTimeout(r));
-    return content.wrappedJSObject.increment;
-  });
-  is(
-    increment,
-    incrementOriginalValue + 1,
-    "setTimeout are resumed after the thread is resumed"
-  );
+  return onPaused;
 }
