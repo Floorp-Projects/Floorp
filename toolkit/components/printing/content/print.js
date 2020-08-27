785eca6e67559c1f35a46719df3d42e4ea8b9ce7
Emilio Cobos Álvarez <emilio@crisal.io>
2020-08-27 10:05:24 +0000

    Bug 1660857 - Disable color switch for print-to-pdf and on mac. r=emalysz, a=RyanVM
    See the comments as for why.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88301

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -604,6 +604,19 @@ const PrintSettingsViewProxy = {
           target.outputFormat == Ci.nsIPrintSettings.kOutputFormatPDF ||
           this.knownSaveToFilePrinters.has(target.printerName)
         );
+      // We allow switching colors except:
+      //
+      //  * For PDF printing, where it'd require rasterization and thus bad
+      //    quality.
+      //
+      //  * For Mac, where there's no API to print in monochrome.
+      //
+      case "supportsColorSwitch":
+        return (
+          target.printerName != PrintUtils.SAVE_TO_PDF_PRINTER &&
+          AppConstants.platform !== "macosx" &&
+          this.get(target, "supportsColor")
+        );
     }
     return target[name];
   },
@@ -772,13 +785,16 @@ class ColorModePicker extends PrintSettingSelect {
   update(settings) {
     let value = settings[this.settingName];
     let supportsColor = settings.supportsColor;
-    let forceChange;
-    if (value && !supportsColor) {
-      forceChange = true;
-      value = false;
+    let supportsColorSwitch = settings.supportsColorSwitch;
+    // If we're switching to a printer that either doesn't allow us to switch
+    // to monochrome, or doesn't support color, force a value change.
+    let forceChange = value != supportsColor && (!supportsColorSwitch || value);
+    if (forceChange) {
+      value = !value;
     }
     this.value = value ? "color" : "bw";
-    this.options.namedItem("color-option").hidden = !supportsColor;
+    this.toggleAttribute("disallowed", !supportsColorSwitch);
+    this.disabled = !supportsColorSwitch;
     if (forceChange) {
       this.dispatchEvent(new Event("change", { bubbles: true }));
     }
