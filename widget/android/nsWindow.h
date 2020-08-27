5dfb75acf721fb8003f681235c3e7be41859e976
Aaron Klotz <aklotz@mozilla.com>
2020-08-27 20:14:56 +0000

    Bug 1651705: Part 7 - Update MediaSessionSupport to work with NativeWeakPtr; r=geckoview-reviewers,agi
    This patch is similar to part 4 but for MediaSessionSupport.
    
    Conversions over to `NativeWeakPtr` are pretty straight forward thanks to the
    type system. Basically we take a `NativeWeakPtr`, call `Access()` on it, and
    if the accessor is truthy, then we call whatever methods we need to call.
    
    Creation of new pointers is done using `NativeWeakPtrHolder::Attach()` and
    detaching of strong references is done by `NativeWeakPtr::Detach()`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88088

--- widget/android/nsWindow.h
+++ widget/android/nsWindow.h
@@ -37,6 +37,7 @@ class AndroidView;
 class GeckoEditableSupport;
 class GeckoViewSupport;
 class LayerViewSupport;
+class MediaSessionSupport;
 class NPZCSupport;
 }  // namespace widget
 
@@ -94,8 +95,8 @@ class nsWindow final : public nsBaseWidget {
   mozilla::jni::NativeWeakPtr<mozilla::a11y::SessionAccessibility>
       mSessionAccessibility;
 
-  class MediaSessionSupport;
-  NativePtr<MediaSessionSupport> mMediaSessionSupport;
+  mozilla::jni::NativeWeakPtr<mozilla::widget::MediaSessionSupport>
+      mMediaSessionSupport;
 
   // Object that implements native GeckoView calls and associated states.
   // nullptr for nsWindows that were not opened from GeckoView.
