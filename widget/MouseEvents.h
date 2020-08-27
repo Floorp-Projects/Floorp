5f407a4f01c244e727e0418109432506de20c34c
Edgar Chen <echen@mozilla.com>
2020-08-27 17:19:07 +0000

    Bug 1653949 - Part 4: Add MouseExitFromWidget event to the delayed event queue if event handling is suppressed; r=smaug
    So that we won't lost it if it happens in nested event loop.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86953

--- widget/MouseEvents.h
+++ widget/MouseEvents.h
@@ -292,6 +292,7 @@ class WidgetMouseEvent : public WidgetMouseEventBase,
     AssignMouseEventBaseData(aEvent, aCopyTargets);
     AssignPointerHelperData(aEvent, /* aCopyCoalescedEvents */ true);
 
+    mExitFrom = aEvent.mExitFrom;
     mIgnoreRootScrollFrame = aEvent.mIgnoreRootScrollFrame;
     mClickCount = aEvent.mClickCount;
     mUseLegacyNonPrimaryDispatch = aEvent.mUseLegacyNonPrimaryDispatch;
