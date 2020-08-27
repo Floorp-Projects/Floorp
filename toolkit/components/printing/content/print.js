f9f2f83011cf0e5b076f4f3d4b79ad9a7effde32
Bogdan Tara <btara@mozilla.com>
2020-08-27 09:01:46 +0300

    Backed out 4 changesets (bug 1657459, bug 1660359) for browser_modal_print.js failures CLOSED TREE
    Backed out changeset 971e2d1a6140 (bug 1657459)
    Backed out changeset a447925c9f8e (bug 1660359)
    Backed out changeset 697accb3ceb3 (bug 1660359)
    Backed out changeset fc650bd7d6b8 (bug 1660359)

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -155,8 +155,6 @@ var PrintEventHandler = {
       })
     );
 
-    await document.l10n.translateElements([this.previewBrowser]);
-
     document.body.removeAttribute("loading");
   },
 
@@ -175,11 +173,6 @@ var PrintEventHandler = {
     });
     printPreviewBrowser.classList.add("printPreviewBrowser");
     printPreviewBrowser.setAttribute("flex", "1");
-    printPreviewBrowser.setAttribute("printpreview", "true");
-    document.l10n.setAttributes(printPreviewBrowser, "printui-preview-label");
-    printPreviewBrowser.addEventListener("contextmenu", e => {
-      e.preventDefault();
-    });
 
     // Create the stack for the loading indicator.
     let ourBrowser = window.docShell.chromeEventHandler;
