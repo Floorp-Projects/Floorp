9f3c748f4b7365c211ba770266a76d67dbb053e6
Emilio Cobos Álvarez <emilio@crisal.io>
2020-08-27 12:33:11 +0000

    Bug 1661123 - Make LightWeightThemeConsumer.jsm sanitize colors faster. r=Gijs,jwatt a=RyanVM
    Instead of creating an element, flushing styles and getting the computed
    value back just to receive, use the existing InspectorUtils.colorToRGBA.
    
    With some refactoring, we can completely get rid of parsing rgba strings
    in LightWeightThemeConsumer too, as a benefit. This should be much
    faster.
    
    This patch tweaks the InspectorUtils API to allow taking a document, so
    that system colors keep working. We could probably get away without
    supporting system colors, but it'd technically be a regression, and
    since we want this patch to be uplifted, and it's easy, let's avoid
    breaking changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88200

--- layout/inspector/InspectorUtils.h
+++ layout/inspector/InspectorUtils.h
@@ -133,7 +133,8 @@ class InspectorUtils {
   //
   // NOTE: Converting a color to RGBA may be lossy when converting from some
   // formats e.g. CMYK.
-  static void ColorToRGBA(GlobalObject& aGlobal, const nsACString& aColorString,
+  static void ColorToRGBA(GlobalObject&, const nsACString& aColorString,
+                          const Document*,
                           Nullable<InspectorRGBATuple>& aResult);
 
   // Check whether a given color is a valid CSS color.
