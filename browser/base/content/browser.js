ffd9bba19bd27425dc529564c133f8b57100a719
pbz <pbz@mozilla.com>
2020-08-28 12:36:00 +0000

    Bug 1660951 - Focus TabDialogBox top dialog when switching tabs. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D88437

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -8934,6 +8934,10 @@ class TabDialogBox {
     this._dialogManager.abortAll();
   }
 
+  focus() {
+    this._dialogManager.focusTopDialog();
+  }
+
   /**
    * If the user navigates away or refreshes the page, close all dialogs for
    * the current browser.
