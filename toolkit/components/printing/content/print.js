3da3b38f007cb91c7cdfd9f3dbba0a18f5cdfa77
Micah Tigley <mtigley@mozilla.com>
2020-08-28 18:58:24 +0000

    Bug 1660060 - Add a keypress listener to the percentScale element. r=emalysz a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D88446

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -898,6 +898,7 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
     this._scaleError = this.querySelector("#error-invalid-scale");
 
     this._percentScale.addEventListener("input", this);
+    this._percentScale.addEventListener("keypress", this);
     this.addEventListener("input", this);
   }
 
@@ -920,6 +921,19 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
   }
 
   handleEvent(e) {
+    if (e.type == "keypress") {
+      let char = String.fromCharCode(e.charCode);
+      if (
+        !char.match(/^[0-9]$/) &&
+        !char.match("\x00") &&
+        !e.ctrlKey &&
+        !e.metaKey
+      ) {
+        e.preventDefault();
+      }
+      return;
+    }
+
     if (e.target == this._shrinkToFitChoice || e.target == this._scaleChoice) {
       if (!this._percentScale.checkValidity()) {
         this._percentScale.value = 100;
