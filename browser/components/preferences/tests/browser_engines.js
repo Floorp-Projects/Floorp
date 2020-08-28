299368891b68d19456feb8b30aa5f11e0bd4bb6d
Dale Harvey <dale@arandomurl.com>
2020-08-28 08:41:48 +0000

    Bug 1658713 - Show internal aliases in about:preferences r=preferences-reviewers,harry,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D87922

--- browser/components/preferences/tests/browser_engines.js
+++ browser/components/preferences/tests/browser_engines.js
@@ -1,3 +1,20 @@
+let engineName = "engine1";
+
+function getCellText(tree, i, cellName) {
+  return tree.view.getCellText(i, tree.columns.getNamedColumn(cellName));
+}
+
+add_task(async function setup() {
+  const engine = await Services.search.addEngineWithDetails(engineName, {
+    alias: ["testing", "customkeyword"],
+    method: "get",
+    template: "http://example.com/engine1?search={searchTerms}",
+  });
+  registerCleanupFunction(async function() {
+    await Services.search.removeEngine(engine);
+  });
+});
+
 // Test Engine list
 add_task(async function() {
   let prefs = await openPreferencesViaOpenPreferencesAPI("search", {
@@ -16,16 +33,46 @@ add_task(async function() {
   let defaultEngines = await Services.search.getDefaultEngines();
   for (let i = 0; i < defaultEngines.length; i++) {
     let engine = defaultEngines[i];
-    let cellName = tree.view.getCellText(
-      i,
-      tree.columns.getNamedColumn("engineName")
-    );
     is(
-      cellName,
+      getCellText(tree, i, "engineName"),
       engine.name,
       "Default search engine " + engine.name + " displayed correctly"
     );
   }
+
+  let customEngineIndex = defaultEngines.length;
+  is(
+    getCellText(tree, customEngineIndex, "engineKeyword"),
+    "testing, customkeyword",
+    "Show internal aliases"
+  );
+
+  // Scroll the treeview into view since mouse operations
+  // off screen can act confusingly.
+  tree.scrollIntoView();
+  let rect = tree.getCoordsForCellItem(
+    customEngineIndex,
+    tree.columns.getNamedColumn("engineKeyword"),
+    "text"
+  );
+  let x = rect.x + rect.width / 2;
+  let y = rect.y + rect.height / 2;
+  let win = tree.ownerGlobal;
+
+  let promise = BrowserTestUtils.waitForEvent(tree, "dblclick");
+  EventUtils.synthesizeMouse(tree.body, x, y, { clickCount: 1 }, win);
+  EventUtils.synthesizeMouse(tree.body, x, y, { clickCount: 2 }, win);
+  await promise;
+
+  EventUtils.sendString("newkeyword");
+  EventUtils.sendKey("RETURN");
+
+  await TestUtils.waitForCondition(() => {
+    return (
+      getCellText(tree, customEngineIndex, "engineKeyword") ===
+      "newkeyword, testing, customkeyword"
+    );
+  });
 
   // Avoid duplicated keywords
   tree.view.setCellText(
@@ -38,11 +85,11 @@ add_task(async function() {
     tree.columns.getNamedColumn("engineKeyword"),
     "keyword"
   );
-  let cellKeyword = tree.view.getCellText(
-    1,
-    tree.columns.getNamedColumn("engineKeyword")
+  isnot(
+    getCellText(tree, 1, "engineKeyword"),
+    "keyword",
+    "Do not allow duplicated keywords"
   );
-  isnot(cellKeyword, "keyword", "Do not allow duplicated keywords");
 
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
 });
