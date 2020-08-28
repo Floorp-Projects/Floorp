b31f99c9b99f03aa14352d01391c61e90be841c8
Kartikaya Gupta <kgupta@mozilla.com>
2020-08-28 20:43:50 +0000

    Bug 1661755 - Update logging. r=tnikkel
    This looks like a holdover from when we added ScrollMetadata as a superobject
    of FrameMetrics.
    
    Depends on D88648
    
    Differential Revision: https://phabricator.services.mozilla.com/D88649

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -974,7 +974,7 @@ class Layer {
     if (mScrollMetadata.Length() != 1 ||
         mScrollMetadata[0] != aScrollMetadata) {
       MOZ_LAYERS_LOG_IF_SHADOWABLE(this,
-                                   ("Layer::Mutated(%p) FrameMetrics", this));
+                                   ("Layer::Mutated(%p) ScrollMetadata", this));
       mScrollMetadata.ReplaceElementsAt(0, mScrollMetadata.Length(),
                                         aScrollMetadata);
       ScrollMetadataChanged();
@@ -1003,7 +1003,7 @@ class Layer {
     Manager()->ClearPendingScrollInfoUpdate();
     if (mScrollMetadata != aMetadataArray) {
       MOZ_LAYERS_LOG_IF_SHADOWABLE(this,
-                                   ("Layer::Mutated(%p) FrameMetrics", this));
+                                   ("Layer::Mutated(%p) ScrollMetadata", this));
       mScrollMetadata = aMetadataArray.Clone();
       ScrollMetadataChanged();
       Mutated();
