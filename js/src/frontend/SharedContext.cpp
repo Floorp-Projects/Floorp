0078ef4b84784831b33378433dcefa929f280b6a
Tooru Fujisawa <arai_a@mac.com>
2020-08-28 14:33:33 +0000

    Bug 1661079 - Part 8: Use SystemAllocPolicy in CompilationStencil.asmJS. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D88346

--- js/src/frontend/SharedContext.cpp
+++ js/src/frontend/SharedContext.cpp
@@ -381,7 +381,11 @@ bool FunctionBox::setAsmJSModule(const JS::WasmModule* module) {
   flags_.setIsExtended();
   flags_.setKind(FunctionFlags::AsmJS);
 
-  return compilationInfo_.stencil.asmJS.putNew(index(), module);
+  if (!compilationInfo_.stencil.asmJS.putNew(index(), module)) {
+    js::ReportOutOfMemory(cx_);
+    return false;
+  }
+  return true;
 }
 
 ModuleSharedContext::ModuleSharedContext(JSContext* cx,
