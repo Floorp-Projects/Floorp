6f152c85d42b2e145267cb0874687dce299e4307
Razvan Maries <rmaries@mozilla.com>
2020-08-28 16:31:40 +0300

    Backed out changeset ede12aa1b8b5 (bug 1661137) for perma failures on printcmd.py and browser_ext_tabs_saveAsPDF.js. CLOSED TREE

--- layout/printing/nsPrintJob.cpp
+++ layout/printing/nsPrintJob.cpp
@@ -2556,8 +2556,9 @@ nsresult nsPrintJob::StartPagePrintTimer(const UniquePtr<nsPrintObject>& aPO) {
     nsCOMPtr<Document> doc = cv->GetDocument();
     NS_ENSURE_TRUE(doc, NS_ERROR_FAILURE);
 
-    mPagePrintTimer =
+    RefPtr<nsPagePrintTimer> timer =
         new nsPagePrintTimer(this, mDocViewerPrint, doc, printPageDelay);
+    timer.forget(&mPagePrintTimer);
 
     nsCOMPtr<nsIPrintSession> printSession;
     nsresult rv =
