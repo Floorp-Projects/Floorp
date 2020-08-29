6de0e8d2768e511d8198175962c43dd75dc62e1a
Edgar Chen <echen@mozilla.com>
2020-08-29 21:11:41 +0000

    Bug 1653949 - Part 3: Dispatch MouseExitFromWidget event with proper ExitFrom type based on the "direction" of movement; r=smaug
    There are three cases,
    - Move to inner OOP frame.
    - Move to outer OOP frame.
    - Move to an OOP frame that is in different sub-tree.
    
    We could use common BrowserParent ancestor to determine which case is and
    dispatch MouseExitFromWidget event with proper ExitFrom type.
    
    Depends on D84748
    
    Differential Revision: https://phabricator.services.mozilla.com/D84761

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -2541,6 +2541,17 @@ nsINode* nsContentUtils::GetCommonAncestorUnderInteractiveContent(
 }
 
 /* static */
+BrowserParent* nsContentUtils::GetCommonBrowserParentAncestor(
+    BrowserParent* aBrowserParent1, BrowserParent* aBrowserParent2) {
+  return GetCommonAncestorInternal(
+      aBrowserParent1, aBrowserParent2, [](BrowserParent* aBrowserParent) {
+        return aBrowserParent->GetBrowserBridgeParent()
+                   ? aBrowserParent->GetBrowserBridgeParent()->Manager()
+                   : nullptr;
+      });
+}
+
+/* static */
 template <typename FPT, typename FRT, typename SPT, typename SRT>
 Maybe<int32_t> nsContentUtils::ComparePoints(
     const RangeBoundaryBase<FPT, FRT>& aFirstBoundary,
