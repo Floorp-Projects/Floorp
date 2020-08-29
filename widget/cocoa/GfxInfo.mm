005a4ff0c7b79baaf491cce0588a4de01d229a0e
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-08-29 02:58:56 +0000

    Bug 1661505 - Blocklist WR on mac Intel HD3000 r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D88672

--- widget/cocoa/GfxInfo.mm
+++ widget/cocoa/GfxInfo.mm
@@ -387,6 +387,10 @@ const nsTArray<GfxDriverInfo>& GfxInfo::GetGfxDriverInfo() {
     IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::IntelRolloutWebRender,
                                    nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
                                    "FEATURE_ROLLOUT_INTEL_MAC");
+    // Intel HD3000 disabled due to bug 1661505
+    IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::IntelHD3000,
+                                   nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_BLOCKED_DEVICE,
+                                   "FEATURE_FAILURE_INTEL_MAC_HD3000_NO_WEBRENDER");
     IMPLEMENT_MAC_DRIVER_BLOCKLIST(OperatingSystem::OSX, DeviceFamily::AtiRolloutWebRender,
                                    nsIGfxInfo::FEATURE_WEBRENDER, nsIGfxInfo::FEATURE_ALLOW_ALWAYS,
                                    "FEATURE_ROLLOUT_AMD_MAC");
