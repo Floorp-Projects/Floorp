2d5d216776ffe3b7d71d48c91eb57f3746695c24
sotaro <sotaro.ikeda.g@gmail.com>
2020-08-30 04:41:24 +0000

    Bug 1661189 - Make directly texture bounded buffers are not held by AsyncImagePipelineManager::mTexturesInUseByGPU on Android r=mattwoodrow
    For android SurfaceTexture and AndroidHardwareBuffer, GPU/hardware task end could be checked by android fence. Then their TextureHost do not need to be held by AsyncImagePipelineManager::mTexturesInUseByGPU.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88239

--- gfx/layers/opengl/TextureHostOGL.h
+++ gfx/layers/opengl/TextureHostOGL.h
@@ -450,6 +450,11 @@ class SurfaceTextureHost : public TextureHost {
                         const Range<wr::ImageKey>& aImageKeys,
                         const bool aPreferCompositorSurface) override;
 
+  // gecko does not need deferred deletion with WebRender
+  // GPU/hardware task end could be checked by android fence.
+  // SurfaceTexture uses android fence internally,
+  bool NeedsDeferredDeletion() const override { return false; }
+
  protected:
   bool EnsureAttached();
 
@@ -527,6 +532,10 @@ class AndroidHardwareBufferTextureHost : public TextureHost {
   AndroidHardwareBuffer* GetAndroidHardwareBuffer() const override {
     return mAndroidHardwareBuffer;
   }
+
+  // gecko does not need deferred deletion with WebRender
+  // GPU/hardware task end could be checked by android fence.
+  bool NeedsDeferredDeletion() const override { return false; }
 
  protected:
   void DestroyEGLImage();
