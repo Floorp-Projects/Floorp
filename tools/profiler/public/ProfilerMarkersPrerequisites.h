9061b70e8cd820b126622c2e1400d1181f516001
Gerald Squelart <gsquelart@mozilla.com>
2020-08-31 23:28:22 +0000

    Bug 1646266 - ProfilerString{,8,16}View - r=gregtatum
    These string views are similar to `std::string_view`, but they are optimized to be serialized in the profiler buffer, and later deserialized and streamed to JSON.
    They accept literal strings, and keep them as unowned raw pointers and sizes.
    They also accept any substring reference, assuming that they will only be used as parameters during function calls, and therefore the dependent string will live during that call where these `StringView`'s are used.
    
    Internally, they also allow optional string ownership, which is only used during deserialization and streaming.
    This is hidden, so that users are not tempted to use potentially expensive string allocations during profiling; it's only used *after* profiling, so it's less of an impact to allocate strings then. (But it could still be optimized later on, as part of bug 1577656.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D87242

--- tools/profiler/public/ProfilerMarkersPrerequisites.h
+++ tools/profiler/public/ProfilerMarkersPrerequisites.h
@@ -4,6 +4,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This header contains basic definitions required to create marker types, and
+// to add markers to the profiler buffers.
+//
+// In most cases, #include "mozilla/ProfilerMarkers.h" instead, or
+// #include "mozilla/ProfilerMarkerTypes.h" for common marker types.
+
 #ifndef ProfilerMarkersPrerequisites_h
 #define ProfilerMarkersPrerequisites_h
 
