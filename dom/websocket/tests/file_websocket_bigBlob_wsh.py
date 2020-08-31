eac429231c6639f7f215da78a0c8dad706b23c6f
Philipp Fischbeck <pfischbeck@googlemail.com>
2020-08-31 14:42:21 +0000

    Bug 1622683 - Make dom/websocket flake8 and pylint compliant. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D88809

--- dom/websocket/tests/file_websocket_bigBlob_wsh.py
+++ dom/websocket/tests/file_websocket_bigBlob_wsh.py
@@ -1,9 +1,11 @@
 from mod_pywebsocket import msgutil
 
+
 def web_socket_do_extra_handshake(request):
-  pass
+    pass
+
 
 def web_socket_transfer_data(request):
-  while True:
-    line = msgutil.receive_message(request)
-    msgutil.send_message(request, line, True, True)
+    while True:
+        line = msgutil.receive_message(request)
+        msgutil.send_message(request, line, True, True)
