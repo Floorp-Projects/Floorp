08e99024ce1da8db166ab64f06b3f10d1c6e8e37
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-08-31 20:27:08 +0300

    Backed out 5 changesets (bug 1658359) for bc crashes on application crashed [@ mozilla::net::nsSocketTransport::InitiateSocket()] . CLOSED TREE
    Backed out changeset 2cb2786b8e97 (bug 1658359)
    Backed out changeset 859f1b00f6b0 (bug 1658359)
    Backed out changeset 27b7e050768d (bug 1658359)
    Backed out changeset 8d705f1fa355 (bug 1658359)
    Backed out changeset 555f1ebcef60 (bug 1658359)

--- toolkit/components/telemetry/core/Telemetry.cpp
+++ toolkit/components/telemetry/core/Telemetry.cpp
@@ -80,6 +80,10 @@
 #endif
 #include "nsVersionComparator.h"
 #include "nsXPCOMCIDInternal.h"
+#include "nsXPCOMPrivate.h"
+#include "nsXULAppAPI.h"
+#include "nsIPropertyBag2.h"
+#include "nsIXULAppInfo.h"
 #include "other/CombinedStacks.h"
 #include "other/TelemetryIOInterposeObserver.h"
 #include "plstr.h"
@@ -1125,6 +1129,73 @@ TelemetryImpl::GetIsOfficialTelemetry(bool* ret) {
   return NS_OK;
 }
 
+#if defined(MOZ_GLEAN)
+// The FOG API is implemented in Rust and exposed to C++ via a set of
+// C functions with the "fog_" prefix.
+// See toolkit/components/glean/*.
+extern "C" {
+nsresult fog_init(const nsACString* dataPath, const nsACString* buildId,
+                  const nsACString* appDisplayVersion, const char* channel,
+                  const nsACString* osVersion, const nsACString* architecture);
+}
+
+static void internal_initGlean() {
+  nsAutoCString dataPath;
+  nsresult rv = Preferences::GetCString(
+      "telemetry.fog.temporary_and_just_for_testing.data_path", dataPath);
+
+  if (NS_FAILED(rv)) {
+    return;
+  }
+
+  nsCOMPtr<nsIXULAppInfo> appInfo =
+      do_GetService("@mozilla.org/xre/app-info;1");
+  if (!appInfo) {
+    NS_WARNING("Can't fetch app info. FOG will not be initialized.");
+    return;
+  }
+
+  nsAutoCString buildID;
+  rv = appInfo->GetAppBuildID(buildID);
+  if (NS_FAILED(rv)) {
+    NS_WARNING("Can't get build ID. FOG will not be initialized.");
+    return;
+  }
+
+  nsAutoCString appVersion;
+  rv = appInfo->GetVersion(appVersion);
+  if (NS_FAILED(rv)) {
+    NS_WARNING("Can't get app version. FOG will not be initialized.");
+    return;
+  }
+
+  nsCOMPtr<nsIPropertyBag2> infoService =
+      do_GetService("@mozilla.org/system-info;1");
+  if (!appInfo) {
+    NS_WARNING("Can't fetch info service. FOG will not be initialized.");
+    return;
+  }
+
+  nsAutoCString osVersion;
+  rv = infoService->GetPropertyAsACString(u"version"_ns, osVersion);
+  if (NS_FAILED(rv)) {
+    NS_WARNING("Can't get OS version. FOG will not be initialized.");
+    return;
+  }
+
+  nsAutoCString architecture;
+  rv = infoService->GetPropertyAsACString(u"arch"_ns, architecture);
+  if (NS_FAILED(rv)) {
+    NS_WARNING("Can't get architecture. FOG will not be initialized.");
+    return;
+  }
+
+  Unused << NS_WARN_IF(NS_FAILED(fog_init(&dataPath, &buildID, &appVersion,
+                                          MOZ_STRINGIFY(MOZ_UPDATE_CHANNEL),
+                                          &osVersion, &architecture)));
+}
+#endif  // defined(MOZ_GLEAN)
+
 already_AddRefed<nsITelemetry> TelemetryImpl::CreateTelemetryInstance() {
   {
     auto lock = sTelemetry.Lock();
@@ -1674,6 +1745,15 @@ TelemetryImpl::FlushBatchedChildTelemetry() {
 NS_IMETHODIMP
 TelemetryImpl::EarlyInit() {
   Unused << MemoryTelemetry::Get();
+
+#if defined(MOZ_GLEAN)
+  // Initialize FOG during early init, which gets called from
+  // TelemetryController.
+  // At that point we have a working preference store.
+  if (XRE_IsParentProcess()) {
+    internal_initGlean();
+  }
+#endif
 
   return NS_OK;
 }
