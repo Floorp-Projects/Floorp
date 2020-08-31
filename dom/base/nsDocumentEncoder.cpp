1372471e217d7aa5859fc99138dc4fb51ada1fae
Mirko Brodesser <mbrodesser@mozilla.com>
2020-08-31 12:30:49 +0000

    Bug 1649121: part 83) Rename arguments of `SerializeNodePartiallyContainedInRange` to conform to style-guide. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D88263

--- dom/base/nsDocumentEncoder.cpp
+++ dom/base/nsDocumentEncoder.cpp
@@ -471,8 +471,8 @@ class nsDocumentEncoder : public nsIDocumentEncoder {
     bool HasInvisibleParentAndShouldBeSkipped(nsINode& aNode) const;
 
     nsresult SerializeNodePartiallyContainedInRange(
-        nsINode* aNode, nsIContent* content,
-        const StartAndEndContent& startAndEndContent, const nsRange* aRange,
+        nsINode* aNode, nsIContent* aContent,
+        const StartAndEndContent& aStartAndEndContent, const nsRange* aRange,
         int32_t aDepth);
 
     nsresult SerializeTextNode(nsINode& aNode, const nsIContent& aContent,
@@ -1086,15 +1086,15 @@ nsresult nsDocumentEncoder::RangeSerializer::SerializeRangeNodes(
 
 nsresult
 nsDocumentEncoder::RangeSerializer::SerializeNodePartiallyContainedInRange(
-    nsINode* aNode, nsIContent* content,
-    const StartAndEndContent& startAndEndContent, const nsRange* aRange,
+    nsINode* aNode, nsIContent* aContent,
+    const StartAndEndContent& aStartAndEndContent, const nsRange* aRange,
     const int32_t aDepth) {
   // due to implementation it is impossible for text node to be both start and
   // end of range.  We would have handled that case without getting here.
   // XXXsmaug What does this all mean?
   if (IsTextNode(aNode)) {
     nsresult rv =
-        SerializeTextNode(*aNode, *content, startAndEndContent, *aRange);
+        SerializeTextNode(*aNode, *aContent, aStartAndEndContent, *aRange);
     NS_ENSURE_SUCCESS(rv, rv);
   } else {
     if (aNode != mClosestCommonInclusiveAncestorOfRange) {
@@ -1103,10 +1103,10 @@ nsDocumentEncoder::RangeSerializer::SerializeNodePartiallyContainedInRange(
         // is so paste client will include this node in paste.
         mHaltRangeHint = true;
       }
-      if ((startAndEndContent.mStart == content) && !mHaltRangeHint) {
+      if ((aStartAndEndContent.mStart == aContent) && !mHaltRangeHint) {
         ++mContextInfoDepth.mStart;
       }
-      if ((startAndEndContent.mEnd == content) && !mHaltRangeHint) {
+      if ((aStartAndEndContent.mEnd == aContent) && !mHaltRangeHint) {
         ++mContextInfoDepth.mEnd;
       }
 
@@ -1124,14 +1124,14 @@ nsDocumentEncoder::RangeSerializer::SerializeNodePartiallyContainedInRange(
     // do some calculations that will tell us which children of this
     // node are in the range.
     int32_t startOffset = 0, endOffset = -1;
-    if (startAndEndContent.mStart == content && mStartRootIndex >= aDepth) {
+    if (aStartAndEndContent.mStart == aContent && mStartRootIndex >= aDepth) {
       startOffset = inclusiveAncestorsOffsetsOfStart[mStartRootIndex - aDepth];
     }
-    if (startAndEndContent.mEnd == content && mEndRootIndex >= aDepth) {
+    if (aStartAndEndContent.mEnd == aContent && mEndRootIndex >= aDepth) {
       endOffset = inclusiveAncestorsOffsetsOfEnd[mEndRootIndex - aDepth];
     }
-    // generated content will cause offset values of -1 to be returned.
-    uint32_t childCount = content->GetChildCount();
+    // generated aContent will cause offset values of -1 to be returned.
+    uint32_t childCount = aContent->GetChildCount();
 
     if (startOffset == -1) startOffset = 0;
     if (endOffset == -1)
@@ -1149,8 +1149,8 @@ nsDocumentEncoder::RangeSerializer::SerializeNodePartiallyContainedInRange(
     }
 
     if (endOffset) {
-      nsresult rv = SerializeChildrenOfContent(*content, startOffset, endOffset,
-                                               aRange, aDepth);
+      nsresult rv = SerializeChildrenOfContent(*aContent, startOffset,
+                                               endOffset, aRange, aDepth);
       NS_ENSURE_SUCCESS(rv, rv);
     }
     // serialize the end of this node
