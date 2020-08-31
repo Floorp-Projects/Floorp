a7cd80631113945dd1274b1616a37adc359c62ce
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-09-01 02:30:36 +0300

    Backed out 2 changesets (bug 1659530, bug 1657476) for bustages on check.svg. CLOSED TREE
    Backed out changeset 761a09f4d153 (bug 1659530)
    Backed out changeset c8afdd9434bb (bug 1657476)

--- toolkit/mozapps/extensions/test/xpinstall/browser_bug645699.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_bug645699.js
@@ -3,11 +3,6 @@
 // content. This should be blocked by the whitelist check.
 // This verifies bug 645699
 function test() {
-  // prompt prior to download
-  SpecialPowers.pushPrefEnv({
-    set: [["extensions.postDownloadThirdPartyPrompt", false]],
-  });
-
   Harness.installConfirmCallback = confirm_install;
   Harness.installBlockedCallback = allow_blocked;
   Harness.installsCompletedCallback = finish_test;
@@ -44,7 +39,7 @@ function confirm_install(panel) {
 
 function finish_test(count) {
   is(count, 0, "0 Add-ons should have been successfully installed");
-  PermissionTestUtils.remove("http://example.org/", "install");
+  PermissionTestUtils.remove("http://addons.mozilla.org", "install");
 
   gBrowser.removeCurrentTab();
   Harness.finish();
