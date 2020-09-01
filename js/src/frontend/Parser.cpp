de5d19c5f5b35e2270255c08e7b43723dd7f080e
Kannan Vijayan <kvijayan@mozilla.com>
2020-09-01 23:50:15 +0000

    Bug 1662260 - OOM check in parser. r=mgaudet DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D89071

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -7325,6 +7325,9 @@ bool GeneralParser<ParseHandler, Unit>::classMember(
       }
       const ParserAtom* storedMethodAtom =
           storedMethodName.finishParserAtom(this->compilationInfo_);
+      if (!storedMethodAtom) {
+        return false;
+      }
       const ParserName* storedMethodProp = storedMethodAtom->asName();
       if (!noteDeclaredName(storedMethodProp, DeclarationKind::Const, pos())) {
         return false;
