b16458ad38042564a227f61062aa70fe8ec17ae5
Mark Banner <standard8@mozilla.com>
2020-09-01 18:08:22 +0000

    Bug 1619926 - Remove distribution search directory provider definitions. r=daleharvey
    Also remove DirectoryProvider as it is now unused.
    
    Depends on D88018
    
    Differential Revision: https://phabricator.services.mozilla.com/D88019

--- browser/components/build/nsBrowserCompsCID.h
+++ browser/components/build/nsBrowserCompsCID.h
@@ -39,11 +39,3 @@
       0xb1, 0x31, 0xd9, 0x3d, 0x7d, 0x70, 0x4f, 0x64 \
     }                                                \
   }
-
-// {6DEB193C-F87D-4078-BC78-5E64655B4D62}
-#define NS_BROWSERDIRECTORYPROVIDER_CID              \
-  {                                                  \
-    0x6deb193c, 0xf87d, 0x4078, {                    \
-      0xbc, 0x78, 0x5e, 0x64, 0x65, 0x5b, 0x4d, 0x62 \
-    }                                                \
-  }
