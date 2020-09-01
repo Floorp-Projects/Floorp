39f211d48868c17f6d79dbccaa28d8f9b845d709
Gavin Lazar Suntop <gavin@gsuntop.com>
2020-09-01 22:31:20 +0000

    Bug 1658404 - Adding handlebars compilation r=thecount
    Differential Revision: https://phabricator.services.mozilla.com/D88647

---
+++ browser/components/pocket/compile_handlebars.js
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* eslint-env node */
+
+const { exec } = require("child_process");
+
+const basePath = `./content/panels/tmpl/`;
+
+let templates = [
+  `ho2/ho2_articleinfo`,
+  `ho2/ho2_download`,
+  `ho2/ho2_download_error`,
+  `ho2/ho2_sharebutton`,
+  `saved_premiumextras`,
+  `saved_premiumshell`,
+  `saved_shell`,
+  `saved_tmplogin`,
+  `signup_shell`,
+  `signupstoryboard_shell`,
+];
+
+// Append extension and base path
+templates = templates.map(templatePath => {
+  return `${basePath}${templatePath}.handlebars`;
+});
+
+// "build:handlebars": "npx handlebars ./content/panels/tmpl/ho2/ho2_articleinfo.handlebars -f handlebarstest.js",
+console.log(`Compiling Handlebars\n`);
+
+templates.forEach(path => {
+  console.log(`Building: ${path}`);
+});
+
+let complilationGroup = templates.reduce((accumulator, currentValue) => {
+  return `${accumulator} ${currentValue}`;
+});
+
+exec(
+  `npx handlebars ${complilationGroup} -f ./content/panels/js/tmpl.js`,
+  (error, stdout, stderr) => {
+    if (stderr) {
+      console.error(stderr);
+    }
+  }
+);
