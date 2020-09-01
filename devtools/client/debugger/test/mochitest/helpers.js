74887a5a466dd6e017dbcd1a6b0c434f73029bf6
Alexandre Poirot <poirot.alex@gmail.com>
2020-09-01 09:11:34 +0000

    Bug 1662066 - Use a single waitFor test helper implementation. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D88834

--- devtools/client/debugger/test/mochitest/helpers.js
+++ devtools/client/debugger/test/mochitest/helpers.js
@@ -38,35 +38,6 @@ function logThreadEvents(dbg, event) {
   });
 }
 
-/**
- * Wait for a predicate to return a result.
- *
- * @param function condition
- *        Invoked once in a while until it returns a truthy value. This should be an
- *        idempotent function, since we have to run it a second time after it returns
- *        true in order to return the value.
- * @param string message [optional]
- *        A message to output if the condition fails.
- * @param number interval [optional]
- *        How often the predicate is invoked, in milliseconds.
- * @return object
- *         A promise that is resolved with the result of the condition.
- */
-async function waitFor(
-  condition,
-  message = "waitFor",
-  interval = 10,
-  maxTries = 500
-) {
-  await BrowserTestUtils.waitForCondition(
-    condition,
-    message,
-    interval,
-    maxTries
-  );
-  return condition();
-}
-
 // Wait until an action of `type` is dispatched. This is different
 // then `waitForDispatch` because it doesn't wait for async actions
 // to be done/errored. Use this if you want to listen for the "start"
