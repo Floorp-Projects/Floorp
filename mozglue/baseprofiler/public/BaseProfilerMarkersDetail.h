5dd7e84aa29f059353fd167132f7cd67ece8dfbb
Gerald Squelart <gsquelart@mozilla.com>
2020-09-01 01:31:17 +0000

    Bug 1646266 - ProfilerMarkers skeleton files - r=gregtatum
    This patch introduces all new files that contain the new markers C++ API and implementation.
    They are mostly empty at this time, only including each other as eventually needed, and with `#ifdef MOZ_GECKO_PROFILER` guards.
    
    Rough inclusion diagram: (header <-- includer)
    
        BaseProfilerMarkerPrerequesites.h <-- ProfilerMarkerPrerequesites.h  (Useful types: Input string view, marker options)
                      ^                                    ^
           BaseProfilerMarkerDetail.h     <--    ProfilerMarkerDetail.h      (Implementation details)
                      ^                                    ^
             BaseProfilerMarkers.h        <--      ProfilerMarkers.h         (Main API)
                      ^      ^---------                    ^   ^---------
           BaseProfilerMarkerTypes.h  |   <--    ProfilerMarkerTypes.h   |   (Common marker types)
                      ^         BaseProfiler.h        <--         GeckoProfiler.h  (Existing main profiler headers)
    
    Differential Revision: https://phabricator.services.mozilla.com/D87241

---
+++ mozglue/baseprofiler/public/BaseProfilerMarkersDetail.h
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BaseProfilerMarkersDetail_h
+#define BaseProfilerMarkersDetail_h
+
+#ifndef BaseProfilerMarkers_h
+#  error "This header should only be #included by BaseProfilerMarkers.h"
+#endif
+
+#include "mozilla/BaseProfilerMarkersPrerequisites.h"
+
+#ifdef MOZ_GECKO_PROFILER
+
+#endif  // MOZ_GECKO_PROFILER
+
+#endif  // BaseProfilerMarkersDetail_h
