be17447d93c5852d19d82c09c7da495e3842eb04
Henrik Skupin <mail@hskupin.info>
2020-09-01 21:41:57 +0000

    Bug 1662544 - [geckodriver] Don't set "toolkit.telemetry.server" to prevent side-effects. r=webdriver-reviewers,chutten,maja_zf
    Differential Revision: https://phabricator.services.mozilla.com/D89037

--- testing/geckodriver/src/prefs.rs
+++ testing/geckodriver/src/prefs.rs
@@ -68,16 +68,16 @@ lazy_static! {
         // Do not warn on quitting Firefox
         ("browser.warnOnQuit", Pref::new(false)),
 
-        // Do not show datareporting policy notifications which can
-        // interfere with tests
-        ("datareporting.healthreport.about.reportUrl", Pref::new("http://%(server)s/dummy/abouthealthreport/")),  // removed in Firefox 59
+        // Defensively disable data reporting systems
         ("datareporting.healthreport.documentServerURI", Pref::new("http://%(server)s/dummy/healthreport/")),
         ("datareporting.healthreport.logging.consoleEnabled", Pref::new(false)),
         ("datareporting.healthreport.service.enabled", Pref::new(false)),
         ("datareporting.healthreport.service.firstRun", Pref::new(false)),
         ("datareporting.healthreport.uploadEnabled", Pref::new(false)),
+
+        // Do not show datareporting policy notifications which can
+        // interfere with tests
         ("datareporting.policy.dataSubmissionEnabled", Pref::new(false)),
-        ("datareporting.policy.dataSubmissionPolicyAccepted", Pref::new(false)),
         ("datareporting.policy.dataSubmissionPolicyBypassNotification", Pref::new(true)),
 
         // Disable the ProcessHangMonitor
@@ -149,8 +149,5 @@ lazy_static! {
 
         // Prevent starting into safe mode after application crashes
         ("toolkit.startup.max_resumed_crashes", Pref::new(-1)),
-
-        // We want to collect telemetry, but we don't want to send in the results
-        ("toolkit.telemetry.server", Pref::new("https://%(server)s/dummy/telemetry/")),
     ];
 }
