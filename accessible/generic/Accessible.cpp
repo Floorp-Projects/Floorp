5748e63854621b77d6d13655f2641f4cf920bde8
Emilio Cobos Álvarez <emilio@crisal.io>
2020-09-02 23:47:17 +0000

    Bug 1662456 - Simplify hitregion accessibility code. r=nical
    There's no reason it has to allocate anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88991

--- accessible/generic/Accessible.cpp
+++ accessible/generic/Accessible.cpp
@@ -591,13 +591,7 @@ Accessible* Accessible::ChildAtPoint(int32_t aX, int32_t aY,
 nsRect Accessible::RelativeBounds(nsIFrame** aBoundingFrame) const {
   nsIFrame* frame = GetFrame();
   if (frame && mContent) {
-    bool* pHasHitRegionRect =
-        static_cast<bool*>(mContent->GetProperty(nsGkAtoms::hitregion));
-    MOZ_ASSERT(!pHasHitRegionRect || *pHasHitRegionRect,
-               "hitregion property is always null or true");
-    bool hasHitRegionRect = pHasHitRegionRect && *pHasHitRegionRect;
-
-    if (hasHitRegionRect && mContent->IsElement()) {
+    if (mContent->GetProperty(nsGkAtoms::hitregion) && mContent->IsElement()) {
       // This is for canvas fallback content
       // Find a canvas frame the found hit region is relative to.
       nsIFrame* canvasFrame = frame->GetParent();
