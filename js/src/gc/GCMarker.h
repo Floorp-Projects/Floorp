281b86f899114f20f26a2d7b21ea2f1e8466e60b
Jon Coppeard <jcoppeard@mozilla.com>
2020-09-02 08:47:52 +0000

    Bug 1661766 - Simplify representation of slots/element ranges on the mark stack r=sfink. CLOSED TREE
    This stores slots/element ranges as object and start index rather than having
    two separate representations (one used during marking, one when we yield to the
    mutator during marking). It's possible that this could affect marking
    performance but we'll catch that by checking the mark rate telemetry. This
    should also reduce the size needed for the mark stack a little.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88723

--- js/src/gc/GCMarker.h
+++ js/src/gc/GCMarker.h
@@ -81,10 +81,10 @@ class MarkStack {
    * the context of push or pop operation.
    */
   enum Tag {
-    ValueArrayTag,
+    SlotsRangeTag,
+    ElementsRangeTag,
     ObjectTag,
     GroupTag,
-    SavedValueArrayTag,
     JitCodeTag,
     ScriptTag,
     TempRopeTag,
@@ -110,28 +110,18 @@ class MarkStack {
     template <typename T>
     T* as() const;
 
-    JSObject* asValueArrayObject() const;
-    JSObject* asSavedValueArrayObject() const;
+    JSObject* asSlotsRangeObject() const;
+    JSObject* asElementsRangeObject() const;
     JSRope* asTempRope() const;
 
     void assertValid() const;
   };
 
-  struct ValueArray {
-    ValueArray(JSObject* obj, HeapSlot* start, HeapSlot* end);
+  struct SlotsOrElementsRange {
+    SlotsOrElementsRange(Tag, JSObject* obj, size_t start);
     void assertValid() const;
 
-    HeapSlot* end;
-    HeapSlot* start;
-    TaggedPtr ptr;
-  };
-
-  struct SavedValueArray {
-    SavedValueArray(JSObject* obj, size_t index, HeapSlot::Kind kind);
-    void assertValid() const;
-
-    uintptr_t kind;
-    uintptr_t index;
+    size_t start;
     TaggedPtr ptr;
   };
 
@@ -156,9 +146,8 @@ class MarkStack {
   template <typename T>
   MOZ_MUST_USE bool push(T* ptr);
 
-  MOZ_MUST_USE bool push(JSObject* obj, HeapSlot* start, HeapSlot* end);
-  MOZ_MUST_USE bool push(const ValueArray& array);
-  MOZ_MUST_USE bool push(const SavedValueArray& array);
+  MOZ_MUST_USE bool push(JSObject* obj, HeapSlot::Kind kind, size_t start);
+  MOZ_MUST_USE bool push(const SlotsOrElementsRange& array);
 
   // GCMarker::eagerlyMarkChildren uses unused marking stack as temporary
   // storage to hold rope pointers.
@@ -168,8 +157,7 @@ class MarkStack {
 
   Tag peekTag() const;
   TaggedPtr popPtr();
-  ValueArray popValueArray();
-  SavedValueArray popSavedValueArray();
+  SlotsOrElementsRange popSlotsOrElementsRange();
 
   void clear() {
     // Fall back to the smaller initial capacity so we don't hold on to excess
@@ -229,13 +217,10 @@ class MarkStackIter {
   bool done() const;
   MarkStack::Tag peekTag() const;
   MarkStack::TaggedPtr peekPtr() const;
-  MarkStack::ValueArray peekValueArray() const;
+  MarkStack::SlotsOrElementsRange peekSlotsOrElementsRange() const;
   void next();
   void nextPtr();
   void nextArray();
-
-  // Mutate the current ValueArray to a SavedValueArray.
-  void saveValueArray(const MarkStack::SavedValueArray& savedArray);
 
  private:
   size_t position() const;
@@ -421,7 +406,10 @@ class GCMarker : public JSTracer {
   template <typename T>
   inline void pushTaggedPtr(T* ptr);
 
-  inline void pushValueArray(JSObject* obj, HeapSlot* start, HeapSlot* end);
+  enum class RangeKind { Elements, FixedSlots, DynamicSlots };
+
+  inline void pushValueRange(JSObject* obj, RangeKind kind, size_t start,
+                             size_t end);
 
   bool isMarkStackEmpty() { return stack.isEmpty() && auxStack.isEmpty(); }
 
@@ -432,16 +420,6 @@ class GCMarker : public JSTracer {
   bool hasGrayEntries() const {
     return !getStack(gc::MarkColor::Gray).isEmpty();
   }
-
-  MOZ_MUST_USE bool restoreValueArray(
-      const gc::MarkStack::SavedValueArray& array, HeapSlot** vpp,
-      HeapSlot** endp);
-  gc::MarkStack::ValueArray restoreValueArray(
-      const gc::MarkStack::SavedValueArray& savedArray);
-
-  void saveValueRanges();
-  gc::MarkStack::SavedValueArray saveValueRange(
-      const gc::MarkStack::ValueArray& array);
 
   inline void processMarkStackTop(SliceBudget& budget);
 
