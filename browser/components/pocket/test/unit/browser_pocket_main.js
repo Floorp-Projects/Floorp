bebe317a39facd50ee9cf92ac0f6abc603743561
Scott <scott.downe@gmail.com>
2020-09-02 17:53:21 +0000

    Bug 1655124 - Show similar recs when a page is saved to Pocket. r=gvn
    Differential Revision: https://phabricator.services.mozilla.com/D86120

---
+++ browser/components/pocket/test/unit/browser_pocket_main.js
@@ -0,0 +1,83 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+function test_runner(test) {
+  let testTask = async () => {
+    // Before each
+    XPCOMUtils.defineLazyScriptGetter(
+      this,
+      "pktUI",
+      "chrome://pocket/content/main.js"
+    );
+    const sandbox = sinon.createSandbox();
+    try {
+      await test({ sandbox });
+    } finally {
+      // After each
+      sandbox.restore();
+    }
+  };
+
+  // Copy the name of the test function to identify the test
+  Object.defineProperty(testTask, "name", { value: test.name });
+  add_task(testTask);
+}
+
+test_runner(async function test_main_getAndShowRecsForItem_on({ sandbox }) {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.pocket.onSaveRecs", true]],
+  });
+  const getRecsForItemStub = sandbox.stub(pktApi, "getRecsForItem");
+
+  pktUI.getAndShowRecsForItem(
+    {
+      resolved_id: "1234",
+    },
+    {
+      success() {},
+    }
+  );
+
+  Assert.ok(getRecsForItemStub.calledOnce);
+  Assert.equal(getRecsForItemStub.getCall(0).args[0], "1234");
+});
+
+test_runner(async function test_main_getAndShowRecsForItem_off({ sandbox }) {
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.pocket.onSaveRecs", false]],
+  });
+  const getRecsForItemStub = sandbox.stub(pktApi, "getRecsForItem");
+
+  pktUI.getAndShowRecsForItem(
+    {
+      resolved_id: "1234",
+    },
+    {
+      success() {},
+    }
+  );
+
+  Assert.ok(getRecsForItemStub.notCalled);
+});
+
+test_runner(async function test_main_getAndShowRecsForItem_locale({ sandbox }) {
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["extensions.pocket.onSaveRecs", true],
+      ["extensions.pocket.onSaveRecs.locales", "de"],
+    ],
+  });
+  const getRecsForItemStub = sandbox.stub(pktApi, "getRecsForItem");
+
+  pktUI.getAndShowRecsForItem(
+    {
+      resolved_id: "1234",
+    },
+    {
+      success() {},
+    }
+  );
+
+  Assert.ok(getRecsForItemStub.notCalled);
+});
---
+++ browser/components/pocket/test/unit/head.js
@@ -0,0 +1,7 @@
+ChromeUtils.defineModuleGetter(
+  this,
+  "pktApi",
+  "chrome://pocket/content/pktApi.jsm"
+);
+
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
