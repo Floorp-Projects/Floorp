319cb6b446b5529c9f19a9232433ab518e9f1937
Henri Sivonen <hsivonen@hsivonen.fi>
2020-09-02 15:05:53 +0000

    Bug 1613054 part 1 - Synchronously request frame focus when .contentWindow.focus() called on OOP iframe. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D70800

--- docshell/base/BrowsingContext.cpp
+++ docshell/base/BrowsingContext.cpp
@@ -1860,6 +1860,10 @@ void BrowsingContext::Close(CallerType aCallerType, ErrorResult& aError) {
 }
 
 void BrowsingContext::Focus(CallerType aCallerType, ErrorResult& aError) {
+  if (mEmbedderElement) {
+    // Make the activeElement in this process update synchronously.
+    nsContentUtils::RequestFrameFocus(*mEmbedderElement, true, aCallerType);
+  }
   if (ContentChild* cc = ContentChild::GetSingleton()) {
     cc->SendWindowFocus(this, aCallerType);
   } else if (ContentParent* cp = Canonical()->GetContentParent()) {
