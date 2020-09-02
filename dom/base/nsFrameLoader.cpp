3052c56f74a18eec7c97a09afc7acd865b6d7f31
Olli Pettay <Olli.Pettay@helsinki.fi>
2020-09-02 03:21:34 +0000

    Bug 1661835 - DocShell::Initialize may change the state of nsFrameLoader, r=kmag a=RyanVM
    This is a guess fix based on possible reason for the crash.
    Use of AutoScriptBlocker would be one option too, but better to return as early as possible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D88808

--- dom/base/nsFrameLoader.cpp
+++ dom/base/nsFrameLoader.cpp
@@ -2185,6 +2185,8 @@ nsresult nsFrameLoader::MaybeCreateDocShell() {
     return NS_ERROR_FAILURE;
   }
 
+  NS_ENSURE_STATE(mOwnerContent);
+
   // If we are an in-process browser, we want to set up our session history.
   if (mIsTopLevelContent && mOwnerContent->IsXULElement(nsGkAtoms::browser) &&
       !mOwnerContent->HasAttr(kNameSpaceID_None, nsGkAtoms::disablehistory)) {
