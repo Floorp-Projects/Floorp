3326cf870e435db4f30ff924a3c05cff1c9d6288
Emilio Cobos Álvarez <emilio@crisal.io>
2020-09-03 17:03:06 +0000

    Bug 1662975 - Don't return from window.print() until the print dialog is closed. r=jwatt, a=RyanVM
    As much as I hate nested event loops, I think we need to do it for this
    case :)
    
    Hopefully print() already assumes nested event loops can happen because
    of the native dialog so it's already properly set up for this.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89254

--- dom/ipc/BrowserChild.cpp
+++ dom/ipc/BrowserChild.cpp
@@ -2295,8 +2295,9 @@ mozilla::ipc::IPCResult BrowserChild::RecvPrintPreview(
 
   sourceWindow->Print(printSettings,
                       /* aListener = */ nullptr, docShellToCloneInto,
-                      /* aIsPreview = */ true, std::move(aCallback),
-                      IgnoreErrors());
+                      nsGlobalWindowOuter::IsPreview::Yes,
+                      nsGlobalWindowOuter::BlockUntilDone::No,
+                      std::move(aCallback), IgnoreErrors());
 #endif
   return IPC_OK();
 }
@@ -2348,7 +2349,8 @@ mozilla::ipc::IPCResult BrowserChild::RecvPrint(const uint64_t& aOuterWindowID,
     outerWindow->Print(printSettings,
                        /* aListener = */ nullptr,
                        /* aWindowToCloneInto = */ nullptr,
-                       /* aIsPreview = */ false,
+                       nsGlobalWindowOuter::IsPreview::No,
+                       nsGlobalWindowOuter::BlockUntilDone::No,
                        /* aPrintPreviewCallback = */ nullptr, rv);
     if (NS_WARN_IF(rv.Failed())) {
       return IPC_OK();
