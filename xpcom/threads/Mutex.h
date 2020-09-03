1d543ffbc13f23c2a7418ae6e17c36e171d51629
Chris Peterson <cpeterson@mozilla.com>
2020-09-03 05:34:23 +0000

    Bug 1662957 - Add [[nodiscard]] to xpcom's Mutex and Monitor TryLock() functions. r=xpcom-reviewers,sg
    Depends on D89092
    
    Differential Revision: https://phabricator.services.mozilla.com/D89093

--- xpcom/threads/Mutex.h
+++ xpcom/threads/Mutex.h
@@ -66,7 +66,7 @@ class OffTheBooksMutex : public detail::MutexImpl, BlockingResourceBase {
   /**
    * Try to lock this mutex, returning true if we were successful.
    **/
-  bool TryLock() { return this->tryLock(); }
+  [[nodiscard]] bool TryLock() { return this->tryLock(); }
 
   /**
    * Unlock this mutex.
@@ -92,7 +92,7 @@ class OffTheBooksMutex : public detail::MutexImpl, BlockingResourceBase {
 
 #else
   void Lock();
-  bool TryLock();
+  [[nodiscard]] bool TryLock();
   void Unlock();
 
   void AssertCurrentThreadOwns() const;
