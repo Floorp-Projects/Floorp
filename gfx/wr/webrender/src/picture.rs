de5d06f905fee4abb6184c1c7f1e8f78dbadca28
Glenn Watson <git@intuitionlibrary.com>
2020-09-08 08:45:28 +0000

    Bug 1663362 - Remove extra raster root assignment pass. r=nical
    This was left as a debug option when the original change was made,
    to allow seeing the differences. It's no longer required.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89328

--- gfx/wr/webrender/src/picture.rs
+++ gfx/wr/webrender/src/picture.rs
@@ -3895,7 +3895,6 @@ pub struct PictureUpdateState<'a> {
     surfaces: &'a mut Vec<SurfaceInfo>,
     surface_stack: Vec<SurfaceIndex>,
     picture_stack: Vec<PictureInfo>,
-    are_raster_roots_assigned: bool,
     composite_state: &'a CompositeState,
 }
 
@@ -3918,7 +3917,6 @@ impl<'a> PictureUpdateState<'a> {
             surfaces,
             surface_stack: buffers.surface_stack.take().cleared(),
             picture_stack: buffers.picture_stack.take().cleared(),
-            are_raster_roots_assigned: true,
             composite_state,
         };
 
@@ -3932,14 +3930,6 @@ impl<'a> PictureUpdateState<'a> {
             clip_store,
             data_stores,
         );
-
-        if !state.are_raster_roots_assigned {
-            state.assign_raster_roots(
-                pic_index,
-                picture_primitives,
-                ROOT_SPATIAL_NODE_INDEX,
-            );
-        }
 
         buffers.surface_stack = state.surface_stack.take();
         buffers.picture_stack = state.picture_stack.take();
@@ -4029,49 +4019,6 @@ impl<'a> PictureUpdateState<'a> {
                 frame_context,
                 data_stores,
             );
-        }
-    }
-
-    /// Process the picture tree again in a depth-first order,
-    /// and adjust the raster roots of the pictures that want to establish
-    /// their own roots but are not able to due to the size constraints.
-    fn assign_raster_roots(
-        &mut self,
-        pic_index: PictureIndex,
-        picture_primitives: &[PicturePrimitive],
-        fallback_raster_spatial_node: SpatialNodeIndex,
-    ) {
-        let picture = &picture_primitives[pic_index.0];
-        if !picture.is_visible() {
-            return
-        }
-
-        let new_fallback = match picture.raster_config {
-            Some(ref config) => {
-                let surface = &mut self.surfaces[config.surface_index.0];
-                if !config.establishes_raster_root {
-                    surface.raster_spatial_node_index = fallback_raster_spatial_node;
-                }
-                surface.raster_spatial_node_index
-            }
-            None => fallback_raster_spatial_node,
-        };
-
-        for cluster in &picture.prim_list.clusters {
-            if !cluster.flags.contains(ClusterFlags::IS_PICTURE) {
-                continue;
-            }
-            for instance in &picture.prim_list.prim_instances[cluster.prim_range()] {
-                let child_pic_index = match instance.kind {
-                    PrimitiveInstanceKind::Picture { pic_index, .. } => pic_index,
-                    _ => unreachable!(),
-                };
-                self.assign_raster_roots(
-                    child_pic_index,
-                    picture_primitives,
-                    new_fallback,
-                );
-            }
         }
     }
 }
@@ -6168,16 +6115,6 @@ impl PicturePrimitive {
             // Pop this surface from the stack
             let surface_index = state.pop_surface();
             debug_assert_eq!(surface_index, raster_config.surface_index);
-
-            // Check if any of the surfaces can't be rasterized in local space but want to.
-            if raster_config.establishes_raster_root
-                && (surface_rect.size.width > MAX_SURFACE_SIZE
-                    || surface_rect.size.height > MAX_SURFACE_SIZE)
-                && frame_context.debug_flags.contains(DebugFlags::DISABLE_RASTER_ROOT_SCALING)
-            {
-                raster_config.establishes_raster_root = false;
-                state.are_raster_roots_assigned = false;
-            }
 
             // Set the estimated and precise local rects. The precise local rect
             // may be changed again during frame visibility.
