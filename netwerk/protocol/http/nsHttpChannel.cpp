072b03084c5edd8a239446be868020de14b82000
Ryan VanderMeulen <ryanvm@gmail.com>
2020-09-08 15:09:34 -0400

    Backed out 3 changesets (bug 1497249) for causing topcrash bug 1663718.
    Backed out changeset 1cd6d2f9a603 (bug 1497249)
    Backed out changeset 2502449c2e24 (bug 1497249)
    Backed out changeset 432c7e9dc19f (bug 1497249)

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -8037,13 +8037,6 @@ nsresult nsHttpChannel::ContinueOnStopRequestAfterAuthRetry(
                         mResponseHead->Status() == 200;
 
   if (upgradeWebsocket || upgradeConnect) {
-    // TODO: Support connection upgrade for socket process in bug 1632809.
-    if (nsIOService::UseSocketProcess() && upgradeConnect) {
-      Unused << mUpgradeProtocolCallback->OnUpgradeFailed(
-          NS_ERROR_NOT_IMPLEMENTED);
-      return ContinueOnStopRequest(aStatus, aIsFromNet, aContentComplete);
-    }
-
     nsresult rv = gHttpHandler->CompleteUpgrade(aTransWithStickyConn,
                                                 mUpgradeProtocolCallback);
     if (NS_FAILED(rv)) {
