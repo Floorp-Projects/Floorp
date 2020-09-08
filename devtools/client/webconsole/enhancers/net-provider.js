1c08496ade5b281f23b6eeb61e72730bdf6345bf
Daisuke Akatsuka <daisuke@birchill.co.jp>
2020-09-08 01:07:51 +0000

    Bug 1661622: See newState instead of old state. r=bomsy
    Differential Revision: https://phabricator.services.mozilla.com/D89359

--- devtools/client/webconsole/enhancers/net-provider.js
+++ devtools/client/webconsole/enhancers/net-provider.js
@@ -65,9 +65,9 @@ function enableNetProvider(webConsoleUI) {
       // to fetch data from the backend.
       if (type == NETWORK_MESSAGE_UPDATE) {
         const { actor } = action.message;
-        const open = getAllMessagesUiById(state).includes(actor);
+        const open = getAllMessagesUiById(newState).includes(actor);
         if (open) {
-          const message = getMessage(state, actor);
+          const message = getMessage(newState, actor);
           message.updates.forEach(updateType => {
             dataProvider.onNetworkResourceUpdated({ ...message, updateType });
           });
