cf0a0ddd7b02bec454ef568f736b6fd3886f2e05
Mitchell Hentges <mhentges@mozilla.com>
2020-09-09 22:28:16 +0000

    Bug 1651424: Bump glean version to 32.3.1 r=firefox-build-system-reviewers,rstewart
    32.3.0 was missing Python wheels for macOS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89641

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -1551,7 +1551,7 @@ class CreateMachEnvironment(MachCommandBase):
             # This can fail on some platforms. See
             # https://bugzilla.mozilla.org/show_bug.cgi?id=1660120
             try:
-                manager.install_pip_package('glean_sdk~=32.3.0')
+                manager.install_pip_package('glean_sdk~=32.3.1')
             except subprocess.CalledProcessError:
                 print('Could not install glean_sdk, so telemetry will not be '
                       'collected. Continuing.')
