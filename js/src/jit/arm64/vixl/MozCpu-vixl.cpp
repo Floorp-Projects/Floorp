674bf663ee0a1cb6f853c5cc6307d0b752f048ff
Benjamin Bouvier <benj@benj.me>
2020-09-10 06:21:40 +0000

    Bug 1663895: aarch64: define the MEMBARRIER_CMD when they don't exist; r=lth
    Values taken from the Linux kernel source, and confirmed in Android source's
    code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89581

--- js/src/jit/arm64/vixl/MozCpu-vixl.cpp
+++ js/src/jit/arm64/vixl/MozCpu-vixl.cpp
@@ -52,6 +52,16 @@ static int membarrier(int cmd, int flags) {
     return syscall(__NR_membarrier, cmd, flags);
 }
 
+// These definitions come from the Linux kernel source, for kernels before 4.16
+// which didn't have access to these membarrier commands.
+#  ifndef MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE
+#  define MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE (1 << 5)
+#  endif
+
+#  ifndef MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE
+#  define MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE (1 << 6)
+#  endif
+
 #endif // __aarch64__
 
 namespace vixl {
