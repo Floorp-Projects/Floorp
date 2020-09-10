58c7254f4bf73c5c8ff9dbf4b532ad856e8e70b8
Ryan Hunt <rhunt@eqrion.net>
2020-09-10 06:08:45 +0000

    Bug 1663862 - Check OOM from wasm::ToString(). r=lth, a=RyanVM
    wasm::ToCString() couldn't OOM, but wasm::ToString() can, so the
    all the use cases need to check for OOM and propagate it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89671

--- js/src/wasm/WasmJS.cpp
+++ js/src/wasm/WasmJS.cpp
@@ -1162,6 +1162,10 @@ static JSString* FuncTypeToString(JSContext* cx, const FuncType& funcType) {
     }
 
     UniqueChars argStr = ToString(arg);
+    if (!argStr) {
+      return nullptr;
+    }
+
     if (!buf.append(argStr.get(), strlen(argStr.get()))) {
       return nullptr;
     }
@@ -1180,6 +1184,10 @@ static JSString* FuncTypeToString(JSContext* cx, const FuncType& funcType) {
     }
 
     UniqueChars resultStr = ToString(result);
+    if (!resultStr) {
+      return nullptr;
+    }
+
     if (!buf.append(resultStr.get(), strlen(resultStr.get()))) {
       return nullptr;
     }
