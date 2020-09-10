e84b1f270333b48efe58abd35a1f1420b7d79c62
Glenn Watson <git@intuitionlibrary.com>
2020-09-10 13:27:59 +0000

    Bug 1664084 - Improve glyph rasterization worker thread usage. r=nical
    Previously, any time a text run was requested where the number of
    glyphs to rasterize was < 8, WR would rasterize those glyphs on
    the main backend thread.
    
    However, this pattern (along with skipping glyphs in the text run
    that are already cached) meant that we will often see frames where
    almost all of the glyph raster work gets done on the main thread,
    even when there are a large number of glyphs to rasterize.
    
    This patch introduces a batching system. Any time a batch of 8
    glyphs for a given font instance are queued, they are dispatched
    to a worker thread. At the end of the glyph request phase, any
    pending glyphs are also sent to worker threads, if there is a
    significant amount of work to do, or done on the main thread if
    there are only a few glyphs left.
    
    During profiling of a number of common pages, this appears to drop
    the mean and median frame building time by a small amount, but drop
    the worst case frame building times by significant amounts (often
    several ms).
    
    Differential Revision: https://phabricator.services.mozilla.com/D89692

--- gfx/wr/webrender/src/glyph_rasterizer/mod.rs
+++ gfx/wr/webrender/src/glyph_rasterizer/mod.rs
@@ -13,6 +13,7 @@ use crate::platform::font::FontContext;
 use crate::device::TextureFilter;
 use crate::gpu_types::UvRectKind;
 use crate::glyph_cache::{GlyphCache, CachedGlyphInfo, GlyphCacheEntry};
+use crate::internal_types::FastHashMap;
 use crate::resource_cache::CachedImageData;
 use crate::texture_cache::{TextureCache, TextureCacheHandle, Eviction};
 use crate::gpu_cache::GpuCache;
@@ -24,6 +25,7 @@ use rayon::ThreadPool;
 use rayon::prelude::*;
 use euclid::approxeq::ApproxEq;
 use euclid::size2;
+use smallvec::SmallVec;
 use std::cmp;
 use std::cell::Cell;
 use std::hash::{Hash, Hasher};
@@ -74,7 +76,6 @@ impl GlyphRasterizer {
                 .lock_shared_context()
                 .has_font(&font.font_key)
         );
-        let mut new_glyphs = Vec::new();
 
         let glyph_key_cache = glyph_cache.get_glyph_key_cache_for_font_mut(font.clone());
 
@@ -95,26 +96,58 @@ impl GlyphRasterizer {
                     GlyphCacheEntry::Blank | GlyphCacheEntry::Pending => continue,
                 }
             }
-            new_glyphs.push(key.clone());
-            glyph_key_cache.add_glyph(key.clone(), GlyphCacheEntry::Pending);
+
+            // Increment the total number of glyphs that are pending. This is used to determine
+            // later whether to use worker threads for the remaining glyphs during resolve time.
+            self.pending_glyph_count += 1;
+
+            // Find a batch container for the font instance for this glyph. Use get_mut to avoid
+            // cloning the font instance, since this is the common path.
+            match self.pending_glyph_requests.get_mut(&font) {
+                Some(container) => {
+                    container.push(*key);
+
+                    // If the batch for this font instance is big enough, kick off an async
+                    // job to start rasterizing these glyphs on other threads now.
+                    if container.len() == 8 {
+                        let glyphs = mem::replace(container, SmallVec::new());
+                        self.flush_glyph_requests(
+                            font.clone(),
+                            glyphs,
+                            true,
+                        );
+                    }
+                }
+                None => {
+                    // If no batch exists for this font instance, add the glyph to a new one.
+                    self.pending_glyph_requests.insert(
+                        font.clone(),
+                        smallvec![*key],
+                    );
+                }
+            }
+
+            glyph_key_cache.add_glyph(*key, GlyphCacheEntry::Pending);
         }
-
-        if new_glyphs.is_empty() {
-            return;
-        }
-
-        self.pending_glyphs += 1;
-
-        self.request_glyphs_from_backend(font, new_glyphs);
     }
 
     pub fn enable_multithreading(&mut self, enable: bool) {
         self.enable_multithreading = enable;
     }
 
-    pub(in super) fn request_glyphs_from_backend(&mut self, font: FontInstance, glyphs: Vec<GlyphKey>) {
+    /// Internal method to flush a list of glyph requests to a set of worker threads,
+    /// or process on this thread if there isn't much work to do (in which case the
+    /// overhead of processing these on a thread is unlikely to be a performance win).
+    fn flush_glyph_requests(
+        &mut self,
+        font: FontInstance,
+        glyphs: SmallVec<[GlyphKey; 16]>,
+        use_workers: bool,
+    ) {
         let font_contexts = Arc::clone(&self.font_contexts);
         let glyph_tx = self.glyph_tx.clone();
+        self.pending_glyph_jobs += 1;
+        self.pending_glyph_count -= glyphs.len();
 
         fn process_glyph(key: &GlyphKey, font_contexts: &FontContexts, font: &FontInstance) -> GlyphRasterJob {
             profile_scope!("glyph-raster");
@@ -158,12 +191,7 @@ impl GlyphRasterizer {
 
         // if the number of glyphs is small, do it inline to avoid the threading overhead;
         // send the result into glyph_tx so downstream code can't tell the difference.
-        if !self.enable_multithreading || glyphs.len() < 8 {
-            let jobs = glyphs.iter()
-                             .map(|key: &GlyphKey| process_glyph(key, &font_contexts, &font))
-                             .collect();
-            glyph_tx.send(GlyphRasterJobs { font, jobs }).unwrap();
-        } else {
+        if self.enable_multithreading && use_workers {
             // spawn an async task to get off of the render backend thread as early as
             // possible and in that task use rayon's fork join dispatch to rasterize the
             // glyphs in the thread pool.
@@ -176,6 +204,11 @@ impl GlyphRasterizer {
 
                 glyph_tx.send(GlyphRasterJobs { font, jobs }).unwrap();
             });
+        } else {
+            let jobs = glyphs.iter()
+                             .map(|key: &GlyphKey| process_glyph(key, &font_contexts, &font))
+                             .collect();
+            glyph_tx.send(GlyphRasterJobs { font, jobs }).unwrap();
         }
     }
 
@@ -188,10 +221,30 @@ impl GlyphRasterizer {
         _: &mut RenderTaskGraph,
         _: &mut TextureCacheProfileCounters,
     ) {
+        // Work around the borrow checker, since we call flush_glyph_requests below
+        let mut pending_glyph_requests = mem::replace(
+            &mut self.pending_glyph_requests,
+            FastHashMap::default(),
+        );
+        // If we have a large amount of remaining work to do, spawn to worker threads,
+        // even if that work is shared among a number of different font instances.
+        let use_workers = self.pending_glyph_count >= 8;
+        for (font, pending_glyphs) in pending_glyph_requests.drain() {
+            self.flush_glyph_requests(
+                font,
+                pending_glyphs,
+                use_workers,
+            );
+        }
+        // Restore this so that we don't heap allocate next frame
+        self.pending_glyph_requests = pending_glyph_requests;
+        debug_assert_eq!(self.pending_glyph_count, 0);
+        debug_assert!(self.pending_glyph_requests.is_empty());
+
         profile_scope!("resolve_glyphs");
         // Pull rasterized glyphs from the queue and update the caches.
-        while self.pending_glyphs > 0 {
-            self.pending_glyphs -= 1;
+        while self.pending_glyph_jobs > 0 {
+            self.pending_glyph_jobs -= 1;
 
             // TODO: rather than blocking until all pending glyphs are available
             // we could try_recv and steal work from the thread pool to take advantage
@@ -641,7 +694,7 @@ impl Into<f64> for SubpixelOffset {
     }
 }
 
-#[derive(Clone, Hash, PartialEq, Eq, Debug, Ord, PartialOrd)]
+#[derive(Copy, Clone, Hash, PartialEq, Eq, Debug, Ord, PartialOrd)]
 #[cfg_attr(feature = "capture", derive(Serialize))]
 #[cfg_attr(feature = "replay", derive(Deserialize))]
 pub struct GlyphKey(u32);
@@ -877,19 +930,17 @@ pub struct GlyphRasterizer {
     workers: Arc<ThreadPool>,
     font_contexts: Arc<FontContexts>,
 
-    // Maintain a set of glyphs that have been requested this
-    // frame. This ensures the glyph thread won't rasterize
-    // the same glyph more than once in a frame. This is required
-    // because the glyph cache hash table is not updated
-    // until the end of the frame when we wait for glyph requests
-    // to be resolved.
-    #[allow(dead_code)]
-    pending_glyphs: usize,
+    /// The current number of individual glyphs waiting in pending batches.
+    pending_glyph_count: usize,
+
+    /// The current number of glyph request jobs that have been kicked to worker threads.
+    pending_glyph_jobs: usize,
+
+    /// A map of current glyph request batches.
+    pending_glyph_requests: FastHashMap<FontInstance, SmallVec<[GlyphKey; 16]>>,
 
     // Receives the rendered glyphs.
-    #[allow(dead_code)]
     glyph_rx: Receiver<GlyphRasterJobs>,
-    #[allow(dead_code)]
     glyph_tx: Sender<GlyphRasterJobs>,
 
     // We defer removing fonts to the end of the frame so that:
@@ -899,9 +950,6 @@ pub struct GlyphRasterizer {
     fonts_to_remove: Vec<FontKey>,
     // Defer removal of font instances, as for fonts.
     font_instances_to_remove: Vec<FontInstance>,
-
-    #[allow(dead_code)]
-    next_gpu_glyph_cache_key: GpuGlyphCacheKey,
 
     // Whether to parallelize glyph rasterization with rayon.
     enable_multithreading: bool,
@@ -930,14 +978,15 @@ impl GlyphRasterizer {
 
         Ok(GlyphRasterizer {
             font_contexts: Arc::new(font_context),
-            pending_glyphs: 0,
+            pending_glyph_jobs: 0,
+            pending_glyph_count: 0,
             glyph_rx,
             glyph_tx,
             workers,
             fonts_to_remove: Vec::new(),
             font_instances_to_remove: Vec::new(),
-            next_gpu_glyph_cache_key: GpuGlyphCacheKey(0),
             enable_multithreading: true,
+            pending_glyph_requests: FastHashMap::default(),
         })
     }
 
@@ -1009,7 +1058,8 @@ impl GlyphRasterizer {
     #[cfg(feature = "replay")]
     pub fn reset(&mut self) {
         //TODO: any signals need to be sent to the workers?
-        self.pending_glyphs = 0;
+        self.pending_glyph_jobs = 0;
+        self.pending_glyph_count = 0;
         self.fonts_to_remove.clear();
         self.font_instances_to_remove.clear();
     }
