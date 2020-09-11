76af3bddae423361a98f6bc5d0933770e5006ad5
Ryan VanderMeulen <ryanvm@gmail.com>
2020-09-11 12:49:41 -0400

    Backed out changeset 65e6c73de900 (bug 1649807) for causing bug 1664405. r=emilio

--- netwerk/protocol/http/nsHttpResponseHead.cpp
+++ netwerk/protocol/http/nsHttpResponseHead.cpp
@@ -39,7 +39,6 @@ nsHttpResponseHead::nsHttpResponseHead(const nsHttpResponseHead& aOther)
   mContentLength = other.mContentLength;
   mContentType = other.mContentType;
   mContentCharset = other.mContentCharset;
-  mHasCacheControl = other.mHasCacheControl;
   mCacheControlPublic = other.mCacheControlPublic;
   mCacheControlPrivate = other.mCacheControlPrivate;
   mCacheControlNoStore = other.mCacheControlNoStore;
@@ -128,7 +127,7 @@ bool nsHttpResponseHead::NoStore() {
 
 bool nsHttpResponseHead::NoCache() {
   RecursiveMutexAutoLock monitor(mRecursiveMutex);
-  return NoCache_locked();
+  return (mCacheControlNoCache || mPragmaNoCache);
 }
 
 bool nsHttpResponseHead::Immutable() {
@@ -756,7 +755,7 @@ bool nsHttpResponseHead::MustValidate() {
 
   // The no-cache response header indicates that we must validate this
   // cached response before reusing.
-  if (NoCache_locked()) {
+  if (mCacheControlNoCache || mPragmaNoCache) {
     LOG(("Must validate since response contains 'no-cache' header\n"));
     return true;
   }
@@ -903,7 +902,6 @@ void nsHttpResponseHead::Reset() {
   mVersion = HttpVersion::v1_1;
   mStatus = 200;
   mContentLength = -1;
-  mHasCacheControl = false;
   mCacheControlPublic = false;
   mCacheControlPrivate = false;
   mCacheControlNoStore = false;
@@ -1007,7 +1005,6 @@ bool nsHttpResponseHead::operator==(const nsHttpResponseHead& aOther) const {
          mContentLength == aOther.mContentLength &&
          mContentType == aOther.mContentType &&
          mContentCharset == aOther.mContentCharset &&
-         mHasCacheControl == aOther.mHasCacheControl &&
          mCacheControlPublic == aOther.mCacheControlPublic &&
          mCacheControlPrivate == aOther.mCacheControlPrivate &&
          mCacheControlNoCache == aOther.mCacheControlNoCache &&
@@ -1119,7 +1116,6 @@ void nsHttpResponseHead::ParseVersion(const char* str) {
 void nsHttpResponseHead::ParseCacheControl(const char* val) {
   if (!(val && *val)) {
     // clear flags
-    mHasCacheControl = false;
     mCacheControlPublic = false;
     mCacheControlPrivate = false;
     mCacheControlNoCache = false;
@@ -1135,7 +1131,6 @@ void nsHttpResponseHead::ParseCacheControl(const char* val) {
   nsDependentCString cacheControlRequestHeader(val);
   CacheControlParser cacheControlRequest(cacheControlRequestHeader);
 
-  mHasCacheControl = true;
   mCacheControlPublic = cacheControlRequest.Public();
   mCacheControlPrivate = cacheControlRequest.Private();
   mCacheControlNoCache = cacheControlRequest.NoCache();
@@ -1156,10 +1151,11 @@ void nsHttpResponseHead::ParsePragma(const char* val) {
     return;
   }
 
-  // Although 'Pragma: no-cache' is not a standard HTTP response header (it's a
-  // request header), caching is inhibited when this header is present so as to
-  // match existing Navigator behavior.
-  mPragmaNoCache = nsHttp::FindToken(val, "no-cache", HTTP_HEADER_VALUE_SEPS);
+  // Although 'Pragma: no-cache' is not a standard HTTP response header (it's
+  // a request header), caching is inhibited when this header is present so
+  // as to match existing Navigator behavior.
+  if (nsHttp::FindToken(val, "no-cache", HTTP_HEADER_VALUE_SEPS))
+    mPragmaNoCache = true;
 }
 
 nsresult nsHttpResponseHead::VisitHeaders(
