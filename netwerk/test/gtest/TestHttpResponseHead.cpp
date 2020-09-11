eaa4c352d6dbc408e44bb68c72fd2a160eb2297e
Emilio Cobos Álvarez <emilio@crisal.io>
2020-09-11 17:17:07 +0000

    Bug 1664405 - Properly copy mHasCacheControl in nsHttpResponseHead::operator=. r=dragana,necko-reviewers
    Oof, I sometimes really hate C++ :/
    
    The new assert makes our tests go boom without the fix.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89940

--- netwerk/test/gtest/TestHttpResponseHead.cpp
+++ netwerk/test/gtest/TestHttpResponseHead.cpp
@@ -8,6 +8,35 @@
 namespace mozilla {
 namespace net {
 
+void AssertRoundTrips(const nsHttpResponseHead& aHead) {
+  {
+    // Assert it round-trips via IPC.
+    UniquePtr<IPC::Message> msg(new IPC::Message(MSG_ROUTING_NONE, 0));
+    IPC::ParamTraits<nsHttpResponseHead>::Write(msg.get(), aHead);
+
+    nsHttpResponseHead deserializedHead;
+    PickleIterator iter(*msg);
+    bool res = IPC::ParamTraits<mozilla::net::nsHttpResponseHead>::Read(
+        msg.get(), &iter, &deserializedHead);
+    ASSERT_TRUE(res);
+    ASSERT_EQ(aHead, deserializedHead);
+  }
+
+  {
+    // Assert it round-trips through copy-ctor.
+    nsHttpResponseHead copied(aHead);
+    ASSERT_EQ(aHead, copied);
+  }
+
+  {
+    // Assert it round-trips through operator=
+    nsHttpResponseHead copied;
+    copied = aHead;
+    ASSERT_EQ(aHead, copied);
+  }
+}
+
+
 TEST(TestHttpResponseHead, Bug1636930)
 {
   // Only create atom table when it's not already created.
@@ -15,30 +44,65 @@ TEST(TestHttpResponseHead, Bug1636930)
     Unused << nsHttp::CreateAtomTable();
   }
 
-  mozilla::UniquePtr<IPC::Message> msg(new IPC::Message(MSG_ROUTING_NONE, 0));
-  mozilla::net::nsHttpResponseHead origHead;
+  nsHttpResponseHead head;
 
-  origHead.ParseStatusLine("HTTP/1.1 200 OK"_ns);
-  Unused << origHead.ParseHeaderLine("content-type: text/plain"_ns);
-  Unused << origHead.ParseHeaderLine("etag: Just testing"_ns);
-  Unused << origHead.ParseHeaderLine("cache-control: max-age=99999"_ns);
-  Unused << origHead.ParseHeaderLine("accept-ranges: bytes"_ns);
-  Unused << origHead.ParseHeaderLine("content-length: 1408"_ns);
-  Unused << origHead.ParseHeaderLine("connection: close"_ns);
-  Unused << origHead.ParseHeaderLine("server: httpd.js"_ns);
-  Unused << origHead.ParseHeaderLine("date: Tue, 12 May 2020 09:24:23 GMT"_ns);
+  head.ParseStatusLine("HTTP/1.1 200 OK"_ns);
+  Unused << head.ParseHeaderLine("content-type: text/plain"_ns);
+  Unused << head.ParseHeaderLine("etag: Just testing"_ns);
+  Unused << head.ParseHeaderLine("cache-control: max-age=99999"_ns);
+  Unused << head.ParseHeaderLine("accept-ranges: bytes"_ns);
+  Unused << head.ParseHeaderLine("content-length: 1408"_ns);
+  Unused << head.ParseHeaderLine("connection: close"_ns);
+  Unused << head.ParseHeaderLine("server: httpd.js"_ns);
+  Unused << head.ParseHeaderLine("date: Tue, 12 May 2020 09:24:23 GMT"_ns);
 
-  IPC::ParamTraits<mozilla::net::nsHttpResponseHead>::Write(msg.get(),
-                                                            origHead);
+  AssertRoundTrips(head);
+}
 
-  mozilla::net::nsHttpResponseHead deserializedHead;
-  PickleIterator iter(*msg);
-  bool res = IPC::ParamTraits<mozilla::net::nsHttpResponseHead>::Read(
-      msg.get(), &iter, &deserializedHead);
-  ASSERT_EQ(res, true);
+TEST(TestHttpResponseHead, bug1649807)
+{
+  // Only create atom table when it's not already created.
+  if (!nsHttp::ResolveAtom("content-type")) {
+    Unused << nsHttp::CreateAtomTable();
+  }
 
-  bool equal = (origHead == deserializedHead);
-  ASSERT_EQ(equal, true);
+  nsHttpResponseHead head;
+
+  head.ParseStatusLine("HTTP/1.1 200 OK"_ns);
+  Unused << head.ParseHeaderLine("content-type: text/plain"_ns);
+  Unused << head.ParseHeaderLine("etag: Just testing"_ns);
+  Unused << head.ParseHeaderLine("cache-control: age=99999"_ns);
+  Unused << head.ParseHeaderLine("accept-ranges: bytes"_ns);
+  Unused << head.ParseHeaderLine("content-length: 1408"_ns);
+  Unused << head.ParseHeaderLine("connection: close"_ns);
+  Unused << head.ParseHeaderLine("server: httpd.js"_ns);
+  Unused << head.ParseHeaderLine("pragma: no-cache"_ns);
+  Unused << head.ParseHeaderLine("date: Tue, 12 May 2020 09:24:23 GMT"_ns);
+
+  ASSERT_FALSE(head.NoCache()) << "Cache-Control wins over Pragma: no-cache";
+  AssertRoundTrips(head);
+}
+
+TEST(TestHttpResponseHead, bug1660200)
+{
+  // Only create atom table when it's not already created.
+  if (!nsHttp::ResolveAtom("content-type")) {
+    Unused << nsHttp::CreateAtomTable();
+  }
+
+  nsHttpResponseHead head;
+
+  head.ParseStatusLine("HTTP/1.1 200 OK"_ns);
+  Unused << head.ParseHeaderLine("content-type: text/plain"_ns);
+  Unused << head.ParseHeaderLine("etag: Just testing"_ns);
+  Unused << head.ParseHeaderLine("cache-control: no-cache"_ns);
+  Unused << head.ParseHeaderLine("accept-ranges: bytes"_ns);
+  Unused << head.ParseHeaderLine("content-length: 1408"_ns);
+  Unused << head.ParseHeaderLine("connection: close"_ns);
+  Unused << head.ParseHeaderLine("server: httpd.js"_ns);
+  Unused << head.ParseHeaderLine("date: Tue, 12 May 2020 09:24:23 GMT"_ns);
+
+  AssertRoundTrips(head);
 }
 
 }  // namespace net
