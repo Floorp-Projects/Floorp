71828536b2e7b524afe2859cc894083f9dae5b44
Nicholas Nethercote <nnethercote@mozilla.com>
2020-09-11 06:22:37 +0000

    Bug 1664105 - Upgrade `ron` to 0.6.2. r=kvark
    Also update `base64` so that all webrender code is on the same version, as
    required by the `webrender-lint-tidy` check.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89698

--- third_party/rust/ron/src/lib.rs
+++ third_party/rust/ron/src/lib.rs
@@ -57,9 +57,19 @@ Serializing / Deserializing is as simple as calling `to_string` / `from_str`.
 
 !*/
 
+#![doc(html_root_url = "https://docs.rs/ron/0.6.0")]
+
 pub mod de;
 pub mod ser;
+
+pub mod error;
 pub mod value;
-pub use crate::value::Value;
+
+pub mod extensions;
+
+pub use de::{from_str, Deserializer};
+pub use error::{Error, Result};
+pub use ser::{to_string, Serializer};
+pub use value::{Map, Number, Value};
 
 mod parse;
