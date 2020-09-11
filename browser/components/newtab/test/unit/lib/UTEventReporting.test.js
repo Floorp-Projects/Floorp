bd8d55a51314c07bb404cb1aacc3c508488f2005
Punam Dahiya <punamdahiya@yahoo.com>
2020-09-11 16:35:04 +0000

    Bug 1659150 - Remove about:welcome trailhead, full page and ReturnToAMO templates r=perftest-reviewers,Mardak,mkaply,nanj,sparky
    Differential Revision: https://phabricator.services.mozilla.com/D89077

--- browser/components/newtab/test/unit/lib/UTEventReporting.test.js
+++ browser/components/newtab/test/unit/lib/UTEventReporting.test.js
@@ -1,8 +1,4 @@
-import {
-  UTSessionPing,
-  UTTrailheadEnrollPing,
-  UTUserEventPing,
-} from "test/schemas/pings";
+import { UTSessionPing, UTUserEventPing } from "test/schemas/pings";
 import { GlobalOverrider } from "test/unit/utils";
 import { UTEventReporting } from "lib/UTEventReporting.jsm";
 
@@ -49,21 +45,6 @@ const FAKE_SESSION_PING_UT = [
     page: "about:newtab",
   },
 ];
-const FAKE_TRAILHEAD_ENROLL_EVENT = {
-  experiment: "activity-stream-trailhead-firstrun-interrupts",
-  type: "as-firstrun",
-  branch: "supercharge",
-};
-const FAKE_TRAILHEAD_ENROLL_EVENT_UT = [
-  "activity_stream",
-  "enroll",
-  "preference_study",
-  "activity-stream-trailhead-firstrun-interrupts",
-  {
-    experimentType: "as-firstrun",
-    branch: "supercharge",
-  },
-];
 
 describe("UTEventReporting", () => {
   let globals;
@@ -106,19 +87,6 @@ describe("UTEventReporting", () => {
 
       let ping = global.Services.telemetry.recordEvent.firstCall.args;
       assert.validate(ping, UTSessionPing);
-    });
-  });
-
-  describe("#sendTrailheadEnrollEvent()", () => {
-    it("should queue up the correct data to send to Events Telemetry", async () => {
-      utEvents.sendTrailheadEnrollEvent(FAKE_TRAILHEAD_ENROLL_EVENT);
-      assert.calledWithExactly(
-        global.Services.telemetry.recordEvent,
-        ...FAKE_TRAILHEAD_ENROLL_EVENT_UT
-      );
-
-      let ping = global.Services.telemetry.recordEvent.firstCall.args;
-      assert.validate(ping, UTTrailheadEnrollPing);
     });
   });
 
