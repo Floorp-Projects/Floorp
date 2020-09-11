5cf6abc3bd1cc10b6f941d8970f07ffcb96be1db
Jonathan Watt <jwatt@jwatt.org>
2020-09-11 01:08:55 +0000

    Bug 1664332. Derive the prepopulated filename for save-to-PDF via the system print dialog from the document title. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D89838

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -164,6 +164,10 @@ var PrintEventHandler = {
           ? PrintUtils.getPrintSettings(this.viewSettings.defaultSystemPrinter)
           : this.settings.clone();
       settings.showPrintProgress = true;
+      // We set the title so that if the user chooses save-to-PDF from the
+      // system dialog the title will be used to generate the prepopulated
+      // filename in the file picker.
+      settings.title = this.previewBrowser.browsingContext.embedderElement.contentTitle;
       const PRINTPROMPTSVC = Cc[
         "@mozilla.org/embedcomp/printingprompt-service;1"
       ].getService(Ci.nsIPrintingPromptService);
