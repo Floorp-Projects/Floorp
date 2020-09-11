4f54e5403d0f7fdd4366c341b2b1b62616eea6de
Michael Comella <michael.l.comella@gmail.com>
2020-09-10 18:38:50 -0700

    [components] No issue: get gecko versions from current ac.

--- mobile/android/android-components/tools/get_cross_repo_hashes.py
+++ mobile/android/android-components/tools/get_cross_repo_hashes.py
@@ -6,7 +6,6 @@
 """See USAGE for details.
 
 Potential script improvements:
-- AUTOMATE: a-c check out -> GV versions
 - AUTOMATE: fenix check out -> a-c hash
 - (later) Support more than nightly
 - (later) Would this be more usable as a website?
@@ -14,17 +13,27 @@ Potential script improvements:
   from Fenix apk or other mechanism
 
 Fragile assumptions in implementation:
-- Hard-coded POM XML format
+- Hard-coded path: Gecko.kt
+- Hard-coded format: Gecko.kt
+- Hard-coded format: POM XML
 """
 
 import os
 import sys
+from enum import Enum
 from urllib.error import HTTPError
 from urllib.request import urlopen
 
 SCRIPT_NAME=os.path.basename(__file__)
-USAGE="""usage: ./{script_name} <gv-nightly-version>
-  <gv-nightly-version>: should look like 82.0.20200907094115
+SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
+
+USAGE="""usage: ./{script_name} [-h] [--help] [--gv-nightly <version>]
+  (no args): maps the currently checked out ac revision to a valid mc
+             revision.
+  --gv-nightly: disables default behavior and maps the given GV nightly version
+                to a valid mc version.
+  --help, -h: prints this information and exits.
+
 
 Purpose: when building across repositories, e.g. fenix with a local a-c,
 sometimes the build will fail with errors unrelated to your changes because of
@@ -39,31 +48,46 @@ to get valid hashes between the major repositories:
     build & install Fenix, hit 3-dot -> Settings -> About Firefox Nightly
     and read hash at the end of the "AC:" line
 
-2) a-c checkout -> GV Nightly version
-    Open buildSrc/src/main/java/Gecko.kt
-    and read `const val nightly_version = ...`
-
-3) GV Nightly version -> mozilla-central hash
+2) a-c checkout (current) -> GV Nightly version
     AUTOMATED: run this script
 
-In the future, we hope to automate steps 1 & 2 too.""".format(script_name=SCRIPT_NAME)
+3) GV nightly version -> mozilla-central hash
+    AUTOMATED: run this script
+    Use `--gv-nightly` to override with a custom version.
+
+In the future, we hope to automate step 1.""".format(script_name=SCRIPT_NAME)
 
 TEMPLATE_NIGHTLY_POM="https://maven.mozilla.org/maven2/org/mozilla/geckoview/geckoview-nightly-arm64-v8a/{version}/geckoview-nightly-arm64-v8a-{version}.pom"
-INDENT= '     '
+INDENT= '  '
+
+GV_VERSION_PATH=os.path.join('buildSrc', 'src', 'main', 'java', 'Gecko.kt')
+
+class Mode(Enum):
+    Default = 1
+    Nightly_version = 2
 
 def print_usage(exit=False):
     print(USAGE, file=sys.stderr)
     if exit: sys.exit(1)
 
 def maybe_display_usage():
-    if sys.argv[1] == '--help' or sys.argv[1] == '-h':
+    if len(sys.argv) >= 2 and (sys.argv[1] == '--help' or sys.argv[1] == '-h'):
         print_usage(exit=True)
 
-def validate_args(gv_nightly_version):
+def validate_args():
+    if len(sys.argv) <= 1:
+        return Mode.Default, None
+
+    if sys.argv[1] != '--gv-nightly':
+        raise Exception('unknown argument ' + sys.argv[1])
+
+    gv_nightly_version = sys.argv[2]
     major = gv_nightly_version[:gv_nightly_version.find('.')]
     if int(major) < 71:  # it may actually be some intermediate v70 that the hash was added.
         print('ERROR: GV versions below 71 do not have an m-c hash associated with their POM. Aborting...', file=sys.stderr)
         sys.exit(1)
+
+    return Mode.Nightly_version, gv_nightly_version
 
 def get_pom(gv_nightly_version):
     url = TEMPLATE_NIGHTLY_POM.format(version=gv_nightly_version)
@@ -91,14 +115,59 @@ def get_mc_hash_from_gv_nightly(gv_nightly_version):
     pom, debug_pom_url = get_pom(gv_nightly_version)
     return get_mc_hash_from_pom(pom, debug_pom_url)
 
+def get_gv_versions_from_ac_checkout(ac_root):
+    def get_version_from_line(line):
+        # Expected format: `const val nightly_version = "82.0.20200907094115"`
+        return line[line.find('"') + 1:line.rfind('"')]
+
+    release_version = None
+    beta_version = None
+    nightly_version = None
+
+    gv_version_path = os.path.join(ac_root, GV_VERSION_PATH)
+    with open(gv_version_path) as f:
+        for line in f:
+            stripped = line.strip()
+            if stripped.startswith('const val nightly_version = "'):
+                nightly_version = get_version_from_line(line)
+            elif stripped.startswith('const val beta_version = "'):
+                beta_version = get_version_from_line(line)
+            elif stripped.startswith('const val release_version = "'):
+                release_version = get_version_from_line(line)
+    return release_version, beta_version, nightly_version
+
+def main_mode_default():
+    # fenix checkout -> ac hash
+    print('fenix checkout -> ac hash')
+    print(INDENT + 'must be done manually. See --help for instructions')
+    print(INDENT + 'WARNING: if a different ac revision is found in fenix than')
+    print(INDENT + 'the currently checked out one, this script will likely need')
+    print(INDENT + 'to be rerun after the new revision is checked out')
+    print()
+
+    # ac checkout -> GV versions
+    gv_version_path = os.path.join(SCRIPT_DIR, '..')
+    releasev, betav, nightlyv = get_gv_versions_from_ac_checkout(gv_version_path)
+    print('ac checkout (current) -> GV versions')
+    print(INDENT + 'nightly: ' + nightlyv)
+    print(INDENT + 'beta:    ' + betav)
+    print(INDENT + 'release: ' + releasev)
+    print()
+
+    # GV Nightly -> mc hash
+    mc_hash = get_mc_hash_from_gv_nightly(nightlyv)
+    print('GV nightly version -> mc hash:')
+    print(INDENT + mc_hash.decode('utf-8'))
+
+def main_mode_nightly(gv_nightly_version):
+    mc_hash = get_mc_hash_from_gv_nightly(gv_nightly_version)
+    print(mc_hash.decode('utf-8'))
+
 def main():
     maybe_display_usage()
-    gv_nightly_version = sys.argv[1]
-    validate_args(gv_nightly_version)
-
-    mc_hash = get_mc_hash_from_gv_nightly(gv_nightly_version)
-    print('gv-nightly-version: ' + gv_nightly_version)
-    print('mozilla-central hash: ' + mc_hash.decode('utf-8'))
+    mode, gv_nightly_version = validate_args()
+    if mode == Mode.Default: main_mode_default()
+    elif mode == Mode.Nightly_version: main_mode_nightly(gv_nightly_version)
 
 if __name__ == '__main__':
     try:
