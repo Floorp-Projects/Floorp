475e72fb04d179769679541a5d241719f97aeb92
Drew Willcoxon <adw@mozilla.com>
2020-09-11 17:55:35 +0000

    Bug 1663025 - Fix Beta failure in browser/components/urlbar/tests/browser/browser_searchMode_autofill.js. r=harry
    This test requires search mode, but it doesn't enable update2, so it fails on
    non-Nightly.  It only needs to enable update2 like other search-mode tests do.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89850

--- browser/components/urlbar/tests/browser/browser_searchMode_autofill.js
+++ browser/components/urlbar/tests/browser/browser_searchMode_autofill.js
@@ -11,6 +11,15 @@
 const DEFAULT_ENGINE_NAME = "Test";
 
 add_task(async function setup() {
+  // This test requires update2.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["browser.urlbar.update2", true],
+      ["browser.urlbar.update2.localOneOffs", true],
+      ["browser.urlbar.update2.oneOffsRefresh", true],
+    ],
+  });
+
   for (let i = 0; i < 5; i++) {
     await PlacesTestUtils.addVisits([{ uri: "http://example.com/" }]);
   }
