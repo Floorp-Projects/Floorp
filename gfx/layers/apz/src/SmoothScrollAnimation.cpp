216859f173ed212934b2543940ac78ddf6c55c96
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-12 14:15:47 +0000

    Bug 1658169 - Add a SmoothScrollAnimation that matches the main-thread smooth scroll physics. r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D89666

---
+++ gfx/layers/apz/src/SmoothScrollAnimation.cpp
@@ -0,0 +1,29 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "SmoothScrollAnimation.h"
+#include "ScrollAnimationBezierPhysics.h"
+#include "mozilla/layers/APZPublicUtils.h"
+
+namespace mozilla {
+namespace layers {
+
+SmoothScrollAnimation::SmoothScrollAnimation(AsyncPanZoomController& aApzc,
+                                             const nsPoint& aInitialPosition,
+                                             ScrollOrigin aOrigin)
+    : GenericScrollAnimation(
+          aApzc, aInitialPosition,
+          apz::ComputeBezierAnimationSettingsForOrigin(aOrigin)),
+      mOrigin(aOrigin) {}
+
+SmoothScrollAnimation* SmoothScrollAnimation::AsSmoothScrollAnimation() {
+  return this;
+}
+
+ScrollOrigin SmoothScrollAnimation::GetScrollOrigin() const { return mOrigin; }
+
+}  // namespace layers
+}  // namespace mozilla
