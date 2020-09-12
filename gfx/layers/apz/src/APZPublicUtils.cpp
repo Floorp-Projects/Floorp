ac2b3ac160a964dc7bc78038688493789fd7aba7
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-12 14:08:10 +0000

    Bug 1658169 - Move the origin-based physics computation to APZPublicUtils. r=tnikkel
    This is just moving code to make it reusable in APZ. As part of this, I replace
    use of Preferences::* with the StaticPrefs::* API, because we can't use
    Preferences:: in the GPU process and we'll want to be able to invoke this code
    from there.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89665

--- gfx/layers/apz/src/APZPublicUtils.cpp
+++ gfx/layers/apz/src/APZPublicUtils.cpp
@@ -75,6 +75,65 @@ static int32_t GetNormalizedAppVersion() {
   return std::make_pair(minMS, maxMS);
 }
 
+ScrollAnimationBezierPhysicsSettings
+ComputeBezierAnimationSettingsForOrigin(ScrollOrigin aOrigin) {
+  int32_t minMS = 0;
+  int32_t maxMS = 0;
+  bool isOriginSmoothnessEnabled = false;
+
+#define READ_DURATIONS(prefbase) \
+  isOriginSmoothnessEnabled = StaticPrefs::general_smoothScroll_ ## prefbase (); \
+  if (isOriginSmoothnessEnabled) { \
+    minMS = StaticPrefs::general_smoothScroll_ ## prefbase ## _durationMinMS(); \
+    maxMS = StaticPrefs::general_smoothScroll_ ## prefbase ## _durationMaxMS(); \
+  }
+
+  switch (aOrigin) {
+    case ScrollOrigin::Pixels:
+      READ_DURATIONS(pixels)
+      break;
+    case ScrollOrigin::Lines:
+      READ_DURATIONS(lines)
+      break;
+    case ScrollOrigin::Pages:
+      READ_DURATIONS(pages)
+      break;
+    case ScrollOrigin::MouseWheel:
+      READ_DURATIONS(mouseWheel)
+      break;
+    case ScrollOrigin::Scrollbars:
+      READ_DURATIONS(scrollbars)
+      break;
+    default:
+      READ_DURATIONS(other)
+      break;
+  }
+
+#undef READ_DURATIONS
+
+  if (aOrigin == ScrollOrigin::MouseWheel && isOriginSmoothnessEnabled) {
+    std::tie(minMS, maxMS) = GetMouseWheelAnimationDurations();
+  }
+
+  if (isOriginSmoothnessEnabled) {
+    static const int32_t kSmoothScrollMaxAllowedAnimationDurationMS = 10000;
+    maxMS = clamped(maxMS, 0, kSmoothScrollMaxAllowedAnimationDurationMS);
+    minMS = clamped(minMS, 0, maxMS);
+  }
+
+  // Keep the animation duration longer than the average event intervals
+  // (to "connect" consecutive scroll animations before the scroll comes to a
+  // stop).
+  double intervalRatio =
+      ((double)StaticPrefs::general_smoothScroll_durationToIntervalRatio()) /
+      100.0;
+
+  // Duration should be at least as long as the intervals -> ratio is at least 1
+  intervalRatio = std::max(1.0, intervalRatio);
+
+  return ScrollAnimationBezierPhysicsSettings{minMS, maxMS, intervalRatio};
+}
+
 }  // namespace apz
 }  // namespace layers
 }  // namespace mozilla
