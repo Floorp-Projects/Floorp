b8d58beefa042ecdf8a32d80468dabe0684daea6
Noemi Erli <nerli@mozilla.com>
2020-09-12 22:56:18 +0300

    Backed out changeset 64eb902728bc (bug 1664156) for causing mochitest failures in test_meta_csp_self.html CLOSED TREE

--- dom/base/nsImageLoadingContent.cpp
+++ dom/base/nsImageLoadingContent.cpp
@@ -102,6 +102,7 @@ nsImageLoadingContent::nsImageLoadingContent()
       mLoading(false),
       // mBroken starts out true, since an image without a URI is broken....
       mBroken(true),
+      mSuppressed(false),
       mNewRequestsWillNeedAnimationReset(false),
       mUseUrgentStartForChannel(false),
       mLazyLoading(false),
@@ -1277,6 +1278,9 @@ EventStates nsImageLoadingContent::ImageState() const {
   if (mBroken) {
     states |= NS_EVENT_STATE_BROKEN;
   }
+  if (mSuppressed) {
+    states |= NS_EVENT_STATE_SUPPRESSED;
+  }
   if (mLoading) {
     states |= NS_EVENT_STATE_LOADING;
   }
@@ -1297,11 +1301,14 @@ void nsImageLoadingContent::UpdateImageState(bool aNotify) {
 
   nsIContent* thisContent = AsContent();
 
-  mLoading = mBroken = false;
+  mLoading = mBroken = mSuppressed = false;
 
-  // If we were blocked, we're broken, so are we if we don't have an image
-  // request at all or the image has errored.
-  if (mImageBlockingStatus != nsIContentPolicy::ACCEPT) {
+  // If we were blocked by server-based content policy, we claim to be
+  // suppressed.  If we were blocked by type-based content policy, we claim to
+  // be user-disabled.  Otherwise, claim to be broken.
+  if (mImageBlockingStatus == nsIContentPolicy::REJECT_SERVER) {
+    mSuppressed = true;
+  } else if (mImageBlockingStatus == nsIContentPolicy::REJECT_TYPE) {
     mBroken = true;
   } else if (!mCurrentRequest) {
     if (!mLazyLoading) {
