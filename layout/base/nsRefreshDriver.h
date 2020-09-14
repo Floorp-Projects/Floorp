c41ea25bf1c16d6893a19d60d3697a21acdc2c0a
Sean Feng <sefeng@mozilla.com>
2020-09-14 18:47:34 +0000

    Bug 1518999 - Update ContentfulPaint algorithm to follow the spec r=emilio
    This patch includes a couple of changes.
    1) Notify contentful paint only during refresh driver ticks.
    2) Not only the root document, sub document should also have their own
       contentful paint entry.
    3) Consider invisible text as contentful as well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89498

--- layout/base/nsRefreshDriver.h
+++ layout/base/nsRefreshDriver.h
@@ -79,7 +79,7 @@ class nsRefreshDriver final : public mozilla::layers::TransactionIdAllocator,
    * ensure that multiple animations started during the same event off
    * the main event loop have the same start time.)
    */
-  mozilla::TimeStamp MostRecentRefresh() const;
+  mozilla::TimeStamp MostRecentRefresh(bool aEnsureTimerStarted = true) const;
 
   /**
    * Add / remove refresh observers.
