eb3031c16476dacce85c3e28148a85db67a97773
Andrea Marchesini <amarchesini@mozilla.com>
2020-09-15 17:51:05 +0000

    Bug 444222 - Update the window.name when doing the navigation, r=smaug,nika
    This patch implements the window.name updating in the spec
    https://html.spec.whatwg.org/#history-traversal.
    
    Differential Revision: https://phabricator.services.mozilla.com/D81361

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -350,6 +350,18 @@ void CanonicalBrowsingContext::SessionHistoryCommit(uint64_t aLoadId,
       newActiveEntry->SetForInitialLoad(false);
       SessionHistoryEntry::RemoveLoadId(aLoadId);
       mLoadingEntries.RemoveElementAt(i);
+
+      // If there is a name in the new entry, clear the name of all contiguous
+      // entries. This is for https://html.spec.whatwg.org/#history-traversal
+      // Step 4.4.2.
+      nsAutoString nameOfNewEntry;
+      newActiveEntry->GetName(nameOfNewEntry);
+      if (!nameOfNewEntry.IsEmpty()) {
+        nsSHistory::WalkContiguousEntries(
+            newActiveEntry,
+            [](nsISHEntry* aEntry) { aEntry->SetName(EmptyString()); });
+      }
+
       if (IsTop()) {
         mActiveEntry = newActiveEntry;
         if (loadFromSessionHistory) {
