84875a09007cc24bca4c64ab1f4dd9b3c83384f5
Bob Owen <bobowencode@gmail.com>
2020-09-15 15:05:52 +0000

    Bug 1665007: Double the memory we allow for DeviceCapabilities arrays for safety. r=jfkthame
    This also doubles the memory we allocate for DEVMODEs, when that value has been
    returned from the other uses of DocumentProperties, because apparently some
    drivers do not return the correct size.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90251

--- widget/windows/nsPrintDialogUtil.cpp
+++ widget/windows/nsPrintDialogUtil.cpp
@@ -88,7 +88,10 @@ static nsReturnRef<nsHGLOBAL> CreateGlobalDevModeAndInit(
     return nsReturnRef<nsHGLOBAL>();
   }
 
-  // Allocate a buffer of the correct size.
+  // Some drivers do not return the correct size for their DEVMODE, so we
+  // over-allocate to try and compensate.
+  // (See https://bugzilla.mozilla.org/show_bug.cgi?id=1664530#c5)
+  needed *= 2;
   nsAutoDevMode newDevMode(
       (LPDEVMODEW)::HeapAlloc(::GetProcessHeap(), HEAP_ZERO_MEMORY, needed));
   if (!newDevMode) {
