2e9f3011e1ee25b29b628182a285ce29d1668dbf
Barret Rennie <barret@brennie.ca>
2020-09-15 17:43:10 +0000

    Bug 1663762 - Add probes to measure input event latency during load correlated with preload r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D90208

--- dom/ipc/WindowGlobalParent.cpp
+++ dom/ipc/WindowGlobalParent.cpp
@@ -657,6 +657,24 @@ WindowGlobalParent::RecvSubmitTimeToFirstInteractionPreloadTelemetry(
   return IPC_OK();
 }
 
+mozilla::ipc::IPCResult
+WindowGlobalParent::RecvSubmitLoadInputEventResponsePreloadTelemetry(
+    uint32_t aMillis) {
+  if (!IsTop()) {
+    return IPC_FAIL(this, "submit preload telemetry on non-toplevel document");
+  }
+
+  if (mDocumentTreeWouldPreloadResources) {
+    Telemetry::Accumulate(Telemetry::LOAD_INPUT_EVENT_RESPONSE_PRELOAD_MS,
+                          aMillis);
+  } else {
+    Telemetry::Accumulate(Telemetry::LOAD_INPUT_EVENT_RESPONSE_NO_PRELOAD_MS,
+                          aMillis);
+  }
+
+  return IPC_OK();
+}
+
 already_AddRefed<mozilla::dom::Promise> WindowGlobalParent::DrawSnapshot(
     const DOMRect* aRect, double aScale, const nsACString& aBackgroundColor,
     mozilla::ErrorResult& aRv) {
