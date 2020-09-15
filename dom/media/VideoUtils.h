81eb1d93f7a08ce2bbca9770a0255bbbefc206a4
Paul Adenot <paul@paul.cx>
2020-09-15 09:00:33 +0000

    Bug 1662805 - Extract the sample-rate decision for an AudioSink to its own function, in the same way it is done for the channel count. r=pehrsons
    Depends on D89633
    
    Differential Revision: https://phabricator.services.mozilla.com/D89634

--- dom/media/VideoUtils.h
+++ dom/media/VideoUtils.h
@@ -155,6 +155,10 @@ void DownmixStereoToMono(mozilla::AudioDataValue* aBuffer, uint32_t aFrames);
 // given AudioInfo and the prefs that are being set.
 uint32_t DecideAudioPlaybackChannels(const AudioInfo& info);
 
+// Decide the sample-rate to use for audio output according to the
+// given AudioInfo and the prefs that are being set.
+uint32_t DecideAudioPlaybackSampleRate(const AudioInfo& info);
+
 bool IsDefaultPlaybackDeviceMono();
 
 bool IsVideoContentType(const nsCString& aContentType);
