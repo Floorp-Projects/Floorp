f187c6d5cbf4c3662ac4f26f794d35284d34c3f3
Andreas Pehrson <apehrson@mozilla.com>
2020-09-15 11:56:35 +0000

    Bug 1656438 - Rid MockCubeb.h of `using namespace` lines. r=padenot
    Depends on D89768
    
    Differential Revision: https://phabricator.services.mozilla.com/D89769

--- dom/media/gtest/MockCubeb.h
+++ dom/media/gtest/MockCubeb.h
@@ -11,9 +11,7 @@
 #include <atomic>
 #include <chrono>
 
-using namespace std::chrono_literals;
-using namespace mozilla;
-
+namespace mozilla {
 const uint32_t NUM_OF_CHANNELS = 2;
 
 #ifdef LOG
@@ -719,5 +717,6 @@ void AddDevices(MockCubeb* mock, uint32_t device_count,
     mock->AddDevice(device);
   }
 }
+}  // namespace mozilla
 
 #endif  // MOCKCUBEB_H_
