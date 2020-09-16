30fe61a114c1ad3e2156bd7e414b343e38429498
Jan de Mooij <jdemooij@mozilla.com>
2020-09-16 17:00:38 +0000

    Bug 1664786 part 8 - Add TrialInliningState to the fallback stub. r=iain
    This patch adds an enum class to track the inlining state per fallback stub and
    uses it for the following:
    
    * Don't scan CacheIR of stubs we definitely can't inline (will matter more for getters/setters).
    * Similarly, optimize the check for trial-inlined stubs in WarpOracle.
    * Assert in WarpOracle that we didn't trial-inline a stub the transpiler doesn't support.
    * Ensure we never attempt a second trial-inlining at the same location.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90410

--- js/src/jit/BaselineIC.h
+++ js/src/jit/BaselineIC.h
@@ -715,6 +715,13 @@ class ICFallbackStub : public ICStub {
   void clearUsedByTranspiler() { state_.clearUsedByTranspiler(); }
   void setUsedByTranspiler() { state_.setUsedByTranspiler(); }
 
+  TrialInliningState trialInliningState() const {
+    return state_.trialInliningState();
+  }
+  void setTrialInliningState(TrialInliningState state) {
+    state_.setTrialInliningState(state);
+  }
+
   // If the transpiler optimized based on this IC, invalidate the script's Warp
   // code.
   void maybeInvalidateWarp(JSContext* cx, JSScript* script);
