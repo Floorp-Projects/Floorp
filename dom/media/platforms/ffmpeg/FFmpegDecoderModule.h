e492cd1424d47de7073d957bb8a7e7ab7b54680a
stransky <stransky@redhat.com>
2020-09-16 13:19:42 +0000

    Bug 1665324 Remove media.ffmpeg.low-latency.enabled preference, r=jya
    Differential Revision: https://phabricator.services.mozilla.com/D90373

--- dom/media/platforms/ffmpeg/FFmpegDecoderModule.h
+++ dom/media/platforms/ffmpeg/FFmpegDecoderModule.h
@@ -38,14 +38,6 @@ class FFmpegDecoderModule : public PlatformDecoderModule {
     if (aParams.VideoConfig().HasAlpha()) {
       return nullptr;
     }
-    if (VPXDecoder::IsVPX(aParams.mConfig.mMimeType) &&
-        aParams.mOptions.contains(CreateDecoderParams::Option::LowLatency) &&
-        !StaticPrefs::media_ffmpeg_low_latency_enabled()) {
-      // We refuse to create a decoder with low latency enabled if it's VP8 or
-      // VP9 unless specifically allowed: this will fallback to libvpx later.
-      // We do allow it for h264.
-      return nullptr;
-    }
     RefPtr<MediaDataDecoder> decoder = new FFmpegVideoDecoder<V>(
         mLib, aParams.VideoConfig(), aParams.mKnowsCompositor,
         aParams.mImageContainer,
