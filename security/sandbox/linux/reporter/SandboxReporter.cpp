1d6e226ddd6987f9cf55a1e7452e7694dbd5d1e8
Jed Davis <jld@mozilla.com>
2020-09-16 18:52:52 +0000

    Bug 1663550 - Add "arm" and "arm64" architecture names to Linux sandbox telemetry. r=gcp
    Differential Revision: https://phabricator.services.mozilla.com/D89995

--- security/sandbox/linux/reporter/SandboxReporter.cpp
+++ security/sandbox/linux/reporter/SandboxReporter.cpp
@@ -26,6 +26,10 @@
 #  define SANDBOX_ARCH_NAME "x86"
 #elif defined(__x86_64__)
 #  define SANDBOX_ARCH_NAME "amd64"
+#elif defined(__arm__)
+#  define SANDBOX_ARCH_NAME "arm"
+#elif defined(__aarch64__)
+#  define SANDBOX_ARCH_NAME "arm64"
 #else
 #  error "unrecognized architecture"
 #endif
