081d0f1d04cf0e5c1ad8188c307137a3023ba05e
Eitan Isaacson <eitan@monotonous.org>
2020-09-16 21:15:55 +0000

    Bug 1620324 - Build breakage followup. r=dluca. CLOSED TREE
    Differential Revision: https://phabricator.services.mozilla.com/D90474

--- accessible/mac/MOXWebAreaAccessible.mm
+++ accessible/mac/MOXWebAreaAccessible.mm
@@ -275,8 +275,6 @@ using namespace mozilla::a11y;
   } else {
     mStartElem = root;
   }
-  MOZ_ASSERT(!mStartElem.IsNull(),
-             "Performing search with null gecko accessible!");
 
   mWebArea = root;
 
