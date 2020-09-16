b08a88bc5846b92d18d19edcb14e9a87cb2acbe2
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-16 18:02:45 +0000

    Bug 1661117 - Follow-up to update standalone WR CI for osmesa-src bump. r=kvark
    This extracts an install-meson.sh helper script to install meson in both
    the wrench-deps task for Firefox CI and the taskcluster.yml in WebRender CI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90441

--- taskcluster/scripts/misc/wrench-deps-vendoring.sh
+++ taskcluster/scripts/misc/wrench-deps-vendoring.sh
@@ -7,7 +7,6 @@ set -x -e -v
 # these dependencies on every test job that uses `wrench`.
 
 UPLOAD_DIR=$HOME/artifacts
-MESON_VER=0.55.1
 
 cd $GECKO_PATH
 export PATH=$PATH:$MOZ_FETCHES_DIR/rustc/bin:$HOME/.cargo/bin
@@ -22,13 +21,8 @@ mkdir wrench-deps/cargo-apk
 # an unpublished version.
 cargo install --path $MOZ_FETCHES_DIR/android-rs-glue/cargo-apk --root wrench-deps/cargo-apk cargo-apk
 
-curl -L https://github.com/mesonbuild/meson/releases/download/$MESON_VER/meson-${MESON_VER}.tar.gz -o meson.tar.gz
-tar -xf meson.tar.gz
-mv meson-${MESON_VER} wrench-deps/meson
-pushd wrench-deps/meson
-mkdir bin
-ln -s ../meson.py bin/meson
-popd
+ci-scripts/install-meson.sh
+mv meson wrench-deps/meson
 
 mkdir -p $UPLOAD_DIR
 tar caf $UPLOAD_DIR/wrench-deps.tar.bz2 wrench-deps
