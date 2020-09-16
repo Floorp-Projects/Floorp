47fdc23738a253847a77e98957081eb54945c5af
Michael Comella <michael.l.comella@gmail.com>
2020-09-16 12:47:21 -0700

    [components] No issue: update get_cross_repo_hashes script name from user feedback.

--- mobile/android/android-components/tools/test_list_compatible_dependency_versions.py
+++ mobile/android/android-components/tools/test_list_compatible_dependency_versions.py
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import get_cross_repo_hashes as module
+import list_compatible_dependency_versions as module
 import os
 import unittest
 
