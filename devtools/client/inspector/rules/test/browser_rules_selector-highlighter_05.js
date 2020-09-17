147b4a952e6d4cd26a6c5ead91674d190844be6a
Razvan Caliman <rcaliman@mozilla.com>
2020-09-17 14:00:38 +0000

    Bug 1572662 - Update tests for SelectorHighlighter r=jdescottes
    Depends on D90248
    
    This patch combines two test helpers into one: removes `getRuleViewSelectorHighlighterIcon()` and folds its logic into `clickSelectorIcon()`.
    The other callsites for `getRuleViewSelectorHighlighterIcon()` are removed in D90250.
    
    The augmented `clickSelectorIcon()` will toggle the selector highlighter accordingly (on or off for the given selector depending on its current state). It returns a promise that resolves with the event data from either the "highlighter-hidden" or "highlighter-shown" events emitted by the selector highlighter. Tests check this data for the expected results.
    
    Tests no longer use mocks for selector highlighter. They test the real thing.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90249

--- devtools/client/inspector/rules/test/browser_rules_selector-highlighter_05.js
+++ devtools/client/inspector/rules/test/browser_rules_selector-highlighter_05.js
@@ -6,9 +6,6 @@ http://creativecommons.org/publicdomain/zero/1.0/ */
 // Test that the selector highlighter is correctly shown when clicking on a
 // inherited element
 
-// Note that in this test, we mock the highlighter front, merely testing the
-// behavior of the style-inspector UI for now
-
 const TEST_URI = `
 <div style="cursor:pointer">
   A
@@ -21,49 +18,28 @@ const TEST_URI = `
 add_task(async function() {
   await addTab("data:text/html;charset=utf-8," + encodeURIComponent(TEST_URI));
   const { inspector, view } = await openRuleView();
-
-  // Mock the highlighter front to get the reference of the NodeFront
-  const HighlighterFront = {
-    isShown: false,
-    nodeFront: null,
-    options: null,
-    show: function(nodeFront, options) {
-      this.nodeFront = nodeFront;
-      this.options = options;
-      this.isShown = true;
-    },
-    hide: function() {
-      this.nodeFront = null;
-      this.options = null;
-      this.isShown = false;
-    },
-  };
-  // Inject the mock highlighter in the rule-view
-  view.selectorHighlighter = HighlighterFront;
+  let data;
 
   info("Checking that the right NodeFront reference and options are passed");
   await selectNode("a", inspector);
 
-  let icon = await getRuleViewSelectorHighlighterIcon(view, "element");
-  await clickSelectorIcon(icon, view);
+  data = await clickSelectorIcon(view, "element");
   is(
-    HighlighterFront.options.selector,
+    data.options.selector,
     "body > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)",
     "The right selector option is passed to the highlighter (1)"
   );
 
-  icon = await getRuleViewSelectorHighlighterIcon(view, "element", 1);
-  await clickSelectorIcon(icon, view);
+  data = await clickSelectorIcon(view, "element", 1);
   is(
-    HighlighterFront.options.selector,
+    data.options.selector,
     "body > div:nth-child(1) > div:nth-child(1)",
     "The right selector option is passed to the highlighter (1)"
   );
 
-  icon = await getRuleViewSelectorHighlighterIcon(view, "element", 2);
-  await clickSelectorIcon(icon, view);
+  data = await clickSelectorIcon(view, "element", 2);
   is(
-    HighlighterFront.options.selector,
+    data.options.selector,
     "body > div:nth-child(1)",
     "The right selector option is passed to the highlighter (1)"
   );
