16237b8115e245689bfd840b16324d65a142ef46
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-09-17 11:26:12 +0300

    Backed out changeset 0b196026ed59 (bug 1643689) for causing issues with manifest scheduling.
    CLOSED TREE

--- taskcluster/taskgraph/decision.py
+++ taskcluster/taskgraph/decision.py
@@ -76,7 +76,6 @@ PER_PROJECT_PARAMETERS = {
     'autoland': {
         'optimize_strategies': 'taskgraph.optimize:project.autoland',
         'target_tasks_method': 'autoland_tasks',
-        'test_manifest_loader': 'bugbug',  # Remove this line to disable "manifest scheduling".
     },
 
     'mozilla-central': {
