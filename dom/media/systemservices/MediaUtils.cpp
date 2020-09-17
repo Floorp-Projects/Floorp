7d3150041c892736357356b4828db3f706686afe
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-09-17 06:57:58 +0000

    Bug 1653060 - P3. Use nsCOMPtr. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D90486

--- dom/media/systemservices/MediaUtils.cpp
+++ dom/media/systemservices/MediaUtils.cpp
@@ -9,11 +9,11 @@
 namespace mozilla {
 namespace media {
 
-RefPtr<nsIAsyncShutdownClient> GetShutdownBarrier() {
+nsCOMPtr<nsIAsyncShutdownClient> GetShutdownBarrier() {
   nsCOMPtr<nsIAsyncShutdownService> svc = services::GetAsyncShutdownService();
   MOZ_RELEASE_ASSERT(svc);
 
-  RefPtr<nsIAsyncShutdownClient> barrier;
+  nsCOMPtr<nsIAsyncShutdownClient> barrier;
   nsresult rv = svc->GetProfileBeforeChange(getter_AddRefs(barrier));
   if (!barrier) {
     // We are probably in a content process. We need to do cleanup at
