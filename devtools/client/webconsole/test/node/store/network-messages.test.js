441c5f02b93627f2c2754989971465dcdfb61dec
Alexandre Poirot <poirot.alex@gmail.com>
2020-09-17 16:39:27 +0000

    Bug 1665305 - Process "message network updates" in console with only one `networkMessageUpdate` action call. r=jdescottes
    This help modifying redux state only once and prevent many uncesarry copies of the whole messages/network events Maps.
    This will also later help getting rid of WebConsoleWrapper throttling in favor of upcoming ResourceWatcher one,
    as we could call only one action from onResourceAvailable/Updated.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90367

--- devtools/client/webconsole/test/node/store/network-messages.test.js
+++ devtools/client/webconsole/test/node/store/network-messages.test.js
@@ -37,7 +37,7 @@ describe("Network message reducer:", () => {
     packet.actor = "message1";
     updatePacket.actor = "message1";
     dispatch(actions.messagesAdd([packet]));
-    dispatch(actions.networkMessageUpdate(updatePacket, null));
+    dispatch(actions.networkMessageUpdates([updatePacket], null));
   });
 
   describe("networkMessagesUpdateById", () => {
