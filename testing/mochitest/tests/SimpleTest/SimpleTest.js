97df5a1c5242a90fe39be9161c6abc03d06f6361
Kris Maglione <maglione.k@gmail.com>
2020-09-18 01:38:59 +0000

    Bug 1661513: Fix reporting of expected failures in fail-if tests. r=ahal
    Without this change, failures in tests marked with `fail-if` are reported as
    passes, though with messages and stacks that describe the failure. That works
    as far as the automation machinery is concerned, but it is extremely confusing
    to people who are trying to reproduce and fix the failures locally.
    
    This change fixes the reporting so that they show up as expected failures in
    logs, but have the same effect on final test status.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90616

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -425,7 +425,6 @@ SimpleTest.record = function(condition, name, diag, stack, expected) {
   if (SimpleTest.expected == "fail") {
     if (!test.result) {
       SimpleTest.num_failed++;
-      test.result = !test.result;
     }
     successInfo = {
       status: "PASS",
