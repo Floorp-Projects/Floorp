b94bb3f69d7277ba8b05c5a5b65d4bea6ec45794
Peter Van der Beken <peterv@propagandism.org>
2020-09-20 12:40:10 +0000

    Bug 1664656 - Decide whether to add a new entry based on the load type. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D90028

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -100,7 +100,8 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   UniquePtr<LoadingSessionHistoryInfo> CreateLoadingSessionHistoryEntryForLoad(
       nsDocShellLoadState* aLoadState, nsIChannel* aChannel);
-  void SessionHistoryCommit(uint64_t aLoadId, const nsID& aChangeID);
+  void SessionHistoryCommit(uint64_t aLoadId, const nsID& aChangeID,
+                            uint32_t aLoadType);
 
   // Calls the session history listeners' OnHistoryReload, storing the result in
   // aCanReload. If aCanReload is set to true and we have an active or a loading
