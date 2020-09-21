1f2f43b04742282b300b24df135a2437f56d2e80
Valentin Gosu <valentin.gosu@gmail.com>
2020-09-21 07:35:53 +0000

    Bug 1663642 - fix r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D90070

--- netwerk/base/nsStandardURL.cpp
+++ netwerk/base/nsStandardURL.cpp
@@ -1550,6 +1550,9 @@ nsresult nsStandardURL::SetSpecWithEncoding(const nsACString& input,
 
   // parse the given URL...
   nsresult rv = ParseURL(spec, specLength);
+  if (mScheme.mLen <= 0) {
+    rv = NS_ERROR_MALFORMED_URI;
+  }
   if (NS_SUCCEEDED(rv)) {
     // finally, use the URLSegment member variables to build a normalized
     // copy of |spec|
