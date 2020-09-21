b81e69ee95b0356c24197ffed8c52afbf600f81b
pbz <pbz@mozilla.com>
2020-09-21 11:54:50 +0000

    Bug 1314912 - Rate limit calls to History and Location interfaces. r=smaug
    This adds a rate limit to methods and setters of the History and Location
    for non-system callers.
    The rate limit is counted per BrowsingContext and can be controlled by prefs.
    
    This patch is based on the original rate limit patch by :freesamael.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90136

--- dom/base/nsHistory.h
+++ dom/base/nsHistory.h
@@ -42,14 +42,17 @@ class nsHistory final : public nsISupports, public nsWrapperCache {
                             mozilla::ErrorResult& aRv);
   void GetState(JSContext* aCx, JS::MutableHandle<JS::Value> aResult,
                 mozilla::ErrorResult& aRv) const;
-  void Go(int32_t aDelta, mozilla::ErrorResult& aRv);
-  void Back(mozilla::ErrorResult& aRv);
-  void Forward(mozilla::ErrorResult& aRv);
+  void Go(int32_t aDelta, mozilla::dom::CallerType aCallerType,
+          mozilla::ErrorResult& aRv);
+  void Back(mozilla::dom::CallerType aCallerType, mozilla::ErrorResult& aRv);
+  void Forward(mozilla::dom::CallerType aCallerType, mozilla::ErrorResult& aRv);
   void PushState(JSContext* aCx, JS::Handle<JS::Value> aData,
                  const nsAString& aTitle, const nsAString& aUrl,
+                 mozilla::dom::CallerType aCallerType,
                  mozilla::ErrorResult& aRv);
   void ReplaceState(JSContext* aCx, JS::Handle<JS::Value> aData,
                     const nsAString& aTitle, const nsAString& aUrl,
+                    mozilla::dom::CallerType aCallerType,
                     mozilla::ErrorResult& aRv);
 
  protected:
@@ -57,6 +60,7 @@ class nsHistory final : public nsISupports, public nsWrapperCache {
 
   void PushOrReplaceState(JSContext* aCx, JS::Handle<JS::Value> aData,
                           const nsAString& aTitle, const nsAString& aUrl,
+                          mozilla::dom::CallerType aCallerType,
                           mozilla::ErrorResult& aRv, bool aReplace);
 
   already_AddRefed<mozilla::dom::ChildSHistory> GetSessionHistory() const;
