1e4fc5fb1b21c33bad8fd80e0c5f293c5443c69a
julianwels <julianwels@mozilla.com>
2020-09-22 17:41:32 +0000

    Bug 1661275 - Cancel HTTPS-Only channel instead of DocumentLoadListener after timeout. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D90899

--- dom/security/nsHTTPSOnlyUtils.h
+++ dom/security/nsHTTPSOnlyUtils.h
@@ -144,6 +144,9 @@ class TestHTTPAnswerRunnable final : public mozilla::Runnable,
 
  private:
   RefPtr<nsIURI> mURI;
+  // We're keeping a reference to DocumentLoadListener instead of a specific
+  // channel, because the current top-level channel can change (for example
+  // through redirects)
   RefPtr<mozilla::net::DocumentLoadListener> mDocumentLoadListener;
   RefPtr<nsITimer> mTimer;
 };
