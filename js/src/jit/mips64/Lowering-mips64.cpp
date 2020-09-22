7c41607bc4cdcfb8676cdfba7b7fbfddeeb3a003
Andreea Pavel <apavel@mozilla.com>
2020-09-22 12:24:01 +0300

    Backed out 8 changesets (bug 1639153, bug 1664953) due to general instability on a CLOSED TREE
    Backed out changeset f999994ff312 (bug 1664953)
    Backed out changeset 09390cf1d667 (bug 1639153)
    Backed out changeset 83a3ed07220d (bug 1639153)
    Backed out changeset 659b24c3626c (bug 1639153)
    Backed out changeset 174a622bf334 (bug 1639153)
    Backed out changeset 444f4554ea94 (bug 1639153)
    Backed out changeset 93f3b8aa5bc5 (bug 1639153)
    Backed out changeset f1af63fb5986 (bug 1639153)

--- js/src/jit/mips64/Lowering-mips64.cpp
+++ js/src/jit/mips64/Lowering-mips64.cpp
@@ -43,10 +43,6 @@ void LIRGeneratorMIPS64::lowerDivI64(MDiv* div) {
   defineInt64(lir, div);
 }
 
-void LIRGeneratorMIPS64::lowerWasmBuiltinDivI64(MWasmBuiltinDivI64* div) {
-  MOZ_CRASH("We don't use runtime div for this architecture");
-}
-
 void LIRGeneratorMIPS64::lowerModI64(MMod* mod) {
   if (mod->isUnsigned()) {
     lowerUModI64(mod);
@@ -56,10 +52,6 @@ void LIRGeneratorMIPS64::lowerModI64(MMod* mod) {
   LDivOrModI64* lir = new (alloc())
       LDivOrModI64(useRegister(mod->lhs()), useRegister(mod->rhs()), temp());
   defineInt64(lir, mod);
-}
-
-void LIRGeneratorMIPS64::lowerWasmBuiltinModI64(MWasmBuiltinModI64* mod) {
-  MOZ_CRASH("We don't use runtime mod for this architecture");
 }
 
 void LIRGeneratorMIPS64::lowerUDivI64(MDiv* div) {
@@ -161,20 +153,10 @@ void LIRGenerator::visitWasmTruncateToInt64(MWasmTruncateToInt64* ins) {
   defineInt64(new (alloc()) LWasmTruncateToInt64(useRegister(opd)), ins);
 }
 
-void LIRGeneratorMIPS64::lowerWasmBuiltinTruncateToInt64(
-    MWasmBuiltinTruncateToInt64* ins) {
-  MOZ_CRASH("We don't use it for this architecture");
-}
-
 void LIRGenerator::visitInt64ToFloatingPoint(MInt64ToFloatingPoint* ins) {
   MDefinition* opd = ins->input();
   MOZ_ASSERT(opd->type() == MIRType::Int64);
   MOZ_ASSERT(IsFloatingPointType(ins->type()));
 
   define(new (alloc()) LInt64ToFloatingPoint(useInt64Register(opd)), ins);
-}
-
-void LIRGeneratorMIPS64::lowerBuiltinInt64ToFloatingPoint(
-    MBuiltinInt64ToFloatingPoint* ins) {
-  MOZ_CRASH("We don't use it for this architecture");
 }
