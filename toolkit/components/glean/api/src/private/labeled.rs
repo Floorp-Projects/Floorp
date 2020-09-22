1c973a1271aeec1fa3237577046b22f0c691aa09
Csoregi Natalia <ncsoregi@mozilla.com>
2020-09-22 23:06:06 +0300

    Backed out 5 changesets (bug 1659109) for bustage on src/dispatcher/mod.rs. CLOSED TREE
    Backed out changeset fbe0ea62f4bb (bug 1659109)
    Backed out changeset b3671a7196cf (bug 1659109)
    Backed out changeset 0ffc40c1046a (bug 1659109)
    Backed out changeset aa9157143036 (bug 1659109)
    Backed out changeset 32c3d23de6ea (bug 1659109)

--- toolkit/components/glean/api/src/private/labeled.rs
+++ toolkit/components/glean/api/src/private/labeled.rs
@@ -15,7 +15,6 @@ use crate::ipc::need_ipc;
 mod private {
     use super::{BooleanMetric, CommonMetricData, CounterMetric, StringMetric};
     use crate::ipc::need_ipc;
-    use std::sync::Arc;
 
     /// The sealed trait.
     ///
@@ -73,7 +72,7 @@ mod private {
 
         fn from_inner(metric: Self::Inner) -> Self {
             assert!(!need_ipc());
-            CounterMetric::Parent(Arc::new(crate::private::counter::CounterMetricImpl(metric)))
+            CounterMetric::Parent(crate::private::counter::CounterMetricImpl(metric))
         }
 
         fn new_inner(meta: CommonMetricData) -> Self::Inner {
