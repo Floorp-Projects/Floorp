bd154d5816a7ed894aa676d42035c7c24f734af5
Mirko Brodesser <mbrodesser@mozilla.com>
2020-09-22 16:03:55 +0000

    Bug 1649121: part 90) Declare some editor methods `const` or `static`. r=m_kato
    Simplifies reasoning about them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90688

--- editor/libeditor/HTMLEditorDataTransfer.cpp
+++ editor/libeditor/HTMLEditorDataTransfer.cpp
@@ -1062,7 +1062,8 @@ nsresult HTMLEditor::PrepareTransferable(nsITransferable** aTransferable) {
   return NS_OK;
 }
 
-nsresult HTMLEditor::PrepareHTMLTransferable(nsITransferable** aTransferable) {
+nsresult HTMLEditor::PrepareHTMLTransferable(
+    nsITransferable** aTransferable) const {
   MOZ_ASSERT(aTransferable);
   MOZ_ASSERT(!*aTransferable);
 
@@ -1913,6 +1914,7 @@ nsresult HTMLEditor::InsertFromDataTransfer(const DataTransfer* aDataTransfer,
   return NS_OK;
 }
 
+// static
 bool HTMLEditor::HavePrivateHTMLFlavor(nsIClipboard* aClipboard) {
   if (NS_WARN_IF(!aClipboard)) {
     return false;
