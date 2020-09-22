db8fa5945c9c1421b091fe8799f97397bd4b564e
Chris H-C <chutten@mozilla.com>
2020-09-22 15:22:03 +0000

    Bug 1664306 - FOG metrics should expire on version, not date. r=janerik
    There are three acceptable `expires` values for FOG metrics:
    `expired` - Used to manually mark a metric as expired.
    `never` - Used to mark permanent data collections.
    `"X"` - A string of digits representing the first major Firefox version where
    this metric will be expired. Like "88".
    
    Differential Revision: https://phabricator.services.mozilla.com/D90442

--- toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
+++ toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
@@ -4,6 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import re
 import rust
 import sys
 
@@ -11,7 +12,21 @@ from glean_parser import lint, parser, util
 from pathlib import Path
 
 
-def main(output_fd, metrics_index_path, which_array):
+def get_parser_options(moz_app_version):
+    app_version_major = moz_app_version.split('.', 1)[0]
+    return {
+        "allow_reserved": False,
+        "custom_is_expired":
+            lambda expires:
+                expires == "expired" or expires != "never" and int(
+                    expires) <= int(app_version_major),
+        "custom_validate_expires":
+            lambda expires:
+                expires in ("expired", "never") or re.fullmatch(r"\d\d+", expires, flags=re.ASCII),
+    }
+
+
+def main(output_fd, metrics_index_path, which_array, moz_app_version):
 
     # Source the list of input files from `metrics_index.py`
     sys.path.append(str(Path(metrics_index_path).parent))
@@ -25,10 +40,10 @@ def main(output_fd, metrics_index_path, which_array):
         sys.exit(1)
 
     # Derived heavily from glean_parser.translate.translate.
-    # Adapted to how mozbuild sends us a fd.
-    options = {"allow_reserved": False}
+    # Adapted to how mozbuild sends us a fd, and to expire on versions not dates.
     input_files = [Path(x) for x in input_files]
 
+    options = get_parser_options(moz_app_version)
     all_objs = parser.parse_objects(input_files, options)
     if util.report_validation_errors(all_objs):
         sys.exit(1)
