227392af307091f18b89d067041194ef5de0d052
Marco Bonardo <mbonardo@mozilla.com>
2020-09-23 08:26:06 +0000

    Bug 1664760 - Improve the set of results when in search mode for a non-general search engine. r=adw
    - always show search suggestions first in search mode
      - use restyleSearches only in search mode, to get cleaner history results and dedupe against search history
      - filter redirects differently depending on restyleSearches
    
    Differential Revision: https://phabricator.services.mozilla.com/D90719

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -350,11 +350,16 @@ pref("browser.urlbar.update2.localOneOffs", true);
 // Whether the urlbar one-offs act as search filters instead of executing a
 // search immediately.
 pref("browser.urlbar.update2.oneOffsRefresh", true);
+// Whether browsing history that is recognized as a previous search should
+// be restyled and deduped against form history. This only happens when
+// search mode is active.
+pref("browser.urlbar.update2.restyleBrowsingHistoryAsSearch", true);
 #else
 pref("browser.urlbar.update2", false);
 pref("browser.urlbar.update2.disableOneOffsHorizontalKeyNavigation", false);
 pref("browser.urlbar.update2.localOneOffs", false);
 pref("browser.urlbar.update2.oneOffsRefresh", false);
+pref("browser.urlbar.update2.restyleBrowsingHistoryAsSearch", false);
 #endif
 
 // Controls the empty search behavior in Search Mode:
