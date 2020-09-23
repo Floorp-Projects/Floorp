ec439df59f282503abf4fb9318e0aa34e1563cb6
Valentin Gosu <valentin.gosu@gmail.com>
2020-09-23 09:30:13 +0000

    Bug 1666711 - Improve TRRDNSListener handling r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D90980

--- netwerk/test/unit/test_trr_cname_chain.js
+++ netwerk/test/unit/test_trr_cname_chain.js
@@ -78,18 +78,12 @@ add_task(async function test_follow_cnames_same_response() {
       data: "1.2.3.4",
     },
   ]);
-  let [
-    inRequest,
-    inRecord,
-    inStatus,
-  ] = await new TRRDNSListener(
+  let [inRequest, inRecord, inStatus] = await new TRRDNSListener(
     "something.foo",
-    "1.2.3.4",
-    true,
-    undefined,
-    "",
-    false,
-    { flags: Ci.nsIDNSService.RESOLVE_CANONICAL_NAME }
+    {
+      expectedAnswer: "1.2.3.4",
+      flags: Ci.nsIDNSService.RESOLVE_CANONICAL_NAME,
+    }
   );
   equal(inRecord.QueryInterface(Ci.nsIDNSAddrRecord).canonicalName, "xyz.foo");
 
@@ -111,7 +105,7 @@ add_task(async function test_follow_cnames_same_response() {
       data: "2.3.4.5",
     },
   ]);
-  await new TRRDNSListener("a.foo", "2.3.4.5");
+  await new TRRDNSListener("a.foo", { expectedAnswer: "2.3.4.5" });
 
   await trrServer.stop();
 });
