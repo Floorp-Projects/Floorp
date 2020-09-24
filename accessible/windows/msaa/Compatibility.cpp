82e7b02c7d9678aaaa6114f5e93edb1f9d8de35b
James Teh <jteh@mozilla.com>
2020-09-24 05:13:15 +0000

    Bug 1666955 part 2: Detect dll shared by JAWS, ZoomText and ZoomText Fusion 2021 and later. r=MarcoZ
    Differential Revision: https://phabricator.services.mozilla.com/D91234

--- accessible/windows/msaa/Compatibility.cpp
+++ accessible/windows/msaa/Compatibility.cpp
@@ -27,9 +27,9 @@ using namespace mozilla::a11y;
  * String versions of consumer flags. See GetHumanReadableConsumersStr.
  */
 static const wchar_t* ConsumerStringMap[CONSUMERS_ENUM_LEN + 1] = {
-    L"NVDA",    L"JAWS",         L"OLDJAWS",  L"WE",       L"DOLPHIN",
-    L"SEROTEK", L"COBRA",        L"ZOOMTEXT", L"KAZAGURU", L"YOUDAO",
-    L"UNKNOWN", L"UIAUTOMATION", L"\0"};
+    L"NVDA",    L"JAWS",         L"OLDJAWS",       L"WE",       L"DOLPHIN",
+    L"SEROTEK", L"COBRA",        L"ZOOMTEXT",      L"KAZAGURU", L"YOUDAO",
+    L"UNKNOWN", L"UIAUTOMATION", L"VISPEROSHARED", L"\0"};
 
 bool Compatibility::IsModuleVersionLessThan(HMODULE aModuleHandle,
                                             unsigned long long aVersion) {
@@ -164,6 +164,10 @@ void Compatibility::InitConsumers() {
   if (::GetModuleHandleW(L"uiautomation") ||
       ::GetModuleHandleW(L"uiautomationcore"))
     sConsumers |= UIAUTOMATION;
+
+  if (::GetModuleHandleW(L"AccEventCache")) {
+    sConsumers |= VISPEROSHARED;
+  }
 
   // If we have a known consumer remove the unknown bit.
   if (sConsumers != Compatibility::UNKNOWN)
