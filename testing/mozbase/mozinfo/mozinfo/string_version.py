14e77d540cac212b58636d87f718baecbfe24b5c
Ben Campbell <ben@thunderbird.net>
2020-09-24 21:43:47 +0000

    Bug 1654915 - Make mozinfo version parsing tolerant of non-numeric version strings. r=gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D85691

--- testing/mozbase/mozinfo/mozinfo/string_version.py
+++ testing/mozbase/mozinfo/mozinfo/string_version.py
@@ -4,42 +4,67 @@
 
 from __future__ import absolute_import
 
-from distutils.version import LooseVersion
+import re
+import six
 
 
-class StringVersion(str):
+class StringVersion(six.text_type):
     """
     A string version that can be compared with comparison operators.
     """
 
+    # Pick out numeric and non-numeric parts (a match group for each type).
+    pat = re.compile(r'(\d+)|([^\d.]+)')
+
     def __init__(self, vstring):
         super(StringVersion, self).__init__()
-        self.version = LooseVersion(vstring)
+
+        # We'll use unicode internally.
+        # This check is mainly for python2 strings (which are bytes).
+        if isinstance(vstring, bytes):
+            vstring = vstring.decode("ascii")
+
+        self.vstring = vstring
+
+        # Store parts as strings to ease comparisons.
+        self.version = []
+        parts = self.pat.findall(vstring)
+        # Pad numeric parts with leading zeros for ordering.
+        for i, obj in enumerate(parts):
+            if obj[0]:
+                self.version.append(obj[0].zfill(8))
+            else:
+                self.version.append(obj[1])
+
+    def __str__(self):
+        return self.vstring
 
     def __repr__(self):
-        return "StringVersion ('%s')" % self
+        return "StringVersion ('%s')" % str(self)
 
-    def __to_version(self, other):
+    def _cmp(self, other):
         if not isinstance(other, StringVersion):
             other = StringVersion(other)
-        return other.version
 
-    # rich comparison methods
+        if self.version == other.version:
+            return 0
+        if self.version < other.version:
+            return -1
+        if self.version > other.version:
+            return 1
+
+    # operator overloads
+    def __eq__(self, other):
+        return self._cmp(other) == 0
 
     def __lt__(self, other):
-        return self.version < self.__to_version(other)
+        return self._cmp(other) < 0
 
     def __le__(self, other):
-        return self.version <= self.__to_version(other)
-
-    def __eq__(self, other):
-        return self.version == self.__to_version(other)
-
-    def __ne__(self, other):
-        return self.version != self.__to_version(other)
+        return self._cmp(other) <= 0
 
     def __gt__(self, other):
-        return self.version > self.__to_version(other)
+        return self._cmp(other) > 0
 
     def __ge__(self, other):
-        return self.version >= self.__to_version(other)
+        return self._cmp(other) >= 0
