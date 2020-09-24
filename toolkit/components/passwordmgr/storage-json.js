d4815c0f662bbb768beed0f3bfcf3fbc4bc48627
prathiksha <prathikshaprasadsuman@gmail.com>
2020-09-24 17:51:20 +0000

    Bug 1647934 - Clean up a user's logins backup when it may no longer be useful. r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D89078

--- toolkit/components/passwordmgr/storage-json.js
+++ toolkit/components/passwordmgr/storage-json.js
@@ -35,6 +35,11 @@ XPCOMUtils.defineLazyServiceGetter(
   "nsIUUIDGenerator"
 );
 
+XPCOMUtils.defineLazyModuleGetters(this, {
+  FXA_PWDMGR_HOST: "resource://gre/modules/FxAccountsCommon.js",
+  FXA_PWDMGR_REALM: "resource://gre/modules/FxAccountsCommon.js",
+});
+
 class LoginManagerStorage_json {
   constructor() {
     this.__crypto = null; // nsILoginManagerCrypto service
@@ -287,6 +292,7 @@ class LoginManagerStorage_json {
 
     LoginHelper.notifyStorageChanged("removeLogin", storedLogin);
     this._recordEntryPresent();
+    this._updateLoginsBackup();
   }
 
   modifyLogin(oldLogin, newLoginData) {
@@ -657,6 +663,7 @@ class LoginManagerStorage_json {
     this._store.saveSoon();
 
     LoginHelper.notifyStorageChanged("removeAllLogins", null);
+    this._updateLoginsBackup();
   }
 
   findLogins(origin, formActionOrigin, httpRealm) {
@@ -841,6 +848,47 @@ class LoginManagerStorage_json {
       "signon.usage.hasEntry",
       !!this._store.data.logins.length
     );
+  }
+
+  // Delete logins backup file if the last saved login was removed using
+  // removeLogin() or if all logins were removed at once using removeAllLogins().
+  // Note that if the user has a fxa key stored as a login, we just update the
+  // backup to only store the key when the last saved user facing login is removed
+  // using removeLogin().
+  async _updateLoginsBackup() {
+    // If we are not maintaining a backup, return early.
+    if (!this._store._options.backupTo) {
+      return;
+    }
+
+    const logins = this._store.data.logins;
+    // Return early if more than one login is stored because the backup won't need
+    // updating in this case.
+    if (logins.length > 1) {
+      return;
+    }
+
+    // If one login is stored and it's a fxa key, we update the backup to store the fxa
+    // key only.
+    if (
+      logins.length &&
+      logins[0].hostname == FXA_PWDMGR_HOST &&
+      logins[0].httpRealm == FXA_PWDMGR_REALM
+    ) {
+      try {
+        await OS.File.copy(this._store.path, this._store._options.backupTo);
+      } catch (ex) {
+        Cu.reportError(ex);
+      }
+    } else if (!logins.length) {
+      // If no logins are stored anymore, delete backup.
+      await OS.File.remove(this._store._options.backupTo, {
+        ignoreAbsent: true,
+      });
+    }
+
+    // This notification is specifically sent out for a test.
+    Services.obs.notifyObservers(null, "logins-backup-updated");
   }
 }
 
