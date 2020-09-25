0547b825402f12414077c3633f8cd22de2f216b0
Frederic Wang <fred.wang@free.fr>
2020-09-25 10:52:38 +0000

    Bug 1667122 - Make CSS tests work when math-style is disabled. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D91387

--- browser/base/content/test/static/browser_parsable_css.js
+++ browser/base/content/test/static/browser_parsable_css.js
@@ -61,6 +61,15 @@ let whitelist = [
   },
 ];
 
+if (!Services.prefs.getBoolPref("layout.css.math-style.enabled")) {
+  // mathml.css UA sheet rule for math-style.
+  whitelist.push({
+    sourceName: /(?:res|gre-resources)\/mathml\.css$/i,
+    errorMessage: /Unknown property .*\bmath-style\b/i,
+    isFromDevTools: false,
+  });
+}
+
 if (
   !Services.prefs.getBoolPref(
     "layout.css.xul-box-display-values.content.enabled"
