7592295080625914dd592d908d98442574a62dc8
Valentin Gosu <valentin.gosu@gmail.com>
2020-09-25 07:56:50 +0000

    Bug 1667085 - Move SendHello out of Http2Session constructor r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D91276

--- netwerk/protocol/http/ASpdySession.cpp
+++ netwerk/protocol/http/ASpdySession.cpp
@@ -36,7 +36,7 @@ ASpdySession* ASpdySession::NewSpdySession(net::SpdyVersion version,
   // from a list provided in the SERVER HELLO filtered by our acceptable
   // versions, so there is no risk of the server ignoring our prefs.
 
-  return new Http2Session(aTransport, version, attemptingEarlyData);
+  return Http2Session::CreateSession(aTransport, version, attemptingEarlyData);
 }
 
 SpdyInformation::SpdyInformation() {
