3f160fefb44bf94ce6cab006427737d695bec663
Emilio Cobos Álvarez <emilio@crisal.io>
2020-09-25 13:39:01 +0000

    Bug 1667342 - Make the "deferred window close" keep making sense after bug 1636728. r=smaug
    This was only being checked on OnDonePrinting() which isn't called in
    the original docshell. Move it to the window because we don't need to
    care about document viewers getting closed during print operations,
    they're top level browsers that don't run script.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91416

--- toolkit/components/printing/tests/browser_window_print.js
+++ toolkit/components/printing/tests/browser_window_print.js
@@ -8,7 +8,7 @@ const TEST_PATH = getRootDirectory(gTestPath).replace(
   "https://example.com"
 );
 
-add_task(async function test() {
+add_task(async function test_print_blocks() {
   // window.print() only shows print preview when print.tab_modal.enabled is
   // true.
   await SpecialPowers.pushPrefEnv({
@@ -71,6 +71,47 @@ add_task(async function test() {
         ok(afterFirst, "Should be in the second print already");
         ok(afterSecond, "Shouldn't have blocked if we have mozPrintCallbacks");
       }
+    }
+  );
+});
+
+add_task(async function test_print_delayed_during_load() {
+  // window.print() only shows print preview when print.tab_modal.enabled is
+  // true.
+  await SpecialPowers.pushPrefEnv({
+    set: [["print.tab_modal.enabled", true]],
+  });
+
+  is(
+    document.querySelector(".printPreviewBrowser"),
+    null,
+    "There shouldn't be any print preview browser"
+  );
+
+  await BrowserTestUtils.withNewTab(
+    `${TEST_PATH}file_window_print_delayed_during_load.html`,
+    async function(browser) {
+      info(
+        "Waiting for the first window.print() to run and ensure we're showing the preview..."
+      );
+
+      await BrowserTestUtils.waitForCondition(
+        () => !!document.querySelector(".printPreviewBrowser")
+      );
+
+      // The print dialog is open, should be open after onload.
+      {
+        let duringLoad = await SpecialPowers.spawn(browser, [], () => {
+          return !!content.document.getElementById("added-during-load");
+        });
+        ok(duringLoad, "Print should've been delayed");
+      }
+
+      gBrowser.getTabDialogBox(browser).abortAllDialogs();
+
+      is(typeof browser.isConnected, "boolean");
+      await BrowserTestUtils.waitForCondition(() => !browser.isConnected);
+      ok(true, "Tab should've been closed after printing");
     }
   );
 });
