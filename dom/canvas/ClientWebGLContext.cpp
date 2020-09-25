fd1e86b487d39e25af10ff79dd418e2cc401f006
Csoregi Natalia <ncsoregi@mozilla.com>
2020-09-25 09:33:40 +0300

    Backed out changeset 3dcacf3ae5a1 (bug 1664905) for assertion failures on GfxInfoBase.cpp. CLOSED TREE

--- dom/canvas/ClientWebGLContext.cpp
+++ dom/canvas/ClientWebGLContext.cpp
@@ -13,7 +13,6 @@
 #include "mozilla/dom/WebGLContextEvent.h"
 #include "mozilla/dom/WorkerCommon.h"
 #include "mozilla/EnumeratedRange.h"
-#include "mozilla/gfx/gfxVars.h"
 #include "mozilla/ipc/Shmem.h"
 #include "mozilla/layers/CompositorBridgeChild.h"
 #include "mozilla/layers/ImageBridgeChild.h"
@@ -24,6 +23,7 @@
 #include "mozilla/ScopeExit.h"
 #include "mozilla/StaticPrefs_webgl.h"
 #include "nsContentUtils.h"
+#include "nsIGfxInfo.h"
 #include "TexUnpackBlob.h"
 #include "WebGLMethodDispatcher.h"
 #include "WebGLChild.h"
@@ -577,21 +577,6 @@ bool ClientWebGLContext::CreateHostContext(const uvec2& requestedSize) {
     if (StaticPrefs::webgl_disable_fail_if_major_performance_caveat()) {
       options.failIfMajorPerformanceCaveat = false;
     }
-
-    if (options.failIfMajorPerformanceCaveat) {
-      const auto backend = GetCompositorBackendType();
-      bool isCompositorSlow = false;
-      isCompositorSlow |= (backend == layers::LayersBackend::LAYERS_BASIC);
-      isCompositorSlow |= (backend == layers::LayersBackend::LAYERS_WR &&
-                           gfx::gfxVars::UseSoftwareWebRender());
-
-      if (isCompositorSlow) {
-        return Err(
-            "failIfMajorPerformanceCaveat: Compositor is not"
-            " hardware-accelerated.");
-      }
-    }
-
     const bool resistFingerprinting = ShouldResistFingerprinting();
 
     const auto& principal = GetCanvas()->NodePrincipal();
