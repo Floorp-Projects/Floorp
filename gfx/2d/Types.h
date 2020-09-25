9ef2ecf7fe90e4dafc41c4608f4f8919d6d1c95e
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-25 08:57:24 +0000

    Bug 1667176 - Replace AppendToString for DeviceColor with ostream operator. r=mattwoodrow
    Depends on D91337
    
    Differential Revision: https://phabricator.services.mozilla.com/D91338

--- gfx/2d/Types.h
+++ gfx/2d/Types.h
@@ -13,6 +13,7 @@
 #include "mozilla/MacroArgs.h"  // for MOZ_CONCAT
 #include "mozilla/TypedEnumBits.h"
 
+#include <iosfwd>  // for ostream
 #include <stddef.h>
 #include <stdint.h>
 
@@ -571,6 +572,9 @@ struct DeviceColor {
   bool operator!=(const DeviceColor& aColor) const {
     return !(*this == aColor);
   }
+
+  friend std::ostream& operator<<(std::ostream& aOut,
+                                  const DeviceColor& aColor);
 
   Float r, g, b, a;
 };
