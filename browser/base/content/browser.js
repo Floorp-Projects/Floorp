363e32f089167ed1605d3a2d0ff6f3f0ee5b8533
Razvan Maries <rmaries@mozilla.com>
2020-09-25 06:03:12 +0300

    Backed out changeset 2b2e72856dc1 (bug 1665237) for perma failures on browser_popupNode.js. CLOSED TREE

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2098,11 +2098,9 @@ var gBrowserInit = {
           child => !("toplevel_name" in child)
         );
         if (children.length) {
-          let managedBookmarksButton = document.createXULElement(
-            "toolbarbutton"
+          let managedBookmarksButton = document.getElementById(
+            "managed-bookmarks"
           );
-          managedBookmarksButton.setAttribute("id", "managed-bookmarks");
-          managedBookmarksButton.setAttribute("class", "bookmark-item");
           let toplevel = managedBookmarks.find(
             element => "toplevel_name" in element
           );
@@ -2111,58 +2109,9 @@ var gBrowserInit = {
               "label",
               toplevel.toplevel_name
             );
-          } else {
-            managedBookmarksButton.setAttribute(
-              "data-l10n-id",
-              "managed-bookmarks"
-            );
+            managedBookmarksButton.removeAttribute("data-l10n-id");
           }
-          managedBookmarksButton.setAttribute("context", "placesContext");
-          managedBookmarksButton.setAttribute("container", "true");
-          managedBookmarksButton.setAttribute("removable", "false");
-          managedBookmarksButton.setAttribute("type", "menu");
-
-          let managedBookmarksPopup = document.createXULElement("menupopup");
-          managedBookmarksPopup.setAttribute("id", "managed-bookmarks-popup");
-          managedBookmarksPopup.setAttribute(
-            "oncommand",
-            "PlacesToolbarHelper.openManagedBookmark(event);"
-          );
-          managedBookmarksPopup.setAttribute(
-            "onclick",
-            "checkForMiddleClick(this, event);"
-          );
-          managedBookmarksPopup.setAttribute(
-            "ondragover",
-            "event.dataTransfer.effectAllowed='none';"
-          );
-          managedBookmarksPopup.setAttribute(
-            "ondragstart",
-            "PlacesToolbarHelper.onDragStartManaged(event);"
-          );
-          managedBookmarksPopup.setAttribute(
-            "onpopupshowing",
-            "PlacesToolbarHelper.populateManagedBookmarks(this);"
-          );
-          managedBookmarksPopup.setAttribute("placespopup", "true");
-          managedBookmarksPopup.setAttribute("is", "places-popup");
-          managedBookmarksButton.appendChild(managedBookmarksPopup);
-
-          gNavToolbox.palette.appendChild(managedBookmarksButton);
-
-          CustomizableUI.ensureWidgetPlacedInWindow(
-            "managed-bookmarks",
-            window
-          );
-
-          // Add button if it doesn't exist
-          if (!CustomizableUI.getPlacementOfWidget("managed-bookmarks")) {
-            CustomizableUI.addWidgetToArea(
-              "managed-bookmarks",
-              CustomizableUI.AREA_BOOKMARKS,
-              0
-            );
-          }
+          managedBookmarksButton.hidden = false;
         }
       }
     }
