bcf21f65ce9ce595827a48f2ae16e492b82acc5f
mcarare <mihai.carare.dev@gmail.com>
2020-09-25 18:27:24 +0300

    [components] For https://github.com/mozilla-mobile/android-components/issues/8500: Add unit tests for toMegabyteOrKilobyteString extension.

--- mobile/android/android-components/components/feature/downloads/src/test/java/mozilla/components/feature/downloads/DownloadDialogFragmentTest.kt
+++ mobile/android/android-components/components/feature/downloads/src/test/java/mozilla/components/feature/downloads/DownloadDialogFragmentTest.kt
@@ -6,12 +6,16 @@ package mozilla.components.feature.downloads
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import mozilla.components.browser.state.state.content.DownloadState
+import mozilla.components.feature.downloads.DownloadDialogFragment.Companion.BYTES_TO_MB_LIMIT
 import mozilla.components.feature.downloads.DownloadDialogFragment.Companion.KEY_FILE_NAME
+import mozilla.components.feature.downloads.DownloadDialogFragment.Companion.KILOBYTE
+import mozilla.components.feature.downloads.DownloadDialogFragment.Companion.MEGABYTE
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.math.roundToLong
 
 @RunWith(AndroidJUnit4::class)
 class DownloadDialogFragmentTest {
@@ -41,5 +45,19 @@ class DownloadDialogFragmentTest {
         assertEquals(fileName, download.fileName)
         assertEquals(url, download.url)
         assertEquals(contentLength, download.contentLength)
+    }
+
+    @Test
+    fun `extension function 'toMegabyteOrKilobyteString' returns MB string when size is or equal or bigger than the limit `() {
+        val size = (BYTES_TO_MB_LIMIT * MEGABYTE).roundToLong()
+        val expectedString = String.format("%.2f MB", size / MEGABYTE)
+        assertEquals(expectedString, size.toMegabyteOrKilobyteString())
+    }
+
+    @Test
+    fun `extension function 'toMegabyteOrKilobyteString' returns KB string when size is smaller than the limit `() {
+        val size = (BYTES_TO_MB_LIMIT * MEGABYTE).roundToLong() - 1
+        val expectedString = String.format("%.2f KB", size / KILOBYTE)
+        assertEquals(expectedString, size.toMegabyteOrKilobyteString())
     }
 }
