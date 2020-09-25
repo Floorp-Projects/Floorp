54792c79ab923003f3c9e398309959da61521e58
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-25 08:57:24 +0000

    Bug 1667176 - Remove the ViewID AppendToString function, and convert call sites to stream writes. r=mattwoodrow
    Depends on D91335
    
    Differential Revision: https://phabricator.services.mozilla.com/D91336

--- gfx/layers/LayersLogging.h
+++ gfx/layers/LayersLogging.h
@@ -34,9 +34,6 @@ enum class ImageFormat;
 namespace layers {
 struct ZoomConstraints;
 
-void AppendToString(std::stringstream& aStream, ScrollableLayerGuid::ViewID n,
-                    const char* pfx = "", const char* sfx = "");
-
 void AppendToString(std::stringstream& aStream, const gfx::sRGBColor& c,
                     const char* pfx = "", const char* sfx = "");
 
