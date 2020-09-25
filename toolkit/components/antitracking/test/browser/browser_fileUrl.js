f7254893475f31a4581a6d92b4b9015f80d283e9
Tim Huang <tihuang@mozilla.com>
2020-09-25 00:08:58 +0000

    Bug 1663192 - Part 1: Add a test to ensure the top-level window in a file url is considered as top-level. r=dimi
    Differential Revision: https://phabricator.services.mozilla.com/D91001

---
+++ toolkit/components/antitracking/test/browser/browser_fileUrl.js
@@ -0,0 +1,38 @@
+/**
+ * Bug 1663192 - Testing for ensuring the top-level window in a fire url is
+ *               treated as first-party.
+ */
+
+"use strict";
+
+add_task(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["network.cookie.cookieBehavior", 1]],
+  });
+});
+
+add_task(async function() {
+  let dir = getChromeDir(getResolvedURI(gTestPath));
+  dir.append("file_localStorage.html");
+  const uriString = Services.io.newFileURI(dir).spec;
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, uriString);
+
+  await SpecialPowers.spawn(tab.linkedBrowser, [], function() {
+    let result = content.document.getElementById("result");
+
+    is(
+      result.textContent,
+      "PASS",
+      "The localStorage is accessible in top-level window"
+    );
+
+    let loadInfo = content.docShell.currentDocumentChannel.loadInfo;
+
+    ok(
+      !loadInfo.isThirdPartyContextToTopWindow,
+      "The top-level window shouldn't be third-party"
+    );
+  });
+
+  BrowserTestUtils.removeTab(tab);
+});
