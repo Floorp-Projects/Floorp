07bb188d28df257a706d7c9c4fdb43864a191154
Miko Mynttinen <mikokm@gmail.com>
2020-09-25 09:38:06 +0000

    Bug 1619370 - Return the rect from BrowserChild::GetVisibleRect() in app units r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D91272

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -4030,13 +4030,10 @@ nsresult nsLayoutUtils::PaintFrame(gfxContext* aRenderingContext,
 
   // If we are in a remote browser, then apply clipping from ancestor browsers
   if (BrowserChild* browserChild = BrowserChild::GetFrom(presShell)) {
-    Maybe<LayoutDeviceIntRect> unscaledVisibleRect =
-        browserChild->GetVisibleRect();
+    Maybe<nsRect> unscaledVisibleRect = browserChild->GetVisibleRect();
+
     if (unscaledVisibleRect) {
-      CSSRect visibleRect =
-          *unscaledVisibleRect / presContext->CSSToDevPixelScale();
-      rootInkOverflow.IntersectRect(rootInkOverflow,
-                                    CSSPixel::ToAppUnits(visibleRect));
+      rootInkOverflow.IntersectRect(rootInkOverflow, *unscaledVisibleRect);
     }
   }
 
