addaa6b6211ce763d7e3eb78bc3290f6fb5df511
Tim Huang <tihuang@mozilla.com>
2020-09-25 00:08:56 +0000

    Bug 1663192 - Part 2: Set the IsThirdPartyToTopWindow in the nsBaseChannel. r=dimi,kershaw,necko-reviewers
    We should also set IsThirdPartyToTopWindow flag and HasStoragePermission
    in loadInfo for channels loading `file:` uri. So, we set this flag in the
    nsBaseChannel::AsyncOpen(). This change would affect about: and data:
    channels.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91002

--- toolkit/components/antitracking/AntiTrackingUtils.cpp
+++ toolkit/components/antitracking/AntiTrackingUtils.cpp
@@ -674,3 +674,51 @@ nsCString AntiTrackingUtils::GrantedReasonToString(
       return "stroage access API"_ns;
   }
 }
+
+/* static */
+void AntiTrackingUtils::UpdateAntiTrackingInfoForChannel(nsIChannel* aChannel) {
+  MOZ_ASSERT(aChannel);
+
+  if (!XRE_IsParentProcess()) {
+    return;
+  }
+
+  MOZ_DIAGNOSTIC_ASSERT(XRE_IsParentProcess());
+
+  nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
+
+  Unused << loadInfo->SetHasStoragePermission(
+      AntiTrackingUtils::HasStoragePermissionInParent(aChannel));
+
+  AntiTrackingUtils::ComputeIsThirdPartyToTopWindow(aChannel);
+
+  // We only update the IsOnContentBlockingAllowList flag and the partition key
+  // for the top-level http channel.
+  //
+  // The IsOnContentBlockingAllowList is only for http. For other types of
+  // channels, such as 'file:', there will be no interface to modify this. So,
+  // we only update it in http channels.
+  //
+  // The partition key is computed based on the site, so it's no point to set it
+  // for channels other than http channels.
+  nsCOMPtr<nsIHttpChannel> httpChannel = do_QueryInterface(aChannel);
+  if (!httpChannel || loadInfo->GetExternalContentPolicyType() !=
+                          nsIContentPolicy::TYPE_DOCUMENT) {
+    return;
+  }
+
+  nsCOMPtr<nsICookieJarSettings> cookieJarSettings;
+  Unused << loadInfo->GetCookieJarSettings(getter_AddRefs(cookieJarSettings));
+
+  // Update the IsOnContentBlockingAllowList flag in the CookieJarSettings
+  // if this is a top level loading. For sub-document loading, this flag
+  // would inherit from the parent.
+  net::CookieJarSettings::Cast(cookieJarSettings)
+      ->UpdateIsOnContentBlockingAllowList(aChannel);
+
+  // We only need to set FPD for top-level loads. FPD will automatically be
+  // propagated to non-top level loads via CookieJarSetting.
+  nsCOMPtr<nsIURI> uri;
+  Unused << aChannel->GetURI(getter_AddRefs(uri));
+  net::CookieJarSettings::Cast(cookieJarSettings)->SetPartitionKey(uri);
+}
