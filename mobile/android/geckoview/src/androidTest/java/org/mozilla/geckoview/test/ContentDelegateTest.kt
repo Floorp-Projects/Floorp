a58bfc2200bcb485e4a410f8e3f55e8d783f6492
owlishDeveloper <bugzeeeeee@gmail.com>
2020-09-25 15:27:42 +0000

    Bug 1666013 - Add convenience headers to download web response r=geckoview-reviewers,snorp
    Differential Revision: https://phabricator.services.mozilla.com/D90906

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentDelegateTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/ContentDelegateTest.kt
@@ -100,6 +100,10 @@ class ContentDelegateTest : BaseSessionTest() {
             override fun onExternalResponse(session: GeckoSession, response: WebResponse) {
                 assertThat("Uri should start with data:", response.uri, startsWith("blob:"))
                 assertThat("We should download the thing", String(response.body?.readBytes()!!), equalTo("Downloaded Data"))
+                // The headers below are special headers that we try to get for responses of any kind (http, blob, etc.)
+                assertThat("Content type should match", response.headers.get("content-type"), equalTo("text/plain"))
+                assertThat("Content length should be non-zero", response.headers.get("content-length")!!.toLong(), greaterThan(0L))
+                assertThat("Filename should match", response.headers.get("content-disposition"), equalTo("attachment; filename=\"download.txt\""))
             }
         })
     }
