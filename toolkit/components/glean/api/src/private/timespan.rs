2b34cdfd969e400157c46df2331916769c17b553
Bogdan Tara <btara@mozilla.com>
2020-09-28 21:13:41 +0300

    Backed out 4 changesets (bug 1662123, bug 1662122) for RustMozCrash raptor crashed CLOSED TREE
    Backed out changeset 832848e2277f (bug 1662123)
    Backed out changeset 4930629bf758 (bug 1662123)
    Backed out changeset a05a1b934110 (bug 1662122)
    Backed out changeset 47e17a661b8e (bug 1662122)

--- toolkit/components/glean/api/src/private/timespan.rs
+++ toolkit/components/glean/api/src/private/timespan.rs
@@ -2,138 +2,36 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use std::sync::{Arc, RwLock};
+use std::sync::RwLock;
 
-use super::{CommonMetricData, Instant, TimeUnit};
+use super::CommonMetricData;
 
-use crate::dispatcher;
-use crate::ipc::{need_ipc, with_ipc_payload, MetricId, TimespanCommand};
+use super::{Instant, TimeUnit};
 
 /// A timespan metric.
 ///
 /// Timespans are used to make a measurement of how much time is spent in a particular task.
 #[derive(Debug)]
-pub enum TimespanMetric {
-    Parent(Arc<TimespanMetricImpl>),
-    Child(TimespanMetricIpc),
-}
-
-#[derive(Debug)]
-pub struct TimespanMetricImpl {
+pub struct TimespanMetric {
     inner: RwLock<glean_core::metrics::TimespanMetric>,
 }
-#[derive(Debug)]
-pub struct TimespanMetricIpc(MetricId);
 
 impl TimespanMetric {
     /// Create a new timespan metric.
     pub fn new(meta: CommonMetricData, time_unit: TimeUnit) -> Self {
-        if need_ipc() {
-            TimespanMetric::Child(TimespanMetricIpc(MetricId::new(meta)))
-        } else {
-            TimespanMetric::Parent(Arc::new(TimespanMetricImpl::new(meta, time_unit)))
-        }
-    }
-
-    pub fn start(&self) {
-        let now = Instant::now();
-
-        match self {
-            TimespanMetric::Parent(p) => {
-                let metric = Arc::clone(&p);
-                dispatcher::launch(move || metric.start(now));
-            }
-            TimespanMetric::Child(c) => {
-                let cmd = TimespanCommand::Start(now);
-                with_ipc_payload(move |payload| {
-                    if let Some(v) = payload.timespans.get_mut(&c.0) {
-                        v.push(cmd);
-                    } else {
-                        let mut v = vec![];
-                        v.push(cmd);
-                        payload.timespans.insert(c.0.clone(), v);
-                    }
-                });
-            }
-        }
-    }
-
-    pub fn stop(&self) {
-        let now = Instant::now();
-
-        match self {
-            TimespanMetric::Parent(p) => {
-                let metric = Arc::clone(&p);
-                dispatcher::launch(move || metric.stop(now));
-            }
-            TimespanMetric::Child(c) => {
-                let cmd = TimespanCommand::Stop(now);
-                with_ipc_payload(move |payload| {
-                    if let Some(v) = payload.timespans.get_mut(&c.0) {
-                        v.push(cmd);
-                    } else {
-                        let mut v = vec![];
-                        v.push(cmd);
-                        payload.timespans.insert(c.0.clone(), v);
-                    }
-                });
-            }
-        }
-    }
-
-    pub fn cancel(&self) {
-        match self {
-            TimespanMetric::Parent(p) => {
-                let metric = Arc::clone(&p);
-                dispatcher::launch(move || metric.cancel());
-            }
-            TimespanMetric::Child(c) => {
-                let cmd = TimespanCommand::Cancel;
-                with_ipc_payload(move |payload| {
-                    if let Some(v) = payload.timespans.get_mut(&c.0) {
-                        v.push(cmd);
-                    } else {
-                        let mut v = vec![];
-                        v.push(cmd);
-                        payload.timespans.insert(c.0.clone(), v);
-                    }
-                });
-            }
-        }
-    }
-
-    /// **Test-only API (exported for FFI purposes).**
-    ///
-    /// Gets the currently stored value as an integer.
-    /// This doesn't clear the stored value.
-    ///
-    /// * `storage_name` - the storage name to look into.
-    ///
-    /// ## Return value
-    ///
-    /// Returns the stored value or `None` if nothing stored.
-    pub fn test_get_value(&self, storage_name: &str) -> Option<u64> {
-        match self {
-            TimespanMetric::Parent(p) => {
-                dispatcher::block_on_queue();
-                p.test_get_value(storage_name)
-            }
-            TimespanMetric::Child(_c) => panic!(
-                "Cannot get test value for {:?} in non-parent process!",
-                self
-            ),
-        }
-    }
-}
-
-impl TimespanMetricImpl {
-    fn new(meta: CommonMetricData, time_unit: TimeUnit) -> Self {
         let inner = RwLock::new(glean_core::metrics::TimespanMetric::new(meta, time_unit));
         Self { inner }
     }
 
-    fn start(&self, now: Instant) {
+    /// Start tracking time for the provided metric.
+    ///
+    /// This records an error if it's already tracking time (i.e. start was already
+    /// called with no corresponding `stop`): in that case the original
+    /// start time will be preserved.
+    pub fn start(&self) {
         crate::with_glean(move |glean| {
+            let now = Instant::now();
+
             let mut inner = self
                 .inner
                 .write()
@@ -142,8 +40,15 @@ impl TimespanMetricImpl {
         })
     }
 
-    fn stop(&self, now: Instant) {
+    /// Stop tracking time for the provided metric.
+    ///
+    /// Sets the metric to the elapsed time.
+    ///
+    /// This will record an error if no `start` was called.
+    pub fn stop(&self) {
         crate::with_glean(move |glean| {
+            let now = Instant::now();
+
             let mut inner = self
                 .inner
                 .write()
@@ -152,7 +57,10 @@ impl TimespanMetricImpl {
         })
     }
 
-    fn cancel(&self) {
+    /// Cancel a previous `start` call.
+    ///
+    /// No error is recorded if no `start` was called.
+    pub fn cancel(&self) {
         let mut inner = self
             .inner
             .write()
@@ -160,7 +68,19 @@ impl TimespanMetricImpl {
         inner.cancel()
     }
 
-    fn test_get_value(&self, storage_name: &str) -> Option<u64> {
+    /// **Test-only API.**
+    ///
+    /// Get the currently stored value as an integer.
+    /// This doesn't clear the stored value.
+    ///
+    /// ## Arguments
+    ///
+    /// * `storage_name` - the storage name to look into.
+    ///
+    /// ## Return value
+    ///
+    /// Returns the stored value or `None` if nothing stored.
+    pub fn test_get_value(&self, storage_name: &str) -> Option<u64> {
         crate::with_glean(move |glean| {
             let inner = self
                 .inner
--- toolkit/components/glean/api/tests/timespan.rs
+++ toolkit/components/glean/api/tests/timespan.rs
@@ -5,8 +5,7 @@
 mod common;
 use common::*;
 
-use fog::ipc;
-use fog::private::{CommonMetricData, CounterMetric, Lifetime, TimeUnit, TimespanMetric};
+use fog::private::{CommonMetricData, Lifetime, TimeUnit, TimespanMetric};
 
 #[test]
 fn smoke_test_timespan() {
@@ -33,54 +32,4 @@ fn smoke_test_timespan() {
     metric.cancel();
 
     assert_eq!(None, metric.test_get_value("store1"));
-}
-
-#[test]
-fn timespan_ipc() {
-    let _lock = lock_test();
-
-    let common = CommonMetricData {
-        name: "timespan_metric".into(),
-        category: "ipc".into(),
-        send_in_pings: vec!["store1".into()],
-        disabled: false,
-        lifetime: Lifetime::Ping,
-        ..Default::default()
-    };
-
-    // IPC enabled in the next block.
-    {
-        let _raii = ipc::test_set_need_ipc(true);
-
-        let child_metric = TimespanMetric::new(common.clone(), TimeUnit::Nanosecond);
-
-        // Instrumentation calls do not panic.
-        child_metric.start();
-        // Stopping right away might not give us data,
-        // if the underlying clock source is not precise enough.
-        // So let's cancel and make sure nothing blows up.
-        child_metric.cancel();
-
-        // (They also shouldn't do anything, we check the absence of data and errors below)
-
-        // Need to catch the panic so that our RAIIs drop nicely.
-        let result = std::panic::catch_unwind(move || {
-            child_metric.test_get_value("store1");
-        });
-        assert!(result.is_err());
-    }
-
-    // IPC disabled here, we can check that no data was recorded.
-    let metric = TimespanMetric::new(common, TimeUnit::Nanosecond);
-    assert_eq!(None, metric.test_get_value("store1"));
-
-    // TODO: Change this to use the error reporting test API.
-    let error_metric = CounterMetric::new(CommonMetricData {
-        name: "invalid_overflow/ipc.timespan_metric".into(),
-        category: "glean.error".into(),
-        lifetime: Lifetime::Ping,
-        send_in_pings: vec!["metrics".into()],
-        ..Default::default()
-    });
-    assert_eq!(None, error_metric.test_get_value("metrics"));
 }
