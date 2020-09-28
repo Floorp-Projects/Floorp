eac54a80028f7cb4c48981baf72db64b6b819f2a
Florian Quèze <florian@queze.net>
2020-09-28 15:12:14 +0000

    Bug 1667272 - Add profiler markers when Services are loaded, r=mstange.
    Differential Revision: https://phabricator.services.mozilla.com/D91459

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -1324,6 +1324,9 @@ nsresult nsComponentManagerImpl::GetServiceLocked(MutexLock& aLock,
   nsresult rv;
   {
     SafeMutexAutoUnlock unlock(mLock);
+    AUTO_PROFILER_MARKER_TEXT(
+        "GetService", OTHER.WithOptions(MarkerStack::Capture()),
+        nsDependentCString(nsIDToCString(aEntry.CID()).get()));
     rv = aEntry.CreateInstance(nullptr, aIID, getter_AddRefs(service));
   }
   if (NS_SUCCEEDED(rv) && !service) {
@@ -1501,6 +1504,8 @@ nsComponentManagerImpl::GetServiceByContractID(const char* aContractID,
     return NS_ERROR_UNEXPECTED;
   }
 
+  AUTO_PROFILER_LABEL_DYNAMIC_CSTR_NONSENSITIVE("GetServiceByContractID", OTHER,
+                                                aContractID);
   MutexLock lock(mLock);
 
   Maybe<EntryWrapper> entry =
