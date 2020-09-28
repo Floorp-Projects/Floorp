b2d14fc7853e6b44b0194f68b97171939b7e06ab
Morgan Reschenberg <mreschenberg@mozilla.com>
2020-09-28 22:32:07 +0000

    Bug 1667174: Create RotorRule base class to abstract checks duplicated across current rotor rules r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D91325

--- accessible/mac/RotorRules.mm
+++ accessible/mac/RotorRules.mm
@@ -12,54 +12,14 @@
 
 using namespace mozilla::a11y;
 
-// Role Rules
+// Generic Rotor Rule
 
-RotorHeadingRule::RotorHeadingRule() : PivotRoleRule(roles::HEADING) {}
-
-RotorHeadingRule::RotorHeadingRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::HEADING, aDirectDescendantsFrom) {}
-
-RotorArticleRule::RotorArticleRule() : PivotRoleRule(roles::ARTICLE) {}
-
-RotorArticleRule::RotorArticleRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::ARTICLE, aDirectDescendantsFrom) {}
-
-RotorTableRule::RotorTableRule() : PivotRoleRule(roles::TABLE) {}
-
-RotorTableRule::RotorTableRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::TABLE, aDirectDescendantsFrom) {}
-
-RotorLandmarkRule::RotorLandmarkRule() : PivotRoleRule(roles::LANDMARK) {}
-
-RotorLandmarkRule::RotorLandmarkRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::LANDMARK, aDirectDescendantsFrom) {}
-
-RotorListRule::RotorListRule() : PivotRoleRule(roles::LIST) {}
-
-RotorListRule::RotorListRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::LIST, aDirectDescendantsFrom) {}
-
-RotorButtonRule::RotorButtonRule() : PivotRoleRule(roles::PUSHBUTTON) {}
-
-RotorButtonRule::RotorButtonRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::PUSHBUTTON, aDirectDescendantsFrom) {}
-
-RotorFrameRule::RotorFrameRule() : PivotRoleRule(roles::DOCUMENT) {}
-
-RotorFrameRule::RotorFrameRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::DOCUMENT, aDirectDescendantsFrom) {}
-
-RotorImageRule::RotorImageRule() : PivotRoleRule(roles::GRAPHIC) {}
-
-RotorImageRule::RotorImageRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : PivotRoleRule(roles::GRAPHIC, aDirectDescendantsFrom) {}
-
-RotorControlRule::RotorControlRule(AccessibleOrProxy& aDirectDescendantsFrom)
+RotorRule::RotorRule(AccessibleOrProxy& aDirectDescendantsFrom)
     : mDirectDescendantsFrom(aDirectDescendantsFrom) {}
 
-RotorControlRule::RotorControlRule() : mDirectDescendantsFrom(nullptr) {}
+RotorRule::RotorRule() : mDirectDescendantsFrom(nullptr) {}
 
-uint16_t RotorControlRule::Match(const AccessibleOrProxy& aAccOrProxy) {
+uint16_t RotorRule::Match(const AccessibleOrProxy& aAccOrProxy) {
   uint16_t result = nsIAccessibleTraversalRule::FILTER_IGNORE;
 
   if (nsAccUtils::MustPrune(aAccOrProxy)) {
@@ -71,84 +31,125 @@ uint16_t RotorControlRule::Match(const AccessibleOrProxy& aAccOrProxy) {
     result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
   }
 
-  switch (aAccOrProxy.Role()) {
-    case roles::PUSHBUTTON:
-    case roles::SPINBUTTON:
-    case roles::DETAILS:
-    case roles::CHECKBUTTON:
-    case roles::COLOR_CHOOSER:
-    case roles::BUTTONDROPDOWNGRID:  // xul colorpicker
-    case roles::LISTBOX:
-    case roles::COMBOBOX:
-    case roles::EDITCOMBOBOX:
-    case roles::RADIOBUTTON:
-    case roles::RADIO_GROUP:
-    case roles::PAGETAB:
-    case roles::SLIDER:
-    case roles::SWITCH:
-    case roles::ENTRY:
-    case roles::OUTLINE:
-    case roles::PASSWORD_TEXT:
-      result |= nsIAccessibleTraversalRule::FILTER_MATCH;
-      break;
-
-    case roles::GROUPING: {
-      // Groupings are sometimes used (like radio groups) to denote
-      // sets of controls. If that's the case, we want to surface
-      // them. We also want to surface grouped time and date controls.
-      for (unsigned int i = 0; i < aAccOrProxy.ChildCount(); i++) {
-        AccessibleOrProxy currChild = aAccOrProxy.ChildAt(i);
-        if (currChild.Role() == roles::CHECKBUTTON ||
-            currChild.Role() == roles::SWITCH ||
-            currChild.Role() == roles::SPINBUTTON ||
-            currChild.Role() == roles::RADIOBUTTON) {
-          result |= nsIAccessibleTraversalRule::FILTER_MATCH;
-          break;
-        }
-      }
-      break;
-    }
-
-    case roles::DATE_EDITOR:
-    case roles::TIME_EDITOR:
-      result |= nsIAccessibleTraversalRule::FILTER_MATCH;
-      result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-      break;
-
-    default:
-      break;
+  if ([GetNativeFromGeckoAccessible(aAccOrProxy) isAccessibilityElement]) {
+    result |= nsIAccessibleTraversalRule::FILTER_MATCH;
   }
 
   return result;
 }
 
-RotorLinkRule::RotorLinkRule() : mDirectDescendantsFrom(nullptr) {}
+// Rotor Role Rule
+
+RotorRoleRule::RotorRoleRule(role aRole,
+                             AccessibleOrProxy& aDirectDescendantsFrom)
+    : RotorRule(aDirectDescendantsFrom), mRole(aRole){};
+
+RotorRoleRule::RotorRoleRule(role aRole) : RotorRule(), mRole(aRole){};
+
+uint16_t RotorRoleRule::Match(const AccessibleOrProxy& aAccOrProxy) {
+  uint16_t result = RotorRule::Match(aAccOrProxy);
+
+  // if a match was found in the base-class's Match function,
+  // it is valid to consider that match again here. if it is
+  // not of the desired role, we flip the match bit to "unmatch"
+  // otherwise, the match persists.
+  if ((result & nsIAccessibleTraversalRule::FILTER_MATCH) &&
+      aAccOrProxy.Role() != mRole) {
+    result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
+  }
+
+  return result;
+}
+
+// Rotor Control Rule
+
+RotorControlRule::RotorControlRule(AccessibleOrProxy& aDirectDescendantsFrom)
+    : RotorRule(aDirectDescendantsFrom){};
+
+RotorControlRule::RotorControlRule() : RotorRule(){};
+
+uint16_t RotorControlRule::Match(const AccessibleOrProxy& aAccOrProxy) {
+  uint16_t result = RotorRule::Match(aAccOrProxy);
+
+  // if a match was found in the base-class's Match function,
+  // it is valid to consider that match again here. if it is
+  // not of the desired role, we flip the match bit to "unmatch"
+  // otherwise, the match persists.
+  if ((result & nsIAccessibleTraversalRule::FILTER_MATCH)) {
+    switch (aAccOrProxy.Role()) {
+      case roles::PUSHBUTTON:
+      case roles::SPINBUTTON:
+      case roles::DETAILS:
+      case roles::CHECKBUTTON:
+      case roles::COLOR_CHOOSER:
+      case roles::BUTTONDROPDOWNGRID:  // xul colorpicker
+      case roles::LISTBOX:
+      case roles::COMBOBOX:
+      case roles::EDITCOMBOBOX:
+      case roles::RADIOBUTTON:
+      case roles::RADIO_GROUP:
+      case roles::PAGETAB:
+      case roles::SLIDER:
+      case roles::SWITCH:
+      case roles::ENTRY:
+      case roles::OUTLINE:
+      case roles::PASSWORD_TEXT:
+        return result;
+
+      case roles::DATE_EDITOR:
+      case roles::TIME_EDITOR:
+        result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
+        return result;
+
+      case roles::GROUPING: {
+        // Groupings are sometimes used (like radio groups) to denote
+        // sets of controls. If that's the case, we want to surface
+        // them. We also want to surface grouped time and date controls.
+        for (unsigned int i = 0; i < aAccOrProxy.ChildCount(); i++) {
+          AccessibleOrProxy currChild = aAccOrProxy.ChildAt(i);
+          if (currChild.Role() == roles::CHECKBUTTON ||
+              currChild.Role() == roles::SWITCH ||
+              currChild.Role() == roles::SPINBUTTON ||
+              currChild.Role() == roles::RADIOBUTTON) {
+            return result;
+          }
+        }
+
+        // if we iterated through the groups children and didn't
+        // find a control with one of the roles above, we should
+        // ignore this grouping
+        result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
+        return result;
+      }
+
+      default:
+        // if we did not match on any above role, we should
+        // ignore this accessible.
+        result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
+    }
+  }
+
+  return result;
+}
+
+// Rotor Link Rule
 
 RotorLinkRule::RotorLinkRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : mDirectDescendantsFrom(aDirectDescendantsFrom) {}
+    : RotorRule(aDirectDescendantsFrom){};
+
+RotorLinkRule::RotorLinkRule() : RotorRule(){};
 
 uint16_t RotorLinkRule::Match(const AccessibleOrProxy& aAccOrProxy) {
-  uint16_t result = nsIAccessibleTraversalRule::FILTER_IGNORE;
+  uint16_t result = RotorRule::Match(aAccOrProxy);
 
-  if (nsAccUtils::MustPrune(aAccOrProxy)) {
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (!mDirectDescendantsFrom.IsNull() &&
-      (aAccOrProxy != mDirectDescendantsFrom)) {
-    // If we've specified mDirectDescendantsFrom, we should ignore
-    // non-direct descendants of from the specified AoP. Because
-    // pivot performs a preorder traversal, the first aAccOrProxy
-    // object(s) that don't equal mDirectDescendantsFrom will be
-    // mDirectDescendantsFrom's children. We'll process them, but ignore
-    // their subtrees thereby processing direct descendants of
-    // mDirectDescendantsFrom only.
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy)) {
-    if ([[nativeMatch moxRole] isEqualToString:@"AXLink"]) {
-      result |= nsIAccessibleTraversalRule::FILTER_MATCH;
+  // if a match was found in the base-class's Match function,
+  // it is valid to consider that match again here. if it is
+  // not of the desired role, we flip the match bit to "unmatch"
+  // otherwise, the match persists.
+  if ((result & nsIAccessibleTraversalRule::FILTER_MATCH)) {
+    mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy);
+    if (![[nativeMatch moxRole] isEqualToString:@"AXLink"]) {
+      result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
     }
   }
 
@@ -162,28 +163,12 @@ RotorVisitedLinkRule::RotorVisitedLinkRule(
     : RotorLinkRule(aDirectDescendantsFrom) {}
 
 uint16_t RotorVisitedLinkRule::Match(const AccessibleOrProxy& aAccOrProxy) {
-  uint16_t result = nsIAccessibleTraversalRule::FILTER_IGNORE;
+  uint16_t result = RotorLinkRule::Match(aAccOrProxy);
 
-  if (nsAccUtils::MustPrune(aAccOrProxy)) {
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (!mDirectDescendantsFrom.IsNull() &&
-      (aAccOrProxy != mDirectDescendantsFrom)) {
-    // If we've specified mDirectDescendantsFrom, we should ignore
-    // non-direct descendants of from the specified AoP. Because
-    // pivot performs a preorder traversal, the first aAccOrProxy
-    // object(s) that don't equal mDirectDescendantsFrom will be
-    // mDirectDescendantsFrom's children. We'll process them, but ignore
-    // their subtrees thereby processing direct descendants of
-    // mDirectDescendantsFrom only.
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy)) {
-    if ([[nativeMatch moxRole] isEqualToString:@"AXLink"] &&
-        [[nativeMatch moxVisited] boolValue] == YES) {
-      result |= nsIAccessibleTraversalRule::FILTER_MATCH;
+  if (result & nsIAccessibleTraversalRule::FILTER_MATCH) {
+    mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy);
+    if (![[nativeMatch moxVisited] boolValue]) {
+      result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
     }
   }
 
@@ -197,54 +182,14 @@ RotorUnvisitedLinkRule::RotorUnvisitedLinkRule(
     : RotorLinkRule(aDirectDescendantsFrom) {}
 
 uint16_t RotorUnvisitedLinkRule::Match(const AccessibleOrProxy& aAccOrProxy) {
-  uint16_t result = nsIAccessibleTraversalRule::FILTER_IGNORE;
+  uint16_t result = RotorLinkRule::Match(aAccOrProxy);
 
-  if (nsAccUtils::MustPrune(aAccOrProxy)) {
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (!mDirectDescendantsFrom.IsNull() &&
-      (aAccOrProxy != mDirectDescendantsFrom)) {
-    // If we've specified mDirectDescendantsFrom, we should ignore
-    // non-direct descendants of from the specified AoP. Because
-    // pivot performs a preorder traversal, the first aAccOrProxy
-    // object(s) that don't equal mDirectDescendantsFrom will be
-    // mDirectDescendantsFrom's children. We'll process them, but ignore
-    // their subtrees thereby processing direct descendants of
-    // mDirectDescendantsFrom only.
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy)) {
-    if ([[nativeMatch moxRole] isEqualToString:@"AXLink"] &&
-        [[nativeMatch moxVisited] boolValue] == NO) {
-      result |= nsIAccessibleTraversalRule::FILTER_MATCH;
+  if (result & nsIAccessibleTraversalRule::FILTER_MATCH) {
+    mozAccessible* nativeMatch = GetNativeFromGeckoAccessible(aAccOrProxy);
+    if ([[nativeMatch moxVisited] boolValue]) {
+      result &= ~nsIAccessibleTraversalRule::FILTER_MATCH;
     }
   }
-
-  return result;
-}
-
-// Match All Rule
-
-RotorAllRule::RotorAllRule(AccessibleOrProxy& aDirectDescendantsFrom)
-    : mDirectDescendantsFrom(aDirectDescendantsFrom) {}
-
-RotorAllRule::RotorAllRule() : mDirectDescendantsFrom(nullptr) {}
-
-uint16_t RotorAllRule::Match(const AccessibleOrProxy& aAccOrProxy) {
-  uint16_t result = nsIAccessibleTraversalRule::FILTER_IGNORE;
-
-  if (nsAccUtils::MustPrune(aAccOrProxy)) {
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  if (!mDirectDescendantsFrom.IsNull() &&
-      (aAccOrProxy != mDirectDescendantsFrom)) {
-    result |= nsIAccessibleTraversalRule::FILTER_IGNORE_SUBTREE;
-  }
-
-  result |= nsIAccessibleTraversalRule::FILTER_MATCH;
 
   return result;
 }
