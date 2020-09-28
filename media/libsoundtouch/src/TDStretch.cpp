2e3fe392e7722a19a1b417484cab3295c2c5de6b
Paul Adenot <paul@paul.cx>
2020-09-28 18:16:45 +0000

    Bug 1624026 - Disable TDStretch::calcCrossCorrAccumulate to sidestep an audio quality bug on Android. r=chunmin
    Differential Revision: https://phabricator.services.mozilla.com/D91614

--- media/libsoundtouch/src/TDStretch.cpp
+++ media/libsoundtouch/src/TDStretch.cpp
@@ -926,45 +926,7 @@ double TDStretch::calcCrossCorr(const short *mixingPos, const short *compare, do
 /// Update cross-correlation by accumulating "norm" coefficient by previously calculated value
 double TDStretch::calcCrossCorrAccumulate(const short *mixingPos, const short *compare, double &norm)
 {
-    long corr;
-    unsigned long lnorm;
-    int i;
-
-    // cancel first normalizer tap from previous round
-    lnorm = 0;
-    for (i = 1; i <= channels; i ++)
-    {
-        lnorm -= (mixingPos[-i] * mixingPos[-i]) >> overlapDividerBitsNorm;
-    }
-
-    corr = 0;
-    // Same routine for stereo and mono. For stereo, unroll loop for better
-    // efficiency and gives slightly better resolution against rounding. 
-    // For mono it same routine, just  unrolls loop by factor of 4
-    for (i = 0; i < channels * overlapLength; i += 4) 
-    {
-        corr += (mixingPos[i] * compare[i] + 
-                 mixingPos[i + 1] * compare[i + 1]) >> overlapDividerBitsNorm;  // notice: do intermediate division here to avoid integer overflow
-        corr += (mixingPos[i + 2] * compare[i + 2] + 
-                 mixingPos[i + 3] * compare[i + 3]) >> overlapDividerBitsNorm;
-    }
-
-    // update normalizer with last samples of this round
-    for (int j = 0; j < channels; j ++)
-    {
-        i --;
-        lnorm += (mixingPos[i] * mixingPos[i]) >> overlapDividerBitsNorm;
-    }
-
-    norm += (double)lnorm;
-    if (norm > maxnorm)
-    {
-        maxnorm = (unsigned long)norm;
-    }
-
-    // Normalize result by dividing by sqrt(norm) - this step is easiest 
-    // done using floating point operation
-    return (double)corr / sqrt((norm < 1e-9) ? 1.0 : norm);
+  return calcCrossCorr(mixingPos, compare, norm);
 }
 
 #endif // SOUNDTOUCH_INTEGER_SAMPLES
