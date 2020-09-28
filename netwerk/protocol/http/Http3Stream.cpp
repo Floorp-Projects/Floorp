4b8e6b5e792b36f4ad05185859a87e1ea80845ef
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-09-28 21:12:49 +0000

    Bug 1655566 - P3 probe the times blocked by stream limits or flow control as a sender r=necko-reviewers,valentin
    Depends on D85809
    
    Differential Revision: https://phabricator.services.mozilla.com/D85810

--- netwerk/protocol/http/Http3Stream.cpp
+++ netwerk/protocol/http/Http3Stream.cpp
@@ -32,7 +32,8 @@ Http3Stream::Http3Stream(nsAHttpTransaction* httpTransaction,
       mSocketTransport(session->SocketTransport()),
       mTotalSent(0),
       mTotalRead(0),
-      mFin(false) {
+      mFin(false),
+      mSendingBlockedByFlowControlCount(0) {
   MOZ_ASSERT(OnSocketThread(), "not on socket thread");
   LOG3(("Http3Stream::Http3Stream [this=%p]", this));
 }
@@ -185,6 +186,9 @@ nsresult Http3Stream::OnReadSegment(const char* buf, uint32_t count,
       break;
     case SENDING_BODY: {
       rv = mSession->SendRequestBody(mStreamId, buf, count, countRead);
+      if (rv == NS_BASE_STREAM_WOULD_BLOCK) {
+          mSendingBlockedByFlowControlCount++;
+      }
       MOZ_ASSERT(mRequestBodyLenRemaining >= *countRead,
                  "We cannot send more that than we promised.");
       if (mRequestBodyLenRemaining < *countRead) {
@@ -204,6 +208,9 @@ nsresult Http3Stream::OnReadSegment(const char* buf, uint32_t count,
                                         NS_NET_STATUS_WAITING_FOR, 0);
         mSession->CloseSendingSide(mStreamId);
         mSendState = SEND_DONE;
+        Telemetry::Accumulate(
+            Telemetry::HTTP3_SENDING_BLOCKED_BY_FLOW_CONTROL_PER_TRANS,
+            mSendingBlockedByFlowControlCount);
       }
     } break;
     case EARLY_RESPONSE:
@@ -403,6 +410,7 @@ nsresult Http3Stream::Finish0RTT(bool aRestart) {
     mTotalSent = 0;
     mTotalRead = 0;
     mFin = false;
+    mSendingBlockedByFlowControlCount = 0;
   }
 
   return rv;
