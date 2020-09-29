b978b553a8cbc3bf9a478b2f4b498d5f6e2203e0
Marco Bonardo <mbonardo@mozilla.com>
2020-09-29 15:41:36 +0000

    Bug 1667133 - Perma failure in browser_searchFunction.js when merging to beta without update2. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D91711

--- browser/components/urlbar/tests/browser/browser_revert.js
+++ browser/components/urlbar/tests/browser/browser_revert.js
@@ -1,44 +1,33 @@
-var tab = null;
+// Test reverting the urlbar value with ESC after a tab switch.
 
-function test() {
-  waitForExplicitFinish();
-
-  let pageLoaded = {
-    onStateChange: function onStateChange(
-      aWebProgress,
-      aRequest,
-      aStateFlags,
-      aStatus
-    ) {
-      if (
-        aStateFlags & Ci.nsIWebProgressListener.STATE_STOP &&
-        aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK
-      ) {
-        gBrowser.removeProgressListener(this);
-        executeSoon(checkURLBarRevert);
-      }
+add_task(async function() {
+  registerCleanupFunction(PlacesUtils.history.clear);
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: "http://example.com",
     },
-  };
+    async function(browser) {
+      let originalValue = gURLBar.value;
+      let tab = gBrowser.selectedTab;
+      info("Put a typed value.");
+      gBrowser.userTypedValue = "foobar";
+      info("Switch tabs.");
+      gBrowser.selectedTab = gBrowser.tabs[0];
+      gBrowser.selectedTab = tab;
+      Assert.equal(
+        gURLBar.value,
+        "foobar",
+        "location bar displays typed value"
+      );
 
-  gBrowser.addProgressListener(pageLoaded);
-  tab = BrowserTestUtils.addTab(gBrowser, "http://example.com");
-  gBrowser.selectedTab = tab;
-}
-
-function checkURLBarRevert() {
-  let originalValue = gURLBar.value;
-
-  gBrowser.userTypedValue = "foobar";
-  gBrowser.selectedTab = gBrowser.tabs[0];
-  gBrowser.selectedTab = tab;
-  is(gURLBar.value, "foobar", "location bar displays typed value");
-
-  gURLBar.focus();
-
-  EventUtils.synthesizeKey("KEY_Escape");
-
-  is(gURLBar.value, originalValue, "ESC reverted the location bar value");
-
-  gBrowser.removeTab(tab);
-  finish();
-}
+      gURLBar.focus();
+      EventUtils.synthesizeKey("KEY_Escape");
+      Assert.equal(
+        gURLBar.value,
+        originalValue,
+        "ESC reverted the location bar value"
+      );
+    }
+  );
+});
