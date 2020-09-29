0e87b455d306b3b59324fb5a1032cc687707bc40
Kartikaya Gupta <kgupta@mozilla.com>
2020-09-29 03:00:54 +0000

    Bug 1667863 - Replace AppendToString of ScaleFactors2D with ostream usage. r=mattwoodrow
    Depends on D91656
    
    Differential Revision: https://phabricator.services.mozilla.com/D91657

--- gfx/layers/LayersLogging.h
+++ gfx/layers/LayersLogging.h
@@ -56,21 +56,6 @@ void AppendToString(std::stringstream& aStream, const FrameMetrics& m,
 void AppendToString(std::stringstream& aStream, const ZoomConstraints& z,
                     const char* pfx = "", const char* sfx = "");
 
-template <class src, class dst>
-void AppendToString(std::stringstream& aStream,
-                    const mozilla::gfx::ScaleFactors2D<src, dst>& scale,
-                    const char* pfx = "", const char* sfx = "") {
-  aStream << pfx;
-  std::streamsize oldPrecision = aStream.precision(3);
-  if (scale.AreScalesSame()) {
-    aStream << scale.xScale;
-  } else {
-    aStream << '(' << scale.xScale << ',' << scale.yScale << ')';
-  }
-  aStream.precision(oldPrecision);
-  aStream << sfx;
-}
-
 void AppendToString(std::stringstream& aStream, const mozilla::gfx::Matrix& m,
                     const char* pfx = "", const char* sfx = "");
 
