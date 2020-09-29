5bd354fcd5b568e56681a68839160f9d24c1681d
stransky <stransky@redhat.com>
2020-09-29 09:32:54 +0000

    Bug 1664911 [Wayland] Use DRM modifiers for WebGL surfaces, r=jhorak
    Differential Revision: https://phabricator.services.mozilla.com/D90168

--- widget/gtk/DMABufLibWrapper.cpp
+++ widget/gtk/DMABufLibWrapper.cpp
@@ -6,9 +6,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "DMABufLibWrapper.h"
+#include "nsWaylandDisplay.h"
 #include "mozilla/StaticPrefs_widget.h"
 #include "mozilla/StaticPrefs_media.h"
 #include "mozilla/gfx/gfxVars.h"
+
+#include <gdk/gdk.h>
+#include <gdk/gdkx.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -17,6 +21,8 @@
 
 namespace mozilla {
 namespace widget {
+
+static nsDMABufDevice* gDMABufDevice;
 
 #define GBMLIB_NAME "libgbm.so.1"
 #define DRMLIB_NAME "libdrm.so.2"
@@ -148,6 +154,56 @@ int nsDMABufDevice::GetGbmDeviceFd() {
   return mGbmFd;
 }
 
+static void dmabuf_modifiers(void* data,
+                             struct zwp_linux_dmabuf_v1* zwp_linux_dmabuf,
+                             uint32_t format, uint32_t modifier_hi,
+                             uint32_t modifier_lo) {
+  switch (format) {
+    case GBM_FORMAT_ARGB8888:
+      gDMABufDevice->AddFormatModifier(true, format, modifier_hi, modifier_lo);
+      break;
+    case GBM_FORMAT_XRGB8888:
+      gDMABufDevice->AddFormatModifier(false, format, modifier_hi, modifier_lo);
+      break;
+    default:
+      break;
+  }
+}
+
+static void dmabuf_format(void* data,
+                          struct zwp_linux_dmabuf_v1* zwp_linux_dmabuf,
+                          uint32_t format) {
+  // XXX: deprecated
+}
+
+static const struct zwp_linux_dmabuf_v1_listener dmabuf_listener = {
+    dmabuf_format, dmabuf_modifiers};
+
+static void global_registry_handler(void* data, wl_registry* registry,
+                                    uint32_t id, const char* interface,
+                                    uint32_t version) {
+  auto* display = static_cast<nsWaylandDisplay*>(data);
+  if (!display) {
+    return;
+  }
+
+  if (strcmp(interface, "zwp_linux_dmabuf_v1") == 0 && version > 2) {
+    auto* dmabuf = WaylandRegistryBind<zwp_linux_dmabuf_v1>(
+        registry, id, &zwp_linux_dmabuf_v1_interface, 3);
+    LOGDMABUF(("zwp_linux_dmabuf_v1 is available."));
+    gDMABufDevice->ResetFormatsModifiers();
+    zwp_linux_dmabuf_v1_add_listener(dmabuf, &dmabuf_listener, data);
+  } else if (strcmp(interface, "wl_drm") == 0) {
+    LOGDMABUF(("wl_drm is available."));
+  }
+}
+
+static void global_registry_remover(void* data, wl_registry* registry,
+                                    uint32_t id) {}
+
+static const struct wl_registry_listener registry_listener = {
+    global_registry_handler, global_registry_remover};
+
 nsDMABufDevice::nsDMABufDevice()
     : mXRGBFormat({true, false, GBM_FORMAT_XRGB8888, nullptr, 0}),
       mARGBFormat({true, true, GBM_FORMAT_ARGB8888, nullptr, 0}),
@@ -156,6 +212,24 @@ nsDMABufDevice::nsDMABufDevice()
       mGdmConfigured(false),
       mIsDMABufEnabled(false),
       mIsDMABufConfigured(false) {}
+
+void nsDMABufDevice::Init() {
+  if (gdk_display_get_default() &&
+      !GDK_IS_X11_DISPLAY(gdk_display_get_default())) {
+    wl_display* display = WaylandDisplayGetWLDisplay();
+    mRegistry = wl_display_get_registry(display);
+    wl_registry_add_listener(mRegistry, &registry_listener, this);
+    wl_display_roundtrip(display);
+    wl_display_roundtrip(display);
+  }
+}
+
+nsDMABufDevice::~nsDMABufDevice() {
+  if (mRegistry) {
+    wl_registry_destroy(mRegistry);
+    mRegistry = nullptr;
+  }
+}
 
 bool nsDMABufDevice::IsDMABufEnabled() {
   if (mIsDMABufConfigured) {
@@ -249,8 +323,11 @@ void nsDMABufDevice::ResetFormatsModifiers() {
 }
 
 nsDMABufDevice* GetDMABufDevice() {
-  static nsDMABufDevice gDMABufDevice;
-  return &gDMABufDevice;
+  if (!gDMABufDevice) {
+    gDMABufDevice = new nsDMABufDevice();
+    gDMABufDevice->Init();
+  }
+  return gDMABufDevice;
 }
 
 }  // namespace widget
