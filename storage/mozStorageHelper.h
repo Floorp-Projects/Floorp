6e97fd8be2b4c226d206c193094b86c53656da82
Simon Giesecke <sgiesecke@mozilla.com>
2020-09-29 10:57:19 +0000

    Bug 1663924 - Split BorrowedStatement from CachedStatement. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D90234

--- storage/mozStorageHelper.h
+++ storage/mozStorageHelper.h
@@ -173,6 +173,12 @@ class MOZ_STACK_CLASS mozStorageStatementScoper {
     if (mStatement) mStatement->Reset();
   }
 
+  mozStorageStatementScoper(mozStorageStatementScoper&&) = default;
+  mozStorageStatementScoper& operator=(mozStorageStatementScoper&&) = default;
+  mozStorageStatementScoper(const mozStorageStatementScoper&) = delete;
+  mozStorageStatementScoper& operator=(const mozStorageStatementScoper&) =
+      delete;
+
   /**
    * Call this to make the statement not reset. You might do this if you know
    * that the statement has been reset.
