6569cf7eb2c7eec1a645b46538355a1a838f53af
Iain Ireland <iireland@mozilla.com>
2020-09-29 12:18:02 +0000

    Bug 1667738: Don't remove inlined scripts from the inlining root when inlining fails r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D91662

--- js/src/jit/JitScript.cpp
+++ js/src/jit/JitScript.cpp
@@ -300,9 +300,6 @@ void ICScript::removeInlinedChild(uint32_t pcOffset) {
   inlinedChildren_->eraseIf([pcOffset](const CallSite& callsite) -> bool {
     return callsite.pcOffset_ == pcOffset;
   });
-
-  // The ICScript is owned by the inlining root. Remove it.
-  inliningRoot()->removeInlinedScript(icScript);
 }
 
 bool ICScript::hasInlinedChild(uint32_t pcOffset) {
