5e3cff83ead0341c60b271f6f04d004f7fe60ff0
Aki Sasaki <asasaki@mozilla.com>
2020-09-30 15:29:21 +0000

    Bug 1666321 - run l10n-bump as part of central-to-beta merge day. r=releng-reviewers,bhearsum DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D91885

--- taskcluster/taskgraph/transforms/merge_automation.py
+++ taskcluster/taskgraph/transforms/merge_automation.py
@@ -16,6 +16,9 @@ transforms = TransformSequence()
 @transforms.add
 def handle_keyed_by(config, tasks):
     """Resolve fields that can be keyed by platform, etc."""
+    if "merge_config" not in config.params:
+        return
+    merge_config = config.params["merge_config"]
     fields = [
         "worker.push",
         "worker-type",
@@ -31,6 +34,7 @@ def handle_keyed_by(config, tasks):
                 **{
                     "project": config.params["project"],
                     "release-type": config.params["release_type"],
+                    "behavior": merge_config["behavior"],
                 }
             )
         yield task
@@ -39,8 +43,6 @@ def handle_keyed_by(config, tasks):
 @transforms.add
 def update_labels(config, tasks):
     for task in tasks:
-        if "merge_config" not in config.params:
-            break
         merge_config = config.params["merge_config"]
         task["label"] = "merge-{}".format(merge_config["behavior"])
         treeherder = task.get("treeherder", {})
@@ -59,6 +61,9 @@ def add_payload_config(config, tasks):
         worker["merge-info"] = config.graph_config["merge-automation"]["behaviors"][
             merge_config["behavior"]
         ]
+
+        if "l10n-bump-info" in worker and worker["l10n-bump-info"] is None:
+            del(worker["l10n-bump-info"])
 
         # Override defaults, useful for testing.
         for field in ["from-repo", "from-branch", "to-repo", "to-branch", "fetch-version-from"]:
