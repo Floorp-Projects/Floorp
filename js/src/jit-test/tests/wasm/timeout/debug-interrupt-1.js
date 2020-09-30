30ea31f9b4f6c2ffb40ea743aab1e1ca0e51072d
Jan de Mooij <jdemooij@mozilla.com>
2020-09-30 20:31:22 +0000

    Bug 1666417 part 7 - Fix ASan test timeouts. r=iain
    Depends on D91898
    
    Differential Revision: https://phabricator.services.mozilla.com/D91899

--- js/src/jit-test/tests/wasm/timeout/debug-interrupt-1.js
+++ js/src/jit-test/tests/wasm/timeout/debug-interrupt-1.js
@@ -3,6 +3,14 @@
 // Don't include wasm.js in timeout tests: when wasm isn't supported, it will
 // quit(0) which will cause the test to fail.
 
+// Note: this test triggers an interrupt and then iloops in Wasm code. If the
+// interrupt fires before the Wasm code is compiled, the test relies on the
+// JS interrupt check in onEnterFrame to catch it. Warp/Ion code however can
+// elide the combined interrupt/overrecursion check in simple leaf functions.
+// Long story short, set the Warp threshold to a non-zero value to prevent
+// intermittent timeouts with --ion-eager.
+setJitCompilerOption("ion.warmup.trigger", 30);
+
 var g = newGlobal({newCompartment: true});
 g.parent = this;
 g.eval("Debugger(parent).onEnterFrame = function() {};");
