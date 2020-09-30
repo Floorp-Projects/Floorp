67da2887eca66e887bc75d87e1c075dd0baa668d
Harry Twyford <htwyford@mozilla.com>
2020-09-30 17:53:47 +0000

    Bug 1668340 - Fix browser_tabKeyBehavior.js on Beta. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D91968

--- browser/components/urlbar/tests/browser/browser_tabKeyBehavior.js
+++ browser/components/urlbar/tests/browser/browser_tabKeyBehavior.js
@@ -188,6 +188,7 @@ add_task(async function tabNoSearchStringSearchMode() {
     set: [
       ["browser.urlbar.update2", true],
       ["browser.urlbar.update2.oneOffsRefresh", true],
+      ["browser.urlbar.update2.localOneOffs", true],
     ],
   });
   await UrlbarTestUtils.promiseAutocompleteResultPopup({
