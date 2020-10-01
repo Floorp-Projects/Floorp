d8dd11c4ac0fa32d0c709f4aa0d72eb73e01b697
Dorel Luca <dluca@mozilla.com>
2020-10-02 02:34:22 +0300

    Backed out 2 changesets (bug 1668054, bug 1667426) for Browser-chrome failure in toolkit/modules/tests/browser/browser_Troubleshoot.js. CLOSED TREE
    Backed out changeset 0a0ad7d6a16b (bug 1668054)
    Backed out changeset fdd402fe6537 (bug 1667426)

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -467,11 +467,7 @@ bool gFxREmbedded = false;
 // - The MOZ_FORCE_ENABLE_FISSION environment variable: If set to any value,
 //   Fission will be enabled.
 //
-// - The 'fission.autostart' preference, if it has been configured by the user.
-static const char kPrefFissionAutostart[] = "fission.autostart";
-//
-// - The fission experiment enrollment status set during the previous run, which
-//   is controlled by the following preferences:
+// - The following preferences:
 //
 // The current enrollment status as controlled by Normandy. This value is only
 // stored in the default preference branch, and is not persisted across
@@ -488,31 +484,23 @@ static const char kPrefFissionExperimentEnrollmentStatus[] =
 // the declaration of `nsIXULRuntime.fissionExperimentStatus`
 static const char kPrefFissionExperimentStartupEnrollmentStatus[] =
     "fission.experiment.startupEnrollmentStatus";
-
-// The computed FissionAutostart value for the session, read by content
-// processes to initialize gFissionAutostart.
 //
-// This pref is locked, and only configured on the default branch, so should
-// never be persisted in a profile.
-static const char kPrefFissionAutostartSession[] = "fission.autostart.session";
+// The "fission.autostart" preference: If none of the above conditions apply,
+// and the experiment enrollment status is unknown, then the value of the
+// "fisison.autostart" preference is used.
 
-static nsIXULRuntime::ExperimentStatus gFissionExperimentStatus =
-    nsIXULRuntime::eExperimentStatusUnenrolled;
-static bool gFissionAutostart = false;
-static bool gFissionAutostartInitialized = false;
+static nsIXULRuntime::ExperimentStatus FissionExperimentStatus() {
+  static nsIXULRuntime::ExperimentStatus sExperimentStatus = ([]() {
+    uint32_t value =
+        Preferences::GetUint(kPrefFissionExperimentStartupEnrollmentStatus);
+    if (value >
+        uint32_t(nsIXULRuntime::ExperimentStatus::eExperimentStatusMax)) {
+      return nsIXULRuntime::ExperimentStatus::eUnknown;
+    }
+    return nsIXULRuntime::ExperimentStatus(value);
+  })();
 
-static bool FissionExperimentEnrolled() {
-  MOZ_ASSERT(XRE_IsParentProcess());
-  return gFissionExperimentStatus == nsIXULRuntime::eExperimentStatusControl ||
-         gFissionExperimentStatus == nsIXULRuntime::eExperimentStatusTreatment;
-}
-
-static void FissionExperimentDisqualify() {
-  MOZ_ASSERT(XRE_IsParentProcess());
-  // Setting this pref's user value will be detected by Normandy, causing the
-  // client to be unenrolled from the experiment.
-  Preferences::SetUint(kPrefFissionExperimentEnrollmentStatus,
-                       nsIXULRuntime::eExperimentStatusDisqualified);
+  return sExperimentStatus;
 }
 
 static void OnFissionEnrollmentStatusChanged(const char* aPref, void* aData) {
@@ -521,91 +509,29 @@ static void OnFissionEnrollmentStatusChanged(const char* aPref, void* aData) {
       Preferences::GetUint(kPrefFissionExperimentEnrollmentStatus));
 }
 
-static void OnFissionAutostartChanged(const char* aPref, void* aData) {
-  MOZ_ASSERT(FissionExperimentEnrolled());
-  if (Preferences::HasUserValue(kPrefFissionAutostart)) {
-    FissionExperimentDisqualify();
-  }
-}
-
-static void EnsureFissionAutostartInitialized() {
-  if (gFissionAutostartInitialized) {
-    return;
-  }
-  gFissionAutostartInitialized = true;
-
-  if (!XRE_IsParentProcess()) {
-    // This pref is configured for the current session by the parent process.
-    gFissionAutostart = Preferences::GetBool(kPrefFissionAutostartSession,
-                                             false, PrefValueKind::Default);
-    return;
-  }
-
-  // Initialize the fission experiment, configuring fission.autostart's
-  // default, before checking other overrides. This allows opting-out of a
-  // fission experiment through about:preferences or about:config from a
-  // safemode session.
-  uint32_t experimentRaw =
-      Preferences::GetUint(kPrefFissionExperimentStartupEnrollmentStatus,
-                           nsIXULRuntime::eExperimentStatusUnenrolled);
-  gFissionExperimentStatus =
-      experimentRaw < nsIXULRuntime::eExperimentStatusCount
-          ? nsIXULRuntime::ExperimentStatus(experimentRaw)
-          : nsIXULRuntime::eExperimentStatusDisqualified;
-
-  // If the user has overridden an active experiment by setting a user value for
-  // "fission.autostart", disqualify the user from the experiment.
-  if (Preferences::HasUserValue(kPrefFissionAutostart) &&
-      FissionExperimentEnrolled()) {
-    FissionExperimentDisqualify();
-    gFissionExperimentStatus = nsIXULRuntime::eExperimentStatusDisqualified;
-  }
-
-  // Configure the default branch for "fission.autostart" based on experiment
-  // enrollment status.
-  if (FissionExperimentEnrolled()) {
-    bool isTreatment =
-        gFissionExperimentStatus == nsIXULRuntime::eExperimentStatusTreatment;
-    Preferences::SetBool(kPrefFissionAutostart, isTreatment,
-                         PrefValueKind::Default);
-  }
-
-  if (gSafeMode) {
-    gFissionAutostart = false;
-  } else if (EnvHasValue("MOZ_FORCE_ENABLE_FISSION")) {
-    gFissionAutostart = true;
-  } else {
-    // NOTE: This will take into account changes to the default due to
-    // `InitializeFissionExperimentStatus`.
-    gFissionAutostart = Preferences::GetBool(kPrefFissionAutostart, false);
-  }
-
-  // Content processes cannot run the same logic as we're running in the parent
-  // process, as the current value of various preferences may have changed
-  // between launches. Instead, the content process will read the default branch
-  // of the locked `fission.autostart.session` preference to determine the value
-  // determined by this method.
-  Preferences::Unlock(kPrefFissionAutostartSession);
-  Preferences::ClearUser(kPrefFissionAutostartSession);
-  Preferences::SetBool(kPrefFissionAutostartSession, gFissionAutostart,
-                       PrefValueKind::Default);
-  Preferences::Lock(kPrefFissionAutostartSession);
-
-  // Watch prefs which may be modified at runtime to configure experiment
-  // enrollment status for the next launch.
-  Preferences::RegisterCallback(&OnFissionEnrollmentStatusChanged,
-                                kPrefFissionExperimentEnrollmentStatus);
-  if (FissionExperimentEnrolled()) {
-    Preferences::RegisterCallback(&OnFissionAutostartChanged,
-                                  kPrefFissionAutostart);
-  }
-}
-
 namespace mozilla {
 
 bool FissionAutostart() {
-  EnsureFissionAutostartInitialized();
-  return gFissionAutostart;
+  static bool sFissionAutostart = ([]() {
+    if (gSafeMode) {
+      return false;
+    }
+
+    if (EnvHasValue("MOZ_FORCE_ENABLE_FISSION")) {
+      return true;
+    }
+
+    switch (FissionExperimentStatus()) {
+      case nsIXULRuntime::ExperimentStatus::eEnrolledControl:
+        return false;
+      case nsIXULRuntime::ExperimentStatus::eEnrolledTreatment:
+        return true;
+      default:
+        return StaticPrefs::fission_autostart_AtStartup_DoNotUseDirectly();
+    }
+  })();
+
+  return sFissionAutostart;
 }
 
 bool SessionHistoryInParent() {
@@ -943,12 +869,7 @@ nsXULAppInfo::GetFissionAutostart(bool* aResult) {
 
 NS_IMETHODIMP
 nsXULAppInfo::GetFissionExperimentStatus(ExperimentStatus* aResult) {
-  if (!XRE_IsParentProcess()) {
-    return NS_ERROR_NOT_AVAILABLE;
-  }
-
-  EnsureFissionAutostartInitialized();
-  *aResult = gFissionExperimentStatus;
+  *aResult = FissionExperimentStatus();
   return NS_OK;
 }
 
@@ -4810,6 +4731,9 @@ nsresult XREMain::XRE_mainRun() {
         kPrefSecurityContentSignatureRootHash);
 #  endif  // defined(MOZ_DEFAULT_BROWSER_AGENT)
 #endif
+
+    Preferences::RegisterCallback(&OnFissionEnrollmentStatusChanged,
+                                  kPrefFissionExperimentEnrollmentStatus);
 
 #if defined(HAVE_DESKTOP_STARTUP_ID) && defined(MOZ_WIDGET_GTK)
     // Clear the environment variable so it won't be inherited by
