681afffa212b9c9ee0c2484a4bcc9e777a266158
Jeff Muizelaar <jmuizelaar@mozilla.com>
2020-10-01 14:23:59 +0000

    Bug 1668377 - Switch printing from WrapAndRecord to just Recording. r=aosmond
    Using WrapAndRecord for printing was just a way to minimize risk when
    working on the RecordingDrawTarget. We would prefer to eliminate
    WrapAndRecord.
    
    These leaves some of old function names around just to keep the patch
    small. If it sticks we can fix those names.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91988

--- gfx/thebes/PrintTarget.cpp
+++ gfx/thebes/PrintTarget.cpp
@@ -172,7 +172,8 @@ already_AddRefed<DrawTarget> PrintTarget::CreateWrapAndRecordDrawTarget(
 
   if (aRecorder) {
     // It doesn't really matter what we pass as the DrawTarget here.
-    dt = gfx::Factory::CreateWrapAndRecordDrawTarget(aRecorder, aDrawTarget);
+    dt = gfx::Factory::CreateRecordingDrawTarget(aRecorder, aDrawTarget,
+                                                 aDrawTarget->GetRect());
   }
 
   if (!dt || !dt->IsValid()) {
