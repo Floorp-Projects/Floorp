d01e76fc5841e378a1802ba94480bc3536e9841e
Matt Woodrow <mwoodrow@mozilla.com>
2020-10-01 22:18:52 +0000

    Bug 1668302 - Implement RenderTextureHostSWL for wrapper texture hosts. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D92008

--- gfx/webrender_bindings/RenderTextureHostWrapper.h
+++ gfx/webrender_bindings/RenderTextureHostWrapper.h
@@ -7,7 +7,7 @@
 #ifndef MOZILLA_GFX_RENDERTEXTUREHOSTWRAPPER_H
 #define MOZILLA_GFX_RENDERTEXTUREHOSTWRAPPER_H
 
-#include "RenderTextureHost.h"
+#include "RenderTextureHostSWGL.h"
 
 namespace mozilla {
 
@@ -24,27 +24,32 @@ namespace wr {
  * RenderTextureHosts(RenderDXGITextureHostOGL and
  * RenderDXGIYCbCrTextureHostOGL) have overhead.
  */
-class RenderTextureHostWrapper final : public RenderTextureHost {
+class RenderTextureHostWrapper final : public RenderTextureHostSWGL {
  public:
   explicit RenderTextureHostWrapper(ExternalImageId aExternalImageId);
 
+  // RenderTextureHost
   wr::WrExternalImage Lock(uint8_t aChannelIndex, gl::GLContext* aGL,
                            wr::ImageRendering aRendering) override;
   void Unlock() override;
   void ClearCachedResources() override;
-
   RenderMacIOSurfaceTextureHostOGL* AsRenderMacIOSurfaceTextureHostOGL()
       override;
+  RenderDXGITextureHostOGL* AsRenderDXGITextureHostOGL() override;
 
-  virtual RenderDXGITextureHostOGL* AsRenderDXGITextureHostOGL() override;
+  // RenderTextureHostSWGL
+  size_t GetPlaneCount() override;
+  bool MapPlane(uint8_t aChannelIndex, PlaneInfo& aPlaneInfo) override;
+  void UnmapPlanes() override;
+  gfx::YUVColorSpace GetYUVColorSpace() const override;
 
  private:
   ~RenderTextureHostWrapper() override;
 
-  void EnsureTextureHost();
+  void EnsureTextureHost() const;
 
   const ExternalImageId mExternalImageId;
-  RefPtr<RenderTextureHost> mTextureHost;
+  mutable RefPtr<RenderTextureHost> mTextureHost;
 };
 
 }  // namespace wr
