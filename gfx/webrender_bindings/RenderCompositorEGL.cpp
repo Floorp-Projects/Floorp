63b41aa30f58c51440964a15e6e091ae23f9cb63
Razvan Maries <rmaries@mozilla.com>
2020-10-02 12:51:55 +0300

    Backed out 3 changesets (bug 1656533, bug 1575765) for WebRender bustages. CLOSED TREE
    Backed out changeset 84c31fd9ff82 (bug 1575765)
    Backed out changeset 48c1532d1d9d (bug 1575765)
    Backed out changeset ed9075d9319b (bug 1656533)

--- gfx/webrender_bindings/RenderCompositorEGL.cpp
+++ gfx/webrender_bindings/RenderCompositorEGL.cpp
@@ -238,54 +238,14 @@ bool RenderCompositorEGL::UsePartialPresent() {
   return gfx::gfxVars::WebRenderMaxPartialPresentRects() > 0;
 }
 
-bool RenderCompositorEGL::RequestFullRender() { return mBufferAge == 0; }
+bool RenderCompositorEGL::RequestFullRender() { return mBufferAge != 2; }
 
 uint32_t RenderCompositorEGL::GetMaxPartialPresentRects() {
   return gfx::gfxVars::WebRenderMaxPartialPresentRects();
 }
 
 bool RenderCompositorEGL::ShouldDrawPreviousPartialPresentRegions() {
-  return true;
-}
-
-size_t RenderCompositorEGL::GetBufferAge() const { return mBufferAge; }
-
-void RenderCompositorEGL::SetBufferDamageRegion(const wr::DeviceIntRect* aRects,
-                                                size_t aNumRects) {
-  const auto& gle = gl::GLContextEGL::Cast(gl());
-  const auto& egl = gle->mEgl;
-  if (gle->HasKhrPartialUpdate()) {
-    std::vector<EGLint> rects;
-    rects.reserve(4 * aNumRects);
-    const auto bufferSize = GetBufferSize();
-    for (size_t i = 0; i < aNumRects; i++) {
-      const auto left =
-          std::max(0, std::min(bufferSize.width, aRects[i].origin.x));
-      const auto top =
-          std::max(0, std::min(bufferSize.height, aRects[i].origin.y));
-
-      const auto right =
-          std::min(bufferSize.width,
-                   std::max(0, aRects[i].origin.x + aRects[i].size.width));
-      const auto bottom =
-          std::min(bufferSize.height,
-                   std::max(0, aRects[i].origin.y + aRects[i].size.height));
-
-      const auto width = right - left;
-      const auto height = bottom - top;
-
-      rects.push_back(left);
-      rects.push_back(bufferSize.height - bottom);
-      rects.push_back(width);
-      rects.push_back(height);
-    }
-    const auto ret =
-        egl->fSetDamageRegion(mEGLSurface, rects.data(), rects.size() / 4);
-    if (ret == LOCAL_EGL_FALSE) {
-      const auto err = egl->mLib->fGetError();
-      gfxCriticalError() << "Error in eglSetDamageRegion: " << gfx::hexa(err);
-    }
-  }
+  return gl::GLContextEGL::Cast(gl())->HasBufferAge();
 }
 
 }  // namespace mozilla::wr
--- gfx/wr/webrender/src/lib.rs
+++ gfx/wr/webrender/src/lib.rs
@@ -209,8 +209,8 @@ pub extern crate api;
 extern crate webrender_build;
 
 #[doc(hidden)]
-pub use crate::composite::{CompositorConfig, Compositor, CompositorCapabilities, CompositorSurfaceTransform};
-pub use crate::composite::{NativeSurfaceId, NativeTileId, NativeSurfaceInfo, PartialPresentCompositor};
+pub use crate::composite::{CompositorConfig, Compositor, CompositorCapabilities};
+pub use crate::composite::{NativeSurfaceId, NativeTileId, NativeSurfaceInfo, CompositorSurfaceTransform};
 pub use crate::device::{UploadMethod, VertexUsageHint, get_gl_target, get_unoptimized_shader_source};
 pub use crate::device::{ProgramBinary, ProgramCache, ProgramCacheObserver, FormatDesc};
 pub use crate::device::Device;
