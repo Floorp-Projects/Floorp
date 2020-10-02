36d959d69319487aee0a0876f8a02423f71b9d57
Jamie Nicol <jnicol@mozilla.com>
2020-10-02 10:23:49 +0000

    Bug 1656533 - Handle buffer ages other than 2 for EGL_EXT_buffer_age. r=sotaro,gw
    Add a trait PartialPresentCompositor with a function get_buffer_age(),
    and allow gecko to pass an implementation to webrender during
    initialization. This allows webrender to query the age of the current
    backbuffer during compositing.
    
    Make webrender track the previous 2 frame's dirty rects, rather than
    just the previous 1 frame's, allowing it to calculate the total damage
    rect for buffer ages of up to 3. If the age is greater than 3, treat
    the entire buffer as invalid. Also handle special cases of ages 0 and
    1, 0 meaning the entire buffer is invalid, and 1 meaning the entire
    buffer is valid. Make gecko stop requesting a full render for buffer
    ages other than 2, as webrender can now handle these cases itself.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91202

--- gfx/webrender_bindings/RenderCompositorOGL.h
+++ gfx/webrender_bindings/RenderCompositorOGL.h
@@ -39,6 +39,7 @@ class RenderCompositorOGL : public RenderCompositor {
   bool RequestFullRender() override;
   uint32_t GetMaxPartialPresentRects() override;
   bool ShouldDrawPreviousPartialPresentRegions() override;
+  size_t GetBufferAge() const override;
 
  protected:
   RefPtr<gl::GLContext> mGL;
