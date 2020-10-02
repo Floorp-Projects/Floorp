bc919ff8878b8a4d020fa147ba3b42e4e614de8c
André Bargull <andre.bargull@gmail.com>
2020-10-02 08:23:56 +0000

    Bug 1668531 - Part 11: Clean-up includes for BytecodeUtil.h. r=jandem
    Depends on D92057
    
    Differential Revision: https://phabricator.services.mozilla.com/D92058

--- js/src/vm/BytecodeUtil.h
+++ js/src/vm/BytecodeUtil.h
@@ -11,22 +11,29 @@
  * JS bytecode definitions.
  */
 
+#include "mozilla/Assertions.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/EndianUtils.h"
 
 #include <algorithm>
+#include <stddef.h>
+#include <stdint.h>
 
 #include "jstypes.h"
 #include "NamespaceImports.h"
 
-#include "frontend/SourceNotes.h"
 #include "js/TypeDecls.h"
 #include "js/UniquePtr.h"
+#include "js/Utility.h"
+#include "js/Value.h"
 #include "vm/BytecodeFormatFlags.h"  // JOF_*
 #include "vm/Opcodes.h"
-#include "vm/Printer.h"
 #include "vm/SharedStencil.h"  // js::GCThingIndex
 #include "vm/ThrowMsgKind.h"   // ThrowMsgKind, ThrowCondition
+
+namespace js {
+class Sprinter;
+}  // namespace js
 
 /*
  * JS operation bytecodes.
