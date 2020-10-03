d3e8e5c93336d2d1f444baacc94993f32f11258a
Botond Ballo <botond@mozilla.com>
2020-10-03 21:08:12 +0000

    Bug 1667475 - Consolidate displayport-related flags into a DisplayPortOptions struct. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D92015

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -4275,7 +4275,8 @@ bool ScrollFrameHelper::DecideScrollableLayer(
     MOZ_ASSERT(content->GetProperty(nsGkAtoms::DisplayPortBase));
     nsRect displayPort;
     usingDisplayPort = DisplayPortUtils::GetDisplayPort(
-        content, &displayPort, DisplayportRelativeTo::ScrollFrame);
+        content, &displayPort,
+        DisplayPortOptions().With(DisplayportRelativeTo::ScrollFrame));
 
     if (usingDisplayPort) {
       // Override the dirty rectangle if the displayport has been set.
@@ -4395,7 +4396,8 @@ bool ScrollFrameHelper::IsRectNearlyVisible(const nsRect& aRect) const {
   // Use the right rect depending on if a display port is set.
   nsRect displayPort;
   bool usingDisplayport = DisplayPortUtils::GetDisplayPort(
-      mOuter->GetContent(), &displayPort, DisplayportRelativeTo::ScrollFrame);
+      mOuter->GetContent(), &displayPort,
+      DisplayPortOptions().With(DisplayportRelativeTo::ScrollFrame));
   return aRect.Intersects(
       ExpandRectToNearlyVisible(usingDisplayport ? displayPort : mScrollPort));
 }
