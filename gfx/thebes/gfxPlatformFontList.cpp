c4408a45614e1a3754cc2cb795a9ad2f12bd5019
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-10-03 14:56:08 +0300

    Backed out changeset cab81767be70 (bug 1665196) for mochitest failures on test_mouse_enterleave_iframe.html CLOSED TREE

--- gfx/thebes/gfxPlatformFontList.cpp
+++ gfx/thebes/gfxPlatformFontList.cpp
@@ -127,6 +127,10 @@ const gfxFontEntry::ScriptRange gfxPlatformFontList::sComplexScriptRanges[] = {
     {0, 0, 0, {0, 0, 0}}  // terminator
 };
 
+// prefs for the font info loader
+#define FONT_LOADER_DELAY_PREF "gfx.font_loader.delay"
+#define FONT_LOADER_INTERVAL_PREF "gfx.font_loader.interval"
+
 static const char* kObservedPrefs[] = {"font.",
                                        "font.name-list.",
                                        "intl.accept_languages",  // hmmmm...
@@ -634,7 +638,7 @@ void gfxPlatformFontList::InitOtherFamilyNames(
   // (This is used so we can reliably run reftests that depend on localized
   // font-family names being available.)
   if (aDeferOtherFamilyNamesLoading &&
-      StaticPrefs::gfx_font_loader_delay_AtStartup() > 0) {
+      Preferences::GetUint(FONT_LOADER_DELAY_PREF) > 0) {
     if (!mPendingOtherFamilyNameTask) {
       RefPtr<mozilla::CancelableRunnable> task =
           new InitOtherFamilyNamesRunnable();
@@ -2100,7 +2104,7 @@ void gfxPlatformFontList::InitLoader() {
 }
 
 #define FONT_LOADER_MAX_TIMESLICE \
-  20  // max time for one pass through RunLoader = 20ms
+  100  // max time for one pass through RunLoader = 100ms
 
 bool gfxPlatformFontList::LoadFontInfo() {
   TimeStamp start = TimeStamp::Now();
@@ -2138,16 +2142,12 @@ bool gfxPlatformFontList::LoadFontInfo() {
       }
     }
 
-    // Limit the time spent reading fonts in one pass, unless the font-loader
-    // delay was set to zero, in which case we run to completion even if it
-    // causes some jank.
-    if (StaticPrefs::gfx_font_loader_delay_AtStartup() > 0) {
-      TimeDuration elapsed = TimeStamp::Now() - start;
-      if (elapsed.ToMilliseconds() > FONT_LOADER_MAX_TIMESLICE &&
-          i + 1 != endIndex) {
-        endIndex = i + 1;
-        break;
-      }
+    // limit the time spent reading fonts in one pass
+    TimeDuration elapsed = TimeStamp::Now() - start;
+    if (elapsed.ToMilliseconds() > FONT_LOADER_MAX_TIMESLICE &&
+        i + 1 != endIndex) {
+      endIndex = i + 1;
+      break;
     }
   }
 
@@ -2216,8 +2216,11 @@ void gfxPlatformFontList::CleanupLoader() {
 }
 
 void gfxPlatformFontList::GetPrefsAndStartLoader() {
-  uint32_t delay = std::max(1u, StaticPrefs::gfx_font_loader_delay_AtStartup());
-  StartLoader(delay);
+  uint32_t delay = std::max(1u, Preferences::GetUint(FONT_LOADER_DELAY_PREF));
+  uint32_t interval =
+      std::max(1u, Preferences::GetUint(FONT_LOADER_INTERVAL_PREF));
+
+  StartLoader(delay, interval);
 }
 
 void gfxPlatformFontList::ForceGlobalReflow() {
