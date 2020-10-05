1dafe712105a3396dede2ad145c98cf3cc09f451
Morgan Reschenberg <mreschenberg@mozilla.com>
2020-10-05 19:52:15 +0000

    Bug 1668653: Add support for navigating by heading level with VO r=eeejay
    Differential Revision: https://phabricator.services.mozilla.com/D92136

--- accessible/tests/browser/mac/browser_navigate.js
+++ accessible/tests/browser/mac/browser_navigate.js
@@ -4,6 +4,211 @@
 
 "use strict";
 
+/**
+ * Test navigation of heading levels
+ */
+addAccessibleTask(
+  `
+  <h1 id="a">a</h1>
+  <h2 id="b">b</h2>
+  <h3 id="c">c</h3>
+  <h4 id="d">d</h4>
+  <h5 id="e">e</h5>
+  <h6 id="f">f</h5>
+  <h1 id="g">g</h1>
+  <h2 id="h">h</h2>
+  <h3 id="i">i</h3>
+  <h4 id="j">j</h4>
+  <h5 id="k">k</h5>
+  <h6 id="l">l</h5>
+  this is some regular text that should be ignored
+  `,
+  async (browser, accDoc) => {
+    const searchPred = {
+      AXSearchKey: "AXHeadingLevel1SearchKey",
+      AXImmediateDescendantsOnly: 0,
+      AXResultsLimit: -1,
+      AXDirection: "AXDirectionNext",
+    };
+
+    const webArea = accDoc.nativeInterface.QueryInterface(
+      Ci.nsIAccessibleMacInterface
+    );
+
+    let h1Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h1Count, "Found two h1 items");
+
+    let h1s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const a = getNativeInterface(accDoc, "a");
+    const g = getNativeInterface(accDoc, "g");
+
+    is(
+      a.getAttributeValue("AXValue"),
+      h1s[0].getAttributeValue("AXValue"),
+      "Found correct h1 heading"
+    );
+
+    is(
+      g.getAttributeValue("AXValue"),
+      h1s[1].getAttributeValue("AXValue"),
+      "Found correct h1 heading"
+    );
+
+    searchPred.AXSearchKey = "AXHeadingLevel2SearchKey";
+
+    let h2Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h2Count, "Found two h2 items");
+
+    let h2s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const b = getNativeInterface(accDoc, "b");
+    const h = getNativeInterface(accDoc, "h");
+
+    is(
+      b.getAttributeValue("AXValue"),
+      h2s[0].getAttributeValue("AXValue"),
+      "Found correct h2 heading"
+    );
+
+    is(
+      h.getAttributeValue("AXValue"),
+      h2s[1].getAttributeValue("AXValue"),
+      "Found correct h2 heading"
+    );
+
+    searchPred.AXSearchKey = "AXHeadingLevel3SearchKey";
+
+    let h3Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h3Count, "Found two h3 items");
+
+    let h3s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const c = getNativeInterface(accDoc, "c");
+    const i = getNativeInterface(accDoc, "i");
+
+    is(
+      c.getAttributeValue("AXValue"),
+      h3s[0].getAttributeValue("AXValue"),
+      "Found correct h3 heading"
+    );
+
+    is(
+      i.getAttributeValue("AXValue"),
+      h3s[1].getAttributeValue("AXValue"),
+      "Found correct h3 heading"
+    );
+
+    searchPred.AXSearchKey = "AXHeadingLevel4SearchKey";
+
+    let h4Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h4Count, "Found two h4 items");
+
+    let h4s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const d = getNativeInterface(accDoc, "d");
+    const j = getNativeInterface(accDoc, "j");
+
+    is(
+      d.getAttributeValue("AXValue"),
+      h4s[0].getAttributeValue("AXValue"),
+      "Found correct h4 heading"
+    );
+
+    is(
+      j.getAttributeValue("AXValue"),
+      h4s[1].getAttributeValue("AXValue"),
+      "Found correct h4 heading"
+    );
+
+    searchPred.AXSearchKey = "AXHeadingLevel5SearchKey";
+
+    let h5Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h5Count, "Found two h5 items");
+
+    let h5s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const e = getNativeInterface(accDoc, "e");
+    const k = getNativeInterface(accDoc, "k");
+
+    is(
+      e.getAttributeValue("AXValue"),
+      h5s[0].getAttributeValue("AXValue"),
+      "Found correct h5 heading"
+    );
+
+    is(
+      k.getAttributeValue("AXValue"),
+      h5s[1].getAttributeValue("AXValue"),
+      "Found correct h5 heading"
+    );
+
+    searchPred.AXSearchKey = "AXHeadingLevel6SearchKey";
+
+    let h6Count = webArea.getParameterizedAttributeValue(
+      "AXUIElementCountForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    is(2, h6Count, "Found two h6 items");
+
+    let h6s = webArea.getParameterizedAttributeValue(
+      "AXUIElementsForSearchPredicate",
+      NSDictionary(searchPred)
+    );
+
+    const f = getNativeInterface(accDoc, "f");
+    const l = getNativeInterface(accDoc, "l");
+
+    is(
+      f.getAttributeValue("AXValue"),
+      h6s[0].getAttributeValue("AXValue"),
+      "Found correct h6 heading"
+    );
+
+    is(
+      l.getAttributeValue("AXValue"),
+      h6s[1].getAttributeValue("AXValue"),
+      "Found correct h6 heading"
+    );
+  }
+);
+
 /*
  * Test rotor with blockquotes
  */
