391fc985ecf676d3e83822e9b8b53167293947bb
Marco Bonardo <mbonardo@mozilla.com>
2020-10-05 17:17:34 +0000

    Bug 1668982 - Perma browser_searchMode_preview.js failure on beta due to lack of update2. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D92462

--- browser/components/urlbar/tests/browser/browser_searchMode_preview.js
+++ browser/components/urlbar/tests/browser/browser_searchMode_preview.js
@@ -14,6 +14,7 @@ add_task(async function setup() {
   await SpecialPowers.pushPrefEnv({
     set: [
       ["browser.urlbar.update2", true],
+      ["browser.urlbar.update2.localOneOffs", true],
       ["browser.urlbar.update2.oneOffsRefresh", true],
     ],
   });
