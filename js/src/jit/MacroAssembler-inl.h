3a63d1b3d403a988403bfefe40da5d7ca1d1521f
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2020-10-05 18:46:22 +0000

    Bug 1661256 part 1 - Add a templatized callWithABI function. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D91781

--- js/src/jit/MacroAssembler-inl.h
+++ js/src/jit/MacroAssembler-inl.h
@@ -18,6 +18,8 @@
 #include "jit/JitFrames.h"
 #include "vm/ProxyObject.h"
 
+#include "jit/ABIFunctionList-inl.h"
+
 #if defined(JS_CODEGEN_X86)
 #  include "jit/x86/MacroAssembler-x86-inl.h"
 #elif defined(JS_CODEGEN_X64)
@@ -100,6 +102,14 @@ void MacroAssembler::callWithABI(void* fun, MoveOp::Type result,
                                  CheckUnsafeCallWithABI check) {
   AutoProfilerCallInstrumentation profiler(*this);
   callWithABINoProfiler(fun, result, check);
+}
+
+template <typename Sig, Sig fun>
+void MacroAssembler::callWithABI(MoveOp::Type result,
+                                 CheckUnsafeCallWithABI check) {
+  ABIFunction<Sig, fun> abiFun;
+  AutoProfilerCallInstrumentation profiler(*this);
+  callWithABINoProfiler(abiFun.address(), result, check);
 }
 
 void MacroAssembler::callWithABI(Register fun, MoveOp::Type result) {
