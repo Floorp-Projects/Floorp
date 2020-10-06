9d6f081a11a60c51355a74c050ca1e03666540b0
Emilio Cobos Álvarez <emilio@crisal.io>
2020-10-06 00:06:30 +0000

    Bug 1663475 - Paint caret geometry from the containing block. r=mats
    This matches other browsers and shouldn't be too terrible, though I'm
    not quite a fan of it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D89483

--- layout/painting/nsDisplayList.cpp
+++ layout/painting/nsDisplayList.cpp
@@ -4935,20 +4935,18 @@ bool nsDisplayCaret::CreateWebRenderCommands(
     mozilla::layers::RenderRootStateManager* aManager,
     nsDisplayListBuilder* aDisplayListBuilder) {
   using namespace mozilla::layers;
-  int32_t contentOffset;
-  nsIFrame* frame = mCaret->GetFrame(&contentOffset);
+  nsRect caretRect;
+  nsRect hookRect;
+  nscolor caretColor;
+  nsIFrame* frame =
+      mCaret->GetPaintGeometry(&caretRect, &hookRect, &caretColor);
+  MOZ_ASSERT(frame == mFrame, "We're referring different frame");
   if (!frame) {
     return true;
   }
-  NS_ASSERTION(frame == mFrame, "We're referring different frame");
 
   int32_t appUnitsPerDevPixel = frame->PresContext()->AppUnitsPerDevPixel();
-
-  nsRect caretRect;
-  nsRect hookRect;
-  mCaret->ComputeCaretRects(frame, contentOffset, &caretRect, &hookRect);
-
-  gfx::DeviceColor color = ToDeviceColor(frame->GetCaretColorAt(contentOffset));
+  gfx::DeviceColor color = ToDeviceColor(caretColor);
   LayoutDeviceRect devCaretRect = LayoutDeviceRect::FromAppUnits(
       caretRect + ToReferenceFrame(), appUnitsPerDevPixel);
   LayoutDeviceRect devHookRect = LayoutDeviceRect::FromAppUnits(
