7c0d9e402e5de430cd4c01dc5ba3c1b2f6e8a638
André Bargull <andre.bargull@gmail.com>
2020-10-06 13:43:50 +0000

    Bug 1669181 - Part 26: Remove unused MIRGenerator arguments from CallInfo methods. r=jandem
    The `MIRGenerator*` arguments is no longer needed after the refactorings from
    bug 1646378.
    
    After removing the `MIRGenerator*` arguments, also remove the include for
    "jit/MIRGenerator.h".
    
    Depends on D92571
    
    Differential Revision: https://phabricator.services.mozilla.com/D92572

--- js/src/jit/WarpBuilder.cpp
+++ js/src/jit/WarpBuilder.cpp
@@ -3236,7 +3236,7 @@ bool WarpBuilder::buildInlinedCall(BytecodeLocation loc,
   callInfo.setImplicitlyUsedUnchecked();
 
   // Capture formals in the outer resume point.
-  if (!callInfo.pushCallStack(&mirGen(), current)) {
+  if (!callInfo.pushCallStack(current)) {
     return false;
   }
   MResumePoint* outerResumePoint =
