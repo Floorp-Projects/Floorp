96c2771bc331f92a2038baae18909c93b9bbc293
Matt Woodrow <mwoodrow@mozilla.com>
2020-10-06 01:45:08 +0000

    Bug 1664804 - Build top layer content before we build scrolled content. r=miko
    Differential Revision: https://phabricator.services.mozilla.com/D91668

--- layout/generic/nsGfxScrollFrame.h
+++ layout/generic/nsGfxScrollFrame.h
@@ -95,8 +95,7 @@ class ScrollFrameHelper : public nsIReflowCallback {
   // wrapped in the async zoom container, if we're building one.
   // It should not be called with an ASR setter on the stack, as the
   // top-layer items handle setting up their own ASRs.
-  void MaybeAddTopLayerItems(nsDisplayListBuilder* aBuilder,
-                             const nsDisplayListSet& aLists);
+  nsDisplayWrapList* MaybeCreateTopLayerItems(nsDisplayListBuilder* aBuilder);
 
   void AppendScrollPartsTo(nsDisplayListBuilder* aBuilder,
                            const nsDisplayListSet& aLists, bool aCreateLayer,
