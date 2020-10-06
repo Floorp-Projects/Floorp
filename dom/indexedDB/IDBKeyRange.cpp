9ad924671bf15c4b3dd600b5a319aaf44486c0a4
Simon Giesecke <sgiesecke@mozilla.com>
2020-10-06 08:37:59 +0000

    Bug 1665347 - Make IDBResult based on mozilla::Result. r=dom-workers-and-storage-reviewers,janv
    Differential Revision: https://phabricator.services.mozilla.com/D90562

--- dom/indexedDB/IDBKeyRange.cpp
+++ dom/indexedDB/IDBKeyRange.cpp
@@ -22,8 +22,8 @@ namespace {
 void GetKeyFromJSVal(JSContext* aCx, JS::Handle<JS::Value> aVal, Key& aKey,
                      ErrorResult& aRv) {
   auto result = aKey.SetFromJSVal(aCx, aVal);
-  if (!result.Is(Ok)) {
-    aRv = result.ExtractErrorResult(
+  if (result.isErr()) {
+    aRv = result.unwrapErr().ExtractErrorResult(
         InvalidMapsTo<NS_ERROR_DOM_INDEXEDDB_DATA_ERR>);
     return;
   }
