d56907816f9de3e80d35ea6270631b4ffe215d61
Simon Giesecke <sgiesecke@mozilla.com>
2020-10-06 14:20:17 +0000

    Bug 1663924 - Replace output parameter of ReclaimFreePagesWhileIdle by return value. r=dom-workers-and-storage-reviewers,ttung,janv
    Differential Revision: https://phabricator.services.mozilla.com/D91160

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -1336,11 +1336,12 @@ class DatabaseConnection final {
   Result<uint32_t, nsresult> GetFreelistCount(
       CachedStatement& aCachedStatement);
 
-  nsresult ReclaimFreePagesWhileIdle(CachedStatement& aFreelistStatement,
-                                     CachedStatement& aRollbackStatement,
-                                     uint32_t aFreelistCount,
-                                     bool aNeedsCheckpoint,
-                                     bool* aFreedSomePages);
+  /**
+   * On success, returns whether some pages were freed.
+   */
+  Result<bool, nsresult> ReclaimFreePagesWhileIdle(
+      CachedStatement& aFreelistStatement, CachedStatement& aRollbackStatement,
+      uint32_t aFreelistCount, bool aNeedsCheckpoint);
 
   Result<int64_t, nsresult> GetFileSize(const nsAString& aPath);
 };
@@ -7402,20 +7403,22 @@ void DatabaseConnection::DoIdleProcessing(bool aNeedsCheckpoint) {
     mInReadTransaction = false;
   }
 
-  bool freedSomePages = false;
-
-  if (freelistCount) {
-    nsresult rv =
-        ReclaimFreePagesWhileIdle(freelistStmt, rollbackStmt, freelistCount,
-                                  aNeedsCheckpoint, &freedSomePages);
-    if (NS_WARN_IF(NS_FAILED(rv))) {
-      MOZ_ASSERT(!freedSomePages);
-    }
+  const bool freedSomePages = freelistCount && [this, &freelistStmt,
+                                                &rollbackStmt, freelistCount,
+                                                aNeedsCheckpoint] {
+    // Warn in case of an error, but do not propagate it. Just indicate we
+    // didn't free any pages.
+    IDB_TRY_INSPECT(const bool& res,
+                    ReclaimFreePagesWhileIdle(freelistStmt, rollbackStmt,
+                                              freelistCount, aNeedsCheckpoint),
+                    false);
 
     // Make sure we didn't leave a transaction running.
     MOZ_ASSERT(!mInReadTransaction);
     MOZ_ASSERT(!mInWriteTransaction);
-  }
+
+    return res;
+  }();
 
   // Truncate the WAL if we were asked to or if we managed to free some space.
   if (aNeedsCheckpoint || freedSomePages) {
@@ -7434,14 +7437,13 @@ void DatabaseConnection::DoIdleProcessing(bool aNeedsCheckpoint) {
   }
 }
 
-nsresult DatabaseConnection::ReclaimFreePagesWhileIdle(
+Result<bool, nsresult> DatabaseConnection::ReclaimFreePagesWhileIdle(
     CachedStatement& aFreelistStatement, CachedStatement& aRollbackStatement,
-    uint32_t aFreelistCount, bool aNeedsCheckpoint, bool* aFreedSomePages) {
+    uint32_t aFreelistCount, bool aNeedsCheckpoint) {
   AssertIsOnConnectionThread();
   MOZ_ASSERT(aFreelistStatement);
   MOZ_ASSERT(aRollbackStatement);
   MOZ_ASSERT(aFreelistCount);
-  MOZ_ASSERT(aFreedSomePages);
   MOZ_ASSERT(!mInReadTransaction);
   MOZ_ASSERT(!mInWriteTransaction);
 
@@ -7452,8 +7454,7 @@ nsresult DatabaseConnection::ReclaimFreePagesWhileIdle(
   MOZ_ASSERT(currentThread);
 
   if (NS_HasPendingEvents(currentThread)) {
-    *aFreedSomePages = false;
-    return NS_OK;
+    return false;
   }
 
   // Make all the statements we'll need up front.
@@ -7530,8 +7531,7 @@ nsresult DatabaseConnection::ReclaimFreePagesWhileIdle(
             mInWriteTransaction = false;
           }));
 
-  *aFreedSomePages = freedSomePages;
-  return NS_OK;
+  return freedSomePages;
 }
 
 Result<uint32_t, nsresult> DatabaseConnection::GetFreelistCount(
