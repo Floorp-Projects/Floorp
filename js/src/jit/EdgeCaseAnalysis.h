95e81d7980e30bea0a6abd683664aaee8c27a32c
André Bargull <andre.bargull@gmail.com>
2020-10-06 13:40:53 +0000

    Bug 1669181 - Part 24: Clean-up jit/EdgeCaseAnalysis.h includes. r=jandem
    Depends on D92569
    
    Differential Revision: https://phabricator.services.mozilla.com/D92570

--- js/src/jit/EdgeCaseAnalysis.h
+++ js/src/jit/EdgeCaseAnalysis.h
@@ -7,11 +7,12 @@
 #ifndef jit_EdgeCaseAnalysis_h
 #define jit_EdgeCaseAnalysis_h
 
-#include "jit/MIRGenerator.h"
+#include "mozilla/Attributes.h"
 
 namespace js {
 namespace jit {
 
+class MIRGenerator;
 class MIRGraph;
 
 class EdgeCaseAnalysis {
