df21e1784ffe3ff245640886a1c604730d23c0f5
Kershaw Chang <kershaw@mozilla.com>
2020-10-06 08:42:01 +0000

    Bug 1669270 - Don't call OnProxyConnectComplete if it's websocket over h2 r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D92494

--- netwerk/protocol/http/TunnelUtils.cpp
+++ netwerk/protocol/http/TunnelUtils.cpp
@@ -1218,14 +1218,15 @@ bool SpdyConnectTransaction::MapStreamToHttpConnection(
       gHttpHandler->ConnMgr()->MakeConnectionHandle(mTunneledConn);
   mDrivingTransaction->SetConnection(wrappedConn);
   mDrivingTransaction->MakeSticky();
-  mDrivingTransaction->OnProxyConnectComplete(aHttpResponseCode);
-
-  if (aHttpResponseCode == 407) {
-    mDrivingTransaction->SetFlat407Headers(aFlat407Headers);
-    mDrivingTransaction->SetProxyConnectFailed();
-  }
 
   if (!mIsWebsocket) {
+    mDrivingTransaction->OnProxyConnectComplete(aHttpResponseCode);
+
+    if (aHttpResponseCode == 407) {
+      mDrivingTransaction->SetFlat407Headers(aFlat407Headers);
+      mDrivingTransaction->SetProxyConnectFailed();
+    }
+
     // jump the priority and start the dispatcher
     Unused << gHttpHandler->InitiateTransaction(
         mDrivingTransaction, nsISupportsPriority::PRIORITY_HIGHEST - 60);
