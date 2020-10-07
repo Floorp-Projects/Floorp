1d39eca138eed10e02abc268b402939b69194b46
Emilio Cobos Álvarez <emilio@crisal.io>
2020-10-07 20:45:08 +0000

    Bug 140718 - Add a "Print selection only" entry to the context menu. r=Gijs,jwatt,fluent-reviewers
    This allows to use the feature from the context menu and is very
    straight-forward.
    
    This has the benefit of working with fission (as print from the focused
    frame) and so on.
    
    Making a checkbox on the UI on top of this is doable, but it is a lot
    more complex (requires a bunch of both front-end and platform work)
    because undoing this implies restoring the DOM which "print selection"
    removes.
    
    I think the context menu entry is very useful regardless of whether we
    add a checkbox to undo it, and other browsers (with smaller context
    menus) seem to agree.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92205

---
+++ toolkit/components/printing/tests/browser_print_selection.js
@@ -0,0 +1,56 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+const frameSource =
+  "<a href='about:mozilla'>some text</a><a id='other' href='about:about'>other text</a>";
+const sources = [
+  `<html><iframe id="f" srcdoc="${frameSource}"></iframe></html>`,
+  `<html><iframe id="f" src="https://example.com/document-builder.sjs?html=${frameSource}"></iframe></html>`,
+];
+
+add_task(async function print_selection() {
+  // Testing the native print dialog is much harder.
+  await SpecialPowers.pushPrefEnv({
+    set: [["print.tab_modal.enabled", true]],
+  });
+
+  for (let source of sources) {
+    is(
+      document.querySelector(".printPreviewBrowser"),
+      null,
+      "There shouldn't be any print preview browser"
+    );
+
+    await BrowserTestUtils.withNewTab(
+      "data:text/html," + source,
+      async function(browser) {
+        let frameBC = browser.browsingContext.children[0];
+        await SpecialPowers.spawn(frameBC, [], () => {
+          let element = content.document.getElementById("other");
+          content.focus();
+          content.getSelection().selectAllChildren(element);
+        });
+
+        // If you change this, change nsContextMenu.printSelection() too.
+        PrintUtils.startPrintWindow(frameBC, null, /* aSelectionOnly = */ true);
+
+        await BrowserTestUtils.waitForCondition(
+          () => !!document.querySelector(".printPreviewBrowser")
+        );
+
+        let previewBrowser = document.querySelector(".printPreviewBrowser");
+        async function getPreviewText() {
+          return SpecialPowers.spawn(previewBrowser, [], function() {
+            return content.document.body.textContent;
+          });
+        }
+        // The preview process is async, wait for it to not be empty.
+        await BrowserTestUtils.waitForCondition(getPreviewText);
+        let textContent = await getPreviewText();
+        is(textContent, "other text", "Correct content loaded");
+        // Closing the tab also closes the preview dialog and such.
+      }
+    );
+  }
+});
