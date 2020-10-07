7e5654b922c1f1622519210b496a5e2d74a74cd7
Razvan Maries <rmaries@mozilla.com>
2020-10-07 17:51:55 +0300

    Backed out changeset aaf5c5213d8d (bug 1665062) for perma failures on test_http_background_auth_request.html. CLOSED TREE

--- netwerk/protocol/http/TRRServiceChannel.h
+++ netwerk/protocol/http/TRRServiceChannel.h
@@ -73,7 +73,6 @@ class TRRServiceChannel : public HttpBaseChannel,
                                  const nsAString& aURL,
                                  const nsAString& aContentType) override;
   NS_IMETHOD SetupFallbackChannel(const char* aFallbackKey) override;
-  NS_IMETHOD GetIsAuthChannel(bool* aIsAuthChannel) override;
 
   NS_IMETHOD SetNotificationCallbacks(
       nsIInterfaceRequestor* aCallbacks) override;
