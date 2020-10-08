b1b5c27cce1c6b280b288389b656ecc4624cdf3a
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-10-08 08:53:16 +0300

    Backed out changeset 7c05a131c811 (bug 1668476) for bc failure on /browser_ext_tabs_printPreview.js . CLOSED TREE

--- toolkit/components/printing/content/print.js
+++ toolkit/components/printing/content/print.js
@@ -65,7 +65,6 @@ function serializeSettings(settings, logPrefix) {
   return nameValues;
 }
 
-let printPending = false;
 let deferredTasks = [];
 function createDeferredTask(fn, timeout) {
   let task = new DeferredTask(fn, timeout);
@@ -77,14 +76,7 @@ function cancelDeferredTasks() {
   for (let task of deferredTasks) {
     task.disarm();
   }
-  PrintEventHandler._updatePrintPreviewTask.disarm();
   deferredTasks = [];
-}
-
-async function finalizeDeferredTasks() {
-  await Promise.all(deferredTasks.map(task => task.finalize()));
-  printPending = true;
-  await PrintEventHandler._updatePrintPreviewTask.finalize();
 }
 
 document.addEventListener(
@@ -266,7 +258,7 @@ var PrintEventHandler = {
 
     // Use a DeferredTask for updating the preview. This will ensure that we
     // only have one update running at a time.
-    this._updatePrintPreviewTask = new DeferredTask(async () => {
+    this._updatePrintPreviewTask = createDeferredTask(async () => {
       await initialPreviewDone;
       await this._updatePrintPreview();
       document.dispatchEvent(new CustomEvent("preview-updated"));
@@ -354,8 +346,6 @@ var PrintEventHandler = {
     }
 
     await window._initialized;
-
-    await finalizeDeferredTasks();
 
     // This seems like it should be handled automatically but it isn't.
     Services.prefs.setStringPref("print_printer", settings.printerName);
@@ -516,10 +506,7 @@ var PrintEventHandler = {
       changedSettings,
       !!changedSettings.printerName
     );
-
-    if (shouldPreviewUpdate && !printPending) {
-      // We do not need to arm the preview task if the user has already printed
-      // and finalized any deferred tasks.
+    if (shouldPreviewUpdate) {
       this.updatePrintPreview();
     }
     document.dispatchEvent(
@@ -1561,17 +1548,6 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
     this._percentScale.addEventListener("keypress", this);
     this._percentScale.addEventListener("paste", this);
     this.addEventListener("input", this);
-
-    this._updateScaleTask = createDeferredTask(
-      () => this.updateScale(),
-      INPUT_DELAY_MS
-    );
-  }
-
-  updateScale() {
-    this.dispatchSettingsChange({
-      scaling: Number(this._percentScale.value / 100),
-    });
   }
 
   update(settings) {
@@ -1593,11 +1569,6 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
   }
 
   handleEvent(e) {
-    if (e.type == "change") {
-      // We listen to input events, no need for change too.
-      return;
-    }
-
     if (e.type == "keypress") {
       this.handleKeypress(e);
       return;
@@ -1606,8 +1577,6 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
     if (e.type === "paste") {
       this.handlePaste(e);
     }
-
-    this._updateScaleTask.disarm();
 
     if (e.target == this._shrinkToFitChoice || e.target == this._scaleChoice) {
       if (!this._percentScale.checkValidity()) {
@@ -1623,8 +1592,14 @@ class ScaleInput extends PrintUIControlMixin(HTMLElement) {
       });
       this._scaleError.hidden = true;
     } else if (e.type == "input") {
+      window.clearTimeout(this.updateSettingsTimeoutId);
+
       if (this._percentScale.checkValidity()) {
-        this._updateScaleTask.arm();
+        this.updateSettingsTimeoutId = window.setTimeout(() => {
+          this.dispatchSettingsChange({
+            scaling: Number(this._percentScale.value / 100),
+          });
+        }, INPUT_DELAY_MS);
       }
     }
 
