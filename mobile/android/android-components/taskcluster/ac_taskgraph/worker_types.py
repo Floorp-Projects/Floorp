78e6bd9e9d1c56606384eee56979e7ae2577e338
Alex López <alex.lopez.zorzano@gmail.com>
2020-10-08 20:27:59 +0200

    [components] Add version to beetmover schema

--- mobile/android/android-components/taskcluster/ac_taskgraph/worker_types.py
+++ mobile/android/android-components/taskcluster/ac_taskgraph/worker_types.py
@@ -65,6 +65,7 @@ def build_scriptworker_signing_payload(config, task, task_def):
     "scriptworker-beetmover",
     schema={
         Required("action"): text_type,
+        Required("version"): text_type,
         Required("artifact-map"): [{
             Required("paths"): {
                 Any(text_type): {
