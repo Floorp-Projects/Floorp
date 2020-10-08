d8bd941a3a6670a36449a62526f7137cdb06d957
dev <dev@devs-MacBook-Pro.local>
2020-10-08 20:21:18 +0000

    Bug 1667124 - Fix invalidation of picture cache tiles attached to zoom nodes. r=aosmond
    Detect when the scale of the root spatial node of a picture cache
    changes, and invalidate tiles when that occurs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92870

--- gfx/wr/tileview/src/main.rs
+++ gfx/wr/tileview/src/main.rs
@@ -60,6 +60,7 @@ static CSS_PRIM_COUNT: &str              = "fill:#40f0f0;fill-opacity:0.1;";
 static CSS_CONTENT: &str                 = "fill:#f04040;fill-opacity:0.1;";
 static CSS_COMPOSITOR_KIND_CHANGED: &str = "fill:#f0c070;fill-opacity:0.1;";
 static CSS_VALID_RECT_CHANGED: &str      = "fill:#ff00ff;fill-opacity:0.1;";
+static CSS_SCALE_CHANGED: &str           = "fill:#ff80ff;fill-opacity:0.1;";
 
 // parameters to tweak the SVG generation
 struct SvgSettings {
@@ -112,6 +113,7 @@ fn tile_to_svg(key: TileOffset,
             Some(InvalidationReason::CompositorKindChanged) => CSS_COMPOSITOR_KIND_CHANGED.to_string(),
             Some(InvalidationReason::Content { .. } ) => CSS_CONTENT.to_string(),
             Some(InvalidationReason::ValidRectChanged) => CSS_VALID_RECT_CHANGED.to_string(),
+            Some(InvalidationReason::ScaleChanged) => CSS_SCALE_CHANGED.to_string(),
             None => {
                 let mut background = tile.background_color;
                 if background.is_none() {
