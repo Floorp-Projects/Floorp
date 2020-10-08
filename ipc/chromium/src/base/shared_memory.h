3680ce4b1970b4953036b3c3dfb1fa16d4b4bdcb
Jed Davis <jld@mozilla.com>
2020-10-08 02:25:20 +0000

    Bug 1440203 - Support memfd_create in IPC shared memory. r=glandium
    This commit also allows `memfd_create` in the seccomp-bpf policy for all
    process types.
    
    `memfd_create` is an API added in Linux 3.17 (and adopted by FreeBSD
    for the upcoming version 13) for creating anonymous shared memory
    not connected to any filesystem.  Supporting it means that sandboxed
    child processes on Linux can create shared memory directly instead of
    messaging a broker, which is unavoidably slower, and it should avoid
    the problems we'd been seeing with overly small `/dev/shm` in container
    environments (which were causing serious problems for using Firefox for
    automated testing of frontend projects).
    
    `memfd_create` also introduces the related operation of file seals:
    irrevocably preventing types of modifications to a file.  Unfortunately,
    the most useful one, `F_SEAL_WRITE`, can't be relied on; see the large
    comment in `SharedMemory:ReadOnlyCopy` for details.  So we still use
    the applicable seals as defense in depth, but read-only copies are
    implemented on Linux by using procfs (and see the comments on the
    `ReadOnlyCopy` function in `shared_memory_posix.cc` for the subtleties
    there).
    
    There's also a FreeBSD implementation, using `cap_rights_limit` for
    read-only copies, if the build host is new enough to have the
    `memfd_create` function.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90605

--- ipc/chromium/src/base/shared_memory.h
+++ ipc/chromium/src/base/shared_memory.h
@@ -218,6 +218,7 @@ class SharedMemory {
   bool external_section_ = false;
 #elif defined(OS_POSIX)
   mozilla::UniqueFileHandle frozen_file_;
+  bool is_memfd_ = false;
 #endif
   bool read_only_ = false;
   bool freezeable_ = false;
