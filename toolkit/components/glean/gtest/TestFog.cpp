e971b34ac17f566b18c118930fe33ae6155225ed
Chris H-C <chutten@mozilla.com>
2020-10-08 09:22:12 +0000

    Bug 1653605 - Register builtin Glean pings so they can be debug-sent. r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D92819

--- toolkit/components/glean/gtest/TestFog.cpp
+++ toolkit/components/glean/gtest/TestFog.cpp
@@ -4,7 +4,10 @@
 
 #include "gtest/gtest.h"
 
+#include "mozilla/Preferences.h"
 #include "nsString.h"
+
+using mozilla::Preferences;
 
 extern "C" {
 // This function is called by the rust code in test.rs if a non-fatal test
@@ -14,6 +17,7 @@ void GTest_FOG_ExpectFailure(const char* aMessage) {
 }
 
 nsresult fog_init();
+nsresult fog_submit_ping(const nsACString* aPingName);
 }
 
 // Initialize FOG exactly once.
@@ -24,3 +28,15 @@ TEST(FOG, FogInitDoesntCrash)
 extern "C" void Rust_MeasureInitializeTime();
 TEST(FOG, TestMeasureInitializeTime)
 { Rust_MeasureInitializeTime(); }
+
+TEST(FOG, BuiltinPingsRegistered)
+{
+  Preferences::SetInt("telemetry.fog.test.localhost_port", -1);
+  nsAutoCString metricsPingName("metrics");
+  nsAutoCString baselinePingName("baseline");
+  nsAutoCString eventsPingName("events");
+  ASSERT_EQ(NS_OK, fog_submit_ping(&metricsPingName));
+  // This will probably change to NS_OK once "duration" is implemented.
+  ASSERT_EQ(NS_ERROR_NO_CONTENT, fog_submit_ping(&baselinePingName));
+  ASSERT_EQ(NS_ERROR_NO_CONTENT, fog_submit_ping(&eventsPingName));
+}
