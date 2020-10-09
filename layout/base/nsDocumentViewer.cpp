824fb768e1448b254b4e78b0634811e195cfb57e
Kris Maglione <maglione.k@gmail.com>
2020-10-09 18:29:34 +0000

    Bug 1655866: Part 9 - Use the same PermitUnloadAction enum in WGP and nsIContentViewer. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D90939

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -1207,7 +1207,7 @@ nsDocumentViewer::PermitUnload(PermitUnloadAction aAction,
   RefPtr<nsDocumentViewer> kungFuDeathGrip(this);
 
   if (StaticPrefs::dom_disable_beforeunload()) {
-    aAction = eDontPromptAndUnload;
+    aAction = PermitUnloadAction::Unload;
   }
 
   *aPermitUnload = true;
@@ -1245,8 +1245,8 @@ nsDocumentViewer::PermitUnload(PermitUnloadAction aAction,
     if (!foundBlocker) {
       return NS_OK;
     }
-    if (aAction != ePrompt) {
-      *aPermitUnload = aAction == eDontPromptAndUnload;
+    if (aAction != PermitUnloadAction::Prompt) {
+      *aPermitUnload = aAction == PermitUnloadAction::Unload;
       return NS_OK;
     }
   }
