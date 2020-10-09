51278a36a599ad32180e7eda317a827e307d4f97
Cosmin Sabou <csabou@mozilla.com>
2020-10-09 07:39:32 +0300

    Backed out 6 changesets (bug 1525076) for OSX xpcshell failures on test_ASRouterTargeting_attribution. CLOSED TREE
    Backed out changeset 25fd5f2e276e (bug 1525076)
    Backed out changeset 4f2c3fe9ee78 (bug 1525076)
    Backed out changeset e7e05723ceae (bug 1525076)
    Backed out changeset d0c81fd932c7 (bug 1525076)
    Backed out changeset c1629391c635 (bug 1525076)
    Backed out changeset e6b09ef2a26c (bug 1525076)

---
+++ browser/components/attribution/test/xpcshell/test_attribution.js
@@ -0,0 +1,39 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+"use strict";
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { AttributionCode } = ChromeUtils.import(
+  "resource:///modules/AttributionCode.jsm"
+);
+
+add_task(async function test_attribution() {
+  let appPath = Services.dirsvc.get("GreD", Ci.nsIFile).parent.parent.path;
+  let attributionSvc = Cc["@mozilla.org/mac-attribution;1"].getService(
+    Ci.nsIMacAttributionService
+  );
+
+  attributionSvc.setReferrerUrl(appPath, "", true);
+  let referrer = attributionSvc.getReferrerUrl(appPath);
+  equal(referrer, "", "force an empty referrer url");
+
+  // Set a url referrer, testing both utm and non-utm codes
+  let url = "http://example.com?content=foo&utm_source=bar&utm_content=baz";
+  attributionSvc.setReferrerUrl(appPath, url, true);
+  referrer = attributionSvc.getReferrerUrl(appPath);
+  equal(referrer, url, "overwrite referrer url");
+
+  // Does not overwrite existing properties.
+  attributionSvc.setReferrerUrl(appPath, "http://test.com", false);
+  referrer = attributionSvc.getReferrerUrl(appPath);
+  equal(referrer, url, "referrer url is not changed");
+
+  let result = await AttributionCode.getAttrDataAsync();
+  Assert.deepEqual(
+    result,
+    { content: "foo", source: "bar" },
+    "parsed attributes match"
+  );
+});
