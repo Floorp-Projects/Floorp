e9e00c70071537efc5decb9e7ec8acd1a42e4a7e
Emilio Cobos Álvarez <emilio@crisal.io>
2020-10-09 03:20:34 +0000

    Bug 1669459 - Introduce InspectorUtils.supports(). r=rcaliman,heycam
    Which allows to run CSS.supports in chrome/user-agent context. We should
    probably add a couple more tweaks for stuff like quirks or what not (the
    `ParsingMode` enum), but that seem lower priority.
    
    I only added the one-value version of CSS.supports because it should be enough
    and supporting the two value version required a bit of awkward code due to when
    we parse the CSS property name right now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92585

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -301,7 +301,6 @@ void nsLayoutStatics::Shutdown() {
 
   if (XRE_IsParentProcess() || XRE_IsContentProcess()) {
     ShutdownServo();
-    URLExtraData::ReleaseDummy();
   }
 
   mozilla::dom::AbstractRange::Shutdown();
