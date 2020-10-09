9c16898a12058e5acb7d7d8e709556b0999b9f9b
Razvan Maries <rmaries@mozilla.com>
2020-10-09 20:17:23 +0300

    Backed out 6 changesets (bug 1525076) for ES Lint failure. CLOSED TREE
    Backed out changeset 912fcc3cb274 (bug 1525076)
    Backed out changeset 690d730341c6 (bug 1525076)
    Backed out changeset 5dd08176812c (bug 1525076)
    Backed out changeset 963c8d33d779 (bug 1525076)
    Backed out changeset f7d26b270884 (bug 1525076)
    Backed out changeset 95fd52531439 (bug 1525076)

--- browser/components/newtab/test/xpcshell/test_ASRouterTargeting_attribution.js
+++ browser/components/newtab/test/xpcshell/test_ASRouterTargeting_attribution.js
@@ -11,13 +11,10 @@ const { AttributionCode } = ChromeUtils.import(
 const { ASRouterTargeting } = ChromeUtils.import(
   "resource://activity-stream/lib/ASRouterTargeting.jsm"
 );
-const { MacAttribution } = ChromeUtils.import(
-  "resource:///modules/MacAttribution.jsm"
-);
 
 add_task(async function check_attribution_data() {
   // Some setup to fake the correct attribution data
-  const appPath = MacAttribution.applicationPath;
+  const appPath = Services.dirsvc.get("GreD", Ci.nsIFile).parent.parent.path;
   const attributionSvc = Cc["@mozilla.org/mac-attribution;1"].getService(
     Ci.nsIMacAttributionService
   );
@@ -26,7 +23,7 @@ add_task(async function check_attribution_data() {
   const referrer = `https://allizom.org/anything/?utm_campaign=${campaign}&utm_source=${source}`;
   attributionSvc.setReferrerUrl(appPath, referrer, true);
   AttributionCode._clearCache();
-  await AttributionCode.getAttrDataAsync();
+  AttributionCode.getAttrDataAsync();
 
   const {
     campaign: attributionCampain,
