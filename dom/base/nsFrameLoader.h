62d6c355b3c8aca6c07a35c4e440d9b72acd512f
Peter Van der Beken <peterv@propagandism.org>
2020-10-11 18:18:28 +0000

    Bug 1668940 - Replace nsFrameLoader::mIsTopLevelContent with mPendingBrowsingContext->IsTopContent(). r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D93157

--- dom/base/nsFrameLoader.h
+++ dom/base/nsFrameLoader.h
@@ -530,7 +530,6 @@ class nsFrameLoader final : public nsStubMutationObserver,
   nsCString mRemoteType;
 
   bool mDepthTooGreat : 1;
-  bool mIsTopLevelContent : 1;
   bool mDestroyCalled : 1;
   bool mNeedsAsyncDestroy : 1;
   bool mInSwap : 1;
