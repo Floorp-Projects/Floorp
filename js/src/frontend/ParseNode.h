398803ade526c96ad30398f422bf347a84ddfaca
Tooru Fujisawa <arai_a@mac.com>
2020-10-11 23:56:56 +0000

    Bug 1670109 - Part 2: Use ParserAtomsTable in Int32ToParserAtom and NumberToParserAtom. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D93032

--- js/src/frontend/ParseNode.h
+++ js/src/frontend/ParseNode.h
@@ -58,6 +58,7 @@ class RegExpObject;
 namespace frontend {
 
 class ParseContext;
+class ParserAtomsTable;
 struct CompilationStencil;
 class ParserSharedBase;
 class FullParseHandler;
@@ -1572,8 +1573,7 @@ class NumericLiteral : public ParseNode {
   void setDecimalPoint(DecimalPoint d) { decimalPoint_ = d; }
 
   // Return the decimal string representation of this numeric literal.
-  const ParserAtom* toAtom(JSContext* cx,
-                           CompilationInfo& compilationInfo) const;
+  const ParserAtom* toAtom(JSContext* cx, ParserAtomsTable& parserAtoms) const;
 };
 
 class BigIntLiteral : public ParseNode {
