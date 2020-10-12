a57bc44617b8673afda917920b8b84fa8751505e
Tooru Fujisawa <arai_a@mac.com>
2020-10-12 12:27:20 +0000

    Bug 1665746 - Do not use self-hosted JS parameter name in decompiler. r=evilpie
    Differential Revision: https://phabricator.services.mozilla.com/D90718

---
+++ js/src/tests/non262/Set/non-iterable-error.js
@@ -0,0 +1,10 @@
+// Self-hosted JS code shouldn't be used for error message.
+
+try {
+  new Set(...[1]);
+} catch (e) {
+  assertEq(e.message, "1 is not iterable");
+}
+
+if (typeof reportCompare === "function")
+  reportCompare(0, 0);
