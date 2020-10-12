faf7b39a92e269608e7563819c6a8d092d86b2e2
Byron Campen [:bwc] <docfaraday@gmail.com>
2020-10-12 20:09:28 +0000

    Bug 1626278: Remove transports from main, just like we do under normal circumstances. r=mjf
    Just in case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93262

--- dom/media/webrtc/jsapi/PeerConnectionMedia.cpp
+++ dom/media/webrtc/jsapi/PeerConnectionMedia.cpp
@@ -628,6 +628,9 @@ void PeerConnectionMedia::SelfDestruct_m() {
 
   ASSERT_ON_THREAD(mMainThread);
 
+  mTransportHandler->RemoveTransportsExcept(std::set<std::string>());
+  mTransportHandler = nullptr;
+
   mMainThread = nullptr;
 
   // Final self-destruct.
@@ -640,9 +643,6 @@ void PeerConnectionMedia::ShutdownMediaTransport_s() {
   CSFLogDebug(LOGTAG, "%s: ", __FUNCTION__);
 
   disconnect_all();
-
-  mTransportHandler->RemoveTransportsExcept(std::set<std::string>());
-  mTransportHandler = nullptr;
 
   // we're holding a ref to 'this' that's released by SelfDestruct_m
   mMainThread->Dispatch(
