8a38c5cef32221e33df75d2abadba4d0bc0c0b7f
Razvan Maries <rmaries@mozilla.com>
2020-10-12 18:07:38 +0300

    Backed out changeset ed5f52188003 (bug 1670317) as per Emilio's request. CLOSED TREE

--- browser/components/places/content/places-menupopup.js
+++ browser/components/places/content/places-menupopup.js
@@ -701,9 +701,6 @@
 
     on_popupshowing(event) {
       if (event.target == this) {
-        // getBoundingClientRect here is a wallpaper, see bug 1670317 and
-        // bug 1666497.
-        this.getBoundingClientRect();
         this.setAttribute("animate", "open");
       }
     }
