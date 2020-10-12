636322d251176fffff69da324dae6fd89155017a
Ryan Hunt <rhunt@eqrion.net>
2020-10-12 20:49:50 +0000

    Bug 1669784 - Replace self-hosted TypedObject read field/element code with native implementation. r=jandem
    The code for reading fields/elements is the only remaining self-hosted code. Replace it with
    a much smaller native implementation.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92855

--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "SelfHostingDefines.h"
-#include "TypedObjectConstants.h"
 
 // Assertions and debug printing, defined here instead of in the header above
 // to make `assert` invisible to C++.
