7d26af97a1c72262b44e32d0733564378632b264
Ryan Hunt <rhunt@eqrion.net>
2020-10-12 20:13:16 +0000

    Bug 1669784 - Don't expose TypedObject definitions on WebAssembly namespace. r=jandem
    There's no point in exposing the TypedObject definitions in the WebAssembly namespace
    until we allow the types to be constructed by JS again.
    
    This commit also removes support for TypedObject types that aren't needed by
    WebAssembly, such as 'string', 'uint32', 'uint64'.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92858

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -5535,10 +5535,6 @@ bool CacheIRCompiler::emitLoadTypedObjectResult(ObjOperandId objId,
   } else {
     ReferenceType type = ReferenceTypeFromSimpleTypeDescrKey(typeDescr);
     switch (type) {
-      case ReferenceType::TYPE_ANY:
-        masm.loadValue(fieldAddr, output.valueReg());
-        break;
-
       case ReferenceType::TYPE_WASM_ANYREF:
         // TODO/AnyRef-boxing: With boxed immediates and strings this may be
         // more complicated.
@@ -5553,11 +5549,6 @@ bool CacheIRCompiler::emitLoadTypedObjectResult(ObjOperandId objId,
         masm.bind(&done);
         break;
       }
-
-      case ReferenceType::TYPE_STRING:
-        masm.loadPtr(fieldAddr, scratch2);
-        masm.tagValue(JSVAL_TYPE_STRING, scratch2, output.valueReg());
-        break;
 
       default:
         MOZ_CRASH("Invalid ReferenceTypeDescr");
@@ -6641,11 +6632,6 @@ void CacheIRCompiler::emitStoreTypedObjectReferenceProp(ValueOperand val,
   // Callers will post-barrier this store.
 
   switch (type) {
-    case ReferenceType::TYPE_ANY:
-      EmitPreBarrier(masm, dest, MIRType::Value);
-      masm.storeValue(val, dest);
-      break;
-
     case ReferenceType::TYPE_WASM_ANYREF:
       // TODO/AnyRef-boxing: With boxed immediates and strings this may be
       // more complicated.
@@ -6660,12 +6646,6 @@ void CacheIRCompiler::emitStoreTypedObjectReferenceProp(ValueOperand val,
       masm.bind(&done);
       break;
     }
-
-    case ReferenceType::TYPE_STRING:
-      EmitPreBarrier(masm, dest, MIRType::String);
-      masm.unboxString(val, scratch);
-      masm.storePtr(scratch, dest);
-      break;
   }
 }
 
