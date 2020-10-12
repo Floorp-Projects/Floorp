680c6e29e5b3ea72c0d93ab1f2d2a05c0ae45f50
Mike Hommey <mh+mozilla@glandium.org>
2020-10-12 21:29:34 +0000

    Bug 1670039 - Filter-out site-packages and dist-packages from sys.path when running virtualenv. r=firefox-build-system-reviewers,andi,rstewart
    Differential Revision: https://phabricator.services.mozilla.com/D93149

--- third_party/python/virtualenv/virtualenv.py
+++ third_party/python/virtualenv/virtualenv.py
@@ -2,8 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# This script is not to be imported.
+assert __name__ == '__main__'
 import os
 import sys
+# If some older version of virtualenv is installed, it may interfere with this one.
+# So filter-out site-packages and dist-packages directories where it might be installed.
+# This is kinda sorta like invoking python with -S, but invoking a virtualenv python 2.7
+# with -S is broken (sys.path becomes completely wrong, and even `import os` fails).
+# (And while yes, it is kind of silly to use a virtualenv python to run virtualenv, we
+# do)
+sys.path = [p for p in sys.path if os.path.basename(p) not in ('site-packages', 'dist-packages')]
 try:
     import importlib.util
     spec = importlib.util.spec_from_file_location('main', os.path.join(os.path.dirname(__file__), '__main__.py'))
