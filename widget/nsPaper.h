cdf2bf9fb5054281fa94229b6fdd23e0e5bdf9bc
Emily McDonough <emcdonough@mozilla.com>
2020-10-13 21:53:47 +0000

    Bug 1667486 part 1 - Make fallback paper sizes public to nsPaper, localize the sizes only once in nsPrinterListBase r=jwatt,emilio
    We will use the fallback, PWG paper sizes for localization of paper sizes in
    the CUPS backend when possible. This will make paper names more consistent, and
    may allow us to avoid extra connections in CUPS.
    
    This also makes the paper size data constexpr data. The localized paper data is
    held in a RefPtr as it will be shared with the printers created by the printers
    when we start using localized PWG names where possible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D91496

--- widget/nsPaper.h
+++ widget/nsPaper.h
@@ -34,13 +34,28 @@ struct PaperInfo {
         mSize(aSize),
         mUnwriteableMargin(aUnwriteableMargin) {}
 
-  const nsString mId;
-  const nsString mName;
+  nsString mId;
+  nsString mName;
 
   SizeDouble mSize;
 
   // The margins may not be known by some back-ends.
-  const Maybe<MarginDouble> mUnwriteableMargin{Nothing()};
+  Maybe<MarginDouble> mUnwriteableMargin{Nothing()};
+};
+
+/**
+ * Plain struct used for commonly used, hard-coded paper sizes.
+ *
+ * Used to construct PaperInfo at runtime by localizing the name.
+ */
+struct CommonPaperSize final {
+  // The standardized PWG name, which should be used as the PaperInfo id
+  nsLiteralString mPWGName;
+  // The name key to localize the name of this paper size using strings in
+  // printUI.ftl
+  nsLiteralCString mLocalizableNameKey;
+  // Size is in points, same as PaperInfo
+  gfx::SizeDouble mSize;
 };
 
 }  // namespace mozilla
@@ -49,6 +64,7 @@ class nsPrinterBase;
 
 class nsPaper final : public nsIPaper {
   using Promise = mozilla::dom::Promise;
+  using CommonPaperSize = mozilla::CommonPaperSize;
 
  public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
@@ -59,6 +75,31 @@ class nsPaper final : public nsIPaper {
   explicit nsPaper(const mozilla::PaperInfo&);
   nsPaper(nsPrinterBase&, const mozilla::PaperInfo&);
 
+  // This list is used for both our fallback paper sizes (used when a printer
+  // does not provide a list of available paper sizes), and for localizing
+  // common paper sizes to avoid querying the printer for extra information in
+  // the common case, as well as to provide more uniform paper names in the
+  // frontend.
+  // If we need to separate these two uses, we will need to either split this
+  // into two lists, or add a flag to indicate what the size is used for.
+#define mm *72.0 / 25.4
+#define in *72.0
+  static constexpr CommonPaperSize kCommonPaperSizes[] = {
+      CommonPaperSize{u"iso_a5"_ns, "a5"_ns, {148 mm, 210 mm}},
+      CommonPaperSize{u"iso_a4"_ns, "a4"_ns, {210 mm, 297 mm}},
+      CommonPaperSize{u"iso_a3"_ns, "a3"_ns, {297 mm, 420 mm}},
+      CommonPaperSize{u"iso_b5"_ns, "b5"_ns, {176 mm, 250 mm}},
+      CommonPaperSize{u"iso_b4"_ns, "b4"_ns, {250 mm, 353 mm}},
+      CommonPaperSize{u"jis_b5"_ns, "jis-b5"_ns, {182 mm, 257 mm}},
+      CommonPaperSize{u"jis_b4"_ns, "jis-b4"_ns, {257 mm, 364 mm}},
+      CommonPaperSize{u"na_letter"_ns, "letter"_ns, {8.5 in, 11 in}},
+      CommonPaperSize{u"na_legal"_ns, "legal"_ns, {8.5 in, 14 in}},
+      CommonPaperSize{u"na_ledger"_ns, "tabloid"_ns, {11 in, 17 in}}};
+#undef mm
+#undef in
+  static constexpr size_t kNumCommonPaperSizes =
+      mozilla::ArrayLength(kCommonPaperSizes);
+
  private:
   ~nsPaper();
 
@@ -68,5 +109,20 @@ class nsPaper final : public nsIPaper {
   RefPtr<Promise> mMarginPromise;
   const mozilla::PaperInfo mInfo;
 };
+
+namespace mozilla {
+
+// Used to allow fixed-sized arrays of PWG paper info to be ref-counted.
+class CommonPaperInfoArray
+    : public Array<PaperInfo, nsPaper::kNumCommonPaperSizes> {
+ public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CommonPaperInfoArray);
+  CommonPaperInfoArray() = default;
+
+ private:
+  ~CommonPaperInfoArray() = default;
+};
+
+}  // namespace mozilla
 
 #endif /* nsPaper_h__ */
