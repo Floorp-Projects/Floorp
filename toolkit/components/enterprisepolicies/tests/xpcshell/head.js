7d4f13d503748b58732ab68f9714992789e25d56
Michael Kaply <mozilla@kaply.com>
2020-10-13 18:42:28 +0000

    Bug 1583889 - Use EnterprisePolicyTesting for xpcshell tests r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D92841

--- toolkit/components/enterprisepolicies/tests/xpcshell/head.js
+++ toolkit/components/enterprisepolicies/tests/xpcshell/head.js
@@ -18,6 +18,9 @@ const { FileTestUtils } = ChromeUtils.import(
 const { PermissionTestUtils } = ChromeUtils.import(
   "resource://testing-common/PermissionTestUtils.jsm"
 );
+const { EnterprisePolicyTesting } = ChromeUtils.import(
+  "resource://testing-common/EnterprisePolicyTesting.jsm"
+);
 
 updateAppInfo({
   name: "XPCShell",
@@ -32,45 +35,13 @@ let policies = Cc["@mozilla.org/enterprisepolicies;1"].getService(
 );
 policies.observe(null, "policies-startup", null);
 
-// Any changes to this function should also be made to the corresponding version
-// in toolkit/components/enterprisepolicies/tests/browser/head.js
 async function setupPolicyEngineWithJson(json, customSchema) {
-  let filePath;
-  if (typeof json == "object") {
-    filePath = FileTestUtils.getTempFile("policies.json").path;
-
-    // This file gets automatically deleted by FileTestUtils
-    // at the end of the test run.
-    await OS.File.writeAtomic(filePath, JSON.stringify(json), {
-      encoding: "utf-8",
-    });
-  } else {
-    filePath = do_get_file(json ? json : "non-existing-file.json").path;
-  }
-
-  Services.prefs.setStringPref("browser.policies.alternatePath", filePath);
-
-  let promise = new Promise(resolve => {
-    Services.obs.addObserver(function observer() {
-      Services.obs.removeObserver(
-        observer,
-        "EnterprisePolicies:AllPoliciesApplied"
-      );
-      resolve();
-    }, "EnterprisePolicies:AllPoliciesApplied");
-  });
-
-  // Clear any previously used custom schema
-  Cu.unload("resource:///modules/policies/schema.jsm");
-
-  if (customSchema) {
-    let schemaModule = ChromeUtils.import(
-      "resource:///modules/policies/schema.jsm",
-      null
+  if (typeof json != "object") {
+    let filePath = do_get_file(json ? json : "non-existing-file.json").path;
+    return EnterprisePolicyTesting.setupPolicyEngineWithJson(
+      filePath,
+      customSchema
     );
-    schemaModule.schema = customSchema;
   }
-
-  Services.obs.notifyObservers(null, "EnterprisePolicies:Restart");
-  return promise;
+  return EnterprisePolicyTesting.setupPolicyEngineWithJson(json, customSchema);
 }
