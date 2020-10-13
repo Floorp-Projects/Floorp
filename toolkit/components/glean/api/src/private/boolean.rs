be342788c7bbe25aecef017c1bd045aadb0d3b21
Chris H-C <chutten@mozilla.com>
2020-10-13 07:47:49 +0000

    Bug 1670309 - Implement dispatcher and IPC for bool metric type r=janerik
    Had a slight issue with a deadlock in the timespan tests, so needed to
    add a `setup_glean` in there to deal with it. Not sure why.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93130

--- toolkit/components/glean/api/src/private/boolean.rs
+++ toolkit/components/glean/api/src/private/boolean.rs
@@ -2,18 +2,34 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::sync::Arc;
+
 use super::CommonMetricData;
+
+use crate::dispatcher;
+use crate::ipc::need_ipc;
 
 /// A boolean metric.
 ///
 /// Records a simple true or false value.
+#[derive(Debug)]
+pub enum BooleanMetric {
+    Parent(Arc<BooleanMetricImpl>),
+    Child(BooleanMetricIpc),
+}
 #[derive(Clone, Debug)]
-pub struct BooleanMetric(pub(crate) glean_core::metrics::BooleanMetric);
+pub struct BooleanMetricImpl(pub(crate) glean_core::metrics::BooleanMetric);
+#[derive(Debug)]
+pub struct BooleanMetricIpc();
 
 impl BooleanMetric {
     /// Create a new boolean metric.
     pub fn new(meta: CommonMetricData) -> Self {
-        Self(glean_core::metrics::BooleanMetric::new(meta))
+        if need_ipc() {
+            BooleanMetric::Child(BooleanMetricIpc {})
+        } else {
+            BooleanMetric::Parent(Arc::new(BooleanMetricImpl::new(meta)))
+        }
     }
 
     /// Set to the specified boolean value.
@@ -22,7 +38,19 @@ impl BooleanMetric {
     ///
     /// * `value` - the value to set.
     pub fn set(&self, value: bool) {
-        crate::with_glean(move |glean| self.0.set(glean, value))
+        match self {
+            BooleanMetric::Parent(p) => {
+                let metric = Arc::clone(&p);
+                dispatcher::launch(move || metric.set(value));
+            }
+            BooleanMetric::Child(_) => {
+                log::error!(
+                    "Unable to set boolean metric {:?} in non-parent process. Ignoring.",
+                    self
+                );
+                // TODO: Record an error.
+            }
+        }
     }
 
     /// **Test-only API.**
@@ -37,6 +65,29 @@ impl BooleanMetric {
     /// ## Return value
     ///
     /// Returns the stored value or `None` if nothing stored.
+    pub fn test_get_value(&self, storage_name: &str) -> Option<bool> {
+        match self {
+            BooleanMetric::Parent(p) => {
+                dispatcher::block_on_queue();
+                p.test_get_value(storage_name)
+            }
+            BooleanMetric::Child(_) => panic!(
+                "Cannot get test value for {:?} in non-parent process!",
+                self
+            ),
+        }
+    }
+}
+
+impl BooleanMetricImpl {
+    pub fn new(meta: CommonMetricData) -> Self {
+        Self(glean_core::metrics::BooleanMetric::new(meta))
+    }
+
+    pub fn set(&self, value: bool) {
+        crate::with_glean(move |glean| self.0.set(glean, value))
+    }
+
     pub fn test_get_value(&self, storage_name: &str) -> Option<bool> {
         crate::with_glean(move |glean| self.0.test_get_value(glean, storage_name))
     }
