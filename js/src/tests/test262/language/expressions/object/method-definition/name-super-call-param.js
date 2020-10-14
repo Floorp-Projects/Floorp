8af0faad85755e5c7194172418230c1375fc9b3b
Jeff Walden <jwalden@mit.edu>
2020-10-14 00:40:04 +0000

    Bug 1670504 - Update test262 to approximately September 10 (just before our update script starts having issues checking out new tests).  r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D93170

--- js/src/tests/test262/language/expressions/object/method-definition/name-super-call-param.js
+++ js/src/tests/test262/language/expressions/object/method-definition/name-super-call-param.js
@@ -5,7 +5,7 @@
 /*---
 description: >
     It is a Syntax Error if HasDirectSuper of MethodDefinition is true.
-es6id: 12.2.5.1
+esid: sec-object-initializer-static-semantics-early-errors
 negative:
   phase: parse
   type: SyntaxError
@@ -14,5 +14,5 @@ negative:
 $DONOTEVALUATE();
 
 ({
-  method(param = super) {}
+  method(param = super()) {}
 });
