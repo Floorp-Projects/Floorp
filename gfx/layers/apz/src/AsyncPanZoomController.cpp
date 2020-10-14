f2dea2616ed0645907bb7c338c479506c418ec6d
Kartikaya Gupta <kgupta@mozilla.com>
2020-10-14 19:23:57 +0000

    Bug 1671048 - Add serializer for RepaintRequest. r=botond
    This prints out selected useful fields from the RepaintRequest when the APZ
    code generates a repaint request.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93437

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -4031,7 +4031,8 @@ void AsyncPanZoomController::RequestContentRepaint(
     return;
   }
 
-  APZC_LOGV_FM(aFrameMetrics, "%p requesting content repaint", this);
+  APZC_LOGV("%p requesting content repaint %s", this,
+            ToString(request).c_str());
   {  // scope lock
     MutexAutoLock lock(mCheckerboardEventLock);
     if (mCheckerboardEvent && mCheckerboardEvent->IsRecordingTrace()) {
