3321573536995f6165a26cd1506720ef7f5d3fa5
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-14 11:32:58 +0000

    Bug 1669956 - [devtools] Move Tree component to devtools/client/shared and remove devtools-components. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D93372

--- devtools/client/shared/components/test/node/jest.config.js
+++ devtools/client/shared/components/test/node/jest.config.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
+/* global __dirname */
+
+"use strict";
+
 const { resolve } = require("path");
 const rootDir = resolve(__dirname, "src");
 module.exports = {
