3e98bef963e73e34d55a74100ce913ae62e221db
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-14 11:20:32 +0000

    Bug 1669956 - [devtools] Remove flow annotations from reps and object inspector. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D93348

--- devtools/client/shared/components/reps/index.js
+++ devtools/client/shared/components/reps/index.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-// @flow
-
 const { MODE } = require("./reps/constants");
 const { REPS, getRep } = require("./reps/rep");
 const objectInspector = require("./object-inspector");
@@ -25,10 +23,3 @@ module.exports = {
   getGripPreviewItems,
   objectInspector,
 };
-
-export type {
-  RdpGrip,
-  GripProperties,
-  Node,
-  Grip,
-} from "./object-inspector/types";
