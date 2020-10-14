c7f0599c764c7a141d2bdc4bb63beab78ba4459d
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-14 11:24:21 +0000

    Bug 1669956 - [devtools] Fix require paths in reps folder. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D93358

--- devtools/client/shared/components/reps/object-inspector/index.js
+++ devtools/client/shared/components/reps/object-inspector/index.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 const ObjectInspector = require("./components/ObjectInspector");
-const utils = require("./utils");
+const utils = require("./utils/index");
 const reducer = require("./reducer");
 const actions = require("./actions");
 
