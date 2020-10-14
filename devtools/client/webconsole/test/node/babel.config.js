80d2dcd261bd373cf2eadbad9e164226a5c0bfdc
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-14 13:19:38 +0000

    Bug 1669956 - [devtools] Fix webconsole mocha tests. r=jdescottes.
    Update babel config to transpile reps files.
    
    Depends on D93480
    
    Differential Revision: https://phabricator.services.mozilla.com/D93481

--- devtools/client/webconsole/test/node/babel.config.js
+++ devtools/client/webconsole/test/node/babel.config.js
@@ -6,7 +6,9 @@
 
 module.exports = {
   plugins: [
+    "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-nullish-coalescing-operator",
+    "transform-amd-to-commonjs",
   ],
 };
