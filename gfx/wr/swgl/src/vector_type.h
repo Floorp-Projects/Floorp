585f27bf83a9e07fa12fb135115b0966d8196086
Lee Salzman <lsalzman@mozilla.com>
2020-10-14 10:50:03 +0000

    Bug 1671055 - support partial stores at end of row in SWGL composites. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D93443

--- gfx/wr/swgl/src/vector_type.h
+++ gfx/wr/swgl/src/vector_type.h
@@ -39,6 +39,16 @@ SI VectorType<T, 16> combine(VectorType<T, 8> a, VectorType<T, 8> b) {
 }
 
 template <typename T>
+SI VectorType<T, 2> lowHalf(VectorType<T, 4> a) {
+  return __builtin_shufflevector(a, a, 0, 1);
+}
+
+template <typename T>
+SI VectorType<T, 2> highHalf(VectorType<T, 4> a) {
+  return __builtin_shufflevector(a, a, 2, 3);
+}
+
+template <typename T>
 SI VectorType<T, 4> lowHalf(VectorType<T, 8> a) {
   return __builtin_shufflevector(a, a, 0, 1, 2, 3);
 }
