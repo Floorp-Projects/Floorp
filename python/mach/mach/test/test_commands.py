9ae637a1fabc88ce00f051865d9be7fb258c5691
Andrew Halberstadt <ahal@pm.me>
2020-10-14 03:32:04 +0000

    Bug 1670385 - [mach] Move 'TestBase._run_mach' to a pytest fixture, r=firefox-build-system-reviewers,rstewart
    I accidentally broke the 'mach' unittests on Python 2 due to some difference in the unittest
    module. Rather than poking into 'unittest', this patch moves us closer to the pytest format
    while also fixing the issue.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93420

--- python/mach/mach/test/test_commands.py
+++ python/mach/mach/test/test_commands.py
@@ -12,7 +12,6 @@ from mozunit import main
 
 from buildconfig import topsrcdir
 import mach
-from mach.test.common import TestBase
 
 ALL_COMMANDS = [
     'cmd_bar',
@@ -25,11 +24,9 @@ ALL_COMMANDS = [
 
 
 @pytest.fixture
-def run_mach():
+def run_completion(run_mach):
 
-    tester = TestBase()
-
-    def inner(args):
+    def inner(args=[]):
         mach_dir = os.path.dirname(mach.__file__)
         providers = [
             'commands.py',
@@ -40,7 +37,8 @@ def run_mach():
             if key == 'topdir':
                 return topsrcdir
 
-        return tester._run_mach(args, providers, context_handler=context_handler)
+        args = ["mach-completion"] + args
+        return run_mach(args, providers, context_handler=context_handler)
 
     return inner
 
@@ -49,26 +47,26 @@ def format(targets):
     return "\n".join(targets) + "\n"
 
 
-def test_mach_completion(run_mach):
-    result, stdout, stderr = run_mach(['mach-completion'])
+def test_mach_completion(run_completion):
+    result, stdout, stderr = run_completion()
     assert result == 0
     assert stdout == format(ALL_COMMANDS)
 
-    result, stdout, stderr = run_mach(['mach-completion', 'cmd_f'])
+    result, stdout, stderr = run_completion(['cmd_f'])
     assert result == 0
     # While it seems like this should return only commands that have
     # 'cmd_f' as a prefix, the completion script will handle this case
     # properly.
     assert stdout == format(ALL_COMMANDS)
 
-    result, stdout, stderr = run_mach(['mach-completion', 'cmd_foo'])
+    result, stdout, stderr = run_completion(['cmd_foo'])
     assert result == 0
     assert stdout == format(['help', '--arg'])
 
 
 @pytest.mark.parametrize("shell", ("bash", "fish", "zsh"))
-def test_generate_mach_completion_script(run_mach, shell):
-    rv, out, err = run_mach(["mach-completion", shell])
+def test_generate_mach_completion_script(run_completion, shell):
+    rv, out, err = run_completion([shell])
     print(out)
     print(err, file=sys.stderr)
     assert rv == 0
