bdc830df8bc3a0c95909b005da7d43f747baaa1b
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-14 11:24:14 +0000

    Bug 1669956 - [devtools] Add moz.build in devtools/client/shared/components/reps. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D93352

--- devtools/client/shared/components/reps/reps/rep.js
+++ devtools/client/shared/components/reps/reps/rep.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-require("./reps.css");
-
 // Load all existing rep templates
 const Undefined = require("./undefined");
 const Null = require("./null");
