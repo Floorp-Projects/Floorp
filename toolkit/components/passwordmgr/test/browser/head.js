035a98e8a24ec7acb754b83e073891624109f949
Bianca Danforth <bdanforth@mozilla.com>
2020-10-14 03:04:39 +0000

    Bug 786276 - Don't autofill logins in frames that are not same-origin with top-level page r=sfoster
    Differential Revision: https://phabricator.services.mozilla.com/D90488

--- toolkit/components/passwordmgr/test/browser/head.js
+++ toolkit/components/passwordmgr/test/browser/head.js
@@ -607,15 +607,21 @@ async function openPasswordManager(openingFunc, waitForFilter) {
 
 // Autocomplete popup related functions //
 
-async function openACPopup(popup, browser, inputSelector) {
+async function openACPopup(
+  popup,
+  browser,
+  inputSelector,
+  iframeBrowsingContext = null
+) {
   let promiseShown = BrowserTestUtils.waitForEvent(popup, "popupshown");
 
   await SimpleTest.promiseFocus(browser);
   info("content window focused");
 
   // Focus the username field to open the popup.
+  let target = iframeBrowsingContext || browser;
   await SpecialPowers.spawn(
-    browser,
+    target,
     [[inputSelector]],
     function openAutocomplete(sel) {
       content.document.querySelector(sel).focus();
