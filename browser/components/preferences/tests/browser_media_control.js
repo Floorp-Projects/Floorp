8ea5bd039b97db3e48bfec05d097ccac42d994ec
alwu <alwu@mozilla.com>
2020-10-15 05:11:40 +0000

    Bug 1667454 - part2 : add telemetry probe to record the number of times a user changes media control setting. r=preferences-reviewers,mossop,Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D92872

--- browser/components/preferences/tests/browser_media_control.js
+++ browser/components/preferences/tests/browser_media_control.js
@@ -14,10 +14,11 @@ add_task(async function testMediaControlCheckBox() {
   );
   ok(checkBox, "check box exists");
 
-  await modifyPrefAndWaitUntilCheckBoxChanges(true);
+  // The pref is true by default.
   await modifyPrefAndWaitUntilCheckBoxChanges(false);
-  await toggleCheckBoxAndWaitUntilPrefValueChanges(true);
+  await modifyPrefAndWaitUntilCheckBoxChanges(true);
   await toggleCheckBoxAndWaitUntilPrefValueChanges(false);
+  await toggleCheckBoxAndWaitUntilPrefValueChanges(true);
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
 });
 
@@ -34,6 +35,7 @@ async function modifyPrefAndWaitUntilCheckBoxChanges(isEnabled) {
     "Waiting for the checkbox gets checked"
   );
   is(checkBox.checked, isEnabled, `check box status is correct`);
+  checkAndClearTelemetryProbe(isEnabled);
 }
 
 async function toggleCheckBoxAndWaitUntilPrefValueChanges(isChecked) {
@@ -47,4 +49,35 @@ async function toggleCheckBoxAndWaitUntilPrefValueChanges(isChecked) {
     isChecked,
     "the pref's value is correct"
   );
+  checkAndClearTelemetryProbe(isChecked, true /* check UI */);
+}
+
+/**
+ * These telemetry related variable and method should be removed after the
+ * telemetry probe `MEDIA_CONTROL_SETTING_CHANGE` gets expired.
+ */
+const HISTOGRAM_ID = "MEDIA_CONTROL_SETTING_CHANGE";
+const HISTOGRAM_KEYS = {
+  EnableFromUI: 0,
+  EnableTotal: 1,
+  DisableFromUI: 2,
+  DisableTotal: 3,
+};
+
+function checkAndClearTelemetryProbe(isEnable, checkUI = false) {
+  const histogram = Services.telemetry.getHistogramById(HISTOGRAM_ID);
+  let keyTotal = isEnable ? "EnableTotal" : "DisableTotal";
+  let keyUI = null;
+  if (checkUI) {
+    keyUI = isEnable ? "EnableFromUI" : "DisableFromUI";
+  }
+  for (let [key, val] of Object.entries(histogram.snapshot().values)) {
+    if (key == HISTOGRAM_KEYS[keyTotal]) {
+      ok(val, "Increase the amount for the probe 'changeing total setting'");
+    }
+    if (keyUI && key == HISTOGRAM_KEYS[keyUI]) {
+      ok(val, "Increase the amount for the probe 'changeing setting from UI'");
+    }
+  }
+  histogram.clear();
 }
