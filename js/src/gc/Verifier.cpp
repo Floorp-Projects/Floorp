cef7f2e978cefe2d4d889c14ad050bb4a62bdad3
Jon Coppeard <jcoppeard@mozilla.com>
2020-10-16 14:32:10 +0000

    Bug 1671125 - Relax assertions to allow pre-barrier verifier to turn on barriers on the self hosting zone as well as the atoms zone r=sfink
    The comments in PreWriteBarrierImpl mention that the per-barrier verifer can
    turn on incremental barriers in the atoms zone outside of a GC. This also
    applies to the self hosting zone so we just need to relax the assertions to
    cover that too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93760

--- js/src/gc/Verifier.cpp
+++ js/src/gc/Verifier.cpp
@@ -307,8 +307,15 @@ void CheckEdgeTracer::onChild(const JS::GCCellPtr& thing) {
 }
 
 void js::gc::AssertSafeToSkipPreWriteBarrier(TenuredCell* thing) {
-  mozilla::DebugOnly<Zone*> zone = thing->zoneFromAnyThread();
-  MOZ_ASSERT(!zone->needsIncrementalBarrier() || zone->isAtomsZone());
+#ifdef DEBUG
+  Zone* zone = thing->zoneFromAnyThread();
+  if (!zone->needsIncrementalBarrier()) {
+    // Barriers are disabled and would be skipped anyway.
+    return;
+  }
+
+  MOZ_ASSERT(zone->isAtomsZone() || zone->isSelfHostingZone());
+#endif
 }
 
 static bool IsMarkedOrAllocated(const EdgeValue& edge) {
