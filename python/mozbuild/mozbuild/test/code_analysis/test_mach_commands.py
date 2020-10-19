33afa43ab74267292812200cf553b8effc9c25a4
Tarek Ziadé <tarek@mozilla.com>
2020-10-19 12:33:50 +0000

    Bug 1615884 - Handle Windows separators correctly when calling clang-format -d on Windows, while allowing writing path with '/' on the command-line. r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D93339

---
+++ python/mozbuild/mozbuild/test/code_analysis/test_mach_commands.py
@@ -0,0 +1,62 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import absolute_import, unicode_literals, print_function
+import os
+import unittest
+import mock
+
+from mozunit import main
+from mach.registrar import Registrar
+from mozbuild.base import MozbuildObject
+
+
+class TestStaticAnalysis(unittest.TestCase):
+    def setUp(self):
+        self.remove_cats = []
+        for cat in ("build", "post-build", "misc", "testing"):
+            if cat in Registrar.categories:
+                continue
+            Registrar.register_category(cat, cat, cat)
+            self.remove_cats.append(cat)
+
+    def tearDown(self):
+        for cat in self.remove_cats:
+            del Registrar.categories[cat]
+            del Registrar.commands_by_category[cat]
+
+    def test_bug_1615884(self):
+        # TODO: cleaner test
+        # we're testing the `_is_ignored_path` but in an ideal
+        # world we should test the clang_analysis mach command
+        # since that small function is an internal detail.
+        # But there is zero test infra for that mach command
+        from mozbuild.code_analysis.mach_commands import StaticAnalysis
+
+        config = MozbuildObject.from_environment()
+        context = mock.MagicMock()
+        context.cwd = config.topsrcdir
+
+        cmd = StaticAnalysis(context)
+        cmd.topsrcdir = "/root/dir"
+        path = "/root/dir/path1"
+
+        ignored_dirs_re = r"path1|path2/here|path3\there"
+        self.assertTrue(cmd._is_ignored_path(ignored_dirs_re, path) is not None)
+
+        # simulating a win32 env
+        win32_path = "\\root\\dir\\path1"
+        cmd.topsrcdir = "\\root\\dir"
+        old_sep = os.sep
+        os.sep = "\\"
+        try:
+            self.assertTrue(cmd._is_ignored_path(ignored_dirs_re, win32_path) is
+                            not None)
+        finally:
+            os.sep = old_sep
+
+        self.assertTrue(cmd._is_ignored_path(ignored_dirs_re, "path2") is None)
+
+
+if __name__ == '__main__':
+    main()
