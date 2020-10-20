96b78da32a00a8fd16085ee926bc2bf521bb6832
Paul Adenot <paul@paul.cx>
2020-10-20 13:08:22 +0000

    Bug 1670917 - Upstream build system and other bits from freebsd-ports. r=cubeb-reviewers,chunmin
    This is the first two chunks in https://github.com/freebsd/freebsd-ports/blob/ed3dd5591642608f136a452c9e1d1b765721d498/www/firefox/files/patch-cubeb-oss, from From: jbeich <jbeich@FreeBSD.org>. The rest of this patch file is now upstream.
    
    This allows OSS users to drop a patch downstream.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93362

--- dom/media/CubebUtils.cpp
+++ dom/media/CubebUtils.cpp
@@ -125,8 +125,9 @@ int sInCommunicationCount = 0;
 const char kBrandBundleURL[] = "chrome://branding/locale/brand.properties";
 
 const char* AUDIOSTREAM_BACKEND_ID_STR[] = {
-    "jack",  "pulse",       "alsa",  "audiounit", "audioqueue", "wasapi",
-    "winmm", "directsound", "sndio", "opensl",    "audiotrack", "kai"};
+    "jack",   "pulse",      "alsa",        "audiounit", "audioqueue",
+    "wasapi", "winmm",      "directsound", "sndio",     "opensl",
+    "oss",    "audiotrack", "kai"};
 /* Index for failures to create an audio stream the first time. */
 const int CUBEB_BACKEND_INIT_FAILURE_FIRST =
     ArrayLength(AUDIOSTREAM_BACKEND_ID_STR);
