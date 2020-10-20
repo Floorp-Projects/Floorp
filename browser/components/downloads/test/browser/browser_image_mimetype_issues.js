cddf557185884889f6c65347f2e8b87a0a86e48d
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-10-20 12:05:10 +0000

    Bug 1667787 - fix saving webp images served with jpeg extensions without content-disposition information, r=mak
    This changes two bits of Firefox that, together with the mime service, end up
    very confused over webp + jpeg.
    
    1) it changes contentAreaUtils.js' getDefaultExtension that if it gets an image
    mimetype as the content type, it should ignore the URL. It doesn't have full channel
    info so it can't really do better anyway. This fixes the context menu's "save image as..."
    case.
    2) it changes the external helper app service to do a few things slightly differently:
    a. If we're told not to get an extension out of a URL, really don't. Don't just get the
       filename and then get it from there anyway...
    b. If we've got a suggested filename, and a primary extension for the mimetype,
       and the extension on the file is not one of the known extensions for the mimetype,
       replace it with the primary extension.
    This fixes the link case.
    
    It also adds tests for both of these mechanisms as well as "save image as."
    
    Differential Revision: https://phabricator.services.mozilla.com/D92306

---
+++ browser/components/downloads/test/browser/browser_image_mimetype_issues.js
@@ -0,0 +1,113 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_ROOT = getRootDirectory(gTestPath).replace(
+  "chrome://mochitests/content",
+  "http://example.com"
+);
+
+var MockFilePicker = SpecialPowers.MockFilePicker;
+MockFilePicker.init(window);
+
+/*
+ * Popular websites implement image optimization as serving files with
+ * extension ".jpg" but content type "image/webp". If we save such images,
+ * we should actually save them with a .webp extension as that is what
+ * they are.
+ */
+
+/**
+ * Test the above with the "save image as" context menu.
+ */
+add_task(async function test_save_image_webp_with_jpeg_extension() {
+  await BrowserTestUtils.withNewTab(
+    `data:text/html,<img src="${TEST_ROOT}/not-really-a-jpeg.jpeg?convert=webp">`,
+    async browser => {
+      let menu = document.getElementById("contentAreaContextMenu");
+      let popupShown = BrowserTestUtils.waitForEvent(menu, "popupshown");
+      BrowserTestUtils.synthesizeMouse(
+        "img",
+        5,
+        5,
+        { type: "contextmenu", button: 2 },
+        browser
+      );
+      await popupShown;
+
+      await new Promise(resolve => {
+        MockFilePicker.showCallback = function(fp) {
+          ok(
+            fp.defaultString.endsWith("webp"),
+            `filepicker for image has "${fp.defaultString}", should end in webp`
+          );
+          setTimeout(resolve, 0);
+          return Ci.nsIFilePicker.returnCancel;
+        };
+        EventUtils.synthesizeMouseAtCenter(
+          menu.querySelector("#context-saveimage"),
+          {}
+        );
+      });
+    }
+  );
+});
+
+/**
+ * Test with the "save link as" context menu.
+ */
+add_task(async function test_save_link_webp_with_jpeg_extension() {
+  await BrowserTestUtils.withNewTab(
+    `data:text/html,<a href="${TEST_ROOT}/not-really-a-jpeg.jpeg?convert=webp">Nice image</a>`,
+    async browser => {
+      let menu = document.getElementById("contentAreaContextMenu");
+      let popupShown = BrowserTestUtils.waitForEvent(menu, "popupshown");
+      BrowserTestUtils.synthesizeMouse(
+        "a[href]",
+        5,
+        5,
+        { type: "contextmenu", button: 2 },
+        browser
+      );
+      await popupShown;
+
+      await new Promise(resolve => {
+        MockFilePicker.showCallback = function(fp) {
+          ok(
+            fp.defaultString.endsWith("webp"),
+            `filepicker for link has "${fp.defaultString}", should end in webp`
+          );
+          setTimeout(resolve, 0);
+          return Ci.nsIFilePicker.returnCancel;
+        };
+        EventUtils.synthesizeMouseAtCenter(
+          menu.querySelector("#context-savelink"),
+          {}
+        );
+      });
+    }
+  );
+});
+
+/**
+ * Test with the main "save page" command.
+ */
+add_task(async function test_save_page_on_image_document() {
+  await BrowserTestUtils.withNewTab(
+    `${TEST_ROOT}/not-really-a-jpeg.jpeg?convert=webp`,
+    async browser => {
+      await new Promise(resolve => {
+        MockFilePicker.showCallback = function(fp) {
+          ok(
+            fp.defaultString.endsWith("webp"),
+            `filepicker for "save page" has "${fp.defaultString}", should end in webp`
+          );
+          setTimeout(resolve, 0);
+          return Ci.nsIFilePicker.returnCancel;
+        };
+        document.getElementById("Browser:SavePage").doCommand();
+      });
+    }
+  );
+});
