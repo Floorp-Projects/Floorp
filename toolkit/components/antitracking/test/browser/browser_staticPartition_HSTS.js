b4d838ac9e024513fd957ca396aacdf9f202dc02
Tim Huang <tihuang@mozilla.com>
2020-10-20 20:55:10 +0000

    Bug 1670820 - Part 1: Add a test case to ensure the HSTS of subresources is partitioned. r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D93392

--- toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.js
+++ toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.js
@@ -4,10 +4,18 @@
 
 var unsecureEmptyURL =
   "http://example.org/browser/toolkit/components/antitracking/test/browser/empty.html";
+var secureEmptyURL =
+  "https://example.org/browser/toolkit/components/antitracking/test/browser/empty.html";
+var secureAnotherEmptyURL =
+  "https://example.com/browser/toolkit/components/antitracking/test/browser/empty.html";
 var secureURL =
   "https://example.com/browser/toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.sjs";
 var unsecureURL =
   "http://example.com/browser/toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.sjs";
+var secureImgURL =
+  "https://example.com/browser/toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.sjs?image";
+var unsecureImgURL =
+  "http://example.com/browser/toolkit/components/antitracking/test/browser/browser_staticPartition_HSTS.sjs?image";
 
 function cleanupHSTS(aPartitionEnabled, aUseSite) {
   // Ensure to remove example.com from the HSTS list.
@@ -15,22 +23,24 @@ function cleanupHSTS(aPartitionEnabled, aUseSite) {
     Ci.nsISiteSecurityService
   );
 
-  let originAttributes = {};
+  for (let origin of ["example.com", "example.org"]) {
+    let originAttributes = {};
 
-  if (aPartitionEnabled) {
-    if (aUseSite) {
-      originAttributes = { partitionKey: "(http,example.com)" };
-    } else {
-      originAttributes = { partitionKey: "example.com" };
+    if (aPartitionEnabled) {
+      if (aUseSite) {
+        originAttributes = { partitionKey: `(http,${origin})` };
+      } else {
+        originAttributes = { partitionKey: origin };
+      }
     }
-  }
 
-  sss.resetState(
-    Ci.nsISiteSecurityService.HEADER_HSTS,
-    NetUtil.newURI("http://example.com/"),
-    0,
-    originAttributes
-  );
+    sss.resetState(
+      Ci.nsISiteSecurityService.HEADER_HSTS,
+      NetUtil.newURI("http://example.com/"),
+      0,
+      originAttributes
+    );
+  }
 }
 
 function promiseTabLoadEvent(aTab, aURL, aFinalURL) {
@@ -39,14 +49,13 @@ function promiseTabLoadEvent(aTab, aURL, aFinalURL) {
   return BrowserTestUtils.browserLoaded(aTab.linkedBrowser, false, aFinalURL);
 }
 
-function waitFor(host) {
+function waitFor(host, type) {
   return new Promise(resolve => {
     const observer = channel => {
       if (
         channel instanceof Ci.nsIHttpChannel &&
         channel.URI.host === host &&
-        channel.loadInfo.internalContentPolicyType ===
-          Ci.nsIContentPolicy.TYPE_INTERNAL_IFRAME
+        channel.loadInfo.internalContentPolicyType === type
       ) {
         Services.obs.removeObserver(observer, "http-on-stop-request");
         resolve(channel.URI.spec);
@@ -78,7 +87,10 @@ add_task(async function() {
       // Let's load a first-party.
       await promiseTabLoadEvent(tab, unsecureEmptyURL, unsecureEmptyURL);
 
-      let finalURL = waitFor("example.com");
+      let finalURL = waitFor(
+        "example.com",
+        Ci.nsIContentPolicy.TYPE_INTERNAL_IFRAME
+      );
 
       await SpecialPowers.spawn(tab.linkedBrowser, [unsecureURL], async url => {
         let ifr = content.document.createElement("iframe");
@@ -90,6 +102,96 @@ add_task(async function() {
         is(await finalURL, unsecureURL, "HSTS doesn't work for 3rd parties");
       } else {
         is(await finalURL, secureURL, "HSTS works for 3rd parties");
+      }
+
+      gBrowser.removeCurrentTab();
+      cleanupHSTS(networkIsolation, partitionPerSite);
+    }
+  }
+});
+
+add_task(async function test_subresource() {
+  for (let networkIsolation of [true, false]) {
+    for (let partitionPerSite of [true, false]) {
+      await SpecialPowers.pushPrefEnv({
+        set: [
+          ["privacy.partition.network_state", networkIsolation],
+          ["privacy.dynamic_firstparty.use_site", partitionPerSite],
+        ],
+      });
+
+      let tab = (gBrowser.selectedTab = BrowserTestUtils.addTab(gBrowser));
+
+      // Load a secure page as first party.
+      await promiseTabLoadEvent(tab, secureEmptyURL, secureEmptyURL);
+
+      let loadPromise = waitFor(
+        "example.com",
+        Ci.nsIContentPolicy.TYPE_INTERNAL_IMAGE
+      );
+
+      // Load a secure subresource to activate HSTS.
+      await SpecialPowers.spawn(
+        tab.linkedBrowser,
+        [secureImgURL],
+        async url => {
+          let ifr = content.document.createElement("img");
+          content.document.body.appendChild(ifr);
+          ifr.src = url;
+        }
+      );
+
+      // Ensure the subresource is loaded.
+      await loadPromise;
+
+      // Reload the secure page as first party.
+      await promiseTabLoadEvent(tab, secureEmptyURL, secureEmptyURL);
+
+      let finalURL = waitFor(
+        "example.com",
+        Ci.nsIContentPolicy.TYPE_INTERNAL_IMAGE
+      );
+
+      // Load a unsecure subresource, this should be upgraded to https.
+      await SpecialPowers.spawn(
+        tab.linkedBrowser,
+        [unsecureImgURL],
+        async url => {
+          let ifr = content.document.createElement("img");
+          content.document.body.appendChild(ifr);
+          ifr.src = url;
+        }
+      );
+
+      is(await finalURL, secureImgURL, "HSTS works for 3rd parties");
+
+      // Load the secure page with a different origin as first party.
+      await promiseTabLoadEvent(
+        tab,
+        secureAnotherEmptyURL,
+        secureAnotherEmptyURL
+      );
+
+      finalURL = waitFor(
+        "example.com",
+        Ci.nsIContentPolicy.TYPE_INTERNAL_IMAGE
+      );
+
+      // Load a unsecure subresource
+      await SpecialPowers.spawn(
+        tab.linkedBrowser,
+        [unsecureImgURL],
+        async url => {
+          let ifr = content.document.createElement("img");
+          content.document.body.appendChild(ifr);
+          ifr.src = url;
+        }
+      );
+
+      if (networkIsolation) {
+        is(await finalURL, unsecureImgURL, "HSTS doesn't work for 3rd parties");
+      } else {
+        is(await finalURL, secureImgURL, "HSTS works for 3rd parties");
       }
 
       gBrowser.removeCurrentTab();
