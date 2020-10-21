de4bec63530fd725b8914504fbe4f5ad7ffc3f22
alwu <alwu@mozilla.com>
2020-10-21 16:48:17 +0000

    Bug 1672294 - change 'test_audioNotification.html' to browser test and move it to the dedicated folder. r=bryce
    Per [1], change this test to a browser test.
    
    [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1672292#c0
    
    Differential Revision: https://phabricator.services.mozilla.com/D94221

---
+++ browser/base/content/test/tabMediaIndicator/browser_mediaplayback_audibility_change.js
@@ -0,0 +1,45 @@
+/**
+ * When media changes its audible state, the sound indicator should be
+ * updated as well, which should appear only when web audio is audible.
+ */
+add_task(async function testUpdateSoundIndicatorWhenMediaPlaybackChanges() {
+  info("create a tab loading media document");
+  const tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    gEMPTY_PAGE_URL
+  );
+  await initMediaPlaybackDocument(tab, "audio.ogg");
+
+  info(`sound indicator should appear when audible audio starts playing`);
+  await playAudio(tab);
+  await waitForTabSoundIndicatorAppears(tab);
+
+  info(`sound indicator should disappear when audio stops playing`);
+  await pauseAudio(tab);
+  await waitForTabSoundIndicatorDisappears(tab);
+
+  info("remove tab");
+  BrowserTestUtils.removeTab(tab);
+});
+
+/**
+ * Following are helper functions
+ */
+function initMediaPlaybackDocument(tab, fileName) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [fileName], async fileName => {
+    content.audio = content.document.createElement("audio");
+    content.audio.src = fileName;
+  });
+}
+
+function playAudio(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async _ => {
+    await content.audio.play();
+  });
+}
+
+function pauseAudio(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async _ => {
+    content.audio.pause();
+  });
+}
