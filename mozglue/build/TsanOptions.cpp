ccc093cd6c8310497d34e568d81b9d701f05967d
Alexis Beingessner <a.beingessner@gmail.com>
2020-10-21 17:01:29 +0000

    Bug 1367344 - Remove supression for seemingly fixed issue. r=decoder
    Doesn't show up in a try run anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94143

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -218,9 +218,6 @@ extern "C" const char* __tsan_default_suppressions() {
          // The rest of these suppressions are miscellaneous issues in gecko
          // that should be investigated and ideally fixed.
 
-         // Bug 1367344
-         "race:TelemetryImpl::sTelemetry\n"
-
          // Bug 1506812
          "race:BeginBackgroundRead\n"
 
