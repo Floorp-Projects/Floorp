6828d648c6a5fa28d6980719a68683850cdfc9f8
Mike Conley <mconley@mozilla.com>
2020-10-21 22:44:50 +0000

    Bug 1647757 - Terminate the about:home startup cache worker when experiencing memory-pressure. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D94391

---
+++ browser/components/newtab/test/xpcshell/head.js
@@ -0,0 +1,11 @@
+/* Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+/* eslint-disable no-unused-vars */
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
