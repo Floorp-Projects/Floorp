0b08f6e493f48ac3e0256b625d6d89839a3b72c5
Dorel Luca <dluca@mozilla.com>
2020-10-22 03:47:17 +0300

    Backed out 2 changesets (bug 1440203) for Backout conflicts with Bug 1654103. CLOSED TREE
    Backed out changeset 6e44c037b2dc (bug 1440203)
    Backed out changeset ab11665d8607 (bug 1440203)

--- ipc/chromium/src/base/shared_memory.h
+++ ipc/chromium/src/base/shared_memory.h
@@ -216,9 +216,8 @@ class SharedMemory {
   // If true indicates this came from an external source so needs extra checks
   // before being mapped.
   bool external_section_ = false;
-#elif defined(OS_POSIX) && !defined(ANDROID)
+#elif defined(OS_POSIX)
   mozilla::UniqueFileHandle frozen_file_;
-  bool is_memfd_ = false;
 #endif
   bool read_only_ = false;
   bool freezeable_ = false;
--- security/sandbox/linux/SandboxFilter.cpp
+++ security/sandbox/linux/SandboxFilter.cpp
@@ -704,10 +704,6 @@ class SandboxPolicyCommon : public SandboxPolicyBase {
       case __NR_munmap:
         return Allow();
 
-        // Shared memory
-      case __NR_memfd_create:
-        return Allow();
-
         // ipc::Shmem; also, glibc when creating threads:
       case __NR_mprotect:
         return Allow();
@@ -1398,6 +1394,11 @@ class ContentSandboxPolicy : public SandboxPolicyCommon {
 
       case __NR_eventfd2:
         return Allow();
+
+#  ifdef __NR_memfd_create
+      case __NR_memfd_create:
+        return Allow();
+#  endif
 
 #  ifdef __NR_rt_tgsigqueueinfo
         // Only allow to send signals within the process.
