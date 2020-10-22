e4ba63c09e30baf0e45b4df189125ae648d277f7
longsonr <longsonr@gmail.com>
2020-10-22 08:28:45 +0000

    Bug 1671950 - Fix assert in SVGSwitchFrame r=emilio
    We need to be consistent with the expectations of SVGContainerFrame::ReflowSVGNonDisplayText calling itself recursively on non-SVG content.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94048

--- layout/svg/SVGSwitchFrame.cpp
+++ layout/svg/SVGSwitchFrame.cpp
@@ -233,7 +233,8 @@ void SVGSwitchFrame::ReflowSVG() {
     // frame list, and we're iterating over that list now anyway.
     ConsiderChildOverflow(overflowRects, child);
   } else if (child && shouldReflowSVGTextFrameInside(child)) {
-    MOZ_ASSERT(child->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY),
+    MOZ_ASSERT(child->HasAnyStateBits(NS_FRAME_IS_NONDISPLAY) ||
+                   !child->IsFrameOfType(nsIFrame::eSVG),
                "Check for this explicitly in the |if|, then");
     ReflowSVGNonDisplayText(child);
   }
