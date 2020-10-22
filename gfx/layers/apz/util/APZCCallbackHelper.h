85cb621970552677748c8d7797897a58b56b857a
Botond Ballo <botond@mozilla.com>
2020-10-22 19:26:51 +0000

    Bug 1669588 - Factor out a OneShotPostRefreshObserver utility class. r=kats
    Differential Revision: https://phabricator.services.mozilla.com/D93897

--- gfx/layers/apz/util/APZCCallbackHelper.h
+++ gfx/layers/apz/util/APZCCallbackHelper.h
@@ -35,20 +35,21 @@ typedef std::function<void(uint64_t, const nsTArray<TouchBehaviorFlags>&)>
     SetAllowedTouchBehaviorCallback;
 
 /* Refer to documentation on SendSetTargetAPZCNotification for this class */
-class DisplayportSetListener : public nsAPostRefreshObserver {
+class DisplayportSetListener : public OneShotPostRefreshObserver {
  public:
   DisplayportSetListener(nsIWidget* aWidget, PresShell* aPresShell,
                          const uint64_t& aInputBlockId,
-                         const nsTArray<ScrollableLayerGuid>& aTargets);
+                         nsTArray<ScrollableLayerGuid>&& aTargets);
   virtual ~DisplayportSetListener();
   bool Register();
-  void DidRefresh() override;
 
  private:
   RefPtr<nsIWidget> mWidget;
-  RefPtr<PresShell> mPresShell;
   uint64_t mInputBlockId;
   nsTArray<ScrollableLayerGuid> mTargets;
+
+  static void OnPostRefresh(DisplayportSetListener* aListener,
+                            PresShell* aPresShell);
 };
 
 /* This class contains some helper methods that facilitate implementing the
