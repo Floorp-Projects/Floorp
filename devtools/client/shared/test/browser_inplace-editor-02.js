98ffa782b9cc57b384b416c1dd871053f11f577e
Ben Delevingne <rockingskier@gmail.com>
2020-10-23 11:01:42 +0000

    Bug 1672333 - remove defer from browser_inplace-editor-02.js. r=Honza
    Differential Revision: https://phabricator.services.mozilla.com/D94273

--- devtools/client/shared/test/browser_inplace-editor-02.js
+++ devtools/client/shared/test/browser_inplace-editor-02.js
@@ -21,64 +21,60 @@ add_task(async function() {
 
 function testNonTrimmed(doc) {
   info("Testing the trimOutput=false option");
-  const def = defer();
-
-  const initial = "\nMultiple\nLines\n";
-  const changed = " \nMultiple\nLines\n with more whitespace ";
-  createInplaceEditorAndClick(
-    {
-      trimOutput: false,
-      multiline: true,
-      initial: initial,
-      start: function(editor) {
-        is(
-          editor.input.value,
-          initial,
-          "Explicit initial value should be used."
-        );
-        editor.input.value = changed;
-        EventUtils.sendKey("return");
+  return new Promise(resolve => {
+    const initial = "\nMultiple\nLines\n";
+    const changed = " \nMultiple\nLines\n with more whitespace ";
+    createInplaceEditorAndClick(
+      {
+        trimOutput: false,
+        multiline: true,
+        initial: initial,
+        start: function(editor) {
+          is(
+            editor.input.value,
+            initial,
+            "Explicit initial value should be used."
+          );
+          editor.input.value = changed;
+          EventUtils.sendKey("return");
+        },
+        done: onDone(changed, true, resolve),
       },
-      done: onDone(changed, true, def),
-    },
-    doc
-  );
-
-  return def.promise;
+      doc
+    );
+  });
 }
 
 function testTrimmed(doc) {
   info("Testing the trimOutput=true option (default value)");
-  const def = defer();
-
-  const initial = "\nMultiple\nLines\n";
-  const changed = " \nMultiple\nLines\n with more whitespace ";
-  createInplaceEditorAndClick(
-    {
-      initial: initial,
-      multiline: true,
-      start: function(editor) {
-        is(
-          editor.input.value,
-          initial,
-          "Explicit initial value should be used."
-        );
-        editor.input.value = changed;
-        EventUtils.sendKey("return");
+  return new Promise(resolve => {
+    const initial = "\nMultiple\nLines\n";
+    const changed = " \nMultiple\nLines\n with more whitespace ";
+    createInplaceEditorAndClick(
+      {
+        initial: initial,
+        multiline: true,
+        start: function(editor) {
+          is(
+            editor.input.value,
+            initial,
+            "Explicit initial value should be used."
+          );
+          editor.input.value = changed;
+          EventUtils.sendKey("return");
+        },
+        done: onDone(changed.trim(), true, resolve),
       },
-      done: onDone(changed.trim(), true, def),
-    },
-    doc
-  );
-
-  return def.promise;
+      doc
+    );
+  });
 }
 
-function onDone(value, isCommit, def) {
+function onDone(value, isCommit, resolve) {
   return function(actualValue, actualCommit) {
     info("Inplace-editor's done callback executed, checking its state");
     is(actualValue, value, "The value is correct");
     is(actualCommit, isCommit, "The commit boolean is correct");
-    def.resolve();
+    resolve();
   };
 }
