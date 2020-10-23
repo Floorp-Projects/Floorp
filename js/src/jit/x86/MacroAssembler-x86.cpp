e554820a0d92778f7d06174cd1e4deebb35c24f2
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-10-23 18:17:36 +0000

    Bug 1639153 - Part 1: Reserve two slots after stack arguments for the future tls preservation. r=lth
    We are going to remove Frame::tls and support trampolines for indirect calls, so we need to get rid of using Frame::tls.
    In this and the followup patches I will iteratively remove all dependencies of Frame::tls and remove it eventually.
    
    In this patch I changed wasm ABI to allocate two stack slots after stack args to preserve caller's and callee's tls'es in the near future.
    
    Differential Revision: https://phabricator.services.mozilla.com/D82881

--- js/src/jit/x86/MacroAssembler-x86.cpp
+++ js/src/jit/x86/MacroAssembler-x86.cpp
@@ -334,7 +334,7 @@ void MacroAssembler::subFromStackPtr(Imm32 imm32) {
 // ABI function calls.
 
 void MacroAssembler::setupUnalignedABICall(Register scratch) {
-  setupABICall();
+  setupNativeABICall();
   dynamicAlignment_ = true;
 
   movl(esp, scratch);
