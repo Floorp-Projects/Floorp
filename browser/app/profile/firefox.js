314c0c84af44e55432a52d46f8262368330607d5
Mike Conley <mconley@mozilla.com>
2020-10-23 22:23:13 +0000

    Bug 1672517 - Enable the about:home startup cache by default on Nightly only. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D94654

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -254,7 +254,11 @@ pref("browser.defaultbrowser.notificationbar.checklimit", 10000);
 // The behavior of option 3 is detailed at: http://wiki.mozilla.org/Session_Restore
 pref("browser.startup.page",                1);
 pref("browser.startup.homepage",            "about:home");
+#ifdef NIGHTLY_BUILD
+pref("browser.startup.homepage.abouthome_cache.enabled", true);
+#else
 pref("browser.startup.homepage.abouthome_cache.enabled", false);
+#endif
 pref("browser.startup.homepage.abouthome_cache.loglevel", "Warn");
 
 // Whether we should skip the homepage when opening the first-run page
