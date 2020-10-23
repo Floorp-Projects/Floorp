01cd60cc1f4e7c0cac7a7811ba347148a7d01775
Gregory Mierzwinski <gmierz2@outlook.com>
2020-10-23 15:28:32 +0000

    Bug 1672740 - Add missing extraOptions for some test variants in Talos. r=perftest-reviewers,davehunt
    This patch adds extra options to the perfherder data for webrender, webrender-sw (webrender-software), and webgl-ipc (to distinguish from webgl results).
    
    Differential Revision: https://phabricator.services.mozilla.com/D94596

--- testing/talos/talos/run_tests.py
+++ testing/talos/talos/run_tests.py
@@ -252,6 +252,16 @@ function FindProxyForURL(url, host) {
     if browser_config.get('enable_fission', False):
         talos_results.add_extra_option('fission')
 
+    # differentiate webrender from non-webrender results
+    if browser_config['preferences'].get('gfx.webrender.software', False):
+        talos_results.add_extra_option("webrender-sw")
+    elif browser_config.get('enable_webrender', False):
+        talos_results.add_extra_option('webrender')
+
+    # differentiate webgl from webgl-ipc results
+    if browser_config['preferences'].get('webgl.out-of-process', False):
+        talos_results.add_extra_option('webgl-ipc')
+
     testname = None
 
     # run the tests
