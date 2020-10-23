0997b290bfe25dcce9834eb66a305b2dedb41f45
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-10-23 18:17:38 +0000

    Bug 1639153 - Part 5: Remove remaining uses of Frame::tls. r=lth
    Here we remove remaining uses of Frame::tls. There are many places where
    we use it: in debug frames, in profiling frames, in jit activation, etc.
    All these places require short fixes to use our new scheme for getting
    tls, so I gathered them together.
    
    Depends on D83045
    
    Differential Revision: https://phabricator.services.mozilla.com/D83051

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -4410,12 +4410,11 @@ class WasmABIArgIter : public ABIArgIterBase<VecT, WasmABIArgGenerator> {
   explicit WasmABIArgIter(const VecT& types)
       : ABIArgIterBase<VecT, WasmABIArgGenerator>(types) {}
 };
-
 }  // namespace jit
 
 namespace wasm {
-TlsData* ExtractCalleeTlsFromFrameWithTls(Frame* fp);
-TlsData* ExtractCallerTlsFromFrameWithTls(Frame* fp);
+const TlsData* ExtractCalleeTlsFromFrameWithTls(const Frame* fp);
+const TlsData* ExtractCallerTlsFromFrameWithTls(const Frame* fp);
 }  // namespace wasm
 
 }  // namespace js
