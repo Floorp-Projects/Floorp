3c023bdbeacb5593c15b53f9d8c83f8b50f15f0e
Dan Minor <dminor@mozilla.com>
2020-10-23 13:29:51 -0400

    Bug 1654112 - Vendor libwebrtc for rel86. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D130052

--- third_party/libwebrtc/webrtc/build/android/buildbot_try_builder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# Temporary placeholders to call new buildbot script locations until
-# buildbot master config can be pointed to new location.
-
-exec $(dirname $0)/buildbot/bb_try_builder.sh "$@"
