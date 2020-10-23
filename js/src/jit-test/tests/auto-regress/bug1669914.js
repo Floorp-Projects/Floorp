fdd9707f1537ee53e10028422b55bfcc4b7f75a7
Caroline Cullen <ccullen@mozilla.com>
2020-10-23 17:14:53 +0000

    Bug 1669914 - Return false if recursive mode DisassembleScript shell function cannot get or create a script. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D94068

---
+++ js/src/jit-test/tests/auto-regress/bug1669914.js
@@ -0,0 +1,10 @@
+function v0(v1, v2) {
+    try {
+        v3 = v0();
+    } catch (v4) {
+        if (typeof disassemble === "function") {
+            disassemble("-r", Function("()=>e,d"));
+        }
+    }
+}
+v24 = v0("matchAll");
