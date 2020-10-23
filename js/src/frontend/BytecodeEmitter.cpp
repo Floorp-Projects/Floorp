4956d7bf15524bd2c727d3a4c9ca5f5024aa4982
Bogdan Tara <btara@mozilla.com>
2020-10-23 21:03:29 +0300

    Backed out changeset f800c9af17ef (bug 1672172) for bustage complaining about SourceNotes.h CLOSED TREE

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -123,7 +123,7 @@ BytecodeEmitter::BytecodeEmitter(BytecodeEmitter* parent, SharedContext* sc,
     : sc(sc),
       cx(sc->cx_),
       parent(parent),
-      bytecodeSection_(cx, sc->extent().lineno, sc->extent().column),
+      bytecodeSection_(cx, sc->extent().lineno),
       perScriptData_(cx, compilationInfo),
       compilationInfo(compilationInfo),
       compilationState(compilationState),
@@ -554,11 +554,6 @@ bool BytecodeEmitter::updateLineNumberNotes(uint32_t offset) {
     unsigned line = er->lineAt(offset);
     unsigned delta = line - bytecodeSection().currentLine();
 
-    // If we use a `SetLine` note below, we want it to be relative to the
-    // scripts initial line number for better chance of sharing.
-    unsigned initialLine = sc->extent().lineno;
-    MOZ_ASSERT(line >= initialLine);
-
     /*
      * Encode any change in the current source line number by using
      * either several SrcNoteType::NewLine notes or just one
@@ -571,9 +566,10 @@ bool BytecodeEmitter::updateLineNumberNotes(uint32_t offset) {
      * SrcNoteType::SetLine.
      */
     bytecodeSection().setCurrentLine(line, offset);
-    if (delta >= SrcNote::SetLine::lengthFor(line, initialLine)) {
+    if (delta >= SrcNote::SetLine::lengthFor(line)) {
+      MOZ_ASSERT(line >= sc->extent().lineno);
       if (!newSrcNote2(SrcNoteType::SetLine,
-                       SrcNote::SetLine::toOperand(line, initialLine))) {
+                       SrcNote::SetLine::toOperand(line))) {
         return false;
       }
     } else {
