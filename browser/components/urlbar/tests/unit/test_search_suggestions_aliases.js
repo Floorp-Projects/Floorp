d6e43703809f70529c830347a677de41875a38aa
Drew Willcoxon <adw@mozilla.com>
2020-10-23 13:49:20 +0000

    Bug 1672680 - Properly handle non-ASCII spaces in search alias and restriction token searches. r=mak
    This fixes all the problems created by bug 1658964 and modifies existing tests
    so we check a variety of spaces.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94555

--- browser/components/urlbar/tests/unit/test_search_suggestions_aliases.js
+++ browser/components/urlbar/tests/unit/test_search_suggestions_aliases.js
@@ -12,6 +12,12 @@ const SUGGEST_PREF = "browser.urlbar.suggest.searches";
 const SUGGEST_ENABLED_PREF = "browser.search.suggest.enabled";
 const HISTORY_TITLE = "fire";
 
+// We make sure that aliases and search terms are correctly recognized when they
+// are separated by each of these different types of spaces and combinations of
+// spaces.  U+3000 is the ideographic space in CJK and is commonly used by CJK
+// speakers.
+const TEST_SPACES = [" ", "\u3000", " \u3000", "\u3000 "];
+
 let engine;
 
 add_task(async function setup() {
@@ -96,15 +102,61 @@ add_task(async function nonTokenAlias_trailingSpace() {
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
   for (let isPrivate of [false, true]) {
-    let context = createContext(alias + " ", { isPrivate });
+    for (let spaces of TEST_SPACES) {
+      info(
+        "Testing: " + JSON.stringify({ isPrivate, spaces: codePoints(spaces) })
+      );
+      let context = createContext(alias + spaces, { isPrivate });
+      await check_results({
+        context,
+        matches: [
+          makeSearchResult(context, {
+            engineName: SUGGESTIONS_ENGINE_NAME,
+            alias,
+            query: "",
+            heuristic: true,
+          }),
+          makeVisitResult(context, {
+            uri: "http://localhost:9000/search?terms=",
+            title: HISTORY_TITLE,
+          }),
+        ],
+      });
+    }
+  }
+});
+
+// Search for "alias HISTORY_TITLE" with a non-token alias in a non-private
+// context.  The remote suggestions and history result should be shown.
+add_task(async function nonTokenAlias_history_nonPrivate() {
+  let alias = "moz";
+  engine.alias = alias;
+  Assert.equal(engine.alias, alias);
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + HISTORY_TITLE, {
+      isPrivate: false,
+    });
     await check_results({
       context,
       matches: [
         makeSearchResult(context, {
           engineName: SUGGESTIONS_ENGINE_NAME,
           alias,
-          query: "",
+          query: HISTORY_TITLE,
           heuristic: true,
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          suggestion: `${HISTORY_TITLE} foo`,
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          suggestion: `${HISTORY_TITLE} bar`,
         }),
         makeVisitResult(context, {
           uri: "http://localhost:9000/search?terms=",
@@ -115,68 +167,33 @@ add_task(async function nonTokenAlias_trailingSpace() {
   }
 });
 
-// Search for "alias HISTORY_TITLE" with a non-token alias in a non-private
-// context.  The remote suggestions and history result should be shown.
-add_task(async function nonTokenAlias_history_nonPrivate() {
-  let alias = "moz";
-  engine.alias = alias;
-  Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} ${HISTORY_TITLE}`, {
-    isPrivate: false,
-  });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        heuristic: true,
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        suggestion: `${HISTORY_TITLE} foo`,
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        suggestion: `${HISTORY_TITLE} bar`,
-      }),
-      makeVisitResult(context, {
-        uri: "http://localhost:9000/search?terms=",
-        title: HISTORY_TITLE,
-      }),
-    ],
-  });
-});
-
 // Search for "alias HISTORY_TITLE" with a non-token alias in a private context.
 // The history result should be shown, but not the remote suggestions.
 add_task(async function nonTokenAlias_history_private() {
   let alias = "moz";
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} ${HISTORY_TITLE}`, {
-    isPrivate: true,
-  });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        heuristic: true,
-      }),
-      makeVisitResult(context, {
-        uri: "http://localhost:9000/search?terms=",
-        title: HISTORY_TITLE,
-      }),
-    ],
-  });
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + HISTORY_TITLE, {
+      isPrivate: true,
+    });
+    await check_results({
+      context,
+      matches: [
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          heuristic: true,
+        }),
+        makeVisitResult(context, {
+          uri: "http://localhost:9000/search?terms=",
+          title: HISTORY_TITLE,
+        }),
+      ],
+    });
+  }
 });
 
 // A token alias without a trailing space should be autofilled with a trailing
@@ -211,18 +228,23 @@ add_task(async function tokenAlias_trailingSpace() {
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
   for (let isPrivate of [false, true]) {
-    let context = createContext(alias + " ", { isPrivate });
-    await check_results({
-      context,
-      matches: [
-        makeSearchResult(context, {
-          engineName: SUGGESTIONS_ENGINE_NAME,
-          alias,
-          query: "",
-          heuristic: true,
-        }),
-      ],
-    });
+    for (let spaces of TEST_SPACES) {
+      info(
+        "Testing: " + JSON.stringify({ isPrivate, spaces: codePoints(spaces) })
+      );
+      let context = createContext(alias + spaces, { isPrivate });
+      await check_results({
+        context,
+        matches: [
+          makeSearchResult(context, {
+            engineName: SUGGESTIONS_ENGINE_NAME,
+            alias,
+            query: "",
+            heuristic: true,
+          }),
+        ],
+      });
+    }
   }
   Services.prefs.clearUserPref("browser.urlbar.update2");
 });
@@ -233,32 +255,35 @@ add_task(async function tokenAlias_history_nonPrivate() {
   let alias = "@moz";
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} ${HISTORY_TITLE}`, {
-    isPrivate: false,
-  });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        heuristic: true,
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        suggestion: `${HISTORY_TITLE} foo`,
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        suggestion: `${HISTORY_TITLE} bar`,
-      }),
-    ],
-  });
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + HISTORY_TITLE, {
+      isPrivate: false,
+    });
+    await check_results({
+      context,
+      matches: [
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          heuristic: true,
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          suggestion: `${HISTORY_TITLE} foo`,
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          suggestion: `${HISTORY_TITLE} bar`,
+        }),
+      ],
+    });
+  }
 });
 
 // Search for "alias HISTORY_TITLE" with a token alias in a private context.
@@ -267,20 +292,23 @@ add_task(async function tokenAlias_history_private() {
   let alias = "@moz";
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} ${HISTORY_TITLE}`, {
-    isPrivate: true,
-  });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: HISTORY_TITLE,
-        heuristic: true,
-      }),
-    ],
-  });
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + HISTORY_TITLE, {
+      isPrivate: true,
+    });
+    await check_results({
+      context,
+      matches: [
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: HISTORY_TITLE,
+          heuristic: true,
+        }),
+      ],
+    });
+  }
 });
 
 // Even when they're disabled, suggestions should still be returned when using a
@@ -291,30 +319,33 @@ add_task(async function suggestionsDisabled_nonPrivate() {
   let alias = "@moz";
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} term`, { isPrivate: false });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: "term",
-        heuristic: true,
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: "term",
-        suggestion: "term foo",
-      }),
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: "term",
-        suggestion: "term bar",
-      }),
-    ],
-  });
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + "term", { isPrivate: false });
+    await check_results({
+      context,
+      matches: [
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: "term",
+          heuristic: true,
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: "term",
+          suggestion: "term foo",
+        }),
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: "term",
+          suggestion: "term bar",
+        }),
+      ],
+    });
+  }
   Services.prefs.clearUserPref(SUGGEST_PREF);
   Services.prefs.clearUserPref(SUGGEST_ENABLED_PREF);
 });
@@ -327,18 +358,34 @@ add_task(async function suggestionsDisabled_private() {
   let alias = "@moz";
   engine.alias = alias;
   Assert.equal(engine.alias, alias);
-  let context = createContext(`${alias} term`, { isPrivate: true });
-  await check_results({
-    context,
-    matches: [
-      makeSearchResult(context, {
-        engineName: SUGGESTIONS_ENGINE_NAME,
-        alias,
-        query: "term",
-        heuristic: true,
-      }),
-    ],
-  });
-  Services.prefs.clearUserPref(SUGGEST_PREF);
-  Services.prefs.clearUserPref(SUGGEST_ENABLED_PREF);
+  for (let spaces of TEST_SPACES) {
+    info("Testing: " + JSON.stringify({ spaces: codePoints(spaces) }));
+    let context = createContext(alias + spaces + "term", { isPrivate: true });
+    await check_results({
+      context,
+      matches: [
+        makeSearchResult(context, {
+          engineName: SUGGESTIONS_ENGINE_NAME,
+          alias,
+          query: "term",
+          heuristic: true,
+        }),
+      ],
+    });
+    Services.prefs.clearUserPref(SUGGEST_PREF);
+    Services.prefs.clearUserPref(SUGGEST_ENABLED_PREF);
+  }
 });
+
+/**
+ * Returns an array of code points in the given string.  Each code point is
+ * returned as a hexidecimal string.
+ *
+ * @param {string} str
+ *   The code points of this string will be returned.
+ * @returns {array}
+ *   Array of code points in the string, where each is a hexidecimal string.
+ */
+function codePoints(str) {
+  return str.split("").map(s => s.charCodeAt(0).toString(16));
+}
