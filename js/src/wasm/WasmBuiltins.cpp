35c51a3d02412250f79a1bab9054e65c1281c8a5
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-10-23 18:17:38 +0000

    Bug 1639153 - Part 5: Remove remaining uses of Frame::tls. r=lth
    Here we remove remaining uses of Frame::tls. There are many places where
    we use it: in debug frames, in profiling frames, in jit activation, etc.
    All these places require short fixes to use our new scheme for getting
    tls, so I gathered them together.
    
    Differential Revision: https://phabricator.services.mozilla.com/D83051
    
    Depends on D83045

--- js/src/wasm/WasmBuiltins.cpp
+++ js/src/wasm/WasmBuiltins.cpp
@@ -304,7 +304,7 @@ static bool WasmHandleDebugTrap() {
   JitActivation* activation = CallingActivation();
   JSContext* cx = activation->cx();
   Frame* fp = activation->wasmExitFP();
-  Instance* instance = fp->instance();
+  Instance* instance = GetNearestEffectiveTls(fp)->instance;
   const Code& code = instance->code();
   MOZ_ASSERT(code.metadata().debugEnabled);
 
@@ -509,7 +509,7 @@ static void* WasmHandleTrap() {
       if (!CheckRecursionLimit(cx)) {
         return nullptr;
       }
-      if (activation->wasmExitFP()->tls()->isInterrupted()) {
+      if (activation->wasmExitTls()->isInterrupted()) {
         return CheckInterrupt(cx, activation);
       }
       return ReportError(cx, JSMSG_OVER_RECURSED);
