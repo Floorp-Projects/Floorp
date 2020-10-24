817501e1d8437de8257ca5ef53f6b42ac6ad4a89
Bogdan Tara <btara@mozilla.com>
2020-10-24 04:10:13 +0300

    Backed out changeset 0b27951e02d6 (bug 1672172) for browser_resources_* devtools failures CLOSED TREE

--- js/src/frontend/BytecodeSection.cpp
+++ js/src/frontend/BytecodeSection.cpp
@@ -188,16 +188,14 @@ JSObject* ObjLiteralStencil::create(JSContext* cx,
   return InterpretObjLiteral(cx, atomCache, atoms_, writer_);
 }
 
-BytecodeSection::BytecodeSection(JSContext* cx, uint32_t lineNum,
-                                 uint32_t column)
+BytecodeSection::BytecodeSection(JSContext* cx, uint32_t lineNum)
     : code_(cx),
       notes_(cx),
       lastNoteOffset_(0),
       tryNoteList_(cx),
       scopeNoteList_(cx),
       resumeOffsetList_(cx),
-      currentLine_(lineNum),
-      lastColumn_(column) {}
+      currentLine_(lineNum) {}
 
 void BytecodeSection::updateDepth(BytecodeOffset target) {
   jsbytecode* pc = code(target);
