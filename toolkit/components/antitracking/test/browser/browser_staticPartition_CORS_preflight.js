3ae5f8331ab0c6b110bf64b67a3173dc27275bac
Tim Huang <tihuang@mozilla.com>
2020-10-26 13:38:59 +0000

    Bug 1670617 - Part 1: Add a test to verify if the preflight cache is partitioned. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D93682

---
+++ toolkit/components/antitracking/test/browser/browser_staticPartition_CORS_preflight.js
@@ -0,0 +1,151 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+const TEST_PAGE =
+  "http://example.org/browser/toolkit/components/antitracking/test/browser/empty.html";
+const TEST_ANOTHER_PAGE =
+  "http://example.com/browser/toolkit/components/antitracking/test/browser/empty.html";
+const TEST_PREFLIGHT_IFRAME_PAGE =
+  "http://mochi.test:8888/browser/toolkit/components/antitracking/test/browser/empty.html";
+const TEST_PREFLIGHT_PAGE =
+  "http://example.net/browser/toolkit/components/antitracking/test/browser/browser_staticPartition_CORS_preflight.sjs";
+
+add_task(async function() {
+  let uuidGenerator = Cc["@mozilla.org/uuid-generator;1"].getService(
+    Ci.nsIUUIDGenerator
+  );
+
+  for (let networkIsolation of [true, false]) {
+    for (let partitionPerSite of [true, false]) {
+      await SpecialPowers.pushPrefEnv({
+        set: [
+          ["privacy.partition.network_state", networkIsolation],
+          ["privacy.dynamic_firstparty.use_site", partitionPerSite],
+        ],
+      });
+
+      // First, create one tab under one first party.
+      let tab = await BrowserTestUtils.openNewForegroundTab(
+        gBrowser,
+        TEST_PAGE
+      );
+      let token = uuidGenerator.generateUUID().toString();
+
+      // Use fetch to verify that preflight cache is working. The preflight
+      // cache is keyed by the loading principal and the url. So, we load an
+      // iframe with one origin and use fetch in there to ensure we will have
+      // the same loading principal.
+      await SpecialPowers.spawn(
+        tab.linkedBrowser,
+        [TEST_PREFLIGHT_PAGE, TEST_PREFLIGHT_IFRAME_PAGE, token],
+        async (url, iframe_url, token) => {
+          let iframe = content.document.createElement("iframe");
+
+          await new Promise(resolve => {
+            iframe.onload = () => {
+              resolve();
+            };
+            content.document.body.appendChild(iframe);
+            iframe.src = iframe_url;
+          });
+
+          await SpecialPowers.spawn(
+            iframe,
+            [url, token],
+            async (url, token) => {
+              const test_url = `${url}?token=${token}`;
+              let response = await content.fetch(
+                new content.Request(test_url, {
+                  mode: "cors",
+                  method: "GET",
+                  headers: [["x-test-header", "check"]],
+                })
+              );
+
+              is(
+                await response.text(),
+                "1",
+                "The preflight should be sent at first time"
+              );
+
+              response = await content.fetch(
+                new content.Request(test_url, {
+                  mode: "cors",
+                  method: "GET",
+                  headers: [["x-test-header", "check"]],
+                })
+              );
+
+              is(
+                await response.text(),
+                "0",
+                "The preflight shouldn't be sent due to the preflight cache"
+              );
+            }
+          );
+        }
+      );
+
+      // Load the tab with a different first party. And use fetch to check if
+      // the preflight cache is partitioned. The fetch will also be performed in
+      // the iframe with the same origin as above to ensure we use the same
+      // loading principal.
+      BrowserTestUtils.loadURI(tab.linkedBrowser, TEST_ANOTHER_PAGE);
+      await BrowserTestUtils.browserLoaded(tab.linkedBrowser);
+
+      await SpecialPowers.spawn(
+        tab.linkedBrowser,
+        [
+          TEST_PREFLIGHT_PAGE,
+          TEST_PREFLIGHT_IFRAME_PAGE,
+          token,
+          networkIsolation,
+        ],
+        async (url, iframe_url, token, partitioned) => {
+          let iframe = content.document.createElement("iframe");
+
+          await new Promise(resolve => {
+            iframe.onload = () => {
+              resolve();
+            };
+            content.document.body.appendChild(iframe);
+            iframe.src = iframe_url;
+          });
+
+          await SpecialPowers.spawn(
+            iframe,
+            [url, token, partitioned],
+            async (url, token, partitioned) => {
+              const test_url = `${url}?token=${token}`;
+
+              let response = await content.fetch(
+                new content.Request(test_url, {
+                  mode: "cors",
+                  method: "GET",
+                  headers: [["x-test-header", "check"]],
+                })
+              );
+
+              if (partitioned) {
+                is(
+                  await response.text(),
+                  "1",
+                  "The preflight cache should be partitioned"
+                );
+              } else {
+                is(
+                  await response.text(),
+                  "0",
+                  "The preflight cache shouldn't be partitioned"
+                );
+              }
+            }
+          );
+        }
+      );
+
+      BrowserTestUtils.removeTab(tab);
+    }
+  }
+});
---
+++ toolkit/components/antitracking/test/browser/browser_staticPartition_CORS_preflight.sjs
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+Components.utils.importGlobalProperties(["URLSearchParams"]);
+
+function handleRequest(request, response) {
+  let query = new URLSearchParams(request.queryString);
+  let token = query.get("token");
+
+  response.setHeader("Content-Type", "text/plain", false);
+  response.setHeader("Access-Control-Allow-Origin", "*", false);
+  response.setHeader("Access-Control-Allow-Headers", "x-test-header", false);
+
+  if (request.method == "OPTIONS") {
+    response.setHeader(
+      "Access-Control-Allow-Methods",
+      request.getHeader("Access-Control-Request-Method"),
+      false);
+    response.setHeader("Access-Control-Max-Age", "20", false);
+
+    setState(token, token);
+  } else {
+    let test_op = request.getHeader("x-test-header");
+
+    if (test_op == "check") {
+      let value = getState(token);
+
+      if (value) {
+        response.write("1");
+        setState(token, "");
+      } else {
+        response.write("0");
+      }
+    }
+  }
+}
