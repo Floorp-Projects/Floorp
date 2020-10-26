8d42cff47dd9224f5b58d7990e0eea0a3cb1b694
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-26 12:55:06 +0000

    Bug 1633712 - [devtools] Add a console message resource test for worker targets. r=jdescottes.
    Differential Revision: https://phabricator.services.mozilla.com/D88770

--- devtools/shared/resources/tests/test_worker.js
+++ devtools/shared/resources/tests/test_worker.js
@@ -1,5 +1,16 @@
 /* Any copyright is dedicated to the Public Domain.
    http://creativecommons.org/publicdomain/zero/1.0/ */
 
-// We don't need any computation in the worker,
-// just it to be alive
+"use strict";
+
+/* global globalThis */
+console.log("[WORKER] started", globalThis.location.toString(), globalThis);
+
+globalThis.onmessage = function(e) {
+  const { type, message } = e.data;
+
+  if (type === "log-in-worker") {
+    // Printing `e` so we can check that we have an object and not a stringified version
+    console.log("[WORKER]", message, e);
+  }
+};
