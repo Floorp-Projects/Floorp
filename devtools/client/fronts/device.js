054244e95b8d0cff2caadcbc5c09fcadcffe1efb
Fabien CASTERS <fabien@vaga.io>
2020-10-26 20:20:57 +0000

    Bug 1672433 - Remove backward compatibility method getDescription from devtools/client/fronts/device.js r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D94731

--- devtools/client/fronts/device.js
+++ devtools/client/fronts/device.js
@@ -17,21 +17,6 @@ class DeviceFront extends FrontClassWithSpec(deviceSpec) {
     // Attribute name from which to retrieve the actorID out of the target actor's form
     this.formAttributeName = "deviceActor";
   }
-
-  /**
-   * Handle backward compatibility for getDescription.
-   * Can be removed on Firefox 70 reaches the release channel.
-   */
-  async getDescription() {
-    const description = await super.getDescription();
-
-    // Backward compatibility when connecting for Firefox 69 or older.
-    if (typeof description.canDebugServiceWorkers === "undefined") {
-      description.canDebugServiceWorkers = !description.isMultiE10s;
-    }
-
-    return description;
-  }
 }
 
 exports.DeviceFront = DeviceFront;
