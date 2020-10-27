b7a9856e97260cfa681bdded908308d3cc6c963d
Aaron Klotz <aklotz@mozilla.com>
2020-10-27 11:41:28 +0000

    Bug 1650118: Part 6 - Update test harnesses; r=bc,sparky,perftest-reviewers,jgraham
    e10s is no longer a GeckoView option, so I'd like to remove it from test
    harnesses when possible.
    
    Depends on D90934
    
    Differential Revision: https://phabricator.services.mozilla.com/D91640

--- testing/mochitest/runjunit.py
+++ testing/mochitest/runjunit.py
@@ -163,9 +163,6 @@ class JUnitTestRunner(MochitestDesktop):
         cmd = "am instrument -w -r"
         # profile location
         cmd = cmd + " -e args '-profile %s'" % self.remote_profile
-        # multi-process
-        e10s = "true" if self.options.e10s else "false"
-        cmd = cmd + " -e use_multiprocess %s" % e10s
         # chunks (shards)
         shards = self.options.totalChunks
         shard = self.options.thisChunk
@@ -452,13 +449,6 @@ class JunitArgumentParser(argparse.ArgumentParser):
             dest="remoteTestRoot",
             help="Remote directory to use as test root "
             "(eg. /data/local/tmp/test_root).",
-        )
-        self.add_argument(
-            "--disable-e10s",
-            action="store_false",
-            dest="e10s",
-            default=True,
-            help="Disable multiprocess mode in test app.",
         )
         self.add_argument(
             "--max-time",
