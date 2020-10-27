b67fde7147da149469aba7595bf2130ba3f2e6bd
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-10-27 15:10:46 +0000

    Bug 1673550 - [devtools] Destroy the thread actor when the worker target actor is destroyed. r=ochameau.
    We have to manually call `ThreadActor#exit` as calling `destroy` won't completely
    destroy it, since it does not call `Actor#destroy`, whereas `ThreadActor#exit` does.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94835

--- devtools/server/actors/targets/worker.js
+++ devtools/server/actors/targets/worker.js
@@ -137,6 +137,15 @@ exports.WorkerTargetActor = ActorClassWithSpec(workerTargetSpec, {
   },
 
   destroy() {
+    if (this.threadActor) {
+      // We have to manually call `exit` as calling `destroy` won't completely destroy it.
+      // ThreadActor.destroy do not call protocoljs.Actor.destroy, ThreadActor.exit does it.
+      // We also need do this before destroying _sources as the thread actor may still
+      // use it while cleaning up things.
+      this.threadActor.exit();
+      this.threadActor = null;
+    }
+
     Actor.prototype.destroy.call(this);
 
     if (this._sources) {
@@ -146,7 +155,6 @@ exports.WorkerTargetActor = ActorClassWithSpec(workerTargetSpec, {
 
     this.workerGlobal = null;
     this._dbg = null;
-    this.threadActor = null;
     this._consoleActor = null;
   },
 });
