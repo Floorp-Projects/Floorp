f8579a1a51d410fb8fe4d91c2fc60af3a7e6efa9
Neil Deakin <neil@mozilla.com>
2020-10-27 00:26:22 +0000

    Bug 1672390, remove frame script and message manager use in devtools/client/shared/sourceeditor, r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D94389

--- devtools/client/shared/sourceeditor/test/browser_vimemacs.js
+++ devtools/client/shared/sourceeditor/test/browser_vimemacs.js
@@ -6,15 +6,8 @@
 const URI =
   "chrome://mochitests/content/browser/devtools/client" +
   "/shared/sourceeditor/test/codemirror/vimemacs.html";
-loadHelperScript("helper_codemirror_runner.js");
 
-async function test() {
+add_task(async function test() {
   requestLongerTimeout(4);
-  waitForExplicitFinish();
-
-  // Needed for a loadFrameScript(data:) call in helper_codemirror_runner.js
-  await pushPref("security.allow_parent_unrestricted_js_loads", true);
-
-  const tab = await addTab(URI);
-  runCodeMirrorTest(tab.linkedBrowser);
-}
+  await runCodeMirrorTest(URI);
+});
