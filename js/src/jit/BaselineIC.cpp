917610026d7a23743a9f687ae7cdc5282e6be181
Iain Ireland <iireland@mozilla.com>
2020-10-28 07:51:59 +0000

    Bug 1673497: Part 1: Invalidate Warp when failing to attach for a transpiled IC r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D94875

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -625,6 +625,11 @@ uint32_t ICStub::getEnteredCount() const {
   }
 }
 
+void ICFallbackStub::trackNotAttached(JSContext* cx, JSScript* script) {
+  maybeInvalidateWarp(cx, script);
+  state().trackNotAttached();
+}
+
 void ICFallbackStub::maybeInvalidateWarp(JSContext* cx, JSScript* script) {
   if (!state_.usedByTranspiler()) {
     return;
@@ -773,7 +778,7 @@ static void TryAttachStub(const char* name, JSContext* cx, BaselineFrame* frame,
         break;
     }
     if (!attached) {
-      stub->state().trackNotAttached();
+      stub->trackNotAttached(cx, frame->invalidationScript());
     }
   }
 }
@@ -1824,7 +1829,12 @@ static bool TryAttachGetPropStub(const char* name, JSContext* cx,
         MOZ_ASSERT_UNREACHABLE("No deferred GetProp stubs");
         break;
     }
+
+    if (!attached) {
+      stub->trackNotAttached(cx, frame->invalidationScript());
+    }
   }
+
   return attached;
 }
 
@@ -2208,7 +2218,7 @@ bool DoSetElemFallback(JSContext* cx, BaselineFrame* frame,
     }
   }
   if (!attached && canAttachStub) {
-    stub->state().trackNotAttached();
+    stub->trackNotAttached(cx, frame->invalidationScript());
   }
   return true;
 }
@@ -2850,7 +2860,7 @@ bool DoSetPropFallback(JSContext* cx, BaselineFrame* frame,
     }
   }
   if (!attached && canAttachStub) {
-    stub->state().trackNotAttached();
+    stub->trackNotAttached(cx, frame->invalidationScript());
   }
 
   return true;
@@ -3042,7 +3052,7 @@ bool DoCallFallback(JSContext* cx, BaselineFrame* frame, ICCall_Fallback* stub,
   }
 
   if (!handled && canAttachStub) {
-    stub->state().trackNotAttached();
+    stub->trackNotAttached(cx, frame->invalidationScript());
   }
   return true;
 }
@@ -3110,6 +3120,10 @@ bool DoSpreadCallFallback(JSContext* cx, BaselineFrame* frame,
         MOZ_ASSERT_UNREACHABLE("No deferred optimizations for spread calls");
         break;
     }
+  }
+
+  if (!handled) {
+    stub->trackNotAttached(cx, frame->invalidationScript());
   }
 
   if (!SpreadCallOperation(cx, script, pc, thisv, callee, arr, newTarget,
@@ -3414,7 +3428,7 @@ bool DoInstanceOfFallback(JSContext* cx, BaselineFrame* frame,
     // ensure we've recorded at least one failure, so we can detect there was a
     // non-optimizable case
     if (!stub->state().hasFailures()) {
-      stub->state().trackNotAttached();
+      stub->trackNotAttached(cx, frame->invalidationScript());
     }
     return true;
   }
