971447736cd0829ca5bc4063fd8f63a7d0285ead
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-10-28 23:00:15 +0200

    Backed out changeset 952da6bff887 (bug 1660054) for causing bc failures in browser_test_reset_scaling_zoom.js
    CLOSED TREE

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -246,8 +246,6 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   void HistoryCommitIndexAndLength();
 
-  void ResetScalingZoom();
-
  protected:
   // Called when the browsing context is being discarded.
   void CanonicalDiscard();
