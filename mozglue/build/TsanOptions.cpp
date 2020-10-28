3cc8d2d14c30b35dec07ad4eee26bb9836ee107e
Alexis Beingessner <a.beingessner@gmail.com>
2020-10-28 13:53:55 +0000

    Bug 1607706 - Remove supression for seemingly fixed issue. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D94855

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -263,9 +263,6 @@ extern "C" const char* __tsan_default_suppressions() {
          "race:nsJARChannel::Suspend\n"
          "race:nsJARChannel::Resume\n"
 
-         // Bug 1607706
-         "race:TemporaryIPCBlobParent::CreateAndShareFile\n"
-
          // Bug 1608068
          "race:makeOwnBaseShape\n"
          "race:numDynamicSlots\n"
