c75a6ce05888c34c96374e4a9444bd48687d0ca9
Neil Deakin <neil@mozilla.com>
2020-10-28 19:19:52 +0000

    Bug 1669369, remove legacy actor from PrintingChild and replace with JSWindowActor, r=mstriemer
    Differential Revision: https://phabricator.services.mozilla.com/D92519

--- toolkit/components/printing/tests/browser_preview_switch_print_selected.js
+++ toolkit/components/printing/tests/browser_preview_switch_print_selected.js
@@ -43,10 +43,7 @@ add_task(async function switch_print_preview_browsers() {
 
   // Enter print preview
   let defaultPPBrowser = PrintPreviewListener.getPrintPreviewBrowser();
-  let defaultPPEntered = BrowserTestUtils.waitForMessage(
-    defaultPPBrowser.messageManager,
-    "Printing:Preview:Entered"
-  );
+  let defaultPPEntered = PrintHelper.waitForOldPrintPreview(defaultPPBrowser);
   document.getElementById("cmd_printPreview").doCommand();
   await defaultPPEntered;
 
@@ -57,9 +54,8 @@ add_task(async function switch_print_preview_browsers() {
 
   // Here we call simplified mode
   let simplifiedPPBrowser = PrintPreviewListener.getSimplifiedPrintPreviewBrowser();
-  let simplifiedPPEntered = BrowserTestUtils.waitForMessage(
-    simplifiedPPBrowser.messageManager,
-    "Printing:Preview:Entered"
+  let simplifiedPPEntered = PrintHelper.waitForOldPrintPreview(
+    simplifiedPPBrowser
   );
   let printPreviewToolbar = document.getElementById("print-preview-toolbar");
 
@@ -96,10 +92,7 @@ add_task(async function switch_print_preview_browsers() {
   );
 
   // Switch back to default print preview content
-  defaultPPEntered = BrowserTestUtils.waitForMessage(
-    defaultPPBrowser.messageManager,
-    "Printing:Preview:Entered"
-  );
+  defaultPPEntered = PrintHelper.waitForOldPrintPreview(defaultPPBrowser);
   printPreviewToolbar.mSimplifyPageCheckbox.click();
   await defaultPPEntered;
 
