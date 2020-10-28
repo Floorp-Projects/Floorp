65657b7571b10755ec725b00254b61ecf105211a
Tooru Fujisawa <arai_a@mac.com>
2020-10-28 15:36:52 +0000

    Bug 1670510 - Do not return function that ends before the specified line in findScript. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D93177

---
+++ js/src/jit-test/tests/debug/Debugger-findScripts-29.js
@@ -0,0 +1,21 @@
+// |jit-test| skip-if: isLcovEnabled()
+
+// If the specified line is the next line after the function,
+// the function shouldn't match.
+
+var g = newGlobal({newCompartment: true});
+var dbg = new Debugger();
+var gw = dbg.addDebuggee(g);
+
+g.eval(`
+function f() {
+}
+`);
+
+gc();
+gc();
+
+let url = thisFilename();
+
+assertEq(dbg.findScripts({url, line: 3}).length, 1);
+assertEq(dbg.findScripts({url, line: 4}).length, 0);
