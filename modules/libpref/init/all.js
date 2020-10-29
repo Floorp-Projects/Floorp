7dcd096fec8ddc861964b17e406efb0de9e12092
pbz <pbz@mozilla.com>
2020-10-29 13:43:46 +0000

    Bug 1565574 - Added permission required to open external protocol handlers. r=Gijs
    - Added pref to toggle permission feature
    - Updated ContentDispatchChooser to check for permission and  manage a multi dialog flow.
    
    Differential Revision: https://phabricator.services.mozilla.com/D92945

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4736,3 +4736,8 @@ pref("dom.postMessage.sharedArrayBuffer.bypassCOOP_COEP.insecure.enabled", false
 
 // Whether to start the private browsing mode at application startup
 pref("browser.privatebrowsing.autostart", false);
+
+// Whether sites require the open-protocol-handler permission to open a
+//preferred external application for a protocol. If a site doesn't have
+// permission we will show a prompt.
+pref("security.external_protocol_requires_permission", true);
