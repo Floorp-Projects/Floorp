dd86c92b68bd1ab85614767629c10bff325cee7d
alwu <alwu@mozilla.com>
2020-10-29 19:06:44 +0000

    Bug 1673521 - move all browser tests into browser folder in order to adopt browser-test eslint setting automatically. r=Standard8
    Differential Revision: https://phabricator.services.mozilla.com/D94872

--- dom/media/mediacontrol/tests/browser/browser_media_control_main_controller.js
+++ dom/media/mediacontrol/tests/browser/browser_media_control_main_controller.js
@@ -1,15 +1,12 @@
-/* eslint-disable no-undef */
-"use strict";
-
 // Import this in order to use `triggerPictureInPicture()`.
-/* import-globals-from ../../../../toolkit/components/pictureinpicture/tests/head.js */
+/* import-globals-from ../../../../../toolkit/components/pictureinpicture/tests/head.js */
 Services.scriptloader.loadSubScript(
   "chrome://mochitests/content/browser/toolkit/components/pictureinpicture/tests/head.js",
   this
 );
 
 const PAGE_NON_AUTOPLAY =
-  "https://example.com/browser/dom/media/mediacontrol/tests/file_non_autoplay.html";
+  "https://example.com/browser/dom/media/mediacontrol/tests/browser/file_non_autoplay.html";
 
 const testVideoId = "video";
 
