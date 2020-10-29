f5c615592600153d0900e0eb6c14e0475c3af4dd
Razvan Maries <rmaries@mozilla.com>
2020-10-30 01:29:15 +0200

    Backed out changeset b952d7de3e3e (bug 1650956) for perma failures on browser_ext_tabs_successors.js. CLOSED TREE

--- browser/components/extensions/parent/ext-tabs.js
+++ browser/components/extensions/parent/ext-tabs.js
@@ -48,7 +48,7 @@ XPCOMUtils.defineLazyGetter(this, "strBundle", function() {
   );
 });
 
-var { DefaultMap, ExtensionError } = ExtensionUtils;
+var { ExtensionError } = ExtensionUtils;
 
 const TABHIDE_PREFNAME = "extensions.webextensions.tabhide.enabled";
 
@@ -803,25 +803,8 @@ this.tabs = class extends ExtensionAPI {
         },
 
         async remove(tabIds) {
-          let nativeTabs = getNativeTabsFromIDArray(tabIds);
-
-          if (nativeTabs.length === 1) {
-            nativeTabs[0].ownerGlobal.gBrowser.removeTab(nativeTabs[0]);
-            return;
-          }
-
-          // Or for multiple tabs, first group them by window
-          let windowTabMap = new DefaultMap(() => []);
-          for (let nativeTab of nativeTabs) {
-            windowTabMap.get(nativeTab.ownerGlobal).push(nativeTab);
-          }
-
-          // Then make one call to removeTabs() for each window, to keep the
-          // count accurate for SessionStore.getLastClosedTabCount().
-          // Note: always passing {animate: false} so that way all tabs are
-          // actually closed when the browser.tabs.remove() promise resolves
-          for (let [eachWindow, tabsToClose] of windowTabMap.entries()) {
-            eachWindow.gBrowser.removeTabs(tabsToClose, { animate: false });
+          for (let nativeTab of getNativeTabsFromIDArray(tabIds)) {
+            nativeTab.ownerGlobal.gBrowser.removeTab(nativeTab);
           }
         },
 
