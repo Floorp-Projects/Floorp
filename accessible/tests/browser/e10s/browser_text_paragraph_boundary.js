c5ddb02be9085598102e6a25497e4907e77aa5a0
James Teh <jteh@mozilla.com>
2020-10-29 06:04:33 +0000

    Bug 1673919: Make ParagraphBoundaryRule skip over OuterDocAccessibles. r=MarcoZ
    The browser test is really a crash test, but it's really difficult to test a11y APIs properly using the crashtest framework.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95107

---
+++ accessible/tests/browser/e10s/browser_text_paragraph_boundary.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Test that we don't crash the parent process when querying the paragraph
+// boundary on an Accessible which has remote ProxyAccessible descendants.
+addAccessibleTask(
+  `test`,
+  async function testParagraphBoundaryWithRemoteDescendants(browser, accDoc) {
+    const root = getRootAccessible(document).QueryInterface(
+      Ci.nsIAccessibleText
+    );
+    let start = {};
+    let end = {};
+    // The offsets will change as the Firefox UI changes. We don't really care
+    // what they are, just that we don't crash.
+    root.getTextAtOffset(0, nsIAccessibleText.BOUNDARY_PARAGRAPH, start, end);
+    ok(true, "Getting paragraph boundary succeeded");
+  }
+);
