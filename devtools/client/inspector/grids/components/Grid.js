61236bcfbecd38e79d00379d35b65bd671608bff
Razvan Caliman <rcaliman@mozilla.com>
2020-10-29 14:35:58 +0000

    Bug 1623906 - [devtools] Dispatch thunks from Grid panel React components to show/hide the Box Model Highlighter r=gl
    Depends on D95035
    
    This patch is using the approach introduced in D79556 to dispatch thunks (functions) to the Redux store to show/hide the box model highlighter from individual React components instead of passing down the methods through deeply nested trees of components that don't use them.
    
    The Redux `dispatch()` method is passed down via the component tree instead. This enables further usage of dispatching thunks for other scenarios avoiding prop drilling.
    The `showBoxModelHighlighterForNode` and `hideBoxModelHighlighter` props and methods are removed in favor of the corresponding thunks.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95036

--- devtools/client/inspector/grids/components/Grid.js
+++ devtools/client/inspector/grids/components/Grid.js
@@ -36,13 +36,12 @@ const Types = require("devtools/client/inspector/grids/types");
 class Grid extends PureComponent {
   static get propTypes() {
     return {
+      dispatch: PropTypes.func.isRequired,
       getSwatchColorPickerTooltip: PropTypes.func.isRequired,
       grids: PropTypes.arrayOf(PropTypes.shape(Types.grid)).isRequired,
       highlighterSettings: PropTypes.shape(Types.highlighterSettings)
         .isRequired,
-      onHideBoxModelHighlighter: PropTypes.func.isRequired,
       onSetGridOverlayColor: PropTypes.func.isRequired,
-      onShowBoxModelHighlighterForNode: PropTypes.func.isRequired,
       onShowGridOutlineHighlight: PropTypes.func.isRequired,
       onToggleGridHighlighter: PropTypes.func.isRequired,
       onToggleShowGridAreas: PropTypes.func.isRequired,
@@ -61,12 +60,11 @@ class Grid extends PureComponent {
     }
 
     const {
+      dispatch,
       getSwatchColorPickerTooltip,
       grids,
       highlighterSettings,
-      onHideBoxModelHighlighter,
       onSetGridOverlayColor,
-      onShowBoxModelHighlighterForNode,
       onShowGridOutlineHighlight,
       onToggleShowGridAreas,
       onToggleGridHighlighter,
@@ -81,11 +79,10 @@ class Grid extends PureComponent {
       dom.div(
         { className: "grid-content" },
         GridList({
+          dispatch,
           getSwatchColorPickerTooltip,
           grids,
-          onHideBoxModelHighlighter,
           onSetGridOverlayColor,
-          onShowBoxModelHighlighterForNode,
           onToggleGridHighlighter,
           setSelectedNode,
         }),
