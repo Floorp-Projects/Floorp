59f936e87f4637105209df7f1180cac8cf67cc95
Dorel Luca <dluca@mozilla.com>
2020-10-29 18:36:53 +0200

    Backed out 7 changesets (bug 1639153) for Spidermonkey failures in wasm/simd/cvt-x64-ion-codegen.j. CLOSED TREE
    Backed out changeset 42728e99fbc5 (bug 1639153)
    Backed out changeset c414a69233f0 (bug 1639153)
    Backed out changeset 1e72f84b4206 (bug 1639153)
    Backed out changeset 25281e1775c9 (bug 1639153)
    Backed out changeset 82fbedcdde75 (bug 1639153)
    Backed out changeset 3e82e75c4992 (bug 1639153)
    Backed out changeset 22c94980495f (bug 1639153)

--- js/src/jit/arm64/Assembler-arm64.h
+++ js/src/jit/arm64/Assembler-arm64.h
@@ -421,7 +421,6 @@ class ABIArgGenerator {
   ABIArg next(MIRType argType);
   ABIArg& current() { return current_; }
   uint32_t stackBytesConsumedSoFar() const { return stackOffset_; }
-  void increaseStackOffset(uint32_t bytes) { stackOffset_ += bytes; }
 
  protected:
   unsigned intRegIndex_;
