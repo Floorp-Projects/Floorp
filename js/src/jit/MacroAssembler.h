3d15121f17bff1354dc6f4c0a574cd898fa360a5
André Bargull <andre.bargull@gmail.com>
2020-10-30 15:50:15 +0000

    Bug 1673840 - Part 1: Move copySign to MacroAssemblerX86Shared. r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D94959

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -1101,6 +1101,11 @@ class MacroAssembler : public MacroAssemblerSpecific {
   void signDoubleToInt32(FloatRegister input, Register output,
                          FloatRegister temp, Label* fail);
 
+  void copySignDouble(FloatRegister lhs, FloatRegister rhs,
+                      FloatRegister output) DEFINED_ON(x86_shared);
+  void copySignFloat32(FloatRegister lhs, FloatRegister rhs,
+                       FloatRegister output) DEFINED_ON(x86_shared);
+
   // Returns a random double in range [0, 1) in |dest|. The |rng| register must
   // hold a pointer to a mozilla::non_crypto::XorShift128PlusRNG.
   void randomDouble(Register rng, FloatRegister dest, Register64 temp0,
