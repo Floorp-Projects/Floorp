2a9013cc85ae1f95dbd1ba207b614dd1cda23181
Masayuki Nakano <masayuki@d-toybox.com>
2020-10-30 13:45:48 +0000

    Bug 1666556 - Make `HTMLEditor` stop accepting focus event when a text control which is an editing host gets focus r=m_kato
    `HTMLEditor` ignores input events when a text control element in an editing
    host has focus.
    https://searchfox.org/mozilla-central/rev/d866b96d74ec2a63f09ee418f048d23f4fd379a2/editor/libeditor/HTMLEditor.cpp#6322
    
    However, focus event does not check it and modifies ancestor limiter of
    `Selection` for the document.  And even in this case, public methods for
    handling some edit actions are open for `execCommand` requests.  Then,
    edit action handlers may set selection outside the elements, but new focus
    event to update the selection ancestor limiter will be handled later.
    Therefore, until getting next focus event, `HTMLEditor` does not work as
    expected since `Selection::*InLimiter()` APIs refuse to change selection.
    
    Ideally, we should make `nsFocusManager` notifies editor of focus change
    synchronously, but I cannot fix it quickly because it needs to touch security
    sensitive area.  Therefore, this patch makes `HTMLEditor` do not set
    ancestor limiter when a text control in any editing host gets focus.
    
    Additionally, this patch makes
    `HTMLEditor::AutoDeleteRangesHandler::AutoBlockElementsJoiner::HandleDeleteAtOtherBlockBoundary`
    stop handling leaf node deletion if it cannot change caret position.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95268

--- editor/libeditor/HTMLEditorDeleteHandler.cpp
+++ editor/libeditor/HTMLEditorDeleteHandler.cpp
@@ -2468,21 +2468,25 @@ nsresult HTMLEditor::AutoDeleteRangesHandler::AutoBlockElementsJoiner::
       aDirectionAndAmount == nsIEditor::ePrevious
           ? EditorRawDOMPoint::AtEndOf(*mLeafContentInOtherBlock)
           : EditorRawDOMPoint(mLeafContentInOtherBlock, 0);
+  // If new caret position is same as current caret position, we can do
+  // nothing anymore.
+  if (aRangesToDelete.IsCollapsed() &&
+      aRangesToDelete.FocusRef() == newCaretPoint.ToRawRangeBoundary()) {
+    return NS_OK;
+  }
   // TODO: Stop modifying the `Selection` for computing the targer ranges.
   nsresult rv = aHTMLEditor.CollapseSelectionTo(newCaretPoint);
-  if (!aHTMLEditor.SelectionRefPtr()->RangeCount()) {
-    NS_WARNING("Failed to put caret to new position");
-    return NS_ERROR_FAILURE;
-  }
   NS_WARNING_ASSERTION(NS_SUCCEEDED(rv),
-                       "HTMLEditor::CollapseSelectionTo() failed, but ignored");
-  aRangesToDelete.Initialize(*aHTMLEditor.SelectionRefPtr());
-  AutoDeleteRangesHandler anotherHandler(mDeleteRangesHandlerConst);
-  rv = anotherHandler.ComputeRangesToDelete(aHTMLEditor, aDirectionAndAmount,
-                                            aRangesToDelete);
-  NS_WARNING_ASSERTION(
-      NS_SUCCEEDED(rv),
-      "Recursive AutoDeleteRangesHandler::ComputeRangesToDelete() failed");
+                       "HTMLEditor::CollapseSelectionTo() failed");
+  if (NS_SUCCEEDED(rv)) {
+    aRangesToDelete.Initialize(*aHTMLEditor.SelectionRefPtr());
+    AutoDeleteRangesHandler anotherHandler(mDeleteRangesHandlerConst);
+    rv = anotherHandler.ComputeRangesToDelete(aHTMLEditor, aDirectionAndAmount,
+                                              aRangesToDelete);
+    NS_WARNING_ASSERTION(
+        NS_SUCCEEDED(rv),
+        "Recursive AutoDeleteRangesHandler::ComputeRangesToDelete() failed");
+  }
   // Restore selection.
   nsresult rvCollapsingSelectionTo =
       aHTMLEditor.CollapseSelectionTo(aCaretPoint);
@@ -2583,17 +2587,17 @@ EditActionResult HTMLEditor::AutoDeleteRangesHandler::AutoBlockElementsJoiner::
           aDirectionAndAmount == nsIEditor::ePrevious
               ? EditorRawDOMPoint::AtEndOf(*mLeafContentInOtherBlock)
               : EditorRawDOMPoint(mLeafContentInOtherBlock, 0);
+      // If new caret position is same as current caret position, we can do
+      // nothing anymore.
+      if (aRangesToDelete.IsCollapsed() &&
+          aRangesToDelete.FocusRef() == newCaretPoint.ToRawRangeBoundary()) {
+        return EditActionCanceled();
+      }
       nsresult rv = aHTMLEditor.CollapseSelectionTo(newCaretPoint);
-      if (NS_WARN_IF(rv == NS_ERROR_EDITOR_DESTROYED)) {
-        return result.SetResult(NS_ERROR_EDITOR_DESTROYED);
+      if (NS_FAILED(rv)) {
+        NS_WARNING("HTMLEditor::CollapseSelectionTo() failed");
+        return result.SetResult(rv);
       }
-      if (!aHTMLEditor.SelectionRefPtr()->RangeCount()) {
-        NS_WARNING("Failed to put caret to new position");
-        return EditActionHandled(rv);
-      }
-      NS_WARNING_ASSERTION(
-          NS_SUCCEEDED(rv),
-          "HTMLEditor::CollapseSelectionTo() failed, but ignored");
       AutoRangeArray rangesToDelete(*aHTMLEditor.SelectionRefPtr());
       AutoDeleteRangesHandler anotherHandler(mDeleteRangesHandler);
       result = anotherHandler.Run(aHTMLEditor, aDirectionAndAmount,
