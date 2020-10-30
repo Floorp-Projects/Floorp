dd83052fb3d96800707bd4f138edb123ddeaa718
Emilio Cobos Álvarez <emilio@crisal.io>
2020-10-30 11:56:27 +0000

    Bug 1674228 - Speed up HasFontTable checks for freetype fonts. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D95280

--- gfx/thebes/gfxFT2FontBase.h
+++ gfx/thebes/gfxFT2FontBase.h
@@ -27,6 +27,10 @@ class gfxFT2FontEntryBase : public gfxFontEntry {
 
   CmapCacheSlot* GetCmapCacheSlot(uint32_t aCharCode);
 
+  static bool HasFontTable(mozilla::gfx::SharedFTFace*, uint32_t aTableTag);
+  static nsresult CopyFontTable(mozilla::gfx::SharedFTFace*, uint32_t aTableTag,
+                                nsTArray<uint8_t>&);
+
  private:
   enum { kNumCmapCacheSlots = 256 };
 
--- gfx/thebes/gfxFcPlatformFontList.h
+++ gfx/thebes/gfxFcPlatformFontList.h
@@ -100,6 +100,8 @@ class gfxFontconfigFontEntry final : public gfxFT2FontEntryBase {
   void GetVariationInstances(
       nsTArray<gfxFontVariationInstance>& aInstances) override;
 
+  bool HasFontTable(uint32_t aTableTag) override;
+  nsresult CopyFontTable(uint32_t aTableTag, nsTArray<uint8_t>&) override;
   hb_blob_t* GetFontTable(uint32_t aTableTag) override;
 
   double GetAspect();
@@ -109,9 +111,6 @@ class gfxFontconfigFontEntry final : public gfxFT2FontEntryBase {
 
   gfxFont* CreateFontInstance(const gfxFontStyle* aFontStyle) override;
 
-  // override to pull data from FTFace
-  virtual nsresult CopyFontTable(uint32_t aTableTag,
-                                 nsTArray<uint8_t>& aBuffer) override;
 
   // pattern for a single face of a family
   RefPtr<FcPattern> mFontPattern;
