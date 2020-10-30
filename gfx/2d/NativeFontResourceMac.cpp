323685d8ddc6f7c9774db7775ae4f2f364ee8d2e
Jeff Muizelaar <jmuizelaar@mozilla.com>
2020-10-30 00:29:55 +0000

    Bug 1674175 - Add a memory reporter for memory allocated in NativeFontResourceMac::Create. r=mstange
    It's very hard for us to encourage the system to free this memory so we
    should make sure we report it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95233

--- gfx/2d/NativeFontResourceMac.cpp
+++ gfx/2d/NativeFontResourceMac.cpp
@@ -4,28 +4,78 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <unordered_set>
 #include "NativeFontResourceMac.h"
 #include "UnscaledFontMac.h"
 #include "Types.h"
 
 #include "mozilla/RefPtr.h"
+#include "mozilla/DataMutex.h"
 
 #ifdef MOZ_WIDGET_UIKIT
 #  include <CoreFoundation/CoreFoundation.h>
 #endif
 
+#include "nsIMemoryReporter.h"
 #include "nsCocoaFeatures.h"
 
 namespace mozilla {
 namespace gfx {
 
+static StaticDataMutex<std::unordered_set<void*>> sWeakFontDataSet("WeakFonts");
+
+void FontDataDeallocate(void*, void* info) {
+  auto set = sWeakFontDataSet.Lock();
+  set->erase(info);
+  free(info);
+}
+
+class NativeFontResourceMacReporter final : public nsIMemoryReporter {
+  ~NativeFontResourceMacReporter() = default;
+
+  MOZ_DEFINE_MALLOC_SIZE_OF(MallocSizeOf)
+ public:
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD CollectReports(nsIHandleReportCallback* aHandleReport,
+                            nsISupports* aData, bool aAnonymize) override {
+    MOZ_COLLECT_REPORT("explicit/gfx/native-font-resource-mac", KIND_HEAP,
+                       UNITS_BYTES, SizeOfData(MallocSizeOf),
+                       "Total memory used by native font API resource data.");
+    return NS_OK;
+  }
+
+  static size_t SizeOfData(mozilla::MallocSizeOf aMallocSizeOf) {
+    auto fontData = sWeakFontDataSet.Lock();
+    size_t total = 0;
+    for (auto& i : *fontData) {
+      total += aMallocSizeOf(i);
+    }
+    return total;
+  }
+};
+
+NS_IMPL_ISUPPORTS(NativeFontResourceMacReporter, nsIMemoryReporter)
+
+void NativeFontResourceMac::RegisterMemoryReporter() {
+  RegisterStrongMemoryReporter(new NativeFontResourceMacReporter);
+}
+
 /* static */
 already_AddRefed<NativeFontResourceMac> NativeFontResourceMac::Create(
     uint8_t* aFontData, uint32_t aDataLength) {
-  // copy font data
-  CFDataRef data = CFDataCreate(kCFAllocatorDefault, aFontData, aDataLength);
-  if (!data) {
-    return nullptr;
+  uint8_t* fontData = (uint8_t*)malloc(aDataLength);
+  memcpy(fontData, aFontData, aDataLength);
+  CFAllocatorContext context = {0,       fontData, nullptr, nullptr,
+                                nullptr, nullptr,  nullptr, FontDataDeallocate,
+                                nullptr};
+  CFAllocatorRef allocator = CFAllocatorCreate(kCFAllocatorDefault, &context);
+  CFDataRef data = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, fontData,
+                                               aDataLength, allocator);
+
+  {
+    auto set = sWeakFontDataSet.Lock();
+    set->insert(fontData);
   }
 
   CTFontDescriptorRef ctFontDesc =
