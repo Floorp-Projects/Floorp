7df2ff8d802383f9427e44efd329737fe1370b28
Belén Albeza <balbeza@mozilla.com>
2020-10-30 13:33:47 +0000

    Bug 1669509 - [devtools] Hide internal indexedDB's when we are not in the browser toolbox r=ochameau
    In https://phabricator.services.mozilla.com/D91122#C3084925OL296 we transferred the filtering of indexedDB dbs to a transformer, but it was not being applied since the `hosts` getter was still used by the client. Since we discussed this filtering should be done in the server, this patch moves the filtering to the storage actor instead.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95020

---
+++ devtools/client/storage/test/browser_storage_indexeddb_hide_internal_dbs.js
@@ -0,0 +1,64 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* import-globals-from ../../shared/test/shared-head.js */
+
+"use strict";
+
+/* global gToolbox */
+/* import-globals-from ../../framework/browser-toolbox/test/helpers-browser-toolbox.js */
+Services.scriptloader.loadSubScript(
+  "chrome://mochitests/content/browser/devtools/client/framework/browser-toolbox/test/helpers-browser-toolbox.js",
+  this
+);
+
+// Test that internal DBs are hidden in the regular toolbox,but visible in the
+// Browser Toolbox
+add_task(async function() {
+  await openTabAndSetupStorage(MAIN_DOMAIN + "storage-empty-objectstores.html");
+  const doc = gPanelWindow.document;
+
+  // check regular toolbox
+  info("Check indexedDB tree in browser toolbox");
+  const hosts = getDBHostsInTree(doc);
+  is(hosts.length, 1, "There is only one host for indexedDB storage");
+  is(hosts[0], "http://test1.example.org", "Host is test1.example.org");
+
+  // check browser toolbox
+  info("awaiting to open browser toolbox");
+  const ToolboxTask = await initBrowserToolboxTask();
+  await ToolboxTask.importFunctions({ getDBHostsInTree });
+
+  await ToolboxTask.spawn(null, async () => {
+    info("Selecting storage panel");
+    await gToolbox.selectTool("storage");
+    info("Check indexedDB tree in browser toolbox");
+    const browserToolboxDoc = gToolbox.getCurrentPanel().panelWindow.document;
+
+    const browserToolboxHosts = getDBHostsInTree(browserToolboxDoc);
+    ok(browserToolboxHosts.length > 1, "There are more than 1 indexedDB hosts");
+    ok(
+      browserToolboxHosts.includes("about:devtools-toolbox"),
+      "about:devtools-toolbox host is present"
+    );
+    ok(browserToolboxHosts.includes("chrome"), "chrome host is present");
+    ok(
+      browserToolboxHosts.includes("indexeddb+++fx-devtools"),
+      "fx-devtools host is present"
+    );
+  });
+
+  info("Destroying browser toolbox");
+  await ToolboxTask.destroy();
+});
+
+function getDBHostsInTree(doc) {
+  const treeId = JSON.stringify(["indexedDB"]);
+  const items = doc.querySelectorAll(
+    `[data-id='${treeId}'] > .tree-widget-children > *`
+  );
+
+  // the host is located at the 2nd element of the array in data-id
+  return [...items].map(x => JSON.parse(x.dataset.id)[1]);
+}
