607682c739cb2e8ba65fb49387f30488a0ca1fd7
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-10-31 09:56:05 +0000

    Bug 1639153 - Part 5: Remove remaining uses of Frame::tls. r=lth
    Here we remove remaining uses of Frame::tls. There are many places where
    we use it: in debug frames, in profiling frames, in jit activation, etc.
    All these places require short fixes to use our new scheme for getting
    tls, so I gathered them together.
    
    Depends on D83045
    
    Differential Revision: https://phabricator.services.mozilla.com/D83051

--- js/src/wasm/WasmTypes.cpp
+++ js/src/wasm/WasmTypes.cpp
@@ -602,10 +602,11 @@ size_t wasm::ComputeMappedSize(uint64_t maxSize) {
 
 /* static */
 DebugFrame* DebugFrame::from(Frame* fp) {
-  MOZ_ASSERT(fp->instance()->code().metadata().debugEnabled);
+  MOZ_ASSERT(
+      GetNearestEffectiveTls(fp)->instance->code().metadata().debugEnabled);
   auto* df =
       reinterpret_cast<DebugFrame*>((uint8_t*)fp - DebugFrame::offsetOfFrame());
-  MOZ_ASSERT(fp->instance() == df->instance());
+  MOZ_ASSERT(GetNearestEffectiveTls(fp)->instance == df->instance());
   return df;
 }
 
@@ -624,6 +625,10 @@ void DebugFrame::alignmentStaticAsserts() {
   // aware of possible problems.
   static_assert(sizeof(DebugFrame) % 16 == 0, "ARM64 SP alignment");
 #endif
+}
+
+Instance* DebugFrame::instance() const {
+  return GetNearestEffectiveTls(&frame_)->instance;
 }
 
 GlobalObject* DebugFrame::global() const {
