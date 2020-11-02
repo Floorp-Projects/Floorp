495d5646bec57db7c9f8ede7820e690beec18d6a
Markus Stange <mstange.moz@gmail.com>
2020-11-02 17:10:24 +0000

    Bug 1671704 - Draw macOS native-looking scrollbars when using the basic theme on macOS. r=spohl
    Differential Revision: https://phabricator.services.mozilla.com/D93846

--- widget/cocoa/nsNativeBasicThemeCocoa.h
+++ widget/cocoa/nsNativeBasicThemeCocoa.h
@@ -9,9 +9,40 @@
 
 #include "nsNativeBasicTheme.h"
 
+#include "ScrollbarDrawingMac.h"
+
 class nsNativeBasicThemeCocoa : public nsNativeBasicTheme {
  public:
   nsNativeBasicThemeCocoa() = default;
+
+  using ScrollbarParams = mozilla::widget::ScrollbarParams;
+  using ScrollbarDrawingMac = mozilla::widget::ScrollbarDrawingMac;
+
+  NS_IMETHOD GetMinimumWidgetSize(nsPresContext* aPresContext, nsIFrame* aFrame,
+                                  StyleAppearance aAppearance,
+                                  mozilla::LayoutDeviceIntSize* aResult,
+                                  bool* aIsOverridable) override;
+
+  void PaintScrollbarThumb(DrawTarget* aDrawTarget, const Rect& aRect,
+                           bool aHorizontal, nsIFrame* aFrame,
+                           const ComputedStyle& aStyle,
+                           const EventStates& aElementState,
+                           const EventStates& aDocumentState,
+                           uint32_t aDpiRatio) override;
+  void PaintScrollbarTrack(DrawTarget* aDrawTarget, const Rect& aRect,
+                           bool aHorizontal, nsIFrame* aFrame,
+                           const ComputedStyle& aStyle,
+                           const EventStates& aDocumentState,
+                           uint32_t aDpiRatio, bool aIsRoot) override;
+  void PaintScrollbar(DrawTarget* aDrawTarget, const Rect& aRect,
+                      bool aHorizontal, nsIFrame* aFrame,
+                      const ComputedStyle& aStyle,
+                      const EventStates& aDocumentState, uint32_t aDpiRatio,
+                      bool aIsRoot) override;
+  void PaintScrollCorner(DrawTarget* aDrawTarget, const Rect& aRect,
+                         nsIFrame* aFrame, const ComputedStyle& aStyle,
+                         const EventStates& aDocumentState, uint32_t aDpiRatio,
+                         bool aIsRoot) override;
 
  protected:
   virtual ~nsNativeBasicThemeCocoa() = default;
