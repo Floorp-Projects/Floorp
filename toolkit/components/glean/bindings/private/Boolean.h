0d7ce6214cd7be941e9daa52f616701066ff2765
brizental <beatriz.rizental@gmail.com>
2020-11-02 14:18:06 +0000

    Bug 1673642 - Implement C++ and JS APIs for the Boolean Metric Type. r=chutten
    BONUS: Improve the docs for adding new metric type in C++ and JS,
    added two missing steps to the C++ part and changed the layout
    a bit to make it easier to follow.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95314

---
+++ toolkit/components/glean/bindings/private/Boolean.h
@@ -0,0 +1,90 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_glean_GleanBoolean_h
+#define mozilla_glean_GleanBoolean_h
+
+#include "nsIGleanMetrics.h"
+
+namespace mozilla {
+namespace glean {
+
+namespace impl {
+extern "C" {
+void fog_boolean_set(uint32_t id, bool value);
+uint32_t fog_boolean_test_has_value(uint32_t id, const char* storageName);
+uint32_t fog_boolean_test_get_value(uint32_t id, const char* storageName);
+}
+
+class BooleanMetric {
+ public:
+  constexpr explicit BooleanMetric(uint32_t id) : mId(id) {}
+
+  /**
+   * Set to the specified boolean value.
+   *
+   * @param value the value to set.
+   */
+  void Set(bool value) const { fog_boolean_set(mId, value); }
+
+  /**
+   * **Test-only API**
+   *
+   * Tests whether a value is stored for the metric.
+   *
+   * This function will attempt to await the last parent-process task (if any)
+   * writing to the the metric's storage engine before returning a value.
+   * This function will not wait for data from child processes.
+   *
+   * Parent process only. Panics in child processes.
+   *
+   * @param aStorageName the name of the ping to retrieve the metric for.
+   * @return true if metric value exists, otherwise false
+   */
+  bool TestHasValue(const char* aStorageName) const {
+    return fog_boolean_test_has_value(mId, aStorageName) != 0;
+  }
+
+  /**
+   * **Test-only API**
+   *
+   * Gets the currently stored value as a boolean.
+   *
+   * This function will attempt to await the last parent-process task (if any)
+   * writing to the the metric's storage engine before returning a value.
+   * This function will not wait for data from child processes.
+   *
+   * This doesn't clear the stored value.
+   * Parent process only. Panics in child processes.
+   *
+   * @return value of the stored metric.
+   */
+  bool TestGetValue(const char* aStorageName) const {
+    return fog_boolean_test_get_value(mId, aStorageName);
+  }
+
+ private:
+  const uint32_t mId;
+};
+}  // namespace impl
+
+class GleanBoolean final : public nsIGleanBoolean {
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIGLEANBOOLEAN
+
+  explicit GleanBoolean(uint32_t id) : mBoolean(id){};
+
+ private:
+  virtual ~GleanBoolean() = default;
+
+  const impl::BooleanMetric mBoolean;
+};
+
+}  // namespace glean
+}  // namespace mozilla
+
+#endif /* mozilla_glean_GleanBoolean.h */
