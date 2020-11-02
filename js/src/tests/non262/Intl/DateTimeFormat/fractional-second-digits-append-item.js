73519c089eb2dd70b815d9b716df2bf8b604b109
André Bargull <andre.bargull@gmail.com>
2020-11-02 15:39:32 +0000

    Bug 1674560: Skip test for "dayPeriod" when not on Nightly. r=yulia
    The "fractionalSecondDigits" option for `Intl.DateTimeFormat` is now shipping,
    but the "dayPeriod" option isn't yet. Skip the test which uses "dayPeriod" when
    not running on Nightly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95520

--- js/src/tests/non262/Intl/DateTimeFormat/fractional-second-digits-append-item.js
+++ js/src/tests/non262/Intl/DateTimeFormat/fractional-second-digits-append-item.js
@@ -1,5 +1,11 @@
 // |reftest| skip-if(!this.hasOwnProperty("Intl"))
 
+if (typeof getBuildConfiguration === "undefined") {
+  var getBuildConfiguration = SpecialPowers.Cu.getJSTestingFunctions().getBuildConfiguration;
+}
+
+var isNightly = !getBuildConfiguration().release_or_beta;
+
 const {
   DayPeriod, Hour, Minute, Second, FractionalSecond, Literal
 } = DateTimeFormatParts
@@ -35,7 +41,11 @@ const tests = [
       Literal(")")
     ]
   },
-  {
+];
+
+if (isNightly) {
+  // https://unicode-org.atlassian.net/browse/ICU-20992
+  tests.push({
     locale: "ckb-IR",
     date: new Date("2020-01-01T00:00:00.123"),
     options: {dayPeriod: "short", fractionalSecondDigits: 3},
@@ -45,8 +55,8 @@ const tests = [
       DayPeriod("ب.ن"),
       Literal(")")
     ]
-  },
-];
+  });
+}
 
 for (let {locale, date, options, parts} of tests) {
   let dtf = new Intl.DateTimeFormat(locale, options);
