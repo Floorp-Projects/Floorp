49855a4e4044afec1bc26fe0888ba756b9c68b6b
Florian Quèze <florian@queze.net>
2020-11-02 19:45:22 +0000

    Bug 1674234 - Add a profiler marker for creating the skeleton UI, r=gerald.
    Differential Revision: https://phabricator.services.mozilla.com/D95219

--- mozglue/misc/PreXULSkeletonUI.cpp
+++ mozglue/misc/PreXULSkeletonUI.cpp
@@ -13,6 +13,7 @@
 #include "mozilla/Assertions.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/glue/Debug.h"
+#include "mozilla/BaseProfilerMarkers.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Vector.h"
 #include "mozilla/WindowsDpiAwareness.h"
@@ -720,6 +721,10 @@ bool LoadGdi32AndUser32Procedures() {
 }
 
 void CreateAndStorePreXULSkeletonUI(HINSTANCE hInstance) {
+#ifdef MOZ_GECKO_PROFILER
+  const TimeStamp skeletonStart = TimeStamp::NowUnfuzzed();
+#endif
+
   HKEY regKey;
   if (!IsWin10OrLater() || !OpenPreXULSkeletonUIRegKey(regKey)) {
     return;
@@ -886,6 +891,10 @@ void CreateAndStorePreXULSkeletonUI(HINSTANCE hInstance) {
     sPreXULSKeletonUIAnimationThread = ::CreateThread(
         nullptr, 256 * 1024, AnimateSkeletonUI, nullptr, 0, nullptr);
   }
+
+  BASE_PROFILER_MARKER_UNTYPED(
+      "CreatePreXULSkeletonUI", OTHER,
+      MarkerTiming::IntervalUntilNowFrom(skeletonStart));
 }
 
 bool WasPreXULSkeletonUIMaximized() { return sMaximized; }
