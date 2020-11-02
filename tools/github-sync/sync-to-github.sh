d08ade5902896e2035bb469451d5b7d8137d8829
Kartikaya Gupta <kgupta@mozilla.com>
2020-11-02 16:14:59 +0000

    Bug 1674468 - Remove a couple of pending TODOs. r=kvark
    We can just use the author of the commit as the committer. I don't know why
    I used anything different in the first place, but I'd like to remove references
    to the graphics-team email address so this seems like a good time to fix it.
    
    Also remove another TODO that I'm probably never going to do.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95394

--- tools/github-sync/sync-to-github.sh
+++ tools/github-sync/sync-to-github.sh
@@ -4,8 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-# TODO(kats): consider moving this to Python
-
 # Do NOT set -x here, since that will expose a secret API token!
 set -o errexit
 set -o nounset
