55983564e66b64408846af22e62f7705d536cc23
brizental <beatriz.rizental@gmail.com>
2020-11-02 14:18:06 +0000

    Bug 1673642 - Implement C++ and JS APIs for the Boolean Metric Type. r=chutten
    BONUS: Improve the docs for adding new metric type in C++ and JS,
    added two missing steps to the C++ part and changed the layout
    a bit to make it easier to follow.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95314

--- toolkit/components/glean/api/src/ffi/mod.rs
+++ toolkit/components/glean/api/src/ffi/mod.rs
@@ -17,3 +17,9 @@ define_metric_ffi!(TIMESPAN_MAP {
     start -> fog_timespan_start(),
     stop -> fog_timespan_stop(),
 });
+
+define_metric_ffi!(BOOLEAN_MAP {
+    test_has -> fog_boolean_test_has_value,
+    test_get -> fog_boolean_test_get_value: bool,
+    set -> fog_boolean_set(value: bool),
+});
