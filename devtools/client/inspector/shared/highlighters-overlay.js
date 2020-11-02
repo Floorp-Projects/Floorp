b61c9cda437a00d3817f4568ea49c7d5d9a88e30
Julian Descottes <jdescottes@mozilla.com>
2020-11-02 12:35:20 +0000

    Bug 1654754 - [devtools] Remove actorHasMethod usage from grid inspector and highlighters-overlay r=rcaliman
    The backward compatibility code was introduced in FF69 and can be removed.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95310

--- devtools/client/inspector/shared/highlighters-overlay.js
+++ devtools/client/inspector/shared/highlighters-overlay.js
@@ -454,17 +454,6 @@ class HighlightersOverlay {
     this.emit("highlighter-hidden", { type, ...data });
   }
 
-  async canGetParentGridNode() {
-    if (this._canGetParentGridNode === undefined) {
-      this._canGetParentGridNode = await this.target.actorHasMethod(
-        "domwalker",
-        "getParentGridNode"
-      );
-    }
-
-    return this._canGetParentGridNode;
-  }
-
   /**
    * Returns true if the grid highlighter can be toggled on/off for the given node, and
    * false otherwise. A grid container can be toggled on if the max grid highlighters
@@ -863,7 +852,7 @@ class HighlightersOverlay {
       await this.hideGridHighlighter(node);
     }
 
-    if (node.displayType === "subgrid" && (await this.canGetParentGridNode())) {
+    if (node.displayType === "subgrid") {
       // Show a translucent highlight of the parent grid container if the given node is
       // a subgrid and the parent grid container is not highlighted.
       const parentGridNode = await this.walker.getParentGridNode(node);
@@ -1800,7 +1789,6 @@ class HighlightersOverlay {
 
     this.subgridToParentMap.clear();
 
-    this._canGetParentGridNode = null;
     this._lastHovered = null;
 
     this.inspector = null;
