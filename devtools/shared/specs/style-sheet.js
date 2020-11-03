2b8a02f1a2282a4ac29aa742b96318e8af7c1dda
Julian Descottes <jdescottes@mozilla.com>
2020-11-03 15:56:12 +0000

    Bug 1674405 - [devtools] Extract StyleSheet, StyleSheets and MediaRule specs to individual files r=daisuke,devtools-backward-compat-reviewers
    Depends on D95328
    
    Differential Revision: https://phabricator.services.mozilla.com/D95329

--- devtools/shared/specs/style-sheet.js
+++ devtools/shared/specs/style-sheet.js
@@ -10,18 +10,8 @@ const {
   types,
 } = require("devtools/shared/protocol");
 
-const mediaRuleSpec = generateActorSpec({
-  typeName: "mediarule",
-
-  events: {
-    "matches-change": {
-      type: "matchesChange",
-      matches: Arg(0, "boolean"),
-    },
-  },
-});
-
-exports.mediaRuleSpec = mediaRuleSpec;
+// Load the "mediarule" type used in this file.
+require("devtools/shared/specs/media-rule");
 
 types.addActorType("stylesheet");
 
@@ -73,52 +63,3 @@ const styleSheetSpec = generateActorSpec({
 });
 
 exports.styleSheetSpec = styleSheetSpec;
-
-const styleSheetsSpec = generateActorSpec({
-  typeName: "stylesheets",
-
-  events: {
-    "stylesheet-added": {
-      type: "stylesheetAdded",
-      sheet: Arg(0, "stylesheet"),
-      isNew: Arg(1, "boolean"),
-      fileName: Arg(2, "nullable:string"),
-    },
-  },
-
-  methods: {
-    getTraits: {
-      request: {},
-      response: { traits: RetVal("json") },
-    },
-    getStyleSheets: {
-      request: {},
-      response: { styleSheets: RetVal("array:stylesheet") },
-    },
-    addStyleSheet: {
-      request: {
-        text: Arg(0, "string"),
-        fileName: Arg(1, "nullable:string"),
-      },
-      response: { styleSheet: RetVal("nullable:stylesheet") },
-    },
-    toggleDisabled: {
-      request: { resourceId: Arg(0, "string") },
-      response: { disabled: RetVal("boolean") },
-    },
-    getText: {
-      request: { resourceId: Arg(0, "string") },
-      response: { text: RetVal("longstring") },
-    },
-    update: {
-      request: {
-        resourceId: Arg(0, "string"),
-        text: Arg(1, "string"),
-        transition: Arg(2, "boolean"),
-      },
-      response: {},
-    },
-  },
-});
-
-exports.styleSheetsSpec = styleSheetsSpec;
