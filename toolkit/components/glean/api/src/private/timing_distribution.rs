ac2aff10ace51856ca9d5d94bcd60be0c67b3618
Jan-Erik Rediger <jrediger@mozilla.com>
2020-11-03 15:41:21 +0000

    Bug 1672706 - Store the metric ID in a timing distribution metric. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D95507

--- toolkit/components/glean/api/src/private/timing_distribution.rs
+++ toolkit/components/glean/api/src/private/timing_distribution.rs
@@ -8,10 +8,10 @@ use std::sync::{
     Arc, RwLock,
 };
 
-use super::{CommonMetricData, Instant, TimeUnit};
+use super::{CommonMetricData, Instant, MetricId, TimeUnit};
 
 use crate::dispatcher;
-use crate::ipc::{need_ipc, with_ipc_payload, MetricId};
+use crate::ipc::{need_ipc, with_ipc_payload};
 
 /// Identifier for a running timer.
 ///
@@ -25,7 +25,13 @@ pub struct TimerId(glean_core::metrics::TimerId);
 /// Timing distributions are used to accumulate and store time measurements for analyzing distributions of the timing data.
 #[derive(Debug)]
 pub enum TimingDistributionMetric {
-    Parent(Arc<TimingDistributionMetricImpl>),
+    Parent {
+        /// The metric's ID.
+        ///
+        /// **TEST-ONLY** - Do not use unless gated with `#[cfg(test)]`.
+        id: MetricId,
+        inner: Arc<TimingDistributionMetricImpl>,
+    },
     Child(TimingDistributionMetricIpc),
 }
 #[derive(Debug)]
@@ -41,17 +47,32 @@ pub struct TimingDistributionMetricIpc {
 
 impl TimingDistributionMetric {
     /// Create a new timing distribution metric.
-    pub fn new(meta: CommonMetricData, time_unit: TimeUnit) -> Self {
+    pub fn new(id: MetricId, meta: CommonMetricData, time_unit: TimeUnit) -> Self {
         if need_ipc() {
             TimingDistributionMetric::Child(TimingDistributionMetricIpc {
-                metric_id: MetricId::new(meta),
+                metric_id: id,
                 next_timer_id: AtomicU64::new(0),
                 instants: RwLock::new(HashMap::new()),
             })
         } else {
-            TimingDistributionMetric::Parent(Arc::new(TimingDistributionMetricImpl::new(
-                meta, time_unit,
-            )))
+            let inner = Arc::new(TimingDistributionMetricImpl::new(meta, time_unit));
+            TimingDistributionMetric::Parent { id, inner }
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) fn child_metric(&self) -> Self {
+        match self {
+            TimingDistributionMetric::Parent { id, .. } => {
+                TimingDistributionMetric::Child(TimingDistributionMetricIpc {
+                    metric_id: *id,
+                    next_timer_id: AtomicU64::new(0),
+                    instants: RwLock::new(HashMap::new()),
+                })
+            }
+            TimingDistributionMetric::Child(_) => {
+                panic!("Can't get a child metric from a child metric")
+            }
         }
     }
 
@@ -62,12 +83,12 @@ impl TimingDistributionMetric {
     /// Returns a new `TimerId` identifying the timer.
     pub fn start(&self) -> TimerId {
         match self {
-            TimingDistributionMetric::Parent(p) => {
+            TimingDistributionMetric::Parent { inner, .. } => {
                 // Since this is a synchronous return, we can't use the
                 // Dispatcher, and we can't rely on the Global Glean to provide
                 // mutual exclusion. Use the RwLock to guard the inner metric,
                 // and synchronously create a new TimerId.
-                p.start()
+                inner.start()
             }
             TimingDistributionMetric::Child(c) => {
                 // There is no glean-core on this process to give us a TimerId,
@@ -96,8 +117,8 @@ impl TimingDistributionMetric {
     ///          with different ids to the same timespan metric.
     pub fn stop_and_accumulate(&self, id: TimerId) {
         match self {
-            TimingDistributionMetric::Parent(p) => {
-                let metric = Arc::clone(&p);
+            TimingDistributionMetric::Parent { inner, .. } => {
+                let metric = Arc::clone(&inner);
                 dispatcher::launch(move || metric.stop_and_accumulate(id));
             }
             TimingDistributionMetric::Child(c) => {
@@ -111,9 +132,7 @@ impl TimingDistributionMetric {
                         if let Some(v) = payload.timing_samples.get_mut(&c.metric_id) {
                             v.push(sample);
                         } else {
-                            payload
-                                .timing_samples
-                                .insert(c.metric_id.clone(), vec![sample]);
+                            payload.timing_samples.insert(c.metric_id, vec![sample]);
                         }
                     });
                 } else {
@@ -132,8 +151,8 @@ impl TimingDistributionMetric {
     ///          same timing distribution metric.
     pub fn cancel(&self, id: TimerId) {
         match self {
-            TimingDistributionMetric::Parent(p) => {
-                let metric = Arc::clone(&p);
+            TimingDistributionMetric::Parent { inner, .. } => {
+                let metric = Arc::clone(&inner);
                 dispatcher::launch(move || metric.cancel(id));
             }
             TimingDistributionMetric::Child(c) => {
@@ -169,9 +188,9 @@ impl TimingDistributionMetric {
     /// Returns the stored value or `None` if nothing stored.
     pub fn test_get_value(&self, storage_name: &str) -> Option<String> {
         match self {
-            TimingDistributionMetric::Parent(p) => {
+            TimingDistributionMetric::Parent { inner, .. } => {
                 dispatcher::block_on_queue();
-                p.test_get_value(storage_name)
+                inner.test_get_value(storage_name)
             }
             TimingDistributionMetric::Child(_c) => panic!(
                 "Cannot get test value for {:?} in non-parent process!",
@@ -264,5 +283,57 @@ impl TimingDistributionMetricImpl {
                 .expect("lock of wrapped metric was poisoned");
             inner.test_get_value_as_json_string(glean, storage_name)
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{common_test::*, ipc, metrics};
+
+    #[test]
+    fn smoke_test_timing_distribution() {
+        let _lock = lock_test();
+
+        let metric = &metrics::test_only_ipc::a_timing_dist;
+
+        let id = metric.start();
+        // Stopping right away might not give us data, if the underlying clock source is not precise
+        // enough.
+        // So let's cancel and make sure nothing blows up.
+        metric.cancel(id);
+
+        // We can't inspect the values yet.
+        assert_eq!(None, metric.test_get_value("store1"));
+    }
+
+    #[test]
+    fn timing_distribution_child() {
+        let _lock = lock_test();
+
+        let parent_metric = &metrics::test_only_ipc::a_timing_dist;
+        let id = parent_metric.start();
+        std::thread::sleep(std::time::Duration::from_millis(10));
+        parent_metric.stop_and_accumulate(id);
+
+        {
+            let child_metric = parent_metric.child_metric();
+
+            // scope for need_ipc RAII
+            let _raii = ipc::test_set_need_ipc(true);
+
+            let id = child_metric.start();
+            let id2 = child_metric.start();
+            assert_ne!(id, id2);
+            std::thread::sleep(std::time::Duration::from_millis(10));
+            child_metric.stop_and_accumulate(id);
+
+            child_metric.cancel(id2);
+        }
+
+        // TODO: implement replay. See bug 1646165.
+        // For now let's ensure there's something in the buffer and replay doesn't error.
+        let buf = ipc::take_buf().unwrap();
+        assert!(buf.len() > 0);
+        assert!(ipc::replay_from_buf(&buf).is_ok());
     }
 }
