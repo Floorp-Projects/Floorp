3fdac8a22a972bf6f10f03e43f28c922cf9daa8a
Jan-Erik Rediger <jrediger@mozilla.com>
2020-11-03 10:37:33 +0000

    Bug 1672706 - Take the metric ID for a boolean metric. r=chutten
    This moves the tests to allow for test-only functionality.
    This will allow to more easily test IPC behavior in pure Rust.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95009

--- toolkit/components/glean/api/src/private/boolean.rs
+++ toolkit/components/glean/api/src/private/boolean.rs
@@ -8,6 +8,7 @@ use super::CommonMetricData;
 
 use crate::dispatcher;
 use crate::ipc::need_ipc;
+use crate::private::MetricId;
 
 /// A boolean metric.
 ///
@@ -20,15 +21,23 @@ pub enum BooleanMetric {
 #[derive(Clone, Debug)]
 pub struct BooleanMetricImpl(pub(crate) glean_core::metrics::BooleanMetric);
 #[derive(Debug)]
-pub struct BooleanMetricIpc();
+pub struct BooleanMetricIpc;
 
 impl BooleanMetric {
     /// Create a new boolean metric.
-    pub fn new(meta: CommonMetricData) -> Self {
+    pub fn new(_id: MetricId, meta: CommonMetricData) -> Self {
         if need_ipc() {
-            BooleanMetric::Child(BooleanMetricIpc {})
+            BooleanMetric::Child(BooleanMetricIpc)
         } else {
             BooleanMetric::Parent(Arc::new(BooleanMetricImpl::new(meta)))
+        }
+    }
+
+    #[cfg(test)]
+    pub(crate) fn child_metric(&self) -> Self {
+        match self {
+            BooleanMetric::Parent(_) => BooleanMetric::Child(BooleanMetricIpc),
+            BooleanMetric::Child(_) => panic!("Can't get a child metric from a child metric"),
         }
     }
 
@@ -90,5 +99,56 @@ impl BooleanMetricImpl {
 
     pub fn test_get_value(&self, storage_name: &str) -> Option<bool> {
         crate::with_glean(move |glean| self.0.test_get_value(glean, storage_name))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{common_test::*, ipc, metrics};
+
+    #[test]
+    fn sets_boolean_value() {
+        let _lock = lock_test();
+
+        let metric = &metrics::test_only_ipc::a_bool;
+        metric.set(true);
+
+        assert!(metric.test_get_value("store1").unwrap());
+    }
+
+    #[test]
+    fn boolean_ipc() {
+        // BooleanMetric doesn't support IPC.
+        let _lock = lock_test();
+
+        let parent_metric = &metrics::test_only_ipc::a_bool;
+
+        parent_metric.set(false);
+
+        {
+            let child_metric = parent_metric.child_metric();
+
+            // scope for need_ipc RAII
+            let _raii = ipc::test_set_need_ipc(true);
+
+            // Instrumentation calls do not panic.
+            child_metric.set(true);
+
+            // (They also shouldn't do anything,
+            // but that's not something we can inspect in this test)
+
+            // Need to catch the panic so that our RAIIs drop nicely.
+            let result = std::panic::catch_unwind(move || {
+                child_metric.test_get_value("store1");
+            });
+            assert!(result.is_err());
+        }
+
+        assert!(ipc::replay_from_buf(&ipc::take_buf().unwrap()).is_ok());
+
+        assert!(
+            false == parent_metric.test_get_value("store1").unwrap(),
+            "Boolean metrics should only work in the parent process"
+        );
     }
 }
