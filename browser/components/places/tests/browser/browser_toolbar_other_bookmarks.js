613998bc92467c20b940894fb584e4a960f7ff95
Dorel Luca <dluca@mozilla.com>
2020-11-05 00:09:47 +0200

    Backed out 2 changesets (bug 1674801, bug 1674756) for Browse-chrome failures in places/tests/browser/browser_toolbar_other_bookmarks.js. CLOSED TREE
    Backed out changeset d2697818f40d (bug 1674801)
    Backed out changeset cbb47b6c9dc3 (bug 1674756)

--- browser/components/places/tests/browser/browser_toolbar_other_bookmarks.js
+++ browser/components/places/tests/browser/browser_toolbar_other_bookmarks.js
@@ -113,49 +113,16 @@ add_task(async function testOtherBookmarksMenuPopup() {
   testIsOtherBookmarksHidden(false);
 
   info("Check the popup menu has correct number of children.");
-  await openMenuPopup("#OtherBookmarksPopup", "#OtherBookmarks");
-  testNumberOfMenuPopupChildren("#OtherBookmarksPopup", 3);
-  await closeMenuPopup("#OtherBookmarksPopup");
+  await openMenuPopup();
+  testNumberOfMenuPopupChildren(3);
+  await closeMenuPopup();
 
   info("Remove a bookmark.");
   await PlacesUtils.bookmarks.remove(bookmarks[0]);
 
-  await openMenuPopup("#OtherBookmarksPopup", "#OtherBookmarks");
-  testNumberOfMenuPopupChildren("#OtherBookmarksPopup", 2);
-  await closeMenuPopup("#OtherBookmarksPopup");
-});
-
-// Test that folders in the Other Bookmarks folder expand
-add_task(async function testFolderPopup() {
-  await SpecialPowers.pushPrefEnv({
-    set: [[BOOKMARKS_H2_2020_PREF, true]],
-  });
-
-  await PlacesUtils.bookmarks.insertTree({
-    guid: PlacesUtils.bookmarks.unfiledGuid,
-    children: [
-      {
-        title: "folder",
-        type: PlacesUtils.bookmarks.TYPE_FOLDER,
-        children: [
-          {
-            title: "example",
-            url: "http://example.com/3",
-          },
-        ],
-      },
-    ],
-  });
-
-  info("Check for popup showing event when folder menuitem is selected.");
-  await openMenuPopup("#OtherBookmarksPopup", "#OtherBookmarks");
-  await openMenuPopup(
-    "#OtherBookmarksPopup menu menupopup",
-    "#OtherBookmarksPopup menu"
-  );
-  ok(true, "Folder menu stored in Other Bookmarks expands.");
-  testNumberOfMenuPopupChildren("#OtherBookmarksPopup menu menupopup", 1);
-  await closeMenuPopup("#OtherBookmarksPopup");
+  await openMenuPopup();
+  testNumberOfMenuPopupChildren(2);
+  await closeMenuPopup();
 });
 
 add_task(async function testOnlyShowOtherFolderInBookmarksToolbar() {
@@ -169,46 +136,6 @@ add_task(async function testOnlyShowOtherFolderInBookmarksToolbar() {
 
   CustomizableUI.reset();
   testIsOtherBookmarksHidden(false);
-});
-
-// Test that the menu popup updates when menu items are deleted from it while
-// it's open.
-add_task(async function testDeletingMenuItems() {
-  await PlacesUtils.bookmarks.eraseEverything();
-
-  await SpecialPowers.pushPrefEnv({
-    set: [[BOOKMARKS_H2_2020_PREF, true]],
-  });
-
-  await PlacesUtils.bookmarks.insertTree({
-    guid: PlacesUtils.bookmarks.unfiledGuid,
-    children: bookmarksInfo,
-  });
-
-  await openMenuPopup("#OtherBookmarksPopup", "#OtherBookmarks");
-  testNumberOfMenuPopupChildren("#OtherBookmarksPopup", 3);
-
-  info("Open context menu for popup.");
-  let placesContext = document.getElementById("placesContext");
-  let popupEventPromise = BrowserTestUtils.waitForPopupEvent(
-    placesContext,
-    "shown"
-  );
-  let menuitem = document.querySelector("#OtherBookmarksPopup menuitem");
-  EventUtils.synthesizeMouseAtCenter(menuitem, { type: "contextmenu" });
-  await popupEventPromise;
-
-  info("Delete bookmark menu item from popup.");
-  let deleteMenuItem = document.getElementById("placesContext_delete");
-  EventUtils.synthesizeMouseAtCenter(deleteMenuItem, {});
-
-  await TestUtils.waitForCondition(() => {
-    let popup = document.querySelector("#OtherBookmarksPopup");
-    let items = popup.querySelectorAll("menuitem");
-    return items.length === 2;
-  }, "Failed to delete bookmark menuitem. Expected 2 menu items after deletion.");
-  ok(true, "Menu item was removed from the popup.");
-  await closeMenuPopup("#OtherBookmarksPopup");
 });
 
 /**
@@ -225,34 +152,20 @@ async function testIsOtherBookmarksHidden(expected) {
 
 /**
  * Tests number of menu items in Other Bookmarks popup.
- *
- * @param {String}  selector
- *        The selector for getting the menupopup element we want to test.
- * @param {Number}  expected
- *        The expected number of menuitem elements inside the menupopup.
  */
-function testNumberOfMenuPopupChildren(selector, expected) {
-  let popup = document.querySelector(selector);
+function testNumberOfMenuPopupChildren(expected) {
+  let popup = document.getElementById("OtherBookmarksPopup");
   let items = popup.querySelectorAll("menuitem");
 
-  is(
-    items.length,
-    expected,
-    `Number of menu items for ${selector} should be ${expected}.`
-  );
+  is(items.length, expected, `Number of menu items should be ${expected}.`);
 }
 
 /**
- * Helper for opening a menu popup.
- *
- * @param {String}  popupSelector
- *        The selector for the menupopup element we want to open.
- * @param {String}  targetSelector
- *        The selector for the element with the popup showing event.
+ * Helper for opening the menupopup
  */
-async function openMenuPopup(popupSelector, targetSelector) {
-  let popup = document.querySelector(popupSelector);
-  let target = document.querySelector(targetSelector);
+async function openMenuPopup() {
+  let popup = document.getElementById("OtherBookmarksPopup");
+  let target = document.getElementById("OtherBookmarks");
 
   EventUtils.synthesizeMouseAtCenter(target, {});
 
@@ -260,13 +173,10 @@ async function openMenuPopup(popupSelector, targetSelector) {
 }
 
 /**
- * Helper for closing a menu popup.
- *
- * @param {String}  popupSelector
- *        The selector for the menupopup element we want to close.
+ * Helper for closing the context menu.
  */
-async function closeMenuPopup(popupSelector) {
-  let popup = document.querySelector(popupSelector);
+async function closeMenuPopup() {
+  let popup = document.getElementById("OtherBookmarksPopup");
 
   info("Closing menu popup.");
   popup.hidePopup();
