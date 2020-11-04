447f0f3ba371ea629f4c88a4dfca70f40ad79a15
Razvan Maries <rmaries@mozilla.com>
2020-11-04 03:15:17 +0200

    Backed out 2 changesets (bug 1672694) for Lint failures on browser_text_selection.js and perma failures on browser_accessibility_node.js. CLOSED TREE
    Backed out changeset 0f340ea56805 (bug 1672694)
    Backed out changeset b222e4ff438f (bug 1672694)

--- accessible/generic/HyperTextAccessible.cpp
+++ accessible/generic/HyperTextAccessible.cpp
@@ -192,12 +192,7 @@ uint64_t HyperTextAccessible::NativeState() const {
     states |= states::READONLY;
   }
 
-  nsIFrame* frame = GetFrame();
-  if ((states & states::EDITABLE) || (frame && frame->IsSelectable(nullptr))) {
-    // If the accessible is editable the layout selectable state only disables
-    // mouse selection, but keyboard (shift+arrow) selection is still possible.
-    states |= states::SELECTABLE_TEXT;
-  }
+  if (HasChildren()) states |= states::SELECTABLE_TEXT;
 
   return states;
 }
