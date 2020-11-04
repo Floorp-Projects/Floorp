135f018f9d5774ac8df2bccb6df59928729d463b
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-11-04 03:16:15 +0000

    Bug 1663000 - Add a bunch of tests including non-scrollable contents but covered by the dynamic toolbar. r=botond,geckoview-reviewers,snorp
    - The test cases cover with/without event handler, and with preventDefault in
      the event handler
    - The non-scrollable contents but covered by the dynamic toolbar cases don't
      pass without the event handler now (the cases with the event handler have been
      fixed by the previous change)
    - There is a test case which don't pass at all right now, that is a case
      (iframe_98vh_scrollable.html) that after scrolled at the bottom edge of an
      iframe
    
    Differential Revision: https://phabricator.services.mozilla.com/D93916

--- mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/BaseSessionTest.kt
+++ mobile/android/geckoview/src/androidTest/java/org/mozilla/geckoview/test/BaseSessionTest.kt
@@ -83,6 +83,13 @@ open class BaseSessionTest(noErrorCollector: Boolean = false) {
         const val MEDIA_SESSION_DEFAULT1_PATH = "/assets/www/media_session_default1.html"
         const val TOUCH_HTML_PATH = "/assets/www/touch.html"
         const val GETUSERMEDIA_XORIGIN_CONTAINER_HTML_PATH = "/assets/www/getusermedia_xorigin_container.html"
+        const val ROOT_100_PERCENT_HEIGHT_HTML_PATH = "/assets/www/root_100_percent_height.html"
+        const val ROOT_98VH_HTML_PATH = "/assets/www/root_98vh.html"
+        const val ROOT_100VH_HTML_PATH = "/assets/www/root_100vh.html"
+        const val IFRAME_100_PERCENT_HEIGHT_NO_SCROLLABLE_HTML_PATH = "/assets/www/iframe_100_percent_height_no_scrollable.html"
+        const val IFRAME_100_PERCENT_HEIGHT_SCROLLABLE_HTML_PATH = "/assets/www/iframe_100_percent_height_scrollable.html"
+        const val IFRAME_98VH_SCROLLABLE_HTML_PATH = "/assets/www/iframe_98vh_scrollable.html"
+        const val IFRAME_98VH_NO_SCROLLABLE_HTML_PATH = "/assets/www/iframe_98vh_no_scrollable.html"
 
         const val TEST_ENDPOINT = GeckoSessionTestRule.TEST_ENDPOINT
     }
