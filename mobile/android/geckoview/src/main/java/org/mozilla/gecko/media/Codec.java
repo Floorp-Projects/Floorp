1f2e27b7a67e5ea817e14e28b5ccdadd046e61ce
John Lin <jolin@mozilla.com>
2020-11-04 21:57:36 +0000

    Bug 1640416 - p1: input size check before creating video encoder. r=alwu,geckoview-reviewers,snorp
    On Android, software H.264 video encoder accepts some sizes when configuring
    but will crash later after frames are sent to the encoder. Use method provided
    in API 21+ to validate the input size and avoid using software encoder on
    earlier versions to prevent crashes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94461

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/Codec.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/media/Codec.java
@@ -6,9 +6,11 @@ package org.mozilla.gecko.media;
 
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
+import android.media.MediaCodecInfo.VideoCapabilities;
 import android.media.MediaCodecList;
 import android.media.MediaCrypto;
 import android.media.MediaFormat;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
@@ -26,6 +28,7 @@ import org.mozilla.gecko.gfx.GeckoSurface;
 /* package */ final class Codec extends ICodec.Stub implements IBinder.DeathRecipient {
     private static final String LOGTAG = "GeckoRemoteCodec";
     private static final boolean DEBUG = false;
+    public static final String SW_CODEC_PREFIX = "OMX.google.";
 
     public enum Error {
         DECODE, FATAL
@@ -414,14 +417,14 @@ import org.mozilla.gecko.gfx.GeckoSurface;
             return false;
         }
 
-        final List<String> found = findMatchingCodecNames(mime, flags == MediaCodec.CONFIGURE_FLAG_ENCODE);
+        final List<String> found = findMatchingCodecNames(fmt, flags == MediaCodec.CONFIGURE_FLAG_ENCODE);
         for (final String name : found) {
             final AsyncCodec codec = configureCodec(name, fmt, surface, flags, drmStubId);
             if (codec == null) {
                 Log.w(LOGTAG, "unable to configure " + name + ". Try next.");
                 continue;
             }
-            mIsHardwareAccelerated = !name.startsWith("OMX.google.");
+            mIsHardwareAccelerated = !name.startsWith(SW_CODEC_PREFIX);
             mCodec = codec;
             mInputProcessor = new InputProcessor();
             final boolean renderToSurface = surface != null;
@@ -440,7 +443,13 @@ import org.mozilla.gecko.gfx.GeckoSurface;
         return false;
     }
 
-    private List<String> findMatchingCodecNames(final String mimeType, final boolean isEncoder) {
+    private List<String> findMatchingCodecNames(final MediaFormat format, final boolean isEncoder) {
+        final String mimeType = format.getString(MediaFormat.KEY_MIME);
+        // Missing width and height value in format means audio;
+        // Video format should never has 0 width or height.
+        final int width = format.containsKey(MediaFormat.KEY_WIDTH) ? format.getInteger(MediaFormat.KEY_WIDTH) : 0;
+        final int height = format.containsKey(MediaFormat.KEY_HEIGHT) ? format.getInteger(MediaFormat.KEY_HEIGHT) : 0;
+
         final int numCodecs = MediaCodecList.getCodecCount();
         final List<String> found = new ArrayList<>();
         for (int i = 0; i < numCodecs; i++) {
@@ -451,12 +460,30 @@ import org.mozilla.gecko.gfx.GeckoSurface;
 
             final String[] types = info.getSupportedTypes();
             for (final String t : types) {
-                if (t.equalsIgnoreCase(mimeType)) {
-                    found.add(info.getName());
-                    if (DEBUG) {
-                        Log.d(LOGTAG, "found " + (isEncoder ? "encoder:" : "decoder:") +
-                                info.getName() + " for mime:" + mimeType);
+                if (!t.equalsIgnoreCase(mimeType)) {
+                    continue;
+                }
+                final String name = info.getName();
+                // API 21+ provide a method to query whether supplied size is supported. For
+                // older version, just avoid software video encoders.
+                if (isEncoder && width > 0 && height > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        final VideoCapabilities c = info.getCapabilitiesForType(mimeType).getVideoCapabilities();
+                        if (c != null && !c.isSizeSupported(width, height)) {
+                            if (DEBUG) {
+                                Log.d(LOGTAG, name + ": " + width + "x" + height + " not supported");
+                            }
+                            continue;
+                        }
+                    } else if (name.startsWith(SW_CODEC_PREFIX)) {
+                        continue;
                     }
+                }
+
+                found.add(name);
+                if (DEBUG) {
+                    Log.d(LOGTAG, "found " + (isEncoder ? "encoder:" : "decoder:") +
+                            name + " for mime:" + mimeType);
                 }
             }
         }
