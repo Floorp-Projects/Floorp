1df9ab67416c8d37156076d4428355f5c5e6a5e6
Tooru Fujisawa <arai_a@mac.com>
2020-11-05 15:56:05 +0000

    Bug 1674351 - Part 6: Merge XDRParserAtomTable into ParserAtomVectorBuilder. r=tcampbell
    XDRParserAtomTable and ParserAtomVector has the same content.
    Removed XDRParserAtomTable and rewrote consumers to use ParserAtomVector.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95844

--- js/src/vm/Xdr.cpp
+++ js/src/vm/Xdr.cpp
@@ -316,18 +316,13 @@ static XDRResult ParserAtomTable(XDRState<mode>* xdr) {
   MOZ_TRY(XDRAtomCount(xdr, &atomCount));
   MOZ_ASSERT(!xdr->hasAtomTable());
 
-  if (!xdr->parserAtomTable().reserve(atomCount)) {
-    ReportOutOfMemory(xdr->cx());
+  if (!xdr->frontendAtoms().reserve(xdr->cx(), atomCount)) {
     return xdr->fail(JS::TranscodeResult_Throw);
   }
 
   for (uint32_t i = 0; i < atomCount; i++) {
     const frontend::ParserAtom* atom = nullptr;
     MOZ_TRY(XDRParserAtomData(xdr, &atom));
-    if (!xdr->parserAtomTable().append(atom)) {
-      ReportOutOfMemory(xdr->cx());
-      return xdr->fail(JS::TranscodeResult_Throw);
-    }
   }
   xdr->finishAtomTable();
 
@@ -770,7 +765,6 @@ XDRResult XDRStencilDecoder::codeStencils(
         cx(), compilationInfos.initial.input.options);
     auto& funInfo = compilationInfos.delazifications[i - 1];
 
-    parserAtomTable_.clear();
     hasFinishedAtomTable_ = false;
 
     frontend::ParserAtomVectorBuilder parserAtomBuilder(
