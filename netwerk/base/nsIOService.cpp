92b3faea4e5b9a8e7f467df5b66d2da38a8eb9c4
Frederik Braun <fbraun@mozilla.com>
2020-11-05 00:51:24 +0000

    Bug 1674735 - add port 5060 to bad port list r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D95504

--- netwerk/base/nsIOService.cpp
+++ netwerk/base/nsIOService.cpp
@@ -162,6 +162,8 @@ int16_t gBadPortList[] = {
     2049,  // nfs
     3659,  // apple-sasl
     4045,  // lockd
+    5060,  // sip
+    5061,  // sips
     6000,  // x11
     6665,  // irc (alternate)
     6666,  // irc (alternate)
