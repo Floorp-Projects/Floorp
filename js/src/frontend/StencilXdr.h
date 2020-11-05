50ac0a5a53b5aae06548bbd7fd07863ac4bfa930
Ted Campbell <tcampbell@mozilla.com>
2020-11-05 04:52:57 +0000

    Bug 1668672 - Move XDRParser*ScopeData methods to StencilXDR interface. r=arai
    The StencilXDR interface has access to internals of ScopeStencil so having
    accessing to the explicit methods is helpful. This also uses the stencil as
    the argument instead of a UniquePtr& which makes it easier to change types
    later.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95941

--- js/src/frontend/StencilXdr.h
+++ js/src/frontend/StencilXdr.h
@@ -28,6 +28,20 @@ class StencilXDR {
   static XDRResult Scope(XDRState<mode>* xdr, ScopeStencil& stencil);
 
   template <XDRMode mode>
+  static XDRResult FunctionScopeData(XDRState<mode>* xdr,
+                                     ScopeStencil& stencil);
+  template <XDRMode mode>
+  static XDRResult VarScopeData(XDRState<mode>* xdr, ScopeStencil& stencil);
+  template <XDRMode mode>
+  static XDRResult LexicalScopeData(XDRState<mode>* xdr, ScopeStencil& stencil);
+  template <XDRMode mode>
+  static XDRResult EvalScopeData(XDRState<mode>* xdr, ScopeStencil& stencil);
+  template <XDRMode mode>
+  static XDRResult GlobalScopeData(XDRState<mode>* xdr, ScopeStencil& stencil);
+  template <XDRMode mode>
+  static XDRResult ModuleScopeData(XDRState<mode>* xdr, ScopeStencil& stencil);
+
+  template <XDRMode mode>
   static XDRResult ObjLiteral(XDRState<mode>* xdr, ObjLiteralStencil& stencil);
 
   template <XDRMode mode>
