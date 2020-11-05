bb2f92fa462aa28918245b25b2f70fa19d4cc6e4
Lars T Hansen <lhansen@mozilla.com>
2020-11-05 10:15:17 +0000

    Bug 1674965 - Test harness for code generation tests. r=rhunt
    The commonalities of the x64 code generation test cases are factored
    into lib/codegen-x64-test.js and that file is included by the test
    cases.
    
    The harness allows the expected patterns in the test cases to be much
    smaller.  The harness adds the pattern prefix and suffix during
    preprocessing, adds the address prefix for each line, and makes
    whitespace matching more flexible.
    
    Additionally the harness exports some variables (representing
    subpatterns) that can be used within the expected patterns to express
    common phrases, such as RIP-relative addresses.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95733

--- js/src/jit-test/tests/wasm/simd/neg-abs-not-x64-ion-codegen.js
+++ js/src/jit-test/tests/wasm/simd/neg-abs-not-x64-ion-codegen.js
@@ -1,92 +1,36 @@
-// |jit-test| skip-if: !wasmSimdEnabled() || wasmCompileMode() != "ion" || !getBuildConfiguration().x64
+// |jit-test| skip-if: !wasmSimdEnabled() || !hasDisassembler() || wasmCompileMode() != "ion" || !getBuildConfiguration().x64; include:codegen-x64-test.js
 
-// Test that there are no extraneous moves for variable SIMD negate
-// instructions. See README-codegen.md for general information about this type
-// of test case.
+// Test that there are no extraneous moves for variable SIMD negate, abs, and
+// not instructions. See README-codegen.md for general information about this
+// type of test case.
 
 // Integer negates don't have to reuse the input for the output, and prefer for
-// the registers to be different.
+// the registers to be different.  So use parameter 1 and ignore parameter 0.
 
-// Inputs (xmm1, xmm0)
-
-for ( let [ op, expected ] of [
-    ['i8x16.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef c0               pxor %xmm0, %xmm0
-000000..  66 0f f8 c1               psubb %xmm1, %xmm0
-000000..  5d                        pop %rbp
+codegenTestX64_IGNOREDxv128_v128(
+    [['i8x16.neg', `
+66 0f ef c0               pxor %xmm0, %xmm0
+66 0f f8 c1               psubb %xmm1, %xmm0
 `],
-    ['i16x8.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef c0               pxor %xmm0, %xmm0
-000000..  66 0f f9 c1               psubw %xmm1, %xmm0
-000000..  5d                        pop %rbp
+     ['i16x8.neg', `
+66 0f ef c0               pxor %xmm0, %xmm0
+66 0f f9 c1               psubw %xmm1, %xmm0
 `],
-    ['i32x4.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef c0               pxor %xmm0, %xmm0
-000000..  66 0f fa c1               psubd %xmm1, %xmm0
-000000..  5d                        pop %rbp
+     ['i32x4.neg', `
+66 0f ef c0               pxor %xmm0, %xmm0
+66 0f fa c1               psubd %xmm1, %xmm0
 `],
-    ['i64x2.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef c0               pxor %xmm0, %xmm0
-000000..  66 0f fb c1               psubq %xmm1, %xmm0
-000000..  5d                        pop %rbp
-`],
-
-] ) {
-
-    let ins = wasmEvalText(`
-  (module
-    (func (export "f") (param v128) (param v128) (result v128)
-      (${op} (local.get 1))))
-`);
-    let output = wasmDis(ins.exports.f, "ion", true);
-    if (output.indexOf('No disassembly available') >= 0)
-        continue;
-    assertEq(output.match(new RegExp(expected)) != null, true);
-}
+     ['i64x2.neg', `
+66 0f ef c0               pxor %xmm0, %xmm0
+66 0f fb c1               psubq %xmm1, %xmm0
+`]] );
 
 // Floating point negate and absolute value, and bitwise not, prefer for the
 // registers to be the same and guarantee that no move is inserted if so.
 
-// Inputs (xmm0, xmm1)
-
-for ( let [ op, expected ] of [
-    ['f32x4.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef 05 .. 00 00 00   pxorx 0x00000000000000.., %xmm0
-000000..  5d                        pop %rbp
-`],
-    ['f64x2.neg', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef 05 .. 00 00 00   pxorx 0x00000000000000.., %xmm0
-000000..  5d                        pop %rbp
-`],
-    ['f32x4.abs', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f db 05 .. 00 00 00   pandx 0x00000000000000.., %xmm0
-000000..  5d                        pop %rbp
-`],
-    ['f64x2.abs', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f db 05 .. 00 00 00   pandx 0x00000000000000.., %xmm0
-000000..  5d                        pop %rbp
-`],
-    ['v128.not', `
-000000..  48 8b ec                  mov %rsp, %rbp
-000000..  66 0f ef 05 .. 00 00 00   pxorx 0x00000000000000.., %xmm0
-000000..  5d                        pop %rbp
-`],
-] ) {
-    var ins = wasmEvalText(`
-  (module
-    (func (export "f") (param v128) (result v128)
-      (${op} (local.get 0))))
-`);
-    let output = wasmDis(ins.exports.f, "ion", true);
-    if (output.indexOf('No disassembly available') >= 0)
-        continue;
-    assertEq(output.match(new RegExp(expected)) != null, true);
-}
+codegenTestX64_v128_v128(
+    [['f32x4.neg', `66 0f ef 05 .. 00 00 00   pxorx ${RIPR}, %xmm0`],
+     ['f64x2.neg', `66 0f ef 05 .. 00 00 00   pxorx ${RIPR}, %xmm0`],
+     ['f32x4.abs', `66 0f db 05 .. 00 00 00   pandx ${RIPR}, %xmm0`],
+     ['f64x2.abs', `66 0f db 05 .. 00 00 00   pandx ${RIPR}, %xmm0`],
+     ['v128.not', `66 0f ef 05 .. 00 00 00   pxorx ${RIPR}, %xmm0`]] );
