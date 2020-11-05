4657310b0c952a1c4616e03e73e90cb762f21282
Lee Salzman <lsalzman@mozilla.com>
2020-11-05 03:36:48 +0000

    Bug 1675419 - remove redundant packing in SWGL fast-paths. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D95957

--- gfx/wr/swgl/src/swgl_ext.h
+++ gfx/wr/swgl/src/swgl_ext.h
@@ -2,22 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-static inline void commit_span(uint32_t* buf, PackedRGBA8 r) {
-  if (blend_key)
-    r = pack(blend_pixels_RGBA8(unaligned_load<PackedRGBA8>(buf), unpack(r)));
-  unaligned_store(buf, r);
+static inline void commit_span(uint32_t* buf, WideRGBA8 r) {
+  if (blend_key) r = blend_pixels_RGBA8(unaligned_load<PackedRGBA8>(buf), r);
+  unaligned_store(buf, pack(r));
 }
 
-UNUSED static inline void commit_solid_span(uint32_t* buf, PackedRGBA8 r,
+UNUSED static inline void commit_solid_span(uint32_t* buf, WideRGBA8 r,
                                             int len) {
   if (blend_key) {
-    auto src = unpack(r);
     for (uint32_t* end = &buf[len]; buf < end; buf += 4) {
       unaligned_store(
-          buf, pack(blend_pixels_RGBA8(unaligned_load<PackedRGBA8>(buf), src)));
+          buf, pack(blend_pixels_RGBA8(unaligned_load<PackedRGBA8>(buf), r)));
     }
   } else {
-    fill_n(buf, len, bit_cast<U32>(r).x);
+    fill_n(buf, len, bit_cast<U32>(pack(r)).x);
   }
 }
 
@@ -34,39 +32,35 @@ UNUSED static inline void commit_texture_span(uint32_t* buf, uint32_t* src,
   }
 }
 
-static inline void commit_span(uint8_t* buf, PackedR8 r) {
-  if (blend_key)
-    r = pack(blend_pixels_R8(unpack(unaligned_load<PackedR8>(buf)), unpack(r)));
-  unaligned_store(buf, r);
+static inline void commit_span(uint8_t* buf, WideR8 r) {
+  if (blend_key) r = blend_pixels_R8(unpack(unaligned_load<PackedR8>(buf)), r);
+  unaligned_store(buf, pack(r));
 }
 
-UNUSED static inline void commit_solid_span(uint8_t* buf, PackedR8 r, int len) {
+UNUSED static inline void commit_solid_span(uint8_t* buf, WideR8 r, int len) {
   if (blend_key) {
-    auto src = unpack(r);
     for (uint8_t* end = &buf[len]; buf < end; buf += 4) {
-      unaligned_store(buf, pack(blend_pixels_R8(
-                               unpack(unaligned_load<PackedR8>(buf)), src)));
+      unaligned_store(
+          buf, pack(blend_pixels_R8(unpack(unaligned_load<PackedR8>(buf)), r)));
     }
   } else {
-    fill_n((uint32_t*)buf, len / 4, bit_cast<uint32_t>(r));
+    fill_n((uint32_t*)buf, len / 4, bit_cast<uint32_t>(pack(r)));
   }
 }
 
 template <typename V>
-static inline PackedRGBA8 pack_span(uint32_t*, const V& v) {
-  return pack(pack_pixels_RGBA8(v));
+static inline WideRGBA8 pack_span(uint32_t*, const V& v) {
+  return pack_pixels_RGBA8(v);
 }
 
-static inline PackedRGBA8 pack_span(uint32_t*) {
-  return pack(pack_pixels_RGBA8());
-}
+static inline WideRGBA8 pack_span(uint32_t*) { return pack_pixels_RGBA8(); }
 
 template <typename C>
-static inline PackedR8 pack_span(uint8_t*, C c) {
-  return pack(pack_pixels_R8(c));
+static inline WideR8 pack_span(uint8_t*, C c) {
+  return pack_pixels_R8(c);
 }
 
-static inline PackedR8 pack_span(uint8_t*) { return pack(pack_pixels_R8()); }
+static inline WideR8 pack_span(uint8_t*) { return pack_pixels_R8(); }
 
 // Forces a value with vector run-class to have scalar run-class.
 template <typename T>
@@ -111,9 +105,9 @@ static inline WideRGBA8 pack_pixels_RGBA8(float alpha) {
 }
 
 // Commit a single chunk of a color scaled by an alpha weight
-#define swgl_commitColor(format, color, alpha)                         \
-  swgl_commitChunk(format, pack(muldiv255(pack_pixels_##format(color), \
-                                          pack_pixels_##format(alpha))))
+#define swgl_commitColor(format, color, alpha)                    \
+  swgl_commitChunk(format, muldiv255(pack_pixels_##format(color), \
+                                     pack_pixels_##format(alpha)))
 #define swgl_commitColorRGBA8(color, alpha) \
   swgl_commitColor(RGBA8, color, alpha)
 #define swgl_commitColorR8(color, alpha) swgl_commitColor(R8, color, alpha)
@@ -165,18 +159,17 @@ static ALWAYS_INLINE T swgl_linearQuantizeStep(S s, T p) {
 // Commit a single chunk from a linear texture fetch
 #define swgl_commitTextureLinear(format, s, p, ...) \
   swgl_commitChunk(format,                          \
-                   textureLinearPacked##format(s, ivec2(p), __VA_ARGS__))
+                   textureLinearUnpacked##format(s, ivec2(p), __VA_ARGS__))
 #define swgl_commitTextureLinearRGBA8(s, p, ...) \
   swgl_commitTextureLinear(RGBA8, s, p, __VA_ARGS__)
 #define swgl_commitTextureLinearR8(s, p, ...) \
   swgl_commitTextureLinear(R8, s, p, __VA_ARGS__)
 
 // Commit a single chunk from a linear texture fetch that is scaled by a color
-#define swgl_commitTextureLinearColor(format, s, p, color, ...)               \
-  swgl_commitChunk(                                                           \
-      format,                                                                 \
-      pack(muldiv255(textureLinearUnpacked##format(s, ivec2(p), __VA_ARGS__), \
-                     pack_pixels_##format(color))))
+#define swgl_commitTextureLinearColor(format, s, p, color, ...)     \
+  swgl_commitChunk(format, muldiv255(textureLinearUnpacked##format( \
+                                         s, ivec2(p), __VA_ARGS__), \
+                                     pack_pixels_##format(color)))
 #define swgl_commitTextureLinearColorRGBA8(s, p, color, ...) \
   swgl_commitTextureLinearColor(RGBA8, s, p, color, __VA_ARGS__)
 #define swgl_commitTextureLinearColorR8(s, p, color, ...) \
