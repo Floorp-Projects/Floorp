50873a9666166d654c70ef1f8287b984fd0cf613
Matt Woodrow <mwoodrow@mozilla.com>
2020-11-05 19:26:21 +0000

    Bug 1673983 - Add D3D11 RenderCompositor for SWGL. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D95797

--- gfx/webrender_bindings/RenderCompositor.cpp
+++ gfx/webrender_bindings/RenderCompositor.cpp
@@ -6,7 +6,9 @@
 
 #include "RenderCompositor.h"
 
+#include "gfxConfig.h"
 #include "GLContext.h"
+#include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/gfx/gfxVars.h"
 #include "mozilla/layers/SyncObject.h"
 #include "mozilla/webrender/RenderCompositorOGL.h"
@@ -15,6 +17,7 @@
 
 #ifdef XP_WIN
 #  include "mozilla/webrender/RenderCompositorANGLE.h"
+#  include "mozilla/webrender/RenderCompositorD3D11SWGL.h"
 #endif
 
 #if defined(MOZ_WAYLAND) || defined(MOZ_WIDGET_ANDROID)
@@ -138,9 +141,17 @@ UniquePtr<RenderCompositor> RenderCompositor::Create(
 #ifdef XP_MACOSX
     // Mac uses NativeLayerCA
     return RenderCompositorNativeSWGL::Create(std::move(aWidget), aError);
-#else
-    return RenderCompositorSWGL::Create(std::move(aWidget), aError);
+#elif defined(XP_WIN)
+    if (StaticPrefs::gfx_webrender_software_d3d11_AtStartup() &&
+        gfx::gfxConfig::IsEnabled(gfx::Feature::D3D11_COMPOSITING)) {
+      UniquePtr<RenderCompositor> comp =
+          RenderCompositorD3D11SWGL::Create(std::move(aWidget), aError);
+      if (comp) {
+        return comp;
+      }
+    }
 #endif
+    return RenderCompositorSWGL::Create(std::move(aWidget), aError);
   }
 
 #ifdef XP_WIN
