bbbca3da802166b5f310abca0ba37038c2a7547d
Nika Layzell <nika@thelayzells.com>
2020-11-05 19:25:40 +0000

    Bug 1673711 - Part 1: Don't shutdown processes before launching, r=jesup
    Previously, it was possible to accidentally shut down a content process while it
    was still launching if the process switch which requested the switch was
    cancelled.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95889

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2081,8 +2081,12 @@ bool ContentParent::ShouldKeepProcessAlive() {
     return true;
   }
 
+  if (IsLaunching()) {
+    return true;
+  }
+
   // If we have already been marked as dead, don't prevent shutdown.
-  if (!IsAlive()) {
+  if (IsDead()) {
     return false;
   }
 
