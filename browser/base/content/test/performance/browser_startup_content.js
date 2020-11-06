c5eff6620590dbf2c5eceb62a1c42fe8e71c8f48
Neil Deakin <neil@mozilla.com>
2020-11-06 15:46:11 +0000

    Bug 1649843, remove now unused legacy actor code, r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D95206

--- browser/base/content/test/performance/browser_startup_content.js
+++ browser/base/content/test/performance/browser_startup_content.js
@@ -41,7 +41,6 @@ const known_scripts = {
     "resource:///actors/LinkHandlerChild.jsm",
     "resource:///actors/PageStyleChild.jsm",
     "resource:///actors/SearchTelemetryChild.jsm",
-    "resource://gre/modules/ActorManagerChild.jsm",
     "resource://gre/modules/E10SUtils.jsm",
     "resource://gre/modules/Readerable.jsm",
 
@@ -57,9 +56,6 @@ const known_scripts = {
   frameScripts: new Set([
     // Test related
     "chrome://mochikit/content/shutdown-leaks-collector.js",
-
-    // Browser front-end
-    "chrome://global/content/browser-content.js",
 
     // Extensions
     "resource://gre/modules/addons/Content.js",
