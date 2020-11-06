cf51e7ef269637ec584dd94690666f309e1e39e5
Csoregi Natalia <ncsoregi@mozilla.com>
2020-11-06 20:32:14 +0200

    Backed out 3 changesets (bug 1675266, bug 1671205) for failures on test_capabilities.js. CLOSED TREE
    Backed out changeset a1900289bdac (bug 1671205)
    Backed out changeset 04e39b99f9c4 (bug 1675266)
    Backed out changeset 3fd5c9f2bd29 (bug 1675266)

--- testing/marionette/capabilities.js
+++ testing/marionette/capabilities.js
@@ -457,11 +457,6 @@ class Capabilities extends Map {
       ["moz:accessibilityChecks", false],
       ["moz:buildID", Services.appinfo.appBuildID],
       [
-        "moz:debuggerAddress",
-        Cc["@mozilla.org/remote/agent;1"].createInstance(Ci.nsIRemoteAgent)
-          .debuggerAddress || null,
-      ],
-      [
         "moz:headless",
         Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo).isHeadless,
       ],
@@ -592,11 +587,6 @@ class Capabilities extends Map {
         case "moz:webdriverClick":
           assert.boolean(v, pprint`Expected ${k} to be boolean, got ${v}`);
           break;
-
-        // Don't set the value because it's only used to return the address
-        // of the Remote Agent's debugger (HTTP server).
-        case "moz:debuggerAddress":
-          continue;
       }
 
       matched.set(k, v);
