636700568ebdf57ceff1d932e0028b912782e16a
Kevin Jacobs <kjacobs@mozilla.com>
2020-11-06 00:54:30 +0000

    Bug 1671713 - land NSS 97751cd6d553 UPGRADE_NSS_RELEASE, r=jcj
    2020-11-03  Kevin Jacobs  <kjacobs@mozilla.com>
    
    	* gtests/common/testvectors/hmac-sha256-vectors.h,
    	gtests/common/testvectors/hmac-sha384-vectors.h,
    	gtests/common/testvectors/hmac-sha512-vectors.h,
    	gtests/common/testvectors_base/test-structs.h,
    	gtests/pk11_gtest/manifest.mn, gtests/pk11_gtest/pk11_gtest.gyp,
    	gtests/pk11_gtest/pk11_hmac_unittest.cc:
    	Bug 1672823 - Add Wycheproof HMAC test cases. r=jcj
    
    	[97751cd6d553] [tip]
    
    	* gtests/common/testvectors/hkdf-sha1-vectors.h,
    	gtests/common/testvectors/hkdf-sha256-vectors.h,
    	gtests/common/testvectors/hkdf-sha384-vectors.h,
    	gtests/common/testvectors/hkdf-sha512-vectors.h,
    	gtests/common/testvectors/hkdf-vectors.h,
    	gtests/common/testvectors_base/test-structs.h,
    	gtests/common/wycheproof/genTestVectors.py,
    	gtests/pk11_gtest/manifest.mn,
    	gtests/pk11_gtest/pk11_hkdf_unittest.cc:
    	Bug 1672823 - Add Wycheproof HKDF test cases. r=bbeurdouche
    
    	[5a02ca2617cf]
    
    	* gtests/common/testvectors/dsa-vectors.h,
    	gtests/common/testvectors_base/test-structs.h,
    	gtests/common/wycheproof/genTestVectors.py,
    	gtests/common/wycheproof/source_vectors/dsa_test.json,
    	gtests/pk11_gtest/manifest.mn,
    	gtests/pk11_gtest/pk11_dsa_unittest.cc,
    	gtests/pk11_gtest/pk11_gtest.gyp:
    	Bug 1672823 - Add Wycheproof DSA test cases. r=jcj
    
    	[3ce42ead87f9]
    
    	* lib/dev/devslot.c, lib/dev/devt.h:
    	Bug 1663661 - Guard against NULL token in nssSlot_IsTokenPresent.
    	r=jcj
    
    	This patch addresses locking inconsistency in
    	`nssSlot_IsTokenPresent` by retaining the slot lock for the duration
    	of accesses to `slot->token`. This is already done correctly
    	elsewhere. As a side effect, this introduces an ordering
    	requirement: we take `slot->lock` followed by `session->lock`.
    
    	[0ed11a5835ac]
    
    2020-10-30  Kevin Jacobs  <kjacobs@mozilla.com>
    
    	* lib/pk11wrap/pk11pars.c:
    	Bug 1670835 - Fixup for 6f79a7695812, add missing return value
    	check. r=rrelyea
    
    	[424974716ef0]
    
    Differential Revision: https://phabricator.services.mozilla.com/D96073

--- security/nss/gtests/common/testvectors_base/test-structs.h
+++ security/nss/gtests/common/testvectors_base/test-structs.h
@@ -32,6 +32,7 @@ typedef struct AesCmacTestVectorStr {
   std::string tag;
   bool invalid;
 } AesCmacTestVector;
+typedef AesCmacTestVector HmacTestVector;
 
 typedef struct AesGcmKatValueStr {
   uint32_t id;
@@ -66,6 +67,8 @@ typedef struct EcdsaTestVectorStr {
   bool valid;
 } EcdsaTestVector;
 
+typedef EcdsaTestVector DsaTestVector;
+
 typedef struct EcdhTestVectorStr {
   uint32_t id;
   std::vector<uint8_t> private_key;
@@ -74,6 +77,16 @@ typedef struct EcdhTestVectorStr {
   bool invalid_asn;
   bool valid;
 } EcdhTestVector;
+
+typedef struct HkdfTestVectorStr {
+  uint32_t id;
+  std::string ikm;
+  std::string salt;
+  std::string info;
+  std::string okm;
+  uint32_t size;
+  bool valid;
+} HkdfTestVector;
 
 typedef struct RsaSignatureTestVectorStr {
   SECOidTag hash_oid;
