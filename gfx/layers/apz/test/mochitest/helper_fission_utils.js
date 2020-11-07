6f74d74e792bc1198de6864514f055490aa68ceb
Kartikaya Gupta <kgupta@mozilla.com>
2020-11-07 02:29:26 +0000

    Bug 1675378 - Add a utility to simplify writing fission hit-testing tests. r=botond
    Differential Revision: https://phabricator.services.mozilla.com/D96034

--- gfx/layers/apz/test/mochitest/helper_fission_utils.js
+++ gfx/layers/apz/test/mochitest/helper_fission_utils.js
@@ -40,3 +40,91 @@ function loadOOPIFrame(iframeElementId, iframePage) {
     await loadPromise;
   };
 }
+
+/**
+ * This is similar to the hitTest function in apz_test_utils.js, in that it
+ * does a hit-test for a point and returns the result. The difference is that
+ * in the fission world, the hit-test may land on an OOPIF, which means the
+ * result information will be in the APZ test data for the OOPIF process. This
+ * function checks both the current process and OOPIF process to see which one
+ * got a hit result, and returns the result regardless of which process got it.
+ * The caller is expected to check the layers id which will allow distinguishing
+ * the two cases.
+ */
+async function fissionHitTest(point, iframeElement) {
+  let get_iframe_compositor_test_data = function() {
+    let utils = SpecialPowers.getDOMWindowUtils(window);
+    return JSON.stringify(utils.getCompositorAPZTestData());
+  };
+
+  let utils = SpecialPowers.getDOMWindowUtils(window);
+
+  // Get the test data before doing the actual hit-test, to get a baseline
+  // of what we can ignore.
+  let oldParentTestData = utils.getCompositorAPZTestData();
+  let oldIframeTestData = JSON.parse(
+    await FissionTestHelper.sendToOopif(
+      iframeElement,
+      `(${get_iframe_compositor_test_data})()`
+    )
+  );
+
+  // Now do the hit-test
+  dump(`Hit-testing point (${point.x}, ${point.y}) in fission context\n`);
+  utils.sendMouseEvent(
+    "MozMouseHittest",
+    point.x,
+    point.y,
+    0,
+    0,
+    0,
+    true,
+    0,
+    0,
+    true,
+    true
+  );
+
+  // Collect the new test data
+  let newParentTestData = utils.getCompositorAPZTestData();
+  let newIframeTestData = JSON.parse(
+    await FissionTestHelper.sendToOopif(
+      iframeElement,
+      `(${get_iframe_compositor_test_data})()`
+    )
+  );
+
+  // See which test data has new hit results
+  let hitResultCount = function(testData) {
+    return Object.keys(testData.hitResults).length;
+  };
+
+  let hitIframe =
+    hitResultCount(newIframeTestData) > hitResultCount(oldIframeTestData);
+  let hitParent =
+    hitResultCount(newParentTestData) > hitResultCount(oldParentTestData);
+
+  // Extract the results from the appropriate test data
+  let lastHitResult = function(testData) {
+    let lastHit =
+      testData.hitResults[Object.keys(testData.hitResults).length - 1];
+    return {
+      hitInfo: lastHit.hitResult,
+      scrollId: lastHit.scrollId,
+      layersId: lastHit.layersId,
+    };
+  };
+  if (hitIframe && hitParent) {
+    throw new Error(
+      "Both iframe and parent got hit-results, that is unexpected!"
+    );
+  } else if (hitIframe) {
+    return lastHitResult(newIframeTestData);
+  } else if (hitParent) {
+    return lastHitResult(newParentTestData);
+  } else {
+    throw new Error(
+      "Neither iframe nor parent got the hit-result, that is unexpected!"
+    );
+  }
+}
