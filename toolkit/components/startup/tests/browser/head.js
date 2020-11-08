9af9d0b1237018fc843d3440ba32fd10ec15c2c1
Bogdan Tara <btara@mozilla.com>
2020-11-08 02:14:49 +0200

    Backed out changeset 5ed5e8a6bbfb (bug 1583696) for modal prompt related failures CLOSED TREE

--- toolkit/components/startup/tests/browser/head.js
+++ toolkit/components/startup/tests/browser/head.js
@@ -3,6 +3,8 @@
 
 "use strict";
 
+const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+
 function whenBrowserLoaded(browser, callback) {
   return BrowserTestUtils.browserLoaded(browser).then(callback);
 }
