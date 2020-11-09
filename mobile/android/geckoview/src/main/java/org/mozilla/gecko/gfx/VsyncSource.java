cd02d90ae3ff8d2f9ea085f961eb1a7b28546574
Markus Stange <mstange.moz@gmail.com>
2020-11-09 18:32:13 +0000

    Bug 1675887 - Respect the time value that's passed to Choreographer.FrameCallback.doFrame. r=kats
    This makes the vsync times more consistent.
    On a Pixel 2 running Android 11, the callback was called between 1ms and 6ms
    after the vsync timestamp in my tests. Having the vsync timestamp slightly in
    the past is what the rest of our vsync infrastructure expects.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96279

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/VsyncSource.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/gfx/VsyncSource.java
@@ -42,13 +42,13 @@ import org.mozilla.gecko.GeckoAppShell;
     }
 
     @WrapForJNI(stubName = "NotifyVsync")
-    private static native void nativeNotifyVsync();
+    private static native void nativeNotifyVsync(final long frameTimeNanos);
 
     // Choreographer callback implementation.
     public void doFrame(final long frameTimeNanos) {
         if (mObservingVsync) {
             mChoreographer.postFrameCallback(this);
-            nativeNotifyVsync();
+            nativeNotifyVsync(frameTimeNanos);
         }
     }
 
