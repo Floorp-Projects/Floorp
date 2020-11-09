21c81dd7e6e1fda79eb9bf332bbade4c8b8cc59d
Yura Zenevich <yura.zenevich@gmail.com>
2020-11-09 16:19:20 +0000

    Bug 1675539 - add telemetry for tabbing order overlay activation. r=jdescottes,janerik
    Differential Revision: https://phabricator.services.mozilla.com/D96091

--- devtools/client/accessibility/components/DisplayTabbingOrder.js
+++ devtools/client/accessibility/components/DisplayTabbingOrder.js
@@ -3,6 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
+/* global gTelemetry */
+
 // React
 const { PureComponent } = require("devtools/client/shared/vendor/react");
 const {
@@ -17,6 +19,9 @@ const { connect } = require("devtools/client/shared/vendor/react-redux");
 const {
   updateDisplayTabbingOrder,
 } = require("devtools/client/accessibility/actions/ui");
+
+const TELEMETRY_TABBING_ORDER_ACTIVATED =
+  "devtools.accessibility.tabbing_order_activated";
 
 class DisplayTabbingOrder extends PureComponent {
   static get propTypes() {
@@ -39,6 +44,9 @@ class DisplayTabbingOrder extends PureComponent {
 
   async onChange() {
     const { dispatch, tabbingOrderDisplayed } = this.props;
+    if (!tabbingOrderDisplayed) {
+      gTelemetry.scalarAdd(TELEMETRY_TABBING_ORDER_ACTIVATED, 1);
+    }
 
     this.setState({ disabled: true });
     await dispatch(updateDisplayTabbingOrder(!tabbingOrderDisplayed));
