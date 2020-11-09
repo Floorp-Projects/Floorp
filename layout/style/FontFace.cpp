acf749aa140a456aa1e51a72ce4d0cf2e91f76c3
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-11-09 09:47:02 +0200

    Backed out changeset 9e2f1e514e84 (bug 1675853) for causing wpt failures in ch-pseudo-recalc-on-font-load.html
    CLOSED TREE

--- layout/style/FontFace.cpp
+++ layout/style/FontFace.cpp
@@ -328,6 +328,8 @@ FontFaceLoadStatus FontFace::Status() { return mStatus; }
 Promise* FontFace::Load(ErrorResult& aRv) {
   MOZ_ASSERT(NS_IsMainThread());
 
+  mFontFaceSet->FlushUserFontSet();
+
   EnsurePromise();
 
   if (!mLoaded) {
