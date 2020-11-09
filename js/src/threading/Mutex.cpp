4883c6089c7641b63444732965fdcee13e733558
Jon Coppeard <jcoppeard@mozilla.com>
2020-11-09 18:19:08 +0000

    Bug 1675192 - Make Mutex::ownedByCurrentThread less racy r=sfink
    This method is inherently racy and is only absolutely safe if the current
    thread holds the mutex. Mostly it's used for the purpose of asserting that.
    However there is one use in the GC where we rely on result as part of some
    debug mode checks.
    
    This patch removes checking of the mutex stack in the case where the current
    thread doesn't hold the mutex, which is racy. It doesn't change the
    owningThread_ check which is also racy.  Hopefully this fixes the crashes while
    we work out how to fix this properly.
    
    (Note this code is only present in debug builds.)
    
    Differential Revision: https://phabricator.services.mozilla.com/D96390

--- js/src/threading/Mutex.cpp
+++ js/src/threading/Mutex.cpp
@@ -58,26 +58,23 @@ void js::Mutex::preUnlockChecks() {
   owningThread_ = ThreadId();
 }
 
-bool js::Mutex::isHeld() const { return owningThread_; }
-
 bool js::Mutex::ownedByCurrentThread() const {
-  // First determine this using the owningThread_ property, then check it via
-  // the mutex stack.
-  bool check = ThreadId::ThisThreadId() == owningThread_;
+  bool owned = ThreadId::ThisThreadId() == owningThread_;
 
-  Mutex* stack = HeldMutexStack.get();
+  // If we own the mutex then check it's on the mutex stack. It's not safe to do
+  // this if we don't own the mutex.
 
-  while (stack) {
-    if (stack == this) {
-      MOZ_ASSERT(check);
-      return true;
-    }
-
-    stack = stack->prev_;
+  if (!owned) {
+    return false;
   }
 
-  MOZ_ASSERT(!check);
-  return false;
+  for (Mutex* mutex = HeldMutexStack.get(); mutex; mutex = mutex->prev_) {
+    if (mutex == this) {
+      return true;
+    }
+  }
+
+  MOZ_CRASH("Mutex not found on the stack of held mutexes");
 }
 
 #endif
