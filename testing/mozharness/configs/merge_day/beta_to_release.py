f84646f23232ab8bb4e2cd018332f71c98d53b1c
Johan Lorenzo <jlorenzo@mozilla.com>
2020-11-09 13:55:32 +0100

    Backed out changeset 19db5464f5d1 DONTBUILD r=me a=release
    This caused `merge-beta-to-release` to fail on mergeday:
    
    ```
    ValueError: /app/workdir/src/build/defines.sh does not contain EARLY_BETA_OR_EARLIER=1
    ```
    
    https://firefox-ci-tc.services.mozilla.com/tasks/auFvwwmrTMika9D9QX37CA/runs/0/logs/https%3A%2F%2Ffirefox-ci-tc.services.mozilla.com%2Fapi%2Fqueue%2Fv1%2Ftask%2FauFvwwmrTMika9D9QX37CA%2Fruns%2F0%2Fartifacts%2Fpublic%2Flogs%2Flive_backing.log#L683
    
    EARLY_BETA_OR_EARLIER was correctly unset in https://hg.mozilla.org/releases/mozilla-beta/rev/1ec12ea99d1a0d300224ce331241b9e7a0347aa1

--- testing/mozharness/configs/merge_day/beta_to_release.py
+++ testing/mozharness/configs/merge_day/beta_to_release.py
@@ -12,9 +12,6 @@ config = {
     ],
     "replacements": [
         # File, from, to
-        ('build/defines.sh',
-         'EARLY_BETA_OR_EARLIER=1',
-         'EARLY_BETA_OR_EARLIER='),
     ],
 
     "vcs_share_base": os.path.join(ABS_WORK_DIR, 'hg-shared'),
