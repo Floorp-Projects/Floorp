9764d3fda09c2477fa184a6133bcd961172ae09a
Robert Mader <robert.mader@posteo.de>
2020-11-09 11:53:41 +0000

    Bug 1676022 - Silence some glib deprecation warnings, r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D96323

--- widget/gtk/nsAppShell.cpp
+++ widget/gtk/nsAppShell.cpp
@@ -116,12 +116,6 @@ nsAppShell::~nsAppShell() {
 }
 
 nsresult nsAppShell::Init() {
-  // For any versions of Glib before 2.36, g_type_init must be explicitly called
-  // to safely use the library. Failure to do so may cause various
-  // failures/crashes in any code that uses Glib, Gdk, or Gtk. In later versions
-  // of Glib, this call is a no-op.
-  g_type_init();
-
   mozilla::hal::Init();
 
 #ifdef MOZ_ENABLE_DBUS
