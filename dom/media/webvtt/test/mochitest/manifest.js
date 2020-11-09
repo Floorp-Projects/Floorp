4b1176bff5412586a831292708d008eec326e1e6
alwu <alwu@mozilla.com>
2020-11-09 15:47:12 +0000

    Bug 1675326 - part3 : move webvtt related plain mochitests to `dom/media/webvtt/test/mochitest`. r=bryce
    Depends on D96104
    
    Differential Revision: https://phabricator.services.mozilla.com/D96105

---
+++ dom/media/webvtt/test/mochitest/manifest.js
@@ -0,0 +1,27 @@
+// Force releasing decoder to avoid timeout in waiting for decoding resource.
+function removeNodeAndSource(n) {
+  n.remove();
+  // reset |srcObject| first since it takes precedence over |src|.
+  n.srcObject = null;
+  n.removeAttribute("src");
+  n.load();
+  while (n.firstChild) {
+    n.firstChild.remove();
+  }
+}
+
+function once(target, name, cb) {
+  var p = new Promise(function(resolve, reject) {
+    target.addEventListener(
+      name,
+      function() {
+        resolve();
+      },
+      { once: true }
+    );
+  });
+  if (cb) {
+    p.then(cb);
+  }
+  return p;
+}
