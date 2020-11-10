103c8a390a3b1b376b2b0316e8a04cb56145cb96
Henrik Skupin <mail@hskupin.info>
2020-11-10 19:13:48 +0000

    Bug 1669169 - [marionette] Enable usage of JSWindowActor by default when Fission is disabled. r=marionette-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D96464

--- testing/marionette/prefs.js
+++ testing/marionette/prefs.js
@@ -237,7 +237,7 @@ class MarionetteBranch extends Branch {
    * implementation for commands that already support Fission.
    */
   get useActors() {
-    return this.get("actors.enabled", Services.appinfo.fissionAutostart);
+    return this.get("actors.enabled", true);
   }
 }
 
