40778f846e8cabebef1c6311ca314534f4d4c52e
Chun-Min Chang <chun.m.chang@gmail.com>
2020-11-10 20:00:22 +0000

    Bug 1675719 - P2: mach vendor rust r=cubeb-reviewers,kinetik
    Depends on D96617
    
    Differential Revision: https://phabricator.services.mozilla.com/D96618

--- third_party/rust/cubeb-coreaudio/src/backend/mixer.rs
+++ third_party/rust/cubeb-coreaudio/src/backend/mixer.rs
@@ -212,6 +212,7 @@ impl Mixer {
         if output_channels.is_empty()
             || out_channel_count != output_channels.len()
             || all_silence == output_channels
+            || Self::non_silent_duplicate_channel_present(&output_channels)
         {
             cubeb_log!("Use invalid layout. Apply default layout instead");
             output_channels = get_default_channel_order(out_channel_count);
@@ -256,6 +257,19 @@ impl Mixer {
             self.buffer.as_ptr(),
             self.buffer.len() * mem::size_of::<u8>(),
         )
+    }
+
+    fn non_silent_duplicate_channel_present(channels: &[audio_mixer::Channel]) -> bool {
+        let mut bitmap: u32 = 0;
+        for channel in channels {
+            if channel != &Channel::Silence {
+                if (bitmap & channel.bitmask()) != 0 {
+                    return true;
+                }
+                bitmap |= channel.bitmask();
+            }
+        }
+        false
     }
 }
 
@@ -450,4 +464,27 @@ fn test_get_default_channel_order() {
             assert_eq!(&channels[CHANNEL_OERDER.len()..], silences.as_slice());
         }
     }
+}
+
+#[test]
+fn test_non_silent_duplicate_channels() {
+    let duplicate = [
+        Channel::FrontLeft,
+        Channel::Silence,
+        Channel::FrontRight,
+        Channel::FrontCenter,
+        Channel::Silence,
+        Channel::FrontRight,
+    ];
+    assert!(Mixer::non_silent_duplicate_channel_present(&duplicate));
+
+    let non_duplicate = [
+        Channel::FrontLeft,
+        Channel::Silence,
+        Channel::FrontRight,
+        Channel::FrontCenter,
+        Channel::Silence,
+        Channel::Silence,
+    ];
+    assert!(!Mixer::non_silent_duplicate_channel_present(&non_duplicate));
 }
