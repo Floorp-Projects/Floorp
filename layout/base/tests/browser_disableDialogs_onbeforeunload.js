4ee88ff70e610d0b569f9c071270c7d6b35886f9
Kris Maglione <maglione.k@gmail.com>
2020-11-10 20:35:04 +0000

    Bug 1669961: Return null from `.contentWindow` when inner window is inactive. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D93853

--- layout/base/tests/browser_disableDialogs_onbeforeunload.js
+++ layout/base/tests/browser_disableDialogs_onbeforeunload.js
@@ -50,9 +50,10 @@ async function openPage(enableDialogs) {
         const { Services } = ChromeUtils.import(
           "resource://gre/modules/Services.jsm"
         );
+        let win = content;
         Services.obs.addObserver(doc => {
-          if (content && doc == content.document) {
-            content.windowUtils[name]();
+          if (doc == win.document) {
+            win.windowUtils[name]();
           }
         }, "document-element-inserted");
       });
