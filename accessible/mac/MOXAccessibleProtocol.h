61086eeb1a78f6fba96aaaa6fa8cdcda40ad8fc3
Eitan Isaacson <eitan@monotonous.org>
2020-11-11 21:12:26 +0000

    Bug 1198336 - P2: Implement live region AX attributes and AXLiveRegionCreated notification. r=morgan
    We use an ivar boolean flag to cache the existance of a live region. This lets us
    hide the live region AX attributes and avoid sync round trips to content.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96292

--- accessible/mac/MOXAccessibleProtocol.h
+++ accessible/mac/MOXAccessibleProtocol.h
@@ -57,6 +57,9 @@
 // Return text delegate if it exists.
 - (id<MOXTextMarkerSupport> _Nullable)moxTextMarkerDelegate;
 
+// Return true if this accessible is a live region
+- (BOOL)moxIsLiveRegion;
+
 @optional
 
 #pragma mark - AttributeGetters
@@ -263,6 +266,15 @@
 
 // AXEditableAncestor
 - (id _Nullable)moxEditableAncestor;
+
+// AXARIAAtomic
+- (NSNumber* _Nullable)moxARIAAtomic;
+
+// AXARIALive
+- (NSString* _Nullable)moxARIALive;
+
+// AXARIARelevant
+- (NSString* _Nullable)moxARIARelevant;
 
 // AXMozDebugDescription
 - (NSString* _Nullable)moxMozDebugDescription;
