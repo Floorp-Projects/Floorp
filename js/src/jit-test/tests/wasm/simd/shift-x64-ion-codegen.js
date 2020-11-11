c324814fd48dcab099faebb9e1dc481bab55ab19
Lars T Hansen <lhansen@mozilla.com>
2020-11-11 15:27:41 +0000

    Bug 1673391 - Codegen test cases for binop-with-constant. r=jseward
    Chiefly, we add many new test cases to binop-x64-ion-codegen.js to test that
    correct code is emitted for binop-with-constant.
    
    Secondarily, we simplify the codegen harness so that instructions that are
    split across two lines are handled automatically, it is not necessary to
    signify the line break explicitly using a \s at the end of the first line
    and a * at the beginning of the second.
    
    Thirdly, just clean up some formatting, and use RIPRADDR throughout in the
    binary disassembly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96004

--- js/src/jit-test/tests/wasm/simd/shift-x64-ion-codegen.js
+++ js/src/jit-test/tests/wasm/simd/shift-x64-ion-codegen.js
@@ -10,15 +10,13 @@
 codegenTestX64_v128xLITERAL_v128(
     [['i8x16.shl', '(i32.const 2)', `
 66 0f fc c0               paddb %xmm0, %xmm0
-66 0f fc c0               paddb %xmm0, %xmm0
-`],
+66 0f fc c0               paddb %xmm0, %xmm0`],
      ['i16x8.shl', '(i32.const 2)', `66 0f 71 f0 02            psllw \\$0x02, %xmm0`],
      ['i32x4.shl', '(i32.const 2)', `66 0f 72 f0 02            pslld \\$0x02, %xmm0`],
      ['i64x2.shl', '(i32.const 2)', `66 0f 73 f0 02            psllq \\$0x02, %xmm0`],
      ['i8x16.shr_u', '(i32.const 2)', `
-66 0f db 05 .. 00 00 00   pandx ${RIPR}, %xmm0
-66 0f 71 d0 02            psrlw \\$0x02, %xmm0
-`],
+66 0f db 05 ${RIPRADDR}   pandx ${RIPR}, %xmm0
+66 0f 71 d0 02            psrlw \\$0x02, %xmm0`],
      ['i16x8.shr_s', '(i32.const 2)', `66 0f 71 e0 02            psraw \\$0x02, %xmm0`],
      ['i16x8.shr_u', '(i32.const 2)', `66 0f 71 d0 02            psrlw \\$0x02, %xmm0`],
      ['i32x4.shr_s', '(i32.const 2)', `66 0f 72 e0 02            psrad \\$0x02, %xmm0`],
