0cd2a53e7dc193b65605f5ac2cc1ca56e194a301
Mike Hommey <mh+mozilla@glandium.org>
2020-11-11 16:05:54 +0000

    Bug 1675740 - Allow make_dmg not to be given any flags. r=firefox-build-system-reviewers,rstewart
    When repacking a dmg, we already have .DS_Store,
    .background/background.png and .VolumeIcon.icns in the directory being
    packed, so there is no need to provide external files. As for the volume
    name, it's also simpler to derive it from the contents if none is
    provided.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96686

--- python/mozbuild/mozbuild/action/make_dmg.py
+++ python/mozbuild/mozbuild/action/make_dmg.py
@@ -4,6 +4,7 @@
 
 from __future__ import absolute_import, print_function
 
+from mozbuild.repackaging.application_ini import get_application_ini_value
 from mozpack import dmg
 
 import argparse
@@ -25,12 +26,22 @@ def main(args):
 
     options = parser.parse_args(args)
 
-    extra_files = [
-        (options.dsstore, ".DS_Store"),
-        (options.background, ".background/background.png"),
-        (options.icon, ".VolumeIcon.icns"),
-    ]
-    dmg.create_dmg(options.inpath, options.dmgfile, options.volume_name, extra_files)
+    extra_files = []
+    if options.dsstore:
+        extra_files.append((options.dsstore, ".DS_Store"))
+    if options.background:
+        extra_files.append((options.background, ".background/background.png"))
+    if options.icon:
+        extra_files.append((options.icon, ".VolumeIcon.icns"))
+
+    if options.volume_name:
+        volume_name = options.volume_name
+    else:
+        volume_name = get_application_ini_value(
+            options.inpath, "App", "CodeName", fallback="Name"
+        )
+
+    dmg.create_dmg(options.inpath, options.dmgfile, volume_name, extra_files)
 
     return 0
 
