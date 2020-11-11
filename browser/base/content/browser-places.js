011fc7fd428d35a76b37da2d7d2daee765487e90
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-11-11 15:36:00 +0000

    Bug 1675551 - ensure we can use the browser.toolbars.bookmarks.2h2020 pref for experiments and enable it on early beta, r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D96709

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -390,7 +390,7 @@ var StarUI = {
     }
 
     // If we're changing where a bookmark gets saved, persist that location.
-    if (didChangeFolder) {
+    if (didChangeFolder && gBookmarksToolbar2h2020) {
       Services.prefs.setCharPref(
         "browser.bookmarks.defaultLocation",
         selectedFolderGuid
@@ -1721,9 +1721,7 @@ var BookmarkingUI = {
       this._uninitView();
       this._isCustomizing = true;
 
-      if (
-        !Services.prefs.getBoolPref("browser.toolbars.bookmarks.2h2020", false)
-      ) {
+      if (!gBookmarksToolbar2h2020) {
         return;
       }
 
@@ -2294,12 +2292,7 @@ var BookmarkingUI = {
 
   async maybeShowOtherBookmarksFolder() {
     // Only show the "Other Bookmarks" folder in the toolbar if pref is enabled.
-    let featureEnabled = Services.prefs.getBoolPref(
-      "browser.toolbars.bookmarks.2h2020",
-      false
-    );
-
-    if (!featureEnabled) {
+    if (!gBookmarksToolbar2h2020) {
       return;
     }
 
