5c0ce4fef9fed1b635c374a3f7f63a1e73914539
Jeff Muizelaar <jmuizelaar@mozilla.com>
2020-11-11 00:13:12 +0000

    Bug 1676507 - Turn on WebRender on the rest of Tesla in Nightly. r=aosmond
    Differential Revision: https://phabricator.services.mozilla.com/D96644

--- widget/GfxDriverInfo.cpp
+++ widget/GfxDriverInfo.cpp
@@ -492,7 +492,13 @@ const GfxDeviceFamily* GfxDriverInfo::GetDeviceFamily(DeviceFamily id) {
       APPEND_DEVICE(0x0a7c);
       break;
     case DeviceFamily::NvidiaRolloutWebRender:
+#ifdef NIGHTLY_BUILD
+      APPEND_RANGE(0x0400, 0x04ff);
+      APPEND_RANGE(0x05e0, 0x05ff);
+      APPEND_RANGE(0x0600, INT32_MAX);
+#else
       APPEND_RANGE(0x06c0, INT32_MAX);
+#endif
       break;
     case DeviceFamily::IntelRolloutWebRender:
 #ifdef NIGHTLY_BUILD
