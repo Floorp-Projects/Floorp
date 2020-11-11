f3f5ed029643d83e37affeab1b22d1b266372db8
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-11-11 19:43:43 +0000

    Bug 1675016 - Add more test for HTTP3 failure r=necko-reviewers,kershaw
    - Add 2 test: 1) server is not listening to the port and 2) server is not responding that will cause the connection to timeout and fall back to HTTP2
     - Adds a server that only reads packets but never sends any to simulate a handshake timing out
    
    Differential Revision: https://phabricator.services.mozilla.com/D95816

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -1412,11 +1412,14 @@ class XPCShellTests(object):
             msg = process.stdout.readline()
             if "server listening" in msg:
                 searchObj = re.search(
-                    r"HTTP3 server listening on ports ([0-9]+) and ([0-9]+)", msg, 0
+                    r"HTTP3 server listening on ports ([0-9]+), ([0-9]+) and ([0-9]+)",
+                    msg,
+                    0,
                 )
                 if searchObj:
                     self.env["MOZHTTP3_PORT"] = searchObj.group(1)
                     self.env["MOZHTTP3_PORT_FAILED"] = searchObj.group(2)
+                    self.env["MOZHTTP3_PORT_NO_RESPONSE"] = searchObj.group(3)
         except OSError as e:
             # This occurs if the subprocess couldn't be started
             self.log.error("Could not run the http3 server: %s" % (str(e)))
