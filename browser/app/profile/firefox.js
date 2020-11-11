eca690112ff315bd8a1d320825392f65da1faa52
Scott <scott.downe@gmail.com>
2020-11-11 18:13:41 +0000

    Bug 1676700 - Turn on English Pocket stories in newtab for English users in India. r=gvn a=pascal
    Differential Revision: https://phabricator.services.mozilla.com/D96736

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1382,7 +1382,7 @@ pref("browser.newtabpage.activity-stream.discoverystream.region-stories-block", 
 // List of locales that get stories, regardless of region-stories-config.
 pref("browser.newtabpage.activity-stream.discoverystream.locale-list-config", "");
 // List of regions that get stories by default.
-pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE");
+pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "US,DE,CA,GB,IE,CH,AT,BE,IN");
 
 // List of regions that get spocs by default.
 pref("browser.newtabpage.activity-stream.discoverystream.region-spocs-config", "US,CA,DE,GB");
