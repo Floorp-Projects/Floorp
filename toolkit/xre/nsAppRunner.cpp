e63d1b0d2123eaa5713ea5d0ab0da8e89a14d50d
Bogdan Tara <btara@mozilla.com>
2020-11-12 20:49:21 +0200

    Backed out changeset 9c13c984601a (bug 1665461) for browser_preXULSkeletonUIRegistry.js failures CLOSED TREE

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -1958,17 +1958,7 @@ static void ReflectSkeletonUIPrefToRegistry(const char* aPref, void* aData) {
   if (shouldBeEnabled && Preferences::HasUserValue(kPrefThemeId)) {
     nsCString themeId;
     Preferences::GetCString(kPrefThemeId, themeId);
-    if (themeId.EqualsLiteral("default-theme@mozilla.org")) {
-      SetPreXULSkeletonUIThemeId(ThemeMode::Default);
-    } else if (themeId.EqualsLiteral("firefox-compact-dark@mozilla.org")) {
-      SetPreXULSkeletonUIThemeId(ThemeMode::Dark);
-    } else if (themeId.EqualsLiteral("firefox-compact-light@mozilla.org")) {
-      SetPreXULSkeletonUIThemeId(ThemeMode::Light);
-    } else {
-      shouldBeEnabled = false;
-    }
-  } else if (shouldBeEnabled) {
-    SetPreXULSkeletonUIThemeId(ThemeMode::Default);
+    shouldBeEnabled = themeId.EqualsLiteral("default-theme@mozilla.org");
   }
 
   if (GetPreXULSkeletonUIEnabled() != shouldBeEnabled) {
