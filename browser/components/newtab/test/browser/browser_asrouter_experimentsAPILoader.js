f5179b63fb3a10d3d921ac5088efd9c0ff9ed1bb
Andrei Oprea <andrei.br92@gmail.com>
2020-11-12 09:50:13 +0000

    Bug 1675544 - X-Man should not process experiment configurations if app.shield.optoutstudies.enabled is false r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D96206

--- browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
+++ browser/components/newtab/test/browser/browser_asrouter_experimentsAPILoader.js
@@ -138,6 +138,7 @@ add_task(async function test_loading_experimentsAPI() {
   // Force the WNPanel provider cache to 0 by modifying updateCycleInMs
   await SpecialPowers.pushPrefEnv({
     set: [
+      ["app.shield.optoutstudies.enabled", true],
       [
         "browser.newtabpage.activity-stream.asrouter.providers.messaging-experiments",
         `{"id":"messaging-experiments","enabled":true,"type":"remote-experiments","messageGroups":["cfr","whats-new-panel","moments-page","snippets","cfr-fxa"],"updateCycleInMs":0}`,
