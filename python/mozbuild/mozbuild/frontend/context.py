c7e2ffeae05f6a17f281d87dd45afeecded65727
Andrew Osmond <aosmond@mozilla.com>
2020-11-13 05:40:10 +0000

    Bug 1676925 - Remove code supporting the defunct WebRender experiments. r=jrmuizel,marionette-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D96943

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -2239,12 +2239,6 @@ VARIABLES = {
         """List of manifest files defining marionette-layout tests.
         """,
     ),
-    "MARIONETTE_GPU_MANIFESTS": (
-        ManifestparserManifestList,
-        list,
-        """List of manifest files defining marionette-gpu tests.
-        """,
-    ),
     "MARIONETTE_UNIT_MANIFESTS": (
         ManifestparserManifestList,
         list,
