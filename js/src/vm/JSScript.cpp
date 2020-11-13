075258e4b82cf7207208cd69d2f55f2d826054fa
Iain Ireland <iireland@mozilla.com>
2020-11-13 13:16:48 +0000

    Bug 1676631: Update local slots in SetFrameArgumentsObject r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D96928

--- js/src/vm/JSScript.cpp
+++ js/src/vm/JSScript.cpp
@@ -4725,6 +4725,15 @@ void js::SetFrameArgumentsObject(JSContext* cx, AbstractFramePtr frame,
       frame.unaliasedLocal(frameSlot) = ObjectValue(*argsobj);
     }
   }
+
+  // JS_OPTIMIZED_ARGUMENTS may also have been stored to a local slot
+  // during bailout. Update those local slots.
+  for (uint32_t i = 0; i < script->nfixed(); i++) {
+    Value& value = frame.unaliasedLocal(i);
+    if (value.isMagic() && value.whyMagic() == JS_OPTIMIZED_ARGUMENTS) {
+      frame.unaliasedLocal(i) = ObjectValue(*argsobj);
+    }
+  }
 }
 
 /* static */
