6f613de51bba639449ff58456c1e6d91f2fd93f2
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-11-13 22:14:45 +0200

    Backed out 3 changesets (bug 1666345, bug 1667152, bug 1661624) for causing mochitest timeouts.
    CLOSED TREE
    
    Backed out changeset baa1f7a615e4 (bug 1666345)
    Backed out changeset b6646baa866d (bug 1661624)
    Backed out changeset e4d550db6037 (bug 1667152)

---
+++ third_party/python/psutil/scripts/internal/generate_manifest.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2009 Giampaolo Rodola'. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Generate MANIFEST.in file.
+"""
+
+import os
+import subprocess
+
+
+SKIP_EXTS = ('.png', '.jpg', '.jpeg')
+SKIP_FILES = ('.travis.yml', 'appveyor.yml')
+SKIP_PREFIXES = ('.ci/', '.github/')
+
+
+def sh(cmd):
+    return subprocess.check_output(
+        cmd, shell=True, universal_newlines=True).strip()
+
+
+def main():
+    files = sh("git ls-files").split('\n')
+    for file in files:
+        if file.startswith(SKIP_PREFIXES) or \
+                os.path.splitext(file)[1].lower() in SKIP_EXTS or \
+                file in SKIP_FILES:
+            continue
+        print("include " + file)
+
+
+if __name__ == '__main__':
+    main()
