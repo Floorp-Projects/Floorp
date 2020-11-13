ad2988b42bcde2b92637ce4fa39ef6ddf712efc9
Kartikaya Gupta <kgupta@mozilla.com>
2020-11-13 22:58:13 +0000

    Bug 1662019 - Encapsulate the ScrollGeneration::New calls a bit more. r=tnikkel
    Instead of the scrollframe manually calling ScrollGeneration::New this has the
    ScrollPositionUpdate do it automatically. Just makes things a little more
    encapsulated.
    
    Note that this patch does contain a small functional change, which is that if
    ScrollFrameHelper::ScrollToImpl is called with ScrollOrigin::apz, it will not
    increment mScrollGeneration whereas it used to before. As far as I can tell
    this should be a harmless change.
    
    Depends on D97036
    
    Differential Revision: https://phabricator.services.mozilla.com/D97037

--- gfx/layers/apz/test/gtest/TestBasic.cpp
+++ gfx/layers/apz/test/gtest/TestBasic.cpp
@@ -449,14 +449,14 @@ TEST_F(APZCBasicTester, ResumeInterruptedTouchDrag_Bug1592435) {
   // a main thread scroll update to a nearby location.
   CSSPoint mainThreadOffset = scrollOffsetBeforeInterruption;
   mainThreadOffset.y -= 5;
-  ScrollGeneration gen = ScrollGeneration::New();
   ScrollMetadata metadata = apzc->GetScrollMetadata();
   metadata.GetMetrics().SetLayoutScrollOffset(mainThreadOffset);
-  metadata.GetMetrics().SetScrollGeneration(gen);
   nsTArray<ScrollPositionUpdate> scrollUpdates;
   scrollUpdates.AppendElement(ScrollPositionUpdate::NewScroll(
-      gen, ScrollOrigin::Other, CSSPoint::ToAppUnits(mainThreadOffset)));
+      ScrollOrigin::Other, CSSPoint::ToAppUnits(mainThreadOffset)));
   metadata.SetScrollUpdates(scrollUpdates);
+  metadata.GetMetrics().SetScrollGeneration(
+      scrollUpdates.LastElement().GetGeneration());
   apzc->NotifyLayersUpdated(metadata, false, true);
 
   // Continue and finish the touch-drag gesture.
@@ -476,10 +476,9 @@ TEST_F(APZCBasicTester, ResumeInterruptedTouchDrag_Bug1592435) {
       apzc->GetFrameMetrics().GetVisualScrollOffset();
   mainThreadOffset = scrollOffsetBeforeInterruption;
   mainThreadOffset.y -= 5;
-  gen = ScrollGeneration::New();
   metadata = apzc->GetScrollMetadata();
   metadata.GetMetrics().SetVisualDestination(mainThreadOffset);
-  metadata.GetMetrics().SetScrollGeneration(gen);
+  metadata.GetMetrics().SetScrollGeneration(ScrollGeneration::New());
   metadata.GetMetrics().SetVisualScrollUpdateType(FrameMetrics::eMainThread);
   scrollUpdates.Clear();
   metadata.SetScrollUpdates(scrollUpdates);
@@ -515,13 +514,12 @@ TEST_F(APZCBasicTester, RelativeScrollOffset) {
   ScrollMetadata mainThreadMetadata = metadata;
   FrameMetrics& mainThreadMetrics = mainThreadMetadata.GetMetrics();
   mainThreadMetrics.SetLayoutScrollOffset(CSSPoint(200, 200));
-  ScrollGeneration newGeneration = ScrollGeneration::New();
-  mainThreadMetrics.SetScrollGeneration(newGeneration);
   nsTArray<ScrollPositionUpdate> scrollUpdates;
   scrollUpdates.AppendElement(ScrollPositionUpdate::NewScroll(
-      newGeneration, ScrollOrigin::Other,
-      CSSPoint::ToAppUnits(CSSPoint(200, 200))));
+      ScrollOrigin::Other, CSSPoint::ToAppUnits(CSSPoint(200, 200))));
   mainThreadMetadata.SetScrollUpdates(scrollUpdates);
+  mainThreadMetrics.SetScrollGeneration(
+      scrollUpdates.LastElement().GetGeneration());
   apzc->NotifyLayersUpdated(mainThreadMetadata, /*isFirstPaint=*/false,
                             /*thisLayerTreeUpdated=*/true);
 
