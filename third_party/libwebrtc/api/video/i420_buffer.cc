7128d7512fd5a0c0b91c9c7435521618669c7982
Dan Minor <dminor@mozilla.com>
2020-11-13 12:42:51 -0500

    Bug 1654112 - Changes to support building with external libyuv. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D130081

--- third_party/libwebrtc/api/video/i420_buffer.cc
+++ third_party/libwebrtc/api/video/i420_buffer.cc
@@ -16,9 +16,9 @@
 
 #include "rtc_base/checks.h"
 #include "rtc_base/ref_counted_object.h"
-#include "third_party/libyuv/include/libyuv/convert.h"
-#include "third_party/libyuv/include/libyuv/planar_functions.h"
-#include "third_party/libyuv/include/libyuv/scale.h"
+#include "libyuv/include/libyuv/convert.h"
+#include "libyuv/include/libyuv/planar_functions.h"
+#include "libyuv/include/libyuv/scale.h"
 
 // Aligning pointer to 64 bytes for improved performance, e.g. use SIMD.
 static const int kBufferAlignment = 64;
--- third_party/libwebrtc/api/video/nv12_buffer.cc
+++ third_party/libwebrtc/api/video/nv12_buffer.cc
@@ -13,7 +13,7 @@
 #include "api/video/i420_buffer.h"
 #include "rtc_base/checks.h"
 #include "rtc_base/ref_counted_object.h"
-#include "third_party/libyuv/include/libyuv/convert.h"
+#include "libyuv/include/libyuv/convert.h"
 
 namespace webrtc {
 
