2731a2e3544c67be81177fff23b264b21775fc09
Emilio Cobos Álvarez <emilio@crisal.io>
2020-11-13 19:06:02 +0100

    Bug 1676843, bug 1618639 - A couple more test fixes.
    MANUAL PUSH: Orange in a CLOSED TREE

--- browser/components/extensions/test/browser/head_browserAction.js
+++ browser/components/extensions/test/browser/head_browserAction.js
@@ -117,10 +117,13 @@ async function testPopupSize(
     });
 
     win = dims.window;
-    is(win.innerHeight, innerHeight, "Window height should not change");
 
     // The getContentSize calculation is not always reliable to single-pixel
     // precision.
+    ok(
+      Math.abs(win.innerHeight - innerHeight) <= 1,
+      `Window height should return to approximately its original value (${win.innerHeight} ~= ${innerHeight})`
+    );
     ok(
       Math.abs(win.innerWidth - innerWidth) <= 1,
       `Window width should return to approximately its original value (${win.innerWidth} ~= ${innerWidth})`
