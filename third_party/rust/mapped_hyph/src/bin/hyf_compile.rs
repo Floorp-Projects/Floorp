c659e3f7bbef5bf8cd34220a6aaddce569be3800
Emilio Cobos Álvarez <emilio@crisal.io>
2020-11-13 12:23:04 +0000

    Bug 1677073 - Update mapped_hyph. r=jfkthame
    This removes a dependency on env_logger 0.7.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96985

--- third_party/rust/mapped_hyph/src/bin/hyf_compile.rs
+++ third_party/rust/mapped_hyph/src/bin/hyf_compile.rs
@@ -7,14 +7,31 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+extern crate log;
 extern crate mapped_hyph;
-extern crate env_logger;
 
 use std::env;
 use std::fs::File;
 
+struct Logger {}
+
+impl log::Log for Logger {
+    fn enabled(&self, _: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        eprintln!("{} - {}", record.level(), record.args());
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: Logger = Logger {};
+
 fn main() -> std::io::Result<()> {
-    env_logger::init();
+    unsafe { log::set_logger_racy(&LOGGER).unwrap() };
+
     let args: Vec<String> = env::args().collect();
     if args.len() == 3 {
         let in_file = File::open(&args[1])?;
