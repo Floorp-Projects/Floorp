bd132e112d6ba51bfb3f1b6faf014602f90b65d6
Valentin Gosu <valentin.gosu@gmail.com>
2020-11-13 13:48:00 +0000

    Bug 1658097 - [fission] Send resource timing data for cross-origin frames to the correct process r=nika,necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D96754

--- dom/performance/PerformanceStorageWorker.h
+++ dom/performance/PerformanceStorageWorker.h
@@ -29,7 +29,9 @@ class PerformanceStorageWorker final : public PerformanceStorage {
 
   void AddEntry(nsIHttpChannel* aChannel,
                 nsITimedChannel* aTimedChannel) override;
-
+  virtual void AddEntry(const nsString& entryName,
+                        const nsString& initiatorType,
+                        UniquePtr<PerformanceTimingData>&& aData) override {}
   void AddEntryOnWorker(UniquePtr<PerformanceProxyData>&& aData);
 
  private:
