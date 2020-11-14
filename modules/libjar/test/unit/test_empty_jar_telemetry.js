b1edb758eceb6081c3eb1d716f96a50feb7c737f
Valentin Gosu <valentin.gosu@gmail.com>
2020-11-14 22:06:35 +0000

    Bug 1675205 - Report telemetry when load an empty file via a JAR archive r=zbraniecki
    This will report when a telemetry even whenever we load an empty file via a
    JAR channel. It will not catch situations where onStopRequest is called with
    an error code, or when the listener of the channel does not consume the stream.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96412

---
+++ modules/libjar/test/unit/test_empty_jar_telemetry.js
@@ -0,0 +1,102 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+"use strict";
+
+const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+const { NetUtil } = ChromeUtils.import("resource://gre/modules/NetUtil.jsm");
+
+const { TelemetryTestUtils } = ChromeUtils.import(
+  "resource://testing-common/TelemetryTestUtils.jsm"
+);
+
+const { TelemetryController } = ChromeUtils.import(
+  "resource://gre/modules/TelemetryController.jsm"
+);
+
+const nsIBinaryInputStream = Components.Constructor(
+  "@mozilla.org/binaryinputstream;1",
+  "nsIBinaryInputStream",
+  "setInputStream"
+);
+
+const fileBase = "test_empty_file.zip";
+const file = do_get_file("data/" + fileBase);
+const jarBase = "jar:" + Services.io.newFileURI(file).spec + "!";
+const tmpDir = Services.dirsvc.get("TmpD", Ci.nsIFile);
+
+function Listener(callback) {
+  this._callback = callback;
+}
+Listener.prototype = {
+  gotStartRequest: false,
+  available: -1,
+  gotStopRequest: false,
+  QueryInterface: ChromeUtils.generateQI(["nsIRequestObserver"]),
+  onDataAvailable(request, stream, offset, count) {
+    try {
+      this.available = stream.available();
+      Assert.equal(this.available, count);
+      // Need to consume stream to avoid assertion
+      new nsIBinaryInputStream(stream).readBytes(count);
+    } catch (ex) {
+      do_throw(ex);
+    }
+  },
+  onStartRequest(request) {
+    this.gotStartRequest = true;
+  },
+  onStopRequest(request, status) {
+    this.gotStopRequest = true;
+    Assert.equal(status, 0);
+    if (this._callback) {
+      this._callback.call(null, this);
+    }
+  },
+};
+
+const TELEMETRY_EVENTS_FILTERS = {
+  category: "network.jar.channel",
+  method: "noData",
+};
+
+add_task(async function test_empty_jar_file() {
+  var copy = tmpDir.clone();
+  copy.append("zzdxphd909dbc6r2bxtqss2m000017");
+  copy.append("zzdxphd909dbc6r2bxtqss2m000017");
+  copy.append(fileBase);
+  file.copyTo(copy.parent, copy.leafName);
+
+  var uri = "jar:" + Services.io.newFileURI(copy).spec + "!/test.txt";
+  var chan = NetUtil.newChannel({ uri, loadUsingSystemPrincipal: true });
+
+  Services.telemetry.setEventRecordingEnabled("network.jar.channel", true);
+  Services.telemetry.clearEvents();
+
+  await new Promise(resolve => {
+    chan.asyncOpen(
+      new Listener(function(l) {
+        Assert.ok(chan.contentLength == 0);
+
+        resolve();
+      })
+    );
+  });
+
+  try {
+    copy.remove(false);
+  } catch (e) {}
+
+  TelemetryTestUtils.assertEvents(
+    [
+      {
+        category: "network.jar.channel",
+        method: "noData",
+        object: "onStop",
+        value: `${fileBase}!/test.txt`,
+      },
+    ],
+    TELEMETRY_EVENTS_FILTERS
+  );
+});
