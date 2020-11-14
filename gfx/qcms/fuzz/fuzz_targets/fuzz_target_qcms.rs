172d94a1397cb5786526570a40818d0a9a4eaaef
Jeff Muizelaar <jmuizelaar@mozilla.com>
2020-11-14 16:35:58 +0000

    Bug 1677309 - Make qcms_profile_precache_output_transform safe. r=aosmond
    There's a couple of formatting cleanups too.
    
    Differential Revision: https://phabricator.services.mozilla.com/D97080

--- gfx/qcms/fuzz/fuzz_targets/fuzz_target_qcms.rs
+++ gfx/qcms/fuzz/fuzz_targets/fuzz_target_qcms.rs
@@ -32,7 +32,7 @@ extern crate libc;
    // Firefox calls this on the display profile to increase performance.
    // Skip with low probability to increase coverage.
    if (size % 15) != 0 {
-     qcms_profile_precache_output_transform(dst_profile);
+     qcms_profile_precache_output_transform(&mut *dst_profile);
    }
  
    let transform =
