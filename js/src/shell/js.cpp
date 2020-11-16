7962b749a1c38f7fb5af822ee623e583f4cebc8b
Jan de Mooij <jdemooij@mozilla.com>
2020-11-16 14:00:04 +0000

    Bug 1673553 part 3 - Remove Warp browser pref. r=iain,preferences-reviewers
    Depends on D96988
    
    Differential Revision: https://phabricator.services.mozilla.com/D96989

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -4844,22 +4844,6 @@ static bool SetJitCompilerOption(JSContext* cx, unsigned argc, Value* vp) {
     return false;
   }
 
-  // Similarly, don't allow enabling or disabling Warp at runtime. Bytecode and
-  // VM data structures depend on whether TI is enabled/disabled.
-  if (opt == JSJITCOMPILER_WARP_ENABLE) {
-    uint32_t warpEnabled;
-    if (!JS_GetGlobalJitCompilerOption(cx, JSJITCOMPILER_WARP_ENABLE,
-                                       &warpEnabled)) {
-      return false;
-    }
-    if (bool(number) != warpEnabled) {
-      JS_ReportErrorASCII(
-          cx, "Enabling or disabling Warp at runtime is not supported.");
-      return false;
-    }
-    return true;
-  }
-
   // Throw if disabling the JITs and there's JIT code on the stack, to avoid
   // assertion failures.
   if ((opt == JSJITCOMPILER_BASELINE_ENABLE ||
