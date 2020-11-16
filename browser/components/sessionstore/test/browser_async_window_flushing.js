5456e84b7792fd3527c5a5014d0f4314fd984e30
Nika Layzell <nika@thelayzells.com>
2020-11-16 14:13:27 +0000

    Bug 1675334 - Part 3: Remove InChild suffix from promiseOnHistoryReplaceEntry, r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D96615

--- browser/components/sessionstore/test/browser_async_window_flushing.js
+++ browser/components/sessionstore/test/browser_async_window_flushing.js
@@ -38,7 +38,7 @@ add_task(async function test_add_interesting_window() {
     content.location = newPage;
   });
 
-  await promiseOnHistoryReplaceEntryInChild(browser);
+  await promiseOnHistoryReplaceEntry(browser);
 
   // Clear out the userTypedValue so that the new window looks like
   // it's really not worth restoring.
