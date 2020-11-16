3102f9de4ce15bff4b70cf09d25ad192cf3c8521
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-11-16 20:38:25 +0000

    Bug 1675491 - Count number of bytes send and received over a socket. r=necko-reviewers,valentin
    This needs to count encrypted data, therefore it is implemented as a NSPR layer right above the PR_NSPR_IO_LAYER layer.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96083

--- netwerk/base/nsSocketTransport2.cpp
+++ netwerk/base/nsSocketTransport2.cpp
@@ -34,6 +34,7 @@
 #include "nsIDNSRecord.h"
 #include "nsIDNSByTypeRecord.h"
 #include "nsICancelable.h"
+#include "NetworkDataCountLayer.h"
 #include "QuicSocketControl.h"
 #include "TCPFastOpenLayer.h"
 #include <algorithm>
@@ -1608,6 +1609,16 @@ nsresult nsSocketTransport::InitiateSocket() {
       SOCKET_LOG(
           ("nsSocketTransport::InitiateSocket TCP Fast Open "
            "started [this=%p]\n",
+           this));
+    }
+  }
+
+  if (Telemetry::CanRecordPrereleaseData() ||
+      Telemetry::CanRecordReleaseData()) {
+    if (NS_FAILED(AttachNetworkDataCountLayer(fd))) {
+      SOCKET_LOG(
+          ("nsSocketTransport::InitiateSocket "
+           "AttachNetworkDataCountLayer failed [this=%p]\n",
            this));
     }
   }
