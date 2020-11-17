27c5480663b2a5700e6fda2b20fa53f5132de6af
Mirko Brodesser <mbrodesser@mozilla.com>
2020-11-17 08:30:12 +0000

    Bug 1677092: add `constexpr` `AnyOf`. r=sg
    The STL supports it only with C++20.
    
    Differential Revision: https://phabricator.services.mozilla.com/D97123

--- mfbt/Algorithm.h
+++ mfbt/Algorithm.h
@@ -28,6 +28,18 @@ constexpr bool AllOf(Iter aFirst, Iter aLast, Pred aPred) {
   return true;
 }
 
+// Like C++20's `std::any_of`.
+template <typename Iter, typename Pred>
+constexpr bool AnyOf(Iter aFirst, Iter aLast, Pred aPred) {
+  for (; aFirst != aLast; ++aFirst) {
+    if (aPred(*aFirst)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 namespace detail {
 template <typename Transform, typename SrcIter>
 using ArrayElementTransformType = typename std::invoke_result_t<
