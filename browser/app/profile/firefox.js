1e9de01c27e8e9b33ebeb33c756dedb04351a6e6
Mihai Alexandru Michis <malexandru@mozilla.com>
2020-11-17 18:29:54 +0200

    Backed out 2 changesets (bug 1671935, bug 1671937) for causing node newtab failures.
    CLOSED TREE
    
    Backed out changeset 9178fe846d75 (bug 1671937)
    Backed out changeset fa49d5e4c9cf (bug 1671935)

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1361,13 +1361,6 @@ pref("prompts.tabChromePromptSubDialog", true);
 // Activates preloading of the new tab url.
 pref("browser.newtab.preload", true);
 
-// Preference to enable the entire new newtab experience at once.
-pref("browser.newtabpage.activity-stream.newNewtabExperience.enabled", false);
-
-// A preference which allows us to enable the fly out customization overlay
-// on the newtab page.
-pref("browser.newtabpage.activity-stream.customizationMenu.enabled", false);
-
 // Activity Stream prefs that control to which page to redirect
 #ifndef RELEASE_OR_BETA
   pref("browser.newtabpage.activity-stream.debug", false);
