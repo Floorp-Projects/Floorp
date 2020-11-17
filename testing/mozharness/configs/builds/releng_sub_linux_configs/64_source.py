c0f6fe8caf1d443f5e70a92ce0aa14ca016c79b9
Sylvestre Ledru <sledru@mozilla.com>
2020-11-17 02:06:54 +0000

    Bug 1562642 - Add missing license headers r=mhoye,marionette-reviewers,webdriver-reviewers,perftest-reviewers,tarek
    Differential Revision: https://phabricator.services.mozilla.com/D97138

--- testing/mozharness/configs/builds/releng_sub_linux_configs/64_source.py
+++ testing/mozharness/configs/builds/releng_sub_linux_configs/64_source.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 config = {
     "default_actions": ["package-source"],
     "stage_platform": "source",  # Not used, but required by the script
--- testing/profiles/unittest-features/user.js
+++ testing/profiles/unittest-features/user.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Base preferences file for enabling unittest features.
 // Not enabling these features cause many unittests that
 // expect these prefs to be set to fail.
