08ec5ac66f841b245a1fcdbd8ae516e7f4f62dce
Jan-Erik Rediger <jrediger@mozilla.com>
2020-11-17 14:04:17 +0000

    Bug 1677701 - Update glean-core to v33.4.0 and enable safe-mode.  r=Dexter
    Differential Revision: https://phabricator.services.mozilla.com/D97288

--- third_party/rust/glean-core/src/traits/counter.rs
+++ third_party/rust/glean-core/src/traits/counter.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use crate::ErrorType;
+
 /// A description for the `CounterMetric` type.
 ///
 /// When changing this trait, make sure all the operations are
@@ -29,4 +31,23 @@ pub trait Counter {
     /// * `ping_name` - represents the optional name of the ping to retrieve the
     ///   metric for. Defaults to the first value in `send_in_pings`.
     fn test_get_value<'a, S: Into<Option<&'a str>>>(&self, ping_name: S) -> Option<i32>;
+
+    /// **Exported for test purposes.**
+    ///
+    /// Gets the number of recorded errors for the given metric and error type.
+    ///
+    /// # Arguments
+    ///
+    /// * `error` - The type of error
+    /// * `ping_name` - represents the optional name of the ping to retrieve the
+    ///   metric for. Defaults to the first value in `send_in_pings`.
+    ///
+    /// # Returns
+    ///
+    /// The number of errors reported.
+    fn test_get_num_recorded_errors<'a, S: Into<Option<&'a str>>>(
+        &self,
+        error: ErrorType,
+        ping_name: S,
+    ) -> i32;
 }
--- third_party/rust/glean/src/private/mod.rs
+++ third_party/rust/glean/src/private/mod.rs
@@ -5,9 +5,13 @@
 //! The different metric types supported by the Glean SDK to handle data.
 
 mod boolean;
+mod counter;
 mod ping;
 mod recorded_experiment_data;
+mod string;
 
 pub use boolean::BooleanMetric;
+pub use counter::CounterMetric;
 pub use ping::PingType;
 pub use recorded_experiment_data::RecordedExperimentData;
+pub use string::StringMetric;
