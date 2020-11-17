3db8fe885041b54249d1c65f4adbe599a082fe29
Sylvestre Ledru <sledru@mozilla.com>
2020-11-17 02:06:54 +0000

    Bug 1562642 - Add missing license headers r=mhoye,marionette-reviewers,webdriver-reviewers,perftest-reviewers,tarek
    Differential Revision: https://phabricator.services.mozilla.com/D97138

--- testing/mozharness/examples/action_config_script.py
+++ testing/mozharness/examples/action_config_script.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python -u
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 """action_config_script.py
 
 Demonstrate actions and config.
