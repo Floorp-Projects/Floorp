565914c2baeff1e9f0c8c11b9e3957e456212080
David Parks <daparks@mozilla.com>
2020-11-18 03:08:29 +0000

    Bug 1675349: Replace plugin behavior with a basic fallback r=jmathies,mixedpuppy
    The browser currently only enables plugin behavior for Flash and our internal test plugins.  This patch replaces support for those plugins with a simple fallback that shows a transparent region where the plugin would have been.  It removes the file system search(es) for the plugin dynamic libraries and short-circuits the logic to determine if plugins should do something special -- all implementations now behave the same in the presence of plugin elements.
    
    The new behavior is:
    1. If the <object> or <embed> element lists a type of something other than "x-shockwave-flash" or "x-test" then the behavior is unchanged.  This means that non-plugin types behave properly and unknown types (for example, typos) are also unaffected (they reduce to 0x0 elements).
    2. If the <object> element has an HTML fallback in the DOM (see spec for <object> elements) then the fallback is always shown.
    3. Otherwise, the element is shown as a transparent region with the size specified in attributes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D95902

--- dom/base/nsContentUtils.cpp
+++ dom/base/nsContentUtils.cpp
@@ -9735,18 +9735,11 @@ uint32_t nsContentUtils::HtmlObjectContentTypeForMIMEType(
     return nsIObjectLoadingContent::TYPE_DOCUMENT;
   }
 
-  RefPtr<nsPluginHost> pluginHost = nsPluginHost::GetInst();
-  if (pluginHost) {
-    nsCOMPtr<nsIPluginTag> tag = PluginTagForType(aMIMEType, aNoFakePlugin);
-    if (tag) {
-      if (!aNoFakePlugin &&
-          nsCOMPtr<nsIFakePluginTag>(do_QueryInterface(tag))) {
-        return nsIObjectLoadingContent::TYPE_FAKE_PLUGIN;
-      }
-
-      // ShouldPlay will handle checking for disabled plugins
-      return nsIObjectLoadingContent::TYPE_PLUGIN;
-    }
+  bool isPlugin = nsPluginHost::GetSpecialType(aMIMEType) !=
+                  nsPluginHost::eSpecialType_None;
+  if (isPlugin) {
+    // ShouldPlay will handle checking for disabled plugins
+    return nsIObjectLoadingContent::TYPE_PLUGIN;
   }
 
   return nsIObjectLoadingContent::TYPE_NULL;
