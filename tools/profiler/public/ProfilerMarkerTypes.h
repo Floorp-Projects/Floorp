770381db0625caf45b20b5938f5267ba0bef88bf
Gerald Squelart <gsquelart@mozilla.com>
2020-11-18 21:45:21 +0000

    Bug 1675409 - Migrated VsyncMarkerPayload to Markers 2.0 API - r=gregtatum
    Differential Revision: https://phabricator.services.mozilla.com/D96035

--- tools/profiler/public/ProfilerMarkerTypes.h
+++ tools/profiler/public/ProfilerMarkerTypes.h
@@ -43,21 +43,6 @@ using LongTask = mozilla::baseprofiler::markers::LongTask;
 using Log = mozilla::baseprofiler::markers::Log;
 using MediaSample = mozilla::baseprofiler::markers::MediaSample;
 
-// Tracks when a vsync occurs according to the HardwareComposer.
-struct Vsync {
-  static constexpr mozilla::Span<const char> MarkerTypeName() {
-    return mozilla::MakeStringSpan("VsyncTimestamp");
-  }
-  static void StreamJSONMarkerData(
-      mozilla::baseprofiler::SpliceableJSONWriter& aWriter) {}
-  static mozilla::MarkerSchema MarkerTypeDisplay() {
-    using MS = mozilla::MarkerSchema;
-    MS schema{MS::Location::markerChart, MS::Location::markerTable};
-    // Nothing outside the defaults.
-    return schema;
-  }
-};
-
 struct Network {
   static constexpr mozilla::Span<const char> MarkerTypeName() {
     return mozilla::MakeStringSpan("Network");
