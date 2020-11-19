80f6a75149c3c513b907565a429a7fe3b1942641
pbz <pbz@mozilla.com>
2020-11-19 18:20:36 +0000

    Bug 1669990 - SubDialog: Use system event listener for escape key. r=Gijs
    If a <select> in a SubDialog closes its popup when the user hits the esc key, it calls `preventDefault()` on the event. The SubDialog code didn't see this preventDefault, because it was using a normal event listener, while the select code uses a system event listener.
    
    https://searchfox.org/mozilla-central/rev/c2e3ac11be4837718c2604e58085fbc8252b69dd/layout/forms/nsListControlFrame.cpp#925,2058,2068-2069,2075-2076
    
    Differential Revision: https://phabricator.services.mozilla.com/D93253

--- browser/base/content/test/tabdialogs/browser_subdialog_esc.js
+++ browser/base/content/test/tabdialogs/browser_subdialog_esc.js
@@ -54,3 +54,65 @@ add_task(async function test_subdialog_esc_does_not_cancel_load() {
     ok(true, "Load completed");
   });
 });
+
+/**
+ * Tests that ESC on a SubDialog with an open dropdown doesn't close the dialog.
+ */
+add_task(async function test_subdialog_esc_on_dropdown_does_not_close_dialog() {
+  await BrowserTestUtils.withNewTab("http://example.com", async function(
+    browser
+  ) {
+    // Open the test dialog
+    let dialogBox = gBrowser.getTabDialogBox(browser);
+    let dialogClose = dialogBox.open(TEST_DIALOG_PATH, {
+      keepOpenSameOriginNav: true,
+    });
+
+    let dialogs = dialogBox._dialogManager._dialogs;
+
+    is(dialogs.length, 1, "Dialog manager has a dialog.");
+
+    let dialog = dialogs[0];
+
+    info("Waiting for dialog to open.");
+    await dialog._dialogReady;
+
+    // Open dropdown
+    let select = dialog._frame.contentDocument.getElementById("select");
+    let shownPromise = BrowserTestUtils.waitForEvent(
+      select,
+      "popupshowing",
+      true
+    );
+
+    info("Opening dropdown");
+    select.focus();
+    EventUtils.synthesizeKey("VK_SPACE", {}, dialog._frame.contentWindow);
+
+    await shownPromise;
+
+    let hiddenPromise = BrowserTestUtils.waitForEvent(
+      select,
+      "popuphiding",
+      true
+    );
+
+    // Race dropdown closing vs SubDialog close
+    let race = Promise.race([
+      hiddenPromise.then(() => true),
+      dialogClose.then(() => false),
+    ]);
+
+    // Close the dropdown with esc key
+    info("Hitting escape key.");
+    await EventUtils.synthesizeKey("KEY_Escape");
+
+    let result = await race;
+    ok(result, "Select closed first");
+
+    await new Promise(resolve => executeSoon(resolve));
+
+    ok(!dialog._isClosing, "Dialog is not closing");
+    ok(dialog._openedURL, "Dialog is open");
+  });
+});
