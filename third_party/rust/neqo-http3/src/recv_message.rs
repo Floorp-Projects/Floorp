27296f18d7d7a229f0ea88dd5a348d5022b9771b
Dragana Damjanovic <dd.mozilla@gmail.com>
2020-11-19 13:06:31 +0000

    Bug 1677743 - Neqo version 0.4.16 r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D97445

--- third_party/rust/neqo-http3/src/recv_message.rs
+++ third_party/rust/neqo-http3/src/recv_message.rs
@@ -20,6 +20,13 @@ use std::convert::TryFrom;
 use std::fmt::Debug;
 use std::rc::Rc;
 
+const PSEUDO_HEADER_STATUS: u8 = 0x1;
+const PSEUDO_HEADER_METHOD: u8 = 0x2;
+const PSEUDO_HEADER_SCHEME: u8 = 0x4;
+const PSEUDO_HEADER_AUTHORITY: u8 = 0x8;
+const PSEUDO_HEADER_PATH: u8 = 0x10;
+const REGULAR_HEADER: u8 = 0x80;
+
 #[derive(Debug)]
 pub enum MessageType {
     Request,
@@ -142,7 +149,7 @@ impl RecvMessage {
         decoder: &mut QPackDecoder,
     ) -> Res<()> {
         let interim = self.is_interim(&headers)?;
-
+        let _valid = self.headers_valid(&headers)?;
         if fin && interim {
             return Err(Error::HttpGeneralProtocolStream);
         }
@@ -325,16 +332,95 @@ impl RecvMessage {
             MessageType::Response => {
                 let status = headers.iter().find(|(name, _value)| name == ":status");
                 if let Some((_name, value)) = status {
-                    let status_code = value
-                        .parse::<i32>()
-                        .map_err(|_| Error::HttpGeneralProtocolStream)?;
+                    let status_code = value.parse::<i32>().map_err(|_| Error::InvalidHeader)?;
                     Ok(status_code >= 100 && status_code < 200)
                 } else {
-                    Err(Error::HttpGeneralProtocolStream)
+                    Err(Error::InvalidHeader)
                 }
             }
             MessageType::Request => Ok(false),
         }
+    }
+
+    fn track_pseudo(name: &str, state: &mut u8, message_type: &MessageType) -> Res<bool> {
+        let (pseudo, bit) = if name.starts_with(':') {
+            if *state & REGULAR_HEADER != 0 {
+                return Err(Error::InvalidHeader);
+            }
+            let bit = match message_type {
+                MessageType::Response => match name {
+                    ":status" => PSEUDO_HEADER_STATUS,
+                    _ => return Err(Error::InvalidHeader),
+                },
+                MessageType::Request => match name {
+                    ":method" => PSEUDO_HEADER_METHOD,
+                    ":scheme" => PSEUDO_HEADER_SCHEME,
+                    ":authority" => PSEUDO_HEADER_AUTHORITY,
+                    ":path" => PSEUDO_HEADER_PATH,
+                    _ => return Err(Error::InvalidHeader),
+                },
+            };
+            (true, bit)
+        } else {
+            (false, REGULAR_HEADER)
+        };
+
+        if *state & bit == 0 || !pseudo {
+            *state |= bit;
+            Ok(pseudo)
+        } else {
+            Err(Error::InvalidHeader)
+        }
+    }
+
+    fn headers_valid(&self, headers: &[Header]) -> Res<bool> {
+        let mut method_value: Option<&String> = None;
+        let mut pseudo_state = 0;
+        for header in headers {
+            let is_pseudo = Self::track_pseudo(&header.0, &mut pseudo_state, &self.message_type)?;
+
+            let mut bytes = header.0.bytes();
+            if is_pseudo {
+                if header.0 == ":method" {
+                    method_value = Some(&header.1);
+                }
+                let _ = bytes.next();
+            }
+
+            if bytes.any(|b| matches!(b, 0 | 0x10 | 0x13 | 0x3a | 0x41..=0x5a)) {
+                return Err(Error::InvalidHeader); // illegal characters.
+            }
+
+            if matches!(
+                header.0.as_str(),
+                "connection"
+                    | "host"
+                    | "keep-alive"
+                    | "proxy-connection"
+                    | "te"
+                    | "transfer-encoding"
+                    | "upgrade"
+            ) {
+                return Err(Error::InvalidHeader);
+            }
+        }
+        // Clear the regular header bit, since we only check pseudo headers below.
+        pseudo_state &= !REGULAR_HEADER;
+        let pseudo_header_mask = match self.message_type {
+            MessageType::Response => PSEUDO_HEADER_STATUS,
+            MessageType::Request => {
+                if method_value == Some(&"CONNECT".to_string()) {
+                    PSEUDO_HEADER_METHOD | PSEUDO_HEADER_AUTHORITY
+                } else {
+                    PSEUDO_HEADER_METHOD | PSEUDO_HEADER_SCHEME | PSEUDO_HEADER_PATH
+                }
+            }
+        };
+        if pseudo_state & pseudo_header_mask != pseudo_header_mask {
+            return Err(Error::InvalidHeader);
+        }
+
+        Ok(true)
     }
 }
 
