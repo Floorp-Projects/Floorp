ab958113799ada7912ece9cfab72b72124b91483
Andreas Pehrson <apehrson@mozilla.com>
2020-11-19 15:56:08 +0000

    Bug 1605134 - Add gtest for slow starting devices. r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D97411

--- dom/media/gtest/TestAudioCallbackDriver.cpp
+++ dom/media/gtest/TestAudioCallbackDriver.cpp
@@ -10,16 +10,17 @@
 #include "gtest/gtest-printers.h"
 #include "gtest/gtest.h"
 
+#include "MediaTrackGraphImpl.h"
 #include "mozilla/Attributes.h"
 #include "mozilla/UniquePtr.h"
 #include "nsTArray.h"
 
 #include "MockCubeb.h"
+#include "WaitFor.h"
 
 using namespace mozilla;
 using IterationResult = GraphInterface::IterationResult;
 using ::testing::NiceMock;
-using ::testing::Return;
 
 class MockGraphInterface : public GraphInterface {
   NS_DECL_THREADSAFE_ISUPPORTS
@@ -43,6 +44,9 @@ class MockGraphInterface : public GraphInterface {
                   aStateComputedTime - mStateComputedTime, mSampleRate);
       aMixer->FinishMixing();
     }
+    if (aStateComputedTime != mStateComputedTime) {
+      ++mIterationCount;
+    }
     mStateComputedTime = aStateComputedTime;
     if (!mKeepProcessing) {
       return IterationResult::CreateStop(
@@ -53,13 +57,20 @@ class MockGraphInterface : public GraphInterface {
       return IterationResult::CreateSwitchDriver(
           next, NS_NewRunnableFunction(__func__, [] {}));
     }
+    if (mEnsureNextIteration) {
+      driver->EnsureNextIteration();
+    }
     return IterationResult::CreateStillProcessing();
   }
+  void SetEnsureNextIteration(bool aEnsure) { mEnsureNextIteration = aEnsure; }
+
 #ifdef DEBUG
   bool InDriverIteration(GraphDriver* aDriver) override {
     return aDriver->OnThread();
   }
 #endif
+
+  size_t IterationCount() const { return mIterationCount; }
 
   GraphTime StateComputedTime() const { return mStateComputedTime; }
   void SetCurrentDriver(GraphDriver* aDriver) { mCurrentDriver = aDriver; }
@@ -70,8 +81,10 @@ class MockGraphInterface : public GraphInterface {
   const TrackRate mSampleRate;
 
  protected:
+  Atomic<size_t> mIterationCount{0};
   Atomic<GraphTime> mStateComputedTime{0};
   Atomic<GraphDriver*> mCurrentDriver{nullptr};
+  Atomic<bool> mEnsureNextIteration{false};
   Atomic<bool> mKeepProcessing{true};
   Atomic<GraphDriver*> mNextDriver{nullptr};
   virtual ~MockGraphInterface() = default;
@@ -108,4 +121,96 @@ MOZ_CAN_RUN_SCRIPT_FOR_DEFINITION {
   MOZ_KnownLive(driver)->Shutdown();
   EXPECT_FALSE(driver->ThreadRunning()) << "Verify thread is not running";
   EXPECT_FALSE(driver->IsStarted()) << "Verify thread is not started";
+}
+
+void TestSlowStart(const TrackRate aRate) MOZ_CAN_RUN_SCRIPT_FOR_DEFINITION {
+  std::cerr << "TestSlowStart with rate " << aRate << std::endl;
+
+  MockCubeb* cubeb = new MockCubeb();
+  const TimeDuration startDelay = TimeDuration::FromSeconds(0.3);
+  cubeb->SetStreamStartDelay(startDelay);
+  CubebUtils::ForceSetCubebContext(cubeb->AsCubebContext());
+
+  RefPtr<AudioCallbackDriver> driver;
+  auto graph = MakeRefPtr<NiceMock<MockGraphInterface>>(aRate);
+  EXPECT_CALL(*graph, NotifyInputStopped).Times(0);
+
+  Maybe<size_t> fallbackIterationCount;
+  Maybe<int64_t> audioStart;
+  Maybe<uint32_t> alreadyBuffered;
+  int64_t inputFrameCount = 0;
+  int64_t outputFrameCount = 0;
+  int64_t processedFrameCount = 0;
+  ON_CALL(*graph, NotifyInputData)
+      .WillByDefault([&](const AudioDataValue*, size_t aFrames, TrackRate aRate,
+                         uint32_t, uint32_t aAlreadyBuffered) {
+        if (!audioStart) {
+          fallbackIterationCount = Some(graph->IterationCount());
+          audioStart = Some(graph->StateComputedTime());
+          alreadyBuffered = Some(aAlreadyBuffered);
+        }
+        EXPECT_NEAR(inputFrameCount,
+                    static_cast<int64_t>(graph->StateComputedTime() -
+                                         *audioStart + *alreadyBuffered),
+                    WEBAUDIO_BLOCK_SIZE)
+            << "Input should be behind state time, due to the delayed start. "
+               "stateComputedTime="
+            << graph->StateComputedTime() << ", audioStartTime=" << *audioStart
+            << ", alreadyBuffered=" << *alreadyBuffered;
+        inputFrameCount += aFrames;
+      });
+  ON_CALL(*graph, NotifyOutputData)
+      .WillByDefault([&](AudioDataValue*, size_t aFrames, TrackRate aRate,
+                         uint32_t) { outputFrameCount += aFrames; });
+
+  driver = MakeRefPtr<AudioCallbackDriver>(graph, nullptr, aRate, 2, 2, nullptr,
+                                           (void*)1, AudioInputType::Voice);
+  EXPECT_FALSE(driver->ThreadRunning()) << "Verify thread is not running";
+  EXPECT_FALSE(driver->IsStarted()) << "Verify thread is not started";
+
+  graph->SetCurrentDriver(driver);
+  graph->SetEnsureNextIteration(true);
+
+  driver->Start();
+  RefPtr<SmartMockCubebStream> stream = WaitFor(cubeb->StreamInitEvent());
+  // Wait for at least 100ms of audio data.
+  WaitUntil(stream->FramesProcessedEvent(), [&](uint32_t aFrames) {
+    processedFrameCount += aFrames;
+    return processedFrameCount >= aRate / 10;
+  });
+
+  // This will block untill all events have been executed.
+  graph->StopIterating();
+  MOZ_KnownLive(driver)->Shutdown();
+
+  const uint32_t tenMillis = aRate / 100;
+  EXPECT_EQ(inputFrameCount, outputFrameCount);
+  // Margin (50ms) is there to cover (fake) audio callbacks that occur while
+  // waiting for fallback driver's next iteration to detect that it shall stop.
+  EXPECT_GE(*audioStart, MediaTrackGraphImpl::RoundUpToEndOfAudioBlock(
+                             startDelay.ToSeconds() * aRate - tenMillis * 5))
+      << "Fallback driver runs for at least the duration of the start delay";
+  // Margin (150ms) is generous because on some try machines the
+  // SystemClockDriver can take upwards of 100ms (should be 10ms) between
+  // iterations.
+  EXPECT_LE(*audioStart, MediaTrackGraphImpl::RoundUpToEndOfAudioBlock(
+                             startDelay.ToSeconds() * aRate + tenMillis * 15))
+      << "Fallback driver does not run significantly longer than the duration "
+         "of the start delay";
+  EXPECT_NEAR(graph->StateComputedTime() - *audioStart,
+              inputFrameCount + *alreadyBuffered, WEBAUDIO_BLOCK_SIZE)
+      << "Graph progresses while audio driver runs. stateComputedTime="
+      << graph->StateComputedTime() << ", inputFrameCount=" << inputFrameCount;
+  EXPECT_GE(*fallbackIterationCount, 3U)
+      << "Fallback driver runs continuously while starting audio driver";
+  EXPECT_LE(*fallbackIterationCount,
+            static_cast<float>(*audioStart) / tenMillis)
+      << "Fallback driver does not iterate more frequently than every 10ms";
+}
+
+TEST(TestAudioCallbackDriver, SlowStart)
+MOZ_CAN_RUN_SCRIPT_FOR_DEFINITION {
+  TestSlowStart(1000);   // 10ms = 10 <<< 128 samples
+  TestSlowStart(8000);   // 10ms = 80  <  128 samples
+  TestSlowStart(44100);  // 10ms = 441 >  128 samples
 }
