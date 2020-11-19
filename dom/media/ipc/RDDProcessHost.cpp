42856cc5e0371322f7c031f4bfa5b7f9a74e416e
Jean-Yves Avenard <jyavenard@mozilla.com>
2020-11-19 01:21:05 +0000

    Bug 1518344 - P3. Launch the RDD process fully asynchronously. r=jld,mjf,bryce
    And clarify which thread a method is called from, ensure all members are only ever accessed on the main thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96668

--- dom/media/ipc/RDDProcessHost.cpp
+++ dom/media/ipc/RDDProcessHost.cpp
@@ -5,12 +5,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "RDDProcessHost.h"
 
+#include "ProcessUtils.h"
+#include "RDDChild.h"
 #include "chrome/common/process_watcher.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/StaticPrefs_media.h"
-
-#include "ProcessUtils.h"
-#include "RDDChild.h"
 
 #if defined(XP_MACOSX) && defined(MOZ_SANDBOX)
 #  include "mozilla/Sandbox.h"
@@ -27,11 +26,7 @@ bool RDDProcessHost::sLaunchWithMacSandbox = false;
 RDDProcessHost::RDDProcessHost(Listener* aListener)
     : GeckoChildProcessHost(GeckoProcessType_RDD),
       mListener(aListener),
-      mTaskFactory(this),
-      mLaunchPhase(LaunchPhase::Unlaunched),
-      mProcessToken(0),
-      mShutdownRequested(false),
-      mChannelClosed(false) {
+      mLiveToken(new media::Refcountable<bool>(true)) {
   MOZ_COUNT_CTOR(RDDProcessHost);
 
 #if defined(XP_MACOSX) && defined(MOZ_SANDBOX)
@@ -45,6 +40,8 @@ RDDProcessHost::RDDProcessHost(Listener* aListener)
 RDDProcessHost::~RDDProcessHost() { MOZ_COUNT_DTOR(RDDProcessHost); }
 
 bool RDDProcessHost::Launch(StringVector aExtraOpts) {
+  MOZ_ASSERT(NS_IsMainThread());
+
   MOZ_ASSERT(mLaunchPhase == LaunchPhase::Unlaunched);
   MOZ_ASSERT(!mRDDChild);
 
@@ -61,19 +58,6 @@ bool RDDProcessHost::Launch(StringVector aExtraOpts) {
   mLaunchPhase = LaunchPhase::Waiting;
   mLaunchTime = TimeStamp::Now();
 
-  if (!GeckoChildProcessHost::AsyncLaunch(aExtraOpts)) {
-    mLaunchPhase = LaunchPhase::Complete;
-    mPrefSerializer = nullptr;
-    return false;
-  }
-  return true;
-}
-
-bool RDDProcessHost::WaitForLaunch() {
-  if (mLaunchPhase == LaunchPhase::Complete) {
-    return !!mRDDChild;
-  }
-
   int32_t timeoutMs = StaticPrefs::media_rdd_process_startup_timeout_ms();
 
   // If one of the following environment variables are set we can
@@ -83,13 +67,62 @@ bool RDDProcessHost::WaitForLaunch() {
       PR_GetEnv("MOZ_DEBUG_CHILD_PAUSE")) {
     timeoutMs = 0;
   }
+  if (timeoutMs) {
+    // We queue a delayed task. If that task runs before the
+    // WhenProcessHandleReady promise gets resolved, we will abort the launch.
+    GetMainThreadSerialEventTarget()->DelayedDispatch(
+        NS_NewRunnableFunction(
+            "RDDProcessHost::Launchtimeout",
+            [this, liveToken = mLiveToken]() {
+              if (!*liveToken || mTimerChecked) {
+                // We have been deleted or the runnable has already started, we
+                // can abort.
+                return;
+              }
+              InitAfterConnect(false);
+              MOZ_ASSERT(mTimerChecked,
+                         "InitAfterConnect must have acted on the promise");
+            }),
+        timeoutMs);
+  }
 
-  // Our caller expects the connection to be finished after we return, so we
-  // immediately set up the IPDL actor and fire callbacks. The IO thread will
-  // still dispatch a notification to the main thread - we'll just ignore it.
-  bool result = GeckoChildProcessHost::WaitUntilConnected(timeoutMs);
-  InitAfterConnect(result);
-  return result;
+  if (!GeckoChildProcessHost::AsyncLaunch(aExtraOpts)) {
+    mLaunchPhase = LaunchPhase::Complete;
+    mPrefSerializer = nullptr;
+    return false;
+  }
+  return true;
+}
+
+RefPtr<GenericNonExclusivePromise> RDDProcessHost::LaunchPromise() {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (mLaunchPromise) {
+    return mLaunchPromise;
+  }
+  mLaunchPromise = MakeRefPtr<GenericNonExclusivePromise::Private>(__func__);
+  WhenProcessHandleReady()->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [this, liveToken = mLiveToken](
+          const ipc::ProcessHandlePromise::ResolveOrRejectValue& aResult) {
+        if (!*liveToken) {
+          // The RDDProcessHost got deleted. Abort. The promise would have
+          // already been rejected.
+          return;
+        }
+        if (mTimerChecked) {
+          // We hit the timeout earlier, abort.
+          return;
+        }
+        mTimerChecked = true;
+        if (aResult.IsReject()) {
+          RejectPromise();
+        }
+        // If aResult.IsResolve() then we have succeeded in launching the
+        // RDD process. The promise will be resolved once the channel has
+        // connected (or failed to) later.
+      });
+  return mLaunchPromise;
 }
 
 void RDDProcessHost::OnChannelConnected(int32_t peer_pid) {
@@ -97,15 +130,12 @@ void RDDProcessHost::OnChannelConnected(int32_t peer_pid) {
 
   GeckoChildProcessHost::OnChannelConnected(peer_pid);
 
-  // Post a task to the main thread. Take the lock because mTaskFactory is not
-  // thread-safe.
-  RefPtr<Runnable> runnable;
-  {
-    MonitorAutoLock lock(mMonitor);
-    runnable =
-        mTaskFactory.NewRunnableMethod(&RDDProcessHost::OnChannelConnectedTask);
-  }
-  NS_DispatchToMainThread(runnable);
+  NS_DispatchToMainThread(NS_NewRunnableFunction(
+      "RDDProcessHost::OnChannelConnected", [this, liveToken = mLiveToken]() {
+        if (*liveToken && mLaunchPhase == LaunchPhase::Waiting) {
+          InitAfterConnect(true);
+        }
+      }));
 }
 
 void RDDProcessHost::OnChannelError() {
@@ -113,71 +143,59 @@ void RDDProcessHost::OnChannelError() {
 
   GeckoChildProcessHost::OnChannelError();
 
-  // Post a task to the main thread. Take the lock because mTaskFactory is not
-  // thread-safe.
-  RefPtr<Runnable> runnable;
-  {
-    MonitorAutoLock lock(mMonitor);
-    runnable =
-        mTaskFactory.NewRunnableMethod(&RDDProcessHost::OnChannelErrorTask);
-  }
-  NS_DispatchToMainThread(runnable);
-}
-
-void RDDProcessHost::OnChannelConnectedTask() {
-  if (mLaunchPhase == LaunchPhase::Waiting) {
-    InitAfterConnect(true);
-  }
-}
-
-void RDDProcessHost::OnChannelErrorTask() {
-  if (mLaunchPhase == LaunchPhase::Waiting) {
-    InitAfterConnect(false);
-  }
+  NS_DispatchToMainThread(NS_NewRunnableFunction(
+      "RDDProcessHost::OnChannelError", [this, liveToken = mLiveToken]() {
+        if (*liveToken && mLaunchPhase == LaunchPhase::Waiting) {
+          InitAfterConnect(false);
+        }
+      }));
 }
 
 static uint64_t sRDDProcessTokenCounter = 0;
 
 void RDDProcessHost::InitAfterConnect(bool aSucceeded) {
+  MOZ_ASSERT(NS_IsMainThread());
+
   MOZ_ASSERT(mLaunchPhase == LaunchPhase::Waiting);
   MOZ_ASSERT(!mRDDChild);
 
   mLaunchPhase = LaunchPhase::Complete;
 
-  if (aSucceeded) {
-    mProcessToken = ++sRDDProcessTokenCounter;
-    mRDDChild = MakeUnique<RDDChild>(this);
-    DebugOnly<bool> rv = mRDDChild->Open(
-        TakeChannel(), base::GetProcId(GetChildProcessHandle()));
-    MOZ_ASSERT(rv);
-
-    // Only clear mPrefSerializer in the success case to avoid a
-    // possible race in the case case of a timeout on Windows launch.
-    // See Bug 1555076 comment 7:
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1555076#c7
-    mPrefSerializer = nullptr;
-
-    if (!mRDDChild->Init()) {
-      // Can't just kill here because it will create a timing race that
-      // will crash the tab. We don't really want to crash the tab just
-      // because RDD linux sandbox failed to initialize.  In this case,
-      // we'll close the child channel which will cause the RDD process
-      // to shutdown nicely avoiding the tab crash (which manifests as
-      // Bug 1535335).
-      mRDDChild->Close();
-      return;
-    }
+  if (!aSucceeded) {
+    RejectPromise();
+    return;
   }
+  mProcessToken = ++sRDDProcessTokenCounter;
+  mRDDChild = MakeUnique<RDDChild>(this);
+  DebugOnly<bool> rv =
+      mRDDChild->Open(TakeChannel(), base::GetProcId(GetChildProcessHandle()));
+  MOZ_ASSERT(rv);
 
-  if (mListener) {
-    mListener->OnProcessLaunchComplete(this);
+  // Only clear mPrefSerializer in the success case to avoid a
+  // possible race in the case case of a timeout on Windows launch.
+  // See Bug 1555076 comment 7:
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1555076#c7
+  mPrefSerializer = nullptr;
+
+  if (!mRDDChild->Init()) {
+    // Can't just kill here because it will create a timing race that
+    // will crash the tab. We don't really want to crash the tab just
+    // because RDD linux sandbox failed to initialize.  In this case,
+    // we'll close the child channel which will cause the RDD process
+    // to shutdown nicely avoiding the tab crash (which manifests as
+    // Bug 1535335).
+    mRDDChild->Close();
+    RejectPromise();
+  } else {
+    ResolvePromise();
   }
 }
 
 void RDDProcessHost::Shutdown() {
+  MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(!mShutdownRequested);
 
-  mListener = nullptr;
+  RejectPromise();
 
   if (mRDDChild) {
     // OnChannelClosed uses this to check if the shutdown was expected or
@@ -208,7 +226,10 @@ void RDDProcessHost::Shutdown() {
 }
 
 void RDDProcessHost::OnChannelClosed() {
+  MOZ_ASSERT(NS_IsMainThread());
+
   mChannelClosed = true;
+  RejectPromise();
 
   if (!mShutdownRequested && mListener) {
     // This is an unclean shutdown. Notify our listener that we're going away.
@@ -219,10 +240,11 @@ void RDDProcessHost::OnChannelClosed() {
 
   // Release the actor.
   RDDChild::Destroy(std::move(mRDDChild));
-  MOZ_ASSERT(!mRDDChild);
 }
 
 void RDDProcessHost::KillHard(const char* aReason) {
+  MOZ_ASSERT(NS_IsMainThread());
+
   ProcessHandle handle = GetChildProcessHandle();
   if (!base::KillProcess(handle, base::PROCESS_END_KILLED_BY_USER, false)) {
     NS_WARNING("failed to kill subprocess!");
@@ -231,20 +253,44 @@ void RDDProcessHost::KillHard(const char* aReason) {
   SetAlreadyDead();
 }
 
-uint64_t RDDProcessHost::GetProcessToken() const { return mProcessToken; }
-
-void RDDProcessHost::KillProcess() { KillHard("DiagnosticKill"); }
+uint64_t RDDProcessHost::GetProcessToken() const {
+  MOZ_ASSERT(NS_IsMainThread());
+  return mProcessToken;
+}
 
 void RDDProcessHost::DestroyProcess() {
-  // Cancel all tasks. We don't want anything triggering after our caller
-  // expects this to go away.
-  {
-    MonitorAutoLock lock(mMonitor);
-    mTaskFactory.RevokeAll();
-  }
+  MOZ_ASSERT(NS_IsMainThread());
+  RejectPromise();
 
-  GetCurrentSerialEventTarget()->Dispatch(
+  // Any pending tasks will be cancelled from now on.
+  *mLiveToken = false;
+
+  NS_DispatchToMainThread(
       NS_NewRunnableFunction("DestroyProcessRunnable", [this] { Destroy(); }));
+}
+
+void RDDProcessHost::ResolvePromise() {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!mLaunchPromiseSettled) {
+    mLaunchPromise->Resolve(true, __func__);
+    mLaunchPromiseSettled = true;
+  }
+  // We have already acted on the promise; the timeout runnable no longer needs
+  // to interrupt anything.
+  mTimerChecked = true;
+}
+
+void RDDProcessHost::RejectPromise() {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  if (!mLaunchPromiseSettled) {
+    mLaunchPromise->Reject(NS_ERROR_FAILURE, __func__);
+    mLaunchPromiseSettled = true;
+  }
+  // We have already acted on the promise; the timeout runnable no longer needs
+  // to interrupt anything.
+  mTimerChecked = true;
 }
 
 #if defined(XP_MACOSX) && defined(MOZ_SANDBOX)
