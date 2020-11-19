186b90e304f94e2ccca714d0442643ba0c5c6938
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-11-20 00:12:10 +0200

    Backed out changeset 189c8d74af36 (bug 1664916) for bc failure on browser_Troubleshoot.js. CLOSED TREE

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -76,9 +76,6 @@ var snapshotFormatters = {
     $("application-box").textContent = data.name;
     $("useragent-box").textContent = data.userAgent;
     $("os-box").textContent = data.osVersion;
-    if (AppConstants.platform == "macosx") {
-      $("rosetta-box").textContent = data.rosetta;
-    }
     $("binary-box").textContent = Services.dirsvc.get(
       "XREExeF",
       Ci.nsIFile
