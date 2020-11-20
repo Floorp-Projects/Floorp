7dd11f8af6d80ea87731de64993d11d783c9de8b
Mike Hommey <mh+mozilla@glandium.org>
2020-11-20 00:03:13 +0000

    Bug 1678154 - Pack the mac OS SDK as a private artifact. r=firefox-build-system-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D97694

---
+++ taskcluster/scripts/misc/pack.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -x
+
+[ -z "$1" ] && echo Missing argument && exit 1
+
+dir=$(dirname "$1")
+name=$(basename "$1")
+
+case "$(uname -s)" in
+Darwin)
+    TAR_FLAGS=--no-fflags
+    ;;
+*)
+    TAR_FLAGS=
+    ;;
+esac
+
+(cd "$dir"; find "$name"/* -not -type d -print0 | tar $TAR_FLAGS -cf - --null -T -) | python3 $GECKO_PATH/taskcluster/scripts/misc/zstdpy > "$name.tar.zst"
+
+mkdir -p "$UPLOAD_DIR"
+mv "$name.tar.zst" "$UPLOAD_DIR"
