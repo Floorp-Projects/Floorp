556c9253a7c770194bef1438387f1e337a2abe16
Simon Giesecke <sgiesecke@mozilla.com>
2020-11-23 16:12:50 +0000

    Bug 1676357 - Hide dependencies of CompositableForwarder. r=mattwoodrow
    Differential Revision: https://phabricator.services.mozilla.com/D97740
    
    Depends on D97739

--- gfx/layers/ipc/CompositableForwarder.h
+++ gfx/layers/ipc/CompositableForwarder.h
@@ -7,30 +7,25 @@
 #ifndef MOZILLA_LAYERS_COMPOSITABLEFORWARDER
 #define MOZILLA_LAYERS_COMPOSITABLEFORWARDER
 
-#include <stdint.h>  // for int32_t, uint64_t
-#include "gfxTypes.h"
-#include "mozilla/Attributes.h"  // for override
-#include "mozilla/UniquePtr.h"
-#include "mozilla/layers/CompositableClient.h"  // for CompositableClient
-#include "mozilla/layers/CompositorTypes.h"
-#include "mozilla/layers/ISurfaceAllocator.h"  // for ISurfaceAllocator
-#include "mozilla/layers/LayersTypes.h"        // for LayersBackend
-#include "mozilla/layers/TextureClient.h"      // for TextureClient
-#include "mozilla/layers/TextureForwarder.h"   // for TextureForwarder
-#include "nsRegion.h"                          // for nsIntRegion
-#include "mozilla/gfx/Rect.h"
-#include "nsHashKeys.h"
-#include "nsTHashtable.h"
+#include <stdint.h>  // for int32_t, uint32_t, uint64_t
+#include "mozilla/Assertions.h"  // for AssertionConditionType, MOZ_ASSERT, MOZ_ASSERT_HELPER1
+#include "mozilla/RefPtr.h"                  // for RefPtr
+#include "mozilla/TimeStamp.h"               // for TimeStamp
+#include "mozilla/layers/KnowsCompositor.h"  // for KnowsCompositor
+#include "nsRect.h"                          // for nsIntRect
+#include "nsRegion.h"                        // for nsIntRegion
+#include "nsTArray.h"                        // for nsTArray
 
 namespace mozilla {
 namespace layers {
-
 class CompositableClient;
+class CompositableHandle;
 class ImageContainer;
-class SurfaceDescriptor;
-class SurfaceDescriptorTiles;
-class ThebesBufferData;
 class PTextureChild;
+class ShadowLayerForwarder;
+class SurfaceDescriptorTiles;
+class TextureClient;
+class ThebesBufferData;
 
 /**
  * A transaction is a set of changes that happenned on the content side, that
@@ -48,6 +43,9 @@ class PTextureChild;
  */
 class CompositableForwarder : public KnowsCompositor {
  public:
+  CompositableForwarder();
+  ~CompositableForwarder();
+
   /**
    * Setup the IPDL actor for aCompositable to be part of layers
    * transactions.
