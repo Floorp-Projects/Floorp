0d0dc5c19adeb267b3b244db353928bd39c249a7
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-11-25 01:28:12 +0200

    Backed out changeset 1f42c376724d (bug 1645528) for leakcheck failures on nsTArray. CLOSED TREE

--- layout/ipc/VsyncChild.cpp
+++ layout/ipc/VsyncChild.cpp
@@ -10,18 +10,17 @@
 #include "mozilla/VsyncDispatcher.h"
 #include "nsThreadUtils.h"
 
-namespace mozilla::dom {
+namespace mozilla::layout {
 
 VsyncChild::VsyncChild()
-    : mIsShutdown(false),
-      mVsyncRate(TimeDuration::Forever()),
-      lastVsyncRateTime(TimeStamp()) {
+    : mObservingVsync(false),
+      mIsShutdown(false),
+      mVsyncRate(TimeDuration::Forever()) {
   MOZ_ASSERT(NS_IsMainThread());
 }
 
 VsyncChild::~VsyncChild() { MOZ_ASSERT(NS_IsMainThread()); }
 
-/* do not delete yet so the file history is preserved
 bool VsyncChild::SendObserve() {
   MOZ_ASSERT(NS_IsMainThread());
   if (!mObservingVsync && !mIsShutdown) {
@@ -39,42 +38,12 @@ bool VsyncChild::SendUnobserve() {
   }
   return true;
 }
-*/
-
-void VsyncChild::AddChildRefreshTimer(VsyncObserver* aVsyncObserver) {
-  MOZ_ASSERT(NS_IsMainThread());
-  MOZ_ASSERT(!mObservers.Contains(aVsyncObserver));
-
-  if (mIsShutdown) {
-    return;
-  }
-
-  if (mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendObserve();
-  }
-  mObservers.AppendElement(std::move(aVsyncObserver));
-}
-
-void VsyncChild::RemoveChildRefreshTimer(VsyncObserver* aVsyncObserver) {
-  MOZ_ASSERT(NS_IsMainThread());
-  if (mIsShutdown) {
-    return;
-  }
-
-  if (mObservers.RemoveElement(aVsyncObserver) && mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendUnobserve();
-  }
-}
 
 void VsyncChild::ActorDestroy(ActorDestroyReason aActorDestroyReason) {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(!mIsShutdown);
   mIsShutdown = true;
-
-  if (!mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendUnobserve();
-  }
-  mObservers.Clear();
+  mObserver = nullptr;
 }
 
 mozilla::ipc::IPCResult VsyncChild::RecvNotify(const VsyncEvent& aVsync) {
@@ -82,26 +51,30 @@ mozilla::ipc::IPCResult VsyncChild::RecvNotify(const VsyncEvent& aVsync) {
   MOZ_ASSERT(!mIsShutdown);
 
   SchedulerGroup::MarkVsyncRan();
-
-  for (VsyncObserver* observer : mObservers.ForwardRange()) {
-    observer->NotifyVsync(aVsync);
+  if (mObservingVsync && mObserver) {
+    mObserver->NotifyVsync(aVsync);
   }
   return IPC_OK();
 }
 
+void VsyncChild::SetVsyncObserver(VsyncObserver* aVsyncObserver) {
+  MOZ_ASSERT(NS_IsMainThread());
+  mObserver = aVsyncObserver;
+}
+
 TimeDuration VsyncChild::GetVsyncRate() {
-  // Throttle vsync rate requests to avoid unnecessary IPC
-  if (lastVsyncRateTime.IsNull() ||
-      (TimeStamp::Now() - lastVsyncRateTime).ToMilliseconds() > 250) {
+  if (mVsyncRate == TimeDuration::Forever()) {
     PVsyncChild::SendRequestVsyncRate();
-    lastVsyncRateTime = TimeStamp::Now();
   }
+
   return mVsyncRate;
 }
+
+TimeDuration VsyncChild::VsyncRate() { return mVsyncRate; }
 
 mozilla::ipc::IPCResult VsyncChild::RecvVsyncRate(const float& aVsyncRate) {
   mVsyncRate = TimeDuration::FromMilliseconds(aVsyncRate);
   return IPC_OK();
 }
 
-}  // namespace mozilla::dom
+}  // namespace mozilla::layout
