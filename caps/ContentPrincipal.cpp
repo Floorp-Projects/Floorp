a1c2ca7af286f23deea08f36056273a50bd2caa5
Christoph Kerschbaumer <ckerschb@christophkerschbaumer.com>
2020-11-24 09:04:56 +0000

    Bug 1678310: Expose siteOriginNoSuffix on nsIPrincipal r=baku
    Differential Revision: https://phabricator.services.mozilla.com/D97605

--- caps/ContentPrincipal.cpp
+++ caps/ContentPrincipal.cpp
@@ -440,7 +440,7 @@ ContentPrincipal::GetBaseDomain(nsACString& aBaseDomain) {
 }
 
 NS_IMETHODIMP
-ContentPrincipal::GetSiteOrigin(nsACString& aSiteOrigin) {
+ContentPrincipal::GetSiteOriginNoSuffix(nsACString& aSiteOrigin) {
   // Handle some special URIs first.
   nsAutoCString baseDomain;
   bool handled;
@@ -450,7 +450,7 @@ ContentPrincipal::GetSiteOrigin(nsACString& aSiteOrigin) {
   if (handled) {
     // This is a special URI ("file:", "about:", "view-source:", etc). Just
     // return the origin.
-    return GetOrigin(aSiteOrigin);
+    return GetSiteOriginNoSuffix(aSiteOrigin);
   }
 
   // For everything else, we ask the TLD service. Note that, unlike in
@@ -491,15 +491,7 @@ ContentPrincipal::GetSiteOrigin(nsACString& aSiteOrigin) {
 
   rv = GenerateOriginNoSuffixFromURI(siteUri, aSiteOrigin);
   MOZ_ASSERT(NS_SUCCEEDED(rv), "failed to create siteOriginNoSuffix");
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  nsAutoCString suffix;
-  rv = GetOriginSuffix(suffix);
-  MOZ_ASSERT(NS_SUCCEEDED(rv), "failed to create suffix");
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  aSiteOrigin.Append(suffix);
-  return NS_OK;
+  return rv;
 }
 
 nsresult ContentPrincipal::GetSiteIdentifier(SiteIdentifier& aSite) {
