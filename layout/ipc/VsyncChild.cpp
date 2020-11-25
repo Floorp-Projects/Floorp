8d8f6d1fccd2656b5c79de6a5aaa65aa99b66afa
Narcis Beleuzu <nbeleuzu@mozilla.com>
2020-11-25 02:59:47 +0200

    Backed out 2 changesets (bug 1645528) for leakcheck failures on nsTArray. CLOSED TREE
    Backed out changeset df3577321bfe (bug 1645528)
    Backed out changeset fbc13c3ea551 (bug 1645528)

--- layout/ipc/VsyncChild.cpp
+++ layout/ipc/VsyncChild.cpp
@@ -10,51 +10,40 @@
 #include "mozilla/VsyncDispatcher.h"
 #include "nsThreadUtils.h"
 
-namespace mozilla::dom {
+namespace mozilla::layout {
 
 VsyncChild::VsyncChild()
-    : mIsShutdown(false),
-      mVsyncRate(TimeDuration::Forever()),
-      lastVsyncRateTime(TimeStamp()) {
+    : mObservingVsync(false),
+      mIsShutdown(false),
+      mVsyncRate(TimeDuration::Forever()) {
   MOZ_ASSERT(NS_IsMainThread());
 }
 
 VsyncChild::~VsyncChild() { MOZ_ASSERT(NS_IsMainThread()); }
 
-void VsyncChild::AddChildRefreshTimer(VsyncObserver* aVsyncObserver) {
+bool VsyncChild::SendObserve() {
   MOZ_ASSERT(NS_IsMainThread());
-  MOZ_ASSERT(!mObservers.Contains(aVsyncObserver));
-
-  if (mIsShutdown) {
-    return;
+  if (!mObservingVsync && !mIsShutdown) {
+    mObservingVsync = true;
+    PVsyncChild::SendObserve();
   }
-
-  if (mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendObserve();
-  }
-  mObservers.AppendElement(std::move(aVsyncObserver));
+  return true;
 }
 
-void VsyncChild::RemoveChildRefreshTimer(VsyncObserver* aVsyncObserver) {
+bool VsyncChild::SendUnobserve() {
   MOZ_ASSERT(NS_IsMainThread());
-  if (mIsShutdown) {
-    return;
+  if (mObservingVsync && !mIsShutdown) {
+    mObservingVsync = false;
+    PVsyncChild::SendUnobserve();
   }
-
-  if (mObservers.RemoveElement(aVsyncObserver) && mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendUnobserve();
-  }
+  return true;
 }
 
 void VsyncChild::ActorDestroy(ActorDestroyReason aActorDestroyReason) {
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(!mIsShutdown);
   mIsShutdown = true;
-
-  if (!mObservers.IsEmpty()) {
-    Unused << PVsyncChild::SendUnobserve();
-  }
-  mObservers.Clear();
+  mObserver = nullptr;
 }
 
 mozilla::ipc::IPCResult VsyncChild::RecvNotify(const VsyncEvent& aVsync) {
@@ -62,26 +51,30 @@ mozilla::ipc::IPCResult VsyncChild::RecvNotify(const VsyncEvent& aVsync) {
   MOZ_ASSERT(!mIsShutdown);
 
   SchedulerGroup::MarkVsyncRan();
-
-  for (VsyncObserver* observer : mObservers.ForwardRange()) {
-    observer->NotifyVsync(aVsync);
+  if (mObservingVsync && mObserver) {
+    mObserver->NotifyVsync(aVsync);
   }
   return IPC_OK();
 }
 
+void VsyncChild::SetVsyncObserver(VsyncObserver* aVsyncObserver) {
+  MOZ_ASSERT(NS_IsMainThread());
+  mObserver = aVsyncObserver;
+}
+
 TimeDuration VsyncChild::GetVsyncRate() {
-  // Throttle vsync rate requests to avoid unnecessary IPC
-  if (lastVsyncRateTime.IsNull() ||
-      (TimeStamp::Now() - lastVsyncRateTime).ToMilliseconds() > 250) {
+  if (mVsyncRate == TimeDuration::Forever()) {
     PVsyncChild::SendRequestVsyncRate();
-    lastVsyncRateTime = TimeStamp::Now();
   }
+
   return mVsyncRate;
 }
+
+TimeDuration VsyncChild::VsyncRate() { return mVsyncRate; }
 
 mozilla::ipc::IPCResult VsyncChild::RecvVsyncRate(const float& aVsyncRate) {
   mVsyncRate = TimeDuration::FromMilliseconds(aVsyncRate);
   return IPC_OK();
 }
 
-}  // namespace mozilla::dom
+}  // namespace mozilla::layout
