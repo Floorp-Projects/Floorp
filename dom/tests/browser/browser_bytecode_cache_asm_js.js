10bba404771ef3d9d706f7f8b0a7879689a41e90
Tooru Fujisawa <arai_a@mac.com>
2020-11-26 20:08:59 +0000

    Bug 1678243 - Clear exception set by JS::FinishIncrementalEncoding. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D98015

---
+++ dom/tests/browser/browser_bytecode_cache_asm_js.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const PAGE_URL =
+  "http://example.com/browser/dom/tests/browser/page_bytecode_cache_asm_js.html";
+
+add_task(async function() {
+  // Eagerly generate bytecode cache.
+  await SpecialPowers.pushPrefEnv({
+    set: [
+      ["dom.script_loader.bytecode_cache.enabled", true],
+      ["dom.script_loader.bytecode_cache.strategy", -1],
+    ],
+  });
+
+  await BrowserTestUtils.withNewTab(
+    {
+      gBrowser,
+      url: PAGE_URL,
+      waitForLoad: true,
+    },
+    async browser => {
+      let result = await SpecialPowers.spawn(browser, [], () => {
+        return content.document.getElementById("result").textContent;
+      });
+      // No error shoud be caught by content.
+      is(result, "ok");
+    }
+  );
+
+  await SpecialPowers.popPrefEnv();
+});
