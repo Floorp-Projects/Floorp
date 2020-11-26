bd2a62766d22e85b21f5f2480e67e067f77fce86
Robert Mader <robert.mader@posteo.de>
2020-11-26 19:15:04 +0000

    Bug 1645528 - Connect nsRefreshDrivers in content processes with a widget-local vsync source r=mattwoodrow,emilio
    To allow `requestAnimationFrame()` and similar things to run at monitor
    speed if there is only a window-specific vsyncsource available.
    This is the case for Wayland and, in the future, EGL/X11. Other backends
    may opt for window specific vsyncsources as well at some point.
    
    The idea is to, instead of using global vsync objects, expose a vsyncsource
    from nsWindow and use it for refresh drivers. For the content process, move
    VsyncChild to BrowserChild, so for each Browserchild there is only one
    VsyncChild to which all refresh drivers connect.
    
    IPC in managed either by PBrowser or PBackground. Right now, PBrowser is
    only used on Wayland, as both PBrowser and the Wayland vsyncsource run
    on the main thread. Other backends keep using the background thread for
    now.
    
    While at it, make it so that we constantly update the refresh rate. This
    is necessary for Wayland, but also on other platforms variable refresh rates
    are increasingly common. When using PVsync, limit updates to once in every
    250ms in order to minimize overhead while still updating fast.
    
    How to test:
     - run the Wayland backend
     - enable `widget.wayland_vsync.enabled`
     - optionally: disable `privacy.reduceTimerPrecision`
     - run `vsynctester.com` or `testufo.com`
    
    Expected results:
    Instead of fixed 60Hz, things should update at monitor refresh rate -
    e.g. 144Hz
    
    Original patch by Kenny Levinsen.
    
    Differential Revision: https://phabricator.services.mozilla.com/D93173

--- dom/ipc/BrowserParent.h
+++ dom/ipc/BrowserParent.h
@@ -18,6 +18,7 @@
 #include "mozilla/dom/BrowserBridgeParent.h"
 #include "mozilla/dom/PBrowserParent.h"
 #include "mozilla/dom/TabContext.h"
+#include "mozilla/dom/VsyncParent.h"
 #include "mozilla/dom/ipc/IdType.h"
 #include "mozilla/layout/RemoteLayerTreeOwner.h"
 #include "nsCOMPtr.h"
@@ -460,6 +461,10 @@ class BrowserParent final : public PBrowserParent,
 
   bool DeallocPColorPickerParent(PColorPickerParent* aColorPicker);
 
+  PVsyncParent* AllocPVsyncParent();
+
+  bool DeallocPVsyncParent(PVsyncParent* aActor);
+
 #ifdef ACCESSIBILITY
   PDocAccessibleParent* AllocPDocAccessibleParent(PDocAccessibleParent*,
                                                   const uint64_t&,
@@ -872,6 +877,8 @@ class BrowserParent final : public PBrowserParent,
   void SendRealTouchMoveEvent(WidgetTouchEvent& aEvent, APZData& aAPZData,
                               uint32_t aConsecutiveTouchMoveCount);
 
+  void UpdateVsyncParentVsyncSource();
+
  public:
   // Unsets sTopLevelWebFocus regardless of its current value.
   static void UnsetTopLevelWebFocusAll();
@@ -956,6 +963,8 @@ class BrowserParent final : public PBrowserParent,
   uint32_t mCustomCursorHotspotX, mCustomCursorHotspotY;
 
   nsTArray<nsString> mVerifyDropLinks;
+
+  RefPtr<VsyncParent> mVsyncParent;
 
 #ifdef DEBUG
   int32_t mActiveSupressDisplayportCount = 0;
