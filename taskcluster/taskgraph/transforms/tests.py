a984c9d503a7550b9d195cc805ed31be499eecd1
Andrew Halberstadt <ahal@pm.me>
2020-11-26 17:42:27 +0000

    Bug 1672181 - [taskgraph] Make test tasks py3 by default and add a py2 override r=bc,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D97669

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -571,6 +571,8 @@ test_description_schema = Schema(
         Optional("fetches"): {
             text_type: optionally_keyed_by("test-platform", [text_type])
         },
+        # Use Python 2 rather than Python 3.
+        Optional("force-py2"): bool,
     }
 )
 
@@ -640,6 +642,7 @@ def set_defaults(config, tasks):
         task.setdefault("limit-platforms", [])
         task.setdefault("docker-image", {"in-tree": "ubuntu1804-test"})
         task.setdefault("checkout", False)
+        task.setdefault("force-py2", False)
         task.setdefault("require-signed-extensions", False)
         task.setdefault("variants", [])
 
@@ -1900,6 +1903,7 @@ def make_job_description(config, tasks):
         run = jobdesc["run"] = {}
         run["using"] = "mozharness-test"
         run["test"] = task
+        run["force-py2"] = task.pop("force-py2", False)
 
         if "workdir" in task:
             run["workdir"] = task.pop("workdir")
