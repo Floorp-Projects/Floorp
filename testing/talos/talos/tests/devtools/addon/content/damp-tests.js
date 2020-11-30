88119f7f9e393815a4555859b16fe438a071db89
Julian Descottes <jdescottes@mozilla.com>
2020-11-30 13:12:22 +0000

    Bug 1574417 - [devtools] Disable netmonitor/compicated DAMP test on Fission platform r=bomsy,perftest-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D97821

--- testing/talos/talos/tests/devtools/addon/content/damp-tests.js
+++ testing/talos/talos/tests/devtools/addon/content/damp-tests.js
@@ -7,7 +7,7 @@
 /* globals module, require */
 
 const Services = require("Services");
-const isFissionEnabled = Services.prefs.getBoolPref("fission.autostart", false);
+const isFissionEnabled = Services.appinfo.fissionAutostart;
 /**
  * This is the registry for all DAMP tests. Tests will be run in the order specified by
  * the DAMP_TESTS array.
@@ -108,6 +108,7 @@ module.exports = [
     description:
       "Measure open/close toolbox on inspector panel against complicated document",
     // Bug 1503499 & 1677587 - disable for frequent failures on fission
+    // Linked to ThreadActor race conditions, see Bug 1678741.
     disabled: isFissionEnabled,
   },
   {
@@ -122,6 +123,7 @@ module.exports = [
     description:
       "Measure open/close toolbox on style editor panel against complicated document",
     // Bug 1677587 - disable for frequent failures on fission
+    // Linked to ThreadActor race conditions, see Bug 1678741.
     disabled: isFissionEnabled,
   },
   {
@@ -129,6 +131,9 @@ module.exports = [
     path: "netmonitor/complicated.js",
     description:
       "Measure open/close toolbox on network monitor panel against complicated document",
+    // Bug 1574417 - disable for frequent failures on fission.
+    // Linked to ThreadActor race conditions, see Bug 1678741.
+    disabled: isFissionEnabled,
   },
   // Run all tests against a document specific to each tool
   {
