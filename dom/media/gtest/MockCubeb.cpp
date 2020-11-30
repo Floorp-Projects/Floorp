2cc36081e51e36f9ed9e8f1b6922a456592618c4
Andreas Pehrson <apehrson@mozilla.com>
2020-11-30 14:16:18 +0000

    Bug 1605134 - Give MockCubeb users the ability to control when to start a MockCubebStream. r=padenot
    Differential Revision: https://phabricator.services.mozilla.com/D97409

--- dom/media/gtest/MockCubeb.cpp
+++ dom/media/gtest/MockCubeb.cpp
@@ -15,11 +15,14 @@ MockCubebStream::MockCubebStream(cubeb* aContext, cubeb_devid aInputDevice,
                                  cubeb_stream_params* aOutputStreamParams,
                                  cubeb_data_callback aDataCallback,
                                  cubeb_state_callback aStateCallback,
-                                 void* aUserPtr, SmartMockCubebStream* aSelf)
+                                 void* aUserPtr, SmartMockCubebStream* aSelf,
+                                 bool aFrozenStart)
     : context(aContext),
       mHasInput(aInputStreamParams),
       mHasOutput(aOutputStreamParams),
       mSelf(aSelf),
+      mFrozenStartMonitor("MockCubebStream::mFrozenStartMonitor"),
+      mFrozenStart(aFrozenStart),
       mDataCallback(aDataCallback),
       mStateCallback(aStateCallback),
       mUserPtr(aUserPtr),
@@ -45,6 +48,21 @@ MockCubebStream::~MockCubebStream() = default;
 int MockCubebStream::Start() {
   mStateCallback(AsCubebStream(), mUserPtr, CUBEB_STATE_STARTED);
   mStreamStop = false;
+  MonitorAutoLock lock(mFrozenStartMonitor);
+  if (mFrozenStart) {
+    NS_DispatchBackgroundTask(NS_NewRunnableFunction(
+        "MockCubebStream::WaitForUnfreezedStart",
+        [this, self = RefPtr<SmartMockCubebStream>(mSelf)] {
+          MonitorAutoLock lock(mFrozenStartMonitor);
+          while (mFrozenStart) {
+            mFrozenStartMonitor.Wait();
+          }
+          if (!mStreamStop) {
+            MockCubeb::AsMock(context)->StartStream(mSelf);
+          }
+        }));
+    return CUBEB_OK;
+  }
   MockCubeb::AsMock(context)->StartStream(this);
   return CUBEB_OK;
 }
@@ -92,6 +110,12 @@ void MockCubebStream::SetDriftFactor(float aDriftFactor) {
 }
 
 void MockCubebStream::ForceError() { mForceErrorState = true; }
+
+void MockCubebStream::Unfreeze() {
+  MonitorAutoLock l(mFrozenStartMonitor);
+  mFrozenStart = false;
+  mFrozenStartMonitor.Notify();
+}
 
 MediaEventSource<uint32_t>& MockCubebStream::FramesProcessedEvent() {
   return mFramesProcessedEvent;
@@ -297,6 +321,10 @@ void MockCubeb::SetSupportDeviceChangeCallback(bool aSupports) {
   mSupportsDeviceCollectionChangedCallback = aSupports;
 }
 
+void MockCubeb::SetStreamStartFreezeEnabled(bool aEnabled) {
+  mStreamStartFreezeEnabled = aEnabled;
+}
+
 int MockCubeb::StreamInit(cubeb* aContext, cubeb_stream** aStream,
                           cubeb_devid aInputDevice,
                           cubeb_stream_params* aInputStreamParams,
@@ -306,7 +334,8 @@ int MockCubeb::StreamInit(cubeb* aContext, cubeb_stream** aStream,
                           cubeb_state_callback aStateCallback, void* aUserPtr) {
   auto mockStream = MakeRefPtr<SmartMockCubebStream>(
       aContext, aInputDevice, aInputStreamParams, aOutputDevice,
-      aOutputStreamParams, aDataCallback, aStateCallback, aUserPtr);
+      aOutputStreamParams, aDataCallback, aStateCallback, aUserPtr,
+      mStreamStartFreezeEnabled);
   *aStream = mockStream->AsCubebStream();
   mStreamInitEvent.Notify(mockStream);
   // AddRef the stream to keep it alive. StreamDestroy releases it.
