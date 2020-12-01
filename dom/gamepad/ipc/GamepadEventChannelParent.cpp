0948c09d020c9ebca2030866584c00da08a47803
june wilde <jewilde@mozilla.com>
2020-12-01 19:56:00 +0000

    Bug 1610040 - Cut PGamepadEventChannel over to use Tainted<> IPC Mechanics; r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D60745

--- dom/gamepad/ipc/GamepadEventChannelParent.cpp
+++ dom/gamepad/ipc/GamepadEventChannelParent.cpp
@@ -66,12 +66,15 @@ void GamepadEventChannelParent::ActorDestroy(ActorDestroyReason aWhy) {
 }
 
 mozilla::ipc::IPCResult GamepadEventChannelParent::RecvVibrateHaptic(
-    const uint32_t& aControllerIdx, const uint32_t& aHapticIndex,
-    const double& aIntensity, const double& aDuration,
-    const uint32_t& aPromiseID) {
+    const Tainted<uint32_t>& aControllerIdx,
+    const Tainted<uint32_t>& aHapticIndex, const Tainted<double>& aIntensity,
+    const Tainted<double>& aDuration, const Tainted<uint32_t>& aPromiseID) {
   // TODO: Bug 680289, implement for standard gamepads
 
-  if (SendReplyGamepadPromise(aPromiseID)) {
+  // TODO: simplify tainted validation, see 1610570
+  if (SendReplyGamepadPromise(MOZ_NO_VALIDATE(
+          aPromiseID,
+          "This value is unused aside from being passed back to the child."))) {
     return IPC_OK();
   }
 
@@ -79,19 +82,23 @@ mozilla::ipc::IPCResult GamepadEventChannelParent::RecvVibrateHaptic(
 }
 
 mozilla::ipc::IPCResult GamepadEventChannelParent::RecvStopVibrateHaptic(
-    const uint32_t& aControllerIdx) {
+    const Tainted<uint32_t>& aControllerIdx) {
   // TODO: Bug 680289, implement for standard gamepads
   return IPC_OK();
 }
 
 mozilla::ipc::IPCResult GamepadEventChannelParent::RecvLightIndicatorColor(
-    const uint32_t& aControllerIdx, const uint32_t& aLightColorIndex,
-    const uint8_t& aRed, const uint8_t& aGreen, const uint8_t& aBlue,
-    const uint32_t& aPromiseID) {
+    const Tainted<uint32_t>& aControllerIdx,
+    const Tainted<uint32_t>& aLightColorIndex, const Tainted<uint8_t>& aRed,
+    const Tainted<uint8_t>& aGreen, const Tainted<uint8_t>& aBlue,
+    const Tainted<uint32_t>& aPromiseID) {
   SetGamepadLightIndicatorColor(aControllerIdx, aLightColorIndex, aRed, aGreen,
                                 aBlue);
 
-  if (SendReplyGamepadPromise(aPromiseID)) {
+  // TODO: simplify tainted validation, see 1610570
+  if (SendReplyGamepadPromise(MOZ_NO_VALIDATE(
+          aPromiseID,
+          "This value is unused aside from being passed back to the child."))) {
     return IPC_OK();
   }
 
