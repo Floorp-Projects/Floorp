1122f1dd53984d884bbfcd2df1b0fdde99d1adc7
Razvan Maries <rmaries@mozilla.com>
2020-12-01 17:13:30 +0200

    Backed out changeset f4d2921dbb2a (bug 1679977) for build bustages. CLOSED TREE

--- third_party/rust/glean-core/src/traits/ping.rs
+++ third_party/rust/glean-core/src/traits/ping.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use crate::error::Result;
+
 /// A description for the `PingType` type.
 ///
 /// When changing this trait, make sure all the operations are
@@ -13,5 +15,9 @@ pub trait Ping {
     ///
     /// * `reason` - the reason the ping was triggered. Included in the
     ///   `ping_info.reason` part of the payload.
-    fn submit(&self, reason: Option<&str>);
+    ///
+    /// # Returns
+    ///
+    /// See [`Glean#submit_ping`](../struct.Glean.html#method.submit_ping) for details.
+    fn submit(&self, reason: Option<&str>) -> Result<bool>;
 }
