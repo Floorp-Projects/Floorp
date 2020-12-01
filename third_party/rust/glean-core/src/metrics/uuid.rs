a86e3767a6c884668871189052a631842d58a779
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-12-01 18:45:12 +0000

    Bug 1662868 - Vendor Glean 33.5.0. r=chutten
    Depends on D98299
    
    Differential Revision: https://phabricator.services.mozilla.com/D98356

--- third_party/rust/glean-core/src/metrics/uuid.rs
+++ third_party/rust/glean-core/src/metrics/uuid.rs
@@ -4,6 +4,7 @@
 
 use uuid::Uuid;
 
+use crate::error_recording::{record_error, ErrorType};
 use crate::metrics::Metric;
 use crate::metrics::MetricType;
 use crate::storage::StorageManager;
@@ -54,6 +55,28 @@ impl UuidMetric {
         glean.storage().record(glean, &self.meta, &value)
     }
 
+    /// Sets to the specified value, from a string.
+    ///
+    /// This should only be used from FFI. When calling directly from Rust, it
+    /// is better to use `set`.
+    ///
+    /// # Arguments
+    ///
+    /// * `glean` - The Glean instance this metric belongs to.
+    /// * `value` - The UUID to set the metric to.
+    pub fn set_from_str(&self, glean: &Glean, value: &str) {
+        if !self.should_record(glean) {
+            return;
+        }
+
+        if let Ok(uuid) = uuid::Uuid::parse_str(&value) {
+            self.set(glean, uuid);
+        } else {
+            let msg = format!("Unexpected UUID value '{}'", value);
+            record_error(glean, &self.meta, ErrorType::InvalidValue, msg, None);
+        }
+    }
+
     /// Generates a new random UUID and set the metric to it.
     ///
     /// # Arguments
@@ -91,14 +114,7 @@ impl UuidMetric {
     /// Gets the currently stored value as a string.
     ///
     /// This doesn't clear the stored value.
-    pub fn test_get_value(&self, glean: &Glean, storage_name: &str) -> Option<String> {
-        match StorageManager.snapshot_metric(
-            glean.storage(),
-            storage_name,
-            &self.meta.identifier(glean),
-        ) {
-            Some(Metric::Uuid(s)) => Some(s),
-            _ => None,
-        }
+    pub fn test_get_value(&self, glean: &Glean, storage_name: &str) -> Option<Uuid> {
+        self.get_value(glean, storage_name)
     }
 }
