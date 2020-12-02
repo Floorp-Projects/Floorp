234952425e879a47bc17de1e0a62a4b1eb73db3b
yulia <ystartsev@mozilla.com>
2020-12-02 12:38:17 +0000

    Bug 1519100 - Add flag for experimental Top Level Await feature; r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D97180

--- js/src/tests/user.js
+++ js/src/tests/user.js
@@ -34,3 +34,4 @@ user_pref("javascript.options.weakrefs", true);
 user_pref("javascript.options.experimental.weakrefs.expose_cleanupSome", true);
 user_pref("javascript.options.experimental.iterator_helpers", true);
 user_pref("javascript.options.experimental.private_fields", true);
+user_pref("javascript.options.experimental.top_level_await", true);
