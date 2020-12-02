0f589bb651d567683c5b438b05dbf37ab04c8499
Lars T Hansen <lhansen@mozilla.com>
2020-12-02 12:47:08 +0000

    Bug 1673391 - Generalize JmpSrc to allow patching not at the end.  r=nbp
    Some instructions, such as RIP-relative CMPPS and CMPPD, have a patch
    field that is embedded in the instruction, not at the end of it.  To
    patch this field, the information about the distance between the field
    and the end of the instruction must be transmitted in the JmpSrc that
    is used for the patching.  (Or we must not use JmpSrc.)
    
    This patch generalizes JmpSrc slightly to carry information about the
    patch field's distance from the end of the instruction.  The only
    values allowed are 0 and 1 bytes, since the only case we care about so
    far has a single byte trailing the patch field, and this restricts the
    offset value in JmpSrc the least while keeping its size small.
    
    Assertions are added to the patching code to guard against values
    other than zero except where such values can be used safely.
    
    Differential Revision: https://phabricator.services.mozilla.com/D97268

--- js/src/jit/x86-shared/Patching-x86-shared.h
+++ js/src/jit/x86-shared/Patching-x86-shared.h
@@ -28,11 +28,11 @@ inline int32_t GetInt32(const void* where) {
   return res;
 }
 
-inline void SetInt32(void* where, int32_t value) {
-  memcpy((char*)where - sizeof(int32_t), &value, sizeof(int32_t));
+inline void SetInt32(void* where, int32_t value, uint32_t trailing = 0) {
+  memcpy((char*)where - trailing - sizeof(int32_t), &value, sizeof(int32_t));
 }
 
-inline void SetRel32(void* from, void* to) {
+inline void SetRel32(void* from, void* to, uint32_t trailing = 0) {
   intptr_t offset =
       reinterpret_cast<intptr_t>(to) - reinterpret_cast<intptr_t>(from);
   MOZ_ASSERT(offset == static_cast<int32_t>(offset),
@@ -41,7 +41,7 @@ inline void SetRel32(void* from, void* to) {
     MOZ_CRASH("offset is too great for a 32-bit relocation");
   }
 
-  SetInt32(from, offset);
+  SetInt32(from, offset, trailing);
 }
 
 inline void* GetRel32Target(void* where) {
@@ -49,14 +49,46 @@ inline void* GetRel32Target(void* where) {
   return (char*)where + rel;
 }
 
+// JmpSrc represents a positive offset within a code buffer, or an uninitialized
+// value.  Lots of code depends on uninitialized JmpSrc holding the value -1, on
+// -1 being a legal value of JmpSrc, and on being able to initialize a JmpSrc
+// with the value -1.
+//
+// The value of the `offset` is always positive and <= MaxCodeBytesPerProcess,
+// see ProcessExecutableMemory.h.  The latter quantity in turn must fit in an
+// i32.  But we further require that the value is not precisely INT32_MAX, so as
+// to allow the JmpSrc value -1 to mean "uninitialized" without ambiguity.
+//
+// The quantity `trailing` denotes the number of bytes of data that follow the
+// patch field in the instruction.  The offset points to the end of the
+// instruction as per normal.  The information about trailing bytes is needed
+// separately from the offset to correctly patch instructions that have
+// immediates trailing the patch field (eg CMPSS and CMPSD).  Currently the only
+// allowed values for `trailing` are 0 and 1.
+
+static_assert(MaxCodeBytesPerProcess < size_t(INT32_MAX), "Invariant");
+
 class JmpSrc {
  public:
-  JmpSrc() : offset_(-1) {}
-  explicit JmpSrc(int32_t offset) : offset_(offset) {}
-  int32_t offset() const { return offset_; }
+  JmpSrc() : offset_(INT32_MAX), trailing_(0) {}
+  explicit JmpSrc(int32_t offset) : offset_(offset), trailing_(0) {
+    // offset -1 is stored as INT32_MAX
+    MOZ_ASSERT(offset == -1 || offset >= 0 && offset < INT32_MAX);
+  }
+  JmpSrc(int32_t offset, uint32_t trailing)
+      : offset_(offset), trailing_(trailing) {
+    // Disallow offset -1 in this situation, it does not apply.
+    MOZ_ASSERT(offset >= 0 && offset < INT32_MAX);
+    MOZ_ASSERT(trailing <= 1);
+  }
+  int32_t offset() const {
+    return offset_ == INT32_MAX ? -1 : int32_t(offset_);
+  }
+  uint32_t trailing() const { return trailing_; }
 
  private:
-  int32_t offset_;
+  uint32_t offset_ : 31;
+  uint32_t trailing_ : 1;
 };
 
 class JmpDst {
