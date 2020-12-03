48ea7af861a0c5dbc474dac0c3af2ecf030155e9
Kagami Sascha Rosylight <krosylight@mozilla.com>
2020-12-03 22:08:44 +0000

    Bug 1680167 - Part 4: MOZ_CAN_RUN_SCRIPT in dom/xul r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D98627

--- dom/xul/nsXULPopupListener.h
+++ dom/xul/nsXULPopupListener.h
@@ -50,7 +50,8 @@ class nsXULPopupListener : public nsIDOMEventListener {
  private:
 #ifndef NS_CONTEXT_MENU_IS_MOUSEUP
   // When a context menu is opened, focus the target of the contextmenu event.
-  nsresult FireFocusOnTargetContent(nsIContent* aTargetContent, bool aIsTouch);
+  MOZ_CAN_RUN_SCRIPT_BOUNDARY nsresult
+  FireFocusOnTargetContent(nsIContent* aTargetContent, bool aIsTouch);
 #endif
 
   // |mElement| is the node to which this listener is attached.
