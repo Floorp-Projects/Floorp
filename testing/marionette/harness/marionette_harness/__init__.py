12d84eba947cb5856be225ab921ee4b8fe37cc13
Henrik Skupin <mail@hskupin.info>
2020-12-03 12:20:20 +0000

    Bug 1675173 - [marionette] Disable TestPageLoadStrategy.test_none when actors are disabled. r=marionette-reviewers,jgraham
    It's not worth the time to fix this particular bug in the old
    framescript code that will go away early next year.
    
    Differential Revision: https://phabricator.services.mozilla.com/D98565

--- testing/marionette/harness/marionette_harness/__init__.py
+++ testing/marionette/harness/marionette_harness/__init__.py
@@ -15,6 +15,7 @@ from .marionette_test import (
     skip,
     skip_if_chrome,
     skip_if_desktop,
+    skip_if_framescript,
     SkipTest,
     skip_unless_browser_pref,
     skip_unless_protocol,
