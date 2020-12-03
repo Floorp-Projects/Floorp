5ceff3ebdc007be680a7b5000b5734641792994f
Mitchell Hentges <mhentges@mozilla.com>
2020-12-03 15:25:36 +0000

    Bug 1680353: Ensure MACOSX_DEPLOYMENT_TARGET is a string r=firefox-build-system-reviewers,dmajor
    The sysconfig config var of "MACOSX_DEPLOYMENT_TARGET" isn't
    consistently a string, so we need to explicitly convert it
    
    Differential Revision: https://phabricator.services.mozilla.com/D98533

--- python/mozbuild/mozbuild/virtualenv.py
+++ python/mozbuild/mozbuild/virtualenv.py
@@ -449,7 +449,10 @@ class VirtualenvManager(VirtualenvHelper):
             )
 
             if sysconfig_target is not None:
-                os.environ["MACOSX_DEPLOYMENT_TARGET"] = sysconfig_target
+                # MACOSX_DEPLOYMENT_TARGET is usually a string (e.g.: "10.14.6"), but
+                # in some cases it is an int (e.g.: 11). Since environment variables
+                # must all be str, explicitly convert it.
+                os.environ["MACOSX_DEPLOYMENT_TARGET"] = str(sysconfig_target)
 
             old_env_variables = {}
             for k in IGNORE_ENV_VARIABLES:
