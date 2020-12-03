bd066e5320a307148eda3eea7a28614d59a3d964
Aaron Train <aaron.train@gmail.com>
2020-12-03 11:36:52 -0500

    [focus] Closes https://github.com/mozilla-mobile/focus-android/issues/4665: Remove pmd checkstyle spotbugs
    These utilities are no longer part of Gradle setup. Remove to get Code Quality passing.

--- mobile/android/focus-android/tools/taskcluster/schedule-master-build.py
+++ mobile/android/focus-android/tools/taskcluster/schedule-master-build.py
@@ -45,7 +45,7 @@ def generate_code_quality_task(buildTaskId):
     return taskcluster.slugId(), generate_task(
         name="(Focus for Android) Code quality",
         description="Run code quality tools on Focus/Klar for Android code base.",
-        command='echo "--" > .adjust_token && ./gradlew --no-daemon clean detektCheck ktlint lint pmd checkstyle spotbugs',
+        command='echo "--" > .adjust_token && ./gradlew --no-daemon clean detektCheck ktlint lint',
         dependencies=[buildTaskId])
 
 
