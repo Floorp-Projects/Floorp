48808328ad9558a80588502f55fa32606d540bd6
yulia <ystartsev@mozilla.com>
2020-12-04 08:54:43 +0000

    Bug 1519100 - Add tests for top level await; r=jonco
    Add tests behind the TLA flag;
    
    Differential Revision: https://phabricator.services.mozilla.com/D93530

--- js/src/tests/test262/language/module-code/top-level-await/new-await.js
+++ js/src/tests/test262/language/module-code/top-level-await/new-await.js
@@ -1,4 +1,4 @@
-// |reftest| skip error:SyntaxError module -- top-level-await is not supported
+// |reftest| shell-option(--enable-top-level-await) skip-if(!xulRuntime.shell) error:SyntaxError module -- requires shell-options
 // Copyright (C) 2020 Rick Waldron. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
