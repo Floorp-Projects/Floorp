b4359382492efc737e28212496fe4e17d841f6aa
Agi Sferro <agi@sferro.dev>
2020-12-04 20:52:45 +0000

    Bug 1625233 - Add EventCallback.resolveTo. r=snorp
    Differential Revision: https://phabricator.services.mozilla.com/D96150

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/EventCallback.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/util/EventCallback.java
@@ -2,6 +2,9 @@ package org.mozilla.gecko.util;
 
 import org.mozilla.gecko.annotation.RobocopTarget;
 import org.mozilla.gecko.annotation.WrapForJNI;
+import org.mozilla.geckoview.GeckoResult;
+
+import javax.annotation.Nullable;
 
 /**
  * Callback interface for Gecko requests.
@@ -19,7 +22,7 @@ public interface EventCallback {
      * @param response The response data to send to Gecko. Can be any of the types accepted by
      *                 JSONObject#put(String, Object).
      */
-    public void sendSuccess(Object response);
+    void sendSuccess(Object response);
 
     /**
      * Sends an error response with the given data.
@@ -27,5 +30,19 @@ public interface EventCallback {
      * @param response The response data to send to Gecko. Can be any of the types accepted by
      *                 JSONObject#put(String, Object).
      */
-    public void sendError(Object response);
+    void sendError(Object response);
+
+    /**
+     * Resolve this Event callback with the result from the {@link GeckoResult}.
+     *
+     * @param response the result that will be used for this callback.
+     */
+    default <T> void resolveTo(final @Nullable GeckoResult<T> response) {
+        if (response == null) {
+            sendSuccess(null);
+            return;
+        }
+        response.accept(this::sendSuccess, throwable ->
+            sendError(throwable.getMessage()));
+    }
 }
