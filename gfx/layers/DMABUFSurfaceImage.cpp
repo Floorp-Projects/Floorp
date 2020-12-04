1a792943ea9bb0ff15c88bd29182ee138ddaf0d7
Martin Stransky <stransky@redhat.com>
2020-12-04 09:41:46 +0000

    Bug 1660336 Build ffvpx FFmpegVideoDecoder module with VAAPI support, r=jya,glandium,jgilbert
    - Remove MOZ_WAYLAND_CFLAGS and /ipc/chromium/chromium-config.mozbuild from ffmpeg58/moz.build
    - Build ffvpx with vaapi support at ffvpx/moz.build
    - Move gfx related headers from DMABUFSurfaceImage.h to DMABUFSurfaceImage.cpp and implement
      the gfx methods there.
    - Remove GL headers from DMABufSurface.h and forward declare GLuint/GLContext there.
    - Move mutex/MessageLoop/task related headers from nsWaylandDisplay.h to nsWaylandDisplay.cpp.
    
    Depends on D90556
    
    Differential Revision: https://phabricator.services.mozilla.com/D90557

--- gfx/layers/DMABUFSurfaceImage.cpp
+++ gfx/layers/DMABUFSurfaceImage.cpp
@@ -5,16 +5,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "DMABUFSurfaceImage.h"
-#include "gfxPlatform.h"
+#include "mozilla/widget/DMABufSurface.h"
 #include "mozilla/layers/CompositableClient.h"
 #include "mozilla/layers/CompositableForwarder.h"
 #include "mozilla/layers/DMABUFTextureClientOGL.h"
 #include "mozilla/layers/TextureForwarder.h"
-#include "mozilla/UniquePtr.h"
 
 using namespace mozilla;
 using namespace mozilla::layers;
 using namespace mozilla::gfx;
+
+DMABUFSurfaceImage::DMABUFSurfaceImage(DMABufSurface* aSurface)
+    : Image(nullptr, ImageFormat::DMABUF), mSurface(aSurface) {
+  mSurface->GlobalRefAdd();
+}
+
+DMABUFSurfaceImage::~DMABUFSurfaceImage() { mSurface->GlobalRefRelease(); }
 
 TextureClient* DMABUFSurfaceImage::GetTextureClient(
     KnowsCompositor* aKnowsCompositor) {
@@ -25,4 +31,8 @@ TextureClient* DMABUFSurfaceImage::GetTextureClient(
         aKnowsCompositor->GetTextureForwarder());
   }
   return mTextureClient;
+}
+
+gfx::IntSize DMABUFSurfaceImage::GetSize() const {
+  return gfx::IntSize::Truncate(mSurface->GetWidth(), mSurface->GetHeight());
 }
