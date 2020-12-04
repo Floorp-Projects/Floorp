68bc625380723c5950893fd7c8127d0ce4f0a4b5
Steve Fink <sfink@mozilla.com>
2020-12-04 16:02:52 +0000

    Bug 1678416 - Implement IdleTaskRunner::SetBudget() and add some comments. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D97670

--- xpcom/threads/IdleTaskRunner.h
+++ xpcom/threads/IdleTaskRunner.h
@@ -30,23 +30,34 @@ class IdleTaskRunner final : public CancelableIdleRunnable {
   using MayStopProcessingCallbackType = std::function<bool()>;
 
  public:
+  // An IdleTaskRunner will attempt to run in idle time, with a budget computed
+  // based on a (capped) estimate for how much idle time is available. If there
+  // is no idle time within `aMaxDelay` ms, it will fall back to running using
+  // a specified `aNonIdleBudget`.
   static already_AddRefed<IdleTaskRunner> Create(
-      const CallbackType& aCallback, const char* aRunnableName, uint32_t aDelay,
-      int64_t aBudget, bool aRepeating,
+      const CallbackType& aCallback, const char* aRunnableName,
+      uint32_t aMaxDelay, int64_t aNonIdleBudget, bool aRepeating,
       const MayStopProcessingCallbackType& aMayStopProcessing);
 
   NS_IMETHOD Run() override;
 
+  // (Used by the task triggering code.) Record the end of the current idle
+  // period, or null if not running during idle time.
   void SetDeadline(mozilla::TimeStamp aDeadline) override;
+
   void SetTimer(uint32_t aDelay, nsIEventTarget* aTarget) override;
+
+  // Update the non-idle time budgeted for this callback. This really only
+  // makes sense for a repeating runner.
+  void SetBudget(int64_t aBudget);
 
   nsresult Cancel() override;
   void Schedule(bool aAllowIdleDispatch);
 
  private:
   explicit IdleTaskRunner(
-      const CallbackType& aCallback, const char* aRunnableName, uint32_t aDelay,
-      int64_t aBudget, bool aRepeating,
+      const CallbackType& aCallback, const char* aRunnableName,
+      uint32_t aMaxDelay, int64_t aNonIdleBudget, bool aRepeating,
       const MayStopProcessingCallbackType& aMayStopProcessing);
   ~IdleTaskRunner();
   void CancelTimer();
@@ -55,9 +66,19 @@ class IdleTaskRunner final : public CancelableIdleRunnable {
   nsCOMPtr<nsITimer> mTimer;
   nsCOMPtr<nsITimer> mScheduleTimer;
   CallbackType mCallback;
+
+  // Wait this long for idle time before giving up and running a non-idle
+  // callback.
   uint32_t mDelay;
+
+  // If running during idle time, the expected end of the current idle period.
+  // The null timestamp when the run is triggered by aMaxDelay instead of idle.
   TimeStamp mDeadline;
+
+  // The expected amount of time the callback will run for, when not running
+  // during idle time.
   TimeDuration mBudget;
+
   bool mRepeating;
   bool mTimerActive;
   MayStopProcessingCallbackType mMayStopProcessing;
