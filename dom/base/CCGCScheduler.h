7be536a11e75382377a9ca4ca34d5d325ad4caf5
Steve Fink <sfink@mozilla.com>
2020-12-04 16:02:52 +0000

    Bug 1678416 - Implement IdleTaskRunner::SetBudget() and add some comments. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D97670

--- dom/base/CCGCScheduler.h
+++ dom/base/CCGCScheduler.h
@@ -3,9 +3,9 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "js/SliceBudget.h"
+#include "mozilla/MainThreadIdlePeriod.h"
 #include "mozilla/Telemetry.h"
 #include "mozilla/TimeStamp.h"
-#include "mozilla/MainThreadIdlePeriod.h"
 #include "nsCycleCollector.h"
 #include "nsJSEnvironment.h"
 
@@ -200,7 +200,7 @@ class CCGCScheduler {
     }
 
     const TimeDuration maxSlice = TimeDuration::FromMilliseconds(
-        MainThreadIdlePeriod::GetLongIdlePeriod());
+        mozilla::MainThreadIdlePeriod::GetLongIdlePeriod());
 
     // Try to make up for a delay in running this slice.
     double sliceDelayMultiplier =
