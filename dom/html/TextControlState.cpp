007e18402d42ed8ecf96042466fa8b98823e2f03
Bogdan Tara <btara@mozilla.com>
2020-12-05 14:45:37 +0200

    Backed out changeset 18c139fe8859 (bug 1675894) for selectionchange.tentative.html failures CLOSED TREE

--- dom/html/TextControlState.cpp
+++ dom/html/TextControlState.cpp
@@ -2098,19 +2098,10 @@ void TextControlState::SetSelectionRange(
 
   if (changed) {
     // It sure would be nice if we had an existing Element* or so to work with.
-    RefPtr<AsyncEventDispatcher> asyncDispatcher = new AsyncEventDispatcher(
-        mTextCtrlElement, eFormSelect, CanBubble::eYes);
+    RefPtr<AsyncEventDispatcher> asyncDispatcher =
+        new AsyncEventDispatcher(mTextCtrlElement, u"select"_ns,
+                                 CanBubble::eYes, ChromeOnlyDispatch::eNo);
     asyncDispatcher->PostDOMEvent();
-
-    // SelectionChangeEventDispatcher covers this when !IsSelectionCached().
-    // XXX(krosylight): Shouldn't it fire before select event?
-    // Currently Gecko and Blink both fire selectionchange after select.
-    if (IsSelectionCached() &&
-        StaticPrefs::dom_select_events_textcontrols_enabled()) {
-      asyncDispatcher = new AsyncEventDispatcher(
-          mTextCtrlElement, eSelectionChange, CanBubble::eNo);
-      asyncDispatcher->PostDOMEvent();
-    }
   }
 
   if (NS_FAILED(rv)) {
