b7eb08175c62d059af01d99fdfcfa51827c1dfc5
James Teh <jteh@mozilla.com>
2020-12-07 04:22:53 +0000

    Bug 494175 part 3: Don't change the focusability of the full screen button in the video controls when fading out if the controls are in the tab order. r=Gijs
    This was added to prevent visual confusion, since otherwise, the full screen button could be focused while the controls were hidden.
    If the controls are in the tab order, they show when focused, so this is not necessary.
    
    Differential Revision: https://phabricator.services.mozilla.com/D97530

--- toolkit/content/widgets/videocontrols.js
+++ toolkit/content/widgets/videocontrols.js
@@ -1531,10 +1531,14 @@ this.VideoControlsImplWidget = class {
             if (!this.hasError() && this.isVideoInFullScreen) {
               this.controlsSpacer.setAttribute("hideCursor", true);
             }
-            // The Full Screen button is currently the only tabbable button
-            // when the controls are shown. Remove it from the tab order when
-            // visually hidden to prevent visual confusion.
-            this.fullscreenButton.setAttribute("tabindex", "-1");
+            if (
+              !this.prefs["media.videocontrols.keyboard-tab-to-all-controls"]
+            ) {
+              // The Full Screen button is currently the only tabbable button
+              // when the controls are shown. Remove it from the tab order when
+              // visually hidden to prevent visual confusion.
+              this.fullscreenButton.setAttribute("tabindex", "-1");
+            }
           }
 
           // No need to fade out if the hidden property returns true
