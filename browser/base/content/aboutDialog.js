281e7584b214a2fbff8e0a14f95318dd22424b13
Emma Malysz <emalysz@mozilla.com>
2020-12-07 21:16:33 +0000

    Bug 1677801, adds UTM parameters to about dialog r=bigiri
    Differential Revision: https://phabricator.services.mozilla.com/D97488

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -72,10 +72,12 @@ async function init(aEvent) {
 
   // Show a release notes link if we have a URL.
   let relNotesLink = document.getElementById("releasenotes");
-  let relNotesPrefType = Services.prefs.getPrefType("app.releaseNotesURL");
+  let relNotesPrefType = Services.prefs.getPrefType(
+    "app.releaseNotesURL.aboutDialog"
+  );
   if (relNotesPrefType != Services.prefs.PREF_INVALID) {
     let relNotesURL = Services.urlFormatter.formatURLPref(
-      "app.releaseNotesURL"
+      "app.releaseNotesURL.aboutDialog"
     );
     if (relNotesURL != "about:blank") {
       relNotesLink.href = relNotesURL;
