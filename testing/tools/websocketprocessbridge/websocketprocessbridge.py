d3c214413439420331cb645f166ef2e5257c0a9b
Bob Clary <bclary@bclary.com>
2020-12-07 19:43:57 +0000

    Bug 1681093 - python3 - from __future__ import print_function, r=jmaher.
    Differential Revision: https://phabricator.services.mozilla.com/D98937

--- testing/tools/websocketprocessbridge/websocketprocessbridge.py
+++ testing/tools/websocketprocessbridge/websocketprocessbridge.py
@@ -3,6 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from __future__ import print_function
+
 from twisted.internet import protocol, reactor
 from twisted.internet.task import LoopingCall
 import txws
