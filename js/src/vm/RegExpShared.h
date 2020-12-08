5e0c8b99bb6931fee09d499c1a03b8d5cceec294
André Bargull <andre.bargull@gmail.com>
2020-12-08 03:04:32 +0000

    Bug 1681084 - Part 1: Remove unused RegExpShared::compile methods. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D98928

--- js/src/vm/RegExpShared.h
+++ js/src/vm/RegExpShared.h
@@ -135,12 +135,6 @@ class RegExpShared
   /* Internal functions. */
   RegExpShared(JSAtom* source, JS::RegExpFlags flags);
 
-  static bool compile(JSContext* cx, MutableHandleRegExpShared res,
-                      HandleLinearString input, CodeKind code);
-  static bool compile(JSContext* cx, MutableHandleRegExpShared res,
-                      HandleAtom pattern, HandleLinearString input,
-                      CodeKind code);
-
   const RegExpCompilation& compilation(bool latin1) const {
     return compilationArray[CompilationIndex(latin1)];
   }
