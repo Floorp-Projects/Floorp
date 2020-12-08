61a276d5250f8645a598d0e784a33dbe21e4d6da
Bob Clary <bclary@bclary.com>
2020-12-08 22:50:01 +0000

    Bug 1681096 - python3 - pylint --py3k - W1602: basestring built-in referenced (basestring-builtin) r=aki,gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D99060

--- testing/mozharness/mozharness/mozilla/repo_manipulation.py
+++ testing/mozharness/mozharness/mozilla/repo_manipulation.py
@@ -3,6 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from __future__ import absolute_import
+import six
 import ConfigParser
 import json
 import os
@@ -137,7 +138,7 @@ class MercurialRepoManipulationMixin(object):
         force=None,
         halt_on_failure=True,
     ):
-        if isinstance(tags, basestring):
+        if isinstance(tags, six.string_types):
             tags = [tags]
         cmd = self.query_exe("hg", return_type="list") + ["tag"]
         if not message:
