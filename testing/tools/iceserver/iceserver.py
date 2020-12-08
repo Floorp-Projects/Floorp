4dc1f377538f41c73c69fbcd76fb442051b4021b
Bob Clary <bclary@bclary.com>
2020-12-08 15:20:15 +0000

    Bug 1681096 - python3 - pylint --py3k - W1649: Accessing a deprecated function on the string module (deprecated-string-function) r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D99094

--- testing/tools/iceserver/iceserver.py
+++ testing/tools/iceserver/iceserver.py
@@ -849,9 +849,9 @@ if __name__ == "__main__":
         lines = f.readlines()
         lines.pop(0)  # Remove BEGIN CERTIFICATE
         lines.pop()  # Remove END CERTIFICATE
-        # pylint --py3k: W1636
-        lines = list(map(string.strip, lines))
-        certbase64 = string.join(lines, "")
+        # pylint --py3k: W1636 W1649
+        lines = list(map(str.strip, lines))
+        certbase64 = "".join(lines)  # pylint --py3k: W1649
 
         turns_url = ', "turns:' + hostname + '"'
         cert_prop = ', "cert":"' + certbase64 + '"'
