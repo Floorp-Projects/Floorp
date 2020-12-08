be0175f4aa4c366d42f53431355a17c387d41aa7
Steve Fink <sfink@mozilla.com>
2020-12-08 22:24:22 +0000

    Bug 1675755 - Enter correct compartment before we might allocate BaseShape r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D97386

--- js/src/debugger/Object.cpp
+++ js/src/debugger/Object.cpp
@@ -2378,6 +2378,11 @@ bool DebuggerObject::forceLexicalInitializationByName(
 
   Rooted<GlobalObject*> referent(cx, &object->referent()->as<GlobalObject>());
 
+  // Shape::search can end up allocating a new BaseShape in Shape::cachify so
+  // we need to be in the right compartment here.
+  Maybe<AutoRealm> ar;
+  EnterDebuggeeObjectRealm(cx, ar, referent);
+
   RootedObject globalLexical(cx, &referent->lexicalEnvironment());
   RootedObject pobj(cx);
   Rooted<PropertyResult> prop(cx);
