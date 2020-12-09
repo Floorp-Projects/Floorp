79f7b4e1c54f3526b384ad8d80a222a9f0ae4c15
Punam Dahiya <punamdahiya@yahoo.com>
2020-12-09 17:26:04 +0000

    Bug 1657343 - Fix browser console errors seen on navigating to about:welcome r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D96789

--- browser/components/newtab/test/unit/aboutwelcome/MultiStageAboutWelcome.test.jsx
+++ browser/components/newtab/test/unit/aboutwelcome/MultiStageAboutWelcome.test.jsx
@@ -1,4 +1,3 @@
-import { act } from "react-dom/test-utils";
 import { GlobalOverrider } from "test/unit/utils";
 import {
   MultiStageAboutWelcome,
@@ -6,7 +5,10 @@ import {
 } from "content-src/aboutwelcome/components/MultiStageAboutWelcome";
 import React from "react";
 import { shallow, mount } from "enzyme";
-import { DEFAULT_WELCOME_CONTENT } from "content-src/lib/aboutwelcome-utils";
+import {
+  DEFAULT_WELCOME_CONTENT,
+  AboutWelcomeUtils,
+} from "content-src/lib/aboutwelcome-utils";
 
 describe("MultiStageAboutWelcome module", () => {
   let globals;
@@ -22,11 +24,12 @@ describe("MultiStageAboutWelcome module", () => {
   beforeEach(async () => {
     globals = new GlobalOverrider();
     globals.set({
-      AWGetDefaultSites: () => Promise.resolve({}),
+      AWGetDefaultSites: () => Promise.resolve([]),
       AWGetImportableSites: () => Promise.resolve("{}"),
       AWGetSelectedTheme: () => Promise.resolve("automatic"),
       AWSendEventTelemetry: () => {},
       AWWaitForRegionChange: () => Promise.resolve(),
+      AWGetRegion: () => Promise.resolve(),
     });
     sandbox = sinon.createSandbox();
   });
@@ -44,20 +47,13 @@ describe("MultiStageAboutWelcome module", () => {
     });
 
     it("should pass activeTheme and initialTheme props to WelcomeScreen", async () => {
-      let wrapper;
-
-      await act(async () => {
-        // need to use mount, as shallow doesn't currently support effects
-        wrapper = mount(<MultiStageAboutWelcome {...DEFAULT_PROPS} />);
-
-        // Spin the event loop to allow the useEffect hooks to execute,
-        // any promises to resolve, and re-rendering to happen after the
-        // promises have updated the state/props
-        await new Promise(resolve => setTimeout(resolve, 0));
-
-        // sync up enzyme's representation with the real DOM
-        wrapper.update();
-      });
+      let wrapper = mount(<MultiStageAboutWelcome {...DEFAULT_PROPS} />);
+      // Spin the event loop to allow the useEffect hooks to execute,
+      // any promises to resolve, and re-rendering to happen after the
+      // promises have updated the state/props
+      await new Promise(resolve => setTimeout(resolve, 0));
+      // sync up enzyme's representation with the real DOM
+      wrapper.update();
 
       let welcomeScreenWrapper = wrapper.find(WelcomeScreen);
       assert.strictEqual(welcomeScreenWrapper.prop("activeTheme"), "automatic");
@@ -66,9 +62,55 @@ describe("MultiStageAboutWelcome module", () => {
         "automatic"
       );
     });
+
+    it("should handle primary Action", () => {
+      const stub = sinon.stub(AboutWelcomeUtils, "sendActionTelemetry");
+      let wrapper = mount(<MultiStageAboutWelcome {...DEFAULT_PROPS} />);
+      wrapper.update();
+
+      let welcomeScreenWrapper = wrapper.find(WelcomeScreen);
+      const btnPrimary = welcomeScreenWrapper.find(".primary");
+      btnPrimary.simulate("click");
+      assert.calledOnce(stub);
+      assert.equal(
+        stub.firstCall.args[0],
+        welcomeScreenWrapper.props().messageId
+      );
+      assert.equal(stub.firstCall.args[1], "primary_button");
+    });
   });
 
   describe("WelcomeScreen component", () => {
+    describe("get started screen", () => {
+      const startScreen = DEFAULT_WELCOME_CONTENT.screens.find(screen => {
+        return screen.id === "AW_GET_STARTED";
+      });
+
+      const GET_STARTED_SCREEN_PROPS = {
+        id: startScreen.id,
+        totalNumberofScreens: 1,
+        order: startScreen.order,
+        content: startScreen.content,
+        topSites: [],
+        messageId: `${DEFAULT_PROPS.message_id}_${startScreen.id}`,
+        UTMTerm: DEFAULT_PROPS.utm_term,
+        flowParams: null,
+      };
+
+      it("should render GetStarted Screen", () => {
+        const wrapper = shallow(
+          <WelcomeScreen {...GET_STARTED_SCREEN_PROPS} />
+        );
+        assert.ok(wrapper.exists());
+      });
+
+      it("should have primary and secondary button in the rendered input", () => {
+        const wrapper = mount(<WelcomeScreen {...GET_STARTED_SCREEN_PROPS} />);
+        assert.ok(wrapper.find(".primary"));
+        assert.isTrue(wrapper.find("div.secondary-cta").hasClass("top"));
+        assert.ok(wrapper.find(".secondary"));
+      });
+    });
     describe("theme screen", () => {
       const themeScreen = DEFAULT_WELCOME_CONTENT.screens.find(screen => {
         return screen.id === "AW_CHOOSE_THEME";
@@ -80,7 +122,7 @@ describe("MultiStageAboutWelcome module", () => {
         order: themeScreen.order,
         content: themeScreen.content,
         navigate: null,
-        topSites: {},
+        topSites: [],
         messageId: `${DEFAULT_PROPS.message_id}_${themeScreen.id}`,
         UTMTerm: DEFAULT_PROPS.utm_term,
         flowParams: null,
