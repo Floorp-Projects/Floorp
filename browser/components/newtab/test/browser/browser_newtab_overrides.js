54ad7a24f67fcf97132e87bdd32ffcbb9b69fde5
Mark Banner <standard8@mozilla.com>
2020-12-09 18:07:44 +0000

    Bug 1680325 - Remove references to jshint from newtab. r=nanj
    Differential Revision: https://phabricator.services.mozilla.com/D98519

--- browser/components/newtab/test/browser/browser_newtab_overrides.js
+++ browser/components/newtab/test/browser/browser_newtab_overrides.js
@@ -13,7 +13,6 @@ function nextChangeNotificationPromise(aNewURL, testMessage) {
     aSubject,
     aData
   ) {
-    // jshint unused:false
     Assert.equal(aData, aNewURL, testMessage);
     return true;
   });
@@ -62,7 +61,7 @@ add_task(async function redirector_ignores_override() {
           "activity stream principal should not match systemPrincipal"
         );
       });
-    }); // jshint ignore:line
+    });
   }
 });
 
@@ -87,7 +86,7 @@ add_task(async function override_loads_in_browser() {
     Assert.ok(AboutNewTab.newTabURLOverridden, "url has been overridden");
 
     // simulate a newtab open as a user would
-    BrowserOpenTab(); // jshint ignore:line
+    BrowserOpenTab();
 
     let browser = gBrowser.selectedBrowser;
     await BrowserTestUtils.browserLoaded(browser);
@@ -99,7 +98,7 @@ add_task(async function override_loads_in_browser() {
         args.url.trim(),
         "Got right URL"
       );
-    }); // jshint ignore:line
+    });
     BrowserTestUtils.removeTab(gBrowser.selectedTab);
   }
 });
@@ -121,7 +120,7 @@ add_task(async function override_blank_loads_in_browser() {
     Assert.ok(AboutNewTab.newTabURLOverridden, "url has been overridden");
 
     // simulate a newtab open as a user would
-    BrowserOpenTab(); // jshint ignore:line
+    BrowserOpenTab();
 
     let browser = gBrowser.selectedBrowser;
     await BrowserTestUtils.browserLoaded(browser);
@@ -133,7 +132,7 @@ add_task(async function override_blank_loads_in_browser() {
         "about:blank",
         "Got right URL"
       );
-    }); // jshint ignore:line
+    });
     BrowserTestUtils.removeTab(gBrowser.selectedTab);
   }
 });
