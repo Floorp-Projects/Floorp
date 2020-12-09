7e99147973826b3ee9840e26db5c5a9901441a11
smolnar <smolnar@mozilla.com>
2020-12-09 13:43:13 +0200

    Backed out 2 changesets (bug 1681084) for causing SM bustage in RegExpObject. CLOSED TREE
    Backed out changeset a2194d3d0611 (bug 1681084)
    Backed out changeset e787af20b790 (bug 1681084)

--- js/src/builtin/RegExp.cpp
+++ js/src/builtin/RegExp.cpp
@@ -750,7 +750,7 @@ static bool regexp_source(JSContext* cx, unsigned argc, JS::Value* vp) {
         cx->markAtom(src);
 
         // Step 7.
-        JSString* escaped = EscapeRegExpPattern(cx, src);
+        JSAtom* escaped = EscapeRegExpPattern(cx, src);
         if (!escaped) {
           return false;
         }
