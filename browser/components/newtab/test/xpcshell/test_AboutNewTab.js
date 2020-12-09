a29f011ce82a38a8e94c231ee262a9dfd7e8bfb0
Mark Banner <standard8@mozilla.com>
2020-12-09 18:07:44 +0000

    Bug 1680325 - Remove references to jshint from newtab. r=nanj
    Differential Revision: https://phabricator.services.mozilla.com/D98519

--- browser/components/newtab/test/xpcshell/test_AboutNewTab.js
+++ browser/components/newtab/test/xpcshell/test_AboutNewTab.js
@@ -65,7 +65,6 @@ function setExpectedUrlsWithoutScripts() {
 function nextChangeNotificationPromise(aNewURL, testMessage) {
   return new Promise(resolve => {
     Services.obs.addObserver(function observer(aSubject, aTopic, aData) {
-      // jshint unused:false
       Services.obs.removeObserver(observer, aTopic);
       Assert.equal(aData, aNewURL, testMessage);
       resolve();
@@ -108,7 +107,6 @@ function addTestsWithPrivilegedContentProcessPref(test) {
 function setBoolPrefAndWaitForChange(pref, value, testMessage) {
   return new Promise(resolve => {
     Services.obs.addObserver(function observer(aSubject, aTopic, aData) {
-      // jshint unused:false
       Services.obs.removeObserver(observer, aTopic);
       Assert.equal(aData, AboutNewTab.newTabURL, testMessage);
       resolve();
