72c2f06b96296f1ed8995037969f502660cc46a2
Erica Wright <ewright@mozilla.com>
2020-12-09 16:21:43 +0000

    Bug 1671943 - Create home content section for customize menu. r=prathiksha,thecount,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D98242

--- browser/components/newtab/content-src/components/Base/Base.jsx
+++ browser/components/newtab/content-src/components/Base/Base.jsx
@@ -13,6 +13,7 @@ import { CustomizeMenu } from "content-src/components/CustomizeMenu/CustomizeMen
 import React from "react";
 import { Search } from "content-src/components/Search/Search";
 import { Sections } from "content-src/components/Sections/Sections";
+import { CSSTransition } from "react-transition-group";
 
 export const PrefsButton = ({ onClick, icon }) => (
   <div className="prefs-button">
@@ -116,6 +117,7 @@ export class BaseContent extends React.PureComponent {
     this.openCustomizationMenu = this.openCustomizationMenu.bind(this);
     this.closeCustomizationMenu = this.closeCustomizationMenu.bind(this);
     this.onWindowScroll = debounce(this.onWindowScroll.bind(this), 5);
+    this.setPref = this.setPref.bind(this);
     this.state = { fixedSearch: false, customizeMenuVisible: false };
   }
 
@@ -154,6 +156,10 @@ export class BaseContent extends React.PureComponent {
     this.setState({ customizeMenuVisible: false });
   }
 
+  setPref(pref, value) {
+    this.props.dispatch(ac.SetPref(pref, value));
+  }
+
   render() {
     const { props } = this;
     const { App } = props;
@@ -185,6 +191,19 @@ export class BaseContent extends React.PureComponent {
     const newNewtabExperienceEnabled = prefs["newNewtabExperience.enabled"];
     const canShowCustomizationMenu =
       customizationMenuEnabled || newNewtabExperienceEnabled;
+    const showCustomizationMenu =
+      canShowCustomizationMenu && this.state.customizeMenuVisible;
+    const enabledSections = {
+      topSitesEnabled: prefs["feeds.topsites"],
+      pocketEnabled: prefs["feeds.section.topstories"],
+      snippetsEnabled: prefs["feeds.snippets"],
+      highlightsEnabled: prefs["feeds.section.highlights"],
+      showSponsoredTopSitesEnabled: prefs.showSponsoredTopSites,
+      showSponsoredPocketEnabled: prefs.showSponsored,
+      topSitesRowsCount: prefs.topSitesRows,
+    };
+    const pocketRegion = prefs["feeds.system.topstories"];
+    const { mayHaveSponsoredTopSites } = prefs;
 
     const outerClassName = [
       "outer-wrapper",
@@ -238,9 +257,22 @@ export class BaseContent extends React.PureComponent {
             <ConfirmDialog />
           </main>
         </div>
-        {canShowCustomizationMenu && this.state.customizeMenuVisible && (
-          <CustomizeMenu onClose={this.closeCustomizationMenu} />
-        )}
+        <CSSTransition
+          timeout={250}
+          classNames="customize-animate"
+          in={showCustomizationMenu}
+          appear={true}
+          unmountOnExit={true}
+        >
+          <CustomizeMenu
+            onClose={this.closeCustomizationMenu}
+            openPreferences={this.openPreferences}
+            setPref={this.setPref}
+            enabledSections={enabledSections}
+            pocketRegion={pocketRegion}
+            mayHaveSponsoredTopSites={mayHaveSponsoredTopSites}
+          />
+        </CSSTransition>
       </div>
     );
   }
--- browser/components/newtab/content-src/components/CustomizeMenu/ContentSection/ContentSection.jsx
+++ browser/components/newtab/content-src/components/CustomizeMenu/ContentSection/ContentSection.jsx
@@ -5,7 +5,215 @@
 import React from "react";
 
 export class ContentSection extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.onPreferenceSelect = this.onPreferenceSelect.bind(this);
+  }
+
+  onPreferenceSelect(e) {
+    let prefName = e.target.getAttribute("preference");
+    let value;
+    if (e.target.nodeName === "SELECT") {
+      value = parseInt(e.target.value, 10);
+    } else if (e.target.nodeName === "INPUT") {
+      value = e.target.checked;
+    }
+    this.props.setPref(prefName, value);
+  }
+
   render() {
-    return <div />;
+    const {
+      topSitesEnabled,
+      pocketEnabled,
+      highlightsEnabled,
+      snippetsEnabled,
+      showSponsoredTopSitesEnabled,
+      showSponsoredPocketEnabled,
+      topSitesRowsCount,
+    } = this.props.enabledSections;
+
+    return (
+      <div className="home-section">
+        <div id="shortcuts-section" className="section">
+          <div>
+            <h2
+              className="title"
+              data-l10n-id="newtab-custom-shortcuts-title"
+            />
+            <p
+              className="subtitle"
+              data-l10n-id="newtab-custom-shortcuts-subtitle"
+            ></p>
+            <div
+              className={`more-info-top-wrapper ${
+                topSitesEnabled ? "" : "shrink"
+              }`}
+            >
+              <div
+                className={`more-information ${
+                  topSitesEnabled ? "expand" : "shrink"
+                }`}
+              >
+                <select
+                  id="row-selector"
+                  className="selector"
+                  name="row-count"
+                  preference="topSitesRows"
+                  value={topSitesRowsCount}
+                  onChange={this.onPreferenceSelect}
+                  disabled={!topSitesEnabled}
+                >
+                  <option
+                    value="1"
+                    data-l10n-id="newtab-custom-row-selector"
+                    data-l10n-args='{"num": 1}'
+                  />
+                  <option
+                    value="2"
+                    data-l10n-id="newtab-custom-row-selector"
+                    data-l10n-args='{"num": 2}'
+                  />
+                  <option
+                    value="3"
+                    data-l10n-id="newtab-custom-row-selector"
+                    data-l10n-args='{"num": 3}'
+                  />
+                  <option
+                    value="4"
+                    data-l10n-id="newtab-custom-row-selector"
+                    data-l10n-args='{"num": 4}'
+                  />
+                </select>
+                {this.props.mayHaveSponsoredTopSites && (
+                  <div className="check-wrapper">
+                    <input
+                      id="sponsored-shortcuts"
+                      className="sponsored-checkbox"
+                      disabled={!topSitesEnabled}
+                      checked={showSponsoredTopSitesEnabled}
+                      type="checkbox"
+                      onChange={this.onPreferenceSelect}
+                      preference="showSponsoredTopSites"
+                    />
+                    <label
+                      className="sponsored"
+                      htmlFor="sponsored-shortcuts"
+                      data-l10n-id="newtab-custom-sponsored-sites"
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+          <label className="switch">
+            <input
+              checked={topSitesEnabled}
+              type="checkbox"
+              onChange={this.onPreferenceSelect}
+              preference="feeds.topsites"
+            />
+            <span className="slider"></span>
+          </label>
+        </div>
+
+        {this.props.pocketRegion && (
+          <div id="pocket-section" className="section">
+            <div>
+              <h2 className="title" data-l10n-id="newtab-custom-pocket-title" />
+              <p
+                className="subtitle"
+                data-l10n-id="newtab-custom-pocket-subtitle"
+              />
+              <div
+                className={`more-info-pocket-wrapper ${
+                  pocketEnabled ? "" : "shrink"
+                }`}
+              >
+                <div
+                  className={`more-information ${
+                    pocketEnabled ? "expand" : "shrink"
+                  }`}
+                >
+                  <div className="check-wrapper">
+                    <input
+                      id="sponsored-pocket"
+                      className="sponsored-checkbox"
+                      disabled={!pocketEnabled}
+                      checked={showSponsoredPocketEnabled}
+                      type="checkbox"
+                      onChange={this.onPreferenceSelect}
+                      preference="showSponsored"
+                    />
+                    <label
+                      className="sponsored"
+                      htmlFor="sponsored-pocket"
+                      data-l10n-id="newtab-custom-pocket-sponsored"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <label className="switch">
+              <input
+                checked={pocketEnabled}
+                type="checkbox"
+                onChange={this.onPreferenceSelect}
+                preference="feeds.section.topstories"
+              />
+              <span className="slider"></span>
+            </label>
+          </div>
+        )}
+
+        <div id="recent-section" className="section">
+          <div>
+            <h2 className="title" data-l10n-id="newtab-custom-recent-title" />
+            <p
+              className="subtitle"
+              data-l10n-id="newtab-custom-recent-subtitle"
+            />
+          </div>
+          <label className="switch">
+            <input
+              checked={highlightsEnabled}
+              type="checkbox"
+              onChange={this.onPreferenceSelect}
+              preference="feeds.section.highlights"
+            />
+            <span className="slider"></span>
+          </label>
+        </div>
+
+        <div id="snippets-section" className="section">
+          <div>
+            <h2 className="title" data-l10n-id="newtab-custom-snippets-title" />
+            <p
+              className="subtitle"
+              data-l10n-id="newtab-custom-snippets-subtitle"
+            />
+          </div>
+          <label className="switch">
+            <input
+              checked={snippetsEnabled}
+              type="checkbox"
+              onChange={this.onPreferenceSelect}
+              preference="feeds.snippets"
+            />
+            <span className="slider"></span>
+          </label>
+        </div>
+
+        <span className="divider"></span>
+
+        <div>
+          <button
+            id="settings-link"
+            className="external-link"
+            onClick={this.props.openPreferences}
+            data-l10n-id="newtab-custom-settings"
+          />
+        </div>
+      </div>
+    );
   }
 }
