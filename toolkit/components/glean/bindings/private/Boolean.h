c1119a3d8128e1b16dc23e575e92ce69af2fd17c
Jan-Erik Rediger <jrediger@mozilla.com>
2020-12-10 13:20:37 +0000

    Bug 1678583 - Apply same cleanup to boolean metric. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D99035

--- toolkit/components/glean/bindings/private/Boolean.h
+++ toolkit/components/glean/bindings/private/Boolean.h
@@ -29,24 +29,6 @@ class BooleanMetric {
   /**
    * **Test-only API**
    *
-   * Tests whether a value is stored for the metric.
-   *
-   * This function will attempt to await the last parent-process task (if any)
-   * writing to the the metric's storage engine before returning a value.
-   * This function will not wait for data from child processes.
-   *
-   * Parent process only. Panics in child processes.
-   *
-   * @param aStorageName the name of the ping to retrieve the metric for.
-   * @return true if metric value exists, otherwise false
-   */
-  bool TestHasValue(const char* aStorageName) const {
-    return fog_boolean_test_has_value(mId, aStorageName) != 0;
-  }
-
-  /**
-   * **Test-only API**
-   *
    * Gets the currently stored value as a boolean.
    *
    * This function will attempt to await the last parent-process task (if any)
@@ -58,8 +40,11 @@ class BooleanMetric {
    *
    * @return value of the stored metric.
    */
-  bool TestGetValue(const char* aStorageName) const {
-    return fog_boolean_test_get_value(mId, aStorageName) != 0;
+  Maybe<bool> TestGetValue(const nsACString& aStorageName) const {
+    if (!fog_boolean_test_has_value(mId, &aStorageName)) {
+      return Nothing();
+    }
+    return Some(fog_boolean_test_get_value(mId, &aStorageName));
   }
 
  private:
