3173c39eb974aa69ced901070bfcc21462663516
Xidorn Quan <me@upsuper.org>
2020-12-11 14:34:23 +0000

    Bug 1681935 - Replace dependency to failure with thiserror in remote crate. r=remote-protocol-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D99442

--- remote/lib.rs
+++ remote/lib.rs
@@ -2,12 +2,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-extern crate failure;
 extern crate http;
 extern crate libc;
 extern crate log;
 extern crate nserror;
 extern crate nsstring;
+extern crate thiserror;
 extern crate xpcom;
 
 mod error;
