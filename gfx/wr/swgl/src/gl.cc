cbc50db9ebf9325782a345313bdb59b40f4bb077
Lee Salzman <lsalzman@mozilla.com>
2020-12-12 01:11:36 +0000

    Bug 1678938 - nudge clipped coordinates inside clip boundary. r=jimb
    Differential Revision: https://phabricator.services.mozilla.com/D99008

--- gfx/wr/swgl/src/gl.cc
+++ gfx/wr/swgl/src/gl.cc
@@ -3748,7 +3748,13 @@ static int clip_side(int nump, Point3D* p, Interpolants* interp, Point3D* outP,
         assert(numClip < nump + 2);
         float prevDist = prevCoord - prevSide * prev.w;
         float curDist = curCoord - prevSide * cur.w;
-        float k = prevDist / (prevDist - curDist);
+        // It may happen that after we interpolate by the weight k that due to
+        // floating point rounding we've underestimated the value necessary to
+        // push it over the clipping boundary. Just in case, nudge the mantissa
+        // by a single increment so that we essentially round it up and move it
+        // further inside the clipping boundary. We use nextafter to do this in
+        // a portable fashion.
+        float k = nextafterf(prevDist / (prevDist - curDist), 1.0f);
         outP[numClip] = prev + (cur - prev) * k;
         outInterp[numClip] = prevInterp + (curInterp - prevInterp) * k;
         numClip++;
@@ -3760,7 +3766,9 @@ static int clip_side(int nump, Point3D* p, Interpolants* interp, Point3D* outP,
         assert(numClip < nump + 2);
         float prevDist = prevCoord - curSide * prev.w;
         float curDist = curCoord - curSide * cur.w;
-        float k = prevDist / (prevDist - curDist);
+        // Calculate interpolation weight k and the nudge it inside clipping
+        // boundary with nextafter.
+        float k = nextafterf(prevDist / (prevDist - curDist), 1.0f);
         outP[numClip] = prev + (cur - prev) * k;
         outInterp[numClip] = prevInterp + (curInterp - prevInterp) * k;
         numClip++;
