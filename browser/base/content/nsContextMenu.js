4edd5a2c6d16103f4860e3beb7be8435c9e83ee6
Masatoshi Kimura <VYV03354@nifty.ne.jp>
2020-12-12 21:28:13 +0000

    Bug 1681642 - Improve error messages when extensions blocked downloads. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D99592

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1472,7 +1472,26 @@ class nsContextMenu {
             );
 
             const title = bundle.GetStringFromName("downloadErrorAlertTitle");
-            const msg = bundle.GetStringFromName("downloadErrorGeneric");
+            let msg = bundle.GetStringFromName("downloadErrorGeneric");
+
+            try {
+              const channel = aRequest.QueryInterface(Ci.nsIChannel);
+              const reason = channel.loadInfo.requestBlockingReason;
+              if (
+                reason == Ci.nsILoadInfo.BLOCKING_REASON_EXTENSION_WEBREQUEST
+              ) {
+                try {
+                  const properties = channel.QueryInterface(Ci.nsIPropertyBag);
+                  const id = properties.getProperty("cancelledByExtension");
+                  msg = bundle.formatStringFromName("downloadErrorBlockedBy", [
+                    WebExtensionPolicy.getByID(id).name,
+                  ]);
+                } catch (err) {
+                  // "cancelledByExtension" doesn't have to be available.
+                  msg = bundle.GetStringFromName("downloadErrorExtension");
+                }
+              }
+            } catch (ex) {}
 
             let window = Services.wm.getOuterWindowWithId(windowID);
             Services.prompt.alert(window, title, msg);
