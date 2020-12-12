ec1907c77cf3a10f5cd6c67a81d7ba909c4b3202
Tooru Fujisawa <arai_a@mac.com>
2020-12-12 01:30:35 +0000

    Bug 1677354 - Part 2: Assert all non-top-level scripts are function. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D97112

---
+++ js/src/jit-test/tests/parser/optimized-out-functions.js
@@ -0,0 +1,7 @@
+// Functions optimized out by constant-folding should have correct
+// stencil data.
+// This shouldn't hit any assertion while iterating stencil while instantiation.
+(function() {
+  (function() {
+  });
+});
