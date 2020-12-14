39b1b83db692c6d154a90bff383f7ac4600fa43f
Kenrick <kenrick95@gmail.com>
2020-12-14 23:02:32 +0000

    Bug 1678616 - Fix elipsis menu of about:logins no longer dismissable after menu button is no longer in focus r=sfoster,tgiles
    Differential Revision: https://phabricator.services.mozilla.com/D97882

--- browser/components/aboutlogins/content/components/menu-button.js
+++ browser/components/aboutlogins/content/components/menu-button.js
@@ -27,9 +27,8 @@ export default class MenuButton extends HTMLElement {
     this._menu = this.shadowRoot.querySelector(".menu");
     this._menuButton = this.shadowRoot.querySelector(".menu-button");
 
-    this.addEventListener("blur", this);
     this._menuButton.addEventListener("click", this);
-    this.addEventListener("keydown", this, true);
+    document.addEventListener("keydown", this, true);
   }
 
   handleEvent(event) {
@@ -82,6 +81,15 @@ export default class MenuButton extends HTMLElement {
           // So that the menu isn't closed when non-buttons (e.g. separators, paddings) are clicked
           this._hideMenu();
         }
+
+        // Explicitly close menu at the catch-all click event (i.e. a click outside of the menu)
+        if (
+          !this._menu.contains(event.originalTarget) &&
+          !this._menuButton.contains(event.originalTarget)
+        ) {
+          this._hideMenu();
+        }
+
         break;
       }
       case "keydown": {
@@ -95,10 +103,10 @@ export default class MenuButton extends HTMLElement {
       event.preventDefault();
       this._toggleMenu();
       this._focusSuccessor(true);
-    } else if (event.key == "Escape") {
+    } else if (event.key == "Escape" && !this._menu.hidden) {
       this._hideMenu();
       this._menuButton.focus();
-    } else if (event.key.startsWith("Arrow")) {
+    } else if (event.key.startsWith("Arrow") && !this._menu.hidden) {
       event.preventDefault();
       this._focusSuccessor(event.key == "ArrowDown");
     }
@@ -141,6 +149,8 @@ export default class MenuButton extends HTMLElement {
 
   _hideMenu() {
     this._menu.hidden = true;
+
+    this.removeEventListener("blur", this);
     document.documentElement.removeEventListener("click", this, true);
   }
 
@@ -150,7 +160,8 @@ export default class MenuButton extends HTMLElement {
 
     this._menu.hidden = false;
 
-    // Add a catch-all event listener to close the menu.
+    // Event listeners to close the menu
+    this.addEventListener("blur", this);
     document.documentElement.addEventListener("click", this, true);
   }
 
