11631b01cdc621e3c5e6275d569660549f4ba6f8
Bob Clary <bclary@bclary.com>
2020-12-14 20:10:22 +0000

    Bug 1681164 - python3 - awsy - python3 fixes. r=perftest-reviewers,jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D99047

--- testing/awsy/mach_commands.py
+++ testing/awsy/mach_commands.py
@@ -9,6 +9,8 @@ import logging
 import os
 import sys
 
+import six
+
 from mozbuild.base import (
     MachCommandBase,
     MachCommandConditions as conditions,
@@ -112,7 +114,7 @@ class MachCommands(MachCommandBase):
         else:
             kwargs["testvars"] = [runtime_testvars_path]
 
-        runtime_testvars_file = open(runtime_testvars_path, "wb")
+        runtime_testvars_file = open(runtime_testvars_path, "wb" if six.PY2 else "w")
         runtime_testvars_file.write(json.dumps(runtime_testvars, indent=2))
         runtime_testvars_file.close()
 
