105c8c96f520dd61b3f661d52879575625578c24
Bob Clary <bclary@bclary.com>
2020-12-14 18:26:00 +0000

    Bug 1681101 - python3 - testing/web-platform/tests - base64.decodestring -> base64.decodebytes. r=webdriver-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D98960

--- testing/web-platform/tests/webdriver/tests/print/user_prompts.py
+++ testing/web-platform/tests/webdriver/tests/print/user_prompts.py
@@ -3,12 +3,17 @@ import base64
 
 import pytest
 
-from six import ensure_binary
+import six
 
 from tests.support.asserts import assert_dialog_handled, assert_error, assert_success
 from tests.support.inline import inline
 from .printcmd import do_print, assert_pdf
 
+
+def decodebytes(s):
+    if six.PY3:
+        return base64.decodebytes(six.ensure_binary(s))
+    return base64.decodestring(s)
 
 @pytest.fixture
 def check_user_prompt_closed_without_exception(session, create_dialog):
@@ -20,7 +25,7 @@ def check_user_prompt_closed_without_exception(session, create_dialog):
         response = do_print(session, {})
         value = assert_success(response)
 
-        pdf = base64.decodestring(ensure_binary(value))
+        pdf = decodebytes(six.ensure_binary(value))
         assert_dialog_handled(session, expected_text=dialog_type, expected_retval=retval)
 
         assert_pdf(pdf)
