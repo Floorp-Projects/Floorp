4b1ffabfd8dae39bcf6a7f7a050e8964af2eb8c8
Nicolas Chevobbe <nchevobbe@mozilla.com>
2020-12-15 08:48:19 +0000

    Bug 1681055 - [devtools] Update beautify-js to 1.13.0. r=ochameau.
    This fix pretty-printing of async functions in console. A test case is added
    to make sure we don't regress.
    
    We used to copy the library tests and run them in xpcshell. The tests changed
    a lot, and I don't think we get much value running tests that are already ran on
    the project CI (we do have a few tests that checks that we get the output we want)
    , so this patch remove the xpcshell test and the associated files.
    
    The upgrade documentation is updated to remove some unecessary steps:
    - no need to rename the exported module for each file
    - no need to replace the acorn module, since what's in the file is just a subset
      of the library (~100 lines)
    - no need to update the test file, which doesn't seem to exist anymore
    
    Differential Revision: https://phabricator.services.mozilla.com/D99320

--- devtools/shared/jsbeautify/beautify.js
+++ devtools/shared/jsbeautify/beautify.js
@@ -1,7 +1,9 @@
-var { cssBeautify } = require("devtools/shared/jsbeautify/src/beautify-css");
-var { htmlBeautify } = require("devtools/shared/jsbeautify/src/beautify-html");
-var { jsBeautify } = require("devtools/shared/jsbeautify/src/beautify-js");
+const { css_beautify } = require("devtools/shared/jsbeautify/src/beautify-css");
+const {
+  html_beautify,
+} = require("devtools/shared/jsbeautify/src/beautify-html");
+const { js_beautify } = require("devtools/shared/jsbeautify/src/beautify-js");
 
-exports.css = cssBeautify;
-exports.html = htmlBeautify;
-exports.js = jsBeautify;
+exports.css = css_beautify;
+exports.html = html_beautify;
+exports.js = js_beautify;
