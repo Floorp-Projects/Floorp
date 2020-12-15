1adb52cfaf8f383dd7919980481bf57440a8f1d4
Henrik Skupin <mail@hskupin.info>
2020-12-15 14:02:03 +0000

    Bug 1669174 - [marionette] Don't use Framescript when actors are enabled. r=marionette-reviewers,jgraham
    Differential Revision: https://phabricator.services.mozilla.com/D99586

--- testing/marionette/driver.js
+++ testing/marionette/driver.js
@@ -142,10 +142,12 @@ this.GeckoDriver = function(server) {
   this.capabilities = new Capabilities();
 
   this.mm = globalMessageManager;
-  this.listener = proxy.toListener(
-    this.sendAsync.bind(this),
-    () => this.curBrowser
-  );
+  if (!MarionettePrefs.useActors) {
+    this.listener = proxy.toListener(
+      this.sendAsync.bind(this),
+      () => this.curBrowser
+    );
+  }
 
   // used for modal dialogs or tab modal alerts
   this.dialog = null;
@@ -277,7 +279,7 @@ GeckoDriver.prototype.QueryInterface = ChromeUtils.generateQI([
 
 GeckoDriver.prototype.init = function() {
   if (MarionettePrefs.useActors) {
-    // When using JSWindowActors, we should not rely on framescript events
+    // When using JSWindowActors, we are not relying on framescript events
     return;
   }
 
@@ -547,6 +549,11 @@ GeckoDriver.prototype.startBrowser = function(window, isNewSession = false) {
  *     True if this is the first time we're talking to this browser.
  */
 GeckoDriver.prototype.whenBrowserStarted = function(window, isNewSession) {
+  // Do not load the framescript when actors are used.
+  if (MarionettePrefs.useActors) {
+    return;
+  }
+
   let mm = window.messageManager;
   if (mm) {
     if (!isNewSession) {
@@ -802,12 +809,6 @@ GeckoDriver.prototype.newSession = async function(cmd) {
     logger.info("Preemptively starting accessibility service in Chrome");
   }
 
-  let registerBrowsers, browserListening;
-  if (!MarionettePrefs.useActors) {
-    registerBrowsers = this.registerPromise();
-    browserListening = this.listeningPromise();
-  }
-
   let waitForWindow = function() {
     let windowTypes;
     switch (this.appId) {
@@ -859,6 +860,16 @@ GeckoDriver.prototype.newSession = async function(cmd) {
     }
   };
 
+  let registerBrowsers;
+  let browserListening;
+
+  if (MarionettePrefs.useActors) {
+    registerCommandsActor();
+  } else {
+    registerBrowsers = this.registerPromise();
+    browserListening = this.listeningPromise();
+  }
+
   if (!MarionettePrefs.contentListener) {
     waitForWindow.call(this);
   } else if (this.appId != APP_ID_FIREFOX && this.curBrowser === null) {
@@ -884,10 +895,6 @@ GeckoDriver.prototype.newSession = async function(cmd) {
   } else {
     await registerBrowsers;
     await browserListening;
-  }
-
-  if (MarionettePrefs.useActors) {
-    registerCommandsActor();
   }
 
   if (this.mainFrame) {
@@ -3064,7 +3071,12 @@ GeckoDriver.prototype.closeChromeWindow = async function() {
 
 /** Delete Marionette session. */
 GeckoDriver.prototype.deleteSession = function() {
-  if (this.curBrowser !== null) {
+  if (MarionettePrefs.useActors) {
+    clearActionInputState();
+    clearElementIdCache();
+
+    unregisterCommandsActor();
+  } else if (this.curBrowser !== null) {
     // frame scripts can be safely reused
     MarionettePrefs.contentListener = false;
 
@@ -3080,13 +3092,6 @@ GeckoDriver.prototype.deleteSession = function() {
         );
       }
     }
-  }
-
-  if (MarionettePrefs.useActors) {
-    clearElementIdCache();
-    clearActionInputState();
-
-    unregisterCommandsActor();
   }
 
   // reset to the top-most frame, and clear browsing context references
@@ -3705,10 +3710,6 @@ GeckoDriver.prototype.receiveMessage = function(message) {
       return { frameId: message.json.frameId };
 
     case "Marionette:ListenersAttached":
-      if (MarionettePrefs.useActors) {
-        return;
-      }
-
       if (message.json.frameId === this.curBrowser.curFrameId) {
         const browsingContext = BrowsingContext.get(message.json.frameId);
 
