5f456b29856c2cdca47c22f2d65e139c7d255872
Butkovits Atila <abutkovits@mozilla.com>
2020-12-15 10:43:44 +0200

    Backed out changeset ab835b3d50f9 (bug 1672160) for causing bustage on codegen-x64-test.js. CLOSED TREE

--- js/src/wasm/WasmConstants.h
+++ js/src/wasm/WasmConstants.h
@@ -700,37 +700,7 @@ enum class SimdOp {
   F32x4ConvertUI32x4 = 0xfb,
   V128Load32Zero = 0xfc,
   V128Load64Zero = 0xfd,
-// Unused = 0xfe and up
-
-// Mozilla extensions, highly experimental and platform-specific
-#ifdef ENABLE_WASM_SIMD_WORMHOLE
-  // The wormhole is a mechanism for injecting experimental, possibly
-  // platform-dependent, opcodes into the generated code.  A wormhole op is
-  // expressed as a two-operation SIMD shuffle op with the pattern <31, 0, 30,
-  // 2, 29, 4, 28, 6, 27, 8, 26, 10, 25, 12, 24, X> where X is the opcode,
-  // 0..31, from the set below.  If an operation uses no operands, the operands
-  // to the shuffle opcode should be const 0.  If an operation uses one operand,
-  // the operands to the shuffle opcode should both be that operand.
-  //
-  // The wormhole must be enabled by a flag and is only supported by ion on x64,
-  // baseline must be disabled.
-  //
-  // The benefit of this mechanism is that it allows experimental opcodes to be
-  // used without updating other tools (compilers, linkers, optimizers).
-  //
-  // These opcodes can be rearranged but the X values associated with them must
-  // remain fixed.
-
-  // X=0, selftest opcode.  No operands.  The result is an 8x16 hex value:
-  // DEADD00DCAFEBABE.
-  MozWHSELFTEST = 0x200,
-
-  // X=1, Intel SSE3 PMADDUBSW instruction. Two operands.
-  MozWHPMADDUBSW = 0x201,
-
-  // X=2, Intel SSE2 PMADDWD instruction. Two operands.
-  MozWHPMADDWD = 0x202,
-#endif
+  // Unused = 0xfe and up
 
   Limit
 };
