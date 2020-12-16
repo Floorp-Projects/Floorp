b8e7c6b5fa97c05407d55eb928ae0b8a05e583f5
Simon Giesecke <sgiesecke@mozilla.com>
2020-12-16 19:10:57 +0000

    Bug 1682542 - Make use of standard algorithms in dom/cache. r=dom-workers-and-storage-reviewers,asuth
    Specifically, instead of using custom loops
    - use std::find_if in CacheOpParent::ProcessCrossOriginResourcePolicyHeader
    - use ReduceEach in SetupAction::RunSyncWithDBOnTarget
    - use std::any_of in Manager::RemoveContext
    - use std::find_if in Manager::(Add|Release)Ref(Cache|Body)Id and
      Manager::Set(Cache|Body)IdOrphanedIfRefed
    - use std::copy_if in Manager::NoteOrphanedBodyIdList
    - use std::any_of in StreamList::ShouldOpenStreamFor
    - use std::find_if in StreamList::Extract, StreamList::NoteClosed
    - use std::exchange in StreamList::CloseAll
    - use std::transform in ToHeadersEntryList and change it to return a value
      rather than modifying an output parameter
    - use std::transform in TypeUtils::ToInternalHeaders
    
    Differential Revision: https://phabricator.services.mozilla.com/D99789

--- dom/quota/QuotaCommon.h
+++ dom/quota/QuotaCommon.h
@@ -842,6 +842,23 @@ auto ReduceEach(InputGenerator aInputGenerator, T aInit,
   return std::move(res);
 }
 
+// This is like std::reduce with a to-be-defined execution policy (we don't want
+// to std::terminate on an error, but probably it's fine to just propagate any
+// error that occurred).
+template <typename Range, typename T, typename BinaryOp>
+auto Reduce(Range&& aRange, T aInit, const BinaryOp& aBinaryOp) {
+  using std::begin;
+  using std::end;
+  return ReduceEach(
+      [it = begin(aRange), end = end(aRange)]() mutable {
+        auto res = ToMaybeRef(it != end ? &*it++ : nullptr);
+        return Result<decltype(res), typename std::invoke_result_t<
+                                         BinaryOp, T, decltype(res)>::err_type>(
+            res);
+      },
+      aInit, aBinaryOp);
+}
+
 template <typename Range, typename Body>
 auto CollectEachInRange(const Range& aRange, const Body& aBody)
     -> Result<mozilla::Ok, nsresult> {
