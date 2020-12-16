da65c94d99cc728210e114a36218fab8f1f112bb
Csoregi Natalia <ncsoregi@mozilla.com>
2020-12-17 00:50:20 +0200

    Backed out 2 changesets (bug 1253129) for causing failures on browser_ext_windows_create_params.js. CLOSED TREE
    Backed out changeset ac8d0372dcb1 (bug 1253129)
    Backed out changeset 016006d107b6 (bug 1253129)

--- browser/components/extensions/test/browser/browser_ext_windows_create_params.js
+++ browser/components/extensions/test/browser/browser_ext_windows_create_params.js
@@ -2,12 +2,6 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-const { AddonTestUtils } = ChromeUtils.import(
-  "resource://testing-common/AddonTestUtils.jsm"
-);
-
-AddonTestUtils.initMochitest(this);
-
 // Tests that incompatible parameters can't be used together.
 add_task(async function testWindowCreateParams() {
   let extension = ExtensionTestUtils.loadExtension({
@@ -36,81 +30,4 @@ add_task(async function testWindowCreateParams() {
   await extension.startup();
   await extension.awaitFinish("window-create-params");
   await extension.unload();
-});
-
-// We do not support the focused param, however we do not want
-// to fail despite an error when it is passed.  This provides
-// better code level compatibility with chrome.
-add_task(async function testWindowCreateFocused() {
-  let extension = ExtensionTestUtils.loadExtension({
-    async background() {
-      async function awaitFocused(windowId) {
-        return new Promise(resolve => {
-          browser.windows.onFocusChanged.addListener(async function listener(
-            wid
-          ) {
-            browser.test.log(`==== window focus ${windowId} ${wid}`);
-            if (windowId == wid) {
-              browser.windows.onFocusChanged.removeListener(listener);
-              // update the window object
-              let window = await browser.windows.get(windowId);
-              resolve(window);
-            }
-          });
-        });
-      }
-      try {
-        let window = await browser.windows.create({});
-        window = await awaitFocused(window.id);
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused without focused param"
-        );
-        browser.test.log("removeWindow");
-        await browser.windows.remove(window.id);
-        window = await browser.windows.create({ focused: true });
-        window = await awaitFocused(window.id);
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused with focused: true"
-        );
-        browser.test.log("removeWindow");
-        await browser.windows.remove(window.id);
-        window = await browser.windows.create({ focused: false });
-        window = await awaitFocused(window.id);
-        browser.test.assertEq(
-          window.focused,
-          true,
-          "window is focused with focused: false"
-        );
-        browser.test.log("removeWindow");
-        await browser.windows.remove(window.id);
-        browser.test.notifyPass("window-create-params");
-      } catch (e) {
-        browser.test.fail(`${e} :: ${e.stack}`);
-        browser.test.notifyFail("window-create-params");
-      }
-    },
-  });
-
-  ExtensionTestUtils.failOnSchemaWarnings(false);
-  let { messages } = await AddonTestUtils.promiseConsoleOutput(async () => {
-    await extension.startup();
-    await extension.awaitFinish("window-create-params");
-    await extension.unload();
-  });
-
-  AddonTestUtils.checkMessages(
-    messages,
-    {
-      expected: [
-        /Warning processing focused: Opening inactive windows is not supported./,
-      ],
-    },
-    "Expected warning processing focused"
-  );
-
-  ExtensionTestUtils.failOnSchemaWarnings(true);
 });
