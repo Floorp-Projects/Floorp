89a8ae36f8fcbd0456120108baa400ab39168774
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2020-12-16 19:14:07 +0000

    Bug 1682887 - roll out 2h2020 bookmarks pref to 100% on 85 and later, r=jaws
    Differential Revision: https://phabricator.services.mozilla.com/D99909

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -2091,13 +2091,9 @@ pref("browser.toolbars.bookmarks.visibility", "newtab");
 pref("browser.toolbars.bookmarks.showOtherBookmarks", true);
 
 // When true, this pref will always show the bookmarks bar on
-// the New Tab Page, allowing showing/hiding via keyboard shortcut,
-// and other functionality to improve the usage of the Bookmarks Toolbar.
-#ifdef EARLY_BETA_OR_EARLIER
+// the New Tab Page, and other functionality to improve the usage of the
+// Bookmarks Toolbar.
 pref("browser.toolbars.bookmarks.2h2020", true);
-#else
-pref("browser.toolbars.bookmarks.2h2020", false);
-#endif
 
 // Prefs to control the Firefox Account toolbar menu.
 // This pref will surface existing Firefox Account information
