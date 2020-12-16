29c054ab8301aaf8f0c7b6379e5b84b71476ce33
Simon Giesecke <sgiesecke@mozilla.com>
2020-12-16 19:09:58 +0000

    Bug 1679987 - Removed unused includes of nsCharSeparatedTokenizer.h. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D99877

--- dom/filesystem/Directory.cpp
+++ dom/filesystem/Directory.cpp
@@ -9,7 +9,6 @@
 #include "GetDirectoryListingTask.h"
 #include "GetFilesTask.h"
 
-#include "nsCharSeparatedTokenizer.h"
 #include "nsIFile.h"
 #include "nsString.h"
 #include "mozilla/dom/BlobImpl.h"
