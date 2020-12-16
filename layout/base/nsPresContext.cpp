9062f002930d84aa753acac7a03b606e35e1bdb9
Steven MacLeod <steven@smacleod.ca>
2020-12-16 01:06:47 +0000

    Bug 1656107 - remove FindContentForSubDocument use from nsPresContext. r=farre
    Differential Revision: https://phabricator.services.mozilla.com/D98618

--- layout/base/nsPresContext.cpp
+++ layout/base/nsPresContext.cpp
@@ -680,8 +680,8 @@ nsresult nsPresContext::Init(nsDeviceContext* aDeviceContext) {
       // XXX the document can change in AttachPresShell, does this work?
       dom::BrowsingContext* browsingContext = mDocument->GetBrowsingContext();
       if (browsingContext && !browsingContext->IsTop()) {
-        Element* containingElement =
-            parent->FindContentForSubDocument(mDocument);
+        MOZ_ASSERT(!browsingContext->IsCached());
+        Element* containingElement = browsingContext->GetEmbedderElement();
         if (!containingElement->IsXULElement() ||
             !containingElement->HasAttr(kNameSpaceID_None,
                                         nsGkAtoms::forceOwnRefreshDriver)) {
