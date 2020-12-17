ae6d4f0915f16efe39307db0ab4af871e6418d21
Mike Hommey <mh+mozilla@glandium.org>
2020-12-17 10:04:18 +0000

    Bug 1682566 - Work around rust miscompilation in webrender on arm64 mac with bug 1682365. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D99986

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -1212,8 +1212,12 @@ impl BatchBuilder {
                     render_tasks,
                 ).unwrap();
 
+                // The run.used_font.clone() is here instead of instead of inline in the `fetch_glyph`
+                // function call to work around a miscompilation.
+                // https://github.com/rust-lang/rust/issues/80111
+                let font = run.used_font.clone();
                 ctx.resource_cache.fetch_glyphs(
-                    run.used_font.clone(),
+                    font,
                     &glyph_keys,
                     &mut self.glyph_fetch_buffer,
                     gpu_cache,
