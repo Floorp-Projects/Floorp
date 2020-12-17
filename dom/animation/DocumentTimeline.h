f78a1ad6271f6a3a6e5cba44778dc785a8c382c6
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2020-12-17 00:51:18 +0000

    Bug 1674776 - Update DocumentTimeline::mLastRefreshDriverTime outside parallel styling. r=boris,decoder
    Note that we can probably use mLastRefreshDriverTime directly in
    DocumentTimeline::GetCurrentTimeStamp(), i.e. we don't need to use the refresh
    driver there, but I'd preserve the current behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D97823

--- dom/animation/DocumentTimeline.h
+++ dom/animation/DocumentTimeline.h
@@ -68,6 +68,8 @@ class DocumentTimeline final : public AnimationTimeline,
 
   Document* GetDocument() const override { return mDocument; }
 
+  void UpdateLastRefreshDriverTime();
+
  protected:
   TimeStamp GetCurrentTimeStamp() const;
   nsRefreshDriver* GetRefreshDriver() const;
@@ -81,7 +83,7 @@ class DocumentTimeline final : public AnimationTimeline,
   // The most recently used refresh driver time. This is used in cases where
   // we don't have a refresh driver (e.g. because we are in a display:none
   // iframe).
-  mutable TimeStamp mLastRefreshDriverTime;
+  TimeStamp mLastRefreshDriverTime;
   bool mIsObservingRefreshDriver;
 
   TimeDuration mOriginTime;
