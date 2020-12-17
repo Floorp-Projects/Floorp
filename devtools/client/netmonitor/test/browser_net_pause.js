ed78ad3373489006d0795a989b0b60672f102e10
Hubert Boma Manilla <hmanilla@mozilla.com>
2020-12-17 17:38:19 +0000

    Bug 1683152 - [devtools] Handle netmonitor pause/resume on the client r=ochameau
    Fixes browser_net_pause.js
    
    Differential Revision: https://phabricator.services.mozilla.com/D99621

--- devtools/client/netmonitor/test/browser_net_pause.js
+++ devtools/client/netmonitor/test/browser_net_pause.js
@@ -7,10 +7,12 @@
  * Tests if the pause/resume button works.
  */
 add_task(async function() {
-  const { tab, monitor } = await initNetMonitor(PAUSE_URL, { requestCount: 1 });
+  const { tab, monitor, toolbox } = await initNetMonitor(PAUSE_URL, {
+    requestCount: 1,
+  });
   info("Starting test... ");
 
-  const { document, store, windowRequire, connector } = monitor.panelWin;
+  const { document, store, windowRequire } = monitor.panelWin;
   const Actions = windowRequire("devtools/client/netmonitor/src/actions/index");
   const pauseButton = document.querySelector(".requests-list-pause-button");
 
@@ -34,7 +36,7 @@ add_task(async function() {
 
   // Click pause, load second request and make sure they don't show up.
   EventUtils.sendMouseEvent({ type: "click" }, pauseButton);
-  await performPausedRequest(connector, tab, monitor);
+  await performPausedRequest(tab, monitor, toolbox);
   ok(noRequest, "There should be no activity when paused.");
   assertRequestCount(store, 1);
 
@@ -47,8 +49,9 @@ add_task(async function() {
   // Click pause, reload the page and check that there are
   // some requests. Page reload should auto-resume.
   EventUtils.sendMouseEvent({ type: "click" }, pauseButton);
+  const networkEvents = waitForNetworkEvents(monitor, 1);
   tab.linkedBrowser.reload();
-  await waitForNetworkEvents(monitor, 1);
+  await networkEvents;
   assertRequestCount(store, 1);
 
   return teardown(monitor);
@@ -81,10 +84,14 @@ async function performRequestAndWait(tab, monitor) {
 /**
  * Execute simple GET request
  */
-async function performPausedRequest(connector, tab, monitor) {
+async function performPausedRequest(tab, monitor, toolbox) {
+  const waitForEventWhenPaused = waitForNetworkResource(toolbox);
   await SpecialPowers.spawn(tab.linkedBrowser, [SIMPLE_SJS], async function(
     url
   ) {
     await content.wrappedJSObject.performRequests(url);
   });
+  // Wait for NETWORK_EVENT resources to be fetched, in order to ensure
+  // that there is no pending request related to their processing.
+  await waitForEventWhenPaused;
 }
