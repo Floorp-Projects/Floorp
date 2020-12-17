e8b23775a35d4c841adebe0970ff3c5c7374b7b2
Simon Giesecke <sgiesecke@mozilla.com>
2020-12-17 16:14:52 +0000

    Bug 1682536 - Use references/NotNull in CacheWorkerRef. r=dom-workers-and-storage-reviewers,asuth
    Differential Revision: https://phabricator.services.mozilla.com/D99774

--- dom/cache/CacheWorkerRef.cpp
+++ dom/cache/CacheWorkerRef.cpp
@@ -92,33 +92,31 @@ SafeRefPtr<CacheWorkerRef> CacheWorkerRef::PreferBehavior(
   return static_cast<bool>(replace) ? std::move(replace) : std::move(orig);
 }
 
-void CacheWorkerRef::AddActor(ActorChild* aActor) {
+void CacheWorkerRef::AddActor(ActorChild& aActor) {
   NS_ASSERT_OWNINGTHREAD(CacheWorkerRef);
-  MOZ_DIAGNOSTIC_ASSERT(aActor);
-  MOZ_ASSERT(!mActorList.Contains(aActor));
+  MOZ_ASSERT(!mActorList.Contains(&aActor));
 
-  mActorList.AppendElement(aActor);
+  mActorList.AppendElement(WrapNotNullUnchecked(&aActor));
 
   // Allow an actor to be added after we've entered the Notifying case.  We
   // can't stop the actor creation from racing with out destruction of the
   // other actors and we need to wait for this extra one to close as well.
   // Signal it should destroy itself right away.
   if (mNotified) {
-    aActor->StartDestroy();
+    aActor.StartDestroy();
   }
 }
 
-void CacheWorkerRef::RemoveActor(ActorChild* aActor) {
+void CacheWorkerRef::RemoveActor(ActorChild& aActor) {
   NS_ASSERT_OWNINGTHREAD(CacheWorkerRef);
-  MOZ_DIAGNOSTIC_ASSERT(aActor);
 
 #if defined(RELEASE_OR_BETA)
-  mActorList.RemoveElement(aActor);
+  mActorList.RemoveElement(&aActor);
 #else
-  MOZ_DIAGNOSTIC_ASSERT(mActorList.RemoveElement(aActor));
+  MOZ_DIAGNOSTIC_ASSERT(mActorList.RemoveElement(&aActor));
 #endif
 
-  MOZ_ASSERT(!mActorList.Contains(aActor));
+  MOZ_ASSERT(!mActorList.Contains(&aActor));
 
   if (mActorList.IsEmpty()) {
     mStrongWorkerRef = nullptr;
@@ -135,9 +133,8 @@ void CacheWorkerRef::Notify() {
 
   // Start the asynchronous destruction of our actors.  These will call back
   // into RemoveActor() once the actor is destroyed.
-  for (uint32_t i = 0; i < mActorList.Length(); ++i) {
-    MOZ_DIAGNOSTIC_ASSERT(mActorList[i]);
-    mActorList[i]->StartDestroy();
+  for (const auto& actor : mActorList) {
+    actor->StartDestroy();
   }
 }
 
