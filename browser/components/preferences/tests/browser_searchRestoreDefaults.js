e53251ac2be20d9663f01507d2c647c816bf6dcc
Butkovits Atila <abutkovits@mozilla.com>
2020-12-17 19:04:40 +0200

    Backed out changeset d5111a57ad25 (bug 1646128) for failure on browser_engines.js.

--- browser/components/preferences/tests/browser_searchRestoreDefaults.js
+++ browser/components/preferences/tests/browser_searchRestoreDefaults.js
@@ -9,7 +9,7 @@ const { SearchUtils } = ChromeUtils.import(
 );
 add_task(async function test_restore_functionality() {
   // Ensure no engines are hidden to begin with.
-  for (let engine of await Services.search.getAppProvidedEngines()) {
+  for (let engine of await Services.search.getDefaultEngines()) {
     if (engine.hidden) {
       engine.hidden = false;
     }
@@ -32,7 +32,7 @@ add_task(async function test_restore_functionality() {
 
   let tree = doc.querySelector("#engineList");
   // Check for default search engines to be displayed in the engineList
-  let defaultEngines = await Services.search.getAppProvidedEngines();
+  let defaultEngines = await Services.search.getDefaultEngines();
   for (let i = 0; i < defaultEngines.length; i++) {
     let cellName = tree.view.getCellText(
       i,
