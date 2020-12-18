d8aa8297fafd0bb2cc09a67c27d58b6a6cdd6ba1
Rob Wu <rob@robwu.nl>
2020-12-18 20:30:57 +0000

    Bug 1678734 - Fix crash in StreamFilter with view-source r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D100146

--- toolkit/components/extensions/webrequest/StreamFilterParent.cpp
+++ toolkit/components/extensions/webrequest/StreamFilterParent.cpp
@@ -18,6 +18,7 @@
 #include "nsSocketTransportService2.h"
 #include "nsStringStream.h"
 #include "mozilla/net/DocumentChannelChild.h"
+#include "nsIViewSourceChannel.h"
 
 namespace mozilla {
 namespace extensions {
@@ -156,7 +157,19 @@ void StreamFilterParent::Init(nsIChannel* aChannel) {
   mChannel = aChannel;
 
   nsCOMPtr<nsITraceableChannel> traceable = do_QueryInterface(aChannel);
-  MOZ_RELEASE_ASSERT(traceable);
+  if (MOZ_UNLIKELY(!traceable)) {
+    // nsViewSourceChannel is not nsITraceableChannel, but wraps one. Unwrap it.
+    nsCOMPtr<nsIViewSourceChannel> vsc = do_QueryInterface(aChannel);
+    if (vsc) {
+      traceable = do_QueryObject(vsc->GetInnerChannel());
+      // OnStartRequest etc. is passed the unwrapped channel, so update mChannel
+      // to prevent OnStartRequest from mistaking it for a redirect, which would
+      // close the filter.
+      mChannel = do_QueryObject(traceable);
+    }
+    // TODO bug 1683403: Replace assertion; Close StreamFilter instead.
+    MOZ_RELEASE_ASSERT(traceable);
+  }
 
   nsresult rv =
       traceable->SetNewListener(this, /* aMustApplyContentConversion = */ true,
