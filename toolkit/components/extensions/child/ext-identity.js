b86a5307650e493daef58a9109c4feb123fbde99
Brendan Early <mymindstorm@evermiss.net>
2020-12-18 14:31:21 +0000

    Bug 1635344 - Allow launchWebAuthFlow redirect URI to be set to loopback address. r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D88738

--- toolkit/components/extensions/child/ext-identity.js
+++ toolkit/components/extensions/child/ext-identity.js
@@ -49,6 +49,13 @@ this.identity = class extends ExtensionAPI {
           // Validate the url and retreive redirect_uri if it was provided.
           let url, redirectURI;
           let baseRedirectURL = this.getRedirectURL();
+
+          // Allow using loopback address for native OAuth flows as some
+          //  providers do not accept the URL provided by getRedirectURL.
+          // For more context, see bug 1635344.
+          let loopbackURL = `http://127.0.0.1/mozoauth2/${computeHash(
+            extension.id
+          )}`;
           try {
             url = new URL(details.url);
           } catch (e) {
@@ -58,7 +65,10 @@ this.identity = class extends ExtensionAPI {
             redirectURI = new URL(
               url.searchParams.get("redirect_uri") || baseRedirectURL
             );
-            if (!redirectURI.href.startsWith(baseRedirectURL)) {
+            if (
+              !redirectURI.href.startsWith(baseRedirectURL) &&
+              !redirectURI.href.startsWith(loopbackURL)
+            ) {
               return Promise.reject({ message: "redirect_uri not allowed" });
             }
           } catch (e) {
