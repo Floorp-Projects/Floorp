9b230b2b7b23ff9dd0d07bbe1a43d8713fb139e7
Andrew Erickson <aerickson@mozilla.com>
2020-12-18 21:38:02 +0000

    Bug 1665012: configure taskgraph to send jobs to s7g devices at bitbar r=jmaher
    Differential Revision: https://phabricator.services.mozilla.com/D100157

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -1764,6 +1764,11 @@ def set_worker_type(config, tasks):
                 task["worker-type"] = "t-bitbar-gw-unit-p2"
             else:
                 task["worker-type"] = "t-bitbar-gw-perf-p2"
+        elif test_platform.startswith("android-hw-s7"):
+            if task["suite"] != "raptor":
+                task["worker-type"] = "t-bitbar-gw-unit-s7"
+            else:
+                task["worker-type"] = "t-bitbar-gw-perf-s7"
         elif test_platform.startswith("android-em-7.0-x86"):
             task["worker-type"] = "t-linux-metal"
         elif test_platform.startswith("linux") or test_platform.startswith("android"):
