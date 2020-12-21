733b7050ffc0b507324ade350bbccf8917807221
Marco Bonardo <mbonardo@mozilla.com>
2020-12-21 20:56:29 +0000

    Bug 1682434 - Limit textruns and substring matching for search suggestions. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D99955

--- browser/components/urlbar/UrlbarResult.jsm
+++ browser/components/urlbar/UrlbarResult.jsm
@@ -240,11 +240,11 @@ class UrlbarResult {
     // For performance reasons limit excessive string lengths, to reduce the
     // amount of string matching we do here, and avoid wasting resources to
     // handle long textruns that the user would never see anyway.
-    for (let prop of ["displayUrl", "title"].filter(p => p in payloadInfo)) {
-      payloadInfo[prop][0] = payloadInfo[prop][0].substring(
-        0,
-        UrlbarUtils.MAX_TEXT_LENGTH
-      );
+    for (let prop of ["displayUrl", "title", "suggestion"]) {
+      let val = payloadInfo[prop]?.[0];
+      if (typeof val == "string") {
+        payloadInfo[prop][0] = val.substring(0, UrlbarUtils.MAX_TEXT_LENGTH);
+      }
     }
 
     let entries = Object.entries(payloadInfo);
--- browser/components/urlbar/tests/browser/browser_textruns.js
+++ browser/components/urlbar/tests/browser/browser_textruns.js
@@ -8,13 +8,29 @@
  */
 
 add_task(async function() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["browser.urlbar.suggest.searches", true]],
+  });
+  let engine = await Services.search.addEngineWithDetails("Test", {
+    template: "http://example.com/?search={searchTerms}",
+  });
+  let oldDefaultEngine = await Services.search.getDefault();
+  await Services.search.setDefault(engine);
+
   let lotsOfSpaces = "%20".repeat(300);
   await PlacesTestUtils.addVisits({
     uri: `https://textruns.mozilla.org/${lotsOfSpaces}/test/`,
     title: `A long ${lotsOfSpaces} title`,
   });
+  await UrlbarTestUtils.formHistory.add([
+    { value: `A long ${lotsOfSpaces} textruns suggestion` },
+  ]);
+
   registerCleanupFunction(async function() {
     await PlacesUtils.history.clear();
+    await UrlbarTestUtils.formHistory.clear();
+    await Services.search.setDefault(oldDefaultEngine);
+    await Services.search.removeEngine(engine);
   });
 
   await UrlbarTestUtils.promiseAutocompleteResultPopup({
@@ -22,6 +38,13 @@ add_task(async function() {
     value: "textruns",
   });
   let result = await UrlbarTestUtils.getDetailsOfResultAt(window, 1);
+  Assert.equal(result.searchParams.engine, "Test", "Sanity check engine");
+  Assert.equal(
+    result.displayed.title.length,
+    UrlbarUtils.MAX_TEXT_LENGTH,
+    "Result title should be limited"
+  );
+  result = await UrlbarTestUtils.getDetailsOfResultAt(window, 2);
   Assert.equal(
     result.displayed.title.length,
     UrlbarUtils.MAX_TEXT_LENGTH,
