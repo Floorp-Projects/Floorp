863614cef60170505a19b99aa39ed976c33d5245
Gerald Squelart <gsquelart@mozilla.com>
2020-12-22 10:51:12 +0000

    Bug 1678651 - Increase expected max stack record size to 128KB - r=canaltinova
    Some sites do have stacks that require more than 64KB to store in the profiler buffer.
    Note that this only affects one semi-permanent buffer per process during profiling, and short-lived buffers when capturing stacks in markers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D99981

--- mozglue/baseprofiler/public/ProfileBufferChunkManager.h
+++ mozglue/baseprofiler/public/ProfileBufferChunkManager.h
@@ -39,7 +39,7 @@ class ProfileBufferChunkManager {
   // Most ChunkManager sub-classes will require chunk sizes, this can serve as
   // a minimum recommendation to hold most backtraces.
   constexpr static ProfileBufferChunk::Length scExpectedMaximumStackSize =
-      64 * 1024;
+      128 * 1024;
 
   // Estimated maximum buffer size.
   [[nodiscard]] virtual size_t MaxTotalSize() const = 0;
