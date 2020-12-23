f2a347b0a9fc7b072522e227ba1ec77fca17239b
MickeyMoz <sebastian@mozilla.com>
2020-12-23 12:11:50 +0000

    [components] Update GeckoView (Nightly) to 86.0.20201223092736.

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "86.0.20201222150234"
+    const val nightly_version = "86.0.20201223092736"
 
     /**
      * GeckoView Beta Version.
