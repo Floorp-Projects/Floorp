e6a5e0e16beb8afed4aa2c0bcdefa3dee72af4fc
Jonathan Kew <jkew@mozilla.com>
2020-12-26 17:11:52 +0000

    Bug 1669855 - Add a primary-name-only parameter to FindFamily, to allow it to skip searching for aliases, and use this when initializing Windows font substitutions. r=jwatt
    Differential Revision: https://phabricator.services.mozilla.com/D96394

--- gfx/thebes/SharedFontList.cpp
+++ gfx/thebes/SharedFontList.cpp
@@ -849,7 +849,9 @@ void FontList::SetLocalNames(
     (void)new (&faces[i]) LocalFaceRec();
     const auto& rec = aLocalNameTable.Get(faceArray[i]);
     faces[i].mKey.Assign(faceArray[i], this);
-    const auto* family = FindFamily(rec.mFamilyName);
+    // Local face name records will refer to the canonical family name; we don't
+    // need to search aliases here.
+    const auto* family = FindFamily(rec.mFamilyName, /*aPrimaryNameOnly*/ true);
     if (!family) {
       // Skip this record if the family was excluded by the font whitelist pref.
       continue;
@@ -905,7 +907,7 @@ nsCString FontList::LocalizedFamilyName(const Family* aFamily) {
   return aFamily->DisplayName().AsString(this);
 }
 
-Family* FontList::FindFamily(const nsCString& aName) {
+Family* FontList::FindFamily(const nsCString& aName, bool aPrimaryNameOnly) {
   struct FamilyNameComparator {
     FamilyNameComparator(FontList* aList, const nsCString& aTarget)
         : mList(aList), mTarget(aTarget) {}
@@ -926,6 +928,10 @@ Family* FontList::FindFamily(const nsCString& aName) {
   if (families && BinarySearchIf(families, 0, header.mFamilyCount,
                                  FamilyNameComparator(this, aName), &match)) {
     return &families[match];
+  }
+
+  if (aPrimaryNameOnly) {
+    return nullptr;
   }
 
   if (header.mAliasCount) {
