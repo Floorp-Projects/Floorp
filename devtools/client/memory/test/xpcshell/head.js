dae09af4dbc0b33374dd1021855f62ee9545500b
bfmags <bfmags@gmail.com>
2020-12-28 16:12:23 +0000

    Bug 1681889 - Remove unused `defer` require from devtools/client/memory/test/xpcshell/head.js. r=ntim
    Differential Revision: https://phabricator.services.mozilla.com/D100498

--- devtools/client/memory/test/xpcshell/head.js
+++ devtools/client/memory/test/xpcshell/head.js
@@ -22,7 +22,6 @@ var { OS } = require("resource://gre/modules/osfile.jsm");
 var { FileUtils } = require("resource://gre/modules/FileUtils.jsm");
 var { TargetFactory } = require("devtools/client/framework/target");
 var promise = require("promise");
-var defer = require("devtools/shared/defer");
 var { expectState } = require("devtools/server/actors/common");
 var HeapSnapshotFileUtils = require("devtools/shared/heapsnapshot/HeapSnapshotFileUtils");
 var HeapAnalysesClient = require("devtools/shared/heapsnapshot/HeapAnalysesClient");
