005741590b0325048f977a813a2cbc4c7b835a21
Chris Peterson <cpeterson@mozilla.com>
2020-12-30 16:18:39 +0000

    Bug 1041377 - Part 3: Disable Backspace as keyboard shortcut for back/forward navigation on Windows and macOS. r=masayuki
    This change aligns Firefox on Windows and macOS with the behavior of Firefox on Linux, Chrome, Edge, and Safari. The only mainstream browsers that navigate back on Backspace in 2020 are Firefox on Windows and macOS and IE11 on Windows.
    
    Depends on D100532
    
    Differential Revision: https://phabricator.services.mozilla.com/D100533

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -824,11 +824,7 @@ pref("browser.proton.enabled", false);
 // 0 goes Back/Forward
 // 1 act like PgUp/PgDown
 // 2 and other values, nothing
-#ifdef UNIX_BUT_NOT_MAC
-  pref("browser.backspace_action", 2);
-#else
-  pref("browser.backspace_action", 0);
-#endif
+pref("browser.backspace_action", 2);
 
 pref("intl.regional_prefs.use_os_locales", false);
 
