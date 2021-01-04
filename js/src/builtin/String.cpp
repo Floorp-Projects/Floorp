e3a222e88727c5718d3324aba0be7f3245067133
Jan de Mooij <jdemooij@mozilla.com>
2021-01-04 19:45:54 +0000

    Bug 1682767 part 20 - Remove unused group argument from StringSplitString. r=iain
    Depends on D100677
    
    Differential Revision: https://phabricator.services.mozilla.com/D100678

--- js/src/builtin/String.cpp
+++ js/src/builtin/String.cpp
@@ -3524,10 +3524,8 @@ static ArrayObject* SplitSingleCharHelper(JSContext* cx, HandleLinearString str,
 }
 
 // ES 2016 draft Mar 25, 2016 21.1.3.17 steps 4, 8, 12-18.
-// TODO(no-TI): remove unused group argument.
-ArrayObject* js::StringSplitString(JSContext* cx, HandleObjectGroup group,
-                                   HandleString str, HandleString sep,
-                                   uint32_t limit) {
+ArrayObject* js::StringSplitString(JSContext* cx, HandleString str,
+                                   HandleString sep, uint32_t limit) {
   MOZ_ASSERT(limit > 0, "Only called for strictly positive limit.");
 
   RootedLinearString linearStr(cx, str->ensureLinear(cx));
