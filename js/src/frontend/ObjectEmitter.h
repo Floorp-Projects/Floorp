03bfb7aa86a6852336298ad98c366692769be9eb
Tooru Fujisawa <arai_a@mac.com>
2021-01-05 15:25:26 +0000

    Bug 1684941 - Remove using Parser*ScopeData aliases and use *Scope::ParserData. r=nbp
    Differential Revision: https://phabricator.services.mozilla.com/D100740

--- js/src/frontend/ObjectEmitter.h
+++ js/src/frontend/ObjectEmitter.h
@@ -762,8 +762,8 @@ class MOZ_STACK_CLASS ClassEmitter : public PropertyEmitter {
  public:
   explicit ClassEmitter(BytecodeEmitter* bce);
 
-  bool emitScope(ParserLexicalScopeData* scopeBindings);
-  bool emitBodyScope(ParserLexicalScopeData* scopeBindings);
+  bool emitScope(LexicalScope::ParserData* scopeBindings);
+  bool emitBodyScope(LexicalScope::ParserData* scopeBindings);
 
   // @param name
   //        Name of the class (nullptr if this is anonymous class)
