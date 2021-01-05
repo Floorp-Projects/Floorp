73c91906a89454b34b9d298f04ee7f19151aebc7
Razvan Maries <rmaries@mozilla.com>
2021-01-05 17:59:54 +0200

    Backed out changeset 7d10fe1f2f2e (bug 1684490) for causing bug 1685106. a=backout DONTBUILD

--- docshell/base/BrowsingContext.cpp
+++ docshell/base/BrowsingContext.cpp
@@ -237,7 +237,7 @@ bool BrowsingContext::IsOwnedByProcess() const {
 }
 
 bool BrowsingContext::SameOriginWithTop() {
-  MOZ_DIAGNOSTIC_ASSERT(IsInProcess());
+  MOZ_ASSERT(IsInProcess());
   // If the top BrowsingContext is not same-process to us, it is cross-origin
   if (!Top()->IsInProcess()) {
     return false;
