35a378146f8f109591dcd51d7b6fb20f1ed25a82
Florin Strugariu <fstrugariu@mozilla.com>
2021-01-05 09:21:15 +0000

    Bug 1679453 - Update mitmproxy recording scripts r=AlexandruIonescu
    Differential Revision: https://phabricator.services.mozilla.com/D100154

--- testing/mozbase/mozproxy/mozproxy/backends/mitm/scripts/inject-deterministic.py
+++ testing/mozbase/mozproxy/mozproxy/backends/mitm/scripts/inject-deterministic.py
@@ -1,30 +1,36 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 from __future__ import absolute_import, print_function
-
 import base64
 import hashlib
 import re
 import time
+from os import path
 
 from mitmproxy import ctx
 
 
 class AddDeterministic:
-    def get_csp_directives(self, headers):
-        csp = headers.get("Content-Security-Policy", "")
+    def __init__(self):
+        self.millis = int(round(time.time() * 1000))
+        ctx.log.info("Init Deterministic JS")
+
+    def load(self, loader):
+        ctx.log.info("Load Deterministic JS")
+
+    def get_csp_directives(self, test_header, headers):
+        csp = headers.get(test_header, "")
         return [d.strip() for d in csp.split(";")]
 
-    def get_csp_script_sources(self, headers):
+    def get_csp_script_sources(self, test_header, headers):
         sources = []
-        for directive in self.get_csp_directives(headers):
+        for directive in self.get_csp_directives(test_header, headers):
             if directive.startswith("script-src "):
                 sources = directive.split()[1:]
         return sources
 
-    def get_nonce_from_headers(self, headers):
+    def get_nonce_from_headers(self, test_header, headers):
         """
         get_nonce_from_headers returns the nonce token from a
         Content-Security-Policy (CSP) header's script source directive.
@@ -36,7 +42,7 @@ class AddDeterministic:
         https://developers.google.com/web/fundamentals/security/csp/
         """
 
-        for source in self.get_csp_script_sources(headers) or []:
+        for source in self.get_csp_script_sources(test_header, headers) or []:
             if source.startswith("'nonce-"):
                 return source.partition("'nonce-")[-1][:-1]
 
@@ -58,7 +64,7 @@ class AddDeterministic:
             return '<script nonce="{}">{}</script>'.format(nonce, script)
         return "<script>{}</script>".format(script)
 
-    def update_csp_script_src(self, headers, sha256):
+    def update_csp_script_src(self, test_header, headers, sha256):
         """
         Update the CSP script directives with appropriate information
 
@@ -72,7 +78,7 @@ class AddDeterministic:
         https://developers.google.com/web/fundamentals/security/csp/
         """
 
-        sources = self.get_csp_script_sources(headers)
+        sources = self.get_csp_script_sources(test_header, headers)
         add_unsafe = True
 
         for token in sources:
@@ -91,14 +97,14 @@ class AddDeterministic:
 
         return "script-src {}".format(" ".join(sources))
 
-    def get_new_csp_header(self, headers, updated_csp_script):
+    def get_new_csp_header(self, test_header, headers, updated_csp_script):
         """
         get_new_csp_header generates a new header object containing
         the updated elements from new_csp_script_directives
         """
 
         if updated_csp_script:
-            directives = self.get_csp_directives(headers)
+            directives = self.get_csp_directives(test_header, headers)
             for index, directive in enumerate(directives):
                 if directive.startswith("script-src "):
                     directives[index] = updated_csp_script
@@ -110,10 +116,7 @@ class AddDeterministic:
         return headers
 
     def response(self, flow):
-
         # pylint: disable=W1633
-        millis = int(round(time.time() * 1000))
-
         if "content-type" in flow.response.headers:
             if "text/html" in flow.response.headers["content-type"]:
                 ctx.log.info(
@@ -123,8 +126,13 @@ class AddDeterministic:
                 flow.response.decode()
                 html = flow.response.text
 
-                with open("scripts/catapult/deterministic.js", "r") as jsfile:
-                    js = jsfile.read().replace("REPLACE_LOAD_TIMESTAMP", str(millis))
+                with open(
+                    path.join(path.dirname(__file__), "catapult/deterministic.js"), "r"
+                ) as jsfile:
+
+                    js = jsfile.read().replace(
+                        "REPLACE_LOAD_TIMESTAMP", str(self.millis)
+                    )
 
                     if js not in html:
                         script_index = re.search("(?i).*?<head.*?>", html)
@@ -142,28 +150,39 @@ class AddDeterministic:
                         script_index = script_index.end()
 
                         nonce = None
-
-                        if flow.response.headers.get("Content-Security-Policy", False):
-                            nonce = self.get_nonce_from_headers(flow.response.headers)
-                            ctx.log.info("nonce : %s" % nonce)
-
-                            if (
-                                self.get_csp_script_sources(flow.response.headers)
-                                and not nonce
-                            ):
-                                # generate sha256 for the script
-                                hash_object = hashlib.sha256(js.encode("utf-8"))
-                                script_sha256 = base64.b64encode(
-                                    hash_object.digest()
-                                ).decode("utf-8")
-
-                                # generate the new response headers
-                                updated_script_sources = self.update_csp_script_src(
-                                    flow.response.headers, script_sha256
+                        for test_header in [
+                            "Content-Security-Policy",
+                            "Content-Security-Policy-Report-Only",
+                        ]:
+                            if flow.response.headers.get(test_header, False):
+                                nonce = self.get_nonce_from_headers(
+                                    test_header, flow.response.headers
                                 )
-                                flow.response.headers = self.get_new_csp_header(
-                                    flow.response.headers, updated_script_sources
-                                )
+                                ctx.log.info("nonce : %s" % nonce)
+
+                                if (
+                                    self.get_csp_script_sources(
+                                        test_header, flow.response.headers
+                                    )
+                                    and not nonce
+                                ):
+                                    # generate sha256 for the script
+                                    hash_object = hashlib.sha256(js.encode("utf-8"))
+                                    script_sha256 = base64.b64encode(
+                                        hash_object.digest()
+                                    ).decode("utf-8")
+
+                                    # generate the new response headers
+                                    updated_script_sources = self.update_csp_script_src(
+                                        test_header,
+                                        flow.response.headers,
+                                        script_sha256,
+                                    )
+                                    flow.response.headers = self.get_new_csp_header(
+                                        test_header,
+                                        flow.response.headers,
+                                        updated_script_sources,
+                                    )
 
                         # generate new html file
                         new_html = (
@@ -186,6 +205,4 @@ class AddDeterministic:
                         )
 
 
-def start():
-    ctx.log.info("Load Deterministic JS")
-    return AddDeterministic()
+addons = [AddDeterministic()]
