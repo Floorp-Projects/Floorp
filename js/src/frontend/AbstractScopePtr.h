8b21e2fb3ab4e5d99b0af695b275771bf48f2a01
Ted Campbell <tcampbell@mozilla.com>
2021-01-07 02:34:18 +0000

    Bug 1655052 - Remove AbstractScopePtr::nextFrameSlot. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D100817

--- js/src/frontend/AbstractScopePtr.h
+++ js/src/frontend/AbstractScopePtr.h
@@ -103,7 +103,6 @@ class AbstractScopePtr {
   ScopeKind kind() const;
   AbstractScopePtr enclosing() const;
   bool hasEnvironment() const;
-  uint32_t nextFrameSlot() const;
   // Valid iff is<FunctionScope>
   bool isArrow() const;
 
