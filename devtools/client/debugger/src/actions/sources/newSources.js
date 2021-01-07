d8a1890e9188f04cea4ccc186d0ce88e4213d646
Hubert Boma Manilla <hmanilla@mozilla.com>
2021-01-07 01:59:40 +0000

    Bug 1602440 - [devtools] Use actor instead of url for worker targets r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D98074

--- devtools/client/debugger/src/actions/sources/newSources.js
+++ devtools/client/debugger/src/actions/sources/newSources.js
@@ -306,8 +306,8 @@ export function newGeneratedSources(sourceInfo: Array<GeneratedSourceData>) {
     const newSourcesObj = {};
     const newSourceActors: Array<SourceActor> = [];
 
-    for (const { thread, isServiceWorker, source, id } of sourceInfo) {
-      const newId = id || makeSourceId(source, isServiceWorker);
+    for (const { thread, isWorkerTarget, source, id } of sourceInfo) {
+      const newId = id || makeSourceId(source, isWorkerTarget);
 
       if (!getSource(getState(), newId) && !newSourcesObj[newId]) {
         newSourcesObj[newId] = {
