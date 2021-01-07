b21657edc754e68f45825c9448b663551f168469
Jamie Nicol <jnicol@mozilla.com>
2021-01-07 14:47:34 +0000

    Bug 1685276 - Fix and re-enable partial present on Mali-Gxx devices. r=kvark
    In bug 1676474 an issue was reported regarding partial present on
    Mali-G77 devices. This was introduced in bug 1675159, which refactored
    some partial present logic and shifted the order of some OpenGL calls
    around. As a precaution, we disabled the feature on all Mali-Gxx
    devices.
    
    The bug seems to occur when eglSetDamageRegion is called after
    rendering to an offscreen render target (in this case due to texture
    cache or GPU cache updates), but without the driver being flushed in
    some way. This appears to be a bug in the Mali driver.
    
    This patch moves the eglSetDamageRegion call back to its original
    location -- after all offscreen render targets have been rendered,
    immediately before rendering to the main framebuffer -- which fixes
    the issue. It also re-enables the feature on all Mali-Gxx devices.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101018

--- gfx/tests/gtest/TestConfigManager.cpp
+++ gfx/tests/gtest/TestConfigManager.cpp
@@ -26,7 +26,6 @@ class MockGfxInfo final : public nsIGfxInfo {
   bool mHasMixedRefreshRate;
   Maybe<bool> mHasBattery;
   const char* mVendorId;
-  const char* mDeviceId;
 
   // Default allows WebRender + compositor, and is desktop NVIDIA.
   MockGfxInfo()
@@ -36,8 +35,7 @@ class MockGfxInfo final : public nsIGfxInfo {
         mMaxRefreshRate(-1),
         mHasMixedRefreshRate(false),
         mHasBattery(Some(false)),
-        mVendorId("0x10de"),
-        mDeviceId("") {}
+        mVendorId("0x10de") {}
 
   NS_IMETHOD GetFeatureStatus(int32_t aFeature, nsACString& aFailureId,
                               int32_t* _retval) override {
@@ -70,14 +68,6 @@ class MockGfxInfo final : public nsIGfxInfo {
       return NS_ERROR_NOT_IMPLEMENTED;
     }
     aAdapterVendorID.AssignASCII(mVendorId);
-    return NS_OK;
-  }
-
-  NS_IMETHOD GetAdapterDeviceID(nsAString& aAdapterDeviceID) override {
-    if (!mDeviceId) {
-      return NS_ERROR_NOT_IMPLEMENTED;
-    }
-    aAdapterDeviceID.AssignASCII(mDeviceId);
     return NS_OK;
   }
 
@@ -172,6 +162,9 @@ class MockGfxInfo final : public nsIGfxInfo {
     return NS_ERROR_NOT_IMPLEMENTED;
   }
   NS_IMETHOD GetAdapterDriver(nsAString& aAdapterDriver) override {
+    return NS_ERROR_NOT_IMPLEMENTED;
+  }
+  NS_IMETHOD GetAdapterDeviceID(nsAString& aAdapterDeviceID) override {
     return NS_ERROR_NOT_IMPLEMENTED;
   }
   NS_IMETHOD GetAdapterSubsysID(nsAString& aAdapterSubsysID) override {
@@ -338,14 +331,6 @@ TEST_F(GfxConfigManager, WebRenderNoPartialPresent) {
   EXPECT_TRUE(mFeatures.mGPUProcess.IsEnabled());
   EXPECT_TRUE(mFeatures.mD3D11HwAngle.IsEnabled());
   EXPECT_FALSE(mFeatures.mWrSoftware.IsEnabled());
-}
-
-TEST_F(GfxConfigManager, WebRenderPartialPresentMali) {
-  mWrPartialPresent = true;
-  mMockGfxInfo->mDeviceId = "Mali-G77";
-  ConfigureWebRender();
-
-  EXPECT_FALSE(mFeatures.mWrPartial.IsEnabled());
 }
 
 TEST_F(GfxConfigManager, WebRenderScaledResolutionWithHwStretching) {
