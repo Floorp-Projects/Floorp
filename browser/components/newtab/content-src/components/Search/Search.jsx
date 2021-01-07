3dc3bb93fdc8c44d36e94428c8e69ffd938306c9
prathiksha <prathikshaprasadsuman@gmail.com>
2021-01-07 14:38:48 +0000

    Bug 1681784 - Update search bar styling on newtab page if newNewtabExperience pref is set to true. r=ewright,fluent-reviewers,flod
    Differential Revision: https://phabricator.services.mozilla.com/D100706

--- browser/components/newtab/content-src/components/Search/Search.jsx
+++ browser/components/newtab/content-src/components/Search/Search.jsx
@@ -131,6 +131,10 @@ export class _Search extends React.PureComponent {
       .filter(v => v)
       .join(" ");
 
+    const isNewNewtabExperienceEnabled = this.props.Prefs.values[
+      "newNewtabExperience.enabled"
+    ];
+
     return (
       <div className={wrapperClassName}>
         {this.props.showLogo && (
@@ -143,10 +147,17 @@ export class _Search extends React.PureComponent {
           <div className="search-inner-wrapper">
             <input
               id="newtab-search-text"
-              data-l10n-id="newtab-search-box-search-the-web-input"
+              data-l10n-id={
+                isNewNewtabExperienceEnabled
+                  ? "newtab-search-box-input"
+                  : "newtab-search-box-search-the-web-input"
+              }
               maxLength="256"
               ref={this.onInputMount}
               type="search"
+              className={`${
+                isNewNewtabExperienceEnabled ? " search-bar-new" : ""
+              }`}
             />
             <button
               id="searchSubmit"
@@ -159,15 +170,25 @@ export class _Search extends React.PureComponent {
         {this.props.handoffEnabled && (
           <div className="search-inner-wrapper">
             <button
-              className="search-handoff-button"
-              data-l10n-id="newtab-search-box-search-the-web-input"
+              className={`search-handoff-button ${
+                isNewNewtabExperienceEnabled ? " search-handoff-button-new" : ""
+              }`}
+              data-l10n-id={
+                isNewNewtabExperienceEnabled
+                  ? "newtab-search-box-input"
+                  : "newtab-search-box-search-the-web-input"
+              }
               ref={this.onSearchHandoffButtonMount}
               onClick={this.onSearchHandoffClick}
               tabIndex="-1"
             >
               <div
                 className="fake-textbox"
-                data-l10n-id="newtab-search-box-search-the-web-text"
+                data-l10n-id={
+                  isNewNewtabExperienceEnabled
+                    ? "newtab-search-box-text"
+                    : "newtab-search-box-search-the-web-text"
+                }
               />
               <input
                 type="search"
@@ -187,4 +208,6 @@ export class _Search extends React.PureComponent {
   }
 }
 
-export const Search = connect()(_Search);
+export const Search = connect(state => ({
+  Prefs: state.Prefs,
+}))(_Search);
