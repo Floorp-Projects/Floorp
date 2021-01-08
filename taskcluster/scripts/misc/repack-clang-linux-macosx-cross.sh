4f699ab1d081dcf84d953b01f49e41bedbfcde80
Mike Hommey <mh+mozilla@glandium.org>
2021-01-08 18:51:53 +0000

    Bug 1685599 - Build a single macosx-cross toolchain. r=firefox-build-system-reviewers,mhentges
    The new toolchain contains both aarch64 and x86_64 compiler-rts. We
    could have a separate for each, but compiler-rt is small enough that
    entirely separate toolchains is not warranted.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101079

--- taskcluster/scripts/misc/repack-clang-linux-macosx-cross.sh
+++ taskcluster/scripts/misc/repack-clang-linux-macosx-cross.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 set -x -e -v
 
-# This script is for building clang for Mac OS X targets on a Linux host,
-# including native Mac OS X Compiler-RT libraries and llvm-symbolizer.
+# This script is for building clang for macOS targets on a Linux host,
+# including native macOS Compiler-RT libraries.
 
-mkdir -p $UPLOAD_DIR
 cd $MOZ_FETCHES_DIR
 
 # We have a native linux64 toolchain in $MOZ_FETCHES_DIR/clang
-# We have a native macosx64 toolchain in $MOZ_FETCHES_DIR/clang-mac/clang
-# What we want is a mostly-linux64 toolchain but with a macosx64 llvm-symbolizer and runtime libraries
+# We have a native aarch64 macos compiler-rt in $MOZ_FETCHES_DIR/aarch64/compiler-rt
+# We have a native x86_64 macos compiler-rt in $MOZ_FETCHES_DIR/x86_64/compiler-rt
+clang_lib=$(echo clang/lib/clang/*/lib)
+mkdir -p $clang_lib/darwin
+find {aarch64,x86_64}/compiler-rt/lib/darwin -type f -printf '%f\n' | sort -u | while read f; do
+  f=compiler-rt/lib/darwin/$f
+  if [ -f aarch64/$f -a -f x86_64/$f ]; then
+    # For compiler-rt files that exist on both ends, merge them
+    $MOZ_FETCHES_DIR/cctools/bin/lipo -create {aarch64,x86_64}/$f -output $clang_lib/darwin/${f##*/}
+  elif [ -f aarch64/$f ]; then
+    # For compiler-rt files that exist on either end, copy the existing one
+    cp aarch64/$f $clang_lib/darwin
+  else
+    cp x86_64/$f $clang_lib/darwin
+  fi
+done
 
-mv clang-mac/clang/bin/llvm-symbolizer clang/bin/
-cp --remove-destination -lr clang/* clang-mac/clang/
+tar -cf - clang | python3 $GECKO_PATH/taskcluster/scripts/misc/zstdpy > clang.tar.zst
 
-cd clang-mac
-tar -c clang | $GECKO_PATH/taskcluster/scripts/misc/zstdpy > $UPLOAD_DIR/clang.tar.zst
+mkdir -p $UPLOAD_DIR
+mv clang.tar.zst $UPLOAD_DIR
