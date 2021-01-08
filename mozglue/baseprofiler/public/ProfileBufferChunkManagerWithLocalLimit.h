4ad68eb4c1da6c7eee930679cff77379a23f3069
Gerald Squelart <gsquelart@mozilla.com>
2021-01-08 12:11:18 +0000

    Bug 1651863 - ProfileBufferChunkManager::ReleaseChunk (no 's' anymore) only accepts zero or one chunk - r=canaltinova
    Differential Revision: https://phabricator.services.mozilla.com/D100859

--- mozglue/baseprofiler/public/ProfileBufferChunkManagerWithLocalLimit.h
+++ mozglue/baseprofiler/public/ProfileBufferChunkManagerWithLocalLimit.h
@@ -121,45 +121,39 @@ class ProfileBufferChunkManagerWithLocalLimit final
     }
   }
 
-  void ReleaseChunks(UniquePtr<ProfileBufferChunk> aChunks) final {
+  void ReleaseChunk(UniquePtr<ProfileBufferChunk> aChunk) final {
+    if (!aChunk) {
+      return;
+    }
+
+    MOZ_RELEASE_ASSERT(!aChunk->GetNext(), "ReleaseChunk only accepts 1 chunk");
+    MOZ_RELEASE_ASSERT(!aChunk->ChunkHeader().mDoneTimeStamp.IsNull(),
+                       "Released chunk should have a 'Done' timestamp");
+
     Update update = [&]() {
       baseprofiler::detail::BaseProfilerAutoLock lock(mMutex);
       MOZ_ASSERT(mUser, "Not registered yet");
       // Keep a pointer to the first newly-released chunk, so we can use it to
-      // prepare an update (after `aChunks` is moved-from).
-      const ProfileBufferChunk* const newlyReleasedChunks = aChunks.get();
-      // Compute the size of all provided chunks.
-      size_t bytes = 0;
-      for (const ProfileBufferChunk* chunk = newlyReleasedChunks; chunk;
-           chunk = chunk->GetNext()) {
-        bytes += chunk->BufferBytes();
-        MOZ_ASSERT(!chunk->ChunkHeader().mDoneTimeStamp.IsNull(),
-                   "All released chunks should have a 'Done' timestamp");
-        MOZ_ASSERT(!chunk->GetNext() ||
-                       (chunk->ChunkHeader().mDoneTimeStamp <
-                        chunk->GetNext()->ChunkHeader().mDoneTimeStamp),
-                   "Released chunk groups must have increasing timestamps");
-      }
-      // Transfer the chunks size from the unreleased bucket to the released
-      // one.
-      mUnreleasedBufferBytes -= bytes;
+      // prepare an update (after `aChunk` is moved-from).
+      const ProfileBufferChunk* const newlyReleasedChunk = aChunk.get();
+      // Transfer the chunk size from the unreleased bucket to the released one.
+      mUnreleasedBufferBytes -= aChunk->BufferBytes();
+      mReleasedBufferBytes += aChunk->BufferBytes();
       if (!mReleasedChunks) {
         // No other released chunks at the moment, we're starting the list.
-        MOZ_ASSERT(mReleasedBufferBytes == 0);
-        mReleasedBufferBytes = bytes;
-        mReleasedChunks = std::move(aChunks);
+        MOZ_ASSERT(mReleasedBufferBytes == aChunk->BufferBytes());
+        mReleasedChunks = std::move(aChunk);
       } else {
         // Add to the end of the released chunks list (oldest first, most recent
         // last.)
         MOZ_ASSERT(mReleasedChunks->Last()->ChunkHeader().mDoneTimeStamp <
-                       aChunks->ChunkHeader().mDoneTimeStamp,
+                       aChunk->ChunkHeader().mDoneTimeStamp,
                    "Chunks must be released in increasing timestamps");
-        mReleasedBufferBytes += bytes;
-        mReleasedChunks->SetLast(std::move(aChunks));
+        mReleasedChunks->SetLast(std::move(aChunk));
       }
 
       return Update(mUnreleasedBufferBytes, mReleasedBufferBytes,
-                    mReleasedChunks.get(), newlyReleasedChunks);
+                    mReleasedChunks.get(), newlyReleasedChunk);
     }();
 
     baseprofiler::detail::BaseProfilerAutoLock lock(mUpdateCallbackMutex);
