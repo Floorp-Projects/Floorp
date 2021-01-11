f1328b0a2b7a90cf91ac31737ac65b3390917ac4
Jan-Erik Rediger <jrediger@mozilla.com>
2021-01-11 08:33:50 +0000

    Bug 1682165 - Don't trigger a panic in test code. r=chutten
    `test_get_value` in those "child" tests triggers a panic, which of
    course still unwinds and therefore poisons the Glean mutex.
    `catch_unwind` only catches the panic after that.
    
    Now why this would only result intermittenly in test failures?
    I don't really know.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101164

--- toolkit/components/glean/api/src/private/timespan.rs
+++ toolkit/components/glean/api/src/private/timespan.rs
@@ -130,11 +130,5 @@ mod test {
 
         // (They also shouldn't do anything,
         // but that's not something we can inspect in this test)
-
-        // Need to catch the panic so that our RAIIs drop nicely.
-        let result = std::panic::catch_unwind(move || {
-            child_metric.test_get_value("store1");
-        });
-        assert!(result.is_err());
     }
 }
