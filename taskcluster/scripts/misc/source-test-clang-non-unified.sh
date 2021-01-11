0bc939c44d792abba32420d52c068bdbd0048348
Emilio Cobos Álvarez <emilio@crisal.io>
2021-01-11 04:25:29 +0000

    Bug 1685957 - Use xargs to test non-unified builds. r=andi
    Passing all the directories to mach allows to take more advantage of
    parallelism (plus not having to start up mach and the whole export once
    for every directory, which itself takes a bit of time)
    
    This takes:
    
        GECKO_PATH=. taskcluster/scripts/misc/source-test-clang-non-unified.sh
    
    from about ten mins to under two minutes on my machine.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101320

--- taskcluster/scripts/misc/source-test-clang-non-unified.sh
+++ taskcluster/scripts/misc/source-test-clang-non-unified.sh
@@ -2,12 +2,4 @@
 
 NON_UNIFIED_CFG=$GECKO_PATH/build/non-unified-compat
 
-while IFS= read -r directory
-do
-  echo "Running non-unified compile test for: $directory"
-  ./mach static-analysis check-syntax $directory
-  if [ $? -ne 0 ]; then
-    # If failure do not continue with the rest of the directories
-    exit 1
-  fi
-done < $NON_UNIFIED_CFG
+cat "$NON_UNIFIED_CFG" | xargs ./mach static-analysis check-syntax
