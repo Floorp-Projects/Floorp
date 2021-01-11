2747a25af689c30651bd94bc2b44aba3b8ee45a9
Mirko Brodesser <mbrodesser@mozilla.com>
2021-01-11 15:03:05 +0000

    Bug 1685303: part 4) Add logging to `Selection::AddRangesForSelectableNodes`. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D101354

--- dom/base/Selection.cpp
+++ dom/base/Selection.cpp
@@ -932,6 +932,12 @@ nsresult Selection::AddRangesForSelectableNodes(
 
   NS_ASSERTION(aOutIndex, "aOutIndex can't be null");
 
+  MOZ_LOG(
+      sSelectionLog, LogLevel::Debug,
+      ("%s: selection=%p, type=%i, range=(%p, StartOffset=%u, EndOffset=%u)",
+       __FUNCTION__, this, static_cast<int>(GetType()), aRange,
+       aRange->StartOffset(), aRange->EndOffset()));
+
   if (mUserInitiated) {
     return AddRangesForUserSelectableNodes(aRange, aOutIndex,
                                            aDispatchSelectstartEvent);
