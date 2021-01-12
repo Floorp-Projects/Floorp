6e22d60a547350425d221fbc9cffc422fe186394
Jon Coppeard <jcoppeard@mozilla.com>
2021-01-12 10:40:39 +0000

    Bug 1686020 - Don't disable the nursery before creating the self hosting zone r=sfink
    This is unnecessary now we do a full collection after creating the self hosting zone.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101357

--- js/src/jsapi-tests/testGCOutOfMemory.cpp
+++ js/src/jsapi-tests/testGCOutOfMemory.cpp
@@ -8,6 +8,7 @@
 
 #include "mozilla/Utf8.h"  // mozilla::Utf8Unit
 
+#include "gc/GCEnum.h"  // js::gc::ZealMode
 #include "js/CompilationAndEvaluation.h"  // JS::Evaluate
 #include "js/SourceText.h"                // JS::Source{Ownership,Text}
 #include "jsapi-tests/tests.h"
@@ -69,6 +70,9 @@ virtual JSContext* createContext() override {
     return nullptr;
   }
   JS_SetGCParameter(cx, JSGC_MAX_NURSERY_BYTES, js::gc::ChunkSize);
+#ifdef JS_GC_ZEAL
+  JS_UnsetGCZeal(cx, uint8_t(js::gc::ZealMode::GenerationalGC));
+#endif
   setNativeStackQuota(cx);
   return cx;
 }
