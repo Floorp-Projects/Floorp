a0668f96665392a23976b41be394f051b8de3642
Narcis Beleuzu <nbeleuzu@mozilla.com>
2021-01-14 21:10:40 +0200

    Backed out changeset 10cb82a1a314 (bug 1643736) for geckoview failures on testBrowserTabsCreate

--- mobile/android/components/extensions/ext-tabs.js
+++ mobile/android/components/extensions/ext-tabs.js
@@ -331,13 +331,6 @@ this.tabs = class extends ExtensionAPI {
             }
           }
 
-          cookieStoreId = getUserContextIdForCookieStoreId(
-            extension,
-            cookieStoreId,
-            false // TODO bug 1372178: support creation of private browsing tabs
-          );
-          cookieStoreId = cookieStoreId ? cookieStoreId.toString() : undefined;
-
           const nativeTab = await GeckoViewTabBridge.createNewTab({
             extensionId: context.extension.id,
             createProperties: {
