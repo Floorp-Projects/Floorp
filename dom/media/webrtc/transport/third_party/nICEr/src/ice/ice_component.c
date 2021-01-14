e8399982e4d83c308ae3c70ce94bba2ee4427bac
Byron Campen [:bwc] <docfaraday@gmail.com>
2021-01-14 19:00:25 +0000

    Bug 1416220: Modify mtransport to delegate DNS lookups for TCP sockets to the socket class, instead of using nr_resolver. r=mjf
    * Modified nr_transport_addr to allow it to represent an
      fqdn/ip-version/protocol tuple, and taught nICEr to handle the
      fqdn case appropriately.
    * Since nr_transport_addr can represent an fqdn, nr_ice_stun_server
      did not need this ability anymore, and was significantly simplified.
    * Taught NrIceCtx to handle creation of a V4/V6 pair of nr_ice_stun_server
      when an fqdn is used, instead of having nICEr create pairs later.
    
    Depends on D101657
    
    Differential Revision: https://phabricator.services.mozilla.com/D101658

--- dom/media/webrtc/transport/third_party/nICEr/src/ice/ice_component.c
+++ dom/media/webrtc/transport/third_party/nICEr/src/ice/ice_component.c
@@ -253,16 +253,12 @@ static int nr_ice_component_initialize_udp(struct nr_ice_ctx_ *ctx,nr_ice_compon
         /* And a srvrflx candidate for each STUN server */
         for(j=0;j<ctx->stun_server_ct;j++){
           /* Skip non-UDP */
-          if(ctx->stun_servers[j].transport!=IPPROTO_UDP)
-            continue;
+          if (ctx->stun_servers[j].addr.protocol != IPPROTO_UDP) continue;
 
-          if (ctx->stun_servers[j].type == NR_ICE_STUN_SERVER_TYPE_ADDR) {
-            if (nr_transport_addr_check_compatibility(
-                  &addrs[i].addr,
-                  &ctx->stun_servers[j].u.addr)) {
-              r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping STUN server because of link local mis-match",ctx->label);
-              continue;
-            }
+          if (nr_transport_addr_check_compatibility(
+                  &addrs[i].addr, &ctx->stun_servers[j].addr)) {
+            r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping STUN server because of address type mis-match",ctx->label);
+            continue;
           }
 
           /* Ensure id is set (nr_ice_ctx_set_stun_servers does not) */
@@ -292,16 +288,13 @@ static int nr_ice_component_initialize_udp(struct nr_ice_ctx_ *ctx,nr_ice_compon
         nr_ice_candidate *srvflx_cand=0;
 
         /* Skip non-UDP */
-        if (ctx->turn_servers[j].turn_server.transport != IPPROTO_UDP)
+        if (ctx->turn_servers[j].turn_server.addr.protocol != IPPROTO_UDP)
           continue;
 
-        if (ctx->turn_servers[j].turn_server.type == NR_ICE_STUN_SERVER_TYPE_ADDR) {
-          if (nr_transport_addr_check_compatibility(
-                &addrs[i].addr,
-                &ctx->turn_servers[j].turn_server.u.addr)) {
-            r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping TURN server because of link local mis-match",ctx->label);
-            continue;
-          }
+        if (nr_transport_addr_check_compatibility(
+                &addrs[i].addr, &ctx->turn_servers[j].turn_server.addr)) {
+          r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping TURN server because of address type mis-match",ctx->label);
+          continue;
         }
 
         if (!(ctx->flags & NR_ICE_CTX_FLAGS_RELAY_ONLY)) {
@@ -499,8 +492,7 @@ static int nr_ice_component_initialize_tcp(struct nr_ice_ctx_ *ctx,nr_ice_compon
 
         /* And srvrflx candidates for each STUN server */
         for(j=0;j<ctx->stun_server_ct;j++){
-          if (ctx->stun_servers[j].transport!=IPPROTO_TCP)
-            continue;
+          if (ctx->stun_servers[j].addr.protocol != IPPROTO_TCP) continue;
 
           if (isock_psv) {
             if(r=nr_ice_candidate_create(ctx,component,
@@ -534,7 +526,7 @@ static int nr_ice_component_initialize_tcp(struct nr_ice_ctx_ *ctx,nr_ice_compon
         nr_ice_socket *turn_isock;
 
         /* Skip non-TCP */
-        if (ctx->turn_servers[j].turn_server.transport != IPPROTO_TCP)
+        if (ctx->turn_servers[j].turn_server.addr.protocol != IPPROTO_TCP)
           continue;
 
         /* Create relay candidate */
@@ -542,13 +534,10 @@ static int nr_ice_component_initialize_tcp(struct nr_ice_ctx_ *ctx,nr_ice_compon
           ABORT(r);
         addr.protocol = IPPROTO_TCP;
 
-        if (ctx->turn_servers[j].turn_server.type == NR_ICE_STUN_SERVER_TYPE_ADDR) {
-          if (nr_transport_addr_check_compatibility(
-                &addr,
-                &ctx->turn_servers[j].turn_server.u.addr)) {
-            r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping TURN server because of link local mis-match",ctx->label);
-            continue;
-          }
+        if (nr_transport_addr_check_compatibility(
+                &addr, &ctx->turn_servers[j].turn_server.addr)) {
+          r_log(LOG_ICE,LOG_INFO,"ICE(%s): Skipping TURN server because of address type mis-match",ctx->label);
+          continue;
         }
 
         if (!ice_tcp_disabled) {
@@ -574,11 +563,9 @@ static int nr_ice_component_initialize_tcp(struct nr_ice_ctx_ *ctx,nr_ice_compon
           }
         }
 
-        /* If we're going to use TLS, make sure that's recorded */
-        if (ctx->turn_servers[j].turn_server.tls) {
-          strncpy(addr.tls_host,
-                  ctx->turn_servers[j].turn_server.u.dnsname.host,
-                  sizeof(addr.tls_host) - 1);
+        if (ctx->turn_servers[j].turn_server.addr.fqdn[0] != 0) {
+          /* If we're going to use TLS, make sure that's recorded */
+          addr.tls = ctx->turn_servers[j].turn_server.addr.tls;
         }
 
         if ((r=nr_transport_addr_fmt_addr_string(&addr)))
