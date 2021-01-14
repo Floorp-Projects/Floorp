440c982118ce25ab11cbac2ffcf8270f51d6f6eb
Oriol Brufau <oriol-bugzilla@hotmail.com>
2021-01-14 15:32:24 +0000

    Bug 1580003 - Remove isLastMultiSelectChange parameter. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D45245

--- browser/components/sessionstore/test/browser_movePendingTabToNewWindow.js
+++ browser/components/sessionstore/test/browser_movePendingTabToNewWindow.js
@@ -92,9 +92,7 @@ add_task(async function test_movePendingTabToNewWindow() {
     newWindowTabs[1].hasAttribute("pending"),
     "Second tab in new window should still be pending"
   );
-  newWindow.gBrowser.clearMultiSelectedTabs({
-    isLastMultiSelectChange: true,
-  });
+  newWindow.gBrowser.clearMultiSelectedTabs();
   ok(
     newWindowTabs.every(t => !t.multiselected),
     "No multiselection should be present"
