a4c4772b35e6cba616f3db2117b04a9a4f611a5b
Iain Ireland <iireland@mozilla.com>
2021-01-15 17:59:55 +0000

    Bug 1686702: Add jit option to disable bailout loop check r=jandem
    To be used in case of fuzzblockers.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101825

--- js/src/jit/JitOptions.h
+++ js/src/jit/JitOptions.h
@@ -52,6 +52,7 @@ struct DefaultJitOptions {
   bool disableScalarReplacement;
   bool disableCacheIR;
   bool disableSink;
+  bool disableBailoutLoopCheck;
   bool baselineInterpreter;
   bool baselineJit;
   bool ion;
