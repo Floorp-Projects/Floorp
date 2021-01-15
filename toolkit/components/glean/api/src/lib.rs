e06fbc368949d4449156922d7413f1867e6cacc8
Chris H-C <chutten@mozilla.com>
2021-01-15 20:01:05 +0000

    Bug 1673663 - Move FOG Timing Distribution to RLB impl r=janerik
    Differential Revision: https://phabricator.services.mozilla.com/D101638

--- toolkit/components/glean/api/src/lib.rs
+++ toolkit/components/glean/api/src/lib.rs
@@ -22,15 +22,3 @@ pub mod ipc;
 #[cfg(test)]
 mod common_test;
 mod ffi;
-
-/// Run a closure with a mutable reference to the locked global Glean object.
-fn with_glean<F, R>(f: F) -> R
-where
-    F: FnOnce(&mut glean_core::Glean) -> R,
-{
-    let mut lock = glean_core::global_glean()
-        .expect("Global Glean object not initialized")
-        .lock()
-        .unwrap();
-    f(&mut lock)
-}
