26c51d2bdd37f2d0cf7d892f79f3bdb4c409ef51
Emilio Cobos Álvarez <emilio@crisal.io>
2021-01-15 21:26:57 +0000

    Bug 1551040 - Allow resource:// stylesheets to use chrome-only rules. r=boris
    We'll use it to use @-moz-document from plaintext.css.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101516

--- servo/components/style/stylesheets/mod.rs
+++ servo/components/style/stylesheets/mod.rs
@@ -156,8 +156,8 @@ impl UrlExtraData {
 
     /// True if this URL scheme is chrome.
     #[inline]
-    pub fn is_chrome(&self) -> bool {
-        self.as_ref().mIsChrome
+    pub fn chrome_rules_enabled(&self) -> bool {
+        self.as_ref().mChromeRulesEnabled
     }
 
     /// Create a reference to this `UrlExtraData` from a reference to pointer.
@@ -215,7 +215,7 @@ impl fmt::Debug for UrlExtraData {
 
         formatter
             .debug_struct("URLExtraData")
-            .field("is_chrome", &self.is_chrome())
+            .field("chrome_rules_enabled", &self.chrome_rules_enabled())
             .field(
                 "base",
                 &DebugURI(self.as_ref().mBaseURI.raw::<structs::nsIURI>()),
