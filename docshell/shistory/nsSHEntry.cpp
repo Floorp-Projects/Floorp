fe70b06a1b7b36afe6972d3c5985dae0ea8163d4
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-01-18 15:09:55 +0000

    Bug 1603734 - browser_frame_history.js triggers a bug in sessionstore when run in e10s, r=farre
    SHIP starts IDs from 1, so non-SHIP could too.
    I decided to change nsSHEntry and not SessionStore since that seems to be safer approach in case
    one passes some explicit json data to the current SessionStore implementation and relies on the old
    behavior.
    
    https://treeherder.mozilla.org/#/jobs?repo=try&revision=104f98e50e82359abc367f34551fa2f5100c578d
    https://treeherder.mozilla.org/jobs?repo=try&selectedTaskRun=Xts2E_kXQ8Sf5sGQVXNK8g.0&revision=62bd27e89022195d102d830a8ff4d864d8bd85fb&test_paths=browser%2Fcomponents%2Fsessionstore%2Ftest
    
    Try looks good, so perhaps we can also enable the test now in more places.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101830

--- docshell/shistory/nsSHEntry.cpp
+++ docshell/shistory/nsSHEntry.cpp
@@ -33,7 +33,7 @@ static uint32_t gEntryID = 0;
 nsSHEntry::nsSHEntry()
     : mShared(new nsSHEntryShared()),
       mLoadType(0),
-      mID(gEntryID++),
+      mID(++gEntryID),  // SessionStore has special handling for 0 values.
       mScrollPositionX(0),
       mScrollPositionY(0),
       mParent(nullptr),
