b32e458629d365bce1e5493b1b30b87435cf1c0b
Caroline Cullen <ccullen@mozilla.com>
2021-01-18 18:18:44 +0000

    Bug 1686586 - Enabled spewing before entering JITs. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D101688

--- js/src/jit/Jit.cpp
+++ js/src/jit/Jit.cpp
@@ -144,6 +144,11 @@ EnterJitStatus js::jit::MaybeEnterJit(JSContext* cx, RunState& state) {
   JSScript* script = state.script();
 
   uint8_t* code = script->jitCodeRaw();
+
+#ifdef JS_CACHEIR_SPEW
+  cx->spewer().enableSpewing();
+#endif
+
   do {
     // Make sure we can enter Baseline Interpreter code. Note that the prologue
     // has warm-up checks to tier up if needed.
@@ -193,6 +198,10 @@ EnterJitStatus js::jit::MaybeEnterJit(JSContext* cx, RunState& state) {
 
     return EnterJitStatus::NotEntered;
   } while (false);
+
+#ifdef JS_CACHEIR_SPEW
+  cx->spewer().disableSpewing();
+#endif
 
   return EnterJit(cx, state, code);
 }
