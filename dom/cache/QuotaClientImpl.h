b011d2e6fb6b3ca0a1b2010ee6f2331ecdf86224
Simon Giesecke <sgiesecke@mozilla.com>
2021-01-18 08:52:06 +0000

    Bug 1683828 - Use QM_TRY et al. in QuotaClient. r=dom-workers-and-storage-reviewers,ttung
    Differential Revision: https://phabricator.services.mozilla.com/D100770

--- dom/cache/QuotaClientImpl.h
+++ dom/cache/QuotaClientImpl.h
@@ -60,68 +60,60 @@ class CacheQuotaClient final : public quota::Client {
   nsresult UpgradeStorageFrom2_0To2_1(nsIFile* aDirectory) override;
 
   template <typename Callable>
-  nsresult MaybeUpdatePaddingFileInternal(nsIFile* aBaseDir,
-                                          mozIStorageConnection* aConn,
+  nsresult MaybeUpdatePaddingFileInternal(nsIFile& aBaseDir,
+                                          mozIStorageConnection& aConn,
                                           const int64_t aIncreaseSize,
                                           const int64_t aDecreaseSize,
-                                          Callable aCommitHook) {
+                                          Callable&& aCommitHook) {
     MOZ_ASSERT(!NS_IsMainThread());
-    MOZ_DIAGNOSTIC_ASSERT(aBaseDir);
-    MOZ_DIAGNOSTIC_ASSERT(aConn);
     MOZ_DIAGNOSTIC_ASSERT(aIncreaseSize >= 0);
     MOZ_DIAGNOSTIC_ASSERT(aDecreaseSize >= 0);
 
-    nsresult rv;
-
     // Temporary should be removed at the end of each action. If not, it means
     // the failure happened.
-    bool temporaryPaddingFileExist =
-        mozilla::dom::cache::DirectoryPaddingFileExists(
-            *aBaseDir, DirPaddingFile::TMP_FILE);
+    const bool temporaryPaddingFileExist =
+        DirectoryPaddingFileExists(aBaseDir, DirPaddingFile::TMP_FILE);
 
     if (aIncreaseSize == aDecreaseSize && !temporaryPaddingFileExist) {
       // Early return here, since most cache actions won't modify padding size.
-      rv = aCommitHook();
-      Unused << NS_WARN_IF(NS_FAILED(rv));
-      return rv;
+      CACHE_TRY(aCommitHook());
+
+      return NS_OK;
     }
 
     {
       MutexAutoLock lock(mDirPaddingFileMutex);
-      rv = mozilla::dom::cache::LockedUpdateDirectoryPaddingFile(
-          *aBaseDir, *aConn, aIncreaseSize, aDecreaseSize,
-          temporaryPaddingFileExist);
-      if (NS_WARN_IF(NS_FAILED(rv))) {
-        // Don't delete the temporary padding file here to force the next action
-        // recalculate the padding size.
-        return rv;
-      }
 
-      rv = aCommitHook();
-      if (NS_WARN_IF(NS_FAILED(rv))) {
-        // Don't delete the temporary padding file here to force the next action
-        // recalculate the padding size.
-        return rv;
-      }
+      // Don't delete the temporary padding file in case of an error to force
+      // the next action recalculate the padding size.
+      CACHE_TRY(LockedUpdateDirectoryPaddingFile(aBaseDir, aConn, aIncreaseSize,
+                                                 aDecreaseSize,
+                                                 temporaryPaddingFileExist));
 
-      rv = mozilla::dom::cache::LockedDirectoryPaddingFinalizeWrite(*aBaseDir);
-      if (NS_WARN_IF(NS_FAILED(rv))) {
-        // Force restore file next time.
-        Unused << mozilla::dom::cache::LockedDirectoryPaddingDeleteFile(
-            *aBaseDir, DirPaddingFile::FILE);
+      // Don't delete the temporary padding file in case of an error to force
+      // the next action recalculate the padding size.
+      CACHE_TRY(aCommitHook());
 
-        // Ensure that we are able to force the padding file to be restored.
-        MOZ_ASSERT(mozilla::dom::cache::DirectoryPaddingFileExists(
-            *aBaseDir, DirPaddingFile::TMP_FILE));
+      CACHE_TRY(
+          ToResult(LockedDirectoryPaddingFinalizeWrite(aBaseDir))
+              .orElse([&aBaseDir](const nsresult) -> Result<Ok, nsresult> {
+                // Force restore file next time.
+                Unused << LockedDirectoryPaddingDeleteFile(
+                    aBaseDir, DirPaddingFile::FILE);
 
-        // Since both the body file and header have been stored in the
-        // file-system, just make the action be resolve and let the padding file
-        // be restored in the next action.
-        rv = NS_OK;
-      }
+                // Ensure that we are able to force the padding file to be
+                // restored.
+                MOZ_ASSERT(DirectoryPaddingFileExists(
+                    aBaseDir, DirPaddingFile::TMP_FILE));
+
+                // Since both the body file and header have been stored in the
+                // file-system, just make the action be resolve and let the
+                // padding file be restored in the next action.
+                return Ok{};
+              }));
     }
 
-    return rv;
+    return NS_OK;
   }
 
   nsresult RestorePaddingFileInternal(nsIFile* aBaseDir,
