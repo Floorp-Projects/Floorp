0e75d62a6e7ed1a021c0ff61265543b93418a6c7
Jan de Mooij <jdemooij@mozilla.com>
2021-01-18 14:24:56 +0000

    Bug 1686936 part 4 - Make TypedArrayBitwiseSlice work with large buffers. r=anba
    Differential Revision: https://phabricator.services.mozilla.com/D102146

--- js/src/jit-test/tests/large-buffers/typed-array.js
+++ js/src/jit-test/tests/large-buffers/typed-array.js
@@ -48,3 +48,32 @@ function testCopyWithin() {
     assertEq(ta[4 * gb + 6], 117);
 }
 testCopyWithin();
+
+function testSlice() {
+    // Large |start| and |end|.
+    ta[4 * gb + 0] = 11;
+    ta[4 * gb + 1] = 22;
+    ta[4 * gb + 2] = 33;
+    ta[4 * gb + 3] = 44;
+    var ta2 = ta.slice(4 * gb, 4 * gb + 4);
+    assertEq(ta2.toString(), "11,22,33,44");
+
+    // Large |start|.
+    ta[ta.length - 3] = 99;
+    ta[ta.length - 2] = 88;
+    ta[ta.length - 1] = 77;
+    ta2 = ta.slice(4 * gb + 8);
+    assertEq(ta2.toString(), "88,77");
+
+    // Relative values.
+    ta2 = ta.slice(-3, -1);
+    assertEq(ta2.toString(), "99,88");
+
+    // Large relative values.
+    ta[0] = 100;
+    ta[1] = 101;
+    ta[2] = 102;
+    ta2 = ta.slice(-ta.length, -ta.length + 3);
+    assertEq(ta2.toString(), "100,101,102");
+}
+testSlice();
