ef0fc05c9c353fe9273258adcf0fa18cdf34923b
Robert Mader <robert.mader@posteo.de>
2021-01-18 09:26:21 +0000

    Bug 1588904 - [Linux/EGL] Use correct rendering device in multi-GPU setup, r=stransky,emilio,jgilbert,geckoview-reviewers,m_kato
    Fetch the DRM device in the EGL version of glxtest, set it in gfxInfo and pass
    it to gfxVars. Finally, use it in nsDMABufDevice::Configure().
    
    While on it, also clean up EGL typedefs and defines a bit to match how it's
    done for GLX.
    
    Inspired by and copied from wlroots and Xwayland. Thanks to emersion!
    
    Differential Revision: https://phabricator.services.mozilla.com/D98108

--- widget/android/GfxInfo.h
+++ widget/android/GfxInfo.h
@@ -59,6 +59,7 @@ class GfxInfo : public GfxInfoBase {
   NS_IMETHOD GetDisplayInfo(nsTArray<nsString>& aDisplayInfo) override;
   NS_IMETHOD GetDisplayWidth(nsTArray<uint32_t>& aDisplayWidth) override;
   NS_IMETHOD GetDisplayHeight(nsTArray<uint32_t>& aDisplayHeight) override;
+  NS_IMETHOD GetDrmRenderDevice(nsACString& aDrmRenderDevice) override;
   using GfxInfoBase::GetFeatureStatus;
   using GfxInfoBase::GetFeatureSuggestedDriverVersion;
 
