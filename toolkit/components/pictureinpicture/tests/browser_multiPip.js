f6b8c4d848713d27df872e118a6a2ccc8d1db88b
Hunter Jones <whjones526@gmail.com>
2021-01-18 12:09:22 +0000

    Bug 1677107: Add Telemetry for tracking multiple PiP support usage r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D97077

--- toolkit/components/pictureinpicture/tests/browser_multiPip.js
+++ toolkit/components/pictureinpicture/tests/browser_multiPip.js
@@ -11,6 +11,12 @@ async function createTab() {
   });
 }
 
+function getTelemetryMaxPipCount(resetMax = false) {
+  const scalarData = Services.telemetry.getSnapshotForScalars("main", resetMax)
+    .parent;
+  return scalarData["pictureinpicture.most_concurrent_players"];
+}
+
 /**
  * Set pref for multiple PiP support first
  */
@@ -120,4 +126,107 @@ add_task(async () => {
   BrowserTestUtils.removeTab(secondTab);
   await secondClosed;
   info("Second PiP closed after closing the second tab");
+});
+
+/**
+ * Check that correct number of pip players are recorded for Telemetry
+ * tracking
+ */
+add_task(async () => {
+  // run this to flush recorded values from previous tests
+  getTelemetryMaxPipCount(true);
+
+  let firstTab = await createTab();
+  let secondTab = await createTab();
+
+  gBrowser.selectedTab = firstTab;
+
+  let firstPip = await triggerPictureInPicture(
+    firstTab.linkedBrowser,
+    "with-controls"
+  );
+  ok(firstPip, "Got first PiP window");
+
+  Assert.equal(
+    getTelemetryMaxPipCount(true),
+    1,
+    "There should only be 1 PiP window"
+  );
+
+  let secondPip = await triggerPictureInPicture(
+    firstTab.linkedBrowser,
+    "no-controls"
+  );
+  ok(secondPip, "Got second PiP window");
+
+  Assert.equal(
+    getTelemetryMaxPipCount(true),
+    2,
+    "There should be 2 PiP windows"
+  );
+
+  await ensureMessageAndClosePiP(
+    firstTab.linkedBrowser,
+    "no-controls",
+    secondPip,
+    false
+  );
+  info("Second PiP was open and is now closed");
+
+  gBrowser.selectedTab = secondTab;
+
+  let thirdPip = await triggerPictureInPicture(
+    secondTab.linkedBrowser,
+    "with-controls"
+  );
+  ok(thirdPip, "Got third PiP window");
+
+  let fourthPip = await triggerPictureInPicture(
+    secondTab.linkedBrowser,
+    "no-controls"
+  );
+  ok(fourthPip, "Got fourth PiP window");
+
+  Assert.equal(
+    getTelemetryMaxPipCount(false),
+    3,
+    "There should now be 3 PiP windows"
+  );
+
+  gBrowser.selectedTab = firstTab;
+
+  await ensureMessageAndClosePiP(
+    firstTab.linkedBrowser,
+    "with-controls",
+    firstPip,
+    false
+  );
+  info("First PiP was open, it is now closed.");
+
+  gBrowser.selectedTab = secondTab;
+
+  await ensureMessageAndClosePiP(
+    secondTab.linkedBrowser,
+    "with-controls",
+    thirdPip,
+    false
+  );
+  info("Third PiP was open, it is now closed.");
+
+  await ensureMessageAndClosePiP(
+    secondTab.linkedBrowser,
+    "no-controls",
+    fourthPip,
+    false
+  );
+  info("Fourth PiP was open, it is now closed.");
+
+  Assert.equal(
+    getTelemetryMaxPipCount(false),
+    3,
+    "Max PiP count should still be 3"
+  );
+
+  BrowserTestUtils.removeTab(firstTab);
+  BrowserTestUtils.removeTab(secondTab);
 });
