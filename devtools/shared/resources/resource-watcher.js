caa34d22a16b1b35dbf1737cc244bde850cf000c
Alexandre Poirot <poirot.alex@gmail.com>
2021-01-19 10:26:53 +0000

    Bug 1662129 - [devtools] Introduce breakpoint resource. r=nchevobbe,jdescottes
    This is the simple step, where we move the ThreadFront code related to pauses
    from Debugger frontend to a legacy listener.
    The next patch is the hard one, which will replace paused/resumed events by resources.
    
    It may sound weird to have pause and resume to becomes resources,
    as they aren't really resources. These are transcient events, which we don't
    really want to record. We especially do not really care to save them in the ResourceWatcher cache.
    
    But, by becoming resources, we benefit from the resources framework, which allows to:
    * listen and emit such event as early as the page/target starts loading
    * do not depend on a particular front to be ready/attached on the frontend
    * once we communicate to the watcher we care about breakpoints, we do register the breakpoint listening code for all the targets.
    (we no longer have to do a per-target work in the frontend)
    
    Differential Revision: https://phabricator.services.mozilla.com/D88851

--- devtools/shared/resources/resource-watcher.js
+++ devtools/shared/resources/resource-watcher.js
@@ -815,6 +815,7 @@ ResourceWatcher.TYPES = ResourceWatcher.prototype.TYPES = {
   INDEXED_DB: "indexed-db",
   NETWORK_EVENT_STACKTRACE: "network-event-stacktrace",
   SOURCE: "source",
+  BREAKPOINT: "breakpoint",
 };
 module.exports = { ResourceWatcher, TYPES: ResourceWatcher.TYPES };
 
@@ -873,6 +874,8 @@ const LegacyListeners = {
     .NETWORK_EVENT_STACKTRACE]: require("devtools/shared/resources/legacy-listeners/network-event-stacktraces"),
   [ResourceWatcher.TYPES
     .SOURCE]: require("devtools/shared/resources/legacy-listeners/source"),
+  [ResourceWatcher.TYPES
+    .BREAKPOINT]: require("devtools/shared/resources/legacy-listeners/breakpoint"),
 };
 
 // Optional transformers for each type of resource.
