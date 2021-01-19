bf3cf30b7a3d11188d619b4853c560ce3a8f81c0
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-01-19 15:45:04 +0000

    Bug 1682340 - [devtools] Prevent triggering Reader Mode with reverse search keyboard shortcut on Windows. r=bomsy.
    We weren't calling preventDefault on the event handler, which was
    allowing the browser to consume it, and toggle reader mode on windows,
    which shares the same keyboard shortcut (F9).
    A test is added to ensure we don't regress this (the test is failing without
    the fix).
    
    Differential Revision: https://phabricator.services.mozilla.com/D101919

---
+++ devtools/client/webconsole/test/browser/browser_jsterm_editor_reverse_search_keyboard_navigation.js
@@ -0,0 +1,123 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+// Ensure keyboard navigation works in editor mode and does
+// not trigger reader mode (See 1682340).
+
+const TEST_URI = `http://example.com/browser/toolkit/components/reader/test/readerModeArticle.html`;
+const isMacOS = AppConstants.platform === "macosx";
+
+add_task(async function() {
+  await pushPref("devtools.webconsole.input.editor", true);
+  await pushPref("reader.parse-on-load.enabled", true);
+  // Disable eager evaluation to avoid intermittent failures due to pending
+  // requests to evaluateJSAsync.
+  await pushPref("devtools.webconsole.input.eagerEvaluation", false);
+
+  const readerModeButtonEl = document.querySelector("#reader-mode-button");
+
+  const hud = await openNewTabAndConsole(TEST_URI);
+  await waitFor(
+    () => !readerModeButtonEl.hidden,
+    "wait for the reader mode button to be displayed"
+  );
+
+  const jstermHistory = [
+    `document
+         .querySelectorAll("*")
+         .forEach(console.log)`,
+    `Dog = "Snoopy"`,
+  ];
+
+  const onLastMessage = waitForMessage(hud, `"Snoopy"`, ".result");
+  for (const input of jstermHistory) {
+    execute(hud, input);
+  }
+  await onLastMessage;
+  await openReverseSearch(hud);
+
+  // Wait for a bit so reader mode would have some time to initialize.
+  await wait(1000);
+  is(
+    readerModeButtonEl.getAttribute("readeractive"),
+    "",
+    "reader mode wasn't activated"
+  );
+
+  EventUtils.sendString("d");
+  const infoElement = await waitFor(() => getReverseSearchInfoElement(hud));
+  is(
+    infoElement.textContent,
+    "2 of 2 results",
+    "The reverse info has the expected text"
+  );
+
+  is(getInputValue(hud), jstermHistory[1], "JsTerm has the expected input");
+
+  await navigateResultsAndCheckState(hud, {
+    direction: "previous",
+    expectedInfoText: "1 of 2 results",
+    expectedJsTermInputValue: jstermHistory[0],
+  });
+
+  await navigateResultsAndCheckState(hud, {
+    direction: "next",
+    expectedInfoText: "2 of 2 results",
+    expectedJsTermInputValue: jstermHistory[1],
+  });
+
+  // Wait for a bit so reader mode would have some time to initialize.
+  await wait(1000);
+  is(
+    readerModeButtonEl.getAttribute("readeractive"),
+    "",
+    "reader mode still wasn't activated"
+  );
+
+  await closeToolbox();
+});
+
+async function navigateResultsAndCheckState(
+  hud,
+  { direction, expectedInfoText, expectedJsTermInputValue }
+) {
+  const onJsTermValueChanged = hud.jsterm.once("set-input-value");
+  if (direction === "previous") {
+    triggerPreviousResultShortcut();
+  } else {
+    triggerNextResultShortcut();
+  }
+  await onJsTermValueChanged;
+
+  is(getInputValue(hud), expectedJsTermInputValue, "JsTerm has expected value");
+
+  const infoElement = getReverseSearchInfoElement(hud);
+  is(
+    infoElement.textContent,
+    expectedInfoText,
+    "The reverse info has the expected text"
+  );
+  is(
+    isReverseSearchInputFocused(hud),
+    true,
+    "reverse search input is still focused"
+  );
+}
+
+function triggerPreviousResultShortcut() {
+  if (isMacOS) {
+    EventUtils.synthesizeKey("r", { ctrlKey: true });
+  } else {
+    EventUtils.synthesizeKey("VK_F9");
+  }
+}
+
+function triggerNextResultShortcut() {
+  if (isMacOS) {
+    EventUtils.synthesizeKey("s", { ctrlKey: true });
+  } else {
+    EventUtils.synthesizeKey("VK_F9", { shiftKey: true });
+  }
+}
