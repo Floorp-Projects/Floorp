f48a8f39b34166d7ef2c4bb8169620348eefe8e3
Robert Mader <robert.mader@posteo.de>
2021-01-19 17:33:38 +0000

    Bug 1645695 - Only update popup allocation when visible. r=stransky
    In `NativeMoveResizeWaylandPopupCB` we update the allocation of popups
    and many menus like the hamburger menu get moved to another position here.
    
    When we close the menu, the allocation gets reset in
    `nsView::ResetWidgetBounds`, making us call `NativeMoveResizeWaylandPopup`,
    triggering `NativeMoveResizeWaylandPopupCB` to set the desired size again,
    making us trigger `NativeMoveResizeWaylandPopup` again - confusing our
    calculation of `isWidgetVisible`, making us show the menu again when it
    should be hidden.
    
    To avoid this, don't connect `NativeMoveResizeWaylandPopupCB` when the
    widget is not visible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102046

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1768,7 +1768,8 @@ void nsWindow::NativeMoveResizeWaylandPopup(GdkPoint* aPosition,
   }
 #endif
 
-  if (!g_signal_handler_find(
+  if (isWidgetVisible &&
+      !g_signal_handler_find(
           gdkWindow, G_SIGNAL_MATCH_FUNC, 0, 0, nullptr,
           FuncToGpointer(NativeMoveResizeWaylandPopupCallback), this)) {
     g_signal_connect(gdkWindow, "moved-to-rect",
