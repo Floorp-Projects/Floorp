41cc8432c0a73de7c3d2eb3b95c8ecdec69c36c0
Ted Campbell <tcampbell@mozilla.com>
2021-01-19 00:41:54 +0000

    Bug 1687338 - Transcode XDR-Stencil header directly to target buffer. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D102243

--- js/src/vm/Xdr.cpp
+++ js/src/vm/Xdr.cpp
@@ -756,21 +756,25 @@ XDRResult XDRIncrementalStencilEncoder::linearize(JS::TranscodeBuffer& buffer,
                 JS::IsTranscodingBytecodeAligned(buffer.begin()));
   MOZ_ASSERT(JS::IsTranscodingBytecodeOffsetAligned(buffer.length()));
 
-  Rooted<ScriptSourceHolder> holder(cx(), ss);
-  uint32_t nchunks = 1 + encodedFunctions_.count();
+  // Use the output buffer directly. The caller may have already have data in
+  // the buffer so ensure we skip over it.
+  XDRBuffer<XDR_ENCODE> outputBuf(cx(), buffer, buffer.length());
 
-  switchToHeaderBuf();
-  MOZ_TRY(XDRStencilHeader(this, nullptr, &holder, &nchunks));
-  switchToMainBuf();
+  // Code the header directly in the output buffer.
+  {
+    switchToBuffer(&outputBuf);
 
-  size_t totalLength = buffer.length() + header_.length() + slices_.length();
-  if (!buffer.reserve(totalLength)) {
-    ReportOutOfMemory(cx());
-    return fail(JS::TranscodeResult_Throw);
+    Rooted<ScriptSourceHolder> holder(cx(), ss);
+    uint32_t nchunks = 1 + encodedFunctions_.count();
+    MOZ_TRY(XDRStencilHeader(this, nullptr, &holder, &nchunks));
+
+    switchToMainBuf();
   }
 
-  buffer.infallibleAppend(header_.begin(), header_.length());
-  buffer.infallibleAppend(slices_.begin(), slices_.length());
+  // The accumlated transcode data can now be copied to the output buffer.
+  if (!buffer.append(slices_.begin(), slices_.length())) {
+    return fail(JS::TranscodeResult_Throw);
+  }
 
   return Ok();
 }
