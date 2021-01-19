2f991e8d427f5a8080cd0e3dc6d42dfcfe23fd14
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-01-19 15:45:04 +0000

    Bug 1682340 - [devtools] Prevent triggering Reader Mode with reverse search keyboard shortcut on Windows. r=bomsy.
    We weren't calling preventDefault on the event handler, which was
    allowing the browser to consume it, and toggle reader mode on windows,
    which shares the same keyboard shortcut (F9).
    A test is added to ensure we don't regress this (the test is failing without
    the fix).
    
    Differential Revision: https://phabricator.services.mozilla.com/D101919

--- devtools/client/webconsole/components/App.js
+++ devtools/client/webconsole/components/App.js
@@ -145,6 +145,8 @@ class App extends Component {
         actions.reverseSearchInputToggle({ initialValue, access: "keyboard" })
       );
       event.stopPropagation();
+      // Prevent Reader Mode to be enabled (See Bug 1682340)
+      event.preventDefault();
     }
 
     if (
