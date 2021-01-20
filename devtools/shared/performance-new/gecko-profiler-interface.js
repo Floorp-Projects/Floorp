c17ccc661a0e4dd30e5bec85d1c0f39be70eee57
Florian Quèze <florian@queze.net>
2021-01-20 14:36:21 +0000

    Bug 1687677 - ignore content windows when checking if a new window is a private window that should lock the profiler, r=julienw.
    Differential Revision: https://phabricator.services.mozilla.com/D102405

--- devtools/shared/performance-new/gecko-profiler-interface.js
+++ devtools/shared/performance-new/gecko-profiler-interface.js
@@ -211,7 +211,10 @@ class ActorReadyGeckoProfilerInterface {
     // events in the perf actor.
     switch (topic) {
       case "chrome-document-global-created":
-        if (PrivateBrowsingUtils.isWindowPrivate(subject)) {
+        if (
+          subject.isChromeWindow &&
+          PrivateBrowsingUtils.isWindowPrivate(subject)
+        ) {
           this.emit("profile-locked-by-private-browsing");
         }
         break;
