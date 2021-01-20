a901a3844e924ae8cb54f7449b2b231809351131
Micah Tigley <mtigley@mozilla.com>
2021-01-20 17:00:36 +0000

    Bug 1682393 - Show origin for tabmodal alert/confirm/prompt dialogs. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D102077

--- browser/base/content/test/tabdialogs/browser_tabdialogbox_content_prompts.js
+++ browser/base/content/test/tabdialogs/browser_tabdialogbox_content_prompts.js
@@ -6,6 +6,14 @@
 const CONTENT_PROMPT_PREF = "prompts.contentPromptSubDialog";
 const TEST_ROOT_CHROME = getRootDirectory(gTestPath);
 const TEST_DIALOG_PATH = TEST_ROOT_CHROME + "subdialog.xhtml";
+const TEST_URL = "data:text/html,<body onload='alert(1)'>";
+
+// Setup.
+add_task(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [[CONTENT_PROMPT_PREF, true]],
+  });
+});
 
 /**
  * Test that a manager for content prompts is added to tab dialog box.
@@ -14,10 +22,6 @@ add_task(async function test_tabdialog_content_prompts() {
   await BrowserTestUtils.withNewTab("http://example.com", async function(
     browser
   ) {
-    await SpecialPowers.pushPrefEnv({
-      set: [[CONTENT_PROMPT_PREF, true]],
-    });
-
     info("Open a tab prompt.");
     let dialogBox = gBrowser.getTabDialogBox(browser);
     dialogBox.open(TEST_DIALOG_PATH);
@@ -40,5 +44,33 @@ add_task(async function test_tabdialog_content_prompts() {
       1,
       "Content prompt manager should have 1 dialog box."
     );
+  });
+});
+
+/**
+ * Test that title text is shown in tabmodal alert/confirm/prompt dialogs.
+ */
+add_task(async function test_tabdialog_show_title() {
+  let dialogShown = BrowserTestUtils.waitForEvent(
+    gBrowser,
+    "DOMWillOpenModalDialog"
+  );
+
+  await BrowserTestUtils.withNewTab(TEST_URL, async function(browser) {
+    info("Waiting for dialog to open.");
+    await dialogShown;
+
+    info("Check the title is visible.");
+    let dialogBox = gBrowser.getTabDialogBox(browser);
+    let contentPromptManager = dialogBox.getContentDialogManager();
+    let dialog = contentPromptManager._dialogs[0];
+
+    info("Waiting for dialog frame to be ready.");
+    await dialog._dialogReady;
+
+    let dialogDoc = dialog._frame.contentWindow.document;
+    let infoTitle = dialogDoc.querySelector("#infoTitle");
+
+    ok(BrowserTestUtils.is_visible(infoTitle), "Title text is visible");
   });
 });
