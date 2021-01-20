dabbe1fbe931509e62a669a7cbed8ebead32346e
Lee Salzman <lsalzman@mozilla.com>
2021-01-20 17:02:34 +0000

    Bug 1683084 - add SWGL fast-path for ps_split_composite shader. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D102444

--- gfx/wr/webrender/res/ps_split_composite.glsl
+++ gfx/wr/webrender/res/ps_split_composite.glsl
@@ -117,4 +117,41 @@ void main(void) {
     vec2 uv = clamp(vUv * perspective_divisor, vUvSampleBounds.xy, vUvSampleBounds.zw);
     write_output(alpha * texture(sColor0, uv));
 }
+
+#ifdef SWGL
+void swgl_drawSpanRGBA8() {
+    if (!swgl_isTextureRGBA8(sColor0) || !swgl_isTextureLinear(sColor0)) {
+        return;
+    }
+
+    float perspective_divisor = mix(swgl_forceScalar(gl_FragCoord.w), 1.0, vPerspective);
+
+    vec2 uv = vUv * perspective_divisor;
+
+    if (!needs_clip() && swgl_allowTextureNearest(sColor0, uv)) {
+        swgl_commitTextureNearestRGBA8(sColor0, uv, vUvSampleBounds, 0);
+        return;
+    }
+
+    uv = swgl_linearQuantize(sColor0, uv);
+    vec2 min_uv = swgl_linearQuantize(sColor0, vUvSampleBounds.xy);
+    vec2 max_uv = swgl_linearQuantize(sColor0, vUvSampleBounds.zw);
+    vec2 step_uv = swgl_linearQuantizeStep(sColor0, swgl_interpStep(vUv)) * perspective_divisor;
+
+    if (needs_clip()) {
+        while (swgl_SpanLength > 0) {
+            float alpha = do_clip();
+            swgl_commitTextureLinearColorRGBA8(sColor0, clamp(uv, min_uv, max_uv), alpha, 0);
+            uv += step_uv;
+            vClipMaskUv += swgl_interpStep(vClipMaskUv);
+        }
+    } else {
+        while (swgl_SpanLength > 0) {
+            swgl_commitTextureLinearRGBA8(sColor0, clamp(uv, min_uv, max_uv), 0);
+            uv += step_uv;
+        }
+    }
+}
+#endif
+
 #endif
