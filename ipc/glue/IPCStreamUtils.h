677e7284e3e1e4f66787038a2739c6afc2992c81
Nika Layzell <nika@thelayzells.com>
2021-01-20 16:20:11 +0000

    Bug 1681529 - Part 5: Serialize `nsIInputStream` over IPC with aDelayedStart by default, r=baku
    The current set of cases where a nsIInputStream is directly serialized over IPC
    is fairly limited, and includes cases where the IPDL struct may be directly
    stored within a content process, having the nsIInputStream left unused.
    
    By switching this serialization to DelayedStart, we can avoid performing
    unnecessary streaming copies of IPC data when sending postdata streams related
    to session history and document navigation, while also avoiding issues like this
    coming up again due to delayed start being the default.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101804

--- ipc/glue/IPCStreamUtils.h
+++ ipc/glue/IPCStreamUtils.h
@@ -200,6 +200,8 @@ class HoldIPCStream final : public AutoIPCStream {
  public:
   NS_INLINE_DECL_REFCOUNTING(HoldIPCStream)
 
+  using AutoIPCStream::AutoIPCStream;
+
  private:
   ~HoldIPCStream() = default;
 };
