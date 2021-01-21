87d8f32132d9b14025afe0d4c9b098c892c95d9c
Florian Quèze <florian@queze.net>
2021-01-21 12:58:33 +0000

    Bug 1687687 - Using the profiler for Talos tests should enable the CPU Utilization feature, r=perftest-reviewers,Bebe
    Differential Revision: https://phabricator.services.mozilla.com/D102450

--- testing/talos/talos/tests/tart/addon/content/Profiler.js
+++ testing/talos/talos/tests/tart/addon/content/Profiler.js
@@ -94,7 +94,7 @@ var Profiler;
         _profiler.StartProfiler(
           profiler_entries,
           profiler_interval,
-          ["js", "leaf", "stackwalk", "threads"],
+          ["js", "leaf", "stackwalk", "threads", "cpu"],
           profiler_threadsArray
         );
         _profiler.PauseSampling();
