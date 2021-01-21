3d510c53ce9c5b2b2baf93a7e5c4ac81c0a2a19a
Edgar Chen <echen@mozilla.com>
2021-01-21 14:54:55 +0000

    Bug 1686037 - Part 3: Remove duplicated ImplicitlyReleasePointerCapture call in PresShell; r=smaug
    EventStateManager::PostHandleEvent would do the same thing, but need to handle
    the cases that PresShell is destroyed and frame is destroyed in
    pointerup/pointercancel event listener.
    
    The former case would be handled in D102403. For the latter case, we allow
    EventStateManager::PostHandleEvent to handle pointerup/pointercancel event while
    frame is no longer available in this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102404

--- layout/base/PresShell.cpp
+++ layout/base/PresShell.cpp
@@ -8269,16 +8269,6 @@ nsresult PresShell::EventHandler::DispatchEvent(
 
   nsContentUtils::SetIsHandlingKeyBoardEvent(wasHandlingKeyBoardEvent);
 
-  if (aEvent->mMessage == ePointerUp || aEvent->mMessage == ePointerCancel) {
-    // Implicitly releasing capture for given pointer.
-    // ePointerLostCapture should be send after ePointerUp or
-    // ePointerCancel.
-    WidgetPointerEvent* pointerEvent = aEvent->AsPointerEvent();
-    MOZ_ASSERT(pointerEvent);
-    PointerEventHandler::ReleasePointerCaptureById(pointerEvent->pointerId);
-    PointerEventHandler::CheckPointerCaptureState(pointerEvent);
-  }
-
   if (mPresShell->IsDestroying()) {
     return NS_OK;
   }
