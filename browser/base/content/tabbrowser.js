c2cbf590a66539a5398f427c94e851510779abe5
Cosmin Sabou <csabou@mozilla.com>
2021-01-21 21:35:33 +0200

    Backed out changeset 975f6057245a (bug 1572798) for turning bug 1511706 to perma. CLOSED TREE

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -2000,7 +2000,6 @@
       uriIsAboutBlank,
       userContextId,
       skipLoad,
-      initiallyActive,
     } = {}) {
       let b = document.createXULElement("browser");
       // Use the JSM global to create the permanentKey, so that if the
@@ -2031,10 +2030,6 @@
       };
       for (let attribute in defaultBrowserAttributes) {
         b.setAttribute(attribute, defaultBrowserAttributes[attribute]);
-      }
-
-      if (!initiallyActive) {
-        b.setAttribute("initiallyactive", "false");
       }
 
       if (userContextId) {
