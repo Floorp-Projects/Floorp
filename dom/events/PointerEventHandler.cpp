915bbf9071905ea6d447252ffd811f5a4c5221a3
Edgar Chen <echen@mozilla.com>
2021-01-21 16:38:59 +0000

    Bug 1686037 - Part 2: Clean up pointer capture info when PresContext is destroyed; r=smaug
    Found a possible leak from running layout/base/tests/test_bug993936.html after
    enable implicit pointer capture for touch event. The test synthesize touchstart
    and touchmove event, but no touchend, so we don't run the release steps and the
    PointerCaptureInfo still hold a reference to Element which cause the leak.
    
    This could also possible happens in real world, for example, user touch a page
    with finger that triggers pointer capture, and then tab get closed before touch
    is released.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102403

--- dom/events/PointerEventHandler.cpp
+++ dom/events/PointerEventHandler.cpp
@@ -356,22 +356,24 @@ void PointerEventHandler::CheckPointerCaptureState(WidgetPointerEvent* aEvent) {
       captureInfo->mPendingElement == captureInfo->mOverrideElement) {
     return;
   }
-  // cache captureInfo->mPendingElement since it may be changed in the pointer
-  // event listener
-  RefPtr<Element> pendingElement = captureInfo->mPendingElement.get();
-  if (captureInfo->mOverrideElement) {
-    RefPtr<Element> overrideElement = captureInfo->mOverrideElement;
+
+  RefPtr<Element> overrideElement = captureInfo->mOverrideElement;
+  RefPtr<Element> pendingElement = captureInfo->mPendingElement;
+
+  // Update captureInfo before dispatching event since sPointerCaptureList may
+  // be changed in the pointer event listener.
+  captureInfo->mOverrideElement = captureInfo->mPendingElement;
+  if (captureInfo->Empty()) {
+    sPointerCaptureList->Remove(aEvent->pointerId);
+  }
+
+  if (overrideElement) {
     DispatchGotOrLostPointerCaptureEvent(/* aIsGotCapture */ false, aEvent,
                                          overrideElement);
   }
   if (pendingElement) {
     DispatchGotOrLostPointerCaptureEvent(/* aIsGotCapture */ true, aEvent,
                                          pendingElement);
-  }
-
-  captureInfo->mOverrideElement = std::move(pendingElement);
-  if (captureInfo->Empty()) {
-    sPointerCaptureList->Remove(aEvent->pointerId);
   }
 }
 
@@ -690,6 +692,29 @@ void PointerEventHandler::DispatchPointerFromMouseOrTouch(
 }
 
 /* static */
+void PointerEventHandler::NotifyDestroyPresContext(
+    nsPresContext* aPresContext) {
+  // Clean up pointer capture info
+  for (auto iter = sPointerCaptureList->Iter(); !iter.Done(); iter.Next()) {
+    PointerCaptureInfo* data = iter.UserData();
+    MOZ_ASSERT(data, "how could we have a null PointerCaptureInfo here?");
+    if (data->mPendingElement &&
+        data->mPendingElement->GetPresContext(Element::eForComposedDoc) ==
+            aPresContext) {
+      data->mPendingElement = nullptr;
+    }
+    if (data->mOverrideElement &&
+        data->mOverrideElement->GetPresContext(Element::eForComposedDoc) ==
+            aPresContext) {
+      data->mOverrideElement = nullptr;
+    }
+    if (data->Empty()) {
+      iter.Remove();
+    }
+  }
+}
+
+/* static */
 uint16_t PointerEventHandler::GetPointerType(uint32_t aPointerId) {
   PointerInfo* pointerInfo = nullptr;
   if (sActivePointersIds->Get(aPointerId, &pointerInfo) && pointerInfo) {
@@ -713,7 +738,7 @@ void PointerEventHandler::DispatchGotOrLostPointerCaptureEvent(
     Element* aCaptureTarget) {
   Document* targetDoc = aCaptureTarget->OwnerDoc();
   RefPtr<PresShell> presShell = targetDoc->GetPresShell();
-  if (NS_WARN_IF(!presShell)) {
+  if (NS_WARN_IF(!presShell || presShell->IsDestroying())) {
     return;
   }
 
