6e232057a8e437fbcc0726d94cf840c8cbaef373
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-01-22 21:40:46 +0000

    Bug 1686229 - Part3. Display a module's loading status on about:support.  r=Gijs,fluent-reviewers
    This patch displays the new telemetry field "loadStatus", which was introduced
    by the earlier patch, on about:support.  With this information, a user can see
    each of injected modules was really loaded or blocked by our DLL blocklist.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102409

--- toolkit/content/aboutSupport.js
+++ toolkit/content/aboutSupport.js
@@ -1189,6 +1189,16 @@ var snapshotFormatters = {
         : $.new(tag, "", "", {
             "data-l10n-id": "support-third-party-modules-no-value",
           });
+    const createLoadStatusElement = (tag, status) => {
+      const labelLoadStatus = [
+        "support-third-party-modules-status-loaded",
+        "support-third-party-modules-status-blocked",
+        "support-third-party-modules-status-redirected",
+      ];
+      return status >= 0 && status < labelLoadStatus.length
+        ? $.new(tag, "", "", { "data-l10n-id": labelLoadStatus[status] })
+        : $.new(tag, status);
+    };
 
     const iconUp = "chrome://global/skin/icons/arrow-up-12.svg";
     const iconDown = "chrome://global/skin/icons/arrow-dropdown-12.svg";
@@ -1233,6 +1243,7 @@ var snapshotFormatters = {
             // loadDurationMS can be empty (not zero) when a module is loaded
             // very early in the process.  processUptimeMS always has a value.
             createElementWithLabel("td", event.loadDurationMS),
+            createLoadStatusElement("td", event.loadStatus),
           ])
         );
       }
@@ -1259,6 +1270,9 @@ var snapshotFormatters = {
                   }),
                   $.new("th", "", "", {
                     "data-l10n-id": "support-third-party-modules-duration",
+                  }),
+                  $.new("th", "", "", {
+                    "data-l10n-id": "support-third-party-modules-status",
                   }),
                 ]),
                 innerTBody,
