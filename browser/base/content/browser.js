9bf4b4a79e2124547238f37dcc372a1dd1af9d87
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2021-01-22 01:13:39 +0000

    Bug 1681138 - avoid delaying loading the bookmarks toolbar in new windows post-startup, r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D101689

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2295,10 +2295,6 @@ var gBrowserInit = {
     }
 
     scheduleIdleTask(() => {
-      PlacesToolbarHelper.startShowingToolbar();
-    });
-
-    scheduleIdleTask(() => {
       // Initialize the Sync UI
       gSync.init();
     });
