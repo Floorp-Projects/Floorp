c88af69e7f575d1b55d84c4ff25cf78f75957318
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-01-23 00:12:49 +0000

    Bug 1686229 - Part1. Add a member to ModuleLoadInfo to indicate the status of DLL load.  r=mhowell
    We used to record a DLL loading event only when a module was loaded.
    With this patch, we record an event for a module blocked by our DLL
    blocklist as well as a loaded module.  It is achieved by calling
    to `ModuleLoadFrame::NotifySectionMap` in `patched_NtMapViewOfSection`
    regardless of the block action.
    
    This patch introduces a new member `ModuleLoadInfo::mStatus` and
    `ProcessedModuleLoadEvent::mLoadStatus` to keep the DLL loading status,
    which will be added to the third-party-modules ping by a following patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102407

--- browser/app/winlauncher/freestanding/DllBlocklist.cpp
+++ browser/app/winlauncher/freestanding/DllBlocklist.cpp
@@ -437,28 +437,41 @@ NTSTATUS NTAPI patched_NtMapViewOfSection(
         resultView.isOk() ? &resultView.inspect()->mK32Exports : nullptr);
   }
 
-  if (blockAction == BlockAction::Allow) {
-    if (nt::RtlGetProcessHeap()) {
-      ModuleLoadFrame::NotifySectionMap(
-          nt::AllocatedUnicodeString(sectionFileName), *aBaseAddress,
-          stubStatus);
-    }
-    return stubStatus;
+  ModuleLoadInfo::Status loadStatus = ModuleLoadInfo::Status::Blocked;
+
+  switch (blockAction) {
+    case BlockAction::Allow:
+      loadStatus = ModuleLoadInfo::Status::Loaded;
+      break;
+
+    case BlockAction::NoOpEntryPoint:
+      loadStatus = ModuleLoadInfo::Status::Redirected;
+      break;
+
+    case BlockAction::SubstituteLSP:
+      // The process heap needs to be available here because
+      // NotifyLSPSubstitutionRequired below copies a given string into
+      // the heap. We use a soft assert here, assuming LSP load always
+      // occurs after the heap is initialized.
+      MOZ_ASSERT(nt::RtlGetProcessHeap());
+
+      // Notify patched_LdrLoadDll that it will be necessary to perform
+      // a substitution before returning.
+      ModuleLoadFrame::NotifyLSPSubstitutionRequired(&leafOnStack);
+      break;
+
+    default:
+      break;
   }
 
-  if (blockAction == BlockAction::SubstituteLSP) {
-    // The process heap needs to be available here because
-    // NotifyLSPSubstitutionRequired below copies a given string into the heap.
-    // We use a soft assert here, assuming LSP load always occurs after the heap
-    // is initialized.
-    MOZ_ASSERT(nt::RtlGetProcessHeap());
-
-    // Notify patched_LdrLoadDll that it will be necessary to perform a
-    // substitution before returning.
-    ModuleLoadFrame::NotifyLSPSubstitutionRequired(&leafOnStack);
+  if (nt::RtlGetProcessHeap()) {
+    ModuleLoadFrame::NotifySectionMap(
+        nt::AllocatedUnicodeString(sectionFileName), *aBaseAddress, stubStatus,
+        loadStatus);
   }
 
-  if (blockAction == BlockAction::NoOpEntryPoint) {
+  if (loadStatus == ModuleLoadInfo::Status::Loaded ||
+      loadStatus == ModuleLoadInfo::Status::Redirected) {
     return stubStatus;
   }
 
