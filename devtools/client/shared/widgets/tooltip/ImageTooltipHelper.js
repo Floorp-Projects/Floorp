fcd2e242f171390993f4ada473a5cb8326b8139c
Sebastian Zartner <sebastianzartner@gmail.com>
2021-01-23 21:38:22 +0000

    Bug 1583901 - New inactive CSS rule to show warning when fill-* or stroke-* is used on non-SVG-basic-shape and non-SVG-text elements. r=fluent-reviewers,flod,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D101295

--- devtools/client/shared/widgets/tooltip/ImageTooltipHelper.js
+++ devtools/client/shared/widgets/tooltip/ImageTooltipHelper.js
@@ -36,8 +36,8 @@ function getImageDimensions(doc, imageUrl) {
     const imgObj = new doc.defaultView.Image();
     imgObj.onload = () => {
       imgObj.onload = null;
-      const { naturalWidth, naturalHeight } = imgObj;
-      resolve({ naturalWidth, naturalHeight });
+      const { width, height } = imgObj;
+      resolve({ width, height });
     };
     imgObj.src = imageUrl;
   });
