0eac118b183b4fd4108b4a943df815217d1bd7e6
Csoregi Natalia <ncsoregi@mozilla.com>
2021-01-23 19:45:30 +0200

    Backed out 2 changesets (bug 1688327) as per request.
    Backed out changeset 991388ec272d (bug 1688327)
    Backed out changeset 6648750ced7e (bug 1688327)

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -2806,6 +2806,8 @@ bool Parser<FullParseHandler, Unit>::skipLazyInnerFunction(
   // lazy functions. Instead that info will be updated when we finish our
   // compilation.
   MOZ_ASSERT(fun->baseScript()->hasEnclosingScript());
+  MOZ_ASSERT_IF(fun->isClassConstructor(),
+                !fun->baseScript()->getMemberInitializers().valid);
 
   PropagateTransitiveParseFlags(funbox, pc_->sc());
 
@@ -7497,9 +7499,11 @@ bool GeneralParser<ParseHandler, Unit>::finishClassConstructor(
   // Fields cannot re-use the constructor obtained via JSOp::ClassConstructor or
   // JSOp::DerivedConstructor due to needing to emit calls to the field
   // initializers in the constructor. So, synthesize a new one.
-  size_t numMemberInitializers = classInitializedMembers.privateMethods +
-                                 classInitializedMembers.instanceFields;
-  if (classStmt.constructorBox == nullptr && numMemberInitializers) {
+  size_t numPrivateMethods = classInitializedMembers.privateMethods;
+  size_t numFields = classInitializedMembers.instanceFields;
+
+  if (classStmt.constructorBox == nullptr &&
+      numFields + numPrivateMethods > 0) {
     MOZ_ASSERT(!options().selfHostingMode);
     // Unconditionally create the scope here, because it's always the
     // constructor.
@@ -7551,11 +7555,7 @@ bool GeneralParser<ParseHandler, Unit>::finishClassConstructor(
     // finished parsing the class.
     ctorbox->setCtorToStringEnd(classEndOffset);
 
-    // Now that we have full set of initializers, update the constructor.
-    MemberInitializers initializers(numMemberInitializers);
-    ctorbox->setMemberInitializers(initializers);
-
-    if (numMemberInitializers) {
+    if (numFields + numPrivateMethods > 0) {
       // Field initialization need access to `this`.
       ctorbox->setCtorFunctionHasThisBinding();
     }
