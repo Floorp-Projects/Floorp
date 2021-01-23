b8f5c2c599428b13caefa4b2d1a0034b83e0249b
Ted Campbell <tcampbell@mozilla.com>
2021-01-23 14:59:51 +0000

    Bug 1688327 - Move memberInitializers to ScriptExtraStencil. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D102823

--- js/src/frontend/SharedContext.cpp
+++ js/src/frontend/SharedContext.cpp
@@ -416,8 +416,8 @@ void FunctionBox::copyScriptFields(ScriptStencil& script) {
 
   SharedContext::copyScriptFields(script);
 
-  if (memberInitializers_) {
-    script.setMemberInitializers(*memberInitializers_);
+  if (hasMemberInitializers()) {
+    script.setHasMemberInitializers();
   }
 
   isScriptFieldCopiedToStencil = true;
@@ -443,6 +443,10 @@ void FunctionBox::copyFunctionFields(ScriptStencil& script) {
 }
 
 void FunctionBox::copyFunctionExtraFields(ScriptStencilExtra& scriptExtra) {
+  if (hasMemberInitializers()) {
+    scriptExtra.setMemberInitializers(memberInitializers());
+  }
+
   scriptExtra.nargs = nargs_;
 }
 
@@ -459,9 +463,16 @@ void FunctionBox::copyUpdatedExtent() {
 }
 
 void FunctionBox::copyUpdatedMemberInitializers() {
-  ScriptStencil& script = functionStencil();
-  if (memberInitializers_) {
-    script.setMemberInitializers(*memberInitializers_);
+  if (hasMemberInitializers()) {
+    ScriptStencil& script = functionStencil();
+    script.setHasMemberInitializers();
+    if (hasFunctionExtraStencil()) {
+      ScriptStencilExtra& scriptExtra = functionExtraStencil();
+      scriptExtra.setMemberInitializers(memberInitializers());
+    } else {
+      // We are delazifying and the original PrivateScriptData has the member
+      // initializer information already. See: JSScript::fullyInitFromStencil.
+    }
   }
 }
 
