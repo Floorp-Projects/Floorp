ecf1986f48277194cde66ae92e289876fa3ae1c3
Lee Salzman <lsalzman@mozilla.com>
2021-01-24 19:51:18 +0000

    Bug 1688323 - refactor WR gradient shaders with gradient_shared. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D102813

--- gfx/wr/webrender/res/prim_shared.glsl
+++ gfx/wr/webrender/res/prim_shared.glsl
@@ -281,49 +281,4 @@ float do_clip() {
 #endif
 }
 
-#ifdef WR_FEATURE_DITHERING
-vec4 dither(vec4 color) {
-    const int matrix_mask = 7;
-
-    ivec2 pos = ivec2(gl_FragCoord.xy) & ivec2(matrix_mask);
-    float noise_normalized = (texelFetch(sDither, pos, 0).r * 255.0 + 0.5) / 64.0;
-    float noise = (noise_normalized - 0.5) / 256.0; // scale down to the unit length
-
-    return color + vec4(noise, noise, noise, 0);
-}
-#else
-vec4 dither(vec4 color) {
-    return color;
-}
-#endif //WR_FEATURE_DITHERING
-
-vec4 sample_gradient(HIGHP_FS_ADDRESS int address, float offset, float gradient_repeat) {
-    // Modulo the offset if the gradient repeats.
-    float x = offset - floor(offset) * gradient_repeat;
-
-    // Calculate the color entry index to use for this offset:
-    //     offsets < 0 use the first color entry, 0
-    //     offsets from [0, 1) use the color entries in the range of [1, N-1)
-    //     offsets >= 1 use the last color entry, N-1
-    //     so transform the range [0, 1) -> [1, N-1)
-
-    // TODO(gw): In the future we might consider making the size of the
-    // LUT vary based on number / distribution of stops in the gradient.
-    // Ensure we don't fetch outside the valid range of the LUT.
-    const float GRADIENT_ENTRIES = 128.0;
-    x = clamp(1.0 + x * GRADIENT_ENTRIES, 0.0, 1.0 + GRADIENT_ENTRIES);
-
-    // Calculate the texel to index into the gradient color entries:
-    //     floor(x) is the gradient color entry index
-    //     fract(x) is the linear filtering factor between start and end
-    float entry_index = floor(x);
-    float entry_fract = x - entry_index;
-
-    // Fetch the start and end color. There is a [start, end] color per entry.
-    vec4 texels[2] = fetch_from_gpu_cache_2(address + 2 * int(entry_index));
-
-    // Finally interpolate and apply dithering
-    return dither(mix(texels[0], texels[1], entry_fract));
-}
-
 #endif //WR_FRAGMENT_SHADER
