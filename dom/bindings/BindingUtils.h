6df9e9bfc7a1446fc94e18f04cf50ef4b143ead3
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-01-25 21:47:12 +0000

    Bug 1688335 - Remove .length and .name from namespaces r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D102830

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -731,19 +731,16 @@ struct NamedConstructor {
  * |name|, which must also be non-null.
  */
 // clang-format on
-void CreateInterfaceObjects(JSContext* cx, JS::Handle<JSObject*> global,
-                            JS::Handle<JSObject*> protoProto,
-                            const JSClass* protoClass,
-                            JS::Heap<JSObject*>* protoCache,
-                            JS::Handle<JSObject*> interfaceProto,
-                            const JSClass* constructorClass, unsigned ctorNargs,
-                            const NamedConstructor* namedConstructors,
-                            JS::Heap<JSObject*>* constructorCache,
-                            const NativeProperties* regularProperties,
-                            const NativeProperties* chromeOnlyProperties,
-                            const char* name, bool defineOnGlobal,
-                            const char* const* unscopableNames, bool isGlobal,
-                            const char* const* legacyWindowAliases);
+void CreateInterfaceObjects(
+    JSContext* cx, JS::Handle<JSObject*> global,
+    JS::Handle<JSObject*> protoProto, const JSClass* protoClass,
+    JS::Heap<JSObject*>* protoCache, JS::Handle<JSObject*> constructorProto,
+    const JSClass* constructorClass, unsigned ctorNargs,
+    const NamedConstructor* namedConstructors,
+    JS::Heap<JSObject*>* constructorCache, const NativeProperties* properties,
+    const NativeProperties* chromeOnlyProperties, const char* name,
+    bool defineOnGlobal, const char* const* unscopableNames, bool isGlobal,
+    const char* const* legacyWindowAliases, bool isNamespace);
 
 /**
  * Define the properties (regular and chrome-only) on obj.
