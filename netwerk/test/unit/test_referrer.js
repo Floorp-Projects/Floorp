6fa728bd89720dd40a0760bd41270d5350647386
Dimi Lee <dlee@mozilla.com>
2021-01-25 13:27:02 +0000

    Bug 1589074 - Fix network mochitest failures after changing default referrer policy to strict-origin-when-cross-origin. r=kershaw,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D88549

--- netwerk/test/unit/test_referrer.js
+++ netwerk/test/unit/test_referrer.js
@@ -54,7 +54,10 @@ function run_test() {
   prefs.setIntPref("network.http.sendRefererHeader", 0);
   Assert.equal(null, getTestReferrer(server_uri, referer_uri));
   prefs.setIntPref("network.http.sendRefererHeader", 2);
-  Assert.equal(getTestReferrer(server_uri, referer_uri), referer_uri);
+  Assert.equal(
+    getTestReferrer(server_uri, referer_uri),
+    "http://foo.example.com/"
+  );
 
   // test that https ref is not sent to http
   Assert.equal(null, getTestReferrer(server_uri_2, referer_uri_https));
