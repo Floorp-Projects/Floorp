3c5c1f4c89dc7e65cfa370a47eb9942be728432c
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-01-26 13:50:15 +0000

    Bug 1438979 - [devtools] Re-enable browser_webconsole_network_messages_expand.js. r=bomsy.
    The test is split in two to make it easier to read.
    The test was quite old and still had references to the `Params` panel, which was
    renamed to `Request`.
    The test was also failing when checking the stacktrace, and this was due to an
    issue from the webconsole messsage reducer where we would completely replace
    the `networkMessagesUpdateById` on NETWORK_MESSAGES_UPDATE, and that would lead
    to the stracktrace being removed.
    In the Stacktrace component, we prevent fetching the stacktrace if it's already
    available.
    
    Differential Revision: https://phabricator.services.mozilla.com/D56884

--- devtools/client/webconsole/test/browser/browser_webconsole_network_messages_expand_before_updates.js
+++ devtools/client/webconsole/test/browser/browser_webconsole_network_messages_expand_before_updates.js
@@ -8,18 +8,48 @@ const TEST_PATH =
   "https://example.com/browser/devtools/client/webconsole/test/browser/";
 const TEST_URI = TEST_PATH + TEST_FILE;
 
-requestLongerTimeout(2);
-
-registerCleanupFunction(async function() {
-  await new Promise(resolve => {
-    Services.clearData.deleteData(Ci.nsIClearDataService.CLEAR_ALL, value =>
-      resolve()
-    );
-  });
-});
+requestLongerTimeout(4);
 
 pushPref("devtools.webconsole.filter.net", false);
 pushPref("devtools.webconsole.filter.netxhr", true);
+
+const tabs = [
+  {
+    id: "headers",
+    testEmpty: testEmptyHeaders,
+    testContent: testHeaders,
+  },
+  {
+    id: "cookies",
+    testEmpty: testEmptyCookies,
+    testContent: testCookies,
+  },
+  {
+    id: "request",
+    testEmpty: testEmptyRequest,
+    testContent: testRequest,
+  },
+  {
+    id: "response",
+    testEmpty: testEmptyResponse,
+    testContent: testResponse,
+  },
+  {
+    id: "timings",
+    testEmpty: testEmptyTimings,
+    testContent: testTimings,
+  },
+  {
+    id: "stack-trace",
+    testEmpty: testEmptyStackTrace,
+    testContent: testStackTrace,
+  },
+  {
+    id: "security",
+    testEmpty: testEmptySecurity,
+    testContent: testSecurity,
+  },
+];
 
 /**
  * Main test for checking HTTP logs in the Console panel.
@@ -30,35 +60,66 @@ add_task(async function task() {
   const currentTab = gBrowser.selectedTab;
   const target = await TargetFactory.forTab(currentTab);
 
-  // Execute XHR and expand it after all network
-  // update events are received. Consequently,
-  // check out content of all (HTTP details) tabs.
+  // Test proper UI update when request is opened.
+  // For every tab (with HTTP details):
+  // 1. Execute long-time request
+  // 2. Expand the net log before the request finishes (set default tab)
+  // 3. Check the default tab empty content
+  // 4. Wait till the request finishes
+  // 5. Check content of all tabs
+  for (const tab of tabs) {
+    info(`Test "${tab.id}" panel`);
+    await openRequestBeforeUpdates(target, hud, tab);
+  }
+});
+
+async function openRequestBeforeUpdates(target, hud, tab) {
   const toolbox = gDevTools.getToolbox(target);
+
+  await clearOutput(hud);
 
   const xhrUrl = TEST_PATH + "sjs_slow-response-test-server.sjs";
   const onMessage = waitForMessage(hud, xhrUrl);
-  const onRequestUpdates = waitForRequestUpdates(hud);
-  const onPayloadReady = waitForPayloadReady(hud);
 
   // Fire an XHR POST request.
   SpecialPowers.spawn(gBrowser.selectedBrowser, [], function() {
     content.wrappedJSObject.testXhrPostSlowResponse();
   });
-
+  info(`Wait for ${xhrUrl} message`);
   const { node: messageNode } = await onMessage;
   ok(messageNode, "Network message found.");
 
-  await onRequestUpdates;
+  // Set the default panel.
+  const state = hud.ui.wrapper.getStore().getState();
+  state.ui.networkMessageActiveTabId = tab.id;
 
   // Expand network log
   await expandXhrMessage(messageNode);
 
-  const toggleButtonNode = messageNode.querySelector(".sidebar-toggle");
-  ok(!toggleButtonNode, "Sidebar toggle button shouldn't be shown");
+  // Except the security tab. It isn't available till the
+  // "securityInfo" packet type is received, so doesn't
+  // fit this part of the test.
+  if (tab.id != "security") {
+    // Make sure the current tab is the expected one.
+    const currentTab = messageNode.querySelector(`#${tab.id}-tab`);
+    is(
+      currentTab.getAttribute("aria-selected"),
+      "true",
+      "The correct tab is selected"
+    );
 
-  await onPayloadReady;
+    if (tab.testEmpty) {
+      info("Test that the tab is empty");
+      tab.testEmpty(messageNode);
+    }
+  }
+
+  info("Test content of the default tab");
+  await tab.testContent(messageNode);
+
+  info("Test all tabs in the network log");
   await testNetworkMessage(toolbox, messageNode);
-});
+}
 
 // Panel testing helpers
 
@@ -75,13 +136,19 @@ async function testNetworkMessage(toolbox, messageNode) {
 }
 
 // Status Info
-
-function testStatusInfo(messageNode) {
-  const statusInfo = messageNode.querySelector(".status-info");
+async function testStatusInfo(messageNode) {
+  const statusInfo = await waitFor(() =>
+    messageNode.querySelector(".status-info")
+  );
   ok(statusInfo, "Status info is not empty");
 }
 
 // Headers
+function testEmptyHeaders(messageNode) {
+  const emptyNotice = messageNode.querySelector("#headers-panel .empty-notice");
+  ok(emptyNotice, "Headers tab is empty");
+}
+
 async function testHeaders(messageNode) {
   const headersTab = messageNode.querySelector("#headers-tab");
   ok(headersTab, "Headers tab is available");
@@ -95,6 +162,11 @@ async function testHeaders(messageNode) {
 }
 
 // Cookies
+function testEmptyCookies(messageNode) {
+  const emptyNotice = messageNode.querySelector("#cookies-panel .empty-notice");
+  ok(emptyNotice, "Cookies tab is empty");
+}
+
 async function testCookies(messageNode) {
   const cookiesTab = messageNode.querySelector("#cookies-tab");
   ok(cookiesTab, "Cookies tab is available");
@@ -108,6 +180,11 @@ async function testCookies(messageNode) {
 }
 
 // Request
+function testEmptyRequest(messageNode) {
+  const emptyNotice = messageNode.querySelector("#request-panel .empty-notice");
+  ok(emptyNotice, "Request tab is empty");
+}
+
 async function testRequest(messageNode) {
   const requestTab = messageNode.querySelector("#request-tab");
   ok(requestTab, "Request tab is available");
@@ -128,6 +205,15 @@ async function testRequest(messageNode) {
 }
 
 // Response
+function testEmptyResponse(messageNode) {
+  const panel = messageNode.querySelector("#response-panel .tab-panel");
+  is(
+    panel.textContent,
+    "No response data available for this request",
+    "Cookies tab is empty"
+  );
+}
+
 async function testResponse(messageNode) {
   const responseTab = messageNode.querySelector("#response-tab");
   ok(responseTab, "Response tab is available");
@@ -136,6 +222,13 @@ async function testResponse(messageNode) {
   // is delayed, so again wait for a little while.
   responseTab.click();
   const responsePanel = messageNode.querySelector("#response-panel");
+  const responsePayloadHeader = await waitFor(() =>
+    responsePanel.querySelector("#responsePayload-header")
+  );
+  // Expand the header if it wasn't yet.
+  if (responsePayloadHeader.getAttribute("aria-expanded") === "false") {
+    responsePayloadHeader.click();
+  }
   await waitForSourceEditor(responsePanel);
   const responseContent = messageNode.querySelector(
     "#response-panel .editor-row-container .CodeMirror"
@@ -145,6 +238,11 @@ async function testResponse(messageNode) {
 }
 
 // Timings
+function testEmptyTimings(messageNode) {
+  const panel = messageNode.querySelector("#timings-panel .tab-panel");
+  is(panel.textContent, "No timings for this request", "Timings tab is empty");
+}
+
 async function testTimings(messageNode) {
   const timingsTab = messageNode.querySelector("#timings-tab");
   ok(timingsTab, "Timings tab is available");
@@ -162,6 +260,11 @@ async function testTimings(messageNode) {
 }
 
 // Stack Trace
+function testEmptyStackTrace(messageNode) {
+  const panel = messageNode.querySelector("#stack-trace-panel .tab-panel");
+  is(panel.textContent, "", "StackTrace tab is empty");
+}
+
 async function testStackTrace(messageNode) {
   const stackTraceTab = messageNode.querySelector("#stack-trace-tab");
   ok(stackTraceTab, "StackTrace tab is available");
@@ -175,8 +278,15 @@ async function testStackTrace(messageNode) {
 }
 
 // Security
+function testEmptySecurity(messageNode) {
+  const panel = messageNode.querySelector("#security-panel .tab-panel");
+  is(panel.textContent, "", "Security tab is empty");
+}
+
 async function testSecurity(messageNode) {
-  const securityTab = messageNode.querySelector("#security-tab");
+  const securityTab = await waitFor(() =>
+    messageNode.querySelector("#security-tab")
+  );
   ok(securityTab, "Security tab is available");
 
   // Select Security tab and check the content.
@@ -187,20 +297,10 @@ async function testSecurity(messageNode) {
   );
 }
 
-// Waiting helpers
-
-async function waitForPayloadReady(hud) {
-  return hud.ui.once("network-request-payload-ready");
-}
-
 async function waitForSourceEditor(panel) {
   return waitUntil(() => {
     return !!panel.querySelector(".CodeMirror");
   });
-}
-
-async function waitForRequestUpdates(hud) {
-  return hud.ui.once("network-messages-updated");
 }
 
 function expandXhrMessage(node) {
