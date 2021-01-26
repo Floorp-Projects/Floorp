73b88f69411e9856e04ecb1615f63b3dc87102a5
Bogdan Tara <btara@mozilla.com>
2021-01-26 23:38:06 +0200

    Backed out changeset 924a3c191cdb (bug 1688784) for rust related windows bustage CLOSED TREE

--- third_party/rust/mp4parse_capi/tests/test_encryption.rs
+++ third_party/rust/mp4parse_capi/tests/test_encryption.rs
@@ -12,6 +12,7 @@ extern "C" fn buf_read(buf: *mut u8, size: usize, userdata: *mut std::os::raw::c
 }
 
 #[test]
+#[allow(clippy::cognitive_complexity)] // TODO: Consider simplifying this
 fn parse_cenc() {
     let mut file = std::fs::File::open("tests/short-cenc.mp4").expect("Unknown file");
     let io = Mp4parseIo {
