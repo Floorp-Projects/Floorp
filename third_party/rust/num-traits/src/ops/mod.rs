4535093154dc48231a81c870a4d8e31373122052
Bogdan Tara <btara@mozilla.com>
2021-01-26 23:38:06 +0200

    Backed out changeset 924a3c191cdb (bug 1688784) for rust related windows bustage CLOSED TREE

--- third_party/rust/num-traits/src/ops/mod.rs
+++ third_party/rust/num-traits/src/ops/mod.rs
@@ -1,6 +1,5 @@
 pub mod checked;
 pub mod inv;
 pub mod mul_add;
-pub mod overflowing;
 pub mod saturating;
 pub mod wrapping;
--- third_party/rust/num-traits/src/sign.rs
+++ third_party/rust/num-traits/src/sign.rs
@@ -213,12 +213,13 @@ fn unsigned_wrapping_is_unsigned() {
     fn require_unsigned<T: Unsigned>(_: &T) {}
     require_unsigned(&Wrapping(42_u32));
 }
-
+/*
 // Commenting this out since it doesn't compile on Rust 1.8,
 // because on this version Wrapping doesn't implement Neg and therefore can't
 // implement Signed.
-// #[test]
-// fn signed_wrapping_is_signed() {
-//     fn require_signed<T: Signed>(_: &T) {}
-//     require_signed(&Wrapping(-42));
-// }
+#[test]
+fn signed_wrapping_is_signed() {
+    fn require_signed<T: Signed>(_: &T) {}
+    require_signed(&Wrapping(-42));
+}
+*/
