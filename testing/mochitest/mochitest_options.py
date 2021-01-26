4cb914c8673db92994a48f33109662660c834f60
Joel Maher <jmaher@mozilla.com>
2021-01-26 14:41:46 +0000

    Bug 1682358 - add --run-failures support to mochitest. r=bhearsum
    add --run-failures support to mochitest
    
    Differential Revision: https://phabricator.services.mozilla.com/D102805

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -886,6 +886,15 @@ class MochitestArguments(ArgumentContainer):
                 "MOZ_UPLOAD_DIR environment variable.",
             },
         ],
+        [
+            ["--run-failures"],
+            {
+                "action": "store",
+                "dest": "runFailures",
+                "default": "",
+                "help": "Run fail-if/skip-if tests that match a keyword given.",
+            },
+        ],
     ]
 
     defaults = {
