035f86c611d33e31bc04295cb03d740d1fc8eb59
Emilio Cobos Álvarez <emilio@crisal.io>
2021-01-26 22:56:03 +0000

    Bug 787521 - Improve tests for chrome-only media queries. r=dholbert
    So as to test that they are in fact not parseable by content.
    
    Keep a centralized list of these rather than duplicating tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102966

---
+++ layout/style/test/chrome/chrome-only-media-queries.js
@@ -0,0 +1,37 @@
+const CHROME_ONLY_TOGGLES = [
+  "-moz-is-glyph",
+  "-moz-print-preview",
+  "-moz-non-native-content-theme",
+  "-moz-scrollbar-start-backward",
+  "-moz-scrollbar-start-forward",
+  "-moz-scrollbar-end-backward",
+  "-moz-scrollbar-end-forward",
+  "-moz-scrollbar-thumb-proportional",
+  "-moz-overlay-scrollbars",
+  "-moz-windows-classic",
+  "-moz-windows-compositor",
+  "-moz-windows-default-theme",
+  "-moz-mac-graphite-theme",
+  "-moz-mac-big-sur-theme",
+  "-moz-menubar-drag",
+  "-moz-windows-accent-color-in-titlebar",
+  "-moz-windows-compositor",
+  "-moz-windows-classic",
+  "-moz-windows-glass",
+  "-moz-swipe-animation-enabled",
+  "-moz-gtk-csd-available",
+  "-moz-gtk-csd-hide-titlebar-by-default",
+  "-moz-gtk-csd-transparent-background",
+  "-moz-gtk-csd-minimize-button",
+  "-moz-gtk-csd-maximize-button",
+  "-moz-gtk-csd-close-button",
+  "-moz-gtk-csd-reversed-placement",
+];
+
+// Non-parseable queries can be tested directly in
+// `test_chrome_only_media_queries.html`.
+const CHROME_ONLY_QUERIES = [
+  "(-moz-os-version: windows-win7)",
+  "(-moz-os-version: windows-win8)",
+  "(-moz-os-version: windows-win10)",
+];
