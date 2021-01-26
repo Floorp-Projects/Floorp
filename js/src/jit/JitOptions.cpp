43ad27f60d276ee9d4cbdec180cdadcff383b8ee
Jan de Mooij <jdemooij@mozilla.com>
2021-01-26 08:49:24 +0000

    Bug 1686890 - Add a JitOption to force megamorphic ICs. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D101345

--- js/src/jit/JitOptions.cpp
+++ js/src/jit/JitOptions.cpp
@@ -145,6 +145,9 @@ DefaultJitOptions::DefaultJitOptions() {
   // Whether Warp should use ICs instead of transpiling Baseline CacheIR.
   SET_DEFAULT(forceInlineCaches, false);
 
+  // Whether all ICs should be initialized as megamorphic ICs.
+  SET_DEFAULT(forceMegamorphicICs, false);
+
   // Toggles whether large scripts are rejected.
   SET_DEFAULT(limitScriptSize, true);
 
