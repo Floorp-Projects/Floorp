b1a1a57cd62d3ce98107ad8f418a2538951772f3
Emilio Cobos Álvarez <emilio@crisal.io>
2021-01-28 14:21:57 +0000

    Bug 1689342 - Remove focus-visible and preload from the experimental features section. r=preferences-reviewers,Gijs
    These are enabled by default and shipped in 85 and 84 respectively.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103316

--- browser/base/content/test/static/browser_misused_characters_in_strings.js
+++ browser/base/content/test/static/browser_misused_characters_in_strings.js
@@ -116,13 +116,6 @@ let gExceptionsList = [
     key: "tos",
     type: "double-quote",
   },
-  // This string contains HTML markup describing `<link rel="preload">` and therefore
-  // is meant to contain actual double quotes.
-  {
-    file: "features.ftl",
-    key: "experimental-features-web-api-link-preload-description",
-    type: "double-quote",
-  },
 ];
 
 /**
