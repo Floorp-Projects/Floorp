42a05726d1d6700154997968793fb2b1a5f787ce
Emilio Cobos Álvarez <emilio@crisal.io>
2021-01-28 14:17:01 +0000

    Bug 1689286 - Make the number input spinners look nicer with padding. r=spohl
    If you add padding to the input on the inline-end, like some of the
    examples in bug 1674431, then the darker border looks off.
    
    Without it, the number input still looks nice, so I think we should do
    this.
    
    We might want to introduce the padding by default (see bug 1689286).
    
    Differential Revision: https://phabricator.services.mozilla.com/D103270

--- widget/nsNativeBasicTheme.h
+++ widget/nsNativeBasicTheme.h
@@ -113,7 +113,6 @@ static const CSSCoord kMinimumSpinnerButtonHeight = 9.0f;
 static const CSSCoord kButtonBorderWidth = 1.0f;
 static const CSSCoord kMenulistBorderWidth = 1.0f;
 static const CSSCoord kTextFieldBorderWidth = 1.0f;
-static const CSSCoord kSpinnerBorderWidth = 1.0f;
 static const CSSCoord kRangeHeight = 6.0f;
 static const CSSCoord kProgressbarHeight = 6.0f;
 static const CSSCoord kMeterHeight = 12.0f;
