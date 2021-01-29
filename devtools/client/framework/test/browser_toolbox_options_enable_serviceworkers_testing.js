3161c0a403540476d9b28b84638f8ef179fd1302
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-01-29 15:00:16 +0000

    Bug 1667526 - [devtools] Refactor and fix intermittent on browser_toolbox_options_enable_serviceworkers_testing.js. r=ladybenko.
    We migrate the test to add_task and async functions and
    properly set preferences at the beginning of the test.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103181

--- devtools/client/framework/test/browser_toolbox_options_enable_serviceworkers_testing.js
+++ devtools/client/framework/test/browser_toolbox_options_enable_serviceworkers_testing.js
@@ -6,38 +6,45 @@
 
 const TEST_URI =
   URL_ROOT + "browser_toolbox_options_enable_serviceworkers_testing.html";
-
 const ELEMENT_ID = "devtools-enable-serviceWorkersTesting";
 
-var toolbox;
+add_task(async function() {
+  await pushPref("dom.serviceWorkers.exemptFromPerDomainMax", true);
+  await pushPref("dom.serviceWorkers.enabled", true);
+  await pushPref("dom.serviceWorkers.testing.enabled", false);
+  // Force the test to start without service worker testing enabled
+  await pushPref("devtools.serviceWorkers.testing.enabled", false);
 
-function test() {
-  // Note: Pref dom.serviceWorkers.testing.enabled is false since we are testing
-  // the same capabilities are enabled with the devtool pref.
-  SpecialPowers.pushPrefEnv(
-    {
-      set: [
-        ["dom.serviceWorkers.exemptFromPerDomainMax", true],
-        ["dom.serviceWorkers.enabled", true],
-        ["dom.serviceWorkers.testing.enabled", false],
-      ],
-    },
-    init
-  );
-}
+  const tab = await addTab(TEST_URI);
+  const toolbox = await openToolboxForTab(tab, "options");
 
-function init() {
-  addTab(TEST_URI).then(async tab => {
-    const target = await TargetFactory.forTab(tab);
-    gDevTools.showToolbox(target).then(testSelectTool);
-  });
-}
+  let data = await register();
+  is(data.success, false, "Register should fail with security error");
 
-function testSelectTool(aToolbox) {
-  toolbox = aToolbox;
-  toolbox.once("options-selected", start);
-  toolbox.selectTool("options");
-}
+  const panel = toolbox.getCurrentPanel();
+  const cbx = panel.panelDoc.getElementById(ELEMENT_ID);
+  is(cbx.checked, false, "The checkbox shouldn't be checked");
+
+  info(`Checking checkbox to enable service workers testing`);
+  cbx.scrollIntoView();
+  cbx.click();
+
+  await refreshTab();
+
+  data = await register();
+  is(data.success, true, "Register should success");
+
+  await unregister();
+  data = await registerAndUnregisterInFrame();
+  is(data.success, true, "Register should success");
+
+  info("Workers should be turned back off when we closes the toolbox");
+  await toolbox.destroy();
+
+  await refreshTab();
+  data = await register();
+  is(data.success, false, "Register should fail with security error");
+});
 
 function sendMessage(name) {
   return SpecialPowers.spawn(gBrowser.selectedBrowser, [name], nameChild => {
@@ -62,67 +69,4 @@ function unregister(swr) {
 
 function registerAndUnregisterInFrame() {
   return sendMessage("devtools:sw-test:iframe:register-and-unregister");
-}
-
-function testRegisterFails(data) {
-  is(data.success, false, "Register should fail with security error");
-  return promise.resolve();
-}
-
-function toggleServiceWorkersTestingCheckbox() {
-  const panel = toolbox.getCurrentPanel();
-  const cbx = panel.panelDoc.getElementById(ELEMENT_ID);
-
-  cbx.scrollIntoView();
-
-  if (cbx.checked) {
-    info("Clearing checkbox to disable service workers testing");
-  } else {
-    info("Checking checkbox to enable service workers testing");
-  }
-
-  cbx.click();
-
-  return promise.resolve();
-}
-
-function reload() {
-  const promise = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
-
-  SpecialPowers.spawn(gBrowser.selectedBrowser, [], () => {
-    content.location.reload(false);
-  });
-  return promise;
-}
-
-function testRegisterSuccesses(data) {
-  is(data.success, true, "Register should success");
-  return promise.resolve();
-}
-
-function start() {
-  register()
-    .then(testRegisterFails)
-    .then(toggleServiceWorkersTestingCheckbox)
-    .then(reload)
-    .then(register)
-    .then(testRegisterSuccesses)
-    .then(unregister)
-    .then(registerAndUnregisterInFrame)
-    .then(testRegisterSuccesses)
-    // Workers should be turned back off when we closes the toolbox
-    .then(toolbox.destroy.bind(toolbox))
-    .then(reload)
-    .then(register)
-    .then(testRegisterFails)
-    .catch(function(e) {
-      ok(false, "Some test failed with error " + e);
-    })
-    .then(finishUp);
-}
-
-function finishUp() {
-  gBrowser.removeCurrentTab();
-  toolbox = null;
-  finish();
 }
