abdbbf2e8eeb4e16cff0530ccc54839fb15adad3
Jonathan Almeida <jalmeida@mozilla.com>
2021-01-29 23:19:06 +0400

    [components] Re-land: "Closes https://github.com/mozilla-mobile/android-components/issues/8693: Integrate GeckoWebExecutor#FETCH_FLAG_PRIVATE."
    This re-lands commit bc1719598b0980b12bf7717d35ee31d15eb05365.

--- mobile/android/android-components/components/lib/fetch-httpurlconnection/src/main/java/mozilla/components/lib/fetch/httpurlconnection/HttpURLConnectionClient.kt
+++ mobile/android/android-components/components/lib/fetch-httpurlconnection/src/main/java/mozilla/components/lib/fetch/httpurlconnection/HttpURLConnectionClient.kt
@@ -32,6 +32,9 @@ class HttpURLConnectionClient : Client() {
 
     @Throws(IOException::class)
     override fun fetch(request: Request): Response {
+        if (request.private) {
+            throw IllegalArgumentException("Client doesn't support private request")
+        }
         if (request.isDataUri()) {
             return fetchDataUri(request)
         }
