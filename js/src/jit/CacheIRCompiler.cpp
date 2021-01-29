854db17dc5e074fefedd3f0344907d8cc48b4d61
David Major <dmajor@mozilla.com>
2021-01-29 16:21:07 +0000

    Bug 1689641 - Work around a potential clang-12 miscompilation in CacheRegisterAllocator::allocateValueRegister r=jandem
    Some time ago a change landed in clang trunk that resulted in `reg1` and `reg2` not getting saved to the right places on the (caller's, since this was inlined) stack. At the time I ran a bisect to find the specific change in LLVM and talked to the developer, but we couldn't find anything wrong with that change itself. It is likely that the change only exposed pre-existing problems, which I never managed to track down. But preventing inline on `allocateValueRegister` has consistently prevented the problem.
    
    We only need this on win32 under clang-12, so I'm open to further scoping down the ifdefs if requested.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103488

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -608,6 +608,10 @@ void CacheRegisterAllocator::allocateFixedValueRegister(MacroAssembler& masm,
 #endif
 }
 
+#ifdef JS_NUNBOX32
+// Possible miscompilation in clang-12 (bug 1689641)
+MOZ_NEVER_INLINE
+#endif
 ValueOperand CacheRegisterAllocator::allocateValueRegister(
     MacroAssembler& masm) {
 #ifdef JS_NUNBOX32
