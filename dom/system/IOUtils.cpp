ff64b4c7e9efc1572b8c04f02ea03873acc3d1a4
Barret Rennie <barret@brennie.ca>
2021-01-30 05:47:25 +0000

    Bug 1689740 - Check for nsIFile::GetParent() returning null in IOUtils r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D103520

--- dom/system/IOUtils.cpp
+++ dom/system/IOUtils.cpp
@@ -1130,6 +1130,9 @@ Result<Ok, IOUtils::IOError> IOUtils::CopyOrMoveSync(CopyOrMoveFn aMethod,
   MOZ_TRY(aDest->GetLeafName(destName));
   MOZ_TRY(aDest->GetParent(getter_AddRefs(destDir)));
 
+  // We know `destName` is a file and therefore must have a parent directory.
+  MOZ_RELEASE_ASSERT(destDir);
+
   // NB: if destDir doesn't exist, then |CopyToFollowingLinks| or
   // |MoveToFollowingLinks| will create it.
   rv = (aSource->*aMethod)(destDir, destName);
@@ -1184,14 +1187,33 @@ Result<Ok, IOUtils::IOError> IOUtils::MakeDirectorySync(nsIFile* aFile,
   if (!aCreateAncestors) {
     nsCOMPtr<nsIFile> parent;
     MOZ_TRY(aFile->GetParent(getter_AddRefs(parent)));
-    bool parentExists = false;
-    MOZ_TRY(parent->Exists(&parentExists));
-    if (!parentExists) {
-      return Err(IOError(NS_ERROR_FILE_NOT_FOUND)
-                     .WithMessage("Could not create directory at %s because "
-                                  "the path has missing "
-                                  "ancestor components",
-                                  aFile->HumanReadablePath().get()));
+    if (parent) {
+      bool parentExists = false;
+      MOZ_TRY(parent->Exists(&parentExists));
+      if (!parentExists) {
+        return Err(IOError(NS_ERROR_FILE_NOT_FOUND)
+                       .WithMessage("Could not create directory at %s because "
+                                    "the path has missing "
+                                    "ancestor components",
+                                    aFile->HumanReadablePath().get()));
+      }
+    } else {
+      // If we don't have a parent directory, which means this was called with a
+      // root directory. If the directory doesn't already exist (e.g., asking
+      // for a drive on Windows that does not exist), we will not be able to
+      // create it.
+      //
+      // Calling `nsLocalFile::Create()` on Windows can fail with
+      // `NS_ERROR_ACCESS_DENIED` trying to create a root directory, but we
+      // would rather the call succeed, so return early if the directory exists.
+      //
+      // Otherwise, we fall through to `nsiFile::Create()` and let it fail there
+      // instead.
+      bool exists = false;
+      MOZ_TRY(aFile->Exists(&exists));
+      if (exists) {
+        return Ok();
+      }
     }
   }
 
