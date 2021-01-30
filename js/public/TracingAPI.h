18192979e2a67e4be06c724b00e3cfdbb6de4299
Jon Coppeard <jcoppeard@mozilla.com>
2021-01-30 11:21:28 +0000

    Bug 1689394 - Make updating tracing context information unconditional on the tracer kind r=sfink
    One thing we have to do when tracaing is udpate context information (e.g. edge name) if the tracer is a kind that requires it. It's simpler and more efficient to give all tracers this context and perform an unconditional write to the stack.
    
    Mostly we can get away without saving/restoring context information too. This adds AutoClearTracingContext for the one place we need to do this because of nested use of the same tracer while tracing something else.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103500

--- js/public/TracingAPI.h
+++ js/public/TracingAPI.h
@@ -111,7 +111,6 @@ struct TraceOptions {
 
 class AutoTracingName;
 class AutoTracingIndex;
-class AutoTracingCallback;
 
 // Optional context information that can be used to construct human readable
 // descriptions of what is being traced.
@@ -185,8 +184,6 @@ class JS_PUBLIC_API JSTracer {
   // Return the runtime set on the tracer.
   JSRuntime* runtime() const { return runtime_; }
 
-  JS::TracingContext* maybeContext() { return maybeContext_; }
-
   JS::TracerKind kind() const { return kind_; }
   bool isMarkingTracer() const { return kind_ == JS::TracerKind::Marking; }
   bool isTenuringTracer() const { return kind_ == JS::TracerKind::Tenuring; }
@@ -206,6 +203,8 @@ class JS_PUBLIC_API JSTracer {
     return options_.idAction == JS::IdTraceAction::CanSkip;
   }
 
+  JS::TracingContext& context() { return context_; }
+
   // Get the current GC number. Only call this method if |isMarkingTracer()|
   // is true.
   uint32_t gcNumberForMarking() const;
@@ -215,13 +214,11 @@ class JS_PUBLIC_API JSTracer {
            JS::TraceOptions options = JS::TraceOptions())
       : runtime_(rt), kind_(kind), options_(options) {}
 
-  void setContext(JS::TracingContext* tcx) { maybeContext_ = tcx; }
-
  private:
   JSRuntime* const runtime_;
-  JS::TracingContext* maybeContext_ = nullptr;
   const JS::TracerKind kind_;
   const JS::TraceOptions options_;
+  JS::TracingContext context_;
 };
 
 namespace js {
@@ -268,12 +265,9 @@ class JS_PUBLIC_API CallbackTracer : public js::GenericTracer {
                  JS::TraceOptions options = JS::TraceOptions())
       : GenericTracer(rt, kind, options) {
     MOZ_ASSERT(isCallbackTracer());
-    setContext(&context_);
   }
   CallbackTracer(JSContext* cx, JS::TracerKind kind = JS::TracerKind::Callback,
                  JS::TraceOptions options = JS::TraceOptions());
-
-  TracingContext& context() { return context_; }
 
   // Override this method to receive notification when a node in the GC
   // heap graph is visited.
@@ -325,78 +319,72 @@ class JS_PUBLIC_API CallbackTracer : public js::GenericTracer {
     onChild(JS::GCCellPtr(shared, JS::TraceKind::RegExpShared));
     return shared;
   }
-
-  TracingContext context_;
 };
 
 // Set the name portion of the tracer's context for the current edge.
 class MOZ_RAII AutoTracingName {
-  TracingContext* tcx_ = nullptr;
-  const char* prior_ = nullptr;
+  JSTracer* trc_;
 
  public:
-  AutoTracingName(JSTracer* trc, const char* name) {
+  AutoTracingName(JSTracer* trc, const char* name) : trc_(trc) {
     MOZ_ASSERT(name);
-    if (TracingContext* tcx = trc->maybeContext()) {
-      tcx_ = tcx;
-      prior_ = tcx_->name_;
-      tcx_->name_ = name;
-    }
+    MOZ_ASSERT(!trc_->context().name_);
+    trc_->context().name_ = name;
   }
   ~AutoTracingName() {
-    if (tcx_) {
-      MOZ_ASSERT(tcx_->name_);
-      tcx_->name_ = prior_;
-    }
+    MOZ_ASSERT(trc_->context().name_);
+    trc_->context().name_ = nullptr;
   }
 };
 
 // Set the index portion of the tracer's context for the current range.
 class MOZ_RAII AutoTracingIndex {
-  TracingContext* tcx_ = nullptr;
+  JSTracer* trc_;
 
  public:
-  explicit AutoTracingIndex(JSTracer* trc, size_t initial = 0) {
-    if (TracingContext* tcx = trc->maybeContext()) {
-      tcx_ = tcx;
-      MOZ_ASSERT(tcx_->index_ == TracingContext::InvalidIndex);
-      tcx_->index_ = initial;
-    }
+  explicit AutoTracingIndex(JSTracer* trc, size_t initial = 0) : trc_(trc) {
+    MOZ_ASSERT(trc_->context().index_ == TracingContext::InvalidIndex);
+    trc_->context().index_ = initial;
   }
   ~AutoTracingIndex() {
-    if (tcx_) {
-      MOZ_ASSERT(tcx_->index_ != TracingContext::InvalidIndex);
-      tcx_->index_ = TracingContext::InvalidIndex;
-    }
+    MOZ_ASSERT(trc_->context().index_ != TracingContext::InvalidIndex);
+    trc_->context().index_ = TracingContext::InvalidIndex;
   }
 
   void operator++() {
-    if (tcx_) {
-      MOZ_ASSERT(tcx_->index_ != TracingContext::InvalidIndex);
-      ++tcx_->index_;
-    }
+    MOZ_ASSERT(trc_->context().index_ != TracingContext::InvalidIndex);
+    ++trc_->context().index_;
   }
 };
 
 // Set a context callback for the trace callback to use, if it needs a detailed
 // edge description.
 class MOZ_RAII AutoTracingDetails {
-  TracingContext* tcx_ = nullptr;
+  JSTracer* trc_;
 
  public:
-  AutoTracingDetails(JSTracer* trc, TracingContext::Functor& func) {
-    if (TracingContext* tcx = trc->maybeContext()) {
-      tcx_ = tcx;
-      MOZ_ASSERT(tcx_->functor_ == nullptr);
-      tcx_->functor_ = &func;
-    }
+  AutoTracingDetails(JSTracer* trc, TracingContext::Functor& func) : trc_(trc) {
+    MOZ_ASSERT(trc_->context().functor_ == nullptr);
+    trc_->context().functor_ = &func;
   }
   ~AutoTracingDetails() {
-    if (tcx_) {
-      MOZ_ASSERT(tcx_->functor_);
-      tcx_->functor_ = nullptr;
-    }
+    MOZ_ASSERT(trc_->context().functor_);
+    trc_->context().functor_ = nullptr;
   }
+};
+
+// Save and clear tracing context when performing nested tracing.
+class MOZ_RAII AutoClearTracingContext {
+  JSTracer* trc_;
+  TracingContext prev_;
+
+ public:
+  explicit AutoClearTracingContext(JSTracer* trc)
+      : trc_(trc), prev_(trc->context()) {
+    trc_->context() = TracingContext();
+  }
+
+  ~AutoClearTracingContext() { trc_->context() = prev_; }
 };
 
 }  // namespace JS
