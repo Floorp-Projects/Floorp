327a049fca7eb7d49564c532355b255d9bb8c5e5
Lee Salzman <lsalzman@mozilla.com>
2021-02-02 22:18:21 +0000

    Bug 1674524 - implement KHR_blend_equation_advanced in SWGL. r=bradwerth
    This patch has a few moving parts. We have to first tell WR that when it
    detects the extension that it is actually allowed to use it. We have to make
    the glsl-to-cxx translator eat the blend_supports_all_equations layout
    qualifier. We have to enable generation of advanced-blend-equation variants
    in the SWGL build setup. Then we report the actual extension inside SWGL.
    Finally, we actually add all the necessary blend equation enums, hash them
    down to a blend key, and implement all the blend modes therein.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103804

--- gfx/wr/swgl/src/swgl_fns.rs
+++ gfx/wr/swgl/src/swgl_fns.rs
@@ -2287,7 +2287,7 @@ impl Gl for Context {
 
     // GL_KHR_blend_equation_advanced
     fn blend_barrier_khr(&self) {
-        panic!();
+        // No barrier required, so nothing to do
     }
 
     // GL_CHROMIUM_copy_texture
