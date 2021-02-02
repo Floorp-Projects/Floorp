e325efe5aa267fc212608d01231946d6910dbc67
Alexis Beingessner <a.beingessner@gmail.com>
2021-02-02 16:03:14 +0000

    Bug 1674835 - remove supression for fixed issue. r=decoder
    Differential Revision: https://phabricator.services.mozilla.com/D103046

--- mozglue/build/TsanOptions.cpp
+++ mozglue/build/TsanOptions.cpp
@@ -262,10 +262,6 @@ extern "C" const char* __tsan_default_suppressions() {
          // Bug 1674776
          "race:DocumentTimeline::GetCurrentTimeAsDuration\n"
 
-         // Bug 1674835
-         "race:nsHttpTransaction::ReadSegments\n"
-         "race:nsHttpTransaction::SecurityInfo\n"
-
          // Bug 1680285
          "race:style::traversal::note_children\n"
          "race:style::matching::MatchMethods::apply_selector_flags\n"
