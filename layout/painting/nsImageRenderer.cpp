55d1e8aa4fa1b2477794f12682325d6830d47339
Brad Werth <bwerth@mozilla.com>
2021-02-02 12:46:27 +0000

    Bug 1669840 Part 2: Make nsImageRenderer::BuildWebRenderDisplayItems premultiply alpha with provided opacity. r=miko,jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D103130

--- layout/painting/nsImageRenderer.cpp
+++ layout/painting/nsImageRenderer.cpp
@@ -634,7 +634,8 @@ ImgDrawResult nsImageRenderer::BuildWebRenderDisplayItems(
       if (mExtendMode == ExtendMode::CLAMP) {
         // The image is not repeating. Just push as a regular image.
         aBuilder.PushImage(dest, clip, !aItem->BackfaceIsHidden(), rendering,
-                           key.value());
+                           key.value(), true,
+                           wr::ColorF{1.0f, 1.0f, 1.0f, aOpacity});
       } else {
         nsPoint firstTilePos = nsLayoutUtils::GetBackgroundFirstTilePos(
             aDest.TopLeft(), aFill.TopLeft(), aRepeatSize);
@@ -665,7 +666,8 @@ ImgDrawResult nsImageRenderer::BuildWebRenderDisplayItems(
 
         aBuilder.PushRepeatingImage(fill, clip, !aItem->BackfaceIsHidden(),
                                     stretchSize, wr::ToLayoutSize(gapSize),
-                                    rendering, key.value());
+                                    rendering, key.value(), true,
+                                    wr::ColorF{1.0f, 1.0f, 1.0f, aOpacity});
       }
       break;
     }
