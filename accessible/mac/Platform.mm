2af3edc6b973a1ca5056459aef48c7f2938fbf9f
Butkovits Atila <abutkovits@mozilla.com>
2021-02-02 02:04:28 +0200

    Backed out 2 changesets (bug 1688730) for causing mochitest failures. CLOSED TREE
    Backed out changeset 677c17ec48ea (bug 1688730)
    Backed out changeset a7d0770117c8 (bug 1688730)

--- accessible/mac/Platform.mm
+++ accessible/mac/Platform.mm
@@ -90,8 +90,7 @@ void ProxyEvent(ProxyAccessible* aProxy, uint32_t aEventType) {
       aEventType != nsIAccessibleEvent::EVENT_REORDER &&
       aEventType != nsIAccessibleEvent::EVENT_LIVE_REGION_ADDED &&
       aEventType != nsIAccessibleEvent::EVENT_LIVE_REGION_REMOVED &&
-      aEventType != nsIAccessibleEvent::EVENT_NAME_CHANGE &&
-      aEventType != nsIAccessibleEvent::EVENT_OBJECT_ATTRIBUTE_CHANGED)
+      aEventType != nsIAccessibleEvent::EVENT_NAME_CHANGE)
     return;
 
   mozAccessible* wrapper = GetNativeFromGeckoAccessible(aProxy);
