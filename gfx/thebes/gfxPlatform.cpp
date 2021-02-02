3bae64cc4b7583d807211393ff28ffabd3be3aef
Andrew Osmond <aosmond@mozilla.com>
2021-02-02 15:30:09 +0000

    Bug 1690280 - Ensure we check the SW-WR/D3D11 pref before assuming it can be used during fallback. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D103793

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -3384,6 +3384,7 @@ bool gfxPlatform::FallbackFromAcceleration(FeatureStatus aStatus,
   // Before we disable D3D11 and HW_COMPOSITING, we should check if we can
   // fallback from WebRender to Software WebRender + D3D11 compositing.
   if (StaticPrefs::gfx_webrender_fallback_software_d3d11_AtStartup() &&
+      StaticPrefs::gfx_webrender_software_d3d11_AtStartup() &&
       gfxConfig::IsEnabled(Feature::WEBRENDER_SOFTWARE) &&
       gfxConfig::IsEnabled(Feature::D3D11_COMPOSITING) &&
       gfxVars::UseWebRender() && !gfxVars::UseSoftwareWebRender()) {
