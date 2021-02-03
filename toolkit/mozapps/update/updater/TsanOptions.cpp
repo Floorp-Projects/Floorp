66104453f60c3a4d5708d76883556f7b3066a232
Mike Hommey <mh+mozilla@glandium.org>
2021-02-03 16:41:19 +0000

    Bug 1690453 - Remove TSAN suppression for bug 1623034. r=Gankra
    Differential Revision: https://phabricator.services.mozilla.com/D103863

--- toolkit/mozapps/update/updater/TsanOptions.cpp
+++ toolkit/mozapps/update/updater/TsanOptions.cpp
@@ -16,10 +16,6 @@ extern "C" const char* __tsan_default_suppressions() {
          // External uninstrumented libraries
          MOZ_TSAN_DEFAULT_EXTLIB_SUPPRESSIONS
 
-         // Bug 1623034
-         "race:QuitProgressUI\n"
-         "race:ShowProgressUI\n"
-
       // End of suppressions.
       ;  // Please keep this semicolon.
   // clang-format on
