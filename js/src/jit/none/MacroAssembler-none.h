cba26ac4163b4290ca8c249e440fd05226e50612
Asumu Takikawa <asumu@igalia.com>
2021-02-03 04:21:30 +0000

    Bug 1335652 - wasm exceptions part 11: add WasmExceptionReg. r=rhunt
    Adds a WasmExceptionReg register definition in Assembler headers.
    This register is used to coordinate between exception throw
    and catch sites in Wasm.
    
    Differential Revision: https://phabricator.services.mozilla.com/D101135

--- js/src/jit/none/MacroAssembler-none.h
+++ js/src/jit/none/MacroAssembler-none.h
@@ -101,6 +101,7 @@ static constexpr Register WasmTableCallSigReg{Registers::invalid_reg};
 static constexpr Register WasmTableCallIndexReg{Registers::invalid_reg};
 static constexpr Register WasmTlsReg{Registers::invalid_reg};
 static constexpr Register WasmJitEntryReturnScratch{Registers::invalid_reg};
+static constexpr Register WasmExceptionReg{Registers::invalid_reg};
 
 static constexpr uint32_t ABIStackAlignment = 4;
 static constexpr uint32_t CodeAlignment = 16;
