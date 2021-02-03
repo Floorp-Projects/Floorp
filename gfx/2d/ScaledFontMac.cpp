d464d755368b5c3a488bd4c34a5cb4c05a6723d8
Jonathan Kew <jkew@mozilla.com>
2021-02-03 17:25:19 +0000

    Bug 1690235 - Don't use a font descriptor for variation fonts on systems before macOS 10.15. r=lsalzman
    Differential Revision: https://phabricator.services.mozilla.com/D103916

--- gfx/2d/ScaledFontMac.cpp
+++ gfx/2d/ScaledFontMac.cpp
@@ -18,6 +18,7 @@
 #ifdef MOZ_WIDGET_UIKIT
 #  include <CoreFoundation/CoreFoundation.h>
 #endif
+#include "gfxPlatform.h"  // just for HasVariationFontSupport()
 #include "nsCocoaFeatures.h"
 #include "mozilla/gfx/Logging.h"
 
@@ -372,6 +373,17 @@ bool UnscaledFontMac::GetFontDescriptor(FontDescriptorOutput aCb,
                                         void* aBaton) {
   if (mIsDataFont) {
     return false;
+  }
+
+  // Bug 1690235: using a font descriptor may fail for variation fonts
+  // on systems prior to 10.15, so we return false in this case.
+  if (gfxPlatform::GetPlatform()->HasVariationFontSupport() &&
+      !nsCocoaFeatures::OnCatalinaOrLater()) {
+    CFDataRef data = CGFontCopyTableForTag(mFont, 0x66766172);  // 'fvar'
+    if (data) {
+      CFRelease(data);
+      return false;
+    }
   }
 
   AutoRelease<CFStringRef> psname(CGFontCopyPostScriptName(mFont));
