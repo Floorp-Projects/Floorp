ab801c5c3f6ede9f2e5f6946734a215d14fa4ea7
Mike Hommey <mh+mozilla@glandium.org>
2021-02-04 14:52:00 +0000

    Bug 1690757 - Build the clang aarch64 cross toolchain as a repack of clang + compiler-rt. r=firefox-build-system-reviewers,andi,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D104013

---
+++ taskcluster/scripts/misc/repack-clang-linux-cross.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x -e -v
+
+# This script is for building clang for cross Linux targets on a Linux host.
+
+cd $MOZ_FETCHES_DIR
+
+# We have a native linux64 toolchain in $MOZ_FETCHES_DIR/clang
+# We have some linux compiler-rt in $MOZ_FETCHES_DIR/compiler-rt
+clang_lib=$(echo clang/lib/clang/*/lib)
+find compiler-rt/lib/linux -type f | while read f; do
+  cp $f $clang_lib/linux
+done
+
+tar -cf - clang | python3 $GECKO_PATH/taskcluster/scripts/misc/zstdpy > clang.tar.zst
+
+mkdir -p $UPLOAD_DIR
+mv clang.tar.zst $UPLOAD_DIR
