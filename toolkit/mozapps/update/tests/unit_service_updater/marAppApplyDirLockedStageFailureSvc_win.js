706a1c6dc376b4dad52dabee043ae4105483bdab
Kirk Steuber <ksteuber@mozilla.com>
2021-02-05 04:16:30 +0000

    Bug 1682810 - Fix tests to expect the correct pending status r=application-update-reviewers,nalexander
    Differential Revision: https://phabricator.services.mozilla.com/D104124

--- toolkit/mozapps/update/tests/unit_service_updater/marAppApplyDirLockedStageFailureSvc_win.js
+++ toolkit/mozapps/update/tests/unit_service_updater/marAppApplyDirLockedStageFailureSvc_win.js
@@ -11,7 +11,7 @@ async function run_test() {
   if (!setupTestCommon()) {
     return;
   }
-  const STATE_AFTER_STAGE = STATE_PENDING;
+  const STATE_AFTER_STAGE = STATE_PENDING_SVC;
   gTestFiles = gTestFilesCompleteSuccess;
   gTestDirs = gTestDirsCompleteSuccess;
   setTestFilesAndDirsForFailure();
