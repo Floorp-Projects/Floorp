d59b72d966ed0760ce6486e33b8b9b7b012ab311
Andreas Pehrson <apehrson@mozilla.com>
2021-02-08 18:15:07 +0100

    Bug 1654112 - Adapt VideoConduit to the new libwebrtc version. r=ng,bwc
    This hooks up VideoReceiveStream::Config with a decoder factory and
    VideoSendStream::Config with both a bitrate allocator factory and an encoder
    factory.
    
    The factories are new and wraps all the types of decoders and encoders that we
    could create prior to this patch. This patch lets libwebrtc create our codecs
    on demand, allowing for reconfiguring the streams and changing codec on the fly.
    
    Some extra care had to be taken to ensure that GMP PluginIDs can be properly
    plumbed. This has been implemented with MediaEvent and MozPromise.
    
    Some other changes are made too, as warranted by runtime or threading changes.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104720

---
+++ dom/media/webrtc/libwebrtcglue/WebrtcVideoCodecFactory.h
@@ -0,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-*/
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef DOM_MEDIA_WEBRTC_LIBWEBRTCGLUE_WEBRTCVIDEOCODECFACTORY_H_
+#define DOM_MEDIA_WEBRTC_LIBWEBRTCGLUE_WEBRTCVIDEOCODECFACTORY_H_
+
+#include "api/video_codecs/video_decoder_factory.h"
+#include "api/video_codecs/video_encoder_factory.h"
+#include "MediaEventSource.h"
+
+namespace mozilla {
+class WebrtcVideoDecoderFactory : public webrtc::VideoDecoderFactory {
+ public:
+  std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override {
+    MOZ_CRASH("Unexpected call");
+    return std::vector<webrtc::SdpVideoFormat>();
+  }
+
+  std::unique_ptr<webrtc::VideoDecoder> CreateVideoDecoder(
+      const webrtc::SdpVideoFormat& aFormat) override;
+};
+
+class WebrtcVideoEncoderFactory : public webrtc::VideoEncoderFactory {
+  class InternalFactory : public webrtc::VideoEncoderFactory {
+   public:
+    std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override {
+      MOZ_CRASH("Unexpected call");
+      return std::vector<webrtc::SdpVideoFormat>();
+    }
+
+    std::unique_ptr<webrtc::VideoEncoder> CreateVideoEncoder(
+        const webrtc::SdpVideoFormat& aFormat) override;
+
+    bool Supports(const webrtc::SdpVideoFormat& aFormat);
+  };
+
+ public:
+  WebrtcVideoEncoderFactory()
+      : mInternalFactory(MakeUnique<InternalFactory>()) {}
+
+  std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override {
+    MOZ_CRASH("Unexpected call");
+    return std::vector<webrtc::SdpVideoFormat>();
+  }
+
+  std::unique_ptr<webrtc::VideoEncoder> CreateVideoEncoder(
+      const webrtc::SdpVideoFormat& aFormat) override;
+
+ private:
+  const UniquePtr<InternalFactory> mInternalFactory;
+};
+}  // namespace mozilla
+
+#endif
