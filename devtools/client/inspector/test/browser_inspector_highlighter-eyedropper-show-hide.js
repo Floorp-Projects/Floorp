ae0d1d8ed8213b8e2b0096b9d61a99a50f801262
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-02-08 07:01:34 +0000

    Bug 1690217 - [devtools] Add test for eyedropper on page with iframes. r=ladybenko.
    This adds a test that checks that the eyedropper displays the expected
    colors when hovering iframes (same origin and remote).
    Since we're going to do extra work from the client in Bug 1568831 to
    actually get the screenshot, we can't simply use the regular inspector
    test highlighter helper, as it spawns an eyedropper eyelighter on its
    own, without calling the inspector client code.
    And since we can't use the helper, we can't use the testActor method
    that the other tests use, as we don't have an highlighter front for
    the eyedropper.
    We create new methods on the testActor which will retrieve the eyedropper
    instance that is created by the inspector actor.
    The new test expectedly fails on fission and is tagged as fail-if.
    We take this as an opportunity to enhance the simple eyedropper test
    so we actually uses the button in the ui instead of only manipulating it
    through the test actor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103746

--- devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-show-hide.js
+++ devtools/client/inspector/test/browser_inspector_highlighter-eyedropper-show-hide.js
@@ -5,44 +5,42 @@
 
 // Test the basic structure of the eye-dropper highlighter.
 
-const HIGHLIGHTER_TYPE = "EyeDropper";
-const ID = "eye-dropper-";
-
 add_task(async function() {
-  const helper = await openInspectorForURL(
+  const { inspector, testActor } = await openInspectorForURL(
     "data:text/html;charset=utf-8,eye-dropper test"
-  ).then(getHighlighterHelperFor(HIGHLIGHTER_TYPE));
-  helper.prefix = ID;
+  );
+  const inspectorFrontActorID = inspector.inspectorFront.actorID;
 
-  await isInitiallyHidden(helper);
-  await canBeShownAndHidden(helper);
-
-  helper.finalize();
-});
-
-async function isInitiallyHidden({ isElementHidden }) {
   info("Checking that the eyedropper is hidden by default");
+  const eyeDropperVisible = await testActor.isEyeDropperVisible(
+    inspectorFrontActorID
+  );
+  is(eyeDropperVisible, false, "The eyedropper is hidden by default");
 
-  const hidden = await isElementHidden("root");
-  ok(hidden, "The eyedropper is hidden by default");
-}
+  const toggleButton = inspector.panelDoc.querySelector(
+    "#inspector-eyedropper-toggle"
+  );
 
-async function canBeShownAndHidden({
-  show,
-  hide,
-  isElementHidden,
-  getElementAttribute,
-}) {
-  info("Asking to show and hide the highlighter actually works");
+  info("Display the eyedropper by clicking on the inspector toolbar button");
+  toggleButton.click();
+  await TestUtils.waitForCondition(() =>
+    testActor.isEyeDropperVisible(inspectorFrontActorID)
+  );
+  ok(true, "Eye dropper is visible after clicking the button in the inspector");
 
-  await show("html");
-  let hidden = await isElementHidden("root");
-  ok(!hidden, "The eyedropper is now shown");
-
-  const style = await getElementAttribute("root", "style");
+  const style = await testActor.getEyeDropperElementAttribute(
+    inspectorFrontActorID,
+    "root",
+    "style"
+  );
   is(style, "top:100px;left:100px;", "The eyedropper is correctly positioned");
 
-  await hide();
-  hidden = await isElementHidden("root");
-  ok(hidden, "The eyedropper is now hidden again");
-}
+  info("Hide the eyedropper by clicking on the inspector toolbar button again");
+  toggleButton.click();
+  await TestUtils.waitForCondition(() =>
+    testActor
+      .isEyeDropperVisible(inspectorFrontActorID)
+      .then(visible => !visible)
+  );
+  ok(true, "Eye dropper is not visible anymore");
+});
