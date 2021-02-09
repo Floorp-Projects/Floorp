4099b5396ef0d27b45e96a403142d688fa93eb0d
Hiroyuki Ikezoe <hikezoe.birchill@mozilla.com>
2021-02-09 02:41:14 +0000

    Bug 1682743 - A new test for popup window position in OOP iframe transformed by an ancestor element in the parent document. r=tnikkel
    Depends on D104367
    
    Differential Revision: https://phabricator.services.mozilla.com/D104368

---
+++ gfx/layers/apz/test/mochitest/browser_test_select_popup_position.js
@@ -0,0 +1,104 @@
+/* This test is a a mash up of
+     https://searchfox.org/mozilla-central/rev/559b25eb41c1cbffcb90a34e008b8288312fcd25/gfx/layers/apz/test/mochitest/browser_test_group_fission.js
+     https://searchfox.org/mozilla-central/rev/559b25eb41c1cbffcb90a34e008b8288312fcd25/gfx/layers/apz/test/mochitest/helper_basic_zoom.html
+     https://searchfox.org/mozilla-central/rev/559b25eb41c1cbffcb90a34e008b8288312fcd25/browser/base/content/test/forms/browser_selectpopup.js
+*/
+
+/* import-globals-from helper_browser_test_utils.js */
+Services.scriptloader.loadSubScript(
+  new URL("helper_browser_test_utils.js", gTestPath).href,
+  this
+);
+
+add_task(async function() {
+  function httpURL(filename) {
+    let chromeURL = getRootDirectory(gTestPath) + filename;
+    return chromeURL.replace(
+      "chrome://mochitests/content/",
+      "http://mochi.test:8888/"
+    );
+  }
+
+  function httpCrossOriginURL(filename) {
+    let chromeURL = getRootDirectory(gTestPath) + filename;
+    return chromeURL.replace(
+      "chrome://mochitests/content/",
+      "http://example.com/"
+    );
+  }
+
+  const pageUrl = httpURL(
+    "helper_test_select_popup_position_transformed_in_parent.html"
+  );
+  let tab = await BrowserTestUtils.openNewForegroundTab(gBrowser, pageUrl);
+
+  // Load the OOP iframe.
+  const iframeUrl = httpCrossOriginURL(
+    "helper_test_select_popup_position.html"
+  );
+  const iframe = await SpecialPowers.spawn(
+    tab.linkedBrowser,
+    [iframeUrl],
+    async url => {
+      const target = content.document.querySelector("iframe");
+      target.src = url;
+      await new Promise(resolve => {
+        target.addEventListener("load", resolve, { once: true });
+      });
+      return target.browsingContext;
+    }
+  );
+
+  await SpecialPowers.spawn(tab.linkedBrowser, [], async () => {
+    await content.wrappedJSObject.promiseApzFlushedRepaints();
+    await content.wrappedJSObject.waitUntilApzStable();
+  });
+
+  const selectRect = await SpecialPowers.spawn(iframe, [], () => {
+    return content.document.querySelector("select").getBoundingClientRect();
+  });
+
+  // Get focus on the select element.
+  await SpecialPowers.spawn(iframe, [], async () => {
+    const select = content.document.querySelector("select");
+    const focusPromise = new Promise(resolve => {
+      select.addEventListener("focus", resolve, { once: true });
+    });
+    select.focus();
+    await focusPromise;
+  });
+
+  const menulist = document.getElementById("ContentSelectDropdown");
+  const selectPopup = menulist.menupopup;
+
+  await openSelectPopup(selectPopup);
+
+  const popup_rect = selectPopup.getBoundingClientRect();
+
+  is(
+    popup_rect.left,
+    selectRect.x * 2.0,
+    "select popup position should be scaled by the desktop zoom"
+  );
+
+  // On platforms other than MaxOSX the popup menu is positioned below the
+  // option element.
+  if (!navigator.platform.includes("Mac")) {
+    is(
+      popup_rect.top,
+      tab.linkedBrowser.getBoundingClientRect().top +
+        (selectRect.y + selectRect.height) * 2.0,
+      "select popup position should be scaled by the desktop zoom"
+    );
+  } else {
+    is(
+      popup_rect.top,
+      tab.linkedBrowser.getBoundingClientRect().top + selectRect.y * 2.0,
+      "select popup position should be scaled by the desktop zoom"
+    );
+  }
+
+  await hideSelectPopup(selectPopup);
+
+  BrowserTestUtils.removeTab(tab);
+});
