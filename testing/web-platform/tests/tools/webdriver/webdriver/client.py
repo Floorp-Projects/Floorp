fd6bccdab240143576ee733c40b5158fdc1d015f
Stephen McGruer <smcgruer@chromium.org>
2021-02-09 04:38:20 +0000

    Bug 1690784 [wpt PR 27479] - [python3] Remove more `six` usages in tools/, a=testonly
    Automatic update from web-platform-tests
    [python3] Remove most uses of six in tools/manifest/
    
    The exception is a single usage of ensure_text, which is not trivially
    replaceable.
    
    --
    [python3] Remove usage of six from tools/webdriver/
    
    --
    [python3] Cleanup a few misc uses of six in tools/
    
    --
    
    wpt-commits: 57b63e53344e52ed44dbad5e315af93d66391c44, 57d726a4f79d592688c7c54216dc82fbca739ba3, c16a8c87cbb0d7cd82607bf21bcc532add3c8c08
    wpt-pr: 27479

--- testing/web-platform/tests/tools/webdriver/webdriver/client.py
+++ testing/web-platform/tests/tools/webdriver/webdriver/client.py
@@ -2,8 +2,7 @@ from . import error
 from . import protocol
 from . import transport
 
-from six import string_types
-from six.moves.urllib import parse as urlparse
+from urllib import parse as urlparse
 
 
 def command(func):
@@ -435,7 +434,7 @@ class Cookies(object):
         cookie = {"name": name,
                   "value": None}
 
-        if isinstance(name, string_types):
+        if isinstance(name, str):
             cookie["value"] = value
         elif hasattr(value, "value"):
             cookie["value"] = value.value
