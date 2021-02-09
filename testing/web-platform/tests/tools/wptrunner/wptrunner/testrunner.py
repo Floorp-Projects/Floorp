b99651b6eae0762c1390d0ebad97b99f53d2daaf
Sam Sneddon <gsnedders@apple.com>
2021-02-09 04:34:25 +0000

    Bug 1690768 [wpt PR 27475] - Call Queue.cancel_join_thread() when recreating queues, a=testonly
    Automatic update from web-platform-tests
    Call Queue.cancel_join_thread() when recreating queues
    
    This avoids the close call from blocking, which is potentially
    problematic if the queue itself has gotten broken in some way.
    
    --
    
    wpt-commits: 3695d5b6eb197dd4bc9148a77776c351a53fdef5
    wpt-pr: 27475

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
@@ -776,14 +776,19 @@ class TestRunnerManager(threading.Thread):
             # This might leak a file handle from the queue
             self.logger.warning("Forcibly terminating runner process")
             self.test_runner_proc.terminate()
+            self.logger.debug("After terminating runner process")
 
             # Multiprocessing queues are backed by operating system pipes. If
             # the pipe in the child process had buffered data at the time of
             # forced termination, the queue is no longer in a usable state
             # (subsequent attempts to retrieve items may block indefinitely).
             # Discard the potentially-corrupted queue and create a new one.
+            self.logger.debug("Recreating command queue")
+            self.command_queue.cancel_join_thread()
             self.command_queue.close()
             self.command_queue = mp.Queue()
+            self.logger.debug("Recreating remote queue")
+            self.remote_queue.cancel_join_thread()
             self.remote_queue.close()
             self.remote_queue = mp.Queue()
         else:
