a5da921b52bd03abb7b83f1046a2d81f573b9281
Mike Hommey <mh+mozilla@glandium.org>
2021-02-09 00:44:02 +0000

    Bug 1690937 - Build lucetc with a sysroot. r=firefox-build-system-reviewers,sheehan,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D104131

--- taskcluster/scripts/misc/build-lucetc.sh
+++ taskcluster/scripts/misc/build-lucetc.sh
@@ -5,7 +5,7 @@ cd $MOZ_FETCHES_DIR/lucet_sandbox_compiler
 
 export PATH=$MOZ_FETCHES_DIR/binutils/bin:$MOZ_FETCHES_DIR/clang/bin:$PATH:$MOZ_FETCHES_DIR/rustc/bin:$MOZ_FETCHES_DIR/cmake/bin
 export CC=$MOZ_FETCHES_DIR/clang/bin/clang
-export CFLAGS="-L$MOZ_FETCHES_DIR/clang/lib"
+export CFLAGS="--sysroot=$MOZ_FETCHES_DIR/sysroot"
 export CXXFLAGS=$CFLAGS
 export CXX=$MOZ_FETCHES_DIR/clang/bin/clang++
 export AR=$MOZ_FETCHES_DIR/clang/bin/llvm-ar
