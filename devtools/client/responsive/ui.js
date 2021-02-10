9bbdb30715d814055783f2957ffbf82d04f91c49
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-02-10 17:49:22 +0000

    Bug 1689951 - [devtools] Display notification for screenshot warnings and error messages. r=jdescottes.
    This patch make the warning and error messages visible when such messages were
    emitted, for the RDM screenshot button, the toolbar screenshot button and the
    inspector screenshot node context menu entry.
    In the case of the toolbar screenshot button in the browser toolbox, we also
    display the saved messages so users have a way to know where the file was saved.
    We don't do anything more for `:screenshot` command in the console, as the messages
    are already displayed directly in the console output.
    Tests are added for the different use case. The RDM one is a bit different as
    we can't have a viewport taller than 9999px, so we bump the dpr to make capture-screenshot
    downsize it (as such image would be too big).
    
    We take this opportunity to fix the `takeNodeScreenshot` helper, which was adding
    a "load" event listener after setting the src on an image, which can lead to races.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104058

--- devtools/client/responsive/ui.js
+++ devtools/client/responsive/ui.js
@@ -44,6 +44,12 @@ loader.lazyRequireGetter(
   "devtools/client/responsive/utils/notification",
   true
 );
+loader.lazyRequireGetter(
+  this,
+  "PriorityLevels",
+  "devtools/client/shared/components/NotificationBox",
+  true
+);
 loader.lazyRequireGetter(this, "l10n", "devtools/client/responsive/utils/l10n");
 loader.lazyRequireGetter(this, "asyncStorage", "devtools/shared/async-storage");
 loader.lazyRequireGetter(
@@ -698,7 +704,29 @@ class ResponsiveUI {
   }
 
   async onScreenshot() {
-    await captureAndSaveScreenshot(this.currentTarget, this.browserWindow);
+    const messages = await captureAndSaveScreenshot(
+      this.currentTarget,
+      this.browserWindow
+    );
+
+    const priorityMap = {
+      error: PriorityLevels.PRIORITY_CRITICAL_HIGH,
+      warn: PriorityLevels.PRIORITY_WARNING_HIGH,
+    };
+    for (const { text, level } of messages) {
+      // captureAndSaveScreenshot returns "saved" messages, that indicate where the
+      // screenshot was saved. We don't want to display them as the download UI can be
+      // used to open the file.
+      if (level !== "warn" && level !== "error") {
+        continue;
+      }
+
+      showNotification(this.browserWindow, this.tab, {
+        msg: text,
+        priority: priorityMap[level],
+      });
+    }
+
     message.post(this.rdmFrame.contentWindow, "screenshot-captured");
   }
 
