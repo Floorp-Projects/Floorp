2201d256a10fe1600a29544f4bf15787ceb34566
Bernard Igiri <bigiri@mozilla.com>
2021-02-10 17:39:14 +0000

    Bug 1691433 - Remove check for updates option from application menus r=mconley,jaws,fluent-reviewers
    Removing Check For Updates button from menu.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104548

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -1079,10 +1079,6 @@ function buildHelpMenu() {
   document.getElementById(
     "feedbackPage"
   ).disabled = !Services.policies.isAllowed("feedbackCommands");
-  let checkForUpdates = document.getElementById("checkForUpdates");
-  if (checkForUpdates) {
-    checkForUpdates.disabled = !Services.policies.isAllowed("appUpdate");
-  }
 
   let supportMenu = Services.policies.getSupportMenu();
   if (supportMenu) {
