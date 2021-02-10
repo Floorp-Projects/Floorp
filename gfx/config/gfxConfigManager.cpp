d636995f23c52925c26992ffe14ba8c61a4bc551
Jamie Nicol <jnicol@mozilla.com>
2021-02-10 13:10:38 +0000

    Bug 1680087 - Disable webrender partial present on Mali-T6xx and T7xx. r=aosmond
    Due to rendering issues reported on a Mali-T628 and Mali-T760, disable
    partial present on all Mali-T6xx and T7xx devices. We know that not
    all T6xx and T7xx devices are affected, so this is being cautious. The
    driver version is probably more important than the GPU model. We
    should make the block more precise once more is known about the bug.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104678

--- gfx/config/gfxConfigManager.cpp
+++ gfx/config/gfxConfigManager.cpp
@@ -390,6 +390,17 @@ void gfxConfigManager::ConfigureWebRender() {
   if (mWrPartialPresent) {
     if (mFeatureWr->IsEnabled() || mFeatureWrSoftware->IsEnabled()) {
       mFeatureWrPartial->EnableByDefault();
+
+      nsString adapter;
+      mGfxInfo->GetAdapterDeviceID(adapter);
+      // Block partial present on Mali-T6xx and T7xx GPUs due to rendering
+      // issues. See bug 1680087.
+      if (adapter.Find("Mali-T6", /*ignoreCase*/ true) >= 0 ||
+          adapter.Find("Mali-T7", /*ignoreCase*/ true) >= 0) {
+        mFeatureWrPartial->Disable(FeatureStatus::Blocked,
+                                   "Partial present blocked on Mali-Txxx",
+                                   "FEATURE_FAILURE_PARTIAL_PRESENT_MALI"_ns);
+      }
     }
   }
 }
