c73fccf9317399ced70f74dd14ab017bac982007
Mike Hommey <mh+mozilla@glandium.org>
2021-02-11 22:06:20 +0000

    Bug 1685764 - Switch openh264 mac builds to the macos SDK toolchain task. r=firefox-build-system-reviewers,sheehan,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D104647

--- testing/mozharness/configs/openh264/macosx64.py
+++ testing/mozharness/configs/openh264/macosx64.py
@@ -13,8 +13,6 @@ external_tools_path = os.path.join(
 )
 
 config = {
-    "tooltool_manifest_file": "osx.manifest",
-    "tooltool_cache": "/builds/tooltool_cache",
     "exes": {
         "gittool.py": [os.path.join(external_tools_path, "gittool.py")],
         "python2.7": "python2.7",
@@ -26,13 +24,17 @@ config = {
     "partial_env": {
         "CXXFLAGS": (
             "-target x86_64-apple-darwin "
-            "-isysroot %(abs_work_dir)s/MacOSX10.11.sdk "
-            "-mmacosx-version-min=10.11"
+            "-isysroot {MOZ_FETCHES_DIR}/MacOSX10.11.sdk "
+            "-mmacosx-version-min=10.11".format(
+                MOZ_FETCHES_DIR=os.environ["MOZ_FETCHES_DIR"]
+            )
         ),
         "LDFLAGS": (
             "-target x86_64-apple-darwin "
-            "-isysroot %(abs_work_dir)s/MacOSX10.11.sdk "
-            "-mmacosx-version-min=10.11"
+            "-isysroot {MOZ_FETCHES_DIR}/MacOSX10.11.sdk "
+            "-mmacosx-version-min=10.11".format(
+                MOZ_FETCHES_DIR=os.environ["MOZ_FETCHES_DIR"]
+            )
         ),
         "PATH": (
             "{MOZ_FETCHES_DIR}/clang/bin/:{MOZ_FETCHES_DIR}/cctools/bin/:%(PATH)s".format(
