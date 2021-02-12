fef8f19fd482eedcf218591b7c6e602a384f9954
Julien Wajsberg <felash@gmail.com>
2021-02-12 15:52:41 +0000

    Bug 1688195 - Properly support RTL languages for about:profiling r=jdescottes,Itiel
    This patch adds the capability to add the necessary attributes to the
    document to the thin Fluent wrapper that devtools panels use. Then it
    should be easy to reuse by other panels.
    
    Differential Revision: https://phabricator.services.mozilla.com/D103050

---
+++ devtools/client/performance-new/test/browser/browser_aboutprofiling-rtl.js
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+add_task(async function() {
+  await withAboutProfiling(async document => {
+    is(document.dir, "ltr", "About profiling has the expected direction ltr");
+    is(
+      document.documentElement.getAttribute("lang"),
+      "en-US",
+      "About profiling has the expected lang"
+    );
+  });
+});
+
+add_task(async function() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["intl.l10n.pseudo", "bidi"]],
+  });
+
+  await withAboutProfiling(async document => {
+    is(document.dir, "rtl", "About profiling has the expected direction rtl");
+    is(
+      document.documentElement.getAttribute("lang"),
+      "en-US",
+      "About profiling has the expected lang"
+    );
+  });
+});
