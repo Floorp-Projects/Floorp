13b61ded01caf05154fb2c9a7255c825757017fe
Marco Bonardo <mbonardo@mozilla.com>
2021-02-12 20:58:18 +0000

    Bug 1692218 - Add Page Action Menu use counter prefs. r=harry
    Also includes:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1692216
    https://bugzilla.mozilla.org/show_bug.cgi?id=1692215
    
    Differential Revision: https://phabricator.services.mozilla.com/D105033

--- browser/modules/test/browser/browser_UsageTelemetry_usagePrefs.js
+++ browser/modules/test/browser/browser_UsageTelemetry_usagePrefs.js
@@ -79,3 +79,64 @@ add_task(async function test_usage_ctrltab_pref_set() {
     `${ctrlTabUsed} should now be true after interacting.`
   );
 });
+
+/**
+ * Tests that browser.engagement.pageAction-panel-*.used.count is incremented
+ * when user clicks on certain page action buttons.
+ */
+add_task(async function test_usage_pageAction_buttons() {
+  // Skip the test if proton is enabled, since these buttons are hidden.
+  if (gProton) {
+    return;
+  }
+
+  await SpecialPowers.pushPrefEnv({
+    set: [["extensions.screenshots.disabled", false]],
+  });
+
+  const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
+  const sandbox = sinon.createSandbox();
+  registerCleanupFunction(sandbox.restore);
+  sandbox.stub(BrowserPageActions, "doCommandForAction").callsFake(() => {});
+
+  await BrowserTestUtils.withNewTab("http://example.com/", async browser => {
+    info("Open the pageAction panel");
+    await promiseOpenPageActionPanel();
+
+    function click(button) {
+      info(`Click on ${button.id}`);
+      EventUtils.synthesizeMouseAtCenter(button, {}, window);
+    }
+
+    let ids = [
+      "pageAction-panel-copyURL",
+      "pageAction-panel-emailLink",
+      "pageAction-panel-pinTab",
+      "pageAction-panel-screenshots_mozilla_org",
+    ];
+    if (
+      AppConstants.platform == "macosx" ||
+      AppConstants.isPlatformAndVersionAtLeast("win", "6.4")
+    ) {
+      ids.push("pageAction-panel-shareURL");
+    }
+    for (let id of ids) {
+      let pref = `browser.engagement.${id}.used-count`;
+      let button = document.getElementById(id);
+      Assert.ok(button, `The button "${id}" should be present`);
+      Services.prefs.clearUserPref(pref);
+      Assert.equal(Services.prefs.getIntPref(pref, 0), 0, "Check initial");
+      click(button);
+      Assert.equal(Services.prefs.getIntPref(pref, 0), 1, "Check increment");
+      click(button);
+      Assert.equal(Services.prefs.getIntPref(pref, 0), 2, "Check increment");
+      Services.prefs.clearUserPref(pref);
+    }
+
+    let hidePromise = promisePageActionPanelHidden();
+    BrowserPageActions.panelNode.hidePopup();
+    await hidePromise;
+  });
+
+  await SpecialPowers.popPrefEnv();
+});
