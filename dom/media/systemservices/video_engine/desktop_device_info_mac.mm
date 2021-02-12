19c256a5fffae7e74402af4425c0cae3fefbd40a
Byron Campen [:bwc] <docfaraday@gmail.com>
2021-02-12 14:28:35 +0000

    Bug 1654112 - OS X include fixes, and add a necessary source file for OS X. r=ng
    Differential Revision: https://phabricator.services.mozilla.com/D105018

--- dom/media/systemservices/video_engine/desktop_device_info_mac.mm
+++ dom/media/systemservices/video_engine/desktop_device_info_mac.mm
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "modules/desktop_capture/mac/desktop_device_info_mac.h"
+#include "desktop_device_info_mac.h"
+#include "mozilla/Sprintf.h"
 #include <AppKit/AppKit.h>
 #include <Cocoa/Cocoa.h>
 #include <unistd.h>
