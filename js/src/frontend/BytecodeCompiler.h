6d8651a8ff95c7e74cf2a2a9fd7a291dcaf306b4
Ted Campbell <tcampbell@mozilla.com>
2021-02-12 17:07:36 +0000

    Bug 1692317 - Simplify ParseModuleToStencil overloads. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D104886

--- js/src/frontend/BytecodeCompiler.h
+++ js/src/frontend/BytecodeCompiler.h
@@ -121,11 +121,6 @@ ModuleObject* CompileModule(JSContext* cx,
 
 // Parse a module of the given source.  This is an internal API; if you want to
 // compile a module as a user, use CompileModule above.
-bool ParseModuleToStencil(JSContext* cx, CompilationStencil& stencil,
-                          JS::SourceText<char16_t>& srcBuf);
-bool ParseModuleToStencil(JSContext* cx, CompilationStencil& stencil,
-                          JS::SourceText<mozilla::Utf8Unit>& srcBuf);
-
 UniquePtr<CompilationStencil> ParseModuleToStencil(
     JSContext* cx, const JS::ReadOnlyCompileOptions& options,
     JS::SourceText<char16_t>& srcBuf);
