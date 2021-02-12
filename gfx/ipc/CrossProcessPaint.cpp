2b2b87d224de2970e1bab5bf0d22e1c85acf0d8b
Matt Woodrow <mwoodrow@mozilla.com>
2021-02-12 00:40:41 +0000

    Bug 1676409 - Attempt to resolve CrossProcessPaint objects for printing immediately, in case we didn't queue any dependencies. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D104782

--- gfx/ipc/CrossProcessPaint.cpp
+++ gfx/ipc/CrossProcessPaint.cpp
@@ -298,6 +298,8 @@ RefPtr<CrossProcessPaint::ResolvePromise> CrossProcessPaint::Start(
   resolver->QueueDependencies(rootFragment.mDependencies);
   resolver->mReceivedFragments.Put(dom::TabId(0), std::move(rootFragment));
 
+  resolver->MaybeResolve();
+
   return promise;
 }
 
