6b4b3b09424a6bb6cd34bdd762aeb1f6b1d65016
Emilio Cobos Álvarez <emilio@crisal.io>
2021-02-15 19:08:42 +0000

    Bug 1689155 - Don't necessarily show outlines on refocus. r=edgar
    Preserve the last state of when we focused the element in that window,
    if the focus method is unknown.
    
    Depends on D104861
    
    Differential Revision: https://phabricator.services.mozilla.com/D104863

--- dom/base/nsFocusManager.h
+++ dom/base/nsFocusManager.h
@@ -747,9 +747,10 @@ class nsFocusManager final : public nsIFocusManager,
   // information could be wrong.
   //
   // aShouldShowFocusRing is only relevant if aGettingFocus is true.
-  static void NotifyFocusStateChange(
-      mozilla::dom::Element* aElement, mozilla::dom::Element* aElementToFocus,
-      int32_t aFlags, bool aGettingFocus, bool aShouldShowFocusRing);
+  static void NotifyFocusStateChange(mozilla::dom::Element* aElement,
+                                     mozilla::dom::Element* aElementToFocus,
+                                     int32_t aFlags, bool aGettingFocus,
+                                     bool aShouldShowFocusRing);
 
   void SetFocusedWindowInternal(nsPIDOMWindowOuter* aWindow);
 
