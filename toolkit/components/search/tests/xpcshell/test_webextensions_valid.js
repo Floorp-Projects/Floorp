515b491ca990db68d32a2f008031c82d0aa90afe
Mark Banner <standard8@mozilla.com>
2021-02-15 18:14:55 +0000

    Bug 1680104 - Rewrite test_webextensions_valid.js to make it simpler to extend. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D105050

--- toolkit/components/search/tests/xpcshell/test_webextensions_valid.js
+++ toolkit/components/search/tests/xpcshell/test_webextensions_valid.js
@@ -28,144 +28,19 @@ add_task(async function setup() {
   await Services.search.init();
   await promiseAfterSettings();
 
-  extension = await SearchTestUtils.installSearchExtension();
-  await extension.awaitStartup();
-
-  // For these tests, stub-out the removeEngine function, so that when we
-  // remove it from the add-on manager, the engine is left in the search
-  // settings.
+  // For these tests, we sometimes stub-out the removeEngine function,
+  // so that when we remove it from the add-on manager,
+  // the engine is left in the search settings.
+  // Save the original function, so we can access it if necessary and restore
+  // it appropriately.
   oldRemoveEngineFunc = Services.search.wrappedJSObject.removeEngine.bind(
     Services.search.wrappedJSObject
   );
-  Services.search.wrappedJSObject.removeEngine = () => {};
 
   registerCleanupFunction(async () => {
+    Services.search.wrappedJSObject.removeEngine = oldRemoveEngineFunc;
     await promiseShutdownManager();
   });
-});
-
-add_task(async function test_valid_extensions_do_nothing() {
-  Services.telemetry.clearScalars();
-
-  Assert.ok(
-    Services.search.getEngineByName("Example"),
-    "Should have installed the engine"
-  );
-
-  await Services.search.runBackgroundChecks();
-
-  let scalars = TelemetryTestUtils.getProcessScalars("parent", true, true);
-
-  Assert.deepEqual(scalars, {}, "Should not have recorded any issues");
-});
-
-add_task(async function test_different_name() {
-  Services.telemetry.clearScalars();
-
-  let engine = Services.search.getEngineByName("Example");
-
-  engine.wrappedJSObject._name = "Example Test";
-
-  await Services.search.runBackgroundChecks();
-
-  TelemetryTestUtils.assertKeyedScalar(
-    TelemetryTestUtils.getProcessScalars("parent", true, true),
-    "browser.searchinit.engine_invalid_webextension",
-    extension.id,
-    5
-  );
-
-  engine.wrappedJSObject._name = "Example";
-});
-
-add_task(async function test_different_url() {
-  Services.telemetry.clearScalars();
-
-  let engine = Services.search.getEngineByName("Example");
-
-  engine.wrappedJSObject._urls = [];
-  engine.wrappedJSObject._setUrls({
-    search_url: "https://example.com/123",
-    search_url_get_params: "?q={searchTerms}",
-  });
-
-  await Services.search.runBackgroundChecks();
-
-  TelemetryTestUtils.assertKeyedScalar(
-    TelemetryTestUtils.getProcessScalars("parent", true, true),
-    "browser.searchinit.engine_invalid_webextension",
-    extension.id,
-    6
-  );
-});
-
-add_task(async function test_extension_no_longer_specifies_engine() {
-  Services.telemetry.clearScalars();
-
-  let extensionInfo = {
-    useAddonManager: "permanent",
-    manifest: {
-      version: "2.0",
-      applications: {
-        gecko: {
-          id: "example@tests.mozilla.org",
-        },
-      },
-    },
-  };
-
-  await extension.upgrade(extensionInfo);
-
-  await Services.search.runBackgroundChecks();
-
-  TelemetryTestUtils.assertKeyedScalar(
-    TelemetryTestUtils.getProcessScalars("parent", true, true),
-    "browser.searchinit.engine_invalid_webextension",
-    extension.id,
-    4
-  );
-});
-
-add_task(async function test_disabled_extension() {
-  // We don't clear scalars between tests to ensure the scalar gets set
-  // to the new value, rather than added.
-
-  // Disable the extension, this won't remove the search engine because we've
-  // stubbed removeEngine.
-  await extension.addon.disable();
-
-  await Services.search.runBackgroundChecks();
-
-  TelemetryTestUtils.assertKeyedScalar(
-    TelemetryTestUtils.getProcessScalars("parent", true, true),
-    "browser.searchinit.engine_invalid_webextension",
-    extension.id,
-    2
-  );
-
-  extension.addon.enable();
-  await extension.awaitStartup();
-});
-
-add_task(async function test_missing_extension() {
-  // We don't clear scalars between tests to ensure the scalar gets set
-  // to the new value, rather than added.
-
-  let extensionId = extension.id;
-  // Remove the extension, this won't remove the search engine because we've
-  // stubbed removeEngine.
-  await extension.unload();
-
-  await Services.search.runBackgroundChecks();
-
-  TelemetryTestUtils.assertKeyedScalar(
-    TelemetryTestUtils.getProcessScalars("parent", true, true),
-    "browser.searchinit.engine_invalid_webextension",
-    extensionId,
-    1
-  );
-
-  await oldRemoveEngineFunc(Services.search.getEngineByName("Example"));
 });
 
 add_task(async function test_user_engine() {
@@ -203,5 +78,130 @@ add_task(async function test_policy_engine() {
     scalars,
     {},
     "Should not have recorded any issues for a policy defined engine"
+  );
+});
+
+async function check_with_extension(
+  runTest,
+  expectedScalarValue,
+  postTest = () => {},
+  skipUnload = false
+) {
+  extension = await SearchTestUtils.installSearchExtension();
+  let extensionId = extension.id;
+
+  Services.telemetry.clearScalars();
+
+  let engine = Services.search.getEngineByName("Example");
+  Assert.ok(engine, "Should have installed the engine");
+  // The test might cause an engine to be removed, but we don't want to
+  // do that here as we want that tested via runBackgroundChecks.
+  Services.search.wrappedJSObject.removeEngine = () => {};
+  await runTest(engine);
+
+  Services.search.wrappedJSObject.removeEngine = oldRemoveEngineFunc;
+  await Services.search.runBackgroundChecks();
+
+  let scalars = TelemetryTestUtils.getProcessScalars("parent", true, true);
+  if (!expectedScalarValue) {
+    Assert.deepEqual(scalars, {}, "Should not have recorded any issues");
+  } else {
+    TelemetryTestUtils.assertKeyedScalar(
+      scalars,
+      "browser.searchinit.engine_invalid_webextension",
+      extensionId,
+      expectedScalarValue
+    );
+  }
+
+  await postTest(engine);
+
+  if (!skipUnload) {
+    await extension.unload();
+  }
+  Assert.ok(
+    !Services.search.getEngineByName("Example"),
+    "Should have been removed"
+  );
+}
+
+add_task(async function test_valid_extensions_do_nothing() {
+  await check_with_extension(
+    () => {},
+    null,
+    async () => {
+      let scalars = TelemetryTestUtils.getProcessScalars("parent", true, true);
+      Assert.deepEqual(scalars, {}, "Should not have recorded any issues");
+    }
+  );
+});
+
+add_task(async function test_different_name() {
+  await check_with_extension(
+    async engine => {
+      engine.wrappedJSObject._name = "Example Test";
+    },
+    5,
+    async engine => {
+      engine.wrappedJSObject._name = "Example";
+    }
+  );
+});
+
+add_task(async function test_different_url() {
+  await check_with_extension(
+    async engine => {
+      engine.wrappedJSObject._urls = [];
+      engine.wrappedJSObject._setUrls({
+        search_url: "https://example.com/123",
+        search_url_get_params: "?q={searchTerms}",
+      });
+    },
+    6,
+    async () => {}
+  );
+});
+
+add_task(async function test_extension_no_longer_specifies_engine() {
+  await check_with_extension(async () => {
+    await extension.upgrade({
+      useAddonManager: "permanent",
+      manifest: {
+        version: "2.0",
+        applications: {
+          gecko: {
+            id: "example@tests.mozilla.org",
+          },
+        },
+      },
+    });
+  }, 4);
+});
+
+add_task(async function test_disabled_extension() {
+  await check_with_extension(
+    async () => {
+      await extension.addon.disable();
+    },
+    2,
+    async () => {
+      extension.addon.enable();
+      await extension.awaitStartup();
+      await AddonTestUtils.waitForSearchProviderStartup(extension);
+    }
+  );
+});
+
+add_task(async function test_missing_extension() {
+  await check_with_extension(
+    async () => {
+      await extension.unload();
+    },
+    1,
+    async engine => {
+      // Make the test harness happy.
+      await Services.search.removeEngine(engine);
+    },
+    true
   );
 });
