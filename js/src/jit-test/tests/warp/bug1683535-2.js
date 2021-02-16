9bb3b1cc2337a3df72cb8e4689c6e4f0fce457c0
Iain Ireland <iireland@mozilla.com>
2021-02-16 17:26:31 +0000

    Bug 1691883: Check for specific instructions in canTruncate r=jandem,nbp
    I originally added the operandTruncateKind check in canTruncate because MMod and MDiv will always force bailouts for their operands. This runs into problems, because some other implementations of operandTruncateKind rely on state that is not set until later. (Some ops call `truncateKind()`, which is set by the `needsTruncation()` implementation for that op. MCompare uses `truncateOperands_`, which is set by `MCompare::truncate`.
    
    After several attempts, I think the simplest option is to check specifically for MDiv and MMod in `canTruncate`. These are the only two instructions that can return `TruncateAfterBailouts` from `operandTruncateKind` without using `TruncateAfterBailouts` themselves.
    
    I've also reduced the fuzz test somewhat.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105059

--- js/src/jit-test/tests/warp/bug1683535-2.js
+++ js/src/jit-test/tests/warp/bug1683535-2.js
@@ -1,10 +1,12 @@
-function testMathyFunction(f, inputs) {
-  var results = [];
-  for (var j = 0; j < inputs.length; ++j) 
-    for (var k = 0; k < inputs.length; ++k) 
-      results.push(f(inputs[j], inputs[k]));
+function foo(y) {
+    return (1 >>> y >> 12) % 1.5 >>> 0
 }
-mathy0 = (function(x, y) {
-  return (Math.clz32((x <= x) >>> y) >> (~(0x080000000 >>> 0))) % Math.acos(~(2 ** 53)) >>> 0
-});
-testMathyFunction(mathy0, [1, 42, 0 / 0, 1 / 0, -Number.MIN_SAFE_INTEGER, -(2 ** 53), (2 ** 53), 1.7976931348623157e308]);
+function bar(y) {
+    return (1 >>> y >> 12) / 1.5 >>> 0
+}
+
+with ({}) {}
+for (var i = 0; i < 100; i++) {
+    foo(1);
+    bar(1);
+}
