6720ad2ca70711c745f26368b19172818b86462f
Markus Stange <mstange.moz@gmail.com>
2021-02-16 22:55:18 +0000

    Bug 1692391 - Clean up nsObjCExceptions.h a little bit, and use -[NSException callStackSymbols] instead of manual symbolication. r=haik
    Differential Revision: https://phabricator.services.mozilla.com/D104951

--- xpcom/base/nsObjCExceptions.h
+++ xpcom/base/nsObjCExceptions.h
@@ -11,127 +11,15 @@
 #undef try
 #undef catch
 
-#import <Foundation/Foundation.h>
-
-#ifdef DEBUG
-#  import <ExceptionHandling/NSExceptionHandler.h>
-#endif
-
-#if defined(__cplusplus)
-#  include "nsICrashReporter.h"
-#  include "nsCOMPtr.h"
-#  include "nsServiceManagerUtils.h"
-#endif
-
-#include <unistd.h>
-#include <signal.h>
-#include "nsError.h"
-
-// Undo the damage that exception_defines.h does.
-#undef try
-#undef catch
+@class NSException;
 
 /* NOTE: Macros that claim to abort no longer abort, see bug 486574.
- * If you actually want to log and abort, call "nsObjCExceptionLogAbort"
- * from an exception handler. At some point we will fix this by replacing
- * all macros in the tree with appropriately-named macros.
  */
 
 // See Mozilla bug 163260.
 // This file can only be included in an Objective-C context.
 
-__attribute__((unused)) static void nsObjCExceptionLog(NSException* aException) {
-  NSLog(@"Mozilla has caught an Obj-C exception [%@: %@]", [aException name], [aException reason]);
-
-#if defined(__cplusplus)
-  // Attach exception info to the crash report.
-  nsCOMPtr<nsICrashReporter> crashReporter = do_GetService("@mozilla.org/toolkit/crash-reporter;1");
-  if (crashReporter) {
-    crashReporter->AppendObjCExceptionInfoToAppNotes(static_cast<void*>(aException));
-  }
-#endif
-
-#ifdef DEBUG
-  @try {
-    // Try to get stack information out of the exception. 10.5 returns the stack
-    // info with the callStackReturnAddresses selector.
-    NSArray* stackTrace = nil;
-    if ([aException respondsToSelector:@selector(callStackReturnAddresses)]) {
-      NSArray* addresses =
-          (NSArray*)[aException performSelector:@selector(callStackReturnAddresses)];
-      if ([addresses count]) {
-        stackTrace = addresses;
-      }
-    }
-
-    // 10.4 doesn't respond to callStackReturnAddresses so we'll try to pull the
-    // stack info out of the userInfo. It might not be there, sadly :(
-    if (!stackTrace) {
-      stackTrace = [[aException userInfo] objectForKey:NSStackTraceKey];
-    }
-
-    if (stackTrace) {
-      // The command line should look like this:
-      //   /usr/bin/atos -p <pid> -printHeader <stack frame addresses>
-      NSMutableArray* args = [NSMutableArray arrayWithCapacity:[stackTrace count] + 3];
-
-      [args addObject:@"-p"];
-      int pid = [[NSProcessInfo processInfo] processIdentifier];
-      [args addObject:[NSString stringWithFormat:@"%d", pid]];
-
-      [args addObject:@"-printHeader"];
-
-      unsigned int stackCount = [stackTrace count];
-      unsigned int stackIndex = 0;
-      for (; stackIndex < stackCount; stackIndex++) {
-        unsigned long address = [[stackTrace objectAtIndex:stackIndex] unsignedLongValue];
-        [args addObject:[NSString stringWithFormat:@"0x%lx", address]];
-      }
-
-      NSPipe* outPipe = [NSPipe pipe];
-
-      NSTask* task = [[NSTask alloc] init];
-      [task setLaunchPath:@"/usr/bin/atos"];
-      [task setArguments:args];
-      [task setStandardOutput:outPipe];
-      [task setStandardError:outPipe];
-
-      NSLog(@"Generating stack trace for Obj-C exception...");
-
-      // This will throw an exception if the atos tool cannot be found, and in
-      // that case we'll just hit our @catch block below.
-      [task launch];
-
-      [task waitUntilExit];
-      [task release];
-
-      NSData* outData = [[outPipe fileHandleForReading] readDataToEndOfFile];
-      NSString* outString = [[NSString alloc] initWithData:outData encoding:NSUTF8StringEncoding];
-
-      NSLog(@"Stack trace:\n%@", outString);
-
-      [outString release];
-    } else {
-      NSLog(@"<No stack information available for Obj-C exception>");
-    }
-  } @catch (NSException* exn) {
-    NSLog(@"Failed to generate stack trace for Obj-C exception [%@: %@]", [exn name], [exn reason]);
-  }
-#endif
-}
-
-__attribute__((unused)) static void nsObjCExceptionAbort() {
-  // We need to raise a mach-o signal here, the Mozilla crash reporter on
-  // Mac OS X does not respond to POSIX signals. Raising mach-o signals directly
-  // is tricky so we do it by just derefing a null pointer.
-  int* foo = nullptr;
-  *foo = 1;
-}
-
-__attribute__((unused)) static void nsObjCExceptionLogAbort(NSException* aException) {
-  nsObjCExceptionLog(aException);
-  nsObjCExceptionAbort();
-}
+void nsObjCExceptionLog(NSException* aException);
 
 #define NS_OBJC_TRY(_e, _fail)    \
   @try {                          \
