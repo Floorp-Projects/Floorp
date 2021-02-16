928da04e8931dea7c1fa063134c01bfd820496f0
Calixte Denizet <cdenizet@mozilla.com>
2021-02-16 13:16:39 +0000

    Bug 1676708 -- Parse a date with GMT+00xx leads to an incorrect timezone offset r=jorendorff
    The MDN documentation specifies that 00xx stands for 0 hours and xx minutes.
    So that patch adds a check with the length of the offset to be sure that minutes won't become hours.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96755

---
+++ js/src/tests/non262/Date/parse-time-zone.js
@@ -0,0 +1,10 @@
+// |reftest| skip-if(xulRuntime.OS=="WINNT") -- Windows doesn't accept IANA names for the TZ env variable
+
+// bug 1676708
+inTimeZone("Europe/London", () => {
+  let dt = new Date("Wed Nov 11 2020 19:18:50 GMT+0010");
+  assertEq(dt.getTime(), new Date(2020, Month.November, 11, 19, 08, 50).getTime());
+});
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
