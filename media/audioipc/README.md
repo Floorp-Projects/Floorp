00a601715e15b7ade2d05fbc96703f61fe740866
Matthew Gregan <kinetik@flim.org>
2021-02-17 20:23:46 +0000

    Bug 1693270 - Switch audioipc-2 to vendored code.  r=cubeb-reviewers,padenot
    Differential Revision: https://phabricator.services.mozilla.com/D105453

--- media/audioipc/README.md
+++
@@ -1,1 +0,0 @@
-# Cubeb Audio Remoting Prototype
---
+++ third_party/rust/audioipc/build.rs
@@ -0,0 +1,7 @@
+use cc;
+
+fn main() {
+    if std::env::var_os("CARGO_CFG_UNIX").is_some() {
+        cc::Build::new().file("src/cmsghdr.c").compile("cmsghdr");
+    }
+}
