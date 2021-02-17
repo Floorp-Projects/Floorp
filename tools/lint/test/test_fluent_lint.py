e69e9a820d6c6fd88cd592338af7bbb620445003
Greg Tatum <gtatum@mozilla.com>
2021-02-17 21:02:48 +0000

    Bug 1691751 - Add whitespace rules for the Fluent linter; r=flod,dminor,fluent-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D105369

--- tools/lint/test/test_fluent_lint.py
+++ tools/lint/test/test_fluent_lint.py
@@ -46,5 +46,66 @@ def test_lint_single_file(lint, paths):
     assert results[8].column == 18
 
 
+def test_comment_group(lint, paths):
+    results = lint(paths("comment-group1.ftl"))
+    assert len(results) == 6
+    assert results[0].rule == "GC03"
+    assert results[0].lineno == 12
+    assert results[0].column == 1
+    assert results[1].rule == "GC02"
+    assert results[1].lineno == 16
+    assert results[1].column == 1
+    assert results[2].rule == "GC04"
+    assert results[2].lineno == 21
+    assert results[2].column == 1
+    assert results[3].rule == "GC03"
+    assert results[3].lineno == 26
+    assert results[3].column == 1
+    assert results[4].rule == "GC02"
+    assert results[4].lineno == 30
+    assert results[4].column == 1
+    assert results[5].rule == "GC01"
+    assert results[5].lineno == 35
+    assert results[5].column == 1
+
+    results = lint(paths("comment-group2.ftl"))
+    assert (len(results)) == 0
+
+
+def test_comment_resource(lint, paths):
+    results = lint(paths("comment-resource1.ftl"))
+    assert len(results) == 1
+    assert results[0].rule == "RC01"
+    assert results[0].lineno == 9
+    assert results[0].column == 1
+
+    results = lint(paths("comment-resource2.ftl"))
+    assert len(results) == 1
+    assert results[0].rule == "RC03"
+    assert results[0].lineno == 4
+    assert results[0].column == 1
+
+    results = lint(paths("comment-resource3.ftl"))
+    assert len(results) == 1
+    assert results[0].rule == "RC02"
+    assert results[0].lineno == 5
+    assert results[0].column == 1
+
+    results = lint(paths("comment-resource4.ftl"))
+    assert len(results) == 1
+    assert results[0].rule == "RC03"
+    assert results[0].lineno == 6
+    assert results[0].column == 1
+
+    results = lint(paths("comment-resource5.ftl"))
+    assert len(results) == 1
+    assert results[0].rule == "RC02"
+    assert results[0].lineno == 5
+    assert results[0].column == 1
+
+    results = lint(paths("comment-resource6.ftl"))
+    assert len(results) == 0
+
+
 if __name__ == "__main__":
     mozunit.main()
