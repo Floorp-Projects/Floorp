3bc7ba7a90761f83e9d07d8d34b96d23ee343b66
Tom Tung <ttung@mozilla.com>
2021-02-17 07:45:13 +0000

    Bug 1665942 - Collect userContextId from tab in _collectBaseTabData and stop collecting it in the session history collection; r=nika,geckoview-reviewers,agi
    ### Story
    
    When a COOP+COEP page is opened, we change the remoteness of the tab. After
    changing the remoteness, the tab is then restored by its TabState. However,
    we haven't collected userContextId through the current mechanism.
    
    To fix that, this patch changes the way to collect userContextId.
    
    ### Test Plan
    
    Test: D98296
    
    Depends on D98296
    
    Differential Revision: https://phabricator.services.mozilla.com/D98468

--- devtools/client/responsive/test/browser/head.js
+++ devtools/client/responsive/test/browser/head.js
@@ -522,7 +522,6 @@ function getSessionHistory(browser) {
     const browsingContext = browser.browsingContext;
     const uri = browsingContext.currentWindowGlobal.documentURI.displaySpec;
     const history = browsingContext.sessionHistory;
-    const userContextId = browsingContext.originAttributes.userContextId;
     const body = ContentTask.spawn(browser, browsingContext, function(
       // eslint-disable-next-line no-shadow
       browsingContext
@@ -536,7 +535,7 @@ function getSessionHistory(browser) {
     const { SessionHistory } = ChromeUtils.import(
       "resource://gre/modules/sessionstore/SessionHistory.jsm"
     );
-    return SessionHistory.collectFromParent(uri, body, history, userContextId);
+    return SessionHistory.collectFromParent(uri, body, history);
     /* eslint-enable no-undef */
   }
   return ContentTask.spawn(browser, null, function() {
