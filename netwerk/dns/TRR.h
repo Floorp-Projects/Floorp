c6147db848e0c7226dc0c5c57bcdef7c5d37aed1
Valentin Gosu <valentin.gosu@gmail.com>
2021-02-17 10:26:39 +0000

    Bug 1691933 - Increase timeout for TRR confirmation request r=dragana,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D104677

--- netwerk/dns/TRR.h
+++ netwerk/dns/TRR.h
@@ -101,6 +101,8 @@ class TRR : public Runnable,
   RefPtr<nsHostRecord> mRec;
   RefPtr<AHostResolver> mHostResolver;
 
+  void SetTimeout(uint32_t aTimeoutMs) { mTimeoutMs = aTimeoutMs; }
+
  protected:
   virtual ~TRR() = default;
   virtual DNSPacket* GetOrCreateDNSPacket();
@@ -140,6 +142,10 @@ class TRR : public Runnable,
   bool mFailed = false;
   bool mPB;
   DOHresp mDNS;
+
+  // The request timeout in milliseconds. If 0 we will use the default timeout
+  // we get from the prefs.
+  uint32_t mTimeoutMs = 0;
   nsCOMPtr<nsITimer> mTimeout;
   nsCString mCname;
   uint32_t mCnameLoop = kCnameChaseMax;  // loop detection counter
