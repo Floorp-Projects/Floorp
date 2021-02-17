1197ac26ba6246c2351608685be90aa747b2e04d
Patrick Storz <eduard.braun2@gmx.de>
2021-02-17 21:27:38 +0000

    Bug 1009728 - Implement "Close Tabs to the Left" r=jaws
    With "Close Tabs to the Right" having moved into a submenu in r533105,
    it's only logical to offer this functionality as well for having feature parity.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104317

--- browser/base/content/test/tabs/browser_removeTabsToTheEnd.js
+++ browser/base/content/test/tabs/browser_removeTabsToTheEnd.js
@@ -2,26 +2,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function test() {
+add_task(async function removeTabsToTheEnd() {
   // Add two new tabs after the original tab. Pin the first one.
   let originalTab = gBrowser.selectedTab;
-  let newTab1 = BrowserTestUtils.addTab(gBrowser);
-  BrowserTestUtils.addTab(gBrowser);
-  gBrowser.pinTab(newTab1);
+  let pinnedTab = await addTab();
+  let lastTab = await addTab();
+  gBrowser.pinTab(pinnedTab);
 
   // Check that there is only one closable tab from originalTab to the end
   is(
     gBrowser.getTabsToTheEndFrom(originalTab).length,
     1,
-    "One unpinned tab to the right"
+    "One unpinned tab towards the end"
   );
 
   // Remove tabs to the end
   gBrowser.removeTabsToTheEndFrom(originalTab);
-  is(gBrowser.tabs.length, 2, "Length is 2");
-  is(gBrowser.tabs[1], originalTab, "Starting tab is not removed");
-  is(gBrowser.tabs[0], newTab1, "Pinned tab is not removed");
 
-  // Remove pinned tab
-  gBrowser.removeTab(newTab1);
-}
+  ok(!originalTab.closing, "Original tab is not closing");
+  ok(!pinnedTab.closing, "Pinned tab is not closing");
+  ok(lastTab.closing, "Last tab is closing");
+
+  // cleanup
+  BrowserTestUtils.removeTab(pinnedTab);
+});
