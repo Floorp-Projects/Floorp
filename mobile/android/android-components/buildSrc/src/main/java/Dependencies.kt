b1b88754e19f3efbc28f3b954ee935af3832d5e1
Sebastian Kaspari <s.kaspari@gmail.com>
2021-02-18 16:04:41 +0100

    [components] Update Kotlin to 1.4.30.

--- mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@
 
 // Synchronized version numbers for dependencies used by (some) modules
 object Versions {
-    const val kotlin = "1.4.21"
+    const val kotlin = "1.4.30"
     const val coroutines = "1.4.2"
 
     const val junit = "4.12"
