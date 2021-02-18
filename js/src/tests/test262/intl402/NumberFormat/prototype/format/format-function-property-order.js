afdffbcc0d7e7b2dc29a020a64762eefbdbbdf93
André Bargull <andre.bargull@gmail.com>
2021-02-18 10:59:25 +0000

    Bug 1670055 - Part 1: Update test262. r=yulia
    Differential Revision: https://phabricator.services.mozilla.com/D105458

---
+++ js/src/tests/test262/intl402/NumberFormat/prototype/format/format-function-property-order.js
@@ -0,0 +1,18 @@
+// Copyright (C) 2020 ExE Boss. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: sec-createbuiltinfunction
+description: NumberFormat bound format function property order
+info: |
+  Set order: "length", "name"
+includes: [compareArray.js]
+---*/
+
+var formatFn = new Intl.NumberFormat().format;
+
+assert.compareArray(
+  Object.getOwnPropertyNames(formatFn),
+  ['length', 'name']
+);
+
+reportCompare(0, 0);
