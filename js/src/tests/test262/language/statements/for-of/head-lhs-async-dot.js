5963a8dad705546e292ec7e12a0b505bafb180e6
André Bargull <andre.bargull@gmail.com>
2021-02-18 10:59:25 +0000

    Bug 1670055 - Part 1: Update test262. r=yulia
    Differential Revision: https://phabricator.services.mozilla.com/D105458

---
+++ js/src/tests/test262/language/statements/for-of/head-lhs-async-dot.js
@@ -0,0 +1,17 @@
+// Copyright (C) 2021 Kevin Gibbons. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: leading `async` token in for-of LHS
+info: |
+  The `async` token is allowed in the LHS if not followed by `of`
+esid: sec-for-in-and-for-of-statements
+---*/
+
+var async = { x: 0 };
+
+for (async.x of [1]) ;
+
+assert.sameValue(async.x, 1);
+
+reportCompare(0, 0);
