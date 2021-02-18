ccd9db9859590dc17bd305d10377edcb2aeb56f7
Nihanth Subramanya <nhnt11@gmail.com>
2021-02-18 14:34:03 +0000

    Bug 1693527 - When migrating old TRR mode, don't clear network.trr.mode if its value is 5. r=valentin
    Differential Revision: https://phabricator.services.mozilla.com/D105625

--- browser/components/doh/test/browser/browser_trrMode_migration.js
+++ browser/components/doh/test/browser/browser_trrMode_migration.js
@@ -30,4 +30,27 @@ add_task(async function testTRRModeMigration() {
     false,
     "TRR mode cleared."
   );
+
+  await DoHController._uninit();
+  Preferences.set(prefs.NETWORK_TRR_MODE_PREF, 5);
+  Preferences.set(prefs.PREVIOUS_TRR_MODE_PREF, 0);
+  previousModePromise = TestUtils.waitForPrefChange(
+    prefs.PREVIOUS_TRR_MODE_PREF
+  );
+  await DoHController.init();
+  try {
+    await TestUtils.waitForCondition(
+      () => !Preferences.isSet(prefs.NETWORK_TRR_MODE_PREF)
+    );
+    ok(false, "TRR mode should not have been cleared after migration");
+  } catch (e) {
+    ok(true, "TRR mode wasn't cleared after migration");
+  }
+  await previousModePromise;
+  is(
+    Preferences.get(prefs.PREVIOUS_TRR_MODE_PREF),
+    undefined,
+    "Previous TRR mode pref cleared."
+  );
+  is(Preferences.get(prefs.NETWORK_TRR_MODE_PREF), 5, "TRR mode remained 5.");
 });
