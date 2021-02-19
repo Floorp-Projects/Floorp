3ded97282f3d4db1782c3a84af1159a85fb2f7fb
Valentin Gosu <valentin.gosu@gmail.com>
2021-02-19 17:02:34 +0000

    Bug 1264117 - Limit length of hostnames to 253 characters r=nhnt11,necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D105609

--- netwerk/test/unit/test_dns_service.js
+++ netwerk/test/unit/test_dns_service.js
@@ -9,6 +9,10 @@ const threadManager = Cc["@mozilla.org/thread-manager;1"].getService(
 );
 const mainThread = threadManager.currentThread;
 
+const overrideService = Cc[
+  "@mozilla.org/network/native-dns-override;1"
+].getService(Ci.nsINativeDNSResolverOverride);
+
 class Listener {
   constructor() {
     this.promise = new Promise(resolve => {
@@ -66,3 +70,82 @@ add_task(async function test_idn_cname() {
   inRecord.QueryInterface(Ci.nsIDNSAddrRecord);
   Assert.equal(inRecord.canonicalName, ACE_IDN, "IDN is returned as punycode");
 });
+
+add_task(
+  {
+    skip_if: () =>
+      Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
+        .processType != Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT,
+  },
+  async function test_long_domain() {
+    let listener = new Listener();
+    let domain = "a".repeat(253);
+    overrideService.addIPOverride(domain, "1.2.3.4");
+    dns.asyncResolve(
+      domain,
+      Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
+      Ci.nsIDNSService.RESOLVE_CANONICAL_NAME,
+      null, // resolverInfo
+      listener,
+      mainThread,
+      defaultOriginAttributes
+    );
+    let [, , inStatus] = await listener;
+    Assert.equal(inStatus, Cr.NS_OK);
+
+    listener = new Listener();
+    domain = "a".repeat(254);
+    overrideService.addIPOverride(domain, "1.2.3.4");
+
+    Services.prefs.setBoolPref("network.dns.limit_253_chars", true);
+
+    if (mozinfo.socketprocess_networking) {
+      // When using the socket process, the call fails asynchronously.
+      dns.asyncResolve(
+        domain,
+        Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
+        Ci.nsIDNSService.RESOLVE_CANONICAL_NAME,
+        null, // resolverInfo
+        listener,
+        mainThread,
+        defaultOriginAttributes
+      );
+      let [, , inStatus] = await listener;
+      Assert.equal(inStatus, Cr.NS_ERROR_UNKNOWN_HOST);
+    } else {
+      Assert.throws(
+        () => {
+          dns.asyncResolve(
+            domain,
+            Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
+            Ci.nsIDNSService.RESOLVE_CANONICAL_NAME,
+            null, // resolverInfo
+            listener,
+            mainThread,
+            defaultOriginAttributes
+          );
+        },
+        /NS_ERROR_UNKNOWN_HOST/,
+        "Should throw for large domains"
+      );
+    }
+
+    listener = new Listener();
+    domain = "a".repeat(254);
+    Services.prefs.setBoolPref("network.dns.limit_253_chars", false);
+    dns.asyncResolve(
+      domain,
+      Ci.nsIDNSService.RESOLVE_TYPE_DEFAULT,
+      Ci.nsIDNSService.RESOLVE_CANONICAL_NAME,
+      null, // resolverInfo
+      listener,
+      mainThread,
+      defaultOriginAttributes
+    );
+    [, , inStatus] = await listener;
+    Assert.equal(inStatus, Cr.NS_OK);
+
+    Services.prefs.clearUserPref("network.dns.limit_253_chars");
+    overrideService.clearOverrides();
+  }
+);
