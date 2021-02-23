dd4e506bfeadd4dac353db4f04326127e67adf24
Matt Woodrow <mwoodrow@mozilla.com>
2021-02-23 02:18:24 +0000

    Bug 1694248 - Disable OMTP. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D106053

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4398,11 +4398,7 @@ pref("toolkit.aboutProcesses.showThreads", false);
   pref("toolkit.crashreporter.include_context_heap", true);
 #endif
 
-#if defined(XP_WIN) || defined(XP_MACOSX) || defined(MOZ_WIDGET_GTK)
-  pref("layers.omtp.enabled", true);
-#else
-  pref("layers.omtp.enabled", false);
-#endif
+pref("layers.omtp.enabled", false);
 
 // Support for legacy customizations that rely on checking the
 // user profile directory for these stylesheets:
