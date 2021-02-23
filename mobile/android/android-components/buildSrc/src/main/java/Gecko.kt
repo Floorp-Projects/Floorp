75a725e0bc5ddff4d272b91ada10113645a08032
Sebastian Kaspari <s.kaspari@gmail.com>
2021-02-23 15:48:48 +0100

    [components] Issue https://github.com/mozilla-mobile/android-components/issues/9746: (Merge day) browser-engine-gecko-nightly -> 88

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -6,7 +6,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Nightly Version.
      */
-    const val nightly_version = "87.0.20210222084406"
+    const val nightly_version = "88.0.20210223085042"
 
     /**
      * GeckoView Beta Version.
