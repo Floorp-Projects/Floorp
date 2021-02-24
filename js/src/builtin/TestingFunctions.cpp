a40c3d117c23dff093f882e54e4cfd9d0eccf567
Tooru Fujisawa <arai_a@mac.com>
2021-02-24 04:22:37 +0000

    Bug 1692577 - Part 14: Use frontend::CompileGlobalScriptToExtensibleStencil in CompileStencilXDR. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D105906

--- js/src/builtin/TestingFunctions.cpp
+++ js/src/builtin/TestingFunctions.cpp
@@ -5003,17 +5003,19 @@ static bool CompileStencilXDR(JSContext* cx, uint32_t argc, Value* vp) {
 
   Rooted<frontend::CompilationInput> input(cx,
                                            frontend::CompilationInput(options));
-  UniquePtr<frontend::CompilationStencil> stencil =
-      frontend::CompileGlobalScriptToStencil(cx, input.get(), srcBuf,
-                                             ScopeKind::Global);
+  auto stencil = frontend::CompileGlobalScriptToExtensibleStencil(
+      cx, input.get(), srcBuf, ScopeKind::Global);
   if (!stencil) {
     return false;
   }
 
   /* Serialize the stencil to XDR. */
   JS::TranscodeBuffer xdrBytes;
-  if (!stencil->serializeStencils(cx, input.get(), xdrBytes)) {
-    return false;
+  {
+    frontend::BorrowingCompilationStencil borrowingStencil(*stencil);
+    if (!borrowingStencil.serializeStencils(cx, input.get(), xdrBytes)) {
+      return false;
+    }
   }
 
   /* Dump the bytes into a javascript ArrayBuffer and return a UInt8Array. */
