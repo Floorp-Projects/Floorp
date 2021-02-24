fa86a20ce27f540a3904f42991b844edec70841c
Nan Jiang <njiang028@gmail.com>
2021-02-24 16:57:27 +0000

    Bug 1693393 - add telemetry for sponsored TopSites in Urlbar r=dao,harry
    Differential Revision: https://phabricator.services.mozilla.com/D105639

---
+++ browser/modules/test/unit/test_PartnerLinkAttribution.js
@@ -0,0 +1,52 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+"use strict";
+
+const {
+  PartnerLinkAttribution,
+  CONTEXTUAL_SERVICES_PING_TYPES,
+} = ChromeUtils.import("resource:///modules/PartnerLinkAttribution.jsm");
+
+const { sinon } = ChromeUtils.import("resource://testing-common/Sinon.jsm");
+
+const FAKE_PING = { tile_id: 1, position: 1 };
+
+let sandbox;
+let stub;
+
+add_task(function setup() {
+  sandbox = sinon.createSandbox();
+  stub = sandbox.stub(
+    PartnerLinkAttribution._pingCentre,
+    "sendStructuredIngestionPing"
+  );
+  stub.returns(200);
+
+  registerCleanupFunction(() => {
+    sandbox.restore();
+  });
+});
+
+add_task(function test_sendContextualService_success() {
+  for (const type of Object.values(CONTEXTUAL_SERVICES_PING_TYPES)) {
+    PartnerLinkAttribution.sendContextualServicesPing(FAKE_PING, type);
+
+    Assert.ok(stub.calledOnce, `Should send the ping for ${type}`);
+
+    const [payload, endpoint] = stub.firstCall.args;
+    Assert.ok(!!payload.context_id, "Should add context_id to the payload");
+    Assert.ok(
+      endpoint.includes(type),
+      "Should include the ping type in the endpoint URL"
+    );
+    stub.resetHistory();
+  }
+});
+
+add_task(function test_rejectUnknownPingType() {
+  PartnerLinkAttribution.sendContextualServicesPing(FAKE_PING, "unknown-type");
+
+  Assert.ok(stub.notCalled, "Should not send the ping with unknown ping type");
+});
