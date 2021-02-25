b4aa071a778912f7e90ae37bb062e228326f14d5
Snehaa2296 <snehasai01@gmail.com>
2021-02-25 11:10:42 +0000

    Bug 1491055 - Remove JS_New and use JS::Construct in JS_New consumers. r=arai
    Differential Revision: https://phabricator.services.mozilla.com/D106257

--- dom/plugins/base/nsJSNPRuntime.cpp
+++ dom/plugins/base/nsJSNPRuntime.cpp
@@ -738,11 +738,12 @@ static bool doInvoke(NPObject* npobj, NPIdentifier method,
   bool ok = false;
 
   if (ctorCall) {
-    JSObject* newObj = ::JS_New(cx, jsobj, jsargs);
+    JS::Rooted<JS::Value> objVal(cx, JS::ObjectValue(*jsobj));
+    JS::Rooted<JSObject*> result(cx);
+    ok = JS::Construct(cx, objVal, jsargs, &result);
 
-    if (newObj) {
-      v.setObject(*newObj);
-      ok = true;
+    if (ok) {
+      v.setObject(*result);
     }
   } else {
     ok = ::JS_CallFunctionValue(cx, jsobj, fv, jsargs, &v);
