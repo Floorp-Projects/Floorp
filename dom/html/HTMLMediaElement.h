234b5d918106000aa5f4ce10ad159a40e6620cf0
alwu <alwu@mozilla.com>
2021-02-25 18:07:06 +0000

    Bug 1691578 - part5 : add test cases to test the ability of reporting error for not supported media type. r=bryce,emilio
    Add test cases for `DecoderDoctorDiagnostics::StoreFormatDiagnostics()`, which is used when checking the ability of whether the decoder is supported for certain type.
    
    Differential Revision: https://phabricator.services.mozilla.com/D104475

--- dom/html/HTMLMediaElement.h
+++ dom/html/HTMLMediaElement.h
@@ -22,6 +22,7 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/StateWatching.h"
 #include "mozilla/WeakPtr.h"
+#include "mozilla/dom/DecoderDoctorNotificationBinding.h"
 #include "mozilla/dom/HTMLMediaElementBinding.h"
 #include "mozilla/dom/MediaDebugInfoBinding.h"
 #include "mozilla/dom/MediaKeys.h"
@@ -653,6 +654,10 @@ class HTMLMediaElement : public nsGenericHTMLElement,
   double TotalPlayTime() const;
   double InvisiblePlayTime() const;
   double VideoDecodeSuspendedTime() const;
+
+  // Test methods for decoder doctor.
+  void SetFormatDiagnosticsReportForMimeType(const nsAString& aMimeType,
+                                             DecoderDoctorReportType aType);
 
   // Synchronously, return the next video frame and mark the element unable to
   // participate in decode suspending.
