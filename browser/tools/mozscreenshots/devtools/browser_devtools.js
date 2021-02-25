1ac3484028bc766375896a95e10f7f0e2f20cdf0
Julian Descottes <jdescottes@mozilla.com>
2021-02-25 12:58:14 +0000

    Bug 1694140 - [devtools] Stop using TargetFactory.forTab outside of devtools r=rpl,ochameau
    Depends on D106000
    
    Differential Revision: https://phabricator.services.mozilla.com/D106001

--- browser/tools/mozscreenshots/devtools/browser_devtools.js
+++ browser/tools/mozscreenshots/devtools/browser_devtools.js
@@ -7,7 +7,6 @@
 "use strict";
 
 const { require } = ChromeUtils.import("resource://devtools/shared/Loader.jsm");
-const { TargetFactory } = require("devtools/client/framework/target");
 const { gDevTools } = require("devtools/client/framework/devtools");
 
 add_task(async function capture() {
@@ -18,6 +17,5 @@ add_task(async function capture() {
 
   await TestRunner.start(sets, "devtools");
 
-  let target = await TargetFactory.forTab(gBrowser.selectedTab);
-  await gDevTools.closeToolbox(target);
+  await gDevTools.closeToolboxForTab(gBrowser.selectedTab);
 });
