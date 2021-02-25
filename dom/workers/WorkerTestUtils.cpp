ad3eca74a26f849a9425cffc7ad11cdbbbeca39f
Eden Chuang <echuang@mozilla.com>
2021-02-25 10:14:12 +0000

    Bug 1684139 - Adding mozilla specific wpt for timer nesting level in workers r=dom-worker-reviewers,asuth
    This patch is developed from D104136#3396152.
    
    This patch creates WorkerTestUtils.webidl under dom/webidl for testing workers with internal APIs. These APIs are exposed to workers only and controlled by dom.workers.testing.enabled pref.
    
    This patch creates a Mozilla-specific web-platform test, testing/web-platform/mozilla/test/workers/worker_timer_nesting_level.html, to test the timer nesting level implementation for workers.
    
    To simplify the test implementation, this patch does not implement the webidl under dom/chrome-webidl/ suggested by D104136#3396152.
    
    Depends on D104136
    
    Differential Revision: https://phabricator.services.mozilla.com/D105332

---
+++ dom/workers/WorkerTestUtils.cpp
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/ErrorResult.h"
+#include "mozilla/dom/WorkerPrivate.h"
+#include "mozilla/dom/WorkerTestUtils.h"
+
+namespace mozilla {
+namespace dom {
+
+uint32_t WorkerTestUtils::CurrentTimerNestingLevel(const GlobalObject& aGlobal,
+                                                   ErrorResult& aErr) {
+  MOZ_ASSERT(!NS_IsMainThread());
+  WorkerPrivate* worker = GetCurrentThreadWorkerPrivate();
+  MOZ_ASSERT(worker);
+  return worker->GetCurrentTimerNestingLevel();
+}
+
+}  // namespace dom
+}  // namespace mozilla
