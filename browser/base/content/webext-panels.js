4af3da178b0f85f70731c52d54bdf9b7448020a0
Tomislav Jovanovic <tomica@gmail.com>
2021-02-26 22:56:17 +0000

    Bug 1646817 - Part 1: Support DocumentChannel process switching in sidebars r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D100396

--- browser/base/content/webext-panels.js
+++ browser/base/content/webext-panels.js
@@ -67,6 +67,9 @@ function getBrowser(panel) {
         oa
       )
     );
+    // TODO: Bug 1646817.
+    // browser.setAttribute("maychangeremoteness", "true");
+
     readyPromise = promiseEvent(browser, "XULFrameLoaderCreated");
   } else {
     readyPromise = Promise.resolve();
@@ -101,7 +104,7 @@ function getBrowser(panel) {
     true
   );
 
-  return readyPromise.then(() => {
+  const initBrowser = () => {
     ExtensionParent.apiManager.emit(
       "extension-browser-inserted",
       browser,
@@ -120,7 +123,10 @@ function getBrowser(panel) {
         : {};
     browser.messageManager.sendAsyncMessage("Extension:InitBrowser", options);
     return browser;
-  });
+  };
+
+  browser.addEventListener("DidChangeBrowserRemoteness", initBrowser);
+  return readyPromise.then(initBrowser);
 }
 
 // Stub tabbrowser implementation for use by the tab-modal alert code.
