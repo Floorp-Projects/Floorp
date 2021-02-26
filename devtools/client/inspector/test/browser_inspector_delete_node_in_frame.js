5eb0757ce79c28a0585d981572ca577c789ef348
Julian Descottes <jdescottes@mozilla.com>
2021-02-26 10:16:36 +0000

    Bug 1694400 - [devtools] Fix infinite loop after deleting node in same-process frame r=nchevobbe
    Deleting a node with the inspector puts the corresponding NodeActor in the retained nodes, in case the user cancels the deletion.
    When a frame is unloaded we try to remove all the retained node to free memory.
    For each retained node, we try to climb up the window hierarchy to know if it should be released or not.
    However the implementation was buggy, relying on a while loop which was incorrectly mixing window objects and frame elements.
    This previously lead to errors, and now leads to infinite loops.
    
    A new test is added to cover this scenario.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106516

---
+++ devtools/client/inspector/test/browser_inspector_delete_node_in_frame.js
@@ -0,0 +1,33 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+const TEST_URL_1 = `http://example.org/document-builder.sjs?html=
+<iframe srcdoc="<div>"></iframe><body>`;
+const TEST_URL_2 = `http://example.org/document-builder.sjs?html=<div id=url2>`;
+
+// Test that deleting a node in a same-process iframe and doing a navigation
+// does not freeze the browser or break the toolbox.
+add_task(async function() {
+  const { inspector } = await openInspectorForURL(TEST_URL_1);
+
+  info("Select a node in a same-process iframe");
+  const node = await selectNodeInFrames(["iframe", "div"], inspector);
+  const parentNode = node.parentNode();
+
+  info("Removing selected element with the context menu.");
+  await deleteNodeWithContextMenu(node, inspector);
+
+  is(
+    inspector.selection.nodeFront,
+    parentNode,
+    "The parent node of the deleted node was selected."
+  );
+
+  const onInspectorReloaded = inspector.once("reloaded");
+  await navigateTo(TEST_URL_2);
+  await onInspectorReloaded;
+
+  const url2NodeFront = await getNodeFront("#url2", inspector);
+  ok(url2NodeFront, "Can retrieve a node front after the navigation");
+});
