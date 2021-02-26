8208bfad6fd47c3a5eacb801af28059491974eb1
Simon Giesecke <sgiesecke@mozilla.com>
2021-02-26 11:36:42 +0000

    Bug 1693541 - Improve uses of nsBaseHashtable and descendants and avoid multiple subsequent lookups in dom/media. r=bryce
    Differential Revision: https://phabricator.services.mozilla.com/D106095

--- dom/media/platforms/agnostic/eme/EMEDecoderModule.cpp
+++ dom/media/platforms/agnostic/eme/EMEDecoderModule.cpp
@@ -145,12 +145,12 @@ class EMEDecryptor : public MediaDataDecoder,
       return;
     }
 
-    mDecrypts.InsertOrUpdate(aSample,
-                             MakeUnique<DecryptPromiseRequestHolder>());
+    const auto& decrypt = mDecrypts.InsertOrUpdate(
+        aSample, MakeUnique<DecryptPromiseRequestHolder>());
     mProxy->Decrypt(aSample)
         ->Then(mThread, __func__, this, &EMEDecryptor::Decrypted,
                &EMEDecryptor::Decrypted)
-        ->Track(*mDecrypts.Get(aSample));
+        ->Track(*decrypt);
   }
 
   void Decrypted(const DecryptResult& aDecrypted) {
