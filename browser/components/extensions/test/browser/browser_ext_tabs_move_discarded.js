13d6eb64dbe7bb5ec4dfead6907f0ee5d5aa046c
Rob Wu <rob@robwu.nl>
2021-02-26 21:34:54 +0000

    Bug 1694699 - Minimize access to tab.hasTabPermission r=rpl,geckoview-reviewers,agi
    tab.hasTabPermission indirectly triggers an access to
    browser.currentURI, which for lazy tab browsers causes an incorrect
    value to be cached. To avoid this, skip the call to hasTabPermission.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106622

--- browser/components/extensions/test/browser/browser_ext_tabs_move_discarded.js
+++ browser/components/extensions/test/browser/browser_ext_tabs_move_discarded.js
@@ -2,7 +2,7 @@
 /* vim: set sts=2 sw=2 et tw=80: */
 "use strict";
 
-add_task(async function() {
+add_task(async function move_discarded_to_window() {
   let extension = ExtensionTestUtils.loadExtension({
     manifest: { permissions: ["tabs"] },
     background: async function() {
@@ -28,4 +28,55 @@ add_task(async function() {
   await extension.startup();
   await extension.awaitFinish("tabs.move");
   await extension.unload();
+});
+
+add_task(async function move_hidden_discarded_to_window() {
+  let extensionWithoutTabsPermission = ExtensionTestUtils.loadExtension({
+    manifest: {
+      permissions: ["http://example.com/"],
+    },
+    background() {
+      browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        if (changeInfo.hidden) {
+          browser.test.assertEq(
+            tab.url,
+            "http://example.com/?hideme",
+            "tab.url is correctly observed without tabs permission"
+          );
+          browser.test.sendMessage("onUpdated_checked");
+        }
+      });
+    },
+  });
+  await extensionWithoutTabsPermission.startup();
+
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: { permissions: ["tabs", "tabHide"] },
+    // ExtensionControlledPopup's populateDescription method requires an addon:
+    useAddonManager: "temporary",
+    async background() {
+      let url = "http://example.com/?hideme";
+      let tab = await browser.tabs.create({ url, discarded: true });
+      await browser.tabs.hide(tab.id);
+
+      let { id: windowId } = await browser.windows.create();
+
+      // Move the tab into that window
+      [tab] = await browser.tabs.move(tab.id, { windowId, index: -1 });
+      browser.test.assertTrue(tab.discarded, "Tab should still be discarded");
+      browser.test.assertTrue(tab.hidden, "Tab should still be hidden");
+      browser.test.assertEq(url, tab.url, "Tab URL should still be correct");
+
+      await browser.windows.remove(windowId);
+      browser.test.notifyPass("move_hidden_discarded_to_window");
+    },
+  });
+
+  await extension.startup();
+  await extension.awaitFinish("move_hidden_discarded_to_window");
+  await extension.unload();
+
+  await extensionWithoutTabsPermission.awaitMessage("onUpdated_checked");
+  await extensionWithoutTabsPermission.awaitMessage("onUpdated_checked");
+  await extensionWithoutTabsPermission.unload();
 });
