b26563e34d7b5e4eb75e11f4636afabc80289df6
Simon Giesecke <sgiesecke@mozilla.com>
2021-02-26 11:36:39 +0000

    Bug 1693541 - Improve uses of nsBaseHashtable and descendants and avoid multiple subsequent lookups in dom/plugins. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D106107

--- dom/plugins/ipc/PluginInstanceParent.cpp
+++ dom/plugins/ipc/PluginInstanceParent.cpp
@@ -1875,14 +1875,14 @@ void PluginInstanceParent::NPP_URLNotify(const char* url, NPReason reason,
 bool PluginInstanceParent::RegisterNPObjectForActor(
     NPObject* aObject, PluginScriptableObjectParent* aActor) {
   NS_ASSERTION(aObject && aActor, "Null pointers!");
-  NS_ASSERTION(!mScriptableObjects.Get(aObject, nullptr), "Duplicate entry!");
+  NS_ASSERTION(!mScriptableObjects.Contains(aObject), "Duplicate entry!");
   mScriptableObjects.InsertOrUpdate(aObject, aActor);
   return true;
 }
 
 void PluginInstanceParent::UnregisterNPObject(NPObject* aObject) {
   NS_ASSERTION(aObject, "Null pointer!");
-  NS_ASSERTION(mScriptableObjects.Get(aObject, nullptr), "Unknown entry!");
+  NS_ASSERTION(mScriptableObjects.Contains(aObject), "Unknown entry!");
   mScriptableObjects.Remove(aObject);
 }
 
