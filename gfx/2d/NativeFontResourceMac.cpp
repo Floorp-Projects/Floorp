a8e208c7648c912d15e679e3cfd302fc6356f6bd
Andi-Bogdan Postelnicu <bpostelnicu@mozilla.com>
2021-02-26 16:39:18 +0000

    Bug 1694161 - some fixes for the MacOS non unified environment. r=sg
    Depends on D105977
    
    Differential Revision: https://phabricator.services.mozilla.com/D106616

--- gfx/2d/NativeFontResourceMac.cpp
+++ gfx/2d/NativeFontResourceMac.cpp
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <unordered_map>
 #include <unordered_set>
 #include "NativeFontResourceMac.h"
 #include "UnscaledFontMac.h"
