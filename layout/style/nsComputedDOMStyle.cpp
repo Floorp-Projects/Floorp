d1b89f4cb3283abcc3c660901f44bcb453cde6c3
Mats Palmgren <mats@mozilla.com>
2021-03-01 16:14:33 +0000

    Bug 1485917 - Make <fieldset> with 'display:inline' support CSSOM width/height.  r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D106728

--- layout/style/nsComputedDOMStyle.cpp
+++ layout/style/nsComputedDOMStyle.cpp
@@ -839,8 +839,8 @@ static bool IsNonReplacedInline(nsIFrame* aFrame) {
   // doesn't apply to ruby boxes.
   return aFrame->StyleDisplay()->IsInlineFlow() &&
          !aFrame->IsFrameOfType(nsIFrame::eReplaced) &&
-         !aFrame->IsBlockFrame() && !aFrame->IsScrollFrame() &&
-         !aFrame->IsColumnSetWrapperFrame();
+         !aFrame->IsFieldSetFrame() && !aFrame->IsBlockFrame() &&
+         !aFrame->IsScrollFrame() && !aFrame->IsColumnSetWrapperFrame();
 }
 
 static Side SideForPaddingOrMarginOrInsetProperty(nsCSSPropertyID aPropID) {
