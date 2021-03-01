d3987c3285a44cb6d4a7a7faee5f861c5d5e7c4f
Edgar Chen <echen@mozilla.com>
2021-03-01 18:36:24 +0000

    Bug 1681819 - Report usecounter when worker is in killing state; r=asuth
    Previously, we report usecounter when clearing main event queue, but js execution
    could still happen on the microtask after that. So defer the reporting to worker
    is in killing state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106769

---
+++ dom/workers/test/file_use_counter_shared_worker_microtask.js
@@ -0,0 +1,12 @@
+onconnect = function(e) {
+  let port = e.ports[0];
+  port.onmessage = function(m) {
+    if (m.data === "RUN") {
+      queueMicrotask(() => {
+        console.log("worker runs");
+      });
+      port.postMessage("DONE");
+      close();
+    }
+  };
+};
