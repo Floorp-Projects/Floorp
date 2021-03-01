66b4545939087e4a9730b75468fd5f66f7317d9f
Chris Martin <cmartin@mozilla.com>
2021-03-01 21:59:49 +0000

    Bug 1658419 - Use shared memory for gamepad state on Windows r=handyman
    Finally, use the primitives from the previous change to deliver gamepad changes.
    
    If the shared memory shortcut is available, all gamepad changes will be
    delivered over it. When the children receive the signal, they will diff their
    last-known state against the new state and generate events to update JS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105129

--- dom/gamepad/GamepadHandle.h
+++ dom/gamepad/GamepadHandle.h
@@ -37,6 +37,7 @@ namespace mozilla::dom {
 
 class GamepadPlatformService;
 class GamepadServiceTest;
+class GamepadTestHelper;
 class XRInputSource;
 
 // The "kind" of a gamepad handle is based on which provider created it
@@ -73,6 +74,7 @@ class GamepadHandle {
   // create new handles and inspect their actual value
   friend class mozilla::dom::GamepadPlatformService;
   friend class mozilla::dom::GamepadServiceTest;
+  friend class mozilla::dom::GamepadTestHelper;
   friend class mozilla::dom::XRInputSource;
   friend class mozilla::gfx::VRDisplayClient;
   friend class mozilla::gfx::VRManager;
