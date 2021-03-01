a31d898d28cdba9f614cc3ff541c0f5349a2b3a2
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-01 09:59:33 +0000

    Bug 1693541 - Improve uses of nsBaseHashtable and descendants and avoid multiple subsequent lookups in xpcom. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D106094

--- xpcom/base/nsINIParser.cpp
+++ xpcom/base/nsINIParser.cpp
@@ -277,7 +277,7 @@ nsresult nsINIParser::RenameSection(const char* aSection,
     return NS_ERROR_INVALID_ARG;
   }
 
-  if (mSections.Get(aNewName, nullptr)) {
+  if (mSections.Contains(aNewName)) {
     return NS_ERROR_ILLEGAL_VALUE;
   }
 
