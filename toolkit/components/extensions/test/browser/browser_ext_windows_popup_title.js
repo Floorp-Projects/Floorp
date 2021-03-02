be835e654dfa0c43659d629fb3525db649638304
Robbendebiene <robbendebiene@mailbox.org>
2021-03-02 09:52:26 +0000

    Bug 1690611 - Add tests to check if windows.Window.title is correctly set depending on the given permissions r=robwu
    Differential Revision: https://phabricator.services.mozilla.com/D105126

--- toolkit/components/extensions/test/browser/browser_ext_windows_popup_title.js
+++ toolkit/components/extensions/test/browser/browser_ext_windows_popup_title.js
@@ -6,12 +6,10 @@ add_task(async function test_popup_title() {
   const name = "custom_title_number_9_please";
   const docTitle = "popup-test-title";
 
-  const extension = ExtensionTestUtils.loadExtension({
+  const extensionWithImplicitHostPermission = ExtensionTestUtils.loadExtension({
     manifest: {
       name,
-      permissions: ["tabs"],
     },
-
     async background() {
       let popup;
 
@@ -20,7 +18,6 @@ add_task(async function test_popup_title() {
         const name = browser.runtime.getManifest().name;
         const { id } = await popup;
         const { title } = await browser.windows.get(id);
-        browser.windows.remove(id);
 
         browser.test.assertTrue(
           title.includes(name),
@@ -35,7 +32,20 @@ add_task(async function test_popup_title() {
           "popup title must not include extension URL"
         );
 
-        browser.test.notifyPass("popup-window-title");
+        // share window data with other extensions
+        browser.test.sendMessage("windowData", {
+          id: id,
+          fullTitle: title,
+        });
+
+        browser.test.onMessage.addListener(async message => {
+          if (message === "cleanup") {
+            await browser.windows.remove(id);
+            browser.test.sendMessage("finishedCleanup");
+          }
+        });
+
+        browser.test.sendMessage("done");
       });
 
       popup = browser.windows.create({
@@ -56,7 +66,68 @@ add_task(async function test_popup_title() {
     },
   });
 
-  await extension.startup();
-  await extension.awaitFinish("popup-window-title");
-  await extension.unload();
+  const extensionWithoutPermissions = ExtensionTestUtils.loadExtension({
+    async background() {
+      const { id } = await new Promise(resolve => {
+        browser.test.onMessage.addListener(message => {
+          resolve(message);
+        });
+      });
+
+      const { title } = await browser.windows.get(id);
+
+      browser.test.assertEq(
+        title,
+        undefined,
+        "popup window must not include title"
+      );
+
+      browser.test.sendMessage("done");
+    },
+  });
+
+  const extensionWithTabsPermission = ExtensionTestUtils.loadExtension({
+    manifest: {
+      permissions: ["tabs"],
+    },
+    async background() {
+      const { id, fullTitle } = await new Promise(resolve => {
+        browser.test.onMessage.addListener(message => {
+          resolve(message);
+        });
+      });
+
+      const { title } = await browser.windows.get(id);
+
+      browser.test.assertEq(
+        title,
+        fullTitle,
+        "popup title equals expected title"
+      );
+
+      browser.test.sendMessage("done");
+    },
+  });
+
+  await extensionWithoutPermissions.startup();
+  await extensionWithTabsPermission.startup();
+  await extensionWithImplicitHostPermission.startup();
+
+  const windowData = await extensionWithImplicitHostPermission.awaitMessage(
+    "windowData"
+  );
+
+  extensionWithoutPermissions.sendMessage(windowData);
+  extensionWithTabsPermission.sendMessage(windowData);
+
+  await extensionWithoutPermissions.awaitMessage("done");
+  await extensionWithTabsPermission.awaitMessage("done");
+  await extensionWithImplicitHostPermission.awaitMessage("done");
+
+  extensionWithImplicitHostPermission.sendMessage("cleanup");
+  await extensionWithImplicitHostPermission.awaitMessage("finishedCleanup");
+
+  await extensionWithoutPermissions.unload();
+  await extensionWithTabsPermission.unload();
+  await extensionWithImplicitHostPermission.unload();
 });
