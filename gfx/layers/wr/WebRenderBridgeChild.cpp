05d89cff8ca86b5de103ff49ff06f025472d1438
smolnar <smolnar@mozilla.com>
2021-03-02 14:38:21 +0200

    Backed out changeset 473b37f1e3e2 (bug 1693541) for causing gfx crashes. a=backout

--- gfx/layers/wr/WebRenderBridgeChild.cpp
+++ gfx/layers/wr/WebRenderBridgeChild.cpp
@@ -260,73 +260,70 @@ Maybe<wr::FontInstanceKey> WebRenderBridgeChild::GetFontKeyForScaledFont(
   MOZ_ASSERT(aScaledFont);
   MOZ_ASSERT(aScaledFont->CanSerialize());
 
-  return mFontInstanceKeys.WithEntryHandle(
-      aScaledFont, [&](auto&& entry) -> Maybe<wr::FontInstanceKey> {
-        if (!entry) {
-          Maybe<wr::IpcResourceUpdateQueue> resources =
-              aResources ? Nothing() : Some(wr::IpcResourceUpdateQueue(this));
-          aResources = resources.ptrOr(aResources);
+  wr::FontInstanceKey instanceKey = {wr::IdNamespace{0}, 0};
+  if (mFontInstanceKeys.Get(aScaledFont, &instanceKey)) {
+    return Some(instanceKey);
+  }
 
-          Maybe<wr::FontKey> fontKey = GetFontKeyForUnscaledFont(
-              aScaledFont->GetUnscaledFont(), aResources);
-          if (fontKey.isNothing()) {
-            return Nothing();
-          }
+  Maybe<wr::IpcResourceUpdateQueue> resources =
+      aResources ? Nothing() : Some(wr::IpcResourceUpdateQueue(this));
+  aResources = resources.ptrOr(aResources);
 
-          wr::FontInstanceKey instanceKey = GetNextFontInstanceKey();
+  Maybe<wr::FontKey> fontKey =
+      GetFontKeyForUnscaledFont(aScaledFont->GetUnscaledFont(), aResources);
+  if (fontKey.isNothing()) {
+    return Nothing();
+  }
 
-          Maybe<wr::FontInstanceOptions> options;
-          Maybe<wr::FontInstancePlatformOptions> platformOptions;
-          std::vector<FontVariation> variations;
-          aScaledFont->GetWRFontInstanceOptions(&options, &platformOptions,
-                                                &variations);
+  instanceKey = GetNextFontInstanceKey();
 
-          aResources->AddFontInstance(
-              instanceKey, fontKey.value(), aScaledFont->GetSize(),
-              options.ptrOr(nullptr), platformOptions.ptrOr(nullptr),
-              Range<const FontVariation>(variations.data(), variations.size()));
-          if (resources.isSome()) {
-            UpdateResources(resources.ref());
-          }
+  Maybe<wr::FontInstanceOptions> options;
+  Maybe<wr::FontInstancePlatformOptions> platformOptions;
+  std::vector<FontVariation> variations;
+  aScaledFont->GetWRFontInstanceOptions(&options, &platformOptions,
+                                        &variations);
 
-          entry.Insert(instanceKey);
-        }
+  aResources->AddFontInstance(
+      instanceKey, fontKey.value(), aScaledFont->GetSize(),
+      options.ptrOr(nullptr), platformOptions.ptrOr(nullptr),
+      Range<const FontVariation>(variations.data(), variations.size()));
+  if (resources.isSome()) {
+    UpdateResources(resources.ref());
+  }
 
-        return Some(*entry);
-      });
+  mFontInstanceKeys.InsertOrUpdate(aScaledFont, instanceKey);
+
+  return Some(instanceKey);
 }
 
 Maybe<wr::FontKey> WebRenderBridgeChild::GetFontKeyForUnscaledFont(
     gfx::UnscaledFont* aUnscaled, wr::IpcResourceUpdateQueue* aResources) {
   MOZ_ASSERT(!mDestroyed);
 
-  return mFontKeys.WithEntryHandle(
-      aUnscaled, [&](auto&& entry) -> Maybe<wr::FontKey> {
-        if (!entry) {
-          Maybe<wr::IpcResourceUpdateQueue> resources =
-              aResources ? Nothing() : Some(wr::IpcResourceUpdateQueue(this));
+  wr::FontKey fontKey = {wr::IdNamespace{0}, 0};
+  if (!mFontKeys.Get(aUnscaled, &fontKey)) {
+    Maybe<wr::IpcResourceUpdateQueue> resources =
+        aResources ? Nothing() : Some(wr::IpcResourceUpdateQueue(this));
 
-          wr::FontKey fontKey = {wr::IdNamespace{0}, 0};
-          FontFileDataSink sink = {&fontKey, this, resources.ptrOr(aResources)};
-          // First try to retrieve a descriptor for the font, as this is much
-          // cheaper to send over IPC than the full raw font data. If this is
-          // not possible, then and only then fall back to getting the raw font
-          // file data. If that fails, then the only thing left to do is signal
-          // failure by returning a null font key.
-          if (!aUnscaled->GetFontDescriptor(WriteFontDescriptor, &sink) &&
-              !aUnscaled->GetFontFileData(WriteFontFileData, &sink)) {
-            return Nothing();
-          }
+    FontFileDataSink sink = {&fontKey, this, resources.ptrOr(aResources)};
+    // First try to retrieve a descriptor for the font, as this is much cheaper
+    // to send over IPC than the full raw font data. If this is not possible,
+    // then and only then fall back to getting the raw font file data. If that
+    // fails, then the only thing left to do is signal failure by returning a
+    // null font key.
+    if (!aUnscaled->GetFontDescriptor(WriteFontDescriptor, &sink) &&
+        !aUnscaled->GetFontFileData(WriteFontFileData, &sink)) {
+      return Nothing();
+    }
 
-          if (resources.isSome()) {
-            UpdateResources(resources.ref());
-          }
+    if (resources.isSome()) {
+      UpdateResources(resources.ref());
+    }
 
-          entry.Insert(fontKey);
-        }
+    mFontKeys.InsertOrUpdate(aUnscaled, fontKey);
+  }
 
-        return Some(*entry);
-      });
+  return Some(fontKey);
 }
 
 void WebRenderBridgeChild::RemoveExpiredFontKeys(
