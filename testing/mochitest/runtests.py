a8b25adc353c022880cc9e95b6d71c6df6db6b9f
Geoff Brown <whole.grains@protonmail.com>
2021-03-02 00:16:43 +0000

    Bug 1695784 - py3 updates for mochitest with --use-test-media-devices; r=jmaher
    Allow for clib text/binary conversions under py3.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106854

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -768,7 +768,7 @@ def checkAndConfigureV4l2loopback(device):
 
     VIDIOC_QUERYCAP = 0x80685600
 
-    fd = libc.open(device, O_RDWR)
+    fd = libc.open(six.ensure_binary(device), O_RDWR)
     if fd < 0:
         return False, ""
 
@@ -776,7 +776,7 @@ def checkAndConfigureV4l2loopback(device):
     if libc.ioctl(fd, VIDIOC_QUERYCAP, ctypes.byref(vcap)) != 0:
         return False, ""
 
-    if vcap.driver != "v4l2 loopback":
+    if six.ensure_text(vcap.driver) != "v4l2 loopback":
         return False, ""
 
     class v4l2_control(ctypes.Structure):
@@ -798,7 +798,7 @@ def checkAndConfigureV4l2loopback(device):
     libc.ioctl(fd, VIDIOC_S_CTRL, ctypes.byref(control))
     libc.close(fd)
 
-    return True, vcap.card
+    return True, six.ensure_text(vcap.card)
 
 
 def findTestMediaDevices(log):
