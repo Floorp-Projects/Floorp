1e12ab7bba7ec4c4c47f67578623edc711fad427
Mihai Alexandru Michis <malexandru@mozilla.com>
2021-03-02 13:20:11 +0200

    Backed out changeset 140002ffa867 (bug 1695807) for causing reftest failures.
    CLOSED TREE

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -1980,7 +1980,7 @@ impl BatchBuilder {
                                             _ => ShaderColorMode::Image,
                                         },
                                         alpha_type: AlphaType::PremultipliedAlpha,
-                                        raster_space: RasterizationSpace::Screen,
+                                        raster_space: RasterizationSpace::Local,
                                         opacity: 1.0,
                                     }.encode(),
                                 );
