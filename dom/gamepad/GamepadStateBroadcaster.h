a19b3af8bc5ebbf7f399616a0b5682d28a8dac8d
Chris Martin <cmartin@mozilla.com>
2021-03-03 18:26:48 +0000

    Bug 1658419 - Use shared memory for gamepad state on Windows r=handyman
    Finally, use the primitives from the previous change to deliver gamepad changes.
    
    If the shared memory shortcut is available, all gamepad changes will be
    delivered over it. When the children receive the signal, they will diff their
    last-known state against the new state and generate events to update JS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105129

--- dom/gamepad/GamepadStateBroadcaster.h
+++ dom/gamepad/GamepadStateBroadcaster.h
@@ -37,6 +37,9 @@ class IProtocol;
 
 namespace mozilla::dom {
 
+class GamepadChangeEvent;
+class GamepadTestHelper;
+
 // IPDL structure that knows how to initialize a receiver
 class GamepadStateBroadcastReceiverInfo;
 
@@ -62,6 +65,9 @@ class GamepadStateBroadcastReceiverInfo;
 //
 // result = aIPCActor->SendBroadcasterInfo(remoteInfo);
 // MOZ_ASSERT(result);
+//
+// maybeBroadcaster->AddGamepad(aHandle);
+// maybeBroadcaster->NewAxisMoveEvent(aHandle, 0 /*axisId*/, 0.0 /*value*/);
 //
 class GamepadStateBroadcaster {
  public:
@@ -89,6 +95,33 @@ class GamepadStateBroadcaster {
   // broadcaster side.
   void RemoveReceiver(const mozilla::ipc::IProtocol* aActor);
 
+  // Adds a new gamepad to shared memory region
+  void AddGamepad(GamepadHandle aHandle, const char* aID,
+                  GamepadMappingType aMapping, GamepadHand aHand,
+                  uint32_t aNumButtons, uint32_t aNumAxes, uint32_t aNumHaptics,
+                  uint32_t aNumLights, uint32_t aNumTouches);
+
+  // Removes a gamepad from the shared memory region
+  void RemoveGamepad(GamepadHandle aHandle);
+
+  // Update a gamepad axis in the shared memory region
+  void NewAxisMoveEvent(GamepadHandle aHandle, uint32_t aAxis, double aValue);
+
+  // Update a gamepad button in the shared memory region
+  void NewButtonEvent(GamepadHandle aHandle, uint32_t aButton, bool aPressed,
+                      bool aTouched, double aValue);
+
+  // Update a gamepad light indicator in the shared memory region
+  void NewLightIndicatorTypeEvent(GamepadHandle aHandle, uint32_t aLight,
+                                  GamepadLightIndicatorType aType);
+
+  // Update a gamepad pose state in the shared memory region
+  void NewPoseEvent(GamepadHandle aHandle, const GamepadPoseState& aState);
+
+  // Update a gamepad multi-touch state in the shared memory region
+  void NewMultiTouchEvent(GamepadHandle aHandle, uint32_t aTouchArrayIndex,
+                          const GamepadTouchState& aState);
+
   // Allow move
   GamepadStateBroadcaster(GamepadStateBroadcaster&& aOther);
   GamepadStateBroadcaster& operator=(GamepadStateBroadcaster&& aOther);
@@ -106,6 +139,9 @@ class GamepadStateBroadcaster {
   explicit GamepadStateBroadcaster(UniquePtr<Impl> aImpl);
 
   UniquePtr<Impl> mImpl;
+
+  friend class GamepadTestHelper;
+  void SendTestCommand(uint32_t aCommandId);
 };
 
 }  // namespace mozilla::dom
