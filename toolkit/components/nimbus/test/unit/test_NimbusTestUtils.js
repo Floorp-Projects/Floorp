3c11cc66902f796fb4fd6968df771898ad5d74d8
Andrei Oprea <andrei.br92@gmail.com>
2021-03-03 16:56:45 +0000

    Bug 1693301 - Nimbus test utility for automatic enrollment and cleanup r=k88hudson
    Differential Revision: https://phabricator.services.mozilla.com/D106162

--- toolkit/components/nimbus/test/unit/test_NimbusTestUtils.js
+++ toolkit/components/nimbus/test/unit/test_NimbusTestUtils.js
@@ -11,3 +11,40 @@ add_task(async function test_recipe_fake_validates() {
     "should produce a valid experiment recipe"
   );
 });
+
+add_task(async function test_enrollmentHelper() {
+  let recipe = ExperimentFakes.recipe("bar");
+  recipe.branches.forEach(branch => {
+    // Use a feature that will set the sync pref cache
+    branch.feature.featureId = "aboutwelcome";
+  });
+  let manager = ExperimentFakes.manager();
+
+  await manager.onStartup();
+
+  let {
+    enrollmentPromise,
+    doExperimentCleanup,
+  } = ExperimentFakes.enrollmentHelper(recipe, { manager });
+
+  await enrollmentPromise;
+
+  Assert.ok(manager.store.getAllActive().length === 1, "Enrolled");
+  Assert.equal(
+    manager.store.getAllActive()[0].slug,
+    recipe.slug,
+    "Has expected slug"
+  );
+  Assert.ok(
+    Services.prefs.prefHasUserValue("nimbus.syncdatastore.aboutwelcome"),
+    "Sync pref cache set"
+  );
+
+  await doExperimentCleanup();
+
+  Assert.ok(manager.store.getAll().length === 0, "Cleanup done");
+  Assert.ok(
+    !Services.prefs.prefHasUserValue("nimbus.syncdatastore.aboutwelcome"),
+    "Sync pref cache is cleared"
+  );
+});
