d5d4ce80b87701b1b88a0b2704390bf8df92d5c3
Tooru Fujisawa <arai_a@mac.com>
2021-03-03 23:00:03 +0000

    Bug 1687095 - Part 1: Add {CompilationStencil,ExtensibleCompilationStencil}::steal. r=tcampbell
    To make it possible to convert them each other.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105909

--- js/src/frontend/ParserAtom.cpp
+++ js/src/frontend/ParserAtom.cpp
@@ -320,6 +320,56 @@ TaggedParserAtomIndex ParserAtomsTable::internLatin1(
   return internChar16Seq<Latin1Char>(cx, addPtr, lookup.hash(), seq, length);
 }
 
+bool IsWide(const InflatedChar16Sequence<char16_t>& seq) {
+  InflatedChar16Sequence<char16_t> seqCopy = seq;
+  while (seqCopy.hasMore()) {
+    char16_t ch = seqCopy.next();
+    if (ch > MAX_LATIN1_CHAR) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+template <typename AtomCharT>
+TaggedParserAtomIndex ParserAtomsTable::internExternalParserAtomImpl(
+    JSContext* cx, const ParserAtom* atom) {
+  InflatedChar16Sequence<AtomCharT> seq(atom->chars<AtomCharT>(),
+                                        atom->length());
+  SpecificParserAtomLookup<AtomCharT> lookup(seq, atom->hash());
+
+  // Check for existing atom.
+  auto addPtr = entryMap_.lookupForAdd(lookup);
+  if (addPtr) {
+    return addPtr->value();
+  }
+
+  return internChar16Seq<AtomCharT>(cx, addPtr, atom->hash(), seq,
+                                    atom->length());
+}
+
+TaggedParserAtomIndex ParserAtomsTable::internExternalParserAtom(
+    JSContext* cx, const ParserAtom* atom) {
+  if (atom->hasLatin1Chars()) {
+    return internExternalParserAtomImpl<JS::Latin1Char>(cx, atom);
+  }
+  return internExternalParserAtomImpl<char16_t>(cx, atom);
+}
+
+bool ParserAtomsTable::addPlaceholder(JSContext* cx) {
+  ParserAtomIndex index = ParserAtomIndex(entries_.length());
+  if (size_t(index) >= TaggedParserAtomIndex::IndexLimit) {
+    ReportAllocationOverflow(cx);
+    return false;
+  }
+  if (!entries_.append(nullptr)) {
+    js::ReportOutOfMemory(cx);
+    return false;
+  }
+  return true;
+}
+
 ParserAtomSpanBuilder::ParserAtomSpanBuilder(JSRuntime* rt,
                                              ParserAtomSpan& entries)
     : wellKnownTable_(*rt->commonParserNames), entries_(entries) {}
@@ -412,20 +462,8 @@ TaggedParserAtomIndex ParserAtomsTable::internChar16(JSContext* cx,
     return addPtr->value();
   }
 
-  // Compute the target encoding.
-  // NOTE: Length in code-points will be same, even if we deflate to Latin1.
-  bool wide = false;
-  InflatedChar16Sequence<char16_t> seqCopy = seq;
-  while (seqCopy.hasMore()) {
-    char16_t ch = seqCopy.next();
-    if (ch > MAX_LATIN1_CHAR) {
-      wide = true;
-      break;
-    }
-  }
-
   // Otherwise, add new entry.
-  return wide
+  return IsWide(seq)
              ? internChar16Seq<char16_t>(cx, addPtr, lookup.hash(), seq, length)
              : internChar16Seq<Latin1Char>(cx, addPtr, lookup.hash(), seq,
                                            length);
