f1909939316aa036744c40190565b51d661d7cc1
Tooru Fujisawa <arai_a@mac.com>
2021-03-03 23:00:05 +0000

    Bug 1687095 - Part 5: Remove gcOutputForDelazification. r=tcampbell
    Differential Revision: https://phabricator.services.mozilla.com/D105156

--- js/src/frontend/BytecodeCompilation.h
+++ js/src/frontend/BytecodeCompilation.h
@@ -71,15 +71,14 @@ CompileGlobalScriptToExtensibleStencil(
 // Part of InstantiateStencils can be done by calling PrepareForInstantiate.
 // PrepareForInstantiate is GC-free operation that can be performed
 // off-main-thread without parse global.
-extern bool PrepareForInstantiate(
+[[nodiscard]] extern bool PrepareForInstantiate(
     JSContext* cx, CompilationInput& input, const CompilationStencil& stencil,
-    CompilationGCOutput& gcOutput,
-    CompilationGCOutput* gcOutputForDelazification = nullptr);
+    CompilationGCOutput& gcOutput);
 
-extern bool InstantiateStencils(
-    JSContext* cx, CompilationInput& input, const CompilationStencil& stencil,
-    CompilationGCOutput& gcOutput,
-    CompilationGCOutput* gcOutputForDelazification = nullptr);
+[[nodiscard]] extern bool InstantiateStencils(JSContext* cx,
+                                              CompilationInput& input,
+                                              const CompilationStencil& stencil,
+                                              CompilationGCOutput& gcOutput);
 
 extern JSScript* CompileGlobalScript(JSContext* cx,
                                      const JS::ReadOnlyCompileOptions& options,
