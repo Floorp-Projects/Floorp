633e98b62e911760a379f3a4b35bde1b9df5a53b
Hubert Boma Manilla <hmanilla@mozilla.com>
2021-03-04 22:08:18 +0000

    Bug 1644196 - [devtools] Server side listening for Websocket resoources r=ochameau
    Differential Revision: https://phabricator.services.mozilla.com/D103847

--- devtools/shared/resources/tests/browser_resources_websocket.js
+++ devtools/shared/resources/tests/browser_resources_websocket.js
@@ -9,21 +9,40 @@ const {
   ResourceWatcher,
 } = require("devtools/shared/resources/resource-watcher");
 
-const TEST_URL = URL_ROOT + "websocket_frontend.html";
 const IS_NUMBER = "IS_NUMBER";
+const SHOULD_EXIST = "SHOULD_EXIST";
+
+const targets = {
+  TOP_LEVEL_DOCUMENT: "top-level-document",
+  IN_PROCESS_IFRAME: "in-process-frame",
+  OUT_PROCESS_IFRAME: "out-process-frame",
+};
 
 add_task(async function() {
-  const tab = await addTab(TEST_URL);
+  info("Testing the top-level document");
+  await testWebsocketResources(targets.TOP_LEVEL_DOCUMENT);
+  info("Testing the in-process iframe");
+  await testWebsocketResources(targets.IN_PROCESS_IFRAME);
+  info("Testing the out-of-process iframe");
+  await testWebsocketResources(targets.OUT_PROCESS_IFRAME);
+});
 
+async function testWebsocketResources(target) {
+  const tab = await addTab(URL_ROOT + "websocket_frontend.html");
   const { client, resourceWatcher, targetList } = await initResourceWatcher(
     tab
   );
 
-  info("Check available resources at initial");
   const availableResources = [];
+  function onResourceAvailable(resources) {
+    availableResources.push(...resources);
+  }
+
   await resourceWatcher.watchResources([ResourceWatcher.TYPES.WEBSOCKET], {
-    onAvailable: resources => availableResources.push(...resources),
+    onAvailable: onResourceAvailable,
   });
+
+  info("Check available resources at initial");
   is(
     availableResources.length,
     0,
@@ -31,12 +50,14 @@ add_task(async function() {
   );
 
   info("Check resource of opening websocket");
-  await ContentTask.spawn(tab.linkedBrowser, [], async () => {
-    await content.wrappedJSObject.openConnection();
-  });
+  await executeFunctionInContext(tab, target, "openConnection");
+
   await waitUntil(() => availableResources.length === 1);
+
   const httpChannelId = availableResources[0].httpChannelId;
+
   ok(httpChannelId, "httpChannelId is present in the resource");
+
   assertResource(availableResources[0], {
     wsMessageType: "webSocketOpened",
     effectiveURI:
@@ -46,16 +67,24 @@ add_task(async function() {
   });
 
   info("Check resource of sending/receiving the data via websocket");
-  await ContentTask.spawn(tab.linkedBrowser, [], async () => {
-    await content.wrappedJSObject.sendData("test");
-  });
+  await executeFunctionInContext(tab, target, "sendData", "test");
+
   await waitUntil(() => availableResources.length === 3);
+
   assertResource(availableResources[1], {
     wsMessageType: "frameSent",
     httpChannelId,
     data: {
       type: "sent",
       payload: "test",
+      timeStamp: SHOULD_EXIST,
+      finBit: SHOULD_EXIST,
+      rsvBit1: SHOULD_EXIST,
+      rsvBit2: SHOULD_EXIST,
+      rsvBit3: SHOULD_EXIST,
+      opCode: SHOULD_EXIST,
+      mask: SHOULD_EXIST,
+      maskBit: SHOULD_EXIST,
     },
   });
   assertResource(availableResources[2], {
@@ -64,13 +93,20 @@ add_task(async function() {
     data: {
       type: "received",
       payload: "test",
+      timeStamp: SHOULD_EXIST,
+      finBit: SHOULD_EXIST,
+      rsvBit1: SHOULD_EXIST,
+      rsvBit2: SHOULD_EXIST,
+      rsvBit3: SHOULD_EXIST,
+      opCode: SHOULD_EXIST,
+      mask: SHOULD_EXIST,
+      maskBit: SHOULD_EXIST,
     },
   });
 
   info("Check resource of closing websocket");
-  await ContentTask.spawn(tab.linkedBrowser, [], async () => {
-    await content.wrappedJSObject.closeConnection();
-  });
+  await executeFunctionInContext(tab, target, "closeConnection");
+
   await waitUntil(() => availableResources.length === 6);
   assertResource(availableResources[3], {
     wsMessageType: "frameSent",
@@ -78,6 +114,14 @@ add_task(async function() {
     data: {
       type: "sent",
       payload: "",
+      timeStamp: SHOULD_EXIST,
+      finBit: SHOULD_EXIST,
+      rsvBit1: SHOULD_EXIST,
+      rsvBit2: SHOULD_EXIST,
+      rsvBit3: SHOULD_EXIST,
+      opCode: SHOULD_EXIST,
+      mask: SHOULD_EXIST,
+      maskBit: SHOULD_EXIST,
     },
   });
   assertResource(availableResources[4], {
@@ -86,6 +130,14 @@ add_task(async function() {
     data: {
       type: "received",
       payload: "",
+      timeStamp: SHOULD_EXIST,
+      finBit: SHOULD_EXIST,
+      rsvBit1: SHOULD_EXIST,
+      rsvBit2: SHOULD_EXIST,
+      rsvBit3: SHOULD_EXIST,
+      opCode: SHOULD_EXIST,
+      mask: SHOULD_EXIST,
+      maskBit: SHOULD_EXIST,
     },
   });
   assertResource(availableResources[5], {
@@ -98,26 +150,72 @@ add_task(async function() {
 
   info("Check existing resources");
   const existingResources = [];
+
+  function onExsistingResourceAvailable(resources) {
+    existingResources.push(...resources);
+  }
+
   await resourceWatcher.watchResources([ResourceWatcher.TYPES.WEBSOCKET], {
-    onAvailable: resources => existingResources.push(...resources),
+    onAvailable: onExsistingResourceAvailable,
   });
+
   is(
     availableResources.length,
     existingResources.length,
     "Length of existing resources is correct"
   );
+
   for (let i = 0; i < availableResources.length; i++) {
-    const availableResource = availableResources[i];
-    const existingResource = existingResources[i];
     ok(
-      availableResource === existingResource,
+      availableResources[i] === existingResources[i],
       `The ${i}th resource is correct`
     );
   }
 
+  await resourceWatcher.unwatchResources([ResourceWatcher.TYPES.WEBSOCKET], {
+    onAvailable: onResourceAvailable,
+  });
+
+  await resourceWatcher.unwatchResources([ResourceWatcher.TYPES.WEBSOCKET], {
+    onAvailable: onExsistingResourceAvailable,
+  });
+
   targetList.destroy();
   await client.close();
-});
+  BrowserTestUtils.removeTab(tab);
+}
+
+/**
+ * Execute global functions defined in the correct
+ * target (top-level-window or frames) contexts.
+ *
+ * @param {object} tab The current window tab
+ * @param {string} target A string identify if we want to test the top level document or iframes
+ * @param {string} funcName The name of the global function which needs to be called.
+ * @param {*} funcArgs The arguments to pass to the global function
+ */
+async function executeFunctionInContext(tab, target, funcName, ...funcArgs) {
+  let browsingContext = tab.linkedBrowser.browsingContext;
+  // If the target is an iframe get its window global
+  if (target !== targets.TOP_LEVEL_DOCUMENT) {
+    browsingContext = await SpecialPowers.spawn(
+      tab.linkedBrowser,
+      [target],
+      async _target => {
+        const iframe = content.document.getElementById(_target);
+        return iframe.browsingContext;
+      }
+    );
+  }
+
+  return SpecialPowers.spawn(
+    browsingContext,
+    [funcName, funcArgs],
+    async (_funcName, _funcArgs) => {
+      await content.wrappedJSObject[_funcName](..._funcArgs);
+    }
+  );
+}
 
 function assertResource(resource, expected) {
   is(
@@ -133,6 +231,8 @@ function assertObject(object, expected) {
   for (const field in expected) {
     if (typeof expected[field] === "object") {
       assertObject(object[field], expected[field]);
+    } else if (expected[field] === SHOULD_EXIST) {
+      ok(object[field] !== undefined, `The value of ${field} exists`);
     } else if (expected[field] === IS_NUMBER) {
       ok(!isNaN(object[field]), `The value of ${field} is number`);
     } else {
