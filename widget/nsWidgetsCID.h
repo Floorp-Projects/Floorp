6bcb9935cceccbf83fdab8905d3557403cb0ddb6
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2021-03-04 22:54:23 +0000

    Bug 1695873 - remove nativemenuservice remnants and fix macOS openedwithkey attribute use, r=mak,mstange
    Although 11 years ago feature-checking for native menus made sense, right now we do not
    expect any other OSes to implement native menus, and macOS' behaviour is currently not
    quite right because we stopped exposing the relevant XPCOM component.
    
    The change in browser-menubar.inc fixes macOS behaviour. The nsWidgetsCID.h change
    is just clean-up.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106962

--- widget/nsWidgetsCID.h
+++ widget/nsWidgetsCID.h
@@ -66,14 +66,6 @@
 // Menus
 //-----------------------------------------------------------
 
-// {0B3FE5AA-BC72-4303-85AE-76365DF1251D}
-#define NS_NATIVEMENUSERVICE_CID                     \
-  {                                                  \
-    0x0B3FE5AA, 0xBC72, 0x4303, {                    \
-      0x85, 0xAE, 0x76, 0x36, 0x5D, 0xF1, 0x25, 0x1D \
-    }                                                \
-  }
-
 // {F6CD4F21-53AF-11d2-8DC4-00609703C14E}
 #define NS_POPUPMENU_CID                           \
   {                                                \
