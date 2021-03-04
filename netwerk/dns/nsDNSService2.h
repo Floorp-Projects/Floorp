8e5a4d95e5d7d7d43d1691b7536a1415b9a154c0
Valentin Gosu <valentin.gosu@gmail.com>
2021-03-04 10:14:59 +0000

    Bug 1513519 - Always hold lock when accessing mResolver. r=necko-reviewers,dragana, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D106913

--- netwerk/dns/nsDNSService2.h
+++ netwerk/dns/nsDNSService2.h
@@ -77,6 +77,9 @@ class nsDNSService final : public nsPIDNSService,
 
   bool DNSForbiddenByActiveProxy(const nsACString& aHostname, uint32_t flags);
 
+  // Locks the mutex and returns an addreffed resolver. May return null.
+  already_AddRefed<nsHostResolver> GetResolverLocked();
+
   RefPtr<nsHostResolver> mResolver;
   nsCOMPtr<nsIIDNService> mIDN;
 
