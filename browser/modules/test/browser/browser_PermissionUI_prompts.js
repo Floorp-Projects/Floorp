b9387263d2d7dbdf8cc0344ceb1531eab48ebe38
Brindusan Cristian <cbrindusan@mozilla.com>
2021-03-04 21:13:50 +0200

    Backed out changeset 34f7e105340a (bug 1695084) for bc failures at browser_permissionsPromptDeny.js. CLOSED TREE

--- browser/modules/test/browser/browser_PermissionUI_prompts.js
+++ browser/modules/test/browser/browser_PermissionUI_prompts.js
@@ -114,8 +114,11 @@ async function testPrompt(Prompt) {
 
       let isNotificationPrompt =
         Prompt == PermissionUI.DesktopNotificationPermissionPrompt;
+      let isPersistentStoragePrompt =
+        Prompt == PermissionUI.PersistentStoragePermissionPrompt;
 
-      let expectedSecondaryActionsCount = isNotificationPrompt ? 2 : 1;
+      let expectedSecondaryActionsCount =
+        isNotificationPrompt || isPersistentStoragePrompt ? 2 : 1;
       Assert.equal(
         notification.secondaryActions.length,
         expectedSecondaryActionsCount,
@@ -168,7 +171,7 @@ async function testPrompt(Prompt) {
       // or by clicking the "never" option from the dropdown (for notifications and persistent-storage).
       popupNotification = getPopupNotificationNode();
       let secondaryActionToClickIndex = 0;
-      if (isNotificationPrompt) {
+      if (isNotificationPrompt || isPersistentStoragePrompt) {
         secondaryActionToClickIndex = 1;
       } else {
         popupNotification.checkbox.checked = true;
