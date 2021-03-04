35b7e1610185bc5a01c47cdf63bb11e42cc461d2
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-03-04 15:05:26 +0000

    Bug 1360715 - Part 2: Modify instanceofs in tests to non-cross-context r=edgar
    Differential Revision: https://phabricator.services.mozilla.com/D106662

--- gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
+++ gfx/layers/apz/test/mochitest/apz_test_native_event_utils.js
@@ -65,7 +65,7 @@ function nativeHorizontalWheelEventMsg() {
 
 // Given an event target which may be a window or an element, get the associated window.
 function windowForTarget(aTarget) {
-  if (aTarget instanceof Window) {
+  if (aTarget.Window && aTarget instanceof aTarget.Window) {
     return aTarget;
   }
   return aTarget.ownerDocument.defaultView;
@@ -73,7 +73,7 @@ function windowForTarget(aTarget) {
 
 // Given an event target which may be a window or an element, get the associated element.
 function elementForTarget(aTarget) {
-  if (aTarget instanceof Window) {
+  if (aTarget.Window && aTarget instanceof aTarget.Window) {
     return aTarget.document.documentElement;
   }
   return aTarget;
