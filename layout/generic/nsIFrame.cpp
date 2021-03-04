a0be0aea13e20b7aaff702e50719a0f9fff3f4da
Ting-Yu Lin <tlin@mozilla.com>
2021-03-04 00:34:03 +0000

    Bug 1690701 Part 1 - Check floating style when determining whether it is a floating first letter. r=layout-reviewers,emilio
    IsFontSizeInflationContainer() is a helper of nsIFrame::Init(). That is,
    when it is called from a caller like
    nsCSSFrameConstructor::CreateFloatingLetterFrame(), the
    `NS_FRAME_OUT_OF_FLOW` bit is not set yet. There is also a hint at the
    call site
    https://searchfox.org/mozilla-central/rev/362676fcadac37f9f585141a244a9a640948794a/layout/generic/nsIFrame.cpp#770
    
    To fix it, we need to change the condition to check only the
    floating style.
    
    layout/reftests/bidi/with-first-letter-2b.html is one of the testcases
    that can trigger the following assertion without this patch.
    
      ###!!! ASSERTION: should not be container for font size inflation
    
    Differential Revision: https://phabricator.services.mozilla.com/D106579

--- layout/generic/nsIFrame.cpp
+++ layout/generic/nsIFrame.cpp
@@ -566,7 +566,8 @@ static bool IsFontSizeInflationContainer(nsIFrame* aFrame,
   bool isInline =
       (nsStyleDisplay::IsInlineFlow(aFrame->GetDisplay()) ||
        RubyUtils::IsRubyBox(frameType) ||
-       (aFrame->IsFloating() && frameType == LayoutFrameType::Letter) ||
+       (aStyleDisplay->IsFloatingStyle() &&
+        frameType == LayoutFrameType::Letter) ||
        // Given multiple frames for the same node, only the
        // outer one should be considered a container.
        // (Important, e.g., for nsSelectsAreaFrame.)
