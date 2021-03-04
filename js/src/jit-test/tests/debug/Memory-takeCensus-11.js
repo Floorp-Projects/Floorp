d66c684b57fd3b67c8c9c62ec0eabdfd7c320eca
Tooru Fujisawa <arai_a@mac.com>
2021-03-04 22:36:22 +0000

    Bug 1694916 - Part 1: Remove RealmBehaviors.disableLazyParsing. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D107006

--- js/src/jit-test/tests/debug/Memory-takeCensus-11.js
+++ js/src/jit-test/tests/debug/Memory-takeCensus-11.js
@@ -5,19 +5,32 @@
 
 // Check byte counts produced by takeCensus.
 
-const g = newGlobal({newCompartment: true, disableLazyParsing: true });
-g.eval("setJitCompilerOption('ion.warmup.trigger', 1000)");
+const g = newGlobal({newCompartment: true });
+g.evaluate("setJitCompilerOption('ion.warmup.trigger', 1000)",
+           {
+             forceFullParse: true,
+           });
 
 const dbg = new Debugger(g);
 
-g.evaluate("function one() {}", { fileName: "one.js" });
+g.evaluate("function one() {}",
+           {
+             fileName: "one.js",
+             forceFullParse: true,
+           });
 g.evaluate(`function two1() {}
             function two2() {}`,
-           { fileName: "two.js" });
+           {
+             fileName: "two.js",
+             forceFullParse: true,
+           });
 g.evaluate(`function three1() {}
             function three2() {}
             function three3() {}`,
-           { fileName: "three.js" });
+           {
+             fileName: "three.js",
+             forceFullParse: true,
+           });
 
 const report = dbg.memory.takeCensus({
   breakdown: {
