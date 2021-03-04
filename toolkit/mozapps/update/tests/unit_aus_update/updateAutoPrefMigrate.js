093ebb7ac9887ac955797efb31f441cacf9ed2e1
Kirk Steuber <ksteuber@mozilla.com>
2021-03-04 17:15:58 +0000

    Bug 1691486 - Change tests that manually read the config JSON r=nalexander,application-update-reviewers
    Some tests manually open the config JSON and verify their contents. We shouldn't do this anymore now that the structure of this JSON file has become somewhat more complicated.
    
    Depends on D106996
    
    Differential Revision: https://phabricator.services.mozilla.com/D106997

--- toolkit/mozapps/update/tests/unit_aus_update/updateAutoPrefMigrate.js
+++ toolkit/mozapps/update/tests/unit_aus_update/updateAutoPrefMigrate.js
@@ -3,25 +3,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-const { OS } = ChromeUtils.import("resource://gre/modules/osfile.jsm");
-
-// Checks both the value returned by the update service and the one written to
-// the app update config file.
-async function verifyPref(configFile, expectedValue) {
-  let decoder = new TextDecoder();
+async function verifyPref(expectedValue) {
   let configValue = await UpdateUtils.getAppUpdateAutoEnabled();
   Assert.equal(
     configValue,
     expectedValue,
     "Value returned by getAppUpdateAutoEnabled should have " +
       "matched the expected value"
-  );
-  let fileContents = await OS.File.read(configFile.path);
-  let saveObject = JSON.parse(decoder.decode(fileContents));
-  Assert.equal(
-    saveObject["app.update.auto"],
-    expectedValue,
-    "Value in update config file should match expected"
   );
 }
 
@@ -35,11 +23,11 @@ async function run_test() {
   Services.prefs.setBoolPref("app.update.auto.migrated", false);
   Services.prefs.setBoolPref("app.update.auto", false);
   Assert.ok(!configFile.exists(), "Config file should not exist yet");
-  await verifyPref(configFile, false);
+  await verifyPref(false);
 
   // Test that migration doesn't happen twice
   Services.prefs.setBoolPref("app.update.auto", true);
-  await verifyPref(configFile, false);
+  await verifyPref(false);
 
   // If the file is deleted after migration, the default value should be
   // returned, regardless of the pref value.
@@ -55,7 +43,7 @@ async function run_test() {
 
   // Setting a new value should cause the value to get written out again
   await UpdateUtils.setAppUpdateAutoEnabled(false);
-  await verifyPref(configFile, false);
+  await verifyPref(false);
 
   // Test migration of a |true| value
   Services.prefs.setBoolPref("app.update.auto.migrated", false);
@@ -65,11 +53,11 @@ async function run_test() {
     !configFile.exists(),
     "App update config file should have been removed"
   );
-  await verifyPref(configFile, true);
+  await verifyPref(true);
 
   // Test that setting app.update.auto without migrating also works
   await UpdateUtils.setAppUpdateAutoEnabled(false);
-  await verifyPref(configFile, false);
+  await verifyPref(false);
 
   doTestFinish();
 }
