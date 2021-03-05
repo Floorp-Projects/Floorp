549cc3c27d6b963415d1dd4da1fabe42b54df8d1
Hector Zhao <bzhao@mozilla.com>
2021-03-05 10:29:16 +0000

    Bug 1694882 - WebExtension alerts should show the extension name as the origin. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D107320

--- browser/base/content/test/tabdialogs/browser_tabdialogbox_content_prompts.js
+++ browser/base/content/test/tabdialogs/browser_tabdialogbox_content_prompts.js
@@ -8,6 +8,29 @@ const TEST_ROOT_CHROME = getRootDirectory(gTestPath);
 const TEST_DIALOG_PATH = TEST_ROOT_CHROME + "subdialog.xhtml";
 
 const TEST_DATA_URI = "data:text/html,<body onload='alert(1)'>";
+const TEST_EXTENSION_DATA = {
+  background() {
+    // eslint-disable-next-line no-undef
+    browser.test.sendMessage("url", browser.extension.getURL("alert.html"));
+  },
+  manifest: {
+    name: "Test Extension",
+  },
+  files: {
+    "alert.html": `<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TabDialogBox Content Modal Test page</title>
+    <script src="./alert.js"></script>
+  </head>
+  <body>
+    <h1>TabDialogBox Content Modal</h1>
+  </body>
+</html>`,
+    "alert.js": `window.addEventListener("load", () => alert("Hi"));`,
+  },
+};
 const TEST_ORIGIN = "http://example.com";
 const TEST_PAGE =
   TEST_ROOT_CHROME.replace("chrome://mochitests/content", TEST_ORIGIN) +
@@ -70,6 +93,28 @@ add_task(async function test_tabdialog_null_principal_title() {
     await dialogShown;
     await checkOriginText(browser, "ScriptDlgNullPrincipalHeading");
   });
+});
+
+/**
+ * Test origin text for an extension page.
+ */
+add_task(async function test_tabdialog_extension_title() {
+  let extension = ExtensionTestUtils.loadExtension(TEST_EXTENSION_DATA);
+
+  await extension.startup();
+  let url = await extension.awaitMessage("url");
+  let dialogShown = BrowserTestUtils.waitForEvent(
+    gBrowser,
+    "DOMWillOpenModalDialog"
+  );
+
+  await BrowserTestUtils.withNewTab(url, async function(browser) {
+    info("Waiting for dialog to open.");
+    await dialogShown;
+    await checkOriginText(browser, "ScriptDlgHeading", "Test Extension");
+  });
+
+  await extension.unload();
 });
 
 /**
