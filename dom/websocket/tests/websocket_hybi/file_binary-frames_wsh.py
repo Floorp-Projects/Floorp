8b9e888686ec5d60eda0285148582bcb5f54cae6
Geoff Brown <whole.grains@protonmail.com>
2021-03-08 15:36:32 +0000

    Bug 1696024 - py3 updates to dom/websocket mochitests; r=ahal
    Differential Revision: https://phabricator.services.mozilla.com/D107282

--- dom/websocket/tests/websocket_hybi/file_binary-frames_wsh.py
+++ dom/websocket/tests/websocket_hybi/file_binary-frames_wsh.py
@@ -1,5 +1,6 @@
 from mod_pywebsocket import common
 from mod_pywebsocket import stream
+import six
 
 
 def web_socket_do_extra_handshake(request):
@@ -9,6 +10,7 @@ def web_socket_do_extra_handshake(request):
 def web_socket_transfer_data(request):
     messages_to_send = ["Hello, world!", "", all_distinct_bytes()]
     for message in messages_to_send:
+        message = six.b(message)
         # FIXME: Should use better API to send binary messages when pywebsocket supports it.
         header = stream.create_header(common.OPCODE_BINARY, len(message), 1, 0, 0, 0, 0)
         request.connection.write(header + message)
