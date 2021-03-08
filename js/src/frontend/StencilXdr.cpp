9dc68204fe60456e042bbb8a348a341241557bcd
Tooru Fujisawa <arai_a@mac.com>
2021-03-08 23:32:23 +0000

    Bug 1693611 - Part 7: Move XDRCheckCompilationStencil to StencilXDR. r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D105918

--- js/src/frontend/StencilXdr.cpp
+++ js/src/frontend/StencilXdr.cpp
@@ -656,30 +656,30 @@ template XDRResult XDRCompilationStencil(XDRState<XDR_ENCODE>* xdr,
 template XDRResult XDRCompilationStencil(XDRState<XDR_DECODE>* xdr,
                                          CompilationStencil& stencil);
 
-template <XDRMode mode>
-XDRResult XDRCheckCompilationStencil(XDRState<mode>* xdr,
-                                     CompilationStencil& stencil) {
-  if (stencil.asmJS) {
-    return xdr->fail(JS::TranscodeResult::Failure_AsmJSNotSupported);
-  }
-
-  return Ok();
-}
-
-template XDRResult XDRCheckCompilationStencil(XDRState<XDR_ENCODE>* xdr,
-                                              CompilationStencil& stencil);
-
-template <XDRMode mode>
-XDRResult XDRCheckCompilationStencil(XDRState<mode>* xdr,
-                                     ExtensibleCompilationStencil& stencil) {
-  if (stencil.asmJS) {
-    return xdr->fail(JS::TranscodeResult::Failure_AsmJSNotSupported);
-  }
-
-  return Ok();
-}
-
-template XDRResult XDRCheckCompilationStencil(
-    XDRState<XDR_ENCODE>* xdr, ExtensibleCompilationStencil& stencil);
-
 }  // namespace js
+
+template <XDRMode mode>
+/* static */ XDRResult StencilXDR::checkCompilationStencil(
+    XDRState<mode>* xdr, CompilationStencil& stencil) {
+  if (stencil.asmJS) {
+    return xdr->fail(JS::TranscodeResult::Failure_AsmJSNotSupported);
+  }
+
+  return Ok();
+}
+
+template XDRResult StencilXDR::checkCompilationStencil(
+    XDRState<XDR_ENCODE>* xdr, CompilationStencil& stencil);
+
+template <XDRMode mode>
+/* static */ XDRResult StencilXDR::checkCompilationStencil(
+    XDRState<mode>* xdr, ExtensibleCompilationStencil& stencil) {
+  if (stencil.asmJS) {
+    return xdr->fail(JS::TranscodeResult::Failure_AsmJSNotSupported);
+  }
+
+  return Ok();
+}
+
+template XDRResult StencilXDR::checkCompilationStencil(
+    XDRState<XDR_ENCODE>* xdr, ExtensibleCompilationStencil& stencil);
