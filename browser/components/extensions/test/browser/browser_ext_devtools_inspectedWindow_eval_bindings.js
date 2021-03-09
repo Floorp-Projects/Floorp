e15ca67cce1042b10f78c1af04dcce325286a521
Julian Descottes <jdescottes@mozilla.com>
2021-03-09 20:50:43 +0000

    Bug 1694906 - [devtools] Fix race conditions in various devtools webextensions tests r=rpl
    Depends on D107099
    
    With the previous patch, we are waiting for toolbox open before creating contexts.
    This makes some tests a bit racy if they are only waiting for toolbox open. They should also wait for the context to be ready.
    Firing an event from the devtools_page seems to work fine for this but don't hesitate to suggest a better approach.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107100

--- browser/components/extensions/test/browser/browser_ext_devtools_inspectedWindow_eval_bindings.js
+++ browser/components/extensions/test/browser/browser_ext_devtools_inspectedWindow_eval_bindings.js
@@ -41,6 +41,7 @@ add_task(async function test_devtools_inspectedWindow_eval_bindings() {
         browser.test.fail(`Error: ${err} :: ${err.stack}`);
       }
     });
+    browser.test.sendMessage("devtools-page-loaded");
   }
 
   let extension = ExtensionTestUtils.loadExtension({
@@ -64,6 +65,7 @@ add_task(async function test_devtools_inspectedWindow_eval_bindings() {
   await extension.startup();
 
   const toolbox = await openToolboxForTab(tab);
+  await extension.awaitMessage("devtools-page-loaded");
 
   // Test $0 binding with no selected node
   info("Test inspectedWindow.eval $0 binding with no selected node");
