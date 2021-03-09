9417a66043cb4323d582bc3e2021b61dad747505
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-09 10:28:16 +0000

    Bug 1695162 - Make nsTHashtable::ConstIter return a real ConstIterator. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D107038

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -528,7 +528,7 @@ void IdentifierMapEntry::FireChangeCallbacks(Element* aOldElement,
                                              bool aImageOnly) {
   if (!mChangeCallbacks) return;
 
-  for (auto iter = mChangeCallbacks->ConstIter(); !iter.Done(); iter.Next()) {
+  for (auto iter = mChangeCallbacks->Iter(); !iter.Done(); iter.Next()) {
     IdentifierMapEntry::ChangeCallbackEntry* entry = iter.Get();
     // Don't fire image changes for non-image observers, and don't fire element
     // changes for image observers when an image override is active.
