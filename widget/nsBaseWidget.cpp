ca86d54b8cec3764e2feb89357f23fcb9d485d75
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-09 11:39:36 +0000

    Bug 1634281 - Use nsTHashMap for nsBaseHashtable/nsDataHashtable uses with nsCOMPtr data type. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D105964

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -122,7 +122,7 @@ int32_t nsIWidget::sPointerIdCounter = 0;
 // Some statics from nsIWidget.h
 /*static*/
 uint64_t AutoObserverNotifier::sObserverId = 0;
-/*static*/ nsDataHashtable<nsUint64HashKey, nsCOMPtr<nsIObserver>>
+/*static*/ nsTHashMap<uint64_t, nsCOMPtr<nsIObserver>>
     AutoObserverNotifier::sSavedObservers;
 
 // The maximum amount of time to let the EnableDragDrop runnable wait in the
