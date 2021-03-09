97857776e30be1d02a7718cfd6e60a11138387c0
mazin <mazinalhaddad05@gmail.com>
2021-03-09 15:46:08 +0000

    Bug 1612738 - Set minimum widget size for mContainer. r=stransky
    Differential Revision: https://phabricator.services.mozilla.com/D105514

--- widget/gtk/nsWindow.cpp
+++ widget/gtk/nsWindow.cpp
@@ -1129,6 +1129,10 @@ void nsWindow::ApplySizeConstraints(void) {
 
     gtk_window_set_geometry_hints(GTK_WINDOW(mShell), nullptr, &geometry,
                                   GdkWindowHints(hints));
+    if (!mIsX11Display) {
+      gtk_widget_set_size_request(GTK_WIDGET(mContainer), geometry.min_width,
+                                  geometry.min_height);
+    }
   }
 }
 
