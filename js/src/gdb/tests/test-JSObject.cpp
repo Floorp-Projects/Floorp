adac84b2accc86caee12e35a6b8416b91e44ac04
Jan de Mooij <jdemooij@mozilla.com>
2021-03-09 14:28:22 +0000

    Bug 1696861 part 1 - Stop marking the global object as delegate. r=tcampbell
    This goes back to when shape teleporting was used for environment chains too.
    See also bug 1608034 where we made this change for (other) environment objects.
    
    The global has a lot of properties and this avoids doing unnecessary work to
    invalidate teleporting.
    
    Add Object.prototype to the test-JSObject GDB test so that we still have a test for
    the delegate flag.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107416

--- js/src/gdb/tests/test-JSObject.cpp
+++ js/src/gdb/tests/test-JSObject.cpp
@@ -7,6 +7,7 @@ FRAGMENT(JSObject, simple) {
 
   JS::Rooted<JSObject*> glob(cx, JS::CurrentGlobalOrNull(cx));
   JS::Rooted<JSObject*> plain(cx, JS_NewPlainObject(cx));
+  JS::Rooted<JSObject*> objectProto(cx, JS::GetRealmObjectPrototype(cx));
   JS::Rooted<JSObject*> global(cx, JS::CurrentGlobalOrNull(cx));
   JS::Rooted<JSObject*> func(
       cx, (JSObject*)JS_NewFunction(cx, (JSNative)1, 0, 0, "dys"));
@@ -31,6 +32,7 @@ FRAGMENT(JSObject, simple) {
 
   use(glob);
   use(plain);
+  use(objectProto);
   use(func);
   use(anon);
   use(funcPtr);
