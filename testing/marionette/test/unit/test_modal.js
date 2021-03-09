340a2daf9db630215bb503b80a8705f3845c7086
Alexandru Michis <malexandru@mozilla.com>
2021-03-10 00:47:12 +0200

    Backed out 3 changesets (bug 1686741) for causing awsy failures in TestMemoryUsage.test_open_tabs
    CLOSED TREE
    
    Backed out changeset 68ceed0186b8 (bug 1686741)
    Backed out changeset edb492e25698 (bug 1686741)
    Backed out changeset 213c96fc82bd (bug 1686741)

--- testing/marionette/test/unit/test_modal.js
+++ testing/marionette/test/unit/test_modal.js
@@ -128,18 +128,6 @@ add_test(function test_handleCallbackOpenTabModalDialog() {
   observer.observe(mockTabModalDialog, "tabmodal-dialog-loaded");
 });
 
-add_test(function test_dialogClosed() {
-  let observer = new modal.DialogObserver();
-
-  observer.dialogClosed(mockTabModalDialog.ownerGlobal).then(() => {
-    run_next_test();
-  });
-  observer.handleEvent({
-    type: "DOMModalDialogClosed",
-    target: mockTabModalDialog,
-  });
-});
-
 add_test(function test_handleCallbackCloseTabModalDialog() {
   let observer = new modal.DialogObserver();
 
