bf39b3bc8ea25009b9de28f35e037a2c7d488f1a
Steven MacLeod <steven@smacleod.ca>
2021-03-09 18:37:26 +0000

    Bug 1647442 - handle OOP ancestors in Document::AdoptNode. r=nika
    We now use BrowsingContext to climb the tree and grab frame elements so
    we skip over any OOP ancestors.
    
    `Document::GetBrowsingContext()` may be `null` when the document was
    just created with `document.implementation.createDocument`.
    `Document::GetWindow()` will also return `null` in this case, so we
    assume the document isn't a descendant of the adoptedNode, preserving
    the old behaviour.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107584

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -9619,16 +9619,20 @@ nsINode* Document::AdoptNode(nsINode& aAdoptedNode, ErrorResult& rv) {
       // a descendant contentDocument, so we check if the frameElement of this
       // document or any of its parents is the adopted node or one of its
       // descendants.
-      Document* doc = this;
-      do {
-        if (nsPIDOMWindowOuter* win = doc->GetWindow()) {
-          nsCOMPtr<nsINode> node = win->GetFrameElementInternal();
-          if (node && node->IsInclusiveDescendantOf(adoptedNode)) {
-            rv.Throw(NS_ERROR_DOM_HIERARCHY_REQUEST_ERR);
-            return nullptr;
-          }
+      RefPtr<BrowsingContext> bc = GetBrowsingContext();
+      while (bc) {
+        nsCOMPtr<nsINode> node = bc->GetEmbedderElement();
+        if (node && node->IsInclusiveDescendantOf(adoptedNode)) {
+          rv.Throw(NS_ERROR_DOM_HIERARCHY_REQUEST_ERR);
+          return nullptr;
         }
-      } while ((doc = doc->GetInProcessParentDocument()));
+
+        if (XRE_IsParentProcess()) {
+          bc = bc->Canonical()->GetParentCrossChromeBoundary();
+        } else {
+          bc = bc->GetParent();
+        }
+      }
 
       // Remove from parent.
       nsCOMPtr<nsINode> parent = adoptedNode->GetParentNode();
