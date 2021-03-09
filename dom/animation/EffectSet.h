cb6c8aead499bc35783547692a127702ddb0fa9a
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-09 10:28:16 +0000

    Bug 1695162 - Make nsTHashtable::ConstIter return a real ConstIterator. r=xpcom-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D107038

--- dom/animation/EffectSet.h
+++ dom/animation/EffectSet.h
@@ -112,38 +112,29 @@ class EffectSet {
   // This allows us to avoid exposing mEffects directly and saves the
   // caller from having to dereference hashtable iterators using
   // the rather complicated: iter.Get()->GetKey().
+  //
+  // XXX Except for the active iterator checks, this could be replaced by the
+  // STL-style iterators of nsTHashtable now.
   class Iterator {
    public:
-    explicit Iterator(EffectSet& aEffectSet)
-        : mEffectSet(aEffectSet),
-          mHashIterator(aEffectSet.mEffects.Iter()),
-          mIsEndIterator(false) {
-#ifdef DEBUG
-      mEffectSet.mActiveIterators++;
-#endif
-    }
+    explicit Iterator(EffectSet& aEffectSet) : Iterator(aEffectSet, false) {}
 
-    Iterator(Iterator&& aOther)
-        : mEffectSet(aOther.mEffectSet),
-          mHashIterator(std::move(aOther.mHashIterator)),
-          mIsEndIterator(aOther.mIsEndIterator) {
-#ifdef DEBUG
-      mEffectSet.mActiveIterators++;
-#endif
-    }
+    Iterator() = delete;
+    Iterator(const Iterator&) = delete;
+    Iterator(Iterator&&) = delete;
+    Iterator& operator=(const Iterator&) = delete;
+    Iterator& operator=(Iterator&&) = delete;
 
     static Iterator EndIterator(EffectSet& aEffectSet) {
-      Iterator result(aEffectSet);
-      result.mIsEndIterator = true;
-      return result;
+      return {aEffectSet, true};
     }
 
-    ~Iterator() {
 #ifdef DEBUG
+    ~Iterator() {
       MOZ_ASSERT(mEffectSet.mActiveIterators > 0);
       mEffectSet.mActiveIterators--;
-#endif
     }
+#endif
 
     bool operator!=(const Iterator& aOther) const {
       if (Done() || aOther.Done()) {
@@ -164,15 +155,24 @@ class EffectSet {
     }
 
    private:
-    Iterator() = delete;
-    Iterator(const Iterator&) = delete;
-    Iterator& operator=(const Iterator&) = delete;
-    Iterator& operator=(const Iterator&&) = delete;
+    Iterator(EffectSet& aEffectSet, bool aIsEndIterator)
+        :
+#ifdef DEBUG
+          mEffectSet(aEffectSet),
+#endif
+          mHashIterator(aEffectSet.mEffects.ConstIter()),
+          mIsEndIterator(aIsEndIterator) {
+#ifdef DEBUG
+      mEffectSet.mActiveIterators++;
+#endif
+    }
 
     bool Done() const { return mIsEndIterator || mHashIterator.Done(); }
 
+#ifdef DEBUG
     EffectSet& mEffectSet;
-    OwningEffectSet::Iterator mHashIterator;
+#endif
+    OwningEffectSet::ConstIterator mHashIterator;
     bool mIsEndIterator;
   };
 
