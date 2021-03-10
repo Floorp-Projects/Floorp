9ec0facdaa50a6ea4440e8bc280566462db7f949
Agi Sferro <agi@sferro.dev>
2021-03-10 22:43:08 +0000

    Bug 1694481 - Remove unused methods in GeckoLoader. r=aklotz
    Differential Revision: https://phabricator.services.mozilla.com/D106181

--- mozglue/android/nsGeckoUtils.cpp
+++ mozglue/android/nsGeckoUtils.cpp
@@ -27,23 +27,6 @@ Java_org_mozilla_gecko_mozglue_GeckoLoader_putenv(JNIEnv* jenv, jclass,
   jenv->ReleaseStringUTFChars(map, str);
 }
 
-extern "C" APKOPEN_EXPORT jboolean MOZ_JNICALL
-Java_org_mozilla_gecko_mozglue_GeckoLoader_verifyCRCs(JNIEnv* jenv, jclass,
-                                                      jstring jApkName) {
-  const char* str;
-  // XXX: java doesn't give us true UTF8, we should figure out something
-  // better to do here
-  str = jenv->GetStringUTFChars(jApkName, nullptr);
-  if (str == nullptr) {
-    return false;
-  }
-
-  RefPtr<Zip> zip = Zip::Create(str);
-  const bool valid = zip->VerifyCRCs();
-  jenv->ReleaseStringUTFChars(jApkName, str);
-  return jboolean(valid);
-}
-
 #ifdef MOZ_CRASHREPORTER
 
 extern "C" __attribute__((visibility("default"))) jboolean MOZ_JNICALL
