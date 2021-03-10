73b987bd31ea47026e18edb878813afc468f1220
Sebastian Streich <sstreich@mozilla.com>
2021-03-10 15:15:24 +0000

    Bug 1685570 Backed out changeset 92d71744323a. r=necko-reviewers,kershaw, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D107670

--- dom/security/ReferrerInfo.cpp
+++ dom/security/ReferrerInfo.cpp
@@ -52,42 +52,6 @@ NS_IMPL_ISUPPORTS_CI(ReferrerInfo, nsIReferrerInfo, nsISerializable)
 #define MIN_CROSS_ORIGIN_SENDING_POLICY 0
 #define MIN_TRIMMING_POLICY 0
 
-/*
- * Default referrer policy to use
- */
-enum DefaultReferrerPolicy : uint32_t {
-  eDefaultPolicyNoReferrer = 0,
-  eDefaultPolicySameOrgin = 1,
-  eDefaultPolicyStrictWhenXorigin = 2,
-  eDefaultPolicyNoReferrerWhenDownGrade = 3,
-};
-
-static uint32_t GetDefaultFirstPartyReferrerPolicyPref(bool privateBrowsing) {
-  return privateBrowsing
-             ? StaticPrefs::network_http_referer_defaultPolicy_pbmode()
-             : StaticPrefs::network_http_referer_defaultPolicy();
-}
-
-static uint32_t GetDefaultThirdPartyReferrerPolicyPref(bool privateBrowsing) {
-  return privateBrowsing
-             ? StaticPrefs::network_http_referer_defaultPolicy_trackers_pbmode()
-             : StaticPrefs::network_http_referer_defaultPolicy_trackers();
-}
-
-static ReferrerPolicy DefaultReferrerPolicyToReferrerPolicy(
-    uint32_t defaultToUse) {
-  switch (defaultToUse) {
-    case DefaultReferrerPolicy::eDefaultPolicyNoReferrer:
-      return ReferrerPolicy::No_referrer;
-    case DefaultReferrerPolicy::eDefaultPolicySameOrgin:
-      return ReferrerPolicy::Same_origin;
-    case DefaultReferrerPolicy::eDefaultPolicyNoReferrerWhenDownGrade:
-      return ReferrerPolicy::No_referrer_when_downgrade;
-  }
-
-  return ReferrerPolicy::Strict_origin_when_cross_origin;
-}
-
 struct LegacyReferrerPolicyTokenMap {
   const char* mToken;
   ReferrerPolicy mPolicy;
@@ -253,10 +217,32 @@ ReferrerPolicy ReferrerInfo::GetDefaultReferrerPolicy(nsIHttpChannel* aChannel,
     }
   }
 
-  return DefaultReferrerPolicyToReferrerPolicy(
-      thirdPartyTrackerIsolated
-          ? GetDefaultThirdPartyReferrerPolicyPref(privateBrowsing)
-          : GetDefaultFirstPartyReferrerPolicyPref(privateBrowsing));
+  uint32_t defaultToUse;
+  if (thirdPartyTrackerIsolated) {
+    if (privateBrowsing) {
+      defaultToUse =
+          StaticPrefs::network_http_referer_defaultPolicy_trackers_pbmode();
+    } else {
+      defaultToUse = StaticPrefs::network_http_referer_defaultPolicy_trackers();
+    }
+  } else {
+    if (privateBrowsing) {
+      defaultToUse = StaticPrefs::network_http_referer_defaultPolicy_pbmode();
+    } else {
+      defaultToUse = StaticPrefs::network_http_referer_defaultPolicy();
+    }
+  }
+
+  switch (defaultToUse) {
+    case DefaultReferrerPolicy::eDefaultPolicyNoReferrer:
+      return ReferrerPolicy::No_referrer;
+    case DefaultReferrerPolicy::eDefaultPolicySameOrgin:
+      return ReferrerPolicy::Same_origin;
+    case DefaultReferrerPolicy::eDefaultPolicyStrictWhenXorigin:
+      return ReferrerPolicy::Strict_origin_when_cross_origin;
+  }
+
+  return ReferrerPolicy::No_referrer_when_downgrade;
 }
 
 /* static */
