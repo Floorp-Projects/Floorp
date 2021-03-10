c2e4916a7ee60a43f731a238e7d1c14b03e1ab7c
Mark Banner <standard8@mozilla.com>
2021-03-10 14:21:13 +0000

    Bug 369739 - Add test to ensure that OpenSearch engines update correctly when the name changes. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D107673

--- toolkit/components/search/tests/xpcshell/test_opensearch_update.js
+++ toolkit/components/search/tests/xpcshell/test_opensearch_update.js
@@ -13,38 +13,55 @@ add_task(async function setup() {
 
 add_task(async function test_engineUpdate() {
   const KEYWORD = "keyword";
-  const FILENAME = "engine.xml";
-  const TOPIC = "browser-search-engine-modified";
   const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 
   let [engine] = await addTestEngines([
-    { name: "Test search engine", xmlFileName: FILENAME },
+    { name: "Test search engine", xmlFileName: "engine.xml" },
   ]);
 
   engine.alias = KEYWORD;
   await Services.search.moveEngine(engine, 0);
+
+  Assert.ok(
+    !Services.search.getEngineByName("A second test engine"),
+    "Should not be able to get the engine by the new name"
+  );
+
   // can't have an accurate updateURL in the file since we can't know the test
   // server origin, so manually set it
-  engine.wrappedJSObject._updateURL = gDataUrl + FILENAME;
+  engine.wrappedJSObject._updateURL = gDataUrl + "engine2.xml";
 
-  await new Promise(resolve => {
-    Services.obs.addObserver(function obs(subject, topic, data) {
-      if (data == "engine-loaded") {
-        let loadedEngine = subject.QueryInterface(Ci.nsISearchEngine);
-        let rawEngine = loadedEngine.wrappedJSObject;
-        equal(loadedEngine.alias, KEYWORD, "Keyword not cleared by update");
-        equal(rawEngine.getAttr("order"), 1, "Order not cleared by update");
-        Services.obs.removeObserver(obs, TOPIC);
-        resolve();
-      }
-    }, TOPIC);
+  let promiseUpdate = SearchTestUtils.promiseSearchNotification(
+    SearchUtils.MODIFIED_TYPE.CHANGED,
+    SearchUtils.TOPIC_ENGINE_MODIFIED
+  );
 
-    // set last update to 8 days ago, since the default interval is 7, then
-    // trigger an update
-    engine.wrappedJSObject.setAttr(
-      "updateexpir",
-      Date.now() - ONE_DAY_IN_MS * 8
-    );
-    Services.search.QueryInterface(Ci.nsITimerCallback).notify(null);
-  });
+  // set last update to 8 days ago, since the default interval is 7, then
+  // trigger an update
+  engine.wrappedJSObject.setAttr("updateexpir", Date.now() - ONE_DAY_IN_MS * 8);
+  Services.search.QueryInterface(Ci.nsITimerCallback).notify(null);
+
+  let changedEngine = await promiseUpdate;
+
+  Assert.equal(
+    engine.name,
+    "A second test engine",
+    "Should have update the engines name"
+  );
+
+  Assert.equal(changedEngine.alias, KEYWORD, "Keyword not cleared by update");
+  Assert.equal(
+    changedEngine.wrappedJSObject.getAttr("order"),
+    1,
+    "Order not cleared by update"
+  );
+
+  Assert.ok(
+    !!Services.search.getEngineByName("A second test engine"),
+    "Should be able to get the engine by the new name"
+  );
+  Assert.ok(
+    !Services.search.getEngineByName("Test search engine"),
+    "Should not be able to get the engine by the old name"
+  );
 });
