3940adf848a7ac938e9813c111ebc726b1bf776d
Kirk Steuber <ksteuber@mozilla.com>
2021-03-10 22:12:24 +0000

    Bug 1691495 - Add tests for app.update.background.enabled and its policy r=nalexander,mkaply
    Note that I made this a browser test rather than an xpcshell test, despite not using any particular browser functionality. I made this choice because I expect to expand this test a bit later, adding checking for preferences UI, much like the browser_policy_app_auto_update.js test in the same directory does.
    
    Depends on D107922
    
    Differential Revision: https://phabricator.services.mozilla.com/D107923

---
+++ browser/components/enterprisepolicies/tests/browser/browser_policy_background_app_update.js
@@ -0,0 +1,76 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+"use strict";
+
+ChromeUtils.defineModuleGetter(
+  this,
+  "UpdateUtils",
+  "resource://gre/modules/UpdateUtils.jsm"
+);
+
+const PREF_NAME = "app.update.background.enabled";
+
+async function test_background_update_pref(expectedEnabled, expectedLocked) {
+  let actualEnabled = await UpdateUtils.readUpdateConfigSetting(PREF_NAME);
+  is(
+    actualEnabled,
+    expectedEnabled,
+    `Actual background update enabled setting should be ${expectedEnabled}`
+  );
+
+  let actualLocked = UpdateUtils.appUpdateSettingIsLocked(PREF_NAME);
+  is(
+    actualLocked,
+    expectedLocked,
+    `Background update enabled setting ${
+      expectedLocked ? "should" : "should not"
+    } be locked`
+  );
+
+  let setSuccess = true;
+  try {
+    await UpdateUtils.writeUpdateConfigSetting(PREF_NAME, actualEnabled);
+  } catch (error) {
+    setSuccess = false;
+  }
+  is(
+    setSuccess,
+    !expectedLocked,
+    `Setting background update pref ${
+      expectedLocked ? "should" : "should not"
+    } fail`
+  );
+}
+
+add_task(async function test_background_app_update_policy() {
+  const origBackgroundUpdateVal = await UpdateUtils.readUpdateConfigSetting(
+    PREF_NAME
+  );
+  registerCleanupFunction(async () => {
+    await UpdateUtils.writeUpdateConfigSetting(
+      PREF_NAME,
+      origBackgroundUpdateVal
+    );
+  });
+
+  await UpdateUtils.writeUpdateConfigSetting(PREF_NAME, true);
+  await test_background_update_pref(true, false);
+
+  await setupPolicyEngineWithJson({
+    policies: {
+      BackgroundAppUpdate: false,
+    },
+  });
+  await test_background_update_pref(false, true);
+
+  await setupPolicyEngineWithJson({});
+  await UpdateUtils.writeUpdateConfigSetting(PREF_NAME, false);
+  await test_background_update_pref(false, false);
+
+  await setupPolicyEngineWithJson({
+    policies: {
+      BackgroundAppUpdate: true,
+    },
+  });
+  await test_background_update_pref(true, true);
+});
