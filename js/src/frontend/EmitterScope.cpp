f58c2488d2c529d66d11508e3341ac5d02b4f44b
Jason Orendorff <jason.orendorff@gmail.com>
2021-03-11 17:54:38 +0000

    Bug 1697223 - Part 2: Statically bind more identifiers in strict direct eval. r=mgaudet
    Toplevel `var` and `function` bindings in strict direct eval are now statically
    bound. Direct eval code is also permitted to search enclosing scopes, the same
    way lazy functions do. References to variables in enclosing scopes will now
    typically use aliased ops.
    
    The test change in class/super-in-nested-eval.js causes the test to continue
    testing what it was testing before, despite the scoping changes in this and
    subsequent patches.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107692

--- js/src/frontend/EmitterScope.cpp
+++ js/src/frontend/EmitterScope.cpp
@@ -173,12 +173,12 @@ NameLocation EmitterScope::searchAndCache(BytecodeEmitter* bce,
   // If the name is not found in the current compilation, walk the Scope
   // chain encompassing the compilation.
   if (!loc) {
-    MOZ_ASSERT(bce->compilationState.input.lazy);
+    MOZ_ASSERT(bce->compilationState.input.lazy ||
+               bce->compilationState.input.target ==
+                   CompilationInput::CompilationTarget::Eval);
     inCurrentScript = false;
-    loc = Some(
-        bce->compilationState.scopeContext.searchInDelazificationEnclosingScope(
-            bce->cx, bce->compilationState.input, bce->parserAtoms(), name,
-            hops));
+    loc = Some(bce->compilationState.scopeContext.searchInEnclosingScope(
+        bce->cx, bce->compilationState.input, bce->parserAtoms(), name, hops));
   }
 
   // Each script has its own frame. A free name that is accessed
@@ -688,9 +688,6 @@ bool EmitterScope::enterEval(BytecodeEmitter* bce, EvalSharedContext* evalsc) {
     return false;
   }
 
-  // For simplicity, treat all free name lookups in eval scripts as dynamic.
-  fallbackFreeNameLocation_ = Some(NameLocation::Dynamic());
-
   // Create the `var` scope. Note that there is also a lexical scope, created
   // separately in emitScript().
   ScopeKind scopeKind =
@@ -704,6 +701,21 @@ bool EmitterScope::enterEval(BytecodeEmitter* bce, EvalSharedContext* evalsc) {
   }
   if (!internBodyScopeCreationData(bce, scopeIndex)) {
     return false;
+  }
+
+  if (evalsc->strict()) {
+    if (evalsc->bindings) {
+      for (ParserBindingIter bi(*evalsc->bindings, true); bi; bi++) {
+        NameLocation loc = NameLocation::fromBinding(bi.kind(), bi.location());
+        if (!putNameInCache(bce, bi.name(), loc)) {
+          return false;
+        }
+      }
+    }
+  } else {
+    // For simplicity, treat all free name lookups in nonstrict eval scripts as
+    // dynamic.
+    fallbackFreeNameLocation_ = Some(NameLocation::Dynamic());
   }
 
   if (hasEnvironment()) {
@@ -723,7 +735,7 @@ bool EmitterScope::enterEval(BytecodeEmitter* bce, EvalSharedContext* evalsc) {
     }
   }
 
-  return true;
+  return checkEnvironmentChainLength(bce);
 }
 
 bool EmitterScope::enterModule(BytecodeEmitter* bce,
