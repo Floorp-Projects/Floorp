adc5d9f2a9be5980383d56513f9dba117bc61743
Mike Hommey <mh+mozilla@glandium.org>
2021-03-11 21:42:04 +0000

    Bug 1693462 - Avoid quarantine attribute being set when using nsILocalFile.copyTo* on macOS. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D107949

--- xpcom/io/nsLocalFileUnix.cpp
+++ xpcom/io/nsLocalFileUnix.cpp
@@ -28,6 +28,10 @@
 #include <ctype.h>
 #include <locale.h>
 
+#if defined(XP_MACOSX)
+#  include <sys/xattr.h>
+#endif
+
 #if defined(HAVE_SYS_QUOTA_H) && defined(HAVE_LINUX_QUOTA_H)
 #  define USE_LINUX_QUOTACTL
 #  include <sys/mount.h>
@@ -908,6 +912,15 @@ nsLocalFile::CopyToNative(nsIFile* aNewParent, const nsACString& aNewName) {
       return NS_OK;
     }
 
+#if defined(XP_MACOSX)
+    bool quarantined = true;
+    if (getxattr(mPath.get(), "com.apple.quarantine", nullptr, 0, 0, 0) == -1) {
+      if (errno == ENOATTR) {
+        quarantined = false;
+      }
+    }
+#endif
+
     PRFileDesc* oldFD;
     rv = OpenNSPRFileDesc(PR_RDONLY, myPerms, &oldFD);
     if (NS_FAILED(rv)) {
@@ -987,6 +1000,13 @@ nsLocalFile::CopyToNative(nsIFile* aNewParent, const nsACString& aNewName) {
               errno);
 #endif
     }
+#if defined(XP_MACOSX)
+    else if (!quarantined) {
+      // If the original file was not in quarantine, lift the quarantine that
+      // file creation added because of LSFileQuarantineEnabled.
+      removexattr(newPathName.get(), "com.apple.quarantine", 0);
+    }
+#endif  // defined(XP_MACOSX)
 
     if (PR_Close(oldFD) < 0) {
       saved_read_close_error = NSRESULT_FOR_ERRNO();
