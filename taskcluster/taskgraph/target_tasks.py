9ae77e4ce3378bd683ac9a86b729ea6b6bd22cb8
Gregory Mierzwinski <gmierz2@outlook.com>
2021-03-11 14:13:09 +0000

    Bug 1696496 - Disable raptor tests running on windows10-64-ref-hw-2017. r=perftest-reviewers,igoldan
    Differential Revision: https://phabricator.services.mozilla.com/D107663

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -1262,40 +1262,6 @@ def target_tasks_raptor_tp6m(full_task_graph, parameters, graph_config):
     return [l for l, t in six.iteritems(full_task_graph.tasks) if filter(t)]
 
 
-@_target_task("raptor_tp6_windows10_64_ref_hw_2017")
-def target_tasks_raptor_tp6_windows10_64_ref_hw_2017(
-    full_task_graph, parameters, graph_config
-):
-    """
-    Select tasks required for running raptor cold tests on raptor_tp6_windows10_64_ref_hw_2017
-    """
-
-    def filter(task):
-        platform = task.attributes.get("test_platform")
-        attributes = task.attributes
-
-        if attributes.get("unittest_suite") != "raptor":
-            return False
-
-        if "windows10-64-ref-hw-2017/opt" not in platform:
-            return False
-
-        try_name = attributes.get("raptor_try_name")
-        if "raptor" in try_name:
-            if "-tp6" in try_name:
-                return False
-            return True
-        elif "browsertime" in try_name:
-            if "-tp6" in try_name:
-                if "tumblr" in try_name:
-                    # See bug 1693108
-                    return False
-                return True
-            return False
-
-    return [l for l, t in six.iteritems(full_task_graph.tasks) if filter(t)]
-
-
 @_target_task("condprof")
 def target_tasks_condprof(full_task_graph, parameters, graph_config):
     """
