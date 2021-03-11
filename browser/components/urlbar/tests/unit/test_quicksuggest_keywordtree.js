8b7f2d29cdbbc9453af53207efb81c1ac7933e9f
Drew Willcoxon <adw@mozilla.com>
2021-03-11 21:00:15 +0000

    Bug 1697678 - Choose quick suggest suggestions only from matching results. r=daleharvey, a=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D107973

--- browser/components/urlbar/tests/unit/test_quicksuggest_keywordtree.js
+++ browser/components/urlbar/tests/unit/test_quicksuggest_keywordtree.js
@@ -6,6 +6,7 @@
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   KeywordTree: "resource:///modules/UrlbarQuickSuggest.jsm",
+  UrlbarQuickSuggest: "resource:///modules/UrlbarQuickSuggest.jsm",
 });
 
 let data = [
@@ -17,9 +18,27 @@ let data = [
     term: "helzo bar",
     keywords: ["helzo b", "helzo ba"],
   },
+  {
+    term: "test1",
+    keywords: ["aaa", "aaab", "aaa111", "aaab111"],
+  },
+  {
+    term: "test2",
+    keywords: ["aaa", "aaab", "aaa222", "aaab222"],
+  },
+
+  // These two "test10" and "test11" objects must be in the following order to
+  // test bug 1697678.
+  { term: "test10", keywords: ["kids sn", "kids sneakers"] },
+  { term: "test11", keywords: ["ki", "kin", "kind", "kindl", "kindle"] },
 ];
 
 function basicChecks(tree) {
+  UrlbarQuickSuggest._results = new Map();
+  for (let { term, keywords } of data) {
+    UrlbarQuickSuggest._results.set(term, { keywords: keywords.concat(term) });
+  }
+
   Assert.equal(tree.get("nomatch").result, null);
   Assert.equal(tree.get("he").result, null);
   Assert.equal(tree.get("hel").result, "helzo foo");
@@ -27,10 +46,46 @@ function basicChecks(tree) {
   Assert.equal(tree.get("helzo").result, "helzo foo");
   Assert.equal(tree.get("helzo").fullKeyword, "helzo");
   Assert.equal(tree.get("helzo ").result, "helzo foo");
+  Assert.equal(tree.get("helzo ").fullKeyword, "helzo foo");
   Assert.equal(tree.get("helzo foo").result, "helzo foo");
   Assert.equal(tree.get("helzo b").result, "helzo bar");
   Assert.equal(tree.get("helzo b").fullKeyword, "helzo bar");
   Assert.equal(tree.get("helzo bar").result, "helzo bar");
+
+  Assert.equal(tree.get("f").result, null);
+  Assert.equal(tree.get("fo").result, null);
+  Assert.equal(tree.get("foo").result, null);
+
+  Assert.equal(tree.get("b").result, null);
+  Assert.equal(tree.get("ba").result, null);
+  Assert.equal(tree.get("bar").result, null);
+
+  Assert.equal(tree.get("aa").result, null);
+  Assert.equal(tree.get("aaa").result, "test2");
+  Assert.equal(tree.get("aaa").fullKeyword, "aaab222");
+  Assert.equal(tree.get("aaab").result, "test2");
+  Assert.equal(tree.get("aaab").fullKeyword, "aaab222");
+  Assert.equal(tree.get("aaab1").result, "test1");
+  Assert.equal(tree.get("aaab1").fullKeyword, "aaab111");
+  Assert.equal(tree.get("aaab2").result, "test2");
+  Assert.equal(tree.get("aaab2").fullKeyword, "aaab222");
+  Assert.equal(tree.get("aaa1").result, "test1");
+  Assert.equal(tree.get("aaa1").fullKeyword, "aaa111");
+  Assert.equal(tree.get("aaa2").result, "test2");
+  Assert.equal(tree.get("aaa2").fullKeyword, "aaa222");
+
+  Assert.equal(tree.get("ki").result, "test11");
+  Assert.equal(tree.get("ki").fullKeyword, "kindle");
+  Assert.equal(tree.get("kin").result, "test11");
+  Assert.equal(tree.get("kin").fullKeyword, "kindle");
+  Assert.equal(tree.get("kind").result, "test11");
+  Assert.equal(tree.get("kind").fullKeyword, "kindle");
+  Assert.equal(tree.get("kid").result, null);
+  Assert.equal(tree.get("kids").result, null);
+  Assert.equal(tree.get("kids ").result, null);
+  Assert.equal(tree.get("kids s").result, null);
+  Assert.equal(tree.get("kids sn").result, "test10");
+  Assert.equal(tree.get("kids sn").fullKeyword, "kids sneakers");
 }
 
 function createTree() {
@@ -60,11 +115,34 @@ add_task(async function test_flatten() {
 
   Assert.deepEqual(
     {
+      k: {
+        i: {
+          "^": "test11",
+          "ds s": { n: { "^": "test10", eaker: { s: { "^": "test10" } } } },
+          ndle: { "^": "test11" },
+        },
+      },
       he: {
         lzo: {
           "^": "helzo foo",
           " ": { foo: { "^": "helzo foo" }, bar: { "^": "helzo bar" } },
         },
+      },
+      aa: {
+        a: {
+          "11": { "1": { "^": "test1" } },
+          "22": { "2": { "^": "test2" } },
+          "^": "test2",
+          b: {
+            "11": { "1": { "^": "test1" } },
+            "22": { "2": { "^": "test2" } },
+            "^": "test2",
+          },
+        },
+      },
+      test: {
+        "1": { "0": { "^": "test10" }, "1": { "^": "test11" }, "^": "test1" },
+        "2": { "^": "test2" },
       },
     },
     tree.toJSONObject()
