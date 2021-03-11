4d3e3067dc4d869b772ebfcf87cfe296413f9a6e
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-03-11 19:36:20 +0000

    Bug 1697171 - Clear various states referring to the old BrowserParent when it enters bfcache, r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D107644

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -600,12 +600,16 @@ void BrowserParent::RemoveWindowListeners() {
   }
 }
 
-void BrowserParent::DestroyInternal() {
+void BrowserParent::Deactivated() {
   UnsetTopLevelWebFocus(this);
   UnsetLastMouseRemoteTarget(this);
   PointerLockManager::ReleaseLockedRemoteTarget(this);
   PointerEventHandler::ReleasePointerCaptureRemoteTarget(this);
   PresShell::ReleaseCapturingRemoteTarget(this);
+}
+
+void BrowserParent::DestroyInternal() {
+  Deactivated();
 
   RemoveWindowListeners();
 
@@ -685,11 +689,7 @@ void BrowserParent::ActorDestroy(ActorDestroyReason why) {
 
   // Even though BrowserParent::Destroy calls this, we need to do it here too in
   // case of a crash.
-  BrowserParent::UnsetTopLevelWebFocus(this);
-  BrowserParent::UnsetLastMouseRemoteTarget(this);
-  PointerLockManager::ReleaseLockedRemoteTarget(this);
-  PointerEventHandler::ReleasePointerCaptureRemoteTarget(this);
-  PresShell::ReleaseCapturingRemoteTarget(this);
+  Deactivated();
 
   if (why == AbnormalShutdown) {
     // dom_reporting_header must also be enabled for the report to be sent.
