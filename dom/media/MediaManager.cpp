73bd7f34e37d893372e9cdf99fae42c192da104d
Bogdan Tara <btara@mozilla.com>
2021-03-13 01:16:10 +0200

    Backed out 3 changesets (bug 1693677) for browser_devices_get_user_media_grace.js failures CLOSED TREE
    Backed out changeset df230c366ab3 (bug 1693677)
    Backed out changeset 816d2b917939 (bug 1693677)
    Backed out changeset f9895c7f1b26 (bug 1693677)

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -480,8 +480,6 @@ class SourceListener : public SupportsWeakPtr {
 
   // Accessed from MediaTrackGraph thread, MediaManager thread, and MainThread
   // No locking needed as they're set on Activate() and never assigned to again.
-  //
-  // A SourceListener may have *both* audio and video for historical reasons.
   UniquePtr<DeviceState> mAudioDeviceState;
   UniquePtr<DeviceState> mVideoDeviceState;
 };
@@ -735,8 +733,7 @@ class GetUserMediaWindowListener {
     for (auto& l : mActiveListeners) {
       if (RefPtr<MediaDevice> device = l->GetAudioDevice()) {
         aOutDevices->AppendElement(device);
-      }
-      if (RefPtr<MediaDevice> device = l->GetVideoDevice()) {
+      } else if (RefPtr<MediaDevice> device = l->GetVideoDevice()) {
         aOutDevices->AppendElement(device);
       }
     }
