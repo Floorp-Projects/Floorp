8f734e8de4ebc59c15748e5d2f6866ab9b7cea61
Ted Campbell <tcampbell@mozilla.com>
2021-03-15 22:20:44 +0000

    Bug 1697904 - Migrate the JIT spectre mitigation prefs to StaticPrefs. r=jandem
    To simplify this code, turns these prefs into unlisted prefs on MIPS
    platforms since the JIT support is missing there. The JitOptions will
    continue to default them to false on MIPS.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108107

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -2643,7 +2643,7 @@ extern JS_PUBLIC_API void JS_SetOffthreadIonCompilationEnabled(JSContext* cx,
   Register(SPECTRE_OBJECT_MITIGATIONS, "spectre.object-mitigations") \
   Register(SPECTRE_STRING_MITIGATIONS, "spectre.string-mitigations") \
   Register(SPECTRE_VALUE_MASKING, "spectre.value-masking") \
-  Register(SPECTRE_JIT_TO_CXX_CALLS, "spectre.jit-to-C++-calls") \
+  Register(SPECTRE_JIT_TO_CXX_CALLS, "spectre.jit-to-cxx-calls") \
   Register(WASM_FOLD_OFFSETS, "wasm.fold-offsets") \
   Register(WASM_DELAY_TIER2, "wasm.delay-tier2") \
   Register(WASM_JIT_BASELINE, "wasm.baseline") \
