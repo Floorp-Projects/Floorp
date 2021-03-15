7d0d356de547e372ff16f90ae9b46430183f773b
Narcis Beleuzu <nbeleuzu@mozilla.com>
2021-03-16 00:15:31 +0200

    Backed out changeset 39ae40980dda (bug 1688653) for bustages on Histograms.json. CLOSED TREE

--- toolkit/components/passwordmgr/test/unit/test_module_LoginCSVImport.js
+++ toolkit/components/passwordmgr/test/unit/test_module_LoginCSVImport.js
@@ -38,10 +38,11 @@ Services.prefs.setBoolPref(
  */
 async function setupCsv(csvLines, extension) {
   // Cleanup state.
-  TTU.getAndClearHistogram("PWMGR_IMPORT_LOGINS_FROM_FILE_CATEGORICAL");
-  TTU.getAndClearHistogram("PWMGR_IMPORT_LOGINS_FROM_FILE_MS");
-  TTU.getAndClearHistogram("PWMGR_IMPORT_LOGINS_FROM_FILE_JANK_MS");
+  TTU.getAndClearKeyedHistogram("FX_MIGRATION_LOGINS_QUANTITY");
+  TTU.getAndClearKeyedHistogram("FX_MIGRATION_LOGINS_IMPORT_MS");
+  TTU.getAndClearKeyedHistogram("FX_MIGRATION_LOGINS_JANK_MS");
   Services.logins.removeAllUserFacingLogins();
+
   let tmpFile = await LoginTestUtils.file.setupCsvFileWithLines(
     csvLines,
     extension
