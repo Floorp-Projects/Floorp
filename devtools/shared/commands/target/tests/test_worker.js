e59877a778182de1b03d098c733b8897153e58ee
Alexandre Poirot <poirot.alex@gmail.com>
2021-03-15 08:08:38 +0000

    Bug 1696375 - [devtools] Move Targets tests to commands folder. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D105615

---
+++ devtools/shared/commands/target/tests/test_worker.js
@@ -0,0 +1,15 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+console.log("[WORKER] started", globalThis.location.toString(), globalThis);
+
+globalThis.onmessage = function(e) {
+  const { type, message } = e.data;
+
+  if (type === "log-in-worker") {
+    // Printing `e` so we can check that we have an object and not a stringified version
+    console.log("[WORKER]", message, e);
+  }
+};
