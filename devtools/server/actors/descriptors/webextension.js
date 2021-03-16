04c65fe8e2a336c5ee2df9f987040e151f2bd17d
Alexandre Poirot <poirot.alex@gmail.com>
2021-03-16 10:31:50 +0000

    Bug 1694352 - [devtools] Avoid debugging addons that are still starting up. r=nchevobbe,rpl
    Differential Revision: https://phabricator.services.mozilla.com/D108032

--- devtools/server/actors/descriptors/webextension.js
+++ devtools/server/actors/descriptors/webextension.js
@@ -68,7 +68,10 @@ const WebExtensionDescriptorActor = protocol.ActorClassWithSpec(
       const policy = ExtensionParent.WebExtensionPolicy.getByID(this.addonId);
       return {
         actor: this.actorID,
-        debuggable: this.addon.isDebuggable,
+        // Note that until the policy becomes active,
+        // getTarget/connectToFrame will fail attaching to the web extension:
+        // https://searchfox.org/mozilla-central/rev/526a5089c61db85d4d43eb0e46edaf1f632e853a/toolkit/components/extensions/WebExtensionPolicy.cpp#551-553
+        debuggable: policy?.active && this.addon.isDebuggable,
         hidden: this.addon.hidden,
         // iconDataURL is available after calling loadIconDataURL
         iconDataURL: this._iconDataURL,
