85aac3e34e646b93be999f84eee6a490b35f16fb
Mike Hommey <mh+mozilla@glandium.org>
2021-03-16 05:18:10 +0000

    Bug 1698697 - Fix BlockingResourceBase code with callstack tracking enabled. r=nika
    Since GetAcquisitionState is never used independently of
    ClearAcquisitionState, we change it to TakeAcquisitionState, such that
    we don't require an expensive array copy in this case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108554

--- xpcom/threads/BlockingResourceBase.h
+++ xpcom/threads/BlockingResourceBase.h
@@ -193,12 +193,21 @@ class BlockingResourceBase {
   }  // NS_NEEDS_RESOURCE(this)
 
   /**
-   * GetAcquisitionState
-   * Return whether or not this resource was acquired.
+   * TakeAcquisitionState
+   * Return whether or not this resource was acquired and mark the resource
+   * as not acquired for subsequent uses.
    *
    * *NOT* thread safe.  Requires ownership of underlying resource.
    */
-  AcquisitionState GetAcquisitionState() { return mAcquired; }
+  AcquisitionState TakeAcquisitionState() {
+#  ifdef MOZ_CALLSTACK_DISABLED
+    bool acquired = mAcquired;
+    ClearAcquisitionState();
+    return acquired;
+#  else
+    return mAcquired.take();
+#  endif
+  }
 
   /**
    * SetAcquisitionState
@@ -206,8 +215,8 @@ class BlockingResourceBase {
    *
    * *NOT* thread safe.  Requires ownership of underlying resource.
    */
-  void SetAcquisitionState(const AcquisitionState& aAcquisitionState) {
-    mAcquired = aAcquisitionState;
+  void SetAcquisitionState(AcquisitionState&& aAcquisitionState) {
+    mAcquired = std::move(aAcquisitionState);
   }
 
   /**
