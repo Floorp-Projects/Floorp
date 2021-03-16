50d50fdf1c882ce0f3972853d252f9ffe5048427
Jan de Mooij <jdemooij@mozilla.com>
2021-03-16 08:21:52 +0000

    Bug 1697803 - Use DOM getter optimization for GetProp on WindowProxy too. r=evilpie
    This is similar to what we do for GetName already.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108429

--- js/src/jit/CacheIR.cpp
+++ js/src/jit/CacheIR.cpp
@@ -1141,11 +1141,19 @@ AttachDecision GetPropIRGenerator::tryAttachWindowProxy(HandleObject obj,
       maybeEmitIdGuard(id);
       ObjOperandId windowObjId =
           GuardAndLoadWindowProxyWindow(writer, objId, windowObj);
-      ValOperandId receiverId = writer.boxObject(windowObjId);
-      EmitCallGetterResult(cx_, writer, windowObj, holder, shape, windowObjId,
-                           receiverId, mode_);
 
-      trackAttached("WindowProxyGetter");
+      if (CanAttachDOMGetterSetter(cx_, JSJitInfo::Getter, windowObj, shape,
+                                   mode_)) {
+        EmitCallDOMGetterResult(cx_, writer, windowObj, holder, shape,
+                                windowObjId);
+        trackAttached("WindowProxyDOMGetter");
+      } else {
+        ValOperandId receiverId = writer.boxObject(windowObjId);
+        EmitCallGetterResult(cx_, writer, windowObj, holder, shape, windowObjId,
+                             receiverId, mode_);
+        trackAttached("WindowProxyGetter");
+      }
+
       return AttachDecision::Attach;
     }
 
