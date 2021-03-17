ae4514b7a8c65578c8aeced3287625a7eebd39db
Florian Quèze <florian@queze.net>
2021-03-17 17:39:35 +0000

    Bug 1665786 - browser_certificateManager.js should wait for the next refresh driver tick instead of relying on the implicit initial waitForCondition timer to wait for strings to be localized, r=keeler.
    Differential Revision: https://phabricator.services.mozilla.com/D108515

--- security/manager/ssl/tests/mochitest/browser/browser_certificateManager.js
+++ security/manager/ssl/tests/mochitest/browser/browser_certificateManager.js
@@ -13,6 +13,9 @@ async function checkServerCertificates(win, expectedValues = []) {
       expectedValues.length
     );
   }, `Expected to have ${expectedValues.length} but got ${win.document.getElementById("serverList").itemChildren.length}`);
+  // The strings we will get from the DOM are localized with Fluent,
+  // so they will only be available after the next refresh driver tick.
+  await new Promise(win.requestAnimationFrame);
 
   let labels = win.document
     .getElementById("serverList")
