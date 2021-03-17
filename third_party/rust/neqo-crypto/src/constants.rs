cb78102ea6071a1b9ff5e86bc6b1478fce006999
Kershaw Chang <kershaw@mozilla.com>
2021-03-17 12:15:54 +0000

    Bug 1698438 - Neqo version 0.4.22 r=necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D108750

--- third_party/rust/neqo-crypto/src/constants.rs
+++ third_party/rust/neqo-crypto/src/constants.rs
@@ -16,11 +16,11 @@ pub type Alert = u8;
 pub type Epoch = u16;
 // TLS doesn't really have an "initial" concept that maps to QUIC so directly,
 // but this should be clear enough.
-pub const TLS_EPOCH_INITIAL: Epoch = 0 as Epoch;
-pub const TLS_EPOCH_ZERO_RTT: Epoch = 1 as Epoch;
-pub const TLS_EPOCH_HANDSHAKE: Epoch = 2 as Epoch;
+pub const TLS_EPOCH_INITIAL: Epoch = 0_u16;
+pub const TLS_EPOCH_ZERO_RTT: Epoch = 1_u16;
+pub const TLS_EPOCH_HANDSHAKE: Epoch = 2_u16;
 // Also, we don't use TLS epochs > 3.
-pub const TLS_EPOCH_APPLICATION_DATA: Epoch = 3 as Epoch;
+pub const TLS_EPOCH_APPLICATION_DATA: Epoch = 3_u16;
 
 /// Rather than defining a type alias and a bunch of constants, which leads to a ton of repetition,
 /// use this macro.
