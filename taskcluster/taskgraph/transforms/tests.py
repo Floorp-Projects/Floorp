4d528ae3f11f1c2943d8b8352ab87c50416df241
Joel Maher <jmaher@mozilla.com>
2021-03-17 15:07:32 +0000

    Bug 1698832 - Turn on Talos Fission and OSX Power tests. r=davehunt
    Turn on Talos fission and OSX Power tests
    
    Differential Revision: https://phabricator.services.mozilla.com/D108678

--- taskcluster/taskgraph/transforms/tests.py
+++ taskcluster/taskgraph/transforms/tests.py
@@ -1760,12 +1760,12 @@ def set_worker_type(config, tasks):
             # This test already has its worker type defined, so just use that (yields below)
             pass
         elif test_platform.startswith("macosx1014-64"):
+            task["worker-type"] = MACOSX_WORKER_TYPES["macosx1014-64"]
+        elif test_platform.startswith("macosx1015-64"):
             if "--power-test" in task["mozharness"]["extra-options"]:
                 task["worker-type"] = MACOSX_WORKER_TYPES["macosx1014-64-power"]
             else:
-                task["worker-type"] = MACOSX_WORKER_TYPES["macosx1014-64"]
-        elif test_platform.startswith("macosx1015-64"):
-            task["worker-type"] = MACOSX_WORKER_TYPES["macosx1015-64"]
+                task["worker-type"] = MACOSX_WORKER_TYPES["macosx1015-64"]
         elif test_platform.startswith("win"):
             # figure out what platform the job needs to run on
             if task["virtualization"] == "hardware":
