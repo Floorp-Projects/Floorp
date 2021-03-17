4adb6b42b8e5190e03918046393d0d4c36fbad62
Henrik Skupin <mail@hskupin.info>
2021-03-17 05:41:27 +0000

    Bug 1691402 - [marionette] Add session module for WebDriver session. r=marionette-reviewers,jdescottes,jgraham
    This patch creates a new module for the WebDriver session
    specific implementation. It no longer requires session data
    like capabilities to be directly bound to the GeckoDriver class.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107552

--- testing/marionette/test/unit/test_session.js
+++ testing/marionette/test/unit/test_session.js
@@ -19,13 +19,57 @@ const {
   Proxy,
   Timeouts,
   UnhandledPromptBehavior,
-} = ChromeUtils.import("chrome://marionette/content/capabilities.js");
+  WebDriverSession,
+} = ChromeUtils.import("chrome://marionette/content/session.js");
 
 // FTP protocol handler is needed for ftpProxy tests
 registerCleanupFunction(function() {
   Preferences.reset("network.ftp.enabled");
 });
 Preferences.set("network.ftp.enabled", true);
+
+add_test(function test_WebDriverSession_ctor() {
+  const session = new WebDriverSession();
+
+  equal(typeof session.id, "string");
+  ok(session.capabilities instanceof Capabilities);
+
+  run_next_test();
+});
+
+add_test(function test_WebDriverSession_getters() {
+  const session = new WebDriverSession();
+
+  equal(
+    session.a11yChecks,
+    session.capabilities.get("moz:accessibilityChecks")
+  );
+  equal(session.pageLoadStrategy, session.capabilities.get("pageLoadStrategy"));
+  equal(session.proxy, session.capabilities.get("proxy"));
+  equal(
+    session.strictFileInteractability,
+    session.capabilities.get("strictFileInteractability")
+  );
+  equal(session.timeouts, session.capabilities.get("timeouts"));
+  equal(
+    session.unhandledPromptBehavior,
+    session.capabilities.get("unhandledPromptBehavior")
+  );
+
+  run_next_test();
+});
+
+add_test(function test_WebDriverSession_setters() {
+  const session = new WebDriverSession();
+
+  const timeouts = new Timeouts();
+  timeouts.pageLoad = 45;
+
+  session.timeouts = timeouts;
+  equal(session.timeouts, session.capabilities.get("timeouts"));
+
+  run_next_test();
+});
 
 add_test(function test_Timeouts_ctor() {
   let ts = new Timeouts();
