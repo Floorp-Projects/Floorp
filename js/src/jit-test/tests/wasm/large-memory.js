9b7d0a1b6333ce3d4684d3e09e5fa9f495a3eee7
Lars T Hansen <lhansen@mozilla.com>
2021-03-17 07:40:17 +0000

    Bug 1698984 - Allow large-memory test to oom.  r=jandem
    This test can oom on some underprovisioned 64-bit platforms like those in our CI,
    so allow that as an outcome.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108740

--- js/src/jit-test/tests/wasm/large-memory.js
+++ js/src/jit-test/tests/wasm/large-memory.js
@@ -1,4 +1,4 @@
-// |jit-test| skip-if: !largeArrayBufferEnabled(); --large-arraybuffers
+// |jit-test| skip-if: !largeArrayBufferEnabled(); --large-arraybuffers; allow-oom
 
 var pagesz = PageSizeInBytes;
 var pages_limit = MaxPagesIn32BitMemory;
