38d659dd74d7c6762c5270dd8cbfe24ba962f124
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-03-17 14:17:31 +0000

    Bug 1699135 - Remove BrowsingContext::SessionHistoryChanged(), r=smacleod
    Differential Revision: https://phabricator.services.mozilla.com/D108785

--- docshell/base/BrowsingContext.h
+++ docshell/base/BrowsingContext.h
@@ -731,8 +731,6 @@ class BrowsingContext : public nsILoadContext, public nsWrapperCache {
 
   bool CrossOriginIsolated();
 
-  void SessionHistoryChanged(int32_t aIndexDelta, int32_t aLengthDelta);
-
   // Check if it is allowed to open a popup from the current browsing
   // context or any of its ancestors.
   bool IsPopupAllowed();
