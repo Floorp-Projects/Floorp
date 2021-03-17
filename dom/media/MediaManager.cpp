470126af4632a6fb2797f7ea4073c46a4f750ae9
Karl Tomlinson <karlt+@karlt.net>
2021-03-17 06:28:17 +0000

    Bug 1699026 - reject MozPromise immediately in GetUserMediaTask::Fail() r=pehrsons
    MozPromise can be rejected from any thread.
    
    "Do after the above runs, as it checks active window list" was obsolete as of
    https://hg.mozilla.org/mozilla-central/rev/99426a6a77bf8ddfb334ed052eb18e2a9bc48122#l3.223
    
    Differential Revision: https://phabricator.services.mozilla.com/D108376

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -1362,13 +1362,8 @@ class GetUserMediaTask final {
 
   void Fail(MediaMgrError::Name aName, const nsCString& aMessage = ""_ns,
             const nsString& aConstraint = u""_ns) {
-    NS_DispatchToMainThread(NS_NewRunnableFunction(
-        "GetUserMediaTask::Fail",
-        [aName, aMessage, aConstraint, holder = std::move(mHolder)]() mutable {
-          holder.Reject(MakeRefPtr<MediaMgrError>(aName, aMessage, aConstraint),
-                        __func__);
-        }));
-    // Do after the above runs, as it checks active window list
+    mHolder.Reject(MakeRefPtr<MediaMgrError>(aName, aMessage, aConstraint),
+                   __func__);
     NS_DispatchToMainThread(NewRunnableMethod(
         "SourceListener::Stop", mSourceListener, &SourceListener::Stop));
   }
