24c8cade19a3fee4cc7917212db9063c0385ea51
Chris H-C <chutten@mozilla.com>
2021-03-17 15:10:38 +0000

    Bug 1686265 - Simplify FOG GeneratedFile statements r=janerik,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D107919

--- toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
+++ toolkit/components/glean/build_scripts/glean_parser_ext/run_glean_parser.py
@@ -6,11 +6,13 @@
 
 import cpp
 import js
+import os
 import re
 import rust
 import sys
 
 from glean_parser import lint, parser, util
+from mozbuild.util import FileAvoidWrite
 from pathlib import Path
 
 
@@ -67,21 +69,17 @@ DEPS_LEN = 13
 
 
 def main(output_fd, *args):
-    args = args[DEPS_LEN:]
+    [cpp_h, js_h] = args[-2:]
+    args = args[DEPS_LEN:-2]
     all_objs, options = parse(args)
+
+    header_path = Path(os.environ["OBJDIR"]) / "toolkit" / "components" / "glean"
+
     rust.output_rust(all_objs, output_fd, options)
-
-
-def cpp_metrics(output_fd, *args):
-    args = args[DEPS_LEN:]
-    all_objs, options = parse(args)
-    cpp.output_cpp(all_objs, output_fd, options)
-
-
-def js_metrics(output_fd, *args):
-    args = args[DEPS_LEN:]
-    all_objs, options = parse(args)
-    js.output_js(all_objs, output_fd, options)
+    with FileAvoidWrite(header_path / cpp_h) as cpp_fd:
+        cpp.output_cpp(all_objs, cpp_fd, options)
+    with FileAvoidWrite(header_path / js_h) as js_fd:
+        js.output_js(all_objs, js_fd, options)
 
 
 if __name__ == "__main__":
