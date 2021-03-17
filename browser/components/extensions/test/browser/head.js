49ed512e6c4850af7042843e54d81cea8c40c211
Florian Quèze <florian@queze.net>
2021-03-17 17:39:32 +0000

    Bug 1680289 - disable bookmarks toolbar animation for browser/components/extensions tests, r=mixedpuppy.
    Depends on D108014
    
    Differential Revision: https://phabricator.services.mozilla.com/D108015

--- browser/components/extensions/test/browser/head.js
+++ browser/components/extensions/test/browser/head.js
@@ -364,12 +364,13 @@ function openBrowserActionPanel(extension, win = window, awaitLoad = false) {
 
 async function toggleBookmarksToolbar(visible = true) {
   let bookmarksToolbar = document.getElementById("PersonalToolbar");
-  let visibilityToggledPromise = TestUtils.waitForCondition(() => {
+  // Third parameter is 'persist' and true is the default.
+  // Fourth parameter is 'animated' and we want no animation.
+  setToolbarVisibility(bookmarksToolbar, visible, true, false);
+
+  return TestUtils.waitForCondition(() => {
     return visible ? !bookmarksToolbar.collapsed : bookmarksToolbar.collapsed;
   }, "waiting for toolbar to become " + (visible ? "visible" : "hidden"));
-
-  setToolbarVisibility(bookmarksToolbar, visible);
-  await visibilityToggledPromise;
 }
 
 async function openContextMenuInPopup(extension, selector = "body") {
