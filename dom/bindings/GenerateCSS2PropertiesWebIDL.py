44b100bed4b16d1d367a5a6f0f48a4fd9f372ea5
Emily McDonough <emcdonough@mozilla.com>
2021-03-18 22:26:24 +0000

    Bug 1647845 Part 1 - Ignore any properties which aren't valid for a style rule when generating CSS2Properties and testing properties r=emilio
    To know the valid rules for each property, we need to put this information
    into the Servo prop list and add an appropriate getter to Longhand/Shorthand.
    
    Differential Revision: https://phabricator.services.mozilla.com/D105825

--- dom/bindings/GenerateCSS2PropertiesWebIDL.py
+++ dom/bindings/GenerateCSS2PropertiesWebIDL.py
@@ -25,6 +25,11 @@ def generate(output, idlFilename, dataFile):
     for p in propList:
         if "Internal" in p.flags:
             continue
+
+        # Skip properties which aren't valid in style rules.
+        if "Style" not in p.rules:
+            continue
+
         # Unfortunately, even some of the getters here are fallible
         # (e.g. on nsComputedDOMStyle).
         extendedAttrs = [
