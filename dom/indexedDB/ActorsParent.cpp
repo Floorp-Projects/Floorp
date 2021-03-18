9131f73d7f6b60fabe2b84310f893c7ba0d5d38e
Tom Tung <shes050117@gmail.com>
2021-03-18 18:45:43 +0000

    Bug 1500116 - Allow mActorDestroyed to be set to true more than one time; r=dom-storage-reviewers,sg
    Differential Revision: https://phabricator.services.mozilla.com/D108918

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -1823,7 +1823,7 @@ class DatabaseOperationBase : public Runnable,
   void NoteActorDestroyed() {
     AssertIsOnOwningThread();
 
-    mActorDestroyed.Flip();
+    mActorDestroyed.EnsureFlipped();
     mOperationMayProceed = false;
   }
 
