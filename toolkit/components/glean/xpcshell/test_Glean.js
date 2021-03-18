d13213e6e62ae1f3d9200cce623f61c0390bc926
Jan-Erik Rediger <jrediger@mozilla.com>
2021-03-18 10:14:59 +0000

    Bug 1678567 - Implement `testGetValue` for events. r=chutten
    Differential Revision: https://phabricator.services.mozilla.com/D108450

--- toolkit/components/glean/xpcshell/test_Glean.js
+++ toolkit/components/glean/xpcshell/test_Glean.js
@@ -151,13 +151,18 @@ add_task(function test_fog_boolean_works() {
 
 add_task(async function test_fog_event_works() {
   Glean.testOnlyIpc.noExtraEvent.record();
-  // FIXME(bug 1678567): Check that the value was recorded when we can.
-  // Assert.ok(Glean.testOnlyIpc.noExtraEvent.testGetValue("store1"));
+  var events = Glean.testOnlyIpc.noExtraEvent.testGetValue();
+  Assert.equal(1, events.length);
+  Assert.equal("test_only.ipc", events[0].category);
+  Assert.equal("no_extra_event", events[0].name);
 
   let extra = { extra1: "can set extras", extra2: "passing more data" };
   Glean.testOnlyIpc.anEvent.record(extra);
-  // FIXME(bug 1678567): Check that the value was recorded when we can.
-  // Assert.ok(Glean.testOnlyIpc.anEvent.testGetValue("store1"));
+  events = Glean.testOnlyIpc.anEvent.testGetValue();
+  Assert.equal(1, events.length);
+  Assert.equal("test_only.ipc", events[0].category);
+  Assert.equal("an_event", events[0].name);
+  Assert.deepEqual(extra, events[0].extra);
 });
 
 add_task(async function test_fog_memory_distribution_works() {
