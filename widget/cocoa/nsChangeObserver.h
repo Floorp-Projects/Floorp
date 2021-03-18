b2e918bec2ff279aac546115b05e8dc1d33f5626
Markus Stange <mstange.moz@gmail.com>
2021-03-18 02:30:37 +0000

    Bug 1698662 - Improve comments on nsChangeObserver. r=harry
    Differential Revision: https://phabricator.services.mozilla.com/D108544

--- widget/cocoa/nsChangeObserver.h
+++ widget/cocoa/nsChangeObserver.h
@@ -30,21 +30,39 @@ class Document;
 // its corresponding content object.
 //
 // This interface is used by our menu code so we only have to have one
-// nsIDocumentObserver.
+// nsIMutationObserver per menu subtree root (e.g. per menubar).
 //
 // Any class that implements this interface must take care to unregister itself
 // on deletion.
+//
+// XXXmstange The methods below use nsIContent*. Eventually, the should be
+// converted to use mozilla::dom::Element* instead.
 class nsChangeObserver {
  public:
-  // XXX use dom::Element
+  // Called when the attribute aAttribute on the element aContent has changed.
+  // Only if aContent is being observed by this nsChangeObserver.
   virtual void ObserveAttributeChanged(mozilla::dom::Document* aDocument,
                                        nsIContent* aContent,
                                        nsAtom* aAttribute) = 0;
 
+  // Called when aChild has been removed from its parent aContainer.
+  // aPreviousSibling is the old previous sibling of aChild.
+  // aContainer is always the old parent node of aChild and of aPreviousSibling.
+  // Only called if aContainer or aContainer's parent node are being observed
+  // by this nsChangeObserver.
+  // In other words: If you observe an element, ObserveContentRemoved is called
+  // if that element's children and grandchildren are removed. NOT if the
+  // observed element itself is removed.
   virtual void ObserveContentRemoved(mozilla::dom::Document* aDocument,
                                      nsIContent* aContainer, nsIContent* aChild,
                                      nsIContent* aPreviousSibling) = 0;
 
+  // Called when aChild has been inserted into its new parent aContainer.
+  // Only called if aContainer or aContainer's parent node are being observed
+  // by this nsChangeObserver.
+  // In other words: If you observe an element, ObserveContentInserted is called
+  // if that element receives a new child or grandchild. NOT if the observed
+  // element itself is inserted anywhere.
   virtual void ObserveContentInserted(mozilla::dom::Document* aDocument,
                                       nsIContent* aContainer,
                                       nsIContent* aChild) = 0;
