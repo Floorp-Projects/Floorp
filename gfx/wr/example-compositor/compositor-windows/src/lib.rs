0a468148949133a5238eb85c0be824e86ff41ae0
Butkovits Atila <abutkovits@mozilla.com>
2021-03-19 00:36:41 +0200

    Backed out changeset 3080c78f7e9e (bug 1695500) for causing bustages. CLOSED TREE

--- gfx/wr/example-compositor/compositor-windows/src/lib.rs
+++ gfx/wr/example-compositor/compositor-windows/src/lib.rs
@@ -88,8 +88,6 @@ extern {
     );
 
     fn com_dc_end_transaction(window: *mut Window);
-
-    fn deinit(window: *mut Window);
 }
 
 pub fn create_window(
@@ -260,8 +258,4 @@ pub fn swap_buffers(window: *mut Window) {
     unsafe {
         com_dc_swap_buffers(window);
     }
-}
-
-pub fn deinit(window: *mut Window) {
-    todo!()
 }
