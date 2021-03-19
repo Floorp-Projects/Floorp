094e3e2281028a1328ddb8396736fad83446d914
Nicolas Silva <nsilva@mozilla.com>
2021-03-19 09:34:27 +0000

    Bug 1687977 - Part 9 - Gracefully handle huge radial gradients. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D108469

--- gfx/wr/webrender/src/prim_store/gradient/mod.rs
+++ gfx/wr/webrender/src/prim_store/gradient/mod.rs
@@ -350,7 +350,7 @@ fn test_struct_sizes() {
     assert_eq!(mem::size_of::<LinearGradientKey>(), 88, "LinearGradientKey size changed");
 
     assert_eq!(mem::size_of::<RadialGradient>(), 72, "RadialGradient size changed");
-    assert_eq!(mem::size_of::<RadialGradientTemplate>(), 128, "RadialGradientTemplate size changed");
+    assert_eq!(mem::size_of::<RadialGradientTemplate>(), 136, "RadialGradientTemplate size changed");
     assert_eq!(mem::size_of::<RadialGradientKey>(), 96, "RadialGradientKey size changed");
 
     assert_eq!(mem::size_of::<ConicGradient>(), 72, "ConicGradient size changed");
