602d0bf518f522ee0b96123a90fe2565e951249b
prathikshaprasadsuman <prathikshaprasadsuman@gmail.com>
2021-03-19 11:37:17 +0000

    Bug 1695353 - WebExtension permission list should suppress the bullet if the list is only 1 item long. r=mconley
    Differential Revision: https://phabricator.services.mozilla.com/D107876

--- browser/base/content/test/webextensions/browser_extension_update_background.js
+++ browser/base/content/test/webextensions/browser_extension_update_background.js
@@ -140,8 +140,11 @@ async function backgroundUpdateTest(url, id, checkIconFn) {
   // The original extension has 1 promptable permission and the new one
   // has 2 (history and <all_urls>) plus 1 non-promptable permission (cookies).
   // So we should only see the 1 new promptable permission in the notification.
-  let list = document.getElementById("addon-webext-perm-list");
-  is(list.childElementCount, 1, "Permissions list contains 1 entry");
+  let singlePermissionEl = document.getElementById(
+    "addon-webext-perm-single-entry"
+  );
+  ok(!singlePermissionEl.hidden, "Single permission entry is not hidden");
+  ok(singlePermissionEl.textContent, "Single permission entry text is set");
 
   // Cancel the update.
   panel.secondaryButton.click();
