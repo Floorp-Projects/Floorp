51f7eb3ae9474a79cb90d517fb211304636b5395
Tom Ritter <tom@mozilla.com>
2021-03-19 19:23:05 +0000

    Bug 1691975: Add logic to remove a file from a moz.build file r=andi
    Differential Revision: https://phabricator.services.mozilla.com/D104757

--- python/mozbuild/mozbuild/test/test_rewrite_mozbuild.py
+++ python/mozbuild/mozbuild/test/test_rewrite_mozbuild.py
@@ -13,6 +13,35 @@ import unittest
 from mozunit import main
 import mozbuild.vendor.rewrite_mozbuild as mu
 
+SAMPLE_MOZBUILD = """
+if CONFIG['OS_ARCH'] != 'Darwin' and CONFIG['CC_TYPE'] in ('clang', 'gcc'):
+    if CONFIG['HAVE_ARM_NEON']:
+        SOURCES += [
+            "pixman-arm-neon-asm-bilinear.S",
+            "pixman-arm-neon-asm.S",
+        ]
+    if CONFIG['HAVE_ARM_SIMD']:
+        SOURCES += [
+            'pixman-arm-simd-asm-scaled.S',
+            'pixman-arm-simd-asm.S']
+
+SOURCES += ['pixman-region32.c',
+    'pixman-solid-fill.c',
+    'pixman-trap.c',
+    'pixman-utils.c',
+    'pixman-x86.c',
+    'pixman.c',
+]
+
+if use_sse2:
+    DEFINES['USE_SSE'] = True
+    DEFINES['USE_SSE2'] = True
+    SOURCES += ['pixman-sse2.c']
+    SOURCES['pixman-sse2.c'].flags += CONFIG['SSE_FLAGS'] + CONFIG['SSE2_FLAGS']
+    if CONFIG['CC_TYPE'] in ('clang', 'gcc'):
+        SOURCES['pixman-sse2.c'].flags += ['-Winline']
+"""
+
 
 class TestUtils(unittest.TestCase):
     def test_normalize_filename(self):
@@ -120,54 +149,24 @@ class TestUtils(unittest.TestCase):
             )
             self.assertEqual(actual, expected)
 
-    def test_mozbuild_rewriting(self):
-        sample_mozbuild = """
-if CONFIG['OS_ARCH'] != 'Darwin' and CONFIG['CC_TYPE'] in ('clang', 'gcc'):
-    if CONFIG['HAVE_ARM_NEON']:
-        SOURCES += [
-            "pixman-arm-neon-asm-bilinear.S",
-            "pixman-arm-neon-asm.S",
-        ]
-    if CONFIG['HAVE_ARM_SIMD']:
-        SOURCES += [
-            'pixman-arm-simd-asm-scaled.S',
-            'pixman-arm-simd-asm.S']
-
-SOURCES += [
-    'pixman-region32.c',
-    'pixman-solid-fill.c',
-    'pixman-trap.c',
-    'pixman-utils.c',
-    'pixman-x86.c',
-    'pixman.c',
-]
-
-if use_sse2:
-    DEFINES['USE_SSE'] = True
-    DEFINES['USE_SSE2'] = True
-    SOURCES += ['pixman-sse2.c']
-    SOURCES['pixman-sse2.c'].flags += CONFIG['SSE_FLAGS'] + CONFIG['SSE2_FLAGS']
-    if CONFIG['CC_TYPE'] in ('clang', 'gcc'):
-        SOURCES['pixman-sse2.c'].flags += ['-Winline']
-"""
+    def test_mozbuild_adding(self):
         test_vectors = [
             # (
             # mozbuild_contents
             # unnormalized_filename_to_add
-            # unnormalized_list_of_files
             # expected_output
             # )
             (
-                sample_mozbuild,
+                SAMPLE_MOZBUILD,
                 "pixman-sse2-more.c",
                 ["pixman-sse2.c"],
-                sample_mozbuild.replace(
+                SAMPLE_MOZBUILD.replace(
                     "SOURCES += ['pixman-sse2.c']",
                     "SOURCES += ['pixman-sse2-more.c','pixman-sse2.c']",
                 ),
             ),
             (
-                sample_mozbuild,
+                SAMPLE_MOZBUILD,
                 "pixman-trap-more.c",
                 [
                     "pixman-region32.c",
@@ -177,36 +176,36 @@ if use_sse2:
                     "pixman-x86.c",
                     "pixman.c",
                 ],
-                sample_mozbuild.replace(
+                SAMPLE_MOZBUILD.replace(
                     "'pixman-trap.c',", "'pixman-trap-more.c',\n    'pixman-trap.c',"
                 ),
             ),
             (
-                sample_mozbuild,
+                SAMPLE_MOZBUILD,
                 "pixman-arm-neon-asm-more.S",
                 [
                     "pixman-arm-neon-asm-bilinear.S",
                     "pixman-arm-neon-asm.S",
                 ],
-                sample_mozbuild.replace(
+                SAMPLE_MOZBUILD.replace(
                     '"pixman-arm-neon-asm.S"',
                     '"pixman-arm-neon-asm-more.S",\n            "pixman-arm-neon-asm.S"',
                 ),
             ),
             (
-                sample_mozbuild,
+                SAMPLE_MOZBUILD,
                 "pixman-arm-simd-asm-smore.S",
                 ["pixman-arm-simd-asm-scaled.S", "pixman-arm-simd-asm.S"],
-                sample_mozbuild.replace(
+                SAMPLE_MOZBUILD.replace(
                     "'pixman-arm-simd-asm.S'",
                     "'pixman-arm-simd-asm-smore.S',\n            'pixman-arm-simd-asm.S'",
                 ),
             ),
             (
-                sample_mozbuild,
+                SAMPLE_MOZBUILD,
                 "pixman-arm-simd-asn.S",
                 ["pixman-arm-simd-asm-scaled.S", "pixman-arm-simd-asm.S"],
-                sample_mozbuild.replace(
+                SAMPLE_MOZBUILD.replace(
                     "'pixman-arm-simd-asm.S'",
                     "'pixman-arm-simd-asm.S',\n            'pixman-arm-simd-asn.S'",
                 ),
@@ -227,7 +226,7 @@ if use_sse2:
             file.write(mozbuild_contents)
             file.close()
 
-            mu.edit_moz_build_file(
+            mu.edit_moz_build_file_to_add_file(
                 filename, unnormalized_filename_to_add, unnormalized_list_of_files
             )
 
@@ -236,6 +235,86 @@ if use_sse2:
             os.remove(filename)
 
             if contents != expected_output:
+                print("File to add:", unnormalized_filename_to_add)
+                print("Contents:")
+                print("-------------------")
+                print(contents)
+                print("-------------------")
+                print("Expected:")
+                print("-------------------")
+                print(expected_output)
+                print("-------------------")
+            self.assertEqual(contents, expected_output)
+
+    def test_mozbuild_removing(self):
+        test_vectors = [
+            # (
+            # mozbuild_contents
+            # unnormalized_filename_to_add
+            # expected_output
+            # )
+            (
+                SAMPLE_MOZBUILD,
+                "pixman-sse2.c",
+                SAMPLE_MOZBUILD.replace(
+                    "SOURCES += ['pixman-sse2.c']",
+                    "SOURCES += []",
+                ),
+            ),
+            (
+                SAMPLE_MOZBUILD,
+                "pixman-trap.c",
+                SAMPLE_MOZBUILD.replace("    'pixman-trap.c',\n", ""),
+            ),
+            (
+                SAMPLE_MOZBUILD,
+                "pixman-arm-neon-asm.S",
+                SAMPLE_MOZBUILD.replace(
+                    '            "pixman-arm-neon-asm.S",\n',
+                    "",
+                ),
+            ),
+            (
+                SAMPLE_MOZBUILD,
+                "pixman-arm-simd-asm.S",
+                SAMPLE_MOZBUILD.replace(
+                    "            'pixman-arm-simd-asm.S'",
+                    "            ",
+                ),
+            ),
+            (
+                SAMPLE_MOZBUILD,
+                "pixman-region32.c",
+                SAMPLE_MOZBUILD.replace(
+                    "'pixman-region32.c',",
+                    "",
+                ),
+            ),
+        ]
+
+        for vector in test_vectors:
+            (
+                mozbuild_contents,
+                unnormalized_filename_to_remove,
+                expected_output,
+            ) = vector
+
+            fd, filename = tempfile.mkstemp(text=True)
+            os.close(fd)
+            file = open(filename, mode="w")
+            file.write(mozbuild_contents)
+            file.close()
+
+            mu.edit_moz_build_file_to_remove_file(
+                filename, unnormalized_filename_to_remove
+            )
+
+            with open(filename) as file:
+                contents = file.read()
+            os.remove(filename)
+
+            if contents != expected_output:
+                print("File to remove:", unnormalized_filename_to_remove)
                 print("Contents:")
                 print("-------------------")
                 print(contents)
