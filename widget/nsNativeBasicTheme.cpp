c6d48b2164c0fd974902c372efe2255cfc9ad448
Emilio Cobos Álvarez <emilio@crisal.io>
2021-03-20 17:04:41 +0000

    Bug 1699931 - Fix checked checkbox radius in the non-native theme. r=mstange
    This also uncovered a minor issue with WebRender disabled where the
    radii was slightly off because it wasn't accounting for the border.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109206

--- widget/nsNativeBasicTheme.cpp
+++ widget/nsNativeBasicTheme.cpp
@@ -851,7 +851,7 @@ void nsNativeBasicTheme::PaintRoundedRectWithRadius(
   // stroke fills exactly the area we want to fill and not more.
   rect.Deflate(borderWidth * 0.5f);
 
-  LayoutDeviceCoord radius(aRadius * aDpiRatio);
+  LayoutDeviceCoord radius(aRadius * aDpiRatio + borderWidth * 0.5f);
   // Fix up the radius if it's too large with the rect we're going to paint.
   {
     LayoutDeviceCoord min = std::min(rect.width, rect.height);
@@ -906,9 +906,10 @@ void nsNativeBasicTheme::PaintCheckboxControl(DrawTarget& aDrawTarget,
   auto [backgroundColor, borderColor] = ComputeCheckboxColors(
       aState, StyleAppearance::Checkbox, aUseSystemColors);
   {
-    const CSSCoord radius = 2.0f;
+    CSSCoord radius = 2.0f;
     CSSCoord borderWidth = kCheckboxRadioBorderWidth;
     if (backgroundColor == borderColor) {
+      radius += borderWidth;
       borderWidth = 0.0f;
     }
     PaintRoundedRectWithRadius(aDrawTarget, aRect, backgroundColor, borderColor,
