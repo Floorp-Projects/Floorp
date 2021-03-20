408c6ea28bd2990d1e4987243af8eefcbaa5d048
Cosmin Sabou <csabou@mozilla.com>
2021-03-20 19:56:27 +0200

    Backed out changeset c793fb0c34e0 (bug 1699931) for Android reftest failures on element-paint-native-widget.html.

--- widget/nsNativeBasicTheme.cpp
+++ widget/nsNativeBasicTheme.cpp
@@ -851,7 +851,7 @@ void nsNativeBasicTheme::PaintRoundedRectWithRadius(
   // stroke fills exactly the area we want to fill and not more.
   rect.Deflate(borderWidth * 0.5f);
 
-  LayoutDeviceCoord radius(aRadius * aDpiRatio + borderWidth * 0.5f);
+  LayoutDeviceCoord radius(aRadius * aDpiRatio);
   // Fix up the radius if it's too large with the rect we're going to paint.
   {
     LayoutDeviceCoord min = std::min(rect.width, rect.height);
@@ -906,10 +906,9 @@ void nsNativeBasicTheme::PaintCheckboxControl(DrawTarget& aDrawTarget,
   auto [backgroundColor, borderColor] = ComputeCheckboxColors(
       aState, StyleAppearance::Checkbox, aUseSystemColors);
   {
-    CSSCoord radius = 2.0f;
+    const CSSCoord radius = 2.0f;
     CSSCoord borderWidth = kCheckboxRadioBorderWidth;
     if (backgroundColor == borderColor) {
-      radius += borderWidth;
       borderWidth = 0.0f;
     }
     PaintRoundedRectWithRadius(aDrawTarget, aRect, backgroundColor, borderColor,
