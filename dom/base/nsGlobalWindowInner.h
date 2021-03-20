a8f24354b022f5a4d8b005d93af0ece9aa4b39c0
Alexandru Michis <malexandru@mozilla.com>
2021-03-20 03:10:02 +0200

    Backed out changeset f588378316fd (bug 1699259) for causing mochitest failures in test_focusrings.xhtml
    CLOSED TREE

--- dom/base/nsGlobalWindowInner.h
+++ dom/base/nsGlobalWindowInner.h
@@ -1131,8 +1131,7 @@ class nsGlobalWindowInner final : public mozilla::dom::EventTarget,
 
   void SetFocusedElement(mozilla::dom::Element* aElement,
                          uint32_t aFocusMethod = 0,
-                         bool aNeedsFocus = false,
-                         bool aWillShowOutline = false) override;
+                         bool aNeedsFocus = false) override;
 
   uint32_t GetFocusMethod() override;
 
