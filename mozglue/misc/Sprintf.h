6cb10c45a45ead1396486eae9678ff9fb06b2787
Mike Hommey <mh+mozilla@glandium.org>
2021-03-22 21:25:30 +0000

    Bug 1699375 - Add {Vs,S}printLiteral-like functions for buffers of arbitrary size. r=nika
    and use them in MozFormatCodeAddress.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108911

--- mozglue/misc/Sprintf.h
+++ mozglue/misc/Sprintf.h
@@ -24,8 +24,8 @@ namespace mozilla {
 namespace detail {
 
 struct MOZ_STACK_CLASS SprintfAppend final : public mozilla::PrintfTarget {
-  template <size_t N>
-  explicit SprintfAppend(char (&aBuf)[N]) : mBuf(aBuf), mBufLen(N) {}
+  explicit SprintfAppend(char* aBuf, size_t aBufLen)
+      : mBuf(aBuf), mBufLen(aBufLen) {}
 
   bool append(const char* aStr, size_t aLen) override {
     if (aLen == 0) {
@@ -50,21 +50,38 @@ struct MOZ_STACK_CLASS SprintfAppend final : public mozilla::PrintfTarget {
 }  // namespace mozilla
 #  endif  // SPRINTF_H_USES_VSNPRINTF
 
+MOZ_FORMAT_PRINTF(3, 0)
+MOZ_MAYBE_UNUSED
+static int VsprintfBuf(char* buffer, size_t bufsize, const char* format,
+                       va_list args) {
+  MOZ_ASSERT(format != buffer);
+#  ifdef SPRINTF_H_USES_VSNPRINTF
+  int result = vsnprintf(buffer, bufsize, format, args);
+  buffer[bufsize - 1] = '\0';
+  return result;
+#  else
+  mozilla::detail::SprintfAppend ss(buffer, bufsize);
+  ss.vprint(format, args);
+  size_t len = ss.emitted();
+  buffer[std::min(len, bufsize - 1)] = '\0';
+  return len;
+#  endif
+}
+
+MOZ_FORMAT_PRINTF(3, 4)
+MOZ_MAYBE_UNUSED
+static int SprintfBuf(char* buffer, size_t bufsize, const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  int result = VsprintfBuf(buffer, bufsize, format, args);
+  va_end(args);
+  return result;
+}
+
 template <size_t N>
 MOZ_FORMAT_PRINTF(2, 0)
 int VsprintfLiteral(char (&buffer)[N], const char* format, va_list args) {
-  MOZ_ASSERT(format != buffer);
-#  ifdef SPRINTF_H_USES_VSNPRINTF
-  int result = vsnprintf(buffer, N, format, args);
-  buffer[N - 1] = '\0';
-  return result;
-#  else
-  mozilla::detail::SprintfAppend ss(buffer);
-  ss.vprint(format, args);
-  size_t len = ss.emitted();
-  buffer[std::min(len, N - 1)] = '\0';
-  return len;
-#  endif
+  return VsprintfBuf(buffer, N, format, args);
 }
 
 template <size_t N>
