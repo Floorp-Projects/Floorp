cd1e4d72091111689b88c87529d08413c114fb31
Henrik Skupin <mail@hskupin.info>
2021-03-22 16:44:42 +0000

    Bug 1699627 - [marionette] Only enable Marionette when MOZ_MARIONETTE is set to "1". r=marionette-reviewers,freddyb
    Differential Revision: https://phabricator.services.mozilla.com/D109037

--- testing/marionette/components/marionette.js
+++ testing/marionette/components/marionette.js
@@ -307,7 +307,7 @@ class MarionetteParentProcess {
     this.alteredPrefs = new Set();
 
     // Initially set the enabled state based on the environment variable.
-    this.enabled = env.exists(ENV_ENABLED);
+    this.enabled = env.get(ENV_ENABLED) === "1";
 
     Services.ppmm.addMessageListener("Marionette:IsRunning", this);
   }
