13f39ef6e8804aefda0298a815fdb240572bec58
Edgar Chen <echen@mozilla.com>
2021-03-23 10:29:14 +0000

    Bug 1700241 - Mark click event as position-less event when accesskey of label element is pressed; r=masayuki
    The click event dispatched for accesskey on other elements is marked as position-less event.
    And other simulated click events are also marked as position-less, e.g. click() call, keydown Enter ...
    
    label element should just behave the same on accesskey for consistency.
    
    It is not clear on spec, see https://github.com/w3c/csswg-drafts/issues/1070,
    but blink/webkit both returns 0 for offset{X|Y}.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109451

--- dom/html/HTMLLabelElement.cpp
+++ dom/html/HTMLLabelElement.cpp
@@ -191,15 +191,9 @@ bool HTMLLabelElement::PerformAccesskey(bool aKeyCausesActivation,
     }
 
     // Click on it if the users prefs indicate to do so.
-    WidgetMouseEvent event(aIsTrustedEvent, eMouseClick, nullptr,
-                           WidgetMouseEvent::eReal);
-    event.mInputSource = MouseEvent_Binding::MOZ_SOURCE_KEYBOARD;
-
     AutoPopupStatePusher popupStatePusher(
         aIsTrustedEvent ? PopupBlocker::openAllowed : PopupBlocker::openAbused);
-
-    EventDispatcher::Dispatch(static_cast<nsIContent*>(this), presContext,
-                              &event);
+    DispatchSimulatedClick(this, aIsTrustedEvent, presContext);
   }
 
   return aKeyCausesActivation;
