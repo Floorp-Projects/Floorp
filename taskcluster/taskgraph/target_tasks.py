be7075966114799c6b48f39bb33196e31c4e6be8
Tom Ritter <tom@mozilla.com>
2021-03-23 17:17:52 +0000

    Bug 1699740 - Make updatebot run on a cron job in m-c r=aki
    Differential Revision: https://phabricator.services.mozilla.com/D109076

--- taskcluster/taskgraph/target_tasks.py
+++ taskcluster/taskgraph/target_tasks.py
@@ -1074,6 +1074,13 @@ def target_tasks_coverity_full(full_task_graph, parameters, graph_config):
     return ["source-test-coverity-coverity-full-analysis"]
 
 
+# Run Updatebot's cron job 4 times daily.
+@_target_task("updatebot_cron")
+def target_tasks_updatebot_cron(full_task_graph, parameters, graph_config):
+    """Select tasks required to run Updatebot's cron job"""
+    return ["updatebot-cron"]
+
+
 @_target_task("customv8_update")
 def target_tasks_customv8_update(full_task_graph, parameters, graph_config):
     """Select tasks required for building latest d8/v8 version."""
