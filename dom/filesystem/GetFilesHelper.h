1657ef316ab4b0aed7588036666a9a67e5628774
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-23 10:36:33 +0000

    Bug 708901 - Migrate to nsTHashSet in dom/filesystem. r=baku
    Depends on D108594
    
    Differential Revision: https://phabricator.services.mozilla.com/D108595

--- dom/filesystem/GetFilesHelper.h
+++ dom/filesystem/GetFilesHelper.h
@@ -12,7 +12,7 @@
 #include "nsCycleCollectionTraversalCallback.h"
 #include "nsHashKeys.h"
 #include "nsTArray.h"
-#include "nsTHashtable.h"
+#include "nsTHashSet.h"
 #include "nsThreadUtils.h"
 
 class nsIGlobalObject;
@@ -59,7 +59,7 @@ class GetFilesHelperBase {
 
   // We populate this array in the I/O thread with the BlobImpl.
   FallibleTArray<RefPtr<BlobImpl>> mTargetBlobImplArray;
-  nsTHashtable<nsStringHashKey> mExploredDirectories;
+  nsTHashSet<nsString> mExploredDirectories;
 };
 
 // Retrieving the list of files can be very time/IO consuming. We use this
