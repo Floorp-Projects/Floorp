d5aee9ed277c9d57cfb633c0a4194ecfdf18cecd
Markus Stange <mstange.moz@gmail.com>
2021-03-23 13:40:26 +0000

    Bug 1699792 - Account for the app menu in nsMenuBarX::CalculateNativeInsertionPoint. r=harry
    Depends on D109119
    
    Differential Revision: https://phabricator.services.mozilla.com/D109120

--- widget/cocoa/nsMenuX.mm
+++ widget/cocoa/nsMenuX.mm
@@ -624,13 +624,8 @@ void nsMenuX::ObserveAttributeChanged(dom::Document* aDocument, nsIContent* aCon
         mVisible = false;
       } else {
         if (parentType == eMenuBarObjectType) {
-          // Before inserting we need to figure out if we should take the native
-          // application menu into account.
           nsMenuBarX* mb = static_cast<nsMenuBarX*>(mParent);
           NSInteger insertionIndex = mb->CalculateNativeInsertionPoint(this);
-          if (mb->MenuContainsAppMenu()) {
-            insertionIndex++;
-          }
           [parentMenu insertItem:mNativeMenuItem atIndex:insertionIndex];
         } else if (parentType == eSubmenuObjectType) {
           nsMenuX* parentMenuX = static_cast<nsMenuX*>(mParent);
