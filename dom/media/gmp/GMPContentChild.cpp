b07f34785d3f450479c022f6d69d368ec591a382
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-24 16:59:00 +0000

    Bug 708901 - Migrate to nsTHashSet in ipc. r=ipc-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D109322

--- dom/media/gmp/GMPContentChild.cpp
+++ dom/media/gmp/GMPContentChild.cpp
@@ -98,19 +98,19 @@ void GMPContentChild::CloseActive() {
   // Invalidate and remove any remaining API objects.
   const ManagedContainer<PGMPVideoDecoderChild>& videoDecoders =
       ManagedPGMPVideoDecoderChild();
-  for (auto iter = videoDecoders.ConstIter(); !iter.Done(); iter.Next()) {
-    iter.Get()->GetKey()->SendShutdown();
+  for (const auto& key : videoDecoders) {
+    key->SendShutdown();
   }
 
   const ManagedContainer<PGMPVideoEncoderChild>& videoEncoders =
       ManagedPGMPVideoEncoderChild();
-  for (auto iter = videoEncoders.ConstIter(); !iter.Done(); iter.Next()) {
-    iter.Get()->GetKey()->SendShutdown();
+  for (const auto& key : videoEncoders) {
+    key->SendShutdown();
   }
 
   const ManagedContainer<PChromiumCDMChild>& cdms = ManagedPChromiumCDMChild();
-  for (auto iter = cdms.ConstIter(); !iter.Done(); iter.Next()) {
-    iter.Get()->GetKey()->SendShutdown();
+  for (const auto& key : cdms) {
+    key->SendShutdown();
   }
 }
 
