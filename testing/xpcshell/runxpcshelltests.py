2586a80626186dd222981ecd9634ff85f2684315
Brindusan Cristian <cbrindusan@mozilla.com>
2021-03-24 23:22:21 +0200

    Backed out changeset d47a138a3b50 (bug 1668903) for causing xpcshell failures. CLOSED TREE

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -795,8 +795,6 @@ class XPCShellTestThread(Thread):
 
         if self.test_object.get("dmd") == "true":
             self.env["PYTHON"] = sys.executable
-            if build:
-                self.env["PYTHON3"] = build.substs.get("PYTHON3")
             self.env["BREAKPAD_SYMBOLS_PATH"] = self.symbolsPath
 
         if self.test_object.get("subprocess") == "true":
