8a5cfc5cdd541a7c1642f45c29bfd502f8dc709d
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-24 16:59:01 +0000

    Bug 708901 - Migrate to nsTHashSet in dom/grid. r=mats
    Differential Revision: https://phabricator.services.mozilla.com/D109330

--- dom/grid/Grid.cpp
+++ dom/grid/Grid.cpp
@@ -11,6 +11,7 @@
 #include "mozilla/dom/Element.h"
 #include "mozilla/dom/GridBinding.h"
 #include "nsGridContainerFrame.h"
+#include "nsTHashSet.h"
 
 namespace mozilla::dom {
 
@@ -34,13 +35,13 @@ Grid::Grid(nsISupports* aParent, nsGridContainerFrame* aFrame)
 
   // Add implicit areas first. Track the names that we add here, because
   // we will ignore future explicit areas with the same name.
-  nsTHashtable<nsRefPtrHashKey<nsAtom>> namesSeen;
+  nsTHashSet<RefPtr<nsAtom>> namesSeen;
   nsGridContainerFrame::ImplicitNamedAreas* implicitAreas =
       aFrame->GetImplicitNamedAreas();
   if (implicitAreas) {
     for (auto iter = implicitAreas->iter(); !iter.done(); iter.next()) {
       auto& areaInfo = iter.get().value();
-      namesSeen.PutEntry(areaInfo.name.AsAtom());
+      namesSeen.Insert(areaInfo.name.AsAtom());
       GridArea* area =
           new GridArea(this, areaInfo.name.AsAtom(), GridDeclaration::Implicit,
                        areaInfo.rows.start, areaInfo.rows.end,
