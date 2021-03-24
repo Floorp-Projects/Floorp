9a52865119077a6ac223785ab9cd18ea15acb477
Agi Sferro <agi@sferro.dev>
2021-03-24 20:06:27 +0000

    Bug 1697844 - Remove unused Fennec code from org.mozilla.gecko. r=esawin
    Differential Revision: https://phabricator.services.mozilla.com/D108071

--- mobile/android/geckoview/src/main/java/org/mozilla/gecko/ScreenManagerHelper.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/gecko/ScreenManagerHelper.java
@@ -11,13 +11,6 @@ import org.mozilla.gecko.annotation.WrapForJNI;
 class ScreenManagerHelper {
 
     /**
-     * The following display types use the same definition in nsIScreen.idl
-     */
-    final static int DISPLAY_PRIMARY  = 0; // primary screen
-    final static int DISPLAY_EXTERNAL = 1; // wired displays, such as HDMI, DisplayPort, etc.
-    final static int DISPLAY_VIRTUAL  = 2; // wireless displays, such as Chromecast, WiFi-Display, etc.
-
-    /**
      * Trigger a refresh of the cached screen information held by Gecko.
      */
     public static void refreshScreenInfo() {
@@ -30,28 +23,4 @@ class ScreenManagerHelper {
 
     @WrapForJNI(stubName = "RefreshScreenInfo", dispatchTo = "gecko")
     private native static void nativeRefreshScreenInfo();
-
-    /**
-     * Add a new nsScreen when a new display in Android is available.
-     *
-     * @param displayType the display type of the nsScreen would be added
-     * @param width       the width of the new nsScreen
-     * @param height      the height of the new nsScreen
-     * @param density     the density of the new nsScreen
-     *
-     * @return            return the ID of the added nsScreen
-     */
-    @WrapForJNI
-    public native static int addDisplay(int displayType,
-                                        int width,
-                                        int height,
-                                        float density);
-
-    /**
-     * Remove the nsScreen by the specific screen ID.
-     *
-     * @param screenId    the ID of the screen would be removed.
-     */
-    @WrapForJNI
-    public native static void removeDisplay(int screenId);
 }
