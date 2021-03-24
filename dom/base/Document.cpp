4bdf674268d95b9a7bd5b59672db3cb4659508d3
Masayuki Nakano <masayuki@d-toybox.com>
2021-03-24 01:55:08 +0000

    Bug 1220696 - part 4: Make `Document` consider whether the target is editable or not-editable with target editor r=smaug
    Currently, `Document` checks it only with whether the document is editable
    or not.  Only with this check, `execCommand` and the other related methods
    work only when there is `contenteditable`.
    
    Therefore, this patch makes it to check whether the target is editable or not
    with target editor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D108570

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -5103,6 +5103,21 @@ TextEditor* Document::AutoEditorCommandTarget::GetTargetEditor() const {
   return nullptr;
 }
 
+bool Document::AutoEditorCommandTarget::IsEditable(Document* aDocument) const {
+  if (RefPtr<Document> doc = aDocument->GetInProcessParentDocument()) {
+    // Make sure frames are up to date, since that can affect whether
+    // we're editable.
+    doc->FlushPendingNotifications(FlushType::Frames);
+  }
+  TextEditor* targetEditor = GetTargetEditor();
+  if (targetEditor && targetEditor->IsTextEditor()) {
+    // FYI: When `disabled` attribute is set, `TextEditor` treats it as
+    //      "readonly" too.
+    return !targetEditor->IsReadonly();
+  }
+  return aDocument->IsEditingOn();
+}
+
 bool Document::AutoEditorCommandTarget::IsCommandEnabled() const {
   TextEditor* targetEditor = GetTargetEditor();
   if (!targetEditor) {
@@ -5187,11 +5202,6 @@ bool Document::ExecCommand(const nsAString& aHTMLCommandName, bool aShowUI,
     return false;
   }
 
-  // if editing is not on, bail
-  if (commandData.IsAvailableOnlyWhenEditable() && !IsEditingOnAfterFlush()) {
-    return false;
-  }
-
   if (commandData.mCommand == Command::GetHTML) {
     return false;
   }
@@ -5217,6 +5227,11 @@ bool Document::ExecCommand(const nsAString& aHTMLCommandName, bool aShowUI,
   // by order of controllers in `nsCommandManager::GetControllerForCommand()`.
   RefPtr<nsPresContext> presContext = GetPresContext();
   AutoEditorCommandTarget editCommandTarget(presContext, commandData);
+  if (commandData.IsAvailableOnlyWhenEditable() &&
+      !editCommandTarget.IsEditable(this)) {
+    return false;
+  }
+
   if (editCommandTarget.DoNothing()) {
     return false;
   }
@@ -5359,13 +5374,12 @@ bool Document::QueryCommandEnabled(const nsAString& aHTMLCommandName,
     return false;
   }
 
-  // if editing is not on, bail
-  if (!IsEditingOnAfterFlush()) {
+  RefPtr<nsPresContext> presContext = GetPresContext();
+  AutoEditorCommandTarget editCommandTarget(presContext, commandData);
+  if (!editCommandTarget.IsEditable(this)) {
     return false;
   }
 
-  RefPtr<nsPresContext> presContext = GetPresContext();
-  AutoEditorCommandTarget editCommandTarget(presContext, commandData);
   if (editCommandTarget.IsEditor()) {
     return editCommandTarget.IsCommandEnabled();
   }
@@ -5399,13 +5413,11 @@ bool Document::QueryCommandIndeterm(const nsAString& aHTMLCommandName,
     return false;
   }
 
-  // if editing is not on, bail
-  if (!IsEditingOnAfterFlush()) {
-    return false;
-  }
-
   RefPtr<nsPresContext> presContext = GetPresContext();
   AutoEditorCommandTarget editCommandTarget(presContext, commandData);
+  if (!editCommandTarget.IsEditable(this)) {
+    return false;
+  }
   RefPtr<nsCommandParams> params = new nsCommandParams();
   if (editCommandTarget.IsEditor()) {
     if (NS_FAILED(editCommandTarget.GetCommandStateParams(*params))) {
@@ -5449,11 +5461,6 @@ bool Document::QueryCommandState(const nsAString& aHTMLCommandName,
     return false;
   }
 
-  // if editing is not on, bail
-  if (!IsEditingOnAfterFlush()) {
-    return false;
-  }
-
   if (aHTMLCommandName.LowerCaseEqualsLiteral("usecss")) {
     // Per spec, state is supported for styleWithCSS but not useCSS, so we just
     // return false always.
@@ -5462,6 +5469,9 @@ bool Document::QueryCommandState(const nsAString& aHTMLCommandName,
 
   RefPtr<nsPresContext> presContext = GetPresContext();
   AutoEditorCommandTarget editCommandTarget(presContext, commandData);
+  if (!editCommandTarget.IsEditable(this)) {
+    return false;
+  }
   RefPtr<nsCommandParams> params = new nsCommandParams();
   if (editCommandTarget.IsEditor()) {
     if (NS_FAILED(editCommandTarget.GetCommandStateParams(*params))) {
@@ -5588,13 +5598,11 @@ void Document::QueryCommandValue(const nsAString& aHTMLCommandName,
     return;
   }
 
-  // if editing is not on, bail
-  if (!IsEditingOnAfterFlush()) {
-    return;
-  }
-
   RefPtr<nsPresContext> presContext = GetPresContext();
   AutoEditorCommandTarget editCommandTarget(presContext, commandData);
+  if (!editCommandTarget.IsEditable(this)) {
+    return;
+  }
   RefPtr<nsCommandParams> params = new nsCommandParams();
   // FYI: Only GetHTML command is not implemented by editor.  Use window's
   //      command table instead.
@@ -5653,17 +5661,6 @@ void Document::QueryCommandValue(const nsAString& aHTMLCommandName,
   nsAutoCString result;
   params->GetCString("state_attribute", result);
   CopyUTF8toUTF16(result, aValue);
-}
-
-bool Document::IsEditingOnAfterFlush() {
-  RefPtr<Document> doc = GetInProcessParentDocument();
-  if (doc) {
-    // Make sure frames are up to date, since that can affect whether
-    // we're editable.
-    doc->FlushPendingNotifications(FlushType::Frames);
-  }
-
-  return IsEditingOn();
 }
 
 void Document::MaybeEditingStateChanged() {
