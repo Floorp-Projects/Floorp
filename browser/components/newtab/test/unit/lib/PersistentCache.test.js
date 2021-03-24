120b60db084cb4ecafcd2eba95674481c6cd48bf
Barret Rennie <barret@brennie.ca>
2021-03-24 18:17:57 +0000

    Bug 1649607 - Migrate PersistentCache.jsm to IOUtils and PathUtils r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D96887

--- browser/components/newtab/test/unit/lib/PersistentCache.test.js
+++ browser/components/newtab/test/unit/lib/PersistentCache.test.js
@@ -2,9 +2,8 @@ import { GlobalOverrider } from "test/unit/utils";
 import { PersistentCache } from "lib/PersistentCache.jsm";
 
 describe("PersistentCache", () => {
-  let fakeOS;
-  let fakeJsonParse;
-  let fakeFetch;
+  let fakeIOUtils;
+  let fakePathUtils;
   let cache;
   let filename = "cache.json";
   let reportErrorStub;
@@ -14,19 +13,18 @@ describe("PersistentCache", () => {
   beforeEach(() => {
     globals = new GlobalOverrider();
     sandbox = sinon.createSandbox();
-    fakeOS = {
-      Constants: { Path: { localProfileDir: "/foo/bar" } },
-      File: {
-        writeAtomic: sinon.stub().returns(Promise.resolve()),
-      },
-      Path: { join: () => filename },
+    fakeIOUtils = {
+      writeJSON: sinon.stub().resolves(0),
+      readJSON: sinon.stub().resolves({}),
     };
-    fakeJsonParse = sandbox.stub().resolves({});
-    fakeFetch = sandbox.stub().resolves({ json: fakeJsonParse });
+    fakePathUtils = {
+      join: sinon.stub().returns(filename),
+      getLocalProfileDir: sinon.stub().resolves("/"),
+    };
     reportErrorStub = sandbox.stub();
-    globals.set("OS", fakeOS);
     globals.set("Cu", { reportError: reportErrorStub });
-    globals.set("fetch", fakeFetch);
+    globals.set("IOUtils", fakeIOUtils);
+    globals.set("PathUtils", fakePathUtils);
 
     cache = new PersistentCache(filename);
   });
@@ -36,28 +34,41 @@ describe("PersistentCache", () => {
   });
 
   describe("#get", () => {
-    it("tries to fetch the file", async () => {
+    it("tries to read the file", async () => {
       await cache.get("foo");
-      assert.calledOnce(fakeFetch);
+      assert.calledOnce(fakeIOUtils.readJSON);
     });
-    it("doesnt try to parse file if it doesn't exist", async () => {
-      fakeFetch.throws();
-      await cache.get("foo");
-      assert.notCalled(fakeJsonParse);
-    });
-    it("doesnt try to fetch the file if it was already loaded", async () => {
+    it("doesnt try to read the file if it was already loaded", async () => {
       await cache._load();
-      fakeFetch.resetHistory();
+      fakeIOUtils.readJSON.resetHistory();
       await cache.get("foo");
-      assert.notCalled(fakeFetch);
+      assert.notCalled(fakeIOUtils.readJSON);
     });
     it("should catch and report errors", async () => {
-      fakeJsonParse.throws();
+      fakeIOUtils.readJSON.rejects(new SyntaxError("Failed to parse JSON"));
       await cache._load();
       assert.calledOnce(reportErrorStub);
+
+      cache._cache = undefined;
+      reportErrorStub.resetHistory();
+
+      fakeIOUtils.readJSON.rejects(
+        new DOMException("IOUtils shutting down", "AbortError")
+      );
+      await cache._load();
+      assert.calledOnce(reportErrorStub);
+
+      cache._cache = undefined;
+      reportErrorStub.resetHistory();
+
+      fakeIOUtils.readJSON.rejects(
+        new DOMException("File not found", "NotFoundError")
+      );
+      await cache._load();
+      assert.notCalled(reportErrorStub);
     });
     it("returns data for a given cache key", async () => {
-      fakeJsonParse.resolves({ foo: "bar" });
+      fakeIOUtils.readJSON.resolves({ foo: "bar" });
       let value = await cache.get("foo");
       assert.equal(value, "bar");
     });
@@ -66,27 +77,23 @@ describe("PersistentCache", () => {
       assert.equal(value, undefined);
     });
     it("returns all the data if no cache key is specified", async () => {
-      fakeJsonParse.resolves({ foo: "bar" });
+      fakeIOUtils.readJSON.resolves({ foo: "bar" });
       let value = await cache.get();
       assert.deepEqual(value, { foo: "bar" });
     });
   });
 
   describe("#set", () => {
-    it("tries to fetch the file on the first set", async () => {
+    it("tries to read the file on the first set", async () => {
       await cache.set("foo", { x: 42 });
-      assert.calledOnce(fakeFetch);
+      assert.calledOnce(fakeIOUtils.readJSON);
     });
-    it("doesnt try to fetch the file if it was already loaded", async () => {
+    it("doesnt try to read the file if it was already loaded", async () => {
       cache = new PersistentCache(filename, true);
       await cache._load();
-      fakeFetch.resetHistory();
+      fakeIOUtils.readJSON.resetHistory();
       await cache.set("foo", { x: 42 });
-      assert.notCalled(fakeFetch);
-    });
-    it("tries to fetch the file on the first set", async () => {
-      await cache.set("foo", { x: 42 });
-      assert.calledOnce(fakeFetch);
+      assert.notCalled(fakeIOUtils.readJSON);
     });
     it("sets a string value", async () => {
       const key = "testkey";
@@ -105,18 +112,18 @@ describe("PersistentCache", () => {
     it("writes the data to file", async () => {
       const key = "testkey";
       const value = { x: 1, y: 2, z: 3 };
-      fakeOS.File.exists = async () => false;
+
       await cache.set(key, value);
-      assert.calledOnce(fakeOS.File.writeAtomic);
+      assert.calledOnce(fakeIOUtils.writeJSON);
       assert.calledWith(
-        fakeOS.File.writeAtomic,
+        fakeIOUtils.writeJSON,
         filename,
-        `{"testkey":{"x":1,"y":2,"z":3}}`,
+        { [[key]]: value },
         { tmpPath: `${filename}.tmp` }
       );
     });
-    it("throws when failing the file", async () => {
-      sandbox.stub(OS.Path, "join").throws("bad file");
+    it("throws when failing to get file path", async () => {
+      fakePathUtils.getLocalProfileDir.rejects(new Error());
 
       let rejected = false;
       try {
