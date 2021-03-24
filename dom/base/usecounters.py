3136f3fd8aefac657084c18f42136f2688866fc8
akshita <gakshita1997@gmail.com>
2021-03-24 15:26:06 +0000

    Bug 1622676 - Made dom/base flake8 compliant.r=championshuttler,sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D108310

--- dom/base/usecounters.py
+++ dom/base/usecounters.py
@@ -2,11 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import buildconfig
 import collections
 import re
-from six import StringIO
-import sys
 
 
 def read_conf(conf_filename):
