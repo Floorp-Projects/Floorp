0bd5fae7dcdd67c0d2288b01aee383ccc5b1c068
Mike Hommey <mh+mozilla@glandium.org>
2021-03-24 05:15:10 +0000

    Bug 1699380 - Add an arm64 mac toolchain task for dump-syms. r=firefox-build-system-reviewers,mhentges
    Differential Revision: https://phabricator.services.mozilla.com/D108922

--- taskcluster/scripts/misc/build-dump-syms.sh
+++ taskcluster/scripts/misc/build-dump-syms.sh
@@ -30,15 +30,20 @@ cd $MOZ_FETCHES_DIR/$PROJECT
 
 case "$(uname -s)" in
 Linux)
-    if [ "$TARGET" == "x86_64-apple-darwin" ]; then
+    case "$TARGET" in
+    *-apple-darwin)
         export PATH="$MOZ_FETCHES_DIR/llvm-dsymutil/bin:$PATH"
         export PATH="$MOZ_FETCHES_DIR/cctools/bin:$PATH"
         export RUSTFLAGS="-C linker=$GECKO_PATH/taskcluster/scripts/misc/osx-cross-linker"
         export CC="$MOZ_FETCHES_DIR/clang/bin/clang"
-        export TARGET_CC="$MOZ_FETCHES_DIR/clang/bin/clang -isysroot $MOZ_FETCHES_DIR/MacOSX10.12.sdk"
-        export TARGET_CXX="$MOZ_FETCHES_DIR/clang/bin/clang++ -isysroot $MOZ_FETCHES_DIR/MacOSX10.12.sdk -stdlib=libc++"
+        if test "$TARGET" = "aarch64-apple-darwin"; then
+            export SDK_VER=11.0
+        fi
+        export TARGET_CC="$MOZ_FETCHES_DIR/clang/bin/clang -isysroot $MOZ_FETCHES_DIR/MacOSX${SDK_VER:-10.12}.sdk"
+        export TARGET_CXX="$MOZ_FETCHES_DIR/clang/bin/clang++ -isysroot $MOZ_FETCHES_DIR/MacOSX${SDK_VER:-10.12}.sdk -stdlib=libc++"
         cargo build --verbose --release --target $TARGET
-    else
+        ;;
+    *)
         export RUSTFLAGS="-Clinker=clang++ -C link-arg=--sysroot=$MOZ_FETCHES_DIR/sysroot"
         export CC=clang
         export CXX=clang++
@@ -46,7 +51,8 @@ Linux)
         export CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 --sysroot=$MOZ_FETCHES_DIR/sysroot"
         export PATH="$MOZ_FETCHES_DIR/clang/bin:$MOZ_FETCHES_DIR/binutils/bin:$PATH"
         cargo build --verbose --release --features "vendored-openssl"
-    fi
+        ;;
+    esac
     ;;
 MINGW*)
     cargo build --verbose --release
