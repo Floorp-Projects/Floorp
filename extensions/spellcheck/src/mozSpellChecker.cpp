266129b704a75234b504154dfe78cba6dbd4729f
Simon Giesecke <sgiesecke@mozilla.com>
2021-03-24 16:59:01 +0000

    Bug 708901 - Migrate to nsTHashSet in extensions/spellcheck. r=masayuki
    Differential Revision: https://phabricator.services.mozilla.com/D109335

--- extensions/spellcheck/src/mozSpellChecker.cpp
+++ extensions/spellcheck/src/mozSpellChecker.cpp
@@ -320,7 +320,7 @@ nsresult mozSpellChecker::GetDictionaryList(
   nsresult rv;
 
   // For catching duplicates
-  nsTHashtable<nsCStringHashKey> dictionaries;
+  nsTHashSet<nsCString> dictionaries;
 
   nsCOMArray<mozISpellCheckingEngine> spellCheckingEngines;
   rv = GetEngineList(&spellCheckingEngines);
@@ -334,9 +334,8 @@ nsresult mozSpellChecker::GetDictionaryList(
     for (auto& dictName : dictNames) {
       // Skip duplicate dictionaries. Only take the first one
       // for each name.
-      if (dictionaries.Contains(dictName)) continue;
+      if (!dictionaries.EnsureInserted(dictName)) continue;
 
-      dictionaries.PutEntry(dictName);
       aDictionaryList->AppendElement(dictName);
     }
   }
