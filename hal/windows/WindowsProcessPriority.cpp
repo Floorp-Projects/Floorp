fc2a156dda0ef0669abb5f89e1a13c408a8660eb
Gabriele Svelto <gsvelto@mozilla.com>
2021-03-25 15:41:23 +0000

    Bug 1604106 - Don't assert on error conditions when changing the priority of a process on Windows r=bobowen
    Differential Revision: https://phabricator.services.mozilla.com/D109088

--- hal/windows/WindowsProcessPriority.cpp
+++ hal/windows/WindowsProcessPriority.cpp
@@ -20,13 +20,6 @@ void SetProcessPriority(int aPid, ProcessPriority aPriority) {
 
   nsAutoHandle processHandle(
       ::OpenProcess(PROCESS_SET_INFORMATION, FALSE, aPid));
-#ifdef DEBUG
-  if (!processHandle) {
-    printf_stderr("::OpenProcess() failed with error %#08x\n",
-                  ::GetLastError());
-  }
-#endif  // DEBUG
-  MOZ_ASSERT(processHandle);
   if (processHandle) {
     DWORD priority = NORMAL_PRIORITY_CLASS;
     if (aPriority == PROCESS_PRIORITY_BACKGROUND) {
@@ -34,11 +27,12 @@ void SetProcessPriority(int aPid, ProcessPriority aPriority) {
     } else if (aPriority == PROCESS_PRIORITY_BACKGROUND_PERCEIVABLE) {
       priority = BELOW_NORMAL_PRIORITY_CLASS;
     }
-    ::SetPriorityClass(processHandle, priority);
-  }
 
-  HAL_LOG("WindowsProcessPriority - priority set to %d for pid %d\n", aPriority,
-          aPid);
+    if (::SetPriorityClass(processHandle, priority)) {
+      HAL_LOG("WindowsProcessPriority - priority set to %d for pid %d\n",
+              aPriority, aPid);
+    }
+  }
 }
 
 }  // namespace hal_impl
