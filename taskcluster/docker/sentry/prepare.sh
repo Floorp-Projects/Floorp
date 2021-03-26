d77f46527a196dc65b1c3e9e0de13465796b6c13
Mitchell Hentges <mhentges@mozilla.com>
2021-03-26 18:02:14 +0000

    Bug 1701137: Properly parse taskcluster Sentry secret r=aki
    Taskcluster secrets are not provided to clients in the same
    format that they are set. Rather, the YAML is rendered
    to JSON, and it's wrapped in a larger object that also
    contains the `expires` timestamp.
    
    Correctly parse the JSON and remove the dependency on `yq`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109907

--- taskcluster/docker/sentry/prepare.sh
+++ taskcluster/docker/sentry/prepare.sh
@@ -10,14 +10,9 @@ apt-get -y update
 # Install:
 # * curl to fetch sentry-cli
 # * jq to parse hgmo pushlog
-# * pip so we can install yq
 apt-get install -y \
     curl \
-    jq \
-    python3-pip
-
-# Install yq to parse secrets YAML
-pip3 install yq
+    jq
 
 # Install sentry-cli to publish releases
 curl -L https://github.com/getsentry/sentry-cli/releases/download/1.63.1/sentry-cli-Linux-x86_64 -o /usr/bin/sentry-cli
