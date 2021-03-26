1eafabedb5e8ad87c57bc8c0804c980b084a4cb6
Tim Nguyen <ntim.bugs@gmail.com>
2021-03-26 07:40:06 +0000

    Bug 1689240 - Remove unused exported ESLint comment from aboutaddons.js. DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D109859

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /* eslint max-len: ["error", 80] */
-/* exported initializeView, showView */
 /* import-globals-from aboutaddonsCommon.js */
 /* import-globals-from abuse-reports.js */
 /* import-globals-from view-controller.js */
