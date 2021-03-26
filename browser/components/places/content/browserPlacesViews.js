78a1c7cafe322368808ba83bb2fc1c19d4ce2e7c
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2021-03-26 17:16:54 +0000

    Bug 1700929 - Back out changeset 9a043bb287a5 to avoid regressing bookmark functionality in the sidebar. r=mak,flod l10n=flod a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D110038

--- browser/components/places/content/browserPlacesViews.js
+++ browser/components/places/content/browserPlacesViews.js
@@ -260,9 +260,6 @@ PlacesViewBase.prototype = {
     );
     existingOtherBookmarksItem?.remove();
 
-    let manageBookmarksMenu = aPopup.querySelector(
-      "#placesContext_showAllBookmarks"
-    );
     // Add the View menu for the Bookmarks Toolbar and "Show Other Bookmarks" menu item
     // if the click originated from the Bookmarks Toolbar.
     if (gBookmarksToolbar2h2020) {
@@ -270,16 +267,13 @@ PlacesViewBase.prototype = {
       existingSubmenu?.remove();
       let bookmarksToolbar = document.getElementById("PersonalToolbar");
       if (bookmarksToolbar?.contains(aPopup.triggerNode)) {
-        manageBookmarksMenu.removeAttribute("hidden");
-
         let menu = BookmarkingUI.buildBookmarksToolbarSubmenu(bookmarksToolbar);
-        aPopup.insertBefore(menu, manageBookmarksMenu);
+        aPopup.appendChild(menu);
 
         if (
           aPopup.triggerNode.id === "OtherBookmarks" ||
           aPopup.triggerNode.id === "PlacesChevron" ||
-          aPopup.triggerNode.id === "PlacesToolbarItems" ||
-          aPopup.triggerNode.parentNode.id === "PlacesToolbarItems"
+          aPopup.triggerNode.id === "PlacesToolbarItems"
         ) {
           let otherBookmarksMenuItem = BookmarkingUI.buildShowOtherBookmarksMenuItem();
 
@@ -290,11 +284,7 @@ PlacesViewBase.prototype = {
             );
           }
         }
-      } else {
-        manageBookmarksMenu.setAttribute("hidden", "true");
       }
-    } else {
-      manageBookmarksMenu.setAttribute("hidden", "true");
     }
 
     return this.controller.buildContextMenu(aPopup);
