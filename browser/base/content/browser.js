2027d031340b4d0a5bcc3cae82a94ab13f835327
Cosmin Sabou <csabou@mozilla.com>
2021-03-27 23:58:04 +0200

    Backed out 2 changesets (bug 1690333, bug 1699250) for mochitest failures on test_notificationbox.xhtml.
    Backed out changeset d6780d34ca05 (bug 1690333)
    Backed out changeset 50cc5db86f1e (bug 1699250)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -396,7 +396,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
   return new MozElements.NotificationBox(element => {
     element.classList.add("global-notificationbox");
     element.setAttribute("notificationside", "top");
-    if (gProtonInfobarsEnabled) {
+    if (Services.prefs.getBoolPref("browser.proton.infobars.enabled", false)) {
       // With Proton enabled all notification boxes are at the top, built into the browser chrome.
       let tabNotifications = document.getElementById("tab-notification-deck");
       gNavToolbox.insertBefore(element, tabNotifications);
@@ -408,7 +408,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
 
 // Regular notification bars shown at the bottom of the window.
 XPCOMUtils.defineLazyGetter(this, "gNotificationBox", () => {
-  return gProtonInfobarsEnabled
+  return Services.prefs.getBoolPref("browser.proton.infobars.enabled", false)
     ? gHighPriorityNotificationBox
     : new MozElements.NotificationBox(element => {
         element.classList.add("global-notificationbox");
@@ -585,14 +585,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
       doc.documentElement.toggleAttribute("proton", newValue);
     }
   }
-);
-
-/* Temporary pref while the Proton infobars work stabilizes. */
-XPCOMUtils.defineLazyPreferenceGetter(
-  this,
-  "gProtonInfobarsEnabled",
-  "browser.proton.infobars.enabled",
-  false
 );
 
 /* Temporary pref while the dust settles around the updated tooltip design
@@ -1055,9 +1047,7 @@ const gStoragePressureObserver = {
       null,
       gHighPriorityNotificationBox.PRIORITY_WARNING_HIGH,
       buttons,
-      null,
-      null,
-      ["branding/brand.ftl", "browser/preferences/preferences.ftl"]
+      null
     );
 
     // This seems to be necessary to get the buttons to display correctly
--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -826,7 +826,9 @@
       if (!browser._notificationBox) {
         browser._notificationBox = new MozElements.NotificationBox(element => {
           element.setAttribute("notificationside", "top");
-          if (gProtonInfobarsEnabled) {
+          if (
+            Services.prefs.getBoolPref("browser.proton.infobars.enabled", false)
+          ) {
             element.setAttribute(
               "name",
               `tab-notification-box-${this._nextNotificationBoxId++}`
@@ -1112,7 +1114,9 @@
 
       this._appendStatusPanel();
 
-      if (gProtonInfobarsEnabled) {
+      if (
+        Services.prefs.getBoolPref("browser.proton.infobars.enabled", false)
+      ) {
         this._updateVisibleNotificationBox(newBrowser);
       }
 
