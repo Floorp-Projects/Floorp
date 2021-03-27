689626568f41a0d83b2c2b63e8222b0ff97f46c4
Nick Alexander <nalexander@mozilla.com>
2021-03-27 20:43:23 +0000

    Bug 1689519 - Pre: Add BackgroundTasksManager.EXIT_CODE. r=bytesized
    Differential Revision: https://phabricator.services.mozilla.com/D108661

--- toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_exitcodes.js
+++ toolkit/components/backgroundtasks/tests/xpcshell/test_backgroundtask_exitcodes.js
@@ -4,22 +4,35 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+"use strict";
+
+// This test exercises functionality and also ensures the exit codes,
+// which are a public API, do not change over time.
+const { EXIT_CODE } = ChromeUtils.import(
+  "resource://gre/modules/BackgroundTasksManager.jsm"
+).BackgroundTasksManager;
+
 add_task(async function test_success() {
   let exitCode = await do_backgroundtask("success");
   Assert.equal(0, exitCode);
+  Assert.equal(EXIT_CODE.SUCCESS, exitCode);
 });
 
 add_task(async function test_failure() {
   let exitCode = await do_backgroundtask("failure");
   Assert.equal(1, exitCode);
+  // There's no single exit code for failure.
+  Assert.notEqual(EXIT_CODE.SUCCESS, exitCode);
 });
 
 add_task(async function test_exception() {
   let exitCode = await do_backgroundtask("exception");
   Assert.equal(3, exitCode);
+  Assert.equal(EXIT_CODE.EXCEPTION, exitCode);
 });
 
 add_task(async function test_not_found() {
   let exitCode = await do_backgroundtask("not_found");
   Assert.equal(2, exitCode);
+  Assert.equal(EXIT_CODE.NOT_FOUND, exitCode);
 });
