171513ebdf5c769866e789bea48cb962b61e0794
Lee Salzman <lsalzman@mozilla.com>
2021-03-29 20:35:12 +0000

    Bug 1700520 - Use PREFER_INLINE on large SWGL functions. r=jrmuizel
    It seems like blend_pixels is the major offender in terms of slowing down GCC
    compile times, so lets annotate such functions with PREFER_INLINE. Then, we ensure
    that PREFER_INLINE disables always_inline for either GCC or debug builds so that
    in cases where we know the compiler is sensitive to binary bloat, we aren't forcing
    inlining.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109789

--- gfx/wr/swgl/src/blend.h
+++ gfx/wr/swgl/src/blend.h
@@ -405,7 +405,7 @@ static ALWAYS_INLINE void restore_aa() {
   blend_key = BlendKey(AA_BLEND_KEY_NONE + blend_key);
 }
 
-static ALWAYS_INLINE WideRGBA8 blend_pixels(uint32_t* buf, PackedRGBA8 pdst,
+static PREFER_INLINE WideRGBA8 blend_pixels(uint32_t* buf, PackedRGBA8 pdst,
                                             WideRGBA8 src, int span = 4) {
   WideRGBA8 dst = unpack(pdst);
   const WideRGBA8 RGB_MASK = {0xFFFF, 0xFFFF, 0xFFFF, 0,      0xFFFF, 0xFFFF,
@@ -686,7 +686,7 @@ static ALWAYS_INLINE WideRGBA8 blend_pixels(uint32_t* buf, PackedRGBA8 pdst,
   // clang-format on
 }
 
-static ALWAYS_INLINE WideR8 blend_pixels(uint8_t* buf, WideR8 dst, WideR8 src,
+static PREFER_INLINE WideR8 blend_pixels(uint8_t* buf, WideR8 dst, WideR8 src,
                                          int span = 4) {
 // clang-format off
 #define BLEND_CASE_KEY(key)                          \
