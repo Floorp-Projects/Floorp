cc92f6993bd302994e57444a7a7f5a4e1ebb1cbe
Andrew Osmond <aosmond@mozilla.com>
2021-03-29 22:43:27 +0000

    Bug 1699224 - Part 3. Complete shutdown of SharedSurfacesParent on the main thread. r=jrmuizel
    We want to use nsExpirationTracker to decide when we should unmap shared
    surfaces from our address space. This relies upon the observer service
    and we need to be both initialized and shutdown on the main thread to
    add/remove the observer. SharedSurfacesParent is already initialized on
    the main thread, and we just need to complete shutdown on the main
    thread.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109439

--- gfx/webrender_bindings/RenderThread.cpp
+++ gfx/webrender_bindings/RenderThread.cpp
@@ -122,6 +122,8 @@ void RenderThread::ShutDown() {
   sRenderThread->Loop()->PostTask(runnable.forget());
   task.Wait();
 
+  layers::SharedSurfacesParent::Shutdown();
+
   sRenderThread = nullptr;
 #ifdef XP_WIN
   if (widget::WinCompositorWindowThread::Get()) {
@@ -142,7 +144,7 @@ void RenderThread::ShutDownTask(layers::SynchronousTask* aTask) {
 
   // Releasing on the render thread will allow us to avoid dispatching to remove
   // remaining textures from the texture map.
-  layers::SharedSurfacesParent::Shutdown();
+  layers::SharedSurfacesParent::ShutdownRenderThread();
 
   ClearAllBlobImageResources();
   ClearSingletonGL();
