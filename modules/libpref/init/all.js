7d6cc76c177285813b67424f19b6dbf54705308a
Harry Twyford <htwyford@mozilla.com>
2021-03-29 17:22:11 +0000

    Bug 738335 - Enable toolbar shifting for all users. r=mac-reviewers,bradwerth
    Depends on D109988
    
    Differential Revision: https://phabricator.services.mozilla.com/D109989

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3880,9 +3880,6 @@ pref("alerts.showFavicons", false);
 #ifdef XP_MACOSX
   // Whether to use macOS native full screen for Fullscreen API
   pref("full-screen-api.macos-native-full-screen", false);
-  // Whether the toolbar should slide down with the menubar when the user mouses
-  // to the top of the screen in fullscreen mode.
-  pref("full-screen-api.macos.shiftToolbar", false);
 #endif
 // whether to prevent the top level widget from going fullscreen
 pref("full-screen-api.ignore-widgets", false);
