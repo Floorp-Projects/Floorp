bd667009d4c19d9597c048d622a2f68df3ffdfcc
Ted Campbell <tcampbell@mozilla.com>
2021-03-29 21:12:16 +0000

    Bug 1426385 - Properly wrap envChainObject to jsshell evaluate. r=mgaudet
    This is a shell-only helper method that vaguely represents some complex Gecko
    use-cases. At the very least we should not crash with scary compartment
    errors.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110153

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -2550,6 +2550,16 @@ static bool Evaluate(JSContext* cx, unsigned argc, Value* vp) {
   }
 
   if (envChain.length() != 0) {
+    {
+      // Wrap the envChainObject list into target realm.
+      JSAutoRealm ar(cx, global);
+      for (size_t i = 0; i < envChain.length(); ++i) {
+        if (!JS_WrapObject(cx, envChain[i])) {
+          return false;
+        }
+      }
+    }
+
     options.setNonSyntacticScope(true);
   }
 
