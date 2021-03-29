f7c577f7a1b46516b84103ee4777da062bc87a58
Andrew Osmond <aosmond@mozilla.com>
2021-03-29 22:43:28 +0000

    Bug 1699224 - Part 5. Unmap shared surfaces if memory pressured during resource updates. r=jrmuizel
    Before we start a resource update, we should check if we are virtual
    memory pressured (32-bit Windows only). If so, pre-emptively unmap
    shared surfaces until the pressure is relieved to try to avoid OOMs
    elsewhere. This only applies to the GPU process because the parent
    process actively watches its own memory pressure and dispatches a
    low-memory event which our expiration tracker is an observer for.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109441

--- gfx/ipc/GPUParent.cpp
+++ gfx/ipc/GPUParent.cpp
@@ -101,17 +101,17 @@ GPUParent* GPUParent::GetSingleton() {
   return sGPUParent;
 }
 
-/* static */ void GPUParent::MaybeFlushMemory() {
+/* static */ bool GPUParent::MaybeFlushMemory() {
 #if defined(XP_WIN) && !defined(HAVE_64BIT_BUILD)
   MOZ_ASSERT(CompositorThreadHolder::IsInCompositorThread());
   if (!XRE_IsGPUProcess()) {
-    return;
+    return false;
   }
 
   MEMORYSTATUSEX stat;
   stat.dwLength = sizeof(stat);
   if (!GlobalMemoryStatusEx(&stat)) {
-    return;
+    return false;
   }
 
   // We only care about virtual process memory space in the GPU process because
@@ -132,6 +132,9 @@ GPUParent* GPUParent::GetSingleton() {
         }));
   }
   sLowMemory = lowMemory;
+  return lowMemory;
+#else
+  return false;
 #endif
 }
 
