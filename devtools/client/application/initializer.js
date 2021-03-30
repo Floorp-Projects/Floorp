12380195f54fa13a6e8dcec06b736a954f2b831c
Alexandre Poirot <poirot.alex@gmail.com>
2021-03-30 21:29:37 +0000

    Bug 1696372 - [devtools] Migrate application panel from toolbox.targetList to commands.TargetCommand. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D110206

--- devtools/client/application/initializer.js
+++ devtools/client/application/initializer.js
@@ -46,7 +46,7 @@ const { safeAsyncMethod } = require("devtools/shared/async-utils");
  * called to start the UI for the panel.
  */
 window.Application = {
-  async bootstrap({ toolbox, panel }) {
+  async bootstrap({ toolbox, commands, panel }) {
     // bind event handlers to `this`
     this.handleOnNavigate = this.handleOnNavigate.bind(this);
     this.updateDomain = this.updateDomain.bind(this);
@@ -60,9 +60,8 @@ window.Application = {
     );
 
     this.toolbox = toolbox;
-    // NOTE: the client is the same through the lifecycle of the toolbox, even
-    // though we get it from toolbox.target
-    this.client = toolbox.target.client;
+    this._commands = commands;
+    this.client = commands.client;
 
     this.telemetry = new Telemetry();
     this.store = configureStore(this.telemetry, toolbox.sessionId);
@@ -83,8 +82,8 @@ window.Application = {
 
     // awaiting for watchTargets will return the targets that are currently
     // available, so we can have our first render with all the data ready
-    await this.toolbox.targetList.watchTargets(
-      [this.toolbox.targetList.TYPES.FRAME],
+    await this._commands.targetCommand.watchTargets(
+      [this._commands.targetCommand.TYPES.FRAME],
       this.onTargetAvailable,
       this.onTargetDestroyed
     );
@@ -140,8 +139,8 @@ window.Application = {
   destroy() {
     this.workersListener.removeListener();
 
-    this.toolbox.targetList.unwatchTargets(
-      [this.toolbox.targetList.TYPES.FRAME],
+    this._commands.targetCommand.unwatchTargets(
+      [this._commands.targetCommand.TYPES.FRAME],
       this.onTargetAvailable,
       this.onTargetDestroyed
     );
@@ -152,6 +151,7 @@ window.Application = {
     this.mount = null;
     this.toolbox = null;
     this.client = null;
+    this._commands = null;
     this.workersListener = null;
     this._destroyed = true;
   },
