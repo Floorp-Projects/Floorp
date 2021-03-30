d67e7e8bc9b887597cab5f3eec076828db5197cc
Karl Tomlinson <karlt+@karlt.net>
2021-03-30 07:52:13 +0000

    Bug 1698238 add a GetUserMediaRequest variant for selectAudioOutput() r=jib,emilio
    Differential Revision: https://phabricator.services.mozilla.com/D108374

--- dom/media/GetUserMediaRequest.cpp
+++ dom/media/GetUserMediaRequest.cpp
@@ -5,6 +5,7 @@
 #include "GetUserMediaRequest.h"
 
 #include "base/basictypes.h"
+#include "mozilla/dom/MediaDevicesBinding.h"
 #include "mozilla/dom/MediaStreamBinding.h"
 #include "mozilla/dom/GetUserMediaRequestBinding.h"
 #include "nsIScriptGlobalObject.h"
@@ -20,6 +21,19 @@ GetUserMediaRequest::GetUserMediaRequest(
       mOuterWindowID(aInnerWindow->GetOuterWindow()->WindowID()),
       mCallID(aCallID),
       mConstraints(new MediaStreamConstraints(aConstraints)),
+      mType(GetUserMediaRequestType::Getusermedia),
+      mIsSecure(aIsSecure),
+      mIsHandlingUserInput(aIsHandlingUserInput) {}
+
+GetUserMediaRequest::GetUserMediaRequest(
+    nsPIDOMWindowInner* aInnerWindow, const nsAString& aCallID,
+    const AudioOutputOptions& aAudioOutputOptions, bool aIsSecure,
+    bool aIsHandlingUserInput)
+    : mInnerWindowID(aInnerWindow->WindowID()),
+      mOuterWindowID(aInnerWindow->GetOuterWindow()->WindowID()),
+      mCallID(aCallID),
+      mAudioOutputOptions(new AudioOutputOptions(aAudioOutputOptions)),
+      mType(GetUserMediaRequestType::Selectaudiooutput),
       mIsSecure(aIsSecure),
       mIsHandlingUserInput(aIsHandlingUserInput) {}
 
@@ -31,6 +45,7 @@ GetUserMediaRequest::GetUserMediaRequest(nsPIDOMWindowInner* aInnerWindow,
       mOuterWindowID(0),
       mRawID(aRawId),
       mMediaSource(aMediaSource),
+      mType(GetUserMediaRequestType::Recording_device_stopped),
       mIsSecure(false),
       mIsHandlingUserInput(aIsHandlingUserInput) {
   if (aInnerWindow && aInnerWindow->GetOuterWindow()) {
@@ -52,6 +67,8 @@ JSObject* GetUserMediaRequest::WrapObject(JSContext* aCx,
 }
 
 nsISupports* GetUserMediaRequest::GetParentObject() { return nullptr; }
+
+GetUserMediaRequestType GetUserMediaRequest::Type() { return mType; }
 
 void GetUserMediaRequest::GetCallID(nsString& retval) { retval = mCallID; }
 
@@ -77,6 +94,14 @@ void GetUserMediaRequest::GetConstraints(MediaStreamConstraints& result) {
              "result should be default initialized");
   if (mConstraints) {
     result = *mConstraints;
+  }
+}
+
+void GetUserMediaRequest::GetAudioOutputOptions(AudioOutputOptions& result) {
+  MOZ_ASSERT(result.mDeviceId.IsEmpty(),
+             "result should be default initialized");
+  if (mAudioOutputOptions) {
+    result = *mAudioOutputOptions;
   }
 }
 
