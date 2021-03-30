f24ffb4130d64ed438092547a9863154d9150287
Emilio Cobos Álvarez <emilio@crisal.io>
2021-03-30 17:43:47 +0000

    Bug 1698606 - Prevent datetime reset button from growing the wrapper (and thus moving the editor). r=jwatt
    Mostly a cosmetic change but this makes the layout more consistent with
    other form controls.
    
    This is the only thing that I think could cause something like comment 0
    is describing, though I couldn't reproduce. But I think it's an
    improvement regardless.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110062

--- toolkit/content/widgets/datetimebox.js
+++ toolkit/content/widgets/datetimebox.js
@@ -159,7 +159,7 @@ this.DateTimeInputBaseImplWidget = class {
           </span>
 
           <button class="datetime-reset-button" id="reset-button" tabindex="-1" aria-label="&datetime.reset.label;">
-            <svg xmlns="http://www.w3.org/2000/svg" class="datetime-reset-button-svg" width="12" height="12">
+            <svg xmlns="http://www.w3.org/2000/svg" class="datetime-reset-button-svg" width="12" height="12" viewBox="0 0 12 12">
               <path d="M 3.9,3 3,3.9 5.1,6 3,8.1 3.9,9 6,6.9 8.1,9 9,8.1 6.9,6 9,3.9 8.1,3 6,5.1 Z M 12,6 A 6,6 0 0 1 6,12 6,6 0 0 1 0,6 6,6 0 0 1 6,0 6,6 0 0 1 12,6 Z"/>
             </svg>
           </button>
