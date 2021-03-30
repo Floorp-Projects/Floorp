37b6d0f84c3b2f31ba0d246f5e6e3dcc2f58d4d0
Jason Orendorff <jason.orendorff@gmail.com>
2021-03-30 19:44:34 +0000

    Bug 1662559 - Part 18: Remove all support for private members from ElemOpEmitter. r=arai
    Depends on D108299
    
    Differential Revision: https://phabricator.services.mozilla.com/D108300

--- js/src/frontend/ElemOpEmitter.h
+++ js/src/frontend/ElemOpEmitter.h
@@ -139,7 +139,6 @@ class MOZ_STACK_CLASS ElemOpEmitter {
 
   Kind kind_;
   ObjKind objKind_;
-  NameVisibility visibility_ = NameVisibility::Public;
 
 #ifdef DEBUG
   // The state of this emitter.
@@ -212,8 +211,7 @@ class MOZ_STACK_CLASS ElemOpEmitter {
 #endif
 
  public:
-  ElemOpEmitter(BytecodeEmitter* bce, Kind kind, ObjKind objKind,
-                NameVisibility visibility);
+  ElemOpEmitter(BytecodeEmitter* bce, Kind kind, ObjKind objKind);
 
  private:
   [[nodiscard]] bool isCall() const { return kind_ == Kind::Call; }
@@ -222,13 +220,7 @@ class MOZ_STACK_CLASS ElemOpEmitter {
     return kind_ == Kind::SimpleAssignment;
   }
 
-  bool isPrivate() { return visibility_ == NameVisibility::Private; }
-
   [[nodiscard]] bool isPropInit() const { return kind_ == Kind::PropInit; }
-
-  [[nodiscard]] bool isPrivateGet() const {
-    return visibility_ == NameVisibility::Private && kind_ == Kind::Get;
-  }
 
   [[nodiscard]] bool isDelete() const { return kind_ == Kind::Delete; }
 
@@ -268,12 +260,6 @@ class MOZ_STACK_CLASS ElemOpEmitter {
   [[nodiscard]] bool emitAssignment();
 
   [[nodiscard]] bool emitIncDec();
-
- private:
-  // When we have private names, we may need to emit a CheckPrivateField
-  // op to potentially throw errors where required.
-  [[nodiscard]] bool emitPrivateGuard();
-  [[nodiscard]] bool emitPrivateGuardForAssignment();
 };
 
 } /* namespace frontend */
