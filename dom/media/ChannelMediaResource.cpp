78a895becbd86a300dedb661ab7c838bcf8921fa
Tom Tung <shes050117@gmail.com>
2021-03-30 00:52:30 +0000

    Bug 1696111 - Extract the code for parsing range header string out and use it later; r=alwu,necko-reviewers,dragana
    Differential Revision: https://phabricator.services.mozilla.com/D106889

--- dom/media/ChannelMediaResource.cpp
+++ dom/media/ChannelMediaResource.cpp
@@ -7,6 +7,7 @@
 
 #include "mozilla/Preferences.h"
 #include "mozilla/dom/HTMLMediaElement.h"
+#include "mozilla/net/OpaqueResponseUtils.h"
 #include "nsIAsyncVerifyRedirectCallback.h"
 #include "nsICachingChannel.h"
 #include "nsIClassOfService.h"
@@ -309,32 +310,12 @@ nsresult ChannelMediaResource::ParseContentRangeHeader(
   NS_ENSURE_SUCCESS(rv, rv);
   NS_ENSURE_FALSE(rangeStr.IsEmpty(), NS_ERROR_ILLEGAL_VALUE);
 
-  // Parse the range header: e.g. Content-Range: bytes 7000-7999/8000.
-  int32_t spacePos = rangeStr.Find(" "_ns);
-  int32_t dashPos = rangeStr.Find("-"_ns, true, spacePos);
-  int32_t slashPos = rangeStr.Find("/"_ns, true, dashPos);
+  auto rangeOrErr = net::ParseContentRangeHeaderString(rangeStr);
+  NS_ENSURE_FALSE(rangeOrErr.isErr(), rangeOrErr.unwrapErr());
 
-  nsAutoCString aRangeStartText;
-  rangeStr.Mid(aRangeStartText, spacePos + 1, dashPos - (spacePos + 1));
-  aRangeStart = aRangeStartText.ToInteger64(&rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-  NS_ENSURE_TRUE(0 <= aRangeStart, NS_ERROR_ILLEGAL_VALUE);
-
-  nsAutoCString aRangeEndText;
-  rangeStr.Mid(aRangeEndText, dashPos + 1, slashPos - (dashPos + 1));
-  aRangeEnd = aRangeEndText.ToInteger64(&rv);
-  NS_ENSURE_SUCCESS(rv, rv);
-  NS_ENSURE_TRUE(aRangeStart < aRangeEnd, NS_ERROR_ILLEGAL_VALUE);
-
-  nsAutoCString aRangeTotalText;
-  rangeStr.Right(aRangeTotalText, rangeStr.Length() - (slashPos + 1));
-  if (aRangeTotalText[0] == '*') {
-    aRangeTotal = -1;
-  } else {
-    aRangeTotal = aRangeTotalText.ToInteger64(&rv);
-    NS_ENSURE_TRUE(aRangeEnd < aRangeTotal, NS_ERROR_ILLEGAL_VALUE);
-    NS_ENSURE_SUCCESS(rv, rv);
-  }
+  aRangeStart = std::get<0>(rangeOrErr.inspect());
+  aRangeEnd = std::get<1>(rangeOrErr.inspect());
+  aRangeTotal = std::get<2>(rangeOrErr.inspect());
 
   LOG("Received bytes [%" PRId64 "] to [%" PRId64 "] of [%" PRId64
       "] for decoder[%p]",
