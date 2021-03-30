5cfb1bfe2034e7235f99e805386a23103e25ff6f
Jason Orendorff <jason.orendorff@gmail.com>
2021-03-30 19:44:34 +0000

    Bug 1662559 - Part 17: Change BytecodeEmitter to use the new PrivateOpEmitter class. r=arai
    Depends on D108298
    
    Differential Revision: https://phabricator.services.mozilla.com/D108299

--- js/src/jit-test/tests/debug/private-methods-eval-in-frame.js
+++ js/src/jit-test/tests/debug/private-methods-eval-in-frame.js
@@ -7,6 +7,16 @@ class B {
     return 12;
   }
 
+  #privF = () => { return 12; }
+
+  callPriv() {
+    return this.#priv();
+  }
+
+  callPrivF() {
+    return this.#privF();
+  }
+
   #val = '';
   set #x(x) {
     this.#val = x + ' haha';
@@ -18,8 +28,23 @@ class B {
   ef(str) {
     return evalInFrame(0, str);
   }
+
+  callFunc(f) {
+    f();
+  }
+
 }
 
 var b = new B();
-assertEq(b.ef(`this.#priv();`), 12);
-assertEq(b.ef(`this.#x = 'Hi'; this.#x`), 'Hi haha');
+
+assertEq(b.ef("this.callPriv()"), 12);
+assertEq(b.ef("this.callPrivF()"), 12);
+assertThrowsInstanceOf(() => b.ef(`this.callFunc(() => { this.#privF(); })`), Error);
+
+// See EmitterScope::lookupPrivate for the reasoning here.
+assertThrowsInstanceOf(() => b.ef(`this.#privF();`), Error);
+assertThrowsInstanceOf(() => b.ef(`var x = () => { return this.#privF(); } x();`), Error);
+assertThrowsInstanceOf(() => b.ef(`this.#priv();`), Error);
+assertThrowsInstanceOf(() => b.ef(`var x = () => { return this.#priv(); } x();`), Error);
+assertThrowsInstanceOf(() => b.ef(`this.#x = 'Hi'; this.#x`), Error);
+assertThrowsInstanceOf(() => b.ef(`var x = () => { this.#x = 'Hi'; return this.#x} x();`), Error);
