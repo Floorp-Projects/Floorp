10b472ef57da844a4c8c10bf59300d254d10d375
Emilio Cobos Álvarez <emilio@crisal.io>
2021-03-31 01:51:46 +0000

    Bug 1700871 - Only allow focus move for links / form submission iff actually handling user input. r=smaug
    The other navigation that allows focus moves (window.open) already goes
    through the popup blocker, so that one is fine.
    
    I think given how weird yet conservative other browsers are, this should
    be a good trade-off to avoid false positives.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110196

--- dom/base/test/browser_bug1691214.js
+++ dom/base/test/browser_bug1691214.js
@@ -6,25 +6,28 @@
 
 const BASE_URL = "http://mochi.test:8888/browser/dom/base/test/";
 
-add_task(async function() {
+async function newFocusedWindow() {
+  let delayedStartupPromise = BrowserTestUtils.waitForNewWindow();
+  let win = await BrowserTestUtils.domWindowOpenedAndLoaded();
+  // New windows get focused after the first paint, see bug 1262946
+  await BrowserTestUtils.waitForContentEvent(
+    win.gBrowser.selectedBrowser,
+    "MozAfterPaint"
+  );
+  await delayedStartupPromise;
+  return win;
+}
+
+add_task(async function bug1691214() {
   await BrowserTestUtils.withNewTab(
     BASE_URL + "file_bug1691214.html",
     async function(browser) {
       let win;
 
       {
-        let newWindow = BrowserTestUtils.domWindowOpenedAndLoaded();
-        let delayedStartupPromise = BrowserTestUtils.waitForNewWindow();
-
+        let newWindow = newFocusedWindow();
         await BrowserTestUtils.synthesizeMouseAtCenter("#link-1", {}, browser);
         win = await newWindow;
-        ok(win, "First navigation should've opened the new window");
-        // New windows get focused after the first paint, see bug 1262946
-        await BrowserTestUtils.waitForContentEvent(
-          win.gBrowser.selectedBrowser,
-          "MozAfterPaint"
-        );
-        await delayedStartupPromise;
         is(Services.focus.focusedWindow, win, "New window should be focused");
       }
 
@@ -52,6 +55,45 @@ add_task(async function() {
         "Existing window should've been targeted and focused"
       );
 
+      win.close();
+    }
+  );
+});
+
+// The tab has a form infinitely submitting to an iframe, and that shouldn't
+// switch focus back. For that, we open a window from the tab, make sure it's
+// focused, and then wait for three submissions (but since we need to listen to
+// trusted events from chrome code, we use the iframe load event instead).
+add_task(async function bug1700871() {
+  await BrowserTestUtils.withNewTab(
+    BASE_URL + "file_bug1700871.html",
+    async function(browser) {
+      let win;
+
+      {
+        let newWindow = newFocusedWindow();
+        await BrowserTestUtils.synthesizeMouseAtCenter("#link-1", {}, browser);
+        win = await newWindow;
+        is(Services.focus.focusedWindow, win, "New window should be focused");
+      }
+
+      info("waiting for three submit events and ensuring focus hasn't moved");
+
+      await SpecialPowers.spawn(browser, [], function() {
+        let pending = 3;
+        return new Promise(resolve => {
+          content.document
+            .querySelector("iframe")
+            .addEventListener("load", function(e) {
+              info("Got load on the frame: " + pending);
+              if (!--pending) {
+                resolve();
+              }
+            });
+        });
+      });
+
+      is(Services.focus.focusedWindow, win, "Focus shouldn't have moved");
       win.close();
     }
   );
