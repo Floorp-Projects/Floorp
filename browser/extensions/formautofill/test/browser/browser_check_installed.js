25362dd5c7e08bf4dd4379313d3d403a9105f94c
Luca Greco <lgreco@mozilla.com>
2021-03-31 11:40:19 +0000

    Bug 1700184 - Bump formautofill builtin addon version. r=mixedpuppy
    Depends on D109394
    
    Differential Revision: https://phabricator.services.mozilla.com/D109395

--- browser/extensions/formautofill/test/browser/browser_check_installed.js
+++ browser/extensions/formautofill/test/browser/browser_check_installed.js
@@ -3,7 +3,7 @@
 add_task(async function test_enabled() {
   let addon = await AddonManager.getAddonByID("formautofill@mozilla.org");
   isnot(addon, null, "Check addon exists");
-  is(addon.version, "1.0", "Check version");
+  is(addon.version, "1.0.1", "Check version");
   is(addon.name, "Form Autofill", "Check name");
   ok(addon.isCompatible, "Check application compatibility");
   ok(!addon.appDisabled, "Check not app disabled");
