f8db566b4776831761e2e18349d195da9bf256fc
Julian Descottes <jdescottes@mozilla.com>
2021-04-01 22:11:26 +0000

    Bug 1644397 - [devtools] Add test for duplicated early messages in console. r=nchevobbe
    Depends on D105546
    
    Differential Revision: https://phabricator.services.mozilla.com/D105547

---
+++ devtools/client/webconsole/test/browser/browser_webconsole_cached_messages_duplicate_after_target_switching.js
@@ -0,0 +1,38 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+"use strict";
+
+const TEST_URI_ORG = `http://example.org/document-builder.sjs?html=<meta charset=utf8></meta>
+<script>
+      console.log("early message on org page");
+</script><body>`;
+const TEST_URI_COM = TEST_URI_ORG.replace(/org/g, "com");
+
+add_task(async function() {
+  info("Add a tab and open the console");
+  const tab = await addTab("about:robots");
+  const hud = await openConsole(tab);
+
+  {
+    await navigateTo(TEST_URI_ORG);
+
+    // Wait for some time in order to let a chance to have duplicated message
+    // and catch such regression
+    await wait(1000);
+
+    info("wait until the ORG message is displayed");
+    await checkUniqueMessageExists(hud, "early message on org page");
+  }
+
+  {
+    await navigateTo(TEST_URI_COM);
+
+    // Wait for some time in order to let a chance to have duplicated message
+    // and catch such regression
+    await wait(1000);
+
+    info("wait until the COM message is displayed");
+    await checkUniqueMessageExists(hud, "early message on com page");
+  }
+});
