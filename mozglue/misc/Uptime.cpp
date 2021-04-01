ed2842300100726885c5e35df52b781c05e2c013
Brindusan Cristian <cbrindusan@mozilla.com>
2021-04-01 17:01:25 +0300

    Backed out changeset 943e1e811d75 (bug 1686405) for causing build bustages in Uptime.cpp. CLOSED TREE

--- mozglue/misc/Uptime.cpp
+++ mozglue/misc/Uptime.cpp
@@ -111,12 +111,7 @@ Maybe<uint64_t> NowIncludingSuspendMs() {
 #  endif
 }
 
-#else  // catch all
-
-Maybe<uint64_t> NowExcludingSuspendMs() { return Nothing(); }
-Maybe<uint64_t> NowIncludingSuspendMs() { return Nothing(); }
-
-#endif
+#endif  // XP_LINUX
 
 };  // anonymous namespace
 
