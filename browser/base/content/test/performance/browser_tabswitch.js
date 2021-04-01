12d78d34b6b73ba17c16de2b8f5c1a122b322fd9
Jared Wein <jwein@mozilla.com>
2021-04-01 21:20:14 +0000

    Bug 1701707 - Remove padding changes to first and last tab since the padding is already applied on the scrollbox to prevent clipping of the shadow. r=desktop-theme-reviewers,harry
    Differential Revision: https://phabricator.services.mozilla.com/D110595

--- browser/base/content/test/performance/browser_tabswitch.js
+++ browser/base/content/test/performance/browser_tabswitch.js
@@ -23,6 +23,11 @@ const EXPECTED_REFLOWS = [
  * tabs that are both fully visible.
  */
 add_task(async function() {
+  // TODO (bug 1702653): Disable tab shadows for tests since the shadow
+  // can extend outside of the boundingClientRect. The tabRect will need
+  // to grow to include the shadow size.
+  gBrowser.tabContainer.setAttribute("noshadowfortests", "true");
+
   await ensureNoPreloadedBrowser();
   await disableFxaBadge();
 
@@ -49,6 +54,11 @@ add_task(async function() {
 
   let tabStripRect = gBrowser.tabContainer.arrowScrollbox.getBoundingClientRect();
   let firstTabRect = origTab.getBoundingClientRect();
+  let tabPaddingStart = parseFloat(
+    getComputedStyle(gBrowser.selectedTab).paddingInlineStart
+  );
+  let minTabWidth = firstTabRect.width - 2 * tabPaddingStart;
+  let maxTabWidth = firstTabRect.width;
   let inRange = (val, min, max) => min <= val && val <= max;
 
   await withPerfObserver(
@@ -75,8 +85,8 @@ add_task(async function() {
                   // width of 2 tabs.
                   inRange(
                     r.w,
-                    (origTab.clientWidth - 1) * 2, // -1 for the border on Win7
-                    origTab.clientWidth * 2
+                    minTabWidth - 1, // -1 for the border on Win7
+                    maxTabWidth * 2
                   )
                 )
               )
