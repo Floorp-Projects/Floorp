50932ba757d1e2be79aea8b8fb453a7f3a7ce577
Nick Alexander <nalexander@mozilla.com>
2021-04-01 19:34:29 +0000

    Bug 1701502 - Fix browser_all_files_referenced when MOZ_UPDATE_AGENT && !MOZ_BACKGROUNDTASKS. r=application-update-reviewers,bytesized
    This was oversight when landing Bug 1687777.  In the not-so-distant
    future we will enable MOZ_BACKGROUNDTASKS beyond Nightly, removing
    these configuration variants.
    
    Depends on D110597
    
    Differential Revision: https://phabricator.services.mozilla.com/D110598

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -258,6 +258,13 @@ if (AppConstants.MOZ_BACKGROUNDTASKS && !AppConstants.MOZ_UPDATE_AGENT) {
   });
 }
 
+if (AppConstants.MOZ_UPDATE_AGENT && !AppConstants.MOZ_BACKGROUNDTASKS) {
+  // Task scheduling is only used for background updates right now.
+  whitelist.push({
+    file: "resource://gre/modules/TaskScheduler.jsm",
+  });
+}
+
 whitelist = new Set(
   whitelist
     .filter(
