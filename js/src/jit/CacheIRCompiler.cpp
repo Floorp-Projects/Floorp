5d3865217b87336f3e6d5ee6c86e11d8e7621d6e
Cosmin Sabou <csabou@mozilla.com>
2021-04-01 22:02:37 +0300

    Backed out 2 changesets (bug 1701787) for causing jit related windows crashes. CLOSED TREE
    Backed out changeset 5be01eeef88d (bug 1701787)
    Backed out changeset 54d193a0c932 (bug 1701787)

--- js/src/jit/CacheIRCompiler.cpp
+++ js/src/jit/CacheIRCompiler.cpp
@@ -6014,28 +6014,22 @@ bool CacheIRCompiler::emitLoadValueTruthyResult(ValOperandId inputId) {
   return true;
 }
 
-bool CacheIRCompiler::emitNewPlainObjectResult(uint32_t numFixedSlots,
-                                               uint32_t numDynamicSlots,
-                                               gc::AllocKind allocKind,
-                                               uint32_t shapeOffset) {
+bool CacheIRCompiler::emitLoadNewObjectFromTemplateResult(
+    uint32_t templateObjectOffset, uint32_t, uint32_t) {
   JitSpew(JitSpew_Codegen, "%s", __FUNCTION__);
   AutoOutputRegister output(*this);
   AutoScratchRegister obj(allocator, masm);
-  AutoScratchRegister scratch(allocator, masm);
-  AutoScratchRegisterMaybeOutput shape(allocator, masm, output);
+  AutoScratchRegisterMaybeOutput scratch(allocator, masm, output);
 
-  StubFieldOffset shapeSlot(shapeOffset, StubField::Type::Shape);
+  TemplateObject templateObj(objectStubFieldUnchecked(templateObjectOffset));
 
   FailurePath* failure;
   if (!addFailurePath(&failure)) {
     return false;
   }
 
-  emitLoadStubField(shapeSlot, shape);
-  masm.createPlainGCObject(obj, shape, scratch, shape, numFixedSlots,
-                           numDynamicSlots, allocKind, gc::DefaultHeap,
-                           failure->label());
-
+  masm.createGCObject(obj, scratch, templateObj, gc::DefaultHeap,
+                      failure->label());
   masm.tagValue(JSVAL_TYPE_OBJECT, obj, output.valueReg());
   return true;
 }
