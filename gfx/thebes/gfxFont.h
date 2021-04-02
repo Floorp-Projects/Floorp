5ceca766b11cb3bb089ce7cafa5f17dd48b6b082
Emilio Cobos Álvarez <emilio@crisal.io>
2021-04-02 12:37:23 +0000

    Bug 1701942 - Use bitwise equality for font variation value comparisons. r=jfkthame, a=tjr
    Just like we do for font sizes / size-adjust in gfxFontStyle.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110519

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -218,8 +218,7 @@ struct gfxFontStyle {
   }
 
   bool Equals(const gfxFontStyle& other) const {
-    return (*reinterpret_cast<const uint64_t*>(&size) ==
-            *reinterpret_cast<const uint64_t*>(&other.size)) &&
+    return mozilla::NumbersAreBitwiseIdentical(size, other.size) &&
            (style == other.style) && (weight == other.weight) &&
            (stretch == other.stretch) && (variantCaps == other.variantCaps) &&
            (variantSubSuper == other.variantSubSuper) &&
@@ -229,8 +228,7 @@ struct gfxFontStyle {
            (printerFont == other.printerFont) &&
            (useGrayscaleAntialiasing == other.useGrayscaleAntialiasing) &&
            (baselineOffset == other.baselineOffset) &&
-           (*reinterpret_cast<const uint32_t*>(&sizeAdjust) ==
-            *reinterpret_cast<const uint32_t*>(&other.sizeAdjust)) &&
+           mozilla::NumbersAreBitwiseIdentical(sizeAdjust, other.sizeAdjust) &&
            (featureSettings == other.featureSettings) &&
            (variantAlternates == other.variantAlternates) &&
            (featureValueLookup == other.featureValueLookup) &&
