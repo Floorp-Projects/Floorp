5134661ff9d1ca3b8068bde62536817bae6cb567
Kajal Sah <kajalksah07@gmail.com>
2021-04-02 18:06:50 +0000

    Bug 1702576 - Fixes eslint failures in browser/extensions/screenshots/sitehelper.js. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110689

--- browser/extensions/screenshots/sitehelper.js
+++ browser/extensions/screenshots/sitehelper.js
@@ -9,15 +9,13 @@
 "use strict";
 
 this.sitehelper = (function() {
-
   // This gives us the content's copy of XMLHttpRequest, instead of the wrapped
   // copy that this content script gets:
   const ContentXMLHttpRequest = content.XMLHttpRequest;
 
-  catcher.registerHandler((errorObj) => {
+  catcher.registerHandler(errorObj => {
     callBackground("reportError", errorObj);
   });
-
 
   const capabilities = {};
   function registerListener(name, func) {
@@ -31,7 +29,7 @@ this.sitehelper = (function() {
       // is safe to transfer:
       detail = JSON.stringify(detail);
     }
-    document.dispatchEvent(new CustomEvent(name, {detail}));
+    document.dispatchEvent(new CustomEvent(name, { detail }));
   }
 
   /** Set the cookie, even if third-party cookies are disabled in this browser
@@ -43,7 +41,10 @@ this.sitehelper = (function() {
 
     // This is a very minimal attempt to verify that the XMLHttpRequest object we got
     // is legitimate. It is not a good test.
-    if (Object.toString.apply(ContentXMLHttpRequest) !== "function XMLHttpRequest() {\n    [native code]\n}") {
+    if (
+      Object.toString.apply(ContentXMLHttpRequest) !==
+      "function XMLHttpRequest() {\n    [native code]\n}"
+    ) {
       console.warn("Insecure copy of XMLHttpRequest");
       return;
     }
@@ -55,40 +56,66 @@ this.sitehelper = (function() {
     req.send("");
     req.onload = () => {
       if (req.status !== 200) {
-        console.warn("Attempt to set Screenshots cookie via /api/set-login-cookie failed:", req.status, req.statusText, req.responseText);
+        console.warn(
+          "Attempt to set Screenshots cookie via /api/set-login-cookie failed:",
+          req.status,
+          req.statusText,
+          req.responseText
+        );
       }
     };
   }
 
-  registerListener("delete-everything", catcher.watchFunction((event) => {
-    // FIXME: reset some data in the add-on
-  }, false));
+  registerListener(
+    "delete-everything",
+    catcher.watchFunction(event => {
+      // FIXME: reset some data in the add-on
+    }, false)
+  );
 
-  registerListener("request-login", catcher.watchFunction((event) => {
-    const shotId = event.detail;
-    catcher.watchPromise(callBackground("getAuthInfo", shotId || null).then((info) => {
-      if (info) {
-        sendBackupCookieRequest(info.authHeaders);
-        sendCustomEvent("login-successful", {deviceId: info.deviceId, accountId: info.accountId, isOwner: info.isOwner, backupCookieRequest: true});
-      }
-    }));
-  }));
+  registerListener(
+    "request-login",
+    catcher.watchFunction(event => {
+      const shotId = event.detail;
+      catcher.watchPromise(
+        callBackground("getAuthInfo", shotId || null).then(info => {
+          if (info) {
+            sendBackupCookieRequest(info.authHeaders);
+            sendCustomEvent("login-successful", {
+              deviceId: info.deviceId,
+              accountId: info.accountId,
+              isOwner: info.isOwner,
+              backupCookieRequest: true,
+            });
+          }
+        })
+      );
+    })
+  );
 
-  registerListener("copy-to-clipboard", catcher.watchFunction(event => {
-    catcher.watchPromise(callBackground("copyShotToClipboard", event.detail));
-  }));
+  registerListener(
+    "copy-to-clipboard",
+    catcher.watchFunction(event => {
+      catcher.watchPromise(callBackground("copyShotToClipboard", event.detail));
+    })
+  );
 
-  registerListener("show-notification", catcher.watchFunction(event => {
-    catcher.watchPromise(callBackground("showNotification", event.detail));
-  }));
+  registerListener(
+    "show-notification",
+    catcher.watchFunction(event => {
+      catcher.watchPromise(callBackground("showNotification", event.detail));
+    })
+  );
 
   // Depending on the script loading order, the site might get the addon-present event,
   // but probably won't - instead the site will ask for that event after it has loaded
-  registerListener("request-addon-present", catcher.watchFunction(() => {
-    sendCustomEvent("addon-present", capabilities);
-  }));
+  registerListener(
+    "request-addon-present",
+    catcher.watchFunction(() => {
+      sendCustomEvent("addon-present", capabilities);
+    })
+  );
 
   sendCustomEvent("addon-present", capabilities);
-
 })();
 null;
