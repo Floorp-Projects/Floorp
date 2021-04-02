d259f8d10be5cc37599a8135541524e1256d5ace
Marco Bonardo <mbonardo@mozilla.com>
2021-04-02 08:15:26 +0000

    Bug 1700582 - Port relevant page actions tests. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D110572

--- browser/base/content/test/pageActions-proton/browser_PageActions_overflow.js
+++ browser/base/content/test/pageActions-proton/browser_PageActions_overflow.js
@@ -3,9 +3,10 @@
 
 "use strict";
 
-add_task(async function init() {
-  // We use an extension that shows a page action so that we can test the
-  // "remove extension" item in the context menu.
+add_task(async function test() {
+  // We use an extension that shows a page action. We must add this additional
+  // action because otherwise the meatball menu would not appear as an overflow
+  // for a single action.
   let extension = ExtensionTestUtils.loadExtension({
     manifest: {
       name: "Test contextMenu",
@@ -20,8 +21,7 @@ add_task(async function init() {
 
   let win = await BrowserTestUtils.openNewBrowserWindow();
   await SimpleTest.promiseFocus(win);
-  let originalWindowWidth = win.outerWidth;
-  Assert.greater(originalWindowWidth, 700, "window is bigger than 700px");
+  Assert.greater(win.outerWidth, 700, "window is bigger than 700px");
   BrowserTestUtils.loadURI(win.gBrowser, "data:text/html,<h1>A Page</h1>");
   await BrowserTestUtils.browserLoaded(win.gBrowser.selectedBrowser);
 
@@ -29,11 +29,6 @@ add_task(async function init() {
   // frame, so before we look for the button we should wait one animation frame
   // as well.
   await promiseAnimationFrame(win);
-
-  registerCleanupFunction(async () => {
-    win.resizeTo(originalWindowWidth, win.outerHeight);
-    await BrowserTestUtils.closeWindow(win);
-  });
 
   info("Check page action buttons are visible, the meatball button is not");
   let addonButton = win.BrowserPageActions.urlbarButtonNodeForActionID(
@@ -50,6 +45,7 @@ add_task(async function init() {
   info(
     "Shrink the window, check page action buttons are not visible, the meatball menu is visible"
   );
+  let originalOuterWidth = win.outerWidth;
   await promiseStableResize(500, win);
   Assert.ok(!BrowserTestUtils.is_visible(addonButton));
   Assert.ok(!BrowserTestUtils.is_visible(starButton));
@@ -67,24 +63,106 @@ add_task(async function init() {
     win.BrowserPageActions.urlbarButtonNodeForActionID(actionId),
     null
   );
+
+  await promiseStableResize(originalOuterWidth, win);
+  await BrowserTestUtils.closeWindow(win);
 });
 
-// TODO (Bug 1700780): Why is this necessary? Without this trick the test
-// fails intermittently on Ubuntu.
-function promiseStableResize(expectedWidth, win = window) {
-  let deferred = PromiseUtils.defer();
-  let id;
-  function listener() {
-    win.clearTimeout(id);
-    info(`Got resize event: ${win.innerWidth} x ${win.innerHeight}`);
-    if (win.innerWidth <= expectedWidth) {
-      id = win.setTimeout(() => {
-        win.removeEventListener("resize", listener);
-        deferred.resolve();
-      }, 100);
-    }
-  }
-  win.addEventListener("resize", listener);
-  win.resizeTo(expectedWidth, win.outerHeight);
-  return deferred.promise;
-}
+add_task(async function bookmark() {
+  // We use an extension that shows a page action. We must add this additional
+  // action because otherwise the meatball menu would not appear as an overflow
+  // for a single action.
+  let extension = ExtensionTestUtils.loadExtension({
+    manifest: {
+      name: "Test contextMenu",
+      page_action: { show_matches: ["<all_urls>"] },
+    },
+
+    useAddonManager: "temporary",
+  });
+
+  await extension.startup();
+  const url = "data:text/html,<h1>A Page</h1>";
+  let win = await BrowserTestUtils.openNewBrowserWindow();
+  await SimpleTest.promiseFocus(win);
+  BrowserTestUtils.loadURI(win.gBrowser, url);
+  await BrowserTestUtils.browserLoaded(win.gBrowser.selectedBrowser);
+
+  // The pageAction implementation enables the button at the next animation
+  // frame, so before we look for the button we should wait one animation frame
+  // as well.
+  await promiseAnimationFrame(win);
+
+  info("Shrink the window if necessary, check the meatball menu is visible");
+  let originalOuterWidth = win.outerWidth;
+  await promiseStableResize(500, win);
+
+  let meatballButton = win.document.getElementById("pageActionButton");
+  Assert.ok(BrowserTestUtils.is_visible(meatballButton));
+
+  // Open the panel.
+  await promisePageActionPanelOpen(win);
+
+  // The bookmark button should read "Bookmark Current Tab" and not be starred.
+  let bookmarkButton = win.document.getElementById("pageAction-panel-bookmark");
+  Assert.equal(bookmarkButton.label, "Bookmark Current Tab");
+  Assert.ok(!bookmarkButton.hasAttribute("starred"));
+
+  // Click the button.
+  let hiddenPromise = promisePageActionPanelHidden(win);
+  let showPromise = BrowserTestUtils.waitForPopupEvent(
+    win.StarUI.panel,
+    "shown"
+  );
+  EventUtils.synthesizeMouseAtCenter(bookmarkButton, {}, win);
+  await hiddenPromise;
+  await showPromise;
+  win.StarUI.panel.hidePopup();
+
+  // Open the panel again.
+  await promisePageActionPanelOpen(win);
+
+  // The bookmark button should now read "Edit This Bookmark" and be starred.
+  Assert.equal(bookmarkButton.label, "Edit This Bookmark");
+  Assert.ok(bookmarkButton.hasAttribute("starred"));
+  Assert.equal(bookmarkButton.getAttribute("starred"), "true");
+
+  // Click it again.
+  hiddenPromise = promisePageActionPanelHidden(win);
+  showPromise = BrowserTestUtils.waitForPopupEvent(win.StarUI.panel, "shown");
+  EventUtils.synthesizeMouseAtCenter(bookmarkButton, {}, win);
+  await hiddenPromise;
+  await showPromise;
+
+  let onItemRemovedPromise = PlacesTestUtils.waitForNotification(
+    "bookmark-removed",
+    events => events.some(event => event.url == url),
+    "places"
+  );
+
+  // Click the remove-bookmark button in the panel.
+  win.StarUI._element("editBookmarkPanelRemoveButton").click();
+
+  // Wait for the bookmark to be removed before continuing.
+  await onItemRemovedPromise;
+
+  // Open the panel again.
+  await promisePageActionPanelOpen(win);
+
+  // The bookmark button should read "Bookmark Current Tab" and not be starred.
+  Assert.equal(bookmarkButton.label, "Bookmark Current Tab");
+  Assert.ok(!bookmarkButton.hasAttribute("starred"));
+
+  // Done.
+  hiddenPromise = promisePageActionPanelHidden();
+  win.BrowserPageActions.panelNode.hidePopup();
+  await hiddenPromise;
+
+  info("Remove the extension");
+  let promiseUninstalled = promiseAddonUninstalled(extension.id);
+  await extension.unload();
+  await promiseUninstalled;
+
+  await promiseStableResize(originalOuterWidth, win);
+  await BrowserTestUtils.closeWindow(win);
+});
