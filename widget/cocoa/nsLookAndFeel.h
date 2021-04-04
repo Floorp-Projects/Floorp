99374165549a118c499fa049708f26b9abcbf5cf
Markus Stange <mstange.moz@gmail.com>
2021-04-04 15:48:00 +0000

    Bug 1702876 - Remove caching from macOS nsLookAndFeel, and add an NSAppearanceForColorScheme function. r=emilio
    This also switches us to use the modern Objective C syntax for accessing properties and NSArray elements.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110762

--- widget/cocoa/nsLookAndFeel.h
+++ widget/cocoa/nsLookAndFeel.h
@@ -12,9 +12,8 @@ class nsLookAndFeel final : public nsXPLookAndFeel {
   nsLookAndFeel();
   virtual ~nsLookAndFeel();
 
-  void NativeInit() final;
-  virtual void RefreshImpl() override;
-  nsresult NativeGetColor(ColorID, ColorScheme, nscolor& aResult) override;
+  void NativeInit() final { RecordTelemetry(); }
+  nsresult NativeGetColor(ColorID, ColorScheme, nscolor& aColor) override;
   nsresult NativeGetInt(IntID, int32_t& aResult) override;
   nsresult NativeGetFloat(FloatID, float& aResult) override;
   bool NativeGetFont(FontID aID, nsString& aFontName,
@@ -28,49 +27,6 @@ class nsLookAndFeel final : public nsXPLookAndFeel {
  protected:
   static bool SystemWantsDarkTheme();
   static nscolor ProcessSelectionBackground(nscolor aColor);
-
- private:
-  struct ColorSet {
-    void Refresh(ColorScheme);
-
-    mozilla::Maybe<nscolor> Get(ColorID) const;
-
-    nscolor mColorTextSelectBackground = 0;
-    nscolor mColorTextSelectBackgroundDisabled = 0;
-    nscolor mColorHighlight = 0;
-    nscolor mColorAlternateSelectedControlText = 0;
-    nscolor mColorControlText = 0;
-    nscolor mColorText = 0;
-    nscolor mColorWindowText = 0;
-    nscolor mColorGrid = 0;
-    nscolor mColorActiveBorder = 0;
-    nscolor mColorGrayText = 0;
-    nscolor mColorControlBackground = 0;
-    nscolor mColorScrollbar = 0;
-    nscolor mColorThreeDHighlight = 0;
-    nscolor mColorDialog = 0;
-    nscolor mColorDragTargetZone = 0;
-    nscolor mColorChromeActive = 0;
-    nscolor mColorChromeInactive = 0;
-    nscolor mColorFocusRing = 0;
-    nscolor mColorTextSelect = 0;
-    nscolor mColorDisabledToolbarText = 0;
-    nscolor mColorMenuSelect = 0;
-    nscolor mColorCellHighlight = 0;
-    nscolor mColorEvenTreeRow = 0;
-    nscolor mColorOddTreeRow = 0;
-    nscolor mColorMenuFontSmoothingBg = 0;
-    nscolor mColorSourceListFontSmoothingBg = 0;
-    nscolor mColorSourceListSelectionFontSmoothingBg = 0;
-    nscolor mColorActiveSourceListSelectionFontSmoothingBg = 0;
-  };
-
-  bool mInitialized = false;
-
-  ColorSet mLightColorSet;
-  ColorSet mDarkColorSet;
-
-  void EnsureInit();
 };
 
 #endif  // nsLookAndFeel_h_
