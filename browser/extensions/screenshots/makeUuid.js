fbb67969ca9372097643b203460cb2764c43e3f2
Kajal Sah <kajalksah07@gmail.com>
2021-04-05 16:13:19 +0000

    Bug 1702567 - Fixes eslint errors in browser/extensions/screenshots/makeUuid.js. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110750

--- browser/extensions/screenshots/makeUuid.js
+++ browser/extensions/screenshots/makeUuid.js
@@ -5,17 +5,16 @@
 "use strict";
 
 this.makeUuid = (function() {
-
   // generates a v4 UUID
-  return function makeUuid() { // eslint-disable-line no-unused-vars
+  return function makeUuid() {
+    // eslint-disable-line no-unused-vars
     // get sixteen unsigned 8 bit random values
-    const randomValues = window
-      .crypto
-      .getRandomValues(new Uint8Array(36));
+    const randomValues = window.crypto.getRandomValues(new Uint8Array(36));
 
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
       const i = Array.prototype.slice.call(arguments).slice(-2)[0]; // grab the `offset` parameter
-      const r = randomValues[i] % 16|0, v = c === "x" ? r : (r & 0x3 | 0x8);
+      const r = randomValues[i] % 16 | 0,
+        v = c === "x" ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
   };
