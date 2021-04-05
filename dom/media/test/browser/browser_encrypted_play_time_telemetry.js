ca29af8bbd37a6a009ec2ff987838ac72ec0903e
Bryce Seager van Dyk <bvandyk@mozilla.com>
2021-04-05 14:32:14 +0000

    Bug 1696034 - Expand EME telemetry test to cover cases involving unecnrypted media. r=alwu
    Expands the EME telemetry test to cover two additional cases.
    1. The EME media is loaded but before playback the src is switched to unecrypted
       media. In this case, we don't expect encrypted playtime to be recorded, but
       we will still collect clearkey playtime as the element still has clearkey
       media keys attached.
    2. Same as the above, but where the media keys are also cleared. In this case we
       do not expect either encrypted or clearkey time to be recorded.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110550

--- dom/media/test/browser/browser_encrypted_play_time_telemetry.js
+++ dom/media/test/browser/browser_encrypted_play_time_telemetry.js
@@ -20,7 +20,7 @@ async function clearTelemetry() {
   await new Promise(resolve => setTimeout(resolve, 2000));
 
   Services.telemetry.clearEvents();
-  await TestUtils.waitForCondition(() => {
+  return TestUtils.waitForCondition(() => {
     let events = Services.telemetry.snapshotEvents(
       Ci.nsITelemetry.DATASET_ALL_CHANNELS,
       true
@@ -29,20 +29,21 @@ async function clearTelemetry() {
   });
 }
 
-// Opens a tab and plays clearkey encrypted media in it. Resolves the returned
-// promise once playback is complete.
-async function playEmeMedia() {
-  // Open a tab with an empty page.
+// Opens a tab containing a blank page, returns a promise that will resolve
+// to that tab.
+async function openTab() {
   const emptyPageUri =
     "https://example.com/browser/dom/media/test/browser/file_empty_page.html";
-  const tab = await BrowserTestUtils.openNewForegroundTab(
-    window.gBrowser,
-    emptyPageUri
-  );
+  return BrowserTestUtils.openNewForegroundTab(window.gBrowser, emptyPageUri);
+}
 
+// Creates and configures a video element for EME playback in `tab`. Does not
+// start playback for the element. Returns a promise that will resolve once
+// the element is setup and ready for playback.
+async function loadEmeVideo(tab) {
   const emeHelperUri =
     gTestPath.substr(0, gTestPath.lastIndexOf("/")) + "/eme_standalone.js";
-  await SpecialPowers.spawn(
+  return SpecialPowers.spawn(
     tab.linkedBrowser,
     [emeHelperUri],
     async _emeHelperUri => {
@@ -63,6 +64,7 @@ async function playEmeMedia() {
       Services.scriptloader.loadSubScript(_emeHelperUri, content);
       // Setup EME with the helper.
       let video = content.document.createElement("video");
+      video.id = "media";
       content.document.body.appendChild(video);
       let emeHelper = new content.wrappedJSObject.EmeHelper();
       emeHelper.SetKeySystem(
@@ -93,61 +95,170 @@ async function playEmeMedia() {
       await once(sb, "updateend");
       ms.endOfStream();
       await once(ms, "sourceended");
-      let endedPromise = once(video, "ended");
-      // Done setting up MSE.
-
-      // Play the video and wait for it to finish.
-      await video.play();
-      await endedPromise;
     }
   );
-
-  BrowserTestUtils.removeTab(tab);
 }
 
-// Verify the telemetry gathered has the expected values.
-async function verifyTelemetry() {
+// Plays the media in `tab` until the 'ended' event is fire. Returns a promise
+// that resolves once that state has been reached.
+async function playMediaThrough(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async () => {
+    let video = content.document.getElementById("media");
+    await Promise.all([new Promise(r => (video.onended = r)), video.play()]);
+  });
+}
+
+// Plays the media in `tab` until the 'timeupdate' event is fire. Returns a
+// promise that resolves once that state has been reached.
+async function playMediaToTimeUpdate(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async () => {
+    let video = content.document.getElementById("media");
+    await Promise.all([
+      new Promise(r => (video.ontimeupdate = r)),
+      video.play(),
+    ]);
+  });
+}
+
+// Aborts existing loads and replaces the media on the media element with an
+// unencrypted file.
+async function replaceMediaWithUnencrypted(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async () => {
+    let video = content.document.getElementById("media");
+    video.src = "gizmo.mp4";
+    video.load();
+  });
+}
+
+// Clears/nulls the media keys on the media in `tab`.
+async function clearMediaKeys(tab) {
+  return SpecialPowers.spawn(tab.linkedBrowser, [], async () => {
+    let video = content.document.getElementById("media");
+    await video.setMediaKeys(null);
+  });
+}
+
+// Wait for telemetry information to be received from the content process
+// then get the relevant histograms for the tests and return the sums of
+// those histograms. If a histogram does not exist this will return a 0
+// sum. Returns a promise the resolves to an object with sums for
+// - VIDEO_PLAY_TIME_MS
+// - VIDEO_ENCRYPTED_PLAY_TIME_MS
+// - VIDEO_CLEARKEY_PLAY_TIME_MS
+// This function clears the histograms as it gets them.
+async function getTelemetrySums() {
   // The telemetry was gathered in the content process, so we have to wait
   // until is arrived in the parent to check it. At time of writing there's
   // not a more elegant way of doing this than polling.
-  let playTimeSums = await TestUtils.waitForCondition(() => {
+  return TestUtils.waitForCondition(() => {
     let histograms = Services.telemetry.getSnapshotForHistograms("main", true)
       .content;
     // All the histogram data should come at the same time, so we just check
-    // for playtime here, but will read more.
+    // for playtime here as we always expect it in these tests, but we'll
+    // grab other values if present.
     if (histograms.VIDEO_PLAY_TIME_MS) {
       // We only expect to have one value for each histogram, so returning the
       // sums is a short hand for returning that one value.
       return {
         VIDEO_PLAY_TIME_MS: histograms.VIDEO_PLAY_TIME_MS.sum,
-        VIDEO_ENCRYPTED_PLAY_TIME_MS:
-          histograms.VIDEO_ENCRYPTED_PLAY_TIME_MS.sum,
-        VIDEO_CLEARKEY_PLAY_TIME_MS: histograms.VIDEO_CLEARKEY_PLAY_TIME_MS.sum,
+        VIDEO_ENCRYPTED_PLAY_TIME_MS: histograms.VIDEO_ENCRYPTED_PLAY_TIME_MS
+          ? histograms.VIDEO_ENCRYPTED_PLAY_TIME_MS.sum
+          : 0,
+        VIDEO_CLEARKEY_PLAY_TIME_MS: histograms.VIDEO_CLEARKEY_PLAY_TIME_MS
+          ? histograms.VIDEO_CLEARKEY_PLAY_TIME_MS.sum
+          : 0,
       };
     }
     return null;
   }, "recorded telemetry from playing media");
+}
 
-  // Finally, verify the telemetry data.
-  ok(playTimeSums, "Should get play time telemetry");
+// Clear telemetry before other tests. Internally the tests clear the telemetry
+// when they check it, so we shouldn't need to do this between tests.
+add_task(clearTelemetry);
+
+add_task(async function testEncryptedMediaPlayback() {
+  let testTab = await openTab();
+
+  await loadEmeVideo(testTab);
+  await playMediaThrough(testTab);
+
+  BrowserTestUtils.removeTab(testTab);
+
+  let telemetrySums = await getTelemetrySums();
+
+  ok(telemetrySums, "Should get play time telemetry");
   is(
-    playTimeSums.VIDEO_PLAY_TIME_MS,
-    playTimeSums.VIDEO_ENCRYPTED_PLAY_TIME_MS,
+    telemetrySums.VIDEO_PLAY_TIME_MS,
+    telemetrySums.VIDEO_ENCRYPTED_PLAY_TIME_MS,
     "Play time should be the same as encrypted play time"
   );
   is(
-    playTimeSums.VIDEO_PLAY_TIME_MS,
-    playTimeSums.VIDEO_CLEARKEY_PLAY_TIME_MS,
+    telemetrySums.VIDEO_PLAY_TIME_MS,
+    telemetrySums.VIDEO_CLEARKEY_PLAY_TIME_MS,
     "Play time should be the same as clearkey play time"
   );
   ok(
-    playTimeSums.VIDEO_PLAY_TIME_MS > 0,
+    telemetrySums.VIDEO_PLAY_TIME_MS > 0,
     "Should have a play time greater than zero"
   );
-}
-
-add_task(async function testEncryptedMediaTelemetryProbes() {
-  await clearTelemetry();
-  await playEmeMedia();
-  await verifyTelemetry();
 });
+
+add_task(async function testChangingFromEncryptedToUnencrypted() {
+  let testTab = await openTab();
+
+  await loadEmeVideo(testTab);
+  await replaceMediaWithUnencrypted(testTab);
+  await playMediaToTimeUpdate(testTab);
+
+  BrowserTestUtils.removeTab(testTab);
+
+  let telemetrySums = await getTelemetrySums();
+
+  ok(telemetrySums, "Should get play time telemetry");
+  is(
+    telemetrySums.VIDEO_ENCRYPTED_PLAY_TIME_MS,
+    0,
+    "Encrypted play time should be 0"
+  );
+  is(
+    telemetrySums.VIDEO_PLAY_TIME_MS,
+    telemetrySums.VIDEO_CLEARKEY_PLAY_TIME_MS,
+    "Play time should be the same as clearkey play time because the media element still has a media keys attached"
+  );
+  ok(
+    telemetrySums.VIDEO_PLAY_TIME_MS > 0,
+    "Should have a play time greater than zero"
+  );
+});
+
+add_task(
+  async function testChangingFromEncryptedToUnencryptedAndClearingMediaKeys() {
+    let testTab = await openTab();
+
+    await loadEmeVideo(testTab);
+    await replaceMediaWithUnencrypted(testTab);
+    await clearMediaKeys(testTab);
+    await playMediaToTimeUpdate(testTab);
+
+    BrowserTestUtils.removeTab(testTab);
+
+    let telemetrySums = await getTelemetrySums();
+
+    ok(telemetrySums, "Should get play time telemetry");
+    is(
+      telemetrySums.VIDEO_ENCRYPTED_PLAY_TIME_MS,
+      0,
+      "Encrypted play time should be 0"
+    );
+    is(
+      telemetrySums.VIDEO_CLEARKEY_PLAY_TIME_MS,
+      0,
+      "Clearkey play time should be 0"
+    );
+    ok(
+      telemetrySums.VIDEO_PLAY_TIME_MS > 0,
+      "Should have a play time greater than zero"
+    );
+  }
+);
