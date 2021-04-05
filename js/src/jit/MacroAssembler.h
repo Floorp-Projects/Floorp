7c36c2e9c568d0743a2df2f7c375642c7c4eb331
Jan de Mooij <jdemooij@mozilla.com>
2021-04-05 18:16:26 +0000

    Bug 1700052 part 5 - Add movePropertyKey to the MacroAssembler. r=iain
    Also clean up the similar Push method a bit to use PropertyKey instead of jsid.
    
    Depends on D110251
    
    Differential Revision: https://phabricator.services.mozilla.com/D110252

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -437,7 +437,7 @@ class MacroAssembler : public MacroAssemblerSpecific {
   void Push(FloatRegister reg) PER_SHARED_ARCH;
   void PushBoxed(FloatRegister reg) PER_ARCH;
   void PushFlags() DEFINED_ON(x86_shared);
-  void Push(jsid id, Register scratchReg);
+  void Push(JS::PropertyKey key, Register scratchReg);
   void Push(const Address& addr);
   void Push(TypedOrValueRegister v);
   void Push(const ConstantOrRegister& v);
@@ -856,6 +856,8 @@ class MacroAssembler : public MacroAssemblerSpecific {
                  const ValueOperand& dest) PER_ARCH;
   void moveValue(const ValueOperand& src, const ValueOperand& dest) PER_ARCH;
   void moveValue(const Value& src, const ValueOperand& dest) PER_ARCH;
+
+  void movePropertyKey(JS::PropertyKey key, Register dest);
 
   // ===============================================================
   // Load instructions
