4b697111b6575b0fac86e9c22412d9d5af494673
David Parks <daparks@mozilla.com>
2021-04-05 23:48:33 +0000

    Bug 1682030 - Remove NPAPI from dom/plugins tests.  r=jmathies
    Removes NPAPI plugin tests in dom/plugins, as well as the TestPlugin, as part of removing all of NPAPI plugin support.  A small handful of tests that are still relevant remain in that directory.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107138

--- dom/plugins/test/testplugin/flashplugin/nptest_name.cpp
+++
@@ -1,8 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-const char* sPluginName = "Shockwave Flash";
-const char* sPluginDescription = "Flash plug-in for testing purposes.";
-const char* sMimeDescription =
-    "application/x-shockwave-flash-test:swf:Flash test type";
