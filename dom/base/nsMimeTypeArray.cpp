0fcbfe4bdae7e1f45d322485bee3858365900f3c
David Parks <daparks@mozilla.com>
2021-04-05 23:48:39 +0000

    Bug 1682030 - Reduce nsMimeTypeArray and nsPluginArray to "constants"  r=jmathies
    We still use these types so that the typing of these arrays in navigator follows the spec but they are never populated since we have removed all NPAPI plugin support.  So these classes are greatly simplified.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107151

--- dom/base/nsMimeTypeArray.cpp
+++ dom/base/nsMimeTypeArray.cpp
@@ -8,12 +8,6 @@
 
 #include "mozilla/dom/MimeTypeArrayBinding.h"
 #include "mozilla/dom/MimeTypeBinding.h"
-#include "nsPIDOMWindow.h"
-#include "nsPluginArray.h"
-#include "mozilla/dom/Navigator.h"
-#include "nsServiceManagerUtils.h"
-#include "nsContentUtils.h"
-#include "nsPluginTags.h"
 
 using namespace mozilla;
 using namespace mozilla::dom;
@@ -25,8 +19,7 @@ NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(nsMimeTypeArray)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
 
-NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(nsMimeTypeArray, mWindow, mMimeTypes,
-                                      mCTPMimeTypes)
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(nsMimeTypeArray, mWindow)
 
 nsMimeTypeArray::nsMimeTypeArray(nsPIDOMWindowInner* aWindow)
     : mWindow(aWindow) {}
@@ -38,165 +31,12 @@ JSObject* nsMimeTypeArray::WrapObject(JSContext* aCx,
   return MimeTypeArray_Binding::Wrap(aCx, this, aGivenProto);
 }
 
-void nsMimeTypeArray::Refresh() {
-  mMimeTypes.Clear();
-  mCTPMimeTypes.Clear();
-}
-
 nsPIDOMWindowInner* nsMimeTypeArray::GetParentObject() const {
   MOZ_ASSERT(mWindow);
   return mWindow;
 }
 
-nsMimeType* nsMimeTypeArray::Item(uint32_t aIndex, CallerType aCallerType) {
-  bool unused;
-  return IndexedGetter(aIndex, unused, aCallerType);
-}
-
-nsMimeType* nsMimeTypeArray::NamedItem(const nsAString& aName,
-                                       CallerType aCallerType) {
-  bool unused;
-  return NamedGetter(aName, unused, aCallerType);
-}
-
-nsMimeType* nsMimeTypeArray::IndexedGetter(uint32_t aIndex, bool& aFound,
-                                           CallerType aCallerType) {
-  aFound = false;
-
-  if (nsContentUtils::ResistFingerprinting(aCallerType)) {
-    return nullptr;
-  }
-
-  EnsurePluginMimeTypes();
-
-  if (aIndex >= mMimeTypes.Length()) {
-    return nullptr;
-  }
-
-  aFound = true;
-
-  return mMimeTypes[aIndex];
-}
-
-static nsMimeType* FindMimeType(const nsTArray<RefPtr<nsMimeType>>& aMimeTypes,
-                                const nsAString& aType) {
-  for (uint32_t i = 0; i < aMimeTypes.Length(); ++i) {
-    nsMimeType* mimeType = aMimeTypes[i];
-    if (aType.Equals(mimeType->Type())) {
-      return mimeType;
-    }
-  }
-
-  return nullptr;
-}
-
-nsMimeType* nsMimeTypeArray::NamedGetter(const nsAString& aName, bool& aFound,
-                                         CallerType aCallerType) {
-  aFound = false;
-
-  if (nsContentUtils::ResistFingerprinting(aCallerType)) {
-    return nullptr;
-  }
-
-  EnsurePluginMimeTypes();
-
-  nsString lowerName(aName);
-  ToLowerCase(lowerName);
-
-  nsMimeType* mimeType = FindMimeType(mMimeTypes, lowerName);
-  if (mimeType) {
-    aFound = true;
-    return mimeType;
-  }
-  nsMimeType* hiddenType = FindMimeType(mCTPMimeTypes, lowerName);
-  if (hiddenType) {
-    nsPluginArray::NotifyHiddenPluginTouched(hiddenType->GetEnabledPlugin());
-  }
-
-  return nullptr;
-}
-
-uint32_t nsMimeTypeArray::Length(CallerType aCallerType) {
-  if (nsContentUtils::ResistFingerprinting(aCallerType)) {
-    return 0;
-  }
-
-  EnsurePluginMimeTypes();
-
-  return mMimeTypes.Length();
-}
-
-void nsMimeTypeArray::GetSupportedNames(nsTArray<nsString>& aRetval,
-                                        CallerType aCallerType) {
-  if (nsContentUtils::ResistFingerprinting(aCallerType)) {
-    return;
-  }
-
-  EnsurePluginMimeTypes();
-
-  for (uint32_t i = 0; i < mMimeTypes.Length(); ++i) {
-    aRetval.AppendElement(mMimeTypes[i]->Type());
-  }
-}
-
-void nsMimeTypeArray::EnsurePluginMimeTypes() {
-  if (!mMimeTypes.IsEmpty() || !mWindow) {
-    return;
-  }
-
-  RefPtr<Navigator> navigator = mWindow->Navigator();
-
-  nsPluginArray* pluginArray = navigator->GetPlugins(IgnoreErrors());
-  if (!pluginArray) {
-    return;
-  }
-
-  pluginArray->GetMimeTypes(mMimeTypes);
-  pluginArray->GetCTPMimeTypes(mCTPMimeTypes);
-}
-
 NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(nsMimeType, AddRef)
 NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(nsMimeType, Release)
 
-NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(nsMimeType, mWindow, mPluginElement)
-
-nsMimeType::nsMimeType(nsPIDOMWindowInner* aWindow,
-                       nsPluginElement* aPluginElement, const nsAString& aType,
-                       const nsAString& aDescription,
-                       const nsAString& aExtension)
-    : mWindow(aWindow),
-      mPluginElement(aPluginElement),
-      mType(aType),
-      mDescription(aDescription),
-      mExtension(aExtension) {
-  MOZ_ASSERT(aPluginElement);
-}
-
-nsMimeType::~nsMimeType() = default;
-
-nsPIDOMWindowInner* nsMimeType::GetParentObject() const {
-  MOZ_ASSERT(mWindow);
-  return mWindow;
-}
-
-JSObject* nsMimeType::WrapObject(JSContext* aCx,
-                                 JS::Handle<JSObject*> aGivenProto) {
-  return MimeType_Binding::Wrap(aCx, this, aGivenProto);
-}
-
-void nsMimeType::GetDescription(nsString& aRetval) const {
-  aRetval = mDescription;
-}
-
-nsPluginElement* nsMimeType::GetEnabledPlugin() const {
-  // mPluginElement might be null if we got unlinked but are still somehow being
-  // called into.
-  if (!mPluginElement || !mPluginElement->PluginTag()->IsEnabled()) {
-    return nullptr;
-  }
-  return mPluginElement;
-}
-
-void nsMimeType::GetSuffixes(nsString& aRetval) const { aRetval = mExtension; }
-
-void nsMimeType::GetType(nsString& aRetval) const { aRetval = mType; }
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(nsMimeType)
