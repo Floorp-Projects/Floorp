9717c27492a08678a3b81c5338ce419e0b4a25d7
Andreas Pehrson <apehrson@mozilla.com>
2021-04-06 20:15:11 +0000

    Bug 1695580 - In xpcom, cancel pending DelayedRunnable timers on shutdown. r=KrisWright
    Because DelayedRunnables are fire-and-forget, there is no way for a targeted
    EventTarget to clean them up on shutdown. Thus if a timer fires after
    EventTarget shutdown it will fail to dispatch the timer event, and avoid
    releasing the timer callback because it's not on the targeted thread. This
    causes a leak as there is a ref-cycle between nsTimerImpl::mCallback and
    DelayedRunnable::mTimer.
    
    This patch adds nsIDelayedRunnableObserver for a target to observe which
    DelayedRunnables are relying on their timer to run them. This allows the target
    to schedule a shutdown task to cancel those timers and release the runnables on
    the target thread.
    
    Supported DelayedRunnable targets with this patch are TaskQueues,
    eventqueue-based nsThreads and XPCOMThreadWrappers that wrap a supported
    nsThread.
    
    An assertion makes sure at runtime that future new uses of DelayedRunnable
    target nsIDelayedRunnableObserver-supported event targets.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109781

--- xpcom/threads/DelayedRunnable.h
+++ xpcom/threads/DelayedRunnable.h
@@ -9,6 +9,7 @@
 
 #include "mozilla/TimeStamp.h"
 #include "nsCOMPtr.h"
+#include "nsIDelayedRunnableObserver.h"
 #include "nsIRunnable.h"
 #include "nsITimer.h"
 #include "nsThreadUtils.h"
@@ -26,11 +27,18 @@ class DelayedRunnable : public Runnable, public nsITimerCallback {
 
   nsresult Init();
 
+  /**
+   * Cancels the underlying timer. Called when the target is going away, so the
+   * runnable can be released safely on the target thread.
+   */
+  void CancelTimer();
+
  private:
   ~DelayedRunnable() = default;
   nsresult DoRun();
 
   const nsCOMPtr<nsIEventTarget> mTarget;
+  const nsCOMPtr<nsIDelayedRunnableObserver> mObserver;
   nsCOMPtr<nsIRunnable> mWrappedRunnable;
   nsCOMPtr<nsITimer> mTimer;
   const TimeStamp mDelayedFrom;
