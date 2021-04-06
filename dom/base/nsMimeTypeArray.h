83acccf8814f80ba1e5e0bdc9947b307a3e4a778
David Parks <daparks@mozilla.com>
2021-04-06 19:28:16 +0000

    Bug 1682030 - Reduce nsMimeTypeArray and nsPluginArray to "constants"  r=jmathies
    We still use these types so that the typing of these arrays in navigator follows the spec but they are never populated since we have removed all NPAPI plugin support.  So these classes are greatly simplified.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107151

--- dom/base/nsMimeTypeArray.h
+++ dom/base/nsMimeTypeArray.h
@@ -7,15 +7,22 @@
 #ifndef nsMimeTypeArray_h___
 #define nsMimeTypeArray_h___
 
-#include "nsString.h"
-#include "nsTArray.h"
 #include "nsWrapperCache.h"
+#include "nsCOMPtr.h"
 #include "nsPIDOMWindow.h"
+#include "nsTArray.h"
 #include "mozilla/dom/BindingDeclarations.h"
 
 class nsMimeType;
 class nsPluginElement;
 
+namespace mozilla::dom {
+enum class CallerType : uint32_t;
+}  // namespace mozilla::dom
+
+/**
+ * Array class backing HTML's navigator.mimeTypes.  This array is always empty.
+ */
 class nsMimeTypeArray final : public nsISupports, public nsWrapperCache {
  public:
   explicit nsMimeTypeArray(nsPIDOMWindowInner* aWindow);
@@ -27,66 +34,82 @@ class nsMimeTypeArray final : public nsISupports, public nsWrapperCache {
   virtual JSObject* WrapObject(JSContext* aCx,
                                JS::Handle<JSObject*> aGivenProto) override;
 
-  void Refresh();
+  void Refresh() {}
 
   // MimeTypeArray WebIDL methods
-  nsMimeType* Item(uint32_t index, mozilla::dom::CallerType aCallerType);
+  nsMimeType* Item(uint32_t index, mozilla::dom::CallerType aCallerType) {
+    return nullptr;
+  }
+
   nsMimeType* NamedItem(const nsAString& name,
-                        mozilla::dom::CallerType aCallerType);
+                        mozilla::dom::CallerType aCallerType) {
+    return nullptr;
+  }
+
   nsMimeType* IndexedGetter(uint32_t index, bool& found,
-                            mozilla::dom::CallerType aCallerType);
+                            mozilla::dom::CallerType aCallerType) {
+    return nullptr;
+  }
+
   nsMimeType* NamedGetter(const nsAString& name, bool& found,
-                          mozilla::dom::CallerType aCallerType);
-  uint32_t Length(mozilla::dom::CallerType aCallerType);
+                          mozilla::dom::CallerType aCallerType) {
+    return nullptr;
+  }
+
+  uint32_t Length(mozilla::dom::CallerType aCallerType) { return 0; }
+
   void GetSupportedNames(nsTArray<nsString>& retval,
-                         mozilla::dom::CallerType aCallerType);
+                         mozilla::dom::CallerType aCallerType) {}
 
  protected:
   virtual ~nsMimeTypeArray();
 
-  void EnsurePluginMimeTypes();
-  void Clear();
-
   nsCOMPtr<nsPIDOMWindowInner> mWindow;
-
-  // mMimeTypes contains MIME types handled by plugins or by an OS
-  // PreferredApplicationHandler.
-  nsTArray<RefPtr<nsMimeType> > mMimeTypes;
-  nsTArray<RefPtr<nsMimeType> > mCTPMimeTypes;
 };
 
+/**
+ * Mime type class backing entries in HTML's navigator.mimeTypes array.
+ * Currently, these cannot be constructed.
+ */
 class nsMimeType final : public nsWrapperCache {
  public:
   NS_INLINE_DECL_CYCLE_COLLECTING_NATIVE_REFCOUNTING(nsMimeType)
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_NATIVE_CLASS(nsMimeType)
 
-  nsMimeType(nsPIDOMWindowInner* aWindow, nsPluginElement* aPluginElement,
-             const nsAString& aType, const nsAString& aDescription,
-             const nsAString& aExtension);
-  nsPIDOMWindowInner* GetParentObject() const;
-  virtual JSObject* WrapObject(JSContext* aCx,
-                               JS::Handle<JSObject*> aGivenProto) override;
+  // Never constructed.
+  nsMimeType() = delete;
 
-  const nsString& Type() const { return mType; }
+  nsPIDOMWindowInner* GetParentObject() const {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+    return nullptr;
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aGivenProto) override {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+    return nullptr;
+  }
 
   // MimeType WebIDL methods
-  void GetDescription(nsString& retval) const;
-  nsPluginElement* GetEnabledPlugin() const;
-  void GetSuffixes(nsString& retval) const;
-  void GetType(nsString& retval) const;
+  void GetDescription(nsString& retval) const {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+  }
+
+  nsPluginElement* GetEnabledPlugin() const {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+    return nullptr;
+  }
+
+  void GetSuffixes(nsString& retval) const {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+  }
+
+  void GetType(nsString& retval) const {
+    MOZ_ASSERT_UNREACHABLE("nsMimeType can not exist");
+  }
 
  protected:
-  virtual ~nsMimeType();
-
-  nsCOMPtr<nsPIDOMWindowInner> mWindow;
-
-  // Strong reference to the active plugin. Note that this creates an explicit
-  // reference cycle through the plugin element's mimetype array. We rely on the
-  // cycle collector to break this cycle.
-  RefPtr<nsPluginElement> mPluginElement;
-  nsString mType;
-  nsString mDescription;
-  nsString mExtension;
+  virtual ~nsMimeType() = default;
 };
 
 #endif /* nsMimeTypeArray_h___ */
