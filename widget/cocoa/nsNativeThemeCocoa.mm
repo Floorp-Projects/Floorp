44938048def3909d530e597190947cc3963f2cd7
Markus Stange <mstange.moz@gmail.com>
2021-04-06 18:40:43 +0000

    Bug 1700839 - Make sure that search field cells render the magnifying glass icon at the start of the field. r=bradwerth, a=RyanVM
    Depends on D110971
    
    Differential Revision: https://phabricator.services.mozilla.com/D110972

--- widget/cocoa/nsNativeThemeCocoa.mm
+++ widget/cocoa/nsNativeThemeCocoa.mm
@@ -258,6 +258,25 @@ static void DrawCellIncludingFocusRing(NSCell* aCell, NSRect aWithFrame, NSView*
 
 @implementation MOZSearchFieldCell
 
+- (instancetype)init {
+  // We would like to render a search field which has the magnifying glass icon at the start of the
+  // search field, and no cancel button.
+  // On 10.12 and 10.13, empty search fields render the magnifying glass icon in the middle of the
+  // field. So in order to get the icon to show at the start of the field, we need to give the field
+  // some content. We achieve this with a single space character.
+  self = [super initTextCell:@" "];
+
+  // However, because the field is now non-empty, by default it shows a cancel button. To hide the
+  // cancel button, override it with a custom NSButtonCell which renders nothing.
+  NSButtonCell* invisibleCell = [[NSButtonCell alloc] initImageCell:nil];
+  invisibleCell.bezeled = NO;
+  invisibleCell.bordered = NO;
+  self.cancelButtonCell = invisibleCell;
+  [invisibleCell release];
+
+  return self;
+}
+
 - (BOOL)_isToolbarMode {
   return self.shouldUseToolbarStyle;
 }
@@ -419,7 +438,7 @@ nsNativeThemeCocoa::nsNativeThemeCocoa() {
   [mTextFieldCell setEditable:YES];
   [mTextFieldCell setFocusRingType:NSFocusRingTypeExterior];
 
-  mSearchFieldCell = [[MOZSearchFieldCell alloc] initTextCell:@""];
+  mSearchFieldCell = [[MOZSearchFieldCell alloc] init];
   [mSearchFieldCell setBezelStyle:NSTextFieldRoundedBezel];
   [mSearchFieldCell setBezeled:YES];
   [mSearchFieldCell setEditable:YES];
