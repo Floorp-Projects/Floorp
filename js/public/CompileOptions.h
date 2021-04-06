735bb5aa2a3bfa303a42aa4b364f73a4ef60a04f
Yulia Startsev <ystartsev@mozilla.com>
2021-04-06 12:10:12 +0000

    Bug 1681046 - Enable Top Level Await by default r=mgaudet
    Differential Revision: https://phabricator.services.mozilla.com/D110394

--- js/public/CompileOptions.h
+++ js/public/CompileOptions.h
@@ -131,7 +131,7 @@ class JS_PUBLIC_API TransitiveCompileOptions {
   bool nonSyntacticScope = false;
   bool privateClassFields = false;
   bool privateClassMethods = false;
-  bool topLevelAwait = false;
+  bool topLevelAwait = true;
 
   // True if transcoding to XDR should use Stencil instead of JSScripts.
   bool useStencilXDR = false;
