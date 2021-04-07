142488d3c4e6f5b38b988e737e463536f3d2078c
Edgar Chen <echen@mozilla.com>
2021-04-07 11:23:45 +0000

    Bug 1677334 - Part 2: Remove Mac-specific check for click event on xul frame; r=NeilDeakin
    Now we won't dispatch click event at all when ctrl key is pressed on Mac, so we
    don't need those Mac-specific checks.
    
    This patch reverts part of https://hg.mozilla.org/mozilla-central/rev/d8726d18021e
    and revise the tests accordingly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D110608

--- layout/xul/nsResizerFrame.cpp
+++ layout/xul/nsResizerFrame.cpp
@@ -286,14 +286,7 @@ nsresult nsResizerFrame::HandleEvent(nsPresContext* aPresContext,
 
     case eMouseClick: {
       WidgetMouseEvent* mouseEvent = aEvent->AsMouseEvent();
-      if (mouseEvent->IsLeftClickEvent()
-#ifdef XP_MACOSX
-          // On Mac, ctrl-click will send a context menu event from the widget,
-          // so we don't want to dispatch widget command if it is redispatched
-          // from the mouse event with ctrl key is pressed.
-          && !mouseEvent->IsControl()
-#endif
-      ) {
+      if (mouseEvent->IsLeftClickEvent()) {
         MouseClicked(mouseEvent);
       }
       break;
