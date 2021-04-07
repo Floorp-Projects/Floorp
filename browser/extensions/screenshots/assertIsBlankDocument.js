7987b55c57c6d4a3b47776b9371c832fb0a82605
Falguni Islam <falgunimst95@gmail.com>
2021-04-07 20:59:46 +0000

    Bug 1702532: fix eslint check passes in browser/extensions/screenshots/assertIsBlankDocument.js. r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110574

--- browser/extensions/screenshots/assertIsBlankDocument.js
+++ browser/extensions/screenshots/assertIsBlankDocument.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/* globals browser */
+
 /** For use inside an iframe onload function, throws an Error if iframe src is not blank.html
 
     Should be applied *inside* catcher.watchFunction
