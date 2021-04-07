ec72828c6bef1c46d9e2c65520ecafbb530892d2
Valentin Gosu <valentin.gosu@gmail.com>
2021-04-07 07:29:58 +0000

    Bug 1700076 - Clean up test_trr_httpssvc.js r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D109598

--- netwerk/test/unit/test_use_httpssvc.js
+++ netwerk/test/unit/test_use_httpssvc.js
@@ -6,9 +6,7 @@
 
 ChromeUtils.import("resource://gre/modules/NetUtil.jsm");
 
-let prefs;
 let h2Port;
-let listen;
 
 const dns = Cc["@mozilla.org/network/dns-service;1"].getService(
   Ci.nsIDNSService
@@ -16,93 +14,28 @@ const dns = Cc["@mozilla.org/network/dns-service;1"].getService(
 const certOverrideService = Cc[
   "@mozilla.org/security/certoverride;1"
 ].getService(Ci.nsICertOverrideService);
-const threadManager = Cc["@mozilla.org/thread-manager;1"].getService(
-  Ci.nsIThreadManager
-);
-const mainThread = threadManager.currentThread;
-
-const defaultOriginAttributes = {};
 
 function setup() {
+  trr_test_setup();
+
   let env = Cc["@mozilla.org/process/environment;1"].getService(
     Ci.nsIEnvironment
   );
   h2Port = env.get("MOZHTTP2_PORT");
   Assert.notEqual(h2Port, null);
   Assert.notEqual(h2Port, "");
-
-  // Set to allow the cert presented by our H2 server
-  do_get_profile();
-  prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
-
-  prefs.setBoolPref("network.http.spdy.enabled", true);
-  prefs.setBoolPref("network.http.spdy.enabled.http2", true);
-  // the TRR server is on 127.0.0.1
-  prefs.setCharPref("network.trr.bootstrapAddress", "127.0.0.1");
-
-  // make all native resolve calls "secretly" resolve localhost instead
-  prefs.setBoolPref("network.dns.native-is-localhost", true);
-
-  // 0 - off, 1 - race, 2 TRR first, 3 TRR only, 4 shadow
-  prefs.setIntPref("network.trr.mode", 2); // TRR first
-  prefs.setBoolPref("network.trr.wait-for-portal", false);
-  // don't confirm that TRR is working, just go!
-  prefs.setCharPref("network.trr.confirmationNS", "skip");
-
-  // So we can change the pref without clearing the cache to check a pushed
-  // record with a TRR path that fails.
-  Services.prefs.setBoolPref("network.trr.clear-cache-on-pref-change", false);
+  Services.prefs.setIntPref("network.trr.mode", 2); // TRR first
 
   Services.prefs.setBoolPref("network.dns.upgrade_with_https_rr", true);
   Services.prefs.setBoolPref("network.dns.use_https_rr_as_altsvc", true);
-
-  // The moz-http2 cert is for foo.example.com and is signed by http2-ca.pem
-  // so add that cert to the trust list as a signing cert.  // the foo.example.com domain name.
-  const certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(
-    Ci.nsIX509CertDB
-  );
-  addCertFromFile(certdb, "http2-ca.pem", "CTu,u,u");
 }
 
 setup();
 registerCleanupFunction(() => {
-  prefs.clearUserPref("network.http.spdy.enabled");
-  prefs.clearUserPref("network.http.spdy.enabled.http2");
-  prefs.clearUserPref("network.dns.localDomains");
-  prefs.clearUserPref("network.dns.native-is-localhost");
-  prefs.clearUserPref("network.trr.mode");
-  prefs.clearUserPref("network.trr.uri");
-  prefs.clearUserPref("network.trr.credentials");
-  prefs.clearUserPref("network.trr.wait-for-portal");
-  prefs.clearUserPref("network.trr.allow-rfc1918");
-  prefs.clearUserPref("network.trr.useGET");
-  prefs.clearUserPref("network.trr.confirmationNS");
-  prefs.clearUserPref("network.trr.bootstrapAddress");
-  prefs.clearUserPref("network.trr.blacklist-duration");
-  prefs.clearUserPref("network.trr.request-timeout");
-  prefs.clearUserPref("network.trr.clear-cache-on-pref-change");
-  prefs.clearUserPref("network.dns.upgrade_with_https_rr");
-  prefs.clearUserPref("network.dns.use_https_rr_as_altsvc");
+  trr_clear_prefs();
+  Services.prefs.clearUserPref("network.dns.upgrade_with_https_rr");
+  Services.prefs.clearUserPref("network.dns.use_https_rr_as_altsvc");
 });
-
-class DNSListener {
-  constructor() {
-    this.promise = new Promise(resolve => {
-      this.resolve = resolve;
-    });
-  }
-  onLookupComplete(inRequest, inRecord, inStatus) {
-    this.resolve([inRequest, inRecord, inStatus]);
-  }
-  // So we can await this as a promise.
-  then() {
-    return this.promise.then.apply(this.promise, arguments);
-  }
-}
-
-DNSListener.prototype.QueryInterface = ChromeUtils.generateQI([
-  "nsIDNSListener",
-]);
 
 function makeChan(url) {
   let chan = NetUtil.newChannel({
@@ -128,7 +61,7 @@ function channelOpenPromise(chan) {
 // the transaction is added in connection manager.
 add_task(async function testUseHTTPSSVCForHttpsUpgrade() {
   // use the h2 server as DOH provider
-  prefs.setCharPref(
+  Services.prefs.setCharPref(
     "network.trr.uri",
     "https://foo.example.com:" + h2Port + "/httpssvc_as_altsvc"
   );
@@ -169,7 +102,7 @@ EventSinkListener.prototype.QueryInterface = ChromeUtils.generateQI([
 // Test if the request is upgraded to https with a HTTPSSVC record.
 add_task(async function testUseHTTPSSVCAsHSTS() {
   // use the h2 server as DOH provider
-  prefs.setCharPref(
+  Services.prefs.setCharPref(
     "network.trr.uri",
     "https://foo.example.com:" + h2Port + "/httpssvc_as_altsvc"
   );
@@ -210,28 +143,16 @@ add_task(async function testUseHTTPSSVCAsHSTS() {
 // the transaction is added in connection manager.
 add_task(async function testUseHTTPSSVC() {
   // use the h2 server as DOH provider
-  prefs.setCharPref(
+  Services.prefs.setCharPref(
     "network.trr.uri",
     "https://foo.example.com:" + h2Port + "/httpssvc_as_altsvc"
   );
 
-  let listener = new DNSListener();
-
   // Do DNS resolution before creating the channel, so the HTTPSSVC record will
   // be resolved from the cache.
-  let request = dns.asyncResolve(
-    "test.httpssvc.com",
-    dns.RESOLVE_TYPE_HTTPSSVC,
-    0,
-    null, // resolverInfo
-    listener,
-    mainThread,
-    defaultOriginAttributes
-  );
-
-  let [inRequest, , inStatus] = await listener;
-  Assert.equal(inRequest, request, "correct request was used");
-  Assert.equal(inStatus, Cr.NS_OK, "status OK");
+  await new TRRDNSListener("test.httpssvc.com", {
+    type: dns.RESOLVE_TYPE_HTTPSSVC,
+  });
 
   // We need to skip the security check, since our test cert is signed for
   // foo.example.com, not test.httpssvc.com.
@@ -291,21 +212,9 @@ add_task(async function testFallback() {
     ],
   });
 
-  let listener = new DNSListener();
-
-  let request = dns.asyncResolve(
-    "test.fallback.com",
-    dns.RESOLVE_TYPE_HTTPSSVC,
-    0,
-    null, // resolverInfo
-    listener,
-    mainThread,
-    defaultOriginAttributes
-  );
-
-  let [inRequest, inRecord, inStatus] = await listener;
-  Assert.equal(inRequest, request, "correct request was used");
-  Assert.equal(inStatus, Cr.NS_OK, "status OK");
+  let [, inRecord] = await new TRRDNSListener("test.fallback.com", {
+    type: dns.RESOLVE_TYPE_HTTPSSVC,
+  });
 
   let record = inRecord
     .QueryInterface(Ci.nsIDNSHTTPSSVCRecord)
