33ae2f101d3fdbf49204a5f2f325e499061ffbe1
Mitchell Hentges <mhentges@mozilla.com>
2021-04-08 23:06:29 +0000

    Bug 1697489: Run rusttests on Mac in CI r=firefox-build-system-reviewers,glandium
    Run rusttests (debug) on `trunk` and rusttests (opt)
    on `mozilla-central`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109513

---
+++ testing/mozharness/configs/builds/releng_sub_mac_configs/rusttests.py
@@ -0,0 +1,12 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+config = {
+    "default_actions": [
+        "build",
+    ],
+    "stage_platform": "macosx64-rusttests",
+    "app_name": "tools/rusttests",
+    "disable_package_metrics": True,
+}
