0252b54eb6fff62935731b4a7dbd65aa8e86005c
Falguni Islam <falgunimst95@gmail.com>
2021-04-08 16:18:01 +0000

    Bug 1702569: fix eslint browser/extensions/screenshots/selector/callBackground.js r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110739

--- browser/extensions/screenshots/selector/callBackground.js
+++ browser/extensions/screenshots/selector/callBackground.js
@@ -2,12 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-/* globals log */
+/* globals log, browser */
 
 "use strict";
 
 this.callBackground = function callBackground(funcName, ...args) {
-  return browser.runtime.sendMessage({funcName, args}).then((result) => {
+  return browser.runtime.sendMessage({ funcName, args }).then(result => {
     if (result && result.type === "success") {
       return result.value;
     } else if (result && result.type === "error") {
@@ -22,8 +22,11 @@ this.callBackground = function callBackground(funcName, ...args) {
       throw exc;
     } else {
       log.error("Unexpected background result:", result);
-      const exc = new Error(`Bad response type from background page: ${result && result.type || undefined}`);
-      exc.resultType = result ? (result.type || "undefined") : "undefined result";
+      const exc = new Error(
+        `Bad response type from background page: ${(result && result.type) ||
+          undefined}`
+      );
+      exc.resultType = result ? result.type || "undefined" : "undefined result";
       throw exc;
     }
   });
