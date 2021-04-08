1a3b80e8a5d93e854a9b3c7cd6a419aa2bba35d1
Tom Schuster <evilpies@gmail.com>
2021-04-08 07:37:06 +0000

    Bug 1701319 - Add GTK platform support for pasting files. r=stransky
    This uses text/uri-list to construct the internal kFileMime.
    
    For testing e10s has to be disabled, due to bug 1308007.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109960

--- widget/gtk/WidgetUtilsGtk.h
+++ widget/gtk/WidgetUtilsGtk.h
@@ -6,11 +6,13 @@
 #ifndef WidgetUtilsGtk_h__
 #define WidgetUtilsGtk_h__
 
+#include "nsString.h"
+#include "nsTArray.h"
+
 #include <stdint.h>
 #include <gdk/gdk.h>
 
-namespace mozilla {
-namespace widget {
+namespace mozilla::widget {
 
 class WidgetUtilsGTK {
  public:
@@ -26,8 +28,9 @@ bool GdkIsX11Display(GdkDisplay* display);
 bool GdkIsWaylandDisplay();
 bool GdkIsX11Display();
 
-}  // namespace widget
+// Parse text/uri-list
+nsTArray<nsCString> ParseTextURIList(const nsACString& data);
 
-}  // namespace mozilla
+}  // namespace mozilla::widget
 
 #endif  // WidgetUtilsGtk_h__
