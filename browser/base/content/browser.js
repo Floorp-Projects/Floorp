0201d62878e73bfd921c888b6cd47df2bfa000c1
Mark Striemer <mstriemer@mozilla.com>
2021-04-08 22:25:04 +0000

    Bug 1702330 - Graduate infobars to main proton pref r=jaws,marionette-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D110638

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -396,7 +396,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
   return new MozElements.NotificationBox(element => {
     element.classList.add("global-notificationbox");
     element.setAttribute("notificationside", "top");
-    if (gProtonInfobarsEnabled) {
+    if (gProton) {
       // With Proton enabled all notification boxes are at the top, built into the browser chrome.
       let tabNotifications = document.getElementById("tab-notification-deck");
       gNavToolbox.insertBefore(element, tabNotifications);
@@ -408,7 +408,7 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
 
 // Regular notification bars shown at the bottom of the window.
 XPCOMUtils.defineLazyGetter(this, "gNotificationBox", () => {
-  return gProtonInfobarsEnabled
+  return gProton
     ? gHighPriorityNotificationBox
     : new MozElements.NotificationBox(element => {
         element.classList.add("global-notificationbox");
@@ -585,14 +585,6 @@ XPCOMUtils.defineLazyPreferenceGetter(
       doc.documentElement.toggleAttribute("proton", newValue);
     }
   }
-);
-
-/* Temporary pref while the Proton infobars work stabilizes. */
-XPCOMUtils.defineLazyPreferenceGetter(
-  this,
-  "gProtonInfobarsEnabled",
-  "browser.proton.infobars.enabled",
-  false
 );
 
 /* Temporary pref while the dust settles around the updated tooltip design
