e68a67bded78e0174e3226c77810134a51646f95
Marco Bonardo <mbonardo@mozilla.com>
2021-04-08 20:22:10 +0000

    Bug 1703835 - Skip uitour/test/browser_showMenu.js for proton urlbar. r=adw
    Differential Revision: https://phabricator.services.mozilla.com/D111281

--- browser/components/uitour/test/browser_showMenu.js
+++ browser/components/uitour/test/browser_showMenu.js
@@ -11,6 +11,10 @@ var gContentWindow;
 add_task(setup_UITourTest);
 
 add_UITour_task(async function test_showMenu_hideMenu_pageActionPanel() {
+  // There is no page actions panel in Proton.
+  if (gProton) {
+    return;
+  }
   let pageActionPanel = BrowserPageActions.panelNode;
   let shownPromise = promisePanelElementShown(window, pageActionPanel);
   await showMenuPromise("pageActionPanel");
