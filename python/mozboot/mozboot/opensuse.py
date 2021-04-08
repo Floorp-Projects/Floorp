936522eb3d0fa8534bfdcac2d6a064504447154f
Csoregi Natalia <ncsoregi@mozilla.com>
2021-04-08 11:04:47 +0300

    Backed out changeset 3ec9f9951ffe (bug 1377445) for bustage due to missing file. CLOSED TREE

--- python/mozboot/mozboot/opensuse.py
+++ python/mozboot/mozboot/opensuse.py
@@ -31,6 +31,7 @@ class OpenSUSEBootstrapper(LinuxBootstrapper, BaseBootstrapper):
         "libXt-devel",
         "libproxy-devel",
         "libuuid-devel",
+        "gtk2-devel",
         "clang-devel",
         "patterns-gnome-devel_gnome",
     ]
