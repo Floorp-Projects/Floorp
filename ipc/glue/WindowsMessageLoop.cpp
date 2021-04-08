5c58dc5815a8f8fe48fb56352ebe130aeff5939e
Brad Werth <bwerth@mozilla.com>
2021-04-08 16:42:07 +0000

    WIP: Bug 1685183 Part 3: Remove remaining references to PluginInstanceChild. r=jgilbert
    These references were left behind in the cleanup of the plugin code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111197

--- ipc/glue/WindowsMessageLoop.cpp
+++ ipc/glue/WindowsMessageLoop.cpp
@@ -419,7 +419,6 @@ ProcessOrDeferMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 }  // namespace
 
-// We need the pointer value of this in PluginInstanceChild.
 LRESULT CALLBACK NeuteredWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
                                     LPARAM lParam) {
   WNDPROC oldWndProc = (WNDPROC)GetProp(hwnd, kOldWndProcProp);
