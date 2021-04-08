540f0a3d43d2730334f0f88dc3ec982b6c50e835
Dan Minor <dminor@mozilla.com>
2021-04-08 14:56:33 +0000

    Bug 1695937 - Add intl::NumberFormat to components; r=zbraniecki,tcampbell,jandem
    This adds an initial version of NumberFormat that is suitable for use by
    Fluent.
    
    The options formatting code is taken from SpiderMonkey and modified to use the
    standard c++ library rather than SpiderMonkey specific code. The code
    generated MeasureUnitGenerated.h is duplicated here temporarily. Once we can
    move SpiderMonkey to using this code, we can update the code generation to
    avoid this duplication.
    
    Some simple gtests have been written to make development easier. Full test
    coverage is not a goal here, as this code will eventually be covered by running
    test262 in SpiderMonkey.
    
    Differential Revision: https://phabricator.services.mozilla.com/D107223

---
+++ intl/components/src/NumberFormatterSkeleton.h
@@ -0,0 +1,89 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef intl_components_NumberFormatterSkeleton_h_
+#define intl_components_NumberFormatterSkeleton_h_
+#include <string_view>
+#include "mozilla/Vector.h"
+#include "unicode/unumberformatter.h"
+
+namespace mozilla {
+namespace intl {
+
+struct NumberFormatOptions;
+
+/**
+ * Class to create a number formatter skeleton.
+ *
+ * The skeleton syntax is documented at:
+ * https://github.com/unicode-org/icu/blob/master/docs/userguide/format_parse/numbers/skeletons.md
+ */
+class MOZ_STACK_CLASS NumberFormatterSkeleton final {
+ public:
+  explicit NumberFormatterSkeleton(const NumberFormatOptions& options);
+
+  /**
+   * Return a new UNumberFormatter based on this skeleton.
+   */
+  UNumberFormatter* toFormatter(std::string_view locale);
+
+ private:
+  static constexpr size_t DefaultVectorSize = 128;
+
+  mozilla::Vector<char16_t, DefaultVectorSize> mVector;
+  bool mValidSkeleton = false;
+
+  bool append(char16_t c) { return mVector.append(c); }
+
+  bool appendN(char16_t c, size_t times) { return mVector.appendN(c, times); }
+
+  template <size_t N>
+  bool append(const char16_t (&chars)[N]) {
+    static_assert(N > 0,
+                  "should only be used with string literals or properly "
+                  "null-terminated arrays");
+    MOZ_ASSERT(chars[N - 1] == '\0',
+               "should only be used with string literals or properly "
+               "null-terminated arrays");
+    // Without trailing \0.
+    return mVector.append(chars, N - 1);
+  }
+
+  template <size_t N>
+  bool appendToken(const char16_t (&token)[N]) {
+    return append(token) && append(' ');
+  }
+
+  bool append(const char* chars, size_t length) {
+    return mVector.append(chars, length);
+  }
+
+  bool currency(std::string_view currency);
+
+  bool currencyDisplay(NumberFormatOptions::CurrencyDisplayStyle display);
+
+  bool unit(std::string_view unit);
+
+  bool unitDisplay(NumberFormatOptions::UnitDisplay display);
+
+  bool percent();
+
+  bool fractionDigits(uint32_t min, uint32_t max);
+
+  bool minIntegerDigits(uint32_t min);
+
+  bool significantDigits(uint32_t min, uint32_t max);
+
+  bool disableGrouping();
+
+  bool notation(NumberFormatOptions::Notation style);
+
+  bool signDisplay(NumberFormatOptions::SignDisplay display);
+
+  bool roundingModeHalfUp();
+};
+
+}  // namespace intl
+}  // namespace mozilla
+
+#endif
