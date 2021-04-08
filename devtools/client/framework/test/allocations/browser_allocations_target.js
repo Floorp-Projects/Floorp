85d335cd0edbe9254098c13efbc4e935d0573e63
Julian Descottes <jdescottes@mozilla.com>
2021-04-08 12:48:23 +0000

    Bug 1703545 - [devtools] Destroy descriptor in browser_allocations_target.js to keep asserting client destruction r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D111102

--- devtools/client/framework/test/allocations/browser_allocations_target.js
+++ devtools/client/framework/test/allocations/browser_allocations_target.js
@@ -56,6 +56,9 @@ async function testScript(tab) {
   await new Promise(resolve => setTimeout(resolve, 1000));
 
   await target.destroy();
+
+  // destroy the descriptor to also destroy the dedicated client.
+  await descriptor.destroy();
 }
 
 add_task(async function() {
