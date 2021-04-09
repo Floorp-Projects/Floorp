03027922359c13c360fa2cff617b390720b3fb98
Emilio Cobos Álvarez <emilio@crisal.io>
2021-04-09 14:50:44 +0000

    Bug 1703693 - Prefer faces for fonts that can only be named in one way. r=jfkthame
    This produces the desired rendering here by effectively "deprioritizing"
    faces from fonts that have multiple names.
    
    Your call whether this is a good idea or not, seems reasonable off-hand
    to me.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111283

--- gfx/thebes/gfxFcPlatformFontList.h
+++ gfx/thebes/gfxFcPlatformFontList.h
@@ -177,7 +177,7 @@ class gfxFontconfigFontFamily final : public gfxFontFamily {
 
   // Families are constructed initially with just references to patterns.
   // When necessary, these are enumerated within FindStyleVariations.
-  void AddFontPattern(FcPattern* aFontPattern);
+  void AddFontPattern(FcPattern* aFontPattern, bool aSingleName);
 
   void SetFamilyContainsAppFonts(bool aContainsAppFonts) {
     mContainsAppFonts = aContainsAppFonts;
