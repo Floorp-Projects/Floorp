d906bbb1e258c5f170df4873917ab5df404de0ce
Andrew Osmond <aosmond@mozilla.com>
2021-04-09 00:27:54 +0000

    Bug 1703839 - Defer OOM crashing for mapping in shared surfaces to frame rendering. r=jrmuizel
    If we fail to map in a shared surface that we support unmapping for,
    don't OOM crash immediately if this is the first time we are mapping it.
    This is because we probably freed up a bunch of virtual memory already
    purging the expiration tracker, but on Windows, this may take some time
    before the virtual address space is available for reuse. Since there
    will be some time gap between the mapping and the frame rendering, this
    may be sufficient time to recover in some cases, avoiding a GPU process
    OOM crash.
    
    This should only affect 32-bit users, since we never unmap on 64-bit
    builds.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111270

--- gfx/layers/SourceSurfaceSharedData.h
+++ gfx/layers/SourceSurfaceSharedData.h
@@ -102,7 +102,7 @@ class SourceSurfaceSharedDataWrapper final : public DataSourceSurface {
     return mozilla::ipc::SharedMemory::PageAlignedSize(GetDataLength());
   }
 
-  void EnsureMapped(size_t aLength);
+  bool EnsureMapped(size_t aLength);
 
   // Protects mapping and unmapping of mBuf.
   Maybe<Mutex> mHandleLock;
