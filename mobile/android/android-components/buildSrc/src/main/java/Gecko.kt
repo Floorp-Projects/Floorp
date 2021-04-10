4126387aeb553b9d6ab1b215fa58e6a119054650
MickeyMoz <sebastian@mozilla.com>
2021-04-10 21:49:37 +0000

    [components] Update GeckoView (Beta) to 88.0.20210410185959.

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -11,7 +11,7 @@ internal object GeckoVersions {
     /**
      * GeckoView Beta Version.
      */
-    const val beta_version = "88.0.20210408190318"
+    const val beta_version = "88.0.20210410185959"
 
     /**
      * GeckoView Release Version.
