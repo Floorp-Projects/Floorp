a17f12a884e53948b54b01476ada5e328bb22614
Falguni Islam <falgunimst95@gmail.com>
2021-04-11 23:38:54 +0000

    Bug 1702545: fix eslint browser/extensions/screenshots/build/buildSettings.js r=emalysz
    Differential Revision: https://phabricator.services.mozilla.com/D110734

--- browser/extensions/screenshots/build/buildSettings.js
+++ browser/extensions/screenshots/build/buildSettings.js
@@ -5,11 +5,10 @@
 window.buildSettings = {
   defaultSentryDsn: "",
   logLevel: "" || "warn",
-  captureText: ("" === "true"),
-  uploadBinary: ("" === "true"),
+  captureText: "" === "true",
+  uploadBinary: "" === "true",
   pngToJpegCutoff: parseInt("" || 2500000, 10),
   maxImageHeight: parseInt("" || 10000, 10),
-  maxImageWidth: parseInt("" || 10000, 10)
+  maxImageWidth: parseInt("" || 10000, 10),
 };
 null;
-
