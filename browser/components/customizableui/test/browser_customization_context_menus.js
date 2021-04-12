4431d18f32b7a5fa2b575b3cb169b0573b664ce4
Gabriel Luong <gabriel.luong@gmail.com>
2021-04-12 10:28:46 +0000

    Bug 1692840 - Add "Open a New Tab" at the top of the tabstrip context menu. r=Gijs
    Differential Revision: https://phabricator.services.mozilla.com/D108365

--- browser/components/customizableui/test/browser_customization_context_menus.js
+++ browser/components/customizableui/test/browser_customization_context_menus.js
@@ -65,6 +65,8 @@ add_task(async function tabstrip_context() {
   let closedTabsAvailable = SessionStore.getClosedTabCount(window) == 0;
   info("Closed tabs: " + closedTabsAvailable);
   let expectedEntries = [
+    ["#toolbar-context-openANewTab", true],
+    ["---"],
     ["#toolbar-context-reloadSelectedTab", true],
     ["#toolbar-context-bookmarkSelectedTab", true],
     ["#toolbar-context-selectAllTabs", true],
