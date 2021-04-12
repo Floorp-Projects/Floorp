d734f6705a01121109cb2274b392ddb2d632bae0
nchevobbe <nchevobbe@mozilla.com>
2021-04-12 05:08:31 +0000

    Bug 1703460 - [devtools] Fix intermittent leak on browser_target_configuration_command_color_scheme.js. r=bomsy.
    It looks like the test was leaking because we were using loadUri to reload, or the fact
    that we were using browserLoaded after calling loadUri.
    So here we do create a promise from browserLoaded before initiating the navigation,
    and we use the more straightforward gBrowser.reloadTab to reload.
    
    Depends on D111396
    
    Differential Revision: https://phabricator.services.mozilla.com/D111397

--- devtools/shared/commands/target-configuration/tests/browser_target_configuration_command_color_scheme.js
+++ devtools/shared/commands/target-configuration/tests/browser_target_configuration_command_color_scheme.js
@@ -55,8 +55,12 @@ add_task(async function() {
   );
 
   info("Reload the page");
-  BrowserTestUtils.loadURI(gBrowser.selectedBrowser, TEST_URI);
-  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+  let onPageLoaded = BrowserTestUtils.browserLoaded(
+    gBrowser.selectedBrowser,
+    true
+  );
+  gBrowser.reloadTab(tab);
+  await onPageLoaded;
 
   is(
     await topLevelDocumentMatchPrefersDarkColorSchemeMediaAtStartup(),
@@ -84,11 +88,12 @@ add_task(async function() {
     "Check that navigating to a page that forces the creation of a new browsing context keep the simulation enabled"
   );
 
+  onPageLoaded = BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser, true);
   BrowserTestUtils.loadURI(
     gBrowser.selectedBrowser,
     URL_ROOT_ORG_SSL + TEST_DOCUMENT + "?crossOriginIsolated=true"
   );
-  await BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+  await onPageLoaded;
 
   isnot(
     gBrowser.selectedBrowser.browsingContext.id,
