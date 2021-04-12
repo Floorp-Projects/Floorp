538130ccbd3adaae518250e70828643eeabe7949
Eugen Sawin <esawin@mozilla.com>
2021-04-12 21:57:15 +0000

    Bug 1691819 - [2.1] Share required prefs to enable credit card and address form autocomplete for GeckoView. r=geckoview-reviewers,agi
    Differential Revision: https://phabricator.services.mozilla.com/D108521

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4631,3 +4631,33 @@ pref("security.external_protocol_requires_permission", true);
 #ifdef XP_WIN
   pref("browser.enableAboutThirdParty", false);
 #endif
+
+// Preferences for the form autofill toolkit component.
+// The truthy values of "extensions.formautofill.available" are "on" and "detect",
+// any other value means autofill isn't available.
+// "detect" means it's enabled if conditions defined in the extension are met.
+pref("extensions.formautofill.available", "detect");
+pref("extensions.formautofill.addresses.enabled", true);
+pref("extensions.formautofill.addresses.capture.enabled", false);
+pref("extensions.formautofill.creditCards.available", true);
+pref("extensions.formautofill.creditCards.enabled", true);
+// Temporary preference to control displaying the UI elements for
+// credit card autofill used for the duration of the A/B test.
+pref("extensions.formautofill.creditCards.hideui", false);
+// Pref for shield/heartbeat to recognize users who have used Credit Card
+// Autofill. The valid values can be:
+// 0: none
+// 1: submitted a manually-filled credit card form (but didn't see the doorhanger
+//    because of a duplicate profile in the storage)
+// 2: saw the doorhanger
+// 3: submitted an autofill'ed credit card form
+pref("extensions.formautofill.creditCards.used", 0);
+pref("extensions.formautofill.firstTimeUse", true);
+pref("extensions.formautofill.heuristics.enabled", true);
+pref("extensions.formautofill.section.enabled", true);
+pref("extensions.formautofill.loglevel", "Warn");
+
+pref("toolkit.osKeyStore.loglevel", "Warn");
+
+pref("extensions.formautofill.supportedCountries", "US,CA");
+pref("extensions.formautofill.supportRTL", false);
