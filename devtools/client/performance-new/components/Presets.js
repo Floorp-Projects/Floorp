3776c29a7c84a97441a0fac778f7f8da636746c8
Itiel <itiel_yn8@walla.com>
2021-04-13 18:46:13 +0000

    Bug 1704027 - Switch to using grid for checkboxes and radiobuttons containers in about:profiling to fix alignment of input and text r=julienw,mconley
    Also reset the margin-inline-start for checkboxes, and make the radiobuttons margin-inline-end the same as the checkboxes, as per spec.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111416

--- devtools/client/performance-new/components/Presets.js
+++ devtools/client/performance-new/components/Presets.js
@@ -64,13 +64,10 @@ class Preset extends PureComponent {
         checked: selected,
         onChange: this.onChange,
       }),
-      div(
-        { className: "perf-presets-text" },
-        div({ className: "perf-toggle-text-label" }, labelText),
-        description
-          ? div({ className: "perf-toggle-description" }, description)
-          : null
-      )
+      div({ className: "perf-toggle-text-label" }, labelText),
+      description
+        ? div({ className: "perf-toggle-description" }, description)
+        : null
     );
   }
 }
