149c050c0399629efad21ff43da4df27f97d7917
Jon Coppeard <jcoppeard@mozilla.com>
2021-04-13 15:51:45 +0000

    Bug 1704576 - Make the CacheIR plain object op handle failure by calling into the VM rather than failing the stub r=jandem
    Differential Revision: https://phabricator.services.mozilla.com/D111687

--- js/src/jit/VMFunctionList-inl.h
+++ js/src/jit/VMFunctionList-inl.h
@@ -192,6 +192,7 @@ namespace jit {
   _(NewCallObject, js::jit::NewCallObject)                                     \
   _(NewObjectOperation, js::NewObjectOperation)                                \
   _(NewObjectOperationWithTemplate, js::NewObjectOperationWithTemplate)        \
+  _(NewPlainObject, js::NewPlainObject)                                        \
   _(NewRegExpStringIterator, js::NewRegExpStringIterator)                      \
   _(NewStringIterator, js::NewStringIterator)                                  \
   _(NewStringObject, js::jit::NewStringObject)                                 \
