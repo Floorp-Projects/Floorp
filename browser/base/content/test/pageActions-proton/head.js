10190f03d95c92a6cb9e361cac7d35c73c4939c8
Markus Stange <mstange.moz@gmail.com>
2021-04-13 16:22:21 +0000

    Bug 1704569 - Don't synthesize events on menus in browser/base/content/test/pageActions-proton/browser_PageActions_removeExtension.js. r=mak
    Differential Revision: https://phabricator.services.mozilla.com/D111777

--- browser/base/content/test/pageActions-proton/head.js
+++ browser/base/content/test/pageActions-proton/head.js
@@ -70,13 +70,13 @@ function promisePanelEvent(panelIDOrNode, eventType, win = window) {
       (eventType == "popupshown" && panel.state == "open") ||
       (eventType == "popuphidden" && panel.state == "closed")
     ) {
-      executeSoon(resolve);
+      executeSoon(() => resolve(panel));
       return;
     }
     panel.addEventListener(
       eventType,
       () => {
-        executeSoon(resolve);
+        executeSoon(() => resolve(panel));
       },
       { once: true }
     );
