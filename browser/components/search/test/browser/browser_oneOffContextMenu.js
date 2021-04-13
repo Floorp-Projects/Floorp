3145edafdb39654044ab8c564ba72958a43fee0c
Harry Twyford <htwyford@mozilla.com>
2021-04-13 22:36:04 +0000

    Bug 1704925 - Stop synthesizing context menu events in search mochitests. r=adw
    I'm adding #mac-reviewers for visibility on the workarounds used in this patch.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111929

--- browser/components/search/test/browser/browser_oneOffContextMenu.js
+++ browser/components/search/test/browser/browser_oneOffContextMenu.js
@@ -3,16 +3,6 @@
 const TEST_ENGINE_NAME = "Foo";
 const TEST_ENGINE_BASENAME = "testEngine.xml";
 
-const searchPopup = document.getElementById("PopupSearchAutoComplete");
-const oneOffInstance = searchPopup.oneOffButtons;
-const contextMenu = oneOffInstance.querySelector(
-  ".search-one-offs-context-menu"
-);
-const oneOffButtons = oneOffInstance.buttons;
-const searchInNewTabMenuItem = oneOffInstance.querySelector(
-  ".search-one-offs-context-open-in-new-tab"
-);
-
 let searchbar;
 let searchIcon;
 
@@ -29,6 +19,11 @@ add_task(async function init() {
 });
 
 add_task(async function telemetry() {
+  let searchPopup = document.getElementById("PopupSearchAutoComplete");
+  let oneOffInstance = searchPopup.oneOffButtons;
+
+  let oneOffButtons = oneOffInstance.buttons;
+
   // Open the popup.
   let shownPromise = promiseEvent(searchPopup, "popupshown");
   let builtPromise = promiseEvent(oneOffInstance, "rebuild");
@@ -51,6 +46,9 @@ add_task(async function telemetry() {
   );
 
   // Open the context menu on the one-off.
+  let contextMenu = oneOffInstance.querySelector(
+    ".search-one-offs-context-menu"
+  );
   let promise = BrowserTestUtils.waitForEvent(contextMenu, "popupshown");
   EventUtils.synthesizeMouseAtCenter(oneOffButton, {
     type: "contextmenu",
@@ -59,8 +57,11 @@ add_task(async function telemetry() {
   await promise;
 
   // Click the Search in New Tab menu item.
+  let searchInNewTabMenuItem = contextMenu.querySelector(
+    ".search-one-offs-context-open-in-new-tab"
+  );
   promise = BrowserTestUtils.waitForNewTab(gBrowser);
-  EventUtils.synthesizeMouseAtCenter(searchInNewTabMenuItem, {});
+  contextMenu.activateItem(searchInNewTabMenuItem);
   let tab = await promise;
 
   // By default the search will open in the background and the popup will stay open:
