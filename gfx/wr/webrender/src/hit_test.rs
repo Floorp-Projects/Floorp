f7a3cacf1df80abb18db215a129440fe07fde6e6
Dorel Luca <dluca@mozilla.com>
2021-04-14 21:38:20 +0300

    Backed out 7 changesets (bug 1675375) for Mochitest failures in gfx/layers/apz/test/mochitest/test_group_hittest-2.html. CLOSED TREE
    Backed out changeset 1a2cb51e0573 (bug 1675375)
    Backed out changeset e1ec44a75cd0 (bug 1675375)
    Backed out changeset a628a5fa5291 (bug 1675375)
    Backed out changeset 5fbf7f44c382 (bug 1675375)
    Backed out changeset 053073e36e53 (bug 1675375)
    Backed out changeset ae13d5fc65ba (bug 1675375)
    Backed out changeset 1a8ae9663eb0 (bug 1675375)

--- gfx/wr/webrender/src/hit_test.rs
+++ gfx/wr/webrender/src/hit_test.rs
@@ -6,8 +6,6 @@ use api::{BorderRadius, ClipMode, HitTestItem, HitTestResult, ItemTag, Primitive
 use api::{PipelineId, ApiHitTester, ClipId};
 use api::units::*;
 use crate::clip::{ClipItemKind, ClipStore, ClipNode, rounded_rectangle_contains_point};
-use crate::clip::{polygon_contains_point};
-use crate::prim_store::PolygonKey;
 use crate::spatial_tree::{SpatialNodeIndex, SpatialTree};
 use crate::internal_types::{FastHashMap, LayoutPrimitiveInfo};
 use std::ops;
@@ -88,12 +86,8 @@ impl HitTestClipNode {
             ClipItemKind::RoundedRectangle { rect, radius, mode } => {
                 HitTestRegion::RoundedRectangle(rect, radius, mode)
             }
-            ClipItemKind::Image { rect, polygon, .. } => {
-                if polygon.point_count > 0 {
-                    HitTestRegion::Polygon(rect, polygon)
-                } else {
-                    HitTestRegion::Rectangle(rect, ClipMode::Clip)
-                }
+            ClipItemKind::Image { rect, .. } => {
+                HitTestRegion::Rectangle(rect, ClipMode::Clip)
             }
             ClipItemKind::BoxShadow { .. } => HitTestRegion::Invalid,
         };
@@ -279,7 +273,6 @@ enum HitTestRegion {
     Invalid,
     Rectangle(LayoutRect, ClipMode),
     RoundedRectangle(LayoutRect, BorderRadius, ClipMode),
-    Polygon(LayoutRect, PolygonKey),
 }
 
 impl HitTestRegion {
@@ -293,8 +286,6 @@ impl HitTestRegion {
                 rounded_rectangle_contains_point(point, &rect, &radii),
             HitTestRegion::RoundedRectangle(rect, radii, ClipMode::ClipOut) =>
                 !rounded_rectangle_contains_point(point, &rect, &radii),
-            HitTestRegion::Polygon(rect, polygon) =>
-                polygon_contains_point(point, &rect, &polygon),
             HitTestRegion::Invalid => true,
         }
     }
