b072fdc41040ea97473f72a7a812812f24f72bff
Jon Bauman <jbauman@mozilla.com>
2021-04-14 23:36:23 +0000

    Bug 1682995 - Revert Enable AVIF support by default. r=aosmond,valentin,necko-reviewers,preferences-reviewers,Gijs
    Revert this change to turn AVIF off by default for now
    
    Differential Revision: https://phabricator.services.mozilla.com/D111892

--- devtools/client/netmonitor/test/browser_net_copy_headers.js
+++ devtools/client/netmonitor/test/browser_net_copy_headers.js
@@ -42,7 +42,7 @@ add_task(async function() {
     `${method} ${SIMPLE_URL} ${httpVersion}`,
     "Host: example.com",
     "User-Agent: " + navigator.userAgent + "",
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "Accept-Language: " + navigator.languages.join(",") + ";q=0.5",
     "Accept-Encoding: gzip, deflate",
     "Connection: keep-alive",
