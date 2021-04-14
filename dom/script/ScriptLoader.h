88fdec632fe430088fa8d8b5a6b8f150f19ea921
Jonatan Klemets <jonatan.r.klemets@gmail.com>
2021-04-14 18:04:48 +0000

    Bug 1668330 - Part 2: Modify the ScriptLoader to work with the API changes for import-assertions. r=yulia
    Differential Revision: https://phabricator.services.mozilla.com/D109495

--- dom/script/ScriptLoader.h
+++ dom/script/ScriptLoader.h
@@ -403,12 +403,12 @@ class ScriptLoader final : public nsISupports {
    *
    * @param aReferencingPrivate A JS::Value which is either undefined
    *                            or contains a LoadedScript private pointer.
-   * @param aSpecifier The module specifier.
+   * @param aModuleRequest A module request object.
    * @param aModuleOut This is set to the module found.
    */
   static void ResolveImportedModule(JSContext* aCx,
                                     JS::Handle<JS::Value> aReferencingPrivate,
-                                    JS::Handle<JSString*> aSpecifier,
+                                    JS::Handle<JSObject*> aModuleRequest,
                                     JS::MutableHandle<JSObject*> aModuleOut);
 
   void StartDynamicImport(ModuleLoadRequest* aRequest);
