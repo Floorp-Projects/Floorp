87114f3b224325d39d7537ec95e874d80332a075
Csoregi Natalia <ncsoregi@mozilla.com>
2021-04-15 22:18:09 +0300

    Backed out changeset 79c1392f7c43 (bug 1701691) for failures on browser_toolbar_prefers_color_scheme.js. CLOSED TREE

--- layout/style/test/chrome/chrome-only-media-queries.js
+++ layout/style/test/chrome/chrome-only-media-queries.js
@@ -39,6 +39,4 @@ const CHROME_ONLY_QUERIES = [
   "(-moz-os-version: windows-win7)",
   "(-moz-os-version: windows-win8)",
   "(-moz-os-version: windows-win10)",
-  "(-moz-toolbar-prefers-color-scheme: dark)",
-  "(-moz-toolbar-prefers-color-scheme: light)",
 ];
