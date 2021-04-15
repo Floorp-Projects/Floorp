17884fe1e41b3de5924926c2fc65892c87cf51e4
Thomas Wisniewski <twisniewski@mozilla.com>
2021-04-15 17:22:48 +0000

    Bug 1705452 - correctly pass in the set of hosts for which the user has opted in to bypassing shims, so that we may call allow or replace on channels appropriately; r=pbz,webcompat-reviewers,denschub
    Differential Revision: https://phabricator.services.mozilla.com/D112215

--- browser/extensions/webcompat/lib/shims.js
+++ browser/extensions/webcompat/lib/shims.js
@@ -45,7 +45,6 @@ class Shim {
     this.needsShimHelpers = opts.needsShimHelpers;
     this.platform = opts.platform || "all";
     this.unblocksOnOptIn = unblocksOnOptIn;
-    this.userHasOptedIn = false;
 
     this._hostOptIns = new Set();
 
@@ -146,7 +145,7 @@ class Shim {
     return browser.trackingProtection.allow(this.id, this.matches, {
       hosts: this.hosts,
       notHosts: this.notHosts,
-      willBeShimming: !this.userHasOptedIn,
+      hostOptIns: Array.from(this._hostOptIns),
     });
   }
 
@@ -185,8 +184,8 @@ class Shim {
       this.userHasOptedIn = true;
       const toUnblock = matches.concat(unblocksOnOptIn);
       await browser.trackingProtection.allow(this.id, toUnblock, {
-        hosts: [host],
-        willBeShimming: false,
+        hosts: this.hosts,
+        hostOptIns: [host],
       });
     }
 
@@ -241,7 +240,7 @@ class Shims {
       return;
     }
 
-    const urls = [...allShimPatterns];
+    const urls = Array.from(allShimPatterns);
     debug("Shimming these match patterns", urls);
 
     browser.webRequest.onBeforeRequest.addListener(
