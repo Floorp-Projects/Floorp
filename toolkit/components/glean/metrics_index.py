fdb0be50b6e881785a40117867a025adb811e572
Dorel Luca <dluca@mozilla.com>
2021-04-15 04:52:04 +0300

    Backed out 6 changesets (bug 1703318) for Doc failure in builds/worker/checkouts/gecko/toolkit/mozapps/update/update_metrics.yaml'. CLOSED TREE
    Backed out changeset 215ec7244a6f (bug 1703318)
    Backed out changeset 2d8b8f11b599 (bug 1703318)
    Backed out changeset c39df4a8d9ad (bug 1703318)
    Backed out changeset 4a9b2fd76c6e (bug 1703318)
    Backed out changeset 4996717d09cd (bug 1703318)
    Backed out changeset 441aa78b46f7 (bug 1703318)

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -9,7 +9,6 @@
 metrics_yamls = [
     "toolkit/components/glean/metrics.yaml",
     "toolkit/components/glean/test_metrics.yaml",
-    "toolkit/mozapps/update/update_metrics.yaml",
 ]
 
 # The list of all Glean pings.yaml files, relative to the top src dir.
@@ -17,5 +16,4 @@ metrics_yamls = [
 pings_yamls = [
     "toolkit/components/glean/pings.yaml",
     "toolkit/components/glean/test_pings.yaml",
-    "toolkit/mozapps/update/update_pings.yaml",
 ]
