ef9f0c275d98ac44ad171235c39db0c0399c0ff7
Noemi Erli <nerli@mozilla.com>
2021-04-16 04:20:52 +0300

    Bug 1704126 - Fix lint failure r=fix CLOSED TREE

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -2275,9 +2275,8 @@ class CreateMachEnvironment(MachCommandBase):
         from mozboot.util import get_mach_virtualenv_root
         from mozbuild.virtualenv import VirtualenvManager
 
-        if (
-            sys.platform.startswith("darwin")
-            and not os.environ.get("MACH_I_DO_WANT_TO_USE_ROSETTA")
+        if sys.platform.startswith("darwin") and not os.environ.get(
+            "MACH_I_DO_WANT_TO_USE_ROSETTA"
         ):
             # If running on arm64 mac, check whether we're running under
             # Rosetta and advise against it.
