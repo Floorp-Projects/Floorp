a7cfc767e61863bb0779efe65c8f6169e0dd102d
Florian Quèze <florian@queze.net>
2021-04-16 17:37:06 +0000

    Bug 1704868 - avoid triggering the notificationbar stack lazy getter for every tab close, r=mstriemer.
    Differential Revision: https://phabricator.services.mozilla.com/D111891

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -3809,7 +3809,7 @@
       }
 
       let notificationBox = this.readNotificationBox(browser);
-      notificationBox?.stack.remove();
+      notificationBox?._stack?.remove();
 
       if (aTab.linkedPanel) {
         if (!adoptedByTab && !gMultiProcessBrowser) {
