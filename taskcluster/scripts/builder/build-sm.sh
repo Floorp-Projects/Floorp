9ede1c5e813c424fee3d72edd878add080fe6590
Steve Fink <sfink@mozilla.com>
2021-04-19 14:21:43 +0000

    Bug 1697929 - Switch autospider.py to use mach for configure, builds, recurse_syms using an autogenerated mozconfig r=firefox-build-system-reviewers,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D108128

--- taskcluster/scripts/builder/build-sm.sh
+++ taskcluster/scripts/builder/build-sm.sh
@@ -30,28 +30,28 @@ if [ "$upload" = "1" ]; then
         gzip -c "$f" > "$UPLOAD_DIR/llvm-symbolizer.gz" || echo "gzip $f failed" >&2
         break
     done
-
-    # Fuzzing also uses a few fields in target.json file for automated downloads to
-    # identify what was built.
-    if [ -n "$MOZ_BUILD_DATE" ] && [ -n "$GECKO_HEAD_REV" ]; then
-        cat >$UPLOAD_DIR/target.json <<EOF
-{
-  "buildid": "$MOZ_BUILD_DATE",
-  "moz_source_stamp": "$GECKO_HEAD_REV"
-}
-EOF
-    fi
 else # !upload
-
 # Provide a note for users on why we don't include artifacts for these builds
 # by default, and how they can get the artifacts if they really need them.
-cat >$UPLOAD_DIR/README-artifacts.txt <<'EOF'
+    cat >"$UPLOAD_DIR"/README-artifacts.txt <<'EOF'
 Artifact upload has been disabled for this build due to infrequent usage of the
 generated artifacts.  If you find yourself in a position where you need the
 shell or similar artifacts from this build, please redo your push with the
 environment variable MOZ_JS_UPLOAD_BINARIES set to 1.  You can provide this as
 the option `--env MOZ_JS_UPLOAD_BINARIES=1` to `mach try fuzzy` or `mach try auto`.
 EOF
+fi
+
+# Fuzzing also uses a few fields in target.json file for automated downloads to
+# identify what was built.
+if [ -n "$MOZ_BUILD_DATE" ] && [ -n "$GECKO_HEAD_REV" ]; then
+    cat >"$UPLOAD_DIR"/target.json <<EOF
+{
+  "buildid": "$MOZ_BUILD_DATE",
+  "moz_source_stamp": "$GECKO_HEAD_REV"
+}
+EOF
+    cp "$GECKO_PATH"/mozconfig.autospider "$UPLOAD_DIR"
 fi
 
 exit $BUILD_STATUS
