c62e9527424dde47cd1cecfff3de0601231cc348
Belén Albeza <balbeza@mozilla.com>
2021-04-20 15:17:09 +0000

    Bug 1666532 - [devtools] Part 2: Add mochitest for indexedDB in fission r=ochameau
    Note: This depends on D112167. This commit adds a mochitest for the new indexedDB storage resource.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112369

--- devtools/client/storage/test/head.js
+++ devtools/client/storage/test/head.js
@@ -1103,11 +1103,19 @@ function isInTree(doc, path) {
  * @param {any} value
  */
 function checkStorageData(name, value) {
-  is(
-    gUI.table.items.get(name)?.value,
-    value,
+  ok(
+    hasStorageData(name, value),
     `Table row has an entry for: ${name} with value: ${value}`
   );
+}
+
+/**
+ * Returns whether the pair <name, value> is displayed at the data table
+ * @param {String} name
+ * @param {any} value
+ */
+function hasStorageData(name, value) {
+  return gUI.table.items.get(name)?.value === value;
 }
 
 /**
