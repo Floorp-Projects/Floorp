29f4753873e231783f3b6ab3a8ba77392d8e1dae
Belén Albeza <balbeza@mozilla.com>
2021-04-20 15:17:09 +0000

    Bug 1666532 - [devtools] Part 2: Add mochitest for indexedDB in fission r=ochameau
    Note: This depends on D112167. This commit adds a mochitest for the new indexedDB storage resource.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112369

---
+++ devtools/client/storage/test/browser_storage_fission_indexeddb.js
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* import-globals-from head.js */
+
+"use strict";
+
+add_task(async function() {
+  await pushPref("devtools.testing.enableServerWatcherSupport", true);
+
+  const URL = URL_ROOT_COM + "storage-indexeddb-iframe.html";
+
+  // open tab
+  await openTabAndSetupStorage(URL);
+  const doc = gPanelWindow.document;
+
+  // check that host appears in the storage tree
+  checkTree(doc, ["indexedDB", "http://example.com"]);
+  // check the table for values
+  await selectTreeItem([
+    "indexedDB",
+    "http://example.com",
+    "db (default)",
+    "store",
+  ]);
+  checkStorageData("foo", JSON.stringify({ key: "foo", value: "bar" }));
+
+  // check that host appears in the storage tree
+  checkTree(doc, ["indexedDB", "http://example.net"]);
+  // check the table for values
+  await selectTreeItem([
+    "indexedDB",
+    "http://example.net",
+    "db (default)",
+    "store",
+  ]);
+  checkStorageData("lorem", JSON.stringify({ key: "lorem", value: "ipsum" }));
+
+  info("Add new data to the iframe DB");
+  await SpecialPowers.spawn(gBrowser.selectedBrowser, [], async function() {
+    const iframe = content.document.querySelector("iframe");
+    return SpecialPowers.spawn(iframe, [], async function() {
+      return new Promise(resolve => {
+        const request = content.window.indexedDB.open("db", 1);
+        request.onsuccess = event => {
+          const db = event.target.result;
+          const transaction = db.transaction(["store"], "readwrite");
+          const addRequest = transaction
+            .objectStore("store")
+            .add({ key: "hello", value: "world" });
+          addRequest.onsuccess = () => resolve();
+        };
+      });
+    });
+  });
+
+  info("Refreshing table");
+  doc.querySelector("#refresh-button").click();
+
+  info("Check that table has new row");
+  await waitUntil(() =>
+    hasStorageData("hello", JSON.stringify({ key: "hello", value: "world" }))
+  );
+  ok(true, "Table has the new data");
+});
