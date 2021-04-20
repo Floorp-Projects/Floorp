f76aa176574f6c581ee243da52a6f1fe2c017156
Chris H-C <chutten@mozilla.com>
2021-04-20 15:42:06 +0000

    Bug 1685402 - FOG JS and C++ TestBeforeNextSubmit r=janerik
    Two notes:
    1) To make builds faster and hide the callback map, I've pulled the C++ impl
       into Ping.cpp.
       Doing this for the rest of the metrics is bug 1705394.
    2) Doesn't wait on the queue to drain, actually writing metric values.
       This means that the callback may be running before values reach the db.
       This is surprisingly okay! Because the only way to check metrics values is
       testGetValue which, you guessed it, waits on the queue to drain.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112220

--- toolkit/components/glean/bindings/private/Ping.cpp
+++ toolkit/components/glean/bindings/private/Ping.cpp
@@ -12,12 +12,67 @@
 
 namespace mozilla::glean {
 
+namespace impl {
+
+#ifndef MOZ_GLEAN_ANDROID
+using CallbackMapType = nsTHashMap<uint32_t, PingTestCallback>;
+using MetricIdToCallbackMutex = StaticDataMutex<UniquePtr<CallbackMapType>>;
+static MetricIdToCallbackMutex::AutoLock GetCallbackMapLock() {
+  static MetricIdToCallbackMutex sCallbacks("sCallbacks");
+  auto lock = sCallbacks.Lock();
+  if (!*lock) {
+    *lock = MakeUnique<CallbackMapType>();
+  }
+  return lock;
+}
+#endif
+
+void Ping::Submit(const nsACString& aReason) const {
+#ifdef MOZ_GLEAN_ANDROID
+  Unused << mId;
+#else
+  {
+    auto lock = GetCallbackMapLock();
+    auto callback = lock.ref()->Extract(mId);
+    if (callback) {
+      callback.extract()(aReason);
+    }
+  }
+  fog_submit_ping_by_id(mId, &aReason);
+#endif
+}
+
+void Ping::TestBeforeNextSubmit(PingTestCallback&& aCallback) const {
+#ifdef MOZ_GLEAN_ANDROID
+  return;
+#else
+  {
+    auto lock = GetCallbackMapLock();
+    lock.ref()->InsertOrUpdate(mId, aCallback);
+  }
+#endif
+}
+
+}  // namespace impl
+
 NS_IMPL_CLASSINFO(GleanPing, nullptr, 0, {0})
 NS_IMPL_ISUPPORTS_CI(GleanPing, nsIGleanPing)
 
 NS_IMETHODIMP
 GleanPing::Submit(const nsACString& aReason) {
   mPing.Submit(aReason);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+GleanPing::TestBeforeNextSubmit(nsIGleanPingTestCallback* aCallback) {
+  if (NS_WARN_IF(!aCallback)) {
+    return NS_ERROR_INVALID_ARG;
+  }
+  // Throw the bare ptr into a COM ptr to keep it around in the lambda.
+  nsCOMPtr<nsIGleanPingTestCallback> callback = aCallback;
+  mPing.TestBeforeNextSubmit(
+      [callback](const nsACString& aReason) { callback->Call(aReason); });
   return NS_OK;
 }
 
