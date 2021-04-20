b44d8fdd36b2f1304da11d383f8ad85921011f01
Ryan Hunt <rhunt@eqrion.net>
2021-04-20 14:36:30 +0000

    Bug 1703105 - wasm: Fix cranelift validation for global.set. r=lth
    Cranelift uses global.isConstant() for determining if the global is mutable or not.
    
    This isn't correct, and causes a spec-test failure when the global is an import.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111455

--- js/src/wasm/cranelift/baldrapi.h
+++ js/src/wasm/cranelift/baldrapi.h
@@ -255,6 +255,7 @@ const js::wasm::GlobalDesc* env_global(const CraneliftModuleEnvironment* env,
                                        size_t globalIndex);
 
 bool global_isConstant(const js::wasm::GlobalDesc*);
+bool global_isMutable(const js::wasm::GlobalDesc*);
 bool global_isIndirect(const js::wasm::GlobalDesc*);
 BD_ConstantValue global_constantValue(const js::wasm::GlobalDesc*);
 js::wasm::TypeCode global_type(const js::wasm::GlobalDesc*);
