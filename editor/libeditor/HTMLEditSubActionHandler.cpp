fd48bf5244ce772439eb34cf84df9f4c183f3f75
Masayuki Nakano <masayuki@d-toybox.com>
2021-04-20 00:41:24 +0000

    Bug 1627175 - part 20: Move `HTMLEditor::IsEmptyTextNode()` to `HTMLEditUtils` r=m_kato
    Now, it does not depend on `HTMLEditor`.  So, we can move it into the
    `HTMLEditUtils`.
    
    Depends on D112509
    
    Differential Revision: https://phabricator.services.mozilla.com/D112510

--- editor/libeditor/HTMLEditSubActionHandler.cpp
+++ editor/libeditor/HTMLEditSubActionHandler.cpp
@@ -6028,7 +6028,8 @@ nsresult HTMLEditor::CollectEditTargetNodes(
       for (int32_t i = aOutArrayOfContents.Length() - 1; i >= 0; i--) {
         if (Text* text = aOutArrayOfContents[i]->GetAsText()) {
           // Don't select empty text except to empty block
-          if (!IsVisibleTextNode(*text)) {
+          if (!HTMLEditUtils::IsVisibleTextNode(*text,
+                                                GetActiveEditingHost())) {
             aOutArrayOfContents.RemoveElementAt(i);
           }
         }
--- editor/libeditor/HTMLEditUtils.cpp
+++ editor/libeditor/HTMLEditUtils.cpp
@@ -341,6 +341,27 @@ bool HTMLEditUtils::SupportsAlignAttr(nsINode& aNode) {
       nsGkAtoms::h4, nsGkAtoms::h5, nsGkAtoms::h6);
 }
 
+bool HTMLEditUtils::IsVisibleTextNode(Text& aText,
+                                      Element* aEditingHost /* = nullptr */) {
+  if (!aText.TextDataLength()) {
+    return false;
+  }
+
+  if (!aText.TextIsOnlyWhitespace()) {
+    return true;
+  }
+
+  if (!aEditingHost) {
+    aEditingHost = HTMLEditUtils::
+        GetInclusiveAncestorEditableBlockElementOrInlineEditingHost(aText);
+  }
+  WSScanResult nextWSScanResult =
+      WSRunScanner::ScanNextVisibleNodeOrBlockBoundary(
+          aEditingHost, EditorRawDOMPoint(&aText, 0));
+  return nextWSScanResult.InNormalWhiteSpacesOrText() &&
+         nextWSScanResult.TextPtr() == &aText;
+}
+
 // We use bitmasks to test containment of elements. Elements are marked to be
 // in certain groups by setting the mGroup member of the `ElementInfo` struct
 // to the corresponding GROUP_ values (OR'ed together). Similarly, elements are
