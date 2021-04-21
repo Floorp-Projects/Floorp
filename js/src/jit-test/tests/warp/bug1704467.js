4cd5b1f48be242b70d7b3d35690b0a966026b477
Iain Ireland <iireland@mozilla.com>
2021-04-21 22:06:27 +0000

    Bug 1706314: Don't specialize OSR phis after pruning path to preheader r=jandem
    This bug is an extension of bug 1704467. That bug was triggered when we had a phi with one undefined operand and one OSRValue operand; this bug is triggered if we have a phi with one undefined operand and a second phi operand, which in turn has OSRValue operands. I've updated the testcase to test the broader problem.
    
    It's relatively simple to fix this, but at this point I'm tired of playing whack-a-mole with this code. All of these problems (this bug, bug 1704467, bug 1701208) happen when we try to specialize OSR phis after branch pruning has removed the path from the entry block to the preheader. The cleanest fix, which I probably should have done a while ago, is to just disable OSR phi specialization in this case. It's not clear that it's even useful: for example, in this testcase, which isn't particularly weird, we end up specializing `o1` and `o2` to null because that's the only information we have, which will cause us to immediately bail out and recompile.
    
    After disabling OSR specialization, we can also remove the extra code we added to handle this weird case.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112827

--- js/src/jit-test/tests/warp/bug1704467.js
+++ js/src/jit-test/tests/warp/bug1704467.js
@@ -1,9 +1,9 @@
-var always_true = true;
-function f() {
-    var obj = {x: 1};
+function f(a,b) {
+    var o1 = {x: 1};
+    var o2 = {x: 1};
     for (var i = 0; i < 100; i++) {
-        var res = always_true ? obj : null;
-        assertEq(res, obj);
+        var res = a ? (b ? o1 : o2) : null;
+        assertEq(res, o1);
     }
 }
-f();
+f(true, true);
