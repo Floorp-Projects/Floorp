52d4c8225c603386e2c0d452a5d27c147783b91a
Jan de Mooij <jdemooij@mozilla.com>
2021-04-21 07:53:39 +0000

    Bug 1706246 part 6 - Use ShapeProperty in more places, remove Shape::writable and Shape::configurable methods. r=jonco
    Differential Revision: https://phabricator.services.mozilla.com/D112711

--- js/src/vm/EnvironmentObject.cpp
+++ js/src/vm/EnvironmentObject.cpp
@@ -1042,14 +1042,21 @@ NamedLambdaObject* NamedLambdaObject::create(JSContext* cx,
   MOZ_ASSERT(callee->isNamedLambda());
   RootedScope scope(cx, callee->nonLazyScript()->maybeNamedLambdaScope());
   MOZ_ASSERT(scope && scope->environmentShape());
-  MOZ_ASSERT(scope->environmentShape()->slot() == lambdaSlot());
-  MOZ_ASSERT(!scope->environmentShape()->writable());
 
 #ifdef DEBUG
-  // There should be exactly one binding in the named lambda scope.
-  BindingIter bi(scope);
-  bi++;
-  MOZ_ASSERT(bi.done());
+  {
+    // Named lambda objects have one (non-writable) property.
+    ShapePropertyIter<NoGC> iter(scope->environmentShape());
+    MOZ_ASSERT(iter->slot() == lambdaSlot());
+    MOZ_ASSERT(!iter->writable());
+    iter++;
+    MOZ_ASSERT(iter.done());
+
+    // There should be exactly one binding in the named lambda scope.
+    BindingIter bi(scope);
+    bi++;
+    MOZ_ASSERT(bi.done());
+  }
 #endif
 
   BlockLexicalEnvironmentObject* obj = BlockLexicalEnvironmentObject::create(
