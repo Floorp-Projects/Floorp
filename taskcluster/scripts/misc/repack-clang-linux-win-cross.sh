7025e0461318d1cb0617d4ed65c2d92299047eb4
Mike Hommey <mh+mozilla@glandium.org>
2021-04-21 22:29:34 +0000

    Bug 1706543 - Use native zstd to compress toolchain artifacts. r=firefox-build-system-reviewers,andi,mhentges
    Now that docker images have been upgraded to Debian buster, we can
    install a native zstd binary and use tar's native support rather than
    relying on piping the output of tar to a python script that uses the
    python-zstandard module. At least we can do that for toolchain tasks
    that run on such docker images, which also means we can't on Mac and
    Windows.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112894

--- taskcluster/scripts/misc/repack-clang-linux-win-cross.sh
+++ taskcluster/scripts/misc/repack-clang-linux-win-cross.sh
@@ -11,7 +11,7 @@ cd $MOZ_FETCHES_DIR
 
 $GECKO_PATH/taskcluster/scripts/misc/zstdpy -d clang-cl/clang.tar.zst | tar -x --wildcards clang/lib/clang/*/lib/windows clang/bin/clang-cl.exe clang/bin/llvm-symbolizer.exe
 chmod +x clang/bin/clang-cl.exe
-tar -c clang | $GECKO_PATH/taskcluster/scripts/misc/zstdpy > clang.tar.zst
+tar caf clang.tar.zst clang
 
 # Put a tarball in the artifacts dir
 mkdir -p $UPLOAD_DIR
