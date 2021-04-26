866087ea8137c6979dda4f7713ea59e71f5c85f1
Daisuke Akatsuka <daisuke@birchill.co.jp>
2021-04-26 22:50:05 +0000

    Bug 1701901: Show urlbar-search-mode-indicator always upon search mode. r=adw,harry
    Differential Revision: https://phabricator.services.mozilla.com/D112901

--- browser/components/urlbar/tests/browser-proton/browser_searchMode_indicator_clickthrough.js
+++ browser/components/urlbar/tests/browser-proton/browser_searchMode_indicator_clickthrough.js
@@ -4,7 +4,7 @@
 "use strict";
 
 /**
- * Check clicking on the search mode label when the urlbar is not focused puts
+ * Check clicking on the search mode indicator when the urlbar is not focused puts
  * focus in the urlbar.
  */
 
@@ -22,26 +22,33 @@ add_task(async function test() {
     });
     const indicator = document.getElementById("urlbar-search-mode-indicator");
     Assert.ok(!BrowserTestUtils.is_visible(indicator));
+    const indicatorCloseButton = document.getElementById(
+      "urlbar-search-mode-indicator-close"
+    );
+    Assert.ok(!BrowserTestUtils.is_visible(indicatorCloseButton));
     const labelBox = document.getElementById("urlbar-label-box");
     Assert.ok(!BrowserTestUtils.is_visible(labelBox));
 
     await UrlbarTestUtils.enterSearchMode(window);
     Assert.ok(BrowserTestUtils.is_visible(indicator));
+    Assert.ok(BrowserTestUtils.is_visible(indicatorCloseButton));
     Assert.ok(!BrowserTestUtils.is_visible(labelBox));
 
     info("Blur the urlbar");
     gURLBar.blur();
-    Assert.ok(!BrowserTestUtils.is_visible(indicator));
-    Assert.ok(BrowserTestUtils.is_visible(labelBox));
+    Assert.ok(BrowserTestUtils.is_visible(indicator));
+    Assert.ok(BrowserTestUtils.is_visible(indicatorCloseButton));
+    Assert.ok(!BrowserTestUtils.is_visible(labelBox));
     Assert.notEqual(
       document.activeElement,
       gURLBar.inputField,
       "URL Bar should not be focused"
     );
 
-    info("Focus the urlbar clicking on the label box");
-    EventUtils.synthesizeMouseAtCenter(labelBox, {});
+    info("Focus the urlbar clicking on the indicator");
+    EventUtils.synthesizeMouseAtCenter(indicator, {});
     Assert.ok(BrowserTestUtils.is_visible(indicator));
+    Assert.ok(BrowserTestUtils.is_visible(indicatorCloseButton));
     Assert.ok(!BrowserTestUtils.is_visible(labelBox));
     Assert.equal(
       document.activeElement,
@@ -52,6 +59,42 @@ add_task(async function test() {
     info("Leave search mode clicking on the close button");
     await UrlbarTestUtils.exitSearchMode(window, { clickClose: true });
     Assert.ok(!BrowserTestUtils.is_visible(indicator));
+    Assert.ok(!BrowserTestUtils.is_visible(indicatorCloseButton));
     Assert.ok(!BrowserTestUtils.is_visible(labelBox));
+  });
+
+  await BrowserTestUtils.withNewTab("about:robots", async browser => {
+    await UrlbarTestUtils.promiseAutocompleteResultPopup({
+      window,
+      value: "test",
+    });
+    const indicator = document.getElementById("urlbar-search-mode-indicator");
+    Assert.ok(!BrowserTestUtils.is_visible(indicator));
+    const indicatorCloseButton = document.getElementById(
+      "urlbar-search-mode-indicator-close"
+    );
+    Assert.ok(!BrowserTestUtils.is_visible(indicatorCloseButton));
+
+    await UrlbarTestUtils.enterSearchMode(window);
+    Assert.ok(BrowserTestUtils.is_visible(indicator));
+    Assert.ok(BrowserTestUtils.is_visible(indicatorCloseButton));
+
+    info("Blur the urlbar");
+    gURLBar.blur();
+    Assert.ok(BrowserTestUtils.is_visible(indicator));
+    Assert.ok(BrowserTestUtils.is_visible(indicatorCloseButton));
+    Assert.notEqual(
+      document.activeElement,
+      gURLBar.inputField,
+      "URL Bar should not be focused"
+    );
+
+    info("Leave search mode clicking on the close button while unfocussing");
+    await UrlbarTestUtils.exitSearchMode(window, {
+      clickClose: true,
+      waitForSearch: false,
+    });
+    Assert.ok(!BrowserTestUtils.is_visible(indicator));
+    Assert.ok(!BrowserTestUtils.is_visible(indicatorCloseButton));
   });
 });
