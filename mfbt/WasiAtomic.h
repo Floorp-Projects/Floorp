bbc49db40643f953ee940174b0463670388e0c66
Chris Fallin <chris@cfallin.org>
2021-04-27 06:09:27 +0000

    Bug 1704775 part 10 - Disable unsupported c++ tests. r=glandium
    WASI doesn't support some c++ tests for threads, so
    in this patch we just stubbed them out.
    
    Differential Revision: https://phabricator.services.mozilla.com/D111815

--- mfbt/WasiAtomic.h
+++ mfbt/WasiAtomic.h
@@ -7,6 +7,8 @@
 #ifndef mozilla_WasiAtomic_h
 #define mozilla_WasiAtomic_h
 
+#include <cstdint>
+
 // WASI doesn't support <atomic> and we use it as single-threaded for now.
 // This is a stub implementation of std atomics to build WASI port of SM.
 
@@ -40,7 +42,9 @@ struct atomic {
   atomic& operator=(const atomic&) volatile = delete;
   ~atomic() noexcept = default;
 
-  T load(memory_order) const volatile noexcept { return value_; }
+  T load(memory_order m = memory_order_seq_cst) const volatile noexcept {
+    return value_;
+  }
 
   void store(T desired,
              memory_order m = memory_order_seq_cst) volatile noexcept {
@@ -177,6 +181,12 @@ struct atomic<T*> {
     return previous;
   }
 };
+
+using atomic_uint8_t = atomic<uint8_t>;
+using atomic_uint16_t = atomic<uint16_t>;
+using atomic_uint32_t = atomic<uint32_t>;
+using atomic_uint64_t = atomic<uint64_t>;
+
 }  // namespace std
 
 #endif  // mozilla_WasiAtomic_h
