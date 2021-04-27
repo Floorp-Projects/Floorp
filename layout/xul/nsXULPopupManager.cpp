a5f6c92a4bc360a018a24e7f534c69eea436740c
Stephen A Pohl <spohl.mozilla.bugs@gmail.com>
2021-04-27 20:44:37 +0000

    Bug 1707869: Introduce helper to detect when native context menus are enabled via prefs on macOS. r=mstange a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D113579

--- layout/xul/nsXULPopupManager.cpp
+++ layout/xul/nsXULPopupManager.cpp
@@ -48,9 +48,7 @@
 #include "mozilla/MouseEvents.h"
 #include "mozilla/PresShell.h"
 #include "mozilla/Services.h"
-#include "mozilla/StaticPrefs_browser.h"
 #include "mozilla/StaticPrefs_ui.h"
-#include "mozilla/StaticPrefs_widget.h"
 #include "mozilla/StaticPrefs_xul.h"
 #include "mozilla/widget/nsAutoRollup.h"
 #ifdef XP_MACOSX
@@ -741,8 +739,7 @@ void nsXULPopupManager::ShowPopup(nsIContent* aPopup,
 
 static bool ShouldUseNativeContextMenus() {
 #ifdef XP_MACOSX
-  return StaticPrefs::widget_macos_native_context_menus() &&
-         StaticPrefs::browser_proton_enabled();
+  return mozilla::widget::NativeMenuSupport::ShouldUseNativeContextMenus();
 #else
   return false;
 #endif
