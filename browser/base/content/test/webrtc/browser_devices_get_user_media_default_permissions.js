bca290cb09f52107ac8397f1f40b1fdbde301646
Csoregi Natalia <ncsoregi@mozilla.com>
2021-04-28 18:09:01 +0300

    Backed out changeset e01ec8694924 (bug 1700957) as requested. CLOSED TREE

--- browser/base/content/test/webrtc/browser_devices_get_user_media_default_permissions.js
+++ browser/base/content/test/webrtc/browser_devices_get_user_media_default_permissions.js
@@ -46,7 +46,7 @@ var gTests = [
       checkDeviceSelectors(true);
 
       // With Proton enabled, the icon does not appear in the panel.
-      if (!gProton) {
+      if (!gProtonDoorhangers) {
         let iconclass = PopupNotifications.panel.firstElementChild.getAttribute(
           "iconclass"
         );
@@ -151,7 +151,7 @@ var gTests = [
       checkDeviceSelectors(false, true);
 
       // With Proton enabled, the icon does not appear in the panel.
-      if (!gProton) {
+      if (!gProtonDoorhangers) {
         let iconclass = PopupNotifications.panel.firstElementChild.getAttribute(
           "iconclass"
         );
