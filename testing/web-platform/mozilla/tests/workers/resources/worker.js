9ea1754799581d8993f4cf5d6021040fa712835f
Eden Chuang <echuang@mozilla.com>
2021-04-28 11:09:38 +0000

    Bug 1694647 - Eliminate intermittent failures caused by imprecise timer delay testing. r=dom-worker-reviewers,asuth
    To eliminate the intermittent failures caused by testing the clamped time on the imprecise timer, this patch implements
    
    1. Relax the clamped time checking from 3ms to 2ms(But spec expected 4ms)
    2. A re-test mechanism for the case if the first clamped time is not more than 2ms.
       Repeat the clamped timeout ten 10 times, then check if the total clamped time is more than 25ms.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113298

--- testing/web-platform/mozilla/tests/workers/resources/worker.js
+++ testing/web-platform/mozilla/tests/workers/resources/worker.js
@@ -2,9 +2,12 @@ const maxNestingLevel = 5;
 let expectedNestingLevel = 1;
 let timer;
 let isInterval = false;
-let stopTimer = false;
-let lastCall = 0;
 let testStage = "ScriptLoaded";
+let stopIncreaseExpectedLevel = false;
+let startClampedTimeStamp = 0;
+let startRepeatingClamped = false;
+let repeatCount = 0;
+let maxRepeatTimes = 10;
 
 let timerCallback = async () => {
   let now = Date.now();
@@ -20,39 +23,65 @@ let timerCallback = async () => {
     return;
   }
 
-  if (!stopTimer) {
+  if (!stopIncreaseExpectedLevel) {
     if (expectedNestingLevel === maxNestingLevel) {
-      stopTimer = true;
+      stopIncreaseExpectedLevel = true;
+      startClampedTimeStamp = now;
     } else {
       expectedNestingLevel = expectedNestingLevel + 1;
     }
     if (!isInterval) {
       setTimeout(timerCallback, 0);
     }
-    lastCall = now;
     return;
   }
 
-  await Promise.resolve(true).then(() => {
-    if (WorkerTestUtils.currentTimerNestingLevel() !== expectedNestingLevel) {
-      postMessage({
-        stage: testStage,
-        status: "FAIL",
-        msg: `Timer nesting level should be in effect for immediately resolved micro-tasks`,
-      });
-    }
-  });
-  if (now - lastCall < 3) {
-    postMessage({
-      stage: testStage,
-      status: "FAIL",
-      msg: `timer nesting level reaches the max nesting level(${maxNestingLevel}), interval time should be clamped at least 3, but got ${now -
-        lastCall}`,
+  // This is the first time the timeout is clamped, checking if it is clamped
+  // to at least 2ms.
+  if (repeatCount === 0) {
+    await Promise.resolve(true).then(() => {
+      if (WorkerTestUtils.currentTimerNestingLevel() !== expectedNestingLevel) {
+        postMessage({
+          stage: testStage,
+          status: "FAIL",
+          msg: `Timer nesting level should be in effect for immediately resolved micro-tasks`,
+        });
+      }
     });
-  } else {
-    postMessage({ stage: testStage, status: "PASS", msg: "" });
+    if (now - startClampedTimeStamp < 2 ) {
+      startRepeatingClamped = true;
+    } else {
+      postMessage({ stage: testStage, status: "PASS", msg: "" });
+    }
   }
-  stopTimer = false;
+
+  // If the first clamped timeout is less than 2ms, start to repeat the clamped
+  // timeout for 10 times. Then checking if total clamped time should be at least
+  // 25ms.
+  if (startRepeatingClamped) {
+    if (repeatCount === 10) {
+      if (now - startClampedTimeStamp < 25) {
+        postMessage({
+          stage: testStage,
+          status: "FAIL",
+          msg: `total clamped time of repeating ten times should be at least 25ms(${now - startClampedTimeStamp})`,
+        });
+      } else {
+        postMessage({ stage: testStage, status: "PASS", msg: "" });
+      }
+    } else {
+      repeatCount = repeatCount + 1;
+      if (!isInterval) {
+        setTimeout(timerCallback, 0);
+      }
+      return;
+    }
+  }
+
+  // reset testing variables
+  repeatCount = 0;
+  startRepeatingClamped = false;
+  stopIncreaseExpectedLevel = false;
   if (isInterval) {
     clearInterval(timer);
   }
