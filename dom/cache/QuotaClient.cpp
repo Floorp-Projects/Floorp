88db9b66faf5a0ef1cef3794144f4ba67860b2d4
Jan Varga <jvarga@mozilla.com>
2021-04-28 07:33:54 +0000

    Bug 1690326 - Follow-up fix for the ERROR_FILE_CORRUPT to NS_ERROR_FILE_FS_CORRUPTED mapping; r=jstutte
    The code which depends on the old generic NS_ERROR_MODULE_WIN32 mapping needs
    to be updated too.
    
    Depends on D113505
    
    Differential Revision: https://phabricator.services.mozilla.com/D113518

--- dom/cache/QuotaClient.cpp
+++ dom/cache/QuotaClient.cpp
@@ -105,19 +105,15 @@ Result<UsageInfo, nsresult> GetBodyUsage(nsIFile& aMorgueDir,
         CACHE_TRY(ToResult(BodyTraverseFiles(QuotaInfo{}, *bodyDir, getUsage,
                                              /* aCanRemoveFiles */ true,
                                              /* aTrackQuota */ false))
-#ifdef WIN32
                       .orElse([](const nsresult rv) -> Result<Ok, nsresult> {
-                        // We treat ERROR_FILE_CORRUPT as if the directory did
-                        // not exist at all.
-                        if (NS_ERROR_GET_MODULE(rv) == NS_ERROR_MODULE_WIN32 &&
-                            NS_ERROR_GET_CODE(rv) == ERROR_FILE_CORRUPT) {
+                        // We treat NS_ERROR_FILE_FS_CORRUPTED as if the
+                        // directory did not exist at all.
+                        if (rv == NS_ERROR_FILE_FS_CORRUPTED) {
                           return Ok{};
                         }
 
                         return Err(rv);
-                      })
-#endif
-        );
+                      }));
         return usageInfo;
       }));
 }
--- dom/quota/QuotaCommon.cpp
+++ dom/quota/QuotaCommon.cpp
@@ -171,14 +171,10 @@ Result<nsIFileKind, nsresult> GetDirEntryKind(nsIFile& aFile) {
       }),
       ([](const nsresult rv) -> Result<nsIFileKind, nsresult> {
         if (rv == NS_ERROR_FILE_NOT_FOUND ||
-            rv == NS_ERROR_FILE_TARGET_DOES_NOT_EXIST
-#ifdef WIN32
-            // We treat ERROR_FILE_CORRUPT as if the file did not exist at
-            // all.
-            || (NS_ERROR_GET_MODULE(rv) == NS_ERROR_MODULE_WIN32 &&
-                NS_ERROR_GET_CODE(rv) == ERROR_FILE_CORRUPT)
-#endif
-        ) {
+            rv == NS_ERROR_FILE_TARGET_DOES_NOT_EXIST ||
+            // We treat NS_ERROR_FILE_FS_CORRUPTED as if the file did not exist
+            // at all.
+            rv == NS_ERROR_FILE_FS_CORRUPTED) {
           return nsIFileKind::DoesNotExist;
         }
 
