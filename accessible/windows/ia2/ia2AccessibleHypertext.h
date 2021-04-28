5c38406902365854d653cc4235471246ed1cc3b8
James Teh <jteh@mozilla.com>
2021-04-28 10:49:51 +0000

    Bug 1694865 part 11: Prepare ia2AccessibleEditableText/Hypertext/Image/Text to not rely on AccessibleWrap inheritance. r=morgan
    Rather than static_casting `this`, there is now an ImageAcc/TextAcc() method which in turn calls MsaaAccessible::LocalAcc().
    Since MsaaAccessible::LocalAcc() returns null if defunct, defunct checks have been adjusted accordingly.
    For LocalAccessibles other than `this`, rather than static_casting, GetNativeInterface is now used.
    ia2AccessibleText was calling AccessibleWrap::ConvertToIA2Attributes, but this is actually declared in ia2Accessible, so we just add the necessary include and change the call.
    Finally, calls to HyperTextProxyFor() in ia2AccessibleEditableText/Hypertext/Text assertions were replaced with calls to LocalAccessible::IsProxy(), since HyperTextProxyFor statically asserts the inheritance we're removing and isn't otherwise useful.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112942

--- accessible/windows/ia2/ia2AccessibleHypertext.h
+++ accessible/windows/ia2/ia2AccessibleHypertext.h
@@ -15,6 +15,7 @@
 
 namespace mozilla {
 namespace a11y {
+class HyperTextAccessibleWrap;
 
 class ia2AccessibleHypertext : public ia2AccessibleText,
                                public IAccessibleHypertext2 {
@@ -38,6 +39,9 @@ class ia2AccessibleHypertext : public ia2AccessibleText,
   virtual /* [propget] */ HRESULT STDMETHODCALLTYPE get_hyperlinks(
       /* [out, size_is(,*nHyperlinks)] */ IAccessibleHyperlink*** hyperlinks,
       /* [out, retval] */ long* nHyperlinks);
+
+ private:
+  HyperTextAccessibleWrap* TextAcc();
 };
 
 }  // namespace a11y
