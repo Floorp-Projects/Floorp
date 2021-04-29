e395378ba74da243b71ce7d17047184e6d31ce32
Jonathan Kew <jkew@mozilla.com>
2021-04-29 14:33:33 +0000

    Bug 739096 - Patch cairo-xlib-xrender-private.h to work around lack of full configure support. r=jrmuizel
    Differential Revision: https://phabricator.services.mozilla.com/D112583

--- gfx/cairo/cairo/src/cairo-xlib-xrender-private.h
+++ gfx/cairo/cairo/src/cairo-xlib-xrender-private.h
@@ -96,6 +96,10 @@ __attribute__((__unused__)) static void   _void_consume_free   (Display *p, XID 
 #define PictOpBlendMaximum			    0x3e
 #endif
 
+/* The mozilla build doesn't set up all the following HAVE_* symbols,
+   so we cheat by just checking the version number for now. */
+#if RENDER_MAJOR == 0 && RENDER_MINOR < 10
+
 #if !HAVE_XRENDERCREATESOLIDFILL
 #define XRenderCreateSolidFill				_int_consume
 #endif
@@ -132,6 +136,7 @@ typedef struct _XConicalGradient {
 } XConicalGradient;
 #endif
 
+#endif /* RENDER_MAJOR == 0 && RENDER_MINOR < 10 */
 
 #else /* !CAIRO_HAS_XLIB_XRENDER_SURFACE */
 
