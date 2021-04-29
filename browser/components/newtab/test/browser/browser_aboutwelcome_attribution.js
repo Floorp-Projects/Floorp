0c8a7d99264f4662f41d3916f1f65e5dad400f10
Shane Caraveo <scaraveo@mozilla.com>
2021-04-29 20:31:34 +0000

    Bug 1707038 remove strict utm_campaign validation for rtamo r=Mardak
    Differential Revision: https://phabricator.services.mozilla.com/D113672

--- browser/components/newtab/test/browser/browser_aboutwelcome_attribution.js
+++ browser/components/newtab/test/browser/browser_aboutwelcome_attribution.js
@@ -14,14 +14,15 @@ const TEST_ATTRIBUTION_DATA = {
   source: "addons.mozilla.org",
   medium: "referral",
   campaign: "non-fx-button",
-  content: "iridium@particlecore.github.io",
+  // with the sinon override, the id doesn't matter
+  content: "rta:whatever",
 };
 
 const TEST_ADDON_INFO = [
   {
     name: "Test Add-on",
     sourceURI: { scheme: "https", spec: "https://test.xpi" },
-    icons: { 32: "https://test.png", 64: "https://test.png" },
+    icons: { 32: "test.png", 64: "test.png" },
   },
 ];
 
