ae563ce6e7a782d63919ff480fb57294ed4721c3
Paul Bone <pbone@mozilla.com>
2021-04-29 01:35:43 +0000

    Bug 1707505 - Initialise NameLocation::slot_ to zero r=yulia
    This field is 24 bits wide and could not fit the previous
    ENVCOORD_SLOT_LIMIT default value (1<<24).
    
    Differential Revision: https://phabricator.services.mozilla.com/D113468

--- js/src/frontend/NameAnalysisTypes.h
+++ js/src/frontend/NameAnalysisTypes.h
@@ -267,14 +267,14 @@ class NameLocation {
   // If the name is closed over and accessed via EnvironmentCoordinate, the
   // slot on the environment.
   //
-  // Otherwise LOCALNO_LIMIT/ENVCOORD_SLOT_LIMIT.
+  // Otherwise 0.
   uint32_t slot_ : ENVCOORD_SLOT_BITS;
 
   static_assert(LOCALNO_BITS == ENVCOORD_SLOT_BITS,
                 "Frame and environment slots must be same sized.");
 
   NameLocation(Kind kind, BindingKind bindingKind, uint8_t hops = UINT8_MAX,
-               uint32_t slot = ENVCOORD_SLOT_LIMIT)
+               uint32_t slot = 0)
       : kind_(kind), bindingKind_(bindingKind), hops_(hops), slot_(slot) {}
 
  public:
