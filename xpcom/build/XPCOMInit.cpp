b65d72ca3caacd0c0c807e8342b9f9c4deecdae3
kriswright <kwright@mozilla.com>
2021-04-30 00:58:42 +0000

    Bug 1704391 - Address timers cancelled after timer shutdown. r=xpcom-reviewers,nika
    There is a chance that a timer will be cancelled by its holder after the timer thread and all of the timers have been cleaned up. Doing this can result in a hang on the mutex lock, likely because the mutex is now pointing to something else. I'm not completely familiar with the inner workings of our timers so I'm not sure what the best fix is here. I decided to follow a pattern in our threading shutdown routines and flag the timer shutdown so the timers can't attempt to cancel past a certain point where they would be shutdown anyway. This is tricky because there are timers created during XPCOM shutdown all the way up until we shutdown the timer thread. Checking for the timer thread outside of the lock could also result in a race with timer shutdown. This value is an attempt to address this without breaking timers shutdown.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113448

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -150,6 +150,8 @@ nsComponentManagerImpl* nsComponentManagerImpl::gComponentManager = nullptr;
 bool gXPCOMShuttingDown = false;
 mozilla::Atomic<bool, mozilla::SequentiallyConsistent> gXPCOMThreadsShutDown(
     false);
+mozilla::Atomic<bool, mozilla::SequentiallyConsistent> gXPCOMTimersShutDown(
+    false);
 bool gXPCOMMainThreadEventsAreDoomed = false;
 char16_t* gGREBinPath = nullptr;
 
@@ -628,6 +630,7 @@ nsresult ShutdownXPCOM(nsIServiceManager* aServMgr) {
     // Shutdown the timer thread and all timers that might still be alive before
     // shutting down the component manager
     nsTimerImpl::Shutdown();
+    gXPCOMTimersShutDown = true;
 
     NS_ProcessPendingEvents(thread);
 
