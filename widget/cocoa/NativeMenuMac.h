1148a6198f861a2d040d181dd1bd1fd4d0613693
Markus Stange <mstange.moz@gmail.com>
2021-04-30 17:45:57 +0000

    Bug 1707598 - When opening a new menu just after closing an old menu, make sure we exit the nested event loop for the old NSMenu before we ask the new NSMenu to open. r=harry
    On CI, where we open and close menu items in quick succession, we sometimes got
    into a state where the new menu was opened while we were still in the old menu's
    nested event loop. So we had the following sequence of events:
    
    ```
     - old menu +[NSMenu popUpContextMenu:withEvent:forView:]
       - nested event loop for old menu
         - old menu -[NSMenu cancelTrackingWithoutAnimation]
         - new menu +[NSMenu popUpContextMenu:withEvent:forView:]
           - nested event loop for new menu
             - new menu -[NSMenu cancelTrackingWithoutAnimation]
         - new menu's event loop is exited, but old menu's event loop remains on the
           stack
         - shutdown hang here
    ```
    
    MOZMenuOpeningCoordinator makes sure that +[NSMenu popUpContextMenu:withEvent:forView:]
    is always called in sequence, never in a nested fashion.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113373

--- widget/cocoa/NativeMenuMac.h
+++ widget/cocoa/NativeMenuMac.h
@@ -31,8 +31,8 @@ class NativeMenuMac : public NativeMenu,
   // NativeMenu
   void ShowAsContextMenu(const mozilla::DesktopPoint& aPosition) override;
   bool Close() override;
-  void ActivateItem(dom::Element* aItemElement, Modifiers aModifiers,
-                    int16_t aButton, ErrorResult& aRv) override;
+  void ActivateItem(dom::Element* aItemElement, Modifiers aModifiers, int16_t aButton,
+                    ErrorResult& aRv) override;
   void OpenSubmenu(dom::Element* aMenuElement) override;
   void CloseSubmenu(dom::Element* aMenuElement) override;
   RefPtr<dom::Element> Element() override;
@@ -68,11 +68,6 @@ class NativeMenuMac : public NativeMenu,
  protected:
   virtual ~NativeMenuMac();
 
-  // Proceed with showing the menu after a call to ShowAsContextMenu.
-  // This is done from a runnable so that the nested event loop doesn't run
-  // during ShowAsContextMenu.
-  void OpenMenu(const mozilla::DesktopPoint& aPosition);
-
   // Find the deepest nsMenuX which contains aElement, only descending into open
   // menus.
   // Returns nullptr if the element was not found or if the menus on the path
@@ -80,11 +75,14 @@ class NativeMenuMac : public NativeMenu,
   RefPtr<nsMenuX> GetOpenMenuContainingElement(dom::Element* aElement);
 
   RefPtr<dom::Element> mElement;
-  RefPtr<CancelableRunnable> mOpenRunnable;
   RefPtr<nsMenuGroupOwnerX> mMenuGroupOwner;
   RefPtr<nsMenuX> mMenu;
   nsTArray<NativeMenu::Observer*> mObservers;
   NSStatusItem* mContainerStatusBarItem;
+
+  // Non-zero after a call to ShowAsContextMenu. Stores the handle from the
+  // MOZMenuOpeningCoordinator.
+  NSInteger mOpeningHandle = 0;
 };
 
 }  // namespace widget
