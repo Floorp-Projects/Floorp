2c1ee5e54cc1388f793c3e286e0ed5268c06d55e
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-04-30 11:36:02 +0000

    Bug 1708625 - [devtools] Remove Promise.jsm usage in breadcrumb.js. r=jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D113911

--- devtools/client/inspector/breadcrumbs.js
+++ devtools/client/inspector/breadcrumbs.js
@@ -4,7 +4,6 @@
 
 "use strict";
 
-const promise = require("promise");
 const flags = require("devtools/shared/flags");
 const { ELLIPSIS } = require("devtools/shared/l10n");
 const EventEmitter = require("devtools/shared/event-emitter");
@@ -613,7 +612,7 @@ HTMLBreadcrumbs.prototype = {
     event.preventDefault();
     event.stopPropagation();
 
-    this.keyPromise = (this.keyPromise || promise.resolve(null)).then(() => {
+    this.keyPromise = (this.keyPromise || Promise.resolve(null)).then(() => {
       let currentnode;
 
       const isLeft = event.code === "ArrowLeft";
