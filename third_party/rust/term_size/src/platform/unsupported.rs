f0014e002da41886f61fd38758e356ef679bbac6
Mike Hommey <mh+mozilla@glandium.org>
2021-04-30 09:17:55 +0000

    Bug 1708634 - Upgrade term_size to 0.3.2. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D113918

---
+++ third_party/rust/term_size/src/platform/unsupported.rs
@@ -0,0 +1,4 @@
+pub fn dimensions() -> Option<(usize, usize)> { None }
+pub fn dimensions_stdout() -> Option<(usize, usize)> { None }
+pub fn dimensions_stdin() -> Option<(usize, usize)> { None }
+pub fn dimensions_stderr() -> Option<(usize, usize)> { None }
