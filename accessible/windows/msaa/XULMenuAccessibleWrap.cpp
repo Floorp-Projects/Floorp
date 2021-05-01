b248fa0492f5d68317a35d7f0be521125acee791
James Teh <jteh@mozilla.com>
2021-05-01 02:51:23 +0000

    Bug 1694865 part 15: Move XULMenuitemAccessibleWrap's MSAA overrides to a new MsaaXULMenuitemAccessible class. r=morgan
    For now, XULMenuitemAccessibleWrap inherits from MsaaXULMenuitemAccessible and MsaaXULMenuitemAccessible inherits from XULMenuitemAccessible.
    Accessible calls are made via MsaaAccessible::LocalAcc().
    Since MsaaAccessible::LocalAcc() returns null if defunct, defunct checks have been adjusted accordingly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112947

--- accessible/windows/msaa/XULMenuAccessibleWrap.cpp
+++ accessible/windows/msaa/XULMenuAccessibleWrap.cpp
@@ -3,8 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "AccessibleWrap.h"
-#include "EnumVariant.h"
 #include "XULMenuAccessibleWrap.h"
 #include "nsNameSpaceManager.h"
 
@@ -16,7 +14,7 @@ using namespace mozilla::a11y;
 
 XULMenuitemAccessibleWrap::XULMenuitemAccessibleWrap(nsIContent* aContent,
                                                      DocAccessible* aDoc)
-    : XULMenuitemAccessible(aContent, aDoc) {}
+    : MsaaXULMenuitemAccessible(aContent, aDoc) {}
 
 ENameValueFlag XULMenuitemAccessibleWrap::Name(nsString& aName) const {
   // XXX This should be done in MSAA's get_accName() so that
@@ -32,32 +30,4 @@ ENameValueFlag XULMenuitemAccessibleWrap::Name(nsString& aName) const {
   if (!accel.IsEmpty()) aName += u"\t"_ns + accel;
 
   return eNameOK;
-}
-
-STDMETHODIMP
-XULMenuitemAccessibleWrap::get_accKeyboardShortcut(
-    /* [optional][in] */ VARIANT varChild,
-    /* [retval][out] */ BSTR __RPC_FAR* pszKeyboardShortcut) {
-  if (!pszKeyboardShortcut) return E_INVALIDARG;
-  *pszKeyboardShortcut = nullptr;
-
-  if (varChild.vt != VT_I4 || varChild.lVal != CHILDID_SELF) {
-    return AccessibleWrap::get_accKeyboardShortcut(varChild,
-                                                   pszKeyboardShortcut);
-  }
-
-  if (IsDefunct()) {
-    return CO_E_OBJNOTCONNECTED;
-  }
-
-  KeyBinding keyBinding = AccessKey();
-  if (keyBinding.IsEmpty()) {
-    return S_FALSE;
-  }
-
-  nsAutoString shortcut;
-  keyBinding.ToString(shortcut);
-
-  *pszKeyboardShortcut = ::SysAllocStringLen(shortcut.get(), shortcut.Length());
-  return *pszKeyboardShortcut ? S_OK : E_OUTOFMEMORY;
 }
