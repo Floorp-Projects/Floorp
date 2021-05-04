11898b2c0089642eb2de477df48a3079ac50ef39
Iain Ireland <iireland@mozilla.com>
2021-05-04 17:51:25 +0000

    Bug 1700443: Remove dead CacheIR ops r=jandem
    The next patch removes CallFlags::FunApplyMagicArgs.
    
    Depends on D114036
    
    Differential Revision: https://phabricator.services.mozilla.com/D114169

--- js/src/jit/WarpOracle.cpp
+++ js/src/jit/WarpOracle.cpp
@@ -879,18 +879,6 @@ AbortReasonOr<Ok> WarpScriptOracle::maybeInlineIC(WarpOpSnapshotList& snapshots,
           return abort(AbortReason::Error);
         }
         break;
-      case CacheOp::GuardFrameHasNoArgumentsObject:
-        if (info_->needsArgsObj()) {
-          // The script used optimized-arguments at some point but not anymore.
-          // Don't transpile this stale Baseline IC stub.
-          [[maybe_unused]] unsigned line, column;
-          LineNumberAndColumn(script_, loc, &line, &column);
-          JitSpew(JitSpew_WarpTranspiler,
-                  "GuardFrameHasNoArgumentsObject with NeedsArgsObj @ %s:%u:%u",
-                  script_->filename(), line, column);
-          return Ok();
-        }
-        break;
       default:
         break;
     }
