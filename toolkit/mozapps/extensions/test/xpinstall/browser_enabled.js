53b48e046ea451dc027253cc858b0a043efe22c2
Tomislav Jovanovic <tomica@gmail.com>
2021-05-04 17:08:29 +0000

    Bug 1702231 - Make InstallTrigger tests work with BFcache in parent r=mixedpuppy
    With bfcacheInParent enabled, frameloader gets replaced on (each?) navigation,
    and now a ContentTask script can't listen for "load" events after navigating.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114051

--- toolkit/mozapps/extensions/test/xpinstall/browser_enabled.js
+++ toolkit/mozapps/extensions/test/xpinstall/browser_enabled.js
@@ -1,30 +1,92 @@
-// ----------------------------------------------------------------------------
+"use strict";
+
 // Test whether an InstallTrigger.enabled is working
-add_task(async function test() {
-  await BrowserTestUtils.openNewForegroundTab(gBrowser, TESTROOT);
+add_task(async function test_enabled() {
+  await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TESTROOT + "enabled.html"
+  );
 
   let text = await ContentTask.spawn(
     gBrowser.selectedBrowser,
-    TESTROOT + "enabled.html",
-    function(url) {
-      return new Promise(resolve => {
-        function page_loaded() {
-          content.removeEventListener("PageLoaded", page_loaded);
-          resolve(content.document.getElementById("enabled").textContent);
-        }
-
-        function load_listener() {
-          removeEventListener("load", load_listener, true);
-          content.addEventListener("PageLoaded", page_loaded);
-        }
-
-        addEventListener("load", load_listener, true);
-
-        content.location.href = url;
-      });
-    }
+    undefined,
+    () => content.document.getElementById("enabled").textContent
   );
 
   is(text, "true", "installTrigger should have been enabled");
   gBrowser.removeCurrentTab();
+});
+
+// Test whether an InstallTrigger.enabled is working
+add_task(async function test_disabled() {
+  Services.prefs.setBoolPref("xpinstall.enabled", false);
+  await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TESTROOT + "enabled.html"
+  );
+
+  let text = await ContentTask.spawn(
+    gBrowser.selectedBrowser,
+    undefined,
+    () => content.document.getElementById("enabled").textContent
+  );
+
+  is(text, "false", "installTrigger should have not been enabled");
+  Services.prefs.clearUserPref("xpinstall.enabled");
+  gBrowser.removeCurrentTab();
+});
+
+// Test whether an InstallTrigger.install call fails when xpinstall is disabled
+add_task(async function test_disabled2() {
+  let installDisabledCalled = false;
+
+  Harness.installDisabledCallback = installInfo => {
+    installDisabledCalled = true;
+    ok(true, "Saw installation disabled");
+  };
+
+  Harness.installBlockedCallback = installInfo => {
+    ok(false, "Should never see the blocked install notification");
+    return false;
+  };
+
+  Harness.installConfirmCallback = panel => {
+    ok(false, "Should never see an install confirmation dialog");
+    return false;
+  };
+
+  Harness.setup();
+  Services.prefs.setBoolPref("xpinstall.enabled", false);
+
+  var triggers = encodeURIComponent(
+    JSON.stringify({
+      "Unsigned XPI": TESTROOT + "amosigned.xpi",
+    })
+  );
+
+  BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    TESTROOT + "installtrigger.html?" + triggers
+  );
+
+  await BrowserTestUtils.waitForContentEvent(
+    gBrowser.selectedBrowser,
+    "InstallTriggered",
+    true,
+    undefined,
+    true
+  );
+
+  let text = await ContentTask.spawn(
+    gBrowser.selectedBrowser,
+    undefined,
+    () => content.document.getElementById("return").textContent
+  );
+
+  is(text, "false", "installTrigger should have not been enabled");
+  ok(installDisabledCalled, "installDisabled callback was called");
+
+  Services.prefs.clearUserPref("xpinstall.enabled");
+  gBrowser.removeCurrentTab();
+  Harness.finish();
 });
