063f9001b0eabb28359aae03928b668f0c5a34a2
Andrew McCreight <continuation@gmail.com>
2021-05-05 20:29:14 +0000

    Bug 1618547 - Support Fission in the process priority manager. r=kmag,gsvelto
    With Fission, there can be multiple BrowserParents in a single tab, so
    this patch moves the tracking of active tabs onto the top BrowsingContext
    in a tab. If the priority of a top BC is changed, then the activity
    of all of the BPs of the BCs in the tree are all adjusted. The flag
    that tracks this state gets carried forward to the new BC in the case
    of a cross-group navigation by the changes in ReplacedBy().
    
    The other change here covers the case where we do a process-switching
    navigation on an iframe. If we create a new BrowserParent with an active
    top BC, then the BP gets marked as active in the priority manager. Doing
    this tracking on the BP instead of the BC (both here in and in the part
    that landed previously in BrowserParent::Deactivated()) means that we
    don't need to track down every place that a BC switches processes.
    
    I left the tracking of activity in ParticularProcessPriorityManager
    centered around BrowserParents, instead of changing it to BCs, to
    minimize the changes required. There are some tricky interactions there
    with wakelocks that I didn't want to have to figure out.
    
    browser_ProcessPriorityManager.js was set up to track a mapping of
    browsers to priorities, but for the purposes of testing Fission support
    I changed it to track a mapping of child IDs to priorities.
    
    In the test, I also removed an assignment (this.window = null) that
    didn't seem to be doing anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112213

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -224,7 +224,6 @@ BrowserParent::BrowserParent(ContentParent* aManager, const TabId& aTabId,
       mRemoteTargetSetsCursor(false),
       mPreserveLayers(false),
       mRenderLayers(true),
-      mActiveInPriorityManager(false),
       mHasLayers(false),
       mHasPresented(false),
       mIsReadyToHandleInputEvents(false),
@@ -234,6 +233,14 @@ BrowserParent::BrowserParent(ContentParent* aManager, const TabId& aTabId,
   // When the input event queue is disabled, we don't need to handle the case
   // that some input events are dispatched before PBrowserConstructor.
   mIsReadyToHandleInputEvents = !ContentParent::IsInputEventQueueSupported();
+
+  // If we're in a BC tree that is active with respect to the priority manager,
+  // ensure that this new BrowserParent is marked as active. This ensures that
+  // the process will be prioritized in a cross-site iframe navigation in an
+  // active tab.
+  if (aBrowsingContext->Top()->IsPriorityActive()) {
+    ProcessPriorityManager::ActivityChanged(this, true);
+  }
 }
 
 BrowserParent::~BrowserParent() = default;
@@ -3377,13 +3384,6 @@ bool BrowserParent::GetHasLayers() { return mHasLayers; }
 bool BrowserParent::GetRenderLayers() { return mRenderLayers; }
 
 void BrowserParent::SetRenderLayers(bool aEnabled) {
-  if (mActiveInPriorityManager != aEnabled) {
-    mActiveInPriorityManager = aEnabled;
-    // Let's inform the priority manager. This operation can end up with the
-    // changing of the process priority.
-    ProcessPriorityManager::ActivityChanged(this, aEnabled);
-  }
-
   if (aEnabled == mRenderLayers) {
     if (aEnabled && mHasLayers && mPreserveLayers) {
       // RenderLayers might be called when we've been preserving layers,
@@ -3443,13 +3443,6 @@ void BrowserParent::NotifyResolutionChanged() {
     // that case.
     Unused << SendUIResolutionChanged(mDPI, mRounding,
                                       mDPI < 0 ? -1.0 : mDefaultScale.scale);
-  }
-}
-
-void BrowserParent::Deprioritize() {
-  if (mActiveInPriorityManager) {
-    ProcessPriorityManager::ActivityChanged(this, false);
-    mActiveInPriorityManager = false;
   }
 }
 
