5d670b60b68aae2e7ddf4e44a4dfdb1a80ce4a20
Nicolas Silva <nsilva@mozilla.com>
2021-05-05 09:22:48 +0000

    Bug 1709535 - Fix clear tiles. r=gfx-reviewers,jnicol
    In bug 1696905 I changed how clear tiles are rendered from multiply blend to opaque, however it breaks window controls on Windows 8 (the only thing that clear tiles are used for). This patch reverts clear tiles to the previous behavior.
    
    I'm not sure how to test this part of the code because it depends on the widget integration code having something to show behind the window.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114327

--- gfx/wr/webrender/src/composite.rs
+++ gfx/wr/webrender/src/composite.rs
@@ -590,7 +590,8 @@ impl CompositeState {
                         (CompositeTileSurface::Color { color: *color }, true)
                     }
                     TileSurface::Clear => {
-                        (CompositeTileSurface::Clear, true)
+                        // Clear tiles are rendered with blend mode pre-multiply-dest-out.
+                        (CompositeTileSurface::Clear, false)
                     }
                     TileSurface::Texture { descriptor, .. } => {
                         let surface = descriptor.resolve(resource_cache, tile_cache.current_tile_size);
