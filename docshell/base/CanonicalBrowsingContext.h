6cfd2ec8393a477ef3a231d0b17bf9ef65102825
Andrew McCreight <continuation@gmail.com>
2021-05-05 20:29:14 +0000

    Bug 1618547 - Support Fission in the process priority manager. r=kmag,gsvelto
    With Fission, there can be multiple BrowserParents in a single tab, so
    this patch moves the tracking of active tabs onto the top BrowsingContext
    in a tab. If the priority of a top BC is changed, then the activity
    of all of the BPs of the BCs in the tree are all adjusted. The flag
    that tracks this state gets carried forward to the new BC in the case
    of a cross-group navigation by the changes in ReplacedBy().
    
    The other change here covers the case where we do a process-switching
    navigation on an iframe. If we create a new BrowserParent with an active
    top BC, then the BP gets marked as active in the priority manager. Doing
    this tracking on the BP instead of the BC (both here in and in the part
    that landed previously in BrowserParent::Deactivated()) means that we
    don't need to track down every place that a BC switches processes.
    
    I left the tracking of activity in ParticularProcessPriorityManager
    centered around BrowserParents, instead of changing it to BCs, to
    minimize the changes required. There are some tricky interactions there
    with wakelocks that I didn't want to have to figure out.
    
    browser_ProcessPriorityManager.js was set up to track a mapping of
    browsers to priorities, but for the purposes of testing Fission support
    I changed it to track a mapping of child IDs to priorities.
    
    In the test, I also removed an assignment (this.window = null) that
    didn't seem to be doing anything.
    
    Differential Revision: https://phabricator.services.mozilla.com/D112213

--- docshell/base/CanonicalBrowsingContext.h
+++ docshell/base/CanonicalBrowsingContext.h
@@ -302,6 +302,17 @@ class CanonicalBrowsingContext final : public BrowsingContext {
 
   bool AllowedInBFCache(const Maybe<uint64_t>& aChannelId);
 
+  // Methods for getting and setting the active state for top level
+  // browsing contexts, for the process priority manager.
+  bool IsPriorityActive() const {
+    MOZ_RELEASE_ASSERT(IsTop());
+    return mPriorityActive;
+  }
+  void SetPriorityActive(bool aIsActive) {
+    MOZ_RELEASE_ASSERT(IsTop());
+    mPriorityActive = aIsActive;
+  }
+
  protected:
   // Called when the browsing context is being discarded.
   void CanonicalDiscard();
@@ -443,6 +454,10 @@ class CanonicalBrowsingContext final : public BrowsingContext {
   RefPtr<FeaturePolicy> mContainerFeaturePolicy;
 
   RefPtr<RestoreState> mRestoreState;
+
+  // If this is a top level context, this is true if our browser ID is marked as
+  // active in the process priority manager.
+  bool mPriorityActive = false;
 };
 
 }  // namespace dom
