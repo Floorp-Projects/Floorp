7b88bc8e6de229051cc4527764965e343cea869d
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-05-06 02:00:57 +0000

    Bug 1707590 - Part 2: Implement nsJXLDecoder r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D113359

--- image/test/gtest/TestDecoders.cpp
+++ image/test/gtest/TestDecoders.cpp
@@ -680,6 +680,7 @@ IMAGE_GTEST_DECODER_BASE_F(BMP)
 IMAGE_GTEST_DECODER_BASE_F(ICO)
 IMAGE_GTEST_DECODER_BASE_F(Icon)
 IMAGE_GTEST_DECODER_BASE_F(WebP)
+IMAGE_GTEST_DECODER_BASE_F(JXL)
 
 TEST_F(ImageDecoders, ICOWithANDMaskDownscaleDuringDecode) {
   CheckDownscaleDuringDecode(DownscaledTransparentICOWithANDMaskTestCase());
@@ -766,6 +767,10 @@ TEST_F(ImageDecoders, AVIFLargeMultiChunk) {
 
 TEST_F(ImageDecoders, AVIFDownscaleDuringDecode) {
   CheckDownscaleDuringDecode(DownscaledAVIFTestCase());
+}
+
+TEST_F(ImageDecoders, JXLLargeMultiChunk) {
+  CheckDecoderMultiChunk(LargeJXLTestCase(), /* aChunkSize */ 64);
 }
 
 TEST_F(ImageDecoders, AnimatedGIFSingleChunk) {
