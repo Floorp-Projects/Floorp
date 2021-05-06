db1bd21022157f337ecad07d61fb16ce5486bedf
Glenn Watson <git@intuitionlibrary.com>
2021-05-06 23:08:12 +0000

    Bug 1709757 - Pt 2 - Remove debugger feature from WR bindings and vendored crates r=gfx-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D114446

--- third_party/rust/image/src/math/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Mathematical helper functions and types.
-pub mod nq;
-pub mod utils;
-
-mod rect;
-pub use self::rect::Rect;
--- third_party/rust/image/src/webp/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Decoding of WebP Images
-
-pub use self::decoder::WebPDecoder;
-
-mod decoder;
-mod transform;
-
-pub mod vp8;
