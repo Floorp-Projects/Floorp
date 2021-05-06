8fb52356f49b816eadac4ce0f9fdd0dc70666f33
Emilio Cobos Álvarez <emilio@crisal.io>
2021-05-06 00:54:14 +0000

    Bug 1708303 - Fix scroll-margin implementation. r=hiro
    So that margin is not included in the rect for visibility calculations,
    and padding and margin are accounted properly on them.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113853

--- dom/ipc/BrowserBridgeChild.cpp
+++ dom/ipc/BrowserBridgeChild.cpp
@@ -217,8 +217,8 @@ mozilla::ipc::IPCResult BrowserBridgeChild::RecvScrollRectIntoView(
       aRect.ScaleToOtherAppUnitsRoundOut(aAppUnitsPerDevPixel, parentAPD);
   rect += extraOffset;
   RefPtr<PresShell> presShell = frame->PresShell();
-  presShell->ScrollFrameRectIntoView(frame, rect, aVertical, aHorizontal,
-                                     aScrollFlags);
+  presShell->ScrollFrameRectIntoView(frame, rect, nsMargin(), aVertical,
+                                     aHorizontal, aScrollFlags);
 
   return IPC_OK();
 }
