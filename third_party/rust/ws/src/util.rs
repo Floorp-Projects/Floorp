0b92f2a90df6563d9f20f3c8ca2bbc4540ecba17
Glenn Watson <git@intuitionlibrary.com>
2021-05-06 23:08:12 +0000

    Bug 1709757 - Pt 2 - Remove debugger feature from WR bindings and vendored crates r=gfx-reviewers,nical
    Differential Revision: https://phabricator.services.mozilla.com/D114446

--- third_party/rust/ws/src/util.rs
+++
@@ -1,9 +0,0 @@
-//! The util module rexports some tools from mio in order to facilitate handling timeouts.
-
-/// Used to identify some timed-out event.
-pub use mio::Token;
-/// A handle to a specific timeout.
-pub use mio_extras::timer::Timeout;
-#[cfg(any(feature = "ssl", feature = "nativetls"))]
-/// TcpStream underlying the WebSocket
-pub use mio::tcp::TcpStream;
