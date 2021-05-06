a39d1e40cbbae39cd166bdc79949fb1f549cba0e
Kagami Sascha Rosylight <krosylight@mozilla.com>
2021-05-06 01:14:21 +0000

    Bug 1707590 - Part 2: Implement nsJXLDecoder r=tnikkel
    Differential Revision: https://phabricator.services.mozilla.com/D113359

--- netwerk/mime/nsMimeTypes.h
+++ netwerk/mime/nsMimeTypes.h
@@ -159,6 +159,7 @@
 #define IMAGE_SVG_XML "image/svg+xml"
 #define IMAGE_WEBP "image/webp"
 #define IMAGE_AVIF "image/avif"
+#define IMAGE_JXL "image/jxl"
 
 #define MESSAGE_EXTERNAL_BODY "message/external-body"
 #define MESSAGE_NEWS "message/news"
