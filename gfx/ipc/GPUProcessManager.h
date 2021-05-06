f30668d2889074ee95b0f6aee24572abf8aacd57
Andrew Osmond <aosmond@mozilla.com>
2021-05-06 19:47:38 +0000

    Bug 1707610 - Make GPU process restart criteria depend on rendering frames and uptime. r=jrmuizel
    Originally, we would restart the GPU process a fixed number of attempts
    based on the layers.gpu-process.max_restarts pref. With this patch, we
    now use this pref to control how many "unstable" restarts we allow. A
    restart is "stable" if and only if the process uptime exceeds the pref
    layers.gpu-process.stable.min-uptime-ts and if the process renders a
    total number of frames exceeding the pref
    layers.gpu-process.stable.frame-threshold. This allows users to keep the
    GPU process for a lot longer if they are encountering infrequent
    crashes. Should the user experience the GPU process crashing quickly
    and/or without rendering many frames, we will disable it as before after
    a few attempts and move into the parent process.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114531

--- gfx/ipc/GPUProcessManager.h
+++ gfx/ipc/GPUProcessManager.h
@@ -159,6 +159,7 @@ class GPUProcessManager final : public GPUProcessHost::Listener {
   void NotifyWebRenderError(wr::WebRenderError aError);
   void OnInProcessDeviceReset(bool aTrackThreshold);
   void OnRemoteProcessDeviceReset(GPUProcessHost* aHost) override;
+  void OnProcessDeclaredStable() override;
   void NotifyListenersOnCompositeDeviceReset();
 
   // Notify the GPUProcessManager that a top-level PGPU protocol has been
@@ -186,7 +187,7 @@ class GPUProcessManager final : public GPUProcessHost::Listener {
   GPUChild* GetGPUChild() { return mGPUChild; }
 
   // Returns whether or not a GPU process was ever launched.
-  bool AttemptedGPUProcess() const { return mNumProcessAttempts > 0; }
+  bool AttemptedGPUProcess() const { return mTotalProcessAttempts > 0; }
 
   // Returns the process host
   GPUProcessHost* Process() { return mProcess; }
@@ -293,7 +294,10 @@ class GPUProcessManager final : public GPUProcessHost::Listener {
   uint32_t mNextNamespace;
   uint32_t mIdNamespace;
   uint32_t mResourceId;
-  uint32_t mNumProcessAttempts;
+
+  uint32_t mUnstableProcessAttempts;
+  uint32_t mTotalProcessAttempts;
+  TimeStamp mProcessAttemptLastTime;
 
   nsTArray<RefPtr<RemoteCompositorSession>> mRemoteSessions;
   nsTArray<RefPtr<InProcessCompositorSession>> mInProcessSessions;
@@ -305,6 +309,7 @@ class GPUProcessManager final : public GPUProcessHost::Listener {
   // Fields that are associated with the current GPU process.
   GPUProcessHost* mProcess;
   uint64_t mProcessToken;
+  bool mProcessStable;
   GPUChild* mGPUChild;
   RefPtr<VsyncBridgeChild> mVsyncBridge;
   // Collects any pref changes that occur during process launch (after
