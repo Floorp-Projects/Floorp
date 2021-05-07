1d41a2905923343a1c390232d027b78d44100399
Mike Conley <mconley@mozilla.com>
2021-05-07 14:15:40 +0000

    Bug 1708809 - Make sure the high priority notification box is contained within an element using the CSS box model. r=Gijs a=pascalc
    Mixing the XUL and CSS box models is always a source of interesting glitches like
    the one that this patch tries to fix. The animation for closing a Proton-y infobar
    involves setting a negative margin on an element using CSS box, which has a parent
    and siblings that are using XUL box.
    
    Interestingly, it seems that only the high priority notification box hits this issue,
    as the per-tab notification box is contained within a <named-deck>, which uses CSS
    box - this observation was what led me to the solution in this patch: when constructing
    the high priority notification box, we now place it inside of a <div>, which uses
    CSS box, which allows us to sidestep this glitch.
    
    The irony of fixing this CSS vs XUL box glitch by adding another CSS box amongst a
    bunch of XUL boxes is not lost on me.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114552

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -401,7 +401,14 @@ XPCOMUtils.defineLazyGetter(this, "gHighPriorityNotificationBox", () => {
     if (gProton) {
       // With Proton enabled all notification boxes are at the top, built into the browser chrome.
       let tabNotifications = document.getElementById("tab-notification-deck");
-      gNavToolbox.insertBefore(element, tabNotifications);
+      // With Proton enabled, notification messages use the CSS box model. When using
+      // negative margins on those notification messages to animate them in or out,
+      // if the ancestry of that node is all using the XUL box model, strange glitches
+      // arise. We sidestep this by containing the global notification box within a
+      // <div> that has CSS block layout.
+      let outer = document.createElement("div");
+      outer.appendChild(element);
+      gNavToolbox.insertBefore(outer, tabNotifications);
     } else {
       document.getElementById("appcontent").prepend(element);
     }
