50209d103e0e1bdb64defb734994e01c3332576c
Philip Jägenstedt <philip@foolip.org>
2021-05-07 19:53:22 +0000

    Bug 1707437 [wpt PR 28677] - Remove outdated copyright notices / license claims, a=testonly
    Automatic update from web-platform-tests
    Remove outdated copyright notices / license claims (#28677)
    
    This removes most references to the W3C Test Suite License. In
    https://github.com/web-platform-tests/wpt/pull/11191, everything
    originally contributed under that license was relicensed to the 3-clause
    BSD license in LICENSE.md.
    
    All the modified files existed at the time at their current paths, with
    the exception of html/canvas/tools/ which was then 2dcontext/tools/, but
    the same files were there.
    --
    
    wpt-commits: df24fb604e2d40528ac1d1b5dd970e32fc5c2978
    wpt-pr: 28677

--- testing/web-platform/tests/resources/testharness.js
+++ testing/web-platform/tests/resources/testharness.js
@@ -1,14 +1,5 @@
 /*global self*/
 /*jshint latedef: nofunc*/
-/*
-Distributed under both the W3C Test Suite License [1] and the W3C
-3-clause BSD License [2]. To contribute to a W3C Test Suite, see the
-policies and contribution forms [3].
-
-[1] http://www.w3.org/Consortium/Legal/2008/04-testsuite-license
-[2] http://www.w3.org/Consortium/Legal/2008/03-bsd-license
-[3] http://www.w3.org/2004/10/27-testcases
-*/
 
 /* Documentation: https://web-platform-tests.org/writing-tests/testharness-api.html
  * (../docs/_writing-tests/testharness-api.md) */
