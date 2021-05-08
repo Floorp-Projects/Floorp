eabf64188b2b3e25129e7a697d82c2beca5cb4a4
Philip Jägenstedt <philip@foolip.org>
2021-05-08 10:04:04 +0000

    Bug 1710007 [wpt PR 28890] - Replace io.open() with just open(), a=testonly
    Automatic update from web-platform-tests
    Replace io.open() with just open() (#28890)
    
    In Python 3, io.open() is an alias for the builtin open() function:
    https://docs.python.org/3/library/io.html#io.open
    
    Part of https://github.com/web-platform-tests/wpt/issues/28776.
    --
    
    wpt-commits: fea1d6a355f790a0ab0415026ff5efd9f7310b80
    wpt-pr: 28890

--- testing/web-platform/tests/tools/ci/tc/tests/test_valid.py
+++ testing/web-platform/tests/tools/ci/tc/tests/test_valid.py
@@ -1,6 +1,5 @@
 import json
 import os
-from io import open
 
 import jsone
 import mock
--- testing/web-platform/tests/tools/wave/configuration_loader.py
+++ testing/web-platform/tests/tools/wave/configuration_loader.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 import json
 import os
-from io import open
 
 from tools.wpt import wpt
 
