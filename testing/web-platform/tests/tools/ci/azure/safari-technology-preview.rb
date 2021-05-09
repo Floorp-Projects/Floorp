600c5382a7c8922cf73a1760fc1fabbccafc4d23
autofoolip <auto@foolip.org>
2021-05-09 08:10:59 +0000

    Bug 1710192 [wpt PR 28912] - Update Safari Technology Preview to 124, a=testonly
    Automatic update from web-platform-tests
    Update Safari Technology Preview to 124 (#28912)
    
    --
    
    wpt-commits: e9ca5ec1d77efb6ea4324e7e1b4acd5285f15c59
    wpt-pr: 28912

--- testing/web-platform/tests/tools/ci/azure/safari-technology-preview.rb
+++ testing/web-platform/tests/tools/ci/azure/safari-technology-preview.rb
@@ -1,10 +1,10 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :catalina
-    version "123,071-23564-20210330-f6b89a7f-4800-4af7-93cb-0dbb005db0e8"
-    sha256 "16e7ed118cd3e839d48c7c68827c59fd2f9d2d3341514d7b301fb5d8c30d55fb"
+    version "124,071-27612-20210507-96cb5150-2454-422d-b3be-d9253bbf746c"
+    sha256 "3c7958aa9a61d1af323ef9c004301b37e27241ace465d98ccd53b34ea282d422"
   else
-    version "123,071-23628-20210330-8224de12-9cd5-4c33-b944-149b5f4cd543"
-    sha256 "9d1319f78bc27cf6163691f61b70f9457a59a125b9adfb44253ac7a597a87767"
+    version "124,071-40182-20210507-d07bb1e1-ffd5-4fc9-ab00-64fc3ca17bcd"
+    sha256 "d69043deee786a304aea2dfaffcce5f9a5f0dd3e77ba427deba47db306241616"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
