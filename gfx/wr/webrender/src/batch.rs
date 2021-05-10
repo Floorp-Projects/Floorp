ef8b8f114147bbe692f879ef06a861a2fe5a20d3
Dzmitry Malyshau <dmalyshau@mozilla.com>
2021-05-10 18:53:53 +0000

    Bug 1709440 - Fix the space mapping for WR glyphs r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D114551

--- gfx/wr/webrender/src/batch.rs
+++ gfx/wr/webrender/src/batch.rs
@@ -1373,14 +1373,17 @@ impl BatchBuilder {
                                     device_bounding_rect = device_bounding_rect.union(&device_glyph_rect);
                                 }
 
-                                let map_device_to_surface: SpaceMapper<DevicePixel, PicturePixel> = SpaceMapper::new_with_target(
-                                    surface_spatial_node_index,
+                                let map_device_to_surface: SpaceMapper<PicturePixel, DevicePixel> = SpaceMapper::new_with_target(
                                     root_spatial_node_index,
-                                    *bounding_rect,
+                                    surface_spatial_node_index,
+                                    device_bounding_rect,
                                     ctx.spatial_tree,
                                 );
 
-                                map_device_to_surface.map(&device_bounding_rect)
+                                match map_device_to_surface.unmap(&device_bounding_rect) {
+                                    Some(r) => r.intersection(&bounding_rect),
+                                    None => Some(*bounding_rect),
+                                }
                             } else {
                                 let mut local_bounding_rect = LayoutRect::default();
 
