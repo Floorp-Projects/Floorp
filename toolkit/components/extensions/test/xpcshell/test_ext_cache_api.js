32b5ea00ee9453a234b99a72302ef857febbd05b
Luca Greco <lgreco@mozilla.com>
2021-05-10 18:23:38 +0000

    Bug 1710138 - Add explicit test coverage for the Cache webAPI called from a moz-extension:// shared worker. r=mixedpuppy
    Differential Revision: https://phabricator.services.mozilla.com/D114761

--- toolkit/components/extensions/test/xpcshell/test_ext_cache_api.js
+++ toolkit/components/extensions/test/xpcshell/test_ext_cache_api.js
@@ -80,6 +80,115 @@ add_task(async function test_cache_api_http_resource_allowed() {
   await extension.unload();
 });
 
+// This test is similar to `test_cache_api_http_resource_allowed` but it does
+// exercise the Cache API from a moz-extension shared worker.
+// We expect the cache API calls to be successfull when it is being used to
+// cache an HTTP url that is allowed for the extensions based on its host
+// permission, but to fail if the extension doesn't have the required host
+// permission to fetch data from that url.
+add_task(async function test_cache_api_from_ext_shared_worker() {
+  if (!WebExtensionPolicy.useRemoteWebExtensions) {
+    // Ensure RemoteWorkerService has been initialized in the main
+    // process.
+    Services.obs.notifyObservers(null, "profile-after-change");
+  }
+
+  const background = async function() {
+    const BASE_URL_OK = `http://example.com/dummy`;
+    const BASE_URL_KO = `http://anotherdomain.com/dummy`;
+    const worker = new SharedWorker("worker.js");
+    const { data: resultOK } = await new Promise(resolve => {
+      worker.port.onmessage = resolve;
+      worker.port.postMessage(["worker-cacheapi-test-allowed", BASE_URL_OK]);
+    });
+    browser.test.log(
+      `Got result from extension worker for allowed host url: ${JSON.stringify(
+        resultOK
+      )}`
+    );
+    const { data: resultKO } = await new Promise(resolve => {
+      worker.port.onmessage = resolve;
+      worker.port.postMessage(["worker-cacheapi-test-disallowed", BASE_URL_KO]);
+    });
+    browser.test.log(
+      `Got result from extension worker for disallowed host url: ${JSON.stringify(
+        resultKO
+      )}`
+    );
+
+    browser.test.assertTrue(
+      await window.caches.has("test-cache-api"),
+      "CacheStorage.has should resolve to true"
+    );
+    const cache = await window.caches.open("test-cache-api");
+    browser.test.assertEq(
+      "test_ext_cache_api.js",
+      await cache.match(BASE_URL_OK).then(res => res.text()),
+      "Got the expected content from the cached http url"
+    );
+    browser.test.assertEq(
+      true,
+      await cache.match(BASE_URL_KO).then(res => res == undefined),
+      "Got no match for the http url that isn't allowed by host permissions"
+    );
+
+    browser.test.sendMessage("test-cacheapi-sharedworker:done", {
+      expectAllowed: resultOK,
+      expectDisallowed: resultKO,
+    });
+  };
+
+  const extension = ExtensionTestUtils.loadExtension({
+    background,
+    manifest: { permissions: ["http://example.com/*"] },
+    files: {
+      "worker.js": function() {
+        self.onconnect = evt => {
+          const port = evt.ports[0];
+          port.onmessage = async evt => {
+            let result = {};
+            let message;
+            try {
+              const [msg, BASE_URL] = evt.data;
+              message = msg;
+              const cache = await self.caches.open("test-cache-api");
+              await cache.add(BASE_URL);
+              result.success = true;
+            } catch (err) {
+              result.error = err.message;
+              throw err;
+            } finally {
+              port.postMessage([`${message}:result`, result]);
+            }
+          };
+        };
+      },
+    },
+  });
+
+  await extension.startup();
+  const { expectAllowed, expectDisallowed } = await extension.awaitMessage(
+    "test-cacheapi-sharedworker:done"
+  );
+  // Increase the chance to have the error message related to an unexpected
+  // failure to be explicitly mention in the failure message.
+  Assert.deepEqual(
+    expectAllowed,
+    ["worker-cacheapi-test-allowed:result", { success: true }],
+    "Expect worker result to be successfull with the required host permission"
+  );
+  Assert.deepEqual(
+    expectDisallowed,
+    [
+      "worker-cacheapi-test-disallowed:result",
+      { error: "NetworkError when attempting to fetch resource." },
+    ],
+    "Expect worker result to be unsuccessfull without the required host permission"
+  );
+
+  await extension.unload();
+});
+
 add_task(async function test_cache_storage_evicted_on_addon_uninstalled() {
   async function background() {
     try {
