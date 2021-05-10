5031f7bcde4763b7c0786f9ee5e13af968a3c71c
Emilio Cobos Álvarez <emilio@crisal.io>
2021-05-10 23:39:04 +0000

    Bug 1709622 - Remove no-op FLAG_WANT_DATA_SURFACE. r=tnikkel,jgilbert
    Depends on D114685
    
    Differential Revision: https://phabricator.services.mozilla.com/D114686

--- dom/canvas/ImageBitmap.cpp
+++ dom/canvas/ImageBitmap.cpp
@@ -1564,9 +1564,8 @@ CreateImageBitmapFromBlob::OnImageReady(imgIContainer* aImgContainer,
   MOZ_ASSERT(aImgContainer);
 
   // Get the surface out.
-  uint32_t frameFlags = imgIContainer::FLAG_SYNC_DECODE |
-                        imgIContainer::FLAG_ASYNC_NOTIFY |
-                        imgIContainer::FLAG_WANT_DATA_SURFACE;
+  uint32_t frameFlags =
+      imgIContainer::FLAG_SYNC_DECODE | imgIContainer::FLAG_ASYNC_NOTIFY;
   uint32_t whichFrame = imgIContainer::FRAME_FIRST;
   RefPtr<SourceSurface> surface =
       aImgContainer->GetFrame(whichFrame, frameFlags);
