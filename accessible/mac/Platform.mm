27ac758192b1222a1fc7a85e3208d1faf5f51a1c
Morgan Reschenberg <mreschenberg@mozilla.com>
2021-05-10 20:22:32 +0000

    Bug 1708522: Remove MOZ_CRASHREPORTER from windows, log AccClient on mac. r=Jamie
    This commit adds crash logging for AccessibilityClient on MacOS and
    enables that logging on windows by removing the defunct MOZ_CRASHREPORTER
    defines. On MacOS, we also introduce logging for SwitchControl and
    unknown clients.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114167

--- accessible/mac/Platform.mm
+++ accessible/mac/Platform.mm
@@ -22,6 +22,7 @@
 // Available from 10.13 onwards; test availability at runtime before using
 @interface NSWorkspace (AvailableSinceHighSierra)
 @property(readonly) BOOL isVoiceOverEnabled;
+@property(readonly) BOOL isSwitchControlEnabled;
 @end
 
 namespace mozilla {
@@ -198,14 +199,28 @@ void ProxyRoleChangedEvent(RemoteAccessible* aTarget, const a11y::role& aRole) {
 - (void)accessibilitySetValue:(id)value forAttribute:(NSString*)attribute {
   if ([attribute isEqualToString:@"AXEnhancedUserInterface"]) {
     mozilla::a11y::sA11yShouldBeEnabled = ([value intValue] == 1);
+    if (sA11yShouldBeEnabled) {
+      // If accessibility should be enabled, log the appropriate client
+      nsAutoString client;
+      if ([[NSWorkspace sharedWorkspace]
+              respondsToSelector:@selector(isVoiceOverEnabled)] &&
+          [[NSWorkspace sharedWorkspace] isVoiceOverEnabled]) {
+        client.Assign(u"VoiceOver"_ns);
+      } else if ([[NSWorkspace sharedWorkspace]
+                     respondsToSelector:@selector(isSwitchControlEnabled)] &&
+                 [[NSWorkspace sharedWorkspace] isSwitchControlEnabled]) {
+        client.Assign(u"SwitchControl"_ns);
+      } else {
+        client.Assign(u"Unknown"_ns);
+      }
+
 #if defined(MOZ_TELEMETRY_REPORTING)
-    if ([[NSWorkspace sharedWorkspace]
-            respondsToSelector:@selector(isVoiceOverEnabled)] &&
-        [[NSWorkspace sharedWorkspace] isVoiceOverEnabled]) {
-      Telemetry::ScalarSet(Telemetry::ScalarID::A11Y_INSTANTIATORS,
-                           u"VoiceOver"_ns);
-    }
+      Telemetry::ScalarSet(Telemetry::ScalarID::A11Y_INSTANTIATORS, client);
 #endif  // defined(MOZ_TELEMETRY_REPORTING)
+      CrashReporter::AnnotateCrashReport(
+          CrashReporter::Annotation::AccessibilityClient,
+          NS_ConvertUTF16toUTF8(client));
+    }
   }
 
   return [super accessibilitySetValue:value forAttribute:attribute];
