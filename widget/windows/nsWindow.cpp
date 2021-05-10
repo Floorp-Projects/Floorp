2474d8dbf7da7217167915842ee573f87b023679
sotaro <sotaro.ikeda.g@gmail.com>
2021-05-10 08:28:16 +0000

    Bug 1710335 - Add gfxCriticalNoteOnce to nsWindow::Resize() on Windows r=nical,gfx-reviewers
    When Bug 1701451 happened, Negative height was passed to nsWindow::Resize(). It is helpful to call gfxCriticalNoteOnce when it happens.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114704

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -2097,6 +2097,10 @@ void nsWindow::Resize(double aWidth, double aHeight, bool aRepaint) {
 
   NS_ASSERTION((width >= 0), "Negative width passed to nsWindow::Resize");
   NS_ASSERTION((height >= 0), "Negative height passed to nsWindow::Resize");
+  if (width < 0 || height < 0) {
+    gfxCriticalNoteOnce << "Negative passed to Resize(" << width << ", "
+                        << height << ") repaint: " << aRepaint;
+  }
 
   ConstrainSize(&width, &height);
 
@@ -2167,6 +2171,11 @@ void nsWindow::Resize(double aX, double aY, double aWidth, double aHeight,
 
   NS_ASSERTION((width >= 0), "Negative width passed to nsWindow::Resize");
   NS_ASSERTION((height >= 0), "Negative height passed to nsWindow::Resize");
+  if (width < 0 || height < 0) {
+    gfxCriticalNoteOnce << "Negative passed to Resize(" << x << " ," << y
+                        << ", " << width << ", " << height
+                        << ") repaint: " << aRepaint;
+  }
 
   ConstrainSize(&width, &height);
 
