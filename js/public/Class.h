0cc8e359eab05a24bb62a6d42e731ea9a7c8652a
Jan de Mooij <jdemooij@mozilla.com>
2021-05-10 11:50:20 +0000

    Bug 1710075 part 3 - Cache array shape in a global object slot. r=jonco
    This will be used by a later patch in the stack, but can likely be used in more
    places in the future.
    
    Depends on D114604
    
    Differential Revision: https://phabricator.services.mozilla.com/D114605

--- js/public/Class.h
+++ js/public/Class.h
@@ -572,7 +572,7 @@ static const uint32_t JSCLASS_FOREGROUND_FINALIZE =
 // application.
 static const uint32_t JSCLASS_GLOBAL_APPLICATION_SLOTS = 5;
 static const uint32_t JSCLASS_GLOBAL_SLOT_COUNT =
-    JSCLASS_GLOBAL_APPLICATION_SLOTS + JSProto_LIMIT * 2 + 30;
+    JSCLASS_GLOBAL_APPLICATION_SLOTS + JSProto_LIMIT * 2 + 31;
 
 static constexpr uint32_t JSCLASS_GLOBAL_FLAGS_WITH_SLOTS(uint32_t n) {
   return JSCLASS_IS_GLOBAL |
