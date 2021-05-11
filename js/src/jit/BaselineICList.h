5ef7434f3789e122883747af35d62e839f236127
Jan de Mooij <jdemooij@mozilla.com>
2021-05-11 06:21:56 +0000

    Bug 1710075 part 8 - Remove classes and kinds for fallback stubs. r=iain
    Each fallback stub is now allocated as just ICFallbackStub.
    
    Also replaces the switch-statement in initICEntries with a faster table lookup
    to map from JSOp to fallback kind (to get the trampoline code for the fallback stub).
    
    Differential Revision: https://phabricator.services.mozilla.com/D114610

--- js/src/jit/BaselineICList.h
+++ js/src/jit/BaselineICList.h
@@ -10,56 +10,8 @@
 namespace js {
 namespace jit {
 
-// List of Baseline IC fallback stub kinds. The stub kind determines which
-// class (deriving from ICFallbackStub) this is.
-#define IC_BASELINE_STUB_KIND_LIST(_) \
-  _(NewArray_Fallback)                \
-  _(NewObject_Fallback)               \
-                                      \
-  _(ToBool_Fallback)                  \
-                                      \
-  _(UnaryArith_Fallback)              \
-                                      \
-  _(Call_Fallback)                    \
-                                      \
-  _(GetElem_Fallback)                 \
-  _(SetElem_Fallback)                 \
-                                      \
-  _(In_Fallback)                      \
-  _(HasOwn_Fallback)                  \
-  _(CheckPrivateField_Fallback)       \
-                                      \
-  _(GetName_Fallback)                 \
-                                      \
-  _(BindName_Fallback)                \
-                                      \
-  _(GetIntrinsic_Fallback)            \
-                                      \
-  _(SetProp_Fallback)                 \
-                                      \
-  _(GetIterator_Fallback)             \
-                                      \
-  _(OptimizeSpreadCall_Fallback)      \
-                                      \
-  _(InstanceOf_Fallback)              \
-                                      \
-  _(TypeOf_Fallback)                  \
-                                      \
-  _(ToPropertyKey_Fallback)           \
-                                      \
-  _(Rest_Fallback)                    \
-                                      \
-  _(BinaryArith_Fallback)             \
-                                      \
-  _(Compare_Fallback)                 \
-                                      \
-  _(GetProp_Fallback)
-
-// List of fallback trampolines. Each of these fallback trampolines exists as
-// part of the JitRuntime. Note that some fallback stubs in previous list may
-// have multiple trampolines in this list. For example, Call_Fallback has
-// constructing/spread variants here with different calling conventions needing
-// different trampolines.
+// List of trampolines for Baseline IC fallback stubs. Trampoline code is
+// allocated as part of the JitRuntime.
 #define IC_BASELINE_FALLBACK_CODE_KIND_LIST(_) \
   _(NewArray)                                  \
   _(NewObject)                                 \
