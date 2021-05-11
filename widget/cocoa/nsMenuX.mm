d8067752b61d38f218422f9d180db5b8e3341094
Markus Stange <mstange.moz@gmail.com>
2021-05-11 20:34:17 +0000

    Bug 1710507 - Make native menus respect the XUL element's directionality. r=emilio
    This affects both native context menus and menubar menus.
    
    Doing a flush here is fine, the nsMenuX constructor already flushes via SetupIcon()
    (which also uses nsComputedDOMStyle, in nsMenuItemIconX::GetIconURI).
    
    Differential Revision: https://phabricator.services.mozilla.com/D114785

--- widget/cocoa/nsMenuX.mm
+++ widget/cocoa/nsMenuX.mm
@@ -20,6 +20,7 @@
 
 #include "nsObjCExceptions.h"
 
+#include "nsComputedDOMStyle.h"
 #include "nsThreadUtils.h"
 #include "nsToolkit.h"
 #include "nsCocoaUtils.h"
@@ -704,6 +705,23 @@ void nsMenuX::OnWillActivateItem(NSMenuItem* aItem) {
   }
 }
 
+// Flushes style.
+static NSUserInterfaceLayoutDirection DirectionForElement(dom::Element* aElement) {
+  // Get the direction from the computed style so that inheritance into submenus is respected.
+  // aElement may not have a frame.
+  RefPtr<ComputedStyle> sc = nsComputedDOMStyle::GetComputedStyle(aElement, nullptr);
+  if (!sc) {
+    return NSApp.userInterfaceLayoutDirection;
+  }
+
+  switch (sc->StyleVisibility()->mDirection) {
+    case StyleDirection::Ltr:
+      return NSUserInterfaceLayoutDirectionLeftToRight;
+    case StyleDirection::Rtl:
+      return NSUserInterfaceLayoutDirectionRightToLeft;
+  }
+}
+
 void nsMenuX::RebuildMenu() {
   MOZ_RELEASE_ASSERT(mNeedsRebuild);
   gConstructingMenu = true;
@@ -713,6 +731,10 @@ void nsMenuX::RebuildMenu() {
   if (!menuPopup) {
     gConstructingMenu = false;
     return;
+  }
+
+  if (menuPopup->IsElement()) {
+    mNativeMenu.userInterfaceLayoutDirection = DirectionForElement(menuPopup->AsElement());
   }
 
   // Iterate over the kids
