ad678cf07647a339226a32e051dc087308196f87
Nika Layzell <nika@thelayzells.com>
2021-05-11 17:29:25 +0000

    Bug 1708734 - Part 3: Add tests for createActor: false, r=kmag
    Differential Revision: https://phabricator.services.mozilla.com/D114793

--- dom/ipc/tests/JSWindowActor/browser_event_listener.js
+++ dom/ipc/tests/JSWindowActor/browser_event_listener.js
@@ -2,33 +2,77 @@
    http://creativecommons.org/publicdomain/zero/1.0/ */
 "use strict";
 
+async function createAndShowDropdown(browser) {
+  // Add a select element to the DOM of the loaded document.
+  await SpecialPowers.spawn(browser, [], async function() {
+    content.document.body.innerHTML += `
+      <select id="testSelect">
+        <option>A</option>
+        <option>B</option>
+      </select>`;
+  });
+
+  // Click on the select to show the dropdown.
+  await BrowserTestUtils.synthesizeMouseAtCenter("#testSelect", {}, browser);
+}
+
 declTest("test event triggering actor creation", {
+  events: { mozshowdropdown: {} },
+
   async test(browser) {
-    // Add a select element to the DOM of the loaded document.
-    await SpecialPowers.spawn(browser, [], async function() {
-      content.document.body.innerHTML += `
-        <select id="testSelect">
-          <option>A</option>
-          <option>B</option>
-        </select>`;
-    });
-
     // Wait for the observer notification.
-    let observePromise = new Promise(resolve => {
-      const TOPIC = "test-js-window-actor-parent-event";
-      Services.obs.addObserver(function obs(subject, topic, data) {
-        is(topic, TOPIC, "topic matches");
+    let observePromise = TestUtils.topicObserved(
+      "test-js-window-actor-parent-event"
+    );
 
-        Services.obs.removeObserver(obs, TOPIC);
-        resolve({ subject, data });
-      }, TOPIC);
-    });
-
-    // Click on the select to show the dropdown.
-    await BrowserTestUtils.synthesizeMouseAtCenter("#testSelect", {}, browser);
+    await createAndShowDropdown(browser);
 
     // Wait for the observer notification to fire, and inspect the results.
-    let { subject, data } = await observePromise;
+    let [subject, data] = await observePromise;
+    is(data, "mozshowdropdown");
+
+    let parent = browser.browsingContext.currentWindowGlobal;
+    let actorParent = parent.getActor("TestWindow");
+    ok(actorParent, "JSWindowActorParent should have value.");
+    is(
+      subject.wrappedJSObject,
+      actorParent,
+      "Should have been recieved on the right actor"
+    );
+  },
+});
+
+declTest("test createActor:false not triggering actor creation", {
+  events: { mozshowdropdown: { createActor: false } },
+
+  async test(browser) {
+    info("before actor has been created");
+    const TOPIC = "test-js-window-actor-parent-event";
+    function obs() {
+      ok(false, "actor should not be created");
+    }
+    Services.obs.addObserver(obs, TOPIC);
+
+    await createAndShowDropdown(browser);
+
+    // Bounce into the content process & create the actor after showing the
+    // dropdown, in order to be sure that if an event was going to be
+    // delivered, it already would've been.
+    await SpecialPowers.spawn(browser, [], async () => {
+      content.windowGlobalChild.getActor("TestWindow");
+    });
+    ok(true, "observer notification should not have fired");
+    Services.obs.removeObserver(obs, TOPIC);
+
+    // ----------------------------------------------------
+    info("after actor has been created");
+    let observePromise = TestUtils.topicObserved(
+      "test-js-window-actor-parent-event"
+    );
+    await createAndShowDropdown(browser);
+
+    // Wait for the observer notification to fire, and inspect the results.
+    let [subject, data] = await observePromise;
     is(data, "mozshowdropdown");
 
     let parent = browser.browsingContext.currentWindowGlobal;
