ae9fd2e09edb9c685a4261607a39478be31f9e98
Alexandre Poirot <poirot.alex@gmail.com>
2021-05-12 15:08:03 +0000

    Bug 1686748 - [devtools] Use navigateTo in debugger tests. r=nchevobbe
    Differential Revision: https://phabricator.services.mozilla.com/D114831

--- devtools/client/debugger/test/mochitest/browser_dbg-preview-module.js
+++ devtools/client/debugger/test/mochitest/browser_dbg-preview-module.js
@@ -7,7 +7,7 @@
 add_task(async function() {
   const dbg = await initDebugger("doc-scripts.html");
 
-  navigate(dbg, "doc-on-load.html");
+  const onNavigated = navigate(dbg, "doc-on-load.html");
 
   // wait for `top-level.js` to load and to pause at a debugger statement
   await waitForSelectedSource(dbg, "top-level.js");
@@ -23,4 +23,8 @@ add_task(async function() {
   ]);
 
   await assertPreviewTooltip(dbg, 2, 7, { result: "3", expression: "func" });
+
+  info("Resume and wait for full navigation of the tab");
+  await resume(dbg);
+  await onNavigated;
 });
