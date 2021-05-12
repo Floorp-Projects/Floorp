ea19316503ba01b38d6fbae6957ac5b4550242e2
Dragana Damjanovic <dd.mozilla@gmail.com>
2021-05-12 18:39:41 +0000

    Bug 1710816 - Enable HTTP/3 by default r=necko-reviewers,kershaw
    Differential Revision: https://phabricator.services.mozilla.com/D114956

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1418,11 +1418,7 @@ pref("network.http.spdy.websockets", true);
 pref("network.http.spdy.enable-hpack-dump", false);
 
 // Http3 parameters
-#if defined(EARLY_BETA_OR_EARLIER)
 pref("network.http.http3.enabled", true);
-#else
-pref("network.http.http3.enabled", false);
-#endif
 
 // Http3 qpack table size.
 pref("network.http.http3.default-qpack-table-size", 65536); // 64k
