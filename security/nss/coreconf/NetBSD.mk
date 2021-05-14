99e603ebb12c43a9e0bbec96cb7c117700a3683d
Benjamin Beurdouche <bbeurdouche@mozilla.com>
2021-05-14 10:46:49 +0000

    Bug 1705477 - land NSS NSS_3_65_RTM UPGRADE_NSS_RELEASE, r=beurdouche
    2021-05-14  Benjamin Beurdouche  <bbeurdouche@mozilla.com>
    
    	* lib/nss/nss.h, lib/softoken/softkver.h, lib/util/nssutil.h:
    	Set version numbers to 3.65 final
    	[0e785b3a4a10] [NSS_3_65_RTM] <NSS_3_65_BRANCH>
    
    	* .hgtags:
    	Added tag NSS_3_65_BETA1 for changeset 1bdb4713e2f0
    	[6f4869107d74] <NSS_3_65_BRANCH>
    
    2021-05-11  Robert Relyea  <rrelyea@redhat.com>
    
    	* gtests/pk11_gtest/pk11_hpke_unittest.cc:
    	fix clang format error from patch for bug 1709750
    	[1bdb4713e2f0] [NSS_3_65_BETA1]
    
    	* coreconf/NetBSD.mk:
    	Bug 1709654 Update for NetBSD configuration patch by Thomas Klausner
    	r=rrelyea
    
    	In the NetBSD configuration, the symbol hiding flags are not
    	defined. This leads to conflicts when openssl and nss are linked
    	into the same binary. For a longer discussion on the topic, see
    	https://groups.google.com/a/mozilla.org/g/dev-tech-
    	crypto/c/Al0Pt0zhARE
    
    	 Match more closely to OpenBSD.mk, and in particular, hide symbols
    	(MAPFILE).
    
    	- fix wrong value of CPU_ARCH on NetBSD/evbarm-earmv7f
    	- s/aarch64eb/aarch64/
    	[a7769615f285]
    
    Differential Revision: https://phabricator.services.mozilla.com/D115135

--- security/nss/coreconf/NetBSD.mk
+++ security/nss/coreconf/NetBSD.mk
@@ -5,9 +5,10 @@
 
 include $(CORE_DEPTH)/coreconf/UNIX.mk
 
-DEFAULT_COMPILER	= gcc
-CC			= gcc
-CCC			= g++
+CC			?= gcc
+CXX			?= g++
+DEFAULT_COMPILER	= ${CC}
+CCC			= ${CXX}
 RANLIB			= ranlib
 
 CPU_ARCH		:= $(shell uname -p)
@@ -15,16 +16,14 @@ ifeq ($(CPU_ARCH),i386)
 OS_REL_CFLAGS		= -Di386
 CPU_ARCH		= x86
 endif
-
-ifndef OBJECT_FMT
-OBJECT_FMT		:= $(shell if echo __ELF__ | $${CC:-cc} -E - | grep -q __ELF__ ; then echo a.out ; else echo ELF ; fi)
+ifeq (,$(filter-out earm%,$(CPU_ARCH)))
+CPU_ARCH		= arm
+endif
+ifeq ($(CPU_ARCH),aarch64eb)
+CPU_ARCH		= aarch64
 endif
 
-ifeq ($(OBJECT_FMT),ELF)
 DLL_SUFFIX		= so
-else
-DLL_SUFFIX		= so.1.0
-endif
 
 OS_CFLAGS		= $(DSO_CFLAGS) $(OS_REL_CFLAGS) -Wall -Wno-switch -pipe -DNETBSD -Dunix -DHAVE_STRERROR -DHAVE_BSD_FLOCK
 
@@ -33,9 +32,16 @@ OS_LIBS			= -lcompat
 ARCH			= netbsd
 
 DSO_CFLAGS		= -fPIC -DPIC
-DSO_LDOPTS		= -shared
-ifeq ($(OBJECT_FMT),ELF)
-DSO_LDOPTS		+= -Wl,-soname,lib$(LIBRARY_NAME)$(LIBRARY_VERSION).$(DLL_SUFFIX)
+DSO_LDOPTS		= -shared -Wl,-soname,lib$(LIBRARY_NAME)$(LIBRARY_VERSION).$(DLL_SUFFIX)
+
+#
+# The default implementation strategy for NetBSD is pthreads.
+#
+ifndef CLASSIC_NSPR
+USE_PTHREADS		= 1
+DEFINES			+= -D_THREAD_SAFE -D_REENTRANT
+OS_LIBS			+= -pthread
+DSO_LDOPTS		+= -pthread
 endif
 
 ifdef LIBRUNPATH
@@ -44,12 +50,8 @@ endif
 
 MKSHLIB			= $(CC) $(DSO_LDOPTS)
 ifdef MAPFILE
-# Add LD options to restrict exported symbols to those in the map file
+	MKSHLIB += -Wl,--version-script,$(MAPFILE)
 endif
-# Change PROCESS to put the mapfile in the correct format for this platform
-PROCESS_MAP_FILE = cp $< $@
+PROCESS_MAP_FILE = grep -v ';-' $< | \
+        sed -e 's,;+,,' -e 's; DATA ;;' -e 's,;;,,' -e 's,;.*,;,' > $@
 
-
-G++INCLUDES		= -I/usr/include/g++
-
-INCLUDES		+= -I/usr/X11R6/include
--- security/nss/gtests/pk11_gtest/pk11_hpke_unittest.cc
+++ security/nss/gtests/pk11_gtest/pk11_hpke_unittest.cc
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef UNSAFE_FUZZER_MODE // See Bug 1709750
+#ifndef UNSAFE_FUZZER_MODE  // See Bug 1709750
 
 #include <memory>
 #include "blapi.h"
