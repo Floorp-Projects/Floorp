0e067a0e53d8d22821bb1d362f069a0c086c09a1
mcarare <mihai.carare.dev@gmail.com>
2021-05-14 14:56:28 +0300

    [components] Closes https://github.com/mozilla-mobile/android-components/issues/10263: Add Credit Card native name to type mapping.
    Also added unit tests.

--- mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/sync/Types.kt
+++ mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/sync/Types.kt
@@ -32,6 +32,7 @@ internal fun String.toSyncEngine(): SyncEngine {
         "bookmarks" -> SyncEngine.Bookmarks
         "passwords" -> SyncEngine.Passwords
         "tabs" -> SyncEngine.Tabs
+        "creditcards" -> SyncEngine.CreditCards
         // This handles a case of engines that we don't yet model in SyncEngine.
         else -> SyncEngine.Other(this)
     }
