e8ac260b11e327131ec3f4b95e694212ed76b273
Jonathan Kew <jkew@mozilla.com>
2021-05-14 18:05:44 +0000

    Bug 1708768 - Create a pref to defer gfxPlatformFontList initialization until first use, and enable this on macOS. r=lsalzman
    This gives the RegisterFonts thread more time to complete its work, so that
    the main thread doesn't have to wait for it in InitFontList.
    
    In the worst case, where the registration thread still hasn't finished
    by the time we call gfxPlatformFontList::PlatformFontList() the first time,
    it's still no worse than previously, it just reorders some of the work
    that has to be done before we can render any content.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114865

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -968,8 +968,13 @@ void gfxPlatform::Init() {
 
   gPlatform->mHasVariationFontSupport = gPlatform->CheckVariationFontSupport();
 
-  if (!gPlatform->CreatePlatformFontList()) {
-    MOZ_CRASH("Could not initialize gfxPlatformFontList");
+  if (!StaticPrefs::gfx_font_list_lazy_init_enabled_AtStartup()) {
+    // On macOS we don't initialize the font list here because we want to allow
+    // more time for the RegisterFonts thread to complete. So it is initialized
+    // lazily on first use; see gfxPlatformFontList::PlatformFontList().
+    if (!gPlatform->CreatePlatformFontList()) {
+      MOZ_CRASH("Could not initialize gfxPlatformFontList");
+    }
   }
 
   gPlatform->mScreenReferenceSurface = gPlatform->CreateOffscreenSurface(
