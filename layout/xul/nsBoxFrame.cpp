51ff25ad595e484b41f9136bccc89bbb8cd852f0
Matt Woodrow <mwoodrow@mozilla.com>
2021-05-17 23:53:45 +0000

    Bug 1540737 - Implement Paint() for all display items, and nsDisplayList so that we can paint without using FrameLayerBuilder. r=miko
    This creates a new leaf nsDisplayWrapper item for consumers that just want an empty wrapper, with a Paint method that asserts. This lets us leaves Paint pure-virtual on nsDisplayWrapList, and lets the compiler force us to implement it for the other leaf types.
    
    It also disables flattening of transform/opacity items. This is because they flatten the items even when used with FlattenedDisplayItemIterator, even though this doesn't add markers to replace them (only FLB does this). We could in theory fix this, but I don't think FLB performance matters much any more.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114676

--- layout/xul/nsBoxFrame.cpp
+++ layout/xul/nsBoxFrame.cpp
@@ -1043,6 +1043,10 @@ class nsDisplayXULEventRedirector final : public nsDisplayWrapList {
   virtual bool ShouldFlattenAway(nsDisplayListBuilder* aBuilder) override {
     return false;
   }
+  void Paint(nsDisplayListBuilder* aBuilder, gfxContext* aCtx) override {
+    GetChildren()->Paint(aBuilder, aCtx,
+                         mFrame->PresContext()->AppUnitsPerDevPixel());
+  }
   NS_DISPLAY_DECL_NAME("XULEventRedirector", TYPE_XUL_EVENT_REDIRECTOR)
  private:
   nsIFrame* mTargetFrame;
@@ -1083,7 +1087,7 @@ void nsDisplayXULEventRedirector::HitTest(nsDisplayListBuilder* aBuilder,
   }
 }
 
-class nsXULEventRedirectorWrapper final : public nsDisplayWrapper {
+class nsXULEventRedirectorWrapper final : public nsDisplayItemWrapper {
  public:
   explicit nsXULEventRedirectorWrapper(nsIFrame* aTargetFrame)
       : mTargetFrame(aTargetFrame) {}
