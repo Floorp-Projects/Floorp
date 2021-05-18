4a4b5a61715b81b1816f5cafcb55679149c9738f
Nika Layzell <nika@thelayzells.com>
2021-05-18 16:24:49 +0000

    Bug 1711090 - Part 4: Remove non-string func timer names, r=KrisWright
    They are not used anywhere in the codebase and come with
    non-insignificant complexity which we can drop.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115267

--- xpcom/threads/nsTimerImpl.h
+++ xpcom/threads/nsTimerImpl.h
@@ -78,14 +78,7 @@ class nsTimerImpl {
   struct FuncCallback {
     nsTimerCallbackFunc mFunc;
     void* mClosure;
-
-    // XXX(nika): It seems like nobody ever constructs any variant for `mName`
-    // other than `const char*` - can we drop the others?
-    struct NameNothing {};
-    using NameString = const char*;
-    using NameFunc = nsTimerNameCallbackFunc;
-    using Name = mozilla::Variant<NameNothing, NameString, NameFunc>;
-    Name mName;
+    const char* mName;
   };
 
   /// A callback defined by an owned closure and its name for logging purposes.
@@ -139,10 +132,6 @@ class nsTimerImpl {
   nsCOMPtr<nsIEventTarget> mEventTarget;
 
   void LogFiring(const Callback& aCallback, uint8_t aType, uint32_t aDelay);
-
-  nsresult InitWithFuncCallbackCommon(nsTimerCallbackFunc aFunc, void* aClosure,
-                                      uint32_t aDelay, uint32_t aType,
-                                      const FuncCallback::Name& aName);
 
   nsresult InitWithClosureCallback(std::function<void(nsITimer*)>&& aCallback,
                                    const mozilla::TimeDuration& aDelay,
