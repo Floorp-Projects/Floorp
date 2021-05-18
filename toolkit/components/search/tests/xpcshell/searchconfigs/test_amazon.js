13bbf88dcba7dcb715ab56ad30da5fb9891c5bcd
Mark Banner <standard8@mozilla.com>
2021-05-18 14:29:45 +0000

    Bug 1711504 - Update Amazon au search definition. r=daleharvey
    Differential Revision: https://phabricator.services.mozilla.com/D115256

--- toolkit/components/search/tests/xpcshell/searchconfigs/test_amazon.js
+++ toolkit/components/search/tests/xpcshell/searchconfigs/test_amazon.js
@@ -101,7 +101,6 @@ const test = new SearchConfigTest({
           regions: ["au"],
         },
       ],
-      searchUrlCode: "tag=mozillaaust0c-22",
       suggestionUrlBase: "https://completion.amazon.com.au/search/complete",
       suggestUrlCode: "mkt=111172",
     },
