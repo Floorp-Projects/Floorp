de161a2e0ca650d6c9fc68fdefedd35ee6c11470
Oana Horvath <oana.horvath@softvision.ro>
2021-05-18 14:31:21 +0300

    [focus] New UI tests for Set default browser setting

--- mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/robots/SettingsGeneralMenuRobot.kt
+++ mobile/android/focus-android/app/src/androidTest/java/org/mozilla/focus/activity/robots/SettingsGeneralMenuRobot.kt
@@ -5,28 +5,81 @@
 package org.mozilla.focus.activity.robots
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isNotChecked
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import org.mozilla.focus.helpers.TestHelper.pressBackKey
+import androidx.test.uiautomator.UiSelector
+import junit.framework.TestCase.assertTrue
+import org.mozilla.focus.R
+import org.mozilla.focus.helpers.TestHelper.appName
+import org.mozilla.focus.helpers.TestHelper.mDevice
+import org.mozilla.focus.helpers.TestHelper.waitingTime
 
 class SettingsGeneralMenuRobot {
+    companion object {
+        const val DEFAULT_APPS_SETTINGS_ACTION = "android.settings.MANAGE_DEFAULT_APPS_SETTINGS"
+    }
 
     fun verifyGeneralSettingsItems() {
         defaultBrowserSwitch.check(matches(isDisplayed()))
         switchToLinkToggleButton.check(matches(isDisplayed()))
     }
 
-    class Transition {
-        fun goBack(interact: SettingsRobot.() -> Unit): SettingsRobot.Transition {
-            pressBackKey()
+    fun clickSetDefaultBrowser() {
+        defaultBrowserSwitch
+                .check(matches(isDisplayed()))
+                .perform(click())
+    }
 
-            SettingsRobot().interact()
-            return SettingsRobot.Transition()
-        }
+    fun verifyAndroidDefaultAppsMenuAppears() {
+        intended(IntentMatchers.hasAction(DEFAULT_APPS_SETTINGS_ACTION))
+    }
+
+    fun verifyOpenWithDialog() {
+        openWithDialogTitle.waitForExists(waitingTime)
+        assertTrue(openWithDialogTitle.exists())
+        assertTrue(openWithList.exists())
+    }
+
+    fun selectAlwaysOpenWithFocus() {
+        mDevice.findObject(UiSelector().text(appName)).click()
+        mDevice.findObject(
+                UiSelector()
+                        .resourceId("android:id/button_always")
+                        .enabled(true)
+        ).click()
+    }
+
+    fun verifySwitchIsToggled(checked: Boolean) {
+        onView(withId(R.id.switch_widget)).check(matches(
+                if (checked)
+                    isChecked()
+                else
+                    isNotChecked()
+        ))
+    }
+
+    class Transition {
+        // add here transitions to other robot classes
     }
 }
 
 private val defaultBrowserSwitch = onView(withText("Make Firefox Focus default browser"))
 
 private val switchToLinkToggleButton = onView(withText("Switch to link in new tab immediately"))
+
+private val openWithDialogTitle = mDevice.findObject(
+        UiSelector()
+                .text("Open with")
+)
+
+private val openWithList = mDevice.findObject(
+        UiSelector()
+                .resourceId("android:id/resolver_list")
+)
