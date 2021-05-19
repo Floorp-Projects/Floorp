6a673050748e90a91357512ae584facc001fa7aa
Arturo Mejia <arturomejiamarmol@gmail.com>
2021-05-18 21:54:13 -0400

    [components] Update to GeckoView 91.0.20210608091819

--- mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
+++ mobile/android/android-components/buildSrc/src/main/java/Gecko.kt
@@ -9,7 +9,7 @@ object Gecko {
     /**
      * GeckoView Version.
      */
-    const val version = "91.0.20210607094749"
+    const val version = "91.0.20210608091819"
 
     /**
      * GeckoView channel
