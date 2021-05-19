1f638b9e0618fd203ce90270685b3571eca0e044
Nicolas Chevobbe <nchevobbe@mozilla.com>
2021-05-19 16:58:07 +0000

    Bug 1711463 - [devtools] Update EvaluationContextSelector items on DOCUMENT_EVENT resources. r=jdescottes.
    This patch adds a `refreshTargets` action that is called from the toolbox when
    a dom-interactive DOCUMENT_EVENT is received.
    This action will only update a new state property, `lastTargetRefresh`, with the
    current timestamp, so the `EvaluationContextSelector` component will re-render;
    as the target information are directly retrieved from the target fronts, any
    changes in the title/url will be shown in the component.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115205

--- devtools/client/webconsole/components/Input/EvaluationContextSelector.js
+++ devtools/client/webconsole/components/Input/EvaluationContextSelector.js
@@ -48,6 +48,7 @@ class EvaluationContextSelector extends Component {
       updateInstantEvaluationResultForCurrentExpression:
         PropTypes.func.isRequired,
       selectedTarget: PropTypes.object,
+      lastTargetRefresh: PropTypes.number,
       targets: PropTypes.array,
       webConsoleUI: PropTypes.object.isRequired,
     };
@@ -57,9 +58,15 @@ class EvaluationContextSelector extends Component {
     if (this.props.selectedTarget !== nextProps.selectedTarget) {
       return true;
     }
+
+    if (this.props.lastTargetRefresh !== nextProps.lastTargetRefresh) {
+      return true;
+    }
+
     if (this.props.targets.length !== nextProps.targets.length) {
       return true;
     }
+
     for (let i = 0; i < nextProps.targets.length; i++) {
       const target = this.props.targets[i];
       const nextTarget = nextProps.targets[i];
@@ -206,6 +213,7 @@ const toolboxConnected = connect(
   state => ({
     targets: targetSelectors.getToolboxTargets(state),
     selectedTarget: targetSelectors.getSelectedTarget(state),
+    lastTargetRefresh: targetSelectors.getLastTargetRefresh(state),
   }),
   dispatch => ({
     selectTarget: actorID => dispatch(frameworkActions.selectTarget(actorID)),
