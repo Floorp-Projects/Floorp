182545ba98c224efa2ab2e687e5b3f3cd5786f47
Tim Huang <tihuang@mozilla.com>
2021-05-20 11:22:11 +0000

    Bug 1706604 - Part 1: Implement the URLQueryStringStripper. r=dimi
    This patch implements a URLQueryStringStripper which provides a static
    function Strip() to strip the target query parameters. The function
    accepts a URI and reutrn the stripped URI.
    
    Differential Revision: https://phabricator.services.mozilla.com/D113842

---
+++ toolkit/components/antitracking/URLQueryStringStripper.h
@@ -0,0 +1,58 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_URLQueryStringStripper_h
+#define mozilla_URLQueryStringStripper_h
+
+#include "nsIObserver.h"
+
+#include "nsStringFwd.h"
+#include "nsTHashSet.h"
+
+class nsIURI;
+
+namespace mozilla {
+
+// URLQueryStringStripper is responsible for stripping certain part of the query
+// string of the given URI to address the bounce(redirect) tracking issues. It
+// will strip every query parameter which matches the strip list defined in
+// the pref 'privacy.query_stripping.strip_list'. Note that It's different from
+// URLDecorationStripper which strips the entire query string from the referrer
+// if there is a tracking query parameter present in the URI.
+//
+// TODO: Given that URLQueryStringStripper and URLDecorationStripper are doing
+//       similar things. We could somehow combine these two modules into one.
+//       We will improve this in the future.
+
+class URLQueryStringStripper final : public nsIObserver {
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+
+  // Strip the query parameters that are in the strip list. Return true if there
+  // is any query parameter has been stripped. Otherwise, false.
+  static bool Strip(nsIURI* aURI, nsCOMPtr<nsIURI>& aOutput);
+
+ private:
+  URLQueryStringStripper() = default;
+
+  ~URLQueryStringStripper() = default;
+
+  static URLQueryStringStripper* GetOrCreate();
+
+  void Init();
+  void Shutdown();
+
+  bool StripQueryString(nsIURI* aURI, nsCOMPtr<nsIURI>& aOutput);
+
+  void PopulateStripList();
+
+  nsTHashSet<nsString> mList;
+};
+
+}  // namespace mozilla
+
+#endif  // mozilla_URLQueryStringStripper_h
