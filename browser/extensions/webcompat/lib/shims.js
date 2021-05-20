f325cee124b5d38a3056fe569cad41d156efeb59
Thomas Wisniewski <twisniewski@mozilla.com>
2021-05-20 16:48:52 +0000

    Bug 1709449 - Changes to the Facebook SmartBlock shim to bring it in line with phase 2 SmartBlock expectations, including relevant SmartBlock engine changes; r=denschub,webcompat-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D115261

--- browser/extensions/webcompat/lib/shims.js
+++ browser/extensions/webcompat/lib/shims.js
@@ -6,6 +6,12 @@
 
 /* globals browser, module, onMessageFromTab */
 
+// To grant shims access to bundled logo images without risking
+// exposing our moz-extension URL, we have the shim request them via
+// nonsense URLs which we then redirect to the actual files (but only
+// on tabs where a shim using a given logo happens to be active).
+const LogosBaseURL = "https://smartblock.firefox.etp/";
+
 const releaseBranchPromise = browser.appConstants.getReleaseBranch();
 
 const platformPromise = browser.runtime.getPlatformInfo().then(info => {
@@ -37,7 +43,8 @@ class Shim {
     this.file = opts.file;
     this.hosts = opts.hosts;
     this.id = opts.id;
-    this.matches = matches;
+    this.logos = opts.logos || [];
+    this.matches = [];
     this.name = opts.name;
     this.notHosts = opts.notHosts;
     this.onlyIfBlockedByETP = opts.onlyIfBlockedByETP;
@@ -53,7 +60,17 @@ class Shim {
     this._disabledByPlatform = false;
     this._disabledByReleaseBranch = false;
 
+    this._activeOnTabs = new Set();
+
     const pref = `disabled_shims.${this.id}`;
+
+    for (const match of matches) {
+      if (!match.types) {
+        this.matches.push({ patterns: [match], types: ["script"] });
+      } else {
+        this.matches.push(match);
+      }
+    }
 
     browser.aboutConfigPrefs.onPrefChange.addListener(async () => {
       const value = await browser.aboutConfigPrefs.getPref(pref);
@@ -142,7 +159,8 @@ class Shim {
   }
 
   _allowRequestsInETP() {
-    return browser.trackingProtection.allow(this.id, this.matches, {
+    const matches = this.matches.map(m => m.patterns).flat();
+    return browser.trackingProtection.allow(this.id, matches, {
       hosts: this.hosts,
       notHosts: this.notHosts,
       hostOptIns: Array.from(this._hostOptIns),
@@ -151,6 +169,18 @@ class Shim {
 
   _revokeRequestsInETP() {
     return browser.trackingProtection.revoke(this.id);
+  }
+
+  setActiveOnTab(tabId, active = true) {
+    if (active) {
+      this._activeOnTabs.add(tabId);
+    } else {
+      this._activeOnTabs.delete(tabId);
+    }
+  }
+
+  isActiveOnTab(tabId) {
+    return this._activeOnTabs.has(tabId);
   }
 
   meantForHost(host) {
@@ -166,23 +196,89 @@ class Shim {
     return true;
   }
 
-  isTriggeredByURL(url) {
-    if (!this.matches) {
-      return false;
+  async unblocksURLOnOptIn(url) {
+    if (!this._optInPatterns) {
+      this._optInPatterns = await this.getApplicableOptIns();
     }
 
-    if (!this._matcher) {
-      this._matcher = browser.matchPatterns.getMatcher(this.matches);
+    if (!this._optInMatcher) {
+      this._optInMatcher = browser.matchPatterns.getMatcher(
+        Array.from(this._optInPatterns)
+      );
     }
 
-    return this._matcher.matches(url);
+    return this._optInMatcher.matches(url);
+  }
+
+  isTriggeredByURLAndType(url, type) {
+    for (const entry of this.matches || []) {
+      if (!entry.types.includes(type)) {
+        continue;
+      }
+      if (!entry.matcher) {
+        entry.matcher = browser.matchPatterns.getMatcher(
+          Array.from(entry.patterns)
+        );
+      }
+      if (entry.matcher.matches(url)) {
+        return entry;
+      }
+    }
+
+    return undefined;
+  }
+
+  getAllOptIns() {
+    if (this._allOptIns) {
+      return this._allOptIns;
+    }
+    const optins = [];
+    for (const unblock of this.unblocksOnOptIn || []) {
+      if (typeof unblock === "string") {
+        optins.push(unblock);
+      } else {
+        optins.push.apply(optins, unblock.patterns);
+      }
+    }
+    this._allOptIns = optins;
+    return optins;
+  }
+
+  async getApplicableOptIns() {
+    if (this._applicableOptIns) {
+      return this._applicableOptIns;
+    }
+    const optins = [];
+    for (const unblock of this.unblocksOnOptIn || []) {
+      if (typeof unblock === "string") {
+        optins.push(unblock);
+        continue;
+      }
+      const { branches, patterns, platforms } = unblock;
+      if (platforms?.length) {
+        const platform = await platformPromise;
+        if (platform !== "all" && !platforms.includes(platform)) {
+          continue;
+        }
+      }
+      if (branches?.length) {
+        const branch = await releaseBranchPromise;
+        if (!branches.includes(branch)) {
+          continue;
+        }
+      }
+      optins.push.apply(optins, patterns);
+    }
+    this._applicableOptIns = optins;
+    return optins;
   }
 
   async onUserOptIn(host) {
-    const { matches, unblocksOnOptIn } = this;
-    if (unblocksOnOptIn) {
+    const optins = await this.getApplicableOptIns();
+    if (optins.length) {
+      const { matches } = this;
       this.userHasOptedIn = true;
-      const toUnblock = matches.concat(unblocksOnOptIn);
+      const toUnblock = [...matches.map(m => m.patterns).flat(), ...optins];
       await browser.trackingProtection.allow(this.id, toUnblock, {
         hosts: this.hosts,
         hostOptIns: [host],
@@ -228,26 +324,75 @@ class Shims {
       }
     }
 
-    const allShimPatterns = new Set();
-    for (const { matches } of this.shims.values()) {
-      for (const matchPattern of matches) {
-        allShimPatterns.add(matchPattern);
+    const allMatchTypePatterns = new Map();
+    const allOptInPatterns = new Set();
+    const allLogos = [];
+    for (const shim of this.shims.values()) {
+      const { logos, matches } = shim;
+      allLogos.push(...logos);
+      const optins = shim.getAllOptIns();
+      if (optins.length) {
+        for (const pattern of optins || []) {
+          allOptInPatterns.add(pattern);
+        }
+      }
+      for (const { patterns, types } of matches || []) {
+        for (const type of types) {
+          if (!allMatchTypePatterns.has(type)) {
+            allMatchTypePatterns.set(type, { patterns: new Set() });
+          }
+          const allSet = allMatchTypePatterns.get(type).patterns;
+          for (const pattern of patterns) {
+            allSet.add(pattern);
+          }
+        }
       }
     }
 
-    if (!allShimPatterns.size) {
+    if (!allMatchTypePatterns.size) {
       debug("Skipping shims; none enabled");
       return;
     }
 
-    const urls = Array.from(allShimPatterns);
-    debug("Shimming these match patterns", urls);
+    if (allLogos.length) {
+      const urls = Array.from(new Set(allLogos)).map(l => {
+        return `${LogosBaseURL}${l}`;
+      });
+      debug("Allowing access to these logos:", urls);
+      const unmarkShimsActive = tabId => {
+        for (const shim of this.shims.values()) {
+          shim.setActiveOnTab(tabId, false);
+        }
+      };
+      browser.tabs.onRemoved.addListener(unmarkShimsActive);
+      browser.tabs.onUpdated.addListener(unmarkShimsActive, {
+        properties: ["discarded", "url"],
+      });
+      browser.webRequest.onBeforeRequest.addListener(
+        this._redirectLogos.bind(this),
+        { urls, types: ["image"] },
+        ["blocking"]
+      );
+    }
 
-    browser.webRequest.onBeforeRequest.addListener(
-      this._ensureShimForRequestOnTab.bind(this),
-      { urls, types: ["script"] },
-      ["blocking"]
-    );
+    if (allOptInPatterns.size) {
+      browser.webRequest.onBeforeRequest.addListener(
+        this._ensureOptInRequestAllowedOnTab.bind(this),
+        { urls: Array.from(allOptInPatterns) },
+        ["blocking"]
+      );
+    }
+
+    for (const [type, { patterns }] of allMatchTypePatterns.entries()) {
+      const urls = Array.from(patterns);
+      debug("Shimming these", type, "URLs:", urls);
+
+      browser.webRequest.onBeforeRequest.addListener(
+        this._ensureShimForRequestOnTab.bind(this),
+        { urls, types: [type] },
+        ["blocking"]
+      );
+    }
   }
 
   async _checkEnabledPref() {
@@ -306,7 +451,13 @@ class Shims {
     }
 
     if (message === "getOptions") {
-      return shim.options;
+      return Object.assign(
+        {
+          platform: await platformPromise,
+          releaseBranch: await releaseBranchPromise,
+        },
+        shim.options
+      );
     } else if (message === "optIn") {
       try {
         await shim.onUserOptIn(new URL(url).hostname);
@@ -337,6 +488,96 @@ class Shims {
     return undefined;
   }
 
+  async _redirectLogos(details) {
+    await this._haveCheckedEnabledPref;
+
+    if (!this.enabled) {
+      return { cancel: true };
+    }
+
+    const { tabId, url } = details;
+    const logo = new URL(url).pathname.slice(1);
+
+    for (const shim of this.shims.values()) {
+      await shim.ready;
+
+      if (!shim.enabled) {
+        continue;
+      }
+
+      if (!shim.logos.includes(logo)) {
+        continue;
+      }
+
+      if (shim.isActiveOnTab(tabId)) {
+        return { redirectUrl: browser.runtime.getURL(`shims/${logo}`) };
+      }
+    }
+
+    return { cancel: true };
+  }
+
+  async _ensureOptInRequestAllowedOnTab(details) {
+    await this._haveCheckedEnabledPref;
+
+    if (!this.enabled) {
+      return undefined;
+    }
+
+    // If a user has opted into allowing some tracking requests, we only
+    // want to allow them on those tabs where the opt-in should apply.
+    // This listener ensures that they are otherwise blocked.
+
+    const { frameId, requestId, tabId, url } = details;
+
+    // Ignore requests unrelated to tabs
+    if (tabId < 0) {
+      return undefined;
+    }
+
+    // We need to base our checks not on the frame's host, but the tab's.
+    const topHost = new URL((await browser.tabs.get(tabId)).url).hostname;
+
+    // Sanity check; only worry about requests ETP would actually block
+    if (!(await browser.trackingProtection.wasRequestUnblocked(requestId))) {
+      return undefined;
+    }
+
+    let userOptedIn = false;
+    for (const shim of this.shims.values()) {
+      await shim.ready;
+
+      if (!shim.enabled) {
+        continue;
+      }
+
+      if (!shim.onlyIfBlockedByETP) {
+        continue;
+      }
+
+      if (!shim.meantForHost(topHost)) {
+        continue;
+      }
+
+      if (!shim.hasUserOptedInAlready(topHost)) {
+        continue;
+      }
+
+      if (await shim.unblocksURLOnOptIn(url)) {
+        userOptedIn = true;
+        break;
+      }
+    }
+
+    // reblock the request if the user has not opted into it on the tab
+    if (!userOptedIn) {
+      return { cancel: true };
+    }
+
+    debug(`Allowing ${url} on tab ${tabId} frame ${frameId} due to opt-in`);
+    return undefined;
+  }
+
   async _ensureShimForRequestOnTab(details) {
     await this._haveCheckedEnabledPref;
 
@@ -348,7 +589,7 @@ class Shims {
     // be shimmed. We never get here if a request is blocked, and we only
     // unblock requests if at least one shim matches it.
 
-    const { frameId, originUrl, requestId, tabId, url } = details;
+    const { frameId, originUrl, requestId, tabId, type, url } = details;
 
     // Ignore requests unrelated to tabs
     if (tabId < 0) {
@@ -361,6 +602,7 @@ class Shims {
       requestId
     );
 
+    let match;
     let shimToApply;
     for (const shim of this.shims.values()) {
       await shim.ready;
@@ -385,26 +627,33 @@ class Shims {
         return undefined;
       }
 
-      // If this URL isn't meant for this shim, don't apply it.
-      if (!shim.isTriggeredByURL(url)) {
-        continue;
+      // If this URL and content type isn't meant for this shim, don't apply it.
+      match = shim.isTriggeredByURLAndType(url, type);
+      if (match) {
+        shimToApply = shim;
+        break;
       }
-
-      shimToApply = shim;
-      break;
     }
 
     if (shimToApply) {
       // Note that sites may request the same shim twice, but because the requests
-      // may differ enough for some to fail (CSP/CORS/etc), we always re-run the
-      // shim JS just in case. Shims should gracefully handle this as well.
+      // may differ enough for some to fail (CSP/CORS/etc), we always let the request
+      // complete via local redirect. Shims should gracefully handle this as well.
+
+      const { target } = match;
       const { bug, file, id, name, needsShimHelpers } = shimToApply;
-      warn("Shimming", name, "on tabId", tabId, "frameId", frameId);
+
+      const redirect = target || file;
+
+      warn(
+        `Shimming tracking ${type} ${url} on tab ${tabId} frame ${frameId} with ${redirect}`
+      );
 
       const warning = `${name} is being shimmed by Firefox. See https://bugzilla.mozilla.org/show_bug.cgi?id=${bug} for details.`;
 
       try {
-        if (needsShimHelpers?.length) {
+        // For scripts, we also set up any needed shim helpers.
+        if (type === "script" && needsShimHelpers?.length) {
           await browser.tabs.executeScript(tabId, {
             file: "/lib/shim_messaging_helper.js",
             frameId,
@@ -416,28 +665,28 @@ class Shims {
             { origin, shimId: id, needsShimHelpers, warning },
             { frameId }
           );
+          shimToApply.setActiveOnTab(tabId);
         } else {
           await browser.tabs.executeScript(tabId, {
             code: `console.warn(${JSON.stringify(warning)})`,
-            frameId,
             runAt: "document_start",
           });
         }
       } catch (_) {}
 
-      // If any shims matched the script to replace it, then let the original
-      // request complete without ever hitting the network, with a blank script.
-      return { redirectUrl: browser.runtime.getURL(`shims/${file}`) };
+      // If any shims matched the request to replace it, then redirect to the local
+      // file bundled with SmartBlock, so the request never hits the network.
+      return { redirectUrl: browser.runtime.getURL(`shims/${redirect}`) };
     }
 
-    // Sanity check: if no shims are over-riding a given URL and it was meant to
-    // be blocked by ETP, then block it.
+    // Sanity check: if no shims end up handling this request,
+    // yet it was meant to be blocked by ETP, then block it now.
     if (unblocked) {
-      error("unexpected:", url, "was not shimmed, and had to be re-blocked");
+      error(`unexpected: ${url} not shimmed on tab ${tabId} frame ${frameId}`);
       return { cancel: true };
     }
 
-    debug("ignoring", url);
+    debug(`ignoring ${url} on tab ${tabId} frame ${frameId}`);
     return undefined;
   }
 }
