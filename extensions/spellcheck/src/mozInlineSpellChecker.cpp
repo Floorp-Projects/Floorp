ddbcce931618a4e4a32a21311886fbbe475d45a9
Mirko Brodesser <mbrodesser@mozilla.com>
2021-05-20 09:03:27 +0000

    Bug 1708422: part 10) Factor `ShouldSpellCheckRange` out. r=smaug
    Differential Revision: https://phabricator.services.mozilla.com/D114104

--- extensions/spellcheck/src/mozInlineSpellChecker.cpp
+++ extensions/spellcheck/src/mozInlineSpellChecker.cpp
@@ -1296,12 +1296,25 @@ class MOZ_STACK_CLASS mozInlineSpellChecker::SpellCheckerTimeSlice {
   void CheckWordsAndAddRangesForMisspellings(
       const nsTArray<nsString>& aWords, nsTArray<NodeOffsetRange>&& aRanges);
 
+  bool ShouldSpellCheckRange(const nsRange& aRange) const;
+
   mozInlineSpellChecker& mInlineSpellChecker;
   mozInlineSpellWordUtil& mWordUtil;
   mozilla::dom::Selection& mSpellCheckSelection;
   const mozilla::UniquePtr<mozInlineSpellStatus>& mStatus;
   bool& mDoneChecking;
 };
+
+bool mozInlineSpellChecker::SpellCheckerTimeSlice::ShouldSpellCheckRange(
+    const nsRange& aRange) const {
+  nsINode* beginNode = aRange.GetStartContainer();
+  nsINode* endNode = aRange.GetEndContainer();
+
+  const nsINode* rootNode = mWordUtil.GetRootNode();
+  return beginNode->IsInComposedDoc() && endNode->IsInComposedDoc() &&
+         beginNode->IsShadowIncludingInclusiveDescendantOf(rootNode) &&
+         endNode->IsShadowIncludingInclusiveDescendantOf(rootNode);
+}
 
 // mozInlineSpellChecker::SpellCheckerTimeSlice::Execute
 //
@@ -1372,12 +1385,7 @@ nsresult mozInlineSpellChecker::SpellCheckerTimeSlice::Execute() {
     nsINode* endNode = mStatus->mRange->GetEndContainer();
     int32_t endOffset = mStatus->mRange->EndOffset();
 
-    // Now check that we're still looking at a range that's under
-    // aWordUtil.GetRootNode()
-    const nsINode* rootNode = mWordUtil.GetRootNode();
-    if (!beginNode->IsInComposedDoc() || !endNode->IsInComposedDoc() ||
-        !beginNode->IsShadowIncludingInclusiveDescendantOf(rootNode) ||
-        !endNode->IsShadowIncludingInclusiveDescendantOf(rootNode)) {
+    if (!ShouldSpellCheckRange(*mStatus->mRange)) {
       // Just bail out and don't try to spell-check this
       return NS_OK;
     }
