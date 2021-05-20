56bc8eb7d2d88b6e9d4bf2d4834f8e75054baba3
James Graham <james@hoppipolla.co.uk>
2021-05-20 12:57:16 +0000

    Bug 1678044 - Switch to explicit profile cleanup rather than using __del__, r=webdriver-reviewers,whimboo
    This requires passing the test_environment into the get_executor_kwargs function
    so that in the firefox wdspec case we can add a cleanup function to the environment
    when running wdspec tests. That seems reasonable since we were previously using
    a variety of data in the environment to setup the kwargs anyway
    
    Depends on D99698
    
    Differential Revision: https://phabricator.services.mozilla.com/D101768

--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/edge.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/edge.py
@@ -42,10 +42,9 @@ def browser_kwargs(logger, test_type, run_info_data, config, **kwargs):
                                                          **kwargs)}
 
 
-def executor_kwargs(logger, test_type, server_config, cache_manager, run_info_data,
+def executor_kwargs(logger, test_type, test_environment, run_info_data,
                     **kwargs):
-    executor_kwargs = base_executor_kwargs(test_type, server_config,
-                                           cache_manager, run_info_data, **kwargs)
+    executor_kwargs = base_executor_kwargs(test_type, test_environment, run_info_data, **kwargs)
     executor_kwargs["close_after_done"] = True
     executor_kwargs["timeout_multiplier"] = get_timeout_multiplier(test_type,
                                                                    run_info_data,
