db06e27c59e75e964cc2b62e92005520f641117a
Jonathan Kew <jkew@mozilla.com>
2021-05-20 08:39:05 +0000

    Bug 1711576 - Ensure we set the primary frame for native anon content where needed. r=dholbert
    Differential Revision: https://phabricator.services.mozilla.com/D115527

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -3226,7 +3226,11 @@ void nsCSSFrameConstructor::ConstructTextFrame(
   // Add the newly constructed frame to the flow
   aFrameList.AppendFrame(nullptr, newFrame);
 
-  if (!aState.mCreatingExtraFrames) aContent->SetPrimaryFrame(newFrame);
+  if (!aState.mCreatingExtraFrames ||
+      (aContent->IsInNativeAnonymousSubtree() &&
+       !aContent->GetPrimaryFrame())) {
+    aContent->SetPrimaryFrame(newFrame);
+  }
 }
 
 /* static */
