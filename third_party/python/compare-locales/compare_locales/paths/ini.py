114966c6e0a0c7190053c9d92f1d4860760e7fcd
Agi Sferro <agi@sferro.dev>
2021-05-20 22:03:03 +0000

    Bug 1701269 - Remove mobile/android/base. r=nalexander,flod,owlish
    Differential Revision: https://phabricator.services.mozilla.com/D109918

--- third_party/python/compare-locales/compare_locales/paths/ini.py
+++ third_party/python/compare-locales/compare_locales/paths/ini.py
@@ -203,8 +203,6 @@ class EnumerateApp(object):
                 'l10n': mozpath.normpath('{l10n_base}/{locale}/%s/**' %
                                          module)
             }
-            if module == 'mobile/android/base':
-                paths['test'] = ['android-dtd']
             projectconfig.add_paths(paths)
         for child in aConfig.children:
             self._config_for_ini(projectconfig, child)
