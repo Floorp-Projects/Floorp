37a75d5aff61c6f9a5bcd504ef93bf90a6c66dfe
Dzmitry Malyshau <dmalyshau@mozilla.com>
2021-05-20 15:30:55 +0000

    Bug 1711868 - Only free WebGPU implicit layouts if they are given r=jimb
    Differential Revision: https://phabricator.services.mozilla.com/D115537

--- dom/webgpu/RenderPipeline.cpp
+++ dom/webgpu/RenderPipeline.cpp
@@ -31,8 +31,10 @@ void RenderPipeline::Cleanup() {
     auto bridge = mParent->GetBridge();
     if (bridge && bridge->IsOpen()) {
       bridge->SendRenderPipelineDestroy(mId);
-      bridge->SendImplicitLayoutDestroy(mImplicitPipelineLayoutId,
-                                        mImplicitBindGroupLayoutIds);
+      if (mImplicitPipelineLayoutId) {
+        bridge->SendImplicitLayoutDestroy(mImplicitPipelineLayoutId,
+                                          mImplicitBindGroupLayoutIds);
+      }
     }
   }
 }
