0538633bbb4cf4c2ecd03c72456af0b42ad2e0a7
Chris Martin <cmartin@mozilla.com>
2021-05-20 14:28:03 +0000

    Bug 1709383 - Gate Win32k Lockdown on whether WebRender is actually enabled r=bobowen
    Win32k Lockdown requires WebRender, but WR is not currently guaranteed
    on all computers. It can also fail to initialize and fallback to
    non-WR render path.
    
    We don't want a situation where "Win32k Lockdown + No WR" occurs without
    the user explicitly requesting unsupported behavior.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114849

--- security/sandbox/common/SandboxSettings.h
+++ security/sandbox/common/SandboxSettings.h
@@ -5,6 +5,7 @@
 
 #ifndef mozilla_SandboxSettings_h
 #define mozilla_SandboxSettings_h
+#include <cinttypes>
 
 namespace mozilla {
 
@@ -17,6 +18,18 @@ int GetEffectiveSocketProcessSandboxLevel();
 
 // Checks whether the effective content sandbox level is > 0.
 bool IsContentSandboxEnabled();
+
+enum class ContentWin32kLockdownState : int32_t {
+  LockdownEnabled = 1,
+  MissingWebRender,
+  OperatingSystemNotSupported,
+  PrefNotSet,
+};
+
+const char* ContentWin32kLockdownStateToString(
+    ContentWin32kLockdownState aValue);
+
+ContentWin32kLockdownState GetContentWin32kLockdownState();
 
 #if defined(XP_MACOSX)
 int ClampFlashSandboxLevel(const int aLevel);
