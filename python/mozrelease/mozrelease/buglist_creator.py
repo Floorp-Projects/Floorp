7f036d26ef452fde739af5b15a5313ac7f9e2693
Julien Cristau <jcristau@mozilla.com>
2021-05-21 12:51:41 +0000

    Bug 1712150 - take branches into account in release-started email notification. r=releng-reviewers,aki
    Instead of using the pushlog, grab the hg log using a revset to avoid
    being confused by branches: we're looking for revisions that are
    ancestors of the new revision but not of the previous tag, i.e.
    "current % previous".
    
    Differential Revision: https://phabricator.services.mozilla.com/D115624

--- python/mozrelease/mozrelease/buglist_creator.py
+++ python/mozrelease/mozrelease/buglist_creator.py
@@ -22,7 +22,7 @@ BUGZILLA_BUGLIST_TEMPLATE = "https://bugzilla.mozilla.org/buglist.cgi?bug_id={bu
 BUG_NUMBER_REGEX = re.compile(r"bug \d+", re.IGNORECASE)
 CHANGELOG_TO_FROM_STRING = "{product}_{version}_RELEASE"
 CHANGESET_URL_TEMPLATE = (
-    "{repo}/{logtype}" "?fromchange={from_version}&tochange={to_version}&full=1"
+    "{repo}/{logtype}" "?rev={to_version}+%25+{from_version}&revcount=1000"
 )
 FULL_CHANGESET_TEMPLATE = "* [Full Mercurial changelog]({url})\n"
 LIST_DESCRIPTION_TEMPLATE = "Comparing Mercurial tag {from_version} to {to_version}:\n"
@@ -66,7 +66,7 @@ def create_bugs_url(product, current_version, current_revision, repo=None):
                 repo=repo,
                 from_version=previous_version_tag,
                 to_version=current_revision,
-                logtype="json-pushes",
+                logtype="json-log",
             )
         )
         changeset_data = resp.json()
@@ -91,7 +91,7 @@ def create_bugs_url(product, current_version, current_revision, repo=None):
                 repo=repo,
                 from_version=previous_version_tag,
                 to_version=current_revision,
-                logtype="pushloghtml",
+                logtype="log",
             )
             description += FULL_CHANGESET_TEMPLATE.format(url=changeset_html)
 
@@ -106,21 +106,20 @@ def create_bugs_url(product, current_version, current_revision, repo=None):
 
 def get_bugs_in_changeset(changeset_data):
     unique_bugs, unique_backout_bugs = set(), set()
-    for data in changeset_data.values():
-        for changeset in data["changesets"]:
-            if is_excluded_change(changeset):
-                continue
+    for changeset in changeset_data["entries"]:
+        if is_excluded_change(changeset):
+            continue
 
-            changeset_desc = changeset["desc"]
-            bug_re = BUG_NUMBER_REGEX.search(changeset_desc)
+        changeset_desc = changeset["desc"]
+        bug_re = BUG_NUMBER_REGEX.search(changeset_desc)
 
-            if bug_re:
-                bug_number = bug_re.group().split(" ")[1]
+        if bug_re:
+            bug_number = bug_re.group().split(" ")[1]
 
-                if is_backout_bug(changeset_desc):
-                    unique_backout_bugs.add(bug_number)
-                else:
-                    unique_bugs.add(bug_number)
+            if is_backout_bug(changeset_desc):
+                unique_backout_bugs.add(bug_number)
+            else:
+                unique_bugs.add(bug_number)
 
     return unique_bugs, unique_backout_bugs
 
