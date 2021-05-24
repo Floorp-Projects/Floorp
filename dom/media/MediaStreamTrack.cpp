9138dbca37733d8f871ffb7ba2b5de4221ff1647
Olli Pettay <Olli.Pettay@helsinki.fi>
2021-05-24 18:27:21 +0000

    Bug 1711564, MediaStreamTrack should pass owner global to the DOMEventTargetHelper, r=jib
    Differential Revision: https://phabricator.services.mozilla.com/D115427

--- dom/media/MediaStreamTrack.cpp
+++ dom/media/MediaStreamTrack.cpp
@@ -118,12 +118,12 @@ class MediaStreamTrack::MTGListener : public MediaTrackListener {
       return;
     }
 
-    if (!mTrack->GetParentObject()) {
+    if (!mTrack->GetOwner()) {
       return;
     }
 
     AbstractThread* mainThread =
-        nsGlobalWindowInner::Cast(mTrack->GetParentObject())
+        nsGlobalWindowInner::Cast(mTrack->GetOwner())
             ->AbstractMainThreadFor(TaskCategory::Other);
     mainThread->Dispatch(NewRunnableMethod("MediaStreamTrack::OverrideEnded",
                                            mTrack.get(),
@@ -186,7 +186,7 @@ MediaStreamTrack::MediaStreamTrack(nsPIDOMWindowInner* aWindow,
                                    MediaStreamTrackState aReadyState,
                                    bool aMuted,
                                    const MediaTrackConstraints& aConstraints)
-    : mWindow(aWindow),
+    : DOMEventTargetHelper(aWindow),
       mInputTrack(aInputTrack),
       mSource(aSource),
       mSink(MakeUnique<TrackSink>(this)),
@@ -204,7 +204,7 @@ MediaStreamTrack::MediaStreamTrack(nsPIDOMWindowInner* aWindow,
     // MediaStreamTrackSource soon enough.
     auto graph = mInputTrack->IsDestroyed()
                      ? MediaTrackGraph::GetInstanceIfExists(
-                           mWindow, mInputTrack->mSampleRate,
+                           GetOwner(), mInputTrack->mSampleRate,
                            MediaTrackGraph::DEFAULT_OUTPUT_DEVICE)
                      : mInputTrack->Graph();
     MOZ_DIAGNOSTIC_ASSERT(graph,
@@ -252,7 +252,6 @@ NS_IMPL_CYCLE_COLLECTION_CLASS(MediaStreamTrack)
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(MediaStreamTrack,
                                                 DOMEventTargetHelper)
   tmp->Destroy();
-  NS_IMPL_CYCLE_COLLECTION_UNLINK(mWindow)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mSource)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mPrincipal)
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mPendingPrincipal)
@@ -261,7 +260,6 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(MediaStreamTrack,
                                                   DOMEventTargetHelper)
-  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mWindow)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mSource)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mPrincipal)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mPendingPrincipal)
@@ -341,7 +339,7 @@ already_AddRefed<Promise> MediaStreamTrack::ApplyConstraints(
                          this, NS_ConvertUTF16toUTF8(str).get()));
   }
 
-  nsIGlobalObject* go = mWindow ? mWindow->AsGlobal() : nullptr;
+  nsIGlobalObject* go = GetOwner() ? GetOwner()->AsGlobal() : nullptr;
 
   RefPtr<Promise> promise = Promise::Create(go, aRv);
   if (aRv.Failed()) {
@@ -361,18 +359,18 @@ already_AddRefed<Promise> MediaStreamTrack::ApplyConstraints(
       ->Then(
           GetCurrentSerialEventTarget(), __func__,
           [this, self, promise, aConstraints](bool aDummy) {
-            if (!mWindow || !mWindow->IsCurrentInnerWindow()) {
+            if (!GetOwner() || !GetOwner()->IsCurrentInnerWindow()) {
               return;  // Leave Promise pending after navigation by design.
             }
             mConstraints = aConstraints;
             promise->MaybeResolve(false);
           },
           [this, self, promise](const RefPtr<MediaMgrError>& aError) {
-            if (!mWindow || !mWindow->IsCurrentInnerWindow()) {
+            if (!GetOwner() || !GetOwner()->IsCurrentInnerWindow()) {
               return;  // Leave Promise pending after navigation by design.
             }
             promise->MaybeReject(
-                MakeRefPtr<MediaStreamError>(mWindow, *aError));
+                MakeRefPtr<MediaStreamError>(GetOwner(), *aError));
           });
   return promise.forget();
 }
