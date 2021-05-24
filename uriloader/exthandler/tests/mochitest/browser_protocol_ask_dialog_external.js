e54a86cede4a645cface593430f12b9010931ad8
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2021-05-24 16:02:50 +0000

    Bug 1700976 - do not prompt for externally-opened web- or extension-handled 'external' protocols, r=zombie
    Differential Revision: https://phabricator.services.mozilla.com/D115478

--- uriloader/exthandler/tests/mochitest/browser_protocol_ask_dialog_external.js
+++ uriloader/exthandler/tests/mochitest/browser_protocol_ask_dialog_external.js
@@ -11,42 +11,45 @@ let gHandlerService = Cc["@mozilla.org/uriloader/handler-service;1"].getService(
  * Creates dummy protocol handler
  */
 function initTestHandlers() {
-  let handlerInfo = HandlerServiceTestUtils.getBlankHandlerInfo("yoink");
+  let handlerInfoThatAsks = HandlerServiceTestUtils.getBlankHandlerInfo(
+    "should-ask"
+  );
 
   let appHandler = Cc[
     "@mozilla.org/uriloader/local-handler-app;1"
   ].createInstance(Ci.nsILocalHandlerApp);
   // This is a dir and not executable, but that's enough for here.
   appHandler.executable = Services.dirsvc.get("XCurProcD", Ci.nsIFile);
-  handlerInfo.possibleApplicationHandlers.appendElement(appHandler);
-  handlerInfo.preferredApplicationHandler = appHandler;
-  handlerInfo.preferredAction = handlerInfo.useHelperApp;
-  handlerInfo.alwaysAskBeforeHandling = false;
-  gHandlerService.store(handlerInfo);
+  handlerInfoThatAsks.possibleApplicationHandlers.appendElement(appHandler);
+  handlerInfoThatAsks.preferredApplicationHandler = appHandler;
+  handlerInfoThatAsks.preferredAction = handlerInfoThatAsks.useHelperApp;
+  handlerInfoThatAsks.alwaysAskBeforeHandling = false;
+  gHandlerService.store(handlerInfoThatAsks);
+
+  let webHandlerInfo = HandlerServiceTestUtils.getBlankHandlerInfo(
+    "web+somesite"
+  );
+  let webHandler = Cc[
+    "@mozilla.org/uriloader/web-handler-app;1"
+  ].createInstance(Ci.nsIWebHandlerApp);
+  webHandler.name = "Somesite";
+  webHandler.uriTemplate = "https://example.com/handle_url?u=%s";
+  webHandlerInfo.possibleApplicationHandlers.appendElement(webHandler);
+  webHandlerInfo.preferredApplicationHandler = webHandler;
+  webHandlerInfo.preferredAction = webHandlerInfo.useHelperApp;
+  webHandlerInfo.alwaysAskBeforeHandling = false;
+  gHandlerService.store(webHandlerInfo);
+
   registerCleanupFunction(() => {
-    gHandlerService.remove(handlerInfo);
+    gHandlerService.remove(webHandlerInfo);
+    gHandlerService.remove(handlerInfoThatAsks);
   });
 }
 
-/**
- * Check that if we get a direct request from another app / the OS to open a
- * link, we always prompt, even if we think we know what the correct answer
- * is. This is to avoid infinite loops in such situations where the OS and
- * Firefox have conflicting ideas about the default handler, or where our
- * checks with the OS don't work (Linux and/or Snap, at time of this comment).
- */
-add_task(async function test_external_asks_anyway() {
-  await SpecialPowers.pushPrefEnv({
-    set: [["network.protocol-handler.prompt-from-external", true]],
-  });
-  initTestHandlers();
-
-  let cmdLineHandler = Cc["@mozilla.org/browser/final-clh;1"].getService(
-    Ci.nsICommandLineHandler
-  );
-  let fakeCmdLine = {
+function makeCmdLineHelper(url) {
+  return {
     length: 1,
-    _arg: "yoink:yoink",
+    _arg: url,
 
     getArgument(aIndex) {
       if (aIndex == 0) {
@@ -81,10 +84,31 @@ add_task(async function test_external_asks_anyway() {
     },
     QueryInterface: ChromeUtils.generateQI(["nsICommandLine"]),
   };
+}
+
+add_task(async function setup() {
+  await SpecialPowers.pushPrefEnv({
+    set: [["network.protocol-handler.prompt-from-external", true]],
+  });
+  initTestHandlers();
+});
+
+/**
+ * Check that if we get a direct request from another app / the OS to open a
+ * link, we always prompt, even if we think we know what the correct answer
+ * is. This is to avoid infinite loops in such situations where the OS and
+ * Firefox have conflicting ideas about the default handler, or where our
+ * checks with the OS don't work (Linux and/or Snap, at time of this comment).
+ */
+add_task(async function test_external_asks_anyway() {
+  let cmdLineHandler = Cc["@mozilla.org/browser/final-clh;1"].getService(
+    Ci.nsICommandLineHandler
+  );
   let chooserDialogOpenPromise = waitForProtocolAppChooserDialog(
     gBrowser,
     true
   );
+  let fakeCmdLine = makeCmdLineHelper("should-ask:dummy");
   cmdLineHandler.handle(fakeCmdLine);
   let dialog = await chooserDialogOpenPromise;
   ok(dialog, "Should have prompted.");
@@ -98,4 +122,50 @@ add_task(async function test_external_asks_anyway() {
   await dialogClosedPromise;
   // We will have opened a tab; close it.
   BrowserTestUtils.removeTab(gBrowser.selectedTab);
+});
+
+/**
+ * Like the previous test, but avoid asking for web and extension handlers,
+ * as we can open those ourselves without looping.
+ */
+add_task(async function test_web_app_doesnt_ask() {
+  // Listen for a dialog open and fail the test if it does:
+  let dialogOpenListener = () => ok(false, "Shouldn't have opened a dialog!");
+  document.documentElement.addEventListener("dialogopen", dialogOpenListener);
+  registerCleanupFunction(() =>
+    document.documentElement.removeEventListener(
+      "dialogopen",
+      dialogOpenListener
+    )
+  );
+
+  // Set up a promise for a tab to open with the right URL:
+  const kURL = "web+somesite:dummy";
+  const kLoadedURL =
+    "https://example.com/handle_url?u=" + encodeURIComponent(kURL);
+  let tabPromise = BrowserTestUtils.waitForNewTab(gBrowser, kLoadedURL);
+
+  // Load the URL:
+  let cmdLineHandler = Cc["@mozilla.org/browser/final-clh;1"].getService(
+    Ci.nsICommandLineHandler
+  );
+  let fakeCmdLine = makeCmdLineHelper(kURL);
+  cmdLineHandler.handle(fakeCmdLine);
+
+  // Check that the tab loaded. If instead the dialog opened, the dialogopen handler
+  // will fail the test.
+  let tab = await tabPromise;
+  is(
+    tab.linkedBrowser.currentURI.spec,
+    kLoadedURL,
+    "Should have opened the right URL."
+  );
+  BrowserTestUtils.removeTab(tab);
+
+  // We do this both here and in cleanup so it's easy to add tasks to this test,
+  // and so we clean up correctly if the test aborts before we get here.
+  document.documentElement.removeEventListener(
+    "dialogopen",
+    dialogOpenListener
+  );
 });
