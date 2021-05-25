2f7c6934487c31c8a638f88772b8b3a4f107322c
Niklas Goegge <ngogge@mozilla.com>
2021-05-25 17:37:08 +0000

    Bug 1648825 - Add nsISHEntry::hasUserActivation r=ckerschb,smaug
    Differential Revision: https://phabricator.services.mozilla.com/D113818

--- docshell/shistory/SessionHistoryEntry.h
+++ docshell/shistory/SessionHistoryEntry.h
@@ -169,6 +169,7 @@ class SessionHistoryInfo {
   bool mScrollRestorationIsManual = false;
   bool mPersist = true;
   bool mHasUserInteraction = false;
+  bool mHasUserActivation = false;
 
   union SharedState {
     SharedState();
