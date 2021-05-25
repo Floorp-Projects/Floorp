8ef6ea3d5db56c547bb31f09fdd8c35739e81e54
Butkovits Atila <abutkovits@mozilla.com>
2021-05-25 19:52:49 +0300

    Backed out 2 changesets (bug 1709346) for causing failures on browser_bug1709346.js. CLOSED TREE
    Backed out changeset 19df1a8bfacc (bug 1709346)
    Backed out changeset 602a7b4a41de (bug 1709346)

--- docshell/base/CanonicalBrowsingContext.cpp
+++ docshell/base/CanonicalBrowsingContext.cpp
@@ -157,61 +157,16 @@ nsISecureBrowserUI* CanonicalBrowsingContext::GetSecureBrowserUI() {
   return mSecureBrowserUI;
 }
 
-namespace {
-// The DocShellProgressBridge is attached to a root content docshell loaded in
-// the parent process. Notifications are paired up with the docshell which they
-// came from, so that they can be fired to the correct
-// BrowsingContextWebProgress and bubble through this tree separately.
-//
-// Notifications are filtered by a nsBrowserStatusFilter before being received
-// by the DocShellProgressBridge.
-class DocShellProgressBridge : public nsIWebProgressListener {
- public:
-  NS_DECL_ISUPPORTS
-  // NOTE: This relies in the expansion of `NS_FORWARD_SAFE` and all listener
-  // methods accepting an `aWebProgress` argument. If this changes in the
-  // future, this may need to be written manually.
-  NS_FORWARD_SAFE_NSIWEBPROGRESSLISTENER(GetTargetContext(aWebProgress))
-
-  explicit DocShellProgressBridge(uint64_t aTopContextId)
-      : mTopContextId(aTopContextId) {}
-
- private:
-  virtual ~DocShellProgressBridge() = default;
-
-  nsIWebProgressListener* GetTargetContext(nsIWebProgress* aWebProgress) {
-    RefPtr<CanonicalBrowsingContext> context;
-    if (nsCOMPtr<nsIDocShell> docShell = do_QueryInterface(aWebProgress)) {
-      context = docShell->GetBrowsingContext()->Canonical();
-    } else {
-      context = CanonicalBrowsingContext::Get(mTopContextId);
-    }
-    return context && !context->IsDiscarded() ? context->GetWebProgress()
-                                              : nullptr;
-  }
-
-  uint64_t mTopContextId = 0;
-};
-
-NS_IMPL_ISUPPORTS(DocShellProgressBridge, nsIWebProgressListener)
-}  // namespace
-
 void CanonicalBrowsingContext::MaybeAddAsProgressListener(
     nsIWebProgress* aWebProgress) {
-  // Only add as a listener if the created docshell is a toplevel content
-  // docshell. We'll get notifications for all of our subframes through a single
-  // listener.
-  if (!IsTopContent()) {
+  if (!GetWebProgress()) {
     return;
   }
-
-  if (!mDocShellProgressBridge) {
-    mDocShellProgressBridge = new DocShellProgressBridge(Id());
+  if (!mStatusFilter) {
     mStatusFilter = new nsBrowserStatusFilter();
-    mStatusFilter->AddProgressListener(mDocShellProgressBridge,
+    mStatusFilter->AddProgressListener(GetWebProgress(),
                                        nsIWebProgress::NOTIFY_ALL);
   }
-
   aWebProgress->AddProgressListener(mStatusFilter, nsIWebProgress::NOTIFY_ALL);
 }
 
@@ -222,10 +177,9 @@ void CanonicalBrowsingContext::ReplacedBy(
   MOZ_ASSERT(!aNewContext->mSessionHistory);
   MOZ_ASSERT(IsTop() && aNewContext->IsTop());
   if (mStatusFilter) {
-    mStatusFilter->RemoveProgressListener(mDocShellProgressBridge);
+    mStatusFilter->RemoveProgressListener(mWebProgress);
     mStatusFilter = nullptr;
   }
-  mWebProgress->ContextReplaced(aNewContext);
   aNewContext->mWebProgress = std::move(mWebProgress);
 
   // Use the Transaction for the fields which need to be updated whether or not
@@ -2280,7 +2234,7 @@ bool CanonicalBrowsingContext::AllowedInBFCache(
 
 NS_IMPL_CYCLE_COLLECTION_INHERITED(CanonicalBrowsingContext, BrowsingContext,
                                    mSessionHistory, mContainerFeaturePolicy,
-                                   mCurrentBrowserParent, mWebProgress)
+                                   mCurrentBrowserParent)
 
 NS_IMPL_ADDREF_INHERITED(CanonicalBrowsingContext, BrowsingContext)
 NS_IMPL_RELEASE_INHERITED(CanonicalBrowsingContext, BrowsingContext)
