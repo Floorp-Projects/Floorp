cb03975a01179d6ce18bee0e9e0c39945b309079
Noemi Erli <nerli@mozilla.com>
2021-05-26 02:23:23 +0300

    Backed out changeset f8b8dd8a7814 (bug 1710751) for causing windows build bustages  CLOSED TREE

--- accessible/base/RelationType.h
+++ accessible/base/RelationType.h
@@ -154,12 +154,7 @@ enum class RelationType {
    */
   ERRORMSG_FOR = 0x18,
 
-  /**
-   * The target object is the anchor referenced by this link.
-   */
-  LINKS_TO = 0x19,
-
-  LAST = LINKS_TO
+  LAST = ERRORMSG_FOR
 };
 
 }  // namespace a11y
