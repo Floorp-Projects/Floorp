475a7b684891326f0d556f8be09c3e2a86598736
Lars T Hansen <lhansen@mozilla.com>
2021-05-25 14:23:24 +0000

    Bug 1712303 - Port strength reduction for i64 multiply to arm64. r=jseward
    Ported the strength reduction optimization for n*c when n is i64 and c
    is a friendly constant from x64 to arm64; a merging of these
    implementations (and the implementations for i32) will happen in a
    later bug.
    
    Added test cases that these optimizations are triggered properly for
    both i32 and i64.
    
    Differential Revision: https://phabricator.services.mozilla.com/D115696

--- js/src/jit-test/tests/wasm/binop-x64-ion-codegen.js
+++ js/src/jit-test/tests/wasm/binop-x64-ion-codegen.js
@@ -1,8 +1,10 @@
 // |jit-test| skip-if: !hasDisassembler() || wasmCompileMode() != "ion" || !getBuildConfiguration().x64 || getBuildConfiguration().simulator; include:codegen-x64-test.js
 
-// Test that multiplication by -1 yields negation.  There may be some redundant
-// moves to set that up (that's bug 1701164) so avoid those with
-// no_prefix/no_suffix.
+// There may be some redundant moves to set some operations up on x64 (that's
+// bug 1701164) so we avoid those with no_prefix/no_suffix flags when the issue
+// comes up.
+
+// Test that multiplication by -1 yields negation.
 
 let neg32 =
     `(module
@@ -11,7 +13,7 @@ let neg32 =
 codegenTestX64_adhoc(
     neg32,
     'f',
-    'f7 d8  neg %eax', {no_prefix:true, no_suffix:true});
+    'f7 d8  neg %eax', {no_prefix:true});
 assertEq(wasmEvalText(neg32).exports.f(-37), 37)
 assertEq(wasmEvalText(neg32).exports.f(42), -42)
 
@@ -22,6 +24,123 @@ let neg64 =
 codegenTestX64_adhoc(
     neg64,
     'f',
-    '48 f7 d8  neg %rax', {no_prefix:true, no_suffix:true});
+    '48 f7 d8  neg %rax', {no_prefix:true});
 assertEq(wasmEvalText(neg64).exports.f(-37000000000n), 37000000000n)
 assertEq(wasmEvalText(neg64).exports.f(42000000000n), -42000000000n)
+
+// Test that multiplication by zero yields zero
+
+let zero32 =
+    `(module
+       (func (export "f") (param i32) (result i32)
+         (i32.mul (local.get 0) (i32.const 0))))`;
+codegenTestX64_adhoc(
+    zero32,
+    'f',
+    '33 c0 xor %eax, %eax', {no_prefix:true});
+assertEq(wasmEvalText(zero32).exports.f(-37), 0)
+assertEq(wasmEvalText(zero32).exports.f(42), 0)
+
+let zero64 = `(module
+       (func (export "f") (param i64) (result i64)
+         (i64.mul (local.get 0) (i64.const 0))))`
+codegenTestX64_adhoc(
+    zero64,
+    'f',
+    '48 33 c0 xor %rax, %rax', {no_prefix:true});
+assertEq(wasmEvalText(zero64).exports.f(-37000000000n), 0n)
+assertEq(wasmEvalText(zero64).exports.f(42000000000n), 0n)
+
+// Test that multiplication by one yields no code
+
+let one32 =
+    `(module
+       (func (export "f") (param i32) (result i32)
+         (i32.mul (local.get 0) (i32.const 1))))`;
+codegenTestX64_adhoc(
+    one32,
+    'f',
+    '', {no_prefix:true});
+assertEq(wasmEvalText(one32).exports.f(-37), -37)
+assertEq(wasmEvalText(one32).exports.f(42), 42)
+
+let one64 = `(module
+       (func (export "f") (param i64) (result i64)
+         (i64.mul (local.get 0) (i64.const 1))))`
+codegenTestX64_adhoc(
+    one64,
+    'f',
+    '', {no_prefix:true});
+assertEq(wasmEvalText(one64).exports.f(-37000000000n), -37000000000n)
+assertEq(wasmEvalText(one64).exports.f(42000000000n), 42000000000n)
+
+// Test that multiplication by two yields an add
+
+let double32 =
+    `(module
+       (func (export "f") (param i32) (result i32)
+         (i32.mul (local.get 0) (i32.const 2))))`;
+codegenTestX64_adhoc(
+    double32,
+    'f',
+    '03 c0 add %eax, %eax', {no_prefix:true});
+assertEq(wasmEvalText(double32).exports.f(-37), -74)
+assertEq(wasmEvalText(double32).exports.f(42), 84)
+
+let double64 = `(module
+       (func (export "f") (param i64) (result i64)
+         (i64.mul (local.get 0) (i64.const 2))))`
+codegenTestX64_adhoc(
+    double64,
+    'f',
+    '48 03 c0 add %rax, %rax', {no_prefix:true});
+assertEq(wasmEvalText(double64).exports.f(-37000000000n), -74000000000n)
+assertEq(wasmEvalText(double64).exports.f(42000000000n), 84000000000n)
+
+// Test that multiplication by four yields a shift
+
+let quad32 =
+    `(module
+       (func (export "f") (param i32) (result i32)
+         (i32.mul (local.get 0) (i32.const 4))))`;
+codegenTestX64_adhoc(
+    quad32,
+    'f',
+    'c1 e0 02 shl \\$0x02, %eax', {no_prefix:true});
+assertEq(wasmEvalText(quad32).exports.f(-37), -148)
+assertEq(wasmEvalText(quad32).exports.f(42), 168)
+
+let quad64 = `(module
+       (func (export "f") (param i64) (result i64)
+         (i64.mul (local.get 0) (i64.const 4))))`
+codegenTestX64_adhoc(
+    quad64,
+    'f',
+    '48 c1 e0 02 shl \\$0x02, %rax', {no_prefix:true});
+assertEq(wasmEvalText(quad64).exports.f(-37000000000n), -148000000000n)
+assertEq(wasmEvalText(quad64).exports.f(42000000000n), 168000000000n)
+
+// Test that multiplication by five yields a multiply
+
+let quint32 =
+    `(module
+       (func (export "f") (param i32) (result i32)
+         (i32.mul (local.get 0) (i32.const 5))))`;
+codegenTestX64_adhoc(
+    quint32,
+    'f',
+    '6b c0 05 imul \\$0x05, %eax, %eax', {no_prefix:true});
+assertEq(wasmEvalText(quint32).exports.f(-37), -37*5)
+assertEq(wasmEvalText(quint32).exports.f(42), 42*5)
+
+let quint64 = `(module
+       (func (export "f") (param i64) (result i64)
+         (i64.mul (local.get 0) (i64.const 5))))`
+codegenTestX64_adhoc(
+    quint64,
+    'f',
+    // Bad code generation, 
+    `41 bb 05 00 00 00         mov \\$0x05, %r11d
+     49 0f af c3               imul %r11, %rax`, {no_prefix:true});
+assertEq(wasmEvalText(quint64).exports.f(-37000000000n), -37000000000n*5n)
+assertEq(wasmEvalText(quint64).exports.f(42000000000n), 42000000000n*5n)
