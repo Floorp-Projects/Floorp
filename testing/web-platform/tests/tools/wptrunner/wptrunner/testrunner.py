f9136d2ed098885146cb426076a1afa63d4885ed
Philip Jägenstedt <philip@foolip.org>
2021-05-26 20:29:01 +0000

    Bug 1710438 [wpt PR 28942] - Run mypy for tools/wptrunner/ (but still allow untyped defs), a=testonly
    Automatic update from web-platform-tests
    Run mypy for tools/wptrunner/ (but still allow untyped defs) (#28942)
    
    --
    
    wpt-commits: a5e1bc0e5b25b7a55e5f58641b524415b63ea95c
    wpt-pr: 28942

--- testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/testrunner.py
@@ -254,7 +254,7 @@ class BrowserManager(object):
 
 class _RunnerManagerState(object):
     before_init = namedtuple("before_init", [])
-    initializing = namedtuple("initializing_browser",
+    initializing = namedtuple("initializing",
                               ["test", "test_group", "group_metadata", "failure_count"])
     running = namedtuple("running", ["test", "test_group", "group_metadata"])
     restarting = namedtuple("restarting", ["test", "test_group", "group_metadata"])
