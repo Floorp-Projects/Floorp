7475b805ead8f237262c864ad91bf7a264ff725f
Peter Van der Beken <peterv@propagandism.org>
2021-05-26 17:11:33 +0000

    Bug 1712180 - Make GetOwnProperty on the Window named properties object return supported properties that are indices. r=evilpie
    Differential Revision: https://phabricator.services.mozilla.com/D115638

--- dom/base/WindowNamedPropertiesHandler.cpp
+++ dom/base/WindowNamedPropertiesHandler.cpp
@@ -77,7 +77,7 @@ bool WindowNamedPropertiesHandler::getOwnPropDescriptor(
     JS::MutableHandle<Maybe<JS::PropertyDescriptor>> aDesc) const {
   aDesc.reset();
 
-  if (!JSID_IS_STRING(aId)) {
+  if (aId.isSymbol()) {
     if (aId.isWellKnownSymbol(JS::SymbolCode::toStringTag)) {
       JS::Rooted<JSString*> toStringTagStr(
           aCx, JS_NewStringCopyZ(aCx, "WindowProperties"));
@@ -91,7 +91,7 @@ bool WindowNamedPropertiesHandler::getOwnPropDescriptor(
       return true;
     }
 
-    // Nothing to do if we're resolving another non-string property.
+    // Nothing to do if we're resolving another symbol property.
     return true;
   }
 
@@ -104,7 +104,7 @@ bool WindowNamedPropertiesHandler::getOwnPropDescriptor(
   }
 
   nsAutoJSString str;
-  if (!str.init(aCx, JSID_TO_STRING(aId))) {
+  if (!str.init(aCx, aId)) {
     return false;
   }
 
