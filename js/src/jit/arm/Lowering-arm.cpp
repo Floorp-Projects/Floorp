a6671b3ca32ab80ed7e87bfdbf1ba010c148fda5
Yury Delendik <ydelendik@mozilla.com>
2021-05-27 22:04:57 +0000

    Bug 1690492 - Use PBLENDVB for blend operations. r=lth
    Differential Revision: https://phabricator.services.mozilla.com/D113582

--- js/src/jit/arm/Lowering-arm.cpp
+++ js/src/jit/arm/Lowering-arm.cpp
@@ -1173,6 +1173,13 @@ void LIRGenerator::visitWasmBinarySimd128(MWasmBinarySimd128* ins) {
   MOZ_CRASH("binary SIMD NYI");
 }
 
+#ifdef ENABLE_WASM_SIMD
+bool MWasmBitselectSimd128::specializeConstantMaskAsShuffle(
+    int8_t shuffle[16]) {
+  return false;
+}
+#endif
+
 bool MWasmBinarySimd128::specializeForConstantRhs() {
   // Probably many we want to do here
   return false;
