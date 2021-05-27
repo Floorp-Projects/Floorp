0f718b825e25adaa4d521cdc1f32c0733f94ac0b
Mark Banner <standard8@mozilla.com>
2021-05-27 07:21:25 +0000

    Bug 1712297 - Add test for checking pinned search shortcuts are hidden when the associate engine is removed. r=amy,dao
    Differential Revision: https://phabricator.services.mozilla.com/D115807

--- browser/components/newtab/test/browser/browser_topsites_section.js
+++ browser/components/newtab/test/browser/browser_topsites_section.js
@@ -191,7 +191,7 @@ test_newtab({
     const searchTopSites = content.document.querySelectorAll(".title.pinned");
     ok(
       searchTopSites.length >= 1,
-      "There should be at least 2 search topsites"
+      "There should be at least 1 search topsites"
     );
 
     searchTopSites[0].click();
@@ -222,4 +222,78 @@ test_newtab({
       "The Urlbar has the searchmode attribute."
     );
   },
+});
+
+// test_newtab is not used here as this test requires two steps into the
+// content process with chrome process activity in-between.
+add_task(async function test_search_topsite_remove_engine() {
+  // Open about:newtab without using the default load listener
+  let tab = await BrowserTestUtils.openNewForegroundTab(
+    gBrowser,
+    "about:newtab",
+    false
+  );
+
+  // Specially wait for potentially preloaded browsers
+  let browser = tab.linkedBrowser;
+  await waitForPreloaded(browser);
+
+  // Add shared helpers to the content process
+  SpecialPowers.spawn(browser, [], addContentHelpers);
+
+  // Wait for React to render something
+  await BrowserTestUtils.waitForCondition(
+    () =>
+      SpecialPowers.spawn(
+        browser,
+        [],
+        () => content.document.getElementById("root").children.length
+      ),
+    "Should render activity stream content"
+  );
+
+  await setDefaultTopSites();
+
+  let [topSiteAlias, numTopSites] = await SpecialPowers.spawn(
+    browser,
+    [],
+    async () => {
+      await ContentTaskUtils.waitForCondition(
+        () => content.document.querySelector(".search-shortcut .title.pinned"),
+        "Wait for pinned search topsites"
+      );
+
+      const searchTopSites = content.document.querySelectorAll(".title.pinned");
+      ok(searchTopSites.length >= 1, "There should be at least one topsite");
+      return [searchTopSites[0].innerText.trim(), searchTopSites.length];
+    }
+  );
+
+  await Services.search.removeEngine(
+    await Services.search.getEngineByAlias(topSiteAlias)
+  );
+
+  registerCleanupFunction(() => {
+    Services.search.restoreDefaultEngines();
+  });
+
+  await SpecialPowers.spawn(
+    browser,
+    [numTopSites],
+    async originalNumTopSites => {
+      await ContentTaskUtils.waitForCondition(
+        () => !content.document.querySelector(".search-shortcut .title.pinned"),
+        "Wait for pinned search topsites"
+      );
+
+      const searchTopSites = content.document.querySelectorAll(".title.pinned");
+      is(
+        searchTopSites.length,
+        originalNumTopSites - 1,
+        "There should be one less search topsites"
+      );
+    }
+  );
+
+  BrowserTestUtils.removeTab(tab);
 });
