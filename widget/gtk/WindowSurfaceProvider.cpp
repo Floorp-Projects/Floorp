0adc9c8175bcc7cbc1da6adbfe40f9fe18836ac5
Dorel Luca <dluca@mozilla.com>
2021-05-27 10:33:48 +0300

    Backed out changeset 84818e46d11d (bug 1710436) for Mochitest failures in toolkit/content/tests/chrome/test_panel.xhtml. CLOSED TREE

--- widget/gtk/WindowSurfaceProvider.cpp
+++ widget/gtk/WindowSurfaceProvider.cpp
@@ -21,17 +21,6 @@
 #  include "WindowSurfaceXRender.h"
 #endif
 
-#undef LOG
-#ifdef MOZ_LOGGING
-#  include "mozilla/Logging.h"
-#  include "nsTArray.h"
-#  include "Units.h"
-extern mozilla::LazyLogModule gWidgetLog;
-#  define LOG(args) MOZ_LOG(gWidgetLog, mozilla::LogLevel::Debug, args)
-#else
-#  define LOG(args)
-#endif /* MOZ_LOGGING */
-
 namespace mozilla {
 namespace widget {
 
