09028e14bc3d548b3e4f8ed41a01e17b98843ac3
Butkovits Atila <abutkovits@mozilla.com>
2021-05-27 21:32:39 +0300

    Backed out changeset 570a1c32aff5 (bug 1700763) for causing Gecko Decision Task bustage. CLOSED TREE

--- testing/mozharness/configs/builds/releng_sub_android_configs/64_x86_64_debug_isolated_process.py
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-config = {
-    "stage_platform": "android-x86_64-debug",
-    "src_mozconfig": "mobile/android/config/mozconfigs/android-x86_64/debug-isolated-process",
-    "debug_build": True,
-}
