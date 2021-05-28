d6a845aca8203b41fcb161cdf8021ddf5df44fd4
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-05-28 04:19:06 +0000

    Bug 1695817 - Part 2: Introduce a blank page about:third-party r=Gijs,fluent-reviewers,xpcom-reviewers,kmag
    This patch adds a blank page about:third-party along with a skeleton XPCOM
    object AboutThirdParty which has a method to invoke a background task in C++.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109303

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -240,6 +240,11 @@ var whitelist = [
     file:
       "resource://gre/localization/en-US/toolkit/updates/backgroundupdate.ftl",
   },
+  // Bug 1713242 - referenced by aboutThirdParty.html which is only for Windows
+  {
+    file: "resource://gre/localization/en-US/toolkit/about/aboutThirdParty.ftl",
+    platforms: ["linux", "macosx"],
+  },
 ];
 
 if (AppConstants.NIGHTLY_BUILD && AppConstants.platform != "win") {
