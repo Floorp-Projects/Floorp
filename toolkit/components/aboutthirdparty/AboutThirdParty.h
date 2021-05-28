66e63b1aa40d0d827308bf239f70c75bcc83c70e
Toshihito Kikuchi <tkikuchi@mozilla.com>
2021-05-28 22:35:58 +0000

    Bug 1695817 - Part 4: Label a module as ShellExtension or IME.  r=Gijs,mhowell
    This patch adds a feature to mark each module in the about:third-party page
    as a shell extension or an IME if it is so.  To achieve this, when the page
    is loaded, it starts a background task to collect registered shell extensions
    from the registry and adds a tag next to a module's name.
    
    Differential Revision: https://phabricator.services.mozilla.com/D109305

--- toolkit/components/aboutthirdparty/AboutThirdParty.h
+++ toolkit/components/aboutthirdparty/AboutThirdParty.h
@@ -9,8 +9,25 @@
 
 #include "mozilla/MozPromise.h"
 #include "nsIAboutThirdParty.h"
+#include "nsTHashMap.h"
 
 namespace mozilla {
+
+enum class KnownModuleType : uint32_t {
+  Ime = 0,
+  IconOverlay,
+  ContextMenuHandler,
+  CopyHookHandler,
+  DragDropHandler,
+  PropertySheetHandler,
+  DataHandler,
+  DropHandler,
+  IconHandler,
+  InfotipHandler,
+  PropertyHandler,
+
+  Last,
+};
 
 using BackgroundThreadPromise =
     MozPromise<bool /* aIgnored */, nsresult, /* IsExclusive */ false>;
@@ -24,9 +41,11 @@ class AboutThirdParty final : public nsIAboutThirdParty {
   };
   Atomic<WorkerState, SequentiallyConsistent> mWorkerState;
   RefPtr<BackgroundThreadPromise::Private> mPromise;
+  nsTHashMap<nsStringCaseInsensitiveHashKey, uint32_t> mKnownModules;
 
   ~AboutThirdParty() = default;
   void BackgroundThread();
+  void AddKnownModule(const nsString& aPath, KnownModuleType aType);
 
  public:
   static already_AddRefed<AboutThirdParty> GetSingleton();
