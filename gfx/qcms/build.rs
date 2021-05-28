f085f3e1e7379069f2069507d6319c57cc7b62bd
Jeff Muizelaar <jmuizelaar@mozilla.com>
2021-05-28 20:18:23 +0000

    Bug 1713012. Only set RUSTC_BOOTSTRAP if it's not already set. r=aosmond
    In bug 1670538 we started setting RUSTC_BOOTSTRAP for Rust 1.50+
    Rust 1.50+ gives a warning if we set in build.rs so don't.
    
    Differential Revision: https://phabricator.services.mozilla.com/D116131

--- gfx/qcms/build.rs
+++ gfx/qcms/build.rs
@@ -1,6 +1,11 @@
 fn main() {
     if cfg!(feature = "neon") {
-        println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
+        if std::env::var_os("RUSTC_BOOTSTRAP").is_none() {
+            // With Rust 1.50 and newer RUSTC_BOOTSTAP should
+            // be set externally so we only set it if it's
+            // not already set.
+            println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
+        }
     }
     let target = std::env::var("TARGET").expect("TARGET environment variable not defined");
     if target.contains("neon") {
